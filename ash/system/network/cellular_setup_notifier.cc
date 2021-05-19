67f1825c1acdf81afdd1d369d9f953b69282af0c
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-05-19 23:25:34 +0000

    [CrOS cellular] Add RTL icon for add cellular network button
    This CL adds a new icon for RTL languages for add cellular network
    button in quick settings.
    
    Screenshot: https://screenshot.googleplex.com/7UZaK5TiV99Rnbz.png
    
    Fixed: 1207636
    Change-Id: I527c253a3f331909ab89703174fe00f132bc2646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906652
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884747}

--- ash/system/network/cellular_setup_notifier.cc
+++ ash/system/network/cellular_setup_notifier.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/network_config_service.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "ash/public/cpp/system_tray_client.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -184,7 +185,7 @@ void CellularSetupNotifier::ShowCellularSetupNotification() {
           message_center::RichNotificationData(),
           base::MakeRefCounted<message_center::HandleNotificationClickDelegate>(
               base::BindRepeating(&OnCellularSetupNotificationClicked)),
-          vector_icons::kAddCellularNetworkIcon,
+          kAddCellularNetworkIcon,
           message_center::SystemNotificationWarningLevel::NORMAL);
 
   message_center::MessageCenter* message_center =
--- ash/system/network/network_section_header_view.cc
+++ ash/system/network/network_section_header_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/metrics/user_metrics_recorder.h"
 #include "ash/public/cpp/system_tray_client.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -354,10 +355,12 @@ void MobileSectionHeaderView::PerformAddExtraButtons(bool enabled) {
   int tooltip_message_id = IsCellularDeviceInhibited()
                                ? IDS_ASH_STATUS_TRAY_INHIBITED_CELLULAR
                                : IDS_ASH_STATUS_TRAY_ADD_CELLULAR_LABEL;
+  const gfx::VectorIcon& icon = base::i18n::IsRTL() ? kAddCellularNetworkRtlIcon
+                                                    : kAddCellularNetworkIcon;
   add_esim_button_ = new TopShortcutButton(
       base::BindRepeating(&MobileSectionHeaderView::AddCellularButtonPressed,
                           base::Unretained(this)),
-      vector_icons::kAddCellularNetworkIcon, tooltip_message_id);
+      icon, tooltip_message_id);
 
   add_esim_button_->SetEnabled(enabled && !IsCellularDeviceInhibited());
 
