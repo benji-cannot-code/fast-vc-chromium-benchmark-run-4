807831fd061bc51eaa317a05b012cf4d3c94788a
Hans Wennborg <hans@chromium.org>
2021-05-19 15:00:06 +0000

    Add missing include to libassistant_service.cc
    See e.g. https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/14454
    
    Bug: none
    Change-Id: Ic0084a4f92c803a24bcedeee495b4f6e5c390654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905664
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Owners-Override: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884476}

--- chromeos/services/libassistant/libassistant_service.cc
+++ chromeos/services/libassistant/libassistant_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "chromeos/services/libassistant/libassistant_factory.h"
+#include "chromeos/services/libassistant/public/mojom/speech_recognition_observer.mojom.h"
 #include "libassistant/shared/internal_api/assistant_manager_internal.h"
 #include "libassistant/shared/public/assistant_manager.h"
 
