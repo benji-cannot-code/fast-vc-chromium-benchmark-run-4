0cfe34e3741ca497f36cddf27eebc8c7d54b5680
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-05-19 14:26:50 +0000

    Add retry logic in NukeProfileFromDisk()
    NukeProfileFromDisk() fails to delete the entire directory surprisingly
    often, so it could leave behind sensitive files. The most common cause
    seems to be LevelDB holding locks to files in the profile directory.
    
    Make NukeProfileFromDisk() try up to 3 times (with a 1-second delay)
    before giving up, and record metrics so we can measure how many users
    are affected by this.
    
    Bug: 1191455
    Change-Id: I07bed6ff445c0b0f6acfbe26adca6911b1ad49ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2826151
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884463}

--- chrome/browser/ui/webui/profile_helper_browsertest.cc
+++ chrome/browser/ui/webui/profile_helper_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_timeouts.h"
 #include "build/build_config.h"
 #include "chrome/browser/apps/platform_apps/shortcut_manager.h"
 #include "chrome/browser/browser_features.h"
@@ -268,8 +269,16 @@ IN_PROC_BROWSER_TEST_P(ProfileHelperTestWithDestroyProfile,
   EXPECT_TRUE(base::Contains(*browser_list, original_browser));
   EXPECT_EQ(1u, storage.GetNumberOfProfiles());
 
-  // TODO(crbug.com/1191455): Once RemoveProfile()/NukeProfileFromDisk() aren't
-  // flaky anymore, EXPECT_FALSE(PathExists(additional_profile_dir)).
+  if (destroy_profile) {
+    // Check that NukeProfileFromDisk() works correctly.
+    base::ScopedAllowBlockingForTesting allow_blocking;
+    base::Time start = base::Time::Now();
+    while (base::PathExists(additional_profile_dir) &&
+           base::Time::Now() - start < TestTimeouts::action_timeout()) {
+      base::RunLoop().RunUntilIdle();
+    }
+    EXPECT_FALSE(base::PathExists(additional_profile_dir));
+  }
 }
 
 #if defined(OS_CHROMEOS)
