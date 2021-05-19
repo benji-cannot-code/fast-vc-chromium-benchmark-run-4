ee92ca56d9ead94db94e588797278e1afa8034cc
Adam Langley <agl@chromium.org>
2021-05-19 18:32:07 +0000

    webauthn: try to reduce compile times.
    Try to fix a few of the biggest cases of WebAuthn headers causing excess
    compile time. Based on
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    Change-Id: I4705999fabe9feacebdf98aeea60b9234269ee0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2885630
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#884586}

--- content/browser/webauth/authenticator_environment_impl.h
+++ content/browser/webauth/authenticator_environment_impl.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/authenticator_environment.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
-#include "third_party/blink/public/mojom/webauthn/virtual_authenticator.mojom.h"
+#include "third_party/blink/public/mojom/webauthn/virtual_authenticator.mojom-forward.h"
 
 namespace device {
 class FidoDiscoveryFactory;
