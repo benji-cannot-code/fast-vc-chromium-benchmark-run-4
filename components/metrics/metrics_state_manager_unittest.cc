b5c9e52ef15a63a58b9bd1859ec30bf5c7d9cb75
Caitlin Fischer <caitlinfischer@google.com>
2021-05-19 19:54:05 +0000

    Add a MetricsStateManager func that updates kStabilityExitedCleanly.
    This is the first change in a series of changes for setting up a client-
    side experiment for extending Variations Safe Mode. The main goal of
    this change is to add a MetricsStateManager function that wraps this:
    clean_exit_beacon_->WriteBeaconValue(false). In a later change, this
    will be used to set kStabilityExitedCleanly to false earlier than in the
    status quo code.
    
    MetricsService::LogNeedForCleanShutdown() still exists because it is
    still called here [1], where there is no direct access to
    MetricsStateManager. We wish to keep this call for the experiment's
    control group, which changes neither when kStabilityExitedCleanly is set
    to false during startup nor when prefs are written to disk. However, the
    plan is to eventually remove MetricsService::LogNeedForCleanShutdown()
    after the variations-safe-mode-related experiment.
    
    This change also updates some Chromecast-related code. Some comments
    mention the clean_shutdown bit, which was removed in crrev/c/2872668.
    The call to MetricsService::LogNeedForCleanShutdown() was replaced with
    MetricsStateManager::LogHasSessionShutdownCleanly().
    
    Lastly, this CL removes a trivial helper function,
    MarkAppCleanShutdownAndCommit(), and adds some trivial unit tests.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/chrome_browser_main.cc?q=logneedforcleanshutdown%20file:.*chrome_browser_main.cc$&ss=chromium
    
    Bug: b/184937096
    Change-Id: Ife2472e648cb9d604813ed24b9f8643562cb8776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2898079
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#884639}

--- components/metrics/metrics_state_manager_unittest.cc
+++ components/metrics/metrics_state_manager_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace metrics {
-
 namespace {
 
 // Verifies that the client id follows the expected pattern.
@@ -221,6 +220,22 @@ TEST_F(MetricsStateManagerTest, ResetMetricsIDs) {
   }
 
   EXPECT_NE(kInitialClientId, prefs_.GetString(prefs::kMetricsClientID));
+}
+
+TEST_F(MetricsStateManagerTest, LogHasSessionShutdownCleanly) {
+  std::unique_ptr<MetricsStateManager> state_manager(CreateStateManager());
+  prefs_.SetBoolean(prefs::kStabilityExitedCleanly, false);
+  state_manager->LogHasSessionShutdownCleanly(
+      /*has_session_shutdown_cleanly=*/true);
+  EXPECT_TRUE(prefs_.GetBoolean(prefs::kStabilityExitedCleanly));
+}
+
+TEST_F(MetricsStateManagerTest, LogSessionHasNotYetShutdownCleanly) {
+  std::unique_ptr<MetricsStateManager> state_manager(CreateStateManager());
+  ASSERT_TRUE(prefs_.GetBoolean(prefs::kStabilityExitedCleanly));
+  state_manager->LogHasSessionShutdownCleanly(
+      /*has_session_shutdown_cleanly=*/false);
+  EXPECT_FALSE(prefs_.GetBoolean(prefs::kStabilityExitedCleanly));
 }
 
 TEST_F(MetricsStateManagerTest, ForceClientIdCreation) {
