b1dd72d903e0d5bbfb2cb62e29c9b3ee8d58903c
Fabio Rocha <fabio.rocha@microsoft.com>
2021-05-19 19:49:29 +0000

    desktop-pwas: Multi-profile support for web app protocol handlers
    This CL refactors Protocol Handling for web apps in order to add support
    for protocols to be handled by apps installed in multiple profiles.
    
    Previously, web app protocol handlers relied on the browser OS
    registration to register new protocols with the OS. Since an app
    registered with Windows (e.g.: the browser), can only register one
    handler per protocol, this created a problem for when web apps wanted to
    handle the same protocol. This was also a problem when the same app in
    multiple profiles was installed.
    
    We solved this by creating a custom disambiguation dialog that would let
    the user pick the right app to handle a protocol launch, and registering
    the browser as the handler with the OS instead. However, that solution
    has many disadvantages, particularly around UX. More about that in the
    one-pager linked below.
    
    In this CL, web apps are registered directly with the OS (via shell_util
    functions) and since multiple apps (or same app in different profiles)
    are different registrations, conflicts are handled by the Windows
    disambiguation ("Open With") dialog. On top of the UX improvement, there
    is substantially less code needed to maintain protocol handlers for web
    apps in the browser.
    
    One-pager: https://docs.google.com/document/d/1ksfUr31h1YYNdsAswwQ5_-567RukXP_i3uQ2c4kJYYo/edit?usp=sharing&resourcekey=0-Lve5NUPuyZJ_ZPcE8g0HEw
    
    Bug: 1019239
    Change-Id: Icf59155432dcc8f9d728144ed357f777c73bfd32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2830297
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Fabio Rocha <fabio.rocha@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#884634}

--- chrome/browser/web_applications/components/web_app_protocol_handler_registration.h
+++ chrome/browser/web_applications/components/web_app_protocol_handler_registration.h
@@ -27,10 +27,7 @@ void RegisterProtocolHandlersWithOs(
     std::vector<apps::ProtocolHandlerInfo> protocol_handlers,
     base::OnceCallback<void(bool)> callback);
 
-void UnregisterProtocolHandlersWithOs(
-    const AppId& app_id,
-    Profile* profile,
-    std::vector<apps::ProtocolHandlerInfo> protocol_handlers);
+void UnregisterProtocolHandlersWithOs(const AppId& app_id, Profile* profile);
 
 }  // namespace web_app
 
