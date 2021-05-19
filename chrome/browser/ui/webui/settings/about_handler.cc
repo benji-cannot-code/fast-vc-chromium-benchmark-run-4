ce203ab4340061701d91c66ebb49a3dea51ce97f
Anastasiia N <anastasiian@chromium.org>
2021-05-19 17:34:54 +0000

    CodeHealth: Remove use of ListValue::empty in /chrome/browser/ui/webui/settings
    This change removes the use of `ListValue::empty()`
    and replaces with `Value::GetList().empty()`.
    
    This CL was uploaded by git cl split.
    
    R=dschuyler@chromium.org
    
    Bug: 1187065
    Change-Id: Idc7fad2936ea9370b1577aa033add36811f8c3d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886005
    Auto-Submit: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884558}

--- chrome/browser/ui/webui/settings/about_handler.cc
+++ chrome/browser/ui/webui/settings/about_handler.cc
@@ -418,7 +418,7 @@ void AboutHandler::PromoteUpdater(const base::ListValue* args) {
 #endif
 
 void AboutHandler::HandleOpenFeedbackDialog(const base::ListValue* args) {
-  DCHECK(args->empty());
+  DCHECK(args->GetList().empty());
   Browser* browser =
       chrome::FindBrowserWithWebContents(web_ui()->GetWebContents());
   chrome::OpenFeedbackDialog(browser,
@@ -426,7 +426,7 @@ void AboutHandler::HandleOpenFeedbackDialog(const base::ListValue* args) {
 }
 
 void AboutHandler::HandleOpenHelpPage(const base::ListValue* args) {
-  DCHECK(args->empty());
+  DCHECK(args->GetList().empty());
   Browser* browser =
       chrome::FindBrowserWithWebContents(web_ui()->GetWebContents());
   chrome::ShowHelp(browser, chrome::HELP_SOURCE_WEBUI);
@@ -434,7 +434,7 @@ void AboutHandler::HandleOpenHelpPage(const base::ListValue* args) {
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 void AboutHandler::HandleOpenDiagnostics(const base::ListValue* args) {
-  DCHECK(args->empty());
+  DCHECK(args->GetList().empty());
   chrome::ShowDiagnosticsApp(profile_);
 }
 
@@ -452,7 +452,7 @@ void AboutHandler::HandleCheckInternetConnection(const base::ListValue* args) {
 }
 
 void AboutHandler::HandleLaunchReleaseNotes(const base::ListValue* args) {
-  DCHECK(args->empty());
+  DCHECK(args->GetList().empty());
   // We can always show the release notes since the Help app caches it, or can
   // show an appropriate error state (e.g. No internet connection).
   base::RecordAction(base::UserMetricsAction("ReleaseNotes.LaunchedAboutPage"));
@@ -461,7 +461,7 @@ void AboutHandler::HandleLaunchReleaseNotes(const base::ListValue* args) {
 }
 
 void AboutHandler::HandleOpenOsHelpPage(const base::ListValue* args) {
-  DCHECK(args->empty());
+  DCHECK(args->GetList().empty());
   Browser* browser =
       chrome::FindBrowserWithWebContents(web_ui()->GetWebContents());
   chrome::ShowHelp(browser, chrome::HELP_SOURCE_WEBUI_CHROME_OS);
