26e94b430d705b98bee8aadcff1f790042f52a43
Dominic Farolino <dom@chromium.org>
2021-05-19 04:06:42 +0000

    Fenced Frames: Initial implementation with ShadowDOMDelegate
    This CL introduces:
     - A base::FeatureParam for the fenced frame feature to run in either
       shadow DOM mode, or MPArch mode
     - A pure virtual interface FencedFrameDelegate owned by
       HTMLFencedFrameElement
     - A single concrete implementation of this called
       FencedFrameShadowDOMDelegate, that implements the initial ShadowDOM
       version of Fenced Frames
     - Unit tests for FencedFrameShadowDOMDelegate
    
    The design of everything in this CL is described in:
    https://docs.google.com/document/d/1ijTZJT3DHQ1ljp4QQe4E4XCCRaYAxmInNzN1SzeJM8s/edit
    
    Bug: 1123606
    Change-Id: I1698034ae218f5aaf4be921ce4a0823efc190a32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873160
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884305}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -590,6 +590,19 @@ const FeatureEntry::FeatureVariation kIntensiveWakeUpThrottlingVariations[] = {
      base::size(kIntensiveWakeUpThrottlingAfter10Seconds), nullptr},
 };
 
+const FeatureEntry::FeatureParam kFencedFramesImplementationTypeShadowDOM[] = {
+    {"implementation_type", "shadow_dom"}};
+const FeatureEntry::FeatureParam kFencedFramesImplementationTypeMPArch[] = {
+    {"implementation_type", "mparch"}};
+
+const FeatureEntry::FeatureVariation
+    kFencedFramesImplementationTypeVariations[] = {
+        {"with ShadowDOM", kFencedFramesImplementationTypeShadowDOM,
+         base::size(kFencedFramesImplementationTypeShadowDOM), nullptr},
+        {"with multiple page architecture",
+         kFencedFramesImplementationTypeMPArch,
+         base::size(kFencedFramesImplementationTypeMPArch), nullptr}};
+
 #if defined(OS_ANDROID)
 const FeatureEntry::FeatureParam kCloseTabSuggestionsStale_Immediate[] = {
     {"baseline_tab_suggestions", "true"},
@@ -5543,7 +5556,9 @@ const FeatureEntry kFeatureEntries[] = {
 
     {"enable-fenced-frames", flag_descriptions::kEnableFencedFramesName,
      flag_descriptions::kEnableFencedFramesDescription, kOsAll,
-     FEATURE_VALUE_TYPE(blink::features::kFencedFrames)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(blink::features::kFencedFrames,
+                                    kFencedFramesImplementationTypeVariations,
+                                    "FencedFrames")},
 
     {"enable-portals", flag_descriptions::kEnablePortalsName,
      flag_descriptions::kEnablePortalsDescription, kOsAll,
---
+++ third_party/blink/renderer/core/html/fenced_frame/fenced_frame_shadow_dom_delegate.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FENCED_FRAME_FENCED_FRAME_SHADOW_DOM_DELEGATE_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FENCED_FRAME_FENCED_FRAME_SHADOW_DOM_DELEGATE_H_
+
+#include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/html/fenced_frame/html_fenced_frame_element.h"
+
+namespace blink {
+
+class KURL;
+
+class CORE_EXPORT FencedFrameShadowDOMDelegate
+    : public HTMLFencedFrameElement::FencedFrameDelegate {
+ public:
+  explicit FencedFrameShadowDOMDelegate(HTMLFencedFrameElement* outer_element);
+
+  void DidGetInserted() override;
+  void Navigate(const KURL&) override;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_HTML_FENCED_FRAME_FENCED_FRAME_SHADOW_DOM_DELEGATE_H_
