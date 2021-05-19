5d00dd67da971cb67d8849e0dcd921d887d17325
Zentaro Kavanagh <zentaro@chromium.org>
2021-05-19 03:56:23 +0000

    Handle Dead keys in shortcuts by resetting input method
    - On some keyboard layouts there are special keys that act
      like one-time modifiers for the key that follows after them.
      These are "dead keys" or "compose keys".
    - These keys are pressed and released not chorded like traditional
      modifiers. When pressed the first time there is no output, but an
      internal state is set waiting for the following key.
    - A simple example is the acute key on many European layouts which
      if followed by certain vowels such as e, produces e-acute [é]
    - When an accelerator uses a dead key as part of it's chord it can
      leave the input in a partially composed state which was not the users
      intent. The result is if the user types a valid secondary key to the
      composition before switching focus the composed key will be produced.
    - Whenever an accelerator is matched the composition state should be
      reset.
    
    Bug: 1174326
    Test: manual
    Change-Id: I35437529ddf479e0ec8566d30f6a3a1bdf797b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2882235
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884287}

--- ash/accelerators/accelerator_controller_impl.cc
+++ ash/accelerators/accelerator_controller_impl.cc
@@ -2506,6 +2506,14 @@ void AcceleratorControllerImpl::PerformAction(
       HandleTopWindowMinimizeOnBack();
       break;
   }
+
+  // Reset any in progress composition.
+  if (::features::IsImprovedKeyboardShortcutsEnabled()) {
+    auto* input_method =
+        Shell::Get()->window_tree_host_manager()->input_method();
+
+    input_method->CancelComposition(input_method->GetTextInputClient());
+  }
 }
 
 bool AcceleratorControllerImpl::ShouldActionConsumeKeyEvent(
