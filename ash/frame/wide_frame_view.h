8705b2f771b5341b07f41a43640bf6ece989973d
chinsenj <chinsenj@chromium.org>
2021-05-19 21:35:55 +0000

    cros: Add the Move to Desks menu to `WideFrameView`.
    Currently the Move to Desks context menu is added to
    `NonClientFrameViewAsh`, but not `WideFrameView`. This can lead to
    situations where a user maximizes an ARC app and loses access to the
    context menu. This CL fixes this by adding a common class that handles
    showing the context menu and using that newly added class in
    `NonClientFrameViewAsh` and `WideFrameView`.
    
    Bug: 1210144
    Change-Id: I1307ad8aa818a2cb58915c662d6c4c95d49b6dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901917
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884691}

--- ash/frame/wide_frame_view.h
+++ ash/frame/wide_frame_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_FRAME_WIDE_FRAME_VIEW_H_
 
 #include "ash/ash_export.h"
+#include "ash/frame/frame_context_menu_controller.h"
 #include "ash/wm/overview/overview_observer.h"
 #include "chromeos/ui/frame/caption_buttons/caption_button_model.h"
 #include "chromeos/ui/frame/immersive/immersive_fullscreen_controller_delegate.h"
@@ -39,7 +40,8 @@ class ASH_EXPORT WideFrameView
     : public views::WidgetDelegateView,
       public aura::WindowObserver,
       public display::DisplayObserver,
-      public chromeos::ImmersiveFullscreenControllerDelegate {
+      public chromeos::ImmersiveFullscreenControllerDelegate,
+      public FrameContextMenuController::Delegate {
  public:
   explicit WideFrameView(views::Widget* target);
   ~WideFrameView() override;
@@ -76,6 +78,10 @@ class ASH_EXPORT WideFrameView
   void SetVisibleFraction(double visible_fraction) override;
   std::vector<gfx::Rect> GetVisibleBoundsInScreen() const override;
 
+  // FrameContextMenuController::Delegate:
+  bool ShouldShowContextMenu(views::View* source,
+                             const gfx::Point& screen_coords_point) override;
+
   HeaderView* GetTargetHeaderView();
 
   // The target widget this frame will control.
@@ -84,6 +90,8 @@ class ASH_EXPORT WideFrameView
   std::unique_ptr<views::Widget> widget_;
 
   HeaderView* header_view_ = nullptr;
+
+  std::unique_ptr<FrameContextMenuController> frame_context_menu_controller_;
 
   // Called when |target_|'s "paint as active" state has changed.
   void PaintAsActiveChanged();
