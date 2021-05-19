31fc6d8f6254b01a4d66b551627fe13b0a43ea9b
Olivier Robin <olivierrobin@google.com>
2021-05-19 17:49:15 +0000

    Fix policy egtest flakiness
    Use kNTPCollectionViewIdentifier for the scroll as
    kContentSuggestionsCollectionIdentifier may be off
    screen.
    
    Change-Id: I3ecda077a2f8977a8f011b2aa1a9d86f4629d50e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905564
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884565}

--- ios/chrome/browser/policy/policy_egtest.mm
+++ ios/chrome/browser/policy/policy_egtest.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/pref_names.h"
 #import "ios/chrome/browser/translate/translate_app_interface.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h"
+#import "ios/chrome/browser/ui/ntp/new_tab_page_constants.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_constants.h"
 #import "ios/chrome/browser/ui/settings/autofill/autofill_constants.h"
 #import "ios/chrome/browser/ui/settings/elements/elements_constants.h"
@@ -406,9 +407,8 @@ void VerifyManagedSettingItem(NSString* accessibilityID,
       selectElementWithMatcher:grey_allOf(grey_accessibilityLabel(feedTitle),
                                           grey_sufficientlyVisible(), nil)]
          usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 200)
-      onElementWithMatcher:grey_accessibilityID(
-                               kContentSuggestionsCollectionIdentifier)]
-      assertWithMatcher:grey_notNil()];
+      onElementWithMatcher:grey_accessibilityID(kNTPCollectionViewIdentifier)]
+      assertWithMatcher:grey_sufficientlyVisible()];
 
   SetPolicy(false, policy::key::kNTPContentSuggestionsEnabled);
 
