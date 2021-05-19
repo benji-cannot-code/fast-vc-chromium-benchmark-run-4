30731ce70eeb824d8a5c3c8e4ae0b4475b58a8cc
Katie Dektar <katie@chromium.org>
2021-05-19 21:58:27 +0000

    Log dictation language with UMA.
    This will help us understand language usage as we roll out
    on-device dictation, and understand how frequently users switch
    their Dictation input languages.
    
    Bug: 1195916
    Change-Id: I91d321df8530cbe1e10a5bce4d3d70ed69b3a8d1
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906135
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884705}

--- chrome/browser/ash/accessibility/dictation.cc
+++ chrome/browser/ash/accessibility/dictation.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/audio/sounds.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/metrics_hashes.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
@@ -97,6 +98,10 @@ bool Dictation::OnToggleDictation() {
   }
   has_committed_text_ = false;
   std::string language = GetUserLanguage(profile_);
+  // Log the language used with CLD3LanguageCode.
+  base::UmaHistogramSparse("Accessibility.CrosDictation.Language",
+                           base::HashMetricName(language));
+
   if (switches::IsExperimentalAccessibilityDictationOfflineEnabled() &&
       OnDeviceSpeechRecognizer::IsOnDeviceSpeechRecognizerAvailable(language)) {
     // On-device recognition is behind a flag and then only available if
