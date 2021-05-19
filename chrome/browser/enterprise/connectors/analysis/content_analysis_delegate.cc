2faf04d83115608b94f1ee8b4b48c4a06e4c5660
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2021-05-19 21:27:31 +0000

    Extract an interface from ContentAnalysisDelegate
    This will allow the creation of a different delegate subtype to handle
    actions in the dialog related to downloads warnings and errors.
    
    Bug: 1210187
    Change-Id: I41f01f6b8a5495d3ed2afb7f66d34f59444d9de8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907092
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884686}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_delegate.cc
@@ -406,9 +406,9 @@ void ContentAnalysisDelegate::StringRequestCallback(
     if (should_warn) {
       text_warning_ = true;
       text_response_ = std::move(response);
-      UpdateFinalResult(FinalResult::WARNING);
+      UpdateFinalResult(ContentAnalysisDelegateBase::FinalResult::WARNING);
     } else {
-      UpdateFinalResult(FinalResult::FAILURE);
+      UpdateFinalResult(ContentAnalysisDelegateBase::FinalResult::FAILURE);
     }
   }
 
@@ -439,14 +439,15 @@ void ContentAnalysisDelegate::CompleteFileRequestCallback(
 
   if (!file_complies) {
     if (result == BinaryUploadService::Result::FILE_TOO_LARGE) {
-      UpdateFinalResult(FinalResult::LARGE_FILES);
+      UpdateFinalResult(ContentAnalysisDelegateBase::FinalResult::LARGE_FILES);
     } else if (result == BinaryUploadService::Result::FILE_ENCRYPTED) {
-      UpdateFinalResult(FinalResult::ENCRYPTED_FILES);
+      UpdateFinalResult(
+          ContentAnalysisDelegateBase::FinalResult::ENCRYPTED_FILES);
     } else if (should_warn) {
       file_warnings_[index] = std::move(response);
-      UpdateFinalResult(FinalResult::WARNING);
+      UpdateFinalResult(ContentAnalysisDelegateBase::FinalResult::WARNING);
     } else {
-      UpdateFinalResult(FinalResult::FAILURE);
+      UpdateFinalResult(ContentAnalysisDelegateBase::FinalResult::FAILURE);
     }
   }
 
@@ -598,7 +599,7 @@ void ContentAnalysisDelegate::MaybeCompleteScanRequest() {
 
   // If showing the warning message, wait before running the callback. The
   // callback will be called either in BypassWarnings or Cancel.
-  if (final_result_ != FinalResult::WARNING)
+  if (final_result_ != ContentAnalysisDelegateBase::FinalResult::WARNING)
     RunCallback();
 
   if (!UpdateDialog() && data_uploaded_) {
@@ -644,7 +645,8 @@ void ContentAnalysisDelegate::OnGotFileInfo(
   UploadFileForDeepScanning(result, data_.paths[index], std::move(request));
 }
 
-void ContentAnalysisDelegate::UpdateFinalResult(FinalResult result) {
+void ContentAnalysisDelegate::UpdateFinalResult(
+    ContentAnalysisDelegateBase::FinalResult result) {
   if (result < final_result_)
     final_result_ = result;
 }
