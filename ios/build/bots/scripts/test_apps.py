532260fc0ecd21e040e0079e8d32a6080619f9a7
Ali Juma <ajuma@chromium.org>
2021-05-19 14:47:01 +0000

    Fix Python 3 compatibility bugs in ios/build/bots/scripts
    This fixes bugs found when running run_cwt_chromedriver.py using
    Python 3, while keeping the scripts still compatible with Python 2.
    
    This also updates run_cwt_chromedriver.py and run_fuzz_test.py to
    run with Python 3 by default, as needed on Clusterfuzz
    bots.
    
    Bug: 1158540
    Change-Id: Ieda08a25e2e3e4058dd192d8afcf2c545020737d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895926
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884472}

--- ios/build/bots/scripts/test_apps.py
+++ ios/build/bots/scripts/test_apps.py
@@ -68,7 +68,7 @@ def get_bundle_id(app_path):
       '-c',
       'Print:CFBundleIdentifier',
       os.path.join(app_path, 'Info.plist'),
-  ]).rstrip()
+  ]).rstrip().decode("utf-8")
 
 
 class GTestsApp(object):
