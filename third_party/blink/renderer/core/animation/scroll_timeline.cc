276d5cc930f4c3c6752f1aa0e8827b4d60d657a8
Anders Hartvoll Ruud <andruud@chromium.org>
2021-05-19 15:21:01 +0000

    Avoid unconditional strong references to scroll-linked animations
    We currently store strong references to animations that attach to
    ScrollTimelines. The are only two ways for an animation to leave the
    strong set: (1) if the timeline of the animation is changed, and
    (2) via Animation::Disposed (GC finalizer). This is problematic, because
    ScrollTimelines are strongly referenced by their associated scroll
    source (see Node::RegisterScrollTimeline) while any animation is
    attached. Hence, as long we don't switch the timeline or GC the
    source Node, scroll-linked animations can *never* be garbage collected
    once they are attached to a ScrollTimeline, no matter how hard they are
    canceled and forgotten about by JS.
    
    AnimationTimeline already has a system for keeping relevant animations
    
    animations that need updates. This should be sufficient for
    ScrollTimeline as well, so we can just remove ScrollTimeline::
    scroll_animations_ and use AnimationTimeline::GetAnimations
    whenever we need to iterate through the animations attached to the
    ScrollTimeline.
    
    alive: a weak set for all attached animations, and a strong set for
    Change-Id: I3e3a11567fbc97b4d5fbfae34d62f3e3c5633b39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2876787
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884485}

--- third_party/blink/renderer/core/animation/scroll_timeline.cc
+++ third_party/blink/renderer/core/animation/scroll_timeline.cc
@@ -595,17 +595,16 @@ void ScrollTimeline::GetCurrentAndMaxOffset(const LayoutBox* layout_box,
 }
 
 void ScrollTimeline::AnimationAttached(Animation* animation) {
-  AnimationTimeline::AnimationAttached(animation);
-  if (resolved_scroll_source_ && scroll_animations_.IsEmpty())
+  if (resolved_scroll_source_ && !HasAnimations())
     resolved_scroll_source_->RegisterScrollTimeline(this);
 
-  scroll_animations_.insert(animation);
+  AnimationTimeline::AnimationAttached(animation);
 }
 
 void ScrollTimeline::AnimationDetached(Animation* animation) {
   AnimationTimeline::AnimationDetached(animation);
-  scroll_animations_.erase(animation);
-  if (resolved_scroll_source_ && scroll_animations_.IsEmpty())
+
+  if (resolved_scroll_source_ && !HasAnimations())
     resolved_scroll_source_->UnregisterScrollTimeline(this);
 }
 
@@ -622,7 +621,6 @@ void ScrollTimeline::WorkletAnimationDetached() {
 }
 
 void ScrollTimeline::Trace(Visitor* visitor) const {
-  visitor->Trace(scroll_animations_);
   visitor->Trace(scroll_source_);
   visitor->Trace(resolved_scroll_source_);
   visitor->Trace(scroll_offsets_);
@@ -662,7 +660,7 @@ void ScrollTimeline::Invalidate(Node* node) {
 }
 
 void ScrollTimeline::InvalidateEffectTargetStyle() {
-  for (Animation* animation : scroll_animations_)
+  for (Animation* animation : GetAnimations())
     animation->InvalidateEffectTargetStyle();
 }
 
