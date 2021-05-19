f33ae4e9b22440c2b892c8b95b867d91eac97362
Angela Yoeurng <yoangela@chromium.org>
2021-05-19 17:46:43 +0000

    [omnibox] Make keyword space triggering pref syncable
    Bug: 1008900
    Change-Id: I14c857ad507582b626430d1bbcb3bf7cba72a424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904900
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884564}

--- components/omnibox/browser/omnibox_prefs.cc
+++ components/omnibox/browser/omnibox_prefs.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -48,7 +49,9 @@ const char kZeroSuggestCachedResults[] = "zerosuggest.cachedresults";
 
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterDictionaryPref(kSuggestionGroupVisibility);
-  registry->RegisterBooleanPref(kKeywordSpaceTriggeringEnabled, true);
+  registry->RegisterBooleanPref(
+      kKeywordSpaceTriggeringEnabled, true,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 }
 
 SuggestionGroupVisibility GetUserPreferenceForSuggestionGroupVisibility(
