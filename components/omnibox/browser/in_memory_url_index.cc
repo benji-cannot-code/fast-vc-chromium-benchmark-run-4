1175ac1f693dae167f7aca4de420f0a1ea39721d
Tommy Li <tommycli@chromium.org>
2021-05-19 18:35:09 +0000

    [omnibox] Add History Rebuild trace events for InMemoryURLIndex
    In case we get slow reports and need to dive into the traces of
    InMemoryURLIndex, this adds some more search points.
    
    Bug: 1141539
    Change-Id: Ifb63fedcfb99fae3b29414f9b9148fcf21ea9d8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2894591
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884589}

--- components/omnibox/browser/in_memory_url_index.cc
+++ components/omnibox/browser/in_memory_url_index.cc
@@ -257,7 +257,7 @@ bool InMemoryURLIndex::OnMemoryDump(
 
 void InMemoryURLIndex::PostRestoreFromCacheFileTask() {
   DCHECK(thread_checker_.CalledOnValidThread());
-  TRACE_EVENT0("browser", "InMemoryURLIndex::PostRestoreFromCacheFileTask");
+  TRACE_EVENT0("omnibox", "InMemoryURLIndex::PostRestoreFromCacheFileTask");
 
   if (base::FeatureList::IsEnabled(
           omnibox::kHistoryQuickProviderAblateInMemoryURLIndexCacheFile)) {
@@ -343,6 +343,9 @@ void InMemoryURLIndex::Shutdown() {
 // Restoring from the History DB -----------------------------------------------
 
 void InMemoryURLIndex::ScheduleRebuildFromHistory() {
+  TRACE_EVENT_NESTABLE_ASYNC_BEGIN0(
+      "omnibox", "InMemoryURLIndex::ScheduleRebuildFromHistory",
+      TRACE_ID_LOCAL(this));
   DCHECK(history_service_);
   history_service_->ScheduleDBTask(
       FROM_HERE,
@@ -355,6 +358,9 @@ void InMemoryURLIndex::ScheduleRebuildFromHistory() {
 void InMemoryURLIndex::DoneRebuidingPrivateDataFromHistoryDB(
     bool succeeded,
     scoped_refptr<URLIndexPrivateData> private_data) {
+  TRACE_EVENT_NESTABLE_ASYNC_END0(
+      "omnibox", "InMemoryURLIndex::ScheduleRebuildFromHistory",
+      TRACE_ID_LOCAL(this));
   DCHECK(thread_checker_.CalledOnValidThread());
   if (succeeded) {
     private_data_tracker_.TryCancelAll();
