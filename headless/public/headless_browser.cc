6524e4d51af4b07b5f90ba5f6a1da38699f8b8fb
sugoi@chromium.org <sugoi@google.com>
2021-05-19 00:56:13 +0000

    Allow using the use-angle switch with headless
    The legacy SwiftShader GL libraries are currently getting replaced with
    SwANGLE (ANGLE + SwiftShader Vulkan). As part of that change, the
    command line switches requires to use software WebGL are changing.
    Currently, we use:
    --use-gl=swiftshader-webgl
    With SwANGLE, we will use:
    --use-gl=angle --use-angle=swiftshader-webgl
    
    This means the use-angle switch must be supported anywhere either:
    --use-gl=swiftshader
    or
    --use-gl=swiftshader-webgl
    is currently used.
    
    Bug: chromium:1209250
    Change-Id: Icce437695b35a96ca0ae9139623d6fbd1064bb47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903707
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884252}

--- headless/public/headless_browser.cc
+++ headless/public/headless_browser.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "build/build_config.h"
 #include "content/public/common/user_agent.h"
 #include "headless/public/version.h"
 #include "ui/gl/gl_switches.h"
@@ -37,6 +38,7 @@ Options::Options(int argc, const char** argv)
     : argc(argc),
       argv(argv),
       gl_implementation(gl::kGLImplementationSwiftShaderForWebGLName),
+      angle_implementation(gl::kANGLEImplementationNoneName),
       product_name_and_version(GetProductNameAndVersion()),
       user_agent(content::BuildUserAgentFromProduct(product_name_and_version)),
       window_size(kDefaultWindowSize),
@@ -117,6 +119,12 @@ Builder& Builder::SetEnableResourceScheduler(bool enable_resource_scheduler) {
 
 Builder& Builder::SetGLImplementation(const std::string& gl_implementation) {
   options_.gl_implementation = gl_implementation;
+  return *this;
+}
+
+Builder& Builder::SetANGLEImplementation(
+    const std::string& angle_implementation) {
+  options_.angle_implementation = angle_implementation;
   return *this;
 }
 
