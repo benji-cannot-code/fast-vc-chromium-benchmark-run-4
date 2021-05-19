9fb142b4a62a17c2ae4b1a037afab848ba2e5357
Yoshifumi Inoue <yosin@chromium.org>
2021-05-19 01:24:56 +0000

    Add a test case for LayoutObjectFactory::CreateWordBreak()
    This patch adds a test case for |LayoutObjectFactory::CreateWordBreak()|
    to avoid regression.
    
    This is the follow-up of the CL[1] where I forgot to add this change.
    
    [1] http://crrev.com/c/2901680: [LayoutNG] Introduce LayoutNGWordBreak
    
    Bug: 1112923
    Change-Id: Ica644e2a86a51cedac6e2a10b8cdff5338e7e29d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905033
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884264}

--- third_party/blink/renderer/core/layout/layout_object_factory_test.cc
+++ third_party/blink/renderer/core/layout/layout_object_factory_test.cc
@@ -36,4 +36,14 @@ TEST_P(LayoutObjectFactoryTest, BR) {
     EXPECT_FALSE(layout_object.IsLayoutNGObject());
 }
 
+TEST_P(LayoutObjectFactoryTest, WordBreak) {
+  SetBodyInnerHTML("<wbr id=sample>");
+  const auto& layout_object = *GetLayoutObjectByElementId("sample");
+
+  if (LayoutNGEnabled())
+    EXPECT_TRUE(layout_object.IsLayoutNGObject());
+  else
+    EXPECT_FALSE(layout_object.IsLayoutNGObject());
+}
+
 }  // namespace blink
