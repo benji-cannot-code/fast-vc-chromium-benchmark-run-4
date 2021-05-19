2a4e29cf50096c3a8a6e6bd15516357579f65231
Tommy Nyquist <nyquist@chromium.org>
2021-05-19 23:37:53 +0000

    [segmentation_platform] Clean up optimization_guide content dependency
    Now that https://crrev.com/c/2904918 has landed, we can clean up the
    content setup for BUILD.gn targets, directory names, and DEPS.
    
    *   Deleted internal/content/DEPS
        *   Still keeping around the -components/optimization_guide/content
            DEPS in the main component DEPS file entry so we do not
            accidentally add a dependency on content later.
    *   Renamed internal/content to internal/execution.
    *   Cleaned up deps vs public_deps across the various GN targets and
        merged the content ones with non-content ones.
        *   We still need to separate the files for execution out, because
            of the requirement of build_with_tflite_lib flag requirement,
            which is not true on all platforms.
    
    Bug: 1210648
    Change-Id: I948d84d327fdfb637c73f6efcf3049229b81cf07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906788
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884755}

--- components/segmentation_platform/internal/execution/segmentation_model_executor_unittest.cc
+++ components/segmentation_platform/internal/execution/segmentation_model_executor_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/segmentation_platform/internal/content/segmentation_model_executor.h"
+#include "components/segmentation_platform/internal/execution/segmentation_model_executor.h"
 
 #include <memory>
 
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
 #include "components/optimization_guide/core/test_optimization_guide_model_provider.h"
 #include "components/optimization_guide/proto/models.pb.h"
-#include "components/segmentation_platform/internal/content/segmentation_model_handler.h"
+#include "components/segmentation_platform/internal/execution/segmentation_model_handler.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
--- components/segmentation_platform/internal/execution/segmentation_model_handler.cc
+++ components/segmentation_platform/internal/execution/segmentation_model_handler.cc
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/segmentation_platform/internal/content/segmentation_model_handler.h"
+#include "components/segmentation_platform/internal/execution/segmentation_model_handler.h"
 
 #include <memory>
 #include <vector>
 
 #include "components/optimization_guide/core/model_executor.h"
 #include "components/optimization_guide/proto/models.pb.h"
-#include "components/segmentation_platform/internal/content/segmentation_model_executor.h"
+#include "components/segmentation_platform/internal/execution/segmentation_model_executor.h"
 
 namespace segmentation_platform {
 
