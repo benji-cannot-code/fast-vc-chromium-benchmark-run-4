e33ac637ff51592546ca6fac67569724a3dfa36d
Mikel Astiz <mastiz@chromium.org>
2021-05-20 15:39:35 +0000

    [sync] Add obfuscatedGaiaId along with encryption keys on ChromeOS
    The field is somewhat redundant because the signin flow gets the user
    identifier (gaia ID) by other means. However, to prevent bugs that can
    lead to mixing up encryption keys across users, this patch bundles the
    gaia ID together with encryption keys and verifies the integrity of
    the information when the signin procedure completes, prior to the
    actual processing of the keys.
    
    Change-Id: I75f834c2f5e6cfccca752ea2d517bb8256ef9aa6
    Fixed: 1081651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897569
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885039}

--- chromeos/login/auth/sync_trusted_vault_keys.cc
+++ chromeos/login/auth/sync_trusted_vault_keys.cc
@@ -16,6 +16,7 @@ namespace {
 
 // Keys in base::Value dictionaries.
 const char kEncryptionKeysDictKey[] = "encryptionKeys";
+const char kGaiaIdDictKey[] = "obfuscatedGaiaId";
 const char kKeyMaterialDictKey[] = "keyMaterial";
 const char kMethodTypeHintDictKey[] = "type";
 const char kPublicKeyDictKey[] = "publicKey";
@@ -104,11 +105,16 @@ SyncTrustedVaultKeys::~SyncTrustedVaultKeys() = default;
 // static
 SyncTrustedVaultKeys SyncTrustedVaultKeys::FromJs(
     const base::DictionaryValue& js_object) {
+  SyncTrustedVaultKeys result;
+  const std::string* gaia_id = js_object.FindStringKey(kGaiaIdDictKey);
+  if (gaia_id) {
+    result.gaia_id_ = *gaia_id;
+  }
+
   const std::vector<KeyMaterialAndVersion> encryption_keys =
       ParseList(js_object.FindListKey(kEncryptionKeysDictKey),
                 base::BindRepeating(&ParseSingleEncryptionKey));
 
-  SyncTrustedVaultKeys result;
   for (const KeyMaterialAndVersion& key : encryption_keys) {
     if (key.version != 0) {
       result.encryption_keys_.push_back(key.key_material);
@@ -121,6 +127,10 @@ SyncTrustedVaultKeys SyncTrustedVaultKeys::FromJs(
                 base::BindRepeating(&ParseSingleTrustedRecoveryMethod));
 
   return result;
+}
+
+const std::string& SyncTrustedVaultKeys::gaia_id() const {
+  return gaia_id_;
 }
 
 const std::vector<std::vector<uint8_t>>& SyncTrustedVaultKeys::encryption_keys()
