f360c82c06dd7d7edefa7aab2bee6a6959bd7f29
Lei Zhang <thestig@chromium.org>
2021-05-20 23:30:52 +0000

    Move base::size(), empty(), and data() to base/cxx17_backports.h.
    These backports of C++17 features should live in their own header to
    eventually reduce the size of base/stl_util.h and to make migrating them
    easier when C++17 is generally available in Chromium. For now, include
    base/cxx17_backports.h in base/stl_util.h to avoid breaking the build.
    
    Along the way, do IWYU for base/stl_util.h, and convert a couple base
    headers to include base/cxx17_backports.h instead of base/stl_util.h.
    
    Bug: 1210983
    Change-Id: I63d3920771e10d8f379b4940633c3a30987f81e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906665
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885290}

--- base/containers/checked_range.h
+++ base/containers/checked_range.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/containers/checked_iterators.h"
-#include "base/stl_util.h"
+#include "base/cxx17_backports.h"
 #include "base/template_util.h"
 
 namespace base {
