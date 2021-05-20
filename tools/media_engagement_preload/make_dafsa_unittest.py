77ec1d2a11b371d858f2feb082f6d7c7091fca53
Takuto Ikuta <tikuta@chromium.org>
2021-05-20 00:49:25 +0000

    chrome: run tools/media_engagement_preload/make_dafsa.py in python3
    Fixed: 1210295
    Change-Id: I531f99ba4209086c25157854321e3addf1ce632e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903407
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884796}

--- tools/media_engagement_preload/make_dafsa_unittest.py
+++ tools/media_engagement_preload/make_dafsa_unittest.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import sys
 import unittest
+
 import make_dafsa
 
 
@@ -679,14 +680,14 @@ class ExamplesTest(unittest.TestCase):
   def testExample1(self):
     """Tests Example 1 from make_dafsa.py."""
     infile = '["https://www.example.com:8081", "http://www.example.org"]'
-    outfile = "\n\x1c\x81www.example\xae\x02\x89com:8081\x80org\x81"
+    outfile = b'\n\x1c\x81www.example\xae\x02\x84org\x81com:8081\x80'
     self.assertEqual(make_dafsa.words_to_proto(make_dafsa.parse_json(infile)),
                       outfile)
 
   def testExample2(self):
     """Tests Example 2 from make_dafsa.py."""
     infile = '["https://www.example.org", "http://www.google.com"]'
-    outfile = "\n\x1e\x81www\xae\x02\x8bgoogle.com\x81example.org\x80"
+    outfile = b'\n\x1e\x81www\xae\x02\x8bgoogle.com\x81example.org\x80'
     self.assertEqual(make_dafsa.words_to_proto(make_dafsa.parse_json(infile)),
                       outfile)
 
