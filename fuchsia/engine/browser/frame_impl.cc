807af46a121372e393ee895f291a02a7cffc157a
Sergey Ulanov <sergeyu@chromium.org>
2021-05-20 01:21:37 +0000

    [Fuchsia] Implement Frame.SetPageScale()
    The new function allows to zoom in/out a web.Frame.
    
    Bug: 1192193
    Change-Id: I3ab9afa2c58e4c80c3f19deb94f71f85f0fefc04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895085
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884805}

--- fuchsia/engine/browser/frame_impl.cc
+++ fuchsia/engine/browser/frame_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/media_session.h"
 #include "content/public/browser/message_port_provider.h"
 #include "content/public/browser/navigation_entry.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/permission_controller_delegate.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/renderer_preferences_util.h"
@@ -54,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "third_party/blink/public/common/logging/logging_utils.h"
 #include "third_party/blink/public/common/messaging/web_message_port.h"
+#include "third_party/blink/public/common/page/page_zoom.h"
 #include "third_party/blink/public/mojom/loader/resource_load_info.mojom.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/compositor.h"
@@ -582,6 +585,15 @@ void FrameImpl::MaybeStartCastStreaming(
       std::move(cast_streaming_receiver));
 }
 
+void FrameImpl::UpdateRenderViewZoomLevel(
+    content::RenderViewHost* render_view_host) {
+  content::HostZoomMap* host_zoom_map =
+      content::HostZoomMap::GetForWebContents(web_contents_.get());
+  host_zoom_map->SetTemporaryZoomLevel(
+      render_view_host->GetProcess()->GetID(), render_view_host->GetRoutingID(),
+      blink::PageZoomFactorToZoomLevel(page_scale_));
+}
+
 void FrameImpl::CreateView(fuchsia::ui::views::ViewToken view_token) {
   scenic::ViewRefPair view_ref_pair = scenic::ViewRefPair::New();
   CreateViewWithViewRef(std::move(view_token),
@@ -949,6 +961,19 @@ void FrameImpl::SetPreferredTheme(fuchsia::settings::ThemeType theme) {
                                      base::Unretained(this)));
 }
 
+void FrameImpl::SetPageScale(float scale) {
+  if (scale <= 0.0) {
+    LOG(ERROR) << "SetPageScale() called with nonpositive scale.";
+    CloseAndDestroyFrame(ZX_ERR_INVALID_ARGS);
+    return;
+  }
+
+  if (scale == page_scale_)
+    return;
+  page_scale_ = scale;
+  UpdateRenderViewZoomLevel(web_contents_->GetRenderViewHost());
+}
+
 void FrameImpl::ForceContentDimensions(
     std::unique_ptr<fuchsia::ui::gfx::vec2> web_dips) {
   if (!web_dips) {
@@ -960,7 +985,7 @@ void FrameImpl::ForceContentDimensions(
 
   gfx::Size web_dips_converted(web_dips->x, web_dips->y);
   if (web_dips_converted.IsEmpty()) {
-    LOG(WARNING) << "Rejecting zero-area size for ForceContentDimensions().";
+    LOG(ERROR) << "Rejecting zero-area size for ForceContentDimensions().";
     CloseAndDestroyFrame(ZX_ERR_INVALID_ARGS);
     return;
   }
@@ -1147,8 +1172,17 @@ void FrameImpl::DidFinishLoad(content::RenderFrameHost* render_frame_host,
 void FrameImpl::RenderFrameCreated(content::RenderFrameHost* frame_host) {
   // The top-level frame is given a transparent background color.
   // GetView() is guaranteed to be non-null until |frame_host| teardown.
-  if (frame_host == web_contents()->GetMainFrame())
+  if (frame_host == web_contents()->GetMainFrame()) {
     frame_host->GetView()->SetBackgroundColor(SK_AlphaTRANSPARENT);
+  }
+}
+
+void FrameImpl::RenderViewHostChanged(content::RenderViewHost* old_host,
+                                      content::RenderViewHost* new_host) {
+  // UpdateRenderViewZoomLevel() sets temporary zoom level for the current
+  // RenderView. It needs to be called again whenever main RenderView is
+  // changed.
+  UpdateRenderViewZoomLevel(new_host);
 }
 
 void FrameImpl::DidFirstVisuallyNonEmptyPaint() {
