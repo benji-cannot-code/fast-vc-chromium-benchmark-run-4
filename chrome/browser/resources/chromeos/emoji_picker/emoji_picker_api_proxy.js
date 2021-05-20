811c88ec4f772578c64192cd007e118d076b2534
John Palmer <jopalmer@chromium.org>
2021-05-20 02:41:41 +0000

    [Emoji picker] Add metric for search string length
    Bug: b/180567450
    Change-Id: I6455f809b84dc1816cd8462d5d85963039757422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2902067
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884833}

--- chrome/browser/resources/chromeos/emoji_picker/emoji_picker_api_proxy.js
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_picker_api_proxy.js
@@ -14,8 +14,9 @@ export class EmojiPickerApiProxy {
    *
    * @param {string} emoji
    * @param {boolean} isVariant
+   * @param {number} searchLength
    */
-  insertEmoji(emoji, isVariant) {}
+  insertEmoji(emoji, isVariant, searchLength) {}
 
   /**
    * @returns {Promise<{incognito:boolean}>}
@@ -37,8 +38,8 @@ export class EmojiPickerApiProxyImpl {
     this.handler.showUI();
   }
   /** @override */
-  insertEmoji(emoji, isVariant) {
-    this.handler.insertEmoji(emoji, isVariant);
+  insertEmoji(emoji, isVariant, searchLength) {
+    this.handler.insertEmoji(emoji, isVariant, searchLength);
   }
 
   /** @override */
