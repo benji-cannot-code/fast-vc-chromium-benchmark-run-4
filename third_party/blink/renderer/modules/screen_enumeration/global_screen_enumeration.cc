e2b8b196d695216f35386337e383740e83587904
Adrienne Walker <enne@chromium.org>
2021-05-20 02:10:36 +0000

    Window Placement: add permission policy check for isExtended
    Also add a check for the deprecated isMultiScreen.
    
    Bug: 1204141
    Change-Id: I28c18a84accca270d119a67c0beae3bd5b12eaaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906659
    Commit-Queue: enne <enne@chromium.org>
    Auto-Submit: enne <enne@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884826}

--- third_party/blink/renderer/modules/screen_enumeration/global_screen_enumeration.cc
+++ third_party/blink/renderer/modules/screen_enumeration/global_screen_enumeration.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
+#include "third_party/blink/public/mojom/permissions_policy/permissions_policy.mojom-blink.h"
 #include "third_party/blink/public/mojom/screen_enumeration/screen_enumeration.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
@@ -109,11 +110,19 @@ ScriptPromise GlobalScreenEnumeration::isMultiScreen(
     return ScriptPromise();
   }
 
+  auto* context = ExecutionContext::From(script_state);
+  if (!context->IsFeatureEnabled(
+          mojom::blink::PermissionsPolicyFeature::kWindowPlacement)) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kInvalidStateError,
+        "Screen information is unavailable or access is not allowed.");
+    return ScriptPromise();
+  }
+
   // TODO(msw): Cache the backend connection.
   mojo::Remote<mojom::blink::ScreenEnumeration> backend;
-  ExecutionContext::From(script_state)
-      ->GetBrowserInterfaceBroker()
-      .GetInterface(backend.BindNewPipeAndPassReceiver());
+  context->GetBrowserInterfaceBroker().GetInterface(
+      backend.BindNewPipeAndPassReceiver());
 
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   auto* raw_backend = backend.get();
