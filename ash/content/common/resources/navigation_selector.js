1c673e8ee4a468e2000d565b7e8b160983f85886
Jimmy Gong <jimmyxgong@google.com>
2021-05-20 00:37:31 +0000

    navigation: Add support for displaying navigation icons
    New icons will need to be added to navigation_icons.html.
    Add basic styling to support selected selectors.
    
    Screenshot: https://screenshot.googleplex.com/6Mi7pvE7ZCYWsE5
    
    Bug: 1205539
    Change-Id: Id4befd86eb498744c287df5ebd236f5b811d8456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904829
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884789}

--- ash/content/common/resources/navigation_selector.js
+++ ash/content/common/resources/navigation_selector.js
@@ -5,15 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import './navigation_icons.js';
 import 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.m.js';
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import 'chrome://resources/polymer/v3_0/iron-collapse/iron-collapse.js';
+import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 
 /**
  * @typedef {{
  *   name: string,
  *   pageIs: string,
+ *   icon: string,
  * }}
  */
 export let SelectorItem;
@@ -140,6 +143,15 @@ export class NavigationSelectorElement extends PolymerElement {
    */
   isCollapsible_(item) {
     return item.properties.isCollapsible;
+  }
+
+  /**
+   * @param {!SelectorItem} item
+   * @return {string}
+   * @protected
+   */
+  getIcon_(item) {
+    return item.icon;
   }
 }
 
--- chrome/test/data/webui/chromeos/ash_common/navigation_selector_test.js
+++ chrome/test/data/webui/chromeos/ash_common/navigation_selector_test.js
@@ -27,10 +27,12 @@ export function navigationSelectorTestSuite() {
   /**
    * @param {string} name
    * @param {string} pageIs
+   * @param {string} icon
    * @return {!SelectorItem}
    */
-  function createSelectorItem(name, pageIs) {
-    let item = /** @type{SelectorItem} */ ({'name': name, 'pageIs': pageIs});
+  function createSelectorItem(name, pageIs, icon) {
+    let item = /** @type{SelectorItem} */ (
+        {'name': name, 'pageIs': pageIs, 'icon': icon});
     return item;
   }
 
@@ -63,8 +65,8 @@ export function navigationSelectorTestSuite() {
   }
 
   test('navigationSelectorLoadEntries', async () => {
-    const item1 = createSelectorItem('test1', 'test-page1');
-    const item2 = createSelectorItem('test2', 'test-page2');
+    const item1 = createSelectorItem('test1', 'test-page1', '');
+    const item2 = createSelectorItem('test2', 'test-page2', '');
 
     const property1 = createProperty(false, false, []);
     const property2 = createProperty(false, false, []);
@@ -86,8 +88,12 @@ export function navigationSelectorTestSuite() {
   });
 
   test('navigationSelectorLoadsCollapsibleEntries', async () => {
-    const item1 = createSelectorItem('test1', 'test-page1');
-    const item2 = createSelectorItem('Advanced', '');
+    const item1 = createSelectorItem(
+        'test1',
+        'test-page1',
+        '',
+    );
+    const item2 = createSelectorItem('Advanced', '', '');
 
     const property = createProperty(true, false, [item1]);
 
