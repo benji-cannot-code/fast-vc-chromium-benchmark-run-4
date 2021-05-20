c980e04645e158ff4d05c8a0404d1463da37205e
Dominic Mazzoni <dmazzoni@chromium.org>
2021-05-20 17:39:18 +0000

    Image descriptions should check security origin, not document url.
    The image descriptions feature is limited by protocol: only allowed
    on http, https, file, and data urls. However, checking the document
    url isn't the best way to determine this, because an iframe created
    by a parent document without a "src" will appear to have a url of
    "about:blank", but it's actually controlled by its parent document.
    
    The solution is to check the security origin, which returns the
    correct protocol of the owning document if different.
    
    This enables the image description feature to work in Google Docs,
    among other places.
    
    Bug: 1211002
    AX-Relnotes: Fixes issue where automatic image descriptions weren't being generated in some iframes.
    Change-Id: Ib41b0e16b3970948dd10654774c3576b70cc6588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906774
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885086}

--- content/renderer/accessibility/render_accessibility_impl_browsertest.cc
+++ content/renderer/accessibility/render_accessibility_impl_browsertest.cc
@@ -1253,6 +1253,7 @@ class AXImageAnnotatorTest : public RenderAccessibilityImplTest {
     GetRenderAccessibilityImpl()->tree_source_->RemoveImageAnnotator();
     GetRenderAccessibilityImpl()->tree_source_->AddImageAnnotator(
         GetRenderAccessibilityImpl()->ax_image_annotator_.get());
+    BlinkAXTreeSource::IgnoreProtocolChecksForTesting();
   }
 
   void TearDown() override {
