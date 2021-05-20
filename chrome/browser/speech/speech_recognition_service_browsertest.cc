84cbf93ef4d22ebc457a43dba3623611bcdb783e
Katie Dektar <katie@chromium.org>
2021-05-20 00:55:55 +0000

    Fix SpeechRecognitionServiceTest build.
    SpeechRecognitionServiceTest was failing to build without
    this additional import.
    
    AX-Relnotes: N/A
    Bug: None
    Change-Id: I0a6db45477b68f7c8c9c1f619b45916f4bdedfd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907812
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#884798}

--- chrome/browser/speech/speech_recognition_service_browsertest.cc
+++ chrome/browser/speech/speech_recognition_service_browsertest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/audio_service.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
+#include "media/audio/audio_device_description.h"
 #include "media/audio/wav_audio_handler.h"
 #include "media/base/media_switches.h"
 #include "media/mojo/mojom/audio_input_stream.mojom.h"
