5f80c9d48b6f3f4cac6c46c41decf41bee7396c9
Chris Davis (EDGE) <chrdavis@microsoft.com>
2021-05-20 21:08:06 +0000

    Fix global static initializer in accessibility_tree_formatter_uia_win.cc
    A recent change added a global static initializer to
    accessibility_tree_formatter_uia_win.cc.  Changing this to a local
    static so it does not impact startup.
    
    Bug: 1211457
    Change-Id: I9f72345a094af624673eab2bb18d0916073c76f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911018
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Chris Davis <chrdavis@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#885216}

--- content/browser/accessibility/accessibility_tree_formatter_uia_win.cc
+++ content/browser/accessibility/accessibility_tree_formatter_uia_win.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file_path.h"
+#include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
@@ -283,12 +284,6 @@ const long AccessibilityTreeFormatterUia::properties_[] = {
     UIA_Selection2ItemCountPropertyId,                  // 30172
     UIA_HeadingLevelPropertyId,                         // 30173
 };
-
-// Without this pragma, GCC returns a "declaration requires an exit-time
-// destructor" warning since this is a global map. This warning is not a problem
-// in this case and needs to be muted to complete the build.
-#pragma GCC diagnostic ignored "-Wexit-time-destructors"
-std::map<long, std::string> custom_properties_map_;
 
 const long AccessibilityTreeFormatterUia::patterns_[] = {
     UIA_SelectionPatternId,       // 10001
@@ -895,11 +890,17 @@ void AccessibilityTreeFormatterUia::AddWindowProperties(
   }
 }
 
+std::map<long, std::string>&
+AccessibilityTreeFormatterUia::GetCustomPropertiesMap() const {
+  static base::NoDestructor<std::map<long, std::string>> custom_properties_map;
+  return *custom_properties_map;
+}
+
 void AccessibilityTreeFormatterUia::AddCustomProperties(
     IUIAutomationElement* node,
     base::DictionaryValue* dict) const {
   // Custom properties need to be added separately.
-  for (const auto& property : custom_properties_map_) {
+  for (const auto& property : GetCustomPropertiesMap()) {
     base::win::ScopedVariant variant;
     if (SUCCEEDED(
             node->GetCurrentPropertyValue(property.first, variant.Receive()))) {
@@ -912,8 +913,8 @@ std::string AccessibilityTreeFormatterUia::GetPropertyName(
     long property_id) const {
   // We cannot infer the property name from a custom property id, so we get it
   // from the map we created manually in `BuildCustomPropertiesMap()`.
-  auto property = custom_properties_map_.find(property_id);
-  if (property != custom_properties_map_.end())
+  auto property = GetCustomPropertiesMap().find(property_id);
+  if (property != GetCustomPropertiesMap().end())
     return property->second;
 
   return UiaIdentifierToCondensedString(property_id);
@@ -1128,7 +1129,7 @@ void AccessibilityTreeFormatterUia::BuildCacheRequests() {
 }
 
 void AccessibilityTreeFormatterUia::BuildCustomPropertiesMap() {
-  custom_properties_map_.insert(
+  GetCustomPropertiesMap().insert(
       {ui::UiaRegistrarWin::GetInstance().GetVirtualContentPropertyId(),
        "VirtualContent"});
 }
@@ -1149,7 +1150,7 @@ std::string AccessibilityTreeFormatterUia::ProcessTreeForOutput(
     ProcessPropertyForOutput(GetPropertyName(i), dict, line);
 
   // Custom properties.
-  for (const auto& i : custom_properties_map_)
+  for (const auto& i : GetCustomPropertiesMap())
     ProcessPropertyForOutput(GetPropertyName(i.first), dict, line);
 
   // Patterns.
