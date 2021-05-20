86775bf157e45ab53656e11f4c99df88d211b49a
Rune Lillesveen <futhark@chromium.org>
2021-05-20 12:20:04 +0000

    Require style containment for container query containers
    Bug: 1199091
    Change-Id: I4ff4a9267c1c80bd83977dcdacc4ef54f5deea71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897152
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884981}

--- third_party/blink/renderer/core/css/container_query_evaluator_test.cc
+++ third_party/blink/renderer/core/css/container_query_evaluator_test.cc
@@ -114,7 +114,7 @@ TEST_F(ContainerQueryEvaluatorTest, SizeInvalidation) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #container {
-        contain: size layout;
+        contain: size layout style;
         width: 500px;
         height: 500px;
       }
@@ -210,7 +210,7 @@ TEST_F(ContainerQueryEvaluatorTest, EvaluatorOnDetachLayoutTree) {
     <style>
       main {
         display: block;
-        contain: size layout;
+        contain: size layout style;
         width: 500px;
         height: 500px;
       }
