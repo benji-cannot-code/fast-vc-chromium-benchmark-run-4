9acc5b1e3277478a31e658d023340ea0dd8feb04
Yusuke Sato <yusukes@chromium.org>
2021-05-20 17:58:58 +0000

    arcvm: Add ArcSessionManager::TrimVmMemory()
    This allows chrome/browser/performance_manager/mechanisms/ to actually
    reclaim ARCVM's memory.
    
    BUG=b:186278424
    TEST=unit_tests
    
    Change-Id: I2605d06b15a1c0f65c309292d377e9f5c1ee31d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908336
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885094}

--- chrome/browser/ash/arc/session/arc_session_manager_unittest.cc
+++ chrome/browser/ash/arc/session/arc_session_manager_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/bind.h"
 #include "base/test/scoped_command_line.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/values.h"
@@ -2140,6 +2141,17 @@ TEST_F(ArcSessionManagerTest, ReadSaltOnDisk) {
                               std::string(kSaltLen + 1, 'x')));  // too long
   EXPECT_TRUE(ReadSaltOnDisk(arc_salt_path, &salt));
   EXPECT_TRUE(salt.empty());
+}
+
+// Tests that TrimVmMemory doesn't crash.
+TEST_F(ArcSessionManagerTest, TrimVmMemory) {
+  bool callback_called = false;
+  arc_session_manager()->TrimVmMemory(
+      base::BindLambdaForTesting([&callback_called](bool, const std::string&) {
+        callback_called = true;
+      }));
+  base::RunLoop().RunUntilIdle();
+  EXPECT_TRUE(callback_called);
 }
 
 class ArcSessionManagerPowerwashTest : public ArcSessionManagerTestBase {
