a5f92402ac2bfa4196807424332465ee4e5ba127
Mike Wasserman <msw@chromium.org>
2021-05-20 18:34:52 +0000

    Window Placement: Fix crash dereferencing display iterator
    Use proactive fallback displays and ids for DisplayLists in RWHV code.
    This prevents dereferencing invalid display iterators in code added by:
      https://chromium-review.googlesource.com/c/chromium/src/+/2893191
    RWHV and friends depend on the presence of a cached current Display.
    
    RWHV previously used implicit fallback via Screen::GetDisplayNearest*.
    Explicit fallback is needed for a multi-display snapshot, because:
    (1) GetAllDisplays() may return an empty vector.
        (e.g. no displays are connected, or the environment is changing)
    (2) GetPrimaryDisplay() & GetDisplayNearest*() may return placeholders.
        (e.g. when no displays are connected, or no such Display is found)
        (placeholders may not match any displays from GetAllDisplays)
    
    Add new Screen helpers to get DisplayLists with fallbacks.
    Add new DisplayList accessors and a stronger validity check.
    Update DisplayList usage in RWHV and friends.
    
    Bug: 1209793,1211118
    Change-Id: I41906e44f778a35384a6e33f20da0b2b0213316b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900926
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885126}

--- ui/display/screen_unittest.cc
+++ ui/display/screen_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/display/scoped_display_for_new_windows.h"
 #include "ui/display/test/test_screen.h"
+#include "ui/gfx/native_widget_types.h"
 
 namespace display {
 
@@ -86,6 +87,17 @@ TEST_F(ScreenTest, ScopedDisplayForNewWindows) {
   }
 
   EXPECT_EQ(DEFAULT_DISPLAY_ID, screen->GetDisplayForNewWindows().id());
+}
+
+TEST_F(ScreenTest, GetDisplayListNearestWindowWithFallbacks) {
+  Screen* screen = Screen::GetScreen();
+  DisplayList display_list =
+      screen->GetDisplayListNearestWindowWithFallbacks(gfx::kNullNativeWindow);
+  ASSERT_FALSE(display_list.displays().empty());
+  EXPECT_EQ(screen->GetPrimaryDisplay().id(),
+            display_list.GetPrimaryDisplay().id());
+  EXPECT_EQ(screen->GetPrimaryDisplay().id(),
+            display_list.GetCurrentDisplay().id());
 }
 
 }  // namespace display
