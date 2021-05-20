bbd6f96276c06f5f71ae9a184b1420b44526094f
Ioana Pandele <ioanap@chromium.org>
2021-05-20 20:06:24 +0000

    [BioAuthPwdFill] Add a 60s interval for which the auth is valid
    For now, this is tab-scoped due to the ownership model for the authenticator.
    
    This will change in the future.
    
    Bug: 1211415
    Change-Id: I68c4aa7aa987e78bbe0d4f5e4febc5b3457a27e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910201
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885178}

--- chrome/browser/password_manager/android/biometric_authenticator_android.cc
+++ chrome/browser/password_manager/android/biometric_authenticator_android.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/time/time.h"
 #include "chrome/browser/password_manager/android/jni_headers/BiometricAuthenticatorBridge_jni.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/password_manager/core/browser/biometric_authenticator.h"
@@ -35,6 +36,8 @@ using password_manager::BiometricsAvailability;
 using password_manager::UiCredential;
 
 namespace {
+
+constexpr unsigned int kAuthValidSeconds = 60;
 
 bool IsSuccessfulResult(BiometricAuthUIResult result) {
   return result == BiometricAuthUIResult::kSuccessWithUnknownMethod ||
@@ -115,7 +118,14 @@ void BiometricAuthenticatorAndroid::Authenticate(
 
   base::UmaHistogramEnumeration(
       "PasswordManager.BiometricAuthPwdFill.AuthRequester", requester);
-
+  if (last_good_auth_timestamp_.has_value() &&
+      base::TimeTicks::Now() - last_good_auth_timestamp_.value() <
+          base::TimeDelta::FromSeconds(kAuthValidSeconds)) {
+    LogAuthResult(password_manager::BiometricAuthFinalResult::kAuthStillValid);
+    std::move(callback_).Run(/*success=*/true);
+    requester_ = absl::nullopt;
+    return;
+  }
   Java_BiometricAuthenticatorBridge_authenticate(AttachCurrentThread(),
                                                  java_object_);
 }
@@ -145,6 +155,11 @@ void BiometricAuthenticatorAndroid::OnAuthenticationCompleted(JNIEnv* env,
     }
     return;
   }
+
+  if (success) {
+    last_good_auth_timestamp_ = base::TimeTicks::Now();
+  }
+
   LogAuthResult(MapUIResultToFinal(ui_result));
   std::move(callback_).Run(success);
   requester_ = absl::nullopt;
