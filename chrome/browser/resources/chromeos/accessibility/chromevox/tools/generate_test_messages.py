2fc1d3d826a5883b800164a948b337ed005acda3
Nico Weber <thakis@chromium.org>
2021-05-20 05:36:02 +0000

    build: Convert chrome/browser/resources/chromeos/accessibility/chromevox/BUILD.gn to py3
    Bug: 1205610
    Change-Id: I9eca56ff6c01c170fa2cc8334c4a4f6bbe3618a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908135
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884886}

--- chrome/browser/resources/chromeos/accessibility/chromevox/tools/generate_test_messages.py
+++ chrome/browser/resources/chromeos/accessibility/chromevox/tools/generate_test_messages.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 '''Generates test_messages.js from an extension message json file.'''
 
 import gzip
+import io
 import optparse
 import sys
 
@@ -43,10 +44,10 @@ def main():
   def _OpenFile(filename):
     if filename.endswith('.gz'):
       return gzip.open(filename)
-    return open(filename)
+    return open(filename, 'rb')
   with _OpenFile(in_file_name) as in_file:
-    json = in_file.read().strip()
-  with open(options.output_file, 'w') as out_file:
+    json = in_file.read().decode('utf-8').strip()
+  with io.open(options.output_file, 'w', encoding='utf-8') as out_file:
     out_file.write(_JS_TEMPLATE % {'in_file': in_file_name, 'json': json})
 
 
