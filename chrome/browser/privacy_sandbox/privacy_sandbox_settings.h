8b3cebd33d0a8dfa7a07f70dfa15b9dbaa080367
sauski <sauski@google.com>
2021-05-20 08:46:56 +0000

    Privacy Sandbox Settings: Reset FLoC on preference change
    The desired user interaction with the Privacy Sandbox page is to reset
    the user's FLoC identifier & compute timer whenever the user enables
    FLoC.
    
    This CL achieves this by resetting the FLoC ID & compute timer whenever
    one of the Privacy Sandbox APIs or FLoC preference changes. This
    achieves the desired behavior at the cost of additionally resetting
    the FLoC ID & compute timer when not needed. Resetting is a lightweight
    operation, so overperforming it is not a concern.
    
    Resetting on any change achieves the same user & web facing behavior
    as only resetting on the disabled -> enabled transition, and is much
    simpler to implement.
    
    Bug: 1205781
    Change-Id: If1587cdaef5b2cc1e3183beda59bf185d1d05efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907633
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#884926}

--- chrome/browser/privacy_sandbox/privacy_sandbox_settings.h
+++ chrome/browser/privacy_sandbox/privacy_sandbox_settings.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/policy/core/common/policy_service.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_service_observer.h"
@@ -173,6 +174,9 @@ class PrivacySandboxSettings : public KeyedService,
   // Clear-Site-Data header, as it's restricted to a specific site.
   void OnCookiesCleared();
 
+  // Called when a preference relevant to the the Privacy Sandbox is changed.
+  void OnPrivacySandboxPrefChanged();
+
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
 
@@ -296,6 +300,8 @@ class PrivacySandboxSettings : public KeyedService,
   base::ScopedObservation<signin::IdentityManager,
                           signin::IdentityManager::Observer>
       identity_manager_observer_{this};
+
+  PrefChangeRegistrar user_prefs_registrar_;
 
   // A manual record of whether policy_service_ is being observerd.
   // Unfortunately PolicyService does not support scoped observers.
