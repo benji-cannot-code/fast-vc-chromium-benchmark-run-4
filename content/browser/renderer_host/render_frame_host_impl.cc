633a1fbea0c2e1d992d43652d8c8b9a8a772f40f
Kevin McNee <mcnee@chromium.org>
2021-05-20 23:32:35 +0000

    Implement RenderFrameHostImpl::ForEachImmediateLocalRoot in terms of ForEachRenderFrameHost
    Bug: 754726, 1208438
    Change-Id: I40ce7c8faba23c085c7aaea1bd4877a071cf1e0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907073
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885291}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/containers/contains.h"
-#include "base/containers/queue.h"
 #include "base/debug/alias.h"
 #include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
@@ -8986,22 +8985,16 @@ service_manager::InterfaceProvider* RenderFrameHostImpl::GetJavaInterfaces() {
 
 void RenderFrameHostImpl::ForEachImmediateLocalRoot(
     const base::RepeatingCallback<void(RenderFrameHostImpl*)>& callback) {
-  if (!frame_tree_node_->child_count())
-    return;
-
-  base::queue<FrameTreeNode*> queue;
-  for (size_t index = 0; index < frame_tree_node_->child_count(); ++index)
-    queue.push(frame_tree_node_->child_at(index));
-  while (queue.size()) {
-    FrameTreeNode* current = queue.front();
-    queue.pop();
-    if (current->current_frame_host()->is_local_root()) {
-      callback.Run(current->current_frame_host());
-    } else {
-      for (size_t index = 0; index < current->child_count(); ++index)
-        queue.push(current->child_at(index));
-    }
-  }
+  ForEachRenderFrameHost(base::BindRepeating(
+      [](const base::RepeatingCallback<void(RenderFrameHostImpl*)>& callback,
+         const RenderFrameHostImpl* starting_rfh, RenderFrameHostImpl* rfh) {
+        if (rfh->is_local_root() && rfh != starting_rfh) {
+          callback.Run(rfh);
+          return FrameIterationAction::kSkipChildren;
+        }
+        return FrameIterationAction::kContinue;
+      },
+      callback, this));
 }
 
 void RenderFrameHostImpl::SetVisibilityForChildViews(bool visible) {
