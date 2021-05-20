163355320f091ce7f4b7fa60683f37fc91c007be
Sergey Ulanov <sergeyu@chromium.org>
2021-05-20 01:27:14 +0000

    Disable browser_tests failing on MaxOS 11
    Bug: 1211141, 1211144, 1211138
    Change-Id: If031d4cb51ee2015af7a3fc922f0520f248a843d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908114
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Owners-Override: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884809}

--- chrome/browser/extensions/api/socket/socket_apitest.cc
+++ chrome/browser/extensions/api/socket/socket_apitest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/ui/browser.h"
@@ -109,7 +110,13 @@ IN_PROC_BROWSER_TEST_F(SocketApiTest, SocketTCPServerUnbindOnUnload) {
   EXPECT_TRUE(catcher.GetNextResult()) << catcher.message();
 }
 
-IN_PROC_BROWSER_TEST_F(SocketApiTest, SocketMulticast) {
+// Fails on MacOS 11, crbug.com/1211141 .
+#if defined(OS_MAC)
+#define MAYBE_SocketMulticast DISABLED_SocketMulticast
+#else
+#define MAYBE_SocketMulticast SocketMulticast
+#endif
+IN_PROC_BROWSER_TEST_F(SocketApiTest, MAYBE_SocketMulticast) {
   ResultCatcher catcher;
   catcher.RestrictToBrowserContext(browser()->profile());
   ExtensionTestMessageListener listener("info_please", true);
