13c2b67f5826ef4d8c048bde4069463ee5e605ab
Xiaohan Wang <xhwang@chromium.org>
2021-05-20 17:57:46 +0000

    Update CancelableCallback threading note
    CancelableCallback uses WeakPtr internally, which can be used on the
    same SequencedTaskRunner. Update CancelableCallback threading note
    to reflect that.
    
    Change-Id: I842ad66523f76e695acec749244c8654c0800ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908293
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885093}

--- base/cancelable_callback.h
+++ base/cancelable_callback.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // THREAD-SAFETY:
 //
 // Cancelable callback objects must be created on, posted to, cancelled on, and
-// destroyed on the same thread.
+// destroyed on the same SequencedTaskRunner.
 //
 //
 // EXAMPLE USAGE:
