e1841e14c32faf681fbe830dffbacd179041e035
Elizabeth Popova <lizapopova@google.com>
2021-05-20 19:17:42 +0000

    [Autofill][Android] Use localized strings in save address prompt
    Bug: 1167061
    Change-Id: I8056af2853e1f173eb5acf4cbbeb3c12bd28d238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907632
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/master@{#885146}

--- chrome/browser/autofill/android/save_address_profile_message_controller.cc
+++ chrome/browser/autofill/android/save_address_profile_message_controller.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/messages/android/message_dispatcher_bridge.h"
+#include "components/strings/grit/components_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace autofill {
 
@@ -113,8 +115,9 @@ void SaveAddressProfileMessageController::RunSaveAddressProfileCallback(
 }
 
 std::u16string SaveAddressProfileMessageController::GetTitle() {
-  // TODO(crbug.com/1167061): Replace with proper localized strings.
-  return original_profile_ ? u"Update address?" : u"Save address?";
+  return l10n_util::GetStringUTF16(
+      original_profile_ ? IDS_AUTOFILL_UPDATE_ADDRESS_PROMPT_TITLE
+                        : IDS_AUTOFILL_SAVE_ADDRESS_PROMPT_TITLE);
 }
 
 std::u16string SaveAddressProfileMessageController::GetDescription() {
@@ -125,8 +128,9 @@ std::u16string SaveAddressProfileMessageController::GetDescription() {
 }
 
 std::u16string SaveAddressProfileMessageController::GetPrimaryButtonText() {
-  // TODO(crbug.com/1167061): Replace with proper localized strings.
-  return original_profile_ ? u"Update…" : u"Save…";
+  return l10n_util::GetStringUTF16(
+      original_profile_ ? IDS_AUTOFILL_UPDATE_ADDRESS_PROMPT_OK_BUTTON_LABEL
+                        : IDS_AUTOFILL_SAVE_ADDRESS_PROMPT_OK_BUTTON_LABEL);
 }
 
 }  // namespace autofill
--- chrome/browser/autofill/android/save_address_profile_message_controller_unittest.cc
+++ chrome/browser/autofill/android/save_address_profile_message_controller_unittest.cc
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/messages/android/mock_message_dispatcher_bridge.h"
+#include "components/strings/grit/components_strings.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace autofill {
 
@@ -133,8 +135,11 @@ SaveAddressProfileMessageControllerTest::GetMessageWrapper() {
 TEST_F(SaveAddressProfileMessageControllerTest, SaveMessageContent) {
   EnqueueSaveMessage(profile_, save_callback_.Get(), action_callback_.Get());
 
-  EXPECT_EQ(u"Save address?", GetMessageWrapper()->GetTitle());
-  EXPECT_EQ(u"Save…", GetMessageWrapper()->GetPrimaryButtonText());
+  EXPECT_EQ(l10n_util::GetStringUTF16(IDS_AUTOFILL_SAVE_ADDRESS_PROMPT_TITLE),
+            GetMessageWrapper()->GetTitle());
+  EXPECT_EQ(l10n_util::GetStringUTF16(
+                IDS_AUTOFILL_SAVE_ADDRESS_PROMPT_OK_BUTTON_LABEL),
+            GetMessageWrapper()->GetPrimaryButtonText());
   EXPECT_EQ(u"John H. Doe, 666 Erebus St.",
             GetMessageWrapper()->GetDescription());
   EXPECT_EQ(ResourceMapper::MapToJavaDrawableId(IDR_ANDROID_AUTOFILL_ADDRESS),
@@ -149,8 +154,11 @@ TEST_F(SaveAddressProfileMessageControllerTest, UpdateMessageContent) {
   EnqueueUpdateMessage(profile_, &original_profile_, save_callback_.Get(),
                        action_callback_.Get());
 
-  EXPECT_EQ(u"Update address?", GetMessageWrapper()->GetTitle());
-  EXPECT_EQ(u"Update…", GetMessageWrapper()->GetPrimaryButtonText());
+  EXPECT_EQ(l10n_util::GetStringUTF16(IDS_AUTOFILL_UPDATE_ADDRESS_PROMPT_TITLE),
+            GetMessageWrapper()->GetTitle());
+  EXPECT_EQ(l10n_util::GetStringUTF16(
+                IDS_AUTOFILL_UPDATE_ADDRESS_PROMPT_OK_BUTTON_LABEL),
+            GetMessageWrapper()->GetPrimaryButtonText());
   EXPECT_EQ(u"Jane A. Smith, 123 Main Street",
             GetMessageWrapper()->GetDescription());
   EXPECT_EQ(ResourceMapper::MapToJavaDrawableId(IDR_ANDROID_AUTOFILL_ADDRESS),
