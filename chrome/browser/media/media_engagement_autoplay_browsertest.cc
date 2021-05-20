7675ca5bf357deff7bf21cf52b8c17f54bf87ffd
Tim Sergeant <tsergeant@chromium.org>
2021-05-20 05:59:41 +0000

    Revert "chrome: run tools/media_engagement_preload/make_dafsa.py in python3"
    This reverts commit 07e05386c5d83fb0d428b3a847ee4ddac4b20bfd.
    
    Reason for revert: MediaEngagementAutoplayBrowserTest is failing on mac bots:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.11%20Tests/63046/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.12%20Tests/43848/overview
    
    Original change's description:
    > chrome: run tools/media_engagement_preload/make_dafsa.py in python3
    >
    > Fixed: 1210295
    > Change-Id: I531f99ba4209086c25157854321e3addf1ce632e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903407
    > Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#884796}
    
    Change-Id: I4bb812fa3ddbfba0450574edf1e4b8e8d36c01eb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909213
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884891}

--- chrome/browser/media/media_engagement_autoplay_browsertest.cc
+++ chrome/browser/media/media_engagement_autoplay_browsertest.cc
@@ -29,8 +29,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 base::FilePath GetPythonPath() {
-  // Every environment should have vpython3.
-  return base::FilePath(FILE_PATH_LITERAL("vpython3"));
+#if defined(OS_WIN)
+  // Windows bots do not have python installed and available on the PATH.
+  // Please see infra/doc/users/python.md
+  base::FilePath bot_path =
+      base::FilePath(FILE_PATH_LITERAL("c:/infra-system/bin/python.exe"));
+
+  if (base::PathExists(bot_path))
+    return bot_path;
+  return base::FilePath(FILE_PATH_LITERAL("python.exe"));
+#else
+  return base::FilePath(FILE_PATH_LITERAL("python"));
+#endif
 }
 
 const base::FilePath kTestDataPath = base::FilePath(
