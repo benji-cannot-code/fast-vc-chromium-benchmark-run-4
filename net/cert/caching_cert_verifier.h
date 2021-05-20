34f1173c0a70fa529608c364fd677f7e72a80b84
Hubert Chao <hchao@google.com>
2021-05-20 21:48:40 +0000

    Add test for CachingCertVerifier confirming that caching includes SCT values.
    Bug: 1208968
    Change-Id: Ibf32e04a67f1671e737993e6e09338d0cb8f34ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910753
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Hubert Chao <hchao@google.com>
    Cr-Commit-Position: refs/heads/master@{#885235}

--- net/cert/caching_cert_verifier.h
+++ net/cert/caching_cert_verifier.h
@@ -53,6 +53,7 @@ class NET_EXPORT CachingCertVerifier : public CertVerifier,
 
  private:
   FRIEND_TEST_ALL_PREFIXES(CachingCertVerifierTest, CacheHit);
+  FRIEND_TEST_ALL_PREFIXES(CachingCertVerifierTest, CacheHitCTResultsCached);
   FRIEND_TEST_ALL_PREFIXES(CachingCertVerifierTest, Visitor);
   FRIEND_TEST_ALL_PREFIXES(CachingCertVerifierTest, AddsEntries);
   FRIEND_TEST_ALL_PREFIXES(CachingCertVerifierTest, DifferentCACerts);
