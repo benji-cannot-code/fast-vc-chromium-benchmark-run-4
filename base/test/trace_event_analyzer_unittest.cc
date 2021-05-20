e02bce460f2c0a3fce68acf7fd18c0cf36feb92e
Sami Kyostila <skyostil@chromium.org>
2021-05-20 15:15:50 +0000

    base/trace_event: Implement Tracelog using Perfetto SDK
    This patch implements tracing in TraceLog on top of the Perfetto SDK
    (a.k.a., the Perfetto client library). This allows code using TraceLog
    to use Perfetto's TRACE_EVENT macro implementation instead of the legacy
    path in //base. In other words, the data flow before was:
    
    1. With the tracing service:
    
      TRACE_EVENT => TraceLog => //services/tracing => perfetto::DataSource
    
    2. Without the tracing service (i.e., in tests):
    
      TRACE_EVENT => TraceLog => TraceBuffer
    
    That is, the tracing service installed an override function that
    redirects all trace events to a custom Perfetto data source implemented
    under //services/tracing. Without the override, we trace directly to
    the legacy TraceBuffer backend.
    
    With this patch (and use_perfetto_client_library=true), all the
    events are written directly to Perfetto:
    
      TRACE_EVENT => perfetto::TrackEvent
    
    Note that only tests are allowed to trace through TraceLog, i.e., call
    TraceLog::SetEnabled(). This is because the conversion from Perfetto's
    protobuf trace format to JSON happens in process in this case, which
    isn't safe to do in the presence of untrusted trace data providers. To
    guard against this, TraceLog will only initialize tracing in single
    process mode.
    
    Notable test expectation changes:
    
    - Tests looking for discrete BEGIN/END events have been disabled, since
      Perfetto always merges these into COMPLETE events.
    - Tests checking TraceLog's legacy internals (e.g., TraceBuffer) have
      been disabled, since that code isn't used in Perfetto mode.
    - Added a check against wildchar trace configs ("foo_?") being used,
      because Perfetto only supports basic wildcard matching.
    - Tests retrieving a handle to an emitted event have been disabled,
      because that isn't possible in Perfetto mode.
    
    Change-Id: Ia8197bc2f2055a08e87f6708c2abdbc92a1287f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2748300
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885032}

--- base/test/trace_event_analyzer_unittest.cc
+++ base/test/trace_event_analyzer_unittest.cc
@@ -407,7 +407,9 @@ TEST_F(TraceEventAnalyzerTest, StringPattern) {
   EXPECT_STREQ("no match", found[0]->name.c_str());
 }
 
-// Test that duration queries work.
+// Test that duration queries work. (BEGIN/END events aren't emitted by
+// Perfetto.)
+#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 TEST_F(TraceEventAnalyzerTest, BeginEndDuration) {
   ManualSetUp();
 
@@ -452,6 +454,7 @@ TEST_F(TraceEventAnalyzerTest, BeginEndDuration) {
   EXPECT_STREQ("name1", found[0]->name.c_str());
   EXPECT_STREQ("name3", found[1]->name.c_str());
 }
+#endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
 // Test that duration queries work.
 TEST_F(TraceEventAnalyzerTest, CompleteDuration) {
@@ -494,7 +497,8 @@ TEST_F(TraceEventAnalyzerTest, CompleteDuration) {
   EXPECT_STREQ("name3", found[1]->name.c_str());
 }
 
-// Test AssociateBeginEndEvents
+// Test AssociateBeginEndEvents. (BEGIN/END events aren't emitted by Perfetto.)
+#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 TEST_F(TraceEventAnalyzerTest, BeginEndAssocations) {
   ManualSetUp();
 
@@ -518,8 +522,10 @@ TEST_F(TraceEventAnalyzerTest, BeginEndAssocations) {
   ASSERT_EQ(1u, found.size());
   EXPECT_STREQ("name2", found[0]->name.c_str());
 }
+#endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
-// Test MergeAssociatedEventArgs
+// Test MergeAssociatedEventArgs. (BEGIN/END events aren't emitted by Perfetto.)
+#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 TEST_F(TraceEventAnalyzerTest, MergeAssociatedEventArgs) {
   ManualSetUp();
 
@@ -546,6 +552,7 @@ TEST_F(TraceEventAnalyzerTest, MergeAssociatedEventArgs) {
   EXPECT_TRUE(found[0]->GetArgAsString("arg", &arg_actual));
   EXPECT_STREQ(arg_string, arg_actual.c_str());
 }
+#endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
 // Test AssociateAsyncBeginEndEvents
 TEST_F(TraceEventAnalyzerTest, AsyncBeginEndAssocations) {
--- base/trace_event/trace_log.h
+++ base/trace_event/trace_log.h
@@ -28,11 +28,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
+namespace perfetto {
+namespace trace_processor {
+class TraceProcessorStorage;
+}  // namespace trace_processor
+}  // namespace perfetto
+
 namespace base {
 class RefCountedString;
 
 template <typename T>
 class NoDestructor;
+
+namespace tracing {
+class PerfettoPlatform;
+}  // namespace tracing
 
 namespace trace_event {
 
@@ -42,6 +52,7 @@ class TraceBufferChunk;
 class TraceEvent;
 class TraceEventFilter;
 class TraceEventMemoryOverhead;
+class JsonStringOutputWriter;
 
 struct BASE_EXPORT TraceLogStatus {
   TraceLogStatus();
@@ -50,7 +61,11 @@ struct BASE_EXPORT TraceLogStatus {
   uint32_t event_count;
 };
 
-class BASE_EXPORT TraceLog : public MemoryDumpProvider {
+class BASE_EXPORT TraceLog :
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+    public perfetto::TrackEventSessionObserver,
+#endif  // BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+    public MemoryDumpProvider {
  public:
   // Argument passed to TraceLog::SetEnabled.
   enum Mode : uint8_t {
@@ -92,8 +107,12 @@ class BASE_EXPORT TraceLog : public MemoryDumpProvider {
   // Returns true if TraceLog is enabled on recording mode.
   // Note: Returns false even if FILTERING_MODE is enabled.
   bool IsEnabled() {
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+    return perfetto::TrackEvent::IsEnabled();
+#else   // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
     AutoLock lock(lock_);
     return enabled_modes_ & RECORDING_MODE;
+#endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   }
 
   // Returns a bitmap of enabled modes from TraceLog::Mode.
@@ -372,9 +391,11 @@ class BASE_EXPORT TraceLog : public MemoryDumpProvider {
   // sort index, ascending, then by their name, and then tid.
   void SetThreadSortIndex(PlatformThreadId thread_id, int sort_index);
 
+#if !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   // Allow setting an offset between the current TimeTicks time and the time
   // that should be reported.
   void SetTimeOffset(TimeDelta offset);
+#endif  // !BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
   size_t GetObserverCountForTest() const;
 
@@ -392,6 +413,13 @@ class BASE_EXPORT TraceLog : public MemoryDumpProvider {
 
   // Replaces |logged_events_| with a new TraceBuffer for testing.
   void SetTraceBufferForTesting(std::unique_ptr<TraceBuffer> trace_buffer);
+
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+  // perfetto::TrackEventSessionObserver implementation.
+  void OnSetup(const perfetto::DataSourceBase::SetupArgs&) override;
+  void OnStart(const perfetto::DataSourceBase::StartArgs&) override;
+  void OnStop(const perfetto::DataSourceBase::StopArgs&) override;
+#endif  // BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
  private:
   typedef unsigned int InternalTraceOptions;
@@ -465,6 +493,11 @@ class BASE_EXPORT TraceLog : public MemoryDumpProvider {
   void FlushInternal(const OutputCallback& cb,
                      bool use_worker_thread,
                      bool discard_events);
+
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+  tracing::PerfettoPlatform* GetOrCreatePerfettoPlatform();
+  void OnTraceData(const char* data, size_t size, bool has_more);
+#endif  // BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 
   // |generation| is used in the following callbacks to check if the callback
   // is called for the flush of the current |logged_events_|.
@@ -575,7 +608,15 @@ class BASE_EXPORT TraceLog : public MemoryDumpProvider {
   std::atomic<OnFlushFunction> on_flush_override_{nullptr};
   std::atomic<UpdateDurationFunction> update_duration_override_{nullptr};
 
-  FilterFactoryForTesting filter_factory_for_testing_;
+#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+  std::unique_ptr<::base::tracing::PerfettoPlatform> perfetto_platform_;
+  std::unique_ptr<perfetto::TracingSession> tracing_session_;
+  std::unique_ptr<perfetto::trace_processor::TraceProcessorStorage>
+      trace_processor_;
+  std::unique_ptr<JsonStringOutputWriter> json_output_writer_;
+#endif  // BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
+
+  FilterFactoryForTesting filter_factory_for_testing_ = nullptr;
 
 #if defined(OS_ANDROID)
   absl::optional<TraceConfig> atrace_startup_config_;
