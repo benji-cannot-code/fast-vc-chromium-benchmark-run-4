9344a518f4816b6bafb790bc9afad62d0da0c43b
Katie Dektar <katie@chromium.org>
2021-05-20 17:46:01 +0000

    Revert "Make mime type sniffing in DevToolsURLLoaderInterceptor consistent with real URLLoader"
    This reverts commit 918d412d5f83ccb98909ebe1478667a85c1737ae.
    
    Reason for revert:
    http/tests/inspector-protocol/fetch/mime-type-sniffing.js consistently failing on linux-trusty-rel. Regression began when this change landed: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-trusty-rel/24163/blamelist
    
    Original change's description:
    > Make mime type sniffing in DevToolsURLLoaderInterceptor consistent with real URLLoader
    >
    > - extract ShouldSniffContent() from url_loader.cc to header_util.{h,cc}
    > - reuse it in DevToolsURLLoaderInterceptor
    >
    > Bug: b/187745024
    > Change-Id: I0cd6904e7912c372aa744b0abb038d867c9195d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905075
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#884328}
    
    Bug: b/187745024,1210841
    Change-Id: I73260f7b6fe85d323eba0946c5386becab4c898c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910515
    Owners-Override: Katie Dektar <katie@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885087}

--- content/browser/devtools/protocol/network_handler.cc
+++ content/browser/devtools/protocol/network_handler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/queue.h"
 #include "base/i18n/i18n_constants.h"
 #include "base/i18n/icu_string_conversions.h"
+#include "base/json/json_reader.h"
 #include "base/process/process_handle.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
