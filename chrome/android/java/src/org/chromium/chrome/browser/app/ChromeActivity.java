5f84d2d0c89fafb3d2ff9acbab6a6333bc83d8f0
liberato@chromium.org <liberato@chromium.org>
2021-05-20 21:40:38 +0000

    Refactor Android Picture in Picture support for Android S.
    This change refactors existing Picture in Picture support to allow
    Android S's auto-enter support.  It does not actually change the
    existing behavior; it just rearranges things to make it easier to add.
    
    The biggest change is that ownership of the various state observers is
    moved to the observer chain itself, rather than being kept within the
    PictureInPictureController.  This facilitates allowing the observers to
    stay around even outside of Picture in Picture mode.  That's required
    for auto-enter support later, but it's a big enough change to introduce
    separately from that.
    
    This change also causes ChromeActivity to wait until it receives
    notification from Android that Picture in Picture has started, before
    it tells WebContents / etc. about it.  Previously, it did that
    speculatively.  Since there's a relayout anyway when we finally get
    the Picture in Picture window size, visually it's no different.
    
    Change-Id: I0ed54c1a7218d9fb4b5c413632453de030bce31a
    Bug: 1209247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2881120
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885228}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -1069,6 +1069,13 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         getManualFillingComponent().onResume();
     }
 
+    private void ensurePictureInPictureController() {
+        if (mPictureInPictureController == null) {
+            mPictureInPictureController = new PictureInPictureController(
+                    this, getActivityTabProvider(), getFullscreenManager());
+        }
+    }
+
     @Override
     protected void onUserLeaveHint() {
         super.onUserLeaveHint();
@@ -1078,12 +1085,24 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         // Can be in finishing state. No need to attempt PIP.
         if (isActivityFinishingOrDestroyed()) return;
 
-        if (mPictureInPictureController == null) {
-            mPictureInPictureController = new PictureInPictureController(
-                    this, getActivityTabProvider(), getFullscreenManager());
-        }
-
+        ensurePictureInPictureController();
         mPictureInPictureController.attemptPictureInPicture();
+        // The attempt might not be successful.  If it is, then `onPictureInPictureModeChanged` will
+        // let us know later.
+    }
+
+    /**
+     * When we're notified that Picture-in-Picture mode has changed, make sure that the controller
+     * is kept up-to-date.
+     */
+    @Override
+    public void onPictureInPictureModeChanged(boolean inPicture, Configuration newConfig) {
+        if (inPicture) {
+            ensurePictureInPictureController();
+            mPictureInPictureController.onEnteredPictureInPictureMode();
+        } else if (mPictureInPictureController != null) {
+            mPictureInPictureController.cleanup();
+        }
     }
 
     @Override
