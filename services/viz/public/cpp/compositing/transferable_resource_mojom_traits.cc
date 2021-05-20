85744d31f0480ccfceab567fdce1dfd3d61e5312
Miguel Casas <mcasas@chromium.org>
2021-05-20 22:50:32 +0000

    viz: add optional HDRMetadata to TransferableResource
    This CL adds a new HDRMetadata field to TransferableResource, fills it
    from the VideoResourceUpdater and wires it to the GLRenderer where
    it'll be reconciled with the display's supported luminance range. This
    would support e.g. content mastered between 0.01-1000 nits needs
    to be presented in a display supporting only 1-500 nits, hence HDR10.
    
    Bug: b:183236148
    Change-Id: Iace0c643cd77aea25579b3b68154515fbd5b72b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880838
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885266}

--- services/viz/public/cpp/compositing/transferable_resource_mojom_traits.cc
+++ services/viz/public/cpp/compositing/transferable_resource_mojom_traits.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/viz/public/cpp/compositing/resource_id_mojom_traits.h"
 #include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
 #include "ui/gfx/mojom/color_space_mojom_traits.h"
+#include "ui/gfx/mojom/hdr_metadata_mojom_traits.h"
 
 namespace mojo {
 
@@ -23,6 +24,7 @@ bool StructTraits<viz::mojom::TransferableResourceDataView,
   if (!data.ReadSize(&out->size) || !data.ReadFormat(&out->format) ||
       !data.ReadMailboxHolder(&out->mailbox_holder) ||
       !data.ReadColorSpace(&out->color_space) ||
+      !data.ReadHdrMetadata(&out->hdr_metadata) ||
       !data.ReadYcbcrInfo(&out->ycbcr_info) || !data.ReadId(&id)) {
     return false;
   }
