86ea0005bd453abc1ddda3090f417965dee1e2d5
David Tseng <dtseng@google.com>
2021-05-20 19:26:25 +0000

    Fix small typo in ItemScanManager test
    R=anastasi@google.com, josiahk@google.com
    
    Bug: none
    Test: cq
    Change-Id: I197a99f9dc2f37fdfd85b7565e6b135c10b09731
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910813
    Auto-Submit: David Tseng <dtseng@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885158}

--- chrome/browser/resources/chromeos/accessibility/switch_access/item_scan_manager_test.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/item_scan_manager_test.js
@@ -408,7 +408,7 @@ TEST_F(
 
         key.performAction('select');
 
-        if (!input.automationNode.value !== 'q') {
+        if (input.automationNode.value !== 'q') {
           // Wait for the potential value change.
           await new Promise(resolve => {
             input.automationNode.addEventListener(
