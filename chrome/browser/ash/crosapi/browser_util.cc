aae0c620249df42fdd3dcfb8b4706de5861b3da3
Gavin Williams <gavinwill@chromium.org>
2021-05-20 18:16:48 +0000

    Lacros: Create Drive Integration Service mojo interface
    - Mojo endpoint for Lacros to access the ash Drive Integration Service
    
    - Drive Integration Service give the file path for the local Google
      Drive mount
    
    - Will be used by the Print Preview to implement Save to Drive
    
    Bug: 1192966
    Change-Id: I1a5954e5127590da36cadb0b48e975e0c1a97485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903855
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885109}

--- chrome/browser/ash/crosapi/browser_util.cc
+++ chrome/browser/ash/crosapi/browser_util.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/crosapi.mojom.h"
 #include "chromeos/crosapi/mojom/device_attributes.mojom.h"
 #include "chromeos/crosapi/mojom/download_controller.mojom.h"
+#include "chromeos/crosapi/mojom/drive_integration_service.mojom.h"
 #include "chromeos/crosapi/mojom/feedback.mojom.h"
 #include "chromeos/crosapi/mojom/file_manager.mojom.h"
 #include "chromeos/crosapi/mojom/holding_space_service.mojom.h"
@@ -221,6 +222,7 @@ constexpr InterfaceVersionEntry kInterfaceVersionEntries[] = {
     MakeInterfaceVersionEntry<crosapi::mojom::Crosapi>(),
     MakeInterfaceVersionEntry<crosapi::mojom::DeviceAttributes>(),
     MakeInterfaceVersionEntry<crosapi::mojom::DownloadController>(),
+    MakeInterfaceVersionEntry<crosapi::mojom::DriveIntegrationService>(),
     MakeInterfaceVersionEntry<crosapi::mojom::Feedback>(),
     MakeInterfaceVersionEntry<crosapi::mojom::FileManager>(),
     MakeInterfaceVersionEntry<crosapi::mojom::HoldingSpaceService>(),
@@ -260,7 +262,7 @@ constexpr bool HasDuplicatedUuid() {
 }
 
 static_assert(
-    crosapi::mojom::Crosapi::Version_ == 28,
+    crosapi::mojom::Crosapi::Version_ == 29,
     "if you add a new crosapi, please add it to kInterfaceVersionEntries");
 static_assert(!HasDuplicatedUuid(),
               "Each Crosapi Mojom interface should have unique UUID.");
