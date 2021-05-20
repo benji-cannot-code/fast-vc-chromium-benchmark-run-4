caf5de0724586242fc317db574d71371b4f3b51e
Sreeja Kamishetty <sreejakshetty@chromium.org>
2021-05-20 12:07:45 +0000

    Add test helpers to check IsCommitDeferringConditionDeferredForTesting
    Bug: 1199724
    Change-Id: I3941bdc0e0f739e124a97303319f78a34a883f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2901998
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884972}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -1488,6 +1488,10 @@ void NavigationRequest::RegisterCommitDeferringConditionForTesting(
   commit_deferrer_->AddConditionForTesting(std::move(condition));  // IN-TEST
 }
 
+bool NavigationRequest::IsCommitDeferringConditionDeferredForTesting() {
+  return commit_deferrer_->is_deferred_for_testing();  // IN-TEST
+}
+
 void NavigationRequest::BeginNavigation() {
   EnterChildTraceEvent("BeginNavigation", this);
   DCHECK(!loader_);
