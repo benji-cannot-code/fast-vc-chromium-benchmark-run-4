c0f8f1ca1239773f0dcd340bc1c470af2b1da4b3
rajendrant <rajendrant@chromium.org>
2021-05-20 20:21:55 +0000

    SubresourceRedirect: Fix images in non http(s) pages
    This CL removes a DCHECK that is hit when non http/https pages have
    images.
    
    Change-Id: Ib1ba6f9a653294ec0313e0c48567c90e4601ddff
    Bug: 1211102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910974
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885184}

--- chrome/renderer/subresource_redirect/login_robots_decider_agent_browsertest.cc
+++ chrome/renderer/subresource_redirect/login_robots_decider_agent_browsertest.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/memory/weak_ptr.h"
-#include "base/test/gtest_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/renderer/subresource_redirect/login_robots_decider_agent.h"
 #include "chrome/renderer/subresource_redirect/robots_rules_parser_cache.h"
@@ -232,7 +231,7 @@ TEST_F(SubresourceRedirectLoginRobotsDeciderAgentTest,
   EXPECT_EQ(SubresourceRedirectResult::kUnknown,
             decider_agent_redirect_result());
   EXPECT_FALSE(*is_pending_navigation_loggged_in());
-  EXPECT_DCHECK_DEATH(ShouldRedirectSubresource("https://foo.com/public.jpg"));
+  EXPECT_FALSE(ShouldRedirectSubresource("https://foo.com/public.jpg"));
 }
 
 TEST_F(SubresourceRedirectLoginRobotsDeciderAgentTest,
@@ -248,7 +247,7 @@ TEST_F(SubresourceRedirectLoginRobotsDeciderAgentTest,
   EXPECT_EQ(SubresourceRedirectResult::kUnknown,
             decider_agent_redirect_result());
   EXPECT_FALSE(is_pending_navigation_loggged_in().has_value());
-  EXPECT_DCHECK_DEATH(ShouldRedirectSubresource("https://foo.com/public.jpg"));
+  EXPECT_FALSE(ShouldRedirectSubresource("https://foo.com/public.jpg"));
 }
 
 }  // namespace subresource_redirect
