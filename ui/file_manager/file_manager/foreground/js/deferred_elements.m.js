958e49e903f7752ce1a4f1d679aae60a4c6e210b
Luciano Pacheco <lucmult@chromium.org>
2021-05-20 02:09:17 +0000

    Files app: Remove Polymer2 from FilesFormatDialog
    Note: This removes the closure type check for the non-module version of
    the code, since we only run the JS-module version.
    
    Bug: 1199015
    Change-Id: I2b525af0f51381f6b28338e727b7a42c049c0508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905117
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884825}

--- ui/file_manager/file_manager/foreground/js/deferred_elements.m.js
+++ ui/file_manager/file_manager/foreground/js/deferred_elements.m.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import 'chrome://resources/cr_elements/cr_toggle/cr_toggle.m.js';
-import '../elements/files_format_dialog.m.js';
+import '../elements/files_format_dialog.js';
 import '../elements/files_message.m.js';
 import '../elements/files_password_dialog.m.js';
 import '../elements/files_ripple.m.js';
