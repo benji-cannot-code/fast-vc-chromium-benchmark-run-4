199163baf9a40d2ae0004378558a330a23cb1e43
Ben Reich <benreich@chromium.org>
2021-05-20 02:39:28 +0000

    [js-code-coverage] Add WebUICoverageObserver to WebUI test harness
    BaseWebUIBrowserTest is the runner for WebUI browser tests.
    RunJavaScriptTestF provides an entrypoint for JavaScript tests written
    in Mocha. Prior to invoking the fixture, a DevToolsListener instance is
    attached to the DevToolsAgentHost. This ensures the correct sequence of
    CDP messages is emitted to turn on v8 code coverage.
    
    When the fixture has finished executing, code coverage is disabled and
    collected into a directory (supplied by the --devtools-code-coverage CLI
    switch). At the moment this simply runs in the fixture, if any tests
    are executed by directly calling the RunJavascriptTest functions
    coverage is not collected for them without explicitly calling
    CollectCoverage.
    
    --devtools-code-coverage=/tmp/a
    
    Bug: 1206082
    Test: Build and run browser_tests --gtest_filter=NewTabPage*
    AX-Relnotes: n/a
    Change-Id: I99f59b29b09900dce7019f49be85c170a5a6f4ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893893
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#884832}

--- chrome/test/base/web_ui_browser_test.h
+++ chrome/test/base/web_ui_browser_test.h
@@ -13,22 +13,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "chrome/browser/ui/webui/web_ui_test_handler.h"
+#include "chrome/test/base/devtools_listener.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/javascript_browser_test.h"
+#include "content/public/browser/devtools_agent_host_observer.h"
 
 namespace {
 class WebUITestMessageHandler;
-}
+class WebUICoverageObserver;
+}  // namespace
 
 namespace base {
 class Value;
-}
+}  // namespace base
 
 namespace content {
 class RenderFrameHost;
 class ScopedWebUIControllerFactoryRegistration;
 class WebUI;
-}
+}  // namespace content
 
 class TestChromeWebUIControllerFactory;
 
@@ -185,6 +188,8 @@ class BaseWebUIBrowserTest : public JavaScriptBrowserTest {
   std::unique_ptr<TestChromeWebUIControllerFactory> test_factory_;
   std::unique_ptr<content::ScopedWebUIControllerFactoryRegistration>
       factory_registration_;
+
+  std::unique_ptr<WebUICoverageObserver> coverage_handler_;
 };
 
 class WebUIBrowserTest : public BaseWebUIBrowserTest {
