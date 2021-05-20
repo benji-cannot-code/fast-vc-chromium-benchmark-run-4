fa403a8806e29a598778cf5c6b0f9a4f52373a1f
Brian Geffon <bgeffon@chromium.org>
2021-05-20 15:55:57 +0000

    Mojo: Linux channel break out of loop on read fail
    Make sure we break out of both loops on read issues.
    
    Bug: chromium:1175855
    Change-Id: I979a06bb526fe32a03be5bfe5d04f737518e3646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907112
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885046}

--- mojo/core/channel_linux.cc
+++ mojo/core/channel_linux.cc
@@ -779,6 +779,7 @@ void ChannelLinux::SharedMemReadReady() {
   CHECK(read_buffer_);
   if (read_buffer_->TryLockForReading()) {
     read_notifier_->Clear();
+    bool read_fail = false;
     do {
       uint32_t bytes_read = 0;
       SharedBuffer::Error read_res = read_buffer_->TryReadLocked(
@@ -786,7 +787,6 @@ void ChannelLinux::SharedMemReadReady() {
       if (read_res == SharedBuffer::Error::kControlCorruption) {
         // This is an error we cannot recover from.
         OnError(Error::kReceivedMalformedData);
-        read_buffer_->UnlockForReading();
         break;
       }
 
@@ -810,6 +810,7 @@ void ChannelLinux::SharedMemReadReady() {
         // full message if we get one something has gone horribly wrong.
         if (result != DispatchResult::kOK) {
           LOG(ERROR) << "Recevied a bad message via shared memory";
+          read_fail = true;
           OnError(Error::kReceivedMalformedData);
           break;
         }
@@ -820,7 +821,7 @@ void ChannelLinux::SharedMemReadReady() {
         // starts.
         data_offset += read_size_hint;
       }
-    } while (true);
+    } while (!read_fail);
     read_buffer_->UnlockForReading();
   }
 }
