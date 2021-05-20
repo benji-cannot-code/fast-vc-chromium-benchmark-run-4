142b121c994a97b6e15f59997954aa5ae7ec9607
Rodney Ding <rodneyding@google.com>
2021-05-20 23:19:45 +0000

    Add histogram for tracking parsed reporting api header types
    This will be used to guide the deprecation of the old Report-To header.
    New enums will be added to the histogram tracking the new
    Reporting-Endpoints header spec'd in Document Reporting, which should
    give us insight into if/how developers are migrating to the new header.
    
    Bug: 1210759
    Change-Id: I422b5e65893088e5b7921c49cc091e301a18432f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903628
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Cr-Commit-Position: refs/heads/master@{#885284}

--- net/reporting/reporting_header_parser.cc
+++ net/reporting/reporting_header_parser.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/feature_list.h"
 #include "base/json/json_reader.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "net/base/features.h"
@@ -260,6 +260,12 @@ bool ProcessEndpointGroupStructuredHeader(
 }  // namespace
 
 // static
+void ReportingHeaderParser::RecordReportingHeaderType(
+    ReportingHeaderType header_type) {
+  base::UmaHistogramEnumeration("Net.Reporting.HeaderType", header_type);
+}
+
+// static
 void ReportingHeaderParser::ParseReportToHeader(
     ReportingContext* context,
     const NetworkIsolationKey& network_isolation_key,
@@ -289,11 +295,17 @@ void ReportingHeaderParser::ParseReportToHeader(
     }
   }
 
+  if (parsed_header.empty() && group_list->GetSize() > 0) {
+    RecordReportingHeaderType(ReportingHeaderType::kReportToInvalid);
+  }
+
   // Remove the client if it has no valid endpoint groups.
   if (parsed_header.empty()) {
     cache->RemoveClient(network_isolation_key, origin);
     return;
   }
+
+  RecordReportingHeaderType(ReportingHeaderType::kReportTo);
 
   cache->OnParsedHeader(network_isolation_key, origin,
                         std::move(parsed_header));
