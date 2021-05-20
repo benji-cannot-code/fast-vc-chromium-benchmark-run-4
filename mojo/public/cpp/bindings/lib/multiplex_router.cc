3b90051880f1460f76bf991e83ac8058581ef322
John Abd-El-Malek <jam@chromium.org>
2021-05-20 23:19:42 +0000

    Revert "GPU: Introduce CommandBuffer Mojo interface"
    This reverts commit 1b1ee65f84ea20408262907ed4ba107836807a4a.
    
    Reason for revert: breaks ProcessHostOnUI which is being Finch'd. Will reland once we're sure https://chromium-review.googlesource.com/c/chromium/src/+/2910960 and this can land for next canary
    
    Original change's description:
    > GPU: Introduce CommandBuffer Mojo interface
    >
    > This adds CommandBuffer as a new Mojo interface associated with its
    > owning GpuChannel. There's also a corresponding CommandBufferClient
    > associated and running in the opposite direction. Since there's
    > a good bit of new infrastructure involved to ensure behavioral
    > parity with the current message dispatch, this CL only migrates
    > one message to each new interface for now.
    >
    > To replicate the existing scheduling behavior of messages moved to
    > the CommandBuffer interface, a new gpu::SchedulerTaskRunner is
    > introduced to expose a base::SequencedTaskRunner interface for a
    > specific sequence on the gpu::Scheduler. The CommandBuffer's
    > Mojo receiver binds using this task runner.
    >
    > This also requires loosening some sequence safety checks inside Mojo
    > bindings. To wit, we want all received messages to be dispatched by
    > a SchedulerTaskRunner, but we want to set up and tear down the
    > receiver from tasks that are NOT run by that SchedulerTaskRunner.
    > Since the base sequencing APIs (and in turn Mojo bindings' internal
    > safety checks) aren't very well suited to this kind of arrangement,
    > we have to pull a few strings: endpoints may now be bound to a
    > sequence other than the one doing the binding (this turns out to be
    > trivially safe to allow), and they can also be torn down from a
    > sequence other than the one to which they're bound, provided the
    > caller knows what they're doing and uses the dubious but aptly
    > foreboding ResetFromAnotherSequenceUnsafe().
    >
    > Bug: 1196476
    > Change-Id: Ie2b2cd775c271f0e9a105949e9df65b88f21ffa2
    > Binary-Size: New usage of associated endpoints introduces a lot of new generated code. https://crbug.com/1208544
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2863930
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#885034}
    
    Bug: 1196476
    Change-Id: I5a85fc8e5dc7acd7b1eb644bd8b33a9b1b5d4222
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910501
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Owners-Override: Katie Dektar <katie@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885283}

--- mojo/public/cpp/bindings/lib/multiplex_router.cc
+++ mojo/public/cpp/bindings/lib/multiplex_router.cc
@@ -89,6 +89,7 @@ class MultiplexRouter::InterfaceEndpoint
     router_->AssertLockAcquired();
     DCHECK(!client_);
     DCHECK(!closed_);
+    DCHECK(runner->RunsTasksInCurrentSequence());
 
     task_runner_ = std::move(runner);
     client_ = client;
@@ -99,6 +100,7 @@ class MultiplexRouter::InterfaceEndpoint
   void DetachClient() {
     router_->AssertLockAcquired();
     DCHECK(client_);
+    DCHECK(task_runner_->RunsTasksInCurrentSequence());
     DCHECK(!closed_);
 
     task_runner_ = nullptr;
