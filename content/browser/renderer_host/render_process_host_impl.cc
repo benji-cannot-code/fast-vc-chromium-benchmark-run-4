332b6c6770d588a1a24961d6f72014f7d1df9a75
Bruce Dawson <brucedawson@chromium.org>
2021-05-21 15:36:33 +0000

    Put --extension-process earlier on the command line
    The process type (--type, --utility-sub-type, and --extension-process)
    flags are useful for human beings looking at chrome command lines.
    Therefore they should be early, before the non-human-readable switches.
    Moving them early also avoids problems with them being trimmed by tools
    (such as ETW tracing) that have limits on the lengths of the command
    lines that they can record.
    
    This change will ensure that UIforETW can reliably identify extension
    processes and will aid human readers also.
    
    Bug: 1211558
    Change-Id: Ib3f62224d4dc2cc13cf4fc234846b25cbad62d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910481
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885493}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -3245,6 +3245,11 @@ void RenderProcessHostImpl::AppendRendererCommandLine(
   command_line->AppendSwitchASCII(switches::kProcessType,
                                   switches::kRendererProcess);
 
+  // Call this as early as possible so that --extension-process will show early
+  // in process listings. See https://crbug.com/1211558 for details.
+  GetContentClient()->browser()->AppendExtraCommandLineSwitches(command_line,
+                                                                GetID());
+
 #if defined(OS_WIN)
   command_line->AppendArg(switches::kPrefetchArgumentRenderer);
 #endif  // defined(OS_WIN)
@@ -3265,9 +3270,6 @@ void RenderProcessHostImpl::AppendRendererCommandLine(
            .empty()) {
     command_line->AppendSwitch(switches::kNoZygote);
   }
-
-  GetContentClient()->browser()->AppendExtraCommandLineSwitches(command_line,
-                                                                GetID());
 
 #if defined(OS_WIN)
   command_line->AppendSwitchASCII(
