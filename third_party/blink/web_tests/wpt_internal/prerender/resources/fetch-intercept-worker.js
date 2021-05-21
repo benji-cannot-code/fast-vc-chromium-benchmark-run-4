6401aafb36ebb0dd41ffd7c64a8e14aaf6871d4e
Asami Doi <asamidoi@chromium.org>
2021-05-21 09:28:54 +0000

    Prerender: add a test for navigation with service worker
    This CL adds a test to check if a navigation request is intercepted by
    a service worker and starts prerendering if it's in the scope.
    
    Bug: 1211211
    Change-Id: I8008a3ee5b3822baff3aa9440f8df2f2d4109574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909941
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885432}

--- third_party/blink/web_tests/wpt_internal/prerender/resources/fetch-intercept-worker.js
+++ third_party/blink/web_tests/wpt_internal/prerender/resources/fetch-intercept-worker.js
@@ -1,5 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 self.addEventListener('fetch', e => {
-  if (e.request.url.includes('should-intercept'))
-    e.respondWith(new Response('intercepted by service worker'));
+  if (e.request.url.includes('should-intercept')) {
+    if (e.request.destination === 'document') {
+      e.respondWith(fetch('./prerendered-page.html'));
+    } else {
+      e.respondWith(new Response('intercepted by service worker'));
+    }
+  }
 });
