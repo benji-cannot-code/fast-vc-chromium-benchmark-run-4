dc57838ba4e35bc1c3dc2dc927a6d988d36363bd
Friedrich Horschig <fhorschig@chromium.org>
2021-05-21 07:37:51 +0000

    [Android][Mfill] Remove webContents caching for KAv2
    This CL completes the wiring that makes browser-scoped sheets
    possible by:
    * registering the updater to the filling state so that the sheets can
      be refreshed as soon as a state becomes active
    * preventing cached responses from providers by adding a
      ConditionalProviderAdapter.
        * Like the CachedProviderAdapter, it stops data propagation if
          browser tabs are inactive.
        * Unlike the CachedProviderAdapter, it doesn't keep a cached copy
          of the provided sheet data.
    
    
    Details and history: (already reflected in the README)
    ====================
    
    The manual filling component has two differently scoped parts:
    The ManualFillingComponent (UI, all coordinators) that should exist once
    for each browser instance.
    The filling controllers (native up to ManualFillingComponentBridge) that
    need to exists once per WebContents (i.e. Tab).
    
    Before this CL, the ManualFillingComponent creates and caches a sheet
    for each Tab. That includes the large sheetData and a sheet component
    that even contains parts of the sheet's view.
    Especially in V1, this helps to switch quickly between tabs without
    having to reload the data from the native side.
    
    With this CL, the ManualFillingComponent creates only one sheet per
    sheet type (pwds, addresses, CCs) that is used for all Tabs. Whenever
    the tab changes, the native side will be asked to provide a new sheet.
    This is expensive (but due to recent refactorings and changed behavior
    in V2) in terms of speed but significantly cheaper in terms of memory.
    
    The exact impact on speed hasn't been measured yet but the retrieval
    can easily become asynchronous and the sheet can temporarily become a
    loading spinner.
    
    Bug: 1169167
    Change-Id: I21ea2fa64635dad10a11d7c8a69d59c4fb9c7dcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2859921
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885413}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingTestHelper.java
@@ -51,7 +51,6 @@ import org.chromium.chrome.browser.autofill.AutofillTestHelper;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryCoordinator;
 import org.chromium.chrome.browser.keyboard_accessory.data.KeyboardAccessoryData;
-import org.chromium.chrome.browser.keyboard_accessory.data.KeyboardAccessoryData.AccessorySheetData;
 import org.chromium.chrome.browser.keyboard_accessory.data.PropertyProvider;
 import org.chromium.chrome.browser.keyboard_accessory.sheet_tabs.AddressAccessorySheetCoordinator;
 import org.chromium.chrome.browser.keyboard_accessory.sheet_tabs.CreditCardAccessorySheetCoordinator;
@@ -82,8 +81,6 @@ public class ManualFillingTestHelper {
     private final ChromeTabbedActivityTestRule mActivityTestRule;
     private final AtomicReference<WebContents> mWebContentsRef = new AtomicReference<>();
     private TestInputMethodManagerWrapper mInputMethodManagerWrapper;
-    private PropertyProvider<AccessorySheetData> mSheetSuggestionsProvider =
-            new PropertyProvider<>();
 
     private EmbeddedTestServer mEmbeddedTestServer;
 
@@ -133,8 +130,6 @@ public class ManualFillingTestHelper {
             final ImeAdapter imeAdapter = ImeAdapter.fromWebContents(mWebContentsRef.get());
             mInputMethodManagerWrapper = TestInputMethodManagerWrapper.create(imeAdapter);
             imeAdapter.setInputMethodManagerWrapper(mInputMethodManagerWrapper);
-            getManualFillingCoordinator().registerSheetDataProvider(
-                    mWebContentsRef.get(), AccessoryTabType.PASSWORDS, mSheetSuggestionsProvider);
         });
     }
 
--- chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingControllerTest.java
+++ chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/ManualFillingControllerTest.java
@@ -56,7 +56,6 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.UserDataHost;
 import org.chromium.base.metrics.test.ShadowRecordHistogram;
-import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.ActivityTabProvider;
 import org.chromium.chrome.browser.ChromeWindow;
@@ -65,6 +64,7 @@ import org.chromium.chrome.browser.compositor.CompositorViewHolder;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.fullscreen.BrowserControlsManager;
 import org.chromium.chrome.browser.fullscreen.FullscreenManager;
+import org.chromium.chrome.browser.keyboard_accessory.ManualFillingComponent.UpdateAccessorySheetDelegate;
 import org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryCoordinator;
 import org.chromium.chrome.browser.keyboard_accessory.data.KeyboardAccessoryData;
 import org.chromium.chrome.browser.keyboard_accessory.data.KeyboardAccessoryData.AccessorySheetData;
@@ -376,11 +376,14 @@ public class ManualFillingControllerTest {
     public void testPasswordItemsPersistAfterSwitchingBrowserTabs() {
         SheetProviderHelper firstTabHelper = new SheetProviderHelper();
         SheetProviderHelper secondTabHelper = new SheetProviderHelper();
+        UpdateAccessorySheetDelegate firstSheetUpdater = mock(UpdateAccessorySheetDelegate.class);
+        UpdateAccessorySheetDelegate secondSheetUpdater = mock(UpdateAccessorySheetDelegate.class);
 
         // Simulate opening a new tab which automatically triggers the registration:
         Tab firstTab = addBrowserTab(mMediator, 1111, null);
         mController.registerSheetDataProvider(mLastMockWebContents, AccessoryTabType.PASSWORDS,
                 firstTabHelper.getSheetDataProvider());
+        mController.registerSheetUpdateDelegate(mLastMockWebContents, firstSheetUpdater);
         getStateForBrowserTab()
                 .getSheetDataProvider(AccessoryTabType.PASSWORDS)
                 .addObserver(firstTabHelper::record);
@@ -391,6 +394,7 @@ public class ManualFillingControllerTest {
         Tab secondTab = addBrowserTab(mMediator, 2222, firstTab);
         mController.registerSheetDataProvider(mLastMockWebContents, AccessoryTabType.PASSWORDS,
                 secondTabHelper.getSheetDataProvider());
+        mController.registerSheetUpdateDelegate(mLastMockWebContents, secondSheetUpdater);
         getStateForBrowserTab()
                 .getSheetDataProvider(AccessoryTabType.PASSWORDS)
                 .addObserver(secondTabHelper::record);
@@ -399,10 +403,16 @@ public class ManualFillingControllerTest {
 
         // Simulate switching back to the first tab:
         switchBrowserTab(mMediator, /*from=*/secondTab, /*to=*/firstTab);
+        // Wiring affects the same sheet only and is triggered after switching
+        verify(firstSheetUpdater).requestSheet(AccessoryTabType.PASSWORDS);
+        firstTabHelper.providePasswordSheet("FirstPassword");
         assertThat(firstTabHelper.getFirstRecordedPassword(), is("FirstPassword"));
 
         // And back to the second:
         switchBrowserTab(mMediator, /*from=*/firstTab, /*to=*/secondTab);
+        // Wiring affects the same sheet only and is triggered after switching
+        verify(secondSheetUpdater).requestSheet(AccessoryTabType.PASSWORDS);
+        secondTabHelper.providePasswordSheet("SecondPassword");
         assertThat(secondTabHelper.getFirstRecordedPassword(), is("SecondPassword"));
     }
 
@@ -504,8 +514,7 @@ public class ManualFillingControllerTest {
         mMediator.getTabObserverForTesting().onHidden(tab, TabHidingType.CHANGED_TABS);
         getStateForBrowserTab().getWebContentsObserverForTesting().wasHidden();
         // The state should be kept if the closure wasn't committed.
-        assertThat(getStateForBrowserTab().getAccessorySheet(AccessoryTabType.PASSWORDS),
-                is(not(nullValue())));
+        assertThat(getStateForBrowserTab().getTabs().length, is(1));
         mLastMockWebContents = null;
 
         // Simulate undo closing the tab and selecting it:
@@ -516,8 +525,8 @@ public class ManualFillingControllerTest {
         WebContents oldWebContents = mLastMockWebContents;
         closeBrowserTab(mMediator, tab);
         // The state should be cleaned up, now that it was committed.
-        assertThat(mCache.getStateFor(oldWebContents).getAccessorySheet(AccessoryTabType.PASSWORDS),
-                is(nullValue()));
+        assertThat(mMediator.getStateCacheForTesting().getStateFor(oldWebContents).getTabs().length,
+                is(0));
 
         ArgumentCaptor<KeyboardAccessoryData.Tab[]> barTabCaptor =
                 ArgumentCaptor.forClass(KeyboardAccessoryData.Tab[].class);
@@ -610,6 +619,7 @@ public class ManualFillingControllerTest {
         reset(mMockAccessorySheet);
         SheetProviderHelper firstTabHelper = new SheetProviderHelper();
         SheetProviderHelper secondTabHelper = new SheetProviderHelper();
+        UpdateAccessorySheetDelegate secondSheetUpdater = mock(UpdateAccessorySheetDelegate.class);
 
         // Simulate opening a new tab:
         Tab firstTab = addBrowserTab(mMediator, 1111, null);
@@ -628,6 +638,7 @@ public class ManualFillingControllerTest {
         Tab secondTab = addBrowserTab(mMediator, 2222, firstTab);
         mController.registerSheetDataProvider(mLastMockWebContents, AccessoryTabType.PASSWORDS,
                 secondTabHelper.getSheetDataProvider());
+        mController.registerSheetUpdateDelegate(mLastMockWebContents, secondSheetUpdater);
         mController.registerActionProvider(
                 mLastMockWebContents, secondTabHelper.getActionListProvider());
         getStateForBrowserTab()
@@ -645,13 +656,15 @@ public class ManualFillingControllerTest {
         mMediator.getTabObserverForTesting().onDestroyed(firstTab);
 
         // The current tab should not be influenced by the destruction...
+        // Wiring affects the same sheet only and is triggered after switching
+        verify(secondSheetUpdater).requestSheet(AccessoryTabType.PASSWORDS);
+        secondTabHelper.providePasswordSheet("SecondPassword");
         assertThat(secondTabHelper.getFirstRecordedPassword(), is("SecondPassword"));
         assertThat(secondTabHelper.getFirstRecordedAction().getCaption(), is("2BKept"));
         assertThat(getStateForBrowserTab(), is(mCache.getStateFor(secondTab)));
         // ... but the other tab's data should be gone.
         assertThat(mCache.getStateFor(firstTab).getActionsProvider(), nullValue());
-        assertThat(mCache.getStateFor(firstTab).getAccessorySheet(AccessoryTabType.PASSWORDS),
-                nullValue());
+        assertThat(mCache.getStateFor(firstTab).getTabs().length, is(0));
     }
 
     @Test
