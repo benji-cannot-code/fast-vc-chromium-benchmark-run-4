25914646b2facdf243d8901baed5268cddf7bf01
Shakti Sahu <shaktisahu@chromium.org>
2021-05-21 07:51:00 +0000

    [segmentation_platform] Added SegmentSelector
    This CL adds segment selection logic, which includes
    - SegmentSelector interface and implementation
    - Save/restore results from prefs
    - Discrete mapping
    - Segment selection TTL
    - Single segment API support
    
    TODO:
    - Read finch configs
    - Finalize when to request model execution
    - Implement SegmentationResultPrefs
    
    Bug: 1207753
    Change-Id: I50cbab059f5bca06cea74436570f8443cec3c1c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891358
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885417}

---
+++ components/segmentation_platform/internal/selection/segmentation_result_prefs.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/segmentation_platform/internal/selection/segmentation_result_prefs.h"
+
+namespace segmentation_platform {
+
+SelectedSegment::SelectedSegment(OptimizationTarget segment_id)
+    : segment_id(segment_id), in_use(false) {}
+
+}  // namespace segmentation_platform
