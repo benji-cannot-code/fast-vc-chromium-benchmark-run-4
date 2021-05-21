b2fd6eeefbff1542e4ea20f4564d815a81e76415
Tsuyoshi Horo <horo@chromium.org>
2021-05-21 06:20:54 +0000

    UseCounter for cookie access in URN document
    Bug: 1211667
    Change-Id: Ibdb5c7674245aed1dea9eefc4102b3599d514a7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910058
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885403}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -5442,6 +5442,9 @@ String Document::cookie(ExceptionState& exception_state) const {
           "Cookies are disabled inside 'data:' URLs.");
     } else {
       exception_state.ThrowSecurityError("Access is denied for this document.");
+      if (Url().ProtocolIs("urn")) {
+        CountUse(WebFeature::kUrnDocumentAccessedCookies);
+      }
     }
     return String();
   } else if (dom_window_->GetSecurityOrigin()->IsLocal()) {
@@ -5467,6 +5470,9 @@ void Document::setCookie(const String& value, ExceptionState& exception_state) {
           "Cookies are disabled inside 'data:' URLs.");
     } else {
       exception_state.ThrowSecurityError("Access is denied for this document.");
+      if (Url().ProtocolIs("urn")) {
+        CountUse(WebFeature::kUrnDocumentAccessedCookies);
+      }
     }
     return;
   } else if (dom_window_->GetSecurityOrigin()->IsLocal()) {
