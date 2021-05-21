2ead4fcbad4079fc661c38bcd9350c7529d6285c
Lei Zhang <thestig@chromium.org>
2021-05-21 07:34:31 +0000

    Swap stl_util.h to cxx17_backports.h in Blink binding generator scripts.
    The generated .cc files only include base/stl_util.h for base::size(),
    which is now in base/cxx17_backports.h.
    
    Bug: 1210983
    Change-Id: I70b8f74df5fa80294c81294521e381eb393d4dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911806
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885408}

--- third_party/blink/renderer/bindings/tests/results/core/nested_union_type.cc
+++ third_party/blink/renderer/bindings/tests/results/core/nested_union_type.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // clang-format off
 #include "third_party/blink/renderer/bindings/tests/results/core/nested_union_type.h"
 
-#include "base/stl_util.h"
+#include "base/cxx17_backports.h"
 #include "third_party/blink/renderer/bindings/core/v8/byte_string_or_node_list.h"
 #include "third_party/blink/renderer/bindings/core/v8/idl_types.h"
 #include "third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h"
