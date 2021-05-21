75712341747d644bd6d7fb2d771b36b5d62d80ef
Sam Chiu <samchiu@google.com>
2021-05-21 01:14:20 +0000

    Log event after the user interacts with a PhoneHub notification by opening app sharing.
    Bug: b/181193705
    Test: Verified the expected histogram sent locally in chrome://histograms
    Change-Id: I4040ea57a9a399384446718721d4f47a0cf7ed2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852857
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Sam Chiu <samchiu@google.com>
    Cr-Commit-Position: refs/heads/master@{#885328}

--- chrome/browser/chromeos/eche_app/eche_app_manager_factory.cc
+++ chrome/browser/chromeos/eche_app/eche_app_manager_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "base/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/device_sync/device_sync_client_factory.h"
@@ -51,6 +52,15 @@ void CloseEcheApp(Profile* profile) {
     return;
   }
 }
+// Enumeration of possible interactions with a PhoneHub notification. Keep in
+// sync with corresponding enum in tools/metrics/histograms/enums.xml. These
+// values are persisted to logs. Entries should not be renumbered and numeric
+// values should never be reused.
+enum class NotificationInteraction {
+  kUnknown = 0,
+  kOpenAppStreaming = 1,
+  kMaxValue = kOpenAppStreaming,
+};
 
 void LaunchEcheApp(Profile* profile, int64_t notification_id) {
   std::string url = "chrome://eche-app/#notification_id=";
@@ -59,6 +69,8 @@ void LaunchEcheApp(Profile* profile, int64_t notification_id) {
   params.url = GURL(url);
   web_app::LaunchSystemWebAppAsync(profile, web_app::SystemAppType::ECHE,
                                    params);
+  base::UmaHistogramEnumeration("Eche.NotificationClicked",
+                                NotificationInteraction::kOpenAppStreaming);
 }
 
 }  // namespace
