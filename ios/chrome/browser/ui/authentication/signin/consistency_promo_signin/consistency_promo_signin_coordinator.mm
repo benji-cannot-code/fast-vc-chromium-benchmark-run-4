cca2f4bfdfbcbbb1c8120bbb89539e4fd7479c4f
jlebel <jlebel@chromium.org>
2021-05-21 10:01:22 +0000

    [iOS][Mice] Sign-in is successful if an identity is set
    If the token is invalid, a sign out happens right after signs in in the
    consistency coordinator.
    We need to check if the user is authenticated and if cookies are set
    when |onAccountsInCookieUpdated:error:| is called.
    
    Fixed: 1211727
    Change-Id: I3ac7e0a73f43a97c4f32f1d876dab66db5239368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910119
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885443}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/mac/foundation_util.h"
 #import "components/signin/public/base/account_consistency_method.h"
+#import "components/signin/public/identity_manager/accounts_in_cookie_jar_info.h"
 #import "components/signin/public/identity_manager/objc/identity_manager_observer_bridge.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/main/browser.h"
@@ -342,7 +343,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
   __weak __typeof(self) weakSelf = self;
-  if (error.state() == GoogleServiceAuthError::State::NONE) {
+  if (error.state() == GoogleServiceAuthError::State::NONE &&
+      self.authenticationService->GetAuthenticatedIdentity() &&
+      accountsInCookieJarInfo.signed_in_accounts.size() > 0) {
     [self.defaultAccountCoordinator stopSigninSpinner];
     [self.navigationController
         dismissViewControllerAnimated:YES
