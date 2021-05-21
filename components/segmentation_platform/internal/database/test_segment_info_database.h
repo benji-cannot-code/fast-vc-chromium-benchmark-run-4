e35abdec7aa053c9b35bb86ab96170a249cc36e8
Shakti Sahu <shaktisahu@chromium.org>
2021-05-21 07:35:38 +0000

    [segmentation_platform] Added ModelExecutionScheduler
    This CL adds the model execution scheduler that
    - can be invoked on startup and from periodic jobs
    - requests execution for models that have expired
    - saves results to the database and notifies observers
    
    More discussion on doc : https://docs.google.com/document/d/1pFFclHMkBJp-156EMs-YtL2IwVutamQV2749wPtU2DQ/edit?hl=en
    
    Bug: 1207753
    Change-Id: Ib766370a6f9037d0ead1dac55e7325fe5bc8135a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886204
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885410}

--- components/segmentation_platform/internal/database/test_segment_info_database.h
+++ components/segmentation_platform/internal/database/test_segment_info_database.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/segmentation_platform/internal/database/segment_info_database.h"
 
-#include "base/logging.h"
-
 namespace segmentation_platform {
 
 namespace test {
@@ -22,15 +20,24 @@ class TestSegmentInfoDatabase : public SegmentInfoDatabase {
 
   // SegmentInfoDatabase overrides.
   void GetAllSegmentInfo(AllSegmentInfoCallback callback) override;
+  void SaveSegmentResult(OptimizationTarget segment_id,
+                         proto::PredictionResult* result,
+                         SuccessCallback callback) override;
 
   // Test helper methods.
   void AddUserAction(OptimizationTarget segment_id,
                      const std::string& user_action);
+  void AddPredictionResult(OptimizationTarget segment_id,
+                           float score,
+                           base::Time timestamp);
+  void AddDiscreteMapping(OptimizationTarget segment_id,
+                          float mappings[][2],
+                          int num_pairs);
 
- private:
   // Finds a segment with given |segment_id|. Creates one if it doesn't exist.
   proto::SegmentInfo* FindOrCreateSegment(OptimizationTarget segment_id);
 
+ private:
   std::vector<std::pair<OptimizationTarget, proto::SegmentInfo>> segment_infos_;
 };
 
