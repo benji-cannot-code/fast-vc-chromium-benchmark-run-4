9dffd2cc941880ebb1e47925748ff2820953ca9f
Lily Chen <chlily@chromium.org>
2021-05-21 01:27:41 +0000

    CookieBrowserTest: Clean up references to default-enabled base::Features
    This removes CookieBrowserTest's usage of a ScopedFeatureList to enable
    two base::Features that are already enabled by default.
    
    Bug: 1211388
    Change-Id: I9726d364e360ca9404b67a9249a155b8a1e3378b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911219
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885339}

--- content/browser/renderer_host/cookie_browsertest.cc
+++ content/browser/renderer_host/cookie_browsertest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/scoped_feature_list.h"
 #include "content/browser/bad_message.h"
 #include "content/browser/renderer_host/frame_tree.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -31,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell.h"
 #include "content/test/content_browser_test_utils_internal.h"
 #include "ipc/ipc_security_test_util.h"
-#include "net/base/features.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_access_result.h"
 #include "net/cookies/cookie_util.h"
@@ -109,10 +107,6 @@ class CookieBrowserTest : public ContentBrowserTest {
   void SetUp() override {
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
-    feature_list_.InitWithFeatures(
-        {net::features::kSameSiteByDefaultCookies,
-         net::features::kCookiesWithoutSameSiteMustBeSecure} /* enabled */,
-        {} /* disabled */);
     ContentBrowserTest::SetUp();
   }
 
@@ -120,9 +114,6 @@ class CookieBrowserTest : public ContentBrowserTest {
     // Support multiple sites on the test server.
     host_resolver()->AddRule("*", "127.0.0.1");
   }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
 };
 
 // Exercises basic cookie operations via javascript, including an http page
@@ -136,7 +127,7 @@ IN_PROC_BROWSER_TEST_F(CookieBrowserTest, Cookies) {
   https_server.SetSSLConfig(net::EmbeddedTestServer::CERT_TEST_NAMES);
   ASSERT_TRUE(https_server.Start());
 
-  // The server sends a HttpOnly cookie. The RenderFrameMessageFilter should
+  // The server sends a HttpOnly cookie. The RestrictedCookieManager should
   // never allow this to be sent to any renderer process.
   GURL https_url =
       https_server.GetURL("a.test", "/set-cookie?notforjs=1;HttpOnly");
