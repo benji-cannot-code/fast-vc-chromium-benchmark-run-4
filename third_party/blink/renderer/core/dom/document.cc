e9fa5db8e726a56ab4f3b32f22c710f9979d1653
Stefan Zager <szager@chromium.org>
2021-05-21 00:27:31 +0000

    Include all style updates in UMA/UKM stats.
    Change-Id: If14777bb34f37fec374a0570b6d591ef9d6279cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906778
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885306}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -184,6 +184,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
+#include "third_party/blink/renderer/core/frame/local_frame_ukm_aggregator.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/page_dismissal_scope.h"
 #include "third_party/blink/renderer/core/frame/performance_monitor.h"
@@ -2072,6 +2073,9 @@ void Document::UpdateStyleAndLayoutTreeForThisDocument() {
     AssertLayoutTreeUpdated(owner->GetDocument());
   }
 #endif
+
+  SCOPED_UMA_AND_UKM_TIMER(View()->EnsureUkmAggregator(),
+                           LocalFrameUkmAggregator::kStyle);
 
   // RecalcSlotAssignments should be done before checking
   // NeedsLayoutTreeUpdate().
