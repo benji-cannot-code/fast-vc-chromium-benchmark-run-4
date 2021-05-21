68fbaf2ac323b27fe5f70df20c3f782b155a16b4
Song Fangzhen <songfangzhen@bytedance.com>
2021-05-21 09:41:58 +0000

    DPWA: Clean up BookmarkApp*
    This change cleans up BookmarkApp* classes which are not used anywhere
    but tests.
    1) BookmarkAppRegistrar
    2) BookmarkAppRegistryController
    3) BookmarkAppIconManager
    4) BookmarkAppInstallFinalizer
    5) BookmarkAppFileHandlerManager
    6) BookmarkAppShortcutManager
    
    Bug: 1065748
    Change-Id: I26034b0c4f76c10dc0098a353adb6ec5226057e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904757
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#885435}

--- chrome/browser/web_applications/components/app_registrar.h
+++ chrome/browser/web_applications/components/app_registrar.h
@@ -27,10 +27,6 @@ struct ShareTarget;
 namespace base {
 class Time;
 }
-// Forward declared to support safe downcast;
-namespace extensions {
-class BookmarkAppRegistrar;
-}
 
 namespace web_app {
 
@@ -160,7 +156,6 @@ class AppRegistrar {
   // Safe downcast.
   virtual WebAppRegistrar* AsWebAppRegistrar() = 0;
   virtual const WebAppRegistrar* AsWebAppRegistrar() const = 0;
-  virtual extensions::BookmarkAppRegistrar* AsBookmarkAppRegistrar();
 
   void SetSubsystems(OsIntegrationManager* os_integration_manager);
 
