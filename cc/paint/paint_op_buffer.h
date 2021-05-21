c2d6a221f39b5eaa9dd0d90648151ec93cbfca07
Mike Reed <reed@google.com>
2021-05-21 22:03:29 +0000

    IWYU -- needed for future skia cl
    See https://skia-review.googlesource.com/c/skia/+/411302
    
    Change-Id: Ia8c546aedcd207fd05dda034e19c394783b09457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910800
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Mike Reed <reed@google.com>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Auto-Submit: Mike Reed <reed@google.com>
    Cr-Commit-Position: refs/heads/master@{#885668}

--- cc/paint/paint_op_buffer.h
+++ cc/paint/paint_op_buffer.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_flags.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRect.h"
 #include "third_party/skia/include/core/SkScalar.h"
 #include "ui/gfx/geometry/rect.h"
