4b8f0a761bfd677712620495267491c737a06534
Alice Wang <aliceywang@chromium.org>
2021-05-21 14:52:46 +0000

    [Signin][Android] Refactor accounts change notification in AccountManagerFacadeImpl
    This CL refactors accounts change notification in
    AccountManagerFacadeImpl by migrating the usage of the blocking method
    tryGetGoogleAccounts() to more straight-forward calls and removing the
    redundant pending task queue execution when accounts update.
    
    Bug: 1211294
    Change-Id: I382e60b6620564af7e5bd2d8f84c48037f26ce91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910198
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885477}

--- components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
@@ -151,8 +151,8 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
     @Override
     public void tryGetGoogleAccounts(Callback<List<Account>> callback) {
         ThreadUtils.assertOnUiThread();
-        if (isCachePopulated()) {
-            ThreadUtils.postOnUiThread(callback.bind(tryGetGoogleAccounts()));
+        if (mFilteredAccounts.get() != null) {
+            ThreadUtils.postOnUiThread(callback.bind(mFilteredAccounts.get()));
         } else {
             mCallbacksWaitingForAccountsFetch.add(callback);
         }
@@ -324,7 +324,7 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
 
     private void incrementUpdateCounter() {
         assert mUpdateTasksCounter >= 0;
-        if (mUpdateTasksCounter++ > 0) return;
+        ++mUpdateTasksCounter;
     }
 
     private void decrementUpdateCounter() {
@@ -333,7 +333,7 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
 
         while (!mCallbacksWaitingForAccountsFetch.isEmpty()) {
             final Callback<List<Account>> callback = mCallbacksWaitingForAccountsFetch.remove();
-            callback.onResult(tryGetGoogleAccounts());
+            callback.onResult(mFilteredAccounts.get());
         }
     }
 
@@ -357,10 +357,6 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
 
         @Override
         protected void onPostExecute(Void v) {
-            while (!mCallbacksWaitingForAccountsFetch.isEmpty()) {
-                final Callback<List<Account>> callback = mCallbacksWaitingForAccountsFetch.remove();
-                callback.onResult(mFilteredAccounts.get());
-            }
             fireOnAccountsChangedNotification();
             decrementUpdateCounter();
         }
