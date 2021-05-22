070f4c7f6fe834a0be76847f70c47ebfa17ca7e8
Nick Diego Yamane <nickdiego@igalia.com>
2021-05-22 00:03:08 +0000

    ozone/wayland: Fix issues when web contents is quickly selected/dragged
    Drag session start may be triggered asynchronously, eg: dragging an
    item within web contents, leading to edge cases where, for example, the
    mouse button triggering the drag is already released when the wayland
    drag session is requested to start, resulting in subtle issues such as
    crbug.com/1209269.
    
    This CL patches drag controller to double-check pointer button state
    before trying to start the drag session and no-op in case it was already
    released. A regression test is also added to avoid future similar
    regressions.
    
    Bug: 1209269
    Test: ozone_unittests --gtest_filter='*WaylandDataDragControllerTest.AsyncNoopStartDrag*'
    Change-Id: I52414765f2bc2dba119ba1fcb31d34f8c45b71d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2897163
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#885714}

--- ui/ozone/platform/wayland/test/wayland_drag_drop_test.cc
+++ ui/ozone/platform/wayland/test/wayland_drag_drop_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/test/wayland_drag_drop_test.h"
 
+#include <wayland-server-protocol.h>
 #include <wayland-util.h>
 
 #include <cstdint>
@@ -13,11 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/ozone/platform/wayland/host/wayland_window.h"
+#include "ui/ozone/platform/wayland/test/mock_pointer.h"
 #include "ui/ozone/platform/wayland/test/mock_surface.h"
 #include "ui/ozone/platform/wayland/test/test_data_device.h"
 #include "ui/ozone/platform/wayland/test/test_data_device_manager.h"
 #include "ui/ozone/platform/wayland/test/test_data_offer.h"
 #include "ui/ozone/platform/wayland/test/test_data_source.h"
+
+using testing::_;
 
 namespace ui {
 
@@ -53,9 +57,43 @@ void WaylandDragDropTest::ReadData(
   data_source_->ReadData(mime_type, std::move(callback));
 }
 
+void WaylandDragDropTest::SendPointerEnter(
+    WaylandWindow* window,
+    MockPlatformWindowDelegate* delegate) {
+  auto* surface = server_.GetObject<wl::MockSurface>(
+      window->root_surface()->GetSurfaceId());
+  wl_pointer_send_enter(pointer_->resource(), NextSerial(), surface->resource(),
+                        0, 0);
+}
+
+void WaylandDragDropTest::SendPointerLeave(
+    WaylandWindow* window,
+    MockPlatformWindowDelegate* delegate) {
+  auto* surface = server_.GetObject<wl::MockSurface>(
+      window->root_surface()->GetSurfaceId());
+  wl_pointer_send_leave(pointer_->resource(), NextSerial(),
+                        surface->resource());
+}
+
+void WaylandDragDropTest::SendPointerButton(
+    WaylandWindow* window,
+    MockPlatformWindowDelegate* delegate,
+    int button,
+    bool pressed) {
+  uint32_t state = pressed ? WL_POINTER_BUTTON_STATE_PRESSED
+                           : WL_POINTER_BUTTON_STATE_RELEASED;
+  wl_pointer_send_button(pointer_->resource(), NextSerial(), NextTime(), button,
+                         state);
+}
+
 void WaylandDragDropTest::SetUp() {
   WaylandTest::SetUp();
+
+  wl_seat_send_capabilities(server_.seat()->resource(),
+                            WL_SEAT_CAPABILITY_POINTER);
   Sync();
+  pointer_ = server_.seat()->pointer();
+  ASSERT_TRUE(pointer_);
 
   data_device_manager_ = server_.data_device_manager();
   ASSERT_TRUE(data_device_manager_);
--- ui/ozone/platform/wayland/test/wayland_drag_drop_test.h
+++ ui/ozone/platform/wayland/test/wayland_drag_drop_test.h
@@ -43,6 +43,15 @@ class WaylandDragDropTest : public WaylandTest,
   void ReadData(const std::string& mime_type,
                 wl::TestDataSource::ReadDataCallback callback);
 
+  virtual void SendPointerEnter(WaylandWindow* window,
+                                MockPlatformWindowDelegate* delegate);
+  virtual void SendPointerLeave(WaylandWindow* window,
+                                MockPlatformWindowDelegate* delegate);
+  virtual void SendPointerButton(WaylandWindow* window,
+                                 MockPlatformWindowDelegate* delegate,
+                                 int button,
+                                 bool pressed);
+
  protected:
   // WaylandTest:
   void SetUp() override;
@@ -59,9 +68,14 @@ class WaylandDragDropTest : public WaylandTest,
   void ScheduleTestTask(base::OnceClosure test_task);
   void RunTestTask(base::OnceClosure test_task);
 
+  WaylandWindowManager* window_manager() const {
+    return connection_->wayland_window_manager();
+  }
+
   // Server objects
   wl::TestDataDeviceManager* data_device_manager_;
   wl::TestDataSource* data_source_;
+  wl::MockPointer* pointer_;
 };
 
 }  // namespace ui
