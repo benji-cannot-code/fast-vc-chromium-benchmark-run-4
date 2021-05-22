b5e34e3df3883dc7e76fa9e0de4badbfb4b81225
Alexander Surkov <asurkov@igalia.com>
2021-05-22 01:27:41 +0000

    Implement BrowserAccessibility::PlatformChildren() method to support range-based for loops
    It allows nicer syntax such as
    for (const auto& child : PlatformChildren() {}
    to iterate platform children.
    
    Also it's needed to implement ui::AXTreeIndexer for the blink tree
    formatter.
    
    Change-Id: I2bee18adc65759dea2199172534aec66cc541765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2907093
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#885730}

--- content/browser/accessibility/browser_accessibility.cc
+++ content/browser/accessibility/browser_accessibility.cc
@@ -2326,11 +2326,8 @@ ui::TextAttributeMap BrowserAccessibility::ComputeTextAttributeMap(
   DCHECK(PlatformChildCount());
 
   int start_offset = 0;
-  for (BrowserAccessibility::PlatformChildIterator it = PlatformChildrenBegin();
-       it != PlatformChildrenEnd(); ++it) {
-    BrowserAccessibility* child = it.get();
-    DCHECK(child);
-    ui::TextAttributeList attributes(child->ComputeTextAttributes());
+  for (const auto& child : PlatformChildren()) {
+    ui::TextAttributeList attributes(child.ComputeTextAttributes());
 
     if (attributes_map.empty()) {
       attributes_map[start_offset] = attributes;
@@ -2348,12 +2345,12 @@ ui::TextAttributeMap BrowserAccessibility::ComputeTextAttributeMap(
       }
     }
 
-    if (child->IsText()) {
+    if (child.IsText()) {
       const ui::TextAttributeMap spelling_attributes =
-          child->GetSpellingAndGrammarAttributes();
+          child.GetSpellingAndGrammarAttributes();
       MergeSpellingAndGrammarIntoTextAttributes(spelling_attributes,
                                                 start_offset, &attributes_map);
-      start_offset += child->GetHypertext().length();
+      start_offset += child.GetHypertext().length();
     } else {
       start_offset += 1;
     }
