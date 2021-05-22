dd580e851e64f03dbdbcf8862c920b268a4363eb
Christian Biesinger <cbiesinger@google.com>
2021-05-22 01:25:30 +0000

    [zoom-for-dsf] Multiply scrollbar inset and border by ScaleFromDIP
    This required passing the scale through ScrollbarExtraParams, similar
    to what some other extra params already do.
    
    It seems the expected behavior is that scrollbars are unaffected by
    zoom; however, they do still need to be multiplied by the DPR.
    
    Bug: 716231
    Change-Id: Idaef5a44814f0dfb4e762bf85feccd51607cca23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904972
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885726}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_mac.mm
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_mac.mm
@@ -259,6 +259,7 @@ WebThemeEngine::ExtraParams GetPaintParams(const Scrollbar& scrollbar,
 
   params.scrollbar_extra.is_hovering =
       scrollbar.HoveredPart() != ScrollbarPart::kNoPart;
+  params.scrollbar_extra.scale_from_dip = scrollbar.ScaleFromDIP();
   return params;
 }
 
