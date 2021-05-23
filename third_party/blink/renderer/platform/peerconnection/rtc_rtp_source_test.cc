fa50007311ad8baab945326db7f33e6726d7464a
Tommi <tommi@chromium.org>
2021-05-23 15:47:58 +0000

    Add missing include for base::TimeTicks.
    Tbr-ing since the build with webrtc is broken and this is blocking
    a roll.
    
    Tbr: kron@chromium.org
    Bug: none
    Change-Id: Ifc46373b177d73b92d856b174ff8935dcb8df306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2912620
    Reviewed-by: Tommi <tommi@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Auto-Submit: Tommi <tommi@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#885831}

--- third_party/blink/renderer/platform/peerconnection/rtc_rtp_source_test.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_rtp_source_test.cc
@@ -1,5 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "third_party/blink/renderer/platform/peerconnection/rtc_rtp_source.h"
+
+#include "base/time/time.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/webrtc/api/rtp_headers.h"
 #include "third_party/webrtc/api/transport/rtp/rtp_source.h"
