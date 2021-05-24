67888cdb92df8412a8d8174a8c1d3ed6bf10cae4
Jimmy Gong <jimmyxgong@google.com>
2021-05-24 20:04:36 +0000

    [navigation_view] Add isActive to onNavigationPageChanged event
    Removes CurrentPage and PreviousPage params to onNavigationPageChanged
    since having isActive is more than enough to determine if the current
    page is the active page. Simplify NotifyEvent, no longer need to
    query into 'detail:' to retrieve the custom event.
    
    rowser_tests --run-manual
        --gtest_filter=AshCommon.MANUAL_NavigationViewPanel
    
    Bug: 1205539
    Change-Id: Ifc2c93fb488451090109f7ded91c4e235ebf4942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911963
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886048}

--- chrome/test/data/webui/chromeos/ash_common/navigation_view_panel_test.js
+++ chrome/test/data/webui/chromeos/ash_common/navigation_view_panel_test.js
@@ -19,10 +19,6 @@ export function navigationViewPanelTestSuite() {
   let eventDetail = {};
   /** @type {number} */
   let numPageChangedCount = 0;
-  /** @type {string} */
-  let previousPage = '';
-  /** @type {string} */
-  let currentPage = '';
 
   setup(() => {
     viewElement =
@@ -37,15 +33,13 @@ export function navigationViewPanelTestSuite() {
     eventCount = 0;
     eventDetail = {};
     numPageChangedCount = 0;
-    previousPage = '';
-    currentPage = '';
   });
 
   /**
    * @param {!Object} e
    */
   function handleEvent(e) {
-    eventDetail = e.detail;
+    eventDetail = e;
     eventCount++;
   }
 
@@ -53,8 +47,6 @@ export function navigationViewPanelTestSuite() {
    * @param {!CustomEvent} e
    */
   function onNavigationPageChanged(e) {
-    previousPage = e.detail.previous;
-    currentPage = e.detail.current;
     numPageChangedCount++;
   }
 
@@ -91,8 +83,6 @@ export function navigationViewPanelTestSuite() {
     // Only one page has implemented "onNavigationPageChanged" by the second
     // navigation click, expect only one client to be notified.
     assertEquals(1, numPageChangedCount);
-    assertEquals(dummyPage1, previousPage);
-    assertEquals(dummyPage2, currentPage);
 
     // Click the first menu item. Expect that dummyPage2 is now hidden and
     // dummyPage1 is not hidden.
@@ -103,8 +93,6 @@ export function navigationViewPanelTestSuite() {
     // Now that both dummy pages have implemented "onNavigationPageChanged",
     // the navigation click will trigger both page's methods.
     assertEquals(3, numPageChangedCount);
-    assertEquals(dummyPage2, previousPage);
-    assertEquals(dummyPage1, currentPage);
   });
 
   test('notifyEvent', async () => {
