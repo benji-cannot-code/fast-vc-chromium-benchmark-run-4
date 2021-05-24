6c23b6e42008183aaeefd8e25fee971841706e97
Yeunjoo Choi <ychoi@igalia.com>
2021-05-24 05:55:22 +0000

    fixup! Pass RemoteMainFrame/Host mojo channels through its creation messages
    The CL fixes the crash reported from https://crbug.com/1212461. Mojo
    channels for RemoteMainFrame/Host don't depend on
    AssociatedInterfaceProvider by the original CL. So, we should execute a
    RemoteMainFrame Mojo method to live instances in |proxy_hosts|.
    
    Bug: 1212461, 1190158
    Change-Id: Ie3f24d0765dc71806499d0339959126d1ecc436b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2912342
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#885881}

--- content/browser/renderer_host/render_frame_host_manager.cc
+++ content/browser/renderer_host/render_frame_host_manager.cc
@@ -3614,6 +3614,8 @@ void RenderFrameHostManager::ExecuteRemoteFramesBroadcastMethod(
       continue;
     if (pair.second->GetSiteInstance() == instance_to_skip)
       continue;
+    if (!pair.second->is_render_frame_proxy_live())
+      continue;
     callback.Run(pair.second.get());
   }
 }
