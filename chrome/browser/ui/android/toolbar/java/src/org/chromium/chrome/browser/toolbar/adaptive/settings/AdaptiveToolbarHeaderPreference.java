343ef01b39ab880eb67c7fb45e204f86a7e2757e
Gang Wu <gangwu@chromium.org>
2021-05-24 23:03:41 +0000

    [AdaptiveButton] Add a header image for the adaptive toolbar setting page
    screenshot:
    https://screenshot.googleplex.com/7g3uyerDojAW2Tx
    https://screenshot.googleplex.com/7HjfnnQPyUGFkNL
    
    Bug:1208520
    
    Change-Id: I85b655d7d8aa8e415ee2ca9f916720b8cf58a930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910449
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886100}

---
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/adaptive/settings/AdaptiveToolbarHeaderPreference.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.toolbar.adaptive.settings;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+
+import org.chromium.chrome.browser.toolbar.R;
+
+/**
+ * Fragment that allows the user to configure toolbar shorcut preferences.
+ */
+public class AdaptiveToolbarHeaderPreference extends Preference {
+    public AdaptiveToolbarHeaderPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // Inflating from XML.
+        setLayoutResource(R.layout.adaptive_toolbar_header_preference);
+    }
+}
