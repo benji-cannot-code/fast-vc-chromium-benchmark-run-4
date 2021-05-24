5030747b8eadb3e026d047d6b306fb9e914f3c17
Yue Zhang <yuezhanggg@chromium.org>
2021-05-24 21:38:58 +0000

    [ChromeCart] Add a feature param for cart heuristics improvement
    This feature param is introduced so that we can guard heuristics
    improvement changes behind it. We need this flag in case we want to
    use FastPath++ to merge back heuristics improvement CLs.
    
    Bug: 1212181
    Change-Id: Id8951ec23a7e7f3e9601cb5d8b006ff6ebc2140b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911360
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886082}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -112,6 +112,8 @@ const char kNtpStatefulTasksModuleDataParam[] =
 const char kNtpChromeCartModuleDataParam[] = "NtpChromeCartModuleDataParam";
 const char kNtpChromeCartModuleAbandonedCartDiscountParam[] =
     "NtpChromeCartModuleAbandonedCartDiscountParam";
+const char kNtpChromeCartModuleHeuristicsImprovementParam[] =
+    "NtpChromeCartModuleHeuristicsImprovementParam";
 const char kNtpDriveModuleDataParam[] = "NtpDriveModuleDataParam";
 const char kNtpDriveModuleManagedUsersOnlyParam[] =
     "NtpDriveModuleManagedUsersOnlyParam";
