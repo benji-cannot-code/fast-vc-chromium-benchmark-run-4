45be87dcd6d2c6acd084dea3c52a12d4c5136ca9
evliu <evliu@google.com>
2021-05-24 19:17:05 +0000

    Keep observing the SodaInstaller after SODA installation if LiveCaptionMultiLanguage is enabled
    This CL updates the CaptionHandler to keep observing the SodaInstaller after SODA is installed if LiveCaptionMultiLanguage is enabled since additional languages might be installed later.
    
    Bug: 1161569
    Change-Id: I0aec03fe7bcb2446f9cd304c531efdc8fdf768c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903852
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#886028}

--- chrome/browser/accessibility/soda_installer_impl.cc
+++ chrome/browser/accessibility/soda_installer_impl.cc
@@ -203,7 +203,10 @@ void SodaInstallerImpl::OnEvent(Events event, const std::string& id) {
 void SodaInstallerImpl::OnSodaBinaryInstalled() {
   soda_binary_installed_ = true;
   if (language_installed_) {
-    component_updater_observation_.Reset();
+    if (!base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+      component_updater_observation_.Reset();
+    }
+
     NotifyOnSodaInstalled();
   }
 }
@@ -214,7 +217,10 @@ void SodaInstallerImpl::OnSodaLanguagePackInstalled(
   NotifyOnSodaLanguagePackInstalled(language_code);
 
   if (soda_binary_installed_) {
-    component_updater_observation_.Reset();
+    if (!base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+      component_updater_observation_.Reset();
+    }
+
     NotifyOnSodaInstalled();
   }
 }
