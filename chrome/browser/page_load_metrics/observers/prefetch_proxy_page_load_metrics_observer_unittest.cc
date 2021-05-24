3042a1758949f9c3fcd28e872ebb14daef390ea5
Peter Kasting <pkasting@chromium.org>
2021-05-24 20:57:37 +0000

    Fix scoping for GURLs/Origins in some test file in chrome/.
    This is a followup to
    https://chromium-review.googlesource.com/c/chromium/src/+/2008048 that
    updates some of the same files to use constants/methods scoped as
    narrowly as possible, to make more clear which pieces of functionality
    are interdependent.  This was discussed on #cxx at the time Michael
    originally landed his changes and seemed the most desirable route
    forward.  To some degree, one could argue this improves compliance with
    https://google.github.io/styleguide/cppguide.html#Local_Variables .
    
    Bug: 1042727
    Change-Id: Ic147baa0506aae3d37444cd259cda0ad0a495709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908118
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886064}

--- chrome/browser/page_load_metrics/observers/prefetch_proxy_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/prefetch_proxy_page_load_metrics_observer_unittest.cc
@@ -20,12 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// TODO(https://crbug.com/1042727): Fix test GURL scoping and remove this getter
-// function.
-GURL TestUrl() {
-  return GURL("https://chromium.org");
-}
-
 page_load_metrics::mojom::ResourceDataUpdatePtr CreateBaseResource(
     bool was_cached,
     bool is_complete) {
@@ -152,7 +146,7 @@ class PrefetchProxyPageLoadMetricsObserverTest
   TestPrefetchProxyPageLoadMetricsObserver* plm_observer_ = nullptr;
   page_load_metrics::mojom::PageLoadTiming timing_;
 
-  GURL navigation_url_ = TestUrl();
+  GURL navigation_url_{"https://chromium.org"};
   bool in_main_frame_ = true;
 
   DISALLOW_COPY_AND_ASSIGN(PrefetchProxyPageLoadMetricsObserverTest);
