a333f25f667ee2b787b3f6f981df5f45482e9830
Luca Hunkeler <hluca@google.com>
2021-05-25 16:07:07 +0000

    [Autofill Assistant] Fix credit card UI test
    Bug: 1102828
    Change-Id: I6df6e354069b21644975374290f3380f45313755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910777
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/master@{#886307}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantGenericUiTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantGenericUiTest.java
@@ -3027,7 +3027,6 @@ public class AutofillAssistantGenericUiTest {
      */
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1102828")
     public void testCreditCardUi() throws Exception {
         // When the toggle button becomes checked, we write the current card to
         // |selected_credit_card|.
@@ -3114,12 +3113,6 @@ public class AutofillAssistantGenericUiTest {
                                                         .addChunk(
                                                                 Chunk.newBuilder().setText("•••• "))
                                                         .addChunk(Chunk.newBuilder().setKey(-4))))
-                                        .addCallbacks(CallbackProto.newBuilder().setCreateNestedUi(
-                                                CreateNestedGenericUiProto.newBuilder()
-                                                        .setGenericUiIdentifier("nested_ui_${i}")
-                                                        .setGenericUi(singleCardUi)
-                                                        .setParentViewIdentifier(
-                                                                "credit_card_container_view")))
                                         .addCallbacks(CallbackProto.newBuilder().setComputeValue(
                                                 ComputeValueProto.newBuilder()
                                                         .setResultModelIdentifier(
@@ -3130,8 +3123,19 @@ public class AutofillAssistantGenericUiTest {
                                                                 createValueReference(
                                                                         "previously_selected_card"),
                                                                 ValueComparisonProto.Mode.EQUAL))))
-
-                                        ))
+                                        // The nested view is created as the last step since
+                                        // creating views can trigger OnValueChanged events for
+                                        // model values. In this case OnValueChanged for
+                                        // credit_card_selected_${i} will trigger in the creation of
+                                        // the radio button, so we need to make sure it has already
+                                        // been updated to the correct value before creating the
+                                        // view.
+                                        .addCallbacks(CallbackProto.newBuilder().setCreateNestedUi(
+                                                CreateNestedGenericUiProto.newBuilder()
+                                                        .setGenericUiIdentifier("nested_ui_${i}")
+                                                        .setGenericUi(singleCardUi)
+                                                        .setParentViewIdentifier(
+                                                                "credit_card_container_view")))))
                         .build());
 
         // Every time |selected_credit_card| changes:
