cc7e4a383e9e540de95ac086f8afbb333f028009
Leonid Baraz <lbaraz@chromium.org>
2021-05-25 23:31:28 +0000

    Enable use of missived daemon by ERP
    BUG=chromium:1174889
    
    Change-Id: I4382f31aa9626e731a693aa988bfe98a727cf2c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2882230
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886519}

--- chromeos/dbus/missive/fake_missive_client.h
+++ chromeos/dbus/missive/fake_missive_client.h
@@ -25,7 +25,7 @@ class FakeMissiveClient : public MissiveClient {
   void Init();
 
  private:
-  void AddRecord(
+  void EnqueueRecord(
       const reporting::Priority priority,
       reporting::Record record,
       base::OnceCallback<void(reporting::Status)> completion_callback) override;
--- components/reporting/storage_selector/storage_selector.cc
+++ components/reporting/storage_selector/storage_selector.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/path_service.h"
 #include "build/build_config.h"
@@ -84,12 +85,14 @@ void StorageSelector::CreateStorageModule(
                  "Missive Client has not returned Storage Module"));
       return;
     }
+    LOG(WARNING) << "Store reporting data by a Missive daemon";
     std::move(cb).Run(missive_module);
     return;
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   // Use Storage in a local file system.
+  LOG(WARNING) << "Store reporting data locally";
   StorageModule::Create(
       StorageOptions()
           .set_directory(local_reporting_path)
