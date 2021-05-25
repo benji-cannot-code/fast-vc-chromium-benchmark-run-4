2de2283588c05f4657543fefc7cdb01357860899
Kristi Park <kristipark@chromium.org>
2021-05-25 22:14:27 +0000

    [SharingHub] Use BUILDFLAG instead of defined for CrOS
    Fixes bug where WML sharing hub opens instead of the sharesheet in some
    environments.
    
    Bug: 1210169
    Change-Id: I9023a31ec741fdff11a1343445904ddd92c2a0cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917800
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886476}

--- chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller.cc
+++ chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller.cc
@@ -29,7 +29,7 @@ namespace sharing_hub {
 
 namespace {
 
-#if defined(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 // Result of the CrOS sharesheet, i.e. whether the user selects a share target
 // after opening the sharesheet.
 // These values are persisted to logs. Entries should not be renumbered and
@@ -87,7 +87,7 @@ void SharingHubBubbleController::HideBubble() {
 }
 
 void SharingHubBubbleController::ShowBubble() {
-#if defined(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   ShowSharesheet();
 #else
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents_);
@@ -149,7 +149,7 @@ void SharingHubBubbleController::OnBubbleClosed() {
   sharing_hub_bubble_view_ = nullptr;
 }
 
-#if defined(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
 void SharingHubBubbleController::ShowSharesheet() {
   if (!base::FeatureList::IsEnabled(features::kSharesheet) ||
       !base::FeatureList::IsEnabled(features::kChromeOSSharingHub)) {
