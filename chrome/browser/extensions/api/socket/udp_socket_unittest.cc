d1720db364ff1ba54fde33539ab9f6b89bb01013
Lukasz Anforowicz <lukasza@chromium.org>
2021-05-25 17:59:55 +0000

    Disabling flaky UDPSocketUnitTest.TestUDPMulticastRecv on Macs.
    Bug: 1210643
    Change-Id: I62c6dc7e14ef7b2ad037c684cf0e5d27444914eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917239
    Owners-Override: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886374}

--- chrome/browser/extensions/api/socket/udp_socket_unittest.cc
+++ chrome/browser/extensions/api/socket/udp_socket_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_service_test_base.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "content/public/browser/storage_partition.h"
@@ -171,7 +172,13 @@ static void OnMulticastReadCompleted(base::OnceClosure quit_run_loop,
   std::move(quit_run_loop).Run();
 }
 
-TEST_F(UDPSocketUnitTest, TestUDPMulticastRecv) {
+// TODO(https://crbug.com/1210643): Test is flaky on Mac.
+#if defined(OS_MAC)
+#define MAYBE_TestUDPMulticastRecv DISABLED_TestUDPMulticastRecv
+#else
+#define MAYBE_TestUDPMulticastRecv TestUDPMulticastRecv
+#endif
+TEST_F(UDPSocketUnitTest, MAYBE_TestUDPMulticastRecv) {
   const int kPort = 9999;
   const char kGroup[] = "237.132.100.17";
   bool packet_received = false;
