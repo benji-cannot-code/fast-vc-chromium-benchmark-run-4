056956f6c31dede79587a3aee36050db93dabc93
Peter Kasting <pkasting@chromium.org>
2021-05-25 16:45:15 +0000

    Fix scoping for GURLs/Origins in some test file in chrome/.
    This is a followup to
    https://chromium-review.googlesource.com/c/chromium/src/+/2008048 that
    updates some of the same files to use constants/methods scoped as
    narrowly as possible, to make more clear which pieces of functionality
    are interdependent.  This was discussed on #cxx at the time Michael
    originally landed his changes and seemed the most desirable route
    forward.  To some degree, one could argue this improves compliance with
    https://google.github.io/styleguide/cppguide.html#Local_Variables .
    
    Bug: 1042727
    Change-Id: I7e09da20bd9bcb636c5ee15bc56c30d5339f77a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2914420
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886327}

--- chrome/browser/prefetch/prefetch_proxy/prefetch_proxy_url_loader_interceptor_unittest.cc
+++ chrome/browser/prefetch/prefetch_proxy/prefetch_proxy_url_loader_interceptor_unittest.cc
@@ -24,12 +24,6 @@ namespace {
 
 const gfx::Size kSize(640, 480);
 
-// TODO(https://crbug.com/1042727): Fix test GURL scoping and remove this getter
-// function.
-GURL TestURL() {
-  return GURL("https://test.com/path");
-}
-
 }  // namespace
 
 // These tests leak mojo objects (like the PrefetchProxyURLLoader) because
@@ -127,10 +121,11 @@ TEST_F(PrefetchProxyURLLoaderInterceptorTest, DISABLE_ASAN(WantIntercept)) {
       std::make_unique<TestPrefetchProxyURLLoaderInterceptor>(
           web_contents()->GetMainFrame()->GetFrameTreeNodeId());
 
-  interceptor->SetHasPrefetchedResponse(TestURL(), true);
+  const GURL kTestUrl("https://test.com/path");
+  interceptor->SetHasPrefetchedResponse(kTestUrl, true);
 
   network::ResourceRequest request;
-  request.url = TestURL();
+  request.url = kTestUrl;
   request.resource_type =
       static_cast<int>(blink::mojom::ResourceType::kMainFrame);
   request.method = "GET";
@@ -151,10 +146,11 @@ TEST_F(PrefetchProxyURLLoaderInterceptorTest,
       std::make_unique<TestPrefetchProxyURLLoaderInterceptor>(
           web_contents()->GetMainFrame()->GetFrameTreeNodeId());
 
-  interceptor->SetHasPrefetchedResponse(TestURL(), false);
+  const GURL kTestUrl("https://test.com/path");
+  interceptor->SetHasPrefetchedResponse(kTestUrl, false);
 
   network::ResourceRequest request;
-  request.url = TestURL();
+  request.url = kTestUrl;
   request.resource_type =
       static_cast<int>(blink::mojom::ResourceType::kMainFrame);
   request.method = "GET";
