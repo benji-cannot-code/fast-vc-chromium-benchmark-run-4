eab386ca65e0e924245f46ca41b5a28e26a64a39
Dominic Mazzoni <dmazzoni@chromium.org>
2021-05-25 15:46:21 +0000

    Port style tests from AccessibilitySnapshotTest to AssistViewStructureTest
    The tests in AccessibilitySnapshotTest are useful, but they're working
    at a level of abstraction that we'd like to remove.
    
    This patch ports all of the style-related tests to
    AssistViewStructureTest, which is more of an end-to-end test for
    testing the ViewStructure code.
    
    Once all of these tests are ported we can get rid of AccessibilitySnapshotNode.
    
    Bug: 1206356
    Change-Id: I15c3f169034858edb761eee3a0f9087d4ae81c00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2913356
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886303}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/TestViewStructure.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/TestViewStructure.java
@@ -27,6 +27,10 @@ public class TestViewStructure extends ViewStructure implements TestViewStructur
     private ArrayList<TestViewStructure> mChildren = new ArrayList<TestViewStructure>();
     private boolean mDone = true;
     private boolean mDumpHtmlTags;
+    private float mTextSize;
+    private int mFgColor;
+    private int mBgColor;
+    private int mStyle;
 
     public TestViewStructure() {}
 
@@ -51,6 +55,26 @@ public class TestViewStructure extends ViewStructure implements TestViewStructur
     @Override
     public String getClassName() {
         return mClassName;
+    }
+
+    @Override
+    public float getTextSize() {
+        return mTextSize;
+    }
+
+    @Override
+    public int getFgColor() {
+        return mFgColor;
+    }
+
+    @Override
+    public int getBgColor() {
+        return mBgColor;
+    }
+
+    @Override
+    public int getStyle() {
+        return mStyle;
     }
 
     private void recursiveDumpToString(StringBuilder builder, int indent, boolean dumpHtmlTags) {
@@ -281,7 +305,12 @@ public class TestViewStructure extends ViewStructure implements TestViewStructur
     public void setText(CharSequence text, int selectionStart, int selectionEnd) {}
 
     @Override
-    public void setTextStyle(float size, int fgColor, int bgColor, int style) {}
+    public void setTextStyle(float size, int fgColor, int bgColor, int style) {
+        mTextSize = size;
+        mFgColor = fgColor;
+        mBgColor = bgColor;
+        mStyle = style;
+    }
 
     @Override
     public void setTextLines(int[] charOffsets, int[] baselines) {}
