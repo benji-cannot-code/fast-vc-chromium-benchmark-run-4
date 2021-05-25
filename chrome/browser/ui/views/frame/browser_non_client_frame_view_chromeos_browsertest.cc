bac957d8864084f458173bd9410618bcfba7a32d
Roman Arora <romanarora@chromium.org>
2021-05-25 22:12:18 +0000

    ChromeOS: Tablet split view mode hit client fix
    Addresses an issue where touch events in the edge are not propagated
    to the Mohnstrudel WebUI Tab Strip container specifically on tablet
    split view mode. This happens because the frame_utils categorizes the
    event as HTTOP while in full screen (non-split view mode) the events
    come as HTCLIENT. In cases where an HTTOP event is dispatched, the
    TopLevelWindowEventHandler will attempt to handle the event and will
    stop it from propagating to the WebUITabStripContainerView's
    DragToOpenHandler.
    
    Bug: 1197211
    Change-Id: Ic47914e86ba2e77b400a95428b1ccb1811d35f00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2854385
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886472}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller_chromeos.h"
 #include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
+#include "chrome/browser/ui/views/frame/webui_tab_strip_container_view.h"
 #include "chrome/browser/ui/views/fullscreen_control/fullscreen_control_host.h"
 #include "chrome/browser/ui/views/location_bar/content_setting_image_view.h"
 #include "chrome/browser/ui/views/location_bar/custom_tab_bar_view.h"
@@ -168,6 +169,17 @@ class TopChromeMdParamTest : public BaseTest,
   ui::TouchUiController::TouchUiScoperForTesting touch_ui_scoper_;
 };
 
+// Template used as a base class for touch-optimized UI test fixtures.
+template <class BaseTest>
+class TopChromeTouchTest : public BaseTest {
+ public:
+  TopChromeTouchTest() : touch_ui_scoper_(true) {}
+  ~TopChromeTouchTest() override = default;
+
+ private:
+  ui::TouchUiController::TouchUiScoperForTesting touch_ui_scoper_;
+};
+
 // Template to be used when a test does not work with the webUI tabstrip.
 template <bool kEnabled, class BaseTest>
 class WebUiTabStripOverrideTest : public BaseTest {
@@ -271,10 +283,14 @@ using views::Widget;
 
 using BrowserNonClientFrameViewChromeOSTest =
     TopChromeMdParamTest<InProcessBrowserTest>;
+using BrowserNonClientFrameViewChromeOSTouchTest =
+    TopChromeTouchTest<InProcessBrowserTest>;
 using BrowserNonClientFrameViewChromeOSTestNoWebUiTabStrip =
     WebUiTabStripOverrideTest<false, BrowserNonClientFrameViewChromeOSTest>;
 using BrowserNonClientFrameViewChromeOSTestWithWebUiTabStrip =
     WebUiTabStripOverrideTest<true, BrowserNonClientFrameViewChromeOSTest>;
+using BrowserNonClientFrameViewChromeOSTouchTestWithWebUiTabStrip =
+    WebUiTabStripOverrideTest<true, BrowserNonClientFrameViewChromeOSTouchTest>;
 
 // This test does not make sense for the webUI tabstrip, since the window layout
 // is different in that case.
@@ -300,6 +316,50 @@ IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewChromeOSTestNoWebUiTabStrip,
   widget->Maximize();
   int expected_value = HTCLIENT;
   EXPECT_EQ(expected_value, frame_view->NonClientHitTest(top_edge));
+}
+
+IN_PROC_BROWSER_TEST_F(
+    BrowserNonClientFrameViewChromeOSTouchTestWithWebUiTabStrip,
+    TabletSplitViewNonClientHitTest) {
+  BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
+  BrowserNonClientFrameViewChromeOS* frame_view = GetFrameViewAsh(browser_view);
+  EXPECT_EQ(0, frame_view->GetBoundsForClientView().y());
+
+  Widget* widget = browser_view->GetWidget();
+  ASSERT_NO_FATAL_FAILURE(
+      ash::ShellTestApi().SetTabletModeEnabledForTest(true));
+  ash::SplitViewTestApi().SnapWindow(widget->GetNativeWindow(),
+                                     ash::SplitViewTestApi::SnapPosition::LEFT);
+
+  // Touch on the top of the window is interpreted as client hit.
+  gfx::Point top_point(widget->GetWindowBoundsInScreen().width() / 2, 0);
+  EXPECT_EQ(HTCLIENT, frame_view->NonClientHitTest(top_point));
+}
+
+IN_PROC_BROWSER_TEST_F(
+    BrowserNonClientFrameViewChromeOSTouchTestWithWebUiTabStrip,
+    TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip) {
+  BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
+  BrowserNonClientFrameViewChromeOS* frame_view = GetFrameViewAsh(browser_view);
+  EXPECT_EQ(0, frame_view->GetBoundsForClientView().y());
+
+  Widget* widget = browser_view->GetWidget();
+  ASSERT_NO_FATAL_FAILURE(
+      ash::ShellTestApi().SetTabletModeEnabledForTest(true));
+  ash::SplitViewTestApi().SnapWindow(widget->GetNativeWindow(),
+                                     ash::SplitViewTestApi::SnapPosition::LEFT);
+
+  // A point above the window.
+  gfx::Point edge_point(widget->GetWindowBoundsInScreen().width() / 2, -1);
+
+  ASSERT_FALSE(browser_view->webui_tab_strip()->GetVisible());
+  aura::Window* window = widget->GetNativeWindow();
+  ui::test::EventGenerator event_generator(window->GetRootWindow());
+  event_generator.SetTouchRadius(10, 5);
+  event_generator.PressTouch(edge_point);
+  event_generator.MoveTouchBy(0, 100);
+  event_generator.ReleaseTouch();
+  ASSERT_TRUE(browser_view->webui_tab_strip()->GetVisible());
 }
 
 // Test that the frame view does not do any painting in non-immersive
