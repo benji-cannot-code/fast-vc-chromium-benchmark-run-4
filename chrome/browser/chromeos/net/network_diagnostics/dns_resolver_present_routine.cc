a39cd3871ffb8e7326d6f3eee86594f63832ebb7
Trent Begin <tbegin@google.com>
2021-05-25 19:15:48 +0000

    network-diagnostics: do not run routine without active network
    This change modifies the DnsResolverPresent routine to return a kNotRun
    verdict if there are no active networks. This better communicates that
    the DNS fields were not actually checked.
    
    Bug: chromium:1212831, chromium:1156802
    Change-Id: I4d63eb0241caadc07b4f223202d0199fd9fc452b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917796
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kartik Hegde <khegde@chromium.org>
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886405}

--- chrome/browser/chromeos/net/network_diagnostics/dns_resolver_present_routine.cc
+++ chrome/browser/chromeos/net/network_diagnostics/dns_resolver_present_routine.cc
@@ -64,7 +64,9 @@ void DnsResolverPresentRoutine::RunRoutine(
 }
 
 void DnsResolverPresentRoutine::AnalyzeResultsAndExecuteCallback() {
-  if (!name_servers_found_) {
+  if (!connected_network_) {
+    set_verdict(mojom::RoutineVerdict::kNotRun);
+  } else if (!name_servers_found_) {
     set_verdict(mojom::RoutineVerdict::kProblem);
     problems_.emplace_back(
         mojom::DnsResolverPresentProblem::kNoNameServersFound);
@@ -139,6 +141,7 @@ void DnsResolverPresentRoutine::OnNetworkStateListReceived(
   if (default_guid.empty()) {
     AnalyzeResultsAndExecuteCallback();
   } else {
+    connected_network_ = true;
     FetchManagedProperties(default_guid);
   }
 }
