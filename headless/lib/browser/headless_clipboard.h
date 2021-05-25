af01a5cdf4d375c7f10b39ad03711b40be336489
Austin Sullivan <asully@chromium.org>
2021-05-25 22:13:08 +0000

    Clipboard: Implement ReadPng for headless clipboard
    Bug: 1201018
    Change-Id: Ia4f932f374259cf495b05a237ca29019f3af732e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2912857
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886473}

--- headless/lib/browser/headless_clipboard.h
+++ headless/lib/browser/headless_clipboard.h
@@ -115,7 +115,7 @@ class HeadlessClipboard : public ui::Clipboard {
     std::map<ui::ClipboardFormatType, std::string> data;
     std::string url_title;
     std::string html_src_url;
-    SkBitmap image;
+    std::vector<uint8_t> png;
     std::vector<ui::FileInfo> filenames;
   };
 
