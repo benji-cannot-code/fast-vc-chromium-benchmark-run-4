6736ffc58c2f66fa40e7a783791645eea39558ff
Alice Wang <aliceywang@chromium.org>
2021-05-25 13:04:04 +0000

    [MinorMode][Android] Suppress sync promo in discover feed for minor account
    This CL suppresses the sync promo in discover feed when the default
    account is minor for signed-out users and signed-in non sync minor
    users.
    
    Bug: 1208395
    Change-Id: Iba39452ae1126dc689f31ab30851e838ddb029be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903083
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886263}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/SigninPromoController.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/SigninPromoController.java
@@ -204,6 +204,11 @@ public class SigninPromoController {
         }
     }
 
+    @AccessPoint
+    int getAccessPoint() {
+        return mAccessPoint;
+    }
+
     /**
      * Called when the signin promo is destroyed.
      */
