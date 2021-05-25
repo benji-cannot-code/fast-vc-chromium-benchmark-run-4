e7170790c5d357cc7f608d7f689c36bc9ea775b4
Etienne Pierre-doray <etiennep@chromium.org>
2021-05-25 16:29:35 +0000

    [Sheriff]: Disable TooltipBrowserTest
    Bug: 1212403
    Change-Id: I580fdec4febf331d836847e83e55778eb8347bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2916854
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Owners-Override: Etienne Pierre-Doray <etiennep@chromium.org>
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886312}

--- chrome/browser/ui/views/tooltip/tooltip_browsertest.cc
+++ chrome/browser/ui/views/tooltip/tooltip_browsertest.cc
@@ -195,8 +195,9 @@ IN_PROC_BROWSER_TEST_F(TooltipBrowserTest,
   helper()->HideAndReset();
 }
 
+// Disabled http://crbug.com/1212403
 IN_PROC_BROWSER_TEST_F(TooltipBrowserTest,
-                       ShowTooltipFromWebContentWithKeyboard) {
+                       DISABLED_ShowTooltipFromWebContentWithKeyboard) {
   if (SkipTestForOldWinVersion())
     return;
 
@@ -213,7 +214,9 @@ IN_PROC_BROWSER_TEST_F(TooltipBrowserTest,
   helper()->HideAndReset();
 }
 
-IN_PROC_BROWSER_TEST_F(TooltipBrowserTest, ShowTooltipFromIFrameWithKeyboard) {
+// Disabled http://crbug.com/1212403
+IN_PROC_BROWSER_TEST_F(TooltipBrowserTest,
+                       DISABLED_ShowTooltipFromIFrameWithKeyboard) {
   if (SkipTestForOldWinVersion())
     return;
 
@@ -255,7 +258,8 @@ IN_PROC_BROWSER_TEST_F(TooltipBrowserTest, ShowTooltipFromIFrameWithKeyboard) {
   helper()->HideAndReset();
 }
 
-IN_PROC_BROWSER_TEST_F(TooltipBrowserTest, HideTooltipOnKeyPress) {
+// Disabled http://crbug.com/1212403
+IN_PROC_BROWSER_TEST_F(TooltipBrowserTest, DISABLED_HideTooltipOnKeyPress) {
   if (SkipTestForOldWinVersion())
     return;
 
