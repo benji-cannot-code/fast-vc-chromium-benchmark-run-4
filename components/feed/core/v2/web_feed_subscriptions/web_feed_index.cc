2da84f4c253dbd8414ca2959577f8c91c08ef364
Ian Wells <iwells@chromium.org>
2021-05-25 23:57:34 +0000

    Update feedv2 protos
    Bug: 1195940
    Change-Id: I36e84c0d9fcae5113978d4dd85742b9c6eb2c5fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917781
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886528}

--- components/feed/core/v2/web_feed_subscriptions/web_feed_index.cc
+++ components/feed/core/v2/web_feed_subscriptions/web_feed_index.cc
@@ -227,8 +227,9 @@ class EntrySetBuilder {
       const std::string* text = nullptr;
       const std::string* regex = nullptr;
       switch (criteria.match_case()) {
-        case feedwire::webfeed::WebFeedMatcher::Criteria::MatchCase::kRegex:
-          regex = &criteria.regex();
+        case feedwire::webfeed::WebFeedMatcher::Criteria::MatchCase::
+            kPartialMatchRegex:
+          regex = &criteria.partial_match_regex();
           if (regex->empty())
             return false;
           break;
@@ -267,8 +268,9 @@ class EntrySetBuilder {
       const std::string* text = nullptr;
       const std::string* regex = nullptr;
       switch (criteria.match_case()) {
-        case feedwire::webfeed::WebFeedMatcher::Criteria::MatchCase::kRegex:
-          regex = &criteria.regex();
+        case feedwire::webfeed::WebFeedMatcher::Criteria::MatchCase::
+            kPartialMatchRegex:
+          regex = &criteria.partial_match_regex();
           break;
         case feedwire::webfeed::WebFeedMatcher::Criteria::MatchCase::kText:
           text = &criteria.text();
