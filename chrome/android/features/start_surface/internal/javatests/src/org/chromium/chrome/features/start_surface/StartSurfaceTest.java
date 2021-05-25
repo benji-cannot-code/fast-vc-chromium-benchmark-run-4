70b744816edabb60d4d54fa9579072da87565d77
Sky Malice <skym@chromium.org>
2021-05-25 20:11:36 +0000

    Mark testShow_SingleAsHomepage_FromResumeShowStart flaky.
    Bug: 1169673
    Change-Id: I2ded688b8845bd7573a27e577bf67b67c456181d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917634
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Owners-Override: Sky Malice <skym@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886437}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -463,6 +463,7 @@ public class StartSurfaceTest {
     @Test
     @MediumTest
     @Feature({"StartSurface"})
+    @FlakyTest(message = "https://crbug.com/1169673")
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single"})
     public void testShow_SingleAsHomepage_FromResumeShowStart() throws Exception {
