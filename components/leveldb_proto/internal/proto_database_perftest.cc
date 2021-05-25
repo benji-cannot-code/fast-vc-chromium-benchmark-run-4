97c13e17ded3bb263991d13ddb08f187bba3f9f2
Keishi Hattori <keishi@chromium.org>
2021-05-25 07:25:20 +0000

    Disable flaky ProtoDB tests on Mac
    These tests are flakily timing out.
    
    Bug: 918874
    Change-Id: Ibaed7b9fd25cb8e222d3756d0fb6a4b5b46cd53a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2915161
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886192}

--- components/leveldb_proto/internal/proto_database_perftest.cc
+++ components/leveldb_proto/internal/proto_database_perftest.cc
@@ -564,8 +564,8 @@ class ProtoDBPerfTest : public testing::Test {
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 };
 
-// Flakily times out on Windows, see http://crbug.com/918874.
-#if defined(OS_WIN)
+// Flakily times out on Windows and Mac, see http://crbug.com/918874.
+#if defined(OS_WIN) || defined(OS_MAC)
 #define MAYBE_InsertMultipleDBsAlternating_Individual_100b \
   DISABLED_InsertMultipleDBsAlternating_Individual_100b
 #else
@@ -579,8 +579,8 @@ TEST_F(ProtoDBPerfTest, MAYBE_InsertMultipleDBsAlternating_Individual_100b) {
                             5);
 }
 
-// Flakily times out on Windows, see http://crbug.com/918874.
-#if defined(OS_WIN)
+// Flakily times out on Windows and Mac, see http://crbug.com/918874.
+#if defined(OS_WIN) || defined(OS_MAC)
 #define MAYBE_InsertMultipleDBsAlternating_Individual_1000b \
   DISABLED_InsertMultipleDBsAlternating_Individual_1000b
 #else
@@ -594,8 +594,8 @@ TEST_F(ProtoDBPerfTest, MAYBE_InsertMultipleDBsAlternating_Individual_1000b) {
                             5);
 }
 
-// Flakily times out on Windows, see http://crbug.com/918874.
-#if defined(OS_WIN)
+// Flakily times out on Windows and Mac, see http://crbug.com/918874.
+#if defined(OS_WIN) || defined(OS_MAC)
 #define MAYBE_InsertSingleDBAlternating_Individual_100b \
   DISABLED_InsertSingleDBAlternating_Individual_100b
 #else
@@ -609,8 +609,8 @@ TEST_F(ProtoDBPerfTest, MAYBE_InsertSingleDBAlternating_Individual_100b) {
                             5);
 }
 
-// Flakily times out on Windows, see http://crbug.com/918874.
-#if defined(OS_WIN)
+// Flakily times out on Windows and Mac, see http://crbug.com/918874.
+#if defined(OS_WIN) || defined(OS_MAC)
 #define MAYBE_InsertSingleDBAlternating_Individual_1000b \
   DISABLED_InsertSingleDBAlternating_Individual_1000b
 #else
@@ -662,22 +662,54 @@ TEST_F(ProtoDBPerfTest, DistributionTestSmall_FewEntries_Multi) {
                                 kFewEntriesDistributionTestParams, false);
 }
 
-TEST_F(ProtoDBPerfTest, DistributionTestSmall_ManyEntries_Single) {
+// Flakily times out on Mac, see http://crbug.com/918874.
+#if defined(OS_MAC)
+#define MAYBE_DistributionTestSmall_ManyEntries_Single \
+  DISABLED_DistributionTestSmall_ManyEntries_Single
+#else
+#define MAYBE_DistributionTestSmall_ManyEntries_Single \
+  DistributionTestSmall_ManyEntries_Single
+#endif
+TEST_F(ProtoDBPerfTest, MAYBE_DistributionTestSmall_ManyEntries_Single) {
   RunDistributionTestAndCleanup("Small_ManyEntries_Single",
                                 kManyEntriesDistributionTestParams, true);
 }
 
-TEST_F(ProtoDBPerfTest, DistributionTestSmall_ManyEntries_Multi) {
+// Flakily times out on Mac, see http://crbug.com/918874.
+#if defined(OS_MAC)
+#define MAYBE_DistributionTestSmall_ManyEntries_Multi \
+  DISABLED_DistributionTestSmall_ManyEntries_Multi
+#else
+#define MAYBE_DistributionTestSmall_ManyEntries_Multi \
+  DistributionTestSmall_ManyEntries_Multi
+#endif
+TEST_F(ProtoDBPerfTest, MAYBE_DistributionTestSmall_ManyEntries_Multi) {
   RunDistributionTestAndCleanup("Small_ManyEntries_Multi",
                                 kManyEntriesDistributionTestParams, false);
 }
 
-TEST_F(ProtoDBPerfTest, DistributionTestSmall_ManyEntries_Batch_Single) {
+// Flakily times out on Mac, see http://crbug.com/918874.
+#if defined(OS_MAC)
+#define MAYBE_DistributionTestSmall_ManyEntries_Batch_Single \
+  DISABLED_DistributionTestSmall_ManyEntries_Batch_Single
+#else
+#define MAYBE_DistributionTestSmall_ManyEntries_Batch_Single \
+  DistributionTestSmall_ManyEntries_Batch_Single
+#endif
+TEST_F(ProtoDBPerfTest, MAYBE_DistributionTestSmall_ManyEntries_Batch_Single) {
   RunDistributionTestAndCleanup("Small_ManyEntries_Batch_Single",
                                 kManyEntriesDistributionTestParams, true);
 }
 
-TEST_F(ProtoDBPerfTest, DistributionTestSmall_ManyEntries_Batch_Multi) {
+// Flakily times out on Mac, see http://crbug.com/918874.
+#if defined(OS_MAC)
+#define MAYBE_DistributionTestSmall_ManyEntries_Batch_Multi \
+  DISABLED_DistributionTestSmall_ManyEntries_Batch_Multi
+#else
+#define MAYBE_DistributionTestSmall_ManyEntries_Batch_Multi \
+  DistributionTestSmall_ManyEntries_Batch_Multi
+#endif
+TEST_F(ProtoDBPerfTest, MAYBE_DistributionTestSmall_ManyEntries_Batch_Multi) {
   RunDistributionTestAndCleanup("Small_ManyEntries_Batch_Multi",
                                 kManyEntriesDistributionTestParams, false);
 }
