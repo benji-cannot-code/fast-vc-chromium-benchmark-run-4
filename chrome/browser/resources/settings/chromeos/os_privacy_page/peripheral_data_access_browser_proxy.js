548766dabd49fbdc911bd5c59bb7a7bcf8f6017a
Garrick Evans <garrick@google.com>
2021-05-25 00:43:00 +0000

    [CrOS Settings] Polymer3 version of Privacy Page
    Upgrade OS privacy page settings before mixing in Chrome Secure DNS
    settings.
    
    BUG=b:180552405
    Bug: 1180979
    
    Change-Id: I40f174fa8bf03edfc9d18423bad2e3e18e02d694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2912251
    Commit-Queue: Garrick Evans <garrick@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886131}

--- chrome/browser/resources/settings/chromeos/os_privacy_page/peripheral_data_access_browser_proxy.js
+++ chrome/browser/resources/settings/chromeos/os_privacy_page/peripheral_data_access_browser_proxy.js
@@ -4,41 +4,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-// #import {sendWithPromise, addSingletonGetter} from 'chrome://resources/js/cr.m.js';
+import { addSingletonGetter,sendWithPromise} from 'chrome://resources/js/cr.m.js';
 // clang-format on
 
 /**
  * @fileoverview Helper browser proxy for peripheral data access client.
  */
 
-cr.define('settings', function() {
   /** @interface */
-  /* #export */ class PeripheralDataAccessBrowserProxy {
-    /**
-     * @return {!Promise<boolean>}
-     * Returns true if the device supports thunderbolt peripherals.
-     */
-    isThunderboltSupported() {}
-  }
+export class PeripheralDataAccessBrowserProxy {
+  /**
+   * @return {!Promise<boolean>}
+   * Returns true if the device supports thunderbolt peripherals.
+   */
+  isThunderboltSupported() {}
+}
 
-  /** @implements {settings.PeripheralDataAccessBrowserProxy} */
-  /* #export */ class PeripheralDataAccessBrowserProxyImpl {
-    /**
-     * @override
-     * @return {!Promise<boolean>}
-     */
-    isThunderboltSupported() {
-      return cr.sendWithPromise('isThunderboltSupported');
-    }
+/** @implements {PeripheralDataAccessBrowserProxy} */
+export class PeripheralDataAccessBrowserProxyImpl {
+  /**
+   * @override
+   * @return {!Promise<boolean>}
+   */
+  isThunderboltSupported() {
+    return sendWithPromise('isThunderboltSupported');
   }
+}
 
   // The singleton instance_ is replaced with a test version of this wrapper
   // during testing.
-  cr.addSingletonGetter(PeripheralDataAccessBrowserProxyImpl);
-
-  // #cr_define_end
-  return {
-    PeripheralDataAccessBrowserProxy: PeripheralDataAccessBrowserProxy,
-    PeripheralDataAccessBrowserProxyImpl: PeripheralDataAccessBrowserProxyImpl,
-  };
-});
+addSingletonGetter(PeripheralDataAccessBrowserProxyImpl);
