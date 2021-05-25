09fd99c5d2c71494bdf4a99a566ae74d152c8167
Colin Blundell <blundell@chromium.org>
2021-05-25 09:58:41 +0000

    [gn check] Enable //chrome/test:browser_tests
    This involves the following:
    
    - Eliminating many unneeded includes that cause unnecessary "gn check"
      problems
    - Adding "nogncheck" to some conditionally-included headers in
      //content. This was a pre-existing issue (the relevant targets are
      conditionally depended on in GN, which doesn't take into account
      conditionality when analyzing includes); it's unclear why GN check
      started complaining about it with the changes in this CL, but the fix
      is correct in any case given the problem.
    - Allow //chrome/test:browser_tests to depend on //pdf:pdf_internal.
      This is due to a real dependence of
      //chrome/browser/chrome_find_request_manager_browsertest.cc on
      //pdf/document_loader_impl.h for a constant defined in that header
      file.
    
    Bug: 1158929
    Change-Id: I87a082a1312306b918cfb730695cd3fada617ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905653
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886224}

--- chrome/browser/ui/browser_tabrestore_browsertest.cc
+++ chrome/browser/ui/browser_tabrestore_browsertest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/recent_tabs_sub_menu_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
-#include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/sessions/core/tab_restore_service.h"
 #include "content/public/browser/navigation_controller.h"
