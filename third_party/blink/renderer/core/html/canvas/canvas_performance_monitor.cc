3c028600c600c2f178f85977ae6bd81c3e63c754
Justin Novosad <junov@chromium.org>
2021-05-25 22:54:21 +0000

    Fix crash in CanvasPerformancMonitor::CurrentTaskDrawsToContext
    Bug: 1211852
    Change-Id: I324fe0f9b258d2c95f67bc476394741f61bf8684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917314
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886501}

--- third_party/blink/renderer/core/html/canvas/canvas_performance_monitor.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_performance_monitor.cc
@@ -170,7 +170,8 @@ void CanvasPerformanceMonitor::CurrentTaskDrawsToContext(
     // canvases per render task.
     measure_current_task_ = !(task_counter_++ % kSamplingProbabilityInv);
 
-    if (context->Host()
+    if (context->Host() && context->Host()->GetTopExecutionContext() &&
+        context->Host()
             ->GetTopExecutionContext()
             ->IsInRequestAnimationFrame()) {
       call_type_ = CallType::kAnimation;
