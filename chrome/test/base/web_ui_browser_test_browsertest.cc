6e4117bedcf0b75a3bdadf500caf74226e353708
Ben Reich <benreich@chromium.org>
2021-05-25 04:29:28 +0000

    [js-code-coverage] Add browser test for WebUI JS coverage
    Adds a smoke test to ensure the CollectCoverage method properly
    initializes the required folders and emits coverage when a JS test is
    executed.
    
    Moved the construction of the WebUICoverageObserver to the
    SetUpOnMainThread call as that occurs on the BrowserThread whereas the
    SetUpCommandLine call does not. When the WebUICoverageObserver adds
    itself as an Observer, the downstream call at
    content::SharedWorkerDevToolsManager::GetInstance() has a check to
    ensure it's being ran on the BrowserThread::UI.
    
    Bug: 1206082
    Test: browser_tests --gtest_filter=WebUICoverageTest*
    Change-Id: I622d46e98a30c4dacd3e918bfc4783120c70f8ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2896113
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886174}

--- chrome/test/base/web_ui_browser_test_browsertest.cc
+++ chrome/test/base/web_ui_browser_test_browsertest.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
+#include "base/files/file_util.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/test/base/test_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/base/web_ui_browser_test.h"
 #include "content/public/browser/web_ui.h"
@@ -273,4 +275,39 @@ IN_PROC_BROWSER_TEST_F(WebUIBrowserAsyncTest, TestTestDoneEarlyPassesAsync) {
 // waiting for async result.
 IN_PROC_BROWSER_TEST_F(WebUIBrowserAsyncTest, TestTestDoneEarlyPasses) {
   ASSERT_TRUE(RunJavascriptTest("testDone"));
+}
+
+class WebUICoverageTest : public WebUIBrowserTest {
+ protected:
+  base::ScopedTempDir tmp_dir_;
+
+ private:
+  void SetUpOnMainThread() override {
+    WebUIBrowserTest::SetUpOnMainThread();
+    AddLibrary(base::FilePath(FILE_PATH_LITERAL("async.js")));
+    ui_test_utils::NavigateToURL(browser(), DummyUrl());
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    CHECK(tmp_dir_.CreateUniqueTempDir());
+    command_line->AppendSwitchPath(switches::kDevtoolsCodeCoverage,
+                                   tmp_dir_.GetPath());
+    WebUIBrowserTest::SetUpCommandLine(command_line);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(WebUICoverageTest, TestCoverageEmits) {
+  base::ScopedAllowBlockingForTesting allow_blocking;
+  ASSERT_TRUE(base::IsDirectoryEmpty(tmp_dir_.GetPath()));
+  ASSERT_TRUE(RunJavascriptTest("testDone"));
+
+  CollectCoverage("foo");
+  ASSERT_FALSE(base::IsDirectoryEmpty(tmp_dir_.GetPath()));
+
+  // Scripts and tests are special directories under the WebUI specific
+  // directory, ensure they have been created and are not empty.
+  base::FilePath coverage_dir =
+      tmp_dir_.GetPath().AppendASCII("webui_javascript_code_coverage");
+  ASSERT_FALSE(base::IsDirectoryEmpty(coverage_dir.AppendASCII("scripts")));
+  ASSERT_FALSE(base::IsDirectoryEmpty(coverage_dir.AppendASCII("tests")));
 }
