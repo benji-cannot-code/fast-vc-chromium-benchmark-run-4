1308ec34f31c54d6a7de392ac68333d27183b965
Sami Kyostila <skyostil@chromium.org>
2021-05-25 20:26:52 +0000

    base/task/sequence_manager: Reduce canceled task sweep allocation overhead
    To reduce the per-task allocation overhead, use a stack container for
    holding swept cancelled tasks instead of always falling back to the
    heap.
    
    Bug: 1190643,1212886
    Change-Id: I5871a25e52805316a9deea76d52c381175c49b1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917000
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886445}

--- base/task/sequence_manager/task_queue_impl.cc
+++ base/task/sequence_manager/task_queue_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/containers/stack_container.h"
 #include "base/logging.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/stringprintf.h"
@@ -1392,15 +1393,16 @@ size_t TaskQueueImpl::DelayedIncomingQueue::PQueue::SweepCancelledTasks(
   // move all the cancelled tasks into a temporary container before deleting
   // them. This is to avoid |c| from changing while c.erase() is running.
   auto delete_start = std::stable_partition(c.begin(), c.end(), keep_task);
-  std::vector<Task> tasks_to_delete;
-  std::move(delete_start, c.end(), std::back_inserter(tasks_to_delete));
+  StackVector<Task, 8> tasks_to_delete;
+  std::move(delete_start, c.end(),
+            std::back_inserter(tasks_to_delete.container()));
   c.erase(delete_start, c.end());
 
   // stable_partition ensures order was not changed if there was nothing to
   // delete.
-  if (!tasks_to_delete.empty()) {
+  if (!tasks_to_delete->empty()) {
     ranges::make_heap(c, comp);
-    tasks_to_delete.clear();
+    tasks_to_delete->clear();
   }
   return num_high_res_tasks_swept;
 }
