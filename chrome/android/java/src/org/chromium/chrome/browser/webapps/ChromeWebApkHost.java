ee2cac12eba4a9d18dba0c0fce6cf17839ad3422
Peter E Conn <peconn@chromium.org>
2021-05-26 06:46:13 +0000

    ☔ Allow --skip-webapk-verification to work on production Chrome builds.
    file I'm touching. I'd like to keep this CL small so it can be merged.
    
    Change-Id: Idb50f93ec18359e91ecb9ef27b4fdaebe5062b1f
    Bug: 1212961
    Binary-Size: A symbol using ForTest already exists in the code in the
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2915716
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886621}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/ChromeWebApkHost.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/ChromeWebApkHost.java
@@ -13,7 +13,6 @@ import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.task.PostTask;
-import org.chromium.chrome.browser.version.ChromeVersionInfo;
 import org.chromium.components.webapk.lib.client.ChromeWebApkHostSignature;
 import org.chromium.components.webapk.lib.client.WebApkValidator;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
@@ -31,8 +30,7 @@ public class ChromeWebApkHost {
     public static void init() {
         WebApkValidator.init(ChromeWebApkHostSignature.EXPECTED_SIGNATURE,
                 ChromeWebApkHostSignature.PUBLIC_KEY);
-        if (ChromeVersionInfo.isLocalBuild()
-                && CommandLine.getInstance().hasSwitch(SKIP_WEBAPK_VERIFICATION)) {
+        if (CommandLine.getInstance().hasSwitch(SKIP_WEBAPK_VERIFICATION)) {
             // Tell the WebApkValidator to work for all WebAPKs.
             WebApkValidator.setDisableValidationForTesting(true);
         }
