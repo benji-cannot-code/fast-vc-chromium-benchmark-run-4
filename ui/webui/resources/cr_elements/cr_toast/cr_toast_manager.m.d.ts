37c7d247548928d0af46020a6dc5ccfe35c55814
dpapad <dpapad@chromium.org>
2021-05-26 09:14:26 +0000

    Typescript: Add definition files for more ui/webui/resources/.
     - Checking in .d.ts file for cr-toast-manager
     - Auto-generate .d.ts files during build time for
       cr_elements/action_link_css.m.js
       cr_elements/cr_page_host_style_css.js
       js/action_link.js
    
    Also, switch PromiseResolver from auto-generating to checked-in, as the
    auto-generated .d.ts was not sufficient, due to the fact that
    PromiseResolver is defined using an assignment
    "var PromiserResolver = class {...}"
    which eventually resulted in the following error thrown by TSC.
    
    "error TS2749: 'PromiseResolver' refers to a value, but is being used
    as a type here. Did you mean 'typeof PromiseResolver'?"
    
    This is in preparation of migrating Downloads to TypeScript.
    
    Bug: 1189595
    Change-Id: I529f1201af9e322963a9df48c5e61a2914f5da8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909919
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886651}

---
+++ ui/webui/resources/cr_elements/cr_toast/cr_toast_manager.m.d.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {LegacyElementMixin} from 'chrome://resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
+
+interface CrToastManagerElement extends LegacyElementMixin, HTMLElement {
+  duration: number;
+  isToastOpen: boolean;
+  slottedHidden: boolean;
+  show(label: string, hideSlotted?: boolean): void;
+  showForStringPieces(
+      pieces: Array<{value: string, collapsible: boolean}>,
+      hideSlotted?: boolean): void;
+  hide(): void;
+}
+
+export {CrToastManagerElement};
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'cr-toast-manager': CrToastManagerElement;
+  }
+}
+
+export function getToastManager(): CrToastManagerElement;
