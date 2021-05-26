fd5e8ee50cae4a181222479db582538650c75ab6
Lei Zhang <thestig@chromium.org>
2021-05-26 01:33:27 +0000

    [unseasoned-pdf] Move Pepper Linux font code to pdf/ppapi_migration/.
    Move Pepper-specific Linux font code from pdf/pdfium/ to
    pdf/ppapi_migration/. In pdf/pdfium/, start using
    blink::WebFontDescription in place of pp::BrowserFontDescription. In
    pdf/ppapi_migration/, translate blink::WebFontDescription to
    pp::BrowserFontDescription.
    
    Bug: 1139079
    Change-Id: Ie5e8fa1bb1e06c73ed0ee28f848ead90ec7d0523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853426
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886555}

--- pdf/pdfium/pdfium_font_linux.h
+++ pdf/pdfium/pdfium_font_linux.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_pdf {
 
-// Initializes a Linux-specific font mapper that proxies font requests via
-// PPAPI. This is necessary because font loading does not work in the sandbox on
-// Linux.
+// Initializes a Linux-specific font mapper.
+// This is necessary because font loading does not work in the sandbox on Linux.
+// Depending on how this process is initialized, it either:
+// a) sends font requests to Blink.
+// b) proxies font requests via PPAPI.
 void InitializeLinuxFontMapper();
 
 }  // namespace chrome_pdf
