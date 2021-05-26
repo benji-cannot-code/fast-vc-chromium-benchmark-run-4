15b857428a92a9c36f7dac211b91d237b38b7ab9
Antonio Sartori <antoniosartori@google.com>
2021-05-26 16:39:37 +0000

    [wpt] Add test for referrer policy of srcdoc
    This CL adds an assertion checking that modifying referrer policy of a
    document has no effect on a srcdoc child iframe which was created
    before.
    
    Bug: 1130587
    Change-Id: I2a5b08e7a00e59687c28e8f40e97960d3cacf6b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919810
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886772}

--- third_party/blink/web_tests/external/wpt/referrer-policy/generic/inheritance/resources/make-html-script.js
+++ third_party/blink/web_tests/external/wpt/referrer-policy/generic/inheritance/resources/make-html-script.js
@@ -2,13 +2,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function createScriptString(origin, referrer) {
   let request_init = referrer ? `{referrer: "${referrer}"}` : "";
   return `<script>
-            fetch("${origin}/common/security-features/subresource/xhr.py",
-                  ${request_init})
-              .then(r => r.json())
-              .then(j => {
-                top.postMessage({referrer: j.headers.referer}, "*")
-              }).catch(e => {
-                top.postMessage({referrer: "FAILURE"}, "*");
-              });
+            function checkReferrer() {
+              fetch("${origin}/common/security-features/subresource/xhr.py",
+                    ${request_init})
+                .then(r => r.json())
+                .then(j => {
+                  top.postMessage({referrer: j.headers.referer}, "*")
+                }).catch(e => {
+                  top.postMessage({referrer: "FAILURE"}, "*");
+                });
+            }
+            checkReferrer();
+            window.addEventListener("message", msg => {
+              if (msg.data === "checkReferrer") checkReferrer();
+            });
           <\/script>`;
 }
