e64499c89e6ec1b99a612c3a39681ef9f8627866
Mason Freed <masonf@chromium.org>
2021-05-26 13:25:53 +0000

    Optimize EditingHost() and Editable()
    These two functions were
     a) not very optimal. For example, IsEditable called IsEditingHost,
        and repeated most of the work.
     b) never called separately. All callers effectively checked if
        IsEditable() || IsEditingHost().
    
    This CL combines them and optimizes (a bit) the sequence.
    
    Along the way, the contentEditable() accessor was previously being
    used as a pseudo-enum, but with string values, which is bug-prone. So
    this CL makes an enum version that can be used for all but the IDL
    accessor.
    
    It is clear that these functions impact perf, as the [1] CL made a
    small change to the callers of these functions and caused a number
    [2] of perf regressions.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2902079
    [2] crbug.com/1211845
    
    Bug: 1211845
    Change-Id: I6fc2033981bbf2f6f2a8b06c39d344bd4ac86fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2918473
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886701}

--- third_party/blink/renderer/core/html/html_element.h
+++ third_party/blink/renderer/core/html/html_element.h
@@ -49,6 +49,13 @@ enum TranslateAttributeMode {
   kTranslateAttributeInherit
 };
 
+enum class ContentEditableType {
+  kInherit,
+  kContentEditable,
+  kNotContentEditable,
+  kPlaintextOnly,
+};
+
 class CORE_EXPORT HTMLElement : public Element {
   DEFINE_WRAPPERTYPEINFO();
 
@@ -73,6 +80,7 @@ class CORE_EXPORT HTMLElement : public Element {
 
   virtual bool HasCustomFocusLogic() const;
 
+  ContentEditableType contentEditableNormalized() const;
   String contentEditable() const;
   void setContentEditable(const String&, ExceptionState&);
   // For HTMLElement.prototype.isContentEditable. This matches to neither
