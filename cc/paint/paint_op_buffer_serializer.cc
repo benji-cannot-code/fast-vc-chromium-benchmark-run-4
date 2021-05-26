5eec86963205935da03f57551445e7f78dd0c215
Michael Ludwig <michaelludwig@google.com>
2021-05-26 20:36:23 +0000

    Apply ScalingBehavior to RecordPaintFilters
    OOP-R needs convert RecordPaintFilters to a fixed-scale during
    serialization, for the same reasons that it converts PaintRecord-based
    PaintShaders to fixed scale. This ensures that any text contained in
    the nested PaintRecord is rasterized to an SkImage (via MakeFromPicture)
    at the scale factor chosen during serialization instead of being
    dependent on Skia's internal choices for an SkPicture drawn directly.
    
    This extracts the scale factor code from PaintShader and puts it into
    PaintOpBuffer; both RecordPaintFilter and PaintShader use it.
    Serializing a paint filter no longer needs a post analysis matrix,
    since that is explicitly baked into the post scale now.
    
    PaintShader converts a PaintRecord to an SkPicture, and then
    uses SkPicture::makeShader() for raster-at-scale, it creates an
    SkImage via SkImage::MakeFromPicture and then gets a shader using
    SkImage::makeShader() for fixed-scale. Now, RecordPaintFilter
    similarly creates a filter using SkImageFilters::Picture from
    an SkPicture for raster-at-scale, or SkImageFilters::Image with
    an image created via SkImage::MakeFromPicture for fixed-scale.
    
    PaintShader has to undo the embedded scale factor using a
    local matrix. RecordPaintFilter is able to do this by adjusting
    the src to dst rectangle mapping.
    
    Bug: 1187246
    Change-Id: Idb8f21314c78604368a1a4ed973dba6331809b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906212
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886888}

--- cc/paint/paint_op_buffer_serializer.cc
+++ cc/paint/paint_op_buffer_serializer.cc
@@ -77,11 +77,9 @@ void PaintOpBufferSerializer::Serialize(const PaintOpBuffer* buffer) {
   SerializeBuffer(canvas.get(), buffer, nullptr);
 }
 
-void PaintOpBufferSerializer::Serialize(
-    const PaintOpBuffer* buffer,
-    const gfx::Rect& playback_rect,
-    const gfx::SizeF& post_scale,
-    const SkMatrix& post_matrix_for_analysis) {
+void PaintOpBufferSerializer::Serialize(const PaintOpBuffer* buffer,
+                                        const gfx::Rect& playback_rect,
+                                        const gfx::SizeF& post_scale) {
   std::unique_ptr<SkCanvas> canvas = MakeAnalysisCanvas(options_);
 
   PlaybackParams params = MakeParams(canvas.get());
@@ -98,7 +96,6 @@ void PaintOpBufferSerializer::Serialize(
     SerializeOp(canvas.get(), &scale_op, nullptr, params);
   }
 
-  canvas->concat(post_matrix_for_analysis);
   SerializeBuffer(canvas.get(), buffer, nullptr);
 }
 
