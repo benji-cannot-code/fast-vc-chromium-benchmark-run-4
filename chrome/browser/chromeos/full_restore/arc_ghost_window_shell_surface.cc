2f43eec79aef7b624613c6304234eeadb9e4c05c
Shengsong Tan <sstan@chromium.org>
2021-05-26 08:31:42 +0000

    full_restore: Set app_id on ARC ghost window
    This CL add app_id field in ARC ghost window shell surface. Also adjust
    the placeholder surface commit time.
    
    BUG=b:177405617
    
    Change-Id: I3ead74e1a0f819e6aa28efa2ba8ff034daf7e4ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2916636
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886644}

--- chrome/browser/chromeos/full_restore/arc_ghost_window_shell_surface.cc
+++ chrome/browser/chromeos/full_restore/arc_ghost_window_shell_surface.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/full_restore/arc_ghost_window_shell_surface.h"
 
+#include "ash/public/cpp/window_properties.h"
 #include "ash/wm/desks/desks_util.h"
 #include "chrome/browser/chromeos/full_restore/arc_ghost_window_delegate.h"
 #include "chrome/browser/chromeos/full_restore/arc_ghost_window_view.h"
@@ -55,7 +56,7 @@ std::unique_ptr<exo::ClientControlledShellSurface> InitArcGhostWindow(
       shell_surface.get(), window_handler, window_id, display_id, bounds));
   shell_surface->set_close_callback(std::move(close_callback));
 
-  shell_surface->SetApplicationId(app_id.c_str());
+  shell_surface->SetAppId(app_id.c_str());
   shell_surface->SetBounds(display_id, bounds);
 
   if (maximum_size.has_value())
@@ -74,10 +75,10 @@ std::unique_ptr<exo::ClientControlledShellSurface> InitArcGhostWindow(
   shell_surface->SetFrameButtons(kAllButtonMask, kAllButtonMask);
   shell_surface->OnSetFrameColors(theme_color, theme_color);
 
+  shell_surface->controller_surface()->Commit();
   shell_surface->InitContentOverlay(app_id, theme_color);
 
   // Relayout overlay.
-  shell_surface->controller_surface()->Commit();
   shell_surface->GetWidget()->LayoutRootViewIfNecessary();
 
   return shell_surface;
@@ -101,7 +102,6 @@ ArcGhostWindowShellSurface::ArcGhostWindowShellSurface(
                                   gpu::kNullSurfaceHandle, nullptr));
   controller_surface_->Attach(buffer_.get());
   controller_surface_->SetFrame(exo::SurfaceFrameType::NORMAL);
-  controller_surface_->Commit();
   SetScale(scale_factor);
   CommitPendingScale();
 }
@@ -115,6 +115,12 @@ exo::Surface* ArcGhostWindowShellSurface::controller_surface() {
   return controller_surface_.get();
 }
 
+void ArcGhostWindowShellSurface::OverrideInitParams(
+    views::Widget::InitParams* params) {
+  ClientControlledShellSurface::OverrideInitParams(params);
+  SetShellAppId(&params->init_properties_container, app_id_);
+}
+
 void ArcGhostWindowShellSurface::InitContentOverlay(const std::string& app_id,
                                                     uint32_t theme_color) {
   exo::ShellSurfaceBase::OverlayParams overlay_params(
@@ -122,6 +128,23 @@ void ArcGhostWindowShellSurface::InitContentOverlay(const std::string& app_id,
   overlay_params.translucent = true;
   overlay_params.overlaps_frame = false;
   AddOverlay(std::move(overlay_params));
+}
+
+void ArcGhostWindowShellSurface::SetAppId(
+    const absl::optional<std::string>& id) {
+  app_id_ = id;
+  if (GetWidget() && GetWidget()->GetNativeWindow()) {
+    SetShellAppId(GetWidget()->GetNativeWindow(), app_id_);
+  }
+}
+
+void ArcGhostWindowShellSurface::SetShellAppId(
+    ui::PropertyHandler* property_handler,
+    const absl::optional<std::string>& id) {
+  if (id)
+    property_handler->SetProperty(ash::kAppIDKey, *id);
+  else
+    property_handler->ClearProperty(ash::kAppIDKey);
 }
 
 }  // namespace full_restore
