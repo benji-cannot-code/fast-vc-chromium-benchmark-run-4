931efa9c7ba65f0c9049d09241792f47bbe7c353
Ian Kilpatrick <ikilpatrick@chromium.org>
2021-05-26 17:23:40 +0000

    [GridNG] Add concept of "explicit"/"implicit" stretching.
    Previously on the constraint-space we had a bit which indicates of we
    should "stretch" of "shrink-to-fit" if the size in the particular
    direction is auto.
    
    This doesn't quite capture all the constraints however. For example:
    
    <div style="display: grid;">
      <div style="aspect-ratio: 1/1; height: 100px;"></div>
    </div>
    
    <div style="display: grid;">
      <div style="aspect-ratio: 1/1; justify-self: stretch; height: 100px;"></div>
    </div>
    
    In the above example both of the inline sizes are "stretched" however
    one is "implicit" (or weak - the default), and one is "explicit" (or
    strong).
    
    Various layout modes have different concepts here. Most of the time it
    is implicit, however flex layout defaults to "explicit"/"strong".
    
    This fixes the various grid aspect-ratio tests that was failing w/
    GridNG enabled, and creates two additional passes:
     - grid-aspect-ratio-018.html
     - grid-aspect-ratio-019.html
    
    Bug: 1045599
    Change-Id: Ifb8c4e010e85f53c020545ce077447d1f4312252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911763
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886786}

--- third_party/blink/renderer/core/layout/ng/ng_base_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/ng/ng_base_layout_algorithm_test.cc
@@ -127,7 +127,9 @@ NGConstraintSpace ConstructBlockLayoutTestConstraintSpace(
                                    is_new_formatting_context);
   builder.SetAvailableSize(size);
   builder.SetPercentageResolutionSize(size);
-  builder.SetStretchInlineSizeIfAuto(stretch_inline_size_if_auto);
+  builder.SetInlineAutoBehavior(stretch_inline_size_if_auto
+                                    ? NGAutoBehavior::kStretchImplicit
+                                    : NGAutoBehavior::kFitContent);
   builder.SetFragmentainerBlockSize(fragmentainer_space_available);
   builder.SetFragmentationType(block_fragmentation);
   return builder.ToConstraintSpace();
--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
@@ -1723,7 +1723,7 @@ NGLayoutResult::EStatus NGBlockLayoutAlgorithm::FinishInflow(
       return NGLayoutResult::kBfcBlockOffsetResolved;
   }
 
-  // We have special behaviour for a self-collapsing child which gets pushed
+  // We have special behavior for a self-collapsing child which gets pushed
   // down due to clearance, see comment inside |ComputeInflowPosition|.
   bool self_collapsing_child_had_clearance =
       is_self_collapsing && has_clearance_past_adjoining_floats;
@@ -2045,7 +2045,7 @@ NGPreviousInflowPosition NGBlockLayoutAlgorithm::ComputeInflowPosition(
 
   bool is_self_collapsing = layout_result.IsSelfCollapsing();
   if (is_self_collapsing) {
-    // The default behaviour for self-collapsing children is they just pass
+    // The default behavior for self-collapsing children is they just pass
     // through the previous inflow position.
     logical_block_offset = previous_inflow_position.logical_block_offset;
 
@@ -2552,7 +2552,7 @@ NGBoxStrut NGBlockLayoutAlgorithm::CalculateMargins(
                                      /* is_new_fc */ false);
     builder.SetAvailableSize(ChildAvailableSize());
     builder.SetPercentageResolutionSize(child_percentage_size_);
-    builder.SetStretchInlineSizeIfAuto(true);
+    builder.SetInlineAutoBehavior(NGAutoBehavior::kStretchImplicit);
     NGConstraintSpace space = builder.ToConstraintSpace();
 
     const auto block_child = To<NGBlockNode>(child);
@@ -2591,7 +2591,7 @@ NGConstraintSpace NGBlockLayoutAlgorithm::CreateConstraintSpaceForChild(
                             child_writing_direction.GetWritingMode())) {
     if (!child.GetLayoutBox()->AutoWidthShouldFitContent() &&
         !child.IsReplaced() && !child.IsTable())
-      builder.SetStretchInlineSizeIfAuto(true);
+      builder.SetInlineAutoBehavior(NGAutoBehavior::kStretchImplicit);
   }
 
   builder.SetAvailableSize(child_available_size);
@@ -2958,7 +2958,7 @@ void NGBlockLayoutAlgorithm::HandleRubyText(NGBlockNode ruby_text_child) {
   builder.SetAvailableSize(ChildAvailableSize());
   if (IsParallelWritingMode(ConstraintSpace().GetWritingMode(),
                             rt_style.GetWritingMode()))
-    builder.SetStretchInlineSizeIfAuto(true);
+    builder.SetInlineAutoBehavior(NGAutoBehavior::kStretchImplicit);
 
   scoped_refptr<const NGLayoutResult> result =
       ruby_text_child.Layout(builder.ToConstraintSpace(), break_token.get());
