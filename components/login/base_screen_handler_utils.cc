7deab64493288521fb69261ddd2c7d6e8fcf37b0
Ayu Ishii <ayui@chromium.org>
2021-05-26 17:46:28 +0000

    CodeHealth: Remove use of Value::GetAsBoolean/Value::GetAsString/Value::GetAsDouble
    This change removes the use of Value::GetAsBoolean
    and replaces with Value::is_bool(), Value::GetBool(),
    Value::GetIfBool(). Same for Value::GetAsString & Value::GetAsDouble.
    
    Bug: 1187003
    Change-Id: Ie33bfba1a9b48784697f9b302f4d3d1ced55dd63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2873068
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886799}

--- components/login/base_screen_handler_utils.cc
+++ components/login/base_screen_handler_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/login/base_screen_handler_utils.h"
 
+#include "base/strings/utf_string_conversions.h"
 #include "components/account_id/account_id.h"
 
 namespace login {
@@ -27,7 +28,11 @@ bool ParseStringList(const base::Value* value, StringListType* out_value) {
 }  // namespace
 
 bool ParseValue(const base::Value* value, bool* out_value) {
-  return value->GetAsBoolean(out_value);
+  if (out_value && value->is_bool()) {
+    *out_value = value->GetBool();
+    return true;
+  }
+  return value->is_bool();
 }
 
 bool ParseValue(const base::Value* value, int* out_value) {
@@ -39,20 +44,36 @@ bool ParseValue(const base::Value* value, int* out_value) {
 }
 
 bool ParseValue(const base::Value* value, double* out_value) {
-  return value->GetAsDouble(out_value);
+  if (out_value && (value->is_double() || value->is_int())) {
+    *out_value = value->GetDouble();
+    return true;
+  }
+  return value->is_double() || value->is_int();
 }
 
 bool ParseValue(const base::Value* value, std::string* out_value) {
-  return value->GetAsString(out_value);
+  if (out_value && value->is_string()) {
+    *out_value = value->GetString();
+    return true;
+  }
+  return value->is_string();
 }
 
 bool ParseValue(const base::Value* value, std::u16string* out_value) {
-  return value->GetAsString(out_value);
+  if (out_value && value->is_string()) {
+    *out_value = base::UTF8ToUTF16(value->GetString());
+    return true;
+  }
+  return value->is_string();
 }
 
 bool ParseValue(const base::Value* value,
                 const base::DictionaryValue** out_value) {
-  return value->GetAsDictionary(out_value);
+  if (out_value && value->is_dict()) {
+    *out_value = static_cast<const base::DictionaryValue*>(value);
+    return true;
+  }
+  return value->is_dict();
 }
 
 bool ParseValue(const base::Value* value, StringList* out_value) {
@@ -64,11 +85,10 @@ bool ParseValue(const base::Value* value, String16List* out_value) {
 }
 
 bool ParseValue(const base::Value* value, AccountId* out_value) {
-  std::string serialized;
-  const bool has_string = value->GetAsString(&serialized);
-  if (!has_string)
+  if (!value->is_string())
     return false;
 
+  std::string serialized = value->GetString();
   if (AccountId::Deserialize(serialized, out_value))
     return true;
 
