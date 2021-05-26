34e63719ee6b1199c6c363a711844e76f20e1c96
Amanda Deacon <amandadeacon@chromium.org>
2021-05-26 01:47:52 +0000

    [Live Caption] Remove browser process dependency from Live Caption Controller.
    Refactor only; no logic changes.
    
    This is part of a series of dependency-modifying changes to make it
    easier to move various Live Caption classes to
    //components/live_caption. The overarching goal is to disentangle Live
    Caption from //chrome/browser for the purposes of taking Live Caption
    system-wide. Design doc: http://shortn/_Qc94K0EzXM
    
    This particular change only reshuffles the browser process dependency
    and does not remove it, but it will make it easier to move
    LiveCaptionController into //components/live_caption.
    
    Bug: 1055150, 1196105
    AX-Relnotes: n/a.
    Change-Id: I2df0912b41660331a06b0868d30082859f001683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2914719
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886574}

--- chrome/browser/accessibility/live_caption_controller_factory.cc
+++ chrome/browser/accessibility/live_caption_controller_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "chrome/browser/accessibility/live_caption_controller.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -48,7 +49,8 @@ content::BrowserContext* LiveCaptionControllerFactory::GetBrowserContextToUse(
 KeyedService* LiveCaptionControllerFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   return new LiveCaptionController(
-      Profile::FromBrowserContext(context)->GetPrefs());
+      Profile::FromBrowserContext(context)->GetPrefs(),
+      g_browser_process->local_state());
 }
 
 }  // namespace captions
