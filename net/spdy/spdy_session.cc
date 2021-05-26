22e9c8f12be89f0ce51d0e2aa118537eff94c63f
David Benjamin <davidben@chromium.org>
2021-05-26 19:55:45 +0000

    Handle SpdySession::ConfirmHandshake() calls during connection shutdown
    This is a speculative fix for https://crbug.com/1211639. (I believe this
    is a reachable cause for the crash, but it seems to require a fairly
    tight race condition.)
    
    StreamSocket::ConfirmHandshake(), like other asynchronous operations on
    the class, does not allow two instances of the operation to run
    concurrently. SpdySession::ConfirmHandshake() is responsible for
    managing this with the in_confirm_handshake_ bit. However, if we
    NotifyRequestsOfConfirmation() in DoDrainSession() and *then* a request
    tries to ConfirmHandshake(), we'll get confused. (Requests shouldn't
    bind to SpdySessions in this state, but there seem to be enough
    PostTask() indirections that this may be possible.)
    
    Instead, match TryCreateStream() and, in STATE_GOAWAY or STATE_DRAINING
    states, return an error. This makes new ConfirmHandshake() calls
    consistent with the ones that were aborted early. Also matching
    TryCreateStream(), move the NotifyRequestsOfConfirmation() from
    DoDrainSession() to StartGoingAway(). Pending confirmation requests are
    analogous to pending_create_stream_queues_, so we should be clearing
    them in the same place.
    
    Bug: 1211639
    Change-Id: Iece068c8061ac280eae103e400ba53bafdbaaeb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917674
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886868}

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -1252,6 +1252,12 @@ bool SpdySession::ShouldSendPriorityUpdate() const {
 }
 
 int SpdySession::ConfirmHandshake(CompletionOnceCallback callback) {
+  if (availability_state_ == STATE_GOING_AWAY)
+    return ERR_FAILED;
+
+  if (availability_state_ == STATE_DRAINING)
+    return ERR_CONNECTION_CLOSED;
+
   int rv = ERR_IO_PENDING;
   if (!in_confirm_handshake_) {
     rv = socket_->ConfirmHandshake(
@@ -1551,6 +1557,8 @@ void SpdySession::StartGoingAway(spdy::SpdyStreamId last_good_stream_id,
   DCHECK_NE(ERR_IO_PENDING, status);
 
   // The loops below are carefully written to avoid reentrancy problems.
+
+  NotifyRequestsOfConfirmation(status);
 
   while (true) {
     size_t old_size = GetTotalSize(pending_create_stream_queues_);
@@ -3061,10 +3069,6 @@ void SpdySession::DoDrainSession(Error err, const std::string& description) {
     return;
   }
   MakeUnavailable();
-
-  // Notify any requests waiting for handshake confirmation that there is an
-  // error.
-  NotifyRequestsOfConfirmation(err);
 
   // Mark host_port_pair requiring HTTP/1.1 for subsequent connections.
   if (err == ERR_HTTP_1_1_REQUIRED) {
