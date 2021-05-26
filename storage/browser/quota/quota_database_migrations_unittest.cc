c661f8bcea1149a51de8cc356b8d2736bdc95ed0
Ayu Ishii <ayui@chromium.org>
2021-05-26 19:04:37 +0000

    StorageBuckets: Delete eviction_info table
    This change removes eviction_info table. This table
    (previously EvictionInfoTable) was added as an early
    exploration of using Site Engagement to evict data.
    With StorageBuckets, it no longer makes sense to keep
    eviction history per origin and will either need to be
    updated or removed. Since Site Engagement logic has already
    been removed, and it seems that no other team is looking
    at this data anymore, it seems safe to remove the table
    entirely.
    
    Bug: 1211438
    Change-Id: I03d45a545b2344949763688d9a8ab41eed3d818b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910136
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886839}

--- storage/browser/quota/quota_database_migrations_unittest.cc
+++ storage/browser/quota/quota_database_migrations_unittest.cc
@@ -15,6 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace storage {
 
+namespace {
+
+const int kCurrentSchemaVersion = 7;
+const int kCurrentCompatibleVersion = 7;
+
+}  // namespace
+
 class QuotaDatabaseMigrationsTest : public testing::Test {
  public:
   void SetUp() override { ASSERT_TRUE(temp_directory_.CreateUniqueTempDir()); }
@@ -60,7 +67,7 @@ class QuotaDatabaseMigrationsTest : public testing::Test {
   std::string GetCurrentSchema() {
     base::FilePath current_version_path =
         temp_directory_.GetPath().AppendASCII("current_version.db");
-    EXPECT_TRUE(LoadDatabase("version_6.sql", current_version_path));
+    EXPECT_TRUE(LoadDatabase("version_7.sql", current_version_path));
     sql::Database db;
     EXPECT_TRUE(db.Open(current_version_path));
     return db.GetSchema();
@@ -75,6 +82,13 @@ TEST_F(QuotaDatabaseMigrationsTest, UpgradeSchemaFromV5) {
   {
     sql::Database db;
     ASSERT_TRUE(db.Open(DbPath()));
+
+    ASSERT_TRUE(sql::MetaTable::DoesTableExist(&db));
+    sql::MetaTable meta_table;
+    ASSERT_TRUE(
+        meta_table.Init(&db, kCurrentSchemaVersion, kCurrentCompatibleVersion));
+    ASSERT_EQ(meta_table.GetVersionNumber(), 5);
+    ASSERT_EQ(meta_table.GetCompatibleVersionNumber(), 2);
 
     ASSERT_TRUE(db.DoesTableExist("HostQuotaTable"));
     ASSERT_TRUE(db.DoesTableExist("EvictionInfoTable"));
@@ -102,8 +116,14 @@ TEST_F(QuotaDatabaseMigrationsTest, UpgradeSchemaFromV5) {
     sql::Database db;
     ASSERT_TRUE(db.Open(DbPath()));
 
+    ASSERT_TRUE(sql::MetaTable::DoesTableExist(&db));
+    sql::MetaTable meta_table;
+    ASSERT_TRUE(
+        meta_table.Init(&db, kCurrentSchemaVersion, kCurrentCompatibleVersion));
+    ASSERT_EQ(meta_table.GetVersionNumber(), kCurrentSchemaVersion);
+    ASSERT_EQ(meta_table.GetCompatibleVersionNumber(), kCurrentSchemaVersion);
+
     ASSERT_TRUE(db.DoesTableExist("quota"));
-    ASSERT_TRUE(db.DoesTableExist("eviction_info"));
     ASSERT_TRUE(db.DoesTableExist("buckets"));
     ASSERT_FALSE(db.DoesTableExist("HostQuotaTable"));
     ASSERT_FALSE(db.DoesTableExist("EvictionInfoTable"));
@@ -121,6 +141,78 @@ TEST_F(QuotaDatabaseMigrationsTest, UpgradeSchemaFromV5) {
             &db, "SELECT * FROM buckets ORDER BY origin ASC", "|", ","));
 
     // Check that HostQuotaTable data is migrated to quota table.
+    EXPECT_EQ("a.com,b.com,c.com",
+              sql::test::ExecuteWithResults(
+                  &db, "SELECT host FROM quota ORDER BY host ASC", "|", ","));
+
+    EXPECT_EQ(GetCurrentSchema(), db.GetSchema());
+  }
+}
+
+TEST_F(QuotaDatabaseMigrationsTest, UpgradeSchemaFromV6) {
+  ASSERT_TRUE(LoadDatabase("version_6.sql", DbPath()));
+
+  {
+    sql::Database db;
+    ASSERT_TRUE(db.Open(DbPath()));
+
+    ASSERT_TRUE(sql::MetaTable::DoesTableExist(&db));
+    sql::MetaTable meta_table;
+    ASSERT_TRUE(
+        meta_table.Init(&db, kCurrentSchemaVersion, kCurrentCompatibleVersion));
+    ASSERT_EQ(meta_table.GetVersionNumber(), 6);
+    ASSERT_EQ(meta_table.GetCompatibleVersionNumber(), 6);
+
+    ASSERT_TRUE(db.DoesTableExist("quota"));
+    ASSERT_TRUE(db.DoesTableExist("buckets"));
+    ASSERT_TRUE(db.DoesTableExist("eviction_info"));
+
+    // Check populated data.
+    EXPECT_EQ(
+        "1|http://a/|0|bucket_a|123|13260644621105493|13242931862595604|"
+        "9223372036854775807|0,"
+        "2|http://b/|0|bucket_b|111|13250042735631065|13260999511438890|"
+        "9223372036854775807|1000,"
+        "3|http://c/|1|bucket_c|321|13261163582572088|13261079941303629|"
+        "9223372036854775807|10000",
+        sql::test::ExecuteWithResults(
+            &db, "SELECT * FROM buckets ORDER BY origin ASC", "|", ","));
+
+    EXPECT_EQ("a.com,b.com,c.com",
+              sql::test::ExecuteWithResults(
+                  &db, "SELECT host FROM quota ORDER BY host ASC", "|", ","));
+  }
+
+  MigrateDatabase();
+
+  // Verify upgraded schema.
+  {
+    sql::Database db;
+    ASSERT_TRUE(db.Open(DbPath()));
+
+    ASSERT_TRUE(sql::MetaTable::DoesTableExist(&db));
+    sql::MetaTable meta_table;
+    ASSERT_TRUE(
+        meta_table.Init(&db, kCurrentSchemaVersion, kCurrentCompatibleVersion));
+    ASSERT_EQ(meta_table.GetVersionNumber(), kCurrentSchemaVersion);
+    ASSERT_EQ(meta_table.GetCompatibleVersionNumber(), kCurrentSchemaVersion);
+
+    ASSERT_TRUE(db.DoesTableExist("quota"));
+    ASSERT_TRUE(db.DoesTableExist("buckets"));
+    ASSERT_FALSE(db.DoesTableExist("eviction_info"));
+
+    // Check that buckets data is still present.
+    EXPECT_EQ(
+        "1|http://a/|0|bucket_a|123|13260644621105493|13242931862595604|"
+        "9223372036854775807|0,"
+        "2|http://b/|0|bucket_b|111|13250042735631065|13260999511438890|"
+        "9223372036854775807|1000,"
+        "3|http://c/|1|bucket_c|321|13261163582572088|13261079941303629|"
+        "9223372036854775807|10000",
+        sql::test::ExecuteWithResults(
+            &db, "SELECT * FROM buckets ORDER BY origin ASC", "|", ","));
+
+    // Check that quota data is still present.
     EXPECT_EQ("a.com,b.com,c.com",
               sql::test::ExecuteWithResults(
                   &db, "SELECT host FROM quota ORDER BY host ASC", "|", ","));
