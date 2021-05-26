05a7d7ec0ee7c3c3bc170c0cf716ed7eb577effd
Kristi Park <kristipark@chromium.org>
2021-05-26 20:54:14 +0000

    [SharingHub] Move omnibox entry point next to bookmarks
    Fix issue where the omnibox entry point is not next to the bookmarks
    icon.
    
    Bug: 1210169
    Change-Id: I661eb045de31527bb3abe0a4c312be8c5d45c78a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919175
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886891}

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -269,15 +269,6 @@ void LocationBarView::Init() {
     params.types_enabled.push_back(PageActionIconType::kQRCodeGenerator);
     if (base::FeatureList::IsEnabled(kSharedClipboardUI))
       params.types_enabled.push_back(PageActionIconType::kSharedClipboard);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-    if (base::FeatureList::IsEnabled(features::kChromeOSSharingHub) &&
-        base::FeatureList::IsEnabled(features::kSharesheet)) {
-      params.types_enabled.push_back(PageActionIconType::kSharingHub);
-    }
-#else
-    if (base::FeatureList::IsEnabled(sharing_hub::kSharingHubDesktopOmnibox))
-      params.types_enabled.push_back(PageActionIconType::kSharingHub);
-#endif
     if (base::FeatureList::IsEnabled(kWebOTPCrossDevice))
       params.types_enabled.push_back(PageActionIconType::kSmsRemoteFetcher);
     if (!base::FeatureList::IsEnabled(
@@ -315,6 +306,17 @@ void LocationBarView::Init() {
       // final mocks.
       params.types_enabled.push_back(PageActionIconType::kSaveAutofillAddress);
     }
+  }
+  if (browser_) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    if (base::FeatureList::IsEnabled(features::kChromeOSSharingHub) &&
+        base::FeatureList::IsEnabled(features::kSharesheet)) {
+      params.types_enabled.push_back(PageActionIconType::kSharingHub);
+    }
+#else
+    if (base::FeatureList::IsEnabled(sharing_hub::kSharingHubDesktopOmnibox))
+      params.types_enabled.push_back(PageActionIconType::kSharingHub);
+#endif
   }
   if (browser_ && !is_popup_mode_)
     params.types_enabled.push_back(PageActionIconType::kBookmarkStar);
