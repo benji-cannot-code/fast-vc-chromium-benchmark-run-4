4e4067cf05ebe246cda8de97116c8514b679c1f9
James Cook <jamescook@chromium.org>
2021-05-27 23:31:54 +0000

    chromeos: Give AppListBubble a dark background
    These aren't the final colors. I just need a dark background so I can
    see the text labels of the app icons.
    
    Reuse some holding space code that sets up label text. I suspect we
    will end up sharing some text styling with holding space. I'll move
    the code to a shared location in a followup.
    
    Screenshots:
    https://screenshot.googleplex.com/9EhyGM4vtvSWoqP
    https://screenshot.googleplex.com/BWi7RyJ7DQAKF9M
    
    Bug: 1204554
    Change-Id: I98211c813948abea884109d61d25548bf30b9195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923963
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887385}

--- ash/app_list/bubble/bubble_search_page.cc
+++ ash/app_list/bubble/bubble_search_page.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "ash/system/holding_space/holding_space_util.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/scroll_view.h"
 #include "ui/views/layout/box_layout.h"
@@ -26,6 +28,8 @@ BubbleSearchPage::BubbleSearchPage() {
   scroll->SetDrawOverflowIndicator(false);
   scroll->SetHorizontalScrollBarMode(
       views::ScrollView::ScrollBarMode::kDisabled);
+  // Don't paint a background. The bubble already has one.
+  scroll->SetBackgroundColor(absl::nullopt);
 
   auto scroll_contents = std::make_unique<views::View>();
   scroll_contents->SetLayoutManager(
@@ -39,7 +43,11 @@ BubbleSearchPage::BubbleSearchPage() {
   results->SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical, gfx::Insets(), kSpacing));
   for (int i = 0; i < 20; ++i) {
-    results->AddChildView(std::make_unique<views::Label>(u"Result"));
+    auto label = std::make_unique<views::Label>(u"Result");
+    // TODO(crbug.com/1199925): Move this method to ash::bubble_utils.
+    holding_space_util::ApplyStyle(label.get(),
+                                   holding_space_util::LabelStyle::kBody);
+    results->AddChildView(std::move(label));
   }
 
   scroll->SetContents(std::move(scroll_contents));
