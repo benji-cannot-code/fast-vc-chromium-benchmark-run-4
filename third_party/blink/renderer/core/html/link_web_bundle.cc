a02a0415b9ee6662d92ca0bb7e4f50de37e92f7f
Hayato Ito <hayato@chromium.org>
2021-05-27 05:39:11 +0000

    Release resources in the network process explicitly when link is removed
    Address bug 1211659. It would be better to release resources
    immediately, instead of waiting for GC, when a <link rel=webbundle>
    element is removed.
    
    Bug: 1211659
    Change-Id: Ib6084aa2037cdc8a2d403bf5571d5a7e6d31d465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2912030
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887050}

--- third_party/blink/renderer/core/html/link_web_bundle.cc
+++ third_party/blink/renderer/core/html/link_web_bundle.cc
@@ -129,6 +129,12 @@ class WebBundleLoader : public GarbageCollected<WebBundleLoader>,
     return web_bundle_token_;
   }
 
+  void ClearReceivers() {
+    // Clear receivers_ explicitly so that resources in the netwok process are
+    // released.
+    receivers_.Clear();
+  }
+
  private:
   void DidFailInternal() {
     if (failed_)
@@ -211,7 +217,7 @@ void LinkWebBundle::Process() {
       // clang-format on
       if (bundle_loader_) {
         active_bundles->Remove(*this);
-        bundle_loader_ = nullptr;
+        ReleaseBundleLoader();
       }
       NotifyLoaded();
       OnWebBundleError("A nested bundle is not supported: " +
@@ -244,7 +250,8 @@ void LinkWebBundle::OwnerRemoved() {
   SubresourceWebBundleList* active_bundles =
       resource_fetcher->GetOrCreateSubresourceWebBundleList();
   active_bundles->Remove(*this);
-  bundle_loader_ = nullptr;
+  if (bundle_loader_)
+    ReleaseBundleLoader();
 }
 
 bool LinkWebBundle::CanHandleRequest(const KURL& url) const {
@@ -298,6 +305,13 @@ const KURL& LinkWebBundle::GetBundleUrl() const {
 const base::UnguessableToken& LinkWebBundle::WebBundleToken() const {
   DCHECK(bundle_loader_);
   return bundle_loader_->WebBundleToken();
+}
+
+void LinkWebBundle::ReleaseBundleLoader() {
+  DCHECK(bundle_loader_);
+  // Clear receivers explicitly here, instead of waiting for Blink GC.
+  bundle_loader_->ClearReceivers();
+  bundle_loader_ = nullptr;
 }
 
 // static
--- third_party/blink/renderer/core/html/link_web_bundle.h
+++ third_party/blink/renderer/core/html/link_web_bundle.h
@@ -61,6 +61,7 @@ class CORE_EXPORT LinkWebBundle final : public LinkResource,
 
  private:
   bool ResourcesOrScopesMatch(const KURL& url) const;
+  void ReleaseBundleLoader();
 
   Member<WebBundleLoader> bundle_loader_;
 };
