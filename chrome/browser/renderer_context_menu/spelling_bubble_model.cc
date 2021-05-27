d2c287e096d8cd6be7d787da0fcafd8b37cb9985
Avi Drissman <avi@chromium.org>
2021-05-27 18:15:43 +0000

    Fix UaF for the enhanced spell check bubble
    For the enhanced spell check bubble, be sure to use a valid
    WebContents for the help page. Adds a browser test.
    
    Note that this is a clone of 427728383657e6ccb06dbfcce0c5118bb557c0af
    that was committed to fix the same issue with the accessibility
    bubble. Alas, these are the only two subclassers of ConfirmBubbleModel
    across the Chromium codebase, and given that ConfirmBubbleModel is in
    ui/ and can't know about this specific lifetime issue, this is the
    simplest way forward.
    
    Bug: 1212498
    Test: As in bug
    Change-Id: I365e9721613ea2a89a8fad902ddc137b24de687f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920807
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887244}

--- chrome/browser/renderer_context_menu/spelling_bubble_model.cc
+++ chrome/browser/renderer_context_menu/spelling_bubble_model.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
@@ -14,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/spellcheck/browser/pref_names.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image.h"
@@ -24,10 +27,9 @@ using content::WebContents;
 
 SpellingBubbleModel::SpellingBubbleModel(Profile* profile,
                                          WebContents* web_contents)
-    : profile_(profile), web_contents_(web_contents) {}
+    : WebContentsObserver(web_contents), profile_(profile) {}
 
-SpellingBubbleModel::~SpellingBubbleModel() {
-}
+SpellingBubbleModel::~SpellingBubbleModel() = default;
 
 std::u16string SpellingBubbleModel::GetTitle() const {
   return l10n_util::GetStringUTF16(IDS_CONTENT_CONTEXT_SPELLING_BUBBLE_TITLE);
@@ -65,7 +67,13 @@ void SpellingBubbleModel::OpenHelpPage() {
   OpenURLParams params(GetHelpPageURL(), Referrer(),
                        WindowOpenDisposition::NEW_FOREGROUND_TAB,
                        ui::PAGE_TRANSITION_LINK, false);
-  web_contents_->OpenURL(params);
+  if (web_contents()) {
+    web_contents()->OpenURL(params);
+    return;
+  }
+  // The web contents used to open this dialog have been destroyed.
+  Browser* browser = chrome::ScopedTabbedBrowserDisplayer(profile_).browser();
+  browser->OpenURL(params);
 }
 
 void SpellingBubbleModel::SetPref(bool enabled) {
