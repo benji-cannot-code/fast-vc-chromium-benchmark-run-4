1fdb84b0f0a621a5423ad9eba633ef6cc609cc6d
Lei Zhang <thestig@chromium.org>
2021-05-27 00:19:01 +0000

    [unseasoned-pdf] Add code path for Blink-based Linux font mapping.
    Introduce FontMappingMode and set it when initializing PDFium. Then
    change the Linux font mapper to read the FontMappingMode and take a
    different, non-implemented path for Blink mode.
    
    Bug: 1139079
    Change-Id: Ic795f5f52a03f6da8ee4aadabf182f358f43387c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2853482
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#886971}

--- pdf/pdfium/pdfium_font_linux.cc
+++ pdf/pdfium/pdfium_font_linux.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/ranges.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
+#include "pdf/pdfium/pdfium_engine.h"
 #include "pdf/ppapi_migration/pdfium_font_linux.h"
 #include "third_party/blink/public/platform/web_font_description.h"
 #include "third_party/pdfium/public/fpdf_sysfontinfo.h"
@@ -21,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome_pdf {
 
 namespace {
+
+bool UsePepperMapping() {
+  return PDFiumEngine::GetFontMappingMode() != FontMappingMode::kBlink;
+}
 
 blink::WebFontDescription::Weight WeightToBlinkWeight(int weight) {
   static_assert(blink::WebFontDescription::kWeight100 == 0, "Blink Weight min");
@@ -146,7 +151,11 @@ void* MapFont(FPDF_SYSFONTINFO*,
     desc.italic = italic > 0;
   }
 
-  return MapPepperFont(desc, charset);
+  if (UsePepperMapping())
+    return MapPepperFont(desc, charset);
+
+  NOTIMPLEMENTED();
+  return nullptr;
 }
 
 unsigned long GetFontData(FPDF_SYSFONTINFO*,
@@ -154,11 +163,20 @@ unsigned long GetFontData(FPDF_SYSFONTINFO*,
                           unsigned int table,
                           unsigned char* buffer,
                           unsigned long buf_size) {
-  return GetPepperFontData(font_id, table, buffer, buf_size);
+  if (UsePepperMapping())
+    return GetPepperFontData(font_id, table, buffer, buf_size);
+
+  NOTIMPLEMENTED();
+  return 0;
 }
 
 void DeleteFont(FPDF_SYSFONTINFO*, void* font_id) {
-  DeletePepperFont(font_id);
+  if (UsePepperMapping()) {
+    DeletePepperFont(font_id);
+    return;
+  }
+
+  NOTIMPLEMENTED();
 }
 
 FPDF_SYSFONTINFO g_font_info = {1,           0, EnumFonts, MapFont,   0,
