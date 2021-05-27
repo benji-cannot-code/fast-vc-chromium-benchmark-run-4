07cc32b60dbaf0ca144834b0eef7809ef543bd8d
Zufeng Wang <zufeng@google.com>
2021-05-27 02:05:32 +0000

    Help app: Remove undefined from customLaunchData.delegate type
    Because receiver.js always sets the delegate unconditionally.
    
    Based on feedback from
    https://chromium-review.googlesource.com/c/chromium/src/+/2916301
    
    Bug: b/189291340
    Change-Id: Ia15c795843ddc7edc81700cc7c361ca17cca7241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919161
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Cr-Commit-Position: refs/heads/master@{#887003}

--- chromeos/components/help_app_ui/resources/help_app.externs.js
+++ chromeos/components/help_app_ui/resources/help_app.externs.js
@@ -214,7 +214,7 @@ helpApp.ClientApiDelegate.prototype.updateLauncherSearchIndex
 /**
  * Launch data that can be read by the app when it first loads.
  * @type {{
- *     delegate: (!helpApp.ClientApiDelegate | undefined),
+ *     delegate: !helpApp.ClientApiDelegate,
  * }}
  */
 window.customLaunchData;
