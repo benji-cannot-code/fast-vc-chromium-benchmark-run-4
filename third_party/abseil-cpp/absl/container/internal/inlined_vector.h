80c1ad6f37d7a98fc386fcd63f6f3930761cc5f2
Danil Chapovalov <danilchap@chromium.org>
2021-05-27 05:51:52 +0000

    Reland "Roll abseil_revision aad2c8a396..18045c4e32"
    This is a reland of 7f5bb02bd232e188dcfcadb20f02b2f661a28708
    
    Original change's description:
    > Roll abseil_revision aad2c8a396..18045c4e32
    >
    > Change Log:
    > https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/aad2c8a396..18045c4e32
    > Full diff:
    > https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/aad2c8a396..18045c4e32
    >
    > No .def changes
    >
    > Bug: None
    > Change-Id: Ib908943312bf8df13ac300d430beb441b4f5f400
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917065
    > Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    > Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#886668}
    
    Bug: 1213693
    Change-Id: I5b8398d94ac17026435feb31f6356aebfda31324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919257
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#887051}

--- third_party/abseil-cpp/absl/container/internal/inlined_vector.h
+++ third_party/abseil-cpp/absl/container/internal/inlined_vector.h
@@ -37,6 +37,7 @@ namespace inlined_vector_internal {
 // GCC does not deal very well with the below code
 #if !defined(__clang__) && defined(__GNUC__)
 #pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Warray-bounds"
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 #endif
 
@@ -956,7 +957,7 @@ auto Storage<T, N, A>::Swap(Storage* other_storage_ptr) -> void {
   swap(*GetAllocPtr(), *other_storage_ptr->GetAllocPtr());
 }
 
-// End ignore "maybe-uninitialized"
+// End ignore "array-bounds" and "maybe-uninitialized"
 #if !defined(__clang__) && defined(__GNUC__)
 #pragma GCC diagnostic pop
 #endif
