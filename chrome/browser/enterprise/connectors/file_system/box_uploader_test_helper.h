af045b8c7e4808d4891b6ed61de90e41d12796be
Shanthanu Bhardwaj <xanth@google.com>
2021-05-27 16:49:24 +0000

    [FileSystemConnector] Deal with file name conflicts when uploading to Box
    Use the same naming scheme as base::GetUniquePath in `file_util.cc` to generate a unique non-conflicting filename for upload to Box. Unfortunately Box doesn't allow use to query multiple filenames in a single batched call, so each potential filename has to be queried sequentially.  If this ends up resulting in too many roundtrips it should be possible to list the contents of the folder and do the de-duping locally, but managing paging through the "list folder" results and parsing the json seems overkill at this point.
    
    Currently, limiting this to 9 retries after which we try a timstamp-based naming scheme.
    
    Change-Id: I9c5a0f33036316fc7743de68cc10b52ee1c64418
    Bug: 1198617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2898492
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Alice Gong <alicego@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887192}

--- chrome/browser/enterprise/connectors/file_system/box_uploader_test_helper.h
+++ chrome/browser/enterprise/connectors/file_system/box_uploader_test_helper.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "chrome/browser/enterprise/connectors/file_system/box_api_call_flow.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
@@ -27,9 +28,12 @@ namespace enterprise_connectors {
 
 class BoxUploader;
 
+// 1994-04-27 00:00:00.001 GMT+2 (South Africa Standard Time)
+const base::Time::Exploded kTestDateTime = {1994, 4, 2, 27, 0, 0, 0, 1};
 class DownloadItemForTest : public content::FakeDownloadItem {
  public:
-  explicit DownloadItemForTest(base::FilePath::StringPieceType file_name);
+  explicit DownloadItemForTest(base::FilePath::StringPieceType file_name,
+                               base::Time::Exploded start_time = kTestDateTime);
   const base::FilePath& GetFullPath() const override;
 
  protected:
