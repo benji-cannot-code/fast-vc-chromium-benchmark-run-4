4722f0430c78a2bacf283ef31a019c5f5f079be1
Mythri A <mythria@chromium.org>
2021-05-27 09:51:06 +0000

    Use UniqueReceiverSet to hold CodeCacheHost mojo receivers
    CodeCacheHostImpl is moving to per-document and it is better to
    maintain them as UniqueReceiverSet since the CodeCacheHostImpl instance
    would be automatically released when there is a new navigation.
    
    Currently UniqueReceiverSet implementation doesn't correctly support
    SwapImplForTesting so this CL updates the mojo bindings to correctly
    handle this. The initial implementation is returned to
    the caller, so it's callers responsibility to hold it and either destroy
    it or swap it back when the test is done to avoid memory leaks.
    
    Change-Id: I0ccd84f75e9786a9a6d53fa43532a8bc3968da21
    BUG: chromium:1213818, chromium:1213476
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2912589
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887096}

--- mojo/public/cpp/bindings/receiver_set.h
+++ mojo/public/cpp/bindings/receiver_set.h
@@ -322,7 +322,7 @@ class ReceiverSetBase {
       return nullptr;
 
     ReceiverEntry& entry = static_cast<ReceiverEntry&>(it->second->receiver());
-    return entry.SwapImplForTesting(new_impl);
+    return entry.SwapImplForTesting(std::move(new_impl));
   }
 
  private:
@@ -356,7 +356,7 @@ class ReceiverSetBase {
     void FlushForTesting() override { receiver_.FlushForTesting(); }
 
     ImplPointerType SwapImplForTesting(ImplPointerType new_impl) {
-      return receiver_.SwapImplForTesting(new_impl);
+      return receiver_.SwapImplForTesting(std::move(new_impl));
     }
 
     PendingType Unbind() { return receiver_.Unbind(); }
