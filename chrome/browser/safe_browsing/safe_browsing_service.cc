058d154c5625c4da8136989095297ab53337a969
Hans Wennborg <hans@chromium.org>
2021-05-27 18:36:24 +0000

    Don't include chrome_password_protection_service.h in services_delegate.h
    It's wasn't used, and include graph analysis says it contributed almost
    400 MB when building the 'chrome' target on Linux.
    
    Bug: 242216
    Change-Id: Iaa328171d9c3100748424978b503c11b4b4b74cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919697
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887262}

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/safe_browsing/chrome_password_protection_service.h"
 #include "chrome/browser/safe_browsing/chrome_password_protection_service_factory.h"
 #include "chrome/browser/safe_browsing/safe_browsing_metrics_collector.h"
 #include "chrome/browser/safe_browsing/safe_browsing_metrics_collector_factory.h"
