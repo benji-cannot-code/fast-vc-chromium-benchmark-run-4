dd0535348741ac49af657b56ae2db95c4e6bfa41
Alexey Baskakov <loyso@chromium.org>
2021-05-27 08:32:58 +0000

    dpwa: Fix SetWebAppManifestFieldsAndWriteData.
    Generalize WebAppInstallTaskTest.WriteDataToDisk test
    to cover all purposes including newly added.
    
    Bug: 1114638
    Change-Id: I96374e6b06454972786728e4c9834dd453b63694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2918036
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887074}

--- chrome/browser/web_applications/test/web_app_icon_test_utils.h
+++ chrome/browser/web_applications/test/web_app_icon_test_utils.h
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_WEB_APPLICATIONS_TEST_WEB_APP_ICON_TEST_UTILS_H_
 
 #include <map>
+#include <vector>
 
 #include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "chrome/browser/web_applications/components/web_app_id.h"
 #include "chrome/browser/web_applications/components/web_app_install_utils.h"
 #include "chrome/browser/web_applications/components/web_application_info.h"
+#include "third_party/blink/public/common/manifest/manifest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColor.h"
 
@@ -46,6 +48,7 @@ base::FilePath GetAppIconsAnyDir(Profile* profile, const AppId& app_id);
 
 base::FilePath GetAppIconsMaskableDir(Profile* profile, const AppId& app_id);
 
+// Performs blocking IO and decompression.
 bool ReadBitmap(FileUtilsWrapper* utils,
                 const base::FilePath& file_path,
                 SkBitmap* bitmap);
@@ -59,6 +62,16 @@ void ExpectImageSkiaRep(const gfx::ImageSkia& image_skia,
                         float scale,
                         SquareSizePx size_px,
                         SkColor color);
+
+blink::Manifest::ImageResource CreateSquareImageResource(
+    const GURL& src,
+    int size_px,
+    const std::vector<IconPurpose>& purposes);
+
+// Performs blocking IO and decompression.
+std::map<SquareSizePx, SkBitmap> ReadPngsFromDirectory(
+    FileUtilsWrapper* file_utils,
+    const base::FilePath& icons_dir);
 
 }  // namespace web_app
 
