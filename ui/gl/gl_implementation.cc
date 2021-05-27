4e13795c34553bb5bc3c3b3d22f647ed46a158ca
Alexis Hetu <sugoi@google.com>
2021-05-27 12:18:41 +0000

    Split switch used for both tests and headless mode
    The command line switch kOverrideUseSoftwareGLForTests was used for both
    tests and headless mode. Since the name was misleading, this cl adds a
    new switch called kOverrideUseSoftwareGLForHeadless, and uses it where
    appropriate.
    
    With both use cases clearly separate, this CL also re-enables the use of
    SwANGLE on Windows and Linux for tests which use the
    kOverrideUseSoftwareGLForTests switch.
    
    Bug: chromium:1211482
    Change-Id: I493f6554bcd036a8de1e71b4a8665a1bb6c6938b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920189
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887118}

--- ui/gl/gl_implementation.cc
+++ ui/gl/gl_implementation.cc
@@ -183,6 +183,15 @@ GLImplementationParts GetSoftwareGLImplementation() {
 }
 
 GLImplementationParts GetSoftwareGLForTestsImplementation() {
+#if defined(OS_WIN) || \
+    (defined(OS_LINUX) && !defined(OS_FUCHSIA) && !defined(USE_OZONE))
+  return GetSoftwareGLImplementation();
+#else
+  return GetLegacySoftwareGLImplementation();
+#endif
+}
+
+GLImplementationParts GetSoftwareGLForHeadlessImplementation() {
   return GetLegacySoftwareGLImplementation();
 }
 
