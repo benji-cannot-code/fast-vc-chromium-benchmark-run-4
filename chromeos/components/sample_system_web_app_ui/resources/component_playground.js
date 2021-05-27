c85892ecbdf3907c09562879cc37e9118313800d
Christopher Lam <calamity@chromium.org>
2021-05-27 04:45:19 +0000

    Add cros-button to Sample System Web App
    This CL adds a cros-button element to the Component Playground.
    cros-button is an extension of mwc-button with extra styling to align
    with Chrome OS mocks. The button is implemented in TS but is compiled to
    JS before check-in due to the TS toolchain still being in development.
    
    The TS output is also run through rewrite_imports.js so that full paths
    can be resolved for rollup.
    
    Bug: b/176932745
    
    Change-Id: I116fd85191268700c05a45034c2f4a924361e510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2790762
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887045}

--- chromeos/components/sample_system_web_app_ui/resources/component_playground.js
+++ chromeos/components/sample_system_web_app_ui/resources/component_playground.js
@@ -3,4 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../../../../third_party/material_web_components/components-chromium/node_modules/@material/mwc-button/mwc-button.js'
+import '../../../../chromeos/components/cros_elements/button/button.js'
+
+customElements.whenDefined('cros-button').then(() => {
+  document.documentElement.classList.remove('loading');
+});
