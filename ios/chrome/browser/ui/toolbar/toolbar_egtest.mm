3abf0dbe59616085f9e2a03de284bb8980ea8cc0
Gauthier Ambard <gambard@chromium.org>
2021-05-27 14:20:23 +0000

    [iOS] Tentative deflake for toolbar test
    Try to deflake tests:
    - Add a delay on the JavaScript test to avoid failing because the test
      is done before the alert is displayed.
    - Add a "SufficientlyVisible" condition to the WebUI test to avoid
      get mixed with non-visible labels.
    
    Fixed: 1210781
    Change-Id: Id20b3debc8f767e28dda05ae49fd9f011b5f0ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909654
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887151}

--- ios/chrome/browser/ui/toolbar/toolbar_egtest.mm
+++ ios/chrome/browser/ui/toolbar/toolbar_egtest.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/ios/ios_util.h"
+#import "base/test/ios/wait_util.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_constants.h"
@@ -314,8 +315,17 @@ using chrome_test_util::SystemSelectionCalloutCopyButton;
 
   [ChromeEarlGreyUI focusOmniboxAndType:@"javascript:alert('Hello');\n"];
 
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(@"Hello")]
-      assertWithMatcher:grey_notNil()];
+  ConditionBlock condition = ^{
+    NSError* error = nil;
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(@"Hello")]
+        assertWithMatcher:grey_sufficientlyVisible()
+                    error:&error];
+    return error == nil;
+  };
+
+  bool alertVisible = base::test::ios::WaitUntilConditionOrTimeout(
+      base::test::ios::kWaitForUIElementTimeout, condition);
+  GREYAssertTrue(alertVisible, @"JavaScript alert didn't appear");
 }
 
 // Loads WebUI page, types JavaScript into Omnibox and verifies that alert is
@@ -331,7 +341,9 @@ using chrome_test_util::SystemSelectionCalloutCopyButton;
   [ChromeEarlGrey loadURL:GURL("chrome://version")];
   [ChromeEarlGreyUI focusOmniboxAndType:@"javascript:alert('Hello');\n"];
 
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(@"Hello")]
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(grey_accessibilityLabel(@"Hello"),
+                                          grey_sufficientlyVisible(), nil)]
       assertWithMatcher:grey_nil()];
 }
 
