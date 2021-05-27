5d0f74de67e8dc01f0a895654bf65661ce46c228
Adam Langley <agl@chromium.org>
2021-05-27 21:28:14 +0000

    Revert "Measure TPM support on Windows."
    This reverts commit 2de69aede9eb6eb29841f85badf3666c27ee6b54.
    
    A clean revert save for adding an <obsolete> tag in the histograms XML
    rather than removing it.
    
    BUG=1184938
    
    Change-Id: I7ce92694b10fe5987b6cf9291a4ec76d52863137
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919171
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887347}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_switches.h"
-#include "crypto/unexportable_key.h"
 #include "services/resource_coordinator/public/cpp/memory_instrumentation/browser_metrics.h"
 #include "ui/base/pointer/pointer_device.h"
 #include "ui/base/ui_base_switches.h"
@@ -224,8 +223,6 @@ void RecordStartupMetrics() {
   // Determine if Applocker is enabled and running. This does not check if
   // Applocker rules are being enforced.
   base::UmaHistogramBoolean("Windows.ApplockerRunning", IsApplockerRunning());
-
-  crypto::MeasureTPMAvailability();
 #endif  // defined(OS_WIN)
 
   bluetooth_utility::ReportBluetoothAvailability();
