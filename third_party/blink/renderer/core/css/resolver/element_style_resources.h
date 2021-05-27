3d154a90b8f6df4cbd7db59a3af13ce6a7fd02b7
Hans Wennborg <hans@chromium.org>
2021-05-27 17:03:29 +0000

    [blink] Don't include fetch_parameters.h in element_style_resources.h
    Include graph analysis says this should cut 1.5GB of build input
    from the 'chrome' target on Linux.
    
    The header was only used for the parameter type of a helper function,
    which this CL moves into the .cc file instead.
    
    Bug: 242216
    Change-Id: I0ad1e1a13d624d0974e64d0591bcedf8a97ce97d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2922342
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887200}

--- third_party/blink/renderer/core/css/resolver/element_style_resources.h
+++ third_party/blink/renderer/core/css/resolver/element_style_resources.h
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
-#include "third_party/blink/renderer/platform/loader/fetch/cross_origin_attribute_value.h"
-#include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
 namespace blink {
@@ -78,12 +76,6 @@ class ElementStyleResources {
 
   void LoadPendingSVGResources(ComputedStyle&);
   void LoadPendingImages(ComputedStyle&);
-
-  StyleImage* LoadPendingImage(
-      ComputedStyle&,
-      CSSValue&,
-      FetchParameters::ImageRequestBehavior = FetchParameters::kNone,
-      CrossOriginAttributeValue = kCrossOriginAttributeNotSet);
 
   Element& element_;
   HashSet<CSSPropertyID> pending_image_properties_;
