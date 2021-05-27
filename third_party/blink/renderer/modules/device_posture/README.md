3c7e816fc1245f189578108d5f47e849735a7f58
baul.eun <baul.eun@samsung.com>
2021-05-27 15:35:58 +0000

    DevicePosture: Implement IDL for DevicePosture
    This change adds initial draft of IDL for DevicePosture.
    This does not have the full implementation of the API and only
    store initial value of posture in the renderer.
    Implementation of the endpoints will come in following CLs.
    This CL adds the following.
    
    window.navigator.devicePosture.type: "no-fold"
    
    Explainer: https://github.com/w3c/device-posture/blob/gh-pages/README.md
    
    R=dcheng@chromium.org
    
    Bug: 1066842
    Change-Id: I3473df603b90c2067efc571fc263f8a2451114c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2598083
    Commit-Queue: Alexis Menard <alexis.menard@intel.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887173}

---
+++ third_party/blink/renderer/modules/device_posture/README.md
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# DevicePosture
+
+This directory contains the implementation of [the DevicePosture API](https://w3c.github.io/device-posture/).
---
+++ third_party/blink/renderer/modules/device_posture/device_posture.h
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_DEVICE_POSTURE_DEVICE_POSTURE_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_DEVICE_POSTURE_DEVICE_POSTURE_H_
+
+#include "third_party/blink/public/mojom/device_posture/device_posture.mojom-blink.h"
+#include "third_party/blink/renderer/core/dom/events/event_target.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
+#include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+
+namespace blink {
+
+class LocalDOMWindow;
+
+class MODULES_EXPORT DevicePosture : public EventTargetWithInlineData,
+                                     public ExecutionContextClient {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  explicit DevicePosture(LocalDOMWindow*);
+  ~DevicePosture() override;
+
+  // Web-exposed interfaces
+  DEFINE_ATTRIBUTE_EVENT_LISTENER(change, kChange)
+  String type() const;
+
+  // EventTarget overrides.
+  ExecutionContext* GetExecutionContext() const override;
+  const AtomicString& InterfaceName() const override;
+
+  void Trace(blink::Visitor*) const override;
+
+ private:
+  // TODO(baul.eun): Retrieve infomation from browser side.
+  // And will process change logic.
+  mojom::blink::DevicePostureType posture_ =
+      mojom::blink::DevicePostureType::kNoFold;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_DEVICE_POSTURE_DEVICE_POSTURE_H_
