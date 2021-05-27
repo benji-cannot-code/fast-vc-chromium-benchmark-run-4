d0cf065de8264f21f91a74cf4988fefe03bf8e20
Darren Shen <shend@chromium.org>
2021-05-27 03:24:21 +0000

    ime: Move Chrome OS only GetTextInputFlags to InputMethodChromeOS.
    It is only used by InputMethodChromeOS, so it doesn't belong in the
    InputMethod interface, which is shared with other platforms.
    
    Bug: 1213281
    Change-Id: Ic558710e6b8acc9a6b7256ef5270558c0b9d083a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920129
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887025}

--- ui/base/ime/dummy_input_method.cc
+++ ui/base/ime/dummy_input_method.cc
@@ -69,10 +69,6 @@ TextInputMode DummyInputMethod::GetTextInputMode() const {
   return TEXT_INPUT_MODE_DEFAULT;
 }
 
-int DummyInputMethod::GetTextInputFlags() const {
-  return 0;
-}
-
 bool DummyInputMethod::IsCandidatePopupOpen() const {
   return false;
 }
