d1218dbafeea3d61bb1a6d97fa2bd538eb24652c
Tobias Grundel <tgrundel@vewd.com>
2021-05-27 19:55:13 +0000

    Call mojo FlushCallback if pending on early disconnect
    Mojo does not support pending callbacks at connection teardown.
    The barrier created for Flush() can be a problem at shutdown as the
    mojo callback is being held in some async task at the time when the
    connnection is torn down. In this case the SessionStorageControl mojo
    interface can get disconnected before all async
    
    StorageAreaImpl::ScheduleImmediateCommit calls have been completed.
    This is another fix for the same root problem as
    https://chromium-review.googlesource.com/c/chromium/src/+/2096679
    
    Bug: 1204088, 938178
    Change-Id: I031fad6ce37bef6e5e812cad4ca8095641ea9a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2839608
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887302}

--- components/services/storage/dom_storage/session_storage_impl.cc
+++ components/services/storage/dom_storage/session_storage_impl.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/storage/dom_storage/async_dom_storage_database.h"
 #include "components/services/storage/dom_storage/dom_storage_database.h"
 #include "components/services/storage/dom_storage/session_storage_area_impl.h"
+#include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "third_party/leveldatabase/env_chromium.h"
 #include "third_party/leveldatabase/leveldb_chrome.h"
@@ -307,8 +308,11 @@ void SessionStorageImpl::Flush(FlushCallback callback) {
 
   base::RepeatingClosure commit_callback = base::BarrierClosure(
       base::saturated_cast<int>(data_maps_.size()), std::move(callback));
+
   for (const auto& it : data_maps_)
-    it.second->storage_area()->ScheduleImmediateCommit(commit_callback);
+    it.second->storage_area()->ScheduleImmediateCommit(
+        mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+            base::OnceClosure(commit_callback)));
 }
 
 void SessionStorageImpl::GetUsage(GetUsageCallback callback) {
