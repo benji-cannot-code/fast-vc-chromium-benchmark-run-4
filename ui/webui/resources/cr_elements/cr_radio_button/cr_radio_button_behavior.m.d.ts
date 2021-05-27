c00ab207551fb75b825593c6201a584a170606e5
dpapad <dpapad@chromium.org>
2021-05-27 08:10:24 +0000

    Typescript: Add definition files for more ui/webui/resources/.
     - Auto-generating definitions for
       - cr_link_row/
       - cr_radio_button/cr_radio_button_style_css.m.js
       - cr_tabs/
       - cr_actionable_row_style.js
       - js/cr/ui/drag_wrapper.js
     - Checking-in definitions for
       - cr_expand_button.js
       - cr_radio_button.js
       - cr_radio_button_behavior.js
       - cr_radio_group.js
       - cr_tooltip_icon.js
    
    This is in preparation of migrating Extensions to TypeScript.
    
    Bug: 1189595
    Change-Id: I1e7f424e90aa96a50feaabc9e5737df7505fea2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917613
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887072}

---
+++ ui/webui/resources/cr_elements/cr_radio_button/cr_radio_button_behavior.m.d.ts
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export interface CrRadioButtonBehaviorInterface {
+  checked: boolean;
+  disabled: boolean;
+  focusable: boolean;
+  label: string;
+  name: string;
+}
+
+export {CrRadioButtonBehavior};
+
+interface CrRadioButtonBehavior extends CrRadioButtonBehaviorInterface {}
+
+declare const CrRadioButtonBehavior: object;
