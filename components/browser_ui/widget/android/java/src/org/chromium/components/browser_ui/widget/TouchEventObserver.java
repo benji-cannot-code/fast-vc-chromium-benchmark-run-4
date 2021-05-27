19719fdd8979f669b1676a3e6e26bce22cc4371e
Jinsuk Kim <jinsukkim@chromium.org>
2021-05-27 06:58:57 +0000

    GestureNav: Remove all deps on chrome_java
    - GestureNavEventHandler is an interface for CompositorViewHolder(CVH)
      to dispatch touch events to GestureNav UI. This replaces
      TouchEventObserver previously defined in CVH. RootUiCoordinator
      instantiates the class, and coordinates CVH and GestureNav component.
    - CurrentTabObserver replaces ActivityTabProvider in the coordinator
    - SwipeRefreshHandler that passes overscroll events to GestureNav
      component is now updated in RootUiCoordinator.
    
    Bug: 1100717
    Change-Id: If3e0d109e27c10d3eba2bd4953ae30f0a0a439ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2681353
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887062}

---
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/TouchEventObserver.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.browser_ui.widget;
+
+import android.view.MotionEvent;
+
+/**
+ * Observer interface for any object that needs to process touch events.
+ */
+public interface TouchEventObserver {
+    /**
+     * Determine if touch events should be forwarded to the observing object.
+     * Should return {@link true} if the object decided to consume the events.
+     * @param e {@link MotionEvent} object to process.
+     * @return {@code true} if the observer will process touch events going forward.
+     */
+    boolean shouldInterceptTouchEvent(MotionEvent e);
+
+    /**
+     * Handle touch events.
+     * @param e {@link MotionEvent} object to process.
+     */
+    void handleTouchEvent(MotionEvent e);
+}
