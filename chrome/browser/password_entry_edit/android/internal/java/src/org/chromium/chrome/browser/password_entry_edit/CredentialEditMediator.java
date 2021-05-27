16611f3f3c2a9c2d4810f9dfc507ccca956f3a60
Ioana Pandele <ioanap@chromium.org>
2021-05-27 14:53:35 +0000

    [PwdEditAndroid]Don't handle text changes before the model is initialized
    Sometimes text change events fire before the initial text is even set on the
    field. This causes an empty string to be set on the model instead of the
    password. Checking if the model is initialized before handling a text
    change should fix the issue.
    
    Bug: 1213843
    Change-Id: I2ec590ec0c37a9f7d36d6128daed8a311ca30071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919759
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887156}

--- chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditMediator.java
+++ chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditMediator.java
@@ -207,6 +207,8 @@ public class CredentialEditMediator implements UiActionHandler {
 
     @Override
     public void onUsernameTextChanged(String username) {
+        // Ignore events before the UI is fully initialized.
+        if (mModel.get(USERNAME) == null) return;
         mModel.set(USERNAME, username);
         boolean hasError =
                 !mOriginalUsername.equals(username) && mExistingUsernames.contains(username);
@@ -219,6 +221,8 @@ public class CredentialEditMediator implements UiActionHandler {
 
     @Override
     public void onPasswordTextChanged(String password) {
+        // Ignore events before the UI is fully initialized.
+        if (mModel.get(PASSWORD) == null) return;
         mModel.set(PASSWORD, password);
         mModel.set(EMPTY_PASSWORD_ERROR, password.isEmpty());
         if (password.isEmpty()) {
