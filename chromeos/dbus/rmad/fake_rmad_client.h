cf578d9d4685e59f68d65814f166c506cca9693c
Gavin Dodd <gavindodd@google.com>
2021-05-28 16:57:20 +0000

    shimless: rmadclient
     - Add GetLogPath method.
     - Add support for observing signals.
    
    Bug: 1194292
    Test: Build default and volteer/Release
          Run tests for rmad_client and fake_rmad_client
    
    Change-Id: I7e54d32655177976ef12897bcb03d9cf5734bdb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917513
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887512}

--- chromeos/dbus/rmad/fake_rmad_client.h
+++ chromeos/dbus/rmad/fake_rmad_client.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_DBUS_RMAD_FAKE_RMAD_CLIENT_H_
 #define CHROMEOS_DBUS_RMAD_FAKE_RMAD_CLIENT_H_
 
+#include <string>
 #include <vector>
 
 #include "base/component_export.h"
+#include "base/observer_list.h"
 #include "chromeos/dbus/rmad/rmad.pb.h"
 #include "chromeos/dbus/rmad/rmad_client.h"
 
@@ -31,9 +33,25 @@ class COMPONENT_EXPORT(RMAD) FakeRmadClient : public RmadClient {
 
   void AbortRma(DBusMethodCallback<rmad::AbortRmaReply> callback) override;
 
+  void GetLogPath(DBusMethodCallback<std::string> callback) override;
+
+  void AddObserver(Observer* observer) override;
+  void RemoveObserver(Observer* observer) override;
+  bool HasObserver(const Observer* observer) const override;
+
   void SetFakeStateReplies(std::vector<rmad::GetStateReply> fake_states);
 
   void SetAbortable(bool abortable);
+
+  void TriggerErrorObservation(rmad::RmadErrorCode error);
+  void TriggerCalibrationProgressObservation(
+      rmad::CalibrateComponentsState::CalibrationComponent component,
+      double progress);
+  void TriggerProvisioningProgressObservation(
+      rmad::ProvisionDeviceState::ProvisioningStep step,
+      double progress);
+  void TriggerHardwareWriteProtectionStateObservation(bool enabled);
+  void TriggerPowerCableStateObservation(bool plugged_in);
 
  private:
   const rmad::GetStateReply& GetStateReply() const;
@@ -44,6 +62,7 @@ class COMPONENT_EXPORT(RMAD) FakeRmadClient : public RmadClient {
   std::vector<rmad::GetStateReply> state_replies_;
   size_t state_index_;
   rmad::AbortRmaReply abort_rma_reply_;
+  base::ObserverList<Observer>::Unchecked observers_;
 };
 
 }  // namespace chromeos
