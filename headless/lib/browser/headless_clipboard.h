ee68565d5cd4061bc8ae151183f652725e1c41c7
Austin Sullivan <asully@chromium.org>
2021-05-28 14:12:38 +0000

    Add TODO to create tests for HeadlessClipboard
    Bug: 1213221
    Change-Id: I564041d78ab6dc946d11f4c7024d2b0b1449e63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917802
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887502}

--- headless/lib/browser/headless_clipboard.h
+++ headless/lib/browser/headless_clipboard.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace headless {
 
+// TODO(crbug.com/1213221): Add tests. This class is mostly untested.
 class HeadlessClipboard : public ui::Clipboard {
  public:
   HeadlessClipboard();
