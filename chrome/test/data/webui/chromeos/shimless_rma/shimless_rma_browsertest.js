60ff27153fcd77a5510257d4759601baff2e0e4d
Gavin Dodd <gavindodd@google.com>
2021-05-28 14:45:10 +0000

    shimless: Add wait for manual HWWP disable page
    http://screen/66DVUXWqFmMxe7x
    
    Bug: 1198187
    Test: Run local build
          browser_tests --gtest_filter=Shimless*
    
    Change-Id: Ib8d26099cf545b6450160c59899415c40766e198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906702
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887504}

--- chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_browsertest.js
+++ chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_browsertest.js
@@ -44,6 +44,7 @@ const debug_suites_list = [
   'ShimlessRMAAppTest',
   'OnboardingChooseWpDisableMethodPageTest',
   'OnboardingUpdatePageTest',
+  'OnboardingWaitForManualWpDisablePageTest',
 ];
 
 TEST_F('ShimlessRMABrowserTest', 'All', function() {
