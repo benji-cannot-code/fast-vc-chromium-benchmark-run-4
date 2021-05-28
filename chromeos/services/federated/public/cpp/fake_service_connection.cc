faa8dc820fc9be055ba568a059feaf150de7a347
alanlxl <alanlxl@chromium.org>
2021-05-28 04:06:42 +0000

    Federated Service: Add a thin client library
    This CL adds a very simple client library
    for client code in Chromium to report examples to Federated service.
    
    Bug: 1125906
    Test: run unit tests; manually tested on DUT by making Chromium trigger
    Test: a ReportExample every time it does a Smart Dim inference, then
    Test: check whether the Federated service daemon receives an example.
    
    Change-Id: I8c5ad04b29f980ec7fa07324683d2bdec40267e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2397130
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887443}

---
+++ chromeos/services/federated/public/cpp/fake_service_connection.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/federated/public/cpp/fake_service_connection.h"
+
+namespace chromeos {
+namespace federated {
+
+FakeServiceConnectionImpl::FakeServiceConnectionImpl() = default;
+FakeServiceConnectionImpl::~FakeServiceConnectionImpl() = default;
+
+void FakeServiceConnectionImpl::BindReceiver(
+    mojo::PendingReceiver<mojom::FederatedService> receiver) {
+  Clone(std::move(receiver));
+}
+
+void FakeServiceConnectionImpl::Clone(
+    mojo::PendingReceiver<mojom::FederatedService> receiver) {
+  receivers_.Add(this, std::move(receiver));
+}
+
+void FakeServiceConnectionImpl::ReportExample(const std::string& client_name,
+                                              mojom::ExamplePtr example) {
+  LOG(INFO) << "In FakeServiceConnectionImpl::ReportExample, does nothing";
+  return;
+}
+
+}  // namespace federated
+}  // namespace chromeos
