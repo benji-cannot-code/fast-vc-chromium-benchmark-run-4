9912dfe3cce82cb5f2a66526a431e1811ee14b52
Christoph Schwering <schwering@google.com>
2021-05-28 14:30:53 +0000

    [Autofill] Fixed special case of autocomplete preventing address import.
    This CL is fixes that the autocomplete attribute may prevent form
    import. This temporary fix only addresses one specific field. The
    general fix will follow.
    
    Bug: 1213301
    Change-Id: Iad326e1d2fb026f5c80a6bb1b6cb93f60cf6abaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923843
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#887503}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -92,6 +92,8 @@ extern const base::Feature kAutofillExtractAllDatalists;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillFixFillableFieldTypes;
 COMPONENT_EXPORT(AUTOFILL)
+extern const base::Feature kAutofillIgnoreAutocompleteForImport;
+COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillServerTypeTakesPrecedence;
 COMPONENT_EXPORT(AUTOFILL)
 extern const base::Feature kAutofillRefillWithRendererIds;
