a922378e72e03ce824bfa6f9a928c7b9e62c1045
Andrey Kosyakov <caseq@chromium.org>
2021-05-28 05:22:20 +0000

    Fix a UAF in RenderFrameDevToolsAgentHost
    Originally regressed by https://crrev.com/c/2826852
    
    Bug: 1211326
    Change-Id: I6f639862ea25f5d7ef745864c38a0f1e96dbb3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920730
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887464}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/reload-with-oopifs-crash.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+      `Tests that reload of an OOPIF page doesn't cause a crash`);
+
+  dp.Target.setAutoAttach({autoAttach: true, flatten: true, waitForDebuggerOnStart: false});
+  dp.Network.enable();
+
+  dp.Page.navigate({url: 'http://localhost:8000/inspector-protocol/resources/iframe-navigation.html'});
+
+  const oopifRequests = new Set();
+  dp.Network.onRequestWillBeSent(event => {
+    const params = event.params;
+    if (/oopif/.test(params.request.url))
+      oopifRequests.add(params.requestId);
+  });
+  dp.Network.onLoadingFinished(event => {
+    if (!oopifRequests.has(event.params.requestId))
+      return;
+    // Site isolation disabled, nothing to test here, bail out.
+    testRunner.log('PASSED: alive and kicking!');
+    testRunner.completeTest();
+  });
+  const attachedEvent = await dp.Target.onceAttachedToTarget();
+  session.evaluate(`location.reload()`);
+  await dp.Target.onceDetachedFromTarget();
+  await dp.Target.onceAttachedToTarget();
+
+  testRunner.log('PASSED: alive and kicking!');
+  testRunner.completeTest();
+})
