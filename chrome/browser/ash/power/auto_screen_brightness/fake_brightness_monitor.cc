1e8671d18de6237adff8cd821457c64acfbb6802
Yeunjoo Choi <ychoi@igalia.com>
2021-05-31 05:14:03 +0000

    Move //chrome/browser/{chromeos => ash}/power [1/3]
    auto_screen_brightness/ is moved to //chrome/browser/ash/power.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Change-Id: I25bf51cd294d96d5393e825fb9c475926487f024
    Bug: 1164001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919255
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887647}

---
+++ chrome/browser/ash/power/auto_screen_brightness/fake_brightness_monitor.cc
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/power/auto_screen_brightness/fake_brightness_monitor.h"
+
+namespace chromeos {
+namespace power {
+namespace auto_screen_brightness {
+
+FakeBrightnessMonitor::FakeBrightnessMonitor() {}
+
+FakeBrightnessMonitor::~FakeBrightnessMonitor() = default;
+
+void FakeBrightnessMonitor::ReportBrightnessMonitorInitialized() const {
+  DCHECK_NE(brightness_monitor_status_, Status::kInitializing);
+  const bool success = brightness_monitor_status_ == Status::kSuccess;
+  for (auto& observer : observers_)
+    observer.OnBrightnessMonitorInitialized(success);
+}
+
+void FakeBrightnessMonitor::ReportUserBrightnessChanged(
+    const double old_brightness_percent,
+    const double new_brightness_percent) const {
+  for (auto& observer : observers_)
+    observer.OnUserBrightnessChanged(old_brightness_percent,
+                                     new_brightness_percent);
+}
+
+void FakeBrightnessMonitor::ReportUserBrightnessChangeRequested() const {
+  for (auto& observer : observers_)
+    observer.OnUserBrightnessChangeRequested();
+}
+
+void FakeBrightnessMonitor::AddObserver(Observer* const observer) {
+  DCHECK(observer);
+  observers_.AddObserver(observer);
+  if (brightness_monitor_status_ != Status::kInitializing) {
+    observer->OnBrightnessMonitorInitialized(brightness_monitor_status_ ==
+                                             Status::kSuccess);
+  }
+}
+
+void FakeBrightnessMonitor::RemoveObserver(Observer* const observer) {
+  DCHECK(observer);
+  observers_.RemoveObserver(observer);
+}
+
+}  // namespace auto_screen_brightness
+}  // namespace power
+}  // namespace chromeos
