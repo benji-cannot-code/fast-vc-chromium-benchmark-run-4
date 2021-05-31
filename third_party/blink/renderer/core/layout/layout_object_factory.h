fe5deb5f8ab555fb57c6921d07210839c3f428ae
Yoshifuim Inoue <yosin@chromium.org>
2021-05-31 00:17:43 +0000

    [LayoutNG] Introduce LayoutNGCounter
    This patch introduces |LayoutNGCounter| class to make CSS "counter" content
    can be a part of text-combine run for preparation of implementing
    text-combine in LayoutNG[1].
    
    See also the CL[2] which introduces LayoutNGWordBreak.
    
    [1] http://crrev.com/c/2862384 Implement text-combine-upright:all in
    LayoutNG
    [2] http://crrev.com/c/2901680 Introduce LayoutNGWordBreak
    
    Bug: 1060007, 1112923
    Change-Id: Id45f73707d2144581c72b5985a4856bdda159d7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2925678
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887627}

--- third_party/blink/renderer/core/layout/layout_object_factory.h
+++ third_party/blink/renderer/core/layout/layout_object_factory.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ComputedStyle;
+class CounterContentData;
 class HTMLElement;
 class LayoutBlock;
 class LayoutBlockFlow;
@@ -25,6 +26,7 @@ class LayoutTableCaption;
 class LayoutText;
 class LayoutTextFragment;
 class Node;
+class PseudoElement;
 
 // Helper class for creation of certain LayoutObject-derived objects that may
 // need to be of different types, depending on whether or not LayoutNG is to be
@@ -71,6 +73,9 @@ class LayoutObjectFactory {
   static LayoutObject* CreateButton(Node& node,
                                     const ComputedStyle& style,
                                     LegacyLayout legacy);
+  static LayoutObject* CreateCounter(PseudoElement& pseduo,
+                                     const CounterContentData& counter,
+                                     LegacyLayout legacy);
   static LayoutBlock* CreateFieldset(Node&, const ComputedStyle&, LegacyLayout);
   static LayoutBlockFlow* CreateFileUploadControl(Node& node,
                                                   const ComputedStyle& style,
