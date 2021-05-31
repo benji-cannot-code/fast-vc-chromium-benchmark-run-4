3ee40156b0d9c03924f3ca2dc9479f202fddfd5d
Jeroen Dhollander <jeroendh@google.com>
2021-05-31 14:48:25 +0000

    Support ECDSA certificates in selectClientCertificates
    Bug: chromium:1153366
    Fixed: chromium:1153366
    Test: browser_tests --gtest_filter="UnmanagedPlatformKey*"
    Change-Id: Ie05a6662c2890e57d86dba9d1f1d3a4d40ff66ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919681
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Reviewed-by: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/master@{#887755}

--- chrome/test/data/extensions/api_test/platform_keys/create_test_certs.sh
+++ chrome/test/data/extensions/api_test/platform_keys/create_test_certs.sh
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Generates the following trees of certificates:
 #
 #     client_root (self-signed root)
-#     \   \
-#      \   \--> client_1_ca --> client_1 (end-entity)
-#       \
-#        \----> client_2_ca --> client_2 (end-entity)
+#     \   \   \
+#      \   \   \-> client_1_ca --> client_1 (end-entity)
+#       \   \
+#        \   \---> client_2_ca --> client_2 (end-entity)
+#         \
+#          \-----> client_3_ca --> client_3 (end-entity)
 #
 #     root (self-signed root)
 #     \   \
@@ -54,13 +56,20 @@ echo 1000 > out/client_root-serial
 
 touch out/client_root-index.txt
 
+# Generate CA keys
+try openssl genrsa -out "out/client_1_ca.key" 2048
+try openssl genrsa -out "out/client_2_ca.key" 2048
+try openssl ecparam -out out/client_3_ca.key -name prime256v1 -genkey
 
-for i in 1 2
+# Generate client keys
+try openssl genrsa -out "out/client_1.key" 2048
+try openssl genrsa -out "out/client_2.key" 2048
+try openssl ecparam -out out/client_3.key -name prime256v1 -genkey
+
+for i in 1 3
 do
 
-  # Create client_{1,2}_ca
-
-  try openssl genrsa -out "out/client_${i}_ca.key" 2048
+  # Create client_{1,2,3}_ca
 
   COMMON_NAME="Client Cert ${i} CA" \
     ID="client_${i}_ca" \
@@ -84,9 +93,7 @@ do
   touch "out/client_${i}_ca-index.txt"
 
 
-  # Create client_{1,2}
-
-  try openssl genrsa -out "out/client_${i}.key" 2048
+  # Create client_{1,2,3}
 
   COMMON_NAME="Client Cert ${i}" \
     ID="client_${i}" \
@@ -190,6 +197,5 @@ CA_ID=root CN=l1_interm \
 
 CA_ID=l1_interm CN=l2_leaf SAN="DNS:${CN}"\
   try issue_cert l2_leaf leaf_cert_san as_der
-
 
 try rm -rf out
