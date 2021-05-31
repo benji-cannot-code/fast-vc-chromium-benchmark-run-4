81788fec3112d740691439fa9e4dccaa4f83c6c7
Colin Blundell <blundell@chromium.org>
2021-05-31 11:23:31 +0000

    [gn check] Enable //chrome/test:xr_browser_tests_common
    Entails the following:
    
    - Moving a file that is required by this target from the
      xr_browser_tests_vr_required to this target itself; that file does not
      actually require that VR be enabled.
    - Splitting a browsertest into two parts, one of which stays in
      xr_browser_tests_common and one of which moves to
      xr_browser_tests_vr_required. The part that moves has a dependence on
      mock_xr_device_hook_base.h, which can actually only be built when
      VR is enabled (it depends on //content test hooks that are built only
      when VR is enabled).
    
    Bug: 1158929
    Change-Id: Ic5ce4bbe059fc89326e10b0314f2437ab5eb406f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2915717
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887715}

--- chrome/browser/vr/webxr_vr_transition_browser_test.cc
+++ chrome/browser/vr/webxr_vr_transition_browser_test.cc
@@ -3,10 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/build_config.h"
+#include "chrome/browser/vr/test/mock_xr_device_hook_base.h"
 #include "chrome/browser/vr/test/multi_class_browser_test.h"
 #include "chrome/browser/vr/test/webxr_vr_browser_test.h"
-#include "content/public/test/browser_test_utils.h"
 #include "device/vr/buildflags/buildflags.h"
 
 // Browser test equivalent of
@@ -24,18 +23,6 @@ void TestApiDisabledWithoutFlagSetImpl(WebXrVrBrowserTestBase* t,
   t->WaitOnJavaScriptStep();
   t->EndTest();
 }
-
-// Tests that WebXR does not return any devices if all runtime support is
-// disabled.
-IN_PROC_BROWSER_TEST_F(WebXrVrRuntimelessBrowserTest,
-                       TestWebXrNoDevicesWithoutRuntime) {
-  LoadFileAndAwaitInitialization("test_webxr_does_not_return_device");
-  WaitOnJavaScriptStep();
-  EndTest();
-}
-
-// Windows-specific tests.
-#ifdef OS_WIN
 
 #if BUILDFLAG(ENABLE_OPENXR)
 IN_PROC_MULTI_CLASS_BROWSER_TEST_F1(WebXrVrOpenXrBrowserTestWebXrDisabled,
@@ -155,7 +142,5 @@ IN_PROC_BROWSER_TEST_F(WebXrVrOpenXrBrowserTest, TestVisibilityChanged) {
   this->EndTest();
 }
 #endif  // BUILDFLAG(ENABLE_OPENXR)
-
-#endif  // OS_WIN
 
 }  // namespace vr
