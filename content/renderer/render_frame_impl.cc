caaf5197ffc9e3a06932560282a26a38c9ac1fde
Dave Tapuska <dtapuska@chromium.org>
2021-05-31 17:34:17 +0000

    Move PropagatePageZoomToNewlyAttachedFrame out of RenderViewImpl.
    This method really didn't need to be on RenderViewImpl and could be
    implemented inside RenderFrameImpl as a helper function.
    
    BUG=1155202
    
    Change-Id: I54889300a80cbad6ac6e98aed9b5d141a499b6ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911616
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887789}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -1106,6 +1106,14 @@ perfetto::protos::pbzero::FrameDeleteIntention FrameDeleteIntentionToProto(
   return ProtoLevel::FRAME_DELETE_INTENTION_NOT_MAIN_FRAME;
 }
 
+void PropagatePageZoomToNewlyAttachedFrame(blink::WebView* web_view,
+                                           float device_scale_factor) {
+  if (RenderThread::Get()->IsUseZoomForDSF())
+    web_view->SetZoomFactorForDeviceScaleFactor(device_scale_factor);
+  else
+    web_view->SetZoomLevel(web_view->ZoomLevel());
+}
+
 }  // namespace
 
 RenderFrameImpl::AssertNavigationCommits::AssertNavigationCommits(
@@ -4599,8 +4607,8 @@ void RenderFrameImpl::UpdateStateForCommit(
     // And when UseZoomForDSF is disabled, in content_browsertests:
     //     IFrameZoomBrowserTest.SubframesDontZoomIndependently (and the whole
     //     suite).
-    render_view_->PropagatePageZoomToNewlyAttachedFrame(
-        RenderThread::Get()->IsUseZoomForDSF(),
+    PropagatePageZoomToNewlyAttachedFrame(
+        GetWebView(),
         GetLocalRootWebFrameWidget()->GetScreenInfo().device_scale_factor);
   }
 
