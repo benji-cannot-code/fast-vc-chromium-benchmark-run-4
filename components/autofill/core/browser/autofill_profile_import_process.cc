e84c90e28bb438238a286fec49c717b042e548ed
Matthias Körber <koerber@google.com>
2021-05-31 12:38:08 +0000

    [Autofill] Set modification date of new, updated and merged Profiles
    This CL correctly set the modification date of profiles that have been
    manipulated in the context of explicit save prompts.
    
    Change-Id: Ia2e60e0c365a55dd7c1ecb71ea1876918393aafc
    Bugs: 1214751, 1175693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928558
    Auto-Submit: Matthias Körber <koerber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#887737}

--- components/autofill/core/browser/autofill_profile_import_process.cc
+++ components/autofill/core/browser/autofill_profile_import_process.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/autofill_profile_comparator.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/common/autofill_clock.h"
 
 namespace autofill {
 
@@ -132,6 +133,7 @@ void ProfileImportProcess::DetermineProfileImportType() {
     }
     // If the profile changed but all settings-visible values are maintained,
     // the profile can be updated silently.
+    merged_profile.set_modification_date(AutofillClock::Now());
     updated_profiles_.emplace_back(merged_profile);
   }
 
@@ -165,6 +167,10 @@ void ProfileImportProcess::DetermineProfileImportType() {
                          ? AutofillProfileImportType::kSilentUpdate
                          : AutofillProfileImportType::kDuplicateImport;
     }
+  }
+
+  if (import_candidate_.has_value()) {
+    import_candidate_->set_modification_date(AutofillClock::Now());
   }
 
   // At this point, all existing profiles are either unchanged, updated and/or
@@ -240,6 +246,7 @@ void ProfileImportProcess::SetUserDecision(
       }
 
       edited_profile->FinalizeAfterImport();
+      edited_profile->set_modification_date(AutofillClock::Now());
       // The `edited_profile` has to have the same `guid` as the original import
       // candidate.
       DCHECK_EQ(import_candidate_.value().guid(), edited_profile->guid());
