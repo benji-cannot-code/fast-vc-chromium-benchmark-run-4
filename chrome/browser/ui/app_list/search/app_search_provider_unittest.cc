fad3b342621193be7dcf2e8280c7dc39958c0c25
David Bienvenu <davidbienvenu@chromium.org>
2021-05-31 14:20:50 +0000

    unit_tests: fix flakiness in AppSearchProviderCrostiniTest
    Tests in previous fixture were not calling TearDown() on arc_test(),
    leaving the DBusThreadManager initialized.
    
    Bug: 956719
    Change-Id: Id70d0c00b04fc83df72cf340ba95b1895629738e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2922868
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887751}

--- chrome/browser/ui/app_list/search/app_search_provider_unittest.cc
+++ chrome/browser/ui/app_list/search/app_search_provider_unittest.cc
@@ -348,6 +348,7 @@ TEST_F(AppSearchProviderTest, Basic) {
   result = RunQuery("app1");
   EXPECT_TRUE(result == "Packaged App 1,Fake App 1" ||
               result == "Fake App 1,Packaged App 1");
+  arc_test().TearDown();
 }
 
 TEST_F(AppSearchProviderTest, DisableAndEnable) {
@@ -418,6 +419,8 @@ TEST_F(AppSearchProviderTest, InstallUninstallArc) {
 
   // Let uninstall code to clean up.
   base::RunLoop().RunUntilIdle();
+
+  arc_test().TearDown();
 }
 
 TEST_F(AppSearchProviderTest, FetchRecommendations) {
@@ -720,6 +723,7 @@ TEST_F(AppSearchProviderTest, FilterDuplicate) {
 
   CreateSearch();
   EXPECT_EQ(kGmailExtensionName, RunQuery(kGmailQuery));
+  arc_test().TearDown();
 }
 
 TEST_F(AppSearchProviderTest, FetchInternalApp) {
@@ -1039,7 +1043,7 @@ class AppSearchProviderWithArcAppInstallType
 
 // TODO (879413): Enable this after resolving flakiness.
 TEST_P(AppSearchProviderWithArcAppInstallType,
-       DISABLED_InstallInernallyRanking) {
+       DISABLED_InstallInternallyRanking) {
   const bool default_app =
       GetParam() == TestArcAppInstallType::INSTALLED_BY_DEFAULT;
   if (default_app) {
@@ -1088,7 +1092,7 @@ TEST_P(AppSearchProviderWithArcAppInstallType,
 
   EXPECT_LT(normal_app->install_time, internal_app->install_time);
 
-  // Installed internally app has runking below other apps, even if it's install
+  // Installed internally app has runking below other apps, even if its install
   // time is later.
   CreateSearch();
   EXPECT_EQ(std::string(kRankingNormalAppName) + "," +
@@ -1102,6 +1106,7 @@ TEST_P(AppSearchProviderWithArcAppInstallType,
   EXPECT_EQ(std::string(kRankingInternalAppName) + "," +
                 std::string(kRankingNormalAppName),
             RunQuery(kRankingAppQuery));
+  arc_test().TearDown();
 }
 
 INSTANTIATE_TEST_SUITE_P(
