41cb9d3f8c2757a663e7c66607e44cd394f173c0
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-05-31 17:29:54 +0000

    [sync] Rename AllStatus to SyncStatusTracker
    No behavior is changed. Also:
    - Rename SyncStatus::sync_id to SyncStatus::cache_guid.
    - Remove SyncStatus::{num_entries_by_type,num_to_delete_entries_by_type}
    unused by chrome://sync-internals.
    - Move SyncStatus member initializations to header file, and remove
    redundant assignments in SyncStatusTracker::ctor().
    
    Bug: 1211421
    Change-Id: I65654456ec02bf96ddf4d80b24238a4bda832314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910916
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887782}

--- components/sync/engine/sync_status.cc
+++ components/sync/engine/sync_status.cc
@@ -7,29 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace syncer {
 
-SyncStatus::SyncStatus()
-    : notifications_enabled(false),
-      notifications_received(0),
-      encryption_conflicts(0),
-      hierarchy_conflicts(0),
-      server_conflicts(0),
-      committed_count(0),
-      syncing(false),
-      updates_received(0),
-      reflected_updates_received(0),
-      tombstone_updates_received(0),
-      num_commits_total(0),
-      num_local_overwrites_total(0),
-      num_server_overwrites_total(0),
-      cryptographer_can_encrypt(false),
-      crypto_has_pending_keys(false),
-      has_keystore_key(false),
-      passphrase_type(PassphraseType::kImplicitPassphrase),
-      num_entries_by_type(GetNumModelTypes(), 0),
-      num_to_delete_entries_by_type(GetNumModelTypes(), 0) {}
+SyncStatus::SyncStatus() = default;
 
 SyncStatus::SyncStatus(const SyncStatus& other) = default;
 
-SyncStatus::~SyncStatus() {}
+SyncStatus::~SyncStatus() = default;
 
 }  // namespace syncer
