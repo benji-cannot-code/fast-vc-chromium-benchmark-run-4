a389293dfd7adf5725f55ffe008455234f659bf9
Nicolás Peña Moreno <npm@chromium.org>
2021-05-31 17:33:13 +0000

    Fix UKM recording for NextJS detection
    Currently we only record when the NextJS framework is detected, which is
    not ideal because that means that we cannot distinguish between null and
    false: null means that we did not run the code recording the metric, and
    false means that we reached the code and NextJS was not detected. This
    CL fixes this.
    
    Bug: 1006274
    Change-Id: Id6996830a629e2ec4f873145726bacceb835573b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920768
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887787}

--- chrome/browser/page_load_metrics/observers/javascript_frameworks_ukm_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/javascript_frameworks_ukm_observer_browsertest.cc
@@ -97,7 +97,8 @@ IN_PROC_BROWSER_TEST_F(JavascriptFrameworksUkmObserverBrowserTest,
   ui_test_utils::NavigateToURL(browser(), url);
   waiter.Wait();
   CloseAllTabs();
-  ExpectMetricCountForUrl(url, "NextJSPageLoad", 0);
+  ExpectMetricCountForUrl(url, "NextJSPageLoad", 1);
+  ExpectMetricValueForUrl(url, "NextJSPageLoad", false);
 }
 
 IN_PROC_BROWSER_TEST_F(JavascriptFrameworksUkmObserverBrowserTest,
@@ -112,4 +113,5 @@ IN_PROC_BROWSER_TEST_F(JavascriptFrameworksUkmObserverBrowserTest,
   waiter.Wait();
   CloseAllTabs();
   ExpectMetricCountForUrl(url, "NextJSPageLoad", 1);
+  ExpectMetricValueForUrl(url, "NextJSPageLoad", true);
 }
