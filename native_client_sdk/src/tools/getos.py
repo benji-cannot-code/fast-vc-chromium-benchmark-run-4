0a42026bae724c677d4bbf2be930d021fc4fb3cc
Nico Weber <thakis@chromium.org>
2021-06-01 16:51:40 +0000

    Reland "build: Convert build/config/nacl/rules.gni to py3"
    This is a reland of 9b0ef8cae9ce6322bee0dfe82fd946f8d8965530
    See patch 1-patch set 2 for a determinism fix that locally helps with
    https://ci.chromium.org/ui/p/chromium/builders/ci/Deterministic%20Linux/30974/overview
    
    Original change's description:
    > build: Convert build/config/nacl/rules.gni to py3
    >
    > Bug: 1205627
    > Change-Id: If1dc022cf0d3e670fb6deb9c3a453bbc2e171166
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909886
    > Commit-Queue: Nico Weber <thakis@chromium.org>
    > Auto-Submit: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Sam Clegg <sbc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#886318}
    
    Bug: 1205627
    Change-Id: I6f14a91fcbf53b1f5b7204fdabc0df5502dc9cb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919608
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Sam Clegg <sbc@chromium.org>
    Reviewed-by: Sam Clegg <sbc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888011}

--- native_client_sdk/src/tools/getos.py
+++ native_client_sdk/src/tools/getos.py
@@ -11,6 +11,8 @@ the location of Chrome.  This is used, for example, to determine the correct
 Toolchain to invoke.
 """
 
+from __future__ import print_function
+
 import argparse
 import os
 import re
@@ -264,7 +266,7 @@ def main(args):
     out = platform
 
   if out:
-    print out
+    print(out)
   return 0
 
 
