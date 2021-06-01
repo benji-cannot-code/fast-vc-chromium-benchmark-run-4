cdcf264415f10401bff7425ae3c0deb146abbf69
Claude van der Merwe <cvandermerwe@google.com>
2021-06-01 18:30:54 +0000

    Revert "[Nearby] Rewrite stream parser for optimization and clarity."
    This reverts commit 38d67328788b1b17303b152f59b430fdd347340c.
    
    Reason for revert: crbug/1213694
    
    Original change's description:
    > [Nearby] Rewrite stream parser for optimization and clarity.
    >
    > Based on remoting/base/protobuf_http_stream_parser.h, this change
    > rewrites the stream parser to use GrowableIOBuffer,CodedInputStream,
    > and WireType for simplification and optimization.
    >
    > Manually verified using Nearby Share and Phone Hub, and confirmed
    > successful WebRTC upgrades.
    >
    > Bug: 1213569
    > Change-Id: Id2563e5485c5246634172cdba0c4560b81fadb5c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2859655
    > Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    > Reviewed-by: James Vecore <vecore@google.com>
    > Reviewed-by: Josh Nohle <nohle@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#886896}
    
    Bug: 1213569
    Change-Id: I23762f227bfb1015a10e3824f7c9f303201610c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929362
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Cr-Commit-Position: refs/heads/master@{#888050}

--- chrome/browser/nearby_sharing/instantmessaging/receive_messages_express_unittest.cc
+++ chrome/browser/nearby_sharing/instantmessaging/receive_messages_express_unittest.cc
@@ -122,7 +122,7 @@ class ReceiveMessagesExpressTest : public testing::Test {
   }
 
   std::string GetFastPathOnlyResponse() {
-    return BuildResponseProto({"message"}, /*include_fast_path_ready=*/true)
+    return BuildResponseProto({}, /*include_fast_path_ready=*/true)
         .SerializeAsString();
   }
 
@@ -250,6 +250,7 @@ TEST_F(ReceiveMessagesExpressTest, SuccessfulPartialResponse) {
 
 TEST_F(ReceiveMessagesExpressTest, StopPreventsPendingTransfer) {
   base::RunLoop run_loop;
+
   StartReceivingMessages(&run_loop, /*token_success=*/true);
 
   // Calls OnDataReceived() in ReceiveMessagesExpress.
