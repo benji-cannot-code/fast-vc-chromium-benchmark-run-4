adf4553f5e8caff61b6c9cf8f779944a6b9e6a9e
Andrew Paseltiner <apaseltiner@chromium.org>
2021-06-01 19:04:10 +0000

    [attribution_reporting] Update internals page with new naming
    We modify the header, the table titles and columns, and corresponding
    HTML and JS structures. Most significantly, "impression" is changed to
    "attribution source", and we remove mention of "conversions".
    
    Bug: 1208584
    Change-Id: Ia0e503b7c842f08fdb28ac2bb6e69e75142377da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2920850
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888071}

--- content/browser/conversions/conversion_internals_browsertest.cc
+++ content/browser/conversions/conversion_internals_browsertest.cc
@@ -90,8 +90,8 @@ IN_PROC_BROWSER_TEST_F(ConversionInternalsWebUiBrowserTest,
   // Execute script to ensure the page has loaded correctly, executing similarly
   // to ExecJsInWebUI().
   EXPECT_EQ(true, EvalJs(shell()->web_contents()->GetMainFrame(),
-                         "document.body.innerHTML.search('Conversion "
-                         "Measurement API Internals') >= 0;",
+                         "document.body.innerHTML.search('Attribution "
+                         "Reporting API Internals') >= 0;",
                          EXECUTE_SCRIPT_DEFAULT_OPTIONS, /*world_id=*/1));
 }
 
@@ -157,11 +157,11 @@ IN_PROC_BROWSER_TEST_F(
   OverrideWebUIConversionManager(&manager);
 
   std::string wait_script = R"(
-    let table = document.getElementById("impression-table-body");
+    let table = document.getElementById("source-table-body");
     let obs = new MutationObserver(() => {
       if (table.children.length === 1 &&
           table.children[0].children[0].innerText ===
-          "No active impressions.") {
+          "No active sources.") {
         document.title = $1;
       }
     });
@@ -193,7 +193,7 @@ IN_PROC_BROWSER_TEST_F(ConversionInternalsWebUiBrowserTest,
   OverrideWebUIConversionManager(&manager);
 
   std::string wait_script = R"(
-    let table = document.getElementById("impression-table-body");
+    let table = document.getElementById("source-table-body");
     let obs = new MutationObserver(() => {
       if (table.children.length === 2 &&
           table.children[0].children[0].innerText === $1 &&
