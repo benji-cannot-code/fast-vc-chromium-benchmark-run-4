c4a921441d7645156e1575ed76d463a0615d411b
John Lee <johntlee@chromium.org>
2021-06-01 20:53:50 +0000

    Settings WebUI: Turn security check rows into cr-actionable-rows
    Fixed: 1195330
    Change-Id: I7eecc2572c817096b8728bd35c59060aa95217fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929452
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888124}

--- chrome/browser/resources/settings/safety_check_page/safety_check_child.js
+++ chrome/browser/resources/settings/safety_check_page/safety_check_child.js
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * have in common. It is used by all safety check elements: parent, updates,
  * passwors, etc.
  */
+import 'chrome://resources/cr_elements/cr_actionable_row_style.m.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
@@ -68,6 +69,7 @@ Polymer({
       type: Boolean,
       value: false,
       reflectToAttribute: true,
+      observer: 'onRowClickableChanged_',
     },
 
     // Is the row directed to external link.
@@ -195,5 +197,11 @@ Polymer({
     return this.rowClickableIcon_ === 'cr:arrow-right' ?
         this.i18n('subpageArrowRoleDescription') :
         '';
+  },
+
+  /** @private */
+  onRowClickableChanged_() {
+    // For cr-actionable-row-style.
+    this.toggleAttribute('effectively-disabled_', !this.rowClickable);
   }
 });
