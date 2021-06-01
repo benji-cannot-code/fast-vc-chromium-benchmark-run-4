23e72ea21985742ae845bd7c8990f1525a6b7f66
Maksim Sisov <msisov@igalia.com>
2021-06-01 13:28:15 +0000

    ozone/wayland: do not create wl_buffers for staging pixmaps. 1/*
    https://crrev.com/c/1837614 introduced a concept of anonymous
    images, which SkiaRenderer might have created and committed. It
    was unknown which surface a pixmap belongs to.
    
    However, this was changed at some point and buffers that
    are to be committed (scheduled as overlays) seem to always be
    created for a specific widget (I checked out the old code when
    the above mentioned patch was submitted and verified that buffers that
    were created to be committed were created without a widget specified,
    but now they are always created for a specific widget).
    
    Thus, do not create buffers unnecessarily. This reduces the number of
    IPCs and  also the memory footprint. And also make it possible for my
    next CL to take a scale factor of a surface that a buffer is created
    for.
    
    Change-Id: Ifc490c4ac298c0c423ad9c5e8d36b6b22f10e5ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910203
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887935}

--- ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.h
+++ ui/ozone/platform/wayland/gpu/gbm_pixmap_wayland.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/linux/gbm_buffer.h"
 #include "ui/gfx/native_pixmap.h"
+#include "ui/gfx/native_widget_types.h"
 
 namespace ui {
 
@@ -28,16 +29,15 @@ class GbmPixmapWayland : public gfx::NativePixmap {
   // |visible_area_size| represents a 'visible size', i.e., a buffer
   // of size |size| may actually contain visible data only in the
   // subregion of size |visible_area_size|. If |visible_area_size| is
-  // not provided, |size| is used.
+  // not provided, |size| is used. If |widget| is provided, browser
+  // side wl_buffer is also created. Otherwise, this pixmap
+  // behaves as a staging pixmap and mustn't be scheduled as an overlay.
   bool InitializeBuffer(
+      gfx::AcceleratedWidget widget,
       gfx::Size size,
       gfx::BufferFormat format,
       gfx::BufferUsage usage,
       absl::optional<gfx::Size> visible_area_size = absl::nullopt);
-
-  // The widget that this pixmap backs can be assigned later. Can be assigned
-  // only once.
-  void SetAcceleratedWiget(gfx::AcceleratedWidget widget);
 
   // gfx::NativePixmap overrides:
   bool AreDmaBufFdsValid() const override;
