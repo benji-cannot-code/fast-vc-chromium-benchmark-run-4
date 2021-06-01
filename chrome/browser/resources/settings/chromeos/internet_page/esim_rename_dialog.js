eb91e1aa79acff11541e32526a411a2e524a24e0
Gordon Seto <gordonseto@google.com>
2021-06-01 23:42:00 +0000

    [CrOS Cellular] Add minimum input length to rename dialog.
    Disabled 'Done' button in <esim-rename-dialog> if the length of its
    input value is less than 1.
    
    Bug: 1214906
    Change-Id: I8e634dc621ecdceb58eda81e2eedf84b0a830e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928926
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#888207}

--- chrome/browser/resources/settings/chromeos/internet_page/esim_rename_dialog.js
+++ chrome/browser/resources/settings/chromeos/internet_page/esim_rename_dialog.js
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @type {number} */
 const MAX_INPUT_LENGTH = 20;
 
+/** @type {number} */
+const MIN_INPUT_LENGTH = 1;
+
 /** @type {RegExp} */
 const EMOJI_REGEX_EXP =
     /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/gi;
@@ -200,8 +203,22 @@ Polymer({
   },
 
   /**
+   * @param {boolean} isRenameInProgress
    * @param {string} esimProfileName
-   * @returns {string}
+   * @return {boolean}
+   * @private
+   */
+  isDoneButtonDisabled_(isRenameInProgress, esimProfileName) {
+    if (isRenameInProgress) {
+      return true;
+    }
+    return esimProfileName.length < MIN_INPUT_LENGTH;
+  },
+
+  /**
+   * @param {string} esimProfileName
+   * @return {string}
+   * @private
    */
   getDoneBtnA11yLabel_(esimProfileName) {
     return this.i18n('eSimRenameProfileDoneBtnA11yLabel', esimProfileName);
