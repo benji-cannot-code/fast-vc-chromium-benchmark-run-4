7eb5bd6ff7641bbd6e1a96d42d5f02cc8d91c021
Tony Herre <toprice@chromium.org>
2021-06-01 11:16:07 +0000

    Propagate Mic Permissions error to getUserMedia
    Propagate the AudioCapturerSource::ErrorCode received by an audio
    source to give the correct getUserMedia error. Concretely, throw a
    NotAllowedError with message "Permission denied by system".
    
    Tested locally on a Windows 10 device, webrtc gUM sample page, now throws a NotAllowedError as expected.
    
    Bug: 1019163
    Change-Id: Ide81b55423a30afd89c588294afc4c31bf73b430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917066
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887916}

--- third_party/blink/renderer/platform/mediastream/media_stream_audio_source.h
+++ third_party/blink/renderer/platform/mediastream/media_stream_audio_source.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "media/base/audio_capturer_source.h"
 #include "media/base/limits.h"
 #include "third_party/blink/public/platform/modules/mediastream/web_platform_media_stream_source.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_audio_deliverer.h"
@@ -128,6 +129,11 @@ class PLATFORM_EXPORT MediaStreamAudioSource
     return absl::nullopt;
   }
 
+  absl::optional<media::AudioCapturerSource::ErrorCode> ErrorCode() {
+    DCHECK(task_runner_->BelongsToCurrentThread());
+    return error_code_;
+  }
+
  protected:
   // Returns a new MediaStreamAudioTrack. |id| is the blink track's ID in UTF-8.
   // Subclasses may override this to provide an extended implementation.
@@ -170,7 +176,8 @@ class PLATFORM_EXPORT MediaStreamAudioSource
   // Called by subclasses when capture error occurs.
   // Note: This can be called on any thread, and will post a task to the main
   // thread to stop the source soon.
-  void StopSourceOnError(const std::string& why);
+  void StopSourceOnError(media::AudioCapturerSource::ErrorCode code,
+                         const std::string& why);
 
   // Sets muted state and notifies it to all registered tracks.
   void SetMutedState(bool state);
@@ -197,6 +204,15 @@ class PLATFORM_EXPORT MediaStreamAudioSource
 
   void LogMessage(const std::string& message);
 
+  void SetErrorCode(media::AudioCapturerSource::ErrorCode code) {
+    DCHECK(task_runner_->BelongsToCurrentThread());
+    error_code_ = code;
+  }
+
+  // The portion of StopSourceOnError processing carried out on the main thread.
+  void StopSourceOnErrorOnTaskRunner(
+      media::AudioCapturerSource::ErrorCode code);
+
   // True if the source of audio is a local device. False if the source is
   // remote (e.g., streamed-in from a server).
   const bool is_local_source_;
@@ -214,6 +230,9 @@ class PLATFORM_EXPORT MediaStreamAudioSource
   // could cause object graph or data flow changes are being called on the main
   // thread.
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+
+  // Code set if this source was closed due to an error.
+  absl::optional<media::AudioCapturerSource::ErrorCode> error_code_;
 
   // Provides weak pointers so that MediaStreamAudioTracks won't call
   // StopAudioDeliveryTo() if this instance dies first.
