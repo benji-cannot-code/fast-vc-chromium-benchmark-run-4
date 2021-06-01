2cec978b36f778dc486a5c79fb0c1a22c12ef349
Sigurdur Asgeirsson <siggi@chromium.org>
2021-06-01 18:29:43 +0000

    Retire ScopedObserver in /weblayer.
    ScopedObserver is being deprecated in favor of two new classes:
    - base::ScopedObservation for observers that only ever observe
      a single source.
    - base::ScopedMultiSourceObservation for observers that do or may
      observe more than a single source.
    
    Bug: 1145565
    Change-Id: I87744102a05e901c949749be3373af5577a2e0d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928737
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888048}

--- weblayer/browser/persistence/browser_persister.h
+++ weblayer/browser/persistence/browser_persister.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
+#include "base/scoped_multi_source_observation.h"
 #include "components/sessions/content/session_tab_helper_delegate.h"
 #include "components/sessions/core/command_storage_manager_delegate.h"
 #include "components/sessions/core/session_service_commands.h"
@@ -138,11 +138,11 @@ class BrowserPersister : public sessions::CommandStorageManagerDelegate,
 
   std::vector<uint8_t> crypto_key_;
 
-  ScopedObserver<TabImpl,
-                 TabImpl::DataObserver,
-                 &TabImpl::AddDataObserver,
-                 &TabImpl::RemoveDataObserver>
-      data_observer_{this};
+  base::ScopedMultiSourceObservation<TabImpl,
+                                     TabImpl::DataObserver,
+                                     &TabImpl::AddDataObserver,
+                                     &TabImpl::RemoveDataObserver>
+      data_observations_{this};
 
   // True while asynchronously reading the state to restore.
   bool is_restore_in_progress_ = true;
