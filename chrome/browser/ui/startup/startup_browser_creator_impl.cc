a4a271d8cfc96d44f48a950bb82837a46ad05035
Stanley Hon <stahon@microsoft.com>
2021-06-01 20:04:47 +0000

    Make WebAppProvider ready before restoring apps. Reenable app restores on windows and linux.
    Previously, on linux it was discovered apps being restored could be
    restored without the correct frame. This was caused by the
    WebAppProvider not being ready while the app was being restored.
    
    Turns out this is an issue on Windows also.
    
    This change also includes additional more comprehensive tests for
    AppSessionService that reside in startup_browser_creator_browsertest.
    
    This change also hardens the restore codepath against null session
    services. see crbug.com/1212840
    
    These tests are closely linked to this improvement.
    
    It's not possible to test this failure scenario, but the new test does
    test that the registry is ready before restores occur.
    
    Bug: 1199109, 1195509, 1212840
    Change-Id: Ibf4a9196f7e54008b21a76134b4dc6713a1cdd7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2849016
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Stanley Hon <stahon@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#888103}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -54,6 +54,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/startup_tab_provider.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/welcome/helpers.h"
+#include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -107,12 +109,10 @@ namespace {
 bool ShouldRestoreApps(bool is_post_restart) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   return true;
-#elif defined(OS_MAC) || defined(OS_LINUX) || defined(OS_WIN)
+#elif defined(OS_MAC)
   // TODO(stahon@microsoft.com)
   // Even when app restores are enabled on mac, don't actually restore apps
   // because they do not restore correctly. http://crbug.com/1194201
-  // On windows and linux, apps can be restored without the proper app frame,
-  // disabling restorations for now. http://crbug.com/1199109
   return false;
 #else
   return is_post_restart;
