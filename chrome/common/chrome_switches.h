985252f659ea7ca5523622056f989b9d19ca9e92
anthonyvd <anthonyvd@chromium.org>
2021-06-01 17:08:20 +0000

    Revert "[Capture Handle] Add browser tests"
    This reverts commit cfc668ecb55157167f5449399940b04cac48d16e.
    
    Reason for revert: Causing failures on some Mac bots: https://ci.chromium.org/p/chromium/builders/ci/Mac10.12%20Tests
    
    Original change's description:
    > [Capture Handle] Add browser tests
    >
    > Add browser tests coverage for Capture Handle.
    >
    > Bug: 1213866
    > Change-Id: If22bb65019bd029c74356977dc5db2015c021455
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919713
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Commit-Queue: Elad Alon <eladalon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#887951}
    
    Bug: 1213866
    Change-Id: I2b6143274e5eda1a99896583bfaa8b5368277285
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928023
    Auto-Submit: anthonyvd <anthonyvd@chromium.org>
    Owners-Override: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#888016}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -54,7 +54,6 @@ extern const char kAppsGalleryURL[];
 extern const char kAuthServerAllowlist[];
 extern const char kAutoOpenDevToolsForTabs[];
 extern const char kAutoSelectDesktopCaptureSource[];
-extern const char kAutoSelectTabCaptureSourceByTitle[];
 extern const char kCheckForUpdateIntervalSec[];
 extern const char kCipherSuiteBlacklist[];
 extern const char kCloudPrintFile[];
