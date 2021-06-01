a2a48635640ca8fbb770a6eda15ba4de799815d2
Dave Tapuska <dtapuska@chromium.org>
2021-06-01 20:28:52 +0000

    Change search box to use main frame routing ID from view routing ID.
    In anticipation of removing RenderView convert searchbox over to use
    the main frame routing ID.
    
    BUG=1155202
    
    Change-Id: Iaacb6dd0710c31b85bba009456762ecba173cb2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2930219
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888110}

--- chrome/renderer/searchbox/searchbox_extension.cc
+++ chrome/renderer/searchbox/searchbox_extension.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/chrome_object_extensions_utils.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
-#include "content/public/renderer/render_view.h"
 #include "gin/data_object_builder.h"
 #include "gin/handle.h"
 #include "gin/object_template_builder.h"
@@ -101,13 +100,14 @@ void Dispatch(blink::WebLocalFrame* frame, const blink::WebString& script) {
 v8::Local<v8::Object> GenerateMostVisitedItem(
     v8::Isolate* isolate,
     float device_pixel_ratio,
-    int render_view_id,
+    int render_frame_id,
     InstantRestrictedID restricted_id) {
   return gin::DataObjectBuilder(isolate)
       .Set("rid", restricted_id)
-      .Set("faviconUrl", base::StringPrintf(
-                             "chrome-search://favicon/size/16@%fx/%d/%d",
-                             device_pixel_ratio, render_view_id, restricted_id))
+      .Set("faviconUrl",
+           base::StringPrintf("chrome-search://favicon/size/16@%fx/%d/%d",
+                              device_pixel_ratio, render_frame_id,
+                              restricted_id))
       .Build();
 }
 
@@ -812,14 +812,13 @@ v8::Local<v8::Value> NewTabPageBindings::GetMostVisited(v8::Isolate* isolate) {
     return v8::Null(isolate);
 
   content::RenderFrame* render_frame = GetMainRenderFrameForCurrentContext();
-  content::RenderView* render_view = render_frame->GetRenderView();
 
   // This corresponds to "window.devicePixelRatio" in JavaScript.
   float zoom_factor =
       blink::PageZoomLevelToZoomFactor(render_frame->GetWebView()->ZoomLevel());
   float device_pixel_ratio = render_frame->GetDeviceScaleFactor() * zoom_factor;
 
-  int render_view_id = render_view->GetRoutingID();
+  int render_frame_id = render_frame->GetRoutingID();
 
   std::vector<InstantMostVisitedItemIDPair> instant_mv_items;
   search_box->GetMostVisitedItems(&instant_mv_items);
@@ -831,8 +830,8 @@ v8::Local<v8::Value> NewTabPageBindings::GetMostVisited(v8::Isolate* isolate) {
     v8_mv_items
         ->CreateDataProperty(
             context, i,
-            GenerateMostVisitedItem(isolate, device_pixel_ratio, render_view_id,
-                                    rid))
+            GenerateMostVisitedItem(isolate, device_pixel_ratio,
+                                    render_frame_id, rid))
         .Check();
   }
   return v8_mv_items;
@@ -944,9 +943,8 @@ v8::Local<v8::Value> NewTabPageBindings::GetMostVisitedItemData(
   if (!search_box->GetMostVisitedItemWithID(rid, &item))
     return v8::Null(isolate);
 
-  int render_view_id =
-      GetMainRenderFrameForCurrentContext()->GetRenderView()->GetRoutingID();
-  return GenerateMostVisitedItemData(isolate, render_view_id, rid, item);
+  int render_frame_id = GetMainRenderFrameForCurrentContext()->GetRoutingID();
+  return GenerateMostVisitedItemData(isolate, render_frame_id, rid, item);
 }
 
 // static
