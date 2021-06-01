2157303c5172e212a8a65b36bb80ddd232777129
Dave Tapuska <dtapuska@chromium.org>
2021-06-01 16:45:31 +0000

    Move the delayed frame sync update timer to RenderFrame proper.
    RenderViewImpl use to keep track of a set of frame IDs that needed
    to be synced. Change this to be a timer per frame as this avoids the
    bridging that happened between these two classes. Now RenderFrame is
    solely responsible for its debouncing of the messages.
    
    BUG=1155202
    
    Change-Id: I08eed2b7796ade26ad2a9de77dc6817a9f0bfe85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911615
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888006}

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/process/process_handle.h"
 #include "base/single_thread_task_runner.h"
+#include "base/timer/timer.h"
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
 #include "cc/input/browser_controls_state.h"
@@ -547,6 +548,7 @@ class CONTENT_EXPORT RenderFrameImpl
                                        bool is_synchronously_committed,
                                        bool is_history_api_navigation,
                                        bool is_client_redirect) override;
+  void WillFreezePage() override;
   void DidSetPageLifecycleState() override;
   void DidUpdateCurrentHistoryItem() override;
   base::UnguessableToken GetDevToolsFrameToken() override;
@@ -717,6 +719,10 @@ class CONTENT_EXPORT RenderFrameImpl
 
   url::Origin GetSecurityOriginOfTopFrame();
 
+  void set_send_content_state_immediately(bool value) {
+    send_content_state_immediately_ = value;
+  }
+
  protected:
   explicit RenderFrameImpl(CreateParams params);
 
@@ -736,6 +742,7 @@ class CONTENT_EXPORT RenderFrameImpl
                            TestOverlayRoutingTokenSendsLater);
   FRIEND_TEST_ALL_PREFIXES(RenderFrameImplTest,
                            TestOverlayRoutingTokenSendsNow);
+  FRIEND_TEST_ALL_PREFIXES(RenderFrameImplTest, SendUpdateCancelsPending);
 
   // Similar to base::AutoReset, but skips restoration of the original value if
   // |this| is already destroyed.
@@ -1051,6 +1058,10 @@ class CONTENT_EXPORT RenderFrameImpl
   void AddMessageToConsoleImpl(blink::mojom::ConsoleMessageLevel level,
                                const std::string& message,
                                bool discard_duplicates);
+
+  // Start a delayed timer to update the frame sync state to the browser.
+  // Debounces many updates in quick succession.
+  void StartDelayedSyncTimer();
 
   // Stores the WebLocalFrame we are associated with.  This is null from the
   // constructor until BindToFrame() is called, and it is null after
@@ -1396,6 +1407,14 @@ class CONTENT_EXPORT RenderFrameImpl
 
   NavigationCommitState navigation_commit_state_ =
       NavigationCommitState::kInitialEmptyDocument;
+
+  // Timer used to delay the updating of frame state.
+  base::OneShotTimer delayed_state_sync_timer_;
+
+  // Whether content state (such as form state, scroll position and page
+  // contents) should be sent to the browser immediately. This is normally
+  // false, but set to true by some tests.
+  bool send_content_state_immediately_ = false;
 
   base::WeakPtrFactory<RenderFrameImpl> weak_factory_{this};
 
