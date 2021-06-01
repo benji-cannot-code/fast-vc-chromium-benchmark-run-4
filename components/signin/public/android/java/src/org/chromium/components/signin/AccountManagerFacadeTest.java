4f40da7351d3a5a8ceb62d41d7f237604341833c
Alice Wang <aliceywang@chromium.org>
2021-06-01 09:06:14 +0000

    [Android] Remove redundant method AccountManagerFacade#isCachePopulated()
    This CL removes the redundant method isCachePopulated() from
    AccountManagerFacade API as this method is no longer needed.
    
    Bug: 1211294
    Change-Id: I960232a81d2db4d4cf50ab12948b15a3977e7625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927307
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#887892}

--- components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeTest.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeTest.java
@@ -77,7 +77,7 @@ public class AccountManagerFacadeTest {
     @SmallTest
     public void testIsCachePopulated() throws InterruptedException {
         // Cache shouldn't be populated until getAccountsSync is unblocked.
-        assertFalse(AccountManagerFacadeProvider.getInstance().isCachePopulated());
+        assertFalse(AccountManagerFacadeProvider.getInstance().getGoogleAccounts().isPresent());
 
         mDelegate.unblockGetAccounts();
         CountDownLatch countDownLatch = new CountDownLatch(1);
@@ -87,7 +87,7 @@ public class AccountManagerFacadeTest {
         });
         // Wait for cache population to finish.
         countDownLatch.await();
-        assertTrue(AccountManagerFacadeProvider.getInstance().isCachePopulated());
+        assertTrue(AccountManagerFacadeProvider.getInstance().getGoogleAccounts().isPresent());
     }
 
     @Test
