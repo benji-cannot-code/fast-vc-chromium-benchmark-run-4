6b17dc2b0180c6121d4be78a2a5efe201c81b983
Yuki Shiino <yukishiino@chromium.org>
2021-06-02 00:40:33 +0000

    push_event: Respect ArrayBufferView's offset and size
    The current implementation ignores ArrayBufferView's offset
    and size and it's simply wrong.  This patch fixes the bug
    and respects the offset and size.
    
    Change-Id: I4c9c5213fd0ecbc9e108585c6d898b8757807042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2925675
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888226}

--- third_party/blink/renderer/modules/push_messaging/push_message_data.cc
+++ third_party/blink/renderer/modules/push_messaging/push_message_data.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/array_buffer_or_array_buffer_view_or_usv_string.h"
 #include "third_party/blink/renderer/core/fileapi/blob.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_array_buffer.h"
+#include "third_party/blink/renderer/core/typed_arrays/dom_array_piece.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/blob/blob_data.h"
@@ -32,14 +33,18 @@ PushMessageData* PushMessageData::Create(const String& message_string) {
 
 PushMessageData* PushMessageData::Create(
     const ArrayBufferOrArrayBufferViewOrUSVString& message_data) {
-  if (message_data.IsArrayBuffer() || message_data.IsArrayBufferView()) {
-    DOMArrayBuffer* buffer = message_data.IsArrayBufferView()
-                                 ? message_data.GetAsArrayBufferView()->buffer()
-                                 : message_data.GetAsArrayBuffer();
-
+  if (message_data.IsArrayBuffer()) {
+    DOMArrayBuffer* buffer = message_data.GetAsArrayBuffer();
     return MakeGarbageCollected<PushMessageData>(
         static_cast<const char*>(buffer->Data()),
         base::checked_cast<wtf_size_t>(buffer->ByteLength()));
+  }
+
+  if (message_data.IsArrayBufferView()) {
+    DOMArrayBufferView* buffer_view = message_data.GetAsArrayBufferView().Get();
+    return MakeGarbageCollected<PushMessageData>(
+        static_cast<const char*>(buffer_view->BaseAddress()),
+        base::checked_cast<wtf_size_t>(buffer_view->byteLength()));
   }
 
   if (message_data.IsUSVString()) {
