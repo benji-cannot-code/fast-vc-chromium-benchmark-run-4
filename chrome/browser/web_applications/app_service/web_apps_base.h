750208689b250ed6437501d69c3339baba27b53e
Eric Willigers <ericwilligers@chromium.org>
2021-06-02 02:39:16 +0000

    [Lacros] WebAppsPublisherHost supports Launch
    WebAppPublisherHost and WebAppsChromeOs now use shared code in
    WebAppPublisherHelper to launch web apps.
    
    
    Bug: 1194709
    Change-Id: Ibeecedc508bf8e530f591056088334446c76f465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2921801
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888257}

--- chrome/browser/web_applications/app_service/web_apps_base.h
+++ chrome/browser/web_applications/app_service/web_apps_base.h
@@ -27,14 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
-namespace apps {
-struct AppLaunchParams;
-}  // namespace apps
-
-namespace content {
-class WebContents;
-}
-
 namespace webapps {
 enum class WebappUninstallSource;
 }  // namespace webapps
@@ -42,7 +34,6 @@ enum class WebappUninstallSource;
 namespace web_app {
 
 class WebApp;
-class WebAppLaunchManager;
 class WebAppProvider;
 class WebAppRegistrar;
 
@@ -70,18 +61,6 @@ class WebAppsBase : public apps::PublisherBase,
   void OnWebAppLastLaunchTimeChanged(
       const std::string& app_id,
       const base::Time& last_launch_time) override;
-
-  apps::IconEffects GetIconEffects(const WebApp* web_app);
-
-  content::WebContents* LaunchAppWithIntentImpl(
-      const std::string& app_id,
-      int32_t event_flags,
-      apps::mojom::IntentPtr intent,
-      apps::mojom::LaunchSource launch_source,
-      int64_t display_id);
-
-  virtual content::WebContents* LaunchAppWithParams(
-      apps::AppLaunchParams params);
 
   const mojo::RemoteSet<apps::mojom::Subscriber>& subscribers() const {
     return subscribers_;
@@ -162,8 +141,6 @@ class WebAppsBase : public apps::PublisherBase,
       content_settings_observation_{this};
 
   WebAppProvider* provider_ = nullptr;
-
-  std::unique_ptr<WebAppLaunchManager> web_app_launch_manager_;
 
   // app_service_ is owned by the object that owns this object.
   apps::mojom::AppService* app_service_;
