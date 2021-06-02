1068960e1eddd123c38b7fa80f43122cfddc820f
Christopher Cameron <ccameron@chromium.org>
2021-06-02 17:05:01 +0000

    Add skia::BlitRGBAToYUVA
    Add a wrapper function that uses a SkColorFilter to apply the
    RGBA to YUVA conversion matrix, selecting out the appropriate
    planes.
    
    Leave the assumption of Rec601 baked in (because the code this is
    replacing has that assumption).
    
    Leave unimplemented (with a NOTREACHED) all formats except Y_UV,
    because they do not have tests coming yet.
    
    Bug: 1206168
    Change-Id: Ibe781233af17a645e18a92570b44b197d33e6d4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2924216
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888505}

---
+++ skia/ext/rgba_to_yuva.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SKIA_EXT_RGBA_TO_YUVA_H_
+#define SKIA_EXT_RGBA_TO_YUVA_H_
+
+#include "third_party/skia/include/core/SkImage.h"
+#include "third_party/skia/include/core/SkSurface.h"
+#include "third_party/skia/include/core/SkYUVAInfo.h"
+
+namespace skia {
+
+// Copy `src_image` from RGBA to the YUVA planes specified in `dst_surfaces`,
+// using the color space and plane configuration information specified in
+// `dst_yuva_info`.
+SK_API void BlitRGBAToYUVA(
+    sk_sp<SkImage> src_image,
+    const SkRect& src_rect,
+    sk_sp<SkSurface> dst_surfaces[SkYUVAInfo::kMaxPlanes],
+    const SkYUVAInfo& dst_yuva_info,
+    const SkRect& dst_rect);
+
+}  // namespace skia
+
+#endif  // SKIA_EXT_RGBA_TO_YUVA_H_
