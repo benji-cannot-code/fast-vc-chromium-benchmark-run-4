d78520ec5e018d1fe70c975fcb922f9b47a4db7b
Luciano Pacheco <lucmult@chromium.org>
2021-06-02 04:51:20 +0000

    Video Player: Stop auto-generating JS module
    Video Player has been running as JS module for a few milestones.
    
    Update the following files to be JS modules directly:
    * test_util.js
    * error_util.js
    * background.js
    * video_player_metrics.js
    * video_player_native_controls.js
    * video_player.js
    
    Bug: 1214953
    Change-Id: I66bbfc7f86b3c7d4672324e06b28dcbf5f83dcee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929406
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888300}

--- ui/file_manager/video_player/js/test_util.js
+++ ui/file_manager/video_player/js/test_util.js
@@ -3,10 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// clang-format off
-// #import {openVideoPlayerWindow} from './background.m.js';
-// #import {test} from 'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj/background/js/test_util_base.m.js';
-// clang-format on
+import {test} from 'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj/background/js/test_util_base.m.js';
+
+import {openVideoPlayerWindow} from './background.m.js';
 
 /**
  * Returns if a video element playing the specified file meet the condition
