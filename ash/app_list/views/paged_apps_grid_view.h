c1341259a413994edaccf7bb8e6a7ff5b07a8023
James Cook <jamescook@chromium.org>
2021-06-02 02:46:53 +0000

    chromeos: Move pagination observer methods to PagedAppsGridView
    AppsGridView is being split into two subclasses:
    - ScrollableAppsGridView for the new bubble
    - PagedAppsGridView for the existing launcher
    
    PaginationModelObserver methods exist to support paging. The
    ScrollableAppsGridView doesn't need this functionality, so move it
    to PagedAppsGridView.
    
    Several AppsGridView members and methods become protected as part of
    this change. Most of them will eventually move to PagedAppsGridView
    as well.
    
    The only non-mechnical changes are moving the PaginationModel
    AddObserver / RemoveObserver call timing, and replacing some
    references to selected_view_ with GetSelectedView().
    
    Bug: 1211608
    Test: ash_unittests including ASAN, manually flip pages on device
    Change-Id: I4a69b7d6b028f33a7b588ea6a527bf9daf103166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927462
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888271}

--- ash/app_list/views/paged_apps_grid_view.h
+++ ash/app_list/views/paged_apps_grid_view.h
@@ -6,8 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_APP_LIST_VIEWS_PAGED_APPS_GRID_VIEW_H_
 #define ASH_APP_LIST_VIEWS_PAGED_APPS_GRID_VIEW_H_
 
+#include <memory>
+
 #include "ash/app_list/views/apps_grid_view.h"
 #include "ash/ash_export.h"
+#include "ash/public/cpp/pagination/pagination_model_observer.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/compositor/throughput_tracker.h"
 #include "ui/events/types/event_type.h"
 #include "ui/gfx/geometry/point_f.h"
 
@@ -22,7 +27,8 @@ class ContentsView;
 // An apps grid that shows the apps on a series of fixed-size pages.
 // Used for the peeking/fullscreen launcher, home launcher and folders.
 // Created by and is a child of AppsContainerView.
-class ASH_EXPORT PagedAppsGridView : public AppsGridView {
+class ASH_EXPORT PagedAppsGridView : public AppsGridView,
+                                     public PaginationModelObserver {
  public:
   PagedAppsGridView(ContentsView* contents_view,
                     AppsGridViewFolderDelegate* folder_delegate);
@@ -48,6 +54,16 @@ class ASH_EXPORT PagedAppsGridView : public AppsGridView {
   gfx::Insets GetTilePadding() const override;
   gfx::Size GetTileGridSize() const override;
 
+  // PaginationModelObserver:
+  void TotalPagesChanged(int previous_page_count, int new_page_count) override;
+  void SelectedPageChanged(int old_selected, int new_selected) override;
+  void TransitionStarting() override;
+  void TransitionStarted() override;
+  void TransitionChanged() override;
+  void TransitionEnded() override;
+  void ScrollStarted() override;
+  void ScrollEnded() override;
+
  private:
   // Indicates whether the drag event (from the gesture or mouse) should be
   // handled by PagedAppsGridView.
@@ -68,6 +84,12 @@ class ASH_EXPORT PagedAppsGridView : public AppsGridView {
   // The last mouse drag location in root window coordinate. Used for
   // between-item drags that move the entire grid, not for app icon drags.
   gfx::PointF last_mouse_drag_point_;
+
+  // Records smoothness of pagination animation.
+  absl::optional<ui::ThroughputTracker> pagination_metrics_tracker_;
+
+  // Records the presentation time for apps grid dragging.
+  std::unique_ptr<PresentationTimeRecorder> presentation_time_recorder_;
 };
 
 }  // namespace ash
