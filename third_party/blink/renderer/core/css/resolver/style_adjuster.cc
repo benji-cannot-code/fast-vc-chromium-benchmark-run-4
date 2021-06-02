c21063f13f32c8b73c153aaab2c3991d73b3975b
Alison Maher <almaher@microsoft.com>
2021-06-02 19:43:02 +0000

    Color-scheme in forced colors mode
    The spec says that color-scheme should compute to 'light dark' in
    forced colors mode. Although we aren't currently doing this, forced
    colors mode currently overrides any of the effects of color-scheme,
    so this update shouldn't result in any real behavior change.
    
    Bug: 1213108
    Change-Id: I488d4846779a1c5fecc027ee53f969f8ea1825ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934277
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#888570}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -658,6 +658,7 @@ void StyleAdjuster::AdjustForForcedColorsMode(ComputedStyle& style) {
 
   style.SetTextShadow(ComputedStyleInitialValues::InitialTextShadow());
   style.SetBoxShadow(ComputedStyleInitialValues::InitialBoxShadow());
+  style.SetColorScheme({"light", "dark"});
   style.SetAccentColor(ComputedStyleInitialValues::InitialAccentColor());
   if (!style.HasUrlBackgroundImage())
     style.ClearBackgroundImage();
