0f883a6135b89290c7120b2d40c784ccb42cecbe
James Cook <jamescook@chromium.org>
2021-06-02 16:30:48 +0000

    chromeos: Refactor AppsGridView GetTileSize()
    Introduce a pure virtual AppsGridView::GetTileSize() method with
    different implementations for the existing paged apps grid and the
    new scrollable apps grid for the updated launcher.
    
    This will make it easier to migrate some existing code to
    PagedAppsGridView, in particular the Layout() method and some
    cardified state support.
    
    Bug: 1211608
    Test: ash_unittests, visual with cardified and regular views
    Change-Id: I091e7e369a5ebc64be122f98429217ad3f4cd4e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928641
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888490}

--- ash/app_list/views/apps_grid_view.h
+++ ash/app_list/views/apps_grid_view.h
@@ -110,9 +110,6 @@ class ASH_EXPORT AppsGridView : public views::View,
   // Returns the size of a tile view including its padding.
   gfx::Size GetTotalTileSize() const;
 
-  // Returns the padding around a tile view.
-  virtual gfx::Insets GetTilePadding() const = 0;
-
   // Returns the size of the entire tile grid with padding between tiles.
   gfx::Size GetTileGridSizeWithPadding() const;
 
@@ -361,6 +358,15 @@ class ASH_EXPORT AppsGridView : public views::View,
  protected:
   class FadeoutLayerDelegate;
 
+  // The cardified apps grid should be scaled down by this factor.
+  static constexpr float kCardifiedScale = 0.84f;
+
+  // Returns the size of a tile view excluding its padding.
+  virtual gfx::Size GetTileViewSize() const = 0;
+
+  // Returns the padding around a tile view.
+  virtual gfx::Insets GetTilePadding() const = 0;
+
   // Returns the size of the entire tile grid.
   virtual gfx::Size GetTileGridSize() const = 0;
 
@@ -408,6 +414,7 @@ class ASH_EXPORT AppsGridView : public views::View,
   int horizontal_tile_padding() const { return horizontal_tile_padding_; }
   const gfx::Point& last_drag_point() const { return last_drag_point_; }
   bool handling_keyboard_move() const { return handling_keyboard_move_; }
+  bool cardified_state() const { return cardified_state_; }
 
   // TODO(crbug.com/1211608): Move these member variables to PagedAppsGridView.
   PaginationModel pagination_model_{this};
@@ -918,6 +925,7 @@ class ASH_EXPORT AppsGridView : public views::View,
   bool ignore_layout_ = false;
 
   // True if the AppList is in cardified state.
+  // TODO(crbug.com/1211608): Move cardified state members to PagedAppsGridView.
   bool cardified_state_ = false;
 
   // The highlighted page during cardified state.
