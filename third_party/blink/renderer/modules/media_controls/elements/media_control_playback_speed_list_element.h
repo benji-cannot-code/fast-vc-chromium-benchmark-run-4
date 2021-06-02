cf20fe8c57d621e27d38abc33e0b0a020129ff30
François Beaufort <beaufort.francois@gmail.com>
2021-06-02 19:34:39 +0000

    Centers vertically the checked item in the playback speed list
    This CL makes sure the playback speed item that is checked in the
    playback speed list is centered vertically on the initial opening so
    users can see the "Normal" playback speed closer to the middle of the
    list by default.
    
    Change-Id: I65d46f5cdda9c7f47f1842df4a55ac2443381f35
    Bug: 1197485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919719
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#888558}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_playback_speed_list_element.h
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_playback_speed_list_element.h
@@ -23,7 +23,11 @@ class MediaControlPlaybackSpeedListElement final
 
   void SetIsWanted(bool) final;
 
+  void Trace(Visitor*) const override;
+
  private:
+  class RequestAnimationFrameCallback;
+
   void DefaultEventHandler(Event&) override;
 
   void RefreshPlaybackSpeedListMenu();
@@ -34,6 +38,11 @@ class MediaControlPlaybackSpeedListElement final
 
   // Creates the header element of the playback speed list.
   Element* CreatePlaybackSpeedHeaderItem();
+
+  // Centers vertically the checked item in the playback speed list.
+  void CenterCheckedItem();
+
+  Member<Element> checked_item_;
 };
 
 }  // namespace blink
