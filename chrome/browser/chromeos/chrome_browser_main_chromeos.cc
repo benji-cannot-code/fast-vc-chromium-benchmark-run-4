fe6e1b0de92add23d170b2c734ef6ddb56451b55
danielng <danielng@google.com>
2021-06-02 23:59:04 +0000

    borealis: Implementing DBUS service for VM disk management
    Implementing the DBUS service for VM disk management to be used for
    managing the Borealis disk. These are used to allow cicerone to request
    information about the disk and resize it (if valid).
    Full design can be found at go/borealis-disk.
    
    Bug: b/174592560
    Change-Id: Ib5f193dec0134fdeb44be1f592bd6c846afd62c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2862302
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Daniel Ng <danielng@google.com>
    Cr-Commit-Position: refs/heads/master@{#888677}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -113,6 +113,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/dbus/screen_lock_service_provider.h"
 #include "chrome/browser/chromeos/dbus/smb_fs_service_provider.h"
 #include "chrome/browser/chromeos/dbus/virtual_file_request_service_provider.h"
+#include "chrome/browser/chromeos/dbus/vm/vm_disk_management_service_provider.h"
 #include "chrome/browser/chromeos/dbus/vm/vm_permission_service_provider.h"
 #include "chrome/browser/chromeos/dbus/vm/vm_sk_forwarding_service_provider.h"
 #include "chrome/browser/chromeos/dbus/vm_applications_service_provider.h"
@@ -355,6 +356,13 @@ class DBusServices {
         CrosDBusService::CreateServiceProviderList(
             std::make_unique<VmApplicationsServiceProvider>()));
 
+    vm_disk_management_service_ = CrosDBusService::Create(
+        system_bus, vm_tools::disk_management::kVmDiskManagementServiceName,
+        dbus::ObjectPath(
+            vm_tools::disk_management::kVmDiskManagementServicePath),
+        CrosDBusService::CreateServiceProviderList(
+            std::make_unique<VmDiskManagementServiceProvider>()));
+
     vm_sk_forwarding_service_ = CrosDBusService::Create(
         system_bus, vm_tools::sk_forwarding::kVmSKForwardingServiceName,
         dbus::ObjectPath(vm_tools::sk_forwarding::kVmSKForwardingServicePath),
@@ -466,6 +474,7 @@ class DBusServices {
     component_updater_service_.reset();
     chrome_features_service_.reset();
     vm_applications_service_.reset();
+    vm_disk_management_service_.reset();
     vm_sk_forwarding_service_.reset();
     vm_permission_service_.reset();
     drive_file_stream_service_.reset();
@@ -495,6 +504,7 @@ class DBusServices {
   std::unique_ptr<CrosDBusService> component_updater_service_;
   std::unique_ptr<CrosDBusService> chrome_features_service_;
   std::unique_ptr<CrosDBusService> vm_applications_service_;
+  std::unique_ptr<CrosDBusService> vm_disk_management_service_;
   std::unique_ptr<CrosDBusService> vm_sk_forwarding_service_;
   std::unique_ptr<CrosDBusService> vm_permission_service_;
   std::unique_ptr<CrosDBusService> drive_file_stream_service_;
