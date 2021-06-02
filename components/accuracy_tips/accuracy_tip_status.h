1194bc2992a0f39f126e3ff451d0347ad9cb6bed
Christian Dullweber <dullweber@chromium.org>
2021-06-02 17:26:22 +0000

    AccuracyTips: Add new component and classes for AccuracyTips
    Add a KeyedService and a WebContentsObserver for accuracy tips.
    Add an interface for a UI class that can show a platform-specific
    dialog.
    
    Bug: 1210891
    Change-Id: I54ec4450cd718df3d872363191dcd24c0e1e9dab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2916997
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888510}

---
+++ components/accuracy_tips/accuracy_tip_status.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ACCURACY_TIPS_ACCURACY_TIP_STATUS_H_
+#define COMPONENTS_ACCURACY_TIPS_ACCURACY_TIP_STATUS_H_
+
+namespace accuracy_tips {
+
+// Represents the different results of the accuracy check.
+enum class AccuracyTipStatus {
+  // No accuracy information for the site.
+  kNone = 0,
+  // Site classified as being misleading.
+  kMisinformation = 1,
+};
+
+}  // namespace accuracy_tips
+
+#endif  // COMPONENTS_ACCURACY_TIPS_ACCURACY_TIP_STATUS_H_
---
+++ components/accuracy_tips/features.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ACCURACY_TIPS_FEATURES_H_
+#define COMPONENTS_ACCURACY_TIPS_FEATURES_H_
+
+#include "base/metrics/field_trial_params.h"
+
+namespace base {
+struct Feature;
+}  // namespace base
+
+namespace accuracy_tips {
+
+extern const base::Feature kAccuracyTipsFeature;
+extern const base::FeatureParam<std::string> kSampleUrl;
+
+}  // namespace accuracy_tips
+
+#endif  // COMPONENTS_ACCURACY_TIPS_FEATURES_H_
