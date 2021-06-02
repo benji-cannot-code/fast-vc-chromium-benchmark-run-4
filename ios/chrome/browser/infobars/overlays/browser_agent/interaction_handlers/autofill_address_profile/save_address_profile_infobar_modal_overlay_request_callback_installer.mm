9fde375642cee1b11dd7a28e0ef2c6347c33c680
Vidhan Jain <vidhanj@google.com>
2021-06-02 19:58:23 +0000

    Reland "[iOS][Autofill][SavePrompts] Send callback based on last action for Addresses"
    This is a reland of 90d506aafc817d9ad9699c6b538f271224bdcd64
    
    Original change's description:
    > [iOS][Autofill][SavePrompts] Send callback based on last action for Addresses
    >
    > Before this patch, kDeclined is sent with the
    > |address_profile_save_prompt_callback_| by default if the save/update
    > address prompt is declined or ignored by the user.
    > However, with this change, the following decision is sent along with the
    > callback:
    > 1. kIgnored is the Default.
    > 2. kMessageTimeout when the banner is ignored.
    > 3. kMessageDeclined when the banner is dismissed by swiping up.
    > 4. kAccepted if the save/update modal is accepted.
    > 5. kEditAccepted if the edit modal is accepted.
    > 6. kDeclined if the save/update modal was cancelled and the user
    > navigated away.
    > 7. kEditDeclined if the edit modal was cancelled and the user
    > navigated away.
    >
    > Bug: 1167062, 1215172
    > Change-Id: I5d5a87175a535ed26d092bad89b9474efcefa8ae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929187
    > Commit-Queue: Vidhan Jain <vidhanj@google.com>
    > Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#888197}
    
    Bug: 1167062
    Bug: 1215172
    Change-Id: Ic1099eb60601276388aaf10a85acb1d3b5247805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2932441
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888575}

--- ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_modal_overlay_request_callback_installer.mm
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/autofill_address_profile/save_address_profile_infobar_modal_overlay_request_callback_installer.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using autofill_address_profile_infobar_overlays::
     SaveAddressProfileModalRequestConfig;
 using save_address_profile_infobar_modal_responses::EditedProfileSaveAction;
+using save_address_profile_infobar_modal_responses::CancelViewAction;
 
 SaveAddressProfileInfobarModalOverlayRequestCallbackInstaller::
     SaveAddressProfileInfobarModalOverlayRequestCallbackInstaller(
@@ -48,8 +49,18 @@ void SaveAddressProfileInfobarModalOverlayRequestCallbackInstaller::
   }
 
   EditedProfileSaveAction* info = response->GetInfo<EditedProfileSaveAction>();
-  interaction_handler_->SaveEditedProfile(GetOverlayRequestInfobar(request),
-                                          info->profile_data());
+  interaction_handler_->SaveEditedProfile(infobar, info->profile_data());
+}
+
+void SaveAddressProfileInfobarModalOverlayRequestCallbackInstaller::
+    CancelModalCallback(OverlayRequest* request, OverlayResponse* response) {
+  InfoBarIOS* infobar = GetOverlayRequestInfobar(request);
+  if (!infobar) {
+    return;
+  }
+
+  CancelViewAction* info = response->GetInfo<CancelViewAction>();
+  interaction_handler_->CancelModal(infobar, info->edit_view_is_dismissed());
 }
 
 #pragma mark - OverlayRequestCallbackInstaller
@@ -66,4 +77,11 @@ void SaveAddressProfileInfobarModalOverlayRequestCallbackInstaller::
               SaveEditedProfileDetailsCallback,
           weak_factory_.GetWeakPtr(), request),
       EditedProfileSaveAction::ResponseSupport()));
+
+  manager->AddDispatchCallback(OverlayDispatchCallback(
+      base::BindRepeating(
+          &SaveAddressProfileInfobarModalOverlayRequestCallbackInstaller::
+              CancelModalCallback,
+          weak_factory_.GetWeakPtr(), request),
+      CancelViewAction::ResponseSupport()));
 }
