0897712e1f35cf6a948b727cfc4689a5e7c87651
Yoshifuim Inoue <yosin@chromium.org>
2021-06-02 07:06:04 +0000

    Mark generated content of <q> for legacy if needed
    This patch changes |QuoteContentData::CreateLayoutObject()| to take
    account |LegacyLayout| parameter |legacy| to mark layout object to
    legacy for ::after and ::before when |legacy == kForce| to avoid
    using |LayoutNGTextCombine| in legacy layout tree.
    
    Note: This is follow-up of the CL[1] which introduces
    |LayoutNGTextCombine|.
    
    [1] http://crrev.com/c/2927086: [LayoutNG] Introduces
    LayoutNGTextCombine for implementing text-combine-upright:all
    
    Bug: 1215026
    Change-Id: I53c68672b4620f85446425deed84ffd64b618970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2932278
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888338}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -1254,6 +1254,7 @@ class CORE_EXPORT Element : public ContainerNode, public Animatable {
       return;
     SetStyleShouldForceLegacyLayoutInternal(force);
   }
+  FRIEND_TEST_ALL_PREFIXES(LayoutNGTextCombineTest, LegacyQuote);
   void SetStyleShouldForceLegacyLayoutInternal(bool);
 
   // Return whether this element needs legacy layout because of a child.
