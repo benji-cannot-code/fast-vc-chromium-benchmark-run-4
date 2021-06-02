55620ccabee4d8993376a8e7addbfd456cd750cd
Moe Ahmadi <mahmadi@chromium.org>
2021-06-02 22:35:34 +0000

    [MV] Moves MV settings into MostVisitedSites
    Currently MV settings/preferences are registered and controlled by the
    InstantService which updates the MostVisitedSites when those preferences
    change in the NTP. MostVisitedSites has a partial knowledge of whether
    custom links are enabled via a state it keeps.
    
    In order to eliminate the dependency of the 1P NTP to the InstantService
    this change moves those prefs into the MostVisitedSites. In the future,
    this will allow the NTP to observe and set those preferences directly in
    MostVisitedSites without needing the InstantService as an intermediary.
    
    This change is not expected to make any changes to the logic around
    setting or using these preferences.
    
    Bug: 1084363
    
    Change-Id: Iad11039743b5126195d27a54dfb6358c2c3e82ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928292
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888643}

--- chrome/browser/search/instant_unittest_base.h
+++ chrome/browser/search/instant_unittest_base.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/search/instant_service.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
+#include "components/ntp_tiles/most_visited_sites.h"
 #include "components/search_engines/template_url_service.h"
 
 // This class provides an extension on top of BrowserWithTestWindowTest, and
@@ -24,6 +25,8 @@ class InstantUnitTestBase : public BrowserWithTestWindowTest {
 
   void SetUp() override;
   void TearDown() override;
+
+  ntp_tiles::MostVisitedSites* most_visited_sites();
 
   // Adds and sets the default search provider using the base_url.
   // The base_url should have the http[s]:// prefix and a trailing / after the
