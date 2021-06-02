864c1ade1562de453b571911a789fd25e46d81be
Robert Flack <flackr@chromium.org>
2021-06-02 22:32:33 +0000

    Implement simple overscroll stretch for android.
    Android 12 and higher have a new visual behavior for overscroll which
    stretches the scrolling content. This patch uses the elastic overscroll
    flag to enable this behavior on Android.
    
    Bug: 1213215
    Change-Id: I3d2cb406311e9ab895da9c5764e63dcf929ef0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917356
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888639}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2107,9 +2107,6 @@ extern const char kCloudPrintXpsDescription[];
 extern const char kD3D11VideoDecoderName[];
 extern const char kD3D11VideoDecoderDescription[];
 
-extern const char kElasticOverscrollWinName[];
-extern const char kElasticOverscrollWinDescription[];
-
 extern const char kEnableIncognitoShortcutOnDesktopName[];
 extern const char kEnableIncognitoShortcutOnDesktopDescription[];
 
@@ -3011,6 +3008,11 @@ extern const char kAutofillCreditCardUploadDescription[];
 extern const char kSendWebUIJavaScriptErrorReportsName[];
 extern const char kSendWebUIJavaScriptErrorReportsDescription[];
 #endif
+
+#if defined(OS_WIN) || defined(OS_ANDROID)
+extern const char kElasticOverscrollName[];
+extern const char kElasticOverscrollDescription[];
+#endif  // defined(OS_WIN) || defined(OS_ANDROID)
 
 #if defined(OS_WIN) || (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) || \
     defined(OS_MAC)
