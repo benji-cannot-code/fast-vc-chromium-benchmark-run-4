aca96eda3a6899a516db547bff1ad3463f7da8e4
François Degros <fdegros@chromium.org>
2021-06-02 02:30:22 +0000

    [ZipFileCreator] Close directory Mojo pipe on cancellation
    Closing the Mojo pipe to the Directory object causes the ZipFileCreator
    service to fail and stop quickly by preventing it from listing and
    opening files.
    
    It still doesn't stop the ZipFileCreator service immediately if it is
    busy compressing a big file. This will be addressed in a subsequent CL.
    
    BUG=chromium:912236
    TEST=browser_tests --gtest_filter="ZipFileCreatorTest*
    TEST=Manual tests
    
    Change-Id: Icd1a25ec6344d5e94a0c973c904265d76e69ba10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929422
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888241}

--- chrome/browser/chromeos/extensions/file_manager/private_api_misc.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_misc.cc
@@ -408,7 +408,6 @@ FileManagerPrivateInternalCancelZipFunction::Run() {
 
   // Tell the ZipFileCreator to stop.
   creator->Stop();
-  DCHECK_EQ(zip_creators->count(dest_file), 0);
 
   return RespondNow(NoArguments());
 }
