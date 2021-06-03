9903d0c3ede159e26fa89a08e82196481b670324
dpapad <dpapad@chromium.org>
2021-06-03 18:58:55 +0000

    Extensions: Migrate to TypeScript, part1.
    In this part migrating non Polymer files (11/40 total files). Also
    adding minimal definition files for chrome.management and
    chrome.runtime.lastError APIs.
    
    Remaining files will be addressed in follow up CL(s).
    
    Bug: 1189595
    Change-Id: If290f7e8c7b8fd117d04d28d3e466e8d84e7019d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2921036
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888953}

--- chrome/browser/resources/extensions/keyboard_shortcut_delegate.ts
+++ chrome/browser/resources/extensions/keyboard_shortcut_delegate.ts
@@ -3,10 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
-
-/** @interface */
-export class KeyboardShortcutDelegate {
+export interface KeyboardShortcutDelegate {
   /**
    * Called when shortcut capturing changes in order to suspend or re-enable
    * global shortcut handling. This is important so that the shortcuts aren't
@@ -14,23 +11,19 @@ export class KeyboardShortcutDelegate {
    * TODO(devlin): From very brief experimentation, it looks like preventing
    * the default handling on the event also does this. Investigate more in the
    * future.
-   * @param {boolean} isCapturing
    */
-  setShortcutHandlingSuspended(isCapturing) {}
+  setShortcutHandlingSuspended(isCapturing: boolean): void;
 
   /**
    * Updates an extension command's keybinding.
-   * @param {string} extensionId
-   * @param {string} commandName
-   * @param {string} keybinding
    */
-  updateExtensionCommandKeybinding(extensionId, commandName, keybinding) {}
+  updateExtensionCommandKeybinding(
+      extensionId: string, commandName: string, keybinding: string): void;
 
   /**
    * Updates an extension command's scope.
-   * @param {string} extensionId
-   * @param {string} commandName
-   * @param {chrome.developerPrivate.CommandScope} scope
    */
-  updateExtensionCommandScope(extensionId, commandName, scope) {}
+  updateExtensionCommandScope(
+      extensionId: string, commandName: string,
+      scope: chrome.developerPrivate.CommandScope): void;
 }
