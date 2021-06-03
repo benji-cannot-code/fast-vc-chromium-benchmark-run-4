0764b8c0d8231a168e1dca2f799e465167acdf6f
Maggie Cai <mxcai@google.com>
2021-06-03 04:42:58 +0000

    [Lacors] Keep Lacros alive when app service is running.
    This CL adds the ScopedKeepAlive pointer in app service to keep the
    Lacros browser running when app service is running.
    
    BUG=1144877
    Test: Manual test: Lacros will be running in background once app service
    proxy instantiated. Open Lacros browser, then turn off browser, the
    crosapi will be disconnected, then it will re-connected in the
    background.
    
    Change-Id: I220ae51a06bfa0114e65fe765519654a4fa2c69b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908381
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888745}

--- chrome/browser/apps/app_service/app_service_proxy_chromeos.h
+++ chrome/browser/apps/app_service/app_service_proxy_chromeos.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_base.h"
 #include "chrome/browser/apps/app_service/paused_apps.h"
+#include "chrome/browser/ash/crosapi/browser_manager.h"
 #include "components/services/app_service/public/cpp/instance_registry.h"
 #include "components/services/app_service/public/mojom/app_service.mojom.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
@@ -191,6 +192,10 @@ class AppServiceProxyChromeOs : public AppServiceProxyBase {
   UninstallDialogs uninstall_dialogs_;
 
   std::unique_ptr<AppPlatformMetricsService> app_platform_metrics_service_;
+
+  // App service require the Lacros Browser to keep alive for web apps.
+  // TODO(crbug.com/1174246): Support Lacros not keeping alive.
+  std::unique_ptr<crosapi::BrowserManager::ScopedKeepAlive> keep_alive_;
 
   base::WeakPtrFactory<AppServiceProxyChromeOs> weak_ptr_factory_{this};
 };
