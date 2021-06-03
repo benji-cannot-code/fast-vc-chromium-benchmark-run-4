df20e4f5a631857ee2af6cf0d27d7c8e135fcaae
Rohit Rao <rohitrao@chromium.org>
2021-06-03 16:51:25 +0000

    [ios] Provide an IO main loop in SignedInAccountsViewControllerTest.
    ShouldBePresentedForBrowserStateNecessary is failing on the waterfall
    and tryservers because a queued task requires an IO loop. Sidestep this
    DCHECK() by providing an IO loop in the test.
    
    BUG=None
    
    Change-Id: Ifefec207ecbd2a1dbad002a6c0cafa8a8a5c1a4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935119
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888904}

--- ios/chrome/browser/ui/authentication/signed_in_accounts_view_controller_unittest.mm
+++ ios/chrome/browser/ui/authentication/signed_in_accounts_view_controller_unittest.mm
@@ -55,7 +55,8 @@ class SignedInAccountsViewControllerTest : public BlockCleanupTest {
   }
 
  protected:
-  web::WebTaskEnvironment task_environment_;
+  web::WebTaskEnvironment task_environment_{
+      web::WebTaskEnvironment::IO_MAINLOOP};
   signin::IdentityTestEnvironment identity_test_env_;
   std::unique_ptr<TestChromeBrowserState> browser_state_;
 };
