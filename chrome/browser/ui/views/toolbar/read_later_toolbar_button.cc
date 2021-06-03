fa4da500f8b083c9ed099e905e1b7b072760f7ab
Peter Boström <pbos@chromium.org>
2021-06-03 15:15:14 +0000

    Trigger side panel on mouse press
    Also adds that this button triggers a menu to the accessibility tree.
    This was copied from ExtensionsToolbarButton.
    
    Bug: None
    Change-Id: Ie8ba0d465f0929c45e31b597ed8b10ddba66e875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935481
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888862}

--- chrome/browser/ui/views/toolbar/read_later_toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/read_later_toolbar_button.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/controls/button/button_controller.h"
 #include "ui/views/controls/webview/webview.h"
 
 namespace {
@@ -67,6 +69,9 @@ ReadLaterToolbarButton::ReadLaterToolbarButton(Browser* browser)
 
   SetVectorIcons(kSidePanelIcon, kSidePanelTouchIcon);
   SetTooltipText(l10n_util::GetStringUTF16(IDS_TOOLTIP_SIDE_PANEL_SHOW));
+  button_controller()->set_notify_action(
+      views::ButtonController::NotifyAction::kOnPress);
+  GetViewAccessibility().OverrideHasPopup(ax::mojom::HasPopup::kMenu);
 }
 
 ReadLaterToolbarButton::~ReadLaterToolbarButton() = default;
