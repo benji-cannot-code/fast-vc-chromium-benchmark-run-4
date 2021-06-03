cb1fcdff522ba3ff8125c2754fbd3348a7dc341f
K. Moon <kmoon@chromium.org>
2021-06-03 03:14:32 +0000

    [unseasoned-pdf] Add interceptor skeletons
    Creates a skeleton NavigationThrottle and URLLoaderRequestInterceptor as
    the first step in implementing the navigation/loader "dance" required to
    secure the in-process PDF plugin.
    
    The NavigationThrottle will be responsible for redirecting the "stream"
    URL to the actual origin, while the URLLoaderRequestInterceptor will be
    responsible for injecting the loader from the original PDF request.
    
    See design docs linked from crbug.com/1123621#c8 for additional details.
    
    Bug: 1123621
    Change-Id: I8837ff89cde86042b3b8bc249d965bcb7c680a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935056
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888726}

---
+++ components/pdf/browser/pdf_navigation_throttle.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/pdf/browser/pdf_navigation_throttle.h"
+
+#include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/navigation_throttle.h"
+
+namespace pdf {
+
+PdfNavigationThrottle::PdfNavigationThrottle(
+    content::NavigationHandle* navigation_handle)
+    : content::NavigationThrottle(navigation_handle) {}
+
+PdfNavigationThrottle::~PdfNavigationThrottle() = default;
+
+const char* PdfNavigationThrottle::GetNameForLogging() {
+  return "PdfNavigationThrottle";
+}
+
+}  // namespace pdf
