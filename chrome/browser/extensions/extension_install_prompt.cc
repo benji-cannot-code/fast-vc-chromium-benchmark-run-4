61e7a013da318cfbf3c09034d39d9fafe1f86611
Igor Ruvinov <igorruvinov@chromium.org>
2021-06-03 17:05:28 +0000

    Update extension request dialog UI
    Following UX feedback for the extension request dialog (refer to linked bug), the default button is changed to "Send" and the cancel button text is updated from "Close" to "Cancel".
    
    Screenshot: https://drive.google.com/file/d/1x_tPvhd4C1oudlC7pwWt42Cxk8P4052W/view
    
    Bug: 1084000
    Change-Id: Ifb57de0f6302689432def8cb2fbd86d9e8d8a513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934300
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888911}

--- chrome/browser/extensions/extension_install_prompt.cc
+++ chrome/browser/extensions/extension_install_prompt.cc
@@ -266,6 +266,7 @@ std::u16string ExtensionInstallPrompt::Prompt::GetAbortButtonLabel() const {
     case REMOTE_INSTALL_PROMPT:
     case REPAIR_PROMPT:
     case DELEGATED_PERMISSIONS_PROMPT:
+    case EXTENSION_REQUEST_PROMPT:
       id = IDS_CANCEL;
       break;
     case PERMISSIONS_PROMPT:
@@ -275,7 +276,6 @@ std::u16string ExtensionInstallPrompt::Prompt::GetAbortButtonLabel() const {
       id = IDS_EXTENSION_EXTERNAL_INSTALL_PROMPT_ABORT_BUTTON;
       break;
     case POST_INSTALL_PERMISSIONS_PROMPT:
-    case EXTENSION_REQUEST_PROMPT:
     case EXTENSION_PENDING_REQUEST_PROMPT:
       id = IDS_CLOSE;
       break;
