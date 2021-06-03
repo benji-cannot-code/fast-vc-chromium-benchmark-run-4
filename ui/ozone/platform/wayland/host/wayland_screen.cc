f89b438aa9773f9bb93577b9d67decdc51c8115a
Alexander Dunaev <adunaev@igalia.com>
2021-06-03 11:19:35 +0000

    [ozone/wayland] Integrated the Mutter IdleMonitor D-Bus service.
    Querying the idle state is specific to platform, and the Wayland
    platform lacks it.
    
    This CL introduces support for the org.gnome.mutter.IdleMonitor D-Bus
    service that is available in environments that use the Mutter compositor
    (i.e., are based on GNOME).
    
    Bug: 1098201
    Change-Id: I39200c1a1e8b37f36101852b6c867d3a2f4251a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917001
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#888810}

--- ui/ozone/platform/wayland/host/wayland_screen.cc
+++ ui/ozone/platform/wayland/host/wayland_screen.cc
@@ -25,6 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/host/wayland_cursor_position.h"
 #include "ui/ozone/platform/wayland/host/wayland_window.h"
 
+#if defined(USE_DBUS)
+#include "ui/ozone/platform/wayland/host/org_gnome_mutter_idle_monitor.h"
+#endif
+
 namespace ui {
 
 WaylandScreen::WaylandScreen(WaylandConnection* connection)
@@ -240,6 +244,24 @@ display::Display WaylandScreen::GetDisplayMatching(
       display::FindDisplayWithBiggestIntersection(display_list_.displays(),
                                                   match_rect);
   return display_matching ? *display_matching : GetPrimaryDisplay();
+}
+
+base::TimeDelta WaylandScreen::CalculateIdleTime() const {
+#if defined(USE_DBUS)
+  // Try the org.gnome.Mutter.IdleMonitor D-Bus service (Mutter).
+  if (!org_gnome_mutter_idle_monitor_)
+    org_gnome_mutter_idle_monitor_ =
+        std::make_unique<OrgGnomeMutterIdleMonitor>();
+  const auto idle_time = org_gnome_mutter_idle_monitor_->GetIdleTime();
+  if (idle_time)
+    return *idle_time;
+#endif  // defined(USE_DBUS)
+
+  // Try the org_kde_kwin_idle Wayland protocol extension (KWin).
+  NOTIMPLEMENTED_LOG_ONCE();
+
+  // No providers.  Return 0 which means the system never gets idle.
+  return base::TimeDelta::FromSeconds(0);
 }
 
 void WaylandScreen::AddObserver(display::DisplayObserver* observer) {
