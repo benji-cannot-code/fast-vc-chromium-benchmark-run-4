e35663d67bfb2f093888035b8636489ce8d6f926
Lingqi Chi <lingqi@chromium.org>
2021-06-03 06:40:10 +0000

    Prerender: Check candidates' schemes in SpeculationHostImpl
    For speculationrules, its parser filters out non-http rules, so
    renderer processes ignore these rules and do not send them to the
    browser side.
    This CL add a security check in SpeculationHostImpl, to ensure that all
    received candidates are http or https ones.
    
    test changes:
    - add more test data into SpeculationRuleSetTest.DropUnrecognizedRules.
    - add a unit test, SpeculationHostImplTest.ReportNonHttpMessage, to
    ensure the inspection works.
    - remove PrerenderBrowserTest.HttpToBlobUrl and BlobToBlobUrl.
    
    
    Bug: 1214964, 1197133
    Change-Id: If4f4061c2e2f53c743be60275e290dc77a75c366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2932289
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888764}

--- content/browser/speculation_rules/speculation_host_impl.cc
+++ content/browser/speculation_rules/speculation_host_impl.cc
@@ -9,10 +9,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_client.h"
+#include "mojo/public/cpp/bindings/message.h"
 #include "third_party/blink/public/common/features.h"
 #include "url/origin.h"
 
 namespace content {
+
+namespace {
+
+// Returns true if all candidates pass the inspection.
+bool InspectCandidates(
+    std::vector<blink::mojom::SpeculationCandidatePtr>& candidates) {
+  for (auto& candidate : candidates) {
+    // These non-http candidates should be filtered out in Blink and
+    // SpeculationHostImpl should not see them. If SpeculationHostImpl receives
+    // non-http candidates, it may mean the renderer process has a bug
+    // or is compromised.
+    if (!candidate->url.SchemeIsHTTPOrHTTPS()) {
+      mojo::ReportBadMessage("SH_NON_HTTP");
+      return false;
+    }
+  }
+  return true;
+}
+
+}  // namespace
 
 // static
 void SpeculationHostImpl::Bind(
@@ -48,6 +69,8 @@ SpeculationHostImpl::~SpeculationHostImpl() = default;
 void SpeculationHostImpl::UpdateSpeculationCandidates(
     std::vector<blink::mojom::SpeculationCandidatePtr> candidates) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  if (!InspectCandidates(candidates))
+    return;
 
   // Only handle messages from an active main frame.
   if (!render_frame_host()->IsActive())
