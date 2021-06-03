1d947709dd8b6957cceb692be5615909e76aa160
Kyle Horimoto <khorimoto@chromium.org>
2021-06-03 18:52:55 +0000

    [CrOS Cellular] Fix single-pSIM device settings UI
    For single-pSIM devices, we should only show <network-siminfo> if the
    pSIM is locked. Additionally, we should not show the cellular on/off
    toggle if the <network-siminfo> is *not* shown.
    
    Bug: 1215320
    Change-Id: I75672362e4f97ae2d1b1e2831434f6782007f462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2931398
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888945}

--- chrome/browser/resources/settings/chromeos/internet_page/network_summary_item.js
+++ chrome/browser/resources/settings/chromeos/internet_page/network_summary_item.js
@@ -226,10 +226,13 @@ Polymer({
     }
 
     const {pSimSlots, eSimSlots} = getSimSlotCount(deviceState);
-    if (this.isUpdatedCellularUiEnabled_ && eSimSlots > 0) {
-      // Do not show simInfo if |updatedCellularActivationUi| flag is enabled
-      // and if we are using an eSIM enabled device.
-      return false;
+    if (this.isUpdatedCellularUiEnabled_) {
+      if (eSimSlots > 0) {
+        // Do not show simInfo if |updatedCellularActivationUi| flag is enabled
+        // and if we are using an eSIM enabled device.
+        return false;
+      }
+      return this.simLocked_(deviceState);
     }
     return this.simLockedOrAbsent_(deviceState);
   },
@@ -278,6 +281,18 @@ Polymer({
     if (deviceState.simAbsent) {
       return true;
     }
+    return this.simLocked_(deviceState);
+  },
+
+  /**
+   * @param {!OncMojo.DeviceStateProperties|undefined} deviceState
+   * @return {boolean}
+   * @private
+   */
+  simLocked_(deviceState) {
+    if (!deviceState) {
+      return false;
+    }
     if (!deviceState.simLockStatus) {
       return false;
     }
@@ -315,16 +330,21 @@ Polymer({
       case mojom.NetworkType.kEthernet:
       case mojom.NetworkType.kVPN:
         return false;
+
       case mojom.NetworkType.kTether:
         return true;
+
       case mojom.NetworkType.kWiFi:
         return deviceState.deviceState !== mojom.DeviceStateType.kUninitialized;
+
       case mojom.NetworkType.kCellular:
-        return (deviceState.deviceState !==
-                    mojom.DeviceStateType.kUninitialized &&
-                !this.simLockedOrAbsent_(deviceState)) ||
-            (this.isUpdatedCellularUiEnabled_ &&
-             this.simLockedOrAbsent_(deviceState));
+        if (deviceState.deviceState === mojom.DeviceStateType.kUninitialized) {
+          return false;
+        }
+
+        // Toggle should be shown as long as we are not also showing the UI for
+        // unlocking the SIM.
+        return !this.showSimInfo_(deviceState);
     }
     assertNotReached();
     return false;
