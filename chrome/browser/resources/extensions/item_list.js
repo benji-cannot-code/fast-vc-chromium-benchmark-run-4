27268c65e4dd2da302ebd1e7065dba5d750d7038
dpapad <dpapad@chromium.org>
2021-06-03 16:40:00 +0000

    Extensions: Migrate to TypeScript compiler, still using JS.
    This preparatory step removes type coverage (from Closure Compiler),
    and is necessary to migrate Extensions to Typescript in an incremental
    way, as opposed to migrating in a single CL.
    
    Type coverage will be re-added (via Typescript compiler) in follow up
    migration CLs.
    
    Bug: 1189595
    Change-Id: Ib4989c780a9dc221c13ee6469207cb4b125d2108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2915994
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888899}

--- chrome/browser/resources/extensions/item_list.js
+++ chrome/browser/resources/extensions/item_list.js
@@ -4,8 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_components/managed_footnote/managed_footnote.js';
-import './shared_style.js';
 import './checkup.js';
+import './item.js';
+import './shared_style.js';
 
 import {CrContainerShadowBehavior} from 'chrome://resources/cr_elements/cr_container_shadow_behavior.m.js';
 import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
