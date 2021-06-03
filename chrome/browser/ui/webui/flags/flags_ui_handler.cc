ee33453a2ebc9131c8640cb6e2c74708dfa83205
rbpotter <rbpotter@chromium.org>
2021-06-03 22:45:23 +0000

    Flags UI: Fix possible null dereference
    This UI calls one of its message handler methods directly, which is not
    an expected pattern. Factor out the code to send features to the UI
    from the message handler and cache the JS callback id so that the
    callback is always resolved exactly once and no null dereference
    occurs.
    
    Bug: 1214076
    Change-Id: I62d34022933bacec8a2d070f5ea7f48f31b600b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935759
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889032}

--- chrome/browser/ui/webui/flags/flags_ui_handler.cc
+++ chrome/browser/ui/webui/flags/flags_ui_handler.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 FlagsUIHandler::FlagsUIHandler()
     : access_(flags_ui::kGeneralAccessFlagsOnly),
-      experimental_features_requested_(false),
+      experimental_features_callback_id_(""),
       deprecated_features_only_(false) {}
 
 FlagsUIHandler::~FlagsUIHandler() {}
@@ -58,8 +58,8 @@ void FlagsUIHandler::Init(flags_ui::FlagsStorage* flags_storage,
   flags_storage_.reset(flags_storage);
   access_ = access;
 
-  if (experimental_features_requested_)
-    HandleRequestExperimentalFeatures(nullptr);
+  if (!experimental_features_callback_id_.empty())
+    SendExperimentalFeatures();
 }
 
 void FlagsUIHandler::HandleRequestExperimentalFeatures(
@@ -67,14 +67,17 @@ void FlagsUIHandler::HandleRequestExperimentalFeatures(
   AllowJavascript();
   const base::Value& callback_id = args->GetList()[0];
 
-  experimental_features_requested_ = true;
+  experimental_features_callback_id_ = callback_id.GetString();
   // Bail out if the handler hasn't been initialized yet. The request will be
   // handled after the initialization.
   if (!flags_storage_) {
-    ResolveJavascriptCallback(callback_id, base::Value());
     return;
   }
 
+  SendExperimentalFeatures();
+}
+
+void FlagsUIHandler::SendExperimentalFeatures() {
   base::DictionaryValue results;
 
   std::unique_ptr<base::ListValue> supported_features(new base::ListValue);
@@ -109,7 +112,9 @@ void FlagsUIHandler::HandleRequestExperimentalFeatures(
   results.SetBoolean(flags_ui::kShowBetaChannelPromotion, false);
   results.SetBoolean(flags_ui::kShowDevChannelPromotion, false);
 #endif
-  ResolveJavascriptCallback(callback_id, results);
+  ResolveJavascriptCallback(base::Value(experimental_features_callback_id_),
+                            results);
+  experimental_features_callback_id_.clear();
 }
 
 void FlagsUIHandler::HandleEnableExperimentalFeatureMessage(
