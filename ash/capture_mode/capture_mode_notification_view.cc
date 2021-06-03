a9b280da92ff04d8232a1923e8595ed2dd4d0c95
Ana Salazar <anasalazar@chromium.org>
2021-06-03 20:59:33 +0000

    Stop showing multipaste shortcut on tablet mode
    The multipaste team added the keyboard shortcut for the clipboard to the
    screenshot notification.
    This change shows the shortcut only on devices on clamshell mode.
    
    Bug: 1188348
    Change-Id: I35929b3dc40f2575628d85438dd6193d4ac6a990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2936123
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889001}

--- ash/capture_mode/capture_mode_notification_view.cc
+++ ash/capture_mode/capture_mode_notification_view.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/assistant/assistant_state.h"
 #include "ash/public/cpp/clipboard_history_controller.h"
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/style/scoped_light_mode_as_default.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/events/keyboard_layout_util.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -128,14 +130,15 @@ std::unique_ptr<views::View> CreateBannerView() {
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   label->SetEnabledColor(text_icon_color);
 
-  if (features::IsClipboardHistoryScreenshotNudgeEnabled()) {
-    banner_view->AddChildView(CreateClipboardShortcutView());
-    layout->SetFlexForView(label, 1);
+  if (!Shell::Get()->tablet_mode_controller()->InTabletMode()) {
+    if (features::IsClipboardHistoryScreenshotNudgeEnabled()) {
+      banner_view->AddChildView(CreateClipboardShortcutView());
+      layout->SetFlexForView(label, 1);
+    }
+
+    // Notify the clipboard history of the created notification.
+    ClipboardHistoryController::Get()->OnScreenshotNotificationCreated();
   }
-
-  // Notify the clipboard history of the created notification.
-  ClipboardHistoryController::Get()->OnScreenshotNotificationCreated();
-
   return banner_view;
 }
 
