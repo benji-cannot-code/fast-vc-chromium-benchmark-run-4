fb4ff09ea8a30c7b210bbfbf8788fbbcce0a9c86
Alexander Dunaev <adunaev@igalia.com>
2021-06-03 18:51:43 +0000

    [ozone/wayland] Integrated the KDE Idle service.
    Querying the idle state is specific to platform, and the Wayland
    platform lacks it.
    
    This CL introduces support for the org_kde_kwin_idle Wayland protocol
    extension that is available in environments that use the KWin compositor
    (i.e., are based on KDE).
    
    Bug: 1098201
    Change-Id: I4f85baa3e3e34480f3cc0be9ec803d55dc293c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2919765
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#888942}

--- ui/ozone/platform/wayland/host/wayland_connection.h
+++ ui/ozone/platform/wayland/host/wayland_connection.h
@@ -33,6 +33,7 @@ class WaylandProxy;
 namespace ui {
 
 class DeviceHotplugEventObserver;
+class OrgKdeKwinIdle;
 class WaylandBufferManagerHost;
 class WaylandCursor;
 class WaylandCursorBufferListener;
@@ -188,6 +189,8 @@ class WaylandConnection {
 
   GtkShell1* gtk_shell1() { return gtk_shell1_.get(); }
 
+  OrgKdeKwinIdle* org_kde_kwin_idle() { return org_kde_kwin_idle_.get(); }
+
   ZwpPrimarySelectionDeviceManager* zwp_primary_selection_device_manager()
       const {
     return zwp_primary_selection_device_manager_.get();
@@ -308,6 +311,9 @@ class WaylandConnection {
   std::unique_ptr<WaylandClipboard> clipboard_;
 
   std::unique_ptr<GtkShell1> gtk_shell1_;
+
+  // Objects specific to KDE Plasma desktop environment.
+  std::unique_ptr<OrgKdeKwinIdle> org_kde_kwin_idle_;
 
   std::unique_ptr<WaylandDataDragController> data_drag_controller_;
   std::unique_ptr<WaylandWindowDragController> window_drag_controller_;
