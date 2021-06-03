2c38634eaf52bef69ad21fee97935e1da96a7df2
Dan Harrington <harringtond@chromium.org>
2021-06-03 20:12:37 +0000

    remove AdaptCallbackForRepeating
    Bug: 730593
    
    Change-Id: I50d16fcf65b96e605c092594d062a8d38f629916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935030
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888983}

--- chromecast/browser/test/mock_cast_content_window_delegate.cc
+++ chromecast/browser/test/mock_cast_content_window_delegate.cc
@@ -11,13 +11,6 @@ MockCastContentWindowDelegate::MockCastContentWindowDelegate() {}
 
 MockCastContentWindowDelegate::~MockCastContentWindowDelegate() = default;
 
-void MockCastContentWindowDelegate::ConsumeGesture(
-    GestureType gesture_type,
-    GestureHandledCallback handled_callback) {
-  ConsumeGesture(gesture_type,
-                 base::AdaptCallbackForRepeating(std::move(handled_callback)));
-}
-
 std::string MockCastContentWindowDelegate::GetId() {
   return "mockContentWindowDelegate";
 }
