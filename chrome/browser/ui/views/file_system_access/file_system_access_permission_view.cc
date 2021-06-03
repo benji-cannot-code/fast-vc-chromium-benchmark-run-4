9c04633ea630f02ba2f63d958f820d7e3d4cc2a5
Austin Sullivan <asully@chromium.org>
2021-06-03 20:00:59 +0000

    FSA: Edit UI strings for persisted permissions
    Removed "until you close all tabs for this site" from UI strings.
    
    Bug: 2867327
    Change-Id: Iac79d2467a94aa0a4c9c782dbe23744c20ff68bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2924251
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888979}

--- chrome/browser/ui/views/file_system_access/file_system_access_permission_view.cc
+++ chrome/browser/ui/views/file_system_access/file_system_access_permission_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
+#include "chrome/browser/file_system_access/chrome_file_system_access_permission_context.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/browser/ui/views/file_system_access/file_system_access_ui_helpers.h"
@@ -30,16 +31,30 @@ using HandleType = content::FileSystemAccessPermissionContext::HandleType;
 int GetMessageText(const FileSystemAccessPermissionView::Request& request) {
   switch (request.access) {
     case AccessType::kRead:
-      return request.handle_type == HandleType::kDirectory
-                 ? IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_READ_PERMISSION_DIRECTORY_TEXT
-                 : IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_READ_PERMISSION_FILE_TEXT;
+      if (base::FeatureList::IsEnabled(
+              features::kFileSystemAccessPersistentPermissions)) {
+        return request.handle_type == HandleType::kDirectory
+                   ? IDS_FILE_SYSTEM_ACCESS_READ_PERMISSION_DIRECTORY_TEXT
+                   : IDS_FILE_SYSTEM_ACCESS_READ_PERMISSION_FILE_TEXT;
+      } else {
+        return request.handle_type == HandleType::kDirectory
+                   ? IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_READ_PERMISSION_DIRECTORY_TEXT
+                   : IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_READ_PERMISSION_FILE_TEXT;
+      }
     case AccessType::kWrite:
     case AccessType::kReadWrite:
       // Only difference between write and read-write access dialog is in button
       // label and dialog title.
-      return request.handle_type == HandleType::kDirectory
-                 ? IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_WRITE_PERMISSION_DIRECTORY_TEXT
-                 : IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_WRITE_PERMISSION_FILE_TEXT;
+      if (base::FeatureList::IsEnabled(
+              features::kFileSystemAccessPersistentPermissions)) {
+        return request.handle_type == HandleType::kDirectory
+                   ? IDS_FILE_SYSTEM_ACCESS_WRITE_PERMISSION_DIRECTORY_TEXT
+                   : IDS_FILE_SYSTEM_ACCESS_WRITE_PERMISSION_FILE_TEXT;
+      } else {
+        return request.handle_type == HandleType::kDirectory
+                   ? IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_WRITE_PERMISSION_DIRECTORY_TEXT
+                   : IDS_FILE_SYSTEM_ACCESS_ORIGIN_SCOPED_WRITE_PERMISSION_FILE_TEXT;
+      }
   }
   NOTREACHED();
 }
