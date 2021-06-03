0756440c76917a69ab28a81684323f7ca3aba7bf
Kartik Hegde <khegde@chromium.org>
2021-06-03 05:52:00 +0000

    shill: Add RequestTrafficCounters to ShillPropertyHandler
    Add RequestTrafficCounters to the ShillPropertyHandler. The function
    here is a conduit to ShillServiceClient's RequestTrafficCounters, which
    retrieves traffic counters for a particular service from Shill.
    
    BUG=b:187642240
    TEST=chromeos_unittests --gtest_filter=ShillPropertyHandlerTest.*
    
    Change-Id: If4e284784de0f9d8bea47deb26185209d1d216cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886058
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#888753}

--- chromeos/network/shill_property_handler.h
+++ chromeos/network/shill_property_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chromeos/dbus/dbus_method_call_status.h"
 #include "chromeos/dbus/shill/shill_property_changed_observer.h"
+#include "chromeos/dbus/shill/shill_service_client.h"
 #include "chromeos/network/managed_state.h"
 #include "chromeos/network/network_handler_callbacks.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -157,6 +158,11 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) ShillPropertyHandler
   // Requests all properties for the service or device (called for new items).
   void RequestProperties(ManagedState::ManagedType type,
                          const std::string& path);
+
+  // Requests traffic counters for a Service denoted by |service_path|.
+  // Traffic counters are returned via |callback|.
+  void RequestTrafficCounters(const std::string& service_path,
+                              ShillServiceClient::ListValueCallback callback);
 
   // ShillPropertyChangedObserver overrides
   void OnPropertyChanged(const std::string& key,
