f05c6ea9d48a0e0325b32888de1ba6bfb7e9cc49
Nicholas Hollingum <hollingum@google.com>
2021-06-03 06:37:53 +0000

    borealis: Add a delay when popping up feedback
    The feedback form pops up too frequently, especially when windows for
    the same app are created and destroyed while the app is still running.
    
    To counteract this we have a delay (set to 5 seconds) which only pops up
    the feedback form if the same app does not re-launch itself.
    
    Bug: b/189392370
    Change-Id: I1acc9dba5784a47e8d74f5f507b0c9db013a4f73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2931481
    Reviewed-by: Daniel Ng <danielng@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#888763}

--- chrome/browser/ash/borealis/borealis_context.cc
+++ chrome/browser/ash/borealis/borealis_context.cc
@@ -5,10 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/borealis/borealis_context.h"
 
+#include <memory>
+
 #include "ash/public/cpp/new_window_delegate.h"
+#include "base/bind.h"
+#include "base/callback_forward.h"
+#include "base/containers/flat_map.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/time/time.h"
 #include "chrome/browser/ash/borealis/borealis_disk_manager_impl.h"
 #include "chrome/browser/ash/borealis/borealis_engagement_metrics.h"
 #include "chrome/browser/ash/borealis/borealis_game_mode_controller.h"
@@ -22,6 +29,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace borealis {
+
+namespace {
+
+// Similar to a delayed callback, but can be cancelled by deleting.
+class ScopedDelayedCallback {
+ public:
+  explicit ScopedDelayedCallback(base::OnceClosure callback,
+                                 base::TimeDelta delay)
+      : weak_factory_(this) {
+    base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&ScopedDelayedCallback::OnComplete,
+                       weak_factory_.GetWeakPtr(), std::move(callback)),
+        delay);
+  }
+
+ private:
+  void OnComplete(base::OnceClosure callback) { std::move(callback).Run(); }
+
+  base::WeakPtrFactory<ScopedDelayedCallback> weak_factory_;
+};
+
+}  // namespace
 
 class BorealisLifetimeObserver
     : public BorealisWindowManager::AppWindowLifetimeObserver {
@@ -44,6 +74,10 @@ class BorealisLifetimeObserver
         .ShutdownWithDelay();
   }
 
+  void OnAppStarted(const std::string& app_id) override {
+    app_delayers_.erase(app_id);
+  }
+
   void OnAppFinished(const std::string& app_id,
                      aura::Window* last_window) override {
     // Launch post-game survey.
@@ -52,8 +86,14 @@ class BorealisLifetimeObserver
         guest_os::GuestOsRegistryServiceFactory::GetForProfile(profile_),
         app_id, base::UTF16ToUTF8(last_window->GetTitle()));
     if (url.is_valid()) {
-      ash::NewWindowDelegate::GetInstance()->NewTabWithUrl(
-          url, /*from_user_interaction=*/true);
+      // Unretained is safe here since we transitively own the
+      // ScopedDelayedCallback's weak factory.
+      app_delayers_.emplace(
+          app_id,
+          std::make_unique<ScopedDelayedCallback>(
+              base::BindOnce(&BorealisLifetimeObserver::OnDelayComplete,
+                             base::Unretained(this), std::move(url), app_id),
+              base::TimeDelta::FromSeconds(5)));
     }
   }
 
@@ -63,10 +103,18 @@ class BorealisLifetimeObserver
   }
 
  private:
+  void OnDelayComplete(GURL gurl, std::string app_id) {
+    app_delayers_.erase(app_id);
+    ash::NewWindowDelegate::GetInstance()->NewTabWithUrl(
+        gurl, /*from_user_interaction=*/true);
+  }
+
   Profile* const profile_;
   base::ScopedObservation<BorealisWindowManager,
                           BorealisWindowManager::AppWindowLifetimeObserver>
       observation_;
+  base::flat_map<std::string, std::unique_ptr<ScopedDelayedCallback>>
+      app_delayers_;
 };
 
 BorealisContext::~BorealisContext() = default;
