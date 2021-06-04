a43c6c7a82db70f3e87d517a3fbfaba6b5feb828
Lily Chen <chlily@chromium.org>
2021-06-04 20:04:05 +0000

    localStorage: Refactor internals to use StorageKey instead of Origin
    This refactors localStorage internals (mainly in LocalStorageImpl) to
    use StorageKey instead of url::Origin in database keys. No data
    migration is needed as the serialization is unchanged.
    
    There are still conversions to url::Origin at interface boundaries. The
    LocalStorageControl mojo interface is not yet modified.
    
    This adds an additional serialization method to
    StorageKey to support the serialization previously used by localStorage,
    which differs from StorageKey::Serialize().
    
    Bug: 1212808
    Change-Id: Ib01d482afcbd720dd47729517bac71ce228e5bc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911340
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889420}

--- third_party/blink/common/storage_key/storage_key.cc
+++ third_party/blink/common/storage_key/storage_key.cc
@@ -30,6 +30,11 @@ std::string StorageKey::Serialize() const {
   return origin_.GetURL().spec();
 }
 
+std::string StorageKey::SerializeForLocalStorage() const {
+  DCHECK(!opaque());
+  return origin_.Serialize();
+}
+
 std::string StorageKey::GetDebugString() const {
   return base::StrCat({"{ origin: ", origin_.GetDebugString(), " }"});
 }
