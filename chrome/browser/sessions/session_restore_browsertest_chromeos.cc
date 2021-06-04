54f7c1574b750290dff39fa757456e1563ce3010
chinsenj <chinsenj@chromium.org>
2021-06-04 22:41:37 +0000

    cros: Fix flaky SessionRestoreTestChromeOS tests.
    Currently `SessionRestoreTestChromeOS.RestoreBrowserWindowsToDesks` is
    flaky. In the PRE phase of the test, multiple browsers are created on
    non-active desks, causing the active desk to be changed. Desk switches
    require animations, so if the active desk is swapped too fast it can
    lead to flaky failures.
    
    This CL fixes this by switching to the browser's target desk before
    creating the new browser, waiting for any async desk-related operations
    to complete.
    
    This CL also re-enables `SessionRestoreTestChromeOS.RestoreMinimized`
    which was disabled for being flaky.
    
    Bug: 1157972
    Change-Id: Ic3c3179e6b67e94dd61c6dafb73df4f8dab2756b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941442
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889492}

--- chrome/browser/sessions/session_restore_browsertest_chromeos.cc
+++ chrome/browser/sessions/session_restore_browsertest_chromeos.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_utils.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/wm/core/wm_core_switches.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -46,10 +47,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 const char* test_app_name1 = "TestApp1";
 const char* test_app_name2 = "TestApp2";
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+// Activates the desk at |index| and waits for its async operations to complete.
+void SwitchToDesk(int index) {
+  base::RunLoop run_loop;
+  ASSERT_TRUE(ash::AutotestDesksApi().ActivateDeskAtIndex(
+      index, run_loop.QuitClosure()));
+  run_loop.Run();
+}
+
+// Removes all the inactive desks and waits for their async operations to
+// complete.
+void RemoveInactiveDesks() {
+  const int kMaxDeskRemovalTries = 100;
+  for (int i = 0; i < kMaxDeskRemovalTries; ++i) {
+    base::RunLoop run_loop;
+    if (!ash::AutotestDesksApi().RemoveActiveDesk(run_loop.QuitClosure()))
+      return;
+    run_loop.Run();
+  }
+  // This should not be reached.
+  ADD_FAILURE();
+}
+#endif
+
 }  // namespace
 
 class SessionRestoreTestChromeOS : public InProcessBrowserTest {
  public:
+  SessionRestoreTestChromeOS()
+      : faster_animations_(
+            ui::ScopedAnimationDurationScaleMode::ZERO_DURATION) {}
   ~SessionRestoreTestChromeOS() override {}
 
  protected:
@@ -90,6 +119,9 @@ class SessionRestoreTestChromeOS : public InProcessBrowserTest {
   }
 
   Profile* profile() { return browser()->profile(); }
+
+ private:
+  ui::ScopedAnimationDurationScaleMode faster_animations_;
 };
 
 // Thse tests are in pairs. The PRE_ test creates some browser windows and
@@ -135,6 +167,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreTestChromeOS,
 
   // Create a second normal browser window in the second desk by
   // setting window workspace property.
+  SwitchToDesk(1);
   Browser* browser_desk1 =
       CreateBrowserWithParams(Browser::CreateParams(profile(), true));
   browser_desk1->SetWindowUserTitle("1");
@@ -143,6 +176,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreTestChromeOS,
 
   // Create a third normal browser window in the third desk
   // specified with params.initial_workspace.
+  SwitchToDesk(2);
   Browser::CreateParams browser_desk2_params =
       Browser::CreateParams(profile(), true);
   browser_desk2_params.initial_workspace = "2";
@@ -178,6 +212,8 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreTestChromeOS,
     ASSERT_EQ(desk_index,
               workspace == aura::client::kUnassignedWorkspace ? 0 : workspace);
   }
+
+  RemoveInactiveDesks();
 }
 
 // Assigns a browser window to all desks.
@@ -229,6 +265,8 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreTestChromeOS,
   // even if there is a desk switch.
   ASSERT_TRUE(ash::DesksHelper::Get()->BelongsToActiveDesk(
       visible_on_all_desks_window));
+
+  RemoveInactiveDesks();
 }
 #endif
 
@@ -376,8 +414,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreTestChromeOS, PRE_RestoreMinimized) {
   TurnOnSessionRestore();
 }
 
-// TODO(crbug.com/1206591): Enable when flakiness is fixed.
-IN_PROC_BROWSER_TEST_F(SessionRestoreTestChromeOS, DISABLED_RestoreMinimized) {
+IN_PROC_BROWSER_TEST_F(SessionRestoreTestChromeOS, RestoreMinimized) {
   size_t total_count = 0;
   size_t minimized_count = 0;
   for (auto* browser : *BrowserList::GetInstance()) {
