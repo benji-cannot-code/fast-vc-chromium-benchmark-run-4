11e62d55d0def5e849e7a96f0ba63f115ade075f
Andrew Paseltiner <apaseltiner@chromium.org>
2021-06-04 00:33:57 +0000

    [attribution_reporting] Measure how often conversions are not handled due to the frame being detached
    It is fine to measure this even when the feature is not enabled because
    the failure mode is the same.
    
    Bug: 1215682
    Change-Id: Ie8a9738ecc6b17553f3bb52a4a3ad6f171ed69f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935496
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889081}

--- third_party/blink/renderer/core/loader/frame_fetch_context.cc
+++ third_party/blink/renderer/core/loader/frame_fetch_context.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_macros.h"
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #include "net/http/structured_headers.h"
@@ -802,7 +803,16 @@ bool FrameFetchContext::SendConversionRequestInsteadOfRedirecting(
     const absl::optional<ResourceRequest::RedirectInfo>& redirect_info,
     ReportingDisposition reporting_disposition,
     const String& devtools_request_id) const {
-  if (GetResourceFetcherProperties().IsDetached())
+  const char kWellKnownConversionRegistrationPath[] =
+      "/.well-known/attribution-reporting/trigger-attribution";
+  if (url.GetPath() != kWellKnownConversionRegistrationPath)
+    return false;
+
+  const bool detached = GetResourceFetcherProperties().IsDetached();
+  UMA_HISTOGRAM_BOOLEAN("Conversions.RedirectInterceptedFrameDetached",
+                        detached);
+
+  if (detached)
     return false;
 
   if (!RuntimeEnabledFeatures::ConversionMeasurementEnabled(
@@ -815,11 +825,6 @@ bool FrameFetchContext::SendConversionRequestInsteadOfRedirecting(
       !SecurityOrigin::AreSameOrigin(url, redirect_info->previous_url)) {
     return false;
   }
-
-  const char kWellKnownConversionRegsitrationPath[] =
-      "/.well-known/attribution-reporting/trigger-attribution";
-  if (url.GetPath() != kWellKnownConversionRegsitrationPath)
-    return false;
 
   if (!document_->domWindow()->IsFeatureEnabled(
           mojom::blink::PermissionsPolicyFeature::kAttributionReporting)) {
