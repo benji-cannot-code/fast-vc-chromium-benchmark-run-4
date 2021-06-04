f509579698133dfd02f186c90a72c686b38e8649
Boris Sazonov <bsazonov@chromium.org>
2021-06-04 14:23:45 +0000

    [Android] Turn off vertical scrollbars while sanitizing views
    MainSettingsFragmentTest.testRenderDifferentSignedInStates was flaky due
    to scrollbar fading animation. This CL fixes the issue by:
    1. Adding setVerticalScrollBarEnabled(false) call to
       RenderTestRule.sanitize.
    2. Calling RenderTestRule.sanitize in the test.
    
    Bug: 1204117
    Change-Id: I8920a9a63dddd5c0db0e340807791a0e7b9b587b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2916959
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889284}

--- ui/android/javatests/src/org/chromium/ui/test/util/RenderTestRule.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/RenderTestRule.java
@@ -235,6 +235,8 @@ public class RenderTestRule extends TestWatcher {
                 ((AnimatedVectorDrawableCompat) drawable).stop();
             }
         }
+        // Scrollbars fade slowly, making tests flaky due to differences in rendered images.
+        view.setVerticalScrollBarEnabled(false);
     }
 
     /**
