165adb68ba1c20b870f8a0c7166eb313eaf05bd3
Joe Mason <joenotcharles@google.com>
2021-06-04 00:17:02 +0000

    Add a SoftwareReporter.RunningTimeWithoutSleep histogram
    This will help estimate how much of the reported
    SoftwareReporter.RunningTimeAccordingToChrome > 5 min is due to the
    computer being asleep for 5+ min.
    
    Also push back the expiry date of
    SoftwareReporter.RunningTimeAccordingToChrome by 6 months since it's
    proved useful in a bug investigation.
    
    R=drubery
    
    Bug: 1213207
    Change-Id: I645b76b5ef6811be044c4f0f83703e586463b1d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929363
    Auto-Submit: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889070}

--- chrome/browser/safe_browsing/chrome_cleaner/reporter_runner_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/reporter_runner_win.cc
@@ -219,9 +219,12 @@ class UMAHistogramReporter {
 
   // Reports the SwReporter run time with UMA both as reported by the tool via
   // the registry and as measured by |ReporterRunner|.
-  void ReportRuntime(const base::TimeDelta& reporter_running_time) const {
+  void ReportRuntime(const base::TimeDelta& reporter_running_time,
+                     const base::TimeDelta& running_time_without_sleep) const {
     RecordLongTimesHistogram("SoftwareReporter.RunningTimeAccordingToChrome",
                              reporter_running_time);
+    RecordLongTimesHistogram("SoftwareReporter.RunningTimeWithoutSleep",
+                             running_time_without_sleep);
 
     // TODO(b/641081): This should only have KEY_QUERY_VALUE and KEY_SET_VALUE.
     base::win::RegKey reporter_key;
@@ -469,6 +472,10 @@ class ReporterRunner {
   }
 
  private:
+  // The type returned by QueryUnbiasedInterruptTime, which does not include
+  // time spent in sleep or hibernation.
+  using TimestampWithoutSleep = ULONGLONG;
+
   // Keeps track of last and upcoming reporter runs and logs uploading.
   //
   // Periodic runs are allowed to start if the last time the reporter ran was
@@ -555,11 +562,15 @@ class ReporterRunner {
     base::TaskRunner* task_runner =
         g_testing_delegate_ ? g_testing_delegate_->BlockingTaskRunner()
                             : blocking_task_runner_.get();
+
+    TimestampWithoutSleep now_without_sleep;
+    ::QueryUnbiasedInterruptTime(&now_without_sleep);
+
     auto launch_and_wait =
         base::BindOnce(&LaunchAndWaitForExit, next_invocation);
     auto reporter_done =
         base::BindOnce(&ReporterRunner::ReporterDone, base::Unretained(this),
-                       Now(), next_invocation);
+                       Now(), now_without_sleep, next_invocation);
     base::PostTaskAndReplyWithResult(task_runner, FROM_HERE,
                                      std::move(launch_and_wait),
                                      std::move(reporter_done));
@@ -569,6 +580,7 @@ class ReporterRunner {
   // has completed. This is run as a task posted from an interruptible worker
   // thread so should be resilient to unexpected shutdown.
   void ReporterDone(const base::Time& reporter_start_time,
+                    TimestampWithoutSleep start_time_without_sleep,
                     SwReporterInvocation finished_invocation,
                     int exit_code) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -591,8 +603,17 @@ class ReporterRunner {
       return;
     }
 
+    TimestampWithoutSleep now_without_sleep;
+    ::QueryUnbiasedInterruptTime(&now_without_sleep);
+
     base::Time now = Now();
     base::TimeDelta reporter_running_time = now - reporter_start_time;
+
+    // QueryUnbiasedInterruptTime returns units of 100 nanoseconds. See
+    // https://docs.microsoft.com/en-us/windows/win32/api/realtimeapiset/nf-realtimeapiset-queryunbiasedinterrupttime
+    base::TimeDelta running_time_without_sleep =
+        base::TimeDelta::FromNanoseconds(
+            100 * (now_without_sleep - start_time_without_sleep));
 
     // Tries to run the next invocation in the queue.
     if (!invocations_.container().empty()) {
@@ -617,7 +638,7 @@ class ReporterRunner {
       local_state->SetInt64(prefs::kSwReporterLastTimeTriggered,
                             now.ToInternalValue());
     }
-    uma.ReportRuntime(reporter_running_time);
+    uma.ReportRuntime(reporter_running_time, running_time_without_sleep);
     uma.ReportMemoryUsage();
     if (finished_invocation.reporter_logs_upload_enabled())
       uma.RecordLogsUploadResult();
