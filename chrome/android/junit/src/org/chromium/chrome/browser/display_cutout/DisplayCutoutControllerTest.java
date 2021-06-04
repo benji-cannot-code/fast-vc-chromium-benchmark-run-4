81fd352061d138505541c60495220ae0b3103752
Peter E Conn <peconn@chromium.org>
2021-06-04 09:47:00 +0000

    👮 Disable DisplayCutoutControllerTests.
    Change-Id: I3a15c846b11ed0ae620466cf8e6b28815ea5ec7c
    Bug: 1216298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940543
    Owners-Override: Peter Conn <peconn@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889201}

--- chrome/android/junit/src/org/chromium/chrome/browser/display_cutout/DisplayCutoutControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/display_cutout/DisplayCutoutControllerTest.java
@@ -18,6 +18,7 @@ import android.view.WindowManager.LayoutParams;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -44,6 +45,7 @@ import java.lang.ref.WeakReference;
  */
 @RunWith(LocalRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
+@Ignore("https://crbug.com/1216298")
 public class DisplayCutoutControllerTest {
     @Mock
     private TabImpl mTab;
