46bcbfa2e8ed7e7264486ddf25ad914d488c42b2
Vincent Boisselle <vincb@google.com>
2021-06-04 15:57:10 +0000

    [iOS] Migrate the new FRE UI code to the FRE agent
    Move showFirstRunUI and the FRE view dismiss handlers from the Scene
    controller to the agent and remove the now unused FRE UI dismiss
    handlers in the Scene controller.
    
    Showing the new FRE UI will now be handled by the agent.
    
    Change-Id: Ib85f161050423192505278cc9d7e83a1f66b5bc1
    Bug: 1210256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927657
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889314}

--- ios/chrome/browser/ui/main/scene_controller.h
+++ ios/chrome/browser/ui/main/scene_controller.h
@@ -45,10 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    completionHandler:(void (^)(BOOL succeeded))completionHandler
     API_AVAILABLE(ios(13));
 
-// TODO(crbug.com/1210256): Remove this once it is migrated to the agent.
-// Shows the new first run UI.
-- (void)showFirstRunUI;
-
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_MAIN_SCENE_CONTROLLER_H_
