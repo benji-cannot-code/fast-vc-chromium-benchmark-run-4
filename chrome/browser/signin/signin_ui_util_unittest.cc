457b943988d4dec5b428f4c52ccddfbfc249ff96
David Roger <droger@chromium.org>
2021-06-04 12:26:50 +0000

    [signin] Update calls to FindExtendedAccountInfo in c/browser/signin
    This CL migrates callers to the new version of the API:
    - does not return an optional (returns empty AccountInfo instead).
    
    Bug: 1213351
    Change-Id: Iafa36c438555c217463e97a10019d0039a369bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917609
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889253}

--- chrome/browser/signin/signin_ui_util_unittest.cc
+++ chrome/browser/signin/signin_ui_util_unittest.cc
@@ -261,10 +261,7 @@ TEST_F(DiceSigninUiUtilTest, EnableSyncWithExistingAccount) {
                      "Signin_Signin_FromBookmarkBubble"));
 
     EnableSync(
-        GetIdentityManager()
-            ->FindExtendedAccountInfoForAccountWithRefreshTokenByAccountId(
-                account_id)
-            .value(),
+        GetIdentityManager()->FindExtendedAccountInfoByAccountId(account_id),
         is_default_promo_account);
     signin_metrics::PromoAction expected_promo_action =
         is_default_promo_account
@@ -321,10 +318,7 @@ TEST_F(DiceSigninUiUtilTest, EnableSyncWithAccountThatNeedsReauth) {
                      "Signin_Signin_FromBookmarkBubble"));
 
     EnableSync(
-        GetIdentityManager()
-            ->FindExtendedAccountInfoForAccountWithRefreshTokenByAccountId(
-                account_id)
-            .value(),
+        GetIdentityManager()->FindExtendedAccountInfoByAccountId(account_id),
         is_default_promo_account);
     ASSERT_FALSE(create_dice_turn_sync_on_helper_called_);
 
--- chrome/browser/ui/webui/signin/dice_turn_sync_on_helper.cc
+++ chrome/browser/ui/webui/signin/dice_turn_sync_on_helper.cc
@@ -88,16 +88,6 @@ class DiceTurnSyncOnHelperShutdownNotifierFactory
   DISALLOW_COPY_AND_ASSIGN(DiceTurnSyncOnHelperShutdownNotifierFactory);
 };
 
-AccountInfo GetAccountInfo(signin::IdentityManager* identity_manager,
-                           const CoreAccountId& account_id) {
-  auto maybe_account_info =
-      identity_manager
-          ->FindExtendedAccountInfoForAccountWithRefreshTokenByAccountId(
-              account_id);
-  return maybe_account_info.has_value() ? maybe_account_info.value()
-                                        : AccountInfo();
-}
-
 // User input handler for the signin confirmation dialog.
 class SigninDialogDelegate : public ui::ProfileSigninConfirmationDelegate {
  public:
@@ -210,7 +200,8 @@ DiceTurnSyncOnHelper::DiceTurnSyncOnHelper(
       signin_promo_action_(signin_promo_action),
       signin_reason_(signin_reason),
       signin_aborted_mode_(signin_aborted_mode),
-      account_info_(GetAccountInfo(identity_manager_, account_id)),
+      account_info_(
+          identity_manager_->FindExtendedAccountInfoByAccountId(account_id)),
       scoped_callback_runner_(std::move(callback)),
       shutdown_subscription_(
           DiceTurnSyncOnHelperShutdownNotifierFactory::GetInstance()
