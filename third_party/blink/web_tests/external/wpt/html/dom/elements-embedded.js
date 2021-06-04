6df5e81078c025e1982d507aa6437cba1142b7e2
Francois Beaufort <beaufort.francois@gmail.com>
2021-06-04 16:41:00 +0000

    Add web platform tests for HTMLMediaElement controlsList
    As promised in the intent[1], this CL adds some web platform tests for
    the HTMLMediaElement controlsList attribute.
    
    [1] https://groups.google.com/a/chromium.org/g/blink-dev/c/u9jsiarDEOg
    
    
    Bug: 1211336
    Change-Id: Id6d6993a0a378c43c2ff96088137ba0942d96041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940166
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#889328}

--- third_party/blink/web_tests/external/wpt/html/dom/elements-embedded.js
+++ third_party/blink/web_tests/external/wpt/html/dom/elements-embedded.js
@@ -94,6 +94,7 @@ var embeddedElements = {
     autoplay: "boolean",
     loop: "boolean",
     controls: "boolean",
+    controlsList: {type: "tokenlist", domAttrName: "controlsList"},
     defaultMuted: {type: "boolean", domAttrName: "muted"},
 
     width: "unsigned long",
