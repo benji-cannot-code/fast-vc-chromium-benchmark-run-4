da5768ac6f3b8d3d3d20387353c51e2084496cf4
Yuheng Huang <yuhengh@chromium.org>
2021-06-04 16:33:14 +0000

    UIDevtools: Implement --enable-ui-devtools=0
    When the browser is launched with command line flag
    --enable-ui-devtools=0, UIDevTools will pick an available port and write
    to |kUIDevToolsActivePortFileName|. This behavior is similar to
    --remote-debugging-port=0 of DevTools.
    
    This is useful for communication to program such as Telemetry without
    needing to assign to a specific port which may not be available.
    
    Bug: 1210302, 1201243
    Change-Id: Ic77d4136488d8739cc8d3aedfbb584b5e08cff12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2933530
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Wei Li <weili@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889327}

--- chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
+++ chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/path_service.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/devtools_process_observer.h"
 #include "chrome/browser/ui/views/media_router/media_router_dialog_controller_views.h"
 #include "chrome/browser/ui/views/relaunch_notification/relaunch_notification_controller.h"
+#include "chrome/common/chrome_paths.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/media_router/browser/media_router_dialog_controller.h"
 #include "components/ui_devtools/connector_delegate.h"
@@ -191,9 +193,12 @@ void ChromeBrowserMainExtraPartsViews::CreateUiDevTools() {
 
   // Starts the UI Devtools server for browser UI (and Ash UI on Chrome OS).
   auto connector = std::make_unique<UiDevtoolsConnector>();
+  base::FilePath output_dir;
+  bool result = base::PathService::Get(chrome::DIR_USER_DATA, &output_dir);
+  DCHECK(result);
   devtools_server_ = ui_devtools::CreateUiDevToolsServerForViews(
       g_browser_process->system_network_context_manager()->GetContext(),
-      std::move(connector));
+      std::move(connector), output_dir);
   devtools_process_observer_ = std::make_unique<DevtoolsProcessObserver>(
       devtools_server_->tracing_agent());
 }
