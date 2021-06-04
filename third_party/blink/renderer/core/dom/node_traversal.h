a02a5ba795fa7396d6561d153ebb9c231e5a26d0
Nayeem Hasan <nayeemhasan.nh01@gmail.com>
2021-06-04 19:34:37 +0000

    Add unit test for NodeTraversal
    Previously, there was no unit test for NodeTraversal. Added unit
    test for NodeTraversal.
    
    Bug: 1211933
    Change-Id: I6f708b449aa9908bda1fc37e90c1b1a61269676e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929636
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889411}

--- third_party/blink/renderer/core/dom/node_traversal.h
+++ third_party/blink/renderer/core/dom/node_traversal.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class NodeTraversal {
+class CORE_EXPORT NodeTraversal {
   STATIC_ONLY(NodeTraversal);
 
  public:
@@ -62,9 +62,8 @@ class NodeTraversal {
   }
 
   // Like next, but skips children and starts with the next sibling.
-  CORE_EXPORT static Node* NextSkippingChildren(const Node&);
-  CORE_EXPORT static Node* NextSkippingChildren(const Node&,
-                                                const Node* stay_within);
+  static Node* NextSkippingChildren(const Node&);
+  static Node* NextSkippingChildren(const Node&, const Node* stay_within);
 
   static Node* FirstWithin(const Node& current) { return current.firstChild(); }
 
@@ -90,23 +89,20 @@ class NodeTraversal {
                                  const Node* stay_within = nullptr);
 
   // Pre-order traversal including the pseudo-elements.
-  CORE_EXPORT static Node* PreviousIncludingPseudo(
-      const Node&,
-      const Node* stay_within = nullptr);
-  CORE_EXPORT static Node* NextIncludingPseudo(
-      const Node&,
-      const Node* stay_within = nullptr);
+  static Node* PreviousIncludingPseudo(const Node&,
+                                       const Node* stay_within = nullptr);
+  static Node* NextIncludingPseudo(const Node&,
+                                   const Node* stay_within = nullptr);
   // See comment for |FlatTreeTraversal::PreviousAbsoluteSibling| for details.
-  CORE_EXPORT static Node* PreviousAbsoluteSiblingIncludingPseudo(
+  static Node* PreviousAbsoluteSiblingIncludingPseudo(
       const Node&,
       const Node* stay_within = nullptr);
-  CORE_EXPORT static Node* NextIncludingPseudoSkippingChildren(
+  static Node* NextIncludingPseudoSkippingChildren(
       const Node&,
       const Node* stay_within = nullptr);
 
-  CORE_EXPORT static Node* NextAncestorSibling(const Node&);
-  CORE_EXPORT static Node* NextAncestorSibling(const Node&,
-                                               const Node* stay_within);
+  static Node* NextAncestorSibling(const Node&);
+  static Node* NextAncestorSibling(const Node&, const Node* stay_within);
   static Node& HighestAncestorOrSelf(const Node&);
 
   // Children traversal.
