656da15b86317cc61f830e33cc09d3bc35242612
Finnur Thorarinsson <finnur@chromium.org>
2021-06-04 15:43:28 +0000

    [App Identity]: Wire up Uninstall button.
    Clicking it takes the user to the App Info screen, where
    the Uninstall functionality is prominent in the top of
    the screen.
    
    Bug: 1205472
    Change-Id: Ic3e922a4819d74a203818532a9ac75df1048d574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934740
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889308}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkIconNameUpdateDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkIconNameUpdateDialog.java
@@ -36,6 +36,9 @@ public class WebApkIconNameUpdateDialog implements ModalDialogProperties.Control
     // The short name of the app before update.
     private String mOldAppShortName;
 
+    // The package name for this app.
+    private String mPackageName;
+
     // The callback to run when the user has made a decision.
     private Callback<Integer> mDialogResultCallback;
 
@@ -44,6 +47,7 @@ public class WebApkIconNameUpdateDialog implements ModalDialogProperties.Control
     /**
      * Shows the dialog.
      * @param manager The {@ModalDialogManager} to use.
+     * @param packageName The package name for this app.
      * @param iconChanging Whether an icon change has been detected.
      * @param shortNameChanging Whether a short name change has been detected.
      * @param nameChanging Whether a name change has been detected.
@@ -57,13 +61,15 @@ public class WebApkIconNameUpdateDialog implements ModalDialogProperties.Control
      * @param newIconAdaptive Whether the updated icon is adaptive.
      * @param callback The callback to use to communicate the results.
      */
-    public void show(ModalDialogManager manager, boolean iconChanging, boolean shortNameChanging,
-            boolean nameChanging, String oldAppShortName, String newAppShortName, String oldAppName,
-            String newAppName, Bitmap currentAppIcon, Bitmap updatedAppIcon,
-            boolean oldIconAdaptive, boolean newIconAdaptive, Callback<Integer> callback) {
+    public void show(ModalDialogManager manager, String packageName, boolean iconChanging,
+            boolean shortNameChanging, boolean nameChanging, String oldAppShortName,
+            String newAppShortName, String oldAppName, String newAppName, Bitmap currentAppIcon,
+            Bitmap updatedAppIcon, boolean oldIconAdaptive, boolean newIconAdaptive,
+            Callback<Integer> callback) {
         Context context = ContextUtils.getApplicationContext();
         Resources resources = context.getResources();
         mOldAppShortName = oldAppShortName;
+        mPackageName = packageName;
         mDialogResultCallback = callback;
 
         int titleId = 0;
@@ -122,7 +128,7 @@ public class WebApkIconNameUpdateDialog implements ModalDialogProperties.Control
                 break;
             case ModalDialogProperties.ButtonType.NEGATIVE:
                 WebApkUpdateReportAbuseDialog reportAbuseDialog = new WebApkUpdateReportAbuseDialog(
-                        mModalDialogManager, mOldAppShortName, this::onUninstall);
+                        mModalDialogManager, mPackageName, mOldAppShortName, this::onUninstall);
                 reportAbuseDialog.show();
                 break;
             default:
