ee23e6e4859891d80e3dfc1160e16820dc3e78fb
Kartik Hegde <khegde@chromium.org>
2021-06-05 21:46:37 +0000

    network_config: Add traffic_counter_reset_time to ManagedProperties
    Add |traffic_counter_reset_time| to ManagedProperties. The NetworkHealth
    snapshot and other clients can use this information to learn when
    traffic counters were last reset.
    
    BUG=b:188837951
    TEST=chromeos_unittests --gtest_filter=CrosNetworkConfigTest.*
    
    Change-Id: I5349e69920c4c9cd82505448e9b6dc441a53b15a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2913225
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889586}

--- components/onc/onc_constants.cc
+++ components/onc/onc_constants.cc
@@ -62,6 +62,7 @@ const char kSourceUserPolicy[] = "UserPolicy";
 const char kSource[] = "Source";
 const char kStaticIPConfig[] = "StaticIPConfig";
 const char kTether[] = "Tether";
+const char kTrafficCounterResetTime[] = "TrafficCounterResetTime";
 const char kType[] = "Type";
 const char kVPN[] = "VPN";
 const char kWiFi[] = "WiFi";
