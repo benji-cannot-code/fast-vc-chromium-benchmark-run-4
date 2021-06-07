e809e51b0f92672a88797f634a6e99988adf195a
Jinsuk Kim <jinsukkim@chromium.org>
2021-06-07 23:24:44 +0000

    Android: Clean up LocaleManager in AppHooks
    Cleans up all the references/methods to LocaleManager in AppHooks
    plus other methods left unused.
    
    Bug: 1201640
    Change-Id: Ibde3438461bbaab3db097477c63ab4355c1711d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2942111
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889992}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -26,7 +26,6 @@ import org.chromium.chrome.browser.historyreport.AppIndexingReporter;
 import org.chromium.chrome.browser.init.ChromeStartupDelegate;
 import org.chromium.chrome.browser.init.ProcessInitializationHandler;
 import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
-import org.chromium.chrome.browser.locale.LocaleManager;
 import org.chromium.chrome.browser.metrics.VariationsSession;
 import org.chromium.chrome.browser.notifications.chime.ChimeDelegate;
 import org.chromium.chrome.browser.omaha.RequestGenerator;
@@ -164,21 +163,6 @@ public abstract class AppHooks {
 
     public InstantAppsHandler createInstantAppsHandler() {
         return new InstantAppsHandler();
-    }
-
-    /**
-     * @return An instance of {@link LocaleManager} that handles customized locale related logic.
-     */
-    protected LocaleManager createLocaleManager() {
-        return new LocaleManager();
-    }
-
-    /**
-     * Return LocaleManager. Create a new one if not available.
-     **/
-    public LocaleManager getLocaleManager() {
-        if (LocaleManager.getInstance() == null) LocaleManager.setInstance(createLocaleManager());
-        return LocaleManager.getInstance();
     }
 
     /**
--- chrome/browser/locale/java/src/org/chromium/chrome/browser/locale/LocaleManager.java
+++ chrome/browser/locale/java/src/org/chromium/chrome/browser/locale/LocaleManager.java
@@ -231,12 +231,6 @@ public class LocaleManager implements DefaultSearchEngineDialogHelper.Delegate {
         mDelegate.recordUserTypeMetrics();
     }
 
-    /** Set a LocaleManager to be used for testing. */
-    @VisibleForTesting
-    public static void setInstanceForTest(LocaleManager instance) {
-        sInstance = instance;
-    }
-
     /** Set a LocaleManagerDelegate to be used for testing. */
     @VisibleForTesting
     public void setDelegateForTest(LocaleManagerDelegate delegate) {
