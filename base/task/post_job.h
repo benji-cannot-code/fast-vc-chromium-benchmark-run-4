9b7a6e1e030a57524464863479c8b3ec32bef566
Lei Zhang <thestig@chromium.org>
2021-06-07 20:32:45 +0000

    Avoid including base/task/task_traits.h in base/task/post_job.h.
    Forward declare instead, as go/chrome-includes [1] shows this as a
    relatively expensive include in terms of build costs.
    
    [1] https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    Change-Id: Ib29d77644addc4dd467b42c8e0946ca7a16dbe49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941552
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889926}

--- base/task/post_job.h
+++ base/task/post_job.h
@@ -14,13 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/task/task_traits.h"
 
 namespace base {
 namespace internal {
 class JobTaskSource;
 class PooledTaskRunnerDelegate;
 }
+
+class TaskTraits;
+enum class TaskPriority : uint8_t;
 
 // Delegate that's passed to Job's worker task, providing an entry point to
 // communicate with the scheduler. To prevent deadlocks, JobDelegate methods
