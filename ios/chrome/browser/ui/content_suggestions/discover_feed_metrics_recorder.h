acb9bd728b779f742094bf3150e36eb8e6b57884
adamta <adamta@google.com>
2021-06-07 18:41:06 +0000

    [iOS] Preserve scroll position and focused omnibox on rotation
    Will keep the same scroll position when rotating the refactored NTP,
    instead of scrolling the NTP back to the top. Also keeps the omnibox
    focused if necessary instead of unfocusing it.
    
    Adds a user action metric for when users rotate the NTP with the feed
    visible, so we can track issues more easily and see how often users
    rotate.
    
    A bit of refactoring and code clean up along the way.
    
    Bug: 1192571
    Change-Id: Iafdf4a368106aa60eeeedbb92f7538850bf4e15b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2905052
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/master@{#889862}

--- ios/chrome/browser/ui/content_suggestions/discover_feed_metrics_recorder.h
+++ ios/chrome/browser/ui/content_suggestions/discover_feed_metrics_recorder.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // feed.
 - (void)recordInfiniteFeedTriggered;
 
+// Record metrics for when the user changes the device orientation with the feed
+// visible.
+- (void)recordDeviceOrientationChanged:(UIDeviceOrientation)orientation;
+
 // Record metrics for when the user selects the 'Learn More' item in the feed
 // header menu.
 - (void)recordHeaderMenuLearnMoreTapped;
