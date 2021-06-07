c076bf3cf4ddf5cefa22b479bccc607d6ba63fcd
Kent Tamura <tkent@chromium.org>
2021-06-07 09:16:15 +0000

    SVG Text NG: Implement getSubStringLength() and getComputedTextLength()
    * Add FragmentItemsInLogicalOrder(), which returns a list of
      NGFragmentItem in the specified LayoutObject, in the logical order.
    
    * NGSvgTextQuery::NumberOfCharacters() is updated to use it.
    
    * Add NGSvgTextQuery::SubStringLength().  It uses
      FragmentItemsInLogicalOrder().
    
    * Implement getSubStringLength() and getComputedTextLength() with
      NGSvgTextQuery.
    
    Specifications:
    https://svgwg.org/svg2-draft/text.html#__svg__SVGTextContentElement__getSubStringLength
    https://svgwg.org/svg2-draft/text.html#__svg__SVGTextContentElement__getComputedTextLength
    
    
    Bug: 1179585
    Change-Id: I60694595e6b021f6c136bdabc75cabd09062c9ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2942117
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889690}

--- third_party/blink/renderer/core/svg/svg_text_content_element.cc
+++ third_party/blink/renderer/core/svg/svg_text_content_element.cc
@@ -116,8 +116,10 @@ float SVGTextContentElement::getComputedTextLength() {
   GetDocument().UpdateStyleAndLayoutForNode(this,
                                             DocumentUpdateReason::kJavaScript);
   auto* layout_object = GetLayoutObject();
-  if (IsNGTextOrInline(layout_object))
-    return 1.0f;  // TODO(1179585): Implement this.
+  if (IsNGTextOrInline(layout_object)) {
+    NGSvgTextQuery query(*layout_object);
+    return query.SubStringLength(0, query.NumberOfCharacters());
+  }
   return SVGTextQuery(layout_object).TextLength();
 }
 
@@ -142,7 +144,7 @@ float SVGTextContentElement::getSubStringLength(
 
   auto* layout_object = GetLayoutObject();
   if (IsNGTextOrInline(layout_object))
-    return 0.0f;  // TODO(1179585): Implement this.
+    return NGSvgTextQuery(*layout_object).SubStringLength(charnum, nchars);
   return SVGTextQuery(layout_object).SubStringLength(charnum, nchars);
 }
 
