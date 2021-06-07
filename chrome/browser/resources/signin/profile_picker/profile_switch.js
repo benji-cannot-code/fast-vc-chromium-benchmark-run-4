c23896381fdfff44353413ac1511cab8018da02b
Alex Ilin <alexilin@chromium.org>
2021-06-07 16:45:48 +0000

    Display an enterprise badge in the profile switch screen
    Attach an enterprise icon to the avatar if a profile is managed.
    
    Screenshots:
    - Light mode: https://drive.google.com/file/d/18EdPNzyUBaq3O2fm3cWame3-aRg7rJj6/view?usp=sharing
    - Dark mode: https://drive.google.com/file/d/1Op4OtMcxZ5HhztgQspzOZRG2Ka4QgvKU/view?usp=sharing
    
    Bug: 1217099
    Change-Id: I2f507f72fcddcff50965cbab6476c2396521b826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2943934
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889796}

--- chrome/browser/resources/signin/profile_picker/profile_switch.js
+++ chrome/browser/resources/signin/profile_picker/profile_switch.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/icons.m.js';
 import './profile_picker_shared_css.js';
 
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
