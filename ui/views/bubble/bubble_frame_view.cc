7507928e5856fab3f2806b9debde2947255ed5dc
Dana Fried <dfried@chromium.org>
2021-06-07 23:22:29 +0000

    Pipe 'display visible arrow' logic throgh bubble frame view
    By making the setter avaialble on the frame view, a bubble delegate may
    specify that it should display a visible arrow, but the setting is not
    easily available to other code (unlike the arrow itself, which has a
    public setter on the delegate itself). This is because the presence of
    an arrow is stylistic to a category of bubbles, and should not generally
    be set on a bubble-by-bubble basis.
    
    See bug and upstream CL for more info.
    
    Bug: 1215802
    Change-Id: Ie2066636024ddb5bf9a7b2ba2139f805bf9b7892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2937457
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889990}

--- ui/views/bubble/bubble_frame_view.cc
+++ ui/views/bubble/bubble_frame_view.cc
@@ -614,6 +614,14 @@ BubbleBorder::Arrow BubbleFrameView::GetArrow() const {
   return bubble_border_->arrow();
 }
 
+void BubbleFrameView::SetDisplayVisibleArrow(bool display_visible_arrow) {
+  bubble_border_->set_visible_arrow(display_visible_arrow);
+}
+
+bool BubbleFrameView::GetDisplayVisibleArrow() const {
+  return bubble_border_->visible_arrow();
+}
+
 void BubbleFrameView::SetBackgroundColor(SkColor color) {
   bubble_border_->set_background_color(color);
   UpdateClientViewBackground();
@@ -941,6 +949,7 @@ ADD_PROPERTY_METADATA(BubbleFrameView::PreferredArrowAdjustment,
                       PreferredArrowAdjustment)
 ADD_PROPERTY_METADATA(int, CornerRadius)
 ADD_PROPERTY_METADATA(BubbleBorder::Arrow, Arrow)
+ADD_PROPERTY_METADATA(bool, DisplayVisibleArrow)
 ADD_PROPERTY_METADATA(SkColor, BackgroundColor, ui::metadata::SkColorConverter)
 END_METADATA
 
