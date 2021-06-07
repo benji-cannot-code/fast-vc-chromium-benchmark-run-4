c6db0947eee66531ebbec3fd937bfff9515ecefe
Anastasiia N <anastasiian@chromium.org>
2021-06-07 08:37:30 +0000

    Revert "capture_mode: Improve test coverage of the recording service"
    This reverts commit f00fa63bb112c012367f630245ce3c0b15b5e893.
    
    Reason for revert: "ash_unittests" failing on builder "Linux ChromiumOS MSan Tests"
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/23810/overview
    
    Original change's description:
    > capture_mode: Improve test coverage of the recording service
    >
    > The recording service had some test coverage as follows:
    >   - Many browser tests that verifies recording end-to-end, and
    >     asserts the produced webm files are valid.
    >   - Many unit tests in ash_unittests that used an instance of
    >     a fake service, just to verify that ash sends it the right
    >     events via its mojo APIs.
    >
    > However, this did not allow us to test the *real* recording
    > service in unit tests, and be able to poke into its internals.
    > This lead to a serious regression (https://crbug.com/1214023)
    > in window recording which could not be caught by any of our
    > tests. That's due to the fact that catching such regression
    > requires intercepting the received video frames from the Viz
    > capturer and verifying them.
    >
    > This CL fixes this by getting rid of the fake service, and
    > instead creates an in-process instance of the *real* service
    > that can be tested via a newly-added testing APIs.
    >
    > BUG=1215819
    > TEST=Existing unittests. Added a new regression test for
    >      https://crbug.com/1214023 that currently fails without
    >      the fix, and hence disabled. This verifies the new
    >      testing infra structure works as intended.
    >
    > Change-Id: I144b4441bc4a31c7a02d4850281e2c9452fd7c59
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939182
    > Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#889412}
    
    Bug: 1215819
    Change-Id: Ie35f58c3331c379c0e28112b4d459321221f2af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944027
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Swapnil Gupta <swapnilgupta@google.com>
    Owners-Override: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889678}

--- chrome/browser/ui/ash/chrome_capture_mode_delegate.h
+++ chrome/browser/ui/ash/chrome_capture_mode_delegate.h
@@ -52,7 +52,6 @@ class ChromeCaptureModeDelegate : public ash::CaptureModeDelegate {
       mojo::PendingReceiver<media::mojom::AudioStreamFactory> receiver)
       override;
   void OnSessionStateChanged(bool started) override;
-  void OnServiceRemoteReset() override;
 
  private:
   // Used to temporarily disable capture mode in certain cases for which neither
