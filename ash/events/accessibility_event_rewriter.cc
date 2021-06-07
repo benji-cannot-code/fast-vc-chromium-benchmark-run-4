5974193ab1f0737cd522342ddf408eb98c1222bc
Katie Dektar <katie@chromium.org>
2021-06-07 19:29:12 +0000

    Fix debug builds NOTREACHED crash on Chrome OS on Linux.
    This allows developers to run Switch Access on Chrome OS emulated
    on Linux when building with debug mode.
    
    Bug: 1217204
    Change-Id: I0a97392f8571de395dc4817d6343604e2b8d571a
    AX-Relnotes: Chrome OS on Linux will no longer crash when DCHECK enabled.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944578
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889889}

--- ash/events/accessibility_event_rewriter.cc
+++ ash/events/accessibility_event_rewriter.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/keyboard/keyboard_util.h"
 #include "ash/public/cpp/accessibility_event_rewriter_delegate.h"
 #include "ash/shell.h"
+#include "base/system/sys_info.h"
 #include "ui/chromeos/events/event_rewriter_chromeos.h"
 #include "ui/events/devices/device_data_manager.h"
 #include "ui/events/event.h"
@@ -30,8 +31,9 @@ ui::InputDeviceType GetInputDeviceType(
     return ui::INPUT_DEVICE_USB;
   if (switch_access_device_type == kSwitchAccessBluetoothDevice)
     return ui::INPUT_DEVICE_BLUETOOTH;
-
-  NOTREACHED();
+  // On Chrome OS emulated on Linux, the keyboard is always "UNKNOWN".
+  if (base::SysInfo::IsRunningOnChromeOS())
+    NOTREACHED();
   return ui::INPUT_DEVICE_UNKNOWN;
 }
 }  // namespace
