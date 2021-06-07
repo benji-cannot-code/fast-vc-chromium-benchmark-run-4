a19124285de43f6832d6460f0c780a6893642428
Oriol Brufau <obrufau@igalia.com>
2021-06-07 19:40:33 +0000

    [DevTools] Let shorthand override longhand with less priority (backend)
    This patch completes the corresponding frontend patch:
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2903495
    
    For example, with this CSS:
    
      .selector {
        margin-top: 100px;
        margin: 0;
      }
    
    The CSS inspector used to show
    
      margin-top: 100px;
      margin: ▾ 0;
        m̶a̶r̶g̶i̶n̶-̶t̶o̶p̶:̶ ̶1̶0̶0̶p̶x̶;̶
        margin-right: 0px;
        margin-bottom: 0px;
        margin-left: 0px;
    
    The frontend patch changed it to
    
      m̶a̶r̶g̶i̶n̶-̶t̶o̶p̶:̶ ̶1̶0̶0̶p̶x̶;̶
      margin: ▾ 0;
        margin-right: 0px;
        margin-bottom: 0px;
        margin-left: 0px;
    
    And this patch adds the missing declaration:
    
      m̶a̶r̶g̶i̶n̶-̶t̶o̶p̶:̶ ̶1̶0̶0̶p̶x̶;̶
      margin: ▾ 0;
        margin-top: 0px;
        margin-right: 0px;
        margin-bottom: 0px;
        margin-left: 0px;
    
    Bug: 1210677
    
    TEST=http/tests/devtools/elements/elements-panel-styles.js
    TEST=http/tests/devtools/elements/styles-2/inactive-properties-with-shorthands.js
    
    Change-Id: Ia64fde43ea63490e6ef9557e832b0aee0afcaa0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2904215
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#889897}

--- third_party/blink/web_tests/http/tests/devtools/elements/styles-1/add-new-rule-inline-style-csp.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/styles-1/add-new-rule-inline-style-csp.js
@@ -110,6 +110,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       return;
     var allProperties = style.allProperties();
     for (var i = 0; i < allProperties.length; ++i)
-      TestRunner.addResult(allProperties[i].text);
+      TestRunner.addResult(allProperties[i].propertyText);
   }
 })();
