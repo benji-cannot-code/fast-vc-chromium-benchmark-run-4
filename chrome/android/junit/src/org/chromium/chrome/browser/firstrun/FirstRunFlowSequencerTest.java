6308ba0ebec09247e61fddb9c3ed7726ff49f245
Alice Wang <aliceywang@chromium.org>
2021-06-07 09:35:59 +0000

    [Android] Migrate tryGetGoogleAccounts() usage in FirstRunFlowSequencer
    This CL migrates the usage of the blocking method
    tryGetGoogleAccounts() to the async version in
    FirstRunFlowSequencer.
    
    Bug: 1211294
    Change-Id: I12b00809b8417fcee40ae4cc45da5bdc516ee7aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935541
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889693}

--- chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencerTest.java
@@ -27,10 +27,10 @@ import org.robolectric.shadows.multidex.ShadowMultiDex;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.test.util.browser.Features;
+import org.chromium.components.signin.AccountUtils;
 import org.chromium.components.signin.ChildAccountStatus;
 
 import java.util.Collections;
-import java.util.List;
 
 /**
  * Tests FirstRunFlowSequencer which contains the core logic of what should be shown during the
@@ -43,8 +43,8 @@ public class FirstRunFlowSequencerTest {
     public TestRule mFeaturesProcessorRule = new Features.JUnitProcessor();
 
     /** Information for Google OS account */
-    private static final String GOOGLE_ACCOUNT_TYPE = "com.google";
-    private static final String DEFAULT_ACCOUNT = "test@gmail.com";
+    private static final Account DEFAULT_ACCOUNT =
+            AccountUtils.createAccountFromName("test@gmail.com");
 
     /**
      * Testing version of FirstRunFlowSequencer that allows us to override all needed checks.
@@ -57,7 +57,6 @@ public class FirstRunFlowSequencerTest {
         public boolean isFirstRunFlowComplete;
         public boolean isSignedIn;
         public boolean isSyncAllowed;
-        public List<Account> googleAccounts;
         public boolean shouldSkipFirstUseHints;
         public boolean isFirstRunEulaAccepted;
         public boolean shouldShowDataReductionPage;
@@ -87,11 +86,6 @@ public class FirstRunFlowSequencerTest {
         @Override
         public boolean isSyncAllowed() {
             return isSyncAllowed;
-        }
-
-        @Override
-        public List<Account> getGoogleAccounts() {
-            return googleAccounts;
         }
 
         @Override
@@ -140,10 +134,9 @@ public class FirstRunFlowSequencerTest {
         mSequencer.isFirstRunFlowComplete = false;
         mSequencer.isSignedIn = false;
         mSequencer.isSyncAllowed = true;
-        mSequencer.googleAccounts = Collections.emptyList();
         mSequencer.shouldSkipFirstUseHints = false;
         mSequencer.shouldShowDataReductionPage = false;
-        mSequencer.initializeSharedState(ChildAccountStatus.NOT_CHILD);
+        mSequencer.initializeSharedState(ChildAccountStatus.NOT_CHILD, Collections.emptyList());
 
         mSequencer.processFreEnvironmentPreNative();
         assertTrue(mSequencer.calledOnFlowIsKnown);
@@ -164,11 +157,10 @@ public class FirstRunFlowSequencerTest {
         mSequencer.isFirstRunFlowComplete = false;
         mSequencer.isSignedIn = false;
         mSequencer.isSyncAllowed = true;
-        mSequencer.googleAccounts =
-                Collections.singletonList(new Account(DEFAULT_ACCOUNT, GOOGLE_ACCOUNT_TYPE));
         mSequencer.shouldSkipFirstUseHints = false;
         mSequencer.shouldShowDataReductionPage = false;
-        mSequencer.initializeSharedState(ChildAccountStatus.REGULAR_CHILD);
+        mSequencer.initializeSharedState(
+                ChildAccountStatus.REGULAR_CHILD, Collections.singletonList(DEFAULT_ACCOUNT));
 
         mSequencer.processFreEnvironmentPreNative();
         assertTrue(mSequencer.calledOnFlowIsKnown);
@@ -189,11 +181,10 @@ public class FirstRunFlowSequencerTest {
         mSequencer.isFirstRunFlowComplete = false;
         mSequencer.isSignedIn = false;
         mSequencer.isSyncAllowed = true;
-        mSequencer.googleAccounts = Collections.emptyList();
         mSequencer.shouldSkipFirstUseHints = false;
         mSequencer.shouldShowDataReductionPage = true;
         mSequencer.shouldShowSearchEnginePage = false;
-        mSequencer.initializeSharedState(ChildAccountStatus.NOT_CHILD);
+        mSequencer.initializeSharedState(ChildAccountStatus.NOT_CHILD, Collections.emptyList());
 
         mSequencer.processFreEnvironmentPreNative();
         assertTrue(mSequencer.calledOnFlowIsKnown);
@@ -214,11 +205,10 @@ public class FirstRunFlowSequencerTest {
         mSequencer.isFirstRunFlowComplete = false;
         mSequencer.isSignedIn = false;
         mSequencer.isSyncAllowed = true;
-        mSequencer.googleAccounts = Collections.emptyList();
         mSequencer.shouldSkipFirstUseHints = false;
         mSequencer.shouldShowDataReductionPage = true;
         mSequencer.shouldShowSearchEnginePage = true;
-        mSequencer.initializeSharedState(ChildAccountStatus.NOT_CHILD);
+        mSequencer.initializeSharedState(ChildAccountStatus.NOT_CHILD, Collections.emptyList());
 
         mSequencer.processFreEnvironmentPreNative();
         assertTrue(mSequencer.calledOnFlowIsKnown);
