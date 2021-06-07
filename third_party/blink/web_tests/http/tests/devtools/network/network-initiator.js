199ea39e2be1b43479454e9d2cf26f65fad4d135
Antonio Sartori <antoniosartori@google.com>
2021-06-07 07:51:47 +0000

    [css] Fix initiator for parser initiated fetches
    This CL fixes a wrong behaviour introduced by
    https://crrev.com/c/2896901, which reported the wrong initiator to
    devtools for fetches initiated by inline CSS.
    
    If the referrer is "about:client", we avoid filling the initiator name
    in CSSImageValue::PrepareFetch and CSSFontFaceSrcValue::PrepareFetch.
    This way, the initiator will be computed using the currently parsed
    document by InspectorNetworkAgent::BuildInitiatorObject.
    
    Bug: 1215272
    Change-Id: I8f9b4a04b8bbcdd4007921ba53ebc6dc2a186c39
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2932516
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889672}

--- third_party/blink/web_tests/http/tests/devtools/network/network-initiator.js
+++ third_party/blink/web_tests/http/tests/devtools/network/network-initiator.js
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     dumpInitiator('module1.js');
     dumpInitiator('module2.js');
     dumpInitiator('example.ttf');
+    dumpInitiator('example2.ttf');
     TestRunner.completeTest();
   }
 })();
