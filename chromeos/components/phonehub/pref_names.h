1a7ad477f73492b0871cf4f12cbbbfab49171a85
Josh Nohle <nohle@google.com>
2021-06-07 15:49:34 +0000

    [PhoneHub] Send one-time notification-access update to observers
    When a notification-access status is received from the phone, notify
    observes if access is granted even if the access status has not changed.
    This is performed at most once for a user.
    
    Background:
    Some valid notification users where unintentionally disabled when
    updating from M89 to M90+, when the notification feature state went from
    enabled-by-default to disabled-by-default. To re-enable those users, we
    once and only once notify observers if access has been granted by the
    phone even if the status hasn't changed. Notably, the
    MultideviceSetupStateUpdate will decide whether or not the notification
    feature should be enabled. Notifications will be re-enabled for users
    who 1) have the top-level Phone Hub feature enabled and 2) have the
    notifications toggle in its default state. Users who have explicitly
    modified the notifications toggle will not be affected. See
    MultideviceSetupStateUpdater's method
    IsWaitingForAccessToInitiallyEnableNotifications() for more details.
    
    This change can be deprecated when there are no more active Phone Hub
    notification users on M89.
    
    Manually verified that notifications will be re-enabled for only the
    affected users. Notably, users that explicitly disabled notifications
    will not have the feature re-enabled. Also verified that existing setup
    flows are unchanged.
    
    Bug: 1215559
    Change-Id: I2f6dcdc333639df79fe42c6206bfcc2ba1f2281d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940344
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889770}

--- chromeos/components/phonehub/pref_names.h
+++ chromeos/components/phonehub/pref_names.h
@@ -14,6 +14,7 @@ extern const char kNotificationAccessStatus[];
 extern const char kHideOnboardingUi[];
 extern const char kIsAwaitingVerifiedHost[];
 extern const char kHasDismissedSetupRequiredUi[];
+extern const char kNeedsOneTimeNotificationAccessUpdate[];
 
 }  // namespace prefs
 }  // namespace phonehub
