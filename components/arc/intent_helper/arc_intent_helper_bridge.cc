38b19f8af10a5b2130ad3b6ebd3913d3207e40ea
Tim Sergeant <tsergeant@chromium.org>
2021-06-07 02:30:52 +0000

    [CrOS WebAPKs] Support launching WebAPKs with a share intent
    WebAPKs will now request a launch using a new method,
    `OnOpenAppWithIntent`, which includes a `LaunchIntent` containing shared
    data. This currently only includes shared text/title, but in a follow
    up we'll add support for files as well. This information gets passed
    through App Service to the Web Apps system using the existing Intent
    handling infrastructure.
    
    This change is guarded behind a feature flag which is currently disabled
    by default.
    
    Bug: b/178656389
    Change-Id: I9642f017be0f49aadbac2d441fc7a81524c9b0be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927091
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889638}

--- components/arc/intent_helper/arc_intent_helper_bridge.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/values.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
+#include "components/arc/arc_features.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/audio/arc_audio_bridge.h"
 #include "components/arc/intent_helper/control_camera_app_delegate.h"
@@ -325,6 +326,27 @@ void ArcIntentHelperBridge::OnDownloadAdded(
 
   for (auto& observer : observer_list_)
     observer.OnArcDownloadAdded(relative_path, owner_package_name);
+}
+
+void ArcIntentHelperBridge::OnOpenAppWithIntent(
+    const GURL& start_url,
+    arc::mojom::LaunchIntentPtr intent) {
+  // Fall-back to the previous behavior where the web app opens without any
+  // share data.
+  if (!base::FeatureList::IsEnabled(arc::kEnableWebAppShareFeature)) {
+    if (intent->data)
+      OnOpenWebApp(intent->data->spec());
+    return;
+  }
+
+  if (!start_url.is_valid())
+    return;
+
+  RecordOpenType(ArcIntentHelperOpenType::WEB_APP);
+
+  // Web app launches should only be invoked on HTTPS URLs.
+  if (start_url.SchemeIs(url::kHttpsScheme))
+    g_open_url_delegate->OpenAppWithIntent(start_url, std::move(intent));
 }
 
 ArcIntentHelperBridge::GetResult ArcIntentHelperBridge::GetActivityIcons(
