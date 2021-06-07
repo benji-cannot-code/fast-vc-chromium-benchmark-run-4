3723a6bd7fc442b82597952d321976c931c82220
Sven Zheng <svenzheng@chromium.org>
2021-06-07 23:58:53 +0000

    Delete use-fake-ml-service-for-test flag
    The fake is now added in
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/test/base/chromeos/fake_ash_test_chrome_browser_main_extra_parts.cc;l=20
    So we don't need to inject fake components into
    production Chromium anymore.
    
    Bug: 1210172
    Change-Id: Ibd075160eff054353bd5ee8e710c3689c892b92a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944581
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890007}

--- ash/constants/ash_switches.h
+++ ash/constants/ash_switches.h
@@ -232,8 +232,6 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kUnfilteredBluetoothDevices[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kUpdateRequiredAueForTest[];
-COMPONENT_EXPORT(ASH_CONSTANTS)
-extern const char kUseFakeMLServiceForTest[];
 
 ////////////////////////////////////////////////////////////////////////////////
 
--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -187,7 +187,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/portal_detector/network_portal_detector_stub.h"
 #include "chromeos/network/system_token_cert_db_storage.h"
 #include "chromeos/services/cros_healthd/public/cpp/service_connection.h"
-#include "chromeos/services/machine_learning/public/cpp/fake_service_connection.h"
 #include "chromeos/services/machine_learning/public/cpp/service_connection.h"
 #include "chromeos/settings/cros_settings_names.h"
 #include "chromeos/system/statistics_provider.h"
@@ -1178,14 +1177,6 @@ void ChromeBrowserMainPartsChromeos::PostBrowserStart() {
       wake_lock_provider.InitWithNewPipeAndPassReceiver());
   dark_resume_controller_ = std::make_unique<system::DarkResumeController>(
       std::move(wake_lock_provider));
-
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kUseFakeMLServiceForTest)) {
-    auto* fake_service_connection =
-        new chromeos::machine_learning::FakeServiceConnectionImpl();
-    chromeos::machine_learning::ServiceConnection::
-        UseFakeServiceConnectionForTesting(fake_service_connection);
-  }
 
   chromeos::machine_learning::ServiceConnection::GetInstance()->Initialize();
 
