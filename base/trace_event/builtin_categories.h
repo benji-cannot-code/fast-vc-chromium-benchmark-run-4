a35b1fbda2a57261dcb880903039cd0287723b31
Christopher Lam <calamity@chromium.org>
2021-06-07 06:39:51 +0000

    Add Chrometto tracing for bad navigations in System Web Apps.
    This CL adds a UMA histogram WebApp.SystemApps.BadNavigate.Type which
    will be used as a Chrometto trigger, so that we can view the state of
    the machine indicated by the TRACE_EVENT added in this CL, which we
    need to help understand a crash in SWAs.
    
    Bug: 1201820
    Change-Id: If961a44eaffcff04ee39695a0dcbd6fe6cb604e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886399
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889659}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -152,6 +152,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("stadia_rtc")                                                        \
   X("startup")                                                           \
   X("sync")                                                              \
+  X("system_apps")                                                       \
   X("test_gpu")                                                          \
   X("thread_pool")                                                       \
   X("toplevel")                                                          \
