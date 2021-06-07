ca62ab882f45b572821ac7e43238e24893010bfb
Darren Shen <shend@chromium.org>
2021-06-07 00:14:36 +0000

    ime: Combine ResetForRulebased and OnCompositionCanceled.
    Both do the same thing, but one is used for rule-based languages and
    the other one for latin-based languages.
    
    Combine them into OnCompositionCanceledBySystem (to make it clear that
    it doesn't trigger when the composition was canceled by IME).
    
    Bug: 1194372
    Change-Id: Id8d5b0460bcf7bda715407239f7f1b19765ab972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2936198
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889627}

--- chromeos/services/ime/decoder/system_engine_unittest.cc
+++ chromeos/services/ime/decoder/system_engine_unittest.cc
@@ -219,7 +219,7 @@ TEST_F(SystemEngineTest, OnCompositionCanceledSendsMessageToSharedLib) {
 
   EXPECT_CALL(decoder_entry_points_, Process).With(EqualsProto(expected_proto));
 
-  client->OnCompositionCanceled();
+  client->OnCompositionCanceledBySystem();
   client.FlushForTesting();
 }
 
