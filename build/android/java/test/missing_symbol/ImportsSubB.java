dda469c2a979a9f6c02dc153cfadf90693307a32
Peter Kotwicz <pkotwicz@chromium.org>
2021-06-07 14:47:05 +0000

    [Android Build] Suggest missing dependencies from compile
    This CL:
    - Makes compile_java.py and turbine.py suggest dependencies to add
      when the symbol in an import statement cannot be found.
    - Colorizes turbine.py output
    
    BUG=1108362
    TEST=//build/android/test:android_lint_test
    
    (Disabling presubmit because presubmit is currently broken for binary
    files)
    
    No-Presubmit: true
    Change-Id: I62ab9586f819e2224f0fad647b664ea7159dbe56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927337
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889754}

---
+++ build/android/java/test/missing_symbol/ImportsSubB.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package test.missing_symbol;
+
+import test.missing_symbol.sub.SubB;
+
+public class ImportsSubB {
+    public ImportsSubB() {
+        new SubB().foo();
+    }
+}
---
+++ build/android/java/test/missing_symbol/sub/BInMethodSignature.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package test.missing_symbol.sub;
+
+import test.missing_symbol.B;
+
+public class BInMethodSignature {
+    public B foo() {
+        return new B();
+    }
+}
