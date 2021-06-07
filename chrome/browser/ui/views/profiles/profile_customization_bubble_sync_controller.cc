3492937ea35b11ea6582787494d40c2f59bee129
Jan Krcal <jkrcal@chromium.org>
2021-06-07 16:59:35 +0000

    [Profile customization bubble] Add more CHECKs for debugging
    Bug: 1213356, 1213109
    Change-Id: I2c16dc6965a60c336fe25e27a57ff45e879fd6e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940858
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#889806}

--- chrome/browser/ui/views/profiles/profile_customization_bubble_sync_controller.cc
+++ chrome/browser/ui/views/profiles/profile_customization_bubble_sync_controller.cc
@@ -21,6 +21,7 @@ namespace {
 bool CanSyncStart(syncer::SyncService* sync_service) {
   if (!sync_service || !sync_service->CanSyncFeatureStart())
     return false;
+  CHECK(sync_service->GetUserSettings());
   if (!sync_service->GetUserSettings()->GetSelectedTypes().Has(
           syncer::UserSelectableType::kThemes)) {
     return false;
@@ -132,6 +133,7 @@ void ProfileCustomizationBubbleSyncController::Init() {
     return;
   }
 
+  CHECK(theme_service_->GetThemeSyncableService());
   theme_observation_.Observe(theme_service_->GetThemeSyncableService());
 
   // Observe also the sync service to abort waiting for theme sync if the user
