008428ccc6ba0a9ed6810fda099af1a885692d4e
Kevin McNee <mcnee@chromium.org>
2021-06-08 21:31:52 +0000

    Implement RecursivelyRegisterFrameSinkIds in terms of RFH::ForEachRenderFrameHost
    GetRenderWidgetHostViewsInWebContentsTree collects RenderWidgetHostViews
    that descend from a given WebContents' main frame including inner
    WebContentses.
    
    Bug: 754726, 1208438
    Change-Id: Ib4e6af886705526aeabe4ba3aeac944b293ba79a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2889206
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890454}

--- content/browser/site_per_process_browsertest.cc
+++ content/browser/site_per_process_browsertest.cc
@@ -1050,8 +1050,8 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessBrowserTest, CrossSiteIframe) {
   {
     // There should be only one RenderWidgetHost when there are no
     // cross-process iframes.
-    std::set<RenderWidgetHostView*> views_set =
-        web_contents()->GetRenderWidgetHostViewsInTree();
+    std::set<RenderWidgetHostViewBase*> views_set =
+        web_contents()->GetRenderWidgetHostViewsInWebContentsTree();
     EXPECT_EQ(1U, views_set.size());
   }
 
@@ -1087,8 +1087,8 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessBrowserTest, CrossSiteIframe) {
   {
     // There should be now two RenderWidgetHosts, one for each process
     // rendering a frame.
-    std::set<RenderWidgetHostView*> views_set =
-        web_contents()->GetRenderWidgetHostViewsInTree();
+    std::set<RenderWidgetHostViewBase*> views_set =
+        web_contents()->GetRenderWidgetHostViewsInWebContentsTree();
     EXPECT_EQ(2U, views_set.size());
   }
   RenderFrameProxyHost* proxy_to_parent =
@@ -1138,8 +1138,8 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessBrowserTest, CrossSiteIframe) {
             child->current_frame_host()->GetProcess());
   EXPECT_NE(rph, child->current_frame_host()->GetProcess());
   {
-    std::set<RenderWidgetHostView*> views_set =
-        web_contents()->GetRenderWidgetHostViewsInTree();
+    std::set<RenderWidgetHostViewBase*> views_set =
+        web_contents()->GetRenderWidgetHostViewsInWebContentsTree();
     EXPECT_EQ(2U, views_set.size());
   }
   EXPECT_EQ(proxy_to_parent, child->render_manager()->GetProxyToParent());
