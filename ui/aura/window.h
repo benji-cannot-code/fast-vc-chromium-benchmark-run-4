a8d85f1a410fa653a4a7fc021950fd9e94ca2064
Ivan Murashov <ivan.murashov@lge.com>
2021-06-08 06:58:45 +0000

    GCC: Make the tokens of the '#error' directives to be a string constants
    Make the tokens of the '#error' directives to be a string constants
    to avoid warnings about the usage of apostrophes.
    Clang doesn't claims about it, but GCC does.
    A warnings example for GCC 9.3.0:
    ../../ui/aura/window.h:49:67: warning: missing terminating ' character
       49 | #error This file must not be included on macOS; Chromium Mac doesn't use Aura.
          |                                                                   ^
    ../../ui/views/widget/native_widget_aura.h:26:67: warning: missing terminating ' character
       26 | #error This file must not be included on macOS; Chromium Mac doesn't use Aura.
          |                                                                   ^
    
    Bug: 819294
    Change-Id: I18d9afbb212fa882b178fcb8895aa4e87d0e535a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940542
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#890132}

--- ui/aura/window.h
+++ ui/aura/window.h
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/native_widget_types.h"
 
 #if defined(OS_APPLE)
-#error This file must not be included on macOS; Chromium Mac doesn't use Aura.
+#error "This file must not be included on macOS; Chromium Mac doesn't use Aura."
 #endif
 
 namespace cc {
--- ui/views/widget/native_widget_aura.h
+++ ui/views/widget/native_widget_aura.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/wm/public/activation_delegate.h"
 
 #if defined(OS_MAC)
-#error This file must not be included on macOS; Chromium Mac doesn't use Aura.
+#error "This file must not be included on macOS; Chromium Mac doesn't use Aura."
 #endif
 
 namespace aura {
