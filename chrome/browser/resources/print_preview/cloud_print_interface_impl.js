1fa1048f645be0746eec1f98084fa9306b0e7d91
Gavin Williams <gavinwill@chromium.org>
2021-06-08 07:41:25 +0000

    print preview: Record metric for cloud printer requests
    - The metric will capture if cloud printer requests are not completing
      thus hanging print preview
    
    Bug: 1217397
    Change-Id: I7c50c0d9c8d4d90cb7bb209644f3f2a5039d3c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946060
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890142}

--- chrome/browser/resources/print_preview/cloud_print_interface_impl.js
+++ chrome/browser/resources/print_preview/cloud_print_interface_impl.js
@@ -10,6 +10,8 @@ import {NativeEventTarget as EventTarget} from 'chrome://resources/js/cr/event_t
 import {CloudPrintInterface, CloudPrintInterfaceErrorEventDetail, CloudPrintInterfaceEventType} from './cloud_print_interface.js';
 import {parseCloudDestination} from './data/cloud_parsers.js';
 import {CloudOrigins, DestinationOrigin} from './data/destination.js';
+import {PrinterType} from './data/destination_match.js';
+import {Metrics, MetricsContext} from './metrics.js';
 // <if expr="chromeos or lacros">
 import {NativeLayerCrosImpl} from './native_layer_cros.js';
 // </if>
@@ -143,6 +145,9 @@ export class CloudPrintInterfaceImpl {
           this.onSearchDone_.bind(this, isRecent));
       this.outstandingCloudSearchRequests_.push(cpRequest);
       this.sendOrQueueRequest_(cpRequest);
+
+      MetricsContext.getPrinters(PrinterType.CLOUD_PRINTER)
+          .record(Metrics.PrintPreviewInitializationEvents.FUNCTION_INITIATED);
     }, this);
   }
 
@@ -459,6 +464,9 @@ export class CloudPrintInterfaceImpl {
               searchDone: lastRequestForThisOrigin,
             }
           }));
+
+      MetricsContext.getPrinters(PrinterType.CLOUD_PRINTER)
+          .record(Metrics.PrintPreviewInitializationEvents.FUNCTION_SUCCESSFUL);
     } else {
       const errorEventDetail = this.createErrorEventDetail_(request);
       errorEventDetail.user = activeUser;
@@ -466,6 +474,9 @@ export class CloudPrintInterfaceImpl {
       this.eventTarget_.dispatchEvent(new CustomEvent(
           CloudPrintInterfaceEventType.SEARCH_FAILED,
           {detail: errorEventDetail}));
+
+      MetricsContext.getPrinters(PrinterType.CLOUD_PRINTER)
+          .record(Metrics.PrintPreviewInitializationEvents.FUNCTION_FAILED);
     }
   }
 
