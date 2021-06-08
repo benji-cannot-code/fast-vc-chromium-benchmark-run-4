efb1652623298d096b12f38fbe281ed34faccdc2
Christian Dullweber <dullweber@chromium.org>
2021-06-08 15:43:15 +0000

    AccuracyTips: Add icon rows to dialog
    Present information within accuracy tip as rows that start with an icon.
    
    Screenshot: https://crbug.com/1210891#c5
    Bug: 1210891
    Change-Id: I07e03d1a570ba9486110765211cc35229b03e96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927582
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890263}

--- chrome/browser/ui/views/page_info/accuracy_tip_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/page_info/accuracy_tip_bubble_view_browsertest.cc
@@ -9,11 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/callback_helpers.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/accuracy_tips/accuracy_service_factory.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/page_info/chrome_accuracy_tip_ui.h"
+#include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/page_info/page_info_bubble_view_base.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -131,4 +134,20 @@ IN_PROC_BROWSER_TEST_F(AccuracyTipBubbleViewBrowserTest, OpenLearnMoreLink) {
   histogram_tester()->ExpectUniqueSample(
       "Privacy.AccuracyTip.AccuracyTipInteraction",
       AccuracyTipUI::Interaction::kLearnMorePressed, 1);
+}
+
+// Render test for accuracy tip ui.
+class AccuracyTipBubbleViewDialogBrowserTest : public DialogBrowserTest {
+ public:
+  // DialogBrowserTest:
+  void ShowUi(const std::string& name) override {
+    ShowAccuracyTipDialog(browser()->tab_strip_model()->GetActiveWebContents(),
+                          accuracy_tips::AccuracyTipStatus::kMisinformation,
+                          base::DoNothing());
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(AccuracyTipBubbleViewDialogBrowserTest,
+                       InvokeUi_default) {
+  ShowAndVerifyUi();
 }
