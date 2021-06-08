ac7c74d1a76339a0c7d1fb985013660f8157b288
Stephen Roe <steveroe@google.com>
2021-06-08 18:07:25 +0000

    Fix typo in new resource paths.
    Bug: 1207714
    Change-Id: I58eaadcbb3c0f9c332e126eaec44ce64d3620e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946455
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Stephen Roe <steveroe@google.com>
    Cr-Commit-Position: refs/heads/master@{#890342}

--- chrome/services/speech/soda/soda_test_paths.h
+++ chrome/services/speech/soda/soda_test_paths.h
@@ -16,7 +16,7 @@ namespace soda {
 constexpr base::FilePath::CharType kSodaResourcePath[] =
     FILE_PATH_LITERAL("third_party/soda-mac64/resources");
 
-constexpr base::FilePath::CharType kSodaTestBinaryRelativePath =
+constexpr base::FilePath::CharType kSodaTestBinaryRelativePath[] =
     FILE_PATH_LITERAL("libsoda_for_testing.so");
 
 #elif defined(OS_WIN) && defined(ARCH_CPU_64_BITS)
@@ -24,7 +24,7 @@ constexpr base::FilePath::CharType kSodaTestBinaryRelativePath =
 constexpr base::FilePath::CharType kSodaResourcePath[] =
     FILE_PATH_LITERAL("third_party/soda-win64/resources");
 
-constexpr base::FilePath::CharType kSodaTestBinaryRelativePath =
+constexpr base::FilePath::CharType kSodaTestBinaryRelativePath[] =
     FILE_PATH_LITERAL("SODA_for_testing.dll");
 
 #elif defined(OS_WIN) && defined(ARCH_CPU_32_BITS)
@@ -32,7 +32,7 @@ constexpr base::FilePath::CharType kSodaTestBinaryRelativePath =
 constexpr base::FilePath::CharType kSodaResourcePath[] =
     FILE_PATH_LITERAL("third_party/soda-win32/resources");
 
-constexpr base::FilePath::CharType kSodaTestBinaryRelativePath =
+constexpr base::FilePath::CharType kSodaTestBinaryRelativePath[] =
     FILE_PATH_LITERAL("SODA_for_testing.dll");
 
 #elif defined(OS_LINUX)
