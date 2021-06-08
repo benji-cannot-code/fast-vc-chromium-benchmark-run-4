fc1cf04ecec584c3b6ba0020a3c833cc2ae50f4b
Yusuke Sato <yusukes@chromium.org>
2021-06-08 21:58:14 +0000

    arc: Improve ArcBootPhaseMonitorBridge::AddObserver()
    Currently, if AddObserver() is called after ARC is fully booted,
    OnBootCompleted() is never called back for the observer. This CL
    changes the behavior so that OnBootCompleted() is always called
    once for each observer even when the observer is added late.
    
    BUG=b:186278424
    TEST=unit_tests
    
    Change-Id: I10df386e7ef8c118b43470288739d0c55fdd3991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945702
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890468}

--- chrome/browser/ash/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.cc
+++ chrome/browser/ash/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.cc
@@ -102,6 +102,8 @@ ArcBootPhaseMonitorBridge::~ArcBootPhaseMonitorBridge() {
 
 void ArcBootPhaseMonitorBridge::AddObserver(Observer* observer) {
   observers_.AddObserver(observer);
+  if (boot_completed_)
+    observer->OnBootCompleted();
 }
 
 void ArcBootPhaseMonitorBridge::RemoveObserver(Observer* observer) {
