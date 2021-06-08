f96004a71735a7d55a4e17b0f7f19031e09876f2
Patrick Monette <pmonette@chromium.org>
2021-06-08 20:28:04 +0000

    Remove DisableBackgroundModuleInspection
    This is no longer being experimented with.
    
    Bug: 928846
    Change-Id: I8a19698d570dd076adddb8234f0bdf49df1e1499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945553
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890415}

--- chrome/browser/win/conflicts/module_inspector.cc
+++ chrome/browser/win/conflicts/module_inspector.cc
@@ -73,9 +73,6 @@ void WriteInspectionResultCacheOnBackgroundSequence(
 }  // namespace
 
 // static
-constexpr base::Feature ModuleInspector::kDisableBackgroundModuleInspection;
-
-// static
 constexpr base::Feature ModuleInspector::kWinOOPInspectModuleFeature;
 
 // static
@@ -100,9 +97,7 @@ ModuleInspector::ModuleInspector(
               &ModuleInspector::MaybeUpdateInspectionResultsCache,
               base::Unretained(this))),
       has_new_inspection_results_(false),
-      connection_error_retry_count_(kConnectionErrorRetryCount),
-      background_inspection_disabled_(
-          base::FeatureList::IsEnabled(kDisableBackgroundModuleInspection)) {
+      connection_error_retry_count_(kConnectionErrorRetryCount) {
   // Use BEST_EFFORT as those will only run after startup is finished.
   content::BrowserThread::PostBestEffortTask(
       FROM_HERE, base::SequencedTaskRunnerHandle::Get(),
@@ -135,14 +130,6 @@ void ModuleInspector::IncreaseInspectionPriority() {
 
   // Assume startup is finished to immediately begin inspecting modules.
   OnStartupFinished();
-
-  // Special case where this instance could be ready to start inspecting but
-  // wasn't because background inspection was disabled.
-  if (background_inspection_disabled_ && inspection_results_cache_read_ &&
-      !queue_.empty()) {
-    background_inspection_disabled_ = false;
-    StartInspectingModule();
-  }
 }
 
 bool ModuleInspector::IsIdle() {
@@ -231,9 +218,6 @@ void ModuleInspector::StartInspectingModule() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(inspection_results_cache_read_);
   DCHECK(!queue_.empty());
-
-  if (background_inspection_disabled_)
-    return;
 
   const ModuleInfoKey& module_key = queue_.front();
 
