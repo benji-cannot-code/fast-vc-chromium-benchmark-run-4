7650b293e1102bf89364525dfc9215c32e085335
James Cook <jamescook@chromium.org>
2021-06-08 19:01:07 +0000

    chromeos: Clean up app list AppsGridView::ClearSelectedView()
    There are two similar "clear selected view" methods. Collapse them into
    one to simplify the AppListItemView::GridDelegate interface, which I
    need to use for recent apps.
    
    ClearSelectedView(view) used to check if `view` was the currently
    selected view. AFAICT that's only needed in one OnBlur() method, so
    move the check out to that method.
    
    ClearAnySelectedView() calls SchedulePaint() on the currently selected
    view. AFAICT that's not necessary. It was added in this CL:
    https://codereview.chromium.org/68513009/
    
    I manually tested all the ClearAnySelectedView() code paths and they
    seem to work fine without the SchedulePaint(). This includes context
    menus, mouse dragging, and folder closing.
    
    Bug: 1216085
    Test: ash_unittests, manual on device
    Change-Id: Ice0a7d0adff76406ff9af773f6eef842e4736409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946136
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890372}

--- ash/app_list/bubble/recent_apps_view.cc
+++ ash/app_list/bubble/recent_apps_view.cc
@@ -99,8 +99,7 @@ class RecentAppsView::GridDelegateImpl : public AppListItemView::GridDelegate {
   // AppListItemView::GridDelegate:
   bool IsInFolder() const override { return false; }
   void SetSelectedView(AppListItemView* view) override {}
-  void ClearSelectedView(AppListItemView* view) override {}
-  void ClearAnySelectedView() override {}
+  void ClearSelectedView() override {}
   bool IsSelectedView(const AppListItemView* view) const override {
     return false;
   }
