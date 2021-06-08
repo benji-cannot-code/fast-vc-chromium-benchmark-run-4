0113908e19e6318052729b843bf70054ef389f48
Yunke Zhou <yunkez@google.com>
2021-06-08 06:17:24 +0000

    Block all notifications before session starts
    Block pop-up type of notification during OOBE and Login states except
    for battery notifications.
    
    Bug: 1122613, 1215453
    Change-Id: Ia78c276eb2a9c9d2d16a804602232e9532fa3da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2932680
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890119}

--- ash/system/message_center/session_state_notification_blocker.cc
+++ ash/system/message_center/session_state_notification_blocker.cc
@@ -49,12 +49,6 @@ bool CalculateShouldShowPopup() {
   SessionControllerImpl* const session_controller =
       Shell::Get()->session_controller();
 
-  // Enable popup in OOBE and login screen to display system notifications
-  // (wifi, etc.).
-  if (session_controller->GetSessionState() == SessionState::OOBE ||
-      session_controller->GetSessionState() == SessionState::LOGIN_PRIMARY)
-    return true;
-
   if (session_controller->IsRunningInAppMode() ||
       session_controller->GetSessionState() != SessionState::ACTIVE) {
     return false;
@@ -119,10 +113,8 @@ bool SessionStateNotificationBlocker::ShouldShowNotificationAsPopup(
   if (session_controller->IsRunningInAppMode())
     return false;
 
-  if (notification.notifier_id().profile_id.empty() &&
-      notification.priority() >= message_center::SYSTEM_PRIORITY) {
+  if (notification.id() == BatteryNotification::kNotificationId)
     return true;
-  }
 
   return should_show_popup_;
 }
