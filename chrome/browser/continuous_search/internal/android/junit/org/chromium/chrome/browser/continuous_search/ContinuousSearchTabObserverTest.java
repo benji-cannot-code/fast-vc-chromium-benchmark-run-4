e47986bd1b282fffb76d39bad8fdd0664fedfaff
ckitagawa <ckitagawa@chromium.org>
2021-06-08 19:24:58 +0000

    [CSN] Fix crash if results return after destroy
    This CL fixes a crash that can happen if the results from
    SearchResultExtractorProducer are returned after a tab is destroyed.
    
    Now the owner of SearchResultExtractorProducer cancels and resets the
    object when the tab is destroyed. This stops the producer from
    providing results which resulted in a crash when calling
    getOrCreateForTab() on a destroyed tab.
    
    Fixed: 1217085
    Change-Id: Ie71f025304f34dc9ba0a46c444de962fad028f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945379
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890385}

--- chrome/browser/continuous_search/internal/android/junit/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabObserverTest.java
+++ chrome/browser/continuous_search/internal/android/junit/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabObserverTest.java
@@ -55,6 +55,7 @@ public class ContinuousSearchTabObserverTest {
     @Mock
     private SearchResultExtractorProducer.Natives mSearchResultExtractorProducerJniMock;
 
+    private boolean mNeedsTeardown;
     private GURL mSrpUrl;
     private GURL mNonSrpUrl;
     private ContinuousSearchTabObserver mObserver;
@@ -89,12 +90,15 @@ public class ContinuousSearchTabObserverTest {
 
         mObserver = new ContinuousSearchTabObserver(mTabMock);
         verify(mTabMock, times(1)).addObserver(eq(mObserver));
+        mNeedsTeardown = true;
     }
 
     @After
     public void tearDown() {
-        mObserver.onDestroyed(mTabMock);
-        verify(mTabMock, times(1)).removeObserver(mObserver);
+        if (mNeedsTeardown) {
+            mObserver.onDestroyed(mTabMock);
+            verify(mTabMock, times(1)).removeObserver(mObserver);
+        }
     }
 
     /**
@@ -168,6 +172,33 @@ public class ContinuousSearchTabObserverTest {
 
         mObserver.onCloseContents(mTabMock);
         inOrder.verify(mUserDataMock).invalidateData();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    /**
+     * Verifies that results are requested and handled when a SRP URL is loaded.
+     */
+    @Test
+    public void testLoadSrpUrlThenCloseTab() {
+        InOrder inOrder = inOrder(mUserDataMock, mProducerMock, mTabMock);
+
+        mObserver.onPageLoadStarted(mTabMock, mSrpUrl);
+        inOrder.verify(mUserDataMock).updateCurrentUrl(eq(mSrpUrl));
+
+        mObserver.onPageLoadFinished(mTabMock, mSrpUrl);
+        inOrder.verify(mUserDataMock).updateCurrentUrl(eq(mSrpUrl));
+        inOrder.verify(mProducerMock).fetchResults(eq(mSrpUrl), eq(TEST_QUERY));
+
+        // Close the tab.
+        mNeedsTeardown = false;
+        mObserver.onDestroyed(mTabMock);
+        ContinuousNavigationUserDataImpl.setInstanceForTesting(null);
+
+        // Metadata will not be returned as the destruction of the tab will cancel the request.
+        inOrder.verify(mProducerMock).cancel();
+        inOrder.verify(mUserDataMock).invalidateData();
+        inOrder.verify(mTabMock).removeObserver(mObserver);
+
         inOrder.verifyNoMoreInteractions();
     }
 
