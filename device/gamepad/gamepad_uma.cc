87af2837872072f55436b4d71a89dcb1d996d345
Peter Kasting <pkasting@chromium.org>
2021-06-08 13:41:19 +0000

    Convert narrowing casts from T{} to static_cast<T>(): device/
    The style guide encourages use of T{}-style casts under the assumption
    that the compiler will warn about narrowing.  This warning is currently
    disabled; enabling it requires fixing up the cases that narrow.
    
    Bug: 1216696
    Change-Id: I5d3c1138bde50bb9931d5e72b462030b4c5ea758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946382
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#890227}

--- device/gamepad/gamepad_uma.cc
+++ device/gamepad/gamepad_uma.cc
@@ -20,7 +20,7 @@ void RecordConnectedGamepad(GamepadId gamepad_id) {
   auto gamepad_id_as_underlying_type =
       static_cast<std::underlying_type<GamepadId>::type>(gamepad_id);
   base::UmaHistogramSparse("Gamepad.KnownGamepadConnectedWithId",
-                           int32_t{gamepad_id_as_underlying_type});
+                           static_cast<int32_t>(gamepad_id_as_underlying_type));
 }
 
 void RecordUnknownGamepad(GamepadSource source) {
