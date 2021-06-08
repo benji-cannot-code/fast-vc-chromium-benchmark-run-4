095305131f918d53686d8870174bb63037dad183
Anton Swifton <swifton@google.com>
2021-06-08 01:43:32 +0000

    Implement noise cancellation toggle logic.
    1. The toggle reflects the InputNoiseCancellationEnabled
    preference.
    2. The toggle isn't displayed on devices without noise cancellation.
    
    Bug: 1205197
    Test: ash_unittests
    Change-Id: I5fadb41f9bbccee98b7a623be4000e81c5efa778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910185
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Anton Swifton <swifton@google.com>
    Cr-Commit-Position: refs/heads/master@{#890049}

--- ash/system/audio/unified_audio_detailed_view_controller.cc
+++ ash/system/audio/unified_audio_detailed_view_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/audio/unified_audio_detailed_view_controller.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/audio/audio_detailed_view.h"
 #include "ash/system/tray/detailed_view_delegate.h"
