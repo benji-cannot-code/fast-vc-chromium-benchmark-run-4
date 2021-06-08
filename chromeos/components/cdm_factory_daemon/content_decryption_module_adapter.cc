a990a96419ac394d78ae87a7b4a65e814c193437
Xiaohan Wang <xhwang@chromium.org>
2021-06-08 17:44:37 +0000

    media: Rename CdmSessionClosedReason enums
    After more W3C Media Working Group discussion, we finalized the enum
    list. This CL updates the implementation to use the updated list.
    
    See https://github.com/w3c/encrypted-media/pull/487
    
    Bug: 1208618
    Change-Id: I2974666d1e48a5434cc23f183351f65b3f72c8a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939319
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890325}

--- chromeos/components/cdm_factory_daemon/content_decryption_module_adapter.cc
+++ chromeos/components/cdm_factory_daemon/content_decryption_module_adapter.cc
@@ -540,7 +540,7 @@ ContentDecryptionModuleAdapter::~ContentDecryptionModuleAdapter() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DVLOG(2) << __func__;
   cdm_session_tracker_.CloseRemainingSessions(
-      session_closed_cb_, media::CdmSessionClosedReason::kCdmUnavailable);
+      session_closed_cb_, media::CdmSessionClosedReason::kInternalError);
 }
 
 void ContentDecryptionModuleAdapter::OnConnectionError() {
@@ -558,7 +558,7 @@ void ContentDecryptionModuleAdapter::OnConnectionError() {
   cdm_promise_adapter_.Clear(
       media::CdmPromiseAdapter::ClearReason::kConnectionError);
   cdm_session_tracker_.CloseRemainingSessions(
-      session_closed_cb_, media::CdmSessionClosedReason::kCdmUnavailable);
+      session_closed_cb_, media::CdmSessionClosedReason::kInternalError);
 }
 
 void ContentDecryptionModuleAdapter::RejectTrackedPromise(
