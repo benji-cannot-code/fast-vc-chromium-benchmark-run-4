401e521b1b3231efbcc0ed92f3691925d5668605
Curtis McMullan <curtismcmullan@chromium.org>
2021-06-08 03:04:30 +0000

    cros-suggest: record multiword user pref
    Record the user pref setting for predictive writing feature
    in UMA.
    
    Bug: 1146266
    Change-Id: I7ac063dc0a41478d078792ef910271bd5fbf7eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939531
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890072}

--- chrome/browser/chromeos/input_method/assistive_suggester.cc
+++ chrome/browser/chromeos/input_method/assistive_suggester.cc
@@ -186,6 +186,10 @@ void RecordAssistiveUserPrefForEmoji(bool value) {
   base::UmaHistogramBoolean("InputMethod.Assistive.UserPref.Emoji", value);
 }
 
+void RecordAssistiveUserPrefForMultiWord(bool value) {
+  base::UmaHistogramBoolean("InputMethod.Assistive.UserPref.MultiWord", value);
+}
+
 void RecordAssistiveNotAllowed(AssistiveType type) {
   base::UmaHistogramEnumeration("InputMethod.Assistive.NotAllowed", type);
 }
@@ -316,6 +320,8 @@ AssistiveSuggester::AssistiveSuggester(InputMethodEngine* engine,
       profile_->GetPrefs()->GetBoolean(prefs::kAssistPersonalInfoEnabled));
   RecordAssistiveUserPrefForEmoji(
       profile_->GetPrefs()->GetBoolean(prefs::kEmojiSuggestionEnabled));
+  RecordAssistiveUserPrefForMultiWord(
+      profile_->GetPrefs()->GetBoolean(prefs::kAssistPredictiveWritingEnabled));
 }
 
 bool AssistiveSuggester::IsAssistiveFeatureEnabled() {
