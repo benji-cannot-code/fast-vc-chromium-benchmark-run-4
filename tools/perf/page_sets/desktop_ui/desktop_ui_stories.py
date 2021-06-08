ac8abc13abf138bbff0ca6e87ff5a6f94d6d8dce
Yuheng Huang <yuhengh@chromium.org>
2021-06-08 18:35:15 +0000

    WebUI tab strip: Add new metric and memory measure for benchmark story
    Change-Id: If5b5114b4ffe26a0bb9d5b5b327345dc2707fab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2889187
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890360}

--- tools/perf/page_sets/desktop_ui/desktop_ui_stories.py
+++ tools/perf/page_sets/desktop_ui/desktop_ui_stories.py
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import sys
 from telemetry import story
 from page_sets.desktop_ui import download_shelf_story, tab_search_story, webui_tab_strip_story
+from page_sets.desktop_ui.ui_devtools_utils import IsMac
 
 
 class DesktopUIStorySet(story.StorySet):
@@ -41,6 +41,7 @@ class DesktopUIStorySet(story.StorySet):
 
   WEBUI_TAB_STRIP_STORIES = [
       webui_tab_strip_story.WebUITabStripStoryCleanSlate,
+      webui_tab_strip_story.WebUITabStripStoryMeasureMemory,
       webui_tab_strip_story.WebUITabStripStoryTop10,
       webui_tab_strip_story.WebUITabStripStoryTop10Loading,
   ]
@@ -69,7 +70,7 @@ class DesktopUIStorySet(story.StorySet):
           ]))
 
     # WebUI Tab Strip is not available on Mac.
-    if sys.platform != 'darwin':
+    if not IsMac():
       for cls in self.WEBUI_TAB_STRIP_STORIES:
         self.AddStory(
             cls(self, [
--- tools/perf/page_sets/desktop_ui/webui_tab_strip_story.py
+++ tools/perf/page_sets/desktop_ui/webui_tab_strip_story.py
@@ -3,14 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from page_sets.desktop_ui.js_utils import MEASURE_JS_MEMORY
 from page_sets.desktop_ui.multitab_story import MultiTabStory
 from page_sets.desktop_ui.ui_devtools_utils import ClickOn
 from page_sets.desktop_ui.url_list import TOP_URL
 from page_sets.desktop_ui.webui_utils import Inspect
 
 WEBUI_TAB_STRIP_BENCHMARK_UMA = [
+    'TabStrip.Tab.Views.ActivationAction',
+    'TabStrip.Tab.WebUI.ActivationAction',
     'WebUITabStrip.CloseAction',
     'WebUITabStrip.CloseTabAction',
+    'WebUITabStrip.LoadCompletedTime',
+    'WebUITabStrip.LoadDocumentTime',
     'WebUITabStrip.OpenAction',
     'WebUITabStrip.OpenDuration',
     'WebUITabStrip.TabActivation',
@@ -27,7 +32,10 @@ class WebUITabStripStory(MultiTabStory):
   def RunPageInteractions(self, action_runner):
     ClickOn(self._devtools, 'WebUITabCounterButton')
     action_runner = Inspect(action_runner.tab.browser, WEBUI_TAB_STRIP_URL)
+    action_runner.ExecuteJavaScript(MEASURE_JS_MEMORY %
+                                    'used_js_heap_size_begin')
     self.InteractWithPage(action_runner)
+    action_runner.ExecuteJavaScript(MEASURE_JS_MEMORY % 'used_js_heap_size_end')
 
   def InteractWithPage(self, action_runner):
     self.ScrollTabs(action_runner)
@@ -65,6 +73,28 @@ class WebUITabStripStoryTop10Loading(WebUITabStripStory):
   URL_LIST = TOP_URL[:10]
   URL = URL_LIST[0]
   WAIT_FOR_NETWORK_QUIESCENCE = False
+
+
+class WebUITabStripStoryMeasureMemory(WebUITabStripStory):
+  NAME = 'webui_tab_strip:measure_memory'
+  URL_LIST = []
+  URL = 'about:blank'
+  WAIT_FOR_NETWORK_QUIESCENCE = False
+
+  def WillStartTracing(self, chrome_trace_config):
+    super(WebUITabStripStoryMeasureMemory,
+          self).WillStartTracing(chrome_trace_config)
+    chrome_trace_config.category_filter.AddExcludedCategory('*')
+    chrome_trace_config.category_filter.AddIncludedCategory('blink.console')
+    chrome_trace_config.category_filter.AddDisabledByDefault(
+        'disabled-by-default-memory-infra')
+
+  def GetExtraTracingMetrics(self):
+    return super(WebUITabStripStoryMeasureMemory,
+                 self).GetExtraTracingMetrics() + ['memoryMetric']
+
+  def InteractWithPage(self, action_runner):
+    action_runner.MeasureMemory(deterministic_mode=True)
 
 
 SCROLL_ELEMENT_FUNCTION = '''
