648d656c9d1645e9c3787565fdacf0b5c5c7ece7
Vasiliy Telezhnikov <vasilyt@chromium.org>
2021-06-08 19:03:14 +0000

    Make sure VulkanImage has right queue on Android
    This CL makes sure we create VulkanImage with the right queue (external
    or foreign) and transfer queue ownership when necessary.
    
    Bug: 1213233
    Change-Id: Ia68689e7e91aa0ad3a226ae5632c2b18676a8b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941044
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890374}

--- gpu/vulkan/vulkan_image_android.cc
+++ gpu/vulkan/vulkan_image_android.cc
@@ -20,7 +20,8 @@ bool VulkanImage::InitializeFromGpuMemoryBufferHandle(
     VkFormat format,
     VkImageUsageFlags usage,
     VkImageCreateFlags flags,
-    VkImageTiling image_tiling) {
+    VkImageTiling image_tiling,
+    uint32_t queue_family_index) {
   if (gmb_handle.type != gfx::GpuMemoryBufferType::ANDROID_HARDWARE_BUFFER) {
     DLOG(ERROR) << "gmb_handle.type is not supported. type:" << gmb_handle.type;
     return false;
@@ -133,8 +134,7 @@ bool VulkanImage::InitializeFromGpuMemoryBufferHandle(
     return false;
   }
 
-  // VkImage is imported from external.
-  queue_family_index_ = VK_QUEUE_FAMILY_FOREIGN_EXT;
+  queue_family_index_ = queue_family_index;
 
   if (ahb_format_props.format == VK_FORMAT_UNDEFINED) {
     ycbcr_info_.emplace(VK_FORMAT_UNDEFINED, ahb_format_props.externalFormat,
