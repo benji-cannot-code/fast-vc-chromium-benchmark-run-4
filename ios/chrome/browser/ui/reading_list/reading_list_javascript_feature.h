7e633671bad445f7a9441f41178a338d3667d22b
Chris Lu <thegreenfrog@chromium.org>
2021-06-08 03:23:28 +0000

    [ios] Create ReadingListJavaScriptFeature
    This JavaScriptFeature object will listen to the result of
    the new dom_distiller.js execution, which calculates quantifier
    inferences about the DOM's elements. This implementation
    is based on the implementation in https://github.com/chromium/
    dom-distiller/blob/master/heuristics/distillable/extract_features.js
    with a few modifications for performance.
    
    The visibility check is removed for its poor performance, and
    getElementsByTagName() is used for its faster runtime. In addition,
    the logic is restructured to only need to retrieve the <p> and <pre>
    elements once.
    
    Bug: 1195978
    Change-Id: Ie3a7257d4c98f5313d0ee2518a183ead34180c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911296
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890077}

---
+++ ios/chrome/browser/ui/reading_list/reading_list_javascript_feature.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_JAVASCRIPT_FEATURE_H_
+#define IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_JAVASCRIPT_FEATURE_H_
+
+#include "base/no_destructor.h"
+#import "ios/web/public/js_messaging/java_script_feature.h"
+
+// A feature which receives DOM attributes and uses them to determine Time to
+// Read and Distillibility.
+class ReadingListJavaScriptFeature : public web::JavaScriptFeature {
+ private:
+  friend class base::NoDestructor<ReadingListJavaScriptFeature>;
+
+  ReadingListJavaScriptFeature();
+  ~ReadingListJavaScriptFeature() override;
+
+  ReadingListJavaScriptFeature(const ReadingListJavaScriptFeature&) = delete;
+  ReadingListJavaScriptFeature& operator=(const ReadingListJavaScriptFeature&) =
+      delete;
+
+  // JavaScriptFeature:
+  absl::optional<std::string> GetScriptMessageHandlerName() const override;
+  void ScriptMessageReceived(web::WebState* web_state,
+                             const web::ScriptMessage& message) override;
+};
+
+#endif  // IOS_CHROME_BROWSER_UI_READING_LIST_READING_LIST_JAVASCRIPT_FEATURE_H_
