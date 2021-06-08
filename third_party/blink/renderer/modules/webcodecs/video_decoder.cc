c33ed4e6dd15da7ca785f43137cd96192b2cd7c9
Dale Curtis <dalecurtis@chromium.org>
2021-06-08 21:46:36 +0000

    [WebCodecs] Implement keyframe enforcement for flush, configure.
    This change checks that the first encoded chunk after a flush() or
    configure() is a keyframe. For AV1, VP8, VP9, and H.264 the
    bitstream is analyzed to ensure the packet is actually a key frame.
    
    Since Chromium doesn't have a Theora or xHE-AAC parser, no
    analysis is done for those codecs beyond checking that the type
    field is set to 'key'. On platforms without libvpx or libaom, we
    will also skip bitstream analysis.
    
    I originally wrote this change using the media::Vp8Parser and
    media::Vp9Parser classes, but since they are not currently used
    on Android that brings in 16k+ of binary size. As such I've
    switched to using libvpx for bitstream analysis.
    
    This change also adds AV1 and VP8 to our list of tested codecs.
    This change also fixes an issue with the AudioEncoder where it
    was marking all frames as delta frames instead of keyframes.
    
    Fixed: 1213680
    Change-Id: Ibd8fb9daeb6519c8a72aec66bdc4711261113e28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2921021
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: James Zern <jzern@google.com>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890463}

--- third_party/blink/renderer/modules/webcodecs/video_decoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_decoder.cc
@@ -42,13 +42,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
+#include "third_party/libaom/libaom_buildflags.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
+
+#if BUILDFLAG(ENABLE_LIBAOM)
+#include "third_party/libaom/source/libaom/aom/aom_decoder.h"
+#include "third_party/libaom/source/libaom/aom/aomdx.h"
+#endif
+
+#if BUILDFLAG(ENABLE_LIBVPX)
+#include "third_party/libvpx/source/libvpx/vpx/vp8dx.h"
+#include "third_party/libvpx/source/libvpx/vpx/vpx_decoder.h"
+#endif
 
 #if BUILDFLAG(USE_PROPRIETARY_CODECS)
 #include "media/filters/h264_to_annex_b_bitstream_converter.h"
 #include "media/formats/mp4/box_definitions.h"
-#endif  // BUILDFLAG(USE_PROPRIETARY_CODECS)
+#endif
 
 namespace blink {
 
@@ -184,6 +195,37 @@ VideoDecoderConfig* CopyConfig(const VideoDecoderConfig& config) {
     copy->setOptimizeForLatency(config.optimizeForLatency());
 
   return copy;
+}
+
+void ParseAv1KeyFrame(const media::DecoderBuffer& buffer, bool* is_key_frame) {
+#if BUILDFLAG(ENABLE_LIBAOM)
+  aom_codec_stream_info_t stream_info = {0};
+  auto status = aom_codec_peek_stream_info(
+      &aom_codec_av1_dx_algo, buffer.data(), buffer.data_size(), &stream_info);
+  *is_key_frame = (status == AOM_CODEC_OK) && stream_info.is_kf;
+#endif
+}
+
+void ParseVpxKeyFrame(const media::DecoderBuffer& buffer,
+                      media::VideoCodec codec,
+                      bool* is_key_frame) {
+#if BUILDFLAG(ENABLE_LIBVPX)
+  vpx_codec_stream_info_t stream_info = {0};
+  stream_info.sz = sizeof(vpx_codec_stream_info_t);
+  auto status = vpx_codec_peek_stream_info(
+      codec == media::kCodecVP8 ? &vpx_codec_vp8_dx_algo
+                                : &vpx_codec_vp9_dx_algo,
+      buffer.data(), static_cast<uint32_t>(buffer.data_size()), &stream_info);
+  *is_key_frame = (status == VPX_CODEC_OK) && stream_info.is_kf;
+#endif
+}
+
+void ParseH264KeyFrame(const media::DecoderBuffer& buffer, bool* is_key_frame) {
+#if BUILDFLAG(USE_PROPRIETARY_CODECS)
+  auto result = media::mp4::AVC::AnalyzeAnnexB(
+      buffer.data(), buffer.data_size(), std::vector<media::SubsampleEntry>());
+  *is_key_frame = result.is_keyframe.value_or(false);
+#endif
 }
 
 }  // namespace
@@ -425,16 +467,19 @@ CodecConfigEval VideoDecoder::MakeMediaConfig(const ConfigType& config,
                                               String* out_console_message) {
   DCHECK(out_media_config);
   DCHECK(out_console_message);
-  return MakeMediaVideoDecoderConfig(config, *out_media_config,
+  auto result = MakeMediaVideoDecoderConfig(config, *out_media_config,
 #if BUILDFLAG(USE_PROPRIETARY_CODECS)
-                                     h264_converter_ /* out */,
-                                     h264_avcc_ /* out */,
+                                            h264_converter_ /* out */,
+                                            h264_avcc_ /* out */,
 #endif  // BUILDFLAG(USE_PROPRIETARY_CODECS)
-                                     *out_console_message);
+                                            *out_console_message);
+  if (result == CodecConfigEval::kSupported)
+    current_codec_ = out_media_config->codec();
+  return result;
 }
 
 media::StatusOr<scoped_refptr<media::DecoderBuffer>>
-VideoDecoder::MakeDecoderBuffer(const InputType& chunk) {
+VideoDecoder::MakeDecoderBuffer(const InputType& chunk, bool verify_key_frame) {
   uint8_t* src = static_cast<uint8_t*>(chunk.data()->Data());
   size_t src_size = chunk.data()->ByteLength();
 
@@ -474,7 +519,19 @@ VideoDecoder::MakeDecoderBuffer(const InputType& chunk) {
                  std::numeric_limits<int64_t>::max() - 1)));
   }
 
-  decoder_buffer->set_is_key_frame(chunk.type() == "key");
+  bool is_key_frame = chunk.type() == "key";
+  if (verify_key_frame) {
+    if (current_codec_ == media::kCodecVP9 ||
+        current_codec_ == media::kCodecVP8) {
+      ParseVpxKeyFrame(*decoder_buffer, current_codec_, &is_key_frame);
+    } else if (current_codec_ == media::kCodecAV1) {
+      ParseAv1KeyFrame(*decoder_buffer, &is_key_frame);
+    } else if (current_codec_ == media::kCodecH264) {
+      ParseH264KeyFrame(*decoder_buffer, &is_key_frame);
+    }
+  }
+
+  decoder_buffer->set_is_key_frame(is_key_frame);
 
   return decoder_buffer;
 }
