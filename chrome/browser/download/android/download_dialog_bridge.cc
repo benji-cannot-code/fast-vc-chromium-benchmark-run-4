aa9cbabd91f918fa013d9f7cf19a10c3910c1114
Xing Liu <xingliu@chromium.org>
2021-06-08 14:53:50 +0000

    Download: Change download later file size trigger to 200Mb.
    This matches the DownloadLater.gcl file. Requested by PM in the PRD.
    
    Bug: NONE
    Change-Id: I06e70b69b8a0db7508473b00f6d764d28da7834e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946052
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890248}

--- chrome/browser/download/android/download_dialog_bridge.cc
+++ chrome/browser/download/android/download_dialog_bridge.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/android/window_android.h"
 
 // Default minimum file size in kilobyte to trigger download later feature.
-const int64_t kDownloadLaterDefaultMinFileSizeKb = 300 * 1024;
+const int64_t kDownloadLaterDefaultMinFileSizeKb = 204800;
 
 // -----------------------------------------------------------------------------
 // DownloadDialogResult
