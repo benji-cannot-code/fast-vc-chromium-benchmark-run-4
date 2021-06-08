50dbe6dbb6ef7c6fb3f07069133873ef333ade78
Guido Urdaneta <guidou@chromium.org>
2021-06-08 06:03:56 +0000

    [BreakoutBox] Add audio transferring optimizer
    Bug: 1184992
    Change-Id: Ifd62948e5f0bcdbcb9171915785d036525c85bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923846
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890115}

--- third_party/blink/renderer/modules/breakout_box/media_stream_video_track_underlying_source.cc
+++ third_party/blink/renderer/modules/breakout_box/media_stream_video_track_underlying_source.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
-#include "third_party/webrtc/api/frame_transformer_interface.h"
 
 namespace blink {
 
