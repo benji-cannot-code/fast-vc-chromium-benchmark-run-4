662797344d6473b2eff19fd3e7e14ff26c90eebf
Salvador Guerrero <salg@google.com>
2021-06-08 20:23:40 +0000

    Revert "Make sure VulkanImage has right queue on Android"
    This reverts commit 5d1c40ad06cb136efcd5050998f9bf0564ffc454.
    
    Reason for revert: Compile error on Android ASAN (dbg) builder.
    
    Original change's description:
    > Make sure VulkanImage has right queue on Android
    >
    > This CL makes sure we create VulkanImage with the right queue (external
    > or foreign) and transfer queue ownership when necessary.
    >
    > Bug: 1213233
    > Change-Id: Ia68689e7e91aa0ad3a226ae5632c2b18676a8b88
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941044
    > Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#890374}
    
    Bug: 1213233
    Change-Id: Ia1dcb21058f19aa3fed5cae07a13fb0f1f457bda
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945707
    Owners-Override: Salvador Guerrero <salg@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/master@{#890411}

--- gpu/vulkan/vulkan_image.h
+++ gpu/vulkan/vulkan_image.h
@@ -69,9 +69,8 @@ class COMPONENT_EXPORT(VULKAN) VulkanImage {
       const gfx::Size& size,
       VkFormat format,
       VkImageUsageFlags usage,
-      VkImageCreateFlags flags,
-      VkImageTiling image_tiling,
-      uint32_t queue_family_index);
+      VkImageCreateFlags flags = 0,
+      VkImageTiling image_tiling = VK_IMAGE_TILING_OPTIMAL);
 
   static std::unique_ptr<VulkanImage> Create(
       VulkanDeviceQueue* device_queue,
@@ -166,8 +165,7 @@ class COMPONENT_EXPORT(VULKAN) VulkanImage {
       VkFormat format,
       VkImageUsageFlags usage,
       VkImageCreateFlags flags,
-      VkImageTiling image_tiling,
-      uint32_t queue_family_index);
+      VkImageTiling image_tiling);
 
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
   bool InitializeWithExternalMemoryAndModifiers(VulkanDeviceQueue* device_queue,
