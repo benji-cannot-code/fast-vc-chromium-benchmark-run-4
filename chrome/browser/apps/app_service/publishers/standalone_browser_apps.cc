857894646ca3c35911287c4117931b9736de9ed6
Jae Hoon Kim <kimjae@chromium.org>
2021-06-08 00:37:01 +0000

    Rootfs Lacros offline consistency + Icon simplification
    1. Remove the pending Icon state for lacros as it will always fallback
    to rootfs lacros-chrome instantly or use a pre-existing stateful
    lacros-chrome.
    
    2. Stronger check on compatible path of stateful lacros-chrome as the
    previous check on registered components wasn't fully correct. This is
    because a component can be registered, but not compatible.
    
    BUG=chromium:1217344
    TEST=deploy chrome + unit_tests
    
    Change-Id: I12224cbd3ac8e3f28f14c3ec9ee82f3bcc19e287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945805
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890024}

--- chrome/browser/apps/app_service/publishers/standalone_browser_apps.cc
+++ chrome/browser/apps/app_service/publishers/standalone_browser_apps.cc
@@ -32,8 +32,7 @@ StandaloneBrowserApps::StandaloneBrowserApps(
 
 StandaloneBrowserApps::~StandaloneBrowserApps() = default;
 
-apps::mojom::AppPtr StandaloneBrowserApps::GetStandaloneBrowserApp(
-    bool is_ready) {
+apps::mojom::AppPtr StandaloneBrowserApps::GetStandaloneBrowserApp() {
   apps::mojom::AppPtr app = apps::PublisherBase::MakeApp(
       apps::mojom::AppType::kStandaloneBrowser, extension_misc::kLacrosAppId,
       apps::mojom::Readiness::kReady,
@@ -41,7 +40,7 @@ apps::mojom::AppPtr StandaloneBrowserApps::GetStandaloneBrowserApp(
       apps::mojom::InstallSource::kSystem);
   // Make Lacros searchable with the term "chrome", too.
   app->additional_search_terms.push_back("chrome");
-  app->icon_key = NewIconKey(is_ready ? State::kReady : State::kLoading);
+  app->icon_key = NewIconKey(State::kReady);
   app->searchable = apps::mojom::OptionalBool::kTrue;
   app->show_in_launcher = apps::mojom::OptionalBool::kTrue;
   app->show_in_shelf = apps::mojom::OptionalBool::kTrue;
@@ -54,9 +53,6 @@ apps::mojom::IconKeyPtr StandaloneBrowserApps::NewIconKey(State state) {
   // Show different icons based on download state.
   apps::IconEffects icon_effects;
   switch (state) {
-    case State::kLoading:
-      icon_effects = apps::IconEffects::kPaused;
-      break;
     case State::kError:
       icon_effects = apps::IconEffects::kBlocked;
       break;
@@ -79,15 +75,13 @@ void StandaloneBrowserApps::Connect(
     mojo::PendingRemote<apps::mojom::Subscriber> subscriber_remote,
     apps::mojom::ConnectOptionsPtr opts) {
   auto* browser_manager = crosapi::BrowserManager::Get();
-  bool is_ready = true;
   // |browser_manager| may be null in tests. For tests, assume Lacros is ready.
   if (browser_manager && !browser_manager->IsReady()) {
-    is_ready = false;
     browser_manager->SetLoadCompleteCallback(base::BindOnce(
         &StandaloneBrowserApps::OnLoadComplete, weak_factory_.GetWeakPtr()));
   }
   std::vector<apps::mojom::AppPtr> apps;
-  apps.push_back(GetStandaloneBrowserApp(is_ready));
+  apps.push_back(GetStandaloneBrowserApp());
 
   mojo::Remote<apps::mojom::Subscriber> subscriber(
       std::move(subscriber_remote));
