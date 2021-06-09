8399379f3cb38f601b4019e25a51ceeb600552b2
Christoph Schwering <schwering@google.com>
2021-06-09 16:37:48 +0000

    [Autofill] Replaced pair of value/label vectors with vector of pairs.
    Bug: 1213313
    Change-Id: Iab2688936636676157bdcdd0b3db6f80f8a53cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935277
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Michael Bai <michaelbai@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#890810}

--- components/autofill/core/common/autofill_data_validation.cc
+++ components/autofill/core/common/autofill_data_validation.cc
@@ -36,7 +36,7 @@ bool IsValidFormFieldData(const FormFieldData& field) {
          IsValidString16(field.value) &&
          IsValidString(field.form_control_type) &&
          IsValidString(field.autocomplete_attribute) &&
-         IsValidString16Vector(field.option_contents);
+         IsValidOptionVector(field.options);
 }
 
 bool IsValidFormData(const FormData& form) {
@@ -55,6 +55,12 @@ bool IsValidPasswordFormFillData(const PasswordFormFillData& form) {
            return IsValidString16(login.username) &&
                   IsValidString16(login.password) && IsValidString(login.realm);
          });
+}
+
+bool IsValidOptionVector(const std::vector<SelectOption>& options) {
+  return options.size() <= kMaxListSize &&
+         base::ranges::all_of(options, &IsValidString16,
+                              &SelectOption::content);
 }
 
 bool IsValidString16Vector(const std::vector<std::u16string>& v) {
