8834c104d60c579627b992d272c58c19f7ec392c
Fabio Tirelo <ftirelo@chromium.org>
2021-06-09 00:53:23 +0000

    [Clank CBCM] Make CloudManagementAndroidConnectionDelegate an interface
    This CL breaks down crrev.com/c/2933817 and corresponds to the work
    that needs to be done to make the connection delegate class an interface
    before landing the Google Chrome-specific changes in the internal repo.
    This is necessary to ensure that we don't break tests downstream.
    
    Once this CL lands, we can land the downstream CL and only then
    crrev.com/c/2933817.
    
    Bug: 1210116
    Change-Id: Ide67c46c497932b55bcb039d634525be95cd931d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947343
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890536}

--- chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/CloudManagementAndroidConnectionDelegate.java
+++ chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/CloudManagementAndroidConnectionDelegate.java
@@ -5,16 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.policy;
 
-import java.util.UUID;
-
 /**
  * Delegate for cloud management functions implemented downstream for Google Chrome.
  */
-public class CloudManagementAndroidConnectionDelegate {
-    /* Returns the client ID to be used in the DM token generation. By default a random UUID is
-     * generated for development and testing purposes. Any device that uses randomly generated UUID
-     * as client id for CBCM might be wiped out from the server without notice. */
-    public String generateClientId() {
-        return UUID.randomUUID().toString();
-    }
+public interface CloudManagementAndroidConnectionDelegate {
+    /* Returns the client ID to be used in the DM token generation. */
+    public String generateClientId();
 }
