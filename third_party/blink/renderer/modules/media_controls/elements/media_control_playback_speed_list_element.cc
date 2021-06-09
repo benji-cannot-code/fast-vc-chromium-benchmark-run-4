ae2e40a95109f0709e06955b6f7e75ba1648ddde
Francois Beaufort <beaufort.francois@gmail.com>
2021-06-09 16:56:09 +0000

    Update default playback rate when user selects a playback speed
    This CL updates the default playback rate for a media element when user
    selects a playback speed in the list of playback speeds on the media
    controls.
    
    Bug: 1197485
    Change-Id: I0c765d7b9151a796a8b9dc9ebb743e0baa62ec26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2948665
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#890817}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_playback_speed_list_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_playback_speed_list_element.cc
@@ -110,6 +110,7 @@ void MediaControlPlaybackSpeedListElement::DefaultEventHandler(Event& event) {
 
     double playback_rate =
         To<Element>(target)->GetFloatingPointAttribute(PlaybackRateAttrName());
+    MediaElement().setDefaultPlaybackRate(playback_rate);
     MediaElement().setPlaybackRate(playback_rate);
 
     // Close the playback speed list.
