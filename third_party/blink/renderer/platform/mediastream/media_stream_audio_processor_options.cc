20dded6e0a98835f335502af3cb0ab76542f1120
Sam Zackrisson <saza@chromium.org>
2021-06-09 10:44:05 +0000

    Remove defunct typing detection from MediaStreamAudioProcessor
    The feature has been disconnected from its associated media constraint for some time [1].
    
    Actual processing functionality, in particular typing noise _suppression_, is unafffected by this change.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1617352
    
    Bug: chromium:1215061
    Change-Id: Icb41dd53a798089fdab1e664f24983c2b368d16c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944964
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890692}

--- third_party/blink/renderer/platform/mediastream/media_stream_audio_processor_options.cc
+++ third_party/blink/renderer/platform/mediastream/media_stream_audio_processor_options.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_parameters.h"
 #include "third_party/webrtc/modules/audio_processing/aec_dump/aec_dump_factory.h"
 #include "third_party/webrtc/modules/audio_processing/include/audio_processing.h"
-#include "third_party/webrtc/modules/audio_processing/typing_detection.h"
 
 namespace blink {
 namespace {
@@ -94,7 +93,6 @@ void AudioProcessingProperties::DisableDefaultProperties() {
   echo_cancellation_type = EchoCancellationType::kEchoCancellationDisabled;
   goog_auto_gain_control = false;
   goog_experimental_echo_cancellation = false;
-  goog_typing_noise_detection = false;
   goog_noise_suppression = false;
   goog_experimental_noise_suppression = false;
   goog_highpass_filter = false;
@@ -122,7 +120,6 @@ bool AudioProcessingProperties::HasSameNonReconfigurableSettings(
          goog_auto_gain_control == other.goog_auto_gain_control &&
          goog_experimental_echo_cancellation ==
              other.goog_experimental_echo_cancellation &&
-         goog_typing_noise_detection == other.goog_typing_noise_detection &&
          goog_noise_suppression == other.goog_noise_suppression &&
          goog_experimental_noise_suppression ==
              other.goog_experimental_noise_suppression &&
@@ -159,15 +156,7 @@ AudioProcessingProperties::ToAudioProcessingSettings() const {
                  : media::AutomaticGainControlType::kDefault)
           : media::AutomaticGainControlType::kDisabled;
   out.high_pass_filter = goog_highpass_filter;
-  out.typing_detection = goog_typing_noise_detection;
   return out;
-}
-
-void EnableTypingDetection(AudioProcessing::Config* apm_config,
-                           webrtc::TypingDetection* typing_detector) {
-  apm_config->voice_detection.enabled = true;
-  // Configure the update period to 1s (100 * 10ms) in the typing detector.
-  typing_detector->SetParameters(0, 0, 0, 0, 0, 100);
 }
 
 void StartEchoCancellationDump(AudioProcessing* audio_processing,
