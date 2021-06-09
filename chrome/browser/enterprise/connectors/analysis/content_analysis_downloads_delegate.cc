0041f448ba894f5627abea2fb80596dabf7c908a
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2021-06-09 21:06:29 +0000

    Add a content analysis delegate to be used for downloads
    Bug: 1210187
    Change-Id: Idcdee2a497e3d3f5fd1d5b6e0e2731942aa85ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945375
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890944}

---
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_downloads_delegate.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/analysis/content_analysis_downloads_delegate.h"
+
+namespace enterprise_connectors {
+
+ContentAnalysisDownloadsDelegate::ContentAnalysisDownloadsDelegate(
+    base::OnceCallback<void()> open_file_callback,
+    base::OnceCallback<void()> discard_file_callback)
+    : open_file_callback_(std::move(open_file_callback)),
+      discard_file_callback_(std::move(discard_file_callback)) {}
+
+ContentAnalysisDownloadsDelegate::~ContentAnalysisDownloadsDelegate() = default;
+
+void ContentAnalysisDownloadsDelegate::BypassWarnings() {
+  if (open_file_callback_)
+    std::move(open_file_callback_).Run();
+  ResetCallbacks();
+}
+
+void ContentAnalysisDownloadsDelegate::Cancel(bool warning) {
+  if (discard_file_callback_)
+    std::move(discard_file_callback_).Run();
+  ResetCallbacks();
+}
+
+void ContentAnalysisDownloadsDelegate::ResetCallbacks() {
+  discard_file_callback_.Reset();
+  open_file_callback_.Reset();
+}
+
+}  // namespace enterprise_connectors
