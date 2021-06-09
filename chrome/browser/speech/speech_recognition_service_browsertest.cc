7d69faaff50336aa0dad67bce88a1f3c81f4c486
Evan Liu <evliu@google.com>
2021-06-09 19:23:39 +0000

    Enable the SODA browser and unit tests for Windows
    This CL enables the SodaClientUnitTests and SpeechRecognitionServiceBrowserTests for Windows.
    
    Bug: 1207714
    Change-Id: Id68155fecb1c7b544d24928d117e9b6519381653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946759
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#890872}

--- chrome/browser/speech/speech_recognition_service_browsertest.cc
+++ chrome/browser/speech/speech_recognition_service_browsertest.cc
@@ -37,6 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/audio/public/cpp/fake_stream_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
+#if defined(OS_WIN)
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
 using testing::StrictMock;
 
 namespace speech {
@@ -301,7 +307,11 @@ void SpeechRecognitionServiceTest::SendAudioChunk(
 
     // Sleep for 20ms to simulate real-time audio. SODA requires audio
     // streaming in order to return events.
+#if defined(OS_WIN)
+    ::Sleep(20);
+#else
     usleep(20000);
+#endif
   }
 }
 
@@ -344,7 +354,11 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest, RecognizePhrase) {
   base::RunLoop().RunUntilIdle();
 
   // Sleep for 50ms to ensure SODA has returned real-time results.
+#if defined(OS_WIN)
+  ::Sleep(50);
+#else
   usleep(50000);
+#endif
   ASSERT_GT(static_cast<int>(recognition_results_.size()), kReplayAudioCount);
   ASSERT_EQ(recognition_results_.back(), "Hey Google Hey Google");
 
@@ -414,7 +428,11 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest,
   base::RunLoop().RunUntilIdle();
 
   // Sleep for 50ms to ensure SODA has returned real-time results.
+#if defined(OS_WIN)
+  ::Sleep(50);
+#else
   usleep(50000);
+#endif
   ASSERT_GT(static_cast<int>(recognition_results_.size()), 3);
   ASSERT_EQ(recognition_results_.back(), "Hey Google Hey Google");
 
