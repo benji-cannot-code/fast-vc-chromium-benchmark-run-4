6238eeca517e14a60ac92ad896eedbc31df69df2
David Bertoni <dbertoni@chromium.org>
2021-06-09 05:10:55 +0000

    [Extensions] Remove the 'name' field from RunOptions. (8)
    This CL removes usages of the 'name' field from the RunOptions struct,
    instead using it as a parameter to RunExtensionTest.
    
    Bug: 1171429
    Change-Id: I3e7419ba2c0069bab4115e50465eb46954e65855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947295
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Ghazale Hosseinabadi <ghazale@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890618}

--- chrome/browser/chromeos/extensions/printing/printing_apitest.cc
+++ chrome/browser/chromeos/extensions/printing/printing_apitest.cc
@@ -133,15 +133,14 @@ IN_PROC_BROWSER_TEST_F(PrintingApiTest, GetPrinters) {
   GetPrintersManager()->AddPrinter(printer, chromeos::PrinterClass::kSaved);
 
   SetCustomArg(kName);
-  ASSERT_TRUE(
-      RunExtensionTest({.name = "printing", .page_url = "get_printers.html"}));
+  ASSERT_TRUE(RunExtensionTest("printing", {.page_url = "get_printers.html"}));
 }
 
 IN_PROC_BROWSER_TEST_F(PrintingApiTest, GetPrinterInfo) {
   AddAvailablePrinter(
       kId, std::make_unique<printing::PrinterSemanticCapsAndDefaults>());
-  ASSERT_TRUE(RunExtensionTest(
-      {.name = "printing", .page_url = "get_printer_info.html"}));
+  ASSERT_TRUE(
+      RunExtensionTest("printing", {.page_url = "get_printer_info.html"}));
 }
 
 // Verifies that:
@@ -162,8 +161,7 @@ IN_PROC_BROWSER_TEST_F(PrintingApiTest, SubmitJob) {
   base::AutoReset<bool> skip_confirmation_dialog_reset(
       PrintJobSubmitter::SkipConfirmationDialogForTesting());
 
-  ASSERT_TRUE(
-      RunExtensionTest({.name = "printing", .page_url = "submit_job.html"}));
+  ASSERT_TRUE(RunExtensionTest("printing", {.page_url = "submit_job.html"}));
 }
 
 // Verifies that:
@@ -180,8 +178,7 @@ IN_PROC_BROWSER_TEST_F(PrintingApiTest, CancelJob) {
   base::AutoReset<bool> skip_confirmation_dialog_reset(
       PrintJobSubmitter::SkipConfirmationDialogForTesting());
 
-  ASSERT_TRUE(
-      RunExtensionTest({.name = "printing", .page_url = "cancel_job.html"}));
+  ASSERT_TRUE(RunExtensionTest("printing", {.page_url = "cancel_job.html"}));
 }
 
 }  // namespace extensions
--- chrome/browser/extensions/api/networking_private/networking_private_service_client_apitest.cc
+++ chrome/browser/extensions/api/networking_private/networking_private_service_client_apitest.cc
@@ -49,8 +49,8 @@ class NetworkingPrivateServiceClientApiTest
 
   bool RunNetworkingSubtest(const std::string& subtest) {
     const std::string page_url = "main.html?" + subtest;
-    return RunExtensionTest({.name = "networking_private/service_client",
-                             .page_url = page_url.c_str()},
+    return RunExtensionTest("networking_private/service_client",
+                            {.page_url = page_url.c_str()},
                             {.load_as_component = true});
   }
 
