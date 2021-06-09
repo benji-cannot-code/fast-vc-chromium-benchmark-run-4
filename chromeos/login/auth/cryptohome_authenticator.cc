285441bb2bc70cebe5ea829dfa21654656674046
Felipe Andrade <fsandrade@chromium.org>
2021-06-09 10:52:02 +0000

    Fix comments in some Chrome OS login files
    Fix comments in some files related to Chrome OS login. The errors were
    found while working on an issue with Chromad password change flow.
    
    Bug: 1053578
    Change-Id: I8f12939cb3d566442d4cc4ce87eeaa076fa0f8ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947019
    Reviewed-by: Yunke Zhou <yunkez@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890694}

--- chromeos/login/auth/cryptohome_authenticator.cc
+++ chromeos/login/auth/cryptohome_authenticator.cc
@@ -94,7 +94,7 @@ const char* AuthStateToString(CryptohomeAuthenticator::AuthState state) {
     case CryptohomeAuthenticator::CREATE_NEW:
       return "CREATE_NEW";
     case CryptohomeAuthenticator::RECOVER_MOUNT:
-      return "RECOVER_MONUT";
+      return "RECOVER_MOUNT";
     case CryptohomeAuthenticator::POSSIBLE_PW_CHANGE:
       return "POSSIBLE_PW_CHANGE";
     case CryptohomeAuthenticator::NEED_NEW_PW:
@@ -172,7 +172,7 @@ void RecordKeyErrorAndResolve(const base::WeakPtr<AuthAttemptState>& attempt,
   resolver->Resolve();
 }
 
-// Callback invoked when cryptohome's GetSantiziedUsername() method has
+// Callback invoked when cryptohome's GetSanitizedUsername() method has
 // finished.
 void OnGetSanitizedUsername(
     base::OnceCallback<void(bool, const std::string&)> callback,
@@ -183,7 +183,7 @@ void OnGetSanitizedUsername(
   std::move(callback).Run(!res.empty(), res);
 }
 
-// Callback invoked when a crypotyhome *Ex method, which only returns a
+// Callback invoked when a cryptohome *Ex method, which only returns a
 // base::Reply, finishes.
 template <typename ReplyType>
 void OnReplyMethod(const base::WeakPtr<AuthAttemptState>& attempt,
@@ -233,7 +233,7 @@ void DoMount(const base::WeakPtr<AuthAttemptState>& attempt,
   CHECK_NE(Key::KEY_TYPE_PASSWORD_PLAIN, key->GetKeyType());
 
   // Set state that username_hash is requested here so that test implementation
-  // that returns directly would not generate 2 OnLoginSucces() calls.
+  // that returns directly would not generate 2 OnLoginSuccess() calls.
   attempt->UsernameHashRequested();
 
   const cryptohome::AuthorizationRequest auth =
