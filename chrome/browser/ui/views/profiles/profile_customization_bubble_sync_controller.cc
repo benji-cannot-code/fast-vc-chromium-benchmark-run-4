764ec6f7dd6cff2a4b88ce0e28b50a76ac17a681
Jan Krcal <jkrcal@chromium.org>
2021-06-09 10:02:52 +0000

    Revert "[Profile customization bubble] Add more CHECKs for debugging"
    This reverts commit 5790fced4de04bb1dfe36ebdbb7269ddee5470a1.
    
    Reason for revert: These CHECKs are no more needed as the real root cause has been found.
    
    Original change's description:
    > [Profile customization bubble] Add more CHECKs for debugging
    >
    > Bug: 1213356, 1213109
    > Change-Id: I2c16dc6965a60c336fe25e27a57ff45e879fd6e7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940858
    > Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#889806}
    
    Bug: 1213356
    Bug: 1213109
    Change-Id: I4fbc0c0887a57aa15f8ed8870f6131fbd4bc3bb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947507
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890680}

--- chrome/browser/ui/views/profiles/profile_customization_bubble_sync_controller.cc
+++ chrome/browser/ui/views/profiles/profile_customization_bubble_sync_controller.cc
@@ -21,7 +21,6 @@ namespace {
 bool CanSyncStart(syncer::SyncService* sync_service) {
   if (!sync_service || !sync_service->CanSyncFeatureStart())
     return false;
-  CHECK(sync_service->GetUserSettings());
   if (!sync_service->GetUserSettings()->GetSelectedTypes().Has(
           syncer::UserSelectableType::kThemes)) {
     return false;
@@ -133,7 +132,6 @@ void ProfileCustomizationBubbleSyncController::Init() {
     return;
   }
 
-  CHECK(theme_service_->GetThemeSyncableService());
   theme_observation_.Observe(theme_service_->GetThemeSyncableService());
 
   // Observe also the sync service to abort waiting for theme sync if the user
