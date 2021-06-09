2a43bf554f88af7e1d94505f7ef3ba3c77452ffe
Ari Chivukula <arichiv@chromium.org>
2021-06-09 19:45:57 +0000

    Remove url::Origin import from some files that don't use it
    I noticed these files when working on the indexed_db migration. Seemed
    worth removing as long as I'm in here.
    
    Bug: 1210555
    Change-Id: Id5ac92118ec3c83c852e33f77f08421389302e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947368
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890908}

--- content/browser/indexed_db/indexed_db_active_blob_registry_unittest.cc
+++ content/browser/indexed_db/indexed_db_active_blob_registry_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "content/browser/indexed_db/indexed_db_active_blob_registry.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "url/origin.h"
 
 namespace content {
 
--- content/browser/indexed_db/indexed_db_pending_connection.h
+++ content/browser/indexed_db/indexed_db_pending_connection.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/indexed_db/indexed_db_data_loss_info.h"
 #include "content/browser/indexed_db/indexed_db_database_callbacks.h"
 #include "content/common/content_export.h"
-#include "url/origin.h"
 
 namespace content {
 
