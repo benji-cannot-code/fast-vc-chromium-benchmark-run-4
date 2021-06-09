a256f79847d5e89fd5dd8e3ef7077bb501d1f31e
Danil Chapovalov <danilchap@chromium.org>
2021-06-09 13:06:56 +0000

    Roll abseil_revision 18045c4e32..f72972654b
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/18045c4e32..f72972654b
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/18045c4e32..f72972654b
    
    No .def files changes
    
    Bug: None
    Change-Id: I18a0c51a0f6b113f51d9af03b58c249e6d0c5532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947509
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890723}

--- third_party/abseil-cpp/ci/cmake_common.sh
+++ third_party/abseil-cpp/ci/cmake_common.sh
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # The commit of GoogleTest to be used in the CMake tests in this directory.
 # Keep this in sync with the commit in the WORKSPACE file.
-readonly ABSL_GOOGLETEST_COMMIT="f5e592d8ee5ffb1d9af5be7f715ce3576b8bf9c4"
+readonly ABSL_GOOGLETEST_COMMIT="5bcd8e3bb929714e031a542d303f818e5a5af45d"
 
 # Avoid depending on GitHub by looking for a cached copy of the commit first.
 if [[ -r "${KOKORO_GFILE_DIR:-}/distdir/${ABSL_GOOGLETEST_COMMIT}.zip" ]]; then
