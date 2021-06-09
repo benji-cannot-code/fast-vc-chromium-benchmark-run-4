aead4293a8471b1c90fbd263befc93563a598489
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-06-09 12:02:10 +0000

    Expose trusted vault errors for signed-in-only users in the avatar menu
    See the document in the linked bug for more details. The error is meant
    to be surfaced only once the user wishes to sync an encrypted type. Due
    to missing APIs, only AlwaysEncryptedUserTypes supported in
    transport-mode are checked for the moment (PASSWORDS). A corresponding
    TODO is added to make the code more general.
    
    Implementation screenshot: crbug.com/1191411#c8
    
    Left for next CLs:
    - Adapting the tooltip string.
    - Adapting the sync icon.
    
    The most important changed function is
    sync_ui_util:: GetAvatarSyncErrorType(),  consumed by ProfileMenuView
    and AvatarToolbarButton (via its delegate). crbug.com/1191411#c3 has
    useful screenshots of the UIs modeled by the 2 classes.
    The current code basically first checks if sync-the-feature is enabled
    and if so, calls sync_ui_util::GetAvatarSyncErrorType() to check for
    errors, like trusted vault ones. This CL changes the function to more
    generally return any errors that should be shown, no matter whether
    sync-the-feature or sync-the-transport is being used. The only errors
    shown to sync-the-transport users today are from trusted vault, but this
    remains an implementation detail of the function.
    
    Bug: 1191411
    Change-Id: I81e2fafe20b6145c0b23c6c3a9a82212e5b92ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2814684
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890710}

--- chrome/browser/sync/sync_error_notifier_ash.cc
+++ chrome/browser/sync/sync_error_notifier_ash.cc
@@ -75,7 +75,8 @@ BubbleViewParameters GetBubbleViewParameters(
     return params;
   }
 
-  DCHECK(sync_ui_util::ShouldShowSyncKeysMissingError(sync_service));
+  DCHECK(sync_ui_util::ShouldShowSyncKeysMissingError(sync_service,
+                                                      profile->GetPrefs()));
 
   BubbleViewParameters params;
   params.message_id =
@@ -115,7 +116,8 @@ void SyncErrorNotifier::OnStateChanged(syncer::SyncService* service) {
 
   const bool should_display_notification =
       sync_ui_util::ShouldShowPassphraseError(sync_service_) ||
-      sync_ui_util::ShouldShowSyncKeysMissingError(sync_service_);
+      sync_ui_util::ShouldShowSyncKeysMissingError(service,
+                                                   profile_->GetPrefs());
 
   if (should_display_notification == notification_displayed_) {
     return;
