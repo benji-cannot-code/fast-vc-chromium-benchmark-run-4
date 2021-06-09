1dc5756fa9d39601b18992b01c2ee9029cafca28
Jason Gerecke <jason.gerecke@wacom.com>
2021-06-09 19:47:30 +0000

    ash: Don't show the Create Note stylus tool on external pen displays
    There may be corner cases that result in a poor user experience when
    the Create Note tool is launched on an external display. This is not
    a critical feature at the moment so temporarily paper over the issue
    by hiding the tool.
    
    Bug: b:183214775
    Test: ash_unittests --gtest_filter="*"; new unit tests
    Change-Id: I41646ce854d845d7d54684ea5738c594d9bea42d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2868024
    Commit-Queue: Harry Cutts <hcutts@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890911}

--- ash/system/palette/tools/create_note_action.cc
+++ ash/system/palette/tools/create_note_action.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/palette/palette_ids.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/events/event.h"
+#include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
 
 namespace ash {
 namespace {
@@ -55,6 +57,9 @@ views::View* CreateNoteAction::CreateView() {
   if (!GetAvailableClient())
     return nullptr;
 
+  if (!ShouldShowOnDisplay())
+    return nullptr;
+
   return CreateDefaultView(
       l10n_util::GetStringUTF16(IDS_ASH_STYLUS_TOOLS_CREATE_NOTE_ACTION));
 }
@@ -78,6 +83,22 @@ void CreateNoteAction::OnKeyEvent(ui::KeyEvent* event) {
 
   // This event shouldn't be handled as a regular key event, so consume it.
   event->StopPropagation();
+}
+
+// Don't show the create note action on external displays. This
+// avoids some corner cases that haven't been addressed yet.
+bool CreateNoteAction::ShouldShowOnDisplay() {
+  if (external_display_for_test_)
+    return false;
+
+  // Allow the unit tests to function normally without a window
+  aura::Window* root = delegate()->GetWindow();
+  if (root == nullptr)
+    return true;
+
+  const display::Display& display =
+      display::Screen::GetScreen()->GetDisplayNearestWindow(root);
+  return display.IsInternal();
 }
 
 }  // namespace ash
--- ash/system/palette/tools/create_note_action.h
+++ ash/system/palette/tools/create_note_action.h
@@ -34,6 +34,8 @@ class ASH_EXPORT CreateNoteAction : public CommonPaletteTool,
   // ui::EventHandler overrides.
   void OnKeyEvent(ui::KeyEvent* event) override;
 
+  bool ShouldShowOnDisplay();
+
   DISALLOW_COPY_AND_ASSIGN(CreateNoteAction);
 };
 
