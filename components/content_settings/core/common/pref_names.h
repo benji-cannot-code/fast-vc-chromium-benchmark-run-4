00d5a1fa61c14f681d2cc0a73193f7c177e13cb7
Steven Bingler <bingler@chromium.org>
2021-06-09 15:48:05 +0000

    Remove LegacySameSiteCookieBehaviorEnabled
    Remove the policy handling code and pref.
    Update the domain list policy to no longer reference the global policy.
    
    Bug: 1214078
    Change-Id: I002ce91b16d36e0d74e22faff845dcd3ae76a932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2923803
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890782}

--- components/content_settings/core/common/pref_names.h
+++ components/content_settings/core/common/pref_names.h
@@ -34,7 +34,6 @@ extern const char kManagedDefaultWebUsbGuardSetting[];
 extern const char kManagedDefaultFileHandlingGuardSetting[];
 extern const char kManagedDefaultFileSystemReadGuardSetting[];
 extern const char kManagedDefaultFileSystemWriteGuardSetting[];
-extern const char kManagedDefaultLegacyCookieAccessSetting[];
 extern const char kManagedDefaultSerialGuardSetting[];
 extern const char kManagedDefaultInsecurePrivateNetworkSetting[];
 
