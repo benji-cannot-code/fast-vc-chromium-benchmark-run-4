220ecf5b05e92515eb2b4d6673c184df1507a715
Dan Zhang <danzh@chromium.org>
2021-06-09 23:43:46 +0000

    Roll src/net/third_party/quiche/src/ 53387f743..99552ade1 (26 commits)
    https://quiche.googlesource.com/quiche.git/+log/53387f7437d9..99552ade1351
    
    $ git log 53387f743..99552ade1 --date=short --no-merges --format='%ad %ae %s'
    2021-06-09 ianswett Remove the cwnd_limited argument from PacingSenderTest::CheckPacketIsSentImmediately because there were already bytes_in_flight and cwnd arguments, so it was redundant.   Also renames bytes_in_flight to prior_in_flight to clarify the argument. Test-only, not flag protected.
    2021-06-09 fayang Internal change
    2021-06-08 quiche-dev Sample LOG network write errors.
    2021-06-08 quiche-dev Don't crash the qbone client when getting a read/write failure but the socket isn't blocked.
    2021-06-08 quiche-dev Adds methods to the Http2Adapter API to get and set per-stream user data.
    2021-06-08 danzh Fix gfe_quic_fuzzer for IETF quic.
    2021-06-08 quiche-dev Adds a method to Http2Adapter to fetch the highest received stream ID.
    2021-06-08 bnc Remove QuicContainsValue() from platform.
    2021-06-08 bnc Implement Http2HeaderBlock::contains().
    2021-06-08 quiche-dev Adds a method to Http2Adapter to determine if it represents a server-side HTTP/2 session.
    2021-06-08 bnc Remove spdy_string_utils and http2_string_utils from platform.
    2021-06-07 fayang When using multiple packet number spaces, ignore peer_max_ack_delay during handshake.
    2021-06-07 quiche-dev Updates CallbackVisitor to accept nghttp2_session_callbacks by const reference.
    2021-06-07 quiche-dev Adds support for trailers to the Http2Adapter API.
    2021-06-07 fayang Try to save one copy and allocation for frame header in QuicSpdyStream::WriteOrBufferBody.
    2021-06-07 vasilvv Provide default implementation for quic_bug_tracker.h API
    2021-06-07 wub Deprecate --gfe2_reloadable_flag_quic_add_packet_flusher_on_async_op_done.
    2021-06-07 wub Deprecate --gfe2_reloadable_flag_quic_batch_writer_fix_write_blocked.
    2021-06-07 bnc Remove unused EstimateMemoryUsage().
    2021-06-07 wub False deprecate --gfe2_reloadable_flag_quic_bbr2_avoid_too_low_probe_bw_cwnd.
    2021-06-07 ianswett Deprecate gfe2_reloadable_flag_quic_bbr2_fix_bw_lo_mode.
    2021-06-07 haoyuewang Stop sending disable_connection_migration param on "RVCM" connection option and other reloadable flags.
    2021-06-07 quiche-dev Automated g4 rollback of changelist 377894659.
    2021-06-07 ianswett Internal change
    2021-06-07 vasilvv Split writing DATA frame header out of WriteBodySlices.
    2021-06-07 vasilvv Provide default implementation for quic_bug_tracker.h API
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    override default quiche_bug_tracker_impl
    
    R=dschinazi@chromium.org
    
    Change-Id: Id50999ab59915fb0cd09b62ecca2b6d838aaeba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2950425
    Commit-Queue: Dan Zhang <danzh@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: Dan Zhang <danzh@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891016}

--- net/third_party/quiche/overrides/quiche_platform_impl/quiche_bug_tracker_impl.h
+++ net/third_party/quiche/overrides/quiche_platform_impl/quiche_bug_tracker_impl.h
@@ -2,8 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#ifndef NET_QUICHE_COMMON_PLATFORM_IMPL_QUICHE_BUG_TRACKER_IMPL_H_
-#define NET_QUICHE_COMMON_PLATFORM_IMPL_QUICHE_BUG_TRACKER_IMPL_H_
+#ifndef NET_THIRD_PARTY_QUICHE_OVERRIDES_QUICHE_PLATFORM_IMPL_QUICHE_BUG_TRACKER_IMPL_H_
+#define NET_THIRD_PARTY_QUICHE_OVERRIDES_QUICHE_PLATFORM_IMPL_QUICHE_BUG_TRACKER_IMPL_H_
 
 #include "net/third_party/quiche/src/quic/platform/api/quic_logging.h"
 
@@ -18,4 +18,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define QUICHE_PEER_BUG_IF_V2_IMPL(bug_id, condition) \
   QUIC_LOG_IF(ERROR, condition)
 
-#endif  // NET_QUICHE_COMMON_PLATFORM_IMPL_QUICHE_BUG_TRACKER_IMPL_H_
+#endif  // NET_THIRD_PARTY_QUICHE_OVERRIDES_QUICHE_PLATFORM_IMPL_QUICHE_BUG_TRACKER_IMPL_H_
