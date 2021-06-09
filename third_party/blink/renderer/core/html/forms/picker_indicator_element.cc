8ac1af7efff7793ea123c15a2f65dee3a34a0849
Dan Clark <daniec@microsoft.com>
2021-06-09 17:55:53 +0000

    Rename AriaRoleForPickerIndicator to AriaLabelForPickerIndicator
    This method is used to set the aria label, not the role, for the picker
    indicator element. So, the name is wrong. Fix it to prevent confusion.
    
    Change-Id: If3d7e6d571ae83cf1542842733a78d7483d3f72f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2895445
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#890854}

--- third_party/blink/renderer/core/html/forms/picker_indicator_element.cc
+++ third_party/blink/renderer/core/html/forms/picker_indicator_element.cc
@@ -176,7 +176,7 @@ void PickerIndicatorElement::DidNotifySubtreeInsertionsToDocument() {
   setAttribute(
       html_names::kAriaLabelAttr,
       AtomicString(
-          this->picker_indicator_owner_->AriaRoleForPickerIndicator()));
+          this->picker_indicator_owner_->AriaLabelForPickerIndicator()));
 }
 
 void PickerIndicatorElement::Trace(Visitor* visitor) const {
