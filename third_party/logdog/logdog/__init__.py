3c451bfa27ff96062a5eef0624d04d0bfb03ce1b
Takuto Ikuta <tikuta@chromium.org>
2021-06-09 01:02:10 +0000

    build: remove reference to tools/swarming_client
    This adds yet another dependency to logdog subdir repository and use
    that from pylib.
    
    This is to remove deprecated python client //tools/swarming_client from
    DEPS later.
    
    third_party addition is approved in
    http://g/chrome-eng-review/6BvlJENKYdA
    
    Fixed: 1051854
    Change-Id: I521923a058e15a6cd36f083e854f472ca727b30d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939408
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890539}

---
+++ third_party/logdog/logdog/__init__.py
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2016 The LUCI Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
