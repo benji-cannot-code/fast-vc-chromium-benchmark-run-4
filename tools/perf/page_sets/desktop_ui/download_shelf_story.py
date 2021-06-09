7e620db45cb8e5b1b173f1534815e651f1ef5a31
Yuheng Huang <yuhengh@chromium.org>
2021-06-09 05:00:35 +0000

    WebUI download shelf: Fix flaky mac crash on benchmark
    There's a occasionally browser crash possibly related to
    UIDevtools on Mac in the download shelf stories.
    
    This CL disables UIDevtools on Mac for the download shelf stories
    since it's not used anyways. It should fix the flaky story failure.
    
    Waterfall link:
    https://ci.chromium.org/ui/p/chrome/builders/ci/mac-10_12_laptop_low_end-perf/18603/overview
    Crash log:
    https://chrome-isolated.appspot.com/browse?digest=da07aa331ff24affeefe8a9e81a74f52d7aae69d
    
    Bug: 1210302
    Change-Id: Ifd985e8daf25324168dbe0bca22770b75bcbf504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947948
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890616}

--- tools/perf/page_sets/desktop_ui/download_shelf_story.py
+++ tools/perf/page_sets/desktop_ui/download_shelf_story.py
@@ -40,7 +40,8 @@ class DownloadShelfStory(MultiTabStory):
         tabs.New(url=url)
       except Exception:
         pass
-    self._devtools = action_runner.tab.browser.GetUIDevtools()
+    if not IsMac():
+      self._devtools = action_runner.tab.browser.GetUIDevtools()
 
   def IsWebUI(self):
     return 'webui' in self.NAME
