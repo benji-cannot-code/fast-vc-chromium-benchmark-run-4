4b9a52185ffe8bee5e1a8ae7da70c049ac1f06e7
Victor Costan <pwnall@chromium.org>
2021-06-09 22:26:35 +0000

    Compute Pressure: Add struct for the data used to compute CPU speeds.
    CpuProbe implementations will need to store per-core data for reporting
    in chrome://compute-pressure-internals.
    
    Bug: 1217924
    Change-Id: Ia4dc16c2a024aed2e8969590b3b1a988432d5917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2948170
    Reviewed-by: Olivier Yiptong <oyiptong@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890984}

--- content/browser/compute_pressure/compute_pressure_test_support.cc
+++ content/browser/compute_pressure/compute_pressure_test_support.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "content/browser/compute_pressure/compute_pressure_sample.h"
+#include "content/browser/compute_pressure/cpu_core_speed_info.h"
 
 namespace content {
 
@@ -27,6 +28,13 @@ std::ostream& operator<<(std::ostream& os,
                          const ComputePressureSample& sample) {
   os << "[utilization: " << sample.cpu_utilization
      << " speed: " << sample.cpu_speed << "]";
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const CpuCoreSpeedInfo& info) {
+  os << "[min: " << info.min_frequency << " max: " << info.max_frequency
+     << " base: " << info.base_frequency
+     << " current: " << info.current_frequency << "]";
   return os;
 }
 
