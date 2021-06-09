36f3d5eeb43cd29f9e7f17b3263604f2a9a8a20f
Sigurdur Asgeirsson <siggi@chromium.org>
2021-06-09 17:07:00 +0000

    Suspend LudicrousTimerSlack when WebRTC has pending delay tasks.
    Bug: 1181297
    Change-Id: If516f07d045e3fed5aff6c26ce18e340b6577248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934816
    Reviewed-by: Markus Handell <handellm@google.com>
    Reviewed-by: Tommi <tommi@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#890823}

--- third_party/webrtc_overrides/task_queue_factory.cc
+++ third_party/webrtc_overrides/task_queue_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/message_loop/timer_slack.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -21,7 +22,9 @@ class WebrtcTaskQueue final : public webrtc::TaskQueueBase {
  public:
   explicit WebrtcTaskQueue(const base::TaskTraits& traits)
       : task_runner_(base::ThreadPool::CreateSequencedTaskRunner(traits)),
-        is_active_(new base::RefCountedData<bool>(true)) {
+        is_active_(new base::RefCountedData<bool>(true)),
+        suspend_timer_slack_(traits.priority() !=
+                             base::TaskPriority::BEST_EFFORT) {
     DCHECK(task_runner_);
   }
 
@@ -36,10 +39,18 @@ class WebrtcTaskQueue final : public webrtc::TaskQueueBase {
   static void RunTask(WebrtcTaskQueue* task_queue,
                       scoped_refptr<base::RefCountedData<bool>> is_active,
                       std::unique_ptr<webrtc::QueuedTask> task);
+  static void ResumeAndRunTask(
+      WebrtcTaskQueue* task_queue,
+      scoped_refptr<base::RefCountedData<bool>> is_active,
+      std::unique_ptr<webrtc::QueuedTask> task);
 
   const scoped_refptr<base::SequencedTaskRunner> task_runner_;
   // Value of |is_active_| is checked and set on |task_runner_|.
   const scoped_refptr<base::RefCountedData<bool>> is_active_;
+
+  // If true, this task runner suspends timer slack while any delay tasks are
+  // outstanding.
+  const bool suspend_timer_slack_;
 };
 
 void Deactivate(scoped_refptr<base::RefCountedData<bool>> is_active,
@@ -74,6 +85,15 @@ void WebrtcTaskQueue::RunTask(
   }
 }
 
+void WebrtcTaskQueue::ResumeAndRunTask(
+    WebrtcTaskQueue* task_queue,
+    scoped_refptr<base::RefCountedData<bool>> is_active,
+    std::unique_ptr<webrtc::QueuedTask> task) {
+  base::ResumeLudicrousTimerSlack();
+
+  RunTask(task_queue, std::move(is_active), std::move(task));
+}
+
 void WebrtcTaskQueue::PostTask(std::unique_ptr<webrtc::QueuedTask> task) {
   // Posted Task might outlive this, but access to this is guarded by
   // ref-counted |is_active_| flag.
@@ -87,11 +107,20 @@ void WebrtcTaskQueue::PostDelayedTask(std::unique_ptr<webrtc::QueuedTask> task,
                                       uint32_t milliseconds) {
   // Posted Task might outlive this, but access to this is guarded by
   // ref-counted |is_active_| flag.
-  task_runner_->PostDelayedTask(
-      FROM_HERE,
-      base::BindOnce(&WebrtcTaskQueue::RunTask, base::Unretained(this),
-                     is_active_, std::move(task)),
-      base::TimeDelta::FromMilliseconds(milliseconds));
+  if (suspend_timer_slack_) {
+    base::SuspendLudicrousTimerSlack();
+    task_runner_->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&WebrtcTaskQueue::ResumeAndRunTask,
+                       base::Unretained(this), is_active_, std::move(task)),
+        base::TimeDelta::FromMilliseconds(milliseconds));
+  } else {
+    task_runner_->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&WebrtcTaskQueue::RunTask, base::Unretained(this),
+                       is_active_, std::move(task)),
+        base::TimeDelta::FromMilliseconds(milliseconds));
+  }
 }
 
 base::TaskTraits TaskQueuePriority2Traits(
