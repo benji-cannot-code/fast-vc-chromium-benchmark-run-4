9ce8b13e2ca17c67460df531a94ae252b95ba336
Fredrik Söderqvist <fs@opera.com>
2021-06-09 09:03:03 +0000

    Drop unused size field from CrossfadeGeneratedImage
    The super class (GeneratedImage) has a |size_| field and
    CrossfadeGeneratedImage has a |crossfade_size_| field. Only the latter
    is used by the derived class - with the exception of SizeAsFloat().
    
    Remove |crossfade_size_| and store that value in the super class' field
    instead.
    
    Bug: 1175005
    Change-Id: Ica226d97f847187db7d6652e4c50a7528fc0b09d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947017
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#890662}

--- third_party/blink/renderer/platform/graphics/crossfade_generated_image.h
+++ third_party/blink/renderer/platform/graphics/crossfade_generated_image.h
@@ -41,16 +41,14 @@ class PLATFORM_EXPORT CrossfadeGeneratedImage final : public GeneratedImage {
       scoped_refptr<Image> from_image,
       scoped_refptr<Image> to_image,
       float percentage,
-      FloatSize crossfade_size,
       const FloatSize& size) {
-    return base::AdoptRef(
-        new CrossfadeGeneratedImage(std::move(from_image), std::move(to_image),
-                                    percentage, crossfade_size, size));
+    return base::AdoptRef(new CrossfadeGeneratedImage(
+        std::move(from_image), std::move(to_image), percentage, size));
   }
 
   bool HasIntrinsicSize() const override { return true; }
 
-  IntSize Size() const override { return FlooredIntSize(crossfade_size_); }
+  IntSize Size() const override { return FlooredIntSize(size_); }
 
  protected:
   void Draw(cc::PaintCanvas*,
@@ -68,7 +66,6 @@ class PLATFORM_EXPORT CrossfadeGeneratedImage final : public GeneratedImage {
   CrossfadeGeneratedImage(scoped_refptr<Image> from_image,
                           scoped_refptr<Image> to_image,
                           float percentage,
-                          FloatSize crossfade_size,
                           const FloatSize&);
 
  private:
@@ -83,7 +80,6 @@ class PLATFORM_EXPORT CrossfadeGeneratedImage final : public GeneratedImage {
   scoped_refptr<Image> to_image_;
 
   float percentage_;
-  FloatSize crossfade_size_;
 };
 
 }  // namespace blink
