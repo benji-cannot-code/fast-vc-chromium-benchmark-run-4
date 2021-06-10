c89693e822c366fb4e01eec578f875098cf8843e
Yanzhu Du <yzd@google.com>
2021-06-10 16:51:42 +0000

    Add desks_storage::DeskModel storage interface.
    The desks_storage::DeskTemplate class is a temporary placeholder. It
    could be replaced by ash::DeskTemplate when it is ready.
    
    Local file and Sync backends will be added in followup CRs.
    
    Bug: 1177413
    Change-Id: I4a5011939467af6bd0229f8b6ff1131ddb72feb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2917259
    Commit-Queue: Yanzhu Du <yzd@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891265}

---
+++ components/desks_storage/core/desk_model.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/desks_storage/core/desk_model.h"
+
+#include "components/desks_storage/core/desk_model_observer.h"
+
+namespace desks_storage {
+
+DeskModel::DeskModel() = default;
+
+DeskModel::~DeskModel() = default;
+
+void DeskModel::AddObserver(DeskModelObserver* observer) {
+  DCHECK(observer);
+  observers_.AddObserver(observer);
+}
+
+void DeskModel::RemoveObserver(DeskModelObserver* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+}  // namespace desks_storage
