cdb4feaf4c634f794c5e8eaa80ef2afaa3a12950
Siyu An <siyua@chromium.org>
2021-06-10 05:35:51 +0000

    [VCN] Send request to server if the user accepts virtual card suggestion
    This CL adds the logic to handle the acceptance of the virtual card
    suggestion. Backend will do normal authentication except one more step
    to fetch the last committed url origin and send it along with the request.
    Server will return response including the full information of the virtual
    card.
    
    Bug: 1196021
    Change-Id: I715b36177f1c6d7ddc99fe40bf2e2201aecfa7ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2927763
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#891104}

--- components/autofill/core/browser/payments/payments_client.h
+++ components/autofill/core/browser/payments/payments_client.h
@@ -105,6 +105,9 @@ class PaymentsClient {
     std::string risk_data;
     CardUnmaskDelegate::UserProvidedUnmaskDetails user_response;
     absl::optional<base::Value> fido_assertion_info;
+    // The url origin of the website where the unmasking happened. Should be
+    // populated when the unmasking is for a virtual card.
+    absl::optional<GURL> last_committed_url_origin;
   };
 
   // Information retrieved from an UnmaskRequest.
@@ -126,6 +129,14 @@ class PaymentsClient {
 
     std::string real_pan;
     std::string dcvv;
+    // The expiration month of the card. It falls in between 1 - 12. Should be
+    // populated when the card is a virtual card which does not necessarily have
+    // the same expiration date as its related actual card.
+    std::string expiration_month;
+    // The four-digit expiration year of the card. Should be populated when the
+    // card is a virtual card which does not necessarily have the same
+    // expiration date as its related actual card.
+    std::string expiration_year;
     // Challenge required for enrolling user into FIDO authentication for future
     // card unmasking.
     absl::optional<base::Value> fido_creation_options;
@@ -136,7 +147,7 @@ class PaymentsClient {
     // OptChange calls together.
     std::string card_authorization_token = std::string();
 
-    // The type of the credit card the current request is fetching.
+    // The type of the returned credit card.
     AutofillClient::PaymentsRpcCardType card_type =
         AutofillClient::PaymentsRpcCardType::UNKNOWN_TYPE;
   };
