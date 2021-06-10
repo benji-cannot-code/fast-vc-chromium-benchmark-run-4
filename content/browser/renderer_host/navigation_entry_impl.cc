074dada682673c79953816a8a18892799cf3c7c9
Nate Chapin <japhet@chromium.org>
2021-06-10 17:22:08 +0000

    Implement appHistory.goTo() (and back() and forward())
    Explainer: https://github.com/WICG/app-history
    
    appHistory introduces a goTo() function which takes a key that
    opaquely identifies a back/forward entry, and navigates to that entry.
    appHistory already has an entries() array that lists all of the entries
    this frame is aware of (the browser ensures that all of these entries
    are contiguously same-origin with the current entry). The key provided
    to goTo() must be the key of one of those entries(). If that check
    passes, the key is sent to the browser, which looks through the
    joint session history for a FrameNavigationEntry that is associated with
    the requesting frame, is contiguously same-origin with the current
    entry, and that matches the given key. If a match is found, we will
    navigate to that joint session history entry. From that point, the
    navigation looks like a conventional back/forward navigation.
    
    Bug: 1183545
    Change-Id: I0ba70cdeea89e683ae66e2044cb097f6aec03fde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2852456
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891273}

--- content/browser/renderer_host/navigation_entry_impl.cc
+++ content/browser/renderer_host/navigation_entry_impl.cc
@@ -74,7 +74,8 @@ void RecursivelyGenerateFrameEntries(
 
   node->frame_entry = base::MakeRefCounted<FrameNavigationEntry>(
       UTF16ToUTF8(state.target.value_or(std::u16string())),
-      state.item_sequence_number, state.document_sequence_number, nullptr,
+      state.item_sequence_number, state.document_sequence_number,
+      UTF16ToUTF8(state.app_history_key.value_or(std::u16string())), nullptr,
       nullptr, GURL(state.url_string.value_or(std::u16string())),
       // TODO(nasko): Supply valid origin once the value is persisted across
       // session restore.
@@ -335,6 +336,7 @@ NavigationEntryImpl::NavigationEntryImpl(
               "",
               -1,
               -1,
+              "",
               std::move(instance),
               nullptr,
               url,
@@ -881,6 +883,7 @@ void NavigationEntryImpl::AddOrUpdateFrameEntry(
     UpdatePolicy update_policy,
     int64_t item_sequence_number,
     int64_t document_sequence_number,
+    const std::string& app_history_key,
     SiteInstanceImpl* site_instance,
     scoped_refptr<SiteInstanceImpl> source_site_instance,
     const GURL& url,
@@ -907,7 +910,7 @@ void NavigationEntryImpl::AddOrUpdateFrameEntry(
 
     root_node()->frame_entry->UpdateEntry(
         frame_tree_node->unique_name(), item_sequence_number,
-        document_sequence_number, site_instance,
+        document_sequence_number, app_history_key, site_instance,
         std::move(source_site_instance), url, origin, referrer,
         initiator_origin, redirect_chain, page_state, method, post_id,
         std::move(blob_url_loader_factory),
@@ -944,9 +947,9 @@ void NavigationEntryImpl::AddOrUpdateFrameEntry(
       // Update the existing FrameNavigationEntry (e.g., for replaceState).
       child->frame_entry->UpdateEntry(
           unique_name, item_sequence_number, document_sequence_number,
-          site_instance, std::move(source_site_instance), url, origin, referrer,
-          initiator_origin, redirect_chain, page_state, method, post_id,
-          std::move(blob_url_loader_factory),
+          app_history_key, site_instance, std::move(source_site_instance), url,
+          origin, referrer, initiator_origin, redirect_chain, page_state,
+          method, post_id, std::move(blob_url_loader_factory),
           std::move(web_bundle_navigation_info),
           std::move(subresource_web_bundle_navigation_info),
           std::move(policy_container_policies));
@@ -959,9 +962,10 @@ void NavigationEntryImpl::AddOrUpdateFrameEntry(
   // or unique name.
   auto frame_entry = base::MakeRefCounted<FrameNavigationEntry>(
       unique_name, item_sequence_number, document_sequence_number,
-      site_instance, std::move(source_site_instance), url, origin, referrer,
-      initiator_origin, redirect_chain, page_state, method, post_id,
-      std::move(blob_url_loader_factory), std::move(web_bundle_navigation_info),
+      app_history_key, site_instance, std::move(source_site_instance), url,
+      origin, referrer, initiator_origin, redirect_chain, page_state, method,
+      post_id, std::move(blob_url_loader_factory),
+      std::move(web_bundle_navigation_info),
       std::move(subresource_web_bundle_navigation_info),
       std::move(policy_container_policies));
   parent_node->children.push_back(
--- third_party/blink/renderer/core/app_history/app_history.h
+++ third_party/blink/renderer/core/app_history/app_history.h
@@ -20,6 +20,7 @@ namespace blink {
 class AppHistoryEntry;
 class AppHistoryNavigateEvent;
 class AppHistoryNavigateOptions;
+class AppHistoryNavigationOptions;
 class HTMLFormElement;
 class HistoryItem;
 class KURL;
@@ -30,7 +31,12 @@ class SerializedScriptValue;
 // TODO(japhet): This should probably move to frame_loader_types.h and possibly
 // be used more broadly once it is in the HTML spec.
 enum class UserNavigationInvolvement { kBrowserUI, kActivation, kNone };
-enum class NavigateEventType { kFragment, kHistoryApi, kCrossDocument };
+enum class NavigateEventType {
+  kFragment,
+  kHistoryApi,
+  kAppHistorySameDocumentGoto,
+  kCrossDocument
+};
 
 class CORE_EXPORT AppHistory final : public EventTargetWithInlineData,
                                      public Supplement<LocalDOMWindow> {
@@ -53,6 +59,9 @@ class CORE_EXPORT AppHistory final : public EventTargetWithInlineData,
   AppHistoryEntry* current() const;
   HeapVector<Member<AppHistoryEntry>> entries();
 
+  bool canGoBack() const;
+  bool canGoForward() const;
+
   ScriptPromise navigate(ScriptState*,
                          const String& url,
                          AppHistoryNavigateOptions*,
@@ -60,6 +69,17 @@ class CORE_EXPORT AppHistory final : public EventTargetWithInlineData,
   ScriptPromise navigate(ScriptState*,
                          AppHistoryNavigateOptions*,
                          ExceptionState&);
+
+  ScriptPromise goTo(ScriptState*,
+                     const String& key,
+                     AppHistoryNavigationOptions*,
+                     ExceptionState&);
+  ScriptPromise back(ScriptState*,
+                     AppHistoryNavigationOptions*,
+                     ExceptionState&);
+  ScriptPromise forward(ScriptState*,
+                        AppHistoryNavigationOptions*,
+                        ExceptionState&);
 
   DEFINE_ATTRIBUTE_EVENT_LISTENER(navigate, kNavigate)
   DEFINE_ATTRIBUTE_EVENT_LISTENER(navigatesuccess, kNavigatesuccess)
@@ -71,7 +91,8 @@ class CORE_EXPORT AppHistory final : public EventTargetWithInlineData,
                              NavigateEventType,
                              WebFrameLoadType,
                              UserNavigationInvolvement,
-                             SerializedScriptValue* = nullptr);
+                             SerializedScriptValue* = nullptr,
+                             HistoryItem* destination_item = nullptr);
   void CancelOngoingNavigateEvent();
 
   int GetIndexFor(AppHistoryEntry*);
@@ -96,6 +117,8 @@ class CORE_EXPORT AppHistory final : public EventTargetWithInlineData,
   scoped_refptr<SerializedScriptValue> navigate_serialized_state_;
 
   bool did_react_to_promise_ = false;
+  Member<ScriptPromiseResolver> goto_promise_resolver_;
+
   ScriptValue navigate_event_info_;
 };
 
