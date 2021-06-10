4f3ff35fdc1e56105b177c00aa2f9127b24c06ac
Xing Liu <xingliu@google.com>
2021-06-10 02:48:45 +0000

    Download service: Build public interfaces on iOS.
    This CL makes the public interface target for download service to build
    on iOS.
    
    Notices ServiceConfig and Download task type enums are not used on
    iOS, but we still include these, for temporarily not changing the
    public api too much.
    
    A following optional thing to do is to create a DownloadServiceLite
    interface and makes the current DownloadService interface to inherit
    the lite interface.
    
    Bug: 1215315
    Change-Id: I8ae21be284b83d576a3c48a7fc6e8073195f5ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947533
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891071}

--- components/download/public/background_service/download_metadata.h
+++ components/download/public/background_service/download_metadata.h
@@ -10,10 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/scoped_refptr.h"
+#include "build/build_config.h"
 #include "net/http/http_response_headers.h"
-#include "storage/browser/blob/blob_data_handle.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
+
+#if !defined(OS_IOS)
+#include "storage/browser/blob/blob_data_handle.h"
+#endif
 
 namespace download {
 
@@ -23,9 +27,11 @@ struct CompletionInfo {
   // to retrieve data.
   base::FilePath path;
 
+#if !defined(OS_IOS)
   // The blob data handle that contains download data.
   // Will be available after the download is completed in incognito mode.
   absl::optional<storage::BlobDataHandle> blob_handle;
+#endif
 
   // Download file size in bytes.
   uint64_t bytes_downloaded = 0u;
