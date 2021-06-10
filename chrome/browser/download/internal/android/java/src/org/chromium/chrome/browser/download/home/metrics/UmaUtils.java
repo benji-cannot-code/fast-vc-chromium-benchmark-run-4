eeffb8581d21e0c6b896563aa0143d9ec4c36b94
Elly Fong-Jones <ellyjones@chromium.org>
2021-06-10 20:41:47 +0000

    sharing: include shares from DownloadManager in AnyShareStarted
    The Sharing.AnyShareStarted metric is supposed to incorporate all
    shares, regardless of source, which means adding metrics logging at
    all points where explicit share intents are created.
    
    Bug: 1195038
    Change-Id: I2970889fec681f9528eb9f906d88efa834b08304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2950864
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891379}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/metrics/UmaUtils.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/metrics/UmaUtils.java
@@ -13,6 +13,7 @@ import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.download.home.filter.Filters;
 import org.chromium.chrome.browser.download.home.filter.Filters.FilterType;
 import org.chromium.chrome.browser.download.internal.R;
+import org.chromium.components.browser_ui.share.ShareHelper;
 import org.chromium.components.offline_items_collection.OfflineItem;
 import org.chromium.components.offline_items_collection.OfflineItemFilter;
 import org.chromium.components.offline_items_collection.RenameResult;
@@ -191,6 +192,7 @@ public class UmaUtils {
 
         RecordHistogram.recordLinearCountHistogram(
                 "Android.DownloadManager.Share.Count", items.size(), 1, 20, 20);
+        ShareHelper.recordShareSource(ShareHelper.ShareSourceAndroid.ANDROID_SHARE_SHEET);
     }
 
     /**
