195094b3ac4d8ca166b8f9ac22b91a6b30205697
Torne (Richard Coles) <torne@google.com>
2021-06-10 14:48:37 +0000

    webview: re-enable shouldOverrideUrlLoading test.
    Re-enable the long-disabled AwContentsClientShouldOverrideUrlLoadingTest
    testCalledWhenTopLevelAboutBlankNavigation. We have rewritten
    shouldOverrideUrlLoading to use a throttle since this test was flaking
    (as discussed in the bug) and it appears to be reliable now without any
    further changes.
    
    Fixed: 462306
    Change-Id: I451016ad1256813e7ec127db0157fd2b224b97c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953003
    Auto-Submit: Richard Coles <torne@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891226}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientShouldOverrideUrlLoadingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientShouldOverrideUrlLoadingTest.java
@@ -30,7 +30,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.NavigationHistory;
@@ -355,12 +354,9 @@ public class AwContentsClientShouldOverrideUrlLoadingTest {
         Assert.assertTrue(mShouldOverrideUrlLoadingHelper.isMainFrame());
     }
 
-    /*
+    @Test
     @SmallTest
     @Feature({"AndroidWebView", "Navigation"})
-    */
-    @Test
-    @DisabledTest(message = "crbug.com/462306")
     public void testCalledWhenTopLevelAboutBlankNavigation() throws Throwable {
         standardSetup();
 
