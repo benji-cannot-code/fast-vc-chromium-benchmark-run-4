9193d71181eb868daac6cc50dd13e95af67b20f0
Preethi Mohan <preethim@google.com>
2021-06-10 19:17:41 +0000

    PY3 migration(blink web tests) - fix sharding issues in PY3.
    Bug: 1197679
    
    hash() function is not consistent across invocations in  PY3 resulting tests duplicated across shards.
    Use hashlib.sha256() instead.
    
    Change-Id: Iec968dd9f3d45562754c26c7b19ecabdce98c66a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2886661
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891333}

--- third_party/blink/tools/blinkpy/web_tests/controllers/web_test_finder.py
+++ third_party/blink/tools/blinkpy/web_tests/controllers/web_test_finder.py
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import errno
 import fnmatch
+import hashlib
 import json
 import logging
 import math
@@ -299,8 +300,11 @@ class WebTestFinder(object):
 
     @staticmethod
     def _split_into_chunks(test_names, index, count):
-        tests_and_indices = [(test_name, hash(test_name) % count)
-                             for test_name in test_names]
+        tests_and_indices = [
+            (test_name,
+             int(hashlib.sha256(test_name.encode('utf-8')).hexdigest(), 16) %
+             count) for test_name in test_names
+        ]
 
         tests_to_run = [
             test_name for test_name, test_index in tests_and_indices
