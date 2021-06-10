b23201c37d61620d9549e6d5c85dca2479cf1cf6
Yan <shaobo.yan@intel.com>
2021-06-10 00:46:54 +0000

    CopyExternalImageToTexture(): Fix bugs for 2d Canvas uploading path
    These bugs are caught by TFJS test cases.
    
    The CL fix:
    - Missing canvas to CanvasSourceImage in GetImageFromExternalImage
    - Wrong DCHECK in HTMLCanvasElement GetSourceImageForCanvas
    
    BUG=1197369
    
    Change-Id: I0aaeb23f0ae5437d38f41ec39f56931b757b02bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2949287
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    Cr-Commit-Position: refs/heads/master@{#891044}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -1315,7 +1315,6 @@ scoped_refptr<Image> HTMLCanvasElement::GetSourceImageForCanvas(
     SourceImageStatus* status,
     const FloatSize&,
     const AlphaDisposition alpha_disposition) {
-  DCHECK_NE(alpha_disposition, kDontChangeAlpha);
 
   return GetSourceImageForCanvasInternal(status, alpha_disposition);
 }
--- third_party/blink/renderer/modules/webgpu/gpu_queue.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_queue.cc
@@ -154,6 +154,7 @@ scoped_refptr<Image> GetImageFromExternalImage(
           "or webgl2 conext");
       return nullptr;
     }
+    source = canvas;
   }
 
   // Neutered external image.
