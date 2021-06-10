cc90993dfa1eda95f931758226ac1d1a9a819004
Trent Begin <tbegin@google.com>
2021-06-10 22:23:39 +0000

    NetworkPortalDialog: remove rotation from dialog size calculation
    This change removes the rotation form the dialog size calculation since
    this is already taken into account by Display::Size().
    
    Bug: chromium:1218547
    Change-Id: Ia1ddbd4df279f75dc9a5985d20d46e699fc8f12d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2954280
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891412}

--- chrome/browser/chromeos/net/network_portal_web_dialog.cc
+++ chrome/browser/chromeos/net/network_portal_web_dialog.cc
@@ -17,25 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const float kNetworkPortalWebDialogWidthFraction = .8;
-const float kNetworkPortalWebDialogHeightFraction = .8;
+const float kNetworkPortalWebDialogScale = .8;
 
 gfx::Size GetPortalDialogSize() {
   const display::Display display =
       display::Screen::GetScreen()->GetPrimaryDisplay();
 
-  gfx::Size display_size = display.size();
-
-  if (display.rotation() == display::Display::ROTATE_90 ||
-      display.rotation() == display::Display::ROTATE_270) {
-    display_size = gfx::Size(display_size.height(), display_size.width());
-  }
-
-  display_size =
-      gfx::Size(display_size.width() * kNetworkPortalWebDialogWidthFraction,
-                display_size.height() * kNetworkPortalWebDialogHeightFraction);
-
-  return display_size;
+  return gfx::Size(display.size().width() * kNetworkPortalWebDialogScale,
+                   display.size().height() * kNetworkPortalWebDialogScale);
 }
 
 }  // namespace
@@ -76,8 +65,7 @@ GURL NetworkPortalWebDialog::GetDialogContentURL() const {
 }
 
 void NetworkPortalWebDialog::GetWebUIMessageHandlers(
-    std::vector<content::WebUIMessageHandler*>* handlers) const {
-}
+    std::vector<content::WebUIMessageHandler*>* handlers) const {}
 
 void NetworkPortalWebDialog::GetDialogSize(gfx::Size* size) const {
   *size = GetPortalDialogSize();
