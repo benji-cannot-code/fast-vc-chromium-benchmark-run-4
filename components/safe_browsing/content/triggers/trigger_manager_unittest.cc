827c268c8c03b755a0626d9c443b55a4cf38c657
Colin Blundell <blundell@chromium.org>
2021-06-10 10:34:06 +0000

    [//components/safe_browsing] Remove usage of test_task_env in content/
    test_task_environment.h is a utility that allows
    //components/safe_browsing/core to use //content's and //ios/web's test
    task environments via different implementations that are conditionally
    included in the GN target. We are going to eliminate this utility as
    part of eliminating //components/safe_browsing/core's incorrect
    dependences on //components/safe_browsing/{content, ios}. As a first
    step, this CL eliminates the usage of the utility in
    //components/safe_browsing/content. That elimination is simple as this
    code can directly use content::BrowserTaskEnvironment.
    
    Bug: 1216341
    Change-Id: I5e09e4336292edc02367d71eeec09d34bbe46f44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2949469
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891169}

--- components/safe_browsing/content/triggers/trigger_manager_unittest.cc
+++ components/safe_browsing/content/triggers/trigger_manager_unittest.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/testing_pref_service.h"
 #include "components/safe_browsing/content/browser/threat_details.h"
 #include "components/safe_browsing/content/triggers/trigger_throttler.h"
-#include "components/safe_browsing/core/common/test_task_environment.h"
 #include "components/safe_browsing/core/features.h"
+#include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_browser_context.h"
 #include "content/public/test/test_web_contents_factory.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -62,9 +62,7 @@ class MockTriggerThrottler : public TriggerThrottler {
 
 class TriggerManagerTest : public ::testing::Test {
  public:
-  TriggerManagerTest()
-      : trigger_manager_(nullptr, nullptr),
-        task_environment_(CreateTestTaskEnvironment()) {}
+  TriggerManagerTest() : trigger_manager_(nullptr, nullptr) {}
   ~TriggerManagerTest() override {}
 
   void SetUp() override {
@@ -153,7 +151,7 @@ class TriggerManagerTest : public ::testing::Test {
  private:
   TriggerManager trigger_manager_;
   MockThreatDetailsFactory mock_threat_details_factory_;
-  std::unique_ptr<base::test::TaskEnvironment> task_environment_;
+  content::BrowserTaskEnvironment task_environment_;
   content::TestBrowserContext browser_context_;
   content::TestWebContentsFactory web_contents_factory_;
   TestingPrefServiceSimple pref_service_;
