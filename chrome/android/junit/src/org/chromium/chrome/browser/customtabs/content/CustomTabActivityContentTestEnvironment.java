249108a971cba11d26b9558f9ef61059065d2373
Michael Thiessen <mthiesse@chromium.org>
2021-06-10 18:14:33 +0000

    Move Trusted Intent creation/verification into IntentUtils
    The code to create and verify trusted intents is embedder-agnostic, and
    could be even be used by webview if it ever had cause to. We have some
    feature modules that need to use trusted intents and right now they're
    using hacky workarounds.
    
    This code could live in components/ or its own feature module, but the
    overhead of creating and using a new module or component is too high
    for something this small, which could just as well sit in base/
    
    This change also removes EXTRA_IS_OPENED_BY_CHROME, which was not
    useful and just begging to cause security bugs.
    
    Bug: 1181700, 1139479
    Change-Id: I3a908fcc7b2e5476873146163804c472d5cbadaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939829
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891303}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityContentTestEnvironment.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityContentTestEnvironment.java
@@ -26,6 +26,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.Callback;
+import org.chromium.base.IntentUtils;
 import org.chromium.base.UserDataHost;
 import org.chromium.chrome.browser.ActivityTabProvider;
 import org.chromium.chrome.browser.IntentHandler;
@@ -74,13 +75,13 @@ public class CustomTabActivityContentTestEnvironment extends TestWatcher {
     public static final String SPECULATED_URL = JUnitTestGURLs.SPECULATED_URL;
     public static final String OTHER_URL = JUnitTestGURLs.EXAMPLE_URL;
 
-    public final Intent intent = new Intent();
+    public final Intent mIntent = new Intent();
 
     // clang-format off
     @Mock public CustomTabDelegateFactory customTabDelegateFactory;
     @Mock public ChromeActivity activity;
     @Mock public CustomTabsConnection connection;
-  @Mock public ColorProvider colorProvider;
+    @Mock public ColorProvider colorProvider;
     @Mock public CustomTabIntentDataProvider intentDataProvider;
     @Mock public TabObserverRegistrar tabObserverRegistrar;
     @Mock public CompositorViewHolder compositorViewHolder;
@@ -130,7 +131,7 @@ public class CustomTabActivityContentTestEnvironment extends TestWatcher {
 
         tabFromFactory = prepareTab();
 
-        when(intentDataProvider.getIntent()).thenReturn(intent);
+        when(intentDataProvider.getIntent()).thenReturn(mIntent);
         when(intentDataProvider.getSession()).thenReturn(session);
         when(intentDataProvider.getUrlToLoad()).thenReturn(INITIAL_URL);
         when(intentDataProvider.getActivityType()).thenReturn(ActivityType.CUSTOM_TAB);
@@ -225,8 +226,8 @@ public class CustomTabActivityContentTestEnvironment extends TestWatcher {
         WebContents webContents = mock(WebContents.class);
         realAsyncTabParamsManager.add(
                 tabId, new AsyncTabCreationParams(mock(LoadUrlParams.class), webContents));
-        IntentHandler.setTabId(intent, tabId);
-        IntentHandler.setForceIntentSenderChromeToTrue(true);
+        IntentHandler.setTabId(mIntent, tabId);
+        IntentUtils.setForceIsTrustedIntentForTesting(true);
         return webContents;
     }
 
