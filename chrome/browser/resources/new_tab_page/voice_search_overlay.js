b2137920b2b958d7e4c7b29d46a79b49c249fe74
Tibor Goldschwendt <tiborg@chromium.org>
2021-06-10 23:54:17 +0000

    [ntp] Triple timeouts for closing voice search overlay on error
    This gives more time for screen readers to explore the error message
    before the overlay disappears.
    
    Fixed: 1188256
    Change-Id: Icad2636056b2e826a3c4add6b0eae15e6aa9e87a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953970
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891457}

--- chrome/browser/resources/new_tab_page/voice_search_overlay.js
+++ chrome/browser/resources/new_tab_page/voice_search_overlay.js
@@ -41,7 +41,7 @@ const IDLE_TIMEOUT_MS = 8000;
  * occurred. This is a short timeout used when no click-target is present.
  * @type {number}
  */
-const ERROR_TIMEOUT_SHORT_MS = 3000;
+const ERROR_TIMEOUT_SHORT_MS = 9000;
 
 /**
  * Time in milliseconds to wait before closing the UI after an error has
@@ -49,7 +49,7 @@ const ERROR_TIMEOUT_SHORT_MS = 3000;
  * present.
  * @type {number}
  */
-const ERROR_TIMEOUT_LONG_MS = 8000;
+const ERROR_TIMEOUT_LONG_MS = 24000;
 
 /**
  * The minimum transition time for the volume rings.
