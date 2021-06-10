2d4e9bfe3cae998c8525c454cce2731b960d02d0
Callistus <callistus@google.com>
2021-06-10 11:37:45 +0000

    Put Help app background page logic behind the HelpAppBackgroundPage flag.
    The background page is currently locked behind the HelpAppLauncherSearch
    feature flag. This is incorrect as it is also used for showing the
    Discover tab notification.
    
    Instead of having to lock it behind all the features that might go in
    the background page, it is more manageable to put it behind its own flag.
    
    Bug: b/190565420
    Change-Id: Iee1df1a2c7e28d8c7499a5496e92276b0dbe9ecc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2950661
    Commit-Queue: Callistus Tan <callistus@google.com>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Reviewed-by: dstockwell <dstockwell@google.com>
    Cr-Commit-Position: refs/heads/master@{#891180}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2597,6 +2597,9 @@ extern const char kFrameThrottleFps30[];
 extern const char kFullRestoreName[];
 extern const char kFullRestoreDescription[];
 
+extern const char kHelpAppBackgroundPageName[];
+extern const char kHelpAppBackgroundPageDescription[];
+
 extern const char kHelpAppDiscoverTabName[];
 extern const char kHelpAppDiscoverTabDescription[];
 
