e2239225fe1f56a054dc30172afb2b1bd322a893
Evan Stade <estade@chromium.org>
2021-06-10 15:31:36 +0000

    Default PWAs: don't uninstall and replace app if force installed
    If one of the apps that would be uninstalled/replaced by a
    preinstalled web apps is force installed by policy, then don't
    install that web app.
    
    Bug: 1216462
    Change-Id: If043449d0d7c00281971a5e326eeb9ee7406f719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2946039
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891235}

--- chrome/browser/web_applications/extensions/extension_status_utils.cc
+++ chrome/browser/web_applications/extensions/extension_status_utils.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/browser/management_policy.h"
 #include "extensions/common/constants.h"
 
 namespace {
@@ -45,6 +46,24 @@ bool IsExtensionInstalled(content::BrowserContext* context,
   auto* registry = ExtensionRegistry::Get(context);
   // May be nullptr in unit tests.
   return registry && registry->GetInstalledExtension(extension_id);
+}
+
+bool IsExtensionForceInstalled(content::BrowserContext* context,
+                               const std::string& extension_id,
+                               std::u16string* reason) {
+  auto* registry = ExtensionRegistry::Get(context);
+  // May be nullptr in unit tests.
+  if (!registry)
+    return false;
+
+  auto* extension_system = ExtensionSystem::Get(context);
+  if (!extension_system)
+    return false;
+
+  const Extension* extension = registry->GetInstalledExtension(extension_id);
+  return extension &&
+         extension_system->management_policy()->MustRemainInstalled(extension,
+                                                                    reason);
 }
 
 bool IsExternalExtensionUninstalled(content::BrowserContext* context,
