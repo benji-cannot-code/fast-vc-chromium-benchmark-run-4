bbb58a778720b7cbe5349560c149676e80e53869
Mikel Astiz <mastiz@chromium.org>
2021-06-10 19:15:26 +0000

    [sync] Implement basic trusted vault recoverability reading
    This patch introduces minimalistic logic to support
    TrustedVaultClient::GetIsRecoverabilityDegraded() for
    StandaloneTrustedVaultClient.
    
    With this change, more relevant code is exercised in integration tests.
    including interactions with the fake server. TODOs remain to actually
    introduce more sophisticated logic like periodic polling.
    
    In tests, two fixtures are merged into one, such that
    SingleClientNigoriSyncTestWithSecurityDomainsServer no longer exists
    after this patch.
    
    Change-Id: If6e2a1f1e2eac2e043ba4fa5e2be0131a140954e
    Bug: 1201659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2948927
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#891329}

--- components/sync/trusted_vault/standalone_trusted_vault_backend.cc
+++ components/sync/trusted_vault/standalone_trusted_vault_backend.cc
@@ -63,6 +63,12 @@ absl::optional<TrustedVaultKeyAndVersion> GetLastTrustedVaultKeyAndVersion(
   return absl::nullopt;
 }
 
+void RetrieveIsRecoverabilityDegradedCompleted(
+    base::OnceCallback<void(bool)> cb,
+    TrustedVaultRecoverabilityStatus status) {
+  std::move(cb).Run(status == TrustedVaultRecoverabilityStatus::kDegraded);
+}
+
 }  // namespace
 
 StandaloneTrustedVaultBackend::PendingTrustedRecoveryMethod::
@@ -237,9 +243,13 @@ bool StandaloneTrustedVaultBackend::MarkKeysAsStale(
 void StandaloneTrustedVaultBackend::GetIsRecoverabilityDegraded(
     const CoreAccountInfo& account_info,
     base::OnceCallback<void(bool)> cb) {
-  // TODO(crbug.com/1201659): Implement logic.
-  NOTIMPLEMENTED();
-  std::move(cb).Run(is_recoverability_degraded_for_testing_);
+  // TODO(crbug.com/1201659): Improve this logic properly and add test coverage,
+  // including throttling and periodic polling.
+  ongoing_degraded_recoverability_request_ =
+      connection_->RetrieveIsRecoverabilityDegraded(
+          account_info,
+          base::BindOnce(&RetrieveIsRecoverabilityDegradedCompleted,
+                         std::move(cb)));
 }
 
 void StandaloneTrustedVaultBackend::AddTrustedRecoveryMethod(
@@ -318,11 +328,6 @@ StandaloneTrustedVaultBackend::GetDeviceRegistrationInfoForTesting(
     return sync_pb::LocalDeviceRegistrationInfo();
   }
   return per_user_vault->local_device_registration_info();
-}
-
-void StandaloneTrustedVaultBackend::SetRecoverabilityDegradedForTesting() {
-  is_recoverability_degraded_for_testing_ = true;
-  delegate_->NotifyRecoverabilityDegradedChanged();
 }
 
 std::vector<uint8_t>
@@ -494,7 +499,6 @@ void StandaloneTrustedVaultBackend::OnTrustedRecoveryMethodAdded(
     TrustedVaultRegistrationStatus status) {
   DCHECK(ongoing_degraded_recoverability_request_);
   ongoing_degraded_recoverability_request_ = nullptr;
-  is_recoverability_degraded_for_testing_ = false;
 
   std::move(cb).Run();
   delegate_->NotifyRecoverabilityDegradedChanged();
--- components/sync/trusted_vault/standalone_trusted_vault_backend.h
+++ components/sync/trusted_vault/standalone_trusted_vault_backend.h
@@ -106,8 +106,6 @@ class StandaloneTrustedVaultBackend
   sync_pb::LocalDeviceRegistrationInfo GetDeviceRegistrationInfoForTesting(
       const std::string& gaia_id);
 
-  void SetRecoverabilityDegradedForTesting();
-
   std::vector<uint8_t> GetLastAddedRecoveryMethodPublicKeyForTesting() const;
 
   void SetClockForTesting(base::Clock* clock);
@@ -206,7 +204,6 @@ class StandaloneTrustedVaultBackend
   // be overridden in tests.
   base::Clock* clock_;
 
-  bool is_recoverability_degraded_for_testing_ = false;
   std::vector<uint8_t> last_added_recovery_method_public_key_for_testing_;
 };
 
