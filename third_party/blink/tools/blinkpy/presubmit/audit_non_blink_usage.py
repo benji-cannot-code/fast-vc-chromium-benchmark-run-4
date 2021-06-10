3fb49c1b140941f012a32677c547634b560c57c9
Nektarios Paisios <nektar@chromium.org>
2021-06-10 21:58:16 +0000

    Expands the definition of a line breaking object and harmonizes it with Blink Editing
    All non-inline and non-ruby text elements are paragraph boundaries. Also, display:none content with aria-hidden=false
    should be marked as a paragraph boundary based on its accessible role.
    The first condition is derived from the rules that govern the movement of Ctrl=Up/Down in blink/editing.
    For example, an inline-block element that comes before an inline element is not a paragraph boundary.
    This can easily be tested using a content editable.
    
    Tests needed to be updated to accommodate this new expanded definition of a paragraph boundary,
    as well as fixed to remove objects that were previously marked as paragraphs but were not.
    Every updated test was manually verified by adding "contenteditable=true" and testing with Ctrl+Up/Down.
    Note that Ctrl+Up/Down is the same as Option-Up/Down on Mac.
    
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    
    AX-Relnotes: Better paragraph navigation in screen reader only text and some rich text fields.
    Bug: 1135589
    Change-Id: I41e22d359b43d1d30650bca2e4e488f380607233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2908332
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891402}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -532,6 +532,7 @@ _CONFIG = [
             'ui::IsLandmark',
             'ui::IsPlatformDocument',
             'ui::IsPresentational',
+            'ui::IsStructure',
             'ui::IsTableLike',
             'ui::IsTableRow',
             'ui::IsTableHeader',
