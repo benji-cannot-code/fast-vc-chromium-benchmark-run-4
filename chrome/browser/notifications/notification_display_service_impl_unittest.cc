d158a3ffbc9c337c9e9db707ef8e8af60ea8bf4a
Richard Knoll <knollr@chromium.org>
2021-06-10 10:38:54 +0000

    Set a default NotificationPlatformBridge in tests
    This sets up a default StubNotificationPlatformBridge to be used in unit
    tests instead of having tests manually set it.
    
    Bug: 1217088
    Change-Id: Ibc53d91bebdf53268e36141ea19818da8bdac705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2949149
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891170}

--- chrome/browser/notifications/notification_display_service_impl_unittest.cc
+++ chrome/browser/notifications/notification_display_service_impl_unittest.cc
@@ -12,13 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_blocker.h"
 #include "chrome/browser/notifications/notification_display_queue.h"
 #include "chrome/browser/notifications/notification_display_service_impl.h"
 #include "chrome/browser/notifications/notification_platform_bridge_delegator.h"
-#include "chrome/browser/notifications/stub_notification_platform_bridge.h"
-#include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -100,12 +97,6 @@ class NotificationDisplayServiceImplTest : public testing::Test {
 
   // BrowserWithTestWindowTest:
   void SetUp() override {
-    TestingBrowserProcess* browser_process = TestingBrowserProcess::GetGlobal();
-    if (browser_process) {
-      browser_process->SetNotificationPlatformBridge(
-          std::make_unique<StubNotificationPlatformBridge>());
-    }
-
     service_ = std::make_unique<NotificationDisplayServiceImpl>(&profile_);
 
     auto notification_delegator =
--- chrome/browser/notifications/notification_display_service_tester.cc
+++ chrome/browser/notifications/notification_display_service_tester.cc
@@ -5,21 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/notifications/notification_display_service_tester.h"
 
-#include <set>
-
 #include "base/bind.h"
-#include "build/buildflag.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/notifications/notification_display_service_factory.h"
-#include "chrome/browser/notifications/notification_platform_bridge.h"
 #include "chrome/browser/notifications/stub_notification_display_service.h"
-#include "chrome/browser/notifications/stub_notification_platform_bridge.h"
 #include "chrome/browser/notifications/system_notification_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/test/base/testing_browser_process.h"
 #include "components/keyed_service/content/browser_context_keyed_service_shutdown_notifier_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "ui/base/buildflags.h"
 #include "ui/message_center/public/cpp/notification.h"
 
 namespace {
@@ -56,14 +49,6 @@ class NotificationDisplayServiceShutdownNotifierFactory
 NotificationDisplayServiceTester::NotificationDisplayServiceTester(
     Profile* profile)
     : profile_(profile) {
-#if !BUILDFLAG(ENABLE_MESSAGE_CENTER)
-  TestingBrowserProcess* browser_process = TestingBrowserProcess::GetGlobal();
-  if (browser_process) {
-    browser_process->SetNotificationPlatformBridge(
-        std::make_unique<StubNotificationPlatformBridge>());
-  }
-#endif
-
   // TODO(peter): Remove the StubNotificationDisplayService in favor of having
   // a fully functional MockNotificationPlatformBridge.
   if (profile_) {
