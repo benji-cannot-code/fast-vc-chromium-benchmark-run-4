59a49b982aaf72ef20309304c8cec4a3bcefc1be
Ari Chivukula <arichiv@chromium.org>
2021-06-10 19:22:35 +0000

    Convert factory to StorageKey from url::Origin
    This is the eleventh of a series of CLs to convert IndexedDB files to
    use the new StorageKey class. Starting from the bottom and pushing up to
    the outer edge.
    
    Bug: 1210555
    Change-Id: Ia321927300899356b543073e9e4d2b5a03436338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953302
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891336}

--- content/browser/indexed_db/indexed_db_unittest.cc
+++ content/browser/indexed_db/indexed_db_unittest.cc
@@ -119,10 +119,10 @@ class IndexedDBTest : public testing::Test {
       // Loop through all open origins, and force close them, and request the
       // deletion of the leveldb state. Once the states are no longer around,
       // delete all of the databases on disk.
-      auto open_factory_origins = factory->GetOpenOrigins();
-      for (auto origin : open_factory_origins) {
+      auto open_factory_storage_keys = factory->GetOpenOrigins();
+      for (const auto& storage_key : open_factory_storage_keys) {
         context_->ForceCloseSync(
-            origin,
+            storage_key.origin(),
             storage::mojom::ForceCloseReason::FORCE_CLOSE_DELETE_ORIGIN);
       }
       // All leveldb databases are closed, and they can be deleted.
@@ -257,7 +257,7 @@ TEST_F(IndexedDBTest, ForceCloseOpenDatabasesOnDelete) {
                 std::make_unique<IndexedDBPendingConnection>(
                     open_callbacks, open_db_callbacks, host_transaction_id,
                     version, std::move(create_transaction_callback1)),
-                kTestStorageKey.origin(), context()->data_path());
+                kTestStorageKey, context()->data_path());
   EXPECT_TRUE(base::DirectoryExists(test_path));
 
   auto create_transaction_callback2 =
@@ -266,7 +266,7 @@ TEST_F(IndexedDBTest, ForceCloseOpenDatabasesOnDelete) {
                 std::make_unique<IndexedDBPendingConnection>(
                     closed_callbacks, closed_db_callbacks, host_transaction_id,
                     version, std::move(create_transaction_callback2)),
-                kTestStorageKey.origin(), context()->data_path());
+                kTestStorageKey, context()->data_path());
   RunPostedTasks();
   ASSERT_TRUE(closed_callbacks->connection());
   closed_callbacks->connection()->AbortTransactionsAndClose(
@@ -313,7 +313,8 @@ TEST_F(IndexedDBTest, DeleteFailsIfDirectoryLocked) {
 }
 
 TEST_F(IndexedDBTest, ForceCloseOpenDatabasesOnCommitFailure) {
-  const Origin kTestOrigin = Origin::Create(GURL("http://test/"));
+  const blink::StorageKey kTestStorageKey =
+      blink::StorageKey::CreateFromStringForTesting("http://test/");
 
   auto* factory =
       static_cast<IndexedDBFactoryImpl*>(context()->GetIDBFactory());
@@ -328,23 +329,24 @@ TEST_F(IndexedDBTest, ForceCloseOpenDatabasesOnCommitFailure) {
       callbacks, db_callbacks,
       transaction_id, IndexedDBDatabaseMetadata::DEFAULT_VERSION,
       std::move(create_transaction_callback1));
-  factory->Open(u"db", std::move(connection), Origin(kTestOrigin),
+  factory->Open(u"db", std::move(connection), kTestStorageKey,
                 context()->data_path());
   RunPostedTasks();
 
   ASSERT_TRUE(callbacks->connection());
 
   // ConnectionOpened() is usually called by the dispatcher.
-  context()->ConnectionOpened(kTestOrigin, callbacks->connection());
+  context()->ConnectionOpened(kTestStorageKey.origin(),
+                              callbacks->connection());
 
-  EXPECT_TRUE(factory->IsBackingStoreOpen(kTestOrigin));
+  EXPECT_TRUE(factory->IsBackingStoreOpen(kTestStorageKey));
 
   // Simulate the write failure.
   leveldb::Status status = leveldb::Status::IOError("Simulated failure");
-  factory->HandleBackingStoreFailure(kTestOrigin);
+  factory->HandleBackingStoreFailure(kTestStorageKey);
 
   EXPECT_TRUE(db_callbacks->forced_close_called());
-  EXPECT_FALSE(factory->IsBackingStoreOpen(kTestOrigin));
+  EXPECT_FALSE(factory->IsBackingStoreOpen(kTestStorageKey));
 }
 
 TEST(ScopesLockManager, TestRangeDifferences) {
