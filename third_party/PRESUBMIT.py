c5b62c5b3a0a21cb921b0f6aa18696404709aea4
Josip Sokcevic <sokcevic@google.com>
2021-06-11 13:23:01 +0000

    [py3] Migrate third_party PRESUBMIT
    Bug: 1212061
    Change-Id: I6dcf4592acb364421f648c1ba94d0dc8ce26bf95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2954192
    Auto-Submit: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891622}

--- third_party/PRESUBMIT.py
+++ third_party/PRESUBMIT.py
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
+USE_PYTHON = True
+
 ANDROID_ALLOWED_LICENSES = [
   'A(pple )?PSL 2(\.0)?',
   'Android Software Development Kit License',
