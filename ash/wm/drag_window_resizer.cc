8c5a0c7043d17a3c8688c9f5c707865fed46b81b
Scott Violet <sky@chromium.org>
2021-06-11 22:57:27 +0000

    ash: remove unnecessary work around
    It looks like Malay fixed the bug that necessitated this work around
    so that the work around should no longer be necessary.
    
    BUG=834114,786628
    TEST=none
    
    Change-Id: Ib561341f11ef6e98b67ae5fda4fd2e694cbf3567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956895
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891830}

--- ash/wm/drag_window_resizer.cc
+++ ash/wm/drag_window_resizer.cc
@@ -30,17 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/wm/core/window_util.h"
 
 namespace ash {
-namespace {
-
-void RecursiveSchedulePainter(ui::Layer* layer) {
-  if (!layer)
-    return;
-  layer->SchedulePaint(gfx::Rect(layer->size()));
-  for (auto* child : layer->children())
-    RecursiveSchedulePainter(child);
-}
-
-}  // namespace
 
 // static
 DragWindowResizer* DragWindowResizer::instance_ = nullptr;
@@ -124,10 +113,6 @@ bool DragWindowResizer::ShouldAllowMouseWarp() {
 
 void DragWindowResizer::EndDragImpl() {
   drag_window_controller_.reset();
-
-  // TODO(malaykeshav) - This is temporary fix/workaround that keeps performance
-  // but may not give the best UI while dragging. See https://crbug/834114
-  RecursiveSchedulePainter(GetTarget()->layer());
 
   // Check if the destination is another display.
   if (details().source == wm::WINDOW_MOVE_SOURCE_TOUCH)
