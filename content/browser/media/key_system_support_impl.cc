f547b8353d0f0bc283004297552604e429d24213
Xiaohan Wang <xhwang@chromium.org>
2021-06-11 01:44:36 +0000

    media: Simplify HEVC handling in chrome_key_systems.cc
    The build and run time flags should be checked in cdm_registration.cc
    when we declare the capabilities. In chrome_key_systems.cc, we should
    just consume that information and there's no need to check again.
    
    This also fixes a rebase issue for Windows where we hard-code the HEVC
    profile support, instead of checking what's in the capabilities.
    
    Bug: 1115687, b/187242911
    Change-Id: I7f8fa20e9380add10f57fac243fe7aca8eea7c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2950567
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891503}

--- content/browser/media/key_system_support_impl.cc
+++ content/browser/media/key_system_support_impl.cc
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/media/key_system_support_win.h"
 #endif
 
+#if BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
+#include "ash/constants/ash_features.h"
+#endif  // BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
+
 namespace content {
 
 namespace {
@@ -120,15 +124,17 @@ absl::optional<media::CdmCapability> GetHardwareSecureCapability(
     bool* lazy_initialize) {
   *lazy_initialize = false;
 
-  // We use the USE_CHROMEOS_PROTECTED_MEDIA build flag on Chrome OS to control
-  // when HW secure decryption is enabled, so disable the feature flag in that
-  // case.
-#if !BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
+#if BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
+  if (!base::FeatureList::IsEnabled(chromeos::features::kCdmFactoryDaemon)) {
+    DVLOG(1) << "Hardware secure decryption disabled";
+    return absl::nullopt;
+  }
+#else
   if (!base::FeatureList::IsEnabled(media::kHardwareSecureDecryption)) {
     DVLOG(1) << "Hardware secure decryption disabled";
     return absl::nullopt;
   }
-#endif
+#endif  // BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
 
   // Secure codecs override takes precedence over other checks.
   auto overridden_capability =
