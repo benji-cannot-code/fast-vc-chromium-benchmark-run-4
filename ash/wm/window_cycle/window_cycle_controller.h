3425104ebeee8443c8c1f20367b770e7bc4dd978
Sammie Quon <sammiequon@chromium.org>
2021-06-11 17:00:32 +0000

    wm: Close window cycler when desk added or removed.
    The tab slider container is created if there are multiple desks at
    creation. If a desk is added, the layout will try to layout the tab
    slider container, resulting in a crash.
    
    We could change visiblility/creation of the tab slider container to
    match the intial number of desks instead of the current, or dynamically
    create and destroy it.
    
    Since this needs to be merged, I opted for the most simple approach,
    which is to close the cycler when a desk is added or removed, and
    added more DCHECKS, since the expectation now is that interactive alt
    tab mode remains the same for the duration of a cycle job.
    
    Bug: 1216238
    Test: some manual testing involving forcing alt tab visible always
    Test: added regression test
    Change-Id: I6ab086b2163bbfe16324f39ab5e91e92141f26aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2952999
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891691}

--- ash/wm/window_cycle/window_cycle_controller.h
+++ ash/wm/window_cycle/window_cycle_controller.h
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/public/cpp/shell_window_ids.h"
+#include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "base/macros.h"
+#include "base/scoped_observation.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_registry_simple.h"
 
@@ -36,7 +38,8 @@ class WindowCycleList;
 // until the cycling ends.  Thus we maintain the state of the windows
 // at the beginning of the gesture so you can cycle through in a consistent
 // order.
-class ASH_EXPORT WindowCycleController : public SessionObserver {
+class ASH_EXPORT WindowCycleController : public SessionObserver,
+                                         public DesksController::Observer {
  public:
   using WindowList = std::vector<aura::Window*>;
 
@@ -144,6 +147,15 @@ class ASH_EXPORT WindowCycleController : public SessionObserver {
   // user switches the alt-tab mode via keyboard navigation or button clicking.
   void OnModeChanged(bool per_desk, ModeSwitchSource source);
 
+  // DesksController::Observer:
+  void OnDeskAdded(const Desk* desk) override;
+  void OnDeskRemoved(const Desk* desk) override;
+  void OnDeskReordered(int old_index, int new_index) override {}
+  void OnDeskActivationChanged(const Desk* activated,
+                               const Desk* deactivated) override {}
+  void OnDeskSwitchAnimationLaunching() override {}
+  void OnDeskSwitchAnimationFinished() override {}
+
  private:
   // Gets a list of windows from the currently open windows, removing windows
   // with transient roots already in the list. The returned list of windows
@@ -195,6 +207,9 @@ class ASH_EXPORT WindowCycleController : public SessionObserver {
 
   // The pref change registrar to observe changes in prefs value.
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
+
+  base::ScopedObservation<DesksController, DesksController::Observer>
+      desks_observation_{this};
 
   DISALLOW_COPY_AND_ASSIGN(WindowCycleController);
 };
