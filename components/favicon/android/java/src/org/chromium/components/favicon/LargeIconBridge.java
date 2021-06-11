0aa1cd253cd8b3b436d5f1ae89be9bc4c1cf6525
Pete Williamson <petewil@chromium.org>
2021-06-11 22:25:50 +0000

    Make the native class for LargeIconBridge JNI method public.
    This will allow us to use it in junit tests such as
    https://chromium-review.googlesource.com/c/chromium/src/+/2956198
    
    Change-Id: Id916dfc8404c45fc65181494905a8431c02a8dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956902
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891821}

--- components/favicon/android/java/src/org/chromium/components/favicon/LargeIconBridge.java
+++ components/favicon/android/java/src/org/chromium/components/favicon/LargeIconBridge.java
@@ -175,7 +175,7 @@ public class LargeIconBridge {
     }
 
     @NativeMethods
-    interface Natives {
+    public interface Natives {
         long init();
         void destroy(long nativeLargeIconBridge);
         boolean getLargeIconForURL(long nativeLargeIconBridge,
