7f53202a206566ba2007a647242fd33094445a20
Felipe Andrade <fsandrade@chromium.org>
2021-06-11 16:25:41 +0000

    Test Chromad login password submission
    Update active_directory_login_browsertest.cc, adding a check for
    submitted password(s). This covers both "login" and "password change"
    flows. FakeAuthpolicyClient was modified, to keep track of the auth
    password.
    
    These tests cover the error fixed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/2945117
    
    Bug: 1203375
    Change-Id: Ib808e66ac77b6e8dcaf41c82050be7772997df91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951721
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891674}

--- chromeos/dbus/authpolicy/fake_authpolicy_client.cc
+++ chromeos/dbus/authpolicy/fake_authpolicy_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/files/file_util.h"
 #include "base/hash/md5.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -57,6 +58,16 @@ void RunSignalCallback(const std::string& interface_name,
                        dbus::ObjectProxy::SignalCallback signal_callback) {
   signal_callback.Run(
       std::make_unique<dbus::Signal>(interface_name, method_name).get());
+}
+
+// Reads the password from the file descriptor `password_fd`.
+// Not very efficient, but simple!
+std::string ReadPassword(int password_fd) {
+  std::string password;
+  char c;
+  while (base::ReadFromFD(password_fd, &c, 1))
+    password.push_back(c);
+  return password;
 }
 
 }  // namespace
@@ -122,6 +133,9 @@ void FakeAuthPolicyClient::AuthenticateUser(
     int password_fd,
     AuthCallback callback) {
   DCHECK(InstallAttributes::Get()->IsActiveDirectoryManaged());
+
+  auth_password_ = ReadPassword(password_fd);
+
   authpolicy::ErrorType error = authpolicy::ERROR_NONE;
   authpolicy::ActiveDirectoryAccountInfo account_info;
   if (auth_error_ != authpolicy::ERROR_NONE) {
