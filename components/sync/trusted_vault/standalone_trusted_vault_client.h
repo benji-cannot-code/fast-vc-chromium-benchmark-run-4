39b92b2664bf83bd40d0ed40dbbe101e262770ca
Mikel Astiz <mastiz@chromium.org>
2021-06-11 16:45:41 +0000

    Reland "[sync] Implement basic trusted vault recoverability reading"
    This is a reland of 125049d1c2da5e2b3ee8fd1b8d9270203d5a7b3f
    
    Reason to reland: the underlying race condition is believed to be
    understood and fixed by using a separate
    TrustedVaultConnection::Request, in case there are concurrent
    requests to the server (add method + get recoverability).
    
    Original change's description:
    > [sync] Implement basic trusted vault recoverability reading
    >
    > This patch introduces minimalistic logic to support
    > TrustedVaultClient::GetIsRecoverabilityDegraded() for
    > StandaloneTrustedVaultClient.
    >
    > With this change, more relevant code is exercised in integration tests.
    > including interactions with the fake server. TODOs remain to actually
    > introduce more sophisticated logic like periodic polling.
    >
    > In tests, two fixtures are merged into one, such that
    > SingleClientNigoriSyncTestWithSecurityDomainsServer no longer exists
    > after this patch.
    >
    > Change-Id: If6e2a1f1e2eac2e043ba4fa5e2be0131a140954e
    > Bug: 1201659
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2948927
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    > Cr-Commit-Position: refs/heads/master@{#891329}
    
    Bug: 1201659
    Change-Id: I0516299ac8cff66bcecece6bc8ac9d628ca72e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2955948
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891681}

--- components/sync/trusted_vault/standalone_trusted_vault_client.h
+++ components/sync/trusted_vault/standalone_trusted_vault_client.h
@@ -75,7 +75,6 @@ class StandaloneTrustedVaultClient : public TrustedVaultClient {
   void FetchBackendPrimaryAccountForTesting(
       base::OnceCallback<void(const absl::optional<CoreAccountInfo>&)> callback)
       const;
-  void SetRecoverabilityDegradedForTesting();
   void GetLastAddedRecoveryMethodPublicKeyForTesting(
       base::OnceCallback<void(const std::vector<uint8_t>&)> callback);
 
