f1091662dec15ec0811bf9fa233b47ee7deefdc3
Guido Urdaneta <guidou@chromium.org>
2021-06-11 06:01:11 +0000

    [BreakoutBox] Update tests for MediaStreamTrackProcessor
    Add worker tests and simplify an existing audio test
    
    Change-Id: I513f9627ffc3646e36db9076e5e3c8580d1e2619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945190
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891542}

---
+++ third_party/blink/web_tests/external/wpt/mediacapture-insertable-streams/MediaStreamTrackProcessor-worker.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+onmessage = async msg => {
+  const reader = msg.data.readable.getReader();
+  let readResult = await reader.read();
+  postMessage(readResult.value);
+  readResult.value.close();
+  // Continue reading until the stream is done due to a track.stop()
+  while (true) {
+    readResult = await reader.read();
+    if (readResult.done) {
+      break;
+    } else {
+      readResult.value.close();
+    }
+  }
+  await reader.closed;
+  postMessage('closed');
+}
