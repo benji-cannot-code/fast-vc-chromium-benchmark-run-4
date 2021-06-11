ebd9e54cf18de57c50c3330dc6c58ad8e814f9f7
Mike Taylor <miketaylr@chromium.org>
2021-06-11 19:20:37 +0000

    Freeze WEBKIT_VERSION_MAJOR and WEBKIT_VERSION_MINOR to 537.37.
    Note that this does not have any user-facing changes, as these haven't
    been updated since 2013, when Blink forked from WebKit.
    
    Also, rename WEBKIT_SVN_REVISION to CHROMIUM_GIT_REVISION,
    because that's what it actually is. :)
    
    It seems the DevTools protocol has callers for GetWebKitVersion
    and GetWebKitRevision, so for simplicity this CL leaves those alone
    to be handled in the future.
    
    Bug: 1215229
    Change-Id: Iffd072313d08f066d6ec5af2128f2191d60e14ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928928
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891749}

--- content/common/user_agent.cc
+++ content/common/user_agent.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "build/util/webkit_version.h"
+#include "build/util/chromium_git_revision.h"
 
 #if defined(OS_MAC)
 #include "base/mac/mac_util.h"
@@ -63,14 +63,11 @@ std::string GetUnifiedPlatform() {
 }
 
 std::string GetWebKitVersion() {
-  return base::StringPrintf("%d.%d (%s)",
-                            WEBKIT_VERSION_MAJOR,
-                            WEBKIT_VERSION_MINOR,
-                            WEBKIT_SVN_REVISION);
+  return base::StringPrintf("537.36 (%s)", CHROMIUM_GIT_REVISION);
 }
 
 std::string GetWebKitRevision() {
-  return WEBKIT_SVN_REVISION;
+  return CHROMIUM_GIT_REVISION;
 }
 
 std::string BuildCpuInfo() {
@@ -329,15 +326,10 @@ std::string BuildUserAgentFromOSAndProduct(const std::string& os_info,
   // This is done to expose our product name in a manner that is maximally
   // compatible with Safari, we hope!!
   std::string user_agent;
-  base::StringAppendF(
-      &user_agent,
-      "Mozilla/5.0 (%s) AppleWebKit/%d.%d (KHTML, like Gecko) %s Safari/%d.%d",
-      os_info.c_str(),
-      WEBKIT_VERSION_MAJOR,
-      WEBKIT_VERSION_MINOR,
-      product.c_str(),
-      WEBKIT_VERSION_MAJOR,
-      WEBKIT_VERSION_MINOR);
+  base::StringAppendF(&user_agent,
+                      "Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "%s Safari/537.36",
+                      os_info.c_str(), product.c_str());
   return user_agent;
 }
 
