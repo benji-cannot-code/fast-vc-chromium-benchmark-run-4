fa21596061eb4126511c927f9989e713d406851c
Lloyd Pique <lpique@chromium.org>
2021-06-11 00:42:44 +0000

    exo: Convert compatibility tests back to Python 3
    This brings back the Python 3 form of the various script files used to
    generate the compatibility tests, as they more or less were in an early
    version of https://crrev.com/c/2402148,
    
    Note that this change also effectively reverts
    https://crrev.com/c/2638785, as the code now properly uses character
    strings instead of byte strings for backwards compatibility.
    
    BUG=b:157254342
    TEST=wayland_client_compatibility_tests
    
    Change-Id: I76fedbb53af36f9309f80c2a3e99e366238cdfc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2654474
    Commit-Queue: Lloyd Pique <lpique@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891479}

--- components/exo/wayland/compatibility_test/wayland_protocol_externals.py
+++ components/exo/wayland/compatibility_test/wayland_protocol_externals.py
@@ -3,11 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from typing import Tuple
+
 import wayland_protocol_data_classes
 
+# Short aliases for typing
+Protocol = wayland_protocol_data_classes.Protocol
 
-def get_external_interfaces_for_protocol(protocol):
-    # type: (Protocol) -> Tuple[str, ...]
+
+def get_external_interfaces_for_protocol(
+        protocol: Protocol) -> Tuple[str, ...]:
     """Gets the names of interfaces referenced but not defined by a protocol."""
 
     # Get the set of interfaces defined by the protocol
