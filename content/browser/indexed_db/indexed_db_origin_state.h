1b0dfa61dc5ef757083beb4afcd9fa88cd8cc00f
Ari Chivukula <arichiv@chromium.org>
2021-06-11 00:59:11 +0000

    Convert origin_state to StorageKey from url::Origin
    This is the twelfth of a series of CLs to convert IndexedDB files to use
    the new StorageKey class. Starting from the bottom and pushing up to
    the outer edge.
    
    Bug: 1210555
    Change-Id: I2ccc399b1038518f5f1150d03c4a47bedd9c78fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953362
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891484}

--- content/browser/indexed_db/indexed_db_origin_state.h
+++ content/browser/indexed_db/indexed_db_origin_state.h
@@ -21,8 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/indexed_db/indexed_db_origin_state_handle.h"
 #include "content/browser/indexed_db/indexed_db_task_helper.h"
 #include "content/common/content_export.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/leveldatabase/src/include/leveldb/status.h"
-#include "url/origin.h"
 
 namespace content {
 class IndexedDBBackingStore;
@@ -37,8 +37,8 @@ constexpr const char kIDBCloseImmediatelySwitch[] = "idb-close-immediately";
 // shut off.
 CONTENT_EXPORT extern const base::Feature kCompactIDBOnClose;
 
-// IndexedDBOriginState manages the per-origin IndexedDB state, and contains the
-// backing store for the origin.
+// IndexedDBOriginState manages the per-storage_key IndexedDB state, and
+// contains the backing store for the storage_key.
 //
 // This class is expected to manage its own lifetime by using the
 // |destruct_myself_| closure, which is expected to destroy this object in the
@@ -62,7 +62,7 @@ class CONTENT_EXPORT IndexedDBOriginState {
   static constexpr const base::TimeDelta kMaxEarliestGlobalSweepFromNow =
       base::TimeDelta::FromHours(1);
   // Maximum time interval between runs of the IndexedDBSweeper for a given
-  // origin. Sweeping only occurs after backing store close.
+  // storage_key. Sweeping only occurs after backing store close.
   // Visible for testing.
   static constexpr const base::TimeDelta kMaxEarliestOriginSweepFromNow =
       base::TimeDelta::FromDays(3);
@@ -73,7 +73,7 @@ class CONTENT_EXPORT IndexedDBOriginState {
   static constexpr const base::TimeDelta kMaxEarliestGlobalCompactionFromNow =
       base::TimeDelta::FromHours(1);
   // Maximum time interval between runs of the IndexedDBCompactionTask for a
-  // given origin. Compaction only occurs after backing store close.
+  // given storage_key. Compaction only occurs after backing store close.
   // Visible for testing.
   static constexpr const base::TimeDelta kMaxEarliestOriginCompactionFromNow =
       base::TimeDelta::FromDays(3);
@@ -94,7 +94,7 @@ class CONTENT_EXPORT IndexedDBOriginState {
   // |earliest_global_sweep_time| and |earliest_global_compaction_time| are
   // expected to outlive this object.
   IndexedDBOriginState(
-      url::Origin origin,
+      blink::StorageKey storage_key,
       bool persist_for_incognito,
       base::Clock* clock,
       TransactionalLevelDBFactory* transactional_leveldb_factory,
@@ -124,7 +124,7 @@ class CONTENT_EXPORT IndexedDBOriginState {
 
   void StopPersistingForIncognito();
 
-  const url::Origin& origin() { return origin_; }
+  const blink::StorageKey& storage_key() { return storage_key_; }
   IndexedDBBackingStore* backing_store() {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     return backing_store_.get();
@@ -209,7 +209,7 @@ class CONTENT_EXPORT IndexedDBOriginState {
 
   SEQUENCE_CHECKER(sequence_checker_);
 
-  url::Origin origin_;
+  blink::StorageKey storage_key_;
 
   // True if this factory should be remain alive due to the storage partition
   // being for incognito mode, and our backing store being in-memory. This is
