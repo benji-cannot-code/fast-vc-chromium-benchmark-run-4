0f968774b1e0d213ccb94ce604bfa8e2507c2d97
Hui Yingst <nigi@chromium.org>
2021-06-11 18:38:12 +0000

    [unseasoned-pdf] Use a helper for parsing in PdfViewWebPlugin.
    This CL adds a helper ParseWebPluginParams() which creates a new struct
    `ParsedParams` from a given `blink::WebPluginParams`, and adds unit
    tests for them.
    
    By replacing the process of parsing `initial_params_` with the helper,
    this CL increases PdfViewWebPlugin's test coverage.
    
    Also fix an IWYU issue in //pdf/pdf_view_web_plugin_unittest.cc.
    
    Bug: 1199558
    Change-Id: Id2eca7bb046e987bb1b30aa34d8e03045a376f26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951783
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891730}

--- pdf/pdf_view_web_plugin_unittest.cc
+++ pdf/pdf_view_web_plugin_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_text_input_type.h"
+#include "third_party/blink/public/platform/web_vector.h"
 #include "third_party/blink/public/web/web_associated_url_loader.h"
 #include "third_party/blink/public/web/web_plugin_params.h"
 #include "third_party/skia/include/core/SkBitmap.h"
