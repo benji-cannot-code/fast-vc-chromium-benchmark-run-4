38fca2f1b925613bfa448694e683c7c42eabe62d
Nektarios Paisios <nektar@chromium.org>
2021-06-11 17:01:40 +0000

    Computes the value attribute on the browser for content editables
    This feature is not turned on in the platform layer yet, i.e. it is not exposed
    to code in BrowserAccessibility, Automation Bindings and AXPlatformNodeBase.
    This patch only implements the internals of the feature in AXNode and AXComputedNodeData.
    
    If a content editable has no kValue attribute set in its AXNodeData, AXComputedNodeData
    will compute one for it from its inner text. The inner text will be cached for faster
    retrieval of the kValue attribute if it is requested again before the tree updates. If the tree updates, any cached
    computed node data is deleted. If the kValue attribute is present, it
    overrides any computed node data.
    
    Also implements a framework that could be used for computing any string attribute on the browser
    and not only the kValue attribute since string attributes are the most costly
    to serialize and send from Blink to the browser process.
    
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    
    AX-Relnotes: n/a.
    Bug: 1123141
    Change-Id: I997bd7905cab3ad5ce88a095b6d06e19e31cd96a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951581
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891692}

--- ui/accessibility/ax_computed_node_data.cc
+++ ui/accessibility/ax_computed_node_data.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_computed_node_data.h"
 
 #include "base/logging.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node.h"
@@ -18,6 +19,49 @@ namespace ui {
 AXComputedNodeData::AXComputedNodeData(const AXNode& node) : owner_(&node) {}
 
 AXComputedNodeData::~AXComputedNodeData() = default;
+
+bool AXComputedNodeData::HasOrCanComputeAttribute(
+    const ax::mojom::StringAttribute attribute) const {
+  DCHECK(owner_);
+  if (owner_->data().HasStringAttribute(attribute))
+    return true;
+
+  switch (attribute) {
+    case ax::mojom::StringAttribute::kValue:
+      // The value attribute could be computed on the browser for content
+      // editables and ARIA text/search boxes.
+      return owner_->data().IsNonAtomicTextField();
+    default:
+      return false;
+  }
+}
+
+const std::string& AXComputedNodeData::GetOrComputeAttributeUTF8(
+    const ax::mojom::StringAttribute attribute) const {
+  DCHECK(owner_);
+  if (owner_->data().HasStringAttribute(attribute))
+    return owner_->data().GetStringAttribute(attribute);
+
+  switch (attribute) {
+    case ax::mojom::StringAttribute::kValue:
+      if (owner_->data().IsNonAtomicTextField()) {
+        DCHECK(HasOrCanComputeAttribute(attribute))
+            << "Code in `HasOrCanComputeAttribute` should be in sync with "
+               "'GetOrComputeAttributeUTF8`";
+        return GetOrComputeInnerTextUTF8();
+      }
+      return base::EmptyString();
+    default:
+      // This is a special case: for performance reasons do not use
+      // `base::EmptyString()` in other places throughout the codebase.
+      return base::EmptyString();
+  }
+}
+
+std::u16string AXComputedNodeData::GetOrComputeAttributeUTF16(
+    const ax::mojom::StringAttribute attribute) const {
+  return base::UTF8ToUTF16(GetOrComputeAttributeUTF8(attribute));
+}
 
 const std::string& AXComputedNodeData::GetOrComputeInnerTextUTF8() const {
   if (!inner_text_utf8_) {
--- ui/accessibility/ax_computed_node_data.h
+++ ui/accessibility/ax_computed_node_data.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/accessibility/ax_enums.mojom-forward.h"
 #include "ui/accessibility/ax_export.h"
 
 namespace ui {
@@ -25,6 +26,21 @@ class AX_EXPORT AXComputedNodeData final {
   virtual ~AXComputedNodeData();
   AXComputedNodeData(const AXComputedNodeData& other) = delete;
   AXComputedNodeData& operator=(const AXComputedNodeData& other) = delete;
+
+  // Given an accessibility string attribute, returns whether the attribute is
+  // currently present in the node's data, or if it can always be computed on
+  // demand.
+  bool HasOrCanComputeAttribute(
+      const ax::mojom::StringAttribute attribute) const;
+
+  // Given an accessibility string attribute, returns the attribute's value. The
+  // attribute is computed if not provided by the tree's source, otherwise it is
+  // simply returned from the node's data. String attributes are potentially the
+  // slowest to compute at the tree's source, e.g. in Blink.
+  const std::string& GetOrComputeAttributeUTF8(
+      const ax::mojom::StringAttribute attribute) const;
+  std::u16string GetOrComputeAttributeUTF16(
+      const ax::mojom::StringAttribute attribute) const;
 
   // Retrieves from the cache or computes the on-screen text that is found
   // inside the associated node and all its descendants, caches the result, and
