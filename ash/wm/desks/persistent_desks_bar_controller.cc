a4c4f7fe2fef136eaa32709af4526b85727b7b74
minch <minch@chromium.org>
2021-06-11 22:58:56 +0000

    bento_bar: No bento bar while in overview mode
    Bug: 1142045
    Test: PersistentDesksBarTest.LeavingOrEnteringTabletModeWithOverviewModeOn
    Change-Id: Ic7cc373e8c217bef4955f7e14df72f277d6abafb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957332
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891832}

--- ash/wm/desks/persistent_desks_bar_controller.cc
+++ ash/wm/desks/persistent_desks_bar_controller.cc
@@ -52,6 +52,7 @@ std::unique_ptr<views::Widget> CreatePersistentDesksBarWidget() {
 // Returns true if the persistent desks bar should be created and shown.
 bool ShouldPersistentDesksBarBeCreated() {
   return !TabletMode::Get()->InTabletMode() &&
+         !Shell::Get()->overview_controller()->InOverviewSession() &&
          DesksController::Get()->desks().size() > 1;
 }
 
@@ -94,8 +95,7 @@ void PersistentDesksBarController::OnOverviewModeEndingAnimationComplete(
 }
 
 void PersistentDesksBarController::OnDeskAdded(const Desk* desk) {
-  if (!Shell::Get()->overview_controller()->InOverviewSession())
-    MaybeInitBarWidget();
+  MaybeInitBarWidget();
 }
 
 void PersistentDesksBarController::OnDeskRemoved(const Desk* desk) {
