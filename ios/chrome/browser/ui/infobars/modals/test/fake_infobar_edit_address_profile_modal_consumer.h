ad3c76575c5c614cd2f7dc291ed389833d31f201
Vidhan Jain <vidhanj@google.com>
2021-06-11 16:36:28 +0000

    [iOS][Autofill] Add consumer tests for SaveAddressProfileInfobarModalOverlayMediatorTest
    Bug: 1167062
    Change-Id: I7e973f2432183dc0ec84e813dc51f9028a005313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951003
    Auto-Submit: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891678}

---
+++ ios/chrome/browser/ui/infobars/modals/test/fake_infobar_edit_address_profile_modal_consumer.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_TEST_FAKE_INFOBAR_EDIT_ADDRESS_PROFILE_MODAL_CONSUMER_H_
+#define IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_TEST_FAKE_INFOBAR_EDIT_ADDRESS_PROFILE_MODAL_CONSUMER_H_
+
+#import "ios/chrome/browser/ui/infobars/modals/infobar_edit_address_profile_modal_consumer.h"
+
+@interface FakeInfobarEditAddressProfileModalConsumer
+    : NSObject <InfobarEditAddressProfileModalConsumer>
+// Allow read access to values passed to InfobarEditAddressProfileModalConsumer
+// interface.
+@property(nonatomic, strong) NSMutableDictionary* profileData;
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_TEST_FAKE_INFOBAR_EDIT_ADDRESS_PROFILE_MODAL_CONSUMER_H_
---
+++ ios/chrome/browser/ui/infobars/modals/test/fake_infobar_edit_address_profile_modal_consumer.mm
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/infobars/modals/test/fake_infobar_edit_address_profile_modal_consumer.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation FakeInfobarEditAddressProfileModalConsumer
+- (void)setupModalViewControllerWithData:(NSDictionary*)data {
+  self.profileData = [NSMutableDictionary dictionaryWithDictionary:data];
+}
+
+- (void)setIsEditForUpdate:(BOOL)isEditForUpdate {
+}
+@end
