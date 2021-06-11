6da8812f7b7340652c493f3c3c894053318488f1
K. Moon <kmoon@chromium.org>
2021-06-11 22:00:57 +0000

    Reland "[unseasoned-pdf] Register PdfNavigationThrottle"
    This is a reland of 72175b332f446faf1d349ef431df7b012cfdf8db
    
    Original change broke android-archive-rel compilation. The missing
    dependency generated Mojom sources, which presumably weren't cleaned out
    for other builds after the //components/pdf dependencies went away.
    
    The updated change defines a new //components/pdf/browser:interceptors
    target, rather than trying to fix all //components/pdf/browser usage.
    Fixing //components/pdf/browser has been deferred to crbug.com/1218971.
    
    Original change's description:
    > [unseasoned-pdf] Register PdfNavigationThrottle
    >
    > Conditionally registers the PdfNavigationThrottle if the PdfUnseasoned
    > feature is enabled.
    >
    > Bug: 1123621
    > Change-Id: I6a48daa7948ec8bea519346db92a47db49227227
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953777
    > Reviewed-by: Hui Yingst <nigi@chromium.org>
    > Commit-Queue: K. Moon <kmoon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#891532}
    
    Bug: 1123621
    Change-Id: I79fbf79105ea7c3133a8d2b5b2d28e55cc0be349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956390
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891810}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -616,6 +616,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.h"
 #endif
 
+#if BUILDFLAG(ENABLE_PDF)
+#include "components/pdf/browser/pdf_navigation_throttle.h"
+#endif  // BUILDFLAG(ENABLE_PDF)
+
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
 #include "chrome/browser/supervised_user/supervised_user_google_auth_navigation_throttle.h"
 #endif
@@ -4100,6 +4104,10 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
 
   MaybeAddThrottle(PDFIFrameNavigationThrottle::MaybeCreateThrottleFor(handle),
                    &throttles);
+#if BUILDFLAG(ENABLE_PDF)
+  MaybeAddThrottle(pdf::PdfNavigationThrottle::MaybeCreateThrottleFor(handle),
+                   &throttles);
+#endif  // BUILDFLAG(ENABLE_PDF)
 
   MaybeAddThrottle(TabUnderNavigationThrottle::MaybeCreate(handle), &throttles);
 
