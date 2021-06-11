9db5e0089cc3046aaf3411e105612b992e5d8999
Sky Malice <skym@chromium.org>
2021-06-11 23:45:55 +0000

    Explicitly set textColorPrimary/Secondary in setTheme
    Calling #setTheme() seems to cause some Preference to incorrectly cache
    light/dark theme values after switching 2+ times. This is actually
    worked around by just explicitly setting the colors in the theme
    overlay being used in #setTheme().
    
    Note that the colors of the summary actually subtly change in light
    mode, verified with UX that it should be grey_700 is old modern world.
    Old: https://screenshot.googleplex.com/BHTaAKPMoUtibiB
    New: https://screenshot.googleplex.com/7WSV65VeAbMsdAx
    
    Bug: 1217038
    Change-Id: Ic8253fa795bfd2f84fc0c5823a6fe3c731ea135d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951143
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891842}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/InstantStartTest.java
@@ -154,7 +154,7 @@ public class InstantStartTest {
 
     @Rule
     public ChromeRenderTestRule mRenderTestRule =
-            ChromeRenderTestRule.Builder.withPublicCorpus().build();
+            ChromeRenderTestRule.Builder.withPublicCorpus().setRevision(1).build();
 
     @Rule
     public ErrorCollector collector = new ErrorCollector();
