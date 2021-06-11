0dc80153b77dd559241c7179fe126a3d7f64b916
Erik Chen <erikchen@chromium.org>
2021-06-11 19:12:39 +0000

    Lacros: Expose protected media identifier content setting.
    The protected media identifier content setting is one of several checks
    that is used to enable the ChallengePlatform API. This CL simply exposes
    it to Lacros. Future CLs will refactor the existing ChallengePlatform
    code to use this content setting.
    
    This CL changes Lacros to use the PLATFORM_CHROMEOS website setting
    registry instead of PLATFORM_LINUX. The only website setting this
    affects is the protected media identifier.
    
    Bug: 1121769
    Change-Id: I67209f2aef91e024f93d978085a55fcba524089d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951407
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891745}

--- chrome/browser/media/protected_media_identifier_permission_context.cc
+++ chrome/browser/media/protected_media_identifier_permission_context.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "net/base/url_util.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom.h"
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include <utility>
 
@@ -36,7 +37,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/permissions/permission_uma_util.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/user_prefs/user_prefs.h"
-#elif !defined(OS_ANDROID) && !defined(OS_WIN)
+#endif
+
+#if !(defined(OS_ANDROID) || defined(OS_WIN) || defined(OS_CHROMEOS))
 #error This file currently only supports Chrome OS, Android and Windows.
 #endif
 
