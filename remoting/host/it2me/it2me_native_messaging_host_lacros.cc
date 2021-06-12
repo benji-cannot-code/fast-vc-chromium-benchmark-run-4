58e953a651c669f6e1a7f5fb96de8e8d5f1e871f
Joe Downing <joedow@google.com>
2021-06-12 04:57:09 +0000

    Removing an unused CRD NMH create function param
    The CL which added the Lacros CRD NMH create function provided two task
    runners (UI and IO) which mimicked those used for the monolithic ChromeOS
    binary.  It turns out we only needed the UI task runner for Lacros so
    this CL removes the unused param.
    
    Bug: NONE
    Change-Id: Iac255a6fc5a4551cc6eeb5e051ac24bcbff5bf22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939568
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891884}

--- remoting/host/it2me/it2me_native_messaging_host_lacros.cc
+++ remoting/host/it2me/it2me_native_messaging_host_lacros.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <string>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/json/json_reader.h"
@@ -118,7 +119,7 @@ class It2MeNativeMessagingHostLacros : public extensions::NativeMessageHost,
 
 It2MeNativeMessagingHostLacros::It2MeNativeMessagingHostLacros(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner)
-    : task_runner_(task_runner) {}
+    : task_runner_(std::move(task_runner)) {}
 
 It2MeNativeMessagingHostLacros::~It2MeNativeMessagingHostLacros() = default;
 
@@ -421,10 +422,9 @@ void It2MeNativeMessagingHostLacros::SendErrorAndExit(
 
 std::unique_ptr<extensions::NativeMessageHost>
 CreateIt2MeNativeMessagingHostForLacros(
-    scoped_refptr<base::SingleThreadTaskRunner> io_runnner,
     scoped_refptr<base::SingleThreadTaskRunner> ui_runnner) {
-  // TODO(joedow): Verify |io_runner| is not required then remove it.
-  return std::make_unique<It2MeNativeMessagingHostLacros>(ui_runnner);
+  return std::make_unique<It2MeNativeMessagingHostLacros>(
+      std::move(ui_runnner));
 }
 
 }  // namespace remoting
