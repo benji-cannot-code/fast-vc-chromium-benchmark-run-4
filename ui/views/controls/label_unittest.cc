b388d64eba51aa04fc8f0da25f91a5b40d74e195
Peter Kasting <pkasting@chromium.org>
2021-06-14 20:28:59 +0000

    Various changes to prevent narrowing conversions: ui/
    Bug: 1216696
    Change-Id: I803d04479f5ce405b9f14fb277e9d8e91d04b501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2958314
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892249}

--- ui/views/controls/label_unittest.cc
+++ ui/views/controls/label_unittest.cc
@@ -189,7 +189,7 @@ class LabelSelectionTest : public LabelTest {
     label()->OnPaint(&canvas);
   }
 
-  gfx::Point GetCursorPoint(int index) {
+  gfx::Point GetCursorPoint(uint32_t index) {
     SimulatePaint();
     gfx::RenderText* render_text =
         label()->GetRenderTextForSelectionController();
