c46df2c19319cce580eeb4df7051bca5996b3d57
Josip Sokcevic <sokcevic@google.com>
2021-06-14 09:39:51 +0000

    [py3] Migrate ios/chrome/browser/web/resources PRESUBMIT
    Bug: 1212042
    Change-Id: I81b0940bed3b217a63c9992e36c595048eb49984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957341
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892009}

--- ios/chrome/browser/web/resources/PRESUBMIT.py
+++ ios/chrome/browser/web/resources/PRESUBMIT.py
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Makes sure that injected JavaScript is clang-format clean."""
 
+USE_PYTHON3 = True
+
+
 def CheckChangeOnUpload(input_api, output_api):
   """Special Top level function called by git_cl."""
   return input_api.canned_checks.CheckPatchFormatted(
