172f75345f5cb78017a0ae5381d12447606465ae
Josip Sokcevic <sokcevic@google.com>
2021-06-14 14:32:26 +0000

    [py3] Migrate net/tools/dafsa PRESUBMIT
    Bug: 1212049
    Change-Id: I0a8d3f4b7e58f6d872152174aa49221b3bf8a6c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957352
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Auto-Submit: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892068}

--- net/tools/dafsa/PRESUBMIT.py
+++ net/tools/dafsa/PRESUBMIT.py
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
 """Chromium presubmit script for src/net/tools/dafsa."""
+
+
+USE_PYTHON3 = True
 
 
 def _RunMakeDafsaTests(input_api, output_api):
