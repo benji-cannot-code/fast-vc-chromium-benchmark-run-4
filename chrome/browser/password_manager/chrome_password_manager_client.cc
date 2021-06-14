d09b94af539d1108f9707c814ff409b38a5b2e5b
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-06-14 12:37:26 +0000

    Brush up AccountStorageAuthHelper
    - Condition the class instantiation to ENABLE_DICE_SUPPORT, since the
    class doesn't do anything otherwise.
    - Inject minimal dependencies rather than pass the whole Profile, which
    makes testing easier.
    - Replace FindBrowserWithProfile() with FindBrowserWithWebContents()
    which seems more appropriate in this context.
    - Replace signin::ConsentLevel::kSync with kSignin in the unit test.
    
    Bug: None
    Change-Id: I248c7e142788b2cd80ccc75eeca7f4bda0d85d8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953565
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892046}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -147,6 +147,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 #include "chrome/browser/signin/dice_web_signin_interceptor_factory.h"
+#include "chrome/browser/ui/browser.h"
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
@@ -605,17 +606,17 @@ void ChromePasswordManagerClient::NotifyUserCredentialsWereLeaked(
 void ChromePasswordManagerClient::TriggerReauthForPrimaryAccount(
     signin_metrics::ReauthAccessPoint access_point,
     base::OnceCallback<void(ReauthSucceeded)> reauth_callback) {
-#if defined(OS_ANDROID)
-  std::move(reauth_callback).Run(ReauthSucceeded(false));
-#else   // !defined(OS_ANDROID)
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   account_storage_auth_helper_.TriggerOptInReauth(access_point,
                                                   std::move(reauth_callback));
-#endif  // defined(OS_ANDROID)
+#else
+  std::move(reauth_callback).Run(ReauthSucceeded(false));
+#endif
 }
 
 void ChromePasswordManagerClient::TriggerSignIn(
     signin_metrics::AccessPoint access_point) {
-#if !defined(OS_ANDROID)
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   account_storage_auth_helper_.TriggerSignIn(access_point);
 #endif
 }
@@ -1183,11 +1184,18 @@ ChromePasswordManagerClient::ChromePasswordManagerClient(
           this,
           base::BindRepeating(&GetSyncService, profile_),
           DiceWebSigninInterceptorFactory::GetForProfile(profile_)),
+      account_storage_auth_helper_(
+          IdentityManagerFactory::GetForProfile(profile_),
+          &password_feature_manager_,
+          base::BindRepeating(
+              [](content::WebContents* web_contents) {
+                Browser* browser =
+                    chrome::FindBrowserWithWebContents(web_contents);
+                return browser ? browser->signin_view_controller() : nullptr;
+              },
+              web_contents)),
 #else
       credentials_filter_(this, base::BindRepeating(&GetSyncService, profile_)),
-#endif
-#if !defined(OS_ANDROID)
-      account_storage_auth_helper_(profile_, &password_feature_manager_),
 #endif
       helper_(this) {
   ContentPasswordManagerDriverFactory::CreateForWebContents(web_contents, this,
