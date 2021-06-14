bf689af2ac48897ba5abaa74f53e00e7c8306273
Michael Ershov <miersh@google.com>
2021-06-14 18:14:52 +0000

    [Lacros] Update dependencies of KeystoreServiceFactoryAsh
    KeyPermissionsService[Factory] is used for KeystoreService::GetKeyTags,
    AddKeyTags,CanUserGrantPermissionForKey.
    
    Bug: 1152421
    Test: EnterprisePlatformKeys* browsertest, KeystoreService* unittest
    Change-Id: Ifbce5f111f5baa8cfac3a0ebd84f2f2ea3810238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2959911
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/master@{#892167}

--- chrome/browser/ash/crosapi/keystore_service_factory_ash.cc
+++ chrome/browser/ash/crosapi/keystore_service_factory_ash.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "chrome/browser/ash/crosapi/keystore_service_ash.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chrome/browser/chromeos/platform_keys/key_permissions/key_permissions_service_factory.h"
 #include "chrome/browser/chromeos/platform_keys/platform_keys_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -46,6 +47,8 @@ KeystoreServiceFactoryAsh::KeystoreServiceFactoryAsh()
           "KeystoreServiceFactoryAsh",
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(chromeos::platform_keys::PlatformKeysServiceFactory::GetInstance());
+  DependsOn(
+      chromeos::platform_keys::KeyPermissionsServiceFactory::GetInstance());
 }
 
 KeyedService* KeystoreServiceFactoryAsh::BuildServiceInstanceFor(
