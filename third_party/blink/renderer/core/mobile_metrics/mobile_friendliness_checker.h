230a78f1dd4aef29d5a95f334bfc3e30bb1cd499
Michael Lippautz <mlippautz@chromium.org>
2021-06-14 17:52:44 +0000

    Fix Oilpan library compilation
    Bug: chromium:1056170
    Change-Id: Ia8afd1570940756f55803e480f3a73fd6cccbdaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961308
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892152}

--- third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h
+++ third_party/blink/renderer/core/mobile_metrics/mobile_friendliness_checker.h
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/heap/visitor.h"
 
 namespace blink {
 
 class LocalFrameView;
 class LayoutObject;
-class Visitor;
 
 struct ViewportDescription;
 
