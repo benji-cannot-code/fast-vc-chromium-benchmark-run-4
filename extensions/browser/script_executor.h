d2e81999a2ccfca8d2119204ad93baf512875716
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-06-14 20:51:00 +0000

    [Extensions] Add mojom types for CSSInjection and JSInjection
    Add separate types for CSSInjection and JSInjection and use them in
    ExecuteCodeParams (the message details sent to execute code in the
    renderer on behalf of an extension, triggered by executeScript(),
    insertCSS(), and removeCSS()).
    
    Initially, these types hold very little - only the code and the
    script url for JS or the injection key for CSS. However, many of the
    members in ExecuteCodeParams (and passed to
    ScriptExecutor::ExecuteScript()) are unique to either CSS or JS
    injection, and will be moved into these specific structs in
    followups.
    
    This should have no behavior change.
    
    Bug: 1218648
    Change-Id: Ie173b8fb5542e14e374a64b29d8ec457dacc1bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956394
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892261}

--- extensions/browser/script_executor.h
+++ extensions/browser/script_executor.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/mojom/action_type.mojom-shared.h"
+#include "extensions/common/mojom/code_injection.mojom.h"
 #include "extensions/common/mojom/css_origin.mojom-shared.h"
 #include "extensions/common/mojom/host_id.mojom-forward.h"
 #include "extensions/common/mojom/run_location.mojom-shared.h"
@@ -98,6 +99,15 @@ class ScriptExecutor {
   using ScriptFinishedCallback =
       base::OnceCallback<void(std::vector<FrameResult> frame_results)>;
 
+  // Generates an injection key based on the host ID and either the file URL, if
+  // available, or the code string. The format of the key is
+  // "<type><host_id><digest>", where <type> is one of "F" (file) and "C"
+  // (code), <host_id> is the host ID, and <digest> is an unspecified hash
+  // digest of the file URL or the code string, respectively.
+  static std::string GenerateInjectionKey(const mojom::HostID& host_id,
+                                          const GURL& script_url,
+                                          const std::string& code);
+
   // Executes a script. The arguments match mojom::ExecuteCodeParams in
   // frame.mojom (request_id is populated automatically).
   //
@@ -110,15 +120,14 @@ class ScriptExecutor {
   // before a response is received (in this case the callback will be with a
   // failure and appropriate error message).
   void ExecuteScript(const mojom::HostID& host_id,
+                     mojom::CodeInjectionPtr injection,
                      mojom::ActionType action_type,
-                     const std::string& code,
                      FrameScope frame_scope,
                      const std::set<int>& frame_ids,
                      MatchAboutBlank match_about_blank,
                      mojom::RunLocation run_at,
                      ProcessType process_type,
                      const GURL& webview_src,
-                     const GURL& script_url,
                      bool user_gesture,
                      mojom::CSSOrigin css_origin,
                      ResultType result_type,
