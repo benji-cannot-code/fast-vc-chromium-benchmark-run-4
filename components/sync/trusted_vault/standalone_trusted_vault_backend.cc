558551c23f361565eeac1ec356ea4e607327b030
Scott Violet <sky@chromium.org>
2021-06-14 21:28:57 +0000

    Revert "Reland "[sync] Implement basic trusted vault recoverability reading""
    This reverts commit b59c399f14a25d3ceeba9b2f9eb4a98a80afb992.
    
    Reason for revert: This seems to have resulted in flake in two SingleClientNigoriWithRecoveryAndPasswordsAccountStorageTest tests: ShouldReportDegradedTrustedVaultRecoverability and ShouldAcceptEncryptionKeysFromTheWeb . See https://bugs.chromium.org/p/chromium/issues/detail?id=1218713 . 
    
    Original change's description:
    > Reland "[sync] Implement basic trusted vault recoverability reading"
    >
    > This is a reland of 125049d1c2da5e2b3ee8fd1b8d9270203d5a7b3f
    >
    > Reason to reland: the underlying race condition is believed to be
    > understood and fixed by using a separate
    > TrustedVaultConnection::Request, in case there are concurrent
    > requests to the server (add method + get recoverability).
    >
    > Original change's description:
    > > [sync] Implement basic trusted vault recoverability reading
    > >
    > > This patch introduces minimalistic logic to support
    > > TrustedVaultClient::GetIsRecoverabilityDegraded() for
    > > StandaloneTrustedVaultClient.
    > >
    > > With this change, more relevant code is exercised in integration tests.
    > > including interactions with the fake server. TODOs remain to actually
    > > introduce more sophisticated logic like periodic polling.
    > >
    > > In tests, two fixtures are merged into one, such that
    > > SingleClientNigoriSyncTestWithSecurityDomainsServer no longer exists
    > > after this patch.
    > >
    > > Change-Id: If6e2a1f1e2eac2e043ba4fa5e2be0131a140954e
    > > Bug: 1201659
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2948927
    > > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > > Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#891329}
    >
    > Bug: 1201659
    > Change-Id: I0516299ac8cff66bcecece6bc8ac9d628ca72e38
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2955948
    > Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    > Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#891681}
    
    Bug: 1201659
    Change-Id: I70eeab42c67993dc41f781bad9944d27ebfc2c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960666
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Owners-Override: Scott Violet <sky@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892276}

--- components/sync/trusted_vault/standalone_trusted_vault_backend.cc
+++ components/sync/trusted_vault/standalone_trusted_vault_backend.cc
@@ -63,12 +63,6 @@ absl::optional<TrustedVaultKeyAndVersion> GetLastTrustedVaultKeyAndVersion(
   return absl::nullopt;
 }
 
-void RetrieveIsRecoverabilityDegradedCompleted(
-    base::OnceCallback<void(bool)> cb,
-    TrustedVaultRecoverabilityStatus status) {
-  std::move(cb).Run(status == TrustedVaultRecoverabilityStatus::kDegraded);
-}
-
 }  // namespace
 
 StandaloneTrustedVaultBackend::PendingTrustedRecoveryMethod::
@@ -190,8 +184,7 @@ void StandaloneTrustedVaultBackend::RemoveAllStoredKeys() {
   base::DeleteFile(file_path_);
   data_.Clear();
   AbandonConnectionRequest();
-  ongoing_get_recoverability_request_.reset();
-  ongoing_add_recovery_method_request_.reset();
+  ongoing_degraded_recoverability_request_.reset();
 }
 
 void StandaloneTrustedVaultBackend::SetPrimaryAccount(
@@ -201,8 +194,7 @@ void StandaloneTrustedVaultBackend::SetPrimaryAccount(
   }
   primary_account_ = primary_account;
   AbandonConnectionRequest();
-  ongoing_get_recoverability_request_.reset();
-  ongoing_add_recovery_method_request_.reset();
+  ongoing_degraded_recoverability_request_.reset();
   if (!primary_account_.has_value()) {
     DCHECK(!pending_trusted_recovery_method_.has_value());
     return;
@@ -245,13 +237,9 @@ bool StandaloneTrustedVaultBackend::MarkKeysAsStale(
 void StandaloneTrustedVaultBackend::GetIsRecoverabilityDegraded(
     const CoreAccountInfo& account_info,
     base::OnceCallback<void(bool)> cb) {
-  // TODO(crbug.com/1201659): Improve this logic properly and add test coverage,
-  // including throttling and periodic polling.
-  ongoing_get_recoverability_request_ =
-      connection_->RetrieveIsRecoverabilityDegraded(
-          account_info,
-          base::BindOnce(&RetrieveIsRecoverabilityDegradedCompleted,
-                         std::move(cb)));
+  // TODO(crbug.com/1201659): Implement logic.
+  NOTIMPLEMENTED();
+  std::move(cb).Run(is_recoverability_degraded_for_testing_);
 }
 
 void StandaloneTrustedVaultBackend::AddTrustedRecoveryMethod(
@@ -305,9 +293,9 @@ void StandaloneTrustedVaultBackend::AddTrustedRecoveryMethod(
   }
 
   // |this| outlives |connection_| and
-  // |ongoing_add_recovery_method_request_|, so it's safe to use
+  // |ongoing_degraded_recoverability_request_|, so it's safe to use
   // base::Unretained() here.
-  ongoing_add_recovery_method_request_ =
+  ongoing_degraded_recoverability_request_ =
       connection_->RegisterAuthenticationFactor(
           *primary_account_, last_trusted_vault_key_and_version,
           SecureBoxKeyPair::GenerateRandom()->public_key(),
@@ -330,6 +318,11 @@ StandaloneTrustedVaultBackend::GetDeviceRegistrationInfoForTesting(
     return sync_pb::LocalDeviceRegistrationInfo();
   }
   return per_user_vault->local_device_registration_info();
+}
+
+void StandaloneTrustedVaultBackend::SetRecoverabilityDegradedForTesting() {
+  is_recoverability_degraded_for_testing_ = true;
+  delegate_->NotifyRecoverabilityDegradedChanged();
 }
 
 std::vector<uint8_t>
@@ -500,8 +493,9 @@ void StandaloneTrustedVaultBackend::OnKeysDownloaded(
 void StandaloneTrustedVaultBackend::OnTrustedRecoveryMethodAdded(
     base::OnceClosure cb,
     TrustedVaultRegistrationStatus status) {
-  DCHECK(ongoing_add_recovery_method_request_);
-  ongoing_add_recovery_method_request_ = nullptr;
+  DCHECK(ongoing_degraded_recoverability_request_);
+  ongoing_degraded_recoverability_request_ = nullptr;
+  is_recoverability_degraded_for_testing_ = false;
 
   std::move(cb).Run();
   delegate_->NotifyRecoverabilityDegradedChanged();
