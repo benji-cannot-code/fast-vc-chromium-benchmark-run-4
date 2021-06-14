07b83d7f81490b375bef4b4e7abb250fb307b386
Jinsuk Kim <jinsukkim@chromium.org>
2021-06-14 05:07:10 +0000

    Android: Delete DownloadUtils/shadow from LocationBarMediatorTest
    The dependency on DownloadUtils was replaced with an interface
    SaveOfflineButtonStatus in crrev.com/c/2823039. Deletes it from its
    unit test as well.
    
    Bug: 1171512
    Change-Id: I8061fccbb16a1ba6f7485744f03163ffd8f7207f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947153
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#891972}

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
@@ -59,7 +59,6 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.build.BuildConfig;
-import org.chromium.chrome.browser.download.DownloadUtils;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.gsa.GSAState;
 import org.chromium.chrome.browser.lens.LensController;
@@ -70,7 +69,7 @@ import org.chromium.chrome.browser.omnibox.status.StatusCoordinator;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinator;
 import org.chromium.chrome.browser.omnibox.voice.AssistantVoiceSearchService;
 import org.chromium.chrome.browser.omnibox.voice.VoiceRecognitionHandler;
-import org.chromium.chrome.browser.privacy.settings.PrivacyPreferencesManagerImpl;
+import org.chromium.chrome.browser.privacy.settings.PrivacyPreferencesManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileJni;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
@@ -98,8 +97,7 @@ import java.util.List;
 @Config(shadows = {LocationBarMediatorTest.ShadowUrlUtilities.class,
                 LocationBarMediatorTest.ShadowGeolocationHeader.class,
                 LocationBarMediatorTest.ObjectAnimatorShadow.class,
-                LocationBarMediatorTest.GSAStateShadow.class,
-                LocationBarMediatorTest.DownloadUtilsShadow.class})
+                LocationBarMediatorTest.GSAStateShadow.class})
 @Features.EnableFeatures(ChromeFeatureList.OMNIBOX_ASSISTANT_VOICE_SEARCH)
 @Features.DisableFeatures(ChromeFeatureList.VOICE_BUTTON_IN_TOP_TOOLBAR)
 public class LocationBarMediatorTest {
@@ -150,14 +148,6 @@ public class LocationBarMediatorTest {
         }
     }
 
-    @Implements(DownloadUtils.class)
-    static class DownloadUtilsShadow {
-        @Implementation
-        public static boolean isAllowedToDownloadPage(Tab tab) {
-            return true;
-        }
-    }
-
     private static final String TEST_URL = "http://www.example.org";
 
     private static int sGeoHeaderPrimeCount;
@@ -194,7 +184,7 @@ public class LocationBarMediatorTest {
     @Mock
     private StatusCoordinator mStatusCoordinator;
     @Mock
-    private PrivacyPreferencesManagerImpl mPrivacyPreferencesManager;
+    private PrivacyPreferencesManager mPrivacyPreferencesManager;
     @Mock
     private OmniboxPrerender.Natives mPrerenderJni;
     @Mock
