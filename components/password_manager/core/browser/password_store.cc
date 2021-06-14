7bd10a9b4eb24f983de11ac28da7a7ea938cfb81
Viktor Semeniuk <vsemeniuk@google.com>
2021-06-14 17:14:50 +0000

    Fixes flakiness caused by https://crrev.com/c/2909862
    This CL fixes flakiness in
    ProfileBrowserTestWithoutDestroyProfile.DestroyRegularProfileBeforeOTRs.
    Before changing the pref value we should also make sure prefs_ exist
    and ShutdownOnUIThread wasn't called.
    
    Bug: 1217988
    Change-Id: I5f9ccc7c28710f174d03aeef78aad33954dbb950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953346
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892131}

--- components/password_manager/core/browser/password_store.cc
+++ components/password_manager/core/browser/password_store.cc
@@ -1329,7 +1329,7 @@ bool PasswordStore::RemoveOldGoogleLogins() {
 }
 
 void PasswordStore::MarkOldGoogleLoginsRemoved(bool success) {
-  if (success) {
+  if (success && prefs_ && !shutdown_called_) {
     prefs_->SetBoolean(prefs::kWereOldGoogleLoginsRemoved, true);
   }
 }
