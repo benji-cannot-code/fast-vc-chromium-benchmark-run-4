b97c8e58bc06b5021e0b172392ed49a75762968a
Anupam Snigdha <snianu@microsoft.com>
2021-06-14 18:07:29 +0000

    [Clipboard API] Clipboard Custom Formats implementation Part 1.
    This patch contains the runtime flag implementation of custom
    clipboard formats in async clipboard APIs and also adds a member to
    ClipboardItemOptions to include custom formats in the async clipboard
    api read/write. Subsequent CLs will have the reader/writer
    implementation and tests.
    
    Prototype: crrev.com/c/2921023.
    i2p: https://groups.google.com/a/chromium.org/g/blink-dev/c/Lo7WBM_v_LY/m/LncCKkXeAwAJ
    
    Bug: 106449
    
    Change-Id: I43dc7668e861a522488e5e10a1e9c1b203dc18bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941615
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892159}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -248,6 +248,9 @@ extern const char kContextMenuSearchAndShopWithGoogleLensDescription[];
 extern const char kContextMenuTranslateWithGoogleLensName[];
 extern const char kContextMenuTranslateWithGoogleLensDescription[];
 
+extern const char kClipboardCustomFormatsName[];
+extern const char kClipboardCustomFormatsDescription[];
+
 extern const char kClickToOpenPDFName[];
 extern const char kClickToOpenPDFDescription[];
 
