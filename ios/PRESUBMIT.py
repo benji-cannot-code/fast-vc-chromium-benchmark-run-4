e5f7a29705e778d1c0c92e69d69895f0bb650aef
Josip Sokcevic <sokcevic@google.com>
2021-06-14 09:37:31 +0000

    [py3] Migrate ios PRESUBMIT
    Bug: 1212040
    Change-Id: Ia7600f8e3da825591d7c4a277e583d9dc6a89026
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957342
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892008}

--- ios/PRESUBMIT.py
+++ ios/PRESUBMIT.py
@@ -11,6 +11,8 @@ for more details about the presubmit API built into depot_tools.
 
 import os
 
+USE_PYTHON3 = True
+
 NULLABILITY_PATTERN = r'(nonnull|nullable|_Nullable|_Nonnull)'
 TODO_PATTERN = r'TO[D]O\(([^\)]*)\)'
 CRBUG_PATTERN = r'crbug\.com/\d+$'
