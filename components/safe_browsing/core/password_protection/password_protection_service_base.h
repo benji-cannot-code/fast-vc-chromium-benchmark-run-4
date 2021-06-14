b534a465f5b21830cff39a7fe51fe6c55e7256a1
Colin Blundell <blundell@chromium.org>
2021-06-14 15:02:38 +0000

    [SafeBrowsing] Remove thread_utils.h from PasswordProtectionServiceBase
    thread_utils.h is a utility that allows //components/safe_browsing/core
    to use //content's and //ios/web's threading primitives via different
    implementations that are conditionally included in the GN target. We
    are going to eliminate this utility as part of eliminating
    //components/safe_browsing/core's incorrect dependences on
    //components/safe_browsing/{content, ios}.
    
    This CL removes usage of thread_utils.h in
    //components/safe_browsing/core's PasswordProtectionServiceBase. This
    class executes entirely on the UI thread, so removing these references
    consists of:
    
    - Changing DCHECKs about currently being on the UI thread with usage
      of a ThreadChecker
    - Changing the posting of a task to the UI thread (which is the current
      thread) with posting the task to the current thread
    
    Bug: 1216341
    Change-Id: I869ba97ab641c06c41cbc8608ef2cbbc452a5909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953923
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892074}

--- components/safe_browsing/core/password_protection/password_protection_service_base.h
+++ components/safe_browsing/core/password_protection/password_protection_service_base.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/task/cancelable_task_tracker.h"
+#include "base/threading/thread_checker.h"
 #include "build/build_config.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_service_observer.h"
@@ -422,6 +423,8 @@ class PasswordProtectionServiceBase : public history::HistoryServiceObserver {
   // Get the content area size of current browsing window.
   virtual gfx::Size GetCurrentContentAreaSize() const = 0;
 #endif
+
+  THREAD_CHECKER(thread_checker_);
 
   std::vector<std::string> saved_passwords_matching_domains_;
 
