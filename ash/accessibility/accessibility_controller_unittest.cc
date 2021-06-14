348f9414341a52c84da817dca0da10ceedc1336f
Katie Dektar <katie@chromium.org>
2021-06-14 17:50:39 +0000

    Reland "Windows behind the lock screen are not accessible."
    This reverts commit b6cb0d71d49d09d8f7ee5f527214b3d4cac048b7
    after fixing msan tests.
    
    Original description:
    > Windows behind the lock screen are not accessible.
    >
    > This fixes a privacy concern where ChromeVox and Switch Access
    > could access windows that were visibly hidden behind the lock
    > screen.
    >
    > When the user session is blocked, the AccessibilityController marks
    > the NonLockScreenContainersContainer window as forced invsible,
    > pruning all of its children. This keeps accessibility features like
    > ChromeVox and SwitchAccess from accessing parts of the tree hidden
    > behind the lock screen. When the user session is unblocked, the
    > children are re-added and accessible again.
    >
    
    Bug: 940278
    Change-Id: I8ec7af214d9f28f2bb6c9bd81c903f23dd82b7c0
    AX-Relnotes: ChromeVox, Switch Access cannot access content behind lock screen.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960652
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892151}

--- ash/accessibility/accessibility_controller_unittest.cc
+++ ash/accessibility/accessibility_controller_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/power/fake_power_manager_client.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/testing_pref_service.h"
+#include "ui/accessibility/aura/aura_window_properties.h"
 #include "ui/message_center/message_center.h"
 
 using message_center::MessageCenter;
@@ -1201,6 +1202,33 @@ TEST_P(AccessibilityControllerSigninTest, SwitchAccessPrefsSyncToSignIn) {
   // has no effect on the user profile.
   signin_prefs->Set(kAccessibilitySwitchAccessEnabled, base::Value(false));
   EXPECT_TRUE(user_prefs->GetBoolean(kAccessibilitySwitchAccessEnabled));
+}
+
+TEST_P(AccessibilityControllerSigninTest,
+       UpdatesNonLoginWindowVisibilityOnLogin) {
+  aura::Window* container =
+      Shell::GetContainer(Shell::GetPrimaryRootWindow(),
+                          kShellWindowId_NonLockScreenContainersContainer);
+
+  BlockUserSession(BLOCKED_BY_LOCK_SCREEN);
+  EXPECT_TRUE(
+      container->GetProperty(ui::kAXConsiderInvisibleAndIgnoreChildren));
+
+  UnblockUserSession();
+  EXPECT_FALSE(
+      container->GetProperty(ui::kAXConsiderInvisibleAndIgnoreChildren));
+
+  BlockUserSession(BLOCKED_BY_LOGIN_SCREEN);
+  EXPECT_TRUE(
+      container->GetProperty(ui::kAXConsiderInvisibleAndIgnoreChildren));
+
+  UnblockUserSession();
+  EXPECT_FALSE(
+      container->GetProperty(ui::kAXConsiderInvisibleAndIgnoreChildren));
+
+  BlockUserSession(BLOCKED_BY_USER_ADDING_SCREEN);
+  EXPECT_TRUE(
+      container->GetProperty(ui::kAXConsiderInvisibleAndIgnoreChildren));
 }
 
 }  // namespace ash
