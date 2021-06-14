6cddc43e10387cb6ea1e2e2cb12345a8d70c0b87
ttefera <ttefera@google.com>
2021-06-14 19:06:22 +0000

    ARC Light Mode by default.
    ash::ColorProvider returns dark mode when the flag is default. So
    instead by checking to see if the flag is enabled we are able to
    return light mode when it is default.
    
    system UI" flag on and off while toggling the Dark Theme tile.
    
    Bug: b:185137820
    Test: Tested manually on DUT, by toggling the "Dark/light mode of
    Change-Id: Id1466367b2899cf11b74a7657f2e2a04c6c3c9f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909843
    Reviewed-by: Sara Kato <sarakato@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Auto-Submit: Theodore Tefera <ttefera@google.com>
    Commit-Queue: Theodore Tefera <ttefera@google.com>
    Cr-Commit-Position: refs/heads/master@{#892206}

--- components/arc/dark_theme/arc_dark_theme_bridge.cc
+++ components/arc/dark_theme/arc_dark_theme_bridge.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/arc/dark_theme/arc_dark_theme_bridge.h"
 
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/style/color_provider.h"
 #include "base/bind.h"
 #include "base/logging.h"
@@ -71,7 +72,9 @@ ArcDarkThemeBridge::~ArcDarkThemeBridge() {
 void ArcDarkThemeBridge::OnConnectionReady() {
   auto* provider = ash::ColorProvider::Get();
   bool dark_theme_status = false;
-  if (provider)
+  // Checking to see if the flag is enabled because provider returns dark mode
+  // when the flag is default.
+  if (provider && ash::features::IsDarkLightModeEnabled())
     dark_theme_status = provider->IsDarkModeEnabled();
   if (!ArcDarkThemeBridge::SendDeviceDarkThemeState(dark_theme_status)) {
     LOG(ERROR) << "OnConnectionReady failed to get Dark Theme instance for "
