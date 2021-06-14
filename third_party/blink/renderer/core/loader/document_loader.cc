a5528a1ae5881f8c6707933bbb6d99476d8d0525
Nate Chapin <japhet@chromium.org>
2021-06-14 18:05:36 +0000

    Don't crash when using respondWith() in appHistory.onnavigate for same-document back/forward
    https://chromium-review.googlesource.com/c/chromium/src/+/2852456
    implemented onnavigate behavior appHistory.goTo(), and mostly made it
    work for other same-document back/forward navigations, too. However, it
    didn't plumb all of the necessary state for those other cases, so if
    respondWith() is invoked in that case, we crash. Fix that crash, and
    actually test non-goTo same-document back/forward.
    
    Bug: 1186299
    Change-Id: Iacb6cf2f9610f0e49abd0cf9eeb0c5776c2310c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957358
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892158}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -1285,9 +1285,9 @@ mojom::CommitResult DocumentLoader::CommitSameDocumentNavigation(
                mojom::blink::TriggeringEventInfo::kFromTrustedEvent) {
       involvement = UserNavigationInvolvement::kActivation;
     }
-    if (!app_history->DispatchNavigateEvent(url, nullptr,
-                                            NavigateEventType::kFragment,
-                                            frame_load_type, involvement)) {
+    if (!app_history->DispatchNavigateEvent(
+            url, nullptr, NavigateEventType::kFragment, frame_load_type,
+            involvement, nullptr, history_item)) {
       return mojom::blink::CommitResult::Aborted;
     }
   }
