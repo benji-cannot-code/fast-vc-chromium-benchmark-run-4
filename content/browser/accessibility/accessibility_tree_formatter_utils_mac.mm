975f277c42fc6bb79d497c83889f0a8658cb71c0
Alexander Surkov <asurkov@igalia.com>
2021-06-14 23:13:53 +0000

    ax_inspect mac scripting: support AXTextMarkerRange.anchor/focus properties
    It allows to retrieve anchor/focus text markers from a text range, for
    example: p.AXTextMarkerRangeForUIElement(p).anchor
    
    Bug: 1136957
    Change-Id: I22a4ed68cdfa91c8dbc0a57e3c55f673e74992ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2954240
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#892328}

--- content/browser/accessibility/accessibility_tree_formatter_utils_mac.mm
+++ content/browser/accessibility/accessibility_tree_formatter_utils_mac.mm
@@ -11,6 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using ui::AXPropertyNode;
 
+extern "C" {
+
+CFTypeRef AXTextMarkerRangeCopyStartMarker(CFTypeRef);
+
+CFTypeRef AXTextMarkerRangeCopyEndMarker(CFTypeRef);
+}  // extern "C"
+
 namespace content {
 namespace a11y {
 
@@ -99,7 +106,7 @@ OptionalNSObject AttributeInvoker::Invoke(
   while (current_node) {
     auto target_optional = InvokeFor(target, *current_node);
     if (!target_optional.IsNotNil()) {
-      LOG(ERROR) << "No target for " << current_node->ToString();
+      LOG(ERROR) << "Null result of " << current_node->ToString();
       return target_optional;
     }
     target = *target_optional;
@@ -114,6 +121,10 @@ OptionalNSObject AttributeInvoker::InvokeFor(
     const AXPropertyNode& property_node) const {
   if (IsBrowserAccessibilityCocoa(target) || IsAXUIElement(target))
     return InvokeForAXElement(target, property_node);
+
+  if (content::IsAXTextMarkerRange(target)) {
+    return InvokeForAXTextMarkerRange(target, property_node);
+  }
 
   if ([target isKindOfClass:[NSArray class]])
     return InvokeForArray(target, property_node);
@@ -147,6 +158,20 @@ OptionalNSObject AttributeInvoker::InvokeForAXElement(
 
   // Unmatched attribute.
   return OptionalNSObject::NotApplicable();
+}
+
+OptionalNSObject AttributeInvoker::InvokeForAXTextMarkerRange(
+    const id target,
+    const AXPropertyNode& property_node) const {
+  if (property_node.name_or_value == "anchor")
+    return OptionalNSObject(static_cast<id>(
+        AXTextMarkerRangeCopyStartMarker(static_cast<CFTypeRef>(target))));
+
+  if (property_node.name_or_value == "focus")
+    return OptionalNSObject(static_cast<id>(
+        AXTextMarkerRangeCopyEndMarker(static_cast<CFTypeRef>(target))));
+
+  return OptionalNSObject::Error();
 }
 
 OptionalNSObject AttributeInvoker::InvokeForArray(
