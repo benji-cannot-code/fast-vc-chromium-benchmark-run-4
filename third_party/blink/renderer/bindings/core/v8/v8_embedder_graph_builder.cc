e4568d3d93c8e6e139003c1374a8859c0a66c0bd
Michael Lippautz <mlippautz@chromium.org>
2021-06-14 19:01:56 +0000

    ActiveScriptWrappables: Remove fake root entry in heap snapshot
    ASW are retained by an object named "Pending activities" that itself is
    rooted via regular Persistent reference. This removes the special
    case of a root category for ASW.
    
    The CL unblock the Oilpan library which does not have knowledge of such
    a root.
    
    Dependencies:
    - https://crrev.com/c/2954763
    - https://crrev.com/c/2956328
    
    Bug: chromium:1056170
    Change-Id: Ia3479a0b5e4cf1d974b41c719d5a97683df8c2fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960454
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892198}

--- third_party/blink/renderer/bindings/core/v8/v8_embedder_graph_builder.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_embedder_graph_builder.cc
@@ -363,7 +363,6 @@ class GC_PLUGIN_IGNORE(
   void AddEphemeronEdgeName(Traceable backing, State* parent, State* current);
 
   void VisitPersistentHandleInternal(v8::Local<v8::Object>, uint16_t);
-  void VisitPendingActivities();
   void VisitBlinkRoots();
   void VisitTransitiveClosure();
 
@@ -423,7 +422,6 @@ void V8EmbedderGraphBuilder::BuildEmbedderGraph() {
       ThreadState::Current()->unified_heap_controller());
   tracer->IterateTracedGlobalHandles(this);
   VisitBlinkRoots();
-  VisitPendingActivities();
   VisitTransitiveClosure();
   DCHECK(worklist_.empty());
   // ephemeron_worklist_ might not be empty. We might have an ephemeron whose
@@ -623,20 +621,6 @@ void V8EmbedderGraphBuilder::VisitEphemeron(
   ephemeron_worklist_.push_back(std::make_unique<EphemeronItem>(
       current_parent_, key, value_trace_descriptor.base_object_payload,
       value_trace_descriptor.callback));
-}
-
-void V8EmbedderGraphBuilder::VisitPendingActivities() {
-  // Ownership of the new node is transferred to the graph_.
-  EmbedderNode* root =
-      static_cast<EmbedderNode*>(graph_->AddNode(std::unique_ptr<Graph::Node>(
-          new EmbedderRootNode("Pending activities"))));
-  EnsureRootState(root);
-  ParentScope parent(this, root);
-  auto* asw_manager =
-      V8PerIsolateData::From(isolate_)->GetActiveScriptWrappableManager();
-  if (asw_manager) {
-    asw_manager->IterateActiveScriptWrappables(this);
-  }
 }
 
 void V8EmbedderGraphBuilder::VisitBlinkRoots() {
