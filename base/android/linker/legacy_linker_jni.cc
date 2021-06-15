1bf589020fe567d3e988f5d6e94beda21208fea0
Egor Pasko <pasko@chromium.org>
2021-06-15 16:17:29 +0000

    base/android/linker: Replace memfd with ASharedMemory
    Currently ASharedMemory is backed by ashmem on all Android releases.
    There is a plan to reimplement it on top of memfd in the future.
    
    Using memfd explicitly in Chrome was introduced to apply an R+
    workaround allowing to open a file in the App Zygote Preload.
    Unfortunately this workaround is blocked by SELinux policies.
    
    There is an Android change (currently in review) to allow ashmem FDs
    created by Preload to stay open across fork(2). It will go live not
    earlier than in S.
    
    Here, remove explicit use of memfd_create(2) and replace it with
    ASharedMemory.
    
    With this change The current usage of RELRO regions should be
    unaffected: RELRO sharing mechanics in linkers is used only in
    privileged+browser processes. It will start making a difference when the
    app zygote starts creating RELRO regions.
    
    Fly-by cleanups:
    
    1. In *linker_jni.cc refer to the main comment instead of repeating.
    
    2. Make the debug printing consistently use Log.i guarded by DEBUG
       instead of Log.d - these uses were introduced accidentally.
    
    Bug: 1154224
    Change-Id: I09effaa27c7f2f0c28f044783e8cad1ebc5c5706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960932
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892603}

--- base/android/linker/legacy_linker_jni.cc
+++ base/android/linker/legacy_linker_jni.cc
@@ -3,11 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This is the version of the Android-specific Chromium linker that uses
-// the crazy linker to load libraries.
-
-// This source code *cannot* depend on anything from base/ or the C++
-// STL, to keep the final library small, and avoid ugly dependency issues.
+// This is a part of the Android-specific Chromium dynamic linker.
+//
+// See linker_jni.h for more details and the dependency rules.
 
 #include "base/android/linker/legacy_linker_jni.h"
 
