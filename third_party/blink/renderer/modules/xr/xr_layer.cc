0c5da4f971272dae13ac61e0fb6976ac2733098a
Brandon Jones <bajones@chromium.org>
2021-06-15 18:25:57 +0000

    Ensure that XRLayer includes base EventTarget in Trace
    Trace was skipping a level in the class hierarchy and calling
    ScriptWrappable::Trace() instead. This was likely the result of the
    class inheritance changing in the spec a while back and getting updated
    elsewhere but not here, since it didn't raise any warnings.
    
    Bug: 1219857
    Change-Id: I4ac9f7b037ac5e5dd0e6d670f1d5a30e6344862f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964533
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892650}

--- third_party/blink/renderer/modules/xr/xr_layer.cc
+++ third_party/blink/renderer/modules/xr/xr_layer.cc
@@ -22,7 +22,7 @@ const AtomicString& XRLayer::InterfaceName() const {
 
 void XRLayer::Trace(Visitor* visitor) const {
   visitor->Trace(session_);
-  ScriptWrappable::Trace(visitor);
+  EventTargetWithInlineData::Trace(visitor);
 }
 
 }  // namespace blink
