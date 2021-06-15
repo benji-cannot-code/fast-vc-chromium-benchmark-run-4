1f2368bc1b38d1777da8c94f013194b44c4e80c1
Lei Zhang <thestig@chromium.org>
2021-06-15 23:54:43 +0000

    Revert "[chromecast] Implement ApiBindings::Connect"
    This reverts commit 52d5ca75fb3bbd101d08bdea34ecf9dc4b2698e7.
    
    Reason for revert: Broke the build.
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-cast/121929/overview
    
    Original change's description:
    > [chromecast] Implement ApiBindings::Connect
    >
    > - BindingsManagerCast implements ApiBindings::Connect.
    > - NamedMessagePortConnector will live in browser component
    >   (CastWebContents).
    > - Dependency on CastWebContents has been removed from
    >   BindingsManagerCast class.
    >
    > Merge-With: eureka-internal/603702
    >
    > Test: BindingsManagerCast, CastWebContentsBrowsertest
    > Bug: Internal b/183378843
    > Change-Id: I908611d06cfc085e36ba9ab53e1e1067c2893668
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2835080
    > Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Sean Topping <seantopping@chromium.org>
    > Commit-Queue: Jiawei Li <lijiawei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#892782}
    
    Bug: Internal b/183378843
    Change-Id: Ia53c132fdc9c22e81c26159ced990975ee8719d4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965600
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892790}

--- chromecast/bindings/named_message_port_connector_cast.cc
+++ chromecast/bindings/named_message_port_connector_cast.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chromecast/browser/named_message_port_connector_cast.h"
+#include "chromecast/bindings/named_message_port_connector_cast.h"
 
 #include <string>
 #include <utility>
@@ -11,23 +11,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chromecast/browser/cast_web_contents.h"
+#include "components/cast/api_bindings/manager.h"
 #include "components/cast/message_port/message_port_cast.h"
 #include "components/cast/named_message_port_connector/grit/named_message_port_connector_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 
 namespace chromecast {
+namespace bindings {
 namespace {
 
-constexpr uint64_t kNamedMessagePortConnectorBindingsId = 1000;
+const char kNamedMessagePortConnectorBindingsId[] =
+    "NAMED_MESSAGE_PORT_CONNECTOR";
 
 }  // namespace
 
 NamedMessagePortConnectorCast::NamedMessagePortConnectorCast(
-    chromecast::CastWebContents* cast_web_contents)
-    : cast_web_contents_(cast_web_contents) {
+    chromecast::CastWebContents* cast_web_contents,
+    cast_api_bindings::Manager* bindings_manager)
+    : cast_web_contents_(cast_web_contents),
+      bindings_manager_(bindings_manager) {
   DCHECK(cast_web_contents_);
-
-  CastWebContents::Observer::Observe(cast_web_contents_);
+  DCHECK(bindings_manager_);
 
   // Register the port connection JS script for early injection.
   std::string bindings_script_string =
@@ -35,14 +39,11 @@ NamedMessagePortConnectorCast::NamedMessagePortConnectorCast(
           IDR_PORT_CONNECTOR_JS);
   DCHECK(!bindings_script_string.empty())
       << "NamedMessagePortConnector resources not loaded.";
-
-  cast_web_contents->AddBeforeLoadJavaScript(
-      kNamedMessagePortConnectorBindingsId, bindings_script_string);
+  bindings_manager_->AddBinding(kNamedMessagePortConnectorBindingsId,
+                                bindings_script_string);
 }
 
-NamedMessagePortConnectorCast::~NamedMessagePortConnectorCast() {
-  CastWebContents::Observer::Observe(nullptr);
-}
+NamedMessagePortConnectorCast::~NamedMessagePortConnectorCast() = default;
 
 void NamedMessagePortConnectorCast::OnPageLoaded() {
   // Send the port connection message to the page once it is loaded.
@@ -58,24 +59,5 @@ void NamedMessagePortConnectorCast::OnPageLoaded() {
                                              std::move(ports));
 }
 
-void NamedMessagePortConnectorCast::OnPageStateChanged(
-    CastWebContents* cast_web_contents) {
-  auto page_state = cast_web_contents->page_state();
-
-  switch (page_state) {
-    case CastWebContents::PageState::DESTROYED:
-    case CastWebContents::PageState::ERROR:
-      CastWebContents::Observer::Observe(nullptr);
-      cast_web_contents_ = nullptr;
-      break;
-    case CastWebContents::PageState::LOADED:
-      OnPageLoaded();
-      break;
-    case CastWebContents::PageState::IDLE:
-    case CastWebContents::PageState::LOADING:
-    case CastWebContents::PageState::CLOSED:
-      break;
-  }
-}
-
+}  // namespace bindings
 }  // namespace chromecast
