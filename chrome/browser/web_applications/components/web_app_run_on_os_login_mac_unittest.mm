249b91c878444868dc2a0e8131af23876efb2244
Mohsen Izadi <mohsen@chromium.org>
2021-06-15 17:46:45 +0000

    Revert "Adds a way to create app shortcuts in different folder on Mac/Win/Linux."
    This reverts commit 479f56930ace7c1d1a0223f1ebb9fb38a0b916e9.
    
    Reason for revert: Suspected cause of failures on Mac10.12 Tests bot: https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.12%20Tests/44436/overview
    
    Original change's description:
    > Adds a way to create app shortcuts in different folder on Mac/Win/Linux.
    >
    > Change-Id: I3c06104c86333cbbd0857085cf7d7dd1fca63616
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906422
    > Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#892494}
    
    Change-Id: I9dd117d7f5e688c9efb176ec437c70c1a3add748
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964352
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892637}

--- chrome/browser/web_applications/components/web_app_run_on_os_login_mac_unittest.mm
+++ chrome/browser/web_applications/components/web_app_run_on_os_login_mac_unittest.mm
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace web_app {
 
@@ -98,9 +97,7 @@ class WebAppRunOnOsLoginMacTest : public WebAppTest {
     user_data_dir_ = base::MakeAbsoluteFilePath(user_data_dir_);
     app_data_dir_ = base::MakeAbsoluteFilePath(app_data_dir_);
 
-    ShortcutOverrideForTesting shortcut_override;
-    shortcut_override.chrome_apps_folder = destination_dir_;
-    web_app::SetShortcutOverrideForTesting(shortcut_override);
+    SetChromeAppsFolderForTesting(destination_dir_);
 
     info_ = GetShortcutInfo();
     base::FilePath shim_base_name =
@@ -113,7 +110,7 @@ class WebAppRunOnOsLoginMacTest : public WebAppTest {
 
   void TearDown() override {
     WebAppAutoLoginUtil::SetInstanceForTesting(nullptr);
-    web_app::SetShortcutOverrideForTesting(absl::nullopt);
+    SetChromeAppsFolderForTesting(base::FilePath());
     WebAppShortcutCreator::ResetHaveLocalizedAppDirNameForTesting();
     WebAppTest::TearDown();
   }
