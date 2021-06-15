8ff6c3f9e8a8beb9dc76ac387cc207bbf2f77cc8
Yusuke Sato <yusukes@chromium.org>
2021-06-15 03:42:07 +0000

    arcvm: Add ArcMemoryBridge class for easier access to memory.mojom
    BUG=b:190430751
    TEST=components_unittests
    
    Change-Id: I936d1dcabda9f8c03dd1b592184d36d6671c1957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2955300
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892413}

---
+++ components/arc/test/fake_memory_instance.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/arc/test/fake_memory_instance.h"
+
+#include "base/callback_helpers.h"
+
+namespace arc {
+
+FakeMemoryInstance::FakeMemoryInstance() = default;
+FakeMemoryInstance::~FakeMemoryInstance() = default;
+
+void FakeMemoryInstance::DropCaches(DropCachesCallback callback) {
+  std::move(callback).Run(drop_caches_result_);
+}
+
+}  // namespace arc
