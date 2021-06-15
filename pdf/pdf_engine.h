8c8dcf4ec79aa87a5d02f57fd00562e7cf5fa030
Daniel Hosseinian <dhoss@chromium.org>
2021-06-15 04:13:36 +0000

    [unseasoned-pdf] Propagate duplex type as a printing::mojom::DuplexMode
    PDFEngine currently returns the duplex type enum as a type-unsafe int,
    which later gets cast to a PP_PrivateDuplexMode_Dev.
    
    In preparation for use with the Unseasoned viewer, return the duplex
    type as a printing::mojom::DuplexMode, and then convert it to a
    PP_PrivateDuplexMode_Dev enum in OutOfProcessInstance.
    
    Meanwhile, renamed PDFEngine::GetDuplexType() to
    PDFEngine::GetDuplexMode().
    
    Bug: 1200000
    Change-Id: Idb2ab28520c4621c7df85eb99aba8e1604154946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961652
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892418}

--- pdf/pdf_engine.h
+++ pdf/pdf_engine.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/cpp/completion_callback.h"
 #include "ppapi/cpp/private/pdf.h"
 #include "ppapi/cpp/url_loader.h"
+#include "printing/mojom/print.mojom-forward.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/cursor/mojom/cursor_type.mojom-forward.h"
@@ -432,7 +433,7 @@ class PDFEngine {
   // Returns number of copies to be printed.
   virtual int GetCopiesToPrint() = 0;
   // Returns the duplex setting.
-  virtual int GetDuplexType() = 0;
+  virtual printing::mojom::DuplexMode GetDuplexMode() = 0;
   // Returns the uniform page size of the document in points. Returns
   // `absl::nullopt` if the document has more than one page size.
   virtual absl::optional<gfx::Size> GetUniformPageSizePoints() = 0;
--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/url_loader_wrapper_impl.h"
 #include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/private/pdf.h"
+#include "printing/mojom/print.mojom-shared.h"
 #include "printing/units.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "third_party/blink/public/common/input/web_keyboard_event.h"
@@ -2611,8 +2612,17 @@ int PDFiumEngine::GetCopiesToPrint() {
   return FPDF_VIEWERREF_GetNumCopies(doc());
 }
 
-int PDFiumEngine::GetDuplexType() {
-  return static_cast<int>(FPDF_VIEWERREF_GetDuplex(doc()));
+printing::mojom::DuplexMode PDFiumEngine::GetDuplexMode() {
+  switch (FPDF_VIEWERREF_GetDuplex(doc())) {
+    case Simplex:
+      return printing::mojom::DuplexMode::kSimplex;
+    case DuplexFlipShortEdge:
+      return printing::mojom::DuplexMode::kShortEdge;
+    case DuplexFlipLongEdge:
+      return printing::mojom::DuplexMode::kLongEdge;
+    default:
+      return printing::mojom::DuplexMode::kUnknownDuplexMode;
+  }
 }
 
 absl::optional<gfx::Size> PDFiumEngine::GetUniformPageSizePoints() {
