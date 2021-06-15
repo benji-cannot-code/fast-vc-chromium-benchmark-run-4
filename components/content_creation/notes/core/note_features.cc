54f5b43de2bffd95c2e55a4cfbf24ec63b4c9175
SebSG <sebsg@chromium.org>
2021-06-15 13:45:50 +0000

    [Note] Add feature flag for publish.
    Change-Id: I6b1530740ebfea6b6f8d23b73503a869feda99b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953940
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892552}

--- components/content_creation/notes/core/note_features.cc
+++ components/content_creation/notes/core/note_features.cc
@@ -13,6 +13,9 @@ const base::Feature kWebNotesStylizeEnabled{"WebNotesStylize",
 const base::FeatureParam<bool> kRandomizeOrderParam{&kWebNotesStylizeEnabled,
                                                     "randomize_order", false};
 
+const base::Feature kWebNotesPublish{"WebNotesPublish",
+                                     base::FEATURE_DISABLED_BY_DEFAULT};
+
 bool IsStylizeEnabled() {
   return base::FeatureList::IsEnabled(kWebNotesStylizeEnabled);
 }
@@ -20,6 +23,10 @@ bool IsStylizeEnabled() {
 bool IsRandomizeOrderEnabled() {
   DCHECK(IsStylizeEnabled());
   return kRandomizeOrderParam.Get();
+}
+
+bool IsPublishEnabled() {
+  return base::FeatureList::IsEnabled(kWebNotesPublish);
 }
 
 }  // namespace content_creation
