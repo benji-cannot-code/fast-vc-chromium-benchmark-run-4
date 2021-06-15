007faffe48d794ba1635e66eed197f8505043ade
Gavin Dodd <gavindodd@google.com>
2021-06-15 17:47:04 +0000

    shimless: Add WP disable complete page.
    Bug: 1198187
    Test: Build and run Chrome, check the screen is displayed when expected
    
    Change-Id: I88f192b8b519a21983bc2f0a37ef8f9203474381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2954660
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/master@{#892638}

---
+++ ash/content/shimless_rma/resources/onboarding_wp_disable_complete_page.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './shimless_rma_shared_css.js';
+import './base_page.js';
+
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @fileoverview
+ * 'onboarding-wp-disable-complete-page' notifies the user that HWWP disable was
+ * successful.
+ */
+export class OnboardingWpDisableCompletePage extends PolymerElement {
+  static get is() {
+    return 'onboarding-wp-disable-complete-page';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
+  /** @override */
+  ready() {
+    super.ready();
+  }
+};
+
+customElements.define(
+    OnboardingWpDisableCompletePage.is, OnboardingWpDisableCompletePage);
