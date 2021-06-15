ab6d7b0328538aa788a3ea2710f4c13623dae1b0
Nohemi Fernandez <fernandex@chromium.org>
2021-06-15 18:07:50 +0000

    [iOS] Move SigninCompletionInfo out of signin_constants.h.
    Follow-up to crrev.com/c/2960457.
    
    Bug: N/A
    Change-Id: I32c904db31883d79f8f3aae70207fa6bf131d59e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2963993
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892644}

---
+++ ios/chrome/browser/ui/authentication/signin/signin_completion_info.mm
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/authentication/signin/signin_completion_info.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation SigninCompletionInfo {
+  GURL _completionURL;
+}
+
++ (instancetype)signinCompletionInfoWithIdentity:(ChromeIdentity*)identity {
+  return [[SigninCompletionInfo alloc]
+            initWithIdentity:identity
+      signinCompletionAction:SigninCompletionActionNone];
+}
+
+- (instancetype)initWithIdentity:(ChromeIdentity*)identity
+          signinCompletionAction:
+              (SigninCompletionAction)signinCompletionAction {
+  self = [super init];
+  if (self) {
+    _identity = identity;
+    _signinCompletionAction = signinCompletionAction;
+  }
+  return self;
+}
+
+- (const GURL&)completionURL {
+  return _completionURL;
+}
+
+- (void)setCompletionURL:(const GURL&)completionURL {
+  if (_completionURL == completionURL)
+    return;
+  DCHECK(completionURL.is_valid());
+  DCHECK(_signinCompletionAction == SigninCompletionActionOpenCompletionURL);
+  _completionURL = completionURL;
+}
+
+@end
--- ios/chrome/browser/ui/authentication/signin/signin_coordinator.h
+++ ios/chrome/browser/ui/authentication/signin/signin_coordinator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/ios/block_types.h"
 #import "components/signin/public/base/signin_metrics.h"
+#import "ios/chrome/browser/ui/authentication/signin/signin_completion_info.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_constants.h"
 #import "ios/chrome/browser/ui/coordinators/chrome_coordinator.h"
 
