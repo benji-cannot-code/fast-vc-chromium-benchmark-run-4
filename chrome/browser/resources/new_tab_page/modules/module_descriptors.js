7f1c5b497ec8b396bd7cc620a3f2ebc23911dd90
Deanna Oei <doei@google.com>
2021-06-15 20:37:40 +0000

    [ntp][modules] Copy Cart Module
    When modules redesigned flag is enabled, display a copy of the cart
    module.
    
    Bug: 1217289
    Change-Id: I211b39d9eba2caa123e73e1d82a56de20867cf36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956396
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Deanna Oei <doei@google.com>
    Cr-Commit-Position: refs/heads/master@{#892705}

--- chrome/browser/resources/new_tab_page/modules/module_descriptors.js
+++ chrome/browser/resources/new_tab_page/modules/module_descriptors.js
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {loadTimeData} from '../i18n_setup.js';
 
 import {chromeCartDescriptor} from './cart/module.js';
+import {chromeCartDescriptor as chromeCartV2Descriptor} from './cart_v2/module.js';
 import {driveDescriptor} from './drive/module.js';
 // <if expr="not is_official_build">
 import {dummyDescriptor, dummyDescriptor2} from './dummy/module.js';
@@ -29,7 +30,11 @@ if (loadTimeData.getBoolean('recipeTasksModuleEnabled')) {
 }
 
 if (loadTimeData.getBoolean('chromeCartModuleEnabled')) {
-  descriptors.push(chromeCartDescriptor);
+  if (loadTimeData.getBoolean('modulesRedesignedEnabled')) {
+    descriptors.push(chromeCartV2Descriptor);
+  } else {
+    descriptors.push(chromeCartDescriptor);
+  }
 }
 
 if (loadTimeData.getBoolean('driveModuleEnabled')) {
