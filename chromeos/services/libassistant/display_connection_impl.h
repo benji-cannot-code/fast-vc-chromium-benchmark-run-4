cb6cd1740ab109db388991f3d0f90aa76671503a
Xiaohui Chen <xiaohuic@google.com>
2021-06-15 16:04:51 +0000

    assistant_proto: remove internal proto deps
    This helps further refactoring the internal proto targets.
    
    BUG=None
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ib37fb8911edbb0c8de5644cc46743833177cce35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961375
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892598}

--- chromeos/services/libassistant/display_connection_impl.h
+++ chromeos/services/libassistant/display_connection_impl.h
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/sequenced_task_runner.h"
 #include "base/synchronization/lock.h"
+#include "chromeos/assistant/internal/proto/google3/libassistant/display/proto/display_connection.pb.h"
 #include "chromeos/services/libassistant/public/cpp/android_app_info.h"
-#include "libassistant/display/proto/display_connection.pb.h"
 #include "libassistant/shared/internal_api/display_connection.h"
 
 namespace chromeos {
