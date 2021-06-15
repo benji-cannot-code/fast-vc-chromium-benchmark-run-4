607b82d604c9bc477bec916b099099442f99955f
Alice Wang <aliceywang@chromium.org>
2021-06-15 08:18:10 +0000

    [Android][Signin] Migrate to new getAccounts() API in account picker
    This CL migrates the usage of the old tryGetGoogleAccounts() to the
    new getAccounts() API in account picker.
    
    Bug: 1211294
    Change-Id: I69347b8ba4cba6c29d274b9022a70b92f8f3526a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953823
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892485}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerBottomSheetMediator.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerBottomSheetMediator.java
@@ -117,7 +117,7 @@ class AccountPickerBottomSheetMediator implements AccountPickerCoordinator.Liste
      */
     @Override
     public void onAccountsChanged() {
-        mAccountManagerFacade.tryGetGoogleAccounts(this::updateAccounts);
+        mAccountManagerFacade.getAccounts().then(this::updateAccounts);
     }
 
     /**
