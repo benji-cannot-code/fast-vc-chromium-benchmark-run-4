1ece7202402678e223bfe04836f4b4b7ab77561f
Gavin Dodd <gavindodd@google.com>
2021-06-15 23:18:08 +0000

    shimless: Add stub for shimless_rma_service
     - Add ShimlessRmaService with stub functions.
     - Create the shimless rma service at app start.
     - Register shimless rma service binders with ash
    
    Bug: 1198187
    Test: Build out/Default chrome
    
    Change-Id: I0e663b100ac0faada4e7034ecfcfd65f3d4c72c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2924162
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892781}

--- ash/content/shimless_rma/shimless_rma.h
+++ ash/content/shimless_rma/shimless_rma.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_CONTENT_SHIMLESS_RMA_SHIMLESS_RMA_H_
 #define ASH_CONTENT_SHIMLESS_RMA_SHIMLESS_RMA_H_
 
+#include <memory>
+
+#include "ash/content/shimless_rma/backend/shimless_rma_service.h"
+#include "ash/content/shimless_rma/mojom/shimless_rma.mojom-forward.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom-forward.h"  // nogncheck
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
@@ -28,8 +32,12 @@ class ShimlessRMADialogUI : public ui::MojoWebDialogUI {
   void BindInterface(
       mojo::PendingReceiver<chromeos::network_config::mojom::CrosNetworkConfig>
           receiver);
+  void BindInterface(
+      mojo::PendingReceiver<shimless_rma::mojom::ShimlessRmaService> receiver);
 
  private:
+  std::unique_ptr<shimless_rma::ShimlessRmaService> shimless_rma_manager_;
+
   WEB_UI_CONTROLLER_TYPE_DECL();
 };
 
