b2540f2f10218b6c1e031b010c2087f96b86f2b1
Xiaoling Bao <xiaolingbao@chromium.org>
2021-06-15 20:47:42 +0000

    As the first step, Remove dependency on //components/policy/core/common for the production code. This DEP triple size the updater binary and is undesirable. We can bring back the dependency and reuse code there later on once //components/policy is refactored (crbug.com/1204665) to a smaller target.
    Bug: 1219760
    Change-Id: Ide1c7a29c0ed10ade1fd920082f26162d4807733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964915
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892708}

--- chrome/updater/device_management/dm_message.cc
+++ chrome/updater/device_management/dm_message.cc
@@ -10,9 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/fixed_flat_map.h"
 #include "base/logging.h"
+#include "chrome/updater/device_management/cloud_policy_util.h"
 #include "chrome/updater/device_management/dm_response_validator.h"
-#include "components/policy/core/common/cloud/cloud_policy_constants.h"
-#include "components/policy/core/common/cloud/cloud_policy_util.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 
 namespace updater {
