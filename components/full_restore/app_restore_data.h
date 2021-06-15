b944b61ef47199f4a5b6f2431ae21cc2cf1f140d
Sammie Quon <sammiequon@chromium.org>
2021-06-15 19:36:59 +0000

    full_restore: Save pre-minimized state.
    This is so users can unminimize to the state the were in before
    minimizing.
    
    Adds a new property to be saved, pre-minimized show state. Additionally,
    NativeWidgetAura is modified so the restore bounds are set when the
    widget is minimized, otherwise we won't be able to restore to the
    correct bounds after minimize->maximize->restore.
    
    Fixed: 1217818
    Test: manual, added test
    Change-Id: I8aad6ebba2ecc5e8db61965b4d5e8dbdc4b62256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956404
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892678}

--- components/full_restore/app_restore_data.h
+++ components/full_restore/app_restore_data.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/base/window_state_type.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/base/ui_base_types.h"
 #include "ui/gfx/geometry/rect.h"
 #include "url/gurl.h"
 
@@ -95,6 +96,7 @@ struct COMPONENT_EXPORT(FULL_RESTORE) AppRestoreData {
   absl::optional<gfx::Rect> restore_bounds;
   absl::optional<gfx::Rect> current_bounds;
   absl::optional<chromeos::WindowStateType> window_state_type;
+  absl::optional<ui::WindowShowState> pre_minimized_show_state_type;
 
   // Extra ARC window's information.
   absl::optional<gfx::Size> minimum_size;
