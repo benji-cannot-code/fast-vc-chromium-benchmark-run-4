fe8aca17040c5daafaa34442b8e1069d94d1cca6
Lingqi Chi <lingqi@chromium.org>
2021-06-15 04:17:45 +0000

    Prerender: Set referrer for speculation candidates
    For prerenders triggered by speculationrules scripts, their referrer
    are not set appropriately so some web tests fails.
    This CL adds a new field into mojom::SpeculationCandidate, and sends the
    candidates' referrers to the browser process, so that PrerenderHost can
    know the correct referrers.
    
    Bug: 1215932
    Change-Id: Ib72637619bde0385d54517c110baf89126984c4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939842
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892421}

--- content/browser/speculation_rules/speculation_host_impl_unittest.cc
+++ content/browser/speculation_rules/speculation_host_impl_unittest.cc
@@ -72,6 +72,7 @@ class SpeculationHostImplTest : public RenderViewHostImplTestHarness {
     auto candidate = blink::mojom::SpeculationCandidate::New();
     candidate->action = blink::mojom::SpeculationAction::kPrerender;
     candidate->url = url;
+    candidate->referrer = blink::mojom::Referrer::New();
     return candidate;
   }
 
