9d3ae3fb67410ba4abb0ed3cda7a64cb9a712dd7
Stephan Hartmann <stha09@googlemail.com>
2021-06-15 17:34:02 +0000

    libstdc++: do not declare deque of const types
    libstdc++ requires that value_type of std::deque is non-const.
    
    Bug: 957519
    Change-Id: Icf543cadb7e8de13224efa76c0277e8736e94d1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2959889
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#892628}

--- components/segmentation_platform/internal/execution/model_execution_manager_impl.cc
+++ components/segmentation_platform/internal/execution/model_execution_manager_impl.cc
@@ -45,7 +45,7 @@ struct ModelExecutionManagerImpl::ExecutionState {
   SegmentationModelHandler* model_handler = nullptr;
   ModelExecutionCallback callback;
   base::TimeDelta bucket_duration;
-  std::deque<const proto::Feature> features;
+  std::deque<proto::Feature> features;
 };
 
 ModelExecutionManagerImpl::ModelExecutionManagerImpl(
