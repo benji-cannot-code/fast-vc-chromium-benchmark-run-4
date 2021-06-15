01efe306f13e19e63530de314f5ff1aa3e9bd718
Gordon Seto <gordonseto@google.com>
2021-06-15 21:17:45 +0000

    [CrOS Settings] Create BluetoothHandler.
    Create BluetoothHandler to handle Bluetooth-related WebUI requests. Add
    handling request to fetch the is_blocked_by_policy property of a
    Bluetooth device. This will be used to check if a Bluetooth device is
    enterprise-managed or not, and if so, display an icon and tooltip. The
    UI will be added in a follow-up CL.
    
    Design Doc:
    go/bluetooth-enterprise-policy-ui
    
    Bug: 1208155
    Change-Id: Ieea98e6bb5274c73bfc2edc9ca0311500e9be6a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956473
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#892718}

--- chrome/browser/ui/webui/settings/chromeos/bluetooth_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/bluetooth_section.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "chrome/browser/ui/webui/chromeos/bluetooth_dialog_localized_strings_provider.h"
+#include "chrome/browser/ui/webui/settings/chromeos/bluetooth_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
 #include "chrome/browser/ui/webui/settings/chromeos/constants/setting.mojom.h"
 #include "chrome/browser/ui/webui/settings/chromeos/search/search.mojom.h"
@@ -193,6 +194,10 @@ void BluetoothSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
   };
   html_source->AddLocalizedStrings(kLocalizedStrings);
   chromeos::bluetooth_dialog::AddLocalizedStrings(html_source);
+}
+
+void BluetoothSection::AddHandlers(content::WebUI* web_ui) {
+  web_ui->AddMessageHandler(std::make_unique<BluetoothHandler>());
 }
 
 int BluetoothSection::GetSectionNameMessageId() const {
