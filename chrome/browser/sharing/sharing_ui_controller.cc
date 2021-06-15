728e57f3c817d53b13d95d5717feee4daba447d7
Yi Gu <yigu@chromium.org>
2021-06-15 14:28:38 +0000

    [WebOTP] Fix DCHECK failure upon cancel callback
    A NOTREACHED [1] could be triggered upon cancel callback when we try to
    show an error dialog but there is no need to do so in this case. One
    possible solution is to not showing error dialog when |send_result_|
    is kCancelled.
    
    SharingSendMessageResult::kCancelled represents that the cancel callback
    has been called and it does not mean that the request is failed to be
    sent out. Therefore alternatively we could update |HasSendFailed|.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/sharing/sharing_ui_controller.cc;l=76
    
    Bug: None
    Change-Id: Ib1d168eb72188abee778f4a2640e635494738852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961076
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892568}

--- chrome/browser/sharing/sharing_ui_controller.cc
+++ chrome/browser/sharing/sharing_ui_controller.cc
@@ -150,7 +150,8 @@ SharingUiController::GetDevices() const {
 }
 
 bool SharingUiController::HasSendFailed() const {
-  return send_result_ != SharingSendMessageResult::kSuccessful;
+  return send_result_ != SharingSendMessageResult::kSuccessful &&
+         send_result_ != SharingSendMessageResult::kCancelled;
 }
 
 void SharingUiController::MaybeShowErrorDialog() {
