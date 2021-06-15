59980f96c21191ca46e2c4b5a842a1d1ad0ec65b
Vincent Boisselle <vincb@google.com>
2021-06-15 19:38:04 +0000

    [iOS] Delete the first run agent after usage
    The First Run agent isn't needed anymore when passed the
    InitStageFirstRun stage; it should then be deleted.
    
    Bug: 1218982
    Change-Id: Ia5703fbccd4382f1403358a25aab2e3e468a29bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961232
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892680}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -291,8 +291,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
   // List of closure to run as part of shutdown. The closure will be called
   // in reverse order of registration.
   std::vector<base::OnceClosure> _cleanupClosures;
-
-  FirstRunAppAgent* _firstRunAppAgent;
 }
 
 // Handles collecting metrics on user triggered screenshots
@@ -652,9 +650,7 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
   [self.appState addAgent:[[EnterpriseAppAgent alloc] init]];
   [self.appState addAgent:[[IncognitoUsageAppStateAgent alloc] init]];
 
-  FirstRunAppAgent* firstRunAppAgent = [[FirstRunAppAgent alloc] init];
-  _firstRunAppAgent = firstRunAppAgent;
-  [self.appState addAgent:firstRunAppAgent];
+  [self.appState addAgent:[[FirstRunAppAgent alloc] init]];
 }
 
 #pragma mark - Property implementation.
@@ -1299,10 +1295,6 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 #pragma mark - TestingOnly
 
 @implementation MainController (TestingOnly)
-
-- (FirstRunAppAgent*)firstRunAppAgent {
-  return _firstRunAppAgent;
-}
 
 - (void)setStartupParametersWithURL:(const GURL&)launchURL {
   NSString* sourceApplication = @"Fake App";
