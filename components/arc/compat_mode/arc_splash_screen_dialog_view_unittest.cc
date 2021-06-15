ea4b45183c01635edd2a3d7c296ec9ee0b83f699
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-06-15 02:24:53 +0000

    Show splash screen dialog as bubble
    This CL changes |ShowSplashScreenDialog| to show the splash screen as
    bubble dialog instead of overlay dialog.
    The new UI spec of the splash dialog requires it to be “anchored” to the
    caption bar (HeaderView), so the dialog is no longer shown for the app
    window itself but the specific view.
    Therefore, showing the dialog as a bubble makes sense to match the API
    purpose, and that enables us to set the “anchor” easily.
    Please note that the scrim background is still shown as an overlay
    dialog.
    
    BUG=b:187488731
    TEST=ArcSplashScreenDialogViewTest
    
    Change-Id: I0be6e147f7eb0502f9488059fa748c04090871e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2958991
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892388}

--- components/arc/compat_mode/arc_splash_screen_dialog_view_unittest.cc
+++ components/arc/compat_mode/arc_splash_screen_dialog_view_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/arc/compat_mode/arc_splash_screen_dialog_view.h"
 
+#include <memory>
+
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/views/test/views_test_base.h"
@@ -37,16 +39,22 @@ class ArcSplashScreenDialogViewTest : public views::ViewsTestBase {
   // views::ViewsTestBase:
   void SetUp() override {
     views::ViewsTestBase::SetUp();
-    widget_ = CreateTestWidget();
-    widget_->SetBounds(gfx::Rect(800, 800));
+    parent_widget_ = CreateTestWidget(views::Widget::InitParams::TYPE_WINDOW);
+    parent_widget_->Show();
+
     auto dialog_view = std::make_unique<ArcSplashScreenDialogView>(
         base::BindRepeating(&ArcSplashScreenDialogViewTest::OnCloseCallback,
-                            base::Unretained(this)));
-    dialog_view_ = widget_->SetContentsView(std::move(dialog_view));
+                            base::Unretained(this)),
+        parent_widget_->GetNativeView(), nullptr);
+    dialog_view_ = dialog_view.get();
+    bubble_widget_ =
+        views::BubbleDialogDelegateView::CreateBubble(std::move(dialog_view));
+    bubble_widget_->Show();
   }
 
   void TearDown() override {
-    widget_.reset();
+    parent_widget_->CloseNow();
+    parent_widget_.reset();
     views::ViewsTestBase::TearDown();
   }
 
@@ -59,7 +67,8 @@ class ArcSplashScreenDialogViewTest : public views::ViewsTestBase {
 
  private:
   ArcSplashScreenDialogView* dialog_view_;
-  std::unique_ptr<views::Widget> widget_;
+  std::unique_ptr<views::Widget> parent_widget_;
+  views::Widget* bubble_widget_;
 };
 
 TEST_F(ArcSplashScreenDialogViewTest, TestBuildSplashScreenDialogView) {
