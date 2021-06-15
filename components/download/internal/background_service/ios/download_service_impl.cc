6dcd5eb27b240129a3501af5cf335c645073e90b
Xing Liu <xingliu@google.com>
2021-06-15 21:38:30 +0000

    Download service: Add iOS keyed service factory.
    Adds keyed service factory on iOS. This CL only adds the boilerplate
    class and directory structure. Actual service will be implemented later.
    
    Bug: 1215315
    Change-Id: I38855f04677ae9687c78ac06dbf6a6bfdb5225db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2954419
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892727}

--- components/download/internal/background_service/ios/download_service_impl.cc
+++ components/download/internal/background_service/ios/download_service_impl.cc
@@ -57,4 +57,9 @@ void DownloadServiceImpl::ChangeDownloadCriteria(
   NOTIMPLEMENTED();
 }
 
+Logger* DownloadServiceImpl::GetLogger() {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
 }  // namespace download
--- ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/device_sharing/device_sharing_manager_factory.h"
 #include "ios/chrome/browser/discover_feed/discover_feed_service_factory.h"
 #include "ios/chrome/browser/dom_distiller/dom_distiller_service_factory.h"
+#include "ios/chrome/browser/download/background_service/download_service_factory.h"
 #include "ios/chrome/browser/download/browser_download_service_factory.h"
 #include "ios/chrome/browser/favicon/favicon_service_factory.h"
 #include "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
@@ -120,6 +121,7 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   DeviceSharingManagerFactory::GetInstance();
   DiscoverFeedServiceFactory::GetInstance();
   DomainDiversityReporterFactory::GetInstance();
+  DownloadServiceFactory::GetInstance();
   GoogleLogoServiceFactory::GetInstance();
   IdentityManagerFactory::GetInstance();
   IOSChromeContentSuggestionsServiceFactory::GetInstance();
