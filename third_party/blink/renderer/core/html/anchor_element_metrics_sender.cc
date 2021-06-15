76b1a40ae606ef783aef5a0b2f194b9dc1de0e64
Peter Kasting <pkasting@chromium.org>
2021-06-15 20:24:49 +0000

    Various changes to prevent narrowing conversions: third_party/blink/
    Bug: 1216696
    Change-Id: Ie20b4664869501d45a913185f9e2cc00c30af0fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2958312
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892700}

--- third_party/blink/renderer/core/html/anchor_element_metrics_sender.cc
+++ third_party/blink/renderer/core/html/anchor_element_metrics_sender.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 namespace {
-static double INTERSECTION_RATIO_THRESHOLD = 0.5;
+static float INTERSECTION_RATIO_THRESHOLD = 0.5f;
 
 }  // namespace
 
