7c2c18c588f65a3cc1ea7bcfe3e6f95dbedac026
Justin Novosad <junov@chromium.org>
2021-06-16 01:41:28 +0000

    Update command line flags for webg{l|pu}_fast_call perf tests
    Now that v8 fast api calls are enabled by default, the perf test
    variant logic needs to be inverted to cover the "fast call disabled"
    case.
    
    This CL also increases the number of iterations in the
    webgl/binding-bind-buffer.html benchmark because its results were too
    noisy. Therefore timings for that benchmark are expected to increase
    10x from ~3ms to ~30ms. This is not a regression.
    
    Bug: 1052746
    Change-Id: I02ebde638c432b394a8ddee757a330055dcb5b34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2950805
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892842}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -726,10 +726,13 @@ class BlinkPerfWebGL(_BlinkPerfBenchmark):
   def Name(cls):
     return 'blink_perf.webgl'
 
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs(['--disable-features=V8TurboFastApiCalls'])
+
 
 @benchmark.Info(emails=[
     'kbr@chromium.org', 'enga@chromium.org', 'mslekova@chromium.org',
-    'webgl-team@google.com'
+    'junov@chromium.org', 'webgl-team@google.com'
 ],
                 component='Blink>WebGL',
                 documentation_url='https://bit.ly/blink-perf-benchmarks')
@@ -742,7 +745,7 @@ class BlinkPerfWebGLFastCall(_BlinkPerfBenchmark):
     return 'blink_perf.webgl_fast_call'
 
   def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(['--enable-unsafe-fast-js-calls'])
+    options.AppendExtraBrowserArgs(['--enable-features=V8TurboFastApiCalls'])
 
 
 @benchmark.Info(emails=[
@@ -759,12 +762,13 @@ class BlinkPerfWebGPU(_BlinkPerfBenchmark):
     return 'blink_perf.webgpu'
 
   def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(['--enable-unsafe-webgpu'])
+    options.AppendExtraBrowserArgs(
+        ['--enable-unsafe-webgpu', '--disable-features=V8TurboFastApiCalls'])
 
 
 @benchmark.Info(emails=[
     'enga@chromium.org', 'cwallez@chromium.org', 'mslekova@chromium.org',
-    'webgpu-developers@google.com'
+    'junov@chromium.org', 'webgpu-developers@google.com'
 ],
                 component='Blink>WebGPU',
                 documentation_url='https://bit.ly/blink-perf-benchmarks')
@@ -778,4 +782,4 @@ class BlinkPerfWebGPUFastCall(_BlinkPerfBenchmark):
 
   def SetExtraBrowserOptions(self, options):
     options.AppendExtraBrowserArgs(
-        ['--enable-unsafe-webgpu', '--enable-unsafe-fast-js-calls'])
+        ['--enable-unsafe-webgpu', '--enable-features=V8TurboFastApiCalls'])
