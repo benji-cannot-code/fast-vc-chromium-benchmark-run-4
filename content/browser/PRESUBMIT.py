44044d8e60dad83597aa2e719a717d7d6e786b76
Elaine Chien <elainechien@chromium.org>
2021-06-16 00:16:12 +0000

    Python3Migration: content/browser/PRESUBMIT
    Bug: 1212023
    Change-Id: Iec10b23d36d7df688ae619896259f720d601ebda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964634
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892794}

--- content/browser/PRESUBMIT.py
+++ content/browser/PRESUBMIT.py
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 match the corresponding bad_message.h file.
 """
 
+USE_PYTHON3 = True
+
 def CheckChangeOnCommit(input_api, output_api):
   return _CommonChecks(input_api, output_api)
 
