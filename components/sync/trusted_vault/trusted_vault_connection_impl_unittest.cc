78bb9ac523fc6386de6ea1d877a131072219b8b6
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-06-16 07:36:11 +0000

    Replace some TaskEnvironment-s in c/sync with single-thread version
    No behavior is changed.
    [1] advises that SingleThreadTaskEnvironment should be preferred over
    TaskEnvironment when sufficient. This CLs does all such replacements
    in components/sync/ which don't break tests.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:base/test/task_environment.h;l=47;drc=668ead109e76af58276b4ddfe40a99d6ac26a069
    
    Bug: None
    Change-Id: I50043696a38364794c31334c81923747c3f05eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960492
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#892911}

--- components/sync/trusted_vault/trusted_vault_connection_impl_unittest.cc
+++ components/sync/trusted_vault/trusted_vault_connection_impl_unittest.cc
@@ -153,7 +153,7 @@ class TrustedVaultConnectionImplTest : public testing::Test {
   const GURL kTestURL = GURL("https://test.com/test");
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 
   network::TestURLLoaderFactory test_url_loader_factory_;
 
