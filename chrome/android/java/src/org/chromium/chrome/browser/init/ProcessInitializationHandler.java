cb8e3b7c64ef6b1e0ddc0aaf052786145683ff3c
Max Tauro <maxtauro@google.com>
2021-06-16 16:57:55 +0000

    Added a stub implementation of the Quick Action Search Widget
    The branded strings are defined in this CL: https://crrev.com/i/3903128
    
    + The widget is gated behind a Finch flag and is also
    gated by Build channel using jinja vars
    
    Bug: 1213541
    Change-Id: If79087954701e610fda26985c7964a3607f786f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2936719
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893043}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -70,6 +70,7 @@ import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.ProfileManagerUtils;
 import org.chromium.chrome.browser.query_tiles.QueryTileUtils;
+import org.chromium.chrome.browser.quickactionsearchwidget.QuickActionSearchWidgetProvider;
 import org.chromium.chrome.browser.rlz.RevenueStats;
 import org.chromium.chrome.browser.searchwidget.SearchWidgetProvider;
 import org.chromium.chrome.browser.share.clipboard.ClipboardImageFileProvider;
@@ -89,6 +90,8 @@ import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.components.minidump_uploader.CrashFileManager;
 import org.chromium.components.signin.AccountManagerFacadeImpl;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
+import org.chromium.components.version_info.Channel;
+import org.chromium.components.version_info.VersionConstants;
 import org.chromium.components.viz.common.VizSwitches;
 import org.chromium.components.viz.common.display.DeJellyUtils;
 import org.chromium.components.webapps.AppBannerManager;
@@ -244,6 +247,12 @@ public class ProcessInitializationHandler {
                 });
 
         SearchWidgetProvider.initialize();
+
+        if (VersionConstants.CHANNEL == Channel.CANARY
+                || VersionConstants.CHANNEL == Channel.DEFAULT) {
+            QuickActionSearchWidgetProvider.initialize();
+        }
+
         HistoryDeletionBridge.getInstance().addObserver(new ContentCaptureHistoryDeletionObserver(
                 () -> PlatformContentCaptureController.getInstance()));
     }
