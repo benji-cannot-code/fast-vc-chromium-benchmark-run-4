93d90cccc6f13c471a195f66d79b321b646c703a
Hazem Ashmawy <hazems@chromium.org>
2021-06-16 02:47:27 +0000

    [AW] Check packages allowlist expiry date
    Check the expiry date of the app packages names allowlist in the
    manifest. Only lookup the package name in the allowlist if it
    hasn't expired.
    
    Bug: 1202703
    Test: bin/run_android_webview_unittests
    Change-Id: I2b24a4226bd0b5a0038f3c9fa49b4abae8c376c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2934847
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892862}

--- android_webview/browser/component_updater/loader_policies/aw_apps_package_names_allowlist_component_loader_policy_unittest.cc
+++ android_webview/browser/component_updater/loader_policies/aw_apps_package_names_allowlist_component_loader_policy_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "base/version.h"
 #include "components/optimization_guide/core/bloom_filter.h"
@@ -32,11 +33,23 @@ constexpr int kNumHash = 11;
 constexpr int kNumBitsPerEntry = 16;
 const std::string kTestAllowlist[] = {"com.example.test", "my.fake.app",
                                       "yet.another.app"};
+double OneDayFromNowMs() {
+  return (base::Time::Now() + base::TimeDelta::FromDays(1) -
+          base::Time::UnixEpoch())
+      .InMillisecondsF();
+}
+
+double OneDayAgoMs() {
+  return (base::Time::Now() - base::TimeDelta::FromDays(1) -
+          base::Time::UnixEpoch())
+      .InMillisecondsF();
+}
 
 std::unique_ptr<base::DictionaryValue> BuildTestManifest() {
   auto manifest = std::make_unique<base::DictionaryValue>();
   manifest->SetIntPath(kBloomFilterNumHashKey, kNumHash);
   manifest->SetIntPath(kBloomFilterNumBitsKey, 3 * kNumBitsPerEntry);
+  manifest->SetDoublePath(kExpiryDateKey, OneDayFromNowMs());
 
   return manifest;
 }
@@ -205,6 +218,28 @@ TEST_F(AwAppsPackageNamesAllowlistComponentLoaderPolicyTest,
   base::flat_map<std::string, int> fd_map;
   fd_map[kAllowlistBloomFilterFileName] = OpenAndGetAllowlistFd();
   auto manifest = BuildTestManifest();
+
+  auto policy =
+      std::make_unique<AwAppsPackageNamesAllowlistComponentLoaderPolicy>(
+          kTestAllowlist[1],
+          base::BindOnce(&AwAppsPackageNamesAllowlistComponentLoaderPolicyTest::
+                             LookupConfirmationCallback,
+                         base::Unretained(this)));
+
+  policy->ComponentLoaded(base::Version(), fd_map, std::move(manifest));
+
+  lookup_run_loop_.Run();
+  EXPECT_FALSE(lookup_result_);
+}
+
+TEST_F(AwAppsPackageNamesAllowlistComponentLoaderPolicyTest,
+       TestExpiredAllowlist) {
+  WritePackageNamesAllowListToFile(
+      {kTestAllowlist, kTestAllowlist + base::size(kTestAllowlist)});
+  base::flat_map<std::string, int> fd_map;
+  fd_map[kAllowlistBloomFilterFileName] = OpenAndGetAllowlistFd();
+  auto manifest = BuildTestManifest();
+  manifest->SetDoublePath(kExpiryDateKey, OneDayAgoMs());
 
   auto policy =
       std::make_unique<AwAppsPackageNamesAllowlistComponentLoaderPolicy>(
