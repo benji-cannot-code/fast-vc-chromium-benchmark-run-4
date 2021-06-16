0cf52dc843185db37edc37be5cbdb0be8acab783
Hassan Talat <hatalat@microsoft.com>
2021-06-16 16:28:51 +0000

    dpwas: macPWAs WindowControlsOverlay app should observe the frame view
    This CL fixes the bug where the toggle button & the three dot menu
    button of a wco app could not be clicked after a vertical resize.
    
    The overlay NSView did not receive bounds changed events during a
    window resize.
    
    Bug: 1218980
    Change-Id: I32695b284f725c76ef1bbfd8775ba804038059e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961580
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Hassan Talat <hatalat@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#893035}

--- chrome/browser/ui/views/frame/window_controls_overlay_input_routing_mac.h
+++ chrome/browser/ui/views/frame/window_controls_overlay_input_routing_mac.h
@@ -23,7 +23,7 @@ class BrowserNonClientFrameViewMac;
 class WindowControlsOverlayInputRoutingMac : public views::ViewObserver {
  public:
   WindowControlsOverlayInputRoutingMac(
-      BrowserNonClientFrameViewMac const* browser_non_client_frame_view_mac,
+      BrowserNonClientFrameViewMac* browser_non_client_frame_view_mac,
       views::View* overlay_view,
       remote_cocoa::mojom::WindowControlsOverlayNSViewType overlay_type);
   ~WindowControlsOverlayInputRoutingMac() override;
@@ -37,7 +37,7 @@ class WindowControlsOverlayInputRoutingMac : public views::ViewObserver {
  private:
   void UpdateNSViewPosition();
 
-  BrowserNonClientFrameViewMac const* browser_non_client_frame_view_mac =
+  BrowserNonClientFrameViewMac* browser_non_client_frame_view_mac_ =
       nullptr;                 // weak
   views::View* overlay_view_;  // weak. Owned by BrowserNonClientFrameViewMac.
   views::NativeWidgetMacNSWindowHost* host_;  // weak. Owned by NativeWidgetMac.
--- chrome/browser/ui/views/frame/window_controls_overlay_input_routing_mac.mm
+++ chrome/browser/ui/views/frame/window_controls_overlay_input_routing_mac.mm
@@ -12,17 +12,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 WindowControlsOverlayInputRoutingMac::WindowControlsOverlayInputRoutingMac(
-    BrowserNonClientFrameViewMac const* browser_non_client_frame_view_mac,
+    BrowserNonClientFrameViewMac* browser_non_client_frame_view_mac,
     views::View* overlay_view,
     remote_cocoa::mojom::WindowControlsOverlayNSViewType overlay_type)
-    : browser_non_client_frame_view_mac(browser_non_client_frame_view_mac),
+    : browser_non_client_frame_view_mac_(browser_non_client_frame_view_mac),
       overlay_view_(overlay_view),
       overlay_type_(overlay_type) {
-  overlay_view_->AddObserver(this);
+  // WebAppOriginText animates and disappears during initial launch so we want
+  // to observe that and update the remote view accordingly.
+  if (overlay_type_ ==
+      remote_cocoa::mojom::WindowControlsOverlayNSViewType::kWebAppFrameToolbar)
+    overlay_view_->AddObserver(this);
+  else
+    browser_non_client_frame_view_mac_->AddObserver(this);
 }
 
 WindowControlsOverlayInputRoutingMac::~WindowControlsOverlayInputRoutingMac() {
-  overlay_view_->RemoveObserver(this);
+  if (overlay_type_ ==
+      remote_cocoa::mojom::WindowControlsOverlayNSViewType::kWebAppFrameToolbar)
+    overlay_view_->RemoveObserver(this);
+  else
+    browser_non_client_frame_view_mac_->RemoveObserver(this);
 }
 
 void WindowControlsOverlayInputRoutingMac::OnViewBoundsChanged(
@@ -32,7 +42,7 @@ void WindowControlsOverlayInputRoutingMac::OnViewBoundsChanged(
 
 void WindowControlsOverlayInputRoutingMac::Enable() {
   auto window =
-      browser_non_client_frame_view_mac->GetWidget()->GetNativeWindow();
+      browser_non_client_frame_view_mac_->GetWidget()->GetNativeWindow();
   host_ = views::NativeWidgetMacNSWindowHost::GetFromNativeWindow(window);
 
   host_->AddRemoteWindowControlsOverlayView(overlay_type_);
