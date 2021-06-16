f6b0908bf3403ea63dc2e4dc0a094f3036ebd8e3
Guillaume Jenkins <gujen@google.com>
2021-06-16 13:59:35 +0000

    [iOS FRE] Add highlight to UMA checkbox on tap
    Adds an animated highlight on the UMA checkbox of the Welcome screen of
    the new FRE while the UMA button is being pressed. The animation is
    similar to the one in the settings when tapping a checkbox cell, such
    as choosing the default search engine.
    
    Bug: 1214739
    Change-Id: Iafe77b35992b4d6c105247535ad14210f58ea2ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964334
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892996}

--- ios/chrome/browser/ui/first_run/welcome/checkbox_button.mm
+++ ios/chrome/browser/ui/first_run/welcome/checkbox_button.mm
@@ -19,6 +19,11 @@ const CGFloat kContentMargin = 13;
 // Default corner radius for the button shape.
 const CGFloat kDefaultCornerRadius = 8;
 
+// Tha alpha to apply when the button is highlighted.
+const CGFloat kHighlightedAlpha = 0.8;
+// The duration of the animation when adding / removing transparency.
+const CGFloat kAnimationDuration = 0.25;
+
 }  // namespace
 
 @interface CheckboxButton ()
@@ -108,6 +113,16 @@ const CGFloat kDefaultCornerRadius = 8;
 
 - (NSString*)accessibilityLabel {
   return self.label.text;
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+  __weak __typeof(self) weakSelf = self;
+  CGFloat targetAlpha = highlighted ? kHighlightedAlpha : 1.0;
+  [UIView animateWithDuration:kAnimationDuration
+                   animations:^{
+                     weakSelf.alpha = targetAlpha;
+                   }];
+  [super setHighlighted:highlighted];
 }
 
 @end
