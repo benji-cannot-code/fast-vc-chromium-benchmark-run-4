7c7f2adfe51688057b104130027d3e6a41a2074e
Roman Sorokin <rsorokin@chromium.org>
2021-06-16 13:55:06 +0000

    [ash-chrome] Focus link on the Eula screen after the next render
    I tested locally with the stress and it looks fine.
    
    Fixed: 1175244
    Change-Id: Ie62827552b9e3fe70d9c3155fb1fa4330e2f8e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964119
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#892993}

--- chrome/browser/resources/chromeos/login/screens/oobe/oobe_eula.js
+++ chrome/browser/resources/chromeos/login/screens/oobe/oobe_eula.js
@@ -440,7 +440,8 @@ Polymer({
    * @private
    */
   focusAdditionalTermsLink_() {
-    this.$.additionalTerms.focus();
+    Polymer.RenderStatus.afterNextRender(
+        this, () => this.$.additionalTerms.focus());
   },
 
   /**
