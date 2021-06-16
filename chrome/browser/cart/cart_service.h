e8f4e1f00363510ffccd8eb40f5df02e285df6d5
Yue Zhang <yuezhanggg@chromium.org>
2021-06-16 20:40:03 +0000

    [RBD] Fix discount URL load from context menu
    Before this change, opening a new tab from discounted cart in module
    via context menu will open the original cart URL instead of cart URL
    with discount. This CL fixes this issue by adding a second load so that
    user will end up in the correct URL.
    
    Bug: 1218979
    Change-Id: I145682ad99f2997117df2b384889db6529945179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956894
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893132}

--- chrome/browser/cart/cart_service.h
+++ chrome/browser/cart/cart_service.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cart/cart_discount_link_fetcher.h"
 #include "chrome/browser/cart/cart_metrics_tracker.h"
 #include "chrome/browser/cart/cart_service_factory.h"
+#include "chrome/browser/cart/discount_url_loader.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_service_observer.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
+class DiscountURLLoader;
 class FetchDiscountWorker;
 
 // Service to maintain and read/write data for chrome cart module.
@@ -95,11 +97,12 @@ class CartService : public history::HistoryServiceObserver,
   void GetDiscountURL(const GURL& cart_url,
                       base::OnceCallback<void(const GURL&)> callback);
   // Gets called when a navigation to |cart_url| is happening or might happen.
-  // This can be triggered by either left click on a cart in cart module or
-  // right click to open context menu which could lead to open the cart later.
-  // This method is used to record the latest interacted cart, and then use that
-  // to identify whether a navigation originated from cart module has happened.
-  void PrepareForNavigation(const GURL& cart_url);
+  // |is_navigating| indicates whether the navigation is happening (e.g. left
+  // click on the cart item) or might happen later (e.g. right click to open
+  // context menu). This method 1) Record the latest interacted cart,
+  // and then use that to identify whether a navigation originated from cart
+  // module has happened. 2) Help identify whether to load discount URL.
+  void PrepareForNavigation(const GURL& cart_url, bool is_navigating);
   // history::HistoryServiceObserver:
   void OnURLsDeleted(history::HistoryService* history_service,
                      const history::DeletionInfo& deletion_info) override;
@@ -112,6 +115,7 @@ class CartService : public history::HistoryServiceObserver,
   friend class CartServiceFactory;
   friend class CartServiceTest;
   friend class CartServiceDiscountTest;
+  friend class CartServiceBrowserDiscountTest;
   FRIEND_TEST_ALL_PREFIXES(CartHandlerNtpModuleFakeDataTest,
                            TestEnableFakeData);
 
@@ -188,6 +192,7 @@ class CartService : public history::HistoryServiceObserver,
   std::unique_ptr<CartDiscountLinkFetcher> discount_link_fetcher_;
   optimization_guide::OptimizationGuideDecider* optimization_guide_decider_;
   std::unique_ptr<CartMetricsTracker> metrics_tracker_;
+  std::unique_ptr<DiscountURLLoader> discount_url_loader_;
   base::WeakPtrFactory<CartService> weak_ptr_factory_{this};
 };
 
