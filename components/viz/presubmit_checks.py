982d5eee82b6c6f27e974cb4bb53d44c42ee8092
Elaine Chien <elainechien@chromium.org>
2021-06-16 19:58:56 +0000

    Reland "Python3Migration: components/viz/PRESUBMIT"
    This is a reland of 8fa4304dfd96e181ce06822524020e1e275b35eb
    
    This change fixes the python3 code that caused it to be reverted
    in https://crrev.com/c/2965760. A similar change was landed in master
    before this and this CL was rebased onto that.
    
    Original change's description:
    > Python3Migration: components/viz/PRESUBMIT
    >
    > Bug: 1212022
    > Change-Id: Iaacc8f9291bbed9511b1e30d73bcaae55746d051
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964918
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Commit-Queue: Elaine Chien <elainechien@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#892668}
    
    Bug: 1212022
    Change-Id: I0fa14e7d18f1602d766798bc38b49fdfbe37349f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965653
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893118}

--- components/viz/presubmit_checks.py
+++ components/viz/presubmit_checks.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Presubmit checks used in viz"""
 
 import re
-import string
 
 def CheckChangeLintsClean(input_api, output_api, allowlist, denylist=None):
   source_filter = lambda x: input_api.FilterSourceFile(x, allowlist, denylist)
