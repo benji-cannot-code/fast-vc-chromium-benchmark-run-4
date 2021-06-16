9c94a286ea403d5df95c33d12e884e79d6698ee9
Moe Ahmadi <mahmadi@chromium.org>
2021-06-16 22:32:28 +0000

    [memories] Display related searches under the top visit
    screenshot/AMidnbuJj6AucFa
    
    Bug: 1173908
    Change-Id: Ibfc38d1333e216ccd372f91462b2d1e1fa2cc49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966238
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893194}

--- chrome/browser/resources/memories/cluster.js
+++ chrome/browser/resources/memories/cluster.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './page_favicon.js';
-import './search_query.js';
 import './shared_vars.js';
 import './top_visit.js';
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
