f1935a982b9dcad0bc679e6e5be4d4c61555c728
Callistus <callistus@google.com>
2021-06-16 00:26:09 +0000

    Enable a new Release Notes notification for M93.
    Since this change will only affect M93 and above, we update the
    condition to show the notification if the last seen was < 93.
    
    Bug: b/191092696
    Change-Id: I74d0aa4b8ec8fd57e8a9a38fc8a9fd497560e85e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960300
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/master@{#892811}

--- chrome/browser/ash/release_notes/release_notes_storage.cc
+++ chrome/browser/ash/release_notes/release_notes_storage.cc
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// This stores the latest milestone with new Release Notes content. If the last
+// milestone the user has seen the notification is before this, a new
+// notification will be shown.
+constexpr int kLastChromeVersionWithReleaseNotes = 93;
 constexpr int kTimesToShowSuggestionChip = 3;
 
 int GetMilestone() {
@@ -85,10 +89,7 @@ bool ReleaseNotesStorage::ShouldNotify() {
         ChromeVersionService::GetVersion(profile_->GetPrefs()));
     last_milestone = profile_version.components()[0];
   }
-  // Hardcoding this to M91 as that should be the last release notes update that
-  // the current chrome version should see. There is not an update every
-  // milestone.
-  return last_milestone < 91;
+  return last_milestone < kLastChromeVersionWithReleaseNotes;
 }
 
 void ReleaseNotesStorage::MarkNotificationShown() {
