53414ca2be00b08282b6b7afe79fbc473ed719ca
Aliona DANGLA <alionadangla@google.com>
2021-06-16 18:45:54 +0000

    [iOS] Add sync settings button metric
    Add metric when a user taps on the advance sync settings button in the
    FRE sync screen stage.
    
    Bug: 1218855
    Change-Id: Ia9ac931e9b9c30fcc3cf91325ab1dea4fcb317c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2962712
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Cr-Commit-Position: refs/heads/master@{#893078}

--- ios/chrome/browser/ui/first_run/sync/sync_screen_coordinator.mm
+++ ios/chrome/browser/ui/first_run/sync/sync_screen_coordinator.mm
@@ -128,6 +128,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)showSyncSettings {
+  base::UmaHistogramEnumeration(
+      "FirstRun.Stage", first_run::kSyncScreenCompletionWithSyncSettings);
   [self.delegate skipAllAndShowSyncSettings];
 }
 
