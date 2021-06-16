805fbb92d335af49184d8b2aae1f8e8c69f0091f
Daniel Rubery <drubery@chromium.org>
2021-06-16 22:43:53 +0000

    Fix ClientSideDetectionHost disconnecting Mojo pipe on new render frame
    When a new render frame is created, we currently send it the CSD model
    in case it's in a new process. As implemented, this was disconnecting
    the old pipe and reusing. This is problematic if there are pending
    classifications that should return on the old pipe. The solution is to
    maintain a collection of mojo::Remotes in the ClientSideDetectionHost,
    one per active RenderFrameHost
    
    Bug: 1217128
    Change-Id: Ia25372be894a0dca141f6421b91da16c15962199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944723
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893197}

--- chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_unittest.cc
@@ -260,12 +260,28 @@ class ClientSideDetectionHostTestBase : public ChromeRenderViewHostTestHarness {
  public:
   typedef security_interstitials::UnsafeResource UnsafeResource;
 
+  class WebContentsObserver : public content::WebContentsObserver {
+   public:
+    WebContentsObserver(ClientSideDetectionHostTestBase* harness,
+                        content::WebContents* contents)
+        : content::WebContentsObserver(contents), harness_(harness) {}
+
+    void RenderFrameCreated(
+        content::RenderFrameHost* render_frame_host) override {
+      harness_->InitTestApi(render_frame_host);
+    }
+
+   private:
+    // The raw pointer is safe because `harness_` owns this.
+    ClientSideDetectionHostTestBase* harness_;
+  };
+
   explicit ClientSideDetectionHostTestBase(bool is_incognito)
       : is_incognito_(is_incognito) {}
 
-  void InitTestApi() {
+  void InitTestApi(content::RenderFrameHost* rfh) {
     service_manager::InterfaceProvider* remote_interfaces =
-        web_contents()->GetMainFrame()->GetRemoteInterfaces();
+        rfh->GetRemoteInterfaces();
 
     service_manager::InterfaceProvider::TestApi test_api(remote_interfaces);
 
@@ -278,6 +294,8 @@ class ClientSideDetectionHostTestBase : public ChromeRenderViewHostTestHarness {
   void SetUp() override {
     ChromeRenderViewHostTestHarness::SetUp();
 
+    observer_ = std::make_unique<WebContentsObserver>(this, web_contents());
+
     if (is_incognito_) {
       auto incognito_web_contents =
           content::WebContentsTester::CreateTestWebContents(
@@ -289,7 +307,7 @@ class ClientSideDetectionHostTestBase : public ChromeRenderViewHostTestHarness {
     // Initiate the connection to a (pretend) renderer process.
     NavigateAndCommit(GURL("about:blank"));
 
-    InitTestApi();
+    InitTestApi(web_contents()->GetMainFrame());
 
     // Inject service classes.
     csd_service_ = std::make_unique<MockClientSideDetectionService>();
@@ -404,6 +422,7 @@ class ClientSideDetectionHostTestBase : public ChromeRenderViewHostTestHarness {
   const bool is_incognito_;
   signin::IdentityTestEnvironment identity_test_env_;
   base::test::ScopedFeatureList feature_list_;
+  std::unique_ptr<WebContentsObserver> observer_;
 };
 
 class ClientSideDetectionHostTest : public ClientSideDetectionHostTestBase {
@@ -936,9 +955,6 @@ TEST_F(ClientSideDetectionHostTest, TestPreClassificationCheckTwoNavigations) {
   ExpectPreClassificationChecks(url2, &kFalse, &kFalse, &kFalse, &kFalse,
                                 &kFalse);
   NavigateAndKeepLoading(web_contents(), url2);
-  // Re-override the binder for PhishingDetector because navigation causes
-  // a new web InterfaceProvider to be created
-  InitTestApi();
   WaitAndCheckPreClassificationChecks();
 
   fake_phishing_detector_.CheckMessage(&url2);
--- components/safe_browsing/content/browser/client_side_detection_host.h
+++ components/safe_browsing/content/browser/client_side_detection_host.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/containers/flat_map.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "components/safe_browsing/content/browser/base_ui_manager.h"
@@ -101,6 +102,7 @@ class ClientSideDetectionHost : public content::WebContentsObserver {
   // From content::WebContentsObserver.
   void WebContentsDestroyed() override;
   void RenderFrameCreated(content::RenderFrameHost* render_frame_host) override;
+  void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
 
   // Used for testing.
   void set_ui_manager(BaseUIManager* ui_manager);
@@ -117,8 +119,8 @@ class ClientSideDetectionHost : public content::WebContentsObserver {
   // classifiers.
   void OnPhishingPreClassificationDone(bool should_classify);
 
-  // |verdict| is an encoded ClientPhishingRequest protocol message, |result| is
-  // the outcome of the renderer classification.
+  // |verdict| is an encoded ClientPhishingRequest protocol message, |result|
+  // is the outcome of the renderer classification.
   void PhishingDetectionDone(mojom::PhishingDetectorResult result,
                              const std::string& verdict);
 
@@ -157,12 +159,13 @@ class ClientSideDetectionHost : public content::WebContentsObserver {
     account_signed_in_callback_ = account_signed_in_callback;
   }
 
-  // Check if CSD can get an access Token. Should be enabled only for ESB users,
-  // who are signed in and not in incognito mode.
+  // Check if CSD can get an access Token. Should be enabled only for ESB
+  // users, who are signed in and not in incognito mode.
   bool CanGetAccessToken();
 
   // Set phishing model in PhishingDetector in renderers.
-  void SetPhishingModel();
+  void SetPhishingModel(
+      const mojo::Remote<mojom::PhishingDetector>& phishing_detector);
 
   // Send the client report to CSD server.
   void SendRequest(std::unique_ptr<ClientPhishingRequest> verdict,
@@ -172,7 +175,8 @@ class ClientSideDetectionHost : public content::WebContentsObserver {
   void OnGotAccessToken(std::unique_ptr<ClientPhishingRequest> verdict,
                         const std::string& access_token);
 
-  // This pointer may be nullptr if client-side phishing detection is disabled.
+  // This pointer may be nullptr if client-side phishing detection is
+  // disabled.
   ClientSideDetectionService* csd_service_;
   // The WebContents that the class is observing.
   content::WebContents* tab_;
@@ -184,8 +188,12 @@ class ClientSideDetectionHost : public content::WebContentsObserver {
   scoped_refptr<ShouldClassifyUrlRequest> classification_request_;
   // The current URL
   GURL current_url_;
-  // The currently active message pipe to the renderer PhishingDetector.
-  mojo::Remote<mojom::PhishingDetector> phishing_detector_;
+  // A map from the live RenderFrameHosts to their PhishingDetector. These
+  // correspond to the `phishing_detector_receiver_` in the
+  // PhishingClassifierDelegate.
+  base::flat_map<content::RenderFrameHost*,
+                 mojo::Remote<mojom::PhishingDetector>>
+      phishing_detectors_;
 
   // Records the start time of when phishing detection started.
   base::TimeTicks phishing_detection_start_time_;
