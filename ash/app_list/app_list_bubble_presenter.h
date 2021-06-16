3c4487c21d22715cec2b93047e11fc8b58cff82a
Alex Newcomer <newcomer@chromium.org>
2021-06-16 21:17:08 +0000

    cros: Create Apps.AppListBubbleShowSource
    Create the new "AppListShow" metric for AppListBubble, and add a test.
    
    Bug: 1220139
    Change-Id: I5e4ead417ea7e6f690d389ebdc86fdd785a5af7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965527
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893150}

--- ash/app_list/app_list_bubble_presenter.h
+++ ash/app_list/app_list_bubble_presenter.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/public/cpp/shelf_types.h"
 #include "ui/views/widget/widget_observer.h"
 
 namespace ash {
@@ -32,8 +33,9 @@ class ASH_EXPORT AppListBubblePresenter : public views::WidgetObserver {
   // Shows the bubble on the display with `display_id`.
   void Show(int64_t display_id);
 
-  // Shows or hides the bubble on the display with `display_id`.
-  void Toggle(int64_t display_id);
+  // Shows or hides the bubble on the display with `display_id`. Returns the
+  // appropriate ShelfAction to indicate whether the bubble was shown or hidden.
+  ShelfAction Toggle(int64_t display_id);
 
   // Closes and destroys the bubble.
   void Dismiss();
