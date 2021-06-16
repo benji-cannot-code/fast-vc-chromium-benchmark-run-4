ef1d88dfd05451e7caefdee8720389af27ddcc69
Alexander Dunaev <adunaev@igalia.com>
2021-06-16 03:14:02 +0000

    [ozone] Introduced display::Screen::SupportsServerSideDecorations().
    Some platforms may not support server side decorations, and when that is
    the case, the system setting that toggles the use of system title bar
    and borders makes no sense.  The issue with support of that feature is
    that it may not be known at compile time for some platforms.
    
    This patch proposes a new kind of Ozone platform properties which are
    known only at run time.  The new supports_server_side_window_decorations
    runtime property is introduced, and the settings WebUI can show or hide
    the button depending on value of that property.
    
    Bug: 1207965
    Change-Id: I149d9afaf50ea354f16b43e0931e0e98097983a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944873
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#892867}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -114,6 +114,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/accessibility_features.h"
 #endif
 
+#if defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "ui/display/screen.h"
+#endif
+
 #if defined(OS_WIN)
 #include "chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.h"
 #include "device/fido/win/webauthn_api.h"
@@ -129,6 +133,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(USE_NSS_CERTS)
 #include "chrome/browser/ui/webui/certificate_manager_localized_strings_provider.h"
 #endif
+
+#if defined(USE_OZONE)
+#include "ui/base/ui_base_features.h"
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 namespace settings {
 namespace {
 
@@ -370,6 +380,21 @@ void AddAppearanceStrings(content::WebUIDataSource* html_source,
                          zoom::GetPresetZoomFactorsAsJSON());
   html_source->AddBoolean("showReaderModeOption",
                           dom_distiller::OfferReaderModeInSettings());
+
+// TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
+// of lacros-chrome is complete.
+#if defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(USE_OZONE)
+  const bool show_custom_chrome_frame =
+      features::IsUsingOzonePlatform() &&
+      ui::OzonePlatform::GetInstance()
+          ->GetPlatformRuntimeProperties()
+          .supports_server_side_window_decorations;
+#else
+  const bool show_custom_chrome_frame = true;
+#endif
+  html_source->AddBoolean("showCustomChromeFrame", show_custom_chrome_frame);
+#endif
 }
 
 void AddClearBrowsingDataStrings(content::WebUIDataSource* html_source,
