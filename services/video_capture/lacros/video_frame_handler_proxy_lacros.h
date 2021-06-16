8669bc4bf218a454c9ec69107b01101aa7eb80ad
Henrik Boström <hbos@chromium.org>
2021-06-16 16:28:09 +0000

    Remove include of a header no longer being generated by mojo.
    The file that is used to generate this header was deleted in:
    https://chromium-review.googlesource.com/c/chromium/src/+/2871727
    
    We forgot to remove that include.
    
    Bug: chromium:1203616
    Change-Id: I1113596d7816f346661ea5365609fb47c41e92e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964121
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893034}

--- services/video_capture/lacros/video_frame_handler_proxy_lacros.h
+++ services/video_capture/lacros/video_frame_handler_proxy_lacros.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
-#include "services/video_capture/public/mojom/scoped_access_permission.mojom.h"
 #include "services/video_capture/public/mojom/video_frame_handler.mojom.h"
 
 namespace video_capture {
