ae25095438e09de3bd8df3a0939e47118d389620
Tomasz Wiszkowski <ender@google.com>
2021-06-16 01:33:59 +0000

    Clean up AutocompleteControllerAndroid
    Bug: 1138587
    Change-Id: I09dd67b243a126123fcecd94b11887e5f33d0b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964578
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#892835}

--- chrome/browser/android/omnibox/autocomplete_controller_android.h
+++ chrome/browser/android/omnibox/autocomplete_controller_android.h
@@ -8,23 +8,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/android/jni_android.h"
 #include "base/android/jni_weak_ref.h"
 #include "base/macros.h"
-#include "base/memory/singleton.h"
+#include "base/memory/weak_ptr.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/omnibox/browser/autocomplete_controller.h"
 #include "components/omnibox/browser/autocomplete_input.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/notification_service.h"
+#include "content/public/browser/browser_context.h"
 #include "third_party/metrics_proto/omnibox_event.pb.h"
 
 class AutocompleteController;
-struct AutocompleteMatch;
 class AutocompleteResult;
 class ChromeAutocompleteProviderClient;
 class Profile;
+
+namespace base {
+template <typename Type>
+struct DefaultSingletonTraits;
+}  // namespace base
 
 // The native part of the Java AutocompleteController class.
 class AutocompleteControllerAndroid : public AutocompleteController::Observer,
