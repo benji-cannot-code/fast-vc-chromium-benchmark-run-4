c96dea0ea347a1318c72ff85ad8dc7f442cbc39f
Matthew Mourgos <mmourgos@chromium.org>
2021-06-16 23:44:22 +0000

    CrOS AppList: Remove outline calculation for GhostImageView
    Now that app icons are all circular, the code for generating icon
    outlines is no longer needed. This CL removes the outline generation
    code and opts to instead just draw a circle for the ghost image of apps.
    
    Bug: 914094
    Change-Id: I5f8edf5cd043a958fc8ae4ad45fe46b8ad55d1bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964247
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893220}

--- ash/app_list/views/ghost_image_view.h
+++ ash/app_list/views/ghost_image_view.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 
 namespace gfx {
-class ImageSkia;
 class Point;
 }  // namespace gfx
 
@@ -59,9 +58,6 @@ class GhostImageView : public views::ImageView,
   // ui::ImplicitAnimationObserver overrides:
   void OnImplicitAnimationsCompleted() override;
 
-  // Returns an ImageSkia with just an outline of the input ImageSkia.
-  gfx::ImageSkia GetIconOutline(const gfx::ImageSkia& original_icon);
-
   // Whether the view is hiding.
   bool is_hiding_;
 
@@ -74,18 +70,14 @@ class GhostImageView : public views::ImageView,
   // Page this this view belongs to, used to calculate transition offset.
   int page_;
 
+  // The radius used for drawing the icons shown inside the folder ghost image.
+  int inner_icon_radius_;
+
   // Icon bounds used to determine size and placement of the GhostImageView.
   gfx::Rect icon_bounds_;
 
   // The number of items within the GhostImageView folder.
   absl::optional<size_t> num_items_;
-
-  // The outline of the dragged item's icon. Used as the ghost image.
-  gfx::ImageSkia outline_;
-
-  // The outlines of the top icons within a folder. Used for the folder ghost
-  // image.
-  std::vector<gfx::ImageSkia> inner_folder_icon_outlines_;
 
   // The origins of the top icons within a folder icon. Used for the folder
   // ghost image.
