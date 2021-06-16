0a7c1bc15293b426b0c4ece95bd6b9ea55b7218c
David Bienvenu <davidbienvenu@chromium.org>
2021-06-16 21:35:23 +0000

    unit_tests: fix mock warnings in SessionTests
    Use NiceMock to silence uninteresting mock function call warnings. This
    required removing final from some mock class definitions because
    NiceMock doesn't seem to work with final classes. This fixes > 20
    warnings.
    
    Also remove DISALLOW_COPY_AND_ASSIGN and fix lint warnings.
    
    Change-Id: I1ab641a1d25e78a9c853f065a2e8b0bab6a85f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965646
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893165}

--- components/mirroring/service/fake_video_capture_host.h
+++ components/mirroring/service/fake_video_capture_host.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mirroring {
 
-class FakeVideoCaptureHost final : public media::mojom::VideoCaptureHost {
+class FakeVideoCaptureHost : public media::mojom::VideoCaptureHost {
  public:
   explicit FakeVideoCaptureHost(
       mojo::PendingReceiver<media::mojom::VideoCaptureHost> receiver);
