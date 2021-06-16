497eb7dabc3cd3cd4e1bddc7e61ca9648f1f87aa
James Cook <jamescook@chromium.org>
2021-06-16 17:36:17 +0000

    chromeos: Make SearchBoxViewBase just a View, not a WidgetDelegateView
    Precursor to introducing a subclass of SearchBoxViewBase that does not
    want to be a WidgetDelegate.
    
    This also fixes a TODO for the keyboard shortcut viewer, which also
    uses SearchBoxViewBase but doesn't want it to be a WidgetDelegate.
    
    Bug: 1220218
    Test: ash_unittests, with and without address sanitizer
    Change-Id: I50e5dd700a3ce52d376bea8d76a50305726239f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965644
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893047}

--- ash/app_list/views/search_result_list_view.cc
+++ ash/app_list/views/search_result_list_view.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/background.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/widget/widget.h"
 
 namespace ash {
 
