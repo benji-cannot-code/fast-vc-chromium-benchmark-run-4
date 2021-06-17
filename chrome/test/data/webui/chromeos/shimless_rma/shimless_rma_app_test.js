4847554db2c9ad7c427be7a7c89fb32a56922f9d
Joon Ahn <joonbug@chromium.org>
2021-06-17 19:55:57 +0000

    shimless: enable navigation tests now that network page is up
    Bug: 1198187
    Change-Id: I920a1fcdb9d2ab86a5dac9177616458c039645dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967045
    Auto-Submit: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893545}

--- chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_app_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_app_test.js
@@ -93,11 +93,10 @@ export function shimlessRMAAppTest() {
 
     await clickNext();
 
-    // TODO(joonbug): enable when page is ready.
-    // const selectComponentPage =
-    //     component.shadowRoot.querySelector('onboarding-select-components');
-    // assertTrue(!!selectComponentPage);
-    // assertFalse(selectComponentPage.hidden);
+    const selectNetworkPage =
+        component.shadowRoot.querySelector('onboarding-network-page');
+    assertTrue(!!selectNetworkPage);
+    assertFalse(selectNetworkPage.hidden);
     assertTrue(!!initialPage);
     assertTrue(initialPage.hidden);
 
@@ -106,8 +105,8 @@ export function shimlessRMAAppTest() {
     await flushTasks();
 
     // components page should not be destroyed.
-    // assertTrue(!!selectComponentPage);
-    // assertTrue(selectComponentPage.hidden);
+    assertTrue(!!selectNetworkPage);
+    assertTrue(selectNetworkPage.hidden);
     assertFalse(initialPage.hidden);
   });
 
