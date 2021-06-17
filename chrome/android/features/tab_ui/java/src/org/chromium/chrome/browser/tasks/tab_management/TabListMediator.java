5a779335a5f986a9afa8d989deb4cb857d70ca8c
Wenyu Fu <wenyufu@google.com>
2021-06-17 01:22:06 +0000

    Clean up TabListFaviconProvider#getFaviconSync
    The icon passed in is never null. Clean up the dead code and simplify
    the interface.
    
    Bug: 1217141
    Change-Id: I788d70689ee799a6cf93a26f879bcf3a4d75337b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965587
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893262}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -1634,8 +1634,7 @@ class TabListMediator {
 
         // If there is an available icon, we fetch favicon synchronously; otherwise asynchronously.
         if (icon != null) {
-            Drawable drawable = mTabListFaviconProvider.getFaviconForUrlSync(
-                    pseudoTab.getUrl(), pseudoTab.isIncognito(), icon);
+            Drawable drawable = mTabListFaviconProvider.getFaviconForUrlSync(icon);
             mModel.get(modelIndex).model.set(TabProperties.FAVICON, drawable);
             return;
         }
