1fbe43d391d29cccb2b9ec7d105c12b44a1de3f8
Kuo Jen Wei <inker@google.com>
2021-06-17 03:50:40 +0000

    CCA: Judge PTZ enabled in photo mode with ENALBE_PTZ state
    For some board their builtin camera may claim for supporting PTZ from
    video track capability while the PTZ is currently only enabled on
    external camera. By judging from the ENALBE_PTZ state, whether camera is
    external facing can also be considered.
    
    Bug: b/190791718
    Test: Pass "tast run <DUT> camera.CCAUIResolutions" on kukui.
    Change-Id: I625f147d6f1b1313ce2d2ea3a062bda3af70f9e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964462
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893292}

--- chromeos/components/camera_app_ui/resources/js/views/camera/mode/photo.js
+++ chromeos/components/camera_app_ui/resources/js/views/camera/mode/photo.js
@@ -108,15 +108,6 @@ export class Photo extends ModeBase {
   }
 
   /**
-   * @return {boolean}
-   */
-  supportPTZ_() {
-    const {pan, tilt, zoom} =
-        this.stream_.getVideoTracks()[0].getCapabilities();
-    return pan !== undefined || tilt !== undefined || zoom !== undefined;
-  }
-
-  /**
    * @override
    */
   async start_() {
@@ -157,7 +148,7 @@ export class Photo extends ModeBase {
    * @return {!Promise<!Blob>}
    */
   async takePhoto_() {
-    if (this.supportPTZ_()) {
+    if (state.get(state.State.ENABLE_PTZ)) {
       // Workaround for b/184089334 on PTZ camera to use preview frame as
       // photo result.
       return this.handler_.getPreviewFrame();
