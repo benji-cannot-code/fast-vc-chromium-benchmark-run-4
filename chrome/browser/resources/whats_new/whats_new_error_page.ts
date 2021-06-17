0bfe4cb60f7c19d35f3e8fa173618d2fcd3a0e9a
rbpotter <rbpotter@chromium.org>
2021-06-17 07:18:33 +0000

    What's New: Add error page
    Bug: 1219381
    Change-Id: Ieb99c5a7c4f97e4a5128243770dc1b7d28bf759c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961853
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893330}

---
+++ chrome/browser/resources/whats_new/whats_new_error_page.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './strings.m.js';
+
+import 'chrome://resources/cr_elements/shared_style_css.m.js';
+import 'chrome://resources/cr_elements/shared_vars_css.m.js';
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+export class WhatsNewErrorPageElement extends PolymerElement {
+  static get is() {
+    return 'whats-new-error-page';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+}
+customElements.define(WhatsNewErrorPageElement.is, WhatsNewErrorPageElement);
--- chrome/browser/ui/webui/whats_new/whats_new_ui.cc
+++ chrome/browser/ui/webui/whats_new/whats_new_ui.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/whats_new_resources.h"
 #include "chrome/grit/whats_new_resources_map.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "ui/base/webui/web_ui_util.h"
 
@@ -22,6 +24,13 @@ content::WebUIDataSource* CreateWhatsNewUIHtmlSource(Profile* profile) {
   webui::SetupWebUIDataSource(
       source, base::make_span(kWhatsNewResources, kWhatsNewResourcesSize),
       IDR_WHATS_NEW_WHATS_NEW_HTML);
+  static constexpr webui::LocalizedString kStrings[] = {
+      {"pageCantBeReached", IDS_WHATS_NEW_PAGE_CANT_BE_REACHED},
+      {"reloadOrTryAgain", IDS_WHATS_NEW_RELOAD_TRY_AGAIN},
+      {"reloadButton", IDS_RELOAD},
+  };
+  source->AddLocalizedStrings(kStrings);
+
   return source;
 }
 
