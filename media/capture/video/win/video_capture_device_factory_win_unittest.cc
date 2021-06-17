f9fbf001be88a75805ff647e5025de500e69a7ac
Ilya Nikolaevskiy <ilnik@chromium.org>
2021-06-17 19:39:45 +0000

    Fix mediafoundation zero-copy capturer for cameras with no nv12
    - Don't use GMB pool if format is not NV12
    - Don't filter out non-nv12 buffers if there is no nv12 support
    
    Bug: 1214028
    Change-Id: If81ca656be8217aa2f989f329c8693d1d7340785
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2944882
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#893542}

--- media/capture/video/win/video_capture_device_factory_win_unittest.cc
+++ media/capture/video/win/video_capture_device_factory_win_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ksmedia.h>
 #include <mfapi.h>
 #include <mferror.h>
+#include <mfobjects.h>
 #include <stddef.h>
 #include <vidcap.h>
 #include <wrl.h>
@@ -64,6 +65,11 @@ const wchar_t* kDirectShowDeviceName5 = L"Dazzle";
 
 const wchar_t* kDirectShowDeviceId6 = L"\\\\?\\usb#vid_eb1a&pid_2860&mi_00";
 const wchar_t* kDirectShowDeviceName6 = L"Empia Device";
+
+constexpr int kWidth = 1280;
+constexpr int kHeight = 720;
+
+constexpr int kFps = 30;
 
 using iterator = std::vector<VideoCaptureDeviceInfo>::const_iterator;
 iterator FindDeviceInRange(iterator begin,
@@ -386,6 +392,11 @@ class StubMFMediaSource final : public StubDeviceInterface<IMFMediaSourceEx> {
       *object = AddReference(static_cast<IMFMediaSource*>(this));
       return S_OK;
     }
+    if (riid == _uuidof(IMFMediaEventGenerator)) {
+      *object = AddReference(static_cast<IMFMediaEventGenerator*>(this));
+      return S_OK;
+    }
+
     return StubDeviceInterface::QueryInterface(riid, object);
   }
   // IMFMediaEventGenerator
@@ -434,6 +445,15 @@ class StubMFMediaSource final : public StubDeviceInterface<IMFMediaSourceEx> {
           break;
       }
       hr = media_type->SetGUID(MF_MT_SUBTYPE, subType);
+      if (FAILED(hr)) {
+        return hr;
+      }
+      hr = MFSetAttributeSize(media_type.Get(), MF_MT_FRAME_SIZE, kWidth,
+                              kHeight);
+      if (FAILED(hr)) {
+        return hr;
+      }
+      hr = MFSetAttributeRatio(media_type.Get(), MF_MT_FRAME_RATE, kFps, 1);
       if (FAILED(hr)) {
         return hr;
       }
@@ -1580,6 +1600,7 @@ TEST_P(VideoCaptureDeviceFactoryMFWinTest,
                                   base::SysWideToUTF8(kMFDeviceId0));
   ASSERT_NE(it, devices_info.end());
   EXPECT_EQ(it->descriptor.display_name(), base::SysWideToUTF8(kMFDeviceName0));
+  EXPECT_FALSE(it->supported_formats.empty());
   for (size_t i = 0; i < it->supported_formats.size(); i++) {
     SCOPED_TRACE(i);
     EXPECT_EQ(it->supported_formats[i].pixel_format,
@@ -1590,9 +1611,11 @@ TEST_P(VideoCaptureDeviceFactoryMFWinTest,
                          base::SysWideToUTF8(kMFDeviceId1));
   ASSERT_NE(it, devices_info.end());
   EXPECT_EQ(it->descriptor.display_name(), base::SysWideToUTF8(kMFDeviceName1));
+  EXPECT_FALSE(it->supported_formats.empty());
   for (size_t i = 0; i < it->supported_formats.size(); i++) {
     SCOPED_TRACE(i);
-    EXPECT_EQ(it->supported_formats[i].pixel_format, PIXEL_FORMAT_I420);
+    EXPECT_EQ(it->supported_formats[i].pixel_format,
+              expected_pixel_format_for_nv12);
   }
 }
 
