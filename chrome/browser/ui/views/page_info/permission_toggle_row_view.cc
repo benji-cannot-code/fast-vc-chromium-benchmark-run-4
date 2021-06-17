04a2cb2d41e9cd1fb14e9ea74e7be69b1f36074c
Eugene Zemtsov <eugene@chromium.org>
2021-06-17 22:32:02 +0000

    Revert "Update icons in page info"
    This reverts commit 242829fbd44212a65e30dadcaf383c52b0a0eb65.
    
    Reason for revert: 
    PageInfoBubbleViewDialogBrowserTest tests are failing on Win10 Tests x64
    
    Example:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/56236/test-results
    
    Original change's description:
    > Update icons in page info
    >
    > Updated all icons to size 16px. Most of them are 16px by default, some
    > of them require size to set (permissions, certificate). There was a
    > comment about why permission icons were 18px: some of them have internal
    > padding which might make them look smaller. I checked with a UX
    > designer, it looks fine in 16px (screenshots attached in the bug).
    >
    > Added layout constant for page info icon size instead of constant field
    > in page_info_ui. Moved icons methods from page_info_ui to
    > PageInfoViewFactory since they are for desktop only and it makes sense
    > to keep them closer to usages. Also, this makes using layout constant
    > easier.
    >
    > Icons have Get*Icon methods if they:
    > 1. are used in multiple views
    > 2. require a size to set
    > 3. require logic to determine the right icon (permissions)
    >
    > Added new icon - secondary back arrow. kBackArrowIcon doesn't fit here
    > and looks too big compared to other icons.
    >
    > Screenshots: https://bugs.chromium.org/p/chromium/issues/detail?id=1188101#c25
    >
    > Bug: 1188101
    > Change-Id: Id72aa6055275d4b12d986fb39e34369aa163a5a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2949468
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Emily Stark <estark@chromium.org>
    > Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    > Cr-Commit-Position: refs/heads/master@{#893515}
    
    Bug: 1188101
    Change-Id: Ia335c135fe4e29252beb0d2eddc8dab9c5577457
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2970729
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893588}

--- chrome/browser/ui/views/page_info/permission_toggle_row_view.cc
+++ chrome/browser/ui/views/page_info/permission_toggle_row_view.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/page_info/permission_toggle_row_view.h"
 
-#include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/page_info/chrome_page_info_ui_delegate.h"
 #include "chrome/browser/ui/views/accessibility/non_accessible_image_view.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
@@ -30,7 +29,7 @@ PermissionToggleRowView::PermissionToggleRowView(
   SetUseDefaultFillLayout(true);
   row_view_ = AddChildView(std::make_unique<PageInfoRowView>());
   row_view_->SetTitle(PageInfoUI::PermissionTypeToUIString(permission.type));
-  row_view_->SetIcon(PageInfoViewFactory::GetPermissionIcon(permission));
+  row_view_->SetIcon(PageInfoUI::GetPermissionIcon(permission));
 
   // Add extra details as sublabel.
   std::u16string detail = delegate->GetPermissionDetail(permission.type);
@@ -64,7 +63,7 @@ void PermissionToggleRowView::AddObserver(
 void PermissionToggleRowView::PermissionChanged(
     const PageInfo::PermissionInfo& permission) {
   // Change the permission icon to reflect the selected setting.
-  row_view_->SetIcon(PageInfoViewFactory::GetPermissionIcon(permission));
+  row_view_->SetIcon(PageInfoUI::GetPermissionIcon(permission));
 
   for (PermissionSelectorRowObserver& observer : observer_list_) {
     observer.OnPermissionChanged(permission);
@@ -120,8 +119,8 @@ void PermissionToggleRowView::InitForUserSource() {
           base::Unretained(this)),
       vector_icons::kSubmenuArrowIcon);
   views::InstallCircleHighlightPathGenerator(subpage_button.get());
-  const int icon_size = GetLayoutConstant(PAGE_INFO_ICON_SIZE);
-  subpage_button->SetMinimumImageSize({icon_size, icon_size});
+  subpage_button->SetMinimumImageSize({PageInfoViewFactory::kVectorIconSize,
+                                       PageInfoViewFactory::kVectorIconSize});
 
   // If type isn't supported by the PermissionManager, don't show the button
   // that opens subpage. Instead, use a spacer view to align this row with
@@ -155,6 +154,8 @@ void PermissionToggleRowView::InitForManagedSource(
   row_view_->AddControl(std::move(state_label));
 
   auto managed_icon = std::make_unique<NonAccessibleImageView>();
-  managed_icon->SetImage(PageInfoViewFactory::GetManagedIcon());
+  managed_icon->SetImage(ui::ImageModel::FromVectorIcon(
+      vector_icons::kBusinessIcon, ui::NativeTheme::kColorId_DefaultIconColor,
+      PageInfoViewFactory::kVectorIconSize));
   row_view_->AddControl(std::move(managed_icon));
 }
