9788d3993fb23a85bf498e05d69f8fa792eaee51
Ken Rockot <rockot@google.com>
2021-06-17 02:12:42 +0000

    Remove unused legacy IPC APIs from GPU & Utility processes
    GPU and Utility processes no longer use any legacy IPCs. This removes
    their process hosts' implementation of IPC::Sender and IPC::Listener
    interfaces, as well as from GpuChildThread.
    
    This also omits initialization of the main legacy IPC Channel for
    GPU processes, as it's no longer used.
    
    Bug: 993189, 1196476
    Change-Id: Ie0e74d09ef1b6bbcd4ebbc2d482dcaebb79481b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967049
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893274}

--- content/browser/utility_process_host.cc
+++ content/browser/utility_process_host.cc
@@ -95,13 +95,6 @@ base::WeakPtr<UtilityProcessHost> UtilityProcessHost::AsWeakPtr() {
   return weak_ptr_factory_.GetWeakPtr();
 }
 
-bool UtilityProcessHost::Send(IPC::Message* message) {
-  if (!StartProcess())
-    return false;
-
-  return process_->Send(message);
-}
-
 void UtilityProcessHost::SetSandboxType(
     sandbox::policy::SandboxType sandbox_type) {
   sandbox_type_ = sandbox_type;
@@ -340,10 +333,6 @@ bool UtilityProcessHost::StartProcess() {
                                        GetV8SnapshotFilesToPreload(), true);
   }
 
-  return true;
-}
-
-bool UtilityProcessHost::OnMessageReceived(const IPC::Message& message) {
   return true;
 }
 
