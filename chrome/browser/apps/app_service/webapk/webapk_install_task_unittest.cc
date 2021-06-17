577884638d3ab65b6ebf8db7a1f77944f7629e1a
Tim Sergeant <tsergeant@chromium.org>
2021-06-17 07:59:43 +0000

    [CrOS WebAPKs] Add a timeout for requests to the WebAPK minter
    Requests to the WebAPK minter now have a 60 second window to complete.
    In future CLs we'll add metrics so we can check how often this timeout
    is hit.
    
    Bug: 1198433
    Change-Id: Ib41a8440102e0f6f3c8a50ab1b500552ba5be335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967663
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893341}

--- chrome/browser/apps/app_service/webapk/webapk_install_task_unittest.cc
+++ chrome/browser/apps/app_service/webapk/webapk_install_task_unittest.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "chrome/browser/apps/app_service/app_service_test.h"
 #include "chrome/browser/apps/app_service/webapk/webapk_prefs.h"
-#include "chrome/browser/extensions/extension_service.h"
-#include "chrome/browser/extensions/test_extension_system.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_test.h"
 #include "chrome/browser/web_applications/components/web_application_info.h"
 #include "chrome/browser/web_applications/test/test_web_app_provider.h"
@@ -25,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/test/fake_webapk_instance.h"
 #include "components/webapk/webapk.pb.h"
 #include "content/public/test/browser_task_environment.h"
+#include "net/test/embedded_test_server/default_handlers.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
@@ -112,18 +111,11 @@ class WebApkInstallTaskTest : public testing::Test {
   void SetUp() override {
     testing::Test::SetUp();
 
-    extensions::TestExtensionSystem* extension_system(
-        static_cast<extensions::TestExtensionSystem*>(
-            extensions::ExtensionSystem::Get(&profile_)));
-    extension_service_ = extension_system->CreateExtensionService(
-        base::CommandLine::ForCurrentProcess(), base::FilePath(), false);
-    extension_service_->Init();
-
     app_service_test_.SetUp(&profile_);
 
     auto* const provider = web_app::TestWebAppProvider::Get(&profile_);
-    provider->SetRunSubsystemStartupTasks(true);
-    provider->Start();
+    provider->SkipAwaitingExtensionSystem();
+    web_app::test::AwaitStartWebAppProviderAndSubsystems(profile());
 
     arc_test_.SetUp(&profile_);
     auto* arc_bridge_service =
@@ -135,6 +127,7 @@ class WebApkInstallTaskTest : public testing::Test {
 
     test_server_.RegisterRequestHandler(base::BindRepeating(
         &WebApkInstallTaskTest::HandleWebApkRequest, base::Unretained(this)));
+    net::test_server::RegisterDefaultHandlers(&test_server_);
     ASSERT_TRUE(test_server_.Start());
 
     GURL server_url = test_server_.GetURL(kServerPath);
@@ -167,9 +160,13 @@ class WebApkInstallTaskTest : public testing::Test {
 
   std::unique_ptr<net::test_server::HttpResponse> HandleWebApkRequest(
       const net::test_server::HttpRequest& request) {
-    last_webapk_request_ = std::make_unique<webapk::WebApk>();
-    last_webapk_request_->ParseFromString(request.content);
-    return webapk_response_builder_.Run();
+    if (request.relative_url == kServerPath) {
+      last_webapk_request_ = std::make_unique<webapk::WebApk>();
+      last_webapk_request_->ParseFromString(request.content);
+      return webapk_response_builder_.Run();
+    }
+
+    return nullptr;
   }
 
   TestingProfile* profile() { return &profile_; }
@@ -182,12 +179,13 @@ class WebApkInstallTaskTest : public testing::Test {
 
   webapk::WebApk* last_webapk_request() { return last_webapk_request_.get(); }
 
+  net::EmbeddedTestServer* test_server() { return &test_server_; }
+
  private:
   content::BrowserTaskEnvironment task_environment_;
   TestingProfile profile_;
   apps::AppServiceTest app_service_test_;
   ArcAppTest arc_test_;
-  extensions::ExtensionService* extension_service_ = nullptr;
 
   net::EmbeddedTestServer test_server_;
 
@@ -304,4 +302,23 @@ TEST_F(WebApkInstallTaskTest, FailedArcInstall) {
   ASSERT_EQ(fake_webapk_instance()->handled_packages()[0],
             "org.chromium.webapk.some_package");
   ASSERT_EQ(apps::webapk_prefs::GetWebApkAppIds(profile()).size(), 0);
+}
+
+TEST_F(WebApkInstallTaskTest, MinterTimeout) {
+  auto app_id =
+      web_app::test::InstallWebApp(profile(), BuildDefaultWebAppInfo());
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kWebApkServerUrl, test_server()->GetURL("/slow?1000").spec());
+
+  bool install_success;
+  apps::WebApkInstallTask install_task(profile(), app_id);
+  install_task.SetTimeoutForTesting(base::TimeDelta::FromMilliseconds(100));
+  base::RunLoop run_loop;
+  install_task.Start(base::BindLambdaForTesting([&](bool success) {
+    install_success = success;
+    run_loop.Quit();
+  }));
+  run_loop.Run();
+
+  ASSERT_FALSE(install_success);
 }
