5214f24149cdc9f147bc406d6973ab1ca2e53473
Adam Rice <ricea@chromium.org>
2021-06-17 09:41:10 +0000

    WebTransport: Hook up outgoing datagram attributes
    Connect outgoingMaxAge and outgoingHighWaterMark attributes on
    DatagramDuplexStream to WebTransport, so that setting them works as
    expected.
    
    To support the existing non-standard datagramWritableHighWaterMark
    WebTransport option, its value is passed to DatagramDuplexStream on
    construction. DatagramUnderlyingSink now reads the live value from the
    DatagramDuplexStream object, so there are no synchronisation issues.
    
    Changes to outgoingHighWaterMark are forwarded to the non-standard
    WebTransport setDatagramWritableQueueExpirationDuration() method, which
    will likely be renamed and removed from the IDL before shipping. This
    results in a Mojo call to the network service where the expiration is
    actually implemented.
    
    In order to test the call to
    setDatagramWritableQueueExpirationDuration() we now need a properly
    initialised WebTransport object. To do this, factor out some code from
    the BidirectionalStream test ScopedWebTransport class into a new
    "TestWebTransportCreator" class in test_utils.h. Then reuse that code in
    the DatagramDuplexStream tests.
    
    Bug: 1011392
    Change-Id: Ied395f157c80bc580dbb0fa30050666bb6e3a09a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960704
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893364}

--- third_party/blink/renderer/modules/webtransport/web_transport_test.cc
+++ third_party/blink/renderer/modules/webtransport/web_transport_test.cc
@@ -551,11 +551,13 @@ TEST_F(WebTransportTest, GarbageCollection) {
 
   WeakPersistent<WebTransport> web_transport;
 
+  auto* isolate = scope.GetIsolate();
+
   {
     // The streams created when creating a WebTransport create some v8 handles.
     // To ensure these are collected, we need to create a handle scope. This is
     // not a problem for garbage collection in normal operation.
-    v8::HandleScope handle_scope(scope.GetIsolate());
+    v8::HandleScope handle_scope(isolate);
     web_transport = CreateAndConnectSuccessfully(scope, "https://example.com");
   }
 
@@ -565,7 +567,10 @@ TEST_F(WebTransportTest, GarbageCollection) {
 
   EXPECT_TRUE(web_transport);
 
-  web_transport->close(nullptr);
+  {
+    v8::HandleScope handle_scope(isolate);
+    web_transport->close(nullptr);
+  }
 
   test::RunPendingTasks();
 
@@ -930,12 +935,14 @@ TEST_F(WebTransportTest, SendStreamGarbageCollection) {
   WeakPersistent<WebTransport> web_transport;
   WeakPersistent<SendStream> send_stream;
 
+  auto* isolate = scope.GetIsolate();
+
   {
     // The streams created when creating a WebTransport or SendStream create
     // some v8 handles. To ensure these are collected, we need to create a
     // handle scope. This is not a problem for garbage collection in normal
     // operation.
-    v8::HandleScope handle_scope(scope.GetIsolate());
+    v8::HandleScope handle_scope(isolate);
 
     web_transport = CreateAndConnectSuccessfully(scope, "https://example.com");
     send_stream = CreateSendStreamSuccessfully(scope, web_transport);
@@ -946,7 +953,10 @@ TEST_F(WebTransportTest, SendStreamGarbageCollection) {
   EXPECT_TRUE(web_transport);
   EXPECT_TRUE(send_stream);
 
-  web_transport->close(nullptr);
+  {
+    v8::HandleScope handle_scope(isolate);
+    web_transport->close(nullptr);
+  }
 
   test::RunPendingTasks();
 
