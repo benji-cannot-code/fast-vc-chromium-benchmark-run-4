e065e484a59dbe6a33271ad2302e7279a3ab63c2
Chris Mumford <cmumford@chromium.org>
2021-06-17 00:32:32 +0000

    [Bluetooth] Ensure pairing operation does not outlive delegate.
    The device::BluetoothDevice::Pair() contract specifies that the pairing
    delegate must be valid throughout the pairing operation. Ensure that
    WebBluetoothPairingManager (which is the delegate) cannot be used
    after being deleted.
    
    Bug: 960258
    Change-Id: I65bce9ef5379f8f2cb83baf08a38850cf3e72cfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2943163
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893246}

--- content/browser/bluetooth/web_bluetooth_pairing_manager.h
+++ content/browser/bluetooth/web_bluetooth_pairing_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/containers/flat_set.h"
 #include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
 #include "device/bluetooth/bluetooth_device.h"
@@ -50,11 +51,13 @@ class CONTENT_EXPORT WebBluetoothPairingManager
  private:
   void OnReadCharacteristicValuePairSuccess(
       std::string characteristic_instance_id,
+      blink::WebBluetoothDeviceId device_id,
       blink::mojom::WebBluetoothService::RemoteCharacteristicReadValueCallback
           read_callback);
 
   void OnReadCharacteristicValuePairFailure(
       std::string characteristic_instance_id,
+      blink::WebBluetoothDeviceId device_id,
       int num_pair_attempts,
       blink::mojom::WebBluetoothService::RemoteCharacteristicReadValueCallback
           read_callback,
@@ -71,6 +74,9 @@ class CONTENT_EXPORT WebBluetoothPairingManager
   void ConfirmPasskey(device::BluetoothDevice* device,
                       uint32_t passkey) override;
   void AuthorizePairing(device::BluetoothDevice* device) override;
+
+  // The device IDs currently in the pairing process.
+  base::flat_set<blink::WebBluetoothDeviceId> pending_pair_device_ids_;
 
   // The purpose of WebBluetoothPairingManagerDelegate is to support
   // this class. Currently the WebBluetoothPairingManagerDelegate implementation
