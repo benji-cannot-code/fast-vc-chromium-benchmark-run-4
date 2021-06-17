62468f03a56738c8ba846ea77c9f190962581719
Lei Zhang <thestig@chromium.org>
2021-06-17 18:11:41 +0000

    Swap from base/stl_util.h to cxx17_backports.h in url/ .cc files.
    base::size() has been moved to base/cxx17_backports.h, so .cc files that
    use base::size(), but no other function from base/stl_util.h, can
    directly include base/cxx17_backports.h and not base/stl_util.h.
    
    Bug: 1210983
    Change-Id: I7b0b96300f74c481c09349146ba2eb518248a4af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967350
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893507}

--- url/url_canon_icu_unittest.cc
+++ url/url_canon_icu_unittest.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include "base/cxx17_backports.h"
 #include "base/logging.h"
-#include "base/stl_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/icu/source/common/unicode/ucnv.h"
 #include "url/url_canon.h"
--- url/url_canon_unittest.cc
+++ url/url_canon_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <errno.h>
 #include <stddef.h>
 
-#include "base/stl_util.h"
+#include "base/cxx17_backports.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/gtest_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
