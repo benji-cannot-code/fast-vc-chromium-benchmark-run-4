e46614ddb6d104ac08af42bf80d52a81f8090b40
Renato Silva <rrsilva@google.com>
2021-06-17 14:00:42 +0000

    OOBE - Split buttons into different files
    Splits OOBE buttons into separate files and creates dedicated
    resource paths with which they are now being served.
    Tested OOBE and the Assistant in-session UI locally.
    
    Bug: 1184731
    Change-Id: I76e79fdd4375f3d0e3c9e9828eb6eadcb7040526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2844560
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893408}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_optin.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_optin.js
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 HTMLImports.whenReady(() => {
 // <include src="../login/components/behaviors/multi_step_behavior.js">
 // <include src="../login/components/oobe_types.js">
-// <include src="../login/components/oobe_buttons.js">
 // <include src="assistant_optin_flow.js">
 // <include src="browser_proxy.js">
 
