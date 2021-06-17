d2376d67a57ca40a1869581ff84cbca00ff15276
Kent Tamura <tkent@chromium.org>
2021-06-17 15:17:38 +0000

    SVG Text NG: Add two TRACE_EVENT0()
    Bug: 1179585
    Change-Id: Ibb5b3e5ca11533d7b3e877ffda3fcbc043523e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966632
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893443}

--- third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_algorithm.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_algorithm.h"
 
+#include "base/trace_event/trace_event.h"
 #include "third_party/blink/renderer/core/layout/ng/svg/svg_inline_node_data.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_inline_text.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_text_path.h"
@@ -76,6 +77,7 @@ NGSvgTextLayoutAlgorithm::NGSvgTextLayoutAlgorithm(NGInlineNode node,
 void NGSvgTextLayoutAlgorithm::Layout(
     const String& ifc_text_content,
     NGFragmentItemsBuilder::ItemWithOffsetList& items) {
+  TRACE_EVENT0("blink", "NGSvgTextLayoutAlgorithm::Layout");
   // https://svgwg.org/svg2-draft/text.html#TextLayoutAlgorithm
   //
   // The major difference from the algorithm in the specification:
