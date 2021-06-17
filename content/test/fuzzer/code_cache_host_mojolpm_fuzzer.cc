a6a7d0be7d55dec18750d670037b8df8af3dff0e
Victor Costan <pwnall@chromium.org>
2021-06-17 14:51:12 +0000

    Cache Storage: Fix null pointer crash in code_cache_host_mojolpm_fuzzer.
    The fuzzer crashes due to accessing a null pointer in two places.
    
    1) In content::BlobStorageContextWrapper::BlobStorageContextWrapper(),
       because CacheStorageControlWrapper was given a mojo::NullRemote()
       instead of a valid storage::mojom::BlobStorageContext remote.
    
    2) (reported in the attached bug) In
       base::FeatureList::GetFeatureOverridesImpl(), while attempting to
       start the utility process that hosts the Network Service.
    
    This CL fixes the first crash by passing a valid
    storage::mojom::BlobStorageContext remote, obtained from
    content::TestBrowserContext.
    
    This CL fixes the second crash by instructing //content to run the
    Network Service in-process. Since we need to set up //content to run the
    Network Service in-process, we might as well set up the Storage Service
    to run in-process. This will be needed when Cache Storage moves into the
    Storage Service, and preserves consistency with
    content::UnitTestTestSuite::UnitTestTestSuite(), which provided the
    inspiration for running the //content services in-process.
    
    Bug: 1216277
    Change-Id: Ia3cccc235362a2e8fbcbf9315222c65170224f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957097
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893431}

--- content/test/fuzzer/code_cache_host_mojolpm_fuzzer.cc
+++ content/test/fuzzer/code_cache_host_mojolpm_fuzzer.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <stdint.h>
+
 #include <utility>
 
 #include "base/at_exit.h"
@@ -15,8 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_timeouts.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread.h"
+#include "content/browser/blob_storage/chrome_blob_storage_context.h"  // [nogncheck]
 #include "content/browser/code_cache/generated_code_cache_context.h"  // [nogncheck]
+#include "content/browser/network_service_instance_impl.h"       // [nogncheck]
 #include "content/browser/renderer_host/code_cache_host_impl.h"  // [nogncheck]
+#include "content/browser/storage_partition_impl.h"              // [nogncheck]
 #include "content/browser/storage_partition_impl_map.h"          // [nogncheck]
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -63,6 +67,10 @@ class ContentFuzzerEnvironment {
     logging::SetMinLogLevel(logging::LOG_FATAL);
     mojo::core::Init();
     base::i18n::InitializeICU();
+
+    content::ForceCreateNetworkServiceDirectlyForTesting();
+    content::StoragePartitionImpl::ForceInProcessStorageServiceForTesting();
+
     fuzzer_thread_.StartAndWaitForTesting();
   }
 
@@ -218,7 +226,8 @@ void CodeCacheHostTestcase::SetUpOnUIThread() {
           browser_context_->GetDefaultStoragePartition()
               ->GetQuotaManager()
               ->proxy(),
-          /*blob_storage_context=*/mojo::NullRemote());
+          content::ChromeBlobStorageContext::GetRemoteFor(
+              browser_context_.get()));
 
   generated_code_cache_context_ =
       base::MakeRefCounted<content::GeneratedCodeCacheContext>();
@@ -344,7 +353,7 @@ void CodeCacheHostTestcase::AddCodeCacheHost(
 // Helper function to keep scheduling fuzzer actions on the current runloop
 // until the testcase has completed, and then quit the runloop.
 void NextAction(CodeCacheHostTestcase* testcase,
-                base::RepeatingClosure quit_closure) {
+                base::OnceClosure quit_closure) {
   if (!testcase->IsFinished()) {
     testcase->NextAction();
     GetFuzzerTaskRunner()->PostTask(
