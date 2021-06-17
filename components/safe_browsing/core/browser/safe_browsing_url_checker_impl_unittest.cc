3613c5628011b9b491bbe64d183d29d1e9e3871d
Colin Blundell <blundell@chromium.org>
2021-06-17 13:57:46 +0000

    [SafeBrowsing] Remove thread_utils.h from UrlChecker unittest
    thread_utils.h is a utility that allows //components/safe_browsing/core
    to use //content's and //ios/web's threading primitives via different
    implementations that are conditionally included in the GN target. We
    are going to eliminate this utility as part of eliminating
    //components/safe_browsing/core's incorrect dependences on
    //components/safe_browsing/{content, ios}.
    
    The UrlCheckerImpl unittest posts tasks on the IO thread via
    thread_utils.h. AFAICT this is inessential to the flow of the tests.
    This CL changes these tasks to be posted on the current thread.
    
    Bug: 1216341
    Change-Id: I2533566ae2fc5d656f4938cd44eb42e83e773766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961225
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893404}

--- components/safe_browsing/core/browser/safe_browsing_url_checker_impl_unittest.cc
+++ components/safe_browsing/core/browser/safe_browsing_url_checker_impl_unittest.cc
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "components/safe_browsing/core/browser/safe_browsing_token_fetcher.h"
 #include "components/safe_browsing/core/browser/url_checker_delegate.h"
 #include "components/safe_browsing/core/common/test_task_environment.h"
-#include "components/safe_browsing/core/common/thread_utils.h"
 #include "components/safe_browsing/core/db/test_database_manager.h"
 #include "components/safe_browsing/core/db/v4_protocol_manager_util.h"
 #include "components/safe_browsing/core/proto/csd.pb.h"
@@ -52,10 +52,9 @@ class MockSafeBrowsingDatabaseManager : public TestSafeBrowsingDatabaseManager {
       return true;
     }
     if (!urls_delayed_callback_[url]) {
-      GetTaskRunner(ThreadID::IO)
-          ->PostTask(FROM_HERE,
-                     base::BindOnce(
-                         &MockSafeBrowsingDatabaseManager::OnCheckBrowseURLDone,
+      base::ThreadTaskRunnerHandle::Get()->PostTask(
+          FROM_HERE,
+          base::BindOnce(&MockSafeBrowsingDatabaseManager::OnCheckBrowseURLDone,
                          this, gurl, client));
     } else {
       // If delayed callback is set to true, store the client in |urls_client_|.
@@ -94,10 +93,9 @@ class MockSafeBrowsingDatabaseManager : public TestSafeBrowsingDatabaseManager {
     std::string url = gurl.spec();
     DCHECK(base::Contains(urls_delayed_callback_, url));
     DCHECK_EQ(true, urls_delayed_callback_[url]);
-    GetTaskRunner(ThreadID::IO)
-        ->PostTask(FROM_HERE,
-                   base::BindOnce(
-                       &MockSafeBrowsingDatabaseManager::OnCheckBrowseURLDone,
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE,
+        base::BindOnce(&MockSafeBrowsingDatabaseManager::OnCheckBrowseURLDone,
                        this, gurl, urls_client_[url]));
   }
 
