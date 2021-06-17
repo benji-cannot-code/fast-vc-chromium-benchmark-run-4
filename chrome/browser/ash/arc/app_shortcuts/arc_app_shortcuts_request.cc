1c711bfef562f0870c2b9cbb5ce31fe62057b037
Maggie Cai <mxcai@google.com>
2021-06-17 01:40:13 +0000

    [Lacros] Implement GetMenuModel interface for Lacros web apps.
    This CL:
    1. Add crosapi interface to get the necessary menu items from
    lacros-chrome
    2. Create the menu model in the ash-chrome web app publisher
    
    BUG=1144877
    
    Change-Id: Iff4b198f6e60b2b2b84414483d53775e3bf03c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2955259
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893268}

--- chrome/browser/ash/arc/app_shortcuts/arc_app_shortcuts_request.cc
+++ chrome/browser/ash/arc/app_shortcuts/arc_app_shortcuts_request.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_service/app_icon_factory.h"
+#include "chrome/browser/apps/app_service/menu_item_constants.h"
 #include "chrome/browser/apps/app_service/menu_util.h"
 #include "chrome/browser/ash/arc/icon_decode_request.h"
 #include "chrome/common/chrome_features.h"
--- chrome/browser/web_applications/app_service/web_apps_chromeos.cc
+++ chrome/browser/web_applications/app_service/web_apps_chromeos.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/launch_utils.h"
+#include "chrome/browser/apps/app_service/menu_item_constants.h"
 #include "chrome/browser/apps/app_service/menu_util.h"
 #include "chrome/browser/ash/arc/arc_util.h"
 #include "chrome/browser/ash/arc/arc_web_contents_data.h"
