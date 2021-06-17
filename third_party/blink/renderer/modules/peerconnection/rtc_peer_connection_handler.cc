a032f2815c1921b6e8e01ddde46bf774f49820e1
Yuki Shiino <yukishiino@chromium.org>
2021-06-17 06:14:27 +0000

    webrtc: Fix invalid access to 'optional' IDL dict member
    RTCSessionDescriptionInit::type() is an optional IDL dictionary
    member, and it's invalid to access it when !hasType().
    
    Fixes it so that it never accesses the dictionary member when
    it's missing.
    
    Bug: 1220404
    Change-Id: I3179869ebc6d68776b945731f8ee0c651f88f0a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966435
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893313}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.cc
@@ -610,8 +610,11 @@ ParsedSessionDescription::ParsedSessionDescription(const String& sdp_type,
 // static
 ParsedSessionDescription ParsedSessionDescription::Parse(
     const RTCSessionDescriptionInit* session_description_init) {
-  ParsedSessionDescription temp(session_description_init->type(),
-                                session_description_init->sdp());
+  ParsedSessionDescription temp(
+      session_description_init->hasType()
+          ? session_description_init->type().AsString()
+          : String(),
+      session_description_init->sdp());
   temp.DoParse();
   return temp;
 }
@@ -2777,4 +2780,5 @@ void RTCPeerConnectionHandler::ResetUMAStats() {
   ice_connection_checking_start_ = base::TimeTicks();
   memset(ice_state_seen_, 0, sizeof(ice_state_seen_));
 }
+
 }  // namespace blink
