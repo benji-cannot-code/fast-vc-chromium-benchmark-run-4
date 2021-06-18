477d1ba18a6f916cd2fce7abffa7ec0d464f2500
Scott Violet <sky@chromium.org>
2021-06-18 00:15:47 +0000

    cc: plumbs debug-name to ResourcePool
    This way the debug-name can be shown in memory dumps.
    
    BUG=1220096
    TEST=none
    
    Change-Id: Ie3ee6dbbc6ca5c64fefdbd3ce8ec57c2023b037b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956403
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893611}

--- cc/raster/raster_source.h
+++ cc/raster/raster_source.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "base/containers/flat_map.h"
@@ -126,6 +127,9 @@ class CC_EXPORT RasterSource : public base::RefCountedThreadSafe<RasterSource> {
 
   size_t* max_op_size_hint() { return &max_op_size_hint_; }
 
+  void set_debug_name(const std::string& name) { debug_name_ = name; }
+  const std::string& debug_name() const { return debug_name_; }
+
  protected:
   // RecordingSource is the only class that can create a raster source.
   friend class RecordingSource;
@@ -164,6 +168,8 @@ class CC_EXPORT RasterSource : public base::RefCountedThreadSafe<RasterSource> {
   const gfx::Size size_;
   const int slow_down_raster_scale_factor_for_debug_;
   const float recording_scale_factor_;
+  // Used for debugging and tracing.
+  std::string debug_name_;
 };
 
 }  // namespace cc
--- cc/resources/resource_pool.h
+++ cc/resources/resource_pool.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/containers/circular_deque.h"
@@ -206,9 +207,11 @@ class CC_EXPORT ResourcePool : public base::trace_event::MemoryDumpProvider {
   ResourcePool& operator=(const ResourcePool&) = delete;
 
   // Tries to reuse a resource. If none are available, makes a new one.
-  InUsePoolResource AcquireResource(const gfx::Size& size,
-                                    viz::ResourceFormat format,
-                                    const gfx::ColorSpace& color_space);
+  InUsePoolResource AcquireResource(
+      const gfx::Size& size,
+      viz::ResourceFormat format,
+      const gfx::ColorSpace& color_space,
+      const std::string& debug_name = std::string());
 
   // Tries to acquire the resource with |previous_content_id| for us in partial
   // raster. If successful, this function will retun the invalidated rect which
@@ -218,7 +221,8 @@ class CC_EXPORT ResourcePool : public base::trace_event::MemoryDumpProvider {
       const gfx::Rect& new_invalidated_rect,
       uint64_t previous_content_id,
       gfx::Rect* total_invalidated_rect,
-      const gfx::ColorSpace& raster_color_space);
+      const gfx::ColorSpace& raster_color_space,
+      const std::string& debug_name = std::string());
 
   // Gives the InUsePoolResource a |resource_id_for_export()| in order to allow
   // exporting of the resource to the display compositor. This must be called
@@ -323,6 +327,9 @@ class CC_EXPORT ResourcePool : public base::trace_event::MemoryDumpProvider {
                       const viz::ClientResourceProvider* resource_provider,
                       bool is_free) const;
 
+    void set_debug_name(const std::string& name) { debug_name_ = name; }
+    const std::string& debug_name() const { return debug_name_; }
+
    private:
     const size_t unique_id_;
     const gfx::Size size_;
@@ -349,6 +356,9 @@ class CC_EXPORT ResourcePool : public base::trace_event::MemoryDumpProvider {
     // out by ResourcePool, to be filled in by the client. Is destroyed on the
     // compositor thread.
     std::unique_ptr<SoftwareBacking> software_backing_;
+
+    // Used for debugging and tracing.
+    std::string debug_name_;
   };
 
   // Callback from the ResourceProvider to notify when an exported PoolResource
