b06b0a4c0adea48fd14c9997fe7f6ca01f578c10
Takashi Toyoshima <toyoshim@chromium.org>
2021-06-18 18:43:57 +0000

    Prerender: Do not make the host ready if the navigation fails
    This patch adds a check at DidFinishNavigation to cancel the
    prerendering rather than to make the host ready if the
    navigation hasn't committed due to an error.
    
    Currently, we've tried cancelling the prerender host on any failures,
    but just in case, this patch cancels the prerendering with a specific
    final status code, FinalStatus::kNavigationNotCommitted, there so
    that we can check if the existing cancellation calls are enough.
    
    Bug: 1132746
    Change-Id: I1ce66379a2e4d4ef3ee5adbbc8ff787774689d1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966364
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893907}

--- content/browser/prerender/prerender_host.cc
+++ content/browser/prerender/prerender_host.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "base/trace_event/trace_conversion_helper.h"
+#include "content/browser/prerender/prerender_host_registry.h"
 #include "content/browser/renderer_host/frame_tree.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/renderer_host/navigation_controller_impl.h"
@@ -249,9 +250,11 @@ PrerenderHost::PrerenderHost(blink::mojom::PrerenderAttributesPtr attributes,
   CreatePageHolder(*static_cast<WebContentsImpl*>(web_contents));
 }
 
-// TODO(https://crbug.com/1132746): Abort ongoing prerendering and notify the
-// mojo capability controller in the destructor.
 PrerenderHost::~PrerenderHost() {
+  // Stop observing here. Otherwise, destructing members may lead
+  // DidFinishNavigation call after almost everything being destructed.
+  Observe(nullptr);
+
   for (auto& observer : observers_)
     observer.OnHostDestroyed();
 
@@ -320,13 +323,19 @@ void PrerenderHost::DidFinishNavigation(NavigationHandle* navigation_handle) {
   if (navigation_handle->GetFrameTreeNodeId() != frame_tree_node_id_)
     return;
 
+  // Stop observing the events about the prerendered contents.
+  Observe(nullptr);
+
+  // Cancel if the initial prerender navigation hasn't committed.
+  if (!navigation_handle->HasCommitted()) {
+    Cancel(FinalStatus::kNavigationNotCommitted);
+    return;
+  }
+
   // The prerendered contents are considered ready for activation when the
   // main frame navigation reaches DidFinishNavigation.
   DCHECK(!is_ready_for_activation_);
   is_ready_for_activation_ = true;
-
-  // Stop observing the events about the prerendered contents.
-  Observe(nullptr);
 }
 
 std::unique_ptr<BackForwardCacheImpl::Entry> PrerenderHost::Activate(
@@ -409,6 +418,19 @@ absl::optional<int64_t> PrerenderHost::GetInitialNavigationId() const {
 void PrerenderHost::SetInitialNavigationId(int64_t navigation_id) {
   DCHECK(!initial_navigation_id_.has_value());
   initial_navigation_id_ = navigation_id;
+}
+
+void PrerenderHost::Cancel(FinalStatus status) {
+  // Already cancelled.
+  if (final_status_)
+    return;
+
+  RenderFrameHostImpl* host = PrerenderHost::GetPrerenderedMainFrameHost();
+  DCHECK(host);
+  PrerenderHostRegistry* registry =
+      host->delegate()->GetPrerenderHostRegistry();
+  DCHECK(registry);
+  registry->CancelHost(frame_tree_node_id_, status);
 }
 
 }  // namespace content
