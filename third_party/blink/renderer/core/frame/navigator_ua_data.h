89e3e1a87e1fede746a0f9d19fc26a4066108fac
Aaron Tagliaboschi <aarontag@chromium.org>
2021-06-18 00:19:13 +0000

    [UA-CH] Add toJSON method to NavigatorUAData
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/dafizBGwWMw
    
    Bug: 1201806
    Change-Id: Ic34d4de8ef347942ce25482c1dd2bc7d61d0ec6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2849455
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893618}

--- third_party/blink/renderer/core/frame/navigator_ua_data.h
+++ third_party/blink/renderer/core/frame/navigator_ua_data.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_navigator_ua_brand_version.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_object_builder.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -39,6 +40,7 @@ class NavigatorUAData : public ScriptWrappable, ExecutionContextClient {
   const HeapVector<Member<NavigatorUABrandVersion>>& brands() const;
   bool mobile() const;
   ScriptPromise getHighEntropyValues(ScriptState*, Vector<String>&) const;
+  ScriptValue toJSON(ScriptState*) const;
 
   void Trace(Visitor* visitor) const final;
 
