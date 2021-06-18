3aef486bd9713ee8ca5f5a0a901c7159767ad2c0
Fergal Daly <fergal@chromium.org>
2021-06-18 05:33:41 +0000

    Delete event related elements of WebSchedulerTrackedFeature.
    They do not block BFCache and are not used for anything else.
    
    Change-Id: I42138130564893e99ac3d944f187b0d25ebace9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960288
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893705}

--- content/browser/renderer_host/back_forward_cache_metrics_browsertest.cc
+++ content/browser/renderer_host/back_forward_cache_metrics_browsertest.cc
@@ -39,9 +39,6 @@ namespace content {
 
 namespace {
 
-constexpr uint64_t kPageShowFeature = static_cast<uint64_t>(
-    blink::scheduler::WebSchedulerTrackedFeature::kPageShowEventListener);
-
 constexpr uint64_t kRequestedGeolocationPermissionFeature =
     static_cast<uint64_t>(blink::scheduler::WebSchedulerTrackedFeature::
                               kRequestedGeolocationPermission);
@@ -457,9 +454,8 @@ IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest, Features_MainFrame) {
   // ukm::SourceId id2 = ToSourceId(navigation_ids_[1]);
   ukm::SourceId id3 = ToSourceId(navigation_ids_[2]);
 
-  EXPECT_THAT(
-      GetFeatureUsageMetrics(&recorder),
-      testing::ElementsAre(FeatureUsage{id3, 1 << kPageShowFeature, 0, 0}));
+  EXPECT_THAT(GetFeatureUsageMetrics(&recorder),
+              testing::ElementsAre(FeatureUsage{id3, 0, 0, 0}));
 }
 
 IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest,
@@ -484,9 +480,8 @@ IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest,
   // ukm::SourceId id2 = ToSourceId(navigation_ids_[1]);
   ukm::SourceId id3 = ToSourceId(navigation_ids_[2]);
 
-  EXPECT_THAT(
-      GetFeatureUsageMetrics(&recorder),
-      testing::ElementsAre(FeatureUsage{id3, 1 << kPageShowFeature, 0, 0}));
+  EXPECT_THAT(GetFeatureUsageMetrics(&recorder),
+              testing::ElementsAre(FeatureUsage{id3, 0, 0, 0}));
 }
 
 // https://crbug.com/1219373 fails with BFCache field trial testing config.
@@ -518,9 +513,8 @@ IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest,
   // ukm::SourceId id3 = ToSourceId(navigation_ids_[2]);
   ukm::SourceId id4 = ToSourceId(navigation_ids_[3]);
 
-  EXPECT_THAT(
-      GetFeatureUsageMetrics(&recorder),
-      testing::ElementsAre(FeatureUsage{id4, 0, 1 << kPageShowFeature, 0}));
+  EXPECT_THAT(GetFeatureUsageMetrics(&recorder),
+              testing::ElementsAre(FeatureUsage{id4, 0, 0, 0}));
 }
 
 IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest,
@@ -547,9 +541,8 @@ IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest,
   // ukm::SourceId id3 = ToSourceId(navigation_ids_[2]);
   ukm::SourceId id4 = ToSourceId(navigation_ids_[3]);
 
-  EXPECT_THAT(
-      GetFeatureUsageMetrics(&recorder),
-      testing::ElementsAre(FeatureUsage{id4, 0, 1 << kPageShowFeature, 0}));
+  EXPECT_THAT(GetFeatureUsageMetrics(&recorder),
+              testing::ElementsAre(FeatureUsage{id4, 0, 0, 0}));
 }
 
 // https://crbug.com/1219373 fails with BFCache field trial testing config.
@@ -583,9 +576,8 @@ IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest,
   // ukm::SourceId id3 = ToSourceId(navigation_ids_[2]);
   ukm::SourceId id4 = ToSourceId(navigation_ids_[3]);
 
-  EXPECT_THAT(
-      GetFeatureUsageMetrics(&recorder),
-      testing::ElementsAre(FeatureUsage{id4, 0, 0, 1 << kPageShowFeature}));
+  EXPECT_THAT(GetFeatureUsageMetrics(&recorder),
+              testing::ElementsAre(FeatureUsage{id4, 0, 0, 0}));
 }
 
 IN_PROC_BROWSER_TEST_F(BackForwardCacheMetricsBrowserTest, DedicatedWorker) {
