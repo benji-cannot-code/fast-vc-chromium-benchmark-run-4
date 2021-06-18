ce19924dc852c1cb801173058cef56ffb864f383
rbpotter <rbpotter@chromium.org>
2021-06-18 21:33:39 +0000

    Chrome What's New: Add to main menu / help section on branded builds
    - Add What's New to the Help section of the main menu
    - Add a "New" badge (and associated feature flag) for this entry
    
    Bug: 1219381
    Change-Id: I1fa054be378b86ffc2655498f5027367adf39caf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967564
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893954}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -225,6 +225,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_TOGGLE_COMMANDER     40261
 #define IDC_SHOW_KALEIDOSCOPE          40262
 #define IDC_CHROME_TIPS                40263
+#define IDC_CHROME_WHATS_NEW           40264
 
 // Spell-check
 // Insert any additional suggestions before _LAST; these have to be consecutive.
--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -197,6 +197,7 @@ const char kChromeUIWebFooterExperimentURL[] =
 const char kChromeUIWelcomeHost[] = "welcome";
 const char kChromeUIWelcomeURL[] = "chrome://welcome/";
 const char kChromeUIWhatsNewHost[] = "whats-new";
+const char kChromeUIWhatsNewURL[] = "chrome://whats-new/";
 
 #if defined(OS_WIN)
 // TODO(crbug.com/1003960): Remove when issue is resolved.
