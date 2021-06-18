8a5761207aee49862018b367529c683b128df419
Justin Cohen <justincohen@google.com>
2021-06-18 12:40:23 +0000

    ios: Correct error page virtualURL and user agent for native restore.
    Bug: 1221250
    Change-Id: I94d527696267f38eb8018a10b6552e7f523fd1cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2970444
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893792}

--- ios/web/navigation/wk_navigation_util.mm
+++ ios/web/navigation/wk_navigation_util.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/values.h"
 #include "ios/web/common/features.h"
+#import "ios/web/navigation/crw_error_page_helper.h"
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/web_client.h"
 #include "net/base/escape.h"
@@ -89,6 +90,12 @@ bool URLNeedsUserAgentType(const GURL& url) {
 
   if (url.SchemeIs(url::kFileScheme) && IsRestoreSessionUrl(url))
     return true;
+
+  if (url.SchemeIs(url::kFileScheme) &&
+      base::FeatureList::IsEnabled(web::features::kUseJSForErrorPage) &&
+      [CRWErrorPageHelper isErrorPageFileURL:url]) {
+    return true;
+  }
 
   if (url.SchemeIs(url::kFileScheme))
     return false;
