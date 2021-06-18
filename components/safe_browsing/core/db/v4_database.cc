e8b32f27cf98b38707c7907459794909ec7359df
Colin Blundell <blundell@chromium.org>
2021-06-18 08:57:31 +0000

    [SafeBrowsing] Remove thread_utils.h from V4Database
    thread_utils.h is a utility that allows //components/safe_browsing/core
    to use //content's and //ios/web's threading primitives via different
    implementations that are conditionally included in the GN target. We
    are going to eliminate this utility as part of eliminating
    //components/safe_browsing/core's incorrect dependences on
    //components/safe_browsing/{content, ios}.
    
    This CL removes usage of thread_utils.h in
    //components/safe_browsing/core's V4Database. This class executes
    on the DB and IO thread and uses thread_utils.h to verify that certain
    methods are called on the IO thread. We replace those DCHECKs with
    usage of a ThreadChecker. However, it is slightly tricky to do so as
    the object is created on the DB thread and never explicitly told which
    thread is the IO thread. To handle this, we add a
    V4Database::InitializeOnIOThread() method that the client is responsible
    for invoking on the IO thread.
    
    Bug: 1216341
    Change-Id: I439d90eb8a0bdaf74f918471b0976b639c2cec6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966500
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893746}

--- components/safe_browsing/core/db/v4_database.cc
+++ components/safe_browsing/core/db/v4_database.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/task_runner_util.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "components/safe_browsing/core/common/thread_utils.h"
 #include "components/safe_browsing/core/proto/webui.pb.h"
 
 using base::TimeTicks;
@@ -133,13 +132,25 @@ V4Database::V4Database(
       db_task_runner_(db_task_runner),
       pending_store_updates_(0) {
   DCHECK(db_task_runner->RunsTasksInCurrentSequence());
+  // This method executes on the DB task runner, whereas |io_thread_checker_| is
+  // meant to verify methods that should execute on the IO thread. Detach that
+  // thread checker here; it will be bound to the IO thread in
+  // InitializeOnIOThread().
+  DETACH_FROM_THREAD(io_thread_checker_);
+}
+
+void V4Database::InitializeOnIOThread() {
+  // This invocation serves to bind |io_thread_checker_| to the IO thread after
+  // its having been detached from the DB task runner in this object's
+  // constructor.
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
 }
 
 // static
 void V4Database::Destroy(std::unique_ptr<V4Database> v4_database) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
   V4Database* v4_database_raw = v4_database.release();
   if (v4_database_raw) {
+    DCHECK_CALLED_ON_VALID_THREAD(v4_database_raw->io_thread_checker_);
     v4_database_raw->weak_factory_on_io_.InvalidateWeakPtrs();
     v4_database_raw->db_task_runner_->DeleteSoon(FROM_HERE, v4_database_raw);
   }
@@ -152,7 +163,7 @@ V4Database::~V4Database() {
 void V4Database::ApplyUpdate(
     std::unique_ptr<ParsedServerResponse> parsed_server_response,
     DatabaseUpdatedCallback db_updated_callback) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   DCHECK(!pending_store_updates_);
   DCHECK(db_updated_callback_.is_null());
 
@@ -193,7 +204,7 @@ void V4Database::ApplyUpdate(
 
 void V4Database::UpdatedStoreReady(ListIdentifier identifier,
                                    std::unique_ptr<V4Store> new_store) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   DCHECK(pending_store_updates_);
   if (new_store) {
     (*store_map_)[identifier].swap(new_store);
@@ -219,7 +230,7 @@ std::unique_ptr<StoreStateMap> V4Database::GetStoreStateMap() {
 
 bool V4Database::AreAnyStoresAvailable(
     const StoresToCheck& stores_to_check) const {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   for (const ListIdentifier& identifier : stores_to_check) {
     if (IsStoreAvailable(identifier))
       return true;
@@ -229,7 +240,7 @@ bool V4Database::AreAnyStoresAvailable(
 
 bool V4Database::AreAllStoresAvailable(
     const StoresToCheck& stores_to_check) const {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   for (const ListIdentifier& identifier : stores_to_check) {
     if (!IsStoreAvailable(identifier))
       return false;
@@ -241,7 +252,7 @@ void V4Database::GetStoresMatchingFullHash(
     const FullHash& full_hash,
     const StoresToCheck& stores_to_check,
     StoreAndHashPrefixes* matched_store_and_hash_prefixes) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   matched_store_and_hash_prefixes->clear();
   for (const ListIdentifier& identifier : stores_to_check) {
     if (!IsStoreAvailable(identifier))
@@ -258,7 +269,7 @@ void V4Database::GetStoresMatchingFullHash(
 
 void V4Database::ResetStores(
     const std::vector<ListIdentifier>& stores_to_reset) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   for (const ListIdentifier& identifier : stores_to_reset) {
     store_map_->at(identifier)->Reset();
   }
@@ -266,7 +277,7 @@ void V4Database::ResetStores(
 
 void V4Database::VerifyChecksum(
     DatabaseReadyForUpdatesCallback db_ready_for_updates_callback) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
 
   // Make a threadsafe copy of store_map_ w/raw pointers that we can hand to
   // the DB thread. The V4Stores ptrs are guaranteed to be valid because their
@@ -288,7 +299,7 @@ void V4Database::VerifyChecksum(
 void V4Database::OnChecksumVerified(
     DatabaseReadyForUpdatesCallback db_ready_for_updates_callback,
     const std::vector<ListIdentifier>& stores_to_reset) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   std::move(db_ready_for_updates_callback).Run(stores_to_reset);
 }
 
