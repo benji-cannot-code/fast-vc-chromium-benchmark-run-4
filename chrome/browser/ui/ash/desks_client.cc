7c3992dfffad78a2d0f201b9c34e3b11b2d1f75e
Sammie Quon <sammiequon@chromium.org>
2021-06-18 02:39:05 +0000

    desks_template: LaunchDeskTemplate skeleton.
    This patch supports launching a template with no windows.
    
    Bug: 1213712
    Test: added test
    Change-Id: Iafc9a109860c71b75d6cbaa7b69d8058e396e8a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945953
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893665}

--- chrome/browser/ui/ash/desks_client.cc
+++ chrome/browser/ui/ash/desks_client.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/desks_client.h"
 
 #include "ash/public/cpp/desks_helper.h"
+#include "base/bind.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 
@@ -19,6 +20,7 @@ DesksClient::DesksClient() : desks_helper_(ash::DesksHelper::Get()) {
   DCHECK(!g_desks_client_instance);
   g_desks_client_instance = this;
 }
+
 DesksClient::~DesksClient() {
   DCHECK_EQ(this, g_desks_client_instance);
   g_desks_client_instance = nullptr;
@@ -41,4 +43,27 @@ std::unique_ptr<ash::DeskTemplate> DesksClient::CaptureActiveDeskAsTemplate() {
     return nullptr;
 
   return desks_helper_->CaptureActiveDeskAsTemplate(user_profile->GetPath());
+}
+
+void DesksClient::LaunchDeskTemplate(double template_uuid) {
+  // TODO: Find the saved template associated with `template_uuid` from storage.
+  if (!launch_template_for_test_ ||
+      launch_template_for_test_->uuid() != template_uuid) {
+    return;
+  }
+
+  // Launch the windows as specified in the template to a new desk.
+  desks_helper_->CreateAndActivateNewDeskForTemplate(
+      launch_template_for_test_->desk_name(),
+      base::BindOnce(&DesksClient::OnCreateAndActivateNewDesk,
+                     weak_ptr_factory_.GetWeakPtr(),
+                     launch_template_for_test_.get()));
+}
+
+void DesksClient::OnCreateAndActivateNewDesk(ash::DeskTemplate* desk_template,
+                                             bool on_create_activate_success) {
+  if (!on_create_activate_success)
+    return;
+
+  // TODO: Launch windows.
 }
--- chrome/browser/ui/ash/desks_client.h
+++ chrome/browser/ui/ash/desks_client.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback.h"
+#include "base/memory/weak_ptr.h"
+
 namespace ash {
 class DesksHelper;
 class DeskTemplate;
@@ -31,8 +34,26 @@ class DesksClient {
   // template will not be saved to storage.
   std::unique_ptr<ash::DeskTemplate> CaptureActiveDeskAsTemplate();
 
+  // Launches the desk template with `template_uuid` as a new desk. `desk_uuid`
+  // is the unique id for an existing desk template.
+  void LaunchDeskTemplate(double template_uuid);
+
  private:
-  ash::DesksHelper* const desks_helper_ = nullptr;
+  friend class DesksClientTest;
+
+  // Callback function that is ran after a desk is created, or has failed to be
+  // created.
+  void OnCreateAndActivateNewDesk(ash::DeskTemplate* desk_template,
+                                  bool on_create_activate_success);
+
+  // Convenience pointer to the desks helper which is `ash::DesksController`.
+  // Guaranteed to be not null for the duration of `this`.
+  ash::DesksHelper* const desks_helper_;
+
+  // A test only template for testing `LaunchDeskTemplate`.
+  std::unique_ptr<ash::DeskTemplate> launch_template_for_test_;
+
+  base::WeakPtrFactory<DesksClient> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_ASH_DESKS_CLIENT_H_
