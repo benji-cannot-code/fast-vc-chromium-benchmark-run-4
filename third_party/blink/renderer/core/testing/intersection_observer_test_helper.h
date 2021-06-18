8922585a42bf1ed0e838c7f9f782fd94ca45d0d7
Stefan Zager <szager@chromium.org>
2021-06-18 22:45:35 +0000

    Add LocalFrameUkmAggregator::IterativeTimer
    This is meant to optimize IntersectionObserver on platforms with an
    expensive Now() function (e.g., arm32 Android).
    
    Bug: 1215345
    Change-Id: If556f0e130189a6c151480c647c5e825a103559e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2937341
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894009}

--- third_party/blink/renderer/core/testing/intersection_observer_test_helper.h
+++ third_party/blink/renderer/core/testing/intersection_observer_test_helper.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_TESTING_INTERSECTION_OBSERVER_TEST_HELPER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_TESTING_INTERSECTION_OBSERVER_TEST_HELPER_H_
 
+#include "third_party/blink/renderer/core/frame/local_frame_ukm_aggregator.h"
 #include "third_party/blink/renderer/core/intersection_observer/intersection_observer_delegate.h"
 #include "third_party/blink/renderer/core/intersection_observer/intersection_observer_entry.h"
 
@@ -13,13 +14,16 @@ namespace blink {
 
 class TestIntersectionObserverDelegate : public IntersectionObserverDelegate {
  public:
-  explicit TestIntersectionObserverDelegate(Document& document)
-      : document_(document), call_count_(0) {}
+  explicit TestIntersectionObserverDelegate(
+      Document& document,
+      LocalFrameUkmAggregator::MetricId metric_id =
+          LocalFrameUkmAggregator::kJavascriptIntersectionObserver)
+      : document_(document), metric_id_(metric_id), call_count_(0) {}
   // TODO(szager): Add tests for the synchronous delivery code path. There is
   // already some indirect coverage by unit tests exercising features that rely
   // on it, but we should have some direct coverage in here.
   LocalFrameUkmAggregator::MetricId GetUkmMetricId() const override {
-    return LocalFrameUkmAggregator::kJavascriptIntersectionObserver;
+    return metric_id_;
   }
   IntersectionObserver::DeliveryBehavior GetDeliveryBehavior() const override {
     return IntersectionObserver::kPostTaskToDeliver;
@@ -54,6 +58,7 @@ class TestIntersectionObserverDelegate : public IntersectionObserverDelegate {
 
  private:
   Member<Document> document_;
+  LocalFrameUkmAggregator::MetricId metric_id_;
   HeapVector<Member<IntersectionObserverEntry>> entries_;
   int call_count_;
 };
