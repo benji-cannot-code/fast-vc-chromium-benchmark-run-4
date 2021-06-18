2a7fc45ffc09e6a1d2f7671c840fcb68ce741caf
Elaine Chien <elainechien@chromium.org>
2021-06-18 00:23:30 +0000

    Python3Migration: chrome/common* PRESUBMITs
    Bug: 1211980, 1211979, 1211981
    Change-Id: Ic961199007a116f40d8735acfd00a50c5baf2e48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2968795
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893625}

--- chrome/common/extensions/PRESUBMIT.py
+++ chrome/common/extensions/PRESUBMIT.py
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
 """
+
+USE_PYTHON3 = True
+
 import fnmatch
 import os
 import re
