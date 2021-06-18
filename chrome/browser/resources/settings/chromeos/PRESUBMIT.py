40129dde94402807f8bf055426b266ab59018a54
Elaine Chien <elainechien@chromium.org>
2021-06-18 19:12:43 +0000

    Python3Migration: chrome/browser/resources* PRESUBMITs
    There are still a couple of files under chrome/browser/resources* that
    are blocked by crbug.com/1212120
    
    AX-Relnotes: n/a No user facing changes.
    
    Bug: 1211971, 1211973, 1211974, 1211975
    Change-Id: I1dc83fece259847edeaca0b65f92d4da1144cba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2969644
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893922}

--- chrome/browser/resources/settings/chromeos/PRESUBMIT.py
+++ chrome/browser/resources/settings/chromeos/PRESUBMIT.py
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 """Presubmit script for OS Settings."""
 
+USE_PYTHON3 = True
+
 import re
 
 
