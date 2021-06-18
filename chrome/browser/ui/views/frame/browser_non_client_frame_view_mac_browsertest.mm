18af6c9d6382e5dc8e36b6079104521c7f1ef9eb
Hassan Talat <hatalat@microsoft.com>
2021-06-18 02:06:34 +0000

    dpwas: Clear values when a WindowControlsOverlay App opens in browser
    navigator.windowControlsOverlay.getBoundingClientRect() is only meant to
    return valid values for a WCO PWA. This CL clears the bounding rect
    values of this javascript api when a WCO PWA is opened in browser from
    '...' -> 'Open in Chromium'.
    
    Bug: 1213988
    Change-Id: I4329e410091e724e553d04c5e6da41801ea366cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964989
    Commit-Queue: Hassan Talat <hatalat@microsoft.com>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893656}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_mac_browsertest.mm
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_mac_browsertest.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
 #include "build/build_config.h"
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -610,4 +611,22 @@ IN_PROC_BROWSER_TEST_F(WebAppBrowserFrameViewMacWindowControlsOverlayTest,
   browser_view_->ToggleWindowControlsOverlayEnabled();
   EXPECT_FALSE(browser_view_->IsWindowControlsOverlayEnabled());
   EXPECT_TRUE(browser_view_->AppUsesWindowControlsOverlay());
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppBrowserFrameViewMacWindowControlsOverlayTest,
+                       OpenInChrome) {
+  InstallAndLaunchWebAppWithWindowControlsOverlay();
+
+  // Toggle overlay on, and validate JS API reflects the expected
+  // values.
+  ToggleWindowControlsOverlayEnabledAndWait();
+
+  // Validate non-empty bounds are being sent.
+  EXPECT_TRUE(GetWindowControlOverlayVisibility());
+
+  chrome::ExecuteCommand(browser_view_->browser(), IDC_OPEN_IN_CHROME);
+
+  // Validate bounds are cleared.
+  EXPECT_EQ(false, EvalJs(browser()->tab_strip_model()->GetActiveWebContents(),
+                          "window.navigator.windowControlsOverlay.visible"));
 }
--- chrome/browser/ui/views/frame/glass_browser_frame_view_browsertest_win.cc
+++ chrome/browser/ui/views/frame/glass_browser_frame_view_browsertest_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -574,4 +575,23 @@ IN_PROC_BROWSER_TEST_F(WebAppGlassBrowserFrameViewWindowControlsOverlayTest,
   browser_view_->ToggleWindowControlsOverlayEnabled();
   EXPECT_FALSE(browser_view_->IsWindowControlsOverlayEnabled());
   EXPECT_TRUE(browser_view_->AppUsesWindowControlsOverlay());
+}
+
+IN_PROC_BROWSER_TEST_F(WebAppGlassBrowserFrameViewWindowControlsOverlayTest,
+                       OpenInChrome) {
+  if (!InstallAndLaunchWebAppWithWindowControlsOverlay())
+    return;
+
+  // Toggle overlay on, and validate JS API reflects the expected
+  // values.
+  ToggleWindowControlsOverlayEnabledAndWait();
+
+  // Validate non-empty bounds are being sent.
+  EXPECT_TRUE(GetWindowControlOverlayVisibility());
+
+  chrome::ExecuteCommand(browser_view_->browser(), IDC_OPEN_IN_CHROME);
+
+  // Validate bounds are cleared.
+  EXPECT_EQ(false, EvalJs(browser()->tab_strip_model()->GetActiveWebContents(),
+                          "window.navigator.windowControlsOverlay.visible"));
 }
