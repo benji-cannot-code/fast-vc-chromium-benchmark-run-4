563d4570ae9227bffad39bc86754a2a9df0f62d5
Melissa Zhang <melzhang@google.com>
2021-06-18 07:12:42 +0000

    [WebAPKs] Add update unittests and fix bugs.
    This CL adds unittests for WebAPK updates. It also fixes the bugs:
    - missing return after callback call.
    - Diffing optional variables instead of their values.
    
    Bug: 1214695
    Change-Id: I149c7a67d58eddc344e71f66582576d9f451706f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2971666
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893723}

--- components/arc/test/fake_webapk_instance.h
+++ components/arc/test/fake_webapk_instance.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ARC_TEST_FAKE_WEBAPK_INSTANCE_H_
 #define COMPONENTS_ARC_TEST_FAKE_WEBAPK_INSTANCE_H_
 
+#include <unordered_set>
+
 #include "components/arc/mojom/webapk.mojom.h"
 
 namespace arc {
@@ -28,7 +30,7 @@ class FakeWebApkInstance : public mojom::WebApkInstance {
   void GetWebApkInfo(const std::string& package_name,
                      GetWebApkInfoCallback callback) override;
 
-  const std::vector<std::string>& handled_packages() {
+  const std::unordered_set<std::string>& handled_packages() {
     return handled_packages_;
   }
 
@@ -36,11 +38,17 @@ class FakeWebApkInstance : public mojom::WebApkInstance {
     install_result_ = result;
   }
 
+  void set_web_apk_info(arc::mojom::WebApkInfoPtr web_apk_info) {
+    web_apk_info_ = std::move(web_apk_info);
+  }
+
  private:
-  std::vector<std::string> handled_packages_;
+  std::unordered_set<std::string> handled_packages_;
 
   arc::mojom::WebApkInstallResult install_result_ =
       arc::mojom::WebApkInstallResult::kSuccess;
+
+  arc::mojom::WebApkInfoPtr web_apk_info_;
 };
 
 }  // namespace arc
