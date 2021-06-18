78528972c611ca0bdecb9d49cac13e799349a21f
Xi Han <hanxi@google.com>
2021-06-18 18:34:29 +0000

    Support multiple observers in MostVisitedSites.
    This is a precursor CL of showing NTP tiles in Omnibox in Start surface
    Finale (crbug.com/1164995). Currently, a new fetch of data can only be
    triggered when the observer is set, and any observer has to create a new
    MostVisitedSites instance to be able to observe. This isn't memory usage
    wise.
    
    This CL changes SetMostVisitedURLsObserver to AddMostVisitedURLsObserver
    which allows multiple observers which uses the same |max_sites_num| to
    register to the same MostVisitedSites instance. The MostVisitedSites
    starts observing any sources' changes when the first observer is added.
    
    Bug: 1218649
    Change-Id: I7317f83896742f4fc66610e4e6fea6719db86e6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953883
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893902}

--- components/ntp_tiles/webui/ntp_tiles_internals_message_handler.cc
+++ components/ntp_tiles/webui/ntp_tiles_internals_message_handler.cc
@@ -112,7 +112,7 @@ void NTPTilesInternalsMessageHandler::HandleRegisterForEvents(
   suggestions_status_.clear();
   popular_sites_json_.clear();
   most_visited_sites_ = client_->MakeMostVisitedSites();
-  most_visited_sites_->SetMostVisitedURLsObserver(this, site_count_);
+  most_visited_sites_->AddMostVisitedURLsObserver(this, site_count_);
   SendSourceInfo();
 }
 
@@ -171,7 +171,7 @@ void NTPTilesInternalsMessageHandler::HandleUpdate(
   // TODO(sfiera): refresh MostVisitedSites without re-creating it, as soon as
   // that will pick up changes to the Popular Sites overrides.
   most_visited_sites_ = client_->MakeMostVisitedSites();
-  most_visited_sites_->SetMostVisitedURLsObserver(this, site_count_);
+  most_visited_sites_->AddMostVisitedURLsObserver(this, site_count_);
   SendSourceInfo();
 }
 
