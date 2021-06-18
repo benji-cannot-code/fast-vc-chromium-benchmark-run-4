392f7c1dc96a3af1c51365622576baf3510f5862
Austin Sullivan <asully@chromium.org>
2021-06-18 15:03:10 +0000

    Revert "[Nearby] Move callbacks in stream parser for clarity."
    This reverts commit 5e9a21c54e489e89a027910e9f2df7a9c378a5bb.
    
    Reason for revert: https://crrev.com/c/2913016
    
    Original change's description:
    > [Nearby] Move callbacks in stream parser for clarity.
    >
    > This change extends the rewrite of the stream parser and moves the
    > callbacks to receive message express. The stream parser now returns a
    > vector of ReceiveMessagesResponse protos once data is appended.
    >
    > Manually verified using Nearby Share and Phone Hub, and confirmed
    > successful WebRTC upgrades.
    >
    > Fixed: 1213569
    > Change-Id: I8b3a4b599ca043de4fa505cbc4460408f59975c3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2913016
    > Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    > Reviewed-by: Josh Nohle <nohle@chromium.org>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: James Vecore <vecore@google.com>
    > Cr-Commit-Position: refs/heads/master@{#893610}
    
    Change-Id: I869a096a3f4dfca0228686a8b64a2aed51cc21b7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2971548
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Owners-Override: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893825}

--- chrome/browser/nearby_sharing/instantmessaging/stream_parser.h
+++ chrome/browser/nearby_sharing/instantmessaging/stream_parser.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/strings/string_piece.h"
-#include "chrome/browser/nearby_sharing/instantmessaging/proto/instantmessaging.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace google {
@@ -29,39 +28,31 @@ namespace chrome_browser_nearby_sharing_instantmessaging {
 class StreamBody;
 }  // namespace chrome_browser_nearby_sharing_instantmessaging
 
-// Parses incoming stream of data into valid proto objects.
+// Parses incoming stream of data into valid proto objects and delegates them to
+// the registered callback.
 class StreamParser {
  public:
-  StreamParser();
+  explicit StreamParser(
+      base::RepeatingCallback<void(const std::string& message)> listener,
+      base::OnceClosure fastpath_ready);
   ~StreamParser();
 
   // Appends the stream data (which should be the partial or full serialized
-  // StreamBody) and returns a vector of response protos that can be parsed
-  // from the available stream data. If no responses can be parsed yet, an
-  // empty vector is returned.
-  std::vector<
-      chrome_browser_nearby_sharing_instantmessaging::ReceiveMessagesResponse>
-  Append(base::StringPiece data);
+  // StreamBody).
+  void Append(base::StringPiece data);
 
  private:
-  enum class StreamParsingResult {
-    kSuccessfullyParsedResponse,
-    kNoop,
-    kNotEnoughDataYet,
-    kParsingUnexpectedlyFailed
-  };
-
-  std::vector<
-      chrome_browser_nearby_sharing_instantmessaging::ReceiveMessagesResponse>
-  ParseStreamIfAvailable();
+  void ParseStreamIfAvailable();
 
   // Only supports reading single field type and assumes the StreamBody messages
-  // field and the noop field are the only ones we will ever see.
-  StreamParsingResult ParseNextMessagesFieldFromStream(
-      google::protobuf::io::CodedInputStream* input_stream,
-      chrome_browser_nearby_sharing_instantmessaging::ReceiveMessagesResponse*
-          parsed_response);
+  // field and the messages noop field are the only ones we will ever see.
+  bool ParseNextMessagesFieldFromStream(
+      google::protobuf::io::CodedInputStream* input_stream);
 
+  void DelegateMessage(const std::string& message);
+
+  base::RepeatingCallback<void(const std::string& message)> listener_;
+  base::OnceClosure fastpath_ready_callback_;
   scoped_refptr<net::GrowableIOBuffer> unparsed_data_buffer_;
 };
 
