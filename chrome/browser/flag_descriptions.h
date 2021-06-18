8ad1704ae24b5d882544ea9fc6455ab95bcdba38
Tetsui Ohkubo <tetsui@chromium.org>
2021-06-18 11:13:35 +0000

    arc: Add a Chrome flag for image copy paste compat
    A feature has to be controllable from a Chrome flag in order to qualify
    for feature launch review.
    
    Android side CL: http://ag/15023456
    
    TEST=adb shell getprop ro.boot.image_copy_paste_compat
    BUG=b:188861982
    
    Change-Id: I08bbfeb5efbf7cf7f08cbb5644c356637db74c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2971677
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893778}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2256,6 +2256,9 @@ extern const char kArcDocumentsProviderUnknownSizeDescription[];
 extern const char kArcFilePickerExperimentName[];
 extern const char kArcFilePickerExperimentDescription[];
 
+extern const char kArcImageCopyPasteCompatName[];
+extern const char kArcImageCopyPasteCompatDescription[];
+
 extern const char kArcNativeBridgeToggleName[];
 extern const char kArcNativeBridgeToggleDescription[];
 
