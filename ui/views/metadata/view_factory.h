ea69399492025084217236d371dc1bd572e7b011
Allen Bauer <kylixrd@chromium.org>
2021-06-18 00:24:41 +0000

    BuilderT::AddChild should use an RValue param.
    Bug: <None>
    Change-Id: Ifa73a257cf87257f93bfefbb195606ff262fb68b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2937346
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893628}

--- ui/views/metadata/view_factory.h
+++ ui/views/metadata/view_factory.h
@@ -34,7 +34,7 @@ class BaseViewBuilderT : public internal::ViewBuilderCore {
   }
 
   template <typename Child>
-  Builder& AddChild(Child& child) {
+  Builder& AddChild(Child&& child) {
     children_.push_back(child);
     return *static_cast<Builder*>(this);
   }
