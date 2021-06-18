24a22ba8a1d904ee7eaa855ecb4aba9aa4201234
Eliot Courtney <edcourtney@google.com>
2021-06-18 04:58:57 +0000

    Add code to test wayland client using explicit synchronization.
    Bug: 1149641, 1120244
    Test: manually using my prototype - webgl, video, ARC, crostini, lacros
    Change-Id: Ia95d72f5b236764a3528e4046ae03a8b81ca179f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2928695
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893699}

--- components/exo/wayland/clients/explicit_synchronization.cc
+++ components/exo/wayland/clients/explicit_synchronization.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
+#include "base/containers/flat_map.h"
 #include "base/files/scoped_file.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/task/single_thread_task_executor.h"
@@ -29,6 +30,27 @@ void FrameCallback(void* data, wl_callback* callback, uint32_t time) {
   bool* frame_callback_pending = static_cast<bool*>(data);
   *frame_callback_pending = false;
 }
+
+void BufferReleaseFencedRelease(
+    void* data,
+    struct zwp_linux_buffer_release_v1* buffer_release,
+    int32_t fd) {
+  ClientBase::Buffer* buffer = static_cast<ClientBase::Buffer*>(data);
+  gfx::GpuFenceHandle release_fence;
+  release_fence.owned_fd = base::ScopedFD(fd);
+  gfx::GpuFence(std::move(release_fence)).Wait();
+  buffer->busy = false;
+}
+
+void BufferReleaseImmediateRelease(
+    void* data,
+    struct zwp_linux_buffer_release_v1* buffer_release) {
+  ClientBase::Buffer* buffer = static_cast<ClientBase::Buffer*>(data);
+  buffer->busy = false;
+}
+
+zwp_linux_buffer_release_v1_listener g_linux_buffer_release_listener = {
+    BufferReleaseFencedRelease, BufferReleaseImmediateRelease};
 
 }  // namespace
 
@@ -71,6 +93,9 @@ void ExplicitSynchronizationClient::Run() {
   std::unique_ptr<wl_callback> frame_callback;
   bool frame_callback_pending = false;
 
+  base::flat_map<Buffer*, std::unique_ptr<zwp_linux_buffer_release_v1>>
+      buffer_releases;
+
   do {
     if (frame_callback_pending)
       continue;
@@ -78,6 +103,8 @@ void ExplicitSynchronizationClient::Run() {
     Buffer* buffer = DequeueBuffer();
     if (!buffer)
       continue;
+
+    buffer_releases.erase(buffer);
 
     /* Oscillate between 0 and kMaxDrawStep */
     draw_step += draw_step_dir;
@@ -111,6 +138,14 @@ void ExplicitSynchronizationClient::Run() {
     zwp_linux_surface_synchronization_v1_set_acquire_fence(
         surface_synchronization.get(), fence_fd.get());
 
+    // Register a buffer release event listener.
+    buffer_releases[buffer].reset(
+        zwp_linux_surface_synchronization_v1_get_release(
+            surface_synchronization.get()));
+    zwp_linux_buffer_release_v1_add_listener(buffer_releases[buffer].get(),
+                                             &g_linux_buffer_release_listener,
+                                             buffer);
+
     // Set up the frame callback.
     frame_callback_pending = true;
     frame_callback.reset(wl_surface_frame(surface_.get()));
@@ -135,6 +170,11 @@ int main(int argc, char* argv[]) {
     return 1;
 
   base::SingleThreadTaskExecutor main_task_executor(base::MessagePumpType::UI);
+
+  // Don't add a buffer listener, buffer release events are handled by
+  // the linux-explicit-synchronization protocol.
+  params.use_release_fences = true;
+
   exo::wayland::clients::ExplicitSynchronizationClient client;
   if (!client.Init(params))
     return 1;
