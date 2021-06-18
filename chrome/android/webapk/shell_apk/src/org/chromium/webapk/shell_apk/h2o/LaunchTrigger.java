c20dd469b090ff049d64b6062e852614b3fe787e
Peter E Conn <peconn@chromium.org>
2021-06-18 07:20:15 +0000

    ☔ Use Android S Splash Screens in WebAPKs.
    Bug: 1206643
    Change-Id: Ib9b82fc3ee321a926275c8a3332fd742b8d36017
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2878885
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893727}

--- chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/h2o/LaunchTrigger.java
+++ chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/h2o/LaunchTrigger.java
@@ -8,7 +8,7 @@ package org.chromium.webapk.shell_apk.h2o;
 /**
  * Controls when to launch the WebAPK for {@link SplashActivity}.
  *
- * Executes the provided Runnable when all of {@link #onSplashScreenLaidOut}, {@link #onWillLaunch}
+ * Executes the provided Runnable when all of {@link #onSplashScreenReady}, {@link #onWillLaunch}
  * and {@link #onHostBrowserSelected} have been called. The provided Runnable is only called once,
  * but this can be reset by calling {@link #reset}.
  */
@@ -16,7 +16,7 @@ public class LaunchTrigger {
     private final Runnable mCallback;
 
     // Variables that determine whether we are ready to encode the splash screen.
-    private boolean mSplashScreenLaidOut;
+    private boolean mSplashScreenReady;
     private boolean mWillLaunch;
     private boolean mHostBrowserSelected;
     private boolean mLaunchingOrLaunched;
@@ -26,8 +26,8 @@ public class LaunchTrigger {
     }
 
     // Methods that could trigger the splash screen encoding.
-    public void onSplashScreenLaidOut() {
-        mSplashScreenLaidOut = true;
+    public void onSplashScreenReady() {
+        mSplashScreenReady = true;
         maybeTrigger();
     }
 
@@ -48,8 +48,7 @@ public class LaunchTrigger {
     }
 
     private void maybeTrigger() {
-        if (!mHostBrowserSelected || !mSplashScreenLaidOut || !mWillLaunch
-                || mLaunchingOrLaunched) {
+        if (!mHostBrowserSelected || !mSplashScreenReady || !mWillLaunch || mLaunchingOrLaunched) {
             return;
         }
         mLaunchingOrLaunched = true;
