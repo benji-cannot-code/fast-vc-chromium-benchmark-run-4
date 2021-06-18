8947e8c01c84c5ee76f55a6db14774010f118d3a
Nancy Wang <nancylingwang@chromium.org>
2021-06-18 00:57:18 +0000

    Modify the restore apps on startup setting.
    Based on the latest ui review deck, move the restore apps setting to
    apps page:
    https://docs.google.com/presentation/d/1gZbfpG_-4Sum7-ETMcV8-gOXxjd97mhacA__uiCjM88/edit#slide=id.gd9bda4eac4_1_12
    
    Add the dropdown-menu to the apps page to set and show the restore apps
    setting.
    
    BUG=1146900
    
    Change-Id: Ica2d77b0238588c74efc26d662a33b7c72370396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2962692
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893638}

--- chrome/browser/resources/settings/chromeos/os_apps_page/os_apps_page.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/os_apps_page.js
@@ -85,9 +85,6 @@ Polymer({
               settings.routes.ANDROID_APPS_DETAILS.path,
               '#android-apps .subpage-arrow');
         }
-        if (settings.routes.ON_STARTUP) {
-          map.set(settings.routes.ON_STARTUP.path, '#onStartup');
-        }
         return map;
       },
     },
@@ -99,6 +96,22 @@ Polymer({
     app_: Object,
 
     /**
+     * List of options for the on startup drop-down menu.
+     * @type {!DropdownMenuOptionList}
+     */
+    onStartupOptions_: {
+      readOnly: true,
+      type: Array,
+      value() {
+        return [
+          {value: 1, name: loadTimeData.getString('onStartupAlways')},
+          {value: 2, name: loadTimeData.getString('onStartupAskEveryTime')},
+          {value: 3, name: loadTimeData.getString('onStartupDoNotRestore')},
+        ];
+      },
+    },
+
+    /**
      * Used by DeepLinkingBehavior to focus this page's deep links.
      * @type {!Set<!chromeos.settings.mojom.Setting>}
      */
@@ -107,6 +120,7 @@ Polymer({
       value: () => new Set([
         chromeos.settings.mojom.Setting.kManageAndroidPreferences,
         chromeos.settings.mojom.Setting.kTurnOnPlayStore,
+        chromeos.settings.mojom.Setting.kRestoreAppsAndPages,
       ]),
     },
   },
@@ -188,11 +202,6 @@ Polymer({
     const isKeyboardAction = event.detail === 0;
     settings.AndroidAppsBrowserProxyImpl.getInstance().showAndroidAppsSettings(
         isKeyboardAction);
-  },
-
-  /** @private */
-  onStartupClick_() {
-    settings.Router.getInstance().navigateTo(settings.routes.ON_STARTUP);
   },
 
 });
