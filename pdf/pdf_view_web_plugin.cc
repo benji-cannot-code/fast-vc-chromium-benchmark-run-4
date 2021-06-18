c826ff49635608b808a0d12dfd64abed45543763
Daniel Hosseinian <dhoss@chromium.org>
2021-06-18 22:32:39 +0000

    [unseasoned-pdf] Implement PdfViewWebPlugin::NotifySelectionChanged()
    Meanwhile, correct the implementation of
    PdfViewWebPlugin::GetPdfService() to return a nullptr when the
    mojo::AssociatedRemote is not bound.
    
    Bug: 1191817
    Change-Id: Ia21fe7b421d8beee7972bd1309c258c1859f9911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2971518
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894005}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -673,7 +673,11 @@ void PdfViewWebPlugin::NotifySelectionChanged(const gfx::PointF& left,
                                               int left_height,
                                               const gfx::PointF& right,
                                               int right_height) {
-  NOTIMPLEMENTED();
+  auto* service = GetPdfService();
+  if (!service)
+    return;
+
+  service->SelectionChanged(left, left_height, right, right_height);
 }
 
 void PdfViewWebPlugin::NotifyUnsupportedFeature() {
@@ -738,7 +742,7 @@ bool PdfViewWebPlugin::Redo() {
 }
 
 pdf::mojom::PdfService* PdfViewWebPlugin::GetPdfService() {
-  return pdf_service_remote_.get();
+  return pdf_service_remote_.is_bound() ? pdf_service_remote_.get() : nullptr;
 }
 
 }  // namespace chrome_pdf
