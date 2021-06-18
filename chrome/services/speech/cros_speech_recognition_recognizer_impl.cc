394057e0610840334639d6b3d120bc4a294cbf71
Katie Dektar <katie@chromium.org>
2021-06-18 02:33:26 +0000

    RecognitionMode can be set in Chrome OS speech recognition.
    This allows clients of the Speech Recognition service to pick
    caption or IME type speech recognition.
    
    AX-Relnotes: N/A
    Bug: 1173135
    Change-Id: Ifdb1ce4356b351c2ee75dcc506246869401e1f69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2961530
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#893663}

--- chrome/services/speech/cros_speech_recognition_recognizer_impl.cc
+++ chrome/services/speech/cros_speech_recognition_recognizer_impl.cc
@@ -18,7 +18,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 
 namespace speech {
+
+namespace {
 constexpr char kNoClientError[] = "No cros soda client.";
+
+chromeos::machine_learning::mojom::SodaRecognitionMode
+GetSodaSpeechRecognitionMode(
+    media::mojom::SpeechRecognitionMode recognition_mode) {
+  switch (recognition_mode) {
+    case media::mojom::SpeechRecognitionMode::kIme:
+      return chromeos::machine_learning::mojom::SodaRecognitionMode::kIme;
+    case media::mojom::SpeechRecognitionMode::kCaption:
+      return chromeos::machine_learning::mojom::SodaRecognitionMode::kCaption;
+    case media::mojom::SpeechRecognitionMode::kUnknown:
+      // Chrome OS SODA doesn't support unknown recognition type. Default to
+      // caption.
+      NOTREACHED();
+      return chromeos::machine_learning::mojom::SodaRecognitionMode::kCaption;
+  }
+}
+}  // namespace
 
 void CrosSpeechRecognitionRecognizerImpl::Create(
     mojo::PendingReceiver<media::mojom::SpeechRecognitionRecognizer> receiver,
@@ -94,8 +113,8 @@ void CrosSpeechRecognitionRecognizerImpl::
     config->api_key = google_apis::GetSodaAPIKey();
     config->language_dlc_path = languagepack_path_.value();
     config->library_dlc_path = binary_path_.value();
-    // TODO(crbug.com/1173135): Set options_->recognition_mode in the SodaConfig
-    // when available.
+    config->recognition_mode =
+        GetSodaSpeechRecognitionMode(options_->recognition_mode);
     config->enable_formatting =
         options_->enable_formatting
             ? chromeos::machine_learning::mojom::OptionalBool::kTrue
