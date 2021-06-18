0fc518db13376a9035a808e543066d299a8ddae7
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-06-18 07:35:28 +0000

    Redesign the splash screen dialog
    This CL redesigns the splash screen dialog for the AMAC-e apps.
    * Changes the close button from the [x] icon-style button to the [Got it] text-style button
    * Changes the dialog body text
    * Refactors views code with views::Builder
    * Fix the preferred width calc in CalculatePreferredSize
    
    BUG=b:191203576
    BUG=b:187489486
    TEST=ArcSplashScreenDialogViewTest
    
    Change-Id: I9131fc45a5ab01d5724ed486debef14671c39ef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972010
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893732}

--- components/arc/compat_mode/arc_splash_screen_dialog_view.h
+++ components/arc/compat_mode/arc_splash_screen_dialog_view.h
@@ -15,7 +15,7 @@ class Window;
 
 namespace views {
 class View;
-class Button;
+class MdTextButton;
 }  // namespace views
 
 namespace arc {
@@ -55,11 +55,10 @@ class ArcSplashScreenDialogView : public views::BubbleDialogDelegateView {
   void AddedToWidget() override;
 
  private:
-  void OnLinkClicked();
   void OnCloseButtonClicked();
 
   base::OnceClosure close_callback_;
-  views::Button* close_button_ = nullptr;
+  views::MdTextButton* close_button_ = nullptr;
 };
 
 }  // namespace arc
