2599e3352256ebfa526a03001c4e46508f0841e3
Aya ElAttar <ayaelattar@chromium.org>
2021-06-18 13:58:51 +0000

    [sheriff] Disable AdditionalToS on ChromeOS
    Disable AdditionalToS on linux-chromeos-rel
    instead of dbg
    
    TBR=antrim@chromium.org
    
    Bug: 1220963
    Change-Id: Ica7754f537baf8e0ef4f932cc6502caf1990aa19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972686
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Owners-Override: Aya Elsayed <ayaelattar@chromium.org>
    Auto-Submit: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893810}

--- chrome/browser/ash/login/eula_browsertest.cc
+++ chrome/browser/ash/login/eula_browsertest.cc
@@ -351,7 +351,7 @@ IN_PROC_BROWSER_TEST_F(EulaTest, LearnMore) {
           1)));
 }
 
-#if defined(OS_CHROMEOS) && !defined(NDEBUG)
+#if defined(OS_CHROMEOS) && defined(NDEBUG)
 #define MAYBE_AdditionalToS DISABLED_AdditionalToS
 #else
 #define MAYBE_AdditionalToS AdditionalToS
