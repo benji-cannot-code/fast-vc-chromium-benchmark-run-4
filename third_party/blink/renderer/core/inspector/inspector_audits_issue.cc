71b2b66e4ebe574ae0abf2ff6be91f5ef6d9edaf
Taylor Bergquist <tbergquist@chromium.org>
2021-06-18 18:46:58 +0000

    Revert "Reland "Associate requestId/issueId with CORS TypeError exceptions""
    This reverts commit 5019d4de128c95cdfaf132f148dc7ce64445a73c.
    
    Reason for revert: Speculative revert for test failures in https://bugs.chromium.org/p/chromium/issues/detail?id=1221089
    
    Original change's description:
    > Reland "Associate requestId/issueId with CORS TypeError exceptions"
    >
    > This is a reland of 840dd2bac953a6648bb3ec372ba0830482f95182
    >
    > The original CL was reverted because the V8 API it uses seems to have
    > a subtle bug (see crbug.com/1218340) and some performance issues. To
    > investigate that bug and the performance issues, we rel-and this
    > change with the critical API calls are behind a blink runtime flag.
    >
    > The PS1 is the original change, PS2 contains modifications that turn
    > off the actual metadata association except for tests.
    >
    > Original change's description:
    > > Associate requestId/issueId with CORS TypeError exceptions
    > >
    > > This CL uses the exception meta data to associate the DevTools
    > > requestId and issueId with the TypeError exception to faclilitate
    > > cross-linking the entities in the DevTools front-end.
    > >
    > > Bug: chromium:1213393
    > >
    > >
    > > Change-Id: I0c049e7b54394e5b4c8a4d6833ccc700326cad83
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2790880
    > > Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    > > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#890647}
    >
    > Bug: chromium:1213393
    > Change-Id: I03ce0acbdabd257818fbeb126dd3fd789c02744e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964603
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#893320}
    
    Bug: chromium:1213393
    Change-Id: Iecc817f9bfbde2c834e4f76f184a53e09e1a24a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2971615
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Owners-Override: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893909}

--- third_party/blink/renderer/core/inspector/inspector_audits_issue.cc
+++ third_party/blink/renderer/core/inspector/inspector_audits_issue.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/inspector/inspector_audits_issue.h"
 
-#include "base/unguessable_token.h"
 #include "third_party/blink/renderer/bindings/core/v8/source_location.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
 #include "third_party/blink/renderer/core/dom/element.h"
@@ -80,14 +79,12 @@ std::unique_ptr<protocol::Audits::SourceCodeLocation> CreateProtocolLocation(
   return protocol_location;
 }
 
-void AuditsIssue::ReportCorsIssue(
-    ExecutionContext* execution_context,
-    int64_t identifier,
-    RendererCorsIssueCode code,
-    String url,
-    String initiator_origin,
-    String failedParameter,
-    absl::optional<base::UnguessableToken> issue_id) {
+void AuditsIssue::ReportCorsIssue(ExecutionContext* execution_context,
+                                  int64_t identifier,
+                                  RendererCorsIssueCode code,
+                                  String url,
+                                  String initiator_origin,
+                                  String failedParameter) {
   String devtools_request_id =
       IdentifiersFactory::SubresourceRequestId(identifier);
   std::unique_ptr<protocol::Audits::AffectedRequest> affected_request =
@@ -118,9 +115,6 @@ void AuditsIssue::ReportCorsIssue(
                    .setCode(protocol::Audits::InspectorIssueCodeEnum::CorsIssue)
                    .setDetails(std::move(details))
                    .build();
-  if (issue_id) {
-    issue->setIssueId(IdentifiersFactory::IdFromToken(*issue_id));
-  }
   execution_context->AddInspectorIssue(AuditsIssue(std::move(issue)));
 }
 
