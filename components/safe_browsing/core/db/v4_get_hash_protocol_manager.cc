488e10968d8363e2cf0df9261b6ed59aa7bed6e5
Colin Blundell <blundell@chromium.org>
2021-06-18 12:09:00 +0000

    [SafeBrowsing] Remove thread_utils.h from V4GetHashProtocolManager
    thread_utils.h is a utility that allows //components/safe_browsing/core
    to use //content's and //ios/web's threading primitives via different
    implementations that are conditionally included in the GN target. We
    are going to eliminate this utility as part of eliminating
    //components/safe_browsing/core's incorrect dependences on
    //components/safe_browsing/{content, ios}.
    
    This CL removes usage of thread_utils.h in
    //components/safe_browsing/core's V4GetHashProtocolManager. This class
    is created and executes entirely on the IO thread [1]. Hence, we can
    simply use the SequenceChecker member that it already has.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/safe_browsing/core/db/database_manager.cc;l=113;drc=8d990c92df3d03ff3d313428f25dd11b7e509bcf;bpv=1;bpt=1
    
    Bug: 1216341
    Change-Id: I9474ec51760e4f9b2e6e9429bef74ec03e97330b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2963828
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893787}

--- components/safe_browsing/core/db/v4_get_hash_protocol_manager.cc
+++ components/safe_browsing/core/db/v4_get_hash_protocol_manager.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/traced_value.h"
-#include "components/safe_browsing/core/common/thread_utils.h"
 #include "components/safe_browsing/core/features.h"
 #include "net/base/load_flags.h"
 #include "net/http/http_response_headers.h"
@@ -272,7 +271,6 @@ void V4GetHashProtocolManager::GetFullHashes(
         full_hash_to_store_and_hash_prefixes,
     const std::vector<std::string>& list_client_states,
     FullHashCallback callback) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!full_hash_to_store_and_hash_prefixes.empty());
 
@@ -364,7 +362,7 @@ void V4GetHashProtocolManager::GetFullHashesWithApis(
     const GURL& url,
     const std::vector<std::string>& list_client_states,
     ThreatMetadataForApiCallback api_callback) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(url.SchemeIs(url::kHttpScheme) || url.SchemeIs(url::kHttpsScheme));
 
   std::vector<FullHash> full_hashes;
@@ -718,7 +716,7 @@ void V4GetHashProtocolManager::UpdateCache(
     const std::vector<HashPrefix>& prefixes_requested,
     const std::vector<FullHashInfo>& full_hash_infos,
     const Time& negative_cache_expire) {
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   // If negative_cache_expire is null, don't cache the results since it's not
   // clear till what time they should be considered valid.
@@ -774,7 +772,6 @@ void V4GetHashProtocolManager::OnURLLoaderComplete(
     network::SimpleURLLoader* url_loader,
     std::unique_ptr<std::string> response_body) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(CurrentlyOnThread(ThreadID::IO));
 
   int response_code = 0;
   if (url_loader->ResponseInfo() && url_loader->ResponseInfo()->headers)
