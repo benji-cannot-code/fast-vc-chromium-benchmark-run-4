9e49c568d8dc941042ac8b5d9789e2670928d8ab
dpapad <dpapad@chromium.org>
2021-06-18 05:40:08 +0000

    WebUI: Remove unnecessary imports of legacy Polymer() function.
    Also updating polymer_modulizer() to not add the unnecessary import when
    auto-generating Polymer3 code, as well as omitting the 'html' tag
    literal import where not needed.
    
    Bug: 1206112
    Change-Id: I549dce31531cd67bf16703e8523daf81c41df404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2968323
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#893708}

--- chrome/browser/resources/history/shared_vars.ts
+++ chrome/browser/resources/history/shared_vars.ts
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import 'chrome://resources/polymer/v3_0/paper-styles/color.js';
-
-import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 const $_documentContainer = document.createElement('template');
 $_documentContainer.innerHTML = `{__html_template__}`;
