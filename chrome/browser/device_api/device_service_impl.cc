b3e8f9d81f9887b31363227c5714c6cfef880724
Anqing Zhao <anqing@google.com>
2021-06-19 09:20:35 +0000

    Run an origin trial for Device Attributes API
    To run an origin trial for this API, the runtime feature status needs to
    be changed accordingly. The 'ManagedInterface' runtime feature should be
    removed because 'Managed Configuration' has been at stable stage. The
    interface is always exposed to website since we are using 'implied_by'
    field to connect with each other.
    
    The default value of Chrome feature 'EnableRestrictedWebApis' is changed
    from false to true. Device users are still able to turn it off in the
    flags page if needed.
    
    Bug: 1132865
    Change-Id: I5fcb05c91e6d742479f057f9774665cc3204d387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2940838
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894072}

--- chrome/browser/device_api/device_service_impl.cc
+++ chrome/browser/device_api/device_service_impl.cc
@@ -20,13 +20,18 @@ namespace {
 
 bool IsTrustedContext(content::RenderFrameHost* host,
                       const url::Origin& origin) {
-  // TODO(anqing): This is used for dev trial. The flag will be removed when
-  // permission policies are ready.
+  // TODO(anqing): This feature flag is turned on by default for origin trial.
+  // The flag will be removed when permission policies are ready.
   if (!base::FeatureList::IsEnabled(features::kEnableRestrictedWebApis))
     return false;
 
   PrefService* prefs =
       Profile::FromBrowserContext(host->GetBrowserContext())->GetPrefs();
+
+  if (!prefs->GetBoolean(
+          prefs::kManagedWebAppsAccessToDeviceAttributesAllowed)) {
+    return false;
+  }
 
   // TODO(apotapchuk): Implement a more efficient way of checking the trustness
   // status of the app.
@@ -48,8 +53,12 @@ DeviceServiceImpl::DeviceServiceImpl(
   pref_change_registrar_.Init(
       Profile::FromBrowserContext(host->GetBrowserContext())->GetPrefs());
   pref_change_registrar_.Add(
+      prefs::kManagedWebAppsAccessToDeviceAttributesAllowed,
+      base::BindRepeating(&DeviceServiceImpl::OnDisposingIfNeeded,
+                          base::Unretained(this)));
+  pref_change_registrar_.Add(
       prefs::kWebAppInstallForceList,
-      base::BindRepeating(&DeviceServiceImpl::OnForceInstallWebAppListChanged,
+      base::BindRepeating(&DeviceServiceImpl::OnDisposingIfNeeded,
                           base::Unretained(this)));
 }
 
@@ -78,7 +87,7 @@ void DeviceServiceImpl::RegisterProfilePrefs(PrefRegistrySimple* registry) {
       prefs::kManagedWebAppsAccessToDeviceAttributesAllowed, true);
 }
 
-void DeviceServiceImpl::OnForceInstallWebAppListChanged() {
+void DeviceServiceImpl::OnDisposingIfNeeded() {
   // DeviceServiceImpl is allocated on the heap, thus it is safe to remove it
   // like this.
   if (!IsTrustedContext(host_, origin())) {
