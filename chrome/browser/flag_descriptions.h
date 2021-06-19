2dc4fe850869fc1bf1ac08e6971721a286bb1e54
Curt Clemens <cclem@google.com>
2021-06-19 00:30:06 +0000

    [SmartLock] Add About Flag for Smart Lock revamp
    Add an About Flag that will be used to to turn on the Smart Lock revamp
    feature, which will replace the existing Smart Lock UI on the lock
    screen with a new, more user-friendly UI, and add Smart Lock to the
    Lock screen section of Settings.
    
    go/cros-smartlock-ui-revamp
    
    Change-Id: I5cce02e8d8dee900e6891b918796995df8b1d63a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973215
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894040}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2836,6 +2836,9 @@ extern const char kShowTapsDescription[];
 extern const char kShowTouchHudName[];
 extern const char kShowTouchHudDescription[];
 
+extern const char kSmartLockUIRevampName[];
+extern const char kSmartLockUIRevampDescription[];
+
 extern const char kSpectreVariant2MitigationName[];
 extern const char kSpectreVariant2MitigationDescription[];
 
