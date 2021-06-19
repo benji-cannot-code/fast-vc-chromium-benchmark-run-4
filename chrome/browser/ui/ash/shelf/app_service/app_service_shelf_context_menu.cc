805fb90bec80590a497b8d6a301a017772fbde62
Joel Hockey <joelhockey@chromium.org>
2021-06-19 21:17:17 +0000

    Always put "Open window" above "Pin" in context menu
    Bug: 1112838
    Change-Id: I4cb9c2f6adf5ae0db6554c8dfa191d6dd80c61a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972242
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894094}

--- chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu.cc
@@ -274,7 +274,7 @@ void AppServiceShelfContextMenu::OnGetMenuModel(
           menu_items->items, menu_model.get(), submenu_.get(),
           base::BindOnce(&AppServiceShelfContextMenu::GetCommandIdVectorIcon,
                          base::Unretained(this)))) {
-    index = 1;
+    ++index;
   }
 
   // The special rule to ensure that FilesManager's first menu item is "New
@@ -285,6 +285,14 @@ void AppServiceShelfContextMenu::OnGetMenuModel(
 
   if (build_extension_menu_before_pin)
     BuildExtensionAppShortcutsMenu(menu_model.get());
+
+  // "New Window" should go above "Pin".
+  if (menu_items->items.size() > index &&
+      menu_items->items[index]->command_id == ash::MENU_NEW_WINDOW) {
+    AddContextMenuOption(menu_model.get(), ash::MENU_NEW_WINDOW,
+                         menu_items->items[index]->string_id);
+    ++index;
+  }
 
   if (ShouldAddPinMenu())
     AddPinMenu(menu_model.get());
