588ea22e4ea03f84569a62507a9cee9d9b77e9c1
Regan Hsu <hsuregan@google.com>
2021-06-21 18:27:27 +0000

    [CrOS Cellular] Add flag to use Stork SMDS server for pending profiles.
    When this flag is enabled, instead of using the standard GSMA server
    to fetch pending ESim profiles, the Stork test SMDS server will be used
    instead to fetch pending ESim profiles. The enables developers to
    rapidly test / develop SMDS related flows in the future.
    
    Fixed: 1221104
    Change-Id: I787cc73d5a7ad1b64e673e6f21ddedb6cdd7ebd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2968147
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894315}

--- chromeos/services/cellular_setup/esim_manager.cc
+++ chromeos/services/cellular_setup/esim_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <sstream>
 
+#include "ash/constants/ash_features.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chromeos/dbus/hermes/hermes_manager_client.h"
 #include "chromeos/network/network_event_log.h"
@@ -21,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace cellular_setup {
 namespace {
+
+// The Stork SM-DS Prod server used to fetch pending ESim profiles.
+const char kStorkSmdsServerAddress[] = "prod.smds.rsp.goog";
 
 void LogEuiccPaths(const std::set<dbus::ObjectPath>& new_euicc_paths) {
   if (new_euicc_paths.empty()) {
@@ -38,6 +42,14 @@ void LogEuiccPaths(const std::set<dbus::ObjectPath>& new_euicc_paths) {
 }
 
 }  // namespace
+
+// static
+std::string ESimManager::GetRootSmdsAddress() {
+  // An empty string indicates that the root GSMA address should be used.
+  return features::IsUseStorkSmdsServerAddressEnabled()
+             ? kStorkSmdsServerAddress
+             : std::string();
+}
 
 ESimManager::ESimManager()
     : ESimManager(NetworkHandler::Get()->cellular_connection_handler(),
