457579a26081547d82a18b4f4245f26013f0827d
Kamila Hasanbega <hkamila@google.com>
2021-06-21 14:50:01 +0000

    Revert "Reland "assistant: Enable sandbox of Libassistant service""
    This reverts commit 834452e1b08d80546e3f43ddfa04707c05c6b726.
    
    Reason for revert: The reland seem corresponds with the starting of the failures here https://ci.chromium.org/p/chrome/builders/ci/chromeos-kevin-chrome specifically https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-kevin-chrome/15397/blamelist. -- build sheriff
    
    Original change's description:
    > Reland "assistant: Enable sandbox of Libassistant service"
    >
    > This is a reland of 5d6b07c080e80183fa3ae5907274d9a5f2536dcc
    >
    > Original change's description:
    > > assistant: Enable sandbox of Libassistant service
    > >
    > > This cl turns on the sandbox flag.
    > >
    > > Bug: b/155328340
    > > Test: manual
    > > Change-Id: I327abaafd7c412e724e50b09c3c9659b711b2419
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965655
    > > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > > Commit-Queue: Tao Wu <wutao@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#893146}
    >
    > Bug: b/155328340
    > Change-Id: I064461faffb22d5176623feeaf9c0ea2974bcf5a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967770
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Commit-Queue: Tao Wu <wutao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#893537}
    
    Bug: b/155328340
    Change-Id: I3bc832dd9c4545f354fd3868852fc6c9c7b9743f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972590
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@google.com>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Owners-Override: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/master@{#894243}

--- chrome/browser/ui/ash/assistant/assistant_browsertest.cc
+++ chrome/browser/ui/ash/assistant/assistant_browsertest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/audio/cras_audio_handler.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_run_loop_timeout.h"
 #include "base/time/time.h"
 #include "chrome/browser/ui/ash/assistant/assistant_test_mixin.h"
