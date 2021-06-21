a2da7898674554151a1ca1c7f4c615e4b2bab07c
Rohit Agarwal <roagarwal@chromium.org>
2021-06-21 08:07:48 +0000

    Remove sync icon from guest avatar.
    Screenshot: crbug.com/1221993
    
    Bug: 1221993
    Change-Id: I17af538101e6dcc4b0c943acea016bccfaefbc7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972697
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894170}

--- chrome/browser/ui/views/profiles/profile_menu_view.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view.cc
@@ -184,7 +184,7 @@ void ProfileMenuView::BuildMenu() {
 
 gfx::ImageSkia ProfileMenuView::GetSyncIcon() const {
   Profile* profile = browser()->profile();
-  if (profile->IsOffTheRecord())
+  if (profile->IsOffTheRecord() || IsGuest(profile))
     return gfx::ImageSkia();
 
   absl::optional<AvatarSyncErrorType> error = GetAvatarSyncErrorType(profile);
