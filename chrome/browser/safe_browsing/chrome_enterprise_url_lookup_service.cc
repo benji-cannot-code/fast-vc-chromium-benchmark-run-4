b820a38659342e7988d7f8e6001aa93c7010fde4
Kamila Hasanbega <hkamila@chromium.org>
2021-06-21 11:04:57 +0000

    Revert "[Safe Browsing] Remove thread_utils.h and its implementations"
    This reverts commit 7c83c671b60d6b27a31b13535c234110d9cf79cb.
    
    Reason for revert: tree closer
    
    Original change's description:
    > [Safe Browsing] Remove thread_utils.h and its implementations
    >
    > thread_utils.h is a utility that allows //components/safe_browsing/core
    > to use //content's and //ios/web's threading primitives via different
    > implementations that are conditionally included in the GN target. This
    > utility is one of //components/safe_browsing/core's incorrect
    > dependences on //components/safe_browsing/{content, ios}, all of which
    > we are eliminating.
    >
    > In previous CLs we have eliminated all usage of thread_utils.h from the
    > codebase; we can now eliminate thread_utils.h itself.
    >
    > Bug: 1216341
    > Change-Id: Ibb0a37193442c61f6e161d0f6df1c8fcd97aedc1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964938
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#894185}
    
    Bug: 1216341
    Change-Id: If0e871edfbcc42e70077c46a41ad4b7ee2916af0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973110
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Owners-Override: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/master@{#894195}

--- chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
+++ chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/dm_token.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/browser/referrer_chain_provider.h"
+#include "components/safe_browsing/core/common/thread_utils.h"
 #include "components/safe_browsing/core/features.h"
 #include "components/safe_browsing/core/proto/csd.pb.h"
 #include "components/safe_browsing/core/proto/realtimeapi.pb.h"
