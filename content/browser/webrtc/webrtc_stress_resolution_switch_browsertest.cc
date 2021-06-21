4d9981ddb04a9cb4ddb08466412fa1d97b16cc14
Evan Shrubsole <eshr@google.com>
2021-06-21 11:31:40 +0000

    Allow plan-b to fix failing Webrtc stress tests.
    These are currently blocking the webrtc roll.
    
    Bug: 857004
    Change-Id: I059688b3e47bb33c7f438c13210d5b1c3993d8ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2975339
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Evan Shrubsole <eshr@google.com>
    Cr-Commit-Position: refs/heads/master@{#894204}

--- content/browser/webrtc/webrtc_stress_resolution_switch_browsertest.cc
+++ content/browser/webrtc/webrtc_stress_resolution_switch_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/platform_thread.h"
 #include "build/build_config.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/public/test/test_utils.h"
+#include "third_party/blink/public/common/features.h"
 
 namespace content {
 
@@ -47,12 +49,16 @@ class MAYBE_WebRtcStressResolutionSwitchBrowserTest
     WebRtcContentBrowserTestBase::SetUpCommandLine(command_line);
     // Automatically grant device permission.
     AppendUseFakeUIForMediaStreamFlag();
+    // Allow Plan B.
+    scoped_features_.InitAndEnableFeature(
+        blink::features::kRTCAllowPlanBOutsideDeprecationTrial);
   }
 
  protected:
   void MakeTypicalPeerConnectionCall(const std::string& javascript) {
     MakeTypicalCall(javascript, "/media/peerconnection-resolution-switch.html");
   }
+  base::test::ScopedFeatureList scoped_features_;
 };
 
 IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcStressResolutionSwitchBrowserTest,
