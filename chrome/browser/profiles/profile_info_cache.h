fff95a4e6a1aabfab7a5c0e367a6b824f2a8a279
Alex Ilin <alexilin@chromium.org>
2021-06-21 11:40:31 +0000

    [ProfileInfoCache] Move *GAIAPicture() methods to ProfileAttributesEntry
    This CL moves the implementation of the following methods from
    deprecated ProfileInfoCache to ProfileAttributesEntry while keeping
    the identical behavior:
    - {Get,Set}GAIAPicture()
    - ShouldUpdateGAIAPicture()
    - {Set,}IsUsingGAIAPicture()
    
    All corresponding unittests were moved from
    profile_info_cache_unittest.cc to profile_attributes_storage_unittest.cc
    
    Methods for managing GAIA pictures on disk were added to
    ProfileAttributesStorage as this is a class that maintains the cache of
    images that were loaded from disk.
    
    Avatar-related methods in ProfileInfoCache have no more callers and can
    be removed.
    
    Bug: 305720
    Change-Id: I6d241ab74aaf489d1e60c0ee34af6023888f272e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972707
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894206}

--- chrome/browser/profiles/profile_info_cache.h
+++ chrome/browser/profiles/profile_info_cache.h
@@ -27,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_info_interface.h"
 #include "components/signin/public/base/persistent_repeating_timer.h"
 
-namespace gfx {
-class Image;
-}
-
 namespace base {
 class DictionaryValue;
 }
@@ -67,23 +63,6 @@ class ProfileInfoCache : public ProfileInfoInterface,
       const base::FilePath& profile_path) const override;
   // Will be removed SOON with ProfileInfoCache tests. Do not use!
   base::FilePath GetPathOfProfileAtIndex(size_t index) const override;
-  // Returns the GAIA picture for the given profile. This may return NULL
-  // if the profile does not have a GAIA picture or if the picture must be
-  // loaded from disk.
-  const gfx::Image* GetGAIAPictureOfProfileAtIndex(size_t index) const override;
-  bool IsUsingGAIAPictureOfProfileAtIndex(size_t index) const override;
-  bool ProfileIsUsingDefaultAvatarAtIndex(size_t index) const override;
-
-  // Returns true if a GAIA picture has been loaded or has failed to load for
-  // profile at |index|.
-  bool IsGAIAPictureOfProfileAtIndexLoaded(size_t index) const;
-
-  void SetGAIAPictureOfProfileAtIndex(size_t index,
-                                      const std::string& image_url_with_size,
-                                      gfx::Image image);
-
-  void SetIsUsingGAIAPictureOfProfileAtIndex(size_t index, bool value);
-  void SetProfileIsUsingDefaultAvatarAtIndex(size_t index, bool value);
 
   // Notify IsSignedInRequired to all observer
   void NotifyIsSigninRequiredChanged(const base::FilePath& profile_path);
@@ -140,12 +119,6 @@ class ProfileInfoCache : public ProfileInfoInterface,
       size_t index,
       const std::string& image_url_with_size);
 
-  bool ShouldUpdateGAIAPictureOfProfileAtIndex(
-      size_t index,
-      const std::string& old_file_name,
-      const std::string& key,
-      const std::string& image_url_with_size,
-      bool image_is_empty) const;
 #if !defined(OS_ANDROID)
   void LoadGAIAPictureIfNeeded();
 #endif
--- chrome/browser/profiles/profile_info_interface.h
+++ chrome/browser/profiles/profile_info_interface.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 
-namespace gfx {
-class Image;
-}
-
 // This abstract interface is used to query the profiles backend for information
 // about the different profiles. Its sole concrete implementation is the
 // ProfileInfoCache. This interface exists largely to assist in testing.
@@ -28,15 +24,6 @@ class ProfileInfoInterface {
       const base::FilePath& profile_path) const = 0;
 
   virtual base::FilePath GetPathOfProfileAtIndex(size_t index) const = 0;
-
-  virtual const gfx::Image* GetGAIAPictureOfProfileAtIndex(
-      size_t index) const = 0;
-
-  // Checks if the GAIA picture should be used as the profile's avatar icon.
-  virtual bool IsUsingGAIAPictureOfProfileAtIndex(size_t index) const = 0;
-
-  // Returns true if the user has never manually selected a profile avatar.
-  virtual bool ProfileIsUsingDefaultAvatarAtIndex(size_t index) const = 0;
 
  protected:
   virtual ~ProfileInfoInterface() {}
