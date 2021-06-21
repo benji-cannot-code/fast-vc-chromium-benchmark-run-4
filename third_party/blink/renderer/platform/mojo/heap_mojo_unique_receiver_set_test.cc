3048149677e17b2e1db578fcfd9fcfc1bf0331c3
Keishi Hattori <keishi@chromium.org>
2021-06-21 09:11:21 +0000

    Add final to GCOwner
    Add final to GCOwner. This is required when we apply rewrite_raw_ptr_fields, and test_
    is converted to a CheckedPtr. CheckedPtr is not trivialy destructable and we hit the
    "Finalized GarbageCollected class should either have a virtual destructor or be marked as final" static assert.
    
    Change-Id: Ic81c3b8bdf3b98c8c75003a86d9e323c98708c94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2974951
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894182}

--- third_party/blink/renderer/platform/mojo/heap_mojo_unique_receiver_set_test.cc
+++ third_party/blink/renderer/platform/mojo/heap_mojo_unique_receiver_set_test.cc
@@ -21,7 +21,7 @@ namespace blink {
 namespace {
 
 template <HeapMojoWrapperMode Mode>
-class GCOwner : public GarbageCollected<GCOwner<Mode>> {
+class GCOwner final : public GarbageCollected<GCOwner<Mode>> {
  public:
   explicit GCOwner(MockContextLifecycleNotifier* context)
       : receiver_set_(context) {}
