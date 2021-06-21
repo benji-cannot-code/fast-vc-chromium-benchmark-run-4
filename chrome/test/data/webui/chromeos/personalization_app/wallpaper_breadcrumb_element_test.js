bb894b2ae1790f4914c0b4c3fcc872fefaf9d856
Jason Thai <jasontt@chromium.org>
2021-06-21 20:27:51 +0000

    Personalization: add resource string for my images label
    Bug: b/184774974
    Test: browser_test --gtest-filter="PersonalizationApp*"
    Change-Id: I758d4946a406cc724eda9854af452ad677495827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973412
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894376}

--- chrome/test/data/webui/chromeos/personalization_app/wallpaper_breadcrumb_element_test.js
+++ chrome/test/data/webui/chromeos/personalization_app/wallpaper_breadcrumb_element_test.js
@@ -84,6 +84,7 @@ export function WallpaperBreadcrumbTest() {
     assertFalse(div.hidden);
     const span =
         wallpaperBreadcrumbElement.shadowRoot.getElementById('pageLabel');
-    assertEquals('My Images', span.textContent);
+    assertEquals(
+        wallpaperBreadcrumbElement.i18n('myImagesLabel'), span.textContent);
   });
 }
