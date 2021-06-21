dbef4a851704ec8759ba92c4c9efd30e6f7e9c16
Peter Kasting <pkasting@chromium.org>
2021-06-21 19:27:41 +0000

    Fix -Wc++11-narrowing: /components/soda
    This CL was uploaded by git cl split.
    
    R=evliu@google.com
    
    Bug: 1216696
    Change-Id: Ie9566debcde84320ec2fa8389163339e328ae5c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2974265
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#894346}

--- components/soda/soda_installer_impl_chromeos.cc
+++ components/soda/soda_installer_impl_chromeos.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
+#include "base/numerics/safe_conversions.h"
 #include "chromeos/dbus/dlcservice/dlcservice_client.h"
 #include "components/live_caption/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -169,7 +170,7 @@ void SodaInstallerImplChromeOS::OnSodaCombinedProgress() {
   // e.g.: (1) starting progress from 0% if we are downloading language
   // only (2) weighting download progress proportionally to DLC binary size.
   const double progress = (soda_progress_ + language_progress_) / 2;
-  NotifyOnSodaProgress(int{100 * progress});
+  NotifyOnSodaProgress(base::ClampFloor(100 * progress));
 }
 
 void SodaInstallerImplChromeOS::OnDlcUninstalled(const std::string& dlc_id,
