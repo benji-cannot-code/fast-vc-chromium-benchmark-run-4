a34d65f1eea844a52366a7401ba9e5b04b2bbc18
Justin Novosad <junov@chromium.org>
2021-06-21 15:32:50 +0000

    Make the destructor of NoAllocDirectCallExceptionState inline-able
    This significantly reduces the bindings overhead cost of methods
    that use [NoAllocDirectCall] in combination with: [RaisesException],
    or Interface object arguments.
    
    This improves the score of the webgl/binding-bind-buffer.html benchmark
    by 5% on MacOS
    
    Bug: 1218898
    Change-Id: Ib0cc2e3ad76f914fad9179ba829630007dc6d7a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965107
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894250}

--- third_party/blink/renderer/platform/bindings/no_alloc_direct_call_exception_state.h
+++ third_party/blink/renderer/platform/bindings/no_alloc_direct_call_exception_state.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/no_alloc_direct_call_host.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
 
@@ -15,6 +16,8 @@ namespace blink {
 // When a Throw* method is called, the creation of the exception object is
 // deferred using the deferral mechanism provided by NoAllocDirectCallHost.
 class PLATFORM_EXPORT NoAllocDirectCallExceptionState : public ExceptionState {
+  STACK_ALLOCATED();
+
  public:
   NoAllocDirectCallExceptionState(NoAllocDirectCallHost* host,
                                   v8::Isolate* isolate,
@@ -22,10 +25,16 @@ class PLATFORM_EXPORT NoAllocDirectCallExceptionState : public ExceptionState {
                                   const char* interface_name,
                                   const char* property_name)
       : ExceptionState(isolate, context_type, interface_name, property_name),
-        host_(host) {}
+        host_(host),
+        deferred_exception_()  // Explicit init necessary because of union.
+  {}
 
-  virtual ~NoAllocDirectCallExceptionState() {
-    if (deferred_exception_) {
+  // Performance hack: since this class is always allocated on the stack, we
+  // know it will never be destroyed via a base type pointer. Therefore it
+  // is safe for the destructor to be non-virtual, which allows it to be
+  // inlined.
+  ALWAYS_INLINE ~NoAllocDirectCallExceptionState() {
+    if (UNLIKELY(deferred_exception_)) {
       host_->PostDeferrableAction(std::move(deferred_exception_));
     }
   }
@@ -42,7 +51,17 @@ class PLATFORM_EXPORT NoAllocDirectCallExceptionState : public ExceptionState {
 
  private:
   NoAllocDirectCallHost* host_;
-  NoAllocDirectCallHost::DeferrableAction deferred_exception_;
+
+  // Performance hack: The use of a "union" here prevents deferred_exception_
+  // from being destroyed automatically by the destructor of
+  // NoAllocDirectCallExceptionState, which removes an unnecessary non-inlinable
+  // call to CallbackBase::~CallbackBase. It is safe to free
+  // deferred_exception_ without calling its destructor because the logic in
+  // ~NoAllocDirectCallExceptionState already guarantees that
+  // deferred_exception_'s internal reference is null.
+  union {
+    NoAllocDirectCallHost::DeferrableAction deferred_exception_;
+  };
 };
 
 }  // namespace blink
