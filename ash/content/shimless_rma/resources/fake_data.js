f351a1c2b53eaf5f07ef7794e319ebe181dc3305
Gavin Dodd <gavindodd@google.com>
2021-06-21 17:22:48 +0000

    shimless: Add firmware reimaging page
    Reimage required: http://screenshot/BGvUPhzw2y3hTzj
    Reimage optional: http://screenshot/BRB9EHZdN3Uq46G
    
    Bug: 1198187
    Test: Run Chrome and check page displays
          Run browser tests for Shimless*
    
    Change-Id: I93779f7321e70db6546b1e0e71f8db270b10b69f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951144
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/master@{#894287}

--- ash/content/shimless_rma/resources/fake_data.js
+++ ash/content/shimless_rma/resources/fake_data.js
@@ -18,6 +18,7 @@ export const fakeStates = [
   {state: RmaState.kEnterRSUWPDisableCode, error: RmadErrorCode.kOk},
   {state: RmaState.kWPDisableComplete, error: RmadErrorCode.kOk},
   {state: RmaState.kSelectComponents, error: RmadErrorCode.kOk},
+  {state: RmaState.kChooseFirmwareReimageMethod, error: RmadErrorCode.kOk},
 ];
 
 /** @type {!Array<string>} */
