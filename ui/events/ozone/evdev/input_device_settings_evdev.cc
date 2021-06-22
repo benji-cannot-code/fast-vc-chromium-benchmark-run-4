9d35b4feafb18d015f5edc735873da943687a835
Harry Cutts <hcutts@chromium.org>
2021-06-22 18:20:31 +0000

    ozone: evdev: enable touchscreen event logs when flag is set
    Bug: b:185411893
    Test: enable flag, collect system logs from chrome://network/#logs,
    Test: check they contain touchscreen events. Disable flag, collect logs,
    Test: check they no longer contain touchscreen events.
    Change-Id: I5d5f8b6235a99aec61e8a3febe33105524921e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973729
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Commit-Queue: Harry Cutts <hcutts@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894798}

--- ui/events/ozone/evdev/input_device_settings_evdev.cc
+++ ui/events/ozone/evdev/input_device_settings_evdev.cc
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/ozone/evdev/input_device_settings_evdev.h"
 
+#include "base/feature_list.h"
+#include "ui/events/ozone/features.h"
+
 namespace ui {
 
 InputDeviceSettingsEvdev::InputDeviceSettingsEvdev() {
+  touch_event_logging_enabled =
+      base::FeatureList::IsEnabled(ui::kEnableInputEventLogging);
 }
 
 InputDeviceSettingsEvdev::InputDeviceSettingsEvdev(
