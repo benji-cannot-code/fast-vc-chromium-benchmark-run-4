c7a23487894a8ebb2683e2657290bb9ed7a72759
rajendrant <rajendrant@chromium.org>
2021-06-22 09:59:11 +0000

    SubresourceRedirect: Allow compression for CSP restricted images
    This CL relaxes CSP to allow images to be redirected to the subresource
    redirect LitePages origin, if the original image URL passed the CSP check
    when the request started. This is experimentally controllable.
    
    Bug: 1216652
    Change-Id: I5ab3da3d817c178375c1ffd66f011190b03b5548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2939196
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894603}

--- third_party/blink/renderer/core/loader/base_fetch_context.cc
+++ third_party/blink/renderer/core/loader/base_fetch_context.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/identifiers_factory.h"
 #include "third_party/blink/renderer/core/loader/frame_client_hints_preferences_context.h"
 #include "third_party/blink/renderer/core/loader/subresource_filter.h"
+#include "third_party/blink/renderer/core/loader/subresource_redirect_util.h"
 #include "third_party/blink/renderer/platform/exported/wrapped_resource_request.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/loader/cors/cors.h"
@@ -408,6 +409,11 @@ BaseFetchContext::CheckCSPForRequestInternal(
     ContentSecurityPolicy::CheckHeaderType check_header_type) const {
   if (options.content_security_policy_option ==
       network::mojom::CSPDisposition::DO_NOT_CHECK) {
+    return absl::nullopt;
+  }
+
+  if (ShouldDisableCSPCheckForSubresourceRedirectOrigin(request_context,
+                                                        redirect_status, url)) {
     return absl::nullopt;
   }
 
