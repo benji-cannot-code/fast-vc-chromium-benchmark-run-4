da2276a3f5d42e24db7657dd187e1945c40d28b3
Clemens Arbesser <arbesser@google.com>
2021-06-22 13:41:53 +0000

    [Autofill Assistant] Avoid calling GetNextPageUkmSourceId()
    This is a workaround to prevent a crash that may be caused in above
    method. Since we currently don't have UKMs in regular flows, this should
    not negatively affect any of our metrics.
    
    That being said, once the bug has a proper fix this should be reverted
    again, such that UKMs in regular flows will report the correct URL,
    even when started during redirects.
    
    Bug: 1217927
    Change-Id: If63c6131ec240073423b184b11842274a08503f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976593
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#894665}

--- components/autofill_assistant/browser/starter.cc
+++ components/autofill_assistant/browser/starter.cc
@@ -172,7 +172,8 @@ Starter::Starter(content::WebContents* web_contents,
                  base::WeakPtr<RuntimeManagerImpl> runtime_manager,
                  const base::TickClock* tick_clock)
     : content::WebContentsObserver(web_contents),
-      next_ukm_source_id_(ukm::GetSourceIdForWebContentsDocument(web_contents)),
+      current_ukm_source_id_(
+          ukm::GetSourceIdForWebContentsDocument(web_contents)),
       cached_failed_trigger_script_fetches_(
           GetOrCreateFailedTriggerScriptFetchesCache()),
       user_denylisted_domains_(kMaxUserDenylistedCacheSize),
@@ -187,13 +188,6 @@ Starter::Starter(content::WebContents* web_contents,
 }
 
 Starter::~Starter() = default;
-
-void Starter::DidStartNavigation(content::NavigationHandle* navigation_handle) {
-  if (!navigation_handle->IsInMainFrame()) {
-    return;
-  }
-  next_ukm_source_id_ = navigation_handle->GetNextPageUkmSourceId();
-}
 
 void Starter::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
@@ -213,6 +207,8 @@ void Starter::DidFinishNavigation(
             .value_or(GURL()));
 
     if (navigated_to_target_domain) {
+      current_ukm_source_id_ =
+          ukm::GetSourceIdForWebContentsDocument(web_contents());
       if (waiting_for_deeplink_navigation_) {
         Start(std::move(pending_trigger_context_));
       }
@@ -228,7 +224,7 @@ void Starter::DidFinishNavigation(
       // Note: this will record for the current domain, not the target domain.
       // There seems to be no way to avoid this.
       Metrics::RecordTriggerScriptStarted(
-          ukm_recorder_, next_ukm_source_id_,
+          ukm_recorder_, ukm::GetSourceIdForWebContentsDocument(web_contents()),
           navigation_handle->IsErrorPage()
               ? Metrics::TriggerScriptStarted::NAVIGATION_ERROR
               : Metrics::TriggerScriptStarted::NAVIGATED_AWAY);
@@ -250,6 +246,8 @@ void Starter::DidFinishNavigation(
   }
 
   if (navigation_handle->HasCommitted() && !navigation_handle->IsErrorPage()) {
+    current_ukm_source_id_ =
+        ukm::GetSourceIdForWebContentsDocument(web_contents());
     MaybeStartImplicitlyForUrl(
         navigation_handle->GetURL(),
         ukm::ConvertToSourceId(navigation_handle->GetNavigationId(),
@@ -430,7 +428,7 @@ void Starter::Start(std::unique_ptr<TriggerContext> trigger_context) {
     // Fail immediately if there is no deeplink domain to wait for.
     // Note: this will record the impression for the current domain.
     Metrics::RecordTriggerScriptStarted(
-        ukm_recorder_, next_ukm_source_id_,
+        ukm_recorder_, current_ukm_source_id_,
         Metrics::TriggerScriptStarted::NO_INITIAL_URL);
     OnStartDone(/* start_regular_script = */ false);
     return;
@@ -447,7 +445,7 @@ void Starter::Start(std::unique_ptr<TriggerContext> trigger_context) {
   // a baseline.
   if (IsTriggerScriptContext(*pending_trigger_context_)) {
     Metrics::RecordTriggerScriptStarted(
-        ukm_recorder_, next_ukm_source_id_, startup_mode,
+        ukm_recorder_, current_ukm_source_id_, startup_mode,
         platform_delegate_->GetFeatureModuleInstalled(),
         platform_delegate_->GetIsFirstTimeUser());
   }
@@ -547,7 +545,7 @@ void Starter::StartTriggerScript() {
           script_parameters.GetBase64TriggerScriptsResponseProto().value());
       if (!service_request_sender) {
         Metrics::RecordTriggerScriptFinished(
-            ukm_recorder_, next_ukm_source_id_,
+            ukm_recorder_, current_ukm_source_id_,
             TriggerScriptProto::UNSPECIFIED_TRIGGER_UI_TYPE,
             Metrics::TriggerScriptFinishedState::BASE64_DECODING_ERROR);
         OnTriggerScriptFinished(
@@ -579,7 +577,7 @@ void Starter::StartTriggerScript() {
       std::make_unique<StaticTriggerConditions>(
           platform_delegate_, pending_trigger_context_.get(), startup_url),
       std::make_unique<DynamicTriggerConditions>(), ukm_recorder_,
-      next_ukm_source_id_);
+      current_ukm_source_id_);
 
   // Note: for the duration of the trigger script, the trigger script
   // coordinator will take ownership of the pending trigger context.
