006ca22ddf36e3e964058497ebd44db3ce0657be
Fabrice de Gans <fdegans@chromium.org>
2021-06-22 21:33:48 +0000

    [code-health] Migrate Blink bluetooth testing presubmit to python3
    Bug: 1212074
    Change-Id: I369d0f60c6823ea5fd519a25132bbde3e72ec5e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2980202
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894870}

--- third_party/blink/renderer/modules/bluetooth/testing/clusterfuzz/PRESUBMIT.py
+++ third_party/blink/renderer/modules/bluetooth/testing/clusterfuzz/PRESUBMIT.py
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 """Script that runs tests before uploading a patch."""
 
+USE_PYTHON3 = True
+
 
 def _RunTests(input_api, output_api):
     """Runs all test files in the directory."""
@@ -12,7 +14,7 @@ def _RunTests(input_api, output_api):
     test_cmd = input_api.Command(
         name=cmd_name, cmd=cmd, kwargs={}, message=output_api.PresubmitError)
     if input_api.verbose:
-        print 'Running ' + cmd_name
+        print('Running ' + cmd_name)
     return input_api.RunTests([test_cmd])
 
 
