c71bd6143fb426817b6ddada9da238bc51e993d1
Swapnil <swapnilgupta@google.com>
2021-06-22 13:32:22 +0000

    Add OVERRIDDEN_BY_SETTINGS error to known misconfigurations
    We have 2 policies that are used for specifying whether the extension
    is force installed or not, ExtensionInstallForceList and
    ExtensionSettings. If the extension is listed in
    ExtensionInstallForceList policy, and is marked as blocked or
    allowed in ExtensionSettings, we do not install the extension as
    ExtensionSettings has greater priority than ExtensionInstallForceList.
    This is a conflict in policy configuration from the admin side and is
    considered as an admin side misconfiguration.
    
    Bug: b/191435865
    Change-Id: Ib6c5c23ddf8f1b4f3f634bd8791d9a86f95f91c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972663
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894660}

--- chrome/browser/extensions/forced_extensions/force_installed_tracker.cc
+++ chrome/browser/extensions/forced_extensions/force_installed_tracker.cc
@@ -284,6 +284,11 @@ bool ForceInstalledTracker::IsMisconfiguration(
     }
   }
 
+  if (installation_data.failure_reason ==
+      InstallStageTracker::FailureReason::OVERRIDDEN_BY_SETTINGS) {
+    return true;
+  }
+
   return false;
 }
 
