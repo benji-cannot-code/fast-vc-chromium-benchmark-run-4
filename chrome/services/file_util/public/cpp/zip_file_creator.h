06c6d8cba5d00f9682e4f22eac46e07a3ec5dcea
François Degros <fdegros@chromium.org>
2021-06-22 01:58:25 +0000

    [ZipFileCreator] Pass ZIP progress information via Mojo
    Added a way for the ZipFileCreator Mojo server to report regular
    progress information while creating a ZIP file. On the client side, this
    progress information is not used yet, but will be in a subsequent CL.
    
    BUG=chromium:912236
    TEST=browser_tests --gtest_filter="ZipFileCreatorTest*
    TEST=Manual tests
    
    Change-Id: Id1b57e5468ddfab35a3b95d01b05f61b7dbfb8fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2952082
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894503}

--- chrome/services/file_util/public/cpp/zip_file_creator.h
+++ chrome/services/file_util/public/cpp/zip_file_creator.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/file_util/public/mojom/file_util_service.mojom.h"
 #include "chrome/services/file_util/public/mojom/zip_file_creator.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 
@@ -23,7 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // directories under a common parent directory. This is done in a sandboxed
 // utility process to protect the browser process from handling arbitrary
 // input data from untrusted sources.
-class ZipFileCreator : public base::RefCountedThreadSafe<ZipFileCreator> {
+class ZipFileCreator : public base::RefCountedThreadSafe<ZipFileCreator>,
+                       private chrome::mojom::ZipListener {
  public:
   // Callback reporting the success or failure of the ZIP creation.
   using ResultCallback = base::OnceCallback<void(bool)>;
@@ -43,7 +45,7 @@ class ZipFileCreator : public base::RefCountedThreadSafe<ZipFileCreator> {
  private:
   friend class base::RefCountedThreadSafe<ZipFileCreator>;
 
-  ~ZipFileCreator();
+  ~ZipFileCreator() override;
 
   // Called after the dest_file |file| is opened on the blocking pool to
   // create the ZIP file in it using a sandboxed utility process.
@@ -55,9 +57,14 @@ class ZipFileCreator : public base::RefCountedThreadSafe<ZipFileCreator> {
   void BindDirectory(
       mojo::PendingReceiver<filesystem::mojom::Directory> receiver) const;
 
-  // Notifies by calling |callback| specified in the constructor the end of the
-  // ZIP operation. Deletes this.
+  // Notifies by calling |result_callback| specified in the constructor the end
+  // of the ZIP operation.
   void ReportDone(bool success);
+
+  // ZIP progress report.
+  void OnProgress(uint64_t bytes,
+                  uint32_t files,
+                  uint32_t directories) override;
 
   // The final result callback.
   ResultCallback result_callback_;
@@ -75,6 +82,9 @@ class ZipFileCreator : public base::RefCountedThreadSafe<ZipFileCreator> {
   // Remote interfaces to the file util service. Only used from the UI thread.
   mojo::Remote<chrome::mojom::FileUtilService> service_;
   mojo::Remote<chrome::mojom::ZipFileCreator> remote_zip_file_creator_;
+
+  // Listener receiver.
+  mojo::Receiver<chrome::mojom::ZipListener> listener_{this};
 
   DISALLOW_COPY_AND_ASSIGN(ZipFileCreator);
 };
