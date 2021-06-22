0371d7289666dbff751943543a7d388e2f0f443e
Olivier Robin <olivierrobin@google.com>
2021-06-22 15:25:45 +0000

    Upload Metrickit crash reports
    Create a Finch experiment to upload Metrickit reports.
    Metrickit reports json are compressed (deflate) and sent
    in place of the minidump.
    It is necessary to have a specific processing of those
    reports.
    Reports are sent to a specific product: Chrome_Metrickit.
    
    Change-Id: I649aca22de82e879ce588b548f2cb0178dcdf8bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2935540
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894702}

--- ios/chrome/browser/crash_report/features.cc
+++ ios/chrome/browser/crash_report/features.cc
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const base::Feature kCrashpadIOS{"CrashpadIOSEnabler",
                                  base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kMetrickitCrashReport{"MetrickitCrashReport",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kSyntheticCrashReportsForUte{
     "SyntheticCrashReportsForUte", base::FEATURE_DISABLED_BY_DEFAULT};
 
--- ios/chrome/browser/crash_report/features.h
+++ ios/chrome/browser/crash_report/features.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 extern const base::Feature kCrashpadIOS;
 
+extern const base::Feature kMetrickitCrashReport;
+
 extern const base::Feature kSyntheticCrashReportsForUte;
 
 // Returns true if kSyntheticCrashReportsForUte and kLogBreadcrumbs features are
