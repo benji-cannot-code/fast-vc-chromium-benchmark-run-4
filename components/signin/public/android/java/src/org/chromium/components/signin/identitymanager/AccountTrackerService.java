0eb5d59cd10ddb6df0018abaf8c855ef2a2415e3
Tanmoy Mollik <triploblastic@chromium.org>
2021-06-22 12:01:32 +0000

    [Android] Migrate tryGetGoogleAccounts() usage in AccountTrackerService
    This CL migrates the usage of the old tryGetGoogleAccounts() to the
    new getAccounts() API in AndroidSyncSettings.
    
    Bug: 1211294
    Change-Id: I4ee6268647f8fcc4f87b8e4ad16b54bfcc38a878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2963822
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894628}

--- components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/AccountTrackerService.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/AccountTrackerService.java
@@ -147,7 +147,7 @@ public class AccountTrackerService {
             accountManagerFacade.addObserver(mAccountsChangeObserver);
         }
 
-        accountManagerFacade.tryGetGoogleAccounts(accounts -> {
+        accountManagerFacade.getAccounts().then(accounts -> {
             final List<String> emails = AccountUtils.toAccountNames(accounts);
             new AsyncTask<List<String>>() {
                 @Override
