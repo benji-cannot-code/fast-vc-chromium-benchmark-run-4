2bf971235f16a249e5f7457b17ae3d1b00027cb6
Bruce Dawson <brucedawson@chromium.org>
2021-06-22 16:22:08 +0000

    Explicitly include windows.h in remoting code
    In order to reduce windows.h usage in header files it is necessary to
    explicitly included it in .cc files when it is needed.
    heartbeat_sender.cc uses GetComputerNameExW() but depended on
    sys_addrinfo.h for windows.h. This fixes that.
    
    Bug: 1222496
    Change-Id: I18f714b3c8a69b12fa0c78e57a65a2c340993930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976558
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894731}

--- remoting/host/heartbeat_sender.cc
+++ remoting/host/heartbeat_sender.cc
@@ -36,6 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "base/strings/utf_string_conversions.h"
+
+// Needed for GetComputerNameExW/ComputerNameDnsFullyQualified.
+#include <windows.h>
 #endif
 
 namespace remoting {
