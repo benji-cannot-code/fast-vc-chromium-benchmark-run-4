da733a43b14664715789ad07c3859278aa75b716
Song Fangzhen <songfangzhen@bytedance.com>
2021-06-22 08:56:49 +0000

    CodeHealth: Remove use of DictionaryValue::Set in tools/json_schema_compiler/test
    This change removes the use of DictionaryValue::Set and replaces with
    Value::SetKey() or Value::SetPath().
    
    Bug: 1187023
    Change-Id: I6875ada53fa8c41bd4f5e1f42fc964964f02772d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972037
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894584}

--- tools/json_schema_compiler/test/crossref_unittest.cc
+++ tools/json_schema_compiler/test/crossref_unittest.cc
@@ -31,7 +31,8 @@ std::unique_ptr<base::DictionaryValue> CreateTestTypeValue() {
 
 TEST(JsonSchemaCompilerCrossrefTest, CrossrefTypePopulateAndToValue) {
   base::DictionaryValue crossref_orig;
-  crossref_orig.Set("testType", CreateTestTypeValue());
+  crossref_orig.SetKey("testType",
+                       base::Value::FromUniquePtrValue(CreateTestTypeValue()));
   crossref_orig.SetString("testEnumRequired", "one");
   crossref_orig.SetString("testEnumOptional", "two");
 
@@ -90,7 +91,8 @@ TEST(JsonSchemaCompilerCrossrefTest, TestTypeInObjectParamsCreate) {
   {
     auto params_value = std::make_unique<base::ListValue>();
     auto param_object_value = std::make_unique<base::DictionaryValue>();
-    param_object_value->Set("testType", CreateTestTypeValue());
+    param_object_value->SetKey(
+        "testType", base::Value::FromUniquePtrValue(CreateTestTypeValue()));
     param_object_value->SetBoolean("boolean", true);
     params_value->Append(std::move(param_object_value));
     std::unique_ptr<crossref::TestTypeInObject::Params> params(
@@ -125,7 +127,8 @@ TEST(JsonSchemaCompilerCrossrefTest, TestTypeInObjectParamsCreate) {
   {
     auto params_value = std::make_unique<base::ListValue>();
     auto param_object_value = std::make_unique<base::DictionaryValue>();
-    param_object_value->Set("testType", CreateTestTypeValue());
+    param_object_value->SetKey(
+        "testType", base::Value::FromUniquePtrValue(CreateTestTypeValue()));
     params_value->Append(std::move(param_object_value));
     std::unique_ptr<crossref::TestTypeInObject::Params> params(
         crossref::TestTypeInObject::Params::Create(*params_value));
