b5b33a67a549a6b9b1e6b5f633528eb036b31679
Trent Begin <tbegin@google.com>
2021-06-22 16:56:38 +0000

    connectivity-diagnostics: launch webui dialog in kiosk mode
    This change removes the last vestiges of the legacy Connectivity
    Diagnostics Chrome App and replaces them with the new WebUI dialog.
    
    Bug: chromium:1218270
    Change-Id: I777cdb65af1ac8d4b52a91e76756ff80650c1ac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951210
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894750}

--- chrome/browser/ash/login/screens/error_screen.cc
+++ chrome/browser/ash/login/screens/error_screen.cc
@@ -28,11 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/settings/cros_settings.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
 #include "chrome/browser/chrome_notification_types.h"
-#include "chrome/browser/extensions/component_loader.h"
-#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/webui/chromeos/connectivity_diagnostics_dialog.h"
 #include "chrome/browser/ui/webui/chromeos/login/error_screen_handler.h"
-#include "chrome/common/extensions/extension_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
@@ -43,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/session_manager/core/session_manager.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/notification_service.h"
-#include "extensions/browser/extension_system.h"
-#include "extensions/common/constants.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 #include "ui/gfx/native_widget_types.h"
 
@@ -346,24 +342,7 @@ void ErrorScreen::OnConfigureCerts() {
 }
 
 void ErrorScreen::OnDiagnoseButtonClicked() {
-  Profile* profile = GetAppProfile();
-  extensions::ExtensionService* extension_service =
-      extensions::ExtensionSystem::Get(profile)->extension_service();
-
-  std::string extension_id = extension_service->component_loader()->Add(
-      IDR_CONNECTIVITY_DIAGNOSTICS_MANIFEST,
-      base::FilePath(extension_misc::kConnectivityDiagnosticsPath));
-
-  apps::AppServiceProxyFactory::GetForProfile(profile)
-      ->BrowserAppLauncher()
-      ->LaunchAppWithParams(apps::AppLaunchParams(
-          extension_id, apps::mojom::LaunchContainer::kLaunchContainerWindow,
-          WindowOpenDisposition::NEW_WINDOW,
-          apps::mojom::AppLaunchSource::kSourceChromeInternal));
-  KioskAppManager::Get()->InitSession(profile, extension_id);
-
-  LoginDisplayHost::default_host()->Finalize(base::BindOnce(
-      [] { session_manager::SessionManager::Get()->SessionStarted(); }));
+  chromeos::ConnectivityDiagnosticsDialog::ShowDialog();
 }
 
 void ErrorScreen::OnLaunchOobeGuestSession() {
---
+++ chrome/browser/ui/webui/chromeos/connectivity_diagnostics_dialog.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_CONNECTIVITY_DIAGNOSTICS_DIALOG_H_
+#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_CONNECTIVITY_DIAGNOSTICS_DIALOG_H_
+
+#include "chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h"
+
+namespace chromeos {
+
+class ConnectivityDiagnosticsDialog : public SystemWebDialogDelegate {
+ public:
+  static void ShowDialog();
+
+ protected:
+  ConnectivityDiagnosticsDialog();
+  ~ConnectivityDiagnosticsDialog() override;
+
+  ConnectivityDiagnosticsDialog(const ConnectivityDiagnosticsDialog&) = delete;
+  ConnectivityDiagnosticsDialog& operator=(
+      const ConnectivityDiagnosticsDialog&) = delete;
+
+  // ui::WebDialogDelegate
+  void GetDialogSize(gfx::Size* size) const override;
+};
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_CONNECTIVITY_DIAGNOSTICS_DIALOG_H_
