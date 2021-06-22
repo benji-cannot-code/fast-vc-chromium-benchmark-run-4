5c0f2630c4cd11d362a34dfa60011b6831af585a
Andrew Paseltiner <apaseltiner@chromium.org>
2021-06-22 23:31:39 +0000

    [attribution_reporting] Add missing sequence checks to delegate
    Change-Id: I11b19a9ab535979150f3ba9c82cbab0387a6bd6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2980312
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894916}

--- content/browser/conversions/conversion_storage_delegate_impl.cc
+++ content/browser/conversions/conversion_storage_delegate_impl.cc
@@ -24,21 +24,25 @@ int ConversionStorageDelegateImpl::GetMaxConversionsPerImpression(
 }
 
 int ConversionStorageDelegateImpl::GetMaxImpressionsPerOrigin() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return 1024;
 }
 
 int ConversionStorageDelegateImpl::GetMaxConversionsPerOrigin() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return 1024;
 }
 
 int ConversionStorageDelegateImpl::GetMaxAttributionDestinationsPerEventSource()
     const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // TODO(apaseltiner): Finalize a value for this.
   return INT_MAX;
 }
 
 ConversionStorage::Delegate::RateLimitConfig
 ConversionStorageDelegateImpl::GetRateLimits() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // TODO(csharrison): Finalize max_attributions_per_window value.
   return {
       .time_window = base::TimeDelta::FromDays(30),
@@ -49,6 +53,7 @@ ConversionStorageDelegateImpl::GetRateLimits() const {
 StorableImpression::AttributionLogic
 ConversionStorageDelegateImpl::SelectAttributionLogic(
     const StorableImpression& impression) const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return StorableImpression::AttributionLogic::kTruthfully;
 }
 
