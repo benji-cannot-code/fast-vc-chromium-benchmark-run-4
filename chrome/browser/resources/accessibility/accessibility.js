031587994b1d463f4c568e12f4ed5e8f442f1e31
Dominic Mazzoni <dmazzoni@chromium.org>
2021-06-22 20:21:25 +0000

    Add an AXMode for HTML metadata
    We have a request to include some HTML metadata in the Android
    ViewStructure. This naturally fits with AXTreeData, but it's not
    something that we need in most situations, so add a new AXMode for
    it so we only retrieve it when needed.
    
    Bug: 1175288
    Change-Id: I80a7f9855db251a53e87957e8adb1140df57e0dc
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967443
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894847}

--- chrome/browser/resources/accessibility/accessibility.js
+++ chrome/browser/resources/accessibility/accessibility.js
@@ -58,8 +58,9 @@ const AXMode = {
   kInlineTextBoxes: 1 << 2,
   kScreenReader: 1 << 3,
   kHTML: 1 << 4,
-  kLabelImages: 1 << 5,
-  kPDF: 1 << 6,
+  kHTMLMetadata: 1 << 5,
+  kLabelImages: 1 << 6,
+  kPDF: 1 << 7,
 
   get kAXModeBasic() {
     return AXMode.kNativeAPIs | AXMode.kWebContents;
@@ -300,6 +301,7 @@ function formatRow(row, data, requestType) {
     row.appendChild(createModeElement(AXMode.kInlineTextBoxes, data, 'web'));
     row.appendChild(createModeElement(AXMode.kScreenReader, data, 'web'));
     row.appendChild(createModeElement(AXMode.kHTML, data, 'web'));
+    row.appendChild(createModeElement(AXMode.kHTMLMetadata, data, 'metadata'));
     row.appendChild(
         createModeElement(AXMode.kLabelImages, data, 'labelImages'));
     row.appendChild(createModeElement(AXMode.kPDF, data, 'pdf'));
@@ -386,6 +388,8 @@ function getNameForAccessibilityMode(mode) {
       return 'Screen reader';
     case AXMode.kHTML:
       return 'HTML';
+    case AXMode.kHTMLMetadata:
+      return 'HTML Metadata';
     case AXMode.kLabelImages:
       return 'Label images';
     case AXMode.kPDF:
