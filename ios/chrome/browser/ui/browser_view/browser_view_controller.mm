9dea9275e34912c17fb43035c1588999488a4a70
Tina Wang <tinazwang@google.com>
2021-06-22 00:09:28 +0000

    🐻[Pandora] Change transition to FRE
    Currently we are showing the splash screen for longer to avoid
    displaying the NTP before showing the FRE. It's align with the
    behavior of legacy FRE, but it causes a issue that the splash
    screen will flash before showing settings page.
    
    The reason is that we should check if the FRE is on showing
    but not the flag value.
    
    Here is the screen record after fix:
    https://drive.google.com/file/d/1CD6nr3tbbHU5hW0RJxqQb4expJnD1OM4/view?usp=sharing
    
    Bug: 1214740,1222079
    Change-Id: Ic2dcb3ded6288242ccb8b0199af6ffd72ed8f5e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2977511
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894466}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/default_promo/default_promo_non_modal_presentation_delegate.h"
 #import "ios/chrome/browser/ui/download/download_manager_coordinator.h"
 #import "ios/chrome/browser/ui/elements/activity_overlay_coordinator.h"
+#import "ios/chrome/browser/ui/first_run/first_run_screen_view_controller.h"
 #import "ios/chrome/browser/ui/first_run/first_run_util.h"
 #import "ios/chrome/browser/ui/first_run/welcome_to_chrome_view_controller.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_animator.h"
@@ -1796,7 +1797,8 @@ NSString* const kBrowserViewControllerSnackbarCategory =
             viewControllerToPresent);
     if ([navController.topViewController
             isMemberOfClass:[WelcomeToChromeViewController class]] ||
-        base::FeatureList::IsEnabled(kEnableFREUIModuleIOS)) {
+        [navController.topViewController
+            isKindOfClass:[FirstRunScreenViewController class]]) {
       self.hideStatusBar = YES;
 
       // Load view from Launch Screen and add it to window.
