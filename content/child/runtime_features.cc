5637077f869f377c2320693122253128c5674f80
Benjamin Beaudry <bebeaudr@microsoft.com>
2021-06-22 21:16:47 +0000

    [Keyboard Tooltip] Split KeyboardAccessibleTooltip flag in two
    Unlinking the browser-side flag to its renderer-side counterpart so we
    can turn one on and not the other one. In Edge, we are going to start
    experimenting with the renderer-side flag enabled.
    
    While working on this CL, I noticed that I made a terrible mistake when
    I introduced the flag in the first place: the default value of the
    browser-side flag was set to true somehow instead of false. I fixed it
    in this CL, but I'll have to do a merge request to M92 to prevent any
    side-effect in the stable version.
    
    Bug: N/A
    Change-Id: I9d5656339fd80c81d8f7e0f326c93092f6309a43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973578
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#894864}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -328,7 +328,6 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnableWebXRPlaneDetection, features::kWebXrIncubations},
     {wf::EnableWebXRViewportScale, features::kWebXrIncubations},
     {wf::EnableWheelEventRegions, features::kWheelEventRegions},
-    {wf::EnableKeyboardAccessibleTooltip, features::kKeyboardAccessibleTooltip},
     {wf::EnableRemoveMobileViewportDoubleTap,
      features::kRemoveMobileViewportDoubleTap},
   };
