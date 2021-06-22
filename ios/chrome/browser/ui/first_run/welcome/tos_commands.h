28ac2c0c2cba8e3882948be8692a59ec3fa5b385
Aliona DANGLA <alionadangla@google.com>
2021-06-22 16:55:29 +0000

    [iOS] Fix ToS presentation
    The new FRE was using the old ToS screen. This is the new ToS screen
    following the new architecture and design.
    
    Bug: 1214738
    Change-Id: I5b887ec2b64f8fe014f98bb6604d0bfef8697eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2950761
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Cr-Commit-Position: refs/heads/master@{#894749}

---
+++ ios/chrome/browser/ui/first_run/welcome/tos_commands.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FIRST_RUN_WELCOME_TOS_COMMANDS_H_
+#define IOS_CHROME_BROWSER_UI_FIRST_RUN_WELCOME_TOS_COMMANDS_H_
+
+// Commands related to the Terms of Service (ToS) UI use in welcome screen from
+// the first run experience. It displays the ToS in modal view with a top
+// navigation bar with a button which should dismiss the page. Those commands
+// should manage the ToS coordinator.
+@protocol TOSCommands
+
+// Shows the ToS page.
+- (void)showTOSPage;
+
+// Hides the ToS page.
+- (void)hideTOSPage;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_WELCOME_TOS_COMMANDS_H_
