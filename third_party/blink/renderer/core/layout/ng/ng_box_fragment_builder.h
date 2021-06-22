22129cb3a27e7ff05cb1ced9f4d37475b217fd3b
Alison Maher <almaher@microsoft.com>
2021-06-22 19:32:56 +0000

    [LayoutNG] OOF with inline containing block in multicol
    This CL adds support for fragmented OOFs with an inline containing
    block. This involved determining the containing block info at the right
    time and collecting all of the inline items from all fragmentainers in
    order to compute the correct full containing block rect. Some of the
    work involved in this update included:
    
    1. Skipping over the call to ComputeInlineContainingBlocks() in
    NGOutOfFlowLayoutPart::LayoutCandidates() when inside a fragmentation
    context.
    
    2. Adding a similar method to ComputeInlineContainingBlocks() called
    ComputeInlineContainingBlocksForFragmentainer() that performs the
    same calculation but in the case of fragmentation. This is called
    in NGOutOfFlowLayoutPart::LayoutFragmentainerDescendants() in order
    to set up the correct ContainingBlockInfo for the inline containing
    block.
    
    3. Move the existing inline containing block helpers from
    NGBoxFragmentBuilder to a new utils file: InlineContainingBlockUtils.
    One such helper, ComputeInlineContainerGeometry(), required different
    logic in the case of fragmentation, so a new helper,
    ComputeInlineContainerGeometryForFragmentainer(), was added to handle
    the collection of inline items in this case. Besides the newly added
    method, no changes were made to the methods moved from
    NGBoxFragmentBuilder to InlineContainingBlockUtils.
    
    4. Move PreviousFragmentainerBreakToken() from NGOutOfFlowLayoutPart
    to ng_fragmentation_utils for easier access in
    InlineContainingBlockUtils.
    
    A few follow-up changes include handling the relpos offset for inline
    containing blocks, legacy writeback, and handling OOFs whose inline
    containing block is a direct child of the multicol (CL:2938502).
    
    Bug: 1079031
    Change-Id: Ie5b46b7e49304d5c198c2df2e6a6735f9d2fc68c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2969783
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894823}

--- third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.h
+++ third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.h
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/table/ng_table_fragment_data.h"
 #include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
-#include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
 namespace blink {
 
@@ -554,26 +553,6 @@ class CORE_EXPORT NGBoxFragmentBuilder final
   // Returns offset for given child. DCHECK if child not found.
   // Warning: Do not call unless necessary.
   LogicalOffset GetChildOffset(const LayoutObject* child) const;
-
-  // Inline containing block geometry is defined by two rectangles, generated
-  // by fragments of the LayoutInline.
-  struct InlineContainingBlockGeometry {
-    DISALLOW_NEW();
-    // Union of fragments generated on the first line.
-    PhysicalRect start_fragment_union_rect;
-    // Union of fragments generated on the last line.
-    PhysicalRect end_fragment_union_rect;
-  };
-
-  using InlineContainingBlockMap =
-      HashMap<const LayoutObject*,
-              absl::optional<InlineContainingBlockGeometry>>;
-
-  // Computes the geometry required for any inline containing blocks.
-  // |inline_containing_block_map| is a map whose keys specify which inline
-  // containing block geometry is required.
-  void ComputeInlineContainerGeometry(
-      InlineContainingBlockMap* inline_containing_block_map);
 
 #if DCHECK_IS_ON()
   // If we don't participate in a fragmentation context, this method can check
