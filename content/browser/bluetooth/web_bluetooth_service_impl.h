4568fd9ea68207bd704444f9c9465b9653b7dade
Chris Mumford <cmumford@chromium.org>
2021-06-22 16:16:15 +0000

    [Bluetooth] Add on-demand device pairing for descriptor read.
    Add support to WebBluetoothPairingManager for on-demand pairing
    when reading descriptors. This is will be unused until the
    write operations and UI code is complete.
    
    Bug: 960258
    Change-Id: Ifd1370cdf0724fd2b09524cc169742be003bd04c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941106
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894729}

--- content/browser/bluetooth/web_bluetooth_service_impl.h
+++ content/browser/bluetooth/web_bluetooth_service_impl.h
@@ -217,7 +217,7 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
       const absl::optional<device::BluetoothUUID>& characteristics_uuid,
       RemoteCharacteristicGetDescriptorsCallback callback) override;
   void RemoteDescriptorReadValue(
-      const std::string& characteristic_instance_id,
+      const std::string& descriptor_instance_id,
       RemoteDescriptorReadValueCallback callback) override;
   void RemoteDescriptorWriteValue(
       const std::string& descriptor_instance_id,
@@ -408,6 +408,8 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
   // WebBluetoothPairingManagerDelegate implementation:
   blink::WebBluetoothDeviceId GetCharacteristicDeviceID(
       const std::string& characteristic_instance_id) override;
+  blink::WebBluetoothDeviceId GetDescriptorDeviceId(
+      const std::string& descriptor_instance_id) override;
   void PairDevice(
       const blink::WebBluetoothDeviceId& device_id,
       device::BluetoothDevice::PairingDelegate* pairing_delegate,
