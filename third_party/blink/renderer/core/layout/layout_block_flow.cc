f8a9b59223b431f2b1315271cc3b6cfcd544e50f
Morten Stenshorne <mstensho@chromium.org>
2021-06-22 08:47:25 +0000

    Properly disallow custom layout + multicol, attempt 2.
    CL:2964675 wasn't enough for fieldsets that attempted both multicol and
    custom layout, since the column properties are inherited by the
    anonymous fieldset contents wrapper (while custom layout isn't
    inherited).
    
    Move checks from CreateOrDestroyMultiColumnFlowThreadIfNeeded() to a
    separate method, so that the fieldset code can call it and make sure
    that we don't let the anonymous wrapper inherit column properties in
    custom layout.
    
    Also add a DCHECK to catch any remaining bugs like this earlier (when
    building the layout tree, rather than in paint).
    
    Bug: 1221885
    Change-Id: Ibdf07cea4932e165fd15ccf131c1ab06984d7dd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2975442
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894578}

--- third_party/blink/renderer/core/layout/layout_block_flow.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow.cc
@@ -4283,6 +4283,26 @@ void LayoutBlockFlow::UpdateAncestorShouldPaintFloatingObject(
   }
 }
 
+bool LayoutBlockFlow::AllowsColumns() const {
+  // Ruby elements manage child insertion in a special way, and would mess up
+  // insertion of the flow thread. The flow thread needs to be a direct child of
+  // the multicol block (|this|).
+  if (IsRuby())
+    return false;
+
+  // We don't allow custom layout and multicol on the same object. This is
+  // similar to not allowing it for flexbox, grids and tables (although those
+  // don't create LayoutBlockFlow, so we don't need to check for those here).
+  if (StyleRef().IsDisplayLayoutCustomBox())
+    return false;
+
+  // MathML layout objects don't support multicol.
+  if (IsMathML())
+    return false;
+
+  return true;
+}
+
 bool LayoutBlockFlow::AllowsPaginationStrut() const {
   NOT_DESTROYED();
   // The block needs to be contained by a LayoutBlockFlow (and not by e.g. a
@@ -4466,10 +4486,7 @@ void LayoutBlockFlow::CreateOrDestroyMultiColumnFlowThreadIfNeeded(
   if (IsListItemIncludingNG())
     UseCounter::Count(GetDocument(), WebFeature::kMultiColAndListItem);
 
-  // Ruby elements manage child insertion in a special way, and would mess up
-  // insertion of the flow thread. The flow thread needs to be a direct child of
-  // the multicol block (|this|).
-  if (IsRuby())
+  if (!AllowsColumns())
     return;
 
   // Fieldsets look for a legend special child (layoutSpecialExcludedChild()).
@@ -4487,15 +4504,15 @@ void LayoutBlockFlow::CreateOrDestroyMultiColumnFlowThreadIfNeeded(
   if (element && element->IsFormControlElement())
     return;
 
-  // We don't allow custom layout and multicol on the same object. This is
-  // similar to not allowing it for flexbox, grids and tables (although those
-  // don't create LayoutBlockFlow, so we don't need to check for those here).
-  if (StyleRef().IsDisplayLayoutCustomBox())
-    return;
-
-  // MathML layout objects don't support multicol.
-  if (IsMathML())
-    return;
+  // Make sure that we don't attempt to create a LayoutNG multicol container
+  // when the feature isn't enabled. There is a mechanism that causes us to fall
+  // back to legacy layout if columns are specified when
+  // LayoutNGBlockFragmentation is disabled, but then there are cases where
+  // we'll override this and force NG anyway (if the layout type isn't
+  // implemented in the legacy engine, which is the case for things like custom
+  // layout).
+  DCHECK(!IsLayoutNGObject() ||
+         RuntimeEnabledFeatures::LayoutNGBlockFragmentationEnabled());
 
   auto* flow_thread = LayoutMultiColumnFlowThread::CreateAnonymous(
       GetDocument(), StyleRef(), !IsLayoutNGObject());
