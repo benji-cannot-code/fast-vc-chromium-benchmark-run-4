a5717bd55deaac2d3c242998aa133402f66fd442
siashah <siashah@chromium.org>
2021-06-22 19:40:06 +0000

    [VirtualCards] Log metrics for autofill error dialog view
    Bug: 1196021
    Change-Id: Idef9e4c6d905004c817edeac768405a52f0874fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965560
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894824}

--- chrome/browser/ui/autofill/payments/autofill_error_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/payments/autofill_error_dialog_controller_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/autofill/payments/autofill_error_dialog_controller_impl.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -28,6 +29,7 @@ void AutofillErrorDialogControllerImpl::Show(
   autofill_error_dialog_view_ = std::move(autofill_error_dialog_view);
   error_dialog_type_ = error_dialog_type;
   autofill_error_dialog_view_->Show();
+  base::UmaHistogramEnumeration("Autofill.ErrorDialogShown", error_dialog_type);
 }
 
 void AutofillErrorDialogControllerImpl::OnDismissed() {
