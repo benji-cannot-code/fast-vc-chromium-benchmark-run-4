892dcccf1b50db6395c43aee7c54f23d9a071468
Bence Béky <bnc@chromium.org>
2021-06-22 15:47:42 +0000

    Change QuicSmallOrderedSetImpl to base::flat_set.
    QuicSmallOrderedSet is used by QuicIntervalSet, which is used for
    keeping track of retransmissions and ACKs.  As described at
    https://source.chromium.org/chromium/chromium/src/+/main:net/third_party/quiche/src/quic/platform/api/quic_containers.h;l=16-18;drc=a97fbb7c9d8e1297453a5d45a36d487485b5de43
    this container usually contains few elements and therefore an
    implementation like base::flat_set which is optimized for that should be
    used.  See
    https://chromium.googlesource.com/chromium/src/+/main/base/containers/README.md#map-and-set-selection-usage-advice
    
    Change-Id: Iac0f7a6bf533480f3e496fb15ee0fa2fdb776a38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976460
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894716}

--- net/quic/platform/impl/quic_containers_impl.h
+++ net/quic/platform/impl/quic_containers_impl.h
@@ -6,11 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_QUIC_PLATFORM_IMPL_QUIC_CONTAINERS_IMPL_H_
 #define NET_QUIC_PLATFORM_IMPL_QUIC_CONTAINERS_IMPL_H_
 
-#include <memory>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "net/third_party/quiche/src/common/quiche_linked_hash_map.h"
-#include "third_party/abseil-cpp/absl/container/btree_set.h"
 
 namespace quic {
 
@@ -26,7 +25,7 @@ template <typename T, size_t N, typename A = std::allocator<T>>
 using QuicInlinedVectorImpl = std::vector<T, A>;
 
 template <typename Key, typename Compare>
-using QuicSmallOrderedSetImpl = absl::btree_set<Key, Compare>;
+using QuicSmallOrderedSetImpl = base::flat_set<Key, Compare>;
 
 }  // namespace quic
 
