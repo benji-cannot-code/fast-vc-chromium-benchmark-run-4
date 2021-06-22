e4865c04e481a8b1a360ec1974f1604cb7af10ca
Stephan Hartmann <stha09@googlemail.com>
2021-06-22 09:46:59 +0000

    libstdc++: remove format include
    format is C++20 feature that is not available in libstdc++ yet. But
    it is also not required in this context.
    
    Bug: 957519
    Change-Id: Ic17d9815bd8aca7e597c30cace9a66cee1ea63b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972980
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#894598}

--- components/segmentation_platform/internal/execution/model_execution_manager_impl.cc
+++ components/segmentation_platform/internal/execution/model_execution_manager_impl.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/execution/model_execution_manager_impl.h"
 
 #include <deque>
-#include <format>
 #include <map>
 #include <memory>
 #include <vector>
