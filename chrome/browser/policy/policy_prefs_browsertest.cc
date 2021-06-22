f089caf4d761740444671309c89d9208036330c3
Takumi Fujimoto <takumif@chromium.org>
2021-06-22 17:55:18 +0000

    Disable PolicyPrefsTestCoverageTest.AllPoliciesHaveATestCase on Mac
    The test is failing on multiple Mac bots and it looks like we'd need to
    revert at least three CLs, so I'm disabling it instead.
    
    Bug: 1222709, 1151251
    Change-Id: I6162466610265a86aa293836d17ffdaaf6787fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2979173
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894788}

--- chrome/browser/policy/policy_prefs_browsertest.cc
+++ chrome/browser/policy/policy_prefs_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
@@ -59,7 +60,14 @@ base::FilePath GetTestCasePath() {
 
 typedef PlatformBrowserTest PolicyPrefsTestCoverageTest;
 
-IN_PROC_BROWSER_TEST_F(PolicyPrefsTestCoverageTest, AllPoliciesHaveATestCase) {
+// TODO(crbug.com/1222709): Failing on Mac.
+#if defined(OS_MAC)
+#define MAYBE_AllPoliciesHaveATestCase DISABLED_AllPoliciesHaveATestCase
+#else
+#define MAYBE_AllPoliciesHaveATestCase AllPoliciesHaveATestCase
+#endif
+IN_PROC_BROWSER_TEST_F(PolicyPrefsTestCoverageTest,
+                       MAYBE_AllPoliciesHaveATestCase) {
   VerifyAllPoliciesHaveATestCase(GetTestCasePath());
 }
 
