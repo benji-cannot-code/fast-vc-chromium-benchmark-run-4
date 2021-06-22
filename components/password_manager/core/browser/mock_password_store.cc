462ee589e85862311ccada789ebf7bd3e90364f1
Viktor Semeniuk <vsemeniuk@google.com>
2021-06-22 15:45:25 +0000

    Dropping initial phished credentials upload
    Bug: 1137775
    Change-Id: I0fcc31583cb9846eacde0ce8e39d105439f9caa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966499
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#894712}

--- components/password_manager/core/browser/mock_password_store.cc
+++ components/password_manager/core/browser/mock_password_store.cc
@@ -19,8 +19,7 @@ MockPasswordStore::CreateBackgroundTaskRunner() const {
   return base::SequencedTaskRunnerHandle::Get();
 }
 
-bool MockPasswordStore::InitOnBackgroundSequence(
-    bool upload_phished_credentials_to_sync) {
+bool MockPasswordStore::InitOnBackgroundSequence() {
   return true;
 }
 
