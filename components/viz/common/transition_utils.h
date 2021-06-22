10642d38b9f568d6843c71ac648963dd7cf7514c
Khushal <khushalsagar@chromium.org>
2021-06-22 20:07:59 +0000

    viz: Remove shared elements from src content copy requests.
    When taking snapshots of shared elements to prepare for a transition,
    ensure that each shared element is only drawn in its own layer. This
    covers both the root element and the case of a shared element nested
    inside another shared element.
    
    R=kylechar@chromium.org
    
    Change-Id: I3912f6e72215b4d3cdee38d4333527a08836228d
    Bug: 1221335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972764
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894843}

--- components/viz/common/transition_utils.h
+++ components/viz/common/transition_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_COMMON_TRANSITION_UTILS_H_
 #define COMPONENTS_VIZ_COMMON_TRANSITION_UTILS_H_
 
+#include "base/callback_forward.h"
 #include "components/viz/common/quads/compositor_render_pass.h"
 #include "components/viz/common/viz_common_export.h"
 
@@ -22,6 +23,22 @@ class VIZ_COMMON_EXPORT TransitionUtils {
   static float ComputeAccumulatedOpacity(
       const CompositorRenderPassList& render_passes,
       CompositorRenderPassId target_id);
+
+  // Creates a deep copy of |source_pass| retaining all state except copy
+  // requests. |filter_callback| is invoked for each render pass draw quad to
+  // let the caller modify the copy of these quads. If the callback returns true
+  // the quad is skipped otherwise it is copied as-is.
+  using FilterCallback =
+      base::RepeatingCallback<bool(const CompositorRenderPassDrawQuad&,
+                                   CompositorRenderPass&)>;
+  static std::unique_ptr<CompositorRenderPass> CopyPassWithRenderPassFiltering(
+      const CompositorRenderPass& source_pass,
+      FilterCallback filter_callback);
+
+  static CompositorRenderPassId NextRenderPassId(
+      const CompositorRenderPassId& id) {
+    return CompositorRenderPassId(id.GetUnsafeValue() + 1);
+  }
 };
 
 }  // namespace viz
