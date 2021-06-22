8dc946bd066a0136fb47d53b12d13878c075d2fb
Hiroshige Hayashizaki <hiroshige@chromium.org>
2021-06-22 01:36:45 +0000

    Do not evaluate isolated world scripts if frame is detached
    This fixes crashes when content scripts detach Windows
    during evaluation.
    
    Bug: 1220761
    Change-Id: I0469dce23e73256cf66a370c66ba23a35d365bb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967765
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894496}

---
+++ chrome/test/data/extensions/api_test/content_scripts/detach_during_evaluation/detach.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+{
+  const div = parent.document.createElement('div');
+  div.setAttribute('id', 'detach-evaluated');
+  parent.document.body.appendChild(div);
+}
+
+parent.document.querySelector('#injected').remove();
