241041746a231d4cfeffe26239cac065e4a2f0b2
Rouslan Solomakhin <rouslan@chromium.org>
2021-06-23 18:18:55 +0000

    [Web Payment] Require SPC instrument during payment and sign it.
    Before this patch, SPC generated its own challenge field from the
    payment information and signed it. The instrument icon and name were
    collected only during enrollment.
    
    This patch adds the required "instrument" field to the
    SecurePaymentConfirmationRequest dictionary, when
    SecurePaymentConfirmationAPIV2 flag is enabled. The "instrument" field
    contains the displayName and icon of the instrument, identical to the
    enrollment phase.  The renderer passes the "instrument" field to the
    browser. Once the user approves the SPC payment dialog, the "instrument
    info" and "total amount" fields are passed to WebAuthn to sign together
    with other client data. Since the client data now contains the payment
    specific information, the browser no longer modifies the challenge being
    sent to WebAuthn.
    
    After this patch, if SecurePaymentConfirmationAPIV2 flag is enabled,
    WebAuthn generates clientDataJSON from payment information, so the
    RP's server-side "challenge" value remains unaltered.
    
    Bug: 1210488
    Change-Id: I774257a55f838998569d583496a72700d582d24b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976692
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895234}

--- third_party/blink/renderer/modules/payments/secure_payment_confirmation_type_converter.cc
+++ third_party/blink/renderer/modules/payments/secure_payment_confirmation_type_converter.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_arraybuffer_arraybufferview.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_payment_credential_instrument.h"
 #include "third_party/blink/renderer/modules/credentialmanager/credential_manager_type_converters.h"
+#include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace mojo {
@@ -49,6 +51,14 @@ TypeConverter<payments::mojom::blink::SecurePaymentConfirmationRequestPtr,
   // through mojo IPC, so the browser can set a default (e.g., 3 minutes).
   if (input->hasTimeout())
     output->timeout = base::TimeDelta::FromMilliseconds(input->timeout());
+
+  output->instrument =
+      blink::RuntimeEnabledFeatures::SecurePaymentConfirmationAPIV2Enabled()
+          ? payments::mojom::blink::PaymentCredentialInstrument::New(
+                input->instrument()->displayName(),
+                blink::KURL(input->instrument()->icon()))
+          : payments::mojom::blink::PaymentCredentialInstrument::New(
+                "", blink::KURL());
 
   return output;
 }
