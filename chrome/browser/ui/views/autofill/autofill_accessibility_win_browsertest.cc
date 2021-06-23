18a676fd15f225b80031e119c592012599d05518
Yuki Yamada <yukiy@chromium.org>
2021-06-23 02:00:08 +0000

    Disable AutofillAccessibilityWinBrowserTest.AutofillPopupControllerFor
    Disable the test on Windows because it's flaky.
    
    Bug: 1221273
    Change-Id: I6213ead8240f16451a82b05542628d340301c46f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976344
    Auto-Submit: Yuki Yamada <yukiy@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#894982}

--- chrome/browser/ui/views/autofill/autofill_accessibility_win_browsertest.cc
+++ chrome/browser/ui/views/autofill/autofill_accessibility_win_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/win/scoped_variant.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/accessibility/uia_accessibility_event_waiter.h"
@@ -79,8 +80,14 @@ class AutofillAccessibilityWinBrowserTest : public InProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(AutofillAccessibilityWinBrowserTest);
 };
 
+// The test is flaky on Windows. See https://crbug.com/1221273
+#if defined(OS_WIN)
+#define MAYBE_AutofillPopupControllerFor DISABLED_AutofillPopupControllerFor
+#else
+#define MAYBE_AutofillPopupControllerFor AutofillPopupControllerFor
+#endif
 IN_PROC_BROWSER_TEST_F(AutofillAccessibilityWinBrowserTest,
-                       AutofillPopupControllerFor) {
+                       MAYBE_AutofillPopupControllerFor) {
   content::AccessibilityNotificationWaiter waiter(
       GetWebContents(), ui::kAXModeComplete, ax::mojom::Event::kLoadComplete);
   ui_test_utils::NavigateToURL(
