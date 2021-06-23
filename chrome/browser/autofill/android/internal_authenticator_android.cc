d30cab9df267f71c0e0b87d06e45af2245b55cbd
Liquan (Max) Gu <maxlg@chromium.org>
2021-06-23 17:38:40 +0000

    [WebLayerSPC] Remove copy from some JavaByteBufferToNativeByteVector()
    Context:
    In order to support SPC on WebLayer, its dependency
    internal_authenticator_android needs to be moved into //components to
    be shareable between //chrome and //weblayer. Specifically, this file
    should be moved into //components/webauthn which is logical for payments
    and Autofill can depend on. However, this file uses the
    JavaByteBufferToNativeByteVector() method which is a payments method.
    So this CL refactors this method so that the file can remove the payment
    dependency.
    
    Changes:
    * Inlines the method.
    * Removes the unnecessary copying. Without the copying part, the method
      becomes too trivial to reuse, which justifies the inlining.
    
    Bug: 1204561
    Change-Id: Ic4ea1d7e0e67a3b611e68a00365db0fc16d0c148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956449
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895215}

--- chrome/browser/autofill/android/internal_authenticator_android.cc
+++ chrome/browser/autofill/android/internal_authenticator_android.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/timer/timer.h"
 #include "chrome/android/chrome_jni_headers/InternalAuthenticator_jni.h"
-#include "components/payments/content/android/byte_buffer_helper.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/base/time_mojom_traits.h"
@@ -123,10 +122,11 @@ void InternalAuthenticatorAndroid::InvokeMakeCredentialResponse(
 
   // |byte_buffer| may be null if authentication failed.
   if (byte_buffer) {
+    jbyte* buf_in =
+        static_cast<jbyte*>(env->GetDirectBufferAddress(byte_buffer.obj()));
+    jlong buf_size = env->GetDirectBufferCapacity(byte_buffer.obj());
     blink::mojom::MakeCredentialAuthenticatorResponse::Deserialize(
-        std::move(payments::android::JavaByteBufferToNativeByteVector(
-            env, byte_buffer)),
-        &response);
+        buf_in, buf_size, &response);
   }
 
   std::move(make_credential_response_callback_)
@@ -142,10 +142,11 @@ void InternalAuthenticatorAndroid::InvokeGetAssertionResponse(
 
   // |byte_buffer| may be null if authentication failed.
   if (byte_buffer) {
+    jbyte* buf_in =
+        static_cast<jbyte*>(env->GetDirectBufferAddress(byte_buffer.obj()));
+    jlong buf_size = env->GetDirectBufferCapacity(byte_buffer.obj());
     blink::mojom::GetAssertionAuthenticatorResponse::Deserialize(
-        std::move(payments::android::JavaByteBufferToNativeByteVector(
-            env, byte_buffer)),
-        &response);
+        buf_in, buf_size, &response);
   }
 
   std::move(get_assertion_response_callback_)
