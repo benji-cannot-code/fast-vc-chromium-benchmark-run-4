0b3b4d18543f28ab90ecef65637d04921072376f
Gavin Williams <gavinwill@chromium.org>
2021-06-23 18:45:36 +0000

    scanning: Show checkbox for enabling multi-page scanning
    - Only show the checkbox when Flatbed and PDF settings are chosen
    
    - Hide the checkbox behind the kScanAppMultiPageScan flag
    
    screenshot: http://screen/8oHbGwjogqHZBqE
    
    Bug: 1210271
    Change-Id: I3f7346f9b0845ca31341ac3e11c7e9c5b49aa0da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2979918
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895252}

--- chrome/test/data/webui/chromeos/scanning/scanning_app_browsertest.js
+++ chrome/test/data/webui/chromeos/scanning/scanning_app_browsertest.js
@@ -38,6 +38,7 @@ ScanningAppBrowserTest.prototype = {
   featureList: {
     enabled: [
       'chromeos::features::kScanAppMediaLink',
+      'chromeos::features::kScanAppMultiPageScan',
       'chromeos::features::kScanAppSearchablePdf',
       'chromeos::features::kScanAppStickySettings',
     ]
