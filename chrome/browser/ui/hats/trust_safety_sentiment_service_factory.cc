7f2f6490596dad837f98e49166c17ff4ff28f4a7
sauski <sauski@google.com>
2021-06-23 11:43:03 +0000

    T&S Sentiment: Add trigger action input for Privacy Settings
    Extended the TrustSafetySentimentService to include logic for
    Privacy Settings triggers:
      - Spending time on settings after interacting with privacy settings
      - Running Safety Check
    
    To support the first trigger, a SettingWatcher nested class is added
    to the sentiment service to watch a WebContents to confirm a user has
    stayed on settings.
    
    The set of Product Specific Data for the Privacy Settings feature area
    is also included, this consists of 1 bit indicating whether the user is
    being surveyed because they ran safety check, and 1 bit indicating
    whether the user has any non-default core privacy settings.
    
    Bug: 1221076
    Change-Id: I99bdedb2d1257a505930bf090aa8622e0dc78f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2978514
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895090}

--- chrome/browser/ui/hats/trust_safety_sentiment_service_factory.cc
+++ chrome/browser/ui/hats/trust_safety_sentiment_service_factory.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "chrome/browser/ui/hats/trust_safety_sentiment_service_factory.h"
 
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/ui/hats/hats_service.h"
 #include "chrome/browser/ui/hats/hats_service_factory.h"
 #include "chrome/common/chrome_features.h"
@@ -15,6 +16,7 @@ TrustSafetySentimentServiceFactory::TrustSafetySentimentServiceFactory()
           "TrustSafetySentimentService",
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(HatsServiceFactory::GetInstance());
+  DependsOn(HostContentSettingsMapFactory::GetInstance());
 }
 
 TrustSafetySentimentServiceFactory*
