bf56f1e1c92d1b51722e1b33f24ad51e51439a67
Anastasia Helfinstein <anastasi@google.com>
2021-06-23 23:49:07 +0000

    [Switch Access] Setup guide: Handle case with >1 switch
    This change adds new placeholder pages for assigning switches to NEXT
    and PREVIOUS, and adds logic to decide which pages to show based on
    how many switches the user chose to use.
    
    AX-Relnotes: n/a.
    Test: Modified existing navigation test.
    Bug: 1207694
    Change-Id: Ideb9ce4ac99df15474420716191369214d716788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965601
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#895379}

--- chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_setup_guide_dialog.js
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_setup_guide_dialog.js
@@ -65,6 +65,7 @@ SASetupPageList[SASetupPageId.INTRO] = {
   ]
 };
 
+// Placeholder.
 SASetupPageList[SASetupPageId.ASSIGN_SELECT] = {
   titleId: 'switchAccessSetupIntroTitle',
   visibleElements: [
@@ -94,6 +95,24 @@ SASetupPageList[SASetupPageId.AUTO_SCAN_SPEED] = {
   visibleElements: [
     SASetupElement.NEXT_BUTTON, SASetupElement.PREVIOUS_BUTTON,
     SASetupElement.AUTO_SCAN_SPEED_CONTENT
+  ]
+};
+
+// Placeholder.
+SASetupPageList[SASetupPageId.ASSIGN_NEXT] = {
+  titleId: 'switchAccessSetupIntroTitle',
+  visibleElements: [
+    SASetupElement.NEXT_BUTTON, SASetupElement.PREVIOUS_BUTTON,
+    SASetupElement.ASSIGN_SELECT_CONTENT
+  ]
+};
+
+// Placeholder.
+SASetupPageList[SASetupPageId.ASSIGN_PREVIOUS] = {
+  titleId: 'switchAccessSetupIntroTitle',
+  visibleElements: [
+    SASetupElement.NEXT_BUTTON, SASetupElement.PREVIOUS_BUTTON,
+    SASetupElement.ASSIGN_SELECT_CONTENT
   ]
 };
 
@@ -221,7 +240,18 @@ Polymer({
       case SASetupPageId.AUTO_SCAN_ENABLED:
         return SASetupPageId.CHOOSE_SWITCH_COUNT;
       case SASetupPageId.CHOOSE_SWITCH_COUNT:
-        return SASetupPageId.AUTO_SCAN_SPEED;
+        if (this.switchCount_ === 3 || this.switchCount_ === 2) {
+          return SASetupPageId.ASSIGN_NEXT;
+        } else {
+          return SASetupPageId.AUTO_SCAN_SPEED;
+        }
+      case SASetupPageId.ASSIGN_NEXT:
+        if (this.switchCount_ === 3) {
+          return SASetupPageId.ASSIGN_PREVIOUS;
+        } else {
+          return SASetupPageId.CLOSING;
+        }
+      case SASetupPageId.ASSIGN_PREVIOUS:
       case SASetupPageId.AUTO_SCAN_SPEED:
       default:
         return SASetupPageId.CLOSING;
@@ -236,7 +266,16 @@ Polymer({
   getPreviousPageId_() {
     switch (this.currentPageId_) {
       case SASetupPageId.CLOSING:
-        return SASetupPageId.AUTO_SCAN_SPEED;
+        if (this.switchCount_ === 3) {
+          return SASetupPageId.ASSIGN_PREVIOUS;
+        } else if (this.switchCount_ === 2) {
+          return SASetupPageId.ASSIGN_NEXT;
+        } else {
+          return SASetupPageId.AUTO_SCAN_SPEED;
+        }
+      case SASetupPageId.ASSIGN_PREVIOUS:
+        return SASetupPageId.ASSIGN_NEXT;
+      case SASetupPageId.ASSIGN_NEXT:
       case SASetupPageId.AUTO_SCAN_SPEED:
         return SASetupPageId.CHOOSE_SWITCH_COUNT;
       case SASetupPageId.CHOOSE_SWITCH_COUNT:
@@ -269,6 +308,18 @@ Polymer({
           this.getPref('settings.a11y.switch_access.auto_scan.enabled').value);
       chrome.settingsPrivate.setPref(
           'settings.a11y.switch_access.auto_scan.enabled', true);
+    }
+
+    if (this.currentPageId_ === SASetupPageId.CLOSING) {
+      if (this.switchCount_ >= 2) {
+        chrome.settingsPrivate.setPref(
+            'settings.a11y.switch_access.auto_scan.enabled', false);
+        this['$']['closing-instructions'].textContent =
+            this.i18n('switchAccessSetupClosingManualScanInstructions');
+      } else {
+        this['$']['closing-instructions'].textContent =
+            this.i18n('switchAccessSetupClosingAutoScanInstructions');
+      }
     }
   },
 
@@ -349,6 +400,7 @@ Polymer({
   /**
    * @param {!Array<number>} ticksInMs
    * @return {!Array<!cr_slider.SliderTick>}
+   * @private
    */
   ticksWithLabelsInSec_(ticksInMs) {
     // Dividing by 1000 to convert milliseconds to seconds for the label.
