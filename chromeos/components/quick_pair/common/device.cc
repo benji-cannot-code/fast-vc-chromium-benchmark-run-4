57b9879eafa7a8a6d0f6ddaf192b06e64631780d
Shane Fitzpatrick <shanefitz@google.com>
2021-06-23 22:09:38 +0000

    [FastPair] Device and Protocol classes
    This changes adds the 'Device' and 'Protocol' classes for the Quick
    Pair System.
    
    'Device' is a thin immutable struct which is copyable and moveable.
    
    'Protocol' is an enum class with a single entry - Fast Pair.
    
    Change-Id: Icba527ef2784295b4ec9567c9f29b8888dfa00ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982311
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895336}

---
+++ chromeos/components/quick_pair/common/device.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/quick_pair/common/device.h"
+
+#include "chromeos/components/quick_pair/common/protocol.h"
+
+namespace chromeos {
+namespace quick_pair {
+
+Device::Device(std::string metadata_id, std::string address, Protocol protocol)
+    : metadata_id(std::move(metadata_id)),
+      address(std::move(address)),
+      protocol(protocol) {}
+
+}  // namespace quick_pair
+}  // namespace chromeos
