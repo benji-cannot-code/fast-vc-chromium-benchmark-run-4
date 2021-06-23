f91f2bcdb07a9117fe767b5d272ce172c143b600
Sebastien Marchand <sebmarchand@chromium.org>
2021-06-23 23:15:40 +0000

    [battery] Record the screen brightness
    Only available on Mac with an Intel processor.
    
    Change-Id: I3fc196024f5fb3125a5963d7b8ecd1834774792d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2956909
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895369}

---
+++ chrome/browser/metrics/power/power_details_provider.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_METRICS_POWER_POWER_DETAILS_PROVIDER_H_
+#define CHROME_BROWSER_METRICS_POWER_POWER_DETAILS_PROVIDER_H_
+
+#include <memory>
+
+// Used to retrieve some power related metrics.
+class PowerDetailsProvider {
+ public:
+  static constexpr double kInvalidScreenBrightness = -1.0;
+
+  // Creates a platform specific PowerDetailsProvider.
+  static std::unique_ptr<PowerDetailsProvider> Create();
+
+  virtual ~PowerDetailsProvider() = default;
+
+  PowerDetailsProvider(const PowerDetailsProvider& other) = delete;
+  PowerDetailsProvider& operator=(const PowerDetailsProvider& other) = delete;
+
+  // Returns the brightness of the main screen when available,
+  // |kInvalidScreenBrightness| otherwise.
+  virtual double GetMainScreenBrightnessLevel() = 0;
+
+ protected:
+  PowerDetailsProvider() = default;
+};
+
+#endif  // CHROME_BROWSER_METRICS_POWER_POWER_DETAILS_PROVIDER_H_
--- chrome/browser/metrics/power/power_metrics_reporter.cc
+++ chrome/browser/metrics/power/power_metrics_reporter.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/lifetime/browser_shutdown.h"
+#include "chrome/browser/metrics/power/power_details_provider.h"
 #include "chrome/browser/performance_monitor/process_metrics_recorder_util.h"
 #include "chrome/browser/performance_monitor/process_monitor.h"
 #include "services/metrics/public/cpp/metrics_utils.h"
@@ -27,6 +28,10 @@ constexpr const char* kBatteryDischargeModeHistogramName =
 constexpr const char* kZeroWindowSuffix = ".ZeroWindow";
 constexpr const char* kBatterySamplingDelayHistogramName =
     "Power.BatterySamplingDelay";
+constexpr const char* kMainScreenBrightnessHistogramName =
+    "Power.MainScreenBrightness";
+constexpr const char* kMainScreenBrightnessAvailableHistogramName =
+    "Power.MainScreenBrightnessAvailable";
 
 // Calculates the UKM bucket |value| falls in and returns it. This uses an
 // exponential bucketing approach with an exponent base of 1.3, resulting in
@@ -57,6 +62,10 @@ PowerMetricsReporter::PowerMetricsReporter(
   battery_level_provider_->GetBatteryState(
       base::BindOnce(&PowerMetricsReporter::OnFirstBatteryStateSampled,
                      weak_factory_.GetWeakPtr()));
+
+#if defined(OS_MAC)
+  power_details_provider_ = PowerDetailsProvider::Create();
+#endif
 }
 
 PowerMetricsReporter::~PowerMetricsReporter() {
@@ -205,6 +214,22 @@ void PowerMetricsReporter::ReportUKMsAndHistograms(
   ReportBatteryHistograms(interval_data, sampling_interval, interval_duration,
                           discharge_mode, discharge_rate_during_interval,
                           suffixes);
+
+  bool brightness_read_successfully = false;
+  if (power_details_provider_.get()) {
+    auto brightness = power_details_provider_->GetMainScreenBrightnessLevel();
+    if (brightness != PowerDetailsProvider::kInvalidScreenBrightness) {
+      // Report the percentage as an integer as UMA doesn't allow reporting
+      // reals.
+      int brightness_int = brightness * 100;
+      DCHECK_GE(100, brightness_int);
+      brightness_read_successfully = true;
+      base::UmaHistogramPercentage(kMainScreenBrightnessHistogramName,
+                                   brightness_int);
+    }
+  }
+  base::UmaHistogramBoolean(kMainScreenBrightnessAvailableHistogramName,
+                            brightness_read_successfully);
 }
 
 // static
