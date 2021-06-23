0a63431fdd98fe5565529d1d0cc1b4471b1155cb
Oleg Davydov <burunduk@chromium.org>
2021-06-23 17:40:40 +0000

    [App mode] Fix UB in StartupAppLauncherUpdateChecker::Observe
    Notification NOTIFICATION_EXTENSION_UPDATE_FOUND is supplied with
    extensions::UpdateDetails, which is declared in extension_downloader.h.
    But the method mentioned used pair<string, Version> instead, which is
    the same data as UpdateDetails has, but exact binary structure is not
    guaranteed. Also the method used data with references to temporary
    objects, which looked like undefined behaviour (so, may be subject to
    incorrect compiler optimization).
    
    Presumably, this will fix crash mentioned in the bug.
    
    Bug: 1210082
    Change-Id: Ibb1099c3a37a74b6ced9e40dcd3fc9481206a49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982461
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Auto-Submit: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895217}

--- chrome/browser/ash/app_mode/startup_app_launcher_update_checker.cc
+++ chrome/browser/ash/app_mode/startup_app_launcher_update_checker.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/notification_service.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/browser/updater/extension_downloader.h"
 
 namespace ash {
 
@@ -58,12 +59,9 @@ void StartupAppLauncherUpdateChecker::Observe(
     const content::NotificationDetails& details) {
   DCHECK_EQ(extensions::NOTIFICATION_EXTENSION_UPDATE_FOUND, type);
 
-  using UpdateDetails = const std::pair<std::string, base::Version>;
-  const std::string& id = content::Details<UpdateDetails>(details)->first;
-  const base::Version& version =
-      content::Details<UpdateDetails>(details)->second;
-  SYSLOG(INFO) << "Found extension update id=" << id
-               << " version=" << version.GetString();
+  const content::Details<extensions::UpdateDetails> update_details(details);
+  SYSLOG(INFO) << "Found extension update id=" << update_details->id
+               << " version=" << update_details->version.GetString();
   update_found_ = true;
 }
 
