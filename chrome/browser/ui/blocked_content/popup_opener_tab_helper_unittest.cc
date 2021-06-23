47b972c3d8be5ceec717736e6457cc2b604c0999
Gyuyoung Kim <gyuyoung@igalia.com>
2021-06-23 14:32:27 +0000

    MPArch: Only allow creating TabUnderNavigationThrottle on the primary main frame
    As one of the MPArch tasks, the prerendering cannot create a
    new object of TabUnderNavigationThrottle. In other words,
    the primary mainframe only can create a new object. So, this
    CL migrates IsInMainFrame() to IsInPrimaryMainFrame() in
    TabUnderNavigationThrottle::MaybeCreate in order to create
    a new object only on the primary mainframe.
    
    And, this CL adds a new unit test to check if the prerendering
    cannot create a new TabUnderNavigationThrottle object.
    
    Bug: 1221067
    Change-Id: I8e60e92544c9f7a0da3932432c428d77addab8ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2967705
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#895135}

--- chrome/browser/ui/blocked_content/popup_opener_tab_helper_unittest.cc
+++ chrome/browser/ui/blocked_content/popup_opener_tab_helper_unittest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ukm/content/source_url_recorder.h"
 #include "components/ukm/test_ukm_recorder.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/test/mock_navigation_handle.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
@@ -711,6 +712,17 @@ TEST_F(BlockTabUnderTest,
             simulator->GetLastThrottleCheckResult());
   simulator->Commit();
   ExpectUIShown(false);
+}
+
+TEST_F(BlockTabUnderTest, OnlyCreateThrottleForPrimaryMainframe) {
+  content::MockNavigationHandle handle(GURL("http://example.com"), main_rfh());
+  handle.set_is_in_primary_main_frame(true);
+  auto throttle = TabUnderNavigationThrottle::MaybeCreate(&handle);
+  EXPECT_NE(throttle, nullptr);
+
+  handle.set_is_in_primary_main_frame(false);
+  auto throttle2 = TabUnderNavigationThrottle::MaybeCreate(&handle);
+  EXPECT_EQ(throttle2, nullptr);
 }
 
 class BlockTabUnderDisabledTest : public BlockTabUnderTest {
