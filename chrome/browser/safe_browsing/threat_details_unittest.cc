49085e376bd9ae2e73dbb1661eaac38ba5096ae1
Rakina Zata Amni <rakina@chromium.org>
2021-06-23 23:40:08 +0000

    Calculate DidCommitProvisionalLoadParams' referrer in the browser
    DidCommitProvisionalLoadParams has the `referrer` param that is used
    to save the `referrer` attribute in FrameNavigationEntry after commit,
    calculated using various information in the renderer.
    
    We are planning to remove the `referrer` param entirely, and instead
    calculate the referrer to save in FrameNavigationEntry entirely in the
    browser. This CL adds the referrer calculation for most navigations in
    the browser, overwriting DidCommitProvisionalLoadParams' version. A
    future CL will add the referrer calculation for the rest of the
    navigations (same-document navigation & synchronous about:blank
    navigation) and completely remove the DidCommitProvisionalLoadParams
    version.
    
    Since this CL removes the need of most referrer calculation logic in
    the renderer side, it deletes some parts that are no longer needed in
    the renderer. It also fixes some bugs that are caused by the renderer
    calculations.
    
    Bug: 1171210, 1131832
    Change-Id: Ic87819306bef99fcd38b1733944a9f78118900bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2933038
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: David Van Cleve <davidvc@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895375}

--- chrome/browser/safe_browsing/threat_details_unittest.cc
+++ chrome/browser/safe_browsing/threat_details_unittest.cc
@@ -432,9 +432,8 @@ TEST_F(ThreatDetailsTest, ThreatSubResource) {
   expected.set_type(ClientSafeBrowsingReportRequest::URL_MALWARE);
   expected.set_url(kThreatURL);
   expected.set_page_url(kLandingURL);
-  // Note that the referrer policy is not actually enacted here, since that's
-  // done in Blink.
-  expected.set_referrer_url(kReferrerURL);
+  // The referrer is stripped to its origin because it's a cross-origin URL.
+  expected.set_referrer_url(GURL(kReferrerURL).GetOrigin().spec());
   expected.set_did_proceed(true);
   expected.set_repeat_visit(true);
 
@@ -447,7 +446,7 @@ TEST_F(ThreatDetailsTest, ThreatSubResource) {
   pb_resource->set_url(kThreatURL);
   pb_resource = expected.add_resources();
   pb_resource->set_id(2);
-  pb_resource->set_url(kReferrerURL);
+  pb_resource->set_url(GURL(kReferrerURL).GetOrigin().spec());
 
   VerifyResults(actual, expected);
 }
@@ -490,9 +489,8 @@ TEST_F(ThreatDetailsTest, SuspiciousSiteWithReferrerChain) {
       ClientSafeBrowsingReportRequest::PVER4_NATIVE);
   expected.set_url(kThreatURL);
   expected.set_page_url(kLandingURL);
-  // Note that the referrer policy is not actually enacted here, since that's
-  // done in Blink.
-  expected.set_referrer_url(kReferrerURL);
+  // The referrer is stripped to its origin because it's a cross-origin URL.
+  expected.set_referrer_url(GURL(kReferrerURL).GetOrigin().spec());
   expected.set_did_proceed(true);
   expected.set_repeat_visit(true);
 
@@ -505,7 +503,7 @@ TEST_F(ThreatDetailsTest, SuspiciousSiteWithReferrerChain) {
   pb_resource->set_url(kThreatURL);
   pb_resource = expected.add_resources();
   pb_resource->set_id(2);
-  pb_resource->set_url(kReferrerURL);
+  pb_resource->set_url(GURL(kReferrerURL).GetOrigin().spec());
 
   // Make sure the referrer chain returned by the provider is copied into the
   // resulting proto.
@@ -1511,9 +1509,8 @@ TEST_F(ThreatDetailsTest, ThreatWithPendingLoad) {
       ClientSafeBrowsingReportRequest::PVER4_NATIVE);
   expected.set_url(kThreatURL);
   expected.set_page_url(kLandingURL);
-  // Note that the referrer policy is not actually enacted here, since that's
-  // done in Blink.
-  expected.set_referrer_url(kReferrerURL);
+  // The referrer is stripped to its origin because it's a cross-origin URL.
+  expected.set_referrer_url(GURL(kReferrerURL).GetOrigin().spec());
   expected.set_did_proceed(true);
   expected.set_repeat_visit(true);
 
@@ -1526,7 +1523,7 @@ TEST_F(ThreatDetailsTest, ThreatWithPendingLoad) {
   pb_resource->set_url(kThreatURL);
   pb_resource = expected.add_resources();
   pb_resource->set_id(2);
-  pb_resource->set_url(kReferrerURL);
+  pb_resource->set_url(GURL(kReferrerURL).GetOrigin().spec());
 
   VerifyResults(actual, expected);
 }
