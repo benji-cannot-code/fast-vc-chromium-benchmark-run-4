a2e58ba9637e209616e06b5e6518be09ed778500
Tanmoy Mollik <triploblastic@chromium.org>
2021-06-23 15:33:25 +0000

    [ChromeActivity] Move ModalDialogManager lifecycle from AsyncInitializationActivity to ChromeBaseAppCompatActivity
    We want to use modal dialog manager in SyncConsentActivity and
    FirstRunActivity. SyncConsentActivity branches out from
    ChromeBaseAppCompatActivity, while FirstRunActivity branches out
    separately from AsyncInitializationActivity.
    
    This cl moves the creation logic of ModalDialogManager to the parent
    class of AsyncInitializationActivity - ChromeBaseAppCompatActivity.
    This way the lifecycle of ModalDialogManager for all the chrome
    activity subclasses will be in a common place.
    
    Bug: 1197194
    Change-Id: I0d9faeecbaa832b041fabc1a01e4ead60d3b3c5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960458
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895157}

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -33,8 +33,6 @@ import org.chromium.base.TraceEvent;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LoaderErrors;
 import org.chromium.base.library_loader.ProcessInitException;
-import org.chromium.base.supplier.ObservableSupplier;
-import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeBaseAppCompatActivity;
 import org.chromium.chrome.browser.IntentHandler;
@@ -83,8 +81,6 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
     private long mOnCreateTimestampMs;
 
     private ActivityWindowAndroid mWindowAndroid;
-    private final ObservableSupplierImpl<ModalDialogManager> mModalDialogManagerSupplier =
-            new ObservableSupplierImpl<>();
     private Bundle mSavedInstanceState;
     private int mCurrentOrientation;
     private boolean mDestroyed;
@@ -131,11 +127,6 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
         if (mWindowAndroid != null) {
             mWindowAndroid.destroy();
             mWindowAndroid = null;
-        }
-
-        if (mModalDialogManagerSupplier.get() != null) {
-            mModalDialogManagerSupplier.get().destroy();
-            mModalDialogManagerSupplier.set(null);
         }
 
         super.onDestroy();
@@ -361,7 +352,6 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
 
         mWindowAndroid = createWindowAndroid();
         mIntentRequestTracker.restoreInstanceState(getSavedInstanceState());
-        mModalDialogManagerSupplier.set(createModalDialogManager());
 
         mStartupDelayed = shouldDelayBrowserStartup();
         ChromeBrowserInitializer.getInstance().handlePreNativeStartupAndLoadLibraries(this);
@@ -654,28 +644,13 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
     }
 
     /**
-     * @return The {@link ModalDialogManager} created for this class.
-     */
-    @Nullable
-    protected ModalDialogManager createModalDialogManager() {
-        return null;
-    }
-
-    /**
      * @return The {@link ModalDialogManager} that manages the display of modal dialogs (e.g.
      *         JavaScript dialogs).
      */
     @Override
     public ModalDialogManager getModalDialogManager() {
-        // TODO(jinsukkim): Remove this method in favor of getModalDialogManagerSupplier below.
-        return mModalDialogManagerSupplier.get();
-    }
-
-    /**
-     * @return The supplier of {@link ModalDialogManager} that manages the display of modal dialogs.
-     */
-    public ObservableSupplier<ModalDialogManager> getModalDialogManagerSupplier() {
-        return mModalDialogManagerSupplier;
+        // TODO(jinsukkim): Remove this method in favor of super.getModalDialogManagerSupplier().
+        return getModalDialogManagerSupplier().get();
     }
 
     /**
