11a17becdf4d24dee8ca80233a69ed1e1c09a630
Bruce Dawson <brucedawson@chromium.org>
2021-06-23 19:26:13 +0000

    Explicitly include windows.h in chrome/browser code
    In order to reduce windows.h usage in header files it is necessary to
    explicitly included it in .cc files when it is needed. These source
    files use Windows functions without including windows.h. This fixes
    that.
    
    Bug: 1222496
    Change-Id: I2d4f9e3e3fba389bc8e1a8094223519832f5f0c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2978736
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895268}

--- chrome/browser/plugins/plugin_infobar_delegates.cc
+++ chrome/browser/plugins/plugin_infobar_delegates.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_WIN)
+#include <windows.h>
+
 #include <shellapi.h>
 #include "ui/base/win/shell.h"
 #endif
--- chrome/browser/ui/prefs/prefs_tab_helper.cc
+++ chrome/browser/ui/prefs/prefs_tab_helper.cc
@@ -58,6 +58,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/themes/theme_service_factory.h"
 #endif
 
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
 using blink::web_pref::WebPreferences;
 using content::WebContents;
 
