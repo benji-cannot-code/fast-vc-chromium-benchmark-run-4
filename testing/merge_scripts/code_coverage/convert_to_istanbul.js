9fdd33592a9ee06dd0cd250ee958d2b8a8ceddf4
Ben Reich <benreich@chromium.org>
2021-06-23 04:23:01 +0000

    [js-code-coverage] Separate coverage packages from WebUI
    WebUI npm packages contain modules which are used to build source code
    that will eventually end up in the Chrome binary. The code coverage
    packages perform post processing and contain a large number of transient
    dependencies that are not beneficial to have in every developers
    checkout.
    
    This splits them into two package.json's with their own update shell
    scripts. Developers working on or wanting code coverage can add the
    checkout_js_coverage_modules: True custom var to their .gclient file and
    the relevant bots which perform coverage will do the same.
    
    The following packages have been added for JS code coverage:
    - istanbuljs/v8-to-istanbul v7.1.2
    - mozilla/source-map v0.7.3
    - istanbuljs/nyc v15.1.0
    
    These have been approved for usage by chrome-eng-review:
    https://groups.google.com/a/google.com/g/chrome-eng-review/c/l10lJSzM704/m/AjEk9QP0CQAJ
    
    Bug: 1211595
    Test: Add .gclient var as True and False and ensure modules downloaded.
    Test: ./update_npm_deps
    Change-Id: I8b01776cb6cfdda8261074d347c37e4def923229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2945263
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895010}

--- testing/merge_scripts/code_coverage/convert_to_istanbul.js
+++ testing/merge_scripts/code_coverage/convert_to_istanbul.js
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // Relative path to the node modules.
-const NODE_MODULES = ['..', '..', '..', 'third_party', 'node', 'node_modules'];
+const NODE_MODULES = [
+  '..', '..', '..', 'third_party', 'js_code_coverage', 'node_modules'];
 
 const {createHash} = require('crypto');
 const {join, normalize} = require('path');
