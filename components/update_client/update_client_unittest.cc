e8ab414a2e0d83c9b9e3b86dff020196575f938e
Joshua Pawlicki <waffles@chromium.org>
2021-06-23 15:48:10 +0000

    Component updater: fix flaky test.
    COMPONENT_WAIT is only sent for the second component if the first one
    took longer than 1 second to resolve.
    
    Fixed: 1223019
    Change-Id: Id784559270116da533627ecb9f78664eaaa3a6e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982957
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895160}

--- components/update_client/update_client_unittest.cc
+++ components/update_client/update_client_unittest.cc
@@ -1450,7 +1450,8 @@ TEST_F(UpdateClientTest, MAYBE_TwoCrxUpdateDownloadTimeout) {
     EXPECT_CALL(observer, OnEvent(Events::COMPONENT_UPDATE_FOUND,
                                   "ihfokbkgjpifnbbojhneepfflplebdkc")).Times(1);
     EXPECT_CALL(observer, OnEvent(Events::COMPONENT_WAIT,
-                                  "ihfokbkgjpifnbbojhneepfflplebdkc")).Times(1);
+                                  "ihfokbkgjpifnbbojhneepfflplebdkc"))
+        .Times(AnyNumber());
     EXPECT_CALL(observer, OnEvent(Events::COMPONENT_UPDATE_DOWNLOADING,
                                   "ihfokbkgjpifnbbojhneepfflplebdkc"))
         .Times(AtLeast(1));
