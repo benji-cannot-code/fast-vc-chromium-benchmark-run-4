7685e19eda96f606b35ffe0fa95fb7bac6fffecd
Antonio Gomes <tonikitoo@igalia.com>
2021-06-24 13:39:51 +0000

    [ozone/wayland] Wrap zwp_pointer_constraints
    ... with a WaylandZwpPointerConstraints class.
    
    This is the first out of N steps to add support to native
    "pointer locking" in Ozone/Wayland. Both Chrome/Wayland and
    Lacros will benefit.
    
    The class will "lock' the pointer motion events on the server.
    
    The implementation is still no-op, and will be filled up by the
    follow up patches (see relation chain).
    
    R=msisov@igalia.com
    
    Change-Id: I4b3a0c0722119da541272c208d531fa4850dc8b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2980732
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#895568}

--- ui/ozone/platform/wayland/host/wayland_connection.h
+++ ui/ozone/platform/wayland/host/wayland_connection.h
@@ -46,6 +46,7 @@ class WaylandShm;
 class WaylandTouch;
 class WaylandZAuraShell;
 class WaylandZcrCursorShapes;
+class WaylandZwpPointerConstraints;
 class WaylandZwpPointerGestures;
 class WaylandZwpLinuxDmabuf;
 class WaylandDataDeviceManager;
@@ -204,6 +205,10 @@ class WaylandConnection {
     return window_drag_controller_.get();
   }
 
+  WaylandZwpPointerConstraints* wayland_zwp_pointer_constraints() const {
+    return wayland_zwp_pointer_constraints_.get();
+  }
+
   XdgForeignWrapper* xdg_foreign() const { return xdg_foreign_.get(); }
 
   // Returns true when dragging is entered or started.
@@ -299,6 +304,8 @@ class WaylandConnection {
   std::unique_ptr<WaylandCursorPosition> wayland_cursor_position_;
   std::unique_ptr<WaylandZAuraShell> zaura_shell_;
   std::unique_ptr<WaylandZcrCursorShapes> zcr_cursor_shapes_;
+  std::unique_ptr<WaylandZwpPointerConstraints>
+      wayland_zwp_pointer_constraints_;
   std::unique_ptr<WaylandZwpPointerGestures> wayland_zwp_pointer_gestures_;
   std::unique_ptr<WaylandZwpLinuxDmabuf> zwp_dmabuf_;
   std::unique_ptr<WaylandDrm> drm_;
