059e955023c6ca054debe50442be6bed34780e95
Nohemi Fernandez <fernandex@chromium.org>
2021-06-24 19:18:44 +0000

    Reland "[iOS] Use modern UITextView link API for sign-in Sync flow."
    This reverts commit 9406c3cfcc3c39dd7c86979977470d08c940a90f.
    
    Reason for revert: Following the iOS 12 support deprecation, we can
    publish the new link API without breaking iOS 12 tests.
    
    Original change's description:
    > Revert "[iOS] Use modern UITextView link API for sign-in Sync flow."
    >
    > This reverts commit 58b237b5c2fb149f07330ab89071dc999119ba26.
    >
    > Reason for revert: Breaks many EG tests using Sign-in flows.
    >
    > Starting from this CI build:
    > https://ci.chromium.org/p/chrome/builders/ci/iphone-simulator/19244
    >
    > Original change's description:
    > > [iOS] Use modern UITextView link API for sign-in Sync flow.
    > >
    > > Prior to this API the Chrome team had created a custom
    > > LabelLinkController that manually keeps track of tap locations and
    > > compares it to the computed text UI location.
    > >
    > > This manual check is error-prone causing issues with accessibility and
    > > foreign-language text.
    > >
    > > Bug: 1179690, 1105853
    > > Change-Id: Ice3ff211cc3be1f5d7667abd6f5b0be6925492ed
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2712953
    > > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > > Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#859330}
    >
    > Bug: 1179690
    > Bug: 1105853
    > Change-Id: I3463bada4dc118396dd91c1c7265015c57e00f9b
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732288
    > Auto-Submit: Sebastien Lalancette <seblalancette@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#859360}
    
    Bug: 1179690
    Bug: 1105853
    Change-Id: Ica009450c1ce415f20e4e2f4b61f072b5b1c51bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732096
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895727}

--- ios/chrome/common/string_util.mm
+++ ios/chrome/common/string_util.mm
@@ -55,7 +55,10 @@ NSAttributedString* AttributedStringFromStringWithLink(
                                              attributes:text_attributes];
 
   DCHECK(parsed_string.range.location != NSNotFound);
-  [attributed_string addAttributes:link_attributes range:parsed_string.range];
+
+  if (link_attributes != nil) {
+    [attributed_string addAttributes:link_attributes range:parsed_string.range];
+  }
 
   return attributed_string;
 }
