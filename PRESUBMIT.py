95971520cc8eef51148ca05b96b868ebf33e55d5
Bruce Dawson <brucedawson@chromium.org>
2021-06-24 15:05:39 +0000

    Reland "Fix build_config.h presubmit"
    This reverts commit 83a4552de0b56533f9e4f9222020267458bc227d.
    
    Reason for revert: Fixed to handle deleted files by specifying
    include_deletes=False.
    
    Original change's description:
    > Revert "Fix build_config.h presubmit"
    >
    > This reverts commit 76c67f3bea38341351f0d427c7eb780c7226f0a3.
    >
    > Reason for revert: This change causes the presubmit check to fail for changes that move files. AffectedFiles() includes a file's old name from before moving, so ReadFile() tries to read a file that doesn't exist.
    >
    > Example log: https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8843618273276417712/+/u/presubmit_py3/stdout
    >
    > Original change's description:
    > > Fix build_config.h presubmit
    > >
    > > Chromium has a presubmit to make sure that nobody uses macros likes
    > > OS_WIN without first including build/build_config.h. It's a good idea
    > > but it had two flaws:
    > >
    > > 1) It only detects bugs in new lines of code. Existing abuses of the
    > > OS_* macros are often ignored.
    > > 2) It confirms that build/build_config.h is included, but it does not
    > > confirm that it is included before the macro is used.
    > >
    > > Both of these issues appeared in
    > > content/web_test/browser/web_test_download_manager_delegate.cc. This
    > > source file uses some Windows types and it appeared to include
    > > windows.h, but it actually didn't, because OS_WIN is used before it is
    > > defined. This bug is being fixed separately.
    > >
    > > This change fixes both presubmit issues.
    > >
    > > Change-Id: I66b37f3d4ec528910d7240414c8acbe2c87d6e82
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2981136
    > > Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    > > Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    > > Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#895132}
    >
    > Change-Id: Icd23cb27c73f448e88eabb27b903c66e0fd4b9c5
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982304
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    > Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    > Commit-Queue: Ian Wells <iwells@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#895188}
    
    Change-Id: I93f1909d02777732ad8174a26e7dd7fa00bb3adf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982308
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895588}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -4232,12 +4232,13 @@ def CheckBuildConfigMacrosWithoutInclude(input_api, output_api):
       r'^#include\s+"build/build_config.h"', input_api.re.MULTILINE)
   extension_re = input_api.re.compile(r'\.[a-z]+$')
   errors = []
-  for f in input_api.AffectedFiles():
+  for f in input_api.AffectedFiles(include_deletes=False):
     if not f.LocalPath().endswith(('.h', '.c', '.cc', '.cpp', '.m', '.mm')):
       continue
     found_line_number = None
     found_macro = None
-    for line_num, line in f.ChangedContents():
+    all_lines = input_api.ReadFile(f, 'r').splitlines()
+    for line_num, line in enumerate(all_lines):
       match = macro_re.search(line)
       if match:
         found_line_number = line_num
@@ -4246,12 +4247,12 @@ def CheckBuildConfigMacrosWithoutInclude(input_api, output_api):
     if not found_line_number:
       continue
 
-    found_include = False
-    for line in f.NewContents():
+    found_include_line = -1
+    for line_num, line in enumerate(all_lines):
       if include_re.search(line):
-        found_include = True
+        found_include_line = line_num
         break
-    if found_include:
+    if found_include_line >= 0 and found_include_line < found_line_number:
       continue
 
     if not f.LocalPath().endswith('.h'):
@@ -4262,7 +4263,7 @@ def CheckBuildConfigMacrosWithoutInclude(input_api, output_api):
           continue
       except IOError:
         pass
-    errors.append('%s:%d %s macro is used without including build/'
+    errors.append('%s:%d %s macro is used without first including build/'
                   'build_config.h.'
                   % (f.LocalPath(), found_line_number, found_macro))
   if errors:
