fa0020b6076afe0b486adf93f961d8ff38cd81e2
Kyle Horimoto <khorimoto@chromium.org>
2021-06-24 00:57:29 +0000

    [CrOS Cellular] Remove erroneous log in internet_detail_menu
    This log stated that we were unable to find an eSIM network with a given
    GUID. However, this check occurs whenever we go to the
    internet_detail_page for any network (not only eSIM).
    
    This CL simply removes this log since it is unnecessary and incorrect.
    
    Bug: b/191180751
    Change-Id: Ia820829784199390f5a04b7a4bca1849da01f4b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982450
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895406}

--- chrome/browser/resources/settings/chromeos/internet_page/internet_detail_menu.js
+++ chrome/browser/resources/settings/chromeos/internet_page/internet_detail_menu.js
@@ -156,7 +156,6 @@ Polymer({
           !response.result.typeState.cellular.eid ||
           !response.result.typeState.cellular.iccid) {
         this.eSimNetworkState_ = null;
-        console.warn('Unable to find eSIM network with GUID: ', this.guid_);
         return;
       }
       this.eSimNetworkState_ = response.result;
