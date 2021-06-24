5d404fc2c739ee352f953eeb9b205b961a0f79b4
Jared Saul <jsaul@google.com>
2021-06-24 22:25:04 +0000

    [AF] Re-enable flaky test for non-Mac platforms
    Bug: 1223042
    Change-Id: I4c01aa971bcd74924dec59f4f620c93cd807a789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2986360
    Auto-Submit: Jared Saul <jsaul@google.com>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895808}

--- chrome/browser/ui/views/autofill/payments/virtual_card_manual_fallback_bubble_views_interactive_uitest.cc
+++ chrome/browser/ui/views/autofill/payments/virtual_card_manual_fallback_bubble_views_interactive_uitest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/strings/string_number_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/autofill/payments/virtual_card_manual_fallback_bubble_controller_impl.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -200,9 +201,16 @@ IN_PROC_BROWSER_TEST_F(VirtualCardManualFallbackBubbleViewsInteractiveUiTest,
   EXPECT_EQ(clipboard_text, u"5454545454545454");
 }
 
-// Disabled due to flakiness: crbug.com/1223042
+// Disabled on Mac due to flakiness: crbug.com/1223042
+#if defined(OS_MAC)
+#define MAYBE_Metrics_BubbleShownAndClosedByUser \
+  DISABLED_Metrics_BubbleShownAndClosedByUser
+#else
+#define MAYBE_Metrics_BubbleShownAndClosedByUser \
+  Metrics_BubbleShownAndClosedByUser
+#endif
 IN_PROC_BROWSER_TEST_F(VirtualCardManualFallbackBubbleViewsInteractiveUiTest,
-                       DISABLED_Metrics_BubbleShownAndClosedByUser) {
+                       MAYBE_Metrics_BubbleShownAndClosedByUser) {
   base::HistogramTester histogram_tester;
 
   ShowBubble();
