93da5bd5d431c16ea4a12fc7477926786781c615
Fabrice de Gans <fdegans@chromium.org>
2021-06-24 17:44:03 +0000

    [code-health] Delete //tools/memory_inspector
    This is no longer used or maintained.
    
    Bug: 1212106
    Change-Id: I12fef7956eacc25f0b8327e43cd621476c685a1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983344
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Primiano Tucci <primiano@chromium.org>
    Auto-Submit: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895668}

--- tools/memory_inspector/memory_inspector/core/exceptions.py
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2014 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-class MemoryInspectorException(Exception):
-  """Base exception class used for all memory inspector related exceptions."""
-  def __init__(self, value):
-    super(MemoryInspectorException, self).__init__(value)
