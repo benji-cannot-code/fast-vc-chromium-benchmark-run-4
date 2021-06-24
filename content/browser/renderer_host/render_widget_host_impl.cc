ac29e5cf921cfbead3b9adb115a44a3fc47d95f7
Alexander Timin <altimin@chromium.org>
2021-06-24 12:20:28 +0000

    Remove unused WebContentsDelegate::LostCapture
    R=alexmos@chromium.org
    
    Change-Id: Ifd7c39e2ad3bf6d34366dfa18552bdf8d5d237a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982342
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895546}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1291,9 +1291,6 @@ void RenderWidgetHostImpl::LostCapture() {
     touch_emulator->CancelTouch();
 
   GetWidgetInputHandler()->MouseCaptureLost();
-
-  if (delegate_)
-    delegate_->LostCapture(this);
 }
 
 void RenderWidgetHostImpl::SetActive(bool active) {
