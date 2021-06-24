59bcd5488b77308bb720787ff8cb112e43c14083
Phu Le <phle@microsoft.com>
2021-06-24 06:03:29 +0000

    Enable support for OpenXR SceneUnderstanding extension
    The OpenXR 's SceneUnderstand extension is no longer a _preview but an
    official XR_MSFT_scene_understanding extension. Chromium can now
    consume it to light up AR HitTesting scenarios on applicable
    platforms/devices. This change enables hittest for both headview and
    transient input for OpenXR devices, but behind the
    kOpenXrExtendedFeatureSupport flag.
    
    Change-Id: I17e60109487bd11087480416708944280d8ad1ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2947527
    Commit-Queue: Phu Le <phle@microsoft.com>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#895473}

--- device/vr/openxr/openxr_api_wrapper.h
+++ device/vr/openxr/openxr_api_wrapper.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 #include "device/vr/openxr/openxr_anchor_manager.h"
+#include "device/vr/openxr/openxr_scene_understanding_manager.h"
 #include "device/vr/openxr/openxr_util.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
 #include "device/vr/vr_export.h"
@@ -89,6 +90,8 @@ class OpenXrApiWrapper {
       device::mojom::XRSessionMode session_mode);
 
   OpenXrAnchorManager* GetOrCreateAnchorManager(
+      const OpenXrExtensionHelper& extension_helper);
+  OpenXRSceneUnderstandingManager* GetOrCreateSceneUnderstandingManager(
       const OpenXrExtensionHelper& extension_helper);
 
   void CreateSharedMailboxes(viz::ContextProvider* context_provider);
@@ -202,6 +205,7 @@ class OpenXrApiWrapper {
   std::vector<XrCompositionLayerProjectionView> layer_projection_views_;
 
   std::unique_ptr<OpenXrAnchorManager> anchor_manager_;
+  std::unique_ptr<OpenXRSceneUnderstandingManager> scene_understanding_manager_;
 
   base::WeakPtrFactory<OpenXrApiWrapper> weak_ptr_factory_{this};
 
