5871ed4285d96b5d7c3cb9aed3e40f179064fa69
amehfooz <amehfooz@chromium.org>
2021-06-24 21:51:59 +0000

    Disable user chooser view for guest session
    Regression caused by changes made for crbug/1102768
    Adding a special case to make sure we don't show
    the user chooser menu for guest mode.
    
    Bug: 1205371
    Change-Id: I20db14da7df7e2860f5fd71ac3e373453ceb1001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953683
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895797}

--- ash/system/unified/user_chooser_detailed_view_controller.cc
+++ ash/system/unified/user_chooser_detailed_view_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
 #include "ash/system/unified/user_chooser_view.h"
+#include "components/user_manager/user_type.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace ash {
@@ -33,6 +34,12 @@ bool UserChooserDetailedViewController::IsUserChooserEnabled() {
   SessionControllerImpl* session = Shell::Get()->session_controller();
   if (session->IsUserSessionBlocked())
     return false;
+
+  // Only allow for regular user session.
+  if (session->GetPrimaryUserSession()->user_info.type !=
+      user_manager::USER_TYPE_REGULAR) {
+    return false;
+  }
 
   return true;
 }
