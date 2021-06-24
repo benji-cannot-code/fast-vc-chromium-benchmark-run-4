5fdef08fb93ef7b53efc7e5d824e8dff6f5908bd
Juliet Levesque <julietlevesque@google.com>
2021-06-24 19:04:04 +0000

    [Fast Pair] Set up FastPairNotificationController class.
    Create FastPairNotificationController class which will eventually
    display system notifications for Fast Pair error, Fast Pair discovery,
    and Fast Pair pairing.
    
    Bug: 1222720
    Change-Id: I883acb6f90b16ae1a7261c587f011e4b5986ed59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2971976
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/master@{#895721}

---
+++ ash/system/quick_pair/fast_pair_notification_controller.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/quick_pair/fast_pair_notification_controller.h"
+
+namespace ash {
+
+FastPairNotificationController::FastPairNotificationController() = default;
+
+FastPairNotificationController::~FastPairNotificationController() = default;
+
+void FastPairNotificationController::ShowErrorNotification() {}
+
+void FastPairNotificationController::ShowDiscoveryNotification() {}
+
+void FastPairNotificationController::ShowPairingNotification() {}
+
+}  // namespace ash
