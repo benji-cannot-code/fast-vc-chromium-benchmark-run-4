e6542317b0f3046033969d87ce8e002f9f565fb4
Philipp Hancke <philipp.hancke@googlemail.com>
2021-06-24 13:12:34 +0000

    webrtc: use GetSSLCertificate instead of deprecated get_ssl_certificate
    which is marked as deprecated here:
      https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/rtc_base/rtc_certificate.h;l=69;drc=d00dc06c77afa057d8a77d67159d81494b080cf1;bpv=1;bpt=1
    
    BUG=webrtc:9860
    
    Change-Id: I968685c1e69fd07e3adaf6f82dcdf94ccb70b838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2981592
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895562}

--- third_party/blink/renderer/modules/peerconnection/rtc_certificate.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_certificate.cc
@@ -48,7 +48,7 @@ DOMTimeStamp RTCCertificate::expires() const {
 
 HeapVector<Member<RTCDtlsFingerprint>> RTCCertificate::getFingerprints() {
   std::unique_ptr<rtc::SSLCertificateStats> first_certificate_stats =
-      certificate_->ssl_certificate().GetStats();
+      certificate_->GetSSLCertificate().GetStats();
 
   HeapVector<Member<RTCDtlsFingerprint>> fingerprints;
   for (rtc::SSLCertificateStats* certificate_stats =
