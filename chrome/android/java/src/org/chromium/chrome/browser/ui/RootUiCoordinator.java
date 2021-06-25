fcf5f09d3aade2c675737b0a3f0ce299bcb4693c
bttk <bttk@chromium.org>
2021-06-25 02:44:53 +0000

    AdaptiveButton: Show setting shortcut on long click
    Bug: 1208528
    Change-Id: I7c056a3230ab7eb1b22b07d2852c1687d21f37db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2913061
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Auto-Submit: bttk <bttk@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#895896}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -65,6 +65,7 @@ import org.chromium.chrome.browser.omnibox.voice.VoiceRecognitionHandler.VoiceIn
 import org.chromium.chrome.browser.paint_preview.DemoPaintPreview;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
+import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.chrome.browser.share.ShareButtonController;
 import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.share.ShareDelegate.ShareOrigin;
@@ -80,6 +81,7 @@ import org.chromium.chrome.browser.toolbar.ButtonDataProvider;
 import org.chromium.chrome.browser.toolbar.ToolbarIntentMetadata;
 import org.chromium.chrome.browser.toolbar.ToolbarManager;
 import org.chromium.chrome.browser.toolbar.VoiceToolbarButtonController;
+import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveButtonActionMenuCoordinator;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarButtonController;
 import org.chromium.chrome.browser.toolbar.adaptive.AdaptiveToolbarFeatures.AdaptiveToolbarButtonVariant;
 import org.chromium.chrome.browser.toolbar.adaptive.OptionalNewTabButtonController;
@@ -172,6 +174,8 @@ public class RootUiCoordinator
     private ScrimCoordinator mScrimCoordinator;
     private DirectActionInitializer mDirectActionInitializer;
     private List<ButtonDataProvider> mButtonDataProviders;
+    @Nullable
+    private AdaptiveToolbarButtonController mAdaptiveToolbarButtonController;
     private IdentityDiscController mIdentityDiscController;
     private ChromeActionModeHandler mChromeActionModeHandler;
     private final ToolbarActionModeCallback mActionModeControllerCallback;
@@ -346,6 +350,11 @@ public class RootUiCoordinator
             }
             mToolbarManager.destroy();
             mToolbarManager = null;
+        }
+
+        if (mAdaptiveToolbarButtonController != null) {
+            mAdaptiveToolbarButtonController.destroy();
+            mAdaptiveToolbarButtonController = null;
         }
 
         if (mAppMenuCoordinator != null) {
@@ -712,7 +721,6 @@ public class RootUiCoordinator
                             mActivityTabProvider, trackerSupplier,
                             mActivity.getLifecycleDispatcher(), mActivity.getModalDialogManager(),
                             voiceSearchDelegate);
-
             OptionalNewTabButtonController newTabButtonController =
                     new OptionalNewTabButtonController(mActivity,
                             AppCompatResources.getDrawable(mActivity, R.drawable.new_tab_icon),
@@ -720,7 +728,9 @@ public class RootUiCoordinator
                             mActivity.getTabCreatorManagerSupplier(), mTabModelSelectorSupplier,
                             trackerSupplier);
             AdaptiveToolbarButtonController adaptiveToolbarButtonController =
-                    new AdaptiveToolbarButtonController(mActivity.getLifecycleDispatcher());
+                    new AdaptiveToolbarButtonController(mActivity, new SettingsLauncherImpl(),
+                            mActivity.getLifecycleDispatcher(),
+                            new AdaptiveButtonActionMenuCoordinator());
             adaptiveToolbarButtonController.addButtonVariant(
                     AdaptiveToolbarButtonVariant.NEW_TAB, newTabButtonController);
             adaptiveToolbarButtonController.addButtonVariant(
@@ -730,7 +740,6 @@ public class RootUiCoordinator
             mButtonDataProviders =
                     Arrays.asList(mIdentityDiscController, adaptiveToolbarButtonController,
                             shareButtonController, voiceToolbarButtonController);
-
             mToolbarManager = new ToolbarManager(mActivity, mBrowserControlsManager,
                     mActivity.getFullscreenManager(), toolbarContainer,
                     mActivity.getCompositorViewHolder(), urlFocusChangedCallback,
