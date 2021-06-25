11123c6234fb619f46cef88a24793a8a14458428
Adam Langley <agl@chromium.org>
2021-06-25 01:31:45 +0000

    webauthn: collect phones from parent profile.
    When changing from merging phones from all profiles I didn't think hard
    enough about Incognito mode. Such windows should collect phones from the
    parent profile but, for makeCredential, should show the existing
    interstitial warning that information may be recorded.
    
    BUG=1002262
    
    Change-Id: I463c78920fb1c806ba9bcb8a49094442718fc87c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2979896
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#895875}

--- chrome/browser/webauthn/authenticator_request_dialog_model.h
+++ chrome/browser/webauthn/authenticator_request_dialog_model.h
@@ -84,9 +84,11 @@ class AuthenticatorRequestDialogModel {
     kBlePowerOnAutomatic,
     kBlePowerOnManual,
 
-    // Let the user confirm that they want to create a platform credential in an
-    // off-the-record browsing context.
-    kPlatformAuthenticatorOffTheRecordInterstitial,
+    // Let the user confirm that they want to create a credential in an
+    // off-the-record browsing context. Used for platform and caBLE credentials,
+    // where we feel that it's perhaps not obvious that something will be
+    // recorded.
+    kOffTheRecordInterstitial,
 
     // Phone as a security key.
     kCableActivate,
@@ -322,10 +324,10 @@ class AuthenticatorRequestDialogModel {
   // Valid action when at all steps.
   void StartPlatformAuthenticatorFlow();
 
-  // Proceeds straight to the platform authenticator prompt.
-  //
-  // Valid action when at all steps.
-  void HideDialogAndDispatchToPlatformAuthenticator();
+  // OnOffTheRecordInterstitialAccepted is called when the user accepts the
+  // interstitial that warns that platform/caBLE authenticators may record
+  // information even in incognito mode.
+  void OnOffTheRecordInterstitialAccepted();
 
   // Show guidance about caBLE USB fallback.
   void ShowCableUsbFallback();
@@ -569,6 +571,7 @@ class AuthenticatorRequestDialogModel {
 
   // Contacts a paired phone. The phone is specified by name.
   void ContactPhone(const std::string& name, size_t mechanism_index);
+  void ContactPhoneAfterOffTheRecordInterstitial(std::string name);
 
   void StartLocationBarBubbleRequest();
 
@@ -577,6 +580,11 @@ class AuthenticatorRequestDialogModel {
 
   void ContactNextPhoneByName(const std::string& name);
   void PopulateMechanisms();
+
+  // Proceeds straight to the platform authenticator prompt.
+  //
+  // Valid action when at all steps.
+  void HideDialogAndDispatchToPlatformAuthenticator();
 
   EphemeralState ephemeral_state_;
 
@@ -598,6 +606,11 @@ class AuthenticatorRequestDialogModel {
   // powered. Only set while the |current_step_| is either kBlePowerOnManual,
   // kBlePowerOnAutomatic.
   absl::optional<Step> next_step_once_ble_powered_;
+
+  // after_off_the_record_interstitial_ contains the closure to run if the user
+  // accepts the interstitial that warns that platform/caBLE authenticators may
+  // record information even in incognito mode.
+  base::OnceClosure after_off_the_record_interstitial_;
 
   base::ObserverList<Observer>::Unchecked observers_;
 
