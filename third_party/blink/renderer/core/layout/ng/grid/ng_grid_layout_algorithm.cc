1b6190d3e6012c3f091255c5bf97218d6504a18d
Kurt Catti-Schmidt (SCHMIDT) <kschmi@microsoft.com>
2021-06-25 22:41:56 +0000

    [GridNG] MightBeSaturated following equation fixes
    This is a follow-up to
    https://chromium-review.googlesource.com/c/chromium/src/+/2967206, in
    which I incorrectly dismissed one of Christian's suggestions. He
    appropriately brought up that this DCHECK could still be hit due to
    the results of the equation. I thought that base::ClampSub would prevent
    it from reaching the saturation point, but I was wrong - it will reach
    but not go over the saturation point, at which point MightBeSaturated
    will return true.
    
    This change maintains the DCHECK by resetting the results of the
    equation to 0 if it's saturated. Another option is to remove the
    DCHECK, which I am open to doing instead of this.
    
    Bug: 1222027
    Change-Id: I70163793d475748f4791067d589f9872d5c12bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983736
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#896240}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.cc
@@ -3686,6 +3686,9 @@ void NGGridLayoutAlgorithm::ComputeGridItemOffsetAndSize(
     } else {
       *size = set_geometry.sets[end_index].offset - *start_offset -
               set_geometry.gutter_size;
+
+      if (size->MightBeSaturated())
+        *size = LayoutUnit();
     }
   }
   DCHECK(!size->MightBeSaturated());
