77fc00b2de4563fb9f62b16c9481c201ddaa29f7
Eric Willigers <ericwilligers@chromium.org>
2021-06-25 05:52:18 +0000

    WebAppPublisherHelper methods take single argument
    WebAppPublisherHelper methods
    - ConvertWebApp()
    - MakeIconKey()
    - GetIconEffects()
    now each take a single const WebApp* web_app argument.
    
    They deduce is_disabled and readiness from the web app:
      web_app->chromeos_data()->is_disabled
    
    Note that OnWebAppDisabledStateChanged() is only called
    when web_app->chromos_data().is_disabled
    has already been updated, so we do not need to forward
    the argument received by OnWebAppDisabledStateChanged()
    
    
    Bug: 1223409
    Change-Id: I3ea460e2894b4e7a48df0e1efd7b47291fa508a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2986930
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895934}

--- chrome/browser/web_applications/app_service/web_apps_base.cc
+++ chrome/browser/web_applications/app_service/web_apps_base.cc
@@ -202,8 +202,7 @@ void WebAppsBase::ModifyWebAppCapabilityAccess(
                          std::move(accessing_microphone));
 }
 
-void WebAppsBase::ConvertWebApps(apps::mojom::Readiness readiness,
-                                 std::vector<apps::mojom::AppPtr>* apps_out) {
+void WebAppsBase::ConvertWebApps(std::vector<apps::mojom::AppPtr>* apps_out) {
   const WebAppRegistrar* registrar = GetRegistrar();
   // Can be nullptr in tests.
   if (!registrar)
@@ -211,8 +210,7 @@ void WebAppsBase::ConvertWebApps(apps::mojom::Readiness readiness,
 
   for (const WebApp& web_app : registrar->GetApps()) {
     if (Accepts(web_app.app_id())) {
-      apps_out->push_back(
-          publisher_helper().ConvertWebApp(&web_app, readiness));
+      apps_out->push_back(publisher_helper().ConvertWebApp(&web_app));
     }
   }
 }
@@ -220,7 +218,7 @@ void WebAppsBase::ConvertWebApps(apps::mojom::Readiness readiness,
 void WebAppsBase::StartPublishingWebApps(
     mojo::PendingRemote<apps::mojom::Subscriber> subscriber_remote) {
   std::vector<apps::mojom::AppPtr> apps;
-  ConvertWebApps(apps::mojom::Readiness::kReady, &apps);
+  ConvertWebApps(&apps);
 
   mojo::Remote<apps::mojom::Subscriber> subscriber(
       std::move(subscriber_remote));
