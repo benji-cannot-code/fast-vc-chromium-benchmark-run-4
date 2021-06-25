bf39f74e791f3e72e17a6deaf65d50452943cf39
Tomasz Wiszkowski <ender@google.com>
2021-06-25 16:22:45 +0000

    Retire the qualifyPartialUrlQuery method.
    The method is similar to AutocompleteController::Classify but strips
    the result from a volume of useful information, that seems to be
    later reconstructed by calling parties (eg. with the help of the
    TemplateUrlService).
    
    The call was - and still is pretty expensive and should only be used
    where the information is needed.
    
    While similar, the two mechanisms are a bit different, specifically:
    1. the original call used AutocompleteClassifierFactory, that next
       created its very own Autocomplete mechanism to identify the match
       type
    2. the updated call uses embedded/owned Autocomplete factory to do
       the same, at the expense of reconstructing owned ACResult.
    
    It is safe, because in both uses of the qualifyPartialUrlQuery the
    calls are made outside (ahead of, or entirely separately from) a
    regular Autocomplete cycle:
    - Voice search uses this call to determine whether default voice
      input is a Search or URL to initiate navigation, or to determine
      the Type of individual suggestions to be presented to the User,
    - IntentHandler uses this call to determine the type of navigation
      that is requested (search vs url).
    
    There are no other use cases here, making this substitute safe (ie.
    it won't affect the AutocompleteResult information being offered to
    the user, because the Autocomplete cycle is not running at that time).
    
    This CL:
    - removes recording of obsolete LongPressPasteAge histogram recording
      (confirmed with the owner that the histogram is no longer needed),
    - updates VoiceRecognitionHandler to use the Classify() method that
      already supplies the information the VRH attempts to reconstruct,
    - updates IntentHandler to do the same, making IntentHandler also
      report AssistedQueryStats in the process (Voice searches will be
      properly accounted for in RASTA from this point).
    
    The change also removes the obsolete method, and requires callers to
    explicitly supply the Profile for which the classify cycle should be
    executed (this was previously inferred from last used profile on
    behalf of the caller).
    
    Bug: 1138587
    Change-Id: Id7508ba274982548b19fa4ff56e2b2dec83861b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973637
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#896084}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/voice/VoiceRecognitionHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/voice/VoiceRecognitionHandlerTest.java
@@ -9,6 +9,9 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyBoolean;
+import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -39,6 +42,7 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
@@ -46,7 +50,9 @@ import org.chromium.chrome.browser.omnibox.LocationBarDataProvider;
 import org.chromium.chrome.browser.omnibox.NewTabPageDelegate;
 import org.chromium.chrome.browser.omnibox.UrlBarData;
 import org.chromium.chrome.browser.omnibox.UrlBarEditingTextStateProvider;
+import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteController;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteController.OnSuggestionsReceivedListener;
+import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteControllerJni;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinator;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteDelegate;
 import org.chromium.chrome.browser.omnibox.suggestions.OmniboxSuggestionsDropdownEmbedder;
@@ -65,6 +71,7 @@ import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.components.metrics.OmniboxEventProtos.OmniboxEventProto.PageClassification;
+import org.chromium.components.omnibox.AutocompleteMatch;
 import org.chromium.components.omnibox.AutocompleteResult;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -91,6 +98,8 @@ import java.util.concurrent.ExecutionException;
 public class VoiceRecognitionHandlerTest {
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
+    @Rule
+    public JniMocker mJniMocker = new JniMocker();
 
     @Mock
     Intent mIntent;
@@ -104,6 +113,12 @@ public class VoiceRecognitionHandlerTest {
     Tab mTab;
     @Mock
     VoiceRecognitionHandler.Observer mObserver;
+    @Mock
+    AutocompleteController mController;
+    @Mock
+    AutocompleteController.Natives mControllerJniMock;
+    @Mock
+    AutocompleteMatch mMatch;
 
     private TestDataProvider mDataProvider;
     private TestDelegate mDelegate;
@@ -598,6 +613,11 @@ public class VoiceRecognitionHandlerTest {
     @Before
     public void setUp() throws InterruptedException, ExecutionException {
         MockitoAnnotations.initMocks(this);
+        mJniMocker.mock(AutocompleteControllerJni.TEST_HOOKS, mControllerJniMock);
+        doReturn(mController).when(mControllerJniMock).getForProfile(any());
+        doReturn(mMatch).when(mController).classify(any(), anyBoolean());
+        doReturn(new GURL("https://www.google.com/search?q=abc")).when(mMatch).getUrl();
+        doReturn(true).when(mMatch).isSearchSuggestion();
         mActivityTestRule.startMainActivityOnBlankPage();
         mLifecycleDispatcher = mActivityTestRule.getActivity().getLifecycleDispatcher();
 
@@ -1407,6 +1427,7 @@ public class VoiceRecognitionHandlerTest {
     @Test
     @SmallTest
     public void testParseResults_VoiceResponseURLConversion() {
+        doReturn(false).when(mMatch).isSearchSuggestion();
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             String[] texts =
                     new String[] {"a", "www. b .co .uk", "engadget .com", "www.google.com"};
