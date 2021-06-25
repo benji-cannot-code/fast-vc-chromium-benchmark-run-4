940c0e5239c84213aad745bee38aa180b5503a24
Sergey Poromov <poromov@chromium.org>
2021-06-25 09:47:03 +0000

    dlp: Add feature to disable Files restriction.
    We need to disable DLP Files daemon by default until it's ready.
    We can control it via passing or not the policy to the daemon.
    
    Bug: 1222812
    Test: Added unit test
    Change-Id: Ifae6cc98754a46bafbcbda2e5e17eb156e653e07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2979573
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895972}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -138,6 +138,9 @@ extern const base::Feature kCryptohomeUserDataAuthKillswitch;
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDataLeakPreventionPolicy;
+
+COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kDataLeakPreventionFilesRestriction;
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
