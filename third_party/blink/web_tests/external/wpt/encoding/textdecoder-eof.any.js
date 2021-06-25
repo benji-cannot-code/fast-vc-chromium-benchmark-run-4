fa957b02f4cae1ea99ad3d74629c09cf578cdfce
Weizhong Xia <weizhong@google.com>
2021-06-25 21:15:47 +0000

    Import wpt@77d54aa9e0405f737987b59331f3584e3e1c26f9
    Using wpt-import in Chromium e8c81fc22166853d3e55f4e6128e9b32ad1c66fe.
    With Chromium commits locally applied on WPT:
    a5a6393262 "Throw error if connections are made between different contexts"
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    jsbell@chromium.org:
      external/wpt/encoding
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I98c858e4137bf975302e4a41d198753b737197aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987284
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Kyle Ju <kyleju@chromium.org>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/master@{#896207}

---
+++ third_party/blink/web_tests/external/wpt/encoding/textdecoder-eof.any.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+test(() => {
+  assert_equals(new TextDecoder().decode(new Uint8Array([0xF0])), "\uFFFD");
+  assert_equals(new TextDecoder().decode(new Uint8Array([0xF0, 0x9F])), "\uFFFD");
+  assert_equals(new TextDecoder().decode(new Uint8Array([0xF0, 0x9F, 0x92])), "\uFFFD");
+}, "TextDecoder end-of-queue handling");
+
+test(() => {
+  const decoder = new TextDecoder();
+  decoder.decode(new Uint8Array([0xF0]), { stream: true });
+  assert_equals(decoder.decode(), "\uFFFD");
+
+  decoder.decode(new Uint8Array([0xF0]), { stream: true });
+  decoder.decode(new Uint8Array([0x9F]), { stream: true });
+  assert_equals(decoder.decode(), "\uFFFD");
+
+  decoder.decode(new Uint8Array([0xF0, 0x9F]), { stream: true });
+  assert_equals(decoder.decode(new Uint8Array([0x92])), "\uFFFD");
+}, "TextDecoder end-of-queue handling using stream: true");
