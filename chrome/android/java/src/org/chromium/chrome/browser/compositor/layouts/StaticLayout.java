8a2bf2cf2d1c0c257be77ec70bcc5a06ef8c7c44
Matt Jones <mdjones@chromium.org>
2021-06-25 15:20:17 +0000

    Have StaticLayout detect tablets internally
    Prior to this patch, StaticLayout depended on an overridden version of
    the LayoutManager to tell it if it was being used on tablet. This is
    entirely unnecessary and has been moved into StaticLayout's
    constructor.
    
    Bug: 882508
    Change-Id: I87c8c0f1e70d6a6a57bdc7a45ff9f649cecf14b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2984920
    Commit-Queue: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896058}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/StaticLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/StaticLayout.java
@@ -37,6 +37,7 @@ import org.chromium.chrome.browser.theme.ThemeUtils;
 import org.chromium.chrome.browser.theme.TopUiThemeColorProvider;
 import org.chromium.chrome.browser.ui.native_page.NativePage;
 import org.chromium.components.embedder_support.util.UrlConstants;
+import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.resources.ResourceManager;
 import org.chromium.url.GURL;
@@ -127,6 +128,8 @@ public class StaticLayout extends Layout {
             Supplier<TopUiThemeColorProvider> topUiThemeColorProvider) {
         super(context, updateHost, renderHost);
         mContext = context;
+        // Only handle tab lifecycle on tablets.
+        mHandlesTabLifecycles = DeviceFormFactor.isNonMultiDisplayContextOnTablet(mContext);
         mViewHost = viewHost;
         mRequestSupplier = requestSupplier;
         assert tabContentManager != null;
@@ -230,14 +233,6 @@ public class StaticLayout extends Layout {
             }
         };
         mBrowserControlsStateProvider.addObserver(mBrowserControlsStateProviderObserver);
-    }
-
-    /**
-     * @param handlesTabLifecycles Whether or not this {@link Layout} should handle tab closing and
-     *                             creating events.
-     */
-    public void setLayoutHandlesTabLifecycles(boolean handlesTabLifecycles) {
-        mHandlesTabLifecycles = handlesTabLifecycles;
     }
 
     @Override
