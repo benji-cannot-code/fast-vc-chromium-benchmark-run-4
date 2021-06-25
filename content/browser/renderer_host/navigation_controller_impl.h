cfa245a327b124489ce1d0ef35393e77761052ed
Nate Chapin <japhet@chromium.org>
2021-06-25 18:29:25 +0000

    Allow navigating parent and child during history traversal
    This is a behavior we've wanted for years, but which could cause
    serious breakage in the absence of FrameNavigationEntries being
    shared across multiple NavigationEntries. That was implemented in
    https://chromium-review.googlesource.com/c/chromium/src/+/2910529
    
    Normally, when sharing FrameNavigationEntries, we "fork" the entry
    when it is updated due to navigating cross-document (unless reloading
    the same url). However, this does not work well when navigating away
    from an initial empty document, because we want to hide any empty
    documents at the start of an iframe's lifetime. In particular, in the
    case where an iframe and the main frame racily navigate, we can end
    up with an initial empty document's FNE shared across 2 different
    NavigationEntries. It is important that when the first real navigation
    takes place, we update the existing entry rather than fork,
    so that the initial empty document is overwritten in both places.
    Therefore, add a special case in
    NavigationControllerImpl::RendererDidNavigateAutoSubrame, to ensure
    an initial empty document is always updated rather than replaced.
    
    Bug: 542299, 598043
    Change-Id: I4113cbbcb8dd5d5065e88ad8f5761acc09847543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2953549
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896133}

--- content/browser/renderer_host/navigation_controller_impl.h
+++ content/browser/renderer_host/navigation_controller_impl.h
@@ -265,6 +265,9 @@ class CONTENT_EXPORT NavigationControllerImpl : public NavigationController {
   // In the case that nothing has changed, the details structure is undefined
   // and it will return false.
   //
+  // |was_on_initial_empty_document| indicates whether the document being
+  // navigated away from was an initial empty document.
+  //
   // |previous_document_was_activated| is true if the previous document had user
   // interaction. This is used for a new renderer-initiated navigation to decide
   // if the page that initiated the navigation should be skipped on
@@ -273,6 +276,7 @@ class CONTENT_EXPORT NavigationControllerImpl : public NavigationController {
                            const mojom::DidCommitProvisionalLoadParams& params,
                            LoadCommittedDetails* details,
                            bool is_same_document_navigation,
+                           bool was_on_initial_empty_document,
                            bool previous_document_was_activated,
                            NavigationRequest* navigation_request);
 
@@ -561,6 +565,7 @@ class CONTENT_EXPORT NavigationControllerImpl : public NavigationController {
       RenderFrameHostImpl* rfh,
       const mojom::DidCommitProvisionalLoadParams& params,
       bool is_same_document,
+      bool was_on_initial_empty_document,
       NavigationRequest* request);
 
   // Allows the derived class to issue notifications that a load has been
