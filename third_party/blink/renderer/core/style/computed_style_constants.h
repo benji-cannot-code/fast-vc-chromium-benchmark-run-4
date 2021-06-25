96700530b07ec153c0ef2852ad8f9e83124f9435
Rune Lillesveen <futhark@chromium.org>
2021-06-25 14:45:29 +0000

    Cache ::selection:window-inactive
    The non-standard :window-inactive allows ::selection style to be
    different for active and inactive windows in Blink. Since we only had
    one pseudo element type to identify style type in the pseudo style
    cache, we used a non-caching version for the inactive case.
    
    Instead, introduce an internal pseudo id for inactive selections such
    that we can cache for inactive windows as well.
    
    Change-Id: I878d729c462de7846157691b43b242597db07221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983277
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896046}

--- third_party/blink/renderer/core/style/computed_style_constants.h
+++ third_party/blink/renderer/core/style/computed_style_constants.h
@@ -78,6 +78,7 @@ enum PseudoId : uint8_t {
   kPseudoIdScrollbarCorner,
   kPseudoIdResizer,
   kPseudoIdInputListButton,
+  kPseudoIdSelectionInactive,
   // Special values follow:
   kAfterLastInternalPseudoId,
   kFirstPublicPseudoId = kPseudoIdFirstLine,
