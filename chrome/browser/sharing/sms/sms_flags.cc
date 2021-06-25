41ce3fcfce91a187af77323c575c1f3a67683afe
Yi Gu <yigu@chromium.org>
2021-06-25 20:44:29 +0000

    [WebOTP] Enable WebOTP on desktop by default
    This feature is only available for Chrome users who have signed in with
    their Google credentials on desktop and mobile.
    
    Launch bug: https://crbug.com/1183062
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/Yy86jN-TjOE/m/4GusoauKAgAJ
    
    Changes in this patch:
    
      1. kWebOTPCrossDevice: disabled -> enabled
        This is for both desktop and Android to use the Sharing feature
        between devices
      2. WebOTP: status: experimental -> stable
        This is to enable window.OTPCredential on desktop which is needed
        for desktop to call WebOTP API
      3. WebOTPAssertionFeaturePolicy: status: experimental -> stable
        This is to allow desktop to call WebOTP API in cross-origin iframes
        gated by permissions policy
    
    Bug: 1015645
    Change-Id: Id81d18f78ebd59a0193cc48006d1d906e9478b47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965094
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896190}

--- chrome/browser/sharing/sms/sms_flags.cc
+++ chrome/browser/sharing/sms/sms_flags.cc
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sharing/sms/sms_flags.h"
 
 const base::Feature kWebOTPCrossDevice{"WebOTPCrossDevice",
-                                       base::FEATURE_DISABLED_BY_DEFAULT};
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
