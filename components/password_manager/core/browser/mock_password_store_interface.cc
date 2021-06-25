af581168a81e17e1fc23e7ade42d0d77164fb04e
Mohamed Amir Yosef <mamir@chromium.org>
2021-06-25 11:09:01 +0000

    [Passwords] Introduce MockPasswordStoreInterface
    This CL introduce a new simpler mock for the PasswordStoreInterface than
    the MockPasswordStore of PasswordStore.
    It also migrates FormSaverImpl test to use the new mock.
    
    Follow up CLs will migrate other usages to the new mock.
    
    Bug: 1218413
    Change-Id: I79b67328aac20884d68ff647985ab1f80cdc8e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988022
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895999}

---
+++ components/password_manager/core/browser/mock_password_store_interface.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/mock_password_store_interface.h"
+
+namespace password_manager {
+
+MockPasswordStoreInterface::MockPasswordStoreInterface() = default;
+
+MockPasswordStoreInterface::~MockPasswordStoreInterface() = default;
+
+void MockPasswordStoreInterface::ShutdownOnUIThread() {}
+
+}  // namespace password_manager
