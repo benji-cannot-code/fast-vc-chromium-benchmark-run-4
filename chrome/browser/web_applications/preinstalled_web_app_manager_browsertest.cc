e62d8cbd32433a6822debc19fb7371f5d8413cbf
Eric Willigers <ericwilligers@chromium.org>
2021-06-25 21:45:33 +0000

    PreinstalledWebAppManagerBrowserTest avoid extensions
    PreinstalledWebAppManagerBrowserTest no longer inherits from
    extensions::ExtensionBrowserTest.
    
    One of the tests does rely on extensions, it moves to a
    new class PreinstalledWebAppManagerExtensionBrowserTest.
    
    
    Bug: 1223723
    Change-Id: Ib8735f915cf8443761b6c98be33dd1dcdfc4749f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988259
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896218}

--- chrome/browser/web_applications/preinstalled_web_app_manager_browsertest.cc
+++ chrome/browser/web_applications/preinstalled_web_app_manager_browsertest.cc
@@ -77,19 +77,15 @@ base::FilePath GetDataFilePath(const base::FilePath& relative_path,
 }  // namespace
 
 class PreinstalledWebAppManagerBrowserTest
-    : public extensions::ExtensionBrowserTest {
+    : virtual public InProcessBrowserTest {
  public:
   PreinstalledWebAppManagerBrowserTest() {
     feature_list_.InitAndEnableFeature(features::kRecordWebAppDebugInfo);
     PreinstalledWebAppManager::SkipStartupForTesting();
   }
 
-  void SetUpOnMainThread() override {
-    ExtensionBrowserTest::SetUpOnMainThread();
-  }
-
   void TearDownOnMainThread() override {
-    url_loader_interceptor_.reset();
+    ResetInterceptor();
     InProcessBrowserTest::TearDownOnMainThread();
   }
 
@@ -128,12 +124,11 @@ class PreinstalledWebAppManagerBrowserTest
   }
 
   const AppRegistrar& registrar() {
-    return WebAppProvider::Get(browser()->profile())->registrar();
+    return WebAppProvider::Get(profile())->registrar();
   }
 
   const PreinstalledWebAppManager& manager() {
-    return WebAppProvider::Get(browser()->profile())
-        ->preinstalled_web_app_manager();
+    return WebAppProvider::Get(profile())->preinstalled_web_app_manager();
   }
 
   void SyncEmptyConfigs() {
@@ -141,7 +136,7 @@ class PreinstalledWebAppManagerBrowserTest
     PreinstalledWebAppManager::SetConfigsForTesting(&app_configs);
 
     base::RunLoop run_loop;
-    WebAppProvider::Get(browser()->profile())
+    WebAppProvider::Get(profile())
         ->preinstalled_web_app_manager()
         .LoadAndSynchronizeForTesting(base::BindLambdaForTesting(
             [&](std::map<GURL, ExternallyManagedAppManager::InstallResult>
@@ -185,7 +180,7 @@ class PreinstalledWebAppManagerBrowserTest
 
     absl::optional<InstallResultCode> code;
     base::RunLoop sync_run_loop;
-    WebAppProvider::Get(browser()->profile())
+    WebAppProvider::Get(profile())
         ->preinstalled_web_app_manager()
         .LoadAndSynchronizeForTesting(base::BindLambdaForTesting(
             [&](std::map<GURL, ExternallyManagedAppManager::InstallResult>
@@ -206,6 +201,11 @@ class PreinstalledWebAppManagerBrowserTest
   }
 
   ~PreinstalledWebAppManagerBrowserTest() override = default;
+
+  Profile* profile() { return browser()->profile(); }
+
+ protected:
+  void ResetInterceptor() { url_loader_interceptor_.reset(); }
 
  private:
   std::unique_ptr<content::URLLoaderInterceptor> url_loader_interceptor_;
@@ -354,11 +354,23 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerBrowserTest,
       launch_url);
 }
 
-IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerBrowserTest,
+class PreinstalledWebAppManagerExtensionBrowserTest
+    : public extensions::ExtensionBrowserTest,
+      public PreinstalledWebAppManagerBrowserTest {
+ public:
+  PreinstalledWebAppManagerExtensionBrowserTest() = default;
+  ~PreinstalledWebAppManagerExtensionBrowserTest() override = default;
+
+  void TearDownOnMainThread() override {
+    ResetInterceptor();
+    extensions::ExtensionBrowserTest::TearDownOnMainThread();
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerExtensionBrowserTest,
                        UninstallAndReplace) {
   PreinstalledWebAppManager::BypassOfflineManifestRequirementForTesting();
   ASSERT_TRUE(embedded_test_server()->Start());
-  Profile* profile = browser()->profile();
 
   // Install Chrome app to be replaced.
   const char kChromeAppDirectory[] = "app";
@@ -371,7 +383,7 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerBrowserTest,
 
   // Start listening for Chrome app uninstall.
   extensions::TestExtensionRegistryObserver uninstall_observer(
-      extensions::ExtensionRegistry::Get(profile));
+      extensions::ExtensionRegistry::Get(browser()->profile()));
 
   constexpr char kAppConfigTemplate[] =
       R"({
@@ -554,7 +566,7 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerBrowserTest,
   // theme_color must be installed opaque.
   EXPECT_EQ(registrar().GetAppThemeColor(app_id),
             SkColorSetARGB(0xFF, 0xBB, 0xCC, 0xDD));
-  EXPECT_EQ(ReadAppIconPixel(browser()->profile(), app_id, /*size=*/192,
+  EXPECT_EQ(ReadAppIconPixel(profile(), app_id, /*size=*/192,
                              /*x=*/0, /*y=*/0),
             SK_ColorBLUE);
 }
@@ -650,7 +662,7 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerBrowserTest,
   // theme_color must be installed opaque.
   EXPECT_EQ(registrar().GetAppThemeColor(app_id),
             SkColorSetARGB(0xFF, 0xBB, 0xCC, 0xDD));
-  EXPECT_EQ(ReadAppIconPixel(browser()->profile(), app_id, /*size=*/192,
+  EXPECT_EQ(ReadAppIconPixel(profile(), app_id, /*size=*/192,
                              /*x=*/0, /*y=*/0),
             SK_ColorBLUE);
 }
@@ -702,7 +714,7 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerBrowserTest,
   // theme_color must be installed opaque.
   EXPECT_EQ(registrar().GetAppThemeColor(app_id),
             SkColorSetARGB(0xFF, 0xBB, 0xCC, 0xDD));
-  EXPECT_EQ(ReadAppIconPixel(browser()->profile(), app_id, /*size=*/192,
+  EXPECT_EQ(ReadAppIconPixel(profile(), app_id, /*size=*/192,
                              /*x=*/0, /*y=*/0),
             SK_ColorBLUE);
 }
@@ -729,7 +741,7 @@ IN_PROC_BROWSER_TEST_F(PreinstalledWebAppManagerBrowserTest,
   {
     base::RunLoop run_loop;
     WebAppPolicyManager& policy_manager =
-        WebAppProvider::Get(browser()->profile())->policy_manager();
+        WebAppProvider::Get(profile())->policy_manager();
     policy_manager.SetOnAppsSynchronizedCompletedCallbackForTesting(
         run_loop.QuitClosure());
     const char kWebAppPolicy[] = R"([{
