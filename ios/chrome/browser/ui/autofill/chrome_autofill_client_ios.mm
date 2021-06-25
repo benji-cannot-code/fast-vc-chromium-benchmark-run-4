3740ad4b9aaf9a23e194476f130e58122611c881
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-06-25 10:34:14 +0000

    [b4pay/ios] Fix accidental launch of account indication
    crrev.com/c/2982136 changed the conditions for the account information
    to be plumbed to the UI, resulting in the information already being
    shown for some users on ToT. This CL fixes the issue by always
    guarding the new behavior under a main toggle
    (kAutofillEnableSaveCardInfoBarAccountIndicationFooter).
    
    
    Bug: 1206190
    Change-Id: I9b3a8fb7e02b151023857dd6001e77936dd40991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988319
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#895988}

--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_client.h"
 #include "components/autofill/core/browser/ui/payments/card_unmask_prompt_view.h"
 #include "components/autofill/core/common/autofill_features.h"
+#import "components/autofill/core/common/autofill_payments_features.h"
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/autofill/ios/browser/autofill_util.h"
 #include "components/infobars/core/infobar.h"
@@ -299,11 +300,14 @@ void ChromeAutofillClientIOS::ConfirmSaveCreditCardToCloud(
   AccountInfo account_info;
   // AccountInfo data should be passed down only if the following conditions are
   // satisfied:
-  // 1) Sync is off or the
+  // 1) kAutofillEnableSaveCardInfoBarAccountIndicationFooter is on (main flag).
+  // 2) Sync is off or the
   //   kAutofillEnableInfoBarAccountIndicationFooterForSyncUsers flag is on.
-  // 2) User has multiple accounts or the
+  // 3) User has multiple accounts or the
   //   kAutofillEnableInfoBarAccountIndicationFooterForSingleAccountUsers is on.
-  if ((sync_disabled_wallet_transport_enabled ||
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillEnableSaveCardInfoBarAccountIndicationFooter) &&
+      (sync_disabled_wallet_transport_enabled ||
        base::FeatureList::IsEnabled(
            features::
                kAutofillEnableInfoBarAccountIndicationFooterForSyncUsers)) &&
