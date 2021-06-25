d0ddaeddc01f272e2c4ca8eb5e9993c443b933af
Song Fangzhen <songfangzhen@bytedance.com>
2021-06-25 03:45:16 +0000

    DPWA: Remove TestAppRegistrar
    After removing the use of TestAppRegistrar, now TestAppRegistrar itself
    should be removed.
    
    Bug: 1212594
    Change-Id: Iedfd40a90f252018567af2f5c8615a3cd80964be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2978711
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Fangzhen Song <songfangzhen@bytedance.com>
    Cr-Commit-Position: refs/heads/master@{#895910}

--- chrome/browser/badging/badge_manager_unittest.cc
+++ chrome/browser/badging/badge_manager_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/badging/badge_manager_factory.h"
 #include "chrome/browser/badging/test_badge_manager_delegate.h"
 #include "chrome/browser/web_applications/components/web_app_provider_base.h"
-#include "chrome/browser/web_applications/test/test_app_registrar.h"
 #include "chrome/browser/web_applications/test/test_app_registry_controller.h"
 #include "chrome/browser/web_applications/test/test_web_app_provider.h"
 #include "chrome/test/base/testing_profile.h"
