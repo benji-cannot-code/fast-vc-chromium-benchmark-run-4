b110c82a8ace660732823d67881deee36d4239ee
Yi Gu <yigu@chromium.org>
2021-06-25 22:45:13 +0000

    [WebOTP] Rename destroy[UserConsent|Verification]Receiver
    The current name is confusing because we destroy the UserConsentReceiver
    when the VerificationReceiver succeeded but not destroy the Verification
    Receiver when UserConsentReceiver succeeded. Rather, we destroy the
    VerificationReceiver when it failed.
    
    This patch rename the functions to reduce confusion.
    
    Bug: None
    Change-Id: I9b626fe7b4c237e96b79e65c39cb17db45e653ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2989174
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896242}

--- content/public/android/junit/src/org/chromium/content/browser/sms/SmsProviderGmsTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/sms/SmsProviderGmsTest.java
@@ -133,7 +133,7 @@ public class SmsProviderGmsTest {
     public void testDestroyUserConsentReceiverWithLocalRequest() {
         createSmsProviderGms(GmsBackend.AUTO);
         boolean isLocalRequest = true;
-        mProvider.destoryUserConsentReceiver(isLocalRequest);
+        mProvider.verificationReceiverSucceeded(isLocalRequest);
         Mockito.verify(mProvider.getUserConsentReceiverForTesting(), times(1)).destroy();
     }
 
@@ -141,7 +141,7 @@ public class SmsProviderGmsTest {
     public void testNotDestroyUserConsentReceiverWithRemoteRequest() {
         createSmsProviderGms(GmsBackend.AUTO);
         boolean isLocalRequest = false;
-        mProvider.destoryUserConsentReceiver(isLocalRequest);
+        mProvider.verificationReceiverSucceeded(isLocalRequest);
         Mockito.verify(mProvider.getUserConsentReceiverForTesting(), times(0)).destroy();
     }
 
@@ -149,7 +149,7 @@ public class SmsProviderGmsTest {
     public void testDestroyVerificationReceiverWithLocalRequest() {
         createSmsProviderGms(GmsBackend.AUTO);
         boolean isLocalRequest = true;
-        mProvider.destoryVerificationReceiver(isLocalRequest);
+        mProvider.verificationReceiverFailed(isLocalRequest);
         Mockito.verify(mProvider.getVerificationReceiverForTesting(), times(1)).destroy();
     }
 
@@ -157,7 +157,7 @@ public class SmsProviderGmsTest {
     public void testNotDestroyVerificationReceiverWithRemoteRequest() {
         createSmsProviderGms(GmsBackend.AUTO);
         boolean isLocalRequest = false;
-        mProvider.destoryVerificationReceiver(isLocalRequest);
+        mProvider.verificationReceiverFailed(isLocalRequest);
         Mockito.verify(mProvider.getVerificationReceiverForTesting(), times(0)).destroy();
     }
 
