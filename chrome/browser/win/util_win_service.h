6fae79c9820d86d81a2e3d9edbd2dfee3ca1a9c7
Alex Gough <ajgo@chromium.org>
2021-06-25 15:55:45 +0000

    Use distinct service for RecordProcessorMetrics
    Before this CL RecordProcessorMetrics was hosted in the UtilWin service
    which runs in a utility with kNoSandbox, but with CET disabled. This
    allows third party dlls to load safely into the utility which some of
    its functions required.
    
    However, this meant that the CetEnabled metric was always false, as
    CET was disabled for the process, making the metric useless. CET for the
    service was disabled in M91 and we did not realise that this would break
    the histogram.
    
    This CL rectifies this by creating a new service,
    RecordProcessorMetrics, and moves all cpu measurement code into the
    service. This runs with kNoSandbox and will run with CET enabled as
    the utility delegate defaults to enabled.
    
    Other CPU metrics are not affected.
    
    Tests: ProcessMetricsTest.*, manual testing on supporting hardware.
    Bug: 1222776
    Change-Id: Ie7438555708bce99a46d354c10a0ab01e4351835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2984326
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896072}

--- chrome/browser/win/util_win_service.h
+++ chrome/browser/win/util_win_service.h
@@ -14,4 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // to the lifetime of the returned Remote.
 mojo::Remote<chrome::mojom::UtilWin> LaunchUtilWinServiceInstance();
 
+// Spawns a new isolated instance of the Windows processor metrics service and
+// returns a remote interface to it. The lifetime of the service process is tied
+// strictly to the lifetime of the returned Remote.
+mojo::Remote<chrome::mojom::ProcessorMetrics> LaunchProcessorMetricsService();
+
 #endif  // CHROME_BROWSER_WIN_UTIL_WIN_SERVICE_H_
--- chrome/services/util_win/util_win_impl.cc
+++ chrome/services/util_win/util_win_impl.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/win/conflicts/module_info_util.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/services/util_win/av_products.h"
-#include "chrome/services/util_win/processor_metrics.h"
 #include "third_party/metrics_proto/system_profile.pb.h"
 #include "ui/shell_dialogs/execute_select_file_win.h"
 
@@ -276,11 +275,3 @@ void UtilWinImpl::GetAntiVirusProducts(bool report_full_names,
   std::move(callback).Run(::GetAntiVirusProducts(report_full_names));
 }
 
-void UtilWinImpl::RecordProcessorMetrics(
-    RecordProcessorMetricsCallback callback) {
-  // TODO(sebmarchand): Check if we should move the ScopedCOMInitializer to the
-  // UtilWinImpl class.
-  base::win::ScopedCOMInitializer scoped_com_initializer;
-  ::RecordProcessorMetrics();
-  std::move(callback).Run();
-}
