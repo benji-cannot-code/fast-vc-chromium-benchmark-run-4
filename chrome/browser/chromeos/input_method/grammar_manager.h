8dc0faf7f47080378988f39e785ab6f128feff96
Jing Wang <jiwan@chromium.org>
2021-06-25 03:09:50 +0000

    grammar: Only check grammar on sentences around the cursor.
    Before this CL, we truncate long text and only check the last sentence.
    This could be problematic if the user is editing in middle of a long
    paragraph. With this CL, we will always check grammar on the sentences
    around the cursor.
    
    Now we have two triggers for grammar checking:
    1. When the last sentence before the cursor changes. When the user
    finishes a sentence, or split an existing sentence into two, or start
    to edit in middle of some text, this could be triggered immediately.
    We don't need time out for this case because you can only edit current
    sentence so the last sentence couldn't change very frequently.
    2. When the user stopped typing for some time. This is similar as before
    but now we only check grammar on the current sentence. Also we don't
    check too short (<5 chars) sentences.
    
    And for all checking, we will validate if the sentence still exists
    before we render the markers, because text might be changed during
    grammar checking.
    
    Also added tests for multiple sentence scenario.
    
    Bug: 1132699,1222895
    Test: passed unit tests.
    Change-Id: Ic561c6e720cd02af7544efc485ab8a69ed56cec0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982867
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895903}

--- chrome/browser/chromeos/input_method/grammar_manager.h
+++ chrome/browser/chromeos/input_method/grammar_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/chromeos/input_method/grammar_service_client.h"
 #include "chrome/browser/chromeos/input_method/suggestion_handler_interface.h"
+#include "chrome/browser/chromeos/input_method/text_utils.h"
 #include "chrome/browser/chromeos/input_method/ui/assistive_delegate.h"
 #include "chrome/browser/profiles/profile.h"
 #include "ui/events/event.h"
@@ -63,10 +64,10 @@ class GrammarManager {
   void IgnoreSuggestion();
 
  private:
-  void Check(const std::u16string& text);
+  void Check(const text_utils::Sentence& sentence);
 
   void OnGrammarCheckDone(
-      const std::u16string& text,
+      const text_utils::Sentence& sentence,
       bool success,
       const std::vector<ui::GrammarFragment>& results) const;
 
@@ -85,6 +86,7 @@ class GrammarManager {
   const ui::ime::AssistiveWindowButton ignore_button_;
   bool suggestion_shown_ = false;
   ui::ime::ButtonId highlighted_button_ = ui::ime::ButtonId::kNone;
+  text_utils::Sentence last_sentence_;
 };
 
 }  // namespace chromeos
