5317262a95dca8ccf1c82761ca467111593ef70f
Peter Kasting <pkasting@chromium.org>
2021-06-25 19:59:35 +0000

    Fix -Wloop-analysis warnings in net/.
    These indicate when a range-based for loop is using an index whose type
    (value, pointer, or reference) doesn't match what the loop actually
    extracts from the range.  Fix by matching the actual type better.
    
    This shouldn't cause any behavior/performance change, just be slightly
    clearer about what's actually happening when reading the code.
    
    Bug: 1223264
    Change-Id: Ia36a7e3dc5168ade450807ed76a9ba5d4aef678a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2984972
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896175}

--- net/tools/transport_security_state_generator/input_file_parsers.cc
+++ net/tools/transport_security_state_generator/input_file_parsers.cc
@@ -346,7 +346,7 @@ bool ParseJSON(base::StringPiece json,
       return false;
     }
 
-    for (const auto& entry_value : parsed.DictItems()) {
+    for (auto entry_value : parsed.DictItems()) {
       if (!base::Contains(valid_keys, entry_value.first)) {
         LOG(ERROR) << "The entry for " << entry->hostname
                    << " contains an unknown " << entry_value.first << " field";
