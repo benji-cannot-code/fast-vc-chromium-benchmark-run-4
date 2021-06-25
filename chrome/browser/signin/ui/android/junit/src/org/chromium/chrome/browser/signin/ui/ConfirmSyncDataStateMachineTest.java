f09765e34642e235b4770751d079d96b793c91ab
Tanmoy Mollik <triploblastic@chromium.org>
2021-06-25 18:31:22 +0000

    [Signin][Android] Refactor ConfirmImportSyncDataDialog to modal dialog
    This CL refactors the ConfirmImportSyncDataDialog from alert dialog to
    modal dialog structure.
    
    Bug: 1197194
    Change-Id: I29e0131cfaa10e4d5d39cc00f679cb3cc429aefe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2851198
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896136}

--- chrome/browser/signin/ui/android/junit/src/org/chromium/chrome/browser/signin/ui/ConfirmSyncDataStateMachineTest.java
+++ chrome/browser/signin/ui/android/junit/src/org/chromium/chrome/browser/signin/ui/ConfirmSyncDataStateMachineTest.java
@@ -75,7 +75,8 @@ public class ConfirmSyncDataStateMachineTest {
         ConfirmSyncDataStateMachine stateMachine = new ConfirmSyncDataStateMachine(
                 mDelegateMock, mOldAccountName, mNewAccountName, mStateMachineListenerMock);
         verify(mDelegateMock)
-                .showConfirmImportSyncDataDialog(any(ConfirmImportSyncDataDialog.Listener.class),
+                .showConfirmImportSyncDataDialog(
+                        any(ConfirmImportSyncDataDialogCoordinator.Listener.class),
                         eq(mOldAccountName), eq(mNewAccountName));
     }
 
@@ -86,7 +87,8 @@ public class ConfirmSyncDataStateMachineTest {
                 oldAndNewAccountName, oldAndNewAccountName, mStateMachineListenerMock);
         verify(mDelegateMock, never())
                 .showConfirmImportSyncDataDialog(
-                        any(ConfirmImportSyncDataDialog.Listener.class), anyString(), anyString());
+                        any(ConfirmImportSyncDataDialogCoordinator.Listener.class), anyString(),
+                        anyString());
         verify(mDelegateMock)
                 .showFetchManagementPolicyProgressDialog(
                         any(ConfirmSyncDataStateMachineDelegate.ProgressDialogListener.class));
@@ -98,7 +100,8 @@ public class ConfirmSyncDataStateMachineTest {
                 mDelegateMock, null, mNewAccountName, mStateMachineListenerMock);
         verify(mDelegateMock, never())
                 .showConfirmImportSyncDataDialog(
-                        any(ConfirmImportSyncDataDialog.Listener.class), anyString(), anyString());
+                        any(ConfirmImportSyncDataDialogCoordinator.Listener.class), anyString(),
+                        anyString());
         verify(mDelegateMock)
                 .showFetchManagementPolicyProgressDialog(
                         any(ConfirmSyncDataStateMachineDelegate.ProgressDialogListener.class));
