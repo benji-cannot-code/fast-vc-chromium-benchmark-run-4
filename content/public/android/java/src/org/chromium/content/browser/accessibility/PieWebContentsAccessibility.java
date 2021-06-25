81a19313c3c42a74db42b261935d88b89dd831a1
Mark Schillaci <mschillaci@google.com>
2021-06-25 18:39:35 +0000

    [Reland] Added accessibility event on Android for when dialogs are opened
    This CL relands crrev.com/c/2708619
    
    This CL expands accessibility support for modal dialogs.
    
    With the current implementation, when a site opens a modal dialog,
    Chrome alerts AT of a window content change, but does not include
    any further information such as dialog title/text.
    
    With this CL we update to now send a TYPE_WINDOW_STATE_CHANGED event
    specifying that a new pane has appeared, and we add text for the
    title of that pane.
    
    To populate the text, we look for the aria-labelledby nodes for the
    dialog or alertdialog in question, and concatenate all inner texts.
    Some pages use aria-describedby, which we check when no labels are
    available. And if neither labels nor describedby are available, we
    default to a simple message of: "dialog opened."
    
    This CL applies to role="dialog" and role="alertdialog" elements.
    
    AX-Relnotes: role="dialog" and role="alertdialog" elements on Android now proactively send their title to AT when opened.
    Bug: 1179314
    Change-Id: Iadbe30bff41dc4960b9b8ee95da5871122b8a20f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2980223
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/master@{#896139}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/PieWebContentsAccessibility.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/PieWebContentsAccessibility.java
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.accessibility;
 
+import static android.view.accessibility.AccessibilityEvent.CONTENT_CHANGE_TYPE_PANE_APPEARED;
+
 import android.annotation.TargetApi;
 import android.os.Build;
+import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.autofill.AutofillManager;
 
@@ -28,6 +31,19 @@ public class PieWebContentsAccessibility extends OWebContentsAccessibility {
             // always initialize the native parts when the user has an Autofill service enabled.
             refreshState();
             getAccessibilityNodeProvider();
+        }
+    }
+
+    @Override
+    protected void handleDialogModalOpened(int virtualViewId) {
+        if (isAccessibilityEnabled()) {
+            AccessibilityEvent event =
+                    AccessibilityEvent.obtain(AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED);
+            if (event == null) return;
+
+            event.setContentChangeTypes(CONTENT_CHANGE_TYPE_PANE_APPEARED);
+            event.setSource(mView, virtualViewId);
+            super.requestSendAccessibilityEvent(event);
         }
     }
 
--- content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
@@ -1431,6 +1431,11 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProvider
     }
 
     @CalledByNative
+    protected void handleDialogModalOpened(int virtualViewId) {
+        // Requires P or higher.
+    }
+
+    @CalledByNative
     private void announceLiveRegionText(String text) {
         if (isAccessibilityEnabled()) {
             AccessibilityEvent event =
@@ -1774,7 +1779,7 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProvider
         if (rect.bottom > viewportRectBottom) rect.bottom = viewportRectBottom;
     }
 
-    private void requestSendAccessibilityEvent(AccessibilityEvent event) {
+    protected void requestSendAccessibilityEvent(AccessibilityEvent event) {
         // If there is no parent, then the event can be ignored. In general the parent is only
         // transiently null (such as during teardown, switching tabs...). Also ensure that
         // accessibility is still enabled, throttling may result in events sent late.
