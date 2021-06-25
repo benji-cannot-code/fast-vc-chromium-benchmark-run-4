964f1c2915b365ee6261b1f1b6bb378e068e37c3
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2021-06-25 17:31:46 +0000

    SVG Text NG: Fix Text not drawing when CSS 'outline' is specified
    LayoutSVGInline::ObjectBoundingBox was empty with 'outline' specified
    because there is an NGFragmentItem::ItemType::kBox between the
    LayoutSVGInline and NGFragmentItem::ItemType::kSvgText.
    
    Now iterating descendants of non-kSvgText items when accumulating the
    ObjectBoundingBox.
    
    Bug: 1179585
    Change-Id: I7ba217431821f43f258552fb1ad5883fef11d3b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983506
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#896103}

--- third_party/blink/renderer/core/layout/svg/layout_svg_inline.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_inline.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class NGInlineCursor;
+
 class LayoutSVGInline : public LayoutInline {
  public:
   explicit LayoutSVGInline(Element*);
@@ -72,6 +74,9 @@ class LayoutSVGInline : public LayoutInline {
   void WillBeRemovedFromTree() override;
 
   bool IsObjectBoundingBoxValid() const;
+
+  static void ObjectBoundingBoxForCursor(NGInlineCursor& cursor,
+                                         FloatRect& bounds);
 };
 
 template <>
