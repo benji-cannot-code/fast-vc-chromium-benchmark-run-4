5e17b915132f18eb0336efa7af6d826d5a6b7f7d
Steve Kobes <skobes@chromium.org>
2021-06-25 13:58:01 +0000

    [Scroll Unif.] Chain to local parent frame in main-thread hit test.
    If the scroll hits an iframe that isn't scrollable for some reason, we
    need to look for a scrollable container in the parent frame.
    
    Bug: 1223032
    Change-Id: I4c1fd69712532802ec120821441d443087bed3c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983821
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896035}

--- third_party/blink/renderer/core/layout/hit_test_result.cc
+++ third_party/blink/renderer/core/layout/hit_test_result.cc
@@ -43,7 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
 #include "third_party/blink/renderer/core/layout/layout_block.h"
+#include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
 #include "third_party/blink/renderer/core/layout/layout_image.h"
+#include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/page/scrolling/top_document_root_scroller_controller.h"
@@ -258,7 +260,10 @@ CompositorElementId HitTestResult::GetScrollableContainer() const {
           cur_box->UniqueId(), CompositorElementIdNamespace::kScroll);
     }
 
-    cur_box = cur_box->ContainingBlock();
+    if (IsA<LayoutView>(cur_box))
+      cur_box = cur_box->GetFrame()->OwnerLayoutObject();
+    else
+      cur_box = cur_box->ContainingBlock();
   }
 
   return InnerNode()
