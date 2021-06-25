6ff3e18c59dd8dc8e7ad3df686db6d200674df96
Tommy Li <tommycli@chromium.org>
2021-06-25 20:19:15 +0000

    [memories] Plumb the score from the clustering backend to the WebUI
    This CL completes the plumbing that moves the score from the clustering
    backend all the way to the WebUI.
    
    This CL also sorts the visits from best to worst within each cluster.
    Then it also sorts each cluster from newest to oldest based on the top
    visit within each cluster.
    
    It then adds a test to verify the above.
    
    Bug: 1171352
    Change-Id: Ie267ce47a86022ab403b36c28cc794f0c33c0eee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983242
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896179}

--- components/history_clusters/core/remote_clustering_backend.cc
+++ components/history_clusters/core/remote_clustering_backend.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "components/history/core/browser/history_types.h"
 #include "components/history_clusters/core/memories_features.h"
 #include "components/history_clusters/core/proto/clusters.pb.h"
 #include "net/base/net_errors.h"
@@ -106,8 +107,10 @@ std::vector<history::Cluster> ParseResponseProto(
       const auto visits_it = base::ranges::find(
           visits, cluster_visit.visit_id(),
           [](const auto& visit) { return visit.visit_row.visit_id; });
-      if (visits_it != visits.end())
-        cluster.annotated_visits.push_back(*visits_it);
+      if (visits_it != visits.end()) {
+        cluster.scored_annotated_visits.push_back(
+            {*visits_it, cluster_visit.score()});
+      }
     }
     clusters.push_back(cluster);
   }
