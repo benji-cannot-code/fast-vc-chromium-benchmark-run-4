ce11ceacea610d12a8e5336c024a3ef98f6b9e26
Juliet Levesque <julietlevesque@google.com>
2021-06-25 22:38:50 +0000

    [Fast Pair] Create FastPairScanner to observe BluetoothAdapter events
    Creates Fast Pair Scanner to observe devices being added and removed to
    the Bluetooth Adapter and exposes these events to observers. A follow up
    CL will add discovery session logic.
    
    Change-Id: I0577248ca66fd0d276e96d993008fce6d7492ceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2984119
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#896239}

--- chromeos/components/quick_pair/scanning/scanner_broker_impl.cc
+++ chromeos/components/quick_pair/scanning/scanner_broker_impl.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace quick_pair {
 
+ScannerBrokerImpl::ScannerBrokerImpl() = default;
+
+ScannerBrokerImpl::~ScannerBrokerImpl() = default;
+
 void ScannerBrokerImpl::AddObserver(Observer* observer) {
   observers_.AddObserver(observer);
 }
