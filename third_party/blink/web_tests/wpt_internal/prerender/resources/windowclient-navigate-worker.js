a2a19af1775b56c33f81d04a2c778d7a7bd73de9
Asami Doi <asamidoi@chromium.org>
2021-06-25 10:34:44 +0000

    Prerender: fix WindowClient.navigate() in a prerendered main frame
    If WindowClient is a prerendered main page, `navigate()` should throw a
    TypeError.
    The behavior of navigate() is aligned with the main frame navigation
    during the prerendering state (https://crbug.com/1194414) and the type
    of an error comes from the Service Worker's spec
    (https://w3c.github.io/ServiceWorker/#dom-windowclient-navigate).
    
    Previously `Navigator::RequestOpenURL()` didn't set `frame_tree_node_id`
    for prerendered main frames so it does not to find a correct frame
    tree and ends up to create a new WebContentsImpl.
    This CL disallows the navigation from a prerendered main frame by
    calling `DidNavigate()` in `NavigateClient()` and it doesn't call
    `RequestOpenURL()` and doesn't create a `OpenURLObserver`.
    
    This CL also adds a test to check if `navigate()` throws a TypeError.
    
    Discussion for navigate():
    https://docs.google.com/document/d/1m-8rVqnAAgPhz2LkLWZFbIcH2PRfJdx8ldHQVA58-HI/edit?resourcekey=0-Bi9ruk3wa5gHbjjExTjmsw&disco=AAAAIcqNiB4
    
    Bug: 1217835
    Change-Id: I9fe90f05220e6b17f00ea33aa2af2bb34fcff493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2936739
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895989}

---
+++ third_party/blink/web_tests/wpt_internal/prerender/resources/windowclient-navigate-worker.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.onmessage = e => {
+  const url = e.data;
+
+  e.waitUntil(async function() {
+    const source = e.source;
+    const clients = await self.clients.matchAll();
+    const client = clients.find(c => c.url.includes('prerendering'));
+    if (!client) {
+      source.postMessage('Client was not found');
+      return;
+    }
+
+    try {
+      await client.navigate(url);
+      source.postMessage('navigate() succeeded');
+      return;
+    } catch (e) {
+      if (e instanceof TypeError) {
+        source.postMessage('navigate() failed with TypeError');
+      } else {
+        source.postMessage('navigate() failed with unknown error');
+      }
+      return;
+    }
+  }());
+};
