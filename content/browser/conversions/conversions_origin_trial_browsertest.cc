ce53fa5d821ea891d4f2a75e1b2f82ad18fe72c2
John Delaney <johnidel@chromium.org>
2021-06-25 16:26:08 +0000

    Disable ConversionsOriginTrialBrowserTest.OriginTrialEnabled_ImpressionRegistered
    Resulting in a flaky data race on linux tsan
    
    Change-Id: Idaef63cc07283f205a6699801dcbf6a5f9991271
    Bug: 1121464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988161
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896086}

--- content/browser/conversions/conversions_origin_trial_browsertest.cc
+++ content/browser/conversions/conversions_origin_trial_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "build/build_config.h"
 #include "base/strings/strcat.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
@@ -93,8 +94,15 @@ IN_PROC_BROWSER_TEST_F(ConversionsOriginTrialBrowserTest,
   EXPECT_EQ(true, EvalJs(shell(), "window.attributionReporting === undefined"));
 }
 
+#if defined(OS_LINUX)
+// TODO(https://crbug.com/1121464): Flaky on linux.
+#define MAYBE_OriginTrialEnabled_ImpressionRegistered DISABLED_OriginTrialEnabled_ImpressionRegistered
+#else
+#define MAYBE_OriginTrialEnabled_ImpressionRegistered OriginTrialEnabled_ImpressionRegistered
+#endif
+
 IN_PROC_BROWSER_TEST_F(ConversionsOriginTrialBrowserTest,
-                       OriginTrialEnabled_ImpressionRegistered) {
+                       MAYBE_OriginTrialEnabled_ImpressionRegistered) {
   EXPECT_TRUE(NavigateToURL(
       shell(), GURL("https://example.test/impression_with_origin_trial.html")));
 
