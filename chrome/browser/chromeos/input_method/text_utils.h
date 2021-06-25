e14ddee337bd69f479bc58c73c8a66fedab1e522
Jing Wang <jiwan@chromium.org>
2021-06-25 02:37:44 +0000

    grammar: Add more util functions for sentence segmentation.
    Add some higher level functions for sentence segmentation, the existing
    lower level functions may turn out to be not necessary to be exposed.
    But I am keeping them for now for a better test coverage, and will
    remove them in the future if they are not useful in the end.
    
    Bug: 1132699,1222895
    Change-Id: I45167f51749216a5e6714ec9b0ccc0e83c52565e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982941
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#895891}

--- chrome/browser/chromeos/input_method/text_utils.h
+++ chrome/browser/chromeos/input_method/text_utils.h
@@ -8,11 +8,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ui/gfx/range/range.h"
+
 // TODO(crbug/1223213): Move these to a sandbox environment.
 namespace chromeos {
 namespace text_utils {
 
 const int kUndefined = -1;
+
+struct Sentence {
+  Sentence();
+  Sentence(const gfx::Range& range, const std::u16string& text);
+  Sentence(const Sentence& other);
+  ~Sentence();
+
+  bool operator==(const Sentence& other) const;
+  bool operator!=(const Sentence& other) const;
+
+  // The range of the sentence in the original text.
+  gfx::Range original_range;
+  std::u16string text;
+};
 
 // Find the index of the last sentence end before |pos|, returns |kUndefined| if
 // not found.
@@ -21,6 +37,12 @@ int FindLastSentenceEnd(const std::u16string& text, int pos);
 // Find the index of the first sentence end equal or after |pos|, returns
 // |kUndefined| if not found.
 int FindNextSentenceEnd(const std::u16string& text, int pos);
+
+// Find the last sentence before cursor position |pos|.
+Sentence FindLastSentence(const std::u16string& text, int pos);
+
+// Find the sentence containing the cursor position |pos|.
+Sentence FindCurrentSentence(const std::u16string& text, int pos);
 
 }  // namespace text_utils
 }  // namespace chromeos
