4c36850587cee22a7cabf607bdf4dfce0fa31e83
Leonid Baraz <lbaraz@chromium.org>
2021-06-25 18:20:14 +0000

    Fix missived client leak
    The error case affects both fake and prod client, although for the prod
    case it is immaterial: the client is used as a singleton.
    
    Bug: 1216789
    Change-Id: I498b840434c78029652438a160b9d2d661e3a4dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2989097
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#896128}

--- chromeos/dbus/missive/missive_client.cc
+++ chromeos/dbus/missive/missive_client.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
+#include "base/no_destructor.h"
 #include "chromeos/dbus/missive/fake_missive_client.h"
 #include "components/reporting/proto/interface.pb.h"
 #include "components/reporting/proto/record.pb.h"
@@ -208,12 +209,14 @@ MissiveClient::~MissiveClient() {
 // static
 void MissiveClient::Initialize(dbus::Bus* bus) {
   DCHECK(bus);
-  (new MissiveClientImpl())->Init(bus);
+  static base::NoDestructor<MissiveClientImpl> missive_client;
+  missive_client->Init(bus);
 }
 
 // static
 void MissiveClient::InitializeFake() {
-  (new FakeMissiveClient())->Init();
+  static base::NoDestructor<FakeMissiveClient> missive_client;
+  missive_client->Init();
 }
 
 // static
