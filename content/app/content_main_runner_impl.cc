3c2434d52bed5c24c528e3a2d2c5d70f5e16c5b8
Benoit Lize <lizeb@chromium.org>
2021-06-25 16:10:42 +0000

    [content/app] Log library preloading failures.
    Some libraries are loaded in the zygote before the sandbox is
    initialized. Then they are later used by loading them again (which will
    not trigger an actual load since dlopen() deduplicates the
    requests). This means that our only chance to log a failure is in zygote
    initialization, and not when the library is later "loaded".
    
    This is the case for Widevine CDM for instance, which is used to decrypt
    DRM'd video content. In media/cdm/cdm_module.cc, there is a LOG(ERROR)
    when loading fails, which is not reached in production. Align the two
    logging statements (the one in cdm_module.cc is kept, as it is useful in
    tests) to the same level/verbosity.
    
    The current verbosity means that we cannot get any log messages for
    library loading issues happening on devices (see linked bug).
    
    Bug: b/191314803
    Change-Id: I58f97eaeb0c371f90a7dfe8af554db041f0693b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987826
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896078}

--- content/app/content_main_runner_impl.cc
+++ content/app/content_main_runner_impl.cc
@@ -326,10 +326,8 @@ void PreloadPepperPlugins() {
       base::NativeLibraryLoadError error;
       base::NativeLibrary library =
           base::LoadNativeLibrary(plugin.path, &error);
-      VLOG_IF(1, !library) << "Unable to load plugin " << plugin.path.value()
-                           << " " << error.ToString();
-
-      ignore_result(library);  // Prevent release-mode warning.
+      LOG_IF(ERROR, !library) << "Unable to load plugin " << plugin.path.value()
+                              << " " << error.ToString();
     }
   }
 }
@@ -344,9 +342,8 @@ void PreloadLibraryCdms() {
   for (const auto& cdm : cdms) {
     base::NativeLibraryLoadError error;
     base::NativeLibrary library = base::LoadNativeLibrary(cdm.path, &error);
-    VLOG_IF(1, !library) << "Unable to load CDM " << cdm.path.value()
-                         << " (error: " << error.ToString() << ")";
-    ignore_result(library);  // Prevent release-mode warning.
+    LOG_IF(ERROR, !library) << "Unable to load CDM " << cdm.path.value()
+                            << " (error: " << error.ToString() << ")";
   }
 }
 #endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
