49e5dc60e51cb57e0e964e269302caae12405f3e
Clarissa Garvey <clarissagarvey@chromium.org>
2021-06-26 00:19:37 +0000

    media/base: Add Bitrate with Mode for VBR/CBR use
    This CL:
    - Introduces a new Bitrate class to transport data related to the
      bitrate, including the bitrate mode
    - Introduces a new Bitrate::Mode enum (for VBR and CBR)
    
    This CL is the first in a series of CLs that add an API for VBR
    encoding. The design doc is available to @google.com users at
    go/vbr-vea.
    
    Bug: b:181797390
    Test: No new functionality; tests completed in follow-up CLs.
    Change-Id: I264db695aac234914ff4375830eca3434b84f572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2986204
    Commit-Queue: Clarissa Garvey <clarissagarvey@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896273}

---
+++ media/base/bitrate.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/bitrate.h"
+#include "base/check_op.h"
+
+namespace media {
+
+bool Bitrate::operator==(const Bitrate& right) const {
+  return (this->mode_ == right.mode_) && (this->target_ == right.target_) &&
+         (this->peak_ == right.peak_);
+}
+
+uint32_t Bitrate::peak() const {
+  DCHECK_EQ(mode_ == Mode::kConstant, peak_ == 0u);
+  return peak_;
+}
+
+}  // namespace media
