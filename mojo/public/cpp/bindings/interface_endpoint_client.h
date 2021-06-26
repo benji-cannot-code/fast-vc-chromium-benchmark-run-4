8ab0a08089f44e0a007c1a8d2c205ef11f650b05
Ken Rockot <rockot@google.com>
2021-06-26 00:10:30 +0000

    mojo: Interrupt off-thread sync waits on disconnect
    This fixes a bug which could lead to hangs when performing off-thread
    sync waits via SharedRemote or SharedAssociatedRemote if there are other
    undelivered tasks from the pipe for the waiting thread when the pipe is
    disconnected.
    
    The lifetime of an off-thread sync wait is tied to that of its
    request's async responder in InterfaceEndpointClient, and the
    same request ID is registered with the MultiplexRouter's
    corresponding Endpoint. The approach here is to ensure that on
    pipe disconnection, MultiplexRouter goes over all its endpoints and
    all their registered off-thread sync waits, and tells the
    InterfaceEndpointClients to drop all corresponding async responders.
    This in turn signals the waiting thread to wake up and give up.
    
    Fixed: 1223628
    Change-Id: Id21b35c547f2679e0b602e247fa2b3678c3b6f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987244
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#896267}

--- mojo/public/cpp/bindings/interface_endpoint_client.h
+++ mojo/public/cpp/bindings/interface_endpoint_client.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/sequenced_task_runner.h"
+#include "base/synchronization/lock.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -86,6 +87,7 @@ class COMPONENT_EXPORT(MOJO_CPP_BINDINGS) InterfaceEndpointClient
   // Returns true if this endpoint has any pending callbacks.
   bool has_pending_responders() const {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    base::AutoLock lock(async_responders_lock_);
     return !async_responders_.empty() || !sync_responses_.empty();
   }
 
@@ -202,6 +204,11 @@ class COMPONENT_EXPORT(MOJO_CPP_BINDINGS) InterfaceEndpointClient
   // surprise message dispatches.
   void ResetFromAnotherSequenceUnsafe();
 
+  // Tells the client to forget about a pending async request for which it still
+  // hasn't seen a response. Called by the router, possibly from other threads.
+  // The router lock must be held when calling this.
+  void ForgetAsyncRequest(uint64_t request_id);
+
  private:
   // Maps from the id of a response to the MessageReceiver that handles the
   // response.
@@ -285,7 +292,8 @@ class COMPONENT_EXPORT(MOJO_CPP_BINDINGS) InterfaceEndpointClient
   HandleIncomingMessageThunk thunk_{this};
   MessageDispatcher dispatcher_;
 
-  AsyncResponderMap async_responders_;
+  mutable base::Lock async_responders_lock_;
+  AsyncResponderMap async_responders_ GUARDED_BY(async_responders_lock_);
   SyncResponseMap sync_responses_;
 
   uint64_t next_request_id_ = 1;
