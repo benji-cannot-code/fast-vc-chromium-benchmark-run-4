79ee3cbaa1732467d0d80c7a601f54c246bae3a9
Andrew Paseltiner <apaseltiner@chromium.org>
2021-06-26 00:51:04 +0000

    [attribution_reporting] Falsely attribute event sources
    https://github.com/WICG/conversion-measurement-api/blob/main/event_attribution_reporting.md#noise-on-the-attribution-triggering-event
    
    Bug: 1185874
    Change-Id: I9c1283986349fee8afe7f291c643484f05b7ca5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2926295
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896281}

--- content/browser/conversions/conversion_storage_delegate_impl.cc
+++ content/browser/conversions/conversion_storage_delegate_impl.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/conversions/conversion_storage_delegate_impl.h"
 
+#include "base/rand_util.h"
+#include "content/browser/conversions/conversion_policy.h"
+
 namespace content {
 
 ConversionStorageDelegateImpl::ConversionStorageDelegateImpl(bool debug_mode)
@@ -54,7 +57,30 @@ StorableImpression::AttributionLogic
 ConversionStorageDelegateImpl::SelectAttributionLogic(
     const StorableImpression& impression) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  return StorableImpression::AttributionLogic::kTruthfully;
+
+  if (debug_mode_)
+    return StorableImpression::AttributionLogic::kTruthfully;
+
+  switch (impression.source_type()) {
+    case StorableImpression::SourceType::kNavigation:
+      return StorableImpression::AttributionLogic::kTruthfully;
+    case StorableImpression::SourceType::kEvent: {
+      // TODO(apaseltiner): Finalize a value for this so that noise is actually
+      // triggered.
+      const double kNoise = 0;
+      if (base::RandDouble() < (1 - kNoise))
+        return StorableImpression::AttributionLogic::kTruthfully;
+      if (base::RandInt(0, 1) == 0)
+        return StorableImpression::AttributionLogic::kNever;
+      return StorableImpression::AttributionLogic::kFalsely;
+    }
+  }
+}
+
+uint64_t ConversionStorageDelegateImpl::GetFakeEventSourceTriggerData() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return ConversionPolicy::NoiseProvider::GetNoisedEventSourceTriggerDataImpl(
+      base::RandUint64());
 }
 
 base::Time ConversionStorageDelegateImpl::GetReportTime(
--- content/browser/conversions/conversion_test_utils.h
+++ content/browser/conversions/conversion_test_utils.h
@@ -89,6 +89,7 @@ class ConfigurableStorageDelegate : public ConversionStorage::Delegate {
   StorableImpression::AttributionLogic SelectAttributionLogic(
       const StorableImpression& impression) const override;
   int GetMaxAttributionDestinationsPerEventSource() const override;
+  uint64_t GetFakeEventSourceTriggerData() const override;
 
   void set_max_conversions_per_impression(int max) {
     max_conversions_per_impression_ = max;
@@ -113,6 +114,10 @@ class ConfigurableStorageDelegate : public ConversionStorage::Delegate {
     attribution_logic_ = attribution_logic;
   }
 
+  void set_fake_event_source_trigger_data(uint64_t data) {
+    fake_event_source_trigger_data_ = data;
+  }
+
   void set_report_time_ms(int report_time_ms) {
     report_time_ms_ = report_time_ms;
   }
@@ -130,6 +135,8 @@ class ConfigurableStorageDelegate : public ConversionStorage::Delegate {
 
   StorableImpression::AttributionLogic attribution_logic_ =
       StorableImpression::AttributionLogic::kTruthfully;
+
+  uint64_t fake_event_source_trigger_data_ = 0;
 
   int report_time_ms_ = 0;
 };
