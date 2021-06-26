ecf6d49c8df35c6c8e504beadc26cdf3b67c2d52
Sean Kau <skau@chromium.org>
2021-06-26 00:48:23 +0000

    Remove Cloud Print Printers from search types if it's disabled
    If Cloud Print is disabled, we try to call search() on a null object.
    Because the search never starts, we never get a SEARCH_DONE or SEARCH_FAILED
    event to allow us to continue. Additionally, if we never fail we always
    have PrinterType.CLOUD_PRINTER in our TypesToSearch causing fallback
    destination selection to never complete.
    
    Bug: b:191052942, chromium:1195284
    Change-Id: I2ab83d96384e2be6d80a81dc250cf3bd2d7a3fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2977972
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896280}

--- chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
+++ chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
@@ -602,6 +602,12 @@ TEST_F(
     });
 
 TEST_F(
+    'PrintPreviewDestinationStoreTest', 'RecentCloudPrintFallback', function() {
+      this.runMochaTest(
+          destination_store_test.TestNames.RecentCloudPrintFallback);
+    });
+
+TEST_F(
     'PrintPreviewDestinationStoreTest', 'MultipleRecentDestinations',
     function() {
       this.runMochaTest(
@@ -613,6 +619,13 @@ TEST_F(
     function() {
       this.runMochaTest(destination_store_test.TestNames
                             .MultipleRecentDestinationsOneRequest);
+    });
+
+TEST_F(
+    'PrintPreviewDestinationStoreTest',
+    'MultipleRecentDestinationsAndCloudPrint', function() {
+      this.runMochaTest(destination_store_test.TestNames
+                            .MultipleRecentDestinationsAndCloudPrint);
     });
 
 TEST_F(
@@ -772,7 +785,6 @@ var PrintPreviewDestinationDialogCrosTest = class extends PrintPreviewTest {
   get suiteName() {
     return destination_dialog_cros_test.suiteName;
   }
-
 };
 
 TEST_F('PrintPreviewDestinationDialogCrosTest', 'PrinterList', function() {
