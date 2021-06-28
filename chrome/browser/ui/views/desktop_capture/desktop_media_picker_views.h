63b2ccbabf4175406b301c8e2a4646c54ade6709
Elad Alon <eladalon@chromium.org>
2021-06-28 16:21:52 +0000

    Refactor DesktopMediaPickerDialogView
    This refactoring reduces some clutter in DesktopMediaPickerDialogView
    and paves the way to later moving the audio checkbox into the newly
    defined DisplaySurfaceCategory, thereby allowing distinct audio
    checkboxes to be associated with different display surface categories.
    
    Bug: 1224342, 1220450
    Change-Id: I33e4e9b19c9eef96dd45b00b60cb5ea6147e67b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988110
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896539}

--- chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h
+++ chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h
@@ -69,6 +69,19 @@ class DesktopMediaPickerDialogView : public views::DialogDelegateView,
  private:
   friend class DesktopMediaPickerViewsTestApi;
 
+  struct DisplaySurfaceCategory {
+    DisplaySurfaceCategory(
+        DesktopMediaList::Type type,
+        std::unique_ptr<DesktopMediaListController> controller);
+
+    DisplaySurfaceCategory(DisplaySurfaceCategory&& other);
+
+    ~DisplaySurfaceCategory();
+
+    DesktopMediaList::Type type;
+    std::unique_ptr<DesktopMediaListController> controller;
+  };
+
   void OnSourceTypeSwitched(int index);
 
   int GetSelectedTabIndex() const;
@@ -87,8 +100,7 @@ class DesktopMediaPickerDialogView : public views::DialogDelegateView,
   views::Checkbox* audio_share_checkbox_ = nullptr;
 
   views::TabbedPane* tabbed_pane_ = nullptr;
-  std::vector<std::unique_ptr<DesktopMediaListController>> list_controllers_;
-  std::vector<DesktopMediaList::Type> source_types_;
+  std::vector<DisplaySurfaceCategory> categories_;
 
   DialogSource dialog_source_;
 
