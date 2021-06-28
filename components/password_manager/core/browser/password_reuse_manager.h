3a17f181be53157a92a7d12adcd80587543a8d6e
Viktor Semeniuk <vsemeniuk@google.com>
2021-06-28 09:28:01 +0000

    Factories for PasswordReuseManager
    This CL created factories for PasswordReuseManager on desktop(Android)
    and iOS.
    
    Bug: 715987
    Change-Id: Ifc733337311773cab6ec1589aab9803499ec4b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983029
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: John Wu <jzw@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896450}

--- components/password_manager/core/browser/password_reuse_manager.h
+++ components/password_manager/core/browser/password_reuse_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "components/keyed_service/core/keyed_service.h"
 #include "components/password_manager/core/browser/hash_password_manager.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_reuse_detector.h"
@@ -24,10 +25,9 @@ using metrics_util::GaiaPasswordHashChange;
 
 // Per-store class responsible for detection of password reuse, i.e. that the
 // user input on some site contains the password saved on another site.
-class PasswordReuseManager {
+class PasswordReuseManager : public KeyedService {
  public:
   PasswordReuseManager() = default;
-  virtual ~PasswordReuseManager() = default;
 
   PasswordReuseManager(const PasswordReuseManager&) = delete;
   PasswordReuseManager& operator=(const PasswordReuseManager&) = delete;
