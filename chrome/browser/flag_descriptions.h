c4f9e7d18d3534f8e789abcb36c59d70b545f922
Curtis McMullan <curtismcmullan@chromium.org>
2021-06-28 01:13:25 +0000

    emoji-suggest: add enhanced emoji flag
    Add the enhanced emoji suggestions flag to cros.
    
    Bug: 1223666
    Change-Id: I18c0b01a6f97653775bcbccc7b6e7166b4549dad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2986937
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896385}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2681,6 +2681,9 @@ extern const char kHideArcMediaNotificationsDescription[];
 extern const char kImeAssistAutocorrectName[];
 extern const char kImeAssistAutocorrectDescription[];
 
+extern const char kImeAssistEmojiEnhancedName[];
+extern const char kImeAssistEmojiEnhancedDescription[];
+
 extern const char kImeAssistMultiWordName[];
 extern const char kImeAssistMultiWordDescription[];
 
