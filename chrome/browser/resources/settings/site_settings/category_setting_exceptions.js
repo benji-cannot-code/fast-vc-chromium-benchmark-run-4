418bd6946a61e37f9afc4ae1c3b7c29c0d5107a8
Xiaohan Wang <xhwang@chromium.org>
2021-06-28 19:27:46 +0000

    media: Update protected media identifier exception description
    As discussed with UX, on the "Protected content" content setting page,
    the "Customized Behaviors" says "Sites listed below follow a custom
    setting instead of the default", where "default" typically refers to the
    "Default behavior" for most settings. But for "Protected content", we
    only show the title "Default behavior" for the "Protected content"
    setting, not the "Protected content IDs", which makes it confusing.
    
    The recommendation by UX is removing the phrase "instead of the default"
    from the sentence "Sites listed below follow a custom setting instead of
    the default".
    
    This CL updates category_setting_exceptions.js to support a customized
    description, and use the shorter description on "Protected content"
    settings page.
    
    Screenshots:
    Old: https://photos.app.goo.gl/6GN8FZgWP5F7UnWQ9
    New: https://photos.app.goo.gl/5LE5JSxrR8jGoe3r9
    
    Bug: 1221658
    Change-Id: I11bdc51a125ef18b9c85defa9e6ff591dfb1d132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987903
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#896597}

--- chrome/browser/resources/settings/site_settings/category_setting_exceptions.js
+++ chrome/browser/resources/settings/site_settings/category_setting_exceptions.js
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import './site_list.js';
 
+import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
 import {WebUIListenerBehavior} from 'chrome://resources/js/web_ui_listener_behavior.m.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -22,9 +23,18 @@ Polymer({
 
   _template: html`{__html_template__}`,
 
-  behaviors: [SiteSettingsBehavior, WebUIListenerBehavior],
+  behaviors: [I18nBehavior, SiteSettingsBehavior, WebUIListenerBehavior],
 
   properties: {
+    /**
+     * The string description shown below the header.
+     */
+    description: {
+      type: String,
+      value() {
+        return this.i18n('siteSettingsCustomizedBehaviorsDescription');
+      }
+    },
 
     /**
      * The string ID of the category that this element is displaying data for.
