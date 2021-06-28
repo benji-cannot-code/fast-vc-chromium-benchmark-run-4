9c2771c22efbb2d41536cd23111c8a5b790ed5a9
Brandon Wylie <wylieb@chromium.org>
2021-06-28 21:23:33 +0000

    Prevent stale icon callbacks in HistoryItemView
    Bug: 1222726
    Change-Id: Idc075fb7949931741edeab3bdee841d32019454d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2986984
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896654}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryItemView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryItemView.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.history;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,15 +25,13 @@ import org.chromium.chrome.browser.ui.favicon.FaviconHelper.DefaultFaviconHelper
 import org.chromium.chrome.browser.ui.favicon.FaviconUtils;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
 import org.chromium.components.browser_ui.widget.selectable_list.SelectableItemView;
-import org.chromium.components.favicon.IconType;
-import org.chromium.components.favicon.LargeIconBridge.LargeIconCallback;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.user_prefs.UserPrefs;
 
 /**
  * The SelectableItemView for items displayed in the browsing history UI.
  */
-public class HistoryItemView extends SelectableItemView<HistoryItem> implements LargeIconCallback {
+public class HistoryItemView extends SelectableItemView<HistoryItem> {
     private ImageButton mRemoveButton;
     private VectorDrawableCompat mBlockedVisitDrawable;
 
@@ -157,18 +154,19 @@ public class HistoryItemView extends SelectableItemView<HistoryItem> implements 
         if (getItem() != null) getItem().onItemClicked();
     }
 
-    @Override
-    public void onLargeIconAvailable(Bitmap icon, int fallbackColor, boolean isFallbackColorDefault,
-            @IconType int iconType) {
-        Drawable drawable = FaviconUtils.getIconDrawableWithoutFilter(icon, getItem().getUrl(),
-                fallbackColor, mIconGenerator, getResources(), mDisplayedIconSize);
-        setStartIconDrawable(drawable);
-    }
-
     private void requestIcon() {
-        if (!getItem().wasBlockedVisit()) {
-            getItem().getLargeIconForUrl(mMinIconSize, this);
-        }
+        HistoryItem item = getItem();
+        if (item.wasBlockedVisit()) return;
+        item.getLargeIconForUrl(
+                mMinIconSize, (icon, fallbackColor, isFallbackColorDefault, iconType) -> {
+                    // Prevent stale icons from making it through to the UI.
+                    if (item != getItem()) return;
+
+                    Drawable drawable = FaviconUtils.getIconDrawableWithoutFilter(icon,
+                            getItem().getUrl(), fallbackColor, mIconGenerator, getResources(),
+                            mDisplayedIconSize);
+                    setStartIconDrawable(drawable);
+                });
     }
 
     private void updateRemoveButtonVisibility() {
