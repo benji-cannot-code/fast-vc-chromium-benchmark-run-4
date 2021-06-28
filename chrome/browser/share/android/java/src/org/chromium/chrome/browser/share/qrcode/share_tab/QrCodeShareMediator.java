a8cf1baffd9e01d92fbda2b73ea9d23812d8204d
Jeffrey Cohen <jeffreycohen@chromium.org>
2021-06-28 22:13:27 +0000

    QRCode Crash fix
    Bug: 1224301
    Change-Id: Ice5652f6c947577ebdd691f4f02b12246ff1dcd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992743
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896684}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareMediator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareMediator.java
@@ -101,7 +101,7 @@ class QrCodeShareMediator {
     protected void downloadQrCode(View view) {
         logDownload();
         Bitmap qrcodeBitmap = mPropertyModel.get(QrCodeShareViewProperties.QRCODE_BITMAP);
-        if (qrcodeBitmap != null && !mIsDownloadInProgress) {
+        if (qrcodeBitmap != null && !mIsDownloadInProgress && mPermissionDelegate != null) {
             FileAccessPermissionHelper.requestFileAccessPermission(
                     mPermissionDelegate, this::finishDownloadWithPermission);
             return;
@@ -129,6 +129,7 @@ class QrCodeShareMediator {
 
     /** Returns whether the user can be prompted for storage permissions. */
     private Boolean canPromptForPermission() {
+        if (mPermissionDelegate == null) return false;
         return mPermissionDelegate.canRequestPermission(permission.WRITE_EXTERNAL_STORAGE);
     }
 
