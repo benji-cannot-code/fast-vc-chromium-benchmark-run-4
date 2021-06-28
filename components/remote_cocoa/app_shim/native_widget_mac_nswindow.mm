1e1a294d41a4ac6ee97ad189dc41b5ec24e63e3a
Christopher Cameron <ccameron@chromium.org>
2021-06-28 20:15:13 +0000

    Fix crash in NativeWidgetMacNSWindow observeValueForKeyPath
    The asserts in this method were too aggressive.
    
    Bug: 1222985
    Change-Id: If05fa65cad765404eb1c16e37996291b3cc3af1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991357
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896624}

--- components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
+++ components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
@@ -128,12 +128,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                       ofObject:(id)object
                         change:(NSDictionary*)change
                        context:(void*)context {
-  DCHECK(_isEnforcingNeverMadeVisible);
-  DCHECK([keyPath isEqual:@"visible"]);
-  DCHECK_EQ(object, self);
-  DCHECK_EQ(context, nil);
-  if ([change[NSKeyValueChangeNewKey] boolValue])
-    base::debug::DumpWithoutCrashing();
+  if ([keyPath isEqual:@"visible"]) {
+    DCHECK(_isEnforcingNeverMadeVisible);
+    DCHECK_EQ(object, self);
+    DCHECK_EQ(context, nil);
+    if ([change[NSKeyValueChangeNewKey] boolValue])
+      base::debug::DumpWithoutCrashing();
+  }
+  [super observeValueForKeyPath:keyPath
+                       ofObject:object
+                         change:change
+                        context:context];
 }
 
 // Public methods.
