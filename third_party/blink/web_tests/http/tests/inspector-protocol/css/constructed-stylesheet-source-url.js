2b30e9bdb570b550c12dfa490e6919965acca842
Sigurd Schneider <sigurds@chromium.org>
2021-06-28 07:44:35 +0000

    Sort sheets to make test output determininistic
    The test
    http/tests/inspector-protocol/css/constructed-stylesheet-source-url.js
    was dumping CDP events in order of reception, which depends on internal
    browser state. This CL sorts the events before dumping them for
    comparison with the golden file.
    
    Fixed: chromium:1222531
    Change-Id: Ib7785754c7180a3f2cc7a4dd2534caac13dc2ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2989020
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896431}

--- third_party/blink/web_tests/http/tests/inspector-protocol/css/constructed-stylesheet-source-url.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/css/constructed-stylesheet-source-url.js
@@ -13,11 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     dp.DOM.enable();
     dp.CSS.enable();
 
-    var styleSheets = [];
-    for (var i = 0; i < 2; ++i)
+    const styleSheets = [];
+    for (let i = 0; i < 2; ++i)
       styleSheets.push(await dp.CSS.onceStyleSheetAdded());
 
-    for (var styleSheet of styleSheets) {
+    styleSheets.sort((a,b) => a.params.header.sourceURL.localeCompare(b.params.header.sourceURL));
+
+    for (const styleSheet of styleSheets) {
       testRunner.log(`isConstructed: ${styleSheet.params.header.isConstructed}`);
       const sourceURL = styleSheet.params.header.sourceURL;
       const trimmedSourceURL = sourceURL.substring(sourceURL.lastIndexOf("/") + 1);
