6cf842da11b599ab24fc0ff5835c0bebc9798009
Austin Sullivan <asully@chromium.org>
2021-06-28 23:24:57 +0000

    Add FileSystemHandle::Remove method
    Currently, it is not possible to remove a file or directory given its
    handle. You must obtain the handle of the parent directory and call
    FileSystemDirectoryHandle::RemoveEntry.
    
    This CL allows for the removal of a file or directory from the local
    file system without needing the handle of its parent.
    
    Bug: 1114923
    Change-Id: I1db1ef54e6c3a15069a3b8afa100b34f3f912e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2732700
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896704}

--- content/browser/file_system_access/file_system_access_file_handle_impl_unittest.cc
+++ content/browser/file_system_access/file_system_access_file_handle_impl_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
@@ -93,6 +94,22 @@ class FileSystemAccessFileHandleImplTest : public testing::Test {
     }
   }
 
+  std::unique_ptr<FileSystemAccessFileHandleImpl>
+  GetHandleWithPermissions(const base::FilePath& path, bool read, bool write) {
+    auto url_and_fs = manager_->CreateFileSystemURLFromPath(
+        test_src_origin_, FileSystemAccessEntryFactory::PathType::kLocal, path);
+    auto handle = std::make_unique<FileSystemAccessFileHandleImpl>(
+        manager_.get(),
+        FileSystemAccessManagerImpl::BindingContext(
+            test_src_origin_, test_src_url_, /*worker_process_id=*/1),
+        url_and_fs.url,
+        FileSystemAccessManagerImpl::SharedHandleState(
+            /*read_grant=*/read ? allow_grant_ : deny_grant_,
+            /*write_grant=*/write ? allow_grant_ : deny_grant_,
+            url_and_fs.file_system));
+    return handle;
+  }
+
  protected:
   const GURL test_src_url_ = GURL("http://example.com/foo");
   const url::Origin test_src_origin_ = url::Origin::Create(test_src_url_);
@@ -109,6 +126,10 @@ class FileSystemAccessFileHandleImplTest : public testing::Test {
   scoped_refptr<FixedFileSystemAccessPermissionGrant> allow_grant_ =
       base::MakeRefCounted<FixedFileSystemAccessPermissionGrant>(
           FixedFileSystemAccessPermissionGrant::PermissionStatus::GRANTED,
+          base::FilePath());
+  scoped_refptr<FixedFileSystemAccessPermissionGrant> deny_grant_ =
+      base::MakeRefCounted<FixedFileSystemAccessPermissionGrant>(
+          FixedFileSystemAccessPermissionGrant::PermissionStatus::DENIED,
           base::FilePath());
   std::unique_ptr<FileSystemAccessFileHandleImpl> handle_;
 };
@@ -164,6 +185,40 @@ TEST_F(FileSystemAccessFileHandleImplTest, CreateFileWriterOverLimitNotOK) {
                       result->status);
             loop.Quit();
           }));
+  loop.Run();
+}
+
+TEST_F(FileSystemAccessFileHandleImplTest, Remove_NoWriteAccess) {
+  base::FilePath file;
+  ASSERT_TRUE(base::CreateTemporaryFileInDir(dir_.GetPath(), &file));
+
+  auto handle = GetHandleWithPermissions(file, /*read=*/true, /*write=*/false);
+
+  base::RunLoop loop;
+  handle->Remove(
+      base::BindLambdaForTesting([&file](blink::mojom::FileSystemAccessErrorPtr
+                                             result) {
+        EXPECT_EQ(result->status,
+                  blink::mojom::FileSystemAccessStatus::kPermissionDenied);
+        EXPECT_TRUE(base::PathExists(file));
+      }).Then(loop.QuitClosure()));
+  loop.Run();
+}
+
+TEST_F(FileSystemAccessFileHandleImplTest, Remove_HasWriteAccess) {
+  base::FilePath file;
+  ASSERT_TRUE(base::CreateTemporaryFileInDir(dir_.GetPath(), &file));
+
+  auto handle = GetHandleWithPermissions(file, /*read=*/true, /*write=*/true);
+
+  base::RunLoop loop;
+  handle->Remove(base::BindLambdaForTesting(
+                     [&file](blink::mojom::FileSystemAccessErrorPtr result) {
+                       EXPECT_EQ(result->status,
+                                 blink::mojom::FileSystemAccessStatus::kOk);
+                       EXPECT_FALSE(base::PathExists(file));
+                     })
+                     .Then(loop.QuitClosure()));
   loop.Run();
 }
 
--- third_party/blink/renderer/modules/file_system_access/file_system_handle.h
+++ third_party/blink/renderer/modules/file_system_access/file_system_handle.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class ExecutionContext;
 class FileSystemHandlePermissionDescriptor;
+class FileSystemRemoveOptions;
 
 class FileSystemHandle : public ScriptWrappable, public ExecutionContextClient {
   DEFINE_WRAPPERTYPEINFO();
@@ -41,6 +42,8 @@ class FileSystemHandle : public ScriptWrappable, public ExecutionContextClient {
   ScriptPromise requestPermission(ScriptState*,
                                   const FileSystemHandlePermissionDescriptor*);
 
+  ScriptPromise remove(ScriptState*, const FileSystemRemoveOptions* options);
+
   ScriptPromise isSameEntry(ScriptState*, FileSystemHandle* other);
 
   // Grab a handle to a transfer token. This may return an invalid PendingRemote
@@ -58,6 +61,9 @@ class FileSystemHandle : public ScriptWrappable, public ExecutionContextClient {
       bool writable,
       base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr,
                               mojom::blink::PermissionStatus)>) = 0;
+  virtual void RemoveImpl(
+      const FileSystemRemoveOptions* options,
+      base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr)>) = 0;
   virtual void IsSameEntryImpl(
       mojo::PendingRemote<mojom::blink::FileSystemAccessTransferToken> other,
       base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr,
