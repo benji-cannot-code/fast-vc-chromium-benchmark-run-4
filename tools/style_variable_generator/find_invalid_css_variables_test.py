f4f5ed583ad159ef09832ce4fad4152621dd5d7e
Christopher Lam <calamity@chromium.org>
2021-06-28 10:45:48 +0000

    [style_var_gen] Fix find_invalid_css_variables.py
    This CL updates the script to work with Python3 and adds opacities to
    the variables to search for.
    
    Bug: 1018654
    Change-Id: I66796b2e13b5c41c98769a695a0777605a888960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991419
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896473}

--- tools/style_variable_generator/find_invalid_css_variables_test.py
+++ tools/style_variable_generator/find_invalid_css_variables_test.py
@@ -4,6 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import sys
+import os
+
+sys.path += [os.path.dirname(os.path.dirname(__file__))]
+
 from style_variable_generator.find_invalid_css_variables import FindInvalidCSSVariables
 import unittest
 
@@ -11,7 +16,7 @@ import unittest
 class FindInvalidCSSVariablesTest(unittest.TestCase):
     def testUnspecified(self):
         def GitResult(command):
-            return '''--test-not-specified
+            return b'''--test-not-specified
 --test-only-rgb-used-rgb
 --test-toolbar'''
 
@@ -29,8 +34,7 @@ class FindInvalidCSSVariablesTest(unittest.TestCase):
 }
         '''
 
-        result = FindInvalidCSSVariables(json_string,
-                                         'test',
+        result = FindInvalidCSSVariables({'test': json_string},
                                          git_runner=GitResult)
         unused = set()
         self.assertEqual(result['unused'], unused)
@@ -39,7 +43,7 @@ class FindInvalidCSSVariablesTest(unittest.TestCase):
 
     def testUnused(self):
         def GitResult(command):
-            return '''--test-toolbar'''
+            return b'''--test-toolbar'''
 
         json_string = '''
 {
@@ -55,8 +59,7 @@ class FindInvalidCSSVariablesTest(unittest.TestCase):
 }
         '''
 
-        result = FindInvalidCSSVariables(json_string,
-                                         'test',
+        result = FindInvalidCSSVariables({'test': json_string},
                                          git_runner=GitResult)
         unused = set(['--test-unused'])
         self.assertEqual(result['unused'], unused)
@@ -76,8 +79,7 @@ class FindInvalidCSSVariablesTest(unittest.TestCase):
         '''
         self.assertRaises(KeyError,
                           FindInvalidCSSVariables,
-                          json_string,
-                          'test',
+                          {'test': json_string},
                           git_runner=GitResult)
 
 
