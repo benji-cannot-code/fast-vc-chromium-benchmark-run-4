0b97c2c3292a3576e9975be4ee71c3539d579414
Roland Bock <rbock@google.com>
2021-06-28 14:12:53 +0000

    PolicyService: Use THREAD_CHECKER macros
    Using ThreadChecker directly is considered bad practice and uses extra
    bytes in production.
    See base/threading/thread_checker.h or PRESUBMIT.py.
    
    Also replace DISALLOW_COPY_AND_ASSIGN.
    
    Bug: None
    Change-Id: I327e5cb4b7ef4ddef135bd4f89187ca32e851e9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988766
    Commit-Queue: Roland Bock <rbock@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896517}

--- components/policy/core/common/policy_service_impl.h
+++ components/policy/core/common/policy_service_impl.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
-#include "base/threading/thread_checker.h"
+#include "base/sequence_checker.h"
 #include "build/build_config.h"
 #include "components/policy/core/common/configuration_policy_provider.h"
 #include "components/policy/core/common/policy_bundle.h"
@@ -54,6 +54,9 @@ class POLICY_EXPORT PolicyServiceImpl
   static std::unique_ptr<PolicyServiceImpl> CreateWithThrottledInitialization(
       Providers providers,
       Migrators migrators = std::vector<std::unique_ptr<PolicyMigrator>>());
+
+  PolicyServiceImpl(const PolicyServiceImpl&) = delete;
+  PolicyServiceImpl& operator=(const PolicyServiceImpl&) = delete;
 
   ~PolicyServiceImpl() override;
 
@@ -171,8 +174,8 @@ class POLICY_EXPORT PolicyServiceImpl
   std::unique_ptr<android::PolicyServiceAndroid> policy_service_android_;
 #endif
 
-  // Used to verify thread-safe usage.
-  base::ThreadChecker thread_checker_;
+  // Used to verify usage in correct sequence.
+  SEQUENCE_CHECKER(sequence_checker_);
 
   // Used to create tasks to delay new policy updates while we may be already
   // processing previous policy updates.
@@ -181,8 +184,6 @@ class POLICY_EXPORT PolicyServiceImpl
 
   // Used to protect against crbug.com/747817 until crbug.com/1221454 is done.
   base::WeakPtrFactory<PolicyServiceImpl> weak_ptr_factory_{this};
-
-  DISALLOW_COPY_AND_ASSIGN(PolicyServiceImpl);
 };
 
 }  // namespace policy
