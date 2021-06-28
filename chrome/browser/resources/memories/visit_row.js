10076292f11e4050f1edbe64e508cde850941a58
Moe Ahmadi <mahmadi@chromium.org>
2021-06-28 19:40:40 +0000

    [memories] Displays bookmark and tab group annotations in landing page
    + some CSS tweaks.
    
    Light mode: screenshot/49QqVbmhCeJZBVo
    Dark mode: screenshot/4zG3KsaNA2mwJT3
    
    Bug: 2982963
    Change-Id: I492852a8f06e8d0868f17e100476f3d67b9e6df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2984248
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896604}

--- chrome/browser/resources/memories/visit_row.js
+++ chrome/browser/resources/memories/visit_row.js
@@ -22,6 +22,15 @@ import {getHostnameFromUrl} from './utils.js';
  * as well as an action menu.
  */
 
+/**
+ * Maps supported annotations to localized string identifiers.
+ * @type {Map<number,string>}
+ */
+const annotationToStringId = new Map([
+  [Annotation.kBookmarked, 'bookmarked'],
+  [Annotation.kTabGrouped, 'savedInTabGroup'],
+]);
+
 class VisitRowElement extends PolymerElement {
   static get is() {
     return 'visit-row';
@@ -56,6 +65,15 @@ class VisitRowElement extends PolymerElement {
       //========================================================================
       // Private properties
       //========================================================================
+
+      /**
+       * Annotations to show for the visit (e.g., whether page was bookmarked).
+       * @private {!Array<string>}
+       */
+      annotations_: {
+        type: Object,
+        computed: 'computeAnnotations_(visit)',
+      },
 
       /**
        * Whether the visit is to the search results page.
@@ -126,6 +144,14 @@ class VisitRowElement extends PolymerElement {
   //============================================================================
   // Helper methods
   //============================================================================
+
+  /** @private */
+  computeAnnotations_() {
+    return this.visit.annotations
+        .map(annotation => annotationToStringId.get(annotation))
+        .filter(id => !!id)
+        .map(id => loadTimeData.getString(id));
+  }
 
   /** @private */
   computeIsSearchResultsPage_() {
