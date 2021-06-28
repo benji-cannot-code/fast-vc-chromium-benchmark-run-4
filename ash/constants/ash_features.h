54d2d4f47a6ba601e27e0bcf69e449ef94085592
danielng <danielng@google.com>
2021-06-28 06:31:36 +0000

    borealis: add flag for disk management
    Adding a flag for disk management to help control its rollout. It will
    only appear on borealis enabled devices.
    
    Bug: b/191828731
    Change-Id: I67b5051e14116170fe99b2695c8db0a7dc3bc9dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2980355
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Commit-Queue: Daniel Ng <danielng@google.com>
    Cr-Commit-Position: refs/heads/master@{#896414}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -89,6 +89,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kBluetoothPhoneFilter;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kBluetoothWbsDogfood;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kBorealisDiskManagement;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kButtonARCNetworkDiagnostics;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kCameraPrivacySwitchNotifications;
