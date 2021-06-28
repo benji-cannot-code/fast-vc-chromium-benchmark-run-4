f90163779c4105c4ecf85719b0a77947c6d4aafd
Shakti Sahu <shaktisahu@chromium.org>
2021-06-28 17:39:31 +0000

    [segmentation_platform] Miscellaneous cleanups
    This CL adds
    - Ensure that signal DB is initialized before any method calls.
    - Added an unittest in metadata_utils.
    - Removed redundant call to ModelExecutionScheduler from SegmentSelector.
    
    Bug: 1223469
    Change-Id: I31ab53d692d993ec681adbaea1266fe9215aafa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2990030
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896560}

--- components/segmentation_platform/internal/selection/segment_selector_impl.cc
+++ components/segmentation_platform/internal/selection/segment_selector_impl.cc
@@ -77,6 +77,8 @@ void SegmentSelectorImpl::OnModelExecutionCompleted(
 void SegmentSelectorImpl::set_model_execution_scheduler(
     ModelExecutionScheduler* model_execution_scheduler) {
   model_execution_scheduler_ = model_execution_scheduler;
+  // TODO(shaktisahu): After this change, this field is unused. Remove it.
+  // This will also cleanup weird dependency issue in the service constructor.
 }
 
 void SegmentSelectorImpl::FindBestSegment(
@@ -154,10 +156,6 @@ void SegmentSelectorImpl::ReadScoresFromLastSession(
 
   initialized_ = true;
   std::move(callback).Run();
-
-  // Request model execution.
-  model_execution_scheduler_->RequestModelExecutionForEligibleSegments(
-      true /* expired_only */);
 }
 
 int SegmentSelectorImpl::ConvertToDiscreteScore(
