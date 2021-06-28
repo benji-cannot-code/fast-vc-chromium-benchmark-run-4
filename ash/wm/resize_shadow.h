5b16dcb7e51503e98c8d7a93ef410d262e0cff46
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-06-28 01:02:55 +0000

    Ensure resize shadow type
    This CL changes to recreate the ResizeShadow if the requested resize
    shadow type is different from the current type when we call
    RSController::ShowShadow().
    
    BUG=b:191832367
    TEST=ResizeShadowAndCursorTest.ResizeShadowTypeChange
    
    Change-Id: I8cb64e2ef23a5eeb171807879489d2874a0131e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982577
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896383}

--- ash/wm/resize_shadow.h
+++ ash/wm/resize_shadow.h
@@ -53,6 +53,7 @@ class ResizeShadow {
 
   int GetLastHitTestForTest() const { return last_hit_test_; }
   const ui::Layer* GetLayerForTest() const { return layer_.get(); }
+  ResizeShadowType GetResizeShadowTypeForTest() const { return type_; }
 
  private:
   friend class ResizeShadowController;
