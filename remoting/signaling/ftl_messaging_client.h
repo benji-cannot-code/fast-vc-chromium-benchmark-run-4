fb1e9e93454ea0e3f396b2caedbecb1999607e7c
Joe Downing <joedow@google.com>
2021-06-28 19:58:43 +0000

    Removing PullMessages from the Chromium FTL impl
    This was added a few years ago for testing before we knew which APIs we
    would be need to use.  There doesn't seem to be any use case for the
    PullMessage path and it pops up from time to time in maintenance CLs so
    I think we should just remove it and simplify the implementation.
    
    Change-Id: I1f704d701dd075419abeb642e7191a8cb519a064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973515
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896613}

--- remoting/signaling/ftl_messaging_client.h
+++ remoting/signaling/ftl_messaging_client.h
@@ -52,7 +52,6 @@ class FtlMessagingClient final : public MessagingClient {
   // MessagingClient implementations.
   base::CallbackListSubscription RegisterMessageCallback(
       const MessageCallback& callback) override;
-  void PullMessages(DoneCallback on_done) override;
   void SendMessage(const std::string& destination,
                    const std::string& destination_registration_id,
                    const ftl::ChromotingMessage& message,
@@ -75,11 +74,6 @@ class FtlMessagingClient final : public MessagingClient {
                       std::unique_ptr<google::protobuf::MessageLite> request,
                       CallbackFunctor callback_functor,
                       DoneCallback on_done);
-
-  void OnPullMessagesResponse(
-      DoneCallback on_done,
-      const ProtobufHttpStatus& status,
-      std::unique_ptr<ftl::PullMessagesResponse> response);
 
   void OnSendMessageResponse(DoneCallback on_done,
                              const ProtobufHttpStatus& status,
