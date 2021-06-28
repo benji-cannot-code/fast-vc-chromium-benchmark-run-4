81b70e545f50fc6f089f056c39f8aea982ab5746
Will Harris <wfh@chromium.org>
2021-06-28 20:40:29 +0000

    Fix typo in OptionalTest.
    Bug=None
    
    Change-Id: I129f7c080d5934613515faae38d4580a598673fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2986785
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896638}

--- base/optional_unittest.cc
+++ base/optional_unittest.cc
@@ -1654,7 +1654,7 @@ TEST(OptionalTest, ValueNotEq_NotEmpty) {
   }
 }
 
-TEST(OPtionalTest, ValueNotEq_DifferentType) {
+TEST(OptionalTest, ValueNotEq_DifferentType) {
   absl::optional<int> opt(0);
   EXPECT_FALSE(opt != 0.0);
 }
@@ -1700,7 +1700,7 @@ TEST(OptionalTest, ValueLess_NotEmpty) {
   }
 }
 
-TEST(OPtionalTest, ValueLess_DifferentType) {
+TEST(OptionalTest, ValueLess_DifferentType) {
   absl::optional<int> opt(0);
   EXPECT_TRUE(opt < 1.0);
 }
