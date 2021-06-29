8fad1fa93a31d3d0f57f2927e0dbeff29ead377c
Justin Cohen <justincohen@google.com>
2021-06-29 14:54:26 +0000

    Update Crashpad to c4081b8e6ddf08b94dd61d8f1a5df421ab42a1cf
    3b0ebc29cdba Cleanup some diffs between Chromium and Crashpad
    c4081b8e6ddf fuchsia: Port syslog includes to use SDK path
    
    Change-Id: Icc74ab52906e1259943f20b128874a9966df28e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992463
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896920}

--- third_party/crashpad/crashpad/client/annotation.cc
+++ third_party/crashpad/crashpad/client/annotation.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <type_traits>
 
+#include "base/check_op.h"
 #include "client/annotation_list.h"
 
 namespace crashpad {
