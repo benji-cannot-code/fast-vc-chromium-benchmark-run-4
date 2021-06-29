adaf104c1bfb3b167fe38e34d21432881930c8f0
Irem Uguz <iremuguz@google.com>
2021-06-29 08:44:48 +0000

    ash: Add notification handler to debug_daemon
    Adds notification handler that implements the observer functions of
    debug_daemon_client. The notification handler shows an informational
    pinned notification when a packet capture start signal is received. The
    user can stop ongoing packet capture processes by clicking the stop
    button on the notification.
    
    BUG=b:181525070
    TEST= Open Crosh and run "packet_capture" command. A notification should
    appear when the command is running and it should disappear when the
    command is terminated or stop button is clicked.
    
    Change-Id: I5a99c848b0b9199de3776cd012c44d13adca025a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2964332
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Irem Uğuz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/master@{#896823}

--- chromeos/dbus/debug_daemon/debug_daemon_client.h
+++ chromeos/dbus/debug_daemon/debug_daemon_client.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/tracing_agent.h"
 #include "chromeos/dbus/dbus_client.h"
 #include "chromeos/dbus/dbus_method_call_status.h"
+#include "dbus/message.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace cryptohome {
@@ -311,6 +312,15 @@ class COMPONENT_EXPORT(DEBUG_DAEMON) DebugDaemonClient
   virtual void SetSwapParameter(const std::string& parameter,
                                 int32_t value,
                                 DBusMethodCallback<std::string> callback) = 0;
+
+  // Stops the packet capture process identified with |handle|. |handle| is a
+  // unique process identifier that is returned from debugd's PacketCaptureStart
+  // D-Bus method when the packet capture process is started. Stops all on-going
+  // packet capture operations if the |handle| is empty.
+  virtual void StopPacketCapture(const std::string& handle) = 0;
+
+  virtual void PacketCaptureStartSignalReceived(dbus::Signal* signal) = 0;
+  virtual void PacketCaptureStopSignalReceived(dbus::Signal* signal) = 0;
 
   // Factory function, creates a new instance and returns ownership.
   // For normal usage, access the singleton via DBusThreadManager::Get().
