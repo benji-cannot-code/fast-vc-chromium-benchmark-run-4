5d518035eb3eab88ad22368b965a4e584fb22a09
Nico Weber <thakis@chromium.org>
2021-06-29 15:04:19 +0000

    Revert "Reland "Reland "Adds a way to create app shortcuts in different folder on Mac/Win/Linux."""
    This reverts commit 7c1b95bbd9fa7a988d5f87c50bee1b4b305d337f.
    
    Reason for revert: Test is still extremely flaky:
    https://ci.chromium.org/p/chromium/builders/ci/mac-arm64-rel-tests
    
    Original change's description:
    > Reland "Reland "Adds a way to create app shortcuts in different folder on Mac/Win/Linux.""
    >
    > This is a reland of 8e311e70bc2b5dfe8ae0654a8cb8c346a4924178
    >
    > Original change's description:
    > > Reland "Adds a way to create app shortcuts in different folder on Mac/Win/Linux."
    > >
    > > This is a reland of 479f56930ace7c1d1a0223f1ebb9fb38a0b916e9
    > >
    > > Original change's description:
    > > > Adds a way to create app shortcuts in different folder on Mac/Win/Linux.
    > > >
    > > > Change-Id: I3c06104c86333cbbd0857085cf7d7dd1fca63616
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2906422
    > > > Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    > > > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#892494}
    > >
    > > Change-Id: I3d003f1cb644402ffafaa2553d225da42c230081
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2965263
    > > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > > Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#895652}
    >
    > Change-Id: I1291946931f5bc1dab76e0a1339e99b5cd5a2b8a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987632
    > Commit-Queue: Clifford Cheng <cliffordcheng@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#896707}
    
    Change-Id: I6e2e1ce4b1429644707109f784224aeeee41c4f2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992467
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#896923}

--- chrome/browser/web_applications/components/web_app_shortcut_linux.cc
+++ chrome/browser/web_applications/components/web_app_shortcut_linux.cc
@@ -49,14 +49,6 @@ web_app::LaunchXdgUtilityForTesting& GetInstalledLaunchXdgUtilityForTesting() {
   return *instance;
 }
 
-base::FilePath GetDesktopPath() {
-  base::FilePath desktop_path;
-  if (web_app::GetShortcutOverrideForTesting())
-    return web_app::GetShortcutOverrideForTesting()->desktop;
-  base::PathService::Get(base::DIR_USER_DESKTOP, &desktop_path);
-  return desktop_path;
-}
-
 // Result of creating app shortcut icon.
 // Success is recorded for each icon image, but the first two errors
 // are per app, so the success/error ratio might not be very meaningful.
@@ -210,8 +202,8 @@ bool CreateShortcutAtLocation(const base::FilePath location_path,
 
 bool CreateShortcutOnDesktop(const base::FilePath& shortcut_filename,
                              const std::string& contents) {
-  base::FilePath desktop_path = GetDesktopPath();
-  if (desktop_path.empty()) {
+  base::FilePath desktop_path;
+  if (!base::PathService::Get(base::DIR_USER_DESKTOP, &desktop_path)) {
     RecordCreateShortcut(CreateShortcutResult::kFailToGetDesktopPath);
     return false;
   }
@@ -222,7 +214,6 @@ bool CreateShortcutOnDesktop(const base::FilePath& shortcut_filename,
 bool CreateShortcutInAutoStart(base::Environment* env,
                                const base::FilePath& shortcut_filename,
                                const std::string& contents) {
-  DCHECK(!web_app::GetShortcutOverrideForTesting());
   base::FilePath autostart_path = AutoStart::GetAutostartDirectory(env);
   if (!base::DirectoryExists(autostart_path) &&
       !base::CreateDirectory(autostart_path)) {
@@ -241,7 +232,6 @@ bool CreateShortcutInApplicationsMenu(base::Environment* env,
                                       const std::string& contents,
                                       const base::FilePath& directory_filename,
                                       const std::string& directory_contents) {
-  DCHECK(!web_app::GetShortcutOverrideForTesting());
   base::ScopedTempDir temp_dir;
   if (!temp_dir.CreateUniqueTempDir()) {
     RecordCreateShortcut(CreateShortcutResult::kFailToCreateTempDir);
@@ -332,9 +322,9 @@ base::FilePath GetAppShortcutFilename(const base::FilePath& profile_path,
 }
 
 bool DeleteShortcutOnDesktop(const base::FilePath& shortcut_filename) {
-  base::FilePath desktop_path = GetDesktopPath();
+  base::FilePath desktop_path;
   bool result = false;
-  if (!desktop_path.empty())
+  if (base::PathService::Get(base::DIR_USER_DESKTOP, &desktop_path))
     result = base::DeleteFile(desktop_path.Append(shortcut_filename));
   return result;
 }
@@ -373,21 +363,6 @@ bool CreateDesktopShortcut(base::Environment* env,
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::MAY_BLOCK);
 
-  bool create_shortcut_in_startup = creation_locations.in_startup;
-  // Do not create the shortcuts in startup directory when testing because
-  // xdg-utility (which creates this shortcut) doesn't work well with temp
-  // directories.
-  if (web_app::GetShortcutOverrideForTesting())
-    create_shortcut_in_startup = false;
-
-  ApplicationsMenuLocation applications_menu_location =
-      creation_locations.applications_menu_location;
-  // Do not create the shortcuts in startup directory when testing because
-  // xdg-utility (which creates this shortcut) doesn't work well with temp
-  // directories.
-  if (web_app::GetShortcutOverrideForTesting())
-    applications_menu_location = APP_MENU_LOCATION_NONE;
-
   base::FilePath shortcut_filename;
   if (!shortcut_info.extension_id.empty()) {
     shortcut_filename = GetAppShortcutFilename(shortcut_info.profile_path,
@@ -397,11 +372,11 @@ bool CreateDesktopShortcut(base::Environment* env,
     if (creation_locations.on_desktop)
       DeleteShortcutOnDesktop(shortcut_filename);
 
-    if (create_shortcut_in_startup)
-      // if (creation_locations.in_startup)
+    if (creation_locations.in_startup)
       DeleteShortcutInAutoStart(env, shortcut_filename);
 
-    if (applications_menu_location != APP_MENU_LOCATION_NONE) {
+    if (creation_locations.applications_menu_location !=
+        APP_MENU_LOCATION_NONE) {
       DeleteShortcutInApplicationsMenu(shortcut_filename, base::FilePath());
     }
   } else {
@@ -436,8 +411,7 @@ bool CreateDesktopShortcut(base::Environment* env,
     success = CreateShortcutOnDesktop(shortcut_filename, contents);
   }
 
-  if (create_shortcut_in_startup) {
-    // if (creation_locations.in_startup) {
+  if (creation_locations.in_startup) {
     std::string contents = shell_integration_linux::GetDesktopFileContents(
         chrome_exe_path, app_name, shortcut_info.url,
         shortcut_info.extension_id, shortcut_info.title, icon_name,
@@ -446,7 +420,7 @@ bool CreateDesktopShortcut(base::Environment* env,
         CreateShortcutInAutoStart(env, shortcut_filename, contents) && success;
   }
 
-  if (applications_menu_location == APP_MENU_LOCATION_NONE) {
+  if (creation_locations.applications_menu_location == APP_MENU_LOCATION_NONE) {
     return success;
   }
 
@@ -506,8 +480,9 @@ ShortcutLocations GetExistingShortcutLocations(
   ShortcutLocations locations;
 
   // Determine whether there is a shortcut on desktop.
-  base::FilePath desktop_path = GetDesktopPath();
-
+  base::FilePath desktop_path;
+  // If Get returns false, just leave desktop_path empty.
+  base::PathService::Get(base::DIR_USER_DESKTOP, &desktop_path);
   if (!desktop_path.empty()) {
     locations.on_desktop =
         base::PathExists(desktop_path.Append(shortcut_filename));
@@ -568,8 +543,8 @@ bool DeleteAllDesktopShortcuts(base::Environment* env,
 
   bool result = true;
   // Delete shortcuts from Desktop.
-  base::FilePath desktop_path = GetDesktopPath();
-  if (!desktop_path.empty()) {
+  base::FilePath desktop_path;
+  if (base::PathService::Get(base::DIR_USER_DESKTOP, &desktop_path)) {
     std::vector<base::FilePath> shortcut_filenames_desktop =
         shell_integration_linux::GetExistingProfileShortcutFilenames(
             profile_path, desktop_path);
@@ -636,7 +611,9 @@ std::vector<base::FilePath> GetShortcutLocations(
   DCHECK(!shortcut_filename.empty());
 
   if (locations.on_desktop) {
-    base::FilePath desktop_path = GetDesktopPath();
+    base::FilePath desktop_path;
+    // If Get returns false, just leave |desktop_path| empty.
+    base::PathService::Get(base::DIR_USER_DESKTOP, &desktop_path);
     if (!desktop_path.empty()) {
       base::FilePath desktop_shortcut_path =
           desktop_path.Append(shortcut_filename);
