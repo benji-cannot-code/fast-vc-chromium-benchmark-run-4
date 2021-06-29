2d90a2cc101614a66dcf2fdbc28d923b770f2233
Dan Sanders <sandersd@chromium.org>
2021-06-29 21:56:29 +0000

    [webcodecs] Replace ConstraintError.
    ConstraintError was used early in development but doesn't accurately
    describe the nature of errors in WebCodecs. This CL replaces all uses
    with more appropriate errors.
    
    In most cases, the correct error is a TypeError, indicating that
    arguments were not valid according to the specification. In cases where
    an implementation detail resulted in a ConstraintError, OperationError
    is used.
    
    Change-Id: If124557e2fe9fb8d9f150ad107b627def75f27a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2993573
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897118}

--- third_party/blink/web_tests/external/wpt/webcodecs/videoFrame-copyTo.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/videoFrame-copyTo.any.js
@@ -100,7 +100,7 @@ promise_test(async t => {
 promise_test(async t => {
   const frame = makeI420_4x2();
   const data = new Uint8Array(11);
-  await promise_rejects_dom(t, 'ConstraintError', frame.copyTo(data));
+  await promise_rejects_js(t, TypeError, frame.copyTo(data));
 }, 'Test undersized buffer.');
 
 promise_test(async t => {
@@ -108,9 +108,9 @@ promise_test(async t => {
   const options = {
     layout: [{offset: 0, stride: 4}],
   };
-  assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
+  assert_throws_js(TypeError, () => frame.allocationSize(options));
   const data = new Uint8Array(12);
-  await promise_rejects_dom(t, 'ConstraintError', frame.copyTo(data, options));
+  await promise_rejects_js(t, TypeError, frame.copyTo(data, options));
 }, 'Test incorrect plane count.');
 
 promise_test(async t => {
@@ -169,9 +169,9 @@ promise_test(async t => {
           {offset: 10, stride: 2},
       ],
   };
-  assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
+  assert_throws_js(TypeError, () => frame.allocationSize(options));
   const data = new Uint8Array(12);
-  await promise_rejects_dom(t, 'ConstraintError', frame.copyTo(data, options));
+  await promise_rejects_js(t, TypeError, frame.copyTo(data, options));
 }, 'Test invalid stride.');
 
 promise_test(async t => {
@@ -183,9 +183,9 @@ promise_test(async t => {
           {offset: 2 ** 32 - 2, stride: 2},
       ],
   };
-  assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
+  assert_throws_js(TypeError, () => frame.allocationSize(options));
   const data = new Uint8Array(12);
-  await promise_rejects_dom(t, 'ConstraintError', frame.copyTo(data, options));
+  await promise_rejects_js(t, TypeError, frame.copyTo(data, options));
 }, 'Test address overflow.');
 
 promise_test(async t => {
@@ -225,9 +225,9 @@ promise_test(async t => {
   const options = {
       rect: {x: 0, y: 0, width: 4, height: 1},
   };
-  assert_throws_dom('ConstraintError', () => frame.allocationSize(options));
+  assert_throws_js(TypeError, () => frame.allocationSize(options));
   const data = new Uint8Array(12);
-  await promise_rejects_dom(t, 'ConstraintError', frame.copyTo(data, options));
+  await promise_rejects_js(t, TypeError, frame.copyTo(data, options));
 }, 'Test unaligned rect.');
 
 promise_test(async t => {
