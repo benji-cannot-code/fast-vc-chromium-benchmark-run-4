4018aa4bb2361073d6eaa1c3fee70b68dee9e97b
dpapad <dpapad@chromium.org>
2021-06-29 19:45:56 +0000

    Settings: Migrate top-level UI elements to Polymer class syntax.
    Specifically migrating settings-about-page, settings-basic-page,
    settings-main and settings-ui.
    
    Bug: 1206112
    Change-Id: I199d159167e53823767995a050ab61bd9108ae2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991796
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897062}

--- chrome/test/data/webui/settings/advanced_page_test.js
+++ chrome/test/data/webui/settings/advanced_page_test.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {CrSettingsPrefs} from 'chrome://settings/settings.js';
+import {CrSettingsPrefs, SettingsBasicPageElement, SettingsMainElement, SettingsUiElement} from 'chrome://settings/settings.js';
 
 import {assertEquals, assertGT, assertTrue} from '../chai_assert.js';
 
@@ -21,7 +21,8 @@ suite('AdvancedPage', function() {
 
   suiteSetup(function() {
     document.body.innerHTML = '';
-    const settingsUi = document.createElement('settings-ui');
+    const settingsUi = /** @type {SettingsUiElement} */ (
+        document.createElement('settings-ui'));
     document.body.appendChild(settingsUi);
     return CrSettingsPrefs.initialized
         .then(() => {
@@ -30,7 +31,7 @@ suite('AdvancedPage', function() {
         .then(page => {
           basicPage = page;
           const settingsMain = /** @type {!SettingsMainElement} */ (
-              settingsUi.$$('settings-main'));
+              settingsUi.shadowRoot.querySelector('settings-main'));
           assertTrue(!!settingsMain);
           settingsMain.advancedToggleExpanded = true;
           flush();
--- chrome/test/data/webui/settings/settings_ui_tests.js
+++ chrome/test/data/webui/settings/settings_ui_tests.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {CrDrawerElement, CrSettingsPrefs, CrToolbarElement, CrToolbarSearchFieldElement, Router, routes, SettingsMenuElement} from 'chrome://settings/settings.js';
+import {CrDrawerElement, CrSettingsPrefs, CrToolbarElement, CrToolbarSearchFieldElement, Router, routes, SettingsMenuElement, SettingsUiElement} from 'chrome://settings/settings.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 import {eventToPromise} from '../test_util.m.js';
@@ -29,8 +29,10 @@ suite('SettingsUIToolbarAndDrawer', function() {
     document.body.appendChild(ui);
     return CrSettingsPrefs.initialized.then(() => {
       flush();
-      toolbar = /** @type {!CrToolbarElement} */ (ui.$$('cr-toolbar'));
-      drawer = /** @type {!CrDrawerElement} */ (ui.$$('#drawer'));
+      toolbar = /** @type {!CrToolbarElement} */ (
+          ui.shadowRoot.querySelector('cr-toolbar'));
+      drawer = /** @type {!CrDrawerElement} */ (
+          ui.shadowRoot.querySelector('#drawer'));
     });
   });
 
@@ -44,7 +46,7 @@ suite('SettingsUIToolbarAndDrawer', function() {
   });
 
   test('app drawer', async () => {
-    assertEquals(null, ui.$$('cr-drawer settings-menu'));
+    assertEquals(null, ui.shadowRoot.querySelector('cr-drawer settings-menu'));
     assertFalse(!!drawer.open);
 
     const drawerOpened = eventToPromise('cr-drawer-opened', drawer);
@@ -53,7 +55,7 @@ suite('SettingsUIToolbarAndDrawer', function() {
 
     // Validate that dialog is open and menu is shown so it will animate.
     assertTrue(drawer.open);
-    assertTrue(!!ui.$$('cr-drawer settings-menu'));
+    assertTrue(!!ui.shadowRoot.querySelector('cr-drawer settings-menu'));
 
     await drawerOpened;
     const drawerClosed = eventToPromise('close', drawer);
@@ -63,7 +65,7 @@ suite('SettingsUIToolbarAndDrawer', function() {
     // Drawer is closed, but menu is still stamped so
     // its contents remain visible as the drawer slides
     // out.
-    assertTrue(!!ui.$$('cr-drawer settings-menu'));
+    assertTrue(!!ui.shadowRoot.querySelector('cr-drawer settings-menu'));
   });
 
   test('app drawer closes when exiting narrow mode', async () => {
@@ -93,13 +95,14 @@ suite('SettingsUIAdvanced', function() {
   });
 
   test('advanced UIs stay in sync', function() {
-    const main = ui.$$('settings-main');
+    const main = ui.shadowRoot.querySelector('settings-main');
     const floatingMenu =
-        /** @type {!SettingsMenuElement} */ (ui.$$('#left settings-menu'));
+        /** @type {!SettingsMenuElement} */ (
+            ui.shadowRoot.querySelector('#left settings-menu'));
     assertTrue(!!main);
     assertTrue(!!floatingMenu);
 
-    assertFalse(!!ui.$$('cr-drawer settings-menu'));
+    assertFalse(!!ui.shadowRoot.querySelector('cr-drawer settings-menu'));
     assertFalse(ui.getAdvancedOpenedInMainForTest());
     assertFalse(ui.getAdvancedOpenedInMenuForTest());
     assertFalse(floatingMenu.advancedOpened);
@@ -108,22 +111,22 @@ suite('SettingsUIAdvanced', function() {
     main.advancedToggleExpanded = true;
     flush();
 
-    assertFalse(!!ui.$$('cr-drawer settings-menu'));
+    assertFalse(!!ui.shadowRoot.querySelector('cr-drawer settings-menu'));
     assertTrue(ui.getAdvancedOpenedInMainForTest());
     assertTrue(ui.getAdvancedOpenedInMenuForTest());
     assertTrue(floatingMenu.advancedOpened);
     assertTrue(main.advancedToggleExpanded);
 
-    ui.$$('#drawerTemplate').if = true;
+    ui.shadowRoot.querySelector('#drawerTemplate').if = true;
     flush();
 
-    const drawerMenu = ui.$$('cr-drawer settings-menu');
+    const drawerMenu = ui.shadowRoot.querySelector('cr-drawer settings-menu');
     assertTrue(!!drawerMenu);
     assertTrue(floatingMenu.advancedOpened);
     assertTrue(drawerMenu.advancedOpened);
 
     // Collapse 'Advanced' in the menu
-    drawerMenu.$$('#advancedButton').click();
+    drawerMenu.shadowRoot.querySelector('#advancedButton').click();
     flush();
 
     // Collapsing it in the menu should not collapse it in the main area
@@ -134,7 +137,7 @@ suite('SettingsUIAdvanced', function() {
     assertTrue(ui.getAdvancedOpenedInMainForTest());
 
     // Expand both 'Advanced's again
-    drawerMenu.$$('#advancedButton').click();
+    drawerMenu.shadowRoot.querySelector('#advancedButton').click();
 
     // Collapse 'Advanced' in the main area
     main.advancedToggleExpanded = false;
@@ -165,7 +168,8 @@ suite('SettingsUISearch', function() {
     document.body.appendChild(ui);
     return CrSettingsPrefs.initialized.then(() => {
       flush();
-      toolbar = /** @type {!CrToolbarElement} */ (ui.$$('cr-toolbar'));
+      toolbar = /** @type {!CrToolbarElement} */ (
+          ui.shadowRoot.querySelector('cr-toolbar'));
       searchField =
           /** @type {!CrToolbarSearchFieldElement} */ (
               toolbar.getSearchField());
@@ -221,8 +225,9 @@ suite('SettingsUISearch', function() {
   test('MaintainsFocusOnMenus', async () => {
     // Start in non-narrow mode with focus in the left menu.
     toolbar.narrow = false;
-    ui.$$('#leftMenu').focusFirstItem();
-    assertEquals(ui.$$('#leftMenu'), ui.shadowRoot.activeElement);
+    ui.shadowRoot.querySelector('#leftMenu').focusFirstItem();
+    assertEquals(
+        ui.shadowRoot.querySelector('#leftMenu'), ui.shadowRoot.activeElement);
 
     // Switch to narrow mode and test that focus moves to menu button.
     toolbar.narrow = true;
@@ -233,6 +238,7 @@ suite('SettingsUISearch', function() {
     // Switch back to non-narrow mode and test that focus moves to left menu.
     toolbar.narrow = false;
     flush();
-    assertEquals(ui.$$('#leftMenu'), ui.shadowRoot.activeElement);
+    assertEquals(
+        ui.shadowRoot.querySelector('#leftMenu'), ui.shadowRoot.activeElement);
   });
 });
