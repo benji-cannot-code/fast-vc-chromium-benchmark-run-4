1ff1b868120febba543fbd3ea1ae39f882e729ae
siashah <siashah@chromium.org>
2021-06-29 19:40:28 +0000

    [VirtualCards] Add support for displaying formatted creditcard number
    Currently, the displayText in the UserInfoField is used to fill the form
    as well. However, for virtual cards, we'd like to show the displayText
    to be formatted and have spaces like "4111 1111 1111 1111" and when
    the UserInfoField is tapped, we'd like to fill "4111111111111111" in the
    form.
    
    Bug: 1196021
    Change-Id: I59532514d95d5e0f701ff9f002b77575988e789a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2977512
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897056}

--- chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
+++ chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
@@ -202,6 +202,7 @@ TEST_F(CreditCardAccessoryControllerTest, RefreshSuggestions) {
       CreditCardAccessorySheetDataBuilder()
           .AddUserInfo(kVisaCard)
           .AppendField(card.ObfuscatedLastFourDigits(),
+                       /*text_to_fill=*/std::u16string(),
                        card.ObfuscatedLastFourDigits(), card.guid(),
                        /*is_obfuscated=*/false,
                        /*selectable=*/true)
@@ -231,6 +232,7 @@ TEST_F(CreditCardAccessoryControllerTest, PreventsFillingInsecureContexts) {
                     IDS_AUTOFILL_WARNING_INSECURE_CONNECTION))
                 .AddUserInfo(kVisaCard)
                 .AppendField(card.ObfuscatedLastFourDigits(),
+                             /*text_to_fill=*/std::u16string(),
                              card.ObfuscatedLastFourDigits(), card.guid(),
                              /*is_obfuscated=*/false,
                              /*selectable=*/false)
@@ -267,6 +269,7 @@ TEST_F(CreditCardAccessoryControllerTest, ServerCardUnmask) {
   controller()->RefreshSuggestions();
 
   UserInfo::Field field(card.ObfuscatedLastFourDigits(),
+                        /*text_to_fill=*/std::u16string(),
                         card.ObfuscatedLastFourDigits(), card.guid(),
                         /*is_obfuscated=*/false,
                         /*selectable=*/true);
@@ -356,6 +359,7 @@ TEST_F(CreditCardAccessoryControllerTest, UnmaskedCacheCardsReorderedToTheTop) {
           .AppendSimpleField(cvc)
           .AddUserInfo(kMasterCard)
           .AppendField(masked_card.ObfuscatedLastFourDigits(),
+                       /*text_to_fill=*/std::u16string(),
                        masked_card.ObfuscatedLastFourDigits(),
                        masked_card.guid(),
                        /*is_obfuscated=*/false,
@@ -393,6 +397,7 @@ TEST_F(CreditCardAccessoryControllerTestWithoutSupportingUnmaskedCards,
       CreditCardAccessorySheetDataBuilder()
           .AddUserInfo(kVisaCard)
           .AppendField(card.ObfuscatedLastFourDigits(),
+                       /*text_to_fill=*/std::u16string(),
                        card.ObfuscatedLastFourDigits(), card.guid(),
                        /*is_obfuscated=*/false,
                        /*selectable=*/true)
@@ -438,6 +443,7 @@ TEST_F(CreditCardAccessoryControllerTestWithoutSupportingUnmaskedCards,
           .AppendSimpleField(cvc)
           .AddUserInfo(kVisaCard)
           .AppendField(unmasked_card.ObfuscatedLastFourDigits(),
+                       /*text_to_fill=*/std::u16string(),
                        unmasked_card.ObfuscatedLastFourDigits(),
                        unmasked_card.guid(),
                        /*is_obfuscated=*/false,
