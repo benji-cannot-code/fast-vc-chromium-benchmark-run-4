3a3873256c96a0b1227f697aa4d91e951534a9bf
Xiaoling Bao <xiaolingbao@chromium.org>
2021-06-29 18:46:56 +0000

    Load all updater's group policy settings into memory.
    Caching policy values in memory because:
    1) To be consistent with other policy providers.
    2) Only do EnterCriticalPolicySection() during initialization and
    skip it for all subsequent policy queries.
    EnterCriticalPolicySection() could be expensive in certain situations.
    
    Bug: 221348
    Change-Id: I519ded1805fe103a9dbcbf621d138b53c41d958e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2994799
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897026}

--- chrome/updater/policy/win/group_policy_manager.h
+++ chrome/updater/policy/win/group_policy_manager.h
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_UPDATER_POLICY_WIN_GROUP_POLICY_MANAGER_H_
 #define CHROME_UPDATER_POLICY_WIN_GROUP_POLICY_MANAGER_H_
 
-#include <memory>
 #include <string>
 
-#include "base/win/registry.h"
+#include "base/values.h"
 #include "chrome/updater/policy/manager.h"
 
 namespace updater {
@@ -26,9 +25,6 @@ class GroupPolicyManager : public PolicyManagerInterface {
   std::string source() const override;
 
   bool IsManaged() const override;
-
-  // TODO(crbug.com/1221348): preload all policy values in memory to avoid
-  // calling EnterCriticalPolicySection.
 
   bool GetLastCheckPeriodMinutes(int* minutes) const override;
   bool GetUpdatesSuppressedTimes(
@@ -54,10 +50,11 @@ class GroupPolicyManager : public PolicyManagerInterface {
   bool GetProxyServer(std::string* proxy_server) const override;
 
  private:
-  bool ReadValue(const wchar_t* name, std::string* value) const;
-  bool ReadValueDW(const wchar_t* name, int* value) const;
+  void LoadAllPolicies();
+  bool GetIntPolicy(const std::string& key, int* value) const;
+  bool GetStringPolicy(const std::string& key, std::string* value) const;
 
-  base::win::RegKey key_;
+  base::Value policies_;
 };
 
 }  // namespace updater
