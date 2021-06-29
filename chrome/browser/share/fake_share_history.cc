7aa0637150486ef27d6db00386ba8cbd39644a6d
Elly Fong-Jones <ellyjones@chromium.org>
2021-06-29 16:33:07 +0000

    sharing: implement the rest of ShareRanking
    This change implements the rest of ShareRanking - specifically,
    ShareRanking::Rank, which will connect the UI to
    ShareRanking::ComputeRank and the rest of the ranking system. The
    subsequent CL in this series will add a JNI wrapper and invoke it from
    the UI when the experiment is enabled.
    
    ShareRanking::Rank involves three async fetches from two different
    LevelDB instances, so this change introduces a small implicit state
    machine into ShareRanking, with the state threaded through using a new
    type ShareRanking::PendingRankCall.
    
    This change also introduces one test for ShareRanking::Rank, and the
    skeletons of a few others; implementing those is pending discussions
    with the leveldb_proto OWNERS about how to avoid the nasty hack in
    ShareRankingTest.InitialStateNoHistory.
    
    Change-Id: If1bed0ab2fb1d63a0beb02dcafcbb9500632f67d
    Bug: 1217186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982966
    Reviewed-by: Sophey <sophey@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896963}

---
+++ chrome/browser/share/fake_share_history.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/share/fake_share_history.h"
+
+#include "base/bind.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+
+namespace sharing {
+
+FakeShareHistory::FakeShareHistory() = default;
+FakeShareHistory::~FakeShareHistory() = default;
+
+void FakeShareHistory::AddShareEntry(const std::string& component_name) {
+  NOTIMPLEMENTED();
+}
+
+void FakeShareHistory::GetFlatShareHistory(GetFlatHistoryCallback callback,
+                                           int window) {
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), history_));
+}
+
+}  // namespace sharing
