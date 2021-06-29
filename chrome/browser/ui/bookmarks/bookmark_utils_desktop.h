c68185dac9b8eb7ac9f213b33f48aa8da6697e58
Emily Shack <emshack@chromium.org>
2021-06-29 01:31:58 +0000

    Add folders for tab groups on Bookmark All Tabs
    When the user has one or more tab groups open and selects "Bookmark All
    Tabs", instead of getting a folder with a flat list of tabs, they will
    now see nested folders for any tab groups, with naming aligned with the
    tab group title. The order of urls and nested folders will match the tab
    strip order. If multiple tab groups share the same title, their tabs
    will get combined into a single folder at the index of the first group.
    
    Bug: 1181583
    Change-Id: Ie2900f26a2dd231d62775a29b250d691e0f5480e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957470
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896744}

--- chrome/browser/ui/bookmarks/bookmark_utils_desktop.h
+++ chrome/browser/ui/bookmarks/bookmark_utils_desktop.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_forward.h"
+#include "chrome/browser/ui/bookmarks/bookmark_editor.h"
+#include "chrome/browser/ui/tabs/tab_group.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/gfx/native_widget_types.h"
 
@@ -24,6 +26,9 @@ class PageNavigator;
 }
 
 namespace chrome {
+
+using TabGroupData =
+    std::pair<absl::optional<tab_groups::TabGroupId>, std::u16string>;
 
 // Number of bookmarks we'll open before prompting the user to see if they
 // really want to open all.
@@ -87,6 +92,13 @@ bool HasBookmarkURLs(
 bool HasBookmarkURLsAllowedInIncognitoMode(
     const std::vector<const bookmarks::BookmarkNode*>& selection,
     content::BrowserContext* browser_context);
+
+// Populates |folder_data| with all tab items and sub-folders for any open tab
+// groups.
+void GetURLsAndFoldersForTabEntries(
+    std::vector<BookmarkEditor::EditDetails::BookmarkData>* folder_data,
+    std::vector<std::pair<GURL, std::u16string>> tab_entries,
+    base::flat_map<int, TabGroupData> groups_by_index);
 
 }  // namespace chrome
 
