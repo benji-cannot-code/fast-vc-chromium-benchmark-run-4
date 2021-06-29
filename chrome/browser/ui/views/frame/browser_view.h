71af19ae2420d375b545e3bac9047c33d56389f9
Jan Krcal <jkrcal@chromium.org>
2021-06-29 12:02:10 +0000

    [Profile menu] Focus other profiles only for Ctrl-Shift-M
    This CL restricts the custom focus behavior of the profile menu. After
    this change, the custom behavior (selecting other profiles) occurs only
    when triggering the menu using the Ctrl+Shift+M shortcut. When entering
    the menu from the toolbar using the keyboard, the default focus behavior
    is applied.
    
    This CL also removes a browser test for the removed behavior. The
    remaining custom focus behavior is covered by an interactive ui test
    introduced in CL 2988763.
    
    Bug: 1188106
    Change-Id: Id6b2155965335106712d101d1d0bf267417a3969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2871474
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896866}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -497,7 +497,7 @@ class BrowserView : public BrowserWindow,
   void ShowAvatarBubbleFromAvatarButton(
       AvatarBubbleMode mode,
       signin_metrics::AccessPoint access_point,
-      bool is_source_keyboard) override;
+      bool is_source_accelerator) override;
   void MaybeShowProfileSwitchIPH() override;
   void ShowHatsDialog(
       const std::string& site_id,
