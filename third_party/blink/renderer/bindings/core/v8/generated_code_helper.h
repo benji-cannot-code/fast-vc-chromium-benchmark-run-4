dea00e25a8d187a92ea80e32e97d2ecdc8621ccf
Yuki Shiino <yukishiino@chromium.org>
2021-06-29 14:25:10 +0000

    bind-gen: Add branch predictions
    Through the discussion at https://crrev.com/c/2976632, add more
    branch predictions, which shouldn't be harmful and potentially
    beneficial.
    
    Bug: 839389
    Change-Id: I50e7ae5c9dd614e808a568629ac8932ad251d3bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2994582
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896902}

--- third_party/blink/renderer/bindings/core/v8/generated_code_helper.h
+++ third_party/blink/renderer/bindings/core/v8/generated_code_helper.h
@@ -48,7 +48,7 @@ class CORE_EXPORT ExceptionToRejectPromiseScope final {
                                 ExceptionState& exception_state)
       : info_(info), exception_state_(exception_state) {}
   ~ExceptionToRejectPromiseScope() {
-    if (!exception_state_.HadException())
+    if (LIKELY(!exception_state_.HadException()))
       return;
 
     // As exceptions must always be created in the current realm, reject
@@ -147,7 +147,7 @@ typename IDLSequence<T>::ImplType VariadicArgumentsToNativeValues(
   for (int i = start_index; i < length; ++i) {
     result.UncheckedAppend(NativeValueTraits<T>::ArgumentValue(
         isolate, i, info[i], exception_state, extra_args...));
-    if (exception_state.HadException())
+    if (UNLIKELY(exception_state.HadException()))
       return VectorType();
   }
   return std::move(result);
@@ -239,7 +239,7 @@ bool GetDictionaryMemberFromV8Object(v8::Isolate* isolate,
 
   value = NativeValueTraits<IDLType>::NativeValue(isolate, v8_value,
                                                   exception_state);
-  if (exception_state.HadException()) {
+  if (UNLIKELY(exception_state.HadException())) {
     return false;
   }
   presence = true;
