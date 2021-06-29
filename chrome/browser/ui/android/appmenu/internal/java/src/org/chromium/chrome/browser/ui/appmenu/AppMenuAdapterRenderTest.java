e85dce51d473725890acf9fa3ac6378a90b685fb
Sinan Sahin <sinansahin@google.com>
2021-06-29 23:21:46 +0000

    Fix App Menu corner size
    This CL removes the menuCornerSize attribute and makes the App Menu
    corner radius 8dp since we don't need to make it dynamic.
    
    Change-Id: I55d06cc158db4857776403a0f815ebe254353ddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991171
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897153}

--- chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuAdapterRenderTest.java
+++ chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuAdapterRenderTest.java
@@ -68,7 +68,7 @@ public class AppMenuAdapterRenderTest extends DummyUiActivityTestCase {
 
     @Rule
     public ChromeRenderTestRule mRenderTestRule =
-            ChromeRenderTestRule.Builder.withPublicCorpus().build();
+            ChromeRenderTestRule.Builder.withPublicCorpus().setRevision(1).build();
 
     @Override
     public void setUpTest() throws Exception {
