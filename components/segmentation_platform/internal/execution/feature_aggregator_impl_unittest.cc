c7baf36f83662d611063878ad9a1e6aa21a18bb4
Tommy Nyquist <nyquist@chromium.org>
2021-06-29 00:41:05 +0000

    [segmentation_platform] Rewrite metadata protos.
    This CL rewrites the segmentation platform metadata protos in a
    backwards incompatible way. This is something that is usually not OK to
    do, but these protos have never been used in the wild, as the feature
    is off by default, and there is currently no experiment running with
    it turned on or with any of the required configuration.
    
    The Feature sub-messages have been removed, since they were in practice
    equal across all the current types, except for one optional missing name
    field for user actions and the additional enum_ids for enum histograms.
    This simplified a lot of code, and made it possible to be more
    consistent in using the SignalType proto enum across the code base.
    
    In addition, the current |length| field for a metadata feature has been
    split into two parts: |bucket_count| and |tensor_length|, since they
    are conceptually different, and it was unnecessary complex to have
    them stored in the same field. The |tensor_length| field is defined
    based on the |aggregation| and |bucket_count| field in a deterministic
    way.
    
    As part of this, we also now skip processing anything that has a
    |bucket_length| of 0 when it comes to model execution. A test was
    added for this.
    
    It also updates the Aggregation type to be more copmlete across both
    count and sum, so they provide the same functionality.
    
    All relevant code that depends on the protos have been updated, and
    tests have been added or updated.
    
    Bug: 1223467, 1210648
    Change-Id: I86ac5766ade44a2bce03faf5ccef99a2067cf603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991712
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896725}

--- components/segmentation_platform/internal/execution/feature_aggregator_impl_unittest.cc
+++ components/segmentation_platform/internal/execution/feature_aggregator_impl_unittest.cc
@@ -42,7 +42,7 @@ TEST_F(FeatureAggregatorImplTest, SumCountAggregation) {
   };
 
   std::vector<float> res = feature_aggregator_->Process(
-      proto::SignalType::HISTOGRAM_VALUE, proto::Aggregation::SUM_COUNT, 1u,
+      proto::SignalType::HISTOGRAM_VALUE, proto::Aggregation::COUNT, 1u,
       clock_.Now(), base::TimeDelta::FromSeconds(10), samples);
   // SUM_COUNT always produces a single value.
   EXPECT_EQ(1u, res.size());
@@ -58,7 +58,7 @@ TEST_F(FeatureAggregatorImplTest, SumValuesAggregationHistogram) {
   };
 
   std::vector<float> res = feature_aggregator_->Process(
-      proto::SignalType::HISTOGRAM_VALUE, proto::Aggregation::SUM_VALUES, 1u,
+      proto::SignalType::HISTOGRAM_VALUE, proto::Aggregation::SUM, 1u,
       clock_.Now(), base::TimeDelta::FromSeconds(10), samples);
   // SUM_VALUES always produces a single value.
   EXPECT_EQ(1u, res.size());
@@ -74,8 +74,8 @@ TEST_F(FeatureAggregatorImplTest, SumValuesAggregationUserAction) {
   };
 
   std::vector<float> res = feature_aggregator_->Process(
-      proto::SignalType::USER_ACTION, proto::Aggregation::SUM_VALUES, 1u,
-      clock_.Now(), base::TimeDelta::FromSeconds(10), samples);
+      proto::SignalType::USER_ACTION, proto::Aggregation::SUM, 1u, clock_.Now(),
+      base::TimeDelta::FromSeconds(10), samples);
   // SUM_VALUES always produces a single value.
   EXPECT_EQ(1u, res.size());
   // We should have summed up to 1+1+1=3.
@@ -90,8 +90,8 @@ TEST_F(FeatureAggregatorImplTest, SumValuesAggregationUserActionIgnoresValue) {
   };
 
   std::vector<float> res = feature_aggregator_->Process(
-      proto::SignalType::USER_ACTION, proto::Aggregation::SUM_VALUES, 1u,
-      clock_.Now(), base::TimeDelta::FromSeconds(10), samples);
+      proto::SignalType::USER_ACTION, proto::Aggregation::SUM, 1u, clock_.Now(),
+      base::TimeDelta::FromSeconds(10), samples);
   // SUM_VALUES always produces a single value.
   EXPECT_EQ(1u, res.size());
   // We should have summed up to 1+1+1=3.
