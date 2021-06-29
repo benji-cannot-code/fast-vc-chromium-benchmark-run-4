66c5ea6f41c91a8fb4b058978b2d9f1fc1ff5668
Chad Duffin <chadduffin@chromium.org>
2021-06-29 23:34:38 +0000

    [CrOS Cellular] Reorganize cellular toggle subtext test.
    This change reorganizes the cellular toggle subtext test into a distinct
    test suite since it does not need the heavier internet details page
    setup. This suite will be extended to include future cellular roaming
    toggle tests.
    
    Bug: 1220691
    Test: Ran affected browser_tests.
    Change-Id: I424d91cc997496e3cc202ab5756a07abaa7ca668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988886
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897162}

---
+++ chrome/test/data/webui/settings/chromeos/cellular_roaming_toggle_button_test.js
@@ -0,0 +1,65 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// clang-format off
+// #import 'chrome://os-settings/chromeos/os_settings.js';
+
+// #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// clang-format on
+
+suite('CellularRoamingToggleButton', function() {
+  /** @type {CellularRoamingToggleButton|undefined} */
+  let cellularRoamingToggleButton;
+
+  setup(function() {
+    cellularRoamingToggleButton =
+        document.createElement('cellular-roaming-toggle-button');
+    document.body.appendChild(cellularRoamingToggleButton);
+    Polymer.dom.flush();
+  });
+
+  /**
+   * @param {boolean} allowRoaming
+   * @param {string} roamingState
+   */
+  function setManagedProperties(allowRoaming, roamingState) {
+    cellularRoamingToggleButton.managedProperties = {
+      typeProperties: {
+        cellular: {
+          allowRoaming: allowRoaming,
+          roamingState: roamingState,
+        },
+      },
+    };
+    Polymer.dom.flush();
+  }
+
+  test('Cellular roaming subtext', function() {
+    const cellularRoamingToggle =
+        cellularRoamingToggleButton.getCellularRoamingToggle();
+
+    setManagedProperties(
+        /* allowRoaming= */ false, /* roamingState= */ null);
+
+    assertEquals(
+        cellularRoamingToggleButton.i18n('networkAllowDataRoamingDisabled'),
+        cellularRoamingToggle.subLabel);
+
+    setManagedProperties(
+        /* allowRoaming= */ true, /* roamingState= */ 'Home');
+
+    assertEquals(
+        cellularRoamingToggleButton.i18n('networkAllowDataRoamingEnabledHome'),
+        cellularRoamingToggle.subLabel);
+
+    setManagedProperties(
+        /* allowRoaming= */ true, /* roamingState= */ 'Roaming');
+
+    assertEquals(
+        cellularRoamingToggleButton.i18n(
+            'networkAllowDataRoamingEnabledRoaming'),
+        cellularRoamingToggle.subLabel);
+  });
+});
