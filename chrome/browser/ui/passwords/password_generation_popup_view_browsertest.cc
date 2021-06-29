b573c1e4cd0eceeb51036abefa7fae086ffb8565
Maxim Kolosovskiy <kolos@chromium.org>
2021-06-29 16:44:09 +0000

    Disabled flaky test PasswordGenerationPopupViewPrerenderingTest.PasswordGenerationPopupControllerInPrerendering
    Bug: 1223793
    Change-Id: I5ce41a62348d8a13e4da46683f490dcff65b44b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988656
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Commit-Queue: W. James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: W. James MacLean <wjmaclean@chromium.org>
    Owners-Override: Maxim Kolosovskiy  <kolos@chromium.org>
    Auto-Submit: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896967}

--- chrome/browser/ui/passwords/password_generation_popup_view_browsertest.cc
+++ chrome/browser/ui/passwords/password_generation_popup_view_browsertest.cc
@@ -150,7 +150,7 @@ class PasswordGenerationPopupViewPrerenderingTest
 #endif
 
 IN_PROC_BROWSER_TEST_F(PasswordGenerationPopupViewPrerenderingTest,
-                       PasswordGenerationPopupControllerInPrerendering) {
+                       MAYBE_PasswordGenerationPopupControllerInPrerendering) {
   GURL url =
       embedded_test_server()->GetURL("/password/nonplaceholder_username.html");
   ui_test_utils::NavigateToURL(browser(), url);
