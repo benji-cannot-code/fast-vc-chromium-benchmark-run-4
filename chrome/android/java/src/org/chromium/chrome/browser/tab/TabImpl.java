ecb88145bd99e2dd45982d983d64df422d490bb0
David Maunder <davidjm@chromium.org>
2021-06-29 00:11:33 +0000

    Add integration with OptimizationGuide navigation API for price drops
    Bug: 1213115
    Change-Id: I260cacc3fd50796049d15703b68aa812a97e477e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2959102
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896714}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
@@ -797,6 +797,11 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
         mIsTabSaveEnabledSupplier.set(isTabSaveEnabled);
     }
 
+    @VisibleForTesting
+    public ObservableSupplierImpl<Boolean> getIsTabSaveEnabledSupplierForTesting() {
+        return mIsTabSaveEnabledSupplier;
+    }
+
     // TabObscuringHandler.Observer
 
     @Override
