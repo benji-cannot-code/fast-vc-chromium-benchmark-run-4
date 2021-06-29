821f4e4d3b24c95da0bb729f49125f2cbd2ea001
Lei Zhang <thestig@chromium.org>
2021-06-29 23:38:58 +0000

    Update some includes in Blink generated code and headers.
    Swap base/stl_util.h to base/cxx17_backports.h.
    
    Bug: 1210983
    Change-Id: I490b42dcf69eb5cbaf7ae106689975989523b65d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991753
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897165}

--- third_party/blink/renderer/platform/bindings/v8_dom_wrapper.h
+++ third_party/blink/renderer/platform/bindings/v8_dom_wrapper.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_V8_DOM_WRAPPER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_V8_DOM_WRAPPER_H_
 
-#include "base/stl_util.h"
+#include "base/cxx17_backports.h"
 #include "third_party/blink/renderer/platform/bindings/binding_security_for_platform.h"
 #include "third_party/blink/renderer/platform/bindings/custom_wrappable.h"
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
