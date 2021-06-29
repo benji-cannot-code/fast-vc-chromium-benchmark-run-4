9635d8471f3614fdfc136ad862e09f172ba7b834
Hans Wennborg <hans@chromium.org>
2021-06-29 08:38:26 +0000

    Don't include stl_util.h in integer_to_string_conversion.h
    stl_util.h is a big header, and integer_to_string_conversion.h is
    included a lot (ca 7k instances or 20% of all translation units, for the
    chrome target).
    
    Include graph analysis shows [1] that this edge was responsible for
    adding ca 1.6 GB to the source size processed by the compiler for the
    chrome target on linux.
    
    [1] https://commondatastorage.googleapis.com/chromium-browser-clang/chrome_includes_2021-06-28_125122.html#view=edges&includer=%5Ethird_party%2Fblink%2Frenderer%2Fplatform%2Fwtf%2Ftext%2Finteger_to_string_conversion%5C.h%24
    
    Bug: 242216, 1194078
    Change-Id: Ia51e2c704a7c4e7194f37114b316f3ebcffde147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992303
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896820}

--- third_party/blink/renderer/platform/audio/audio_destination.h
+++ third_party/blink/renderer/platform/audio/audio_destination.h
@@ -44,6 +44,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 
+namespace absl {
+template <typename>
+class optional;
+}
+
 namespace blink {
 
 class PushPullFIFO;
--- third_party/blink/renderer/platform/wtf/text/integer_to_string_conversion.h
+++ third_party/blink/renderer/platform/wtf/text/integer_to_string_conversion.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/numerics/safe_conversions.h"
-#include "base/stl_util.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/unicode.h"
@@ -46,7 +45,7 @@ class IntegerToStringConverter {
                 "IntegerType must be a type of integer.");
 
   explicit IntegerToStringConverter(IntegerType input) {
-    LChar* end = buffer_ + base::size(buffer_);
+    LChar* end = buffer_ + kBufferSize;
     begin_ = end;
 
     // We need to switch to the unsigned type when negating the value since
