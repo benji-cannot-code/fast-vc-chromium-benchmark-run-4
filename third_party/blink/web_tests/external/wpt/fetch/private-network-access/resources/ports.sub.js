dc5c1ac89afd9249520091aaf40bee9ab627ecbe
Titouan Rigoudy <titouan@chromium.org>
2021-06-29 17:21:42 +0000

    [Private Network Access] Add more web platform tests.
    This implements WPT RFC 72 support for the Blink web test runner. This
    draws on similar work done for wptrunner in the following WPT PR:
    https://github.com/web-platform-tests/wpt/pull/28870
    
    That PR taught wptserve how to run servers for http(s)-{private,public}
    ports and configure Chrome to treat those ports as belonging to the
    `private` and `public` address spaces. This CL teaches the Blink web
    test runner to do the same, then adds some tests verifying that.
    
    Bug: chromium:1138904
    Change-Id: I7c118b64d62ac6932988ad40e370b0d7c72b56b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987794
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#896984}

---
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/ports.sub.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// For convenience in other tests, defines a dictionary of HTTP(S) ports.
+const kPorts = {
+  httpLocal:    {{ports[http][0]}},
+  httpPrivate:  {{ports[http-private][0]}},
+  httpPublic:   {{ports[http-public][0]}},
+  httpsLocal:   {{ports[https][0]}},
+  httpsPrivate: {{ports[https-private][0]}},
+  httpsPublic:  {{ports[https-public][0]}},
+};
