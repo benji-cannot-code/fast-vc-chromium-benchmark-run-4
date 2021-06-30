2e60a5b36ebcf53ba9255764f95e61de8dfc8357
Yuheng Huang <yuhengh@chromium.org>
2021-06-30 21:52:50 +0000

    Refactor TabIcon::ThemeImage to favicon_util
    This CL makes it possible to reuse favicon theming in WebUI tab strip.
    
    Bug: 1210233
    Change-Id: Ic5cb888c686b1c548622e7f645fe5bd064acbfd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2994924
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897556}

--- chrome/browser/favicon/favicon_utils.cc
+++ chrome/browser/favicon/favicon_utils.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/color_analysis.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/font_list.h"
 #include "ui/gfx/image/image.h"
@@ -200,6 +201,32 @@ void SaveFaviconEvenIfInIncognito(content::WebContents* contents) {
   favicon_service->SetFavicons({page_url}, favicon_status.url,
                                favicon_base::IconType::kFavicon,
                                favicon_status.image);
+}
+
+gfx::ImageSkia ThemeFavicon(const gfx::ImageSkia& source,
+                            SkColor alternate_color,
+                            SkColor active_tab_background,
+                            SkColor inactive_tab_background) {
+  // Choose between leaving the image as-is or masking with |alternate_color|.
+  const SkColor original_color =
+      color_utils::CalculateKMeanColorOfBitmap(*source.bitmap());
+
+  // Compute the minimum contrast of each color against foreground and
+  // background tabs (for active windows).
+  const float original_contrast = std::min(
+      color_utils::GetContrastRatio(original_color, active_tab_background),
+      color_utils::GetContrastRatio(original_color, inactive_tab_background));
+  const float alternate_contrast = std::min(
+      color_utils::GetContrastRatio(alternate_color, active_tab_background),
+      color_utils::GetContrastRatio(alternate_color, inactive_tab_background));
+
+  // Recolor the image if the original has low minimum contrast and recoloring
+  // will improve it.
+  return ((original_contrast < color_utils::kMinimumVisibleContrastRatio) &&
+          (alternate_contrast > original_contrast))
+             ? gfx::ImageSkiaOperations::CreateColorMask(source,
+                                                         alternate_color)
+             : source;
 }
 
 }  // namespace favicon
