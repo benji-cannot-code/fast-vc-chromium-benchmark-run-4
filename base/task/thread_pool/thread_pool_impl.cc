a7b407e780fa56b1b7b32f4740f4f811715c3a87
Francois Doray <fdoray@chromium.org>
2021-06-30 19:09:22 +0000

    [threadpool] Add debug info for crbug.com/1224432
    Bug: 1224432
    Change-Id: I02944eb267bb199c469a5e30863abf043da4ae6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2995071
    Auto-Submit: François Doray <fdoray@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897487}

--- base/task/thread_pool/thread_pool_impl.cc
+++ base/task/thread_pool/thread_pool_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/debug/alias.h"
 #include "base/feature_list.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/metrics/field_trial_params.h"
@@ -437,6 +438,15 @@ bool ThreadPoolImpl::PostTaskWithSequence(Task task,
   // for details.
   CHECK(task.task);
   DCHECK(sequence);
+
+#if defined(OS_WIN)
+  // Force reading |task.posted_from.file_name()| to produce a useful crash
+  // report if the address is invalid. A crash report generated later when the
+  // task is executed would not contain the PostTask stack.
+  //
+  // TODO(crbug.com/1224432): Remove after resolving the crash.
+  DEBUG_ALIAS_FOR_CSTR(task_posted_from, task.posted_from.file_name(), 32);
+#endif
 
   if (!task_tracker_->WillPostTask(&task, sequence->shutdown_behavior()))
     return false;
