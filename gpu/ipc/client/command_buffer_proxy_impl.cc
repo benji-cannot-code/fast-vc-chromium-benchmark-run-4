5ba23fdb16a32e0ca8ae709271dbf44b51f06b83
Alexander Alekseev <alemate@chromium.org>
2021-06-30 08:29:09 +0000

    [gpu ipc] Add GPU.EnsureWorkVisibleDuration2 histogram.
    GPU.EnsureWorkVisibleDuration2 is a sister histogram to
    GPU.EnsureWorkVisibleDuration. The original one is our target goal, but
    does not have enough granularity in the current blocking call duration
    region on Chrome OS (10ms - 1.5s interval is not well covered by the
    original histogram).
    
    We will use this one until we improve the system enough to get into the more
    granular region.
    
    See go/chrome-io-thread-unclogging for details.
    
    Bug: 1153709
    Change-Id: Ie79fcb9ade6f884666d70ec4d31d354469475757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2974489
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897280}

--- gpu/ipc/client/command_buffer_proxy_impl.cc
+++ gpu/ipc/client/command_buffer_proxy_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/timer/elapsed_timer.h"
@@ -480,6 +481,11 @@ void CommandBufferProxyImpl::EnsureWorkVisible() {
   channel_->VerifyFlush(UINT32_MAX);
   GetUMAHistogramEnsureWorkVisibleDuration()->Add(
       elapsed_timer.Elapsed().InMicroseconds());
+
+  UMA_HISTOGRAM_CUSTOM_TIMES("GPU.EnsureWorkVisibleDurationLowRes",
+                             elapsed_timer.Elapsed(),
+                             base::TimeDelta::FromMilliseconds(1),
+                             base::TimeDelta::FromSeconds(5), 100);
 }
 
 gpu::CommandBufferNamespace CommandBufferProxyImpl::GetNamespaceID() const {
