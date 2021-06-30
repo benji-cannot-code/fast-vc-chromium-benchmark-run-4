d13d47217f16493d49693a5d8fa65dbb71f9eecb
Adrienne Walker <enne@chromium.org>
2021-06-30 20:17:55 +0000

    Skip requestFullscreen checks on current fullscreen element
    This will allow an already fullscreen element to switch displays
    (with window-placement permission) without requiring an additional
    user activation.  This is similar to how moveTo behaves.
    
    Filed here: https://github.com/whatwg/fullscreen/issues/193
    
    Bug: 1218483
    Change-Id: Ia793eccb7e3f25492690c703025f7e975b597ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2970683
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897516}

--- third_party/blink/renderer/core/fullscreen/fullscreen.cc
+++ third_party/blink/renderer/core/fullscreen/fullscreen.cc
@@ -278,7 +278,7 @@ bool AllowedToUseFullscreen(const Document& document,
       mojom::blink::PermissionsPolicyFeature::kFullscreen, report_on_failure);
 }
 
-bool AllowedToRequestFullscreen(Document& document) {
+bool AllowedToRequestFullscreen(Document& document, Element& element) {
   //  WebXR DOM Overlay integration, cf.
   //  https://immersive-web.github.io/dom-overlays/
   //
@@ -308,6 +308,12 @@ bool AllowedToRequestFullscreen(Document& document) {
              << ": rejecting change of fullscreen element for XR DOM overlay";
     return false;
   }
+
+  // If the element is already fullscreen, then it is allowed to repeat a
+  // request to fullscreen (possibly on another display) without requiring
+  // user activation.
+  if (element == Fullscreen::FullscreenElementFrom(document))
+    return true;
 
   // An algorithm is allowed to request fullscreen if one of the following is
   // true:
@@ -391,7 +397,7 @@ bool RequestFullscreenConditionsMet(Element& pending, Document& document) {
     return false;
 
   // This algorithm is allowed to request fullscreen.
-  if (!AllowedToRequestFullscreen(document))
+  if (!AllowedToRequestFullscreen(document, pending))
     return false;
 
   return true;
