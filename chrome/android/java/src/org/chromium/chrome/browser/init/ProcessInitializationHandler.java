979c61a79ac434dc0a9c8edad4df6dc8cd733e79
Clemens Arbesser <arbesser@google.com>
2021-06-30 07:56:30 +0000

    [Autofill Assistant] Clear first-time user flag on history clear
    See bug for details.
    
    Bug: b/189728709
    Change-Id: I63e22efd89dc4309f550d15b59cc33614cf5a34d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988912
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#897273}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -37,6 +37,7 @@ import org.chromium.chrome.browser.DefaultBrowserInfo;
 import org.chromium.chrome.browser.DeferredStartupHandler;
 import org.chromium.chrome.browser.DevToolsServer;
 import org.chromium.chrome.browser.app.video_tutorials.VideoTutorialShareHelper;
+import org.chromium.chrome.browser.autofill_assistant.AutofillAssistantHistoryDeletionObserver;
 import org.chromium.chrome.browser.bookmarkswidget.BookmarkWidgetProvider;
 import org.chromium.chrome.browser.contacts_picker.ChromePickerAdapter;
 import org.chromium.chrome.browser.content_capture.ContentCaptureHistoryDeletionObserver;
@@ -255,6 +256,8 @@ public class ProcessInitializationHandler {
 
         HistoryDeletionBridge.getInstance().addObserver(new ContentCaptureHistoryDeletionObserver(
                 () -> PlatformContentCaptureController.getInstance()));
+        HistoryDeletionBridge.getInstance().addObserver(
+                new AutofillAssistantHistoryDeletionObserver());
     }
 
     /**
