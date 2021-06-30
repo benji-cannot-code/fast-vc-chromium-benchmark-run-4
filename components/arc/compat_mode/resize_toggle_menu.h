d72d0b5e088e502953be44a63113594fa1be908d
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-06-30 09:18:18 +0000

    Cancel the ongoing delayed collapse when another button is clicked
    This CL changes to cancel the ongoing callback for the delayed collapse
    when another button is clicked.
    
    BUG=b:192207295
    TEST=ResizeToggleMenuTest.TestDelayedAutoClose
    
    Change-Id: Ib100dc6617319c617da6b1a2cfc2771a82957a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2994233
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897298}

--- components/arc/compat_mode/resize_toggle_menu.h
+++ components/arc/compat_mode/resize_toggle_menu.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/callback_forward.h"
+#include "base/cancelable_callback.h"
 #include "base/scoped_multi_source_observation.h"
 #include "components/arc/compat_mode/resize_util.h"
 #include "ui/views/controls/button/button.h"
@@ -92,6 +93,8 @@ class ResizeToggleMenu : public views::WidgetObserver {
 
   base::ScopedMultiSourceObservation<views::Widget, views::WidgetObserver>
       widget_observations_{this};
+
+  base::CancelableOnceClosure auto_close_closure_;
 
   // Store only for testing.
   views::Widget* bubble_widget_{nullptr};
