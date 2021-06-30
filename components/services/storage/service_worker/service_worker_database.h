f3c7499fe428a63977c7f168ade18e891cc9bf4c
Steven Bingler <bingler@chromium.org>
2021-06-30 21:18:32 +0000

    Check StorageKey when loading service worker registrations from DB
    Add StorageKey as an argument to ParseRegistrationData. This allows
    the function to do a corruption check against the `key` and is prep
    work for a later CL in which the `out` object will carry the `key`.
    
    Bug: 1199077
    Change-Id: Iec10be436e3209d5fade92f71143e13c0e577694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987278
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897542}

--- components/services/storage/service_worker/service_worker_database.h
+++ components/services/storage/service_worker/service_worker_database.h
@@ -304,6 +304,7 @@ class ServiceWorkerDatabase {
   // Parses |serialized| as a RegistrationData object and pushes it into |out|.
   ServiceWorkerDatabase::Status ParseRegistrationData(
       const std::string& serialized,
+      const blink::StorageKey& key,
       mojom::ServiceWorkerRegistrationDataPtr* out);
 
   // Populates |batch| with operations to write |registration|. It does not
