e4c2197d382ddf953b1cbf86db6a6f19c88c461b
rajendrant <rajendrant@chromium.org>
2021-06-30 20:19:18 +0000

    Model validation support
    Adds a CLI flag to validate model
    
    Bug: 1219668
    Change-Id: I500358ef437fdd97100b39ad3a0f7a21e0934511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988086
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897517}

--- chrome/browser/optimization_guide/optimization_guide_keyed_service.cc
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/leveldb_proto/public/proto_database_provider.h"
 #include "components/optimization_guide/core/command_line_top_host_provider.h"
 #include "components/optimization_guide/core/hints_processing_util.h"
+#include "components/optimization_guide/core/model_validator.h"
 #include "components/optimization_guide/core/optimization_guide_constants.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_navigation_data.h"
@@ -207,6 +208,9 @@ void OptimizationGuideKeyedService::Initialize() {
   // old paths. Remove this code in 04/2022 since it should be assumed that all
   // clients that had the previous path have had their previous stores deleted.
   DeleteOldStorePaths(profile_path);
+
+  // Perform model validation when enabled via command line.
+  optimization_guide::DoValidateModel(this);
 }
 
 OptimizationGuideHintsManager*
@@ -297,14 +301,14 @@ void OptimizationGuideKeyedService::AddObserverForOptimizationTargetModel(
 void OptimizationGuideKeyedService::RemoveObserverForOptimizationTargetModel(
     optimization_guide::proto::OptimizationTarget optimization_target,
     optimization_guide::OptimizationTargetModelObserver* observer) {
-    prediction_manager_->RemoveObserverForOptimizationTargetModel(
-        optimization_target, observer);
+  prediction_manager_->RemoveObserverForOptimizationTargetModel(
+      optimization_target, observer);
 }
 
 void OptimizationGuideKeyedService::RegisterOptimizationTypes(
     const std::vector<optimization_guide::proto::OptimizationType>&
         optimization_types) {
-    hints_manager_->RegisterOptimizationTypes(optimization_types);
+  hints_manager_->RegisterOptimizationTypes(optimization_types);
 }
 
 optimization_guide::OptimizationGuideDecision
