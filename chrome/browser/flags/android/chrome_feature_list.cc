9f21e6e0e92d9963be2e3f78490ea9c09fb21836
Jinsuk Kim <jinsukkim@chromium.org>
2021-06-30 00:04:32 +0000

    Android: A new feature flag for instance switcher
    Adds a feature flag (disabled by default) to enable instance switcher
    user interface.
    
    Bug: 1175953
    Change-Id: I1de6850ce540b6f1f6b00164661e6b9b5eb8d5ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992777
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897173}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -207,6 +207,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kImmersiveUiMode,
     &kIncognitoScreenshot,
     &kInlineUpdateFlow,
+    &kInstanceSwitcher,
     &kInstantStart,
     &kKitKatSupported,
     &kLensCameraAssistedSearch,
@@ -584,6 +585,9 @@ const base::Feature kSearchEnginePromoNewDevice{
 
 const base::Feature kNewWindowAppMenu{"NewWindowAppMenu",
                                       base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kInstanceSwitcher{"InstanceSwitcher",
+                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 // TODO(knollr): This is a temporary kill switch, it can be removed once we feel
 // okay about leaving it on.
