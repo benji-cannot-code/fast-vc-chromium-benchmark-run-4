1b0a74c44ddd2619f3d05fb2d8982fe8bceca12d
Yaron Friedman <yfriedman@chromium.org>
2021-06-30 14:24:08 +0000

    Revert "Add memory mapped file to CriticalPersistedTabData"
    This reverts commit f8bf72beebb8f67e04676de82d5d4eedfeafa190.
    
    Reason for revert: Speculative fix for test flakiness
    Bug: 1225259
    
    Original change's description:
    > Add memory mapped file to CriticalPersistedTabData
    >
    > A memory mapped file is used in the file based storage as a performance
    > optimization to avoid copying bytes to the heap.
    >
    > Bug: 1222928
    > Change-Id: I4539163c5c132b5630f27e0b41fc39479b59df20
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2981462
    > Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    > Commit-Queue: David Maunder <davidjm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#896530}
    
    Bug: 1222928
    Change-Id: I2b4e53f904aa5d9e57be43d42e20b88627c4deab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2996762
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897362}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorage.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorage.java
@@ -27,9 +27,7 @@ import org.chromium.base.task.SequencedTaskRunner;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -37,7 +35,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
 import java.util.LinkedList;
 import java.util.Locale;
 
@@ -376,16 +373,11 @@ public class FilePersistedTabDataStorage implements PersistedTabDataStorage {
         @Override
         public ByteBuffer executeSyncTask() {
             boolean success = false;
-            ByteBuffer res = null;
-            FileInputStream fileInputStream = null;
-            DataInputStream dataInputStream = null;
+            byte[] res = null;
             try {
                 long startTime = SystemClock.elapsedRealtime();
                 AtomicFile atomicFile = new AtomicFile(mFile);
-                fileInputStream = atomicFile.openRead();
-                dataInputStream = new DataInputStream(fileInputStream);
-                FileChannel channel = fileInputStream.getChannel();
-                res = channel.map(MapMode.READ_ONLY, channel.position(), channel.size());
+                res = atomicFile.readFully();
                 success = true;
                 RecordHistogram.recordTimesHistogram(
                         String.format(Locale.US, "Tabs.PersistedTabData.Storage.LoadTime.%s",
@@ -403,13 +395,10 @@ public class FilePersistedTabDataStorage implements PersistedTabDataStorage {
                                 "IOException while attempting to restore "
                                         + "%s. Details: %s",
                                 mFile, e.getMessage()));
-            } finally {
-                StreamUtil.closeQuietly(dataInputStream);
-                StreamUtil.closeQuietly(fileInputStream);
             }
             RecordHistogram.recordBooleanHistogram(
                     "Tabs.PersistedTabData.Storage.Restore." + getUmaTag(), success);
-            return res;
+            return res == null ? null : ByteBuffer.wrap(res);
         }
 
         @Override
