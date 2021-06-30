f002ef8ce8d698edbe16965fe31e850a60189dfc
dpapad <dpapad@chromium.org>
2021-06-30 20:58:38 +0000

    Settings: Fix $$() invocations via Polymer bindings in class syntax.
    The $$() helper method no longer exists after switching a Polymer
    element to use the class syntax, but several calls to $$() are made via
    Polymer bindings, mostly when searching in Settings.
    
    Adding a new BaseMixin to hold the $$() method, and planning to also
    add a fire() helper in a follow-up. Updating only
    search_page/search_page.js in this CL, which does not inherit any legacy
    Behavior and as such is truly missing the $$() helper causing a runtime
    error. Other places will be updated as needed once all legacy Behaviors
    are converted to Mixins.
    
    Fixed: 1225008
    Change-Id: I8d0797928078ff0dccc94257be875c50581a73af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2994834
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897536}

---
+++ chrome/browser/resources/settings/base_mixin.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Provides a couple of helper methods used by several Polymer
+ * elements.
+ */
+
+import {dedupingMixin} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/** @interface */
+export class BaseMixinInterface {
+  /**
+   * @param {string} query
+   * @return {?HTMLElement}
+   */
+  $$(query) {}
+}
+
+/**
+ * @polymer
+ * @mixinFunction
+ */
+export const BaseMixin = dedupingMixin(superClass => {
+  /**
+   * @polymer
+   * @mixinClass
+   * @implements {BaseMixinInterface}
+   */
+  class BaseMixin extends superClass {
+    /** @override */
+    $$(query) {
+      return this.shadowRoot.querySelector(query);
+    }
+  }
+
+  return BaseMixin;
+});
