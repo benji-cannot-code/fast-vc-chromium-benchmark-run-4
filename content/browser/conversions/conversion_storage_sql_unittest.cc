990a3d23a9bfe2105724dcca0affba82a8603238
Andrew Paseltiner <apaseltiner@chromium.org>
2021-06-30 21:11:29 +0000

    [attribution_reporting] Allow new conversions to be prioritized over existing ones
    https://github.com/WICG/conversion-measurement-api#triggering-attribution
    
    Bug: 1218053
    Change-Id: Icd4936856d00c3901b190e46525670f846862889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2951620
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897538}

--- content/browser/conversions/conversion_storage_sql_unittest.cc
+++ content/browser/conversions/conversion_storage_sql_unittest.cc
@@ -357,6 +357,17 @@ TEST_F(ConversionStorageSqlTest,
           .SetConversionDestination(net::SchemefulSite(conversion_origin))
           .SetReportingOrigin(reporting_origin)
           .Build()));
+  EXPECT_EQ(1u, storage()->GetActiveImpressions().size());
+
+  // Force the impression to be deactivated by ensuring that the next report is
+  // in a different window.
+  delegate()->set_report_time_ms(1);
+  EXPECT_FALSE(storage()->MaybeCreateAndStoreConversionReport(
+      ConversionBuilder()
+          .SetConversionDestination(net::SchemefulSite(conversion_origin))
+          .SetReportingOrigin(reporting_origin)
+          .Build()));
+  EXPECT_EQ(0u, storage()->GetActiveImpressions().size());
 
   clock()->Advance(base::TimeDelta::FromDays(1));
   EXPECT_TRUE(storage()->DeleteConversion(1));
@@ -403,6 +414,17 @@ TEST_F(ConversionStorageSqlTest,
           .SetConversionDestination(net::SchemefulSite(conversion_origin))
           .SetReportingOrigin(reporting_origin)
           .Build()));
+  EXPECT_EQ(1u, storage()->GetActiveImpressions().size());
+
+  // Force the impression to be deactivated by ensuring that the next report is
+  // in a different window.
+  delegate()->set_report_time_ms(1);
+  EXPECT_FALSE(storage()->MaybeCreateAndStoreConversionReport(
+      ConversionBuilder()
+          .SetConversionDestination(net::SchemefulSite(conversion_origin))
+          .SetReportingOrigin(reporting_origin)
+          .Build()));
+  EXPECT_EQ(0u, storage()->GetActiveImpressions().size());
 
   clock()->Advance(base::TimeDelta::FromDays(1));
   EXPECT_TRUE(storage()->DeleteConversion(1));
