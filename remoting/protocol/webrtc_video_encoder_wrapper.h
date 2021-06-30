b22f65150b034f9c9ef8252e27b63caea614525b
Lambros Lambrou <lambroslambrou@chromium.org>
2021-06-30 01:42:56 +0000

    [remoting] Detect large frames in WebrtcVideoEncoderWrapper.
    This copies the large-frame-detection logic from the scheduler into
    the new encoder. After the switch to standard-encoding-pipeline, the
    scheduler's large-frame-detection no longer had any effect.
    
    This is only used for VP8 (though in future, similar logic may be
    used for H264). The VP9 encoder already has this feature: it
    re-encodes the frame at lower quality if it detects overshoot of
    target FPS.
    
    Bug: 1192865
    Change-Id: I6f7d1143182e2bd984a4bdda32de9da4bdf0b00e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987254
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897210}

--- remoting/protocol/webrtc_video_encoder_wrapper.h
+++ remoting/protocol/webrtc_video_encoder_wrapper.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/single_thread_task_runner.h"
 #include "base/thread_annotations.h"
+#include "remoting/base/running_samples.h"
 #include "remoting/codec/webrtc_video_encoder.h"
 #include "third_party/webrtc/api/video/video_codec_type.h"
 #include "third_party/webrtc/api/video_codecs/sdp_video_format.h"
@@ -52,6 +53,8 @@ class WebrtcVideoEncoderWrapper : public webrtc::VideoEncoder {
   webrtc::VideoEncoder::EncoderInfo GetEncoderInfo() const override;
 
  private:
+  static constexpr int kStatsWindow = 5;
+
   // Returns an encoded frame to WebRTC's registered callback.
   webrtc::EncodedImageCallback::Result ReturnEncodedFrame(
       const WebrtcVideoEncoder::EncodedFrame& frame);
@@ -66,6 +69,13 @@ class WebrtcVideoEncoderWrapper : public webrtc::VideoEncoder {
   // Sets whether top-off is active, and fires a notification if the setting
   // changes.
   void SetTopOffActive(bool active);
+
+  // Returns whether the frame should be encoded at low quality, to reduce
+  // latency for large frame updates. This is only done here for VP8, as VP9
+  // automatically detects target-overshoot and re-encodes the frame at
+  // lower quality. This calculation is based on |frame|'s update-region
+  // (compared with recent history) and the current bandwidth-estimation.
+  bool ShouldDropQualityForLargeFrame(const webrtc::DesktopFrame& frame);
 
   std::unique_ptr<WebrtcVideoEncoder> encoder_
       GUARDED_BY_CONTEXT(sequence_checker_);
@@ -109,6 +119,12 @@ class WebrtcVideoEncoderWrapper : public webrtc::VideoEncoder {
   // always lies within the frame's bounding rect).
   webrtc::VideoFrame::UpdateRect accumulated_update_rect_
       GUARDED_BY_CONTEXT(sequence_checker_){};
+
+  // Used by ShouldDropQualityForLargeFrame(). This stores the most recent
+  // update-region areas of previously-encoded frames, in order to detect an
+  // unusually-large update.
+  RunningSamples updated_region_area_ GUARDED_BY_CONTEXT(sequence_checker_){
+      kStatsWindow};
 
   // TaskRunner used for notifying |video_channel_state_observer_|.
   scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
