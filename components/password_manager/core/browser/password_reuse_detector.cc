6a6c173b4e4328000bd64f39aef59879b47d7c65
Viktor Semeniuk <vsemeniuk@google.com>
2021-06-30 15:41:23 +0000

    Calculating the number of saved passwords dynamically
    This change calculates the number of saved passwords inside
    PasswordReuseDetector dynamically instead of relying on the variable.
    
    Change-Id: I6b359054c23ff63d034f668ea521f50f52f8f997
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988129
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#897384}

--- components/password_manager/core/browser/password_reuse_detector.cc
+++ components/password_manager/core/browser/password_reuse_detector.cc
@@ -116,7 +116,8 @@ void PasswordReuseDetector::CheckReuse(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(consumer);
   if (input.size() < kMinPasswordLengthToCheck) {
-    consumer->OnReuseCheckDone(false, 0, absl::nullopt, {}, saved_passwords_);
+    consumer->OnReuseCheckDone(false, 0, absl::nullopt, {},
+                               SavedPasswordsCount());
     return;
   }
 
@@ -142,7 +143,8 @@ void PasswordReuseDetector::CheckReuse(
                 enterprise_reused_password_length});
 
   if (max_reused_password_length == 0) {
-    consumer->OnReuseCheckDone(false, 0, absl::nullopt, {}, saved_passwords_);
+    consumer->OnReuseCheckDone(false, 0, absl::nullopt, {},
+                               SavedPasswordsCount());
     return;
   }
 
@@ -153,9 +155,9 @@ void PasswordReuseDetector::CheckReuse(
   } else if (enterprise_reused_password_length != 0) {
     reused_protected_password_hash = std::move(reused_enterprise_password_hash);
   }
-  consumer->OnReuseCheckDone(true, max_reused_password_length,
-                             reused_protected_password_hash,
-                             matching_reused_credentials, saved_passwords_);
+  consumer->OnReuseCheckDone(
+      true, max_reused_password_length, reused_protected_password_hash,
+      matching_reused_credentials, SavedPasswordsCount());
 }
 
 absl::optional<PasswordHashData> PasswordReuseDetector::CheckGaiaPasswordReuse(
@@ -328,12 +330,8 @@ void PasswordReuseDetector::AddPassword(const PasswordForm& form) {
   if (form.password_value.size() < kMinPasswordLengthToCheck)
     return;
 
-  const auto result =
-      passwords_with_matching_reused_credentials_[form.password_value].insert(
-          {form.signon_realm, form.username_value, form.in_store});
-  if (result.second) {
-    saved_passwords_++;
-  }
+  passwords_with_matching_reused_credentials_[form.password_value].insert(
+      {form.signon_realm, form.username_value, form.in_store});
 }
 
 void PasswordReuseDetector::RemovePassword(const PasswordForm& form) {
@@ -362,7 +360,6 @@ void PasswordReuseDetector::RemovePassword(const PasswordForm& form) {
       if (stored_credentials_for_password_value.empty()) {
         password_value_iter = passwords_with_matching_reused_credentials_.erase(
             password_value_iter);
-        saved_passwords_--;
       }
     } else {
       password_value_iter++;
@@ -406,6 +403,15 @@ PasswordReuseDetector::FindNextSavedPassword(
   return IsSuffix(input, it->first)
              ? it
              : passwords_with_matching_reused_credentials_.end();
+}
+
+size_t PasswordReuseDetector::SavedPasswordsCount() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  size_t count = 0;
+  for (const auto& pair : passwords_with_matching_reused_credentials_) {
+    count += pair.second.size();
+  }
+  return count;
 }
 
 }  // namespace password_manager
