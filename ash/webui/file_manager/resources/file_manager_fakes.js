bc78ccb3a75fd37c2e53d119bb21023f7a23cab8
Luciano Pacheco <lucmult@chromium.org>
2021-06-30 03:27:25 +0000

    Files SWA: Fix copy and paste
    Add chrome.extension.getViews() to the SWA fakes to fix the copy and
    paste operation in the Files SWA.  This is used in the
    FileTransferController which was raising an exception.
    
    This fixes copying and paste via keyboard shortcut Ctrl+C/Ctrl+V however
    the context menu items still show as disabled because of execCommand()
    return false for 'paste', which should be fixed in follow up CLs.
    
    Many Transfer/* tests issue an execCommand() and are also failing
    because the paste operation doesn't work.
    
    Test: browset_tests --gtest_filter="Transfer/FilesAppBrowserTest.Test/*_FilesSwa"
    Bug: b/191831968
    Change-Id: I8b5780dc286ff4fd2edf456147693fd36480dad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2981611
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897230}

--- ash/webui/file_manager/resources/file_manager_fakes.js
+++ ash/webui/file_manager/resources/file_manager_fakes.js
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @suppress {checkTypes} */
 window.chrome.extension = {
   inIncognitoContext: false,
+  getViews() {
+    return [];
+  },
 };
 
 window.DriveSyncHandler = class extends EventTarget {
