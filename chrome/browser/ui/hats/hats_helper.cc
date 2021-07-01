f01c8d3178796b5bb62bdd78128f529ec59c2de1
Victor Costan <pwnall@chromium.org>
2021-07-01 16:37:58 +0000

    Fix broken build: Add missing include.
    https://crrev.com/c/2993096 uses content::NavigationHandle in
    //chrome/browser/ui/hats/hats_helper.cc but did not add an #include
    for the relevant header. This CL adds the missing #include, which fixes
    compile errors in some build configurations.
    
    Change-Id: I3ea00411c4dbed1359ad1b886b955d528b7d0015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001004
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897789}

--- chrome/browser/ui/hats/hats_helper.cc
+++ chrome/browser/ui/hats/hats_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/hats/trust_safety_sentiment_service_factory.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/webui_url_constants.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 
 HatsHelper::~HatsHelper() = default;
