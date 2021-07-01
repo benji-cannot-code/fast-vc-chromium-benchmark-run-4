d6e37c0c01a063e8912b557d3522f9123659cbf0
Lei Zhang <thestig@chromium.org>
2021-07-01 19:59:25 +0000

    Fix missing includes in Chrome OS printing tests.
    These fail to build for me locally. Maybe they do not have sufficient
    bot coverage.
    
    Change-Id: Iebd509317b233c4ed6139e4ff3d956411c1ae74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998183
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897859}

--- chrome/browser/chromeos/extensions/printing_metrics/print_job_finished_event_dispatcher_apitest.cc
+++ chrome/browser/chromeos/extensions/printing_metrics/print_job_finished_event_dispatcher_apitest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/test/extension_test_message_listener.h"
