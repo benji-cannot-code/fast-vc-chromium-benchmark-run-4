159d9873e01d48033f7e20bda72e3af8ddb8a358
Mikel Astiz <mastiz@chromium.org>
2021-07-01 15:27:27 +0000

    [sync] Display trusted vault errors in Android error infobar
    Besides settings, let's display these errors in the infobar, just like
    other preexisting analogous errors like the passphrase-required case.
    
    Change-Id: Ia397a07ba4e46c69191f55196de0045ca30c113c
    Bug: 1100279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988604
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897754}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/FakeSyncServiceImpl.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/FakeSyncServiceImpl.java
@@ -96,8 +96,11 @@ public class FakeSyncServiceImpl extends SyncServiceImpl {
 
     public void setTrustedVaultKeyRequiredForPreferredDataTypes(
             boolean trustedVaultKeyRequiredForPreferredDataTypes) {
-        mTrustedVaultKeyRequiredForPreferredDataTypes =
-                trustedVaultKeyRequiredForPreferredDataTypes;
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mTrustedVaultKeyRequiredForPreferredDataTypes =
+                    trustedVaultKeyRequiredForPreferredDataTypes;
+            syncStateChanged();
+        });
     }
 
     @Override
@@ -106,7 +109,10 @@ public class FakeSyncServiceImpl extends SyncServiceImpl {
     }
 
     public void setTrustedVaultRecoverabilityDegraded(boolean recoverabilityDegraded) {
-        mTrustedVaultRecoverabilityDegraded = recoverabilityDegraded;
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            mTrustedVaultRecoverabilityDegraded = recoverabilityDegraded;
+            syncStateChanged();
+        });
     }
 
     @Override
