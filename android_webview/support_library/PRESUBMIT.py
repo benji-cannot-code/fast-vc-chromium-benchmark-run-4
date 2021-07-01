77aa7fcac70511d17607a16b83d3a6be303a1e1b
Keren Zhu <kerenzhu@chromium.org>
2021-07-01 04:04:01 +0000

    [code-health] Migrate //android_webview/support_library to Python3
    Verified by making whitespace changes to .java files under
    boundary_interfaces and then running git cl presubmit --upload
    
    Bug: 1211946
    Change-Id: Ib2e619d49d19319de0c91204cfb4b5387dd7e30f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998171
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897649}

--- android_webview/support_library/PRESUBMIT.py
+++ android_webview/support_library/PRESUBMIT.py
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Runs various style checks before upload.
 """
 
+USE_PYTHON3 = True
+
+
 def CheckChangeOnUpload(input_api, output_api):
   results = []
   results.extend(_CheckAnnotatedInvocationHandlers(input_api, output_api))
