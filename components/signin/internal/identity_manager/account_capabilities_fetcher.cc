61afb49309cb3b479f58ee6e9353d00d04193859
Alex Ilin <alexilin@chromium.org>
2021-07-01 09:34:39 +0000

    Record AccountCapabilitiesFetcher histograms
    This CL adds two new histograms to measure performance of
    AccountCapabilitiesFetcher:
    - Signin.AccountCapabilities.FetchDuration.{Success,Failure}
    - Signin.AccountCapabilities.FetchResult
    
    Bug: 1213071
    Change-Id: Id919a2d2a6e9394411d1c9a74599aa62f68f4b7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991875
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897713}

--- components/signin/internal/identity_manager/account_capabilities_fetcher.cc
+++ components/signin/internal/identity_manager/account_capabilities_fetcher.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/signin/internal/identity_manager/account_capabilities_fetcher.h"
 
+#include "base/metrics/histogram_functions.h"
+#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "components/signin/internal/identity_manager/account_capabilities_constants.h"
 #include "components/signin/internal/identity_manager/account_fetcher_service.h"
@@ -33,11 +35,13 @@ AccountCapabilitiesFetcher::AccountCapabilitiesFetcher(
 AccountCapabilitiesFetcher::~AccountCapabilitiesFetcher() {
   TRACE_EVENT_NESTABLE_ASYNC_END0("AccountFetcherService",
                                   "AccountCapabilitiesFetcher", this);
+  RecordFetchResultAndDuration(FetchResult::kCancelled);
 }
 
 void AccountCapabilitiesFetcher::Start() {
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("AccountFetcherService", "GetAccessToken",
                                     this);
+  fetch_start_time_ = base::TimeTicks::Now();
   OAuth2AccessTokenManager::ScopeSet scopes;
   scopes.insert(GaiaConstants::kAccountCapabilitiesOAuth2Scope);
   login_token_request_ =
@@ -70,6 +74,7 @@ void AccountCapabilitiesFetcher::OnGetTokenFailure(
   VLOG(1) << "OnGetTokenFailure: " << error.ToString();
   DCHECK_EQ(request, login_token_request_.get());
   login_token_request_.reset();
+  RecordFetchResultAndDuration(FetchResult::kGetTokenFailure);
   service_->OnAccountCapabilitiesFetchFailure(account_id_);
 }
 
@@ -82,10 +87,12 @@ void AccountCapabilitiesFetcher::OnGetAccountCapabilitiesResponse(
   if (!parsed_capabilities) {
     VLOG(1) << "Failed to parse account capabilities for " << account_id_
             << ". Response body: " << account_capabilities->DebugString();
+    RecordFetchResultAndDuration(FetchResult::kParseResponseFailure);
     service_->OnAccountCapabilitiesFetchFailure(account_id_);
     return;
   }
 
+  RecordFetchResultAndDuration(FetchResult::kSuccess);
   service_->OnAccountCapabilitiesFetchSuccess(account_id_,
                                               parsed_capabilities.value());
 }
@@ -95,6 +102,7 @@ void AccountCapabilitiesFetcher::OnOAuthError() {
                                   "GetAccountCapabilities", this, "error",
                                   "OAuthError");
   VLOG(1) << "OnOAuthError";
+  RecordFetchResultAndDuration(FetchResult::kOAuthError);
   service_->OnAccountCapabilitiesFetchFailure(account_id_);
 }
 
@@ -103,5 +111,32 @@ void AccountCapabilitiesFetcher::OnNetworkError(int response_code) {
       "AccountFetcherService", "GetAccountCapabilities", this, "error",
       "NetworkError", "response_code", response_code);
   VLOG(1) << "OnNetworkError " << response_code;
+  RecordFetchResultAndDuration(FetchResult::kNetworkError);
   service_->OnAccountCapabilitiesFetchFailure(account_id_);
+}
+
+void AccountCapabilitiesFetcher::RecordFetchResultAndDuration(
+    FetchResult result) {
+  if (fetch_histograms_recorded_) {
+    // Record histograms only once.
+    return;
+  }
+  fetch_histograms_recorded_ = true;
+
+  base::UmaHistogramEnumeration("Signin.AccountCapabilities.FetchResult",
+                                result);
+
+  if (fetch_start_time_.is_null()) {
+    // Cannot record duration for a fetch that hasn't started.
+    DCHECK_EQ(result, FetchResult::kCancelled);
+    return;
+  }
+  base::TimeDelta duration = base::TimeTicks::Now() - fetch_start_time_;
+  if (result == FetchResult::kSuccess) {
+    base::UmaHistogramMediumTimes(
+        "Signin.AccountCapabilities.FetchDuration.Success", duration);
+  } else {
+    base::UmaHistogramMediumTimes(
+        "Signin.AccountCapabilities.FetchDuration.Failure", duration);
+  }
 }
