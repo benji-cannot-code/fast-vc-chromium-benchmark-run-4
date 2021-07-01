6749f50cf248711a30a71d1624bc202cfd4ec35b
Christopher Lam <calamity@chromium.org>
2021-07-01 03:59:57 +0000

    Create libblink_extensions_chromeos.so
    This CL adds the infrastructure for the Chrome OS Blink Extensions. It
    adds a new component with the bindings and implementations of the
    extensions_chromeos component.
    
    It also adds a concrete implementation for the chromeos namespace that
    can is exported on the shared library.
    
    The CL can be tested by setting the GN arg
    use_blink_extensions_chromeos = true, compiling, and then running
    nm -gDC out/Default/libblink_extensions_chromeos.so
    
    Bug: 1223304
    
    Change-Id: Ie927c8ba9b6ba302e43bbfc0f3ab283f8a6a646a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2981771
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897647}

--- third_party/blink/renderer/extensions/chromeos/chromeos.cc
+++ third_party/blink/renderer/extensions/chromeos/chromeos.cc
@@ -3,6 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-namespace ChromeOS {
-  boolean myEmbedderFunction(boolean testArg);
-};
+#include "third_party/blink/renderer/extensions/chromeos/chromeos.h"
+
+namespace blink {
+
+bool ChromeOS::myEmbedderFunction(bool testArg) {
+  return !testArg;
+}
+
+}  // namespace blink
