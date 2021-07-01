44228088fd07dfa757d07c7f7dd7d027fd921916
Ramin Halavati <rhalavati@chromium.org>
2021-07-01 09:59:30 +0000

    Remove EnableEphemeralGuestProfilesOnDesktop flag and dependencies.
    Ephemeral Guest profiles are deprecated. The flag is removed and all
    dependencies on it are updated.
    
    Bug: 1225156
    Change-Id: I0fa9cd707cb909c0f692e23d97229034099d0859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2995443
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897718}

--- chrome/browser/ui/profile_picker_unittest.cc
+++ chrome/browser/ui/profile_picker_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ui_features.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
@@ -127,39 +126,6 @@ TEST_F(ProfilePickerTest, ShouldShowAtLaunch_MultipleProfiles_OneActive) {
 
 TEST_F(ProfilePickerTest, ShouldShowAtLaunch_SingleProfile) {
   testing_profile_manager()->CreateTestingProfile("profile1");
-  local_state()->SetBoolean(prefs::kBrowserProfilePickerShown, true);
-
-  EXPECT_FALSE(ProfilePicker::ShouldShowAtLaunch());
-}
-
-class ProfilePickerTestEphemeralGuest : public ProfilePickerTest {
- public:
-  ProfilePickerTestEphemeralGuest() {
-    feature_list_.InitAndEnableFeature(
-        features::kEnableEphemeralGuestProfilesOnDesktop);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
-
-TEST_F(ProfilePickerTestEphemeralGuest,
-       ShouldShowAtLaunch_MultipleProfiles_OneGuest) {
-  TestingProfile* profile1 =
-      testing_profile_manager()->CreateTestingProfile("profile1");
-  GetProfileAttributes(profile1)->SetActiveTimeToNow();
-  testing_profile_manager()->CreateTestingProfile("profile2");
-  TestingProfile* guest_profile =
-      testing_profile_manager()->CreateGuestProfile();
-  GetProfileAttributes(guest_profile)->SetActiveTimeToNow();
-
-  EXPECT_FALSE(ProfilePicker::ShouldShowAtLaunch());
-}
-
-TEST_F(ProfilePickerTestEphemeralGuest,
-       ShouldShowAtLaunch_MultipleProfiles_OneGuest_SeenPicker) {
-  testing_profile_manager()->CreateTestingProfile("profile1");
-  testing_profile_manager()->CreateGuestProfile();
   local_state()->SetBoolean(prefs::kBrowserProfilePickerShown, true);
 
   EXPECT_FALSE(ProfilePicker::ShouldShowAtLaunch());
