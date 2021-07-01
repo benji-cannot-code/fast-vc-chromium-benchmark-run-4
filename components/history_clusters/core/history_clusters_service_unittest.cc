0a73808e6d7ca8112b139b7b03a3d3e8b702c85a
Yuta Kitamura <yutak@chromium.org>
2021-07-01 08:10:08 +0000

    Sheriffing: Disable flaky test.
    Disables the following test on Linux:
    HistoryClustersServiceTest.QueryClustersWithEmptyEndpoint
    
    Bug: 1225511
    Change-Id: I7c92c3f5f1beff7738b2c9bb858a87483b817acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999945
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Owners-Override: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897692}

--- components/history_clusters/core/history_clusters_service_unittest.cc
+++ components/history_clusters/core/history_clusters_service_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "components/history/core/browser/history_context.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history/core/browser/url_row.h"
@@ -477,7 +478,15 @@ TEST_F(HistoryClustersServiceTest, QueryClustersWithEmptyVisits) {
   run_loop_.Run();
 }
 
-TEST_F(HistoryClustersServiceTest, QueryClustersWithEmptyEndpoint) {
+// https://crbug.com/1225511
+#if defined(OS_LINUX)
+#define MAYBE_QueryClustersWithEmptyEndpoint \
+  DISABLED_QueryClustersWithEmptyEndpoint
+#else
+#define MAYBE_QueryClustersWithEmptyEndpoint QueryClustersWithEmptyEndpoint
+#endif
+
+TEST_F(HistoryClustersServiceTest, MAYBE_QueryClustersWithEmptyEndpoint) {
   EnableMemoriesWithEndpoint("");
   AddHardcodedTestDataToHistoryService();
 
