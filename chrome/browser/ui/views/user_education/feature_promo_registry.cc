44d00042bcf338cac9b098ae24c7ef92bb7d8264
Tom <tluk@chromium.org>
2021-07-01 02:07:25 +0000

    [tab search] Add IPH promo configuration and invocation p1
    Bug: 1224218
    Change-Id: I1f9ab98e294122d4ed12a43742d405ba0e4e529c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992529
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897617}

--- chrome/browser/ui/views/user_education/feature_promo_registry.cc
+++ chrome/browser/ui/views/user_education/feature_promo_registry.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/page_action/page_action_icon_controller.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 #include "chrome/browser/ui/views/read_later/read_later_button.h"
+#include "chrome/browser/ui/views/tab_search_bubble_host.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/toolbar/browser_app_menu_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
@@ -92,6 +93,12 @@ views::View* GetAvatarToolbarButton(BrowserView* browser_view) {
   return browser_view->toolbar_button_provider()->GetAvatarToolbarButton();
 }
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
+
+// kIPHTabSearchFeature:
+views::View* GetTabSearchButton(BrowserView* browser_view) {
+  auto* tab_search_host = browser_view->GetTabSearchBubbleHost();
+  return tab_search_host ? tab_search_host->button() : nullptr;
+}
 
 #if BUILDFLAG(ENABLE_WEBUI_TAB_STRIP)
 // kIPHWebUITabStripFeature:
@@ -269,6 +276,16 @@ void FeaturePromoRegistry::RegisterKnownFeatures() {
 
     RegisterFeature(feature_engagement::kIPHReopenTabFeature, params,
                     base::BindRepeating(GetAppMenuButton));
+  }
+
+  {
+    // kIPHTabSearchFeature:
+    FeaturePromoBubbleParams params;
+    params.body_string_specifier = IDS_TAB_SEARCH_PROMO;
+    params.arrow = views::BubbleBorder::Arrow::TOP_RIGHT;
+
+    RegisterFeature(feature_engagement::kIPHTabSearchFeature, params,
+                    base::BindRepeating(GetTabSearchButton));
   }
 
 #if BUILDFLAG(ENABLE_WEBUI_TAB_STRIP)
