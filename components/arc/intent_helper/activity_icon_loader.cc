399cfd1256198424b5312935f6d61337ead378a4
Mitsuru Oshima <oshima@chromium.org>
2021-07-01 21:33:01 +0000

    Rename ScaleFactor to ResourceScaleFactor in components/arc|exo
    Bug: 1217405
    Change-Id: I47c0d34dc1c1181dcc741f943f6d792d0de97559
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3000834
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897901}

--- components/arc/intent_helper/activity_icon_loader.cc
+++ components/arc/intent_helper/activity_icon_loader.cc
@@ -33,8 +33,9 @@ constexpr size_t kLargeIconSizeInDip = 20;
 constexpr size_t kMaxIconSizeInPx = 200;
 constexpr char kPngDataUrlPrefix[] = "data:image/png;base64,";
 
-ui::ScaleFactor GetSupportedScaleFactor() {
-  std::vector<ui::ScaleFactor> scale_factors = ui::GetSupportedScaleFactors();
+ui::ResourceScaleFactor GetSupportedResourceScaleFactor() {
+  std::vector<ui::ResourceScaleFactor> scale_factors =
+      ui::GetSupportedResourceScaleFactors();
   DCHECK(!scale_factors.empty());
   return scale_factors.back();
 }
@@ -93,8 +94,8 @@ ActivityIconLoader::ActivityName GenerateActivityName(
 // data: URL.
 scoped_refptr<base::RefCountedData<GURL>> GeneratePNGDataUrl(
     const gfx::ImageSkia& image,
-    ui::ScaleFactor scale_factor) {
-  float scale = ui::GetScaleForScaleFactor(scale_factor);
+    ui::ResourceScaleFactor scale_factor) {
+  float scale = ui::GetScaleForResourceScaleFactor(scale_factor);
   std::vector<unsigned char> output;
   gfx::PNGCodec::EncodeBGRASkBitmap(image.GetRepresentation(scale).GetBitmap(),
                                     false /* discard_transparency */, &output);
@@ -107,8 +108,9 @@ scoped_refptr<base::RefCountedData<GURL>> GeneratePNGDataUrl(
       new base::RefCountedData<GURL>(GURL(kPngDataUrlPrefix + encoded)));
 }
 
-ActivityIconLoader::Icons ResizeIconsInternal(const gfx::ImageSkia& image,
-                                              ui::ScaleFactor scale_factor) {
+ActivityIconLoader::Icons ResizeIconsInternal(
+    const gfx::ImageSkia& image,
+    ui::ResourceScaleFactor scale_factor) {
   // Resize the original icon to the sizes intent_helper needs.
   gfx::ImageSkia icon_large(gfx::ImageSkiaOperations::CreateResizedImage(
       image, skia::ImageOperations::RESIZE_BEST,
@@ -126,7 +128,7 @@ ActivityIconLoader::Icons ResizeIconsInternal(const gfx::ImageSkia& image,
 
 std::unique_ptr<ActivityIconLoader::ActivityToIconsMap> ResizeAndEncodeIcons(
     std::vector<mojom::ActivityIconPtr> icons,
-    ui::ScaleFactor scale_factor) {
+    ui::ResourceScaleFactor scale_factor) {
   auto result = std::make_unique<ActivityIconLoader::ActivityToIconsMap>();
   for (size_t i = 0; i < icons.size(); ++i) {
     static const size_t kBytesPerPixel = 4;
@@ -156,7 +158,7 @@ std::unique_ptr<ActivityIconLoader::ActivityToIconsMap> ResizeAndEncodeIcons(
 std::unique_ptr<ActivityIconLoader::ActivityToIconsMap> ResizeIcons(
     std::vector<ActivityIconLoader::ActivityName> activity_names,
     const std::vector<gfx::ImageSkia>& images,
-    ui::ScaleFactor scale_factor) {
+    ui::ResourceScaleFactor scale_factor) {
   DCHECK_EQ(activity_names.size(), images.size());
   auto result = std::make_unique<ActivityIconLoader::ActivityToIconsMap>();
   for (size_t i = 0; i < activity_names.size(); ++i) {
@@ -192,7 +194,7 @@ bool ActivityIconLoader::ActivityName::operator<(
 }
 
 ActivityIconLoader::ActivityIconLoader()
-    : scale_factor_(GetSupportedScaleFactor()) {}
+    : scale_factor_(GetSupportedResourceScaleFactor()) {}
 
 ActivityIconLoader::~ActivityIconLoader() = default;
 
