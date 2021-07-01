1c889a8929dd11cd06c932d9d82aae11f233dbef
Etienne Bergeron <etienneb@chromium.org>
2021-07-01 01:06:31 +0000

    Sequentialize the calls to CheckResourceIntegrity
    This CL is adding a sequence to run all the calls to
    CheckResourceIntegrity in sequence instead than in parallel.
    
    The main reason is to avoid Shutdown hangs where all the worker
    background threads are stalled on a Read(...) and won't allow
    the scheduler to dispatch remaining BLOCK_SHUTDOWN tasks.
    
    Bug: 1222871
    Change-Id: I671900e2843aa6ee9e7662f2cedc1ad8328c5e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998444
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897601}

--- chrome/browser/resources_integrity_unittest.cc
+++ chrome/browser/resources_integrity_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/app/packed_resources_integrity.h"
 #include "chrome/browser/buildflags.h"
@@ -32,6 +33,7 @@ TEST_F(CheckResourceIntegrityTest, Match) {
 
   base::RunLoop loop;
   CheckResourceIntegrity(test_data_path.AppendASCII("circle.svg"), expected,
+                         base::SequencedTaskRunnerHandle::Get(),
                          base::BindLambdaForTesting([&](bool matches) {
                            EXPECT_TRUE(matches);
                            loop.Quit();
@@ -47,6 +49,7 @@ TEST_F(CheckResourceIntegrityTest, Mismatch) {
   base::RunLoop loop;
   CheckResourceIntegrity(test_data_path.AppendASCII("circle.svg"),
                          base::make_span<32>(unexpected),
+                         base::SequencedTaskRunnerHandle::Get(),
                          base::BindLambdaForTesting([&](bool matches) {
                            EXPECT_FALSE(matches);
                            loop.Quit();
@@ -60,6 +63,7 @@ TEST_F(CheckResourceIntegrityTest, NonExistentFile) {
   CheckResourceIntegrity(
       base::FilePath(FILE_PATH_LITERAL("this file does not exist.moo")),
       base::make_span<crypto::kSHA256Length>(unexpected),
+      base::SequencedTaskRunnerHandle::Get(),
       base::BindLambdaForTesting([&](bool matches) {
         EXPECT_FALSE(matches);
         loop.Quit();
