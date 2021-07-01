e3c04da34de237f46296e373383769fb9472a418
Yue Li <updowndota@google.com>
2021-07-01 19:21:01 +0000

    Quick Answers V2: Update to use flexlayout for QuickAnswersView
    Also re-order the existing methods to match the header file.
    
    Bug: b/186906279
    Test: Manual Test
    Change-Id: I6caad652faf03e511b338e9e7101dc20f623a98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992533
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897848}

--- ash/quick_answers/ui/user_consent_view.cc
+++ ash/quick_answers/ui/user_consent_view.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/menu/menu_config.h"
 #include "ui/views/controls/menu/menu_controller.h"
-#include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/layout_provider.h"
