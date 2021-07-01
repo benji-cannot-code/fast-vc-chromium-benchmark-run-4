26a71456c752953ab9201968d5af1537dc43679a
Christoph Schwering <schwering@google.com>
2021-07-01 18:16:15 +0000

    [Autofill] Migrated PagePasswordsAnalyser to {Form,Field}RendererIds.
    Bug: 1007974
    Change-Id: Ic086c58a34bb955b821b23626171b4d51d965965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998470
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#897822}

--- components/autofill/content/renderer/page_passwords_analyser.h
+++ components/autofill/content/renderer/page_passwords_analyser.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "components/autofill/core/common/unique_ids.h"
 #include "third_party/blink/public/web/web_console_message.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 
@@ -43,9 +44,9 @@ class PagePasswordsAnalyser {
   // By default, the analyser will log to the DevTools console.
   void AnalyseDocumentDOM(blink::WebLocalFrame* frame);
 
-  // A set of renderer_ids which have already been analyzed.
-  std::set<uint32_t> skip_control_element_renderer_ids_;
-  std::set<uint32_t> skip_form_element_renderer_ids_;
+  // Sets of renderer_ids which have already been analyzed.
+  std::set<FormRendererId> skip_form_element_renderer_ids_;
+  std::set<FieldRendererId> skip_control_element_renderer_ids_;
   // This is true when new DOM content is available since the last time
   // the page was analysed, meaning the page needs to be reanalysed.
   bool page_dirty_;
