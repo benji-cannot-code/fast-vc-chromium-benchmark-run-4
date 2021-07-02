74f233b78e72a8294e65679f07e2a12cbcc8e3df
Shengsong Tan <sstan@chromium.org>
2021-07-02 07:07:26 +0000

    full_restore: Add memory pressure listener for ArcAppLaunchHandler
    This CL add d-bus observer for sensing memory pressure. It is for monitor
    system pressure info when restore apps. D-bus side will update memory
    pressure in 1s interval.
    
    BUG=b:191224722
    BUG=b:177405617
    
    Change-Id: I3de592fa26f87dc371e1ee9f4b877f225f0656da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999923
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898051}

--- chrome/browser/chromeos/full_restore/arc_app_launch_handler.h
+++ chrome/browser/chromeos/full_restore/arc_app_launch_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/memory/weak_ptr.h"
+#include "chromeos/dbus/resourced/resourced_client.h"
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
 
 namespace apps {
@@ -27,7 +28,8 @@ class FullRestoreAppLaunchHandler;
 // 1. Add memory pressure checking before launch ARC apps.
 // 2. Add app launch policy.
 // 3. Check whether the ARC app is ready before launch the ARC apps.
-class ArcAppLaunchHandler : public apps::AppRegistryCache::Observer {
+class ArcAppLaunchHandler : public apps::AppRegistryCache::Observer,
+                            public chromeos::ResourcedClient::Observer {
  public:
   explicit ArcAppLaunchHandler(FullRestoreAppLaunchHandler* handler);
   ArcAppLaunchHandler(const ArcAppLaunchHandler&) = delete;
@@ -43,6 +45,11 @@ class ArcAppLaunchHandler : public apps::AppRegistryCache::Observer {
   void OnAppRegistryCacheWillBeDestroyed(
       apps::AppRegistryCache* cache) override;
 
+ protected:
+  // Override chromeos::ResourcedClient::Observer
+  void OnMemoryPressure(chromeos::ResourcedClient::PressureLevel level,
+                        uint64_t reclaim_target_kb) override;
+
  private:
   void LaunchApp(const std::string& app_id);
 
@@ -53,6 +60,8 @@ class ArcAppLaunchHandler : public apps::AppRegistryCache::Observer {
   std::set<std::string> app_ids_;
 
   apps::AppRegistryCache& cache_;
+
+  chromeos::ResourcedClient::PressureLevel pressure_level_;
 
   base::WeakPtrFactory<ArcAppLaunchHandler> weak_ptr_factory_{this};
 };
