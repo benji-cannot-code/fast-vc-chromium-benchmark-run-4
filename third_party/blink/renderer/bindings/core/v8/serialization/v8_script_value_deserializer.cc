d3b561bdcb7839aaaee6eb83011162a42a1a3ced
Sigurd Schneider <sigurds@chromium.org>
2021-07-02 12:01:17 +0000

    Report issue on WASM non-same-origin module sharing
    Bug: chromium:1224881
    Change-Id: I3b9451d0b571108618d594bc5e90268b5f3b60bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992201
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898122}

--- third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_deserializer.cc
+++ third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_deserializer.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/geometry/dom_rect_read_only.h"
 #include "third_party/blink/renderer/core/html/canvas/image_data.h"
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap.h"
+#include "third_party/blink/renderer/core/inspector/inspector_audits_issue.h"
 #include "third_party/blink/renderer/core/messaging/message_port.h"
 #include "third_party/blink/renderer/core/mojo/mojo_handle.h"
 #include "third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h"
@@ -756,13 +757,19 @@ V8ScriptValueDeserializer::GetWasmModuleFromId(v8::Isolate* isolate,
     ExecutionContext* execution_context = ExecutionContext::From(script_state_);
     DCHECK(serialized_script_value_->origin());
     UseCounter::Count(execution_context, WebFeature::kWasmModuleSharing);
+    const v8::CompiledWasmModule& wasm_module =
+        serialized_script_value_->WasmModules()[id];
     if (!serialized_script_value_->origin()->IsSameOriginWith(
             execution_context->GetSecurityOrigin())) {
       UseCounter::Count(execution_context,
                         WebFeature::kCrossOriginWasmModuleSharing);
+      AuditsIssue::ReportCrossOriginWasmModuleSharingIssue(
+          execution_context, wasm_module.source_url(),
+          serialized_script_value_->origin()->ToString(),
+          execution_context->GetSecurityOrigin()->ToString(),
+          true /* is_warning */);
     }
-    return v8::WasmModuleObject::FromCompiledModule(
-        isolate, serialized_script_value_->WasmModules()[id]);
+    return v8::WasmModuleObject::FromCompiledModule(isolate, wasm_module);
   }
   CHECK(serialized_script_value_->WasmModules().IsEmpty());
   return v8::MaybeLocal<v8::WasmModuleObject>();
