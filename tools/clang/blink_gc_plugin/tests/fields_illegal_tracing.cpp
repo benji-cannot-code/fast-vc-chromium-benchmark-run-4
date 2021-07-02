a148d7048b5bcf59fc246e206ffa029be0d6c9c5
Omer Katz <omerkatz@chromium.org>
2021-07-02 12:50:55 +0000

    Reland "heap: Update clang gc plugin definition of class requires tracing"
    This is a reland of a888f5391fafbf21cd37fba989a3216205699621
    
    Original change's description:
    > heap: Update clang gc plugin definition of class requires tracing
    >
    > The plugin used to only consider class as requiring tracing if they had
    > fields or multiple bases that require tracing. Ignored field for example
    > didn't contribute to the check, which caused issues in the past.
    >
    > Update the check to also consider having a Trace method as a criteria.
    > We assume that if the user implemented a Trace method it was done for a
    > reason and that the class requires tracing.
    >
    > Bug: 1220331
    > Change-Id: I5d4880c70d3b9ce6dd7ab9e5e290558169673c68
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992850
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#896621}
    
    Bug: 1220331
    Change-Id: I4b0c6a84a864963f4003f8f9dac1c3d978565121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992753
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898132}

--- tools/clang/blink_gc_plugin/tests/fields_illegal_tracing.cpp
+++ tools/clang/blink_gc_plugin/tests/fields_illegal_tracing.cpp
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void PartObject::Trace(Visitor* visitor) const {
+void PartObjectWithTrace::Trace(Visitor* visitor) const {
   visitor->Trace(m_obj2);
   visitor->Trace(m_obj3);
   visitor->Trace(m_obj4);
