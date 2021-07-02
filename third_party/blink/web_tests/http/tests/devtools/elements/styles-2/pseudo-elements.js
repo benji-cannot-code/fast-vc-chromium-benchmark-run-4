4e415af4391c41cb09d0ccdfa5c6ebc8aae60df3
Fernando Fiori <ffiori@microsoft.com>
2021-07-02 22:30:20 +0000

    Highlight API: Implement devtools support
    Bug: 1195196
    Change-Id: I8ecc4b63e1502e2555c65ca324450ab1ffe16cc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998242
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Fernando Fiori <ffiori@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#898375}

--- third_party/blink/web_tests/http/tests/devtools/elements/styles-2/pseudo-elements.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/styles-2/pseudo-elements.js
@@ -21,6 +21,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         color: teal;
       }
 
+      #inspected::highlight(foo) {
+        color: fuchsia;
+      }
+
+      #inspected::highlight(bar) {
+        color: cyan;
+      }
+
       #inspected {
         display: list-item;
       }
