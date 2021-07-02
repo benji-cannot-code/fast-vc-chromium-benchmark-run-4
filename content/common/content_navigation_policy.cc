92529cd65f3d10d927d18363438d8d25540f6541
Jayson Adams <shrike@chromium.org>
2021-07-02 16:37:05 +0000

    [content_nav_policy] Cache lookup of kSkipEarlyCommitPendingForCrashedFrame
    Calls to ShouldSkipEarlyCommitPendingForCrashedFrame() account for
    2.8% of main browser thread time when idling on dictionary.com.
    This cl caches the result of the feature lookup to avoid calling
    base::FeatureList::IsEnabled() repeatedly.
    
    Bug: 1225485
    Change-Id: Ib07c2b2c298c5fbb76351f483f935df7d0770091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999028
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898211}

--- content/common/content_navigation_policy.cc
+++ content/common/content_navigation_policy.cc
@@ -170,8 +170,10 @@ bool ShouldCreateNewHostForSameSiteSubframe() {
 }
 
 bool ShouldSkipEarlyCommitPendingForCrashedFrame() {
-  return base::FeatureList::IsEnabled(
-      features::kSkipEarlyCommitPendingForCrashedFrame);
+  static bool skip_early_commit_pending_for_crashed_frame =
+      base::FeatureList::IsEnabled(
+          features::kSkipEarlyCommitPendingForCrashedFrame);
+  return skip_early_commit_pending_for_crashed_frame;
 }
 
 }  // namespace content
