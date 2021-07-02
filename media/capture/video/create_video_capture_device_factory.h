deef1045d745d44f869094360553e0385dc2d1a7
Wei Lee <wtlee@google.com>
2021-07-02 07:50:22 +0000

    lacros: Support fake/file camera
    Originally, Lacros-Chrome will also forward video capture requests for
    fake/file camera but since the corresponding flags are not forwarded to
    Ash-Chrome, it will still use the real cameras.
    
    This CL avoids Lacros-Chrome from forwarding video capture requests to
    Ash-Chrome and handle them itself instead.
    
    Bug: b:192533181
    Test: Manually
    Change-Id: I04c62aab8497abd85174bba3b076199b23d60dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002788
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898058}

--- media/capture/video/create_video_capture_device_factory.h
+++ media/capture/video/create_video_capture_device_factory.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+bool CAPTURE_EXPORT ShouldUseFakeVideoCaptureDeviceFactory();
+
 std::unique_ptr<VideoCaptureDeviceFactory> CAPTURE_EXPORT
 CreateVideoCaptureDeviceFactory(
     scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner);
