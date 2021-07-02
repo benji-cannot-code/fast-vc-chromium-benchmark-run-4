c7931cb72f8d5002d77a03ee22e8e8ec29eca3e5
Ana SollanoKim <ansollan@microsoft.com>
2021-07-02 17:25:04 +0000

    [GridNG] Placement of grid items with a negative margins
    Previously, in |ContributionSizeForGridItem| margins were clamped to
    zero before adding them to the contribution. Therefore, grid items with
    negative margins were not being placed correctly. In this CL, the order
    of operations is flipped: add the margin to the contribution and the
    result is then clamped to zero.
    
    Bug: 1045599, 1225424
    Change-Id: I6fc2726d4744d520430b2ca96b3509a396d73538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002394
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898232}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.cc
@@ -1109,9 +1109,8 @@ LayoutUnit NGGridLayoutAlgorithm::ContributionSizeForGridItem(
     return fragment.BlockSize() + baseline_shim;
   };
 
-  const LayoutUnit clamped_margin_sum =
-      (is_for_columns ? margins.InlineSum() : margins.BlockSum())
-          .ClampNegativeToZero();
+  const LayoutUnit margin_sum =
+      is_for_columns ? margins.InlineSum() : margins.BlockSum();
 
   LayoutUnit contribution;
   switch (contribution_type) {
@@ -1217,8 +1216,8 @@ LayoutUnit NGGridLayoutAlgorithm::ContributionSizeForGridItem(
             // computed margins, border, and padding in the given dimension,
             // flooring at zero so that the inner size is not negative.
             spanned_tracks_definite_max_size =
-                (spanned_tracks_definite_max_size - baseline_shim -
-                 clamped_margin_sum - border_padding_sum)
+                (spanned_tracks_definite_max_size - baseline_shim - margin_sum -
+                 border_padding_sum)
                     .ClampNegativeToZero();
 
             // Add the baseline shim, border, and padding (margins will be added
@@ -1270,8 +1269,7 @@ LayoutUnit NGGridLayoutAlgorithm::ContributionSizeForGridItem(
       break;
   }
 
-  DCHECK_GE(contribution, 0);
-  return contribution + clamped_margin_sum;
+  return (contribution + margin_sum).ClampNegativeToZero();
 }
 
 void NGGridLayoutAlgorithm::ConstructAndAppendGridItems(
