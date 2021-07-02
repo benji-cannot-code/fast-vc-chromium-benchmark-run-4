547448d54f3022dd9ccc6743f0393c4de88b3b8b
Mikel Astiz <mastiz@chromium.org>
2021-07-02 20:33:23 +0000

    [sync] Implement IOSTrustedVaultClient::MarkKeysAsStale()
    The function now invokes the newly-added and analogous function
    ChromeTrustedVaultService::MarkLocalKeysStale(), which has actually the
    same behaviour in this patch, but now allows embedders to provide custom
    logic.
    
    Change-Id: I4f25c029151bb8c96929841e00db4dc909abc1b5
    Bug: 1100278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001844
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898318}

--- ios/public/provider/chrome/browser/signin/chrome_trusted_vault_service.mm
+++ ios/public/provider/chrome/browser/signin/chrome_trusted_vault_service.mm
@@ -23,6 +23,12 @@ void ChromeTrustedVaultService::RemoveObserver(Observer* observer) {
   observer_list_.RemoveObserver(observer);
 }
 
+void ChromeTrustedVaultService::MarkLocalKeysAsStale(
+    ChromeIdentity* chrome_identity,
+    base::OnceCallback<void(bool)> callback) {
+  std::move(callback).Run(false);
+}
+
 void ChromeTrustedVaultService::Reauthentication(
     ChromeIdentity* chrome_identity,
     UIViewController* presentingViewController,
