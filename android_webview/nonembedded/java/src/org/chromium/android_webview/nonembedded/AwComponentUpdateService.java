34e271d921a4a3c641f588ccef85795b280f7801
Mugdha Lakhani <nator@chromium.org>
2021-07-02 19:38:54 +0000

    [AW][SafeMode] Add ComponentUpdaterReset precaution.
    Add ComponentUpdaterReset precaution which causes Component Updater
    services, ComponentsProviderService and AwComponentUpdateService to
    delete their downloaded dynamic configs, and WebView embedders to not
    receive any configs while the Safe Mode is on.
    
    Design doc: go/component-updater-safe-mode
    
    Bug: 1221610
    Test: ./out/emulator/bin/run_webview_instrumentation_test_apk -f *ComponentsProviderServiceTest$AutoBindServiceTests*
    Change-Id: Ia42e7d6ccf0829382232edb6213f828af3e65622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2985640
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898290}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/AwComponentUpdateService.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/AwComponentUpdateService.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.SystemClock;
 
+import org.chromium.android_webview.services.ComponentUpdaterSafeModeUtils;
 import org.chromium.android_webview.services.ComponentsProviderPathUtil;
 import org.chromium.base.Callback;
 import org.chromium.base.FileUtils;
@@ -71,14 +72,18 @@ public class AwComponentUpdateService extends JobService {
     public boolean onStartJob(JobParameters params) {
         assert mJobParameters == null;
         mJobParameters = params;
-        return startUpdates();
+        return maybeStartUpdates();
     }
 
     // Called by JobScheduler.
     @Override
     public boolean onStopJob(JobParameters params) {
+        ComponentUpdaterSafeModeUtils.executeSafeModeIfEnabled(
+                new File(ComponentsProviderPathUtil.getComponentUpdateServiceDirectoryPath()));
+
         // TODO(https://crbug.com/1221092): Stop native updates when onStopJob, onDestroy are
         // called.
+
         setUnexpectedExit(false);
         mJobParameters = null;
 
@@ -97,7 +102,7 @@ public class AwComponentUpdateService extends JobService {
         // Always keep the most recent startId as this is the one that should be used to stop
         // the service.
         mServiceStartedId = startId;
-        if (!startUpdates()) {
+        if (!maybeStartUpdates()) {
             stopSelf(startId);
             mServiceStartedId = 0;
         }
@@ -110,10 +115,16 @@ public class AwComponentUpdateService extends JobService {
      * @return {@code true} if it successfully triggers component updates or if component are
      *         already updating, {@code false} if it fails to trigger the updates.
      */
-    private boolean startUpdates() {
+    private boolean maybeStartUpdates() {
         if (mIsUpdating) {
             return true;
         }
+
+        if (ComponentUpdaterSafeModeUtils.executeSafeModeIfEnabled(new File(
+                    ComponentsProviderPathUtil.getComponentUpdateServiceDirectoryPath()))) {
+            return false;
+        }
+
         maybeRecordUnexpectedExit();
 
         // TODO(http://crbug.com/1179297) look at doing this in a task on a background thread
@@ -139,6 +150,9 @@ public class AwComponentUpdateService extends JobService {
     // Call the appropriate stop method according to how the service is launched.
     private void stopService() {
         mIsUpdating = false;
+        ComponentUpdaterSafeModeUtils.executeSafeModeIfEnabled(
+                new File(ComponentsProviderPathUtil.getComponentUpdateServiceDirectoryPath()));
+
         // Service is launched as a started service.
         if (mServiceStartedId > 0) {
             stopSelf(mServiceStartedId);
---
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/ComponentUpdaterResetSafeModeAction.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.nonembedded;
+
+import androidx.annotation.NonNull;
+
+import org.chromium.android_webview.common.SafeModeAction;
+
+/**
+ * A {@link SafeModeAction} to reset Component Updater.
+ */
+public class ComponentUpdaterResetSafeModeAction implements SafeModeAction {
+    private static final String TAG = "WebViewSafeMode";
+
+    // This ID should not be changed or reused.
+    private static final String ID = "reset_component_updater";
+
+    @Override
+    @NonNull
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public void execute() {
+        // Each Component Updater Service will take requisite steps on startup.
+        // Do nothing here.
+    }
+}
