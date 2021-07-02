a657949c43fc924c1d43c8190902b37fc219c8ea
Phillis Tang <phillis@chromium.org>
2021-07-02 23:30:16 +0000

    DPWA: remove unused bookmark_app_finalizer_utils
    Remove bookmark_app_finalizer_utils.* as they are not used anymore.
    
    Change-Id: I55a31364016feef7a3fa704b6e7c60227413e543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003988
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898391}

--- chrome/browser/ui/webui/ntp/app_launcher_handler.cc
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.cc
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_provider_base.h"
 #include "chrome/browser/web_applications/components/web_application_info.h"
-#include "chrome/browser/web_applications/extensions/bookmark_app_finalizer_utils.h"
 #include "chrome/browser/web_applications/extensions/bookmark_app_util.h"
 #include "chrome/browser/web_applications/web_app_icon_manager.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
