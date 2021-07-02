caa6834d12877a9182f5d70e63d0fc2311661363
Evan Stade <estade@chromium.org>
2021-07-02 00:44:43 +0000

    Re-enable WebAppFileHandlingPermissionDialogTest suite.
    A race condition fixed by way of PermissionRequestObserver.
    
    Without this fix, I was able to reproduce the flake locally about
    1/15 times on Chrome OS. With the fix, I was unable to reproduce
    in 100 runs.
    
    Bug: 1221772
    Change-Id: I80c142f1fa243bdac499e4db9567ba36c8a0b40b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999349
    Auto-Submit: Evan Stade <estade@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#897990}

--- chrome/browser/ui/web_applications/web_app_file_handling_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_file_handling_browsertest.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/embedder_support/switches.h"
+#include "components/permissions/test/permission_request_observer.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
@@ -594,7 +595,6 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingBrowserTest,
                                    ContentSettingsType::FILE_HANDLING));
 }
 
-// TODO(crbug/1221772): This test suite is disabled due to flake.
 class WebAppFileHandlingPermissionDialogTest
     : public WebAppFileHandlingBrowserTest {
  public:
@@ -613,6 +613,11 @@ class WebAppFileHandlingPermissionDialogTest
     test_file_path_ = NewTestFilePath("txt");
     LaunchWithFiles(app_id(), GetTextFileHandlerActionURL(), {test_file_path_});
 
+    // The permission request is dequeued asynchronously. It may or may not be
+    // showing by now.
+    if (!FileHandlingPermissionRequestDialogTestApi::IsShowing())
+      permissions::PermissionRequestObserver(web_contents_).Wait();
+
     // A dialog is showing now.
     ASSERT_TRUE(FileHandlingPermissionRequestDialogTestApi::IsShowing());
 
@@ -625,8 +630,7 @@ class WebAppFileHandlingPermissionDialogTest
   base::FilePath test_file_path_;
 };
 
-IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest,
-                       DISABLED_AllowAlways) {
+IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest, AllowAlways) {
   FileHandlingPermissionRequestDialogTestApi::Resolve(/*checked=*/true,
                                                       /*accept=*/true);
   VerifyPwaDidReceiveFileLaunchParams(true, test_file_path_);
@@ -634,16 +638,14 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest,
             GetFileHandlingPermission(GetSecureAppURL()));
 }
 
-IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest,
-                       DISABLED_AllowOnce) {
+IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest, AllowOnce) {
   FileHandlingPermissionRequestDialogTestApi::Resolve(/*checked=*/false,
                                                       /*accept=*/true);
   VerifyPwaDidReceiveFileLaunchParams(true, test_file_path_);
   EXPECT_EQ(CONTENT_SETTING_ASK, GetFileHandlingPermission(GetSecureAppURL()));
 }
 
-IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest,
-                       DISABLED_BlockAlways) {
+IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest, BlockAlways) {
   FileHandlingPermissionRequestDialogTestApi::Resolve(/*checked=*/true,
                                                       /*accept=*/false);
   VerifyPwaDidReceiveFileLaunchParams(false);
@@ -651,8 +653,7 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest,
             GetFileHandlingPermission(GetSecureAppURL()));
 }
 
-IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest,
-                       DISABLED_BlockOnce) {
+IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest, BlockOnce) {
   FileHandlingPermissionRequestDialogTestApi::Resolve(/*checked=*/false,
                                                       /*accept=*/false);
   VerifyPwaDidReceiveFileLaunchParams(false);
