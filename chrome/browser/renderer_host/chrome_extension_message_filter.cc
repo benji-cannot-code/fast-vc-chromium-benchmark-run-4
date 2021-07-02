c875fa0cd689b388540e1c9857c69fcc8a41f553
David Bertoni <dbertoni@chromium.org>
2021-07-02 23:20:36 +0000

    [CodeHealth] Remove uses of ListValue::DeepCopy() in /chrome/browser/renderer_host.
    Where possible, the code is migrated to use Value::Clone(). Where
    that's not practical, calls are replaced with
    ListValue::CreateDeepCopy().
    
    This CL was uploaded by git cl split.
    
    R=lazyboy@chromium.org
    
    Bug: 1187108
    Change-Id: If37978a47682057703931251f88f0dca77d3697c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003013
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898384}

--- chrome/browser/renderer_host/chrome_extension_message_filter.cc
+++ chrome/browser/renderer_host/chrome_extension_message_filter.cc
@@ -189,7 +189,7 @@ void ChromeExtensionMessageFilter::OnAddAPIActionToExtensionActivityLog(
   scoped_refptr<extensions::Action> action = new extensions::Action(
       extension_id, base::Time::Now(), extensions::Action::ACTION_API_CALL,
       params.api_call);
-  action->set_args(base::WrapUnique(params.arguments.DeepCopy()));
+  action->set_args(params.arguments.CreateDeepCopy());
   if (!params.extra.empty()) {
     action->mutable_other()->SetString(
         activity_log_constants::kActionExtra, params.extra);
@@ -206,7 +206,7 @@ void ChromeExtensionMessageFilter::OnAddDOMActionToExtensionActivityLog(
   scoped_refptr<extensions::Action> action = new extensions::Action(
       extension_id, base::Time::Now(), extensions::Action::ACTION_DOM_ACCESS,
       params.api_call);
-  action->set_args(base::WrapUnique(params.arguments.DeepCopy()));
+  action->set_args(params.arguments.CreateDeepCopy());
   action->set_page_url(params.url);
   action->set_page_title(base::UTF16ToUTF8(params.url_title));
   action->mutable_other()->SetInteger(activity_log_constants::kActionDomVerb,
@@ -223,7 +223,7 @@ void ChromeExtensionMessageFilter::OnAddEventToExtensionActivityLog(
   scoped_refptr<extensions::Action> action = new extensions::Action(
       extension_id, base::Time::Now(), extensions::Action::ACTION_API_EVENT,
       params.api_call);
-  action->set_args(base::WrapUnique(params.arguments.DeepCopy()));
+  action->set_args(params.arguments.CreateDeepCopy());
   if (!params.extra.empty()) {
     action->mutable_other()->SetString(activity_log_constants::kActionExtra,
                                        params.extra);
