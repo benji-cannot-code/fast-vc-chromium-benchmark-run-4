b37af13fa8b37450dc743a7a54a83bf960994992
Shengsong Tan <sstan@chromium.org>
2021-07-02 08:15:20 +0000

    full_restore: Save local bounds for ARC app window
    ARC app window needs bounds in display to restore app window position
    correctly. This change save this info to ArcExtraInfo.
    
    BUG=1222933
    BUG=b:177405617
    
    Change-Id: I7b5772f80a840b5d8016e13194e9311eaa2d9f59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991079
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898069}

--- components/full_restore/app_restore_data.cc
+++ components/full_restore/app_restore_data.cc
@@ -35,6 +35,7 @@ constexpr char kPreMinimizedShowStateTypeKey[] = "pre_min_state";
 constexpr char kMinimumSizeKey[] = "min_size";
 constexpr char kMaximumSizeKey[] = "max_size";
 constexpr char kTitleKey[] = "title";
+constexpr char kBoundsInRoot[] = "bounds_in_root";
 constexpr char kPrimaryColorKey[] = "primary_color";
 constexpr char kStatusBarColorKey[] = "status_bar_color";
 
@@ -254,6 +255,7 @@ AppRestoreData::AppRestoreData(base::Value&& value) {
   maximum_size = GetSizeFromDict(*data_dict, kMaximumSizeKey);
   minimum_size = GetSizeFromDict(*data_dict, kMinimumSizeKey);
   title = GetU16StringValueFromDict(*data_dict, kTitleKey);
+  bounds_in_root = GetBoundsRectFromDict(*data_dict, kBoundsInRoot);
   primary_color = GetUIntValueFromDict(*data_dict, kPrimaryColorKey);
   status_bar_color = GetUIntValueFromDict(*data_dict, kStatusBarColorKey);
 
@@ -336,6 +338,9 @@ std::unique_ptr<AppRestoreData> AppRestoreData::Clone() const {
 
   if (title.has_value())
     data->title = title.value();
+
+  if (bounds_in_root.has_value())
+    data->bounds_in_root = bounds_in_root.value();
 
   if (primary_color.has_value())
     data->primary_color = primary_color.value();
@@ -429,6 +434,11 @@ base::Value AppRestoreData::ConvertToValue() const {
     launch_info_dict.SetStringKey(kTitleKey, base::UTF16ToUTF8(title.value()));
   }
 
+  if (bounds_in_root.has_value()) {
+    launch_info_dict.SetKey(kBoundsInRoot,
+                            ConvertRectToValue(bounds_in_root.value()));
+  }
+
   if (primary_color.has_value()) {
     launch_info_dict.SetKey(kPrimaryColorKey,
                             ConvertUintToValue(primary_color.value()));
@@ -470,6 +480,7 @@ void AppRestoreData::ModifyWindowInfo(const WindowInfo& window_info) {
     minimum_size = window_info.arc_extra_info->minimum_size;
     maximum_size = window_info.arc_extra_info->maximum_size;
     title = window_info.arc_extra_info->title;
+    bounds_in_root = window_info.arc_extra_info->bounds_in_root;
   }
 }
 
@@ -489,6 +500,7 @@ void AppRestoreData::ClearWindowInfo() {
   minimum_size.reset();
   maximum_size.reset();
   title.reset();
+  bounds_in_root.reset();
   primary_color.reset();
   status_bar_color.reset();
 }
@@ -534,11 +546,12 @@ std::unique_ptr<WindowInfo> AppRestoreData::GetWindowInfo() const {
   }
 
   if (maximum_size.has_value() || minimum_size.has_value() ||
-      title.has_value()) {
+      title.has_value() || bounds_in_root.has_value()) {
     window_info->arc_extra_info = WindowInfo::ArcExtraInfo();
     window_info->arc_extra_info->maximum_size = maximum_size;
     window_info->arc_extra_info->minimum_size = minimum_size;
     window_info->arc_extra_info->title = title;
+    window_info->arc_extra_info->bounds_in_root = bounds_in_root;
   }
 
   // Display id is set as the app launch parameter, so we don't need to return
