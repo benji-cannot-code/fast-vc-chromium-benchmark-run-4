29d3da72bd34b051d6d34fa385a815c731eeca0d
Anupam Snigdha <snianu@microsoft.com>
2021-07-02 22:26:53 +0000

    [VirtualKeyboard] Add WPT tests for the VK feature.
    This patch adds wpt tests for the new VK API.
    Explainers and Design Doc:
    https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/VirtualKeyboardPolicy/explainer.md
    https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/VirtualKeyboardAPI/explainer.md
    https://docs.google.com/document/d/1I0LUNxK_gP5IaNQsbYN6gL6Zpm71XzduCKkublU5o5Q/edit?usp=sharing
    
    Bug: 856269
    
    Change-Id: I4f3ef35f71499a45e53e3fe79dd1df59257b8e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2993938
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898372}

---
+++ third_party/blink/web_tests/external/wpt/virtual-keyboard/idlharness.https.window.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: timeout=long
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['virtual-keyboard.tentative'],
+  ['html', 'dom'],
+  idl_array => {
+    idl_array.add_objects({
+      Navigator: ['navigator'],
+      VirtualKeyboard: ['navigator.virtualKeyboard'],
+      GeometryChangeEvent: ['new GeometryChangeEvent("x")'],
+    });
+  }
+);
---
+++ third_party/blink/web_tests/virtual/virtual-keyboard/README.md
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# VirtualKeyboard
+This directory contains (tentative) tests for the [VirtualKeyboard policy](https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/VirtualKeyboardPolicy/explainer.md) & [VirtualKeyboard APIs](https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/VirtualKeyboardAPI/explainer.md) proposals.
+
+**This suite runs the tests with** `--enable-features=VirtualKeyboard`
