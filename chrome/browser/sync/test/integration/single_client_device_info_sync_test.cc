899c0b0d046f3a16730f75fa8c431964677e53f8
Rushan Suleymanov <rushans@google.com>
2021-07-02 13:26:52 +0000

    [Sync] Reupload local device info on incoming tombstone
    The reupload takes place at most once per session when the data type is
    started. This is needed to prevent uncontrolled reuploads which base on
    incoming updates.
    
    Fixed: 1225961
    Change-Id: I341a53ba36d17716c7ce0b2598d273becf359156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001364
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#898138}

--- chrome/browser/sync/test/integration/single_client_device_info_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_device_info_sync_test.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/sync_prefs.h"
 #include "components/sync/base/time.h"
 #include "components/sync/driver/sync_driver_switches.h"
+#include "components/sync/engine/loopback_server/persistent_tombstone_entity.h"
 #include "components/sync/invalidations/switches.h"
 #include "components/sync/protocol/proto_value_conversions.h"
 #include "components/sync/protocol/sync.pb.h"
@@ -316,6 +317,25 @@ IN_PROC_BROWSER_TEST_F(SingleClientDeviceInfoSyncTest,
   GetFakeServer()->GetLastCommitMessage(&message);
 
   EXPECT_FALSE(message.commit().config_params().single_client());
+}
+
+IN_PROC_BROWSER_TEST_F(SingleClientDeviceInfoSyncTest,
+                       ShouldReuploadLocalDeviceIfRemovedFromServer) {
+  ASSERT_TRUE(SetupSync());
+
+  const std::vector<sync_pb::SyncEntity> server_device_infos =
+      GetFakeServer()->GetSyncEntitiesByModelType(syncer::DEVICE_INFO);
+  ASSERT_THAT(server_device_infos, Contains(HasCacheGuid(GetLocalCacheGuid())));
+
+  GetFakeServer()->InjectEntity(
+      syncer::PersistentTombstoneEntity::CreateFromEntity(
+          server_device_infos.front()));
+
+  // On receiving the tombstone, the client should reupload its own device info.
+  EXPECT_TRUE(
+      ServerDeviceInfoMatchChecker(
+          GetFakeServer(), ElementsAre(HasCacheGuid(GetLocalCacheGuid())))
+          .Wait());
 }
 
 // PRE_* tests aren't supported on Android browser tests.
