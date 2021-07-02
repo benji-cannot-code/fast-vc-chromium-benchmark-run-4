97e183ad6f69dd654a4e7af9931b9df4149d40c4
Min Qin <qinmin@chromium.org>
2021-07-02 19:18:45 +0000

    Enable MIME sniffing for download not triggered by navigation
    For download triggered by navigation, the URLLoader was created with
    MIME sniffing enabled: https://source.chromium.org/chromium/chromium/src/+/main:content/browser/loader/navigation_url_loader_impl.cc;l=295.
    However, for download initiated from a link or context menu, MIME sniffing
    was not enabled. This CL enabled MIME sniffing for such download.
    
    BUG=1223250
    
    Change-Id: I18934fd590f377d6b60fde896a2e77afe2cc35b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3000735
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898283}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -2422,7 +2422,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
                          "  \"incognito\": false,"
                          "  \"bytesReceived\": 0.0,"
                          "  \"fileSize\": 0.0,"
-                         "  \"mime\": \"\","
+                         "  \"mime\": \"text/plain\","
                          "  \"paused\": false,"
                          "  \"url\": \"%s\"}]",
                          download_url.c_str())));
@@ -2561,7 +2561,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
       item, download::DOWNLOAD_INTERRUPT_REASON_SERVER_BAD_CONTENT,
       base::StringPrintf("[{\"danger\": \"safe\","
                          "  \"incognito\": false,"
-                         "  \"mime\": \"\","
+                         "  \"mime\": \"text/plain\","
                          "  \"paused\": false,"
                          "  \"id\": %d,"
                          "  \"url\": \"%s\"}]",
@@ -2602,7 +2602,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
       item, download::DOWNLOAD_INTERRUPT_REASON_SERVER_BAD_CONTENT,
       base::StringPrintf("[{\"danger\": \"safe\","
                          "  \"incognito\": false,"
-                         "  \"mime\": \"\","
+                         "  \"mime\": \"text/plain\","
                          "  \"paused\": false,"
                          "  \"id\": %d,"
                          "  \"url\": \"%s\"}]",
