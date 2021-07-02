5fb7b44ecf51cde01a8ee5defd4cf83054aad7c4
David Bertoni <dbertoni@chromium.org>
2021-07-02 21:08:03 +0000

    [CodeHealth] Remove uses of ListValue::DeepCopy() in /chrome/test/chromedriver.
    Where possible, the code is migrated to use Value::Clone(). Where
    that's not practical, calls are replaced with
    ListValue::CreateDeepCopy().
    
    This CL was uploaded by git cl split.
    
    R=johnchen@chromium.org
    
    Bug: 1187108
    Change-Id: Id1b142a6f2faa683d180feb8b104613dcffacfc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003073
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898336}

--- chrome/test/chromedriver/devtools_events_logger.h
+++ chrome/test/chromedriver/devtools_events_logger.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class DevToolsEventsLogger : public DevToolsEventListener {
  public:
   // Creates a |DevToolsEventsLogger| with specific preferences.
-  DevToolsEventsLogger(Log* log, const base::ListValue* prefs);
+  DevToolsEventsLogger(Log* log, const base::Value& prefs);
 
   ~DevToolsEventsLogger() override;
 
@@ -39,7 +39,7 @@ class DevToolsEventsLogger : public DevToolsEventListener {
  private:
   Log* log_;  // The log where to create entries.
 
-  const base::ListValue* prefs_;
+  const base::Value& prefs_;
   std::unordered_set<std::string> events_;
 
   DISALLOW_COPY_AND_ASSIGN(DevToolsEventsLogger);
