62127bcf7b22772ea48e50195bd21bed5a896ba5
Vasilii Sukhanov <vasilii@chromium.org>
2021-07-02 08:38:21 +0000

    Google Mobile Services for passwords.
    Bug: 1217070
    Change-Id: I737705b4cd9393c1ac1bedeea2dca8fa13635033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001582
    Auto-Submit: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#898078}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -2642,7 +2642,8 @@ const char kTurnOffStreamingMediaCachingAlwaysDescription[] =
     "Reduces disk activity during media playback, which can result in "
     "power savings.";
 
-const char kUnifiedPasswordManagerAndroidName[] = "Unified Password Manager";
+const char kUnifiedPasswordManagerAndroidName[] =
+    "Google Mobile Services for passwords";
 const char kUnifiedPasswordManagerAndroidDescription[] =
     "Uses Google Mobile Services to store and retrieve passwords."
     "Warning: Highly experimental. May lead to loss of passwords and "
