58995097ccd730dc99b34c9deea48d86955ea3dc
Tommy Li <tommycli@chromium.org>
2021-07-02 17:51:23 +0000

    [memories] Separate remote-backend tests from service tests.
    Currently, every unit test within HistoryClustersServiceTest goes
    through the remote backend.
    
    In that sense, every test becomes an integration test through the remote
    backend.
    
    Since we are moving to the on-device backend as being the default
    implementation, this test moves some items to use a
    TestClusteringBackend so we can test just the service behavior without
    going through the remote backend.
    
    Some remote tests are still there although we may reduce their number
    or combine them over time (in the future).
    
    Bug: 1171352, 1225511
    Change-Id: I5c7fbf55d99ac2e73969603acc40c5c37944c43e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001413
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898242}

--- components/history_clusters/core/history_clusters_service_test_api.h
+++ components/history_clusters/core/history_clusters_service_test_api.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history/core/test/history_service_test_util.h"
+#include "components/history_clusters/core/clustering_backend.h"
 #include "components/history_clusters/core/history_clusters_service.h"
 
 namespace history_clusters {
@@ -39,6 +40,17 @@ class HistoryClustersServiceTestApi {
     history::BlockUntilHistoryProcessesPendingRequests(history_service_);
 
     return result;
+  }
+
+  void SetClusteringBackendForTest(std::unique_ptr<ClusteringBackend> backend) {
+    DCHECK(backend.get());
+
+    history_clusters_service_->backend_ = std::move(backend);
+    // TODO(tommycli): Eliminate this `backend_weak_factory_` idiom. It's error
+    // prone, and I think we can work around the need for it.
+    history_clusters_service_->backend_weak_factory_ =
+        std::make_unique<base::WeakPtrFactory<ClusteringBackend>>(
+            history_clusters_service_->backend_.get());
   }
 
   HistoryClustersService* const history_clusters_service_;
