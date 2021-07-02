6f849dff86966c087cb317b22a8da22fe3aa8ab5
sandromaggi <sandromaggi@google.com>
2021-07-02 09:30:51 +0000

    [Autofill Assistant] Remove non-empty fields from response
    The CUD used to collect non-empty fields and send them in the
    response to the backend. This information was never used.
    
    The logic has since been inverted (i.e. the CUD enforcing
    a certain set of data).
    
    There is no reason anymore to send this data.
    
    Bug: b/192326675
    Change-Id: I6da382c9c5af5893221e791e534ca7363349adca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001346
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#898092}

--- components/autofill_assistant/browser/actions/collect_user_data_action.cc
+++ components/autofill_assistant/browser/actions/collect_user_data_action.cc
@@ -389,17 +389,6 @@ void SetInitialUserDataForAdditionalSection(
   }
 }
 
-void AddNonEmptyFieldNames(
-    const autofill::AutofillProfile* profile,
-    google::protobuf::RepeatedPtrField<std::string>* dest) {
-  DCHECK(profile != nullptr);
-  const auto& map = autofill_assistant::field_formatter::CreateAutofillMappings(
-      *profile, /* locale= */ "en-US");
-  for (const auto& it : map) {
-    *dest->Add() = it.first;
-  }
-}
-
 }  // namespace
 
 namespace autofill_assistant {
@@ -1157,11 +1146,6 @@ void CollectUserDataAction::WriteProcessedAction(UserData* user_data,
         contact_details_proto.contact_details_name());
 
     if (selected_profile != nullptr) {
-      AddNonEmptyFieldNames(
-          selected_profile,
-          processed_action_proto_->mutable_collect_user_data_result()
-              ->mutable_non_empty_contact_field());
-
       if (contact_details_proto.request_payer_name()) {
         Metrics::RecordPaymentRequestFirstNameOnly(
             selected_profile->GetRawInfo(autofill::NAME_LAST).empty());
@@ -1180,11 +1164,6 @@ void CollectUserDataAction::WriteProcessedAction(UserData* user_data,
     auto* selected_shipping_address = user_data->selected_address(
         proto().collect_user_data().shipping_address_name());
     if (selected_shipping_address != nullptr) {
-      AddNonEmptyFieldNames(
-          selected_shipping_address,
-          processed_action_proto_->mutable_collect_user_data_result()
-              ->mutable_non_empty_shipping_address_field());
-
       profiles_used.emplace(selected_shipping_address);
     }
   }
@@ -1192,11 +1171,6 @@ void CollectUserDataAction::WriteProcessedAction(UserData* user_data,
     auto* selected_billing_address = user_data->selected_address(
         proto().collect_user_data().billing_address_name());
     if (selected_billing_address != nullptr) {
-      AddNonEmptyFieldNames(
-          selected_billing_address,
-          processed_action_proto_->mutable_collect_user_data_result()
-              ->mutable_non_empty_billing_address_field());
-
       profiles_used.emplace(selected_billing_address);
     }
   }
