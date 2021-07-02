16ab576e9bbfb6b900d35271c53f4579b008bc33
Gavin Williams <gavinwill@chromium.org>
2021-07-02 03:13:07 +0000

    scanning: Create multi-page action toolbar
    - Add to the toolbar to the scan preview window
    
    - Add 2 new icons to the scanning resources for the toolbar
    
    - The toolbar only shows for multi-page scans and has a static location.
      The location will be fixed in the next CLs
    
    - The page number text is added to the toolbar in the next CLs
    
    screenshot: http://screen/42BAcCgebY8VdSf
    
    Bug: 1210271
    Change-Id: I660cd5255e40577b2cb71bc365e90df3f135b804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999053
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898022}

---
+++ ash/webui/scanning/resources/action_toolbar.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './scanning_shared_css.js';
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @fileoverview
+ * 'action-toolbar' is a floating toolbar that contains post-scan page options.
+ */
+Polymer({
+  is: 'action-toolbar',
+
+  _template: html`{__html_template__}`,
+});
--- ash/webui/scanning/resources/scan_preview.js
+++ ash/webui/scanning/resources/scan_preview.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './action_toolbar.js';
 import './scanning_fonts_css.js';
 import './scanning_shared_css.js';
 import 'chrome://resources/polymer/v3_0/paper-progress/paper-progress.js';
@@ -87,6 +88,9 @@ Polymer({
       type: Number,
       value: null,
     },
+
+    /** @type {boolean} */
+    multiPageScanChecked: Boolean,
   },
 
   observers: [
