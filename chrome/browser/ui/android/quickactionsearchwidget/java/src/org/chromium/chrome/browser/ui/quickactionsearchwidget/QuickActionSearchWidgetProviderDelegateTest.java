82db947277ef65fa453952fc80c4073f03d2a1dc
Peter E Conn <peconn@chromium.org>
2021-07-02 12:01:29 +0000

    👮 Disable a QuickActionSearchWidgetProviderDelegateTest.
    Change-Id: I6a587f8d6b5c75fcf20b2ce25db4d4729425ad29
    Bug: 1225949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003142
    Owners-Override: Peter Conn <peconn@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898123}

--- chrome/browser/ui/android/quickactionsearchwidget/java/src/org/chromium/chrome/browser/ui/quickactionsearchwidget/QuickActionSearchWidgetProviderDelegateTest.java
+++ chrome/browser/ui/android/quickactionsearchwidget/java/src/org/chromium/chrome/browser/ui/quickactionsearchwidget/QuickActionSearchWidgetProviderDelegateTest.java
@@ -20,6 +20,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.quickactionsearchwidget.QuickActionSearchWidgetReceiver;
@@ -101,6 +102,7 @@ public class QuickActionSearchWidgetProviderDelegateTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1225949")
     public void testDinoButtonClick() {
         for (View view : mWidgetViews) {
             // clang-format off
