26a9e27864699b7866d5b715ab59140f540868b4
Rohit Rao <rohitrao@chromium.org>
2021-07-02 18:45:28 +0000

    [ios] Fixes flake in PolicyURLBlockingTestCase.
    When the URLAllowlist/URLBlocklist policies are updated, the new
    blocklist is generated on a background thread. This means that the
    policy update is asynchronous, and if a page is loaded quickly enough
    after policies are updated, the load may not use the latest
    blocklist. This was causing some PolicyURLBlockingTestCase tests to
    flake.
    
    This CL adds EG2 helpers to wait for the blocklist to be updated before
    continuing with the page load.
    
    BUG=1226048
    
    Change-Id: Iee8bb317001ad5031e95f157ba5021308dc2bc51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002227
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898269}

--- ios/chrome/browser/policy/policy_app_interface.h
+++ ios/chrome/browser/policy/policy_app_interface.h
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Clear all policy values.
 + (void)clearPolicies;
 
+// Returns YES if the given |URL| is blocked by the URLBlocklist and
+// URLAllowlist policies.
++ (BOOL)isURLBlocked:(NSString*)URL;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_POLICY_POLICY_APP_INTERFACE_H_
