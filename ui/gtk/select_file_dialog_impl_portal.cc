89c4f588df6811b25800c1407cc5df2436a8d6c0
Maksim Ivanov <emaxx@chromium.org>
2021-07-02 17:59:46 +0000

    Fix unsequenced moves in //ui/gtk/
    Fix potential issues in select_file_dialog_impl_portal.cc with the
    same object being used for reading and for std::move()'ing from
    within the same expression.
    
    Note: this specific problem will disappear once Chromium fully switches
    to C++17, due to a change introduced in the evaluation order in C++17.
    
      select_file_dialog_impl_portal.cc:607:
      'info' used after it was moved
      select_file_dialog_impl_portal.cc:609:
      move occurred here
      the use and move are unsequenced, i.e. there is no guarantee about the
      order in which they are evaluated
    
      select_file_dialog_impl_portal.cc:616:
      'info' used after it was moved
      select_file_dialog_impl_portal.cc:618:
      move occurred here
      the use and move are unsequenced, i.e. there is no guarantee about the
      order in which they are evaluated
    
    Bug: 1122844
    Change-Id: Ia8e8e86518039bb840199401fc4f823c6363adab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003147
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898245}

--- ui/gtk/select_file_dialog_impl_portal.cc
+++ ui/gtk/select_file_dialog_impl_portal.cc
@@ -608,8 +608,7 @@ void SelectFileDialogImplPortal::CompleteOpen(
   info->main_task_runner->PostTask(
       FROM_HERE,
       base::BindOnce(&SelectFileDialogImplPortal::CompleteOpenOnMainThread,
-                     base::Unretained(this), std::move(info),
-                     std::move(paths)));
+                     base::Unretained(this), info, std::move(paths)));
 }
 
 void SelectFileDialogImplPortal::CancelOpen(scoped_refptr<DialogInfo> info) {
@@ -617,7 +616,7 @@ void SelectFileDialogImplPortal::CancelOpen(scoped_refptr<DialogInfo> info) {
   info->main_task_runner->PostTask(
       FROM_HERE,
       base::BindOnce(&SelectFileDialogImplPortal::CancelOpenOnMainThread,
-                     base::Unretained(this), std::move(info)));
+                     base::Unretained(this), info));
 }
 
 void SelectFileDialogImplPortal::CompleteOpenOnMainThread(
