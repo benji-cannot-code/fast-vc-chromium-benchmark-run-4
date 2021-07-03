fbd17b9e85a065ff8ba79f9e1c41797b4bd41b4f
Asanka Herath <asanka@chromium.org>
2021-07-03 22:14:03 +0000

    [privacy_budget] Introduce RepresentativeSurface.
    A "representative" surface is a strong alias for an IdentifiableSurface
    that's used by the surface valuation system.
    
    Bug: 1224752
    Change-Id: Ia02c07aba68caa321dda46f3b6ebbb498cc40d41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2997206
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/master@{#898430}

---
+++ chrome/browser/privacy_budget/representative_surface_set.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/privacy_budget/representative_surface_set.h"
+
+#include "base/strings/string_number_conversions.h"
+#include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
+
+std::string RepresentativeSurfaceToString(const RepresentativeSurface& v) {
+  return base::NumberToString(v.value().ToUkmMetricHash());
+}
+
+size_t RepresentativeSurfaceHash::operator()(
+    const RepresentativeSurface& s) const {
+  blink::IdentifiableSurfaceHash hasher;
+  return hasher(s.value());
+}
