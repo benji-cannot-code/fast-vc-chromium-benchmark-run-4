25af69f0efb34c7121ddf2eb1e301fa183db19e4
Alex Danilo <adanilo@chromium.org>
2021-07-05 03:03:15 +0000

    Files SWA: Stop system notifications if feature flag is off
    Bug: b/187656842
    Change-Id: I6595abe2403adfadee27cd4144ffa974a05c55fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002790
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898501}

--- chrome/browser/chromeos/extensions/file_manager/system_notification_manager.cc
+++ chrome/browser/chromeos/extensions/file_manager/system_notification_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/extensions/file_manager/system_notification_manager.h"
 
+#include "ash/constants/ash_features.h"
 #include "base/bind.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
@@ -15,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace file_manager {
 
 SystemNotificationManager::SystemNotificationManager(Profile* profile)
-    : profile_(profile) {}
+    : profile_(profile),
+      swa_enabled_(ash::features::IsFileManagerSwaEnabled()) {}
 
 SystemNotificationManager::~SystemNotificationManager() = default;
 
@@ -84,6 +86,9 @@ void SystemNotificationManager::Dismiss(const std::string& notification_id) {
 
 void SystemNotificationManager::HandleDeviceEvent(
     const file_manager_private::DeviceEvent& event) {
+  if (!swa_enabled_) {
+    return;
+  }
   std::unique_ptr<message_center::Notification> notification;
 
   const char* id = file_manager_private::ToString(event.type);
@@ -152,6 +157,9 @@ void SystemNotificationManager::HandleEvent(const extensions::Event& event) {}
 void SystemNotificationManager::HandleCopyStart(
     int copy_id,
     file_manager_private::CopyOrMoveProgressStatus& status) {
+  if (!swa_enabled_) {
+    return;
+  }
   if (status.size) {
     required_copy_space_[copy_id] = *status.size;
   }
@@ -164,6 +172,9 @@ namespace file_manager_private = extensions::api::file_manager_private;
 void SystemNotificationManager::HandleCopyEvent(
     int copy_id,
     file_manager_private::CopyOrMoveProgressStatus& status) {
+  if (!swa_enabled_) {
+    return;
+  }
   std::unique_ptr<message_center::Notification> notification;
   int progress = 0;
   std::string id =
