f57d52bf6cc3da3f80b5c70483d42d8ec9933746
Adrienne Walker <enne@chromium.org>
2021-07-05 14:36:06 +0000

    Convert /chrome/browser/ash/arc to ScopedDisplayObserver
    This is a refactoring to make it easier for future changes to
    DisplayObserver, and to clean up manual Add/Remove calls with RAII.
    
    ScopedDisplayObserver is an RAII class that directly replaces
    display::Screen::Add/RemoveObserver.
    
    ScopedOptionalDisplayObserver is the same thing, but handles the case
    where display::Screen::GetScreen() is null.  This can happen in tests.
    
    This is a non-functional change, intending to replicate existing
    behavior exactly.
    
    This CL was uploaded by git cl split.
    
    R=yhanada@chromium.org
    
    Bug: 1225463
    Change-Id: Ideb68e8a363e7cae04d1f7afef3b0d2f7c6ede9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004129
    Auto-Submit: enne <enne@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898615}

--- chrome/browser/ash/arc/arc_support_host.cc
+++ chrome/browser/ash/arc/arc_support_host.cc
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/chromeos/devicetype_utils.h"
-#include "ui/display/screen.h"
 
 using sync_pb::UserConsentTypes;
 
@@ -446,9 +445,7 @@ void ArcSupportHost::SetMessageHost(arc::ArcSupportMessageHost* message_host) {
     DisconnectMessageHost();
   message_host_ = message_host;
   message_host_->SetObserver(this);
-  display::Screen* screen = display::Screen::GetScreen();
-  if (screen)
-    screen->AddObserver(this);
+  display_observer_.emplace(this);
 
   if (!Initialize()) {
     Close();
@@ -485,9 +482,7 @@ void ArcSupportHost::UnsetMessageHost(
 
 void ArcSupportHost::DisconnectMessageHost() {
   DCHECK(message_host_);
-  display::Screen* screen = display::Screen::GetScreen();
-  if (screen)
-    screen->RemoveObserver(this);
+  display_observer_.reset();
   message_host_->SetObserver(nullptr);
   message_host_ = nullptr;
 }
