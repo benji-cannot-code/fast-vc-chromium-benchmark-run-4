c3e74d559d2d0fa0f50069096cc30bb715304983
Luciano Pacheco <lucmult@chromium.org>
2021-07-05 02:34:53 +0000

    Reland "Files SWA: Fix missing translations"
    This is a reland of 2ef25a4b4addb5e33e734838ec6276c86667fc66
    
    Test FileManagerJsTest.FilesDisplayPanel was failing because the
    implementation changed the translation from "$i18n{FILE_ERROR_GENERIC}"
    to just "FILE_ERROR_GENERIC", so the unittest had to be updated
    accordingly.
    
    Fix:
    https://chromium-review.googlesource.com/c/chromium/src/+/3004121/1..2
    
    Original change's description:
    > Files SWA: Fix missing translations
    >
    > The SWA uses source->EnableReplaceI18nInJS() which only replaces
    > translation strings within the template markup:
    > <!--_html_template_start_--> and <!--_html_template_end_-->.
    >
    > To fix the translation this CL move all HTML from the JS file to a
    > HTML file and use html_to_js() build rule to merge the content in the
    > JS.  This build rule already appends the required markup around the
    > template.  This makes all JS files consistently using html_to_js().
    >
    > Some translations are updated to use the runtime translation function
    > str().
    >
    > Bug: b/187783576 1225571
    > Change-Id: I1ee9f5d91baa167c508c0bfa62f0121db51538ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999922
    > Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Bo Majewski <majewski@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#898039}
    
    Bug: b/187783576 1225571
    Change-Id: Ieb96930f36ec96eeba3a6f8aadbc8e5c2e296ab9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004121
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898495}

--- ui/file_manager/file_manager/foreground/elements/files_xf_elements_unittest.m.js
+++ ui/file_manager/file_manager/foreground/elements/files_xf_elements_unittest.m.js
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {assert} from 'chrome://resources/js/assert.m.js';
-import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://test/chai_assert.js';
 
 import {util} from '../../common/js/util.js';
@@ -219,7 +218,7 @@ export function testFilesDisplayPanelErrorText() {
 
   // Check the default primary text displays a generic error message.
   // Note, the i18n message gets smooshed into 'An error occurred.' in the app.
-  assertEquals('$i18n{FILE_ERROR_GENERIC}', panelItem.primaryText);
+  assertEquals('FILE_ERROR_GENERIC', panelItem.primaryText);
 
   // Check the secondary text is empty.
   assertEquals('', panelItem.secondaryText);
