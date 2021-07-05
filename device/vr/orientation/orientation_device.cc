cfb9aaa855474bb6540b7172676974ce8b8446c1
Ivan Murashov <ivan.murashov@lge.com>
2021-07-05 07:00:28 +0000

    GCC: Fix initialization for NoDestructor<std::vector<T>>
    GCC requires double brace-initialization for
    NoDestructor<std::vector<T>>.
    Otherwise it produces the errors like:
    ../../device/vr/orientation/orientation_device.cc:48:4: error:
    call of overloaded 'NoDestructor(<brace-enclosed initializer list>)'
    is ambiguous
    ../../base/no_destructor.h:62:3: note: candidate:
    'base::NoDestructor<T>::NoDestructor(const base::NoDestructor<T>&)
    [with T = std::__1::vector<device::mojom::XRSessionFeature>]' <deleted>
    ../../base/no_destructor.h:60:12: note: candidate:
    'base::NoDestructor<T>::NoDestructor(T&&)
    [with T = std::__1::vector<device::mojom::XRSessionFeature>]'
    ../../base/no_destructor.h:59:12: note: candidate:
    'base::NoDestructor<T>::NoDestructor(const T&)
    [with T = std::__1::vector<device::mojom::XRSessionFeature>]'
    
    Bug: 995377, 819294
    Change-Id: I349f302d7034ca531035444ab3c5ed1ef552dde7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001889
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/master@{#898533}

--- device/vr/orientation/orientation_device.cc
+++ device/vr/orientation/orientation_device.cc
@@ -42,11 +42,11 @@ display::Display::Rotation GetRotation() {
 
 const std::vector<mojom::XRSessionFeature>& GetSupportedFeatures() {
   static base::NoDestructor<std::vector<mojom::XRSessionFeature>>
-      kSupportedFeatures({
+      kSupportedFeatures{{
     mojom::XRSessionFeature::REF_SPACE_VIEWER,
     mojom::XRSessionFeature::REF_SPACE_LOCAL,
     mojom::XRSessionFeature::REF_SPACE_LOCAL_FLOOR,
-  });
+  }};
 
   return *kSupportedFeatures;
 }
