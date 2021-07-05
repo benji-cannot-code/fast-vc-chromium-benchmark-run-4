c4c94aec49512a7e1ce722a6831840d35b016753
Jesse McKenna <jessemckenna@google.com>
2021-07-05 18:52:51 +0000

    breadcrumbs: ensure non-null persistent storage mgr
    When GetStoredEvents() is called, the persistent storage manager
    should never be null. This change enforces that expectation with a
    DCHECK rather than an if block.
    
    Change-Id: I6cdd8dfb642b215e36b451df733d63a16fde599a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002357
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898671}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -1014,12 +1014,9 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 
   breadcrumbs::BreadcrumbPersistentStorageManager* persistentStorageManager =
       GetApplicationContext()->GetBreadcrumbPersistentStorageManager();
+  DCHECK(persistentStorageManager);
 
-  // Application context can return a null persistent storage manager if
-  // breadcrumbs are not being persisted.
-  if (persistentStorageManager) {
-    breadcrumbService->StartPersisting(persistentStorageManager);
-  }
+  breadcrumbService->StartPersisting(persistentStorageManager);
 
   // Get stored persistent breadcrumbs from last run to set on crash reports.
   persistentStorageManager->GetStoredEvents(
