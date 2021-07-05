1914fe992b05c1b6e5659239ed000d75246e1d42
Muhammad Hasan Khan <mhasank@chromium.org>
2021-07-05 18:36:39 +0000

    arc: Add metrics for ARC re-auth scenarios
    There are situations when ARC loses its android id or primary account.
    We want to monitor the frequency of those cases as we try to recover
    the device state via re-initiating the auth flow.
    
    BUG=b:177476012
    TEST=CQ
    
    Change-Id: I0c72d3d8f8f1d54ec3b5f00d5ca054e120282ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002493
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Muhammad Hasan Khan <mhasank@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898669}

--- chrome/browser/ash/arc/auth/arc_auth_service.cc
+++ chrome/browser/ash/arc/auth/arc_auth_service.cc
@@ -371,6 +371,10 @@ void ArcAuthService::ReportAccountCheckStatus(
   UpdateAuthAccountCheckStatus(status, profile_);
 }
 
+void ArcAuthService::ReportAccountReauthReason(mojom::ReauthReason reason) {
+  UpdateAccountReauthReason(reason, profile_);
+}
+
 void ArcAuthService::ReportManagementChangeStatus(
     mojom::ManagementChangeStatus status) {
   UpdateSupervisionTransitionResultUMA(status);
