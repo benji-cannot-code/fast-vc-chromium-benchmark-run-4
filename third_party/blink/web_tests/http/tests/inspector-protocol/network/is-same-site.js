016d755c15aa41abf63bc67f636c1b77c04c7fc4
Danil Somsikov <dsv@chromium.org>
2021-07-05 07:47:12 +0000

    Fix same site flag for the main frame.
    Bug: 1057457
    Change-Id: Ib14b218cc15209e81d32b4e65975f7539b405b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001523
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898543}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/is-same-site.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/is-same-site.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
-  const {page, session, dp} = await testRunner.startBlank(
+  const {page, session, dp} = await testRunner.startURL(
+      'https://devtools.oopif.test:8443/',
       `Verifies that same site requests are marked as such`);
 
   await dp.Network.enable();
