13b57eb93e8f623eee61ff6b946f5129b84d67b1
Colin Blundell <blundell@chromium.org>
2021-07-05 12:38:48 +0000

    [Page load metrics] Disable flaky OriginStatusMetricEmbeddedEmpty test
    Bug: 1226500, 1210190
    Change-Id: I4341037e1b8b9638a8abee5c538952558c69b573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003153
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898594}

--- weblayer/browser/ads_page_load_metrics_observer_browsertest.cc
+++ weblayer/browser/ads_page_load_metrics_observer_browsertest.cc
@@ -90,8 +90,9 @@ IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverBrowserTest,
 }
 
 // Test that an empty embedded ad isn't reported at all.
+// TODO(crbug.com/1226500): This test is flaky.
 IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverBrowserTest,
-                       OriginStatusMetricEmbeddedEmpty) {
+                       DISABLED_OriginStatusMetricEmbeddedEmpty) {
   base::HistogramTester histogram_tester;
   NavigateAndWaitForCompletion(
       embedded_test_server()->GetURL(
