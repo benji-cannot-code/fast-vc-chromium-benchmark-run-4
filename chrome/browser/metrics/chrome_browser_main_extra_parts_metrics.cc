3450c161d7cf8bb6264e5bf220369bc408fdc323
Adrienne Walker <enne@chromium.org>
2021-07-05 20:39:28 +0000

    Convert /chrome/browser/metrics to ScopedDisplayObserver
    This is a refactoring to make it easier for future changes to
    DisplayObserver, and to clean up manual Add/Remove calls with RAII.
    
    ScopedDisplayObserver is an RAII class that directly replaces
    display::Screen::Add/RemoveObserver.
    
    ScopedOptionalDisplayObserver is the same thing, but handles the case
    where display::Screen::GetScreen() is null.  This can happen in tests.
    
    This is a non-functional change, intending to replicate existing
    behavior exactly.
    
    This CL was uploaded by git cl split.
    
    R=bcwhite@chromium.org
    
    Bug: 1225463
    Change-Id: Id80ccbe1769a87a7796cd470abb257f44c49f0a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004003
    Auto-Submit: enne <enne@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898693}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -521,13 +521,10 @@ void RecordDisplayHDRStatus(const display::Display& display) {
 }  // namespace
 
 ChromeBrowserMainExtraPartsMetrics::ChromeBrowserMainExtraPartsMetrics()
-    : display_count_(0), is_screen_observer_(false) {
-}
+    : display_count_(0) {}
 
-ChromeBrowserMainExtraPartsMetrics::~ChromeBrowserMainExtraPartsMetrics() {
-  if (is_screen_observer_)
-    display::Screen::GetScreen()->RemoveObserver(this);
-}
+ChromeBrowserMainExtraPartsMetrics::~ChromeBrowserMainExtraPartsMetrics() =
+    default;
 
 void ChromeBrowserMainExtraPartsMetrics::PreProfileInit() {
   RecordMicroArchitectureStats();
@@ -770,9 +767,6 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
 #endif  // defined(OS_WIN)
 
   auto* screen = display::Screen::GetScreen();
-  screen->AddObserver(this);
-  is_screen_observer_ = true;
-
   display_count_ = screen->GetNumDisplays();
   base::UmaHistogramCounts100("Hardware.Display.Count.OnStartup",
                               display_count_);
@@ -780,6 +774,8 @@ void ChromeBrowserMainExtraPartsMetrics::PostBrowserStart() {
   for (const auto& display : screen->GetAllDisplays()) {
     RecordDisplayHDRStatus(display);
   }
+
+  display_observer_.emplace(this);
 
 #if !defined(OS_ANDROID)
   metrics::BeginFirstWebContentsProfiling();
