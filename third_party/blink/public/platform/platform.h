8faecf20d9adfd509c5dfecf7d0ad10072b6b3de
Fredrik Söderqvist <fs@opera.com>
2021-07-05 10:24:25 +0000

    Remove unnecessary includes from blink/public/platform/platform.h
    Replace includes of:
    
      services/network/public/cpp/shared_url_loader_factory.h and
      third_party/blink/public/platform/web_url_loader_factory.h
    
    with appropriate forward declarations - moving stub definitions to the
    .cc file as needed.
    
    These two are heaviest includes according to the include analysis.
    
    Bug: 242216
    Change-Id: Ia482954252ced219cdae34b30185ea2810d9d8d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001308
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#898575}

--- third_party/blink/public/platform/platform.h
+++ third_party/blink/public/platform/platform.h
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_latency.h"
 #include "media/base/audio_renderer_sink.h"
 #include "media/base/media_log.h"
-#include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/common/security/protocol_handler_security_level.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
@@ -62,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_data.h"
 #include "third_party/blink/public/platform/web_dedicated_worker_host_factory_client.h"
 #include "third_party/blink/public/platform/web_string.h"
-#include "third_party/blink/public/platform/web_url_loader_factory.h"
 #include "third_party/blink/public/platform/web_v8_value_converter.h"
 #include "third_party/blink/public/platform/websocket_handshake_throttle_provider.h"
 #include "third_party/webrtc/api/video/video_codec_type.h"
@@ -103,8 +101,10 @@ class BigBuffer;
 
 namespace network {
 namespace mojom {
+class URLLoaderFactory;
 class URLLoaderFactoryInterfaceBase;
 }
+class PendingSharedURLLoaderFactory;
 class SharedURLLoaderFactory;
 }
 
@@ -143,6 +143,7 @@ class WebResourceRequestSenderDelegate;
 class WebSandboxSupport;
 class WebSecurityOrigin;
 class WebThemeEngine;
+class WebURLLoaderFactory;
 class WebVideoCaptureImplManager;
 struct WebContentSecurityPolicyHeader;
 
@@ -293,9 +294,7 @@ class BLINK_PLATFORM_EXPORT Platform {
   // network::mojom::URLLoaderFactory.
   virtual std::unique_ptr<WebURLLoaderFactory> WrapURLLoaderFactory(
       CrossVariantMojoRemote<network::mojom::URLLoaderFactoryInterfaceBase>
-          url_loader_factory) {
-    return nullptr;
-  }
+          url_loader_factory);
 
   // Returns a new WebURLLoaderFactory that wraps the given
   // network::SharedURLLoaderFactory.
@@ -778,7 +777,7 @@ class BLINK_PLATFORM_EXPORT Platform {
           worker_timing_callback_task_runner,
       base::RepeatingCallback<
           void(int, mojo::PendingReceiver<blink::mojom::WorkerTimingContainer>)>
-          worker_timing_callback) {}
+          worker_timing_callback);
 
   // WebCrypto ----------------------------------------------------------
 
