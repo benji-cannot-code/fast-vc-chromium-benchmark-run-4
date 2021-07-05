3155c462a17a68e2aa692175e671875d95f2b624
Rachel Wong <wrong@chromium.org>
2021-07-05 04:02:15 +0000

    [files] Add atime boosts for local file search.
    This CL adds small score boosts for more recently accessed files. This
    will distinguish between files that have the same match relevance score.
    
    This requires calculating the relevance within the provider itself, so
    the CalculateRelevance method has been exposed from FileResult to be
    used by both the local file and Drive search providers.
    
    Change-Id: Ic142df3577fdc783caa724cef0969c3809fee9fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976455
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898514}

--- chrome/browser/ui/app_list/search/files/file_result.h
+++ chrome/browser/ui/app_list/search/files/file_result.h
@@ -33,8 +33,7 @@ class FileResult : public ChromeSearchResult {
              const base::FilePath& filepath,
              ResultType result_type,
              const std::u16string& query,
-             const absl::optional<chromeos::string_matching::TokenizedString>&
-                 tokenized_query,
+             float relevance,
              Type type,
              Profile* profile);
   ~FileResult() override;
@@ -44,6 +43,12 @@ class FileResult : public ChromeSearchResult {
 
   // ChromeSearchResult overrides:
   void Open(int event_flags) override;
+
+  // Calculates file's match relevance score. Will return a default score if the
+  // query is missing or the filename is empty.
+  static double CalculateRelevance(
+      const absl::optional<chromeos::string_matching::TokenizedString>& query,
+      const base::FilePath& filepath);
 
  private:
   FileResult(const std::string& schema,
