9ebf324e2a125623328445a89d6bef1355e92a61
siashah <siashah@chromium.org>
2021-07-05 18:12:46 +0000

    [VirtualCards] Update the error dialog content to contain custom message
    We need to show the error message in the dialog in red instead of the
    default text color, thus introducing a custom view for the content part
    of the error dialog.
    
    Also, updated the unit test to instrumentation test as we are now
    inflating layout using the LayoutInflater.
    
    Screenshot: https://screenshot.googleplex.com/5ZqZbykbxtq76jB.png
    
    Bug: 1196021
    Change-Id: I81bd1d3e488e296770ed588f9e7a68dce9384b7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998791
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898666}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AutofillErrorDialogBridge.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AutofillErrorDialogBridge.java
@@ -6,12 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.autofill;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.core.content.res.ResourcesCompat;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.ui.autofill.internal.R;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -71,11 +75,14 @@ public class AutofillErrorDialogBridge {
      */
     @CalledByNative
     public void show(String title, String description, String buttonLabel, int titleIconId) {
+        View errorDialogContentView =
+                LayoutInflater.from(mContext).inflate(R.layout.autofill_error_dialog, null);
+        ((TextView) errorDialogContentView.findViewById(R.id.error_message)).setText(description);
         PropertyModel.Builder builder =
                 new PropertyModel.Builder(ModalDialogProperties.ALL_KEYS)
                         .with(ModalDialogProperties.CONTROLLER, mModalDialogController)
                         .with(ModalDialogProperties.TITLE, title)
-                        .with(ModalDialogProperties.MESSAGE, description)
+                        .with(ModalDialogProperties.CUSTOM_VIEW, errorDialogContentView)
                         .with(ModalDialogProperties.NEGATIVE_BUTTON_DISABLED, true)
                         .with(ModalDialogProperties.POSITIVE_BUTTON_TEXT, buttonLabel);
         if (titleIconId != 0) {
