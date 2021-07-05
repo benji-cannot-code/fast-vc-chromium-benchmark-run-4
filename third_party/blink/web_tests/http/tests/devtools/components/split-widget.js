64757d831d9a5363ab919e2fc993d9f651a96f5d
Jan Scheffler <janscheffler@chromium.org>
2021-07-05 21:22:42 +0000

    Reenable tests after DevTools migration
    CL with actual changes: https://chromium-review.googlesource.com/2987978
    
    Bug: chromium:1207342
    Change-Id: I60aca021d894bcdd3549aa3200e697d59b13ccfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999982
    Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898698}

--- third_party/blink/web_tests/http/tests/devtools/components/split-widget.js
+++ third_party/blink/web_tests/http/tests/devtools/components/split-widget.js
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     TestRunner.addResult(
         '    Sidebar size = ' + sidebarSize + ', showMode = ' + splitWidget.showMode() + ', ' + orientation);
     TestRunner.addResult(
-        '    Setting value: ' + JSON.stringify(Common.settings.settingForTest(splitWidget._setting._name).get()));
+        '    Setting value: ' + JSON.stringify(Common.settings.settingForTest(splitWidget._setting.name).get()));
   }
 
   function testSplitWidgetSizes(useFraction, shouldSaveShowMode) {
