edb60cf328f96431dc6aff8cb4fa3099a50ec2af
Austin Tankiang <austinct@chromium.org>
2021-07-05 06:49:16 +0000

    Revert "[Demo Mode] Add ToggleFullscreen() Mojo API to Demo Mode SWA"
    This reverts commit 9bd26a1fb284a71658595870899e010158643db2.
    
    Reason for revert: Breaks Chrome uprev - see b/192805136
    
    Original change's description:
    > [Demo Mode] Add ToggleFullscreen() Mojo API to Demo Mode SWA
    >
    > We provide this separate API instead of using the standard document.EnterFullscreen()
    > method, because document.EnterFullscreen() requires user interaction,
    > whereas we want the app to start in fullscreen mode.
    >
    > Even if this isn't the final approach that we use for fullscreen, I
    > want to get this code checked in so that it's easier for others to try
    > out, as well as to get the Mojo glue checked in to start implementing
    > other APIs.
    >
    > Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome,linux-chrome,lacros-amd64-generic-chrome
    > Bug: b/178636059
    > Change-Id: I7d5e5b936e225119ba208bb3d83d25dbeb168938
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2827416
    > Reviewed-by: calamity <calamity@chromium.org>
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    > Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    > Cr-Commit-Position: refs/heads/master@{#898018}
    
    Bug: b/178636059, b/192805136
    Change-Id: Ie4aa730851efa3339e581b58f8b7c51e745e2016
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome,linux-chrome,lacros-amd64-generic-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3005490
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Owners-Override: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898532}

--- chromeos/components/demo_mode_app_ui/demo_mode_app_ui.h
+++ chromeos/components/demo_mode_app_ui/demo_mode_app_ui.h
@@ -6,16 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_COMPONENTS_DEMO_MODE_APP_UI_DEMO_MODE_APP_UI_H_
 #define CHROMEOS_COMPONENTS_DEMO_MODE_APP_UI_DEMO_MODE_APP_UI_H_
 
-#include "chromeos/components/demo_mode_app_ui/mojom/demo_mode_app_ui.mojom.h"
-#include "mojo/public/cpp/bindings/pending_receiver.h"
-#include "mojo/public/cpp/bindings/pending_remote.h"
-#include "mojo/public/cpp/bindings/receiver.h"
 #include "ui/webui/mojo_web_ui_controller.h"
 
 namespace chromeos {
 // The WebUI for chrome://demo-mode-app
-class DemoModeAppUI : public ui::MojoWebUIController,
-                      public mojom::demo_mode::PageHandlerFactory {
+class DemoModeAppUI : public ui::MojoWebUIController {
  public:
   explicit DemoModeAppUI(content::WebUI* web_ui);
   ~DemoModeAppUI() override;
@@ -23,19 +18,7 @@ class DemoModeAppUI : public ui::MojoWebUIController,
   DemoModeAppUI(const DemoModeAppUI&) = delete;
   DemoModeAppUI& operator=(const DemoModeAppUI&) = delete;
 
-  void BindInterface(
-      mojo::PendingReceiver<mojom::demo_mode::PageHandlerFactory> factory);
-
  private:
-  // mojom::DemoModePageHandlerFactory
-  void CreatePageHandler(
-      mojo::PendingReceiver<mojom::demo_mode::PageHandler> handler) override;
-
-  mojo::Receiver<mojom::demo_mode::PageHandlerFactory> demo_mode_page_factory_{
-      this};
-
-  std::unique_ptr<mojom::demo_mode::PageHandler> demo_mode_page_handler_;
-
   WEB_UI_CONTROLLER_TYPE_DECL();
 };
 
