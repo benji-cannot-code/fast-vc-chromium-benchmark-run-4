c9dcc1485e16650feb47cc876c537ca6c78ac0cd
Adrienne Walker <enne@chromium.org>
2021-07-05 03:39:51 +0000

    Convert /chromeos/components/camera_app_ui to ScopedDisplayObserver
    This is a refactoring to make it easier for future changes to
    DisplayObserver, and to clean up manual Add/Remove calls with RAII.
    
    ScopedDisplayObserver is an RAII class that directly replaces
    display::Screen::Add/RemoveObserver.
    
    ScopedOptionalDisplayObserver is the same thing, but handles the case
    where display::Screen::GetScreen() is null.  This can happen in tests.
    
    This is a non-functional change, intending to replicate existing
    behavior exactly.
    
    This CL was uploaded by git cl split.
    
    R=shik@chromium.org
    
    Bug: 1225463
    Change-Id: I168bc4e57fa9a3660f0b8186a7294b641d034594
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004099
    Auto-Submit: enne <enne@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898508}

--- chromeos/components/camera_app_ui/camera_app_helper_impl.h
+++ chromeos/components/camera_app_ui/camera_app_helper_impl.h
@@ -114,6 +114,8 @@ class CameraAppHelperImpl : public ash::TabletModeObserver,
   std::unique_ptr<chromeos::CameraAppWindowStateController>
       window_state_controller_;
 
+  display::ScopedDisplayObserver display_observer_{this};
+
   DISALLOW_COPY_AND_ASSIGN(CameraAppHelperImpl);
 };
 
