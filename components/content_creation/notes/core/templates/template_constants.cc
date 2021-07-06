dc0ee8b4ca1bb14b4f4cf9b22b43ce5e8efd0fb1
SebSG <sebsg@chromium.org>
2021-07-06 14:11:36 +0000

    [Stylize] Set specific max size for certain templates.
    See bug for video.
    
    Fixed: 1222795
    Change-Id: I856eec62c53b22a416b9f995f39d3ab07f175f61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3006371
    Auto-Submit: sebsg <sebsg@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898846}

--- components/content_creation/notes/core/templates/template_constants.cc
+++ components/content_creation/notes/core/templates/template_constants.cc
@@ -15,7 +15,9 @@ const uint16_t k400Weight = 400;
 const uint16_t k700Weight = 700;
 
 const int kDefaultMinTextSizeSP = 14;
-const int kDefaultMaxTextSizeSP = 26;
+const int kDefaultMaxTextSizeSP = 48;
+const int kFriendlyMaxSizeSP = 32;
+const int kBiggerMaxTextSizeSP = 60;
 
 const char kBebasNeueFontName[] = "Bebas Neue";
 const char kMansalvaFontName[] = "Mansalva";
@@ -84,7 +86,7 @@ NoteTemplate GetFriendlyTemplate() {
       TextStyle(kRockSaltFontName,
                 /*font_color=*/kGrey900Color, k400Weight,
                 /*all_caps=*/false, TextAlignment::kStart,
-                kDefaultMinTextSizeSP, kDefaultMaxTextSizeSP),
+                kDefaultMinTextSizeSP, kFriendlyMaxSizeSP),
       /*footer_style=*/CreateLightBackgroundFooterStyle());
 }
 
@@ -122,7 +124,7 @@ NoteTemplate GetImpactfulTemplate() {
       TextStyle(kBebasNeueFontName,
                 /*font_color=*/kBlackColor, k400Weight,
                 /*all_caps=*/true, TextAlignment::kCenter,
-                kDefaultMinTextSizeSP, kDefaultMaxTextSizeSP,
+                kDefaultMinTextSizeSP, kBiggerMaxTextSizeSP,
                 /*highlight_color=*/kWhiteColor, HighlightStyle::kHalf),
       /*footer_style=*/CreateLightBackgroundFooterStyle());
 }
@@ -150,7 +152,7 @@ NoteTemplate GetGroovyTemplate() {
       TextStyle(kBebasNeueFontName,
                 /*font_color=*/kYellow400Color, k400Weight,
                 /*all_caps=*/true, TextAlignment::kStart, kDefaultMinTextSizeSP,
-                kDefaultMaxTextSizeSP,
+                kBiggerMaxTextSizeSP,
                 /*highlight_color=*/kBlue900Color, HighlightStyle::kFull),
       /*footer_style=*/CreateDarkBackgroundFooterStyle());
 }
@@ -164,7 +166,7 @@ NoteTemplate GetMonochromeTemplate() {
       TextStyle(kBebasNeueFontName,
                 /*font_color=*/kWhiteColor, k400Weight,
                 /*all_caps=*/true, TextAlignment::kCenter,
-                kDefaultMinTextSizeSP, kDefaultMaxTextSizeSP),
+                kDefaultMinTextSizeSP, kBiggerMaxTextSizeSP),
       /*footer_style=*/CreateDarkBackgroundFooterStyle());
 }
 
@@ -176,7 +178,7 @@ NoteTemplate GetBoldTemplate() {
       TextStyle(kBebasNeueFontName,
                 /*font_color=*/kBlackColor, k400Weight,
                 /*all_caps=*/true, TextAlignment::kCenter,
-                kDefaultMinTextSizeSP, kDefaultMaxTextSizeSP,
+                kDefaultMinTextSizeSP, kBiggerMaxTextSizeSP,
                 /*highlight_color=*/kLightYellowColor, HighlightStyle::kHalf),
       /*footer_style=*/CreateLightBackgroundFooterStyle());
 }
