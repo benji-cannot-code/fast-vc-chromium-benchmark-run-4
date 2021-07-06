8041c08f8a7253d8d58ff2fe89872811ca2fc443
Hidehiko Abe <hidehiko@chromium.org>
2021-07-06 18:06:48 +0000

    Move Wayland Pointer and Keyboard focus management to WaylandWindowManager.
    To centerize the management code.
    
    BUG=1158779
    TEST=Ran on DUT.
    
    Change-Id: I6dc981c5fc5cd6e2ded9d3b19cb08c273be8a636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009379
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898912}

--- ui/ozone/platform/wayland/host/wayland_window_manager_unittests.cc
+++ ui/ozone/platform/wayland/host/wayland_window_manager_unittests.cc
@@ -100,6 +100,7 @@ TEST_P(WaylandWindowManagerTest, GetCurrentFocusedWindow) {
   Sync();
 
   EXPECT_FALSE(manager_->GetCurrentFocusedWindow());
+  EXPECT_FALSE(manager_->GetCurrentPointerFocusedWindow());
 
   auto* pointer = server_.seat()->pointer();
   ASSERT_TRUE(pointer);
@@ -112,12 +113,14 @@ TEST_P(WaylandWindowManagerTest, GetCurrentFocusedWindow) {
 
   EXPECT_FALSE(manager_->GetCurrentKeyboardFocusedWindow());
   EXPECT_TRUE(window1.get() == manager_->GetCurrentFocusedWindow());
+  EXPECT_TRUE(window1.get() == manager_->GetCurrentPointerFocusedWindow());
 
   wl_pointer_send_leave(pointer->resource(), 2, surface->resource());
 
   Sync();
 
   EXPECT_FALSE(manager_->GetCurrentFocusedWindow());
+  EXPECT_FALSE(manager_->GetCurrentPointerFocusedWindow());
 }
 
 TEST_P(WaylandWindowManagerTest, GetCurrentKeyboardFocusedWindow) {
