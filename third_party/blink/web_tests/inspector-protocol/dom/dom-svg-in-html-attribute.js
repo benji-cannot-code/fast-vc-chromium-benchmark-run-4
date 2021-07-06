78b26d9b72afcd21be36f0c8dab18bcaefd54e6f
Mathias Bynens <mathias@chromium.org>
2021-07-06 16:01:44 +0000

    Support “add attribute” for SVG-in-HTML
    Prior to this patch, any attribute modifications to SVG elements
    were interpreted as XML, even in the case of SVG-in-HTML.
    
    This patch corrects the behavior and ensures the HTML parser is used
    when appropriate.
    
    Fixed: chromium:947069
    Change-Id: Id3e1ceb44a1db72c5bd1cfd3f8dd82d930821990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003157
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898873}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-svg-in-html-attribute.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {dp} = await testRunner.startHTML(`
+    <svg></svg>
+  `, 'Test that SVG-in-HTML attribute modifications are treated as HTML, not XML');
+
+  let response = await dp.DOM.getDocument();
+  const rootNodeId = response.result.root.nodeId;
+
+  response = await dp.DOM.querySelector({nodeId: rootNodeId, selector: 'svg'});
+  const nodeId = response.result.nodeId;
+
+  testRunner.log('Original outerHTML:');
+  response = await dp.DOM.getOuterHTML({nodeId});
+  testRunner.log(response.result.outerHTML);
+
+  dp.DOM.setAttributesAsText({nodeId, name: 'xlink:href', text: 'xlink:href=""'});
+  response = await dp.DOM.onceAttributeModified();
+  testRunner.log('Modified attribute:');
+  testRunner.log(response.params.name + '=' + JSON.stringify(response.params.value));
+
+  testRunner.log('Modified outerHTML:');
+  response = await dp.DOM.getOuterHTML({nodeId});
+  testRunner.log(response.result.outerHTML);
+
+  testRunner.completeTest();
+});
