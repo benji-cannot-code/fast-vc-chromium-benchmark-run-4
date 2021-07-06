c360a6f185280fa782051e0b4e34f25cba78f3c0
Claudius van der Merwe <cvandermerwe@google.com>
2021-07-06 18:15:08 +0000

    [Adv Monitoring] Add Advertisement Monitor Service Provider
    This CL implement the org.bluez.AdvertisementMonitor1 BlueZ D-Bus
    interface, which allows applications to scan for low energy
    advertisements with filter criteria and get notified on the service
    provider.
    
    Note: This CL is part of an end-to-end prototype. It is gated behind
    a feature flag with the understanding that we will add unit tests and
    productionize before feature launch.
    
    Bug: 1223148
    Change-Id: I87b8b7587e6bd1f7df94f5fcb44b0139e8bfa280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987212
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898914}

---
+++ device/bluetooth/dbus/bluetooth_advertisement_monitor_service_provider.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/bluetooth/dbus/bluetooth_advertisement_monitor_service_provider.h"
+
+#include "base/memory/ptr_util.h"
+#include "device/bluetooth/dbus/bluetooth_advertisement_monitor_service_provider_impl.h"
+
+namespace bluez {
+
+BluetoothAdvertisementMonitorServiceProvider::
+    BluetoothAdvertisementMonitorServiceProvider() = default;
+
+BluetoothAdvertisementMonitorServiceProvider::
+    ~BluetoothAdvertisementMonitorServiceProvider() = default;
+
+// static
+std::unique_ptr<BluetoothAdvertisementMonitorServiceProvider>
+BluetoothAdvertisementMonitorServiceProvider::Create(
+    dbus::Bus* bus,
+    const dbus::ObjectPath& object_path,
+    std::unique_ptr<device::BluetoothLowEnergyScanFilter> filter,
+    base::WeakPtr<BluetoothAdvertisementMonitorServiceProvider::Delegate>
+        delegate) {
+  return std::make_unique<BluetoothAdvertisementMonitorServiceProviderImpl>(
+      bus, object_path, std::move(filter), delegate);
+}
+
+}  // namespace bluez
