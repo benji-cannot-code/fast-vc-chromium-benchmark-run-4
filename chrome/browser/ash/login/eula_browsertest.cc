7c02fee4e4a6e594d5b03ab2acce4c956bb02c23
Roman Sorokin <rsorokin@chromium.org>
2021-07-07 16:38:34 +0000

    EulaTest.AdditionalToS: Use focus waiter to check the focus
    I could not reproduce locally. So speculatively use focus waiter.
    
    Bug: 1220963
    Change-Id: Icb78c2e4383bf117cd453ee26594605832872815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009580
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#899159}

--- chrome/browser/ash/login/eula_browsertest.cc
+++ chrome/browser/ash/login/eula_browsertest.cc
@@ -351,13 +351,8 @@ IN_PROC_BROWSER_TEST_F(EulaTest, LearnMore) {
           1)));
 }
 
-#if defined(OS_CHROMEOS) && defined(NDEBUG)
-#define MAYBE_AdditionalToS DISABLED_AdditionalToS
-#else
-#define MAYBE_AdditionalToS AdditionalToS
-#endif
 // Tests that "Additional ToS" dialog could be opened and closed.
-IN_PROC_BROWSER_TEST_F(EulaTest, MAYBE_AdditionalToS) {
+IN_PROC_BROWSER_TEST_F(EulaTest, AdditionalToS) {
   base::HistogramTester histogram_tester;
   ShowEulaScreen();
 
@@ -375,7 +370,8 @@ IN_PROC_BROWSER_TEST_F(EulaTest, MAYBE_AdditionalToS) {
       .CreateWaiter(test::GetOobeElementPath(kAdditionalTermsDialog) +
                     ".open === false")
       ->Wait();
-  test::OobeJS().ExpectFocused(kAdditionalTermsLink);
+
+  test::OobeJS().CreateFocusWaiter(kAdditionalTermsLink)->Wait();
 
   EXPECT_THAT(
       histogram_tester.GetAllSamples("OOBE.EulaScreen.UserActions"),
