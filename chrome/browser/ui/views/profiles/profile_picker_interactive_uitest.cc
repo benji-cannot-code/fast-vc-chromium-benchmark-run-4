936aa4867c38897ac526d49f35a990ea734f8202
David Roger <droger@chromium.org>
2021-07-07 14:10:44 +0000

    [test] More expectations in ProfilePickerInteractiveUiTest.NavigateBackWithKeyboard
    This test is flaky, with a timeout failure which is hard to understand.
    To make the failures more actionable, this CL adds more expectations to
    the test. The goal is that the test will now fail sooner without hitting
    the timeout, and give more information about the failure cause.
    
    Bug: 1173544
    Change-Id: I8554941b90c2b8b3d918a0556d45f7d29f6ba57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3010376
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899117}

--- chrome/browser/ui/views/profiles/profile_picker_interactive_uitest.cc
+++ chrome/browser/ui/views/profiles/profile_picker_interactive_uitest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/profiles/profile_picker_test_base.h"
 #include "chrome/test/base/interactive_test_utils.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -214,13 +215,18 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerInteractiveUiTest,
 IN_PROC_BROWSER_TEST_F(ProfilePickerInteractiveUiTest,
                        MAYBE_NavigateBackWithKeyboard) {
   // Simulate walking through the flow starting at the picker so that navigating
-  // back to the picker makes sense.
+  // back to the picker makes sense. Check that the navigation list is populated
+  // correctly.
   ShowAndFocusPicker(ProfilePicker::EntryPoint::kProfileMenuManageProfiles);
   WaitForLoadStop(web_contents(), GURL("chrome://profile-picker"));
+  EXPECT_EQ(1, web_contents()->GetController().GetEntryCount());
+  EXPECT_EQ(0, web_contents()->GetController().GetLastCommittedEntryIndex());
   web_contents()->GetController().LoadURL(
       GURL("chrome://profile-picker/new-profile"), content::Referrer(),
       ui::PAGE_TRANSITION_AUTO_TOPLEVEL, std::string());
   WaitForLoadStop(web_contents(), GURL("chrome://profile-picker/new-profile"));
+  EXPECT_EQ(2, web_contents()->GetController().GetEntryCount());
+  EXPECT_EQ(1, web_contents()->GetController().GetLastCommittedEntryIndex());
 
   // Simulate a click on the signin button.
   base::MockCallback<base::OnceCallback<void(bool)>> switch_finished_callback;
@@ -236,10 +242,12 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerInteractiveUiTest,
   SendBackKeyboardCommand();
   WaitForLayoutWithoutToolbar();
   WaitForLoadStop(web_contents(), GURL("chrome://profile-picker/new-profile"));
+  EXPECT_EQ(1, web_contents()->GetController().GetLastCommittedEntryIndex());
 
   // Navigate again back with the keyboard.
   SendBackKeyboardCommand();
   WaitForLoadStop(web_contents(), GURL("chrome://profile-picker"));
+  EXPECT_EQ(0, web_contents()->GetController().GetLastCommittedEntryIndex());
 
   // Navigating back once again does nothing.
   SendBackKeyboardCommand();
