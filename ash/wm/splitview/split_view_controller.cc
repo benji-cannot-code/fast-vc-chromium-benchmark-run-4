6489071e79cf293d3f3f8281daf22b7e5c326d14
Yuheng Huang <yuhengh@chromium.org>
2021-07-07 19:32:30 +0000

    WebUI Tab Strip: Fix drag tab to new Window bug in split view mode
    Currently when in split screen mode, dragging a tab from one side to
    the other has undefined behavior, this CL address it by snapping the
    newly created window to the left of right based on drop position.
    
    Bug: 1159912
    Change-Id: I600d9f43de0bf9fa96e44a278d0e68b6730ddcf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2957353
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899234}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -1319,6 +1319,17 @@ void SplitViewController::OnWindowDragCanceled() {
     split_view_divider_->OnWindowDragEnded();
 }
 
+SplitViewController::SnapPosition SplitViewController::ComputeSnapPosition(
+    const gfx::Point& last_location_in_screen) {
+  const int divider_position = InSplitViewMode() ? this->divider_position()
+                                                 : GetDefaultDividerPosition();
+  const int position = IsLayoutHorizontal() ? last_location_in_screen.x()
+                                            : last_location_in_screen.y();
+  return (position <= divider_position) == IsLayoutRightSideUp()
+             ? SplitViewController::LEFT
+             : SplitViewController::RIGHT;
+}
+
 void SplitViewController::AddObserver(SplitViewObserver* observer) {
   observers_.AddObserver(observer);
 }
@@ -2343,17 +2354,6 @@ void SplitViewController::EndWindowDragImpl(
                      : SplitViewController::LEFT);
     }
   }
-}
-
-SplitViewController::SnapPosition SplitViewController::ComputeSnapPosition(
-    const gfx::Point& last_location_in_screen) {
-  const int divider_position = InSplitViewMode() ? this->divider_position()
-                                                 : GetDefaultDividerPosition();
-  const int position = IsLayoutHorizontal() ? last_location_in_screen.x()
-                                            : last_location_in_screen.y();
-  return (position <= divider_position) == IsLayoutRightSideUp()
-             ? SplitViewController::LEFT
-             : SplitViewController::RIGHT;
 }
 
 void SplitViewController::DoSplitDividerSpawnAnimation(aura::Window* window) {
