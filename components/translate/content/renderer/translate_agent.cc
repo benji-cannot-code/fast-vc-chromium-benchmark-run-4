b92f3634fd888263f427891131099e6a7d586d6c
Megan Jablonski <megjablon@chromium.org>
2021-07-07 19:35:47 +0000

    Don't reset Translate content length when page reset
    Bug: 1220804
    Change-Id: Ieb914a353c8d0d52f4fa60b61e5f99b90c1a649c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3010777
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899235}

--- components/translate/content/renderer/translate_agent.cc
+++ components/translate/content/renderer/translate_agent.cc
@@ -577,7 +577,6 @@ void TranslateAgent::ResetPage() {
   receiver_.reset();
   translate_callback_pending_.Reset();
   CancelPendingTranslation();
-  page_contents_length_ = 0;
 }
 
 void TranslateAgent::OnDestruct() {
