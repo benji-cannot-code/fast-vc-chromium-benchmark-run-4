6ecfacfc8e1a83950d634580a358a735b9a6f638
Danil Somsikov <dsv@chromium.org>
2021-07-07 08:44:49 +0000

    Don't open new web socket connection when emulating offline network
    Later I want to add a proxying to SendPendingDataFrames and ReadAndSendFromDataPipe to emulate slow and offline network for open connections as the design doc describes:
    https://docs.google.com/document/d/1evUbONSgQ4TFUWMx8GtsjqYRdhZ_vyLbpD6o4fBcTpo/edit?ts=60d55782#heading=h.sv055tybxdw
    
    
    
    Bug: 423246
    Change-Id: Ifc9a862f4888418c7c852c1e4691798b82f6e42d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3000963
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899046}

---
+++ third_party/blink/web_tests/http/tests/devtools/websocket/websocket-emulate-offline.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.runAsyncTestSuite([async function testNoSocketOpenWhenOffline() {
+    SDK.multitargetNetworkManager.setNetworkConditions(
+        SDK.NetworkManager.OfflineConditions);
+
+    TestRunner.addResult(await TestRunner.evaluateInPageAsync(`
+        new Promise((resolve) => {
+          const ws = new WebSocket('ws://localhost:8880/echo');
+          let log = '';
+          ws.onopen = () => {log += 'onopen '; ws.close(); };
+          ws.onmessage = () => log += 'onmessage ';
+          ws.onerror = () => log += 'onerror ';
+          ws.onclose = () => {
+            log += 'onclose ';
+            resolve(log);
+          };
+        });`));
+  }]);
+})();
