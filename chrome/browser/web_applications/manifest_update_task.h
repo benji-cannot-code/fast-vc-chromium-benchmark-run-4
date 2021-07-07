acb079886e399a600474ccfa2558b94b4eb7447d
Sunggook Chue <sunggch@microsoft.com>
2021-07-07 21:00:04 +0000

    ManifestUpdateManagerBrowserTest.CheckCancelledByAppUninstalled is flaky on Mac
    Test waits ManifestUpdateResult::kAppUninstalled set in order to check
    uninstallation finished and continue to do next thing. ManifestUpdateManager
    sets kAppUninstalled during OnWebappWillBeUninstalled, however OnWebappWillBeUninstalled,
    is before os integration and registry cleanup. It means the test tries to
    do the next thing or terminate before uninstallation operation is done.
    It can cause unexpected behavior when component handling WebAppUninstalled or os
    integration cleanup do something.
    
    Bug: 1224914
    Change-Id: I97c83d48728b258b036057508fca0b80fc120fdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003021
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#899283}

--- chrome/browser/web_applications/manifest_update_task.h
+++ chrome/browser/web_applications/manifest_update_task.h
@@ -54,7 +54,7 @@ enum ManifestUpdateResult {
   kNoAppInScope = 0,
   kThrottled = 1,
   kWebContentsDestroyed = 2,
-  kAppUninstalled = 3,
+  kAppUninstalling = 3,
   kAppIsPlaceholder = 4,
   kAppUpToDate = 5,
   kAppNotEligible = 6,
