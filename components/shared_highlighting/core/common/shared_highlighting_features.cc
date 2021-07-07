20478c43bcac68f8d9d7dacb90012f49bf6ca16d
sebsg <sebsg@chromium.org>
2021-07-07 18:24:12 +0000

    [SH] Enable CopyLinkToText and PreemptiveLTTGeneration by default.
    Bug: 1105862, 1091844
    Fixed: 1227027
    Change-Id: I012346dcebf76a770fa53648cb4db29daafd271d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009492
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899210}

--- components/shared_highlighting/core/common/shared_highlighting_features.cc
+++ components/shared_highlighting/core/common/shared_highlighting_features.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace shared_highlighting {
 
 const base::Feature kPreemptiveLinkToTextGeneration{
-    "PreemptiveLinkToTextGeneration", base::FEATURE_DISABLED_BY_DEFAULT};
+    "PreemptiveLinkToTextGeneration", base::FEATURE_ENABLED_BY_DEFAULT};
 constexpr base::FeatureParam<int> kPreemptiveLinkGenTimeoutLengthMs{
     &kPreemptiveLinkToTextGeneration, "TimeoutLengthMs", 500};
 
