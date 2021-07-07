1fd2a0e01c5a2049cff879ce0b5f3edd37ada7b7
Peter Kasting <pkasting@chromium.org>
2021-07-07 01:06:47 +0000

    Remove some unnecessary #includes.
    According to
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    these were responsible for 1,460,113,428 bytes of input to the compiler,
    or roughly 0.58% of the input used to build Chrome.
    
    Bug: 242216
    Change-Id: I1dd0a5fd3fcceb2da9bcf3dbae40e18590faf145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009975
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898977}

--- base/allocator/partition_allocator/partition_address_space.cc
+++ base/allocator/partition_allocator/partition_address_space.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_address_space.h"
 
 #include <array>
+#include <ostream>
 
 #include "base/allocator/partition_allocator/address_pool_manager.h"
 #include "base/allocator/partition_allocator/page_allocator.h"
