1a00685cdb09c69c67101e22c5653135c3a62c11
Alan Screen <awscreen@chromium.org>
2021-07-07 22:45:13 +0000

    Expand CUPS destination discovery to use cupsEnumDests()
    Use of the older CUPS function cupsGetDests2() can filter out some
    destinations if the same device reports multiple times with different
    printer type flags set.  This can lead to printers errantly
    disappearing from the Print Preview list of destinations.
    
    Use the alternate method cupsEnumDests() to retrieve the list of local
    destinations, since that allows us to control which destinations to
    filter out.
    
    Bug: 1209175
    Change-Id: If3534e8e636c17cce90c9b9db01f444b0497f29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2936182
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899327}

--- printing/backend/cups_connection.cc
+++ printing/backend/cups_connection.cc
@@ -12,13 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
+#include "printing/backend/cups_helper.h"
 #include "printing/backend/cups_jobs.h"
 
 namespace printing {
 
 namespace {
-
-constexpr int kTimeoutMs = 3000;
 
 // The number of jobs we'll retrieve for a queue.  We expect a user to queue at
 // most 10 jobs per printer.  If they queue more, they won't receive updates for
@@ -87,18 +86,11 @@ class CupsConnectionImpl : public CupsConnection {
       LOG(WARNING) << "CUPS connection failed: ";
       return false;
     }
-
-    // On macOS, AirPrint destinations show up even if they're not added to the
-    // system, and their capabilities cannot be read in that situation
-    // (crbug.com/1027834). Therefore, only show discovered destinations that
-    // have been added locally. Also exclude fax and scanner devices.
-    constexpr cups_ptype_t kMask =
-        CUPS_PRINTER_FAX | CUPS_PRINTER_SCANNER | CUPS_PRINTER_DISCOVERED;
     DestinationEnumerator enumerator;
     const int success =
-        cupsEnumDests(CUPS_DEST_FLAGS_NONE, kTimeoutMs,
+        cupsEnumDests(CUPS_DEST_FLAGS_NONE, kCupsTimeoutMs,
                       /*cancel=*/nullptr,
-                      /*type=*/CUPS_PRINTER_LOCAL, kMask,
+                      /*type=*/CUPS_PRINTER_LOCAL, kDestinationsFilterMask,
                       &DestinationEnumerator::cups_callback, &enumerator);
 
     if (!success) {
@@ -200,7 +192,7 @@ class CupsConnectionImpl : public CupsConnection {
 
     cups_http_.reset(httpConnect2(host.c_str(), port, nullptr, AF_UNSPEC,
                                   cups_encryption_, blocking_ ? 1 : 0,
-                                  kTimeoutMs, nullptr));
+                                  kCupsTimeoutMs, nullptr));
     return !!cups_http_;
   }
 
