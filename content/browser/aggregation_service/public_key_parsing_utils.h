6d7e22e38802578a18a8a6065271ba9c25aec473
Nan Lin <linnan@chromium.org>
2021-07-07 19:49:46 +0000

    Skeleton for aggregation service client infra Phase 1a
    Some proposed web APIs require use of a private, secure aggregation
    service. To avoid duplication, core functionality such as constructing
    aggregatable reports, including generation of secret shares, fetching
    helper server public keys and encryption will be implemented in a shared
    infrastructure.
    
    For now, this infrastructure will only be used by a command-line testing
    tool (i.e. not by the browser itself) to test interoperability with a
    reference server implementation. This cl begins implementation of this
    testing tool and the infrastructure needed for it, including the storage
    and public key parsing logic.
    
    Bug: 1207993
    Change-Id: Ia8c1ed274977c16d72e07849d9c0e24272d85d82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2891183
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899247}

---
+++ content/browser/aggregation_service/public_key_parsing_utils.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_AGGREGATION_SERVICE_PUBLIC_KEY_PARSING_UTILS_H_
+#define CONTENT_BROWSER_AGGREGATION_SERVICE_PUBLIC_KEY_PARSING_UTILS_H_
+
+#include <vector>
+
+#include "base/values.h"
+#include "content/browser/aggregation_service/public_key.h"
+#include "content/common/content_export.h"
+
+namespace content {
+
+namespace aggregation_service {
+
+// Constructs a public key vector from multiple JSON arrays of key definition
+// tagged with version ids. In case of an error or invalid JSON, returns an
+// empty vector.
+//
+// The expected JSON schema is as follows.
+//
+// {
+//   <Version ID> : [
+//     {
+//        "id" : <arbitrary string identifying the key, e.g. a UUID>,
+//        "key" : <base64-encoded public key>,
+//        "not_before" : <when key become valid, encoded as a string
+//                      representation of an integer timestamp in milliseconds
+//                      since the Unix epoch>,
+//        "not_after" : <key expiry, encoded similarly to not_before>,
+//     },
+//     {  "id" : <different ID string>, ... },
+//     ...
+//   ]
+// }
+CONTENT_EXPORT std::vector<PublicKey> GetPublicKeys(base::Value& value);
+
+}  // namespace aggregation_service
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_AGGREGATION_SERVICE_PUBLIC_KEY_PARSING_UTILS_H_
---
+++ content/public/test/test_aggregation_service.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/test/test_aggregation_service.h"
+
+#include "content/test/test_aggregation_service_impl.h"
+
+namespace content {
+
+std::unique_ptr<TestAggregationService> TestAggregationService::Create() {
+  return std::make_unique<TestAggregationServiceImpl>();
+}
+
+}  // namespace content
