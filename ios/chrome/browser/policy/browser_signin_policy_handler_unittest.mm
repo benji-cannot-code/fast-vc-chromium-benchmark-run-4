1bd6550c7b3f0c4136c0af3f404ab6a332733602
Sylvain Defresne <sdefresne@chromium.org>
2021-07-07 18:20:47 +0000

    [ios] Fix flaky tests
    The following two tests were leaving some incompatible state into
    the NSUserDefaults under the same key which caused them to fail if
    the app was run multiple time (or the test order randomized):
    
    -   BrowserDMTokenStorageIOSTest.InitEnrollmentToken
    -   PolicyTest.PolicyToPrefMappings
    
    Fix this by having the test clean after themselves and before they
    are run (to avoid polluting the NSUserDefaults and to recover if
    it has been polluted by a previous run).
    
    Also fix SettingsTableViewControllerTest which was cleaning the
    NSUserDefaults before execution but not after running (thus could
    leave it in a state incompatible with another test suite).
    
    Bug: none
    Change-Id: I1aad09c397745c3142a95fabfcf8f52e7991dbef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009627
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899206}

--- ios/chrome/browser/policy/browser_signin_policy_handler_unittest.mm
+++ ios/chrome/browser/policy/browser_signin_policy_handler_unittest.mm
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/policy/browser_signin_policy_handler.h"
 
+#import <Foundation/Foundation.h>
+
 #include "components/policy/core/browser/policy_error_map.h"
+#import "components/policy/core/common/policy_loader_ios_constants.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/schema.h"
 #include "components/prefs/pref_value_map.h"
@@ -13,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/policy/policy_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace policy {
 namespace {
@@ -28,7 +35,20 @@ const char kTestSchema[] = R"(
       }
     })";
 
-using BrowserSigninPolicyHandlerTest = PlatformTest;
+class BrowserSigninPolicyHandlerTest : public PlatformTest {
+ protected:
+  BrowserSigninPolicyHandlerTest() {
+    // Make sure there is no pre-existing policy present.
+    [[NSUserDefaults standardUserDefaults]
+        removeObjectForKey:kPolicyLoaderIOSConfigurationKey];
+  }
+
+  ~BrowserSigninPolicyHandlerTest() override {
+    // Cleanup any policies left from the test.
+    [[NSUserDefaults standardUserDefaults]
+        removeObjectForKey:kPolicyLoaderIOSConfigurationKey];
+  }
+};
 
 const char* BrowserSigninModeToString(BrowserSigninMode mode) {
   switch (mode) {
