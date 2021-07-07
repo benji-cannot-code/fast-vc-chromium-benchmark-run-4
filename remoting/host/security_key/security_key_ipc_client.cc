fe576bc02c4c4af78166768527bf17ca87db3a08
Rune Lillesveen <futhark@chromium.org>
2021-07-07 19:35:51 +0000

    Fix some gn deps in //remoting/host/security_key
    Down to two failures. Both remoting/host/chromoting_messages.h
    
    Bug: 1159013
    Change-Id: I9a82d261dc98803f168abdf3cc15f92d9a0abe91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3008337
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899236}

--- remoting/host/security_key/security_key_ipc_client.cc
+++ remoting/host/security_key/security_key_ipc_client.cc
@@ -12,12 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_listener.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_macros.h"
 #include "remoting/host/chromoting_messages.h"
-#include "remoting/host/ipc_constants.h"
 #include "remoting/host/security_key/security_key_ipc_constants.h"
 
 namespace remoting {
--- remoting/host/security_key/security_key_ipc_server.cc
+++ remoting/host/security_key/security_key_ipc_server.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_macros.h"
 #include "remoting/base/logging.h"
-#include "remoting/host/chromoting_messages.h"
 #include "remoting/host/security_key/security_key_ipc_server_impl.h"
 
 namespace {
