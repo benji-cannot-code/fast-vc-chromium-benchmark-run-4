18376433c4db6e418dd2acfa7c9a2b5101646783
kouhei <kouhei@chromium.org>
2021-07-07 10:57:44 +0000

    fix comment typo: should be WebContentsObserver
    RenderFrameHostObserver doesn't exist.
    Refer to WebContentsObserver instead.
    
    Bug: None
    Change-Id: Iaef908d17a3a0114808c746b0cc088a61b229c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011092
    Auto-Submit: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899076}

--- content/browser/renderer_host/render_frame_host_impl_browsertest.cc
+++ content/browser/renderer_host/render_frame_host_impl_browsertest.cc
@@ -4995,7 +4995,7 @@ namespace {
 void SetupRemoteObjectInvocation(Shell* shell, const GURL& url) {
   WebContents* web_contents = shell->web_contents();
 
-  // The first load triggers RenderFrameCreated on a RenderFrameHostObserver
+  // The first load triggers RenderFrameCreated on a WebContentsObserver
   // instance, where the object injection happens.
   shell->LoadURL(url);
   EXPECT_TRUE(WaitForLoadStop(web_contents));
