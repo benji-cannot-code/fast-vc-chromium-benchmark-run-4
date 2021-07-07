84acb3c3b0b2a3f2e9f9a3f742f62ad25b94282a
adamta <adamta@google.com>
2021-07-07 18:20:02 +0000

    [iOS] Handle ContentSuggestionsMetricsRecorder in DiscoverFeedProvider
    Moves ContentSuggestionsMetricsRecorder to DiscoverFeedService so that
    it exists as a singleton throughout the app. Previously, it was being
    reset each time an NTP was created, which would cause the 5 minute
    sessions to not be respected.
    
    Bug: 1224381
    Change-Id: Idfc6b0927292e8519c349b271816f430da43c429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991897
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/master@{#899205}

--- ios/chrome/browser/discover_feed/discover_feed_service.h
+++ ios/chrome/browser/discover_feed/discover_feed_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 
 class AuthenticationService;
+@class ContentSuggestionsMetricsRecorder;
 @class DiscoverFeedMetricsRecorder;
 class PrefService;
 
@@ -25,9 +26,14 @@ class DiscoverFeedService : public KeyedService,
                       signin::IdentityManager* identity_manager);
   ~DiscoverFeedService() override;
 
-  // Returns the FeedMetricsRecorder to be used by the Feed, a single instance
-  // of DiscoverFeedMetricsRecorder needs to be used per BrowserState.
+  // Returns the DiscoverFeedMetricsRecorder to be used by the Discover Feed, a
+  // single instance needs to be used per BrowserState.
   DiscoverFeedMetricsRecorder* GetDiscoverFeedMetricsRecorder();
+
+  // Returns the ContentSuggestionsMetricsRecorder to be used by the Zine Feed,
+  // a single instance needs to be used per BrowserState.
+  // TODO(crbug.com/1200303): Remove this when we launch the Discover feed.
+  ContentSuggestionsMetricsRecorder* GetContentSuggestionsMetricsRecorder();
 
   // KeyedService:
   void Shutdown() override;
@@ -44,6 +50,10 @@ class DiscoverFeedService : public KeyedService,
 
   // Metrics recorder for the DiscoverFeed.
   __strong DiscoverFeedMetricsRecorder* discover_feed_metrics_recorder_ = nil;
+
+  // Metrics recorder for the Zine feed.
+  __strong ContentSuggestionsMetricsRecorder*
+      content_suggestions_metrics_recorder_ = nil;
 
   DISALLOW_COPY_AND_ASSIGN(DiscoverFeedService);
 };
--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -119,7 +119,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ContentSuggestionsMediator* contentSuggestionsMediator;
 @property(nonatomic, strong)
     ContentSuggestionsHeaderSynchronizer* headerCollectionInteractionHandler;
-@property(nonatomic, strong) ContentSuggestionsMetricsRecorder* metricsRecorder;
 @property(nonatomic, strong) UIViewController* discoverFeedViewController;
 @property(nonatomic, strong) UIView* discoverFeedHeaderMenuButton;
 @property(nonatomic, strong) URLDragDropHandler* dragDropHandler;
@@ -256,9 +255,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.headerController.promoCanShow =
       [self.contentSuggestionsMediator notificationPromo]->CanShow();
 
-  self.metricsRecorder = [[ContentSuggestionsMetricsRecorder alloc] init];
-  self.metricsRecorder.delegate = self.contentSuggestionsMediator;
-
+  self.contentSuggestionsMetricsRecorder.delegate =
+      self.contentSuggestionsMediator;
 
   // Offset to maintain Discover feed scroll position.
   CGFloat offset = 0;
@@ -285,7 +283,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.suggestionsViewController.audience = self;
   self.suggestionsViewController.overscrollDelegate = self;
   self.suggestionsViewController.themeChangeDelegate = self;
-  self.suggestionsViewController.metricsRecorder = self.metricsRecorder;
+  self.suggestionsViewController.metricsRecorder =
+      self.contentSuggestionsMetricsRecorder;
   id<SnackbarCommands> dispatcher =
       static_cast<id<SnackbarCommands>>(self.browser->GetCommandDispatcher());
   self.suggestionsViewController.dispatcher = dispatcher;
@@ -318,7 +317,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.ntpMediator.NTPMetrics = [[NTPHomeMetrics alloc]
       initWithBrowserState:self.browser->GetBrowserState()
                   webState:self.webState];
-  self.ntpMediator.metricsRecorder = self.metricsRecorder;
+  self.ntpMediator.metricsRecorder = self.contentSuggestionsMetricsRecorder;
   self.ntpMediator.suggestionsViewController = self.suggestionsViewController;
   self.ntpMediator.suggestionsMediator = self.contentSuggestionsMediator;
   self.ntpMediator.suggestionsService = contentSuggestionsService;
