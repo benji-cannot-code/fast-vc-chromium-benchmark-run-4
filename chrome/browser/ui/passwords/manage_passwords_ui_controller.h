7216a7752d22058e1c17c9e70f1ec9c54614488b
Mohamed Amir Yosef <mamir@chromium.org>
2021-07-07 11:48:22 +0000

    [Passwords] Switch ManagePasswordsUIController to PasswordStoreInterface
    Bug: 1218413
    Change-Id: Id1660050152805de6ef106619dbddd6e4f636866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009578
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899084}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller.h
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/passwords_model_delegate.h"
 #include "chrome/common/buildflags.h"
 #include "components/password_manager/core/browser/password_manager_client.h"
-#include "components/password_manager/core/browser/password_store.h"
+#include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/password_manager/core/browser/ui/post_save_compromised_helper.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
