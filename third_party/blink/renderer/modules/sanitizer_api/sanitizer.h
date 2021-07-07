abc21fd9365fd26490e2795b1ff0fe24ae08ca2c
Daniel Vogelheim <vogelheim@chromium.org>
2021-07-07 11:48:49 +0000

    [Sanitizer API] Adapt new string API. Also adapt tests.
    Implement Sanitizer.sanitizeFor.
    Drive-by fix: Ensure secure context.
    
    Both name, exact edge and error conditions for these methods are still
    in discussion, so this will likely require follow-on work. But the
    basics we're implementing here allow us to experiment, and will also
    likely hold.
    
    Ref: https://github.com/WICG/sanitizer-api/pull/99
    Bug: 1221153
    Change-Id: I9c7f74f71e07f5fc1a172ae8f7f6181178f3c8d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2969103
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899085}

--- third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
+++ third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ContainerNode;
 class Document;
 class DocumentFragment;
 class ExceptionState;
@@ -35,15 +36,19 @@ class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
   static Sanitizer* Create(ExecutionContext*,
                            const SanitizerConfig*,
                            ExceptionState&);
-  explicit Sanitizer(ExecutionContext*, const SanitizerConfig*);
+  Sanitizer(ExecutionContext*, const SanitizerConfig*);
   ~Sanitizer() override;
 
   String sanitizeToString(ScriptState* script_state,
                           const V8SanitizerInput* input,
                           ExceptionState& exception_state);
   DocumentFragment* sanitize(ScriptState* script_state,
-                             const V8SanitizerInputWithTrustedHTML* input,
+                             const V8SanitizerInput* input,
                              ExceptionState& exception_state);
+  Element* sanitizeFor(ScriptState* script_state,
+                       const String& element,
+                       const String& markup,
+                       ExceptionState& exception_state);
 
   SanitizerConfig* config() const;
   static SanitizerConfig* defaultConfig();
@@ -51,9 +56,9 @@ class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
   void Trace(Visitor*) const override;
 
  private:
-  Node* DropElement(Node*, DocumentFragment*);
-  Node* BlockElement(Node*, DocumentFragment*, ExceptionState&);
-  Node* KeepElement(Node*, DocumentFragment*, String&, LocalDOMWindow*);
+  Node* DropElement(Node*, ContainerNode*);
+  Node* BlockElement(Node*, ContainerNode*, ExceptionState&);
+  Node* KeepElement(Node*, ContainerNode*, String&, LocalDOMWindow*);
 
   void ElementFormatter(HashSet<String>&, const Vector<String>&);
   void AttrFormatter(HashMap<String, Vector<String>>&,
@@ -63,9 +68,7 @@ class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
                                     ScriptState* script_state,
                                     const V8SanitizerInput* input,
                                     ExceptionState& exception_state);
-  DocumentFragment* DoSanitizing(DocumentFragment*,
-                                 LocalDOMWindow*,
-                                 ExceptionState&);
+  void DoSanitizing(ContainerNode*, LocalDOMWindow*, ExceptionState&);
   DocumentFragment* SanitizeImpl(ScriptState* script_state,
                                  const V8SanitizerInput* input,
                                  ExceptionState& exception_state);
