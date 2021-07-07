85d084f4a739969b75b2599e20a2160287e427e7
Song Fangzhen <songfangzhen@bytedance.com>
2021-07-07 03:06:35 +0000

    CodeHealth: Remove use of DictionaryValue::Remove in /chrome/browser/ui
    This change removes the use of DictionaryValue::Remove and replaces with
    Value::RemoveKey().
    
    Bug: 1187051
    Change-Id: I851d618fb36da685abda6c12382284c32ba83b49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3008019
    Commit-Queue: calamity <calamity@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#898990}

--- chrome/browser/ui/app_list/app_list_syncable_service.cc
+++ chrome/browser/ui/app_list/app_list_syncable_service.cc
@@ -149,7 +149,7 @@ sync_pb::AppListSpecifics::AppListItemType GetAppListItemType(
 void RemoveSyncItemFromLocalStorage(Profile* profile,
                                     const std::string& item_id) {
   DictionaryPrefUpdate(profile->GetPrefs(), prefs::kAppListLocalState)
-      ->Remove(item_id, nullptr);
+      ->RemoveKey(item_id);
 }
 
 void UpdateSyncItemInLocalStorage(
