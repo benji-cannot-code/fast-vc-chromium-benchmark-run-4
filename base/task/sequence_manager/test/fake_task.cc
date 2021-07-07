19d87fa51f62835cda62a273a90f736adb40296e
Etienne Pierre-doray <etiennep@chromium.org>
2021-07-07 20:15:13 +0000

    [tasks]: Move DelayedWakeup out of internal namespace.
    Moves DelayedWakeUp and WakeUpResolution out of internal namespace
    and remove sequence_num from DelayedWakeUp.
    
    In follow up (https://chromium-review.googlesource.com/c/chromium/src/+/2896025)
    DelayedWakeup is used more explicitly in TaskQueueThrottler, which will
    allow more precise control (especially for slack).
    
    Change-Id: I43b7586b0eb0f11bd33fff9676a022eede0f3a51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2909523
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899257}

--- base/task/sequence_manager/test/fake_task.cc
+++ base/task/sequence_manager/test/fake_task.cc
@@ -20,7 +20,7 @@ FakeTask::FakeTask(TaskType task_type)
            TimeTicks(),
            EnqueueOrder(),
            EnqueueOrder(),
-           internal::WakeUpResolution::kLow) {}
+           WakeUpResolution::kLow) {}
 
 FakeTaskTiming::FakeTaskTiming()
     : TaskTiming(false /* has_wall_time */, false /* has_thread_time */) {}
