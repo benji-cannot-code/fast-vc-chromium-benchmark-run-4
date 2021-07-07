df9c6fb981f8490d3a4fdb1690802d9d154f6aca
Adithya Srinivasan <adithyas@chromium.org>
2021-07-07 19:41:26 +0000

    MPArch: Fix DevToolsIssueStorage for prerendering
    A prerendering page can move between FTNs, and currently, any issues
    that were added to DevToolsIssueStorage for a prerendering page would be
    associated with a constant ftn_id that doesn't update after the page
    gets activated. This CL fixes this by removing the class' reliance on
    ftn_id and using RFH ids instead.
    
    Change-Id: Ieffd9f38ac0a39ab7add983e0c6a37450382e568
    Bug: 1220221, 1090679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2963914
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899240}

--- content/browser/devtools/devtools_issue_storage_browsertest.cc
+++ content/browser/devtools/devtools_issue_storage_browsertest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/prerender_test_util.h"
 #include "content/public/test/test_utils.h"
 #include "content/shell/browser/shell.h"
 #include "net/dns/mock_host_resolver.h"
@@ -43,6 +44,13 @@ class DevToolsIssueStorageBrowserTest : public DevToolsProtocolTest {
 
   RenderFrameHostImpl* main_frame_host() {
     return web_contents()->GetFrameTree()->GetMainFrame();
+  }
+
+  void WaitForDummyIssueNotification() {
+    std::unique_ptr<base::DictionaryValue> notification =
+        WaitForNotification("Audits.issueAdded", true);
+    EXPECT_EQ(*(notification->FindDictPath("issue")->FindStringPath("code")),
+              protocol::Audits::InspectorIssueCodeEnum::SameSiteCookieIssue);
   }
 };
 
@@ -80,7 +88,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageBrowserTest,
   SendCommand("Audits.enable", std::make_unique<base::DictionaryValue>());
 
   // 6) Verify we have received the SameSite issue.
-  WaitForNotification("Audits.issueAdded", true);
+  WaitForDummyIssueNotification();
 }
 
 IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageBrowserTest,
@@ -99,7 +107,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageBrowserTest,
   ReportDummyIssue(main_frame_host());
 
   // 5) Verify we have received the SameSite issue.
-  WaitForNotification("Audits.issueAdded", true);
+  WaitForDummyIssueNotification();
 }
 
 IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageBrowserTest,
@@ -128,7 +136,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageBrowserTest,
   SendCommand("Audits.enable", std::make_unique<base::DictionaryValue>());
 
   // 5) Verify we have received the SameSite issue on the main target.
-  WaitForNotification("Audits.issueAdded", true);
+  WaitForDummyIssueNotification();
 }
 
 IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageBrowserTest,
@@ -204,7 +212,56 @@ IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageWithBackForwardCacheBrowserTest,
   SendCommand("Audits.enable", std::make_unique<base::DictionaryValue>());
 
   // 6) Verify we have received the SameSite issue on the main target.
-  WaitForNotification("Audits.issueAdded", true);
+  WaitForDummyIssueNotification();
 }
 
+class DevToolsIssueStorageWithPrerenderBrowserTest
+    : public DevToolsIssueStorageBrowserTest {
+ public:
+  DevToolsIssueStorageWithPrerenderBrowserTest()
+      : prerender_test_helper_(base::BindRepeating(
+            &DevToolsIssueStorageWithPrerenderBrowserTest::GetWebContents,
+            base::Unretained(this))) {}
+
+  void SetUpOnMainThread() override {
+    DevToolsIssueStorageBrowserTest::SetUpOnMainThread();
+    prerender_test_helper().SetUpOnMainThread(embedded_test_server());
+  }
+
+  test::PrerenderTestHelper& prerender_test_helper() {
+    return prerender_test_helper_;
+  }
+
+ private:
+  WebContents* GetWebContents() { return web_contents(); }
+  test::PrerenderTestHelper prerender_test_helper_;
+};
+
+IN_PROC_BROWSER_TEST_F(DevToolsIssueStorageWithPrerenderBrowserTest,
+                       IssueWhilePrerendering) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  GURL main_url(embedded_test_server()->GetURL("/empty.html"));
+  GURL prerender_url(embedded_test_server()->GetURL("/title1.html"));
+
+  // 1) Navigate to |main_url|.
+  EXPECT_TRUE(NavigateToURL(shell(), main_url));
+
+  // 2) Prerender |prerender_url|.
+  int host_id = prerender_test_helper().AddPrerender(prerender_url);
+  RenderFrameHostImpl* prerender_rfh = static_cast<RenderFrameHostImpl*>(
+      prerender_test_helper().GetPrerenderedMainFrameHost(host_id));
+
+  // 3) Report an empty SameSite cookie issue in prerendering page.
+  ReportDummyIssue(prerender_rfh);
+
+  // 4) Activate prerendering page.
+  prerender_test_helper().NavigatePrimaryPage(prerender_url);
+
+  // 5) Open DevTools and enable Audits domain.
+  Attach();
+  SendCommand("Audits.enable", std::make_unique<base::DictionaryValue>());
+
+  // 6) Verify we have received the SameSite issue on the main target.
+  WaitForDummyIssueNotification();
+}
 }  // namespace content
