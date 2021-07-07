3dd755e4c67a05dca4d9cc588553a91fe1b25239
Dominic Schulz <dominicschulz@google.com>
2021-07-07 08:25:55 +0000

    system-web-apps: Add a histogram for background task start delay
    Bug: 187992598
    Change-Id: Ie2e05bb75a777121d7a79b30cdf624b0edf0b0fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3008194
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Dominic Schulz <dominicschulz@google.com>
    Cr-Commit-Position: refs/heads/master@{#899042}

--- chrome/browser/web_applications/system_web_apps/system_web_app_background_task.cc
+++ chrome/browser/web_applications/system_web_apps/system_web_app_background_task.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/profiles/profile.h"
@@ -27,6 +28,9 @@ SystemAppBackgroundTaskInfo::SystemAppBackgroundTaskInfo(
     const GURL& url,
     bool open_immediately)
     : period(period), url(url), open_immediately(open_immediately) {}
+
+// static
+const char SystemAppBackgroundTask::kBackgroundStartDelayHistogramName[];
 
 SystemAppBackgroundTask::SystemAppBackgroundTask(
     Profile* profile,
@@ -105,6 +109,10 @@ void SystemAppBackgroundTask::MaybeOpenPage() {
                   base::BindOnce(&SystemAppBackgroundTask::MaybeOpenPage,
                                  weak_ptr_factory_.GetWeakPtr()));
   }
+
+  base::UmaHistogramLongTimes(kBackgroundStartDelayHistogramName,
+                              polling_duration);
+
   polling_since_time_ = base::Time();
   state_ = WAIT_PERIOD;
   NavigateBackgroundPage();
