413a33eeaec94ef9127949439f8f95960d7dca69
Alice Wang <aliceywang@chromium.org>
2021-07-07 15:18:58 +0000

    [Signin][Android] Fix NullPointerException when showing NTP sync promo
    This CL fixes the NullPointerException when showing showing the sync
    promo on NTP by checking the capability only with a valid account.
    
    Bug: 1226830
    Change-Id: I01538a07e063132a160ee8574ed628ba0247e9e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009614
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899130}

--- components/signin/public/android/java/src/org/chromium/components/signin/test/util/FakeAccountManagerFacade.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/test/util/FakeAccountManagerFacade.java
@@ -114,6 +114,7 @@ public class FakeAccountManagerFacade implements AccountManagerFacade {
 
     @Override
     public Optional<Boolean> canOfferExtendedSyncPromos(Account account) {
+        assert account != null;
         return Optional.absent();
     }
 
