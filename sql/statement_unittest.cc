2f0123126eb41106fde89723a4371dcecb3d6634
Victor Costan <pwnall@chromium.org>
2021-07-08 20:53:45 +0000

    sql: Statement::BindString() accepts base::StringPiece.
    base::StringPiece has constexpr constructors, so this CL makes
    BindString() friendlier to code that wants to use constexpr constants.
    
    Bug: 1227135
    Change-Id: I9f9aee7e062d2d0954674c3ae92bbf04cff42ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3012195
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899693}

--- sql/statement_unittest.cc
+++ sql/statement_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/strings/string_piece_forward.h"
 #include "sql/database.h"
 #include "sql/statement.h"
 #include "sql/test/error_callback_support.h"
@@ -179,6 +180,20 @@ TEST_F(SQLStatementTest, BindString) {
     ASSERT_TRUE(select.Step());
     EXPECT_EQ(value, select.ColumnString(0));
   }
+  EXPECT_FALSE(select.Step());
+}
+
+TEST_F(SQLStatementTest, BindString_NullData) {
+  ASSERT_TRUE(db_.Execute("CREATE TABLE strings (s TEXT NOT NULL)"));
+
+  Statement insert(db_.GetUniqueStatement("INSERT INTO strings VALUES(?)"));
+  insert.BindString(0, base::StringPiece(nullptr, 0));
+  ASSERT_TRUE(insert.Run());
+
+  Statement select(db_.GetUniqueStatement("SELECT s FROM strings"));
+  ASSERT_TRUE(select.Step());
+  EXPECT_EQ(std::string(), select.ColumnString(0));
+
   EXPECT_FALSE(select.Step());
 }
 
