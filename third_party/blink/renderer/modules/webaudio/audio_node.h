7c966ef01fb0069d6a2977f573c98d2348c01af1
Peter Kasting <pkasting@chromium.org>
2021-07-08 16:41:26 +0000

    Remove some unnecessary #includes.
    According to
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    these were responsible for 1,460,113,428 bytes of input to the compiler,
    or roughly 0.58% of the input used to build Chrome.
    
    Bug: 242216
    Change-Id: If0380951303b3a3b01eb46f8c53dc23666bfb1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3010770
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899582}

--- third_party/blink/renderer/modules/webaudio/audio_node.h
+++ third_party/blink/renderer/modules/webaudio/audio_node.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
-#include "third_party/blink/renderer/modules/webaudio/audio_graph_tracer.h"
 #include "third_party/blink/renderer/modules/webaudio/inspector_helper_mixin.h"
 #include "third_party/blink/renderer/platform/audio/audio_bus.h"
 #include "third_party/blink/renderer/platform/audio/audio_utilities.h"
