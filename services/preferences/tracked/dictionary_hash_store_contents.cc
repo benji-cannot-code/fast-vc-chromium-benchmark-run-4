d74130865e054e9751b3114d81adc65a7161a2eb
Song Fangzhen <songfangzhen@bytedance.com>
2021-07-08 02:33:11 +0000

    CodeHealth: Remove use of DictionaryValue::Remove in /services
    This change removes the use of DictionaryValue::Remove and replaces with
    Value::RemovePath() or Value::RemoveKey().
    
    In addition, DictionaryValueUpdate::Remove() is updated to accept only
    one input parameter, so the caller.
    
    Bug: 1187051
    Change-Id: I4592f90fe31b7cf93aaa7ce268e4e5d3938a59fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3008057
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Auto-Submit: Fangzhen Song <songfangzhen@bytedance.com>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899405}

--- services/preferences/tracked/dictionary_hash_store_contents.cc
+++ services/preferences/tracked/dictionary_hash_store_contents.cc
@@ -46,7 +46,7 @@ base::StringPiece DictionaryHashStoreContents::GetUMASuffix() const {
 }
 
 void DictionaryHashStoreContents::Reset() {
-  storage_->Remove(kPreferenceMACs, NULL);
+  storage_->RemovePath(kPreferenceMACs);
 }
 
 bool DictionaryHashStoreContents::GetMac(const std::string& path,
