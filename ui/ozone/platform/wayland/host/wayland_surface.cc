7f08d52651d9a220bb1f774957a428b0d0f84d6a
Kramer Ge <fangzhoug@chromium.org>
2021-07-08 18:08:04 +0000

    [Ozone/Wayland] Store |display_size_dip_| instead of |display_size_px_|
    |buffer_scale_| is unset in UpdateBufferDamageRegion(), leading to
    incorrect calculation of |viewport_dst| (in DIP), and results in
    expanded |damage|. Store |display_size_dip_| to avoid the scaling
    calculation.
    
    Change-Id: I41befafdb9893d0100168124ef103230db820960
    Bug: 1220213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3007134
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899625}

--- ui/ozone/platform/wayland/host/wayland_surface.cc
+++ ui/ozone/platform/wayland/host/wayland_surface.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/geometry/size_f.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/ozone/platform/wayland/common/wayland_util.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
@@ -136,9 +137,8 @@ void WaylandSurface::UpdateBufferDamageRegion(
   }
   // Apply viewport scale (wp_viewport.set_destination).
   gfx::Size viewport_dst = bounds;
-  if (!display_size_px_.IsEmpty()) {
-    viewport_dst =
-        gfx::ScaleToCeiledSize(display_size_px_, 1.f / buffer_scale_);
+  if (!display_size_dip_.IsEmpty()) {
+    viewport_dst = display_size_dip_;
   }
 
   if (connection_->compositor_version() >=
@@ -309,22 +309,20 @@ void WaylandSurface::SetViewportSource(const gfx::RectF& src_rect) {
 }
 
 void WaylandSurface::SetViewportDestination(const gfx::Size& dest_size_px) {
-  if (dest_size_px == display_size_px_)
+  if (dest_size_px == gfx::ScaleToRoundedSize(display_size_dip_, buffer_scale_))
     return;
 
   if (dest_size_px.IsEmpty()) {
-    display_size_px_ = gfx::Size();
+    display_size_dip_ = gfx::Size();
     if (viewport()) {
       wp_viewport_set_destination(viewport(), -1, -1);
     }
     return;
   }
-  display_size_px_ = dest_size_px;
-  gfx::Size viewport_dst =
-      gfx::ScaleToCeiledSize(display_size_px_, 1.f / buffer_scale_);
+  display_size_dip_ = gfx::ScaleToCeiledSize(dest_size_px, 1.f / buffer_scale_);
   if (viewport()) {
-    wp_viewport_set_destination(viewport(), viewport_dst.width(),
-                                viewport_dst.height());
+    wp_viewport_set_destination(viewport(), display_size_dip_.width(),
+                                display_size_dip_.height());
   }
 }
 
