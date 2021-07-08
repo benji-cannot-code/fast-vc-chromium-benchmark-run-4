e72ba33b53fe2c6dab0ac201562c7110c7085c9f
Ian Clelland <iclelland@chromium.org>
2021-07-08 19:22:38 +0000

    Revert "regenerate manifest on builders"
    This reverts commit c4e4bd9d1ab9b410b8711cd81a1f6a15e4fb9a4d.
    
    Reason for revert: The build rules for the manifest files don't capture
    the dependency on the test files properly; if a new WPT is added, an
    incremental build will not cause the manifest to be regenerated.
    
    This showed up as failures in the deterministic builder here:
    https://ci.chromium.org/p/chromium/builders/ci/Deterministic%20Fuchsia%20%28dbg%29/8855
    https://ci.chromium.org/p/chromium/builders/ci/Deterministic%20Fuchsia%20%28dbg%29/8856
    
    
    Original change's description:
    > regenerate manifest on builders
    >
    > Previously manifest is regenerated on test bots. Move this to the
    > builder so that we don't need to repeat this on every shard, and
    > builders are more likely to get an SSD.
    >
    > Bug: 982208
    > Change-Id: Idb3755feaa4249755fb67a3db9069711bf479d7c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2993382
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > Cr-Commit-Position: refs/heads/master@{#899294}
    
    Bug: 982208
    Change-Id: I56079e4dbb0488effb1ab4c2bb37e87f15a57e48
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016238
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Owners-Override: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899653}

--- third_party/blink/tools/blinkpy/w3c/wpt_manifest_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/wpt_manifest_unittest.py
@@ -32,8 +32,6 @@ class WPTManifestUnitTest(unittest.TestCase):
             '--no-download',
             '--tests-root',
             WEB_TEST_DIR + '/external/wpt',
-            '--path',
-            WEB_TEST_DIR + '/external/wpt/MANIFEST.json'
         ]])
 
     def test_ensure_manifest_updates_manifest_if_it_exists(self):
@@ -58,8 +56,6 @@ class WPTManifestUnitTest(unittest.TestCase):
             '--no-download',
             '--tests-root',
             WEB_TEST_DIR + '/external/wpt',
-            '--path',
-            WEB_TEST_DIR + '/external/wpt/MANIFEST.json'
         ]])
 
     def test_ensure_manifest_raises_exception(self):
@@ -82,8 +78,6 @@ class WPTManifestUnitTest(unittest.TestCase):
             '--no-download',
             '--tests-root',
             WEB_TEST_DIR + '/wpt_internal',
-            '--path',
-            WEB_TEST_DIR + '/wpt_internal/MANIFEST.json',
         ]])
 
     def test_does_not_throw_when_missing_some_test_types(self):
