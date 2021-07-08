f7bd521cda34fa879a7c7bc4535d0bbce00ba2fc
Shane Fitzpatrick <shanefitz@google.com>
2021-07-08 19:30:25 +0000

    [FastPair] Connect UI triggers to notification controller
    This change connects the debug page UI trigger buttons to the
    FastPairNotificationController.
    
    I confirmed that it is ok for this webui controller to directly depend
    on ash::quick_pair::FastPairNotificationController, which simplifies
    the integration significantly.
    
    These triggers are intended to be used to quickly verify the look,
    localization, etc. of the UI, and thus none of the buttons are
    connected to any functionality.
    
    Change-Id: I7b41b29eccea02e97e19d0656be9ac8c3d61e557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011111
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899659}

--- chrome/browser/ui/webui/nearby_internals/quick_pair/quick_pair_handler.h
+++ chrome/browser/ui/webui/nearby_internals/quick_pair/quick_pair_handler.h
@@ -6,12 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_NEARBY_INTERNALS_QUICK_PAIR_QUICK_PAIR_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_NEARBY_INTERNALS_QUICK_PAIR_QUICK_PAIR_HANDLER_H_
 
+#include <memory>
 #include "ash/quick_pair/common/log_buffer.h"
 #include "ash/quick_pair/common/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/values.h"
 #include "content/public/browser/web_ui_message_handler.h"
+
+namespace ash {
+namespace quick_pair {
+class FastPairNotificationController;
+}  // namespace quick_pair
+}  // namespace ash
 
 // WebUIMessageHandler for the Quick Pair debug page at
 // chrome://nearby-internals
@@ -44,6 +51,9 @@ class QuickPairHandler : public content::WebUIMessageHandler,
   void NotifyFastPairError(const base::ListValue* args);
   void NotifyFastPairDiscovery(const base::ListValue* args);
   void NotifyFastPairPairing(const base::ListValue* args);
+
+  std::unique_ptr<ash::quick_pair::FastPairNotificationController>
+      fast_pair_notification_controller_;
 
   base::ScopedObservation<ash::quick_pair::LogBuffer,
                           ash::quick_pair::LogBuffer::Observer>
