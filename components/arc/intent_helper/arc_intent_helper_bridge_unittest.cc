c6533132740ce8a31a2ecda518aefac3af3406f5
Tim Sergeant <tsergeant@chromium.org>
2021-07-08 17:57:19 +0000

    ArcIntentHelper: Allow Web Apps to open for localhost URLs
    This is consistent with how PWAs are treated elsewhere in the system,
    and allows end-to-end testing using a PWA hosted on a local HTTP server.
    
    Bug: 1226730
    Change-Id: I9d270e26171bc3f63645d9eca2cb07c6c7abacf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009388
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899621}

--- components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
@@ -382,10 +382,14 @@ TEST_F(ArcIntentHelperTest, TestOnOpenUrl) {
             test_open_url_delegate_->TakeLastOpenedUrl());
 }
 
-// Tests that OnOpenWebApp opens only HTTPS URLs.
+// Tests that OnOpenWebApp opens only HTTPS URLs or localhost.
 TEST_F(ArcIntentHelperTest, TestOnOpenWebApp) {
   instance_->OnOpenWebApp("http://google.com");
   EXPECT_EQ(GURL(), test_open_url_delegate_->TakeLastOpenedUrl());
+
+  instance_->OnOpenWebApp("http://localhost/");
+  EXPECT_EQ(GURL("http://localhost/"),
+            test_open_url_delegate_->TakeLastOpenedUrl());
 
   instance_->OnOpenWebApp("https://google.com");
   EXPECT_EQ(GURL("https://google.com"),
@@ -435,6 +439,11 @@ TEST_F(ArcIntentHelperTest, TestOnOpenAppWithIntent) {
                                    mojom::LaunchIntent::New());
     EXPECT_FALSE(test_open_url_delegate_->TakeLastOpenedUrl().is_valid());
     EXPECT_TRUE(test_open_url_delegate_->TakeLastOpenedIntent().is_null());
+
+    instance_->OnOpenAppWithIntent(GURL("http://localhost:8000/foo"),
+                                   mojom::LaunchIntent::New());
+    EXPECT_TRUE(test_open_url_delegate_->TakeLastOpenedUrl().is_valid());
+    EXPECT_FALSE(test_open_url_delegate_->TakeLastOpenedIntent().is_null());
 
     instance_->OnOpenAppWithIntent(GURL("chrome://settings"),
                                    mojom::LaunchIntent::New());
