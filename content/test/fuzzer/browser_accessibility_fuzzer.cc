378edcf63d1b01e660f7e10805240526be284fb2
Jacques Newman <janewman@microsoft.com>
2021-07-08 16:45:38 +0000

    Add BrowserTaskEnvironment to the browser accessibility fuzzer.
    CL 2832220 added usage of `GetUIThreadTaskRunner({})->PostDelayedTask`,
    this requires a task environment, which was not available in the browser
    accessibility fuzzer.
    This change adds one, fixing the (fuzzer-only) crash.
    
    Bug: 1225337
    Change-Id: Ic92d0bde2186fcfbaa0d0710023235e1e2cbcd32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009976
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Jacques Newman <janewman@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#899584}

--- content/test/fuzzer/browser_accessibility_fuzzer.cc
+++ content/test/fuzzer/browser_accessibility_fuzzer.cc
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/accessibility/browser_accessibility_manager.h"
 #include "content/browser/accessibility/one_shot_accessibility_tree_search.h"
 #include "content/browser/accessibility/test_browser_accessibility_delegate.h"
+#include "content/public/test/browser_task_environment.h"
 
 struct Env {
   Env() { base::CommandLine::Init(0, nullptr); }
+  content::BrowserTaskEnvironment task_environment;
   base::AtExitManager at_exit;
 };
 
