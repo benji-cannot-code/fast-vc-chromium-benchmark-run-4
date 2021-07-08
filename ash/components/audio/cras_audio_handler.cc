ea3c2cbf034411ea8b96f0d549b3787758f371af
Anton Swifton <swifton@google.com>
2021-07-08 02:20:57 +0000

    Revert "Set the input noise cancellation pref to 'on' by default."
    This reverts commit 11837aa2b3767ec631dabeb1a5f71a3550683516.
    
    Reason for revert: Disable the feature due to a stability crash:
    b/187678153
    
    Original change's description:
    > Set the input noise cancellation pref to 'on' by default.
    >
    > Input noise cancellation will be enabled by default.
    > The feature flag EnableInputNoiseCancellationUi will be
    > enabled by default.
    >
    > Bug: 1205197
    > Test: ash_unittests
    > Change-Id: Ia494175ececde1d2e585735f3b20cf3d365a87f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2937657
    > Commit-Queue: Anton Swifton <swifton@google.com>
    > Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#890524}
    
    Bug: 1205197
    Change-Id: Ia3bbd3c362dd2c9ee40aa488be2b361df847ac19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003764
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Anton Swifton <swifton@google.com>
    Cr-Commit-Position: refs/heads/master@{#899384}

--- ash/components/audio/cras_audio_handler.cc
+++ ash/components/audio/cras_audio_handler.cc
@@ -1961,6 +1961,7 @@ void CrasAudioHandler::HandleGetDefaultOutputBufferSize(
 }
 
 bool CrasAudioHandler::noise_cancellation_supported() const {
+  DCHECK(main_task_runner_->BelongsToCurrentThread());
   return noise_cancellation_supported_;
 }
 
