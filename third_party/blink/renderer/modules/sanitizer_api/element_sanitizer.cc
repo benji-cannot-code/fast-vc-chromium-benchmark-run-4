1fcf60522b8056c1f08fcba0d318db07db7b7a6d
Daniel Vogelheim <vogelheim@chromium.org>
2021-07-08 13:58:24 +0000

    [Sanitizer API] Implement new string API, Element.SetSanitizedHTML.
    Follow-on CL for crrev.com/c/2969103, which implements the other part
    Sanitizer.sanitizeFor.
    
    Ref: https://github.com/WICG/sanitizer-api/pull/99
    Bug: 1221153
    Change-Id: I9648112eed212d7b9f5d539de1551eaf1017edae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998469
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899531}

---
+++ third_party/blink/renderer/modules/sanitizer_api/element_sanitizer.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "element_sanitizer.h"
+
+#include "third_party/blink/renderer/modules/sanitizer_api/sanitizer.h"
+
+namespace blink {
+
+void ElementSanitizer::SetSanitizedHTML(ScriptState* script_state,
+                                        Element& element,
+                                        const String& markup,
+                                        Sanitizer* sanitizer,
+                                        ExceptionState& exception_state) {
+  sanitizer->ElementSetSanitizedHTML(script_state, element, markup,
+                                     exception_state);
+}
+
+}  // namespace blink
