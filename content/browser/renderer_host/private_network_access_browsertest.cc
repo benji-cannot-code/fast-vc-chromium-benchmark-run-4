c20aa3d7f9004b549f60254023c82e5c0580c539
Titouan Rigoudy <titouan@chromium.org>
2021-07-08 18:36:25 +0000

    [Private Network Access] Enable deprecation trial by default.
    mkwst@ confirms that yoavweiss@' LGTM on the Ready For Deprecation Trial
    blink-dev@ thread is enough, no disagreements remain among API owners.
    
    Bug: chromium:1225977
    Change-Id: Ie04edacc8c640dfe52bd6ec5b1aad3c4059aeb66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3013537
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899638}

--- content/browser/renderer_host/private_network_access_browsertest.cc
+++ content/browser/renderer_host/private_network_access_browsertest.cc
@@ -2341,20 +2341,21 @@ class OriginTrialURLLoaderInterceptor {
 };
 
 // Test with insecure private network requests blocked, excluding navigations.
-class PrivateNetworkAccessDeprecationTrialBrowserTest
+class PrivateNetworkAccessDeprecationTrialDisabledBrowserTest
     : public PrivateNetworkAccessBrowserTestBase {
  public:
-  PrivateNetworkAccessDeprecationTrialBrowserTest()
+  PrivateNetworkAccessDeprecationTrialDisabledBrowserTest()
       : PrivateNetworkAccessBrowserTestBase(
             {
                 features::kBlockInsecurePrivateNetworkRequests,
-                features::kBlockInsecurePrivateNetworkRequestsDeprecationTrial,
             },
-            {}) {}
+            {
+                features::kBlockInsecurePrivateNetworkRequestsDeprecationTrial,
+            }) {}
 };
 
-IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessBrowserTest,
-                       DeprecationTrialDisabled) {
+IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessDeprecationTrialDisabledBrowserTest,
+                       OriginEnabledDoesNothing) {
   OriginTrialURLLoaderInterceptor interceptor;
 
   EXPECT_TRUE(NavigateToURL(shell(), interceptor.EnabledUrl()));
@@ -2367,8 +2368,7 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessBrowserTest,
             network::mojom::PrivateNetworkRequestPolicy::kBlock);
 }
 
-IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessDeprecationTrialBrowserTest,
-                       OriginEnabled) {
+IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessBrowserTest, OriginEnabled) {
   OriginTrialURLLoaderInterceptor interceptor;
 
   EXPECT_TRUE(NavigateToURL(shell(), interceptor.EnabledUrl()));
@@ -2383,8 +2383,7 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessDeprecationTrialBrowserTest,
             network::mojom::PrivateNetworkRequestPolicy::kBlock);
 }
 
-IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessDeprecationTrialBrowserTest,
-                       OriginDisabled) {
+IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessBrowserTest, OriginDisabled) {
   OriginTrialURLLoaderInterceptor interceptor;
 
   EXPECT_TRUE(NavigateToURL(shell(), interceptor.DisabledUrl()));
