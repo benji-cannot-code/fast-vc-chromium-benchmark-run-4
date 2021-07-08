0c976a4ab27eb1e45465a24657163c093d3caed2
Viktor Semeniuk <vsemeniuk@google.com>
2021-07-08 12:55:09 +0000

    Helper object to Add/Remove phished credentials
    InsecureCredentialsHelper will be used from the
    ChromePasswordProtectionService instead of
    PasswordStore::{Add,Remove}InsecureCredentials.
    
    Bug: 1223022
    Change-Id: I2e6365e6c986017793072f21d7cdcc3d426739e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009340
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899515}

---
+++ components/password_manager/core/browser/insecure_credentials_helper.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_INSECURE_CREDENTIALS_HELPER_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_INSECURE_CREDENTIALS_HELPER_H_
+
+namespace password_manager {
+
+class PasswordStoreInterface;
+struct MatchingReusedCredential;
+
+// Adds PhishedCredentials for matching PasswordForms from the |store| if it was
+// not marked as phished already.
+void AddPhishedCredentials(PasswordStoreInterface* store,
+                           const MatchingReusedCredential& credential);
+
+// Removes PhishedCredentials for matching PasswordForms from the |store| if it
+// was marked as phished.
+void RemovePhishedCredentials(PasswordStoreInterface* store,
+                              const MatchingReusedCredential& credential);
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_INSECURE_CREDENTIALS_HELPER_H_
