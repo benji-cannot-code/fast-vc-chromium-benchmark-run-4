3556d4c84335d481a09467f22bfdea4c5d030a7a
wutao <wutao@chromium.org>
2021-07-08 02:35:54 +0000

    assistant: Fix incomplete type
    Bug: NA
    Test: Local bulld
    
    Cq-Include-Trybots: luci.chrome.try:chromeos-eve-chrome,chromeos-kevin-chrome,linux-chromeos-chrome
    Change-Id: I1a4ffc7ae148fca296ee939d477e45d37dc07415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3012274
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Auto-Submit: Tao Wu <wutao@chromium.org>
    Owners-Override: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899407}

--- chromeos/services/libassistant/chromium_http_connection.cc
+++ chromeos/services/libassistant/chromium_http_connection.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "net/base/load_flags.h"
 #include "services/network/public/cpp/header_util.h"
+#include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
