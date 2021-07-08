7522ee592be82b3385483da1083c77719e91a82f
Bruce Dawson <brucedawson@chromium.org>
2021-07-08 20:56:16 +0000

    Remove windows.h from blink's precompile.h
    Precompiled header files are used in non-goma builds of Chrome. This
    change has no effect on goma builds, but has been tested on non-goma
    builds.
    
    As we work to remove windows.h from most translation units (it is
    already gone from most translation units in blink) it is increasingly
    inappropriate to include it in precompile.h. Doing so reduces the size
    of the blink .pch files from ~15.1 MB to ~7.7 MB. This saving is
    multiplied by the number of blink .pch files which is somewhere around
    60 for a savings of ~440 MB. This also reduces the non-trivial time to
    load the .pch files, leading to faster non-goma build times.
    
    The main advantage, however, is that this further reduces the amount of
    namespace pollution from the many windows.h macros, and reduces the
    differences between goma and non-goma builds.
    
    Bug: 796644
    Change-Id: I9a4af6874854153cc4ef0465d779b81cab40695e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015668
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899695}

--- third_party/blink/renderer/build/win/precompile.h
+++ third_party/blink/renderer/build/win/precompile.h
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define _USE_MATH_DEFINES  // Make math.h behave like other platforms.
 
-#include <Windows.h>
-
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
