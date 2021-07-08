b8ea81efe70bea6d77bece3b35ec79b2482ce646
Felipe Andrade <fsandrade@chromium.org>
2021-07-08 17:27:49 +0000

    Remove unused method from policy manager
    Remove unused getter method from DeviceCloudPolicyManagerChromeOS.
    This clean up work was identified while working on crbug.com/1209246.
    
    Bug: 1209246
    Change-Id: I006485fe0ed05c277d9342b6754d6e22a4030ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3014897
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899608}

--- chrome/browser/ash/policy/core/device_cloud_policy_manager_chromeos.h
+++ chrome/browser/ash/policy/core/device_cloud_policy_manager_chromeos.h
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace attestation {
 class AttestationPolicyObserver;
-class EnrollmentPolicyObserver;
 class EnrollmentCertificateUploader;
+class EnrollmentPolicyObserver;
 class MachineCertificateUploader;
 }  // namespace attestation
 }  // namespace ash
@@ -130,13 +130,6 @@ class DeviceCloudPolicyManagerChromeOS : public CloudPolicyManager {
       bool component_policy_disabled_for_testing) {
     component_policy_disabled_for_testing_ =
         component_policy_disabled_for_testing;
-  }
-
-  // Return a pointer to the enrollment certificate uploader. The callers do
-  // not take ownership of that pointer.
-  ash::attestation::EnrollmentCertificateUploader*
-  GetEnrollmentCertificateUploader() {
-    return enrollment_certificate_uploader_.get();
   }
 
   // Return a pointer to the machine certificate uploader. The callers do
