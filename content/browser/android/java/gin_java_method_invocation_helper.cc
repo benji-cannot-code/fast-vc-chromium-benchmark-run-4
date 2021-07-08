fc102a0c64de7e8634b5f70e09f59ba5b5840908
David Bertoni <dbertoni@chromium.org>
2021-07-08 17:30:41 +0000

    [CodeHealth] Remove uses of ListValue::DeepCopy() in /content/browser/android/java.
    Where possible, the code is migrated to use Value::Clone(). Where
    that's not practical, calls are replaced with
    ListValue::CreateDeepCopy().
    
    This CL was uploaded by git cl split.
    
    R=torne@chromium.org
    
    Bug: 1187108
    Change-Id: I425c290dea8e96b2a834bf4f76e778c496c5bd09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003684
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899611}

--- content/browser/android/java/gin_java_method_invocation_helper.cc
+++ content/browser/android/java/gin_java_method_invocation_helper.cc
@@ -36,7 +36,7 @@ GinJavaMethodInvocationHelper::GinJavaMethodInvocationHelper(
     const base::ListValue& arguments)
     : object_(std::move(object)),
       method_name_(method_name),
-      arguments_(arguments.DeepCopy()),
+      arguments_(arguments.CreateDeepCopy()),
       invocation_error_(kGinJavaBridgeNoError) {}
 
 GinJavaMethodInvocationHelper::~GinJavaMethodInvocationHelper() {}
@@ -179,7 +179,7 @@ void GinJavaMethodInvocationHelper::SetInvocationError(
 void GinJavaMethodInvocationHelper::SetPrimitiveResult(
     const base::ListValue& result_wrapper) {
   holds_primitive_result_ = true;
-  primitive_result_.reset(result_wrapper.DeepCopy());
+  primitive_result_ = result_wrapper.CreateDeepCopy();
 }
 
 void GinJavaMethodInvocationHelper::SetObjectResult(
