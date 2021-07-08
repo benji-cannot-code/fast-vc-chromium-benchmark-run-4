21579fc64b5c3c35f138f7eb958d2dee3d9ee97e
Colin Blundell <blundell@chromium.org>
2021-07-08 12:02:37 +0000

    [Safe Browsing] Componentize SafeBrowsingMetricsCollector
    This code is a dependency of //chrome's SafeBrowsingBlockingPage, which
    we are looking to componentize and share with //weblayer. Fortunately,
    componentization of SafeBrowsingMetricsCollector is straightforward as
    it has no //chrome dependencies.
    
    Bug: 1226567
    Change-Id: I28fe88ead292f5c8b5db8d917456b98228b9fa03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3006351
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899500}

--- chrome/browser/safe_browsing/safe_browsing_metrics_collector_factory.cc
+++ chrome/browser/safe_browsing/safe_browsing_metrics_collector_factory.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/safe_browsing/safe_browsing_metrics_collector.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/safe_browsing/content/browser/safe_browsing_metrics_collector.h"
 #include "content/public/browser/browser_context.h"
 
 namespace safe_browsing {
--- components/safe_browsing/content/browser/safe_browsing_metrics_collector.cc
+++ components/safe_browsing/content/browser/safe_browsing_metrics_collector.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/safe_browsing/safe_browsing_metrics_collector.h"
+#include "components/safe_browsing/content/browser/safe_browsing_metrics_collector.h"
 
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
