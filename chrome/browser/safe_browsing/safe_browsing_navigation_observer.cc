28b84dcec6b69cb7229af98c791b2585eaba8cf6
Colin Blundell <blundell@chromium.org>
2021-07-08 09:47:38 +0000

    [Safe Browsing] Eliminate unneeded //chrome-level includes
    By eliminating these includes, we get more clarity on the challenges to
    componentizing this code.
    
    Bug: 1226567
    Change-Id: I07c4674076fd93f7e81fe55a889b25eb7bea074d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3006350
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899483}

--- chrome/browser/safe_browsing/safe_browsing_navigation_observer.cc
+++ chrome/browser/safe_browsing/safe_browsing_navigation_observer.cc
@@ -8,12 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/time/time.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/safe_browsing_navigation_observer_manager.h"
 #include "chrome/browser/safe_browsing/safe_browsing_navigation_observer_manager_factory.h"
-#include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "components/page_info/page_info_ui.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/browser_context.h"
