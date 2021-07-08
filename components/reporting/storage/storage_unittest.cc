de0682c3f8a696689410297ad4977a4a44311e33
Leonid Baraz <lbaraz@chromium.org>
2021-07-08 03:43:08 +0000

    Further ensure upload retry on failure
    If upload succeeded but during retry delay period not all events
    have been confirmed, retry the upload, as if it failed.
    
    Bug: b:192666219
    Change-Id: Icba776f01b06d2017cd0b3925f9073d96fc2f3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011940
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899417}

--- components/reporting/storage/storage_unittest.cc
+++ components/reporting/storage/storage_unittest.cc
@@ -849,6 +849,9 @@ TEST_P(StorageTest, WriteIntoNewStorageAndUploadWithKeyUpdate) {
     EXPECT_OK(storage_->Flush(MANUAL_BATCH));
   }
 
+  // Confirm written data to prevent upload retry.
+  ConfirmOrDie(MANUAL_BATCH, /*sequencing_id=*/2);
+
   // Write more data.
   WriteStringOrDie(MANUAL_BATCH, kMoreData[0]);
   WriteStringOrDie(MANUAL_BATCH, kMoreData[1]);
@@ -865,9 +868,6 @@ TEST_P(StorageTest, WriteIntoNewStorageAndUploadWithKeyUpdate) {
       .WillOnce(
           WithArg<1>(Invoke([&waiter](MockUploadClient* mock_upload_client) {
             MockUploadClient::SetUp(MANUAL_BATCH, mock_upload_client, &waiter)
-                .Required(0, kData[0])
-                .Required(1, kData[1])
-                .Required(2, kData[2])
                 .Required(3, kMoreData[0])
                 .Required(4, kMoreData[1])
                 .Required(5, kMoreData[2]);
@@ -1276,6 +1276,9 @@ TEST_P(StorageTest, WriteAndRepeatedlyUploadMultipleQueues) {
 
   WriteStringOrDie(SLOW_BATCH, kMoreData[1]);
 
+  // Confirm #1 IMMEDIATE, removing data #0 and #1, to prevent upload retry.
+  ConfirmOrDie(IMMEDIATE, /*sequencing_id=*/1);
+
   // Set uploader expectations for FAST_BATCH and SLOW_BATCH.
   {
     test::TestCallbackAutoWaiter waiter;
@@ -1295,9 +1298,6 @@ TEST_P(StorageTest, WriteAndRepeatedlyUploadMultipleQueues) {
   // Confirm #0 SLOW_BATCH, removing data #0
   ConfirmOrDie(SLOW_BATCH, /*sequencing_id=*/0);
 
-  // Confirm #1 IMMEDIATE, removing data #0 and #1
-  ConfirmOrDie(IMMEDIATE, /*sequencing_id=*/1);
-
   // Add more data
   {
     test::TestCallbackAutoWaiter waiter;
@@ -1306,7 +1306,6 @@ TEST_P(StorageTest, WriteAndRepeatedlyUploadMultipleQueues) {
         .WillOnce(
             WithArg<1>(Invoke([&waiter](MockUploadClient* mock_upload_client) {
               MockUploadClient::SetUp(IMMEDIATE, mock_upload_client, &waiter)
-                  .Possible(1, kData[1])
                   .Required(2, kData[2]);
               return Status::StatusOK();
             })))
@@ -1314,6 +1313,9 @@ TEST_P(StorageTest, WriteAndRepeatedlyUploadMultipleQueues) {
     WriteStringOrDie(IMMEDIATE, kData[2]);
   }
   WriteStringOrDie(SLOW_BATCH, kMoreData[2]);
+
+  // Confirm #2 IMMEDIATE, to prevent upload retry.
+  ConfirmOrDie(IMMEDIATE, /*sequencing_id=*/2);
 
   // Set uploader expectations for FAST_BATCH and SLOW_BATCH.
   {
