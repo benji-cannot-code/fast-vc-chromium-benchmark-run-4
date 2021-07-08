aaee2073ffb28b113d4c1b6177bb52b1278ea550
Bruce Dawson <brucedawson@chromium.org>
2021-07-08 23:14:50 +0000

    Changes to remoting to allow removing windows.h
    Before we can remove includes of windows.h from header files we need to
    _add_ includes of windows.h to .cc files that require it. This change
    does this for source files in remoting. Windows.h needs to be included
    before commctrl.h.
    
    Bug: 796644
    Change-Id: I52c42f22dc24f1a54de47f26126e020c1a191288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016912
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899751}

--- remoting/host/host_main.cc
+++ remoting/host/host_main.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_APPLE)
 
 #if defined(OS_WIN)
+#include <windows.h>
+
 #include <commctrl.h>
 #include <shellapi.h>
 #endif  // defined(OS_WIN)
--- remoting/host/it2me/it2me_native_messaging_host_main.cc
+++ remoting/host/it2me/it2me_native_messaging_host_main.cc
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_APPLE)
 
 #if defined(OS_WIN)
+#include <windows.h>
+
 #include <commctrl.h>
 
 #include "remoting/host/switches.h"
