afd03788fb96c610cf1e93b0c327ed87c3f9ed74
Hans Wennborg <hans@chromium.org>
2021-07-08 14:44:12 +0000

    Reland "[mojo] Enable use of forward declarations of type-mapped mojom types"
    This is a reland of 63b02ed4d64037d4a506946af78975a3370a1591
    
    Original change's description:
    > [mojo] Enable use of forward declarations of type-mapped mojom types
    >
    > This makes it possible to reduce generated header sizes significantly
    > when the full type is not necessary.
    >
    > For example, url_loader_factory.mojom defines an interface using the
    > URLRequest mojom type, which is type-mapped to the C++ type
    > ResourceRequest.
    >
    > Making it possible to forward declare ResourceRequest avoids the need
    > to include url_request_mojom.h in url_loader_factory.mojom.h, which
    > reduces the compiler input size for the chrome target on Linux by
    > 1.3 GB, saving 19 CPU-minutes of compile time on my machine.
    >
    > Bug: 1226821, 242216
    > Change-Id: I7ba187f714183ef5a7bc909458276aa4aeb05b2f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3008756
    > Commit-Queue: Hans Wennborg <hans@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#899267}
    
    Bug: 1226821, 242216
    Change-Id: I4e118bb79df04e0483c13128939a8f990fdcf5bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3014316
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899547}

--- mojo/public/tools/bindings/generate_type_mappings.py
+++ mojo/public/tools/bindings/generate_type_mappings.py
@@ -83,6 +83,7 @@ def LoadCppTypemapConfig(path):
       for entry in config['types']:
         configs[entry['mojom']] = {
             'typename': entry['cpp'],
+            'forward_declaration': entry.get('forward_declaration', None),
             'public_headers': config.get('traits_headers', []),
             'traits_headers': config.get('traits_private_headers', []),
             'copyable_pass_by_value': entry.get('copyable_pass_by_value',
