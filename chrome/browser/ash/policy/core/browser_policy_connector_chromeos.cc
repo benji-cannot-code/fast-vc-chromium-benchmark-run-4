87c8e56dd746f58c4e4203b35de043e0e500f040
Sanja Perisic <sanjaperisic@chromium.org>
2021-07-08 17:08:27 +0000

    Remove native timer from DeviceScheduledUpdateChecker
    Remove NativeTimer from DeviceScheduledUpdateChecker and use ScheduledTaskExecutor instead for starting the timer.
    
    Bug: 1211256
    Test: DeviceScheduledUpdateCheckerTest.*
    Change-Id: I07eb68c620a6e6657d36e4acf86e168a640675d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2978513
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Sanja Perisic <sanjaperisic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899591}

--- chrome/browser/ash/policy/core/browser_policy_connector_chromeos.cc
+++ chrome/browser/ash/policy/core/browser_policy_connector_chromeos.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/policy/core/browser_policy_connector_chromeos.h"
 
+#include <memory>
 #include <string>
 #include <utility>
 
@@ -58,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/networking/device_network_configuration_updater.h"
 #include "chrome/browser/chromeos/policy/remote_commands/affiliated_remote_commands_invalidator.h"
 #include "chrome/browser/chromeos/policy/scheduled_task_handler/device_scheduled_update_checker.h"
+#include "chrome/browser/chromeos/policy/scheduled_task_handler/scheduled_task_executor.h"
 #include "chrome/browser/chromeos/policy/server_backed_state/device_cloud_state_keys_uploader.h"
 #include "chrome/browser/chromeos/policy/server_backed_state/server_backed_state_keys_broker.h"
 #include "chrome/browser/chromeos/printing/bulk_printers_calculator_factory.h"
@@ -280,7 +282,9 @@ void BrowserPolicyConnectorChromeOS::Init(
   device_scheduled_update_checker_ =
       std::make_unique<DeviceScheduledUpdateChecker>(
           ash::CrosSettings::Get(),
-          chromeos::NetworkHandler::Get()->network_state_handler());
+          chromeos::NetworkHandler::Get()->network_state_handler(),
+          std::make_unique<ScheduledTaskExecutor>(
+              update_checker_internal::kUpdateCheckTimerTag));
 
   chromeos::BulkPrintersCalculatorFactory* calculator_factory =
       chromeos::BulkPrintersCalculatorFactory::Get();
