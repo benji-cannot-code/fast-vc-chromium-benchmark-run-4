b4e71ef6ca77cabd4012fb5a8f8230ceea91f162
Greg Thompson <grt@chromium.org>
2021-07-08 13:37:39 +0000

    Fix compile break due to missing #include.
    windows.h is needed for Windows error code values.
    
    Bug: 1227245
    Change-Id: I52b37c6c14c8a8ea3b1decaf9c5e022a7b9cacf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3013695
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899522}

--- chrome/browser/win/conflicts/third_party_conflicts_manager.cc
+++ chrome/browser/win/conflicts/third_party_conflicts_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/win/conflicts/third_party_conflicts_manager.h"
 
+#include <windows.h>
+
 #include <string>
 #include <utility>
 
