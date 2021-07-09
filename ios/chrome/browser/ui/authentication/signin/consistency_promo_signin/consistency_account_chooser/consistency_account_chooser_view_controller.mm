e357959f369a950c83046e4531cef54297dc1250
Nohemi Fernandez <fernandex@chromium.org>
2021-07-09 07:26:10 +0000

    [iOS][MICE] Use settings-refresh style for web sign-in account list.
    Bug: 1218432
    Change-Id: I4e806b4b6bac7c90d3abfee1b1879fd205aa223d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3013539
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899896}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_account_chooser/consistency_account_chooser_view_controller.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_account_chooser/consistency_account_chooser_view_controller.mm
@@ -71,11 +71,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (ConsistencyAccountChooserTableViewController*)tableViewController {
   if (!_tableViewController) {
-    UITableViewStyle style = base::FeatureList::IsEnabled(kSettingsRefresh)
-                                 ? ChromeTableViewStyle()
-                                 : UITableViewStylePlain;
     _tableViewController = [[ConsistencyAccountChooserTableViewController alloc]
-        initWithStyle:style];
+        initWithStyle:UITableViewStyleInsetGrouped];
   }
   return _tableViewController;
 }
