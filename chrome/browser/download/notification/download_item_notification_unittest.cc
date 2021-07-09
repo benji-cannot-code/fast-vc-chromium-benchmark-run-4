6d8f6347da99169ecde867d6a0b2e6c764416d67
Xinghui Lu <xinghuilu@chromium.org>
2021-07-09 23:53:35 +0000

    Record DangerousDownloadWarningShown histograms on ChromeOS.
    These histograms are recorded in download shelves already. ChromeOS is
    using the notification rather than download shelves, so the histograms
    are missing.
    
    Bug: 1217350
    Change-Id: I2ca0b637d66a4fce4daba9d142521583b8b92068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017454
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900208}

--- chrome/browser/download/notification/download_item_notification_unittest.cc
+++ chrome/browser/download/notification/download_item_notification_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/download/chrome_download_manager_delegate.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
+#include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/mock_download_item.h"
 #include "components/enterprise/common/proto/download_item_reroute_info.pb.h"
 #include "content/public/browser/download_item_utils.h"
@@ -156,6 +158,7 @@ class DownloadItemNotificationTest : public testing::Test {
 };
 
 TEST_F(DownloadItemNotificationTest, ShowAndCloseNotification) {
+  base::HistogramTester histograms;
   EXPECT_EQ(0u, NotificationCount());
 
   // Shows a notification
@@ -173,6 +176,32 @@ TEST_F(DownloadItemNotificationTest, ShowAndCloseNotification) {
 
   // Makes sure the DownloadItem::Cancel() is never called.
   EXPECT_CALL(*download_item_, Cancel(_)).Times(0);
+
+  // Not logged because the download is safe.
+  histograms.ExpectTotalCount("Download.ShowedDownloadWarning", 0);
+}
+
+TEST_F(DownloadItemNotificationTest, ShowAndCloseDangerousNotification) {
+  base::HistogramTester histograms;
+  EXPECT_CALL(*download_item_, GetDangerType())
+      .WillRepeatedly(Return(download::DOWNLOAD_DANGER_TYPE_DANGEROUS_CONTENT));
+  EXPECT_CALL(*download_item_, IsDangerous()).WillRepeatedly(Return(true));
+
+  // Shows a notification
+  CreateDownloadItemNotification();
+  download_item_->NotifyObserversDownloadOpened();
+  EXPECT_EQ(1u, NotificationCount());
+
+  // Closes it once.
+  RemoveNotification();
+
+  // Confirms that the notification is closed.
+  EXPECT_EQ(0u, NotificationCount());
+
+  // The download warning showed histogram is logged.
+  histograms.ExpectBucketCount("Download.ShowedDownloadWarning",
+                               download::DOWNLOAD_DANGER_TYPE_DANGEROUS_CONTENT,
+                               1);
 }
 
 TEST_F(DownloadItemNotificationTest, PauseAndResumeNotification) {
