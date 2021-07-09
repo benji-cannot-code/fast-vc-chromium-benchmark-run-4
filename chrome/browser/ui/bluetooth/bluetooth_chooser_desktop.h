d5291dde819ba7365a4793299afa19084286ebeb
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2021-07-09 10:12:35 +0000

    Move chrome::ShowDeviceChooserDialog calls to ChromeBluetoothDelegate.
    This aligns with the pattern established in more recent code such as
    SerialChooser.
    
    This removes the last //chrome layer dependencies from
    BluetoothChooserDesktop and BluetoothScanningPromptDesktop in
    preparation for componentization.
    
    Bug: 1076462
    Change-Id: I6781a03f335fc0694fac2677b12c5da72e187120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2996823
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899941}

--- chrome/browser/ui/bluetooth/bluetooth_chooser_desktop.h
+++ chrome/browser/ui/bluetooth/bluetooth_chooser_desktop.h
@@ -6,17 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_BLUETOOTH_BLUETOOTH_CHOOSER_DESKTOP_H_
 #define CHROME_BROWSER_UI_BLUETOOTH_BLUETOOTH_CHOOSER_DESKTOP_H_
 
-#include "base/callback.h"
+#include "base/callback_helpers.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "content/public/browser/bluetooth_chooser.h"
 
-namespace content {
-class RenderFrameHost;
-}  // namespace content
-
 namespace permissions {
 class BluetoothChooserController;
+class ChooserController;
 }  // namespace permissions
 
 // Represents a Bluetooth chooser to ask the user to select a Bluetooth
@@ -24,9 +21,14 @@ class BluetoothChooserController;
 // BluetoothChooserAndroid implements the mobile part.
 class BluetoothChooserDesktop : public content::BluetoothChooser {
  public:
+  // The OnceClosure returned by |show_dialog_callback| can be invoked to close
+  // the dialog. It should be a no-op to invoke the closure if the dialog has
+  // already been closed by the user.
   BluetoothChooserDesktop(
-      content::RenderFrameHost* frame,
-      const content::BluetoothChooser::EventHandler& event_handler);
+      std::unique_ptr<permissions::BluetoothChooserController> controller,
+      base::OnceCallback<
+          base::OnceClosure(std::unique_ptr<permissions::ChooserController>)>
+          show_dialog_callback);
   ~BluetoothChooserDesktop() override;
 
   // BluetoothChooser:
@@ -46,7 +48,7 @@ class BluetoothChooserDesktop : public content::BluetoothChooser {
 
   // Closes the displayed UI if it is still open. Used to ensure the bubble
   // closes if this controller is torn down.
-  base::OnceClosure close_closure_;
+  base::ScopedClosureRunner close_closure_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(BluetoothChooserDesktop);
 };
--- chrome/browser/ui/bluetooth/bluetooth_scanning_prompt_desktop.h
+++ chrome/browser/ui/bluetooth/bluetooth_scanning_prompt_desktop.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_BLUETOOTH_BLUETOOTH_SCANNING_PROMPT_DESKTOP_H_
 #define CHROME_BROWSER_UI_BLUETOOTH_BLUETOOTH_SCANNING_PROMPT_DESKTOP_H_
 
-#include "base/callback.h"
+#include <string>
+
+#include "base/callback_helpers.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "content/public/browser/bluetooth_scanning_prompt.h"
@@ -17,6 +19,7 @@ class RenderFrameHost;
 
 namespace permissions {
 class BluetoothScanningPromptController;
+class ChooserController;
 }  // namespace permissions
 
 // Represents a Bluetooth scanning prompt to ask the user permission to
@@ -24,9 +27,16 @@ class BluetoothScanningPromptController;
 // devices. This implementation is for desktop.
 class BluetoothScanningPromptDesktop : public content::BluetoothScanningPrompt {
  public:
-  explicit BluetoothScanningPromptDesktop(
+  // The OnceClosure returned by |show_dialog_callback| can be invoked to close
+  // the dialog. It should be a no-op to invoke the closure if the dialog has
+  // already been closed by the user.
+  BluetoothScanningPromptDesktop(
       content::RenderFrameHost* frame,
-      const content::BluetoothScanningPrompt::EventHandler& event_handler);
+      const content::BluetoothScanningPrompt::EventHandler& event_handler,
+      std::u16string title,
+      base::OnceCallback<
+          base::OnceClosure(std::unique_ptr<permissions::ChooserController>)>
+          show_dialog_callback);
   ~BluetoothScanningPromptDesktop() override;
 
   // content::BluetoothScanningPrompt:
@@ -41,7 +51,7 @@ class BluetoothScanningPromptDesktop : public content::BluetoothScanningPrompt {
 
   // Closes the displayed UI, if there is one. This is used to ensure the UI
   // closes if this controller is destroyed.
-  base::OnceClosure close_closure_;
+  base::ScopedClosureRunner close_closure_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(BluetoothScanningPromptDesktop);
 };
