61aebeb07d121f7a4b000b0539c7a99e5ac83d22
Anders Hartvoll Ruud <andruud@chromium.org>
2021-07-09 12:39:14 +0000

    Revert "Store base ComputedStyle on main ComputedStyle"
    This reverts commit 4b03422f438df0087ae88aca943b1f55d133bbfa.
    
    Reason for revert: Performance regressions, crbug.com/1227488
    
    Original change's description:
    > Store base ComputedStyle on main ComputedStyle
    >
    > Instead of storing the base ComputedStyle on ElementAnimations, this
    > CL suggesting storing it on the (animated) ComputedStyle itself.
    > Any animated ComputedStyle produced by StyleResolver gets an associated
    > "StyleBaseData" object, which contains the base ComputedStyle and
    > the base "important set".
    >
    > Even without other considerations, this appears to be far simpler to
    > deal with than storing it "on the side", however, the true motivation
    > for this change is related to container queries:
    >
    > Since container queries can run style recalc multiple times for the
    > same element, we essentially pass through multiple style resolves where
    > the "old style" (i.e. current style on the Element) changes each time.
    > In other words, there can be multiple apparent "old styles" (some older
    > than others). In order to deal with transitions correctly, it's
    > necessary to get the *base style* of the *oldest* of the old styles
    > (so to speak). Having to keep track of the "true old style" and
    > "true old *base* style" separately exceeds my pain threshold. It seems
    > far more sensible to simply make the base style of any ComputedStyle
    > reachable from *that* ComputedStyle. That way we only need to keep
    > track of *one* ComputedStyle to access either the animated style,
    > or the associated base style.
    >
    > Bug: 1180159
    >
    > Change-Id: Ia43755438c0e2ba7bf8281826c5621c7f3dbf55d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3010116
    > Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Reviewed-by: Kevin Ellis <kevers@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#899510}
    
    Bug: 1180159
    Change-Id: I90a269f6139d998375ebc87782bc3f519ae4c875
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017983
    Auto-Submit: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#899976}

--- third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
@@ -89,14 +89,13 @@ TEST_F(StyleResolverTest, AnimationBaseComputedStyle) {
   animations.SetAnimationStyleChange(true);
 
   StyleResolver& resolver = GetStyleEngine().GetStyleResolver();
-  auto style1 = resolver.ResolveStyle(div, StyleRecalcContext());
-  ASSERT_TRUE(style1);
-  EXPECT_EQ(20, style1->FontSize());
-  ASSERT_TRUE(style1->GetBaseComputedStyle());
-  EXPECT_EQ(20, style1->GetBaseComputedStyle()->FontSize());
+  ASSERT_TRUE(resolver.ResolveStyle(div, StyleRecalcContext()));
+  EXPECT_EQ(20, resolver.ResolveStyle(div, StyleRecalcContext())->FontSize());
+  ASSERT_TRUE(animations.BaseComputedStyle());
+  EXPECT_EQ(20, animations.BaseComputedStyle()->FontSize());
 
-  // Getting style with customized parent style should not affect previously
-  // produced animation base computed style.
+  // Getting style with customized parent style should not affect cached
+  // animation base computed style.
   const ComputedStyle* parent_style =
       GetDocument().documentElement()->GetComputedStyle();
   StyleRequest style_request;
@@ -104,8 +103,8 @@ TEST_F(StyleResolverTest, AnimationBaseComputedStyle) {
   style_request.layout_parent_override = parent_style;
   EXPECT_EQ(10, resolver.ResolveStyle(div, StyleRecalcContext(), style_request)
                     ->FontSize());
-  ASSERT_TRUE(style1->GetBaseComputedStyle());
-  EXPECT_EQ(20, style1->GetBaseComputedStyle()->FontSize());
+  ASSERT_TRUE(animations.BaseComputedStyle());
+  EXPECT_EQ(20, animations.BaseComputedStyle()->FontSize());
   EXPECT_EQ(20, resolver.ResolveStyle(div, StyleRecalcContext())->FontSize());
 }
 
@@ -136,6 +135,9 @@ TEST_F(StyleResolverTest, BaseReusableIfFontRelativeUnitsAbsent) {
   GetDocument().Lifecycle().AdvanceTo(DocumentLifecycle::kInStyleRecalc);
   StyleForId("div");
 
+  ASSERT_TRUE(div->GetElementAnimations());
+  EXPECT_TRUE(div->GetElementAnimations()->BaseComputedStyle());
+
   StyleResolverState state(GetDocument(), *div);
   EXPECT_TRUE(StyleResolver::CanReuseBaseComputedStyle(state));
 }
@@ -163,12 +165,13 @@ TEST_F(StyleResolverTest, AnimationNotMaskedByImportant) {
 
   div->SetNeedsAnimationStyleRecalc();
   GetDocument().Lifecycle().AdvanceTo(DocumentLifecycle::kInStyleRecalc);
-  auto style = StyleForId("div");
+  StyleForId("div");
 
-  const CSSBitset* bitset = style->GetBaseImportantSet();
+  ASSERT_TRUE(div->GetElementAnimations());
+  const CSSBitset* bitset = div->GetElementAnimations()->BaseImportantSet();
   EXPECT_FALSE(CSSAnimations::IsAnimatingStandardProperties(
       div->GetElementAnimations(), bitset, KeyframeEffect::kDefaultPriority));
-  EXPECT_TRUE(style->GetBaseComputedStyle());
+  EXPECT_TRUE(div->GetElementAnimations()->BaseComputedStyle());
   EXPECT_FALSE(bitset && bitset->Has(CSSPropertyID::kWidth));
   EXPECT_TRUE(bitset && bitset->Has(CSSPropertyID::kHeight));
 }
@@ -233,10 +236,11 @@ TEST_F(StyleResolverTest, AnimationMaskedByImportant) {
 
   div->SetNeedsAnimationStyleRecalc();
   GetDocument().Lifecycle().AdvanceTo(DocumentLifecycle::kInStyleRecalc);
-  auto style = StyleForId("div");
+  StyleForId("div");
 
-  EXPECT_TRUE(style->GetBaseComputedStyle());
-  EXPECT_TRUE(style->GetBaseImportantSet());
+  ASSERT_TRUE(div->GetElementAnimations());
+  EXPECT_TRUE(div->GetElementAnimations()->BaseComputedStyle());
+  EXPECT_TRUE(div->GetElementAnimations()->BaseImportantSet());
 
   StyleResolverState state(GetDocument(), *div);
   EXPECT_FALSE(StyleResolver::CanReuseBaseComputedStyle(state));
@@ -291,6 +295,53 @@ TEST_F(StyleResolverTest,
             ComputedValue("font-size", *StyleForId("target")));
 }
 
+TEST_F(StyleResolverTest, NonCachableStyleCheckDoesNotAffectBaseComputedStyle) {
+  GetDocument().documentElement()->setInnerHTML(R"HTML(
+    <style>
+      .adjust { color: rgb(0, 0, 0); }
+    </style>
+    <div>
+      <div style="color: rgb(0, 128, 0)">
+        <div id="target" style="transition: color 1s linear"></div>
+      </div>
+    </div>
+  )HTML");
+  UpdateAllLifecyclePhasesForTest();
+
+  Element* target = GetDocument().getElementById("target");
+
+  EXPECT_EQ("rgb(0, 128, 0)", ComputedValue("color", *StyleForId("target")));
+
+  // Trigger a transition on an inherited property.
+  target->setAttribute(html_names::kClassAttr, "adjust");
+  UpdateAllLifecyclePhasesForTest();
+  ElementAnimations* element_animations = target->GetElementAnimations();
+  EXPECT_TRUE(element_animations);
+  Animation* transition = (*element_animations->Animations().begin()).key;
+  EXPECT_TRUE(transition);
+
+  // Advance to the midpoint of the transition.
+  transition->setCurrentTime(MakeGarbageCollected<V8CSSNumberish>(500),
+                             ASSERT_NO_EXCEPTION);
+  UpdateAllLifecyclePhasesForTest();
+  EXPECT_EQ("rgb(0, 64, 0)", ComputedValue("color", *StyleForId("target")));
+  EXPECT_TRUE(element_animations->BaseComputedStyle());
+
+  element_animations->ClearBaseComputedStyle();
+
+  // Perform a non-cacheable style resolution, and ensure that the base computed
+  // style is not updated.
+  StyleRequest style_request;
+  style_request.matching_behavior = kMatchAllRulesExcludingSMIL;
+  GetStyleEngine().GetStyleResolver().ResolveStyle(target, StyleRecalcContext(),
+                                                   style_request);
+  EXPECT_FALSE(element_animations->BaseComputedStyle());
+
+  // Computing the style with default args updates the base computed style.
+  EXPECT_EQ("rgb(0, 64, 0)", ComputedValue("color", *StyleForId("target")));
+  EXPECT_TRUE(element_animations->BaseComputedStyle());
+}
+
 class StyleResolverFontRelativeUnitTest
     : public testing::WithParamInterface<const char*>,
       public StyleResolverTest {};
@@ -313,7 +364,8 @@ TEST_P(StyleResolverFontRelativeUnitTest,
   auto computed_style = StyleForId("div");
 
   EXPECT_TRUE(computed_style->HasFontRelativeUnits());
-  EXPECT_TRUE(computed_style->GetBaseComputedStyle());
+  ASSERT_TRUE(div->GetElementAnimations());
+  EXPECT_TRUE(div->GetElementAnimations()->BaseComputedStyle());
 
   StyleResolverState state(GetDocument(), *div);
   EXPECT_FALSE(StyleResolver::CanReuseBaseComputedStyle(state));
@@ -337,7 +389,8 @@ TEST_P(StyleResolverFontRelativeUnitTest,
   auto computed_style = StyleForId("div");
 
   EXPECT_TRUE(computed_style->HasFontRelativeUnits());
-  EXPECT_TRUE(computed_style->GetBaseComputedStyle());
+  ASSERT_TRUE(div->GetElementAnimations());
+  EXPECT_TRUE(div->GetElementAnimations()->BaseComputedStyle());
 
   StyleResolverState state(GetDocument(), *div);
   EXPECT_TRUE(StyleResolver::CanReuseBaseComputedStyle(state));
--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/pass_key.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
-#include "third_party/blink/renderer/core/css/properties/css_bitset.h"
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
 #include "third_party/blink/renderer/core/css/style_auto_color.h"
 #include "third_party/blink/renderer/core/css/style_color.h"
@@ -397,22 +396,6 @@ class ComputedStyle : public ComputedStyleBase,
   const ComputedStyle* AddCachedPseudoElementStyle(
       scoped_refptr<const ComputedStyle>) const;
   void ClearCachedPseudoElementStyles() const;
-
-  // If this ComputedStyle is affected by animation/transitions, then the
-  // unanimated "base" style can be retrieved with this function.
-  //
-  // If this function returns nullptr, then this ComputedStyle is not
-  // affected by animations, and *is* the base style.
-  CORE_EXPORT const ComputedStyle* GetBaseComputedStyle() const;
-
-  // Indicates which properties are !important in the base style.
-  CORE_EXPORT const CSSBitset* GetBaseImportantSet() const;
-
-  CORE_EXPORT const ComputedStyle* GetBaseComputedStyleOrThis() const {
-    if (auto* base = GetBaseComputedStyle())
-      return base;
-    return this;
-  }
 
   /**
    * ComputedStyle properties
