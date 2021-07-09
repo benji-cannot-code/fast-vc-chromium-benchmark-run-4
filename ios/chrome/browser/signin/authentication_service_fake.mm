fa2e157ad416fc4280f4beb33b7dc85d9f608aae
jlebel <jlebel@chromium.org>
2021-07-09 17:31:51 +0000

    [iOS] Rename AuthenticationServiceFake authenticated_identity_
    Rename authenticated_identity_ to primary_identity_
    
    Change-Id: I0475f1afc849579d6c2d94566ada0b33c89aea4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015577
    Auto-Submit: Jérôme <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900061}

--- ios/chrome/browser/signin/authentication_service_fake.mm
+++ ios/chrome/browser/signin/authentication_service_fake.mm
@@ -42,7 +42,7 @@ void AuthenticationServiceFake::SignIn(ChromeIdentity* identity) {
   // AuthenticationService.
   DCHECK(identity);
   sync_setup_service_->PrepareForFirstSyncSetup();
-  authenticated_identity_ = identity;
+  primary_identity_ = identity;
   consent_level_ = signin::ConsentLevel::kSignin;
 }
 
@@ -65,7 +65,7 @@ void AuthenticationServiceFake::SignOut(
 }
 
 void AuthenticationServiceFake::SignOutInternal(ProceduralBlock completion) {
-  authenticated_identity_ = nil;
+  primary_identity_ = nil;
   consent_level_ = signin::ConsentLevel::kSignin;
   if (completion)
     completion();
@@ -75,12 +75,11 @@ ChromeIdentity* AuthenticationServiceFake::GetPrimaryIdentity(
     signin::ConsentLevel consent_level) const {
   switch (consent_level) {
     case signin::ConsentLevel::kSignin:
-      return authenticated_identity_;
+      return primary_identity_;
       break;
     case signin::ConsentLevel::kSync:
-      return (consent_level_ == signin::ConsentLevel::kSync)
-                 ? authenticated_identity_
-                 : nil;
+      return (consent_level_ == signin::ConsentLevel::kSync) ? primary_identity_
+                                                             : nil;
       break;
   }
   return nil;
@@ -91,8 +90,8 @@ bool AuthenticationServiceFake::HasPrimaryIdentityManaged(
   if (!GetPrimaryIdentity(consent_level)) {
     return false;
   }
-  return [authenticated_identity_.userEmail
-      hasSuffix:ios::kManagedIdentityEmailSuffix];
+  return
+      [primary_identity_.userEmail hasSuffix:ios::kManagedIdentityEmailSuffix];
 }
 
 std::unique_ptr<KeyedService>
