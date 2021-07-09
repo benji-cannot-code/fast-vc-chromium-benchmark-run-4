cbe6bb0002795bc68b36094cb98ec175366dbc30
Bruce Dawson <brucedawson@chromium.org>
2021-07-09 22:52:46 +0000

    Changes to chrome_cleaner to allow removing windows.h
    Before we can remove includes of windows.h from header files we need to
    _add_ includes of windows.h to .cc files that require it. This change
    does this for source files in chrome_cleaner.
    
    Bug: 796644
    Change-Id: Idbdea79b22867c72581ed66284b4b6de034a4f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017108
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900189}

--- chrome/chrome_cleaner/test/child_process_logger.cc
+++ chrome/chrome_cleaner/test/child_process_logger.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 
+#include <windows.h>
+
 namespace chrome_cleaner {
 
 ChildProcessLogger::ChildProcessLogger() = default;
