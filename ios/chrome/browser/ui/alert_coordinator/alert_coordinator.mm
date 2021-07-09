59f9cfe622805bd3cb03536ed5a76d964a83d80d
jlebel <jlebel@chromium.org>
2021-07-09 10:00:27 +0000

    [iOS] Cleanup AlertCoordinator before calling callback
    This patch marks AlertCoordinator as being dismissed before calling
    action callbacks.
    This change makes as noop -[AlertCoordinator stop] from the action
    callbacks.
    Therefore the stop method can be call without questions from
    the AlertCoordinator owner point of view.
    
    Needed for crrev.com/c/2966502
    
    Bug: 1220779
    Change-Id: I9747ac99f45c5c50f24312543cfb5ba013533cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015316
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899938}

--- ios/chrome/browser/ui/alert_coordinator/alert_coordinator.mm
+++ ios/chrome/browser/ui/alert_coordinator/alert_coordinator.mm
@@ -84,10 +84,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [UIAlertAction actionWithTitle:title
                                style:style
                              handler:^(UIAlertAction*) {
-                               [weakSelf setNoInteractionAction:nil];
+                               [weakSelf alertDismissed];
                                if (actionBlock)
                                  actionBlock();
-                               [weakSelf alertDismissed];
                              }];
 
   alertAction.enabled = enabled;
