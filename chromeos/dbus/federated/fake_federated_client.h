07d574526d7ac56a9f58c15357f8ae6e405b89e6
alanlxl <alanlxl@chromium.org>
2021-07-09 00:24:57 +0000

    Replace DISALLOW_COPY_AND_ASSIGN with =delete
    Bug: 1125906
    Test: build pass
    Change-Id: Ib7e9b58ff2c4edd740b8e600132dbe2f89cf68f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011317
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899781}

--- chromeos/dbus/federated/fake_federated_client.h
+++ chromeos/dbus/federated/fake_federated_client.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/files/scoped_file.h"
-#include "base/macros.h"
 #include "chromeos/dbus/federated/federated_client.h"
 
 namespace chromeos {
@@ -18,14 +17,13 @@ class FakeFederatedClient : public FederatedClient {
  public:
   FakeFederatedClient();
   ~FakeFederatedClient() override;
+  FakeFederatedClient(const FakeFederatedClient&) = delete;
+  FakeFederatedClient& operator=(const FakeFederatedClient&) = delete;
 
   // FederatedClient:
   void BootstrapMojoConnection(
       base::ScopedFD fd,
       base::OnceCallback<void(bool success)> result_callback) override;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(FakeFederatedClient);
 };
 
 }  // namespace chromeos
--- chromeos/dbus/federated/federated_client.cc
+++ chromeos/dbus/federated/federated_client.cc
@@ -26,6 +26,8 @@ class FederatedClientImpl : public FederatedClient {
  public:
   FederatedClientImpl() = default;
   ~FederatedClientImpl() override = default;
+  FederatedClientImpl(const FederatedClientImpl&) = delete;
+  FederatedClientImpl& operator=(const FederatedClientImpl&) = delete;
 
   // FederatedClient:
   void BootstrapMojoConnection(
@@ -49,8 +51,6 @@ class FederatedClientImpl : public FederatedClient {
   }
 
  private:
-  dbus::ObjectProxy* federated_service_proxy_ = nullptr;
-
   // Passes the success/failure of `dbus_response` on to `result_callback`.
   void OnBootstrapMojoConnectionResponse(
       base::OnceCallback<void(bool success)> result_callback,
@@ -59,10 +59,9 @@ class FederatedClientImpl : public FederatedClient {
     std::move(result_callback).Run(success);
   }
 
+  dbus::ObjectProxy* federated_service_proxy_ = nullptr;
   // Must be last class member.
   base::WeakPtrFactory<FederatedClientImpl> weak_ptr_factory_{this};
-
-  DISALLOW_COPY_AND_ASSIGN(FederatedClientImpl);
 };
 
 }  // namespace
