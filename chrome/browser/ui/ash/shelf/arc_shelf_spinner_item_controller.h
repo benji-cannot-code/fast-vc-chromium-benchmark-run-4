6d9490f5c4f907eb8f2161266b34ae5ec445eb79
Nancy Wang <nancylingwang@chromium.org>
2021-07-09 01:06:58 +0000

    Clean ArcShelfSpinnerItemController.
    The ghost window doesn't use ArcShelfSpinnerItemController to show the
    icon on Shelf, but use AppServiceAppWindowShelfItemController for the
    icon item on Shelf. So the full restore code in
    ArcShelfSpinnerItemController can be removed.
    
    BUG=1146900
    
    Change-Id: Ia72c8be88d73c576e0eb620097b912d5d668dbce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015356
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899792}

--- chrome/browser/ui/ash/shelf/arc_shelf_spinner_item_controller.h
+++ chrome/browser/ui/ash/shelf/arc_shelf_spinner_item_controller.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/ash/arc/session/arc_session_manager_observer.h"
-#include "chrome/browser/chromeos/full_restore/arc_window_handler.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ui/ash/shelf/shelf_spinner_item_controller.h"
@@ -23,11 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // ArcShelfSpinnerItemController displays the icon of the ARC app that
 // cannot be launched immediately (due to ARC not being ready) on Chrome OS'
 // shelf, with an overlaid spinner to provide visual feedback.
-class ArcShelfSpinnerItemController
-    : public ShelfSpinnerItemController,
-      public ArcAppListPrefs::Observer,
-      public arc::ArcSessionManagerObserver,
-      public chromeos::full_restore::ArcWindowHandler::Observer {
+class ArcShelfSpinnerItemController : public ShelfSpinnerItemController,
+                                      public ArcAppListPrefs::Observer,
+                                      public arc::ArcSessionManagerObserver {
  public:
   ArcShelfSpinnerItemController(const std::string& arc_app_id,
                                 int event_flags,
@@ -47,9 +44,6 @@ class ArcShelfSpinnerItemController
   // arc::ArcSessionManagerObserver:
   void OnArcPlayStoreEnabledChanged(bool enabled) override;
 
-  // chromeos::full_restore::ArcWindowHandler::Observer:
-  void OnWindowCloseRequested(int window_id) override;
-
  private:
   // The flags of the event that caused the ARC app to be activated. These will
   // be propagated to the launch event once the app is actually launched.
@@ -62,10 +56,6 @@ class ArcShelfSpinnerItemController
 
   // Unowned
   Profile* observed_profile_ = nullptr;
-
-  base::ScopedObservation<chromeos::full_restore::ArcWindowHandler,
-                          chromeos::full_restore::ArcWindowHandler::Observer>
-      arc_handler_observation_{this};
 
   DISALLOW_COPY_AND_ASSIGN(ArcShelfSpinnerItemController);
 };
