78f3007eac9ff9f7ea3117eb0e9d7bbeb8065d0e
Kent Tamura <tkent@chromium.org>
2021-07-09 06:50:14 +0000

    Move the blink.mojom.HighPriorityLocalFrame implementation
    ... from blink::LocalFrame to blink::LocalFrameMojoReceiver in order to
    reduce the size of local_frame.h.
    
    This CL has no behavior changes.
    
    Bug: 1227229, 1227131
    Change-Id: Icd4bd403b679625cda0cb9cb627bdcbb0285ed6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016657
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899885}

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -143,13 +143,11 @@ class WebURLLoaderFactory;
 extern template class CORE_EXTERN_TEMPLATE_EXPORT Supplement<LocalFrame>;
 
 // A LocalFrame is a frame hosted inside this process.
-class CORE_EXPORT LocalFrame final
-    : public Frame,
-      public FrameScheduler::Delegate,
-      public Supplementable<LocalFrame>,
-      public mojom::blink::LocalFrame,
-      public mojom::blink::LocalMainFrame,
-      public mojom::blink::HighPriorityLocalFrame {
+class CORE_EXPORT LocalFrame final : public Frame,
+                                     public FrameScheduler::Delegate,
+                                     public Supplementable<LocalFrame>,
+                                     public mojom::blink::LocalFrame,
+                                     public mojom::blink::LocalMainFrame {
  public:
   // Returns the LocalFrame instance for the given |frame_token|.
   static LocalFrame* FromFrameToken(const LocalFrameToken& frame_token);
@@ -657,8 +655,6 @@ class CORE_EXPORT LocalFrame final
       mojom::blink::ResourceTimingInfoPtr timing,
       const String& server_timing_values) final;
   void BeforeUnload(bool is_reload, BeforeUnloadCallback callback) final;
-  void DispatchBeforeUnload(bool is_reload,
-                            BeforeUnloadCallback callback) final;
   void MediaPlayerActionAt(
       const gfx::Point& window_point,
       blink::mojom::blink::MediaPlayerActionPtr action) final;
@@ -941,8 +937,6 @@ class CORE_EXPORT LocalFrame final
   static void BindToMainFrameReceiver(
       blink::LocalFrame* frame,
       mojo::PendingAssociatedReceiver<mojom::blink::LocalMainFrame> receiver);
-  void BindToHighPriorityReceiver(
-      mojo::PendingReceiver<mojom::blink::HighPriorityLocalFrame> receiver);
   void BindTextFragmentReceiver(
       mojo::PendingReceiver<mojom::blink::TextFragmentReceiver> receiver);
 
@@ -1070,9 +1064,6 @@ class CORE_EXPORT LocalFrame final
   // LocalFrame can be reused by multiple ExecutionContext.
   HeapMojoAssociatedReceiver<mojom::blink::LocalMainFrame, LocalFrame>
       main_frame_receiver_{this, nullptr};
-  // LocalFrame can be reused by multiple ExecutionContext.
-  HeapMojoReceiver<mojom::blink::HighPriorityLocalFrame, LocalFrame>
-      high_priority_frame_receiver_{this, nullptr};
   // TODO(crbug.com/1227229): Move the above HeapMojoReceivers to
   // LocalFrameMojoReceiver.
   Member<LocalFrameMojoReceiver> mojo_receiver_;
