841429849589000df6aaeff19e8ac75c609a5488
Sebastien Marchand <sebmarchand@chromium.org>
2021-07-09 11:24:13 +0000

    [PGO] Add a missing include.
    This file apparently gets included indirectly on other platforms but
    it's causing a compile failure for the Android PGO instrumented build.
    
    ```
    FAILED: obj/chrome/browser/browser/browser_shutdown.o
    /b/s/w/ir/cache/goma/client/gomacc ../../third_party/llvm-build/Release+Asserts/bin/clang++ -MMD -MF...(too long)
    ../../chrome/browser/lifetime/browser_shutdown.cc:178:41: error: member access into incomplete type 'content::ChildProcessHost'
    browser_child_iter.GetHost()->DumpProfilingData(base::BindOnce(
    ^
    ../../content/public/browser/browser_child_process_host_iterator.h:20:7: note: forward declaration of 'content::ChildProcessHost'
    ```
    
    Bug: 1227575
    Change-Id: I6fa5f93af1422fb2e510bf28566fc799a1decd77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017580
    Auto-Submit: Sébastien Marchand <sebmarchand@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899956}

--- chrome/browser/lifetime/browser_shutdown.cc
+++ chrome/browser/lifetime/browser_shutdown.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/gpu_utils.h"
+#include "content/public/common/child_process_host.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/profiling_utils.h"
 #endif
