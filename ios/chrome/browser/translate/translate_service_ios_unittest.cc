f36ca3257c05d7a7b8ee57b4757795d19795af91
Sylvain Defresne <sdefresne@chromium.org>
2021-07-09 12:38:54 +0000

    [ios] Preparation for exporting a free function based provider API
    Add //ios/chrome/browser/providers:null_provider_api that exposes
    a no-op version of the API (based on ChromiumBrowserProvider) and
    also //ios/public/provider/chrome/browser:test_provider_api that
    exposes a test version of the API (uses TestChromeBrowserProvider).
    
    Both those targets also depends on MaterialDesignComponents since
    this is an indirect dependency that the internal provider exposes
    in a similar way. This allow not adding checks in the rest of the
    code on whether to link with MaterialDesignComponents.
    
    Change all the target to only link with a single version of the
    provider API. Since all of those API exposes the same functions,
    it will causes linker errors if more than one is linked at the
    same time.
    
    Remove the class TestChromeProviderInitializer and instead use the
    CreateChromeBrowserProvider() factory function in the Chrome test
    suite class (thus ensuring that tests can choose which provider is
    used).
    
    Bug: 1201182
    Change-Id: Id73cadfcb6b808bb1a6275c33f6d867c0f5f275f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3010380
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899975}

--- ios/chrome/browser/translate/translate_service_ios_unittest.cc
+++ ios/chrome/browser/translate/translate_service_ios_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/translate/translate_service_ios.h"
 
 #include "ios/chrome/browser/chrome_url_constants.h"
-#include "ios/public/provider/chrome/browser/test_chrome_provider_initializer.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 #include "url/gurl.h"
