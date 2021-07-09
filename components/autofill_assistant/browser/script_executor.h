8dffb26aca9be3dbe21df9552da415278beda9c0
sandromaggi <sandromaggi@google.com>
2021-07-09 11:25:13 +0000

    [Autofill Assistant] Restore scrolling after interrupt
    This adds a restore point for scrolling back to an element
    after an interrupt for:
    * ShowCastProto (ActionDelegate::ScrollToElementPosition)
    * ScrollIntoViewProto (ActionDelegate::ScrollIntoView)
    
    Any failure will be silently ignored.
    
    Bug: b/192809309
    Change-Id: Id919f94ea222066e92051dd9eb629c948820412c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015339
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#899957}

--- components/autofill_assistant/browser/script_executor.h
+++ components/autofill_assistant/browser/script_executor.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/service.pb.h"
 #include "components/autofill_assistant/browser/top_padding.h"
 #include "components/autofill_assistant/browser/wait_for_dom_observer.h"
+#include "components/autofill_assistant/browser/web/element.h"
 #include "components/autofill_assistant/browser/web/element_finder.h"
 #include "components/autofill_assistant/browser/web/element_store.h"
 
@@ -198,12 +199,7 @@ class ScriptExecutor : public ActionDelegate,
                               const autofill::FormData& form_data,
                               const autofill::FormFieldData& field_data)>
           callback) override;
-  void ScrollToElementPosition(
-      const Selector& selector,
-      const TopPadding& top_padding,
-      std::unique_ptr<ElementFinder::Result> container,
-      const ElementFinder::Result& element,
-      base::OnceCallback<void(const ClientStatus&)> callback) override;
+  void StoreScrolledToElement(const ElementFinder::Result& element) override;
   void SetTouchableElementArea(
       const ElementAreaProto& touchable_element_area) override;
   void ExpectNavigation() override;
@@ -492,8 +488,7 @@ class ScriptExecutor : public ActionDelegate,
   bool should_clean_contextual_ui_on_finish_ = false;
   ActionProto::ActionInfoCase previous_action_type_ =
       ActionProto::ACTION_INFO_NOT_SET;
-  Selector last_focused_element_selector_;
-  TopPadding last_focused_element_top_padding_;
+  absl::optional<DomObjectFrameStack> last_focused_element_;
   std::unique_ptr<ElementAreaProto> touchable_element_area_;
 
   // Steps towards the requirements for calling |on_expected_navigation_done_|
