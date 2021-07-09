78b4c45c851572db1f003b8f9c603b7a9cb60dd7
Ian Struiksma <ianstruiksma@google.com>
2021-07-09 11:58:17 +0000

    [Autofill][ReplayTests] Add api server exclusion to allow prod tests
    Host resolver must ignore the api url, and the actual decision to
    replay from recorded archive or live is handled by cache_replayer
    
    Bug: 1214041
    Change-Id: I8eaa752b3ee74fe3c43cdce7dd3e96286a7fbc5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011883
    Auto-Submit: Ian Struiksma <ianstruiksma@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#899965}

--- chrome/browser/autofill/captured_sites_test_utils.cc
+++ chrome/browser/autofill/captured_sites_test_utils.cc
@@ -557,6 +557,7 @@ void TestRecipeReplayer::SetUpCommandLine(base::CommandLine* command_line) {
           "MAP *:443 127.0.0.1:%d,"
           // Set to always exclude, allows cache_replayer overwrite
           "EXCLUDE clients1.google.com,"
+          "EXCLUDE content-autofill.googleapis.com,"
           "EXCLUDE localhost",
           kHostHttpPort, kHostHttpsPort));
   command_line->AppendSwitchASCII(
