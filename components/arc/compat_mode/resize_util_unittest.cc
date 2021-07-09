acba01435fbdaf40fdd9f658f7db06b5dd8a1662
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-07-09 08:34:36 +0000

    Limit window size on small display
    This CL changes ResizeLockTo(Phone|Tablet) not to resize the window
    beyond the display’s work area size.
    
    BUG=b:192994148
    TEST=ResizeUtilTest.TestResizeLockToPhoneTabletOnSmallDisplay
    
    Change-Id: I3ca01f702186fe32589968c006c9ea687545f985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3013170
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899919}

--- components/arc/compat_mode/resize_util_unittest.cc
+++ components/arc/compat_mode/resize_util_unittest.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/test/exo_test_base_views.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/window.h"
+#include "ui/display/test/scoped_screen_override.h"
+#include "ui/display/test/test_screen.h"
 #include "ui/views/test/widget_test.h"
 #include "ui/views/widget/widget.h"
 
@@ -96,6 +98,14 @@ class ResizeUtilTest : public exo::test::ExoTestBaseViews {
     widget_ = CreateTestWidget(views::Widget::InitParams::TYPE_WINDOW);
     widget_->GetNativeWindow()->SetProperty(ash::kAppIDKey,
                                             std::string(kTestAppId));
+    // FHD size by default.
+    SetDisplayWorkArea(gfx::Rect(0, 0, 1920, 1080));
+  }
+
+  void SetDisplayWorkArea(const gfx::Rect& work_area) {
+    display::Display display = test_screen_.GetPrimaryDisplay();
+    display.set_work_area(work_area);
+    test_screen_.display_list().UpdateDisplay(display);
   }
 
   TestArcResizeLockPrefDelegate* pref_delegate() { return &pref_delegate_; }
@@ -104,6 +114,8 @@ class ResizeUtilTest : public exo::test::ExoTestBaseViews {
  private:
   TestArcResizeLockPrefDelegate pref_delegate_;
   std::unique_ptr<views::Widget> widget_;
+  display::test::TestScreen test_screen_;
+  display::test::ScopedScreenOverride scoped_screen_override_{&test_screen_};
 };
 
 // Test that resize phone works properly in both needs-confirmation and no
@@ -136,6 +148,29 @@ TEST_F(ResizeUtilTest, TestResizeLockToTablet) {
             widget()->GetWindowBoundsInScreen().height());
   EXPECT_EQ(PredictCurrentMode(widget(), pref_delegate()),
             ResizeCompatMode::kTablet);
+}
+
+// Test that resize phone/tablet works properly on small displays.
+TEST_F(ResizeUtilTest, TestResizeLockToPhoneTabletOnSmallDisplay) {
+  pref_delegate()->SetResizeLockNeedsConfirmation(kTestAppId, false);
+
+  // Set small workarea size.
+  constexpr gfx::Size workarea_size(300, 300);
+  SetDisplayWorkArea(gfx::Rect(workarea_size));
+
+  ResizeLockToPhone(widget(), pref_delegate());
+  EXPECT_LT(widget()->GetWindowBoundsInScreen().width(),
+            widget()->GetWindowBoundsInScreen().height());
+  EXPECT_LT(widget()->GetWindowBoundsInScreen().width(), workarea_size.width());
+  EXPECT_LT(widget()->GetWindowBoundsInScreen().height(),
+            workarea_size.height());
+
+  ResizeLockToTablet(widget(), pref_delegate());
+  EXPECT_GT(widget()->GetWindowBoundsInScreen().width(),
+            widget()->GetWindowBoundsInScreen().height());
+  EXPECT_LT(widget()->GetWindowBoundsInScreen().width(), workarea_size.width());
+  EXPECT_LT(widget()->GetWindowBoundsInScreen().height(),
+            workarea_size.height());
 }
 
 // Test that enabling resizing works properly in both needs-confirmation and no
