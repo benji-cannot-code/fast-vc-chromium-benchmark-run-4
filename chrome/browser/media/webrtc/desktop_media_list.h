6edca6f6445368b00eaed1b0e6b93af448754f7d
Elad Alon <eladalon@chromium.org>
2021-07-09 14:08:22 +0000

    Remove dead-code: getCurrentBrowsingContextMedia confirmation-only flow
    The confirmation-only flow of getCurrentBrowsingContextMedia is dead
    code at the moment, and maintaining it increases the development time
    of other features/improvements, such as changing the behavior of the
    audio checkbox in the media-picker. This CL removes this dead code.
    This CL can be undone when getViewportMedia is standardized and
    a confirmation-only flow becomes possible.
    
    Bug: 1136942
    Change-Id: I3f2b90d96706dbb746f38745be9df5dc1c8f2d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017862
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900000}

--- chrome/browser/media/webrtc/desktop_media_list.h
+++ chrome/browser/media/webrtc/desktop_media_list.h
@@ -48,9 +48,6 @@ class DesktopMediaList {
 
   using UpdateCallback = base::OnceClosure;
 
-  // TODO(crbug.com/1136942): Add support for this flow.
-  static constexpr bool kConfirmationOnlyDialogSupported = false;
-
   virtual ~DesktopMediaList() {}
 
   // Sets time interval between updates. By default list of sources and their
--- chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/desktop_capture/desktop_media_source_view.h"
-#include "chrome/browser/ui/views/desktop_capture/get_current_browsing_context_media_dialog.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
@@ -676,10 +675,5 @@ void DesktopMediaPickerViews::NotifyDialogResult(DesktopMediaID source) {
 // static
 std::unique_ptr<DesktopMediaPicker> DesktopMediaPicker::Create(
     const content::MediaStreamRequest* request) {
-  if (request && DesktopMediaList::kConfirmationOnlyDialogSupported &&
-      request->video_type ==
-          blink::mojom::MediaStreamType::DISPLAY_VIDEO_CAPTURE_THIS_TAB) {
-    return std::make_unique<GetCurrentBrowsingContextMediaDialog>();
-  }
   return std::make_unique<DesktopMediaPickerViews>();
 }
