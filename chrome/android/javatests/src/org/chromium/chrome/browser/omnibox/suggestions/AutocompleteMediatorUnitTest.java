736b84fe52a0a96fedef48d8b1699981cb9aab59
Xi Han <hanxi@google.com>
2021-07-09 22:45:42 +0000

    [Start] Fix "explore sites" icon is grey in Omnibox.
    The large icon of the explore sites can be get via
    ExploreSitesBridge.getSummaryImage(). Due to the dependency, in this CL,
    we introduce an ExploreIconState interface, which exposes this API to
    MostVisitedSitesProcessor.
    
    Before:
    https://drive.google.com/file/d/1FbMxXvSzLyLW9PHf3D3KrT5NOKPb2hwO/view?usp=sharing
    After:
    https://drive.google.com/file/d/1ue4p3ARAyVD8g-RnW4y6sRXCC6ZF0NQa/view?usp=sharing
    
    Bug: 1222094
    Change-Id: Ie105ebf09b0a36757b3f5244991bc9081b171a15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2991295
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900183}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediatorUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediatorUnitTest.java
@@ -221,7 +221,8 @@ public class AutocompleteMediatorUnitTest {
         mMediator = new AutocompleteMediator(ContextUtils.getApplicationContext(),
                 mAutocompleteDelegate, mTextStateProvider, mListModel,
                 mHandler, () -> mModalDialogManager, null, null,
-                mLocationBarDataProvider, tab -> {}, null, url -> false, new DummyJankTracker());
+                mLocationBarDataProvider, tab -> {}, null, url -> false, new DummyJankTracker(),
+                (pixelSize, callback) -> {});
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             mMediator.setAutocompleteProfile(mProfile);
---
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/mostvisited/ExploreIconProvider.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.omnibox.suggestions.mostvisited;
+
+import android.graphics.Bitmap;
+
+import org.chromium.base.Callback;
+
+/** The provider of the explore sites icon. */
+public interface ExploreIconProvider {
+    /**
+     * @param pixelSize The icon width in pixel.
+     * @param callback Called when the icon is available.
+     */
+    void getSummaryImage(int pixelSize, Callback<Bitmap> callback);
+}
