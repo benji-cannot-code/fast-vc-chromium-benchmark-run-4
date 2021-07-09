ce15b1647b9b1a7db924ab1e143e9e40855fea1c
Lei Shi <leileilei@google.com>
2021-07-09 07:21:18 +0000

    Add data fetching logic in Enhanced Network TTS
    GetAudioData fetches audio data from a Google server (i.e., ReadAloud)
    and sends the data back to the calling clients.
    
    Bug: 1217301
    Change-Id: I1eaca56744f010b5f2cfa17af35084502d46a7e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2999423
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Lei Shi <leileilei@google.com>
    Cr-Commit-Position: refs/heads/master@{#899892}

---
+++ chrome/browser/ash/enhanced_network_tts/enhanced_network_tts_constants.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/enhanced_network_tts/enhanced_network_tts_constants.h"
+
+namespace ash {
+namespace enhanced_network_tts {
+
+const char kGoogApiKeyHeader[] = "X-Goog-Api-Key";
+
+const char kReadAloudServerUrl[] =
+    "https://readaloud.googleapis.com//v1:generateAudioDocStream";
+
+const char kNetworkRequestUploadType[] = "application/json";
+
+}  // namespace enhanced_network_tts
+}  // namespace ash
---
+++ chrome/browser/ash/enhanced_network_tts/enhanced_network_tts_utils.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_ENHANCED_NETWORK_TTS_ENHANCED_NETWORK_TTS_UTILS_H_
+#define CHROME_BROWSER_ASH_ENHANCED_NETWORK_TTS_ENHANCED_NETWORK_TTS_UTILS_H_
+
+#include <string>
+#include <vector>
+
+#include "base/values.h"
+
+namespace ash {
+namespace enhanced_network_tts {
+
+// Format the |input_text| to a JSON string that will be accepted by the
+// ReadAloud server.
+std::string FormatJsonRequest(const std::string& input_text);
+
+// Generate a result when encountering errors (e.g., server error, parsing
+// error). We return an empty vector for now.
+// TODO(crbug.com/1217301): Provide more specific error information.
+std::vector<uint8_t> GetResultOnError();
+
+// Unpack the JSON audio data from the server response to a vector.
+std::vector<uint8_t> UnpackJsonResponse(const base::Value& json_data);
+
+}  // namespace enhanced_network_tts
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_ENHANCED_NETWORK_TTS_ENHANCED_NETWORK_TTS_UTILS_H_
