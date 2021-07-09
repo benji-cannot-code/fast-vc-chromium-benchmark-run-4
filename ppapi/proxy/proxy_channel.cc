3fa66e2f0932a0f3edf963fa5b4df5c7bad676fb
cfredric <cfredric@chromium.org>
2021-07-09 21:02:18 +0000

    Remove dependency on testonly target from ppapi/proxy/proxy_channel.
    This CL simply changes the type used for `test_sink_`, since there's no
    reason ProxyChannel has to be aware it's anything more specific than a
    IPC::Sender.
    
    Bug: 1158995
    Change-Id: Ibd8c9c203c89e8c59b7fa865c594df740ee65c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017716
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900143}

--- ppapi/proxy/proxy_channel.cc
+++ ppapi/proxy/proxy_channel.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "build/build_config.h"
 #include "ipc/ipc_platform_file.h"
-#include "ipc/ipc_test_sink.h"
+#include "ipc/ipc_sender.h"
 
 #if defined(OS_NACL)
 #include <unistd.h>
@@ -45,9 +45,9 @@ bool ProxyChannel::InitWithChannel(
   return true;
 }
 
-void ProxyChannel::InitWithTestSink(IPC::TestSink* test_sink) {
+void ProxyChannel::InitWithTestSink(IPC::Sender* sender) {
   DCHECK(!test_sink_);
-  test_sink_ = test_sink;
+  test_sink_ = sender;
 #if !defined(OS_NACL)
   peer_pid_ = base::GetCurrentProcId();
 #endif
