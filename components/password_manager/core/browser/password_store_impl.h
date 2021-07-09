9d1c327ef797ac3bdb5eec3f9a2273fbc0365fe3
Viktor Semeniuk <vsemeniuk@google.com>
2021-07-09 16:24:56 +0000

    Moving PasswordStoreSync methods to the PasswordStoreImpl
    This CL moves some of the PasswordStoreSync methods to the
    PasswordStoreImpl. This CL also prevents updates of affiliated passwords
    through sync.
    
    Bug: 1226042
    Change-Id: I2bbca9d259b7f97927fcdc92a1f9ff5139d7b807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001129
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900038}

--- components/password_manager/core/browser/password_store_impl.h
+++ components/password_manager/core/browser/password_store_impl.h
@@ -87,6 +87,16 @@ class PasswordStoreImpl : public PasswordStore, public PasswordStoreBackend {
   bool IsEmpty() override;
 
   // Implements PasswordStoreSync interface.
+  PasswordStoreChangeList AddLoginSync(const PasswordForm& form,
+                                       AddLoginError* error) override;
+  bool AddInsecureCredentialsSync(
+      base::span<const InsecureCredential> credentials) override;
+  PasswordStoreChangeList UpdateLoginSync(const PasswordForm& form,
+                                          UpdateLoginError* error) override;
+  bool UpdateInsecureCredentialsSync(
+      const PasswordForm& form,
+      base::span<const InsecureCredential> credentials) override;
+  PasswordStoreChangeList RemoveLoginSync(const PasswordForm& form) override;
   bool BeginTransaction() override;
   void RollbackTransaction() override;
   bool CommitTransaction() override;
@@ -101,6 +111,11 @@ class PasswordStoreImpl : public PasswordStore, public PasswordStoreBackend {
   bool DeleteAndRecreateDatabaseFile() override;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(PasswordStoreTest,
+                           UpdatePasswordsStoredForAffiliatedWebsites);
+  FRIEND_TEST_ALL_PREFIXES(PasswordStoreTest, AddInsecureCredentialsSync);
+  FRIEND_TEST_ALL_PREFIXES(PasswordStoreTest, UpdateInsecureCredentialsSync);
+
   // Implements PasswordStoreBackend interface.
 
   void GetAllLoginsAsync(LoginsReply callback) override;
