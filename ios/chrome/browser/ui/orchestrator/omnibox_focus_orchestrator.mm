7f93af878c7555d8ae9c51ba181ad139db1ab107
Robbie Gibson <rkgibson@google.com>
2021-07-09 15:31:51 +0000

    [iOS] Fix testVoiceSearchUIIsShown on iOS 15
    The root cause of the bug was that sometimes, the toolbar animations were
    finishing before the omnibox animations were queued up (from inside a
    dispatch_async(dispatch_get_main_queue()). This threw the animation
    count off.
    
    Now, we ensure that the omnibox animations are queued (and finished)
    before allowing the final -animationFinished to proceed.
    
    Bug: 1224915
    Change-Id: Ie757947eb482f8730a02883a56398ca2e42e030f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018088
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900023}

--- ios/chrome/browser/ui/orchestrator/omnibox_focus_orchestrator.mm
+++ ios/chrome/browser/ui/orchestrator/omnibox_focus_orchestrator.mm
@@ -21,7 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, assign) BOOL stateChangedDuringAnimation;
 @property(nonatomic, assign) BOOL finalOmniboxFocusedState;
 @property(nonatomic, assign) BOOL finalToolbarExpandedState;
-@property(nonatomic, assign) unsigned int inProgressAnimationCount;
+@property(nonatomic, assign) int inProgressAnimationCount;
+
+// Sometimes, the toolbar animations finish before the omnibox animations are
+// even queued, causing the final completions to be run too early.
+@property(nonatomic, assign) BOOL areOmniboxChangesQueued;
 
 @end
 
@@ -44,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   self.isAnimating = animated;
+  self.areOmniboxChangesQueued = NO;
   self.inProgressAnimationCount = 0;
 
   if (toolbarExpanded) {
@@ -63,10 +68,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // with a constraint animation seems to be to let the constraint animation
   // start and compute the final frames, then perform the transform animation.
   dispatch_async(dispatch_get_main_queue(), ^{
+    self.areOmniboxChangesQueued = YES;
     if (omniboxFocused) {
       [self focusOmniboxAnimated:animated];
     } else {
       [self defocusOmniboxAnimated:animated];
+    }
+
+    // Make sure that some omnibox animations were queued. Otherwise, the final
+    // call to |animationFinished after the toolbar animations finished was
+    // interrupted and cleanup still needs to occur.
+    if (self.inProgressAnimationCount == 0 && self.isAnimating) {
+      [self animationFinished];
     }
   });
 }
@@ -140,6 +153,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         completion:^(BOOL _) {
           [self animationFinished];
         }];
+
     self.inProgressAnimationCount += 1;
     [UIView animateWithDuration:duration * 0.2
         delay:duration * 0.8
@@ -321,19 +335,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)animationFinished {
   self.inProgressAnimationCount -= 1;
-  if (self.inProgressAnimationCount > 0) {
+  // Make sure all the animations have been queued and finished.
+  if (!self.areOmniboxChangesQueued || self.inProgressAnimationCount > 0) {
     return;
   }
 
   // inProgressAnimation count should never be negative because it should
   // always be incremented before starting an animation and decremented
   // when the animation finishes.
-  // TODO(crbug.com/1224915): Relaxing DCHECK for iOS15 as this negative. See
-  // crbug for steps to reproduce.
-  if (@available(iOS 15, *)) {
-  } else {
-    DCHECK(self.inProgressAnimationCount == 0);
-  }
+  DCHECK(self.inProgressAnimationCount == 0);
 
   self.isAnimating = NO;
   if (self.stateChangedDuringAnimation) {
