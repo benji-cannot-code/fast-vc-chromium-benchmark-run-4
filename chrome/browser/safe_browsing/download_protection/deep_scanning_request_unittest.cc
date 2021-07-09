dd78f503ed0f9d503fe565d17fe4cbf947a4305e
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-07-09 14:24:17 +0000

    Add content_type to ContentMetaData and ContentAnalysisRequest
    Bug: 1227373
    Change-Id: If1d6a3d01a4b01a3d82779573d9ec2eddc5261d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015447
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900005}

--- chrome/browser/safe_browsing/download_protection/deep_scanning_request_unittest.cc
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_request_unittest.cc
@@ -343,6 +343,11 @@ TEST_F(DeepScanningRequestTest, GeneratesCorrectRequestFromPolicy) {
                   .request_data()
                   .url(),
               download_url_.spec());
+    EXPECT_EQ(download_protection_service_.GetFakeBinaryUploadService()
+                  ->last_request()
+                  .request_data()
+                  .content_type(),
+              "application/octet-stream");
   }
 
   {
@@ -1003,6 +1008,11 @@ TEST_F(DeepScanningRequestTest, PopulatesRequest) {
                 .digest(),
             // Hex-encoding of 'hash'
             "76E00EB33811F5778A5EE557512C30D9341D4FEB07646BCE3E4DB13F9428573C");
+  EXPECT_EQ(download_protection_service_.GetFakeBinaryUploadService()
+                ->last_request()
+                .request_data()
+                .content_type(),
+            "application/octet-stream");
 }
 
 }  // namespace safe_browsing
