c82015e45b484e4c6f16f5c30a931ce33ed57cab
Charlie Reis <creis@chromium.org>
2021-07-09 07:31:38 +0000

    Reland "Avoid getting a WebUI via WebContents in PopulateBinderMapWithContext."
    This reverts commit c18001b35f000dd0435506fcc65da9cc44355ca9.
    
    Reason for reland: Fixed test flakiness.
    
    Original change's description:
    > Revert "Avoid getting a WebUI via WebContents in PopulateBinderMapWithContext."
    >
    > This reverts commit 4251e06c82eba2bf31e3edc61c283d6c0a3ad773.
    >
    > Reason for revert: Suspecting of causing
    > SecurityExploitBrowserTest.BindToWebUIFromWebViaMojo to fail starting
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.11%20Tests/63946/overview
    >
    > Original change's description:
    > > Avoid getting a WebUI via WebContents in PopulateBinderMapWithContext.
    > >
    > > The RenderFrameHost itself should be used for getting the WebUI and
    > > URL, rather than going through WebContents (which might consult a
    > > different RenderFrameHost than the one passed).
    > >
    > > Bug: 1225929
    > > Change-Id: Ic4e06be002b24565e57777f3b9f44d2058f975f5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002443
    > > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Commit-Queue: Charlie Reis <creis@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#898347}
    >
    > Bug: 1225929
    > Change-Id: Iea257fa4e36ef83ed50fbb8a6a8981979a0bdc81
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004123
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: calamity <calamity@chromium.org>
    > Owners-Override: calamity <calamity@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#898520}
    
    Bug: 1225929
    Change-Id: I83e8a44d17c3b3e2935d3f94ab1d7e0b86782d53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017427
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899899}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -2371,6 +2371,12 @@ class CONTENT_EXPORT RenderFrameHostImpl
   FRIEND_TEST_ALL_PREFIXES(
       RenderFrameHostManagerUnloadBrowserTest,
       PendingDeleteRFHProcessShutdownDoesNotRemoveSubframes);
+  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
+                           AttemptDuplicateRenderViewHost);
+  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
+                           AttemptDuplicateRenderWidgetHost);
+  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
+                           BindToWebUIFromWebViaMojo);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
                            RenderViewHostIsNotReusedAfterDelayedUnloadACK);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
@@ -2387,8 +2393,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
                            RenderFrameProxyNotRecreatedDuringProcessShutdown);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
                            UnloadACKArrivesPriorToProcessShutdownRequest);
-  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
-                           AttemptDuplicateRenderViewHost);
   FRIEND_TEST_ALL_PREFIXES(WebContentsImplBrowserTest,
                            FullscreenAfterFrameUnload);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest, UnloadHandlerSubframes);
@@ -2415,8 +2419,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessSSLBrowserTest,
                            UnloadHandlersArePowerfulGrandChild);
   FRIEND_TEST_ALL_PREFIXES(RenderFrameHostImplTest, ExpectedMainWorldOrigin);
-  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
-                           AttemptDuplicateRenderWidgetHost);
   FRIEND_TEST_ALL_PREFIXES(RenderDocumentHostUserDataTest,
                            CheckInPendingDeletionState);
   FRIEND_TEST_ALL_PREFIXES(WebContentsImplBrowserTest, FrozenAndUnfrozenIPC);
