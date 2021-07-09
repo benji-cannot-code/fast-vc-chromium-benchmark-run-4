61f45e34a5dc83a1e494344cb860c486849ff6f0
John Lee <johntlee@chromium.org>
2021-07-09 22:49:52 +0000

    Side panel: Add keyboard navigation for up/down arrows
    This CL adds support for keyboard users to use the up and down arrow
    keys to navigate focus along the Bookmarks tree.
    
    BookmarkFolderElement manages focus between its folder row and its
    children by exposing a method |moveFocus| that takes in a parameter of
    either -1 or 1 to either move the focus up or down, respectively. The
    method returns a boolean if focus was successfully moved.
    
    BookmarkListElement uses this method to move focus along the root
    BookmarkFolderElements it houses. Both BookmarkListElement and
    BookmarkFolderElements loop the focus back and forth when the end is
    reached.
    
    Other keys, such as Home/End, are not yet supported in the Reading List
    tab and therefore are not yet implemented here.
    
    Bug: 1199527
    Change-Id: I89ba9c9d8e8222dd1255ebb055a8407570fe54b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016388
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900187}

--- chrome/browser/resources/read_later/side_panel/bookmarks_list.ts
+++ chrome/browser/resources/read_later/side_panel/bookmarks_list.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {ListPropertyUpdateBehavior} from 'chrome://resources/js/list_property_update_behavior.m.js';
 import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {FOLDER_OPEN_CHANGED_EVENT} from './bookmark_folder.js';
+import {BookmarkFolderElement, FOLDER_OPEN_CHANGED_EVENT} from './bookmark_folder.js';
 import {BookmarksApiProxy} from './bookmarks_api_proxy.js';
 
 // Key for localStorage object that refers to all the open folders.
@@ -50,6 +50,7 @@ export class BookmarksListElement extends BookmarksListElementBase {
         FOLDER_OPEN_CHANGED_EVENT,
         e => this.onFolderOpenChanged_(
             e as CustomEvent<{id: string, open: boolean}>));
+    this.addEventListener('keydown', e => this.onKeydown_(e));
   }
 
   connectedCallback() {
@@ -182,6 +183,31 @@ export class BookmarksListElement extends BookmarksListElementBase {
     }
     window.localStorage[LOCAL_STORAGE_OPEN_FOLDERS_KEY] =
         JSON.stringify(this.openFolders_);
+  }
+
+  private onKeydown_(event: KeyboardEvent) {
+    if (!['ArrowDown', 'ArrowUp'].includes(event.key)) {
+      return;
+    }
+
+    if (!(this.shadowRoot!.activeElement instanceof BookmarkFolderElement)) {
+      // If the key event did not happen within a BookmarkFolderElement, do
+      // not do anything.
+      return;
+    }
+
+    const allFolderElements: BookmarkFolderElement[] =
+        Array.from(this.shadowRoot!.querySelectorAll('bookmark-folder'));
+
+    const delta = event.key === 'ArrowUp' ? -1 : 1;
+    let currentIndex =
+        allFolderElements.indexOf(this.shadowRoot!.activeElement);
+    let focusHasMoved = false;
+    while (!focusHasMoved) {
+      focusHasMoved = allFolderElements[currentIndex]!.moveFocus(delta);
+      currentIndex = (currentIndex + delta + allFolderElements.length) %
+          allFolderElements.length;
+    }
   }
 
   private onMoved_(movedInfo: chrome.bookmarks.MoveInfo) {
