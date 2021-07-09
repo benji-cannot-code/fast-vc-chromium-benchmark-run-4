37ed162be28a7ba94d9b8748364275f1ca84bc32
Mugdha Lakhani <nator@chromium.org>
2021-07-09 09:39:02 +0000

    [PageInfo] PageInfo UI handles WebContents being destroyed.
    A lot of PageInfo and PageInfoUI overrides keep a pointer to
    WebContents, which can lead to use after free if WebContents is
    destroyed.
    
    The UI should be closed when this happens.
    
    Bug: 1218468
    Change-Id: I9623aab6d1f304fbb629fd8e2880542925ca2aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2972872
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#899930}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_base.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_base.cc
@@ -93,5 +93,9 @@ void PageInfoBubbleViewBase::DidChangeVisibleSecurityState() {
   GetWidget()->Close();
 }
 
+void PageInfoBubbleViewBase::WebContentsDestroyed() {
+  GetWidget()->Close();
+}
+
 BEGIN_METADATA(PageInfoBubbleViewBase, views::BubbleDialogDelegateView)
 END_METADATA
