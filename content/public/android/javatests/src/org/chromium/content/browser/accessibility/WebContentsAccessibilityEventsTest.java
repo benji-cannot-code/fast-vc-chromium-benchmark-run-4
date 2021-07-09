d7e79de50ab47fae0c836713e3debaf809a9317c
Mark Schillaci <mschillaci@google.com>
2021-07-09 21:15:55 +0000

    Added Android accessibility events tests for web contents
    This CL adds a few WebContentsAccessibilityEventsTest's to the suite
    of Android accessibility tests.
    
    These tests are included on other platforms with expectation files in
    the same directory, but we leverage the full accessibility suite for
    the Android tests and this requires separately adding the tests in
    this file to run. This CL adds the tests that have been added to the
    suite over the last quarter but were not added on Android.
    
    AX-Relnotes: N/A
    Bug: N/A
    Change-Id: I387cdd47e6ef774fec4a903322eea4ad3fdee4be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011942
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/master@{#900148}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityEventsTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityEventsTest.java
@@ -209,6 +209,13 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    public void test_addAlertWithRoleChange() {
+        performTest("add-alert-with-role-change.html",
+                "add-alert-with-role-change-expected-android.txt");
+    }
+
+    @Test
+    @SmallTest
     public void test_addChild() {
         performTest("add-child.html", EMPTY_EXPECTATIONS_FILE);
     }
@@ -262,6 +269,12 @@ public class WebContentsAccessibilityEventsTest {
     @SmallTest
     public void test_ariaAtomicChanged() {
         performTest("aria-atomic-changed.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
+    public void test_ariaAtomicChanged2() {
+        performTest("aria-atomic-changed2.html", EMPTY_EXPECTATIONS_FILE);
     }
 
     @Test
@@ -467,6 +480,12 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    public void test_ariaRelevantChanged2() {
+        performTest("aria-relevant-changed2.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
     public void test_ariaRequiredChanged() {
         performTest("aria-required-changed.html", EMPTY_EXPECTATIONS_FILE);
     }
@@ -526,6 +545,24 @@ public class WebContentsAccessibilityEventsTest {
     @SmallTest
     public void test_ariaSpinbuttonValuetextChanged() {
         performTest("aria-spinbutton-valuetext-change.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
+    public void test_ariaTextboxChildrenChange() {
+        performTest("aria-textbox-children-change.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
+    public void test_ariaTextboxEditabilityChanges() {
+        performTest("aria-textbox-editability-changes.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
+    public void test_ariaTextboxWithFocusableChildren() {
+        performTest("aria-textbox-with-focusable-children.html", EMPTY_EXPECTATIONS_FILE);
     }
 
     @Test
@@ -799,8 +836,20 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    public void test_liveRegionOff() {
+        performTest("live-region-off.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
     public void test_liveRegionRemove() {
         performTest("live-region-remove.html", EMPTY_EXPECTATIONS_FILE);
+    }
+
+    @Test
+    @SmallTest
+    public void test_menuBarShowHideMenus() {
+        performTest("menubar-show-hide-menus.html", EMPTY_EXPECTATIONS_FILE);
     }
 
     @Test
@@ -930,6 +979,13 @@ public class WebContentsAccessibilityEventsTest {
     public void test_reportValidityInvalidField() {
         performTest("report-validity-invalid-field.html",
                 "report-validity-invalid-field-expected-android.txt");
+    }
+
+    @Test
+    @SmallTest
+    public void test_samePageLinkNavigation() {
+        performTest(
+                "same-page-link-navigation.html", "same-page-link-navigation-expected-android.txt");
     }
 
     @Test
