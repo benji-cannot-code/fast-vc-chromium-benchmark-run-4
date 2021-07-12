2610e552386fbb29cdc7d8af5fe92b9fe07a996a
Moe Ahmadi <mahmadi@chromium.org>
2021-07-12 18:41:00 +0000

    [memories] Adds an option to remove the top visit only
    - When a top visit is removed, whether alone or along with the rest of
      the visits in the cluster, the cluster should either be entirely
      removed or restructured. This CL adds logic to refresh the list of
      clusters in that scenario so those changes are reflected in the UI.
    - Also fixes a bug when deleting related visits was not being reflected
      in the UI.
    
    screenshot/8bR4yCRaAU5PncX
    
    Bug: 1173908
    Change-Id: Ia4e08585ab158b12ae5bcd2bf73536bda7ba65a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998321
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900567}

--- chrome/browser/resources/memories/top_visit.js
+++ chrome/browser/resources/memories/top_visit.js
@@ -61,7 +61,7 @@ class TopVisitElement extends PolymerElement {
        */
       hiddenRelatedVisits_: {
         type: Object,
-        computed: `computeHiddenRelatedVisits_(visit)`,
+        computed: `computeHiddenRelatedVisits_(visit.*)`,
       },
 
       /** @private {string} */
@@ -76,7 +76,7 @@ class TopVisitElement extends PolymerElement {
        */
       visibleRelatedVisits_: {
         type: Object,
-        computed: `computeVisibleRelatedVisits_(visit)`,
+        computed: `computeVisibleRelatedVisits_(visit.*)`,
       },
     };
   }
