b238d201a5cc2b566fe5a22b2854db96241ccbe2
paulzhchen <paulzchen@google.com>
2021-07-12 23:47:07 +0000

    Add feature flag: PhoneHubRecentApps
    This flag to Enables the Recent Apps feature in Phone Hub, which
    allows users to relaunch the streamed app.
    
    Bug: b/190466630
    Test: Pass AboutFlagsHistogramTest.
    Change-Id: Iedb3862584747e3eadc5cac326bf1929f77fbea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983086
    Auto-Submit: Paulz Chen <paulzchen@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Paulz Chen <paulzchen@google.com>
    Cr-Commit-Position: refs/heads/master@{#900716}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -298,6 +298,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kOverviewButton;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kPerDeskShelf;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kPhoneHub;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kPhoneHubCameraRoll;
+COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kPhoneHubRecentApps;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kPinSetupForManagedUsers;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kPipRoundedCorners;
@@ -492,6 +493,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPciguardUiEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPerDeskShelfEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPhoneHubCameraRollEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPhoneHubEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPhoneHubRecentAppsEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPinAutosubmitBackfillFeatureEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPinAutosubmitFeatureEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsPinSetupForManagedUsersEnabled();
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2852,6 +2852,9 @@ extern const char kNearbySharingWebRtcDescription[];
 extern const char kPhoneHubCameraRollName[];
 extern const char kPhoneHubCameraRollDescription[];
 
+extern const char kPhoneHubRecentAppsName[];
+extern const char kPhoneHubRecentAppsDescription[];
+
 extern const char kReduceDisplayNotificationsName[];
 extern const char kReduceDisplayNotificationsDescription[];
 
