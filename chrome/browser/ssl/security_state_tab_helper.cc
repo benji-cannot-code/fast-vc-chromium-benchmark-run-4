d1d8de28e42d148b73d749cb8a3808c6616509b9
Mustafa Emre Acer <meacer@google.com>
2021-07-12 22:13:55 +0000

    Record UKM for form submissions on HTTPS sites
    This CL records a new UKM when a form is submitted on an HTTPS page.
    This metric will be used to understand if the updated connection
    security indicator regresses form submissions on popular sites.
    
    UKM collection doc is at
    https://docs.google.com/document/d/1T9_eVN0qf-rr0dL7eknv6Mqh9Ksvux99r8z5Mt7Lf3I/edit
    
    Bug: 1204293
    Change-Id: I32be5188888642b2014e877345ae8f7d88087b73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2973730
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900661}

--- chrome/browser/ssl/security_state_tab_helper.cc
+++ chrome/browser/ssl/security_state_tab_helper.cc
@@ -46,6 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/x509_certificate.h"
 #include "net/ssl/ssl_cipher_suite_names.h"
 #include "net/ssl/ssl_connection_status_flags.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "third_party/boringssl/src/include/openssl/ssl.h"
 #include "url/gurl.h"
@@ -153,6 +155,18 @@ void SecurityStateTabHelper::DidStartNavigation(
           "Security.SecurityLevel.InsecureMainFrameFormSubmission",
           GetSecurityLevel(), security_state::SECURITY_LEVEL_COUNT);
     }
+
+    if (navigation_handle->GetURL().SchemeIs(url::kHttpsScheme)) {
+      ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
+      CHECK(ukm_recorder);
+      ukm::SourceId source_id =
+          ukm::ConvertToSourceId(navigation_handle->GetNavigationId(),
+                                 ukm::SourceIdType::NAVIGATION_ID);
+      ukm::builders::OmniboxSecurityIndicator_FormSubmission(source_id)
+          .SetSubmitted(true)
+          .Record(ukm_recorder);
+    }
+
   } else if (navigation_handle->IsInMainFrame() &&
              !security_state::IsSchemeCryptographic(
                  GetVisibleSecurityState()->url)) {
