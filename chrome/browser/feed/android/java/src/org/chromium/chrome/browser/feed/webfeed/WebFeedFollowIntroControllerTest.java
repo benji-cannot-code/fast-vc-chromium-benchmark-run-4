b4491b51a54d3f5bd2f9c5c69a7ed61b33292003
Carlos Knippschild <carlosk@chromium.org>
2021-07-12 19:37:40 +0000

    Add debug logging to WebFeedFollowIntroController and reenable tests
    Logs temporarily using ERROR level so that output will be visible in
    release bots.
    
    Bug: 1227610
    Change-Id: Ibdf7fdc5e7c611b72489c0ccba371c11eee67ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018739
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900593}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/webfeed/WebFeedFollowIntroControllerTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/webfeed/WebFeedFollowIntroControllerTest.java
@@ -42,7 +42,6 @@ import org.chromium.base.FeatureList;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.task.test.ShadowPostTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.feed.webfeed.WebFeedSnackbarController.FeedLauncher;
@@ -153,7 +152,6 @@ public final class WebFeedFollowIntroControllerTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1227610")
     public void meetsShowingRequirements_showsIntro() {
         setWebFeedIntroLastShownTimeMsPref(0);
         setWebFeedIntroWebFeedIdShownTimeMsPref(0);
@@ -202,7 +200,6 @@ public final class WebFeedFollowIntroControllerTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1227610")
     public void tooShortDelay_doesNotShowIntro() {
         setWebFeedIntroLastShownTimeMsPref(0);
         setWebFeedIntroWebFeedIdShownTimeMsPref(0);
@@ -268,7 +265,6 @@ public final class WebFeedFollowIntroControllerTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1227610")
     public void featureEngagementTrackerSaysDoNotShow_doesNotShowIntro() {
         setWebFeedIntroLastShownTimeMsPref(0);
         setWebFeedIntroWebFeedIdShownTimeMsPref(0);
