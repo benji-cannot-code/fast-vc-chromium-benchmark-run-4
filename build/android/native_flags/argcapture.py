580b54547d9f30d54380dc4638dc9d8bf15f267c
Peter Wen <wnwen@chromium.org>
2021-07-12 16:30:57 +0000

    Revert "Android: Switch build scripts to use python3"
    This reverts commit 3cfa4a63a14e4ee7b125d3cd576ba486676e6a1a.
    
    Reason for revert: gLinux upgraded to python3.9 and is now breaking catapult due to python3.9's rename of IsAlive -> is_alive
    
    Original change's description:
    > Android: Switch build scripts to use python3
    >
    > All scripts previously using python under build/android should now use
    > python3 instead. Ran:
    >
    > find build/android -name '*.py' -print0 |\
    >   xargs -0 grep 'env python' -H |\
    >   grep -v 'python3' -H
    >
    > Bug: None
    > Change-Id: Ife826b8269e65c80cc668eb56742578bf73ae866
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017583
    > Auto-Submit: Peter Wen <wnwen@chromium.org>
    > Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    > Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#900097}
    
    Bug: 1228231
    Fixed: 1228231
    Change-Id: I0c2f001d32a6fa5f7d98f00afd77e1ee2ba5283d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3020366
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#900514}

--- build/android/native_flags/argcapture.py
+++ build/android/native_flags/argcapture.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright 2021 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
