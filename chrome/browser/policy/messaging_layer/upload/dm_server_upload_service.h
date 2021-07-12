6d5bff42f123097494b5d4f76dc8d328377f858a
Hirthanan Subenderan <hirthanan@google.com>
2021-07-12 15:20:35 +0000

    Remove status_macros dependency in unused headers.
    Dependency is causing a redefinition of defined macro error.
    
    Error message: 'ASSIGN_OR_RETURN' macro redefined [-Werror,-Wmacro-redefined]
    
    Change-Id: I5a2b12d3f3a62a17027beddec30dd476d2dc647b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3019298
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/master@{#900486}

--- chrome/browser/policy/messaging_layer/upload/dm_server_upload_service.h
+++ chrome/browser/policy/messaging_layer/upload/dm_server_upload_service.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reporting/proto/record.pb.h"
 #include "components/reporting/proto/record_constants.pb.h"
 #include "components/reporting/util/status.h"
-#include "components/reporting/util/status_macros.h"
 #include "components/reporting/util/statusor.h"
 #include "components/reporting/util/task_runner_context.h"
 #include "net/base/backoff_entry.h"
--- chrome/browser/policy/messaging_layer/upload/record_handler_impl.h
+++ chrome/browser/policy/messaging_layer/upload/record_handler_impl.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reporting/proto/record.pb.h"
 #include "components/reporting/util/shared_queue.h"
 #include "components/reporting/util/status.h"
-#include "components/reporting/util/status_macros.h"
 #include "components/reporting/util/statusor.h"
 #include "components/reporting/util/task_runner_context.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
