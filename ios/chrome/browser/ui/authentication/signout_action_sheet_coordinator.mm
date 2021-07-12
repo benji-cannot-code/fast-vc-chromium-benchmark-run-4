061f29ee506fb52c3268e20cba17323fd06e3f92
jlebel <jlebel@chromium.org>
2021-07-12 12:19:29 +0000

    [iOS] Dismiss MyGoogle view when account table view is popped out
    In AccountsTableViewController class:
    When the primary account is removed while MyGoogle UI is opened by
    account list view, MyGoogle UI is removed twice.
    Once, from |onEndBatchOfRefreshTokenStateChanges|, by calling
    |dismissAccountDetailsViewControllerBlock| callback.
    And once, from |popViewIfSignedOut|, by dismissing any presented view
    controller.
    
    The issue is:
    |dismissAccountDetailsViewControllerBlock| dismisses MyGoogle UI, and
    |popViewIfSignedOut| dismisses the settings since MyGoogle UI is already
    being dismissed.
    The result is the settings view controller is dismissed without
    the scene controller getting aware.
    
    Solution:
    MyGoogleUI dismissal callback is invoked only in |popViewIfSignedOut|.
    And alert sheets are dismissed explicitly.
    This patch also moves |dismissAccountDetailsViewControllerBlock| from
    an instance variable to property to make sure the block is copied.
    
    Also -[SignoutActionSheetCoordinator stop] is updated to stop its
    action sheet.
    
    Fixed: 1220779
    Change-Id: I6920d7bdea4cd626a0da79ed26fde8a0908730e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2966502
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900434}

--- ios/chrome/browser/ui/authentication/signout_action_sheet_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signout_action_sheet_coordinator.mm
@@ -140,15 +140,15 @@ typedef NS_ENUM(NSUInteger, SignedInUserState) {
   [self.actionSheetCoordinator
       addItemWithTitle:l10n_util::GetNSString(IDS_CANCEL)
                 action:^{
-                  if (weakSelf) {
+                  if (weakSelf)
                     weakSelf.completion(NO);
-                  }
                 }
                  style:UIAlertActionStyleCancel];
   [self.actionSheetCoordinator start];
 }
 
 - (void)stop {
+  [self.actionSheetCoordinator stop];
   self.actionSheetCoordinator = nil;
 }
 
