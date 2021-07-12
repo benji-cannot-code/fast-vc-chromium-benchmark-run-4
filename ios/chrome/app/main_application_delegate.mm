95a2d16bacb85064557cec0816a5d90157d21e43
David Jean <djean@chromium.org>
2021-07-12 19:15:39 +0000

    [ios] Fixed crash in ChromeInternal with 2nd party sdk
    2nd party SDK relies on UIApplication.delegate.window as
    not optional, while it it, causing a crash.
    
    Bug: 1227456
    Change-Id: I36e109580e034299e25e59c4d016388b0f4c0af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021141
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900581}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/main/scene_state.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
+#import "ios/web/common/uikit_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -252,6 +253,12 @@ const int kMainIntentCheckDelay = 1;
                                memoryHelper:_memoryHelper];
 }
 
+#pragma mark - optionals from UIApplicationDelegate required by ChromeInternal
+
+// TODO(crbug.com/1227456): Remove when fixed in ChromeInternal
+- (UIWindow*)window {
+  return GetAnyKeyWindow();
+}
 
 #pragma mark - Testing methods
 
