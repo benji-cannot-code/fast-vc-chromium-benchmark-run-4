1a448e14aec7d712ea1fb3bf7b8dc0cd265f64ae
Clark DuVall <cduvall@chromium.org>
2021-07-12 21:28:20 +0000

    Add histograms to track cookie related operations
    This tracks CookieJar's SetCookie, Cookies, and CookiesEnabled
    operations, as well as whether a cookie manager needed to be requested
    before the operation. This is important because the cookie manager
    request must be completed on the browser process UI thread, which may
    add extra latency if it is busy.
    
    Bug: 1187753
    Change-Id: Icfaf99bd12ceb0e27ca57f96eca8fac763c4a36f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015253
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900653}

--- third_party/blink/renderer/core/loader/cookie_jar.h
+++ third_party/blink/renderer/core/loader/cookie_jar.h
@@ -26,7 +26,7 @@ class CookieJar : public GarbageCollected<CookieJar> {
   bool CookiesEnabled();
 
  private:
-  void RequestRestrictedCookieManagerIfNeeded();
+  bool RequestRestrictedCookieManagerIfNeeded();
 
   HeapMojoRemote<network::mojom::blink::RestrictedCookieManager> backend_;
   Member<blink::Document> document_;
