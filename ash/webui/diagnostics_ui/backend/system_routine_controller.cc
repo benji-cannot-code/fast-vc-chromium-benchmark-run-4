f6baabfa463d46c4dcaf3b0d34711e08fa5e0979
Michael Checo <michaelcheco@google.com>
2021-07-12 19:08:52 +0000

    Diagnostics: Remove hardcoded kCpuStress
     - Currently, the CPU stress routine is always passed to
       `OnStandardRoutineResult`. This change correctly reports the
       routine type when a routine fails to start.
    
    Bug: 1227792
    Change-Id: Ifa17d5722b5b415c26b57f3967de10993bd413cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018424
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900580}

--- ash/webui/diagnostics_ui/backend/system_routine_controller.cc
+++ ash/webui/diagnostics_ui/backend/system_routine_controller.cc
@@ -313,7 +313,7 @@ void SystemRoutineController::OnRoutineStarted(
   if (response_ptr->status ==
           healthd::DiagnosticRoutineStatusEnum::kFailedToStart ||
       response_ptr->id == healthd::kFailedToStartId) {
-    OnStandardRoutineResult(mojom::RoutineType::kCpuStress,
+    OnStandardRoutineResult(routine_type,
                             TestStatusToResult(response_ptr->status));
     return;
   }
