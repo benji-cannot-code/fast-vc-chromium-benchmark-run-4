16ed6639e75c661b52710e22d34581cbf2923690
Ian Helmke <helmke@google.com>
2021-07-12 22:14:04 +0000

    Handle lacros platform in api_binding_util
    This updates api_binding_util.cc to handle lacros as a platform
    instead of treating it as linux. This allows us to add extensions to
    lacros without affecting linux.
    
    Bug: 1111879
    Change-Id: I1d34b06c6deec011ee81852125c031f2d7baf365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015845
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ian Helmke <helmke@google.com>
    Cr-Commit-Position: refs/heads/master@{#900662}

--- extensions/renderer/bindings/api_binding_unittest.cc
+++ extensions/renderer/bindings/api_binding_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "extensions/renderer/bindings/api_binding_hooks.h"
 #include "extensions/renderer/bindings/api_binding_hooks_test_delegate.h"
@@ -686,7 +687,12 @@ TEST_F(APIBindingUnittest, TestProperties) {
       "    'type': 'string',"
       "    'platforms': ['linux']"
       "  },"
-      "  'nonLinuxOnly': {"
+      "  'lacrosOnly': {"
+      "    'value': 'lacros',"
+      "    'type': 'string',"
+      "    'platforms': ['lacros']"
+      "  },"
+      "  'notLinuxOrLacros': {"
       "    'value': 'nonlinux',"
       "    'type': 'string',"
       "    'platforms': ['win', 'mac', 'chromeos']"
@@ -704,16 +710,27 @@ TEST_F(APIBindingUnittest, TestProperties) {
 
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  EXPECT_EQ("\"lacros\"",
+            GetStringPropertyFromObject(binding_object, context, "lacrosOnly"));
+  EXPECT_EQ("undefined",
+            GetStringPropertyFromObject(binding_object, context, "linuxOnly"));
+  EXPECT_EQ("undefined", GetStringPropertyFromObject(binding_object, context,
+                                                     "notLinuxOrLacros"));
+#elif defined(OS_LINUX)
   EXPECT_EQ("\"linux\"",
             GetStringPropertyFromObject(binding_object, context, "linuxOnly"));
   EXPECT_EQ("undefined", GetStringPropertyFromObject(binding_object, context,
-                                                     "nonLinuxOnly"));
+                                                     "notLinuxOrLacros"));
+  EXPECT_EQ("undefined",
+            GetStringPropertyFromObject(binding_object, context, "lacrosOnly"));
 #else
   EXPECT_EQ("undefined",
             GetStringPropertyFromObject(binding_object, context, "linuxOnly"));
+  EXPECT_EQ("undefined",
+            GetStringPropertyFromObject(binding_object, context, "lacrosOnly"));
   EXPECT_EQ("\"nonlinux\"", GetStringPropertyFromObject(binding_object, context,
-                                                        "nonLinuxOnly"));
+                                                        "notLinuxOrLacros"));
 #endif
 }
 
