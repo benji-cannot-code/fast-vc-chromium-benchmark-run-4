15228d73ccf40d3870736318e70136cfa206c5be
Daniel Vogelheim <vogelheim@chromium.org>
2021-07-12 15:20:19 +0000

    [Sanitizer API] Implement comment sanitization.
    Implement comment handling. Also follow the spec on handling non-Element
    node types, as this touches the same methods.
    
    This also eliminates the last known-fail test cases, so we can
    remove the expectations files.
    
    Ref:
    - https://github.com/WICG/sanitizer-api/pull/106
    - https://github.com/WICG/sanitizer-api/pull/105
    
    Bug: 1227409
    Change-Id: I28fe5ab013736c100a31c241839ada96969c2bf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015918
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900485}

--- third_party/blink/renderer/modules/sanitizer_api/sanitizer_api_fuzzer.cc
+++ third_party/blink/renderer/modules/sanitizer_api/sanitizer_api_fuzzer.cc
@@ -8,11 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // To test out locally:
 // - Assuming
 //     $OUT is your local build output directory with use_libbfuzzer set.
-//     $GEN is the suitable 'gen' directory under $OUT. $GEN is
-//         $OUT/gen/third_party_blink/renderer/modules/sanitizer_api.
+//     $GEN is the suitable 'gen' directory under $OUT.
+//
 // - Build:
 //   $ ninja -C $OUT sanitizer_api_fuzzer
 // - Run with:
+//   $ GEN=$OUT/gen/third_party/blink/renderer/modules/sanitizer_api
 //   $ $OUT/sanitizer_api_fuzzer --dict=$GEN/sanitizer_api.dict \
 //       $(mktemp -d) $GEN/corpus/
 
@@ -78,6 +79,7 @@ void MakeConfiguration(SanitizerConfig* sanitizer_config,
     sanitizer_config->setDropAttributes(drop_attributes);
   }
   sanitizer_config->setAllowCustomElements(proto.allow_custom_elements());
+  sanitizer_config->setAllowComments(proto.allow_comments());
 }
 
 void TextProtoFuzzer(const SanitizerConfigProto& proto,
