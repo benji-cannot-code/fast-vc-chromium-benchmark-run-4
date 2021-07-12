b8784e7f2d384a5bdd59aeb1bf12f4fab062ece4
Thomas Lukaszewicz <tluk@chromium.org>
2021-07-12 23:50:22 +0000

    CodeHealth: Remove use of Value::GetAsBoolean
    This change removes the use of Value::GetAs* and replaces with
    Value::is_*(), Value::Get*(), Value::GetIf*().
    
    Bug: 1187003
    Change-Id: Ie1505631f52701c5dd48bf2ea6123523faf533bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021984
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900722}

--- chrome/test/base/web_ui_browser_test_browsertest.cc
+++ chrome/test/base/web_ui_browser_test_browsertest.cc
@@ -76,7 +76,7 @@ IN_PROC_BROWSER_TEST_F(WebUIBrowserExpectFailTest, MAYBE_TestFailsFast) {
   AddLibrary(base::FilePath(FILE_PATH_LITERAL("sample_downloads.js")));
   ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUIDownloadsURL));
   EXPECT_FATAL_FAILURE(RunJavascriptTestNoReturn("DISABLED_BogusFunctionName"),
-                       "GetAsBoolean(&run_test_succeeded_)");
+                       "result.is_bool()");
 }
 
 // Test that bogus javascript fails fast - no timeout waiting for result.
@@ -84,7 +84,7 @@ IN_PROC_BROWSER_TEST_F(WebUIBrowserExpectFailTest, TestRuntimeErrorFailsFast) {
   AddLibrary(base::FilePath(FILE_PATH_LITERAL("runtime_error.js")));
   ui_test_utils::NavigateToURL(browser(), DummyUrl());
   EXPECT_FATAL_FAILURE(RunJavascriptTestNoReturn("TestRuntimeErrorFailsFast"),
-                       "GetAsBoolean(&run_test_succeeded_)");
+                       "result.is_bool()");
 }
 
 // Test times out in debug builds: https://crbug.com/902310
@@ -102,7 +102,7 @@ IN_PROC_BROWSER_TEST_F(WebUIBrowserExpectFailTest, MAYBE_TestFailsAsyncFast) {
   ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUIDownloadsURL));
   EXPECT_FATAL_FAILURE(
       RunJavascriptAsyncTestNoReturn("DISABLED_BogusFunctionName"),
-      "GetAsBoolean(&run_test_succeeded_)");
+      "result.is_bool()");
 }
 
 // Tests that the async framework works.
