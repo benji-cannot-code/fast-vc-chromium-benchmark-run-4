a720e641f5242bcf120928418262871848d77bea
Ewann <ewannpv@google.com>
2021-07-12 14:57:55 +0000

    [iOS] Uses ChromeAccountManagerService in AccountsTableViewController
    This CL uses APIs from ChromeAccountManagerService instead
    of ChromeIdentityService.
    
    Bug: 1220345
    Change-Id: I229ddb579f1e55538c36081b3f859d8e864ce3fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001307
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900475}

--- ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.mm
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
 #include "ios/chrome/browser/signin/authentication_service_factory.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/signin/chrome_identity_service_observer_bridge.h"
 #include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service.h"
@@ -240,12 +242,14 @@ typedef NS_ENUM(NSInteger, ItemType) {
   signin::IdentityManager* identityManager =
       IdentityManagerFactory::GetForBrowserState(_browser->GetBrowserState());
 
+  ChromeAccountManagerService* accountManagerService =
+      ChromeAccountManagerServiceFactory::GetForBrowserState(
+          _browser->GetBrowserState());
+
   NSString* authenticatedEmail = [authenticatedIdentity userEmail];
   for (const auto& account : identityManager->GetAccountsWithRefreshTokens()) {
-    ios::ChromeIdentityService* identityService =
-        ios::GetChromeBrowserProvider().GetChromeIdentityService();
     ChromeIdentity* identity =
-        identityService->GetIdentityWithGaiaID(account.gaia);
+        accountManagerService->GetIdentityWithGaiaID(account.gaia);
     if (!identity) {
       // Ignore the case in which the identity is invalid at lookup time. This
       // may be due to inconsistencies between the identity service and
