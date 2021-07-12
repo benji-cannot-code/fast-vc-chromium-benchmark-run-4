1fb60825172c32f36d91970df25bfb98938f1db4
David Bertoni <dbertoni@chromium.org>
2021-07-12 16:59:53 +0000

    [Extensions] Use a persistent directory for converting some extensions.
    Normally, when converting a test extension to run using a service
    worker, the converted extension is stored in a ScopedTempDir that
    is owned by the test fixture. This doesn't work when the test has a
    PRE_ phase that loads the extension, since the converted extension
    will be cleaned up when that phase is completed.
    
    This CL uses the the system temp directory for these cases, since that
    directory is not scoped to the lifetime of the test fixture. It also
    updates an existing test that previously used two separate test
    extensions to use a single extension instead.
    
    In addition, the test used packed extensions to work around issues
    with reloading that caused flakiness. Since that issue has been
    fixed, the test now uses an unpacked extension. Testing for
    flakiness was done in patchsets 1 and 2.
    
    Bug: 1093066, 1146173
    Change-Id: I16305ed59f8e8ba1a2e222c91520703ee091ad92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001781
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900529}

--- chrome/browser/extensions/extension_context_menu_browsertest.cc
+++ chrome/browser/extensions/extension_context_menu_browsertest.cc
@@ -97,7 +97,7 @@ class StateStoreObserver : public StateStore::TestObserver {
   base::ScopedObservation<StateStore, StateStore::TestObserver> observed_{this};
 };
 
-constexpr char kPersistentExtensionId[] = "cmgkkmeeoiceijkpmaabbmpgnkpaaela";
+constexpr char kPersistentExtensionId[] = "knldjmfmopnpolahpmmgbagdohdnhkik";
 
 }  // namespace
 
@@ -313,13 +313,6 @@ class ExtensionContextMenuLazyTest
                                          {.allow_in_incognito = true});
   }
 
-  base::FilePath GetDirForContext(base::StringPiece subdirectory) {
-    const char* context_dir = GetParam() == ContextType::kServiceWorker
-                                  ? "service_worker"
-                                  : "event_page";
-    return GetRootDir().AppendASCII(subdirectory).AppendASCII(context_dir);
-  }
-
   // This creates an extension that starts |enabled| and then switches to
   // |!enabled|.
   void TestEnabledContextMenu(bool enabled) {
@@ -398,9 +391,8 @@ IN_PROC_BROWSER_TEST_P(ExtensionContextMenuLazyTest, Simple) {
 IN_PROC_BROWSER_TEST_P(ExtensionContextMenuLazyTest, PRE_Persistent) {
   StateStoreObserver observer(profile());
   ResultCatcher catcher;
-  base::FilePath path =
-      GetDirForContext("persistent").AddExtensionASCII(".crx");
-  const extensions::Extension* extension = LoadExtension(path);
+  const extensions::Extension* extension =
+      LoadContextMenuExtension("persistent");
   ASSERT_TRUE(extension);
 
   // Wait for the extension to tell us it's been installed and the
