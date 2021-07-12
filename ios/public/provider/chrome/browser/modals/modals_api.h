022d275539d41d057c071279a60dbf104287c542
Sylvain Defresne <sdefresne@chromium.org>
2021-07-12 09:45:29 +0000

    [ios] Expose `modals` API as free function
    Add two free functions exposing the `modals` API with the goal to
    replace `ModalsProvider` class. Do not convert the code to use
    the function as they have no internal implementation yet.
    
    Bug: 1201182
    Change-Id: Id54605ef39e21e3ee20adcc676ec71aabbddba0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009584
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900385}

---
+++ ios/public/provider/chrome/browser/modals/modals_api.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_MODALS_MODALS_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_MODALS_MODALS_API_H_
+
+#import <UIKit/UIKit.h>
+
+namespace ios {
+namespace provider {
+
+// Dismisses any modals presented from a |collection_view| item.
+void DismissModalsForCollectionView(UICollectionView* collection_view);
+
+// Dismisses any modals presented from a |table_view| cell.
+void DismissModalsForTableView(UITableView* table_view);
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_MODALS_MODALS_API_H_
