abbaa449d8b9bd956bff753b73489b7a9eedecbf
Ian Wells <iwells@chromium.org>
2021-07-12 23:39:21 +0000

    Feed launch reliability logging: misc. changes to support integration tests
    This CL adds methods to XSurface allowing Chrome-side tests to retrieve information about the final reliability logs that were sent. It also adds a command-line switch to skip the metrics-reporting-enabled check because metrics reporting shouldn't be enabled for tests.
    
    Bug: 1195940
    Change-Id: Ic5fcf0de0202473f54b518c5846b2f5e8221e0c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018806
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900710}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -28,6 +28,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.CommandLine;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.R;
@@ -838,6 +839,8 @@ public class FeedSurfaceCoordinator implements FeedSurfaceProvider {
 
     private boolean isReliabilityLoggingEnabled() {
         return ChromeFeatureList.isEnabled(ChromeFeatureList.FEED_RELIABILITY_LOGGING)
-                && mPrivacyPreferencesManager.isMetricsReportingEnabled();
+                && (mPrivacyPreferencesManager.isMetricsReportingEnabled()
+                        || CommandLine.getInstance().hasSwitch(
+                                "force-enable-feed-reliability-logging"));
     }
 }
