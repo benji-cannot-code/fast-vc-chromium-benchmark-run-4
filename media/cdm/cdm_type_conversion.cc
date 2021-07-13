342d6f56f85433c10f14d1a8b4ef1688f096f14d
Xiaohan Wang <xhwang@chromium.org>
2021-07-13 19:45:11 +0000

    media: Report CDM origin on MediaFoundationService crash
    When MediaFoundationService process (for protected content) crashes, we
    report the `cdm_origin` in the crash report. This is done by storing the
    scoped crash key in MediaFoundationCdmFactory. This works because the
    MediaFoundationService process is per-site.
    
    Tested by adding CHECK(false) in MediaFoundationCdm::UpdateSession()
    on Windows and then playing a protected content to trigger a crash. Then
    by manually checking the strings in the crash
    .dmp file as follows:
    ```
    strings64.exe \
    <user-data-dir>/Crashpad/reports/<crash-guid>.dmp | grep -C2 cdm-
    ```
    And saw the following:
    ```
    cdm-origin
    https://integration.uat-nightly.widevine.com
    
    ```
    
    Bug: 999747
    Change-Id: Iea6a4bcd7f4ee34797fc891865ee66df2c8ef6bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022529
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901141}

--- media/cdm/cdm_type_conversion.cc
+++ media/cdm/cdm_type_conversion.cc
@@ -589,7 +589,7 @@ void ToCdmInputBuffer(const DecoderBuffer& encrypted_buffer,
 
   const DecryptConfig* decrypt_config = encrypted_buffer.decrypt_config();
   if (!decrypt_config) {
-    DVLOG(2) << __func__ << ": Clear buffer.";
+    DVLOG(3) << __func__ << ": Clear buffer.";
     return;
   }
 
--- media/cdm/win/media_foundation_cdm_factory.h
+++ media/cdm/win/media_foundation_cdm_factory.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/unguessable_token.h"
 #include "base/win/scoped_com_initializer.h"
+#include "components/crash/core/common/crash_key.h"
 #include "media/base/cdm_factory.h"
 #include "media/base/media_export.h"
 #include "media/cdm/cdm_auxiliary_helper.h"
@@ -88,6 +89,9 @@ class MEDIA_EXPORT MediaFoundationCdmFactory : public CdmFactory {
 
   std::unique_ptr<CdmAuxiliaryHelper> helper_;
   base::FilePath user_data_dir_;
+
+  // CDM origin crash key used in crash reporting.
+  crash_reporter::ScopedCrashKeyString cdm_origin_crash_key_;
 
   // IMFContentDecryptionModule implementations typically require MTA to run.
   base::win::ScopedCOMInitializer com_initializer_{
