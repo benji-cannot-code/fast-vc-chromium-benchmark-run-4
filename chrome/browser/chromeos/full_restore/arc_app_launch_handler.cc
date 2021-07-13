3bdc9fedad61a8bd1b5f69cd01fff32d44c75df2
Nancy Wang <nancylingwang@chromium.org>
2021-07-13 03:59:14 +0000

    Add ARC apps restoration metrics.
    This CL adds 2 metrics:
    
    1. Apps.RestoreArcAppsResult:
       Records whether all Arc app windows have been restored when the
       restoration process finishes.
    
    2. Apps.RestoreArcWindowCount:
       Records the number of restored ARC app windows.
    
    Add logs to record the CPU rate issue and the memory pressure issue.
    
    BUG=1146900
    
    Change-Id: I39de77660db136e4d53d251489d3d82a43429870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3020725
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900824}

--- chrome/browser/chromeos/full_restore/arc_app_launch_handler.cc
+++ chrome/browser/chromeos/full_restore/arc_app_launch_handler.cc
@@ -66,6 +66,11 @@ constexpr int kCpuUsageThreshold = 90;
 // |kCpuRestrictCoresCondition|.
 constexpr int kCpuRestrictCoresCondition = 2;
 
+constexpr char kRestoredAppWindowCountHistogram[] =
+    "Apps.RestoreArcWindowCount";
+
+constexpr char kRestoredArcAppResultHistogram[] = "Apps.RestoreArcAppsResult";
+
 constexpr char kArcGhostWindowLaunchHistogram[] = "Apps.ArcGhostWindowLaunch";
 
 }  // namespace
@@ -283,6 +288,9 @@ void ArcAppLaunchHandler::LoadRestoreData() {
       }
     }
   }
+
+  base::UmaHistogramCounts100(kRestoredAppWindowCountHistogram,
+                              windows_.size() + no_stack_windows_.size());
 }
 
 void ArcAppLaunchHandler::PrepareAppLaunching(const std::string& app_id) {
@@ -360,16 +368,27 @@ bool ArcAppLaunchHandler::HasRestoreData() {
 
 bool ArcAppLaunchHandler::CanLaunchApp() {
   if (should_apply_cpu_restirction_) {
-    if (GetCpuUsageRate() >= kCpuUsageThreshold)
+    int cpu_usage_rate = GetCpuUsageRate();
+    if (cpu_usage_rate >= kCpuUsageThreshold) {
+      LOG(WARNING) << "CPU usage rate is too high to restore Arc apps: "
+                   << cpu_usage_rate;
       return false;
+    }
   }
 
   switch (pressure_level_) {
     case chromeos::ResourcedClient::PressureLevel::NONE:
       return true;
     case chromeos::ResourcedClient::PressureLevel::MODERATE:
-    case chromeos::ResourcedClient::PressureLevel::CRITICAL:
+    case chromeos::ResourcedClient::PressureLevel::CRITICAL: {
+      LOG(WARNING)
+          << "Stop restoring Arc apps due to memory pressure: "
+          << (pressure_level_ ==
+                      chromeos::ResourcedClient::PressureLevel::MODERATE
+                  ? "MODERATE"
+                  : "CRITICAL");
       return false;
+    }
   }
 }
 
@@ -386,8 +405,10 @@ bool ArcAppLaunchHandler::IsAppReady(const std::string& app_id) {
 }
 
 void ArcAppLaunchHandler::MaybeLaunchApp() {
-  if (!first_run_ && !CanLaunchApp())
+  if (!first_run_ && !CanLaunchApp()) {
+    MaybeReStartTimer(kAppLaunchCheckingDelay);
     return;
+  }
 
   for (auto it = pending_windows_.begin(); it != pending_windows_.end(); ++it) {
     if (IsAppReady(it->app_id)) {
@@ -575,6 +596,12 @@ void ArcAppLaunchHandler::StopRestore() {
   stop_restore_timer_.reset();
 
   StopCpuUsageCount();
+
+  base::UmaHistogramEnumeration(
+      kRestoredArcAppResultHistogram,
+      windows_.empty() && no_stack_windows_.empty() && pending_windows_.empty()
+          ? RestoreResult::kFinish
+          : RestoreResult::kNotFinish);
 }
 
 int ArcAppLaunchHandler::GetCpuUsageRate() {
