4a9f5686aa0a9e8d340417b109ec92f605e679a4
Ahmed Nasr <anasr@google.com>
2021-07-13 12:43:46 +0000

    Reset login display host at ExistingUserController test tear down
    ExistingUserController is accessible from the login display host,
    the raw pointer of the existing_user_controller_ used in the test is
    extracted and used by the mock_login_display_host_, so we need to
    destroy the mock_login_display_host_ when the existing_user_controller_
    is destroyed
    
    Bug: b:192818906
    Change-Id: I0c25af3f21b992d98398c5319d8eeb3cbf763704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3019295
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/master@{#900966}

--- chrome/browser/ash/login/existing_user_controller_browsertest.cc
+++ chrome/browser/ash/login/existing_user_controller_browsertest.cc
@@ -104,6 +104,7 @@ using ::testing::_;
 using ::testing::AnyNumber;
 using ::testing::Invoke;
 using ::testing::InvokeWithoutArgs;
+using ::testing::Mock;
 using ::testing::Return;
 using ::testing::ReturnNull;
 using ::testing::WithArg;
@@ -261,7 +262,9 @@ class ExistingUserControllerTest : public policy::DevicePolicyCrosBrowserTest {
 
     // `existing_user_controller_` has data members that are CrosSettings
     // observers. They need to be destructed before CrosSettings.
+    Mock::VerifyAndClear(mock_login_display_host_.get());
     existing_user_controller_.reset();
+    mock_login_display_host_.reset();
 
     // Test case may be configured with the real user manager but empty user
     // list initially. So network OOBE screen is initialized.
