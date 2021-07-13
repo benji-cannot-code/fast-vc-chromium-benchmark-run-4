1b3cab7eae89e8965fd4ccfab94dfc750b437597
Antonio Sartori <antoniosartori@chromium.org>
2021-07-13 08:42:47 +0000

    Propagate anonymous attribute to the browser
    This CL adds logic for propagating the (experimental) iframe
    'anonymous' attribute to the browser process. In particular, this CL
    adds a boolean `anonymous_` parameter to the FrameTreeNode, the
    NavigationRequest and the RenderFrameHost, with the following
    meanings:
    - A FrameTreeNode is anonymous if either the corresponding iframe has
      the 'anonymous' attribute, or if its parent is an anonymous
      RenderFrameHost.
    - A NavigationRequest is anonymous if it will commit into an anonymous
      FrameTreeNode.
    - A RenderFrameHost is anonymous if it was committed by an anonymous
      NavigationRequest.
    
    See the design document for more details:
    https://docs.google.com/document/d/1f8lDmVCPc4CA8zCIlVtVcYEHStFJOEimqltB1z6_W1U/edit
    
    Note: the 'anonymous' attribute is currently parsed only when the
    blink runtime feature "AnonymousIframe" is enabled. Hence this whole
    code is a no-op unless that feature flag is set.
    
    Bug: 1226469
    Change-Id: Ifbb097dfe21afde0ce4f8f5779f3660d8df621e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002073
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900899}

--- third_party/blink/renderer/core/frame/frame_owner.h
+++ third_party/blink/renderer/core/frame/frame_owner.h
@@ -69,7 +69,7 @@ class CORE_EXPORT FrameOwner : public GarbageCollectedMixin {
 
  protected:
   virtual void FrameOwnerPropertiesChanged() {}
-  virtual void CSPAttributeChanged() {}
+  virtual void DidChangeAttributes() {}
 
  private:
   virtual void SetIsSwappingFrames(bool) {}
@@ -98,7 +98,7 @@ class FrameSwapScope {
     if (frame_owner_) {
       frame_owner_->SetIsSwappingFrames(false);
       frame_owner_->FrameOwnerPropertiesChanged();
-      frame_owner_->CSPAttributeChanged();
+      frame_owner_->DidChangeAttributes();
     }
   }
 
--- third_party/blink/renderer/core/html/html_iframe_element.cc
+++ third_party/blink/renderer/core/html/html_iframe_element.cc
@@ -220,8 +220,15 @@ void HTMLIFrameElement::ParseAttribute(
     }
     if (required_csp_ != value) {
       required_csp_ = value;
-      CSPAttributeChanged();
+      DidChangeAttributes();
       UseCounter::Count(GetDocument(), WebFeature::kIFrameCSPAttribute);
+    }
+  } else if (name == html_names::kAnonymousAttr &&
+             RuntimeEnabledFeatures::AnonymousIframeEnabled()) {
+    bool new_value = !value.IsNull();
+    if (anonymous_ != new_value) {
+      anonymous_ = new_value;
+      DidChangeAttributes();
     }
   } else if (name == html_names::kAllowAttr) {
     if (allow_ != value) {
@@ -462,7 +469,7 @@ HTMLIFrameElement::ConstructTrustTokenParams() const {
   return parsed_params;
 }
 
-void HTMLIFrameElement::CSPAttributeChanged() {
+void HTMLIFrameElement::DidChangeAttributes() {
   // Don't notify about updates if ContentFrame() is null, for example when
   // the subframe hasn't been created yet; or if we are in the middle of
   // swapping one frame for another, in which case the final state
@@ -483,9 +490,9 @@ void HTMLIFrameElement::CSPAttributeChanged() {
           network::mojom::blink::ContentSecurityPolicySource::kHTTP, KURL());
   DCHECK_LE(csp.size(), 1u);
 
-  GetDocument().GetFrame()->GetLocalFrameHostRemote().DidChangeCSPAttribute(
+  GetDocument().GetFrame()->GetLocalFrameHostRemote().DidChangeIframeAttributes(
       ContentFrame()->GetFrameToken(),
-      csp.IsEmpty() ? nullptr : std::move(csp[0]));
+      csp.IsEmpty() ? nullptr : std::move(csp[0]), anonymous_);
 }
 
 }  // namespace blink
