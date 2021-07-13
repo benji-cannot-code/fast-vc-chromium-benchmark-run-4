1203a6e20f17144fbfe4af09cd85422f4c5e9020
Dan Sanders <sandersd@chromium.org>
2021-07-13 00:09:49 +0000

    [webcodecs] Implement color spaces
    This CL adds color spaces to WebCodecs APIs, as described in [1].
    
    [1] https://github.com/w3c/webcodecs/issues/47#issuecomment-854156824
    
    Bug: 1138680
    Change-Id: I82d4fb2fd23f8b0cf30b56d8d08225286904c6d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011944
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900738}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_arraybuffer_arraybufferview.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_avc_encoder_config.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_encoded_video_chunk_metadata.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_video_color_space_init.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_decoder_config.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_encoder_config.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_encoder_encode_options.h"
@@ -51,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webcodecs/codec_state_helper.h"
 #include "third_party/blink/renderer/modules/webcodecs/encoded_video_chunk.h"
 #include "third_party/blink/renderer/modules/webcodecs/gpu_factories_retriever.h"
+#include "third_party/blink/renderer/modules/webcodecs/video_color_space.h"
 #include "third_party/blink/renderer/platform/bindings/enumeration_base.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
@@ -243,6 +245,7 @@ VideoEncoderTraits::ParsedConfig* ParseConfigStatic(
   bool is_codec_ambiguous = true;
   result->codec = media::kUnknownVideoCodec;
   result->profile = media::VIDEO_CODEC_PROFILE_UNKNOWN;
+  // TODO(crbug.com/1138680): Default to sRGB if encoding an RGB format.
   result->color_space = media::VideoColorSpace::REC709();
   result->level = 0;
   result->codec_string = config->codec();
@@ -254,6 +257,12 @@ VideoEncoderTraits::ParsedConfig* ParseConfigStatic(
   if (!parse_succeeded || is_codec_ambiguous) {
     exception_state.ThrowTypeError("Unknown codec.");
     return nullptr;
+  }
+
+  if (config->hasColorSpace()) {
+    VideoColorSpace* color_space =
+        MakeGarbageCollected<VideoColorSpace>(config->colorSpace());
+    result->color_space = color_space->ToMediaColorSpace();
   }
 
   // We are done with the parsing.
@@ -339,6 +348,12 @@ VideoEncoderConfig* CopyConfig(const VideoEncoderConfig& config) {
 
   if (config.hasBitrateMode())
     result->setBitrateMode(config.bitrateMode());
+
+  if (config.hasColorSpace()) {
+    VideoColorSpace* color_space =
+        MakeGarbageCollected<VideoColorSpace>(config.colorSpace());
+    result->setColorSpace(color_space->toJSON());
+  }
 
   if (config.hasLatencyMode())
     result->setLatencyMode(config.latencyMode());
@@ -726,6 +741,10 @@ void VideoEncoder::CallOutputCallback(
       decoder_config->setDisplayAspectWidth(
           active_config->display_size.value().width());
     }
+
+    VideoColorSpace* color_space =
+        MakeGarbageCollected<VideoColorSpace>(active_config->color_space);
+    decoder_config->setColorSpace(color_space->toJSON());
 
     if (codec_desc.has_value()) {
       auto* desc_array_buf = DOMArrayBuffer::Create(codec_desc.value().data(),
--- third_party/blink/renderer/modules/webcodecs/video_frame.h
+++ third_party/blink/renderer/modules/webcodecs/video_frame.h
@@ -40,6 +40,7 @@ class ExecutionContext;
 class PlaneInit;
 class ScriptPromise;
 class ScriptState;
+class VideoColorSpace;
 class VideoFrameBufferInit;
 class VideoFrameCopyToOptions;
 class VideoFrameInit;
@@ -75,14 +76,17 @@ class MODULES_EXPORT VideoFrame final : public ScriptWrappable,
 
   absl::optional<V8VideoPixelFormat> format() const;
 
+  absl::optional<int64_t> timestamp() const;
+  absl::optional<uint64_t> duration() const;
+
   // DEPRECATED.
   absl::optional<HeapVector<Member<Plane>>> planes(ExecutionContext*);
 
   uint32_t codedWidth() const;
   uint32_t codedHeight() const;
 
-  DOMRectReadOnly* codedRect();
-  DOMRectReadOnly* visibleRect();
+  absl::optional<DOMRectReadOnly*> codedRect();
+  absl::optional<DOMRectReadOnly*> visibleRect();
 
   // DEPRECATED.
   VideoFrameRect* codedRegion(ExecutionContext*) const;
@@ -97,8 +101,7 @@ class MODULES_EXPORT VideoFrame final : public ScriptWrappable,
   uint32_t displayWidth() const;
   uint32_t displayHeight() const;
 
-  absl::optional<int64_t> timestamp() const;
-  absl::optional<uint64_t> duration() const;
+  absl::optional<VideoColorSpace*> colorSpace();
 
   uint32_t allocationSize(VideoFrameCopyToOptions* options, ExceptionState&);
 
@@ -151,7 +154,7 @@ class MODULES_EXPORT VideoFrame final : public ScriptWrappable,
   HeapVector<Member<Plane>> planes_;
   Member<DOMRectReadOnly> coded_rect_;
   Member<DOMRectReadOnly> visible_rect_;
-  Member<DOMRectReadOnly> empty_rect_;
+  Member<VideoColorSpace> color_space_;
 };
 
 }  // namespace blink
