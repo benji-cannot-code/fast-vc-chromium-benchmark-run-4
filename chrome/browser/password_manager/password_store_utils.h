9b6cf93f4f49de8e3eeb13185990a5a93bcf2a51
Viktor Semeniuk <vsemeniuk@google.com>
2021-07-13 09:49:17 +0000

    Removing EditSavedPasswords since it's no longer used
    Bug: 1136815
    Change-Id: I1be1141a2d93b964fbc7cc3e23a89d494e41b4ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021144
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#900923}

--- chrome/browser/password_manager/password_store_utils.h
+++ chrome/browser/password_manager/password_store_utils.h
@@ -8,30 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_STORE_UTILS_H_
 #define CHROME_BROWSER_PASSWORD_MANAGER_PASSWORD_STORE_UTILS_H_
 
-#include <string>
-
-#include "base/containers/span.h"
 #include "base/memory/scoped_refptr.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace password_manager {
 class PasswordStoreInterface;
-struct PasswordForm;
 }
 
 class Profile;
-
-// Changes a credential record in password store. If new_password is null it
-// isn't changed, but if it is non-null it can't be empty.
-// |forms_to_change| are the forms in which the usernames and passwords are
-// changed. This function assumes that all conflicts checks have already
-// been performed, prior to calling it.
-void EditSavedPasswords(
-    Profile* profile,
-    base::span<const std::unique_ptr<password_manager::PasswordForm>>
-        forms_to_change,
-    const std::u16string& new_username,
-    const absl::optional<std::u16string>& new_password);
 
 // Returns the password store associated with the currently active profile.
 password_manager::PasswordStoreInterface* GetPasswordStore(
