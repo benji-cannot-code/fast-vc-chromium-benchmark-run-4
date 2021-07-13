d202c3da813bfc5cc3a88dd476752d306834087c
Hiroki Sato <hirokisato@google.com>
2021-07-13 08:41:27 +0000

    arc-a11y: Support multi display
    This fixes an issue where automation node bounds of ARC++ tree are
    incorrect in a multi-display environment.
    
    Android sends the bounds relative to the display. With this change,
    bounds are correctly offset.
    
    Note that older Android (pi-arc) has its own coordinate system and it's
    out of the scope of this change. Either before or after this change, the
    bounds are correct only in a single display environment.
    
    Also note that the scaling on an external display is incorrect, even on
    browsers, and it's tracked in crbug/1037968.
    
    AX-Relnotes: ARC++ supports a11y in multi display environment if the device is running Android 11.
    
    Bug: b:161758424
    Test: Play ChromeVox, S2S, "highlight focus" with multi display.
    Change-Id: Icb751112dc93617f1a8cb4b4dd50f5c7e7ec7658
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3008854
    Reviewed-by: Sara Kato <sarakato@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900898}

--- chrome/browser/ash/arc/accessibility/arc_accessibility_helper_bridge.cc
+++ chrome/browser/ash/arc/accessibility/arc_accessibility_helper_bridge.cc
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/views/controls/native/native_view_host.h"
@@ -100,6 +102,10 @@ void DispatchFocusChange(arc::mojom::AccessibilityNodeInfoData* node_data,
                  active_window));
   bounds_in_screen.Offset(0,
                           arc::GetChromeWindowHeightOffsetInDip(active_window));
+
+  const display::Display display =
+      display::Screen::GetScreen()->GetDisplayNearestView(active_window);
+  bounds_in_screen.Offset(display.bounds().x(), display.bounds().y());
 
   bool is_editable = arc::GetBooleanProperty(
       node_data, arc::mojom::AccessibilityBooleanProperty::EDITABLE);
--- chrome/browser/ash/arc/accessibility/geometry_util.h
+++ chrome/browser/ash/arc/accessibility/geometry_util.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_ARC_ACCESSIBILITY_GEOMETRY_UTIL_H_
 #define CHROME_BROWSER_ASH_ARC_ACCESSIBILITY_GEOMETRY_UTIL_H_
 
-// TODO(hirokisato): support multiple display.
-
 namespace gfx {
 class Rect;
 class RectF;
