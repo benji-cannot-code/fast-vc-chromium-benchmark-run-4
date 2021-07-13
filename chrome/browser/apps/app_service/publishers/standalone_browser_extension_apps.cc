e7a07cee07ae9039859ce5fb5584334576cc0fa7
Erik Chen <erikchen@chromium.org>
2021-07-13 20:17:11 +0000

    lacros: Add a BindChromeAppPublisher crosapi.
    The AppPublisher interface is generic and can be used for different
    types of apps, include Web Apps and Chrome Apps. However, there is
    a single Bind method called BindAppPublisher which is implicitly bound
    to the WebAppPublisher in ash.
    
    There are two ways to reuse the same AppPublisher interface for Chrome
    Apps. The first is to multiplex both Web Apps and Chrome Apps method
    calls over a single remote/interface. The second is to create two
    remotes/interfaces.
    
    While some methods like OnApps() can be easily multiplexed, since the
    input parameters include sufficient information to route to an
    appropriate publisher, other methods like OnCapabilityAccesses cannot be
    easily multiplexed. As such, this CL chooses the second approach, which
    has no drawbacks. The performance cost of creating a single mojo
    interface/remote is negligible.
    
    This CL renames the existing BindAppPublisher method to be
    BindWebAppPublisher, and adds a new method called
    BindChromeAppPublisher.
    
    Note that this CL intentionally avoids using the syntax sugar in
    LacrosChromeServiceImpl. The syntax sugar relies on the assumption that
    each Interface only has a single associated Bind method.
    
    Bug: 1225848
    Change-Id: I4a1396d8632453a0f9220c1da7796eee417e523d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018482
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901157}

--- chrome/browser/apps/app_service/publishers/standalone_browser_extension_apps.cc
+++ chrome/browser/apps/app_service/publishers/standalone_browser_extension_apps.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_service/publishers/standalone_browser_extension_apps.h"
 
+#include <utility>
+
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
@@ -25,6 +27,20 @@ StandaloneBrowserExtensionApps::StandaloneBrowserExtensionApps(
 }
 
 StandaloneBrowserExtensionApps::~StandaloneBrowserExtensionApps() = default;
+
+void StandaloneBrowserExtensionApps::RegisterChromeAppsCrosapiHost(
+    mojo::PendingReceiver<crosapi::mojom::AppPublisher> receiver) {
+  // At the moment the app service publisher will only accept one client
+  // publishing apps to ash chrome. Any extra clients will be ignored.
+  // TODO(crbug.com/1174246): Support SxS lacros.
+  if (receiver_.is_bound()) {
+    return;
+  }
+  receiver_.Bind(std::move(receiver));
+  receiver_.set_disconnect_handler(
+      base::BindOnce(&StandaloneBrowserExtensionApps::OnReceiverDisconnected,
+                     weak_factory_.GetWeakPtr()));
+}
 
 void StandaloneBrowserExtensionApps::Connect(
     mojo::PendingRemote<apps::mojom::Subscriber> subscriber_remote,
@@ -53,6 +69,7 @@ void StandaloneBrowserExtensionApps::Launch(
     apps::mojom::LaunchSource launch_source,
     apps::mojom::WindowInfoPtr window_info) {
   // TODO(https://crbug.com/1225848): Implement.
+  NOTIMPLEMENTED();
 }
 
 void StandaloneBrowserExtensionApps::GetMenuModel(
@@ -61,6 +78,40 @@ void StandaloneBrowserExtensionApps::GetMenuModel(
     int64_t display_id,
     GetMenuModelCallback callback) {
   // TODO(https://crbug.com/1225848): Implement.
+  NOTIMPLEMENTED();
+}
+
+void StandaloneBrowserExtensionApps::OnApps(
+    std::vector<apps::mojom::AppPtr> deltas) {
+  // TODO(https://crbug.com/1225848): Implement.
+  NOTIMPLEMENTED();
+}
+
+void StandaloneBrowserExtensionApps::RegisterAppController(
+    mojo::PendingRemote<crosapi::mojom::AppController> controller) {
+  if (controller_.is_bound()) {
+    return;
+  }
+  controller_.Bind(std::move(controller));
+  controller_.set_disconnect_handler(
+      base::BindOnce(&StandaloneBrowserExtensionApps::OnControllerDisconnected,
+                     weak_factory_.GetWeakPtr()));
+}
+
+void StandaloneBrowserExtensionApps::OnCapabilityAccesses(
+    std::vector<apps::mojom::CapabilityAccessPtr> deltas) {
+  // TODO(https://crbug.com/1225848): Implement.
+  NOTIMPLEMENTED();
+}
+
+void StandaloneBrowserExtensionApps::OnReceiverDisconnected() {
+  receiver_.reset();
+  controller_.reset();
+}
+
+void StandaloneBrowserExtensionApps::OnControllerDisconnected() {
+  receiver_.reset();
+  controller_.reset();
 }
 
 }  // namespace apps
