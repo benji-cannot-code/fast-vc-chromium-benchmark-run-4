37f3e13ea81b2d01237e0709ba38c861ce4e27a7
Raymond Toy <rtoy@chromium.org>
2021-07-13 17:55:54 +0000

    Update WATCHLISTS and OWNERS files
    Change-Id: I5e8ef9968857182e7d9c201109f1041a59e9b126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3012199
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901090}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -702,7 +702,8 @@ class BlinkPerfDisplayLocking(_BlinkPerfBenchmark):
     options.AppendExtraBrowserArgs(
       ['--enable-blink-features=DisplayLocking,CSSContentSize'])
 
-@benchmark.Info(emails=['hongchan@chromium.org', 'rtoy@chromium.org'],
+
+@benchmark.Info(emails=['hongchan@chromium.org'],
                 component='Blink>WebAudio',
                 documentation_url='https://bit.ly/blink-perf-benchmarks')
 class BlinkPerfWebAudio(_BlinkPerfBenchmark):
