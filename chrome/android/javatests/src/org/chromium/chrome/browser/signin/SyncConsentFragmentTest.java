a97642536c1ed44480d02f15d74c4d5857556851
Tanmoy Mollik <triploblastic@chromium.org>
2021-07-13 20:32:56 +0000

    [Signin] Remove parameter from SyncConsentFragmentBase constructor
    Passing ModalDialogManager through fragment constructor is not stable
    as it can create problems when the fragment is recreated. When the
    fragment is recreated the default no-argument constructor is called
    and the ModalDialogManager reference might be a null pointer.
    
    This cl lets ChromeBaseAppCompatActivity implement the
    ModalDialogManagerHolder interface so that its subclasses can supply
    a ModalDialogManager when a fragment is attached to the activity.
    
    Bug: 1224314, 1225102
    Change-Id: I24400e16852570608e7970eac48d7c79e1b811f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001063
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901175}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
@@ -16,7 +16,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.lifecycle.Stage;
@@ -62,15 +61,12 @@ import org.chromium.chrome.test.util.ActivityTestUtils;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
 import org.chromium.chrome.test.util.browser.sync.SyncTestUtil;
-import org.chromium.components.browser_ui.modaldialog.AppModalPresenter;
 import org.chromium.components.externalauth.ExternalAuthUtils;
 import org.chromium.components.signin.ChildAccountStatus;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
 import org.chromium.components.signin.test.util.FakeAccountInfoService;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
-import org.chromium.ui.modaldialog.ModalDialogManager;
-import org.chromium.ui.modaldialog.ModalDialogManager.ModalDialogType;
 import org.chromium.ui.test.util.DisableAnimationsTestRule;
 import org.chromium.ui.test.util.DummyUiActivity;
 import org.chromium.ui.test.util.ThemedDummyUiActivityTestRule;
@@ -90,10 +86,6 @@ public class SyncConsentFragmentTest {
      */
     public static class CustomSyncConsentFirstRunFragment extends SyncConsentFirstRunFragment {
         private FirstRunPageDelegate mFirstRunPageDelegate;
-
-        public CustomSyncConsentFirstRunFragment(Context context) {
-            super(new ModalDialogManager(new AppModalPresenter(context), ModalDialogType.APP));
-        }
 
         @Override
         public FirstRunPageDelegate getPageDelegate() {
@@ -227,8 +219,7 @@ public class SyncConsentFragmentTest {
                 new HistogramDelta("Signin.AndroidDeviceAccountsNumberWhenEnteringFRE", 0);
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
-        CustomSyncConsentFirstRunFragment fragment =
-                new CustomSyncConsentFirstRunFragment(mActivityTestRule.getActivity());
+        CustomSyncConsentFirstRunFragment fragment = new CustomSyncConsentFirstRunFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(
                 SyncConsentFirstRunFragment.CHILD_ACCOUNT_STATUS, ChildAccountStatus.NOT_CHILD);
@@ -251,8 +242,7 @@ public class SyncConsentFragmentTest {
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
         mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_EMAIL);
-        CustomSyncConsentFirstRunFragment fragment =
-                new CustomSyncConsentFirstRunFragment(mActivityTestRule.getActivity());
+        CustomSyncConsentFirstRunFragment fragment = new CustomSyncConsentFirstRunFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(
                 SyncConsentFirstRunFragment.CHILD_ACCOUNT_STATUS, ChildAccountStatus.NOT_CHILD);
@@ -275,8 +265,7 @@ public class SyncConsentFragmentTest {
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
         mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_EMAIL);
-        CustomSyncConsentFirstRunFragment fragment =
-                new CustomSyncConsentFirstRunFragment(mActivityTestRule.getActivity());
+        CustomSyncConsentFirstRunFragment fragment = new CustomSyncConsentFirstRunFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(
                 SyncConsentFirstRunFragment.CHILD_ACCOUNT_STATUS, ChildAccountStatus.REGULAR_CHILD);
@@ -299,8 +288,7 @@ public class SyncConsentFragmentTest {
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
         mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_EMAIL);
-        CustomSyncConsentFirstRunFragment fragment =
-                new CustomSyncConsentFirstRunFragment(mActivityTestRule.getActivity());
+        CustomSyncConsentFirstRunFragment fragment = new CustomSyncConsentFirstRunFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(
                 SyncConsentFirstRunFragment.CHILD_ACCOUNT_STATUS, ChildAccountStatus.USM_CHILD);
