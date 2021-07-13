192b4b9e564dfa4eefe22f89db3b4583fe205863
Omer Katz <omerkatz@chromium.org>
2021-07-13 09:00:08 +0000

    Fix cppgc library build
    No-try: true
    Bug: 1056170
    Change-Id: I1cf20fc343737c0a1ade542edd71003716c506c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021147
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900908}

--- third_party/blink/renderer/modules/webaudio/audio_scheduled_source_node.h
+++ third_party/blink/renderer/modules/webaudio/audio_scheduled_source_node.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_node.h"
+#include "third_party/blink/renderer/platform/wtf/threading_primitives.h"
 
 namespace base {
 class SingleThreadTaskRunner;
--- third_party/blink/renderer/modules/webaudio/script_processor_node.h
+++ third_party/blink/renderer/modules/webaudio/script_processor_node.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/audio/audio_bus.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
+#include "third_party/blink/renderer/platform/wtf/threading_primitives.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace base {
