4490e21b7135968d5e424f5e8e517e1508968acd
Matt Jones <mdjones@chromium.org>
2021-07-13 18:33:33 +0000

    Fix BottomSheet layout in RTL mode
    This patch fixes an RTL bug in the BottomSheet's positioning logic.
    In RTL mode, (0, 0) is on the right side of the screen rather than the
    left. Consequently, the sheet and shadow's x adjustment must be
    inverted to be displayed correctly.
    
    Bug: 1227175
    Change-Id: I003d4a95415adcbad2009753eeb611f74bb74c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016601
    Commit-Queue: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901108}

--- components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheet.java
+++ components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheet.java
@@ -34,6 +34,7 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.Shee
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.StateChangeReason;
 import org.chromium.components.browser_ui.bottomsheet.internal.R;
 import org.chromium.ui.KeyboardVisibilityDelegate;
+import org.chromium.ui.base.LocalizationUtils;
 import org.chromium.ui.util.AccessibilityUtil;
 
 /**
@@ -183,7 +184,7 @@ class BottomSheet extends FrameLayout
             super(context, atts);
             mShadowLength = context.getResources().getDimensionPixelSize(
                     R.dimen.bottom_sheet_shadow_length);
-            setTranslationX(-mShadowLength);
+            setTranslationX((LocalizationUtils.isLayoutRtl() ? 1 : -1) * mShadowLength);
             setTranslationY(-mShadowLength);
         }
 
@@ -1073,7 +1074,8 @@ class BottomSheet extends FrameLayout
     private void sizeAndPositionSheetInParent() {
         int maxSheetWidth = getMaxSheetWidth();
         getLayoutParams().width = maxSheetWidth;
-        setTranslationX((mContainerWidth - maxSheetWidth) / 2f);
+        setTranslationX((LocalizationUtils.isLayoutRtl() ? -1 : 1)
+                * (mContainerWidth - maxSheetWidth) / 2f);
         requestLayout();
     }
 
