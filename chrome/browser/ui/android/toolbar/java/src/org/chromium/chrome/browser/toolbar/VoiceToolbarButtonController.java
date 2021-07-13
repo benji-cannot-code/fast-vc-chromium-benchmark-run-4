9af52988a91b5ec096da68be371e66bf66c2cc44
Patrick Noland <pnoland@chromium.org>
2021-07-13 02:22:25 +0000

    [segmentation_platform] Fix voice being disabled for customization
    The voice button controller was erroneously turned off when
    customization is on in a past CL. This change reverts that.
    
    Bug: 1228393
    Change-Id: I552d8118e243e45bffb0ab340c64b07d392c1d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022306
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900788}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/VoiceToolbarButtonController.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/VoiceToolbarButtonController.java
@@ -231,7 +231,8 @@ public class VoiceToolbarButtonController
         if (!FeatureList.isInitialized()) return false;
         return AdaptiveToolbarFeatures.isSingleVariantModeEnabled()
                 && AdaptiveToolbarFeatures.getSingleVariantMode()
-                == AdaptiveToolbarButtonVariant.VOICE;
+                        == AdaptiveToolbarButtonVariant.VOICE
+                || AdaptiveToolbarFeatures.isCustomizationEnabled();
     }
 
     private void notifyObservers(boolean hint) {
