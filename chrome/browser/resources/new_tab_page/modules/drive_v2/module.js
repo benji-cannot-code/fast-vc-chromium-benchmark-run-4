68a8ce9e66d0508fb40cca6aba986c766a21c4ae
Deanna Oei <doei@google.com>
2021-07-13 23:13:39 +0000

    [ntp][modules] Fetch files for Drive V2 Module
    Currently, the module is empty. This CL will add the files from Drive
    to the module. Files will be styled in future CLs.
    
    * Fetch Drive files and display inside the module.
    * Add unit test for fetching files.
    
    Before: empty module
    After: screenshot/6RAFGkQDCMCn4n4
    
    Fixed: 1228385
    Change-Id: I613a1448d6fb18002f296a72b921d87ea96b802d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022581
    Commit-Queue: Deanna Oei <doei@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901196}

--- chrome/browser/resources/new_tab_page/modules/drive_v2/module.js
+++ chrome/browser/resources/new_tab_page/modules/drive_v2/module.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {loadTimeData} from '../../i18n_setup.js';
+import {DriveProxy} from '../drive/drive_module_proxy.js';
 import {ModuleDescriptor} from '../module_descriptor.js';
 
 class DriveModuleElement extends PolymerElement {
@@ -16,13 +17,25 @@ class DriveModuleElement extends PolymerElement {
   static get template() {
     return html`{__html_template__}`;
   }
+
+  static get properties() {
+    return {
+      /** @type {!Array<!drive.mojom.File>} */
+      files: Array,
+    };
+  }
 }
 
 customElements.define(DriveModuleElement.is, DriveModuleElement);
 
-/** @return {!Promise<!DriveModuleElement>} */
+/** @return {!Promise<DriveModuleElement>} */
 async function createDriveElement() {
+  const {files} = await DriveProxy.getInstance().handler.getFiles();
+  if (files.length === 0) {
+    return null;
+  }
   const element = new DriveModuleElement();
+  element.files = files;
   return element;
 }
 
