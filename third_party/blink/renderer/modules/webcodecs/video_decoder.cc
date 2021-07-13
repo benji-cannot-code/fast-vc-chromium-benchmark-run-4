da5126301426c1cc1dde9e8ed63f77ad54cdf279
Dan Sanders <sandersd@chromium.org>
2021-07-13 23:17:24 +0000

    [webcodecs] Remove deprecated APIs
    Change-Id: I0a64add72a1f9b989e29ceb9e3d71a05e88db805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017512
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901236}

--- third_party/blink/renderer/modules/webcodecs/video_decoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_decoder.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_color_space_init.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_decoder_config.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_decoder_support.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_video_frame_rect.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/webcodecs/codec_config_eval.h"
 #include "third_party/blink/renderer/modules/webcodecs/encoded_video_chunk.h"
