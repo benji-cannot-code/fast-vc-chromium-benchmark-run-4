f821fa4fc3295642fb5c5041b9344a5e4c30c230
Rob Buis <rbuis@igalia.com>
2021-07-13 10:20:27 +0000

    [AspectRatio] Fix implicit/explicit stretching tests in legacy
    Fix various combinations of implicit/explicit stretching in inline and
    block directions as tested by grid-aspect-ratio-028.html until
    grid-aspect-ratio-038.html.
    
    Bug: 1045668
    
    Change-Id: Ic5797b2a2a10bea63fb478af5339e4ab57fdee5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3010377
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#900927}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -4295,8 +4295,8 @@ void LayoutBox::ComputeLogicalWidth(
     computed_values.extent_ =
         ComputeReplacedLogicalWidth() + BorderAndPaddingLogicalWidth();
   } else if (StyleRef().LogicalWidth().IsAuto() &&
-             (!IsGridItem() || !HasStretchedLogicalWidth() ||
-              !HasStretchedLogicalHeight()) &&
+             (!IsGridItem() || !ShouldComputeSizeAsReplaced() ||
+              !HasStretchedLogicalWidth() || !HasStretchedLogicalHeight()) &&
              ComputeLogicalWidthFromAspectRatio(&computed_values.extent_)) {
     /* we're good */
   } else {
@@ -5546,7 +5546,6 @@ LayoutUnit LayoutBox::AvailableLogicalHeightUsing(
       }
     }
   }
-
   if (ShouldComputeLogicalHeightFromAspectRatio()) {
     NGBoxStrut border_padding(BorderStart() + ComputedCSSPaddingStart(),
                               BorderEnd() + ComputedCSSPaddingEnd(),
