ec78199ca9b6211d771ced271d0645f1f5fe5226
Sylvain Defresne <sdefresne@chromium.org>
2021-07-13 08:24:15 +0000

    [CodeHealth] Remove use of ListValue/DictionaryValue in //base
    Remove the last usages of ListValue/DictionaryValue in //base
    apart from the Value unit tests (and the Value implementation).
    
    Bug: 1187061
    Change-Id: Ic87111c212d00c06d2981000f3f3ae814b7fe2f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021184
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900891}

--- base/task/thread_pool/task_tracker.cc
+++ base/task/thread_pool/task_tracker.cc
@@ -67,7 +67,7 @@ class TaskTracingInfo : public trace_event::ConvertableToTraceFormat {
 };
 
 void TaskTracingInfo::AppendAsTraceFormat(std::string* out) const {
-  DictionaryValue dict;
+  Value dict(Value::Type::DICTIONARY);
 
   dict.SetStringKey("task_priority",
                     base::TaskPriorityToString(task_traits_.priority()));
