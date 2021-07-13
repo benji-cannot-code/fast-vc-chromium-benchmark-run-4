f6f33e60bbb7e2fc6f6b7b178cd65b095f96f478
Lily Chen <chlily@chromium.org>
2021-07-13 01:19:41 +0000

    Remove expired histograms for SameSite cookie site engagement score
    These histograms were expired, and the logging code was apparently CPU
    intensive.
    
    Bug: 1228065
    Change-Id: I51a900d568926c60a648364a784e6036c0608eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021591
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900768}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -282,7 +282,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/content/ssl_error_handler.h"
 #include "components/security_interstitials/content/ssl_error_navigation_throttle.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
-#include "components/site_engagement/content/site_engagement_service.h"
 #include "components/site_isolation/pref_names.h"
 #include "components/site_isolation/preloaded_isolated_origins.h"
 #include "components/site_isolation/site_isolation_policy.h"
@@ -383,7 +382,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/switches.h"
 #include "third_party/blink/public/mojom/federated_learning/floc.mojom.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom.h"
-#include "third_party/blink/public/mojom/site_engagement/site_engagement.mojom.h"
 #include "third_party/blink/public/mojom/user_agent/user_agent_metadata.mojom.h"
 #include "third_party/blink/public/mojom/webpreferences/web_preferences.mojom.h"
 #include "third_party/blink/public/public_buildflags.h"
@@ -1229,40 +1227,6 @@ bool IsInHostedApp(WebContents* web_contents) {
 #else
   return false;
 #endif
-}
-
-void MaybeRecordSameSiteCookieEngagementHistogram(
-    content::RenderFrameHost* render_frame_host,
-    blink::mojom::WebFeature feature) {
-  if (feature != blink::mojom::WebFeature::kCookieNoSameSite &&
-      feature != blink::mojom::WebFeature::kCookieInsecureAndSameSiteNone) {
-    return;
-  }
-
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK(render_frame_host);
-
-  content::BrowserContext* browser_context =
-      WebContents::FromRenderFrameHost(render_frame_host)->GetBrowserContext();
-  Profile* profile = Profile::FromBrowserContext(browser_context);
-  auto* site_engagement_service =
-      site_engagement::SiteEngagementService::Get(profile);
-  if (!site_engagement_service)
-    return;
-
-  blink::mojom::EngagementLevel engagement_level =
-      site_engagement_service->GetEngagementLevel(
-          render_frame_host->GetLastCommittedURL());
-  if (feature == blink::mojom::WebFeature::kCookieNoSameSite) {
-    UMA_HISTOGRAM_ENUMERATION(
-        "Net.SameSiteBlockedCookieSiteEngagement.CookieNoSameSite",
-        engagement_level);
-  } else {
-    UMA_HISTOGRAM_ENUMERATION(
-        "Net.SameSiteBlockedCookieSiteEngagement."
-        "CookieInsecureAndSameSiteNone",
-        engagement_level);
-  }
 }
 
 bool IsErrorPageAutoReloadEnabled() {
@@ -5593,11 +5557,6 @@ void ChromeContentBrowserClient::LogWebFeatureForCurrentPage(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   page_load_metrics::MetricsWebContentsObserver::RecordFeatureUsage(
       render_frame_host, feature);
-
-  // For the SameSite-by-default-cookies related features, log
-  // the site engagement score for the site whose cookie was blocked. This is to
-  // gauge the user impact of the cookies being blocked.
-  MaybeRecordSameSiteCookieEngagementHistogram(render_frame_host, feature);
 }
 
 std::string ChromeContentBrowserClient::GetProduct() {
