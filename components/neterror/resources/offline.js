d15568a06b20f43ddd4f0c7b92c57252b528a276
Olivier Robin <olivierrobin@google.com>
2021-07-13 17:49:53 +0000

    Fix dino game layout on RTL 3x devices
    On 3x devices (notably iphones) the transformation puts
    the texts (score, gameover, retry arrow) out of the displayed
    zone.
    
    The purpose of this transformation is to keep the draw
    x coordinate constant between RTL and LTR.
    This draw coordinate is computed using the centerX.
    The affine transformation that keeps centerX constant and
    reverse X axis is:
    translate(2*centerX, 0)
    scale(-1,1)
    
    As centerX = this.canvasDimensions.WIDTH / 2, use
    translate(this.canvasDimensions.WIDTH, 0)
    
    Bug: 1227697
    Change-Id: Ia40348841754dc7678879a8499e25a4abc702ab2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024088
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Edward Jung (EMEA) <edwardjung@chromium.org>
    Commit-Queue: Edward Jung (EMEA) <edwardjung@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901086}

--- components/neterror/resources/offline.js
+++ components/neterror/resources/offline.js
@@ -1857,7 +1857,7 @@ GameOverPanel.prototype = {
     this.canvasCtx.save();
 
     if (IS_RTL) {
-      this.canvasCtx.translate(this.canvas.width / 2, 0);
+      this.canvasCtx.translate(this.canvasDimensions.WIDTH, 0);
       this.canvasCtx.scale(-1, 1);
     }
 
@@ -1917,7 +1917,7 @@ GameOverPanel.prototype = {
     this.canvasCtx.save();
 
     if (IS_RTL) {
-      this.canvasCtx.translate(this.canvas.width / 2, 0);
+      this.canvasCtx.translate(this.canvasDimensions.WIDTH, 0);
       this.canvasCtx.scale(-1, 1);
     }
 
@@ -2909,6 +2909,7 @@ function DistanceMeter(canvas, spritePos, canvasWidth) {
 
   this.config = DistanceMeter.config;
   this.maxScoreUnits = this.config.MAX_DISTANCE_UNITS;
+  this.canvasWidth = canvasWidth;
   this.init(canvasWidth);
 }
 
@@ -3016,12 +3017,12 @@ DistanceMeter.prototype = {
     if (IS_RTL) {
       if (opt_highScore) {
         this.canvasCtx.translate(
-            (this.canvas.width / 2) -
+            this.canvasWidth -
                 (DistanceMeter.dimensions.WIDTH * (this.maxScoreUnits + 3)),
             this.y);
       } else {
         this.canvasCtx.translate(
-            this.canvas.width / 2 - DistanceMeter.dimensions.WIDTH, this.y);
+            this.canvasWidth - DistanceMeter.dimensions.WIDTH, this.y);
       }
       this.canvasCtx.scale(-1, 1);
     } else {
