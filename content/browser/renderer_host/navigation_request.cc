b2ac4b6d3f884400c0730c5c5ce87916f5121362
Ali Beyad <abeyad@chromium.org>
2021-07-13 07:49:53 +0000

    Fix spelling mistake in client hints function name
    Navigation was spelled incorrectly in
    ParseAndPersistAcceptCHForNagivation, so this CL updates the function
    name and its callers.
    
    Change-Id: Ibeb8178e0e331e56911a7479f28becbb6f2f05c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021048
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900878}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -3694,7 +3694,7 @@ void NavigationRequest::OnRedirectChecksComplete(
       browser_context->GetClientHintsControllerDelegate();
   if (client_hints_delegate) {
     net::HttpRequestHeaders client_hints_extra_headers;
-    ParseAndPersistAcceptCHForNagivation(
+    ParseAndPersistAcceptCHForNavigation(
         commit_params_->redirects.back(),
         commit_params_->redirect_response.back()->parsed_headers,
         browser_context, client_hints_delegate, frame_tree_node_);
@@ -4089,7 +4089,7 @@ void NavigationRequest::CommitNavigation() {
     absl::optional<std::vector<network::mojom::WebClientHintsType>>
         opt_in_hints_from_response;
     if (response()) {
-      opt_in_hints_from_response = ParseAndPersistAcceptCHForNagivation(
+      opt_in_hints_from_response = ParseAndPersistAcceptCHForNavigation(
           common_params_->url, response()->parsed_headers, browser_context,
           client_hints_delegate, frame_tree_node_);
     }
