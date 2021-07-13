da9ee52dff465ae852706863f694d5cc5e28fb46
Ramin Halavati <rhalavati@chromium.org>
2021-07-13 12:23:06 +0000

    Remove |kGuestProfilesNumCreated| profile state.
    |kGuestProfilesNumCreated| profile state was added to keep track of the
    number of Ephemeral Guest profiles.
    Ephemeral Guest profiles are deprecated and removed and so this state is
    not needed anymore.
    
    Bug: 1225156
    Change-Id: I292913f5251d43ccc718ffea6d285532b591b744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015511
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900961}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -628,6 +628,7 @@ const char kNumberSignInPasswordPromoShown[] =
     "profile.number_sign_in_password_promo_shown";
 const char kSignInPasswordPromoRevive[] =
     "profile.sign_in_password_promo_revive";
+const char kGuestProfilesNumCreated[] = "profile.guest_profiles_created";
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 constexpr char kProfileSwitchInterceptionDeclinedPref[] =
     "signin.ProfileSwitchInterceptionDeclinedPref";
@@ -805,6 +806,7 @@ void RegisterProfilePrefsForMigration(
   registry->RegisterBooleanPref(kWasSignInPasswordPromoClicked, false);
   registry->RegisterIntegerPref(kNumberSignInPasswordPromoShown, 0);
   registry->RegisterBooleanPref(kSignInPasswordPromoRevive, false);
+  registry->RegisterIntegerPref(kGuestProfilesNumCreated, 1);
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   registry->RegisterDictionaryPref(kProfileSwitchInterceptionDeclinedPref);
@@ -1585,6 +1587,7 @@ void MigrateObsoleteProfilePrefs(Profile* profile) {
   profile_prefs->ClearPref(kWasSignInPasswordPromoClicked);
   profile_prefs->ClearPref(kNumberSignInPasswordPromoShown);
   profile_prefs->ClearPref(kSignInPasswordPromoRevive);
+  profile_prefs->ClearPref(kGuestProfilesNumCreated);
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   profile_prefs->ClearPref(kProfileSwitchInterceptionDeclinedPref);
 #endif
