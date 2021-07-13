c330953396a2aaa09165b92121381e23cc9cda40
Bruce Dawson <brucedawson@chromium.org>
2021-07-13 20:16:57 +0000

    Remove windows.h from some profiler headers
    Some of the sampling profiler header files end up included in many
    different translation units. These two both included windows.h -
    directly or indirectly - and now they don't. This reduces namespace
    pollution.
    
    Header files should (within reason) strive to include as little as
    possible to avoid a transitive include-tree explosion.
    
    Bug: 796644
    Change-Id: Ieab17254afe402c7b8b430295624c981d45bfeb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022537
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901155}

--- base/profiler/module_cache.h
+++ base/profiler/module_cache.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #if defined(OS_WIN)
-#include <windows.h>
+#include "base/win/windows_types.h"
 #endif
 
 namespace base {
