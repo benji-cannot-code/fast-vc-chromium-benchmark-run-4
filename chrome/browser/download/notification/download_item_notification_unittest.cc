6a13f23dabcb85adbe6a0a4332f205045561f40c
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-07-13 23:16:29 +0000

    Fix CrOS scanning missing filename
    Bug: 1228751
    Change-Id: I0d957a197f34a890ce936f6a9ca187f81dd6a0e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024409
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901226}

--- chrome/browser/download/notification/download_item_notification_unittest.cc
+++ chrome/browser/download/notification/download_item_notification_unittest.cc
@@ -146,6 +146,10 @@ class DownloadItemNotificationTest : public testing::Test {
         *download_item_notification_->notification_, /*metadata=*/nullptr);
   }
 
+  std::u16string GetStatusString() {
+    return download_item_notification_->GetStatusString();
+  }
+
   content::BrowserTaskEnvironment task_environment_;
 
   std::unique_ptr<TestingProfileManager> profile_manager_;
@@ -325,6 +329,7 @@ TEST_F(DownloadItemNotificationTest, DeepScanning) {
       )");
   EXPECT_CALL(*download_item_, OpenDownload()).Times(0);
   EXPECT_CALL(*download_item_, SetOpenWhenComplete(true)).Times(1);
+  EXPECT_EQ(u"TITLE.bin is being scanned.", GetStatusString());
   download_item_notification_->Click(absl::nullopt, absl::nullopt);
 
   // Can be opened while scanning.
@@ -338,6 +343,7 @@ TEST_F(DownloadItemNotificationTest, DeepScanning) {
         }
       )");
   EXPECT_CALL(*download_item_, OpenDownload()).Times(1);
+  EXPECT_EQ(u"TITLE.bin is being scanned.", GetStatusString());
   download_item_notification_->Click(absl::nullopt, absl::nullopt);
 
   // Scanning finished, warning.
