f25d1cb2996dfa4e4f666ccd168d6db42716bc3c
Francois Doray <fdoray@chromium.org>
2021-07-13 20:33:05 +0000

    [blink] Remove unused function in EventTarget.
    Finishes the cleanup started in https://crrev.com/c/2960288.
    
    Change-Id: Iaa0753f68e302c9881615084c22e2f4f9d8d25d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024004
    Auto-Submit: François Doray <fdoray@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901176}

--- third_party/blink/renderer/core/dom/events/event_target.cc
+++ third_party/blink/renderer/core/dom/events/event_target.cc
@@ -210,12 +210,6 @@ void CountFiringEventListeners(const Event& event,
   }
 }
 
-void RegisterWithScheduler(ExecutionContext* execution_context,
-                           const AtomicString& event_type) {
-  if (!execution_context || !execution_context->GetScheduler())
-    return;
-}
-
 }  // namespace
 
 EventTargetData::EventTargetData() = default;
@@ -536,8 +530,6 @@ void EventTarget::AddedEventListener(
       }
     }
   }
-
-  RegisterWithScheduler(GetExecutionContext(), event_type);
 
   if (event_util::IsDOMMutationEventType(event_type)) {
     if (ExecutionContext* context = GetExecutionContext()) {
