11a05193a688b3a24af79994e9fc13176f7c95b9
David Baron <dbaron@chromium.org>
2021-07-13 02:16:30 +0000

    Revert "Moving more sync related logic to the PasswordStoreImpl"
    This reverts commit c45aa2aeb157aa7e07d6dff25e0f1eea6fa41c83.
    
    Reason for revert: failing ChromeBrowsingDataRemoverDelegateTest.RemovePasswordsByTimeOnly in unit_tests on Win7.  See log:
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%281%29/116807
    
    Original change's description:
    > Moving more sync related logic to the PasswordStoreImpl
    >
    > This CL implements RemoveLoginsByURLAndTimeAsync and moves some more
    > sync related logic to the PasswordStoreImpl.
    >
    > Bug: 1226042
    > Change-Id: Ida93c9d952a14fca1f1660daa14974890e3d428c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3006377
    > Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#900513}
    
    Bug: 1226042
    Change-Id: I59ed5b2e3ad4f988afe4e0dae012f61d6a99a3b4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021605
    Auto-Submit: David Baron <dbaron@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900786}

--- components/password_manager/core/browser/test_password_store.h
+++ components/password_manager/core/browser/test_password_store.h
@@ -97,13 +97,6 @@ class TestPasswordStore : public PasswordStore, public PasswordStoreBackend {
                         const PasswordForm& form) override;
   void RemoveLoginAsync(OptionalStoreChangeListReply callback,
                         const PasswordForm& form) override;
-  void RemoveLoginsByURLAndTimeAsync(
-      OptionalStoreChangeListReply callback,
-      const base::RepeatingCallback<bool(const GURL&)>& url_filter,
-      base::Time delete_begin,
-      base::Time delete_end,
-      base::OnceClosure completion,
-      base::OnceCallback<void(bool)> sync_completion) override;
   // PasswordStore interface
   PasswordStoreChangeList AddLoginImpl(const PasswordForm& form,
                                        AddLoginError* error) override;
@@ -150,9 +143,7 @@ class TestPasswordStore : public PasswordStore, public PasswordStoreBackend {
   std::vector<FieldInfo> GetAllFieldInfoImpl() override;
   void RemoveFieldInfoByTimeImpl(base::Time remove_begin,
                                  base::Time remove_end) override;
-  void SetUnsyncedCredentialsDeletionNotifier(
-      std::unique_ptr<UnsyncedCredentialsDeletionNotifier> deletion_notifier)
-      override;
+
   // PasswordStoreSync interface.
   // TODO(crbug.bom/1226042): Remove this after PasswordStore no longer
   // inherits PasswordStoreSync.
@@ -166,9 +157,6 @@ class TestPasswordStore : public PasswordStore, public PasswordStoreBackend {
       const PasswordForm& form,
       base::span<const InsecureCredential> credentials) override;
   PasswordStoreChangeList RemoveLoginSync(const PasswordForm& form) override;
-  void NotifyDeletionsHaveSynced(bool success) override;
-  void NotifyUnsyncedCredentialsWillBeDeleted(
-      std::vector<PasswordForm> unsynced_credentials) override;
   bool BeginTransaction() override;
   void RollbackTransaction() override;
   bool CommitTransaction() override;
