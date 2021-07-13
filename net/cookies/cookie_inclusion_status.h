9531e062e9dabab0e8f0bd025b737db64d0f19ce
Ken Rockot <rockot@google.com>
2021-07-13 07:00:28 +0000

    net: Add missing <ostream> include
    Recently Chromium has failed to build in some standard Windows dev
    environments. This has been tracked down to r900181 and is a result of a
    missing <ostream> include in net/cookies/cookie_inclusion_status.h.
    
    This change adds the missing include where it belongs.
    
    Bug: None
    Change-Id: I96da68a8be8aa1135d4952d691bce4b7aef41f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022535
    Commit-Queue: Mike West <mkwst@chromium.org>
    Auto-Submit: Ken Rockot <rockot@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900866}

--- net/cookies/cookie_inclusion_status.h
+++ net/cookies/cookie_inclusion_status.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_COOKIES_COOKIE_INCLUSION_STATUS_H_
 #define NET_COOKIES_COOKIE_INCLUSION_STATUS_H_
 
+#include <ostream>
 #include <string>
 #include <vector>
 
