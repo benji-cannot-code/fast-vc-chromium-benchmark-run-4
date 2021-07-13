f919aa4240b4bbfe8c54db030e60d9c1c3bb93d9
James Cook <jamescook@chromium.org>
2021-07-13 17:33:12 +0000

    chromeos: Remove deprecated DBusThreadManager::ClientSet
    For the mustash project we used to initialize separate sets of dbus
    clients in the browser process versus the "ash" process. This enum
    kept track of which set to initialize.
    
    Mustash was cancelled in 2019 and this enum has no meaning any more.
    
    Just cleanup, no behavior changes.
    
    Bug: none
    Change-Id: I9c694154c6c0e2478be9bd393c02f6bb2b865f57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004177
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901078}

--- chrome/browser/ash/dbus/ash_dbus_helper.cc
+++ chrome/browser/ash/dbus/ash_dbus_helper.cc
@@ -90,7 +90,7 @@ void InitializeDBus() {
   chromeos::SystemSaltGetter::Initialize();
 
   // Initialize DBusThreadManager for the browser.
-  chromeos::DBusThreadManager::Initialize(DBusThreadManager::kAll);
+  chromeos::DBusThreadManager::Initialize();
 
   // Initialize Chrome dbus clients.
   dbus::Bus* bus = chromeos::DBusThreadManager::Get()->GetSystemBus();
