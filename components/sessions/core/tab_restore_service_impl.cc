6a0f4b4ebc3cea9b5ff593b23495da9cf8cca521
Bruce Dawson <brucedawson@chromium.org>
2021-07-13 01:19:20 +0000

    Changes to components to allow removing windows.h
    Before we can remove includes of windows.h from header files we need to
    _add_ includes of windows.h to .cc files that require it. This change
    does this for source files in components.
    
    The undef of LoadBitmap is needed in some contexts due to windows.h
    namespace pollution - LoadBitmap is defined to LoadBitmapW which alters
    Chrome member functions of that name.
    
    Bug: 796644
    Change-Id: I5f654d96dbd0ddc90a013e97b44ab97d3dbb32e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017101
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#900767}

--- components/sessions/core/tab_restore_service_impl.cc
+++ components/sessions/core/tab_restore_service_impl.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
 
+#undef LoadBitmap
+
 namespace sessions {
 
 namespace {
