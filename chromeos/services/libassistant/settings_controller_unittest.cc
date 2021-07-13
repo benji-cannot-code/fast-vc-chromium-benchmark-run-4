9aa0b8ae11662d5849d379f8d39932ea2b98a274
Yunke Zhou <yunkez@google.com>
2021-07-13 11:39:37 +0000

    Reland "Reland "Assistant: use equal weight buttons based on settings value""
    This is a reland of da5f7720e69fe1b3c783c5853a1378d0a6859ffa
    
    The previous CL breaks linux-chromeos-chrome because I missed to update
    chromeos/services/libassistant/settings_controller_unittest.cc with
    the updated parameters. I've fixed it in this CL.
    
    Original change's description:
    > Reland "Assistant: use equal weight buttons based on settings value"
    >
    > This is a reland of b5cd3736c679b6205763b71ec351829c9d77f7ff
    >
    > Original change's description:
    > > Assistant: use equal weight buttons based on settings value
    > >
    > > 1. Add an extra parameter `include_header` in mojom method
    > > `GetSettings`. When `include_header` is true, returns a seralized
    > > proto of |GetSettingsUiResponse|; otherwise, returns |SettingsUi|.
    > > 2. Read the newly-added value `footer_button_layout` from header and
    > > use it to determine whether to use equal weight buttons on Assistant
    > > UIs (both OOBE and in-session).
    > >
    > > Bug: 1224850
    > > Change-Id: I81c255914a3a8aa31f36325044b11d9272132668
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015335
    > > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > > Reviewed-by: Yue Li <updowndota@chromium.org>
    > > Reviewed-by: Tao Wu <wutao@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Reviewed-by: Sam McNally <sammc@chromium.org>
    > > Commit-Queue: Yunke Zhou <yunkez@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#900414}
    >
    > Bug: 1224850
    > Change-Id: I7069870fa815406e90474558707517e723d0bfc8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3020766
    > Reviewed-by: Yue Li <updowndota@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Reviewed-by: Tao Wu <wutao@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Commit-Queue: Yunke Zhou <yunkez@google.com>
    > Cr-Commit-Position: refs/heads/master@{#900827}
    
    Bug: 1224850
    Change-Id: Ib3905e078b76c156870744b52f148cbfd90e5577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021206
    Reviewed-by: Satoru Takabayashi <satorux@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/master@{#900950}

--- chromeos/services/libassistant/settings_controller_unittest.cc
+++ chromeos/services/libassistant/settings_controller_unittest.cc
@@ -350,7 +350,7 @@ TEST_F(AssistantSettingsControllerTest,
 
   EXPECT_CALL(callback, Run(std::string{}));
 
-  controller().GetSettings("selector", callback.Get());
+  controller().GetSettings("selector", /*include_header=*/false, callback.Get());
 }
 
 TEST_F(AssistantSettingsControllerTest,
@@ -358,7 +358,7 @@ TEST_F(AssistantSettingsControllerTest,
   CreateAndStartLibassistant();
 
   base::MockCallback<SettingsController::GetSettingsCallback> callback;
-  controller().GetSettings("selector", callback.Get());
+  controller().GetSettings("selector", /*include_header=*/false, callback.Get());
 
   EXPECT_CALL(callback, Run(std::string{}));
   DestroyLibassistant();
