e37f69b6bd9e2e62da4c6887b5bccd03ef42b0a3
Jason Chase <chasej@chromium.org>
2021-07-13 23:36:59 +0000

    Add regression tests for origin trials enabled after window.open
    There were no tests to ensure that trials are enabled in a document that
    was navigated via window.open.
    
    This CL adds tests for that scenario, using a new test-only attribute
    added to the Window interface. This can be extended in the future with
    a test-only attribute on other globals (i.e. ServiceWorker).
    
    Also adds tests specifically for storageFoundation, which is the trial
    where the problem was discovered.
    
    Bug: 1206219
    Change-Id: If533a259f62f73a5d89802ca7c542b2368cc234e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2910277
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: Rodney Ding <rodneyding@google.com>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901257}

---
+++ third_party/blink/renderer/core/testing/origin_trials_test_global.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_TESTING_ORIGIN_TRIALS_TEST_GLOBAL_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_TESTING_ORIGIN_TRIALS_TEST_GLOBAL_H_
+
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+
+namespace blink {
+
+class LocalDOMWindow;
+
+// This is used for testing that the Window interface can be extended with
+// origin trial-enabled IDL members at run-time.
+class OriginTrialsTestGlobal final {
+  STATIC_ONLY(OriginTrialsTestGlobal);
+
+ public:
+  static bool testOriginTrialGlobalAttribute(LocalDOMWindow&) { return true; }
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_TESTING_ORIGIN_TRIALS_TEST_GLOBAL_H_
--- third_party/blink/web_tests/http/tests/resources/origin-trials-helper.js
+++ third_party/blink/web_tests/http/tests/resources/origin-trials-helper.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file provides an OriginTrialsHelper object which can be used by
-// LayoutTests that are checking members exposed to script by origin trials.
+// WebTests that are checking members exposed to script by origin trials.
 //
 // The current available methods are:
 // check_properties_exist:
