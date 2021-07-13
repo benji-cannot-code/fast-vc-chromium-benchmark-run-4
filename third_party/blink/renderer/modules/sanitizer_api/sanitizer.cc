afa3a62f6b3a0964973347c5ad6dc3fb27cb94f5
Daniel Vogelheim <vogelheim@chromium.org>
2021-07-13 13:19:07 +0000

    [Sanitizer API] Remove .sanitizeToString.
    Remove .sanitizeToString and string inputs to .sanitize.
    Remove tests for .sanitizeToString.
    Adapt tests, that sometimes used .sanitizeToString as a generic
    sanitizer method.
    
    Bug: 1221153
    Change-Id: I33723ffe9e5a7cd8dd45b655efe31e7436d0eff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988762
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900979}

--- third_party/blink/renderer/modules/sanitizer_api/sanitizer.cc
+++ third_party/blink/renderer/modules/sanitizer_api/sanitizer.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_node_filter.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_parse_from_string_options.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_union_document_documentfragment_string.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_union_document_documentfragment.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_sanitizer_config.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
@@ -108,17 +108,28 @@ Sanitizer* Sanitizer::Create(ExecutionContext* execution_context,
   return MakeGarbageCollected<Sanitizer>(execution_context, config);
 }
 
-String Sanitizer::sanitizeToString(ScriptState* script_state,
-                                   const V8SanitizerInput* input,
-                                   ExceptionState& exception_state) {
-  return CreateMarkup(SanitizeImpl(script_state, input, exception_state),
-                      kChildrenOnly);
-}
-
 DocumentFragment* Sanitizer::sanitize(ScriptState* script_state,
                                       const V8SanitizerInput* input,
                                       ExceptionState& exception_state) {
-  return SanitizeImpl(script_state, input, exception_state);
+  V8SanitizerInput* new_input = nullptr;
+  switch (input->GetContentType()) {
+    case V8SanitizerInput::ContentType::kDocument:
+      new_input =
+          MakeGarbageCollected<V8SanitizerInput>(input->GetAsDocument());
+      break;
+    case V8SanitizerInput::ContentType::kDocumentFragment:
+      new_input = MakeGarbageCollected<V8SanitizerInput>(
+          input->GetAsDocumentFragment());
+      break;
+  }
+  LocalDOMWindow* window = LocalDOMWindow::From(script_state);
+  DocumentFragment* fragment =
+      PrepareFragment(window, script_state, new_input, exception_state);
+  if (exception_state.HadException()) {
+    return nullptr;
+  }
+  DoSanitizing(fragment, window, exception_state);
+  return fragment;
 }
 
 Element* Sanitizer::sanitizeFor(ScriptState* script_state,
@@ -184,21 +195,6 @@ DocumentFragment* Sanitizer::PrepareFragment(LocalDOMWindow* window,
       UseCounter::Count(window->GetExecutionContext(),
                         WebFeature::kSanitizerAPIFromFragment);
       return input->GetAsDocumentFragment();
-    case V8SanitizerInput::ContentType::kString: {
-      UseCounter::Count(window->GetExecutionContext(),
-                        WebFeature::kSanitizerAPIFromString);
-      Document* document =
-          window->document()
-              ? window->document()->implementation().createHTMLDocument()
-              : DOMParser::Create(script_state)
-                    ->parseFromString(
-                        "<!DOCTYPE html><html><body></body></html>",
-                        "text/html", ParseFromStringOptions::Create());
-      // TODO(https://crbug.com/1178774): Behavior difference need further
-      // investgate.
-      return document->createRange()->createContextualFragment(
-          input->GetAsString(), exception_state);
-    }
   }
   NOTREACHED();
   return nullptr;
@@ -289,19 +285,6 @@ void Sanitizer::DoSanitizing(ContainerNode* fragment,
         break;
     }
   }
-}
-
-DocumentFragment* Sanitizer::SanitizeImpl(ScriptState* script_state,
-                                          const V8SanitizerInput* input,
-                                          ExceptionState& exception_state) {
-  LocalDOMWindow* window = LocalDOMWindow::From(script_state);
-  DocumentFragment* fragment =
-      PrepareFragment(window, script_state, input, exception_state);
-  if (exception_state.HadException()) {
-    return nullptr;
-  }
-  DoSanitizing(fragment, window, exception_state);
-  return fragment;
 }
 
 // If the current element needs to be dropped, remove current element entirely
