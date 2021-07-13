c15de57463cc96c31f60a1c12d34678cad54fda2
Alice Gong <alicego@google.com>
2021-07-13 01:00:49 +0000

    Update download history page & CrOS notification "Show in folder" link text.
    Update "Show in folder/Finder" to "Show in Box" for file rerouted.
    
    In generated_resources.grd:
    ~ Update IDS_DOWNLOAD_LINK_SHOW to take an argument, which is either
    folder/Finder (depending on platform), or display name for a web drive.
    
    In FileSystemRenameHandler:
    + Add ctor for completed download item.
    
    In DownloadUIModel:
    + Add GetShowInFolderText() to compute show in folder text.
    
    In DownloadsListTracker::CreateDownloadData():
    + Add the formluated string to mojom data.
    
    The string formulated is passed all the way into web ui page as: DownloadUIModel
     -> DownloadsListTracker file_value -> downloads.mojom -> item.ts -> item.html.
    
    Fixed: 1206299
    Change-Id: I76cebaddd181ece15dff804ceec9b4af8313d11f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2998663
    Commit-Queue: Alice Gong <alicego@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900756}

--- chrome/browser/resources/downloads/item.ts
+++ chrome/browser/resources/downloads/item.ts
@@ -60,6 +60,13 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
         value: true,
       },
 
+      hasShowInFolderLink_: {
+        computed: 'computeHasShowInFolderLink_(' +
+            'data.state, data.fileExternallyRemoved)',
+        type: Boolean,
+        value: true,
+      },
+
       controlledBy_: {
         computed: 'computeControlledBy_(data.byExtId, data.byExtName)',
         type: String,
@@ -209,6 +216,11 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
   private computeCompletelyOnDisk_(): boolean {
     return this.data.state === States.COMPLETE &&
         !this.data.fileExternallyRemoved;
+  }
+
+  private computeHasShowInFolderLink_(): boolean {
+    return loadTimeData.getBoolean('hasShowInFolder') &&
+        this.computeCompletelyOnDisk_();
   }
 
   private computeControlledBy_(): string {
