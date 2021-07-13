b855dde44c22c7fa917bb32a3978763f80e8ee8a
Sky Malice <skym@chromium.org>
2021-07-13 02:37:26 +0000

    Log instead of throw on DisableAnimationsTestRule exception.
    Currently the constructor of DisableAnimationsTestRule is erroring out
    on all Android S devices. Because rules are created before filtering is
    done on tests, this effectively fails all chrome_public_test_apk runs on
    Android S devices.
    
    The majority of test cases that use this rule do not actually need
    animations to be disabled to run correctly, just a handful will fail.
    While this change isn't sufficient to enable an Android S try bot, it
    should reduce the pain of trying to run tests locally for developers
    that use Android S devices for testing until a long term solution is
    found.
    
    Bug: 1225707
    Change-Id: I196df9d346d7b7bc7c812ee257a9e02860af96f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021535
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#900794}

--- ui/android/javatests/src/org/chromium/ui/test/util/DisableAnimationsTestRule.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/DisableAnimationsTestRule.java
@@ -12,6 +12,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+import org.chromium.base.BuildInfo;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 
@@ -24,7 +25,7 @@ import java.util.Arrays;
 
 /**
  * {@link TestRule} to disable animations for UI testing, or enable animation with new
- * DisableAnimationsTestRule(true).
+ * DisableAnimationsTestRule(true). Does not work on Android S, see https://crbug.com/1225707.
  */
 public class DisableAnimationsTestRule implements TestRule {
     /**
@@ -68,7 +69,14 @@ public class DisableAnimationsTestRule implements TestRule {
             IBinder windowManagerBinder = (IBinder) getService.invoke(null, "window");
             mWindowManagerObject = asInterface.invoke(null, windowManagerBinder);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to access animation methods", e);
+            // TODO(https://crbug.com/1225707): Always throw once this works on Android S. The above
+            // API is no longer accessible and will crash regardless of filter rules so just warn
+            // instead.
+            if (BuildInfo.isAtLeastS()) {
+                Log.w(TAG, "Failed to access animation methods", e);
+            } else {
+                throw new RuntimeException("Failed to access animation methods", e);
+            }
         }
     }
 
