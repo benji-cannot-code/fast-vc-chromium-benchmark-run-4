c36870a9b919ddfe94a8d096e5e9303a5614605a
Sunny Sachanandani <sunnyps@chromium.org>
2021-07-14 05:23:17 +0000

    gpu: Remove DComp FramesSinceColorSpaceChange UMA histogram
    Bug: 1205075
    Change-Id: I7349bd27b533f94cd3133150c8695ea597e9afe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022315
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901368}

--- ui/gl/swap_chain_presenter.cc
+++ ui/gl/swap_chain_presenter.cc
@@ -853,14 +853,6 @@ bool SwapChainPresenter::PresentToDecodeSwapChain(
   }
 
   swap_chain_size_ = swap_chain_size;
-  if (swap_chain_format_ == DXGI_FORMAT_NV12) {
-    frames_since_color_space_change_++;
-  } else {
-    UMA_HISTOGRAM_COUNTS_1000(
-        "GPU.DirectComposition.FramesSinceColorSpaceChange",
-        frames_since_color_space_change_);
-    frames_since_color_space_change_ = 0;
-  }
   swap_chain_format_ = DXGI_FORMAT_NV12;
   RecordPresentationStatistics();
   return true;
@@ -1106,7 +1098,6 @@ bool SwapChainPresenter::PresentToSwapChain(ui::DCRendererLayerParams& params) {
     return false;
   }
   last_presented_images_ = params.images;
-  frames_since_color_space_change_++;
   RecordPresentationStatistics();
   return true;
 }
@@ -1357,15 +1348,7 @@ bool SwapChainPresenter::ReallocateSwapChain(
 
   DCHECK(!swap_chain_size.IsEmpty());
   swap_chain_size_ = swap_chain_size;
-
   protected_video_type_ = protected_video_type;
-
-  if (swap_chain_format_ != swap_chain_format) {
-    UMA_HISTOGRAM_COUNTS_1000(
-        "GPU.DirectComposition.FramesSinceColorSpaceChange",
-        frames_since_color_space_change_);
-    frames_since_color_space_change_ = 0;
-  }
 
   ReleaseSwapChainResources();
 
