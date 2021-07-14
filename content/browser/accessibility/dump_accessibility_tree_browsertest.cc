b90bef016bc9c45baba6c35704003f71649c546f
Aaron Leventhal <aleventhal@google.com>
2021-07-14 23:23:21 +0000

    Tests for ARIA radio buttons inside shadow root
    Bug: 1158144
    Change-Id: Ibe387fba2abfca42a9569a7b6c61848839138679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027641
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#901694}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -1177,6 +1177,11 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityAriaRadiogroup) {
   RunAriaTest(FILE_PATH_LITERAL("aria-radiogroup.html"));
 }
 
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
+                       AccessibilityAriaRadioInShadowRoot) {
+  RunAriaTest(FILE_PATH_LITERAL("aria-radio-in-shadow-root.html"));
+}
+
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityAriaReadonly) {
   RunAriaTest(FILE_PATH_LITERAL("aria-readonly.html"));
 }
