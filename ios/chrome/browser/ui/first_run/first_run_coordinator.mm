79096b2ea6ea998adfe1bf1837936d0e624aa672
Tina Wang <tinazwang@google.com>
2021-07-14 17:28:16 +0000

    🐻[Pandora] Don't show sync error message as it is deprecated
    Previously, no matter if the user click on "Turn on Sync" or
    "Don't Sync" on sync screen, the sync error message will always
    shown(asking for sync password if have one). It shouldn't show up
    if the user doesn't have a intent to sync. Also, the sync engine
    won't start if the user haven't tap on "Turn on Sync", so the error
    message should be shown in this case.
    
    The original change add checks if the user tap on "Turn on Sync"
    to decide whether to show the sync error message. However, since the
    Sync error message is deprecated, we just remove it. So, after the
    fix, the sync error message won't be shown.
    
    More context:
    MICE is deprecating the sync error message(crbug/1151287).
    https://docs.google.com/presentation/d/1xqVrulp3OZw-jZuELcNOvgin4gd0svG0XWxfkqgfaMg/edit?resourcekey=0-TzNOA87KdxM48AR2wdeoYg#slide=id.gbb0b810cb2_0_145
    
    Bug: 1225489
    Change-Id: Ic153a18d553e3feedab0c9967e6327f2a415f81d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021697
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901533}

--- ios/chrome/browser/ui/first_run/first_run_coordinator.mm
+++ ios/chrome/browser/ui/first_run/first_run_coordinator.mm
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/first_run/signin/signin_screen_coordinator.h"
 #import "ios/chrome/browser/ui/first_run/sync/sync_screen_coordinator.h"
 #import "ios/chrome/browser/ui/first_run/welcome/welcome_screen_coordinator.h"
-#import "ios/chrome/browser/ui/settings/sync/utils/sync_util.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -77,11 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   void (^completion)(void) = ^{
     base::UmaHistogramEnumeration("FirstRun.Stage", first_run::kComplete);
     WriteFirstRunSentinel();
-    // Display the sync errors infobar.
-    web::WebState* webState =
-        self.browser->GetWebStateList()->GetActiveWebState();
-    DisplaySyncErrors(self.browser->GetBrowserState(), webState,
-                      self.presenter);
 
     // If the remaining screens have been skipped, additional actions will be
     // executed.
