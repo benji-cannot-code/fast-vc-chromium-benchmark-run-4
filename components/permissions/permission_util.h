63aa18ba58f06da94c3299573c11dccb1ad49016
Illia Klimov <elklm@google.com>
2021-07-14 20:36:34 +0000

    Get permission status based on RenderFrameHost only.
    This CL focuses on the introduction of a new API in PermissionManager.
    
    Bug: 698985
    Change-Id: I45bdae73497244d53b84d259d49b23ef026b3549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009622
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Illia Klimov <elklm@google.com>
    Cr-Commit-Position: refs/heads/master@{#901609}

--- components/permissions/permission_util.h
+++ components/permissions/permission_util.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 enum class PermissionType;
 class WebContents;
+class RenderFrameHost;
 }  // namespace content
 
 class GURL;
@@ -73,6 +74,8 @@ class PermissionUtil {
   // ultimately all call sites should be migrated to determine the authoritative
   // security origin based on the requesting RenderFrameHost.
   static GURL GetLastCommittedOriginAsURL(content::WebContents* web_contents);
+  static GURL GetLastCommittedOriginAsURL(
+      content::RenderFrameHost* render_frame_host);
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(PermissionUtil);
