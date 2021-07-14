b70b7ca6922d0e1250a2e7b7cdbd5f1444c1f62b
Xiyuan Xia <xiyuan@chromium.org>
2021-07-14 22:31:55 +0000

    Revert "[CrOS Device Name] Fix initialization issue with |DeviceNameStore|"
    This reverts commit 1f0c93ed7bac78769d7806e4152e09c6aaaf0ba3.
    
    Reason for revert:
    Failed on linux-chromeos-chrome.
    
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/15864/overview
    
    [ RUN      ] DeviceNameStoreTest.GetInstanceBeforeInitializeError
    ../../chrome/browser/chromeos/device_name_store_unittest.cc:54: Failure
    Death test: DeviceNameStore::GetInstance()
        Result: died but not with expected error.
      Expected: contains regular expression "Check failed: g_instance"
    Actual msg:
    [  DEATH   ] WARNING unit_tests[11219:11219]: [test_suite.cc(390)] Test launcher output path /b/s/w/itg210x7i8/.com.google.Chrome.CIB0rG/gpBG9Y/test_results.xml exists. Not adding test launcher result printer.
    [  DEATH   ] 
    Stack trace:
    #0 0x55ea26fb5f83 chromeos::DeviceNameStoreTest_GetInstanceBeforeInitializeError_Test::TestBody()
    
    [  FAILED  ] DeviceNameStoreTest.GetInstanceBeforeInitializeError (468 ms)
    [15931/21576] DeviceNameStoreTest.GetInstanceBeforeInitializeError (468 ms)
    [ RUN      ] DeviceNameStoreTest.EnableHostnameSettingFlagOff
    ../../chrome/browser/chromeos/device_name_store_unittest.cc:62: Failure
    Death test: InitializeDeviceNameStore( false)
        Result: died but not with expected error.
      Expected: contains regular expression "Check failed: base::FeatureList::IsEnabled"
    Actual msg:
    [  DEATH   ] WARNING unit_tests[11401:11401]: [test_suite.cc(390)] Test launcher output path /b/s/w/itg210x7i8/.com.google.Chrome.CIB0rG/gpBG9Y/test_results.xml exists. Not adding test launcher result printer.
    [  DEATH   ] 
    Stack trace:
    #0 0x55ea26fb6148 chromeos::DeviceNameStoreTest_EnableHostnameSettingFlagOff_Test::TestBody()
    
    [  FAILED  ] DeviceNameStoreTest.EnableHostnameSettingFlagOff (457 ms)
    
    
    Original change's description:
    > [CrOS Device Name] Fix initialization issue with |DeviceNameStore|
    >
    > GetInstance() could initially return a non-null val before Initialize()
    > was called. This CL fixes this and also makes sure that Initialize()
    > crashes if kEnableHostnameSetting flag is off since |DeviceNameStore|
    > should not have been called in the first place in that case.
    >
    > Change-Id: I0c736c137e341b2ced1668ca70b162e2bf69d4a7
    > Bug: 126802
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022590
    > Commit-Queue: Jason Ah Chuen <ahchuen@google.com>
    > Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#901592}
    
    Bug: 126802, 1229387
    Change-Id: I38639dc8a9f8b5a5aa4a188b48693c7fa3d5fe33
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3028805
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Owners-Override: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901672}

--- chrome/browser/chromeos/device_name_store.cc
+++ chrome/browser/chromeos/device_name_store.cc
@@ -5,29 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/device_name_store.h"
 
-#include "ash/constants/ash_features.h"
+#include <math.h>
+
+#include "base/rand_util.h"
+#include "base/strings/char_traits.h"
+#include "base/strings/string_number_conversions.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_thread.h"
 
 namespace chromeos {
+
 namespace {
-
 const char kDefaultDeviceName[] = "ChromeOS";
-
-// This will point to the singleton instance upon initialization.
-DeviceNameStore* g_instance = nullptr;
-
 }  // namespace
-
-DeviceNameStore::~DeviceNameStore() = default;
 
 // static
 DeviceNameStore* DeviceNameStore::GetInstance() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  CHECK(g_instance);
-  return g_instance;
+  return base::Singleton<DeviceNameStore>::get();
 }
 
 // static
@@ -36,26 +33,13 @@ void DeviceNameStore::RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterStringPref(prefs::kDeviceName, "");
 }
 
-// static
 void DeviceNameStore::Initialize(PrefService* prefs) {
-  CHECK(base::FeatureList::IsEnabled(features::kEnableHostnameSetting));
-  CHECK(!g_instance);
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  g_instance = new DeviceNameStore(prefs);
-}
+  DCHECK(prefs);
+  prefs_ = prefs;
 
-// static
-void DeviceNameStore::Shutdown() {
-  if (g_instance) {
-    delete g_instance;
-    g_instance = nullptr;
-  }
-}
-
-DeviceNameStore::DeviceNameStore(PrefService* prefs) : prefs_(prefs) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK(prefs_);
-  if (prefs_->GetString(prefs::kDeviceName).empty()) {
+  const std::string device_name = prefs_->GetString(prefs::kDeviceName);
+  if (device_name.empty()) {
     prefs_->SetString(prefs::kDeviceName, kDefaultDeviceName);
   }
 }
