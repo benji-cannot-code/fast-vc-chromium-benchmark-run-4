4cd9366cdc7004f9faf2e6606f641536bbf12ff4
SebSG <sebsg@chromium.org>
2021-07-14 15:17:18 +0000

    [Stylize] Add metrics about share destination (1st or 3rd party).
    Bug: 1217761
    Change-Id: Ifba5e6a7f9963376b43c0d5c12bb0bbe4e6bee4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021042
    Commit-Queue: sebsg <sebsg@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#901502}

--- chrome/browser/content_creation/notes/internal/android/java/src/org/chromium/chrome/browser/content_creation/notes/NoteCreationCoordinatorImpl.java
+++ chrome/browser/content_creation/notes/internal/android/java/src/org/chromium/chrome/browser/content_creation/notes/NoteCreationCoordinatorImpl.java
@@ -142,7 +142,8 @@ public class NoteCreationCoordinatorImpl implements NoteCreationCoordinator, Top
                                         @Override
                                         public void onTargetChosen(ComponentName chosenComponent) {
                                             NoteCreationMetrics.recordNoteShared(
-                                                    getTimeElapsedSinceCreationStart());
+                                                    getTimeElapsedSinceCreationStart(),
+                                                    chosenComponent);
                                         }
 
                                         @Override
--- chrome/browser/content_creation/notes/internal/android/java/src/org/chromium/chrome/browser/content_creation/notes/NoteCreationMetrics.java
+++ chrome/browser/content_creation/notes/internal/android/java/src/org/chromium/chrome/browser/content_creation/notes/NoteCreationMetrics.java
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.content_creation.notes;
 
+import android.content.ComponentName;
+
 import androidx.annotation.IntDef;
 
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.chrome.browser.share.share_sheet.ChromeProvidedSharingOptionsProvider;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -43,6 +46,15 @@ public final class NoteCreationMetrics {
         int BOLD = 9;
         int DREAMY = 10;
         int NUM_ENTRIES = 11;
+    }
+
+    // Constants used to log the share destination for the created note.
+    // Constants used to log the Note Creation Funnel enum histogram.
+    @IntDef({NoteShareDestination.FIRST_PARTY, NoteShareDestination.THIRD_PARTY})
+    private @interface NoteShareDestination {
+        int FIRST_PARTY = 0;
+        int THIRD_PARTY = 1;
+        int NUM_ENTRIES = 2;
     }
 
     /**
@@ -86,14 +98,24 @@ public final class NoteCreationMetrics {
      *
      * @param duration The time elapsed between the start of the creation flow and when the user
      *         shared their created note.
+     * @param chosenComponent The component that was picked as a share desitination.
      */
-    public static void recordNoteShared(long duration) {
+    public static void recordNoteShared(long duration, ComponentName chosenComponent) {
         RecordHistogram.recordEnumeratedHistogram("NoteCreation.Funnel",
                 NoteCreationFunnel.NOTE_SHARED, NoteCreationFunnel.NUM_ENTRIES);
 
         RecordHistogram.recordBooleanHistogram("NoteCreation.NoteShared", true);
 
         RecordHistogram.recordMediumTimesHistogram("NoteCreation.TimeTo.ShareCreation", duration);
+
+        if (chosenComponent.equals(
+                    ChromeProvidedSharingOptionsProvider.CHROME_PROVIDED_FEATURE_COMPONENT_NAME)) {
+            RecordHistogram.recordEnumeratedHistogram("NoteCreation.ShareDestination",
+                    NoteShareDestination.FIRST_PARTY, NoteShareDestination.NUM_ENTRIES);
+        } else {
+            RecordHistogram.recordEnumeratedHistogram("NoteCreation.ShareDestination",
+                    NoteShareDestination.THIRD_PARTY, NoteShareDestination.NUM_ENTRIES);
+        }
     }
 
     /**
