7208c77540361847816b20a875a68f29b8ca5472
Ryan Lester <rlester@google.com>
2021-07-14 21:58:34 +0000

    Add a Network Settings Hotline service.
    This service will be used by Meet Devices to open a custom network
    settings dialog, allowing them to fix the network without reboot the
    device or logging out of Kiosk mode.
    
    Bug: b:35772861
    Cq-Include-Trybots: luci.chromium.try:linux-cfm-rel
    Change-Id: I3ca1bcc4ec11f383902ece85ac4c187d0689d0c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2997863
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ryan Lester <rlester@google.com>
    Cr-Commit-Position: refs/heads/master@{#901657}

--- chrome/browser/chromeos/chromebox_for_meetings/cfm_chrome_services.cc
+++ chrome/browser/chromeos/chromebox_for_meetings/cfm_chrome_services.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/chromebox_for_meetings/device_info/device_info_service.h"
 #include "chrome/browser/chromeos/chromebox_for_meetings/diagnostics/diagnostics_service.h"
 #include "chrome/browser/chromeos/chromebox_for_meetings/logger/cfm_logger_service.h"
+#include "chrome/browser/chromeos/chromebox_for_meetings/network_settings/network_settings_service.h"
 #include "chromeos/components/chromebox_for_meetings/features/features.h"
 #include "chromeos/dbus/chromebox_for_meetings/cfm_hotline_client.h"
 
@@ -25,6 +26,7 @@ void InitializeCfmServices() {
   CfmLoggerService::Initialize();
   DeviceInfoService::Initialize();
   DiagnosticsService::Initialize();
+  NetworkSettingsService::Initialize();
 }
 
 void ShutdownCfmServices() {
@@ -33,6 +35,7 @@ void ShutdownCfmServices() {
     return;
   }
 
+  NetworkSettingsService::Shutdown();
   DiagnosticsService::Shutdown();
   DeviceInfoService::Shutdown();
   CfmLoggerService::Shutdown();
