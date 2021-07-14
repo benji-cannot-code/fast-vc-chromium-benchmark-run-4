9c092ce16ee6ed75566dafd32a4b1e73be4a5492
Ankit Kumar 🌪️ <ankk@microsoft.com>
2021-07-14 06:57:23 +0000

    [unseasoned-pdf] Replace usage of PP_PrivateAccessibilityDocInfo
    PP_PrivateAccessibilityDocInfo is a Pepper struct. The replacement
    ensures that PP_PrivateAccessibilityDocInfo is scoped to parts
    specific to Pepper implementation.
    
    Bug: 1144444
    Change-Id: Ibf424f7c5cfccefb03477b550812adfc187fefd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3019903
    Auto-Submit: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901393}

--- components/pdf/renderer/pdf_accessibility_tree.cc
+++ components/pdf/renderer/pdf_accessibility_tree.cc
@@ -1298,7 +1298,7 @@ void PdfAccessibilityTree::SetAccessibilityViewportInfo(
 }
 
 void PdfAccessibilityTree::SetAccessibilityDocInfo(
-    const PP_PrivateAccessibilityDocInfo& doc_info) {
+    const chrome_pdf::AccessibilityDocInfo& doc_info) {
   content::RenderAccessibility* render_accessibility =
       GetRenderAccessibilityIfEnabled();
   if (!render_accessibility)
