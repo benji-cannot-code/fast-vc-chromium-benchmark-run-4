78d20806a4e5614c8083c18543b67f9b3453ff9e
Ian Kilpatrick <ikilpatrick@chromium.org>
2021-07-14 01:39:01 +0000

    [cleanup] Rename Is{Fixed,Initial}BlockSizeIndefinite
    As above.
    
    There shouldn't be any behaviour change. This only "interesting" change
    is in ng_length_utils.cc AdjustChildPercentageSize.
    
    This code was present before we introduced the concept of an initial
    block-size, however this shouldn't be needed, as the initial
    block-size should already be indefinite.
    
    Change-Id: Ifbd781d653095dd4708f42976f6f1c865da190dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2971196
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901306}

--- third_party/blink/renderer/core/layout/layout_box_model_object.cc
+++ third_party/blink/renderer/core/layout/layout_box_model_object.cc
@@ -794,7 +794,7 @@ bool LayoutBoxModelObject::HasAutoHeightOrContainingBlockWithAutoHeight(
       // BoxLayoutExtraInput that we could add a flag to.
       const NGConstraintSpace& space =
           this_box->GetCachedLayoutResult()->GetConstraintSpaceForCaching();
-      if (space.IsFixedBlockSize() && !space.IsFixedBlockSizeIndefinite())
+      if (space.IsFixedBlockSize() && !space.IsInitialBlockSizeIndefinite())
         return false;
     }
   }
