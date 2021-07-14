c3a20b3b850aa1e54652f3a0a1fa88cc3f16d6b1
Ian Wells <iwells@chromium.org>
2021-07-14 20:50:24 +0000

    Feed launch reliability logging: make Chrome version and channel info available for logging
    Bug: 1195940
    Change-Id: Idd6cf93f6f462cc6562f2822592e734c3706cd4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021491
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901615}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedProcessScopeDependencyProvider.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedProcessScopeDependencyProvider.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.browser.version.ChromeVersionInfo;
 import org.chromium.chrome.browser.xsurface.ImageFetchClient;
 import org.chromium.chrome.browser.xsurface.PersistentKeyValueCache;
 import org.chromium.chrome.browser.xsurface.ProcessScopeDependencyProvider;
+import org.chromium.components.version_info.VersionConstants;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 
 /**
@@ -126,5 +127,15 @@ public class FeedProcessScopeDependencyProvider implements ProcessScopeDependenc
     @Override
     public String getGoogleApiKey() {
         return GoogleAPIKeys.GOOGLE_API_KEY;
+    }
+
+    @Override
+    public String getChromeVersion() {
+        return VersionConstants.PRODUCT_VERSION;
+    }
+
+    @Override
+    public int getChromeChannel() {
+        return VersionConstants.CHANNEL;
     }
 }
