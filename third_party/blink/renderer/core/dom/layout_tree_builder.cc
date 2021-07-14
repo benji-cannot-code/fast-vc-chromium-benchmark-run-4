acdc8f75ecd89ff7b2cedd2d8ce847856d4b374f
Yoshifumi Inoue <yosin@chromium.org>
2021-07-14 05:46:10 +0000

    Revert "Merge combined texts in |LayoutObject::WillBeRemovedFromTree()| if needed"
    This reverts commit 555f0c2e6ecb359074987f2e3309867daec96fbd.
    
    Reason for revert: This CL causes lots of crashes...
    
    Original change's description:
    > Merge combined texts in |LayoutObject::WillBeRemovedFromTree()| if needed
    >
    > This patch changes |LayoutObject::WillBeRemovedFromTree()| to merge adjacent
    > |LayoutNGTextCombine| objects after removal, e.g.
    >   Before removal of <b>
    >    LayoutNGTextCombine
    >      LayoutText "a"
    >    LayoutInline B
    >      LayoutText "X"
    >    LayoutNGTextCombine
    >      LayoutText "z"
    >   After removal of <b>
    >    LayoutNGTextCombine
    >      LayoutText "a"
    >      LayoutText "z"
    >
    > This patch also changes |LayoutTreeBuilderForText::CreateLayoutObject()|
    > to insert new |LayoutText| into |LayoutNGTextCombine| in right place.
    > Before this change, we insert new |LayoutText| before first child.
    >
    > Bug: 1227066
    > Change-Id: I734a3c5633268404f605277ef0ec80f1cdfe242c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3019910
    > Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    > Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Koji Ishii <kojii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#900905}
    
    Bug: 1227066, 1228731, 1227066, 1228837, 1228840, 1228999, 1229001, 1229004, 1229031, 1229032, 1229056
    Change-Id: I7e1798876619724ca7d81dd80051d1f62bfb2e05
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3023814
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901380}

--- third_party/blink/renderer/core/dom/layout_tree_builder.cc
+++ third_party/blink/renderer/core/dom/layout_tree_builder.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
-#include "third_party/blink/renderer/core/layout/ng/inline/layout_ng_text_combine.h"
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/core/svg_names.h"
 
@@ -148,11 +147,6 @@ void LayoutTreeBuilderForText::CreateLayoutObject() {
   if (LayoutObject* wrapper = CreateInlineWrapperForDisplayContentsIfNeeded()) {
     layout_object_parent = wrapper;
     next_layout_object = nullptr;
-  } else if (next_layout_object) {
-    auto* const text_combine =
-        DynamicTo<LayoutNGTextCombine>(next_layout_object->Parent());
-    if (UNLIKELY(text_combine))
-      layout_object_parent = text_combine;
   }
 
   LegacyLayout legacy_layout = layout_object_parent->ForceLegacyLayout()
