5e9916d6b2956dc58a0349b58bd77696162717ca
Jimmy Gong <jimmyxgong@google.com>
2021-07-14 02:06:33 +0000

    shortcuts: Add AcceleratorView
    AcceleratorView currently only displays a read-only view of a
    shortcut. Currently uses a stub Object type, follow-up CL's will
    replace it with the actual mojom type.
    
    Screenshot: https://screenshot.googleplex.com/4obrjRQv4xKEB5Z
    
    Bug: 1060690
    Test: browser_test
    Change-Id: I0a24b8ecf832840b5e45b3bf5b78279d4fff3351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022588
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901316}

--- ash/webui/shortcut_customization_ui/resources/accessibility_shortcuts_page.js
+++ ash/webui/shortcut_customization_ui/resources/accessibility_shortcuts_page.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './shortcut_input.js';
+import './accelerator_view.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
--- chrome/test/data/webui/chromeos/shortcut_customization/shortcut_customization_unified_test.js
+++ chrome/test/data/webui/chromeos/shortcut_customization/shortcut_customization_unified_test.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
 
+import {acceleratorViewTest} from './accelerator_view_test.js';
 import {shortcutCustomizationAppTest} from './shortcut_customization_test.js';
 
 window.test_suites_list = [];
@@ -15,3 +16,4 @@ function runSuite(suiteName, testFn) {
 }
 
 runSuite('ShortcutCustomizationApp', shortcutCustomizationAppTest);
+runSuite('AcceleratorViewTest', acceleratorViewTest);
