c1733a64c276bcad9b86bc766549a92b48687b0d
Peter Kotwicz <pkotwicz@chromium.org>
2021-07-14 03:17:58 +0000

    [Refactor] Remove uses of @RemovableInRelease for runtime asserts
    This CL replaces cases where @RemovableInRelease is used to guard
    debug-only asserts with
    if (BuildConfig.ENABLE_ASSERTS) {}
    
    We are deprecating the @RemovableInRelease.
    
    BUG=1180332
    
    Change-Id: Idb34b66ae50d47f759e815ab0518fc7ca9c72be6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021102
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901341}

--- ui/android/java/src/org/chromium/ui/modelutil/PropertyModel.java
+++ ui/android/java/src/org/chromium/ui/modelutil/PropertyModel.java
@@ -15,7 +15,7 @@ import androidx.annotation.StringRes;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.util.ObjectsCompat;
 
-import org.chromium.base.annotations.RemovableInRelease;
+import org.chromium.build.BuildConfig;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -274,9 +274,8 @@ public class PropertyModel extends PropertyObservable<PropertyKey> {
         mData = startingValues;
     }
 
-    @RemovableInRelease
     private void validateKey(PropertyKey key) {
-        if (!mData.containsKey(key)) {
+        if (BuildConfig.ENABLE_ASSERTS && !mData.containsKey(key)) {
             throw new IllegalArgumentException(
                     "Invalid key passed in: " + key + ". Current data is: " + mData.toString());
         }
@@ -466,9 +465,8 @@ public class PropertyModel extends PropertyObservable<PropertyKey> {
             mData = values;
         }
 
-        @RemovableInRelease
         private void validateKey(PropertyKey key) {
-            if (!mData.containsKey(key)) {
+            if (BuildConfig.ENABLE_ASSERTS && !mData.containsKey(key)) {
                 throw new IllegalArgumentException("Invalid key passed in: " + key);
             }
         }
