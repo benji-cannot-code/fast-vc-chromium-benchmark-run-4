312cb3f6b851ebe3e4d47f4df2faf5e6eee7dd83
Caitlin Fischer <caitlinfischer@google.com>
2021-07-14 16:05:29 +0000

    Add integration tests for variations safe mode on iOS.
    Bug: b/184937096
    Change-Id: I7b624ceaf40bef15635b3e0ac9d5169744e31d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3020141
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#901510}

--- components/variations/variations_test_utils.h
+++ components/variations/variations_test_utils.h
@@ -15,12 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace variations {
 
-// The below seed and signature pair were generated using the server's private
-// key.
+// The test seed data is associated with a VariationsSeed with one study,
+// "UMA-Uniformity-Trial-10-Percent", and ten equally weighted groups: "default"
+// and "group_01" through "group_09". The study is not associated with channels,
+// platforms, or features.
 //
-// The seed contains one study, "UMA-Uniformity-Trial-10-Percent", and ten
-// equally weighted groups: "default" and "group_01" through "group_09".
+// The seed and signature pair were generated using the server's private key.
 extern const char kUncompressedBase64TestSeedData[];
+extern const char kCompressedBase64TestSeedData[];
 extern const char kBase64TestSeedSignature[];
 extern const char kTestSeedStudyName[];
 
