3bdf9e4e2664e2eed715d64ff4638460a0f042f8
Mike Dougherty <michaeldo@chromium.org>
2021-07-14 16:20:18 +0000

    Remove IOS.Dialogs.JavaScriptDialogClosed
    This metric has already expired and is no longer used.
    
    Fixed: 1021167
    Change-Id: Id5bd68aa36a098f03c544c9f3a6e1c42afde7ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3025025
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901514}

--- ios/chrome/browser/ui/dialogs/overlay_java_script_dialog_presenter.mm
+++ ios/chrome/browser/ui/dialogs/overlay_java_script_dialog_presenter.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/overlays/public/overlay_response.h"
 #import "ios/chrome/browser/overlays/public/web_content_area/java_script_dialog_overlay.h"
 #import "ios/chrome/browser/ui/dialogs/java_script_dialog_blocking_state.h"
-#include "ios/chrome/browser/ui/dialogs/java_script_dialog_metrics.h"
 #import "ios/web/public/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -42,10 +41,6 @@ void HandleJavaScriptDialogResponse(web::DialogClosedCallback callback,
   if (!dialog_response) {
     // A null response is used if the dialog was not closed by user interaction.
     // This occurs either for navigation or because of WebState closures.
-    IOSJavaScriptDialogDismissalCause cause =
-        web_state ? IOSJavaScriptDialogDismissalCause::kNavigation
-                  : IOSJavaScriptDialogDismissalCause::kClosure;
-    RecordDialogDismissalCause(cause);
     std::move(callback).Run(/*success=*/false, /*user_input=*/nil);
     return;
   }
@@ -57,7 +52,6 @@ void HandleJavaScriptDialogResponse(web::DialogClosedCallback callback,
     blocking_state->JavaScriptDialogBlockingOptionSelected();
   }
 
-  RecordDialogDismissalCause(IOSJavaScriptDialogDismissalCause::kUser);
   bool confirmed = action == JavaScriptDialogResponse::Action::kConfirm;
   NSString* user_input = confirmed ? dialog_response->user_input() : nil;
   std::move(callback).Run(confirmed, user_input);
@@ -84,7 +78,6 @@ void OverlayJavaScriptDialogPresenter::RunJavaScriptDialog(
   JavaScriptDialogBlockingState::CreateForWebState(web_state);
   if (JavaScriptDialogBlockingState::FromWebState(web_state)->blocked()) {
     // Block the dialog if needed.
-    RecordDialogDismissalCause(IOSJavaScriptDialogDismissalCause::kBlocked);
     std::move(callback).Run(NO, nil);
     return;
   }
