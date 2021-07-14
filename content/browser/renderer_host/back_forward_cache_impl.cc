33599bdf5967c91c25885312d2e0ace31ba51d65
Yuzu Saijo <yuzus@chromium.org>
2021-07-14 11:19:41 +0000

    [bfcache] Restore cache-control:no-store if cookies do not change
    This CL adds a flag to restore pages with cache-control:no-store from
    back/forward cache, if cookies are not modified.
    
    Bug: 1228611
    Change-Id: Ib446c99469d3d3761264054f0e80ec6a884dbca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3023925
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901449}

--- content/browser/renderer_host/back_forward_cache_impl.cc
+++ content/browser/renderer_host/back_forward_cache_impl.cc
@@ -650,7 +650,7 @@ BackForwardCacheImpl::CanPotentiallyStorePageLater(RenderFrameHostImpl* rfh) {
   if (!Intersection(rfh->scheduler_tracked_features(),
                     cache_control_no_store_feature)
            .Empty()) {
-    if (!ShouldCacheControlNoStoreEnterBackForwardCache()) {
+    if (!AllowStoringPagesWithCacheControlNoStore()) {
       // Block pages with cache-control: no-store only when
       // |should_cache_control_no_store_enter| flag is false. If true, put the
       // page in and evict later.
@@ -886,6 +886,9 @@ void BackForwardCacheImpl::MaybeEvictDueToCacheControlNoStoreBeforeRestore(
         BackForwardCacheMetrics::NotRestoredReason::
             kCacheControlNoStoreCookieModified);
   } else {
+    // Do not evict if the flag is on when cookies do not change.
+    if (AllowRestoringPagesWithCacheControlNoStore())
+      return;
     entry->render_frame_host->EvictFromBackForwardCacheWithReason(
         BackForwardCacheMetrics::NotRestoredReason::kCacheControlNoStore);
   }
@@ -1019,7 +1022,7 @@ BackForwardCacheImpl::Entry* BackForwardCacheImpl::GetEntry(
   if (matching_entry == entries_.end())
     return nullptr;
 
-  if (ShouldCacheControlNoStoreEnterBackForwardCache() &&
+  if (AllowStoringPagesWithCacheControlNoStore() &&
       (*matching_entry)
           ->render_frame_host->scheduler_tracked_features()
           .Has(WebSchedulerTrackedFeature::kMainResourceHasCacheControlNoStore))
@@ -1151,12 +1154,21 @@ void BackForwardCacheImpl::WillCommitNavigationToCachedEntry(
   }
 }
 
-bool BackForwardCacheImpl::ShouldCacheControlNoStoreEnterBackForwardCache() {
+bool BackForwardCacheImpl::AllowStoringPagesWithCacheControlNoStore() {
   if (!IsBackForwardCacheEnabled())
     return false;
 
   return base::FeatureList::IsEnabled(
       features::kCacheControlNoStoreEnterBackForwardCache);
+}
+
+bool BackForwardCacheImpl::AllowRestoringPagesWithCacheControlNoStore() {
+  if (!IsBackForwardCacheEnabled() ||
+      !AllowStoringPagesWithCacheControlNoStore())
+    return false;
+
+  return base::FeatureList::IsEnabled(
+      kCacheControlNoStoreRestoreFromBackForwardCacheUnlessCookieChange);
 }
 
 bool BackForwardCache::DisabledReason::operator<(
