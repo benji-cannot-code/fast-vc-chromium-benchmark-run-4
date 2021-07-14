79c3e1e0562b809f9f38c425e68dc81053335c81
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-07-14 19:59:31 +0000

    Revert "Remove some unnecessary #includes."
    This reverts commit 91aebec2c73303351fdb8847f0884344dfc94c1f.
    
    Reason for revert: Closed tree (https://ci.chromium.org/ui/p/chromium/builders/ci/Win%20Builder/118924/overview)
    
    Original change's description:
    > Remove some unnecessary #includes.
    >
    > According to
    > https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    > these were responsible for 1,460,113,428 bytes of input to the compiler,
    > or roughly 0.58% of the input used to build Chrome.
    >
    > Bug: 242216
    > Change-Id: I154d2ec2ddbb50a26b94d9caf378cccfbe1d4769
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3025890
    > Reviewed-by: danakj <danakj@chromium.org>
    > Owners-Override: danakj <danakj@chromium.org>
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#901569}
    
    Bug: 242216
    Change-Id: Iea760a5eb05e167c0393c72148372db4b766f1e7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3028565
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@google.com>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    Cr-Commit-Position: refs/heads/master@{#901579}

--- cc/paint/paint_canvas.h
+++ cc/paint/paint_canvas.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_export.h"
 #include "cc/paint/paint_image.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-
-class SkTextBlob;
+#include "third_party/skia/include/core/SkTextBlob.h"
 
 namespace printing {
 class MetafileSkia;
--- chrome/chrome_cleaner/parsers/parser_utils/parse_tasks_remaining_counter.cc
+++ chrome/chrome_cleaner/parsers/parser_utils/parse_tasks_remaining_counter.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/chrome_cleaner/parsers/parser_utils/parse_tasks_remaining_counter.h"
 
-#include <ostream>
-
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/waitable_event.h"
 
