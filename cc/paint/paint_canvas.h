2f4c16ce50562cef4fc6a42e31eb9ac00efa95ce
Peter Kasting <pkasting@chromium.org>
2021-07-14 19:21:39 +0000

    Remove some unnecessary #includes.
    According to
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    these were responsible for 1,460,113,428 bytes of input to the compiler,
    or roughly 0.58% of the input used to build Chrome.
    
    Bug: 242216
    Change-Id: I154d2ec2ddbb50a26b94d9caf378cccfbe1d4769
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3025890
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901569}

--- cc/paint/paint_canvas.h
+++ cc/paint/paint_canvas.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_export.h"
 #include "cc/paint/paint_image.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "third_party/skia/include/core/SkTextBlob.h"
+
+class SkTextBlob;
 
 namespace printing {
 class MetafileSkia;
