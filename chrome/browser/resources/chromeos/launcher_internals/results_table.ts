c013f0b30a36515601e41be3e82122350a7378f4
Rachel Wong <wrong@chromium.org>
2021-07-15 01:52:41 +0000

    [internals] Implement expand-on-click for row results.
    Clicking on a result row will toggle its "selected" state. Selected rows
    are expanded and this state persists across sorting.
    
    This aims to allow all information to be inspected while still
    maintaining table readability and column width.
    
    Bug: 1211232
    Change-Id: I176b63b4da44ba1291d2903477ce17c6cad04811
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3023712
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901763}

--- chrome/browser/resources/chromeos/launcher_internals/results_table.ts
+++ chrome/browser/resources/chromeos/launcher_internals/results_table.ts
@@ -35,6 +35,10 @@ export class LauncherResultsTableElement extends PolymerElement {
   // this will change whenever the user clicks on a new header to sort by.
   private sortKey: string = 'Score';
 
+  // The IDs of results that are currently selected. This is used to persist
+  // formatting when the table is sorted.
+  private selectedIds: Set<string> = new Set();
+
   connectedCallback() {
     super.connectedCallback();
     this.$.scoreHeader.addEventListener(
@@ -97,6 +101,10 @@ export class LauncherResultsTableElement extends PolymerElement {
     this.$.resultsSection.innerHTML = '';
     for (const result of sortedResults) {
       const newRow = this.$.resultsSection.insertRow();
+      newRow.addEventListener('click', (e: Event) => this.toggleRowSelected(e));
+      if (this.selectedIds.has(result.id)) {
+        newRow.classList.add('selected');
+      }
       [result.id,
        result.title,
        result.description,
@@ -120,6 +128,23 @@ export class LauncherResultsTableElement extends PolymerElement {
       outputScores.push(score === undefined ? '' : score.toString());
     }
     return outputScores;
+  }
+
+  // Toggles selection in the class list of the targeted row.
+  private toggleRowSelected(event: Event) {
+    const row = (event.target as HTMLElement).closest('tr');
+    if (row == null || row.cells.length === 0) {
+      return;
+    }
+
+    const id = row.cells[0]!.textContent!;
+    if (row.classList.contains('selected')) {
+      row.classList.remove('selected');
+      this.selectedIds.delete(id);
+    } else {
+      row.classList.add('selected');
+      this.selectedIds.add(id);
+    }
   }
 }
 
