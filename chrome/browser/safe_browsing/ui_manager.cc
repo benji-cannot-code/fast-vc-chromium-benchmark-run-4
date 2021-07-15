cff2d4fc9297cbe9bf81936bae89a53ec3d67d0d
Colin Blundell <blundell@chromium.org>
2021-07-15 15:45:05 +0000

    [Safe Browsing] Remove unused includes from //chrome's ui_manager.cc
    These //chrome-level includes are not used. Eliminating them clarifies
    the blockers to componentizing this code for sharing with WebLayer.
    
    Bug: 1226567
    Change-Id: I94b01f2e31ef623c69f31a32bf69e9d72a676938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021093
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901971}

--- chrome/browser/safe_browsing/ui_manager.cc
+++ chrome/browser/safe_browsing/ui_manager.cc
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/safe_browsing/safe_browsing_subresource_tab_helper.h"
-#include "chrome/browser/tab_contents/tab_util.h"
-#include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "components/no_state_prefetch/browser/no_state_prefetch_contents.h"
 #include "components/prefs/pref_service.h"
