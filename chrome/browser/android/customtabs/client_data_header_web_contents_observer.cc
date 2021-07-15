23b14c0ef1a6f5c56c7ba33dab7e99bc95fef973
Dave Tapuska <dtapuska@chromium.org>
2021-07-15 14:49:48 +0000

    Iterate sending the CCT header to all frames including prerenders.
    Change GetAllFrames to ForEachRenderFrameHost. Add a test that checks
    CCT headers are set for prerenders and that they also adapt to changes
    in the headers.
    
    BUG=1208438
    
    Change-Id: Ice7e607c25184886985317d7e9aa71149e360727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3028562
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901950}

--- chrome/browser/android/customtabs/client_data_header_web_contents_observer.cc
+++ chrome/browser/android/customtabs/client_data_header_web_contents_observer.cc
@@ -20,9 +20,9 @@ ClientDataHeaderWebContentsObserver::ClientDataHeaderWebContentsObserver(
 
 void ClientDataHeaderWebContentsObserver::SetHeader(const std::string& header) {
   header_ = header;
-  auto frames = web_contents()->GetAllFrames();
-  for (auto* frame : frames)
-    UpdateFrameCCTHeader(frame);
+  web_contents()->ForEachRenderFrameHost(base::BindRepeating(
+      &ClientDataHeaderWebContentsObserver::UpdateFrameCCTHeader,
+      base::Unretained(this)));
 }
 
 void ClientDataHeaderWebContentsObserver::RenderFrameCreated(
@@ -32,6 +32,8 @@ void ClientDataHeaderWebContentsObserver::RenderFrameCreated(
 
 void ClientDataHeaderWebContentsObserver::UpdateFrameCCTHeader(
     content::RenderFrameHost* render_frame_host) {
+  if (!render_frame_host->IsRenderFrameLive())
+    return;
   mojo::AssociatedRemote<chrome::mojom::ChromeRenderFrame> client;
   render_frame_host->GetRemoteAssociatedInterfaces()->GetInterface(&client);
   client->SetCCTClientHeader(header_);
