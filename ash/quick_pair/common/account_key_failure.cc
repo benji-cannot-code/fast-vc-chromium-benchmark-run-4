26ff9252f2d9155471a320a978ac6085f37c5cc0
Shane Fitzpatrick <shanefitz@google.com>
2021-07-15 21:06:28 +0000

    [FastPair] PairerBroker and FastPairPairer classes
    This change adds the entry point class for the pairing component -
    PairerBroker, and the Fast Pair implementation.
    
    It also adds an AccountKeyFailure enum to represent those failures.
    These are separate from the PairFailure instance because they are not
    terminal.
    
    Change-Id: I8e5c860f843300974666f2e3d9d7f662cae656c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024920
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902143}

---
+++ ash/quick_pair/common/account_key_failure.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/quick_pair/common/account_key_failure.h"
+
+namespace ash {
+namespace quick_pair {
+
+std::ostream& operator<<(std::ostream& stream, AccountKeyFailure failure) {
+  switch (failure) {
+    case AccountKeyFailure::kAccountKeyCharacteristicDiscovery:
+      stream << "[Failed to find the Account Key GATT characteristic]";
+      break;
+
+    case AccountKeyFailure::kKeyBasedPairingCharacteristicWrite:
+      stream << "[Failed to write to the Account Key GATT characteristic]";
+      break;
+  }
+
+  return stream;
+}
+
+}  // namespace quick_pair
+}  // namespace ash
