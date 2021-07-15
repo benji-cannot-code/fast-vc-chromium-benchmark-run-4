d0fcde2b690cfc5f2ef166459bd7ef3206ce681f
Yoshifumi Inoue <yosin@chromium.org>
2021-07-15 09:10:12 +0000

    Change ContainerNode::RemoveChild() to merge adjacent combined texts
    This patch changes |ContainerNode::RemoveChild()| to request to
    merge adjacent |LayoutNGTextCombine| objects as result of removal by
    forcing reattach of containing node.
    
    |LayoutNGTextCombine| objects after removal, e.g.
      Before removal of <b>
       LayoutNGTextCombine
         LayoutText "a"
       LayoutInline B
         LayoutText "X"
       LayoutNGTextCombine
         LayoutText "z"
    
      After removal of <b>
       LayoutNGTextCombine
         LayoutText "a"
         LayoutText "z"
    
    This is another approach of the reverted CL[1], which does merging at
    removal of layout object.
    
    [1] http://crrev.com/c/3019910 Merge combined texts in
    |LayoutObject::WillBeRemovedFromTree()| if needed
    
    
    Bug: 1227066
    
    Change-Id: Ic2e33dd2658c13ecf181bdfe5553a07cf938b9c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026389
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901891}

--- third_party/blink/renderer/core/dom/container_node.cc
+++ third_party/blink/renderer/core/dom/container_node.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/core/layout/line/inline_text_box.h"
 #include "third_party/blink/renderer/core/layout/line/root_inline_box.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/layout_ng_text_combine.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/runtime_call_stats.h"
@@ -717,6 +718,14 @@ Node* ContainerNode::RemoveChild(Node* old_child,
         "child of this node. Perhaps it was moved "
         "in response to a mutation?");
     return nullptr;
+  }
+
+  if (auto* layout_object = child->GetLayoutObject()) {
+    // Request to merge previous and next |LayoutNGTextCombine| of |child|.
+    // See http:://crbug.com/1227066
+    if (UNLIKELY(IsA<LayoutNGTextCombine>(layout_object->PreviousSibling())) &&
+        UNLIKELY(IsA<LayoutNGTextCombine>(layout_object->NextSibling())))
+      SetForceReattachLayoutTree();
   }
 
   {
