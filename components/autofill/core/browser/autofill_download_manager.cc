f6676a046239fb82c2e594e12fac25dd39a6029d
Matt Menke <mmenke@chromium.org>
2021-07-15 20:37:23 +0000

    Add includes to components/autofill and components/password_manager
    This was split off of
    https://chromium-review.googlesource.com/c/chromium/src/+/3025728,
    which fixes over a hundred IWYU issues expecting SimpleURLLoader to
    bring in url_response_head.mojom.h, which I'm trying to remove.
    
    Bug: 1227131
    Change-Id: I4e91546e60f36752013d4dbb0b97598d0d65cba3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031685
    Reviewed-by: Vadym Doroshenko  <dvadym@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902116}

--- components/autofill/core/browser/autofill_download_manager.cc
+++ components/autofill/core/browser/autofill_download_manager.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 #include "url/gurl.h"
 
 namespace autofill {
--- components/password_manager/core/browser/well_known_change_password_state_unittest.cc
+++ components/password_manager/core/browser/well_known_change_password_state_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/test/test_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "services/network/test/test_utils.h"
