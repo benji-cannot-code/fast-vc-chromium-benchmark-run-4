d518c244bb2c7cb79f10b5b43a0fa3fcd2521fda
Matt Menke <mmenke@chromium.org>
2021-07-15 01:44:11 +0000

    Add missing includes in extension and geolocation code.
    This was split off of
    https://chromium-review.googlesource.com/c/chromium/src/+/3025728,
    which fixes over a hundred IWYU issues expecting SimpleURLLoader to
    bring in url_response_head.mojom.h, which I'm trying to remove.
    
    Bug: 1227131
    Change-Id: I30c3ed3c757c991f65ccf7881d73d14f6dab370d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027345
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901760}

--- chrome/browser/extensions/updater/extension_updater_unittest.cc
+++ chrome/browser/extensions/updater/extension_updater_unittest.cc
@@ -84,6 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_headers.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "services/network/test/test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
--- chrome/browser/extensions/webstore_install_helper.cc
+++ chrome/browser/extensions/webstore_install_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/referrer_policy.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 
 using content::BrowserThread;
 
