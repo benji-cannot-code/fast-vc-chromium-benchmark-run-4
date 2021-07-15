2c2af4989412ba81147c9a5ae3f8524ad30786f0
jlebel <jlebel@chromium.org>
2021-07-15 16:55:23 +0000

    [iOS] Better logs for DCHECK(!self.signinCoordinator) in SceneController
    Patch to complete (to get all logs consistent):
    crrev.com/c/3024293
    
    Change-Id: Ice44e5827aaec5983e0592bed8fbaaa83d1a4d3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031941
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902008}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -1430,8 +1430,10 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
   // This dispatch is necessary to give enough time for the tools menu to
   // disappear before taking a screenshot.
   dispatch_async(dispatch_get_main_queue(), ^{
-    DCHECK(!self.signinCoordinator) << base::SysNSStringToUTF8(
-        NSStringFromClass(self.signinCoordinator.class));
+    DCHECK(!self.signinCoordinator)
+        << "self.signinCoordinator class: "
+        << base::SysNSStringToUTF8(
+               NSStringFromClass(self.signinCoordinator.class));
     if (self.settingsNavigationController)
       return;
     Browser* browser = self.mainInterface.browser;
@@ -2326,8 +2328,10 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
   ProceduralBlock completionWithBVC = ^{
     DCHECK(self.currentInterface.viewController);
     DCHECK(!self.mainCoordinator.isTabGridActive);
-    DCHECK(!self.signinCoordinator) << base::SysNSStringToUTF8(
-        NSStringFromClass(self.signinCoordinator.class));
+    DCHECK(!self.signinCoordinator)
+        << "self.signinCoordinator class: "
+        << base::SysNSStringToUTF8(
+               NSStringFromClass(self.signinCoordinator.class));
     // This will dismiss the SSO view controller.
     [self.interfaceProvider.currentInterface
         clearPresentedStateWithCompletion:completion
@@ -2337,8 +2341,10 @@ const char kMultiWindowOpenInNewWindowHistogram[] =
     // |self.currentInterface.bvc| may exist but tab switcher should be
     // active.
     DCHECK(self.mainCoordinator.isTabGridActive);
-    DCHECK(!self.signinCoordinator) << base::SysNSStringToUTF8(
-        NSStringFromClass(self.signinCoordinator.class));
+    DCHECK(!self.signinCoordinator)
+        << "self.signinCoordinator class: "
+        << base::SysNSStringToUTF8(
+               NSStringFromClass(self.signinCoordinator.class));
     // History coordinator can be started on top of the tab grid.
     // This is not true of the other tab switchers.
     DCHECK(self.mainCoordinator);
