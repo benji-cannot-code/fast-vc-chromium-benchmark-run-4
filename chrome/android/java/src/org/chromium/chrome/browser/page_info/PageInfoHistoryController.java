d87743a0b4d26033bba6d2294f04113fd7d82fae
Ehimare Okoyomon <eokoyomon@chromium.org>
2021-07-15 06:34:47 +0000

    [Android] Add history summary string references
    Bug: 1173154
    Change-Id: Id409899c0967167cf0b72b50988e70b51bb99530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026797
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Side Yilmaz <sideyilmaz@google.com>
    Cr-Commit-Position: refs/heads/master@{#901843}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoHistoryController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoHistoryController.java
@@ -4,17 +4,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.page_info;
 
+import android.content.res.Resources;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.history.HistoryContentManager;
 import org.chromium.chrome.browser.history.HistoryItem;
+import org.chromium.components.browser_ui.util.date.StringUtils;
 import org.chromium.components.page_info.PageInfoAction;
 import org.chromium.components.page_info.PageInfoControllerDelegate;
 import org.chromium.components.page_info.PageInfoMainController;
 import org.chromium.components.page_info.PageInfoRowView;
 import org.chromium.components.page_info.PageInfoSubpageController;
+
+import java.util.Date;
 
 /**
  * Class for controlling the page info history section.
@@ -27,6 +32,7 @@ public class PageInfoHistoryController
     private final String mTitle;
     private final String mHost;
     private HistoryContentManager mContentManager;
+    private long mLastVisitedTimestamp;
 
     public PageInfoHistoryController(PageInfoMainController mainController, PageInfoRowView rowView,
             PageInfoControllerDelegate delegate, String host) {
@@ -73,7 +79,8 @@ public class PageInfoHistoryController
     private void setupHistoryRow() {
         PageInfoRowView.ViewParams rowParams = new PageInfoRowView.ViewParams();
         rowParams.title = getRowTitle();
-        rowParams.visible = mDelegate.isSiteSettingsAvailable() && !mDelegate.isIncognito();
+        rowParams.visible = rowParams.title != null && mDelegate.isSiteSettingsAvailable()
+                && !mDelegate.isIncognito();
         rowParams.iconResId = R.drawable.ic_history_googblue_24dp;
         rowParams.clickCallback = this::launchSubpage;
 
@@ -81,8 +88,24 @@ public class PageInfoHistoryController
     }
 
     private String getRowTitle() {
-        // TODO(crbug.com/1173154): This should return a string about how long since the last visit.
-        return mTitle;
+        if (mLastVisitedTimestamp == 0) {
+            return mTitle;
+        }
+        // TODO(crbug.com/1173154): Set last visit timestamp based on history query.
+        long difference = 0;
+        Resources resources = mRowView.getContext().getResources();
+        if (difference == 0) {
+            return resources.getString(R.string.page_info_history_last_visit_today);
+        } else if (difference == DateUtils.DAY_IN_MILLIS) {
+            return resources.getString(R.string.page_info_history_last_visit_yesterday);
+        } else if (difference > DateUtils.DAY_IN_MILLIS
+                && difference <= DateUtils.DAY_IN_MILLIS * 7) {
+            return resources.getString(R.string.page_info_history_last_visit_days,
+                    (int) (difference / DateUtils.DAY_IN_MILLIS));
+        } else {
+            return resources.getString(R.string.page_info_history_last_visit_date,
+                    StringUtils.dateToHeaderString(new Date(mLastVisitedTimestamp)));
+        }
     }
 
     @Override
