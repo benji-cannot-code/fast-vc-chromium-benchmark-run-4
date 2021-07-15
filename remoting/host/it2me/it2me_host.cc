2f4dfe9422ecf43be199caf40b65a63df08121aa
Joe Downing <joedow@google.com>
2021-07-15 21:59:10 +0000

    Ensure session-terminate signaling messages are sent in It2Me sessions
    I hit this issue while looking into the session rejection DCHECK bug.
    When the It2Me host owns its own signaling strategy and the session
    terminates, it queues a signaling message with the disconnect reason
    but the message isn't sent as signal_strategy is torn down shortly
    afterwards.
    
    This CL delays destruction of the signal_strategy by ~2 seconds to
    provide enough time for the message to be sent. This should be
    safe as we have similar logic in client_session.cc which has been
    running for a few years now and the signal_strategy instance doesn't
    rely on any other objects.
    
    Bug: 1181824
    Change-Id: I10f1a47a5f477cc94247be5dae096ab6bc55c265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033161
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902187}

--- remoting/host/it2me/it2me_host.cc
+++ remoting/host/it2me/it2me_host.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/time/time.h"
 #include "build/chromeos_buildflags.h"
 #include "components/policy/policy_constants.h"
 #include "net/url_request/url_request_context_getter.h"
@@ -58,6 +59,11 @@ using protocol::ValidatingAuthenticator;
 typedef ValidatingAuthenticator::Result ValidationResult;
 typedef ValidatingAuthenticator::ValidationCallback ValidationCallback;
 typedef ValidatingAuthenticator::ResultCallback ValidationResultCallback;
+
+// The amount of time to wait before destroying the signal strategy.  This delay
+// ensures there is time for the session-terminate message to be sent.
+constexpr base::TimeDelta kDestroySignalingDelay =
+    base::TimeDelta::FromSeconds(2);
 
 }  // namespace
 
@@ -553,7 +559,19 @@ void It2MeHost::DisconnectOnNetworkThread() {
   host_status_logger_ = nullptr;
   log_to_server_ = nullptr;
   ftl_signaling_connector_ = nullptr;
-  signal_strategy_ = nullptr;
+
+  if (signal_strategy_) {
+    // Delay destruction of the signaling strategy by a few seconds to give it
+    // a chance to send any outgoing messages (e.g. session-terminate) so the
+    // other end of the connection can display and log an accurate disconnect
+    // reason.
+    host_context_->network_task_runner()->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce([](std::unique_ptr<SignalStrategy> signaling) {},
+                       std::move(signal_strategy_)),
+        kDestroySignalingDelay);
+  }
+
   host_event_logger_ = nullptr;
 
   // Post tasks to delete UI objects on the UI thread.
