020e7c933bcd5e559f6959f78a950e010d253e88
Tommy Li <tommycli@chromium.org>
2021-07-15 20:08:10 +0000

    [memories] Make HistoryService::GetAnnotatedVisits() take QueryOptions
    We'd like to support pagination in the History Clusters WebUI, just like
    History does today.
    
    We'd also like to take advantage of the basic de-duplication and
    visibility checking that HistoryService already does.
    
    To that end, this CL rewrites `GetAnnotatedVisits()` to use the same
    underlying logic as `QueryHistory()`, and tacks on the annotations
    afterwards.
    
    This CL also fixes the issue where the content_annotations were not
    attached to AnnotatedVisits.
    
    Bug: 1171352, 1220765
    Change-Id: I2f6349f3d80833eab53aae2ed5474c68cacbc8fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3003803
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902098}

--- components/history_clusters/core/history_clusters_service.cc
+++ components/history_clusters/core/history_clusters_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/case_conversion.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/history/core/browser/history_types.h"
 #include "components/history_clusters/core/history_clusters_buildflags.h"
 #include "components/history_clusters/core/memories_features.h"
 #include "components/history_clusters/core/remote_clustering_backend.h"
@@ -259,14 +260,16 @@ void HistoryClustersService::QueryClusters(
           .Then(base::BindOnce(&ClustersToMojom))
           .Then(std::move(callback)));
 
-  history_service_->GetRecentClusterIdsAndAnnotatedVisits(
-      base::Time::Min(), kMaxVisitsToCluster.Get(),
+  // TODO(tommycli): Support pagination by setting `begin_time` on `options`.
+  history::QueryOptions options;
+  options.max_count = kMaxVisitsToCluster.Get();
+
+  history_service_->GetAnnotatedVisits(
+      options,
       base::BindOnce(
           [](const IncompleteVisitMap& incomplete_visits,
              const base::Time& end_time,
-             history::ClusterIdsAndAnnotatedVisitsResult result) {
-            auto& visits = result.annotated_visits;
-
+             std::vector<history::AnnotatedVisit> visits) {
             // Append incomplete visits to `visits` too, as otherwise they will
             // be mysteriously missing from the Clusters UI. They haven't
             // recorded the page end metrics yet, but that's fine.
