2d0c5b59118dd9f6b1c084c2c094852b7cc4197a
Cattalyya Nuengsigkapian <cattalyya@chromium.org>
2021-07-15 02:47:25 +0000

    dark_mode: Add settings search for enabler toggle and accent color
    - Added dark mode icon in settings search.
    - Implemented search for dark mode toggle and theme radio buttons.
    - Added deep link and unit tests for toggle and radio button group.
    
    Bug: 1217436
    Test: Manual and `browser_tests --gtest_filter=*DarkModeSubpage*`
    Change-Id: I5a04345e667f84c7bcffea9d6b26a93bc629addb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027068
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Cattalyya Nuengsigkapian <cattalyya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901794}

--- chrome/browser/ui/webui/settings/chromeos/personalization_section.h
+++ chrome/browser/ui/webui/settings/chromeos/personalization_section.h
@@ -45,6 +45,8 @@ class PersonalizationSection : public OsSettingsSection {
   // ash::AmbientModeService::Observer:
   void OnAmbientModeEnabledStateChanged();
 
+  void OnDarkModeEnabledStateChanged();
+
   PrefService* pref_service_;
   PrefChangeRegistrar pref_change_registrar_;
 };
