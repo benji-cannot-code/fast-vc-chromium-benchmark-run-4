586e264fce07c78c605ee6c9ed86e51ca65ba276
Andrew Xu <andrewxu@chromium.org>
2021-07-15 02:15:28 +0000

    [Shelf] Create a unified api to pin or unpin shelf icons
    BUG=b:190654715
    
    Change-Id: Ib146acf0c4ec77fbe34a1ef62a663ebc410f0fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983491
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901781}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.h
@@ -1279,6 +1279,7 @@ class AutotestPrivateSetArcTouchModeFunction : public ExtensionFunction {
   ResponseAction Run() override;
 };
 
+// Deprecated. Use `AutotestPrivateSetShelfIconPinFunction` instead.
 class AutotestPrivatePinShelfIconFunction : public ExtensionFunction {
  public:
   AutotestPrivatePinShelfIconFunction();
@@ -1286,6 +1287,17 @@ class AutotestPrivatePinShelfIconFunction : public ExtensionFunction {
                              AUTOTESTPRIVATE_PINSHELFICON)
  private:
   ~AutotestPrivatePinShelfIconFunction() override;
+  ResponseAction Run() override;
+};
+
+class AutotestPrivateSetShelfIconPinFunction : public ExtensionFunction {
+ public:
+  AutotestPrivateSetShelfIconPinFunction();
+  DECLARE_EXTENSION_FUNCTION("autotestPrivate.setShelfIconPin",
+                             AUTOTESTPRIVATE_SETSHELFICONPIN)
+
+ private:
+  ~AutotestPrivateSetShelfIconPinFunction() override;
   ResponseAction Run() override;
 };
 
