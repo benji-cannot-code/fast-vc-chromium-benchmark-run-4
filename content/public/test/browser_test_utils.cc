eaa338ec3fdcaa996c4c8e427a641dfd123af144
Bruce Dawson <brucedawson@chromium.org>
2021-07-15 15:23:18 +0000

    Include combaseapi.h where needed
    uiautomation.h requires that combaseapi.h be included first. This file
    currently depends on other header files to do this but is broken by
    various windows.h reduction changes.
    
    Bug: 796644
    Change-Id: If7d0e47d4d2638904dfebd2a7893d32998ddee28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027560
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901960}

--- content/public/test/browser_test_utils.cc
+++ content/public/test/browser_test_utils.cc
@@ -131,6 +131,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/resources/grit/webui_generated_resources.h"
 
 #if defined(OS_WIN)
+#include <combaseapi.h>
 #include <uiautomation.h>
 #include <wrl/client.h>
 #include "base/win/scoped_safearray.h"
