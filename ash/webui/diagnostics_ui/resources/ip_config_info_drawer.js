66971fe9d9db92ad9c7291e16f2726417fd20d24
Ashley Prasad <ashleydp@google.com>
2021-07-15 19:41:13 +0000

    Diagnostics: Create skeleton ip-config-info-drawer
    - Creates skeletal ip-config-info-drawer
    - Adds test to confirm drawer can be added to DOM
    - Follow on CLs will add drawer to utilizing elements and add
      data-points to UI.
    
    Bug: 1197335
    Test: browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I1e11207aea62a4510b0b79835aba5dc9eae2b5a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021696
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/master@{#902081}

---
+++ ash/webui/diagnostics_ui/resources/ip_config_info_drawer.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './diagnostics_fonts_css.js';
+import './diagnostics_shared_css.js';
+
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @fileoverview
+ * 'ip-config-info-drawer' displays standard IP related configuration data in a
+ * collapsible drawer.
+ */
+Polymer({
+  is: 'ip-config-info-drawer',
+
+  _template: html`{__html_template__}`,
+
+  properties: {},
+});
--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_browsertest.js
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_browsertest.js
@@ -93,6 +93,7 @@ const debug_suites_list = [
   'FakeSystemDataProvider',
   'FakeSystemRoutineContoller',
   'InputList',
+  'IpConfigInfoDrawer',
   'MemoryCard',
   'NetworkCard',
   'NetworkInfo',
