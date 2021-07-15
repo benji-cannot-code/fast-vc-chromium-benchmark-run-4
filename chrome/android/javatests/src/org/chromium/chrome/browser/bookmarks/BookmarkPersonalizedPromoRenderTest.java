8506c6338a211cfb4736e3e8d1cb3e1db1aab53c
Shu Yang <shuyng@google.com>
2021-07-15 01:36:38 +0000

    Migrate cards to MaterialCardView Batch4
    Migrate personalized_signin_promo_view* layouts to MaterialCardView,
    which give the cards a solid background color and 16dp cornerSize.
    This is the fourth batch of changes, which will be followed by more CLs.
    
    Bug: 1217140
    Change-Id: If212191e2f0595bfd8e84a32b2f93c96ce534397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009684
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901756}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedPromoRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedPromoRenderTest.java
@@ -132,6 +132,6 @@ public class BookmarkPersonalizedPromoRenderTest {
     private View getPersonalizedPromoView() {
         BookmarkActivity bookmarkActivity = mBookmarkTestRule.getBookmarkActivity();
         Assert.assertNotNull("BookmarkActivity should not be null", bookmarkActivity);
-        return bookmarkActivity.findViewById(R.id.signin_promo_view_container);
+        return bookmarkActivity.findViewById(R.id.signin_promo_view_wrapper);
     }
 }
