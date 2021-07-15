3f15e85e6f79358bf31ffe7c51824845b102cfa9
Vidhan Jain <vidhanj@google.com>
2021-07-15 19:25:35 +0000

    [iOS][Autofill] Hide Tap interactions behind feature flag
    This change hides the autofill address prompts tap interactions behind
    the feature flag
    AutofillAddressProfileSavePromptAddressVerificationSupport.
    
    Bug: 1167062
    Change-Id: I0f3ad054115ba09eaca0bf216d3f248305f2ad08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027019
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/master@{#902067}

--- ios/chrome/browser/ui/infobars/modals/autofill_address_profile/infobar_save_address_profile_table_view_controller.mm
+++ ios/chrome/browser/ui/infobars/modals/autofill_address_profile/infobar_save_address_profile_table_view_controller.mm
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/infobars/modals/autofill_address_profile/infobar_save_address_profile_table_view_controller.h"
 
+#include "base/feature_list.h"
 #include "base/mac/foundation_util.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/infobars/infobar_metrics_recorder.h"
 #import "ios/chrome/browser/ui/autofill/autofill_ui_type.h"
@@ -179,9 +181,13 @@ typedef NS_ENUM(NSInteger, ItemType) {
     didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
   TableViewModel* model = self.tableViewModel;
   NSInteger itemType = [model itemTypeForIndexPath:indexPath];
-  if (itemType == ItemTypeSaveModalFields ||
-      itemType == ItemTypeUpdateModalFields) {
-    [self ensureContextMenuShownForItemType:itemType atIndexPath:indexPath];
+  if (base::FeatureList::IsEnabled(
+          autofill::features::
+              kAutofillAddressProfileSavePromptAddressVerificationSupport)) {
+    if (itemType == ItemTypeSaveModalFields ||
+        itemType == ItemTypeUpdateModalFields) {
+      [self ensureContextMenuShownForItemType:itemType atIndexPath:indexPath];
+    }
   }
 }
 
