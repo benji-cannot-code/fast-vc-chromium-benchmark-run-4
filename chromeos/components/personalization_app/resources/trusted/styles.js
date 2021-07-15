337861764231bb9b0cbee25a37d2803026159e5b
Jeffrey Young <cowmoo@google.com>
2021-07-15 20:08:46 +0000

    personalization: use cros colors
    Load cros_colors.generated.css to define ChromeOS specific
    semantic colors.
    Also add cros_color_overrides to override cr_element styles for
    ChromeOS.
    Continue using shared_vars_css as a base list of colors.
    
    BUG=b/193566293
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Id2b43888ddec65767283bfda5fffcfb0fdd6d6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024049
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Mitsuru Oshima (catching up) <oshima@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902099}

--- chromeos/components/personalization_app/resources/trusted/styles.js
+++ chromeos/components/personalization_app/resources/trusted/styles.js
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import 'chrome://resources/cr_elements/chromeos/cros_color_overrides.m.js';
+import 'chrome://resources/cr_elements/shared_vars_css.m.js';
+import 'chrome://resources/polymer/v3_0/paper-styles/color.js';
 
 const styles = document.createElement('dom-module');
 
--- chromeos/components/personalization_app/untrusted_personalization_app_ui_config.cc
+++ chromeos/components/personalization_app/untrusted_personalization_app_ui_config.cc
@@ -17,8 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/common/url_constants.h"
 #include "services/network/public/mojom/content_security_policy.mojom-shared.h"
+#include "ui/chromeos/colors/cros_colors.h"
 #include "ui/resources/grit/webui_generated_resources.h"
 #include "ui/resources/grit/webui_generated_resources_map.h"
+#include "ui/resources/grit/webui_resources.h"
 #include "url/gurl.h"
 
 namespace chromeos {
@@ -38,6 +40,17 @@ void AddStrings(content::WebUIDataSource* source) {
   // to import load_time_data.m.js at this unusual path.
   source->AddResourcePath("load_time_data.js", IDR_WEBUI_JS_LOAD_TIME_DATA_JS);
   source->UseStringsJs();
+}
+
+void AddCrosColors(content::WebUIDataSource* source) {
+  source->AddResourcePath("chromeos/colors/cros_colors.generated.css",
+                          IDR_WEBUI_CROS_COLORS_CSS);
+
+  source->AddString(
+      "crosColorsDebugOverrides",
+      base::FeatureList::IsEnabled(ash::features::kSemanticColorsDebugOverride)
+          ? cros_colors::kDebugOverrideCssString
+          : std::string());
 }
 
 class UntrustedPersonalizationAppUI : public ui::UntrustedWebUIController {
@@ -67,6 +80,8 @@ class UntrustedPersonalizationAppUI : public ui::UntrustedWebUIController {
     // inside untrusted iframe.
     source->AddResourcePaths(base::make_span(kWebuiGeneratedResources,
                                              kWebuiGeneratedResourcesSize));
+
+    AddCrosColors(source.get());
 
     source->AddFrameAncestor(GURL(kChromeUIPersonalizationAppURL));
 
