577707de36fb60bde12ab6af84465c08474880a8
Kunihiko Sakamoto <ksakamoto@chromium.org>
2021-07-15 04:15:48 +0000

    Revert "[Android] Create host-specific history query"
    This reverts commit 09659abe97cdae6c72ed3b885355d6be79592faf.
    
    Reason for revert: HistoryQueryTest.Paging is flaky crbug.com/1229461
    
    Original change's description:
    > [Android] Create host-specific history query
    >
    > Add backend function to query history urls and only returning matching
    > hosts.
    >
    > Screenshot: https://crbug.com/1173154#c17
    > Bug: 1173154
    > Change-Id: I7a588400bec9c4b3aeddffd203e6b3349191c6f6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001890
    > Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#901635}
    
    Bug: 1173154,1229461
    Change-Id: Id82506c1c74ba714186aa833ddbe3aa96c6661b9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026899
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Owners-Override: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901811}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
@@ -67,7 +67,6 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
     private boolean mPrivacyDisclaimersVisible;
     private boolean mClearBrowsingDataButtonVisible;
     private String mQueryText = EMPTY_QUERY;
-    private String mHostName;
 
     private boolean mDisableScrollToLoadForTest;
 
@@ -100,11 +99,7 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
         mAreHeadersInitialized = false;
         mIsLoadingItems = true;
         mClearOnNextQueryComplete = true;
-        if (mHostName != null) {
-            mHistoryProvider.queryHistoryForHost(mHostName);
-        } else {
-            mHistoryProvider.queryHistory(mQueryText);
-        }
+        mHistoryProvider.queryHistory(mQueryText);
     }
 
     @Override
@@ -417,9 +412,12 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
         if (mAreHeadersInitialized) setHeaders();
     }
 
-    /** @param hostName The hostName to retrieve history entries for. */
-    public void setHostName(String hostName) {
-        mHostName = hostName;
+    /**
+     * Sets the query text.
+     * @param query The text to be searched for.
+     */
+    void setQueryText(String query) {
+        mQueryText = query;
     }
 
     @VisibleForTesting
