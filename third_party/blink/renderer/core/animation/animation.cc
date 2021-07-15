20c4e9cac0af986386c5262df1213e32de7e645d
Jordan Taylor <jortaylo@microsoft.com>
2021-07-15 20:02:26 +0000

    Refactored scroll timeline tests to defaults to progress based testing.
    Converted a few tests to be progress based and fixed a conversion bug in
    animation.cc
    
    Bug: 1216655
    Change-Id: I0882800677a4af0863883d59970594ea27cfb582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3025004
    Commit-Queue: Jordan Taylor <jortaylo@microsoft.com>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902091}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -855,6 +855,15 @@ void Animation::setTimeline(AnimationTimeline* timeline) {
   AnimationPlayState old_play_state = CalculateAnimationPlayState();
   absl::optional<AnimationTimeDelta> old_current_time = CurrentTimeInternal();
 
+  // In some cases, we need to preserve the progress of the animation between
+  // the old timeline and the new one. We do this by storing the progress using
+  // the old current time and the effect end based on the old timeline. Pending
+  // spec issue: https://github.com/w3c/csswg-drafts/issues/6452
+  double progress = 0;
+  if (old_current_time && !EffectEnd().is_zero()) {
+    progress = old_current_time.value() / EffectEnd();
+  }
+
   CancelAnimationOnCompositor();
 
   // 3. Let the timeline of the animation be the new timeline.
@@ -874,7 +883,8 @@ void Animation::setTimeline(AnimationTimeline* timeline) {
     document_->Timeline().AnimationAttached(this);
   SetOutdated();
 
-  // Update content timing to be based on new timeline type
+  // Update content timing to be based on new timeline type. This ensures that
+  // EffectEnd() is returning a value appropriate to the new timeline.
   if (content_ && timeline_)
     content_->InvalidateNormalizedTiming();
 
@@ -900,7 +910,8 @@ void Animation::setTimeline(AnimationTimeline* timeline) {
           if (old_current_time) {
             reset_current_time_on_resume_ = true;
             start_time_ = absl::nullopt;
-            SetHoldTimeAndPhase(old_current_time, TimelinePhase::kInactive);
+            SetHoldTimeAndPhase(progress * EffectEnd(),
+                                TimelinePhase::kInactive);
           } else if (PendingInternal()) {
             start_time_ = boundary_time;
           }
@@ -911,7 +922,7 @@ void Animation::setTimeline(AnimationTimeline* timeline) {
       }
     } else if (old_current_time && old_timeline &&
                !old_timeline->IsMonotonicallyIncreasing()) {
-      SetCurrentTimeInternal(old_current_time.value());
+      SetCurrentTimeInternal(progress * EffectEnd());
     }
   }
 
