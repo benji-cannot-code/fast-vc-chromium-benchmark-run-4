3f76ea37ec0f747187dfbf338bb465828938b037
Francois Doray <fdoray@chromium.org>
2021-07-15 20:28:42 +0000

    [battery] Add scenario suffixes to all power-related histograms.
    This will enable assessing the impact of an experiment on a specific
    scenario using regular dashboards.
    
    Change-Id: Ic4feef97001d068022c0fe3e551a7fb4611cf995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027655
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Olivier Li <olivierli@google.com>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902114}

--- chrome/browser/performance_monitor/process_metrics_recorder.cc
+++ chrome/browser/performance_monitor/process_metrics_recorder.cc
@@ -56,9 +56,4 @@ void ProcessMetricsRecorder::OnMetricsSampled(
   }
 }
 
-void ProcessMetricsRecorder::OnAggregatedMetricsSampled(
-    const ProcessMonitor::Metrics& metrics) {
-  RecordProcessHistograms("Total", metrics);
-}
-
 }  // namespace performance_monitor
--- chrome/browser/performance_monitor/process_monitor.h
+++ chrome/browser/performance_monitor/process_monitor.h
@@ -39,6 +39,10 @@ struct ProcessMetadata {
 // upon noticing serious performance degradation.
 class ProcessMonitor {
  public:
+  // The interval at which ProcessMonitor performs its timed collections.
+  static constexpr base::TimeDelta kGatherInterval =
+      base::TimeDelta::FromMinutes(2);
+
   struct Metrics {
     // The percentage of time spent executing, across all threads of the
     // process, in the interval since the last time the metric was sampled. This
@@ -91,10 +95,6 @@ class ProcessMonitor {
 
   // Start the cycle of metrics gathering.
   void StartGatherCycle();
-
-  // Returns the expected sampling interval according to how it's scheduled.
-  // This might vary from the actual interval. Virtual for testing.
-  virtual base::TimeDelta GetScheduledSamplingInterval() const;
 
   // Adds/removes an observer.
   void AddObserver(Observer* observer);
