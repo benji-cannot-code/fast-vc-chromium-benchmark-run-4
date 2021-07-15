f3b05c5815f0c84f8ee38788aeb6d4bef43780cb
Matt Menke <mmenke@chromium.org>
2021-07-15 16:57:21 +0000

    Add missing includes to components/quirks/
    This was split off of
    https://chromium-review.googlesource.com/c/chromium/src/+/3025728,
    which fixes over a hundred IWYU issues expecting SimpleURLLoader to
    bring in url_response_head.mojom.h, which I'm trying to remove.
    
    Bug: 1227131
    Change-Id: I463d8c2ef8bd1c84563ad9be2967d325d313e20e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032281
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Mitsuru Oshima (catching up) <oshima@chromium.org>
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Mitsuru Oshima (catching up) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902010}

--- components/quirks/quirks_client.cc
+++ components/quirks/quirks_client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_status_code.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 
 namespace quirks {
 
