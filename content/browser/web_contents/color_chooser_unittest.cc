19fd402007b61de08d835694975206faf30a5df8
Lei Zhang <thestig@chromium.org>
2021-07-15 02:00:07 +0000

    Remove unnecessary includes in WebContentsImpl.
    WebContentsImpl is one of the biggest classes around. Reduce excess
    includes to try to help reduce its size. Headers being removed are based
    on suggestions from the IWYU tool.
    
    Bug: 242216
    Change-Id: I68daa6541595ecd5d0cdbf0c925573b0c8058ae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027267
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901770}

--- content/browser/web_contents/color_chooser_unittest.cc
+++ content/browser/web_contents/color_chooser_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "content/public/browser/color_chooser.h"
+
 #include "base/test/scoped_feature_list.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/content_navigation_policy.h"
