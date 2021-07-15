0cbfda04dbcd99c34b1606924d5791f49e25bede
Benoit Lize <lizeb@chromium.org>
2021-07-15 14:32:29 +0000

    [base/allocator] Rework size -> bucket mapping.
    We need to quickly map a requested size to a bucket at allocation
    time. To that end, we precompute tables, and then the mapping is done
    purely with ALU/logic operations and table lookups (with small constant
    tables)
    
    There are two things that are not ideal with the current tables:
    - Part of it is constexpr, part of it computed at runtime when the
      buckets are created. These two pieces of code have to match.
    - It leaves "holes", that is invalid buckets, particularly in the first
      few very hot buckets. For instance, with 8 buckets per order, the
      first valid buckets are #0 and #8, with 7 invalid ones
      in-between. This increases the memory footprint of e.g. the thread
      cache, but also decreases cache locality.
    
    Finally, this is not very flexible to experiment with different bucket
    size distributions.
    
    This CL does two things:
    - Unify the runtime and constexpr parts
    - Remove the invalid buckets.
    
    This increases cache locality in the thread cache. Assuming a 64 bit
    machine with 16 byte alignment and the current default setting of 4
    buckets per order, we had before in the first 2 cache lines of the
    thread cache (XX) denotes an invalid bucket:
    16 (20) (24) (28)
    32 (40) (48)  56
    
    And now:
    16 32 48  64
    80 96 112 128
    
    So in the first 2 cache lines, we handle allocations up to 128 bytes, which is
    >70% of all allocations for renderers (from speedometer 2.0 data, on Linux). The
    difference is starker with 8 buckets per order.
    
    Bug: 998048
    Change-Id: Ib0f31973969c4058319d5629b8670cbd4eb8f200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3013896
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#901941}

--- base/trace_event/malloc_dump_provider.cc
+++ base/trace_event/malloc_dump_provider.cc
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/allocator_shim_default_dispatch_to_partition_alloc.h"
 #endif
 
+#if defined(PA_THREAD_CACHE_ALLOC_STATS)
+#include "base/allocator/partition_allocator/partition_alloc_constants.h"
+#endif
+
 namespace base {
 namespace trace_event {
 
@@ -392,9 +396,11 @@ void ReportPartitionAllocThreadCacheStats(ProcessMemoryDump* pmd,
     if (detailed) {
       std::string name = dump->absolute_name();
       for (size_t i = 0; i < kNumBuckets; i++) {
-        std::string dump_name =
-            base::StringPrintf("%s/buckets_alloc/%d", name.c_str(),
-                               static_cast<int>(stats.bucket_size_[i]));
+        size_t bucket_size = stats.bucket_size_[i];
+        if (bucket_size == kInvalidBucketSize)
+          continue;
+        std::string dump_name = base::StringPrintf(
+            "%s/buckets_alloc/%d", name.c_str(), static_cast<int>(bucket_size));
         auto* buckets_alloc_dump = pmd->CreateAllocatorDump(dump_name);
         buckets_alloc_dump->AddScalar("count", "objects",
                                       stats.allocs_per_bucket_[i]);
