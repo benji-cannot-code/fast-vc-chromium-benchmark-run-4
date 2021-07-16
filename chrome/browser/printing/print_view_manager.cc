4562e5bdb8e0dca9e62345644cc86ee98cb322bf
Jan Kopanski <jkopanski@google.com>
2021-07-16 09:23:52 +0000

    Fix sending double reporting events for DLP printing restrictions
    On the printing path we were checking twice if printing is restricted
    by DLP rules. As a result for REPORT level rules, two reporting
    events were sent each time a single restriction was triggered.
    
    Now, all printing paths for Chrome OS should still by covered by the DLP
    restriction check.
    Additionally, crbug.com/1213872 is resolved.
    
    The classes TestPrintViewManager and PrintPreviewDialogClonedObserver
    are now duplicated in print_preview_dialog_controller_browsertest.cc and
    dlp_content_manager_browsertest.cc. This will be addressed in a
    subsequent CL.
    
    Bug: 1226488,1213872
    Change-Id: I673291415c531d4a10ba8ea4a2da5571a49e6f3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024447
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jan Kopański <jkopanski@google.com>
    Cr-Commit-Position: refs/heads/master@{#902371}

--- chrome/browser/printing/print_view_manager.cc
+++ chrome/browser/printing/print_view_manager.cc
@@ -193,6 +193,7 @@ bool PrintViewManager::RejectPrintPreviewRequestIfRestricted(
   if (!IsPrintingRestricted())
     return false;
   GetPrintRenderFrame(rfh)->OnPrintPreviewDialogClosed();
+  PrintPreviewDone();
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   policy::ShowDlpPrintDisabledNotification();
 #endif
@@ -228,13 +229,6 @@ bool PrintViewManager::PrintPreview(
   // Don't print / print preview crashed tabs.
   if (IsCrashed())
     return false;
-
-  if (IsPrintingRestricted()) {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-    policy::ShowDlpPrintDisabledNotification();
-#endif
-    return false;
-  }
 
   GetPrintRenderFrame(rfh)->InitiatePrintPreview(std::move(print_renderer),
                                                  has_selection);
