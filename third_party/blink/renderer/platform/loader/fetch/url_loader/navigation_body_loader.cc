f9a49b19ad01c2b3fd50197530414ce0d728cb92
Mason Freed <masonf@chromium.org>
2021-07-16 21:57:01 +0000

    Add a Finch-tunable parameter for the loader's navigation chunk size
    See [1] for a related CL. This CL adds a parameter to [1] to control
    the NavigationBodyLoader's chunk size, for experimentation. See [2]
    for the eventual goal of increasing that size from 512kB to ~2MB or
    more. But this CL will allow experimentation first, to ensure no
    memory regressions, and find a good final value.
    
    Without any changes via Finch, this CL should have no effect on
    behavior.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3027323
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/2983494
    
    Bug: 1041006
    Change-Id: Ic9db4516262ee1ecce455806d20aae28e0432a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3027705
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902692}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/navigation_body_loader.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/navigation_body_loader.cc
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/strcat.h"
 #include "base/trace_event/trace_event.h"
+#include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/url_loader_completion_status.h"
 #include "services/network/public/mojom/early_hints.mojom.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
@@ -22,9 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_navigation_params.h"
 
 namespace blink {
-
-// static
-constexpr uint32_t NavigationBodyLoader::kMaxNumConsumedBytesInTask;
 
 NavigationBodyLoader::NavigationBodyLoader(
     const KURL& original_url,
@@ -234,9 +234,9 @@ void NavigationBodyLoader::ReadFromDataPipe() {
       NotifyCompletionIfAppropriate();
       return;
     }
-    DCHECK_LE(num_bytes_consumed, kMaxNumConsumedBytesInTask);
-    available =
-        std::min(available, kMaxNumConsumedBytesInTask - num_bytes_consumed);
+    uint32_t chunk_size = network::features::GetLoaderChunkSize();
+    DCHECK_LE(num_bytes_consumed, chunk_size);
+    available = std::min(available, chunk_size - num_bytes_consumed);
     if (available == 0) {
       // We've already consumed many bytes in this task. Defer the remaining
       // to the next task.
@@ -375,5 +375,4 @@ void WebNavigationBodyLoader::FillNavigationParamsResponseAndBodyLoader(
         std::move(resource_load_info_notifier_wrapper), is_main_frame));
   }
 }
-
 }  // namespace blink
