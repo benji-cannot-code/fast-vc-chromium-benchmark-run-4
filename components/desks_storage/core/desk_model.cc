045da3fa4b305611fb60911ba406a9fb0bacc2d9
avynn <avynn@google.com>
2021-07-16 02:23:25 +0000

    Changed GetAllUuids to GetAllEntries
    Changed desk model and local desk storage manager to retrieve all
    entries instead of retrieving all uuids.  This is to help integrate the
    storage layer's with the window management team's desk template
    extension API.
    
    Bug: 1213712
    Change-Id: Ic5589aa2bf91f1f5efd251edebb9fa9c0528319a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021756
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902282}

--- components/desks_storage/core/desk_model.cc
+++ components/desks_storage/core/desk_model.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/desks_storage/core/desk_model.h"
 
+#include "ash/public/cpp/desk_template.h"
 #include "components/desks_storage/core/desk_model_observer.h"
 
 namespace desks_storage {
@@ -12,6 +13,20 @@ namespace desks_storage {
 DeskModel::DeskModel() = default;
 
 DeskModel::~DeskModel() = default;
+
+GetAllEntriesResult::GetAllEntriesResult(
+    GetAllEntriesStatus status,
+    std::vector<ash::DeskTemplate*> entries)
+    : status(status), entries(std::move(entries)) {}
+
+GetAllEntriesResult::~GetAllEntriesResult() = default;
+
+GetEntryByUuidResult::GetEntryByUuidResult(
+    GetEntryByUuidStatus status,
+    std::unique_ptr<ash::DeskTemplate> entry)
+    : status(status), entry(std::move(entry)) {}
+
+GetEntryByUuidResult::~GetEntryByUuidResult() = default;
 
 void DeskModel::AddObserver(DeskModelObserver* observer) {
   DCHECK(observer);
