3da4fb22d8fadaaf5d156e98a882046cc98b1ea3
Miguel Casas <mcasas@chromium.org>
2021-07-16 20:14:26 +0000

    media: fix kVp9kSVCHW(De|En)coding flag names
    media::kVp9kSVCHW(De|En)coding should be media::kVaapiVp9...
    to align with similar feature flags, e.g. VaapiVP8Encoder etc.
    This CL does that, and also simplifies a few of the preprocessor
    guards (USE_VAAPI implies ARCH_CPU_X86_FAMILY).
    
    No new functionality.
    
    Bug: b:191203129
    Change-Id: I73a093c49c153ca776dccc4149e46b413ff5c4d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033290
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902626}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder.cc
@@ -1595,9 +1595,8 @@ bool RTCVideoEncoder::Vp9HwSupportForSpatialLayers() {
   // encoder instead of checking OS.
   // Underlying encoder will check the spatial SVC encoding capability on CrOS.
 #if defined(ARCH_CPU_X86_FAMILY) && BUILDFLAG(IS_CHROMEOS_ASH)
-  if (base::FeatureList::IsEnabled(media::kVp9kSVCHWEncoding))
-    return true;
-#endif  // defined(ARCH_CPU_X86_FAMILY) && BUILDFLAG(IS_CHROMEOS_ASH)
+  return base::FeatureList::IsEnabled(media::kVaapiVp9kSVCHWEncoding);
+#endif
   // Spatial layers are not supported by hardware encoders.
   return false;
 }
