2e2367044f9425873139364c76c3e53f9768869b
Ivan Šandrk <isandrk@chromium.org>
2021-07-16 16:26:59 +0000

    [dPWA] Sub Apps - interfaces & renderer part
    Enable PWAs to create multiple launch icons with distinct names.
    
    This CL was originally started in crrev.com/c/2684855 and is now being
    committed in parts and polished.
    
    Explainer: https://github.com/ivansandrk/work/blob/main/explainer.md
    
    Design doc: go/multi-apps (draft)
    
    Bug: 1171317
    Change-Id: I0e170e0949181e7e3119b2cec10a5f594e9702f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2929966
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902459}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -442,6 +442,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kAllowDropAlphaForMediaStream;
 
 BLINK_COMMON_EXPORT extern const base::Feature kThirdPartyStoragePartitioning;
 
+BLINK_COMMON_EXPORT extern const base::Feature kDesktopPWAsSubApps;
+
 }  // namespace features
 }  // namespace blink
 
