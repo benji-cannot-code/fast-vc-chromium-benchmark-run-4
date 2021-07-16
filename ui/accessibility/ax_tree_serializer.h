aed5b5154a7dbfc0a3f4a920a0d4ec23a21ed900
Aaron Leventhal <aleventhal@google.com>
2021-07-16 18:03:56 +0000

    Enforce clean serialization
    Update severity of serialization failures to CHECK() because of
    confidence in stability of Blink accessibility, and desire to find
    and repair any remaining issues in serialization.
    
    In general, we want to improve the architecture so that tree resets
    are not necessary, because they destroy the user's position within
    their screen reader, sending them to the top of the document.
    
    Bug: None
    Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    Change-Id: I4b9ebac58b6f61095f49ac0422c69393a92ebecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3028406
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902530}

--- ui/accessibility/ax_tree_serializer.h
+++ ui/accessibility/ax_tree_serializer.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
@@ -425,10 +424,7 @@ ClientTreeNode* AXTreeSerializer<AXSourceNode>::GetClientTreeNodeParent(
         "ax_ts_missing_parent_err", base::debug::CrashKeySize::Size256);
     base::debug::SetCrashKeyString(missing_parent_err,
                                    error.str().substr(0, 230));
-#if defined(AX_FAIL_FAST_BUILD)
     CHECK(false) << error.str();
-#endif  // defined(AX_FAIL_FAST_BUILD)
-    base::debug::DumpWithoutCrashing();
   }
   return parent;
 }
@@ -634,7 +630,6 @@ bool AXTreeSerializer<AXSourceNode>::SerializeChangedNodes(
 
     ClientTreeNode* client_child = ClientTreeNodeById(new_child_id);
     if (client_child && GetClientTreeNodeParent(client_child) != client_node) {
-#if defined(AX_FAIL_FAST_BUILD)
       // This condition leads to performance problems. It will
       // also reset virtual buffers, causing users to lose their place.
       std::ostringstream error;
@@ -648,16 +643,6 @@ bool AXTreeSerializer<AXSourceNode>::SerializeChangedNodes(
           "ax_ts_reparent_err", base::debug::CrashKeySize::Size256);
       base::debug::SetCrashKeyString(reparent_err, error.str().substr(0, 230));
       CHECK(false) << error.str();
-#endif  // defined(AX_FAIL_FAST_BUILD)
-      static bool has_sent_reparent_err = false;
-      if (!has_sent_reparent_err) {
-        std::srand(std::time(nullptr));  // use current time as seed.
-        if (std::rand() % 50 == 0) {     // Roughly 2% of the time.
-          base::debug::DumpWithoutCrashing();
-          has_sent_reparent_err = true;  // Only send once.
-        }
-      }
-      Reset();
       return false;
     }
   }
@@ -750,18 +735,7 @@ bool AXTreeSerializer<AXSourceNode>::SerializeChangedNodes(
         static auto* dupe_id_err = base::debug::AllocateCrashKeyString(
             "ax_ts_dupe_id_err", base::debug::CrashKeySize::Size256);
         base::debug::SetCrashKeyString(dupe_id_err, error.str().substr(0, 230));
-#if defined(AX_FAIL_FAST_BUILD)
         CHECK(false) << error.str();
-#endif  // defined(AX_FAIL_FAST_BUILD)
-        static bool has_sent_dupe_id_err = false;
-        if (!has_sent_dupe_id_err) {
-          std::srand(std::time(nullptr));  // use current time as seed.
-          if (std::rand() % 50 == 0) {     // Roughly 2% of the time.
-            base::debug::DumpWithoutCrashing();
-            has_sent_dupe_id_err = true;  // Only send once.
-          }
-        }
-        Reset();
         return false;
       }
       client_id_map_[child_id] = new_child;
