cb20664dae318f887d5dab82685dd5fe3568e650
Roman Arora <romanarora@chromium.org>
2021-07-16 16:29:16 +0000

    [TabSearch] Support tab updates for non previously fetched tabs
    Adds support for rendering tabs that were not returned as part of
    the initial GetProfileData call. This can happen when tabs don't
    have a committed URL initially but do at a later point in time.
    
    Fixed: 1222365
    Change-Id: I19931d8afe26cd024e0a1f84e109032472ee1310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032288
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902461}

--- chrome/browser/ui/webui/tab_search/tab_search_page_handler_unittest.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_page_handler_unittest.cc
@@ -57,7 +57,7 @@ class MockPage : public tab_search::mojom::Page {
   mojo::Receiver<tab_search::mojom::Page> receiver_{this};
 
   MOCK_METHOD1(TabsChanged, void(tab_search::mojom::ProfileDataPtr));
-  MOCK_METHOD1(TabUpdated, void(tab_search::mojom::TabPtr));
+  MOCK_METHOD1(TabUpdated, void(tab_search::mojom::TabUpdateInfoPtr));
   MOCK_METHOD1(TabsRemoved, void(const std::vector<int32_t>& tab_ids));
 };
 
@@ -549,7 +549,9 @@ TEST_F(TabSearchPageHandlerTest, TabsNotChanged) {
   ASSERT_FALSE(IsTimerRunning());
 }
 
-bool VerifyTabUpdated(const tab_search::mojom::TabPtr& tab) {
+bool VerifyTabUpdated(
+    const tab_search::mojom::TabUpdateInfoPtr& tab_update_info) {
+  const tab_search::mojom::TabPtr& tab = tab_update_info->tab;
   ExpectNewTab(tab.get(), kTabUrl1, kTabName1, 1);
   return true;
 }
