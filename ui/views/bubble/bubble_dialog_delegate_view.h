22339995f15326ce315abb1f06526c856456b1a6
Robert Liao <robliao@chromium.org>
2021-07-16 19:48:42 +0000

    Maintain a Primary BubbleDialogDelegate When Multiple Bubbles Use The Same Anchor
    Multiple bubbles may request the same anchor primarily when at least
    one bubble unsets close_on_deactivate and the user requests another
    bubble.
    
    Because bubbles generally don't coordinate with each other and each
    anchor is agnostic towards bubbles, when the latest (deemed primary)
    bubble is closed, the remaining bubble's requests for things like
    anchor highlighting are no longer respected.
    
    The resolution here keeps a stack of BubbleDialogDelegates for an
    anchor and designates the top of the stack as the primary bubble.
    
    Once the primary dismissed, it's popped off the stack and the top
    bubble, if available, is notified that it is the new primary,
    reestablishing whatever state it needs on the anchor.
    
    BUG=911099
    
    Change-Id: If424388f1ffe8fd64678e4a23bd3812f112a9314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031812
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902598}

--- ui/views/bubble/bubble_dialog_delegate_view.h
+++ ui/views/bubble/bubble_dialog_delegate_view.h
@@ -291,6 +291,10 @@ class VIEWS_EXPORT BubbleDialogDelegate : public DialogDelegate,
                            VisibleWidgetShowsInkDropOnAttaching);
   FRIEND_TEST_ALL_PREFIXES(BubbleDialogDelegateViewTest,
                            AttachedWidgetShowsInkDropWhenVisible);
+  FRIEND_TEST_ALL_PREFIXES(BubbleDialogDelegateViewTest,
+                           MultipleBubbleAnchorHighlightTestInOrder);
+  FRIEND_TEST_ALL_PREFIXES(BubbleDialogDelegateViewTest,
+                           MultipleBubbleAnchorHighlightTestOutOfOrder);
 
   friend class AnchorViewObserver;
   friend class AnchorWidgetObserver;
@@ -312,6 +316,14 @@ class VIEWS_EXPORT BubbleDialogDelegate : public DialogDelegate,
   void OnBubbleWidgetPaintAsActiveChanged();
 
   void OnDeactivate();
+
+  // Notify this bubble that it is now the primary anchored bubble. When a new
+  // bubble becomes the primary anchor, the previous primary silently loses its
+  // primary status. This method is only called when this bubble becomes primary
+  // after losing it.
+  void NotifyAnchoredBubbleIsPrimary();
+
+  void SetAnchoredDialogKey();
 
   gfx::Insets title_margins_;
   BubbleBorder::Arrow arrow_ = BubbleBorder::NONE;
