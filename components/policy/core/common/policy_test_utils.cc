39368e3466d7907094c749baf9e137961d7a6797
Owen Min <zmin@chromium.org>
2021-07-16 19:38:36 +0000

    Try to enable some flaky tests
    Re-enable
    CloudPolicyManagerTest.Register
    CloudPolicyManagerTest.RegisterWithRetry
    
    This test is failed on trybot due to policy detected. However, this can
    not be verified at this point. Re-enable the test with some extra log
    file to see if it will still fail.
    
    Bug: 1224321
    Change-Id: Ia8ec4f5cca4d841c56fc14affda76c6125a41d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035702
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902593}

--- components/policy/core/common/policy_test_utils.cc
+++ components/policy/core/common/policy_test_utils.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "components/policy/core/common/policy_bundle.h"
+#include "components/policy/policy_constants.h"
 
 #if defined(OS_APPLE)
 #include <CoreFoundation/CoreFoundation.h>
@@ -50,6 +51,9 @@ bool PolicyServiceIsEmpty(const PolicyService* service) {
     for (auto it = map.begin(); it != map.end(); ++it)
       dict.SetKey(it->first, it->second.value()->Clone());
     LOG(WARNING) << "There are pre-existing policies in this machine: " << dict;
+#if defined(OS_WIN)
+    LOG(WARNING) << "From: " << kRegistryChromePolicyKey;
+#endif
   }
   return map.empty();
 }
