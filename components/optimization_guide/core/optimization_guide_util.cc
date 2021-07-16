0e546974b8c542bbe695a8e8f577cef3c710fd9b
Sophie Chang <sophiechang@chromium.org>
2021-07-16 02:56:26 +0000

    Add optimization target for page entities
    Bug: 1228790
    Change-Id: Idf3fa072036f01c7059b4d736f4569d86191768d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033623
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902295}

--- components/optimization_guide/core/optimization_guide_util.cc
+++ components/optimization_guide/core/optimization_guide_util.cc
@@ -35,6 +35,8 @@ std::string GetStringNameForOptimizationTarget(
       return "SegmentationVoice";
     case proto::OPTIMIZATION_TARGET_MODEL_VALIDATION:
       return "ModelValidation";
+    case proto::OPTIMIZATION_TARGET_PAGE_ENTITIES:
+      return "PageEntities";
   }
   NOTREACHED();
   return std::string();
