1ee939d7ad6841e99b8eb9cbc2e80fbd852a6901
Sergey Poromov <poromov@chromium.org>
2021-07-16 09:16:31 +0000

    Revert "Remove kAccessibilityFocusFallsbackToWidgetKey window property"
    This reverts commit 8ceec698bb5dc0e59bd01b32665bad99e9b67f9a.
    
    Reason for revert: Possible culprit for the flaky failures in Linux Tests builders:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests/102573/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tester%20(Ozone%20X11)/14227/overview
    
    Bug: 1229965
    
    Original change's description:
    > Remove kAccessibilityFocusFallsbackToWidgetKey window property
    >
    > R=​hirokisato@chromium.org
    >
    > Bug: none
    > Test: manually tab with ChromeVox, Lacros and ARC++.
    > Change-Id: Ia9d4d8af994e0db97d5f91e7da72ddfbded1ad18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021747
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#902317}
    
    Bug: none
    Change-Id: I9624b8ff62da57d831e225d2095bb084de2e6b72
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032093
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Owners-Override: Sergey Poromov <poromov@chromium.org>
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902369}

--- chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
+++ chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
@@ -243,11 +243,9 @@ IN_PROC_BROWSER_TEST_F(LoggedInSpokenFeedbackTest, NavigateNotificationCenter) {
   });
   sm_.ExpectSpeech(
       "Quick Settings, Press search plus left to access the notification "
-      "center.");
+      "center., window");
 
   sm_.Call([this]() { SendKeyPressWithSearch(ui::VKEY_LEFT); });
-  sm_.Call([this]() { SendKeyPressWithSearch(ui::VKEY_LEFT); });
-
   // If you are hitting this in the course of changing the UI, please fix. This
   // item needs a label.
   sm_.ExpectSpeech("List item");
@@ -670,7 +668,7 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackTest, OpenStatusTray) {
   });
   sm_.ExpectSpeech(
       "Quick Settings, Press search plus left to access the notification "
-      "center.");
+      "center., window");
   sm_.Replay();
 }
 
@@ -685,7 +683,7 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackTest, NavigateSystemTray) {
   });
   sm_.ExpectSpeech(
       "Quick Settings, Press search plus left to access the notification "
-      "center.");
+      "center., window");
 
   // Avatar button. Disabled for guest account.
   if (GetParam() != kTestAsGuestUser) {
