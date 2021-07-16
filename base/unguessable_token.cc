853cafe26aeb1b94160c696a494f1ebac63be2b4
Liza Burakova <liza@chromium.org>
2021-07-16 14:09:05 +0000

    Modified UnguessableToken == and != operators to be time constant
    Bug: 1004921
    Change-Id: I7612027c6a80c3388df3d4032a97137647d13f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3023887
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902410}

--- base/unguessable_token.cc
+++ base/unguessable_token.cc
@@ -9,6 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/format_macros.h"
 #include "base/rand_util.h"
+#include "build/build_config.h"
+
+#if !defined(OS_NACL)
+#include "third_party/boringssl/src/include/openssl/mem.h"
+#endif
 
 namespace base {
 
@@ -34,6 +39,17 @@ UnguessableToken UnguessableToken::Deserialize(uint64_t high, uint64_t low) {
   // backup check.
   DCHECK(!(high == 0 && low == 0));
   return UnguessableToken(Token{high, low});
+}
+
+bool UnguessableToken::operator==(const UnguessableToken& other) const {
+#if defined(OS_NACL)
+  // BoringSSL is unavailable for NaCl builds so it remains timing dependent.
+  return token_ == other.token_;
+#else
+  auto bytes = token_.AsBytes();
+  auto other_bytes = other.token_.AsBytes();
+  return CRYPTO_memcmp(bytes.data(), other_bytes.data(), bytes.size()) == 0;
+#endif
 }
 
 std::ostream& operator<<(std::ostream& out, const UnguessableToken& token) {
