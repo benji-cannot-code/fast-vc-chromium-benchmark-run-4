263588e2ab93c6f346971691404b95b19dbe838f
Yuwei Huang <yuweih@chromium.org>
2021-07-16 18:37:24 +0000

    [remoting host] Refactor URL forwarder code for multi-platform support
    This CL factors out a RemoteOpenUrlClient::Delegate interface for
    platform specific implementation, and provides a dummy implementation
    for Windows that doesn't do local fallback.
    
    Bug: b:183135000
    Change-Id: Iee1ff71707624828a736f2edc61b3d80d39d99a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032046
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902558}

---
+++ remoting/host/remote_open_url_client_delegate_win.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_REMOTE_OPEN_URL_CLIENT_DELEGATE_WIN_H_
+#define REMOTING_HOST_REMOTE_OPEN_URL_CLIENT_DELEGATE_WIN_H_
+
+#include "remoting/host/remote_open_url_client.h"
+
+namespace remoting {
+
+// RemoteOpenUrlClient::Delegate implementation for Windows.
+class RemoteOpenUrlClientDelegateWin final
+    : public RemoteOpenUrlClient::Delegate {
+ public:
+  RemoteOpenUrlClientDelegateWin();
+  ~RemoteOpenUrlClientDelegateWin() override;
+
+  bool IsInRemoteDesktopSession() override;
+  void OpenUrlOnFallbackBrowser(const GURL& url) override;
+  void ShowOpenUrlError(const GURL& url) override;
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_REMOTE_OPEN_URL_CLIENT_DELEGATE_WIN_H_
