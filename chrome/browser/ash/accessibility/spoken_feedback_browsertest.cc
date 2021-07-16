90d9325bcabec2a63d0db95a0cc7cda41f58d3c7
David Tseng <dtseng@google.com>
2021-07-16 21:52:19 +0000

    Reland: Remove kAccessibilityFocusFallsbackToWidgetKey window property
    Bug: 1229965
    Test: manually tab with ChromeVox, Lacros and ARC++.
    Change-Id: I1d12e57fbb94c73469f2c6ab04f68e52a87b73ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035192
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902686}

--- chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
+++ chrome/browser/ash/accessibility/spoken_feedback_browsertest.cc
@@ -243,9 +243,11 @@ IN_PROC_BROWSER_TEST_F(LoggedInSpokenFeedbackTest, NavigateNotificationCenter) {
   });
   sm_.ExpectSpeech(
       "Quick Settings, Press search plus left to access the notification "
-      "center., window");
+      "center.");
 
   sm_.Call([this]() { SendKeyPressWithSearch(ui::VKEY_LEFT); });
+  sm_.Call([this]() { SendKeyPressWithSearch(ui::VKEY_LEFT); });
+
   // If you are hitting this in the course of changing the UI, please fix. This
   // item needs a label.
   sm_.ExpectSpeech("List item");
@@ -668,7 +670,7 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackTest, OpenStatusTray) {
   });
   sm_.ExpectSpeech(
       "Quick Settings, Press search plus left to access the notification "
-      "center., window");
+      "center.");
   sm_.Replay();
 }
 
@@ -683,7 +685,7 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackTest, NavigateSystemTray) {
   });
   sm_.ExpectSpeech(
       "Quick Settings, Press search plus left to access the notification "
-      "center., window");
+      "center.");
 
   // Avatar button. Disabled for guest account.
   if (GetParam() != kTestAsGuestUser) {
