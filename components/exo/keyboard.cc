692b42c84ac1a84c38c8e9cb381e3162fab006dd
Yuichiro Hanada <yhanada@google.com>
2021-07-16 17:16:29 +0000

    exo: Use a focused window's {Pre,Post}EventHandler.
    Before this CL, exo::Keyboard added itsell to ash::Shell's
    {Pre,Post}EventHandler to observe key events.
    However, it causes that exo::Keyboard receives a key event which is not
    targeting to an ARC++ window.
    This CL fixes the problem by adding exo::Keyboard as a event handler of
    a focused window's toplevel window.
    
    Bug: 1227326
    Change-Id: I61f05c7d3092c673a4a577e5381f40cd5b8942ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026698
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Mitsuru Oshima (catching up) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902481}

--- components/exo/keyboard.cc
+++ components/exo/keyboard.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/shell_surface.h"
 #include "components/exo/shell_surface_util.h"
 #include "components/exo/surface.h"
-#include "components/exo/wm_helper.h"
 #include "components/exo/xkb_tracker.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/focus_client.h"
@@ -168,7 +167,6 @@ Keyboard::Keyboard(std::unique_ptr<KeyboardDelegate> delegate, Seat* seat)
       seat_(seat),
       expiration_delay_for_pending_key_acks_(base::TimeDelta::FromMilliseconds(
           kExpirationDelayForPendingKeyAcksMs)) {
-  AddEventHandler();
   seat_->AddObserver(this, kKeyboardSeatObserverPriority);
   ash::KeyboardController::Get()->AddObserver(this);
   ash::ImeControllerImpl* ime_controller = ash::Shell::Get()->ime_controller();
@@ -432,6 +430,7 @@ void Keyboard::OnKeyboardLayoutNameChanged(const std::string& layout_name) {
 
 void Keyboard::SetFocus(Surface* surface) {
   if (focus_) {
+    RemoveEventHandler();
     delegate_->OnKeyboardLeave(focus_);
     focus_->RemoveSurfaceObserver(this);
     focus_ = nullptr;
@@ -444,6 +443,7 @@ void Keyboard::SetFocus(Surface* surface) {
     focus_ = surface;
     focus_->AddSurfaceObserver(this);
     focused_on_ime_supported_surface_ = IsImeSupportedSurface(surface);
+    AddEventHandler();
   }
 }
 
@@ -486,19 +486,35 @@ void Keyboard::ScheduleProcessExpiredPendingKeyAcks(base::TimeDelta delay) {
 }
 
 void Keyboard::AddEventHandler() {
-  auto* helper = WMHelper::GetInstance();
+  if (!focus_)
+    return;
+
+  // Toplevel window can be not ShellSurface, for example for a notification
+  // surface.
+  aura::Window* toplevel_window = focus_->window();
+  if (toplevel_window->GetToplevelWindow())
+    toplevel_window = toplevel_window->GetToplevelWindow();
+
   if (are_keyboard_key_acks_needed_)
-    helper->AddPreTargetHandler(this);
+    toplevel_window->AddPreTargetHandler(this);
   else
-    helper->AddPostTargetHandler(this);
+    toplevel_window->AddPostTargetHandler(this);
 }
 
 void Keyboard::RemoveEventHandler() {
-  auto* helper = WMHelper::GetInstance();
+  if (!focus_)
+    return;
+
+  // Toplevel window can be not ShellSurface, for example for a notification
+  // surface.
+  aura::Window* toplevel_window = focus_->window();
+  if (toplevel_window->GetToplevelWindow())
+    toplevel_window = toplevel_window->GetToplevelWindow();
+
   if (are_keyboard_key_acks_needed_)
-    helper->RemovePreTargetHandler(this);
+    toplevel_window->RemovePreTargetHandler(this);
   else
-    helper->RemovePostTargetHandler(this);
+    toplevel_window->RemovePostTargetHandler(this);
 }
 
 }  // namespace exo
