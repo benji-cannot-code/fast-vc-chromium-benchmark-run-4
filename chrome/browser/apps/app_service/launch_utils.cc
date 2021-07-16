e83b8b0f95996b02512a3abe446d9470759506f8
Maggie Cai <mxcai@google.com>
2021-07-16 03:10:56 +0000

    [Lacros] Use app service instead of web app provider to check app installed.
    This CL updates the code in launch_util to use the app service to check
    if the app is installed instead of web app provider. This is to reduce
    the direct usage of web app provider where possible.
    
    BUG=1144877
    
    Change-Id: I6ca2ffce8f1652f4abc69daf7d284e724ffe2b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3006555
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902304}

--- chrome/browser/apps/app_service/launch_utils.cc
+++ chrome/browser/apps/app_service/launch_utils.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "chrome/browser/apps/app_service/app_service_proxy.h"
+#include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -26,6 +28,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "ui/events/event_constants.h"
 #include "url/gurl.h"
+
+namespace {
+
+bool IsAppInstalled(Profile* profile, const std::string& app_id) {
+  if (!apps::AppServiceProxyFactory::IsAppServiceAvailableForProfile(profile)) {
+    return false;
+  }
+  auto* proxy = apps::AppServiceProxyFactory::GetForProfile(profile);
+  bool app_installed = false;
+  proxy->AppRegistryCache().ForOneApp(
+      app_id, [&app_installed](const apps::AppUpdate& update) {
+        app_installed = update.Readiness() == apps::mojom::Readiness::kReady;
+      });
+  return app_installed;
+}
+
+}  // namespace
 
 namespace apps {
 
@@ -59,9 +78,7 @@ bool IsInstalledApp(Profile* profile, const std::string& app_id) {
     DCHECK(extension->is_app());
     return true;
   }
-  web_app::WebAppRegistrar& registrar =
-      web_app::WebAppProvider::Get(profile)->registrar();
-  return registrar.IsInstalled(app_id);
+  return IsAppInstalled(profile, app_id);
 }
 
 void SetAppIdForWebContents(Profile* profile,
@@ -82,10 +99,9 @@ void SetAppIdForWebContents(Profile* profile,
     extensions::TabHelper::FromWebContents(web_contents)
         ->SetExtensionAppById(app_id);
   } else {
-    web_app::WebAppRegistrar& registrar =
-        web_app::WebAppProvider::Get(profile)->registrar();
+    bool app_installed = IsAppInstalled(profile, app_id);
     web_app::WebAppTabHelper::FromWebContents(web_contents)
-        ->SetAppId(registrar.IsInstalled(app_id) ? app_id : std::string());
+        ->SetAppId(app_installed ? app_id : std::string());
     extensions::TabHelper::FromWebContents(web_contents)
         ->SetExtensionAppById(std::string());
   }
