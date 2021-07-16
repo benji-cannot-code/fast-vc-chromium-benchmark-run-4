787d167a975d0e6be57e0ea524d4d85d4b0397d5
Ana Salazar <anasalazar@chromium.org>
2021-07-16 19:57:10 +0000

    Add continue section view to AppListBubble
    Initial class builder for the continue section view in the app list
    bubble.
    
    ContinueSectionView will be the wrapper around the tasks suggestions
    within the new launcher.
    
    Bug: 1216084
    Change-Id: Ie631aaa426be162897309573c3d6e447a46b73bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018735
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902605}

---
+++ ash/app_list/views/continue_task_view.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/app_list/views/continue_task_view.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "ash/bubble/bubble_utils.h"
+#include "base/strings/string_util.h"
+#include "extensions/common/constants.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/views/controls/label.h"
+#include "ui/views/layout/fill_layout.h"
+
+namespace ash {
+
+ContinueTaskView::ContinueTaskView(const std::u16string& task_title) {
+  SetLayoutManager(std::make_unique<views::FillLayout>());
+  auto* label = AddChildView(std::make_unique<views::Label>(task_title));
+  bubble_utils::ApplyStyle(label, bubble_utils::LabelStyle::kBody);
+}
+
+ContinueTaskView::~ContinueTaskView() = default;
+
+BEGIN_METADATA(ContinueTaskView, views::View)
+END_METADATA
+
+}  // namespace ash
