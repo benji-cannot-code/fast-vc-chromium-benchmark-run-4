aa34a55c9abc7d78aaf8eab2ee32e1e3e83e869d
Alice Wang <aliceywang@chromium.org>
2021-07-16 10:18:05 +0000

    [Android] Migrate to FakeAccountInfoService in ProfileDataCacheRenderTest
    This CL migrates the FakeProfileDataSource usage to
    FakeAccountInfoService in ProfileDataCacheRenderTest as the
    former is deprecated.
    
    Bug: 1223414
    Change-Id: Ifc734f49ab3cd6ca073a80680e7c989702f74c8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3001371
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902379}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCacheRenderTest.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCacheRenderTest.java
@@ -52,17 +52,13 @@ import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.DummyUiChromeActivityTestCase;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.browser.Features;
-import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
-import org.chromium.chrome.test.util.browser.Features.EnableFeatures;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
-import org.chromium.components.signin.ProfileDataSource;
 import org.chromium.components.signin.base.AccountInfo;
 import org.chromium.components.signin.base.CoreAccountId;
 import org.chromium.components.signin.identitymanager.AccountInfoServiceProvider;
 import org.chromium.components.signin.identitymanager.AccountTrackerService;
 import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.components.signin.identitymanager.IdentityManagerJni;
-import org.chromium.components.signin.test.util.FakeProfileDataSource;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.widget.ChromeImageView;
 
@@ -75,7 +71,7 @@ import java.util.List;
  */
 @RunWith(ParameterizedRunner.class)
 @UseRunnerDelegate(ChromeJUnit4RunnerDelegate.class)
-@DisableFeatures({ChromeFeatureList.DEPRECATE_MENAGERIE_API})
+@Features.EnableFeatures({ChromeFeatureList.DEPRECATE_MENAGERIE_API})
 @Batch(ProfileDataCacheRenderTest.PROFILE_DATA_BATCH_NAME)
 public class ProfileDataCacheRenderTest extends DummyUiChromeActivityTestCase {
     public static final String PROFILE_DATA_BATCH_NAME = "profile_data";
@@ -101,14 +97,13 @@ public class ProfileDataCacheRenderTest extends DummyUiChromeActivityTestCase {
     public final Features.JUnitProcessor mProcessor = new Features.JUnitProcessor();
 
     @Rule
-    public final AccountManagerTestRule mAccountManagerTestRule =
-            new AccountManagerTestRule(new FakeProfileDataSource());
+    public final AccountManagerTestRule mAccountManagerTestRule = new AccountManagerTestRule();
 
     @Rule
     public final JniMocker mocker = new JniMocker();
 
     @Rule
-    public final MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+    public final MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.LENIENT);
 
     @Mock
     private AccountTrackerService mAccountTrackerServiceMock;
@@ -132,6 +127,10 @@ public class ProfileDataCacheRenderTest extends DummyUiChromeActivityTestCase {
     public void setUp() {
         mocker.mock(IdentityManagerJni.TEST_HOOKS, mIdentityManagerNativeMock);
         AccountInfoServiceProvider.init(mIdentityManager, mAccountTrackerServiceMock);
+        doAnswer(AdditionalAnswers.answerVoid(Runnable::run))
+                .when(mAccountTrackerServiceMock)
+                .seedAccountsIfNeeded(any(Runnable.class));
+
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             Activity activity = getActivity();
             mContentView = new FrameLayout(activity);
@@ -141,44 +140,12 @@ public class ProfileDataCacheRenderTest extends DummyUiChromeActivityTestCase {
 
             mProfileDataCache = new ProfileDataCache(activity, mImageSize,
                     /*badgeConfig=*/null);
-            // ProfileDataCache only populates the cache when an observer is added.
-            mProfileDataCache.addObserver(accountId -> {});
         });
     }
 
     @After
     public void tearDown() {
         AccountInfoServiceProvider.resetForTests();
-    }
-
-    @Test
-    @MediumTest
-    @Feature("RenderTest")
-    public void testProfileDataWithAvatarFromIdentityManager() throws IOException {
-        doAnswer(AdditionalAnswers.answerVoid(Runnable::run))
-                .when(mAccountTrackerServiceMock)
-                .seedAccountsIfNeeded(any(Runnable.class));
-        when(mIdentityManagerNativeMock.findExtendedAccountInfoByEmailAddress(
-                     anyLong(), eq(ACCOUNT_EMAIL)))
-                .thenReturn(mAccountInfoWithAvatar);
-        mAccountManagerTestRule.addAccount(
-                new ProfileDataSource.ProfileData(ACCOUNT_EMAIL, null, "Full Name", "Given Name"));
-        TestThreadUtils.runOnUiThreadBlocking(() -> { checkImageIsScaled(ACCOUNT_EMAIL); });
-        mRenderTestRule.render(mImageView, "profile_data_cache_avatar" + mImageSize);
-    }
-
-    @Test
-    @MediumTest
-    @Feature("RenderTest")
-    public void testProfileDataUpdatedFromIdentityManagerObserver() throws IOException {
-        doAnswer(AdditionalAnswers.answerVoid(Runnable::run))
-                .when(mAccountTrackerServiceMock)
-                .seedAccountsIfNeeded(any(Runnable.class));
-        mAccountManagerTestRule.addAccount(
-                new ProfileDataSource.ProfileData(ACCOUNT_EMAIL, null, "Full Name", "Given Name"));
-        mIdentityManager.onExtendedAccountInfoUpdated(mAccountInfoWithAvatar);
-        TestThreadUtils.runOnUiThreadBlocking(() -> { checkImageIsScaled(ACCOUNT_EMAIL); });
-        mRenderTestRule.render(mImageView, "profile_data_cache_avatar" + mImageSize);
     }
 
     @Test
@@ -193,12 +160,8 @@ public class ProfileDataCacheRenderTest extends DummyUiChromeActivityTestCase {
 
     @Test
     @MediumTest
-    @EnableFeatures({ChromeFeatureList.DEPRECATE_MENAGERIE_API})
     @Feature("RenderTest")
     public void testProfileDataPopulatedWithoutGmsProfileDataSource() throws IOException {
-        doAnswer(AdditionalAnswers.answerVoid(Runnable::run))
-                .when(mAccountTrackerServiceMock)
-                .seedAccountsIfNeeded(any(Runnable.class));
         when(mIdentityManagerNativeMock.findExtendedAccountInfoByEmailAddress(
                      anyLong(), eq(ACCOUNT_EMAIL)))
                 .thenReturn(mAccountInfoWithAvatar);
@@ -241,8 +204,9 @@ public class ProfileDataCacheRenderTest extends DummyUiChromeActivityTestCase {
     @MediumTest
     @Feature("RenderTest")
     public void testPlaceholderIsScaled() throws IOException {
-        TestThreadUtils.runOnUiThreadBlocking(
-                () -> { checkImageIsScaled("no.data.for.this.account@example.com"); });
+        final String email = "no.data.for.this.account@example.com";
+        mAccountManagerTestRule.addAccount(email);
+        TestThreadUtils.runOnUiThreadBlocking(() -> { checkImageIsScaled(email); });
         mRenderTestRule.render(mImageView, "profile_data_cache_placeholder" + mImageSize);
     }
 
@@ -250,9 +214,10 @@ public class ProfileDataCacheRenderTest extends DummyUiChromeActivityTestCase {
     @MediumTest
     @Feature("RenderTest")
     public void testAvatarIsScaled() throws IOException {
-        ProfileDataSource.ProfileData profileData = new ProfileDataSource.ProfileData(
-                ACCOUNT_EMAIL, createAvatar(), "Full Name", "Given Name");
-        mAccountManagerTestRule.addAccount(profileData);
+        when(mIdentityManagerNativeMock.findExtendedAccountInfoByEmailAddress(
+                     anyLong(), eq(ACCOUNT_EMAIL)))
+                .thenReturn(mAccountInfoWithAvatar);
+        mAccountManagerTestRule.addAccount(ACCOUNT_EMAIL);
         TestThreadUtils.runOnUiThreadBlocking(() -> { checkImageIsScaled(ACCOUNT_EMAIL); });
         mRenderTestRule.render(mImageView, "profile_data_cache_avatar" + mImageSize);
     }
