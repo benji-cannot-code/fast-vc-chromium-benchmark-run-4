e45355bc540078f02b3cc3f3ffc60df87fa8fd20
Lei Zhang <thestig@chromium.org>
2021-07-16 01:16:10 +0000

    Remove unnecessary includes in ChromeContentBrowserClient.
    ChromeContentBrowserClient is one of the biggest classes around. Reduce
    excess includes to try to help reduce its size. Headers being removed
    are based on suggestions from the IWYU tool.
    
    Bug: 242216
    Change-Id: I9ec17365ddbe689f720c8b935a49446d8efe9fe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032865
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902253}

--- chrome/browser/chrome_content_browser_client.h
+++ chrome/browser/chrome_content_browser_client.h
@@ -11,13 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 #include <string>
-#include <utility>
 #include <vector>
 
 #include "base/callback.h"
 #include "base/containers/flat_map.h"
 #include "base/macros.h"
-#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
