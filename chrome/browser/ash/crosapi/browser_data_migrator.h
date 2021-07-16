3ad51db3700ea39cc1eac23c935d2aaefb996156
Yuta Hijikata <ythjkt@google.com>
2021-07-16 02:49:49 +0000

    Prevent BrowserDataMigrator from following symlinks.
    The change ensures that BrowserDataMigrator does not follow symlinks
    while copying files under user data directory.
    
    Bug: 1178702
    Test: unit tests
    Change-Id: I8d60bbff7d34702441e685bb41c7895056187a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3023716
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902292}

--- chrome/browser/ash/crosapi/browser_data_migrator.h
+++ chrome/browser/ash/crosapi/browser_data_migrator.h
@@ -114,6 +114,7 @@ class BrowserDataMigrator {
   FRIEND_TEST_ALL_PREFIXES(BrowserDataMigratorTest,
                            IsMigrationRequiredOnWorker);
   FRIEND_TEST_ALL_PREFIXES(BrowserDataMigratorTest, GetTargetInfo);
+  FRIEND_TEST_ALL_PREFIXES(BrowserDataMigratorTest, CopyDirectory);
   FRIEND_TEST_ALL_PREFIXES(BrowserDataMigratorTest, RecordStatus);
   FRIEND_TEST_ALL_PREFIXES(BrowserDataMigratorTest, Migrate);
 
@@ -137,6 +138,14 @@ class BrowserDataMigrator {
   // Compares space available under `from_dir_` against total byte size that
   // needs to be copied.
   bool HasEnoughDiskSpace(const TargetInfo& target_info) const;
+  // Copies `item` to location pointed by `dest`. Returns true on success and
+  // false on failure.
+  bool CopyTargetItem(const BrowserDataMigrator::TargetItem& item,
+                      const base::FilePath& dest) const;
+  // Copies the contents of `from_path` to `to_path` recursively. Unlike
+  // `base::CopyDirectory()` it skips symlinks.
+  bool CopyDirectory(const base::FilePath& from_path,
+                     const base::FilePath& to_path) const;
   // Copies files from `from_dir_` to `tmp_dir_`.
   bool CopyToTmpDir(const TargetInfo& target_info) const;
   // Moves `tmp_dir_` to `to_dir_`.
