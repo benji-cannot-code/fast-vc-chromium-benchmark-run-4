1c2673587ffabc5cd55594966d37be5dd8571d64
Fan Yang <fayang@chromium.org>
2021-07-16 17:48:53 +0000

    Roll src/net/third_party/quiche/src/ 6f99b68e1..9bfd181de (6 commits)
    https://quiche.googlesource.com/quiche.git/+log/6f99b68e1995..bf2d194d095ca983af35ed1836c063ba78937d42
    
    $ git log 6f99b68e1..bf2d194d095ca --date=short --no-merges --format='%ad %ae %s'
    2021-07-15 fayang Fix missing default value of stream_id and max_data in QuicWindowUpdateFrame.
    2021-07-15 fayang Not update ACK timeout for NEW_CONNECTION_ID and RETIRE_CONNECTION_ID frames.
    2021-07-14 bnc Fix issues identified by Cross.
    2021-07-14 fayang Add missing MaybeUpdateAckTimeout for ack-eliciting frames.
    2021-07-14 bnc Remove unused CancelPushFrame and PushPromiseFrame structs and some more unused methods.
    2021-07-14 vasilvv Use absl::Span<QuicMemSlice> for QUIC datagrams.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    R=dschinazi@chromium.org
    
    Change-Id: Id1bdd1ea2951950296f165ba9c996106f9e90719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031902
    Auto-Submit: Fan Yang <fayang@chromium.org>
    Reviewed-by: Dan Zhang <danzh@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902186}
    
    
    Roll src/net/third_party/quiche/src/ 6f99b68e1..9772ae360 (8 commits)
    
    https://quiche.googlesource.com/quiche.git/+log/6f99b68e1995..9772ae360b76
    
    $ git log 6f99b68e1..9772ae360 --date=short --no-merges --format='%ad %ae %s'
    2021-07-15 quiche-dev Internal change
    2021-07-15 fayang Initialize data_buffer in PathChallenge and PathResponse frames to all 0s.
    2021-07-15 fayang Fix missing default value of stream_id and max_data in QuicWindowUpdateFrame.
    2021-07-15 fayang Not update ACK timeout for NEW_CONNECTION_ID and RETIRE_CONNECTION_ID frames.
    2021-07-14 bnc Fix issues identified by Cross.
    2021-07-14 fayang Add missing MaybeUpdateAckTimeout for ack-eliciting frames.
    2021-07-14 bnc Remove unused CancelPushFrame and PushPromiseFrame structs and some more unused methods.
    2021-07-14 vasilvv Use absl::Span<QuicMemSlice> for QUIC datagrams.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I2b1c2846d1c175e0bec2d7a191617d17857046ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035301
    Auto-Submit: Fan Yang <fayang@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902515}

--- net/quic/quic_network_transaction_unittest.cc
+++ net/quic/quic_network_transaction_unittest.cc
@@ -3781,7 +3781,7 @@ TEST_P(QuicNetworkTransactionTest, ResetAfterHandshakeConfirmedThenBroken) {
   if (VersionUsesHttp3(version_.transport_version)) {
     quic_data.AddWrite(
         SYNCHRONOUS,
-        client_maker_->MakeRstAckAndDataPacket(
+        client_maker_->MakeAckRstAndDataPacket(
             packet_num++, true, GetNthClientInitiatedBidirectionalStreamId(0),
             quic::QUIC_HEADERS_TOO_LARGE, 1, 1, GetQpackDecoderStreamId(),
             false, StreamCancellationQpackDecoderInstruction(0)));
@@ -4050,7 +4050,7 @@ TEST_P(QuicNetworkTransactionTest,
   if (VersionUsesHttp3(version_.transport_version)) {
     mock_quic_data.AddWrite(
         SYNCHRONOUS,
-        client_maker_->MakeRstAckAndDataPacket(
+        client_maker_->MakeAckRstAndDataPacket(
             packet_num++, /*include_version=*/true,
             GetNthClientInitiatedBidirectionalStreamId(1),
             quic::QUIC_HEADERS_TOO_LARGE, 3, 2, GetQpackDecoderStreamId(),
@@ -5410,7 +5410,7 @@ TEST_P(QuicNetworkTransactionTest, RstStreamBeforeHeaders) {
   if (VersionUsesHttp3(version_.transport_version)) {
     mock_quic_data.AddWrite(
         SYNCHRONOUS,
-        client_maker_->MakeRstAckAndDataPacket(
+        client_maker_->MakeAckRstAndDataPacket(
             packet_num++, false, GetNthClientInitiatedBidirectionalStreamId(0),
             quic::QUIC_STREAM_CANCELLED, 1, 1, GetQpackDecoderStreamId(), false,
             StreamCancellationQpackDecoderInstruction(0)));
