f5a5a96b87528544079342abc7d281425ae8bc7f
Aaron Leventhal <aleventhal@google.com>
2021-07-17 13:27:17 +0000

    Reland: Enforce clean serialization
    Update severity of serialization failures to CHECK() because of
    confidence in stability of Blink accessibility, and desire to find
    and repair any remaining issues in serialization.
    
    In general, we want to improve the architecture so that tree resets
    are not necessary, because they destroy the user's position within
    their screen reader, sending them to the top of the document.
    
    Original: CL:3028406
    Revert: CL:3035862
    Reason for revert:
    - accessibility_unittests failed because of:
      AXTreeSerializerTest.DuplicateIdsReturnsErrorAndFlushes
    
    Bug: None
    Change-Id: I29327508080d5115ef2dddfbb9bf5195a255e751
    Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel,linux-trusty-rel,linux-bionic-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036164
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902824}

--- ui/accessibility/ax_tree_serializer_unittest.cc
+++ ui/accessibility/ax_tree_serializer_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/accessibility/ax_common.h"
 #include "ui/accessibility/ax_node.h"
 #include "ui/accessibility/ax_serializable_tree.h"
 
@@ -340,10 +339,9 @@ TEST_F(AXTreeSerializerTest, MaximumSerializedNodeCount) {
   ASSERT_EQ(5u, update.nodes.size());
 }
 
-#if !defined(AX_FAIL_FAST_BUILD)
-// If duplicate ids are encountered, it returns an error and the next
-// update will re-send the entire tree.
-TEST_F(AXTreeSerializerTest, DuplicateIdsReturnsErrorAndFlushes) {
+#if defined(GTEST_HAS_DEATH_TEST)
+// If duplicate ids are encountered, it crashes via CHECK(false).
+TEST_F(AXTreeSerializerTest, DuplicateIdsCrashes) {
   // (1 (2 (3 (4) 5)))
   treedata0_.root_id = 1;
   treedata0_.nodes.resize(5);
@@ -382,18 +380,16 @@ TEST_F(AXTreeSerializerTest, DuplicateIdsReturnsErrorAndFlushes) {
   tree1_->GetFromId(2)->SwapChildren(&node2_children);
 
   AXTreeUpdate update;
-  ASSERT_FALSE(serializer_->SerializeChanges(tree1_->GetFromId(7), &update));
+  EXPECT_DEATH(serializer_->SerializeChanges(tree1_->GetFromId(7), &update),
+               "");
 
-  // Swap it back, fixing the tree.
+  // Swap it back, fixing the tree. Given the above crash, this is just to
+  // ensure the test can clean up properly and avoid a different failure.
   tree1_->GetFromId(2)->SwapChildren(&node2_children);
-
-  // Now try to serialize again. We should get the whole tree because the
-  // previous failed call to SerializeChanges reset it.
   update = AXTreeUpdate();
-  serializer_->SerializeChanges(tree1_->GetFromId(7), &update);
-  ASSERT_EQ(5u, update.nodes.size());
+  EXPECT_TRUE(serializer_->SerializeChanges(tree1_->GetFromId(7), &update));
 }
-#endif  // !defined(AX_FAIL_FAST_BUILD)
+#endif
 
 // If a tree serializer is reset, that means it doesn't know about
 // the state of the client tree anymore. The safest thing to do in
