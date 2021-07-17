1567abc7a6b176cc3bfda4d7734a402a04c22215
Angela Yoeurng <yoangela@chromium.org>
2021-07-17 00:39:03 +0000

    [omnibox] Clean up refined focus UI feature flag
    This CL cleans up code related to the #omnibox-refined-focus-state
    feature flag, which has been launched to 100% stable.
    Some leftover instances of "focus bar" naming have also been renamed to
    "selection indicator" for consistency.
    
    Bug: 1129612
    Change-Id: Icb06b1bbeeaa60b2e403a01e417d0724eef70433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036231
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902764}

--- components/omnibox/browser/omnibox_field_trial.h
+++ components/omnibox/browser/omnibox_field_trial.h
@@ -384,9 +384,6 @@ bool IsKeywordSearchButtonEnabled();
 // assortment of keyword mode experiments.
 bool IsExperimentalKeywordModeEnabled();
 
-// Returns true if the new focus UI is enabled.
-bool IsRefinedFocusStateEnabled();
-
 // On Device Head Suggestions feature and its helper functions.
 bool IsOnDeviceHeadSuggestEnabledForIncognito();
 bool IsOnDeviceHeadSuggestEnabledForNonIncognito();
