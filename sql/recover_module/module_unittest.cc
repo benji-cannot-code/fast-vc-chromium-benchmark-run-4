ab5f6e5e1ecc237aaea724249865bcfbd7aa43b0
Victor Costan <pwnall@chromium.org>
2021-07-17 04:11:46 +0000

    sql: Fix overly optimistic assumption in Fuchsia's VFS wrapper.
    Fuchsia's VFS wrapper assumes that xUnlock() will never be called with
    SQLITE_LOCK_SHARED after it was called with SQLITE_LOCK_NONE. This
    assumption turns out to be false.
    
    This CL uses the same assumption as our VFS implementation used for
    sandboxing the Storage Service utility process and renderer processes
    (for WebSQL).
    
    Change-Id: I849374c3468e27c8dfa5e74d67d07332e7d15b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035186
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902782}

--- sql/recover_module/module_unittest.cc
+++ sql/recover_module/module_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/scoped_temp_dir.h"
 #include "base/strings/stringprintf.h"
-#include "build/build_config.h"
 #include "sql/database.h"
 #include "sql/statement.h"
 #include "sql/test/database_test_peer.h"
@@ -37,10 +36,6 @@ class RecoverModuleTest : public testing::Test {
  protected:
   base::ScopedTempDir temp_dir_;
   sql::Database db_{sql::DatabaseOptions{
-#if defined(OS_FUCHSIA)
-      // TODO(pwnall): Figure out the vfs_wrapper_fuchsia.cc DCHECK.
-      .exclusive_locking = false,
-#endif  // defined(OS_FUCHSIA)
       .enable_virtual_tables_discouraged = true,
   }};
 };
