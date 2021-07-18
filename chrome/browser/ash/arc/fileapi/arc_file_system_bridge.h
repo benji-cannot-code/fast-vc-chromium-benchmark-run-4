cc4780f5605304d40dac919ae7916fcab889fe27
Youkichi Hosoi <youkichihosoi@chromium.org>
2021-07-18 21:15:30 +0000

    Reland "arc: Implement a new Mojo method GetLastModified() for FileSystemHost"
    This is a reland of b4ca3a9ca2826294586aa8d0a044ed5b9a0c142c
    
    Reason for reland: Originally ArcFileSystemBridgeTest.GetLastModified
    failed when unit_tests was run without DCHECK enabled. It was because
    FakeProvidedFileSystem contained a DCHECK with a side-effect, which has
    been fixed by crrev.com/c/3016484.
    
    Original change's description:
    > arc: Implement a new Mojo method GetLastModified() for FileSystemHost
    >
    > Some Android apps query the last modified timestamp of a file provided
    > by ArcFileSystemBridge.
    >
    > BUG=b:176918971
    > TEST=unit_tests --gtest_filter="ArcFileSystemBridgeTest.*"
    > TEST=ARC can get the last modified timestamp of a file via the method.
    >
    > Change-Id: Ic68642270674f08ae667216af5b81772d5d5d0c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987528
    > Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    > Reviewed-by: Risan <risan@chromium.org>
    > Commit-Queue: Youkichi Hosoi <youkichihosoi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#899834}
    
    Bug: b:176918971
    Change-Id: I4ffde1f9262db2b2bb694cfb5584d4b765e6ff15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3030902
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Youkichi Hosoi <youkichihosoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902865}

--- chrome/browser/ash/arc/fileapi/arc_file_system_bridge.h
+++ chrome/browser/ash/arc/fileapi/arc_file_system_bridge.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/mojom/file_system.mojom-forward.h"
 #include "components/arc/session/connection_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "storage/browser/file_system/file_system_operation.h"
 #include "storage/browser/file_system/watcher_manager.h"
 
 class BrowserContextKeyedServiceFactory;
@@ -86,6 +87,8 @@ class ArcFileSystemBridge
                    GetFileNameCallback callback) override;
   void GetFileSize(const std::string& url,
                    GetFileSizeCallback callback) override;
+  void GetLastModified(const GURL& url,
+                       GetLastModifiedCallback callback) override;
   void GetFileType(const std::string& url,
                    GetFileTypeCallback callback) override;
   void OnDocumentChanged(int64_t watcher_id,
@@ -120,6 +123,11 @@ class ArcFileSystemBridge
   // Used to implement GetFileSize().
   void GetFileSizeInternal(const GURL& url_decoded,
                            GetFileSizeCallback callback);
+
+  // Used to implement GetFileSize() and GetLastModified().
+  void GetMetadata(const GURL& url_decoded,
+                   int flags,
+                   storage::FileSystemOperation::GetMetadataCallback callback);
 
   // Used to implement GetVirtualFileId().
   void GetVirtualFileIdInternal(const GURL& url_decoded,
