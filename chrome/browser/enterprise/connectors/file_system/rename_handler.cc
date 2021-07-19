130bde213f91fca92caa76e5a2a87d4f02fe436d
Yiming Zhou <uwyiming@google.com>
2021-07-19 22:09:52 +0000

    Add an integration test for FSC direct upload to Box.
    This change sets up the FSC captures sites integration test framework, and adds a test for the basic "direct upload to Box.com using Single Factor Authentication" scenario.
    
    To build:
    - Add "checkout_chromium_fsc_test_dependencies: True" to .gclient
    - gclient sync
    - ninja -C out/Default -j 100 chrome/test:enterprise_connector_file_system_interactive_tests
    
    To run the test:
    out/Default/enterprise_connector_file_system_interactive_tests --test-launcher-timeout=1000000 --ui-test-action-max-timeout=1000000 --enable-pixel-output-in-tests --gtest_filter="BoxCapturedSitesInteractiveTest.SFA_DownloadSmallFileSuccess"
    
    Bug: 1214825
    Change-Id: I6405ace5693ebefe7b4cbc7adb7c33a8ea5d8e51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2993451
    Reviewed-by: Alice Gong <alicego@google.com>
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Cr-Commit-Position: refs/heads/master@{#903211}

--- chrome/browser/enterprise/connectors/file_system/rename_handler.cc
+++ chrome/browser/enterprise/connectors/file_system/rename_handler.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "chrome/browser/enterprise/connectors/file_system/access_token_fetcher.h"
 #include "chrome/browser/enterprise/connectors/file_system/box_uploader.h"
+#include "chrome/browser/enterprise/connectors/file_system/signin_dialog_delegate.h"
 #include "chrome/browser/enterprise/connectors/file_system/signin_experience.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
@@ -83,7 +84,10 @@ FileSystemRenameHandler::FileSystemRenameHandler(
     : download::DownloadItemRenameHandler(download_item),
       uploader_(BoxUploader::Create(download_item)) {}
 
-FileSystemRenameHandler::~FileSystemRenameHandler() = default;
+FileSystemRenameHandler::~FileSystemRenameHandler() {
+  for (auto& observer : observers_)
+    observer.OnDestruction();
+}
 
 void FileSystemRenameHandler::Start(ProgressUpdateCallback progress_update_cb,
                                     DownloadCallback upload_complete_cb) {
@@ -104,7 +108,8 @@ void FileSystemRenameHandler::PromptUserSignInForAuthorization(
   StartFileSystemConnectorSigninExperienceForDownloadItem(
       contents, settings_,
       base::BindOnce(&FileSystemRenameHandler::OnAuthorization,
-                     weak_factory_.GetWeakPtr()));
+                     weak_factory_.GetWeakPtr()),
+      signin_observer_);
 }
 
 void FileSystemRenameHandler::FetchAccessToken(
@@ -116,6 +121,8 @@ void FileSystemRenameHandler::FetchAccessToken(
       kOAuthConsumerName,
       base::BindOnce(&FileSystemRenameHandler::OnAccessTokenFetched,
                      weak_factory_.GetWeakPtr()));
+  for (auto& observer : observers_)
+    observer.OnFetchAccessTokenStart();
   token_fetcher_->Start(settings_.client_id, settings_.client_secret,
                         settings_.scopes);
 }
@@ -227,6 +234,9 @@ void FileSystemRenameHandler::OnAccessTokenFetched(
     const GoogleServiceAuthError& status,
     const std::string& access_token,
     const std::string& refresh_token) {
+  for (auto& observer : observers_)
+    observer.OnAccessTokenFetched(status);
+
   // Case 1d or 3c:
   const net::Error net_error = static_cast<net::Error>(status.network_error());
   if (net_error) {
@@ -284,6 +294,68 @@ void FileSystemRenameHandler::OnApiAuthenticationError() {
 
 PrefService* FileSystemRenameHandler::GetPrefs() {
   return PrefsFromDownloadItem(download_item());
+}
+
+base::WeakPtr<FileSystemRenameHandler>
+FileSystemRenameHandler::RegisterSigninObserverForTesting(
+    SigninExperienceTestObserver* observer) {
+  // The FileSystemRenameHandler should open trigger the Box signin workflow
+  // once. The observer should be set only once, otherwise one observer will
+  // forcibly detach another observer.
+  DCHECK(signin_observer_ == nullptr);
+  signin_observer_ = observer;
+  return weak_factory_.GetWeakPtr();
+}
+
+void FileSystemRenameHandler::UnregisterSigninObserverForTesting(
+    SigninExperienceTestObserver* observer) {
+  DCHECK(observer == signin_observer_);
+  signin_observer_ = nullptr;
+}
+
+// FileSystemRenameHandler::TestObserver
+FileSystemRenameHandler::TestObserver::TestObserver(
+    FileSystemRenameHandler* rename_handler)
+    : rename_handler_(rename_handler->weak_factory_.GetWeakPtr()) {
+  rename_handler->observers_.AddObserver(this);
+}
+
+FileSystemRenameHandler::TestObserver::~TestObserver() {
+  if (rename_handler_)
+    rename_handler_->observers_.RemoveObserver(this);
+}
+
+void FileSystemRenameHandler::TestObserver::OnDestruction() {
+  rename_handler_.reset();
+}
+
+// static
+BoxUploader* FileSystemRenameHandler::TestObserver::GetBoxUploader(
+    FileSystemRenameHandler* rename_handler) {
+  return rename_handler->uploader_.get();
+}
+
+// BoxFetchAccessTokenTestObserver
+BoxFetchAccessTokenTestObserver::BoxFetchAccessTokenTestObserver(
+    FileSystemRenameHandler* rename_handler)
+    : FileSystemRenameHandler::TestObserver(rename_handler) {}
+
+void BoxFetchAccessTokenTestObserver::OnFetchAccessTokenStart() {
+  status_ = Status::kInProgress;
+}
+
+void BoxFetchAccessTokenTestObserver::OnAccessTokenFetched(
+    const GoogleServiceAuthError& status) {
+  fetch_token_err_ = status;
+  status_ = Status::kSucceeded;
+  if (run_loop_.running())
+    run_loop_.Quit();
+}
+
+bool BoxFetchAccessTokenTestObserver::WaitForFetch() {
+  if (status_ != Status::kSucceeded)
+    run_loop_.Run();
+  return fetch_token_err_.state() == GoogleServiceAuthError::State::NONE;
 }
 
 }  // namespace enterprise_connectors
