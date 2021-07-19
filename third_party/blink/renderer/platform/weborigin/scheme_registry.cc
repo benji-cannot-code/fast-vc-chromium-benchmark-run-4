3ad57f5adf6f059e4ae724a4bb02a315da21d86f
Joe Mason <joenotcharles@google.com>
2021-07-19 18:16:41 +0000

    Make URLSchemesRegistry::GetInstance cross-thread-safe
    The class comment says "URLSchemesRegistry is accessed from multiple
    threads" but trying to actually do so will hit a DCHECK
    "IsNotRacy(allow_cross_thread_use)" in DEFINE_STATIC_LOCAL, even if
    GetMutableURLSchemesRegistry is called before creating any threads as
    the comment suggests.
    
    Bug: 1225936
    Change-Id: I62818793f41fbd5457cdddf4db5e7f4c16fe0a58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033361
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903099}

--- third_party/blink/renderer/platform/weborigin/scheme_registry.cc
+++ third_party/blink/renderer/platform/weborigin/scheme_registry.cc
@@ -114,7 +114,7 @@ class URLSchemesRegistry final {
   friend URLSchemesRegistry& GetMutableURLSchemesRegistryForTest();
 
   static URLSchemesRegistry& GetInstance() {
-    DEFINE_STATIC_LOCAL(URLSchemesRegistry, schemes, ());
+    DEFINE_THREAD_SAFE_STATIC_LOCAL(URLSchemesRegistry, schemes, ());
     return schemes;
   }
 };
