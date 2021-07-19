bacc2632c71c45e7e3fe91f107b48e1d96f64903
Tamir Duberstein <tamird@google.com>
2021-07-19 14:54:50 +0000

    [fuchsia] s/fit::result/fpromise::result/g
    fit::result is deprecated in favor of fpromise::result. Without this
    change, https://fxrev.dev/555757 causes the Chromium build to fail by
    removing the legacy header from this file's transitive includes.
    
    Bug: 80976, fuchsia:78633
    Change-Id: Ifd258e6b2f16339964088759ac7800cd80f02140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3038681
    Commit-Queue: Tamir Duberstein <tamird@google.com>
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Tamir Duberstein <tamird@google.com>
    Cr-Commit-Position: refs/heads/master@{#902998}

--- media/fuchsia/cdm/service/fuchsia_cdm_manager.cc
+++ media/fuchsia/cdm/service/fuchsia_cdm_manager.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fuchsia/media/drm/cpp/fidl.h>
 #include <lib/fidl/cpp/binding_set.h>
+#include <lib/fpromise/promise.h>
 
 #include "base/bind.h"
 #include "base/callback.h"
@@ -217,8 +218,8 @@ class FuchsiaCdmManager::KeySystemClient {
 
     key_system_->AddDataStore(
         data_store_id, std::move(params),
-        [this, data_store_id,
-         storage_path](fit::result<void, fuchsia::media::drm::Error> result) {
+        [this, data_store_id, storage_path](
+            fpromise::result<void, fuchsia::media::drm::Error> result) {
           if (result.is_error()) {
             DLOG(ERROR) << "Failed to add data store " << data_store_id
                         << ", path: " << storage_path;
