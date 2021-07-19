51c5892a34fb716f961eeb72ab55363db8e536b7
Maks Orlovich <morlovich@chromium.org>
2021-07-19 17:28:19 +0000

    SimpleCache: audit expiring histograms
    CreationToIndex[Fail]: keep, since these are quite important for non-HTTP uses which
      hit those a lot.
    
    Disk* : keep for now, I looked at them and was surprised.
    EntryCreationTime: keep, critical performance metric.
    
    Eviction stuff: only keep Time* and EntryCount. Everything else is basically
    overlaps sizing on init, and that's less frequent.
    
    HeaderSize: keep, architectural sanity check
    
    Index:
      IndexCreatedEntryCount: keep, surprising
      IndexNumEntriesOnInit: keep, size metrics.
      IndexNumEntriesOnWrite: drop, on init is probably enough.
      PercentFullOnInit: drop.
      IndexWriteReason, IndexWriteReasonAtLoad: drop.
          (write reason for stale load might be more interesting?)
      Index load state, staleness stuff: keep, staleness still an issue.
    
    FileDescriptorLimiterAction: keep, but pretty marginal; but it's newish and rare
    OpenEntryIndexState: keep as hit rate.
    
    Sync*: convert to local histograms.
    
    Bug: 1209974
    Change-Id: I763b9444326a2fa8ec6d2cdd9275b7ef0ee56d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024587
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903072}

--- net/disk_cache/simple/simple_synchronous_entry.cc
+++ net/disk_cache/simple/simple_synchronous_entry.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_macros_local.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/timer/elapsed_timer.h"
@@ -41,24 +42,23 @@ namespace {
 
 void RecordSyncOpenResult(net::CacheType cache_type, OpenEntryResult result) {
   DCHECK_LT(result, OPEN_ENTRY_MAX);
-  SIMPLE_CACHE_UMA(ENUMERATION,
-                   "SyncOpenResult", cache_type, result, OPEN_ENTRY_MAX);
+  SIMPLE_CACHE_LOCAL(ENUMERATION, "SyncOpenResult", cache_type, result,
+                     OPEN_ENTRY_MAX);
 }
 
 void RecordWriteResult(net::CacheType cache_type, SyncWriteResult result) {
-  SIMPLE_CACHE_UMA(ENUMERATION, "SyncWriteResult", cache_type, result,
-                   SYNC_WRITE_RESULT_MAX);
+  SIMPLE_CACHE_LOCAL(ENUMERATION, "SyncWriteResult", cache_type, result,
+                     SYNC_WRITE_RESULT_MAX);
 }
 
 void RecordCheckEOFResult(net::CacheType cache_type, CheckEOFResult result) {
-  SIMPLE_CACHE_UMA(ENUMERATION,
-                   "SyncCheckEOFResult", cache_type,
-                   result, CHECK_EOF_RESULT_MAX);
+  SIMPLE_CACHE_LOCAL(ENUMERATION, "SyncCheckEOFResult", cache_type, result,
+                     CHECK_EOF_RESULT_MAX);
 }
 
 void RecordCloseResult(net::CacheType cache_type, CloseResult result) {
-  SIMPLE_CACHE_UMA(ENUMERATION,
-                   "SyncCloseResult", cache_type, result, CLOSE_RESULT_MAX);
+  SIMPLE_CACHE_LOCAL(ENUMERATION, "SyncCloseResult", cache_type, result,
+                     CLOSE_RESULT_MAX);
 }
 
 void RecordOpenPrefetchMode(net::CacheType cache_type, OpenPrefetchMode mode) {
@@ -67,7 +67,7 @@ void RecordOpenPrefetchMode(net::CacheType cache_type, OpenPrefetchMode mode) {
 }
 
 void RecordDiskCreateLatency(net::CacheType cache_type, base::TimeDelta delay) {
-  SIMPLE_CACHE_UMA(TIMES, "DiskCreateLatency", cache_type, delay);
+  SIMPLE_CACHE_LOCAL(TIMES, "DiskCreateLatency", cache_type, delay);
 }
 
 bool CanOmitEmptyFile(int file_index) {
@@ -1162,9 +1162,8 @@ bool SimpleSynchronousEntry::OpenFiles(SimpleEntryStat* out_entry_stat) {
 
     if (!MaybeOpenFile(i, &error)) {
       RecordSyncOpenResult(cache_type_, OPEN_ENTRY_PLATFORM_FILE_ERROR);
-      SIMPLE_CACHE_UMA(ENUMERATION,
-                       "SyncOpenPlatformFileError", cache_type_,
-                       -error, -base::File::FILE_ERROR_MAX);
+      SIMPLE_CACHE_LOCAL(ENUMERATION, "SyncOpenPlatformFileError", cache_type_,
+                         -error, -base::File::FILE_ERROR_MAX);
       while (--i >= 0)
         CloseFile(i);
       return false;
@@ -1217,9 +1216,8 @@ bool SimpleSynchronousEntry::CreateFiles(SimpleEntryStat* out_entry_stat) {
   for (int i = 0; i < kSimpleEntryNormalFileCount; ++i) {
     base::File::Error error;
     if (!MaybeCreateFile(i, FILE_NOT_REQUIRED, &error)) {
-      SIMPLE_CACHE_UMA(ENUMERATION,
-                       "SyncCreatePlatformFileError", cache_type_,
-                       -error, -base::File::FILE_ERROR_MAX);
+      SIMPLE_CACHE_LOCAL(ENUMERATION, "SyncCreatePlatformFileError",
+                         cache_type_, -error, -base::File::FILE_ERROR_MAX);
       while (--i >= 0)
         CloseFile(i);
       return false;
