186421d31f813b07673d9eee61459ef552ef6ea5
Vincent Boisselle <vincb@google.com>
2021-07-19 15:32:32 +0000

    🐻[Pandora] Force portrait for new modal FRE on small devices.
    Force portrait orientation for FRE on small screens such as iPhone
    and iPod.
    
    IPhone - Before: https://drive.google.com/file/d/19ZAG2pmjUAPV2aSnX_EFFdfxonnS-uUY/view?usp=sharing&resourcekey=0-NwmKFsLnBUwOiQMczM0UZw
    Iphone - After: https://drive.google.com/file/d/17xJJ4zj41n5V2Qu4c-mUPi8ACgS7JYNf/view?usp=sharing&resourcekey=0-0OQTdR01ENfgr7uw8iPawA
    
    IPad - Before: https://drive.google.com/file/d/1FDKc_R_h_5YrKErTUXXg6DtYy8dpJBA6/view?usp=sharing&resourcekey=0-mCrIc46hKA2uP2WzcVeF6Q
    IPad - After: https://drive.google.com/file/d/1I91jz9NHli8E-8mjQZKP--yo82_rl_SJ/view?usp=sharing&resourcekey=0-0hbGQhPC-NgpFb8oIwTvGw
    
    As discussed in crrev.com/c/3015302, since the new FRE is presented
    as a modal, the portrait orientation has to be forced to the entire app
    to force portrait for the FRE modal.
    
    Credits to tinazwang@ who did the original CL.
    
    Change-Id: Ice9258cf49679342074908f2f20068b5215772da
    Bug: 1214745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024590
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903010}

--- ios/chrome/app/application_delegate/app_state.mm
+++ ios/chrome/app/application_delegate/app_state.mm
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/thread/web_task_traits.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
+#include "ui/base/device_form_factor.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -220,6 +221,16 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
   [self.observers appState:self willTransitionToInitStage:newInitStage];
   _initStage = newInitStage;
   [self.observers appState:self didTransitionFromInitStage:previousInitStage];
+}
+
+- (BOOL)portraitOnly {
+  if (ui::GetDeviceFormFactor() != ui::DEVICE_FORM_FACTOR_PHONE) {
+    return NO;
+  }
+
+  // Return YES if the First Run UI is showing.
+  return self.initStage == InitStageFirstRun &&
+         self.startupInformation.isFirstRun;
 }
 
 #pragma mark - Public methods.
