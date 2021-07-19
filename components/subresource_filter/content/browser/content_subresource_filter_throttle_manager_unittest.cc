dd7818180a1c93e742099d3f27ef15cd1b2e4de5
Alex Turner <alexmt@chromium.org>
2021-07-19 16:50:57 +0000

    Fix typo in ContentSubresourceFilterThrottleManagerTest test name
    Bug: 1225350
    Change-Id: I0a98399977a6e12f5091581822502900f9925438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036086
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903039}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager_unittest.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager_unittest.cc
@@ -1378,7 +1378,7 @@ TEST_P(
 }
 
 TEST_P(ContentSubresourceFilterThrottleManagerTest,
-       ReadyToCommitNavigationThenRenderFrameDeletes_MetricsNotRecoded) {
+       ReadyToCommitNavigationThenRenderFrameDeletes_MetricsNotRecorded) {
   NavigateAndCommitMainFrame(GURL(kTestURLWithDryRun));
   ExpectActivationSignalForFrame(main_rfh(), true /* expect_activation */);
 
