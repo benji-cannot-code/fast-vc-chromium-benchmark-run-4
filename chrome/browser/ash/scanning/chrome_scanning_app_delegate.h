c1a56f273c78d3aeec7bb2ee5b1646ffd3173023
Gavin Williams <gavinwill@chromium.org>
2021-07-19 17:48:19 +0000

    scanning: Move file path checking to separate class
    -Create a separate class for checking if a file path is supported for
     scanning. The new class allows us to consolidate the logic
    
    -Change to using a single test setter function SetFilesPathsForTesting()
     to ease test setup.
    
    Bug: 1216011
    Change-Id: I991d557fd57b9e1bf182b1860abdd92f227154fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3029485
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903082}

--- chrome/browser/ash/scanning/chrome_scanning_app_delegate.h
+++ chrome/browser/ash/scanning/chrome_scanning_app_delegate.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/scanning/scanning_app_delegate.h"
 #include "base/files/file_path.h"
+#include "chrome/browser/ash/scanning/scanning_file_path_helper.h"
 
 class PrefService;
 
@@ -52,11 +53,10 @@ class ChromeScanningAppDelegate : public ScanningAppDelegate {
   // Register scan settings prefs.
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
-  // Sets |google_drive_path_| for tests.
-  void SetGoogleDrivePathForTesting(const base::FilePath& google_drive_path);
-
-  // Sets |my_files_path_| for tests.
-  void SetMyFilesPathForTesting(const base::FilePath& my_files_path);
+  // Initializes ScanningFilePathHelper with |google_drive_path_| and
+  // |my_files_path_|.
+  void SetValidPaths(const base::FilePath& google_drive_path,
+                     const base::FilePath& my_files_path);
 
  private:
   // Returns the PrefService for the active Profile.
@@ -64,9 +64,8 @@ class ChromeScanningAppDelegate : public ScanningAppDelegate {
 
   content::WebUI* web_ui_;  // Owns |this|.
 
-  // The paths to the user's My files and Google Drive directories.
-  base::FilePath google_drive_path_;
-  base::FilePath my_files_path_;
+  // Helper class for for file path manipulation and verification.
+  ScanningFilePathHelper file_path_helper_;
 };
 
 }  // namespace ash
--- chrome/browser/ash/scanning/scan_service_unittest.cc
+++ chrome/browser/ash/scanning/scan_service_unittest.cc
@@ -268,12 +268,18 @@ class ScanServiceTest : public testing::Test {
     const AccountId account_id(AccountId::FromUserEmail(kUserEmail));
     user_manager_->AddUser(account_id);
     user_manager_->LoginUser(account_id);
-    scan_service_ = std::make_unique<ScanService>(
-        &fake_lorgnette_scanner_manager_, base::FilePath(), base::FilePath(),
-        profile_);
+    SetupScanService(scanned_files_mount_->GetRootPath(),
+                     base::FilePath("/google/drive"));
   }
 
-  void SetUp() override {
+  void SetupScanService(base::FilePath my_files_path,
+                        base::FilePath google_drive_path) {
+    scan_service_ = std::make_unique<ScanService>(
+        &fake_lorgnette_scanner_manager_, my_files_path, google_drive_path,
+        profile_);
+    if (scan_service_remote_.is_bound()) {
+      scan_service_remote_.reset();
+    }
     scan_service_->BindInterface(
         scan_service_remote_.BindNewPipeAndPassReceiver());
   }
@@ -433,9 +439,9 @@ TEST_F(ScanServiceTest, ScanWithUnsupportedFilePath) {
   ASSERT_EQ(scanners.size(), 1u);
 
   const base::FilePath my_files_path(kMyFilesPath);
-  scan_service_->SetMyFilesPathForTesting(my_files_path);
   const mojo_ipc::ScanSettings settings = CreateScanSettings(
       my_files_path.Append("../../../var/log"), mojo_ipc::FileType::kPng);
+  SetupScanService(my_files_path, base::FilePath("/google/drive"));
   EXPECT_FALSE(StartScan(scanners[0]->id, settings.Clone()));
 }
 
@@ -456,7 +462,6 @@ TEST_F(ScanServiceTest, Scan) {
   base::HistogramTester histogram_tester;
   int num_single_file_scans = 0u;
   int num_multi_file_scans = 0u;
-  scan_service_->SetMyFilesPathForTesting(scanned_files_mount_->GetRootPath());
   for (int type_num = static_cast<int>(mojo_ipc::FileType::kMinValue);
        type_num <= static_cast<int>(mojo_ipc::FileType::kMaxValue);
        ++type_num) {
@@ -510,7 +515,6 @@ TEST_F(ScanServiceTest, RotateEpsonADF) {
   // Since we're using mock time, this is deterministic.
   base::Time::Now().LocalExplode(&scan_time);
 
-  scan_service_->SetMyFilesPathForTesting(scanned_files_mount_->GetRootPath());
   mojo_ipc::ScanSettings settings =
       CreateScanSettings(scanned_files_mount_->GetRootPath(),
                          mojo_ipc::FileType::kPdf, "ADF Duplex");
@@ -535,7 +539,6 @@ TEST_F(ScanServiceTest, ScanFails) {
   auto scanners = GetScanners();
   ASSERT_EQ(scanners.size(), 1u);
 
-  scan_service_->SetMyFilesPathForTesting(scanned_files_mount_->GetRootPath());
   const mojo_ipc::ScanSettings settings = CreateScanSettings(
       scanned_files_mount_->GetRootPath(), mojo_ipc::FileType::kPng);
 
@@ -555,7 +558,6 @@ TEST_F(ScanServiceTest, ScanAfterFailedScan) {
   auto scanners = GetScanners();
   ASSERT_EQ(scanners.size(), 1u);
 
-  scan_service_->SetMyFilesPathForTesting(scanned_files_mount_->GetRootPath());
   const mojo_ipc::ScanSettings settings = CreateScanSettings(
       scanned_files_mount_->GetRootPath(), mojo_ipc::FileType::kPng);
 
@@ -604,7 +606,6 @@ TEST_F(ScanServiceTest, FailedScanAfterSuccessfulScan) {
   // Since we're using mock time, this is deterministic.
   base::Time::Now().LocalExplode(&scan_time);
 
-  scan_service_->SetMyFilesPathForTesting(scanned_files_mount_->GetRootPath());
   const mojo_ipc::ScanSettings settings = CreateScanSettings(
       scanned_files_mount_->GetRootPath(), mojo_ipc::FileType::kPng);
   const std::vector<base::FilePath> saved_scan_paths =
@@ -642,7 +643,6 @@ TEST_F(ScanServiceTest, CancelScanBeforeScanCompletes) {
   auto scanners = GetScanners();
   ASSERT_EQ(scanners.size(), 1u);
 
-  scan_service_->SetMyFilesPathForTesting(scanned_files_mount_->GetRootPath());
   const mojo_ipc::ScanSettings settings = CreateScanSettings(
       scanned_files_mount_->GetRootPath(), mojo_ipc::FileType::kPng);
 
@@ -674,7 +674,6 @@ TEST_F(ScanServiceTest, HoldingSpaceScan) {
   size_t num_items_in_holding_space = 0u;
   ASSERT_EQ(num_items_in_holding_space, holding_space_model->items().size());
 
-  scan_service_->SetMyFilesPathForTesting(scanned_files_mount_->GetRootPath());
   for (int type_num = static_cast<int>(mojo_ipc::FileType::kMinValue);
        type_num <= static_cast<int>(mojo_ipc::FileType::kMaxValue);
        ++type_num) {
