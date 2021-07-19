611917d9a0edccb2eb0bdc5350c75ef92ee1978c
Bartek Nowierski <bartekn@chromium.org>
2021-07-19 15:16:11 +0000

    [BRP] Check that valid BRP pool pointers are outside of the first partition page
    This check existed before, in a simplified form, but had to be disabled,
    because that wouldn't work when direct map is supported. This CL
    implements a more complex version of this check, that works with direct map
    support.
    
    Change-Id: I6fcb72b6168e97db4b13bcaaa628f252adca9d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3023707
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#903005}

--- base/memory/checked_ptr.cc
+++ base/memory/checked_ptr.cc
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/allocator/partition_allocator/partition_ref_count.h"
 #include "base/allocator/partition_allocator/partition_root.h"
+#include "base/allocator/partition_allocator/reservation_offset_table.h"
 #include "base/check.h"
+#include "base/dcheck_is_on.h"
 
 namespace base {
 
@@ -42,6 +44,20 @@ bool BackupRefPtrImpl::IsPointeeAlive(void* ptr) {
 bool BackupRefPtrImpl::IsValidDelta(void* ptr, ptrdiff_t delta) {
   return PartitionAllocIsValidPtrDelta(ptr, delta);
 }
+
+#if DCHECK_IS_ON() || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
+void CheckThatAddressIsntWithinFirstPartitionPage(void* ptr) {
+  if (IsManagedByDirectMap(ptr)) {
+    uintptr_t reservation_start = GetDirectMapReservationStart(ptr);
+    CHECK(reinterpret_cast<uintptr_t>(ptr) - reservation_start >=
+          PartitionPageSize());
+  } else {
+    CHECK(IsManagedByNormalBuckets(ptr));
+    CHECK(reinterpret_cast<uintptr_t>(ptr) % kSuperPageSize >=
+          PartitionPageSize());
+  }
+}
+#endif  // DCHECK_IS_ON() || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
 
 }  // namespace internal
 
--- base/memory/checked_ptr.h
+++ base/memory/checked_ptr.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/buildflags.h"
 #include "base/check.h"
 #include "base/compiler_specific.h"
+#include "base/dcheck_is_on.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
 
@@ -27,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc_config.h"
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/base_export.h"
-#include "base/dcheck_is_on.h"
 #endif  // BUILDFLAG(USE_BACKUP_REF_PTR)
 
 namespace base {
@@ -92,6 +92,10 @@ struct CheckedPtrNoOpImpl {
 
 #if BUILDFLAG(USE_BACKUP_REF_PTR)
 
+#if DCHECK_IS_ON() || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
+void CheckThatAddressIsntWithinFirstPartitionPage(void* ptr);
+#endif
+
 struct BackupRefPtrImpl {
   // Note that `BackupRefPtrImpl` itself is not thread-safe. If multiple threads
   // modify the same smart pointer object without synchronization, a data race
@@ -136,15 +140,10 @@ struct BackupRefPtrImpl {
     // This allows us to make a stronger assertion that if
     // IsManagedByPartitionAllocBRPPool returns true for a valid pointer,
     // it must be at least partition page away from the beginning of a super
-    // page. This, however, can't be easily checked for direct maps, where a
-    // pointer on a consecutive super page may easily land in its first
-    // partition page.
-    // TODO(bartekn): Keep the assert for non-DirectMap as well as for the
-    // first page of DirectMap allocations.
-#if 0
+    // page.
+#if DCHECK_IS_ON() || BUILDFLAG(ENABLE_BACKUP_REF_PTR_SLOW_CHECKS)
     if (ret) {
-      DCHECK(reinterpret_cast<uintptr_t>(ptr) % kSuperPageSize >=
-             PartitionPageSize());
+      CheckThatAddressIsntWithinFirstPartitionPage(ptr);
     }
 #endif
 
