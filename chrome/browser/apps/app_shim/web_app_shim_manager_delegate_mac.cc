05772f7117323089654f9aa2d178e72696bdac03
Mike Jackson <mjackson@microsoft.com>
2021-07-19 19:39:17 +0000

    dpwas: Close the PWA if protocol handling launch is cancelled
    If the user cancels a protocol launch, then previously the app was still
    running the background. This change updates the behavior so that the
    app closes, if no browser instance is associated with the app.
    
    Testing:
    - Enable enable-desktop-pwas-protocol-handling
    - Install http://fabiorocha.github.io/pwa
    - From cmd run "open web+github://testvalue"
    - Observe the dialog and select "Cancel".
    - The app should close (but the browser instance remains open)
    
    If you already have the app running, the app should continue  to
    remain running, and should not close down.
    
    Bug: 1215255
    Change-Id: I35b2e3d316d251931f4bc87491158866664b006f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3018567
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Fabio Rocha <fabio.rocha@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#903137}

--- chrome/browser/apps/app_shim/web_app_shim_manager_delegate_mac.cc
+++ chrome/browser/apps/app_shim/web_app_shim_manager_delegate_mac.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/browser_app_launcher.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/web_applications/components/app_registry_controller.h"
 #include "chrome/browser/web_applications/components/os_integration_manager.h"
@@ -206,6 +207,9 @@ void WebAppShimManagerDelegate::LaunchApp(
               apps::AppServiceProxyFactory::GetForProfile(profile)
                   ->BrowserAppLauncher()
                   ->LaunchAppWithParams(std::move(params));
+            } else {
+              apps::AppShimManager::Get()->OnAppLaunchCancelled(profile,
+                                                                params.app_id);
             }
           },
           std::move(params), profile);
