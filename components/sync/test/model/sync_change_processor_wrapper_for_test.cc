6be9a1177c7d9415ab9294fd7640123674a6cba0
Mikel Astiz <mastiz@chromium.org>
2021-07-19 12:40:00 +0000

    [sync] Delete SyncChangeProcessor::GetAllData()
    It's not used in production code and the only unittest that relies
    on this functionality can implement the functionality themselves.
    
    Change-Id: Ieed1994d0c678b4b81737add4b6abef40d781e67
    Bug: 1152824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035768
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902967}

--- components/sync/test/model/sync_change_processor_wrapper_for_test.cc
+++ components/sync/test/model/sync_change_processor_wrapper_for_test.cc
@@ -35,10 +35,4 @@ SyncChangeProcessorWrapperForTest::ProcessSyncChanges(
   return process_sync_changes_.Run(from_here, change_list);
 }
 
-SyncDataList SyncChangeProcessorWrapperForTest::GetAllSyncData(
-    ModelType type) const {
-  NOTREACHED();
-  return SyncDataList();
-}
-
 }  // namespace syncer
