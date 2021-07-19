ab9d043de6ae6b87fa1ae28a9017c5df47fa08fb
Chad Duffin <chadduffin@chromium.org>
2021-07-19 15:50:07 +0000

    [CrOS Cellular] Add per-network allow roaming ONC<=>Shill.
    This change adds support for per-network allow roaming translation
    between ONC and Shill. Instead of guarding the translation behind
    feature flags we instead translate all values that exist, and will
    instead either use or not use the provided values in the UI.
    
    Bug: 1220691
    Test: Executed unit tests locally.
    Change-Id: Ia4704c0d6f6d55b68d29ca4e9fca346352f54656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036442
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903016}

--- chromeos/network/onc/onc_translation_tables.cc
+++ chromeos/network/onc/onc_translation_tables.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstddef>
 
-#include "ash/constants/ash_features.h"
-#include "base/feature_list.h"
 #include "base/logging.h"
 #include "chromeos/network/network_type_pattern.h"
 #include "chromeos/network/tether_constants.h"
@@ -216,6 +214,7 @@ const FieldTranslationEntry cellular_fields[] = {
     {::onc::cellular::kActivationType, shill::kActivationTypeProperty},
     // This field is converted during translation, see onc_translator_*.
     // { ::onc::cellular::kActivationState, shill::kActivationStateProperty},
+    {::onc::cellular::kAllowRoaming, shill::kCellularAllowRoamingProperty},
     {::onc::cellular::kAutoConnect, shill::kAutoConnectProperty},
     {::onc::cellular::kEID, shill::kEidProperty},
     {::onc::cellular::kICCID, shill::kIccidProperty},
@@ -423,11 +422,7 @@ const StringTranslationEntry kOpenVpnCompressionAlgorithmTable[] = {
 const FieldTranslationEntry kCellularDeviceTable[] = {
     // This field is converted during translation, see onc_translator_*.
     // { ::onc::cellular::kAPNList, shill::kCellularApnListProperty},
-    {::onc::cellular::kAllowRoaming,
-     base::FeatureList::IsEnabled(
-         ash::features::kCellularAllowPerNetworkRoaming)
-         ? shill::kCellularPolicyAllowRoamingProperty
-         : shill::kCellularAllowRoamingProperty},
+    {::onc::cellular::kAllowRoaming, shill::kCellularAllowRoamingProperty},
     {::onc::cellular::kESN, shill::kEsnProperty},
     {::onc::cellular::kFamily, shill::kTechnologyFamilyProperty},
     {::onc::cellular::kFirmwareRevision, shill::kFirmwareRevisionProperty},
