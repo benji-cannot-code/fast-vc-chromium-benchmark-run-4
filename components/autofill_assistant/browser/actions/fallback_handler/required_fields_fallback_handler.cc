161bac63e6c119fd30b8dadcf68823840c6c4927
sandromaggi <sandromaggi@google.com>
2021-07-19 10:23:24 +0000

    [Autofill Assistant] Remove set_form_field_value_action
    This is deprecated and replaced by mini-actions.
    
    This change also cleans up the action_delegate_util a bit, removing
    now unused functionality and unifying the remaining uses (mainly in
    RequiredField handling).
    
    Bug: b/191940103
    Change-Id: I4392ccd0a808a45dec2a300b6ffa8206bc8d4d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2982021
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#902938}

--- components/autofill_assistant/browser/actions/fallback_handler/required_fields_fallback_handler.cc
+++ components/autofill_assistant/browser/actions/fallback_handler/required_fields_fallback_handler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/batch_element_checker.h"
 #include "components/autofill_assistant/browser/client_status.h"
 #include "components/autofill_assistant/browser/field_formatter.h"
+#include "components/autofill_assistant/browser/web/element_action_util.h"
 #include "components/autofill_assistant/browser/web/element_finder.h"
 #include "components/autofill_assistant/browser/web/web_controller.h"
 #include "third_party/re2/src/re2/re2.h"
@@ -424,12 +425,16 @@ void RequiredFieldsFallbackHandler::FillJsDrivenDropdown(
     // default: TAP
     click_type = ClickType::TAP;
   }
-  action_delegate_util::ClickOrTapElement(
-      action_delegate_, required_field.selector, click_type,
+  action_delegate_->FindElement(
+      required_field.selector,
       base::BindOnce(
-          &RequiredFieldsFallbackHandler::OnClickOrTapFallbackElement,
-          weak_ptr_factory_.GetWeakPtr(), re2_value, case_sensitive,
-          required_field, std::move(set_next_field)));
+          &element_action_util::TakeElementAndPerform,
+          base::BindOnce(&action_delegate_util::PerformClickOrTapElement,
+                         action_delegate_, click_type),
+          base::BindOnce(
+              &RequiredFieldsFallbackHandler::OnClickOrTapFallbackElement,
+              weak_ptr_factory_.GetWeakPtr(), re2_value, case_sensitive,
+              required_field, std::move(set_next_field))));
 }
 
 void RequiredFieldsFallbackHandler::OnClickOrTapFallbackElement(
@@ -482,12 +487,16 @@ void RequiredFieldsFallbackHandler::OnShortWaitForElement(
     // default: TAP
     click_type = ClickType::TAP;
   }
-  action_delegate_util::ClickOrTapElement(
-      action_delegate_, selector_to_click, click_type,
-      base::BindOnce(&RequiredFieldsFallbackHandler::OnSetFallbackFieldValue,
-                     weak_ptr_factory_.GetWeakPtr(), required_field,
-                     std::move(set_next_field),
-                     /* element= */ nullptr));
+  action_delegate_->FindElement(
+      selector_to_click,
+      base::BindOnce(
+          &element_action_util::TakeElementAndPerform,
+          base::BindOnce(&action_delegate_util::PerformClickOrTapElement,
+                         action_delegate_, click_type),
+          base::BindOnce(
+              &RequiredFieldsFallbackHandler::OnSetFallbackFieldValue,
+              weak_ptr_factory_.GetWeakPtr(), required_field,
+              std::move(set_next_field), /* element= */ nullptr)));
 }
 
 void RequiredFieldsFallbackHandler::OnSetFallbackFieldValue(
--- components/autofill_assistant/browser/protocol_utils.cc
+++ components/autofill_assistant/browser/protocol_utils.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/actions/save_generated_password_action.h"
 #include "components/autofill_assistant/browser/actions/select_option_action.h"
 #include "components/autofill_assistant/browser/actions/set_attribute_action.h"
-#include "components/autofill_assistant/browser/actions/set_form_field_value_action.h"
 #include "components/autofill_assistant/browser/actions/set_persistent_ui_action.h"
 #include "components/autofill_assistant/browser/actions/set_touchable_area_action.h"
 #include "components/autofill_assistant/browser/actions/show_cast_action.h"
@@ -203,8 +202,6 @@ std::unique_ptr<Action> ProtocolUtils::CreateAction(ActionDelegate* delegate,
       return std::make_unique<ShowDetailsAction>(delegate, action);
     case ActionProto::ActionInfoCase::kCollectUserData:
       return std::make_unique<CollectUserDataAction>(delegate, action);
-    case ActionProto::ActionInfoCase::kSetFormValue:
-      return std::make_unique<SetFormFieldValueAction>(delegate, action);
     case ActionProto::ActionInfoCase::kShowProgressBar:
       return std::make_unique<ShowProgressBarAction>(delegate, action);
     case ActionProto::ActionInfoCase::kSetAttribute:
