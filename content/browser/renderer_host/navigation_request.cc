b88732cf754bd0ec80faa6d5e6f724cea303a241
Kenichi Ishibashi <bashi@chromium.org>
2021-07-19 09:03:41 +0000

    Early Hints: Don't create URLLoaderFactory when process is shutting down
    A BrowserContext associated with a RenderProcessHost is cleared when the
    process is shutting down. Creating an appropriate URLLoaderFactory for
    Early Hints requires a valid BrowserContext.
    
    Bug: 1230273
    Change-Id: I55e44822eaf9f6b3c3742d0cc45a8e07a0f1c22b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035112
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#902918}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -3232,7 +3232,8 @@ void NavigationRequest::OnRequestFailed(
       status.should_collapse_initiator /* collapse_frame */);
 }
 
-url::Origin NavigationRequest::CreateURLLoaderFactoryForEarlyHintsPreload(
+absl::optional<url::Origin>
+NavigationRequest::CreateURLLoaderFactoryForEarlyHintsPreload(
     mojo::PendingReceiver<network::mojom::URLLoaderFactory> factory_receiver,
     const network::mojom::EarlyHints& early_hints) {
   // Early Hints preloads should happen only before the final response is
@@ -3247,6 +3248,10 @@ url::Origin NavigationRequest::CreateURLLoaderFactoryForEarlyHintsPreload(
   RenderProcessHost* process = frame_tree_node_->render_manager()
                                    ->GetSiteInstanceForNavigationRequest(this)
                                    ->GetProcess();
+
+  // The process is shutting down.
+  if (!process->GetBrowserContext())
+    return absl::nullopt;
 
   // Compute sandbox flags. Currently just inherit from the frame.
   // TODO(crbug.com/1225556): Think about the right way the specification should
