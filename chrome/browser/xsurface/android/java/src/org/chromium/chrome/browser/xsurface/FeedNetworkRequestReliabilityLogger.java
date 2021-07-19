0b0e06be7471cadfbdd4bfad0b66ab536485cecd
Ian Wells <iwells@chromium.org>
2021-07-19 21:33:57 +0000

    Feed launch reliability logging: handle feed tab switching, and add some web feed launch end cases
    Bug: 1195940
    Change-Id: I3caec5fda8b1716e5e1d0101dfda46a75149052f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3025775
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903201}

--- chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/FeedNetworkRequestReliabilityLogger.java
+++ chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/FeedNetworkRequestReliabilityLogger.java
@@ -13,16 +13,22 @@ package org.chromium.chrome.browser.xsurface;
  */
 public interface FeedNetworkRequestReliabilityLogger {
     /**
-     * Log when making a feed query request.
+     * Log before filling out and serializing a feed query request. Starts the network request flow.
      * @param timestamp Event time.
      */
     default void logFeedQueryRequestStart(long timestamp) {}
 
     /**
-     * Log just before making a feed actions upload request.
+     * Log before filling out and serializing a feed actions upload request. Starts the network
+     * request flow.
      * @param timestamp Event time.
      */
     default void logActionsUploadRequestStart(long timestamp) {}
+
+    /**
+     * Log before filling out and serializing a web feed request. Starts the network request flow.
+     */
+    default void logWebFeedRequestStart(long timestamp) {}
 
     /**
      * Log after the request has been sent.
@@ -41,7 +47,7 @@ public interface FeedNetworkRequestReliabilityLogger {
 
     /**
      * Log after logResponseReceived() if there's a network error, or after parsing the response
-     * otherwise.
+     * otherwise. Ends the network request flow.
      * @param timestamp Event time.
      * @param canonicalStatus Network request status code. See
      *         //third_party/abseil-cpp/absl/status/status.h.
