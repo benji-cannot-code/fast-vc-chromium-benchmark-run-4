05d528d7af698b31cd5df6656d6e6b48fb0f2675
David Tseng <dtseng@google.com>
2021-07-19 19:18:27 +0000

    Simplify TtsEngineExtensionApiChromeOS::Play signature
    Also, speculatively fixes invalid EventRouter access within this function.
    
    R=leileilei@google.com
    
    Fixed: 1230754
    Test: none
    Change-Id: I427c60ba198dbf573261534a87efde0d3e730eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035421
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903129}

--- chrome/browser/speech/extension_api/tts_engine_extension_api_chromeos.h
+++ chrome/browser/speech/extension_api/tts_engine_extension_api_chromeos.h
@@ -19,10 +19,6 @@ namespace content {
 class BrowserContext;
 }
 
-namespace extensions {
-class EventRouter;
-}
-
 // TtsEngineDelegate implementation used by TtsController on Chrome OS.
 class TtsExtensionEngineChromeOS
     : public TtsExtensionEngine,
@@ -68,8 +64,7 @@ class TtsExtensionEngineChromeOS
                                              Profile* profile);
 
   // Helper to start audio playback.
-  void Play(extensions::EventRouter* event_router,
-            std::unique_ptr<base::ListValue> args,
+  void Play(std::unique_ptr<base::ListValue> args,
             const std::string& engine_id,
             Profile* profile);
 
