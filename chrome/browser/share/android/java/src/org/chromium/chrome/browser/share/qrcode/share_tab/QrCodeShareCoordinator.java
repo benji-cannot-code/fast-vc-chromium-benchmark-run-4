3e5910ec9f9457e9bc120330b20e072c95d636f3
Jeffrey Cohen <jeffreycohen@chromium.org>
2021-07-19 18:11:41 +0000

    [QRCode] exit on battery saver bug
    Bug: 1224853
    Change-Id: Ida35c4aea98191565aad8cf52ddd66bc8a6aa84e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036363
    Reviewed-by: sebsg <sebsg@chromium.org>
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903097}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareCoordinator.java
@@ -53,6 +53,8 @@ public class QrCodeShareCoordinator implements QrCodeDialogTab {
 
     @Override
     public void updatePermissions(AndroidPermissionDelegate windowAndroid) {
-        mMediator.updatePermissions(windowAndroid);
+        if (mMediator != null) {
+            mMediator.updatePermissions(windowAndroid);
+        }
     }
 }
