85df6989ceb88ad76f473d2ace676ae5398102ff
Liquan (Max) Gu <maxlg@chromium.org>
2021-07-20 00:48:29 +0000

    Create SecurePaymentConfirmationAPIV3 flag
    This flag will be used to implement API changes for Secure Payment
    Confirmation for M94. Initially disabled by default until the API
    changes are complete.
    
    Bug: 1228924
    Change-Id: Icdce50787c562179e7264c317ba95b795f19c8cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3025526
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903282}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -497,6 +497,10 @@ void WebRuntimeFeatures::EnableSecurePaymentConfirmationAPIV2(bool enable) {
   RuntimeEnabledFeatures::SetSecurePaymentConfirmationAPIV2Enabled(enable);
 }
 
+void WebRuntimeFeatures::EnableSecurePaymentConfirmationAPIV3(bool enable) {
+  RuntimeEnabledFeatures::SetSecurePaymentConfirmationAPIV3Enabled(enable);
+}
+
 void WebRuntimeFeatures::EnableSecurePaymentConfirmationDebug(bool enable) {
   RuntimeEnabledFeatures::SetSecurePaymentConfirmationDebugEnabled(enable);
 }
