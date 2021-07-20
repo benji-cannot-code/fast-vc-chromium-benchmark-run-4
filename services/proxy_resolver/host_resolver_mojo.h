44d53de8eefa9efc294f239c118efd9916fc302a
Eric Orth <ericorth@chromium.org>
2021-07-20 20:53:18 +0000

    Create ProxyHostResolverCache
    Serves as a replacement for the net::HostCache usage in
    /services/proxy_resolver.
    
    Nice features of this cache over net::HostCache:
    * Divorced from net::HostCache to allow that class to make its own
      changes with less dependencies to worry about.
    * More performant (lg(n)) eviction when full.  net::HostCache is more
      complicated and always needs to do a full linear scan of the entire
      cache.
    * Stale entries always removed when found. net::HostCache needs to deal
      with a lot more complications around keeping and sometimes using
      stale entries.
    * Key/entry types specialized to proxy usage instead of having a bunch
      of unused fields and requiring some conversions.
    * TTL logic better encapsulated instead of making callers deal with
      passing in times and deciding TTL.
    * Skips lots of net::HostCache functionality that was completely
      unnecessary for proxy script resolution usage.
    
    Old logic used default cache size from
    net::HostCache::CreateDefaultCache(), which has a max size of 100 or
    1000 depending on ENABLE_BUILT_IN_DNS. But this code's caching needs
    has no relation to whether or not the built-in DNS resolver is being
    used, so I arbitrarily split the difference in setting the new cache's
    max size to 500.
    
    Also worth noting that the new cache is only used for positive results.
    HostResolverMojo previously saved error results into its
    net::HostCache, but it did so with a TTL of 0, resulting in the entry
    being stale and unusable as soon as any measurable amount of time has
    passed.
    
    Fixed: 1229361
    Change-Id: I02f2c37f3a3419997d65d4ab90a0df31f9a23c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032014
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903641}

--- services/proxy_resolver/host_resolver_mojo.h
+++ services/proxy_resolver/host_resolver_mojo.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
-#include "net/dns/host_cache.h"
 #include "net/proxy_resolution/proxy_resolve_dns_operation.h"
 #include "services/proxy_resolver/proxy_host_resolver.h"
+#include "services/proxy_resolver/proxy_host_resolver_cache.h"
 #include "services/proxy_resolver/public/mojom/proxy_resolver.mojom.h"
 
 namespace net {
@@ -53,8 +53,9 @@ class HostResolverMojo : public ProxyHostResolver {
 
   Impl* const impl_;
 
-  std::unique_ptr<net::HostCache> host_cache_;
-  base::WeakPtrFactory<net::HostCache> host_cache_weak_factory_;
+  ProxyHostResolverCache host_cache_;
+  base::WeakPtrFactory<ProxyHostResolverCache> host_cache_weak_factory_{
+      &host_cache_};
 
   base::ThreadChecker thread_checker_;
 
--- services/proxy_resolver/mojo_proxy_resolver_v8_tracing_bindings.h
+++ services/proxy_resolver/mojo_proxy_resolver_v8_tracing_bindings.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/address_family.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/network_isolation_key.h"
-#include "net/dns/host_resolver.h"
 #include "net/log/net_log_with_source.h"
 #include "net/proxy_resolution/proxy_resolve_dns_operation.h"
 #include "services/proxy_resolver/host_resolver_mojo.h"
