a1f488b465e465d3407c47ce9bd10434f782b06c
Luzanne Batoon <batoon@google.com>
2021-07-20 00:41:36 +0000

    Track empty shortcut icon
    Use UMA to record when apps do not provide an icon for their shortcuts.
    This will let us know how often the Google Play Store icon is shown in
    place of a shortcut.
    
    Bug: b:185953363
    Test: manual
    Change-Id: Iee31cd7f7b74b0cdd2edd14c8a9881d8ad1d4e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3004229
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Luzanne Batoon <batoon@google.com>
    Cr-Commit-Position: refs/heads/master@{#903277}

--- chrome/browser/ui/app_list/search/arc/arc_app_shortcut_search_result.cc
+++ chrome/browser/ui/app_list/search/arc/arc_app_shortcut_search_result.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_config.h"
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "base/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_service/app_icon_factory.h"
 #include "chrome/browser/ash/arc/icon_decode_request.h"
@@ -50,6 +51,15 @@ ArcAppShortcutSearchResult::ArcAppShortcutSearchResult(
   SetDisplayType(ash::SearchResultDisplayType::kTile);
   SetMetricsType(ash::PLAY_STORE_APP_SHORTCUT);
   SetIsRecommendation(is_recommendation);
+
+  if (!data_->icon || !data_->icon->icon_png_data ||
+      data_->icon->icon_png_data->empty()) {
+    UMA_HISTOGRAM_ENUMERATION("Arc.AppShortcutSearchResult.ShortcutStatus",
+                              arc::ArcAppShortcutStatus::kEmpty);
+  } else {
+    UMA_HISTOGRAM_ENUMERATION("Arc.AppShortcutSearchResult.ShortcutStatus",
+                              arc::ArcAppShortcutStatus::kNotEmpty);
+  }
 
   const int icon_dimension =
       ash::SharedAppListConfig::instance().search_tile_icon_dimension();
