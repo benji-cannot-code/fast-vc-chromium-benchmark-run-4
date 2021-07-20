58e5a24e0728fbd6f28b58750ec0548b69cd73d0
dpapad <dpapad@chromium.org>
2021-07-20 07:13:53 +0000

    WebUI: Add minimal TypeScript definitions for Mojo's core bindings.js.
    Checking in a minimal bindings.d.ts file so that files that refer to [1]
    can be passed to the TS Compiler without resulting in errors.
    
    The definition file should idelly be auto-generated by passing [2]
    directly to the TS compiler, but unfortunately currently these results
    in an unusable auto-generated bindings.d.ts file.
    
    [1] chrome://resources/mojo/mojo/public/js/bindings.js
    [2] gen/mojo/public/js/bindings_uncompiled.js
    
    Bug: 1189595
    Change-Id: Id1bd58b00d31811a6087c5e3dee379fd37c08b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031977
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903363}

---
+++ ui/webui/resources/mojo/bindings.d.ts
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Minimal definitions for the Mojo core bindings, just enough to make the TS
+// compiler to not throw errors. These should be fleshed out, or even better
+// auto-generated from bindings_uncompiled.js eventually. The latter currently
+// does not produce definitions that work.
+
+export namespace mojo {
+  namespace internal {
+    namespace interfaceSupport {
+
+      interface Endpoint {}
+
+      interface PendingReceiver {
+        readonly handle: Endpoint;
+      }
+
+      function getEndpointForReceiver(handle: Endpoint): Endpoint;
+    }
+  }
+}
