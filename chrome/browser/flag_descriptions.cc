a7a2cf27726f1c8b664316608f248f74f930bee7
SebSG <sebsg@chromium.org>
2021-07-20 22:07:14 +0000

    [Publish] Cache failures to publish, and disable the feature then.
    Caches failures to publish and disables the feature in that case.
    Also runs the callback after saving the note, and add the feature flag.
    
    Fixed: 1216571, 1216572
    Change-Id: Ibdee9d0f4be53ad1bd035986e7ac7e8d46be2996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3039425
    Commit-Queue: sebsg <sebsg@chromium.org>
    Auto-Submit: sebsg <sebsg@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#903664}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3679,6 +3679,10 @@ const char kXsurfaceMetricsReportingName[] = "Xsurface Metrics Reporting";
 const char kXsurfaceMetricsReportingDescription[] =
     "Allows metrics reporting state to be passed to Xsurface";
 
+const char kWebNotesPublishName[] = "WebNotes Publish";
+const char kWebNotesPublishDescription[] =
+    "Allows users to save their created notes.";
+
 // Non-Android -----------------------------------------------------------------
 
 #else  // !defined(OS_ANDROID)
