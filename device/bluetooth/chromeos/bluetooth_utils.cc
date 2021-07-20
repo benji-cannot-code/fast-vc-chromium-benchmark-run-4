beaa8fd9a539b955a01ad249087ac0828c890704
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-07-20 17:45:12 +0000

    [CrOS Bluetooth] Split BluetoothUISurfaces variant into two
    With bluetooth revamp getting underway, BluetoothUISurfaces variant
    is not sufficient to capture all bluetooth UI surfaces. This CL
    splits BluetoothUISurfaces into two indicating where each UI surface
    originated from.
    
    Bug: 1010321
    Change-Id: Icdf1c96ddcb59e3f49a7241651de75d2d4601289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3039279
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/master@{#903564}

--- device/bluetooth/chromeos/bluetooth_utils.cc
+++ device/bluetooth/chromeos/bluetooth_utils.cc
@@ -249,7 +249,7 @@ void RecordPairingResult(absl::optional<ConnectionFailureReason> failure_reason,
 
 void RecordUserInitiatedReconnectionAttemptResult(
     absl::optional<ConnectionFailureReason> failure_reason,
-    BluetoothUiSurface surface) {
+    UserInitiatedReconnectionUISurfaces surface) {
   bool success = !failure_reason.has_value();
   std::string base_histogram_name =
       "Bluetooth.ChromeOS.UserInitiatedReconnectionAttempt.Result";
@@ -257,7 +257,9 @@ void RecordUserInitiatedReconnectionAttemptResult(
   base::UmaHistogramBoolean(base_histogram_name, success);
 
   std::string surface_name =
-      (surface == BluetoothUiSurface::kSettings ? "Settings" : "SystemTray");
+      (surface == UserInitiatedReconnectionUISurfaces::kSettings
+           ? "Settings"
+           : "SystemTray");
   base::UmaHistogramBoolean(base_histogram_name + "." + surface_name, success);
 
   if (!success) {
@@ -270,7 +272,7 @@ void RecordUserInitiatedReconnectionAttemptResult(
 }
 
 void RecordDeviceSelectionDuration(base::TimeDelta duration,
-                                   BluetoothUiSurface surface,
+                                   DeviceSelectionUISurfaces surface,
                                    bool was_paired,
                                    BluetoothTransport transport) {
   // Throw out longtail results of the user taking longer than
@@ -285,7 +287,8 @@ void RecordDeviceSelectionDuration(base::TimeDelta duration,
   RecordDeviceSelectionDuration(base_histogram_name, duration);
 
   std::string surface_name =
-      (surface == BluetoothUiSurface::kSettings ? "Settings" : "SystemTray");
+      (surface == DeviceSelectionUISurfaces::kSettings ? "Settings"
+                                                       : "SystemTray");
   std::string surface_histogram_name = base_histogram_name + "." + surface_name;
   RecordDeviceSelectionDuration(surface_histogram_name, duration);
 
