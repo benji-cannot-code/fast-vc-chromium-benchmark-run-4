80c880b39a0247c6e552f76c156cd354f4dc8743
James Cook <jamescook@chromium.org>
2021-07-20 00:41:30 +0000

    views: Add checks for crash in TouchSelectionMenuViews
    The current hypothesis is that `client_` is deleted, but we don't know
    which client. Add simple lifetime tracking to the client class. Put
    the client type in a crash key. Also convert one DCHECK to CHECK, just
    in case there's an issue with multiple menu instances.
    
    Bug: 1146270
    Test: manual on Chrome OS, this doesn't add new crashes AFAICT
    Change-Id: If73e159852e5d7f42f4d6adb6c087f838a5500c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036232
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903276}

--- ui/views/touchui/touch_selection_menu_views.h
+++ ui/views/touchui/touch_selection_menu_views.h
@@ -54,6 +54,8 @@ class VIEWS_EXPORT TouchSelectionMenuViews : public BubbleDialogDelegateView {
 
   void ButtonPressed(int command, const ui::Event& event);
 
+  void EllipsisPressed(const ui::Event& event);
+
   // Helper to disconnect this menu object from its owning menu runner.
   void DisconnectOwner();
 
@@ -63,6 +65,9 @@ class VIEWS_EXPORT TouchSelectionMenuViews : public BubbleDialogDelegateView {
 
   TouchSelectionMenuRunnerViews* owner_;
   ui::TouchSelectionMenuClient* const client_;
+
+  // TODO(jamescook): Remove after investigation of https://crbug.com/1146270
+  const char* const client_type_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchSelectionMenuViews);
 };
