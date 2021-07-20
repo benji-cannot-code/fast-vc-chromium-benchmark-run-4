dc3f7d0deebcae37cc5fa0f840491c470e8302f7
Scott Haseley <shaseley@chromium.org>
2021-07-20 21:33:23 +0000

    [blink scheduler] Prioritize postMessage forwarding tasks
    When calling postMessage on a remote frame, we defer sending the
    postMessage IPC until after the current task completes in order to
    guarantee that other IPCs in the same task are sent first. This is
    accomplished by queuing a task that then sends the postMessage IPC.
    
    Prior to this CL, this "postMessage forwarding task" used the
    kPostedMessage TaskRunner, which corresponds to a normal priority task
    queue. Since many other tasks run at this priority, these forwarding
    can incur queuing delays on the *sending side* if that main thread is
    congested, in addition to queuing delays on the receiving side.
    
    This CL changes these forwarding tasks to use a new task type that
    corresponds to a very high priority task queue. Since the postMessage
    IPC is still deferred via a task, we maintain the property that other
    non-postMessage IPCs from the same task are sent first, but we decrease
    the queueing delay on the sending side by running these tasks at a
    higher priority.
    
    As a result of this CL, the ordering between postMessage tasks on the
    receiving side and tasks on the sending side may change. This reordering
    should be safe, but this CL adds a killswitch just in case.
    
    Bug: 1212894
    Change-Id: I8e1b59435d2f10d16c8c8e86ac0e677bdc933b0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2912307
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903654}

--- third_party/blink/public/platform/task_type.h
+++ third_party/blink/public/platform/task_type.h
@@ -262,6 +262,9 @@ enum class TaskType : unsigned char {
   // Tasks related to the WebGPU API
   kWebGPU = 78,
 
+  // Cross-process PostMessage IPCs that are deferred in the current task.
+  kInternalPostMessageForwarding = 79,
+
   ///////////////////////////////////////
   // The following task types are only for thread-local queues.
   ///////////////////////////////////////
@@ -289,7 +292,7 @@ enum class TaskType : unsigned char {
   kWorkerThreadTaskQueueV8 = 47,
   kWorkerThreadTaskQueueCompositor = 48,
 
-  kCount = 79,
+  kCount = 80,
 };
 
 }  // namespace blink
