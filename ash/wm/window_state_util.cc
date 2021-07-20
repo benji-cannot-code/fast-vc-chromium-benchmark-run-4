c7c968bc62c783e929a05b97086a75cfd1901eca
Judy Wang <shidi@chromium.org>
2021-07-20 00:14:53 +0000

    ChromeOS: Add bounce animation to invalid max/fullscreen shortcut press.
    Add bounce animation when press maximize (and fullscreen) shortcut
    on non maximizeable window.
    
    Test: Manual Test.
    Bug: 1229786
    Change-Id: I35689673e9db052df600bd8406188b9337028647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035796
    Commit-Queue: Judy Wang <shidi@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903263}

--- ash/wm/window_state_util.cc
+++ ash/wm/window_state_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/window_state_util.h"
 
+#include "ash/public/cpp/window_animation_types.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_state_delegate.h"
 #include "ui/wm/core/window_util.h"
@@ -16,8 +17,11 @@ void ToggleFullScreen(WindowState* window_state,
   // Window which cannot be maximized should not be full screen'ed.
   // It can, however, be restored if it was full screen'ed.
   bool is_fullscreen = window_state->IsFullscreen();
-  if (!is_fullscreen && !window_state->CanMaximize())
+  if (!is_fullscreen && !window_state->CanMaximize()) {
+    // If `window` cannot be maximized, then do a window bounce animation.
+    wm::AnimateWindow(window_state->window(), wm::WINDOW_ANIMATION_TYPE_BOUNCE);
     return;
+  }
 
   if (delegate && delegate->ToggleFullscreen(window_state))
     return;
