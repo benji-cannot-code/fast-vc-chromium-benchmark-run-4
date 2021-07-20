f3771996f47a53ae2ad04acd872be3e6712b088e
Victor Costan <pwnall@chromium.org>
2021-07-20 14:48:04 +0000

    components: Covert double-quoted SQL string literals to single-quoted.
    Double-quoted string literals are non-standard SQL. Allowing
    double-quoted string literals is now considered a misfeature by SQLite
    authors. See https://www.sqlite.org/quirks.html#dblquote
    
    This CL converts existing occurrences of double-quoted string literals,
    in preparation for blocking them for Chrome features.
    
    This CL also re-formats some of the modified queries, to match the (new)
    guidelines in //sql/README.md.
    
    Bug: 1230199
    Change-Id: Ie5a04e666fccafe5cb0e955499a9c4c4d337ac88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3040823
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903468}

--- components/test/data/offline_pages/prefetch/version_schemas/v1.sql
+++ components/test/data/offline_pages/prefetch/version_schemas/v1.sql
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 INSERT OR REPLACE INTO meta (key, value)
-VALUES ("version", 1), ("last_compatible_version", 1);
+VALUES ('version', 1), ('last_compatible_version', 1);
 
 CREATE TABLE IF NOT EXISTS prefetch_items
 (
