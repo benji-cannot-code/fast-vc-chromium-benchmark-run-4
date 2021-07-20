9657f81d7f0b91379ad0418fe3f26449347a87c6
mlcui <mlcui@google.com>
2021-07-20 05:53:26 +0000

    Remove languages_page from CrOS browser settings build
    This CL ensures that files in settings/languages_page are never included
    in the CrOS browser settings build, and should slightly reduce the CrOS
    build binary size as a result. To do this, we move the "Manage languages
    in Chrome OS settings" button from <settings-languages-page> to
    <settings-basic-page>.
    
    Drive-by: Delete languages_metrics_proxy.js and all references to it
    from browser settings, as no file depends on it as a result of this
    change.
    
    Files in this CL are taken verbatim from crrev.com/c/2896754/16 except
    for languages_{sub,}page.{js,html}, which only has the changes necessary
    for this CL (removal of LanguagesMetricsProxy and "open CrOS settings"
    button). Further removal of CrOS-only code in these files will be done
    in a future CL.
    
    Bug: 1097328, 1109431
    Change-Id: Ie6b97290be51c1fa0293a8f025fc69632c1ea3b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026228
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/master@{#903348}

--- chrome/browser/resources/settings/languages_page/languages_page.js
+++ chrome/browser/resources/settings/languages_page/languages_page.js
@@ -49,10 +49,6 @@ import {PrefsBehavior, PrefsBehaviorInterface} from '../prefs/prefs_behavior.js'
 import {routes} from '../route.js';
 import {Route, Router} from '../router.js';
 
-// <if expr="chromeos">
-import {LanguagesMetricsProxy, LanguagesMetricsProxyImpl, LanguagesPageInteraction} from './languages_metrics_proxy.js';
-// </if>
-
 import {LanguageSettingsActionType, LanguageSettingsMetricsProxy, LanguageSettingsMetricsProxyImpl, LanguageSettingsPageImpressionType} from './languages_settings_metrics_proxy.js';
 
 /**
@@ -204,24 +200,10 @@ class SettingsLanguagesPageElement extends SettingsLanguagesPageElementBase {
   constructor() {
     super();
 
-    // <if expr="chromeos">
-    /** @private {!LanguagesMetricsProxy} */
-    this.languagesMetricsProxy_ = LanguagesMetricsProxyImpl.getInstance();
-    // </if>
     /** @private {!LanguageSettingsMetricsProxy} */
     this.languageSettingsMetricsProxy_ =
         LanguageSettingsMetricsProxyImpl.getInstance();
   }
-
-  // <if expr="chromeos">
-  /** @private */
-  onOpenChromeOSLanguagesSettingsClick_() {
-    const chromeOSLanguagesSettingsPath =
-        loadTimeData.getString('chromeOSLanguagesSettingsPath');
-    window.location.href =
-        `chrome://os-settings/${chromeOSLanguagesSettingsPath}`;
-  }
-  // </if>
 
   // <if expr="not is_macosx">
   /**
@@ -338,10 +320,6 @@ class SettingsLanguagesPageElement extends SettingsLanguagesPageElementBase {
    * @private
    */
   onEditDictionaryTap_() {
-    // <if expr="chromeos">
-    this.languagesMetricsProxy_.recordInteraction(
-        LanguagesPageInteraction.OPEN_CUSTOM_SPELL_CHECK);
-    // </if>
     Router.getInstance().navigateTo(
         /** @type {!Route} */ (routes.EDIT_DICTIONARY));
   }
@@ -368,16 +346,6 @@ class SettingsLanguagesPageElement extends SettingsLanguagesPageElementBase {
   getProspectiveUILanguageName_(languageCode) {
     return this.languageHelper.getLanguage(languageCode).displayName;
   }
-
-  // <if expr="chromeos">
-  /**
-   * @param {!Event} e
-   * @private
-   */
-  onSpellcheckToggleChange_(e) {
-    this.languagesMetricsProxy_.recordToggleSpellCheck(e.target.checked);
-  }
-  // </if>
 
   /**
    * Handler to initiate another attempt at downloading the spell check
