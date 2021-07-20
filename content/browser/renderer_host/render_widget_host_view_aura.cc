f7a37305638120b9cffed3937b072718ee2dfa78
James Cook <jamescook@chromium.org>
2021-07-20 17:37:48 +0000

    content: Explicitly dismiss touch selection handles in ~RWHVA
    Speculative fix for a crash with touch selection menus. The touch
    selection controller is owned by RenderWidgetHostViewAura, but does not
    explicitly hide the selection handles during teardown.
    
    Anecdotally we've had some reports of touch handles staying visible
    after navigations and PDF crashes.
    
    Bug: 1146270
    Change-Id: I385d8c8968619cedbe84a3647820dd18eed59b1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3039356
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: W. James MacLean <wjmaclean@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903557}

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -2028,6 +2028,8 @@ RenderWidgetHostViewAura::~RenderWidgetHostViewAura() {
   // Ask the RWH to drop reference to us.
   host()->ViewDestroyed();
 
+  // Dismiss any visible touch selection handles or touch selection menu.
+  selection_controller_->HideAndDisallowShowingAutomatically();
   selection_controller_.reset();
   selection_controller_client_.reset();
 
