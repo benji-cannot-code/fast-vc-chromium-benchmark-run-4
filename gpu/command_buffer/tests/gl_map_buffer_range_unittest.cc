1a673aa5bf17a6219e2a685cac261d5b14851444
Sadrul Habib Chowdhury <sadrul@chromium.org>
2021-07-20 04:33:07 +0000

    Reland crrev.com/c/3024404: [flake] Fix flakiness in gl_tests
    gl_tests (and gl_tests_passthrough) have a lot of flake. The flakes
    are caused by the GL environment not being set-up correctly between
    tests. To fix this, GLTestSetupHelper is introduced, as an instance of
    testing::TestEventListener, to automatically set-up the GL environment
    at the start of every test, and tear-down at the end. This makes each
    test run in a clean environment, and fixes the flake.
    
    Bug: 1229261, 1228007, 1230038
    Change-Id: I9487f02476d75445659b5d299acd0b358a763827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036165
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903338}

--- gpu/command_buffer/tests/gl_map_buffer_range_unittest.cc
+++ gpu/command_buffer/tests/gl_map_buffer_range_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <GLES3/gl3.h>
 #include <stdint.h>
 
+#include "build/build_config.h"
 #include "gpu/command_buffer/service/context_group.h"
 #include "gpu/command_buffer/tests/gl_manager.h"
 #include "gpu/command_buffer/tests/gl_test_utils.h"
@@ -315,6 +316,12 @@ TEST_F(ES3MapBufferRangeTest, ReadPixels) {
 
   glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
   GLTestHelper::CheckGLError("no errors", __LINE__);
+
+#if defined(OS_MAC)
+  // TODO(crbug.com/1230038): This step causes a crash on mac intel-uhd bot.
+  if (GPUTestBotConfig::CurrentConfigMatches("Mac Intel 0x3e9b"))
+    return;
+#endif
 
   glReadPixels(0, 0, kCanvasSize, kCanvasSize, GL_RGBA, GL_UNSIGNED_BYTE, 0);
   GLTestHelper::CheckGLError("no errors", __LINE__);
