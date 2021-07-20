80c3f41d7dcb5adce7ba9e0e9064e3885ffbceec
Tina Wang <tinazwang@google.com>
2021-07-20 17:26:12 +0000

    [iOS] Disable tests in FallbackCoordinatorTestCase due to bot failures.
    https://ci.chromium.org/ui/p/chrome/builders/official/ios/6224/overview
    https://ci.chromium.org/ui/p/chrome/builders/official/ios/6221/overview
    https://ci.chromium.org/ui/p/chrome/builders/official/ios/6218/overview
    
    Bug: 1218869
    Change-Id: I917d22a53b42251e6b91e11ebd40caf56f63b8c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041902
    Owners-Override: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903542}

--- ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
@@ -426,7 +426,8 @@ BOOL WaitForKeyboardToAppear() {
 
 // Test the input accessory bar is present when undocking then docking the
 // keyboard.
-- (void)testInputAccessoryBarIsPresentAfterUndockingKeyboard {
+// TODO(crbug.com/1218869): Re-enable this test.
+- (void)DISABLED_testInputAccessoryBarIsPresentAfterUndockingKeyboard {
   if (![ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"Test not applicable for iPhone.");
   }
