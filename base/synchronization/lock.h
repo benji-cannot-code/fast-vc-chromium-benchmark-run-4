27be303e798a52e54d396e3bb5392c84d1785999
Peter Kasting <pkasting@chromium.org>
2021-07-20 19:47:56 +0000

    Remove some unnecessary #includes.
    According to
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    these were responsible for 1,460,113,428 bytes of input to the compiler,
    or roughly 0.58% of the input used to build Chrome.
    
    Bug: 242216, 1227131
    Change-Id: I50957aeb06d43da4aa440f5714be7ca5fe57a053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3029254
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903616}

--- base/synchronization/lock.h
+++ base/synchronization/lock.h
@@ -8,11 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/check_op.h"
+#include "base/dcheck_is_on.h"
 #include "base/macros.h"
 #include "base/synchronization/lock_impl.h"
 #include "base/thread_annotations.h"
-#include "base/threading/platform_thread.h"
 #include "build/build_config.h"
+
+#if DCHECK_IS_ON()
+#include "base/threading/platform_thread.h"
+#endif
 
 namespace base {
 
