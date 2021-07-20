ed7ba002e2eebce3592f968f723cee51b2c8c6c5
Juanmi Huertas <juanmihd@chromium.org>
2021-07-20 16:33:43 +0000

    Revert "Reland "Add an integration test for FSC direct upload to Box.""
    This reverts commit f811d9a62da569b1d2bc0b9cc886aa803fa9487d.
    
    Reason for revert: Fails to compile, line 334 should have a return false; with the NOTREACHED()
    
    Original change's description:
    > Reland "Add an integration test for FSC direct upload to Box."
    >
    > This reverts commit 669301f3429be86c3ef00891080b079cdcf00054.
    >
    > Reason for revert: Retry submitting new FSC tests.
    >
    > Original change's description:
    > > Revert "Add an integration test for FSC direct upload to Box."
    > >
    > > This reverts commit ab82fb398d91e89456ac282c0db2f9d760b373e9.
    > >
    > > Reason for revert:
    > >
    > > Note: It is reported that sheriffs cannot submit CL created by Findit
    > > (crbug.com/1187426). A workaround in the mean time is to abandon this
    > > CL and create another revert CL.
    > >
    > > Findit (https://goo.gl/kROfz5) identified CL at revision 903211 as the
    > > culprit for failures in the build cycles as shown on:
    > > https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2FiODJmYjM5OGQ5MWU4OTQ1NmFjMjgyYzBkYjJmOWQ3NjBiMzczZTkM
    > >
    > > Sample Failed Build: https://ci.chromium.org/b/8841240734991453440
    > >
    > > Sample Failed Step: compile
    > >
    > > Original change's description:
    > > > Add an integration test for FSC direct upload to Box.
    > > > 
    > > > This change sets up the FSC captures sites integration test framework, and adds a test for the basic "direct upload to Box.com using Single Factor Authentication" scenario.
    > > > 
    > > > To build:
    > > > - Add "checkout_chromium_fsc_test_dependencies: True" to .gclient
    > > > - gclient sync
    > > > - ninja -C out/Default -j 100 chrome/test:enterprise_connector_file_system_interactive_tests
    > > > 
    > > > To run the test:
    > > > out/Default/enterprise_connector_file_system_interactive_tests --test-launcher-timeout=1000000 --ui-test-action-max-timeout=1000000 --enable-pixel-output-in-tests --gtest_filter="BoxCapturedSitesInteractiveTest.SFA_DownloadSmallFileSuccess"
    > > > 
    > > > Bug: 1214825
    > > > Change-Id: I6405ace5693ebefe7b4cbc7adb7c33a8ea5d8e51
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2993451
    > > > Reviewed-by: Alice Gong <alicego@google.com>
    > > > Commit-Queue: Yiming Zhou <uwyiming@google.com>
    > > > Cr-Commit-Position: refs/heads/master@{#903211}
    > >
    > >
    > > Change-Id: I21d616cd55819d04c58b8ae4e0056ba55c4455a5
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 1214825
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3040020
    > > Owners-Override: Fergus Dall <sidereal@google.com>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/master@{#903237}
    >
    > Bug: 1214825
    > Change-Id: Iea173763b5de48cdbb346f976fade5a1e83a8141
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3040244
    > Reviewed-by: Alice Gong <alicego@google.com>
    > Commit-Queue: Alice Gong <alicego@google.com>
    > Cr-Commit-Position: refs/heads/master@{#903461}
    
    Bug: 1214825
    Change-Id: I8c10ab59f19588e4df520622a3a7aef41065606a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041224
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Owners-Override: Juanmi Huertas <juanmihd@chromium.org>
    Auto-Submit: Juanmi Huertas <juanmihd@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903503}

--- chrome/browser/enterprise/connectors/file_system/signin_confirmation_modal.cc
+++ chrome/browser/enterprise/connectors/file_system/signin_confirmation_modal.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/connectors/file_system/signin_confirmation_modal.h"
 
-#include "chrome/browser/enterprise/connectors/file_system/signin_experience.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/models/image_model.h"
@@ -28,29 +27,25 @@ namespace enterprise_connectors {
 
 // static
 void FileSystemConfirmationModal::Show(gfx::NativeWindow context,
-                                       const std::u16string& title,
-                                       const std::u16string& message,
-                                       const std::u16string& cancel_button,
-                                       const std::u16string& accept_button,
-                                       Callback callback,
-                                       SigninExperienceTestObserver* observer) {
-  auto* confirmation_modal = new FileSystemConfirmationModal(
-      title, message, cancel_button, accept_button, std::move(callback));
+                                       std::u16string title,
+                                       std::u16string message,
+                                       std::u16string cancel_button,
+                                       std::u16string accept_button,
+                                       Callback callback) {
   auto* modal_view = constrained_window::CreateBrowserModalDialogViews(
-      confirmation_modal, context);
-
-  if (observer)
-    observer->OnConfirmationDialogCreated(confirmation_modal);
+      new FileSystemConfirmationModal(title, message, cancel_button,
+                                      accept_button, std::move(callback)),
+      context);
   // The naked new is OK here because CreateBrowserModalDialogViews() takes
   // ownership.
   modal_view->Show();
 }
 
 FileSystemConfirmationModal::FileSystemConfirmationModal(
-    const std::u16string& title,
-    const std::u16string& message,
-    const std::u16string& cancel_button,
-    const std::u16string& accept_button,
+    std::u16string title,
+    std::u16string message,
+    std::u16string cancel_button,
+    std::u16string accept_button,
     Callback callback)
     : title_(title), message_(message), callback_(std::move(callback)) {
   SetShowIcon(true);
