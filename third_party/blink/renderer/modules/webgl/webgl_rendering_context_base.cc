372648f8ecef1f2472f342a73e0b4c61b9f2c448
Alexander Hendrich <hendrich@chromium.org>
2021-07-20 16:46:37 +0000

    Reland "Early-out of transferToImageBitmap if context has been lost."
    This reverts commit f12ab4de5432efc6ae8c90f76330eb8dba148240.
    
    Reason for revert: CL is not the suspected culprit of https://crbug.com/1231072, sorry for the noise
    
    Original change's description:
    > Revert "Early-out of transferToImageBitmap if context has been lost."
    >
    > This reverts commit 821d2c7d7e5fcac588c0573504ee7cdac8e0f7df.
    >
    > Reason for revert: speculative revert for https://crbug.com/1231072
    >
    > Original change's description:
    > > Early-out of transferToImageBitmap if context has been lost.
    > >
    > > Bug: 1229816
    > > Change-Id: I3382952cb40c2ab52d370dfe0b983bd0d950a92c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036238
    > > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > > Commit-Queue: Kenneth Russell <kbr@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#903309}
    >
    > Bug: 1229816, 1231072
    > Change-Id: I597a7a2c64b03b2851d99575ae57a282c25222ce
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041458
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Alexander Hendrich <hendrich@chromium.org>
    > Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    > Reviewed-by: Omar Morsi <omorsi@google.com>
    > Reviewed-by: Martin Rodriguez <rodmartin@google.com>
    > Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#903425}
    
    Bug: 1229816, 1231072
    Change-Id: Iba4eb413feb2513b0f19a56369e427b2d0fc235b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041431
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Owners-Override: Alexander Hendrich <hendrich@chromium.org>
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903517}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -587,6 +587,11 @@ ImageBitmap* WebGLRenderingContextBase::TransferToImageBitmapBase(
     ScriptState* script_state) {
   WebFeature feature = WebFeature::kOffscreenCanvasTransferToImageBitmapWebGL;
   UseCounter::Count(ExecutionContext::From(script_state), feature);
+  if (!GetDrawingBuffer()) {
+    // Context is lost.
+    return nullptr;
+  }
+
   return MakeGarbageCollected<ImageBitmap>(
       GetDrawingBuffer()->TransferToStaticBitmapImage());
 }
