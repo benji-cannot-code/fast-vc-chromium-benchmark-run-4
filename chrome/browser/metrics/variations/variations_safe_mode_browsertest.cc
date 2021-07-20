0a9c3ff7eaddd9cdeee4059619b5bacc261be56c
Caitlin Fischer <caitlinfischer@google.com>
2021-07-20 18:10:28 +0000

    Clean up a variations test function.
    Bug: 1205645
    Change-Id: Ibc3626e6fbc5939a4b301d21f192c52b2ecaafa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042059
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/master@{#903578}

--- chrome/browser/metrics/variations/variations_safe_mode_browsertest.cc
+++ chrome/browser/metrics/variations/variations_safe_mode_browsertest.cc
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// The tests in this file verify the behavior of variations safe mode. The tests
+// should be kept in sync with those in ios/chrome/browser/variations/
+// variations_safe_mode_egtest.mm.
+
 #include <string>
 
 #include "base/metrics/field_trial.h"
@@ -28,7 +32,7 @@ void StoreTestSeedAndSignature(PrefService* local_state,
   const std::string seed_pref = use_safe_seed_prefs
                                     ? prefs::kVariationsSafeCompressedSeed
                                     : prefs::kVariationsCompressedSeed;
-  local_state->SetString(seed_pref, GetTestSeedForPrefs());
+  local_state->SetString(seed_pref, kCompressedBase64TestSeedData);
 
   const std::string signature_pref = use_safe_seed_prefs
                                          ? prefs::kVariationsSafeSeedSignature
