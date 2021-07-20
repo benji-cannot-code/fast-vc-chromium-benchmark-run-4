509c91f256874fe355859925a35237cf9c49cdb6
Peter Kotwicz <pkotwicz@chromium.org>
2021-07-20 14:43:10 +0000

    [Reland] [Refactor] Remove uses of @RemovableInRelease for runtime asserts
    This CL replaces cases where @RemovableInRelease is used to guard
    debug-only asserts with
    if (BuildConfig.ENABLE_ASSERTS) {}
    
    We are deprecating the @RemovableInRelease.
    
    BUG=1180332
    
    Previous attempts:
    - https://chromium-review.googlesource.com/c/chromium/src/+/3021102
    - https://chromium-review.googlesource.com/c/chromium/src/+/3035642
    were reverted due to PropertyModelTest#ensureValidKey()
    TabListMediatorUnitTest#addSpecialItem_withoutTabListModelProperties()
    test failures.
    
    Change-Id: I637fb36e4c13108b9459368bafae91d445157c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3039780
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903464}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
@@ -16,6 +16,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -90,6 +91,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.build.BuildConfig;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.endpoint_fetcher.EndpointFetcher;
 import org.chromium.chrome.browser.endpoint_fetcher.EndpointFetcherJni;
@@ -1707,9 +1709,16 @@ public class TabListMediatorUnitTest {
         assertEquals(TabProperties.UiType.DIVIDER, mModel.get(1).type);
     }
 
-    @Test(expected = AssertionError.class)
+    @Test
     public void addSpecialItem_withoutTabListModelProperties() {
-        mMediator.addSpecialItemToModel(0, TabProperties.UiType.DIVIDER, new PropertyModel());
+        if (!BuildConfig.ENABLE_ASSERTS) return;
+
+        try {
+            mMediator.addSpecialItemToModel(0, TabProperties.UiType.DIVIDER, new PropertyModel());
+        } catch (AssertionError e) {
+            return;
+        }
+        fail("PropertyModel#validateKey() assert should have failed.");
     }
 
     @Test
