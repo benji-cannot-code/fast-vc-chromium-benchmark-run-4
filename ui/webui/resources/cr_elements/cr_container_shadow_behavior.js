96bd22bf0bcb86c8cd3de77c953a3047de6597c0
dpapad <dpapad@chromium.org>
2021-07-20 08:13:44 +0000

    WebUI: Port CrContainerShadowBehavior to a Mixin.
    This is necessary for leveraging the showDropShadows() method from
    settings-ui#currentRouteChanged() method in the follow-up CL, as
    CrContainerShadowBehavior#attached() needs to run before
    RouteObserverMixin's connectedCallback() and that coordination is not
    possible when one is a Behavior and the other one is a Mixin.
    
    Unfortunately the old Behavior can't be fully removed yet, since there
    are still places that use it and have not been converted to Polymer
    class-based syntax.
    
    Bug: 1177509,1206112
    Change-Id: Ia805c78f5fd09b2d790ced5a9a95c76e45102bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035422
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903374}

--- ui/webui/resources/cr_elements/cr_container_shadow_behavior.js
+++ ui/webui/resources/cr_elements/cr_container_shadow_behavior.js
@@ -30,10 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Clients should either use the existing shared styling in
  * shared_styles_css.html, '#cr-container-shadow-[top/bottom]' and
  * '#cr-container-shadow-[top/bottom].has-shadow', or define their own styles.
+ *
+ * NOTE: This file is deprecated in favor of cr_container_shadow_mixin.js. Don't
+ * use it in any new code.
  */
 
 // clang-format off
-// #import {assert} from '../js/assert.m.js'
+// #import {assert} from '../js/assert.m.js';
 // clang-format on
 
 /** @enum {string} */
