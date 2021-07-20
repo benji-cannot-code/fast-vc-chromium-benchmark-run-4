5b209c30a40a83ced61af49aabb221f657b4a5ac
Michael Thiessen <mthiesse@chromium.org>
2021-07-20 18:35:26 +0000

    Roll the Android support library to a version with sources
    I honestly have no idea if this is rolling to a newer or older
    version, but the jars we pulled from maven had no sources or git hashes
    associated with them so I can't compare the versions.
    
    This version is pulled from
    https://android.googlesource.com/platform/frameworks/testing/+/refs/heads/android-support-test
    which will at least allow us to understand what the library is doing
    since it's totally undocumented these days. It'll also allow us to
    modify this library and fix bugs in it at least until we can migrate
    to androidx (this library was deprecated in 2018(?)) as it is no longer
    under development or supported.
    
    Bug: 1230157
    Change-Id: I76e55a696c363398eda04be09a41ae877c2596e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035542
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903590}

---
+++ third_party/android_support_test_runner/runner/src/main/java/android/support/test/internal/runner/tracker/UsageTrackerRegistry.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+package android.support.test.internal.runner.tracker;
+
+import static android.support.test.internal.util.Checks.checkNotNull;
+
+/**
+ * A registry to hold the global {@link UsageTracker}.
+ * <p/>
+ * Instrumentation will configure this registry at startup.
+ */
+public final class UsageTrackerRegistry {
+
+    // By default we use a NoOp class.
+    private static volatile UsageTracker instance =
+            new UsageTracker.NoOpUsageTracker();
+
+    public static void registerInstance(UsageTracker tracker) {
+        instance = checkNotNull(tracker);
+    }
+
+    public static UsageTracker getInstance() {
+        return instance;
+    }
+
+    private UsageTrackerRegistry() {
+    }
+}
