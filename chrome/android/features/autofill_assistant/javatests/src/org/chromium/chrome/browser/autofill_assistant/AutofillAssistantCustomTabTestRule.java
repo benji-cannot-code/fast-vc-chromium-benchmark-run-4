73ffd4f6900da056c92e3d8deeccf27c1c48c059
sandromaggi <sandromaggi@google.com>
2021-07-20 09:10:35 +0000

    [Autofill Assistant] Create custom test rule
    This adds a new test rule to start a custom tab for Autofill Assistant.
    
    It changes the screen orientation helper into a base test rule.
    
    The rule needs to be chained after the original test rule.
    
    Bug: b/185210908
    Change-Id: I09127392a751998bb9c17fafddac516cb2362a47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035162
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#903386}

---
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantCustomTabTestRule.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill_assistant;
+
+import android.support.test.InstrumentationRegistry;
+
+import org.chromium.chrome.browser.customtabs.CustomTabActivityTestRule;
+import org.chromium.chrome.browser.customtabs.CustomTabsTestUtils;
+
+class AutofillAssistantCustomTabTestRule
+        extends AutofillAssistantTestRule<CustomTabActivityTestRule> {
+    private static final String HTML_DIRECTORY = "/components/test/data/autofill_assistant/html/";
+
+    private final String mTestPage;
+
+    AutofillAssistantCustomTabTestRule(CustomTabActivityTestRule testRule, String testPage) {
+        super(testRule);
+        mTestPage = testPage;
+    }
+
+    @Override
+    public void start() {
+        getTestRule().startCustomTabActivityWithIntent(
+                CustomTabsTestUtils.createMinimalCustomTabIntent(
+                        InstrumentationRegistry.getTargetContext(),
+                        getTestRule().getTestServer().getURL(HTML_DIRECTORY + mTestPage)));
+    }
+}
