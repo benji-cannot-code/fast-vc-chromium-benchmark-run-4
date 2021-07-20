73d7b73e796ec540f18c5e4a580f103cc26eca2f
Tomasz Wiszkowski <ender@google.com>
2021-07-20 02:58:26 +0000

    Include SRP in web-contextual suggestions mechanism.
    The change expands the scope of an existing On-Focus Contextual
    Suggestions mechanism to include Search Results Page.
    
    The SRP surface is only included when a corresponding finch feature
    flag is enabled.
    
    The change does not introduce any new functionality, merely allowing
    us to enable an existing functionality in an additional context.
    
    The change applies necessary update to address a situation where we
    now only present specialized suggestions followed by suggestions with
    headers. In this situation the partial grouping request issued by
    Adaptive Suggestions would run into a corner case where the number of
    suggestions to group by search vs url is 0, making first and last
    suggestion to rearrange "the same one".
    
    Bug: 1230753
    Change-Id: Ie6e5842529a2959cca9188154339ca53e9ced618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3038872
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#903316}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1045,6 +1045,9 @@ extern const char kOmniboxRichAutocompletionPromisingDescription[];
 extern const char kOmniboxRichEntitiesInLauncherName[];
 extern const char kOmniboxRichEntitiesInLauncherDescription[];
 
+extern const char kOmniboxOnFocusSuggestionsContextualWebAllowSRPName[];
+extern const char kOmniboxOnFocusSuggestionsContextualWebAllowSRPDescription[];
+
 extern const char kOmniboxOnFocusSuggestionsContextualWebName[];
 extern const char kOmniboxOnFocusSuggestionsContextualWebDescription[];
 
