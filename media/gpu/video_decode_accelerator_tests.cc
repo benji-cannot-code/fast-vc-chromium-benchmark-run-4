de2fb655e1b5a318f6e15bdb6856e03155e2f690
Fritz Koenig <frkoenig@chromium.org>
2021-07-21 21:07:17 +0000

    media/gpu/v4l2: Mechanism to force hardware video decoder framerate
    Command line switch to keep the framerate
    consistent during playback.
    
    BUG=b:183236163
    
    Change-Id: Ibc452edf84d45395e0b27bb4b9ce846c65aa72d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042279
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904046}

--- media/gpu/video_decode_accelerator_tests.cc
+++ media/gpu/video_decode_accelerator_tests.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/encryption_scheme.h"
+#include "media/base/media_switches.h"
 #include "media/base/media_util.h"
 #include "media/base/test_data_util.h"
 #include "media/base/video_decoder_config.h"
@@ -145,6 +146,11 @@ class VideoDecoderTest : public ::testing::Test {
             VideoFrameValidator::ValidationMode::kThreshold, kSSIMTolerance));
       }
     }
+
+    base::CommandLine& command_line = *base::CommandLine::ForCurrentProcess();
+    command_line.AppendSwitchASCII(
+        switches::kHardwareVideoDecodeFrameRate,
+        base::NumberToString(g_env->Video()->FrameRate()));
 
     config.implementation = g_env->GetDecoderImplementation();
 
