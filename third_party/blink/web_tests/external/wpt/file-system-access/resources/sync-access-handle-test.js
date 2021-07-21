99571e4f1f06a7356baed05f01afa2083d25f28c
Andreas Haas <ahaas@chromium.org>
2021-07-21 04:55:27 +0000

    [fsa] Implement read and write for the sync access handle
    The implementation is ported from the implementation of
    Storage Foundation in
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/native_io/native_io_file_sync.cc.
    
    This is part of the efforts to merge Storage Foundation into OPFS.
    
    Bug: chromium:1218431
    Change-Id: Ibbd3288fa198f3fc3ce135c6b056fde9d2c02909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3014235
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903781}

---
+++ third_party/blink/web_tests/external/wpt/file-system-access/resources/sync-access-handle-test.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async function cleanupSandboxedFileSystem() {
+  const dir = await navigator.storage.getDirectory();
+  for await (let entry of dir.values())
+    await dir.removeEntry(entry.name, {recursive: entry.kind === 'directory'});
+}
+
+function sync_access_handle_test(test, description) {
+  promise_test(async t => {
+    // To be extra resilient against bad tests, cleanup before every test.
+    await cleanupSandboxedFileSystem();
+
+    const dir = await navigator.storage.getDirectory();
+    const fileHandle = await dir.getFileHandle('OPFS.test', {create: true});
+    const syncHandle = await fileHandle.createSyncAccessHandle();
+    await test(t, syncHandle);
+  }, description);
+}
