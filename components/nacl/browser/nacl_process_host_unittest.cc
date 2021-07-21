00abc828caf4040c89e1746cfb1227595bac68da
Bruce Dawson <brucedawson@chromium.org>
2021-07-21 20:33:09 +0000

    Include windows.h where needed in components
    Removing windows.h from socket_descriptor.h reveals that some source
    files use declarations from windows.h without including it. This fixes
    that for components by adding one include of windows.h.
    
    Bug: 796644
    Change-Id: If56e368ab0e234327f9b1688ae945e1c958ea7ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041819
    Commit-Queue: Derek Schuff <dschuff@chromium.org>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904029}

--- components/nacl/browser/nacl_process_host_unittest.cc
+++ components/nacl/browser/nacl_process_host_unittest.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 #if defined(OS_WIN)
+#include <windows.h>
+
 TEST(NaClProcessHostTest, AddressSpaceAllocation) {
   size_t size = 1 << 20;  // 1 MB
   void* addr = nacl::AllocateAddressSpaceASLR(GetCurrentProcess(), size);
