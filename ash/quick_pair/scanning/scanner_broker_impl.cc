c6d6ed74ffab2fd4378c839963caf0dc16527362
Shane Fitzpatrick <shanefitz@google.com>
2021-07-21 17:20:45 +0000

    [FastPair] Add BluetoothAdapter to ScannerBroker and delay scanning
    This changes adds the BluetoothAdapter to the ScannerBroker and delays
    the start scanning requests until it is received.
    
    Change-Id: I359984f916e803ce6f1913273ea848c8e21658e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3036162
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903955}

--- ash/quick_pair/scanning/scanner_broker_impl.cc
+++ ash/quick_pair/scanning/scanner_broker_impl.cc
@@ -10,14 +10,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/common/device.h"
 #include "ash/quick_pair/common/logging.h"
 #include "ash/quick_pair/common/protocol.h"
+#include "base/bind.h"
+#include "base/callback.h"
+#include "base/check.h"
 #include "base/memory/scoped_refptr.h"
+#include "device/bluetooth/bluetooth_adapter_factory.h"
 
 namespace ash {
 namespace quick_pair {
 
-ScannerBrokerImpl::ScannerBrokerImpl() = default;
+ScannerBrokerImpl::ScannerBrokerImpl() {
+  device::BluetoothAdapterFactory::Get()->GetAdapter(base::BindOnce(
+      &ScannerBrokerImpl::OnGetAdapter, weak_pointer_factory_.GetWeakPtr()));
+}
 
 ScannerBrokerImpl::~ScannerBrokerImpl() = default;
+
+void ScannerBrokerImpl::OnGetAdapter(
+    scoped_refptr<device::BluetoothAdapter> adapter) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  adapter_ = adapter;
+
+  if (start_scanning_on_adapter_callbacks_.empty())
+    return;
+
+  QP_LOG(INFO) << __func__ << ": Running saved callbacks.";
+
+  for (auto& callback : start_scanning_on_adapter_callbacks_)
+    std::move(callback).Run();
+
+  start_scanning_on_adapter_callbacks_.clear();
+}
 
 void ScannerBrokerImpl::AddObserver(Observer* observer) {
   observers_.AddObserver(observer);
@@ -28,6 +51,18 @@ void ScannerBrokerImpl::RemoveObserver(Observer* observer) {
 }
 
 void ScannerBrokerImpl::StartScanning(Protocol protocol) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  QP_LOG(INFO) << __func__ << ": protocol=" << protocol;
+
+  if (!adapter_) {
+    QP_LOG(INFO) << __func__ << ": No adapter yet, saving callback for later.";
+
+    start_scanning_on_adapter_callbacks_.push_back(
+        base::BindOnce(&ScannerBrokerImpl::StartScanning,
+                       weak_pointer_factory_.GetWeakPtr(), protocol));
+    return;
+  }
+
   switch (protocol) {
     case Protocol::kFastPair:
       StartFastPairScanning();
@@ -36,6 +71,8 @@ void ScannerBrokerImpl::StartScanning(Protocol protocol) {
 }
 
 void ScannerBrokerImpl::StopScanning(Protocol protocol) {
+  QP_LOG(INFO) << __func__ << ": protocol=" << protocol;
+
   switch (protocol) {
     case Protocol::kFastPair:
       StopFastPairScanning();
