1292b38f1e0259fed8759665d526c94f52838c83
Sunny Sachanandani <sunnyps@chromium.org>
2021-07-21 21:07:58 +0000

    gpu: Destroy shared image overlay representation after scoped access
    Currently the overlay scoped access can outlive its representation which
    triggers a CHECK failure in ~SharedImageRepresentation (which prevents a
    use-after-free).
    
    This CL makes the OverlayData map in SkiaOutputDeviceGL outlive the GL
    surface.  This is necessary so that the shared image representation in
    OverlayData outlives the corresponding scoped access in the release
    image callback in DC layer params held by the surface.  Also, we make
    sure that the DCRendererLayerParams destructor runs the release image
    callbacks.  Note that this is not strictly necessary since the callbacks
    only hold on to the scoped access unique_ptr which would be released
    even if the callback is merely destroyed instead of being run.
    
    Bug: 1227771
    Change-Id: I128bfc93e41e9799332b98d1f46689b165dacceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024635
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904047}

--- ui/gl/dc_renderer_layer_params.cc
+++ ui/gl/dc_renderer_layer_params.cc
@@ -10,6 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 DCRendererLayerParams::DCRendererLayerParams() = default;
-DCRendererLayerParams::~DCRendererLayerParams() = default;
+
+DCRendererLayerParams::~DCRendererLayerParams() {
+  for (auto& callback : release_image_cb) {
+    if (callback)
+      std::move(callback).Run();
+  }
+}
 
 }  // namespace ui
