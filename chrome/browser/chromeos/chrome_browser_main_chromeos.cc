d32a38eaf805a91094841856ef08cc5610a05752
Miriam Polzer <mpolzer@google.com>
2021-07-21 06:36:20 +0000

    Add rollback_network_config service
    Add a new service responsible for importing and exporting rollback
    network data.
    
    Export should happen on shutdown, while shill and Chrome are still
    running, it will be requested from a Chrome OS process via dbus.
    
    Import happens during oobe, before ownership is taken, the networks are
    configured as user configured. Once ownership is taken, the service
    attempts to restore the pre-rollback state in terms of network.
    
    Bug: 1212958
    Change-Id: I157bb0daeac0c15b93a9999854d2dba5eb57dcdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2997083
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Miriam Polzer <mpolzer@google.com>
    Cr-Commit-Position: refs/heads/master@{#903799}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/startup_utils.h"
 #include "chrome/browser/ash/login/users/chrome_user_manager.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
+#include "chrome/browser/ash/net/rollback_network_config/rollback_network_config_service.h"
 #include "chrome/browser/ash/notifications/debugd_notification_handler.h"
 #include "chrome/browser/ash/notifications/gnubby_notification.h"
 #include "chrome/browser/ash/notifications/low_disk_notification.h"
@@ -458,6 +459,7 @@ class DBusServices {
   }
 
   ~DBusServices() {
+    ash::rollback_network_config::Shutdown();
     sensors::SensorHalDispatcher::Shutdown();
     NetworkHandler::Shutdown();
     disks::DiskMountManager::Shutdown();
@@ -757,7 +759,6 @@ void ChromeBrowserMainPartsChromeos::PreProfileInit() {
   // keyboard::KeyboardController initializes ChromeKeyboardUI which depends
   // on ChromeKeyboardControllerClient.
   chrome_keyboard_controller_client_ = ChromeKeyboardControllerClient::Create();
-
 
   // ProfileHelper has to be initialized after UserManager instance is created.
   ProfileHelper::Get()->Initialize();
---
+++ chromeos/services/rollback_network_config/README.md
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Rollback Network Config
+
+This service provides support for Chrome OS Enterprise Rollback. It handles
+importing and exporting network configurations to be preserved during a rollback.
+
+The implementation of this service lives in
+chrome/browser/ash/net/rollback_network_config because it has Chrome
+dependencies.
