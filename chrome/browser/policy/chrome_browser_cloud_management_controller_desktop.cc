86fd3485465602b6666362b8d1d289577953118e
Wez <wez@chromium.org>
2021-07-21 15:01:18 +0000

    [fuchsia] Provide stub implementation of ExpandPathVariables().
    - Clean up policy_path_provider.h not to define functions that are
      never used on some platforms.
    - Provide a stub implementation of ExpandPathVariables() that replaces
      the ${user} and ${machine} variables with static values.
    - Update GN rules and cloud management controller for desktop to know
      about Fuchsia.
    
    Bug: 1231482, 1211758
    Change-Id: If20bb85caf4f6d4d771f87396083e8eff7eba1ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043607
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Benjamin Lerman <qsr@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903896}

--- chrome/browser/policy/chrome_browser_cloud_management_controller_desktop.cc
+++ chrome/browser/policy/chrome_browser_cloud_management_controller_desktop.cc
@@ -54,6 +54,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/install_static/install_util.h"
 #endif  // defined(OS_WIN)
 
+#if defined(OS_FUCHSIA)
+#include "base/notreached.h"
+#include "components/enterprise/browser/controller/browser_dm_token_storage.h"
+#endif
+
 namespace policy {
 
 namespace {
@@ -82,7 +87,7 @@ void ChromeBrowserCloudManagementControllerDesktop::
 #elif defined(OS_WIN)
   storage_delegate = std::make_unique<BrowserDMTokenStorageWin>();
 #else
-  NOT_REACHED();
+  NOTREACHED();
 #endif
 
   BrowserDMTokenStorage::SetDelegate(std::move(storage_delegate));
--- chrome/browser/policy/policy_path_parser.h
+++ chrome/browser/policy/policy_path_parser.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_POLICY_POLICY_PATH_PARSER_H_
 
 #include "base/files/file_path.h"
+#include "build/build_config.h"
 
 namespace policy {
 
@@ -52,6 +53,7 @@ namespace path_parser {
 base::FilePath::StringType ExpandPathVariables(
     const base::FilePath::StringType& untranslated_string);
 
+#if defined(OS_WIN) || defined(OS_MAC)
 // A helper function used to read the UserDataDir path policy without relying on
 // any policy infrastructure. This is required because this policy is needed
 // much earlier before the PrefService is initialized.
@@ -62,6 +64,7 @@ base::FilePath::StringType ExpandPathVariables(
 // profile path is not read from the PathService which already takes this into
 // account.
 void CheckUserDataDirPolicy(base::FilePath* user_data_dir);
+#endif  // defined(OS_WIN) || defined(OS_MAC)
 
 }  // namespace path_parser
 
