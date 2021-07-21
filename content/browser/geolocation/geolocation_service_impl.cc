8f0ff81361183e899ed4c15cdeab0b6f55e8f85d
Yeunjoo Choi <ychoi@igalia.com>
2021-07-21 01:12:59 +0000

    [mparch] GeolocationServiceImpl should grab origin from RenderFrameHost
    This CL is part of MPArch tasks and let GeolocationServiceImpl grab the
    origin from RenderFrameHost rather than WebContents.
    
    A test in case of prerendering is added, because permission of
    geolocation should be requested after the prerendered page is activated.
    
    Change-Id: I74ed0dccf5069449af00ae34498720884711eeb2
    Bug: 1229110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026388
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#903733}

--- content/browser/geolocation/geolocation_service_impl.cc
+++ content/browser/geolocation/geolocation_service_impl.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/permission_type.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
-#include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy.mojom.h"
 
@@ -105,11 +104,9 @@ void GeolocationServiceImpl::CreateGeolocationWithPermissionStatus(
   if (permission_status != blink::mojom::PermissionStatus::GRANTED)
     return;
 
-  WebContents* web_contents =
-      WebContents::FromRenderFrameHost(render_frame_host_);
-
-  geolocation_context_->BindGeolocation(
-      std::move(receiver), web_contents->GetLastCommittedURL().GetOrigin());
+  const auto& origin =
+      render_frame_host_->GetMainFrame()->GetLastCommittedOrigin();
+  geolocation_context_->BindGeolocation(std::move(receiver), origin.GetURL());
 }
 
 }  // namespace content
