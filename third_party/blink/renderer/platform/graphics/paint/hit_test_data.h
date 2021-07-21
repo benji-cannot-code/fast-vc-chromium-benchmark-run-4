27a5ca6bfda247aba678a30894491ebe29b1aa69
L. David Baron <dbaron@chromium.org>
2021-07-21 23:41:24 +0000

    Reference count HitTestData::scroll_translation.
    Since we're seeing crashes that could be explained by scroll_translation
    being a dangling pointer, make it a scoped_refptr rather than a raw
    pointer.
    
    (If this does fix the crashes, we probably want to do further work to
    fix the invalidation, but it's probably still good for this to be a
    scoped_refptr.)
    
    Bug: 1228506
    Change-Id: Ib12c6fa135924d0ef77feb0dbdc2bfe6b861b9c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3044599
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904113}

--- third_party/blink/renderer/platform/graphics/paint/hit_test_data.h
+++ third_party/blink/renderer/platform/graphics/paint/hit_test_data.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_HIT_TEST_DATA_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PAINT_HIT_TEST_DATA_H_
 
+#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.h"
 #include "third_party/blink/renderer/platform/graphics/touch_action_rect.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
@@ -22,7 +23,7 @@ struct PLATFORM_EXPORT HitTestData {
   // container, and whether the region allows composited scrolling depends
   // whether the scroll_translation is composited.
   IntRect scroll_hit_test_rect;
-  const TransformPaintPropertyNode* scroll_translation = nullptr;
+  scoped_refptr<const TransformPaintPropertyNode> scroll_translation;
 
   bool operator==(const HitTestData& rhs) const {
     return touch_action_rects == rhs.touch_action_rects &&
