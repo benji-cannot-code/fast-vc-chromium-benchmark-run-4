b45438c0abc433f481e19c70d5d5b173f410426c
Ramin Halavati <rhalavati@chromium.org>
2021-07-21 09:01:45 +0000

    Add metric to measure forced Incognito mode.
    Adds |Startup.IncognitoForcedStart| metric to measure if Incognito mode
    was requested while starting browser by switch or other means, and
    whether it was applied or not.
    
    Bug: 1231417
    Change-Id: I3d422c85f241757521e5330f467c37662a560a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041459
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903827}

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_base.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/statistics_recorder.h"
 #include "base/scoped_multi_source_observation.h"
@@ -496,6 +497,31 @@ bool MaybeLaunchUrlHandlerWebAppFromCmd(
 }
 #endif
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused. Updates need to be reflected in
+// enum IncognitoForcedStart in tools/metrics/histograms/enums.xml.
+enum class IncognitoForcedStart {
+  kNoSwitchAndNotForced = 0,
+  kSwitchButNotForced = 1,
+  kNoSwitchButForced = 2,
+  kSwitchAndForced = 3,
+  kMaxValue = kSwitchAndForced,
+};
+
+void RecordIncognitoForcedStart(bool should_launch_incognito,
+                                bool has_incognito_switch) {
+  if (has_incognito_switch) {
+    base::UmaHistogramEnumeration(
+        "Startup.IncognitoForcedStart",
+        should_launch_incognito ? IncognitoForcedStart::kSwitchAndForced
+                                : IncognitoForcedStart::kSwitchButNotForced);
+  } else {
+    base::UmaHistogramEnumeration(
+        "Startup.IncognitoForcedStart",
+        should_launch_incognito ? IncognitoForcedStart::kNoSwitchButForced
+                                : IncognitoForcedStart::kNoSwitchAndNotForced);
+  }
+}
 }  // namespace
 
 StartupBrowserCreator::StartupBrowserCreator() = default;
@@ -843,10 +869,13 @@ bool StartupBrowserCreator::ProcessCmdLineImpl(
   }
 
   bool silent_launch = false;
+  bool should_launch_incognito = IncognitoModePrefs::ShouldLaunchIncognito(
+      command_line, last_used_profile->GetPrefs());
   bool can_use_last_profile =
-      (CanOpenProfileOnStartup(last_used_profile) &&
-       !IncognitoModePrefs::ShouldLaunchIncognito(
-           command_line, last_used_profile->GetPrefs()));
+      CanOpenProfileOnStartup(last_used_profile) && !should_launch_incognito;
+
+  RecordIncognitoForcedStart(should_launch_incognito,
+                             command_line.HasSwitch(switches::kIncognito));
 
   // |last_used_profile| is never off-the-record. If Incognito or Guest
   // enforcement switch or policy are provided, use the appropriate private
