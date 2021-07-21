15a4e88484591e8e0d084c5b0ebf75ee6f1512af
kylechar <kylechar@chromium.org>
2021-07-21 20:45:10 +0000

    Remove viz restrictions on features.h
    There was a DEPS rule banning components/viz/common/features.h from
    being checked in most viz code. This hasn't been effective so remove the
    DEPS rule and add some guidance to viz readme about how to define and
    where to check features.
    
    Bug: 1065910
    Change-Id: I5aeafb7cb3a9cca3d3c27e9b8783b19c47ec4241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3044001
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904034}

--- components/viz/common/features.h
+++ components/viz/common/features.h
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/viz_common_export.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
+// See the following for guidance on adding new viz feature flags:
+// https://cs.chromium.org/chromium/src/components/viz/README.md#runtime-features
+
 namespace features {
 
 VIZ_COMMON_EXPORT extern const base::Feature kAdpf;
