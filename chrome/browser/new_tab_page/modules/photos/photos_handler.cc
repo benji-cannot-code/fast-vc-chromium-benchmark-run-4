8a541f216ee8f168890fd988b5c99378cf10d086
Jeremy Selier <jerem@google.com>
2021-07-21 15:28:15 +0000

    [ntp][modules] First pass at Photos Service module for NTP
    Service currently fetch the token with the right scopes and query the
    right API but does not process the response.
    
    R=tiborg@chromium.org
    
    Bug: 1230867
    Change-Id: I02dc99c945c2e157593a9b830e901a11f3263012
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041877
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Jeremy Selier <jerem@google.com>
    Cr-Commit-Position: refs/heads/master@{#903913}

---
+++ chrome/browser/new_tab_page/modules/photos/photos_handler.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/new_tab_page/modules/photos/photos_handler.h"
+
+#include "chrome/browser/new_tab_page/modules/photos/photos_service.h"
+#include "chrome/browser/new_tab_page/modules/photos/photos_service_factory.h"
+
+PhotosHandler::PhotosHandler(
+    mojo::PendingReceiver<photos::mojom::PhotosHandler> handler,
+    Profile* profile)
+    : handler_(this, std::move(handler)), profile_(profile) {}
+
+PhotosHandler::~PhotosHandler() = default;
+
+void PhotosHandler::GetMemories(GetMemoriesCallback callback) {
+  PhotosServiceFactory::GetForProfile(profile_)->GetMemories(
+      std::move(callback));
+}
