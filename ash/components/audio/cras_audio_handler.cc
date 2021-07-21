159730be54b587432d75c38eda04772764931d0b
Anton Swifton <swifton@google.com>
2021-07-21 21:30:55 +0000

    Set the input noise cancellation pref to 'on' by default.
    Input noise cancellation will be enabled by default.
    The feature flag EnableInputNoiseCancellationUi will be
    enabled by default.
    
    This is a reland of a reverted CL. The feature that it enables was
    punted to M93. The original CL was merged for M92 and later reverted
    due to a stability crash: b/187678153
    
    Bug: 1205197
    Test: ash_unittests
    Change-Id: I34680a3c7750aa98b13d1c8ac9acbbc93fdd32f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2937657
    Commit-Queue: Anton Swifton <swifton@google.com>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#890524}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033192
    Cr-Commit-Position: refs/heads/master@{#904059}

--- ash/components/audio/cras_audio_handler.cc
+++ ash/components/audio/cras_audio_handler.cc
@@ -1961,7 +1961,6 @@ void CrasAudioHandler::HandleGetDefaultOutputBufferSize(
 }
 
 bool CrasAudioHandler::noise_cancellation_supported() const {
-  DCHECK(main_task_runner_->BelongsToCurrentThread());
   return noise_cancellation_supported_;
 }
 
