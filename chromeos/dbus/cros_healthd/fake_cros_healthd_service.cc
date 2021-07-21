3157dc0486df0197474d6a788dded6f9b129c23a
Yilin Yang <kerker@google.com>
2021-07-21 03:25:50 +0000

    cros_healthd: add support for audio events
    Expose the new audio observer through cros_healthd's
    ServiceConnection.
    
    BUG=b:186192122
    TEST=chromeos_unittests --gtest_filter=CrosHealthdServiceConnectionTest.*
    
    Change-Id: I5ebac6d1dfc70a3392b0709769bcf257f1d70c38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3016661
    Reviewed-by: Paul Moy <pmoy@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903768}

--- chromeos/dbus/cros_healthd/fake_cros_healthd_service.cc
+++ chromeos/dbus/cros_healthd/fake_cros_healthd_service.cc
@@ -326,6 +326,11 @@ void FakeCrosHealthdService::AddNetworkObserver(
   network_observers_.Add(std::move(observer));
 }
 
+void FakeCrosHealthdService::AddAudioObserver(
+    mojo::PendingRemote<mojom::CrosHealthdAudioObserver> observer) {
+  audio_observers_.Add(std::move(observer));
+}
+
 void FakeCrosHealthdService::ProbeTelemetryInfo(
     const std::vector<mojom::ProbeCategoryEnum>& categories,
     ProbeTelemetryInfoCallback callback) {
@@ -430,6 +435,11 @@ void FakeCrosHealthdService::EmitLidClosedEventForTesting() {
 void FakeCrosHealthdService::EmitLidOpenedEventForTesting() {
   for (auto& observer : lid_observers_)
     observer->OnLidOpened();
+}
+
+void FakeCrosHealthdService::EmitAudioUnderrunEventForTesting() {
+  for (auto& observer : audio_observers_)
+    observer->OnUnderrun();
 }
 
 void FakeCrosHealthdService::EmitConnectionStateChangedEventForTesting(
