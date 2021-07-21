ea155f4f085ceb0d2f202c038030274fecd4f73c
Yeunjoo Choi <ychoi@igalia.com>
2021-07-21 06:57:42 +0000

    Move //chrome/browser/{chromeos => ash}/policy [9/10]
    scheduled_task_handler/, server_backed_state/ and status_collector/ are
    moved to //chrome/browser/ash/policy.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE
    
    Bug: 1164001
    Change-Id: Ie5b2ced7c38ad7bd197e63024c1a9a74448b8709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2984962
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#903802}

--- chrome/browser/ash/login/reporting/login_logout_reporter.h
+++ chrome/browser/ash/login/reporting/login_logout_reporter.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/queue.h"
 #include "base/scoped_observation.h"
 #include "chrome/browser/ash/login/reporting/login_logout_record.pb.h"
-#include "chrome/browser/chromeos/policy/status_collector/managed_session_service.h"
+#include "chrome/browser/ash/policy/status_collector/managed_session_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/login/auth/auth_status_consumer.h"
 #include "components/policy/core/common/cloud/dm_token.h"
--- chrome/browser/chromeos/policy/uploading/status_uploader.cc
+++ chrome/browser/chromeos/policy/uploading/status_uploader.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/syslog_logging.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/ash/policy/core/device_local_account.h"
-#include "chrome/browser/chromeos/policy/status_collector/status_collector.h"
+#include "chrome/browser/ash/policy/status_collector/status_collector.h"
 #include "chromeos/settings/cros_settings_names.h"
 #include "chromeos/settings/cros_settings_provider.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
