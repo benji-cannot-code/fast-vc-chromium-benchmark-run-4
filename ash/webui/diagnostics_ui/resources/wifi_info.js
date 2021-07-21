35d3791d30ca545779f7cc1a653fe481798f5193
Ashley Prasad <ashleydp@google.com>
2021-07-21 15:01:49 +0000

    Diagnostics: Remove deprecated properties from wifi-info
    - Removes the 'show-ip-config-properties' property.
    - Removes html related to IP configuration data.
    - Removes functions which populate IP configuration data.
    
    No visual changes.
    
    Bug: 1197335
    Change-Id: Ia392c3f6c5082ab307599e03dc02e60e752f496a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043159
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#903902}

--- ash/webui/diagnostics_ui/resources/wifi_info.js
+++ ash/webui/diagnostics_ui/resources/wifi_info.js
@@ -27,22 +27,6 @@ Polymer({
     network: {
       type: Object,
     },
-
-    /** @type {boolean} */
-    showIpConfigProperties: {
-      type: Boolean,
-      value: false,
-    },
-  },
-
-  /**
-   * Returns a concatenated list of strings.
-   * @protected
-   * @param {!Array<string>} nameServers
-   * @return {string}
-   */
-  joinNameServers_(nameServers) {
-    return nameServers ? nameServers.join(', ') : '';
   },
 
   /**
@@ -57,14 +41,5 @@ Polymer({
     const channel = convertFrequencyToChannel(frequency);
     const ghz = (frequency / 1000).toFixed(3);
     return `${channel || '?'} (${ghz} GHz)`;
-  },
-
-  /**
-   * @protected
-   * @param {number} prefix
-   * @return {string}
-   */
-  getSubnetMask_(prefix) {
-    return getSubnetMaskFromRoutingPrefix(prefix);
   },
 });
