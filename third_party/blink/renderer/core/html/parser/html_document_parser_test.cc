cfecff01205d70864cc6517290e4bbb99c7390e7
Mason Freed <masonf@chromium.org>
2021-07-21 21:39:36 +0000

    Revert "Modify parser yielding heuristic"
    This reverts commit 6662fe0e891cc25fbc8325b2ed7b10d0e9b43625.
    
    Reason for revert: Looks like it causes this crasher: crbug.com/1231040
    
    Original change's description:
    > Modify parser yielding heuristic
    >
    > This patch modifies the heuristic used to set the budget for the
    > synchronous budgeted HTML parser. It was previously a fixed (but Finch-
    > configurable) value of 250 tokens. It is now non-Finch-configurable,
    > and starts at 250 tokens. But after a total of three yields, the budget
    > is expanded to a very large number.
    >
    > Bug: 1041006
    > Change-Id: Ibe670bcf5a66eab2897ce452ca220b5e10d488fa
    > Cq-Do-Not-Cancel-Tryjobs: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2983494
    > Auto-Submit: Mason Freed <masonf@chromium.org>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Commit-Queue: Mason Freed <masonf@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#903116}
    
    Bug: 1041006
    Bug: 1231040
    Change-Id: Id7207571d35cf8283f0e6956f53ad1a76477dfcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3044439
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904063}

--- third_party/blink/renderer/core/html/parser/html_document_parser_test.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser_test.cc
@@ -35,7 +35,8 @@ class MockNoStatePrefetchClient : public NoStatePrefetchClient {
 
 class HTMLDocumentParserTest
     : public PageTestBase,
-      public testing::WithParamInterface<ParserSynchronizationPolicy>,
+      public testing::WithParamInterface<
+          testing::tuple<ParserSynchronizationPolicy, int>>,
       private ScopedForceSynchronousHTMLParsingForTest {
  protected:
   HTMLDocumentParserTest()
@@ -59,8 +60,9 @@ class HTMLDocumentParserTest
   }
 
   HTMLDocumentParser* CreateParser(HTMLDocument& document) {
-    auto* parser =
-        MakeGarbageCollected<HTMLDocumentParser>(document, GetParam());
+    auto* parser = MakeGarbageCollected<HTMLDocumentParser>(
+        document, testing::get<0>(GetParam()));
+    parser->SetMaxTokenizationBudgetForTesting(testing::get<1>(GetParam()));
     std::unique_ptr<TextResourceDecoder> decoder(
         BuildTextResourceDecoderFor(&document, "text/html", g_null_atom));
     parser->SetDecoder(std::move(decoder));
@@ -68,17 +70,21 @@ class HTMLDocumentParserTest
   }
 
  private:
-  ParserSynchronizationPolicy Policy() const { return GetParam(); }
+  ParserSynchronizationPolicy Policy() const {
+    return testing::get<0>(GetParam());
+  }
 
   bool original_threaded_parsing_;
 };
 
 }  // namespace
 
-INSTANTIATE_TEST_SUITE_P(HTMLDocumentParserTest,
-                         HTMLDocumentParserTest,
-                         testing::Values(kForceSynchronousParsing,
-                                         kAllowDeferredParsing));
+INSTANTIATE_TEST_SUITE_P(
+    HTMLDocumentParserTest,
+    HTMLDocumentParserTest,
+    testing::Combine(testing::Values(kForceSynchronousParsing,
+                                     kAllowDeferredParsing),
+                     testing::Values(250, 500, 1000)));
 
 TEST_P(HTMLDocumentParserTest, StopThenPrepareToStopShouldNotCrash) {
   auto& document = To<HTMLDocument>(GetDocument());
@@ -160,7 +166,7 @@ TEST_P(HTMLDocumentParserTest, AppendNoPrefetch) {
   HTMLParserScriptRunnerHost* script_runner_host =
       parser->AsHTMLParserScriptRunnerHostForTesting();
   EXPECT_EQ(script_runner_host->HasPreloadScanner(),
-            GetParam() == kAllowDeferredParsing);
+            testing::get<0>(GetParam()) == kAllowDeferredParsing);
   EXPECT_EQ(HTMLTokenizer::kTagNameState, parser->Tokenizer()->GetState());
   // Cancel any pending work to make sure that RuntimeFeatures DCHECKs do not
   // fire.
