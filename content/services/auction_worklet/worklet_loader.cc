7c7f500ce5be5a9974b0e25bc28c1563e93a2f38
Maks Orlovich <morlovich@chromium.org>
2021-07-22 21:39:13 +0000

    Fix likely cause of flakes in WorkletLoaderTest.DeleteDuringCallbackSuccess
    We ended up releasing the AuctionV8Helper from WorkerLoader::Result::~Result before the UnboundScript because on-thread release of a RefCountedDeleteOnSequence is synchronous while that of a OnTaskRunnerDeleter is always async.
    
    To fix this, use OnTaskRunnerDelete on the entire state of WorkerLoade::Result so it all gets deleted at once;
    this also removes the awkward unique_ptr of v8::Global.
    
    Bug: 1231690
    Change-Id: I1baba60c28e16ba13a85897387eed4ae7d7da077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043893
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904488}

--- content/services/auction_worklet/worklet_loader.cc
+++ content/services/auction_worklet/worklet_loader.cc
@@ -22,14 +22,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace auction_worklet {
 
 WorkletLoader::Result::Result()
-    : script_(nullptr, base::OnTaskRunnerDeleter(nullptr)) {}
+    : state_(nullptr, base::OnTaskRunnerDeleter(nullptr)) {}
 
 WorkletLoader::Result::Result(scoped_refptr<AuctionV8Helper> v8_helper,
                               v8::Global<v8::UnboundScript> script)
-    : v8_helper_(std::move(v8_helper)),
-      script_(new v8::Global<v8::UnboundScript>(std::move(script)),
-              base::OnTaskRunnerDeleter(v8_helper_->v8_runner())) {
-  DCHECK(v8_helper_->v8_runner()->RunsTasksInCurrentSequence());
+    : state_(new V8Data(v8_helper, std::move(script)),
+             base::OnTaskRunnerDeleter(v8_helper->v8_runner())) {
+  DCHECK(v8_helper->v8_runner()->RunsTasksInCurrentSequence());
 }
 
 WorkletLoader::Result::Result(Result&&) = default;
@@ -37,11 +36,14 @@ WorkletLoader::Result::~Result() = default;
 WorkletLoader::Result& WorkletLoader::Result::operator=(Result&&) = default;
 
 v8::Global<v8::UnboundScript> WorkletLoader::Result::TakeScript() {
-  DCHECK(v8_helper_->v8_runner()->RunsTasksInCurrentSequence());
-  v8::Global<v8::UnboundScript> result = script_->Pass();
-  script_.reset();
-  return result;
+  DCHECK(state_->v8_helper->v8_runner()->RunsTasksInCurrentSequence());
+  return state_->script.Pass();
 }
+
+WorkletLoader::Result::V8Data::V8Data(scoped_refptr<AuctionV8Helper> v8_helper,
+                                      v8::Global<v8::UnboundScript> script)
+    : v8_helper(std::move(v8_helper)), script(std::move(script)) {}
+WorkletLoader::Result::V8Data::~V8Data() = default;
 
 WorkletLoader::WorkletLoader(
     network::mojom::URLLoaderFactory* url_loader_factory,
