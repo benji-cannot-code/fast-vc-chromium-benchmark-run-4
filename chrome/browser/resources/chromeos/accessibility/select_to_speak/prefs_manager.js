e17a18b3f5840e6b5f3a6f9b092824c1a045707f
Ajit Narayanan <ajitnarayanan@google.com>
2021-07-22 01:08:27 +0000

    Show confirmation dialog to turn on natural voices in STS.
    The first time the user starts Select-to-speak after updating ChromeOS
    to a version that supports natural voices, STS shows a popup asking if
    the user would like to use the new network-based voices. Also add tests
    to ensure the user's choice is reflected in the setting.
    
    
    Screenshot: https://screenshot.googleplex.com/AbxkpjfVpaBWzBZ
    
    AX-Relnotes: n/a
    Change-Id: I7742e98d12807e3920431a50b0ef4b600564f12c
    Bug: 1230191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3034977
    Commit-Queue: Ajit Narayanan <ajitnarayanan@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904139}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/prefs_manager.js
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/prefs_manager.js
@@ -43,6 +43,9 @@ export class PrefsManager {
 
     /** @private {boolean} */
     this.enhancedNetworkVoicesEnabled_ = true;
+
+    /** @private {boolean} */
+    this.enhancedVoicesDialogShown_ = false;
   }
 
   /**
@@ -232,7 +235,8 @@ export class PrefsManager {
       chrome.storage.sync.get(
           [
             'voice', 'rate', 'pitch', 'wordHighlight', 'highlightColor',
-            'backgroundShading', 'navigationControls', 'enhancedNetworkVoices'
+            'backgroundShading', 'navigationControls', 'enhancedNetworkVoices',
+            'enhancedVoicesDialogShown'
           ],
           (prefs) => {
             if (prefs['voice']) {
@@ -266,6 +270,14 @@ export class PrefsManager {
             } else {
               chrome.storage.sync.set({
                 'enhancedNetworkVoices': this.enhancedNetworkVoicesEnabled_
+              });
+            }
+            if (prefs['enhancedVoicesDialogShown'] !== undefined) {
+              this.enhancedVoicesDialogShown_ =
+                  prefs['enhancedVoicesDialogShown'];
+            } else {
+              chrome.storage.sync.set({
+                'enhancedVoicesDialogShown': this.enhancedVoicesDialogShown_
               });
             }
             if (prefs['rate'] && prefs['pitch']) {
@@ -372,6 +384,32 @@ export class PrefsManager {
    */
   enhancedNetworkVoicesEnabled() {
     return this.enhancedNetworkVoicesEnabled_;
+  }
+
+  /**
+   * Gets whether the initial popup authorizing enhanced network voices has been
+   * shown to the user or not.
+   *
+   * @returns {boolean} True if the initial popup dialog has been shown already.
+   */
+  enhancedVoicesDialogShown() {
+    return this.enhancedVoicesDialogShown_;
+  }
+
+  /**
+   * Sets whether enhanced network voices are enabled or not from initial popup.
+   * @param {boolean} enabled Specifies if the user enabled enhanced voices in
+   *     the popup.
+   */
+  setEnhancedNetworkVoicesFromDialog(enabled) {
+    if (enabled !== undefined) {
+      this.enhancedNetworkVoicesEnabled_ = enabled;
+      chrome.storage.sync.set(
+          {'enhancedNetworkVoices': this.enhancedNetworkVoicesEnabled_});
+      this.enhancedVoicesDialogShown_ = true;
+      chrome.storage.sync.set(
+          {'enhancedVoicesDialogShown': this.enhancedVoicesDialogShown_});
+    }
   }
 }
 
