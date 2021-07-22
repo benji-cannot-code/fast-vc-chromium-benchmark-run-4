24f659ee471812ffcc2a9c572ca3058208db615c
Peter Kotwicz <pkotwicz@chromium.org>
2021-07-22 21:57:24 +0000

    Remove @RemovableInRelease usage in //base
    This CL is a step towards deleting @RemovableInRelease. (See
    linked crbug for rationale)
    
    This CL:
    - replaces @RemovableInRelease usage
      in org.chromium.base.Log with dedicated proguard rules.
    - Removes stale comments in org.chromium.base.Log and
      org.chromium.base.library_loader.LibraryLoader
    - Removes references to @RemovableInRelease in //docs/android_logging.md
    - Adds Logging#isLoggingEnabled() and adds recommendation in
      //docs/android_logging.md to guard logging-related code with it.
    
    BUG=1180332
    
    Change-Id: I84a58496dce593259e92bd5b4e30b45e4a9a93c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043622
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904502}

--- base/android/java/src/org/chromium/base/Log.java
+++ base/android/java/src/org/chromium/base/Log.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base;
 
-import org.chromium.base.annotations.RemovableInRelease;
+import org.chromium.base.annotations.CheckDiscard;
 
 import java.util.Locale;
 
@@ -83,15 +83,14 @@ public class Log {
         return "[" + getCallOrigin() + "] " + formatLog(messageTemplate, tr, params);
     }
 
-    @RemovableInRelease
     private static boolean isDebug() {
-        // @RemovableInRelease causes this to return false in release builds.
+        // Proguard sets value to false in release builds.
         return true;
     }
 
     /**
-     * In debug: Forwards to {@link android.util.Log#isLoggable(String, int)}, but alway
-     * In release: Always returns false (via @RemovableInRelease).
+     * In debug: Forwards to {@link android.util.Log#isLoggable(String, int)}, but always
+     * In release: Always returns false (via proguard rule).
      */
     public static boolean isLoggable(String tag, int level) {
         // Early return helps optimizer eliminate calls to isLoggable().
@@ -104,10 +103,6 @@ public class Log {
     /**
      * Sends a {@link android.util.Log#VERBOSE} log message.
      *
-     * For optimization purposes, only the fixed parameters versions are visible. If you need more
-     * than 7 parameters, consider building your log message using a function annotated with
-     * {@link RemovableInRelease}.
-     *
      * @param tag Used to identify the source of a log message. Might be modified in the output
      *            (see {@link #normalizeTag(String)})
      * @param messageTemplate The message you would like logged. It is to be specified as a format
@@ -115,8 +110,10 @@ public class Log {
      * @param args Arguments referenced by the format specifiers in the format string. If the last
      *             one is a {@link Throwable}, its trace will be printed.
      */
-    @RemovableInRelease
+    @CheckDiscard("crbug.com/1231625")
     public static void v(String tag, String messageTemplate, Object... args) {
+        if (!isDebug()) return;
+
         Throwable tr = getThrowableToLog(args);
         String message = formatLogWithStack(messageTemplate, tr, args);
         if (tr != null) {
@@ -129,10 +126,6 @@ public class Log {
     /**
      * Sends a {@link android.util.Log#DEBUG} log message.
      *
-     * For optimization purposes, only the fixed parameters versions are visible. If you need more
-     * than 7 parameters, consider building your log message using a function annotated with
-     * {@link RemovableInRelease}.
-     *
      * @param tag Used to identify the source of a log message. Might be modified in the output
      *            (see {@link #normalizeTag(String)})
      * @param messageTemplate The message you would like logged. It is to be specified as a format
@@ -140,8 +133,10 @@ public class Log {
      * @param args Arguments referenced by the format specifiers in the format string. If the last
      *             one is a {@link Throwable}, its trace will be printed.
      */
-    @RemovableInRelease
+    @CheckDiscard("crbug.com/1231625")
     public static void d(String tag, String messageTemplate, Object... args) {
+        if (!isDebug()) return;
+
         Throwable tr = getThrowableToLog(args);
         String message = formatLogWithStack(messageTemplate, tr, args);
         if (tr != null) {
@@ -250,7 +245,7 @@ public class Log {
     }
 
     /** Returns a string form of the origin of the log call, to be used as secondary tag.*/
-    @RemovableInRelease
+    @CheckDiscard("crbug.com/1231625")
     private static String getCallOrigin() {
         StackTraceElement[] st = Thread.currentThread().getStackTrace();
 
