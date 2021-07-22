c3564474fa4d3bfd5ed87786f417a68a2edef539
Gabriel Charette <gab@chromium.org>
2021-07-22 17:36:29 +0000

    [base] Improve logging of DCHECKs in lock.cc
    This proved useful when debugging crbug.com/1209622#c55
    
    R=danakj@chromium.org
    
    Bug: 1209622
    Change-Id: Ia588e561fae01d16050d15e18ca8eb39d71b4a30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043085
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904384}

--- base/threading/platform_thread.cc
+++ base/threading/platform_thread.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <atomic>
 #include <memory>
+#include <ostream>
 
 #include "base/feature_list.h"
 #include "base/time/time.h"
@@ -28,6 +29,11 @@ const Feature kThreadPrioritiesFeature{"ThreadPriorities",
 std::atomic<bool> g_use_thread_priorities(true);
 
 }  // namespace
+
+std::ostream& operator<<(std::ostream& os, const PlatformThreadRef& ref) {
+  os << ref.id_;
+  return os;
+}
 
 // static
 void PlatformThread::SetCurrentThreadPriority(ThreadPriority priority) {
--- base/threading/platform_thread.h
+++ base/threading/platform_thread.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <iosfwd>
+
 #include "base/base_export.h"
 #include "base/macros.h"
 #include "base/time/time.h"
@@ -57,7 +59,7 @@ class PlatformThreadRef {
 #else  //  OS_POSIX
   typedef pthread_t RefType;
 #endif
-  constexpr PlatformThreadRef() : id_(0) {}
+  constexpr PlatformThreadRef() = default;
 
   explicit constexpr PlatformThreadRef(RefType id) : id_(id) {}
 
@@ -70,9 +72,16 @@ class PlatformThreadRef {
   bool is_null() const {
     return id_ == 0;
   }
+
  private:
-  RefType id_;
+  friend BASE_EXPORT std::ostream& operator<<(std::ostream& os,
+                                              const PlatformThreadRef& ref);
+
+  RefType id_ = 0;
 };
+
+BASE_EXPORT std::ostream& operator<<(std::ostream& os,
+                                     const PlatformThreadRef& ref);
 
 // Used to operate on threads.
 class PlatformThreadHandle {
