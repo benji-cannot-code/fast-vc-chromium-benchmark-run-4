8b6c5bf9c29c216d4e292f8bbed5de267610adf6
David Maunder <davidjm@chromium.org>
2021-07-22 22:37:22 +0000

    Persist currency code across restarts in ShoppingPersistedTabData
    Not doing so is responsible for a bug where the currency symbol is
    not shown after a restart. The reason is after a restart the currency
    code is empty and the currency formatter doesn't know what currency
    symbol to use.
    
    Bug: 1231828
    Change-Id: Idaf35ea724078db4983f1123d7ab419b8ad59352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3044842
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904519}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/ShoppingPersistedTabData.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/ShoppingPersistedTabData.java
@@ -768,6 +768,10 @@ public class ShoppingPersistedTabData extends PersistedTabData {
             builder.setMainOfferId(mPriceDropData.offerId);
         }
 
+        if (mPriceDropData.currencyCode != null) {
+            builder.setPriceCurrencyCode(mPriceDropData.currencyCode);
+        }
+
         return () -> {
             return builder.build().toByteString().asReadOnlyByteBuffer();
         };
@@ -789,6 +793,7 @@ public class ShoppingPersistedTabData extends PersistedTabData {
             setLastUpdatedMs(shoppingPersistedTabDataProto.getLastUpdatedMs());
             mLastPriceChangeTimeMs = shoppingPersistedTabDataProto.getLastPriceChangeTimeMs();
             mPriceDropData.offerId = shoppingPersistedTabDataProto.getMainOfferId();
+            mPriceDropData.currencyCode = shoppingPersistedTabDataProto.getPriceCurrencyCode();
             mPriceDropMetricsLogger = new PriceDropMetricsLogger(this);
             return true;
         } catch (InvalidProtocolBufferException e) {
