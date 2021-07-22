201104830f855135344721bb61aeadd621077e5f
Joey Arhar <jarhar@chromium.org>
2021-07-22 23:27:39 +0000

    Tint accent-color for hover and press
    When hovering and pressing clickable form controls, different shades of
    blue are used to render the control to show feedback.
    This patch replicates this behavior when accent-color is used by
    applying the same difference in luminance to the accent-color when
    controls are hovered and pressed.
    
    Fixed: 1228930
    Change-Id: I48891273004972a6eed89aac645518e326c28dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043360
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904537}

--- ui/native_theme/native_theme_base.cc
+++ ui/native_theme/native_theme_base.cc
@@ -64,6 +64,22 @@ const SkScalar kTrackHeightRatio = 8.0f / 16;
 const SkScalar kMenuListArrowStrokeWidth = 2.f;
 const int kSliderThumbSize = 16;
 
+// This value was created with the following steps:
+// 1. Take the SkColors returned by GetControlColor for kAccent and
+//    kHoveredAccent.
+// 2. use color_utils::SkColorToHSL to convert those colors to HSL.
+// 3. Take the difference of the luminance component of the HSL between those
+//    two colors.
+// 4. Round to the nearest two decimal points.
+//
+// This is used to emulate the changes in color used for hover and pressed
+// states when a custom accent-color is used to draw form controls. It just so
+// happens that the luminance difference is the same for hover and press, and it
+// also happens that GetDarkModeControlColor has very close values when you run
+// these steps, which makes it work well for forced color-scheme for contrast
+// with certain accent-colors.
+const double kAccentLuminanceAdjust = 0.11;
+
 // Get a color constant based on color-scheme
 SkColor GetColor(const SkColor colors[2],
                  ui::NativeTheme::ColorScheme color_scheme) {
@@ -108,6 +124,33 @@ ui::NativeTheme::ColorScheme ColorSchemeForAccentColor(
   }
 
   return color_scheme;
+}
+
+SkColor AdjustLuminance(const SkColor& color, double luminance) {
+  color_utils::HSL hsl;
+  color_utils::SkColorToHSL(color, &hsl);
+  hsl.l = base::ClampToRange(hsl.l + luminance, 0., 1.);
+  return color_utils::HSLToSkColor(hsl, SkColorGetA(color));
+}
+
+SkColor CustomAccentColorForState(
+    const SkColor& accent_color,
+    ui::NativeTheme::State state,
+    const ui::NativeTheme::ColorScheme& color_scheme) {
+  bool make_lighter = false;
+  bool is_dark_mode = color_scheme == ui::NativeTheme::ColorScheme::kDark;
+  switch (state) {
+    case ui::NativeTheme::kHovered:
+      make_lighter = is_dark_mode;
+      break;
+    case ui::NativeTheme::kPressed:
+      make_lighter = !is_dark_mode;
+      break;
+    default:
+      return accent_color;
+  }
+  return AdjustLuminance(accent_color,
+                         (make_lighter ? 1 : -1) * kAccentLuminanceAdjust);
 }
 
 }  // namespace
@@ -609,7 +652,8 @@ void NativeThemeBase::PaintCheckbox(
       // Draw the accent background.
       flags.setStyle(cc::PaintFlags::kFill_Style);
       if (accent_color && state != kDisabled) {
-        flags.setColor(*accent_color);
+        flags.setColor(
+            CustomAccentColorForState(*accent_color, state, color_scheme));
       } else {
         flags.setColor(ControlsAccentColorForState(state, color_scheme));
       }
@@ -659,7 +703,8 @@ SkRect NativeThemeBase::PaintCheckboxRadioCommon(
   if (skrect.width() <= 2) {
     cc::PaintFlags flags;
     if (accent_color && state != kDisabled) {
-      flags.setColor(*accent_color);
+      flags.setColor(
+          CustomAccentColorForState(*accent_color, state, color_scheme));
     } else {
       flags.setColor(GetControlColor(kBorder, color_scheme));
     }
@@ -695,7 +740,8 @@ SkRect NativeThemeBase::PaintCheckboxRadioCommon(
     SkColor border_color;
     if (button.checked && !button.indeterminate) {
       if (accent_color && state != kDisabled) {
-        border_color = *accent_color;
+        border_color =
+            CustomAccentColorForState(*accent_color, state, color_scheme);
       } else {
         border_color = ControlsAccentColorForState(state, color_scheme);
       }
@@ -742,7 +788,8 @@ void NativeThemeBase::PaintRadio(
     flags.setAntiAlias(true);
     flags.setStyle(cc::PaintFlags::kFill_Style);
     if (accent_color && state != kDisabled) {
-      flags.setColor(*accent_color);
+      flags.setColor(
+          CustomAccentColorForState(*accent_color, state, color_scheme));
     } else {
       flags.setColor(ControlsAccentColorForState(state, color_scheme));
     }
@@ -959,9 +1006,8 @@ void NativeThemeBase::PaintSliderTrack(
 
   // Paint the value slider track.
   if (accent_color && state != kDisabled) {
-    // TODO(crbug.com/1092093): Decide what to do when state is kHovered or
-    // kPressed.
-    flags.setColor(*accent_color);
+    flags.setColor(
+        CustomAccentColorForState(*accent_color, state, color_scheme));
   } else {
     flags.setColor(ControlsSliderColorForState(state, color_scheme));
   }
@@ -1015,9 +1061,8 @@ void NativeThemeBase::PaintSliderThumb(
   // Paint the background (is not visible behind the rounded corners).
   thumb_rect.inset(border_width / 2, border_width / 2);
   if (accent_color && state != kDisabled) {
-    // TODO(crbug.com/1092093): Decide what to do when state is kHovered or
-    // kPressed.
-    flags.setColor(*accent_color);
+    flags.setColor(
+        CustomAccentColorForState(*accent_color, state, color_scheme));
   } else {
     flags.setColor(ControlsSliderColorForState(state, color_scheme));
   }
