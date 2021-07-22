b24ef5248dbb35560e45faf884c13e1878f8c682
tom <tluk@chromium.org>
2021-07-22 13:03:14 +0000

    CodeHealth: Remove use of Value::GetAsString from //components/policy
    This change removes the use of Value::GetAsString and replaces with
    Value::is_string, Value::GetString(), Value::GetIfString().
    
    Bug: 1187007
    Change-Id: Idab85c4911709d0a5593f64240a520e8d72a4dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033106
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904292}

--- components/policy/core/browser/url_blocklist_policy_handler.cc
+++ components/policy/core/browser/url_blocklist_policy_handler.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/logging.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/values.h"
@@ -110,10 +111,9 @@ void URLBlocklistPolicyHandler::ApplyPolicySettings(const PolicyMap& policies,
   // handling URLBlocklists.
   if (disabled_schemes) {
     for (const auto& entry : disabled_schemes->GetList()) {
-      std::string entry_value;
-      if (entry.GetAsString(&entry_value)) {
-        entry_value.append("://*");
-        merged_url_blocklist.emplace_back(std::move(entry_value));
+      if (entry.is_string()) {
+        merged_url_blocklist.emplace_back(
+            base::StrCat({entry.GetString(), "://*"}));
       }
     }
   }
