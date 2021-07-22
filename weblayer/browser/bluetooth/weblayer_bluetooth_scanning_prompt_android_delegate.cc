143bdb33237fed48e9f4f4ec7ad6f3eb96dbe45e
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2021-07-22 21:56:06 +0000

    Add WebLayerBluetoothDelegateImplClient and some deps.
    These new classes mirror similar ones in Chrome:
    * WebLayerBluetoothDelegateImplClient
    * WebLayerBluetoothChooserContextFactory
    * WebLayerBluetoothChooserAndroidDelegate
    * WebLayerBluetoothScanningPromptAndroidDelegate
    
    Due to missing dialog support on desktop in weblayer
    (chrome::ShowDeviceChooserDialog) this works on Android only.
    
    Bug: 1076462
    Change-Id: Ie15879687963f9dc730f824b6fcfd9ff7ac77fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3034893
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904500}

---
+++ weblayer/browser/bluetooth/weblayer_bluetooth_scanning_prompt_android_delegate.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "weblayer/browser/bluetooth/weblayer_bluetooth_scanning_prompt_android_delegate.h"
+
+#include "base/android/jni_android.h"
+#include "components/security_state/content/content_utils.h"
+#include "weblayer/browser/java/jni/WebLayerBluetoothScanningPromptAndroidDelegate_jni.h"
+
+namespace weblayer {
+
+WebLayerBluetoothScanningPromptAndroidDelegate::
+    WebLayerBluetoothScanningPromptAndroidDelegate() {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  java_delegate_.Reset(
+      Java_WebLayerBluetoothScanningPromptAndroidDelegate_create(env));
+}
+
+WebLayerBluetoothScanningPromptAndroidDelegate::
+    ~WebLayerBluetoothScanningPromptAndroidDelegate() = default;
+
+base::android::ScopedJavaLocalRef<jobject>
+WebLayerBluetoothScanningPromptAndroidDelegate::GetJavaObject() {
+  return base::android::ScopedJavaLocalRef<jobject>(java_delegate_);
+}
+
+security_state::SecurityLevel
+WebLayerBluetoothScanningPromptAndroidDelegate::GetSecurityLevel(
+    content::WebContents* web_contents) {
+  auto state = security_state::GetVisibleSecurityState(web_contents);
+  return security_state::GetSecurityLevel(
+      *state,
+      /*used_policy_installed_certificate=*/false);
+}
+
+}  // namespace weblayer
---
+++ weblayer/browser/java/org/chromium/weblayer_private/bluetooth/WebLayerBluetoothChooserAndroidDelegate.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private.bluetooth;
+
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.components.omnibox.AutocompleteSchemeClassifier;
+import org.chromium.components.permissions.BluetoothChooserAndroidDelegate;
+import org.chromium.weblayer_private.AutocompleteSchemeClassifierImpl;
+
+/**
+ *  The implementation of {@link BluetoothChooserAndroidDelegate} for WebLayer.
+ */
+@JNINamespace("weblayer")
+public class WebLayerBluetoothChooserAndroidDelegate implements BluetoothChooserAndroidDelegate {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public AutocompleteSchemeClassifier createAutocompleteSchemeClassifier() {
+        return new AutocompleteSchemeClassifierImpl();
+    }
+
+    @CalledByNative
+    private static WebLayerBluetoothChooserAndroidDelegate create() {
+        return new WebLayerBluetoothChooserAndroidDelegate();
+    }
+}
