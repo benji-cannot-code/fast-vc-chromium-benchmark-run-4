8363c167565e1245fad91e7fe2a1530fcaec643a
Ewann <ewannpv@google.com>
2021-07-22 14:10:14 +0000

    [iOS Enterprise] Adds RestrictAccountsToPatterns policy
    This CL adds the RestrictAccountsToPatterns policy.
    
    Bug: 1194919
    Change-Id: Ifd94791f95df015e2a907ce8e09dc1cfddd824c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024284
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904312}

--- ios/chrome/browser/signin/chrome_account_manager_service_factory.mm
+++ ios/chrome/browser/signin/chrome_account_manager_service_factory.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
 
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
+#include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/signin/chrome_account_manager_service.h"
 
@@ -37,8 +38,6 @@ ChromeAccountManagerServiceFactory::~ChromeAccountManagerServiceFactory() =
 std::unique_ptr<KeyedService>
 ChromeAccountManagerServiceFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
-  ChromeBrowserState* browser_state =
-      ChromeBrowserState::FromBrowserState(context);
   return std::make_unique<ChromeAccountManagerService>(
-      browser_state->GetPrefs());
+      GetApplicationContext()->GetLocalState());
 }
