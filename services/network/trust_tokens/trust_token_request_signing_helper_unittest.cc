812730086ef18a8a31454f57d517f5621aea02f3
Callum May <camay@microsoft.com>
2021-07-22 19:14:39 +0000

    [Trust Tokens] Add nested alg in trust token request signing
    We move the alg field into the nested signature list inside the
    Sec-Signature header so we can support multiple signature algorithms
    per signed request in the future.
    
    Fixed: 1202232, 1205658
    Change-Id: If7b01654f062f48fcb1e0451edbf9c40ab4f0022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033541
    Commit-Queue: Callum May <camay@microsoft.com>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904427}

--- services/network/trust_tokens/trust_token_request_signing_helper_unittest.cc
+++ services/network/trust_tokens/trust_token_request_signing_helper_unittest.cc
@@ -72,7 +72,7 @@ class FakeSigner : public TrustTokenRequestSigningHelper::Signer {
     return false;
   }
 
-  std::string GetAlgorithmIdentifier() override { return "fake-signer"; }
+  std::string GetAlgorithmIdentifier() const override { return "fake-signer"; }
 };
 
 // IdentitySigner returns a "signature" over given signing data whose value
@@ -91,7 +91,9 @@ class IdentitySigner : public TrustTokenRequestSigningHelper::Signer {
               base::span<const uint8_t> verification_key) override {
     return std::equal(data.begin(), data.end(), signature.begin());
   }
-  std::string GetAlgorithmIdentifier() override { return "identity-signer"; }
+  std::string GetAlgorithmIdentifier() const override {
+    return "identity-signer";
+  }
 };
 
 // FailingSigner always fails the Sign and Verify options.
@@ -107,7 +109,9 @@ class FailingSigner : public TrustTokenRequestSigningHelper::Signer {
               base::span<const uint8_t> verification_key) override {
     return false;
   }
-  std::string GetAlgorithmIdentifier() override { return "failing-signer"; }
+  std::string GetAlgorithmIdentifier() const override {
+    return "failing-signer";
+  }
 };
 
 // Reconstructs |request|'s canonical request data, extracts the signatures from
