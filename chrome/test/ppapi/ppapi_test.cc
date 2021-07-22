e79988639c320bf2e1b170deb5cb1fab0c4c7803
Lingqi Chi <lingqi@chromium.org>
2021-07-22 08:33:36 +0000

    Sheriff: disable OutOfProcessPPAPITest.Printing on Win7
    Bug: 1231528
    Change-Id: I25dded3fc60a3907df471ce7f8095a35501dd114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041074
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904228}

--- chrome/test/ppapi/ppapi_test.cc
+++ chrome/test/ppapi/ppapi_test.cc
@@ -50,6 +50,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "ui/gl/gl_switches.h"
 
+#if defined(OS_WIN)
+#include "base/win/windows_version.h"
+#endif
+
 using content::RenderViewHost;
 using content::TestMessageHandler;
 
@@ -313,6 +317,18 @@ OutOfProcessPPAPITest::OutOfProcessPPAPITest() {
 void OutOfProcessPPAPITest::SetUpCommandLine(base::CommandLine* command_line) {
   PPAPITest::SetUpCommandLine(command_line);
   command_line->AppendSwitch(switches::kUseFakeUIForMediaStream);
+}
+
+void OutOfProcessPPAPITest::RunTest(const std::string& test_case) {
+  // TODO(crbug.com/1231528): Investigate why this test fails on Win 7 bots.
+#if defined(OS_WIN)
+  if (test_case == "Printing" &&
+      base::win::GetVersion() <= base::win::Version::WIN7) {
+    return;
+  }
+#endif
+
+  PPAPITestBase::RunTest(test_case);
 }
 
 // Send touch events to a plugin and expect the events to reach the renderer
