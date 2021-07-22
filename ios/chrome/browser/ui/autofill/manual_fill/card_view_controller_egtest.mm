c65ecae166a5f980718000f1701c04ceb0ad5663
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-07-22 15:28:56 +0000

    [iOS][MF] Remove obsolete tests
    Testing logic around docking and undocking the keyboard is not necessary
    now that the system is in charge of adding the input views in the right
    place.
    See https://chromium-review.googlesource.com/c/chromium/src/+/3042590
    
    Bug: 1218869, 1221204
    Change-Id: Ie26bdcf960a70b8c819094b265c0c8fbd7f45faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042257
    Auto-Submit: Javier Flores <javierrobles@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904335}

--- ios/chrome/browser/ui/autofill/manual_fill/card_view_controller_egtest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/card_view_controller_egtest.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/app_launch_configuration.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
-#import "ios/testing/earl_grey/keyboard_app_interface.h"
 #include "ios/web/public/test/element_selector.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "url/gurl.h"
