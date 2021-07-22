65b963099f53013d0b09c46533ce4ecd92eeef75
Hans Wennborg <hans@chromium.org>
2021-07-22 11:58:15 +0000

    Don't include wtf/string_builder.h in exception_messages.h
    This should save almost 700 MB of compiler input [1].
    
    [1] https://commondatastorage.googleapis.com/chromium-browser-clang/chrome_includes_2021-07-22_034738.html#view=edges&includer=%5Ethird_party%2Fblink%2Frenderer%2Fplatform%2Fbindings%2Fexception_messages%5C.h
    
    Bug: 1227131,242216
    Change-Id: I51030b4e90057f226ad9bffb12a4101ae118e44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042146
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904278}

--- third_party/blink/renderer/modules/webaudio/iir_filter_node.cc
+++ third_party/blink/renderer/modules/webaudio/iir_filter_node.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
+#include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
 
