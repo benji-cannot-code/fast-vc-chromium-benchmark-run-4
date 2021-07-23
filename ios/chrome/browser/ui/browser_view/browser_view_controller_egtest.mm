82e5f1cbe5690cb12b986378ec67216897f3362a
Justin Cohen <justincohen@google.com>
2021-07-23 22:23:37 +0000

    ios: Disable flakey BrowserViewControllerTestCase/testMultiWindowURLLoading.
    See: chrome/builders/ci/ipad-simulator/23160/overview
      BrowserViewControllerTestCase/testMultiWindowURLLoading
    
    `
    
    Bug: 1232517
    Change-Id: Id49f5ee7a81a846dcd51777a3cea48b5711034bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3049194
    Owners-Override: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904914}

--- ios/chrome/browser/ui/browser_view/browser_view_controller_egtest.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller_egtest.mm
@@ -208,7 +208,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - Multiwindow
 
-- (void)testMultiWindowURLLoading {
+// TODO(crbug.com/1232517) Re-enable test.
+- (void)DISABLED_testMultiWindowURLLoading {
   if (![ChromeEarlGrey areMultipleWindowsSupported])
     EARL_GREY_TEST_DISABLED(@"Multiple windows can't be opened.");
 
