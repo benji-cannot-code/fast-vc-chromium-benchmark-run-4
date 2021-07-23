53dcf55d15d78b85d009730ffa709d4fa5fa2a95
Jason Zhang <jiajunz@google.com>
2021-07-23 17:15:02 +0000

    [CrOS Network] Add CellularESimInstaller class for ESim installation
    This CL adds a new CellularESimInstaller which is responsible for
    handling Esim profile installation and its corresponding network.
    
    Bug: 1231305
    Change-Id: If6aa94456ab67a930777fb7bddf98ae037da9045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3044784
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904813}

--- chromeos/network/network_handler.cc
+++ chromeos/network/network_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "chromeos/network/auto_connect_handler.h"
 #include "chromeos/network/cellular_connection_handler.h"
+#include "chromeos/network/cellular_esim_installer.h"
 #include "chromeos/network/cellular_esim_profile_handler_impl.h"
 #include "chromeos/network/cellular_esim_uninstall_handler.h"
 #include "chromeos/network/cellular_inhibitor.h"
@@ -53,6 +54,7 @@ NetworkHandler::NetworkHandler()
       new ManagedNetworkConfigurationHandlerImpl());
   network_connection_handler_.reset(new NetworkConnectionHandlerImpl());
   if (features::IsCellularActivationUiEnabled()) {
+    cellular_esim_installer_.reset(new CellularESimInstaller());
     cellular_esim_uninstall_handler_.reset(new CellularESimUninstallHandler());
   }
   cellular_metrics_logger_.reset(new CellularMetricsLogger());
@@ -98,6 +100,9 @@ void NetworkHandler::Init() {
       managed_network_configuration_handler_.get(),
       cellular_connection_handler_.get());
   if (features::IsCellularActivationUiEnabled()) {
+    cellular_esim_installer_->Init(
+        cellular_connection_handler_.get(), cellular_inhibitor_.get(),
+        network_connection_handler_.get(), network_state_handler_.get());
     cellular_esim_uninstall_handler_->Init(
         cellular_inhibitor_.get(), cellular_esim_profile_handler_.get(),
         network_configuration_handler_.get(), network_connection_handler_.get(),
@@ -197,6 +202,10 @@ AutoConnectHandler* NetworkHandler::auto_connect_handler() {
 
 CellularConnectionHandler* NetworkHandler::cellular_connection_handler() {
   return cellular_connection_handler_.get();
+}
+
+CellularESimInstaller* NetworkHandler::cellular_esim_installer() {
+  return cellular_esim_installer_.get();
 }
 
 CellularESimProfileHandler* NetworkHandler::cellular_esim_profile_handler() {
