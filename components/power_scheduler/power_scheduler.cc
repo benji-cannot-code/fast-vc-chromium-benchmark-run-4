eacd6dce6abd11364e161d7290f64f10a35ee0dd
Eric Seckler <eseckler@chromium.org>
2021-07-23 15:09:44 +0000

    PowerScheduler: Add a field trial param to include charging
    This param will also allow us to test/benchmark the power scheduling
    feature in the lab -- For this purpose, this patch also adds a
    field trial testing config.
    
    Bug: 1232346,1166695
    Change-Id: I0ec3e3239f8eb712ff5fb7370dc334a6270360da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3048431
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/master@{#904760}

--- components/power_scheduler/power_scheduler.cc
+++ components/power_scheduler/power_scheduler.cc
@@ -95,6 +95,7 @@ static const char kPolicyThrottleIdleAndNopAnimation[] =
 static const char kMinTimeInModeMsKey[] = "min_time_in_mode_ms";
 static const char kMinCputimeRatioKey[] = "min_cputime_ratio";
 static const char kProcessTypesKey[] = "process_types";
+static const char kIncludeChargingKey[] = "include_charging";
 
 }  // anonymous namespace
 
@@ -256,6 +257,10 @@ void PowerScheduler::InitializePolicyFromFeatureList() {
         return;
       }
     }
+
+    bool include_charging = field_trial_params[kIncludeChargingKey] == "true";
+    if (include_charging)
+      arbiter_->SetChargingModeEnabled(false);
   }
 
   SetPolicy(params);
