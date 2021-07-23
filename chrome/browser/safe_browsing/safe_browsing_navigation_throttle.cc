aa1a765acedf6c564c5e540fcb4877a6c25922dc
Colin Blundell <blundell@chromium.org>
2021-07-23 06:10:46 +0000

    [Safe Browsing] Abstract nav throttle dep on enterprise_utils.h
    As the last step before componentizing //chrome's
    SafeBrowsingNavigationThrottle for sharing with WebLayer, this CL
    abstracts its dependence on //chrome's enterprise_utils.h through
    SafeBrowsingUIManager. The latter already has a hook out to the
    embedder to invoke this functionality.
    
    Bug: 1226567
    Change-Id: Ia963645018937006aaaa093e16ba0224d3e68059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043638
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904646}

--- chrome/browser/safe_browsing/safe_browsing_navigation_throttle.cc
+++ chrome/browser/safe_browsing/safe_browsing_navigation_throttle.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/safe_browsing_navigation_throttle.h"
 
 #include "base/memory/ptr_util.h"
-#include "chrome/browser/interstitials/enterprise_util.h"
 #include "components/safe_browsing/content/browser/safe_browsing_blocking_page.h"
 #include "components/safe_browsing/content/browser/safe_browsing_blocking_page_factory.h"
 #include "components/safe_browsing/content/browser/ui_manager.h"
@@ -39,7 +38,7 @@ SafeBrowsingNavigationThrottle::WillFailRequest() {
         manager_->blocking_page_factory()->CreateSafeBrowsingPage(
             manager_, handle->GetWebContents(), handle->GetURL(), {resource},
             true);
-    MaybeTriggerSecurityInterstitialShownEvent(
+    manager_->ForwardSecurityInterstitialShownExtensionEventToEmbedder(
         handle->GetWebContents(), handle->GetURL(),
         SafeBrowsingUIManager::GetThreatTypeStringForInterstitial(
             resource.threat_type),
