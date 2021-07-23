542f17d7ad9809f82d1bed80ea1ecde53189d0c5
Sharon Yang <yangsharon@chromium.org>
2021-07-23 15:25:51 +0000

    Explicitly isolate origin in browsertest
    Ensure bar.com is cross process in
    ProcessManagementExtensionIsolationTest.ExtensionAndWebProcessOverflow
    by explicitly isolated.
    
    Bug: 1158650
    Change-Id: I2cc9aeabab1be1957676aba9e11f13728de89f72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3044519
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904765}

--- chrome/browser/extensions/process_management_browsertest.cc
+++ chrome/browser/extensions/process_management_browsertest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/content_browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_host.h"
@@ -504,6 +505,10 @@ IN_PROC_BROWSER_TEST_P(ProcessManagementExtensionIsolationTest,
   }
 
   // Add a cross-site web process.
+  // Ensure bar.com has its own process by explicitly isolating it.
+  content::IsolateOriginsForTesting(
+      embedded_test_server(),
+      browser()->tab_strip_model()->GetActiveWebContents(), {"bar.com"});
   GURL cross_site_url(
       embedded_test_server()->GetURL("bar.com", "/title1.html"));
   ui_test_utils::NavigateToURLWithDisposition(
