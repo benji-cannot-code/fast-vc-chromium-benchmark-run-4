38e9c136d11d261fa823c2c7e4c4c5b0426a0b0b
Daniel Cheng <dcheng@chromium.org>
2021-07-23 23:04:44 +0000

    Remove no-longer-called WebRemoteFrame public methods.
    These methods are no longer called, due to the continuing import of
    //content/renderer into //third_party/blink
    
    Change-Id: I79bccc97af80d012501bc61081853a0b07cf63d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3049858
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904934}

--- third_party/blink/renderer/core/frame/web_frame_test.cc
+++ third_party/blink/renderer/core/frame/web_frame_test.cc
@@ -13976,7 +13976,8 @@ TEST_F(WebFrameTest, RemoteFrameCompositingScaleFactor) {
   // The compositing scale factor tells the OOPIF compositor to raster at a
   // lower scale since the frame is scaled down in the parent webview.
   EXPECT_EQ(remote_frame->GetCompositingRect(), gfx::Rect(0, 0, 1600, 1200));
-  EXPECT_EQ(remote_frame->GetCompositingScaleFactor(), 0.5f);
+  EXPECT_EQ(remote_frame->GetFrame()->View()->GetCompositingScaleFactor(),
+            0.5f);
 }
 
 TEST_F(WebFrameTest, RotatedRemoteFrameCompositingScaleFactor) {
@@ -14014,7 +14015,8 @@ TEST_F(WebFrameTest, RotatedRemoteFrameCompositingScaleFactor) {
   // The compositing scale factor tells the OOPIF compositor to raster at a
   // lower scale since the frame is scaled down in the parent webview.
   EXPECT_EQ(remote_frame->GetCompositingRect(), gfx::Rect(0, 0, 1600, 1200));
-  EXPECT_EQ(remote_frame->GetCompositingScaleFactor(), 0.5f);
+  EXPECT_EQ(remote_frame->GetFrame()->View()->GetCompositingScaleFactor(),
+            0.5f);
 }
 
 TEST_F(WebFrameTest, ZeroScaleRemoteFrameCompositingScaleFactor) {
@@ -14051,7 +14053,8 @@ TEST_F(WebFrameTest, ZeroScaleRemoteFrameCompositingScaleFactor) {
 
   // The compositing scale factor tells the OOPIF compositor to raster at a
   // reasonable minimum scale even though the iframe's transform scale is zero.
-  EXPECT_EQ(remote_frame->GetCompositingScaleFactor(), 0.25f);
+  EXPECT_EQ(remote_frame->GetFrame()->View()->GetCompositingScaleFactor(),
+            0.25f);
 }
 
 TEST_F(WebFrameTest, LargeScaleRemoteFrameCompositingScaleFactor) {
@@ -14087,7 +14090,8 @@ TEST_F(WebFrameTest, LargeScaleRemoteFrameCompositingScaleFactor) {
   RunPendingTasks();
 
   // The compositing scale factor is at most 5.0 irrespective of iframe scale.
-  EXPECT_EQ(remote_frame->GetCompositingScaleFactor(), 5.0f);
+  EXPECT_EQ(remote_frame->GetFrame()->View()->GetCompositingScaleFactor(),
+            5.0f);
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/frame/web_remote_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_remote_frame_impl.cc
@@ -381,10 +381,6 @@ void WebRemoteFrameImpl::DidStartLoading() {
   GetFrame()->DidStartLoading();
 }
 
-bool WebRemoteFrameImpl::IsIgnoredForHitTest() const {
-  return GetFrame()->IsIgnoredForHitTest();
-}
-
 void WebRemoteFrameImpl::UpdateUserActivationState(
     mojom::blink::UserActivationUpdateType update_type,
     mojom::blink::UserActivationNotificationType notification_type) {
@@ -404,18 +400,6 @@ v8::Local<v8::Object> WebRemoteFrameImpl::GlobalProxy() const {
 
 gfx::Rect WebRemoteFrameImpl::GetCompositingRect() {
   return GetFrame()->View()->GetCompositingRect();
-}
-
-void WebRemoteFrameImpl::SynchronizeVisualProperties() {
-  GetFrame()->SynchronizeVisualProperties();
-}
-
-void WebRemoteFrameImpl::ResendVisualProperties() {
-  GetFrame()->ResendVisualProperties();
-}
-
-float WebRemoteFrameImpl::GetCompositingScaleFactor() {
-  return GetFrame()->View()->GetCompositingScaleFactor();
 }
 
 WebString WebRemoteFrameImpl::UniqueName() const {
