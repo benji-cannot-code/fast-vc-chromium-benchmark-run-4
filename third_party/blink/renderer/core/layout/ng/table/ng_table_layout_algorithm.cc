a5a05b3aa6c16dbe42d2330e88a360ac23e06492
Manuel Rego Casasnovas <rego@igalia.com>
2021-07-23 11:22:15 +0000

    [mathml] Add basic support tabular elements
    This adds some basic CSS rules for <mtable>, <mtr> and <mtd>.
    
    Apart from that it also includes a small change
    in NGTableLayoutAlgorithm::GenerateFragment() in order to
    vertically align the <mtable> with the math axis:
    https://w3c.github.io/mathml-core/#table-or-matrix-mtable
    
    This change adds a new test table-003, that doesn't pass right now.
    The problem is that the <mtd> is not horizontally centering its contents.
    
    Bug: 6606, 1125111
    Test: external/wpt/mathml/presentation-markup/tables/table-001.html
    Test: external/wpt/mathml/presentation-markup/tables/table-002.html
    Test: external/wpt/mathml/presentation-markup/tables/table-003.html
    Change-Id: I536f1e8344baf1ed4de431f2da00aa55dbe3aa6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2390636
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Frédéric Wang <fwang@igalia.com>
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#904702}

--- third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.h"
 
 #include "third_party/blink/renderer/core/layout/geometry/writing_mode_converter.h"
+#include "third_party/blink/renderer/core/layout/ng/mathml/ng_math_layout_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.h"
@@ -28,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/table/ng_table_node.h"
 #include "third_party/blink/renderer/core/layout/text_autosizer.h"
+#include "third_party/blink/renderer/core/mathml/mathml_element.h"
+#include "third_party/blink/renderer/core/mathml_names.h"
 
 namespace blink {
 
@@ -896,6 +899,9 @@ scoped_refptr<const NGLayoutResult> NGTableLayoutAlgorithm::GenerateFragment(
                                    grid_converter.ToPhysical(table_grid_rect),
                                    border_spacing, column_block_size);
 
+  if (Node().GetDOMNode() &&
+      Node().GetDOMNode()->HasTagName(mathml_names::kMtableTag))
+    table_baseline = MathTableBaseline(Style(), block_offset);
   if (table_baseline)
     container_builder_.SetBaseline(*table_baseline);
 
