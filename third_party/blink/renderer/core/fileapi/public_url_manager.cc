9c3724f61f95401d7c83af229d1990a6ed42d7b4
Ari Chivukula <arichiv@chromium.org>
2021-07-23 02:44:41 +0000

    Return unsafe_agent_cluster_id from BlobURLStore.Resolve*
    In order to track potential breakage if/when the blob store is
    partitioned we need to track the agent_cluster_id in order detect and
    count usage across contexts. This CL focuses on returning the agent
    and the next will use that information for a use counter.
    
    Bug: 1224926
    Change-Id: I247d362b2ccef46cff64ad035a6aa8545612ccb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043457
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904602}

--- third_party/blink/renderer/core/fileapi/public_url_manager.cc
+++ third_party/blink/renderer/core/fileapi/public_url_manager.cc
@@ -125,7 +125,12 @@ void PublicURLManager::Resolve(
     return;
 
   DCHECK(url.ProtocolIs("blob"));
-  url_store_->ResolveAsURLLoaderFactory(url, std::move(factory_receiver));
+  url_store_->ResolveAsURLLoaderFactory(
+      url, std::move(factory_receiver),
+      WTF::Bind([](const absl::optional<base::UnguessableToken>&
+                       unsafe_agent_cluster_id) {
+        // TODO(https://crbug.com/1224926): Add the use counter.
+      }));
 }
 
 void PublicURLManager::Resolve(
@@ -135,7 +140,12 @@ void PublicURLManager::Resolve(
     return;
 
   DCHECK(url.ProtocolIs("blob"));
-  url_store_->ResolveForNavigation(url, std::move(token_receiver));
+  url_store_->ResolveForNavigation(
+      url, std::move(token_receiver),
+      WTF::Bind([](const absl::optional<base::UnguessableToken>&
+                       unsafe_agent_cluster_id) {
+        // TODO(https://crbug.com/1224926): Add the use counter.
+      }));
 }
 
 void PublicURLManager::ContextDestroyed() {
