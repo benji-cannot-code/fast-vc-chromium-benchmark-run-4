0a07ccfd01aed549f6f99b307496dac1175c43fe
Richard Knoll <knollr@chromium.org>
2021-07-23 14:52:28 +0000

    Enable Android PWA bottom sheet install by default
    This enables the new bottom sheet install UI by default for websites
    that fulfil all criteria (e.g. have screenshots defined in their
    manifest).
    
    Bug: 1169951
    Change-Id: I22dedb47c6d095d7eb209ccd05da29f7ae8accf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3048472
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904756}

--- chrome/browser/webapps/android/features.cc
+++ chrome/browser/webapps/android/features.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace webapps {
 namespace features {
 
-const base::Feature kPwaInstallUseBottomSheet{
-    "PwaInstallUseBottomSheet", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kPwaInstallUseBottomSheet{"PwaInstallUseBottomSheet",
+                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace webapps
