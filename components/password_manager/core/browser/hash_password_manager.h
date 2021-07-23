a12bdb78359420d93c1a298a70cf8ddba31b4567
Jose Dapena Paz <jdapena@igalia.com>
2021-07-23 10:17:49 +0000

    IWYU: missing include for using std::vector in hash password manager.
    Fix build breakage:
    ../../components/password_manager/core/browser/hash_password_manager.h:44:8: error: ‘vector’ in namespace ‘std’ does not name a template type
       44 |   std::vector<PasswordHashData> RetrieveAllPasswordHashes();
          |        ^~~~~~
    
    Bug: 819294
    Change-Id: I8c8a4ec3972eedb87a312c5ec56adf4a21b1b2a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041046
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904696}

--- components/password_manager/core/browser/hash_password_manager.h
+++ components/password_manager/core/browser/hash_password_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_HASH_PASSWORD_MANAGER_H_
 
 #include <string>
+#include <vector>
 
 #include "base/callback.h"
 #include "base/callback_list.h"
