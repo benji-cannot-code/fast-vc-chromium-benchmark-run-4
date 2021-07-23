d2a77a95ff654e838f6ca2dbaf3b5c90e83c0650
Fabio Tirelo <ftirelo@chromium.org>
2021-07-23 14:50:28 +0000

    [Clank CBCM] Modularize build rules for //c/b/policy tests - 1/N
    The idea is to create test targets inside //chrome/browser/policy to
    make it easier to determine which tests are enabled on each platform.
    
    The current list of tests in //chrome/test/BUILD.gn is confusing, which
    makes the task of enabling tests on Android much harder. This will
    become ever harder once I enable all applicable tests from
    //chrome/browser/policy/test (go/cbe-mobile-browsertests).
    
    More tests will be moved in follow-up CLs as they are enabled on Android
    or once it's clear they are not applicable to the platform.
    
    Bug: 1231661
    
    Change-Id: I73008e209f3857227ec319327402e430e56cb9ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041367
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904755}

--- chrome/browser/policy/cloud/chrome_browser_cloud_management_browsertest.cc
+++ chrome/browser/policy/cloud/chrome_browser_cloud_management_browsertest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/policy/cloud/chrome_browser_cloud_management_browsertest_delegate.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_result_codes.h"
 #include "chrome/common/chrome_switches.h"
@@ -65,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(OS_ANDROID)
 #include "chrome/browser/device_identity/device_oauth2_token_service.h"
 #include "chrome/browser/device_identity/device_oauth2_token_service_factory.h"
+#include "chrome/browser/ui/browser_finder.h"
 #endif  // !defined(OS_ANDROID)
 
 #if defined(OS_MAC)
