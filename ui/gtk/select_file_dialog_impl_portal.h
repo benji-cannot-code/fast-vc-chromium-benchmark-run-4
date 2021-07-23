eb7ecf3b2c3457db707858255fb0f7916cdd8546
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-07-23 16:53:06 +0000

    Fix filter index in SelectFileDialogImplPortal
    Bug: 885292
    Change-Id: I13bd73dcc587795faa6c82de80d55a142ae6ae6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3046105
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#904804}

--- ui/gtk/select_file_dialog_impl_portal.h
+++ ui/gtk/select_file_dialog_impl_portal.h
@@ -158,13 +158,15 @@ class SelectFileDialogImplPortal : public SelectFileDialogImpl {
   // Completes an open call, notifying the listener with the given paths, and
   // marks the dialog as closed.
   void CompleteOpen(scoped_refptr<DialogInfo> info,
-                    std::vector<base::FilePath> paths);
+                    std::vector<base::FilePath> paths,
+                    std::string current_filter);
   // Completes an open call, notifying the listener with a cancellation, and
   // marks the dialog as closed.
   void CancelOpen(scoped_refptr<DialogInfo> info);
 
   void CompleteOpenOnMainThread(scoped_refptr<DialogInfo> info,
-                                std::vector<base::FilePath> paths);
+                                std::vector<base::FilePath> paths,
+                                std::string current_filter);
   void CancelOpenOnMainThread(scoped_refptr<DialogInfo> info);
 
   // Removes the DialogInfo parent. Must be called on the UI task runner.
@@ -185,7 +187,8 @@ class SelectFileDialogImplPortal : public SelectFileDialogImpl {
 
   bool CheckResponseCode(dbus::MessageReader* reader);
   bool ReadResponseResults(dbus::MessageReader* reader,
-                           std::vector<std::string>* uris);
+                           std::vector<std::string>* uris,
+                           std::string* current_filter);
   std::vector<base::FilePath> ConvertUrisToPaths(
       const std::vector<std::string>& uris);
 
@@ -196,6 +199,8 @@ class SelectFileDialogImplPortal : public SelectFileDialogImpl {
 
   // Used by the D-Bus thread to generate unique handle tokens.
   static int handle_token_counter_;
+
+  std::vector<PortalFilter> filters_;
 };
 
 }  // namespace gtk
