ee206ab4aa85c4633d69311ea9857a5d6d53a20a
Trent Begin <tbegin@google.com>
2021-07-24 18:48:29 +0000

    connectivity-diagnostics: use RoutineResult network-diagnostics API
    This change converts the connectivity-diagnostics API calls to use the
    new Run* calls that return a common RoutineResult.
    
    Bug: chromium:1228547
    Change-Id: If13a955e74b836aad17f52b85fd9ec00b8dd2dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3025005
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905060}

--- ui/webui/resources/cr_components/chromeos/network_health/routine_group.js
+++ ui/webui/resources/cr_components/chromeos/network_health/routine_group.js
@@ -114,7 +114,7 @@ Polymer({
 
   /**
    * Helper function to get the icon for a routine based on the result.
-   * @param {!RoutineResponse} result
+   * @param {!chromeos.networkDiagnostics.mojom.RoutineResult} result
    * @return {string}
    * @private
    */
