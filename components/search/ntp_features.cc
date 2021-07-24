ed142701e6887e305d2a3b59ae966b476c824c56
Mei Liang <meiliang@chromium.org>
2021-07-24 02:50:39 +0000

    [RBD] Add UTM tag
    Change-Id: I57e775344a9f6f5db4103ee6ad436b86ac6bb002
    Bug: 1226160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2993552
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905003}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -120,6 +120,8 @@ const char kNtpRecipeTasksModuleCacheMaxAgeSParam[] =
 const char kNtpChromeCartModuleDataParam[] = "NtpChromeCartModuleDataParam";
 const char kNtpChromeCartModuleAbandonedCartDiscountParam[] =
     "NtpChromeCartModuleAbandonedCartDiscountParam";
+const char NtpChromeCartModuleAbandonedCartDiscountUseUtmParam[] =
+    "NtpChromeCartModuleAbandonedCartDiscountUseUtmParam";
 const char kNtpChromeCartModuleHeuristicsImprovementParam[] =
     "NtpChromeCartModuleHeuristicsImprovementParam";
 const char kNtpDriveModuleDataParam[] = "NtpDriveModuleDataParam";
