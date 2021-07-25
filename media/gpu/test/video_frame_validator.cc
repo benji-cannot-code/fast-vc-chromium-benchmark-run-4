352c3d5e1460cdad193712935ff1f1b3168f518f
Maksim Ivanov <emaxx@chromium.org>
2021-07-25 03:59:43 +0000

    Fix no-op seq checks in //media/gpu/test/
    Fix the bug that SEQUENCE_CHECKER was used instead of
    DCHECK_CALLED_ON_VALID_SEQUENCE, leading to creating a useless sequence
    checker object at a local scope instead of actually DCHECK'ing anything.
    
    Bug: 1232384
    Change-Id: If4dde5cdbe1eee4022763b62f66299d157781112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3048162
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905075}

--- media/gpu/test/video_frame_validator.cc
+++ media/gpu/test/video_frame_validator.cc
@@ -330,7 +330,7 @@ RawVideoFrameValidator::~RawVideoFrameValidator() = default;
 std::unique_ptr<VideoFrameValidator::MismatchedFrameInfo>
 RawVideoFrameValidator::Validate(scoped_refptr<const VideoFrame> frame,
                                  size_t frame_index) {
-  SEQUENCE_CHECKER(validator_thread_sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(validator_thread_sequence_checker_);
   auto model_frame = get_model_frame_cb_.Run(frame_index);
   CHECK(model_frame);
   size_t diff_cnt =
@@ -384,7 +384,7 @@ PSNRVideoFrameValidator::~PSNRVideoFrameValidator() = default;
 std::unique_ptr<VideoFrameValidator::MismatchedFrameInfo>
 PSNRVideoFrameValidator::Validate(scoped_refptr<const VideoFrame> frame,
                                   size_t frame_index) {
-  SEQUENCE_CHECKER(validator_thread_sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(validator_thread_sequence_checker_);
   auto model_frame = get_model_frame_cb_.Run(frame_index);
   CHECK(model_frame);
   double psnr = ComputePSNR(*frame, *model_frame);
@@ -457,7 +457,7 @@ SSIMVideoFrameValidator::~SSIMVideoFrameValidator() = default;
 std::unique_ptr<VideoFrameValidator::MismatchedFrameInfo>
 SSIMVideoFrameValidator::Validate(scoped_refptr<const VideoFrame> frame,
                                   size_t frame_index) {
-  SEQUENCE_CHECKER(validator_thread_sequence_checker_);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(validator_thread_sequence_checker_);
   auto model_frame = get_model_frame_cb_.Run(frame_index);
 
   CHECK(model_frame);
