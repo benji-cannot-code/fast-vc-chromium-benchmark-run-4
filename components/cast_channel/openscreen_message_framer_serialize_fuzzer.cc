4d5bd3c010fd607a921bcbea17b317b35116b2b4
btolsch <btolsch@chromium.org>
2021-07-26 08:54:18 +0000

    Add openscreen message framer fuzzers
    This changes ports the existing //components/cast_channel message framer
    fuzzers to instead call the same message framing logic from
    //third_party/openscreen.  These are not landing in
    //third_party/openscreen so they will be picked up by ClusterFuzz.
    
    Bug: b/185815206
    Change-Id: Ib5d4d01b23cbda5632de4183d75357ddcabb48cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043677
    Commit-Queue: Brandon Tolsch <btolsch@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905158}

---
+++ components/cast_channel/openscreen_message_framer_serialize_fuzzer.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "testing/libfuzzer/proto/lpm_interface.h"
+#include "third_party/openscreen/src/cast/common/channel/message_framer.h"
+
+namespace cast_channel {
+namespace fuzz {
+
+DEFINE_PROTO_FUZZER(const cast::channel::CastMessage& input) {
+  openscreen::ErrorOr<std::vector<uint8_t>> result =
+      openscreen::cast::message_serialization::Serialize(input);
+}
+
+}  // namespace fuzz
+}  // namespace cast_channel
