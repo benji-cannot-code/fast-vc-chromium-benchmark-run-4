989a8472f9ad09aadcc80250f6d6fc39fe1b0e67
Pranav Batra <batrapranav@chromium.org>
2021-07-26 23:34:01 +0000

    Update local_printer.mojom
    Add functions needed to enable the chrome.printing API on lacros.
    
    CancelPrintJob() is used to cancel a print job.
    AddExtensionPrintJobObserver() adds an extension print job observer.
    
    Bug: b/189970798
    Change-Id: If2c1e13287c11d898afd0eac09ed47d4aaaa0a70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017067
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Pranav Batra <batrapranav@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905491}

--- chrome/test/chromeos/printing/fake_local_printer_chromeos.cc
+++ chrome/test/chromeos/printing/fake_local_printer_chromeos.cc
@@ -36,6 +36,12 @@ void FakeLocalPrinter::CreatePrintJob(crosapi::mojom::PrintJobPtr job,
   FAIL();
 }
 
+void FakeLocalPrinter::CancelPrintJob(const std::string& printer_id,
+                                      unsigned int job_id,
+                                      CancelPrintJobCallback callback) {
+  FAIL();
+}
+
 void FakeLocalPrinter::GetPrintServersConfig(
     GetPrintServersConfigCallback callback) {
   FAIL();
@@ -64,5 +70,12 @@ void FakeLocalPrinter::GetUsernamePerPolicy(
 
 void FakeLocalPrinter::GetPrinterTypeDenyList(
     GetPrinterTypeDenyListCallback callback) {
+  FAIL();
+}
+
+void FakeLocalPrinter::AddPrintJobObserver(
+    mojo::PendingRemote<crosapi::mojom::PrintJobObserver> remote,
+    crosapi::mojom::PrintJobSource source,
+    AddPrintJobObserverCallback callback) {
   FAIL();
 }
