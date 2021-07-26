9017f906e145272443b4ca2a2c641ae4398326f1
Kyle Horimoto <khorimoto@chromium.org>
2021-07-26 17:39:33 +0000

    [CrOS Bluetooth] Add client code for CrosBluetoothConfig service
    This CL creates an in-process instance of the service for use by clients
    in Ash. Additionally, we provide a GetBluetoothConfigService() function
    in //ash/public/cpp which is intended to be the main entrypoint to the
    API by both WebUI and Ash.
    
    I've manually verified that this code can be called correctly. A
    follow-up CL will set up bindings so that this API can be called from
    WebUI.
    
    Bug: 1010321
    Change-Id: Ic12a67bc9ff93651c16f5f64ba9be3f79fe0eaa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3048999
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905315}

---
+++ ash/public/cpp/bluetooth_config_service.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/bluetooth_config_service.h"
+
+#include "chromeos/services/bluetooth_config/in_process_instance.h"
+
+namespace ash {
+
+void GetBluetoothConfigService(
+    mojo::PendingReceiver<
+        chromeos::bluetooth_config::mojom::CrosBluetoothConfig> receiver) {
+  chromeos::bluetooth_config::BindToInProcessInstance(std::move(receiver));
+}
+
+}  // namespace ash
