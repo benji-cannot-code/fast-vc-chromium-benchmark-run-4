48e8d852f902cd5e1cc330460238777c345a4d24
Olesia Marukhno <olesiamarukhno@google.com>
2021-07-26 12:34:06 +0000

    Update SafetyTipPageInfoBubbleViewBrowserTest to support page info V2
    Bug: 1188101
    Change-Id: I08e7d34b57bc68df8eedfe0e42039314423abf01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053971
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/master@{#905215}

--- chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view_browsertest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/lookalikes/core/features.h"
 #include "components/lookalikes/core/lookalike_url_util.h"
+#include "components/page_info/features.h"
 #include "components/reputation/core/safety_tip_test_utils.h"
 #include "components/reputation/core/safety_tips.pb.h"
 #include "components/reputation/core/safety_tips_config.h"
@@ -365,6 +366,24 @@ class SafetyTipPageInfoBubbleViewBrowserTest
     return AreLookalikeWarningsEnabled() ? IsUIShowing() : !IsUIShowing();
   }
 
+  const std::u16string GetPageInfoBubbleViewSummaryText() {
+    auto* label =
+        PageInfoBubbleView::GetPageInfoBubbleForTesting()->GetViewByID(
+            PageInfoViewFactory::VIEW_ID_PAGE_INFO_SECURITY_SUMMARY_LABEL);
+    return static_cast<views::StyledLabel*>(label)->GetText();
+  }
+
+  std::u16string GetSafetyTipSummaryText() {
+    auto* page_info = PageInfoBubbleView::GetPageInfoBubbleForTesting();
+    if (base::FeatureList::IsEnabled(page_info::kPageInfoV2Desktop)) {
+      auto* summary_label = page_info->GetViewByID(
+          PageInfoViewFactory::VIEW_ID_PAGE_INFO_SECURITY_SUMMARY_LABEL);
+      return static_cast<views::StyledLabel*>(summary_label)->GetText();
+    }
+
+    return page_info->GetWindowTitle();
+  }
+
   void CheckPageInfoShowsSafetyTipInfo(
       Browser* browser,
       security_state::SafetyTipStatus expected_safety_tip_status,
@@ -383,14 +402,14 @@ class SafetyTipPageInfoBubbleViewBrowserTest
     switch (expected_safety_tip_status) {
       case security_state::SafetyTipStatus::kBadReputation:
       case security_state::SafetyTipStatus::kBadReputationIgnored:
-        EXPECT_EQ(page_info->GetWindowTitle(),
+        EXPECT_EQ(GetSafetyTipSummaryText(),
                   l10n_util::GetStringUTF16(
                       IDS_PAGE_INFO_SAFETY_TIP_BAD_REPUTATION_TITLE));
         break;
 
       case security_state::SafetyTipStatus::kLookalike:
       case security_state::SafetyTipStatus::kLookalikeIgnored:
-        EXPECT_EQ(page_info->GetWindowTitle(),
+        EXPECT_EQ(GetSafetyTipSummaryText(),
                   l10n_util::GetStringFUTF16(
                       IDS_PAGE_INFO_SAFETY_TIP_LOOKALIKE_TITLE,
                       security_interstitials::common_string_util::
@@ -419,9 +438,9 @@ class SafetyTipPageInfoBubbleViewBrowserTest
         PageInfoBubbleViewBase::GetPageInfoBubbleForTesting());
     ASSERT_TRUE(page_info);
     EXPECT_TRUE(
-        page_info->GetWindowTitle() ==
+        GetSafetyTipSummaryText() ==
             l10n_util::GetStringUTF16(IDS_PAGE_INFO_NOT_SECURE_SUMMARY) ||
-        page_info->GetWindowTitle() ==
+        GetSafetyTipSummaryText() ==
             l10n_util::GetStringUTF16(IDS_PAGE_INFO_INTERNAL_PAGE));
     if (PageInfoBubbleViewBase::GetShownBubbleType() ==
         PageInfoBubbleViewBase::BubbleType::BUBBLE_PAGE_INFO) {
@@ -692,9 +711,8 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
   TriggerWarningFromBlocklist(browser(), kNavigatedUrl,
                               WindowOpenDisposition::CURRENT_TAB);
   ASSERT_NO_FATAL_FAILURE(CheckNoButtons());
-  auto* page_info = PageInfoBubbleViewBase::GetPageInfoBubbleForTesting();
   EXPECT_EQ(
-      page_info->GetWindowTitle(),
+      GetSafetyTipSummaryText(),
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SAFETY_TIP_BAD_REPUTATION_TITLE));
 }
 
