ffd6bb71b13bef7c786e11579b63545130772e69
Peng Huang <penghuang@chromium.org>
2021-07-26 22:17:00 +0000

    Only recover from context lost for GL
    The context lost can be caused by variant reasons, for example:
    surface resize failed, OOM, semaphores creation failed, import or export
    vulkan resources from or to fd, etc.
    For those cases, we are trying to recreate GrContext without restarting
    GPU process. However, in many case recreating GrContext can not fix
    the problem, the same issue will happen again. So the context lost and
    recover loop happens, especial for non-GL backend. When the loop
    happens, it will never end until GPU process crashes. We got many weird
    crashes related this context lost and recover loop. So fix the problem
    by restarting GPU process for non-GL backends.
    
    Bug: 1228416,1156614,1204047
    Change-Id: Ifa5cb9a718835fde890dfb3ac238d89ea02aae48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042209
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905449}

--- gpu/ipc/service/gpu_channel_manager.cc
+++ gpu/ipc/service/gpu_channel_manager.cc
@@ -936,7 +936,7 @@ void GpuChannelManager::OnContextLost(bool synthetic_loss) {
 
   context_lost_time_ = lost_time;
 
-  if (synthetic_loss)
+  if (synthetic_loss && shared_context_state_->GrContextIsGL())
     return;
 
   // Lose all other contexts.
