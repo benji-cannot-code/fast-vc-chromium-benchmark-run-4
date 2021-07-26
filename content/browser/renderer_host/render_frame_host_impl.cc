f727d45b157b573bb992b1e43df5cdbca326aad9
Julie Jeongeun Kim <jkim@igalia.com>
2021-07-26 05:09:47 +0000

    [mparch] Introduce GetBluetoothAllowedNotCalledInPrerendering test
    This CL introduces a test that ensures that
    WebBluetoothServiceImpl::GetBluetoothAllowed() is not called in the
    prerendering since the method accesses to the main frame and checks
    the origin using it.
    
    Bug: 1225909
    Change-Id: Iad2f98bfe7f3c1805564d1df67ea3de63a6a9eee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015076
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#905131}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -8732,7 +8732,8 @@ void RenderFrameHostImpl::CreateWebBluetoothService(
 
 WebBluetoothServiceImpl*
 RenderFrameHostImpl::GetWebBluetoothServiceForTesting() {
-  DCHECK(web_bluetooth_services_.back());
+  if (web_bluetooth_services_.empty())
+    return nullptr;
   return web_bluetooth_services_.back().get();
 }
 
