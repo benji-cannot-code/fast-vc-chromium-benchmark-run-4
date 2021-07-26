0907d76cfe5e727897a903279dffff15e75df0fb
Nancy Wang <nancylingwang@chromium.org>
2021-07-26 03:37:01 +0000

    Save the launch info in LaunchPlatformAppWithFileHandler.
    The file app calls LaunchPlatformAppWithFileHandler to launch the
    Chrome app to open the file:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/file_manager/file_tasks.cc;l=583?q=LaunchPlatformAppWithFileHandler&sq=&ss=chromium%2Fchromium%2Fsrc
    
    Modify LaunchPlatformAppWithFileHandler to call the full restore
    interface to save the launch info.
    
    Modify the full restsore component to save the parameter of
    LaunchPlatformAppWithFileHandler.
    
    Modify LaunchSystemWebAppOrChromeApp to call
    LaunchPlatformAppWithFileHandler to restore/launch the Chrome app to
    to open the file if `handler_id` has a value.
    
    BUG=1230978
    
    Change-Id: Icb9900369f209bb912b98ab7e4671f71147b6337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3052330
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905122}

--- apps/launcher.cc
+++ apps/launcher.cc
@@ -51,6 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "components/full_restore/app_launch_info.h"
+#include "components/full_restore/full_restore_utils.h"
 #include "components/user_manager/user_manager.h"
 #endif
 
@@ -476,6 +478,12 @@ void LaunchPlatformAppWithFileHandler(
     const Extension* app,
     const std::string& handler_id,
     const std::vector<base::FilePath>& entry_paths) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  auto launch_info = std::make_unique<full_restore::AppLaunchInfo>(
+      app->id(), handler_id, entry_paths);
+  full_restore::SaveAppLaunchInfo(context->GetPath(), std::move(launch_info));
+#endif
+
   scoped_refptr<PlatformAppPathLauncher> launcher =
       new PlatformAppPathLauncher(context, app, entry_paths);
   launcher->LaunchWithHandler(handler_id);
--- chrome/browser/chromeos/full_restore/app_launch_handler.cc
+++ chrome/browser/chromeos/full_restore/app_launch_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "apps/launcher.h"
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -18,7 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/full_restore/full_restore_read_handler.h"
 #include "components/services/app_service/public/cpp/types_util.h"
+#include "extensions/browser/extension_registry.h"
 #include "extensions/common/constants.h"
+#include "extensions/common/extension.h"
 
 namespace chromeos {
 
@@ -179,6 +182,19 @@ void AppLaunchHandler::LaunchSystemWebAppOrChromeApp(
 
   for (const auto& it : launch_list) {
     RecordRestoredAppLaunch(GetHistogrameAppType(app_type));
+
+    if (it.second->handler_id.has_value()) {
+      const extensions::Extension* extension =
+          extensions::ExtensionRegistry::Get(profile_)->GetInstalledExtension(
+              app_id);
+      if (extension) {
+        DCHECK(it.second->file_paths.has_value());
+        apps::LaunchPlatformAppWithFileHandler(profile_, extension,
+                                               it.second->handler_id.value(),
+                                               it.second->file_paths.value());
+      }
+      continue;
+    }
 
     DCHECK(it.second->container.has_value());
     DCHECK(it.second->disposition.has_value());
