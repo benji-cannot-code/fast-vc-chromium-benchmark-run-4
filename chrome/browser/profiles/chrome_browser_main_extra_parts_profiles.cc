2c14b77d20e6f660d8ff9cc1bb904d8491b4b0f5
Christian Dullweber <dullweber@chromium.org>
2021-07-26 17:07:40 +0000

    AccuracyTips: Implement rate-limit and opt-out logic
    Add preferences to store previous interactions with accuracy tips in
    order to perform rate limiting and persist opt-out decisions.
    To make it easier to separate data from the dark launch, there is a
    separate set of preferences for it.
    The rate limit is configurable through finch.
    
    Bug: 1210891
    Change-Id: Idc5084d235616d318d1ed29b8a941762326b1620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3045410
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905296}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/accuracy_tips/accuracy_service_factory.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier_factory.h"
 #include "chrome/browser/autocomplete/in_memory_url_index_factory.h"
 #include "chrome/browser/autocomplete/shortcuts_backend_factory.h"
@@ -252,6 +253,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   AccountConsistencyModeManagerFactory::GetInstance();
   AccountInvestigatorFactory::GetInstance();
   AccountReconcilorFactory::GetInstance();
+#if !defined(OS_ANDROID)
+  AccuracyServiceFactory::GetInstance();
+#endif
   AdaptiveQuietNotificationPermissionUiEnabler::Factory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   app_list::AppListSyncableServiceFactory::GetInstance();
---
+++ components/accuracy_tips/pref_names.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ACCURACY_TIPS_PREF_NAMES_H_
+#define COMPONENTS_ACCURACY_TIPS_PREF_NAMES_H_
+
+namespace accuracy_tips {
+namespace prefs {
+
+// TODO(crbug.com/1233024): Delete data for "DisabledUi" preferences after
+// feature is launched.
+extern const char kLastAccuracyTipShown[];
+extern const char kLastAccuracyTipShownDisabledUi[];
+extern const char kPreviousInteractions[];
+extern const char kPreviousInteractionsDisabledUi[];
+
+}  // namespace prefs
+}  // namespace accuracy_tips
+
+#endif  // COMPONENTS_ACCURACY_TIPS_PREF_NAMES_H_
