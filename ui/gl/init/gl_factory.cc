e8cd249bdd01d08d85840bfa0f3da1e8a2e2dbe4
Alexis Hetu <sugoi@google.com>
2021-07-26 17:47:27 +0000

    Enable SwANGLE on Wayland
    This CL changes the following things:
    1) It enables Wayland to use SwANGLE (ANGLE+SwiftShader Vulkan, as
       opposed to using legacy SwiftShader GL) and removes all Wayland
       specific restrictions on the use of SwANGLE.
    2) It adds a new class: WaylandGLEGLUtility, derived from
       PlatformGLEGLUtility, which is used to properly set the EGL
       attributes to Headless mode when using SwANGLE, which otherwise
       works as is. In this mode, rendered pixels are read back and stored
       into a shared memory backed by wl_shm that Wayland draws later.
    3) It fixes 2 views_unittests which were failing with this change
       by requiring that a Visual Manager be present in order to check
       for transparency. These tests were enabled by adding a new
       class based on PlatformGLEGLUtility, which is checked by the
       "CanHaveCompositingManager()" function, and which was only true
       for GLEGLUtilityX11 prior to this CL, so these tests remain X11
       specific, as they were before this CL.
    
    Bug: chromium:1205025
    Change-Id: Id74321068519ca2b907641f8d3a212bd12ee6b7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009475
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905322}

--- ui/gl/init/gl_factory.cc
+++ ui/gl/init/gl_factory.cc
@@ -173,13 +173,7 @@ bool InitializeGLOneOffPlatformHelper(bool init_extensions) {
 
 GLImplementationParts GetSoftwareGLForTestsImplementation() {
 #if defined(OS_WIN) || defined(OS_LINUX)
-#if defined(USE_OZONE)
-  if (!features::IsUsingOzonePlatform() ||
-      (ui::OzonePlatform::GetPlatformNameForTest() != "wayland"))
-#endif
-  {
-    return GetSoftwareGLImplementation();
-  }
+  return GetSoftwareGLImplementation();
 #endif
   return GetLegacySoftwareGLImplementation();
 }
--- ui/views/widget/widget_unittest.cc
+++ ui/views/widget/widget_unittest.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(USE_OZONE)
 #include "ui/base/ui_base_features.h"
 #include "ui/ozone/public/ozone_platform.h"
+#include "ui/ozone/public/platform_gl_egl_utility.h"
 #endif
 
 namespace views {
@@ -4138,9 +4139,9 @@ namespace {
 bool CanHaveCompositingManager() {
 #if defined(USE_OZONE)
   if (features::IsUsingOzonePlatform()) {
-    const auto* const egl_utility =
+    auto* const egl_utility =
         ui::OzonePlatform::GetInstance()->GetPlatformGLEGLUtility();
-    return egl_utility != nullptr;
+    return (egl_utility != nullptr) && egl_utility->HasVisualManager();
   }
 #endif
 #if defined(USE_X11)
