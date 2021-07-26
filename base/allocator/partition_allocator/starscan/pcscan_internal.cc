06d8c647f56077fc6e032f9df12ed4ea913d8505
Yuki Shiino <yukishiino@chromium.org>
2021-07-26 10:02:00 +0000

    partition alloc: Add DEPS to control dependencies
    We're planning to make PartitionAlloc a stand-alone library
    separated from everything including //base.  In order to better
    control the dependencies of PartitionAlloc, adds DEPS file.
    
    Bug: 1151236
    Change-Id: I0b231f6cb5fb970bacb57d23a6a77f57f97bfcb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041142
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905172}

--- base/allocator/partition_allocator/starscan/pcscan_internal.cc
+++ base/allocator/partition_allocator/starscan/pcscan_internal.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if defined(ARCH_CPU_X86_64)
 // Include order is important, so we disable formatting.
