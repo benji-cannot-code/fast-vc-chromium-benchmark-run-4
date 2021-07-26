20e04d7ec2a38dff33bce690099e10c44d305568
Wez <wez@chromium.org>
2021-07-26 20:02:47 +0000

    [fuchsia] Stub parts of the downloads implementation.
    - Opt-in OS_FUCHSIA to checks for PDF downloads.
    - Stub out download progress update API.
    
    Bug: 1226242
    Change-Id: I1f50ac6ff7a2692cbec49310e8dbd83d22f5a0bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3045275
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905382}

--- chrome/browser/download/download_commands.cc
+++ chrome/browser/download/download_commands.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 
 #if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
-    defined(OS_MAC)
+    defined(OS_MAC) || defined(OS_FUCHSIA)
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
@@ -167,7 +167,7 @@ void DownloadCommands::ExecuteCommand(Command command) {
 }
 
 #if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
-    defined(OS_CHROMEOS)
+    defined(OS_CHROMEOS) || defined(OS_FUCHSIA)
 
 Browser* DownloadCommands::GetBrowser() const {
   if (!model_)
@@ -196,13 +196,13 @@ bool DownloadCommands::CanOpenPdfInSystemViewer() const {
   return IsDownloadPdf() &&
          (IsAdobeReaderDefaultPDFViewer() ? is_adobe_pdf_reader_up_to_date
                                           : true);
-#elif defined(OS_MAC) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+#else  // defined(OS_WIN)
   return IsDownloadPdf();
 #endif
 }
 
 #endif  // defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) ||
-        // defined(OS_CHROMEOS)
+        // defined(OS_CHROMEOS) || defined(OS_FUCHSIA)
 
 void DownloadCommands::CopyFileAsImageToClipboard() {
   if (!model_)
---
+++ chrome/browser/download/download_status_updater_fuchsia.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/download/download_status_updater.h"
+
+#include "base/notreached.h"
+
+void DownloadStatusUpdater::UpdateAppIconDownloadProgress(
+    download::DownloadItem* download) {
+  // TODO(crbug.com/1226242): Integrate with shell app-icon updates, if/when
+  // the platform supports them.
+  NOTIMPLEMENTED_LOG_ONCE();
+}
