04fce3c3a40b9ca8ece9e9f91f97392389f576ea
Maksim Ivanov <emaxx@chromium.org>
2021-07-26 20:09:33 +0000

    Fix unused seq checker in UsageScenarioTracker
    Bug: 1232384
    Change-Id: Icddbae35e7e32a370eef8cc57228ca88ba214e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3049072
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905384}

--- chrome/browser/metrics/usage_scenario/usage_scenario_tracker.cc
+++ chrome/browser/metrics/usage_scenario/usage_scenario_tracker.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/metrics/usage_scenario/usage_scenario_tracker.h"
 
+#include "base/sequence_checker.h"
 #include "chrome/browser/metrics/tab_stats/tab_stats_tracker.h"
 
 UsageScenarioTracker::UsageScenarioTracker()
@@ -15,4 +16,6 @@ UsageScenarioTracker::UsageScenarioTracker()
       &tab_usage_scenario_tracker_);
 }
 
-UsageScenarioTracker::~UsageScenarioTracker() = default;
+UsageScenarioTracker::~UsageScenarioTracker() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
