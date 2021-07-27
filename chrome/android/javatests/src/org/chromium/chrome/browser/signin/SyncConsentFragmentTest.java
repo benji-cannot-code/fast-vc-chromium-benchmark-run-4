dbf76b16b62817c56c910d2f34abf56a4514eb63
Alice Wang <aliceywang@chromium.org>
2021-07-27 14:02:01 +0000

    [Android][Test] Enable SyncConsentFragmentTest render test
    This CL re-enables the SyncConsentFragmentTest render test by
    confirming the new golden.
    
    Bug: 1231491
    Change-Id: I4272eb15549d7aae0354427b7a87b42166913377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3047660
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905721}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
@@ -45,7 +45,6 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Matchers;
 import org.chromium.base.test.util.MetricsUtils.HistogramDelta;
@@ -261,9 +260,6 @@ public class SyncConsentFragmentTest {
     @Test
     @LargeTest
     @Feature("RenderTest")
-    // Flaky on Android Marshmallow, see
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=1231491
-    @DisabledTest
     public void testSigninFragmentForcedSigninWithRegularChild() throws IOException {
         HistogramDelta countHistogram =
                 new HistogramDelta("Signin.AndroidDeviceAccountsNumberWhenEnteringFRE", 1);
