53732d64a2479e2be761a840bd2ef6c4275751f4
Chad Duffin <chadduffin@chromium.org>
2021-07-27 17:42:59 +0000

    Remove leftover commented code from unified_system_tray_view.h.
    Change-Id: I75c59f5da756441dc828a0e4affa6bbfe865c48d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055689
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905798}

--- ash/system/unified/unified_system_tray_view.h
+++ ash/system/unified/unified_system_tray_view.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/view.h"
 
-// namespace views {
-// class FocusSearch;
-// }
-
 namespace ash {
 
 class FeaturePodButton;
