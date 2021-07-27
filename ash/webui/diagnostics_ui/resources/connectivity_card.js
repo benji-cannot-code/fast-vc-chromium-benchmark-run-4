fcead07d38216e3f8154fabb69fb10fcd772a093
Michael Checo <michaelcheco@google.com>
2021-07-27 20:08:05 +0000

    Diagnostics: Use localized strings in network utils
    Screenshot: http://shortn/_KPLv5s9IfD
    
    Bug: 1197335
    Change-Id: Ie3ce750fb919d268e54dd57a35ac411809410452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055799
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/master@{#905868}

--- ash/webui/diagnostics_ui/resources/connectivity_card.js
+++ ash/webui/diagnostics_ui/resources/connectivity_card.js
@@ -139,7 +139,6 @@ Polymer({
 
   /** @protected */
   getNetworkCardTitle_() {
-    // TODO(michaelcheco): Map network state to an icon or localize.
     var title = this.networkType_;
     if (this.networkState_) {
       title = title + ' (' + this.networkState_ + ')';
