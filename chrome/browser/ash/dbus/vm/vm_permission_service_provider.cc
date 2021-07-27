364840e99489f4a938b86bd00dcd66a8d8055796
Lucy Qu <lqu@google.com>
2021-07-27 08:23:24 +0000

    Add microphone permission to borealis
    BUG=b:193079085
    
    Change-Id: I9e20d3577ebb03ddbef122cef80536be0dcaa245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017236
    Commit-Queue: Lucy Qu <lqu@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Daniel Ng <danielng@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Cr-Commit-Position: refs/heads/master@{#905639}

--- chrome/browser/ash/dbus/vm/vm_permission_service_provider.cc
+++ chrome/browser/ash/dbus/vm/vm_permission_service_provider.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
+#include "chrome/browser/ash/borealis/borealis_prefs.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_pref_names.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browser_process.h"
@@ -143,6 +144,9 @@ void VmPermissionServiceProvider::RegisterVm(
   VmInfo::VmType vm_type;
   if (request.type() == vm_permission_service::RegisterVmRequest::PLUGIN_VM) {
     vm_type = VmInfo::VmType::PluginVm;
+  } else if (request.type() ==
+             vm_permission_service::RegisterVmRequest::BOREALIS) {
+    vm_type = VmInfo::VmType::Borealis;
   } else {
     LOG(ERROR) << "Unsupported VM " << request.owner_id() << "/"
                << request.name() << " type: " << request.type();
@@ -325,6 +329,9 @@ void VmPermissionServiceProvider::UpdateVmPermissions(VmInfo* vm) {
     case VmInfo::PluginVm:
       UpdatePluginVmPermissions(vm);
       break;
+    case VmInfo::Borealis:
+      UpdateBorealisPermissions(vm);
+      break;
     case VmInfo::CrostiniVm:
       NOTREACHED();
   }
@@ -346,6 +353,20 @@ void VmPermissionServiceProvider::UpdatePluginVmPermissions(VmInfo* vm) {
   if (prefs->GetBoolean(prefs::kAudioCaptureAllowed)) {
     vm->permission_to_enabled_map[VmInfo::PermissionMicrophone] =
         prefs->GetBoolean(plugin_vm::prefs::kPluginVmMicAllowed);
+  }
+}
+
+void VmPermissionServiceProvider::UpdateBorealisPermissions(VmInfo* vm) {
+  Profile* profile = ProfileManager::GetPrimaryUserProfile();
+  if (!profile || chromeos::ProfileHelper::GetUserIdHashFromProfile(profile) !=
+                      vm->owner_id) {
+    return;
+  }
+
+  const PrefService* prefs = profile->GetPrefs();
+  if (prefs->GetBoolean(prefs::kAudioCaptureAllowed)) {
+    vm->permission_to_enabled_map[VmInfo::PermissionMicrophone] =
+        prefs->GetBoolean(borealis::prefs::kBorealisMicAllowed);
   }
 }
 
