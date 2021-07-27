55e3caff53e8b3253c0917bc85144bcd4fe35575
Robbie Gibson <rkgibson@google.com>
2021-07-27 14:39:02 +0000

    [iOS] CPE: Remove search bar when there are no saved passwords
    Now, if password creation is enabled, the "No Saved Passwords" modal
    should not show (because the user could create a new password). And the
    search bar shouldn't show if there are no passwords to search.
    
    Bug: 1225360
    Change-Id: I9c405559778528b01737177a2a5862715f1c1d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055999
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#905730}

--- ios/chrome/credential_provider_extension/ui/credential_list_mediator.mm
+++ ios/chrome/credential_provider_extension/ui/credential_list_mediator.mm
@@ -89,7 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.suggestedCredentials = suggestions;
 
     dispatch_async(dispatch_get_main_queue(), ^{
-      if (!self.allCredentials.count) {
+      if (!IsPasswordCreationEnabled() && !self.allCredentials.count) {
         [self.UIHandler showEmptyCredentials];
         return;
       }
--- ios/chrome/credential_provider_extension/ui/credential_list_view_controller.mm
+++ ios/chrome/credential_provider_extension/ui/credential_list_view_controller.mm
@@ -132,6 +132,17 @@ UIColor* BackgroundColor() {
   self.showNewPasswordOption = showNewPasswordOption;
   [self.tableView reloadData];
   [self.tableView layoutIfNeeded];
+
+  // Remove or add the search controller depending on whether there are
+  // passwords to search.
+  if (self.allPasswords.count > 0) {
+    self.navigationItem.searchController = self.searchController;
+  } else {
+    if (self.navigationItem.searchController.isActive) {
+      self.navigationItem.searchController.active = NO;
+    }
+    self.navigationItem.searchController = nil;
+  }
 }
 
 - (void)setTopPrompt:(NSString*)prompt {
@@ -341,7 +352,7 @@ UIColor* BackgroundColor() {
 - (BOOL)isSuggestedPasswordSection:(int)section {
   int sections = [self numberOfSections];
   if ((sections == 2 && section == 0) ||
-      (sections == 1 && self.suggestedPasswords.count)) {
+      (sections == 1 && [self numberOfRowsInSuggestedPasswordSection])) {
     return YES;
   } else {
     return NO;
