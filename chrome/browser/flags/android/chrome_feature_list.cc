cf330eed4f2d67f4d31ff6728ec71e90e4a00078
Basia Zimirska <basiaz@google.com>
2021-07-27 20:39:08 +0000

    Consent logic changes.
    Two new experimental treatments:
    1) tapping outside of the sheet or tapping back button are
    not treated as consent rejections.
    2) like above but there's a counter: first X taps are ignored, x+1th
    is treated as rejection (X = 3 for now)
    
    This cl also includes changes to feature and about flags (one
    parametrized flag instead of two separate ones)
    
    Bug: 1197255
    Change-Id: Ia6122a707a75f5b3760e4365270818f7495aabc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3022016
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905878}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -148,11 +148,10 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidLayoutChangeTabReparenting,
     &kAndroidPartnerCustomizationPhenotype,
     &kAndroidSearchEngineChoiceNotification,
-    &kAssistantExplicitVoiceConsent,
+    &kAssistantConsentV2,
     &kAssistantIntentExperimentId,
     &kAssistantIntentPageUrl,
     &kAssistantIntentTranslateInfo,
-    &kAssistantVoiceConsentTapsCounter,
     &kAppLaunchpad,
     &kAppMenuMobileSiteOption,
     &kAppToWebAttribution,
@@ -383,8 +382,8 @@ const base::Feature kAndroidPartnerCustomizationPhenotype{
 const base::Feature kAndroidSearchEngineChoiceNotification{
     "AndroidSearchEngineChoiceNotification", base::FEATURE_ENABLED_BY_DEFAULT};
 
-const base::Feature kAssistantExplicitVoiceConsent{
-    "AssistantExplicitVoiceConsent", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kAssistantConsentV2{"AssistantConsentV2",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kAssistantIntentExperimentId{
     "AssistantIntentExperimentId", base::FEATURE_DISABLED_BY_DEFAULT};
@@ -394,9 +393,6 @@ const base::Feature kAssistantIntentPageUrl{"AssistantIntentPageUrl",
 
 const base::Feature kAssistantIntentTranslateInfo{
     "AssistantIntentTranslateInfo", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kAssistantVoiceConsentTapsCounter{
-    "AssistantVoiceConsentTapsCounter", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kAppLaunchpad{"AppLaunchpad",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
