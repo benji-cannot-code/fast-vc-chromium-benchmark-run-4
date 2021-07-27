0bbebea238633ed50de6612b06f1c5d1eefd638d
Michael Ershov <miersh@google.com>
2021-07-27 18:07:53 +0000

    Lacros: Deprecate Add/RemoveCertificate mojo methods
    Deprecate Add/RemoveCertificate mojo methods by renaming them into
    DEPRECATED_*. Replacement methods will be introduced in following
    CLs. The goal is to update all Keystore APIs to use
    KeystoreError-s and vector<uint8>-s instead of strings.
    
    Bug: 1128022
    Test: *KeystoreService* unittests
    Change-Id: I004a556a7970660de030851c97cabe3b58d5674a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043914
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905818}

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api.cc
@@ -261,7 +261,7 @@ EnterprisePlatformKeysImportCertificateFunction::Run() {
   EXTENSION_FUNCTION_VALIDATE(params);
 
   std::string error = ValidateCrosapi(
-      KeystoreService::kAddCertificateMinVersion, browser_context());
+      KeystoreService::kDEPRECATED_AddCertificateMinVersion, browser_context());
   if (!error.empty()) {
     return RespondNow(Error(error));
   }
@@ -273,7 +273,7 @@ EnterprisePlatformKeysImportCertificateFunction::Run() {
   auto c = base::BindOnce(
       &EnterprisePlatformKeysImportCertificateFunction::OnAddCertificate, this);
   GetKeystoreService(browser_context())
-      ->AddCertificate(keystore, params->certificate, std::move(c));
+      ->DEPRECATED_AddCertificate(keystore, params->certificate, std::move(c));
   return RespondLater();
 }
 
@@ -294,8 +294,9 @@ EnterprisePlatformKeysRemoveCertificateFunction::Run() {
       api_epk::RemoveCertificate::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params);
 
-  std::string error = ValidateCrosapi(
-      KeystoreService::kRemoveCertificateMinVersion, browser_context());
+  std::string error =
+      ValidateCrosapi(KeystoreService::kDEPRECATED_RemoveCertificateMinVersion,
+                      browser_context());
   if (!error.empty()) {
     return RespondNow(Error(error));
   }
@@ -308,7 +309,8 @@ EnterprisePlatformKeysRemoveCertificateFunction::Run() {
       &EnterprisePlatformKeysRemoveCertificateFunction::OnRemoveCertificate,
       this);
   GetKeystoreService(browser_context())
-      ->RemoveCertificate(keystore, params->certificate, std::move(c));
+      ->DEPRECATED_RemoveCertificate(keystore, params->certificate,
+                                     std::move(c));
   return RespondLater();
 }
 
