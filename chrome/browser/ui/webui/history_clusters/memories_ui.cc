3cc33eccc3acd457324f57930b82285c196f4107
Tommy Li <tommycli@chromium.org>
2021-07-27 02:38:01 +0000

    [memories] Add debug info printing onto the WebUI surface
    A lot of zeroes in the scores for some reason, this CL is meant to help
    us debug why.
    
    Bug: 1171352, 1220765
    Change-Id: I7ef34a43f8ff7456d7bce21241575055b1c2d7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053063
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905557}

--- chrome/browser/ui/webui/history_clusters/memories_ui.cc
+++ chrome/browser/ui/webui/history_clusters/memories_ui.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/history_clusters/memories_ui.h"
 
+#include "base/feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/history_clusters/history_clusters_handler.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/history_clusters_resources.h"
 #include "chrome/grit/history_clusters_resources_map.h"
 #include "components/favicon_base/favicon_url_parser.h"
+#include "components/history_clusters/core/memories_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -48,6 +50,9 @@ content::WebUIDataSource* CreateAndSetupWebUIDataSource(Profile* profile) {
   source->AddString("searchPrompt", u"Search clusters");
   source->AddString("toggleButtonLabelLess", u"Show less");
   source->AddString("toggleButtonLabelMore", u"Show more");
+
+  source->AddBoolean("isDebug",
+                     base::FeatureList::IsEnabled(history_clusters::kDebug));
 
   webui::SetupWebUIDataSource(
       source,
