d9cc45cf2e72754f7d2e49499378002fef11648e
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2021-07-27 20:52:33 +0000

    SVG Text NG: Support 'outline' CSS Property
    Mirror legacy LayoutSVGText + SVGTextPainter Paint methods by adding an
    LayoutNGSVGText::Paint override to handle painting outlines after
    foreground painting.
    
    NGPhysicalBoxFragment::AddOutlineRectsForInlineBox, skipping one of the
    rects for IsSVG because current.RectInContainerFragment() could result
    in an incorrectly placed outline. The correct outlines are applied in
    AddOutlineRectsForCursor.
    
    NGPhysicalBoxFragment::AddOutlineRects, adding a branch for IsSvgText
    because the rect was sometimes appearing as a line or the wrong size.
    This affects the outline around <text> elements, not their descendants.
    
    NGPhysicalFragment::AddOutlineRectsForCursor, split kSvgText from kText
    and kGeneratedText so that kSvgText can accumulate outlines for either
    |outline_type|.
    
    Bug: 1179585
    Change-Id: Ic0328afb988571786cfc64b4b501370da05c5aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3002502
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905885}

--- third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.cc
+++ third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.cc
@@ -1245,8 +1245,16 @@ void NGPhysicalBoxFragment::AddOutlineRects(
   DCHECK(IsOutlineOwner());
 
   // For anonymous blocks, the children add outline rects.
-  if (!IsAnonymousBlock())
-    outline_rects->emplace_back(additional_offset, Size().ToLayoutSize());
+  if (!IsAnonymousBlock()) {
+    if (IsSvgText()) {
+      if (const NGFragmentItems* items = Items()) {
+        outline_rects->emplace_back(PhysicalRect::EnclosingRect(
+            GetLayoutObject()->ObjectBoundingBox()));
+      }
+    } else {
+      outline_rects->emplace_back(additional_offset, Size().ToLayoutSize());
+    }
+  }
 
   if (outline_type == NGOutlineType::kIncludeBlockVisualOverflow &&
       !HasNonVisibleOverflow() && !HasControlClip(*this)) {
@@ -1308,7 +1316,7 @@ void NGPhysicalBoxFragment::AddOutlineRectsForInlineBox(
 #if DCHECK_IS_ON()
     has_this_fragment = has_this_fragment || current.BoxFragment() == this;
 #endif
-    if (!current.Size().IsZero())
+    if (!current.Size().IsZero() && !current.GetLayoutObject()->IsSVG())
       rects->push_back(current.RectInContainerFragment());
 
     // Add descendants if any, in the container-relative coordinate.
