a4dcd48ee71767b2568e24ff5287733b2396a678
Keishi Hattori <keishi@chromium.org>
2021-07-27 10:12:56 +0000

    Reland "Treat raw_ptr specially in FindBadConstructsConsumer"
    This is a reland of 4f741d34e009c30524ec5e3e68224f460bff1aea
    
    Original change's description:
    > Treat raw_ptr specially in FindBadConstructsConsumer
    >
    > CheckedPtr is getting renamed to raw_ptr. This adds the flag raw-ptr-template-as-trivial-member so we can treat raw_ptr<T> as a trivial member when calculating the complexity score.
    >
    > Bug: 1080832
    > Change-Id: I8343d7a7052244433eed0ea286fd763bc7c00eaa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033206
    > Commit-Queue: Keishi Hattori <keishi@chromium.org>
    > Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#903782}
    
    Bug: 1080832
    Change-Id: Ib65dba6d662ce1d980075eaf70d981806af97b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054411
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905664}

---
+++ tools/clang/plugins/tests/base/memory/raw_ptr.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_CLANG_PLUGINS_TESTS_BASE_MEMORY_RAW_PTR_H_
+#define TOOLS_CLANG_PLUGINS_TESTS_BASE_MEMORY_RAW_PTR_H_
+
+namespace base {
+
+template <typename T>
+class raw_ptr {};
+
+}  // namespace base
+
+using base::raw_ptr;
+
+#endif  // TOOLS_CLANG_PLUGINS_TESTS_BASE_MEMORY_RAW_PTR_H_
