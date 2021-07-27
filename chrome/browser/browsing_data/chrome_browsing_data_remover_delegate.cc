401cfe279616bac135baba32560cbbbfe1e38f14
Elly Fong-Jones <ellyjones@chromium.org>
2021-07-27 15:32:42 +0000

    smartsorting: clear usage databases when clearing browsing data
    The ShareHistory database is cleared for the time interval the user
    clears for. Since ShareRanking doesn't have a concept of which ranking
    data is from which time, ShareRanking deletes its entire database
    whenever the user clears any interval of history.
    
    Note that the test coverage for ShareRanking is missing pending a fix to
    issue 1232529, without which the tests are too unwieldy and brittle.
    
    Change-Id: I3514400d037d4d57966e3f41ff738628063dc94c
    Bug: 1229722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3046948
    Reviewed-by: Sophey <sophey@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905747}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
@@ -73,6 +73,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/scoped_profile_keep_alive.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/share/share_history.h"
+#include "chrome/browser/share/share_ranking.h"
 #include "chrome/browser/spellchecker/spellcheck_factory.h"
 #include "chrome/browser/spellchecker/spellcheck_service.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
@@ -641,6 +643,13 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
 
     FindBarStateFactory::GetForBrowserContext(profile_)->SetLastSearchText(
         std::u16string());
+
+#if defined(OS_ANDROID)
+    if (auto* share_history = sharing::ShareHistory::Get(profile_))
+      share_history->Clear(delete_begin_, delete_end_);
+    if (auto* share_ranking = sharing::ShareRanking::Get(profile_))
+      share_ranking->Clear(delete_begin_, delete_end_);
+#endif
   }
 
   //////////////////////////////////////////////////////////////////////////////
