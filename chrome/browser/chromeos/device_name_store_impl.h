a0b8fc0df7a6b7a258a4e4b3fc568c3d3d0c3120
jahchuen <ahchuen@google.com>
2021-07-27 18:11:06 +0000

    [CrOS Device Name] Add observer class and NameUpdateResult enum class
    This CL adds a new observer class to DeviceNameStore. We create a new
    NotifyDeviceNameChanged() function that notifies observers of name
    changes. We change the return type of DeviceNameStore::SetDeviceName to
    NameUpdateResult to give the result of the update. In unit tests, we now
    specify whether the fake user created is a device owner or not.
    
    Change-Id: I559224cc122e34319fb25f9942be2c6683d125c9
    Bug: 126802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3046497
    Commit-Queue: Jason Ah Chuen <ahchuen@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905820}

--- chrome/browser/chromeos/device_name_store_impl.h
+++ chrome/browser/chromeos/device_name_store_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/device_name_store.h"
 
+#include "base/scoped_observation.h"
 #include "chrome/browser/ash/policy/handlers/device_name_policy_handler.h"
 
 namespace chromeos {
@@ -26,6 +27,8 @@ class DeviceNameStoreImpl : public DeviceNameStore,
 
   // DeviceNameStore:
   std::string GetDeviceName() const override;
+  DeviceNameStore::SetDeviceNameResult SetDeviceName(
+      const std::string& new_device_name) override;
 
  private:
   friend class DeviceNameStoreImplTest;
@@ -40,15 +43,26 @@ class DeviceNameStoreImpl : public DeviceNameStore,
   // Computes the new device name according to the device name policy.
   std::string ComputeDeviceName() const;
 
-  // Updates the device name if it is different from the one set previously in
-  // |prefs_|.
-  void UpdateDeviceName();
+  // Returns whether the device name policy in place prohits name update.
+  bool IsConfiguringDeviceNameProhibitedByPolicy();
+
+  // Sets the device name and notify observers of DeviceNameStore class.
+  void ChangeDeviceName(const std::string& device_name);
+
+  // Called from OnHostnamePolicyChanged() and SetDeviceName() to set the device
+  // name and notify observers of DeviceNameStore class. The new device name
+  // must be different from the one set previously in |prefs_|.
+  void AttemptDeviceNameChange(const std::string& device_name);
 
   // Provides access and persistence for the device name value.
   PrefService* prefs_;
 
   policy::DeviceNamePolicyHandler* handler_;
   std::unique_ptr<DeviceNameApplier> device_name_applier_;
+
+  base::ScopedObservation<policy::DeviceNamePolicyHandler,
+                          policy::DeviceNamePolicyHandler::Observer>
+      observation_{this};
 };
 
 }  // namespace chromeos
