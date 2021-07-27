fad97aa16091d230e86f284f131e5b738cc567b2
Sergey Poromov <poromov@chromium.org>
2021-07-27 10:46:46 +0000

    dlp: Fix destination URL for clipboard events.
    Due to a bug source URL was reported in destination URL field.
    Tests are also updated.
    
    Bug: 1233416
    Test: Update unit tests to properly test it.
    Change-Id: Ibe949eaa199eecb19322a4357a43dd81e57e8e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055298
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905674}

--- chrome/browser/ash/policy/dlp/dlp_reporting_manager_unittest.cc
+++ chrome/browser/ash/policy/dlp/dlp_reporting_manager_unittest.cc
@@ -44,10 +44,10 @@ TEST_F(DlpReportingManagerTest, ReportEvent) {
                        DlpRulesManager::Level::kBlock);
 
   EXPECT_EQ(events_.size(), 1);
-  EXPECT_THAT(events_[0],
-              IsDlpPolicyEvent(CreateDlpPolicyEvent(
-                  kCompanyPattern, DlpRulesManager::Restriction::kPrinting,
-                  DlpRulesManager::Level::kBlock)));
+  EXPECT_EQ(events_[0].source().url(), kCompanyPattern);
+  EXPECT_FALSE(events_[0].has_destination());
+  EXPECT_EQ(events_[0].restriction(), DlpPolicyEvent_Restriction_PRINTING);
+  EXPECT_EQ(events_[0].mode(), DlpPolicyEvent_Mode_BLOCK);
 }
 
 TEST_F(DlpReportingManagerTest, ReportEventWithUrlDst) {
@@ -57,10 +57,11 @@ TEST_F(DlpReportingManagerTest, ReportEventWithUrlDst) {
                        DlpRulesManager::Level::kBlock);
 
   EXPECT_EQ(events_.size(), 1);
-  EXPECT_THAT(events_[0], IsDlpPolicyEvent(CreateDlpPolicyEvent(
-                              kCompanyPattern, dst_pattern,
-                              DlpRulesManager::Restriction::kClipboard,
-                              DlpRulesManager::Level::kBlock)));
+  EXPECT_EQ(events_[0].source().url(), kCompanyPattern);
+  EXPECT_EQ(events_[0].destination().url(), dst_pattern);
+  EXPECT_FALSE(events_[0].destination().has_component());
+  EXPECT_EQ(events_[0].restriction(), DlpPolicyEvent_Restriction_CLIPBOARD);
+  EXPECT_EQ(events_[0].mode(), DlpPolicyEvent_Mode_BLOCK);
 }
 
 TEST_F(DlpReportingManagerTest, ReportEventWithComponentDst) {
@@ -69,10 +70,12 @@ TEST_F(DlpReportingManagerTest, ReportEventWithComponentDst) {
                        DlpRulesManager::Level::kBlock);
 
   EXPECT_EQ(events_.size(), 1);
-  EXPECT_THAT(events_[0], IsDlpPolicyEvent(CreateDlpPolicyEvent(
-                              kCompanyPattern, DlpRulesManager::Component::kArc,
-                              DlpRulesManager::Restriction::kClipboard,
-                              DlpRulesManager::Level::kBlock)));
+  EXPECT_EQ(events_[0].source().url(), kCompanyPattern);
+  EXPECT_FALSE(events_[0].destination().has_url());
+  EXPECT_EQ(events_[0].destination().component(),
+            DlpPolicyEventDestination_Component_ARC);
+  EXPECT_EQ(events_[0].restriction(), DlpPolicyEvent_Restriction_CLIPBOARD);
+  EXPECT_EQ(events_[0].mode(), DlpPolicyEvent_Mode_BLOCK);
 }
 
 TEST_F(DlpReportingManagerTest, MetricsReported) {
