fa03c0c30808a638d2ea015d65286a31d16519bb
Tomasz Wiszkowski <ender@google.com>
2021-07-27 20:40:57 +0000

    Add logic tracking possible UAF for ACMatch.
    The logic here should help us track and report any instances where
    ACMatch was used after being freed.
    
    This logic will track, report and prevent any native operations on
    freed ACMatch objects including, but not limited to, match deletions
    and navigations. Note that the navigation itself will still work, but
    the change may prevent some data (such as AQS) from being reported
    if the native match is already deleted.
    
    Assuming we have no instances of UAF, the change should be safe to
    roll back as a whole, since the metric enum introduced here should
    never be recorded - alternatively we can keep the enum value around.
    
    Bug: 1217575
    Change-Id: I6cdd88bd37cff8b882b3b952d9edff32f3019ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053416
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905881}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/cxx17_backports.h"
 #include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/i18n/case_conversion.h"
 #include "base/logging.h"
@@ -42,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-
 bool IsTrivialClassification(const ACMatchClassifications& classifications) {
   return classifications.empty() ||
       ((classifications.size() == 1) &&
@@ -225,7 +225,11 @@ AutocompleteMatch::AutocompleteMatch(const AutocompleteMatch& match)
       additional_info(match.additional_info),
       duplicate_matches(match.duplicate_matches),
       query_tiles(match.query_tiles),
-      navsuggest_tiles(match.navsuggest_tiles) {}
+      navsuggest_tiles(match.navsuggest_tiles) {
+#if defined(OS_ANDROID)
+  magic_signature_ = match.magic_signature_;
+#endif
+}
 
 AutocompleteMatch::AutocompleteMatch(AutocompleteMatch&& match) noexcept {
   *this = std::move(match);
@@ -286,6 +290,8 @@ AutocompleteMatch& AutocompleteMatch::operator=(
   DestroyJavaObject();
   std::swap(java_match_, match.java_match_);
   UpdateJavaObjectNativeRef();
+  magic_signature_ = match.magic_signature_;
+  match.magic_signature_ = 0;
 #endif
   return *this;
 }
@@ -293,6 +299,7 @@ AutocompleteMatch& AutocompleteMatch::operator=(
 AutocompleteMatch::~AutocompleteMatch() {
 #if defined(OS_ANDROID)
   DestroyJavaObject();
+  magic_signature_ = 0;
 #endif
 }
 
@@ -965,6 +972,16 @@ void AutocompleteMatch::RecordAdditionalInfo(const std::string& property,
 
 std::string AutocompleteMatch::GetAdditionalInfo(
     const std::string& property) const {
+#if defined(OS_ANDROID)
+  if (!CheckMatchAlive()) {
+    SCOPED_CRASH_KEY_STRING32("ACMatch", "bad-magic", "true");
+    DCHECK(false)
+        << "Please report this on crbug.com/1217575, and include both C++ and "
+        << "Java stack traces where possible.";
+    base::debug::DumpWithoutCrashing();
+    return "";
+  }
+#endif
   auto i(additional_info.find(property));
   return (i == additional_info.end()) ? std::string() : i->second;
 }
