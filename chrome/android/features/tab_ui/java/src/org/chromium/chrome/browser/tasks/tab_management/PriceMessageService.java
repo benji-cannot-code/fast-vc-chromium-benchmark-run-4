416bb20550358f3a05940cd30cc2476d1428c66b
Zhiyuan Cai <zhiyuancai@chromium.org>
2021-07-27 23:15:40 +0000

    [PriceTracking] Add metrics for price message cards
    Bug: 1233215
    Change-Id: I6dcafce3019cd1d084335ffd9360eb4a864460a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054542
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905939}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/PriceMessageService.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/PriceMessageService.java
@@ -9,6 +9,7 @@ import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.price_tracking.PriceDropNotificationManager;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.state.ShoppingPersistedTabData;
@@ -217,6 +218,7 @@ public class PriceMessageService extends MessageService {
             mPriceWelcomeMessageProvider.showPriceDropTooltip(bindingTabIndex);
             PriceTrackingUtilities.disablePriceWelcomeMessageCard();
             mPriceTabData = null;
+            RecordUserAction.record("Commerce.PriceWelcomeMessageCard.Reviewed");
         } else if (type == PriceMessageType.PRICE_ALERTS) {
             if (mNotificationManager.areAppNotificationsEnabled()) {
                 mNotificationManager.createNotificationChannel();
@@ -224,6 +226,7 @@ public class PriceMessageService extends MessageService {
                 mNotificationManager.launchNotificationSettings();
             }
             PriceTrackingUtilities.disablePriceAlertsMessageCard();
+            RecordUserAction.record("Commerce.PriceAlertsMessageCard.Reviewed");
         }
     }
 
@@ -232,8 +235,10 @@ public class PriceMessageService extends MessageService {
         if (type == PriceMessageType.PRICE_WELCOME) {
             PriceTrackingUtilities.disablePriceWelcomeMessageCard();
             mPriceTabData = null;
+            RecordUserAction.record("Commerce.PriceWelcomeMessageCard.Dismissed");
         } else if (type == PriceMessageType.PRICE_ALERTS) {
             PriceTrackingUtilities.disablePriceAlertsMessageCard();
+            RecordUserAction.record("Commerce.PriceAlertsMessageCard.Dismissed");
         }
     }
 
