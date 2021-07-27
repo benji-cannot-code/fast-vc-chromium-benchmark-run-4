c27dab6bb1599bfeee1630af5e28899cfe61c8d4
Ben Reich <benreich@chromium.org>
2021-07-27 07:22:55 +0000

    [FilesBanner] Implement initialize method with smoke unittest
    The initialize method sets up the local storage cache using either
    existing values or defaults. This includes the basic plumbing for the JS
    unittests. Avoiding unittesting the initialization logic as it will be
    heavily tested in upcoming CLs around reconciliation and banner display
    logic.
    
    Bug: 1228128
    Test: browser_tests --gtest_filter=FileManagerJsTest.BannerController
    Change-Id: I515458eabb824049ad8fb906e7656916ddf394dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053245
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905612}

--- ui/file_manager/file_manager/common/js/mock_chrome.js
+++ ui/file_manager/file_manager/common/js/mock_chrome.js
@@ -59,7 +59,10 @@ export class MockChromeStorageAPI {
       local: {
         get: this.get_.bind(this),
         set: this.set_.bind(this),
-      }
+      },
+      onChanged: {
+        addListener: function() {},
+      },
     };
   }
 
