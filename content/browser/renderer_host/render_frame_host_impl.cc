3686fed0564f2504145a2dde41ce04a4708f6e22
Victor Costan <pwnall@chromium.org>
2021-07-27 02:56:15 +0000

    Font Access: Make FontAccessManager::BindingContext private.
    BindingContext is an implementation detail of FontAccessManager, and
    should not be exposed outside the class. For example,
    RenderFrameHostImpl shouldn't require changes whenever BindingContext is
    changed.
    
    This CL fixes this abstraction problem.
    
    Bug: 1232812
    Change-Id: I59dd1d9133e548d2d5f37cbbf082af69f1f0bc18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3052712
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905564}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -9202,8 +9202,7 @@ void RenderFrameHostImpl::GetFontAccessManager(
     mojo::PendingReceiver<blink::mojom::FontAccessManager> receiver) {
   static_cast<StoragePartitionImpl*>(GetProcess()->GetStoragePartition())
       ->GetFontAccessManager()
-      ->BindReceiver(FontAccessManagerImpl::BindingContext(
-                         GetLastCommittedOrigin(), GetGlobalId()),
+      ->BindReceiver(GetLastCommittedOrigin(), GetGlobalId(),
                      std::move(receiver));
 }
 
