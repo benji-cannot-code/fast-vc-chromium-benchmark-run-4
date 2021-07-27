1a8bdf26dbca701a8b8a5f28ae58cd66355cf71a
btolsch <btolsch@chromium.org>
2021-07-27 15:57:18 +0000

    Add openscreen cast_auth_util fuzzer
    This change ports the existing //components/cast_channel authentication
    fuzzer to instead call the same logic from //third_party/openscreen.
    This is not landing in //third_party/openscren so it will be picked up
    by ClusterFuzz.
    
    Bug: b/185815206
    Change-Id: Ic802f3c055834640caa312c4d4cb78f4466ec8f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053514
    Commit-Queue: Brandon Tolsch <btolsch@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905756}

---
+++ components/cast_channel/cast_auth_util_fuzzer_shared.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CAST_CHANNEL_CAST_AUTH_UTIL_FUZZER_SHARED_H_
+#define COMPONENTS_CAST_CHANNEL_CAST_AUTH_UTIL_FUZZER_SHARED_H_
+
+#include <string>
+#include <vector>
+#include "components/cast_channel/fuzz_proto/fuzzer_inputs.pb.h"
+
+namespace cast_channel {
+namespace fuzz {
+
+// Potentially updates |input| before it should be used for fuzzing.  For
+// example, if an auth message is present in the proto, it will override
+// |cast_message|.
+void SetupAuthenticateChallengeReplyInput(
+    const std::vector<std::string>& certs,
+    CastAuthUtilInputs::AuthenticateChallengeReplyInput* input);
+
+}  // namespace fuzz
+}  // namespace cast_channel
+
+#endif  // COMPONENTS_CAST_CHANNEL_CAST_AUTH_UTIL_FUZZER_SHARED_H_
