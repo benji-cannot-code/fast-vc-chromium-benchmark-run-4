c3431577c4af552bdc91723cae17c3ad72510492
Kuo Jen Wei <inker@google.com>
2021-07-27 06:37:37 +0000

    CCA: Add new camera folder thumbnail search file type "SCN_*"
    Add files with "SCN_*" name pattern to be scanned in camera folder for
    displaying thumbnail.
    
    Bug: b/190689433
    Test: Manually
    Change-Id: Ic70092adc41075f737d9c7758f328dc5a47e410d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054216
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905603}

--- chromeos/components/camera_app_ui/resources/js/models/file_system.js
+++ chromeos/components/camera_app_ui/resources/js/models/file_system.js
@@ -6,7 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {assert} from '../chrome_util.js';
 import {WaitableEvent} from '../waitable_event.js';
 
-import {Filenamer, IMAGE_PREFIX, VIDEO_PREFIX} from './file_namer.js';
+import {
+  DOCUMENT_PREFIX,
+  Filenamer,
+  IMAGE_PREFIX,
+  VIDEO_PREFIX,
+} from './file_namer.js';
 import {
   DirectoryAccessEntry,  // eslint-disable-line no-unused-vars
   DirectoryAccessEntryImpl,
@@ -32,6 +37,15 @@ export function hasVideoPrefix(entry) {
  */
 function hasImagePrefix(entry) {
   return entry.name.startsWith(IMAGE_PREFIX);
+}
+
+/**
+ * Checks if the entry's name has the document prefix.
+ * @param {!FileAccessEntry} entry File entry.
+ * @return {boolean} Has the document prefix or not.
+ */
+function hasDocumentPrefix(entry) {
+  return entry.name.startsWith(DOCUMENT_PREFIX);
 }
 
 /**
@@ -174,7 +188,8 @@ export async function createPrivateTempVideoFile() {
 export async function getEntries() {
   const entries = await cameraDir.getFiles();
   return entries.filter((entry) => {
-    if (!hasVideoPrefix(entry) && !hasImagePrefix(entry)) {
+    if (!hasVideoPrefix(entry) && !hasImagePrefix(entry) &&
+        !hasDocumentPrefix(entry)) {
       return false;
     }
     return entry.name.match(/_(\d{8})_(\d{6})(?: \((\d+)\))?/);
