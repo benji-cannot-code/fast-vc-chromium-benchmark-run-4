716c27947833056a15ec197af7887e41298230fd
Shane Fitzpatrick <shanefitz@google.com>
2021-07-27 17:22:42 +0000

    [FastPair] Browser delegate to access chrome/browser dependencies
    This change introduces QuickPairBrowserDelegate, a delegate
    implementation to enable the //ash/quick_pair code to access
    dependencies from chrome/browser.
    
    Initial implementation has an accessor for the active profile's
    SharedURLLoaderFactory.
    
    Change-Id: I3efa783cb6df996c2cf0887d8e362de30e5f7469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055015
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#905787}

--- ash/quick_pair/common/constants.cc
+++ ash/quick_pair/common/constants.cc
@@ -3,19 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_QUICK_PAIR_COMMON_CONSTANTS_H_
-#define ASH_QUICK_PAIR_COMMON_CONSTANTS_H_
+#include "ash/quick_pair/common/constants.h"
 
-#include "base/component_export.h"
 #include "device/bluetooth/public/cpp/bluetooth_uuid.h"
 
 namespace ash {
 namespace quick_pair {
 
-COMPONENT_EXPORT(QUICK_PAIR_COMMON)
-extern const device::BluetoothUUID kFastPairBluetoothUuid;
+const device::BluetoothUUID kFastPairBluetoothUuid("0xFE2C");
 
-}  // namespace quick_pair
+}
 }  // namespace ash
-
-#endif  // ASH_QUICK_PAIR_COMMON_CONSTANTS_H_
