3119cbef9bb944e851518276a580466df5424a8c
Maria Lavinia Nedelea <lavinian@google.com>
2021-07-27 09:45:19 +0000

    Enable MICE for EG Class.
    This patch enables MICE for sync_encryption_passphrase_table_view_controller_egtest.mm.
    
    Change-Id: Icf680bbb14d3f2d258b4f14c49898b1d55ae77c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053973
    Commit-Queue: Maria Lavinia Nedelea <lavinian@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905661}

--- ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
+++ ios/chrome/browser/ui/settings/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import <UIKit/UIKit.h>
-#include "components/signin/public/base/account_consistency_method.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h"
@@ -35,12 +34,6 @@ using chrome_test_util::PrimarySignInButton;
 @end
 
 @implementation SyncEncryptionPassphraseTestCase
-
-- (AppLaunchConfiguration)appConfigurationForTestCase {
-  AppLaunchConfiguration config;
-  config.features_disabled.push_back(signin::kMobileIdentityConsistency);
-  return config;
-}
 
 // Tests to open the sync passphrase view, and to close it.
 - (void)testShowSyncPassphraseAndDismiss {
@@ -101,11 +94,6 @@ using chrome_test_util::PrimarySignInButton;
   [ChromeEarlGreyUI tapSettingsMenuButton:PrimarySignInButton()];
   [SigninEarlGreyUI tapSettingsLink];
 
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(
-                                   kManageSyncCellAccessibilityIdentifier)]
-      performAction:grey_tap()];
-
   // Scroll to bottom of Manage Sync Settings, if necessary.
   [[EarlGrey selectElementWithMatcher:
                  grey_allOf(grey_accessibilityID(
@@ -130,8 +118,6 @@ using chrome_test_util::PrimarySignInButton;
                                        l10n_util::GetNSString(
                                            IDS_IOS_SYNC_DECRYPT_BUTTON)),
                                    nil)] performAction:grey_tap()];
-  [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
-      performAction:grey_tap()];
   [[EarlGrey selectElementWithMatcher:SyncSettingsConfirmButton()]
       performAction:grey_tap()];
 
