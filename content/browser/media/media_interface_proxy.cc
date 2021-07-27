a1fa72c1df71c35532263b6dd0b58dbbcce9cd94
Jeffrey Kardatzke <jkardatzke@google.com>
2021-07-27 19:04:27 +0000

    Remove expired cdm-factory-daemon feature flag
    This expired in M91, removing it and defaulting it to enabled. This code
    will also be used by Lacros and it references an ash feature, so it
    needs to be removed for that as well.
    
    BUG=b:194429120
    TEST=Protected content works on volteer
    
    Change-Id: Iec52a6f5e2e1232da2cf8bd5e478ef186f2ebc0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053461
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#905844}

--- content/browser/media/media_interface_proxy.cc
+++ content/browser/media/media_interface_proxy.cc
@@ -49,9 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/mojom/cdm_service.mojom.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#if BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
-#include "ash/constants/ash_features.h"
-#endif  // BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 #endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
 
@@ -360,8 +357,7 @@ void MediaInterfaceProxy::CreateCdm(const std::string& key_system,
 
   // Handle `use_hw_secure_codecs` cases first.
 #if BUILDFLAG(IS_CHROMEOS_ASH) && BUILDFLAG(USE_CHROMEOS_PROTECTED_MEDIA)
-  if (base::FeatureList::IsEnabled(chromeos::features::kCdmFactoryDaemon) &&
-      cdm_config.use_hw_secure_codecs &&
+  if (cdm_config.use_hw_secure_codecs &&
       cdm_config.allow_distinctive_identifier) {
     auto* factory = media_interface_factory_ptr_->Get();
     if (factory) {
