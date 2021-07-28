cfdf1b88766116553b5fe656c19e0fc5bd9e4893
Jeffrey Young <cowmoo@google.com>
2021-07-28 22:36:19 +0000

    personalization: correct file comment
    update @fileoverview comment to use the correct class name and
    give more information.
    
    BUG=NONE
    
    Change-Id: I185486d59309471815f96e1c7308927ee504f294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056780
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906419}

--- chromeos/components/personalization_app/resources/trusted/wallpaper_error_element.js
+++ chromeos/components/personalization_app/resources/trusted/wallpaper_error_element.js
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @fileoverview NetworkErrorElement displays a network error svg. This is done
- * in-line instead of using iron-iconset-svg because iron-iconset-svg is
- * designed for small square icons that may have multiple sizes, not large
- * rectangular svgs.
+ * @fileoverview WallpaperErrorElement displays an error svg when wallpaper
+ * collections fail to load. This is done in-line instead of using
+ * iron-iconset-svg because iron-iconset-svg is designed for small square icons
+ * that may have multiple sizes, not large rectangular svgs.
  */
 
 import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
