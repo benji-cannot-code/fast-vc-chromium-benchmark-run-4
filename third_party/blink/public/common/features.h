bf3ee474d036d0b8ebbaaf54b5699d49e19ec303
Yuki Shiino <yukishiino@chromium.org>
2021-07-28 08:52:14 +0000

    bindings: Remove a feature flag that's enabled in Stable
    Removes a feature flag used at bindings, which has already been
    enabled in Stable channel.
    
    Bug: 538562
    Change-Id: Idb259a6b2e9ffffcd9b06ab3c114eaeaebf1c3ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056210
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906105}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -167,9 +167,6 @@ BLINK_COMMON_EXPORT extern const base::Feature
 BLINK_COMMON_EXPORT extern const base::Feature
     kBlinkCompositorUseDisplayThreadPriority;
 
-BLINK_COMMON_EXPORT extern const base::Feature
-    kIgnoreCrossOriginWindowWhenNamedAccessOnWindow;
-
 BLINK_COMMON_EXPORT extern const base::Feature kTransformInterop;
 BLINK_COMMON_EXPORT extern const base::Feature kBackfaceVisibilityInterop;
 
