d24cb9d8e12a8584cc9d6a0683b38eb124ab0988
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 23:22:15 +0000

    [automated] Drop needless <vector> includes in /components/url_matcher
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=battre@chromium.org
    
    Bug: 1233968
    Change-Id: Id9ae552a15f9314700673462c60b0880bc3424e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059484
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906444}

--- components/url_matcher/string_pattern.h
+++ components/url_matcher/string_pattern.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_URL_MATCHER_STRING_PATTERN_H_
 
 #include <string>
-#include <vector>
 
 #include "base/macros.h"
 #include "components/url_matcher/url_matcher_export.h"
