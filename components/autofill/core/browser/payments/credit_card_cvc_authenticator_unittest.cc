a51e136af39b043fd23c6646d5c50b87537a6c3a
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 19:26:26 +0000

    [automated] Drop needless <vector> includes in /components/autofill/core/browser/payments
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=jsaul@google.com
    
    Bug: 1233968
    Change-Id: I7d6e2db23577c150c42fb5063bf5fe04d4e5a8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056925
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#906320}

--- components/autofill/core/browser/payments/credit_card_cvc_authenticator_unittest.cc
+++ components/autofill/core/browser/payments/credit_card_cvc_authenticator_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <tuple>
 #include <utility>
-#include <vector>
 
 #include "base/base64.h"
 #include "base/command_line.h"
