afe7e988f49c2b71277f99b28721c25801bde5f9
Yohei Maeda <yoheimaeda@google.com>
2021-07-28 10:10:17 +0000

    Remove unnecessary IsServedFromBackForwardCache() condition
    This IsServedFromBackForwardCache() condition is included in
    IsInMainFrame() condition a few lines ago because back forward
    cache navigations are always in the main frame.
    
    Both conditional branches return the same result, so
    the IsServedFromBackForwardCache() condition can be removed.
    
    Bug: 1233839
    Change-Id: I6795e8aa4e49aff672f1fb803768b3d4137043b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056214
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Yohei Maeda <yoheimaeda@google.com>
    Cr-Commit-Position: refs/heads/master@{#906120}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -4811,11 +4811,11 @@ void NavigationRequest::SetupCSPEmbeddedEnforcement() {
 
 NavigationRequest::CSPEmbeddedEnforcementResult
 NavigationRequest::CheckCSPEmbeddedEnforcement() {
-  // We enforce CSPEE only for frames, not for portals.
+  // We enforce CSPEE only for subframes, not for portals.
   if (IsInMainFrame())
     return CSPEmbeddedEnforcementResult::ALLOW_RESPONSE;
 
-  if (IsSameDocument() || IsServedFromBackForwardCache())
+  if (IsSameDocument())
     return CSPEmbeddedEnforcementResult::ALLOW_RESPONSE;
 
   if (!required_csp_)
