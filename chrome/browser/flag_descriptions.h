83cf2fedf059df0291c583383fcb298667177ed7
Kyle Milka <kmilka@chromium.org>
2021-07-28 00:34:02 +0000

    [DesktopScreenshots] Add to about:flags
    Change-Id: I1bec16fb8c448f8405555aff66f8873e4d7b3a7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3048135
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905970}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1349,6 +1349,9 @@ extern const char kServiceWorkerSubresourceFilterDescription[];
 extern const char kSharedClipboardUIName[];
 extern const char kSharedClipboardUIDescription[];
 
+extern const char kSharingDesktopScreenshotsName[];
+extern const char kSharingDesktopScreenshotsDescription[];
+
 extern const char kSharingHubDesktopAppMenuName[];
 extern const char kSharingHubDesktopAppMenuDescription[];
 
