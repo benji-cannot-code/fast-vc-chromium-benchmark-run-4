dce877d4003c8663939529ae933ba8435625f333
tby <tby@chromium.org>
2021-07-28 06:55:53 +0000

    [Structured metrics] Add getter for the last day a key was rotated.
    This will be used for some uses that want to record that a user is
    about to disappear from the logs due to key rotation, and not some
    other reason like the device being powerwashed.
    
    Bug: 1148168
    Change-Id: Ibd8804327bf96ce53cdc91dd3434e2f2414ac49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053259
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906075}

--- components/metrics/structured/event_base.h
+++ components/metrics/structured/event_base.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 #include <vector>
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace metrics {
 namespace structured {
@@ -67,6 +68,10 @@ class EventBase {
   // Finalizes the event and sends it for recording. After this call, the event
   // is left in an invalid state and should not be used further.
   void Record();
+
+  // Returns when the key for |project_name_hash| was last rotated, in days
+  // since epoch. Returns nullopt if the information is not available.
+  absl::optional<int> LastKeyRotation();
 
   std::vector<Metric> metrics() const { return metrics_; }
 
