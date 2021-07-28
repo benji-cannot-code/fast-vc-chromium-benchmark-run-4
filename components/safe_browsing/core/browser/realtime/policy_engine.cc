2c1a59ba7f7f13b9597a5a942b2c65eea25f36f6
Xinghui Lu <xinghuilu@chromium.org>
2021-07-28 17:50:27 +0000

    Remove ResourceType from Safe Browsing metrics.
    ResourceType was replaced with RequestDestination in M89[1]. We decided
    to keep it around in the safe_browsing namespace for UMA logging.
    
    Now that most users have updated to M89 and the new histograms are
    propagated, we can safely replace *ResourceTypes2* histograms with
    *RequestDestination* histograms. Also remove other helper functions
    for converting RequestDestination to ResourceTypes2.
    
    Histograms obsoleted in this CL:
    SB2.ResourceTypes2
    SB2.ResourceTypes2.Checked
    SB2.ResourceTypes2.Skipped
    SB2.ResourceTypes2.Unsafe
    SafeBrowsing.RT.ResourceTypes.Checked
    SafeBrowsing.RT.ResourceTypes.Requested
    SB2.RequestDestination (marked as base="true" since the base histogram
    is not logged)
    
    Histograms extended in this CL:
    SB2.RequestDestination.Checked
    SB2.RequestDestination.Skipped
    SB2.RequestDestination.Unsafe
    
    [1] https://crrev.com/c/2589575
    
    Bug: 1214878
    Change-Id: Ia2a3d768ef4050f5e49ccee6c201d4656d79e44e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042845
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906261}

--- components/safe_browsing/core/browser/realtime/policy_engine.cc
+++ components/safe_browsing/core/browser/realtime/policy_engine.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
-#include "components/safe_browsing/core/common/safebrowsing_constants.h"
 #include "components/safe_browsing/core/common/utils.h"
 #include "components/unified_consent/pref_names.h"
 #include "components/user_prefs/user_prefs.h"
@@ -92,10 +91,6 @@ bool RealTimePolicyEngine::CanPerformEnterpriseFullURLLookup(
 bool RealTimePolicyEngine::CanPerformFullURLLookupForRequestDestination(
     network::mojom::RequestDestination request_destination,
     bool can_rt_check_subresource_url) {
-  UMA_HISTOGRAM_ENUMERATION(
-      "SafeBrowsing.RT.ResourceTypes.Requested",
-      safe_browsing::GetResourceTypeFromRequestDestination(
-          request_destination));
   UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.RT.RequestDestinations.Requested",
                             request_destination);
   if (request_destination == network::mojom::RequestDestination::kDocument) {
