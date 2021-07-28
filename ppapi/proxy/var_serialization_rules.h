919686b31e1ff93ffee6b6277c4bb823a79eb75f
Maksim Ivanov <emaxx@chromium.org>
2021-07-28 00:53:04 +0000

    Make VarSerializationRules refcounter thread-safe
    Switch the ppapi::proxy::VarSerializationRules to use a thread-safe
    reference counting. This fixes the DCHECKs being hit when running
    some NaCl applications.
    
    The underlying root cause is not fully understood, but it seems related
    to message passing, as the DCHECK stack trace (obtained by manually
    adding logs) points to the SerializedVarSendInputShmem class and its
    instantiation in PPB_Instance_Proxy::PostMessage(); the actual DCHECK
    hit is triggered when incrementing the VarSerializationRules's ref
    counter.
    
    It seems that the implementation of the whole PPAPI proxy is actually
    thread-safe due to the use of a global ppapi::ProxyLock. Therefore the
    DCHECK hits might actually be false positives, and switching
    VarSerializationRules from RefCounted to RefCountedThreadSafe should
    suppress them.
    
    Bug: 756531
    Change-Id: I675002e882cf21fdd8bba63ba4f078f71ffe5bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3051930
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905984}

--- ppapi/proxy/var_serialization_rules.h
+++ ppapi/proxy/var_serialization_rules.h
@@ -15,7 +15,8 @@ namespace proxy {
 // Encapsulates the rules for serializing and deserializing vars to and from
 // the local process. The renderer and the plugin process each have separate
 // bookkeeping rules.
-class VarSerializationRules : public base::RefCounted<VarSerializationRules> {
+class VarSerializationRules
+    : public base::RefCountedThreadSafe<VarSerializationRules> {
  public:
   // Caller-owned calls --------------------------------------------------------
   //
@@ -79,7 +80,7 @@ class VarSerializationRules : public base::RefCounted<VarSerializationRules> {
   virtual ~VarSerializationRules() {}
 
  private:
-  friend class base::RefCounted<VarSerializationRules>;
+  friend class base::RefCountedThreadSafe<VarSerializationRules>;
 };
 
 }  // namespace proxy
