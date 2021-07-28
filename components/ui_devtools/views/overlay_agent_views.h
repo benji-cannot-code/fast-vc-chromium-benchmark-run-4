c9fdc0da8838ecbae53a853c45538ad3c526464f
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 22:25:24 +0000

    [automated] Drop needless <vector> includes in /components/ui_devtools/views
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=lgrey@chromium.org
    
    Bug: 1233968
    Change-Id: I61707caaa8f98b84cc6ca054c38e7a0693065ee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059446
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906414}

--- components/ui_devtools/views/overlay_agent_views.h
+++ components/ui_devtools/views/overlay_agent_views.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_UI_DEVTOOLS_VIEWS_OVERLAY_AGENT_VIEWS_H_
 #define COMPONENTS_UI_DEVTOOLS_VIEWS_OVERLAY_AGENT_VIEWS_H_
 
-#include <vector>
 
 #include "components/ui_devtools/Overlay.h"
 #include "components/ui_devtools/overlay_agent.h"
