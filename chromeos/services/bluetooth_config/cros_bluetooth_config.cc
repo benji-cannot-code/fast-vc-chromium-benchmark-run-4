b4e6080354ba0eab9fb9cfd7aae51a4c127026da
Kyle Horimoto <khorimoto@chromium.org>
2021-07-28 20:57:25 +0000

    [CrOS Bluetooth] Add SetBluetoothEnabledState() Mojo API
    This CL adds a new SetBluetoothEnabledState() function to
    AdapterStateController, then adds a new Mojo API function which
    delegates to this function. Internally, AdapterStateControllerImpl
    queues requests to enable/disable Bluetooth and executes them one after
    another, cancelling a request if the adapter is already in the correct
    state.
    
    Additionally, we add new kEnabling and kDisabling BluetoothSystemState
    enum values.
    
    Bug: 1010321
    Change-Id: Id5aee03ac844ca6a8488189ade2c02b4a6b573fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058391
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906374}

--- chromeos/services/bluetooth_config/cros_bluetooth_config.cc
+++ chromeos/services/bluetooth_config/cros_bluetooth_config.cc
@@ -31,5 +31,9 @@ void CrosBluetoothConfig::ObserveSystemProperties(
   system_properties_provider_->Observe(std::move(observer));
 }
 
+void CrosBluetoothConfig::SetBluetoothEnabledState(bool enabled) {
+  adapter_state_controller_->SetBluetoothEnabledState(enabled);
+}
+
 }  // namespace bluetooth_config
 }  // namespace chromeos
--- chromeos/services/bluetooth_config/fake_adapter_state_controller.h
+++ chromeos/services/bluetooth_config/fake_adapter_state_controller.h
@@ -23,6 +23,7 @@ class FakeAdapterStateController : public AdapterStateController {
  private:
   // AdapterStateController:
   mojom::BluetoothSystemState GetAdapterState() const override;
+  void SetBluetoothEnabledState(bool enabled) override;
 
   mojom::BluetoothSystemState system_state_ =
       mojom::BluetoothSystemState::kEnabled;
