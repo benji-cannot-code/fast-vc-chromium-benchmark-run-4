b7602eb127c413c2e06d43fbe48e944f8189948e
Kuo Jen Wei <inker@google.com>
2021-07-28 06:48:26 +0000

    CCA: Add new feature toast for scanner mode button
    Extract new feature toast effect into js module and apply document
    scanning new feature toast on scanner mode button.
    
    Bug: b/190689433
    Test: Manually
    
    Change-Id: Ic854ed018f6fd6c6f1bb371119aae222df4b5762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3047544
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906073}

--- chromeos/components/camera_app_ui/resources/.eslintrc.js
+++ chromeos/components/camera_app_ui/resources/.eslintrc.js
@@ -393,6 +393,7 @@ module.exports = {
     // TODO(b/190689433): Remove this once we have
     // https://github.com/sindresorhus/globals/pull/178 merged in ESlint and
     // Chromium.
+    'CSSNumericValue': 'readable',
     'CSSRotate': 'readable',
     'CSSScale': 'readable',
     'CSSTransformValue': 'readable',
--- chromeos/components/camera_app_ui/resources/js/externs/w3c_api.js
+++ chromeos/components/camera_app_ui/resources/js/externs/w3c_api.js
@@ -130,6 +130,12 @@ function StylePropertyMapReadOnly() {}
 StylePropertyMapReadOnly.prototype.get = function(property) {};
 
 /**
+ * @param {string} property
+ * @return {boolean}
+ */
+StylePropertyMapReadOnly.prototype.has = function(property) {};
+
+/**
  * @return {!StylePropertyMapReadOnly}
  */
 Element.prototype.computedStyleMap = function() {};
@@ -197,3 +203,15 @@ CSS.rad;
  * @return {!CSSStyleValue}
  */
 CSS.number;
+
+/**
+ * @constructor
+ * @extends {CSSStyleValue}
+ */
+function CSSNumericValue() {}
+
+/**
+ * @param {string} str
+ * @return {!CSSStyleValue}
+ */
+CSSNumericValue.parse;
