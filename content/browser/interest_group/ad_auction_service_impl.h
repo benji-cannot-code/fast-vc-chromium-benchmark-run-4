e617d46bc47bb41740f9153db0312a932f82561d
Maks Orlovich <morlovich@chromium.org>
2021-07-28 19:48:51 +0000

    AuctionRunner: add DebuggableAuctionWorklet
    This will be used by the debugger target host to interface to the worklets; it provides observation of creation/destruction and enumeration as well as a way of getting to the mojo pipe to the corresponding worklet, to be used for attaching to the agent.
    
    Bug: 1209620
    
    Change-Id: I2fc8b08f7f2faaf27866a06d38135b4f9d779bdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032183
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906336}

--- content/browser/interest_group/ad_auction_service_impl.h
+++ content/browser/interest_group/ad_auction_service_impl.h
@@ -26,6 +26,7 @@ namespace content {
 
 class AuctionRunner;
 class RenderFrameHost;
+class RenderFrameHostImpl;
 
 // Implements the AdAuctionService service called by Blink code.
 class CONTENT_EXPORT AdAuctionServiceImpl final
@@ -45,6 +46,7 @@ class CONTENT_EXPORT AdAuctionServiceImpl final
   // AuctionRunner::Delegate implementation:
   network::mojom::URLLoaderFactory* GetFrameURLLoaderFactory() override;
   network::mojom::URLLoaderFactory* GetTrustedURLLoaderFactory() override;
+  RenderFrameHostImpl* GetFrame() override;
 
   using DocumentServiceBase::origin;
   using DocumentServiceBase::render_frame_host;
---
+++ content/browser/interest_group/debuggable_auction_worklet.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/interest_group/debuggable_auction_worklet.h"
+
+#include "content/browser/interest_group/debuggable_auction_worklet_tracker.h"
+
+namespace content {
+
+DebuggableAuctionWorklet::DebuggableAuctionWorklet(
+    RenderFrameHostImpl* owning_frame,
+    const GURL& url,
+    auction_worklet::mojom::BidderWorklet* bidder_worklet,
+    bool& should_pause_on_start)
+    : owning_frame_(owning_frame), url_(url), bidder_worklet_(bidder_worklet) {
+  DebuggableAuctionWorkletTracker::GetInstance()->NotifyCreated(
+      this, should_pause_on_start);
+}
+
+DebuggableAuctionWorklet::DebuggableAuctionWorklet(
+    RenderFrameHostImpl* owning_frame,
+    const GURL& url,
+    auction_worklet::mojom::SellerWorklet* seller_worklet,
+    bool& should_pause_on_start)
+    : owning_frame_(owning_frame), url_(url), seller_worklet_(seller_worklet) {
+  DebuggableAuctionWorkletTracker::GetInstance()->NotifyCreated(
+      this, should_pause_on_start);
+}
+
+DebuggableAuctionWorklet::~DebuggableAuctionWorklet() {
+  DebuggableAuctionWorkletTracker::GetInstance()->NotifyDestroyed(this);
+}
+
+}  // namespace content
