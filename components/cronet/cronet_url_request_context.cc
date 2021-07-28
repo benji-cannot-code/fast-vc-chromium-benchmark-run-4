dccf9b4aa7d0e945381442e9570c7008811a4bdd
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 18:52:43 +0000

    [automated] Drop needless <vector> includes in /components/cronet
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=dschinazi@chromium.org
    
    Bug: 1233968
    Change-Id: I5b6486068bc90d14a41fac548f3477a8b0463113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056928
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906304}

--- components/cronet/cronet_url_request_context.cc
+++ components/cronet/cronet_url_request_context.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 #include <utility>
-#include <vector>
 
 #include "base/base64.h"
 #include "base/bind.h"
