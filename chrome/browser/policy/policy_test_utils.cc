495a4ec51e2381df2eacd5c7b6409b182e152e88
Sergey Poromov <poromov@chromium.org>
2021-07-28 06:37:45 +0000

    Refactor and re-enable ScreenshotPolicyTest
    We can try to re-enable the test after moving to the new
    ScreenCapture implementation.
    
    Bug: 476964
    Test: Run the enabled test
    Change-Id: I0464d2ad12ad98675d7bddfe3ecf39a6c5d682f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3048298
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906072}

--- chrome/browser/policy/policy_test_utils.cc
+++ chrome/browser/policy/policy_test_utils.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/policy/policy_test_utils.h"
 
-#include "ash/public/cpp/capture_mode_api.h"
-#include "ash/public/cpp/capture_mode_test_api.h"
 #include "base/callback_helpers.h"
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
@@ -41,10 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/transport_security_state.h"
 #include "services/network/public/mojom/network_service_test.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "ui/snapshot/screenshot_grabber.h"
-#endif
 
 using content::BrowserThread;
 using testing::_;
@@ -118,14 +112,6 @@ void PolicyTest::SetUpCommandLine(base::CommandLine* command_line) {
       {{"sendingThreshold", "1.0"}}, command_line);
 }
 
-void PolicyTest::SetScreenshotPolicy(bool enabled) {
-  PolicyMap policies;
-  policies.Set(key::kDisableScreenshots, POLICY_LEVEL_MANDATORY,
-               POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(!enabled),
-               nullptr);
-  UpdateProviderPolicy(policies);
-}
-
 void PolicyTest::SetRequireCTForTesting(bool required) {
   if (content::IsOutOfProcessNetworkService()) {
     mojo::Remote<network::mojom::NetworkServiceTest> network_service_test;
@@ -145,19 +131,6 @@ void PolicyTest::SetRequireCTForTesting(bool required) {
       base::BindOnce(&net::TransportSecurityState::SetRequireCTForTesting,
                      required));
 }
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-void PolicyTest::TestScreenshotFile(bool enabled) {
-  base::RunLoop run_loop;
-  ash::CaptureModeTestApi().SetOnCaptureFileSavedCallback(
-      base::BindLambdaForTesting(
-          [&run_loop](const base::FilePath& path) { run_loop.Quit(); }));
-
-  SetScreenshotPolicy(enabled);
-  ash::CaptureScreenshotsOfAllDisplays();
-  run_loop.Run();
-}
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 scoped_refptr<const extensions::Extension> PolicyTest::LoadUnpackedExtension(
     const base::FilePath::StringType& name) {
