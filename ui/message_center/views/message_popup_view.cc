702a6484372dfa43d956b58c4a1f48acdf003de2
Ahmed Mehfooz <amehfooz@chromium.org>
2021-07-28 19:36:25 +0000

    Change message popup timers behavior
    Currently message popup timers are paused if a popup is activated.
    This results in popups not being automatically dismissable right after
    login because they get activated by the FocusController in the absence
    of any other focusable window.
    
    This CL changes the criteria of pausing popup timers from activation
    to actually having focus. If a notification popup explicitly contains
    keyboard focus we will now pause popup timers.
    
    Bug: 1231181
    Change-Id: I7d29e748b5688ab076b5159e819c733eb9db700e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3050411
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906327}

--- ui/message_center/views/message_popup_view.cc
+++ ui/message_center/views/message_popup_view.cc
@@ -55,6 +55,8 @@ MessagePopupView::MessagePopupView(MessagePopupCollection* popup_collection)
 
 MessagePopupView::~MessagePopupView() {
   popup_collection_->NotifyPopupClosed(this);
+  if (focus_manager_)
+    focus_manager_->RemoveFocusChangeListener(this);
 }
 
 void MessagePopupView::UpdateContents(const Notification& notification) {
@@ -128,7 +130,6 @@ void MessagePopupView::Show() {
   views::Widget* widget = new views::Widget();
   popup_collection_->ConfigureWidgetInitParamsForContainer(widget, &params);
   widget->set_focus_on_creation(false);
-  observation_.Observe(widget);
 
 #if defined(OS_WIN)
   // We want to ensure that this toast always goes to the native desktop,
@@ -164,6 +165,11 @@ void MessagePopupView::Close() {
 
   if (!GetWidget()->IsClosed())
     GetWidget()->CloseNow();
+}
+
+void MessagePopupView::OnDidChangeFocus(views::View* before, views::View* now) {
+  is_focused_ = Contains(now);
+  popup_collection_->Update();
 }
 
 void MessagePopupView::OnMouseEntered(const ui::MouseEvent& event) {
@@ -209,15 +215,17 @@ void MessagePopupView::OnFocus() {
   GetFocusManager()->SetFocusedView(message_view_);
 }
 
-void MessagePopupView::OnWidgetActivationChanged(views::Widget* widget,
-                                                 bool active) {
-  is_active_ = active;
-  popup_collection_->Update();
+void MessagePopupView::AddedToWidget() {
+  focus_manager_ = GetFocusManager();
+  if (focus_manager_) {
+    focus_manager_->AddFocusChangeListener(this);
+  }
 }
 
-void MessagePopupView::OnWidgetDestroyed(views::Widget* widget) {
-  DCHECK(observation_.IsObservingSource(widget));
-  observation_.Reset();
+void MessagePopupView::RemovedFromWidget() {
+  if (focus_manager_)
+    focus_manager_->RemoveFocusChangeListener(this);
+  focus_manager_ = nullptr;
 }
 
 bool MessagePopupView::IsWidgetValid() const {
