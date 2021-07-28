3fbf9745d58da397df72b68ba02eca4a25a54422
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 21:35:07 +0000

    [automated] Drop needless <vector> includes in /components/zucchini
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=etiennep@chromium.org
    
    Bug: 1233968
    Change-Id: I4f84f26bfdf81c3997cc939c3d8a692e17ddde9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059580
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906383}

--- components/zucchini/reference_bytes_mixer.h
+++ components/zucchini/reference_bytes_mixer.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
-#include <vector>
 
 #include "components/zucchini/buffer_view.h"
 #include "components/zucchini/image_utils.h"
