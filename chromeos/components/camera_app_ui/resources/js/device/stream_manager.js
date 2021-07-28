aee12198a4c2a46656c76f97dc87af6cf8866aff
Pi-Hsun Shih <pihsun@chromium.org>
2021-07-28 06:29:41 +0000

    CCA: Change CaptureStream constructor to public
    In closure compiler, @private means private to the file, but for
    TypeScript it means private to the class. Mark the constructor as public
    to fix tsc warning.
    
    This drops the error of `cca.py tsc` from 139 to 138.
    
    Bug: b/172340451
    Test: cca.py tsc
    Change-Id: I510c5fd117c52c62bf00a2cd4448fc7587ac8165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054678
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906070}

--- chromeos/components/camera_app_ui/resources/js/device/stream_manager.js
+++ chromeos/components/camera_app_ui/resources/js/device/stream_manager.js
@@ -52,7 +52,7 @@ export class CaptureStream {
   /**
    * @param {string} deviceId Device id of currently working video device
    * @param {!MediaStream} stream Capture stream
-   * @private
+   * @public
    */
   constructor(deviceId, stream) {
     /**
