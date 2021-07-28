a3e090cd0fc4c393ba45bee473aebb1b64a91b3c
Antony Vu <antonyvu@google.com>
2021-07-28 17:37:29 +0000

    [Fuchsia] Create a long-standing emulator with fvdl
    Starts up an emulator for which can serve as a device for developers.
    Remove fuchsia_out_dir variable from emulator target classes.
    Add IsEmuStillRunning in fvdl_target to allow start_emulator to
    check if the emulator is still up.
    Add GetNetworkAddress to get the PID from vdl_output in order to
    connect with a FVDL emulator using tuntap.
    Add a check for --with-network flag to enable tuntap (-N)
    Created Start method in fvdl_target to wait for FVDL command to
    finish before trying to connect to it.
    Add unit test to check for -N flag
    Edited fuchsia.py and run_web_test.py to use a Namespace to create
    target classes and allow the deletion of fuchsia_out_dir in those
    target classes.
    
    Bug: 1199079
    Change-Id: I77b82506957644a83fdb5cace314d1ac5029f115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3034495
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Chong Gu <chonggu@google.com>
    Reviewed-by: Rohan Pavone <rohpavone@google.com>
    Commit-Queue: Antony Vu <antonyvu@google.com>
    Cr-Commit-Position: refs/heads/master@{#906253}

--- build/fuchsia/qemu_target.py
+++ build/fuchsia/qemu_target.py
@@ -41,16 +41,9 @@ def GetTargetType():
 class QemuTarget(emu_target.EmuTarget):
   EMULATOR_NAME = 'qemu'
 
-  def __init__(self,
-               out_dir,
-               target_cpu,
-               system_log_file,
-               cpu_cores,
-               require_kvm,
-               ram_size_mb,
-               fuchsia_out_dir=None):
-    super(QemuTarget, self).__init__(out_dir, target_cpu, system_log_file,
-                                     fuchsia_out_dir)
+  def __init__(self, out_dir, target_cpu, system_log_file, cpu_cores,
+               require_kvm, ram_size_mb):
+    super(QemuTarget, self).__init__(out_dir, target_cpu, system_log_file)
     self._cpu_cores=cpu_cores
     self._require_kvm=require_kvm
     self._ram_size_mb=ram_size_mb
@@ -58,8 +51,7 @@ class QemuTarget(emu_target.EmuTarget):
   @staticmethod
   def CreateFromArgs(args):
     return QemuTarget(args.out_dir, args.target_cpu, args.system_log_file,
-                      args.cpu_cores, args.require_kvm, args.ram_size_mb,
-                      args.fuchsia_out_dir)
+                      args.cpu_cores, args.require_kvm, args.ram_size_mb)
 
   def _IsKvmEnabled(self):
     kvm_supported = sys.platform.startswith('linux') and \
