12d91cfda765be9685fa45bbcdcfe86b39aec281
Mikel Astiz <mastiz@chromium.org>
2021-07-28 08:46:22 +0000

    [sync] Relax preconditions for SyncService::TriggerRefresh()
    Callers are now free to specify any arbitrary set of types to refresh,
    and the implementation may do so for a subset only (the actively-syncing
    datatypes). Note that proxy types never become active from the engine's
    perspective, so they never get refreshed either.
    
    Bug: 1232372
    Change-Id: I136e9f2c649b29dc33aeb0310fac2a287d4008b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055357
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906102}

--- components/sync/driver/sync_service_impl.cc
+++ components/sync/driver/sync_service_impl.cc
@@ -301,7 +301,7 @@ void SyncServiceImpl::StartSyncingWithServer() {
   if (engine_)
     engine_->StartSyncingWithServer();
   if (IsLocalSyncEnabled()) {
-    TriggerRefresh(Intersection(GetActiveDataTypes(), ProtocolTypes()));
+    TriggerRefresh(ModelTypeSet::All());
   }
 }
 
