28f698479b4255a80f8dadc3530aef9015136e46
Nicolás Peña Moreno <npm@chromium.org>
2021-07-28 14:31:13 +0000

    [FCP] Always use IgnorePaintTimingScope::IgnoreDepth in PaintTiming
    Currently we don't always gate FCP on the opacity depth, which means we
    report FCP when nothing is being painted. This CL adds checks in
    PaintTiming to ensure that the metric is not reported too early. Due to
    content sometimes not going through Blink paint after changing opacity,
    this may cause some page loads to stop reporting FCP.
    
    Bug: 1233212
    Change-Id: I4939d61571e488cc3e3e392d1df9d02c206b3f33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057290
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906181}

--- third_party/blink/renderer/core/paint/paint_timing.cc
+++ third_party/blink/renderer/core/paint/paint_timing.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/core/timing/dom_window_performance.h"
 #include "third_party/blink/renderer/core/timing/window_performance.h"
+#include "third_party/blink/renderer/platform/graphics/paint/ignore_paint_timing_scope.h"
 #include "third_party/blink/renderer/platform/instrumentation/resource_coordinator/document_resource_coordinator.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher.h"
@@ -105,6 +106,7 @@ void PaintTiming::MarkFirstPaint() {
   // markFirstPaint().
   if (!first_paint_.is_null())
     return;
+  DCHECK_EQ(IgnorePaintTimingScope::IgnoreDepth(), 0);
   SetFirstPaint(clock_->NowTicks());
 }
 
@@ -115,12 +117,15 @@ void PaintTiming::MarkFirstContentfulPaint() {
   // markFirstContentfulPaint().
   if (!first_contentful_paint_.is_null())
     return;
+  if (IgnorePaintTimingScope::IgnoreDepth() > 0)
+    return;
   SetFirstContentfulPaint(clock_->NowTicks());
 }
 
 void PaintTiming::MarkFirstImagePaint() {
   if (!first_image_paint_.is_null())
     return;
+  DCHECK_EQ(IgnorePaintTimingScope::IgnoreDepth(), 0);
   first_image_paint_ = clock_->NowTicks();
   SetFirstContentfulPaint(first_image_paint_);
   RegisterNotifyPresentationTime(PaintEvent::kFirstImagePaint);
@@ -177,6 +182,8 @@ void PaintTiming::SetFirstMeaningfulPaint(
 void PaintTiming::NotifyPaint(bool is_first_paint,
                               bool text_painted,
                               bool image_painted) {
+  if (IgnorePaintTimingScope::IgnoreDepth() > 0)
+    return;
   if (is_first_paint)
     MarkFirstPaint();
   if (text_painted)
@@ -224,6 +231,7 @@ void PaintTiming::SetFirstPaint(base::TimeTicks stamp) {
   if (!first_paint_.is_null())
     return;
 
+  DCHECK_EQ(IgnorePaintTimingScope::IgnoreDepth(), 0);
   LocalFrame* frame = GetFrame();
   if (frame && frame->GetDocument()) {
     Document* document = frame->GetDocument();
@@ -239,6 +247,7 @@ void PaintTiming::SetFirstPaint(base::TimeTicks stamp) {
 void PaintTiming::SetFirstContentfulPaint(base::TimeTicks stamp) {
   if (!first_contentful_paint_.is_null())
     return;
+  DCHECK_EQ(IgnorePaintTimingScope::IgnoreDepth(), 0);
   SetFirstPaint(stamp);
   first_contentful_paint_ = stamp;
   RegisterNotifyPresentationTime(PaintEvent::kFirstContentfulPaint);
