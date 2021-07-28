1532f18cccccc5f7ae612a2058890412b75bb261
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 22:47:29 +0000

    [automated] Drop needless <vector> includes in /components/mirroring
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=mfoltz@chromium.org
    
    Bug: 1233968
    Change-Id: Ic7c94c50b040be3e3e0c4612466fd09c7b2d07ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059480
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906426}

--- components/mirroring/service/receiver_setup_querier.cc
+++ components/mirroring/service/receiver_setup_querier.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/mirroring/service/receiver_setup_querier.h"
 
 #include <string>
-#include <vector>
 
 #include "base/bind.h"
 #include "base/json/json_reader.h"
