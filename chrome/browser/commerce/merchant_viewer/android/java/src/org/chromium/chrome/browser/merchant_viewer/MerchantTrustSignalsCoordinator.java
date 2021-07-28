8eb79a664d2a17c99a7bcfc9c0637e58948f32e2
Zhiyuan Cai <zhiyuancai@chromium.org>
2021-07-28 21:54:26 +0000

    [MerchantTrust] Use SiteEngagement
    We leverage SiteEngagement to skip triggering trust signals when user is
    already familiar with this merchant.
    
    Bug: 1226206
    Change-Id: I1af66c3e1c708f079ecf99493aa132384d4de39b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3024333
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906395}

--- chrome/browser/commerce/merchant_viewer/android/java/src/org/chromium/chrome/browser/merchant_viewer/MerchantTrustSignalsCoordinator.java
+++ chrome/browser/commerce/merchant_viewer/android/java/src/org/chromium/chrome/browser/merchant_viewer/MerchantTrustSignalsCoordinator.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.merchant_viewer;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 
 import androidx.annotation.VisibleForTesting;
@@ -20,6 +21,7 @@ import org.chromium.chrome.tab_ui.R;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.messages.DismissReason;
 import org.chromium.components.messages.MessageDispatcher;
+import org.chromium.components.site_engagement.SiteEngagementService;
 import org.chromium.content_public.browser.NavigationHandle;
 import org.chromium.ui.base.IntentRequestTracker;
 import org.chromium.ui.base.WindowAndroid;
@@ -38,6 +40,7 @@ public class MerchantTrustSignalsCoordinator {
     private final MerchantTrustMetrics mMetrics;
     private final MerchantTrustSignalsDataProvider mDataProvider;
     private final MerchantTrustSignalsStorageFactory mStorageFactory;
+    private final ObservableSupplier<Profile> mProfileSupplier;
 
     /** Creates a new instance. */
     public MerchantTrustSignalsCoordinator(Context context, WindowAndroid windowAndroid,
@@ -46,7 +49,7 @@ public class MerchantTrustSignalsCoordinator {
             ObservableSupplier<Profile> profileSupplier, MerchantTrustMetrics metrics,
             IntentRequestTracker intentRequestTracker) {
         this(context, new MerchantTrustMessageScheduler(messageDispatcher, metrics), tabSupplier,
-                new MerchantTrustSignalsDataProvider(), metrics,
+                new MerchantTrustSignalsDataProvider(), profileSupplier, metrics,
                 new MerchantTrustBottomSheetCoordinator(context, windowAndroid,
                         bottomSheetController, tabSupplier, layoutView, metrics,
                         intentRequestTracker),
@@ -56,12 +59,14 @@ public class MerchantTrustSignalsCoordinator {
     @VisibleForTesting
     MerchantTrustSignalsCoordinator(Context context, MerchantTrustMessageScheduler messageScheduler,
             ObservableSupplier<Tab> tabSupplier, MerchantTrustSignalsDataProvider dataProvider,
-            MerchantTrustMetrics metrics, MerchantTrustBottomSheetCoordinator detailsTabCoordinator,
+            ObservableSupplier<Profile> profileSupplier, MerchantTrustMetrics metrics,
+            MerchantTrustBottomSheetCoordinator detailsTabCoordinator,
             MerchantTrustSignalsStorageFactory storageFactory) {
         mContext = context;
         mDataProvider = dataProvider;
         mMetrics = metrics;
         mStorageFactory = storageFactory;
+        mProfileSupplier = profileSupplier;
 
         mMediator = new MerchantTrustSignalsMediator(tabSupplier, this::maybeDisplayMessage);
         mMessageScheduler = messageScheduler;
@@ -98,6 +103,23 @@ public class MerchantTrustSignalsCoordinator {
         }
     }
 
+    private boolean isFamiliarMerchant(String url) {
+        if (!MerchantViewerConfig.doesTrustSignalsUseSiteEngagement() || TextUtils.isEmpty(url)) {
+            return false;
+        }
+        Profile profile = mProfileSupplier.get();
+        if (profile == null || profile.isOffTheRecord()) {
+            return false;
+        }
+        return getSiteEngagementScore(profile, url)
+                > MerchantViewerConfig.getTrustSignalsSiteEngagementThreshold();
+    }
+
+    @VisibleForTesting
+    double getSiteEngagementScore(Profile profile, String url) {
+        return SiteEngagementService.getForBrowserContext(profile).getScore(url);
+    }
+
     @VisibleForTesting
     void onMessageEnqueued(MerchantTrustMessageContext messageContext) {
         if (messageContext == null) {
@@ -116,7 +138,8 @@ public class MerchantTrustSignalsCoordinator {
     private void getDataForUnfamiliarMerchant(
             NavigationHandle navigationHandle, Callback<MerchantTrustSignals> callback) {
         MerchantTrustSignalsEventStorage storage = mStorageFactory.getForLastUsedProfile();
-        if (storage == null || navigationHandle == null || navigationHandle.getUrl() == null) {
+        if (storage == null || navigationHandle == null || navigationHandle.getUrl() == null
+                || isFamiliarMerchant(navigationHandle.getUrl().getSpec())) {
             return;
         }
 
