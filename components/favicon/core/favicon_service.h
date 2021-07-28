ae7927d26994661bdc95dd083c66c6b4c90df891
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 21:59:47 +0000

    [automated] Drop needless <vector> includes in /components/favicon
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=pkotwicz@chromium.org
    
    Bug: 1233968
    Change-Id: Ifa926ab9b290ae08afe312ab7031859537b50d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059027
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906401}

--- components/favicon/core/favicon_service.h
+++ components/favicon/core/favicon_service.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_FAVICON_CORE_FAVICON_SERVICE_H_
 #define COMPONENTS_FAVICON_CORE_FAVICON_SERVICE_H_
 
-#include <vector>
 
 #include "base/callback.h"
 #include "base/task/cancelable_task_tracker.h"
