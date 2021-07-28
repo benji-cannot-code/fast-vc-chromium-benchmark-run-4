907eb8e11b142a34f9636d6ea84d3187b02bb511
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 18:43:29 +0000

    [automated] Drop needless <vector> includes in /components/constrained_window
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=wittman@chromium.org
    
    Bug: 1233968
    Change-Id: I5658778d2e7e75459419221cad98fa10e1f9ab60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059023
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906293}

--- components/constrained_window/constrained_window_views_unittest.cc
+++ components/constrained_window/constrained_window_views_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/constrained_window/constrained_window_views.h"
 
 #include <memory>
-#include <vector>
 
 #include "base/macros.h"
 #include "components/constrained_window/constrained_window_views_client.h"
