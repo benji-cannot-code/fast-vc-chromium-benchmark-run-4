e0cea6d8d11b918d7f1c0d42b22c3fa68d7b7309
Austin Sullivan <asully@chromium.org>
2021-07-28 18:15:49 +0000

    Remove CHECK to fix crash in TestRunnerBindings::CopyImageThen
    The CHECK was added in https://crrev.com/c/3011783 when switching
    clipboard sequence numbers to be a TokenType.
    
    Bug: 872076, 1232810
    Change-Id: I2589af2b4c3419018293f9b5e2aaf5cecfdca499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055687
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906277}

--- content/web_test/renderer/test_runner.cc
+++ content/web_test/renderer/test_runner.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/cors.mojom.h"
 #include "third_party/blink/public/common/page/page_zoom.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
+#include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
 #include "third_party/blink/public/mojom/app_banner/app_banner.mojom.h"
 #include "third_party/blink/public/mojom/clipboard/clipboard.mojom.h"
@@ -1930,8 +1931,10 @@ void TestRunnerBindings::CopyImageThen(int x,
   GetWebFrame()->CopyImageAtForTesting(gfx::Point(x, y));
   auto sequence_number_after = sequence_number_before;
   while (sequence_number_before.value() == sequence_number_after.value()) {
-    CHECK(remote_clipboard->GetSequenceNumber(ui::ClipboardBuffer::kCopyPaste,
-                                              &sequence_number_after));
+    // TODO(crbug.com/872076): Ideally we would CHECK here that the mojo call
+    // succeeded, but this crashes under some circumstances (crbug.com/1232810).
+    remote_clipboard->GetSequenceNumber(ui::ClipboardBuffer::kCopyPaste,
+                                        &sequence_number_after);
   }
 
   SkBitmap bitmap;
