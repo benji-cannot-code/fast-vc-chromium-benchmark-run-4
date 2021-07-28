b0f767e0c7d384e0d0eb102afda73d04fbb5a79c
Mihai Sardarescu <msarda@chromium.org>
2021-07-28 08:04:59 +0000

    CHECK instead of DCHECK when fetching access tokens
    The list of scopes for which it is allowed to fetch access tokens when
    the user is signed in and syncing is now updated. As fetching access
    tokens for an account without being in the right consent state has
    a privacy risk attached, this CL enforces checking the consent level
    in production code as well
    
    Bug: 1172944
    
    Change-Id: I669a0b70e274c839eb66ae86c423c56387e3c998
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056871
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906088}

--- components/signin/public/identity_manager/access_token_fetcher_unittest.cc
+++ components/signin/public/identity_manager/access_token_fetcher_unittest.cc
@@ -768,8 +768,6 @@ TEST_F(AccessTokenFetcherTest,
   }
 }
 
-// Client access tests that require DCHECK_IS_ON().
-#if DCHECK_IS_ON()
 // Tests that a request with an unconsented client accessing an OAuth2 API
 // that requires privileged access fails.
 TEST_F(AccessTokenFetcherTest,
@@ -787,6 +785,5 @@ TEST_F(AccessTokenFetcherTest,
   EXPECT_CHECK_DEATH(VerifyScopeAccess(account_id, "test_consumer",
                                        {GaiaConstants::kAnyApiOAuth2Scope}));
 }
-#endif  // DCHECK_IS_ON()
 
 }  // namespace signin
