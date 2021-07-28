10b9686a03f198f65ff2c13be5c3c22a77fe7786
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 19:00:59 +0000

    [automated] Drop needless <vector> includes in /components/cdm
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=jrummell@chromium.org
    
    Bug: 1233968
    Change-Id: Ieab91562a0bf4920e0c8fed3a9c1c7c605bcc977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059022
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906311}

--- components/cdm/renderer/widevine_key_system_properties.h
+++ components/cdm/renderer/widevine_key_system_properties.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_CDM_RENDERER_WIDEVINE_KEY_SYSTEM_PROPERTIES_H_
 
 #include <string>
-#include <vector>
 
 #include "base/containers/flat_set.h"
 #include "media/base/key_system_properties.h"
