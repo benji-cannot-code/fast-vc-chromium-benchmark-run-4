634119021b96a66895fec2d0b4ed4083faa463d2
Kevin McNee <mcnee@chromium.org>
2021-07-28 16:39:04 +0000

    Remove unneeded include of content_param_traits_macros.h from synthetic_pointer_action_params.h
    Traits related to SyntheticPointerActionParams have already been
    converted to mojo, so we remove this include. We also fix IWYU issues
    that this removal revealed.
    
    Bug: 1227131
    Change-Id: Id3e46df27cb62819c3ae99ef95fc12c9afcef78e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057753
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906226}

--- content/common/input/synthetic_pointer_action_params.h
+++ content/common/input/synthetic_pointer_action_params.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "content/common/content_export.h"
-#include "content/common/content_param_traits_macros.h"
 #include "content/common/input/synthetic_gesture_params.h"
 #include "third_party/blink/public/common/input/web_mouse_event.h"
 #include "third_party/blink/public/common/input/web_touch_event.h"
