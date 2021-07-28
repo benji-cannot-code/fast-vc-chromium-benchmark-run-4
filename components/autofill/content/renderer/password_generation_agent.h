e4a51ba50d303b5d4c07a7be2fead20147c15f67
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-28 18:45:30 +0000

    [automated] Drop needless <vector> includes in /components/autofill/content/renderer
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=vasilii@chromium.org
    
    Bug: 1233968
    Change-Id: I1ef29f2be06ea4c536404d7829bbbae4e2a5eed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059021
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906296}

--- components/autofill/content/renderer/password_generation_agent.h
+++ components/autofill/content/renderer/password_generation_agent.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <utility>
-#include <vector>
 
 #include "base/macros.h"
 #include "components/autofill/content/common/mojom/autofill_agent.mojom.h"
