b9b22cc334c81f38d5e073cd8b079231c7142e77
Lily Chen <chlily@chromium.org>
2021-07-28 00:41:08 +0000

    Decouple legacy SameSite cookie policy, content settings from Feature
    In preparation for the removal of the base::Features:
    
      net::features::kSameSiteByDefaultCookies
      net::features::kCookiesWithoutSameSiteMustBeSecure
    
    (since these features have launched and are now enabled by default),
    this change decouples the legacy SameSite cookie content settings and
    enterprise policy from the base::Features that are about to be removed.
    
    Previously, the content setting's initial default was tied to the
    kSameSiteByDefaultCookies feature state. After this change, it is set to
    block legacy access by default.
    
    This causes the CookieAccessSemantics to always default to NONLEGACY
    unless there is an enterprise policy.
    
    At a high level, effectively the only behavior change is that running
    Chrome with the flag --disable-features=SameSiteByDefaultCookies will no
    longer force legacy access semantics (and trying to use this flag to
    disable the modern SameSite behavior will essentially stop
    working), but this base::Feature will be removed imminently
    anyway.
    
    Bug: 1211388
    Change-Id: I0a79663c603b891d8c92669cc604d9a77c532f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3046782
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#905973}

--- components/content_settings/core/browser/content_settings_registry.cc
+++ components/content_settings/core/browser/content_settings_registry.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/content_settings/core/browser/website_settings_registry.h"
 #include "components/content_settings/core/common/content_settings.h"
-#include "net/cookies/cookie_util.h"
 
 #if defined(OS_ANDROID)
 #include "media/base/android/media_drm_bridge.h"
@@ -346,13 +345,9 @@ void ContentSettingsRegistry::Init() {
            ContentSettingsInfo::PERSISTENT,
            ContentSettingsInfo::EXCEPTIONS_ON_SECURE_AND_INSECURE_ORIGINS);
 
-  ContentSetting legacy_cookie_access_initial_default =
-      net::cookie_util::IsSameSiteByDefaultCookiesEnabled()
-          ? CONTENT_SETTING_BLOCK
-          : CONTENT_SETTING_ALLOW;
   Register(ContentSettingsType::LEGACY_COOKIE_ACCESS, "legacy-cookie-access",
-           legacy_cookie_access_initial_default,
-           WebsiteSettingsInfo::UNSYNCABLE, AllowlistedSchemes(),
+           CONTENT_SETTING_BLOCK, WebsiteSettingsInfo::UNSYNCABLE,
+           AllowlistedSchemes(),
            ValidSettings(CONTENT_SETTING_ALLOW, CONTENT_SETTING_BLOCK),
            WebsiteSettingsInfo::SINGLE_ORIGIN_ONLY_SCOPE,
            WebsiteSettingsRegistry::ALL_PLATFORMS,
