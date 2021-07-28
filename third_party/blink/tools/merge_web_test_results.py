370b04c9adb2bec347c4f5d94bd389498710799c
Weizhong Xia <weizhong@google.com>
2021-07-28 19:08:17 +0000

    Revert "Run blink merge_results.py under py3"
    This reverts commit 4aeda2cb7796229d85fd52d9bc800567660b5a06.
    
    Reason for revert: This breaks some mac*-blink-rel bots
    
    Original change's description:
    > Run blink merge_results.py under py3
    >
    > By running merge_result.py under py3, we saved 44 seconds for Windows,
    > 12 seconds for Linux, 8 seconds for Mac.
    >
    > Will revert this change once all merge script are running under py3.
    >
    > Bug: 1056628
    > Change-Id: I1b5c1f1d0524c46b14911636e04857dca53facf4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3050540
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > Cr-Commit-Position: refs/heads/master@{#905395}
    
    Bug: 1056628
    Change-Id: I82c7c8f3613dcc2380f7c5154b060e4676deccb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058853
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/master@{#906313}

--- third_party/blink/tools/merge_web_test_results.py
+++ third_party/blink/tools/merge_web_test_results.py
@@ -5,22 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
 import sys
 
-from blinkpy.common.host import Host
+from blinkpy.web_tests.merge_results import main
 
-BLINK_TOOLS_PATH = os.path.abspath(os.path.dirname(__file__))
-
-def main():
-    path_to_merge_script = os.path.join(BLINK_TOOLS_PATH,
-                                        'blinkpy',
-                                        'web_tests',
-                                        'merge_results.py')
-    command = ['python3', path_to_merge_script] + sys.argv[1:]
-    host = Host()
-    output = host.executive.run_command(command)
-    print("Output of merge_results.py:\n%s\n" % output)
-
-if __name__ == '__main__':
-    main()
+main(sys.argv[1:])
