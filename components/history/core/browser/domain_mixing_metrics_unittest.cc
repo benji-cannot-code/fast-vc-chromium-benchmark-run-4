e41a1d2a79f46408af673112b907f6f3bc99906d
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 03:02:28 +0000

    [automated] Drop needless <vector> includes in /components/history
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1233968
    Change-Id: Ifb64081ea31d3181baa0de2d9feaf4d743023fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058249
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906542}

--- components/history/core/browser/domain_mixing_metrics_unittest.cc
+++ components/history/core/browser/domain_mixing_metrics_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history/core/browser/domain_mixing_metrics.h"
 
 #include <memory>
-#include <vector>
 
 #include "base/bind.h"
 #include "base/test/metrics/histogram_tester.h"
