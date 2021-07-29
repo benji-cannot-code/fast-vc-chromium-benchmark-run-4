2f30f3fb137e86b9201a4e6513576a378e62f93d
Mark Cogan <marq@chromium.org>
2021-07-29 14:19:45 +0000

    Revert "[iOS] Move webstate closure into BrowserViewWrangler"
    This reverts commit d28ca46fabb52b86d76edee712b948278afc968a.
    
    Reason for revert: crbug.com/1233998 -- tabs being lost.
    
    Original change's description:
    > [iOS] Move webstate closure into BrowserViewWrangler
    >
    > Closing all of the webstates in a web state list is part of shutting
    > down the browser, which is handled by the BrowserViewWrangler. This CL
    > moves this logic out of TabModel.
    >
    > Bug: 783777
    > Change-Id: I545403dbc5713e4bbe48387f86354405e4ff2b8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054051
    > Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    > Commit-Queue: Mark Cogan <marq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#905837}
    
    Bug: 783777
    Change-Id: If587a365d1b5fc4c21a33ef83181d2e8364f7af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060380
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906698}

--- ios/chrome/browser/tabs/tab_model_unittest.mm
+++ ios/chrome/browser/tabs/tab_model_unittest.mm
@@ -69,10 +69,6 @@ class TabModelTest : public PlatformTest {
   ~TabModelTest() override = default;
 
   void TearDown() override {
-    @autoreleasepool {
-      browser_->GetWebStateList()->CloseAllWebStates(
-          WebStateList::CLOSE_NO_FLAGS);
-    }
     SetTabModel(nil);
     PlatformTest::TearDown();
   }
