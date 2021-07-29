3d329ef75d4884eba279bff071cf3a060016ae12
Tomasz Wiszkowski <ender@google.com>
2021-07-29 23:10:18 +0000

    Re-enable SearchActivityTests which should not flake again.
    We have made some improvements to Autocomplete over the last few
    months that may help us re-enable the SearchActivityTests that once
    were flaking.
    
    The changes include how we handle cached suggestions and how we
    determine whether the Autocomplete subsystem is ready to operate.
    
    If these flakes persist, new logs may help us understand better where
    else we may need to improve the Autocomplete subsystem
    
    Bug: 1166647, 1171794
    Change-Id: Iadac5013d2be50041ff51972d3dbce657fd36802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061068
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906896}

--- chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
@@ -299,7 +299,6 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "crbug/1171794")
     public void testTypeBeforeNativeIsLoaded() throws Exception {
         // Wait for the activity to load, but don't let it load the native library.
         mTestDelegate.shouldDelayLoadingNative = true;
@@ -531,7 +530,6 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "crbug/1166647")
     public void testNewIntentDiscardsQuery() {
         final SearchActivity searchActivity = startSearchActivity();
         setUrlBarText(searchActivity, "first query");
