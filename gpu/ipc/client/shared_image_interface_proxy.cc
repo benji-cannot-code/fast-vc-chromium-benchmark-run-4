ef3992b7284c9ce4b699c044afff65b2653531bb
Sergey Ulanov <sergeyu@chromium.org>
2021-07-29 21:46:08 +0000

    [Fuchsia] Fix crash when playing video
    WebEngine was crashing when FuchsiaVideoDecoder tries to create shared
    images from GMB. This happens because FuchsiaVideoDecoder doesn't supply
    GpuMemoryBufferManager, but OTOH synchronization is not required in that
    case. Update SharedImageInterfaceProxy to handle this case properly.
    
    Bug: 1234184
    Change-Id: I2a7803e45a5723c0100c735f3705218ba3a36e70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059806
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906860}

--- gpu/ipc/client/shared_image_interface_proxy.cc
+++ gpu/ipc/client/shared_image_interface_proxy.cc
@@ -192,7 +192,11 @@ Mailbox SharedImageInterfaceProxy::CreateSharedImage(
 
   bool requires_sync_token =
 #if defined(OS_FUCHSIA)
-      params->buffer_handle.type == gfx::NATIVE_PIXMAP ||
+      // Synchronization is not required if the image is being created by
+      // FuchsiaVideoDecoder. |gpu_memory_buffer_manager| is nullptr in that
+      // case.
+      (gpu_memory_buffer_manager &&
+       params->buffer_handle.type == gfx::NATIVE_PIXMAP) ||
 #endif
       params->buffer_handle.type == gfx::IO_SURFACE_BUFFER;
   {
@@ -207,8 +211,9 @@ Mailbox SharedImageInterfaceProxy::CreateSharedImage(
     host_->EnsureFlush(last_flush_id_);
   }
   if (requires_sync_token) {
-    gpu::SyncToken sync_token = GenVerifiedSyncToken();
+    DCHECK(gpu_memory_buffer_manager);
 
+    gpu::SyncToken sync_token = GenVerifiedSyncToken();
     gpu_memory_buffer_manager->SetDestructionSyncToken(gpu_memory_buffer,
                                                        sync_token);
   }
