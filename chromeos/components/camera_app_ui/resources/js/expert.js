ac3dc33b347ff1f376a852c1737b8a8be925710b
Kuo Jen Wei <inker@google.com>
2021-07-29 13:25:07 +0000

    CCA: Show expert mode setting options not required chromeOS VCD
    Use |.require-chromeos-vcd| to label if per expert mode option require
    chromeOS VCD support and show/hide option accordingly.  Skip showing
    corner indicator in document mode when using fake VCD.
    
    Bug: b/193196284
    Test: tast run <DUT> "camera.CCAUI*"
    Change-Id: I1bda218b82dafd7258212e6dea64542042e08832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058608
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906686}

--- chromeos/components/camera_app_ui/resources/js/expert.js
+++ chromeos/components/camera_app_ui/resources/js/expert.js
@@ -3,31 +3,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {I18nString} from './i18n_string.js';
 import * as localStorage from './models/local_storage.js';
-import {DeviceOperator} from './mojo/device_operator.js';
 import * as state from './state.js';
-import * as toast from './toast.js';
 
 /**
  * Enables or disables expert mode.
  * @param {boolean} enable Whether to enable or disable expert mode
- * @return {!Promise}
  */
-export async function setExpertMode(enable) {
-  if (!await DeviceOperator.isSupported()) {
-    toast.show(I18nString.ERROR_MSG_EXPERT_MODE_NOT_SUPPORTED);
-    return;
-  }
+export function setExpertMode(enable) {
   state.set(state.State.EXPERT, enable);
   localStorage.set('expert', enable);
 }
 
 /**
  * Toggles expert mode.
- * @return {!Promise}
  */
-export async function toggleExpertMode() {
+export function toggleExpertMode() {
   const newState = !state.get(state.State.EXPERT);
-  await setExpertMode(newState);
+  setExpertMode(newState);
 }
