bc019452de9d98f10950e5c986dfaef5bd6ae16b
Mikel Astiz <mastiz@chromium.org>
2021-07-29 08:59:09 +0000

    [bookmarks] Ban a specific GUID known to be problematic due to sync
    Crashes have been observed (see linked bug) due to a combination of rare
    bugs, which can lead to a bookmark getting a GUID assigned for a value
    that is known to be wrong.
    
    The problematic GUID is "da39a3ee-5e6b-fb0d-b255-bfef95601890", which is
    the result of exercising some sync code that deals with backward
    compatibility, with the wrong input (empty strings). In certain rare
    circumstances, this can end up stored in BookmarkModel. This GUID is now
    represented with using the newly-introduced constant
    BookmarkNode::kBannedGuidDueToPastSyncBug.
    
    This patch treats this GUID as banned, with two consequences:
    1. Remote sync updates that include this banned value get ignored.
    2. If, upon startup, a bookmark node contains this GUID, it gets
       assigned another random GUID. This also means, for sync-ing users,
       that the initial merge procedure will kick in (and upload the
       bookmark with the new GUID).
    
    The precise scenarios that led to this bug are not very clear. The
    working theory is that, while all bookmarks created with a client tag
    must have had a GUID in specifics upon creation, old client (pre-81) may
    later modify such bookmark and clear the GUID field. In this scenario,
    the incoming update (for a modern client) has no GUID in specifics and in
    fact the GUID cannot be inferred, but some versions of the client
    (pre-93) may infer a constant (now banned) GUID.
    
    Bug: 1231450
    Change-Id: I7e3ab4e168e79bc078893af7b5e2741d8f7f4674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043559
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906630}

--- components/sync_bookmarks/bookmark_specifics_conversions.cc
+++ components/sync_bookmarks/bookmark_specifics_conversions.cc
@@ -55,8 +55,9 @@ enum class InvalidBookmarkSpecificsError {
   kInvalidGUID = 5,
   kInvalidParentGUID = 6,
   kInvalidUniquePosition = 7,
+  kBannedGUID = 8,
 
-  kMaxValue = kInvalidUniquePosition,
+  kMaxValue = kBannedGUID,
 };
 
 void LogInvalidSpecifics(InvalidBookmarkSpecificsError error) {
@@ -411,9 +412,15 @@ bool IsValidBookmarkSpecifics(const sync_pb::BookmarkSpecifics& specifics) {
     is_valid = false;
   }
   const base::GUID guid = base::GUID::ParseLowercase(specifics.guid());
+
   if (!guid.is_valid()) {
-    DLOG(ERROR) << "Invalid bookmark: invalid GUID in the specifics.";
+    DLOG(ERROR) << "Invalid bookmark: invalid GUID in specifics.";
     LogInvalidSpecifics(InvalidBookmarkSpecificsError::kInvalidGUID);
+    is_valid = false;
+  } else if (guid.AsLowercaseString() ==
+             bookmarks::BookmarkNode::kBannedGuidDueToPastSyncBug) {
+    DLOG(ERROR) << "Invalid bookmark: banned GUID in specifics.";
+    LogInvalidSpecifics(InvalidBookmarkSpecificsError::kBannedGUID);
     is_valid = false;
   }
   if (specifics.has_parent_guid()) {
