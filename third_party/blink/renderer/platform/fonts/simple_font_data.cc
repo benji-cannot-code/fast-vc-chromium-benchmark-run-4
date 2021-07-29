dacba056fdb1c3be56b77be14466263787ab06e0
Kent Tamura <tkent@chromium.org>
2021-07-29 12:08:34 +0000

    Text controls: Intrinsic widths should not be smaller than SkFontMetrics::fAvgCharWidth * N
    LayoutTextControl::GetAvgCharWidth(), which is the source of intrinsic
    widths of INPUT/TEXTAREA, should not return a value smaller than
    SkFontMetrics::fAvgCharWidth.  It made intrinsic widths in which N
    characters didn't fit.
    
    * SimpleFontData::PlatformInit() should not round fAvgCharWidth.
    
    * TextFieldIntrinsicInlineSize() is also updated so that it does not
      return fractional numbers.
    
    This CL is for non-mac platforms. crrev.com/905271 was for macOS.
    
    Bug: 1227341
    Change-Id: I0c27c7456836cef6e9f64ebf337c425841231d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058013
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906667}

--- third_party/blink/renderer/platform/fonts/simple_font_data.cc
+++ third_party/blink/renderer/platform/fonts/simple_font_data.cc
@@ -163,7 +163,7 @@ void SimpleFontData::PlatformInit(bool subpixel_ascent_descent,
 
 #if !defined(OS_MAC)
   if (metrics.fAvgCharWidth) {
-    avg_char_width_ = SkScalarRoundToInt(metrics.fAvgCharWidth);
+    avg_char_width_ = SkScalarToFloat(metrics.fAvgCharWidth);
   } else {
 #endif
     avg_char_width_ = x_height;
