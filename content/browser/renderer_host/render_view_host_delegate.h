029120e42cda2e1a5b83862e7f62e47f7e3aca22
Lingqi Chi <lingqi@chromium.org>
2021-07-29 01:53:54 +0000

    Prerender: Cancel prerendering if it wants to change WebContents-level preferred size
    RenderFrameHostImpl will cancel prerendering if it receives
    the ContentsPreferredSizeChanged Mojo message.
    We decide to do so because:
    - We predict that this behavior will not significantly impact coverage
    because renderers only send this message when running in
    `PreferredSizeChanged` mode, which is enabled by extensions.
    - Supporting this feature in prerendering pages would be a big project.
    
    This CL also impacts BackForwardCache, as we use
    `IsInactiveAndDisallowActivation` to check the activated state and
    cancel prerendering.
    
    Bug: 1231961
    Change-Id: I37d275dea5b6ad5b236a7a17c3a0076de6e3d82e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053256
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906519}

--- content/browser/renderer_host/render_view_host_delegate.h
+++ content/browser/renderer_host/render_view_host_delegate.h
@@ -24,10 +24,6 @@ struct WebPreferences;
 struct RendererPreferences;
 }  // namespace blink
 
-namespace gfx {
-class Size;
-}
-
 namespace content {
 
 class RenderViewHost;
@@ -85,9 +81,6 @@ class CONTENT_EXPORT RenderViewHostDelegate {
   // The page wants the hosting window to activate itself (it called the
   // JavaScript window.focus() method).
   virtual void Activate() {}
-
-  // The contents' preferred size changed.
-  virtual void UpdatePreferredSize(const gfx::Size& pref_size) {}
 
   // Returns a copy of the map of all session storage namespaces related
   // to this view.
