30b553336ae09cc8bcce971d1ac387425b7db884
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 01:06:20 +0000

    [automated] Drop needless <vector> includes in /components/services/storage/indexed_db
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=dmurph@chromium.org
    
    Bug: 1233968
    Change-Id: If83ce23600dbd2131034e08bc71181d80bc7b403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059600
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#906503}

--- components/services/storage/indexed_db/scopes/leveldb_scopes_coding.h
+++ components/services/storage/indexed_db/scopes/leveldb_scopes_coding.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <string>
 #include <tuple>
-#include <vector>
 
 #include "base/containers/span.h"
 #include "third_party/leveldatabase/src/include/leveldb/slice.h"
--- components/services/storage/indexed_db/scopes/scopes_lock_manager_unittest.cc
+++ components/services/storage/indexed_db/scopes/scopes_lock_manager_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/services/storage/indexed_db/scopes/scopes_lock_manager.h"
 
-#include <vector>
 
 #include "base/bind.h"
 #include "base/test/bind.h"
