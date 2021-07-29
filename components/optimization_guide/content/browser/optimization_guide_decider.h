8ad89f6a6b116cd37805d7e236a3431066170d9f
rajendrant <rajendrant@chromium.org>
2021-07-29 04:53:05 +0000

    Split OptGuide decider enums away from //content
    OptGuide Decision and DecisionCallback enums are used in hints manager
    and are not specific to //content. This CL moves them to
    components/core.
    
    Bug: 1232862
    Change-Id: I56f55a512bf4036ced131f7db2d803a15ab0b39f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059551
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906573}

--- components/optimization_guide/content/browser/optimization_guide_decider.h
+++ components/optimization_guide/content/browser/optimization_guide_decider.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_forward.h"
+#include "components/optimization_guide/core/optimization_guide_decision.h"
 #include "components/optimization_guide/core/optimization_metadata.h"
 #include "components/optimization_guide/proto/hints.pb.h"
 #include "components/optimization_guide/proto/models.pb.h"
@@ -20,30 +21,6 @@ class NavigationHandle;
 class GURL;
 
 namespace optimization_guide {
-
-// Represents the decision made by the optimization guide.
-// Keep in sync with OptimizationGuideOptimizationGuideDecision in enums.xml.
-// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.components.optimization_guide
-enum class OptimizationGuideDecision {
-  // The necessary information to make the decision is not yet available.
-  kUnknown,
-  // The necessary information to make the decision is available and the
-  // decision is affirmative.
-  kTrue,
-  // The necessary information to make the decision is available and the
-  // decision is negative.
-  kFalse,
-
-  // New values above this line.
-  kMaxValue = kFalse,
-};
-
-using OptimizationGuideTargetDecisionCallback =
-    base::OnceCallback<void(optimization_guide::OptimizationGuideDecision)>;
-
-using OptimizationGuideDecisionCallback =
-    base::OnceCallback<void(optimization_guide::OptimizationGuideDecision,
-                            const optimization_guide::OptimizationMetadata&)>;
 
 class OptimizationGuideDecider {
  public:
