0b3d0846d3ef9449fbbf28dd0a0575b92f2499c9
Anastasia Helfinstein <anastasi@google.com>
2021-07-29 18:05:49 +0000

    [Switch Access] Add TestUtility in preparation for SAATLite
    To prepare for the upcoming Switch Access Autoamte Test Language, this
    change adds a TestUtility class that performs some specific behaviors
    for testing purposes.
    
    This change also updates the FocusRingManagerTest to utilize those
    functions, as an example of how they can be helpful outside the
    SAATLite framework.
    
    AX-Relnotes: n/a.
    Bug: 1228525
    Change-Id: I32d612c99352834d59a3142e240ea8853a0a5b73
    Testing: All existing tests still pass. No new production code added.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057915
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/master@{#906766}

--- chrome/browser/resources/chromeos/accessibility/switch_access/switch_access_predicate.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/switch_access_predicate.js
@@ -170,9 +170,11 @@ export const SwitchAccessPredicate = {
    * @param {!SACache} cache
    * @return {boolean}
    */
-  isInteresting: (node, scope, cache) =>
-      SwitchAccessPredicate.isActionable(node, cache) ||
-      SwitchAccessPredicate.isGroup(node, scope, cache),
+  isInteresting: (node, scope, cache) => {
+    cache = cache || new SACache();
+    return SwitchAccessPredicate.isActionable(node, cache) ||
+        SwitchAccessPredicate.isGroup(node, scope, cache);
+  },
 
   /**
    * Returns true if the element is visible to the user for any reason.
@@ -197,6 +199,7 @@ export const SwitchAccessPredicate = {
    * @return {boolean}
    */
   isInterestingSubtree: (node, cache) => {
+    cache = cache || new SACache();
     if (cache.isInterestingSubtree.has(node)) {
       return cache.isInterestingSubtree.get(node);
     }
