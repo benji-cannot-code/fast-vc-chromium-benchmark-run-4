bd61920529d3627f2f15ea0ab69bc469b58c2b1f
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 10:54:56 +0000

    [automated] Drop needless <vector> includes in /components/arc/storage_manager
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=hashimoto@chromium.org
    
    Bug: 1233968
    Change-Id: I2f2099c0866d04032e8b4976f0bf7f0a406aa6c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056959
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#906648}

--- components/arc/storage_manager/arc_storage_manager.cc
+++ components/arc/storage_manager/arc_storage_manager.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/storage_manager/arc_storage_manager.h"
 
 #include <utility>
-#include <vector>
 
 #include "base/bind.h"
 #include "base/memory/singleton.h"
