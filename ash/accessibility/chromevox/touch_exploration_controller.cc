a65330501bd29bbd37ae9f4e77e9261c1912ca7b
David Tseng <dtseng@google.com>
2021-07-29 00:22:23 +0000

    Set ChromeVox to use a 45 degree max_swipe_deviation_angle
    By default, max_swipe_deviation_angle is set to 20. Therefore, (also
    verified experimentally), there is a gap of ~(90 - 20*2) = 50 degrees of
    swipe angle in which a swipe would never register as a swipe.
    
    In the future, it might be appropriate, given more user feedback, to
    bias towards left/right, or up/down single fingered swipes.
    
    R=dmazzoni@chromium.org
    
    Bug: none
    Test: manually swipe with one finger slightly rotating the hand counterclockwise in ChromeVox Learn Mode. Verify, given a stable straight motion, the four announcements "swipe one finger " [right|up|left|down] are spoken for every gesture. Without this change, the expected gaps are seen.
    Change-Id: I63326c13f81c015d883cf40c59b8ea092da898d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059525
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906479}

--- ash/accessibility/chromevox/touch_exploration_controller.cc
+++ ash/accessibility/chromevox/touch_exploration_controller.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event.h"
 #include "ui/events/event_processor.h"
 #include "ui/events/event_utils.h"
+#include "ui/events/gesture_detection/gesture_configuration.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/wm/core/coordinate_conversion.h"
 
@@ -53,6 +54,22 @@ void SetTouchAccessibilityFlag(ui::Event* event) {
   event->set_flags(event->flags() | ui::EF_TOUCH_ACCESSIBILITY);
 }
 
+std::unique_ptr<ui::GestureProviderAura> BuildGestureProviderAura(
+    TouchExplorationController* owner) {
+  // Tune some aspects of gesture detection for ChromeVox.
+  auto* config = ui::GestureConfiguration::GetInstance();
+  float original_max_swipe_deviation_angle =
+      config->max_swipe_deviation_angle();
+  config->set_max_swipe_deviation_angle(45);
+
+  auto gesture_provider =
+      std::make_unique<ui::GestureProviderAura>(owner, owner);
+
+  config->set_max_swipe_deviation_angle(original_max_swipe_deviation_angle);
+
+  return gesture_provider;
+}
+
 }  // namespace
 
 TouchExplorationController::TouchExplorationController(
@@ -63,7 +80,7 @@ TouchExplorationController::TouchExplorationController(
       delegate_(delegate),
       state_(NO_FINGERS_DOWN),
       anchor_point_state_(ANCHOR_POINT_NONE),
-      gesture_provider_(new ui::GestureProviderAura(this, this)),
+      gesture_provider_(BuildGestureProviderAura(this)),
       prev_state_(NO_FINGERS_DOWN),
       VLOG_on_(true),
       touch_accessibility_enabler_(touch_accessibility_enabler) {
@@ -1101,7 +1118,7 @@ void TouchExplorationController::SetState(State new_state,
       max_gesture_touch_points_ = 0;
       break;
     case NO_FINGERS_DOWN:
-      gesture_provider_ = std::make_unique<ui::GestureProviderAura>(this, this);
+      gesture_provider_ = BuildGestureProviderAura(this);
       if (sound_timer_.IsRunning())
         sound_timer_.Stop();
       tap_timer_.Stop();
