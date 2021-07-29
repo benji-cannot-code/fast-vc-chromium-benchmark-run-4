af9f372d16b5f4bcdb41f7abb58ebf44cb41f890
Kyle Horimoto <khorimoto@chromium.org>
2021-07-29 22:51:41 +0000

    [CrOS Bluetooth] Add mojom::BluetoothDeviceProperties
    This CL introduced Mojo definitions for the BluetoothDeviceProperties
    struct, which also includes some nested structs and enum values.
    Additionally, this CL adds a GenerateBluetoothDeviceMojoProperties()
    function which converts a device::BluetoothDevice pointer into a
    mojom::BluetoothDevicePropertiesPtr.
    
    These definitions and the conversion function are not used within this
    CL, but a follow-up CL uses them to provide information about paired
    devices as part of the ObserveSystemProperties() API function.
    
    Bug: 1010321
    Change-Id: I34e201cc47eee8b7018bafb09ff12b2c3d426aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060082
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906886}

---
+++ chromeos/services/bluetooth_config/device_conversion_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_BLUETOOTH_CONFIG_DEVICE_CONVERSION_UTIL_H_
+#define CHROMEOS_SERVICES_BLUETOOTH_CONFIG_DEVICE_CONVERSION_UTIL_H_
+
+#include "chromeos/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom.h"
+
+namespace device {
+class BluetoothDevice;
+}  // namespace device
+
+namespace chromeos {
+namespace bluetooth_config {
+
+mojom::BluetoothDevicePropertiesPtr GenerateBluetoothDeviceMojoProperties(
+    device::BluetoothDevice* device);
+
+}  // namespace bluetooth_config
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_BLUETOOTH_CONFIG_DEVICE_CONVERSION_UTIL_H_
