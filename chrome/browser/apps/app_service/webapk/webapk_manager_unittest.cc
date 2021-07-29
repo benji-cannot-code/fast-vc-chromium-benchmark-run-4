9b09dd8fae66b566b507b44c927f3dd67243e1cc
Tim Sergeant <tsergeant@chromium.org>
2021-07-29 08:02:21 +0000

    [CrOS WebAPKs] Enable by default
    This feature is now complete (modulo bug fixes) and is ready for testing
    on canary/dev.
    
    Bug: 1200177, 1198433, 1234279
    Change-Id: I9fa4c76e26fe4391e84001f7b88e54d7234b1794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058606
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906618}

--- chrome/browser/apps/app_service/webapk/webapk_manager_unittest.cc
+++ chrome/browser/apps/app_service/webapk/webapk_manager_unittest.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/constants/ash_features.h"
 #include "base/strings/strcat.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_chromeos.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/app_service_test.h"
@@ -57,6 +59,11 @@ class WebApkManagerTest : public testing::Test {
 
   void SetUp() override {
     testing::Test::SetUp();
+    // Disable the WebAPKs feature so that App Service does not start a
+    // WebApkManager which interferes with the test.
+    // TODO(crbug.com/1234279): Reuse the WebApkManager from App Service
+    // instead.
+    scoped_feature_list_.InitAndDisableFeature(ash::features::kWebApkGenerator);
 
     arc_test_.SetUp(&profile_);
 
@@ -95,6 +102,7 @@ class WebApkManagerTest : public testing::Test {
   }
 
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
   content::BrowserTaskEnvironment task_environment_;
   TestingProfile profile_;
   ArcAppTest arc_test_;
