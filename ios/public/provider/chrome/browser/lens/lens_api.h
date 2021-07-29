9d9583eb0c20a4fdc6951efe0f49ea0608ee1dc1
Ali Stanfield <stanfield@google.com>
2021-07-29 21:50:18 +0000

    [ios] Add Lens provider.
    Bug: 1234532
    Change-Id: I4265c3465c081a312b949f50eb5f0f913e3ddd00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058862
    Commit-Queue: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906862}

---
+++ ios/public/provider/chrome/browser/lens/lens_api.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LENS_LENS_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LENS_LENS_API_H_
+
+@class LensConfiguration;
+@class UIViewController;
+
+namespace ios {
+namespace provider {
+
+// Returns a Lens view controller.
+UIViewController* NewLensViewController(LensConfiguration* config);
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LENS_LENS_API_H_
---
+++ ios/public/provider/chrome/browser/lens/lens_configuration.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LENS_LENS_CONFIGURATION_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LENS_LENS_CONFIGURATION_H_
+
+#import <Foundation/Foundation.h>
+
+// Configuration object used by the LensProvider.
+@interface LensConfiguration : NSObject
+
+@end
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_LENS_LENS_CONFIGURATION_H_
