1dd4ccd60f232ab806c9a3a56aa9ec11a15ee220
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 05:55:00 +0000

    [automated] Drop needless <vector> includes in /components/security_interstitials
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=estark@chromium.org
    
    Bug: 1233968
    Change-Id: I45b9d6c79e2c8df000186eac6bda08798d74018b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059618
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906589}

--- components/security_interstitials/content/ssl_blocking_page.h
+++ components/security_interstitials/content/ssl_blocking_page.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SECURITY_INTERSTITIALS_CONTENT_SSL_BLOCKING_PAGE_H_
 
 #include <string>
-#include <vector>
 
 #include "base/macros.h"
 #include "base/task/cancelable_task_tracker.h"
