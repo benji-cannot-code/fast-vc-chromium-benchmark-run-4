4aba4682816de87853c2d2f6e4c1ca35f9977a8f
Alexander Bolodurin <alexbn@chromium.org>
2021-07-29 05:18:00 +0000

    [lacros shelf] Use ShelfControllerHelper logic to get app ID
    Revert back to using ShelfControllerHelper in BrowserAppsTracker to
    detect app ID. Using web_app::WebAppTabHelper and extensions::TabHelper
    was not always accurate (the latter is only used in a limited number of
    cases), and deviating from ShelfControllerHelper logic breaks a number
    of shelf tests when BrowserAppsTracker is used by ChromeShelfController.
    
    ShelfControllerHelper is updated to handle web app "uninstalling" flag
    to make it compatible with BrowserAppsTracker (tab's app ID should
    reflect app service install/uninstall events), and the shelf-specific
    helper function to get app ID from WebContents is made public so we
    don't have to create unnecessary ShelfControllerHelper objects.
    
    Later it should be replaced with a more generic implementation, and
    shelf-specific logic should move to ChromeShelfController, but for now
    use it to preserve existing behaviour.
    
    Bug: 1203992
    Change-Id: I8d6a52f23c16febed7185a97131f002a42149472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056147
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906581}

--- chrome/browser/ui/ash/shelf/browser_apps_tracker.cc
+++ chrome/browser/ui/ash/shelf/browser_apps_tracker.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/macros.h"
 #include "chrome/browser/extensions/tab_helper.h"
+#include "chrome/browser/ui/ash/shelf/shelf_controller_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -38,16 +39,10 @@ Browser* GetBrowserWithTabStripModel(TabStripModel* tab_strip_model) {
 }
 
 std::string GetAppId(content::WebContents* contents) {
-  if (auto* helper = web_app::WebAppTabHelper::FromWebContents(contents)) {
-    const web_app::AppId& app_id = helper->GetAppId();
-    if (!app_id.empty()) {
-      return app_id;
-    }
-  }
-  if (auto* helper = extensions::TabHelper::FromWebContents(contents)) {
-    return helper->GetExtensionAppId();
-  }
-  return "";
+  // TODO(crbug.com/1203992): shelf-specific logic doesn't really belong here,
+  // replace with more generic implementation to detect apps, and move
+  // shelf-specific bits to ChromeShelfController.
+  return GetShelfAppIdForWebContents(contents).value_or("");
 }
 
 bool IsBrowserVisible(Browser* browser) {
--- chrome/browser/ui/ash/shelf/shelf_controller_helper.h
+++ chrome/browser/ui/ash/shelf/shelf_controller_helper.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_types.h"
 #include "base/macros.h"
 #include "chrome/browser/ui/extensions/extension_enable_flow_delegate.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 class ArcAppListPrefs;
 class ExtensionEnableFlow;
@@ -20,6 +21,10 @@ class Profile;
 namespace content {
 class WebContents;
 }
+
+// A helper to get app ID in a tab for displaying in the shelf.
+absl::optional<std::string> GetShelfAppIdForWebContents(
+    content::WebContents* tab);
 
 // Assists ChromeShelfController with ExtensionService interaction.
 class ShelfControllerHelper : public ExtensionEnableFlowDelegate {
