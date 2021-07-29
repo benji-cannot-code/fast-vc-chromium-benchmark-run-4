7640e9510b8b1801ef3844cab6ffd1ac38626ad3
Vincent Boisselle <vincb@google.com>
2021-07-29 18:13:21 +0000

    Change font style for TOS string and metrics consent checkbox
    The egtest is also updated to support the "Read More" to handle the
    scrollable content overflow which happens for small devices (e.g., iPhone
    SE).
    
    Bug: 1232522
    Change-Id: I375ae049968f45dcf7b12e80ebc891f575483fb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059419
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906771}

--- ios/chrome/browser/ui/first_run/welcome/welcome_screen_view_controller.mm
+++ ios/chrome/browser/ui/first_run/welcome/welcome_screen_view_controller.mm
@@ -202,7 +202,7 @@ NSString* const kEnterpriseIconImageName = @"enterprise_icon";
   NSDictionary* textAttributes = @{
     NSForegroundColorAttributeName : [UIColor colorNamed:kTextSecondaryColor],
     NSFontAttributeName :
-        [UIFont preferredFontForTextStyle:UIFontTextStyleCaption1],
+        [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote],
     NSParagraphStyleAttributeName : paragraphStyle
   };
   NSDictionary* linkAttributes =
