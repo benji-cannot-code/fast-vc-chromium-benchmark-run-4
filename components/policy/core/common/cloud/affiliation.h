f6b4d807bc8fa216d86744baf6e5c76f6d1c219a
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 11:51:53 +0000

    [automated] Drop needless <vector> includes in /components/policy/core/common/cloud
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=pastarmovj@chromium.org
    
    Bug: 1233968
    Change-Id: I9cd650fca0ce8fcf4611f65cbcd91c7d699a3001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059443
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906664}

--- components/policy/core/common/cloud/affiliation.h
+++ components/policy/core/common/cloud/affiliation.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_POLICY_CORE_COMMON_CLOUD_AFFILIATION_H_
 
 #include <string>
-#include <vector>
 
 #include "base/containers/flat_set.h"
 #include "components/policy/policy_export.h"
