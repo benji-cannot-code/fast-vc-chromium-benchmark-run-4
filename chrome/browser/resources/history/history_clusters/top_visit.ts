67a0da336ea72fe19aa29a9c762aabe81b35457a
Moe Ahmadi <mahmadi@chromium.org>
2021-07-29 03:38:32 +0000

    [memories] Removes chrome://memories
    With crrev.com/c/3046546, <history-clusters> previously hosted under
    chrome://memories is now hosted under chrome://history/journeys. This
    change moves the necessary resources from //c/b/r/memories to
    //c/b/r/history/history_clusters and deletes the rest along with the
    code related to chrome://memories
    
    Fixed: 1229397
    Change-Id: I1fa6632e974a4a5108da923f1209518d4423798a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058392
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906553}

--- chrome/browser/resources/history/history_clusters/top_visit.ts
+++ chrome/browser/resources/history/history_clusters/top_visit.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './search_query.js';
 import './shared_style.js';
-import './visit_row.js';
+import './url_visit.js';
 import 'chrome://resources/cr_elements/cr_icons_css.m.js';
 import 'chrome://resources/polymer/v3_0/iron-collapse/iron-collapse.js';
 
