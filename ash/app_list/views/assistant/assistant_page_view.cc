1b33c1a7a8125368cb3b5397b72392a8aa2e7fe0
Yuki Awano <yawano@google.com>
2021-07-29 00:59:21 +0000

    Clean up dependencies of ax_assistant_structure.h
    - ax_assistant_structure.h depends on //skia through AXTreeUpdate.
      Replace include of ax_tree_update.h with forward declaration.
    - ax_export.h is removed from includes of ax_assistant_structure.h as
      it's not used.
    - Other files had dependencies included through ax_assistant_structure
      and ax_tree_update. Fixed those dependencies.
    
    Bug: 1176919
    Test: All build passes. No functional change.
    AX-Relnotes: n/a.
    Change-Id: I59965d47d9d33db47c53248f90444ab2fd7ecba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056217
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/master@{#906499}

--- ash/app_list/views/assistant/assistant_page_view.cc
+++ ash/app_list/views/assistant/assistant_page_view.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/animation_throughput_reporter.h"
--- ui/accessibility/ax_assistant_structure.h
+++ ui/accessibility/ax_assistant_structure.h
@@ -13,14 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/strings/string_split.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/accessibility/ax_enums.mojom-forward.h"
-#include "ui/accessibility/ax_export.h"
-#include "ui/accessibility/ax_tree_update.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/range/range.h"
 
 namespace ui {
+
+struct AXTreeUpdate;
 
 struct AssistantNode {
   AssistantNode();
