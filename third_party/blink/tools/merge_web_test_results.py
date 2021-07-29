7512cbea267399400deefb8e7d5450194245994b
Weizhong Xia <weizhong@google.com>
2021-07-29 03:59:54 +0000

    Reland Run blink merge_results.py under py3
    By running merge_result.py under py3, we saved 44 seconds for Windows,
    12 seconds for Linux, 8 seconds for Mac.
    
    Will revert this change once all merge script are running under py3.
    
    Bug: 1056628
    Change-Id: I0cb069e9a54ec4de6180ffc25cde2705831d3daf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058748
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/master@{#906563}

--- third_party/blink/tools/merge_web_test_results.py
+++ third_party/blink/tools/merge_web_test_results.py
@@ -5,8 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+import subprocess
 import sys
 
-from blinkpy.web_tests.merge_results import main
+BLINK_TOOLS_PATH = os.path.abspath(os.path.dirname(__file__))
 
-main(sys.argv[1:])
+def main():
+    path_to_merge_script = os.path.join(BLINK_TOOLS_PATH,
+                                        'blinkpy',
+                                        'web_tests',
+                                        'merge_results.py')
+    command = ['python3', path_to_merge_script] + sys.argv[1:]
+    subprocess.check_call(command)
+
+if __name__ == '__main__':
+    main()
