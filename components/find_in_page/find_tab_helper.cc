399ab7439a76838fef94fdf43c5b7168f432571b
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 01:03:00 +0000

    [automated] Drop needless <vector> includes in /components/find_in_page
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=msw@chromium.org
    
    Bug: 1233968
    Change-Id: I259eb733caa92cae71713cb588a5527b487991a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059479
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906502}

--- components/find_in_page/find_tab_helper.cc
+++ components/find_in_page/find_tab_helper.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/find_in_page/find_tab_helper.h"
 
 #include <utility>
-#include <vector>
 
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
--- components/find_in_page/find_tab_helper.h
+++ components/find_in_page/find_tab_helper.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/observer_list.h"
 #include "build/build_config.h"
 #include "components/find_in_page/find_notification_details.h"
 #include "content/public/browser/web_contents_user_data.h"
