926db5cc14ff167941d94cda12b603cc939d1a77
Rebekah Potter <rbpotter@chromium.org>
2021-07-29 19:54:18 +0000

    Reland "Print Preview: Update model element to class-based syntax"
    This reverts commit 30080bcb7071b93adc0204bb943cdc615f6c86d3.
    
    Reason for revert: CL was not related to failures, which were caused by the following CL (also reverted)
    https://chromium-review.googlesource.com/c/chromium/src/+/3057795
    
    Original change's description:
    > Revert "Print Preview: Update model element to class-based syntax"
    >
    > This reverts commit e23cf9079dc0430190e95bff1589c2f7efba0bf6.
    >
    > Reason for revert: Suspicious about test failures of PrintBrowserTest.PDFPluginNotKeyboardFocusable.
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.11%20Tests/64510/overview
    >
    > Original change's description:
    > > Print Preview: Update model element to class-based syntax
    > >
    > > Bug: 1206112
    > > Change-Id: Ibc2e15d7d752cb03061e9db36a6f9e91757f2421
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058311
    > > Reviewed-by: dpapad <dpapad@chromium.org>
    > > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#906364}
    >
    > Bug: 1206112
    > Change-Id: I275abdf892d38535148b3242fe704f243fc7c1ef
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058833
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    > Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#906512}
    
    Bug: 1206112
    Change-Id: I04494c3c818ea3fcb32e8d00a856bb5246325fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061059
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906814}

--- chrome/test/data/webui/print_preview/model_test.js
+++ chrome/test/data/webui/print_preview/model_test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {Cdd, Destination, DestinationConnectionStatus, DestinationOrigin, DestinationType, DuplexMode, MarginsType, PrinterType, ScalingType, Size} from 'chrome://print/print_preview.js';
+import {Cdd, Destination, DestinationConnectionStatus, DestinationOrigin, DestinationType, DuplexMode, MarginsType, PrinterType, PrintPreviewModelElement, ScalingType, Size} from 'chrome://print/print_preview.js';
 import {assert} from 'chrome://resources/js/assert.m.js';
 import {isChromeOS, isLacros} from 'chrome://resources/js/cr.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
--- chrome/test/data/webui/print_preview/scaling_settings_interactive_test.js
+++ chrome/test/data/webui/print_preview/scaling_settings_interactive_test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {ScalingType} from 'chrome://print/print_preview.js';
+import {PrintPreviewModelElement, ScalingType} from 'chrome://print/print_preview.js';
 import {assert} from 'chrome://resources/js/assert.m.js';
 import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
 import {selectOption} from 'chrome://test/print_preview/print_preview_test_utils.js';
