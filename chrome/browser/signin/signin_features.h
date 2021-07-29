7c6e725cc982d32f3c1339cee053e3e1d6702e9f
David Roger <droger@chromium.org>
2021-07-29 15:09:12 +0000

    [Lacros] Add feature flag for mutli-profile account consistency
    New flag:
    chrome://flags/#multi-profile-account-consistency
    
    Note: in about_flags.cc, kOsLinux is required (kOsCros does not work
    for Lacros)
    
    Bug: 1233933
    Change-Id: I7e661387961bccc945fdaad3fcfbe3266821ef1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058237
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906706}

--- chrome/browser/signin/signin_features.h
+++ chrome/browser/signin/signin_features.h
@@ -7,9 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SIGNIN_SIGNIN_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "build/chromeos_buildflags.h"
 
 extern const base::Feature kProcessGaiaRemoveLocalAccountHeader;
 
 extern const base::Feature kAccountPoliciesLoadedWithoutSync;
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+extern const base::Feature kMultiProfileAccountConsistency;
+#endif
 
 #endif  // CHROME_BROWSER_SIGNIN_SIGNIN_FEATURES_H_
