31db67b005cc9e38d4f7ad1f260f42b38fe23ab1
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 02:26:18 +0000

    [automated] Drop needless <vector> includes in /components/safe_browsing/android
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=xinghuilu@chromium.org
    
    Bug: 1233968
    Change-Id: If93b8682244a5117739439b840b59fb0976d06bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059579
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906529}

--- components/safe_browsing/android/safe_browsing_api_handler.h
+++ components/safe_browsing/android/safe_browsing_api_handler.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SAFE_BROWSING_ANDROID_SAFE_BROWSING_API_HANDLER_H_
 
 #include <memory>
-#include <vector>
 
 #include "base/callback.h"
 #include "components/safe_browsing/core/browser/db/util.h"
