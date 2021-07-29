fed9f47624c8326bb2787a3ea7326644cf059080
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 00:51:13 +0000

    [automated] Drop needless <vector> includes in /components/ntp_snippets
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=carlosk@chromium.org
    
    Bug: 1233968
    Change-Id: I13bff4a8779fff43676e027fbc6191ff2f26e75c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059559
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906493}

--- components/ntp_snippets/remote/json_to_categories.h
+++ components/ntp_snippets/remote/json_to_categories.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_NTP_SNIPPETS_REMOTE_JSON_TO_CATEGORIES_H_
 #define COMPONENTS_NTP_SNIPPETS_REMOTE_JSON_TO_CATEGORIES_H_
 
+#include <vector>
+
 #include "base/time/time.h"
 #include "base/values.h"
 #include "components/ntp_snippets/category.h"
