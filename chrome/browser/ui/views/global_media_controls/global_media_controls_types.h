9928c169b827d98b50581caa1d71fbbe6f75062c
Takumi Fujimoto <takumif@chromium.org>
2021-07-29 00:15:52 +0000

    Fix the GlobalMediaControlsCastActionAndEntryPoint histogram enum
    This CL puts the enum in sync with its C++ counterpart.
    
    Bug: 1234101, b/194924183
    Change-Id: Ief115870ea99352b4a6b8fc581835fb227f9d42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058865
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906476}

--- chrome/browser/ui/views/global_media_controls/global_media_controls_types.h
+++ chrome/browser/ui/views/global_media_controls/global_media_controls_types.h
@@ -20,7 +20,8 @@ enum class GlobalMediaControlsEntryPoint {
 };
 
 // These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
+// numeric values should never be reused. Keep this in sync with its counterpart
+// in tools/metrics/histograms/enums.xml.
 enum class GlobalMediaControlsCastActionAndEntryPoint {
   kStartViaToolbarIcon = 0,
   kStopViaToolbarIcon = 1,
