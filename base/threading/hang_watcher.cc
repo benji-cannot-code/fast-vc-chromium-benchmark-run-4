b45e3c043ff4ff314a8bbbd4e287fcb6776f2cbd
Daniel Hosseinian <dhoss@chromium.org>
2021-07-29 23:32:45 +0000

    Revert "Reduce allocations in HangWatcher by reusing WatchStateSnapshot."
    This reverts commit b7dd681e71e87ddb24c8f0ad854ed9cce68a0a39.
    
    Reason for revert: Causing
    RenderProcessHostTest.KeepAliveRendererProcess_Hung to fail on
    Win 7 Tests x64 [1].
    
    [1] https://ci.chromium.org/ui/p/chromium/builders/ci/Win%207%20Tests%20x64%20(1)/79981/overview
    
    Original change's description:
    > Reduce allocations in HangWatcher by reusing WatchStateSnapshot.
    >
    > Having allocations during a hang can cause the capture itself
    > to hang if the hang under capture is related to problematic allocations.
    >
    > Bug: 1034046
    > Change-Id: I0fcfdfa30dbcb9c2f488f0de62d56c912d6fc70d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057410
    > Commit-Queue: Olivier Li <olivierli@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#906334}
    
    Bug: 1034046
    Change-Id: Iffc57fe8423b9c92a55dcad0991fb6f59ce24211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061004
    Owners-Override: Daniel Hosseinian <dhoss@chromium.org>
    Auto-Submit: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#906912}

--- base/threading/hang_watcher.cc
+++ base/threading/hang_watcher.cc
@@ -490,24 +490,15 @@ ScopedClosureRunner HangWatcher::RegisterThread(ThreadType thread_type) {
 }
 
 base::TimeTicks HangWatcher::WatchStateSnapShot::GetHighestDeadline() const {
-  DCHECK(IsActionable());
-
+  DCHECK(!hung_watch_state_copies_.empty());
   // Since entries are sorted in increasing order the last entry is the largest
   // one.
   return hung_watch_state_copies_.back().deadline;
 }
 
-HangWatcher::WatchStateSnapShot::WatchStateSnapShot() = default;
-
-void HangWatcher::WatchStateSnapShot::Init(
+HangWatcher::WatchStateSnapShot::WatchStateSnapShot(
     const HangWatchStates& watch_states,
     base::TimeTicks deadline_ignore_threshold) {
-  DCHECK(!initialized_);
-
-  // No matter if the snapshot is actionable or not after this function
-  // it will have been initialized.
-  initialized_ = true;
-
   const base::TimeTicks now = base::TimeTicks::Now();
   bool all_threads_marked = true;
   bool found_deadline_before_ignore_threshold = false;
@@ -620,11 +611,6 @@ void HangWatcher::WatchStateSnapShot::Init(
                });
 }
 
-void HangWatcher::WatchStateSnapShot::Clear() {
-  hung_watch_state_copies_.clear();
-  initialized_ = false;
-}
-
 HangWatcher::WatchStateSnapShot::WatchStateSnapShot(
     const WatchStateSnapShot& other) = default;
 
@@ -656,14 +642,12 @@ std::string HangWatcher::WatchStateSnapShot::PrepareHungThreadListCrashKey()
 }
 
 bool HangWatcher::WatchStateSnapShot::IsActionable() const {
-  DCHECK(initialized_);
   return !hung_watch_state_copies_.empty();
 }
 
 HangWatcher::WatchStateSnapShot HangWatcher::GrabWatchStateSnapshotForTesting()
     const {
-  WatchStateSnapShot snapshot;
-  snapshot.Init(watch_states_, deadline_ignore_threshold_);
+  WatchStateSnapShot snapshot(watch_states_, deadline_ignore_threshold_);
   return snapshot;
 }
 
@@ -676,13 +660,12 @@ void HangWatcher::Monitor() {
   if (watch_states_.empty())
     return;
 
-  watch_state_snapshot_.Init(watch_states_, deadline_ignore_threshold_);
+  WatchStateSnapShot watch_state_snapshot(watch_states_,
+                                          deadline_ignore_threshold_);
 
-  if (watch_state_snapshot_.IsActionable()) {
-    DoDumpWithoutCrashing(watch_state_snapshot_);
+  if (watch_state_snapshot.IsActionable()) {
+    DoDumpWithoutCrashing(watch_state_snapshot);
   }
-
-  watch_state_snapshot_.Clear();
 }
 
 void HangWatcher::DoDumpWithoutCrashing(
