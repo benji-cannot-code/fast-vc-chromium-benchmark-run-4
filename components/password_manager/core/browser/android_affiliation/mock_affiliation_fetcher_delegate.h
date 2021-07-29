d5979da19f878e37fcecaf8da2ebcc811afd32bc
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 00:51:50 +0000

    [automated] Drop needless <vector> includes in /components/password_manager/core/browser/android_affiliation
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=engedy@chromium.org
    
    Bug: 1233968
    Change-Id: I81f7cbfafe3810fab852df7b37c4ddc041ceedac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059442
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906494}

--- components/password_manager/core/browser/android_affiliation/mock_affiliation_fetcher_delegate.h
+++ components/password_manager/core/browser/android_affiliation/mock_affiliation_fetcher_delegate.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_ANDROID_AFFILIATION_MOCK_AFFILIATION_FETCHER_DELEGATE_H_
 
 #include <memory>
-#include <vector>
 
 #include "components/password_manager/core/browser/android_affiliation/affiliation_fetcher_delegate.h"
 #include "testing/gmock/include/gmock/gmock.h"
