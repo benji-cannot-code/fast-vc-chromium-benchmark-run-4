a28b544973456c0e11f550f64ba37e29a5bc05b2
David Tseng <dtseng@google.com>
2021-07-29 17:25:43 +0000

    Reland: Introduce AccessibilityAnimationOneShot
    Fix flakey test by not animating when
    no_fade_for_testing_ is set.
    
    R=josiahk@google.com
    
    Bug: none
    Test: existing ash_unittests
    Change-Id: If29868ac2a961dfa3389ce7c9a4cf734b3a17ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058804
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906748}

--- ash/accessibility/switch_access/point_scan_controller.cc
+++ ash/accessibility/switch_access/point_scan_controller.cc
@@ -32,6 +32,10 @@ void PointScanController::Start() {
   HideAll();
   ResetAnimation();
   StartHorizontalRangeScan();
+  point_scan_animation_ = std::make_unique<AccessibilityAnimationOneShot>(
+      gfx::Rect(0, 0, 0, 0),
+      base::BindRepeating(&PointScanController::AnimateLine,
+                          base::Unretained(this)));
 }
 
 void PointScanController::StartHorizontalRangeScan() {
@@ -91,6 +95,7 @@ void PointScanController::Stop() {
   state_ = PointScanState::kOff;
   vertical_line_layer_->Pause();
   vertical_range_layer_->SetOpacity(0);
+  point_scan_animation_.reset();
 }
 
 void PointScanController::HideAll() {
@@ -177,10 +182,6 @@ void PointScanController::SetSpeedDipsPerSecond(int speed_dips_per_second) {
 
 void PointScanController::OnDeviceScaleFactorChanged() {}
 
-void PointScanController::OnAnimationStep(base::TimeTicks timestamp) {
-  AnimateLine(timestamp);
-}
-
 void PointScanController::UpdateTimeInfo(
     PointScanLayerAnimationInfo* animation_info,
     base::TimeTicks timestamp) {
@@ -188,7 +189,7 @@ void PointScanController::UpdateTimeInfo(
   animation_info->change_time = timestamp;
 }
 
-void PointScanController::AnimateLine(base::TimeTicks timestamp) {
+bool PointScanController::AnimateLine(base::TimeTicks timestamp) {
   if (horizontal_range_layer_->IsMoving()) {
     ComputeOffset(&horizontal_range_layer_info_, timestamp);
     horizontal_range_layer_->SetSubpixelPositionOffset(
@@ -210,6 +211,8 @@ void PointScanController::AnimateLine(base::TimeTicks timestamp) {
         gfx::Vector2dF(0.0, vertical_line_layer_info_.offset));
     UpdateTimeInfo(&vertical_line_layer_info_, timestamp);
   }
+
+  return false;
 }
 
 }  // namespace ash
--- ash/accessibility/switch_access/point_scan_layer.cc
+++ ash/accessibility/switch_access/point_scan_layer.cc
@@ -79,9 +79,6 @@ bool PointScanLayer::IsMoving() const {
   return is_moving_;
 }
 
-bool PointScanLayer::NeedToAnimate() const {
-  return true;
-}
 int PointScanLayer::GetInset() const {
   return 0;
 }
