e712baec573aee6498ee36994e89db1eeb54a616
Ian Vollick <vollick@chromium.org>
2021-07-29 02:11:28 +0000

    Disallow prerendering in supervised user mode
    With this change, the supervised user throttles cancel in a
    prerendered main frame since this is not yet supported.
    
    This change also includes a few updates to the prerendering
    test helper. It now supports use from SetUp in addition to
    SetUpOnMainThread. See crbug.com/1230090 about migrating
    all code to SetUp; keeping both functions was to reduce
    churn in this patch.
    
    I've also added a call to WaitForLoadStop in navigate
    primary page. This should not be required -- see
    crbug.com/1230068 for details.
    
    In order to use TestNavigationObserver, I needed to add
    support for observing uncommitted, unstarted navigations.
    This had some impact on test APIs, but it turned out that
    this parameter wasn't widely used so the churn appears
    minor.
    
    Bug: 1229685
    Change-Id: I23aa027ab02d1ccd93eb84dc062c662ae275c5d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031862
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Dan S <danan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906523}

--- chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/page_type.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/prerender_test_util.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
@@ -127,7 +128,10 @@ void InnerWebContentsAttachedWaiter::WaitForInnerWebContentsAttached() {
 class SupervisedUserNavigationThrottleTest
     : public MixinBasedInProcessBrowserTest {
  protected:
-  SupervisedUserNavigationThrottleTest() = default;
+  SupervisedUserNavigationThrottleTest()
+      : prerender_helper_(base::BindRepeating(
+            &SupervisedUserNavigationThrottleTest::web_contents,
+            base::Unretained(this))) {}
   ~SupervisedUserNavigationThrottleTest() override = default;
 
   void SetUp() override;
@@ -145,6 +149,14 @@ class SupervisedUserNavigationThrottleTest
 
   virtual chromeos::LoggedInUserMixin::LogInType GetLogInType() {
     return chromeos::LoggedInUserMixin::LogInType::kChild;
+  }
+
+  content::test::PrerenderTestHelper& prerender_helper() {
+    return prerender_helper_;
+  }
+
+  content::WebContents* web_contents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
   }
 
  private:
@@ -180,6 +192,7 @@ class SupervisedUserNavigationThrottleTest
   }
 
   std::unique_ptr<chromeos::LoggedInUserMixin> logged_in_user_mixin_;
+  content::test::PrerenderTestHelper prerender_helper_;
 };
 
 bool SupervisedUserNavigationThrottleTest::IsInterstitialBeingShownInMainFrame(
@@ -193,6 +206,7 @@ bool SupervisedUserNavigationThrottleTest::IsInterstitialBeingShownInMainFrame(
 }
 
 void SupervisedUserNavigationThrottleTest::SetUp() {
+  prerender_helper_.SetUp(embedded_test_server());
   // Polymorphically initiate logged_in_user_mixin_.
   logged_in_user_mixin_ = std::make_unique<chromeos::LoggedInUserMixin>(
       &mixin_host_, GetLogInType(), embedded_test_server(), this);
@@ -205,6 +219,35 @@ void SupervisedUserNavigationThrottleTest::SetUpOnMainThread() {
   ASSERT_TRUE(embedded_test_server()->Started());
 
   logged_in_user_mixin_->LogInUser();
+}
+
+// Tests that prerendering fails in supervised user mode.
+IN_PROC_BROWSER_TEST_F(SupervisedUserNavigationThrottleTest,
+                       DisallowPrerendering) {
+  base::HistogramTester histogram_tester;
+  const GURL initial_url = embedded_test_server()->GetURL("/simple.html");
+  const GURL allowed_url =
+      embedded_test_server()->GetURL("/supervised_user/simple.html");
+
+  prerender_helper().NavigatePrimaryPage(initial_url);
+
+  // If throttled, the prerendered navigation should not have started and we
+  // should not be requesting corresponding resources.
+  content::TestNavigationObserver observer(
+      web_contents(), content::MessageLoopRunner::QuitMode::IMMEDIATE,
+      /*ignore_uncommitted_navigations*/ false);
+  prerender_helper().AddPrerenderAsync(allowed_url);
+  observer.WaitForNavigationFinished();
+  EXPECT_FALSE(observer.last_navigation_succeeded());
+  EXPECT_EQ(allowed_url, observer.last_navigation_url());
+  EXPECT_EQ(0, prerender_helper().GetRequestCount(allowed_url));
+
+  // Regular navigation should proceed, however.
+  prerender_helper().NavigatePrimaryPage(allowed_url);
+  observer.WaitForNavigationFinished();
+  EXPECT_TRUE(observer.last_navigation_succeeded());
+  EXPECT_EQ(allowed_url, observer.last_navigation_url());
+  EXPECT_EQ(1, prerender_helper().GetRequestCount(allowed_url));
 }
 
 // Tests that navigating to a blocked page simply fails if there is no
