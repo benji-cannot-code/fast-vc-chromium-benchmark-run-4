618629aa6e7d44ec890b5c4d92b92a311f4ed8d1
jahchuen <ahchuen@google.com>
2021-07-29 23:27:43 +0000

    [CrOS Device Name] Move IsValidDeviceName() helper to its own util file.
    This CL moves IsValidDeviceName() helper function into
    device_name_validator.h/cc files. We also move move unit tests for valid
    and invalid device names to device_name_validator_unittest.cc
    
    Change-Id: I095094e505229361f2996b050dc4f6435f609652
    Bug: 126802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060055
    Commit-Queue: Jason Ah Chuen <ahchuen@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906909}

--- chrome/browser/chromeos/device_name_store_impl.cc
+++ chrome/browser/chromeos/device_name_store_impl.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/device_name_store_impl.h"
 
-#include "base/strings/string_util.h"
 #include "chrome/browser/chromeos/device_name_applier_impl.h"
+#include "chrome/browser/chromeos/device_name_validator.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
@@ -15,33 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
-
 const char kDefaultDeviceName[] = "ChromeOS";
-
-// For maximum compatibility with existing network services (e.g., Active
-// Directory), the upper limit for hostname length is 15 characters.
-const int kMaxDeviceNameLength = 15;
-
-const char kDeviceNameAllowedChars[] = "0123456789-abcdefghijklmnopqrstuvwxyz";
-
-bool IsValidDeviceName(const std::string& device_name) {
-  // Device name be not be empty string.
-  if (device_name.empty())
-    return false;
-
-  // Device name should be <=15 characters long.
-  if (device_name.length() > kMaxDeviceNameLength)
-    return false;
-
-  // Device name may contain only letters, numbers and hyphens.
-  if (!base::ContainsOnlyChars(base::ToLowerASCII(device_name),
-                               kDeviceNameAllowedChars)) {
-    return false;
-  }
-
-  return true;
-}
-
 }  // namespace
 
 DeviceNameStoreImpl::DeviceNameStoreImpl(
---
+++ chrome/browser/chromeos/device_name_validator.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_DEVICE_NAME_VALIDATOR_H_
+#define CHROME_BROWSER_CHROMEOS_DEVICE_NAME_VALIDATOR_H_
+
+#include <string>
+
+namespace chromeos {
+
+// Valid names must be >0 characters and <=15 characters long and contain only
+// letters, numbers or hyphens. Examples of invalid names include "Chrome
+// OS" (uses a space), "ChromeOS!" (uses an exclamation point),
+// "0123456789012345" (too long), "" (empty string).
+bool IsValidDeviceName(const std::string& device_name);
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_DEVICE_NAME_VALIDATOR_H_
