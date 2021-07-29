898bdd225fbf1ba5da9344485d9043f8e3b639f6
Shane Fitzpatrick <shanefitz@google.com>
2021-07-29 22:18:23 +0000

    [FastPair] Move browser delegate to common component
    This is a better home for this class, and will prevent circular
    dependencies as this is used by more parts of our system.
    
    Change-Id: Idc3d47252a9e9a31d451454411dcbab7e876897e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061504
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#906873}

--- chrome/browser/ash/quick_pair/quick_pair_browser_delegate_impl.h
+++ chrome/browser/ash/quick_pair/quick_pair_browser_delegate_impl.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_QUICK_PAIR_QUICK_PAIR_BROWSER_DELEGATE_IMPL_H_
 #define CHROME_BROWSER_ASH_QUICK_PAIR_QUICK_PAIR_BROWSER_DELEGATE_IMPL_H_
 
-#include "ash/quick_pair/quick_pair_browser_delegate.h"
+#include "ash/quick_pair/common/quick_pair_browser_delegate.h"
 
 namespace network {
 class SharedURLLoaderFactory;
