4b9c67289a6284d86199b65d84654b1c865ecbe8
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 00:08:30 +0000

    [automated] Drop needless <vector> includes in /components/offline_pages
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=carlosk@chromium.org
    
    Bug: 1233968
    Change-Id: Id74309240c82f2cb1d96d6763cc7108e4f183395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059441
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906471}

--- components/offline_pages/core/offline_page_metadata_store_test_util.h
+++ components/offline_pages/core/offline_page_metadata_store_test_util.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_OFFLINE_PAGES_CORE_OFFLINE_PAGE_METADATA_STORE_TEST_UTIL_H_
 
 #include <memory>
-#include <vector>
 
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
