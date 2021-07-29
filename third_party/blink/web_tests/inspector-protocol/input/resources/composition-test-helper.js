887fc196a72d0a34840029e8d6ef4d43d0a28d71
Javier Contreras Tenorio <t-javierc@microsoft.com>
2021-07-29 18:55:46 +0000

    DevTools: Input.imeSetComposition
    Adds new protocol method imeSetComposition
    for emulating composition events.
    
    Bug: 1222642
    Change-Id: I913a29cc7e582118d0eeffdba7ab732e9af79c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3009937
    Commit-Queue: Javier Contreras Tenorio <t-javierc@microsoft.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906785}

---
+++ third_party/blink/web_tests/inspector-protocol/input/resources/composition-test-helper.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(class CompositionTestHelper {
+  static async init(session, inputValue) {
+    await session.evaluate(`
+    window.input = document.createElement('input');
+    document.body.appendChild(window.input);
+    window.input.focus();
+    window.input.value = '` + inputValue + `';
+
+    window.logs = [];
+
+    logs.push('Initial value:' + JSON.stringify(window.input.value));
+
+    window.input.addEventListener('compositionupdate', logEvent);
+    window.input.addEventListener('input', logEvent);
+    window.input.addEventListener('compositionstart', logEvent);
+    window.input.addEventListener('compositionend', logEvent);
+    window.input.addEventListener('beforeinput', logEvent);
+    window.addEventListener('keydown', logEvent);
+    window.addEventListener('keypress', logEvent);
+    window.addEventListener('keyup', logEvent);
+
+    function logEvent(event) {
+      logs.push('');
+      logs.push('event: ' + event.type);
+      logs.push('value: ' + input.value);
+      logs.push('selectionStart: ' + input.selectionStart);
+      logs.push('selectionEnd: ' + input.selectionEnd);
+      if (event.keyCode)
+        logs.push('keyCode: ' + event.keyCode);
+      if (event.key)
+        logs.push('key: ' + event.key);
+      if (event.charCode)
+        logs.push('charCode: ' + event.charCode);
+      if (event.text)
+        logs.push('text: ' + event.text);
+      if (event.location)
+        logs.push('location: ' + event.location);
+      if (event.code)
+        logs.push('code: ' + event.code);
+    }
+  `);
+  }
+})
