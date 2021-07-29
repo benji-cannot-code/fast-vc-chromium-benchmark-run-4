fdda9ce045363305ffff3cad15481c39140a74a8
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-07-29 02:22:32 +0000

    [automated] Drop needless <vector> includes in /components/safe_browsing
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=xinghuilu@chromium.org
    
    Bug: 1233968
    Change-Id: I8672bc661fb51d9cc1313f7bafbdfb6774c9be8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056967
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906526}

--- components/safe_browsing/content/browser/mojo_safe_browsing_impl.cc
+++ components/safe_browsing/content/browser/mojo_safe_browsing_impl.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/content/browser/mojo_safe_browsing_impl.h"
 
 #include <memory>
-#include <vector>
 
 #include "base/bind.h"
 #include "base/supports_user_data.h"
--- components/safe_browsing/content/browser/safe_browsing_blocking_page.h
+++ components/safe_browsing/content/browser/safe_browsing_blocking_page.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SAFE_BROWSING_CONTENT_BROWSER_SAFE_BROWSING_BLOCKING_PAGE_H_
 
 #include <map>
-#include <vector>
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
