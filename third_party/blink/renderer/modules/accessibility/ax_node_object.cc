630d30ce70d3e1abac8c0fbdb5591fbc29538627
Nina Satragno <nsatragno@chromium.org>
2021-07-30 21:48:33 +0000

    Revert "Do not expose aria-errormessage when aria-invalid is false"
    This reverts commit 5b6bfaae62d8c6a5065fe4e1ceff4ddfc9909042.
    
    Reason for revert: sus for crbug.com/1234859
    
    Bug: 1234859
    
    Original change's description:
    > Do not expose aria-errormessage when aria-invalid is false
    >
    > ARIA 1.2 added the following requirement:
    >
    >   User agents MUST NOT expose aria-errormessage for an object with an
    >   aria-invalid value of false.
    >
    > We were exposing it unconditionally. This commit fixes that.
    >
    > AX-Relnotes: Implement ARIA 1.2 requirement to not expose
    > aria-errormessage when aria-invalid is false.
    >
    > Bug: 1233978
    >
    > Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    > Change-Id: I9cc8e654ea22919c105e331d88fd69d49990494c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062841
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#907136}
    
    Bug: 1233978
    Change-Id: I8c395d221e53a317ef8775b37708dc5a8b160d39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064440
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Owners-Override: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907275}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -4478,9 +4478,6 @@ void AXNodeObject::HandleActiveDescendantChanged() {
 }
 
 AXObject* AXNodeObject::ErrorMessage() const {
-  if (GetInvalidState() == ax::mojom::blink::InvalidState::kFalse)
-    return nullptr;
-
   // Check for aria-errormessage.
   Element* existing_error_message =
       GetAOMPropertyOrARIAAttribute(AOMRelationProperty::kErrorMessage);
