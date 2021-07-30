19b8c4fe5a9907e30aa3d0bb77d6ad24c1c33941
Nina Satragno <nsatragno@chromium.org>
2021-07-30 21:10:25 +0000

    Revert "[Passwords] ChromeBrowsingDataRemoverDelegateTest uses PwdStoreInterface"
    This reverts commit 575271e6977a9931a2a963efd859b90c443ec3db.
    
    Reason for revert: CL sus crbug.com/1234803
    
    Now that
    https://chromium-review.googlesource.com/c/chromium/src/+/3062942
    has been reverted, landing this patch should not break the build.
    
    This was originally https://crrev.com/c/3064000 but apparently
    the rubber stamp bot doesn't work on relands.
    
    Original change's description:
    > [Passwords] ChromeBrowsingDataRemoverDelegateTest uses PwdStoreInterface
    >
    > This CL switches ChromeBrowsingDataRemoverDelegateTest to use
    > MockPasswordStoreInterface instead of MockPasswordStore.
    >
    > This requires:
    > 1- Introducing new method in the PasswordStoreFactory to  create
    > the interface.
    > 2- Updating bunch of Sync code to use PasswordStoreInterface instead
    > of PasswordStore.
    >
    > Bug: 1218413
    > Change-Id: I9ea6ef2b488b122b07b50bbbc5dc629e34be960f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031068
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#907092}
    
    Bug: 1218413, 1234803
    Change-Id: Ic31a5426e1d4b32f52206556acee5d165ea73b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064480
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Owners-Override: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907256}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -44,11 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using password_manager::PasswordStore;
-using password_manager::PasswordStoreInterface;
 
-// TODO(crbug.com/1218413): Delete this method when the migration to
-// PasswordStoreInterface is complete and rename the method below to
-// GetForProfile. static
+// static
 scoped_refptr<PasswordStore> PasswordStoreFactory::GetForProfile(
     Profile* profile,
     ServiceAccessType access_type) {
@@ -60,13 +57,6 @@ scoped_refptr<PasswordStore> PasswordStoreFactory::GetForProfile(
     return nullptr;
   return base::WrapRefCounted(static_cast<password_manager::PasswordStore*>(
       GetInstance()->GetServiceForBrowserContext(profile, true).get()));
-}
-
-// static
-scoped_refptr<PasswordStoreInterface>
-PasswordStoreFactory::GetInterfaceForProfile(Profile* profile,
-                                             ServiceAccessType access_type) {
-  return GetForProfile(profile, access_type);
 }
 
 // static
