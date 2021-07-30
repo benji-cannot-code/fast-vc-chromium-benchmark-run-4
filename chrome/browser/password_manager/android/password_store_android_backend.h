c9f84ce1a1d11d102978f1a64b7273492afde81f
Friedrich Horschig <fhorschig@chromium.org>
2021-07-30 13:02:44 +0000

    [Android][Passwords] Map JNI tasks to backend replies
    Java can't hold or call C++ callbacks via JNI (as far as I know).
    Therefore, this CL introduces a mapping from TaskIds to callbacks that
    are called upon creation of a JNI task.
    
    Since each Task only needs a callback, a variant allows to store exactly
    the one that will be called afterwards. It is most-likely that the data
    filtering happens on the java side and therefore, other parameters won't
    be necessary.
    Other native post-processing steps can be bound as callback as well.
    
    Bug: 1229654, 1229655
    Change-Id: I483469e91b159d4aa38d0baf1114c7a52d32530b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3046191
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907099}

--- chrome/browser/password_manager/android/password_store_android_backend.h
+++ chrome/browser/password_manager/android/password_store_android_backend.h
@@ -7,10 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_PASSWORD_STORE_ANDROID_BACKEND_H_
 
 #include <memory>
+#include <unordered_map>
 
+#include "base/containers/small_map.h"
 #include "chrome/browser/password_manager/android/password_store_android_backend_bridge.h"
 #include "components/password_manager/core/browser/password_store_backend.h"
 #include "components/sync/model/model_type_controller_delegate.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
+
+namespace syncer {
+class ModelTypeControllerDelegate;
+}  // namespace syncer
 
 namespace password_manager {
 
@@ -63,6 +70,13 @@ class PasswordStoreAndroidBackend
         this};
   };
 
+  using TaskId = PasswordStoreAndroidBackendBridge::TaskId;
+  using ReplyVariant = absl::variant<LoginsReply, PasswordStoreChangeListReply>;
+  // Using a small_map should ensure that we handle rare cases with many tasks
+  // like a bulk deletion just as well as the normal, rather small task load.
+  using TaskMap =
+      base::small_map<std::unordered_map<TaskId, ReplyVariant, TaskId::Hasher>>;
+
   // Implements PasswordStoreBackend interface.
   void InitBackend(RemoteChangesReceived remote_form_changes_received,
                    base::RepeatingClosure sync_enabled_or_disabled_cb,
@@ -103,11 +117,20 @@ class PasswordStoreAndroidBackend
   base::WeakPtr<syncer::ModelTypeControllerDelegate>
   GetSyncControllerDelegate();
 
+  ReplyVariant GetAndEraseTask(TaskId task_id);
+
   // Observer to propagate remote form changes to.
   RemoteChangesReceived remote_form_changes_received_;
 
   // Delegate to handle sync events.
   SyncModelTypeControllerDelegate sync_controller_delegate_;
+
+  // TaskRunner for all the background operations.
+  scoped_refptr<base::SequencedTaskRunner> main_task_runner_;
+
+  // Used to store callbacks for each invoked tasks since callbacks can't be
+  // called via JNI directly.
+  TaskMap request_for_task_;
 
   // This object is the proxy to the JNI bridge that performs the API requests.
   std::unique_ptr<PasswordStoreAndroidBackendBridge> bridge_;
