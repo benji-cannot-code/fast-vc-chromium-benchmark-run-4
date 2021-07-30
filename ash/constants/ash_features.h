b1d1dad6f333f8c77b23af0d92d85c437793316b
Andrew Xu <andrewxu@chromium.org>
2021-07-30 18:39:40 +0000

    [Launcher] Add an internal flag to remove launcher spaces
    Add an internal flag for development only. When the flag is enabled,
    only the last launcher page is allowed to have empty slots.
    
    Bug: 1227736
    Change-Id: I13d63bced69d5da7811e8f5842ff15ce2ce9740e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041934
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907206}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -249,6 +249,8 @@ extern const base::Feature kLanguageSettingsUpdate2;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kLauncherAppSort;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kLauncherRemoveEmptySpace;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kLicensePackagedOobeFlow;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kLimitAltTabToActiveDesk;
@@ -489,6 +491,7 @@ bool IsInstantTetheringBackgroundAdvertisingSupported();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsKeyboardBasedDisplayArrangementInSettingsEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLauncherAppSortEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLauncherRemoveEmptySpaceEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLicensePackagedOobeFlowEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsLockScreenHideSensitiveNotificationsSupported();
