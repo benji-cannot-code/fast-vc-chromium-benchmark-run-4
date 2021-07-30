1647da18ff6069f20deed427b045fd373167d230
Kevin McNee <mcnee@chromium.org>
2021-07-30 01:46:36 +0000

    Remove unneeded include for extensions notification types
    Bug: 1174746
    Change-Id: I75c8953be3accb150a9c004fd628278253ec37d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057770
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906965}

--- extensions/browser/notification_types.h
+++ extensions/browser/notification_types.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_NOTIFICATION_TYPES_H_
 #define EXTENSIONS_BROWSER_NOTIFICATION_TYPES_H_
 
-#include <string>
-
 #include "content/public/browser/notification_types.h"
 #include "extensions/buildflags/buildflags.h"
 
