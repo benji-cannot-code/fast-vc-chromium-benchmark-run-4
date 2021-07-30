d5aedc111e80dc742f025527b3f3dce41823ff6f
Keith Lee <keithlee@chromium.org>
2021-07-30 06:57:43 +0000

    VK API: added feature flag
    Guard the Show/Hide functionality behind this flag.
    
    CRBUG=1231021
    
    Change-Id: Ie54dc346127a4e1ebd61b32d259afd08bec6b563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056324
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907030}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1093,6 +1093,10 @@ const base::Feature kUserActivityPrediction{"UserActivityPrediction",
 const base::Feature kVerticalSplitScreen{"VerticalSplitScreen",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
+// Enable the Virtual Keyboard API.
+const base::Feature kVirtualKeyboardApi{"VirtualKeyboardApi",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enable or disable using the floating virtual keyboard as the default option
 // on Chrome OS.
 const base::Feature kVirtualKeyboardFloatingDefault{
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3038,6 +3038,9 @@ extern const char kVaapiWebPImageDecodeAccelerationDescription[];
 extern const char kVirtualKeyboardName[];
 extern const char kVirtualKeyboardDescription[];
 
+extern const char kVirtualKeyboardApiName[];
+extern const char kVirtualKeyboardApiDescription[];
+
 extern const char kVirtualKeyboardBorderedKeyName[];
 extern const char kVirtualKeyboardBorderedKeyDescription[];
 
