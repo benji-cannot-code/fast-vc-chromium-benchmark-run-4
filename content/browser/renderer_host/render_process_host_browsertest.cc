dbb6fd37e166aac450f6f0348eca05f82ffcde73
Luciano Pacheco <lucmult@chromium.org>
2021-07-30 07:27:41 +0000

    [Sheriff] Disable flake tests: RenderProcessHostTest.*_Hung
    Bug: 872269
    Change-Id: I3f8e38ef285099ec884fd558708a099e48a6aa83
    No-Try: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061871
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907034}

--- content/browser/renderer_host/render_process_host_browsertest.cc
+++ content/browser/renderer_host/render_process_host_browsertest.cc
@@ -1145,7 +1145,7 @@ IN_PROC_BROWSER_TEST_F(RenderProcessHostTest,
 }
 
 // Test is flaky on Android builders: https://crbug.com/875179
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_WIN)
 #define MAYBE_KeepAliveRendererProcess_Hung \
   DISABLED_KeepAliveRendererProcess_Hung
 #else
@@ -1185,7 +1185,7 @@ IN_PROC_BROWSER_TEST_F(RenderProcessHostTest,
 }
 
 // Test is flaky on Android builders: https://crbug.com/875179
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_WIN)
 #define MAYBE_FetchKeepAliveRendererProcess_Hung \
   DISABLED_FetchKeepAliveRendererProcess_Hung
 #else
