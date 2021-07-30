f2196065f7bb62c900b967db61b0d7e5a2285d52
jahchuen <ahchuen@google.com>
2021-07-30 00:04:13 +0000

    [CrOS Device Name] Add DeviceNameState enum and GetDeviceNameMetadata()
    This CL adds a new enum class that provides the state of the device name
    (can be modified, forbidden because of policies, forbidden because not
    device owner). GetDeviceName() is changed to GetDeviceNameMetadata()
    which returns a Metadata struct that contains the device name and its
    state. Unit tests also now check for both the device name and its state.
    
    Change-Id: Ibb914b0c15e00a6ff60b7b1c137c9aced4ef9bbe
    Bug: 126802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060529
    Commit-Queue: Jason Ah Chuen <ahchuen@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906927}

--- chrome/browser/chromeos/fake_device_name_store.cc
+++ chrome/browser/chromeos/fake_device_name_store.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/fake_device_name_store.h"
 
+#include "base/strings/string_util.h"
+#include "chrome/browser/chromeos/device_name_validator.h"
+
 namespace chromeos {
 
 // static
@@ -14,20 +17,33 @@ FakeDeviceNameStore::FakeDeviceNameStore() = default;
 
 FakeDeviceNameStore::~FakeDeviceNameStore() = default;
 
-std::string FakeDeviceNameStore::GetDeviceName() const {
-  return device_name_;
+DeviceNameStore::DeviceNameMetadata FakeDeviceNameStore::GetDeviceNameMetadata()
+    const {
+  return {device_name_, device_name_state_};
 }
 
 DeviceNameStore::SetDeviceNameResult FakeDeviceNameStore::SetDeviceName(
     const std::string& new_device_name) {
-  if (name_update_result_ == DeviceNameStore::SetDeviceNameResult::kSuccess)
-    device_name_ = new_device_name;
-  return name_update_result_;
+  switch (device_name_state_) {
+    case DeviceNameStore::DeviceNameState::kCannotBeModifiedBecauseOfPolicy:
+      return DeviceNameStore::SetDeviceNameResult::kProhibitedByPolicy;
+
+    case DeviceNameStore::DeviceNameState::
+        kCannotBeModifiedBecauseNotDeviceOwner:
+      return DeviceNameStore::SetDeviceNameResult::kNotDeviceOwner;
+
+    case DeviceNameStore::DeviceNameState::kCanBeModified:
+      if (!IsValidDeviceName(new_device_name))
+        return DeviceNameStore::SetDeviceNameResult::kInvalidName;
+
+      device_name_ = new_device_name;
+      return DeviceNameStore::SetDeviceNameResult::kSuccess;
+  }
 }
 
-void FakeDeviceNameStore::SetNextSetDeviceNameResult(
-    DeviceNameStore::SetDeviceNameResult name_update_result) {
-  name_update_result_ = name_update_result;
+void FakeDeviceNameStore::SetDeviceNameState(
+    DeviceNameStore::DeviceNameState device_name_state) {
+  device_name_state_ = device_name_state;
 }
 
 }  // namespace chromeos
