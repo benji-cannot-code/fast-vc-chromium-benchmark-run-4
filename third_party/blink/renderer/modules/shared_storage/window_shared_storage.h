a5b373dfa5174feb0e68d5ea9c09b7e2c8eb8e89
Yao Xiao <yaoxia@chromium.org>
2021-07-30 22:25:12 +0000

    [shared storage] Add the renderer side API
    Add the renderer side API behind a RuntimeEnabledFeature. Add some
    preliminary error checking but leave the major part unimplemented.
    
    Explainer:
    https://github.com/pythagoraskitty/shared-storage/blob/main/README.md
    
    I2P:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/_quChIvPzT8/m/0W7IxD_1AAAJ
    
    Bug: 1218540
    Change-Id: Ibd81e63e3f7a386e99d3ee356527121f42e61e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021592
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907286}

---
+++ third_party/blink/renderer/modules/shared_storage/window_shared_storage.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_SHARED_STORAGE_WINDOW_SHARED_STORAGE_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_SHARED_STORAGE_WINDOW_SHARED_STORAGE_H_
+
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+
+namespace blink {
+
+class SharedStorage;
+class LocalDOMWindow;
+
+// Implement the sharedStorage attribute under Window.
+class WindowSharedStorage {
+  STATIC_ONLY(WindowSharedStorage);
+
+ public:
+  static SharedStorage* sharedStorage(LocalDOMWindow&);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_SHARED_STORAGE_WINDOW_SHARED_STORAGE_H_
