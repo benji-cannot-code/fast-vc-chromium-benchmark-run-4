732df77e4217f1c8f7b61d0604e2e352a99eac97
Mohamed Amir Yosef <mamir@chromium.org>
2021-07-30 15:42:25 +0000

    [Passwords] PasswordManagerClient can return PasswordStoreInterface
    Not all users of PasswordManagerClient are already to switch to
    PasswordStoreInterface. For example, many are still blocked on the
    refactoring planned in PasswordStore for insecure credentials
    information storage.
    
    In order to unblock more migration to PasswordStoreInterface, this
    CL in introducing two temporary methods in the PasswordManagerClient
    that return PasswordStoreInterface.
    
    Bug: 1218413
    Change-Id: I7a0bdcc5fb3800540805d511539a1d800bc0bed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2988750
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907137}

--- components/password_manager/core/browser/password_manager_client.h
+++ components/password_manager/core/browser/password_manager_client.h
@@ -79,6 +79,7 @@ class HttpAuthManager;
 class PasswordRequirementsService;
 class PasswordReuseManager;
 class PasswordStore;
+class PasswordStoreInterface;
 struct PasswordForm;
 
 enum class SyncState {
@@ -240,10 +241,9 @@ class PasswordManagerClient {
                                 const PasswordFormManagerForUI* form_manager);
 
   // Informs the embedder that user credentials were leaked.
-  virtual void NotifyUserCredentialsWereLeaked(
-      CredentialLeakType leak_type,
-      const GURL& origin,
-      const std::u16string& username);
+  virtual void NotifyUserCredentialsWereLeaked(CredentialLeakType leak_type,
+                                               const GURL& origin,
+                                               const std::u16string& username);
 
   // Requests a reauth for the primary account with |access_point| representing
   // where the reauth was triggered.
@@ -265,6 +265,11 @@ class PasswordManagerClient {
 
   // Returns the account PasswordStore associated with this instance.
   virtual PasswordStore* GetAccountPasswordStore() const = 0;
+
+  // TODO(crbug.com/1218413): remove the follow two method once the two above
+  // methods are returning PasswordStoreInterface.
+  virtual PasswordStoreInterface* GetProfilePasswordStoreInterface() const;
+  virtual PasswordStoreInterface* GetAccountPasswordStoreInterface() const;
 
   // Returns the PasswordReuseManager associated with this instance.
   virtual PasswordReuseManager* GetPasswordReuseManager() const = 0;
