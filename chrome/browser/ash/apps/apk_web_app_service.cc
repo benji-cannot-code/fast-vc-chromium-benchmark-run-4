b3f620ba43fc1daeeadf206efad5757a98e1663c
Tim Sergeant <tsergeant@chromium.org>
2021-07-30 03:38:31 +0000

    Reland "[CrOS WebAPKs] Enable by default"
    This reverts commit 66ee71baffb0cc8d0e80a2b59710c9f5ecabc627.
    
    Reason for revert:
    
    Fixes crash in ApkWebAppPrefs by resetting the ArcAppListPrefs observer
    when it is destroyed by the test.
    
    Original change's description:
    > Revert "[CrOS WebAPKs] Enable by default"
    >
    > This reverts commit efd6be34c87a63a80b4a11269d02964841796a20.
    >
    > Reason for revert: Likely causing failures to
    > *ArcDefaultAppTest.*DefaultApps* tests on multiple bots [1][2][3].
    >
    > [1] https://ci.chromium.org/ui/p/chromium/builders/ci/linux-cfm-rel/8221/overview
    > [2] https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/49892/overview
    > [3] https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/24412/overview
    >
    > Original change's description:
    > > [CrOS WebAPKs] Enable by default
    > >
    > > This feature is now complete (modulo bug fixes) and is ready for testing
    > > on canary/dev.
    > >
    > > Bug: 1200177, 1198433, 1234279
    > > Change-Id: I9fa4c76e26fe4391e84001f7b88e54d7234b1794
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058606
    > > Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    > > Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#906618}
    >
    > Bug: 1200177, 1198433, 1234279
    > Change-Id: Ia31a2e1fb01f63b22e1dcaf20689851595deaa3d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059980
    > Auto-Submit: Daniel Hosseinian <dhoss@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    > Owners-Override: Daniel Hosseinian <dhoss@chromium.org>
    > Reviewed-by: Patrick Monette <pmonette@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#906725}
    
    Bug: 1200177, 1198433, 1234279
    Change-Id: Ic40c1156e4aa666cd7eebbc0d27138eecc923a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061958
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906998}

--- chrome/browser/ash/apps/apk_web_app_service.cc
+++ chrome/browser/ash/apps/apk_web_app_service.cc
@@ -408,6 +408,10 @@ void ApkWebAppService::OnPackageListInitialRefreshed() {
   }
 }
 
+void ApkWebAppService::OnArcAppListPrefsDestroyed() {
+  arc_app_list_prefs_observer_.Reset();
+}
+
 void ApkWebAppService::OnWebAppWillBeUninstalled(
     const web_app::AppId& web_app_id) {
   if (!base::FeatureList::IsEnabled(features::kApkWebAppInstalls))
