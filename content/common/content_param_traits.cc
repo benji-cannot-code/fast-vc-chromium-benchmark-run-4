4302c8d78c507285fa6f58732afb902c720068fd
Kevin McNee <mcnee@chromium.org>
2021-07-30 17:47:43 +0000

    Remove several unused IPC_ENUM_TRAITS from content/common/
    Bug: 1227131
    Change-Id: I7829b3d432392a09b057cd5f0c6b755959c819cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057072
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907154}

--- content/common/content_param_traits.cc
+++ content/common/content_param_traits.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/blob/blob.mojom.h"
 #include "third_party/blink/public/mojom/permissions_policy/policy_value.mojom.h"
 #include "ui/accessibility/ax_mode.h"
+#include "ui/gfx/ipc/geometry/gfx_param_traits.h"
 
 namespace IPC {
 
