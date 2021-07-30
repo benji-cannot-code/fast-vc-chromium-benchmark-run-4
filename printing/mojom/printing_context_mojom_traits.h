90b89ca774b806c7d63ca0e9c31c2dc13e87794d
Alan Screen <awscreen@chromium.org>
2021-07-30 21:23:59 +0000

    Add printing PageSetup mojo type with mapping
    Print driver settings queries and document printing with the print
    backend service requires the ability to transfer PrintSettings over
    Mojo.  This requires types used by its members to also be mojoified.
    
    Add a mojo definition for printing::PageSetup as part of the overall
    mojofication for PrintSettings.
    
    Bug: 809738
    Change-Id: If3c0aaddea8043f051a587ca49a9b6ea2c249909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062419
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907264}

--- printing/mojom/printing_context_mojom_traits.h
+++ printing/mojom/printing_context_mojom_traits.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "printing/mojom/printing_context.mojom-shared.h"
 #include "printing/page_setup.h"
+#include "ui/gfx/geometry/size.h"
 
 namespace mojo {
 
@@ -23,6 +24,39 @@ struct StructTraits<printing::mojom::PageMarginsDataView,
 
   static bool Read(printing::mojom::PageMarginsDataView data,
                    printing::PageMargins* out);
+};
+
+template <>
+struct StructTraits<printing::mojom::PageSetupDataView, printing::PageSetup> {
+  static const gfx::Size& physical_size(const printing::PageSetup& s) {
+    return s.physical_size();
+  }
+  static const gfx::Rect& printable_area(const printing::PageSetup& s) {
+    return s.printable_area();
+  }
+  static const gfx::Rect& overlay_area(const printing::PageSetup& s) {
+    return s.overlay_area();
+  }
+  static const gfx::Rect& content_area(const printing::PageSetup& s) {
+    return s.content_area();
+  }
+  static const printing::PageMargins& effective_margins(
+      const printing::PageSetup& s) {
+    return s.effective_margins();
+  }
+  static const printing::PageMargins& requested_margins(
+      const printing::PageSetup& s) {
+    return s.requested_margins();
+  }
+  static bool forced_margins(const printing::PageSetup& s) {
+    return s.forced_margins();
+  }
+  static int32_t text_height(const printing::PageSetup& s) {
+    return s.text_height();
+  }
+
+  static bool Read(printing::mojom::PageSetupDataView data,
+                   printing::PageSetup* out);
 };
 
 }  // namespace mojo
