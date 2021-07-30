a0800cd8344068a2e9c27b3402a9b04e31a2da43
Jon Mann <jonmann@chromium.org>
2021-07-30 03:24:01 +0000

    FastPair: Integrate DeviceMetadataFetcher with FastPairRepository.
    Change-Id: I3954ea6953def83b17092138f0112992df8cb37f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061498
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#906992}

--- ash/quick_pair/repository/fast_pair_repository.cc
+++ ash/quick_pair/repository/fast_pair_repository.cc
@@ -6,20 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/repository/fast_pair_repository.h"
 
 #include "ash/quick_pair/common/logging.h"
+#include "ash/quick_pair/repository/fast_pair/device_metadata_fetcher.h"
 #include "device/bluetooth/bluetooth_device.h"
 
 namespace ash {
 namespace quick_pair {
 
-FastPairRepository::FastPairRepository() = default;
+FastPairRepository::FastPairRepository()
+    : device_metadata_fetcher_(std::make_unique<DeviceMetadataFetcher>()) {}
 FastPairRepository::~FastPairRepository() = default;
 
 void FastPairRepository::GetDeviceMetadata(
     const std::string& hex_model_id,
-    base::OnceCallback<void(absl::optional<nearby::fastpair::Device>)>
+    base::OnceCallback<
+        void(absl::optional<nearby::fastpair::GetObservedDeviceResponse>)>
         callback) {
   QP_LOG(INFO) << __func__;
-  std::move(callback).Run(absl::nullopt);
+  device_metadata_fetcher_->LookupHexDeviceId(hex_model_id,
+                                              std::move(callback));
 }
 
 void FastPairRepository::IsValidModelId(
