63fde006452bf7a20ddd629f0cc930d77c667c29
Sylvain Defresne <sdefresne@chromium.org>
2021-07-30 10:16:24 +0000

    [ios] Expose `branded_images` API as free function
    Add one free function exposing the `branded_image` API with the goal
    to replace `BrandedImageProvider` class. Do not convert the code to
    use the function as it has no internal implementation yet. As some
    of the methods of `BrandedImageProvider` are never called, do not
    expose enumerator for the corresponding images.
    
    Implement `ChromiumBrandedImageProvider` by calling the new API to
    avoid having duplicated implementation.
    
    Bug: 1201182
    Change-Id: Ifaed526c2d09edc97f6013e7e581b52d10056451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059496
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907071}

---
+++ ios/public/provider/chrome/browser/branded_images/test_branded_images.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/branded_images/branded_images_api.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace ios {
+namespace provider {
+
+UIImage* GetBrandedImage(BrandedImage branded_image) {
+  // No image in tests.
+  return nil;
+}
+
+}  // namespace provider
+}  // namespace ios
