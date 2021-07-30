58aa67e9d4f788eb54421479c6d4c1c5b7b1b6d3
Avi Drissman <avi@chromium.org>
2021-07-30 18:25:02 +0000

    Add missing include.
    The new base::clamp includes std::less thus requiring <functional>.
    Thank you to thestig for catching this.
    
    Bug: 1231569
    Change-Id: I1ff791f0472194ab7198182d9074ab5387066c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062522
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907202}

--- base/cxx17_backports.h
+++ base/cxx17_backports.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_CXX17_BACKPORTS_H_
 
 #include <array>
+#include <functional>
 #include <initializer_list>
 #include <memory>
 #include <string>
