f1c4565ef24094c33b38ddb8037d01cb952d0a99
Jon Mann <jonmann@chromium.org>
2021-07-30 02:22:04 +0000

    FastPair: Use a real SharedURLLoaderFactory.
    This change integrates HttpFetcher with the new
    QuickPairBrowserDelegate to get a real instance of
    the active user's SharedURLLoaderFactory.
    
    Change-Id: Ia519a69b5f9f697b891a353cffa6c996a743bac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061322
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/master@{#906977}

--- ash/quick_pair/repository/http_fetcher.cc
+++ ash/quick_pair/repository/http_fetcher.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/repository/http_fetcher.h"
 
 #include "ash/quick_pair/common/logging.h"
+#include "ash/quick_pair/common/quick_pair_browser_delegate.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
@@ -39,7 +40,7 @@ HttpFetcher::~HttpFetcher() = default;
 
 void HttpFetcher::ExecuteGetRequest(const GURL& url,
                                     FetchCompleteCallback callback) {
-  QP_LOG(VERBOSE) << __func__ << " executing request to: " << url;
+  QP_LOG(VERBOSE) << __func__ << ": executing request to: " << url;
 
   auto resource_request = std::make_unique<network::ResourceRequest>();
   resource_request->url = url;
@@ -48,9 +49,17 @@ void HttpFetcher::ExecuteGetRequest(const GURL& url,
 
   auto loader = network::SimpleURLLoader::Create(std::move(resource_request),
                                                  traffic_annotation_);
+  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory =
+      QuickPairBrowserDelegate::Get()->GetURLLoaderFactory();
+  if (!url_loader_factory) {
+    QP_LOG(WARNING) << __func__ << ": No SharedURLLoaderFactory is available.";
+    std::move(callback).Run(nullptr);
+    return;
+  }
+
   auto* loader_ptr = loader.get();
   loader_ptr->DownloadToString(
-      /*url_loader_factory=*/nullptr,
+      url_loader_factory.get(),
       base::BindOnce(&HttpFetcher::OnComplete, weak_ptr_factory_.GetWeakPtr(),
                      std::move(loader), std::move(callback)),
       kMaxDownloadBytes);
