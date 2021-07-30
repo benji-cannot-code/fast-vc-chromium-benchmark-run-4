48b2a9a287eff7075538ab3bef88037aadb1932d
Robert Ogden <robertogden@chromium.org>
2021-07-30 17:54:57 +0000

    OptGuide Skeleton iOS Directory and Boilerplate
    This CL creates a new directory for OptGuide on iOS at the chrome layer.
    Nothing is going on here, just adding enough code to show I have a
    working development setup and adding boilerplate for the future.
    
    Bug: 1232854
    Change-Id: I55847d63d8432d63c26439e24ae2eaef52171212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057212
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907163}

--- ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/browser_state_keyed_service_factories.mm
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/language/url_language_histogram_factory.h"
 #import "ios/chrome/browser/metrics/ios_profile_session_durations_service_factory.h"
 #include "ios/chrome/browser/ntp_snippets/ios_chrome_content_suggestions_service_factory.h"
+#include "ios/chrome/browser/optimization_guide/optimization_guide_service_factory.h"
 #include "ios/chrome/browser/passwords/ios_chrome_password_check_manager_factory.h"
 #include "ios/chrome/browser/passwords/ios_chrome_password_store_factory.h"
 #import "ios/chrome/browser/policy/policy_features.h"
@@ -137,6 +138,7 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   LanguageModelManagerFactory::GetInstance();
   ManagedBookmarkServiceFactory::GetInstance();
   ModelTypeStoreServiceFactory::GetInstance();
+  OptimizationGuideServiceFactory::GetInstance();
   SyncServiceFactory::GetInstance();
   ReadingListModelFactory::GetInstance();
   RealTimeUrlLookupServiceFactory::GetInstance();
---
+++ ios/chrome/browser/optimization_guide/optimization_guide_service.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OPTIMIZATION_GUIDE_OPTIMIZATION_GUIDE_SERVICE_H_
+#define IOS_CHROME_BROWSER_OPTIMIZATION_GUIDE_OPTIMIZATION_GUIDE_SERVICE_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+// A BrowserState keyed service that is used to own the underlying Optimization
+// Guide components.
+class OptimizationGuideService : public KeyedService {
+ public:
+  OptimizationGuideService();
+  ~OptimizationGuideService() override;
+
+  OptimizationGuideService(const OptimizationGuideService&) = delete;
+  OptimizationGuideService& operator=(const OptimizationGuideService&) = delete;
+};
+
+#endif  // IOS_CHROME_BROWSER_OPTIMIZATION_GUIDE_OPTIMIZATION_GUIDE_SERVICE_H_
