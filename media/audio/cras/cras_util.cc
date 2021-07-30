0728126536a81b5affe446d93eebfd5f111860e5
Yu-Hsuan Hsu <yuhsuan@chromium.org>
2021-07-30 02:39:28 +0000

    Fix the bug of virtual devices
    The virtual device "Built-in mic" is for internal mics not external
    mics.
    
    BUG=b:173113548
    
    Change-Id: I9200912ce45677a98d6d4c0c1df4ab3bbdf35939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060500
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#906980}

--- media/audio/cras/cras_util.cc
+++ media/audio/cras/cras_util.cc
@@ -48,6 +48,10 @@ bool IsForSimpleUsage(std::string type) {
          type == kUSB || type == kBluetooth || type == kAlsaLoopback;
 }
 
+bool IsInternalMic(std::string type) {
+  return type == kInternalMic || type == kFrontMic || type == kRearMic;
+}
+
 // Connects to the CRAS server.
 libcras_client* CrasConnect() {
   libcras_client* client;
@@ -141,7 +145,8 @@ void mergeDevices(CrasDevice& old_dev, CrasDevice& new_dev) {
              new_dev.node_type == kInternalSpeaker) {
     old_dev.name = kInternalOutputVirtualDevice;
     old_dev.node_type = "";
-  } else if (old_dev.node_type == kMic || new_dev.node_type == kMic) {
+  } else if (IsInternalMic(old_dev.node_type) ||
+             IsInternalMic(new_dev.node_type)) {
     old_dev.name = kInternalInputVirtualDevice;
     old_dev.node_type = "";
   } else {
