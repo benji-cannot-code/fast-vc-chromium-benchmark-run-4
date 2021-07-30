a20480e10863d5ec9bd8697970d0ba0e65802be8
Gauthier Ambard <gambard@chromium.org>
2021-07-30 13:31:35 +0000

    [iOS] Add test for FRE SignIn screen
    This CL adds tests for the FRE SignIn screen checking:
    - Not Signing In
    - Opening the screen with no account and adding one
    - Opening the screen with accounts and switching the selected one
    
    Fixed: 1233951
    Change-Id: I912aba4aa63f4a43de99b504cd6a6dc08df3b26c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060060
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907105}

--- ios/chrome/browser/ui/authentication/signin_matchers.mm
+++ ios/chrome/browser/ui/authentication/signin_matchers.mm
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "ios/chrome/browser/ui/authentication/signin_matchers.h"
+
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey_app_interface.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 
