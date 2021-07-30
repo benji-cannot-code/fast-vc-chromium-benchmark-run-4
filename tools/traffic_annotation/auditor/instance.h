c0e7391b4451418dab62acd5dee09d718547005c
Peter Kasting <pkasting@chromium.org>
2021-07-30 18:18:48 +0000

    Fix some instances of -Wdeprecated-copy: tools/
    Bug: 1213098
    Change-Id: Ib1f6943314633b506ae715ff7ff1f33cabbb09c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056864
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907191}

--- tools/traffic_annotation/auditor/instance.h
+++ tools/traffic_annotation/auditor/instance.h
@@ -157,6 +157,7 @@ class CallInstance : public InstanceBase {
  public:
   CallInstance();
   CallInstance(const CallInstance& other);
+  CallInstance& operator=(const CallInstance& other);
 
   // Deserializes an instance from serialized lines of text provided by the
   // extractor.
