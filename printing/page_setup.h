62f0ffe0fce6da93aee70d1bcc94f947847c744e
Alan Screen <awscreen@chromium.org>
2021-07-30 16:00:56 +0000

    Add printing PageMargins mojo type with mapping
    Print driver settings queries and document printing with the print
    backend service requires the ability to transfer PrintSettings over
    Mojo.  This requires types used by its members to also be mojoified.
    
    Add a mojo definition for printing::PageMargins as part of the overall
    mojofication for PrintSettings.
    
    Add this into a new //printing/mojom:printing_context build definition.
    This is needed as it must be separate from //printing/mojom, which would
    otherwise have a circular dependency with //printing.
    
    Bug: 809738
    Change-Id: I0574cb52abf211ee23bf493d7576faf90186071a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061552
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907146}

--- printing/page_setup.h
+++ printing/page_setup.h
@@ -15,6 +15,7 @@ namespace printing {
 class COMPONENT_EXPORT(PRINTING) PageMargins {
  public:
   PageMargins();
+  PageMargins(int header, int footer, int left, int right, int top, int bottom);
 
   void Clear();
 
