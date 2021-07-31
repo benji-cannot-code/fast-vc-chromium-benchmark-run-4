37cd9c345a7272df99af17399a429825a0f98691
Stephan Hartmann <stha09@googlemail.com>
2021-07-31 18:53:24 +0000

    IWYU: add atomic for std::atomic in time_override
    Bug: None
    Change-Id: Id2c0ba73fe10ec192090658afea6e3206de6d3c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3065459
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907392}

--- base/time/time_override.h
+++ base/time/time_override.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_TIME_TIME_OVERRIDE_H_
 #define BASE_TIME_TIME_OVERRIDE_H_
 
+#include <atomic>
+
 #include "base/base_export.h"
 #include "base/macros.h"
 #include "base/time/time.h"
