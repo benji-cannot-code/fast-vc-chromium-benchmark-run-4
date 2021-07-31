c3c943897d1ae041c7939f9339df7f783b4c2ceb
Chris Thompson <cthomp@chromium.org>
2021-07-31 00:32:18 +0000

    Exempt Chrome Apps <webview> from HTTPS-First Mode
    This fixes a crash caused by the HttpsOnlyModeTabHelper not being
    created for Chrome Apps <webview> contexts, and makes things a bit more
    robust by creating the TabHelper if it does not yet exist when the
    HttpsOnlyModeNavigationThrottle is created. This also exempts Chrome
    Apps <webview> contexts from HTTPS-First Mode, since they fall outside
    the domain of the feature as currently implemented (and because Apps
    have superpowers that could bypass this and other features already).
    Also includes new WebViewHttpsFirstModeTest regression tests for this
    crash for both HTTP and HTTPS page loads in <webview>.
    
    Bug: 1233889
    Change-Id: I6a2d5fb5325e5ae390cdb88f43e234cc9430c0d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059961
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907334}

--- chrome/browser/ssl/https_only_mode_upgrade_interceptor.cc
+++ chrome/browser/ssl/https_only_mode_upgrade_interceptor.cc
@@ -13,11 +13,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/security_interstitials/content/stateful_ssl_host_state_delegate.h"
 #include "content/public/browser/web_contents.h"
+#include "extensions/buildflags/buildflags.h"
 #include "net/base/url_util.h"
 #include "third_party/blink/public/mojom/loader/resource_load_info.mojom.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
 #include "url/url_util.h"
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "components/guest_view/browser/guest_view_base.h"
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 namespace {
 
@@ -57,13 +62,14 @@ void HttpsOnlyModeUpgradeInterceptor::MaybeCreateLoader(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   // If there isn't a BrowserContext/Profile for this, then just allow it.
-  if (!browser_context) {
+  Profile* profile = Profile::FromBrowserContext(browser_context);
+  if (!profile) {
     std::move(callback).Run({});
     return;
   }
 
   // Don't upgrade if the HTTPS-Only Mode setting isn't enabled.
-  auto* prefs = Profile::FromBrowserContext(browser_context)->GetPrefs();
+  auto* prefs = profile->GetPrefs();
   if (!prefs || !prefs->GetBoolean(prefs::kHttpsOnlyModeEnabled)) {
     std::move(callback).Run({});
     return;
@@ -76,12 +82,16 @@ void HttpsOnlyModeUpgradeInterceptor::MaybeCreateLoader(
     std::move(callback).Run({});
     return;
   }
-  Profile* profile =
-      Profile::FromBrowserContext(web_contents->GetBrowserContext());
-  if (!profile) {
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  // If this is a GuestView (e.g., Chrome Apps <webview>) then HTTPS-First Mode
+  // should not apply. See crbug.com/1233889 for more details.
+  if (guest_view::GuestViewBase::IsGuest(web_contents)) {
     std::move(callback).Run({});
     return;
   }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+
   // TODO(crbug.com/1228188) There might be a race condition where we
   // get here before there is a tab. If the issue was caused by web
   // contents, profile or tab helper being null, try to determine the
