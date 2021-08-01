fbd9ecb3666f132a60b0725f8fcda0edee78d7c6
Hiroshige Hayashizaki <hiroshige@chromium.org>
2021-08-01 00:38:44 +0000

    [WPT] Fix execution-order/116.html and 146.html
    116.html: `find-body.js` should output body's name.
    146.html: SVG's <script> elements ignore src attributes and
              use href instead.
    
    Fixes https://github.com/web-platform-tests/wpt/issues/20515.
    
    Bug: 1234521
    Change-Id: I69c2b661abf4a08e0911bf46a46754cab9a1ee10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064899
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907398}

--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/execution-timing/scripts/find-body.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/execution-timing/scripts/find-body.js
@@ -1,3 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-log('document.body: '+(document.body?'<BODY>':null));
+log(
+  'document.body: ' +
+  (document.body ? '<' + document.body.localName.toUpperCase() + '>' : null));
 var findBodyLoaded=true;
