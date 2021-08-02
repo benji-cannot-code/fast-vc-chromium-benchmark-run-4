1f1af563d2aefe729f084a3a460816b0edde380d
John Abd-El-Malek <jam@chromium.org>
2021-08-02 18:39:22 +0000

    Update NavigationCallback#onPageLanguageDetermined on trunk to 93 minimum version now that it's merged.
    Bug: 1221164
    Change-Id: I81f21fa4e90888f9234d2dd9601079909ba5ab62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067548
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907635}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/NavigationTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/NavigationTest.java
@@ -652,7 +652,7 @@ public class NavigationTest {
                 curRedirectedCount, Arrays.asList(Uri.parse(url), Uri.parse(finalUrl)));
     }
 
-    @MinWebLayerVersion(94)
+    @MinWebLayerVersion(93)
     @Test
     @SmallTest
     public void testGetPageInOnNavigationCompletedForIncompleteNavigation() throws Exception {
@@ -1630,7 +1630,7 @@ public class NavigationTest {
         assertEquals(2, mCallback.onCompletedCallback.getNavigationEntryOffset());
     }
 
-    @MinWebLayerVersion(94)
+    @MinWebLayerVersion(93)
     @Test
     @SmallTest
     public void testOnPageLanguageDetermined() throws Exception {
