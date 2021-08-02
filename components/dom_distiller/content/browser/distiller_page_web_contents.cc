03841ad50e201cdda5ce9e9790f27e78b01007e6
kouhei <kouhei@chromium.org>
2021-08-02 08:28:20 +0000

    DistillerPageWebContents: Remove usage of GetMainFrame
    This CL simply replaces `WebContents::GetMainFrame()` usages with
    `GetPrimaryPage().GetMainDocument()`. Simple replacement suffices, since
    the class operates on the WebContents it has created itself, which
    does not support the MPArch customer projects.
    
    Bug: 1190019
    Change-Id: Id4b7755b8ddcb412b1e871065b0ff02a6e2f397f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061876
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907476}

--- components/dom_distiller/content/browser/distiller_page_web_contents.cc
+++ components/dom_distiller/content/browser/distiller_page_web_contents.cc
@@ -91,8 +91,9 @@ void DistillerPageWebContents::DistillPageImpl(const GURL& url,
   if (source_page_handle_ && source_page_handle_->web_contents() &&
       source_page_handle_->web_contents()->GetLastCommittedURL() == url) {
     if (source_page_handle_->web_contents()
-            ->GetMainFrame()
-            ->IsDOMContentLoaded()) {
+            ->GetPrimaryPage()
+            .GetMainDocument()
+            .IsDOMContentLoaded()) {
       // Main frame has already loaded for the current WebContents, so execute
       // JavaScript immediately.
       ExecuteJavaScript();
@@ -144,8 +145,9 @@ gfx::Size DistillerPageWebContents::GetSizeForNewRenderView(
 
 void DistillerPageWebContents::DOMContentLoaded(
     content::RenderFrameHost* render_frame_host) {
-  if (render_frame_host ==
-      source_page_handle_->web_contents()->GetMainFrame()) {
+  if (render_frame_host == &source_page_handle_->web_contents()
+                                ->GetPrimaryPage()
+                                .GetMainDocument()) {
     ExecuteJavaScript();
   }
 }
@@ -163,9 +165,8 @@ void DistillerPageWebContents::DidFailLoad(
 }
 
 void DistillerPageWebContents::ExecuteJavaScript() {
-  content::RenderFrameHost* frame =
-      source_page_handle_->web_contents()->GetMainFrame();
-  DCHECK(frame);
+  content::RenderFrameHost& frame =
+      source_page_handle_->web_contents()->GetPrimaryPage().GetMainDocument();
   DCHECK_EQ(LOADING_PAGE, state_);
   state_ = EXECUTING_JAVASCRIPT;
   content::WebContentsObserver::Observe(nullptr);
@@ -174,7 +175,7 @@ void DistillerPageWebContents::ExecuteJavaScript() {
   source_page_handle_->web_contents()->Stop();
   DVLOG(1) << "Beginning distillation";
   RunIsolatedJavaScript(
-      frame, script_,
+      &frame, script_,
       base::BindOnce(&DistillerPageWebContents::OnWebContentsDistillationDone,
                      weak_factory_.GetWeakPtr(),
                      source_page_handle_->web_contents()->GetLastCommittedURL(),
