c2246f4e13524e019864ca4ff32532dae8bf8f40
Gavin Dodd <gavindodd@google.com>
2021-08-02 19:58:06 +0000

    shimless: Remove deprecated rmad.proto values.
    Enum values removed:
     - RmadErrorCode::RMAD_ERROR_NEED_REBOOT
     - ComponentType::kMainboardRework
    
    Change-Id: Ie0e2c869e46737c9b4de111eae495b3d9c74f8b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3063796
    Auto-Submit: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907672}

--- ash/webui/shimless_rma/mojom/shimless_rma_mojom_traits_unittest.cc
+++ ash/webui/shimless_rma/mojom/shimless_rma_mojom_traits_unittest.cc
@@ -114,8 +114,6 @@ TEST_F(ShimlessRmaMojoToProtoTest, ErrorsMatch) {
                                                 rmad::RmadErrorCode>(
       {{mojom::RmadErrorCode::kOk, rmad::RmadErrorCode::RMAD_ERROR_OK},
        {mojom::RmadErrorCode::kWait, rmad::RmadErrorCode::RMAD_ERROR_WAIT},
-       {mojom::RmadErrorCode::kNeedReboot,
-        rmad::RmadErrorCode::RMAD_ERROR_NEED_REBOOT},
        {mojom::RmadErrorCode::kExpectReboot,
         rmad::RmadErrorCode::RMAD_ERROR_EXPECT_REBOOT},
        {mojom::RmadErrorCode::kExpectShutdown,
@@ -193,10 +191,7 @@ TEST_F(ShimlessRmaMojoToProtoTest, ErrorsMatch) {
 TEST_F(ShimlessRmaMojoToProtoTest, RepairComponentsMatch) {
   constexpr auto enums =
       base::MakeFixedFlatMap<mojom::ComponentType, rmad::RmadComponent>(
-          {{mojom::ComponentType::kMainboardRework,
-            rmad::RmadComponent::RMAD_COMPONENT_MAINBOARD_REWORK},
-
-           {mojom::ComponentType::kAudioCodec,
+          {{mojom::ComponentType::kAudioCodec,
             rmad::RmadComponent::RMAD_COMPONENT_AUDIO_CODEC},
            {mojom::ComponentType::kBattery,
             rmad::RmadComponent::RMAD_COMPONENT_BATTERY},
