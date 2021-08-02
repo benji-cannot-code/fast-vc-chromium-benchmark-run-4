20de94d99842fc0422e96f91300b4939b5d382dd
Luciano Pacheco <lucmult@chromium.org>
2021-08-02 04:39:39 +0000

    Files SWA: Allow clipboard access for Files SWA
    The execCommand() fails for some commands in the SWA (System Web Apps)
    context.  This CL allow list Files app SWA for the
    AllowReadFromClipboard() check.
    
    Reducing the allow list to only:
    *  Files SWA
    *  AllowReadFromClipboard() which is used for the `paste` command.
    
    NOTE: This only allows list in the renderer side. In the browser side
    this is currently default-allowed for legacy reasons:
    https://bit.ly/3zQSVY1
    
    NOTE #2: This is a temporary workaround and the permanent solution is
    to migrate to Clipboard API which has support for properly granting
    permissions for SWAs.
    
    Bug: b/191831968 b/193686962 chromium:1233395
    Test: browser_tests --gtest_filter="Transfer/FilesAppBrowserTest*"
    Change-Id: I90326e77efab7947014abbb224ba890b3af01bc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2992804
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907449}

--- chrome/renderer/chrome_content_settings_agent_delegate.cc
+++ chrome/renderer/chrome_content_settings_agent_delegate.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/chrome_content_settings_agent_delegate.h"
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/webui/file_manager/url_constants.h"
+#endif
 #include "base/containers/contains.h"
 #include "chrome/common/ssl_insecure_content.h"
+#include "content/public/common/url_constants.h"
 #include "content/public/renderer/render_frame.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 
@@ -79,6 +83,10 @@ ChromeContentSettingsAgentDelegate::AllowReadFromClipboard() {
           extensions::mojom::APIPermissionID::kClipboardRead)) {
     return true;
   }
+
+  if (IsAllowListedSystemWebApp()) {
+    return true;
+  }
 #endif
   return absl::nullopt;
 }
@@ -138,6 +146,20 @@ bool ChromeContentSettingsAgentDelegate::IsPlatformApp() {
 #else
   return false;
 #endif
+}
+
+bool ChromeContentSettingsAgentDelegate::IsAllowListedSystemWebApp() {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  blink::WebLocalFrame* frame = render_frame_->GetWebFrame();
+  blink::WebSecurityOrigin origin = frame->GetDocument().GetSecurityOrigin();
+  // TODO(crbug.com/1233395): Migrate Files SWA to Clipboard API and remove this
+  // allow-list.
+  if (origin.Protocol().Ascii() == ::content::kChromeUIScheme &&
+      origin.Host().Utf8() == ::ash::file_manager::kChromeUIFileManagerHost) {
+    return true;
+  }
+#endif
+  return false;
 }
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
--- chrome/renderer/chrome_content_settings_agent_delegate.h
+++ chrome/renderer/chrome_content_settings_agent_delegate.h
@@ -53,6 +53,9 @@ class ChromeContentSettingsAgentDelegate
   // Whether the observed RenderFrame is for a platform app.
   bool IsPlatformApp();
 
+  // Whether the observed RenderFrame is an allow-listed System Web App.
+  bool IsAllowListedSystemWebApp();
+
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   // If |origin| corresponds to an installed extension, returns that extension.
   // Otherwise returns null.
