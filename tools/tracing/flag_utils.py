98f8703218e70a9c6268c8fa7dc5adc2acd1b8e3
Uwem Wilson <uwemwilson@google.com>
2021-08-02 22:03:50 +0000

    Modified view option to display trace after symbolization.
    This change modifies the code in profile_chrome_startup and
    symbolize_trace to display a trace after symbolization. Before it would
    display after a trace_file was produced.
    
    Bug: 1224717
    Change-Id: Ib4d2082a4b213a0ce4fcf3e8c3c4b971d1219f49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056772
    Reviewed-by: ssid <ssid@chromium.org>
    Commit-Queue: Uwem Wilson <uwemwilson@google.com>
    Cr-Commit-Position: refs/heads/master@{#907723}

--- tools/tracing/flag_utils.py
+++ tools/tracing/flag_utils.py
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 This script provides the necessary flags to symbolize proto traces.
 """
 
+import os
+import sys
 import optparse
 import logging
+import webbrowser
+import subprocess
 
 
 def SymbolizeOptions(parser):
