b94c9f8573acb9b867ead736e2b19cc0fcd1ad25
Callistus <callistus@google.com>
2021-08-02 03:18:10 +0000

    Update strings for Discover tab notification and suggestion chip.
    Bug: b/194967254
    Change-Id: Iabd1d197b5f2a8cdc8ee008ad43b3d16e90ceff2
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome,chromeos-betty-pi-arc-chrome
    Change-Id: Iabd1d197b5f2a8cdc8ee008ad43b3d16e90ceff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056226
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: dstockwell <dstockwell@google.com>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/master@{#907442}

--- chrome/browser/ui/app_list/search/app_list_search_browsertest.cc
+++ chrome/browser/ui/app_list/search/app_list_search_browsertest.cc
@@ -49,9 +49,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/help_app_ui/help_app_manager_factory.h"
 #include "chromeos/components/help_app_ui/search/search.mojom.h"
 #include "chromeos/components/help_app_ui/search/search_handler.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_navigation_observer.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/events/event_constants.h"
 
 using web_app::test::CrosapiParam;
@@ -189,7 +191,8 @@ IN_PROC_BROWSER_TEST_F(AppListSearchBrowserTest,
 
   ChromeSearchResult* result = FindResult("help-app://discover");
   ASSERT_TRUE(result);
-  EXPECT_EQ(base::UTF16ToASCII(result->title()), "Make your own game");
+  EXPECT_EQ(result->title(), l10n_util::GetStringUTF16(
+                                 IDS_HELP_APP_DISCOVER_TAB_SUGGESTION_CHIP));
   EXPECT_EQ(result->metrics_type(), ash::HELP_APP_DISCOVER);
 
   // Open the search result. This should open the help app at the expected url.
@@ -232,7 +235,8 @@ IN_PROC_BROWSER_TEST_F(AppListSearchBrowserTest,
 
   auto* result = FindResult("help-app://updates");
   ASSERT_TRUE(result);
-  EXPECT_EQ(base::UTF16ToASCII(result->title()), "What's new with Chrome OS");
+  EXPECT_EQ(result->title(),
+            l10n_util::GetStringUTF16(IDS_HELP_APP_WHATS_NEW_SUGGESTION_CHIP));
   EXPECT_EQ(result->metrics_type(), ash::HELP_APP_UPDATES);
   // Displayed in first position.
   EXPECT_EQ(result->position_priority(), 1.0f);
