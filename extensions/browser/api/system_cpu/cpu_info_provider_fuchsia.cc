a2d8f5912930243674dbddc0b01e5f425e524ada
Wez <wez@chromium.org>
2021-08-02 12:05:01 +0000

    [fuchsia] Add stubs for extensions integration points.
    Provide stubs for fine-grained CPU usage, removable storage, and
    native-messaging extensions APIs, under Fuchsia.
    
    Bug: 1233550, 1233555
    Change-Id: Ice9e82ccd4205dc7f353b30c4883b5f74c0476bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055958
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Benjamin Lerman <qsr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907510}

---
+++ extensions/browser/api/system_cpu/cpu_info_provider_fuchsia.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/browser/api/system_cpu/cpu_info_provider.h"
+
+namespace extensions {
+
+bool CpuInfoProvider::QueryCpuTimePerProcessor(
+    std::vector<api::system_cpu::ProcessorInfo>* infos) {
+  DCHECK(infos);
+  // TODO(crbug.com/1233555): Integrate with platform APIs, when available.
+  NOTIMPLEMENTED_LOG_ONCE();
+  return false;
+}
+
+}  // namespace extensions
