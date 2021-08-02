86d032253fe56861f5e7d559f1546cbf633b6aa8
Joon Ahn <joonbug@chromium.org>
2021-08-02 22:33:34 +0000

    shimless: component chip tooltip
    http://screen/9i3PMxVhWcBKGob
    
    Bug: 1198187
    Change-Id: Idad6f4c1b0697dd6f5f1aef7ee7ed012cf4caf1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3065038
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907740}

--- chrome/test/data/webui/chromeos/shimless_rma/repair_component_chip_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/repair_component_chip_test.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {RepairComponentChipElement} from 'chrome://shimless-rma/repair_component_chip.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
-import {flushTasks} from '../../test_util.m.js';
+import {flushTasks, isVisible} from '../../test_util.m.js';
 
 export function repairComponentChipElementTest() {
   /** @type {?RepairComponentChipElement} */
@@ -62,17 +62,25 @@ export function repairComponentChipElementTest() {
   test('ComponentToggleChecked', async () => {
     await initializeRepairComponentChip('cpu');
 
+    const checkIcon = component.shadowRoot.querySelector('#checkedIcon');
+
     await clickChip();
     assertTrue(component.checked);
+    assertTrue(isVisible(checkIcon));
 
     await clickChip();
     assertFalse(component.checked);
+    assertFalse(isVisible(checkIcon));
   });
 
   test('ComponentNoToggleOnDisabled', async () => {
     await initializeRepairComponentChip('cpu');
     component.disabled = true;
     await flushTasks();
+
+    const infoIcon = component.shadowRoot.querySelector('#infoIcon');
+    assertTrue(isVisible(infoIcon));
+
     await clickChip();
 
     assertFalse(component.checked);
