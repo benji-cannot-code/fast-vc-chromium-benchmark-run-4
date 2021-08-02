0162d1696ae1428e20abfb356f6e03c05cf8df8f
Gauthier Ambard <gambard@chromium.org>
2021-08-02 12:35:58 +0000

    [iOS] Remove Dark colorsets
    Those colorsets were used for iOS 12 compatibility. We can now remove
    them as iOS 12 have been deprecated.
    
    It also updates the last places where those colours were used, like
    the incognito steady view.
    
    Bug: 981889
    Change-Id: I160c310da3a95ec601b1c939e893765848b2c79a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067060
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#907517}

--- ios/chrome/browser/ui/browser_view/hider/browser_view_hider_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/hider/browser_view_hider_view_controller.mm
@@ -46,7 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.steadyView.translatesAutoresizingMaskIntoConstraints = NO;
   [self.view addSubview:self.steadyView];
   self.steadyView.colorScheme =
-      [LocationBarSteadyViewColorScheme incognitoScheme];
+      [LocationBarSteadyViewColorScheme standardScheme];
   self.steadyView.locationButton.enabled = NO;
 }
 
--- ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
@@ -143,8 +143,7 @@ const NSString* kScribbleOmniboxElementId = @"omnibox";
 - (void)setIncognito:(BOOL)incognito {
   _incognito = incognito;
   self.locationBarSteadyView.colorScheme =
-      incognito ? [LocationBarSteadyViewColorScheme incognitoScheme]
-                : [LocationBarSteadyViewColorScheme standardScheme];
+      [LocationBarSteadyViewColorScheme standardScheme];
 }
 
 - (void)setDispatcher:(id<ActivityServiceCommands,
