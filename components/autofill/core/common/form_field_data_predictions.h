fffa2aead5f9b40b40999cce949f65dc19f3a9c7
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-02 06:53:13 +0000

    [automated] Drop needless <vector> includes in /components/autofill/core/common
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=koerber@google.com
    
    Bug: 1233968
    Change-Id: I7bc099b944d4547e3c386ab7274e8375e39aa24d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056926
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/master@{#907464}

--- components/autofill/core/common/form_field_data_predictions.h
+++ components/autofill/core/common/form_field_data_predictions.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_AUTOFILL_CORE_COMMON_FORM_FIELD_DATA_PREDICTIONS_H_
 
 #include <string>
-#include <vector>
 
 #include "components/autofill/core/common/form_field_data.h"
 
