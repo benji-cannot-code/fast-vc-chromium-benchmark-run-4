244407113a147155c5202e8a0554e663d2e39ddd
Benjamin Lerman <qsr@chromium.org>
2021-08-02 08:55:49 +0000

    [fuchsia] Add stubs for //chrome/browser/media integration points.
    Bug: 1234750, 1211758
    
    Change-Id: I0dd231e06f8266938205290e8f444987a0613a30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062801
    Commit-Queue: Benjamin Lerman <qsr@chromium.org>
    Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907481}

---
+++ chrome/browser/media/router/discovery/discovery_network_list_fuchsia.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/router/discovery/discovery_network_list.h"
+
+#include "base/notreached.h"
+
+namespace media_router {
+
+std::vector<DiscoveryNetworkInfo> GetDiscoveryNetworkInfoList() {
+  // TODO(crbug.com/1234750)
+  NOTIMPLEMENTED_LOG_ONCE();
+  return {};
+}
+
+}  // namespace media_router
