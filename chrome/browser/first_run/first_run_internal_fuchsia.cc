96dd1d7de646d431c2a667d48433b5c7002641a7
Benjamin Lerman <qsr@chromium.org>
2021-08-02 09:42:59 +0000

    [fuchsia] Add stubs for //chrome/browser/first_run integration points.
    Bug: 1234776, 1211758
    
    Change-Id: I41a0520975c71ea8c81ce0ec0146a11ccfbaec14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3063039
    Commit-Queue: Benjamin Lerman <qsr@chromium.org>
    Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Carlos Pizano <cpu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907484}

---
+++ chrome/browser/first_run/first_run_internal_fuchsia.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/first_run/first_run_internal.h"
+
+#include "base/files/file_path.h"
+#include "base/notreached.h"
+
+namespace first_run {
+namespace internal {
+
+bool IsOrganicFirstRun() {
+  // We treat all installs as organic.
+  return true;
+}
+
+base::FilePath InitialPrefsPath() {
+  // TODO(crbug.com/1234776)
+  NOTIMPLEMENTED_LOG_ONCE();
+  return base::FilePath();
+}
+
+void DoPostImportPlatformSpecificTasks(Profile* profile) {
+  // TODO(crbug.com/1234776)
+  NOTIMPLEMENTED_LOG_ONCE();
+}
+
+bool ShowPostInstallEULAIfNeeded(installer::InitialPreferences* install_prefs) {
+  // The EULA is only handled on Windows.
+  return true;
+}
+
+}  // namespace internal
+}  // namespace first_run
