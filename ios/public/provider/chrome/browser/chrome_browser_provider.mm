d737e001e47a5d0387b54ef8d27dc050d7e928c8
Sylvain Defresne <sdefresne@chromium.org>
2021-08-02 09:50:25 +0000

    [ios] Remove obsolete `ChromeBrowserProvider::GetRiskData()` method
    The code has been obsoleted by `risk_data` API and the
    internal override removed. The public code can now be
    cleaned up.
    
    Bug: 1201182
    Change-Id: I69623228bc587739681dfe99b456a517ae2f2978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056873
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907487}

--- ios/public/provider/chrome/browser/chrome_browser_provider.mm
+++ ios/public/provider/chrome/browser/chrome_browser_provider.mm
@@ -71,10 +71,6 @@ ChromeBrowserProvider::GetChromeTrustedVaultService() {
   return nullptr;
 }
 
-std::string ChromeBrowserProvider::GetRiskData() {
-  return std::string();
-}
-
 UITextField* ChromeBrowserProvider::CreateStyledTextField() const {
   return nil;
 }
