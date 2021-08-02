9e5b2fd36f33fff41a2ef1f624136dc2647f9c88
Harvey Yang <chenghaoyang@chromium.org>
2021-08-02 18:34:18 +0000

    generic_sensor: Let iioservice support the Gravity sensor
    BUG=b:190923184
    TEST=unit tests and run on coachz
    
    Change-Id: Iaeac9de708523425fe0d45ce208e120d627d4564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035108
    Commit-Queue: Cheng-Hao Yang <chenghaoyang@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907631}

--- services/device/generic_sensor/platform_sensor_chromeos.cc
+++ services/device/generic_sensor/platform_sensor_chromeos.cc
@@ -101,6 +101,7 @@ void PlatformSensorChromeOS::OnSampleUpdated(
       break;
 
     case mojom::SensorType::ACCELEROMETER:
+    case mojom::SensorType::GRAVITY:
       DCHECK_EQ(channel_indices_.size(), 4u);
       reading.accel.x = GetScaledValue(sample.at(channel_indices_[0]));
       reading.accel.y = GetScaledValue(sample.at(channel_indices_[1]));
@@ -298,6 +299,10 @@ void PlatformSensorChromeOS::SetRequiredChannels() {
 
     case mojom::SensorType::MAGNETOMETER:
       axes_prefix = chromeos::sensors::mojom::kMagnetometerChannel;
+      break;
+
+    case mojom::SensorType::GRAVITY:
+      axes_prefix = chromeos::sensors::mojom::kGravityChannel;
       break;
 
     default:
--- services/device/generic_sensor/platform_sensor_provider_linux_base.cc
+++ services/device/generic_sensor/platform_sensor_provider_linux_base.cc
@@ -18,9 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace device {
 
-// static
 bool PlatformSensorProviderLinuxBase::IsFusionSensorType(
-    mojom::SensorType type) {
+    mojom::SensorType type) const {
   switch (type) {
     case mojom::SensorType::LINEAR_ACCELERATION:
     case mojom::SensorType::GRAVITY:
