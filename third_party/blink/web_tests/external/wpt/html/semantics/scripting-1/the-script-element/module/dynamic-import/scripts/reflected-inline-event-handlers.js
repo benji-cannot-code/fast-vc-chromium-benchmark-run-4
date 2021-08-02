718f573011fab204bffca8157953f39d8558b552
Hiroshige Hayashizaki <hiroshige@chromium.org>
2021-08-02 20:35:46 +0000

    [WPT] Distinguish Document base URL, Document URL, and inline script base URL in dynamic-import
    This exposes Chromium bugs:
    
    - The base URL of dynamic imports from event handlers is
      Document URL while it should be Document base URL
      (crbug.com/1235202)
    - The base URL of dynamic imports from setTimeout is
      Document base URL while it should be initiating script's base URL
      (crbug.com/1133238)
    
    Bug: 1133238, 1235202
    Change-Id: I8d9af692af29fbbe02bdacca2a4e320c2d565f80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3066340
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907686}

--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/dynamic-import/scripts/reflected-inline-event-handlers.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/module/dynamic-import/scripts/reflected-inline-event-handlers.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // import()s in an event handler are resolved relative to the document base.
-window.dummyDiv.setAttribute("onclick", `import('../imports-a.js?label=' + window.label).then(window.continueTest, window.errorTest)`);
+window.dummyDiv.setAttribute("onclick", `import('./imports-a.js?label=' + window.label).then(window.continueTest, window.errorTest)`);
 window.dummyDiv.onclick();
