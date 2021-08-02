20093a82bee2a8b0f42543ea1d91afbc8c0b2b15
Victor Costan <pwnall@chromium.org>
2021-08-02 13:43:16 +0000

    NativeIO: Remove unnecessary StorageType checks in NativeIOManager.
    QuotaManger guarantees to only call QuotaClients with the StorageTypes
    used to register the clients. NativeIO is only registered for temporary
    storage. (In fact, all storage APIs except for the deprecated FileSystem
    API only use the temporary type.)
    
    For example, NativeIOManager::GetStorageKeysForType() is called from
    NativeIOQuotaClient::GetStorageKeysForType(), which DCHECKS that the
    StorageType is temporary. The other modified methods follow the same
    pattern.
    
    This CL removes unnecessary checks in NativeIOManager and replaces them
    with DCHECKs, and removes a test that was covering the unnecessary
    checks.
    
    Change-Id: I98a64c52969d821e3b739c5ee489cd6de12e5455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064868
    Reviewed-by: Richard Stotz <rstz@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907529}

--- content/browser/native_io/native_io_manager.cc
+++ content/browser/native_io/native_io_manager.cc
@@ -240,10 +240,7 @@ void NativeIOManager::DeleteStorageKeyData(
 void NativeIOManager::GetStorageKeysForType(
     blink::mojom::StorageType type,
     storage::mojom::QuotaClient::GetStorageKeysForTypeCallback callback) {
-  if (type != blink::mojom::StorageType::kTemporary) {
-    std::move(callback).Run({});
-    return;
-  }
+  DCHECK_EQ(type, blink::mojom::StorageType::kTemporary);
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE,
       {
@@ -266,10 +263,7 @@ void NativeIOManager::GetStorageKeysForHost(
     blink::mojom::StorageType type,
     const std::string& host,
     storage::mojom::QuotaClient::GetStorageKeysForHostCallback callback) {
-  if (type != blink::mojom::StorageType::kTemporary) {
-    std::move(callback).Run({});
-    return;
-  }
+  DCHECK_EQ(type, blink::mojom::StorageType::kTemporary);
 
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE,
@@ -295,10 +289,7 @@ void NativeIOManager::GetStorageKeyUsage(
     const blink::StorageKey& storage_key,
     blink::mojom::StorageType type,
     storage::mojom::QuotaClient::GetStorageKeyUsageCallback callback) {
-  if (type != blink::mojom::StorageType::kTemporary) {
-    std::move(callback).Run(0);
-    return;
-  }
+  DCHECK_EQ(type, blink::mojom::StorageType::kTemporary);
 
   base::FilePath storage_key_root = RootPathForStorageKey(storage_key);
 
