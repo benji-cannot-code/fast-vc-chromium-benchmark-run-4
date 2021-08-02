1ba55abe364cc373ddf7d513f062c8261a428f65
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-02 19:24:14 +0000

    [automated] Drop needless <vector> includes in /components/variations
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=asvitkine@chromium.org
    
    Bug: 1233968
    Change-Id: I59aa614e87ee335b89daefd539c29095aa6109c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059485
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907653}

--- components/variations/entropy_provider.h
+++ components/variations/entropy_provider.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <functional>
 #include <random>
 #include <string>
-#include <vector>
 
 #include "base/compiler_specific.h"
 #include "base/component_export.h"
