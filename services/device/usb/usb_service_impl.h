a4d8e3fd3187b183171172e271807d053a6740d0
Reilly Grant <reillyg@chromium.org>
2021-08-02 21:42:40 +0000

    Remove libusb dependency from Windows
    The kNewUsbBackend flag has been enabled by default since M-87 and the
    about:flags entry has been removed in M-92. Now that M-93 has branched
    this code can be removed.
    
    Bug: 637404
    Change-Id: I18242aa752316716e33173db3a0a888bf38470b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531186
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907715}

--- services/device/usb/usb_service_impl.h
+++ services/device/usb/usb_service_impl.h
@@ -25,11 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/libusb/src/libusb/libusb.h"
 
-#if defined(OS_WIN)
-#include "base/scoped_observation.h"
-#include "device/base/device_monitor_win.h"
-#endif  // OS_WIN
-
 struct libusb_context;
 
 namespace device {
@@ -38,11 +33,7 @@ typedef struct libusb_context* PlatformUsbContext;
 
 class UsbDeviceImpl;
 
-class UsbServiceImpl final :
-#if defined(OS_WIN)
-    public DeviceMonitorWin::Observer,
-#endif  // OS_WIN
-    public UsbService {
+class UsbServiceImpl final : public UsbService {
  public:
   UsbServiceImpl();
   ~UsbServiceImpl() override;
@@ -50,14 +41,6 @@ class UsbServiceImpl final :
  private:
   // device::UsbService implementation
   void GetDevices(GetDevicesCallback callback) override;
-
-#if defined(OS_WIN)
-  // device::DeviceMonitorWin::Observer implementation
-  void OnDeviceAdded(const GUID& class_guid,
-                     const std::wstring& device_path) override;
-  void OnDeviceRemoved(const GUID& class_guid,
-                       const std::wstring& device_path) override;
-#endif  // OS_WIN
 
   void OnUsbContext(scoped_refptr<UsbContext> context);
 
@@ -95,14 +78,11 @@ class UsbServiceImpl final :
 
   // When available the device list will be updated when new devices are
   // connected instead of only when a full enumeration is requested.
-  // TODO(reillyg): Support this on all platforms. crbug.com/411715
-  bool hotplug_enabled_ = false;
   libusb_hotplug_callback_handle hotplug_handle_;
 
   // Enumeration callbacks are queued until an enumeration completes.
   bool enumeration_ready_ = false;
   bool enumeration_in_progress_ = false;
-  base::queue<std::wstring> pending_path_enumerations_;
   std::vector<GetDevicesCallback> pending_enumeration_callbacks_;
 
   // The map from libusb_device to UsbDeviceImpl. The key is a weak pointer to
@@ -119,11 +99,6 @@ class UsbServiceImpl final :
   // enumerated. This is a weak pointer to a libusb_device object owned by a
   // UsbDeviceImpl.
   std::set<libusb_device*> devices_being_enumerated_;
-
-#if defined(OS_WIN)
-  base::ScopedObservation<DeviceMonitorWin, DeviceMonitorWin::Observer>
-      device_observation_{this};
-#endif  // OS_WIN
 
   // This WeakPtr is used to safely post hotplug events back to the thread this
   // object lives on.
--- services/device/usb/usb_service_win.cc
+++ services/device/usb/usb_service_win.cc
@@ -5,14 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/device/usb/usb_service_win.h"
 
+// windows.h must be included first.
+#include <windows.h>
+
+#define INITGUID
+
+#include <devpkey.h>
 #include <objbase.h>
 #include <setupapi.h>
 #include <stdint.h>
 #include <usbiodef.h>
-#include "base/strings/string_piece_forward.h"
-
-#define INITGUID
-#include <devpkey.h>
 
 #include "base/bind.h"
 #include "base/containers/contains.h"
@@ -22,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_generic.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_piece_forward.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
