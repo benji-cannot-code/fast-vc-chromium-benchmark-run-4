6c552489d458775ebdc951026a9a86a7e506fd32
Sylvain Defresne <sdefresne@chromium.org>
2021-08-03 13:20:33 +0000

    [ios] Expose `app_distribution` API as free function
    Add free functions exposing the `app_distribution` API with the goal
    to replace `AppDistributionProvider` class. Do not convert the code
    to use the functions as it has no internal implementation yet.
    
    Bug: 1201182
    Change-Id: I6bdebeb0baf72fb06662c2f88d8ad7a13b0792af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062846
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907949}

---
+++ ios/public/provider/chrome/browser/app_distribution/test_app_distribution.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/public/provider/chrome/browser/app_distribution/app_distribution_api.h"
+
+namespace ios {
+namespace provider {
+
+std::string GetBrandCode() {
+  // Test has no brand code.
+  return std::string();
+}
+
+void ScheduleAppDistributionNotifications(
+    const scoped_refptr<network::SharedURLLoaderFactory>& url_loader_factory,
+    bool is_first_run) {
+  // Nothing to do for tests.
+}
+
+void CancelAppDistributionNotifications() {
+  // Nothing to do for tests.
+}
+
+void InitializeFirebase(base::Time install_date, bool is_first_run) {
+  // Nothing to do for tests.
+}
+
+}  // namespace provider
+}  // namespace ios
