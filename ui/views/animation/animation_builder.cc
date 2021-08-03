0769397ecc660835eba00c141a1f8473e458580e
Allen Bauer <kylixrd@chromium.org>
2021-08-03 21:37:18 +0000

    Add AnimationBuilder skeletal callback support
    Bug: 1236069
    Change-Id: I8ba67fe8df4e11f2ef136388f6b21be0adc1a747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069197
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908126}

--- ui/views/animation/animation_builder.cc
+++ ui/views/animation/animation_builder.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/animation/animation_builder.h"
 
+#include "base/containers/contains.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_sequence.h"
 #include "ui/compositor/layer_animator.h"
@@ -90,6 +91,16 @@ AnimationBuilder& AnimationBuilder::EndSequence() {
   return *this;
 }
 
+void AnimationBuilder::OnStarted(base::OnceClosure callback) {}
+
+void AnimationBuilder::OnEnded(base::OnceClosure callback) {}
+
+void AnimationBuilder::OnWillRepeat(base::RepeatingClosure callback) {}
+
+void AnimationBuilder::OnAborted(base::OnceClosure callback) {}
+
+void AnimationBuilder::OnScheduled(base::OnceClosure callback) {}
+
 void AnimationBuilder::CreateNewEntry(const AnimationKey& key) {
   auto new_sequence = std::make_unique<ui::LayerAnimationSequence>();
   new_sequence->set_is_repeating(is_sequence_repeating_);
@@ -105,6 +116,43 @@ void AnimationBuilder::AddAnimation(
   if (animation_sequences_.find(key) == animation_sequences_.end())
     CreateNewEntry(key);
   animation_sequences_[key].back()->AddElement(std::move(element));
+}
+
+AnimationBuilder::AnimationBuilderObserver::AnimationBuilderObserver() =
+    default;
+
+AnimationBuilder::AnimationBuilderObserver::~AnimationBuilderObserver() {
+  Reset();
+}
+
+void AnimationBuilder::AnimationBuilderObserver::ObserveAnimationSequence(
+    ui::LayerAnimationSequence* sequence) {
+  DCHECK(!base::Contains(sequences_, sequence,
+                         &base::WeakPtr<ui::LayerAnimationSequence>::get));
+  sequence->AddObserver(this);
+  sequences_.emplace_back(sequence->AsWeakPtr());
+}
+
+void AnimationBuilder::AnimationBuilderObserver::OnLayerAnimationStarted(
+    ui::LayerAnimationSequence* sequence) {}
+
+void AnimationBuilder::AnimationBuilderObserver::OnLayerAnimationEnded(
+    ui::LayerAnimationSequence* sequence) {}
+
+void AnimationBuilder::AnimationBuilderObserver::OnLayerAnimationWillRepeat(
+    ui::LayerAnimationSequence* sequence) {}
+
+void AnimationBuilder::AnimationBuilderObserver::OnLayerAnimationAborted(
+    ui::LayerAnimationSequence* sequence) {}
+
+void AnimationBuilder::AnimationBuilderObserver::OnLayerAnimationScheduled(
+    ui::LayerAnimationSequence* sequence) {}
+
+void AnimationBuilder::AnimationBuilderObserver::Reset() {
+  for (auto& sequence : sequences_) {
+    if (sequence.get())
+      sequence.get()->RemoveObserver(this);
+  }
 }
 
 }  // namespace views
