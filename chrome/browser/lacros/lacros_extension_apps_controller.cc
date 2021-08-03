f94287d8857aca6e83883f2d8ccf20c6ea4eca66
Erik Chen <erikchen@chromium.org>
2021-08-03 22:39:06 +0000

    lacros: Factor out some common code.
    This CL is a refactor with no intended behavior change. This CL
    pulls out a common method GetExtensionUninstallReason.
    
    Change-Id: If8e320fe1fd79b27852ee6fcb3ba6ff5424eb3e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059969
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908154}

--- chrome/browser/lacros/lacros_extension_apps_controller.cc
+++ chrome/browser/lacros/lacros_extension_apps_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lacros/lacros_extension_apps_controller.h"
 
 #include "chrome/browser/apps/app_service/app_icon_factory.h"
+#include "chrome/browser/apps/app_service/publishers/extension_apps_util.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/lacros/lacros_extension_apps_utility.h"
 #include "chrome/browser/ui/browser.h"
@@ -16,25 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/uninstall_reason.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_urls.h"
-
-namespace {
-
-extensions::UninstallReason GetUninstallReason(
-    apps::mojom::UninstallSource uninstall_source) {
-  switch (uninstall_source) {
-    case apps::mojom::UninstallSource::kUnknown:
-      // We assume if the reason is unknown that it's user inititated.
-      return extensions::UNINSTALL_REASON_USER_INITIATED;
-    case apps::mojom::UninstallSource::kAppList:
-    case apps::mojom::UninstallSource::kAppManagement:
-    case apps::mojom::UninstallSource::kShelf:
-      return extensions::UNINSTALL_REASON_USER_INITIATED;
-    case apps::mojom::UninstallSource::kMigration:
-      return extensions::UNINSTALL_REASON_MIGRATED;
-  }
-}
-
-}  // namespace
 
 LacrosExtensionAppsController::LacrosExtensionAppsController() = default;
 LacrosExtensionAppsController::~LacrosExtensionAppsController() = default;
@@ -57,7 +39,8 @@ void LacrosExtensionAppsController::Uninstall(
   std::u16string error;
   extensions::ExtensionSystem::Get(profile)
       ->extension_service()
-      ->UninstallExtension(extension_id, GetUninstallReason(uninstall_source),
+      ->UninstallExtension(extension_id,
+                           apps::GetExtensionUninstallReason(uninstall_source),
                            &error);
 
   if (report_abuse) {
