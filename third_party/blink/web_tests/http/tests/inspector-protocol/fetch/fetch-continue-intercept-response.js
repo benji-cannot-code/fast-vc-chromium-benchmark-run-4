17716deff38c4c3f99652ad398632e5e3e1e3246
Yury Semikhatsky <yurys@chromium.org>
2021-08-03 22:56:05 +0000

    DevTools: allow to skip response interception for intercepted request
    Consider the following use case: after analyzing intercepted request
    the client decides that it needs to resume and intercept response. It
    is not possible to enable response interception on the fly when
    request is already intercepted because stages of InterceptionJob are
    initialized in the constructor. To support this scenario a new option
    is used: both request and response interception are enabled ahead of
    time and if the client doesn't need to intercept certain responses it
    can pass the new option to Fetch.continueRequest and skip response
    interception for given request id.
    
    Bug: none
    Change-Id: Id6e7efb9d6b782926b71b232ece0990b3a45b53c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056432
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908175}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fetch/fetch-continue-intercept-response.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+    `Test interceptResponse=true parameter in request interceptor`);
+
+  await dp.Runtime.enable();
+  await dp.Network.enable();
+  await dp.Fetch.enable({patterns: [{requestStage: 'Request'}, {requestStage: 'Response'}]});
+
+  function isResponse(params) {
+    return "responseErrorReason" in params || "responseStatusCode" in params;
+  }
+
+  const navigatePromise = session.navigate(testRunner.url('./resources/hello-world.html'))
+  dp.Fetch.onRequestPaused(async event => {
+    const params = {
+      requestId: event.params.requestId,
+    };
+    if (isResponse(event.params)) {
+      testRunner.log(`Intercepted response`);
+    } else {
+      testRunner.log(`Intercepted request`);
+      params.interceptResponse = true;
+    }
+    await dp.Fetch.continueRequest(params);
+  });
+
+  const result = await navigatePromise
+  testRunner.log('Finished navigation');
+  testRunner.completeTest();
+})
