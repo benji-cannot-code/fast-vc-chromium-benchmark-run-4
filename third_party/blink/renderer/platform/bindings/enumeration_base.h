749b268c910b9f42fb63583e80e2279f746824c8
Yuki Shiino <yukishiino@chromium.org>
2021-08-03 10:02:40 +0000

    webrtc: Fix invalid access to RTCSessionDescriptionInit::type()
    RTCSessionDescriptionInit::type is an optional IDL enum type, so
    the existence of `type` should be checked beforehand (= hasType()).
    This patch adds the check of `hasType()`.
    
    Bug: 1235074
    Change-Id: I3ede29c2fad68241e7099d1d8054cbd68e5aaecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3066806
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907903}

--- third_party/blink/renderer/platform/bindings/enumeration_base.h
+++ third_party/blink/renderer/platform/bindings/enumeration_base.h
@@ -76,7 +76,7 @@ typename std::enable_if_t<
     bool>
 operator==(const EnumTypeClass& lhs, const EnumTypeClass& rhs) {
   DCHECK(!lhs.IsEmpty() && !rhs.IsEmpty());
-  return lhs.AsCStr() == rhs.AsCStr();
+  return lhs.AsEnum() == rhs.AsEnum();
 }
 
 template <typename EnumTypeClass>
@@ -84,8 +84,41 @@ typename std::enable_if_t<
     std::is_base_of<bindings::EnumerationBase, EnumTypeClass>::value,
     bool>
 operator!=(const EnumTypeClass& lhs, const EnumTypeClass& rhs) {
-  DCHECK(!lhs.IsEmpty() && !rhs.IsEmpty());
-  return lhs.AsCStr() != rhs.AsCStr();
+  return !(lhs == rhs);
+}
+
+template <typename EnumTypeClass>
+typename std::enable_if_t<
+    std::is_base_of<bindings::EnumerationBase, EnumTypeClass>::value,
+    bool>
+operator==(const EnumTypeClass& lhs, typename EnumTypeClass::Enum rhs) {
+  DCHECK(!lhs.IsEmpty());
+  return lhs.AsEnum() == rhs;
+}
+
+template <typename EnumTypeClass>
+typename std::enable_if_t<
+    std::is_base_of<bindings::EnumerationBase, EnumTypeClass>::value,
+    bool>
+operator==(typename EnumTypeClass::Enum lhs, const EnumTypeClass& rhs) {
+  DCHECK(!rhs.IsEmpty());
+  return lhs == rhs.AsEnum();
+}
+
+template <typename EnumTypeClass>
+typename std::enable_if_t<
+    std::is_base_of<bindings::EnumerationBase, EnumTypeClass>::value,
+    bool>
+operator!=(const EnumTypeClass& lhs, typename EnumTypeClass::Enum rhs) {
+  return !(lhs == rhs);
+}
+
+template <typename EnumTypeClass>
+typename std::enable_if_t<
+    std::is_base_of<bindings::EnumerationBase, EnumTypeClass>::value,
+    bool>
+operator!=(typename EnumTypeClass::Enum lhs, const EnumTypeClass& rhs) {
+  return !(lhs == rhs);
 }
 
 // Migration adapters
