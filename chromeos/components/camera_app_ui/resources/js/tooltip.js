4962efea9e5ae281c75304b5da437deb10279d7d
Pi-Hsun Shih <pihsun@chromium.org>
2021-08-03 10:03:46 +0000

    CCA: Remove target from CustomEvent arguments
    CustomEvent can't (and doesn't need to) specify target, since the target
    is always the element dispatchEvent is called on. Remove the target
    field.
    
    This drops the error of `cca.py tsc` from 134 to 132.
    
    Bug: b:172340451
    Test: cca.py tsc
    Test: Check the PTZ panel focus ring is still the same
    Test: Check the PTZ panel tooltip is still the same
    Change-Id: I1f7cadabb0b4ea9d4c77e1f8d916db3b2f43a824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068720
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907905}

--- chromeos/components/camera_app_ui/resources/js/tooltip.js
+++ chromeos/components/camera_app_ui/resources/js/tooltip.js
@@ -71,8 +71,8 @@ function show(element) {
   }
   wrapper.textContent = message;
   hovered = element;
-  const positionEvent = new CustomEvent(
-      TOOLTIP_POSITION_EVENT_NAME, {cancelable: true, target: hovered});
+  const positionEvent =
+      new CustomEvent(TOOLTIP_POSITION_EVENT_NAME, {cancelable: true});
   const doDefault = hovered.dispatchEvent(positionEvent);
   if (doDefault) {
     position(hovered.getBoundingClientRect());
