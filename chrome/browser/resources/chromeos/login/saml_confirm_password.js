c42dae55e7464c02ebf408805ace6450db3e79b4
Denis Kuznetsov <antrim@google.com>
2021-08-03 10:10:42 +0000

    Login : fix spinner on SAML password confirmation screen
    Bug: 1234787
    Change-Id: I9137b1facceac4b302582ad441de8e364d86190e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062805
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907907}

--- chrome/browser/resources/chromeos/login/saml_confirm_password.js
+++ chrome/browser/resources/chromeos/login/saml_confirm_password.js
@@ -83,10 +83,7 @@ Polymer({
     cr.ui.Oobe.showScreen({id: 'saml-confirm-password'});
   },
 
-  reset() {
-    if (this.$.cancelConfirmDlg.open)
-      this.$.cancelConfirmDlg.hideDialog();
-    this.setUIStep(UIState.PASSWORD);
+  resetFields() {
     this.$.passwordInput.invalid = false;
     this.$.passwordInput.value = '';
     if (this.isManualInput) {
@@ -94,6 +91,14 @@ Polymer({
       this.$$('#confirmPasswordInput').value = '';
     }
   },
+
+  reset() {
+    if (this.$.cancelConfirmDlg.open)
+      this.$.cancelConfirmDlg.hideDialog();
+    this.setUIStep(UIState.PASSWORD);
+    this.resetFields();
+  },
+
 
   onCancel_() {
     this.$.cancelConfirmDlg.showDialog();
@@ -125,10 +130,9 @@ Polymer({
         return;
       }
     }
-
     this.setUIStep(UIState.PROGRESS);
     this.callback_(this.$.passwordInput.value);
-    this.reset();
+    this.resetFields();
   },
 
   onDialogOverlayClosed_() {
