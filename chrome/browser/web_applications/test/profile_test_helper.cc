a0af3b2f01a973b4edb672d31aa36089aecc97f1
Eric Willigers <ericwilligers@chromium.org>
2021-08-03 01:06:35 +0000

    [Lacros] Test System Web Apps with CrosAPI enabled
    Bug: 1228119
    Change-Id: I6957f6aadc25bbf6b69303e52153949842e7caba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061964
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#907791}

--- chrome/browser/web_applications/test/profile_test_helper.cc
+++ chrome/browser/web_applications/test/profile_test_helper.cc
@@ -6,24 +6,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/profile_test_helper.h"
 
 #include "base/notreached.h"
-#include "build/chromeos_buildflags.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
+#include "chrome/common/chrome_features.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/user_names.h"
 #endif
 
 std::string TestProfileTypeToString(
-    const ::testing::TestParamInfo<TestProfileType>& info) {
-  switch (info.param) {
+    const ::testing::TestParamInfo<TestProfileParam>& info) {
+  std::string result;
+  switch (info.param.profile_type) {
     case TestProfileType::kRegular:
-      return "Regular";
+      result = "Regular";
+      break;
     case TestProfileType::kIncognito:
-      return "Incognito";
+      result = "Incognito";
+      break;
     case TestProfileType::kGuest:
-      return "Guest";
+      result = "Guest";
+      break;
   }
+
+  if (info.param.crosapi_state == web_app::test::CrosapiParam::kEnabled) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    result += "Crosapi";
+#else
+    NOTREACHED();
+#endif
+  }
+
+  return result;
 }
 
 void ConfigureCommandLineForGuestMode(base::CommandLine* command_line) {
@@ -37,4 +52,21 @@ void ConfigureCommandLineForGuestMode(base::CommandLine* command_line) {
 #else
   NOTREACHED();
 #endif
+}
+
+void InitCrosapiFeaturesForParam(
+    web_app::test::CrosapiParam crosapi_state,
+    base::test::ScopedFeatureList* scoped_feature_list) {
+  if (crosapi_state == web_app::test::CrosapiParam::kEnabled) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    scoped_feature_list->InitWithFeatures(
+        {chromeos::features::kLacrosSupport, features::kWebAppsCrosapi}, {});
+#else
+    NOTREACHED();
+#endif
+  } else {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    scoped_feature_list->InitWithFeatures({}, {features::kWebAppsCrosapi});
+#endif
+  }
 }
