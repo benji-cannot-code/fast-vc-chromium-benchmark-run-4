cc6e8773e47e630da5ec7c4cc91157a19bd7fe37
Joshua Pawlicki <waffles@chromium.org>
2021-08-03 19:16:39 +0000

    Updater: wire up some enterprise policies.
    Specifically adds handling for:
      AutoUpdateCheckPeriod
      DownloadPreference
      UpdateCheckSuppressedPeriod
    
    Also fixes a misplacement of some test code in
    /chrome/updater:base
    
    Bug: 1210393
    Change-Id: I5bb8c197180ce0d53805931907a443166706a93b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3066525
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908074}

--- chrome/updater/configurator.cc
+++ chrome/updater/configurator.cc
@@ -66,7 +66,10 @@ int Configurator::ServerKeepAliveSeconds() const {
 }
 
 int Configurator::NextCheckDelay() const {
-  return 5 * kDelayOneHour;
+  int minutes = 0;
+  return policy_service_->GetLastCheckPeriodMinutes(nullptr, &minutes)
+             ? minutes * kDelayOneMinute
+             : 5 * kDelayOneHour;
 }
 
 int Configurator::OnDemandDelay() const {
@@ -115,7 +118,10 @@ base::flat_map<std::string, std::string> Configurator::ExtraRequestParams()
 }
 
 std::string Configurator::GetDownloadPreference() const {
-  return {};
+  std::string preference;
+  return policy_service_->GetDownloadPreferenceGroupPolicy(nullptr, &preference)
+             ? preference
+             : std::string();
 }
 
 scoped_refptr<update_client::NetworkFetcherFactory>
