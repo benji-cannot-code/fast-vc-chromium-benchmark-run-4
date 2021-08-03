849076cf5fadb21189585ff321dfda4a44c650eb
Maksim Moskvitin <mmoskvitin@google.com>
2021-08-03 08:46:22 +0000

    Revert "Reland "[Passwords] ChromeBrowsingDataRemoverDelegateTest uses PwdStoreInterface""
    This reverts commit 90b828a389e83f3e38a11028db4383143aeb9350.
    
    Reason for revert: causes tests failure on Linux CFI builder (see crbug.com/1235972).
    
    Original change's description:
    > Reland "[Passwords] ChromeBrowsingDataRemoverDelegateTest uses PwdStoreInterface"
    >
    > This is a reland of 575271e6977a9931a2a963efd859b90c443ec3db
    >
    > Original change's description:
    > > [Passwords] ChromeBrowsingDataRemoverDelegateTest uses PwdStoreInterface
    > >
    > > This CL switches ChromeBrowsingDataRemoverDelegateTest to use
    > > MockPasswordStoreInterface instead of MockPasswordStore.
    > >
    > > This requires:
    > > 1- Introducing new method in the PasswordStoreFactory to  create
    > > the interface.
    > > 2- Updating bunch of Sync code to use PasswordStoreInterface instead
    > > of PasswordStore.
    > >
    > > Bug: 1218413
    > > Change-Id: I9ea6ef2b488b122b07b50bbbc5dc629e34be960f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3031068
    > > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > > Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#907092}
    >
    > Bug: 1218413
    > Change-Id: I8e51c3d30c11a264d7321f055f25b6875288bff6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064610
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#907536}
    
    Bug: 1218413
    Change-Id: If211b5516bdf4c10b349b2faf7cec27bdd321de9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3065466
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Owners-Override: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#907886}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -44,30 +44,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using password_manager::PasswordStore;
-using password_manager::PasswordStoreInterface;
 
-// TODO(crbug.com/1218413): Delete this method when the migration to
-// PasswordStoreInterface is complete and rename the method below to
-// GetForProfile.
 // static
 scoped_refptr<PasswordStore> PasswordStoreFactory::GetForProfile(
     Profile* profile,
     ServiceAccessType access_type) {
-  return base::WrapRefCounted(static_cast<PasswordStore*>(
-      GetInterfaceForProfile(profile, access_type).get()));
-}
-
-// static
-scoped_refptr<PasswordStoreInterface>
-PasswordStoreFactory::GetInterfaceForProfile(Profile* profile,
-                                             ServiceAccessType access_type) {
   // |profile| gets always redirected to a non-Incognito profile below, so
   // Incognito & IMPLICIT_ACCESS means that incognito browsing session would
   // result in traces in the normal profile without the user knowing it.
   if (access_type == ServiceAccessType::IMPLICIT_ACCESS &&
       profile->IsOffTheRecord())
     return nullptr;
-  return base::WrapRefCounted(static_cast<PasswordStoreInterface*>(
+  return base::WrapRefCounted(static_cast<password_manager::PasswordStore*>(
       GetInstance()->GetServiceForBrowserContext(profile, true).get()));
 }
 
