75483bf40c726f4326c2c97f1d26fc9e3d0853dd
Alice Gong <alicego@google.com>
2021-08-04 19:03:41 +0000

    Reland "Add reroute_info into download in-progress db."
    This reverts commit 98245f2e42d03f108c61fa1483f19065f2ffc3b3.
    
    Reason for revert: deps on download_shelf mojom bindings
    
    Original change's description:
    > Revert "Add reroute_info into download in-progress db."
    >
    > This reverts commit bd9851d3b929544f5e4dd91265ccad5ce203fad7.
    >
    > Reason for revert:
    >
    > Findit (https://goo.gl/kROfz5) identified CL at revision 908141 as the
    > culprit for failures in the build cycles as shown on:
    > https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2JkOTg1MWQzYjkyOTU0NGY1ZTRkZDkxMjY1Y2NhZDVjZTIwM2ZhZDcM
    >
    > Sample Failed Build: https://ci.chromium.org/b/8839877754479126288
    >
    > Sample Failed Step: compile
    >
    > Original change's description:
    > > Add reroute_info into download in-progress db.
    > > 
    > > + New field reroute_info in in_progress_info.h
    > > ~ In download_db_cache.cc: Update ShouldUpdateHistory() to check for reroute_info diff.
    > > ~ In download_utils.cc: Update CreateDownloadDBEntryFromItem() to take reroute_info into InProgressInfo.
    > > 
    > > Bug: 1203753
    > > Change-Id: I907614e03e39b8d2c10b35c6a83619eec57cc422
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056776
    > > Reviewed-by: Owen Min <zmin@chromium.org>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Reviewed-by: Min Qin <qinmin@chromium.org>
    > > Commit-Queue: Avi Drissman <avi@chromium.org>
    > > Auto-Submit: Alice Gong <alicego@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#908141}
    >
    >
    > Change-Id: I6c78c3587460ad5eb29f83afab1ba6b22f600a8a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1203753
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069729
    > Owners-Override: Jit Yao Yap <jityao@google.com>
    > Reviewed-by: Jit Yao Yap <jityao@google.com>
    > Commit-Queue: Jit Yao Yap <jityao@google.com>
    > Cr-Commit-Position: refs/heads/master@{#908368}
    
    Bug: 1203753
    Change-Id: I2b34d57a0da840843a862ca5f7d75dcfa9a3b5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067770
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Alice Gong <alicego@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Alice Gong <alicego@google.com>
    Cr-Commit-Position: refs/heads/master@{#908550}

--- chrome/browser/download/notification/download_item_notification_unittest.cc
+++ chrome/browser/download/notification/download_item_notification_unittest.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/mock_download_item.h"
-#include "components/enterprise/common/proto/download_item_reroute_info.pb.h"
+#include "components/enterprise/common/download_item_reroute_info.h"
 #include "content/public/browser/download_item_utils.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/mock_download_manager.h"
