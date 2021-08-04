757d195b3406ab57c2437089deebea56971b68b1
Xiaohan Wang <xhwang@chromium.org>
2021-08-04 00:32:39 +0000

    [CodeHealth] Remove NOTIFICATION_LOAD_START in ChromeMetricsServiceClient
    Bug: 1174762, 1174764
    Test: Existing tests pass; also manually tested.
    Change-Id: Id123ce5c2ea31c1ad1d9c7dbe3ecb6a54b70705b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043687
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908241}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -930,10 +930,6 @@ void ChromeMetricsServiceClient::RecordCommandLineMetrics() {
 }
 
 bool ChromeMetricsServiceClient::RegisterForNotifications() {
-  registrar_.Add(this, content::NOTIFICATION_LOAD_START,
-                 content::NotificationService::AllSources());
-  registrar_.Add(this, content::NOTIFICATION_LOAD_STOP,
-                 content::NotificationService::AllSources());
   registrar_.Add(this, content::NOTIFICATION_RENDERER_PROCESS_CLOSED,
                  content::NotificationService::AllSources());
   registrar_.Add(this, content::NOTIFICATION_RENDER_WIDGET_HOST_HANG,
@@ -1022,8 +1018,6 @@ void ChromeMetricsServiceClient::Observe(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   switch (type) {
-    case content::NOTIFICATION_LOAD_STOP:
-    case content::NOTIFICATION_LOAD_START:
     case content::NOTIFICATION_RENDERER_PROCESS_CLOSED:
     case content::NOTIFICATION_RENDER_WIDGET_HOST_HANG:
       metrics_service_->OnApplicationNotIdle();
@@ -1044,6 +1038,10 @@ void ChromeMetricsServiceClient::Observe(
     default:
       NOTREACHED();
   }
+}
+
+void ChromeMetricsServiceClient::LoadingStateChanged(bool /*is_loading*/) {
+  metrics_service_->OnApplicationNotIdle();
 }
 
 void ChromeMetricsServiceClient::OnURLOpenedFromOmnibox(OmniboxLog* log) {
--- components/metrics/metrics_service_client.h
+++ components/metrics/metrics_service_client.h
@@ -117,6 +117,9 @@ class MetricsServiceClient {
   // data.
   virtual bool ShouldStartUpFastForTesting() const;
 
+  // Called when loading state changed, e.g. start/stop loading.
+  virtual void LoadingStateChanged(bool is_loading) {}
+
   // Called on plugin loading errors.
   virtual void OnPluginLoadingError(const base::FilePath& plugin_path) {}
 
