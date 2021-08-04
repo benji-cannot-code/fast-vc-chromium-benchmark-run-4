fbd6b8c2ec4225829f4eae644d932347fa37110f
Gavin Dodd <gavindodd@google.com>
2021-08-04 21:14:54 +0000

    shimless: Implement GetCurrentOsVersion
    Return the real Chrome OS version using
    chromeos::version_loader::GetVersion
    
          update screen
    
    Bug: 1198187
    Test: Run device build and check correct version is displayed in chrome
    Change-Id: I31240ad9e9c87b9796acaaed9f9eb922e6b418c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061752
    Reviewed-by: Joon Ahn <joonbug@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Auto-Submit: Gavin Dodd <gavindodd@google.com>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908614}

--- ash/webui/shimless_rma/backend/shimless_rma_service.cc
+++ ash/webui/shimless_rma/backend/shimless_rma_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "chromeos/dbus/rmad/rmad.pb.h"
 #include "chromeos/dbus/rmad/rmad_client.h"
+#include "chromeos/dbus/util/version_loader.h"
 #include "components/qr_code_generator/qr_code_generator.h"
 
 using chromeos::network_config::mojom::ConnectionStateType;
@@ -122,8 +123,9 @@ void ShimlessRmaService::NetworkSelectionComplete(
 
 void ShimlessRmaService::GetCurrentOsVersion(
     GetCurrentOsVersionCallback callback) {
-  // TODO(gavindodd): Get actual Chrome version.
-  std::move(callback).Run("Chrome OS 0.0.0.0");
+  // TODO(gavindodd): Decide whether to use full or short Chrome version.
+  std::move(callback).Run(chromeos::version_loader::GetVersion(
+      chromeos::version_loader::VERSION_FULL));
 }
 
 void ShimlessRmaService::CheckForOsUpdates(CheckForOsUpdatesCallback callback) {
