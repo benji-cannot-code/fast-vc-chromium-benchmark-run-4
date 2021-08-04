26ce26b330e937179013cfb7e008c9d843b02c76
Daniel Rubery <drubery@chromium.org>
2021-08-04 01:28:57 +0000

    Obsolete SBClientDownload.ExtractDmgFeaturesTimeMedium
    This metric is not being actively monitored, and has always remained
    acceptably low.
    
    Fixed: 1217248
    Change-Id: I04d72f814a1ee5d332341a47cdce66c8468971e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069378
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908275}

--- chrome/browser/safe_browsing/download_protection/file_analyzer.cc
+++ chrome/browser/safe_browsing/download_protection/file_analyzer.cc
@@ -240,7 +240,6 @@ void FileAnalyzer::StartExtractDmgFeatures() {
                           weakptr_factory_.GetWeakPtr()),
       LaunchFileUtilService());
   dmg_analyzer_->Start();
-  dmg_analysis_start_time_ = base::TimeTicks::Now();
 }
 
 void FileAnalyzer::ExtractFileOrDmgFeatures(
@@ -278,9 +277,6 @@ void FileAnalyzer::OnDmgAnalysisFinished(
   } else {
     results_.type = ClientDownloadRequest::MAC_ARCHIVE_FAILED_PARSING;
   }
-
-  UMA_HISTOGRAM_MEDIUM_TIMES("SBClientDownload.ExtractDmgFeaturesTimeMedium",
-                             base::TimeTicks::Now() - dmg_analysis_start_time_);
 
   std::move(callback_).Run(std::move(results_));
 }
