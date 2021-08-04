134171fa68122e81b8f2c23b12fb902eda4b39cd
Kyle Milka <kmilka@chromium.org>
2021-08-04 18:47:01 +0000

    [DesktopScreenshot] Copy to clipboard
    Automatically copy the image to the clipboard after
    it is captured.
    
    Bug: 1235845
    Change-Id: Ica6faf275cbe41d7e08f71644c76b7fb1efb2b34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069077
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908545}

--- chrome/browser/ui/sharing_hub/screenshot/screenshot_captured_bubble_controller.cc
+++ chrome/browser/ui/sharing_hub/screenshot/screenshot_captured_bubble_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/base/clipboard/scoped_clipboard_writer.h"
 
 namespace sharing_hub {
 
@@ -30,6 +31,8 @@ ScreenshotCapturedBubbleController* ScreenshotCapturedBubbleController::Get(
 void ScreenshotCapturedBubbleController::ShowBubble(
     const image_editor::ScreenshotCaptureResult& image) {
   const gfx::Image& captured_image = image.image;
+  ui::ScopedClipboardWriter(ui::ClipboardBuffer::kCopyPaste)
+      .WriteImage(*captured_image.ToSkBitmap());
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents_);
   browser->window()->ShowScreenshotCapturedBubble(web_contents_, captured_image,
                                                   this);
