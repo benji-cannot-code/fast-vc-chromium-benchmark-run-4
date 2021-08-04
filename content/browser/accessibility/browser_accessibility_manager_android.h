b7b12b61961188317c826b93412d5ecc0c349fcd
Mark Schillaci <mschillaci@google.com>
2021-08-04 17:38:37 +0000

    Add a set to track already cleared accessibility nodes on Android per update
    This CL makes an update to the accessibility code for Android.
    
    With this CL we add a new unordered_set to the
    browser_accessibility_manager_android to track which nodes have been
    cleared during each atomic update. The AccessibilityNodeInfo object
    cache is stored in the Java-side code and is cleared based on events
    in the tree. There is no need to clear a node more than once per
    atomic update, and so adding the values to an unordered_set and
    checking their presence before each call to clear we can
    eliminate superfluous calls. We clear the set of cleared nodes at
    the end of each atomic update.
    
    AX-Relnotes: N/A
    Bug: N/A
    Change-Id: I4da690ba0ff37ba666b2ae5de5a8813051614160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069677
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/master@{#908508}

--- content/browser/accessibility/browser_accessibility_manager_android.h
+++ content/browser/accessibility/browser_accessibility_manager_android.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_ACCESSIBILITY_BROWSER_ACCESSIBILITY_MANAGER_ANDROID_H_
 #define CONTENT_BROWSER_ACCESSIBILITY_BROWSER_ACCESSIBILITY_MANAGER_ANDROID_H_
 
+#include <unordered_set>
 #include <utility>
 
 #include "content/browser/accessibility/browser_accessibility_manager.h"
@@ -162,6 +163,11 @@ class CONTENT_EXPORT BrowserAccessibilityManagerAndroid
   // any node uses touch passthrough in any frame. See comment next to
   // any_node_uses_touch_passthrough(), above, for details.
   bool touch_passthrough_enabled_ = false;
+
+  // An unordered_set of |unique_id| values for nodes cleared from the cache
+  // with each atomic update to prevent superfluous cache clear calls.
+  std::unordered_set<int32_t> nodes_already_cleared_ =
+      std::unordered_set<int32_t>();
 
   DISALLOW_COPY_AND_ASSIGN(BrowserAccessibilityManagerAndroid);
 };
