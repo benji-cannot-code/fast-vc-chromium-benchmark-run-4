3c630b0f5379ac25ddcc6a94ee34d2d7264a1c53
Koji Ishii <kojii@chromium.org>
2021-08-04 03:24:10 +0000

    [block-in-inline] Implement MinMax
    This patch implements |ComputeContentSize| for block-in-inline,
    using the shared logic for atomic inlines.
    
    Fixes computing MinMax for:
      fast/css-generated-content/015.html
    though the test still fails due to incorrect margin collapsing
    which ikilpatrick@ is working on.
    
    Bug: 716930
    Change-Id: I7cf19a62c485afb5e5233a62c04cc02d59463f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3070099
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908308}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
@@ -1728,7 +1728,12 @@ static LayoutUnit ComputeContentSize(
           // NGInlineItem.
           continue;
         }
-        if (item.Type() == NGInlineItem::kAtomicInline) {
+#if DCHECK_IS_ON()
+        if (item.Type() == NGInlineItem::kBlockInInline)
+          DCHECK(line_info.HasForcedBreak());
+#endif
+        if (item.Type() == NGInlineItem::kAtomicInline ||
+            item.Type() == NGInlineItem::kBlockInInline) {
           // The max-size for atomic inlines are cached in |max_size_cache|.
           unsigned item_index = &item - items_data.items.begin();
           position += max_size_cache[item_index];
@@ -1748,10 +1753,6 @@ static LayoutUnit ComputeContentSize(
             continue;
           }
         }
-#if DCHECK_IS_ON()
-        if (item.Type() == NGInlineItem::kBlockInInline)
-          DCHECK(line_info.HasForcedBreak());
-#endif
         position += result.inline_size;
       }
       // Compute the forced break after all results were handled, because
