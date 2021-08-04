a1fcf5d8cea3dd121c43a9ab2e4862e03f8aaaef
Keren Zhu <kerenzhu@chromium.org>
2021-08-04 20:56:06 +0000

    Use struct for AnimationKey
    Bug: 1236069
    Change-Id: I2a0919f5f060ef96f8b765067f19bef77ad1153b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071862
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908608}

--- ui/views/animation/animation_builder.h
+++ ui/views/animation/animation_builder.h
@@ -6,12 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_ANIMATION_ANIMATION_BUILDER_H_
 #define UI_VIEWS_ANIMATION_ANIMATION_BUILDER_H_
 
-#include <map>
 #include <memory>
+#include <tuple>
 #include <utility>
 #include <vector>
 
 #include "base/callback_forward.h"
+#include "base/containers/flat_map.h"
 #include "base/scoped_observation.h"
 #include "ui/compositor/layer_animation_element.h"
 #include "ui/compositor/layer_animation_observer.h"
@@ -59,9 +60,18 @@ class VIEWS_EXPORT AnimationBuilder {
   void OnScheduled(base::OnceClosure callback);
 
  private:
-  // We may want to change this to our own struct.
-  using AnimationKey =
-      std::pair<View*, ui::LayerAnimationElement::AnimatableProperty>;
+  struct AnimationKey {
+    View* view;
+    ui::LayerAnimationElement::AnimatableProperty property;
+
+    bool operator==(const AnimationKey& key) const {
+      return std::tie(view, property) == std::tie(key.view, key.property);
+    }
+
+    bool operator<(const AnimationKey& key) const {
+      return std::tie(view, property) < std::tie(key.view, key.property);
+    }
+  };
 
   class AnimationBuilderObserver : ui::LayerAnimationObserver {
    public:
@@ -92,8 +102,8 @@ class VIEWS_EXPORT AnimationBuilder {
   void AddAnimation(const AnimationKey& key,
                     std::unique_ptr<ui::LayerAnimationElement> element);
 
-  std::map<AnimationKey,
-           std::vector<std::unique_ptr<ui::LayerAnimationSequence>>>
+  base::flat_map<AnimationKey,
+                 std::vector<std::unique_ptr<ui::LayerAnimationSequence>>>
       animation_sequences_;
 
   base::TimeDelta duration_ = base::TimeDelta::FromSeconds(1);
