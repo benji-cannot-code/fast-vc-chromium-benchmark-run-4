6a71ea639e17b2e2ec8149d6ef74b8865c7e0169
Zhiyuan Cai <zhiyuancai@chromium.org>
2021-08-04 17:46:32 +0000

    [MerchantTrust] Improve trust signal triggering logic
    Currently some merchant sites require multiple navigations to trigger
    the trust signal, while some sites will trigger the signal when user
    navigate to them at the first time. This CL improves the triggering
    logic to avoid such conditions.
    
    Bug: 1230876
    Change-Id: I8517ce61adc2669369ae2ee18df787ec8e519c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3042442
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908515}

--- chrome/browser/commerce/merchant_viewer/android/java/src/org/chromium/chrome/browser/merchant_viewer/MerchantTrustMessageContext.java
+++ chrome/browser/commerce/merchant_viewer/android/java/src/org/chromium/chrome/browser/merchant_viewer/MerchantTrustMessageContext.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.merchant_viewer;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import org.chromium.content_public.browser.NavigationHandle;
 import org.chromium.content_public.browser.WebContents;
@@ -24,11 +25,26 @@ class MerchantTrustMessageContext {
         mWebContents = webContents;
     }
 
-    /** Returns the host name for which the message is intended to be shown. */
+    /**
+     * Returns the host name for which the message is intended to be shown. Can return null if the
+     * navigationHandle or its GURL is null.
+     */
+    @Nullable
     String getHostName() {
         return (mNavigationHandle == null || mNavigationHandle.getUrl() == null)
-                ? ""
+                ? null
                 : mNavigationHandle.getUrl().getHost();
+    }
+
+    /**
+     * Returns the URL for which the message is intended to be shown. Can return null if the
+     * navigationHandle or its GURL is null.
+     */
+    @Nullable
+    String getUrl() {
+        return (mNavigationHandle == null || mNavigationHandle.getUrl() == null)
+                ? null
+                : mNavigationHandle.getUrl().getSpec();
     }
 
     /* Returns the {@link WebContentns} for which the message is intended to be shown. */
