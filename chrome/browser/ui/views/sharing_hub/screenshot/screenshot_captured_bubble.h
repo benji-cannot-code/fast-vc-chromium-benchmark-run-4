2006bb45a4603dede2f07d9b1a82c14a14077d9f
Kyle Milka <kmilka@chromium.org>
2021-08-04 22:41:37 +0000

    [DesktopScreenshots] Remove share button
    For the initial feature we decided against a share button.
    Sharing images is not currently supported in the sharing hub
    and is technically complex to support.
    
    Bug: 1236218
    Change-Id: I52257c4e91d93fd43e21f655cf3b83c9fa20f8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072719
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908654}

--- chrome/browser/ui/views/sharing_hub/screenshot/screenshot_captured_bubble.h
+++ chrome/browser/ui/views/sharing_hub/screenshot/screenshot_captured_bubble.h
@@ -52,8 +52,6 @@ class ScreenshotCapturedBubble : public LocationBarBubbleDelegateView {
 
   void EditButtonPressed();
 
-  void ShareButtonPressed();
-
   const gfx::Image& image_;
 
   content::WebContents* web_contents_;
@@ -62,7 +60,6 @@ class ScreenshotCapturedBubble : public LocationBarBubbleDelegateView {
   views::ImageView* image_view_ = nullptr;
   views::MdTextButton* download_button_ = nullptr;
   views::LabelButton* edit_button_ = nullptr;
-  views::LabelButton* share_button_ = nullptr;
 };
 
 }  // namespace sharing_hub
