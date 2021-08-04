4cab3cd0e59246e2fda9197c8994bc51dfcefe7c
Martin Robinson <mrobinson@igalia.com>
2021-08-04 10:32:17 +0000

    Re-land: Fire live region events when a node is removed.
    Adds an AXLiveRegionTracker class to keep track of live regions
    in an AXTree. Uses it to fix a bug where we weren't firing the
    LIVE_REGION_CHANGED event on the live root when a node was removed,
    only when a node was added or changed.
    
    I'm going to follow this up with code that optionally computes
    the text of a live region change, that we can use on Android,
    Chrome OS, and some older versions of macOS. So AXLiveRegionTracker
    is simple now, but it will be a convenient place to put that
    logic.
    
    Originally landed as https://crrev.com/c/1988742, but reverted due to
    a UAF. The issue was that node deletion always called
    FireLiveRegionEvents for the deleted node. In the case that the
    deleted node was live region root, this would trigger a
    LIVE_REGION_CHANGED on the deleted node. Instead of calling
    FireLiveRegionEvents for deleted nodes, this version will only trigger
    a LIVE_REGION_CHANGED event for the root on deletion and only when the
    deleted node is not itself the root of the live region.
    
    Bug: 560599, 930763
    Change-Id: I3fbcd9c122a55e7a59f6cfbe61a116409cefaba6
    AX-RelNotes: Fix an issue where VoiceOver failed to announce cleared live regions that were filled with the previous text.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3013676
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908399}

--- ui/accessibility/ax_event_generator.h
+++ ui/accessibility/ax_event_generator.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <bitset>
 #include <map>
+#include <memory>
 #include <ostream>
 #include <set>
 #include <string>
@@ -20,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_tree_observer.h"
 
 namespace ui {
+
+class AXLiveRegionTracker;
 
 // Subclass of AXTreeObserver that automatically generates AXEvents to fire
 // based on changes to an accessibility tree.  Every platform
@@ -341,6 +344,9 @@ class AX_EXPORT AXEventGenerator : public AXTreeObserver {
   // Please make sure that this ScopedObserver is always declared last in order
   // to prevent any use-after-free.
   base::ScopedObservation<AXTree, AXTreeObserver> tree_event_observation_{this};
+
+  // Helper that tracks live regions.
+  std::unique_ptr<AXLiveRegionTracker> live_region_tracker_;
 };
 
 AX_EXPORT std::ostream& operator<<(std::ostream& os,
