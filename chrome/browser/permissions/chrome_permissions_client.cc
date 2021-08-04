94d68ae4b91fbaebc052758c6781f621e31196d5
Illia Klimov <elklm@google.com>
2021-08-04 09:30:27 +0000

    Add NTP verification for microphone permission in PermissionManager.
    After permission API migration from WebContents->GetLastCommittedURL()
    to WebContents->GetMainFrame()->GetLastCommittedOrigin() a permission
    request for microphone from search speech-to-text widget got broken.
    This CL verifies if  WebContents is NTP and returns correct
    "chrome://newtab/" URL.
    
    Bug: 1229942, 698985
    Change-Id: I6a8d3bc5fe4a5f5aa86ab00686f262905cd0e092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067062
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908389}

--- chrome/browser/permissions/chrome_permissions_client.cc
+++ chrome/browser/permissions/chrome_permissions_client.cc
@@ -296,6 +296,7 @@ bool ChromePermissionsClient::CanBypassEmbeddingOriginCheck(
   // excluded as currently they can request permission from iframes when
   // embedded in non-secure contexts (https://crbug.com/530507).
   return embedding_origin == GURL(chrome::kChromeUINewTabURL).GetOrigin() ||
+         embedding_origin == GURL(chrome::kChromeUINewTabPageURL).GetOrigin() ||
          requesting_origin.SchemeIs(extensions::kExtensionScheme);
 }
 
@@ -320,6 +321,13 @@ absl::optional<GURL> ChromePermissionsClient::OverrideCanonicalOrigin(
   }
 
   return absl::nullopt;
+}
+
+bool ChromePermissionsClient::DoOriginsMatchNewTabPage(
+    const GURL& requesting_origin,
+    const GURL& embedding_origin) {
+  return embedding_origin == GURL(chrome::kChromeUINewTabURL).GetOrigin() &&
+         requesting_origin == GURL(chrome::kChromeUINewTabPageURL).GetOrigin();
 }
 
 #if defined(OS_ANDROID)
