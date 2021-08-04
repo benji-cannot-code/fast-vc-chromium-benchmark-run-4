d2a3682eb8cb1a7eea29be5dcebc56872c8241e7
Danil Chapovalov <danilchap@chromium.org>
2021-08-04 09:53:46 +0000

    Mark absl import script as python3 in bash friendly way
    Bug: None
    Change-Id: Ibe1dc1afe70ee48dc6a7b67500f7a9c145a8ca47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3070841
    Auto-Submit: Danil Chapovalov <danilchap@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908392}

--- third_party/abseil-cpp/roll_abseil.py
+++ third_party/abseil-cpp/roll_abseil.py
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
-
-# NOTE: This script requires python 3.
+#!/usr/bin/env python3
 
 """Script to do the first step of Abseil roll into chromium.
 """
