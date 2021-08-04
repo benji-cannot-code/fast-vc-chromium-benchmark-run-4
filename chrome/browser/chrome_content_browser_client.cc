5e26d60eea9776a3d4172c2568833a802fd534f1
Chris Thompson <cthomp@chromium.org>
2021-08-04 01:25:52 +0000

    Fix interstitial loop in HTTPS-First Mode
    This changes the SSL error suppression logic to only suppress the SSL
    error if the user has not already added the host to the HTTPS-First Mode
    allowlist (i.e., they have not yet clicked through the HTTPS-First Mode
    interstitial). This means that the HTTPS-First Mode interstitial will
    take precedent over an SSL interstitial, but only if the HTTPS-First
    Mode interstitial should be shown to the user. This adds an explicit
    regression test to exercise this edge case.
    
    Bug: 1233207
    Change-Id: I35684dc74bb46fc65b9bb4f7e9bcaa99622e2d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068145
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908270}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -4027,7 +4027,7 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
       std::make_unique<CertificateReportingServiceCertReporter>(web_contents),
       base::BindOnce(&HandleSSLErrorWrapper), base::BindOnce(&IsInHostedApp),
       base::BindOnce(
-          &ShouldIgnoreInterstitialBecauseNavigationDefaultedToHttps)));
+          &ShouldIgnoreSslInterstitialBecauseNavigationDefaultedToHttps)));
 
   throttles.push_back(std::make_unique<LoginNavigationThrottle>(handle));
 
