5227b773e0fdb04922e3fd98a5c0f5b3d6a5c2f7
Gordon Seto <gordonseto@google.com>
2021-08-04 00:46:06 +0000

    [CrOS Bluetooth] Create paired bluetooth list.
    Create skeleton <os-settings-paired-bluetooth-list> and
    <os-settings-paired-bluetooth-list-item>. UI and data population will be
    added in a follow-up CL.
    
    Also create skeleton test classes for
    <os-settings-paired-bluetooth-list>,
    <os-settings-paired-bluetooth-list-item> and
    <os-settings-bluetooth-devices-subpage>.
    
    Screenshot:
    https://screenshot.googleplex.com/4G7wpqzhcdbTjtf.png
    
    Bug: 1010321
    Change-Id: Icbde248c826bdefa21114ff4abbc8f6c3fb82e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068051
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908252}

--- chrome/browser/ui/webui/settings/chromeos/bluetooth_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/bluetooth_section.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/bluetooth_device.h"
@@ -202,6 +203,10 @@ void BluetoothSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
        IDS_BLUETOOTH_ACCESSIBILITY_DEVICE_TYPE_KEYBOARD_MOUSE_COMBO},
       {"bluetoothDeviceType_unknown",
        IDS_BLUETOOTH_ACCESSIBILITY_DEVICE_TYPE_UNKNOWN},
+      {"bluetoothDeviceListCurrentlyConnected",
+       IDS_BLUETOOTH_DEVICE_LIST_CURRENTLY_CONNECTED},
+      {"bluetoothDeviceListPreviouslyConnected",
+       IDS_BLUETOOTH_DEVICE_LIST_PREVIOUSLY_CONNECTED},
   };
   html_source->AddLocalizedStrings(kLocalizedStrings);
   chromeos::bluetooth::AddLoadTimeData(html_source);
