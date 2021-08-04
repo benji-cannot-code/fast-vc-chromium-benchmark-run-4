11ce148cef03ea3c9da5883b57cc74bbde8e57d4
Tommy Martino <tmartino@google.com>
2021-08-04 16:21:07 +0000

    [SH iOS] Transition Text Fragments to JavaScriptFeature
    Bug: 1230573, 1129390
    Change-Id: I7bd643ca960e5f3d2059918d3598f906c6ae9484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3032965
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908479}

--- ios/web/js_messaging/java_script_feature_util_impl.mm
+++ ios/web/js_messaging/java_script_feature_util_impl.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/js_features/window_error/window_error_java_script_feature.h"
 #include "ios/web/public/js_messaging/java_script_feature.h"
 #import "ios/web/public/web_client.h"
+#import "ios/web/text_fragments/text_fragments_java_script_feature.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -105,8 +106,11 @@ std::vector<JavaScriptFeature*> GetBuiltInJavaScriptFeatures(
     BrowserState* browser_state) {
   std::vector<JavaScriptFeature*> features = {
       ContextMenuJavaScriptFeature::FromBrowserState(browser_state),
-      FindInPageJavaScriptFeature::GetInstance(), GetFaviconJavaScriptFeature(),
-      GetScrollHelperJavaScriptFeature(), GetWindowErrorJavaScriptFeature()};
+      FindInPageJavaScriptFeature::GetInstance(),
+      GetFaviconJavaScriptFeature(),
+      GetScrollHelperJavaScriptFeature(),
+      GetWindowErrorJavaScriptFeature(),
+      TextFragmentsJavaScriptFeature::GetInstance()};
 
   // Plugin Placeholder is no longer used as of iOS 14.5 as <applet> support is
   // completely removed.
