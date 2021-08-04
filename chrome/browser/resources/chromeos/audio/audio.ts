6f4ec2e3760a71d6efee0dff9f356f1e3d7dd696
Lauren Wei <laurenwei@google.com>
2021-08-04 05:43:51 +0000

    Audio WebUI: implement a mojo interface
    Adds a mojo interface and an audio handler to hook up the frontend audio
    page with the c++ backend through mojo. The current implementation
    allows the renderer end to call getAudioDeviceInfo to obtain audio
    device information from the CrasAudioHandler.
    
    
    Change-Id: I4c383b493f2636c690b0bd5faf0e70ec75bc7795
    Bug: b/193596247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035148
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Lauren Wei <laurenwei@google.com>
    Cr-Commit-Position: refs/heads/master@{#908341}

--- chrome/browser/resources/chromeos/audio/audio.ts
+++ chrome/browser/resources/chromeos/audio/audio.ts
@@ -3,7 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {AudioBroker} from './audio_broker.js';
+
 function initialize() {
+  const handler = AudioBroker.getInstance().handler;
+  handler.getAudioDeviceInfo().then(({deviceName}) => {
+    console.log('mock device name output: ' + deviceName);
+  });
   console.log('welcome to the audio page.');
 }
 
--- chrome/browser/ui/webui/chromeos/audio/audio_ui.cc
+++ chrome/browser/ui/webui/chromeos/audio/audio_ui.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/chromeos/audio/audio_ui.h"
 
+#include <memory>
+#include <utility>
+
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/audio_resources.h"
@@ -14,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 
-AudioUI::AudioUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
+namespace chromeos {
+
+AudioUI::AudioUI(content::WebUI* web_ui) : ui::MojoWebUIController(web_ui) {
   // Set up the chrome://audio source.
   content::WebUIDataSource* html_source =
       content::WebUIDataSource::Create(chrome::kChromeUIAudioHost);
@@ -28,4 +33,21 @@ AudioUI::AudioUI(content::WebUI* web_ui) : content::WebUIController(web_ui) {
   content::WebUIDataSource::Add(browser_context, html_source);
 }
 
+WEB_UI_CONTROLLER_TYPE_IMPL(AudioUI)
+
 AudioUI::~AudioUI() = default;
+
+void AudioUI::BindInterface(
+    mojo::PendingReceiver<audio::mojom::PageHandlerFactory> receiver) {
+  factory_receiver_.reset();
+  factory_receiver_.Bind(std::move(receiver));
+}
+
+void AudioUI::CreatePageHandler(
+    mojo::PendingRemote<audio::mojom::Page> page,
+    mojo::PendingReceiver<audio::mojom::PageHandler> receiver) {
+  page_handler_ =
+      std::make_unique<AudioHandler>(std::move(receiver), std::move(page));
+}
+
+}  // namespace chromeos
