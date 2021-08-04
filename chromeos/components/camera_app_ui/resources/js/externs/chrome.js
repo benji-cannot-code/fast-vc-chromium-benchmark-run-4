cf3034ed3f040b096e20cec7c56a0f22bb4bb471
Pi-Hsun Shih <pihsun@chromium.org>
2021-08-04 03:58:45 +0000

    CCA: Add TypeScript type definition for IdleDetector
    Also change the call site to use global IdleDetector instead of
    window.IdleDetector, and fix corresponding ESLint and closure
    compiler file.
    
    This drops the error of `cca.py tsc` from 125 to 124.
    
    Bug: b:172340451
    Test: cca.py tsc
    Change-Id: I4b498ac442b43e5d4ae8be7edcb31ca8da607fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068963
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908323}

--- chromeos/components/camera_app_ui/resources/js/externs/chrome.js
+++ chromeos/components/camera_app_ui/resources/js/externs/chrome.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* eslint-disable no-unused-vars, no-var */
+
 /**
  * It is available since we enabled and filled data in it while creating
  * content::WebUIDataSource for CCA.
@@ -21,7 +23,7 @@ window.loadTimeData;
  *   start: function(): !Promise<void>,
  * }}
  */
-window.IdleDetector;
+let IdleDetector;
 
 /**
  * @typedef {{
