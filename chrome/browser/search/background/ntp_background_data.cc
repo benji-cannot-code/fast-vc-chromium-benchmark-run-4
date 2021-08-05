ff9bcfa91dea6d44095020275f21e3bb700adb8b
Tibor Goldschwendt <tiborg@chromium.org>
2021-08-05 02:37:33 +0000

    [ntp] Move custom background handling to new service
    Moves logic calculating custom background config from InstantService to
    the new NtpCustomBackgroundService. To goal is to eventually remove
    InstantService from handling custom backgrounds and, thus, the WebUI
    NTP's dependency on InstantService. For the most part, this CL is a
    verbatim move with some extra plumbing and observers to establish the
    communication between InstantService and NtpCustomBackgroundService.
    
    
    Bug: 1084363
    Change-Id: I5cbd4709853da869c227d0087657bc02973cf47b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067671
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908733}

--- chrome/browser/search/background/ntp_background_data.cc
+++ chrome/browser/search/background/ntp_background_data.cc
@@ -109,3 +109,12 @@ void ErrorInfo::ClearError() {
   error_type = ErrorType::NONE;
   net_error = 0;
 }
+
+CustomBackground::CustomBackground() = default;
+CustomBackground::CustomBackground(const CustomBackground&) = default;
+CustomBackground::CustomBackground(CustomBackground&&) = default;
+CustomBackground::~CustomBackground() = default;
+
+CustomBackground& CustomBackground::operator=(const CustomBackground&) =
+    default;
+CustomBackground& CustomBackground::operator=(CustomBackground&&) = default;
