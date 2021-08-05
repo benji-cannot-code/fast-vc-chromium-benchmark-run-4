c5ac7b6d40593643dab4b320004eabcca127543d
Alex Gough <ajgo@chromium.org>
2021-08-05 00:12:24 +0000

    Specify XrDeviceService sandbox in mojom
    The XRDeviceService sandbox is different on Windows than other platforms
    so needs to be parameterised in the mojom attribute somehow.
    
    An alternative to this approach is to use #defines in
    //sandbox/policy/sandbox_type.h mapping something like kXrDeviceService
    to kXrCompositing on Windows and kUtility on other platforms. This
    is less invasive to mojom, but also hides that there is a difference
    on each platform. This alternative approach will also prevent us from
    collapsing the sandbox.mojom.Sandbox and sandbox::policy::SandboxType
    enums in future.
    
    Change-Id: I6f1499cd7bb68f612c433794a212c1b92b955f6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041926
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908690}

--- content/browser/xr/service/xr_device_service.cc
+++ content/browser/xr/service/xr_device_service.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/no_destructor.h"
 #include "build/build_config.h"
-#include "content/browser/service_sandbox_type.h"
 #include "content/public/browser/gpu_client.h"
 #include "content/public/browser/service_process_host.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
