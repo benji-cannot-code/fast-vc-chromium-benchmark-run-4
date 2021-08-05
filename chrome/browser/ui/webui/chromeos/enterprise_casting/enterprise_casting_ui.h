202f6aa31a323b79d3bd9da98048ab30f4aeec05
Benjamin Zielinski <bzielinski@google.com>
2021-08-05 20:59:09 +0000

    [Cast2Class] Add page handler for enterprise casting WebUI.
    Stub implementation for the WebUI and page handler, to ensure we have
    the mojom, WebUI, and page handler communicating correctly.
    Additionally, add ChromeOS EDU team as OWNERS of the enterprise casting
    resources folder.
    
    Bug: b/193421383
    Change-Id: I346203207f657b0ab1755cf46677749d6a916684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064083
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/master@{#909038}

--- chrome/browser/ui/webui/chromeos/enterprise_casting/enterprise_casting_ui.h
+++ chrome/browser/ui/webui/chromeos/enterprise_casting/enterprise_casting_ui.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_ENTERPRISE_CASTING_ENTERPRISE_CASTING_UI_H_
 
 #include "chrome/browser/ui/webui/chromeos/enterprise_casting/enterprise_casting.mojom.h"
+#include "chrome/browser/ui/webui/chromeos/enterprise_casting/enterprise_casting_handler.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -30,6 +31,13 @@ class EnterpriseCastingUI
           receiver);
 
  private:
+  // launcher_internals::mojom::PageHandlerFactory:
+  void CreatePageHandler(
+      mojo::PendingRemote<enterprise_casting::mojom::Page> page,
+      mojo::PendingReceiver<enterprise_casting::mojom::PageHandler>
+          page_handler) override;
+
+  std::unique_ptr<EnterpriseCastingHandler> page_handler_;
   mojo::Receiver<enterprise_casting::mojom::PageHandlerFactory>
       factory_receiver_{this};
 
