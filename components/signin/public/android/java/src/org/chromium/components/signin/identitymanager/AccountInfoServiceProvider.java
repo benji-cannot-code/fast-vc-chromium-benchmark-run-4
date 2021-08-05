76b16e61339238ae52110625c13566c826b7e151
Alice Wang <aliceywang@chromium.org>
2021-08-05 12:49:45 +0000

    [Android] Clean up redundant instance field in AccountInfoServiceProvider
    This CL cleans up the redundant instance field in
    AccountInfoServiceProvider.
    
    Bug: 1231112
    Change-Id: Ie8677c99a6e77f5691eacb3fde7ec7be16859ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058244
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908844}

--- components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/AccountInfoServiceProvider.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/AccountInfoServiceProvider.java
@@ -12,14 +12,11 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.Promise;
 import org.chromium.base.ThreadUtils;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 /**
  * This class groups all the {@link AccountInfoService} instance manipulation
  * methods in one place.
  */
 public final class AccountInfoServiceProvider {
-    private static final AtomicReference<AccountInfoService> sInstance = new AtomicReference<>();
     private static @Nullable Promise<AccountInfoService> sInstancePromise;
 
     /**
@@ -28,14 +25,15 @@ public final class AccountInfoServiceProvider {
     @MainThread
     public static void init(
             IdentityManager identityManager, AccountTrackerService accountTrackerService) {
-        if (sInstance.get() != null) {
+        if (sInstancePromise != null && sInstancePromise.isFulfilled()) {
             return;
         }
-        sInstance.set(new AccountInfoServiceImpl(identityManager, accountTrackerService));
+        final AccountInfoService service =
+                new AccountInfoServiceImpl(identityManager, accountTrackerService);
         if (sInstancePromise == null) {
-            sInstancePromise = Promise.fulfilled(sInstance.get());
+            sInstancePromise = Promise.fulfilled(service);
         } else {
-            sInstancePromise.fulfill(sInstance.get());
+            sInstancePromise.fulfill(service);
         }
     }
 
@@ -44,11 +42,14 @@ public final class AccountInfoServiceProvider {
      *
      * This method must be invoked after {@link AccountInfoService} is initialized.
      */
+    @MainThread
     public static AccountInfoService get() {
-        if (sInstance.get() == null) {
+        ThreadUtils.assertOnUiThread();
+        final Promise<AccountInfoService> promise = getPromise();
+        if (!promise.isFulfilled()) {
             throw new RuntimeException("The AccountInfoService is not yet initialized!");
         }
-        return sInstance.get();
+        return promise.getResult();
     }
 
     /**
@@ -69,13 +70,11 @@ public final class AccountInfoServiceProvider {
     @VisibleForTesting
     public static void setInstanceForTests(AccountInfoService accountInfoService) {
         ThreadUtils.assertOnUiThread();
-        sInstance.set(accountInfoService);
         sInstancePromise = Promise.fulfilled(accountInfoService);
     }
 
     @VisibleForTesting
     public static void resetForTests() {
-        sInstance.set(null);
         sInstancePromise = null;
     }
 
