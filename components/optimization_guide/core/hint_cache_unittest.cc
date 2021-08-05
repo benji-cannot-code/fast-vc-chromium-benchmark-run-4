c2ca42b41c6a4bbc72214fe721bdef92abd3d45a
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-05 18:44:21 +0000

    [automated] Drop needless <vector> includes in /components/optimization_guide
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=tbansal@chromium.org
    
    Bug: 1233968
    Change-Id: I227aef2f39ec5c6a74eea0cbe8de897e446f4782
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059560
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908965}

--- components/optimization_guide/core/hint_cache_unittest.cc
+++ components/optimization_guide/core/hint_cache_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/hint_cache.h"
 
 #include <string>
-#include <vector>
 
 #include "base/bind.h"
 #include "base/files/scoped_temp_dir.h"
--- components/optimization_guide/core/store_update_data_unittest.cc
+++ components/optimization_guide/core/store_update_data_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/store_update_data.h"
 
 #include <string>
-#include <vector>
 
 #include "base/macros.h"
 #include "base/time/time.h"
