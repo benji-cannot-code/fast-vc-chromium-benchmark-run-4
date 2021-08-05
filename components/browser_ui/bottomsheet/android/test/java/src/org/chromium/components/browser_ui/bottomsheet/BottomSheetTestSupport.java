27a8605d946936898a012124e2f674c440461097
Theresa Sullivan <twellington@chromium.org>
2021-08-05 15:27:03 +0000

    🤠 Fix threading issue in BottomSheetTestSupport
    In crrev.com/c/3068184 a new BottomSheetTestSupport method was added
    that mixed checks for bottom sheet state on the instrumentation thread
    and UI thread, causing flakes due to a missed state  change signal.
    Consolidate checks on the UI thread.
    
    Bug: 1228920
    Change-Id: I952c79f1347cb40e0effd8b4a602193a849710e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071796
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908891}

--- components/browser_ui/bottomsheet/android/test/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetTestSupport.java
+++ components/browser_ui/bottomsheet/android/test/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheetTestSupport.java
@@ -123,7 +123,6 @@ public class BottomSheetTestSupport {
      * @param state The state to wait for.
      */
     public static void waitForState(BottomSheetController controller, @SheetState int state) {
-        if (controller.getSheetState() == state) return;
         CallbackHelper stateChangeHelper = new CallbackHelper();
         final BottomSheetObserver observer = new EmptyBottomSheetObserver() {
             @Override
@@ -132,7 +131,13 @@ public class BottomSheetTestSupport {
             }
         };
 
-        TestThreadUtils.runOnUiThreadBlocking(() -> controller.addObserver(observer));
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            if (controller.getSheetState() == state) {
+                stateChangeHelper.notifyCalled();
+            } else {
+                controller.addObserver(observer);
+            }
+        });
 
         try {
             stateChangeHelper.waitForFirst();
@@ -149,10 +154,6 @@ public class BottomSheetTestSupport {
      * @param controller The controller for the bottom sheet.
      */
     public static void waitForOpen(BottomSheetController controller) {
-        if (controller.getSheetState() == BottomSheetController.SheetState.HALF
-                || controller.getSheetState() == BottomSheetController.SheetState.FULL) {
-            return;
-        }
         CallbackHelper stateChangeHelper = new CallbackHelper();
 
         final BottomSheetObserver observer = new EmptyBottomSheetObserver() {
@@ -165,12 +166,20 @@ public class BottomSheetTestSupport {
             }
         };
 
-        TestThreadUtils.runOnUiThreadBlocking(() -> controller.addObserver(observer));
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            if (controller.getSheetState() == BottomSheetController.SheetState.HALF
+                    || controller.getSheetState() == BottomSheetController.SheetState.FULL) {
+                stateChangeHelper.notifyCalled();
+            } else {
+                controller.addObserver(observer);
+            }
+        });
 
         try {
             stateChangeHelper.waitForFirst();
         } catch (TimeoutException ex) {
-            assert false : "Bottom sheet state never half or full";
+            assert false : "Bottom sheet state never half or full. Current State: "
+                           + sheetStateToString(controller.getSheetState());
         }
 
         TestThreadUtils.runOnUiThreadBlocking(() -> controller.removeObserver(observer));
