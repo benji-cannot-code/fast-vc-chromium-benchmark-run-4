6b545d9a1c581a35192e8bd8a4661ac108d91518
Pranav Batra <batrapranav@chromium.org>
2021-08-05 04:51:03 +0000

    Prepare to enable the chrome.printing API in lacros
    Disable and update unit tests.
    Enable kPrintingAPIExtensionsAllowlist preference in lacros.
    Enable ShowPrintJobConfirmationDialog() function in lacros.
    Update LocalPrinterAsh.
    
    Bug: b/189970798
    Change-Id: I3ef19547d392825849746ba37f287dec61f6ce5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061738
    Commit-Queue: Pranav Batra <batrapranav@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908761}

--- chrome/browser/extensions/api/printing/printing_api_utils_unittest.cc
+++ chrome/browser/extensions/api/printing/printing_api_utils_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/printing/printing_api_utils.h"
 
 #include "base/json/json_reader.h"
+#include "chromeos/crosapi/mojom/local_printer.mojom.h"
 #include "chromeos/printing/printer_configuration.h"
 #include "printing/backend/print_backend.h"
 #include "printing/mojom/print.mojom.h"
@@ -132,11 +133,8 @@ TEST(PrintingApiUtilsTest, GetDefaultPrinterRules_EmptyPref) {
 }
 
 TEST(PrintingApiUtilsTest, PrinterToIdl) {
-  chromeos::Printer printer(kId);
-  printer.set_display_name(kName);
-  printer.set_description(kDescription);
-  EXPECT_TRUE(printer.SetUri(kUri));
-  printer.set_source(chromeos::Printer::SRC_POLICY);
+  crosapi::mojom::LocalDestinationInfo printer(kId, kName, kDescription, true,
+                                               kUri);
 
   absl::optional<DefaultPrinterRules> default_printer_rules =
       DefaultPrinterRules();
