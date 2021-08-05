306e9eb5c119dcceb5e76b3e7c237658e8c606e9
Chad Duffin <chadduffin@chromium.org>
2021-08-05 18:36:04 +0000

    [CrOS Cellular] Cond. set roaming preference.
    This change introduces a helper class that does a best-effort attempt at
    preserving the roaming configuration when per-service cellular roaming
    becomes disabled. This class will only be enabled when per-service
    cellular roaming configuration is disabled and the device is not
    enterprise managed.
    
    Bug: 1220691
    Test: Verified on-device.
    Change-Id: I3485da0ae83be1460ce1083791db83b0114944af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068269
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908959}

--- chrome/browser/ash/policy/networking/device_network_configuration_updater.cc
+++ chrome/browser/ash/policy/networking/device_network_configuration_updater.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/ash/policy/networking/network_roaming_state_migration_handler_impl.h"
+#include "chrome/browser/ash/policy/networking/roaming_configuration_migration_handler.h"
 #include "chrome/browser/ash/settings/cros_settings.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
@@ -43,7 +44,8 @@ std::string GetDeviceAssetID() {
 
 }  // namespace
 
-DeviceNetworkConfigurationUpdater::~DeviceNetworkConfigurationUpdater() {}
+DeviceNetworkConfigurationUpdater::~DeviceNetworkConfigurationUpdater() =
+    default;
 
 // static
 std::unique_ptr<DeviceNetworkConfigurationUpdater>
@@ -88,6 +90,11 @@ DeviceNetworkConfigurationUpdater::DeviceNetworkConfigurationUpdater(
           ash::features::kCellularAllowPerNetworkRoaming)) {
     network_roaming_state_migration_handler_ =
         std::make_unique<NetworkRoamingStateMigrationHandlerImpl>();
+    if (!chromeos::InstallAttributes::Get()->IsEnterpriseManaged()) {
+      roaming_configuration_migration_handler_ =
+          std::make_unique<RoamingConfigurationMigrationHandler>(
+              network_roaming_state_migration_handler_.get());
+    }
   }
 }
 
