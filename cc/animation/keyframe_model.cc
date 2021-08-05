f3032498038770cee2ac157a98dc39ffe105426e
Kevin Ellis <kevers@chromium.org>
2021-08-05 20:06:28 +0000

    [Code health]: Cleanup of CompositorKeyframeModel constructors
    Bug: 1234377
    Change-Id: I3e884f60fc3870e87bd3826ed0a27207f13787ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060658
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909013}

--- cc/animation/keyframe_model.cc
+++ cc/animation/keyframe_model.cc
@@ -55,7 +55,13 @@ KeyframeModel::TargetPropertyId::TargetPropertyId(
 KeyframeModel::TargetPropertyId::TargetPropertyId(
     const TargetPropertyId& other) = default;
 
+KeyframeModel::TargetPropertyId::TargetPropertyId(TargetPropertyId&& other) =
+    default;
+
 KeyframeModel::TargetPropertyId::~TargetPropertyId() = default;
+
+KeyframeModel::TargetPropertyId& KeyframeModel::TargetPropertyId::operator=(
+    TargetPropertyId&& other) = default;
 
 std::unique_ptr<KeyframeModel> KeyframeModel::Create(
     std::unique_ptr<gfx::AnimationCurve> curve,
@@ -63,7 +69,8 @@ std::unique_ptr<KeyframeModel> KeyframeModel::Create(
     int group_id,
     TargetPropertyId target_property_id) {
   return base::WrapUnique(new KeyframeModel(std::move(curve), keyframe_model_id,
-                                            group_id, target_property_id));
+                                            group_id,
+                                            std::move(target_property_id)));
 }
 
 std::unique_ptr<KeyframeModel> KeyframeModel::CreateImplInstance(
@@ -97,7 +104,7 @@ KeyframeModel::KeyframeModel(std::unique_ptr<gfx::AnimationCurve> curve,
                          keyframe_model_id,
                          target_property_id.target_property_type()),
       group_(group_id),
-      target_property_id_(target_property_id),
+      target_property_id_(std::move(target_property_id)),
       needs_synchronized_start_time_(false),
       received_finished_event_(false),
       is_controlling_instance_(false),
