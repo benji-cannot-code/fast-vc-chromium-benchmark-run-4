44c79f552e5e84cd28b485761e093bf44046614a
Maria Kazinova <kazinova@google.com>
2021-08-05 14:55:31 +0000

    [iOS] Migrate PasswordSettingsAppInterface to PasswordStoreInterface
    PasswordStoreInterface is an interface that should be used by other
    classes instead of interacting directly with PasswordStore.
    
    Bug: 1218413
    Change-Id: Ie915edf356a8b5e1d0e1f402ac1e7d07ebf92b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074859
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#908880}

--- ios/chrome/browser/ui/settings/password/passwords_settings_app_interface.mm
+++ ios/chrome/browser/ui/settings/password/passwords_settings_app_interface.mm
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/wait_util.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/password_manager/core/browser/password_form.h"
-#include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
+#include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -34,13 +34,13 @@ using chrome_test_util::SetUpAndReturnMockReauthenticationModuleForExport;
 
 namespace {
 
-scoped_refptr<password_manager::PasswordStore> GetPasswordStore() {
+scoped_refptr<password_manager::PasswordStoreInterface> GetPasswordStore() {
   // ServiceAccessType governs behaviour in Incognito: only modifications with
   // EXPLICIT_ACCESS, which correspond to user's explicit gesture, succeed.
   // This test does not deal with Incognito, and should not run in Incognito
   // context. Therefore IMPLICIT_ACCESS is used to let the test fail if in
   // Incognito context.
-  return IOSChromePasswordStoreFactory::GetForBrowserState(
+  return IOSChromePasswordStoreFactory::GetInterfaceForBrowserState(
       chrome_test_util::GetOriginalBrowserState(),
       ServiceAccessType::IMPLICIT_ACCESS);
 }
