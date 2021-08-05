9460e8907e8e24983d3ad3a75098252f82e7eb13
Harry Cutts <hcutts@chromium.org>
2021-08-05 23:54:13 +0000

    Use hex in CrosUsbDetector "not allowed" warning
    Vendor and product IDs are very rarely stated in decimal, making it
    harder to compare the ones from this warning message. The vendor ID is
    also stated first, normally.
    
    Bug: none
    Change-Id: I64c71ef64c20f23eeff638cb59b03c4327abe4b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3073071
    Auto-Submit: Harry Cutts <hcutts@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909119}

--- chrome/browser/ash/usb/cros_usb_detector.cc
+++ chrome/browser/ash/usb/cros_usb_detector.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/notification_utils.h"
 #include "base/callback_helpers.h"
 #include "base/files/file_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/crostini/crostini_features.h"
 #include "chrome/browser/ash/crostini/crostini_manager.h"
@@ -541,9 +542,9 @@ void CrosUsbDetector::OnDeviceChecked(
     bool hide_notification,
     bool allowed) {
   if (!allowed) {
-    LOG(WARNING) << "Device not allowed by Permission Broker. product:"
-                 << device_info->product_id
-                 << " vendor:" << device_info->vendor_id;
+    LOG(WARNING) << "Device not allowed by Permission Broker. vendor: 0x"
+                 << std::hex << device_info->vendor_id << " product: 0x"
+                 << device_info->product_id;
     return;
   }
 
