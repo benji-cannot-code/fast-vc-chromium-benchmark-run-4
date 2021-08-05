3ac16fbfb3bdcd2229492c26bb271d479e412404
jlebel <jlebel@chromium.org>
2021-08-05 15:45:59 +0000

    [iOS][MICE] Add EG tests for ConsistencyPromoSigninCoordinator
    This patch tests the dismissal count for
    ConsistencyPromoSigninCoordinator.
    
    The max dismissal count is hardcoded at 3. The finch implementation was
    not working.
    
    Related to:
    crrev.com/c/3044020
    crrev.com/c/3021236
    
    Fixed: 1232071
    Change-Id: I8d7d3e717a380218b75e6b8e44bf2866323f7de2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059688
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908894}

--- ios/chrome/browser/ui/authentication/signin/signin_constants.mm
+++ ios/chrome/browser/ui/authentication/signin/signin_constants.mm
@@ -23,3 +23,8 @@ NSString* const kWebSigninContinueAsButtonAccessibilityIdentifier =
     @"WebSigninContinueAsButtonAccessibilityIdentifier";
 NSString* const kWebSigninSkipButtonAccessibilityIdentifier =
     @"WebSigninSkipButtonAccessibilityIdentifier";
+
+const char* kWebSigninConsistencyConsecutiveActiveDismissalLimitParam =
+    "consecutive_active_dismissal_limit";
+
+const int kDefaultWebSignInDismissalCount = 3;
