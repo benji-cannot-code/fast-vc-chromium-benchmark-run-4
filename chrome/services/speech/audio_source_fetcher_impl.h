52937cce18d56b1fa35d7ca891d313e8e47419d0
Rob Schonberger <robsc@chromium.org>
2021-08-05 04:54:13 +0000

    Add AudioLog to AudioSourceFetcher.
    Adds AudioLog functionality to AudioSourceFetcher, which is only enabled if
    vlogging is enabled for the file in particular.
    
    Change-Id: I39db05d1da56b0acec345e3c1423e7fc391ef6a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3013110
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908762}

--- chrome/services/speech/audio_source_fetcher_impl.h
+++ chrome/services/speech/audio_source_fetcher_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "media/base/audio_capturer_source.h"
 #include "media/mojo/common/audio_data_s16_converter.h"
+#include "media/mojo/mojom/audio_logging.mojom.h"
 #include "media/mojo/mojom/speech_recognition_service.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -17,14 +18,15 @@ namespace speech {
 
 class SpeechRecognitionRecognizerImpl;
 
-// Class to get microphone audio data and send it to a
+// Class to get device audio data and send it to a
 // SpeechRecognitionRecognizerImpl for transcription. Runs on the IO thread in
 // the Browser process in Chrome OS and in the Speech Recognition Service
 // utility process on Chrome or web speech fallback.
 class AudioSourceFetcherImpl
     : public media::mojom::AudioSourceFetcher,
       public media::AudioCapturerSource::CaptureCallback,
-      public media::AudioDataS16Converter {
+      public media::AudioDataS16Converter,
+      public media::mojom::AudioLog {
  public:
   AudioSourceFetcherImpl(
       std::unique_ptr<SpeechRecognitionRecognizerImpl> recognition_recognizer);
@@ -52,6 +54,16 @@ class AudioSourceFetcherImpl
   void OnCaptureError(media::AudioCapturerSource::ErrorCode code,
                       const std::string& message) final;
   void OnCaptureMuted(bool is_muted) final {}
+  // media::mojom::AudioLog
+  void OnCreated(const media::AudioParameters& params,
+                 const std::string& device_id) override;
+  void OnStarted() override;
+  void OnStopped() override;
+  void OnClosed() override;
+  void OnError() override;
+  void OnSetVolume(double volume) override;
+  void OnLogMessage(const std::string& message) override;
+  void OnProcessingStateChanged(const std::string& message) override;
 
   void set_audio_capturer_source_for_tests(
       media::AudioCapturerSource* audio_capturer_source_for_tests) {
@@ -87,6 +99,8 @@ class AudioSourceFetcherImpl
 
   // Whether audio capture is started.
   bool is_started_;
+
+  mojo::Receiver<media::mojom::AudioLog> audio_log_receiver_{this};
 
   base::WeakPtrFactory<AudioSourceFetcherImpl> weak_factory_{this};
 };
