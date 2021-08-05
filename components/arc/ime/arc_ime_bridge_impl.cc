3c57a93d004611371c928e6fca8f920af671cae5
Darren Shen <shend@chromium.org>
2021-08-05 03:53:16 +0000

    arc-ime: Add a segment style attribute to CompositionSegment.
    Certain IMEs need an invisible composition segment, so add a style
    attribute to ARC++.
    
    Bug: b:188735413
    Change-Id: Id1b3709e3b64e018723dcc865ae8de8d686f7da3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3017551
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908747}

--- components/arc/ime/arc_ime_bridge_impl.cc
+++ components/arc/ime/arc_ime_bridge_impl.cc
@@ -21,6 +21,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace arc {
 namespace {
 
+mojom::SegmentStyle GetSegmentStyle(const ui::ImeTextSpan& ime_text_span) {
+  if (ime_text_span.thickness == ui::ImeTextSpan::Thickness::kNone ||
+      ime_text_span.underline_style == ui::ImeTextSpan::UnderlineStyle::kNone) {
+    return mojom::SegmentStyle::NONE;
+  }
+  if (ime_text_span.thickness == ui::ImeTextSpan::Thickness::kThick) {
+    return mojom::SegmentStyle::EMPHASIZED;
+  }
+  return mojom::SegmentStyle::DEFAULT;
+}
+
 std::vector<mojom::CompositionSegmentPtr> ConvertSegments(
     const ui::CompositionText& composition) {
   std::vector<mojom::CompositionSegmentPtr> segments;
@@ -32,6 +43,7 @@ std::vector<mojom::CompositionSegmentPtr> ConvertSegments(
         (ime_text_span.thickness == ui::ImeTextSpan::Thickness::kThick ||
          (composition.selection.start() == ime_text_span.start_offset &&
           composition.selection.end() == ime_text_span.end_offset));
+    segment->style = GetSegmentStyle(ime_text_span);
     segments.push_back(std::move(segment));
   }
   return segments;
