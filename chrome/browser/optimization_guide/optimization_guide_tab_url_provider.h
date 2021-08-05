8564d7047b22e70bdae0e89544804f680c951c41
Xing Liu <xingliu@google.com>
2021-08-05 19:19:06 +0000

    Optimization guide: Add iOS TabUrlProvider class structure.
    Adds an empty TabUrlProviderImpl class on iOS. Also build
    components/optimization_guide/core on iOS.
    
    Bug: 1232858
    Change-Id: I740a2e3df6d468d6d38c3327a5ab21bf8ccbb3c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072965
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908984}

--- chrome/browser/optimization_guide/optimization_guide_tab_url_provider.h
+++ chrome/browser/optimization_guide/optimization_guide_tab_url_provider.h
@@ -22,11 +22,6 @@ class OptimizationGuideTabUrlProvider
   explicit OptimizationGuideTabUrlProvider(Profile* profile);
   ~OptimizationGuideTabUrlProvider() override;
 
-  OptimizationGuideTabUrlProvider(
-      const OptimizationGuideTabUrlProvider& other) = delete;
-  OptimizationGuideTabUrlProvider& operator=(
-      const OptimizationGuideTabUrlProvider&) = delete;
-
   // optimization_guide::TabUrlProvider:
   const std::vector<GURL> GetUrlsOfActiveTabs(
       const base::TimeDelta& duration_since_last_shown) override;
