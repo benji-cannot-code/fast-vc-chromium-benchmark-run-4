824bffc8be858425cfabbd22658129ca86577c85
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-05 17:38:44 +0000

    [automated] Drop needless <vector> includes in /components/policy
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=pastarmovj@chromium.org
    
    Bug: 1233968
    Change-Id: I9d3c329b9dbe845eaf53b7e090a07378834f2cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058208
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908936}

--- components/policy/core/browser/url_blocklist_manager.cc
+++ components/policy/core/browser/url_blocklist_manager.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <set>
 #include <utility>
-#include <vector>
 
 #include "base/bind.h"
 #include "base/check.h"
