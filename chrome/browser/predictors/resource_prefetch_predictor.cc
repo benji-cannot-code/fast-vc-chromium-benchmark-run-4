50b68886e43b31d61aefdb67a2645b86a5066a4a
Matt Menke <mmenke@chromium.org>
2021-08-05 00:22:10 +0000

    DCHECK that predictor always has a non-empty NetworkIsolationKey.
    And do the same for calls to NetworkContext::PreconnectSockets when
    require_network_isolation_key_ is true.
    
    All consumers should be passing in a non-empty NIK, even when it's
    disabled at the network stack layer.
    
    Also fix SearchEnginePreconnector bug that was causing it to pass in
    empty NetworkIsolationKeys (and make its creation of a NIK a little
    more efficient, while I'm at it).
    
    This does not add a DCHECK to NetworkContext for DNS resolutions, which
    will be done in another CL, since it has more consumers.
    
    Bug: 1082280
    Change-Id: I370519a9083c15b55e43badae3ad11e56c0c8aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067698
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908696}

--- chrome/browser/predictors/resource_prefetch_predictor.cc
+++ chrome/browser/predictors/resource_prefetch_predictor.cc
@@ -76,6 +76,7 @@ PreconnectRequest::PreconnectRequest(
       num_sockets(num_sockets),
       network_isolation_key(network_isolation_key) {
   DCHECK_GE(num_sockets, 0);
+  DCHECK(!network_isolation_key.IsEmpty());
 }
 
 PrefetchRequest::PrefetchRequest(
@@ -86,6 +87,7 @@ PrefetchRequest::PrefetchRequest(
       network_isolation_key(network_isolation_key),
       destination(destination) {
   DCHECK(base::FeatureList::IsEnabled(features::kLoadingPredictorPrefetch));
+  DCHECK(!network_isolation_key.IsEmpty());
 }
 
 PreconnectPrediction::PreconnectPrediction() = default;
