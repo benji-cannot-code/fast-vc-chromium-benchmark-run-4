bcd2796c2440afcd5e5ce6968fd6be8c65c71aae
Christopher Lam <calamity@chromium.org>
2021-08-05 10:34:26 +0000

    Fix tooltip in Bookmark Manager.
    This CL fixes a tooltip showing 'undefined' because folders have no url.
    
    Bug: 1212361
    Change-Id: Iefeb5f4aa23b150fe7e774c27fa739eecb895029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2987530
    Auto-Submit: calamity <calamity@chromium.org>
    Commit-Queue: Jiewei Qian  <qjw@chromium.org>
    Reviewed-by: Jiewei Qian  <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908819}

--- chrome/browser/resources/bookmarks/item.ts
+++ chrome/browser/resources/bookmarks/item.ts
@@ -179,6 +179,10 @@ export class BookmarksItemElement extends BookmarksItemElementBase {
     }));
   }
 
+  private getItemUrl_(): string {
+    return this.item_.url || '';
+  }
+
   private onItemIdChanged_() {
     // TODO(tsergeant): Add a histogram to measure whether this assertion fails
     // for real users.
