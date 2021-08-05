a5c58783d5b87ff4de6d6b8ef2e4756366205210
Dylan Cutler <dylancutler@google.com>
2021-08-05 14:24:37 +0000

    Add feature flag for the Partitioned cookie attribute.
    The feature flag is not used anywhere yet, but it will be convenient to
    have this implemented as we implement the in-memory storage logic and
    partitioning logic later on.
    
    Bug: 1225444
    Change-Id: Ic6633782796d8e70747c5d4909781ffd39ef09f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3070479
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/master@{#908867}

--- net/base/features.cc
+++ net/base/features.cc
@@ -260,5 +260,8 @@ const base::Feature kCookieSameSiteConsidersRedirectChain{
 const base::Feature kSamePartyCookiesConsideredFirstParty{
     "SamePartyCookiesConsideredFirstParty", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kPartitionedCookies{"PartitionedCookies",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace features
 }  // namespace net
