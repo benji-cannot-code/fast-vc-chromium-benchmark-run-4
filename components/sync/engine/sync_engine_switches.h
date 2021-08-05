aa09d67460360be01b1d72964d5331fe96feb9a6
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-05 18:06:33 +0000

    Make ModelTypeWorker::min_gu_responses_to_ignore_key_ a FeatureParam
    R=treib@chromium.org
    
    Bug: 1109221
    Change-Id: Ibb5abf4a91f1cef03d37141fbbaec4f50f48a5ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074744
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#908950}

--- components/sync/engine/sync_engine_switches.h
+++ components/sync/engine/sync_engine_switches.h
@@ -7,13 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SYNC_ENGINE_SYNC_ENGINE_SWITCHES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 
 namespace switches {
 
 extern const base::Feature kSyncResetPollIntervalOnStart;
 extern const base::Feature kSyncUseScryptForNewCustomPassphrases;
 extern const base::Feature kSyncTriggerFullKeystoreMigration;
+
 extern const base::Feature kIgnoreSyncEncryptionKeysLongMissing;
+extern const base::FeatureParam<int> kMinGuResponsesToIgnoreKey;
 
 }  // namespace switches
 
