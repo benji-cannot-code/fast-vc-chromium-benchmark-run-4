cfd395d6a54019b4a6350c21a8811f56e3caf30b
Nohemi Fernandez <fernandex@chromium.org>
2021-08-05 17:14:22 +0000

    [iOS] Record user action for closing Encryption passphrase views.
    DCHECK is triggered when the user closes the Encryption view due to the
    missing metrics, this patch fixes the issue.
    
    Bug: 1226539
    Change-Id: I7a1bf076498f06bc165bbbe6545faa85ffc51352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3065645
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908925}

--- ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller.mm
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/mac/foundation_util.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/google/core/common/google_util.h"
 #include "components/strings/grit/components_strings.h"
@@ -202,7 +204,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 #pragma mark - SettingsControllerProtocol callbacks
 
 - (void)reportDismissalUserAction {
-  NOTREACHED();
+  base::RecordAction(
+      base::UserMetricsAction("MobileSyncEncryptionSettingsClose"));
 }
 
 - (void)reportBackUserAction {
