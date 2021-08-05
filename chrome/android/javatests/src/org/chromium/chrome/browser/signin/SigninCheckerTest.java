0b254b46a41abb53e594d30f5d67657cd2fa0d20
Alice Wang <aliceywang@chromium.org>
2021-08-05 13:39:53 +0000

    Reland "Revert "[Signin][Android] Fix SigninCheckerTest flakiness""
    This is a reland of fb6d13048fb888feee5c183fc0ea7da5e1bd1632
    
    Reason for reland: The tests SigninCheckerTest are still flaky today
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-official-tests/2474/
    due to the seeding timeout. We should find another way to fix them.
    
    Original change's description:
    > Revert "[Signin][Android] Fix SigninCheckerTest flakiness"
    >
    > This reverts commit 2c212e8777d403f61d335135d7d3aa5fc77ec706.
    >
    > Reason for revert: The tests were flaky originally due to a timeout during the seeding. After this fix, they are still flaky for the same reason on the bot android-arm-official-tests https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-official-tests/2468/overview.
    >
    > Original change's description:
    > > [Signin][Android] Fix SigninCheckerTest flakiness
    > >
    > > Signin tests that remove accounts were sometimes failing due to
    > > IllegalArgumentException thrown from FakeAccountManagerFacade when an
    > > async request for an access token was arriving after the account has
    > > been removed. This CL changes the exception thrown in this case to
    > > AuthException, that is correctly handled by ConnectionRetry and won't
    > > cause test failures.
    > >
    > > This should fix signinWhenPrimaryAccountIsRenamedToAKnownAccount and
    > > signoutWhenPrimaryAccountIsRenamedToAnUnknownAccount in
    > > SigninCheckerTest, so these tests are enabled.
    > >
    > > Bug: 1205346
    > > Change-Id: Ia7bac06968d849596b60d77e809ff1842e113046
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059478
    > > Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    > > Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    > > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#907546}
    >
    > Bug: 1205346
    > Change-Id: I3f09ef06f66be1c2c0aec0f7e2e8aedae96f31e8
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3063977
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Alice Wang <aliceywang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#907922}
    
    Bug: 1205346
    Change-Id: I0c4e73213828f17ea4ca6e159dadd44daedc9d76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068929
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Auto-Submit: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908858}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninCheckerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninCheckerTest.java
@@ -26,6 +26,7 @@ import org.mockito.quality.Strictness;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.UserActionTester;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -85,6 +86,7 @@ public class SigninCheckerTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/1205346")
     public void signinWhenPrimaryAccountIsRenamedToAKnownAccount() {
         mActivityTestRule.startMainActivityOnBlankPage();
         mAccountManagerTestRule.addAccountAndWaitForSeeding("the.second.account@gmail.com");
@@ -106,6 +108,7 @@ public class SigninCheckerTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/1205346")
     public void signoutWhenPrimaryAccountIsRenamedToAnUnknownAccount() {
         mActivityTestRule.startMainActivityOnBlankPage();
         mAccountManagerTestRule.addAccountAndWaitForSeeding("the.second.account@gmail.com");
@@ -127,6 +130,7 @@ public class SigninCheckerTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/1205346")
     public void signoutWhenPrimaryAccountIsRemoved() {
         mActivityTestRule.startMainActivityOnBlankPage();
         mAccountManagerTestRule.addAccountAndWaitForSeeding("the.second.account@gmail.com");
@@ -145,6 +149,7 @@ public class SigninCheckerTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/1205346")
     public void signoutWhenPrimaryAccountWithoutSyncConsentIsRemoved() {
         mActivityTestRule.startMainActivityOnBlankPage();
         mAccountManagerTestRule.addAccountAndWaitForSeeding("the.second.account@gmail.com");
--- components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/ProfileOAuth2TokenServiceDelegateTest.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/identitymanager/ProfileOAuth2TokenServiceDelegateTest.java
@@ -34,7 +34,6 @@ import org.chromium.base.test.util.JniMocker;
 import org.chromium.components.signin.AccessTokenData;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
 import org.chromium.components.signin.AccountUtils;
-import org.chromium.components.signin.AuthException;
 import org.chromium.components.signin.test.util.FakeAccountManagerFacade;
 
 import java.util.List;
@@ -111,7 +110,7 @@ public class ProfileOAuth2TokenServiceDelegateTest {
 
     @Test
     @SmallTest
-    public void testGetOAuth2AccessTokenOnSuccess() throws AuthException {
+    public void testGetOAuth2AccessTokenOnSuccess() {
         final String scope = "oauth2:http://example.com/scope";
         mAccountManagerFacade.addAccount(ACCOUNT);
         final AccessTokenData expectedToken = mAccountManagerFacade.getAccessToken(ACCOUNT, scope);
@@ -123,7 +122,7 @@ public class ProfileOAuth2TokenServiceDelegateTest {
 
     @Test
     @SmallTest
-    public void testGetOAuth2AccessTokenOnFailure() throws AuthException {
+    public void testGetOAuth2AccessTokenOnFailure() {
         final String scope = "oauth2:http://example.com/scope";
         mAccountManagerFacade.addAccount(ACCOUNT);
         doReturn(null).when(mAccountManagerFacade).getAccessToken(any(Account.class), anyString());
