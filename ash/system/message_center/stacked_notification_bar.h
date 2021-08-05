86238d72ea7bba303f0f3f5f665d058d98fc86b9
Alex Newcomer <newcomer@chromium.org>
2021-08-05 23:02:47 +0000

    Fix multiple icons animating and jumpy animation
    For StackingNotificationBar, when ShiftLeft is called due to a scroll,
    we AnimateOut() the first icon and immediately add the backfill icon.
    
    This results in a jumping label (+n notifications).
    
    Instead of this, just add the icon after the animation completes.
    
    Additionally, the old code allowed multiple icons to animate out at
    once, even if the icon is long scrolled away. Instead, once the next
    icon is AnimateOut()-ed, stop animating the current one. The result is
    a much better looking animation.
    
    Bug: 1228767
    Change-Id: Ie3f54e322ce71f9aafd4af57af9d11e648ecbce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057742
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909100}

--- ash/system/message_center/stacked_notification_bar.h
+++ ash/system/message_center/stacked_notification_bar.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/message_center/message_center_scroll_bar.h"
 #include "ash/system/message_center/unified_message_center_view.h"
 #include "ash/system/message_center/unified_message_list_view.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/gfx/animation/animation_delegate.h"
 #include "ui/message_center/message_center_observer.h"
@@ -26,8 +27,8 @@ class Notification;
 namespace ash {
 
 // The header shown above the notification list displaying the number of hidden
-// notifications. There are currently two UI implementations toggled by the
-// NotificationStackedBarRedesign feature flag.
+// notifications. Has a dynamic list of icons which hide/show as notifications
+// are scrolled.
 class StackedNotificationBar : public views::View,
                                public message_center::MessageCenterObserver {
  public:
@@ -51,9 +52,6 @@ class StackedNotificationBar : public views::View,
   // Set notification bar state to expanded.
   void SetExpanded();
 
-  // Clean up icon view after it's removal animation is complete.
-  void OnIconAnimatedOut(views::View* icon);
-
   // views::View:
   void OnPaint(gfx::Canvas* canvas) override;
   const char* GetClassName() const override;
@@ -67,8 +65,14 @@ class StackedNotificationBar : public views::View,
   class StackedNotificationBarIcon;
   friend class UnifiedMessageCenterViewTest;
 
-  // Get the first icon which is not animating out.
-  StackedNotificationBarIcon* GetFrontIcon();
+  // Clean up icon view after it's removal animation is complete, adds an icon
+  // for `notification` if needed. Called from a callback registered in
+  // `ShiftIconsLeft()`.
+  void OnIconAnimatedOut(message_center::Notification* notification,
+                         views::View* icon);
+
+  // Get the first icon which is `animating_out`.
+  StackedNotificationBarIcon* GetFrontIcon(bool animating_out);
 
   // Search for a icon view in the stacked notification bar based on a provided
   // notification id.
@@ -106,6 +110,9 @@ class StackedNotificationBar : public views::View,
   views::Label* const count_label_;
   views::Button* const clear_all_button_;
   views::Button* const expand_all_button_;
+
+  base::WeakPtrFactory<StackedNotificationBar> weak_ptr_factory_{this};
+
   DISALLOW_COPY_AND_ASSIGN(StackedNotificationBar);
 };
 
