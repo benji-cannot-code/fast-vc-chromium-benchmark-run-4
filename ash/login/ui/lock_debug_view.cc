6cedc4d8589e9c93a6cc33b785c4dd0b4b1fdbc4
Joshua Pawlicki <waffles@chromium.org>
2021-08-05 19:11:49 +0000

    Revert "Remove `delete_children` RemoveAllChildViews() arg"
    This reverts commit d6240fed8c4d31c9920a32df6f10b469b29ce643.
    
    Reason for revert: compile failure https://cr-buildbucket.appspot.com/build/8839712642281727265
    
    Original change's description:
    > Remove `delete_children` RemoveAllChildViews() arg
    >
    > This removes the last few call sites with non-default arguments.
    > TriView::RemoveAllChildren() was the only remaining one with a
    > non-constant argument but it turns out to be dead code.
    >
    > Bug: 1044687
    > Change-Id: Ifc233b1613a9b49384c82cdbd7810914b8f8cc07
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074817
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Peter Boström <pbos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#908973}
    
    Bug: 1044687
    Change-Id: Ic23291890eef38d00402ce09b0849171063e8c02
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3075263
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Owners-Override: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908980}

--- ash/login/ui/lock_debug_view.cc
+++ ash/login/ui/lock_debug_view.cc
@@ -1056,7 +1056,8 @@ void LockDebugView::CycleAuthErrorMessage() {
 }
 
 void LockDebugView::UpdatePerUserActionContainer() {
-  per_user_action_view_container_->RemoveAllChildViews();
+  per_user_action_view_container_->RemoveAllChildViews(
+      true /*delete_children*/);
 
   int num_users = debug_data_dispatcher_->GetUserCount();
   for (int i = 0; i < num_users; ++i) {
@@ -1156,7 +1157,8 @@ void LockDebugView::UpdatePerUserActionContainerAndLayout() {
 }
 
 void LockDebugView::UpdateDetachableBaseColumn() {
-  global_action_detachable_base_group_->RemoveAllChildViews();
+  global_action_detachable_base_group_->RemoveAllChildViews(
+      true /*delete_children*/);
 
   AddButton("Debug detachable base",
             base::BindRepeating(
