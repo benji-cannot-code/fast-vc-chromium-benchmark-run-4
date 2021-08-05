9a5f0e12d163df4f093e81e41e7f013b310cae6c
Toni Barzic <tbarzic@chromium.org>
2021-08-05 18:17:26 +0000

    Start cardified state when only once drag icon is created
    Changes the timing when apps grid enters cardified state. Cardified
    state is now entered only after drag vector exceeds drag threshold, and
    app drag icon gets created. This avoid an edge case where drag view may
    get clipped by grid bounds during transition to cardified state.
    Also, it will allow simplifying logic for updating drag view
    bounds/transforms during cardified state transition, as the transition
    will happen only when the original drag view gets hidden (coming in a
    separate CL).
    
    While here, fixes the cardified entry animations for RTL - when
    calculating view transforms, we have to mirror bounds in RTL UI. Views
    framework mirrors bounds during layout, but this was not taken into
    account while calculating transforms for cardified state entry.
    
    BUG=1234002
    
    Change-Id: I76ea3270ec0b200618f1eeba8dfdea40b291d53c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064290
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908953}

--- ash/app_list/views/apps_grid_view.cc
+++ ash/app_list/views/apps_grid_view.cc
@@ -501,8 +501,6 @@ bool AppsGridView::UpdateDragFromItem(bool is_touch,
   if (!drag_view_)
     return false;  // Drag canceled.
 
-  MaybeStartCardifiedView();
-
   gfx::Point drag_point_in_grid_view;
   ExtractDragLocation(event.root_location(), &drag_point_in_grid_view);
   const Pointer pointer = is_touch ? TOUCH : MOUSE;
@@ -531,8 +529,11 @@ void AppsGridView::UpdateDrag(Pointer pointer, const gfx::Point& point) {
 
   gfx::Vector2d drag_vector(point - drag_start_grid_view_);
 
-  if (!IsDragging() && ExceededDragThreshold(drag_vector))
-    TryStartDragAndDropHostDrag(pointer);
+  if (ExceededDragThreshold(drag_vector)) {
+    if (!IsDragging())
+      TryStartDragAndDropHostDrag(pointer);
+    MaybeStartCardifiedView();
+  }
 
   if (drag_pointer_ != pointer)
     return;
