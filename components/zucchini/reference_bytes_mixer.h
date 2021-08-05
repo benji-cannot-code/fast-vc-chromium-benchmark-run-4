8093fe953f1ede75a16925ce95ae2f0bfa4b5c91
Samuel Huang <huangs@chromium.org>
2021-08-05 16:46:38 +0000

    [Zucchini] Add ARM support for ELF files.
    This CL enables ARM-ELF (AArch32 and AArch64) support in Zucchini.
    * Define ARM {AArch32, AArch64}ReferenceType.
    * Add Rel32Finder{Arm, AArch32, AArch64} (with tests) to use
      previously-added ARM disassembly code to extract rel32 references.
    * Add DisassemblerElf{Arm, AArch32, AArch64} to parse ARM ELF files and
      create reference readers / writers, and reference groups.
      * For AArch32: Add heuristic detection of ARM vs. Thumb2 mode.
    * Add IsTargetOffsetInElfSectionList() (with tests) to help ARM reject
      false positive references.
    * Add ReferenceBytesMixerElfArm to remove redundant reference target
      information from bytewise correction data.
    
    Bug: 918867
    Change-Id: I1e6d3d8b8d174c85a3d44ca6d642b7ff0bd6a6a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2922822
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908913}

--- components/zucchini/reference_bytes_mixer.h
+++ components/zucchini/reference_bytes_mixer.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/zucchini/buffer_view.h"
 #include "components/zucchini/image_utils.h"
+#include "components/zucchini/rel32_utils.h"
 
 namespace zucchini {
 
@@ -82,6 +83,35 @@ class ReferenceBytesMixer {
                               offset_t old_offset,
                               ConstBufferView new_view,
                               offset_t new_offset);
+};
+
+// In AArch32 and AArch64, instructions mix operation bits and payload bits in
+// complex ways. This is the main use case of ReferenceBytesMixer.
+class ReferenceBytesMixerElfArm : public ReferenceBytesMixer {
+ public:
+  // |exe_type| must be EXE_TYPE_ELF_ARM or EXE_TYPE_ELF_AARCH64.
+  explicit ReferenceBytesMixerElfArm(ExecutableType exe_type);
+  ReferenceBytesMixerElfArm(const ReferenceBytesMixerElfArm&) = delete;
+  const ReferenceBytesMixerElfArm& operator=(const ReferenceBytesMixerElfArm&) =
+      delete;
+  ~ReferenceBytesMixerElfArm() override;
+
+  // ReferenceBytesMixer:
+  int NumBytes(uint8_t type) const override;
+  ConstBufferView Mix(uint8_t type,
+                      ConstBufferView old_view,
+                      offset_t old_offset,
+                      ConstBufferView new_view,
+                      offset_t new_offset) override;
+
+ private:
+  ArmCopyDispFun GetCopier(uint8_t type) const;
+
+  // For simplicity, 32-bit vs. 64-bit distinction is represented by state
+  // |exe_type_|, instead of creating derived classes.
+  const ExecutableType exe_type_;
+
+  std::vector<uint8_t> out_buffer_;
 };
 
 }  // namespace zucchini
