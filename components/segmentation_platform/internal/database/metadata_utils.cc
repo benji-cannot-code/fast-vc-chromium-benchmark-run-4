1bcaa8bcfd71b5d5b808b1508e49f5a54e47c58a
Tommy Nyquist <nyquist@chromium.org>
2021-08-05 23:14:31 +0000

    [segmentation_platform] Set and validate feature name hash
    For segmentation model metadata some features have both the name and
    the name hash set. For those features, we should verify that the name
    and the name_hash match using the base::HashMetricName("feature name")
    functionality. This is important to add support for, because these are
    easy to get wrong, and it would lead to a surprise with a model
    only partly working, but with no errors.
    
    In addition, since the name hash is dependent on the feature name, this
    CL also starts force-overwriting the name hash based on the name.
    
    This CL adds support for this in code and metrics, and also updates all
    relevant tests. To make the tests easier to read and write, we are
    currently using base::HashMetricName directly in tests, instead of the
    result.
    
    Bug: 1236827
    Change-Id: I0f03081868b74fe674241dfebc77d7daba670abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072569
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909104}

--- components/segmentation_platform/internal/database/metadata_utils.cc
+++ components/segmentation_platform/internal/database/metadata_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/segmentation_platform/internal/database/metadata_utils.h"
 
+#include "base/metrics/metrics_hashes.h"
 #include "base/notreached.h"
 #include "base/time/time.h"
 #include "components/optimization_guide/proto/models.pb.h"
@@ -75,6 +76,11 @@ ValidationResult ValidateMetadataFeature(const proto::Feature& feature) {
   if (feature.name_hash() == 0)
     return ValidationResult::kFeatureNameHashNotFound;
 
+  if (!feature.name().empty() &&
+      base::HashMetricName(feature.name()) != feature.name_hash()) {
+    return ValidationResult::kFeatureNameHashDoesNotMatchName;
+  }
+
   if (feature.aggregation() == proto::Aggregation::UNKNOWN)
     return ValidationResult::kFeatureAggregationNotFound;
 
@@ -107,6 +113,14 @@ ValidationResult ValidateSegmentInfoMetadataAndFeatures(
     return segment_info_result;
 
   return ValidateMetadataAndFeatures(segment_info.model_metadata());
+}
+
+void SetFeatureNameHashesFromName(
+    proto::SegmentationModelMetadata* model_metadata) {
+  for (int i = 0; i < model_metadata->features_size(); ++i) {
+    proto::Feature* feature = model_metadata->mutable_features(i);
+    feature->set_name_hash(base::HashMetricName(feature->name()));
+  }
 }
 
 bool HasExpiredOrUnavailableResult(const proto::SegmentInfo& segment_info) {
