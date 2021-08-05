46de2bd20f045200ba6becd921e6aae2326e1a50
Alexey Baskakov <loyso@chromium.org>
2021-08-05 00:50:20 +0000

    Revert "Reland "Do not expose aria-errormessage when aria-invalid is false""
    This reverts commit ebd839c9b6e68ff542eba172e38886ecd59cb329.
    
    Reason for revert: Fails
    All/DumpAccessibilityTreeTest.AccessibilityAriaErrorMessage/linux
    test
    on linux-xenial-rel.
    
    Example failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-xenial-rel/12274/overview
    
    Original change's description:
    > Reland "Do not expose aria-errormessage when aria-invalid is false"
    >
    > This is a reland of commit 5b6bfaae62d8c6a5065fe4e1ceff4ddfc9909042.
    >
    > It also adds the ability to have specific expectations files for
    > Trusty in the content browser tests.
    >
    > The reason the change was reverted is that Trusty runs a much older
    > version of ATK which does not support the ATK relation types used
    > for aria-errormessage. There are quite a few other things that are
    > not supported in the runtime version of ATK on Trusty. Thus having
    > Trusty-specific expectations will give us the ability to test more
    > things via content browser tests and using fewer workarounds.
    >
    > Original change's description:
    > > Do not expose aria-errormessage when aria-invalid is false
    > >
    > > ARIA 1.2 added the following requirement:
    > >
    > >   User agents MUST NOT expose aria-errormessage for an object with an
    > >   aria-invalid value of false.
    > >
    > > We were exposing it unconditionally. This commit fixes that.
    > >
    > > AX-Relnotes: Implement ARIA 1.2 requirement to not expose
    > > aria-errormessage when aria-invalid is false.
    > >
    > > Bug: 1233978
    > >
    > > Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    > > Change-Id: I9cc8e654ea22919c105e331d88fd69d49990494c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062841
    > > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > > Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    > > Cr-Commit-Position: refs/heads/master@{#907136}
    >
    > Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel,linux-trusty-rel
    > Change-Id: I426b47a440cd28a687b272227bcb95239cb5f6e9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3065525
    > Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#908653}
    
    Change-Id: I6de352cade74ac101fe9e094d88c9793777e2226
    Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel,linux-trusty-rel
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3070782
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Owners-Override: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908703}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -4481,9 +4481,6 @@ void AXNodeObject::HandleActiveDescendantChanged() {
 }
 
 AXObject* AXNodeObject::ErrorMessage() const {
-  if (GetInvalidState() == ax::mojom::blink::InvalidState::kFalse)
-    return nullptr;
-
   // Check for aria-errormessage.
   Element* existing_error_message =
       GetAOMPropertyOrARIAAttribute(AOMRelationProperty::kErrorMessage);
