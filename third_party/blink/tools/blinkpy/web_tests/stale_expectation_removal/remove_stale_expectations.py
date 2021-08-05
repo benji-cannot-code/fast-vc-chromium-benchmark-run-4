80eb35831c1c3921bb8bcdd7c266980c3ffbff69
Brian Sheedy <bsheedy@chromium.org>
2021-08-05 16:00:50 +0000

    Add web test expectation removal builders module
    Adds blinkpy.web_tests.stale_expectation_removal.builders and its
    associated unittests.
    
    As a side-effect, also updates some of the shared and GPU code to
    accept an iterable for a certain field instead of a single
    string.
    
    Bug: 1222827
    Change-Id: Ib06549f77b69b2a0400dafddd087121810dc1887
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069392
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#908898}

--- third_party/blink/tools/blinkpy/web_tests/stale_expectation_removal/remove_stale_expectations.py
+++ third_party/blink/tools/blinkpy/web_tests/stale_expectation_removal/remove_stale_expectations.py
@@ -9,6 +9,9 @@ import sys
 
 assert sys.version_info[0] == 3
 
+from blinkpy.web_tests.stale_expectation_removal import builders
+from unexpected_passes_common import builders as common_builders
+
 
 def ParseArgs():
     parser = argparse.ArgumentParser(description=(
@@ -79,6 +82,8 @@ def main():
     raise RuntimeError(
         'Script is still under active development and not currently functional'
     )
+    builders_instance = builders.WebTestBuilders()
+    common_builders.RegisterInstance(builders_instance)
     return 0
 
 
