ddf30cbb9b09d3ac6213a76e873f9817d571da47
Cathy Li <chili@chromium.org>
2021-08-06 18:44:00 +0000

    Use a BadgeDrawable instead of FrameLayout to place the new content indicator at a more aesthetic spot.
    Also updates the "For you" text to match Discover branding.
    
    Change-Id: I4847246e60712162f103ad2be895cf81b6f2a1dd
    
    Bug: b/194830873, b/191077133
    Change-Id: I4847246e60712162f103ad2be895cf81b6f2a1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3039283
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909379}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -254,7 +254,6 @@ public class FeedSurfaceMediator
     // that Feed content is still loading at that time and the {@link mContentFirstAvailableTimeMs}
     // hasn't been set yet.
     private boolean mHasPendingUmaRecording;
-    private int mToggleswitchMenuIndex;
     private ScrollState mRestoreScrollState;
 
     private final HashMap<Integer, Stream> mTabToStreamMap = new HashMap<>();
@@ -830,7 +829,7 @@ public class FeedSurfaceMediator
         final int sectionHeaderStringId;
 
         if (ChromeFeatureList.isEnabled(ChromeFeatureList.WEB_FEED) && isSignedIn()) {
-            sectionHeaderStringId = R.string.ntp_for_you;
+            sectionHeaderStringId = R.string.ntp_discover_on;
         } else if (isDefaultSearchEngineGoogle) {
             sectionHeaderStringId =
                     isExpanded ? R.string.ntp_discover_on : R.string.ntp_discover_off;
@@ -869,7 +868,6 @@ public class FeedSurfaceMediator
         itemList.add(getMenuToggleSwitch(
                 mSectionHeaderModel.get(SectionHeaderListProperties.IS_SECTION_ENABLED_KEY),
                 iconId));
-        mToggleswitchMenuIndex = itemList.size() - 1;
         return itemList;
     }
 
