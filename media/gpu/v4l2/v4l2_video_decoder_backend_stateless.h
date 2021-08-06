de101c10cac76a80e56bae4f1031d609dd1ecc70
Miguel Casas <mcasas@chromium.org>
2021-08-06 19:39:18 +0000

    media/gpu/v4l2 (stateless): implement PlatformVideoDecoding histogram
    Media.PlatformVideoDecoding.Decode is a histogram collected by ChromeOS
    Tast performance tests [1] to figure out how long it takes to decode
    each video frame. It's implemented for VA-API platforms [2]; this CL
    introduces it for the V4L2 stateless backend.
    
    Verified running it in Tast [2].
    
    [1] https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/platform/tast-tests/src/chromiumos/tast/local/bundles/cros/video/playback/playback.go;l=142;drc=f8e4ab458c3664f55408e81355d670451f847bbe
    [2] tast run <asurada ip> video.PlaybackPerf.h264_720p_30fps_hw
    
    Bug: b:190615065
    Change-Id: I7d8eff2950ab1ada253c7c94824fd9d3d4b4a6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077166
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909419}

--- media/gpu/v4l2/v4l2_video_decoder_backend_stateless.h
+++ media/gpu/v4l2/v4l2_video_decoder_backend_stateless.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/mru_cache.h"
 #include "base/containers/queue.h"
+#include "base/containers/small_map.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/sequenced_task_runner.h"
@@ -172,6 +173,9 @@ class V4L2StatelessVideoDecoderBackend : public V4L2VideoDecoderBackend,
 
   // Reference to request queue to get free requests.
   V4L2RequestsQueue* requests_queue_;
+
+  // Map of enqueuing timestamps to wall clock, for histogramming purposes.
+  base::small_map<std::map<int64_t, base::TimeTicks>> encoding_timestamps_;
 
   base::WeakPtr<V4L2StatelessVideoDecoderBackend> weak_this_;
   base::WeakPtrFactory<V4L2StatelessVideoDecoderBackend> weak_this_factory_{
