5b39bc954a72b803d7907ad0588741e14d953838
Brian Geffon <bgeffon@chromium.org>
2021-08-06 19:57:56 +0000

    CrOS: Userspace Swap: Add additional PA mem area code.
    This CL introduces code to transfer a dedicated memory region
    as described in (go/userspace-swap) to be used as a destination
    for the MREMAP_DONTUNMAP operations. This memory area is chosen at
    random in each renderer and transferred with the userfaultfd.
    
    This CL additionally introduces code to query PA on swap and then
    determine which of those regions is resident. This final set of areas
    will be the areas which can be swapped (landing in the next CL).
    
    BUG=b:172223747
    
    Change-Id: I182c5350312e7d4121ae4931653fb513c81ca2a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059361
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909430}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -39,8 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "base/allocator/buildflags.h"
+#if defined(ARCH_CPU_X86_64)
 #include "chrome/browser/performance_manager/policies/userspace_swap_policy_chromeos.h"
-
+#endif  // defined(ARCH_CPU_X86_64)
 #if BUILDFLAG(USE_TCMALLOC)
 #include "chrome/browser/performance_manager/policies/dynamic_tcmalloc_policy_chromeos.h"
 #include "chrome/common/performance_manager/mojom/tcmalloc.mojom.h"
@@ -101,11 +102,13 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
   }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#if defined(ARCH_CPU_X86_64)
   if (performance_manager::policies::UserspaceSwapPolicy::
           UserspaceSwapSupportedAndEnabled()) {
     graph->PassToGraph(
         std::make_unique<performance_manager::policies::UserspaceSwapPolicy>());
   }
+#endif  // defined(ARCH_CPU_X86_64)
 
 #if BUILDFLAG(USE_TCMALLOC)
   if (base::FeatureList::IsEnabled(
--- chrome/renderer/browser_exposed_renderer_interfaces.cc
+++ chrome/renderer/browser_exposed_renderer_interfaces.cc
@@ -25,7 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "base/allocator/buildflags.h"
+#if defined(ARCH_CPU_X86_64)
 #include "chrome/renderer/performance_manager/mechanisms/userspace_swap_impl_chromeos.h"
+#endif  // defined(ARCH_CPU_X86_64)
 #if BUILDFLAG(USE_TCMALLOC)
 #include "chrome/common/performance_manager/mojom/tcmalloc.mojom.h"
 #include "chrome/renderer/performance_manager/mechanisms/tcmalloc_tunables_impl.h"
@@ -66,6 +68,7 @@ void ExposeChromeRendererInterfacesToBrowser(
                base::SequencedTaskRunnerHandle::Get());
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#if defined(ARCH_CPU_X86_64)
   if (performance_manager::mechanism::UserspaceSwapImpl::
           PlatformSupportsUserspaceSwap()) {
     binders->Add(
@@ -73,6 +76,7 @@ void ExposeChromeRendererInterfacesToBrowser(
             &performance_manager::mechanism::UserspaceSwapImpl::Create),
         base::SequencedTaskRunnerHandle::Get());
   }
+#endif  // defined(ARCH_CPU_X86_64)
 
 #if BUILDFLAG(USE_TCMALLOC)
   binders->Add(
