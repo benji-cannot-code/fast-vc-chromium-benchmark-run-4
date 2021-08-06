476db923fd1cf18b44e0f15dc6d80841dd31fb58
Lijin Shen <lazzzis@google.com>
2021-08-06 20:47:22 +0000

    [Messages] Rename flag of Grouped Permission to Notification Blocked
    Bug: 1230927
    Change-Id: I8943dfc3c326660519a59abd77f2b9cc64337ff5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069866
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909465}

--- components/messages/android/messages_feature.cc
+++ components/messages/android/messages_feature.cc
@@ -13,8 +13,8 @@ const base::Feature kMessagesForAndroidAdsBlocked{
 const base::Feature kMessagesForAndroidChromeSurvey{
     "MessagesForAndroidChromeSurvey", base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kMessagesForAndroidGroupedPermission{
-    "MessagesForAndroidGroupedPermission", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kMessagesForAndroidNotificationBlocked{
+    "MessagesForAndroidNotificationBlocked", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kMessagesForAndroidInfrastructure{
     "MessagesForAndroidInfrastructure", base::FEATURE_ENABLED_BY_DEFAULT};
@@ -70,9 +70,9 @@ bool IsUpdatePasswordMessagesUiEnabled() {
          base::FeatureList::IsEnabled(kMessagesForAndroidUpdatePassword);
 }
 
-bool IsGroupedPermissionMessagesUiEnabled() {
+bool IsNotificationBlockedMessagesUiEnabled() {
   return base::FeatureList::IsEnabled(kMessagesForAndroidInfrastructure) &&
-         base::FeatureList::IsEnabled(kMessagesForAndroidGroupedPermission);
+         base::FeatureList::IsEnabled(kMessagesForAndroidNotificationBlocked);
 }
 
 bool IsPermissionUpdateMessagesUiEnabled() {
