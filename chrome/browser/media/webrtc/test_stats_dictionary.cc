57fd2fa751ce1a130ad3c8df7e6363010b063161
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-06 09:32:17 +0000

    [CodeHealth] Remove deprecated ListValue::GetDouble()
    Follow-up to crrev.com/c/3071180, now removing the remaining uses in
    tools/ and test code, as well as deleting the deprecated API.
    DomDistillerPageFeaturesTest.TestCalculateDerivedFeatures was
    particularly hard to read and is rewritten.
    
    There is one behavior change: tools/ipc_fuzzer/fuzzer/fuzzer.cc now
    also generates empty values (i.e. with type base::Value::Type::NONE).
    
    Fixed: 1187088
    Change-Id: I64be5bd12df1065869f65986174e2a5c592e5f86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071186
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#909214}

--- chrome/browser/media/webrtc/test_stats_dictionary.cc
+++ chrome/browser/media/webrtc/test_stats_dictionary.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/json/json_writer.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace content {
 
@@ -177,17 +178,19 @@ bool TestStatsDictionary::GetSequenceBoolean(
 bool TestStatsDictionary::GetSequenceNumber(
     const std::string& key,
     std::vector<double>* out) const {
-  const base::ListValue* list;
-  if (!stats_->GetList(key, &list))
+  const base::Value* number_sequence = stats_->FindListKey(key);
+  if (!number_sequence)
     return false;
-  std::vector<double> sequence;
-  double element;
-  for (size_t i = 0; i < list->GetSize(); ++i) {
-    if (!list->GetDouble(i, &element))
+
+  out->clear();
+  for (const base::Value& element : number_sequence->GetList()) {
+    absl::optional<double> double_value = element.GetIfDouble();
+    if (!double_value)
       return false;
-    sequence.push_back(element);
+
+    out->push_back(*double_value);
   }
-  *out = std::move(sequence);
+
   return true;
 }
 
