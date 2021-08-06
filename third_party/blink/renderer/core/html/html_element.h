7945b1bd9fa83e4caaba7db160e98c2db0b49f26
Miyoung Shin <myid.shin@igalia.com>
2021-08-06 11:42:17 +0000

    Fix an element directionality
    This CL fixes the bugs raised up during the discussion of slots
    at https://github.com/whatwg/html/issues/3699.
     - Resolve the light tree element's directionality in the light tree
       and do not walk into the shadow tree for encapsulation.
     - Resolve the element's directionality when there is not a slotted
       node.
    
    Bug: 1236384
    Change-Id: Id22aa3e5b624a2e3234c645e198749360f3a050d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3066807
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#909242}

--- third_party/blink/renderer/core/html/html_element.h
+++ third_party/blink/renderer/core/html/html_element.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_ELEMENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_ELEMENT_H_
 
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/simulated_click_options.h"
@@ -228,8 +229,11 @@ class CORE_EXPORT HTMLElement : public Element {
   void AdjustDirectionalityIfNeededAfterChildAttributeChanged(Element* child);
   void AdjustDirectionalityIfNeededAfterChildrenChanged(
       const ChildrenChange& change);
-  TextDirection ResolveAutoDirectionality(bool& is_deferred,
-                                          Node* stay_within) const;
+
+  template <typename Traversal>
+  absl::optional<TextDirection> ResolveAutoDirectionality(
+      bool& is_deferred,
+      Node* stay_within) const;
 
   TranslateAttributeMode GetTranslateAttributeMode() const;
 
