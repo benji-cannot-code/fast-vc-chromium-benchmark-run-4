73ab211a8c5659602494f501c137770291e73626
Katie Dektar <katie@chromium.org>
2021-08-06 17:13:20 +0000

    Show the Dictation locale offline nudge after SODA availability change.
    When a user hasn't explicitly enabled Dictation or changed locales,
    for example on start-up or after SODA becomes available on a device,
    we show a nudge letting the user know their speech is no longer being
    sent to Google's servers, but instead processed on device.
    
    See design at go/cros-dictation-offline-nudge.
    Dictation nudge screenshot: https://screenshot.googleplex.com/6VSM47LDsDxjEbx
    
    AX-Relnotes: N/A
    Bug: 1232525
    Change-Id: I2889dc7e10d60329ab2d9264e348c2df46bb4ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069534
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909345}

--- ash/constants/ash_pref_names.h
+++ ash/constants/ash_pref_names.h
@@ -150,6 +150,8 @@ extern const char kAccessibilityDictationEnabled[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kAccessibilityDictationLocale[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const char kAccessibilityDictationLocaleOfflineNudge[];
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kShouldAlwaysShowAccessibilityMenu[];
 
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kAltTabPerDesk[];
