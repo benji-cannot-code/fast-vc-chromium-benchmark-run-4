d2cc5753902d04d77e34cc2b64e3a24c401afb9a
Nancy Wang <nancylingwang@chromium.org>
2021-08-06 18:49:08 +0000

    Modify the full restore notification.
    Change the full restore notification based on the latest ui decision:
    https://docs.google.com/presentation/d/1gZbfpG_-4Sum7-ETMcV8-gOXxjd97mhacA__uiCjM88/edit#slide=id.ge7aea646b0_8_5
    
    1. The Cancel button of the restore notification is changed from Cancel
    to Settings. If the user chooses “Settings”, open the settings app, to
    show the restore setting, and not close the notification.
    
    The Cancel button of the crash notification is not changed, still the
    Cancel button.
    
    2. If user clicks the notification body, restore the last session, and
    close the notification.
    
    This change applied to both the restore notification and the crash
    notification.
    
    BUG=1146900
    
    Change-Id: If139b991159a35d7ff7a686c2a460078524894e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074640
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909388}

--- chrome/browser/ash/full_restore/full_restore_service_unittest.cc
+++ chrome/browser/ash/full_restore/full_restore_service_unittest.cc
@@ -526,8 +526,8 @@ TEST_F(FullRestoreServiceTestHavingFullRestoreFile, AskEveryTimeAndRestore) {
 }
 
 // If the OS restore setting is 'Ask every time', after reboot, show the restore
-// notification, and verify the restore flag when click the cancel button.
-TEST_F(FullRestoreServiceTestHavingFullRestoreFile, AskEveryTimeAndCancel) {
+// notification, and verify the restore flag when click the Settings button.
+TEST_F(FullRestoreServiceTestHavingFullRestoreFile, AskEveryTimeAndSettings) {
   profile()->GetPrefs()->SetInteger(
       kRestoreAppsAndPagesPrefName,
       static_cast<int>(RestoreOption::kAskEveryTime));
@@ -538,6 +538,8 @@ TEST_F(FullRestoreServiceTestHavingFullRestoreFile, AskEveryTimeAndCancel) {
   VerifyNotification(false /* has_crash_notification */,
                      true /* has_restore_notification */);
 
+  // For the restore notification, the cancel button is used to show the full
+  // restore settings.
   SimulateClick(kRestoreNotificationId,
                 RestoreNotificationButtonIndex::kCancel);
 
@@ -545,7 +547,9 @@ TEST_F(FullRestoreServiceTestHavingFullRestoreFile, AskEveryTimeAndCancel) {
   EXPECT_FALSE(::full_restore::ShouldRestore(account_id()));
   EXPECT_TRUE(::full_restore::CanPerformRestore(account_id()));
 
-  VerifyNotification(false, false);
+  // Click the setting button, the restore notification should not be closed.
+  VerifyNotification(false /* has_crash_notification */,
+                     true /* has_restore_notification */);
 }
 
 // If the OS restore setting is 'Always', after reboot, don't show any
