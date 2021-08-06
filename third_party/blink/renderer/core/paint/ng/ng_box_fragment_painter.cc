c1a1351c45b98952f6fdf4b3e37c4f12cbd3029f
Kent Tamura <tkent@chromium.org>
2021-08-06 17:34:54 +0000

    SVG Text NG: Disable culled inlines in SVG <text>
    We'd like to call ScopedSVGPaintState for inline elements in SVG <text>.
    
    * LayoutInline::ComputeInitialShouldCreateBoxFragment():
      Disable culled-inlines for LayoutSVGInline.
    
    * FragmentItemsInVisualOrder() in ng_svg_text_query.cc:
      Fix NGFragmentItem iteration for non-culled-inlines.
    
    * LayoutSVGInline::ObjectBoundingBoxForCursor():
      Ditto.
    
    * NGBoxFragmentPainter::PaintBoxItem() and
      NGInlineBoxFragmentPainter::Paint().
      Move ScopedSVGPaintState instantiation. It should be instantiated
      after ScopedDisplayItemFragment in order to set a correct FragmentId.
    
    This CL affects svg/batik/text/smallFonts.svg and
    svg/batik/text/textFeatures.svg, however they still fail due to another
    issue.
    
    Bug: 1179585
    Change-Id: I0d672eaf93bd9b0673b7a44cb912e96bc6533588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3073420
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909356}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
@@ -1662,14 +1662,7 @@ void NGBoxFragmentPainter::PaintBoxItem(
     return;
   }
 
-  const LayoutObject* layout_object = child_fragment.GetLayoutObject();
   if (child_fragment.IsInlineBox()) {
-    if (layout_object->IsSVGInline()) {
-      ScopedSVGPaintState paint_state(*layout_object, paint_info);
-      NGInlineBoxFragmentPainter(cursor, item, child_fragment)
-          .Paint(paint_info, paint_offset);
-      return;
-    }
     NGInlineBoxFragmentPainter(cursor, item, child_fragment)
         .Paint(paint_info, paint_offset);
     return;
@@ -1677,7 +1670,7 @@ void NGBoxFragmentPainter::PaintBoxItem(
 
   // Block-in-inline
   DCHECK(RuntimeEnabledFeatures::LayoutNGBlockInInlineEnabled());
-  DCHECK(!layout_object->IsInline());
+  DCHECK(!child_fragment.GetLayoutObject()->IsInline());
   PaintInfo paint_info_for_descendants = paint_info.ForDescendants();
   paint_info_for_descendants.SetIsInFragmentTraversal();
   PaintBlockChild({&child_fragment, item.OffsetInContainerFragment()},
