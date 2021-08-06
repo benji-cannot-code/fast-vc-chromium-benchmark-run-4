c8d34c027b5eb9bf5c20b91988541aa0e9803c14
Caleb Raitto <caraitto@chromium.org>
2021-08-06 22:28:35 +0000

    Delete IdentifiabilityPaintOpDigest.
    Now that crrev.com/c/2611465 has landed, digests are computed without
    need of IdentifiabilityPaintOpDigest.
    
    Includes 2 small IWYU fixes needed to prevent build breakage due to the
    deletion (in web_frame_test.cc and webgl2_rendering_context_base.cc).
    
    Bug: 1096123,1086245,973801
    Change-Id: Icf15946d705be10dd1c1358bcd1c34c8fec4c584
    Fixed: 1096123,1086245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076064
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909517}

--- third_party/blink/renderer/core/frame/web_frame_test.cc
+++ third_party/blink/renderer/core/frame/web_frame_test.cc
@@ -211,6 +211,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 #include "third_party/blink/renderer/platform/wtf/hash_set.h"
+#include "third_party/skia/include/core/SkTextBlob.h"
 #include "ui/base/ime/mojom/text_input_state.mojom-blink.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/events/keycodes/dom/dom_key.h"
