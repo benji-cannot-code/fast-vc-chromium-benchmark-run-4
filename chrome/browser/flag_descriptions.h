dd0dfe5d95e0cf0faa214e485f467de6d002b8f1
Orin Jaworski <orinj@google.com>
2021-08-06 22:51:38 +0000

    [omnibox] Add omnibox-pedals-translation-console flag
    This CL adds an about:flags entry to control the
    OmniboxPedalsTranslationConsole feature for functional testing.
    
    chrome://flags/#omnibox-pedals-translation-console
    
    Bug: 893183, 1224679
    Change-Id: Icab6e0aabfa63c4f4f18b51b2e8dc015cfff9084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076527
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909526}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2222,6 +2222,9 @@ extern const char kOmniboxPedalsBatch3Description[];
 extern const char kOmniboxPedalsDefaultIconColoredName[];
 extern const char kOmniboxPedalsDefaultIconColoredDescription[];
 
+extern const char kOmniboxPedalsTranslationConsoleName[];
+extern const char kOmniboxPedalsTranslationConsoleDescription[];
+
 extern const char kOmniboxKeywordSearchButtonName[];
 extern const char kOmniboxKeywordSearchButtonDescription[];
 
