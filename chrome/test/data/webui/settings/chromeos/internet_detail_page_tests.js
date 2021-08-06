1c0b6698e9e7eec732f6943671c8c26e3c3f6547
Chad Duffin <chadduffin@chromium.org>
2021-08-06 15:38:28 +0000

    [CrOS Cellular] Internet detail page cellular toggle fixes.
    This change adds a default for the cros.signed.data_roaming_enabled pref
    and provides defaults for whether roaming is enabled via managed
    properties to allow the tests to run properly with and without the
    per-network roaming flag enabled.
    
    Bug: 1220691
    Test: Verified via unit tests.
    Change-Id: I7db457dde618a79b0a20d2c3532261bf0213e969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076191
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909308}

--- chrome/test/data/webui/settings/chromeos/internet_detail_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/internet_detail_page_tests.js
@@ -34,6 +34,15 @@ suite('InternetDetailPage', function() {
       type: chrome.settingsPrivate.PrefType.BOOLEAN,
       value: true,
     },
+    'cros': {
+      'signed': {
+        'data_roaming_enabled': {
+          key: 'data_roaming_enabled',
+          value: true,
+          controlledBy: chrome.settingsPrivate.ControlledBy.DEVICE_POLICY,
+        },
+      },
+    },
     // Added use_shared_proxies because triggering a change in prefs_ without
     // it will fail a "Pref is missing" assertion in the network-proxy-section
     'settings': {
@@ -595,6 +604,9 @@ suite('InternetDetailPage', function() {
           getManagedProperties(mojom.NetworkType.kCellular, 'cellular');
       cellularNetwork.typeProperties.cellular.iccid = test_iccid;
       cellularNetwork.connectable = false;
+      // Required for allowDataRoamingButton to be rendered.
+      cellularNetwork.typeProperties.cellular.allowRoaming =
+          OncMojo.createManagedBool(false);
       mojoApi_.setManagedPropertiesForTest(cellularNetwork);
 
       // Set SIM as active so that configurable sections are displayed.
@@ -683,6 +695,9 @@ suite('InternetDetailPage', function() {
           const cellularNetwork = getManagedProperties(
               mojom.NetworkType.kCellular, 'cellular', mojom.OncSource.kDevice);
           cellularNetwork.typeProperties.cellular.iccid = test_iccid;
+          // Required for allowDataRoamingButton to be rendered.
+          cellularNetwork.typeProperties.cellular.allowRoaming =
+              OncMojo.createManagedBool(false);
 
           mojoApi_.setManagedPropertiesForTest(cellularNetwork);
           internetDetailPage.init('cellular_guid', 'Cellular', 'cellular');
@@ -863,6 +878,9 @@ suite('InternetDetailPage', function() {
           mojom.NetworkType.kCellular, 'cellular', mojom.OncSource.kDevice);
       // Required for connectDisconnectButton to be rendered.
       cellularNetwork.connectionState = mojom.ConnectionStateType.kConnected;
+      // Required for allowDataRoamingButton to be rendered.
+      cellularNetwork.typeProperties.cellular.allowRoaming =
+          OncMojo.createManagedBool(false);
       // Required for advancedFields to be rendered.
       cellularNetwork.typeProperties.cellular.networkTechnology = 'LTE';
       // Required for infoFields to be rendered.
