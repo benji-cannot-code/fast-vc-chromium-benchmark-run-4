88ecbe33c7b1be1c360f2d064ef012355c9a9edf
Hiroki Nakagawa <nhiroki@chromium.org>
2021-08-06 02:58:48 +0000

    Prerender: Remove unnecessary PrerenderHost::ReservationInfo struct
    PrerenderHost::ReservationInfo struct was introduced for implementing
    cancellation during activation. However, we made prerender activation
    synchronous instead of cancellable, so the struct is no longer
    necessary.
    
    Bug: 1195751
    Change-Id: I0b9d250eff057359cb232751b53d363865c2b835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074706
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909166}

--- content/browser/prerender/prerender_host_registry.cc
+++ content/browser/prerender/prerender_host_registry.cc
@@ -189,9 +189,8 @@ int PrerenderHostRegistry::ReserveHostToActivate(
   DCHECK_EQ(host_id, host->frame_tree_node_id());
 
   // Reserve the host for activation.
-  auto result = reserved_prerender_host_by_frame_tree_node_id_.try_emplace(
-      host_id, std::move(host),
-      navigation_request.frame_tree_node()->frame_tree_node_id());
+  auto result = reserved_prerender_host_by_frame_tree_node_id_.emplace(
+      host_id, std::move(host));
   DCHECK(result.second);
 
   return host_id;
@@ -204,7 +203,7 @@ RenderFrameHostImpl* PrerenderHostRegistry::GetRenderFrameHostForReservedHost(
   if (iter == reserved_prerender_host_by_frame_tree_node_id_.end()) {
     return nullptr;
   }
-  return iter->second.prerender_host->GetPrerenderedMainFrameHost();
+  return iter->second->GetPrerenderedMainFrameHost();
 }
 
 std::unique_ptr<StoredPage> PrerenderHostRegistry::ActivateReservedHost(
@@ -213,8 +212,7 @@ std::unique_ptr<StoredPage> PrerenderHostRegistry::ActivateReservedHost(
   auto iter =
       reserved_prerender_host_by_frame_tree_node_id_.find(frame_tree_node_id);
   CHECK(iter != reserved_prerender_host_by_frame_tree_node_id_.end());
-  std::unique_ptr<PrerenderHost> prerender_host =
-      std::move(iter->second.prerender_host);
+  std::unique_ptr<PrerenderHost> prerender_host = std::move(iter->second);
   reserved_prerender_host_by_frame_tree_node_id_.erase(iter);
   return prerender_host->Activate(navigation_request);
 }
@@ -268,7 +266,7 @@ PrerenderHost* PrerenderHostRegistry::FindReservedHostById(
       reserved_prerender_host_by_frame_tree_node_id_.find(frame_tree_node_id);
   if (iter == reserved_prerender_host_by_frame_tree_node_id_.end())
     return nullptr;
-  return iter->second.prerender_host.get();
+  return iter->second.get();
 }
 
 std::vector<RenderFrameHostImpl*>
@@ -278,7 +276,7 @@ PrerenderHostRegistry::GetPrerenderedMainFrames() {
     result.push_back(i.second->GetPrerenderedMainFrameHost());
   }
   for (auto& i : reserved_prerender_host_by_frame_tree_node_id_) {
-    result.push_back(i.second.prerender_host->GetPrerenderedMainFrameHost());
+    result.push_back(i.second->GetPrerenderedMainFrameHost());
   }
   return result;
 }
@@ -382,17 +380,5 @@ void PrerenderHostRegistry::NotifyTrigger(const GURL& url) {
   for (Observer& obs : observers_)
     obs.OnTrigger(url);
 }
-
-PrerenderHostRegistry::ReservationInfo::ReservationInfo(
-    std::unique_ptr<PrerenderHost> prerender_host,
-    int activator_frame_tree_node_id)
-    : prerender_host(std::move(prerender_host)),
-      activator_frame_tree_node_id(activator_frame_tree_node_id) {}
-
-PrerenderHostRegistry::ReservationInfo::ReservationInfo(ReservationInfo&& info)
-    : ReservationInfo(std::move(info.prerender_host),
-                      info.activator_frame_tree_node_id) {}
-
-PrerenderHostRegistry::ReservationInfo::~ReservationInfo() = default;
 
 }  // namespace content
