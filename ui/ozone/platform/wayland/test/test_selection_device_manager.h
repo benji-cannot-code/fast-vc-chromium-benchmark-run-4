ec5a32be736e523607b34d89d7200f3e4688ee53
Nick Diego Yamane <nickdiego@igalia.com>
2021-08-06 16:59:30 +0000

    ozone/wayland: Move more stuff to TestSelectionSource
    More methods and variables, previously part of wl::TestDataSource, need
    to be moved to the base class wl::TestSelectionSource, so they can be
    transparently used in clipboard test code (upcoming CL).
    
    R=tonikitoo@igalia.com
    
    Bug: 1204670
    Change-Id: I30647b18fb2d60ae467f81d21432490fd0be445c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3057774
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#909337}

--- ui/ozone/platform/wayland/test/test_selection_device_manager.h
+++ ui/ozone/platform/wayland/test/test_selection_device_manager.h
@@ -8,10 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
+#include "base/callback_forward.h"
+#include "base/files/scoped_file.h"
+#include "base/memory/scoped_refptr.h"
 #include "ui/ozone/platform/wayland/test/global_object.h"
 #include "ui/ozone/platform/wayland/test/server_object.h"
 #include "ui/ozone/public/platform_clipboard.h"
+
+namespace base {
+class SequencedTaskRunner;
+}
 
 namespace wl {
 
@@ -94,7 +102,9 @@ class TestSelectionOffer : public ServerObject {
 class TestSelectionSource : public ServerObject {
  public:
   struct Delegate {
-    virtual void HandleOffer(const std::string& mime_type) = 0;
+    virtual void SendSend(const std::string& mime_type,
+                          base::ScopedFD write_fd) = 0;
+    virtual void SendCancelled() = 0;
     virtual void OnDestroying() = 0;
 
    protected:
@@ -104,6 +114,13 @@ class TestSelectionSource : public ServerObject {
   TestSelectionSource(wl_resource* resource, Delegate* delegate);
   ~TestSelectionSource() override;
 
+  using ReadDataCallback = base::OnceCallback<void(std::vector<uint8_t>&&)>;
+  void ReadData(const std::string& mime_type, ReadDataCallback callback);
+
+  void OnCancelled();
+
+  const std::vector<std::string>& mime_types() const { return mime_types_; }
+
   // Protocol object requests:
   static void Offer(struct wl_client* client,
                     struct wl_resource* resource,
@@ -111,6 +128,9 @@ class TestSelectionSource : public ServerObject {
 
  private:
   Delegate* const delegate_;
+
+  std::vector<std::string> mime_types_;
+  const scoped_refptr<base::SequencedTaskRunner> task_runner_;
 };
 
 class TestSelectionDevice : public ServerObject {
