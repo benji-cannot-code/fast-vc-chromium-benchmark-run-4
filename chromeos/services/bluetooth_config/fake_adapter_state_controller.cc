edb7791bbc690f9d5efe1bcbf84d6cb06b9b1722
Chad Duffin <chadduffin@chromium.org>
2021-08-06 23:31:34 +0000

    [CrOS Bluetooth] Run task loop after setting system state.
    This change adds base::RunLoop().RunUntilIdle() to the
    FakeAdapterStateController so that tests that rely on being notified on
    system state changes properly receive notifications.
    
    Bug: 1233175
    Test: Manually with unit tests.
    Change-Id: I9f15235d378642e095e5f60f939a4769cc542015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078891
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909545}

--- chromeos/services/bluetooth_config/fake_adapter_state_controller.cc
+++ chromeos/services/bluetooth_config/fake_adapter_state_controller.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/services/bluetooth_config/fake_adapter_state_controller.h"
 
+#include "base/run_loop.h"
+
 namespace chromeos {
 namespace bluetooth_config {
 
@@ -19,6 +21,7 @@ void FakeAdapterStateController::SetSystemState(
 
   system_state_ = system_state;
   NotifyAdapterStateChanged();
+  base::RunLoop().RunUntilIdle();
 }
 
 mojom::BluetoothSystemState FakeAdapterStateController::GetAdapterState()
