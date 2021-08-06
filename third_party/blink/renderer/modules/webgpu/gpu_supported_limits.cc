fc7771c789ed1f208c5eca355397039a2b8b12ab
Brandon Jones <bajones@chromium.org>
2021-08-06 19:48:38 +0000

    Update two GPUSupportedLimit names
    These changes slipped into the spec immediately after I started my last
    patch.
    
    Bug: 1212868
    Change-Id: I6f4698ddf3bb566de1bdb27421c75efa5255a38e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078439
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909424}

--- third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_supported_limits.cc
@@ -41,13 +41,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       max_inter_stage_shader_components_)                                      \
   MAX(unsigned, maxComputeWorkgroupStorageSize,                                \
       max_compute_workgroup_storage_size_)                                     \
-  MAX(unsigned, maxComputeWorkgroupInvocations,                                \
-      max_compute_workgroup_invocations_)                                      \
+  MAX(unsigned, maxComputeInvocationsPerWorkgroup,                             \
+      max_compute_invocations_per_workgroup_)                                  \
   MAX(unsigned, maxComputeWorkgroupSizeX, max_compute_workgroup_size_x_)       \
   MAX(unsigned, maxComputeWorkgroupSizeY, max_compute_workgroup_size_y_)       \
   MAX(unsigned, maxComputeWorkgroupSizeZ, max_compute_workgroup_size_z_)       \
-  MAX(unsigned, maxComputePerDimensionDispatchSize,                            \
-      max_compute_per_dimension_dispatch_size_)
+  MAX(unsigned, maxComputeWorkgroupsPerDimension,                              \
+      max_compute_workgroups_per_dimension_)
 
 namespace blink {
 
