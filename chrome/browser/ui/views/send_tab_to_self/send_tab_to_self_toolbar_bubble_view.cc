57427af36333049ea3efb6f11ed6a6b26c4913a4
Kyle Milka <kmilka@chromium.org>
2021-08-06 20:00:56 +0000

    [STTS2.0] Record notification timeout
    Record when a STTS notification times out on desktop.
    
    Change-Id: I94b41f24bba7695821be7a3f2555889a743618e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072867
    Reviewed-by: Travis Skare <skare@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909432}

--- chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_toolbar_bubble_view.cc
+++ chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_toolbar_bubble_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_toolbar_icon_view.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/send_tab_to_self/metrics_util.h"
 #include "components/send_tab_to_self/send_tab_to_self_entry.h"
 #include "components/url_formatter/elide_url.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -128,6 +129,7 @@ void SendTabToSelfToolbarBubbleView::OpenInNewTab() {
 }
 
 void SendTabToSelfToolbarBubbleView::Timeout() {
+  send_tab_to_self::RecordNotificationTimedOut();
   GetWidget()->Close();
 }
 
