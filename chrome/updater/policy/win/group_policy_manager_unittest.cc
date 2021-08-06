d0d009e987bd9fc339bbf24c94d00aa698ea5a11
Michael Chang <donchan@microsoft.com>
2021-08-06 17:38:34 +0000

    Use Registry override for GroupPolicyManagerTests
    Currently the GroupPolicyManagerTests in chrome/updater do not work for
    medium integrity machines because the tests are trying to access HKLM
    directly. Instead of allowing the test to access HKLM directly, use
    RegistryOverride so that the tests don't need high integrity to run.
    
    Bug: 1237236
    Change-Id: Icde43054494d9453a7f21e0b48e59e9d184e9010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076055
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Michael Chang <donchan@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#909360}

--- chrome/updater/policy/win/group_policy_manager_unittest.cc
+++ chrome/updater/policy/win/group_policy_manager_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/test_reg_util_win.h"
 #include "base/win/registry.h"
 #include "base/win/win_util.h"
 #include "chrome/updater/win/win_constants.h"
@@ -23,6 +24,8 @@ class GroupPolicyManagerTests : public ::testing::Test {
   void SetUp() override;
   void TearDown() override;
 
+  registry_util::RegistryOverrideManager registry_override_;
+
  private:
   void DeletePolicyKey();
 };
@@ -36,6 +39,8 @@ void GroupPolicyManagerTests::TearDown() {
 }
 
 void GroupPolicyManagerTests::DeletePolicyKey() {
+  ASSERT_NO_FATAL_FAILURE(
+      registry_override_.OverrideRegistry(HKEY_LOCAL_MACHINE));
   base::win::RegKey key(HKEY_LOCAL_MACHINE);
   LONG result = key.DeleteKey(UPDATER_POLICIES_KEY);
   ASSERT_TRUE(result == ERROR_SUCCESS || result == ERROR_FILE_NOT_FOUND);
@@ -104,6 +109,8 @@ TEST_F(GroupPolicyManagerTests, NoPolicySet) {
 }
 
 TEST_F(GroupPolicyManagerTests, PolicyRead) {
+  ASSERT_NO_FATAL_FAILURE(
+      registry_override_.OverrideRegistry(HKEY_LOCAL_MACHINE));
   base::win::RegKey key(HKEY_LOCAL_MACHINE, UPDATER_POLICIES_KEY,
                         KEY_ALL_ACCESS);
 
@@ -211,6 +218,8 @@ TEST_F(GroupPolicyManagerTests, PolicyRead) {
 }
 
 TEST_F(GroupPolicyManagerTests, WrongPolicyValueType) {
+  ASSERT_NO_FATAL_FAILURE(
+      registry_override_.OverrideRegistry(HKEY_LOCAL_MACHINE));
   base::win::RegKey key(HKEY_LOCAL_MACHINE, UPDATER_POLICIES_KEY,
                         KEY_ALL_ACCESS);
 
