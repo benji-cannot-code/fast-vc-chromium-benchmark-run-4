eef5371fec64f8c7b0ae69c3d42dfde6f803f1ab
Sky Malice <skym@chromium.org>
2021-08-06 02:11:10 +0000

    Splinter dynamic color attrs.
    Bug: 1210689
    Change-Id: Ia53338b13184faab7759cf781c524b7f8a987334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069748
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909152}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -530,7 +530,8 @@ public class FeedSurfaceCoordinator
             view = (RecyclerView) mHybridListRenderer.bind(mContentManager);
             view.setId(R.id.feed_stream_recycler_view);
             view.setClipToPadding(false);
-            view.setBackgroundColor(MaterialColors.getColor(context, R.attr.default_bg_color, TAG));
+            view.setBackgroundColor(
+                    MaterialColors.getColor(context, R.attr.default_bg_color_dynamic, TAG));
         } else {
             view = null;
         }
@@ -580,7 +581,7 @@ public class FeedSurfaceCoordinator
         mStream = createFeedStream(true);
         mFeedSurfaceLifecycleManager = mDelegate.createStreamLifecycleManager(mActivity, this);
         mRecyclerView.setBackgroundColor(
-                MaterialColors.getColor(mActivity, R.attr.default_bg_color, TAG));
+                MaterialColors.getColor(mActivity, R.attr.default_bg_color_dynamic, TAG));
 
         // For New Tab Page, mSwipeRefreshLayout has not been added to a view container. We need to
         // do it here.
