3d32c3de9cfe74e7abbcd2b4e16533903b3dd221
Tal Pressman <talp@chromium.org>
2021-08-06 01:28:49 +0000

    Notify observers of AXTree ID change after navigation commit.
    Before this CL, AXTree ID change notifications triggered when a document
    is committed. However, the code handling the change relies on
    GetMainFrame(), which returns the _active_ main frame[1] and not
    necessarily the frame for which the event was dispatched.
    
    In order to avoid having any state go out of sync, this CL adds a
    notification of ID change after an active main frame navigation is
    committed.
    
    [1] "Active" frame refers to the currently visible frame in the tab,
    as opposed to "inactive" frames such as pre-rendered or bfcached frames.
    
    Change-Id: Iaded74617e5896705861371d5682037373d359c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071062
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Tal Pressman <talp@google.com>
    Cr-Commit-Position: refs/heads/master@{#909147}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -5599,6 +5599,7 @@ void WebContentsImpl::DidNavigateMainFramePostCommit(
     }
     OnThemeColorChanged(page);
     OnBackgroundColorChanged(page);
+    AXTreeIDForMainFrameHasChanged();
   }
 }
 
