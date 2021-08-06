65e8f9790567812d0c1b822348d132fa2730d257
jlebel <jlebel@chromium.org>
2021-08-06 08:40:43 +0000

    [iOS][MICE] Adds egtests for websignin consistency metrics
    Add egtests for Signin.AccountConsistencyPromoAction.Shown.Count metric.
    
    Bug: 1232426
    Change-Id: Iccfc7bd8764418395338cc655fa5b0767679a528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3067231
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909207}

--- ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.mm
@@ -40,18 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-namespace {
-
-// Metrics to record the number of times the web sign-in is displayed.
-const char* kSigninAccountConsistencyPromoActionShownCount =
-    "Signin.AccountConsistencyPromoAction.Shown.Count";
-// Metrics to record how many times the web sign-in has been displayed before
-// the user signs in.
-const char* kSigninAccountConsistencyPromoActionSignedInCount =
-    "Signin.AccountConsistencyPromoAction.SignedIn.Count";
-
-}  // namespace
-
 @interface ConsistencyPromoSigninCoordinator () <
     ConsistencyAccountChooserCoordinatorDelegate,
     ConsistencyDefaultAccountCoordinatorDelegate,
@@ -196,6 +184,8 @@ const char* kSigninAccountConsistencyPromoActionSignedInCount =
       int displayCount = [self.class displayCountWithPrefService:prefService];
       base::UmaHistogramExactLinear(
           kSigninAccountConsistencyPromoActionSignedInCount, displayCount, 100);
+      // Reset dismissal count.
+      prefService->SetInteger(prefs::kSigninWebSignDismissalCount, 0);
       break;
     }
     case SigninCoordinatorResultCanceledByUser:
