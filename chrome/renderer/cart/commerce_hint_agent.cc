7d81c7450e2a8a4cda2cf4aa4a17ccfee0a31b3c
Yue Zhang <yuezhanggg@chromium.org>
2021-08-06 02:18:35 +0000

    [ChromeCart] Add histogram for cart extraction time cost
    Bug: 1234940
    Change-Id: I1bfbb38425e97aaadb70fa818c1362fa830d4db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064341
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909158}

--- chrome/renderer/cart/commerce_hint_agent.cc
+++ chrome/renderer/cart/commerce_hint_agent.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -670,8 +671,17 @@ CommerceHintAgent::JavaScriptRequest::JavaScriptRequest(
 
 CommerceHintAgent::JavaScriptRequest::~JavaScriptRequest() = default;
 
+void CommerceHintAgent::JavaScriptRequest::WillExecute() {
+  start_time_ = base::TimeTicks::Now();
+}
+
 void CommerceHintAgent::JavaScriptRequest::Completed(
     const blink::WebVector<v8::Local<v8::Value>>& result) {
+  // Only record when the start time is correctly captured.
+  if (!start_time_.is_null()) {
+    UMA_HISTOGRAM_TIMES("Commerce.Carts.ExtractionExecutionTime",
+                        base::TimeTicks::Now() - start_time_);
+  }
   if (!agent_)
     return;
   blink::WebLocalFrame* main_frame = agent_->render_frame()->GetWebFrame();
