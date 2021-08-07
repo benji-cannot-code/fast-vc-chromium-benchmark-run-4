395b2ac812bc768d606df2f70b2b9182117ed67d
Nate Fischer <ntfschr@chromium.org>
2021-08-07 00:48:49 +0000

    Android: prefer @CommandLineFlags annotation
    No change to logic. This swaps out existing uses of
    CommandLine.{appendSwitch,removeSwitch} with the corresponding
    @CommandLineFlags annotations. The annotations have the advantage of
    cleaning up the CommandLine automatically during tear down.
    
    This CL fixes the links in /chrome/android/junit/src/org/chromium/chrome/browser/survey.
    
    This CL was uploaded by git cl split.
    
    R=wenyufu@chromium.org
    
    Bug: 1237604
    Test: run_chrome_junit_tests
    Change-Id: I40867370c2abd96e78d792d1b6522a1e258e8d6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078448
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909574}

--- chrome/android/junit/src/org/chromium/chrome/browser/survey/ChromeSurveyControllerFlowTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/survey/ChromeSurveyControllerFlowTest.java
@@ -22,6 +22,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -41,6 +42,7 @@ import org.chromium.base.metrics.test.ShadowRecordHistogram;
 import org.chromium.base.task.test.BackgroundShadowAsyncTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.PayloadCallbackHelper;
 import org.chromium.chrome.R;
@@ -78,6 +80,9 @@ import java.util.concurrent.TimeUnit;
         })
 //TODO(crbug.com/1210371): Rewrite using paused loop. See crbug for details.
 @LooperMode(LooperMode.Mode.LEGACY)
+// Set user is selected and by pass the rate limit. The rate limiting logic is tested in
+// ChromeSurveyControllerTest.
+@CommandLineFlags.Add(ChromeSwitches.CHROME_FORCE_ENABLE_SURVEY)
 public class ChromeSurveyControllerFlowTest {
     // clang-format on
     private static final String TEST_TRIGGER_ID = "test_trigger_id";
@@ -140,6 +145,8 @@ public class ChromeSurveyControllerFlowTest {
     public MockitoRule mRule = MockitoJUnit.rule();
     @Rule
     public JniMocker mocker = new JniMocker();
+    @Rule
+    public TestRule mCommandLineFlagsRule = CommandLineFlags.getTestRule();
 
     @Mock
     TabModelSelector mMockModelSelector;
@@ -175,10 +182,6 @@ public class ChromeSurveyControllerFlowTest {
         ShadowInfoBarContainer.sInfoBarContainer = mMockInfoBarContainer;
         ShadowSurveyInfoBar.sShowInfoBarCallback = new PayloadCallbackHelper<>();
 
-        // Set user is selected and by pass the rate limit. The rate limiting logic is tested in
-        // ChromeSurveyControllerTest.
-        CommandLine.getInstance().appendSwitch(ChromeSwitches.CHROME_FORCE_ENABLE_SURVEY);
-
         SurveyController.setInstanceForTesting(mTestSurveyController);
 
         ChromeSurveyController.forceIsUMAEnabledForTesting(true);
@@ -202,7 +205,6 @@ public class ChromeSurveyControllerFlowTest {
         ShadowRecordHistogram.reset();
 
         CommandLine.getInstance().removeSwitch(ChromeSurveyController.COMMAND_LINE_PARAM_NAME);
-        CommandLine.getInstance().removeSwitch(ChromeSwitches.CHROME_FORCE_ENABLE_SURVEY);
     }
 
     @Test
