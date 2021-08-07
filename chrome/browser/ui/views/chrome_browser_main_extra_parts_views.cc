ea786c87f408502f14de67e1b3e2961eb17d42bf
Peter Kvitek <kvitekp@chromium.org>
2021-08-07 00:35:35 +0000

    [headless] Added native headless mode.
    Native headless mode is using Chrome browser running in unattended
    mode with ozone/headless. This is different from the traditional
    headless mode that uses a completely different browser implementation.
    
    Native headless mode is enabled by adding 'chrome' value to --headless
    command line switch or by setting the USE_HEADLESS_CHROME environment
    variable.
    
    Bug: 1237546
    Change-Id: I2125f9f29eb61d64e75cd854ac32c99c7e007b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068568
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909569}

--- chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
+++ chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/headless/headless_mode_util.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/ui/commander/commander.h"
 #include "chrome/browser/ui/views/chrome_constrained_window_views_client.h"
@@ -115,6 +116,8 @@ void ChromeBrowserMainExtraPartsViews::PreCreateThreads() {
   if (!display::Screen::GetScreen())
     screen_ = views::CreateDesktopScreen();
 #endif
+  if (headless::IsChromeNativeHeadless())
+    headless::SetHeadlessDisplayBounds();
 }
 
 void ChromeBrowserMainExtraPartsViews::PreProfileInit() {
