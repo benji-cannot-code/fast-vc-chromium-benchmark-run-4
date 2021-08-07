65c333a85255ce56fc0a9a5bc9807eaa09b4b89d
Brandon Jones <bajones@chromium.org>
2021-08-07 03:55:46 +0000

    Update GPUTextureUsage flag names
    SAMPLED and STORAGE have been deprecated and replaced by TEXTURE_BINDING
    and STORAGE_BINDING respectively.
    
    Bug: 1069302
    Change-Id: Id3ef3ca1fd630a1a47ce1e8741a2901a4b853f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078643
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909603}

--- third_party/blink/renderer/modules/webgpu/gpu_texture_usage.h
+++ third_party/blink/renderer/modules/webgpu/gpu_texture_usage.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_TEXTURE_USAGE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_TEXTURE_USAGE_H_
 
+#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
 namespace blink {
+
+class ExecutionContext;
 
 class GPUTextureUsage : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
@@ -17,9 +20,12 @@ class GPUTextureUsage : public ScriptWrappable {
   // gpu_texture_usage.idl
   static constexpr uint32_t kCopySrc = 1;
   static constexpr uint32_t kCopyDst = 2;
-  static constexpr uint32_t kSampled = 4;
-  static constexpr uint32_t kStorage = 8;
+  static constexpr uint32_t kTextureBinding = 4;
+  static constexpr uint32_t kStorageBinding = 8;
   static constexpr uint32_t kRenderAttachment = 16;
+
+  static unsigned SAMPLED(ExecutionContext* execution_context);
+  static unsigned STORAGE(ExecutionContext* execution_context);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(GPUTextureUsage);
