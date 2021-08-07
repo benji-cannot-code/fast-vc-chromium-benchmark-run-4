101371d5d4c5ebc390e0c5d681a68e05485f5d66
Yue Li <updowndota@google.com>
2021-08-07 00:51:15 +0000

    Quick Answers V2: Only show report query button for NoResult cases
    Bug: b/195780583
    Test: Manual Test
    Change-Id: I9268c18dd6db7851c76fe0ac67a5c89a125725dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077136
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909576}

--- chromeos/components/quick_answers/quick_answers_model.h
+++ chromeos/components/quick_answers/quick_answers_model.h
@@ -125,7 +125,7 @@ struct QuickAnswer {
   QuickAnswer();
   ~QuickAnswer();
 
-  ResultType result_type;
+  ResultType result_type = ResultType::kNoResult;
   std::vector<std::unique_ptr<QuickAnswerUiElement>> title;
   std::vector<std::unique_ptr<QuickAnswerUiElement>> first_answer_row;
   std::vector<std::unique_ptr<QuickAnswerUiElement>> second_answer_row;
