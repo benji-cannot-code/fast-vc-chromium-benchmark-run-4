e9032e56e6442688c9e196c64d02c2073ba20cf4
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-08-08 14:34:41 +0000

    Revert "ax_dump_tree: Move PID into selectors object"
    This reverts commit aaf0ea441189c2cc5b4ac0f831b08ed18156ceab.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 909668 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2FhZjBlYTQ0MTE4OWMyY2M1YjRhYzBmODMxYjA4ZWQxODE1NmNlYWIM
    
    Sample Failed Build: https://ci.chromium.org/b/8839479079026749777
    
    Sample Failed Step: compile
    
    Original change's description:
    > ax_dump_tree: Move PID into selectors object
    > 
    > First patch toward the goal of respecting `--active-tab` argument
    > when `--pid` is used. Adds PID to selectors object in order
    > to unify the two paths BuildTreeForWindow and BuildTreeForSelector.
    > 
    > 
    > Bug: 1215960
    > Change-Id: Iaed58c03f4d25ffedb4eae72e50229786767f8c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056865
    > Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#909668}
    
    
    Change-Id: I29b4c3e5b6ea9c97555186f021160640b1ed77f4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1215960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3079799
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Owners-Override: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909670}

--- tools/accessibility/inspect/ax_utils.h
+++ tools/accessibility/inspect/ax_utils.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define TOOLS_ACCESSIBILITY_INSPECT_AX_UTILS_H_
 
 #include "base/command_line.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/accessibility/platform/inspect/ax_inspect.h"
 
 namespace tools {
@@ -16,8 +15,7 @@ namespace tools {
 void PrintHelpForTreeSelectors();
 
 // Returns tree selector from command line arguments.
-absl::optional<ui::AXTreeSelector> TreeSelectorFromCommandLine(
-    const base::CommandLine& command_line);
+ui::AXTreeSelector TreeSelectorFromCommandLine(const base::CommandLine*);
 
 }  // namespace tools
 
--- ui/accessibility/platform/inspect/ax_inspect.h
+++ ui/accessibility/platform/inspect/ax_inspect.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ui/accessibility/ax_export.h"
-#include "ui/gfx/native_widget_types.h"
 
 namespace ui {
 
@@ -29,19 +28,12 @@ struct AX_EXPORT AXTreeSelector {
   };
   int types{None};
   std::string pattern;
-  gfx::AcceleratedWidget widget{0};
 
   AXTreeSelector() = default;
   AXTreeSelector(int types, const std::string& pattern)
       : types(types), pattern(pattern) {}
-  AXTreeSelector(int types,
-                 const std::string& pattern,
-                 gfx::AcceleratedWidget widget)
-      : types(types), pattern(pattern), widget(widget) {}
 
-  bool empty() const {
-    return (types & ~(ActiveTab)) == None && widget == 0 && pattern.empty();
-  }
+  bool empty() const { return types == None && pattern.empty(); }
 
   // Returns an application name for a type if the type specifies an
   // application.
