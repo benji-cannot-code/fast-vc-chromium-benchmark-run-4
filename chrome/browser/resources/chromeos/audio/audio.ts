2c812932082816e6339538b65d4fef99536bf7d7
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-08-08 23:42:08 +0000

    Revert "Audio WebUI: implement a device table"
    This reverts commit f181426e0f3221708b1fd4a778e621f57558ce61.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 909678 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2YxODE0MjZlMGYzMjIxNzA4YjFmZDRhNzc4ZTYyMWY1NzU1OGNlNjEM
    
    Sample Failed Build: https://ci.chromium.org/b/8839454152925061857
    
    Sample Failed Step: compile
    
    Original change's description:
    > Audio WebUI: implement a device table
    > 
    > Adds a device table that dynamically displays audio information
    > obtained from CrasAudioHandler.
    > 
    > Bug: b/193596247
    > Change-Id: I39bfe95cf4eeddeb19ea9827f21cfaee4d70efb1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060135
    > Commit-Queue: Lauren Wei <laurenwei@google.com>
    > Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#909678}
    
    
    Change-Id: I4a313d7d9e2b957f324a85f824030844c466ce08
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/193596247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076681
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Owners-Override: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909679}

--- chrome/browser/resources/chromeos/audio/audio.ts
+++ chrome/browser/resources/chromeos/audio/audio.ts
@@ -3,10 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {DevicePage} from './device_page.js';
+import {AudioBroker} from './audio_broker.js';
 
 function initialize() {
-  DevicePage.getInstance();
+  const handler = AudioBroker.getInstance().handler;
+  handler.getAudioDeviceInfo().then(({deviceName}) => {
+    console.log('mock device name output: ' + deviceName);
+  });
+  console.log('welcome to the audio page.');
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
--- chrome/browser/ui/webui/chromeos/audio/audio_ui.h
+++ chrome/browser/ui/webui/chromeos/audio/audio_ui.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_AUDIO_AUDIO_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_AUDIO_AUDIO_UI_H_
 
-#include <memory>
-
 #include "chrome/browser/ui/webui/chromeos/audio/audio.mojom.h"
 #include "chrome/browser/ui/webui/chromeos/audio/audio_handler.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -30,6 +28,7 @@ class AudioUI : public ui::MojoWebUIController,
       mojo::PendingReceiver<audio::mojom::PageHandlerFactory> receiver);
 
  private:
+  // audio::mojom::PageHandlerFactory
   void CreatePageHandler(
       mojo::PendingRemote<audio::mojom::Page> page,
       mojo::PendingReceiver<audio::mojom::PageHandler> receiver) override;
