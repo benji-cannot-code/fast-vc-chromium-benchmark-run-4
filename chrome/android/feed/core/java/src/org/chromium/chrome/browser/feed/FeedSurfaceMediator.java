435d3d4c4855beef8d4752aaeec6daa37418be09
Cathy Li <chili@chromium.org>
2021-08-09 23:46:48 +0000

    [WebFeed]: Open the feed that was last seen when opening NTP or Start Surfaced
    Add last-seen feed type as a field to preferences
    
    Bug: b/192120162
    Change-Id: I0d10d9bf31ad7928059b089333d9f00fbb711e2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082767
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910057}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -162,6 +162,7 @@ public class FeedSurfaceMediator
             }
 
             maybeLogLaunchFinished(DiscoverLaunchResult.SWITCHED_FEED_TABS);
+            getPrefService().setInteger(Pref.LAST_SEEN_FEED_TYPE, index);
             bindStream(mTabToStreamMap.get(index));
         }
     }
@@ -278,7 +279,6 @@ public class FeedSurfaceMediator
         mSigninManager = IdentityServicesProvider.get().getSigninManager(
                 Profile.getLastUsedRegularProfile());
         mPageNavigationDelegate = pageNavigationDelegate;
-        mRestoreTabId = openingTabId;
 
         if (sTestPrefChangeRegistar != null) {
             mPrefChangeRegistrar = sTestPrefChangeRegistar;
@@ -287,6 +287,12 @@ public class FeedSurfaceMediator
         }
         mHasHeader = headerModel != null;
         mPrefChangeRegistrar.addObserver(Pref.ENABLE_SNIPPETS, this::updateContent);
+
+        if (openingTabId == FeedSurfaceCoordinator.StreamTabId.DEFAULT) {
+            mRestoreTabId = getPrefService().getInteger(Pref.LAST_SEEN_FEED_TYPE);
+        } else {
+            mRestoreTabId = openingTabId;
+        }
 
         // Check that there is a navigation delegate when using the feed header menu.
         if (mPageNavigationDelegate == null) {
@@ -397,6 +403,9 @@ public class FeedSurfaceMediator
      * being shown again with a different {@link NewTabPageLaunchOrigin}.
      */
     void setTabId(@FeedSurfaceCoordinator.StreamTabId int tabId) {
+        if (tabId == FeedSurfaceCoordinator.StreamTabId.DEFAULT) {
+            tabId = getPrefService().getInteger(Pref.LAST_SEEN_FEED_TYPE);
+        }
         if (mTabToStreamMap.size() <= tabId) tabId = 0;
         mSectionHeaderModel.set(SectionHeaderListProperties.CURRENT_TAB_INDEX_KEY, tabId);
     }
