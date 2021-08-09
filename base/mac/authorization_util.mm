045ce6851ee7184479963b62b5b5568f64e53486
Olivier Li <olivierli@chromium.org>
2021-08-09 19:12:55 +0000

    AllowList Authorization creation prompt for HangWatcher on macOS.
    Answering the prompt can take unbounded time so should not be considered
    as hung.
    
    Bug: 1034046
    Change-Id: I5a938224fad566cad28a9c5b76ead3db5cf58f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076627
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909899}

--- base/mac/authorization_util.mm
+++ base/mac/authorization_util.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/posix/eintr_wrapper.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "base/threading/hang_watcher.h"
 
 namespace base {
 namespace mac {
@@ -37,6 +38,14 @@ AuthorizationRef GetAuthorizationRightsWithPrompt(
     OSSTATUS_LOG(ERROR, status) << "AuthorizationCreate";
     return NULL;
   }
+
+  // Never consider the current WatchHangsInScope as hung. There was most likely
+  // one created in ThreadControllerWithMessagePumpImpl::DoWork(). The current
+  // hang watching deadline is not valid since the user can take unbounded time
+  // to answer the password prompt. HangWatching will resume when the next task
+  // or event is pumped in MessagePumpCFRunLoop so there is not need to
+  // reactivate it. You can see the function comments for more details.
+  base::HangWatcher::InvalidateActiveExpectations();
 
   AuthorizationFlags flags = kAuthorizationFlagDefaults |
                              kAuthorizationFlagInteractionAllowed |
