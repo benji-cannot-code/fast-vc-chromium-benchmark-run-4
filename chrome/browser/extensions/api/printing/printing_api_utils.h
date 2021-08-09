f0a0edb24440b5afbaf8047c0aba7eb20be1bd21
Pranav Batra <batrapranav@chromium.org>
2021-08-09 23:22:17 +0000

    Enable chrome.printing API in lacros
    Remove PrintingCapabilitiesProvider - use the crosapi LocalPrinter
    mojo interface to query printers and printer capabilities instead.
    
    Remove references to ash-exclusive classes. When necessary, forward
    calls to ash chrome via the LocalPrinter interface.
    
    Also enable disabled unit tests.
    
    Bug: b/189970798
    Change-Id: I2ecc2d46c5ddf6c3caf2f77eea2be920066c8f9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3058066
    Commit-Queue: Pranav Batra <batrapranav@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910044}

--- chrome/browser/extensions/api/printing/printing_api_utils.h
+++ chrome/browser/extensions/api/printing/printing_api_utils.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/containers/flat_map.h"
 #include "chrome/browser/chromeos/printing/printer_error_codes.h"
 #include "chrome/common/extensions/api/printing.h"
+#include "chromeos/crosapi/mojom/local_printer.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace base {
@@ -42,7 +44,7 @@ absl::optional<DefaultPrinterRules> GetDefaultPrinterRules(
     const std::string& default_destination_selection_rules);
 
 api::printing::Printer PrinterToIdl(
-    const chromeos::Printer& printer,
+    const crosapi::mojom::LocalDestinationInfo& printer,
     const absl::optional<DefaultPrinterRules>& default_printer_rules,
     const base::flat_map<std::string, int>& recently_used_ranks);
 
--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1732,6 +1732,11 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
   handlers->AddHandler(std::make_unique<extensions::ExtensionListPolicyHandler>(
       key::kAttestationExtensionAllowlist,
       prefs::kAttestationExtensionAllowlist, false));
+#if defined(USE_CUPS)
+  handlers->AddHandler(std::make_unique<extensions::ExtensionListPolicyHandler>(
+      key::kPrintingAPIExtensionsAllowlist,
+      prefs::kPrintingAPIExtensionsAllowlist, /*allow_wildcards=*/false));
+#endif  // defined(USE_CUPS)
 #else  // defined(OS_CHROMEOS)
   std::vector<std::unique_ptr<ConfigurationPolicyHandler>>
       signin_legacy_policies;
@@ -1998,11 +2003,6 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
       SimpleSchemaValidatingPolicyHandler::RECOMMENDED_PROHIBITED,
       SimpleSchemaValidatingPolicyHandler::MANDATORY_ALLOWED));
   handlers->AddHandler(std::make_unique<LacrosAvailabilityPolicyHandler>());
-#if defined(USE_CUPS)
-  handlers->AddHandler(std::make_unique<extensions::ExtensionListPolicyHandler>(
-      key::kPrintingAPIExtensionsAllowlist,
-      prefs::kPrintingAPIExtensionsAllowlist, /*allow_wildcards=*/false));
-#endif  // defined(USE_CUPS)
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 // On most platforms, there is a legacy policy
