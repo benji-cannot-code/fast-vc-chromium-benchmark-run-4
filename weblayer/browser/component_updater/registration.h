c246ca5268cb6e1a8e929b437f4d8d5031c995ab
Hazem Ashmawy <hazems@chromium.org>
2021-08-09 22:35:28 +0000

    [WebLayer] Add support for ComponentUpdater
    Reuse WebView's ComponentsProviderService to get configs downloaded via
    the component updater.
    
    This CL just adds all necessary code to support ComponentUpdater in
    WebLayer but it doesn't add any components yet.  No service connection
    is made until individual components are added/registered.
    
    see http://go/wv-component-updater
    
    Fixed: 1162263
    Test: Add debug logs and manually launch WebLayer shell
    Change-Id: I8cc71fc1aab8d6bd423472fba1dc818a79508cd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077479
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910025}

---
+++ weblayer/browser/component_updater/registration.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBLAYER_BROWSER_COMPONENT_UPDATER_REGISTRATION_H_
+#define WEBLAYER_BROWSER_COMPONENT_UPDATER_REGISTRATION_H_
+
+#include "components/component_updater/android/component_loader_policy.h"
+
+namespace weblayer {
+
+// ComponentLoaderPolicies for component to load in WebLayer during startup.
+component_updater::ComponentLoaderPolicyVector GetComponentLoaderPolicies();
+
+}  // namespace weblayer
+
+#endif  // WEBLAYER_BROWSER_COMPONENT_UPDATER_REGISTRATION_H_
