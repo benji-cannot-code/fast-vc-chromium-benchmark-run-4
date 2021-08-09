1b2e3140d87d40192bab0ef5643d343754256394
Josh Nohle <nohle@google.com>
2021-08-09 21:31:42 +0000

    [Nearby] Roll nearby-connections from 7c1f42301400 to 7a02eaad37a4
    Notes:
    * Regression testing passed: https://docs.google.com/spreadsheets/d/1RnMjwDc4KkvxIY9fbjCJnborlawdlJOVfBJWejl6DZM/edit#gid=904293171
    * The medium enum BLE_L2CAP was added upstream.
    * This uprev will fix the spike in ~LogMessage crashes caused by
      misused upstream FATAL-level logging.
    * This will be the last uprev before the ServiceControllerRouter
      refactor uprev.
    
    ----------------------------------
    
    Roll nearby-connections from 7c1f42301400 to 7a02eaad37a4 (19 revisions)
    
    https://chromium.googlesource.com/external/github.com/google/nearby-connections.git/+log/7c1f42301400..7a02eaad37a4
    
    2021-08-05 hais@google.com Implemented BluetoothClassicMedium::ConnectToService
    PiperOrigin-RevId: 389007196
    
    2021-08-04 xlythe@google.com Add '//buildenv/target:non_prod' compatibility
    PiperOrigin-RevId: 388806419
    
    2021-08-04 jsobczak@google.com Move license text to BUILD files top
    PiperOrigin-RevId: 388579579
    
    2021-08-03 nohle@google.com [NearbyConnections][C++] Change FATAL logs to ERROR
    PiperOrigin-RevId: 388525052
    
    2021-08-03 hais@google.com Moved connections/windows to third_party, and copied the blueprint file into third_party
    PiperOrigin-RevId: 388521021
    
    2021-08-03 jsobczak@google.com Internal change
    PiperOrigin-RevId: 388461110
    
    2021-08-03 hais@google.com Moved connections/windows to third_party, and copied the blueprint file into third_party
    PiperOrigin-RevId: 387621210
    
    2021-08-02 hais@google.com nearby presence with fake advertisement.
    PiperOrigin-RevId: 388309745
    
    2021-07-30 xlythe@google.com asdf
    PiperOrigin-RevId: 387879731
    
    2021-07-29 hais@google.com Moved connections/windows to third_party, and copied the blueprint file into third_party
    PiperOrigin-RevId: 387621210
    
    2021-07-29 hais@google.com Moved connections/windows to third_party, and copied the blueprint file into third_party
    PiperOrigin-RevId: 387621210
    
    2021-07-29 xlythe@google.com BEGIN_PUBLIC
    Microsoft has updated the winrt library
    END_PUBLC
    
    PiperOrigin-RevId: 387591860
    
    2021-07-29 hais@google.com BEGIN_PUBLIC
    Microsoft has updated the winrt library
    END_PUBLC
    
    PiperOrigin-RevId: 387168248
    
    2021-07-23 edwinwu@google.com Internal change
    PiperOrigin-RevId: 386356072
    
    2021-07-22 hais@google.com Internal change
    PiperOrigin-RevId: 386221204
    
    2021-07-22 edwinwu@google.com Internal change
    PiperOrigin-RevId: 386179189
    
    2021-07-20 jsobczak@google.com Internal change
    PiperOrigin-RevId: 385836456
    
    2021-07-20 hais@google.com Internal change
    PiperOrigin-RevId: 385702172
    
    2021-07-19 hais@google.com Internal change
    
    PiperOrigin-RevId: 385574533
    Change-Id: I3589598bdf0f5287184a185968887cc50e01a603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078485
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909986}

--- chrome/browser/nearby_sharing/nearby_share_metrics_logger.cc
+++ chrome/browser/nearby_sharing/nearby_share_metrics_logger.cc
@@ -103,7 +103,8 @@ enum class UpgradedMedium {
   kWifiDirect = 8,
   kWebRtc = 9,
   kNoUpgrade = 10,
-  kMaxValue = kNoUpgrade
+  kBleL2Cap = 11,
+  kMaxValue = kBleL2Cap
 };
 
 AttachmentType FileMetadataTypeToAttachmentType(
@@ -300,6 +301,7 @@ std::string GetUpgradedMediumSubcategoryName(
     case location::nearby::connections::mojom::Medium::kWifiAware:
     case location::nearby::connections::mojom::Medium::kNfc:
     case location::nearby::connections::mojom::Medium::kWifiDirect:
+    case location::nearby::connections::mojom::Medium::kBleL2Cap:
       return ".UnknownMediumUpgrade";
   }
 }
@@ -332,6 +334,8 @@ UpgradedMedium GetUpgradedMediumForMetrics(
       return UpgradedMedium::kWifiDirect;
     case location::nearby::connections::mojom::Medium::kWebRtc:
       return UpgradedMedium::kWebRtc;
+    case location::nearby::connections::mojom::Medium::kBleL2Cap:
+      return UpgradedMedium::kBleL2Cap;
   }
 }
 
