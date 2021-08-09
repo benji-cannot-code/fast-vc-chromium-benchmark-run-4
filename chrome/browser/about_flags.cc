8151d2e59b30e62fd75f81f9dd4b769a03b28808
Basia Zimirska <basiaz@google.com>
2021-08-09 17:24:46 +0000

    Add a feature param for ContentLanguagesInLanguagePicker feature.
    The new param controls subscribing for pref updates and will be used
    to debug a memory regression that is not observable after app startup.
    
    
    Bug: 1173577
    Change-Id: Iff20f0e9eebdb704343ae8d42328b0f7d0c002cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3055400
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/master@{#909844}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2618,6 +2618,20 @@ const FeatureEntry::FeatureVariation kSCTAuditingVariations[] = {
 };
 #endif  // !defined(OS_ANDROID)
 
+#if defined(OS_ANDROID)
+// The variations of ContentLanguagesInLanguagePicker.
+const FeatureEntry::FeatureParam
+    kContentLanguagesInLanguagePickerDisableObservers[] = {
+        {language::kContentLanguagesDisableObserversParam, "true"}};
+
+const FeatureEntry::FeatureVariation
+    kContentLanguagesInLanguaePickerVariations[] = {
+        {"Without observers", kContentLanguagesInLanguagePickerDisableObservers,
+         base::size(kContentLanguagesInLanguagePickerDisableObservers),
+         nullptr},
+};
+#endif  // defined(OS_ANDROID)
+
 const FeatureEntry::FeatureParam kCheckOfflineCapabilityWarnOnly[] = {
     {"check_mode", "warn_only"}};
 const FeatureEntry::FeatureParam kCheckOfflineCapabilityEnforce[] = {
@@ -7258,7 +7272,9 @@ const FeatureEntry kFeatureEntries[] = {
     {"content-languages-in-language-picker",
      flag_descriptions::kContentLanguagesInLanguagePickerName,
      flag_descriptions::kContentLanguagesInLanguagePickerName, kOsAndroid,
-     FEATURE_VALUE_TYPE(language::kContentLanguagesInLanguagePicker)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(language::kContentLanguagesInLanguagePicker,
+                                    kContentLanguagesInLanguaePickerVariations,
+                                    "ContentLanguagesInLanguagePicker")},
 #endif
 
     {"filling-across-affiliated-websites",
