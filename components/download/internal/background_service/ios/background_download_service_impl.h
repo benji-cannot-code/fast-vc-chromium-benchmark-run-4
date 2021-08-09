be254b785d6c41282c956590f140d965e1a05cdf
Xing Liu <xingliu@google.com>
2021-08-09 21:36:27 +0000

    Reland "Background download: Fix a threading issue in BackgroundDownloadTaskHelper."
    Reland commit 7298d134e2b0ae59a6245fc97876d4a5b4e38085.
    Reverted in commit 69da1a6585c6bac1cc2bca2903de02e7e4682382.
    
    Diff: Use DCHECK_CALLED_ON_VALID_SEQUENCE in unit test, non debug build
    will remove the sequence_checker.
    
    Bug: 1215315
    Change-Id: I2d16d3abfa96872698bef3f9b7c880621919eb06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077119
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909989}

--- components/download/internal/background_service/ios/background_download_service_impl.h
+++ components/download/internal/background_service/ios/background_download_service_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "base/time/clock.h"
 #include "components/download/internal/background_service/log_source.h"
 #include "components/download/internal/background_service/model_impl.h"
@@ -118,6 +119,7 @@ class BackgroundDownloadServiceImpl : public BackgroundDownloadService,
   // pruned frequently.
   const base::FilePath download_dir_;
 
+  SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<BackgroundDownloadServiceImpl> weak_ptr_factory_{this};
 };
 
