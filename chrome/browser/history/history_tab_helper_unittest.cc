0c14453bebad50805828655567b7c1f3c4a7dcbf
David Bienvenu <davidbienvenu@chromium.org>
2021-08-09 20:52:45 +0000

    unit_tests: fix mock spam in HistoryTabHelperTests
    Use NickMock to silence uninteresting mock warnings.
    
    Change-Id: I36167a1dd8f3a75a37512e6da8a9e41052f57d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082214
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Auto-Submit: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909964}

--- chrome/browser/history/history_tab_helper_unittest.cc
+++ chrome/browser/history/history_tab_helper_unittest.cc
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/history/history_tab_helper.h"
 
+#include <memory>
+#include <set>
+#include <string>
+#include <utility>
+
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
@@ -32,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/feed/core/v2/public/feed_service.h"
 #include "components/feed/core/v2/public/test/stub_feed_api.h"
 #endif
+
+using testing::NiceMock;
 
 namespace {
 
@@ -162,7 +169,7 @@ TEST_F(HistoryTabHelperTest, ShouldLimitTitleUpdatesPerPage) {
 }
 
 TEST_F(HistoryTabHelperTest, CreateAddPageArgsReferringURLMainFrameNoReferrer) {
-  content::MockNavigationHandle navigation_handle(web_contents());
+  NiceMock<content::MockNavigationHandle> navigation_handle(web_contents());
   navigation_handle.set_redirect_chain({GURL("https://someurl.com")});
   navigation_handle.set_previous_main_frame_url(GURL("http://previousurl.com"));
   history::HistoryAddPageArgs args =
@@ -174,7 +181,7 @@ TEST_F(HistoryTabHelperTest, CreateAddPageArgsReferringURLMainFrameNoReferrer) {
 
 TEST_F(HistoryTabHelperTest,
        CreateAddPageArgsReferringURLMainFrameSameOriginReferrer) {
-  content::MockNavigationHandle navigation_handle(web_contents());
+  NiceMock<content::MockNavigationHandle> navigation_handle(web_contents());
   navigation_handle.set_redirect_chain({GURL("https://someurl.com")});
   navigation_handle.set_previous_main_frame_url(
       GURL("http://previousurl.com/abc"));
@@ -191,7 +198,7 @@ TEST_F(HistoryTabHelperTest,
 
 TEST_F(HistoryTabHelperTest,
        CreateAddPageArgsReferringURLMainFrameSameOriginReferrerDifferentPath) {
-  content::MockNavigationHandle navigation_handle(web_contents());
+  NiceMock<content::MockNavigationHandle> navigation_handle(web_contents());
   navigation_handle.set_redirect_chain({GURL("https://someurl.com")});
   navigation_handle.set_previous_main_frame_url(
       GURL("http://previousurl.com/def"));
@@ -208,7 +215,7 @@ TEST_F(HistoryTabHelperTest,
 
 TEST_F(HistoryTabHelperTest,
        CreateAddPageArgsReferringURLMainFrameCrossOriginReferrer) {
-  content::MockNavigationHandle navigation_handle(web_contents());
+  NiceMock<content::MockNavigationHandle> navigation_handle(web_contents());
   auto referrer = blink::mojom::Referrer::New();
   referrer->url = GURL("http://crossorigin.com");
   referrer->policy = network::mojom::ReferrerPolicy::kDefault;
@@ -227,8 +234,8 @@ TEST_F(HistoryTabHelperTest, CreateAddPageArgsReferringURLNotMainFrame) {
       content::RenderFrameHostTester::For(main_rfh());
   main_rfh_tester->InitializeRenderFrameIfNeeded();
   content::RenderFrameHost* subframe = main_rfh_tester->AppendChild("subframe");
-  content::MockNavigationHandle navigation_handle(GURL("http://someurl.com"),
-                                                  subframe);
+  NiceMock<content::MockNavigationHandle> navigation_handle(
+      GURL("http://someurl.com"), subframe);
   navigation_handle.set_redirect_chain({GURL("https://someurl.com")});
   navigation_handle.set_previous_main_frame_url(GURL("http://previousurl.com"));
   history::HistoryAddPageArgs args =
