746454d38426081405b298d361e28edf22df3ca5
Aaron Krajeski <aaronhk@chromium.org>
2021-08-09 18:34:09 +0000

    Add use counter for CanvasRenderingContext2D context lost and restored
    These features could potentially be abused so we need to monitor them
    for usage spikes.
    
    Bug: 1201359
    Change-Id: I9134b819056a5eef5e76fe41e002fd247aea5367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082162
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909881}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/cssom/css_color_value.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/canvas/text_metrics.h"
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
 #include "third_party/blink/renderer/modules/canvas/canvas2d/canvas_filter.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/stroke_data.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 
 namespace blink {
 
@@ -1680,6 +1682,9 @@ void BaseRenderingContext2D::DispatchContextLostEvent(TimerBase*) {
       RuntimeEnabledFeatures::NewCanvas2DAPIEnabled()) {
     Event* event = Event::CreateCancelable(event_type_names::kContextlost);
     GetCanvasRenderingContextHost()->HostDispatchEvent(event);
+
+    UseCounter::Count(GetCanvasRenderingContextHost()->GetTopExecutionContext(),
+                      WebFeature::kCanvasRenderingContext2DContextLostEvent);
     if (event->defaultPrevented()) {
       context_restorable_ = false;
     }
@@ -1701,6 +1706,9 @@ void BaseRenderingContext2D::DispatchContextRestoredEvent(TimerBase*) {
   if (RuntimeEnabledFeatures::NewCanvas2DAPIEnabled()) {
     Event* event(Event::Create(event_type_names::kContextrestored));
     GetCanvasRenderingContextHost()->HostDispatchEvent(event);
+    UseCounter::Count(
+        GetCanvasRenderingContextHost()->GetTopExecutionContext(),
+        WebFeature::kCanvasRenderingContext2DContextRestoredEvent);
   }
 }
 
