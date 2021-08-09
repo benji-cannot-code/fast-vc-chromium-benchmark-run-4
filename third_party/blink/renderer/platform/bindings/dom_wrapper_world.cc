06663e0c2be8e8e45011c93b267ccf743dcaddda
Dan McArdle <dmcardle@chromium.org>
2021-08-09 19:26:56 +0000

    [WTF::HashMap Refactor] Use DeprecatedAtOrEmptyValue() in /third_party/blink/renderer/platform/bindings
    This CL is a true behavior-preserving refactor. It will not affect
    performance because HashMap<>::DeprecatedAtOrEmptyValue() has an
    identical implementation to HashMap<>::at().
    
    In the near future, we will change the definition of HashMap<>::at() to
    CHECK that the map contains the given key.
    
    All occurrences of DeprecatedAtOrEmptyValue will need to be cleaned up.
    If the caller cannot know whether the map already contains the given
    key, one possible workaround is to use HashMap<>::find() instead.
    
    Design doc: https://docs.google.com/document/d/18JIiajErikZaBzCtZvl-wwAJShqZsg0jT9wVyPBhSdU/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=mkwst@chromium.org
    
    Bug: 1058527
    Change-Id: I598618aa0733093e0de34d6f9f67f9c42557c387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082250
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909911}

--- third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
+++ third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
@@ -207,7 +207,7 @@ static IsolatedWorldStableIdMap& IsolatedWorldStableIds() {
 
 String DOMWrapperWorld::NonMainWorldStableId() const {
   DCHECK(!IsMainWorld());
-  return IsolatedWorldStableIds().at(GetWorldId());
+  return IsolatedWorldStableIds().DeprecatedAtOrEmptyValue(GetWorldId());
 }
 
 void DOMWrapperWorld::SetNonMainWorldStableId(int32_t world_id,
