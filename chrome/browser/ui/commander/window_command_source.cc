42dc934713771c70bccc2d0637272b5013094b90
Leonard Grey <lgrey@chromium.org>
2021-08-09 22:49:38 +0000

    Commander: don't show nouns at top level
    Consensus is that these are too confusing as currently presented.
    
    Bug: 1014639
    Change-Id: I33bc0e4eeb59d4bd14cfd134508c2ab32193caef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082961
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910031}

--- chrome/browser/ui/commander/window_command_source.cc
+++ chrome/browser/ui/commander/window_command_source.cc
@@ -18,12 +18,6 @@ namespace commander {
 
 namespace {
 
-// TODO(lgrey): Specifically not deduping this with BookmarkCommandSource right
-// now since I'm not actually sure if we want the same threshold for different
-// nouns.
-size_t constexpr kNounFirstMinimum = 2;
-
-
 // Activates `browser` if it's still present.
 void SwitchToBrowser(base::WeakPtr<Browser> browser) {
   if (browser.get())
@@ -90,9 +84,6 @@ CommandSource::CommandResults WindowCommandSource::GetCommands(
   BrowserList* browser_list = BrowserList::GetInstance();
   if (browser_list->size() < 2)
     return results;
-  if (input.size() >= kNounFirstMinimum) {
-    results = SwitchCommandsForWindowsMatching(browser, input);
-  }
   FuzzyFinder finder(input);
   std::vector<gfx::Range> ranges;
   // TODO(lgrey): Temporarily using untranslated strings since it's not
