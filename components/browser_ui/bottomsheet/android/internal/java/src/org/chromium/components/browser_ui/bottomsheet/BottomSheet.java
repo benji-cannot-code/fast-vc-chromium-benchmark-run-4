9ec0d1722633f71e0c345247568100ca33eeb0a0
Matt Jones <mdjones@chromium.org>
2021-08-09 15:09:04 +0000

    Replace onSheetFullyPeeked with onSheetStateChanged
    This patch removes a single-use API (onSheetFullyPeeked) for the more
    general onSheetStateChanged. The events aren't perfectly identical,
    but the difference should be negligible for the use case.
    
    Bug: 986310
    Change-Id: Ia095621f38ed60521762487dcbe39ce3775e553f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078226
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Commit-Queue: Matt Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909806}

--- components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheet.java
+++ components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheet.java
@@ -891,12 +891,6 @@ class BottomSheet extends FrameLayout
         for (BottomSheetObserver o : mObservers) {
             o.onSheetOffsetChanged(mLastOffsetRatioSent, getCurrentOffsetPx());
         }
-
-        if (isPeekStateEnabled()
-                && MathUtils.areFloatsEqual(
-                        offsetWithBrowserControls, getSheetHeightForState(SheetState.PEEK))) {
-            for (BottomSheetObserver o : mObservers) o.onSheetFullyPeeked();
-        }
     }
 
     /** @see #setSheetState(int, boolean, int) */
