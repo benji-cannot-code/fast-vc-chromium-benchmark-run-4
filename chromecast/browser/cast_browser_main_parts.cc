48a111bf606f9038668c652c21d9d618276bbc97
Benjamin Lerman <qsr@chromium.org>
2021-08-09 16:12:55 +0000

    [fuchsia] Enable unit_tests build on Fuchsia
    Bug: 1211758
    Change-Id: Ibe2cec09e2de9d0a2aee5ce03b467a72e3da7ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3065463
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909823}

--- chromecast/browser/cast_browser_main_parts.cc
+++ chromecast/browser/cast_browser_main_parts.cc
@@ -146,8 +146,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "chromecast/base/cast_sys_info_util.h"
 #include "chromecast/public/cast_sys_info.h"
-#include "components/heap_profiling/multi_process/client_connection_manager.h"
-#include "components/heap_profiling/multi_process/supervisor.h"
+#include "components/heap_profiling/multi_process/client_connection_manager.h"  // nogncheck
+#include "components/heap_profiling/multi_process/supervisor.h"  // nogncheck
 #endif  // !defined(OS_FUCHSIA)
 
 namespace {
