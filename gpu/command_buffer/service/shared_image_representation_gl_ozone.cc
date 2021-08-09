ffb87d7e4db16bcb34319cdeb431eceff60e8d83
Brian Ho <hob@chromium.org>
2021-08-09 21:44:13 +0000

    viz: Initialize and reset current_access_mode_ in Ozone SIs
    SharedImageRepresentationGLOzone::BeginAccess DCHECKs for no access
    mode which always fails. This CL properly initializes and resets
    current_access_mode_ to 0 like in ExternalVkImageBacking [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/external_vk_image_gl_representation.cc;l=166;drc=fb62432bab790c4f6006a4ce04084e214fd2d302
    
    Change-Id: I411918f3eac4bd9779de57e94d8daa68f38c36fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081969
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Brian Ho <hob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909993}

--- gpu/command_buffer/service/shared_image_representation_gl_ozone.cc
+++ gpu/command_buffer/service/shared_image_representation_gl_ozone.cc
@@ -128,6 +128,7 @@ void SharedImageRepresentationGLOzone::EndAccess() {
   bool readonly =
       current_access_mode_ != GL_SHARED_IMAGE_ACCESS_MODE_READWRITE_CHROMIUM;
   ozone_backing()->EndAccess(readonly, std::move(fence));
+  current_access_mode_ = 0;
 }
 
 }  // namespace gpu
