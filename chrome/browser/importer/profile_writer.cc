d61c06da732153f792ee2f47d5dfa4a3cfa91adf
Maria Kazinova <kazinova@google.com>
2021-08-09 10:09:00 +0000

    Migrate ProfileWriter to PasswordStoreInterface
    PasswordStoreInterface is an interface that should be used by other
    classes instead of interacting directly with PasswordStore.
    
    Bug: 1218413
    Change-Id: Ia07c1f4d8a3c1536833ca9edb64b3b57b4fe490d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076573
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#909751}

--- chrome/browser/importer/profile_writer.cc
+++ chrome/browser/importer/profile_writer.cc
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon/core/favicon_service.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/password_manager/core/browser/password_form.h"
-#include "components/password_manager/core/browser/password_store.h"
+#include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/search_engines/template_url.h"
@@ -94,8 +94,8 @@ void ProfileWriter::AddPasswordForm(
 
   if (profile_->GetPrefs()->GetBoolean(
           password_manager::prefs::kCredentialsEnableService)) {
-    PasswordStoreFactory::GetForProfile(profile_,
-                                        ServiceAccessType::EXPLICIT_ACCESS)
+    PasswordStoreFactory::GetInterfaceForProfile(
+        profile_, ServiceAccessType::EXPLICIT_ACCESS)
         ->AddLogin(form);
   }
 }
