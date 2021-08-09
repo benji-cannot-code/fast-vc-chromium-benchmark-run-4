16c6971cd210246b7298537061f5ee9767be3e19
Rune Lillesveen <futhark@chromium.org>
2021-08-09 18:07:31 +0000

    No need for new ComputedStyle for GetForceReattachLayoutTree
    SetForceReattachLayoutTree is a way to force a layout tree reattach
    which is not detectable through a diff of the computed styles. We still
    did generate a new ComputedStyle when such a forced re-attached was
    done, even when we did not mark for style recalc at the same time.
    
    Skip RecalcOwnStyle and just force re-attachment for this case.
    
    Bug: 1145970
    Change-Id: I803fc048c8f7ecde4e5091a9b8a80850e19eb59a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081605
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909868}

--- third_party/blink/renderer/core/css/style_recalc.cc
+++ third_party/blink/renderer/core/css/style_recalc.cc
@@ -22,7 +22,7 @@ bool StyleRecalcChange::TraversePseudoElements(const Element& element) const {
 
 bool StyleRecalcChange::TraverseChild(const Node& node) const {
   return ShouldRecalcStyleFor(node) || node.ChildNeedsStyleRecalc() ||
-         RecalcContainerQueryDependent();
+         node.GetForceReattachLayoutTree() || RecalcContainerQueryDependent();
 }
 
 bool StyleRecalcChange::ShouldRecalcStyleFor(const Node& node) const {
@@ -31,8 +31,6 @@ bool StyleRecalcChange::ShouldRecalcStyleFor(const Node& node) const {
   if (RecalcChildren())
     return true;
   if (node.NeedsStyleRecalc())
-    return true;
-  if (node.GetForceReattachLayoutTree())
     return true;
   // Early exit before getting the computed style.
   if (propagate_ != kClearEnsured && !RecalcContainerQueryDependent())
