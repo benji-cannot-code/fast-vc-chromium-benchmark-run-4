5bbfc44df8bc3b9dd6b659f2115fc38b2b9d4c89
Ankit Kumar 🌪️ <ankk@microsoft.com>
2021-08-09 20:02:54 +0000

    [unseasoned-pdf] Introduce PdfAccessibilityDataHandler
    Introduce an interface class named PdfAccessibilityDataHandler. The
    interface will be implemented by PdfAccessibilityTree. In case of
    unseasoned-pdf-viewer, PdfViewWebPlugin will hold the
    PdfAccessibilityTree class in form of its base class
    PdfAccessibilityDataHandler.
    
    Bug: 1144444
    Change-Id: Iad296eb1d65be43576fcbf0cfac29073760a54b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081306
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909940}

---
+++ pdf/pdf_accessibility_data_handler.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_PDF_ACCESSIBILITY_DATA_HANDLER_H_
+#define PDF_PDF_ACCESSIBILITY_DATA_HANDLER_H_
+
+#include <vector>
+
+namespace chrome_pdf {
+
+struct AccessibilityCharInfo;
+struct AccessibilityDocInfo;
+struct AccessibilityPageInfo;
+struct AccessibilityPageObjects;
+struct AccessibilityTextRunInfo;
+struct AccessibilityViewportInfo;
+
+class PdfAccessibilityDataHandler {
+ public:
+  virtual ~PdfAccessibilityDataHandler() = default;
+
+  virtual void SetAccessibilityViewportInfo(
+      const AccessibilityViewportInfo& viewport_info) = 0;
+  virtual void SetAccessibilityDocInfo(
+      const AccessibilityDocInfo& doc_info) = 0;
+  virtual void SetAccessibilityPageInfo(
+      const AccessibilityPageInfo& page_info,
+      const std::vector<AccessibilityTextRunInfo>& text_runs,
+      const std::vector<AccessibilityCharInfo>& chars,
+      const AccessibilityPageObjects& page_objects) = 0;
+};
+
+}  // namespace chrome_pdf
+
+#endif  // PDF_PDF_ACCESSIBILITY_DATA_HANDLER_H_
