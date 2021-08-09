5573ff220c799958d9cc64d8bc40b5a403a61145
Maksim Ivanov <emaxx@chromium.org>
2021-08-09 18:55:00 +0000

    Fix platform_only in syntax_check_policy_template_json.py
    Fix a copy-paste error that's causing the platform_only flag being
    overwritten by the internal_only flag.
    
    Given that both flags are information-only and don't affect code
    generation, the only effect of the bug was the presubmit not correctly
    protecting against the cloud_only+platform_only combination. There seems
    to be no violation of this rule in policy_templates.json so far, so no
    side effects are expected after this CL.
    
    Bug: 1076560
    Change-Id: I115580ded3b42394ba1081b6d9ceb1d19bf5b9ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081945
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909893}

--- components/policy/tools/syntax_check_policy_template_json.py
+++ components/policy/tools/syntax_check_policy_template_json.py
@@ -1018,7 +1018,7 @@ class PolicyTemplateChecker(object):
           container_name='features')
 
       # 'internal_only' feature must be an optional boolean flag.
-      platform_only = self._CheckContains(features,
+      internal_only = self._CheckContains(features,
                                           'internal_only',
                                           bool,
                                           optional=True,
