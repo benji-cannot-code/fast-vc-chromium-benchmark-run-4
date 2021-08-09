2a218265def29eab50b97475c7f6e72d54e40eba
Austin Sullivan <asully@chromium.org>
2021-08-09 14:01:18 +0000

    Replace DISALLOW_COPY_AND_ASSIGN with =delete in HeadlessClipboard
    This macro has been deprecated in favor of explicitly deleting the copy
    constructor and assignment operator.
    
    Bug: 1010217
    Change-Id: I6623f003d2aba63b0c826094f206d096db926e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3011937
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909789}

--- headless/lib/browser/headless_clipboard.h
+++ headless/lib/browser/headless_clipboard.h
@@ -22,6 +22,8 @@ namespace headless {
 class HeadlessClipboard : public ui::Clipboard {
  public:
   HeadlessClipboard();
+  HeadlessClipboard(const HeadlessClipboard&) = delete;
+  HeadlessClipboard& operator=(const HeadlessClipboard&) = delete;
   ~HeadlessClipboard() override;
 
  private:
@@ -126,8 +128,6 @@ class HeadlessClipboard : public ui::Clipboard {
 
   ui::ClipboardBuffer default_store_buffer_;
   mutable std::map<ui::ClipboardBuffer, DataStore> stores_;
-
-  DISALLOW_COPY_AND_ASSIGN(HeadlessClipboard);
 };
 
 }  // namespace headless
