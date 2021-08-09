65d0ec83f94b04d1047042684b56ebd4b782804a
Aida Zolic <aidazolic@chromium.org>
2021-08-09 12:42:01 +0000

    Add DlpPrintWarnDialog
    Add the warning dialog that is shown before printing.
    
    Bug: 1226028
    Change-Id: Id71722bed89ed680141036191aa4d85080bcb1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071443
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909779}

---
+++ chrome/browser/ash/policy/dlp/dlp_warn_dialog.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_DLP_DLP_WARN_DIALOG_H_
+#define CHROME_BROWSER_ASH_POLICY_DLP_DLP_WARN_DIALOG_H_
+
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
+
+namespace policy {
+
+// PrintWarnDialog is a system dialog that is shown when printing restriction
+// level is set to WARN.
+class PrintWarnDialog : public views::BubbleDialogDelegateView {
+ public:
+  METADATA_HEADER(PrintWarnDialog);
+  PrintWarnDialog(base::OnceClosure accept_callback,
+                  base::OnceClosure cancel_callback);
+  PrintWarnDialog(const PrintWarnDialog&) = delete;
+  PrintWarnDialog& operator=(const PrintWarnDialog&) = delete;
+  ~PrintWarnDialog() override;
+
+ private:
+  void InitializeView();
+};
+
+}  // namespace policy
+
+#endif  // CHROME_BROWSER_ASH_POLICY_DLP_DLP_WARN_DIALOG_H_
