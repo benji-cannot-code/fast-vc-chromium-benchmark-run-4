02fa32eb33f7c5b9971439fa3740c751e3617b71
Andrew Xu <andrewxu@chromium.org>
2021-08-09 16:00:16 +0000

    [Shelf] Wrap the bounds of the dragged shelf item in absl::optional
    We should wrap `ScrollableShelfView::drag_item_bounds_in_screen_` in
    absl::optional and reset it when shelf drag-and-drop ends. It helps to
    indicate whether shelf drag-and-drop is ongoing.
    
    This bug is exposed by a recent CL which refactors shelf drag-and-drop
    (https://crrev.com/c/3046704).
    
    Bug: 1235882
    Change-Id: I0ace13c51164d6eed72bf6777f579df74fd5cc59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3079060
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909817}

--- ash/shelf/scrollable_shelf_view.h
+++ ash/shelf/scrollable_shelf_view.h
@@ -126,6 +126,9 @@ class ASH_EXPORT ScrollableShelfView : public views::AccessiblePaneView,
   // Returns the maximum scroll distance for the current layout.
   float GetScrollUpperBoundForTest() const;
 
+  // Returns whether `page_flip_timer_` is running.
+  bool IsPageFlipTimerBusyForTest() const;
+
   ShelfView* shelf_view() { return shelf_view_; }
   ShelfContainerView* shelf_container_view() { return shelf_container_view_; }
   const ShelfContainerView* shelf_container_view() const {
@@ -528,7 +531,7 @@ class ASH_EXPORT ScrollableShelfView : public views::AccessiblePaneView,
 
   // If page flip timer is active for shelf item drag, the last known drag item
   // bounds in screen coordinates.
-  gfx::Rect drag_item_bounds_in_screen_;
+  absl::optional<gfx::Rect> drag_item_bounds_in_screen_;
 
   base::OneShotTimer page_flip_timer_;
 
