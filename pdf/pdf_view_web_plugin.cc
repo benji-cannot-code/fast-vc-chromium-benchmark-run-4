c16ee86827afed01b63e3f7daae0b7809f42f156
Ankit Kumar 🌪️ <ankk@microsoft.com>
2021-08-09 22:28:17 +0000

    [unseasoned-pdf] Store PdfAccessibilityTree in PdfViewWebPlugin
    PdfViewWebPlugin will own PdfAccessibilityTree in order to pass
    accessibility info from PDF plugin. Since PdfViewWebPlugin can't
    know about PdfAccessibilityTree without violating the layers, store it
    in the form of its base class PdfAccessibilityDataHandler. Generate
    PdfAccessibilityTree using PdfViewWebPlugin::Client.
    
    Bug: 1144444
    Change-Id: I0918db285a0979c626b2b98ba16b675e4c53d12f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081226
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910023}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/accessibility_structs.h"
 #include "pdf/mojom/pdf.mojom.h"
 #include "pdf/parsed_params.h"
+#include "pdf/pdf_accessibility_data_handler.h"
 #include "pdf/pdf_engine.h"
 #include "pdf/pdf_init.h"
 #include "pdf/pdfium/pdfium_engine.h"
@@ -226,15 +227,21 @@ class BlinkContainerWrapper final : public PdfViewWebPlugin::ContainerWrapper {
 
 }  // namespace
 
+std::unique_ptr<PdfAccessibilityDataHandler>
+PdfViewWebPlugin::Client::CreateAccessibilityDataHandler(
+    PdfAccessibilityActionHandler* action_handler) {
+  return nullptr;
+}
+
 PdfViewWebPlugin::PdfViewWebPlugin(
     std::unique_ptr<Client> client,
     mojo::AssociatedRemote<pdf::mojom::PdfService> pdf_service_remote,
     const blink::WebPluginParams& params)
     : client_(std::move(client)),
       pdf_service_remote_(std::move(pdf_service_remote)),
-      initial_params_(params) {
-  DCHECK(client_);
-}
+      initial_params_(params),
+      pdf_accessibility_data_handler_(
+          client_->CreateAccessibilityDataHandler(this)) {}
 
 PdfViewWebPlugin::~PdfViewWebPlugin() = default;
 
