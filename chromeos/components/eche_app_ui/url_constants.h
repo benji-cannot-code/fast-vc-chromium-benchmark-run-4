56a18f198ebbb47fd3346271952d46376fa56d5a
Tommy Martino <tmartino@chromium.org>
2021-08-09 14:40:57 +0000

    Revert "Split Eche into trusted and untrusted(Rebased version)."
    This reverts commit a2198850048255dc081e88246608c676cf3fe56b.
    
    Reason for revert: Causing compile failures, see crbug.com/1237917
    
    Original change's description:
    > Split Eche into trusted and untrusted(Rebased version).
    >
    > This CL is based on the http://go/crrev/c/2757117 and rebased to the top
    > of tree.
    >
    > As landed, Eche runs purely as a chrome:// SWA. This isn't great because
    > Eche handles data from outside of the OS while also having the power to
    > call internal APIs like our Mojo APIs.
    >
    > By creating a chrome-untrusted:// iframe to hold our app code, we can
    > split the app into a trusted and untrusted section. The trusted part
    > handles the Mojo calls. The untrusted part handles everything else.
    >
    > They communicate over postMessage using the MessagePipe abstraction
    > which is also used by several other SWAs on the system.
    >
    > Test:
    > - Pass test cases: browser_tests,chromeos_components_unittests
    > - Manually merged with cl/386185197 and verified locally.
    > Bug: 1182418, b/181242381
    >
    > Change-Id: I312688595fa83133304aeb8d6d8501bc442598f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3040444
    > Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    > Reviewed-by: calamity <calamity@chromium.org>
    > Commit-Queue: Sam Chiu <samchiu@google.com>
    > Cr-Commit-Position: refs/heads/master@{#909788}
    
    Bug: 1237917, 1182418, b/181242381
    Change-Id: I0fbfe4c128bf9d6aea38c3964609306e97caf9d2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081743
    Auto-Submit: Tommy Martino <tmartino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Owners-Override: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#909799}

--- chromeos/components/eche_app_ui/url_constants.h
+++ chromeos/components/eche_app_ui/url_constants.h
@@ -11,8 +11,6 @@ namespace eche_app {
 
 extern const char kChromeUIEcheAppHost[];
 extern const char kChromeUIEcheAppURL[];
-extern const char kChromeUIEcheAppGuestHost[];
-extern const char kChromeUIEcheAppGuestURL[];
 
 }  // namespace eche_app
 }  // namespace chromeos
