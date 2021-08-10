ed9741907c4944f54061caff2c841b17b753588f
Christian Biesinger <cbiesinger@chromium.org>
2021-08-10 04:59:10 +0000

    Reland "[zoom-for-dsf] Scale focus ring by zoom."
    This reverts commit 8351b21fa9d6cc21c0a2c9f7eca5bed5d72d9cf5.
    
    Reason for revert: Now with more rebaselines
    
    Original change's description:
    > Revert "[zoom-for-dsf] Scale focus ring by zoom."
    >
    > This reverts commit fb63c65006116f3c2b853f5edcaa1217138e124c.
    >
    > Reason for revert: Suspicious about test failures of blink_web_tests paint/markers/document-markers-zoom-150.html, etc.
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.13%20Tests/40268/test-results
    >
    > Original change's description:
    > > [zoom-for-dsf] Scale focus ring by zoom.
    > >
    > > Mac only. For consistency with previous versions.
    > >
    > > See here for the changes in test expectations:
    > > https://test-results.appspot.com/data/layout_results/mac-rel/741375/blink_web_tests%20%28retry%20shards%20with%20patch%29/layout-test-results/results.html
    > >
    > > Bug: 1218722
    > > Change-Id: Idf506161f011a73fcc92ea680aefb5592325d962
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041885
    > > Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    > > Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#906340}
    >
    > Bug: 1218722
    > Change-Id: I518756afc44c169c176a6a7d241aef5ecc7ee9eb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056227
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    > Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#906568}
    
    Bug: 1218722
    Change-Id: Ic607762ae2b35820f0a6474b6e7632e26c1e4e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060742
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910161}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/case_map.h"
 #include "third_party/blink/renderer/platform/wtf/text/math_transform.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/native_theme/native_theme.h"
 
 namespace blink {
 
@@ -2367,7 +2368,12 @@ float ComputedStyle::FocusRingStrokeWidth() const {
   DCHECK(OutlineStyleIsAuto());
   // Draw focus ring with thickness in proportion to the zoom level, but never
   // so narrow that it becomes invisible.
-  return std::max(EffectiveZoom(), 3.f);
+  float width = 3.f;
+  if (EffectiveZoom() >= 1.0f) {
+    width = ui::NativeTheme::GetInstanceForWeb()->AdjustBorderWidthByZoom(
+        width, EffectiveZoom());
+  }
+  return std::max(EffectiveZoom(), width);
 }
 
 int ComputedStyle::FocusRingOffset() const {
