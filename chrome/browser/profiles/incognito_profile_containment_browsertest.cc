e99cf4be2948c88d41510a066bc397f4046ebda6
Ramin Halavati <rhalavati@chromium.org>
2021-08-10 13:35:49 +0000

    Update Incognito containment allow list for Mac.
    Bug: 1234755
    Change-Id: I867b5d32ccb238d98ab2f51b4f6ca6210feb9d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3085144
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910282}

--- chrome/browser/profiles/incognito_profile_containment_browsertest.cc
+++ chrome/browser/profiles/incognito_profile_containment_browsertest.cc
@@ -37,7 +37,7 @@ constexpr std::array<const char*, 7> kAllowListPrefixesForAllPlatforms = {
     "/Default/PreferredApps",   "/Default/Reporting and NEL",
     "/Default/shared_proto_db", "/Default/Trust Tokens",
     "/GrShaderCache/GPUCache"};
-#if !defined(OS_MAC)
+#if defined(OS_MAC)
 constexpr std::array<const char*, 4> kAllowListPrefixesForMac = {
     "/Default/data_reduction_proxy_leveldb", "/Default/Preferences",
     "/Default/Shortcuts", "/Default/Visited Links"};
@@ -154,7 +154,7 @@ class IncognitoProfileContainmentBrowserTest : public InProcessBrowserTest {
   IncognitoProfileContainmentBrowserTest()
       : allow_list_(std::begin(kAllowListPrefixesForAllPlatforms),
                     std::end(kAllowListPrefixesForAllPlatforms)) {
-#if !defined(OS_MAC)
+#if defined(OS_MAC)
     allow_list_.insert(std::begin(kAllowListPrefixesForMac),
                        std::end(kAllowListPrefixesForMac));
 #endif
