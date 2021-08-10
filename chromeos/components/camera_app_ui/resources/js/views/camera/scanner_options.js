48b955fae58511c2d80fc286afc3dcbc6090fc38
Pi-Hsun Shih <pihsun@chromium.org>
2021-08-10 04:24:46 +0000

    CCA: Add type assertion for video.srcObject
    The HTMLMediaElement.srcObject can be either MediaStream, MediaSource,
    Blob or File, but we're only expecting to get MediaStream here. Add type
    assertion.
    
    This drops the error count of `cca.py tsc` by 1.
    
    Bug: b:172340451
    Test: cca.py tsc
    Change-Id: Ib70e7a4c428d72ba50b2684368655606cd0c2e36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077903
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910150}

--- chromeos/components/camera_app_ui/resources/js/views/camera/scanner_options.js
+++ chromeos/components/camera_app_ui/resources/js/views/camera/scanner_options.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import * as barcodeChip from '../../barcode_chip.js';
-import {assert} from '../../chrome_util.js';
+import {assert, assertInstanceof} from '../../chrome_util.js';
 // eslint-disable-next-line no-unused-vars
 import {DeviceInfoUpdater} from '../../device/device_info_updater.js';
 import * as dom from '../../dom.js';
@@ -187,7 +187,9 @@ export class ScannerOptions {
     this.barcodeScanner_ = new BarcodeScanner(video, (value) => {
       barcodeChip.show(value);
     });
-    const {deviceId} = video.srcObject.getVideoTracks()[0].getSettings();
+    const {deviceId} = assertInstanceof(video.srcObject, MediaStream)
+                           .getVideoTracks()[0]
+                           .getSettings();
     this.documentCornerOverylay_.attach(deviceId);
     this.previewAttached_ = true;
     const scanType = (() => {
