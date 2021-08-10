7b01808b7b244f53458c0f215f562f7de7943e3e
Mark Pearson <mpearson@chromium.org>
2021-08-10 15:27:01 +0000

    Use A FixedFlatSet Instead of an Array
    Benefits: shorter / cleaner, and probably faster code.
    
    Also clean up the containing loop to make more idiomatic.
    
    It's too bad |events| is not an STL container, else the containing
    loop could be collapsed into a single erase_if call.
    
    Change-Id: I6f571d30587c51ae86a15316cf815de6001c6184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3073184
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910315}

--- components/metrics/structured/external_metrics.cc
+++ components/metrics/structured/external_metrics.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <sys/file.h>
 
+#include "base/containers/fixed_flat_set.h"
 #include "base/files/file.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
@@ -29,7 +30,7 @@ void MaybeFilterBluetoothEvents(
     google::protobuf::RepeatedPtrField<metrics::StructuredEventProto>* events) {
   // Event name hashes of all bluetooth events listed in
   // src/platform2/metrics/structured/structured.xml.
-  static constexpr uint64_t kBluetoothEventHashes[] = {
+  static constexpr auto kBluetoothEventHashes = base::MakeFixedFlatSet<uint64_t>({
       // BluetoothAdapterStateChanged
       UINT64_C(959829856916771459),
       // BluetoothPairingStateChanged
@@ -39,26 +40,19 @@ void MaybeFilterBluetoothEvents(
       // BluetoothProfileConnectionStateChanged
       UINT64_C(7217682640379679663),
       // BluetoothDeviceInfoReport
-      UINT64_C(1506471670382892394),
-  };
+      UINT64_C(1506471670382892394)
+  });
 
   if (base::FeatureList::IsEnabled(kBluetoothSessionizedMetrics))
     return;
 
-  for (int i = 0; i < events->size();) {
-    bool is_bluetooth = false;
-    const uint64_t event_hash = events->Get(i).event_name_hash();
-    for (const uint64_t bt_hash : kBluetoothEventHashes) {
-      if (event_hash == bt_hash) {
-        is_bluetooth = true;
-        break;
-      }
-    }
-
-    if (is_bluetooth) {
-      events->erase(events->begin() + i);
+  // Remove all bluetooth events.
+  auto it = events->begin();
+  while (it != events->end()) {
+    if (kBluetoothEventHashes.contains(it->event_name_hash())) {
+      it = events->erase(it);
     } else {
-      ++i;
+      ++it;
     }
   }
 }
