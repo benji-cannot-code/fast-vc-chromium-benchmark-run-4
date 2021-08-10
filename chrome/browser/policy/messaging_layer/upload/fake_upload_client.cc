b1762b3c201c0fe77834e3fc02f8a34bf74a9301
Matt Menke <mmenke@chromium.org>
2021-08-10 22:32:41 +0000

    Add missing header to fake_upload_client.cc
    It depends on "base/strings/string_number_conversions.h", but is
    depending on pulling it in through some circuitous route that goes
    through net/ rather than including it.
    
    This was split off of
    https://chromium-review.googlesource.com/c/chromium/src/+/3078384, which
    removes unnecessary includes in a couple commonly included net/ files.
    
    Bug: 1227131
    Change-Id: I443ee7c722e9347c3e3ac23b7aaa87d2e890a851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3084421
    Auto-Submit: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Zach Trudo <zatrudo@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910515}

--- chrome/browser/policy/messaging_layer/upload/fake_upload_client.cc
+++ chrome/browser/policy/messaging_layer/upload/fake_upload_client.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/strcat.h"
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/policy/messaging_layer/upload/record_upload_request_builder.h"
 #include "components/reporting/proto/record.pb.h"
 #include "components/reporting/proto/record_constants.pb.h"
