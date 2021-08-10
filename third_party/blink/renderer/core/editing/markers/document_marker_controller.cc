3211c1072367823dff9ee5d07f7286d7f8e65026
Dan McArdle <dmcardle@chromium.org>
2021-08-10 18:05:58 +0000

    [WTF::HashMap Refactor] Use DeprecatedAtOrEmptyValue() in /third_party/blink/renderer/core/editing
    This CL is a true behavior-preserving refactor. It will not affect
    performance because HashMap<>::DeprecatedAtOrEmptyValue() has an
    identical implementation to HashMap<>::at().
    
    In the near future, we will change the definition of HashMap<>::at() to
    CHECK that the map contains the given key.
    
    All occurrences of DeprecatedAtOrEmptyValue will need to be cleaned up.
    If the caller cannot know whether the map already contains the given
    key, one possible workaround is to use HashMap<>::find() instead.
    
    Design doc: https://docs.google.com/document/d/18JIiajErikZaBzCtZvl-wwAJShqZsg0jT9wVyPBhSdU/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=yosin@chromium.org
    
    Bug: 1058527
    Change-Id: I1282ea5da323d92cd1484170b9da7d131e8e6691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081473
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910398}

--- third_party/blink/renderer/core/editing/markers/document_marker_controller.cc
+++ third_party/blink/renderer/core/editing/markers/document_marker_controller.cc
@@ -373,7 +373,7 @@ void DocumentMarkerController::MoveMarkers(const Text& src_node,
     return;
   DCHECK(!markers_.IsEmpty());
 
-  MarkerLists* const src_markers = markers_.at(&src_node);
+  MarkerLists* const src_markers = markers_.DeprecatedAtOrEmptyValue(&src_node);
   if (!src_markers)
     return;
 
@@ -417,7 +417,7 @@ void DocumentMarkerController::RemoveMarkersInternal(
     return;
   DCHECK(!(markers_.IsEmpty()));
 
-  MarkerLists* const markers = markers_.at(&text);
+  MarkerLists* const markers = markers_.DeprecatedAtOrEmptyValue(&text);
   if (!markers)
     return;
 
@@ -903,7 +903,7 @@ static void InvalidatePaintForTickmarks(const Node& node) {
 
 void DocumentMarkerController::InvalidateRectsForTextMatchMarkersInNode(
     const Text& node) {
-  MarkerLists* markers = markers_.at(&node);
+  MarkerLists* markers = markers_.DeprecatedAtOrEmptyValue(&node);
 
   const DocumentMarkerList* const marker_list =
       ListForType(markers, DocumentMarker::kTextMatch);
