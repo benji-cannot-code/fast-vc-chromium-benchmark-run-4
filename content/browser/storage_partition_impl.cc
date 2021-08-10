bb75c93cb11bc688da772cad0204676dd7267954
Ari Chivukula <arichiv@chromium.org>
2021-08-10 02:42:24 +0000

    {BACKTICKS} [Origin -> StorageKey] third_party/blink/public/mojom/dom_storage/dom_storage.mojom
    This CL contains fixes to swap | with ` for comments in files touched by
    the stacked CL as per: https://chromium.googlesource.com/chromium/src/+/refs/heads/main/styleguide/c++/c++-dos-and-donts.md#comment-style
    
    Bug: 1212808
    Change-Id: I1249d250ed7d1ede6d3071f667beb9333e41853f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076816
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910125}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -486,7 +486,7 @@ class LoginHandlerDelegate {
  private:
   void OnRequestCancelled() {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    // This will destroy |login_handler_io_| on the IO thread and, if needed,
+    // This will destroy `login_handler_io_` on the IO thread and, if needed,
     // inform the delegate.
     delete this;
   }
@@ -748,8 +748,8 @@ void FinishGenerateNegotiateAuthToken(
 // surface to something simple and generic. It is designed to be used by
 // callsites in ClearDataImpl.
 //
-// Precondition: |matcher_func| and |storage_origin| cannot both be set.
-// If both |matcher_func| and |storage_origin| are null/empty, should return a
+// Precondition: `matcher_func` and `storage_origin` cannot both be set.
+// If both `matcher_func` and `storage_origin` are null/empty, should return a
 // null callback that indicates all origins should match. This is an
 // optimization for backends to efficiently clear all data.
 //
@@ -944,7 +944,7 @@ class StoragePartitionImpl::QuotaManagedDataDeletionHelper {
 // and IO thread. An instance of this class is created in the beginning of
 // deletion process (StoragePartitionImpl::ClearDataImpl) and the instance is
 // forwarded and updated on each (sub) deletion's callback. The instance is
-// finally destroyed when deletion completes (and |callback| is invoked).
+// finally destroyed when deletion completes (and `callback` is invoked).
 class StoragePartitionImpl::DataDeletionHelper {
  public:
   DataDeletionHelper(uint32_t remove_mask,
@@ -1090,7 +1090,7 @@ class StoragePartitionImpl::ServiceWorkerCookieAccessObserver
     }
   }
 
-  // |storage_partition_| owns this object via UniqueReceiverSet
+  // `storage_partition_` owns this object via UniqueReceiverSet
   // (service_worker_cookie_observers_).
   StoragePartitionImpl* storage_partition_;
 };
@@ -1447,7 +1447,7 @@ StoragePartitionImpl::GetCookieManagerForBrowserProcess() {
   // Create the CookieManager as needed.
   if (!cookie_manager_for_browser_process_ ||
       !cookie_manager_for_browser_process_.is_connected()) {
-    // Reset |cookie_manager_for_browser_process_| before binding it again.
+    // Reset `cookie_manager_for_browser_process_` before binding it again.
     cookie_manager_for_browser_process_.reset();
     GetNetworkContext()->GetCookieManager(
         cookie_manager_for_browser_process_.BindNewPipeAndPassReceiver());
@@ -2155,7 +2155,7 @@ void StoragePartitionImpl::ClearDataImpl(
       remove_mask, quota_storage_remove_mask,
       base::BindOnce(&StoragePartitionImpl::DeletionHelperDone,
                      weak_factory_.GetWeakPtr(), std::move(callback)));
-  // |helper| deletes itself when done in
+  // `helper` deletes itself when done in
   // DataDeletionHelper::DecrementTaskCount().
   deletion_helpers_running_++;
   helper->ClearDataOnUIThread(
@@ -2371,7 +2371,7 @@ void StoragePartitionImpl::DataDeletionHelper::ClearDataOnUIThread(
   DCHECK_NE(remove_mask_, 0u);
   DCHECK(callback_);
 
-  // Only one of |storage_origin| and |origin_matcher| can be set.
+  // Only one of `storage_origin` and `origin_matcher` can be set.
   DCHECK(storage_origin.is_empty() || origin_matcher.is_null());
 
   GetUIThreadTaskRunner({})->PostDelayedTask(
@@ -2388,8 +2388,8 @@ void StoragePartitionImpl::DataDeletionHelper::ClearDataOnUIThread(
       base::WrapRefCounted(special_storage_policy);
 
   if (remove_mask_ & REMOVE_DATA_MASK_COOKIES) {
-    // The CookieDeletionFilter has a redundant time interval to |begin| and
-    // |end|. Ensure that the filter has no time interval specified to help
+    // The CookieDeletionFilter has a redundant time interval to `begin` and
+    // `end`. Ensure that the filter has no time interval specified to help
     // callers detect when they are using the wrong interval values.
     DCHECK(!cookie_deletion_filter->created_after_time.has_value());
     DCHECK(!cookie_deletion_filter->created_before_time.has_value());
@@ -2731,7 +2731,7 @@ void StoragePartitionImpl::InitNetworkContext() {
   devtools_instrumentation::ApplyNetworkContextParamsOverrides(
       browser_context_, context_params.get());
   DCHECK(!context_params->cert_verifier_params)
-      << "|cert_verifier_params| should not be set in the "
+      << "`cert_verifier_params` should not be set in the "
          "NetworkContextParams, as they will be replaced with a new pipe to "
          "the CertVerifierService.";
 
