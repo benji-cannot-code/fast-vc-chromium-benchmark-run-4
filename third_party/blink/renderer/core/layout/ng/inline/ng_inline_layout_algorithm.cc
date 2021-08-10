01f1eb403989130606523a0866dafa9110871490
Ian Kilpatrick <ikilpatrick@chromium.org>
2021-08-10 22:49:41 +0000

    [block-in-inline] Introduce LineBoxBfcBlockOffset.
    This introduces another BfcBlockOffset for line-boxes.
    
    Specifically with block-in-inline we'll have:
     - BfcBlockOffset, e.g. where a child resolved the BfcBlockOffset.
     - LineBoxBfcBlockOffset, e.g. where a line-box has been placed.
    
    These can be different in presence of floats. E.g. we'll place floats
    at the BfcBlockOffset, then the line-box may be pushed down, and will
    be located at the LineBoxBfcBlockOffset. For example:
    
    <div style="width: 100px; display: flow-root;">
      <div style="float: left; width: 200px; height: 20px;"></div>
      text
    </div>
    
    Here we resolve the BFC block-offset at 0px (where we position the
    float), however the line-box BFC block-offset is at 20px.
    
    This patch just introduces this member, however we don't set it to be
    different yet. As such there should be no behaviour change.
    
    Bug: 716930
    Change-Id: I44e4715539a055f8fb469b25da611fde83b6159a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082153
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910545}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.cc
@@ -472,6 +472,7 @@ void NGInlineLayoutAlgorithm::CreateLine(
   container_builder_.SetInlineSize(inline_size);
   container_builder_.SetMetrics(line_box_metrics);
   container_builder_.SetBfcBlockOffset(block_offset);
+  container_builder_.SetLineBoxBfcBlockOffset(block_offset);
 }
 
 void NGInlineLayoutAlgorithm::PlaceControlItem(const NGInlineItem& item,
@@ -617,6 +618,7 @@ void NGInlineLayoutAlgorithm::PlaceBlockInInline(
   container_builder_.SetBaseDirection(line_info.BaseDirection());
   if (absl::optional<LayoutUnit> block_offset = result.BfcBlockOffset()) {
     container_builder_.SetBfcBlockOffset(*block_offset);
+    container_builder_.SetLineBoxBfcBlockOffset(*block_offset);
     container_builder_.SetEndMarginStrut(result.EndMarginStrut());
     container_builder_.SetAdjoiningObjectTypes(result.AdjoiningObjectTypes());
     const NGConstraintSpace& space = ConstraintSpace();
@@ -1064,7 +1066,7 @@ LayoutUnit NGInlineLayoutAlgorithm::ComputeContentSize(
   // layout_object may be null in certain cases, e.g. if it's a kBidiControl.
   if (layout_object && layout_object->IsBR()) {
     const LayoutUnit line_box_bfc_block_offset =
-        ContainerBfcOffset().block_offset;
+        *container_builder_.LineBoxBfcBlockOffset();
     NGBfcOffset bfc_offset = {LayoutUnit(),
                               line_box_bfc_block_offset + content_size};
     AdjustToClearance(
