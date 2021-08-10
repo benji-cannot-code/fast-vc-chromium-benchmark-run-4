de6d9e35bb56acaec5fc500d6f6bb8993963766f
Dylan Cutler <dylancutler@google.com>
2021-08-10 22:40:04 +0000

    Compute cookie partition key when ingesting cookies from the Set-Cookie header.
    We only ingest partition keys if the --partitioned-cookies flag is set.
    Otherwise we will treat the cookie as unpartitioned.
    
    Bug: 1225444
    Change-Id: I138a80d1bac0197c8268b56e7527aac0d757b8fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069119
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910528}

--- net/cookies/cookie_partition_key_unittest.cc
+++ net/cookies/cookie_partition_key_unittest.cc
@@ -7,12 +7,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_COOKIES_COOKIE_PARTITION_KEY_UNITTEST_H_
 
 #include "net/cookies/cookie_partition_key.h"
+#include "base/test/scoped_feature_list.h"
+#include "net/base/features.h"
 #include "net/cookies/cookie_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace net {
 
-TEST(CookiePartitionKeyTest, Serialization) {
+class CookiePartitionKeyTest : public testing::TestWithParam<bool> {
+ protected:
+  // testing::Test
+  void SetUp() override {
+    if (PartitionedCookiesEnabled())
+      scoped_feature_list_.InitAndEnableFeature(features::kPartitionedCookies);
+    testing::TestWithParam<bool>::SetUp();
+  }
+
+  bool PartitionedCookiesEnabled() { return GetParam(); }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+INSTANTIATE_TEST_SUITE_P(/* no label */,
+                         CookiePartitionKeyTest,
+                         testing::Bool());
+
+TEST_P(CookiePartitionKeyTest, Serialization) {
   struct {
     absl::optional<CookiePartitionKey> input;
     bool expected_ret;
@@ -48,7 +69,7 @@ TEST(CookiePartitionKeyTest, Serialization) {
   }
 }
 
-TEST(CookiePartitionKeyTest, Deserialization) {
+TEST_P(CookiePartitionKeyTest, Deserialization) {
   struct {
     std::string input;
     bool expected_ret;
@@ -70,6 +91,23 @@ TEST(CookiePartitionKeyTest, Deserialization) {
     } else {
       EXPECT_FALSE(got.has_value());
     }
+  }
+}
+
+TEST_P(CookiePartitionKeyTest, FromNetworkIsolationKey) {
+  EXPECT_FALSE(
+      CookiePartitionKey::FromNetworkIsolationKey(NetworkIsolationKey()));
+
+  SchemefulSite top_level_site =
+      SchemefulSite(GURL("https://toplevelsite.com"));
+  absl::optional<CookiePartitionKey> got =
+      CookiePartitionKey::FromNetworkIsolationKey(NetworkIsolationKey(
+          top_level_site, SchemefulSite(GURL("https://cookiesite.com"))));
+
+  bool partitioned_cookies_enabled = PartitionedCookiesEnabled();
+  EXPECT_EQ(partitioned_cookies_enabled, got.has_value());
+  if (partitioned_cookies_enabled) {
+    EXPECT_EQ(CookiePartitionKey(top_level_site), got.value());
   }
 }
 
