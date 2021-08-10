90e104b841a8cc64cfe178c82f45b75257e26e66
Bettina Dea <bdea@chromium.org>
2021-08-10 19:16:41 +0000

    Combine IsPrimaryAccountGmail and IsOtherGaiaAccountGmail
    Bug: 1234576
    Change-Id: I12b1972db6c16682b38ae1ae1a4304b3f77187b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061433
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910439}

--- ios/chrome/browser/safe_browsing/chrome_password_protection_service.h
+++ ios/chrome/browser/safe_browsing/chrome_password_protection_service.h
@@ -134,7 +134,7 @@ class ChromePasswordProtectionService
 
   AccountInfo GetAccountInfo() const override;
 
-  AccountInfo GetSignedInNonSyncAccount(
+  AccountInfo GetAccountInfoForUsername(
       const std::string& username) const override;
 
   safe_browsing::LoginReputationClientRequest::PasswordReuseEvent::
@@ -164,9 +164,7 @@ class ChromePasswordProtectionService
 
   bool IsPrimaryAccountSignedIn() const override;
 
-  bool IsPrimaryAccountGmail() const override;
-
-  bool IsOtherGaiaAccountGmail(const std::string& username) const override;
+  bool IsAccountGmail(const std::string& username) const override;
 
   bool IsInExcludedCountry() override;
 
