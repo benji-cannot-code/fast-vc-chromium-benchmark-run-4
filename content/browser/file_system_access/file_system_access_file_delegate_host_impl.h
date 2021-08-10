8d6c598276475238f51a1a57beed8c86b2fb3ce0
Austin Sullivan <asully@chromium.org>
2021-08-10 04:52:06 +0000

    FSA: Implement SetLength for the sync access handle in incognito
    This is part of the efforts to merge Storage Foundation into OPFS.
    
    Also changes FileSystemAccessFileDelegate::SetLength to be async.
    
    Bug: 1225653
    Change-Id: I4a76e69db3c2d90ad5e8f2513604de6b3630c9a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064660
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910157}

--- content/browser/file_system_access/file_system_access_file_delegate_host_impl.h
+++ content/browser/file_system_access/file_system_access_file_delegate_host_impl.h
@@ -39,6 +39,7 @@ class CONTENT_EXPORT FileSystemAccessFileDelegateHostImpl
              mojo::ScopedDataPipeConsumerHandle data,
              WriteCallback callback) override;
   void GetLength(GetLengthCallback callback) override;
+  void SetLength(uint64_t length, SetLengthCallback callback) override;
 
  private:
   // State that is kept for the duration of a write operation, to keep track of
