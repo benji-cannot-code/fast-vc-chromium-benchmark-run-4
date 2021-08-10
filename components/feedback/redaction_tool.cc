f032b3ac6232063bee19f74e19843144d60f0669
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-10 09:17:34 +0000

    [automated] Drop needless <vector> includes in /components/feedback
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=jimmyxgong@chromium.org
    
    Bug: 1233968
    Change-Id: I4d34f696e67c0ce95127adc8eca742bf6cb41d3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059439
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#910215}

--- components/feedback/redaction_tool.cc
+++ components/feedback/redaction_tool.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/feedback/redaction_tool.h"
 
-#include <memory>
 #include <utility>
+#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/strings/strcat.h"
