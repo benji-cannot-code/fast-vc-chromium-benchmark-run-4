75ef22a8bc5fc347c54c611029e59e023d40410c
Tommy Martino <tmartino@google.com>
2021-08-10 19:46:13 +0000

    [SH iOS] Implement URL Consistency
    When the highlighted text fragments are removed by the user, the URL will now be updated to reflect this.
    
    Bug: 1230576
    Change-Id: Iaf3ed9f689437207d819274c8ffbe7d71aca072a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078660
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910449}

--- ios/web/text_fragments/text_fragments_java_script_feature.h
+++ ios/web/text_fragments/text_fragments_java_script_feature.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #import "ios/web/public/js_messaging/java_script_feature.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "url/gurl.h"
 
 namespace web {
 
@@ -32,8 +33,8 @@ class TextFragmentsJavaScriptFeature : public JavaScriptFeature {
                                     std::string foreground_color_hex_rgb);
 
   // Removes all highlights that are currently being displayed on the page as a
-  // result of invoking ProcessTextFragments.
-  virtual void RemoveHighlights(WebState* web_state);
+  // result of invoking ProcessTextFragments. Updates the page URL to |new_url|.
+  virtual void RemoveHighlights(WebState* web_state, const GURL& new_url);
 
  protected:
   // JavaScriptFeature:
