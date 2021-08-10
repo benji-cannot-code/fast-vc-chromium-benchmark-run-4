e2beda8414b925cd5226417137a450d2f398aeb8
Alex Gough <ajgo@chromium.org>
2021-08-10 22:29:28 +0000

    Specify ChromeOS service sandboxes in mojom
    This moves to using the ServiceSandbox mojom attribute to select the
    sandbox, and removes reference from service_sandbox_type.h which can
    now be deleted.
    
    The following services now have their sandbox defined in their mojom
    file:-
    
     ImeService,
     TtsService,
     RecordingService,
     AssistantAudioDecoderFactory,
     LibassistantService
    
    No functional changes, see linked bug for details.
    
    Bug: 1210301
    Change-Id: I1791d1c6b66f7ad2e5bb7827c6c21e54ef25b19c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3073582
    Reviewed-by: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910509}

--- chrome/browser/ui/ash/chrome_capture_mode_delegate.cc
+++ chrome/browser/ui/ash/chrome_capture_mode_delegate.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/ash/policy/dlp/dlp_content_manager.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chromeos/service_sandbox_type.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
