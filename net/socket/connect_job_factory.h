0ce56cf7764f39a978865b577cbb0ad82a332cea
Matt Menke <mmenke@chromium.org>
2021-08-10 16:51:22 +0000

    Use non-empty NIK for proxy DNS lookups.
    We want to DCHECK that all DNS lookups use NIKs, when NIKs are enabled.
    
    Bug: 1082280
    Change-Id: I65648958040cd3d82a48011eabc9eb8722fee6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3064705
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910352}

--- net/socket/connect_job_factory.h
+++ net/socket/connect_job_factory.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "net/base/host_port_pair.h"
+#include "net/base/network_isolation_key.h"
 #include "net/base/privacy_mode.h"
 #include "net/base/request_priority.h"
 #include "net/dns/public/secure_dns_policy.h"
@@ -123,6 +124,13 @@ class NET_EXPORT_PRIVATE ConnectJobFactory {
   std::unique_ptr<TransportConnectJob::Factory> transport_connect_job_factory_;
   std::unique_ptr<WebSocketTransportConnectJob::Factory>
       websocket_transport_connect_job_factory_;
+
+  // Use a single NetworkIsolationKey for looking up proxy hostnames. Proxies
+  // are typically used across sites, but cached proxy IP addresses don't
+  // really expose useful information to destination sites, and not caching
+  // them has a performance cost.
+  net::NetworkIsolationKey proxy_dns_network_isolation_key_ =
+      net::NetworkIsolationKey::CreateTransient();
 };
 
 }  // namespace net
