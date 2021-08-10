8d675adbd88c746d331124d78ad83f5ae445ff94
Rakina Zata Amni <rakina@chromium.org>
2021-08-10 17:48:10 +0000

    Move commit-time initial empty document replacement decision to browser
    Previously, the renderer can change a navigation to do a replacement
    at commit time if it's on the initial empty document. This makes it
    possible for the value of should_replace_current_entry in
    CommonNavigationParams and DidCommitProvisionalLoadParams to differ.
    
    This CL moves that decision to happen on the browser side instead,
    since it's now possible to determine the "initial empty document"-ness
    on the browser side (it's not perfect but it's consistent with the
    renderer's currently used value). With this, the value of
    should_replace_current_entry should be the same in both
    CommonNavigationParams and DidCommitProvisionalLoadParams for all
    cross-document navigations.
    
    Since we still need the "should replace due to initial empty document"
    decision in the renderer for synchronous renderer navigations that
    starts and immediately commits in the renderer (same-document
    navigations and the synchronous about:blank commit), we still keep the
    function but only call it for renderer-initiated navigations at the
    start of the navigation, and also rename it & add better documentation
    for it.
    
    Note that the browser-side "should replace for initial empty document"
    function only takes care of subframes, because the main frame case
    currently handled in the renderer (empty URL navigation) can only
    happen during the synchronous about:blank commit, which is still
    handled in the renderer.
    
    This CL does not introduce any behavior change, except for
    browser-initiated same-document navigations, which will now behave the
    same way as renderer-initiated same-document navigations (it will do
    a replacement if it happens on an initial empty document).
    
    Bug: 1131832, 1215096
    Change-Id: Ife404d6618017d02431c9e4c81da8619c06a9045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3066717
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910392}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -271,8 +271,8 @@ class CONTENT_EXPORT NavigationRequest
   const GURL& GetURL() override;
   SiteInstanceImpl* GetStartingSiteInstance() override;
   SiteInstanceImpl* GetSourceSiteInstance() override;
-  bool IsInMainFrame() override;
-  bool IsInPrimaryMainFrame() override;
+  bool IsInMainFrame() const override;
+  bool IsInPrimaryMainFrame() const override;
   bool IsInPrerenderedMainFrame() override;
   bool IsPrerenderedPageActivation() override;
   bool IsRendererInitiated() override;
@@ -1418,6 +1418,10 @@ class CONTENT_EXPORT NavigationRequest
   // Whether this is a same-URL navigation that should replace the current entry
   // or not. Called when the navigation just started.
   bool ShouldReplaceCurrentEntryForSameUrlNavigation() const;
+
+  // Whether this navigation happens on the initial empty document, and thus
+  // should replace the current entry.  Called when the navigation just started.
+  bool ShouldReplaceCurrentEntryForNavigationFromInitialEmptyDocument() const;
 
   // Whether a failed navigation should replace the current entry or not. Called
   // when an error page is about to be committed.
--- content/public/test/mock_navigation_handle.h
+++ content/public/test/mock_navigation_handle.h
@@ -50,7 +50,7 @@ class MockNavigationHandle : public NavigationHandle {
   SiteInstance* GetSourceSiteInstance() override {
     return nullptr;  // Good enough for unit tests...
   }
-  bool IsInMainFrame() override {
+  bool IsInMainFrame() const override {
     return render_frame_host_ ? !render_frame_host_->GetParent() : true;
   }
   MOCK_METHOD0(IsInPrerenderedMainFrame, bool());
@@ -63,7 +63,9 @@ class MockNavigationHandle : public NavigationHandle {
     NOTIMPLEMENTED();
     return false;
   }
-  bool IsInPrimaryMainFrame() override { return is_in_primary_main_frame_; }
+  bool IsInPrimaryMainFrame() const override {
+    return is_in_primary_main_frame_;
+  }
   MOCK_METHOD0(GetFrameTreeNodeId, int());
   MOCK_METHOD0(GetPreviousRenderFrameHostId, GlobalRenderFrameHostId());
   bool IsServedFromBackForwardCache() override {
