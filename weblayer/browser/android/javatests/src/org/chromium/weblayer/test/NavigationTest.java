02e1152354c5a080ab11073b754f62fb387643a1
bttk <bttk@chromium.org>
2021-08-10 00:46:46 +0000

    Disable testWebResponseExpired which sleeps for 5s
    Bug: 1238151
    Change-Id: If38a81d1f48ccb244741169e6187798904fba287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3083603
    Commit-Queue: bttk <bttk@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Owners-Override: bttk <bttk@chromium.org>
    Auto-Submit: bttk <bttk@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910076}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/NavigationTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/NavigationTest.java
@@ -32,6 +32,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.net.test.util.TestWebServer;
 import org.chromium.weblayer.Browser;
@@ -1368,6 +1369,7 @@ public class NavigationTest {
                 curFailedCount, STREAM_URL, LoadError.CONNECTIVITY_ERROR);
     }
 
+    @DisabledTest(message = "https://crbug.com/1238151")
     @Test
     @SmallTest
     public void testWebResponseExpired() throws Exception {
