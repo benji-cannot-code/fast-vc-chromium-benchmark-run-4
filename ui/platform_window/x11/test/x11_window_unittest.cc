f797989206741c72fe5cf3b7f0a19354111a1e6e
Keren Zhu <kerenzhu@chromium.org>
2021-08-10 18:43:24 +0000

    [x11] Fix activation when window is restored from minimized
    r897795 made X11Window::IsActive() return false when the window is
    minimized, and it caused the deactivation/activation event not being
    propagated to aura::Window level.
    
    * Move the test of IsMinimized() from IsAcitve() to
    AfterActivationStateChanged() so that `was_active_` is properly
    calculated.
    * Add regression tests.
    
    BUG=1237658,1237982
    TEST=manually test with i3 and KDE, X11WindowTest.ToggleMinimizePropogateToPlatformWindowDelegate
    
    Change-Id: Ie6b36abc74fcd2fc4b055ecfa58c48baf51cef0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3084381
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910419}

--- ui/platform_window/x11/test/x11_window_unittest.cc
+++ ui/platform_window/x11/test/x11_window_unittest.cc
@@ -42,6 +42,7 @@ class TestPlatformWindowDelegate : public PlatformWindowDelegate {
 
   gfx::AcceleratedWidget widget() const { return widget_; }
   PlatformWindowState state() const { return state_; }
+  bool active() const { return active_; }
 
   void WaitForBoundsSet(const gfx::Rect& expected_bounds) {
     if (changed_bounds_ == expected_bounds)
@@ -75,7 +76,7 @@ class TestPlatformWindowDelegate : public PlatformWindowDelegate {
   void OnAcceleratedWidgetDestroyed() override {
     widget_ = gfx::kNullAcceleratedWidget;
   }
-  void OnActivationChanged(bool active) override {}
+  void OnActivationChanged(bool active) override { active_ = active; }
   void OnMouseEnter() override {}
   SkPath GetWindowMaskForWindowShapeInPixels() override {
     SkPath window_mask;
@@ -97,6 +98,7 @@ class TestPlatformWindowDelegate : public PlatformWindowDelegate {
   PlatformWindowState state_ = PlatformWindowState::kUnknown;
   gfx::Rect changed_bounds_;
   gfx::Rect expected_bounds_;
+  bool active_ = false;
 
   // Ends the run loop.
   base::OnceClosure quit_closure_;
@@ -470,6 +472,11 @@ TEST_F(X11WindowTest, ToggleMinimizePropogateToPlatformWindowDelegate) {
   }
   EXPECT_TRUE(window->IsMinimized());
   EXPECT_EQ(delegate.state(), PlatformWindowState::kMinimized);
+  // X11 allows a window to be both minimized and active.
+  EXPECT_TRUE(delegate.active());
+
+  window->Deactivate();
+  EXPECT_FALSE(delegate.active());
 
   // Show from minimized by sending _NET_WM_STATE_FOCUSED
   {
@@ -492,6 +499,9 @@ TEST_F(X11WindowTest, ToggleMinimizePropogateToPlatformWindowDelegate) {
   }
   EXPECT_FALSE(window->IsMinimized());
   EXPECT_EQ(delegate.state(), PlatformWindowState::kNormal);
+  EXPECT_FALSE(delegate.active());
+  window->Activate();
+  EXPECT_TRUE(delegate.active());
 }
 
 }  // namespace ui
