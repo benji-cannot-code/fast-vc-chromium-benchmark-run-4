8bb066c5f306cc9f121a22ba0a3d5801754b2940
Koji Ishii <kojii@chromium.org>
2021-08-10 07:16:21 +0000

    Support block-fragmented boxes in |PrePaintTreeWalk|
    When NG block fragmentation and block-in-inline are enabled,
    boxes in inline formatting contexts can be block-fragmented.
    This CL changes |PrePaintTreeWalk::WalkLayoutObjectChildren|
    to support the case by iterating all fragments for a child
    |LayoutObject|.
    
    Also changes to use |NGInlineCursor::MoveTo| instead of
    iterating |NGFragmentItems|. This change utilizes the item
    index cache, without scanning all items.
    
    Fixes 13 |AssertClearedPaintInvalidationFlags| failures for
    tests under `virtual/layout_ng_block_frag` when
    NGBlockInInline is enabled:
      external/wpt/css/css-break/out-of-flow-in-multicolumn-054.html
      external/wpt/css/css-multicol/multicol-span-all-restyle-002.html
      external/wpt/css/css-multicol/multicol-span-float-003.html
      fast/multicol/dynamic/change-spanner-parent-display.html
      fast/multicol/dynamic/insert-spanner-pseudo-after.html
      fast/multicol/dynamic/insert-spanner-pseudo-before-after-in-content.html
      fast/multicol/dynamic/insert-spanner-pseudo-before.html
      fast/multicol/span/anonymous-split-block-crash.html
      fast/multicol/span/pseudo-after-then-content.html
      fast/multicol/span/pseudo-after.html
      fast/multicol/span/pseudo-before-after-in-content.html
      fast/multicol/span/pseudo-before-following-content.html
      fast/multicol/span/pseudo-before.html
    
    Bug: 716930, 829028
    Change-Id: Ic0b1a3ebd2e385d1e861f8885467e32d6dcee7bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077906
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910185}

--- third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
+++ third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_shift_tracker.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
@@ -890,8 +891,10 @@ void PrePaintTreeWalk::WalkLayoutObjectChildren(
     const LayoutObject& parent_object,
     const NGPhysicalBoxFragment* parent_fragment,
     PrePaintTreeWalkContext& context) {
+  absl::optional<NGInlineCursor> inline_cursor;
   for (const LayoutObject* child = parent_object.SlowFirstChild(); child;
-       child = child->NextSibling()) {
+       // Stay on the |child| while iterating fragments of |child|.
+       child = inline_cursor ? child : child->NextSibling()) {
     if (!parent_fragment) {
       // If we haven't found a fragment tree to accompany us in our walk,
       // perform a pure LayoutObject tree walk. This is needed for legacy block
@@ -941,10 +944,22 @@ void PrePaintTreeWalk::WalkLayoutObjectChildren(
     bool is_first_for_node = true;
     bool is_last_for_node = true;
     bool is_inside_fragment_child = false;
-    if (parent_fragment->Items() && child->FirstInlineFragmentItemIndex()) {
-      for (const NGFragmentItem& item : parent_fragment->Items()->Items()) {
-        if (item.GetLayoutObject() != child)
-          continue;
+
+    if (!inline_cursor && parent_fragment->HasItems() &&
+        child->HasInlineFragments()) {
+      // Limit the search to descendants of |parent_fragment|.
+      inline_cursor.emplace(*parent_fragment);
+      inline_cursor->MoveTo(*child);
+      // Searching fragments of |child| may not find any because they may be in
+      // other fragmentainers than |parent_fragment|.
+    }
+    if (inline_cursor) {
+      for (; inline_cursor->Current();
+           inline_cursor->MoveToNextForSameLayoutObject()) {
+        // Check if the search is limited to descendants of |parent_fragment|.
+        DCHECK_EQ(&inline_cursor->ContainerFragment(), parent_fragment);
+        const NGFragmentItem& item = *inline_cursor->Current().Item();
+        DCHECK_EQ(item.GetLayoutObject(), child);
 
         is_last_for_node = item.IsLastForNode();
         if (box_fragment) {
@@ -972,6 +987,12 @@ void PrePaintTreeWalk::WalkLayoutObjectChildren(
 
         // Keep looking for the end. We need to know whether this is the last
         // time we're going to visit this object.
+      }
+      if (is_last_for_node || !inline_cursor->Current()) {
+        // If all fragments are done, move to the next sibling of |child|.
+        inline_cursor.reset();
+      } else {
+        inline_cursor->MoveToNextForSameLayoutObject();
       }
       if (!box_fragment)
         continue;
