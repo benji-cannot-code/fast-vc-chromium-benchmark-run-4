32bfdc57f0e92e821bf96d75999c39ab32625986
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-08-10 09:47:02 +0000

    [automated] Drop needless <vector> includes in /components/media_router
    This is among the most costful includes for the Chromium build.
    https://commondatastorage.googleapis.com/chromium-browser-clang/include-analysis.html
    
    This batch of CLs attempts to detect and remove leftover includes with a
    heuristic: choose files which don't contain 'std::vector' or 'push_back'.
    This doesn't really guarantee that std::vector is not being used, as
    shown by the snippets below, so please review carefully.
    ```
    auto v = GetVector();
    MyVectorAlias v = GetVector();
    my_vector_ = GetVector();
    ```
    
    This CL was uploaded by git cl split.
    
    R=btolsch@chromium.org
    
    Bug: 1233968
    Change-Id: I6206de650c6afe5138195ad8dad7551c64ce8a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059578
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Brandon Tolsch <btolsch@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#910225}

--- components/media_router/browser/media_router_dialog_controller_unittest.cc
+++ components/media_router/browser/media_router_dialog_controller_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/media_router/browser/media_router_dialog_controller.h"
 
 #include <memory>
-#include <vector>
 
 #include "base/bind.h"
 #include "build/build_config.h"
