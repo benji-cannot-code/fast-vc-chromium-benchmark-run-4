127d60c8d4aca69152b46b2154812fb4dca7df2b
Dan McArdle <dmcardle@chromium.org>
2021-08-10 16:50:36 +0000

    [WTF::HashMap Refactor] Use DeprecatedAtOrEmptyValue() in /third_party/blink/renderer/platform/fonts
    This CL is a true behavior-preserving refactor. It will not affect
    performance because HashMap<>::DeprecatedAtOrEmptyValue() has an
    identical implementation to HashMap<>::at().
    
    In the near future, we will change the definition of HashMap<>::at() to
    CHECK that the map contains the given key.
    
    All occurrences of DeprecatedAtOrEmptyValue will need to be cleaned up.
    If the caller cannot know whether the map already contains the given
    key, one possible workaround is to use HashMap<>::find() instead.
    
    Design doc: https://docs.google.com/document/d/18JIiajErikZaBzCtZvl-wwAJShqZsg0jT9wVyPBhSdU/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=schenney@chromium.org
    
    Bug: 1058527
    Change-Id: I3ca32a19bb714092d8284007a7218e379ce6445c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082264
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910351}

--- third_party/blink/renderer/platform/fonts/glyph_metrics_map.h
+++ third_party/blink/renderer/platform/fonts/glyph_metrics_map.h
@@ -120,7 +120,7 @@ GlyphMetricsMap<T>::LocatePageSlowCase(unsigned page_number) {
     filled_primary_page_ = true;
   } else {
     if (pages_) {
-      page = pages_->at(page_number);
+      page = pages_->DeprecatedAtOrEmptyValue(page_number);
       if (page)
         return page;
     } else {
