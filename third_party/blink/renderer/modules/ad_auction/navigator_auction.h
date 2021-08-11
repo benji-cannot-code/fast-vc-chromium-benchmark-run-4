c3ba35a0ed7bc4ec522cab498a300bf119dd05ac
Caleb Raitto <caraitto@chromium.org>
2021-08-11 21:41:36 +0000

    Add FLEDGE update API.
    Bug: 1186444
    Change-Id: I9f83436644aa4a2b926e962a50c9dae3bf12ea6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2880738
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910998}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.h
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.h
@@ -48,6 +48,8 @@ class MODULES_EXPORT NavigatorAuction final
                                    Navigator&,
                                    const AuctionAdInterestGroup*,
                                    ExceptionState&);
+  void updateAdInterestGroups();
+  static void updateAdInterestGroups(ScriptState*, Navigator&);
   ScriptPromise runAdAuction(ScriptState*,
                              const AuctionAdConfig*,
                              ExceptionState&);
