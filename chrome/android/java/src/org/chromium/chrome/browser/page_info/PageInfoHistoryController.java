fe04795e91a911a91562e0adb6a19893ccf61e6f
Ehimare Okoyomon <eokoyomon@chromium.org>
2021-08-11 13:32:03 +0000

    [Android] Decrease page info history icon size
    Icon looked slightly bigger than the others. Use decreaseIconSize param.
    
    Screenshot: https://screenshot.googleplex.com/4hkfwX5EXMYUQ2H.png
    Bug: 1173154
    Change-Id: I5863dd841ea24cb8414705ffe9004894465fed16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088858
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910780}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoHistoryController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoHistoryController.java
@@ -106,6 +106,7 @@ public class PageInfoHistoryController
         rowParams.visible = rowParams.title != null && mDelegate.isSiteSettingsAvailable()
                 && !mDelegate.isIncognito();
         rowParams.iconResId = R.drawable.ic_history_googblue_24dp;
+        rowParams.decreaseIconSize = true;
         rowParams.clickCallback = this::launchSubpage;
 
         mRowView.setParams(rowParams);
