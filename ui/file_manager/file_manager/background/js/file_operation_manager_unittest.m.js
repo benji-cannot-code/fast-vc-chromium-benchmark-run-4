1c4234befe2f1292bd9ac1915d36ab54f0e9dcfc
François Degros <fdegros@chromium.org>
2021-08-11 07:24:17 +0000

    [Files App] Remove debug logs from ZipTask
    BUG=chromium:1207752, chromium:912236
    TEST=Manual tests, looking at the console logs
    
    Change-Id: Ic54a7a32c7479e0b919ebc3e74b1da7a062a4ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3085701
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910697}

--- ui/file_manager/file_manager/background/js/file_operation_manager_unittest.m.js
+++ ui/file_manager/file_manager/background/js/file_operation_manager_unittest.m.js
@@ -1142,7 +1142,6 @@ export function testZip(callback) {
 
   mockChrome.fileManagerPrivate.zipSelection =
       (sources, parent, newName, callback) => {
-        console.log(`zipSelection(${newName})`);
         const newPath = joinPath('/', newName);
         const newEntry = MockFileEntry.create(
             fileSystem, newPath, /** @type {!Metadata} */ ({size: destSize}));
@@ -1151,7 +1150,6 @@ export function testZip(callback) {
       };
 
   mockChrome.fileManagerPrivate.getZipProgress = (zipId, callback) => {
-    console.log(`getZipProgress(${zipId})`);
     assertEquals(wantZipId, zipId);
     const result = 0;  // Success
     callback(result, destSize);
