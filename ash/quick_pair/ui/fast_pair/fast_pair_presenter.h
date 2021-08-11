0b403a806d2863a637ef3cefc24c977cd6a2e0f7
Jon Mann <jonmann@chromium.org>
2021-08-11 17:49:14 +0000

    FastPair: Implement caching for DeviceMetadata.
    Change-Id: I30d4c9146696d1419327dd3e4fc2ab3387ecabfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3085448
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910901}

--- ash/quick_pair/ui/fast_pair/fast_pair_presenter.h
+++ ash/quick_pair/ui/fast_pair/fast_pair_presenter.h
@@ -13,17 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
-namespace nearby {
-namespace fastpair {
-class GetObservedDeviceResponse;
-}  // namespace fastpair
-}  // namespace nearby
-
 namespace ash {
 namespace quick_pair {
 
 struct Device;
-class FastPairImageDecoder;
+struct DeviceMetadata;
 
 using DiscoveryCallback = base::OnceCallback<void(DiscoveryAction)>;
 using PairingFailedCallback = base::OnceCallback<void(PairingFailedAction)>;
@@ -55,18 +49,10 @@ class FastPairPresenter {
   void OnPairingFailedDismissed(PairingFailedCallback callback,
                                 bool user_dismissed);
 
-  void ShowDiscoveryMetadataRetrieved(
-      scoped_refptr<Device> device,
-      DiscoveryCallback callback,
-      absl::optional<nearby::fastpair::GetObservedDeviceResponse>
-          device_metadata);
-  void ShowDiscoveryImpl(
-      scoped_refptr<Device> device,
-      DiscoveryCallback callback,
-      nearby::fastpair::GetObservedDeviceResponse device_metadata,
-      gfx::Image image);
+  void OnDiscoveryMetadataRetrieved(scoped_refptr<Device> device,
+                                    DiscoveryCallback callback,
+                                    DeviceMetadata* device_metadata);
 
-  std::unique_ptr<FastPairImageDecoder> image_decoder_;
   std::unique_ptr<FastPairNotificationController> notification_controller_;
   base::WeakPtrFactory<FastPairPresenter> weak_pointer_factory_{this};
 };
