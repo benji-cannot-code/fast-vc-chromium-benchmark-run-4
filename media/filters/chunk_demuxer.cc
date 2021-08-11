90b4c5e076bc5aeb409b7c0e08e9f009b20a1662
Will Cassella <cassew@google.com>
2021-08-11 06:46:44 +0000

    Avoid copies of base::RepeatingCallback in media::ChunkDemuxer
    media::ChunkDemuxer::SetParseWarningCallback simply forwards its
    argument along to SourceBufferState::SetParseWarningCallback, though it
    incurs a copy when doing so. A simple fix to this is to use std::move,
    which this CL does.
    
    A more complex case is media::ChunkDemuxer::SetTracksWatcher, which
    takes its argument by const& and only incurs a copy in
    SourceBufferState::SetTracksWatcher. Since base::RepeatingCallback is
    essentially just a scoped_refptr, it should follow the style guide
    recommendations for that type and pass by value when the callee is
    guaranteed to make a copy. Therefore this CL updates those functions as
    well.
    
    Additionally, this CL extends the comments on
    it clear that calling these functions is required before sending data to
    the ChunkDemuxer.
    
    media: :ChunkDemuxer::(SetParseWarningCallback|SetTracksWatcher) to make
    Change-Id: Id3cca2599c144d3d49c5d3c66f403ebff4196022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088154
    Commit-Queue: Will Cassella <cassew@google.com>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910640}

--- media/filters/chunk_demuxer.cc
+++ media/filters/chunk_demuxer.cc
@@ -752,12 +752,11 @@ ChunkDemuxer::Status ChunkDemuxer::AddIdInternal(
   return kOk;
 }
 
-void ChunkDemuxer::SetTracksWatcher(
-    const std::string& id,
-    const MediaTracksUpdatedCB& tracks_updated_cb) {
+void ChunkDemuxer::SetTracksWatcher(const std::string& id,
+                                    MediaTracksUpdatedCB tracks_updated_cb) {
   base::AutoLock auto_lock(lock_);
   CHECK(IsValidId(id));
-  source_state_map_[id]->SetTracksWatcher(tracks_updated_cb);
+  source_state_map_[id]->SetTracksWatcher(std::move(tracks_updated_cb));
 }
 
 void ChunkDemuxer::SetParseWarningCallback(
@@ -765,7 +764,7 @@ void ChunkDemuxer::SetParseWarningCallback(
     SourceBufferParseWarningCB parse_warning_cb) {
   base::AutoLock auto_lock(lock_);
   CHECK(IsValidId(id));
-  source_state_map_[id]->SetParseWarningCallback(parse_warning_cb);
+  source_state_map_[id]->SetParseWarningCallback(std::move(parse_warning_cb));
 }
 
 void ChunkDemuxer::RemoveId(const std::string& id) {
