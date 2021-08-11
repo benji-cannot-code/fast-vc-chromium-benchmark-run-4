651c4ca10045f98a5806135e7e41215139d8532a
Maksim Sisov <msisov@igalia.com>
2021-08-11 17:20:21 +0000

    ozone: do not access platform window extensions for null windows.
    X11 can crash as it tries to access a property of a not yet set
    platform_window.
    
    To be on a safe side, this CL also ensures platform_window exists
    when the Wayland extension is accessed.
    
    Bug: 1236692
    Change-Id: I50e54f8c44d8b2b527dec6d34c554c2e3f570c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086564
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#910887}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_linux.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_linux.cc
@@ -161,12 +161,14 @@ base::OnceClosure DesktopWindowTreeHostLinux::DisableEventListening() {
 }
 
 ui::WaylandExtension* DesktopWindowTreeHostLinux::GetWaylandExtension() {
-  return ui::GetWaylandExtension(*(platform_window()));
+  return platform_window() ? ui::GetWaylandExtension(*(platform_window()))
+                           : nullptr;
 }
 
 const ui::WaylandExtension* DesktopWindowTreeHostLinux::GetWaylandExtension()
     const {
-  return ui::GetWaylandExtension(*(platform_window()));
+  return platform_window() ? ui::GetWaylandExtension(*(platform_window()))
+                           : nullptr;
 }
 
 ui::DeskExtension* DesktopWindowTreeHostLinux::GetDeskExtension() {
@@ -300,11 +302,13 @@ void DesktopWindowTreeHostLinux::OnActivationChanged(bool active) {
 }
 
 ui::X11Extension* DesktopWindowTreeHostLinux::GetX11Extension() {
-  return ui::GetX11Extension(*(platform_window()));
+  return platform_window() ? ui::GetX11Extension(*(platform_window()))
+                           : nullptr;
 }
 
 const ui::X11Extension* DesktopWindowTreeHostLinux::GetX11Extension() const {
-  return ui::GetX11Extension(*(platform_window()));
+  return platform_window() ? ui::GetX11Extension(*(platform_window()))
+                           : nullptr;
 }
 
 #if BUILDFLAG(USE_ATK)
