83ef667ec04b024069eaced68edb3ceedf2a7736
Gabriel Szlechtman <gabrielsz@google.com>
2021-08-11 06:42:21 +0000

    [ash-chrome] Implement a basic HaTS to UMA bridge.
    We'd like to correlate the overall survey score with other UMA metrics.
    The way to achieve that is to have the HaTS client inform us of the
    chosen score, so we can store it as UMA metrics. Based on URL fragments,
    we are sending the host code once that selection happens.
    
    Besides the selection, we're now also informing the host of unhandled
    exceptions, which should help with bug fixing.
    
    Change-Id: I34df01d6f4ff88ba8f9693c5b0bde41a761268f3
    Bug: b/192278927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078893
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gabriel Szlechtman <gabrielsz@google.com>
    Cr-Commit-Position: refs/heads/master@{#910631}

--- chrome/browser/ash/hats/hats_dialog_unittest.cc
+++ chrome/browser/ash/hats/hats_dialog_unittest.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/test/metrics/histogram_tester.h"
+#include "hats_dialog.h"
 #include "net/base/url_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
@@ -63,4 +65,27 @@ TEST(HatsDialogTest, GetFormattedSiteContext) {
   EXPECT_NE(std::string::npos, context.find("psdValue2%20%3D%25%5E*%24%23%26"));
 }
 
+TEST(HatsDialogTest, HandleClientTriggeredAction) {
+  // Client asks to close the window
+  EXPECT_TRUE(HatsDialog::HandleClientTriggeredAction("close", "hist-name"));
+  // There was an unhandled error, close the window
+  EXPECT_TRUE(HatsDialog::HandleClientTriggeredAction(
+      "survey-loading-error-12345", "a-suffix"));
+  // Client sent an invalid action, ignore it
+  EXPECT_FALSE(HatsDialog::HandleClientTriggeredAction("Invalid", "hist-name"));
+
+  // Set up the histogram tester
+  base::HistogramTester histogram_tester;
+  std::string histogram("Browser.ChromeOS.HatsSatisfaction.General");
+  histogram_tester.ExpectTotalCount(histogram, 0);
+
+  EXPECT_FALSE(HatsDialog::HandleClientTriggeredAction("smiley-selected-4",
+                                                       "full-histogram-name"));
+
+  // Ensure we logged the right metric
+  // For the example above, it means adding 1 entry in the bucket for score=4
+  std::vector<base::Bucket> expected_buckets{{4, 1}};
+  EXPECT_EQ(histogram_tester.GetAllSamples("full-histogram-name"),
+            expected_buckets);
+}
 }  // namespace ash
