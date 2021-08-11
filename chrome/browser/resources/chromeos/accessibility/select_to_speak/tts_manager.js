c8d280c63464427437d3555e9ddd3ade37029029
Ajit Narayanan <ajitnarayanan@google.com>
2021-08-11 22:37:20 +0000

    Use preferred local voice as fallback in STS.
    In case the user has configured Select-to-speak to use network voices,
    and there's a problem with the network, fallback to the user's preferred
    local voice instead of the default TTS voice.
    
    Fixed: 1238030
    AX-Relnotes: n/a
    Change-Id: Ibb2e5ea4129a02561e8c156e25b7773ce5e6d9d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3083415
    Commit-Queue: Ajit Narayanan <ajitnarayanan@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911026}

--- chrome/browser/resources/chromeos/accessibility/select_to_speak/tts_manager.js
+++ chrome/browser/resources/chromeos/accessibility/select_to_speak/tts_manager.js
@@ -22,6 +22,12 @@ export class TtsManager {
     this.clientTtsOptions_ = /** @type {!chrome.tts.TtsOptions} */ ({});
 
     /**
+     * The fallback voice to use if TTS fails.
+     * @private {string|undefined}
+     */
+    this.fallbackVoice_ = undefined;
+
+    /**
      * The current char index to the |this.text_| indicating the current spoken
      * word. For example, if |this.text_| is "hello world" and TTS is speaking
      * the second word, the |this.currentCharIndex_| should be 6.
@@ -60,8 +66,10 @@ export class TtsManager {
    * @param {string} text The text to read.
    * @param {!chrome.tts.TtsOptions} ttsOptions The options for TTS.
    * @param {boolean} networkVoice Whether a network voice is specified for TTS.
+   * @param {string|undefined} fallbackVoice A voice to use if to retry if TTS
+   *     fails.
    */
-  speak(text, ttsOptions, networkVoice) {
+  speak(text, ttsOptions, networkVoice, fallbackVoice) {
     if (ttsOptions.enqueued) {
       console.warn('TtsManager does not support a queue of utterances.');
       return;
@@ -69,6 +77,7 @@ export class TtsManager {
     this.cleanTtsState_();
     this.text_ = text;
     this.isNetworkVoice_ = networkVoice;
+    this.fallbackVoice_ = fallbackVoice;
     this.startSpeakingTextWithOffset_(0, false /* resume */, ttsOptions);
   }
 
@@ -94,10 +103,10 @@ export class TtsManager {
             console.warn('Network TTS error, retrying with local voice');
             const localOptions = /** @type {!chrome.tts.TtsOptions} */ (
                 Object.assign({}, modifiedOptions));
-            // TODO(crbug.com/1238030): Use user's preferred local voice.
-            localOptions.voiceName = undefined;
+            localOptions.voiceName = this.fallbackVoice_;
             if (this.text_) {
-              this.speak(this.text_, localOptions, /*networkVoice=*/ false);
+              this.speak(
+                  this.text_, localOptions, /*networkVoice=*/ false, undefined);
             }
           }
           break;
