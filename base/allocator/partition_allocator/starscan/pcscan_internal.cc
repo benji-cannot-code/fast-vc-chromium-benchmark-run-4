5b3b59163f935e1b677282def3a9d32f11bbd27c
Anton Bikineev <bikineev@chromium.org>
2021-08-11 19:58:28 +0000

    *Scan: Fix new test failures
    This makes sure that PartitionAddressSpace is always reinitialized on
    PCScan::ReinitForTesting.
    
    Bug: 1129751
    Change-Id: I6267f292efc595dcfa7c39ed08548e89ca3a9687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088863
    Auto-Submit: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910939}

--- base/allocator/partition_allocator/starscan/pcscan_internal.cc
+++ base/allocator/partition_allocator/starscan/pcscan_internal.cc
@@ -194,9 +194,6 @@ SimdSupport DetectSimdSupport() {
 
 void CommitCardTable() {
 #if PA_STARSCAN_USE_CARD_TABLE
-  // First, make sure that GigaCage is initialized.
-  PartitionAddressSpace::Init();
-  // Then, commit the card table.
   RecommitSystemPages(
       reinterpret_cast<void*>(PartitionAddressSpace::BRPPoolBase()),
       sizeof(QuarantineCardTable), PageReadWrite, PageUpdatePermissions);
@@ -1090,6 +1087,10 @@ PCScanInternal::~PCScanInternal() = default;
 
 void PCScanInternal::Initialize(PCScan::WantedWriteProtectionMode wpmode) {
   PA_DCHECK(!is_initialized_);
+#if defined(PA_HAS_64_BITS_POINTERS)
+  // Make sure that GigaCage is initialized.
+  PartitionAddressSpace::Init();
+#endif
   CommitCardTable();
 #if defined(PA_STARSCAN_UFFD_WRITE_PROTECTOR_SUPPORTED)
   if (wpmode == PCScan::WantedWriteProtectionMode::kEnabled)
