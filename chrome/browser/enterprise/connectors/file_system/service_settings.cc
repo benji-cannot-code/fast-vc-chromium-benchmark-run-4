02a6262bc77641bbfd3df75e3f88f9eda456f4d9
Alice Gong <alicego@google.com>
2021-08-11 18:41:40 +0000

    Set FSC finch experiment flag to be enabled by default for TT launch!
    Fixed: 1237171
    Change-Id: I88e9e2148a055d739f046392b8eed3f687728058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3083581
    Auto-Submit: Alice Gong <alicego@google.com>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910913}

--- chrome/browser/enterprise/connectors/file_system/service_settings.cc
+++ chrome/browser/enterprise/connectors/file_system/service_settings.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise_connectors {
 
 const base::Feature kFileSystemConnectorEnabled{
-    "FileSystemConnectorsEnabled", base::FEATURE_DISABLED_BY_DEFAULT};
+    "FileSystemConnectorsEnabled", base::FEATURE_ENABLED_BY_DEFAULT};
 
 FileSystemServiceSettings::FileSystemServiceSettings(
     const base::Value& settings_value,
