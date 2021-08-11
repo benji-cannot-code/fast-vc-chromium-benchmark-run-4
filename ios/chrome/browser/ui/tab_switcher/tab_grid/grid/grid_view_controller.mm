45dc4328259f4e562823e844ad6e9ff04c6fb0ab
Mike Dougherty <michaeldo@chromium.org>
2021-08-11 22:11:10 +0000

    Cleanup GridCells in Tab Grid Edit Mode
    - Remove animation when selecting/deselecting an item
    - Provide an accessibility value for VoiceOver to read the current
      selection state
    
    Change-Id: I8468fd6f3d8c8283bd8b28b00d794a67ff8e8b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089626
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911012}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_controller.mm
@@ -1102,7 +1102,9 @@ NSIndexPath* CreateIndexPath(NSInteger index) {
     } else {
       self.collectionView.dragInteractionEnabled = YES;
     }
-    [self.collectionView reloadItemsAtIndexPaths:@[ indexPath ]];
+    [UIView performWithoutAnimation:^{
+      [self.collectionView reloadItemsAtIndexPaths:@[ indexPath ]];
+    }];
   }
 
   [self.delegate gridViewController:self didSelectItemWithID:itemID];
