5f3aeab664ffd1ed1db966cead6fcfa856d7f12f
Titouan Rigoudy <titouan@chromium.org>
2021-08-11 18:07:45 +0000

    [Private Network Access] Fix typo in feature name.
    Bug: chromium:1234044
    Change-Id: Ic0a1770a6ebfa19d98e8e736c7dc9bf31c912cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089340
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910904}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -133,7 +133,7 @@ const base::Feature kBlockInsecurePrivateNetworkRequests{
 //  - https://wicg.github.io/private-network-access/#integration-fetch
 //  - kBlockInsecurePrivateNetworkRequests
 const base::Feature kBlockInsecurePrivateNetworkRequestsFromPrivate{
-    "BlockInsecurePrivateNetworkRequestsFromPrivateFromPrivate",
+    "BlockInsecurePrivateNetworkRequestsFromPrivate",
     base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Enables use of the PrivateNetworkAccessNonSecureContextsAllowed deprecation
