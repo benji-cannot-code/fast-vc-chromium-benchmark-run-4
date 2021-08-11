40b2908022bd107e28d4b3cb8e93caf0ba742c28
Joanmarie Diggs <jdiggs@igalia.com>
2021-08-11 16:21:22 +0000

    Re-reland "Do not expose aria-errormessage when aria-invalid is false"
    This is a reland of commit ebd839c9b6e68ff542eba172e38886ecd59cb329,
    which was a reland of commit 5b6bfaae62d8c6a5065fe4e1ceff4ddfc9909042.
    
    It also adds the ability to have specific expectations files for
    Trusty and Xenial in the content browser tests.
    
    The reason the change was reverted the first time is that Trusty runs
    a much older version of ATK which does not support the ATK relation
    types used for aria-errormessage. Rather than disabling this test on
    Trusty, the first reland added the capability to have Trusty-specific
    expectations. This approach gives us the ability to test more things
    via content browser tests and use fewer workarounds.
    
    The reason the reland was reverted was because the same test failed on
    Xenial, which has a newer (but not quite new enough) version of ATK.
    Therefore, this reland expands the version-specific expectations to
    support Xenial.
    
    Original (first) change's description:
    > Do not expose aria-errormessage when aria-invalid is false
    >
    > ARIA 1.2 added the following requirement:
    >
    >   User agents MUST NOT expose aria-errormessage for an object with an
    >   aria-invalid value of false.
    >
    > We were exposing it unconditionally. This commit fixes that.
    >
    > AX-Relnotes: Implement ARIA 1.2 requirement to not expose
    > aria-errormessage when aria-invalid is false.
    >
    > Bug: 1233978
    >
    > Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    > Change-Id: I9cc8e654ea22919c105e331d88fd69d49990494c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062841
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#907136}
    
    Cq-Include-Trybots: luci.chromium.try:linux-trusty-rel,linux-xenial-rel,linux-bionic-rel
    Change-Id: Ie49308b839a839dbff3d073c6affd35d6b14b62a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3078002
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910843}

--- content/public/test/dump_accessibility_test_helper.cc
+++ content/public/test/dump_accessibility_test_helper.cc
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "base/win/windows_version.h"
 #endif
+#if BUILDFLAG(USE_ATK)
+extern "C" {
+#include <atk/atk.h>
+}
+#endif
 
 namespace content {
 
@@ -331,6 +336,27 @@ DumpAccessibilityTestHelper::GetVersionSpecificExpectedFileSuffix(
     if (!expectations_qualifier.empty())
       suffix = FILE_PATH_LITERAL("-") + expectations_qualifier;
     return suffix + FILE_PATH_LITERAL("-expected-uia-win7.txt");
+  }
+#endif
+#if BUILDFLAG(USE_ATK)
+  if (expectation_type_ == "linux") {
+    FilePath::StringType version_name;
+    switch (atk_get_minor_version()) {
+      case 10:
+        version_name = FILE_PATH_LITERAL("trusty");
+        break;
+      case 18:
+        version_name = FILE_PATH_LITERAL("xenial");
+        break;
+      default:
+        return FILE_PATH_LITERAL("");
+    }
+
+    FilePath::StringType suffix;
+    if (!expectations_qualifier.empty())
+      suffix = FILE_PATH_LITERAL("-") + expectations_qualifier;
+    return suffix + FILE_PATH_LITERAL("-expected-auralinux-") + version_name +
+           FILE_PATH_LITERAL(".txt");
   }
 #endif
   return FILE_PATH_LITERAL("");
--- third_party/blink/web_tests/http/tests/inspector-protocol/accessibility/accessibility-aria-1.1.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/accessibility/accessibility-aria-1.1.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   var {page, session, dp} = await testRunner.startHTML(`
-    <input data-dump aria-errormessage='err'>
+    <input data-dump aria-errormessage='err' aria-invalid='true'>
     <h3 id='err'>This text field has an error!</h3>
 
     <img data-dump aria-details='d' aria-label='Label'>
