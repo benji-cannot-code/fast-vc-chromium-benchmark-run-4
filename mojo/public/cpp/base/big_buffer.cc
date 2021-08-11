65afea73e5171777eb3be60ca655101ae2a75412
Mark Brand <markbrand@google.com>
2021-08-11 09:51:20 +0000

    Fixing default constructor for BigBufferSharedMemoryRegion.
    The default constructor for BigBufferSharedMemoryRegion doesn't
    initialize the size_ member; this can result in a use of an
    uninitialized value.
    
    Bug: 1221407
    Change-Id: I92b05b925f1d18c0ae51ccd6cc38dc2ccc4d04ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3074200
    Commit-Queue: Mark Brand <markbrand@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#910734}

--- mojo/public/cpp/base/big_buffer.cc
+++ mojo/public/cpp/base/big_buffer.cc
@@ -12,7 +12,7 @@ namespace mojo_base {
 
 namespace internal {
 
-BigBufferSharedMemoryRegion::BigBufferSharedMemoryRegion() = default;
+BigBufferSharedMemoryRegion::BigBufferSharedMemoryRegion() : size_(0) {}
 
 BigBufferSharedMemoryRegion::BigBufferSharedMemoryRegion(
     mojo::ScopedSharedBufferHandle buffer_handle,
