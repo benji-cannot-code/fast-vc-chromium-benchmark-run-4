818d09af629a804efc347fecdd3f2ddfcd580d83
Jinsuk Kim <jinsukkim@chromium.org>
2021-08-11 00:14:03 +0000

    multi-instance: Add new-window command to the picker UI
    A new entry in window picker UI that lets users create a new
    instance. It is disabled when the number of instances reaches
    the maximum.
    
    Bug: 1175953
    Change-Id: Ib41716fcd5dc517ad5e63a95ac9eadb405797273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081321
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910588}

--- chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherItemProperties.java
+++ chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherItemProperties.java
@@ -19,6 +19,9 @@ public class InstanceSwitcherItemProperties {
     public static final PropertyModel.WritableBooleanPropertyKey CURRENT =
             new PropertyModel.WritableBooleanPropertyKey();
 
+    public static final PropertyModel.WritableBooleanPropertyKey ENABLE_COMMAND =
+            new PropertyModel.WritableBooleanPropertyKey();
+
     public static final PropertyModel.WritableObjectPropertyKey<Drawable> FAVICON =
             new PropertyModel.WritableObjectPropertyKey<>();
 
@@ -39,5 +42,5 @@ public class InstanceSwitcherItemProperties {
             new PropertyModel.WritableObjectPropertyKey<>();
 
     public static final PropertyKey[] ALL_KEYS = new PropertyKey[] {
-            CURRENT, FAVICON, TITLE, DESC, INSTANCE_ID, CLICK_LISTENER, MORE_MENU};
+            CURRENT, ENABLE_COMMAND, FAVICON, TITLE, DESC, INSTANCE_ID, CLICK_LISTENER, MORE_MENU};
 }
