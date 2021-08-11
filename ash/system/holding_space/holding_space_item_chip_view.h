8af70fa1dc73e415e7be1d2e55791c68a702e81f
David Black <dmblack@google.com>
2021-08-11 17:36:20 +0000

    Wait until after progress ring pulse before scaling in thumbnails.
    The thumbnail in holding space item chip views is scaled out while in
    progress. Currently, the scale back in is not animated. With this CL,
    the scale in is animated and timed after the pulse animation has
    finished.
    
    Bug: 1184438
    Change-Id: I94bda39ba2648d79de88e24ff29e049d7ab5146f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088394
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910894}

--- ash/system/holding_space/holding_space_item_chip_view.h
+++ ash/system/holding_space/holding_space_item_chip_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/holding_space/holding_space_image.h"
+#include "ash/system/holding_space/holding_space_animation_registry.h"
 #include "ash/system/holding_space/holding_space_item_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/metadata/view_factory.h"
@@ -68,7 +69,9 @@ class ASH_EXPORT HoldingSpaceItemChipView : public HoldingSpaceItemView {
   views::ImageButton* secondary_action_pause_ = nullptr;
   views::ImageButton* secondary_action_resume_ = nullptr;
 
-  base::CallbackListSubscription image_subscription_;
+  base::CallbackListSubscription image_skia_changed_subscription_;
+  HoldingSpaceAnimationRegistry::ProgressRingAnimationChangedCallbackList::
+      Subscription progress_ring_animation_changed_subscription_;
 };
 
 BEGIN_VIEW_BUILDER(/* no export */,
--- ash/system/holding_space/holding_space_progress_ring_animation.cc
+++ ash/system/holding_space/holding_space_progress_ring_animation.cc
@@ -18,9 +18,8 @@ HoldingSpaceProgressRingAnimation::HoldingSpaceProgressRingAnimation(
     bool is_cyclic)
     : type_(type), duration_(duration), is_cyclic_(is_cyclic) {}
 
-HoldingSpaceProgressRingAnimation::~HoldingSpaceProgressRingAnimation() {
-  animation_updated_callback_list_.Notify();
-}
+HoldingSpaceProgressRingAnimation::~HoldingSpaceProgressRingAnimation() =
+    default;
 
 // static
 std::unique_ptr<HoldingSpaceProgressRingAnimation>
@@ -43,6 +42,10 @@ void HoldingSpaceProgressRingAnimation::Start() {
   StartInternal(/*is_cyclic_restart=*/false);
 }
 
+bool HoldingSpaceProgressRingAnimation::IsAnimating() const {
+  return animator_ && animator_->is_animating();
+}
+
 void HoldingSpaceProgressRingAnimation::AnimationProgressed(
     const gfx::Animation* animation) {
   UpdateAnimatableProperties(animation->GetCurrentValue(), &start_position_,
@@ -52,8 +55,10 @@ void HoldingSpaceProgressRingAnimation::AnimationProgressed(
 
 void HoldingSpaceProgressRingAnimation::AnimationEnded(
     const gfx::Animation* animation) {
-  if (!is_cyclic_)
+  if (!is_cyclic_) {
+    animation_updated_callback_list_.Notify();
     return;
+  }
 
   // In tests, animations may be scaled such that duration is zero. When this
   // happens, we need to post cyclic restarts rather than restarting them
