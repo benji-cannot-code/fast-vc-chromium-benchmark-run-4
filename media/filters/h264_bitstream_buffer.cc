51ce1324839a373d6ac07fd5c4cacd5ac704569b
Zhaoliang Ma <zhaoliang.ma@intel.com>
2021-08-11 08:15:02 +0000

    media/gpu/vaapi/H264VaapiVED: Add packed slice header
    User mode driver won't be able to generate all of the slice header
    in some case, e.g. if we change the reference structure for temporal
    SVC purpose. So that we need to sending the correct packed slice
    header to driver in encoding.
    
    Since slice header is not a complete NALU, slice data and rbsp trailing
    is needed with slice header to combine a NALU. so this CL also add new
    api for flushing the cache bits in H264BitstreamBuffer like FinishNALU.
    Meanwhile, due to the slice header maybe not byte aligned, so to get
    the actual bit length for slice header, new api BitsInbuffer is added
    to get the correct bit length.
    
    Bug: b:188854977
    Test: video_encode_accelerator_tests --codec=h264 --output_bitstream
    Change-Id: I24e4fffabc3c435a8bcb86eeff183b9269e68247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069949
    Commit-Queue: Zhaoliang Ma <zhaoliang.ma@intel.com>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910716}

--- media/filters/h264_bitstream_buffer.cc
+++ media/filters/h264_bitstream_buffer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/filters/h264_bitstream_buffer.h"
 
+#include "base/bits.h"
 #include "base/sys_byteorder.h"
 
 namespace media {
@@ -24,6 +25,7 @@ void H264BitstreamBuffer::Reset() {
 
   capacity_ = 0;
   pos_ = 0;
+  bits_in_buffer_ = 0;
   reg_ = 0;
 
   Grow();
@@ -44,7 +46,7 @@ void H264BitstreamBuffer::FlushReg() {
   if (bits_in_reg == 0)
     return;
 
-  size_t bytes_in_reg = (bits_in_reg + 7) / 8;
+  size_t bytes_in_reg = base::bits::AlignUp(bits_in_reg, 8) / 8;
   reg_ <<= (kRegBitSize - bits_in_reg);
 
   // Convert to MSB and append as such to the stream.
@@ -55,6 +57,7 @@ void H264BitstreamBuffer::FlushReg() {
     Grow();
 
   memcpy(data_ + pos_, &reg_, bytes_in_reg);
+  bits_in_buffer_ = pos_ * 8 + bits_in_reg;
   pos_ += bytes_in_reg;
 
   reg_ = 0;
@@ -134,8 +137,16 @@ void H264BitstreamBuffer::FinishNALU() {
   // Byte-alignment zero bits.
   AppendBits(bits_left_in_reg_ % 8, 0);
 
+  Flush();
+}
+
+void H264BitstreamBuffer::Flush() {
   if (bits_left_in_reg_ != kRegBitSize)
     FlushReg();
+}
+
+size_t H264BitstreamBuffer::BitsInBuffer() const {
+  return bits_in_buffer_;
 }
 
 size_t H264BitstreamBuffer::BytesInBuffer() const {
