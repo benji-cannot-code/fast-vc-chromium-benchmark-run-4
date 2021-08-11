ed0733dbcaabedaeb9b8176a9d09c0633edb7711
Chad Duffin <chadduffin@chromium.org>
2021-08-11 16:00:09 +0000

    [CrOS Bluetooth] Fix return conditions to not disable Bluetooth.
    This change updates the return and disable conditions to prevent the
    <cr-toggle> that allows turning Bluetooth on and off to not propagate
    its default |false|, causing Bluetooth to be turned off on every fresh
    load or full page reload.
    
    Bug: 1010321
    Test: Locally verified and executed unit tests.
    Change-Id: If765be206e3f595d59c1bba4188c2a1b1269b5e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086728
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910830}

--- chrome/test/data/webui/settings/chromeos/os_bluetooth_devices_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_bluetooth_devices_subpage_tests.js
@@ -34,6 +34,9 @@ suite('OsBluetoothDevicesSubpageTest', function() {
 
     bluetoothConfig = new FakeBluetoothConfig();
     setBluetoothConfigForTesting(bluetoothConfig);
+  });
+
+  function init() {
     bluetoothDevicesSubpage =
         document.createElement('os-settings-bluetooth-devices-subpage');
     document.body.appendChild(bluetoothDevicesSubpage);
@@ -50,7 +53,7 @@ suite('OsBluetoothDevicesSubpageTest', function() {
       }
     };
     bluetoothConfig.observeSystemProperties(propertiesObserver);
-  });
+  }
 
   function flushAsync() {
     Polymer.dom.flush();
@@ -58,10 +61,21 @@ suite('OsBluetoothDevicesSubpageTest', function() {
   }
 
   test('Base Test', function() {
+    init();
     assertTrue(!!bluetoothDevicesSubpage);
   });
 
+  test('Toggle button creation', async function() {
+    bluetoothConfig.setSystemState(
+        chromeos.bluetoothConfig.mojom.BluetoothSystemState.kEnabled);
+    await flushAsync();
+    init();
+    assertTrue(bluetoothDevicesSubpage.$.enableBluetoothToggle.checked);
+  });
+
   test('Toggle button states', async function() {
+    init();
+
     const enableBluetoothToggle =
         bluetoothDevicesSubpage.$.enableBluetoothToggle;
     assertTrue(!!enableBluetoothToggle);
@@ -109,6 +123,8 @@ suite('OsBluetoothDevicesSubpageTest', function() {
   });
 
   test('Device lists states', async function() {
+    init();
+
     const getDeviceList = (connected) => {
       return bluetoothDevicesSubpage.shadowRoot.querySelector(
           connected ? '#connectedDeviceList' : '#unconnectedDeviceList');
