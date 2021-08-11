36f6d0a2896274c15a02aed65360160a5ae78668
Robert Lin <robertlin@chromium.org>
2021-08-11 13:34:06 +0000

    prerender: defer cross-origin subframe navigations
    Currently, the codebase only compares the origin of a URL at
    WillStartOrRedirectRequest.
    This is inaccurate since the Origin isn't computed, as it
    will fail in the case of iframe with sandbox attributes.
    It is desired to have additional checking at WillProcessResponse.
    At the momemnt, the Origin from GetOriginToCommit is accurate.
    The related test will be added or in a follow-up CL depending
    on the schedule (due to up-coming M94)
    
    Bug: 1229027
    Change-Id: I54b0b3c9e2c73180ba6c3295dae4f5ce3bb0e6e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077117
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#910781}

--- content/browser/prerender/prerender_subframe_navigation_throttle.h
+++ content/browser/prerender/prerender_subframe_navigation_throttle.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/scoped_observation.h"
 #include "content/browser/prerender/prerender_host.h"
+#include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/public/browser/navigation_throttle.h"
 
 namespace content {
@@ -41,6 +42,7 @@ class PrerenderSubframeNavigationThrottle : public NavigationThrottle,
   void DidFinishNavigation(NavigationHandle* nav_handle) override;
 
   ThrottleCheckResult WillStartOrRedirectRequest();
+  void DeferCrossOriginSubframeNavigation(FrameTreeNode* frame_tree_node);
 
   bool is_deferred_ = false;
   const int prerender_root_ftn_id_;
