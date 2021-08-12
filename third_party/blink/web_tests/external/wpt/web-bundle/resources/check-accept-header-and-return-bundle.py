bac76cc671524b392de5110855fcaf9ea4f243be
Kunihiko Sakamoto <ksakamoto@chromium.org>
2021-08-12 04:49:16 +0000

    Set Accept request header for Subresource WebBundle requests
    We are going to make some breaking changes to the WebBundle format. In
    preparation for that, this sets "Accept: application/webbundle;v=b1"
    header to requests for <link rel="webbundle"> to tell the server side
    which version of WebBundle the browser supports.
    
    Bug: 1238641
    Change-Id: Ibf5b89fe17324fab5419ac3ae3e14c8095e4f878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088718
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911149}

---
+++ third_party/blink/web_tests/external/wpt/web-bundle/resources/check-accept-header-and-return-bundle.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import os
+
+# Returns the content of ./wbn/subresource.wbn if the request has an "Accept"
+# header including "application/webbundle;v=b1" type, otherwise returns an
+# empty body with status code 400.
+
+def main(request, response):
+    headers = [
+        (b"Content-Type", b"application/webbundle"),
+        (b"X-Content-Type-Options", b"nosniff"),
+    ]
+
+    accept_values = request.headers.get(b"accept", b"").split(b",")
+    if b"application/webbundle;v=b1" in accept_values:
+        with open(
+            os.path.join(os.path.dirname(__file__), "./wbn/subresource.wbn"),
+            "rb",
+        ) as f:
+            return (200, headers, f.read())
+    else:
+        return (400, [], "")
