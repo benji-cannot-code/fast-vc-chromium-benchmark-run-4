6298af839c9e680c70ba2dde9f42f4bbc186bdcd
Jimmy Gong <jimmyxgong@google.com>
2021-08-12 23:58:35 +0000

    shortcuts: Update AcceleratorEditDialog to use ShortcutTypes
    Bug: 1060690
    Change-Id: I16309a36cc60d0e7e1ef7e6e50407712cc9a5d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3087954
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911570}

--- chrome/test/data/webui/chromeos/shortcut_customization/shortcut_customization_test.js
+++ chrome/test/data/webui/chromeos/shortcut_customization/shortcut_customization_test.js
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {getShortcutProvider, setShortcutProviderForTesting} from 'chrome://shortcut-customization/mojo_interface_provider.js';
 import {ShortcutCustomizationAppElement} from 'chrome://shortcut-customization/shortcut_customization_app.js';
-import {ShortcutProviderInterface} from 'chrome://shortcut-customization/shortcut_types.js';
+import {AcceleratorInfo, Modifier, ShortcutProviderInterface} from 'chrome://shortcut-customization/shortcut_types.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 import {flushTasks} from '../../test_util.m.js';
+
+import {CreateDefaultAccelerator} from './shortcut_customization_test_util.js';
 
 export function shortcutCustomizationAppTest() {
   /** @type {?ShortcutCustomizationAppElement} */
@@ -33,18 +35,25 @@ export function shortcutCustomizationAppTest() {
 
   test('DialogOpensOnEvent', async () => {
     await flushTasks();
+
     // The edit dialog should not be stamped and visible.
     let editDialog = page.shadowRoot.querySelector('#editDialog');
     assertFalse(!!editDialog);
 
     const nav = page.shadowRoot.querySelector('navigation-view-panel');
 
+    /** @type {!AcceleratorInfo} */
+    const acceleratorInfo = CreateDefaultAccelerator(
+        Modifier.SHIFT,
+        /*key=*/ 67,
+        /*key_display=*/ 'c');
+
     // Simulate the trigger event to display the dialog.
     nav.dispatchEvent(new CustomEvent('show-edit-dialog', {
       bubbles: true,
       composed: true,
       detail: /**@type {!Object}*/ (
-          {description: 'test', accelerators: [{modifiers: 1 << 1, key: 'c'}]})
+          {description: 'test', accelerators: [acceleratorInfo]})
     }));
     await flushTasks();
 
