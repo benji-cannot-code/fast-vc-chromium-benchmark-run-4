b5860629570bf6196a3e2355e72c08eab6eb3c61
John Lee <johntlee@chromium.org>
2021-08-12 21:03:39 +0000

    (Reland) Side panel: Add a bit of performance optimization
    - Don't render rows until they are needed using 'content-visibility'
    - Create rows 20 at a time so that the tree can become available as
    soon as possible without blocking rendering.
    - Update some tests to use textContent instead of innerText to
    avoid 'content-visibility' delaying innerText.
    
    Bug: 1199527, 1239120, 1239078
    Change-Id: I7e22dec0540ff4cb78c7fabc8ca26d9ef39d40a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3091925
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911483}

--- chrome/test/data/webui/read_later/side_panel/bookmark_folder_test.js
+++ chrome/test/data/webui/read_later/side_panel/bookmark_folder_test.js
@@ -71,10 +71,7 @@ suite('SidePanelBookmarkFolderTest', () => {
   test('UpdatesDepthVariables', () => {
     bookmarkFolder.depth = 3;
     assertEquals('3', bookmarkFolder.style.getPropertyValue('--node-depth'));
-    assertEquals(
-        '4',
-        bookmarkFolder.shadowRoot.querySelector('#children')
-            .style.getPropertyValue('--node-depth'));
+    assertEquals('4', bookmarkFolder.style.getPropertyValue('--child-depth'));
   });
 
   test('RendersChildren', () => {
@@ -86,10 +83,10 @@ suite('SidePanelBookmarkFolderTest', () => {
 
     assertEquals(
         folder.children[1].title,
-        childElements[1].querySelector('.title').innerText);
+        childElements[1].querySelector('.title').textContent);
     assertEquals(
         folder.children[2].title,
-        childElements[2].querySelector('.title').innerText);
+        childElements[2].querySelector('.title').textContent);
   });
 
   test('ShowsFaviconForBookmarks', () => {
--- chrome/test/data/webui/read_later/side_panel/bookmarks_list_test.js
+++ chrome/test/data/webui/read_later/side_panel/bookmarks_list_test.js
@@ -112,7 +112,7 @@ suite('SidePanelBookmarksListTest', () => {
 
     const folderElement = getFolderElements(bookmarksList)[rootFolderIndex];
     const bookmarkElement = getBookmarkElements(folderElement)[bookmarkIndex];
-    assertEquals('New title', bookmarkElement.innerText);
+    assertEquals('New title', bookmarkElement.textContent);
     assertEquals('http://new/url', bookmarkElement.href);
   });
 
@@ -163,7 +163,7 @@ suite('SidePanelBookmarksListTest', () => {
 
     const bookmarksBarFolder = getFolderElements(bookmarksList)[0];
     const movedBookmarkElement = getBookmarkElements(bookmarksBarFolder)[0];
-    assertEquals('Nested bookmark', movedBookmarkElement.innerText);
+    assertEquals('Nested bookmark', movedBookmarkElement.textContent);
 
     const childFolder = getFolderElements(bookmarksBarFolder)[0];
     const childFolderBookmarks = getBookmarkElements(childFolder);
