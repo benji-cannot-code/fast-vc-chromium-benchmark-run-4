9039f1f3a7f3d1aa104f3323ef66ba9c648ac651
K. Moon <kmoon@chromium.org>
2021-08-12 23:47:04 +0000

    [unseasoned-pdf] Patch GetFontData() in renderers
    Patches the GetFontData() call in all renderer processes to fix a crash
    on Windows when trying to load the unseasoned PDF viewer. GetFontData()
    was patched in PPAPI processes, but the unseasoned PDF viewer runs
    inside a renderer process now.
    
    Also renames MaybeInitializeGDI() to something less misleading.
    
    Unfortunately, it's not possible to replicate this issue in a browser
    test at this time; see crbug.com/1238982 for details.
    
    Fixed: 1238982
    Change-Id: Ifd5ad8c1d7820edd4f50807959029e25e10a7594
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3091845
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911565}

--- chrome/child/pdf_child_init.h
+++ chrome/child/pdf_child_init.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "PDF must be enabled"
 #endif
 
-// Initializes child-process specific code for the PDF module.
-void MaybeInitializeGDI();
+// Possibly patches GDI's `GetFontData()` for use by PDFium.
+void MaybePatchGdiGetFontData();
 
 #endif  // CHROME_CHILD_PDF_CHILD_INIT_H_
