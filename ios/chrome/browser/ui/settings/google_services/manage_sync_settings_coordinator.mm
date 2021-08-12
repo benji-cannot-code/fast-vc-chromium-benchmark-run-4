57eb0bd6b30275a0c3f26ad10580e28881f958e2
jlebel <jlebel@chromium.org>
2021-08-12 09:08:33 +0000

    Reland "[iOS] Removing ChromeIdentityBrowserOpener protocol"
    This is a reland of df892dc3997fd2deedf0a01b4c9a089e458043e7
    
    This patch needs to land after crrev.com/i/4047385
    
    Original change's description:
    > [iOS] Removing ChromeIdentityBrowserOpener protocol
    >
    > ChromeIdentityBrowserOpener is not used anymore.
    >
    > Change-Id: I07e3f28ca24db2c21c595814a3972cd6bbcc7407
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089116
    > Commit-Queue: Jérôme <jlebel@chromium.org>
    > Commit-Queue: Javier Flores <javierrobles@chromium.org>
    > Auto-Submit: Jérôme <jlebel@chromium.org>
    > Reviewed-by: Javier Flores <javierrobles@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#910808}
    
    Change-Id: If7b2453284e97cefc58bff7039ad3af30d12b7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086454
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911212}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/sync/sync_encryption_table_view_controller.h"
 #import "ios/chrome/browser/ui/table_view/table_view_utils.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
-#import "ios/public/provider/chrome/browser/signin/chrome_identity_browser_opener.h"
 #include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #import "net/base/mac/url_conversions.h"
 
@@ -51,7 +50,6 @@ using signin_metrics::AccessPoint;
 using signin_metrics::PromoAction;
 
 @interface ManageSyncSettingsCoordinator () <
-    ChromeIdentityBrowserOpener,
     ManageSyncSettingsCommandHandler,
     SyncErrorSettingsCommandHandler,
     ManageSyncSettingsTableViewControllerPresentationDelegate,
@@ -202,18 +200,6 @@ using signin_metrics::PromoAction;
     (ManageSyncSettingsTableViewController*)controller {
   DCHECK_EQ(self.viewController, controller);
   [self.delegate manageSyncSettingsCoordinatorWasRemoved:self];
-}
-
-#pragma mark - ChromeIdentityBrowserOpener
-
-- (void)openURL:(NSURL*)url
-              view:(UIView*)view
-    viewController:(UIViewController*)viewController {
-  OpenNewTabCommand* command =
-      [OpenNewTabCommand commandWithURLFromChrome:net::GURLWithNSURL(url)];
-  id<ApplicationCommands> handler = HandlerForProtocol(
-      self.browser->GetCommandDispatcher(), ApplicationCommands);
-  [handler closeSettingsUIAndOpenURL:command];
 }
 
 #pragma mark - ManageSyncSettingsCommandHandler
