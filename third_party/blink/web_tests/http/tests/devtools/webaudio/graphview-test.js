e87d797ea785b73f329a4daee6c700366fafec99
Brian Ho <hob@chromium.org>
2021-08-12 01:14:12 +0000

    Fix-forward underscore migration in blink_web_tests
    Bug: 1238850
    Change-Id: I170016a8232b6b2af2cf7a93b04c36a33b468f88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088542
    Commit-Queue: Brian Ho <hob@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Owners-Override: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/master@{#911072}

--- third_party/blink/web_tests/http/tests/devtools/webaudio/graphview-test.js
+++ third_party/blink/web_tests/http/tests/devtools/webaudio/graphview-test.js
@@ -102,8 +102,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   function dumpInOutBoundingMapSize() {
-    TestRunner.addResult(`Number of nodes with out-bound edges: ${graph._outboundEdgeMap.size}`);
-    TestRunner.addResult(`Number of nodes with in-bound edges: ${graph._inboundEdgeMap.size}`);
+    // TODO(crbug.com/1238850): Remove the _outboundEdgeMap branch.
+    if (graph.hasOwnProperty('_outboundEdgeMap')) {
+      TestRunner.addResult(`Number of nodes with out-bound edges: ${graph._outboundEdgeMap.size}`);
+      TestRunner.addResult(`Number of nodes with in-bound edges: ${graph._inboundEdgeMap.size}`);
+    } else {
+      TestRunner.addResult(`Number of nodes with out-bound edges: ${graph.outboundEdgeMap.size}`);
+      TestRunner.addResult(`Number of nodes with in-bound edges: ${graph.inboundEdgeMap.size}`);
+    }
   }
 
 })();
