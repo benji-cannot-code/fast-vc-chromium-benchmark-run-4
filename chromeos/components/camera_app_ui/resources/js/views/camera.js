1ab3e03495f5d628ca18483322cfde11f3aee8ff
Kuo Jen Wei <inker@google.com>
2021-08-12 17:21:30 +0000

    CCA: Check camera view is still on the top shown view after await in focus
    The |View.focus()| will be called immediately after entering a view.
    While the camera view's |focus()| may await some async event then
    focus() onto UI under the view. In case like, entering camera
    view(|focus()| called in meanwhile but blocked on await) and leave the
    camera view or show another view on top immediately, the camera view may
    mis-focus onto its UI after await returned. This CL check if the camera
    view is still on the top view after await returned and don't focus onto
    its UI if the view is gone or it's not top view.
    
    Bug: b:195905083
    Test: Manually
    Change-Id: I9916590bcd59f0e164b87d7c1faeaa8b973f3532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089011
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911367}

--- chromeos/components/camera_app_ui/resources/js/views/camera.js
+++ chromeos/components/camera_app_ui/resources/js/views/camera.js
@@ -600,6 +600,11 @@ export class Camera extends View {
         return;
       }
 
+      // Check the view is still on the top after await.
+      if (!nav.isTopMostView(ViewName.CAMERA)) {
+        return;
+      }
+
       if (newFeatureToast.isShowing()) {
         newFeatureToast.focus();
         return;
