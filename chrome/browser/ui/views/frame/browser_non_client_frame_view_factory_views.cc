43f3843048be2b3d58c3309ee6234a583e67b67d
Hassan Talat <hatalat@microsoft.com>
2021-08-12 01:17:06 +0000

    dpwas: Do not use gtk+ theme for WindowControlsOverlay PWAs
    The frame is not updated with the PWAs theme color when gtk+ theme is
    enabled on linux (which is intended). But it does not look good with
    WCO PWAs (screenshot in the crbug). To a provide a consistent
    seamless experience for WCO PWAs this CL makes an exception
    for PWAs with WCO display override and forces to use
    "Aura" NativeTheme instead.
    
    Screenshots:
    Before:
    https://drive.google.com/file/d/1Za3TfwhCAO6flWTmJ4jBIxJ1fHcd7HWk/view
    
    After
    PWA with WCO: https://drive.google.com/file/d/1ZtyoPKLIuEqF4BPhXtB5ONi6jsfUPC3I/view
    Standalone mode in a WCO PWA:
    https://drive.google.com/file/d/1bkcZPoGHs7psoG68THFA-kQQJY1kU-gW/view
    
    Bug: 1219073
    Change-Id: Ie4255a20550bb77258ff13e688a15295429fa3c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069787
    Commit-Queue: Hassan Talat <hatalat@microsoft.com>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911077}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_views.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_factory_views.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_frame_view_layout_linux_native.h"
 #include "chrome/browser/ui/views/frame/browser_frame_view_linux.h"
 #include "chrome/browser/ui/views/frame/browser_frame_view_linux_native.h"
+#include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "ui/views/linux_ui/linux_ui.h"
 #include "ui/views/linux_ui/nav_button_provider.h"
 #endif
@@ -36,7 +37,13 @@ std::unique_ptr<OpaqueBrowserFrameView> CreateOpaqueBrowserFrameView(
   auto* linux_ui = views::LinuxUI::instance();
   auto* profile = browser_view->browser()->profile();
   auto* theme_service_factory = ThemeServiceFactory::GetForProfile(profile);
-  if (linux_ui && theme_service_factory->UsingSystemTheme()) {
+  auto* app_controller = browser_view->browser()->app_controller();
+  // Ignore GTK+ for web apps with window-controls-overlay as the
+  // display_override so the web contents can blend with the overlay by using
+  // the developer-provided theme color for a better experience. Context:
+  // https://crbug.com/1219073.
+  if (linux_ui && theme_service_factory->UsingSystemTheme() &&
+      !(app_controller && app_controller->AppUsesWindowControlsOverlay())) {
     auto nav_button_provider = linux_ui->CreateNavButtonProvider();
     if (nav_button_provider) {
       return std::make_unique<BrowserFrameViewLinuxNative>(
