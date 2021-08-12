fc9f6183e478b55c6e2e3dfb81be4755c3a5f65d
Jinsuk Kim <jinsukkim@chromium.org>
2021-08-12 01:12:44 +0000

    multi-instance: Detect visible tasks
    In order to display the right description for instances running in
    floating popup/adjacent window in split screen, this CL detects the
    visible state of tasks running Chrome activities.
    
    Bug: 1175953
    Change-Id: I885b7b1e60c5dab51f5014adb6197f5433f0d8e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077934
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911069}

--- chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31UnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31UnitTest.java
@@ -15,6 +15,7 @@ import android.app.Activity;
 import android.text.TextUtils;
 import android.util.Pair;
 import android.util.SparseArray;
+import android.util.SparseBooleanArray;
 
 import androidx.test.filters.SmallTest;
 
@@ -189,7 +190,8 @@ public class MultiInstanceManagerApi31UnitTest {
         }
 
         @Override
-        protected boolean isRunningInAdjacentWindow(Activity activity) {
+        protected boolean isRunningInAdjacentWindow(
+                SparseBooleanArray visibleTasks, Activity activity) {
             return activity == mAdjacentInstance;
         }
 
