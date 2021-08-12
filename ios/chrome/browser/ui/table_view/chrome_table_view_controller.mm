184128c47170c1fee324ed9d9b1b126eeee1c86f
Ewann <ewannpv@google.com>
2021-08-12 13:37:45 +0000

    [iOS] Fixes separator inset
    This CL fixes the cell separator inset.
    screenshot: https://drive.google.com/file/d/1YZuZRekoP7NeyCfa7pTWKywrieBW_CCa/view?usp=sharing&resourcekey=0-v62Vi4aPekF980lA0vSkag
    
    Bug: 1234846
    Change-Id: I202fd1053726353635589cf96cd8d95770a0bfa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3090597
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911288}

--- ios/chrome/browser/ui/table_view/chrome_table_view_controller.mm
+++ ios/chrome/browser/ui/table_view/chrome_table_view_controller.mm
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 const CGFloat kTableViewSeparatorInset = 16;
-const CGFloat kTableViewSeparatorInsetWithIcon = 56;
+const CGFloat kTableViewSeparatorInsetWithIcon = 60;
 
 @interface ChromeTableViewController ()
 // The loading displayed by [self startLoadingIndicatorWithLoadingMessage:].
