c86aa3fdef01ff4f820daa751c66a32bf4e497be
Takumi Fujimoto <takumif@chromium.org>
2021-08-12 16:24:19 +0000

    Revert "scanning: fix page select saved settings bug"
    This reverts commit 20daf6a8f9e871f84810dcb21095a42b9a11919d.
    
    Reason for revert: Causing ScanningAppBrowserTest.All to fail:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/50274/overview
    
    Original change's description:
    > scanning: fix page select saved settings bug
    >
    > When applying saved settings, wait for the next render after changing
    > the scanner source before changing the other settings. Since page select
    > options vary by scanner source, this gives the page select UI element
    > time to update its valid options before using the new options.
    >
    > Bug: b:195972896
    > Change-Id: Ife6f71b51b38f1a873df68370de88fc0c8a16463
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077472
    > Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    > Commit-Queue: Paul Moy <pmoy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#910991}
    
    Bug: b:195972896
    Change-Id: I76d4c953389d41427e41e9160e5052e3b6c26af7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089969
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911343}

--- chrome/test/data/webui/chromeos/scanning/scanning_app_test.js
+++ chrome/test/data/webui/chromeos/scanning/scanning_app_test.js
@@ -52,11 +52,9 @@ const SourceType = {
   ADF_DUPLEX: ash.scanning.mojom.SourceType.kAdfDuplex,
 };
 
-const firstPageSizes = [PageSize.A4, PageSize.Max];
+const firstPageSizes = [PageSize.A4, PageSize.Letter, PageSize.Max];
 
-const secondPageSizes = [PageSize.A4, PageSize.Letter];
-
-const thirdPageSizes = [PageSize.A4, PageSize.Max];
+const secondPageSizes = [PageSize.A4, PageSize.Max];
 
 const firstScannerId =
     /** @type {!mojoBase.mojom.UnguessableToken} */ ({high: 0, low: 1});
@@ -69,7 +67,7 @@ const secondScannerName = 'Scanner 2';
 const firstCapabilities = {
   sources: [
     createScannerSource(SourceType.ADF_DUPLEX, ADF_DUPLEX, firstPageSizes),
-    createScannerSource(SourceType.FLATBED, PLATEN, secondPageSizes),
+    createScannerSource(SourceType.FLATBED, PLATEN, firstPageSizes),
   ],
   colorModes: [ColorMode.BLACK_AND_WHITE, ColorMode.COLOR],
   resolutions: [75, 100, 300]
@@ -77,8 +75,8 @@ const firstCapabilities = {
 
 const secondCapabilities = {
   sources: [
-    createScannerSource(SourceType.ADF_DUPLEX, ADF_DUPLEX, thirdPageSizes),
-    createScannerSource(SourceType.ADF_SIMPLEX, ADF_SIMPLEX, thirdPageSizes),
+    createScannerSource(SourceType.ADF_DUPLEX, ADF_DUPLEX, secondPageSizes),
+    createScannerSource(SourceType.ADF_SIMPLEX, ADF_SIMPLEX, secondPageSizes),
   ],
   colorModes: [ColorMode.BLACK_AND_WHITE, ColorMode.GRAYSCALE],
   resolutions: [150, 600]
@@ -595,7 +593,7 @@ export function scanningAppTest() {
           assertEquals(
               ColorMode.COLOR.toString(), scanningApp.selectedColorMode);
           assertEquals(
-              firstCapabilities.sources[1].pageSizes[1].toString(),
+              firstCapabilities.sources[0].pageSizes[1].toString(),
               scanningApp.selectedPageSize);
           assertEquals(
               firstCapabilities.resolutions[0].toString(),
@@ -1346,12 +1344,6 @@ export function scanningAppTest() {
           return getScannerCapabilities();
         })
         .then(() => {
-          // Ensure the UI elements have updated since the last render. This
-          // allows them to observe the change in scanner source and set mapped
-          // properties appropriately.
-          return flushTasks();
-        })
-        .then(() => {
           assertEquals(
               tokenToString(firstScannerId),
               scanningApp.$$('#scannerSelect').$$('select').value);
@@ -1663,12 +1655,6 @@ export function scanningAppTest() {
     return initializeScanningApp(expectedScanners, capabilities)
         .then(() => {
           return getScannerCapabilities();
-        })
-        .then(() => {
-          // Ensure the UI elements have updated since the last render. This
-          // allows them to observe the change in scanner source and set mapped
-          // properties appropriately.
-          return flushTasks();
         })
         .then(() => {
           scanningApp.$$('#scanButton').click();
