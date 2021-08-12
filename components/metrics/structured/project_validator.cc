90d4d94212d64ef186066166577b46d95efcb9e3
Jonghyun Ahn <jongahn@google.com>
2021-08-12 01:24:12 +0000

    [Structured Metrics] Generates validator code that will be used to validate the IPC messages.
    This validator function call will be used to validate and construct
    EventBase entities to be signed and shipped by the Ash service.
    
    Bug: 1238197
    Change-Id: Idf595e5e5a19ee10c62d2986f4c47f0241204641
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3083613
    Commit-Queue: Jong Ahn <jongahn@google.com>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911090}

---
+++ components/metrics/structured/project_validator.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/metrics/structured/project_validator.h"
+
+#include <cstdint>
+
+namespace metrics {
+namespace structured {
+
+ProjectValidator::ProjectValidator(uint64_t project_hash)
+    : project_hash_(project_hash) {}
+
+uint64_t ProjectValidator::project_hash() const {
+  return project_hash_;
+}
+
+}  // namespace structured
+}  // namespace metrics
