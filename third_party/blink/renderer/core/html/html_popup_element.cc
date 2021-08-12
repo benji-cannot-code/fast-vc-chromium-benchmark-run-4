54e3e01aa064d3b7cfed4b18bfa850d06c58aafc
Mason Freed <masonf@chromium.org>
2021-08-12 16:29:25 +0000

    Update lifecycle to clean from popup.open
    There is a resize observer used by <popup> to detect when it has
    been resized, which is one of the "light-dismiss" conditions.
    Because resize observer always fires once on the first Paint,
    there was a case where the behavior from JS was confusing/
    incorrect. Essentially, if you open the popup and then
    immediately (synchronously) change the size of the popup, it
    would not close. With this change, popup.open() forces a
    rendering lifecycle.
    
    This CL also splits out the resize tests for popup into a
    separate file, and removes the usage of setTimeout(), which was
    previously buttering over this bug. Thanks to wanming.lin@ for
    pointing this out in [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3084482
    
    Bug: 1197633
    
    Change-Id: I733f1215799980d0ed922cf8d8d312cbdcc140b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089750
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#911345}

--- third_party/blink/renderer/core/html/html_popup_element.cc
+++ third_party/blink/renderer/core/html/html_popup_element.cc
@@ -95,7 +95,6 @@ void HTMLPopupElement::Invoke(Element* invoker) {
 void HTMLPopupElement::show() {
   if (open_ || !isConnected())
     return;
-
   // Only hide popups up to this popup's ancestral popup.
   GetDocument().HideAllPopupsUntil(NearestOpenAncestralPopup(this));
   open_ = true;
@@ -105,6 +104,13 @@ void HTMLPopupElement::show() {
   SetFocus();
   being_shown_ = true;
   resize_observer_->observe(this);
+  // We need to update lifecycle phases to ensure the initial resize
+  // observation gets fired before the popup.show() call returns. Otherwise
+  // code like this will fail to work properly:
+  //   popup.show();
+  //   popup.style.width = change; // Should light-dismiss popup, but won't
+  GetDocument().GetFrame()->View()->UpdateAllLifecyclePhases(
+      DocumentUpdateReason::kPagePopup);
 }
 
 bool HTMLPopupElement::IsKeyboardFocusable() const {
