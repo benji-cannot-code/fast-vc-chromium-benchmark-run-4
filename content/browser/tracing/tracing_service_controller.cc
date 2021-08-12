2ea0908883060945a577f0d34b85d59c03ffa3b1
Alex Gough <ajgo@chromium.org>
2021-08-12 23:19:48 +0000

    Specify tracing service sandbox in mojom
    This moves to using the ServiceSandbox mojom attribute to select
    the sandbox, and removes reference from service_sandbox_type.h
    
    No functional changes, see linked bug for details.
    
    Bug: 1210301
    Change-Id: Id98b13b004af382ab008acf5b4a1e91ca74689f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3083318
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911546}

--- content/browser/tracing/tracing_service_controller.cc
+++ content/browser/tracing/tracing_service_controller.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
-#include "content/browser/service_sandbox_type.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/service_process_host.h"
@@ -19,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "services/tracing/public/cpp/traced_process.h"
 #include "services/tracing/public/cpp/tracing_features.h"
+#include "services/tracing/public/mojom/tracing_service.mojom.h"
 #include "services/tracing/tracing_service.h"
 
 namespace content {
