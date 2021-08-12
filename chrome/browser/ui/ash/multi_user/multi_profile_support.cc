c0d6adb7a4d40e2e8f918e2a7d0c95091edf4bca
Nancy Wang <nancylingwang@chromium.org>
2021-08-12 03:08:43 +0000

    Defer the full restore service init.
    When the system restarts, both the primary user and the non primary
    users are created. Therefore, the restore process starts for both the
    primary user and the non primary users if the restore setting is
    'Always restore', and all windows for all users are saved to the
    current active user profile path. This could cause the non primary
    users lost the restore data after reboot or sign in/sign off.
    
    This CL adds ActiveUserChanged to FullRestoreService. If the current
    user is not the primary user and not the active user, don't init the
    full restore service for the restore process. When the user is
    changed to the active user, start the restore process to save the
    restored window to the correct profile path.
    
    BUG=1146900
    
    Change-Id: Idba2cd159d10d74164271310ede491c9639c9a38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088978
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911132}

--- chrome/browser/ui/ash/multi_user/multi_profile_support.cc
+++ chrome/browser/ui/ash/multi_user/multi_profile_support.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/multi_user_window_manager_observer.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
+#include "chrome/browser/ash/full_restore/full_restore_service.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -227,8 +228,13 @@ void MultiProfileSupport::OnWindowOwnerEntryChanged(aura::Window* window,
 
 void MultiProfileSupport::OnTransitionUserShelfToNewAccount() {
   if (full_restore::features::IsFullRestoreEnabled()) {
-    full_restore::SetActiveProfilePath(
-        ProfileManager::GetActiveUserProfile()->GetPath());
+    Profile* profile = ProfileManager::GetActiveUserProfile();
+    full_restore::SetActiveProfilePath(profile->GetPath());
+
+    auto* full_restore_service =
+        ash::full_restore::FullRestoreService::GetForProfile(profile);
+    if (full_restore_service)
+      full_restore_service->OnTransitionedToNewActiveUser(profile);
   }
 
   ChromeShelfController* chrome_shelf_controller =
