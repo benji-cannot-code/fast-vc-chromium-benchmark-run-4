61ad7581179b96fd4f2acd5e3ba6b6145c21adeb
Ewann <ewannpv@google.com>
2021-08-12 15:07:52 +0000

    [iOS][7/N] Removes deprecated ChromeIdentityService APIs
    This CL removes deprecated APIs from SignedInAccountsViewControllerTest.
    
    Bug: 1231916
    Change-Id: I66f8ce938c79c8bb01e6990aa3b7126e37f3bdad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3087668
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911318}

--- ios/chrome/browser/ui/authentication/signed_in_accounts_view_controller_unittest.mm
+++ ios/chrome/browser/ui/authentication/signed_in_accounts_view_controller_unittest.mm
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/authentication_service_delegate_fake.h"
 #include "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/authentication_service_fake.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service_mock.h"
 #include "ios/chrome/test/block_cleanup_test.h"
@@ -34,9 +36,9 @@ class SignedInAccountsViewControllerTest : public BlockCleanupTest {
 
   void SetUp() override {
     BlockCleanupTest::SetUp();
-    ios::FakeChromeIdentityService* identity_service =
+    identity_service_ =
         ios::FakeChromeIdentityService::GetInstanceFromChromeProvider();
-    identity_service->AddIdentities(@[ @"identity1" ]);
+    identity_service_->AddIdentities(@[ @"identity1" ]);
 
     TestChromeBrowserState::Builder builder;
     builder.AddTestingFactory(
@@ -49,10 +51,12 @@ class SignedInAccountsViewControllerTest : public BlockCleanupTest {
     AuthenticationServiceFactory::CreateAndInitializeForBrowserState(
         browser_state_.get(),
         std::make_unique<AuthenticationServiceDelegateFake>());
+    ChromeAccountManagerService* account_manager_service =
+        ChromeAccountManagerServiceFactory::GetForBrowserState(
+            browser_state_.get());
     AuthenticationService* auth_service =
         AuthenticationServiceFactory::GetForBrowserState(browser_state_.get());
-    auth_service->SignIn(
-        [identity_service->GetAllIdentities(nullptr) firstObject]);
+    auth_service->SignIn(account_manager_service->GetDefaultIdentity());
   }
 
  protected:
@@ -62,6 +66,7 @@ class SignedInAccountsViewControllerTest : public BlockCleanupTest {
   std::unique_ptr<TestChromeBrowserState> browser_state_;
   variations::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
       variations::VariationsIdsProvider::Mode::kUseSignedInState};
+  ios::FakeChromeIdentityService* identity_service_;
 };
 
 // Tests that the signed in accounts view shouldn't be presented when the
@@ -76,10 +81,8 @@ TEST_F(SignedInAccountsViewControllerTest,
 // have changed.
 TEST_F(SignedInAccountsViewControllerTest,
        ShouldBePresentedForBrowserStateNecessary) {
-  ios::FakeChromeIdentityService* identity_service =
-      ios::FakeChromeIdentityService::GetInstanceFromChromeProvider();
-  identity_service->AddIdentities(@[ @"identity2" ]);
-  identity_service->FireChromeIdentityReload();
+  identity_service_->AddIdentities(@[ @"identity2" ]);
+  identity_service_->FireChromeIdentityReload();
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE([SignedInAccountsViewController
       shouldBePresentedForBrowserState:browser_state_.get()]);
