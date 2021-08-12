54d0963b3e47a0aabb119e2ff800e7f66c5cc25b
kouhei <kouhei@chromium.org>
2021-08-12 13:23:10 +0000

    Remove unused ScriptState* arg from PendingImportMap ctor
    Bug: None
    Change-Id: I9bf9d86051a1f140460fb274475e685a134976ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3090546
    Auto-Submit: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911284}

--- third_party/blink/renderer/core/script/pending_import_map.h
+++ third_party/blink/renderer/core/script/pending_import_map.h
@@ -41,8 +41,7 @@ class CORE_EXPORT PendingImportMap final
                                         const String& import_map_text,
                                         const KURL& base_url);
 
-  PendingImportMap(ScriptState* script_state,
-                   ScriptElementBase&,
+  PendingImportMap(ScriptElementBase&,
                    ImportMap*,
                    absl::optional<ImportMapError> error_to_rethrow,
                    const ExecutionContext& original_context);
