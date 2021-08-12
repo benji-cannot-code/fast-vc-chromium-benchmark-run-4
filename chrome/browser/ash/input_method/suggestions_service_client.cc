8aabd3e74d6bdb4291f243a38b739089e4aaa3a6
Darren Shen <shend@chromium.org>
2021-08-12 05:22:52 +0000

    cros-suggest: map field trial to experiment
    This CL maps the field trial groups given by finch to their respective
    experiment groups in the suggestion service.
    
    BUG=1146266
    
    Change-Id: I5552adfebf08df625c17bb68e9bb2b99c37f4355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053472
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: My Nguyen <myy@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911160}

--- chrome/browser/ash/input_method/suggestions_service_client.cc
+++ chrome/browser/ash/input_method/suggestions_service_client.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/input_method/suggestions_service_client.h"
 
+#include "ash/constants/ash_features.h"
 #include "base/bind.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "chromeos/services/machine_learning/public/cpp/service_connection.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -17,12 +19,26 @@ namespace {
 using ::chromeos::ime::TextSuggestion;
 using ::chromeos::ime::TextSuggestionMode;
 using ::chromeos::ime::TextSuggestionType;
+using ::chromeos::machine_learning::mojom::MultiWordExperimentGroup;
 using ::chromeos::machine_learning::mojom::NextWordCompletionCandidate;
 using ::chromeos::machine_learning::mojom::TextSuggesterQuery;
 using ::chromeos::machine_learning::mojom::TextSuggesterResultPtr;
+using ::chromeos::machine_learning::mojom::TextSuggesterSpec;
 using ::chromeos::machine_learning::mojom::TextSuggestionCandidatePtr;
 
 constexpr size_t kMaxNumberCharsSent = 100;
+
+MultiWordExperimentGroup GetExperimentGroup(const std::string& finch_trial) {
+  if (finch_trial == "gboard")
+    return MultiWordExperimentGroup::kGboard;
+  if (finch_trial == "gboard_relaxed_a")
+    return MultiWordExperimentGroup::kGboardRelaxedA;
+  if (finch_trial == "gboard_relaxed_b")
+    return MultiWordExperimentGroup::kGboardRelaxedB;
+  if (finch_trial == "gboard_relaxed_c")
+    return MultiWordExperimentGroup::kGboardRelaxedC;
+  return MultiWordExperimentGroup::kDefault;
+}
 
 chromeos::machine_learning::mojom::TextSuggestionMode ToTextSuggestionModeMojom(
     TextSuggestionMode suggestion_mode) {
@@ -62,8 +78,9 @@ void RecordRequestLatency(base::TimeDelta delta) {
 }  // namespace
 
 SuggestionsServiceClient::SuggestionsServiceClient() {
-  auto spec = chromeos::machine_learning::mojom::TextSuggesterSpec::New(
-      chromeos::machine_learning::mojom::MultiWordExperimentGroup::kGboard);
+  std::string field_trial = base::GetFieldTrialParamValueByFeature(
+      chromeos::features::kAssistMultiWord, "group");
+  auto spec = TextSuggesterSpec::New(GetExperimentGroup(field_trial));
 
   chromeos::machine_learning::ServiceConnection::GetInstance()
       ->GetMachineLearningService()
