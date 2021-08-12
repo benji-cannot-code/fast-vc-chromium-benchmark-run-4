c2c413aca7736567be60a608e3d0f63babdf0d13
Kartik Hegde <khegde@chromium.org>
2021-08-12 23:04:20 +0000

    Add feature flag for displaying Traffic Counters in Settings UI
    The flag controls whether data usage for cellular networks is displayed
    in the Settings UI. Disabling the flag will hide data usage. This flag is
    enabled by default.
    
    Bug: b/196258478
    Change-Id: Ifc37d1db68cfea8c3002a6419f3264213575948e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089541
    Reviewed-by: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911539}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -392,6 +392,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kSystemProxyForSystemServices;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kTabClusterUI;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kTelemetryExtension;
+COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kTrafficCountersSettingsUi;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kTrilinearFiltering;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kUseBluetoothSystemInAsh;
