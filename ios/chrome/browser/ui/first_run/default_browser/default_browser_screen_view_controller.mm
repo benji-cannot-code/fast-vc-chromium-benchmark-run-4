f7cd5952f733c0347688b2a37f7d89261353ab38
Aliona DANGLA <alionadangla@google.com>
2021-08-12 13:56:35 +0000

    [iOS] Add egtest for FRE's default browser screen
    This CL does:
    * Add UI test which test
    ** Title
    ** Subtitle
    ** Instructions and its display order
    * Add default browser screen helper
    
    Bug: 1189841
    Change-Id: I3369eb1d8b80c11111cba15197df5c5b7dbb70f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086523
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911295}

--- ios/chrome/browser/ui/first_run/default_browser/default_browser_screen_view_controller.mm
+++ ios/chrome/browser/ui/first_run/default_browser/default_browser_screen_view_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/first_run/default_browser/instruction_table_view.h"
 #import "ios/chrome/browser/ui/first_run/default_browser/instruction_table_view_cell.h"
+#import "ios/chrome/browser/ui/first_run/first_run_constants.h"
 #include "ios/chrome/common/string_util.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
@@ -20,9 +21,6 @@ namespace {
 
 // Reuse ID for registering cell class in table views.
 constexpr NSString* kReuseID = @"InstructionTableCell";
-
-NSString* const kBeginBoldTag = @"BEGIN_BOLD[ \t]*";
-NSString* const kEndBoldTag = @"[ \t]*END_BOLD";
 
 }  // namespace
 
@@ -39,6 +37,8 @@ NSString* const kEndBoldTag = @"[ \t]*END_BOLD";
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
+  self.view.accessibilityIdentifier =
+      first_run::kFirstRunDefaultBrowserScreenAccessibilityIdentifier;
   self.bannerImage = [UIImage imageNamed:@"default_browser_screen_banner"];
   self.titleText =
       l10n_util::GetNSString(IDS_IOS_FIRST_RUN_DEFAULT_BROWSER_SCREEN_TITLE);
@@ -110,8 +110,8 @@ NSString* const kEndBoldTag = @"[ \t]*END_BOLD";
 // Parses a string with an embedded bold part inside, delineated by
 // "BEGIN_BOLD" and "END_BOLD". Returns an attributed string with bold part.
 - (NSAttributedString*)putBoldPartInString:(NSString*)string {
-  StringWithTag parsedString =
-      ParseStringWithTag(string, kBeginBoldTag, kEndBoldTag);
+  StringWithTag parsedString = ParseStringWithTag(
+      string, first_run::kBeginBoldTag, first_run::kEndBoldTag);
 
   NSMutableAttributedString* attributedString =
       [[NSMutableAttributedString alloc] initWithString:parsedString.string];
