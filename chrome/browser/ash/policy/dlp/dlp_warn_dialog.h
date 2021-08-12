1aaecb0158260cc6de12c99439836537bf12a42b
Aida Zolic <aidazolic@chromium.org>
2021-08-12 12:16:14 +0000

    Use DialogDelegate as base for DLPWarnDialogs
    There's no need to use the BubbleDialogDelegateView.
    
    Bug: 1226028
    Change-Id: I72a721858d9330655ee36c091e7628beebeb8a30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3087385
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911266}

--- chrome/browser/ash/policy/dlp/dlp_warn_dialog.h
+++ chrome/browser/ash/policy/dlp/dlp_warn_dialog.h
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_POLICY_DLP_DLP_WARN_DIALOG_H_
 
 #include "ui/base/metadata/metadata_header_macros.h"
-#include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/window/dialog_delegate.h"
 
 namespace policy {
 
 // PrintWarnDialog is a system dialog that is shown when printing restriction
 // level is set to WARN.
-class PrintWarnDialog : public views::BubbleDialogDelegateView {
+class PrintWarnDialog : public views::DialogDelegateView {
  public:
   METADATA_HEADER(PrintWarnDialog);
   PrintWarnDialog(base::OnceClosure accept_callback,
