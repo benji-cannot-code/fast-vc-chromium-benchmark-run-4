770a5f0a8dc1c0db4a89e167f61a6d4f2b9e408b
Lauren Wei <laurenwei@google.com>
2021-08-12 08:02:02 +0000

    Audio WebUI: implement an output device page
    Adds an output device page that presents five audio files to play from.
    By responding to whether you can hear the audio clearly, a map is
    updated to record user response.
    
    Bug: b/193596247
    Change-Id: Ie4d5c90cae59c54d297cf93e6fc732079c1258a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3081382
    Commit-Queue: Lauren Wei <laurenwei@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911196}

--- chrome/browser/resources/chromeos/audio/device_page.ts
+++ chrome/browser/resources/chromeos/audio/device_page.ts
@@ -4,16 +4,19 @@ import {$} from 'chrome://resources/js/util.m.js';
 import {DeviceData, PageCallbackRouter, PageHandlerRemote} from './audio.mojom-webui.js';
 import {AudioBroker} from './audio_broker.js';
 import {DeviceTable} from './device_table.js';
+import {OutputPage} from './output_page.js';
+import {Page} from './page.js';
 
 export interface DeviceMap {
   [id: number]: DeviceData;
 }
 
-export class DevicePage {
+export class DevicePage extends Page {
   private deviceTable: DeviceTable;
   private router: PageCallbackRouter;
   private mojoHandler: PageHandlerRemote;
   constructor() {
+    super('devices');
     this.router = AudioBroker.getInstance().callbackRouter;
     this.mojoHandler = AudioBroker.getInstance().handler;
     this.deviceTable = new DeviceTable();
@@ -38,6 +41,7 @@ export class DevicePage {
 
   updateDeviceInfo(devices: DeviceMap) {
     this.deviceTable.setDevices(devices);
+    OutputPage.getInstance().updateActiveOutputDevice();
   }
 
   updateDeviceVolume(nodeId: number, volume: number) {
