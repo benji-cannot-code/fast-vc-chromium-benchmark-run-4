34b61351575e5432d1a90eea2b20d1e2c9397b85
Yuki Yamada <yukiy@chromium.org>
2021-08-13 11:25:23 +0000

    Layout: Follow-up for https://crrev.com/c/2465602
    This fix two issues: memory leak and allocation error in pre-finalizer.
    
    Memory leak was reported on WebKit Linux Leak bot.
    https://ci.chromium.org/ui/p/chromium/builders/ci/WebKit%20Linux%20Leak/28852/overview
    The Persistents were raw ptrs before the CL, so converting them to
    UntracedMembers shouldn't cause an issue.
    
    Allocation error is reported on Linux Tests (dbg)(1).
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/98187/overview
    Allocation is not allowed in pre-finalizer. We can avoid it by calling
    GetSecureTextTimers() in ctor.
    
    Bug: 1030176, 1239524
    Change-Id: I25e05b4dce78e4b2a57826a755002852030f861c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093110
    Auto-Submit: Yuki Yamada <yukiy@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911705}

--- third_party/blink/renderer/core/layout/layout_text.cc
+++ third_party/blink/renderer/core/layout/layout_text.cc
@@ -177,6 +177,11 @@ LayoutText::LayoutText(Node* node, scoped_refptr<StringImpl> str)
 
   if (node)
     GetFrameView()->IncrementVisuallyNonEmptyCharacterCount(text_.length());
+
+  // Call GetSecureTextTimers() to ensure map exists. It is called in
+  // pre-finalizer where allocation is not allowed.
+  // TODO(yukiy): Remove this once converting SecureTextTimer to GCed.
+  GetSecureTextTimers();
 }
 
 LayoutText* LayoutText::CreateEmptyAnonymous(
