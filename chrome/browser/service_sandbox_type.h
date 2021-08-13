113386eec64c87088ed432056afaedad78b14ada
Alex Gough <ajgo@chromium.org>
2021-08-13 21:21:53 +0000

    Specify removable storage writer sandbox in mojom
    This moves to using the ServiceSandbox mojom attribute to select
    the sandbox, and removes reference from service_sandbox_type.h
    
    No functional changes, see linked bug for details.
    
    Bug: 1210301
    Change-Id: I662192f0ea676d71d9cfa91d341cba07e5a44584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088240
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911890}

--- chrome/browser/service_sandbox_type.h
+++ chrome/browser/service_sandbox_type.h
@@ -21,23 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file maps service classes to sandbox types. See
 // ServiceProcessHost::Launch() for how these templates are consumed.
 
-// chrome::mojom::RemovableStorageWriter
-namespace chrome {
-namespace mojom {
-class RemovableStorageWriter;
-}  // namespace mojom
-}  // namespace chrome
-
-template <>
-inline sandbox::policy::SandboxType
-content::GetServiceSandboxType<chrome::mojom::RemovableStorageWriter>() {
-#if defined(OS_WIN)
-  return sandbox::policy::SandboxType::kNoSandboxAndElevatedPrivileges;
-#else
-  return sandbox::policy::SandboxType::kNoSandbox;
-#endif  // !defined(OS_WIN)
-}
-
 // chrome::mojom::UtilReadIcon
 #if defined(OS_WIN)
 namespace chrome {
