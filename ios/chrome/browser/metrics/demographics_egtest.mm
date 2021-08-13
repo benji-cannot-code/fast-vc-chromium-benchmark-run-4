e1e4f87194e1a16dcbf85e9c955847afc3b4c7ca
Mike Dougherty <michaeldo@chromium.org>
2021-08-13 18:33:43 +0000

    Update DemographicsTestCase testBirthYear_ name
    This instance variable is showing up as a failed test, likely because it
    starts with "test". Rename to birthYear_ in order to prevent this
    incorrect test case parsing.
    
    Example "failure":
    https://ci.chromium.org/ui/p/chrome/builders/official/ios/6301/steps
    
    Change-Id: Id67c375399cb58cb85e10d60f32eb2dd082a5e58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094509
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911825}

--- ios/chrome/browser/metrics/demographics_egtest.mm
+++ ios/chrome/browser/metrics/demographics_egtest.mm
@@ -28,7 +28,7 @@ const metrics::UserDemographicsProto::Gender kTestGender =
 }  // namespace
 
 @interface DemographicsTestCase : ChromeTestCase {
-  int testBirthYear_;
+  int birthYear_;
 }
 @end
 
@@ -46,8 +46,8 @@ const metrics::UserDemographicsProto::Gender kTestGender =
   [MetricsAppInterface updateNetworkTime:now];
 
   // Get the maximum eligible birth year for reporting demographics.
-  testBirthYear_ = [MetricsAppInterface maximumEligibleBirthYearForTime:now];
-  [self addUserDemographicsToSyncServerWithBirthYear:testBirthYear_
+  birthYear_ = [MetricsAppInterface maximumEligibleBirthYearForTime:now];
+  [self addUserDemographicsToSyncServerWithBirthYear:birthYear_
                                               gender:kTestGender];
   [self signInAndSync];
   [self grantMetricsConsent];
@@ -192,7 +192,7 @@ const metrics::UserDemographicsProto::Gender kTestGender =
 
   [self buildAndStoreUKMLog];
 
-  GREYAssertTrue([MetricsAppInterface UKMReportHasBirthYear:testBirthYear_
+  GREYAssertTrue([MetricsAppInterface UKMReportHasBirthYear:birthYear_
                                                      gender:kTestGender],
                  @"The report should contain the specified user demographics");
 
@@ -246,7 +246,7 @@ const metrics::UserDemographicsProto::Gender kTestGender =
   GREYAssertTrue([MetricsAppInterface hasUnsentUMALogs],
                  @"The UKM service should have unsent logs.");
 
-  GREYAssertTrue([MetricsAppInterface UMALogHasBirthYear:testBirthYear_
+  GREYAssertTrue([MetricsAppInterface UMALogHasBirthYear:birthYear_
                                                   gender:kTestGender],
                  @"The report should contain the specified user demographics");
 
