8508a5d781c7c8ddcbc4eb044609776fac5823fb
amraboelkher <amraboelkher@chromium.org>
2021-08-13 12:30:52 +0000

    PSM: Always enable using fake PSM RLWE client for integration tests
    In order to execute successfully any integration tests using PSM
    it needs to use the fake PSM RLWE client, not the real
    implementation.
    
    Using fake PSM RLWE client is being done through setting a
    command line kEnterpriseUseFakePsmRlweClient.
    
    All initial enrollment integration tests are having a switch to
    indicate whether initial enrollment is enabled or not by
    switches::kEnterpriseEnableInitialEnrollment. These are the following
    classes:
    1) WizardControllerDeviceStateWithInitialEnrollmentTest
    2) OobeZeroTouchInteractiveUITest
    3) InitialEnrollmentTest (the command line is already enabled there)
    
    Setting the commandline there will guarantee the correct
    behaviour to take place from PSM perspective.
    
    This CL is setting the use of fake PSM RLWE client switch to allow
    using PSM for integration tests in the following test classes:
    1) WizardControllerDeviceStateWithInitialEnrollmentTest
    2) OobeZeroTouchInteractiveUITest
    
    Bug: 1234777
    Change-Id: I22f4714a46c3bdd2d83ffc9a97210c4f46fc5057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093805
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911710}

--- chrome/browser/ash/login/wizard_controller_browsertest.cc
+++ chrome/browser/ash/login/wizard_controller_browsertest.cc
@@ -1449,6 +1449,10 @@ class WizardControllerDeviceStateWithInitialEnrollmentTest
   void SetUpCommandLine(base::CommandLine* command_line) override {
     WizardControllerDeviceStateTest::SetUpCommandLine(command_line);
 
+    // Enable usage of fake PSM RLWE client (for tests checking initial
+    // enrollment).
+    command_line->AppendSwitch(switches::kEnterpriseUseFakePsmRlweClient);
+
     command_line->AppendSwitchASCII(
         switches::kEnterpriseEnableInitialEnrollment,
         AutoEnrollmentController::kInitialEnrollmentAlways);
