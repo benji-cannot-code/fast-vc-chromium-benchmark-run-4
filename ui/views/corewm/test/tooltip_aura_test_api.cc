b7991d5f05fa64c9adb975b104c204e4ee2222c8
Maksim Sisov <msisov@igalia.com>
2021-08-13 05:43:36 +0000

    ozone: tooltips: calculate anchor for menu bounds. 3/*
    Some system compositors as Wayland do not share global client
    geometry and it's impossible to know if newly created child
    windows (such as tooltips in this particular cases) are going to fit
    display nicely.
    
    Thus, there is additional information required, which is anchor
    position, anchor gravity and others.
    
    Thus, make TooltipAura calculate anchor for new bounds and share
    that data.
    
    Example how it's going to be used:
    https://crrev.com/c/3085042
    
    Bug: 1163646
    Change-Id: I2f85b78011bb8175ea10d5525db2cb6c99766aec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3085324
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#911651}

--- ui/views/corewm/test/tooltip_aura_test_api.cc
+++ ui/views/corewm/test/tooltip_aura_test_api.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/corewm/test/tooltip_aura_test_api.h"
 
 #include "ui/accessibility/ax_node_data.h"
+#include "ui/base/ui_base_types.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/render_text.h"
 #include "ui/views/corewm/tooltip_aura.h"
@@ -25,7 +26,8 @@ void TooltipAuraTestApi::GetAccessibleNodeData(ui::AXNodeData* node_data) {
 gfx::Rect TooltipAuraTestApi::GetTooltipBounds(
     const gfx::Size& tooltip_size,
     const TooltipPosition& position) {
-  return tooltip_aura_->GetTooltipBounds(tooltip_size, position);
+  ui::OwnedWindowAnchor anchor;
+  return tooltip_aura_->GetTooltipBounds(tooltip_size, position, &anchor);
 }
 
 }  // namespace test
