736b7fdd182a14e1a9cfe3a4ebada9770153faeb
Friedrich Horschig <fhorschig@chromium.org>
2021-08-13 13:07:25 +0000

    [Android][Passwords] Create java component to handle backend requests
    This CL adds the scaffold for the java backend. In combination with
    https://crrev.com/c/2704560 (which can't land yet), it allows to
    parse and show all passwords by queryng the ChromeSync module.
    
    To make this parsing possible, it reuses the helpers that used to be
    scoped to the sync bridge by moving it into a util class and adding
    some tests.
    
    Bug: 1229654
    Change-Id: I7ca9094e758465f6d909e81304e227a4b77a3348
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3075103
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911718}

--- chrome/browser/password_manager/android/password_store_android_backend_bridge_impl.h
+++ chrome/browser/password_manager/android/password_store_android_backend_bridge_impl.h
@@ -34,7 +34,7 @@ class PasswordStoreAndroidBackendBridgeImpl
   void OnCompleteWithLogins(
       JNIEnv* env,
       jint task_id,
-      const base::android::JavaParamRef<jobject>& passwords);
+      const base::android::JavaParamRef<jbyteArray>& passwords);
 
  private:
   // Implements PasswordStoreAndroidBackendBridge interface.
