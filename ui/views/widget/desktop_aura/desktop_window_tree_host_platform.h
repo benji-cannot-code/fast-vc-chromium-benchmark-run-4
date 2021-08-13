af6dc7e9006622f267004b867cd6221af0e7369a
Alexander Dunaev <adunaev@igalia.com>
2021-08-13 18:30:32 +0000

    [ozone/wayland] Enabled window transparency on Wayland.
    Wayland compositors support transparency naturally.  This CL enables
    transparency for Wayland windows.
    
    Bug: 1239117
    Change-Id: I453ca5b83fcdd98b6db2d9a8b54d841eefdb64a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089356
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#911823}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_platform.h
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_platform.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
 #include "ui/aura/window_tree_host_platform.h"
@@ -152,6 +153,9 @@ class VIEWS_EXPORT DesktopWindowTreeHostPlatform
   gfx::Rect ToPixelRect(const gfx::Rect& rect_in_dip) const;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(DesktopWindowTreeHostPlatformTest,
+                           UpdateWindowShapeFromWindowMask);
+
   void ScheduleRelayout();
 
   Widget* GetWidget();
