1d65c52b33958478c227101185cd9f5bd245da65
wutao <wutao@chromium.org>
2021-08-13 00:53:16 +0000

    libassistant-v2: Add interfaces for ServiceController
    This patch adds three interfaces:
    1. StartServices(): will also start grpc services in v2.
    2. SetChromeOSApiDelegate(): need to design for v2.
    3. ResetAllDataAndShutdown(): v2 will call
       ConfigSettingsService.
    
    Remaining work will be to migrate DeviceStateListener to
    DeviceStateEvent observer.
    
    Bug: b/193148399
    Test: passed test
    Change-Id: I5bf7234a02943f1e1b8a0fe2d8f22b8f5d0bd345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3054864
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911594}

--- chromeos/services/libassistant/grpc/assistant_client_v1.h
+++ chromeos/services/libassistant/grpc/assistant_client_v1.h
@@ -20,6 +20,9 @@ class AssistantClientV1 : public AssistantClient {
   ~AssistantClientV1() override;
 
   // chromeos::libassistant::AssistantClient:
+  void StartServices() override;
+  void SetChromeOSApiDelegate(
+      assistant_client::ChromeOSApiDelegate* delegate) override;
   bool StartGrpcServices() override;
   void AddExperimentIds(const std::vector<std::string>& exp_ids) override;
   void SendVoicelessInteraction(
@@ -36,6 +39,7 @@ class AssistantClientV1 : public AssistantClient {
   void GetSpeakerIdEnrollmentInfo(
       const GetSpeakerIdEnrollmentInfoRequest& request,
       base::OnceCallback<void(bool user_model_exists)> on_done) override;
+  void ResetAllDataAndShutdown() override;
 };
 
 }  // namespace libassistant
