4c415a9a03093cda4950b3eeac5e5d0a9054f29b
Kuo Jen Wei <inker@google.com>
2021-08-13 02:41:29 +0000

    CCA: Show not detected document dialog when take photo without document
    Mark changeable document dialog title with |.dialog-msg-holder| for
    reflect title change as passing message argument when dialog open. Show
    NOT_DETECTIVE dialog when taking document photo with no document corners
    detected in result photo.
    
    Bug: b:195905083
    Test: Manually
    Change-Id: I8fcc1b47481ca2e7a25f81408e8b6cfdd5a0139c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3084762
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911613}

--- chromeos/components/camera_app_ui/resources/js/views/camera/mode/photo.js
+++ chromeos/components/camera_app_ui/resources/js/views/camera/mode/photo.js
@@ -18,6 +18,7 @@ import {
 import * as state from '../../../state.js';
 import * as toast from '../../../toast.js';
 import {
+  CanceledError,
   Facing,  // eslint-disable-line no-unused-vars
   PerfEvent,
   Resolution,
@@ -148,7 +149,9 @@ export class Photo extends ModeBase {
       state.set(PerfEvent.PHOTO_CAPTURE_SHUTTER, false, {hasError: true});
       state.set(
           PerfEvent.PHOTO_CAPTURE_POST_PROCESSING, false, {hasError: true});
-      toast.show(I18nString.ERROR_MSG_TAKE_PHOTO_FAILED);
+      if (!(e instanceof CanceledError)) {
+        toast.show(I18nString.ERROR_MSG_TAKE_PHOTO_FAILED);
+      }
       throw e;
     }
   }
--- chromeos/components/camera_app_ui/resources/js/views/camera/mode/scanner.js
+++ chromeos/components/camera_app_ui/resources/js/views/camera/mode/scanner.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {Filenamer} from '../../../models/file_namer.js';
 import {ChromeHelper} from '../../../mojo/chrome_helper.js';
 import {
+  CanceledError,
   Facing,  // eslint-disable-line no-unused-vars
   MimeType,
   Resolution,  // eslint-disable-line no-unused-vars
@@ -55,6 +56,11 @@ export class ScannerHandler {
   async getDocumentReviewResult() {}
 
   /**
+   * Handles case when no document detected in photo result.
+   */
+  handleNoDocument() {}
+
+  /**
    * Handles the result document.
    * @param {!DocumentResult} result
    * @param {string} name Name of the document result to be saved as.
@@ -91,6 +97,11 @@ class DocumentPhotoHandler {
     const namer = new Filenamer();
     const helper = await ChromeHelper.getInstance();
     const corners = await helper.scanDocumentCorners(rawBlob);
+    if (corners.length === 0) {
+      this.handler_.clearBlockingShutterEffect();
+      this.handler_.handleNoDocument();
+      throw new CanceledError(`Couldn't detect a document`);
+    }
     const jpegBlob =
         await helper.convertToDocument(rawBlob, corners, MimeType.JPEG);
 
