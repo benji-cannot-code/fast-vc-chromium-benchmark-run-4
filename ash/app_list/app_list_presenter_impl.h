47438d58b65cd65e7b12db1db094b3e82aa73a94
Toni Barzic <tbarzic@chromium.org>
2021-08-13 01:20:07 +0000

    Properly clean up shelf observation in AppListPresenter
    Fixes a case where AppListPresenterImpl can leave an observation to a
    shelf instance after the shelf has been deleted.
    Also fixes an issue where AppListPresenterImpl may observe multiple
    shelfs at the same time - if app list is shown on a display while the
    shelf is animating out on another display, AppListPresenterImpl may
    still be observing the shelf for display where app list view is closing.
    This observation would not get removed when the app list view closed on
    the first display, because the target app list state would be changed to
    visible. This changes the logic to reset shelf old observations when the
    app list gets shown.
    
    Also, observe ShelfLayoutManager instead of Shelf, so
    AppListPresenterImpl gets notified when the shelf gets deleted (so it
    can reset the observer).
    
    BUG=1235056
    
    Change-Id: I5d145807051665195c2f197e99dddece4bf5407d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3091769
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911602}

--- ash/app_list/app_list_presenter_impl.h
+++ ash/app_list/app_list_presenter_impl.h
@@ -16,12 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/pagination/pagination_model_observer.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/public/cpp/shell_window_ids.h"
-#include "ash/shelf/shelf.h"
-#include "ash/shelf/shelf_observer.h"
+#include "ash/shelf/shelf_layout_manager.h"
+#include "ash/shelf/shelf_layout_manager_observer.h"
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "base/scoped_multi_source_observation.h"
+#include "base/scoped_observation.h"
 #include "ui/aura/client/focus_change_observer.h"
 #include "ui/aura/window_observer.h"
 #include "ui/compositor/layer_animation_observer.h"
@@ -47,7 +47,7 @@ class ASH_EXPORT AppListPresenterImpl
       public ui::ImplicitAnimationObserver,
       public views::WidgetObserver,
       public display::DisplayObserver,
-      public ShelfObserver {
+      public ShelfLayoutManagerObserver {
  public:
   static constexpr std::array<int, 7> kIdsOfContainersThatWontHideAppList = {
       kShellWindowId_AppListContainer,
@@ -192,9 +192,10 @@ class ASH_EXPORT AppListPresenterImpl
   void OnDisplayMetricsChanged(const display::Display& display,
                                uint32_t changed_metrics) override;
 
-  // ShelfObserver overrides:
-  void OnBackgroundTypeChanged(ShelfBackgroundType background_type,
-                               AnimationChangeType change_type) override;
+  // ShelfLayoutManagerObserver overrides:
+  void WillDeleteShelfLayoutManager() override;
+  void OnBackgroundUpdated(ShelfBackgroundType background_type,
+                           AnimationChangeType change_type) override;
 
   // Registers a callback that is run when the next frame successfully makes it
   // to the screen.
@@ -215,8 +216,8 @@ class ASH_EXPORT AppListPresenterImpl
   display::ScopedDisplayObserver display_observer_{this};
 
   // An observer that notifies AppListView when the shelf state has changed.
-  base::ScopedMultiSourceObservation<Shelf, ShelfObserver> shelf_observation_{
-      this};
+  base::ScopedObservation<ShelfLayoutManager, ShelfLayoutManagerObserver>
+      shelf_observer_{this};
 
   // The target visibility of the AppListView, true if the target visibility is
   // shown.
