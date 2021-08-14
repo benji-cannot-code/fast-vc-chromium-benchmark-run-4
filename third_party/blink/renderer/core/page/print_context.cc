b4fe23e67f6abab6f5c1de3d10da7705ea2d82d9
Frédéric Wang <fwang@igalia.com>
2021-08-14 00:54:58 +0000

    Rely on the CSS serializer for internals.pageProperty('font-family', ...)
    Static function PrintContext::PageProperty is only used to implement
    internals.pageProperty for web tests. Moreover, the 'font-family'
    is only queried in printing/page-rule-selection.html corresponding to a
    list with a single font family name.
    
    PrintContext::PageProperty's implementation for 'font-family' is just to
    return the string corresponding to the font name without any special
    formatting, which works well for its unique usage but is not very
    consistent with how the CSS font-family property is normally serialized
    per the CSSOM spec [1][2][3]. Also, serialization is currently not
    working properly for generic font-family name like "serif" because of
    how these are parsed internally [4].
    
    This CL instead changes that implementation so that it just relies on
    the CSS serializer. This thus improves consistency while still being
    compatible with the unique API usage. It also removes one more place
    where custom serialization is implemented for font-family, so that it
    is easier to fix [4].
    
    [1] https://drafts.csswg.org/cssom/#serialize-a-comma-separated-list
    [2] https://drafts.csswg.org/cssom/#serialize-a-string
    [3] https://drafts.csswg.org/cssom/#serialize-an-identifier
    [4] https://bugs.chromium.org/p/chromium/issues/detail?id=1065468
    
    Bug: 1065468
    Change-Id: I6580380b4543b33d98e59d7f56487cf3f81d5f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086565
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911956}

--- third_party/blink/renderer/core/page/print_context.cc
+++ third_party/blink/renderer/core/page/print_context.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "third_party/blink/public/web/web_print_page_description.h"
+#include "third_party/blink/renderer/core/css/properties/computed_style_utils.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/page_scale_constraints_set.h"
@@ -272,8 +273,11 @@ String PrintContext::PageProperty(LocalFrame* frame,
     return String::Number(style->LineHeight().Value());
   if (!strcmp(property_name, "font-size"))
     return String::Number(style->GetFontDescription().ComputedPixelSize());
-  if (!strcmp(property_name, "font-family"))
-    return style->GetFontDescription().Family().Family().GetString();
+  if (!strcmp(property_name, "font-family")) {
+    return ComputedStyleUtils::ValueForFontFamily(
+               style->GetFontDescription().Family())
+        ->CssText();
+  }
   if (!strcmp(property_name, "size"))
     return String::Number(style->PageSize().Width()) + ' ' +
            String::Number(style->PageSize().Height());
