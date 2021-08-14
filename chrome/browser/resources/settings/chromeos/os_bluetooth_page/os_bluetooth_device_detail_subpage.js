cf8f2f618b7424e27bacac334be9c9dc437a78be
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-08-14 00:28:51 +0000

    [CrOS Bluetooth] Add bluetooth device detail UI elements
    This CL adds more UI elements to bluetooth device detail page, it does
    not add click event handlers to UI elements, that would be added
    in subsequent CL. A11Y will also be added in a subsequent CL.
    
    Screenshots: https://screenshot.googleplex.com/AqZYd4wbDU5eaqM.png
                 https://screenshot.googleplex.com/7atButXcuAAMfWv.png
                 https://screenshot.googleplex.com/YDK5iwLpzMWNQT8.png
                 https://screenshot.googleplex.com/5BHbJ9S8jbW9G2R.png
    
    
    Bug: 1010321
    Change-Id: I96d667fbe85a71b76d852ba8ebbde1042b8d5f74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3090067
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/master@{#911950}

--- chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_device_detail_subpage.js
+++ chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_device_detail_subpage.js
@@ -10,20 +10,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import '../../settings_shared_css.js';
+import '//resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 
+import {I18nBehavior, I18nBehaviorInterface} from '//resources/js/i18n_behavior.m.js';
 import {html, mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {getDeviceName} from 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_utils.js';
 
+import {loadTimeData} from '../../i18n_setup.js';
 import {Route, RouteObserverBehavior, RouteObserverBehaviorInterface, Router} from '../../router.js';
 import {routes} from '../os_route.m.js';
+
+const mojom = chromeos.bluetoothConfig.mojom;
 
 /**
  * @constructor
  * @extends {PolymerElement}
  * @implements {RouteObserverBehaviorInterface}
+ * @implements {I18nBehaviorInterface}
  */
 const SettingsBluetoothDeviceDetailSubpageElementBase =
-    mixinBehaviors([RouteObserverBehavior], PolymerElement);
+    mixinBehaviors([RouteObserverBehavior, I18nBehavior], PolymerElement);
 
 /** @polymer */
 class SettingsBluetoothDeviceDetailSubpageElement extends
@@ -61,6 +67,13 @@ class SettingsBluetoothDeviceDetailSubpageElement extends
       deviceId_: {
         type: String,
         value: '',
+      },
+
+      /** @private */
+      isDeviceConnected_: {
+        reflectToAttribute: true,
+        type: Boolean,
+        computed: 'computeIsDeviceConnected_(device_.*)',
       },
     };
   }
@@ -107,6 +120,60 @@ class SettingsBluetoothDeviceDetailSubpageElement extends
     }
 
     this.device_ = device;
+  }
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getBluetoothStateIcon_() {
+    return this.isDeviceConnected_ ? 'os-settings:bluetooth-connected' :
+                                     'os-settings:bluetooth-disabled';
+  }
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeIsDeviceConnected_() {
+    return this.device_.deviceProperties.connectionState ===
+        mojom.DeviceConnectionState.kConnected;
+  }
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getBluetoothStateBtnLabel_() {
+    return this.isDeviceConnected_ ? this.i18n('bluetoothDisconnect') :
+                                     this.i18n('bluetoothConnect');
+  }
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getBluetoothStateTextLabel_() {
+    return this.isDeviceConnected_ ?
+        this.i18n('bluetoothDeviceDetailConnected') :
+        this.i18n('bluetoothDeviceDetailDisconnected');
+  }
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getDeviceName_() {
+    return getDeviceName(this.device_);
+  }
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  shouldShowStateBtn_() {
+    return this.device_.deviceProperties.audioCapability ===
+        mojom.AudioOutputCapability.kCapableOfAudioOutput;
   }
 
   /** @private */
