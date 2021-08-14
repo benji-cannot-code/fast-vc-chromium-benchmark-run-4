fffdc4191e4ab521c971405e80be5c2b5c22e48e
Sorin Jianu <sorin@chromium.org>
2021-08-14 00:17:16 +0000

    Make memory allocations more resilient.
    Bug: 1239780
    
    Change-Id: I256d7641293e490836d9dcef90cd4c2e57d5651b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094476
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911947}

--- chrome/updater/updater.cc
+++ chrome/updater/updater.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/message_loop/message_pump_type.h"
+#include "base/process/memory.h"
 #include "base/task/single_thread_task_executor.h"
 #include "base/threading/platform_thread.h"
 #include "build/build_config.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if defined(OS_WIN)
+#include "base/win/process_startup_helper.h"
 #include "chrome/updater/app/server/win/server.h"
 #include "chrome/updater/app/server/win/service_main.h"
 #include "chrome/updater/win/win_util.h"
@@ -118,11 +120,17 @@ int HandleUpdaterCommands(UpdaterScope updater_scope,
   StartCrashReporter(updater_scope, kUpdaterVersion);
 
   InitializeCrashReporting(updater_scope);
-  base::SingleThreadTaskExecutor main_task_executor(base::MessagePumpType::UI);
 
+  // Make the process more resilient to memory allocation issues.
+  base::EnableTerminationOnHeapCorruption();
+  base::EnableTerminationOnOutOfMemory();
 #if defined(OS_WIN)
+  base::win::RegisterInvalidParamHandler();
+
   VLOG(1) << GetUACState();
 #endif
+
+  base::SingleThreadTaskExecutor main_task_executor(base::MessagePumpType::UI);
 
   if (command_line->HasSwitch(kCrashMeSwitch)) {
     // Records a backtrace in the log, crashes the program, saves a crash dump,
