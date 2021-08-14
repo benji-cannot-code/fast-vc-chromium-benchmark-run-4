2d7c0eec17722aae7756c1ae995745a679488686
Bruce Dawson <brucedawson@chromium.org>
2021-08-14 00:31:19 +0000

    Include windows.h where needed in chrome/browser
    While removing windows.h includes from keyboard_codes_win.h there were
    some source files found that used windows.h definitions without
    including it, or while including it in the wrong order. This fixes one
    of those omissions.
    
    Bug: 796644
    Change-Id: I90c4e654a8743743acdce56c9a7b6bb89a89a4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093480
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911952}

--- chrome/browser/ui/web_applications/test/web_app_browsertest_util.cc
+++ chrome/browser/ui/web_applications/test/web_app_browsertest_util.cc
@@ -59,6 +59,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_WIN)
+#include <windows.h>
+
 #include <shellapi.h>
 #include "ui/gfx/icon_util.h"
 #endif
