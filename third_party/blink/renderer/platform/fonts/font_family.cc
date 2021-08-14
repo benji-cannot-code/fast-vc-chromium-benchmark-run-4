739e84211fc98f09f815e18bb0f27d740bdedef5
Frédéric Wang <fwang@igalia.com>
2021-08-14 13:47:46 +0000

    Changes related to internal serialization of FontFamily
    In many places, FontFamily::Family() was used to serialize the CSS
    font-family property, sometimes in web-exposed APIs [1] [2] [3]. They
    were problematic because they duplicate the implementation from the CSS
    serializer without necessarily following all the rules from the CSSOM
    specification [4]. Additionally, generic font families are not properly
    handled by Blink, the CSS serializer has to work around that and so
    should any duplication [5].
    
    This CL adds warnings in font_family.h so that people don't try to
    implement their own serialization of font-family again and instead
    rely on the CSS serializer. It also adds a TODO for bug 1065468
    with detailed explanation of the status.
    
    FontFamily::ToString is tweaked to separate items by a comma followed
    by a space. This is similar to CSSOM serialization people are familiar
    with and is slightly more readable. This is only used internally and
    corresponding tests are updated.
    
    Debug logging in harfbuzz_shaper.cc is also updated so that it directly
    uses FontFamily::ToString. Given the previous whitespace change, the
    difference is minor : the string is no longer built via StringBuilder
    which is slightly less efficient. However, that removes the remaining
    custom serialization of a FontFamily list.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3086444
    [2] https://chromium-review.googlesource.com/c/chromium/src/+/3086565
    [3] https://chromium-review.googlesource.com/c/chromium/src/+/3087350
    [4] https://drafts.csswg.org/cssom/
    [5] https://bugs.chromium.org/p/chromium/issues/detail?id=1065468
    
    Bug: 1065468
    Change-Id: I95621e24f11c282ead41dde1c9ac10e04893522e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3087355
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911996}

--- third_party/blink/renderer/platform/fonts/font_family.cc
+++ third_party/blink/renderer/platform/fonts/font_family.cc
@@ -56,7 +56,7 @@ String FontFamily::ToString() const {
   builder.Append(family_);
   const FontFamily* current = Next();
   while (current) {
-    builder.Append(",");
+    builder.Append(", ");
     builder.Append(current->Family());
     current = current->Next();
   }
