c968ed0495c539e30b8d089537a116723eb9b505
Frédéric Wang <fwang@igalia.com>
2021-08-14 12:47:36 +0000

    InternalPopupMenu: Rely on CSS serializer for serializing font-family
    Currently InternalPopupMenu serializes font-family as an array of
    strings (font-family names) ; listPicker.js parses that array and
    rebuilds a font-family string by quoting each item and joining them
    with commas. This is incorrect for generic font families : they
    should not be quoted [1].
    
    Additionally, the serialization of each item relies on
    FontFamily::Family() which can lead to results different from
    standard CSS serialization [2] [3] [4]. In particular, some
    generic names like "cursive" are currently parsed internally as
    "-webkit-cursive" which is the non-standard value listPicker.js
    would receive (see for example popup-menu-appearance-styled.html).
    
    This CL simplifies this by relying on the CSS serializer to serialize
    the whole font-family value and then passing it as it to listPicker.js
    so it can directly set font-family without further work. This removes
    one more place where a custom serialization of font-family is made,
    fixing inconsistencies and helping with the goal of fixing [5].
    
    [1] https://www.w3.org/TR/css-fonts-4/#generic-family-value
    [2] https://drafts.csswg.org/cssom/#serialize-a-comma-separated-list
    [3] https://drafts.csswg.org/cssom/#serialize-a-string
    [4] https://drafts.csswg.org/cssom/#serialize-an-identifier
    [5] https://bugs.chromium.org/p/chromium/issues/detail?id=1065468
    
    Bug: 1065468
    Change-Id: I6baaec10d86de2b27045b64657586849e5361c96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3087350
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#911995}

--- third_party/blink/renderer/core/html/forms/internal_popup_menu.cc
+++ third_party/blink/renderer/core/html/forms/internal_popup_menu.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/css/css_font_selector.h"
 #include "third_party/blink/renderer/core/css/css_value_id_mappings.h"
+#include "third_party/blink/renderer/core/css/properties/computed_style_utils.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/css/style_request.h"
 #include "third_party/blink/renderer/core/dom/element.h"
@@ -196,13 +197,11 @@ class InternalPopupMenu::ItemIterationContext {
                     : String(),
                 buffer_);
 
-    PagePopupClient::AddString("fontFamily: [", buffer_);
-    for (const FontFamily* f = &BaseFont().Family(); f; f = f->Next()) {
-      AddJavaScriptString(f->Family().GetString(), buffer_);
-      if (f->Next())
-        PagePopupClient::AddString(",", buffer_);
-    }
-    PagePopupClient::AddString("]", buffer_);
+    AddProperty(
+        "fontFamily",
+        ComputedStyleUtils::ValueForFontFamily(BaseFont().Family())->CssText(),
+        buffer_);
+
     PagePopupClient::AddString("},\n", buffer_);
   }
 
@@ -419,13 +418,11 @@ void InternalPopupMenu::AddElementStyle(ItemIterationContext& context,
     AddProperty("fontWeight", font_description.Weight().ToString(), data);
   }
   if (base_font.Family() != font_description.Family()) {
-    PagePopupClient::AddString("fontFamily: [\n", data);
-    for (const FontFamily* f = &font_description.Family(); f; f = f->Next()) {
-      AddJavaScriptString(f->Family().GetString(), data);
-      if (f->Next())
-        PagePopupClient::AddString(",\n", data);
-    }
-    PagePopupClient::AddString("],\n", data);
+    AddProperty(
+        "fontFamily",
+        ComputedStyleUtils::ValueForFontFamily(font_description.Family())
+            ->CssText(),
+        data);
   }
   if (base_font.Style() != font_description.Style()) {
     AddProperty("fontStyle",
