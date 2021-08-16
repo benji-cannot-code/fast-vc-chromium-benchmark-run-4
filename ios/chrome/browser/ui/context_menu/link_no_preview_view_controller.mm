5a408cf66ec458b730f7a84f5e1b71a8c7cb0f03
Gauthier Ambard <gambard@chromium.org>
2021-08-16 08:43:14 +0000

    [iOS] Add a UI for non-preview links
    This CL adds a UI when creating a context menu for a link.
    This UI won't manage the live-preview of the destination page.
    
    Bug: 1237522
    Change-Id: I6dde7bf795a4ead3a4f963891542ad2dd6d87261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089353
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912100}

---
+++ ios/chrome/browser/ui/context_menu/link_no_preview_view_controller.mm
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/context_menu/link_no_preview_view_controller.h"
+
+#import "ios/chrome/browser/ui/context_menu/link_no_preview_view.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation LinkNoPreviewViewController
+
+- (void)loadView {
+  // TODO(crbug.com/1237933): Pass title and subtitle.
+  self.view = [[LinkNoPreviewView alloc] initWithTitle:nil subtitle:nil];
+}
+
+- (void)viewDidLayoutSubviews {
+  self.preferredContentSize = [self computePreferredContentSize];
+}
+
+- (CGSize)computePreferredContentSize {
+  CGFloat width = self.view.bounds.size.width;
+  CGSize minimalSize =
+      [self.view systemLayoutSizeFittingSize:CGSizeMake(width, 0)];
+  return CGSizeMake(width, minimalSize.height);
+}
+
+@end
