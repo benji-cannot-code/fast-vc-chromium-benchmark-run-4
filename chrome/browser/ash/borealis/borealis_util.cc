e29085eccfdb460f4c7b16e5f10ec2b9d4fe11ee
Nicholas Hollingum <hollingum@google.com>
2021-08-16 03:40:28 +0000

    borealis: Make Feedback-form URL generation Asynchronous
    This functionality relies on getting system info via base::SysInfo,
    which is a blocking operation. Blocking is not permitted on the UI
    thread, which is where the call originates.
    
    This CL moves the relevant calls into a threaded helper. There are no
    special requirements for the helper, except that it may block.
    
    Bug: b/196179488
    Change-Id: Ic44449d35a4724bc59d57b2dee9083f812e64dff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086796
    Reviewed-by: Daniel Ng <danielng@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#912058}

--- chrome/browser/ash/borealis/borealis_util.cc
+++ chrome/browser/ash/borealis/borealis_util.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
 #include "chrome/browser/ash/guest_os/guest_os_registry_service.h"
 #include "components/crx_file/id_util.h"
 #include "net/base/url_util.h"
@@ -53,23 +55,8 @@ absl::optional<int> GetBorealisAppId(std::string exec) {
   }
 }
 
-GURL FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
-                     const std::string& app_id,
-                     const std::string& window_title) {
-  // Exclude windows that aren't games.
-  if (app_id.find(kNonGameWindowPrefix) != std::string::npos ||
-      app_id == kBorealisMainAppId) {
-    return GURL();
-  }
-
-  std::string hash = crx_file::id_util::GenerateId(app_id);
-  if (hash == kNonGameIdHash1 || hash == kNonGameIdHash2 ||
-      hash == kNonGameIdHash3) {
-    return GURL();
-  }
-
-  GURL url(kFeedbackUrl);
-  url = net::AppendQueryParameter(url, kAppNameKey, window_title);
+namespace {
+GURL GetSysInfoForUrlAsync(GURL url) {
   url = net::AppendQueryParameter(url, kBoardKey,
                                   base::SysInfo::HardwareModelName());
   url = net::AppendQueryParameter(
@@ -80,6 +67,31 @@ GURL FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
                          base::SysInfo::CPUModelName().c_str()));
   url = net::AppendQueryParameter(url, kPlatformVersionKey,
                                   base::SysInfo::OperatingSystemVersion());
+
+  return url;
+}
+}  // namespace
+
+void FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
+                     const std::string& app_id,
+                     const std::string& window_title,
+                     base::OnceCallback<void(GURL)> url_callback) {
+  // Exclude windows that aren't games.
+  if (app_id.find(kNonGameWindowPrefix) != std::string::npos ||
+      app_id == kBorealisMainAppId) {
+    std::move(url_callback).Run(GURL());
+    return;
+  }
+
+  std::string hash = crx_file::id_util::GenerateId(app_id);
+  if (hash == kNonGameIdHash1 || hash == kNonGameIdHash2 ||
+      hash == kNonGameIdHash3) {
+    std::move(url_callback).Run(GURL());
+    return;
+  }
+
+  GURL url(kFeedbackUrl);
+  url = net::AppendQueryParameter(url, kAppNameKey, window_title);
 
   // Attempt to get the Borealis app ID.
   // TODO(b/173977876): Implement this in a more reliable way.
@@ -93,7 +105,9 @@ GURL FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
     }
   }
 
-  return url;
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, base::MayBlock(), base::BindOnce(&GetSysInfoForUrlAsync, url),
+      std::move(url_callback));
 }
 
 }  // namespace borealis
--- chrome/browser/ash/borealis/borealis_util.h
+++ chrome/browser/ash/borealis/borealis_util.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/callback_forward.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/views/widget/widget.h"
 
@@ -43,11 +44,13 @@ void ShowBorealisSplashScreenView(Profile* profile);
 // Closes the splash screen (borealis_splash_screen_view).
 void CloseBorealisSplashScreenView();
 
-// Returns a URL for a feedback form with prefilled app/device info, or an
-// invalid URL if we don't want to collect feedback for the given |app_id|.
-GURL FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
+// Creates a URL for a feedback form with prefilled app/device info, or an
+// invalid URL if we don't want to collect feedback for the given |app_id|. Will
+// invoke |url_callback| when the url is ready.
+void FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
                      const std::string& app_id,
-                     const std::string& window_title);
+                     const std::string& window_title,
+                     base::OnceCallback<void(GURL)> url_callback);
 
 }  // namespace borealis
 
