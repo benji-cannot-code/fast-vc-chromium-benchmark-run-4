9ac6fe74348b004ec1c78b0a1070484d3c0444cb
Steven Bennetts <stevenjb@chromium.org>
2021-08-16 16:34:27 +0000

    CrOS TrafficCounters: Fix callback lifetime
    If a Callback from a Mojo API is destroyed before it gets called, a
    DCHECK will be triggered (or, with DCHECK disabled, a JS error will
    occur). See the issue for details.
    
    This fix passes a single Callback to the Shill API instead of splitting
    it into two which is more fragile and was responsible for the error.
    
    BUG=b:194440995
    TEST=Manual, see issue
    
    Change-Id: I62c6d21b4a191a0c225c43ed076fca0410ceddda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3092705
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912205}

--- ui/webui/resources/cr_components/chromeos/traffic_counters/traffic_counters.js
+++ ui/webui/resources/cr_components/chromeos/traffic_counters/traffic_counters.js
@@ -191,7 +191,7 @@ export class TrafficCountersElement extends TrafficCountersElementBase {
   async getLastResetTime(guid) {
     const managedPropertiesPromise =
         await this.networkConfig_.getManagedProperties(guid);
-    if (!managedPropertiesPromise) {
+    if (!managedPropertiesPromise || !managedPropertiesPromise.result) {
       return null;
     }
 
@@ -310,6 +310,9 @@ export class TrafficCountersElement extends TrafficCountersElementBase {
    * @private
    */
   lastResetTimeString_(network) {
+    if (network.lastResetTime === null) {
+      return '';
+    }
     return JSON.stringify(network.lastResetTime.internalValue, replacer, '\t');
   }
 }
