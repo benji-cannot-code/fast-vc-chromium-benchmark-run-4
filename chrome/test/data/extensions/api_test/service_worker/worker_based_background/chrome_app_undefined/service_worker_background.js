5ca10c58a0f806b4e3ba0c413e8df35e15820bfe
David Bertoni <dbertoni@chromium.org>
2021-08-16 22:35:12 +0000

    [Extensions] Disallow chrome.app for sw-based extensions.
    This API is deprecated and there no reason to support it for service
    worker-based extensions.
    
    Bug: 1239271
    Change-Id: Ice0ad64189a4a23c6bedbdc11f34a617bdd83a37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094936
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912357}

---
+++ chrome/test/data/extensions/api_test/service_worker/worker_based_background/chrome_app_undefined/service_worker_background.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Test that chrome.app is not defined for service worker-based extensions.
+chrome.test.runTests([
+  function testChromeAppBinding() {
+    chrome.test.assertTrue(chrome.app === undefined);
+    chrome.test.succeed();
+  }
+]);
