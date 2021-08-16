b7ca4c653608ecc6b0bdbab13336fe580b6b4e87
Philipp Hancke <philipp.hancke@googlemail.com>
2021-08-16 12:20:18 +0000

    rtc_encoded_*frame: make Timestamp an uint32_t
    for consistency with the underlying API and since the RTP timestamp is
    defined as uint32_t.
    Also fix unit of RTP timestamp in the stringification since RTP
    timestamps aredefined with a 90khz clock and not microseconds.
    
    BUG=None
    
    Change-Id: I3d3c36537ff792c40c68b42e49ae2d3e50c4de1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3091011
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Cr-Commit-Position: refs/heads/master@{#912133}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_frame.cc
@@ -28,7 +28,7 @@ String RTCEncodedVideoFrame::type() const {
   return delegate_->Type();
 }
 
-uint64_t RTCEncodedVideoFrame::timestamp() const {
+uint32_t RTCEncodedVideoFrame::timestamp() const {
   return delegate_->Timestamp();
 }
 
@@ -79,9 +79,9 @@ String RTCEncodedVideoFrame::toString() const {
     return "empty";
 
   StringBuilder sb;
-  sb.Append("RTCEncodedVideoFrame{timestamp: ");
+  sb.Append("RTCEncodedVideoFrame{rtpTimestamp: ");
   sb.AppendNumber(timestamp());
-  sb.Append("us, size: ");
+  sb.Append(", size: ");
   sb.AppendNumber(data()->ByteLength());
   sb.Append(" bytes, type: ");
   sb.Append(type());
