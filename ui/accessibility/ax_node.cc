10e7d21b45783141731370f84e9cf815b36b722f
Maria Villarreal <mavill@microsoft.com>
2021-08-16 14:02:46 +0000

    Add null check for parent node in AXNode::IsInListMarker
    Since parent_node can be null, we should return false if this is the
    case. Otherwise, we'll encounter a failure when trying to access the
    grandparent_node.
    
    Change-Id: I16fd084d00f7d04655fc1757230f6871fab90139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094700
    Reviewed-by: Victor Fei <vicfei@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912160}

--- ui/accessibility/ax_node.cc
+++ ui/accessibility/ax_node.cc
@@ -1674,7 +1674,10 @@ bool AXNode::IsInListMarker() const {
   // ++StaticText
   // ++++InlineTextBox
   AXNode* parent_node = GetUnignoredParent();
-  if (parent_node && parent_node->data().role == ax::mojom::Role::kListMarker)
+  if (!parent_node)
+    return false;
+
+  if (parent_node->data().role == ax::mojom::Role::kListMarker)
     return true;
 
   AXNode* grandparent_node = parent_node->GetUnignoredParent();
