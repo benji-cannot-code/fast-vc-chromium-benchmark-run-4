2fa84f3bbf6ff369d4d0341338bf6f1117315bc1
Koji Ishii <kojii@chromium.org>
2021-08-16 18:59:02 +0000

    Normalize CJK scripts when matching parentheses
    CJK languages are multi-script languages that computing
    scripts for punctuation and symbol (USCRIPT_COMMON) characters
    from surrounding text can resolve to any one of them. When
    this is combined with parentheses matching, closing
    parentheses may resolve to different scripts from their
    previous punctuation characters.
    
    Most CJK fonts puts the same feature tables to all possible
    scripts for their glyph coverages so that features can apply
    regardless of scripts. But features can't apply if consecutive
    punctuation and symbol characters resolve to multiple scripts.
    
    This patch changes the script resolution of closing
    parentheses not to split runs if both current and opening
    parentheses are one of CJK scripts.
    
    To not to split runs, when encountering a closing bracket do
    not insist on the closing bracket getting assigned the same
    script as the opening bracket if current_set_ provides an
    option to resolve to any other possible Han script as well,
    which avoids breaking the run.
    
    Bug: 1202372
    Change-Id: I12d443d56511b4dfd191e3808734f74fb0428170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3076598
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912274}

--- third_party/blink/renderer/platform/fonts/script_run_iterator_test.cc
+++ third_party/blink/renderer/platform/fonts/script_run_iterator_test.cc
@@ -480,6 +480,92 @@ TEST_F(ScriptRunIteratorTest, QuoteParenChineseParenLatinQuote) {
       {{"\"(萬國碼) ", USCRIPT_HAN}, {"Unicode\"", USCRIPT_LATIN}});
 }
 
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens1) {
+  CHECK_SCRIPT_RUNS({{"「あ", USCRIPT_HIRAGANA},
+                     // The consecutive punctuation should not be split.
+                     {"国。」", USCRIPT_HAN}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens2) {
+  CHECK_SCRIPT_RUNS({{"あ「あ", USCRIPT_HIRAGANA},
+                     // The consecutive punctuation should not be split.
+                     {"国（国）」", USCRIPT_HAN}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens3) {
+  CHECK_SCRIPT_RUNS({{"国「国", USCRIPT_HAN},
+                     {"ア（", USCRIPT_HIRAGANA},
+                     {"A", USCRIPT_LATIN},
+                     // The consecutive punctuation should not be split.
+                     {"）」", USCRIPT_HIRAGANA}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens4) {
+  CHECK_SCRIPT_RUNS({{"A", USCRIPT_LATIN},
+                     // CJK puncutuation after non-CJK resolves to Bopomofo,
+                     // because it's the first script extension in the Unicode
+                     // data. It's not correct but ok because GPOS/GSUB in CJK
+                     // fonts usually include the same features for all CJK
+                     // scripts including Bopomofo, even when they are not
+                     // intended for Traditional Chinese.
+                     {"「", USCRIPT_BOPOMOFO},
+                     {"A", USCRIPT_LATIN},
+                     {"あ（", USCRIPT_HIRAGANA},
+                     // The consecutive punctuation should not be split.
+                     {"国）」", USCRIPT_HAN}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens5) {
+  CHECK_SCRIPT_RUNS({{"「あ", USCRIPT_HIRAGANA},
+                     {"国", USCRIPT_HAN},
+                     {"A", USCRIPT_LATIN},
+                     {"」", USCRIPT_HIRAGANA}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens6) {
+  CHECK_SCRIPT_RUNS({{"A", USCRIPT_LATIN},
+                     {"「", USCRIPT_BOPOMOFO},  // See CJKConsecutiveParens4
+                     {"A", USCRIPT_LATIN},
+                     {"あ（", USCRIPT_HIRAGANA},
+                     {"国）", USCRIPT_HAN},
+                     {"A", USCRIPT_LATIN},
+                     {"」", USCRIPT_BOPOMOFO}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens7) {
+  CHECK_SCRIPT_RUNS({
+      {"「あ", USCRIPT_HIRAGANA},
+      {"国1」", USCRIPT_HAN},
+  });
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens8) {
+  CHECK_SCRIPT_RUNS({
+      {"A", USCRIPT_LATIN},
+      {"「", USCRIPT_BOPOMOFO},  // See CJKConsecutiveParens4
+      {"A", USCRIPT_LATIN},
+      {"あ（", USCRIPT_HIRAGANA},
+      {"国）1」", USCRIPT_HAN},
+  });
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens9) {
+  CHECK_SCRIPT_RUNS({{"「あ", USCRIPT_HIRAGANA},
+                     {"国", USCRIPT_HAN},
+                     {"A1", USCRIPT_LATIN},
+                     {"」", USCRIPT_HIRAGANA}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens10) {
+  CHECK_SCRIPT_RUNS({{"A", USCRIPT_LATIN},
+                     {"「", USCRIPT_BOPOMOFO},  // See CJKConsecutiveParens4
+                     {"A", USCRIPT_LATIN},
+                     {"あ（", USCRIPT_HIRAGANA},
+                     {"国）", USCRIPT_HAN},
+                     {"A1", USCRIPT_LATIN},
+                     {"」", USCRIPT_BOPOMOFO}});
+}
+
 // Emojies are resolved to the leading script.
 TEST_F(ScriptRunIteratorTest, EmojiCommon) {
   CHECK_SCRIPT_RUNS({{"百家姓🌱🌲🌳🌴", USCRIPT_HAN}});
