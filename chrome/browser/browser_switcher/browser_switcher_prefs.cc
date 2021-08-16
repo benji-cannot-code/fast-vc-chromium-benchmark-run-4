83fd43fbd7ac09da2acdaaeb02539bb9f1177e7e
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-08-16 18:53:40 +0000

    [BrowserSwitcher] Improve IESiteListMode
    Improve the way IESiteListMode parses rules to more closely match how
    IE/Edge does it. Instead of using string searches, etc., LBS will now
    parse the rule as a URL, and split it into multiple parts. When
    matching, it compares those parts one-by-one with the parts of the URL.
    
    Rules in memory are now represented as Rule objects, rather than plain
    strings. These rules use polymorphism to match URLs differently, based
    on ParsingMode.
    
    Bug: 1229297
    Change-Id: I9e6ab5b82fdb317487ec5af24ea5ffc40a90ea53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3028410
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912271}

--- chrome/browser/browser_switcher/browser_switcher_prefs.cc
+++ chrome/browser/browser_switcher/browser_switcher_prefs.cc
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/files/file_path.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
@@ -46,8 +49,26 @@ void SetCachedRules(PrefService* prefs,
 
 }  // namespace
 
+NoCopyUrl::NoCopyUrl(const GURL& original) : original_(original) {
+  spec_without_port_ = original_.spec();
+
+  int int_port = original_.IntPort();
+  std::string port_suffix;
+  if (int_port != url::PORT_UNSPECIFIED) {
+    port_suffix = base::StrCat({":", base::NumberToString(int_port)});
+    base::ReplaceSubstringsAfterOffset(&spec_without_port_, 0, port_suffix,
+                                       base::StringPiece());
+  }
+
+  host_and_port_ = base::StrCat({original.host(), port_suffix});
+}
+
+Rule::Rule(base::StringPiece original_rule)
+    : priority_(original_rule.size()),
+      inverted_(base::StartsWith(original_rule, "!")) {}
+
 RuleSet::RuleSet() = default;
-RuleSet::RuleSet(const RuleSet&) = default;
+RuleSet::RuleSet(RuleSet&&) = default;
 RuleSet::~RuleSet() = default;
 
 BrowserSwitcherPrefs::BrowserSwitcherPrefs(Profile* profile)
@@ -286,6 +307,7 @@ void BrowserSwitcherPrefs::AlternativeBrowserParametersChanged() {
 }
 
 void BrowserSwitcherPrefs::ParsingModeChanged() {
+  ParsingMode old_parsing_mode = parsing_mode_;
   parsing_mode_ =
       static_cast<ParsingMode>(prefs_->GetInteger(prefs::kParsingMode));
   if (parsing_mode_ < ParsingMode::kDefault ||
@@ -294,6 +316,12 @@ void BrowserSwitcherPrefs::ParsingModeChanged() {
                  << static_cast<int>(parsing_mode_)
                  << ". Falling back to 'Default' parsing mode.";
     parsing_mode_ = ParsingMode::kDefault;
+  }
+
+  if (parsing_mode_ != old_parsing_mode) {
+    // Parsing mode just changed, re-canonicalize rules.
+    UrlListChanged();
+    GreylistChanged();
   }
 }
 
@@ -309,9 +337,10 @@ void BrowserSwitcherPrefs::UrlListChanged() {
 
   bool has_wildcard = false;
   for (const auto& url : prefs_->GetList(prefs::kUrlList)->GetList()) {
-    std::string canonical = url.GetString();
-    CanonicalizeRule(&canonical);
-    rules_.sitelist.push_back(std::move(canonical));
+    std::unique_ptr<Rule> rule =
+        CanonicalizeRule(url.GetString(), parsing_mode_);
+    if (rule)
+      rules_.sitelist.push_back(std::move(rule));
     if (url.GetString() == "*")
       has_wildcard = true;
   }
@@ -332,9 +361,10 @@ void BrowserSwitcherPrefs::GreylistChanged() {
 
   bool has_wildcard = false;
   for (const auto& url : prefs_->GetList(prefs::kUrlGreylist)->GetList()) {
-    std::string canonical = url.GetString();
-    CanonicalizeRule(&canonical);
-    rules_.greylist.push_back(std::move(canonical));
+    std::unique_ptr<Rule> rule =
+        CanonicalizeRule(url.GetString(), parsing_mode_);
+    if (rule)
+      rules_.greylist.push_back(std::move(rule));
     if (url.GetString() == "*")
       has_wildcard = true;
   }
--- chrome/browser/browser_switcher/browser_switcher_sitelist.h
+++ chrome/browser/browser_switcher/browser_switcher_sitelist.h
@@ -17,9 +17,12 @@ namespace browser_switcher {
 class BrowserSwitcherPrefs;
 class ParsedXml;
 
-// Pre-processes the URL rule and modifies it in-place if needed, to avoid
-// having to convert uppercase/lowercase for every rule at every navigation.
-void CanonicalizeRule(std::string* rule);
+// Return a Rule object for |original_rule|, with the right type depending on
+// |parsing_mode|.
+//
+// Returns nullptr if |original_rule| is not a valid rule.
+std::unique_ptr<Rule> CanonicalizeRule(base::StringPiece original_rule,
+                                       ParsingMode parsing_mode);
 
 enum Action {
   kStay = 0,
@@ -40,7 +43,7 @@ enum Reason {
 };
 
 struct Decision {
-  Decision(Action, Reason, base::StringPiece matching_rule);
+  Decision(Action, Reason, const Rule* matching_rule);
 
   Decision();
   Decision(Decision&);
@@ -52,7 +55,7 @@ struct Decision {
   Reason reason;
   // If reason is kSitelist or kGreylist, this is the rule that caused the
   // decision.
-  base::StringPiece matching_rule;
+  const Rule* matching_rule;
 };
 
 // Interface that decides whether a navigation should trigger a browser
@@ -92,7 +95,7 @@ class BrowserSwitcherSitelist {
 // switch.
 class BrowserSwitcherSitelistImpl : public BrowserSwitcherSitelist {
  public:
-  explicit BrowserSwitcherSitelistImpl(const BrowserSwitcherPrefs* prefs);
+  explicit BrowserSwitcherSitelistImpl(BrowserSwitcherPrefs* prefs);
   ~BrowserSwitcherSitelistImpl() override;
 
   BrowserSwitcherSitelistImpl(const BrowserSwitcherSitelistImpl&) = delete;
@@ -113,10 +116,27 @@ class BrowserSwitcherSitelistImpl : public BrowserSwitcherSitelist {
 
   Decision GetDecisionImpl(const GURL& url) const;
 
+  // Stores the rules from |src| in |dst|, by first calling CanonicalizeRule()
+  // on them.
+  void StoreRules(std::vector<std::unique_ptr<Rule>>* dst,
+                  const std::vector<std::string>& src);
+
+  // CanonicalizeRule() has different output based on ParsingMode, so we need to
+  // re-canonicalize them if the parsing mode changes.
+  void OnPrefsChanged(BrowserSwitcherPrefs* prefs,
+                      const std::vector<std::string>& changed_prefs);
+
   RuleSet ieem_sitelist_;
   RuleSet external_sitelist_;
 
-  const BrowserSwitcherPrefs* const prefs_;
+  // Original values used for canonicalization.
+  std::vector<std::string> original_ieem_sitelist_;
+  std::vector<std::string> original_external_sitelist_;
+  std::vector<std::string> original_external_greylist_;
+
+  base::CallbackListSubscription prefs_changed_subscription_;
+
+  BrowserSwitcherPrefs* const prefs_;
 };
 
 }  // namespace browser_switcher
