e737dcfd1a1c072b8a5c15e5767b2da43afb3313
jlebel <jlebel@chromium.org>
2021-08-16 18:22:44 +0000

    [iOS] Use ChromeAccountManagerService API for the signin infobar
    Get the identity avatars from ChromeAccountManagerService in class
    SigninNotificationInfoBarDelegate
    
    Bug: 1237022
    Change-Id: I3a90723201c3b8963c64210ad4a17c24e06af7f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088865
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Jérôme <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912259}

--- ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.mm
+++ ios/chrome/browser/ui/authentication/signin_notification_infobar_delegate.mm
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service.h"
+#import "ios/chrome/browser/signin/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/ui/commands/application_commands.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
@@ -34,13 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-
-// Avatar profile picture size.
-const CGFloat kAvatarImageDimension = 40.0f;
-
-}  // namespace
 
 // static
 bool SigninNotificationInfoBarDelegate::Create(
@@ -70,13 +65,11 @@ SigninNotificationInfoBarDelegate::SigninNotificationInfoBarDelegate(
   ChromeIdentity* identity =
       auth_service->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
 
-  UIImage* image = ios::GetChromeBrowserProvider()
-                       .GetChromeIdentityService()
-                       ->GetCachedAvatarForIdentity(identity);
-  if (!image) {
-    image = [UIImage imageNamed:@"ios_default_avatar"];
-  }
-  icon_ = gfx::Image(CircularImageFromImage(image, kAvatarImageDimension));
+  ChromeAccountManagerService* accountManagerService =
+      ChromeAccountManagerServiceFactory::GetForBrowserState(browser_state);
+  UIImage* image = accountManagerService->GetIdentityAvatarWithIdentity(
+      identity, IdentityAvatarSize::DefaultLarge);
+  icon_ = gfx::Image(CircularImageFromImage(image, image.size.width));
 
   if (base::FeatureList::IsEnabled(
           signin::kSigninNotificationInfobarUsernameInTitle)) {
