df97b05619219663540b219e25d58637f2ac3a96
Mike Dougherty <michaeldo@chromium.org>
2021-08-16 18:23:49 +0000

    Reland "Add metric and log to track windowID injection time."
    This is a reland of 061230de5049587258c1bebc72874d340b1b5242
    
    Original change's description:
    > Add metric and log to track windowID injection time.
    >
    > Change-Id: I3be213e93c9f53d14631ac54a6e70567a4da2289
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072699
    > Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    > Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    > Reviewed-by: Ali Juma <ajuma@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#912180}
    
    Change-Id: I7bf9cfb4c06f87ba6bb7fb8dfd2fbb7b99841982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097298
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912261}

--- ios/web/js_messaging/crw_js_window_id_manager.mm
+++ ios/web/js_messaging/crw_js_window_id_manager.mm
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 
 #include "base/dcheck_is_on.h"
+#include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/time/time.h"
 #include "crypto/random.h"
 #import "ios/web/js_messaging/page_script_util.h"
 
@@ -24,6 +27,11 @@ namespace {
 const size_t kUniqueKeyLength = 16;
 
 #if DCHECK_IS_ON()
+// The time in seconds which is determined to be a long wait for the injection
+// of the window ID. The wait time will be logged if the time exceeds this
+// value.
+const double kSignificantInjectionTime = 0.1;
+
 // Returns whether |error| represents a failure to execute JavaScript due to
 // JavaScript execution being disallowed.
 bool IsJavaScriptExecutionProhibitedError(NSError* error) {
@@ -62,6 +70,10 @@ bool IsJavaScriptExecutionProhibitedError(NSError* error) {
 }
 
 - (void)inject {
+  [self injectWithStartTime:base::TimeTicks::Now()];
+}
+
+- (void)injectWithStartTime:(base::TimeTicks)startTime {
   _windowID = [[self class] newUniqueKey];
   NSString* script = [web::GetPageScript(@"window_id")
       stringByReplacingOccurrencesOfString:@"$(WINDOW_ID)"
@@ -102,7 +114,16 @@ bool IsJavaScriptExecutionProhibitedError(NSError* error) {
                  // WKUserScript has not been injected yet. Retry window id
                  // injection, because it is critical for the system to
                  // function.
-                 [strongSelf inject];
+                 [strongSelf injectWithStartTime:startTime];
+               } else {
+                 base::TimeDelta elapsed = base::TimeTicks::Now() - startTime;
+#if DCHECK_IS_ON()
+                 DLOG_IF(WARNING,
+                         elapsed.InSecondsF() > kSignificantInjectionTime)
+                     << "Elapsed time for windowID injection: " << elapsed;
+#endif
+                 UMA_HISTOGRAM_TIMES("IOS.WindowIDInjection.ElapsedTime",
+                                     elapsed);
                }
              }];
 }
