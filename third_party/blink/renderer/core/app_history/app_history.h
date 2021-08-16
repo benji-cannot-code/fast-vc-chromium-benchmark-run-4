9f0502c9dc69a3928f4d26bd6a3c9b38bbd6a62a
Nate Chapin <japhet@chromium.org>
2021-08-16 19:32:44 +0000

    Store both a ScriptPromise and ScriptPromiseResolver on OngoingNavigation
    ScriptPromiseResolver assumes that its associated promise will be
    returned, and resolving the promise will happen later. It is not designed
    to resolve before the promise is returned, and clears its reference to
    the promise on resolve. By immediately grabbing the promise from the
    resolver, we can clean up several small messes where AppHistory had to
    work around this limitation.
    
    This also makes it possible to synchronously fire the navigaterror
    event, per issue 1237515.
    
    Finally, we can now reject appHistory.goTo() promises on frame detach.
    This allows us to pass
    external/wpt/app-history/navigate/goTo-detach-in-onnavigate.html,
    which was timing out before.
    
    Bug: 1237515, 1219750
    Change-Id: I104a473f847d67c9251af18609bb8626ee594eb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089789
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912293}

--- third_party/blink/renderer/core/app_history/app_history.h
+++ third_party/blink/renderer/core/app_history/app_history.h
@@ -109,6 +109,8 @@ class CORE_EXPORT AppHistory final : public EventTargetWithInlineData,
   void PopulateKeySet();
   void FinalizeWithAbortedNavigationError(ScriptState*,
                                           AppHistoryApiNavigation*);
+  void RejectPromiseAndFireNavigateErrorEvent(AppHistoryApiNavigation*,
+                                              ScriptValue);
 
   ScriptPromise PerformNonTraverseNavigation(
       ScriptState*,
