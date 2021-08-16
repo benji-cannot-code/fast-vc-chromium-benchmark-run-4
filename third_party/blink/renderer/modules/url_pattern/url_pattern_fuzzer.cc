30c1970af1fd607536bc1e73bccdfe6401ade2b2
Ben Kelly <wanderview@chromium.org>
2021-08-16 19:02:22 +0000

    URLPattern: Limit fuzzer to 4kb inputs.
    This sets both the max_len build option and also provides a strict
    check in the test itself as recommend by:
    
    https://chromium.googlesource.com/chromium/src/testing/libfuzzer/+/HEAD/getting_started.md#common-tricks
    
    Bug: 1239725,1238771
    Change-Id: I0692c62d45bda3d8909a1e221bf4f9bb5d982107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097860
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912275}

--- third_party/blink/renderer/modules/url_pattern/url_pattern_fuzzer.cc
+++ third_party/blink/renderer/modules/url_pattern/url_pattern_fuzzer.cc
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size > 4096)
+    return 0;
+
   static BlinkFuzzerTestSupport test_support = BlinkFuzzerTestSupport();
   DummyExceptionStateForTesting exception_state;
   auto* input = MakeGarbageCollected<V8URLPatternInput>(
