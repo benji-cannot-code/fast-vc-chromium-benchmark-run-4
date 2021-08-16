20d1922c6a227a5c9c30c37ab5c2312f7a55e63d
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-08-16 03:20:47 +0000

    Extract compat mode button handling from ArcRLM
    This CL extracts compat mode button-related handling from ArcRLM to the
    new CompatModeButtonController.
    This refactor reduces the chance of merge conflict and makes it easier
    to improve unittest coverage.
    
    BUG=b:191956214
    TEST=CompatModeButtonControllerTest
    TEST=existing unittest pass
    
    Change-Id: I11fa742431a10b6c760d79d4fb8a42b5879f8fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3034727
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912053}

---
+++ components/arc/compat_mode/compat_mode_button_controller_unittest.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/arc/compat_mode/compat_mode_button_controller.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/views/test/views_test_base.h"
+
+namespace arc {
+
+class CompatModeButtonControllerTest : public views::ViewsTestBase {
+ private:
+  CompatModeButtonController controller_;
+};
+
+TEST_F(CompatModeButtonControllerTest, ConstructDestruct) {}
+
+// TODO(b/191956214): Add more test cases.
+
+}  // namespace arc
