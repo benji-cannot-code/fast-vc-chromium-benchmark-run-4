78b8997ecb1717dbccd7f9d3c2ffb3ffd6c01c0b
Jian Li <jianli@chromium.org>
2021-08-16 22:34:08 +0000

    Check if current stream is null before refreshing it
    Current stream may be NULL when the feed is disabled or the surface
    is destroyed.
    
    Bug: 1239932
    Change-Id: Ic97fca01db271cda2d4ab1ccc10cd38ef234dda2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097096
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912355}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -1088,7 +1088,11 @@ public class FeedSurfaceMediator
     }
 
     public void manualRefresh(Callback<Boolean> callback) {
-        mCurrentStream.triggerRefresh(callback);
+        if (mCurrentStream != null) {
+            mCurrentStream.triggerRefresh(callback);
+        } else {
+            callback.onResult(false);
+        }
     }
 
     void onOverviewShownAtLaunch(long activityCreationTimeMs, boolean isInstantStart) {
