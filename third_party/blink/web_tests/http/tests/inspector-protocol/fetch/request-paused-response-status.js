343389b00b6d74cf624b4235a1e9cf8179a7696d
Yury Semikhatsky <yurys@chromium.org>
2021-08-16 20:03:33 +0000

    DevTools: add response status text to Fetch.pausedRequest
    In addition to status line send response status text to the client.
    
    Bug: none
    Change-Id: I30854068577f99348030ec620a109297a04d5e7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094941
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912309}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/fetch/request-paused-response-status.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {session, dp} = await testRunner.startBlank(
+    `Test that Fetch.requestPaused at reponse stage contains response status text`);
+
+  await dp.Runtime.enable();
+  await dp.Network.enable();
+  await dp.Fetch.enable({patterns: [{requestStage: 'Response'}]});
+
+  const navigatePromise = session.navigate(testRunner.url('./resources/hello-world.html'))
+  dp.Fetch.onRequestPaused(async event => {
+    const params = {
+      requestId: event.params.requestId,
+    };
+    testRunner.log(`Intercepted response status code: ${event.params.responseStatusCode}`);
+    testRunner.log(`Intercepted response status text: '${event.params.responseStatusText}'`);
+    await dp.Fetch.continueRequest(params);
+  });
+  const [response] = await Promise.all([
+    dp.Network.onceResponseReceived(),
+    navigatePromise
+  ]);
+  testRunner.log(`Finished navigation`);
+  testRunner.log(`Response status text: '${response.params.response.statusText}'`);
+  testRunner.completeTest();
+})
