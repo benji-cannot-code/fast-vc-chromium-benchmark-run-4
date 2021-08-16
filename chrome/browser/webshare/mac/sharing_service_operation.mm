04cb454ad9541ddf7bec5fc848de0ed42ccea481
Avi Drissman <avi@chromium.org>
2021-08-16 16:21:37 +0000

    Migrate uses of WebContentsObserver to WeakPtr in c/b/webshare
    Now that WebContents has weak pointer support, migrate uses of
    WebContentsObserver to use it.
    
    Bug: 952390
    Change-Id: I13fd3cdc20d34e9d68e6dc96fc6ee63d37f1dfa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093712
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912199}

--- chrome/browser/webshare/mac/sharing_service_operation.mm
+++ chrome/browser/webshare/mac/sharing_service_operation.mm
@@ -56,7 +56,7 @@ SharingServiceOperation::SharingServiceOperation(
     const GURL& url,
     std::vector<blink::mojom::SharedFilePtr> files,
     content::WebContents* web_contents)
-    : content::WebContentsObserver(web_contents),
+    : web_contents_(web_contents->GetWeakPtr()),
       title_(title),
       text_(text),
       url_(url),
@@ -69,7 +69,7 @@ void SharingServiceOperation::Share(
   callback_ = std::move(callback);
 
   Profile* const profile =
-      Profile::FromBrowserContext(web_contents()->GetBrowserContext());
+      Profile::FromBrowserContext(web_contents_->GetBrowserContext());
   DCHECK(profile);
 
   // File sharing is denied in incognito, as files are written to disk.
@@ -79,8 +79,8 @@ void SharingServiceOperation::Share(
   if (profile->IsIncognitoProfile() && !shared_files_.empty()) {
     // Random number of seconds in the range [1.0, 2.0).
     double delay_seconds = 1.0 + 1.0 * base::RandDouble();
-    VisibilityTimerTabHelper::CreateForWebContents(web_contents());
-    VisibilityTimerTabHelper::FromWebContents(web_contents())
+    VisibilityTimerTabHelper::CreateForWebContents(web_contents_.get());
+    VisibilityTimerTabHelper::FromWebContents(web_contents_.get())
         ->PostTaskAfterVisibleDelay(
             FROM_HERE,
             base::BindOnce(std::move(callback_),
@@ -91,13 +91,13 @@ void SharingServiceOperation::Share(
 
   if (shared_files_.size() == 0) {
     GetSharePickerCallback().Run(
-        web_contents(), file_paths_, text_, title_, url_,
+        web_contents_.get(), file_paths_, text_, title_, url_,
         base::BindOnce(&SharingServiceOperation::OnShowSharePicker,
                        weak_factory_.GetWeakPtr()));
     return;
   }
 
-  BrowserContext* browser_context = web_contents()->GetBrowserContext();
+  BrowserContext* browser_context = web_contents_->GetBrowserContext();
   StoragePartition* const partition =
       browser_context->GetDefaultStoragePartition();
   directory_ = partition->GetPath().Append(kWebShareDirname);
@@ -118,7 +118,7 @@ void SharingServiceOperation::SetSharePickerCallbackForTesting(
 
 void SharingServiceOperation::OnPrepareDirectory(
     blink::mojom::ShareError error) {
-  if (!web_contents() || error != blink::mojom::ShareError::OK) {
+  if (!web_contents_ || error != blink::mojom::ShareError::OK) {
     std::move(callback_).Run(error);
     return;
   }
@@ -142,7 +142,7 @@ void SharingServiceOperation::OnPrepareDirectory(
 
 void SharingServiceOperation::OnPrepareSubDirectory(
     blink::mojom::ShareError error) {
-  if (!web_contents() || error != blink::mojom::ShareError::OK) {
+  if (!web_contents_ || error != blink::mojom::ShareError::OK) {
     std::move(callback_).Run(error);
     return;
   }
@@ -157,7 +157,7 @@ void SharingServiceOperation::OnPrepareSubDirectory(
 }
 
 void SharingServiceOperation::OnStoreFiles(blink::mojom::ShareError error) {
-  if (!web_contents() || error != blink::mojom::ShareError::OK) {
+  if (!web_contents_ || error != blink::mojom::ShareError::OK) {
     PrepareDirectoryTask::ScheduleSharedFileDeletion(
         std::move(file_paths_), base::TimeDelta::FromMinutes(0));
     std::move(callback_).Run(error);
@@ -165,7 +165,7 @@ void SharingServiceOperation::OnStoreFiles(blink::mojom::ShareError error) {
   }
 
   GetSharePickerCallback().Run(
-      web_contents(), file_paths_, text_, title_, url_,
+      web_contents_.get(), file_paths_, text_, title_, url_,
       base::BindOnce(&SharingServiceOperation::OnShowSharePicker,
                      weak_factory_.GetWeakPtr()));
 }
