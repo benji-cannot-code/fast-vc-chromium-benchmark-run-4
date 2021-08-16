28a0352c1639f238cd85149c5c6e5038d6de3e5a
Greg Thompson <grt@chromium.org>
2021-08-16 13:58:29 +0000

    Wait briefly in case of failure before overwriting DisplayVersion.
    When updating the MSI DisplayVersion, wait 10 seconds (the old behavior)
    in case of failure when waiting for the _MSIExecute mutex.
    
    Bug: 1028510
    Change-Id: I2707c8b319731bd375b6d122681a0df96311ed84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097511
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912157}

--- chrome/installer/setup/setup_main.cc
+++ chrome/installer/setup/setup_main.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "base/version.h"
@@ -204,7 +205,12 @@ LONG OverwriteDisplayVersionsAfterMsiexec(const std::wstring& product,
 
     auto wait_result = ::WaitForSingleObject(msi_handle.Get(), 60 * 1000);
     if (wait_result == WAIT_FAILED) {
-      PLOG(ERROR) << "Failed to wait for MSI mutex";
+      // The handle is valid and was opened with SYNCHRONIZE, so the wait should
+      // never fail. If it does, wait ten seconds and proceed with the overwrite
+      // to match the old behavior.
+      PLOG(ERROR) << "Overwriting DisplayVersion in 10s after failing to wait "
+                     "for the MSI mutex";
+      base::PlatformThread::Sleep(base::TimeDelta::FromSeconds(10));
     } else if (wait_result == WAIT_ABANDONED || wait_result == WAIT_OBJECT_0) {
       VLOG(1) << "Acquired MSI mutex; overwriting DisplayVersion.";
       acquired_mutex = true;
