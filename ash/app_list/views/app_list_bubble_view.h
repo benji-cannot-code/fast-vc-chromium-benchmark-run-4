7e0350712bd28a51407038a5b2f044166f0a2e4e
James Cook <jamescook@chromium.org>
2021-08-16 23:10:24 +0000

    Convert AppListBubbleView to a standard views::View and Widget
    AppListBubbleView doesn't need most of the "bubble dialog" aspects of
    BubbleDialogDelegateView. Convert it to views::Widget (with a default
    delegate) and views::View. This eliminates the need to hide shadows,
    hide buttons, set the background transparent, etc.
    
    Update the padding between the bubble and the work area edges to meet
    the spec (since I have to compute the bubble bounds in this CL).
    
    Before: https://screenshot.googleplex.com/6ALUsK9BHzCnJXg
    After: https://screenshot.googleplex.com/B7avagDJ9Zx9fqS
    
    Bug: 1239824
    Test: covered by existing ash_unittests for bubble size/position
    Change-Id: I47f0d98859e60e1b9edfe75ff0c3c0e273752e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093504
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912373}

--- ash/app_list/views/app_list_bubble_view.h
+++ ash/app_list/views/app_list_bubble_view.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/search_box/search_box_view_delegate.h"
-#include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/view.h"
 
 namespace aura {
 class Window;
@@ -24,20 +24,22 @@ class SearchBoxView;
 enum class ShelfAlignment;
 
 // Contains the views for the bubble version of the launcher. It looks like a
-// system tray bubble. It derives from BubbleDialogDelegateView instead of
-// TrayBubbleView because it takes focus by default, uses a different
-// EventHandler for closing, and isn't tied to the system tray area.
-class ASH_EXPORT AppListBubbleView : public views::BubbleDialogDelegateView,
+// system tray bubble. It does not derive from TrayBubbleView because it takes
+// focus by default, uses a different EventHandler for closing, and isn't tied
+// to the system tray area.
+class ASH_EXPORT AppListBubbleView : public views::View,
                                      public SearchBoxViewDelegate {
  public:
-  // Creates the bubble on the display for `root_window`. Anchors the bubble to
-  // a corner of the screen based on `shelf_alignment`.
+  // Creates the bubble on the display for `root_window`.
   AppListBubbleView(AppListViewDelegate* view_delegate,
-                    aura::Window* root_window,
-                    ShelfAlignment shelf_alignment);
+                    aura::Window* root_window);
   AppListBubbleView(const AppListBubbleView&) = delete;
   AppListBubbleView& operator=(const AppListBubbleView&) = delete;
   ~AppListBubbleView() override;
+
+  // Returns the bounds for the bubble widget in root window coordinates.
+  // TODO(jamescook): Move to AppListBubblePresenter.
+  gfx::Rect GetBubbleBounds() const;
 
   // Handles back action if it we have a use for it besides dismissing.
   bool Back();
@@ -71,6 +73,7 @@ class ASH_EXPORT AppListBubbleView : public views::BubbleDialogDelegateView,
   friend class AssistantTestApiImpl;
 
   AppListViewDelegate* const view_delegate_;
+  aura::Window* const root_window_;
   SearchBoxView* search_box_view_ = nullptr;
   AppListBubbleAppsPage* apps_page_ = nullptr;
   AppListBubbleSearchPage* search_page_ = nullptr;
