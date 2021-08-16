f9b214905ae014a4362b01d38d2ee761b97e7a7d
David Bertoni <dbertoni@chromium.org>
2021-08-16 16:56:24 +0000

    [Extensions] Fix the EventsAfterRestart test.
    This test was flaky because the PRE_ step didn't wait for the
    service worker's registration to be stored before returning.
    
    In addition, this CL adds more information to the log statement
    in ServiceWorkerTaskQueue::DidStartWorkerFail() for future
    debugging.
    
    Patchset 2 shows a run of the two flaky platforms before the fix,
    while patchset 3 shows a run after the fix. There was only one
    unrelated flake in 3000 runs after the fix.
    
    Bug: 1236184
    Change-Id: Idfe5c32186a77accbf4528fabe5cc2ab1ecf28ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3092999
    Reviewed-by: Ghazale Hosseinabadi <ghazale@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912214}

--- extensions/browser/service_worker_task_queue.cc
+++ extensions/browser/service_worker_task_queue.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/service_worker_task_queue.h"
 
 #include <memory>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -207,7 +208,10 @@ void ServiceWorkerTaskQueue::DidStartWorkerFail(
   // TODO(https://crbug/1062936): Needs more thought: extension would be in
   // perma-broken state after this as the registration wouldn't be stored if
   // this happens.
-  LOG(ERROR) << "DidStartWorkerFail " << context_id.first.extension_id();
+  LOG(ERROR)
+      << "DidStartWorkerFail " << context_id.first.extension_id() << ": "
+      << static_cast<std::underlying_type_t<blink::ServiceWorkerStatusCode>>(
+             status_code);
 }
 
 void ServiceWorkerTaskQueue::DidInitializeServiceWorkerContext(
