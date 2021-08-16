8bd3101ae9c211349f950553460367fd247b924a
Avi Drissman <avi@chromium.org>
2021-08-16 18:52:36 +0000

    Migrate uses of WebContentsObserver to WeakPtr in content/b/payments
    Now that WebContents has weak pointer support, migrate uses of
    WebContentsObserver to use it.
    
    Bug: 952390
    Change-Id: Id2ca6828c1c5943eca1eb3bce6c7d94270caacd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3095569
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912269}

--- content/browser/payments/payment_app_installer.cc
+++ content/browser/payments/payment_app_installer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/task/post_task.h"
 #include "content/browser/payments/payment_app_context_impl.h"
@@ -21,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/service_worker_context_observer.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents_observer.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_registration_options.mojom.h"
 #include "url/gurl.h"
@@ -32,8 +32,7 @@ namespace {
 
 // Self deleting installer installs a web payment app and deletes itself.
 class SelfDeleteInstaller
-    : public WebContentsObserver,
-      public ServiceWorkerContextObserver,
+    : public ServiceWorkerContextObserver,
       public base::RefCountedThreadSafe<SelfDeleteInstaller> {
  public:
   SelfDeleteInstaller(const std::string& app_name,
@@ -61,9 +60,9 @@ class SelfDeleteInstaller
 
     AddRef();  // Balanced by Release() in FinishInstallation.
 
-    // TODO(crbug.com/782270): Listen for web contents events to terminate
-    // installation early.
-    Observe(web_contents);
+    // TODO: Switch to being a WebContentsObserver and listen for events to
+    // terminate installation early?
+    web_contents_ = web_contents->GetWeakPtr();
 
     use_cache_ = use_cache;
 
@@ -159,14 +158,14 @@ class SelfDeleteInstaller
  private:
   friend class base::RefCountedThreadSafe<SelfDeleteInstaller>;
 
-  ~SelfDeleteInstaller() override {}
+  ~SelfDeleteInstaller() override = default;
 
   // If web contents or browser context are gone, then aborts payment and
   // returns true. Should be called on UI thread.
   bool AbortInstallIfWebContentsOrBrowserContextIsGone() {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-    if (!web_contents() || !web_contents()->GetBrowserContext()) {
+    if (!web_contents_ || !web_contents_->GetBrowserContext()) {
       FinishInstallation(false);
       return true;
     }
@@ -176,11 +175,11 @@ class SelfDeleteInstaller
 
   void SetPaymentAppIntoDatabase() {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    DCHECK(web_contents());
-    DCHECK(web_contents()->GetBrowserContext());
+    DCHECK(web_contents_);
+    DCHECK(web_contents_->GetBrowserContext());
 
     StoragePartitionImpl* partition = static_cast<StoragePartitionImpl*>(
-        web_contents()->GetBrowserContext()->GetDefaultStoragePartition());
+        web_contents_->GetBrowserContext()->GetDefaultStoragePartition());
     scoped_refptr<PaymentAppContextImpl> payment_app_context =
         partition->GetPaymentAppContext();
 
@@ -237,10 +236,10 @@ class SelfDeleteInstaller
 
     service_worker_context_->RemoveObserver(this);
 
-    Observe(nullptr);
     Release();  // Balanced by AddRef() in the constructor.
   }
 
+  base::WeakPtr<content::WebContents> web_contents_;
   std::string app_name_;
   std::string app_icon_;
   GURL sw_url_;
