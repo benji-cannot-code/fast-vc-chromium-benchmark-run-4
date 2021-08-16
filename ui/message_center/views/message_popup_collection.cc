6b885a4ea6f6ead4a454711df3786fcf6f6b81b7
Ahmed Mehfooz <amehfooz@chromium.org>
2021-08-16 20:15:58 +0000

    Reland: "Implement notificication view controller"
    This reverts: commit abb1f267a45f669785963f664b90d22024224b78
    
    Reason for revert: Relanding without the unit test that seems to be
    triggerring the flaky test NotificationViewMDTest.TestInlineReply to
    fail deterministically.
    
    Original change's description:
    > Revert "Implement notification view controller"
    
    > This reverts commit 23cef48abd0834c5b5aee98d4fc3bda9312d6abf.
    
    > Reason for revert: Suspected to be breaking https://ci.chromium.org/p/chromium/builders/ci/linux-lacros-tester-rel & https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tester%20%28Ozone%20Wayland%29 : NotificationViewMDTest.TestInlineReply
    
    > Original change's description:
    > > Implement notification view controller
    > >
    > > This CL adds support for adding and removing grouped
    > > notifications. We create an extra notification to house
    > > all group notifications. This is done by converting
    > > the first notification view that needs to be grouped
    > > into this container and then adding that notification as
    > > a grouped child notification within that view. We do this
    > > to make animating between converting grouped to non-grouped
    > > notifications and vice-versa.
    > >
    > > Currently added a hack to view the notifications in
    > > NotificationViewMD for dogfood purposes.
    > >
    > > Bug:1223697
    > >
    > > Change-Id: I40aa97a2cdc13e316fa3df7628cac66bfd660d25
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3040300
    > > Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    > > Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    > > Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#909088}
    
    > Bug: 1223697
    > Change-Id: Ia8f7738a804be238f8023d9f0f422de52acb30d5
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077220
    > Auto-Submit: Melissa Zhang <melzhang@chromium.org>
    > Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    > Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    > Owners-Override: Melissa Zhang <melzhang@chromium.org>
    > Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/master@{#909157}
    
    Bug: 1223697
    Change-Id: I7f24343d5edd16e46ad4a4a3c446683c5ef040ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3082665
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912316}

--- ui/message_center/views/message_popup_collection.cc
+++ ui/message_center/views/message_popup_collection.cc
@@ -19,6 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/views/message_view.h"
 #include "ui/message_center/views/notification_view_md.h"
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/constants/ash_features.h"
+#endif
 namespace message_center {
 
 namespace {
@@ -139,9 +142,25 @@ MessageView* MessagePopupCollection::GetMessageViewForNotificationId(
   return it->popup->message_view();
 }
 
+void MessagePopupCollection::ConvertNotificationViewToGroupedNotificationView(
+    const std::string& ungrouped_notification_id,
+    const std::string& new_grouped_notification_id) {
+  auto it = std::find_if(
+      popup_items_.begin(), popup_items_.end(),
+      [&](auto popup) { return popup.id == ungrouped_notification_id; });
+  if (it == popup_items_.end())
+    return;
+
+  it->id = new_grouped_notification_id;
+  it->popup->message_view()->set_notification_id(new_grouped_notification_id);
+}
+
 void MessagePopupCollection::OnNotificationAdded(
     const std::string& notification_id) {
-  NotificationViewController::OnNotificationAdded(notification_id);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (ash::features::IsNotificationsRefreshEnabled())
+    NotificationViewController::OnNotificationAdded(notification_id);
+#endif
   // Should not call MessagePopupCollection::Update here. Because notification
   // may be removed before animation which is triggered by the previous
   // operation on MessagePopupCollection ends. As result, when a new
@@ -154,7 +173,10 @@ void MessagePopupCollection::OnNotificationAdded(
 void MessagePopupCollection::OnNotificationRemoved(
     const std::string& notification_id,
     bool by_user) {
-  NotificationViewController::OnNotificationRemoved(notification_id, by_user);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (ash::features::IsNotificationsRefreshEnabled())
+    NotificationViewController::OnNotificationRemoved(notification_id, by_user);
+#endif
 
   if (by_user) {
     recently_closed_by_user_ = true;
@@ -505,8 +527,11 @@ bool MessagePopupCollection::AddPopup() {
       return false;
     }
 
-    item.popup->Show();
     popup_items_.push_back(item);
+    if (new_notification->group_parent())
+      PopulateGroupParent(item.id);
+
+    item.popup->Show();
     NotifyPopupAdded(item.popup);
   }
 
