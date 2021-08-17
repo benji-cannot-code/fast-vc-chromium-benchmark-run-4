5eb287223dd250548811ac1e80638c19e2efa650
Jimmy Gong <jimmyxgong@google.com>
2021-08-17 23:06:46 +0000

    shortcuts: Load initial fake shortcuts
    Loads initial fake shortcuts from the fake mojo provider.
    Styling and polish will be added in a follow CL.
    
    Screenshot: http://shortn/_ZhN2misNFq
    
    MANUAL_ShortcutCustomizationApp
    
    Bug: 1060690
    Test: ShortcutCustomizationAppBrowserTest.\
    Change-Id: I3f74340779d34de3c1742bfac58495c337d1ca93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088333
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912789}

--- ash/webui/shortcut_customization_ui/resources/mojo_interface_provider.js
+++ ash/webui/shortcut_customization_ui/resources/mojo_interface_provider.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {assert} from 'chrome://resources/js/assert.m.js';
-import {fakeAcceleratorConfig} from './fake_data.js'
+import {fakeAcceleratorConfig, fakeLayoutInfo} from './fake_data.js'
 import {FakeShortcutProvider} from './fake_shortcut_provider.js'
 import {ShortcutProviderInterface} from './shortcut_types.js'
 
@@ -36,6 +36,9 @@ function setupFakeShortcutProvider() {
 
   // Setup accelerator config.
   provider.setFakeAcceleratorConfig(fakeAcceleratorConfig);
+
+  // Setup accelerator layout info.
+  provider.setFakeLayoutInfo(fakeLayoutInfo);
 
   // Set the fake provider.
   setShortcutProviderForTesting(provider);
