31825f5da07ffd28d65be251ed9fa06c277a4c5f
James Cook <jamescook@chromium.org>
2021-08-17 00:45:57 +0000

    Fix app list bubble search box focus bar painting
    When I added transparency and blur to the search box I had to switch the
    AppListBubbleView to use a layer with type SOLID_COLOR, which broke the
    blue focus bar painting. I don't think a view with a solid color layer
    can directly paint to its canvas (otherwise it wouldn't be a single
    color).
    
    Move painting to SearchBoxView. Tweak the position slightly to align
    with the search icon. (It's not clear to me from the spec what this bar
    is supposed to line up with.) The bar color is too light on the
    transparent background, but we can fix that separately, since I think
    it affects other focus rings as well.
    
    Original: https://screenshot.googleplex.com/AvjAzyNZqJoDKsJ
    Regressed: https://screenshot.googleplex.com/7KFUmMAk7fve6CG
    This CL: https://screenshot.googleplex.com/3w4QNq6RXk9Ah4X
    
    Bug: 1218229
    Change-Id: I0376b753842d86e2d33b14b0e35a33159a0fe122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098576
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912408}

--- ash/app_list/views/app_list_bubble_view.cc
+++ ash/app_list/views/app_list_bubble_view.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <memory>
 
-#include "ash/app_list/app_list_util.h"
 #include "ash/app_list/views/app_list_bubble_apps_page.h"
 #include "ash/app_list/views/app_list_bubble_search_page.h"
 #include "ash/app_list/views/assistant/app_list_bubble_assistant_page.h"
@@ -222,20 +221,6 @@ gfx::Size AppListBubbleView::CalculatePreferredSize() const {
   }
 
   return gfx::Size(default_width, height);
-}
-
-void AppListBubbleView::OnPaint(gfx::Canvas* canvas) {
-  // Used to draw/hide the focus bar for the search box view.
-  if (search_box_view_->search_box()->HasFocus() &&
-      search_box_view_->search_box()->GetText().empty()) {
-    PaintFocusBar(
-        canvas,
-        GetContentsBounds().origin() +
-            gfx::Vector2d(0,
-                          kUnifiedTrayCornerRadius /*downshift the focus bar*/),
-        /*height=*/kSearchBoxIconSize);
-  }
-  views::View::OnPaint(canvas);
 }
 
 void AppListBubbleView::OnThemeChanged() {
