e1a725abd74c096f05c51cdba4c64deaa94a24f1
Raymes Khoury <raymes@chromium.org>
2021-08-17 23:40:17 +0000

    Convert thread checks to CHECKs to rule out races in crbug.com/1237267
    This converts thread checks in AppRegistryCache to be CHECKs rather than
    DCHECKs to rule out concurrent read/writes.
    
    These should be removed after crbug.com/1237267 is resolved.
    
    BUG=1237267
    
    Change-Id: Ieaa0820357b529ec7bda65a5c6d3eb67e6112a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097257
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912801}

--- components/services/app_service/public/cpp/app_registry_cache.h
+++ components/services/app_service/public/cpp/app_registry_cache.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
-#include "base/sequence_checker.h"
+#include "base/sequence_checker_impl.h"
 #include "components/account_id/account_id.h"
 #include "components/services/app_service/public/cpp/app_update.h"
 
@@ -213,7 +213,9 @@ class COMPONENT_EXPORT(APP_UPDATE) AppRegistryCache {
 
   AccountId account_id_;
 
-  SEQUENCE_CHECKER(my_sequence_checker_);
+  // Use a SequenceCheckerImpl which will provide checks for prod code to rule
+  // out any threading issues triggering https://crbug.com/1237267.
+  base::SequenceCheckerImpl my_sequence_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(AppRegistryCache);
 };
