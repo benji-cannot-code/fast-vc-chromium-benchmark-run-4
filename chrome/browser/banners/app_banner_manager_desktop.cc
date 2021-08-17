727f3c78d2a39da3dfc2165ec5df0cffaf4a177e
Phillis Tang <phillis@chromium.org>
2021-08-17 23:45:02 +0000

    DPWA: override GetAppIdentifier for desktop
    Override |GetAppIdentifier| in |AppBannerManagerDesktop| to use
    |manifest.id|.
    
    Change-Id: Iaee3d9b0c71b9474fdfa3eb44dd2980d08df5773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3100561
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912803}

--- chrome/browser/banners/app_banner_manager_desktop.cc
+++ chrome/browser/banners/app_banner_manager_desktop.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/constants.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/public/common/manifest/manifest_util.h"
 #include "third_party/blink/public/mojom/manifest/display_mode.mojom.h"
 #include "third_party/blink/public/mojom/manifest/manifest.mojom.h"
 
@@ -149,6 +150,11 @@ bool AppBannerManagerDesktop::IsWebAppConsideredInstalled() const {
              Profile::FromBrowserContext(web_contents()->GetBrowserContext()),
              manifest().start_url)
       .has_value();
+}
+
+std::string AppBannerManagerDesktop::GetAppIdentifier() {
+  DCHECK(!blink::IsEmptyManifest(manifest()));
+  return web_app::GenerateAppIdUnhashedFromManifest(manifest());
 }
 
 web_app::WebAppRegistrar& AppBannerManagerDesktop::registrar() {
