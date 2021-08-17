86875941c96c9569b65c5b5b3e8db34e994fd577
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2021-08-17 00:37:01 +0000

    dbus: Add IsInterfaceRegistered to ObjectManager
    This change adds a new public method to check whether a specific
    interface is currently registered with ObjectManager. It will be used
    mainly for testing.
    
    BUG=b:193712484
    TEST=autoninja -c out/Default chrome
    
    Change-Id: I46b0ad58afd7334693393b1c9a0291bb94a58dd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089691
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912402}

--- dbus/object_manager.h
+++ dbus/object_manager.h
@@ -201,6 +201,9 @@ class CHROME_DBUS_EXPORT ObjectManager final
   // ignored.
   void UnregisterInterface(const std::string& interface_name);
 
+  // Checks whether an interface is registered.
+  bool IsInterfaceRegisteredForTesting(const std::string& interface_name) const;
+
   // Returns a list of object paths, in an undefined order, of objects known
   // to this manager.
   std::vector<ObjectPath> GetObjects();
