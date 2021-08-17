966d8631195ea7201a326e0819c93135b34fce50
Prameet Shah <phshah@google.com>
2021-08-17 23:05:34 +0000

    [Nearby Share] Add finch flag for ARC Nearby Share
    Bug: b:191309668
    Test: On eve w/ ARC P and hatch w/ ARC R, [enabled, disabled] the
        chrome://flags/#nearby-sharing-arc flag, and verified the
        Nearby Share target is [visible, hidden] in the Android
        sharesheet.
    Test: ./testing/xvfb.py ./out/Default/components_unittests \
        --gtest_filter="ArcVmClientAdapterTest*"
    Test: ./testing/xvfb.py ./out/Default/unit_tests \
        --gtest_filter=AboutFlagsHistogramTest.CheckHistograms:AboutFlagsTest.EveryFlagHasMetadata
    
    Change-Id: I39fd5929080a45f566db994f1c8af5a40e4ac040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3070498
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Auto-Submit: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Alan Ding <alanding@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912787}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2743,6 +2743,9 @@ extern const char kNearbyKeepAliveFixDescription[];
 extern const char kNearbySharingName[];
 extern const char kNearbySharingDescription[];
 
+extern const char kNearbySharingArcName[];
+extern const char kNearbySharingArcDescription[];
+
 extern const char kNearbySharingBackgroundScanningName[];
 extern const char kNearbySharingBackgroundScanningDescription[];
 
--- components/arc/arc_features.h
+++ components/arc/arc_features.h
@@ -17,6 +17,7 @@ extern const base::Feature kBootCompletedBroadcastFeature;
 extern const base::Feature kCleanArcDataOnRegularToChildTransitionFeature;
 extern const base::Feature kCustomTabsExperimentFeature;
 extern const base::Feature kDocumentsProviderUnknownSizeFeature;
+extern const base::Feature kEnableArcNearbyShare;
 extern const base::Feature kEnableChildToRegularTransitionFeature;
 extern const base::Feature kEnableRegularToChildTransitionFeature;
 extern const base::Feature kEnableThrottlingNotification;
