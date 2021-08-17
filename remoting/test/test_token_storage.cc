2f45656d4192c74fd8041dd22546d70979ee83ba
Maksim Moskvitin <mmoskvitin@google.com>
2021-08-17 18:17:35 +0000

    [CodeHealth] Replace deprecated Value methods in test_token_storage.cc
    Bug: 1187011
    Change-Id: I1fb0364695a3116e12da355b5a553ec8eec35110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099999
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912650}

--- remoting/test/test_token_storage.cc
+++ remoting/test/test_token_storage.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/values.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace {
 const base::FilePath::CharType kTokenFileName[] =
@@ -123,20 +124,19 @@ std::string TestTokenStorageOnDisk::FetchTokenFromKey(const std::string& key) {
   }
 
   absl::optional<base::Value> token_data(base::JSONReader::Read(file_contents));
-  base::DictionaryValue* tokens = nullptr;
-  if (!token_data.has_value() || !token_data->GetAsDictionary(&tokens)) {
+  if (!token_data.has_value() || !token_data->is_dict()) {
     LOG(ERROR) << "File contents were not valid JSON, "
                << "could not retrieve token.";
     return std::string();
   }
 
-  base::Value* token = tokens->FindPath({user_name_, key});
+  const std::string* token = token_data->FindStringPath(user_name_ + '.' + key);
   if (!token) {
     VLOG(1) << "Could not find token for: " << key;
     return std::string();
   }
 
-  return token->GetString();
+  return *token;
 }
 
 bool TestTokenStorageOnDisk::StoreTokenForKey(const std::string& key,
@@ -162,14 +162,13 @@ bool TestTokenStorageOnDisk::StoreTokenForKey(const std::string& key,
   }
 
   absl::optional<base::Value> token_data(base::JSONReader::Read(file_contents));
-  base::DictionaryValue* tokens = nullptr;
-  if (!token_data.has_value() || !token_data->GetAsDictionary(&tokens)) {
+  if (!token_data.has_value() || !token_data->is_dict()) {
     LOG(ERROR) << "Invalid token file format, could not store token.";
     return false;
   }
 
   std::string json_string;
-  tokens->SetPath({user_name_, key}, base::Value(value));
+  token_data->SetStringPath(user_name_ + '.' + key, value);
   if (!base::JSONWriter::Write(*token_data, &json_string)) {
     LOG(ERROR) << "Couldn't convert JSON data to string";
     return false;
