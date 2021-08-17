160cfd32c34f494cbd650b9b3b33bf88c834702f
Eric Willigers <ericwilligers@chromium.org>
2021-08-17 21:49:40 +0000

    report_client_unittest.cc includes string_number_conversions.h
    Change-Id: I895991dd1dfeaa46a6ad28fdc3ed3ccc69174def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3101625
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Owners-Override: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912755}

--- chrome/browser/policy/messaging_layer/public/report_client_unittest.cc
+++ chrome/browser/policy/messaging_layer/public/report_client_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/singleton.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/task/post_task.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
