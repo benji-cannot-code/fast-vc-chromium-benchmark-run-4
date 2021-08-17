a4495018642f87d42d7cb5211fb94992d7e02300
Mike Dougherty <michaeldo@chromium.org>
2021-08-17 05:04:56 +0000

    Cleanup large block and incorrect usages of weakSelf
    Additionally, only update the layout of the toolbar if the size classes
    have changed as recommended by the
    -[UITraitEnvironment traitCollectionDidChange:] documentation.
    
    Change-Id: I3243fb591764e3c782f8c1ce2f66cf84dd3fdb11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072840
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912479}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_bottom_toolbar.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_bottom_toolbar.mm
@@ -47,7 +47,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  [self updateLayout];
+  if ((self.traitCollection.verticalSizeClass !=
+       previousTraitCollection.verticalSizeClass) ||
+      (self.traitCollection.horizontalSizeClass !=
+       previousTraitCollection.horizontalSizeClass)) {
+    [self updateLayout];
+  }
 }
 
 // Controls hit testing of the bottom toolbar. When the toolbar is transparent,
@@ -93,6 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setMode:(TabGridMode)mode {
   if (_mode == mode)
     return;
+  DCHECK(IsTabsBulkActionsEnabled() || mode == TabGridModeNormal);
   _mode = mode;
   // Reset selected tabs count when mode changes.
   self.selectedTabsCount = 0;
@@ -328,6 +334,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       -kTabGridFloatingButtonVerticalInset;
 
   if (self.mode == TabGridModeSelection) {
+    DCHECK(IsTabsBulkActionsEnabled());
     [_toolbar setItems:@[
       _closeTabsButton, _spaceItem, _shareButton, _spaceItem, _addToButton
     ]];
