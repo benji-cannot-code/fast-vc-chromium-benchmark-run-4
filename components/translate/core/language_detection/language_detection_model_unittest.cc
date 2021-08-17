f38b5469b37b30fcac60778f0e085baef6798fb4
Sreeja Kamishetty <sreejakshetty@chromium.org>
2021-08-17 12:35:10 +0000

    [Sheriff]: Disable LanguageDetectionModelTest.UnsupportedModelFileProvided
    This test is failing on the following builders:
    
    Builder: Linux ChromiumOS MSan Tests
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests
    First failing build:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/b8838691969534247104
    --------------------
    Builder: Linux MSan Tests
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests
    First failing build:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests/b8838725175934457584
    
    Bug: 1240561
    Change-Id: Icaca7afef58a2432d283110f97e6e1c3576da09d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097547
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912543}

--- components/translate/core/language_detection/language_detection_model_unittest.cc
+++ components/translate/core/language_detection/language_detection_model_unittest.cc
@@ -56,7 +56,8 @@ TEST(LanguageDetectionModelTest, EmptyFileProvided) {
       LanguageDetectionModelState::kModelFileInvalid, 1);
 }
 
-TEST(LanguageDetectionModelTest, UnsupportedModelFileProvided) {
+// TODO(crbug.com/1240561): Fix flaky test.
+TEST(LanguageDetectionModelTest, DISABLED_UnsupportedModelFileProvided) {
   base::HistogramTester histogram_tester;
 
   base::File file = CreateInvalidModelFile();
