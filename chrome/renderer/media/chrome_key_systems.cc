3022d9d8a998bee8cb30ce34cba6828427a2acda
Nico Weber <thakis@chromium.org>
2021-08-17 18:43:44 +0000

    Enable -Wunreachable-code-aggressive in chrome-branded Windows builds
    Bug: 1066980
    Change-Id: Ibcb3757eb00c35faef3975fe75674e7607244ee7
    Cq-Include-Trybots: luci.chrome.try:win-chrome,win64-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098290
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912663}

--- chrome/renderer/media/chrome_key_systems.cc
+++ chrome/renderer/media/chrome_key_systems.cc
@@ -242,7 +242,6 @@ SupportedCodecs GetSupportedCodecs(const media::CdmCapability& capability) {
       case media::VideoCodec::kVP9:
         supported_codecs |= GetVP9Codecs(codec.second);
         break;
-        break;
       case media::VideoCodec::kAV1:
         supported_codecs |= media::EME_CODEC_AV1;
         break;
