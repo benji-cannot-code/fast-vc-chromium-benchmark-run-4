290e10561774a35c1bd734b306beb1b2cbf563d1
Alex Gough <ajgo@chromium.org>
2021-08-17 02:29:22 +0000

    Tidy includes in zip file creator browsertest
    zip_file_creator_browsertest.cc no longer needs service_sandbox_type.h
    but instead should use the .mojom.h of the interface it is starting in
    a service. This "worked" as the .mojom.h was included from another
    header.
    
    Bug: 1210301
    Change-Id: I1d6e5e23af343e789fff3b5e224f0e18c0f5ae62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098912
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912446}

--- chrome/services/file_util/public/cpp/zip_file_creator_browsertest.cc
+++ chrome/services/file_util/public/cpp/zip_file_creator_browsertest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/threading/thread_restrictions.h"
-#include "chrome/browser/service_sandbox_type.h"
+#include "chrome/services/file_util/public/mojom/file_util_service.mojom.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/service_process_host.h"
 #include "content/public/test/browser_test.h"
