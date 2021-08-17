5c90a075dc0d19aac2670e2a95a2053842100421
Pavel Yatsuk <pavely@google.com>
2021-08-17 00:06:21 +0000

    [Messages] Dismiss message immediately before hide animations
    Previously message.dismiss() was called for displayed message after the
    hide animation is finished. In reality calls to hide() and dismiss() are
    independent, one starts hide animation, another invokes consumer's
    dismiss callback.
    
    This CL makes message.dismiss() call unconditional from dismiss API.
    
    BUG=1234488
    R=lazzzis@chromium.org
    
    Change-Id: I95b346634d91ae87249e28b11a07ad201a26713e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098450
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912391}

--- components/messages/android/internal/java/src/org/chromium/components/messages/MessageQueueManager.java
+++ components/messages/android/internal/java/src/org/chromium/components/messages/MessageQueueManager.java
@@ -115,11 +115,9 @@ class MessageQueueManager implements ScopeChangeController.Delegate {
             mScopeChangeController.lastMessageDismissed(scopeKey);
         }
 
+        message.dismiss(dismissReason);
         if (mCurrentDisplayedMessage == messageState) {
-            hideMessage(updateCurrentMessage,
-                    () -> message.dismiss(dismissReason), updateCurrentMessage);
-        } else {
-            message.dismiss(dismissReason);
+            hideMessage(updateCurrentMessage, updateCurrentMessage);
         }
         MessagesMetrics.recordDismissReason(message.getMessageIdentifier(), dismissReason);
     }
@@ -181,7 +179,7 @@ class MessageQueueManager implements ScopeChangeController.Delegate {
             MessageState candidate = getNextMessage();
             // Another higher priority message has been enqueued.
             if (candidate != mCurrentDisplayedMessage || isQueueSuspended()) {
-                hideMessage(!isQueueSuspended() && animateTransition, null, !isQueueSuspended());
+                hideMessage(!isQueueSuspended() && animateTransition, !isQueueSuspended());
             }
         }
     }
@@ -201,12 +199,10 @@ class MessageQueueManager implements ScopeChangeController.Delegate {
         return mMessages;
     }
 
-    private void hideMessage(
-            boolean animate, Runnable dismissAfterHiding, boolean updateCurrentMessage) {
+    private void hideMessage(boolean animate, boolean updateCurrentMessage) {
         mCurrentDisplayedMessage.handler.hide(animate, () -> {
             mMessageQueueDelegate.onFinishHiding();
             mCurrentDisplayedMessage = null;
-            if (dismissAfterHiding != null) dismissAfterHiding.run();
             if (updateCurrentMessage) updateCurrentDisplayedMessage(true);
         });
     }
