d1a9ae2793c430a3b51623e32f981ae2eff86760
Ari Chivukula <arichiv@chromium.org>
2021-08-17 04:02:23 +0000

    Add base::Feature for Third Party Context WebSQL Deprecation
    The new feature, named (k)DeprecateThirdPartyContextWebSQL, will be used
    for the finch experiment which gates the launch of this deprecation.
    
    Bug: 1212491
    Change-Id: Ia87e11cb70f63f429c4cd02cfd7dca9934dddc16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098355
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912468}

--- third_party/blink/renderer/modules/webdatabase/dom_window_web_database.h
+++ third_party/blink/renderer/modules/webdatabase/dom_window_web_database.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBDATABASE_DOM_WINDOW_WEB_DATABASE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBDATABASE_DOM_WINDOW_WEB_DATABASE_H_
 
+#include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
@@ -38,7 +39,7 @@ class ExceptionState;
 class LocalDOMWindow;
 class V8DatabaseCallback;
 
-class DOMWindowWebDatabase {
+class MODULES_EXPORT DOMWindowWebDatabase {
   STATIC_ONLY(DOMWindowWebDatabase);
 
  public:
@@ -55,6 +56,7 @@ class DOMWindowWebDatabase {
                                 uint32_t estimated_size,
                                 V8DatabaseCallback* creation_callback,
                                 ExceptionState&);
+  static bool IsThirdPartyContextWebSQLDeprecated();
 };
 
 }  // namespace blink
