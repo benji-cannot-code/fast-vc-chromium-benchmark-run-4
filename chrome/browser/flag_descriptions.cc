170a4c32128eebb2cb98821c2c82a87571f9ffd4
Alex Ilasi <alexilasi@google.com>
2021-08-17 12:52:52 +0000

    Revert "Add flag to enable Incognito New Tab Page Real Box"
    This reverts commit 1670e87f23246203df3c7af13103fafa077e4f29.
    
    Reason for revert: This will not be used in the near future.
    
    Original change's description:
    > Add flag to enable Incognito New Tab Page Real Box
    >
    > A flag is added to control Incognito New Tab Page Real Box.
    > This is related to: go/ntp-revamp.
    >
    > Bug: 1231020
    > Change-Id: Ia8de0019f0b2f71cc340735f76ef81bdbb06e54c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3041348
    > Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > Commit-Queue: Alex Ilasi <alexilasi@google.com>
    > Cr-Commit-Position: refs/heads/master@{#904608}
    
    Bug: 1231020
    Change-Id: I6c0630b46208f5b852c6aec1a9c6891b90ac1bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097613
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Alex Ilasi <alexilasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#912547}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1443,11 +1443,6 @@ const char kUpdateHistoryEntryPointsInIncognitoDescription[] =
     "removed for iOS and Desktop. An educative placeholder will be shown for "
     "Android history page.";
 
-const char kIncognitoNtpRealBoxName[] =
-    "Incognito New Tab Page Real Search Box";
-const char kIncognitoNtpRealBoxDescription[] =
-    "When enabled, Incognito New Tap Page will display a Real Search Box.";
-
 const char kIncognitoNtpRevampName[] = "Revamped Incognito New Tab Page";
 const char kIncognitoNtpRevampDescription[] =
     "When enabled, Incognito new tab page will have an updated UI";
