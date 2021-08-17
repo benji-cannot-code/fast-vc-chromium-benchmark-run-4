f8a144d0216eb98549bfb177b74bdab51b91678f
Dana Fried <dfried@chromium.org>
2021-08-17 22:39:18 +0000

    Add icon to IPH bubbles.
    Exact layout tbd.
    
    Bug: 1240322
    Change-Id: I61191fff4eb9230b300a1e8621263844496b7d59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098453
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912778}

--- chrome/browser/ui/views/user_education/feature_promo_bubble_params.h
+++ chrome/browser/ui/views/user_education/feature_promo_bubble_params.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/accelerators/accelerator.h"
+#include "ui/gfx/vector_icon_types.h"
 #include "ui/views/bubble/bubble_border.h"
 
 // Describes the content and appearance of an in-product help bubble.
@@ -43,6 +44,9 @@ struct FeaturePromoBubbleParams {
 
   // String to be announced when bubble is shown. Optional.
   absl::optional<int> screenreader_string_specifier;
+
+  // The icon to display in the bubble next to the text.
+  const gfx::VectorIcon* body_icon = nullptr;
 
   // A keyboard accelerator to access the feature. If
   // |screenreader_string_specifier| is set and contains a placeholder,
--- chrome/browser/ui/views/user_education/feature_promo_bubble_view.h
+++ chrome/browser/ui/views/user_education/feature_promo_bubble_view.h
@@ -16,13 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
+namespace gfx {
+struct VectorIcon;
+}  // namespace gfx
+
 namespace ui {
 class MouseEvent;
 }  // namespace ui
 
 namespace views {
 class MdTextButton;
-}
+}  // namespace views
 
 // NOTE: Avoid using this class directly. FeaturePromoController should
 // be used in almost all cases.
@@ -59,6 +63,7 @@ class FeaturePromoBubbleView : public views::BubbleDialogDelegateView {
     views::View* anchor_view = nullptr;
     views::BubbleBorder::Arrow arrow = views::BubbleBorder::TOP_LEFT;
 
+    const gfx::VectorIcon* body_icon = nullptr;
     std::u16string body_text;
     absl::optional<std::u16string> title_text;
     absl::optional<std::u16string> screenreader_text;
