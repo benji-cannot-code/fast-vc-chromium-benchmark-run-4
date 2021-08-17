577aaa88e516702b08dc518e54b4ddf502ac10db
Tom <tluk@chromium.org>
2021-08-17 02:44:00 +0000

    [tab strip] Update touch drag drop context menu toggle
    This CL updates the touch drag drop behavior for the WebUI Tab Strip
    such that the context menu is shown on a long tap gesture rather than
    a long press. (i.e. after the touch pointer has been lifted off of
    the screen vs while the touch pointer is held on the screen).
    
    This aligns with the expected platform behavior on windows and linux.
    This change also avoids having the context menu forward gesture
    events to the renderer which currently causes issues with nested run
    loops.
    
    Bug: 229301
    Change-Id: Ib6cb3f6a47333548ca3bee5931a8c3f19ceeac58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088106
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912453}

--- chrome/browser/ui/webui/tab_strip/tab_strip_page_handler.cc
+++ chrome/browser/ui/webui/tab_strip/tab_strip_page_handler.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_menu_model.h"
 #include "chrome/browser/ui/tabs/tab_renderer_data.h"
 #include "chrome/browser/ui/tabs/tab_utils.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/tab_strip/tab_strip_ui.h"
 #include "chrome/browser/ui/webui/tab_strip/tab_strip_ui_embedder.h"
 #include "chrome/browser/ui/webui/tab_strip/tab_strip_ui_metrics.h"
@@ -172,6 +173,8 @@ TabStripPageHandler::TabStripPageHandler(
       tab_before_unload_tracker_(
           base::BindRepeating(&TabStripPageHandler::OnTabCloseCancelled,
                               base::Unretained(this))),
+      context_menu_after_tap_(base::FeatureList::IsEnabled(
+          features::kWebUITabStripContextMenuAfterTap)),
       long_press_timer_(std::make_unique<base::RetainingOneShotTimer>(
           FROM_HERE,
           kTouchLongpressDelay,
@@ -409,14 +412,19 @@ bool TabStripPageHandler::PreHandleGestureEvent(
         should_drag_on_gesture_scroll_ = false;
         return false;
       }
-      page_->ShowContextMenu();
+      if (!context_menu_after_tap_)
+        page_->ShowContextMenu();
       return true;
-    case blink::WebInputEvent::Type::kGestureTap:
     case blink::WebInputEvent::Type::kGestureLongTap:
+      if (context_menu_after_tap_)
+        page_->ShowContextMenu();
+      FALLTHROUGH;
+    case blink::WebInputEvent::Type::kGestureTap:
       // Ensure that we reset `should_drag_on_gesture_scroll_` when we encounter
       // a gesture tap event (i.e. an event triggered after the user lifts their
       // finger following a press or long press).
       should_drag_on_gesture_scroll_ = false;
+      long_press_timer_->Stop();
       return false;
     default:
       break;
