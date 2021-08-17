ffa8e901f8b7e4cf8dec2b48e4b99935ee278ab1
Kevin McNee <mcnee@chromium.org>
2021-08-17 20:10:02 +0000

    Reland "Don't include hang_watcher.h from thread_restrictions.h"
    This is a reland of 0e6733427f4e95b1fda5306c849eb5c16de8d8c3
    This reland fixes more IWYU issues.
    
    Original change's description:
    > Don't include hang_watcher.h from thread_restrictions.h
    >
    > This is only needed in thread_restrictions.cc.
    > We also fix IWYU issues that this removal revealed.
    >
    > Bug: 1227131
    > Change-Id: I68e19fc27b3ab32f3b423b44c6b4a431a9927732
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060580
    > Auto-Submit: Kevin McNee <mcnee@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Owners-Override: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Kevin McNee <mcnee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#912313}
    
    Bug: 1227131
    Cq-Include-Trybots: luci.chromium.try:win-official;luci.chrome.try:win-chrome,mac-chrome,linux-chrome
    Change-Id: I436be161287647e132c49ec9a31ad0eeda202564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098329
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912712}

--- base/mac/mach_port_rendezvous_unittest.cc
+++ base/mac/mach_port_rendezvous_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/at_exit.h"
+#include "base/cxx17_backports.h"
 #include "base/mac/foundation_util.h"
 #include "base/mac/mach_logging.h"
 #include "base/strings/stringprintf.h"
--- chrome/credential_provider/gaiacp/gcp_utils.cc
+++ chrome/credential_provider/gaiacp/gcp_utils.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
+#include "base/strings/string_number_conversions_win.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
