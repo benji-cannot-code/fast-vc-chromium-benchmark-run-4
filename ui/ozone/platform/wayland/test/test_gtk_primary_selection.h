0c59f4200a5f9963f893b91d561ffd6aab253744
Nick Diego Yamane <nickdiego@igalia.com>
2021-08-17 00:21:40 +0000

    ozone/wayland: Support gtk-primary-selection in test compositor
    With this, all supported selection protocols are exercised in Wayland
    clipboard unit tests: standard (copypaste buffer), zwp and gtk primary.
    
    R=tonikitoo@igalia.com
    
    Bug: 1204670
    Test: Covered by ozone_unittests.
    Change-Id: I0a7c7421d50c87ebad874e157c3a0d436fc273e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093812
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#912397}

--- ui/ozone/platform/wayland/test/test_gtk_primary_selection.h
+++ ui/ozone/platform/wayland/test/test_gtk_primary_selection.h
@@ -3,15 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_OZONE_PLATFORM_WAYLAND_TEST_TEST_ZWP_PRIMARY_SELECTION_H_
-#define UI_OZONE_PLATFORM_WAYLAND_TEST_TEST_ZWP_PRIMARY_SELECTION_H_
+#ifndef UI_OZONE_PLATFORM_WAYLAND_TEST_TEST_GTK_PRIMARY_SELECTION_H_
+#define UI_OZONE_PLATFORM_WAYLAND_TEST_TEST_GTK_PRIMARY_SELECTION_H_
 
 #include "ui/ozone/platform/wayland/test/test_selection_device_manager.h"
 
 namespace wl {
 
-TestSelectionDeviceManager* CreateTestSelectionManagerZwp();
+TestSelectionDeviceManager* CreateTestSelectionManagerGtk();
 
 }
 
-#endif  // UI_OZONE_PLATFORM_WAYLAND_TEST_TEST_ZWP_PRIMARY_SELECTION_H_
+#endif  // UI_OZONE_PLATFORM_WAYLAND_TEST_TEST_GTK_PRIMARY_SELECTION_H_
