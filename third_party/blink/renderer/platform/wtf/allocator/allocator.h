19a182c494d8f587ae0d8979259c3c1e58cf2f64
Benoit Lize <lizeb@chromium.org>
2021-08-18 14:44:12 +0000

    [PartitionAlloc] Add compiler annotations to PA and blink allocators.
    There are several attributes that we can add to malloc()-like functions:
    - malloc: tells the compiler that this is a malloc()-like function that is its
      return values are distinct and non-overlapping.
    - assume_aligned: tells the compiler that the return value is aligned on a
      certain boundary.
    
    It has practical consequences for aliasing analysis, but also to allow
    the compiler to use aligned SIMD instructions (as we found out the hard
    way in the past).
    
    For instance, from the added unittest disassembly on Linux x86_64, release
    build. Before, zeroing in the constructor was done with:
    
      f6ad45:	e8 f6 00 00 00       	callq  f6ae40 <_ZN4base8internal18PartitionAllocTest5AllocEm>
      f6ad4a:	49 89 c7             	mov    %rax,%r15
      f6ad4d:	0f 57 c0             	xorps  %xmm0,%xmm0
      f6ad50:	0f 11 00             	movups %xmm0,(%rax)  <-- Unaligned
    
    And now it is done with:
    
      f6ad45:	e8 f6 00 00 00       	callq  f6ae40 <_ZN4base8internal18PartitionAllocTest5AllocEm>
      f6ad4a:	49 89 c7             	mov    %rax,%r15
      f6ad4d:	0f 57 c0             	xorps  %xmm0,%xmm0
      f6ad50:	0f 29 00             	movaps %xmm0,(%rax)  <-- Aligned
    
    Change-Id: I89c6426c3da09c74eef2b4d7c06fba3398534746
    Bug: 998048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059455
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912985}

--- third_party/blink/renderer/platform/wtf/allocator/allocator.h
+++ third_party/blink/renderer/platform/wtf/allocator/allocator.h
@@ -8,10 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <atomic>
 
+#include "base/allocator/buildflags.h"
 #include "base/check_op.h"
 #include "build/build_config.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
 #include "third_party/blink/renderer/platform/wtf/type_traits.h"
+
+#if BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+// FastMalloc() defers to malloc() in this case, and including its header
+// ensures that the compiler knows that malloc() is "special", e.g. that it
+// returns properly-aligned, distinct memory locations.
+#include <malloc.h>
+#else
+#include "base/allocator/partition_allocator/partition_alloc.h"
+#endif  // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 
 namespace WTF {
 
