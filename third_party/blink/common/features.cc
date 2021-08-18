100c9ab40d88f8b5f2bc73c6bcb6f570b2c286c6
Xiaocheng Hu <xiaochengh@chromium.org>
2021-08-18 01:44:12 +0000

    Synchronously load data url web fonts
    Design doc: https://bit.ly/3wYlGQK
    
    This patch changes the loading of data url web fonts that, we
    synchronously populate the memory cache when the @font-face rule
    is discovered, so that the font can be immediately available when
    used during rendering. This makes data url fonts behave like local
    fonts, reduces CLS and eliminates developer confusions.
    
    The change is implemented behind a flag for finch experiment.
    
    This patch also adds a small virtual test suite of 4 tests to keep
    track of the test expectations when the flag is disabled.
    
    Bug: 1236283
    Change-Id: I343bb2f0b6cec0273d850a34511c2653e63dc234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3065069
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912852}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1026,5 +1026,8 @@ const base::Feature kCORSErrorsIssueOnly{"CORSErrorsIssueOnly",
 const base::Feature kDeprecateThirdPartyContextWebSQL{
     "DeprecateThirdPartyContextWebSQL", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kSyncLoadDataUrlFonts{"SyncLoadDataUrlFonts",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace features
 }  // namespace blink
