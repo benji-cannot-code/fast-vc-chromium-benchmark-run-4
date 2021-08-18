b2bcce74463ff49fc6e8e793f2581a2255f5088d
Sigurd Schneider <sigurds@chromium.org>
2021-08-18 08:07:25 +0000

    Re-enable more console tests
    Bug: chromium:1207342
    Change-Id: If5e0510f70d08a38175da7a7f7160e5609863666
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3100106
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912924}

--- third_party/blink/web_tests/http/tests/devtools/console/nested-worker-eval-contains-stack.js
+++ third_party/blink/web_tests/http/tests/devtools/console/nested-worker-eval-contains-stack.js
@@ -10,14 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
   `);
 
-  TestRunner.addSniffer(SDK.RuntimeModel.prototype, '_executionContextCreated', contextCreated);
+  TestRunner.addSniffer(SDK.RuntimeModel.prototype, 'executionContextCreated', contextCreated);
   TestRunner.evaluateInPage('startWorker()');
 
   var contexts_still_loading = 2;
   function contextCreated() {
     contexts_still_loading--;
     if (contexts_still_loading > 0) {
-      TestRunner.addSniffer(SDK.RuntimeModel.prototype, '_executionContextCreated', contextCreated);
+      TestRunner.addSniffer(SDK.RuntimeModel.prototype, 'executionContextCreated', contextCreated);
       return;
     }
 
--- third_party/blink/web_tests/http/tests/devtools/console/worker-eval-contains-stack.js
+++ third_party/blink/web_tests/http/tests/devtools/console/worker-eval-contains-stack.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       }
   `);
 
-  TestRunner.addSniffer(SDK.RuntimeModel.prototype, '_executionContextCreated', contextCreated);
+  TestRunner.addSniffer(SDK.RuntimeModel.prototype, 'executionContextCreated', contextCreated);
   TestRunner.evaluateInPage('startWorker()');
 
   function contextCreated() {
