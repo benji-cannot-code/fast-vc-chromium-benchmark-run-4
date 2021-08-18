5178cac1771b312a376929763084054c4687da8a
Jason Zhang <jiajunz@google.com>
2021-08-18 00:58:48 +0000

    [CrOS Network] Add GetInstallEuiccPath() in cellular util
    The GetInstallEuiccPath() will return the euicc path that will be
    installed the new eSim profile depending on the
    kCellularUseExternalEuicc flag.
    
    Bug: 1231305
    Change-Id: I082b7d172ab11e36e09dab5c5cffafaefd1ce5cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098679
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/master@{#912836}

--- chromeos/network/cellular_utils.cc
+++ chromeos/network/cellular_utils.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/network/cellular_utils.h"
 
+#include "ash/constants/ash_features.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/logging.h"
@@ -155,6 +156,17 @@ std::string GenerateStubCellularServicePath(const std::string& iccid) {
 
 bool IsStubCellularServicePath(const std::string& service_path) {
   return base::StartsWith(service_path, kNonShillCellularNetworkPathPrefix);
+}
+
+absl::optional<dbus::ObjectPath> GetCurrentEuiccPath() {
+  bool use_external_euicc = base::FeatureList::IsEnabled(
+      chromeos::features::kCellularUseExternalEuicc);
+  const std::vector<dbus::ObjectPath>& euicc_paths =
+      HermesManagerClient::Get()->GetAvailableEuiccs();
+  if (euicc_paths.empty() || (use_external_euicc && euicc_paths.size() < 2))
+    return absl::nullopt;
+
+  return use_external_euicc ? euicc_paths[1] : euicc_paths[0];
 }
 
 }  // namespace chromeos
--- chromeos/network/cellular_utils.h
+++ chromeos/network/cellular_utils.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "chromeos/network/device_state.h"
 
+namespace dbus {
+class ObjectPath;
+}  // namespace dbus
+
 namespace chromeos {
 
 class CellularESimProfile;
@@ -41,6 +45,11 @@ std::string GenerateStubCellularServicePath(const std::string& iccid);
 
 COMPONENT_EXPORT(CHROMEOS_NETWORK)
 bool IsStubCellularServicePath(const std::string& service_path);
+
+// Returns the path to the Euicc that is currently used for all eSIM operations
+// in OS Settings and System UI.
+COMPONENT_EXPORT(CHROMEOS_NETWORK)
+absl::optional<dbus::ObjectPath> GetCurrentEuiccPath();
 
 }  // namespace chromeos
 
