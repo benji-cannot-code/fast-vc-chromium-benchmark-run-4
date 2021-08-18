bf253d87cde6934cd8fd5a53dfa94a9533f89488
Kent Tamura <tkent@chromium.org>
2021-08-18 08:02:08 +0000

    FIELDSET: Don't reattach on descendant reattach
    Since crrev.com/893709, a FIELDSET has reattached whenever any
    descendant node is reattached.  It caused crbug.com/1240414 because
    SELECT closes its popup on detach.
    
    This CL makes the reattach condition stricter; Reattach a FIELDSET only
    for reattach of a direct child LayoutObject, not a descendant node.
    
    Bug: 1240414
    Change-Id: Ife3843eb4743b51c6f753f4ad2012c343f22cb77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099146
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912923}

--- third_party/blink/renderer/core/html/forms/html_field_set_element.cc
+++ third_party/blink/renderer/core/html/forms/html_field_set_element.cc
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatch_forbidden_scope.h"
+#include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
+#include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/node_lists_node_data.h"
 #include "third_party/blink/renderer/core/html/custom/element_internals.h"
 #include "third_party/blink/renderer/core/html/forms/html_legend_element.h"
@@ -38,6 +40,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 
 namespace blink {
+
+namespace {
+
+bool WillReattachChildLayoutObject(const Node& parent) {
+  for (const Node& child : FlatTreeTraversal::ChildrenOf(parent)) {
+    if (child.NeedsReattachLayoutTree())
+      return true;
+    if (child.ChildNeedsReattachLayoutTree() && child.GetComputedStyle() &&
+        child.GetComputedStyle()->Display() == EDisplay::kContents &&
+        WillReattachChildLayoutObject(child))
+      return true;
+  }
+  return false;
+}
+
+}  // namespace
 
 HTMLFieldSetElement::HTMLFieldSetElement(Document& document)
     : HTMLFormControlElement(html_names::kFieldsetTag, document) {
@@ -138,7 +156,7 @@ LayoutBox* HTMLFieldSetElement::GetLayoutBoxForScrolling() const {
 }
 
 void HTMLFieldSetElement::DidRecalcStyle(const StyleRecalcChange change) {
-  if (ChildNeedsReattachLayoutTree())
+  if (ChildNeedsReattachLayoutTree() && WillReattachChildLayoutObject(*this))
     SetNeedsReattachLayoutTree();
 }
 
