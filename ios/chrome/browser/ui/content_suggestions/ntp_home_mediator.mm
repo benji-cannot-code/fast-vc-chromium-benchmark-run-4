d2c282075eddf2603f9c4443606aa7caceed7e5f
Sylvain Defresne <sdefresne@chromium.org>
2021-08-18 15:15:37 +0000

    [ios] Cleanup unused includes/deps
    Recent refactor removed the need for those includes/deps
    but they were either restored during rebase or not removed.
    
    Bug: none
    Change-Id: I6705d6732ebad0ec893010fe38ccf0a4a1b827ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3077490
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913000}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/favicon/favicon_attributes.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
-#import "ios/public/provider/chrome/browser/signin/signin_resources_api.h"
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #include "ios/web/public/navigation/referrer.h"
