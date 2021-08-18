626446da01ca1a9fe737be1ffec8de66a53f2068
Scott Violet <sky@chromium.org>
2021-08-18 18:06:50 +0000

    weblayer: reenables more tests because of ordering issues
    The problem was in the html file. The ordering of the html file
    meant it was possible for the test to generate events before the
    handle was fully registered with blink
    
    BUG=1238481,1239032,1239026,1239028,1239028,1239034,1225662
    TEST=test only changes
    
    Change-Id: I11486426b263087486d75a3943f1f9e865431bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3101849
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913066}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TabCallbackTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TabCallbackTest.java
@@ -21,7 +21,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.content_public.browser.test.util.TestTouchUtils;
 import org.chromium.weblayer.ContextMenuParams;
@@ -155,7 +154,6 @@ public class TabCallbackTest {
         file.delete();
     }
 
-    @MinWebLayerVersion(88)
     @Test
     @SmallTest
     @DisableIf.
@@ -175,7 +173,6 @@ public class TabCallbackTest {
         waitForFileExist(tempDownloadDirectory, "lorem_ipsum.txt");
     }
 
-    @MinWebLayerVersion(88)
     @Test
     @SmallTest
     @DisableIf.
@@ -197,7 +194,6 @@ public class TabCallbackTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "crbug.com/1239034")
     public void testTabModalOverlay() throws TimeoutException {
         String pageUrl = mActivityTestRule.getTestDataURL("alert.html");
         InstrumentationActivity activity = mActivityTestRule.launchShellWithUrl(pageUrl);
@@ -231,7 +227,6 @@ public class TabCallbackTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "crbug.com/1239026")
     public void testDismissTransientUi() throws TimeoutException {
         String pageUrl = mActivityTestRule.getTestDataURL("alert.html");
         InstrumentationActivity activity = mActivityTestRule.launchShellWithUrl(pageUrl);
