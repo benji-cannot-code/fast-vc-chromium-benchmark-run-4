9c005aaf48743b793378805f3c75f2465a083e4f
John Lee <johntlee@chromium.org>
2021-08-18 15:37:44 +0000

    Side panel: Add permissions for chrome.bookmarkManagerPrivate API
    The side panel is another way for users to manage their bookmarks and
    is built using WebUI. To allow users to drag and drop their bookmarks
    using the side panel, the side panel WebUI will use the
    bookmarkManagerPrivate Extension API as it already has all the
    drag and drop functionality already built into it.
    
    Bug: 1234182
    Change-Id: If0724d2a12e6db3fead7cbf4d4a1974e30fb6530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097104
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913012}

--- chrome/browser/ui/views/toolbar/read_later_toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/read_later_toolbar_button.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics_action.h"
 #include "base/scoped_observation.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/side_panel.h"
 #include "chrome/common/webui_url_constants.h"
@@ -41,6 +43,11 @@ class ReadLaterSidePanelWebView : public views::WebView,
     contents_wrapper_->SetHost(weak_factory_.GetWeakPtr());
     contents_wrapper_->ReloadWebContents();
     SetWebContents(contents_wrapper_->web_contents());
+
+    if (base::FeatureList::IsEnabled(features::kSidePanelDragAndDrop)) {
+      extensions::BookmarkManagerPrivateDragEventRouter::CreateForWebContents(
+          contents_wrapper_->web_contents());
+    }
 
     browser_->tab_strip_model()->AddObserver(this);
   }
