e4946aadd341e1b5df644eb2c357fe221311fccc
Alan Screen <awscreen@chromium.org>
2021-08-18 19:36:33 +0000

    Revert "Close the full restore notification when launch an app from shortcut."
    This reverts commit b4c9a6247a8daef8bf4fbff4eed84b054946d834.
    
    Reason for revert: Suspected cause of many ASAN and MSAN failures.
    
    Original change's description:
    > Close the full restore notification when launch an app from shortcut.
    >
    > Based on the latest ui review decision, launching an app will dismiss
    > the full restore notification:
    > https://docs.google.com/presentation/d/1gZbfpG_-4Sum7-ETMcV8-gOXxjd97mhacA__uiCjM88/edit#slide=id.ge7aea646b0_8_5
    >
    > Add an observer to AcceleratorController, and add FullRestoreService as
    > an observer if the full restore notification is created. When a
    > keyboard shortcut cut is performed, and an app could be launched/opened,
    > close the full restore notification.
    >
    > BUG=1240058
    >
    > Change-Id: I8a24f666073d0cdaddbd11eb08997cc60bbd25ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099366
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#912819}
    
    Bug: 1240058, 1241127
    Change-Id: I7b5f94167e26e6a62707300fcf7f1434c0915844
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3103655
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Owners-Override: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913109}

--- chrome/browser/ash/full_restore/full_restore_service.cc
+++ chrome/browser/ash/full_restore/full_restore_service.cc
@@ -198,9 +198,6 @@ void FullRestoreService::MaybeCloseNotification(bool allow_save) {
   if (notification_ != nullptr && !is_shut_down_) {
     NotificationDisplayService::GetForProfile(profile_)->Close(
         NotificationHandler::Type::TRANSIENT, notification_->id());
-
-    if (accelerator_controller_observer_.IsObserving())
-      accelerator_controller_observer_.Reset();
   }
 
   if (allow_save) {
@@ -276,21 +273,6 @@ void FullRestoreService::Observe(int type,
   ::full_restore::FullRestoreSaveHandler::GetInstance()->SetShutDown();
 }
 
-void FullRestoreService::OnActionPerformed(AcceleratorAction action) {
-  switch (action) {
-    case NEW_INCOGNITO_WINDOW:
-    case NEW_TAB:
-    case NEW_WINDOW:
-    case OPEN_CROSH:
-    case OPEN_DIAGNOSTICS:
-    case RESTORE_TAB:
-      MaybeCloseNotification();
-      return;
-    default:
-      return;
-  }
-}
-
 void FullRestoreService::SetAppLaunchHanlderForTesting(
     std::unique_ptr<FullRestoreAppLaunchHandler> app_launch_handler) {
   app_launch_handler_ = std::move(app_launch_handler);
@@ -303,12 +285,6 @@ void FullRestoreService::Shutdown() {
 void FullRestoreService::MaybeShowRestoreNotification(const std::string& id) {
   if (!ShouldShowNotification())
     return;
-
-  auto* accelerator_controller = ash::AcceleratorController::Get();
-  if (accelerator_controller) {
-    DCHECK(!accelerator_controller_observer_.IsObserving());
-    accelerator_controller_observer_.Observe(accelerator_controller);
-  }
 
   message_center::RichNotificationData notification_data;
 
