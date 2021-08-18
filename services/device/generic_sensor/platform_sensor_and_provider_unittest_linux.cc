fb125dc7622167e7f50311d48fd4e86fce3e1046
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2021-08-18 16:59:56 +0000

    sensors: Remove LinuxMockPlatformSensorClient's default constructor.
    It is not used anywhere, we always initialize instances with a
    PlatformSensor.
    
    Change-Id: If951d70c2572a3bab1db576e98dfd4994eb227bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3103092
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913037}

--- services/device/generic_sensor/platform_sensor_and_provider_unittest_linux.cc
+++ services/device/generic_sensor/platform_sensor_and_provider_unittest_linux.cc
@@ -168,7 +168,6 @@ class MockSensorDeviceManager : public SensorDeviceManager {
 // error and data changes notifications.
 class LinuxMockPlatformSensorClient : public PlatformSensor::Client {
  public:
-  LinuxMockPlatformSensorClient() = default;
   explicit LinuxMockPlatformSensorClient(scoped_refptr<PlatformSensor> sensor)
       : sensor_(sensor) {
     if (sensor_)
