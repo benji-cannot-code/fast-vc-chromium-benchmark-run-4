764fc4d875526d42f3cf067993fa9f39205be4dc
Yuichiro Hanada <yhanada@google.com>
2021-08-18 00:10:46 +0000

    Check '--enable-virtual-keyboard' flag on OOBE screen.
    OnUserSessionAdded() is called only after an user logs in. To make the
    flag effective on OOBE screen, it should be checked not only in
    OnUserSessionAdded().
    
    Bug: b:196889865
    Test: tast run $DUT inputs.VirtualKeyboardOOBE
    Change-Id: If97f11194ddc1f6282bd01fae8580ae84bbfbd8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099417
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912816}

--- ash/keyboard/keyboard_controller_impl.h
+++ ash/keyboard/keyboard_controller_impl.h
@@ -98,7 +98,6 @@ class ASH_EXPORT KeyboardControllerImpl
   void TransferGestureEventToShelf(const ui::GestureEvent& e) override;
   // SessionObserver:
   void OnSessionStateChanged(session_manager::SessionState state) override;
-  void OnUserSessionAdded(const AccountId& id) override;
   void OnSigninScreenPrefServiceInitialized(PrefService* prefs) override;
   void OnActiveUserPrefServiceChanged(PrefService* prefs) override;
 
@@ -129,6 +128,8 @@ class ASH_EXPORT KeyboardControllerImpl
   void ObservePrefs(PrefService* prefs);
   void SendKeyRepeatUpdate();
   void SendKeyboardConfigUpdate();
+
+  void SetEnableFlagFromCommandLine();
 
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
   SessionControllerImpl* session_controller_;  // unowned
