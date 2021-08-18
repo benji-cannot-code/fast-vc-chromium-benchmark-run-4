7ec949d7e4f08243077093c169d5ca799ab9a04a
Dan McArdle <dmcardle@chromium.org>
2021-08-18 20:51:32 +0000

    Drop DeprecatedAtOrEmptyValue from InspectorDOMDebuggerAgent
    Bug: 1236727
    Change-Id: Ib0fe4b335c646e9939ee401c551a3e08dce85c19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3103866
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913147}

--- third_party/blink/renderer/core/inspector/inspector_dom_debugger_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_debugger_agent.cc
@@ -287,8 +287,8 @@ void InspectorDOMDebuggerAgent::DidInvalidateStyleAttr(Node* node) {
 
 void InspectorDOMDebuggerAgent::DidInsertDOMNode(Node* node) {
   if (dom_breakpoints_.size()) {
-    uint32_t mask = dom_breakpoints_.DeprecatedAtOrEmptyValue(
-        InspectorDOMAgent::InnerParentNode(node));
+    uint32_t mask =
+        FindBreakpointMask(InspectorDOMAgent::InnerParentNode(node));
     uint32_t inheritable_types_mask =
         (mask | (mask >> domBreakpointDerivedTypeShift)) &
         inheritableDOMBreakpointTypesMask;
@@ -391,8 +391,7 @@ Response InspectorDOMDebuggerAgent::setDOMBreakpoint(
     return response;
 
   uint32_t root_bit = 1 << type;
-  dom_breakpoints_.Set(
-      node, dom_breakpoints_.DeprecatedAtOrEmptyValue(node) | root_bit);
+  dom_breakpoints_.Set(node, FindBreakpointMask(node) | root_bit);
   if (root_bit & inheritableDOMBreakpointTypesMask) {
     for (Node* child = InspectorDOMAgent::InnerFirstChild(node); child;
          child = InspectorDOMAgent::InnerNextSibling(child))
@@ -416,7 +415,7 @@ Response InspectorDOMDebuggerAgent::removeDOMBreakpoint(
     return response;
 
   uint32_t root_bit = 1 << type;
-  uint32_t mask = dom_breakpoints_.DeprecatedAtOrEmptyValue(node) & ~root_bit;
+  uint32_t mask = FindBreakpointMask(node) & ~root_bit;
   if (mask)
     dom_breakpoints_.Set(node, mask);
   else
@@ -568,8 +567,7 @@ void InspectorDOMDebuggerAgent::BreakProgramOnDOMEvent(Node* target,
     if (!insertion)
       breakpoint_owner = InspectorDOMAgent::InnerParentNode(target);
     DCHECK(breakpoint_owner);
-    while (!(dom_breakpoints_.DeprecatedAtOrEmptyValue(breakpoint_owner) &
-             (1 << breakpoint_type))) {
+    while (!(FindBreakpointMask(breakpoint_owner) & (1 << breakpoint_type))) {
       Node* parent_node = InspectorDOMAgent::InnerParentNode(breakpoint_owner);
       if (!parent_node)
         break;
@@ -592,19 +590,23 @@ void InspectorDOMDebuggerAgent::BreakProgramOnDOMEvent(Node* target,
       v8_inspector::StringView(json.data(), json.size()));
 }
 
-bool InspectorDOMDebuggerAgent::HasBreakpoint(Node* node, int type) {
+bool InspectorDOMDebuggerAgent::HasBreakpoint(Node* node, int type) const {
   if (!dom_agent_->Enabled())
     return false;
   uint32_t root_bit = 1 << type;
   uint32_t derived_bit = root_bit << domBreakpointDerivedTypeShift;
-  return dom_breakpoints_.DeprecatedAtOrEmptyValue(node) &
-         (root_bit | derived_bit);
+  return FindBreakpointMask(node) & (root_bit | derived_bit);
+}
+
+uint32_t InspectorDOMDebuggerAgent::FindBreakpointMask(Node* node) const {
+  auto it = dom_breakpoints_.find(node);
+  return it != dom_breakpoints_.end() ? it->value : 0;
 }
 
 void InspectorDOMDebuggerAgent::UpdateSubtreeBreakpoints(Node* node,
                                                          uint32_t root_mask,
                                                          bool set) {
-  uint32_t old_mask = dom_breakpoints_.DeprecatedAtOrEmptyValue(node);
+  uint32_t old_mask = FindBreakpointMask(node);
   uint32_t derived_mask = root_mask << domBreakpointDerivedTypeShift;
   uint32_t new_mask = set ? old_mask | derived_mask : old_mask & ~derived_mask;
   if (new_mask)
