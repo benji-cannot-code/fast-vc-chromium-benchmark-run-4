f9b707729da095fc82379ad21328f08578426335
Dominick Ng <dominickn@chromium.org>
2021-08-18 04:51:34 +0000

    Fix lifetime issues in App Management.
    This CL ensures observers are removed at destruction, and changes a bare new
    to a std::unique_ptr to help address some lifetime problems.
    
    BUG=1218101
    
    Change-Id: Ie2261fe483dfe4b7605e5879c411953b2544c6d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3096402
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912890}

--- chrome/browser/ui/webui/app_management/app_management_shelf_delegate_chromeos.h
+++ chrome/browser/ui/webui/app_management/app_management_shelf_delegate_chromeos.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_APP_MANAGEMENT_APP_MANAGEMENT_SHELF_DELEGATE_CHROMEOS_H_
 #define CHROME_BROWSER_UI_WEBUI_APP_MANAGEMENT_APP_MANAGEMENT_SHELF_DELEGATE_CHROMEOS_H_
 
+#include <memory>
+
 #include "ash/public/cpp/shelf_model_observer.h"
 #include "base/macros.h"
 #include "chrome/browser/ui/webui/app_management/app_management.mojom.h"
@@ -36,7 +38,7 @@ class AppManagementShelfDelegate : public ash::ShelfModelObserver {
   void ShelfItemChanged(int index, const ash::ShelfItem& old_item) override;
 
   AppManagementPageHandler* page_handler_;
-  ShelfControllerHelper* shelf_controller_helper_;
+  std::unique_ptr<ShelfControllerHelper> shelf_controller_helper_;
 
   DISALLOW_COPY_AND_ASSIGN(AppManagementShelfDelegate);
 };
