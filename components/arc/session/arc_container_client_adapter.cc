3cec20080524663d0164fccbc0150d670a9b4f34
Yury Khmel <khmel@chromium.org>
2021-08-18 19:22:22 +0000

    arc: Disable ureadahead for ARC++ on request.
    This handles explicit request to disable ureadahead in ARC++ for tests.
    ARCVM part: crrev.com/c/3095534
    
    BUG=b:196390269
    TEST=Locally in context of extended tast tests.
         components_unittests --gtest_filter=*Ureadahead*
         observe results of extended arc.CachePerf test
         see crrev.com/c/3098594
    
    Change-Id: I341cf390f35eeeb600de1348568d944059bc4ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3101838
    Reviewed-by: Alan Ding <alanding@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913105}

--- components/arc/session/arc_container_client_adapter.cc
+++ components/arc/session/arc_container_client_adapter.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_method_call_status.h"
 #include "chromeos/dbus/login_manager/arc.pb.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
-#include "components/arc/arc_util.h"
 #include "components/arc/session/arc_session.h"
 
 namespace arc {
@@ -126,6 +125,7 @@ class ArcContainerClientAdapter
     request.set_disable_media_store_maintenance(
         params.disable_media_store_maintenance);
     request.set_disable_download_provider(params.disable_download_provider);
+    request.set_disable_ureadahead(params.disable_ureadahead);
     request.set_arc_generate_pai(params.arc_generate_play_auto_install);
 
     switch (params.usap_profile) {
@@ -137,10 +137,6 @@ class ArcContainerClientAdapter
         VLOG(1) << "USAP profile is not supported for container.";
         break;
     }
-
-    // TODO(b:196390269): Enable handling after arc.proto is uprev-ed.
-    if (IsUreadaheadDisabled())
-      LOG(ERROR) << "Disabling ureadahead is not supported in container yet.";
 
     chromeos::SessionManagerClient::Get()->StartArcMiniContainer(
         request, std::move(callback));
