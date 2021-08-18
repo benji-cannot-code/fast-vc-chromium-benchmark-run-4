1841581b8e271b7fc87efdae4188d3c4289b4af5
Peter Boström <pbos@chromium.org>
2021-08-18 01:05:30 +0000

    Add Ctrl+F6 to directly move focus to web contents
    This adds a modifier to the existing F6 for rotating between
    "accessibility panes". Because the number of panes between the location
    bar and web contents can vary, and current focus may also be anywhere,
    this is added as a direct way to jump to web contents.
    
    Bug: 162000
    Change-Id: I5a87e22c884a7101e6c1cf97b20689f4b82b0c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099253
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Ravjit Singh Uppal <ravjit@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912837}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1692,6 +1692,10 @@ void BrowserView::RotatePaneFocus(bool forwards) {
       views::FocusManager::FocusCycleWrapping::kEnabled);
 }
 
+void BrowserView::FocusWebContentsPane() {
+  contents_web_view_->RequestFocus();
+}
+
 bool BrowserView::ActivateFirstInactiveBubbleForAccessibility() {
   if (GetLocationBarView()->ActivateFirstInactiveBubbleForAccessibility())
     return true;
