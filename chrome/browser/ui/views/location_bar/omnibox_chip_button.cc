c2a500c7808f646d7cd032e34c494ec79e1d99e2
Illia Klimov <elklm@google.com>
2021-08-18 13:16:24 +0000

    Update chip's icon padding.
    This CL reduces padding between the chip's icon and text to 4 px.
    
    Bug: 1230035
    Change-Id: I94b60facc3b0502b5d610ee0a03a0e85805259b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3063041
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912966}

--- chrome/browser/ui/views/location_bar/omnibox_chip_button.cc
+++ chrome/browser/ui/views/location_bar/omnibox_chip_button.cc
@@ -14,6 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/controls/highlight_path_generator.h"
 
+namespace {
+
+// Padding between chip's icon and label.
+constexpr int kChipImagePadding = 4;
+// An extra space between chip's label and right edge.
+constexpr int kExtraRightPadding = 4;
+
+}  // namespace
+
 OmniboxChipButton::OmniboxChipButton(PressedCallback callback,
                                      const gfx::VectorIcon& icon,
                                      std::u16string message,
@@ -30,8 +39,7 @@ OmniboxChipButton::OmniboxChipButton(PressedCallback callback,
   SetElideBehavior(gfx::ElideBehavior::FADE_TAIL);
   SetFocusBehavior(views::View::FocusBehavior::ALWAYS);
   // Equalizing padding on the left, right and between icon and label.
-  SetImageLabelSpacing(
-      GetLayoutInsets(LOCATION_BAR_ICON_INTERIOR_PADDING).left());
+  SetImageLabelSpacing(kChipImagePadding);
   SetCustomPadding(
       gfx::Insets(GetLayoutConstant(LOCATION_BAR_CHILD_INTERIOR_PADDING),
                   GetLayoutInsets(LOCATION_BAR_ICON_INTERIOR_PADDING).left()));
@@ -68,8 +76,8 @@ void OmniboxChipButton::SetExpandAnimationEndedCallback(
 
 gfx::Size OmniboxChipButton::CalculatePreferredSize() const {
   const int fixed_width = GetIconSize() + GetInsets().width();
-  const int collapsable_width =
-      label()->GetPreferredSize().width() + GetInsets().right();
+  const int collapsable_width = label()->GetPreferredSize().width() +
+                                kChipImagePadding + kExtraRightPadding;
   const double animation_value =
       force_expanded_for_testing_ ? 1.0 : animation_->GetCurrentValue();
   const int width =
