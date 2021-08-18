9d1d5d42a26279cf4c6a9bddb6fae56a4fe0a4bd
Samuel Groß <saelo@chromium.org>
2021-08-18 15:24:46 +0000

    [PA] Have one ReservationOffsetTable per Pool in 64-bit mode
    This will allow creating a Pool outside of the GigaCage, which is
    required to support ArrayBuffer caging in V8.
    
    Bug: 1218005
    Change-Id: I0c6fdd1d70b748df1357984ecc3267b144006429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097746
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Samuel Groß <saelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913005}

--- base/allocator/partition_allocator/reservation_offset_table.cc
+++ base/allocator/partition_allocator/reservation_offset_table.cc
@@ -8,8 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
+#if defined(PA_HAS_64_BITS_POINTERS)
+ReservationOffsetTable::_ReservationOffsetTable
+    ReservationOffsetTable::reservation_offset_tables_[];
+#else
 ReservationOffsetTable::_ReservationOffsetTable
     ReservationOffsetTable::reservation_offset_table_;
+#endif
 
 }  // namespace internal
 }  // namespace base
