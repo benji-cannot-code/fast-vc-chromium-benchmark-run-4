bd716ec9f8201cdb2f0d7af1a00edc270b5239ba
Artem Sumaneev <asumaneev@google.com>
2021-08-18 12:08:56 +0000

    Clean up DeviceCloudPolicyInitializer logs.
    Remove logs around DeviceCloudPolicyInitializer causing crash during
    enrollment as it's not responsible for the enrollment anymore.
    
    Remove DeviceCloudPolicyInitializer getter from the connector and test
    checking if the initializer is deleted on policy manager connection.
    
    Bug: chromium:1219487
    Test: none
    Change-Id: I683bbbdb3e881d43603ba8ae700179d98ea96ff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3059569
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/master@{#912956}

--- chrome/browser/ash/policy/core/device_cloud_policy_browsertest.cc
+++ chrome/browser/ash/policy/core/device_cloud_policy_browsertest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/ash/login/test/local_policy_test_server_mixin.h"
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
-#include "chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.h"
 #include "chrome/browser/ash/policy/core/device_cloud_policy_store_ash.h"
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
@@ -37,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
 #include "components/policy/core/common/cloud/device_management_service.h"
-#include "components/policy/core/common/cloud/mock_cloud_policy_client.h"
 #include "components/policy/core/common/cloud/test/policy_builder.h"
 #include "components/policy/core/common/policy_service.h"
 #include "components/policy/core/common/policy_switches.h"
@@ -62,37 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 namespace policy {
-
-namespace {
-
-class DeviceCloudPolicyBrowserTest : public InProcessBrowserTest {
- protected:
-  DeviceCloudPolicyBrowserTest()
-      : mock_client_(std::make_unique<MockCloudPolicyClient>()) {}
-
-  std::unique_ptr<MockCloudPolicyClient> mock_client_;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(DeviceCloudPolicyBrowserTest);
-};
-
-}  // namespace
-
-IN_PROC_BROWSER_TEST_F(DeviceCloudPolicyBrowserTest, Initializer) {
-  BrowserPolicyConnectorAsh* connector =
-      g_browser_process->platform_part()->browser_policy_connector_ash();
-  // Initializer exists at first.
-  EXPECT_TRUE(connector->GetDeviceCloudPolicyInitializer());
-
-  // Initializer is deleted when the manager connects.
-  connector->GetDeviceCloudPolicyManager()->StartConnection(
-      std::move(mock_client_), connector->GetInstallAttributes());
-  EXPECT_FALSE(connector->GetDeviceCloudPolicyInitializer());
-
-  // Initializer is restarted when the manager disconnects.
-  connector->GetDeviceCloudPolicyManager()->Disconnect();
-  EXPECT_TRUE(connector->GetDeviceCloudPolicyInitializer());
-}
 
 namespace {
 
