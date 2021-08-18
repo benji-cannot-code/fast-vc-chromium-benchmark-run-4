42ee5195d490b1189d3392d1aabb0a7b459d3f14
Jiaming Cheng <jiamingc@chromium.org>
2021-08-18 23:59:17 +0000

    Add Calendar month view and Date view
    This cl added the prototypes of Month view and Date view for Calendar
    view, see the attached screenshots.
    
    Screenshots:
    darkmode: https://screenshot.googleplex.com/5TRmXa4HwEdKwWb
    lightmode: https://screenshot.googleplex.com/BGLCFPLkSAHDzQT
    
    design doc:
    https://docs.google.com/document/d/1FW-1-PhtxgHGxtJdv0Sjp8G4-NTyn4sI_PH60uxi4w8/edit?resourcekey=0-S5sU9I3kWtaRDcD7ibvoyw#heading=h.naotwxq045sb
    
    Bug: 1236276
    Change-Id: I004e92cecb169dc2b5afc370b43a5a9e238b889f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3084014
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913201}

---
+++ ash/system/time/calendar_utils.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_TIME_CALENDAR_UTILS_H_
+#define ASH_SYSTEM_TIME_CALENDAR_UTILS_H_
+
+#include "base/time/time.h"
+
+namespace ash {
+
+// Checks if the `selected_date` is local time today.
+bool IsToday(const base::Time::Exploded& selected_date);
+
+// Gets the given `date`'s `Exploded` instance.
+base::Time::Exploded GetExploded(const base::Time& date);
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_TIME_CALENDAR_UTILS_H_
