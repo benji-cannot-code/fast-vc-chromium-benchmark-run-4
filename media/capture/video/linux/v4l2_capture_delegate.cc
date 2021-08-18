dcc501af1b3f1922ce3f2e46f7cb999124f1232b
Tobias Grundel <tgrundel@vewd.com>
2021-08-18 18:42:49 +0000

    Remove DCHECK for is_capturing when stopping video capture
    A DCHECK for the variable is_capturing_ gets triggered when StopStream()
    is called. The default value of is_capturing_ is false and the only time
    it is set to true is at the end of StartStream(), but any time
    SetErrorState() gets called the boolean variable is_capturing_ is set to
    false and since StopStream() will get called after an error has been
    detected and SetErrorState() was called, leading to this DCHECK
    triggering and the browser crashes.
    
    Suggested fix is to remove the DCHECK in StopStream() since it is
    supposed to deallocate resources if an error occur i.e. when the web
    camera is already in use.
    
    Change-Id: I0e63738bce2879031b048278de7533a7a558abb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043975
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913085}

--- media/capture/video/linux/v4l2_capture_delegate.cc
+++ media/capture/video/linux/v4l2_capture_delegate.cc
@@ -975,7 +975,9 @@ void V4L2CaptureDelegate::DoCapture() {
 
 bool V4L2CaptureDelegate::StopStream() {
   DCHECK(v4l2_task_runner_->BelongsToCurrentThread());
-  DCHECK(is_capturing_);
+  if (!is_capturing_)
+    return false;
+
   is_capturing_ = false;
 
   // The order is important: stop streaming, clear |buffer_pool_|,
@@ -1004,7 +1006,6 @@ void V4L2CaptureDelegate::SetErrorState(VideoCaptureError error,
                                         const base::Location& from_here,
                                         const std::string& reason) {
   DCHECK(v4l2_task_runner_->BelongsToCurrentThread());
-  is_capturing_ = false;
   client_->OnError(error, from_here, reason);
 }
 
