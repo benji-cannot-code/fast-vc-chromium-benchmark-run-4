670de3041346c4c282fded872d4b609e066d21f2
François Degros <fdegros@chromium.org>
2021-08-18 04:10:19 +0000

    [Files App] Remove old ZIP mounting system
    Removed the possibility of using the old ZipArchiver-based ZIP mounting
    system. The new FUSE-based system has been released and enabled by
    default in M-90.
    
    Removed Chrome flag files-zip-mount.
    
    Note that the ZipArchiver extension itself is still installed on the
    system, since it is still used by the old ZIP creation system.
    
    BUG=chromium:1201052, chromium:1136317, chromium:912236
    TEST=browser_tests --gtest_filter="FileManagerJsTest.FileTasks"
    TEST=browser_tests --gtest_filter="ZipFiles*"
    TEST=Manual tests, checking that ZIP files can still be mounted
    
    Change-Id: I81b78ea6b1d8e564ca10e08e83aeb082c1348915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097225
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912882}

--- ui/file_manager/file_manager/foreground/js/task_controller.js
+++ ui/file_manager/file_manager/foreground/js/task_controller.js
@@ -507,6 +507,7 @@ export class TaskController {
         this.ui_.defaultTaskMenuItem.style.marginInlineEnd = '';
       }
 
+      // TODO(crbug.com/1201052) Remove these lines when removing ZipArchiver.
       if (util.descriptorEqual(
               defaultTask.descriptor,
               FileTasks.ZIP_ARCHIVER_UNZIP_TASK_DESCRIPTOR)) {
