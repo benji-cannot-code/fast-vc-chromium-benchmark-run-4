a70f00a974ba4f8e2588da9d7d324de53126b109
Aya ElAttar <ayaelattar@chromium.org>
2021-08-18 08:37:49 +0000

    DLP: Add MenuController::GetDropCallback
    Bug: 1175682
    Change-Id: I4b18bf0888fb13afc4d906645357b835eb53dff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069028
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912930}

--- chrome/browser/ui/views/menu_view_drag_and_drop_test.cc
+++ chrome/browser/ui/views/menu_view_drag_and_drop_test.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/bind.h"
 #include "base/macros.h"
 #include "base/scoped_observation.h"
 #include "base/strings/utf_string_conversions.h"
@@ -78,7 +79,12 @@ class TestTargetView : public views::View {
   void OnDragEntered(const ui::DropTargetEvent& event) override;
   int OnDragUpdated(const ui::DropTargetEvent& event) override;
   DragOperation OnPerformDrop(const ui::DropTargetEvent& event) override;
+  DropCallback GetDropCallback(const ui::DropTargetEvent& event) override;
   void OnDragExited() override;
+
+  // Performs the drop operation and updates |output_drag_op| accordingly.
+  void PerformDrop(const ui::DropTargetEvent& event,
+                   ui::mojom::DragOperation& output_drag_op);
 
   // Whether or not we are currently dragging.
   bool dragging_ = false;
@@ -128,13 +134,26 @@ int TestTargetView::OnDragUpdated(const ui::DropTargetEvent& event) {
 }
 
 DragOperation TestTargetView::OnPerformDrop(const ui::DropTargetEvent& event) {
+  auto drop_cb = GetDropCallback(event);
+  ui::mojom::DragOperation output_drag_op = ui::mojom::DragOperation::kNone;
+  std::move(drop_cb).Run(event, output_drag_op);
+  return output_drag_op;
+}
+
+views::View::DropCallback TestTargetView::GetDropCallback(
+    const ui::DropTargetEvent& event) {
   dragging_ = false;
-  dropped_ = true;
-  return DragOperation::kMove;
+  return base::BindOnce(&TestTargetView::PerformDrop, base::Unretained(this));
 }
 
 void TestTargetView::OnDragExited() {
   dragging_ = false;
+}
+
+void TestTargetView::PerformDrop(const ui::DropTargetEvent& event,
+                                 ui::mojom::DragOperation& output_drag_op) {
+  dropped_ = true;
+  output_drag_op = DragOperation::kMove;
 }
 
 }  // namespace
@@ -177,11 +196,19 @@ class MenuViewDragAndDropTest : public MenuTestBase,
   DragOperation OnPerformDrop(views::MenuItemView* menu,
                               DropPosition position,
                               const ui::DropTargetEvent& event) override;
+  views::View::DropCallback GetDropCallback(
+      views::MenuItemView* menu,
+      DropPosition position,
+      const ui::DropTargetEvent& event) override;
   bool CanDrag(views::MenuItemView* menu) override;
   void WriteDragData(views::MenuItemView* sender,
                      ui::OSExchangeData* data) override;
   int GetDragOperations(views::MenuItemView* sender) override;
   bool ShouldCloseOnDragComplete() override;
+
+  // Performs the drop operation and updates |output_drag_op| accordingly.
+  void PerformDrop(const ui::DropTargetEvent& event,
+                   ui::mojom::DragOperation& output_drag_op);
 
   // The special view in the menu, which supports its own drag and drop.
   TestTargetView* target_view_ = nullptr;
@@ -273,8 +300,18 @@ DragOperation MenuViewDragAndDropTest::OnPerformDrop(
     views::MenuItemView* menu,
     DropPosition position,
     const ui::DropTargetEvent& event) {
-  performed_in_menu_drop_ = true;
-  return DragOperation::kMove;
+  auto drop_cb = GetDropCallback(menu, position, event);
+  ui::mojom::DragOperation output_drag_op = ui::mojom::DragOperation::kNone;
+  std::move(drop_cb).Run(event, output_drag_op);
+  return output_drag_op;
+}
+
+views::View::DropCallback MenuViewDragAndDropTest::GetDropCallback(
+    views::MenuItemView* menu,
+    DropPosition position,
+    const ui::DropTargetEvent& event) {
+  return base::BindOnce(&MenuViewDragAndDropTest::PerformDrop,
+                        base::Unretained(this));
 }
 
 bool MenuViewDragAndDropTest::CanDrag(views::MenuItemView* menu) {
@@ -293,6 +330,13 @@ int MenuViewDragAndDropTest::GetDragOperations(views::MenuItemView* sender) {
 bool MenuViewDragAndDropTest::ShouldCloseOnDragComplete() {
   asked_to_close_ = true;
   return false;
+}
+
+void MenuViewDragAndDropTest::PerformDrop(
+    const ui::DropTargetEvent& event,
+    ui::mojom::DragOperation& output_drag_op) {
+  performed_in_menu_drop_ = true;
+  output_drag_op = DragOperation::kMove;
 }
 
 class MenuViewDragAndDropTestTestInMenuDrag : public MenuViewDragAndDropTest {
