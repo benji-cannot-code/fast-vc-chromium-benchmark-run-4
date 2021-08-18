6f09d275a07e7334ce0214881f5dca205e50386d
Wez <wez@chromium.org>
2021-08-18 19:03:54 +0000

    [fuchsia] Fix sequence on which SysInfo::GetHardwareInfo() replies.
    Fix the not-implemented code-path to invoke the reply callback on the
    correct sequence. Previously the callback was invoked off-sequence, via
    the ThreadPool.
    
    Bug: 1238790
    Change-Id: I9703f739f6343607a6ff259fa6222ea5c8ec780b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3101965
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913094}

--- base/system/sys_info.cc
+++ base/system/sys_info.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -98,8 +99,8 @@ void SysInfo::GetHardwareInfo(base::OnceCallback<void(HardwareInfo)> callback) {
       std::move(callback));
 #else
   NOTIMPLEMENTED();
-  base::ThreadPool::PostTask(
-      FROM_HERE, {}, base::BindOnce(std::move(callback), HardwareInfo()));
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), HardwareInfo()));
 #endif
 }
 
