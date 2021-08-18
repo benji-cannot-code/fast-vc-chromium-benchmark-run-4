13b7b121895d2d5a038d14782d2ea36f83eaab2a
Jordan Bayles <jophba@chromium.org>
2021-08-18 00:33:01 +0000

    Add new OpenscreenCastStreamingSession about:flag
    This patch adds a new about:flag entry for the Mirroring Service to use
    Open Screen/libcast's SenderSession implementation for negotiating and
    executing streaming (both mirroring and remoting) sessions.
    
    In future patches, this will be used to manage a fork between the
    current Mirroring Service's Session class and a new session class (name
    TBD, maybe LibcastSession?).
    
    Bug: b/184759616
    Change-Id: Iaa696c84bb6962c2c84b83578c44371bbabbd854
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3091930
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912824}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -770,6 +770,9 @@ extern const char kGlobalMediaControlsModernUIDescription[];
 extern const char kGlobalMediaControlsOverlayControlsName[];
 extern const char kGlobalMediaControlsOverlayControlsDescription[];
 
+extern const char kOpenscreenCastStreamingSessionName[];
+extern const char kOpenscreenCastStreamingSessionDescription[];
+
 extern const char kGoogleLensSdkIntentName[];
 extern const char kGoogleLensSdkIntentDescription[];
 
---
+++ components/mirroring/service/mirroring_features.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MIRRORING_SERVICE_MIRRORING_FEATURES_H_
+#define COMPONENTS_MIRRORING_SERVICE_MIRRORING_FEATURES_H_
+
+#include "base/component_export.h"
+#include "base/feature_list.h"
+#include "build/build_config.h"
+
+namespace mirroring {
+namespace features {
+
+COMPONENT_EXPORT(MIRRORING_SERVICE)
+extern const base::Feature kOpenscreenCastStreamingSession;
+
+}  // namespace features
+}  // namespace mirroring
+
+#endif  // COMPONENTS_MIRRORING_SERVICE_MIRRORING_FEATURES_H_
