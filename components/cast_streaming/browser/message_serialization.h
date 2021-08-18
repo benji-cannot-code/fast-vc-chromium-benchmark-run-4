b0723bc63f3d04816f8a349b3097f45ccb38efa5
Fabrice de Gans <fdegans@chromium.org>
2021-08-18 20:06:31 +0000

    [cast_streaming] Handle media namespace in Cast Streaming
    This adds a handler to send a media status update for the Cast
    Streaming client.
    
    Bug: 1240769
    Change-Id: I355cd8a89c571e76687e5ed4e9edc2a4654022d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2970828
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#913121}

--- components/cast_streaming/browser/message_serialization.h
+++ components/cast_streaming/browser/message_serialization.h
@@ -18,6 +18,7 @@ extern const char kMirroringNamespace[];
 extern const char kRemotingNamespace[];
 extern const char kSystemNamespace[];
 extern const char kInjectNamespace[];
+extern const char kMediaNamespace[];
 
 extern const char kKeySenderId[];
 extern const char kKeyNamespace[];
@@ -25,10 +26,16 @@ extern const char kKeyData[];
 extern const char kKeyType[];
 extern const char kKeyRequestId[];
 extern const char kKeyCode[];
+extern const char kKeyStatus[];
 
 extern const char kValueSystemSenderId[];
 extern const char kValueWrapped[];
 extern const char kValueError[];
+extern const char kValueMediaPlay[];
+extern const char kValueMediaPause[];
+extern const char kValueMediaGetStatus[];
+extern const char kValueMediaStatus[];
+
 extern const char kValueWrappedError[];
 extern const char kValueInjectNotSupportedError[];
 
