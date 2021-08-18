162943393f3d07c24345c111d0d313acdbd6856f
Antonio Gomes <tonikitoo@igalia.com>
2021-08-18 00:09:32 +0000

    [ozone/wayland] Set a crash key to WaylandEventWatcher::CheckForErrors()
    ... so that errors get logged.
    
    BUG=1185748
    R=erikchen@chromium.org
    
    Change-Id: Ic5032102910266da196e59275879dd619e3f7b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3100467
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#912814}

--- ui/ozone/platform/wayland/host/wayland_event_watcher.cc
+++ ui/ozone/platform/wayland/host/wayland_event_watcher.cc
@@ -13,9 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/strings/stringprintf.h"
 #include "base/task/current_thread.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "components/crash/core/common/crash_key.h"
 #include "ui/events/event.h"
 #include "ui/ozone/platform/wayland/common/wayland.h"
 
@@ -235,20 +237,31 @@ bool WaylandEventWatcher::CheckForErrors() {
     // When |err| is EPROTO, we can still use the |display_| to retrieve the
     // protocol error. Otherwise, get the error string from strerror and
     // shutdown the browser.
+    std::string error_string;
     if (err == EPROTO) {
       uint32_t ec, id;
       const struct wl_interface* intf;
       ec = wl_display_get_protocol_error(display_, &intf, &id);
       if (intf) {
-        LOG(ERROR) << "Fatal Wayland protocol error " << ec << " on interface "
-                   << intf->name << " (object " << id << "). Shutting down..";
+        error_string = base::StringPrintf(
+            "Fatal Wayland protocol error %u on interface %s (object %u). "
+            "Shutting down..",
+            ec, intf->name, id);
+        LOG(ERROR) << error_string;
       } else {
-        LOG(ERROR) << "Fatal Wayland protocol error " << ec
-                   << ". Shutting down..";
+        error_string = base::StringPrintf(
+            "Fatal Wayland protocol error %u. Shutting down..", ec);
+        LOG(ERROR) << error_string;
       }
     } else {
-      LOG(ERROR) << "Fatal Wayland communication error: " << std::strerror(err);
+      error_string = base::StringPrintf("Fatal Wayland communication error %s.",
+                                        std::strerror(err));
+      LOG(ERROR) << error_string;
     }
+
+    // Add a crash key so we can figure out why this is happening.
+    static crash_reporter::CrashKeyString<256> wayland_error("wayland_error");
+    wayland_error.Set(error_string);
 
     // This can be null in tests.
     if (!shutdown_cb_.is_null())
