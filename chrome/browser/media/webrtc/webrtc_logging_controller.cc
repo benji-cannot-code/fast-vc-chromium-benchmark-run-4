f6f8b58032e2897ca412224e6cba7e5dc8ac6991
Björn Terelius <terelius@chromium.org>
2021-08-18 13:01:01 +0000

    Ignore calls to StartRtpDump if RTP dump already started.
    Also make it explicit that the stop-callback is cleared after stopping.
    
    Bug: chromium:1234933
    Change-Id: Idf7411f1a40c2b58b656d24ebfc099ca6d66c9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3091006
    Commit-Queue: Björn Terelius <terelius@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#912962}

--- chrome/browser/media/webrtc/webrtc_logging_controller.cc
+++ chrome/browser/media/webrtc/webrtc_logging_controller.cc
@@ -188,6 +188,7 @@ void WebRtcLoggingController::StoreLog(const std::string& log_id,
       base::SequencedTaskRunnerHandle::Get()->PostTask(
           FROM_HERE,
           base::BindOnce(std::move(stop_rtp_dump_callback_), true, true));
+      stop_rtp_dump_callback_.Reset();
     }
 
     rtp_dump_handler_->StopOngoingDumps(
@@ -217,7 +218,13 @@ void WebRtcLoggingController::StoreLogContinue(const std::string& log_id,
 void WebRtcLoggingController::StartRtpDump(RtpDumpType type,
                                            GenericDoneCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(!stop_rtp_dump_callback_);
+
+  if (stop_rtp_dump_callback_) {
+    DCHECK(rtp_dump_handler_);
+    FireGenericDoneCallback(std::move(callback), false,
+                            "RTP dump already started.");
+    return;
+  }
 
   content::RenderProcessHost* host =
       content::RenderProcessHost::FromID(render_process_id_);
@@ -257,6 +264,7 @@ void WebRtcLoggingController::StopRtpDump(RtpDumpType type,
         base::BindOnce(std::move(stop_rtp_dump_callback_),
                        type == RTP_DUMP_INCOMING || type == RTP_DUMP_BOTH,
                        type == RTP_DUMP_OUTGOING || type == RTP_DUMP_BOTH));
+    stop_rtp_dump_callback_.Reset();
   }
 
   rtp_dump_handler_->StopDump(type, std::move(callback));
@@ -419,7 +427,6 @@ void WebRtcLoggingController::TriggerUpload(
     UploadDoneCallback callback,
     const base::FilePath& log_directory) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
   if (rtp_dump_handler_) {
     if (stop_rtp_dump_callback_) {
       base::SequencedTaskRunnerHandle::Get()->PostTask(
