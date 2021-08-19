1db5b911f46ab9341149bebea809dcd7b42cfafa
Chad Duffin <chadduffin@chromium.org>
2021-08-19 23:56:32 +0000

    [CrOS Bluetooth] Add helper function for device name.
    This change introduces a helper function that encapsulates the logic of
    retrieving the device name from a PairedBluetoothDeviceProperties
    object.
    
    Bug: 1010321
    Test: On-device and unit tests.
    Change-Id: I5ada9491843d576a5ed8ad7d621c0e7f296bb532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105174
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913545}

---
+++ chromeos/services/bluetooth_config/public/cpp/cros_bluetooth_config_util.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/bluetooth_config/public/cpp/cros_bluetooth_config_util.h"
+
+#include "base/strings/utf_string_conversions.h"
+
+namespace chromeos {
+namespace bluetooth_config {
+
+std::u16string GetPairedDeviceName(
+    const mojom::PairedBluetoothDeviceProperties* paired_device_properties) {
+  if (paired_device_properties->nickname.has_value())
+    return base::ASCIIToUTF16(paired_device_properties->nickname.value());
+  return paired_device_properties->device_properties->public_name;
+}
+
+}  // namespace bluetooth_config
+}  // namespace chromeos
---
+++ chromeos/services/bluetooth_config/public/cpp/cros_bluetooth_config_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_BLUETOOTH_CONFIG_PUBLIC_CPP_CROS_BLUETOOTH_CONFIG_UTIL_H_
+#define CHROMEOS_SERVICES_BLUETOOTH_CONFIG_PUBLIC_CPP_CROS_BLUETOOTH_CONFIG_UTIL_H_
+
+#include <string>
+
+#include "chromeos/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom.h"
+
+namespace chromeos {
+namespace bluetooth_config {
+
+// Returns the nickname of the provided device if it is set, otherwise returns
+// the public device name.
+std::u16string GetPairedDeviceName(
+    const mojom::PairedBluetoothDeviceProperties* paired_device_properties);
+
+}  // namespace bluetooth_config
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_BLUETOOTH_CONFIG_PUBLIC_CPP_CROS_BLUETOOTH_CONFIG_UTIL_H_
