f36fc3a9fc62a29de446115f43f26b5812d331ba
Mustafa Emre Acer <meacer@chromium.org>
2021-08-19 22:38:00 +0000

    Omnibox Security Indicators: Disable promo when lock icon policy is set
    The in-product help for the new connection security indicators is meant
    to be suppressed if the old indicator (lock icon) is still being shown.
    A new enterprise policy that forces the lock icon to be used was added
    in https://crrev.com/c/2893573 but the IPH suppression was still only
    consulting the Feature (kUpdatedConnectionSecurityIndicators). This CL
    updates the IPH suppression logic to consult both.
    
    Bug: 1241535
    Change-Id: Iedc2e06902307261a1c1dabbae71f233c5550eba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3103855
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913511}

--- components/omnibox/browser/location_bar_model.h
+++ components/omnibox/browser/location_bar_model.h
@@ -80,6 +80,10 @@ class LocationBarModel {
   // should be username/password and trailing slash on bare hostname.
   virtual bool ShouldPreventElision() const = 0;
 
+  // Returns whether the omnibox should use the new security indicators for
+  // secure HTTPS connections.
+  virtual bool ShouldUseUpdatedConnectionSecurityIndicators() const = 0;
+
  protected:
   LocationBarModel() = default;
 
