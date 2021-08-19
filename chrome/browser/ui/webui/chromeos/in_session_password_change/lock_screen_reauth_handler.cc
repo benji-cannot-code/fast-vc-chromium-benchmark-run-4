79f7a42c28fd91d06e4027caa568e2a0135f4f1a
Mohammed Abdon <mohammedabdon@chromium.org>
2021-08-19 11:57:56 +0000

    Fix for Flaky test: LockscreenWebUiTest.Login
    This CL fixes a flaky test that was caused due to RunUntilIdle that
    doesn't wait for a particular event which leads to dereferencing
    a null pointer returned from GetWebUIForTest.
    
    Bug: 1224705
    Change-Id: I1ffe8c41097c47febc29f4459e2483d4c5ea670a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098191
    Commit-Queue: Mohammed Abdon <mohammedabdon@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#913362}

--- chrome/browser/ui/webui/chromeos/in_session_password_change/lock_screen_reauth_handler.cc
+++ chrome/browser/ui/webui/chromeos/in_session_password_change/lock_screen_reauth_handler.cc
@@ -82,7 +82,7 @@ LockScreenReauthHandler::~LockScreenReauthHandler() = default;
 
 void LockScreenReauthHandler::HandleInitialize(const base::ListValue* value) {
   AllowJavascript();
-  OnJsReadyForTesting();
+  OnReauthDialogReadyForTesting();
   LoadAuthenticatorParam();
 }
 
@@ -281,10 +281,9 @@ void LockScreenReauthHandler::OnCookieWaitTimeout() {
   password_sync_manager->DismissDialog();
 }
 
-void LockScreenReauthHandler::OnJsReadyForTesting() {
-    js_ready_ = true;
-    if (initialization_callback_for_testing_)
-      std::move(initialization_callback_for_testing_).Run();
+void LockScreenReauthHandler::OnReauthDialogReadyForTesting() {
+  auto* password_sync_manager = GetInSessionPasswordSyncManager();
+  password_sync_manager->OnReauthDialogReadyForTesting();
 }
 
 void LockScreenReauthHandler::CheckCredentials(
@@ -344,16 +343,5 @@ bool LockScreenReauthHandler::IsAuthenticatorLoaded(
   waiting_caller_ = std::move(callback);
   return false;
 }
-
-bool LockScreenReauthHandler::IsJsReadyForTesting(
-  base::OnceClosure js_ready_callback) {
-    if (js_ready_)
-      return true;
-
-    DCHECK(initialization_callback_for_testing_.is_null());
-    initialization_callback_for_testing_ = std::move(js_ready_callback);
-    return false;
-}
-
 
 }  // namespace chromeos
