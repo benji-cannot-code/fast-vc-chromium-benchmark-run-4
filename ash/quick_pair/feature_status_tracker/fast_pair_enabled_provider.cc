e63e3839aa78cd81c2d737e5abd132206598ca46
Shane Fitzpatrick <shanefitz@google.com>
2021-08-19 18:30:10 +0000

    [FastPair] Log FastPairEnabledProvider::AreSubProvidersEnabled() result
    Change-Id: I569509e24449637f76e23f22b4dae51e4bebee6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3107725
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#913490}

--- ash/quick_pair/feature_status_tracker/fast_pair_enabled_provider.cc
+++ ash/quick_pair/feature_status_tracker/fast_pair_enabled_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/feature_status_tracker/fast_pair_enabled_provider.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/quick_pair/common/logging.h"
 #include "ash/quick_pair/feature_status_tracker/base_enabled_provider.h"
 #include "ash/quick_pair/feature_status_tracker/bluetooth_enabled_provider.h"
 #include "base/bind.h"
@@ -40,6 +41,14 @@ FastPairEnabledProvider::FastPairEnabledProvider(
 FastPairEnabledProvider::~FastPairEnabledProvider() = default;
 
 bool FastPairEnabledProvider::AreSubProvidersEnabled() {
+  QP_LOG(VERBOSE) << __func__ << ": Flag:"
+                  << base::FeatureList::IsEnabled(features::kFastPair)
+                  << " Google API Key:"
+                  << google_api_key_availability_provider_->is_enabled()
+                  << " Logged in User:"
+                  << logged_in_user_enabled_provider_->is_enabled()
+                  << " Bluetooth:" << bluetooth_enabled_provider_->is_enabled();
+
   return base::FeatureList::IsEnabled(features::kFastPair) &&
          google_api_key_availability_provider_->is_enabled() &&
          logged_in_user_enabled_provider_->is_enabled() &&
