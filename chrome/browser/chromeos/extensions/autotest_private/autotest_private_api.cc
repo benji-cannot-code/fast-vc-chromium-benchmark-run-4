def984358d09251ba192cdc46f978e7e84cb6575
Yue Li <updowndota@google.com>
2021-08-19 00:13:28 +0000

    Tast: Add Quick Answers prefs to autotest private API whitelist
    This change adds Quick Answers related prefs to the whitelist
    of autotest private API to change them in Tast tests.
    
    Bug: b/190654441
    Test: Tast tests to be added in https://chromium-review.googlesource.com/c/chromiumos/platform/tast-tests/+/3105377
    Change-Id: If01f9e66a1405d2324dbc6197b5163600ff3cf5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3103649
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913206}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
@@ -117,6 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/extensions/api/autotest_private.h"
 #include "chrome/common/pref_names.h"
+#include "chromeos/components/quick_answers/public/cpp/quick_answers_prefs.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
 #include "chromeos/printing/printer_configuration.h"
@@ -519,6 +520,21 @@ std::string SetWhitelistedPref(Profile* profile,
     DCHECK(value.is_bool());
   } else if (pref_name == prefs::kPrintingAPIExtensionsAllowlist) {
     DCHECK(value.is_list());
+  } else if (pref_name ==
+             chromeos::quick_answers::prefs::kQuickAnswersEnabled) {
+    DCHECK(value.is_bool());
+  } else if (pref_name ==
+             chromeos::quick_answers::prefs::kQuickAnswersDefinitionEnabled) {
+    DCHECK(value.is_bool());
+  } else if (pref_name ==
+             chromeos::quick_answers::prefs::kQuickAnswersTranslationEnabled) {
+    DCHECK(value.is_bool());
+  } else if (pref_name == chromeos::quick_answers::prefs::
+                              kQuickAnswersUnitConverstionEnabled) {
+    DCHECK(value.is_bool());
+  } else if (pref_name ==
+             chromeos::quick_answers::prefs::kQuickAnswersConsentStatus) {
+    DCHECK(value.is_int());
   } else {
     return "The pref " + pref_name + " is not whitelisted.";
   }
