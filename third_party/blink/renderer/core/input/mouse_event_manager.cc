7fa0e1a222ee01a4e6ecf83b235fa33505bd9735
Hongbo Song <hbsong@google.com>
2021-08-19 22:40:15 +0000

    Set mouse events trusted before creating event timing.
    To create an event timing, the event has to be trusted. Because we are
    about to dispatch those mouse events in mouse_event_manager.cc, I think
    it's ok to set them trusted here.
    
    Change-Id: Ib8354a232caaed781b338130c43ad5684c639867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3104525
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Hongbo Song <hbsong@google.com>
    Commit-Queue: Hongbo Song <hbsong@google.com>
    Cr-Commit-Position: refs/heads/main@{#913516}

--- third_party/blink/renderer/core/input/mouse_event_manager.cc
+++ third_party/blink/renderer/core/input/mouse_event_manager.cc
@@ -327,6 +327,7 @@ WebInputEventResult MouseEventManager::DispatchMouseEvent(
           mouse_event.FromTouch() ? MouseEvent::kFromTouch
                                   : MouseEvent::kRealOrIndistinguishable,
           mouse_event.menu_source_type);
+      event->SetTrusted(true);
       if (frame_ && frame_->DomWindow())
         event_timing = EventTiming::Create(frame_->DomWindow(), *event);
       if (should_dispatch) {
@@ -343,6 +344,7 @@ WebInputEventResult MouseEventManager::DispatchMouseEvent(
           mouse_event.FromTouch() ? MouseEvent::kFromTouch
                                   : MouseEvent::kRealOrIndistinguishable,
           mouse_event.menu_source_type);
+      event->SetTrusted(true);
       if (frame_ && frame_->DomWindow())
         event_timing = EventTiming::Create(frame_->DomWindow(), *event);
       if (should_dispatch) {
