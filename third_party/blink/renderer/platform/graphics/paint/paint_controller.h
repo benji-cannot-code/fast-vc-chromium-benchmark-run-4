b1566d6d95cea8629df67faec06f02d8535ee897
Xianzhu Wang <wangxianzhu@chromium.org>
2021-08-19 01:56:38 +0000

    Remove paint benchmark mode partial_invalidation and small_invalidation (blink/cc)
    Reasons are:
    
    - We expected them to somehow simulate the real world, but at least
      the results for pre-CAP and CAP can't be directly compared because
      the numbers of invalidated subsequences/items are different.
    
    - Other benchmark modes can provide enough meaningful data:
      - kForcePaint measures performance of cached subsequence moving;
      - kSubsequenceCachingDisabled measures performance of paint tree
        walk and cached display item moving;
      - kCachingDisabled measures performance of full repaint.
    
    Change-Id: Ibf9f53f769c2c7c76da0ae751804601f02222b81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3100749
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913240}

--- third_party/blink/renderer/platform/graphics/paint/paint_controller.h
+++ third_party/blink/renderer/platform/graphics/paint/paint_controller.h
@@ -36,12 +36,11 @@ enum class PaintBenchmarkMode {
   kNormal,
   kForceRasterInvalidationAndConvert,
   kForcePaintArtifactCompositorUpdate,
+  // Tests PaintController performance of moving cached subsequences.
   kForcePaint,
-  // The above modes don't additionally invalidate paintings, i.e. during
-  // repeated benchmarking, the PaintController is fully cached.
-  kPartialInvalidation,
-  kSmallInvalidation,
+  // Tests performance of core paint tree walk and moving cached display items.
   kSubsequenceCachingDisabled,
+  // Tests performance of full repaint.
   kCachingDisabled,
 };
 
@@ -380,8 +379,6 @@ class PLATFORM_EXPORT PaintController {
 #endif
 
   void SetBenchmarkMode(PaintBenchmarkMode);
-  bool ShouldInvalidateDisplayItemForBenchmark();
-  bool ShouldInvalidateSubsequenceForBenchmark();
 
   void CheckNoNewPaint() const {
 #if DCHECK_IS_ON()
@@ -460,8 +457,6 @@ class PLATFORM_EXPORT PaintController {
   wtf_size_t current_fragment_ = 0;
 
   PaintBenchmarkMode benchmark_mode_ = PaintBenchmarkMode::kNormal;
-  int partial_invalidation_display_item_count_ = 0;
-  int partial_invalidation_subsequence_count_ = 0;
 
   static CounterForTesting* counter_for_testing_;
 
