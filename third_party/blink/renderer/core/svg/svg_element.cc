8a51b517a7eb284e910a91e43951fbe5bbfc2652
Fredrik Söderqvist <fs@opera.com>
2021-08-19 19:00:00 +0000

    Rename SVGElement::MapInstanceToElement
    Rename to AddInstance() to get better symmetry with the Remove...()-
    function - which has its 'Mapping' suffix dropped.
    
    Change-Id: I276d300e7bfd685d00a609d70011af7d8491eab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3103291
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913505}

--- third_party/blink/renderer/core/svg/svg_element.cc
+++ third_party/blink/renderer/core/svg/svg_element.cc
@@ -368,7 +368,7 @@ void SVGElement::RemovedFrom(ContainerNode& root_parent) {
     if (SVGElement* corresponding_element =
             HasSVGRareData() ? SvgRareData()->CorrespondingElement()
                              : nullptr) {
-      corresponding_element->RemoveInstanceMapping(this);
+      corresponding_element->RemoveInstance(this);
       SvgRareData()->SetCorrespondingElement(nullptr);
     }
     RebuildAllIncomingReferences();
@@ -585,7 +585,7 @@ SVGElement* SVGElement::viewportElement() const {
   return nullptr;
 }
 
-void SVGElement::MapInstanceToElement(SVGElement* instance) {
+void SVGElement::AddInstance(SVGElement* instance) {
   DCHECK(instance);
   DCHECK(instance->InUseShadowTree());
 
@@ -596,7 +596,7 @@ void SVGElement::MapInstanceToElement(SVGElement* instance) {
   instances.insert(instance);
 }
 
-void SVGElement::RemoveInstanceMapping(SVGElement* instance) {
+void SVGElement::RemoveInstance(SVGElement* instance) {
   DCHECK(instance);
   // Called during instance->RemovedFrom() after removal from shadow tree
   DCHECK(!instance->isConnected());
--- third_party/blink/renderer/core/svg/svg_use_element.cc
+++ third_party/blink/renderer/core/svg/svg_use_element.cc
@@ -466,7 +466,7 @@ void SVGUseElement::AttachShadowTree(SVGElement& target) {
     }
     // Setup the mapping from the corresponding (original) element back to the
     // instance.
-    corresponding_element->MapInstanceToElement(&instance);
+    corresponding_element->AddInstance(&instance);
   }
 }
 
