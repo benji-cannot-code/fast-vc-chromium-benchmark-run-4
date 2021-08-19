bb5d8f8605997584b047db03df52bbc420a28da7
Ben Reich <benreich@chromium.org>
2021-08-19 05:24:13 +0000

    [FilesBanner] Add dismiss onclick handler for banners
    Educational and Warning banners allow for users to dismiss them and not
    have them shown for a predefined time period (usually 36 hours). This
    will be set at a banner type level.
    
    This also switches the hidden and aria-hidden attribute to not use
    boolean values. CSS in Files app currently checks for the existence of
    the hidden attribute (false or true) so switching to removeAttribute
    and setAttribute empty instead.
    
    Bug: 1228128
    Test: browser_tests --gtest_filter=FileManagerJsTest.Banner*
    Test: browser_tests --gtest_filter=*FilesApp*
    Test: compile chrome and interact with the Files app manually.
    Change-Id: I243940d4d8738043fd4c14d1faf00a1c7e6eff54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088884
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913282}

--- ui/file_manager/file_manager/externs/banner.js
+++ ui/file_manager/file_manager/externs/banner.js
@@ -36,10 +36,11 @@ export class Banner extends HTMLElement {
   diskThreshold() {}
 
   /**
-   * Volume types to watch if they unmount at all.
-   * @return {!Array<!VolumeManagerCommon.VolumeType|!undefined>}
+   * The duration (in seconds) to hide the banner after it has been dismissed by
+   * the user.
+   * @return {number|undefined}
    */
-  unmountedVolumeTypes() {}
+  hideAfterDismissedDurationSeconds() {}
 
   /**
    * Drive connection state to trigger the banner on.
@@ -80,3 +81,12 @@ Banner.DiskThresholdMinSize;
  *          }}
  */
 Banner.DiskThresholdMinRatio;
+
+/**
+ * Events dispatched by concrete banners.
+ * @enum {string}
+ * @const
+ */
+Banner.Event = {
+  BANNER_DISMISSED: 'banner-dismissed',
+};
