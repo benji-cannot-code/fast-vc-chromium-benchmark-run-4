4d68082ab1c7c1defb81c3c802ed1909f0f896bc
Liquan (Max) Gu <maxlg@chromium.org>
2021-08-19 17:41:27 +0000

    [SPCM95] Remove networkData from  tests
    Now that the "networkData" field was in SPC API V1 but was replaced
    with "challenge" since API V2+, this CL removes the "networkData"
    field.
    
    Bug: 1241090
    Change-Id: Id0da0d5895ea3161f7a1f4925d0e685c395ac133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3103629
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913445}

--- components/test/data/payments/create_many_requests.js
+++ components/test/data/payments/create_many_requests.js
@@ -9,7 +9,7 @@ const supportedInstruments = [{
   supportedMethods: 'secure-payment-confirmation',
   data: {
     'credentialIds': [new ArrayBuffer(4)],
-    'networkData': new ArrayBuffer(4),
+    'challenge': new ArrayBuffer(4),
   },
 }];
 
