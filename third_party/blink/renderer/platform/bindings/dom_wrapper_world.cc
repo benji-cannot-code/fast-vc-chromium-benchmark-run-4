85d6a33ff9e511e62eee65fe8bdb6a1e1770e6b7
Dan McArdle <dmcardle@chromium.org>
2021-08-19 00:23:53 +0000

    Use WTF::Copy{Keys,Values}ToVector in /third_party/blink/renderer/platform/bindings
    This CL was uploaded by git cl split.
    
    R=haraken@chromium.org
    
    Bug: 1241114
    Change-Id: Ia87397c4c9ea479b9cd1b76583c201adb397062b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3104147
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913210}

--- third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
+++ third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/dom_data_store.h"
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
+#include "third_party/blink/renderer/platform/wtf/hash_map.h"
 #include "third_party/blink/renderer/platform/wtf/hash_traits.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 
@@ -105,10 +106,10 @@ DOMWrapperWorld& DOMWrapperWorld::MainWorld() {
 
 void DOMWrapperWorld::AllWorldsInCurrentThread(
     Vector<scoped_refptr<DOMWrapperWorld>>& worlds) {
+  DCHECK(worlds.IsEmpty());
+  WTF::CopyValuesToVector(GetWorldMap(), worlds);
   if (IsMainThread())
     worlds.push_back(&MainWorld());
-  for (DOMWrapperWorld* world : GetWorldMap().Values())
-    worlds.push_back(world);
 }
 
 DOMWrapperWorld::~DOMWrapperWorld() {
