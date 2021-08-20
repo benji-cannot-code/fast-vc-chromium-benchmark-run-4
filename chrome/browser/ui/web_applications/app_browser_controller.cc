c56613898be0358073a74bb02f17d34e65d6346c
Eric Willigers <ericwilligers@chromium.org>
2021-08-20 04:37:00 +0000

    Simplify AppBrowserController::GetTitle()
    AppBrowserController::GetTitle() no longer contains
    logic specific to web apps.
    
    That logic has been moved to WebAppBrowserController::GetTitle()
    
    Bug: 1239989
    Change-Id: I35b813cfe4ca9ae882c0fec71a95dcfaee1653d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3106514
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913662}

--- chrome/browser/ui/web_applications/app_browser_controller.cc
+++ chrome/browser/ui/web_applications/app_browser_controller.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/feature_list.h"
-#include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/chromeos_buildflags.h"
@@ -452,22 +451,7 @@ std::u16string AppBrowserController::GetTitle() const {
 
   content::NavigationEntry* entry =
       web_contents->GetController().GetVisibleEntry();
-  std::u16string raw_title = entry ? entry->GetTitle() : std::u16string();
-
-  if (!base::FeatureList::IsEnabled(features::kPrefixWebAppWindowsWithAppName))
-    return raw_title;
-
-  std::u16string app_name =
-      base::UTF8ToUTF16(WebAppProvider::Get(browser()->profile())
-                            ->registrar()
-                            .GetAppShortName(app_id()));
-  if (base::StartsWith(raw_title, app_name)) {
-    return raw_title;
-  } else if (raw_title.empty()) {
-    return app_name;
-  } else {
-    return base::StrCat({app_name, u" - ", raw_title});
-  }
+  return entry ? entry->GetTitle() : std::u16string();
 }
 
 void AppBrowserController::OnTabStripModelChanged(
