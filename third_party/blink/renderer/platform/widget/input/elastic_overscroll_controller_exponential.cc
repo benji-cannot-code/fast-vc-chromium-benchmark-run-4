b543371de79cb9a0dc8893a87686de579210f4bb
Robert Flack <flackr@chromium.org>
2021-08-20 03:24:52 +0000

    Tweak android overscroll stretch parameters.
    Bring the overscroll physics closer in line with the platform feel.
    
    Bug: 1240789
    Change-Id: I636aa7eff2c1807f0e1891865fd4f6e4f63b67ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3108625
    Auto-Submit: Robert Flack <flackr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913627}

--- third_party/blink/renderer/platform/widget/input/elastic_overscroll_controller_exponential.cc
+++ third_party/blink/renderer/platform/widget/input/elastic_overscroll_controller_exponential.cc
@@ -4,14 +4,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/platform/widget/input/elastic_overscroll_controller_exponential.h"
+#include "build/build_config.h"
 #include "ui/gfx/geometry/vector2d_conversions.h"
 
 namespace blink {
 
 namespace {
+#if defined(OS_ANDROID)
+constexpr double kRubberbandStiffness = 20;
+constexpr double kRubberbandAmplitude = 0.2f;
+constexpr double kRubberbandPeriod = 1.1f;
+#else
 constexpr double kRubberbandStiffness = 20;
 constexpr double kRubberbandAmplitude = 0.31f;
 constexpr double kRubberbandPeriod = 1.6f;
+#endif
 }  // namespace
 
 ElasticOverscrollControllerExponential::ElasticOverscrollControllerExponential(
