e58bfb2df390c0b0fc994d6235b81d1ed34294c9
Peter Kvitek <kvitekp@chromium.org>
2021-08-20 22:56:02 +0000

    [headless] Cleaned up Page.printToPDF implementation.
    Got rid of the intermediate HeadlessPrintSettings class storing
    print settings in printing::mojom::PrintPagesParamsPtr.
    
    Consolidated all the CDP API parameters validation and conversion
    into a reusable utility module.
    
    This change is submitted in preparation of a larger change
    https://crrev.com/c/3082312
    
    Bug: 1240796
    Change-Id: I24cdf59c5debe63360b61cf66f10bf5aaec93d21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3111807
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914025}

--- headless/lib/browser/protocol/page_handler.h
+++ headless/lib/browser/protocol/page_handler.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_PRINTING)
-#include "headless/lib/browser/headless_print_manager.h"
+#include "headless/lib/browser/print_to_pdf/pdf_print_manager.h"
 #include "headless/public/headless_export.h"
 #endif
 
@@ -55,14 +55,17 @@ class PageHandler : public DomainHandler, public Page::Backend {
 
  private:
 #if BUILDFLAG(ENABLE_PRINTING)
-  void PDFCreated(bool returnAsStream,
-                  std::unique_ptr<PageHandler::PrintToPDFCallback> callback,
-                  HeadlessPrintManager::PrintResult print_result,
+  void PDFCreated(bool return_as_stream,
+                  std::unique_ptr<PrintToPDFCallback> callback,
+                  headless::PdfPrintManager::PrintResult print_result,
                   scoped_refptr<base::RefCountedMemory> data);
 #endif
+
   scoped_refptr<content::DevToolsAgentHost> agent_host_;
-  content::WebContents* web_contents_;
+  base::WeakPtr<content::WebContents> web_contents_;
+
   base::WeakPtrFactory<PageHandler> weak_factory_{this};
+
   DISALLOW_COPY_AND_ASSIGN(PageHandler);
 };
 
