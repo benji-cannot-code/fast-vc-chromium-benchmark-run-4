846911d9fa291e98a4a30927d5d93259b3eb7379
Gavin Dodd <gavindodd@google.com>
2021-08-20 18:32:30 +0000

    shimless: Minor refactor of state data.
    Reorder the fake states list to more closely match the real order.
    Reorder the state -> component map to match state enum order.
    
    Bug: 1198187
    Change-Id: I682c4a291bf02a3d034fe4792faac745d0d11a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3107965
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913903}

--- ash/webui/shimless_rma/resources/shimless_rma.js
+++ ash/webui/shimless_rma/resources/shimless_rma.js
@@ -69,24 +69,6 @@ const StateComponentMapping = {
     buttonCancel: ButtonState.VISIBLE,
     buttonBack: ButtonState.VISIBLE,
   },
-  [RmaState.kChooseDestination]: {
-    componentIs: 'onboarding-choose-destination-page',
-    buttonNext: ButtonState.VISIBLE,
-    buttonCancel: ButtonState.VISIBLE,
-    buttonBack: ButtonState.VISIBLE,
-  },
-  [RmaState.kChooseWriteProtectDisableMethod]: {
-    componentIs: 'onboarding-choose-wp-disable-method-page',
-    buttonNext: ButtonState.VISIBLE,
-    buttonCancel: ButtonState.VISIBLE,
-    buttonBack: ButtonState.VISIBLE,
-  },
-  [RmaState.kWaitForManualWPDisable]: {
-    componentIs: 'onboarding-wait-for-manual-wp-disable-page',
-    buttonNext: ButtonState.VISIBLE,
-    buttonCancel: ButtonState.HIDDEN,
-    buttonBack: ButtonState.VISIBLE,
-  },
   [RmaState.kUpdateOs]: {
     componentIs: 'onboarding-update-page',
     buttonNext: ButtonState.VISIBLE,
@@ -99,8 +81,26 @@ const StateComponentMapping = {
     buttonCancel: ButtonState.VISIBLE,
     buttonBack: ButtonState.VISIBLE,
   },
+  [RmaState.kChooseDestination]: {
+    componentIs: 'onboarding-choose-destination-page',
+    buttonNext: ButtonState.VISIBLE,
+    buttonCancel: ButtonState.VISIBLE,
+    buttonBack: ButtonState.VISIBLE,
+  },
+  [RmaState.kChooseWriteProtectDisableMethod]: {
+    componentIs: 'onboarding-choose-wp-disable-method-page',
+    buttonNext: ButtonState.VISIBLE,
+    buttonCancel: ButtonState.VISIBLE,
+    buttonBack: ButtonState.VISIBLE,
+  },
   [RmaState.kEnterRSUWPDisableCode]: {
     componentIs: 'onboarding-enter-rsu-wp-disable-code-page',
+    buttonNext: ButtonState.VISIBLE,
+    buttonCancel: ButtonState.HIDDEN,
+    buttonBack: ButtonState.VISIBLE,
+  },
+  [RmaState.kWaitForManualWPDisable]: {
+    componentIs: 'onboarding-wait-for-manual-wp-disable-page',
     buttonNext: ButtonState.VISIBLE,
     buttonCancel: ButtonState.HIDDEN,
     buttonBack: ButtonState.VISIBLE,
@@ -117,6 +117,7 @@ const StateComponentMapping = {
     buttonCancel: ButtonState.HIDDEN,
     buttonBack: ButtonState.VISIBLE,
   },
+  // TODO(gavindodd): kRestock
   [RmaState.kUpdateDeviceInformation]: {
     componentIs: 'reimaging-device-information-page',
     btnNext: ButtonState.VISIBLE,
@@ -129,12 +130,15 @@ const StateComponentMapping = {
     buttonCancel: ButtonState.HIDDEN,
     buttonBack: ButtonState.VISIBLE,
   },
+  // TODO(gavindodd): kSetupCalibration
+  // TODO(gavindodd): kRunCalibration
   [RmaState.kProvisionDevice]: {
     componentIs: 'reimaging-provisioning-page',
     btnNext: ButtonState.VISIBLE,
     btnCancel: ButtonState.HIDDEN,
     btnBack: ButtonState.VISIBLE,
   },
+  // TODO(gavindodd): kWaitForManualWpEnable
   [RmaState.kRepairComplete]: {
     componentIs: 'wrapup-repair-complete-page',
     buttonNext: ButtonState.VISIBLE,
