42b0f661b9527dca616eec93e55536b647dff6a9
Shane Fitzpatrick <shanefitz@google.com>
2021-08-20 02:43:08 +0000

    [FastPair] QuickPairProcessManager class
    This change adds a manager class via which manages the lifetime of our
    utility process and through which we will retrieve pointers to the
    functionality hosted in that process.
    
    The implementation is similar to our NearbyProcessManager in many ways.
    I chose not to refactor that class into something shareable due to the
    chromium guidance to typically wait until there's a third usage.
    
    QuickPairProcessShutdownController houses the logic to debounce the
    process shutdown when it is no longer in use. This CL introduces a
    simple short timer mechanism, but an upcoming CL will likely add to
    that logic to guard against motivated attackers.
    
    Tested via the included unit tests and locally.
    
    Bug: 1238584
    Change-Id: I8300dba678bccd67fb6ffc258b354d33d8d137dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105145
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913610}

---
+++ ash/services/quick_pair/quick_pair_process_manager.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/services/quick_pair/quick_pair_process_manager.h"
+
+#include <ostream>
+
+namespace ash {
+namespace quick_pair {
+
+std::ostream& operator<<(
+    std::ostream& os,
+    const QuickPairProcessManager::ShutdownReason& reason) {
+  switch (reason) {
+    case QuickPairProcessManager::ShutdownReason::kNormal:
+      return os << "[Normal]";
+    case QuickPairProcessManager::ShutdownReason::kCrash:
+      return os << "[Crash]";
+    case QuickPairProcessManager::ShutdownReason::
+        kFastPairDataParserMojoPipeDisconnection:
+      return os << "[FastPairDataParser Mojo Pipe Disconnection]";
+  }
+}
+
+}  // namespace quick_pair
+}  // namespace ash
---
+++ ash/services/quick_pair/quick_pair_process_shutdown_controller.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/services/quick_pair/quick_pair_process_shutdown_controller.h"
+#include "base/callback_forward.h"
+#include "base/memory/ptr_util.h"
+
+namespace ash {
+namespace quick_pair {
+
+constexpr base::TimeDelta kProcessShutdownTimeout =
+    base::TimeDelta::FromSeconds(5);
+
+QuickPairProcessShutdownController::QuickPairProcessShutdownController() =
+    default;
+
+QuickPairProcessShutdownController::~QuickPairProcessShutdownController() =
+    default;
+
+void QuickPairProcessShutdownController::Start(base::OnceClosure callback) {
+  timer_.Start(FROM_HERE, kProcessShutdownTimeout, std::move(callback));
+}
+
+void QuickPairProcessShutdownController::Stop() {
+  timer_.Stop();
+}
+
+}  // namespace quick_pair
+}  // namespace ash
