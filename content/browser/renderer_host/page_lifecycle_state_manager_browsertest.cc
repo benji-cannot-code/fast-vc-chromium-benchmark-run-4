f73f1f9fbc7ed7f04431addd431270441676403d
Yi Gu <yigu@chromium.org>
2021-08-20 17:23:46 +0000

    [Sheriff] Disable CrossProcessIframeHiddenAnFrozen on Win and Lacros
    Test is flaky:
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=content_browsertests%20(with%20patch)&tests=PageLifecycleStateManagerBrowserTest.CrossProcessIframeHiddenAnFrozen
    
    Bug: 1241814
    Change-Id: I1ac954bb59a3e4a3ba3b1460466bd77b88a4eddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110751
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Owners-Override: Yi Gu <yigu@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913855}

--- content/browser/renderer_host/page_lifecycle_state_manager_browsertest.cc
+++ content/browser/renderer_host/page_lifecycle_state_manager_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/location.h"
 #include "base/strings/string_number_conversions.h"
+#include "build/build_config.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
@@ -141,8 +142,15 @@ IN_PROC_BROWSER_TEST_F(PageLifecycleStateManagerBrowserTest, SetVisibility) {
             EvalJs(rfh, "window.performanceObserverEntries"));
 }
 
+// TODO(crbug.com/1241814): Test is flaky on Win and Lacros
+#if defined(OS_WIN) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_CrossProcessIframeHiddenAnFrozen \
+  DISABLED_CrossProcessIframeHiddenAnFrozen
+#else
+#define MAYBE_CrossProcessIframeHiddenAnFrozen CrossProcessIframeHiddenAnFrozen
+#endif
 IN_PROC_BROWSER_TEST_F(PageLifecycleStateManagerBrowserTest,
-                       CrossProcessIframeHiddenAnFrozen) {
+                       MAYBE_CrossProcessIframeHiddenAnFrozen) {
   EXPECT_TRUE(embedded_test_server()->Start());
   // Load a page with a cross-process iframe.
   GURL url_a_b(embedded_test_server()->GetURL(
