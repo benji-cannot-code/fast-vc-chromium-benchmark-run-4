50f8abb9d59594562421679796f05085ea6e5218
W. James MacLean <wjmaclean@chromium.org>
2021-08-20 14:53:07 +0000

    Split Worker ref-count from keep-alive ref count.
    This CL implements tracking for workers separate from
    fetch-keep-alive and delayed-subframe-unload tracking in RPHI.
    In a follow-on CL we will add instrumentation to track the
    time-remaining for fetch-keep-alive in order to better understand
    when ChildProcessSecurityPolicy can safely clean up the
    associated RenderProcess' information.
    
    Bug: 1148542
    Change-Id: I37e30a4e7533768135f3471297627fe16ab02a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2799937
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: W. James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913794}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -6708,7 +6708,7 @@ void RenderFrameHostImpl::CreateNewPopupWidget(
 void RenderFrameHostImpl::GetKeepAliveHandleFactory(
     mojo::PendingReceiver<blink::mojom::KeepAliveHandleFactory> receiver) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (GetProcess()->IsKeepAliveRefCountDisabled())
+  if (GetProcess()->IsWorkerAndKeepAliveRefCountDisabled())
     return;
 
   if (base::FeatureList::IsEnabled(network::features::kDisableKeepaliveFetch)) {
--- content/browser/service_worker/service_worker_process_manager.cc
+++ content/browser/service_worker/service_worker_process_manager.cc
@@ -69,8 +69,8 @@ void ServiceWorkerProcessManager::Shutdown() {
     for (const auto& it : worker_process_map_) {
       if (it.second->HasProcess()) {
         RenderProcessHost* process = it.second->GetProcess();
-        if (!process->IsKeepAliveRefCountDisabled())
-          process->DecrementKeepAliveRefCount();
+        if (!process->IsWorkerAndKeepAliveRefCountDisabled())
+          process->DecrementWorkerRefCount();
       }
     }
   }
@@ -167,8 +167,8 @@ ServiceWorkerProcessManager::AllocateWorkerProcess(
   }
 
   worker_process_map_.emplace(embedded_worker_id, std::move(site_instance));
-  if (!rph->IsKeepAliveRefCountDisabled())
-    rph->IncrementKeepAliveRefCount();
+  if (!rph->IsWorkerAndKeepAliveRefCountDisabled())
+    rph->IncrementWorkerRefCount();
   out_info->process_id = rph->GetID();
   out_info->start_situation = start_situation;
   return blink::ServiceWorkerStatusCode::kOk;
@@ -197,8 +197,8 @@ void ServiceWorkerProcessManager::ReleaseWorkerProcess(int embedded_worker_id) {
 
   if (it->second->HasProcess()) {
     RenderProcessHost* process = it->second->GetProcess();
-    if (!process->IsKeepAliveRefCountDisabled())
-      process->DecrementKeepAliveRefCount();
+    if (!process->IsWorkerAndKeepAliveRefCountDisabled())
+      process->DecrementWorkerRefCount();
   }
   worker_process_map_.erase(it);
 }
