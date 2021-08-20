924ce53e68f884f3d13b14f8c8f4a1ba4e19f360
Fredrik Söderqvist <fs@opera.com>
2021-08-20 09:49:04 +0000

    Don't instantiate a DarkModeFilter if dark mode is not enabled
    Add a DCHECK to GraphicsContext::GetDarkModeFilter() to guard against
    unnecessary instantiations of a DarkModeFilter.
    Adjust ScopedDarkModeElementRoleOverride to avoid instantiating the
    filter.
    
    Bug: 1091095
    Change-Id: I45f37658102c655aa61899e659f3590fa1f48f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3102947
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#913734}

--- third_party/blink/renderer/platform/graphics/graphics_context.cc
+++ third_party/blink/renderer/platform/graphics/graphics_context.cc
@@ -159,11 +159,11 @@ void GraphicsContext::CopyConfigFrom(GraphicsContext& other) {
 }
 
 DarkModeFilter* GraphicsContext::GetDarkModeFilter() {
+  DCHECK(IsDarkModeEnabled());
   if (!dark_mode_filter_) {
     dark_mode_filter_ =
         std::make_unique<DarkModeFilter>(GetCurrentDarkModeSettings());
   }
-
   return dark_mode_filter_.get();
 }
 
