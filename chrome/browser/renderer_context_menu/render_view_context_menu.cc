d3b2f693e47509d0165c5942ec8a140581c2f32d
Yiren Wang <yrw@chromium.org>
2021-08-20 21:26:40 +0000

    Disable the copy video address button for blob URL
    Blob URL is not useful to users since it is used in a single instance of
    the browser, so we want to disable the copy video address button in this
    case.
    
    Bug: 780858
    Change-Id: I8c468ece84479bfd552d0abe3ec4d5a11dc5a8f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3101325
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913982}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -2269,6 +2269,8 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
     }
 
     case IDC_CONTENT_CONTEXT_COPYAVLOCATION:
+      return params_.src_url.is_valid() && !params_.src_url.SchemeIsBlob();
+
     case IDC_CONTENT_CONTEXT_COPYIMAGELOCATION:
       return params_.src_url.is_valid();
 
