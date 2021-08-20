e2357d47db7829acae47f3638249b851dee6004a
Lijin Shen <lazzzis@google.com>
2021-08-20 23:50:40 +0000

    [Messages] Allow setting action callback and dismiss callback
    Allow clients to set action callback and dismiss callback after
    the declaration of message wrapper.
    
    Bug: 1241078
    Change-Id: I3e2b2b2f983b42cbc55ff7bc850841f8fff50356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3107861
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914051}

--- components/messages/android/message_wrapper.cc
+++ components/messages/android/message_wrapper.cc
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace messages {
 
+MessageWrapper::MessageWrapper(MessageIdentifier message_identifier)
+    : MessageWrapper(message_identifier,
+                     base::NullCallback(),
+                     base::NullCallback()) {}
+
 MessageWrapper::MessageWrapper(MessageIdentifier message_identifier,
                                base::OnceClosure action_callback,
                                DismissCallback dismiss_callback)
@@ -143,6 +148,14 @@ void MessageWrapper::SetSecondaryActionCallback(base::OnceClosure callback) {
 void MessageWrapper::SetDuration(long customDuration) {
   JNIEnv* env = base::android::AttachCurrentThread();
   Java_MessageWrapper_setDuration(env, java_message_wrapper_, customDuration);
+}
+
+void MessageWrapper::SetActionClick(base::OnceClosure callback) {
+  action_callback_ = std::move(callback);
+}
+
+void MessageWrapper::SetDismissCallback(DismissCallback callback) {
+  dismiss_callback_ = std::move(callback);
 }
 
 void MessageWrapper::HandleActionClick(JNIEnv* env) {
