bdb1f38dfb2f4bcc482fa382c0bc5bcf38026059
Robbie McElrath <rmcelrath@chromium.org>
2021-08-20 04:44:52 +0000

    [WL] Fix Switches in WebLayer Settings UI
    This CL applies the ColorOverlay style to the WebLayer Settings UI,
    which fixes missing switch colors.
    
    Bug: 1241132
    Change-Id: I7598018d54c5b2c1c4671531a07770a27bc4c9e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3101399
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913675}

--- weblayer/browser/java/org/chromium/weblayer_private/BrowserFragmentImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/BrowserFragmentImpl.java
@@ -17,7 +17,6 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.metrics.RecordHistogram;
-import org.chromium.components.browser_ui.styles.R;
 import org.chromium.components.embedder_support.application.ClassLoaderContextWrapperFactory;
 import org.chromium.weblayer_private.interfaces.BrowserFragmentArgs;
 import org.chromium.weblayer_private.interfaces.IBrowser;
@@ -187,7 +186,9 @@ public class BrowserFragmentImpl extends FragmentHostingRemoteFragmentImpl {
     protected FragmentHostingRemoteFragmentImpl.RemoteFragmentContext createRemoteFragmentContext(
             Context embedderContext) {
         Context wrappedContext = ClassLoaderContextWrapperFactory.get(embedderContext);
-        Context themedContext = new ContextThemeWrapper(wrappedContext, R.style.Theme_BrowserUI);
+        Context themedContext =
+                new ContextThemeWrapper(wrappedContext, R.style.Theme_WebLayer_Settings);
+        themedContext.getTheme().applyStyle(R.style.ColorOverlay_WebLayer, /*force=*/true);
         return new FragmentHostingRemoteFragmentImpl.RemoteFragmentContext(themedContext);
     }
 }
