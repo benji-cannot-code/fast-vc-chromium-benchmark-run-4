9ed3a4ad0abf9d10094752099adee385adb0bc91
James Cook <jamescook@chromium.org>
2021-08-20 23:18:53 +0000

    App list bubble: Add horizontal separator under search box input field
    The mocks call for a separator under the search box. Only show it for
    the apps grid page and the search page, because the assistant handles
    its own search input field.
    
    Spacing will be addressed in a later CL.
    
    Screenshot: https://screenshot.googleplex.com/5B9vM3fjs3NryDq
    
    Bug: 1218229
    Test: added to ash_unittests
    Change-Id: Ib95c127570ae622b87328135e4357a683e21fd17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105010
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914037}

--- ash/app_list/views/app_list_bubble_view.cc
+++ ash/app_list/views/app_list_bubble_view.cc
@@ -36,6 +36,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using views::BoxLayout;
 
 namespace ash {
+namespace {
+
+// A simplified horizontal separator that uses a solid color layer for painting.
+// This is more efficient than using a views::Separator, which would require
+// SetPaintToLayer(ui::LAYER_TEXTURED).
+class SeparatorWithLayer : public views::View {
+ public:
+  SeparatorWithLayer() {
+    SetPaintToLayer(ui::LAYER_SOLID_COLOR);
+    layer()->SetColor(ColorProvider::Get()->GetContentLayerColor(
+        ColorProvider::ContentLayerType::kSeparatorColor));
+    layer()->SetFillsBoundsOpaquely(false);
+  }
+  SeparatorWithLayer(const SeparatorWithLayer&) = delete;
+  SeparatorWithLayer& operator=(const SeparatorWithLayer&) = delete;
+  ~SeparatorWithLayer() override = default;
+
+  // views::View:
+  gfx::Size CalculatePreferredSize() const override {
+    // The parent's layout manager will stretch it horizontally.
+    return gfx::Size(1, 1);
+  }
+};
+
+}  // namespace
 
 AppListBubbleView::AppListBubbleView(
     AppListViewDelegate* view_delegate,
@@ -64,6 +89,10 @@ AppListBubbleView::AppListBubbleView(
   params.show_close_button_when_active = false;
   params.create_background = false;
   search_box_view_->Init(params);
+
+  // The main view has a solid color layer, so the separator needs its own
+  // layer to visibly paint.
+  separator_ = AddChildView(std::make_unique<SeparatorWithLayer>());
 
   AddAccelerator(ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE));
   AddAccelerator(ui::Accelerator(ui::VKEY_BROWSER_BACK, ui::EF_NONE));
@@ -108,6 +137,7 @@ bool AppListBubbleView::IsShowingEmbeddedAssistantUI() const {
 void AppListBubbleView::ShowEmbeddedAssistantUI() {
   // The assistant has its own text input field.
   search_box_view_->SetVisible(false);
+  separator_->SetVisible(false);
 
   apps_page_->SetVisible(false);
   search_page_->SetVisible(false);
