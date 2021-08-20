c9cf26ba9327bb33af06ba287a4812c8c57bb4c9
Ayu Ishii <ayui@chromium.org>
2021-08-20 17:21:21 +0000

    StorageBuckets: Fix crash for bad optional access
    This change fixes bad `result.value()` access for when
    the result errors but isn't a kDatabaseError. Returns
    an empty result its caller.
    
    Bug: 1241358
    Change-Id: I168e7df2133139054f61cd6853323ffe1adf4cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3106032
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913850}

--- storage/browser/quota/quota_manager_impl.cc
+++ storage/browser/quota/quota_manager_impl.cc
@@ -2205,6 +2205,10 @@ void QuotaManagerImpl::DidGetStorageKeys(
     QuotaErrorOr<std::set<StorageKey>> result) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DidDatabaseWork(result.ok() || result.error() != QuotaError::kDatabaseError);
+  if (!result.ok()) {
+    std::move(callback).Run(std::set<StorageKey>());
+    return;
+  }
   std::move(callback).Run(std::move(result.value()));
 }
 
@@ -2214,6 +2218,10 @@ void QuotaManagerImpl::DidGetModifiedBetween(
     QuotaErrorOr<std::set<BucketInfo>> result) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DidDatabaseWork(result.ok() || result.error() != QuotaError::kDatabaseError);
+  if (!result.ok()) {
+    std::move(callback).Run(std::set<BucketInfo>(), type);
+    return;
+  }
   std::move(callback).Run(result.value(), type);
 }
 
