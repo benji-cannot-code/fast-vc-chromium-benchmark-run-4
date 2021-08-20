24c92feae8c07bc45a2be4a1e37da0343196d069
Minoru Chikamune <chikamune@chromium.org>
2021-08-20 04:39:13 +0000

    [MPArch] Get rid of RenderFrameHostDelegate::GetMainFrameLastCommittedURL
    GetMainFrameLastCommittedURL is not compatible with MPArch since it is
    possible to have multiple FrameTrees in MPArch.
    
    This CL gets rid of GetMainFrameLastCommittedURL which is implemented
    on WebContentsImpl::GetMainFrameLastCommittedURL().
    
    There are two callsites of GetMainFrameLastCommittedURL. Both
    callsites essentially belong to RenderFrameHostImpl. So we don’t need
    to get the main frame's last committed URL from WebContentsImpl that
    is coarser than RenderFrameHostImpl.
    
    - RenderFrameHostImpl::DidAddMessageToConsole()
    
    DidAddMessageToConsole() is a member function of
    RenderFrameHostImpl. So we don’t need to access to WebContentsImpl to
    get the main frame's last committed URL. We can obtain the URL from
    RenderFrameHostImpl.
    
    - WebContentsImpl::GetRecordAggregateWatchTimeCallback()
    
    GetRecordAggregateWatchTimeCallback() is called from
    RenderFrameHostImpl::GetRecordAggregateWatchTimeCallback(). So this CL
    gets the main frame's last committed URL from RenderFrameHostImpl, and
    passes the URL via the argument of
    GetRecordAggregateWatchTimeCallback().
    
    Bug: 1240924
    Change-Id: Icfba1f36e1783e716aa11b34de12122c532f797a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3102693
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913666}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -3289,7 +3289,7 @@ void RenderFrameHostImpl::DidAddMessageToConsole(
   // Pass through log severity only on builtin components pages to limit console
   // spew.
   const bool is_builtin_component =
-      HasWebUIScheme(delegate_->GetMainFrameLastCommittedURL()) ||
+      HasWebUIScheme(GetMainFrame()->GetLastCommittedURL()) ||
       GetContentClient()->browser()->IsBuiltinComponent(
           GetProcess()->GetBrowserContext(), GetLastCommittedOrigin());
   const bool is_off_the_record =
@@ -6981,7 +6981,8 @@ void RenderFrameHostImpl::HandleAXLocationChanges(
 
 media::MediaMetricsProvider::RecordAggregateWatchTimeCallback
 RenderFrameHostImpl::GetRecordAggregateWatchTimeCallback() {
-  return delegate_->GetRecordAggregateWatchTimeCallback();
+  return delegate_->GetRecordAggregateWatchTimeCallback(
+      GetMainFrame()->GetLastCommittedURL());
 }
 
 void RenderFrameHostImpl::ResetWaitingState() {
