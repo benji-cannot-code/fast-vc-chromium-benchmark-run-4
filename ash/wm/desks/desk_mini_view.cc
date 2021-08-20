0b3466497c2278737a06b108caffc9c3244dbfc7
Sophie Wen <sophiewen@google.com>
2021-08-20 23:48:41 +0000

    Fix overview highlight to move with desk textfield focus.
    This fixes the bug where after tabbing between desk views, then clicking
    a different textfield view, overview highlight doesn't move with the
    focus ring.
    
    TESTED=unit test and verified in Chromium emulator
    
    Bug: 1174304
    Change-Id: I0ec1679d75fba3a828a434744ed1cc580686ee74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3107855
    Commit-Queue: Sophie Wen <sophiewen@google.com>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914049}

--- ash/wm/desks/desk_mini_view.cc
+++ ash/wm/desks/desk_mini_view.cc
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desks_bar_view.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/desks_restore_util.h"
+#include "ash/wm/overview/overview_controller.h"
+#include "ash/wm/overview/overview_highlight_controller.h"
 #include "base/bind.h"
 #include "base/cxx17_backports.h"
 #include "base/strings/string_util.h"
@@ -392,6 +394,14 @@ void DeskMiniView::OnViewFocused(views::View* observed_view) {
   // Set the unelided desk name so that the full name shows up for the user to
   // be able to change it.
   desk_name_view_->SetText(desk_->name());
+
+  // Set the Overview highlight to move focus with the DeskNameView.
+  auto* highlight_controller = Shell::Get()
+                                   ->overview_controller()
+                                   ->overview_session()
+                                   ->highlight_controller();
+  if (highlight_controller->IsFocusHighlightVisible())
+    highlight_controller->MoveHighlightToView(desk_name_view_);
 
   if (!defer_select_all_)
     desk_name_view_->SelectAll(false);
