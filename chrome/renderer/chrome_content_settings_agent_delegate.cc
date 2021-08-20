f4fbac672caa0c0f394092ee44ec8af513ca8f81
Bo Majewski <majewski@chromium.org>
2021-08-20 04:42:47 +0000

    [Files SWA]: Marks the code to be removed.
    The following marks the code outside file manager directories that needs
    to be removed once we switch to asynchronous APIs.
    
    Bug: b/196004459
    Change-Id: I8f828c57a969e4cebbae350c74e7b682c745d6f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3106631
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#913672}

--- chrome/renderer/chrome_content_settings_agent_delegate.cc
+++ chrome/renderer/chrome_content_settings_agent_delegate.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/chrome_content_settings_agent_delegate.h"
 
+// TODO(b/197163596): Remove File Manager constants
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/webui/file_manager/url_constants.h"
 #endif
