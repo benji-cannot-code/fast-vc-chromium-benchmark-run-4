919cc7b16396984be30ab48ee6e2872670c51ce5
Brian Sheedy <bsheedy@chromium.org>
2021-08-21 02:42:50 +0000

    Include used expectations files in web_test results
    Includes the basenames of any used expectations files in the results
    uploaded to ResultDB. This will allow us to only look at expectations
    a particular test cares about when running the web tests' stale
    expectation remover.
    
    Bug: 1222827
    Change-Id: Id997cf2f682c1b7345027c0bba7473ad781b7fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3087988
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914080}

--- third_party/blink/tools/blinkpy/web_tests/controllers/test_result_sink_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/controllers/test_result_sink_unittest.py
@@ -134,6 +134,26 @@ class TestResultSinkMessage(TestResultSinkTestBase):
                 'value': ''
             },
             {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'TestExpectations',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'WebDriverExpectations',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'NeverFixTests',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'StaleTestExpectations',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'SlowTests',
+            },
+            {
                 'key': 'raw_typ_expectation',
                 'value': 'Failure'
             },
@@ -168,6 +188,26 @@ class TestResultSinkMessage(TestResultSinkTestBase):
             {
                 'key': 'web_tests_flag_specific_config_name',
                 'value': ''
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'TestExpectations',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'WebDriverExpectations',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'NeverFixTests',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'StaleTestExpectations',
+            },
+            {
+                'key': 'web_tests_used_expectations_file',
+                'value': 'SlowTests',
             },
         ]
         sent_data = self.sink(True, tr)
