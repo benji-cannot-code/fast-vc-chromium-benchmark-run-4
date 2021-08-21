6ac3f815d58ef4693891c12491c5c543c3d333b6
Wez <wez@chromium.org>
2021-08-21 10:26:01 +0000

    [fuchsia] Publish memory usage details to Inspect.
    Set up a "memory" node published by each web_instance, that will gather
    details from the instance's processes regarding their memory usage, on-
    demand.
    
    By default this reports the type and Private Memory Footprint of each
    process.  Additional detail can be included, from named allocator-dumps
    specified in an |allocator-dump-names| argument in the web_instance's
    config-data.
    
    Bug: 1238889
    Change-Id: Id517c52650653e1a1dd681a721d845d6bd301545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094003
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914113}

--- fuchsia/engine/browser/web_engine_browser_main_parts.cc
+++ fuchsia/engine/browser/web_engine_browser_main_parts.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia/engine/browser/media_resource_provider_service.h"
 #include "fuchsia/engine/browser/web_engine_browser_context.h"
 #include "fuchsia/engine/browser/web_engine_devtools_controller.h"
+#include "fuchsia/engine/browser/web_engine_memory_inspector.h"
 #include "fuchsia/engine/common/cast_streaming.h"
 #include "fuchsia/engine/switches.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
@@ -127,6 +128,10 @@ int WebEngineBrowserMainParts::PreMainMessageLoopRun() {
   component_inspector_ = std::make_unique<sys::ComponentInspector>(
       base::ComponentContextForProcess());
   cr_fuchsia::PublishVersionInfoToInspect(component_inspector_.get());
+
+  // Add a node providing memory details for this whole web instance.
+  memory_inspector_ =
+      std::make_unique<WebEngineMemoryInspector>(component_inspector_->root());
 
   const auto* command_line = base::CommandLine::ForCurrentProcess();
 
--- fuchsia/engine/browser/web_engine_browser_main_parts.h
+++ fuchsia/engine/browser/web_engine_browser_main_parts.h
@@ -39,6 +39,7 @@ class ComponentInspector;
 }
 
 class MediaResourceProviderService;
+class WebEngineMemoryInspector;
 
 class WEB_ENGINE_EXPORT WebEngineBrowserMainParts
     : public content::BrowserMainParts {
@@ -88,6 +89,7 @@ class WEB_ENGINE_EXPORT WebEngineBrowserMainParts
 
   // Used to publish diagnostics including the active Contexts and FrameHosts.
   std::unique_ptr<sys::ComponentInspector> component_inspector_;
+  std::unique_ptr<WebEngineMemoryInspector> memory_inspector_;
 
   // Browsing contexts for the connected clients.
   fidl::BindingSet<fuchsia::web::Context, std::unique_ptr<ContextImpl>>
