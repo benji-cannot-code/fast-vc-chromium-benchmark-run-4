34ece22fe28f37242696499fae2d381b46dcb631
François Degros <fdegros@chromium.org>
2021-08-22 08:15:49 +0000

    [Files App] Don't load ZipArchiver extension
    The ZipArchiver extension is not used anymore.
    
    Note that the ZipArchiver extension itself is still installed on the
    system. It will be removed in a subsequent CL.
    
    BUG=chromium:1201052, chromium:912236
    TEST=Manual tests, checking that ZIP files can still be created and mounted
    
    Change-Id: Iad4ee1a6ec5b6eea3a6d0d42966834fe949c6730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105629
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914149}

--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -73,7 +73,6 @@ const char* const kBuiltInFirstPartyExtensionIds[] = {
     kEspeakSpeechSynthesisExtensionId,
     kGoogleSpeechSynthesisExtensionId,
     kWallpaperManagerId,
-    kZipArchiverExtensionId,
 #endif        // BUILDFLAG(IS_CHROMEOS_ASH)
     nullptr,  // Null-terminated array.
 };
@@ -119,8 +118,6 @@ const char kGoogleSpeechSynthesisExtensionPath[] =
 const char kGoogleSpeechSynthesisExtensionId[] =
     "gjjabgpgjpampikjhjpfhneeoapjbjaf";
 const char kWallpaperManagerId[] = "obklkkbkpaoaejdabbfldmcfplpdgolj";
-const char kZipArchiverExtensionId[] = "dmboannefpncccogfdikhmhpmdnddgoe";
-const char kZipArchiverExtensionPath[] = "chromeos/zip_archiver";
 const char kCameraAppPath[] = "chromeos/camera";
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
