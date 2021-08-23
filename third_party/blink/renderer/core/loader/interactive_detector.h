0bc4623ce8ae0ffadce67d544e5b01206adcaa59
Dan McArdle <dmcardle@chromium.org>
2021-08-23 22:38:57 +0000

    Drop DeprecatedAtOrEmptyValue from renderer/core/loader
    This CL replaces a HashMap lookup with a lambda function in
    InteractiveDetector::RecordInputEventTimingUKM().
    
    This is a potential behavior because the lambda will crash when given an
    unknown input rather than implicitly default to the "empty" zero value.
    
    Fixed: 1238909
    Change-Id: I2d94cc9a147c7f96681d7bcf8444b6d8d5c652bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110773
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914562}

--- third_party/blink/renderer/core/loader/interactive_detector.h
+++ third_party/blink/renderer/core/loader/interactive_detector.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/supplementable.h"
 #include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/wtf/pod_interval.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace base {
