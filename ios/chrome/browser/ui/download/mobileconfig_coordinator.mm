013b802fab0160018218b1214609e585261cf937
Ewann <ewannpv@google.com>
2021-08-23 10:55:02 +0000

    [iOS][MobileConfig] Updates alert style
    This CL  makes the continue button bold instead of the cancel button.
    
    Bug: 1241798
    Change-Id: I4eba273b3f7d19c0b34324e8ebefd8e4f00df504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3109988
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914257}

--- ios/chrome/browser/ui/download/mobileconfig_coordinator.mm
+++ ios/chrome/browser/ui/download/mobileconfig_coordinator.mm
@@ -170,7 +170,7 @@ const char kUmaDownloadMobileConfigFileUI[] =
                       kUmaDownloadMobileConfigFileUI,
                       DownloadMobileConfigFileUI::KWarningAlertIsDismissed);
                 }
-                 style:UIAlertActionStyleCancel];
+                 style:UIAlertActionStyleDefault];
 
   __weak MobileConfigCoordinator* weakSelf = self;
   [self.alertCoordinator
@@ -179,7 +179,7 @@ const char kUmaDownloadMobileConfigFileUI[] =
                 action:^{
                   [weakSelf presentSFSafariViewController:fileURL];
                 }
-                 style:UIAlertActionStyleDefault];
+                 style:UIAlertActionStyleCancel];
 
   [self.alertCoordinator start];
 }
