a0ca654cbc2ff654c7105fad78a9a692ace82952
Luciano Pacheco <lucmult@chromium.org>
2021-08-23 05:01:32 +0000

    Files SWA: Fix waitForWindow for SWA
    Fix the test helper function waitForWindow() to work for the SWA, by:
    
    1. In the JS for SWA send the messages `findSwaWindow` and
    `loadSwaTestUtils`to the browser test.
    2. In the browser test, scan `BrowserList` to find the Files SWA
    WebContents.
    3. In the browser test: execute, the newly added, test.getSwaAppId() in
    the WebContents to retrieve the appId.
    
    On step #3 the appId is similar to what was in use in the browser test,
    prior to this CL it was the the SWA URL + the `launchDir`, now it's
    the `currentDirectoryURL` without the prefix `filesystem:` which results
    in something like:
    chrome://file-manager/external/Downloads-user/Downloads/photos
    
    Bug: b:194252786
    Change-Id: I10cdf9b6b44c955559dd8b93fd5cb279ab77f0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3080580
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914185}

--- ash/webui/file_manager/resources/test_util_swa.js
+++ ash/webui/file_manager/resources/test_util_swa.js
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // directly load the Files.app's test_util_base.js due to its dependencies on
 // Chrome APIs not available in SWAs.
 
+import {SWA_FILES_APP_URL} from 'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj/common/js/url_constants.js';
 import {test} from 'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj/background/js/test_util.js';
 import {ScriptLoader} from './script_loader.js'
 
@@ -63,4 +64,16 @@ test.swaLoadTestUtils = async () => {
   }
 
   return result;
+};
+
+test.getSwaAppId = () => {
+  // FIXME: Implement the appId for SWA.
+  let appId = window.appState && window.appState.currentDirectoryURL;
+  if (!appId) {
+    appId = SWA_FILES_APP_URL.toString();
+  }
+
+  appId = appId.replace('filesystem:', '');
+
+  window.domAutomationController.send(appId);
 };
