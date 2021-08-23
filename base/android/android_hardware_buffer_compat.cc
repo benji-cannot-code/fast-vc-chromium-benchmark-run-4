a6091a8f157fac6ecdfc95b5271a06e65b25d426
Vikas Soni <vikassoni@chromium.org>
2021-08-23 19:47:41 +0000

    Add DCHECK in AndroidHardwareBufferCompat::Acquire().
    Passing a null buffer to android ndk AHardwareBuffer_Acquire is not
    allowed and causes seg fault in android framework code. Hence adding a
    dcheck in chrome to catch the error early and for documentation.
    
    Bug: 1241319
    Change-Id: I32b8a6796c8ad07a02d5f9e6c8df56a0972373f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3111530
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914480}

--- base/android/android_hardware_buffer_compat.cc
+++ base/android/android_hardware_buffer_compat.cc
@@ -77,6 +77,11 @@ void AndroidHardwareBufferCompat::Allocate(const AHardwareBuffer_Desc* desc,
 
 void AndroidHardwareBufferCompat::Acquire(AHardwareBuffer* buffer) {
   DCHECK(IsSupportAvailable());
+
+  // Null |buffer| is not allowed by |acquire_| and it fails somewhere in
+  // android framework code. Hence adding a DCHECK here for documenting this
+  // info and fail before.
+  DCHECK(buffer);
   acquire_(buffer);
 }
 
