1830677889b0faf69c96ef3abb5e3a1c4efa78aa
Shimi Zhang <ctzsm@chromium.org>
2021-08-23 16:33:09 +0000

    [CCT] Allow user gesture to resize bottom sheet CCT
    To avoid web content relayout and Window resizing, we change the
    height of the Window to be the full height of the current display at
    the beginning of the dragging.
    
    When we are into the moving stage, we only move the y position of the
    Window. The Window, CompsoitorViewHolder and the underlying SurfaceView
    won't change its size, so the Surface won't change its size,
    therefore we won't get relayout signal because the height of the
    Window isn't changed.
    
    After the user gesture end, we restore the status of the Window to be
    before the start of the touch events.
    
    We also need to use SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION flag
    to make sure the navigation bar layout isn't changed, so when we are
    changing the height of the Window, the navigation bar won't flicker.
    
    Please see http://crbug.com/1210827 for a screen record.
    
    Bug: 1210827
    Change-Id: If02ccab6e5cff5b4dcf05468fcecae4f58bd6aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3080401
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914377}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -104,8 +104,8 @@ public class CustomTabActivity extends BaseCustomTabActivity {
 
         CustomTabNavigationBarController.update(getWindow(), mIntentDataProvider, getResources());
 
-        CustomTabHeightStrategy.createStrategy(
-                this, mIntentDataProvider.getInitialActivityHeight(), getLifecycleDispatcher());
+        CustomTabHeightStrategy.createStrategy(this, mIntentDataProvider.getInitialActivityHeight(),
+                getLifecycleDispatcher(), getMultiWindowModeStateDispatcher());
     }
 
     @Override
