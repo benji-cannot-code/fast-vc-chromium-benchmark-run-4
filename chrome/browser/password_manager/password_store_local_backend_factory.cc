51607643bb8a9d9d121f782d260e19ecbc416973
Friedrich Horschig <fhorschig@chromium.org>
2021-08-23 09:43:48 +0000

    [Passwords] Instantiate Android-specific backend only if internal API exists
    This CL is effectively a noop but it prevents crrev.com/c/2704560 from
    breaking the build if enable_chrome_android_internal = false.
    
    To support local password development, this CL moves the factory methods
    into separate files that are only used if Chrome on Android is compiled
    with internal sources.
    That allows to keep using the local backend on Android and prevents
    attempts to compile Chrome without the missing ChromeSync SDK.
    
    Bug: 1229654
    Change-Id: I86467bc6e713adc3142ad36428cdcee75cda06d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3109867
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914239}

---
+++ chrome/browser/password_manager/password_store_local_backend_factory.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/password_store_backend.h"
+
+#include "components/password_manager/core/browser/login_database.h"
+
+namespace password_manager {
+
+std::unique_ptr<PasswordStoreBackend> PasswordStoreBackend::Create(
+    std::unique_ptr<LoginDatabase> login_db) {
+  // TODO(crbug.com/1217071): Once PasswordStoreImpl does not implement the
+  // PasswordStore abstract class anymore, return a local backend.
+  return nullptr;
+}
+
+}  // namespace password_manager
