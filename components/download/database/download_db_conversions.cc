a8f58a448fd72cb5edaac6adf23d593518c41d7e
Kristi Park <kristipark@google.com>
2021-08-23 21:01:15 +0000

    Revert "[Reland] Pass the credentials mode to the download service."
    This reverts commit 12da809487100d8335a6f50dd6a814cdc92daae4.
    
    Reason for revert: Compile failure https://cr-buildbucket.appspot.com/build/8838078851703480065
    
    Original change's description:
    > [Reland] Pass the credentials mode to the download service.
    >
    > The default is still 'Allow' which will not affect any existing download
    > clients, however the clients will be able to customize the credentials
    > mode as needed.
    >
    > The CL was reverted in https://chromium-review.googlesource.com/c/chromium/src/+/3110756.
    > Compilation error: https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket/8838358063796181409/+/u/compile/raw_io.output_failure_summary_
    >
    > Please review from Patchset 1 to see extra changes
    >
    > Bug: 1239709
    > Change-Id: I486388759928ca503bec7ef17222a1fe53cd94b4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113020
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Auto-Submit: Rayan Kanso <rayankans@chromium.org>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#914449}
    
    Bug: 1239709
    Change-Id: I6ef6ee7e6e94d7f269db0b0a5ffb856565f43714
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114777
    Auto-Submit: Kristi Park <kristipark@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kristi Park <kristipark@google.com>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Owners-Override: Kristi Park <kristipark@google.com>
    Cr-Commit-Position: refs/heads/main@{#914514}

--- components/download/database/download_db_conversions.cc
+++ components/download/database/download_db_conversions.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/pickle.h"
 #include "components/download/public/common/download_features.h"
-#include "services/network/public/mojom/fetch_api.mojom-shared.h"
 
 namespace download {
 namespace {
@@ -226,8 +225,6 @@ download_pb::InProgressInfo DownloadDBConversions::InProgressInfoToProto(
   if (in_progress_info.reroute_info.IsInitialized()) {
     *proto.mutable_reroute_info() = in_progress_info.reroute_info;
   }
-  proto.set_credentials_mode(
-      static_cast<int32_t>(in_progress_info.credentials_mode));
   return proto;
 }
 
@@ -286,10 +283,6 @@ InProgressInfo DownloadDBConversions::InProgressInfoFromProto(
   }
   if (proto.has_reroute_info()) {
     info.reroute_info = proto.reroute_info();
-  }
-  if (proto.has_credentials_mode()) {
-    info.credentials_mode = static_cast<::network::mojom::CredentialsMode>(
-        proto.credentials_mode());
   }
 
   return info;
