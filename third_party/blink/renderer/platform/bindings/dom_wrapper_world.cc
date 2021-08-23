4799f7205cc6a855d05f3a9b43671eb9324dc1ba
Dan McArdle <dmcardle@chromium.org>
2021-08-23 17:17:55 +0000

    Drop DeprecatedAtOrEmptyValue from renderer/platform/bindings
    Fixed: 1236742
    Change-Id: Ia8fdcb263b443329180e6e1413a6b58ae256c823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110757
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914405}

--- third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
+++ third_party/blink/renderer/platform/bindings/dom_wrapper_world.cc
@@ -208,7 +208,9 @@ static IsolatedWorldStableIdMap& IsolatedWorldStableIds() {
 
 String DOMWrapperWorld::NonMainWorldStableId() const {
   DCHECK(!IsMainWorld());
-  return IsolatedWorldStableIds().DeprecatedAtOrEmptyValue(GetWorldId());
+  const auto& map = IsolatedWorldStableIds();
+  const auto it = map.find(GetWorldId());
+  return it != map.end() ? it->value : String();
 }
 
 void DOMWrapperWorld::SetNonMainWorldStableId(int32_t world_id,
@@ -228,8 +230,9 @@ static IsolatedWorldHumanReadableNameMap& IsolatedWorldHumanReadableNames() {
 
 String DOMWrapperWorld::NonMainWorldHumanReadableName() const {
   DCHECK(!IsMainWorld());
-  return IsolatedWorldHumanReadableNames().DeprecatedAtOrEmptyValue(
-      GetWorldId());
+  const auto& map = IsolatedWorldHumanReadableNames();
+  const auto it = map.find(GetWorldId());
+  return it != map.end() ? it->value : String();
 }
 
 void DOMWrapperWorld::SetNonMainWorldHumanReadableName(
