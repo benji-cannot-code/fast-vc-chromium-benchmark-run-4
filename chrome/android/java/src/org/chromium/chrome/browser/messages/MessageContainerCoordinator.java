34bf4bc3d04c1d2cbb8033bdce5cd38cd09ba6cf
Stephen Nusko <nuskos@chromium.org>
2021-08-23 17:05:56 +0000

    Prevent unneeded layouts of the MessageContainer when it isn't visible.
    We include a finch experiment to enable/disable this is so we can
    measure its effect. We default on because 300 vs 3 looper calls during a
    scroll seems like a obvious improvement and this fixes a bug introduced
    in M91 with crrev.com/c/2792802 which triggered excessive view draws.
    
    Bug: 1235338
    Change-Id: I90e0e123d47a97f2ce64d9f6224494b9ff86ec7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110349
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914397}

--- chrome/android/java/src/org/chromium/chrome/browser/messages/MessageContainerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/messages/MessageContainerCoordinator.java
@@ -13,6 +13,7 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.fullscreen.BrowserControlsManager;
 import org.chromium.components.messages.MessageContainer;
 
@@ -38,6 +39,11 @@ public class MessageContainerCoordinator implements BrowserControlsStateProvider
     }
 
     private void updateMargins() {
+        if (mContainer.getVisibility() != View.VISIBLE
+                && ChromeFeatureList.isEnabled(
+                        ChromeFeatureList.MESSAGES_FOR_ANDROID_REDUCE_LAYOUT_CHANGES)) {
+            return;
+        }
         CoordinatorLayout.LayoutParams params =
                 (CoordinatorLayout.LayoutParams) mContainer.getLayoutParams();
         params.topMargin = getContainerTopOffset();
