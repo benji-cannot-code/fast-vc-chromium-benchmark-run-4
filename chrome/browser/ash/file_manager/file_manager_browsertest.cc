960ddfff9d199b9c7df5fb9b42c1c2febc8b5ae4
Luciano Pacheco <lucmult@chromium.org>
2021-08-23 06:09:04 +0000

    Files SWA: Fix OpenItem for SWA
    Change ExecuteFileTask() to redirect the tasks for Files app extension
    to the SWA when the SWA flag is enabled.
    
    Bug: b:194252786
    Change-Id: Ide038d3e65dcd846479d8eab48f5be433a8198fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3080581
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914195}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -267,6 +267,9 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("fileDisplayDownloads").FilesSwa().TabletMode(),
         TestCase("fileDisplayLaunchOnLocalFolder").DontObserveFileTasks(),
         TestCase("fileDisplayLaunchOnDrive").DontObserveFileTasks(),
+        TestCase("fileDisplayLaunchOnLocalFolder")
+            .DontObserveFileTasks()
+            .FilesSwa(),
         TestCase("fileDisplayDrive").TabletMode(),
         TestCase("fileDisplayDrive"),
         TestCase("fileDisplayDriveOffline").Offline(),
