8c14cebdd5a315b9d795e9df66cca763e99746e2
Sylvain Defresne <sdefresne@chromium.org>
2021-08-23 17:12:57 +0000

    [ios] Expose `overrides` API as free function
    Add a free function exposing the `overrides` API with the goal
    to replace `OverridesProvider` class. Do not convert the code
    to use the functions as it has no internal implementation yet.
    
    This is part of a multi-CL change:
     => https://crrev.com/c/3113926
      - https://crrev.com/i/4078248
      - https://crrev.com/c/3114067
      - https://crrev.com/i/4078249
      - https://crrev.com/c/3113927
    
    Bug: 1201182
    Change-Id: Ieed6c9a44788db7cdb108bef62b4e0c2c46315fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113926
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914401}

---
+++ ios/public/provider/chrome/browser/overrides/overrides_api.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_OVERRIDES_OVERRIDES_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_OVERRIDES_OVERRIDES_API_H_
+
+namespace ios {
+namespace provider {
+
+// Install overrides.
+void InstallOverrides();
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_OVERRIDES_OVERRIDES_API_H_
---
+++ ios/public/provider/chrome/browser/overrides/test_overrides.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/public/provider/chrome/browser/overrides/overrides_api.h"
+
+namespace ios {
+namespace provider {
+
+void InstallOverrides() {
+  // No overrides for tests.
+}
+
+}  // namespace provider
+}  // namespace ios
