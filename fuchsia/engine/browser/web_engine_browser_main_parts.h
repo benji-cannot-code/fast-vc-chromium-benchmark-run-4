6bc6c1f73324f17b93b65761a016161329d22edd
Maxim Kolosovskiy <kolos@chromium.org>
2021-08-23 11:09:34 +0000

    Revert "[fuchsia] Publish memory usage details to Inspect."
    This reverts commit 3fa3172fdddfeff8de140b8b81c1d27d6c14e19e.
    
    Reason for revert: The CL likely caused service_unittests failures.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/40578/test-results
    [ RUN      ] TracingObserverProtoTest.AddChromeDumpToTraceIfEnabled_When_TraceLog_Disabled
    FATAL services_unittests[31665:31712]: [sequence_checker.h(136)] Check failed: checker.CalledOnValidSequence(&bound_at).
        #0 0x557c6c97d3db in backtrace /b/s/w/ir/cache/builder/src/third_party/llvm/compiler-rt/lib/asan/../sanitizer_common/sanitizer_common_interceptors.inc:4192:13
        #1 0x557c7761a0d9 in base::debug::CollectStackTrace(void**, unsigned long) ./../../base/debug/stack_trace_posix.cc:867:39
        #2 0x557c773a7583 in StackTrace ./../../base/debug/stack_trace.cc:200:12
        #3 0x557c773a7583 in base::debug::StackTrace::StackTrace() ./../../base/debug/stack_trace.cc:197:28
    
    
    Original change's description:
    > [fuchsia] Publish memory usage details to Inspect.
    >
    > Set up a "memory" node published by each web_instance, that will gather
    > details from the instance's processes regarding their memory usage, on-
    > demand.
    >
    > By default this reports the type and Private Memory Footprint of each
    > process.  Additional detail can be included, from named allocator-dumps
    > specified in an |allocator-dump-names| argument in the web_instance's
    > config-data.
    >
    > Bug: 1238889
    > Change-Id: Id517c52650653e1a1dd681a721d845d6bd301545
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3094003
    > Commit-Queue: Wez <wez@chromium.org>
    > Auto-Submit: Wez <wez@chromium.org>
    > Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#914113}
    
    Bug: 1238889
    Change-Id: I4dbc3d2bbc752709c105c4919f7df61ae55fb3cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110459
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Maxim Kolosovskiy  <kolos@chromium.org>
    Auto-Submit: Maxim Kolosovskiy  <kolos@chromium.org>
    Commit-Queue: Maxim Kolosovskiy  <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914261}

--- fuchsia/engine/browser/web_engine_browser_main_parts.h
+++ fuchsia/engine/browser/web_engine_browser_main_parts.h
@@ -39,7 +39,6 @@ class ComponentInspector;
 }
 
 class MediaResourceProviderService;
-class WebEngineMemoryInspector;
 
 class WEB_ENGINE_EXPORT WebEngineBrowserMainParts
     : public content::BrowserMainParts {
@@ -89,7 +88,6 @@ class WEB_ENGINE_EXPORT WebEngineBrowserMainParts
 
   // Used to publish diagnostics including the active Contexts and FrameHosts.
   std::unique_ptr<sys::ComponentInspector> component_inspector_;
-  std::unique_ptr<WebEngineMemoryInspector> memory_inspector_;
 
   // Browsing contexts for the connected clients.
   fidl::BindingSet<fuchsia::web::Context, std::unique_ptr<ContextImpl>>
