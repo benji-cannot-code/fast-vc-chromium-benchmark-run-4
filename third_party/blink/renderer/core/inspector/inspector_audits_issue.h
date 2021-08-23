e3034a5051e90577a1e5934f70141da84af94b4a
Kateryna Prokopenko <kprokopenko@chromium.org>
2021-08-23 15:01:50 +0000

    Migrate SharedBufferIssue reporting in renderer to AuditsIssue
    Non-functional change, refactoring, see bug description for details
    Tested with third_party/blink/web_tests/http/tests/inspector-protocol/issues/sab-creation-issue.js
    
    Bug: 1238337
    Change-Id: Ia4a29d70218fb8bad092f2b79d12e4d7db18b917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3093590
    Commit-Queue: Kateryna Prokopenko <kprokopenko@google.com>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914334}

--- third_party/blink/renderer/core/inspector/inspector_audits_issue.h
+++ third_party/blink/renderer/core/inspector/inspector_audits_issue.h
@@ -41,6 +41,11 @@ enum class AttributionReportingIssueType {
   kAttributionUntrustworthyOrigin,
 };
 
+enum class SharedArrayBufferIssueType {
+  kTransferIssue,
+  kCreationIssue,
+};
+
 // |AuditsIssue| is a thin wrapper around the Audits::InspectorIssue
 // protocol class.
 //
@@ -107,6 +112,11 @@ class CORE_EXPORT AuditsIssue {
       WTF::String source_origin,
       WTF::String target_origin,
       bool is_warning);
+
+  static void ReportSharedArrayBufferIssue(
+      ExecutionContext* execution_context,
+      bool shared_buffer_transfer_allowed,
+      SharedArrayBufferIssueType issue_type);
 
  private:
   explicit AuditsIssue(std::unique_ptr<protocol::Audits::InspectorIssue> issue);
