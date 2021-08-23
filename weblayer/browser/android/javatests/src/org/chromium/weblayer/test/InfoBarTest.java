db103d994e2032ed74084eef46c6b141d6fee33a
Andrew Grieve <agrieve@chromium.org>
2021-08-23 21:02:53 +0000

    Disable flaky tests within weblayer_private_instrumentation_test_apk
    Bug: 1223953
    Change-Id: Ia2aa6e2676e207b37d634cf06f7283e884730645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114987
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914515}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/InfoBarTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/InfoBarTest.java
@@ -33,6 +33,7 @@ import org.chromium.weblayer.shell.InstrumentationActivity;
  */
 @RunWith(WebLayerJUnit4ClassRunner.class)
 @CommandLineFlags.Add("enable-features=ImmediatelyHideBrowserControlsForTest")
+@DisabledTest(message = "crbug.com/1223953, crbug.com/1098625")
 public class InfoBarTest {
     @Rule
     public InstrumentationActivityTestRule mActivityTestRule =
@@ -116,7 +117,6 @@ public class InfoBarTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "crbug.com/1098625")
     /**
      * Tests that infobars respond to scrolling.
      *
