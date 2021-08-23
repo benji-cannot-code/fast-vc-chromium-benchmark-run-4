f73a1caa36eb69a4b2518f98062d3fbae2224756
Ayu Ishii <ayui@chromium.org>
2021-08-23 22:11:24 +0000

    StorageBuckets: Create bucket on access for CacheStorage
    This change ensures that a default bucket for a storage key
    exists on CacheStorage access. This calls to get or create a
    default bucket for the storage key to make sure there is
    an entry in the QuotaDatabase buckets table. This is a
    requirement for migrating Storage APIs to Buckets.
    
    Bug: 1240334
    Change-Id: I2cfe746a7796b2f93a95120e5b1e39eb12e5035b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3100560
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914541}

--- content/browser/cache_storage/cache_storage_context_impl.h
+++ content/browser/cache_storage/cache_storage_context_impl.h
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/sequence_bound.h"
+#include "components/services/storage/public/cpp/quota_error_or.h"
 #include "components/services/storage/public/mojom/blob_storage_context.mojom.h"
 #include "components/services/storage/public/mojom/cache_storage_control.mojom.h"
 #include "components/services/storage/public/mojom/quota_client.mojom-forward.h"
@@ -33,6 +35,7 @@ class SequencedTaskRunner;
 
 namespace storage {
 class QuotaManagerProxy;
+struct BucketInfo;
 }
 
 namespace content {
@@ -49,14 +52,14 @@ class CacheStorageManager;
 class CONTENT_EXPORT CacheStorageContextImpl
     : public storage::mojom::CacheStorageControl {
  public:
-  CacheStorageContextImpl();
+  explicit CacheStorageContextImpl(
+      scoped_refptr<storage::QuotaManagerProxy> quota_manager_proxy);
   ~CacheStorageContextImpl() override;
 
   static scoped_refptr<base::SequencedTaskRunner> CreateSchedulerTaskRunner();
 
   void Init(mojo::PendingReceiver<storage::mojom::CacheStorageControl> control,
             const base::FilePath& user_data_directory,
-            scoped_refptr<storage::QuotaManagerProxy> quota_manager_proxy,
             mojo::PendingReceiver<storage::mojom::QuotaClient>
                 cache_storage_client_remote,
             mojo::PendingReceiver<storage::mojom::QuotaClient>
@@ -91,6 +94,15 @@ class CONTENT_EXPORT CacheStorageContextImpl
  private:
   SEQUENCE_CHECKER(sequence_checker_);
 
+  void AddReceiverWithBucketInfo(
+      const network::CrossOriginEmbedderPolicy& cross_origin_embedder_policy,
+      mojo::PendingRemote<network::mojom::CrossOriginEmbedderPolicyReporter>
+          coep_reporter_remote,
+      const blink::StorageKey& storage_key,
+      storage::mojom::CacheStorageOwner owner,
+      mojo::PendingReceiver<blink::mojom::CacheStorage> receiver,
+      storage::QuotaErrorOr<storage::BucketInfo> result);
+
   // The set of storage keys whose storage should be cleared on shutdown.
   std::set<blink::StorageKey> storage_keys_to_purge_on_shutdown_;
 
@@ -103,6 +115,10 @@ class CONTENT_EXPORT CacheStorageContextImpl
   mojo::ReceiverSet<storage::mojom::CacheStorageControl> receivers_;
 
   std::unique_ptr<CacheStorageDispatcherHost> dispatcher_host_;
+
+  const scoped_refptr<storage::QuotaManagerProxy> quota_manager_proxy_;
+
+  base::WeakPtrFactory<CacheStorageContextImpl> weak_factory_{this};
 };
 
 }  // namespace content
