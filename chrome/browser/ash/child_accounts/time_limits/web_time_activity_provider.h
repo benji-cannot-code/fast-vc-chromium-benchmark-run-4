c7b51bd7eaf1c3a9a754fe599c1df6cecfa473d9
Raymes Khoury <raymes@chromium.org>
2021-08-23 01:56:41 +0000

    Use InstanceKey in AppServiceWrapper::EventListener callbacks
    This removes the dependency on InstanceKey.Window() from
    AppServiceWrapper::EventListener callbacks. The aura::Window won't be
    available for web apps in tabs in Lacros so we need to remove direct
    usage of it.
    
    BUG=1235206
    
    Change-Id: I11109394753bdd23075f41f0dd6e3d8aae21c6df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098825
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914171}

--- chrome/browser/ash/child_accounts/time_limits/web_time_activity_provider.h
+++ chrome/browser/ash/child_accounts/time_limits/web_time_activity_provider.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 
-namespace aura {
-class Window;
-}  // namespace aura
-
 namespace base {
 class Time;
 }  // namespace base
@@ -62,10 +58,10 @@ class WebTimeActivityProvider : public WebTimeNavigationObserver::EventListener,
 
   // AppServiceWrapper::EventListener:
   void OnAppActive(const AppId& app_id,
-                   aura::Window* window,
+                   const apps::Instance::InstanceKey& instance_key,
                    base::Time timestamp) override;
   void OnAppInactive(const AppId& app_id,
-                     aura::Window* window,
+                     const apps::Instance::InstanceKey& instance_key,
                      base::Time timestamp) override;
 
   ChromeAppActivityState chrome_app_activty_state() const {
