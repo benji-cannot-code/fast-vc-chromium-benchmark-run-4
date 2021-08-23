a3e07655a7c638c07aecd50d3a71df42876166b6
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2021-08-23 15:41:31 +0000

    [iOS][Credential-Provider] Generate strong passwords
    Bug: 1224986
    Change-Id: Ic68c9be2623e0fb81abe32d1f258961913236bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088163
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Javier Flores <javierrobles@chromium.org>
    Auto-Submit: Javier Flores <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914351}

--- ios/chrome/credential_provider_extension/ui/new_password_view_controller.mm
+++ ios/chrome/credential_provider_extension/ui/new_password_view_controller.mm
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/credential_provider_extension/ui/new_password_view_controller.h"
 
 #include "base/notreached.h"
+#include "base/strings/sys_string_conversions.h"
+#include "components/autofill/core/browser/proto/password_requirements.pb.h"
+#include "components/password_manager/core/browser/generation/password_generator.h"
 #include "ios/chrome/common/app_group/app_group_metrics.h"
 #import "ios/chrome/common/credential_provider/archivable_credential.h"
 #import "ios/chrome/common/credential_provider/constants.h"
@@ -15,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+using autofill::GeneratePassword;
+using autofill::PasswordRequirementsSpec;
+using base::SysUTF16ToNSString;
 
 @interface NewPasswordViewController () <UITableViewDataSource,
                                          NewPasswordTableCellDelegate>
@@ -128,8 +135,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   NewPasswordTableCell* passwordCell = [self passwordCell];
 
-  // TODO(crbug.com/1224986): Generate password and fill it in.
-  passwordCell.textField.text = @"";
+  // TODO(crbug.com/1224986): Fetch password spec.
+  PasswordRequirementsSpec spec;
+  passwordCell.textField.text = SysUTF16ToNSString(GeneratePassword(spec));
 
   [self updateSaveButtonState];
 
