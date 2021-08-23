e7807fea36308c2e74afe2b3c3a3d0c5e29f5ad7
Elly Fong-Jones <ellyjones@chromium.org>
2021-08-23 21:04:03 +0000

    chrome: remove window naming feature & flag
    This feature has been launched for quite a while and no longer needs
    these holdbacks.
    
    Bug: None
    Change-Id: I63c3a59035d385592fa5a2c5448174bee165676b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114772
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914518}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -739,9 +739,6 @@ extern const base::Feature kWin10AcceleratedDefaultBrowserFlow;
 #endif  // defined(OS_WIN)
 
 COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::Feature kWindowNaming;
-
-COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kWriteBasicSystemProfileToPersistentHistogramsFile;
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
