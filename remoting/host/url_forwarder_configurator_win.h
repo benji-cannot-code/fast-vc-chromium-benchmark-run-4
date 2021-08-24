fc946170144685e48ae96b1eb317d65bfc418398
Yuwei Huang <yuweih@chromium.org>
2021-08-24 22:38:46 +0000

    [remoting host][win] Delay IsUrlForwarderSetUp if user not logged in
    It appears that in uncurtained session, WTS creates a single console
    session whenever the login screen is shown, and the same session will
    be used after the user has signed in. This is problematic for the
    client to determine the URL forwarder state when the user has not
    logged in, since:
    
    * The host can't determine the state when the user is in login screen
      due to the lack of the user context
    * DesktopSessionAgent isn't recreated after the user is logged in, so
      it won't report the URL forwarder state at that point where the user
      context becomes known
    
    This CL prevents this issue by making the IsUrlForwarderSetUp call wait
    for the user to log in. In curtain mode, a new session will be created
    when the user logs in, so this logic isn't very helpful for that case,
    but it still prevents the desktop process from reporting NOT_SET_UP
    when the user context is not known; the relaunched desktop process will
    report the correct state.
    
    This will also require client side change that removes the timeout and
    hides the checkbox when the IsUrlForwarderSetUp response is pending.
    
    Bug: b:183135000
    Change-Id: Ic492e0d8361d9d9ae77f1032c7f964bc5f182bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3108516
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914936}

--- remoting/host/url_forwarder_configurator_win.h
+++ remoting/host/url_forwarder_configurator_win.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_HOST_URL_FORWARDER_CONFIGURATOR_WIN_H_
 #define REMOTING_HOST_URL_FORWARDER_CONFIGURATOR_WIN_H_
 
+#include <stdint.h>
+#include <memory>
+
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
@@ -15,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/url_forwarder_configurator.h"
 
 namespace remoting {
+
+class WtsSessionChangeObserver;
 
 // Windows implementation of UrlForwarderConfigurator.
 // Note that this class requires elevated privileges.
@@ -33,10 +38,26 @@ class UrlForwarderConfiguratorWin final : public UrlForwarderConfigurator {
  private:
   void OnSetUpResponse(bool success);
   void OnReportUserInterventionRequired();
+  void OnWtsSessionChange(uint32_t event, uint32_t session_id);
 
   SEQUENCE_CHECKER(sequence_checker_);
 
   scoped_refptr<base::SequencedTaskRunner> io_task_runner_;
+
+  // For uncurtained sessions, Windows reuses the same console session before
+  // and after the user logs in. So we use these members to delay checking the
+  // URL forwarder state until the user is logged in. They are valid (non-null)
+  // only when the session is not logged in.
+  // For curtained sessions, Windows creates a new session whenever the user
+  // logs in, in which case the desktop process will be destroyed along with the
+  // URL configurator and the pending requests, and a new desktop process will
+  // be launched and IsUrlForwarderSetUp() will be called with a valid user
+  // context.
+  std::unique_ptr<WtsSessionChangeObserver> wts_session_change_observer_
+      GUARDED_BY_CONTEXT(sequence_checker_);
+  IsUrlForwarderSetUpCallback is_url_forwarder_set_up_callback_
+      GUARDED_BY_CONTEXT(sequence_checker_);
+
   SetUpUrlForwarderCallback set_up_url_forwarder_callback_
       GUARDED_BY_CONTEXT(sequence_checker_);
   base::OneShotTimer report_user_intervention_required_timer_
