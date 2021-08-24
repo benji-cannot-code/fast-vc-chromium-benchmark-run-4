03ada68157edc9d188884597e75186a54257298d
Raymes Khoury <raymes@chromium.org>
2021-08-24 08:26:46 +0000

    Add BrowserTest coverage for InstanceKey::IsForWebBasedApp
    This adds browser test coverage for InstanceKey::IsForWebBasedApp
    to ensure that it's set correctly for various types of app instances.
    
    BUG=1235206
    
    Change-Id: I5f01346f4bd6f6e979a386735a7f54ce80f3e57d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113260
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914655}

--- chrome/browser/ui/ash/shelf/app_service/app_service_app_window_browsertest.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_app_window_browsertest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/test/with_crosapi_param.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/session/arc_bridge_service.h"
 #include "components/arc/test/arc_util_test_support.h"
@@ -315,6 +316,7 @@ IN_PROC_BROWSER_TEST_F(AppServiceAppWindowBrowserTest,
       app_service_proxy_->InstanceRegistry().GetInstanceKeys(app_id1);
   EXPECT_EQ(1u, instance_keys.size());
   auto instance_key1 = *instance_keys.begin();
+  EXPECT_TRUE(instance_key1.IsForWebBasedApp());
 
   // The window1 is active.
   EXPECT_EQ(apps::InstanceState::kStarted | apps::InstanceState::kRunning |
@@ -331,6 +333,7 @@ IN_PROC_BROWSER_TEST_F(AppServiceAppWindowBrowserTest,
       app_service_proxy_->InstanceRegistry().GetInstanceKeys(app_id2);
   EXPECT_EQ(1u, instance_keys.size());
   auto instance_key2 = *instance_keys.begin();
+  EXPECT_FALSE(instance_key2.IsForWebBasedApp());
 
   // The window1 is inactive.
   EXPECT_EQ(apps::InstanceState::kStarted | apps::InstanceState::kRunning |
@@ -363,6 +366,16 @@ IN_PROC_BROWSER_TEST_F(AppServiceAppWindowBrowserTest,
   EXPECT_EQ(0u, instance_keys.size());
 }
 
+IN_PROC_BROWSER_TEST_F(AppServiceAppWindowBrowserTest, AshBrowserWindow) {
+  ui_test_utils::NavigateToURL(browser(), GURL("chrome://blank"));
+
+  auto instance_keys = app_service_proxy_->InstanceRegistry().GetInstanceKeys(
+      extension_misc::kChromeAppId);
+  EXPECT_EQ(1u, instance_keys.size());
+  auto instance_key = *instance_keys.begin();
+  EXPECT_FALSE(instance_key.IsForWebBasedApp());
+}
+
 class AppServiceAppWindowLacrosBrowserTest
     : public AppServiceAppWindowBrowserTest {
  public:
@@ -388,6 +401,12 @@ IN_PROC_BROWSER_TEST_F(AppServiceAppWindowLacrosBrowserTest, LacrosWindow) {
             GetAppInstanceState(kLacrosAppId,
                                 apps::Instance::InstanceKey::ForWindowBasedApp(
                                     *windows.begin())));
+
+  auto instance_keys = app_service_proxy_->InstanceRegistry().GetInstanceKeys(
+      extension_misc::kLacrosAppId);
+  EXPECT_EQ(1u, instance_keys.size());
+  auto instance_key = *instance_keys.begin();
+  EXPECT_FALSE(instance_key.IsForWebBasedApp());
 
   // Find the Lacros shelf item.
   int lacros_index = shelf_model()->ItemIndexByAppID(kLacrosAppId);
@@ -567,6 +586,7 @@ IN_PROC_BROWSER_TEST_F(AppServiceAppWindowWebAppBrowserTest, WebAppsWindow) {
   auto instance_keys =
       app_service_proxy_->InstanceRegistry().GetInstanceKeys(app_id);
   EXPECT_EQ(1u, instance_keys.size());
+  EXPECT_TRUE(instance_keys.begin()->IsForWebBasedApp());
   apps::Instance::InstanceKey instance_key = *instance_keys.begin();
   EXPECT_EQ(apps::InstanceState::kStarted | apps::InstanceState::kRunning |
                 apps::InstanceState::kActive | apps::InstanceState::kVisible,
