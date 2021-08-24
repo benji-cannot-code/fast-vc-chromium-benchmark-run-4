0853d10e8f1a867e0399b5a60a99840c5276bf72
Hongbo Song <hbsong@google.com>
2021-08-24 00:04:53 +0000

    Revert "Set mouse events trusted before creating event timing."
    This reverts commit bd20366d28735afadf65246350dde51454690c8a.
    
    Reason for revert:
    This CL caused some chrome crashes: https://bugs.chromium.org/p/chromium/issues/detail?id=1242083
    
    I suspect there might be some unexpected mouse_event types that appeared in mouse_event_manager.cc
    
    
    Original change's description:
    > Set mouse events trusted before creating event timing.
    >
    > To create an event timing, the event has to be trusted. Because we are
    > about to dispatch those mouse events in mouse_event_manager.cc, I think
    > it's ok to set them trusted here.
    >
    > Change-Id: Ib8354a232caaed781b338130c43ad5684c639867
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3104525
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Reviewed-by: Hongbo Song <hbsong@google.com>
    > Commit-Queue: Hongbo Song <hbsong@google.com>
    > Cr-Commit-Position: refs/heads/main@{#913516}
    
    Bug: 1242083,1242062
    Change-Id: I774a82081e1c189d2a1b1dd85a2491aa27b5d116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114185
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Hongbo Song <hbsong@google.com>
    Cr-Commit-Position: refs/heads/main@{#914576}

--- third_party/blink/renderer/core/input/mouse_event_manager.cc
+++ third_party/blink/renderer/core/input/mouse_event_manager.cc
@@ -327,7 +327,6 @@ WebInputEventResult MouseEventManager::DispatchMouseEvent(
           mouse_event.FromTouch() ? MouseEvent::kFromTouch
                                   : MouseEvent::kRealOrIndistinguishable,
           mouse_event.menu_source_type);
-      event->SetTrusted(true);
       if (frame_ && frame_->DomWindow())
         event_timing = EventTiming::Create(frame_->DomWindow(), *event);
       if (should_dispatch) {
@@ -344,7 +343,6 @@ WebInputEventResult MouseEventManager::DispatchMouseEvent(
           mouse_event.FromTouch() ? MouseEvent::kFromTouch
                                   : MouseEvent::kRealOrIndistinguishable,
           mouse_event.menu_source_type);
-      event->SetTrusted(true);
       if (frame_ && frame_->DomWindow())
         event_timing = EventTiming::Create(frame_->DomWindow(), *event);
       if (should_dispatch) {
