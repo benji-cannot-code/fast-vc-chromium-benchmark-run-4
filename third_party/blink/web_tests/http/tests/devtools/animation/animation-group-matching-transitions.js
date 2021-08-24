a8b626ed510dd243d4c90e8d0f939f2e8a95c01f
Tim van der Lippe <tvanderlippe@chromium.org>
2021-08-24 07:56:09 +0000

    Load legacy modules for panels/animation
    The module.json file will be removed in https://crrev.com/c/3109529 and
    the layout tests don't require the CSS files anymore.
    
    R=jacktfranklin@chromium.org
    
    Bug: 1190991
    Change-Id: I1573764001a7b5deabdb38854d9fbf923bc50549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110345
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914648}

--- third_party/blink/web_tests/http/tests/devtools/animation/animation-group-matching-transitions.js
+++ third_party/blink/web_tests/http/tests/devtools/animation/animation-group-matching-transitions.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   TestRunner.addResult(
       `Tests the matching performed in AnimationModel of groups composed of transitions, which are applied through a variety of selectors.\n`);
   await TestRunner.loadModule('elements'); await TestRunner.loadTestModule('elements_test_runner');
-  await TestRunner.loadModule('animation');
+  await TestRunner.loadLegacyModule('animation');
   await TestRunner.showPanel('elements');
   await TestRunner.loadHTML(`
       <style>
