e5c538dfbd26a57e17baee89d6e751de23c47f4f
Nancy Wang <nancylingwang@chromium.org>
2021-08-24 02:43:01 +0000

    Call MarkSourceForDeletion to delete the UKM source id.
    The UKM source id is no longer deleted automatically:
    1. CL:3088637 modified the UKM source id, no longer deleted
    automatically, to resolve the null app id/url issue for the app usage
    UKM.
    2. CL:3092848 exposes the interface MarkSourceForDeletion to delete the
    source id.
    
    All UKM clients using the APP_ID type source id should call
    MarkSourceForDeletion to delete the source id after using it.
    
    This CL modified BadgeManager and AppLaunchEventLogger to call
    MarkSourceForDeletion to delete the source id after emitting the UKM.
    
    BUG=1242249
    
    Change-Id: I380a2d01d5139e4ca63324bea4c71eb1df9cbbd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3112939
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914615}

--- chrome/browser/badging/badge_manager.cc
+++ chrome/browser/badging/badge_manager.cc
@@ -217,6 +217,7 @@ void BadgeManager::SetBadge(blink::mojom::BadgeValuePtr mojo_value) {
           .SetUpdateAppBadge(kSetNumericBadge)
           .Record(recorder);
     }
+    ukm::AppSourceUrlRecorder::MarkSourceForDeletion(source_id);
 
     UpdateBadge(/*app_id=*/std::get<0>(app), absl::make_optional(value));
   }
@@ -235,6 +236,7 @@ void BadgeManager::ClearBadge() {
     ukm::builders::Badging(source_id)
         .SetUpdateAppBadge(kClearBadge)
         .Record(recorder);
+    ukm::AppSourceUrlRecorder::MarkSourceForDeletion(source_id);
     UpdateBadge(/*app_id=*/std::get<0>(app), absl::nullopt);
   }
 }
