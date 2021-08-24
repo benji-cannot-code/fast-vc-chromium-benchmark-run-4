0d3e1854f0e77ef933073e19464d205f8dfd91e1
Frank Liberato <liberato@chromium.org>
2021-08-24 23:48:59 +0000

    Disable broken SSL tests.
    Broken on mac 10.12
    
    Bug: 1243074
    Change-Id: I5185ccc4fce7581183602fd1dfacf180fda51ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116812
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Auto-Submit: Frank Liberato <liberato@chromium.org>
    Owners-Override: Frank Liberato <liberato@google.com>
    Cr-Commit-Position: refs/heads/main@{#914973}

--- chrome/browser/ssl/stateful_ssl_host_state_delegate_test.cc
+++ chrome/browser/ssl/stateful_ssl_host_state_delegate_test.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/simple_test_clock.h"
+#include "build/build_config.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_remover_constants.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/extensions/chrome_test_extension_loader.h"
@@ -578,8 +579,17 @@ IN_PROC_BROWSER_TEST_F(IncognitoSSLHostStateDelegateTest, AfterRestart) {
                                          incognito_tab));
 }
 
+#if defined(OS_MAC)
+// TODO(https://crbug.com/1243074): Disabled for brokenness.
+#define MAYBE_PRE_AfterRestartHttp DISABLED_PRE_AfterRestartHttp
+#define MAYBE_AfterRestartHttp DISABLED_AfterRestartHttp
+#else // OS_MAC
+#define MAYBE_PRE_AfterRestartHttp PRE_AfterRestartHttp
+#define MAYBE_AfterRestartHttp AfterRestartHttp
+#endif  // !OS_MAC
+
 IN_PROC_BROWSER_TEST_F(IncognitoSSLHostStateDelegateTest,
-                       PRE_AfterRestartHttp) {
+                       MAYBE_PRE_AfterRestartHttp) {
   auto* tab = browser()->tab_strip_model()->GetActiveWebContents();
   auto* profile = Profile::FromBrowserContext(tab->GetBrowserContext());
   auto* state = profile->GetSSLHostStateDelegate();
@@ -607,7 +617,8 @@ IN_PROC_BROWSER_TEST_F(IncognitoSSLHostStateDelegateTest,
 
 // AfterRestartHttp ensures that any HTTP decisions made in an incognito profile
 // are forgetten after a session restart.
-IN_PROC_BROWSER_TEST_F(IncognitoSSLHostStateDelegateTest, AfterRestartHttp) {
+IN_PROC_BROWSER_TEST_F(IncognitoSSLHostStateDelegateTest,
+                       MAYBE_AfterRestartHttp) {
   auto* tab = browser()->tab_strip_model()->GetActiveWebContents();
   auto* profile = Profile::FromBrowserContext(tab->GetBrowserContext());
   auto* state = profile->GetSSLHostStateDelegate();
