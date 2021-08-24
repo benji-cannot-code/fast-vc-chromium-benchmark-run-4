2707693ca358377ed3b2a08e2a08d073bf74b65c
Ashley Prasad <ashleydp@google.com>
2021-08-24 17:20:10 +0000

    Reland "Diagnostics: Run routine_log file operations on blockable thread"
    This is a reland of 4db5af7b3c99ba817233c303c20451cd732695dd
    
    Uses TaskEnvironment::RunUntilIdle() and MOCK_TIMER to ensure tests
    with expectations related to routine_log writing to disk complete
    prior to attempting to assert.  Originally test related to RoutineLog
    were using base::RunLoop however upon local testing with
    "--gtest_repeat" this configuration causes intermittent crashes
    and/or test failures.
    
    Original change's description:
    > Diagnostics: Run routine_log file operations on blockable thread
    >
    >
    > - Adds SequenceTaskRunner run tasks which allow blocking.
    > - Adds helper function for I/O operation to ensure file always exists
    >   before attempting to append contents to file.
    > - Resolves DCHECK occurring in routine logging functions by moving
    >   file operations to run on blockable thread.
    > - Tests log files are updated as expected when scheduled tasks complete.
    >
    > Bug: 1197335
    > Test: ./out/Default/ash_webui_unittests --gtest_filter=RoutineLog*
    > Change-Id: I2c9c7a143ec687d95d4e759a440b69917ebdc561
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098888
    > Commit-Queue: Ashley Prasad <ashleydp@google.com>
    > Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#913228}
    
    Bug: 1197335
    Test: ./out/Default/ash_webui_unittests --gtest_filter=*RoutineLog* --gtest_repeat=100
    Change-Id: I9545aed972fd170b305eb76de72aa85ff91c5065
    Cq-Include-Trybots: luci.chromium.try:linux-chromeos-dbg
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3106034
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914777}

--- ash/webui/diagnostics_ui/backend/session_log_handler_unittest.cc
+++ ash/webui/diagnostics_ui/backend/session_log_handler_unittest.cc
@@ -184,7 +184,9 @@ class SessionLogHandlerTest : public testing::Test {
   }
 
  protected:
-  base::test::TaskEnvironment task_environment_;
+  base::test::TaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
+
   content::TestWebUI web_ui_;
   std::unique_ptr<diagnostics::SessionLogHandler> session_log_handler_;
   TelemetryLog* telemetry_log_;
@@ -195,8 +197,10 @@ class SessionLogHandlerTest : public testing::Test {
 };
 
 TEST_F(SessionLogHandlerTest, SaveSessionLog) {
+  base::RunLoop run_loop;
   // Populate routine log
   routine_log_->LogRoutineStarted(mojom::RoutineType::kCpuStress);
+  task_environment_.RunUntilIdle();
 
   // Populate telemetry log
   const std::string expected_board_name = "board_name";
@@ -221,7 +225,6 @@ TEST_F(SessionLogHandlerTest, SaveSessionLog) {
   ui::SelectFileDialog::SetFactory(new TestSelectFileDialogFactory(log_path));
   base::ListValue args;
   args.Append(kHandlerFunctionName);
-  base::RunLoop run_loop;
   session_log_handler_->SetLogCreatedClosureForTest(run_loop.QuitClosure());
   web_ui_.HandleReceivedMessage("saveSessionLog", &args);
   run_loop.Run();
