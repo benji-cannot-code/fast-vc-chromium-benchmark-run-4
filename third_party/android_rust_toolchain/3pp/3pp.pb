3fa11fd7e9cdac7ecdb26056d2f79379b0644ada
Adrian Taylor <adetaylor@chromium.org>
2021-08-24 00:05:16 +0000

    [rust] Add CIPD 3pp.pb file for Android Rust toolchain
    The Android Rust toolchain is distributed as prebuilt binaries in git.
    We don't ideally want to use such large git repositories within Chrome,
    so this metadata will allow us to repackage the binaries as a CIPD
    package.
    
    This is used for experimentation and won't yet affect the shipping
    Chromium.
    
    Bug: 1069271, 1229889
    Change-Id: Id2488852e7007c8682faebd7efca4e1d81d9c2e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3033724
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914577}

---
+++ third_party/android_rust_toolchain/3pp/3pp.pb
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+create {
+  platform_re: "linux-.*|mac-.*"
+  source {
+    git {
+      repo: "https://android.googlesource.com/platform/prebuilts/rust.git"
+      tag_pattern: "rustc-%s",
+    }
+    patch_version: "cr0"
+  }
+  build {
+  }
+}
+
+upload { pkg_prefix: "chromium/third_party" }
---
+++ third_party/android_rust_toolchain/3pp/install.sh
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+set -x
+set -o pipefail
+
+# An auto-created directory whose content will ultimately be uploaded to CIPD.
+# So the commands below should output the built product to this directory.
+PREFIX="$1"
+
+PLATFORM="$(uname -s)"
+PLATFORM=${PLATFORM,,} # lowercase
+
+# We're simply moving things between git and CIPD here, so we don't actually
+# build anything. Instead just transfer the relevant content into the output
+# directory to be packaged by CIPD.
+mv "$PLATFORM-x86/${_3PP_VERSION}" "$PREFIX"
