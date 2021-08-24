ae6851b75c73499949cd6cb5cc79b0db625b0a8c
Kent Tamura <tkent@chromium.org>
2021-08-24 19:45:40 +0000

    FIELDSET: Fix a crash on dynamic changes of pseudo elements
    Bug: 1242229
    Change-Id: I1af06f4c37922f3b1a60f287d93a903143cab37f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114551
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914875}

--- third_party/blink/renderer/core/html/forms/html_field_set_element.cc
+++ third_party/blink/renderer/core/html/forms/html_field_set_element.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatch_forbidden_scope.h"
-#include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
+#include "third_party/blink/renderer/core/dom/layout_tree_builder_traversal.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/node_lists_node_data.h"
 #include "third_party/blink/renderer/core/html/custom/element_internals.h"
@@ -44,12 +44,13 @@ namespace blink {
 namespace {
 
 bool WillReattachChildLayoutObject(const Node& parent) {
-  for (const Node& child : FlatTreeTraversal::ChildrenOf(parent)) {
-    if (child.NeedsReattachLayoutTree())
+  for (const Node* child = LayoutTreeBuilderTraversal::FirstChild(parent);
+       child; child = LayoutTreeBuilderTraversal::NextSibling(*child)) {
+    if (child->NeedsReattachLayoutTree())
       return true;
-    if (child.ChildNeedsReattachLayoutTree() && child.GetComputedStyle() &&
-        child.GetComputedStyle()->Display() == EDisplay::kContents &&
-        WillReattachChildLayoutObject(child))
+    if (child->ChildNeedsReattachLayoutTree() && child->GetComputedStyle() &&
+        child->GetComputedStyle()->Display() == EDisplay::kContents &&
+        WillReattachChildLayoutObject(*child))
       return true;
   }
   return false;
