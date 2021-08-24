b368fc2b4fcf014fc4eff1bc62b8d9abe3e7d43b
Yashar Dabiran <yashard@chromium.org>
2021-08-24 21:53:10 +0000

    [CSN] Do not trigger on tablets
    Bug: 1243012
    Change-Id: Ibafb41db3a851e678453d3732ccf4019f90d9df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115203
    Commit-Queue: Yashar Dabiran <yashard@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914924}

--- chrome/browser/continuous_search/android/junit/src/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabHelperJUnitTest.java
+++ chrome/browser/continuous_search/android/junit/src/org/chromium/chrome/browser/continuous_search/ContinuousSearchTabHelperJUnitTest.java
@@ -6,11 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.continuous_search;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.content.Context;
+import android.content.res.Resources;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
@@ -39,6 +45,15 @@ public class ContinuousSearchTabHelperJUnitTest {
     public MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
     @Rule
     public TestRule mProcessor = new Features.JUnitProcessor();
+
+    @Before
+    public void setUp() {
+        Context context = mock(Context.class);
+        Resources resources = mock(Resources.class);
+        when(mTabMock.getContext()).thenReturn(context);
+        when(context.getResources()).thenReturn(resources);
+        when(resources.getInteger(anyInt())).thenReturn(0);
+    }
 
     /**
      * Tests initialization success of all tab observers.
