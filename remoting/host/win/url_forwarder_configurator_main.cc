142c2be8a744409eba0de64d6a1aa6c28200cdcf
Yuwei Huang <yuweih@chromium.org>
2021-08-24 21:12:49 +0000

    [remoting][windows host] Backup and launch fallback browsers for remote open URL
    This CL makes the URL forwarder setup UI backup the current default
    browser's ProgID to registry (via HostSettingsWin), then makes the URL
    forwarder load it from registry and use it to open URLs that can be
    opened remotely.
    
    Unlike the pairing registry delegate, we are storing the settings in
    HKEY_CURRENT_USER since both the setup UI and the URL forwarder run from
    the user's context, and the setup UI does not have permission to write
    to HKEY_LOCAL_MACHINE.
    
    Bug: b:183135000
    Change-Id: I0138ba5b6b68a974561b24b00c43ab8268aeb87b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3083234
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914884}

--- remoting/host/win/url_forwarder_configurator_main.cc
+++ remoting/host/win/url_forwarder_configurator_main.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <shlobj.h>
 #include <shobjidl.h>
 #include <wrl/client.h>
+
 #include <cwchar>
 #include <memory>
+#include <string>
 
 #include "base/bind.h"
 #include "base/callback_forward.h"
@@ -17,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_type.h"
 #include "base/notreached.h"
 #include "base/run_loop.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_executor.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/threading/sequenced_task_runner_handle.h"
@@ -26,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_types.h"
 #include "remoting/base/logging.h"
 #include "remoting/host/switches.h"
+#include "remoting/host/user_setting_keys.h"
+#include "remoting/host/user_settings.h"
 #include "remoting/host/win/core_resource.h"
 #include "remoting/host/win/simple_task_dialog.h"
 
@@ -45,9 +50,8 @@ constexpr base::TimeDelta kPollingInterval =
     base::TimeDelta::FromMilliseconds(500);
 constexpr base::TimeDelta kPollingTimeout = base::TimeDelta::FromMinutes(1);
 
-// |log_current_default_app| logs the current default app if it is not the CRD
-//     URL forwarder.
-bool IsUrlForwarderSetUp(bool log_current_default_app = false) {
+// Returns the current default browser's ProgID, or an empty string if failed.
+std::wstring GetDefaultBrowserProgId() {
   // This method is modified from chrome/installer/util/shell_util.cc
 
   Microsoft::WRL::ComPtr<IApplicationAssociationRegistration> registration;
@@ -56,7 +60,7 @@ bool IsUrlForwarderSetUp(bool log_current_default_app = false) {
                          CLSCTX_INPROC, IID_PPV_ARGS(&registration));
   if (FAILED(hr)) {
     PLOG(ERROR) << "Failed to create IApplicationAssociationRegistration";
-    return false;
+    return std::wstring();
   }
   base::win::ScopedCoMem<wchar_t> current_app;
   hr = registration->QueryCurrentDefault(kProtocolToTestSetup, AT_URLPROTOCOL,
@@ -64,9 +68,19 @@ bool IsUrlForwarderSetUp(bool log_current_default_app = false) {
   if (FAILED(hr)) {
     PLOG(ERROR) << "Failed to query default app for protocol "
                 << kProtocolToTestSetup;
+    return std::wstring();
+  }
+  return current_app.get();
+}
+
+// |log_current_default_app| logs the current default app if it is not the CRD
+// URL forwarder.
+bool IsUrlForwarderSetUp(bool log_current_default_app = false) {
+  std::wstring current_app = GetDefaultBrowserProgId();
+  if (current_app.empty()) {
     return false;
   }
-  if (std::wcscmp(kUrlForwarderProgId, current_app) != 0) {
+  if (current_app != kUrlForwarderProgId) {
     if (log_current_default_app) {
       HOST_LOG << "Current default app for " << kProtocolToTestSetup << " is "
                << current_app << " instead of " << kUrlForwarderProgId;
@@ -180,6 +194,12 @@ void SetUpProcess::Start(base::OnceCallback<void(bool)> done_callback) {
     return;
   }
 
+  std::wstring prog_id = GetDefaultBrowserProgId();
+  LOG(INFO) << "Setting previous default browser to " << prog_id;
+
+  UserSettings::GetInstance()->SetString(kWinPreviousDefaultWebBrowserProgId,
+                                         base::WideToUTF8(prog_id));
+
   if (ShowSetUpUrlForwarderDialog()) {
     OnSetUpDialogContinue();
   } else {
@@ -188,15 +208,14 @@ void SetUpProcess::Start(base::OnceCallback<void(bool)> done_callback) {
 }
 
 void SetUpProcess::OnSetUpDialogContinue() {
-  HOST_LOG << "Launching default apps settings dialog...";
+  HOST_LOG << "Launching default apps settings dialog";
   if (!LaunchDefaultAppsSettingsModernDialog()) {
     std::move(done_callback_).Run(false);
     return;
   }
 
   DCHECK(total_poll_time_.is_zero());
-  HOST_LOG << "Polling default app for protocol " << kProtocolToTestSetup
-           << "...";
+  HOST_LOG << "Polling default app for protocol " << kProtocolToTestSetup;
   PollUrlForwarderSetupState();
 }
 
