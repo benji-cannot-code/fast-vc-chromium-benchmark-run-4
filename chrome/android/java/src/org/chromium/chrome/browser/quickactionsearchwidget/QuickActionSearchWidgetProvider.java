7c578e51d516756ce603628187aaa7aefd622a7b
Max Tauro <maxtauro@google.com>
2021-08-24 19:16:21 +0000

    [Shortcuts Widget] Remove intent trampoline, start lens integration.
    This change removes intent trampoline from the Shortcuts widget and
    replaces SearchActivity extras with an explicit Action, thus allowing
    more code to be simplified (specifically: we don't need additional
    strings to differentiate pending intents attached to widgets).
    
    The change allows Android S to present a splash screen while the
    SearchActivity (or Chrome) is being loaded.
    
    The change also links Lens button to the SearchActivity, which will
    host the functionality.
    
    Bug: 1213541
    Change-Id: I0689d55e7eb921bb637d699cf1c39e07842c2980
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088328
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914858}

--- chrome/android/java/src/org/chromium/chrome/browser/quickactionsearchwidget/QuickActionSearchWidgetProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/quickactionsearchwidget/QuickActionSearchWidgetProvider.java
@@ -17,6 +17,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.searchwidget.SearchActivity;
 import org.chromium.chrome.browser.ui.quickactionsearchwidget.QuickActionSearchWidgetProviderDelegate;
 import org.chromium.chrome.browser.ui.quickactionsearchwidget.QuickActionSearchWidgetType;
 
@@ -83,9 +84,11 @@ public abstract class QuickActionSearchWidgetProvider extends AppWidgetProvider 
 
             ComponentName widgetReceiverComponent =
                     new ComponentName(context, QuickActionSearchWidgetReceiver.class);
+            ComponentName searchActivityComponent =
+                    new ComponentName(context, SearchActivity.class);
 
             mDelegate = new QuickActionSearchWidgetProviderDelegate(widgetType,
-                    widgetReceiverComponent,
+                    widgetReceiverComponent, searchActivityComponent,
                     IntentHandler.createTrustedOpenNewTabIntent(context, /*incognito=*/true));
         }
         return mDelegate;
