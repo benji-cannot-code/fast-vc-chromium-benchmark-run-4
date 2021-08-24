3a1b9dd48c0fd73bf5d55c53a99746347f074868
Nohemi Fernandez <fernandex@chromium.org>
2021-08-24 12:05:48 +0000

    [iOS] Remove kMobileIdentityConsistency flag.
    Experiment has been launched in https://crbug.com/1159797.
    
    Bug: 1241417
    Change-Id: I6b6d8363a47e32284542178de0e09225ab8ad43e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114651
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#914696}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_coordinator.h"
 
-#import "base/feature_list.h"
 #import "base/ios/block_types.h"
 #import "base/mac/foundation_util.h"
 #import "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -128,12 +127,6 @@ const CGFloat kFadeOutAnimationDuration = 0.16f;
       AuthenticationServiceFactory::GetForBrowserState(
           self.browser->GetBrowserState());
 
-  // The user should be signed out before triggering sign-in or upgrade states.
-  // Users are allowed to be signed-in during FirstRun for testing purposes.
-  DCHECK(base::FeatureList::IsEnabled(signin::kMobileIdentityConsistency) ||
-         !authenticationService->HasPrimaryIdentity(
-             signin::ConsentLevel::kSignin) ||
-         self.signinIntent == UserSigninIntentFirstRun);
   [super start];
 
   self.signinStateOnStart =
