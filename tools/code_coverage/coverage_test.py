abe65625ad7a4187f66206b646c2c96340f5dca1
Akekawit Jitprasert <akekawit@google.com>
2021-08-24 17:48:02 +0000

    Support exporting coverage data in lcov format
    TEST=coverage.py --format lcov works and coverage_test.py passes
    BUG=b:196885721
    
    Change-Id: Ia37165fb20b9a7e0976f1ec51f1e1d9b5e2d8fde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098794
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Akekawit Jitprasert <akekawit@google.com>
    Cr-Commit-Position: refs/heads/main@{#914805}

--- tools/code_coverage/coverage_test.py
+++ tools/code_coverage/coverage_test.py
@@ -56,6 +56,7 @@ class CoverageTest(unittest.TestCase):
                                                    'report1_no_components')
     self.REPORT_DIR_2 = os.path.join(self.BUILD_DIR, 'report2')
     self.REPORT_DIR_3 = os.path.join(self.BUILD_DIR, 'report3')
+    self.REPORT_DIR_4 = os.path.join(self.BUILD_DIR, 'report4')
 
     self.LLVM_COV = os.path.join(self.CHROMIUM_SRC_DIR, 'third_party',
                                  'llvm-build', 'Release+Asserts', 'bin',
@@ -124,6 +125,15 @@ class CoverageTest(unittest.TestCase):
     with open(filepath) as f:
       data = f.read()
 
+    self.assertGreater(data.count('crypto'), 100)
+    self.assertGreater(data.count('libpng'), 10)
+
+  def verify_lcov_file(self, filepath):
+    """Asserts that a given lcov file looks correct."""
+    with open(filepath) as f:
+      data = f.read()
+
+    self.assertGreater(data.count('SF:'), 100)
     self.assertGreater(data.count('crypto'), 100)
     self.assertGreater(data.count('libpng'), 10)
 
@@ -267,7 +277,7 @@ class CoverageTest(unittest.TestCase):
     summary_output = self.run_cmd(cmd)
 
     summary_path = os.path.join(self.REPORT_DIR_3, 'summary.json')
-    with open(summary_path, 'w') as f:
+    with open(summary_path, 'wb') as f:
       f.write(summary_output)
 
     cmd = [
@@ -296,6 +306,28 @@ class CoverageTest(unittest.TestCase):
         os.path.join(self.REPORT_DIR_3, self.PLATFORM, 'file_view_index.html'))
     self.assertEqual(report_1_file_view_data_no_component,
                      report_3_file_view_data)
+
+    # Testcase 4. Export coverage data in lcov format using coverage.py script.
+    cmd = [
+        self.COVERAGE_SCRIPT,
+        'crypto_unittests',
+        'libpng_read_fuzzer',
+        '--format',
+        'lcov',
+        '-v',
+        '-b',
+        self.BUILD_DIR,
+        '-o',
+        self.REPORT_DIR_4,
+        '-c'
+        '%s/crypto_unittests' % self.BUILD_DIR,
+        '-c',
+        '%s/libpng_read_fuzzer -runs=0 third_party/libpng/' % self.BUILD_DIR,
+    ]
+    self.run_cmd(cmd)
+
+    output_dir = os.path.join(self.REPORT_DIR_4, self.PLATFORM)
+    self.verify_lcov_file(os.path.join(output_dir, 'coverage.lcov'))
 
 
 if __name__ == '__main__':
