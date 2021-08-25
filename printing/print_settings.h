f25380bd775cba440bdcd334998407f7becf0363
Alan Screen <awscreen@chromium.org>
2021-08-25 00:47:09 +0000

    Add printing PrintSettings mojo type with mapping
    Print driver settings queries and document printing with the print
    backend service requires the ability to transfer PrintSettings over
    Mojo.
    
    Bug: 809738
    Change-Id: Id3f6320fff90a6d670ab1cb1b649250d8bb45865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3062421
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915002}

--- printing/print_settings.h
+++ printing/print_settings.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/page_range.h"
 #include "printing/page_setup.h"
@@ -78,8 +77,8 @@ class COMPONENT_EXPORT(PRINTING) PrintSettings {
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS)
 
   PrintSettings();
-  PrintSettings(const PrintSettings&) = delete;
-  PrintSettings& operator=(const PrintSettings&) = delete;
+  PrintSettings(const PrintSettings&);
+  PrintSettings& operator=(const PrintSettings&);
   ~PrintSettings();
 
   // Reinitialize the settings to the default values.
@@ -115,6 +114,11 @@ class COMPONENT_EXPORT(PRINTING) PrintSettings {
                                bool landscape_needs_flip);
   const PageSetup& page_setup_device_units() const {
     return page_setup_device_units_;
+  }
+  // `set_page_setup_device_units()` intended to be used only for mojom
+  // validation.
+  void set_page_setup_device_units(const PageSetup& page_setup_device_units) {
+    page_setup_device_units_ = page_setup_device_units;
   }
 
   void set_device_name(const std::u16string& device_name) {
@@ -199,6 +203,9 @@ class COMPONENT_EXPORT(PRINTING) PrintSettings {
   void set_printer_language_type(mojom::PrinterLanguageType type) {
     printer_language_type_ = type;
   }
+  mojom::PrinterLanguageType printer_language_type() const {
+    return printer_language_type_;
+  }
   bool printer_language_is_textonly() const {
     return printer_language_type_ == mojom::PrinterLanguageType::kTextOnly;
   }
@@ -241,7 +248,7 @@ class COMPONENT_EXPORT(PRINTING) PrintSettings {
 
   void set_pin_value(const std::string& pin_value) { pin_value_ = pin_value; }
   const std::string& pin_value() const { return pin_value_; }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // defined(OS_CHROMEOS)
 
   // Cookie generator. It is used to initialize `PrintedDocument` with its
   // associated `PrintSettings`, to be sure that each generated `PrintedPage`
