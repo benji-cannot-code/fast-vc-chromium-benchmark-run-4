bbb2a114ea356cae50b41b8658d21ba9c2537315
ckitagawa <ckitagawa@chromium.org>
2021-08-25 21:37:08 +0000

    [Paint Preview] Fix bitmap locking
    This fixes an issue where the bitmap painter keeps bitmaps locked after
    destruction leading to them never being destroyed.
    
    Tested manually using heap profiler and this fixes the issue.
    
    Bug: 1243039
    Change-Id: I8d39d70c0a1ca99f247538fad73c0ba15e18a536
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119869
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915347}

--- components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameCoordinator.java
+++ components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameCoordinator.java
@@ -74,6 +74,8 @@ public class PlayerFrameCoordinator {
     }
 
     public void destroy() {
+        // Destroy the view first to unlock all bitmaps so they can be destroyed successfully.
+        mView.destroy();
         mMediator.destroy();
         for (PlayerFrameCoordinator subframe : mSubFrames) {
             subframe.destroy();
