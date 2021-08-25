6a6b11bd3daa21ead1e179c336eebe8c58246408
tby <tby@chromium.org>
2021-08-25 12:04:42 +0000

    [Launcher] Add MRFU cache logic.
    This will be used by the various new rankers in a follow-up CL. See the
    header comment in mrfu_cache.h for more details.
    
    Bug: 1199206
    Change-Id: Iddf65375b97e0e977a158383d233a62377d627f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115519
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915147}

--- chrome/browser/ui/app_list/search/ranking/persistent_proto.h
+++ chrome/browser/ui/app_list/search/ranking/persistent_proto.h
@@ -135,6 +135,8 @@ class PersistentProto {
     return *proto_;
   }
 
+  bool initialized() const { return initialized_; }
+
   constexpr bool has_value() const { return proto_.get() != nullptr; }
 
   constexpr explicit operator bool() const { return has_value(); }
@@ -211,6 +213,7 @@ class PersistentProto {
       purge_after_reading_ = false;
     }
 
+    initialized_ = true;
     std::move(on_read_).Run(result.first);
   }
 
@@ -229,6 +232,10 @@ class PersistentProto {
 
   // How long to delay writing to disk for on a call to QueueWrite.
   const base::TimeDelta write_delay_;
+
+  // Whether the proto has finished reading from disk. |proto_| will be empty
+  // before |initialized_| is true.
+  bool initialized_ = false;
 
   // Whether or not a write is currently scheduled.
   bool write_is_queued_ = false;
