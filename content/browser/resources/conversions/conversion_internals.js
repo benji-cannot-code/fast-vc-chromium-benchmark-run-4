3adc358a9c21db28aec2ceb2568166fe46371aae
Andrew Paseltiner <apaseltiner@chromium.org>
2021-08-25 19:37:19 +0000

    [attribution_reporting] Add space between dedup keys in internals UI
    This should help with accessibility, as it is clear that the values are
    separate, rather than part of a single comma-delimited integer.
    
    We could also investigate using an unordered list <ul> here, but
    ensuring that the UI remains legible is more challenging.
    
    Bug: 1225604
    Change-Id: I5378567a45a8a64b367164d3d8573abbe89a7676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3118989
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915304}

--- content/browser/resources/conversions/conversion_internals.js
+++ content/browser/resources/conversions/conversion_internals.js
@@ -94,7 +94,7 @@ function createSourceRow(source) {
   td[5].textContent = new Date(source.expiryTime).toLocaleString();
   td[6].textContent = SourceTypeToText(source.sourceType);
   td[7].textContent = source.priority;
-  td[8].textContent = source.dedupKeys.join();
+  td[8].textContent = source.dedupKeys.join(', ');
   return document.importNode(template.content, true);
 }
 
