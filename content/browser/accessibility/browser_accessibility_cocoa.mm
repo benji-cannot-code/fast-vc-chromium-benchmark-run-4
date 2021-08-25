2b648931745175604084cd0a336103448e184f0e
Joanmarie Diggs <jdiggs@igalia.com>
2021-08-25 10:17:49 +0000

    Implement support for aria-details on macOS
    This exposes aria-details via @"AXDetailsElements" in Mac AX API.
    
    AX-Relnotes: Implements support for aria-details on macOS.
    
    Bug: 1216049
    Change-Id: I81ea98afc85bab02a50fc567b9f860ee330e4eed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114125
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#915122}

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -78,6 +78,7 @@ NSString* const NSAccessibilityAccessKeyAttribute = @"AXAccessKey";
 NSString* const NSAccessibilityAutocompleteValueAttribute =
     @"AXAutocompleteValue";
 NSString* const NSAccessibilityBlockQuoteLevelAttribute = @"AXBlockQuoteLevel";
+NSString* const NSAccessibilityDetailsElementsAttribute = @"AXDetailsElements";
 NSString* const NSAccessibilityDOMClassList = @"AXDOMClassList";
 NSString* const NSAccessibilityDOMIdentifierAttribute = @"AXDOMIdentifier";
 NSString* const NSAccessibilityDropEffectsAttribute = @"AXDropEffects";
@@ -827,6 +828,7 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
       {NSAccessibilityColumnIndexRangeAttribute, @"columnIndexRange"},
       {NSAccessibilityContentsAttribute, @"contents"},
       {NSAccessibilityDescriptionAttribute, @"descriptionForAccessibility"},
+      {NSAccessibilityDetailsElementsAttribute, @"detailsElements"},
       {NSAccessibilityDisclosingAttribute, @"disclosing"},
       {NSAccessibilityDisclosedByRowAttribute, @"disclosedByRow"},
       {NSAccessibilityDisclosureLevelAttribute, @"disclosureLevel"},
@@ -1276,6 +1278,25 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
   }
 
   return @"";
+}
+
+- (NSArray*)detailsElements {
+  if (![self instanceActive])
+    return nil;
+
+  NSMutableArray* elements = [[[NSMutableArray alloc] init] autorelease];
+  const std::vector<int32_t>& detailsIds =
+      _owner->GetIntListAttribute(ax::mojom::IntListAttribute::kDetailsIds);
+  for (size_t i = 0; i < detailsIds.size(); ++i) {
+    if (BrowserAccessibility* element =
+            _owner->manager()->GetFromID(detailsIds[i])) {
+      id cocoa_element = ToBrowserAccessibilityCocoa(element);
+      if ([cocoa_element instanceActive])
+        [elements addObject:cocoa_element];
+    }
+  }
+
+  return [elements count] ? elements : nil;
 }
 
 - (NSNumber*)disclosing {
@@ -3667,6 +3688,10 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
     [ret addObject:NSAccessibilityTitleUIElementAttribute];
   }
 
+  if (!_owner->GetIntListAttribute(ax::mojom::IntListAttribute::kDetailsIds)
+           .empty()) {
+    [ret addObject:NSAccessibilityDetailsElementsAttribute];
+  }
   if (_owner->HasStringAttribute(ax::mojom::StringAttribute::kAutoComplete))
     [ret addObject:NSAccessibilityAutocompleteValueAttribute];
 
