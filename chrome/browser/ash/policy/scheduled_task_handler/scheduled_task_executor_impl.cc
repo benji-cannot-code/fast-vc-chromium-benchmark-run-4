6074ac9ffec7faa4d6c1fbebc56f9aaf6bda7eaa
Sanja Perisic <sanjaperisic@chromium.org>
2021-08-25 13:49:54 +0000

    Fix overflow in ScheduledTaskExecutorImpl
    Use ToJavaTime() instead of ToTimeT() in ConvertUtcToTzIcuTime method.
    Time_t has different sizes in arm and x86 devices, so the calculation causes overflow in arm device.
    
    Bug: 1241047
    Change-Id: Iae8d5e99a572feb32dec136695d77320b92b1c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115978
    Commit-Queue: Sanja Perisic <sanjaperisic@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915168}

--- chrome/browser/ash/policy/scheduled_task_handler/scheduled_task_executor_impl.cc
+++ chrome/browser/ash/policy/scheduled_task_handler/scheduled_task_executor_impl.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ash/policy/scheduled_task_handler/scheduled_task_executor_impl.h"
+#include <cstdint>
 
 #include "base/check.h"
 #include "base/logging.h"
@@ -124,8 +125,7 @@ std::unique_ptr<icu::Calendar> ConvertUtcToTzIcuTime(base::Time cur_time,
   }
   // Erase current time from the calendar.
   cal_tz->clear();
-  time_t ms_from_epoch = cur_time.ToTimeT() * 1000;
-  cal_tz->setTime(ms_from_epoch, status);
+  cal_tz->setTime(cur_time.ToJavaTime(), status);
   if (U_FAILURE(status)) {
     LOG(ERROR) << "Couldn't create calendar";
     return nullptr;
