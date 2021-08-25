27797902e40281aad2b50c3341e1ccc926e723d0
Ashley Prasad <ashleydp@google.com>
2021-08-25 18:40:55 +0000

    Diagnostics: Get Frequency Band
    - Adds function to lookup channel band.
    - Adds test to get channel band.
    
    Bug: 1197335
    Change-Id: I7384873ae762c435ece2b20e6d3a18bef7dfbd5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116728
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915272}

--- chrome/test/data/webui/chromeos/diagnostics/frequency_channel_utils_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/frequency_channel_utils_test.js
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {convertFrequencyToChannel} from 'chrome://diagnostics/frequency_channel_utils.js';
+import {ChannelBand} from 'chrome://diagnostics/diagnostics_types.js';
+import {convertFrequencyToChannel, getFrequencyChannelBand} from 'chrome://diagnostics/frequency_channel_utils.js';
 
 import {assertEquals} from '../../chai_assert.js';
 
@@ -24,5 +25,11 @@ export function frequencyChannelUtilsTestSuite() {
     assertEquals(14, convertFrequencyToChannel(2484));
     assertEquals(32, convertFrequencyToChannel(5160));
     assertEquals(196, convertFrequencyToChannel(5980));
+  });
+
+  test('GetFrequencyChannelBand', () => {
+    assertEquals(ChannelBand.UNKNOWN, getFrequencyChannelBand(0));
+    assertEquals(ChannelBand.TWO_DOT_FOUR_GHZ, getFrequencyChannelBand(2412));
+    assertEquals(ChannelBand.FIVE_GHZ, getFrequencyChannelBand(5160));
   });
 }
