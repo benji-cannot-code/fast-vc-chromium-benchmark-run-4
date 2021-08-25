74c6db6531eb8a8adfe2692b6ce5ed54f00979a8
Wolfgang Beyer <wolfi@chromium.org>
2021-08-25 11:19:49 +0000

    [DevTools] StructTraits for ReportingApiReport
    Bug: 1205856
    Change-Id: I5051ff7a39f8b90371202b48555a4165557dc422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3053749
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915135}

--- content/browser/devtools/protocol/network_handler.h
+++ content/browser/devtools/protocol/network_handler.h
@@ -20,12 +20,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/filter/source_stream.h"
+#include "net/net_buildflags.h"
 #include "services/network/public/mojom/devtools_observer.mojom-forward.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/network_service.mojom.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/public/mojom/loader/resource_load_info.mojom-shared.h"
+
+#if BUILDFLAG(ENABLE_REPORTING)
+#include "services/network/public/mojom/reporting_report.mojom.h"
+#endif  // BUILDFLAG(ENABLE_REPORTING)
 
 namespace net {
 class HttpRequestHeaders;
@@ -58,8 +63,10 @@ class BackgroundSyncRestorer;
 class DevToolsNetworkResourceLoader;
 
 class NetworkHandler : public DevToolsDomainHandler,
-                       public Network::Backend,
-                       public network::mojom::ReportingApiObserver {
+#if BUILDFLAG(ENABLE_REPORTING)
+                       public network::mojom::ReportingApiObserver,
+#endif  // BUILDFLAG(ENABLE_REPORTING)
+                       public Network::Backend {
  public:
   NetworkHandler(const std::string& host_id,
                  const base::UnguessableToken& devtools_token,
@@ -94,8 +101,10 @@ class NetworkHandler : public DevToolsDomainHandler,
                   Maybe<int> max_post_data_size) override;
   Response Disable() override;
 
-  void OnReportsAdded(
-      std::vector<network::mojom::ReportingApiReportPtr> reports) override;
+#if BUILDFLAG(ENABLE_REPORTING)
+  void OnReportAdded(const net::ReportingReport& report) override;
+#endif  // BUILDFLAG(ENABLE_REPORTING)
+
   Response EnableReportingApi(bool enable) override;
 
   Response SetCacheDisabled(bool cache_disabled) override;
@@ -307,7 +316,9 @@ class NetworkHandler : public DevToolsDomainHandler,
   StoragePartition* storage_partition_;
   RenderFrameHostImpl* host_;
   bool enabled_;
+#if BUILDFLAG(ENABLE_REPORTING)
   mojo::Receiver<network::mojom::ReportingApiObserver> reporting_receiver_;
+#endif  // BUILDFLAG(ENABLE_REPORTING)
   std::vector<std::pair<std::string, std::string>> extra_headers_;
   std::unique_ptr<DevToolsURLLoaderInterceptor> url_loader_interceptor_;
   bool bypass_service_worker_;
