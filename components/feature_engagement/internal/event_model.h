35df436de0a0c59655fd33a8e558532b1c48dadc
Hailey Wang <haileywang@google.com>
2021-08-25 20:37:36 +0000

    [Snooze IPH] Event model changes
    Adding functions in the event model to accommodate snooze IPH.
    
    Modifying affected implementation accordingly.
    
    Bug: 1238894
    Change-Id: Ia1b66e620b7935e36e22845a1565d85ef9b075b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3108550
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915325}

--- components/feature_engagement/internal/event_model.h
+++ components/feature_engagement/internal/event_model.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/time/time.h"
 
 namespace feature_engagement {
 class Event;
@@ -39,9 +40,10 @@ class EventModel {
   virtual const Event* GetEvent(const std::string& event_name) const = 0;
 
   // Returns the number of times the event with |event_name| happened in the
-  // past |window_size| days from and including |current_day|.
-  // If |window_size| > |current_day|, all events since UNIX epoch will
-  // be counted, since |current_day| represents number of days since UNIX epoch.
+  // past |window_size| days from and including |current_day|, subtracted by
+  // the number of times the event has been snoozed.
+  // If |window_size| > |current_day|, all events since UNIX epoch will be
+  // counted, since |current_day| represents number of days since UNIX epoch.
   virtual uint32_t GetEventCount(const std::string& event_name,
                                  uint32_t current_day,
                                  uint32_t window_size) const = 0;
@@ -52,6 +54,25 @@ class EventModel {
   // TimeProvider::GetCurrentDay()).
   virtual void IncrementEvent(const std::string& event_name,
                               uint32_t current_day) = 0;
+
+  // Increments the snooze count for the day.
+  // Updates the last_snooze_time_us.
+  virtual void IncrementSnooze(const std::string& event_name,
+                               uint32_t current_day,
+                               base::Time current_time) = 0;
+
+  // Sets the snooze_dismissed flag.
+  virtual void DismissSnooze(const std::string& event_name) = 0;
+
+  // Returns the last snooze timestamp for the feature associated with
+  // |event_name|.
+  virtual base::Time GetLastSnoozeTimestamp(
+      const std::string& event_name) const = 0;
+
+  // Returns the snooze count. Used for comparison against the max limit.
+  virtual uint32_t GetSnoozeCount(const std::string& event_name,
+                                  uint32_t window,
+                                  uint32_t current_day) = 0;
 
  protected:
   EventModel() = default;
