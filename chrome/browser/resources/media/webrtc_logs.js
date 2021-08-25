f0bbe735a5170da961fdf66bd24297ecacd62688
Elad Alon <eladalon@chromium.org>
2021-08-25 20:30:09 +0000

    Fix missing import in webrtc_logs.js: appendParam
    Bug: 1243353
    Change-Id: If360574b3af57e12d07be7d239a4192faf4eecaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3118292
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915319}

--- chrome/browser/resources/media/webrtc_logs.js
+++ chrome/browser/resources/media/webrtc_logs.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './strings.m.js';
 import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-import {$} from 'chrome://resources/js/util.m.js';
+import {$, appendParam} from 'chrome://resources/js/util.m.js';
 
 /**
  * Requests the list of WebRTC logs from the backend.
