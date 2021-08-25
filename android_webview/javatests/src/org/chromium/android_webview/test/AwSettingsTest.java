ea1af92518152ab9735edc4b037e77ca5ac4d720
Nate Fischer <ntfschr@chromium.org>
2021-08-25 22:10:02 +0000

    AW: Re-enablle AwSettingsTest#testUserAgentStringOverrideForHistory
    This reverts commit c3f94eb5d4ce2802940bbe4dc1e04280f842688a.
    
    No change to production logic. I cannot reproduce these flakes locally
    so I'm going to try to re-enable this test case.
    
    Bug: 1240675
    Test: run_webview_instrumentation_test_apk \
    Test: --num_retries=0 --break-on-failure --repeat=100 \
    Test: -f AwSettingsTest.testUserAgentStringOverrideForHistory
    Change-Id: Ic1621d4a569de1831dc8e8c0f9f5bc7857a043a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119753
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915357}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -1854,7 +1854,6 @@ public class AwSettingsTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Preferences"})
-    @DisabledTest(message = "https://crbug.com/1240675")
     public void testUserAgentStringOverrideForHistory() throws Throwable {
         final TestAwContentsClient contentClient = new TestAwContentsClient();
         final AwTestContainerView testContainerView =
