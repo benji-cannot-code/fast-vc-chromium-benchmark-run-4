fe44acde5595739057c0c5950961ce2862c20fbd
Shelley Vohr <shelley.vohr@gmail.com>
2021-08-25 16:53:27 +0000

    Fix missing TOOLKIT_VIEWS browser dialogs include
    Fixes a build failure when TOOLKIT_VIEWS is not defined, whereby
    ui::SelectedFileInfo is referenced without being included. It's forward
    declared in chrome/browser/ui/browser_dialogs.h, so this adds the
    relevant include for this case.
    
    This was encountered in Electron here:
    https://ci.appveyor.com/project/electron-bot/electron-ia32-testing/builds/40485504
    
    and fixes our build failure.
    
    Change-Id: Ie8f77601ffc5a33cd2230463c59c04afce8414f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113977
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915224}

--- chrome/browser/ui/browser_dialogs.cc
+++ chrome/browser/ui/browser_dialogs.cc
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/metrics/histogram_macros.h"
 
+#if !defined(TOOLKIT_VIEWS)
+#include "ui/shell_dialogs/selected_file_info.h"
+#endif
+
 namespace chrome {
 
 void RecordDialogCreation(DialogIdentifier identifier) {
