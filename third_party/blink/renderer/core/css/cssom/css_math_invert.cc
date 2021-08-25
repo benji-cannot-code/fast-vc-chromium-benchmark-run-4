fec33cb3e29b19eb718c66f5846a50320d96b000
Seokho Song <0xdevssh@gmail.com>
2021-08-25 01:15:02 +0000

    Prevent infinity evaluation from CSSMathInvert::SumValue if CSSCalcInfinityAndNaN feature is disabled
    The DCHECK failure could happen if the value is evaluated to infinity or NaN.
    
    Therefore, return nullopt from CSSMathInvert::SumValue when the value is 0 which will evaluate to infinity.
    
    Bug: 1238543
    Change-Id: I28cbafcdf5a4011d85c16ee8b925b75484d5db05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110170
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Seokho Song <0xdevssh@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#915011}

--- third_party/blink/renderer/core/css/cssom/css_math_invert.cc
+++ third_party/blink/renderer/core/css/cssom/css_math_invert.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_cssnumericvalue_double.h"
 #include "third_party/blink/renderer/core/css/css_math_expression_node.h"
 #include "third_party/blink/renderer/core/css/cssom/css_numeric_sum_value.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
@@ -18,7 +19,9 @@ V8CSSNumberish* CSSMathInvert::value() {
 
 absl::optional<CSSNumericSumValue> CSSMathInvert::SumValue() const {
   auto sum = value_->SumValue();
-  if (!sum || sum->terms.size() != 1)
+  if (!sum || sum->terms.size() != 1 ||
+      (!RuntimeEnabledFeatures::CSSCalcInfinityAndNaNEnabled() &&
+       sum->terms[0].value == 0))
     return absl::nullopt;
 
   for (auto& unit_exponent : sum->terms[0].units)
