b6d81a33969a79586bd544b3d83e81ababd1a410
Roman Sorokin <rsorokin@chromium.org>
2021-08-25 13:48:26 +0000

    [ash-chrome] Migrate FullRestoreService to sync categorization feature
    Check kSyncSettingsCategorization instead of deprecated
    kSplitSettingsSync
    
    Bug: 1227693, 1243234
    Change-Id: I785eff6a26f3c787f43b319135c2e46810baf7ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113937
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915167}

--- chrome/browser/ash/full_restore/new_user_restore_pref_handler.cc
+++ chrome/browser/ash/full_restore/new_user_restore_pref_handler.cc
@@ -47,7 +47,7 @@ void NewUserRestorePrefHandler::OnStartedSyncing(const std::string& path) {
 void NewUserRestorePrefHandler::OnIsSyncingChanged() {
   // Wait until the initial sync happens.
   auto* pref_service = PrefServiceSyncableFromProfile(profile_);
-  bool is_syncing = features::IsSplitSettingsSyncEnabled()
+  bool is_syncing = features::IsSyncSettingsCategorizationEnabled()
                         ? pref_service->AreOsPrefsSyncing()
                         : pref_service->IsSyncing();
   if (!is_syncing)
