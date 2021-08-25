5ca741bd766d96d2a3342afdd4e782bc5f094c41
Adam Langley <agl@chromium.org>
2021-08-25 19:09:03 +0000

    webauthn: harmlessly check Bluetooth in server-link mode.
    GMS Core will check and enable Bluetooth if needed before invoking
    Clank. But, in that case, it's harmless to check whether it's enabled
    because it'll always be true. So we might as well reduce the number of
    different code paths and make server-link act like FCM mode.
    
    BUG=1002262
    
    Change-Id: I1b14ed92f64313671b4459918d41e444f99eb13c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3108515
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#915290}

--- chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticatorUI.java
+++ chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticatorUI.java
@@ -191,6 +191,7 @@ public class CableAuthenticatorUI
                 mState = State.RUNNING;
                 break;
 
+            case SERVER_LINK:
             case FCM:
                 BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
                 if (!adapter.isEnabled()) {
@@ -199,11 +200,6 @@ public class CableAuthenticatorUI
                     mState = State.ENABLE_BLUETOOTH;
                     return;
                 }
-                mState = State.BLUETOOTH_PERMISSION;
-                break;
-
-            case SERVER_LINK:
-                // GMS Core should have taken care of enabling Bluetooth.
                 mState = State.BLUETOOTH_PERMISSION;
                 break;
 
@@ -580,6 +576,7 @@ public class CableAuthenticatorUI
                 onBluetoothEnabled();
                 break;
 
+            case SERVER_LINK:
             case FCM:
                 ViewGroup top = (ViewGroup) getView();
                 top.removeAllViews();
