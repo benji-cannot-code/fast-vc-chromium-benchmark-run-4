73ee6ac147185505250f43d10ab7056236ee988d
rbpotter <rbpotter@chromium.org>
2021-08-25 17:11:39 +0000

    Settings: Fix toolbar imports deleted by TS compiler
    TypeScript compiler deletes imports that only import types, such as the
    imports of CrToolbarElement and CrToolbarSearchFieldElement. However,
    these elements are used by settings-ui and need to be imported.
    
    Bug: 1240488
    Change-Id: I3b2451df42630f2b4a0e48fb65eafbcb2ac6d577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115530
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915230}

--- chrome/browser/resources/settings/settings_ui/settings_ui.js
+++ chrome/browser/resources/settings/settings_ui/settings_ui.js
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *    <settings-ui prefs="{{prefs}}"></settings-ui>
  */
 import 'chrome://resources/cr_elements/cr_drawer/cr_drawer.js';
+import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
+import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 import 'chrome://resources/cr_elements/cr_page_host_style_css.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
