274688af1a387289775a2cab1d55726e0d825549
Lu Huang <luhua@microsoft.com>
2021-08-25 18:09:53 +0000

    Add a11y role and selection information to WebAppUrlHandlerHoverButton
    ax::mojom::Role::kListBoxOption is not narrated by a11y tools as a
    clickable or interactable element. Instead of klistBoxOption,
    overriding the role of the buttons used in the
    WebAppUrlHandlerIntentPickerView as kRadioButton allows them to be
    narrated as selectable buttons. Because only 1 option can be selected at
    any time, kRadioButton has a suitable semantic meaning.
    AXNodeData::SetCheckedState is used to expose the selected state of the
    buttons.
    
    Windows Narrator narration example:
    Before: "Chromium"
    After: "Chromium", radio button, selected, 1 of 2
    
    Additionally, the setting of kPosInSet and kSetSize in the buttons was
    replaced with ViewAccessibility::OverridePosInSet from the view
    implementation.
    
    Bug: 1237292
    Change-Id: I7241148f7c8d7714bf725850fd816aca094b9f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3108351
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Lu Huang <luhua@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#915256}

--- chrome/browser/ui/views/web_apps/web_app_url_handler_hover_button.cc
+++ chrome/browser/ui/views/web_apps/web_app_url_handler_hover_button.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 WebAppUrlHandlerHoverButton::WebAppUrlHandlerHoverButton(
-    size_t total_buttons,
     views::Button::PressedCallback callback,
     const web_app::UrlHandlerLaunchParams& url_handler_launch_params,
     web_app::WebAppProvider* provider,
@@ -35,27 +34,39 @@ WebAppUrlHandlerHoverButton::WebAppUrlHandlerHoverButton(
                         display_name,
                         app_start_url),
       url_handler_launch_params_(url_handler_launch_params),
-      is_app_(true),
-      total_buttons_(total_buttons) {}
+      is_app_(true) {}
 
 WebAppUrlHandlerHoverButton::WebAppUrlHandlerHoverButton(
-    size_t total_buttons,
     views::Button::PressedCallback callback)
     : WebAppHoverButton(
           std::move(callback),
           *(ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
               IDR_PRODUCT_LOGO_32)),
           l10n_util::GetStringUTF16(IDS_PRODUCT_NAME)),
-      is_app_(false),
-      total_buttons_(total_buttons) {}
+      is_app_(false) {}
 
 WebAppUrlHandlerHoverButton::~WebAppUrlHandlerHoverButton() = default;
 
 void WebAppUrlHandlerHoverButton::GetAccessibleNodeData(
     ui::AXNodeData* node_data) {
   WebAppHoverButton::GetAccessibleNodeData(node_data);
+  node_data->role = ax::mojom::Role::kRadioButton;
+  const ax::mojom::CheckedState checked_state =
+      selected() ? ax::mojom::CheckedState::kTrue
+                 : ax::mojom::CheckedState::kFalse;
+  node_data->SetCheckedState(checked_state);
+}
 
-  node_data->role = ax::mojom::Role::kListBoxOption;
-  node_data->AddIntAttribute(ax::mojom::IntAttribute::kPosInSet, tag() + 1);
-  node_data->AddIntAttribute(ax::mojom::IntAttribute::kSetSize, total_buttons_);
+void WebAppUrlHandlerHoverButton::MarkAsSelected(const ui::Event* event) {
+  WebAppHoverButton::MarkAsSelected(event);
+  selected_ = true;
+  NotifyAccessibilityEvent(ax::mojom::Event::kStateChanged,
+                           /*send_native_event=*/true);
+}
+
+void WebAppUrlHandlerHoverButton::MarkAsUnselected(const ui::Event* event) {
+  WebAppHoverButton::MarkAsUnselected(event);
+  selected_ = false;
+  NotifyAccessibilityEvent(ax::mojom::Event::kStateChanged,
+                           /*send_native_event=*/true);
 }
