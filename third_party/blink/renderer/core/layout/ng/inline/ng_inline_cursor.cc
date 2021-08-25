117848350626116090f3bd4d8e846bf5269d1cb8
Koji Ishii <kojii@chromium.org>
2021-08-25 04:48:23 +0000

    Turn expensive DCHECK in |NGInlineCursor| to EXPENSIVE_DCHECKS_ARE_ON
    As suggested by crbug.com/1237062#c11.
    
    Bug: 1237062
    Change-Id: I8a9c59a5e82a7f80f9de1ef053b59e2d8f3dfa9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115813
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915055}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.cc
@@ -1332,7 +1332,7 @@ void NGInlineCursor::MoveTo(const LayoutObject& layout_object) {
 
   wtf_size_t item_index = layout_object.FirstInlineFragmentItemIndex();
   if (UNLIKELY(!item_index)) {
-#if DCHECK_IS_ON()
+#if EXPENSIVE_DCHECKS_ARE_ON()
     const LayoutBlockFlow* root = layout_object.FragmentItemsContainer();
     NGInlineCursor check_cursor(*root);
     check_cursor.SlowMoveToFirstFor(layout_object);
@@ -1354,7 +1354,7 @@ void NGInlineCursor::MoveTo(const LayoutObject& layout_object) {
         return;
     }
     item_index -= fragment_items_->SizeOfEarlierFragments();
-#if DCHECK_IS_ON()
+#if EXPENSIVE_DCHECKS_ARE_ON()
     NGInlineCursor check_cursor(*root_block_flow_);
     check_cursor.SlowMoveToFirstFor(layout_object);
     DCHECK_EQ(check_cursor.Current().Item(),
@@ -1381,7 +1381,7 @@ void NGInlineCursor::MoveTo(const LayoutObject& layout_object) {
         }
       }
     }
-#if DCHECK_IS_ON()
+#if EXPENSIVE_DCHECKS_ARE_ON()
     const LayoutBlockFlow* root = layout_object.FragmentItemsContainer();
     NGInlineCursor check_cursor(*root);
     check_cursor.SlowMoveToFirstFor(layout_object);
