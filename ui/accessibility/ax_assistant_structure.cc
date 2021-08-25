e20783daea0138afbf32fdc996dec0ce6cd85e3e
Nektarios Paisios <nektar@chromium.org>
2021-08-25 08:50:14 +0000

    Replaces the use of data().role with AXNode::GetRole() in ui/accessibility
    This patch switches all the code under ui/accessibility to use AXNode::GetRole() for
    retrieving the role of a node in the accessibility tree instead of AXNodeData::role.
    There are two reasons for this:
    1. Retrieving all the other attributes in AXNodeData has already been switched over to
    equivalent getters on AXNode, in order to facilitate computing some of those attributes
    on the browser.
    Retrieving the role by simply accessing its data value would be inconsistent with how other attributes
    are now retrieved.
    2. It will allow potentially computing the role on the browser via AXComputedNodeData.
    
    R=dmazzoni@chromium.org, aleventhal@chromium.org
    
    AX-Relnotes: n/a.
    Change-Id: I1d4127e86c857fb118c3852db75d2ccb93d3f81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115096
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915100}

--- ui/accessibility/ax_assistant_structure.cc
+++ ui/accessibility/ax_assistant_structure.cc
@@ -59,7 +59,7 @@ bool IsLeaf(const AXNode* node) {
     return true;
   }
 
-  switch (node->data().role) {
+  switch (node->GetRole()) {
     case ax::mojom::Role::kImage:
     case ax::mojom::Role::kMeter:
     case ax::mojom::Role::kScrollBar:
@@ -105,9 +105,9 @@ std::u16string GetValue(const AXNode* node) {
 }
 
 std::u16string GetText(const AXNode* node) {
-  if (node->data().role == ax::mojom::Role::kPdfRoot ||
-      node->data().role == ax::mojom::Role::kIframe ||
-      node->data().role == ax::mojom::Role::kIframePresentational) {
+  if (node->GetRole() == ax::mojom::Role::kPdfRoot ||
+      node->GetRole() == ax::mojom::Role::kIframe ||
+      node->GetRole() == ax::mojom::Role::kIframePresentational) {
     return std::u16string();
   }
 
@@ -123,7 +123,7 @@ std::u16string GetText(const AXNode* node) {
     if (node->data().HasState(ax::mojom::State::kEditable))
       return value;
 
-    switch (node->data().role) {
+    switch (node->GetRole()) {
       case ax::mojom::Role::kComboBoxMenuButton:
       case ax::mojom::Role::kTextFieldWithComboBox:
       case ax::mojom::Role::kPopUpButton:
@@ -134,7 +134,7 @@ std::u16string GetText(const AXNode* node) {
     }
   }
 
-  if (node->data().role == ax::mojom::Role::kColorWell) {
+  if (node->GetRole() == ax::mojom::Role::kColorWell) {
     unsigned int color = static_cast<unsigned int>(
         node->GetIntAttribute(ax::mojom::IntAttribute::kColorValue));
     unsigned int red = color >> 16 & 0xFF;
@@ -157,8 +157,8 @@ std::u16string GetText(const AXNode* node) {
   if (text.empty())
     text = value;
 
-  if (node->data().role == ax::mojom::Role::kRootWebArea ||
-      node->data().role == ax::mojom::Role::kPdfRoot) {
+  if (node->GetRole() == ax::mojom::Role::kRootWebArea ||
+      node->GetRole() == ax::mojom::Role::kPdfRoot) {
     return text;
   }
 
@@ -169,8 +169,8 @@ std::u16string GetText(const AXNode* node) {
     }
   }
 
-  if (text.empty() && (ui::IsLink(node->data().role) ||
-                       node->data().role == ax::mojom::Role::kImage)) {
+  if (text.empty() && (ui::IsLink(node->GetRole()) ||
+                       node->GetRole() == ax::mojom::Role::kImage)) {
     std::u16string url =
         node->GetString16Attribute(ax::mojom::StringAttribute::kUrl);
     text = AXUrlBaseText(url);
@@ -242,8 +242,8 @@ void WalkAXTreeDepthFirst(const AXNode* node,
                           AssistantNode* result) {
   result->text = GetText(node);
   result->class_name =
-      AXRoleToAndroidClassName(node->data().role, node->GetUnignoredParent());
-  result->role = AXRoleToString(node->data().role);
+      AXRoleToAndroidClassName(node->GetRole(), node->GetUnignoredParent());
+  result->role = AXRoleToString(node->GetRole());
 
   result->text_size = -1.0;
   result->bgcolor = 0;
