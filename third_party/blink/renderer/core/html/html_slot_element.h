5fc7c7581860a2bb8356756a64242358e93f7e6e
Joey Arhar <jarhar@chromium.org>
2021-08-25 18:12:46 +0000

    Update slot.assign() IDL
    http://crrev.com/914978 fixed a bug where you could pass non-slotable
    nodes to slot.assign() by manually throwing an exception, but it turns
    out that the same bug could also be fixed by updating the IDL from Node
    to (Element or Text) like the spec says it should be:
    https://html.spec.whatwg.org/C/#htmlslotelement
    
    This was pointed out to me here:
    https://github.com/whatwg/html/pull/6988#issuecomment-905040295
    
    Bug: 1240783
    Change-Id: I8f49de87c3ccc25c5d6787c5a8d1007785895434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116799
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915260}

--- third_party/blink/renderer/core/html/html_slot_element.h
+++ third_party/blink/renderer/core/html/html_slot_element.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_SLOT_ELEMENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_SLOT_ELEMENT_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/v8_union_element_text.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 
@@ -114,7 +115,7 @@ class CORE_EXPORT HTMLSlotElement final : public HTMLElement {
   static const AtomicString& UserAgentDefaultSlotName();
 
   // For imperative Shadow DOM distribution APIs
-  void assign(HeapVector<Member<Node>> nodes, ExceptionState&);
+  void assign(HeapVector<Member<V8UnionElementOrText>> nodes, ExceptionState&);
   const HeapLinkedHashSet<WeakMember<Node>>& ManuallyAssignedNodes() const {
     return manually_assigned_nodes_;
   }
