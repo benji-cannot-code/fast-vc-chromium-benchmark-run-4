8baa6de3f81e937fe97c06d59357060636422041
Michael Thiessen <mthiesse@chromium.org>
2021-08-25 18:36:22 +0000

    Implement App Impression reporting when the Browser is initialized
    Plumbs through impressions coming from apps through the ContentProvider
    to our storage layer when the browser is already initialized. A new
    content API is added for this purpose.
    
    A future change will handle impressions when Chrome isn't already
    initialized by scheduling a job avoid initializing the browser on every
    attribution.
    
    Bug: 1210171
    Change-Id: I1c33f888326ef04ffd42d23923dd3ee21fb8f6be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105187
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915268}

--- content/browser/renderer_host/navigation_controller_android.cc
+++ content/browser/renderer_host/navigation_controller_android.cc
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/containers/flat_map.h"
-#include "base/strings/strcat.h"
-#include "content/browser/conversions/conversion_host.h"
+#include "content/browser/conversions/conversion_host_utils.h"
 #include "content/browser/renderer_host/navigation_controller_impl.h"
 #include "content/browser/renderer_host/navigation_entry_impl.h"
+#include "content/common/url_utils.h"
 #include "content/public/android/content_jni_headers/NavigationControllerImpl_jni.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/ssl_host_state_delegate.h"
@@ -320,10 +320,10 @@ void NavigationControllerAndroid::LoadUrl(
     DCHECK(!params.initiator_origin);
     // At the moment, source package name is only used for attribution.
     DCHECK(attribution_source_event_id);
-    params.initiator_origin = OriginFromPackageName(
+    params.initiator_origin = OriginFromAndroidPackageName(
         ConvertJavaStringToUTF8(env, source_package_name));
 
-    params.impression = ConversionHost::ParseImpressionFromApp(
+    params.impression = conversion_host_utils::ParseImpressionFromApp(
         ConvertJavaStringToUTF8(env, attribution_source_event_id),
         ConvertJavaStringToUTF8(env, attribution_destination),
         attribution_report_to
@@ -511,13 +511,6 @@ jboolean NavigationControllerAndroid::IsEntryMarkedToBeSkipped(
     const base::android::JavaParamRef<jobject>& obj,
     jint index) {
   return navigation_controller_->IsEntryMarkedToBeSkipped(index);
-}
-
-// static
-url::Origin NavigationControllerAndroid::OriginFromPackageName(
-    const std::string& package) {
-  return url::Origin::Create(
-      GURL(base::StrCat({content::kAndroidAppScheme, ":", package})));
 }
 
 }  // namespace content
