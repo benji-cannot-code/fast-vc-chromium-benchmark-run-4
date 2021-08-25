6a728373726a96cf43983760e5460965f94bd90e
Trent Apted <tapted@chromium.org>
2021-08-25 03:11:07 +0000

    Generalise codepaths that allow file handlers to work for ChromeOS system apps.
    crrev.com/c/3076601 added Web App file_handlers to the intent system
    with some temporary workarounds to keep ChromeOS system apps working.
    
    This CL re-enables MediaAppIntegrationWithFilesAppAllProfilesTest.FileOpenCanLaunchBothAudioAndImages
    which broke when that happened, with the necessary codepaths generalised.
    
    Bug: 1242867, 1240906
    Change-Id: If4ccf51562a555ae40ee2ec60a10327fea477851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116743
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915040}

--- chrome/browser/web_applications/components/web_app_id_constants.h
+++ chrome/browser/web_applications/components/web_app_id_constants.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_COMPONENTS_WEB_APP_ID_CONSTANTS_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_COMPONENTS_WEB_APP_ID_CONSTANTS_H_
 
+#include "base/strings/string_piece_forward.h"
+
 namespace web_app {
 
 extern const char kA4AppId[];
@@ -41,6 +43,8 @@ extern const char kStadiaAppId[];
 extern const char kYoutubeAppId[];
 extern const char kYoutubeMusicAppId[];
 extern const char kYoutubeTVAppId[];
+
+bool IsSystemAppIdWithFileHandlers(base::StringPiece id);
 
 }  // namespace web_app
 
