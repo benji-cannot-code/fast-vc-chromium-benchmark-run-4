6c1bde52549db3a9cfbab17f64b3a8b47157b541
Marc Treib <treib@chromium.org>
2021-08-26 07:41:30 +0000

    Prepare merging MultiStorePasswordSaveManager into PasswordSaveManagerImpl
    As of crrev.com/c/3097276, PasswordSaveManagerImpl is never used
    directly (outside of some tests), only via its subclass
    MultiStorePasswordSaveManager.
    This CL prepares for merging MultiStorePasswordSaveManager into
    PasswordSaveManagerImpl by moving account_store_form_saver_ (the only
    field of Multi...) into ...Impl. To avoid confusion, it also renames
    form_saver_ to profile_store_form_saver_.
    
    After this, it will be very easy to move all the code up into ...Impl.
    
    Bug: 1108738
    Change-Id: I254f0c4307dcf524a2f1da640c79164203369a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114299
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915493}

--- components/password_manager/core/browser/password_save_manager_impl.cc
+++ components/password_manager/core/browser/password_save_manager_impl.cc
@@ -123,8 +123,10 @@ PasswordSaveManagerImpl::CreatePasswordSaveManagerImpl(
 }
 
 PasswordSaveManagerImpl::PasswordSaveManagerImpl(
-    std::unique_ptr<FormSaver> form_saver)
-    : form_saver_(std::move(form_saver)) {}
+    std::unique_ptr<FormSaver> profile_form_saver,
+    std::unique_ptr<FormSaver> account_form_saver)
+    : profile_store_form_saver_(std::move(profile_form_saver)),
+      account_store_form_saver_(std::move(account_form_saver)) {}
 
 PasswordSaveManagerImpl::~PasswordSaveManagerImpl() = default;
 
@@ -138,7 +140,7 @@ const std::u16string& PasswordSaveManagerImpl::GetGeneratedPassword() const {
 }
 
 FormSaver* PasswordSaveManagerImpl::GetProfileStoreFormSaverForTesting() const {
-  return form_saver_.get();
+  return profile_store_form_saver_.get();
 }
 
 void PasswordSaveManagerImpl::Init(
@@ -253,12 +255,12 @@ void PasswordSaveManagerImpl::Update(
 
 void PasswordSaveManagerImpl::Blocklist(const PasswordFormDigest& form_digest) {
   DCHECK(!client_->IsIncognito());
-  form_saver_->Blocklist(form_digest);
+  profile_store_form_saver_->Blocklist(form_digest);
 }
 
 void PasswordSaveManagerImpl::Unblocklist(
     const PasswordFormDigest& form_digest) {
-  form_saver_->Unblocklist(form_digest);
+  profile_store_form_saver_->Unblocklist(form_digest);
 }
 
 void PasswordSaveManagerImpl::PresaveGeneratedPassword(
@@ -338,7 +340,9 @@ bool PasswordSaveManagerImpl::HasGeneratedPassword() const {
 }
 
 std::unique_ptr<PasswordSaveManager> PasswordSaveManagerImpl::Clone() {
-  auto result = std::make_unique<PasswordSaveManagerImpl>(form_saver_->Clone());
+  auto result = std::make_unique<PasswordSaveManagerImpl>(
+      profile_store_form_saver_->Clone(),
+      account_store_form_saver_ ? account_store_form_saver_->Clone() : nullptr);
   CloneInto(result.get());
   return result;
 }
@@ -471,12 +475,14 @@ void PasswordSaveManagerImpl::SavePendingToStoreImpl(
   auto matches = form_fetcher_->GetAllRelevantMatches();
   std::u16string old_password = GetOldPassword(parsed_submitted_form);
   if (IsNewLogin()) {
-    form_saver_->Save(pending_credentials_, matches, old_password);
+    profile_store_form_saver_->Save(pending_credentials_, matches,
+                                    old_password);
   } else {
     // It sounds wrong that we still update even if the state is NONE. We
     // should double check if this actually necessary. Currently some tests
     // depend on this behavior.
-    form_saver_->Update(pending_credentials_, matches, old_password);
+    profile_store_form_saver_->Update(pending_credentials_, matches,
+                                      old_password);
   }
 }
 
@@ -553,8 +559,8 @@ void PasswordSaveManagerImpl::UploadVotesAndMetrics(
 }
 
 FormSaver* PasswordSaveManagerImpl::GetFormSaverForGeneration() {
-  DCHECK(form_saver_);
-  return form_saver_.get();
+  DCHECK(profile_store_form_saver_);
+  return profile_store_form_saver_.get();
 }
 
 std::vector<const PasswordForm*>
