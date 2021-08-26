79a5bbdc9e27990b05de25eb5781897413133d24
Orin Jaworski <orinj@google.com>
2021-08-26 16:19:26 +0000

    [omnibox] Extract popup selection structures from OmniboxPopupModel
    This CL separates selection and related enums from OmniboxPopupModel
    so they can be used without direct dependence on the model.
    
    Bug: 1208026
    Change-Id: Ia9e3e2cc0680bdedba52e50bd3084d834f4f29fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116122
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915599}

--- chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.h
@@ -77,8 +77,8 @@ class OmniboxPopupContentsView : public views::View,
   // OmniboxPopupView:
   bool IsOpen() const override;
   void InvalidateLine(size_t line) override;
-  void OnSelectionChanged(OmniboxPopupModel::Selection old_selection,
-                          OmniboxPopupModel::Selection new_selection) override;
+  void OnSelectionChanged(OmniboxPopupSelection old_selection,
+                          OmniboxPopupSelection new_selection) override;
   void UpdatePopupAppearance() override;
   void ProvideButtonFocusHint(size_t line) override;
   void OnMatchIconUpdated(size_t match_index) override;
@@ -111,8 +111,8 @@ class OmniboxPopupContentsView : public views::View,
   const AutocompleteMatch& GetMatchAtIndex(size_t index) const;
 
   // Find the index of the match under the given |point|, specified in window
-  // coordinates. Returns OmniboxPopupModel::kNoMatch if there isn't a match at
-  // the specified point.
+  // coordinates. Returns OmniboxPopupSelection::kNoMatch if there isn't a match
+  // at the specified point.
   size_t GetIndexForPoint(const gfx::Point& point);
 
   // Update which result views are visible when the group visibility changes.
