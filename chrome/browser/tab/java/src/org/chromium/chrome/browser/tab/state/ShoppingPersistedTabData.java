13ea870bbdc506d73c78a5e11a98ba41d5f5a4e7
David Maunder <davidjm@chromium.org>
2021-08-26 17:02:31 +0000

    Log price drop metrics when we don't get a response
    We recently introduced metrics logging where we log within the context
    of our price drops feature whether the page was a product detail page,
    has a price and has a price drop. We are finding our metrics are too
    heavy towards the case where we receive a response. We need to log when
    we don't get a response as well and assume that that is a non-shopping
    page (i.e. non product detail page, with no price or price drop).
    
    Bug: 1243441
    Change-Id: I4d42017cdce4e410f87b05ae37f2dc6d1b8417a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119438
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915616}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/ShoppingPersistedTabData.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/ShoppingPersistedTabData.java
@@ -246,12 +246,16 @@ public class ShoppingPersistedTabData extends PersistedTabData {
         OptimizationGuideBridgeFactoryHolder.sOptimizationGuideBridgeFactory.create()
                 .canApplyOptimizationAsync(navigationHandle,
                         HintsProto.OptimizationType.PRICE_TRACKING, (decision, metadata) -> {
+                            mPriceDropMetricsLogger = new PriceDropMetricsLogger(this);
                             if (!tab.isInitialized()
                                     || !tab.getUrl().equals(navigationHandle.getUrl())
                                     || decision != OptimizationGuideDecision.TRUE) {
                                 if (onCompleteForTesting != null) {
                                     onCompleteForTesting.run();
                                 }
+                                mPriceDropMetricsLogger.logPriceDropMetrics(
+                                        METRICS_IDENTIFIER_PREFIX,
+                                        getTimeSinceTabLastOpenedMs(tab));
                                 return;
                             }
                             try {
@@ -259,7 +263,6 @@ public class ShoppingPersistedTabData extends PersistedTabData {
                                         PriceTrackingData.parseFrom(metadata.getValue());
                                 parsePriceTrackingDataProto(tab, priceTrackingDataProto, null);
                                 setLastUpdatedMs(System.currentTimeMillis());
-                                mPriceDropMetricsLogger = new PriceDropMetricsLogger(this);
                                 mPriceDropMetricsLogger.logPriceDropMetrics(
                                         METRICS_IDENTIFIER_PREFIX,
                                         getTimeSinceTabLastOpenedMs(tab));
