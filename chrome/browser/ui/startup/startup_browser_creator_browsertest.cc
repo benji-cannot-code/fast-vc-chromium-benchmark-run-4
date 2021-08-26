3a7ffdec6ec75fc6e7bc87fc38b655e9c5569212
rbpotter <rbpotter@chromium.org>
2021-08-26 20:02:28 +0000

    What's New: Add a fieldtrial testing config
    Note: Not yet enabling on LaCros in the testing config, because LaCros
    bots run tast tests that are reliant on the title of the window opened
    when the browser is opened with a completely new user data directory.
    These tests need to either set the LastWhatsNewVersion pref to the
    current version, or they need to change their expectations when the
    ChromeWhatsNewUI feature is enabled. However, these tests live in a
    different repository so cannot be updated in this CL.
    
    See for example:
    https://chromium.googlesource.com/chromiumos/platform/tast-tests/+/HEAD/src/chromiumos/tast/local/bundles/cros/lacros/app_launcher_launch.go
    
    Bug: 1219381
    Change-Id: I67e1615833692405258921fe9238112b186ddc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119838
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915688}

--- chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
+++ chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/chrome_version.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -197,6 +198,10 @@ bool IsWindows10OrNewer() {
 void DisableWelcomePages(const std::vector<Profile*>& profiles) {
   for (Profile* profile : profiles)
     profile->GetPrefs()->SetBoolean(prefs::kHasSeenWelcomePage, true);
+
+  // Also disable What's New.
+  PrefService* pref_service = g_browser_process->local_state();
+  pref_service->SetInteger(prefs::kLastWhatsNewVersion, CHROME_VERSION_MAJOR);
 }
 
 Browser* OpenNewBrowser(Profile* profile) {
