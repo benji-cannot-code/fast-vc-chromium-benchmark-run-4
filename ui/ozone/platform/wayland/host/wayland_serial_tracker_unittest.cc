919196717e4e9aaf007384ec376e3536400c67c9
Nick Diego Yamane <nickdiego@igalia.com>
2021-08-26 23:40:21 +0000

    ozone/wayland: Make SerialTracker to update WaylandConnection's serial
    This is needed while migration to SerialTracker is still not complete,
    so code using old WaylandConnection's API can be incrementally migrated
    to SerialTracker (in upcoming CLs), and can be reverted once it is done.
    
    R=msisov@igalia.com, tonikitoo@igalia.com
    
    Bug: 1211874
    Test: Covered by ozone_unittests.
    Change-Id: I8e08e667ac6b91fb43fc938840dd835012231ba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122148
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#915768}

--- ui/ozone/platform/wayland/host/wayland_serial_tracker_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_serial_tracker_unittest.cc
@@ -7,12 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/ozone/platform/wayland/host/wayland_connection.h"
 #include "ui/ozone/platform/wayland/host/wayland_serial_tracker.h"
 
 namespace wl {
 
 TEST(WaylandSerialTrackerTest, Basics) {
-  SerialTracker serial_tracker;
+  ui::WaylandConnection connection;
+  SerialTracker serial_tracker(&connection);
   absl::optional<Serial> serial;
 
   // Ensure it is initialized as expected.
@@ -62,7 +64,8 @@ TEST(WaylandSerialTrackerTest, Basics) {
 // Verifies GetSerial() behaves correctly when multiple values are set and
 // queried.
 TEST(WaylandSerialTrackerTest, Queries) {
-  SerialTracker serial_tracker;
+  ui::WaylandConnection connection;
+  SerialTracker serial_tracker(&connection);
   absl::optional<Serial> serial;
 
   serial_tracker.UpdateSerial(SerialType::kMouseEnter, 1u);
