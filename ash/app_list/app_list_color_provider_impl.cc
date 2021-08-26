bba9c04a96aa1e48fbe270814760c2c05502b28c
Yulun Wu <yulunwu@chromium.org>
2021-08-26 16:31:53 +0000

    Fix search result view highlight color.
    Bug: 1216097
    Change-Id: I102cc0f8aee9817758be12c2970f5769a86ac0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3120233
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915604}

--- ash/app_list/app_list_color_provider_impl.cc
+++ ash/app_list/app_list_color_provider_impl.cc
@@ -208,7 +208,7 @@ float AppListColorProviderImpl::GetRippleAttributesHighlightOpacity(
 
 SkColor AppListColorProviderImpl::GetSearchResultViewHighlightColor() const {
   return ash_color_provider_->GetContentLayerColor(
-      AshColorProvider::ContentLayerType::kTextColorURL);
+      AshColorProvider::ContentLayerType::kHighlightColorHover);
 }
 
 }  // namespace ash
