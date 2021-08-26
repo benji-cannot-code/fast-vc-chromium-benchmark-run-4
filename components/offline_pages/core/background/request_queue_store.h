df89e0a8de5a8cdbffc774085f34ecb5af35553d
Lei Zhang <thestig@chromium.org>
2021-08-26 20:42:15 +0000

    Fix headers that include themselves.
    Change-Id: I89301ce38e717bd71dd87f87509d67e36369edeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123127
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915709}

--- components/offline_pages/core/background/request_queue_store.h
+++ components/offline_pages/core/background/request_queue_store.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
 #include "components/offline_pages/core/background/request_queue.h"
-#include "components/offline_pages/core/background/request_queue_store.h"
 #include "components/offline_pages/core/background/save_page_request.h"
 #include "components/offline_pages/core/offline_store_types.h"
 
