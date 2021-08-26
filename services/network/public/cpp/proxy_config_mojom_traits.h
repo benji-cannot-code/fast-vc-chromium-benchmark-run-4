bcecb702891f1508d0135140b082daf79234c326
Nicolas Arciniega <niarci@microsoft.com>
2021-08-26 17:38:42 +0000

    Create a separate build target for ProxyConfig mojo
    This change prepares for the creation of a service that will do proxy
    resolution on Windows using WinHttp APIs. The new service will return
    a proxy resolution result via a net::ProxyList. In order to accomplish
    this with the right mojo typemaps, the new service will need to depend
    on the ProxyConfig mojo interfaces. Breaking those out into a new build
    target will avoid a circular dependency with the network service.
    
    This CL has no functional changes. It just separates out proxy-related
    mojo files into a separate build target.
    
    Bug: 1032820
    Change-Id: I8b704aeafb597e845604d754a4b80664c0615f89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3035668
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Nicolas Arciniega <niarci@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#915636}

--- services/network/public/cpp/proxy_config_mojom_traits.h
+++ services/network/public/cpp/proxy_config_mojom_traits.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/proxy_resolution/proxy_config_with_annotation.h"
 #include "net/proxy_resolution/proxy_list.h"
 #include "services/network/public/mojom/proxy_config.mojom-shared.h"
-#include "url/mojom/url_gurl_mojom_traits.h"
 
 // This file handles the serialization of net::ProxyConfig.
 
