05b6cf6b371a6f4da7b937acd02095482969d519
Delan Azabani <dazabani@igalia.com>
2021-08-26 11:44:24 +0000

    CSS highlight painting: split originating and ::selection decorations
    This patch builds on /c/2902289 by splitting the paints for unselected
    and selection text, which means that for the first time, all of these
    properties should hold true regardless of whether text is selected:
    
    P1. underlines and overlines are never painted over text
    P2. lines-through are always painted over text
    P3. decorations on any unselected parts are in originating td-color
    P4. selected part decorations paint over ::selection background
    P5. ::selection background always paints over unselected text
    
    Here’s a comparison of how these properties have evolved:
    
    • pre-M90 ensured P1 + P4, but violated P2 + P3 + P5
    • /c/2647046 ensured P1 + P5, but violated P2 + P3 + P4
    • /c/2902289 ensured P2 + P4 + P5, but violated P1 + P3
    • this patch ensures P1 + P2 + P3 + P4 + P5
    
    To test the properties more effectively, I’ve replaced one of my
    highlight painting tests with two more effective tests, and added
    smaller tests for properties P1 through P4.
    
    <https://wpt.live/css/css-pseudo/highlight-painting-003.html> now
    tests solid decorations with ::selection underline and originating
    line-through, plus an originating background.
    
    • https://bucket.daz.cat/ee288ed9a20b1a2f.png = pre-M90
    • https://bucket.daz.cat/e689c7ada0c05594.png = /c/2647046
    • https://bucket.daz.cat/42d48433dfc51e15.png = /c/2902289
    • https://bucket.daz.cat/1658767a17294d93.png = this patch
    
    <https://wpt.live/css/css-pseudo/highlight-painting-004.html> tests
    wavy decorations, but with originating underline and ::selection
    line-through, and no originating background:
    
    • https://bucket.daz.cat/5e28cd46a5972359.png = pre-M90
    • https://bucket.daz.cat/b44c05422d47a0b3.png = /c/2647046
    • https://bucket.daz.cat/5e28cd46a5972359.png = /c/2902289
    • https://bucket.daz.cat/6647e4443a92cec5.png = this patch
    
    For reference, here are the refs:
    
    • https://bucket.daz.cat/439df4145d54fede.png = 003-ref
    • https://bucket.daz.cat/05b20f2ec3675e7e.png = 004-ref1
    • https://bucket.daz.cat/c898a37e256b3e44.png = 004-ref2
    • https://bucket.daz.cat/bd4b272d1bcdf932.png = 004-ref3
    
    This brings us closer to passing those tests, but not quite there.
    Next up, we need to recolor the decorations lifted from originating
    style to the ::selection overlay to ::selection’s color, not its
    text-decoration-color as done in <https://crrev.com/c/2383212>.
    
    Bug: 1147859
    Change-Id: I011323db9b481175fb750ab810b6741239522087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2903387
    Commit-Queue: Delan Azabani <dazabani@igalia.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915543}

--- third_party/blink/renderer/core/paint/ng/ng_highlight_painter.h
+++ third_party/blink/renderer/core/paint/ng/ng_highlight_painter.h
@@ -127,11 +127,6 @@ class CORE_EXPORT NGHighlightPainter {
   void Paint(Phase phase);
 
   SelectionPaintState* Selection() { return selection_; }
-  absl::optional<AppliedTextDecoration> SelectionDecoration() {
-    return selection_
-               ? selection_->GetSelectionStyle().selection_text_decoration
-               : absl::nullopt;
-  }
 
  private:
   NGTextPainter& text_painter_;
