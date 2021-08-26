c6cad33a8162a2737e5c29ebc03090a99393c8ce
Tomasz Wiszkowski <ender@google.com>
2021-08-26 00:25:26 +0000

    Omnibox Suggestions: use outline to round images.
    This change implements a lightweight RoundedCornerOutlineProvider that
    can be used by any view to round its corners.
    
    This rounding mechanism:
    - does not constrain users to bitmaps and simple colors - works with
      any type of image (svg included) and any view type, containers
      included
    - does accurate rounding (ie. the rounding is not 'scaled' or
      'stretched')
    - uses hardware acceleration
    - accurately applies the rounding to focus/selection ripple
    - memory-efficient (requires no intermediate bitmaps or canvasses)
    - fully compatible with ImageView (including image tinting etc)
    - can (with minor updates) round 1, 2 or all 4 corners of the view.
    
    More info here:
    http://doc/15b8chFu1gkaIdPlpQ29Jg4CXKCLLLxb0imzG9bPlc-g
    
    Change-Id: I70e942f5042a470a1f52036948c17d8ef5ca86ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098915
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#915380}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinderUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinderUnitTest.java
@@ -33,7 +33,6 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.omnibox.styles.OmniboxTheme;
 import org.chromium.chrome.browser.omnibox.suggestions.SuggestionCommonProperties;
 import org.chromium.chrome.browser.omnibox.suggestions.base.BaseSuggestionViewProperties.Action;
-import org.chromium.components.browser_ui.widget.RoundedCornerImageView;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
@@ -54,7 +53,7 @@ public class BaseSuggestionViewBinderUnitTest {
     DecoratedSuggestionView mDecoratedView;
 
     @Mock
-    RoundedCornerImageView mIconView;
+    ImageView mIconView;
 
     @Mock
     ImageView mContentView;
@@ -95,9 +94,7 @@ public class BaseSuggestionViewBinderUnitTest {
         SuggestionDrawableState state = SuggestionDrawableState.Builder.forColor(0).build();
         mModel.set(BaseSuggestionViewProperties.ICON, state);
 
-        // Expect a single call to setRoundedCorners, and make sure this call sets all radii to 0.
-        verify(mIconView).setRoundedCorners(0, 0, 0, 0);
-        verify(mIconView).setRoundedCorners(anyInt(), anyInt(), anyInt(), anyInt());
+        verify(mIconView).setClipToOutline(false);
 
         verify(mIconView).setVisibility(View.VISIBLE);
         verify(mIconView).setImageDrawable(state.drawable);
@@ -109,9 +106,7 @@ public class BaseSuggestionViewBinderUnitTest {
                 SuggestionDrawableState.Builder.forColor(0).setUseRoundedCorners(true).build();
         mModel.set(BaseSuggestionViewProperties.ICON, state);
 
-        // Expect a single call to setRoundedCorners, and make sure this call sets radii to non-0.
-        verify(mIconView, never()).setRoundedCorners(0, 0, 0, 0);
-        verify(mIconView).setRoundedCorners(anyInt(), anyInt(), anyInt(), anyInt());
+        verify(mIconView).setClipToOutline(true);
 
         verify(mIconView).setVisibility(View.VISIBLE);
         verify(mIconView).setImageDrawable(state.drawable);
