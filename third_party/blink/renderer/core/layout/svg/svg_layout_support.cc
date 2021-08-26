80907be90abd6fdb0c3097a3ff6d29391a281c28
Yuki Yamada <yukiy@chromium.org>
2021-08-26 09:34:27 +0000

    Layout: Apply DISALLOW_NEW to SearchCandidate
    Apply DISALLOW_NEW to SearchCandidate to replace UntracedMembers in it
    added by https://crrev.com/c/2465602 to Member.
    
    This CL is a follow-up for LayoutObject conversion, so this will be
    landed after that.
    
    Bug: 1030176
    Change-Id: Id81823628fd50f55673dda4e7d33fbd4ea4cb003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2397059
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915518}

--- third_party/blink/renderer/core/layout/svg/svg_layout_support.cc
+++ third_party/blink/renderer/core/layout/svg/svg_layout_support.cc
@@ -40,16 +40,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/core/svg/svg_length_context.h"
 #include "third_party/blink/renderer/platform/graphics/stroke_data.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/clear_collection_scope.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 
 namespace blink {
 
 struct SearchCandidate {
+  DISALLOW_NEW();
+
   SearchCandidate()
       : layout_object(nullptr), distance(std::numeric_limits<float>::max()) {}
   SearchCandidate(LayoutObject* layout_object, float distance)
       : layout_object(layout_object), distance(distance) {}
-  UntracedMember<LayoutObject> layout_object;
+  void Trace(Visitor* visitor) const { visitor->Trace(layout_object); }
+
+  Member<LayoutObject> layout_object;
   float distance;
 };
 
@@ -451,7 +456,8 @@ static SearchCandidate SearchTreeForFindClosestLayoutSVGText(
     const FloatPoint& point) {
   // Try to find the closest LayoutSVGText.
   SearchCandidate closest_text;
-  Vector<SearchCandidate> candidates;
+  HeapVector<SearchCandidate> candidates;
+  ClearCollectionScope<HeapVector<SearchCandidate>> scope(&candidates);
 
   // Find the closest LayoutSVGText on this tree level, and also collect any
   // containers that could contain LayoutSVGTexts that are closer.
@@ -522,3 +528,5 @@ void SVGLayoutSupport::NotifySVGRootOfChangedCompositingReasons(
 }
 
 }  // namespace blink
+
+WTF_ALLOW_CLEAR_UNUSED_SLOTS_WITH_MEM_FUNCTIONS(blink::SearchCandidate)
