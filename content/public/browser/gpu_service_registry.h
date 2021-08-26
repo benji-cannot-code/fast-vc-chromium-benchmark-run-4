4fe533e58876c93d7eb60afd94ac4dfcce005cdf
Alex Gough <ajgo@chromium.org>
2021-08-26 02:22:33 +0000

    Validate sandbox is kGpu in BindInterfaceInGpuProcess
    Adds compile time validation that an interface is marked with
    the ServiceSandbox=sandbox.mojom.Sandbox.kGpu attribute in
    content/public/browser/gpu_service_registry.h
    
    Adds the attribute to all the interfaces that now need it.
    
    Bug: 1238192
    Change-Id: I21eb46b8491ebaccb7aceccd74fac0c6070ce2a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3111838
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915443}

--- content/public/browser/gpu_service_registry.h
+++ content/public/browser/gpu_service_registry.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/system/message_pipe.h"
+#include "sandbox/policy/mojom/sandbox.mojom.h"
 
 namespace content {
 
@@ -19,9 +20,18 @@ CONTENT_EXPORT void BindInterfaceInGpuProcess(
     const std::string& interface_name,
     mojo::ScopedMessagePipeHandle interface_pipe);
 
-// Bind to an interface exposed by the GPU process.
+// Bind to an interface exposed by the GPU process. The mojo interface must be
+// marked with a [ServiceSandbox=sandbox.mojom.Sandbox.kGpu] attribute.
 template <typename Interface>
 void BindInterfaceInGpuProcess(mojo::PendingReceiver<Interface> receiver) {
+  using ProvidedSandboxType = decltype(Interface::kServiceSandbox);
+  static_assert(
+      std::is_same<ProvidedSandboxType, const sandbox::mojom::Sandbox>::value,
+      "This interface does not declare a proper ServiceSandbox attribute. "
+      "See //docs/mojo_and_services.md (Specifying a sandbox).");
+  static_assert(Interface::kServiceSandbox == sandbox::mojom::Sandbox::kGpu,
+                "This interface must have [ServiceSandbox=kGpu].");
+
   BindInterfaceInGpuProcess(Interface::Name_, receiver.PassPipe());
 }
 
