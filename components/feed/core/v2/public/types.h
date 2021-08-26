11faf0dad930c95159814dff5bfa6bc7eff22ae6
Dan Harrington <harringtond@chromium.org>
2021-08-26 23:01:18 +0000

    webfeed: fetch favicons directly
    Fetch them directly using the image URLs provided
    by the server. Fallback to the site URL when
    the favicon image url is not present, or when
    the fetch fails.
    
    Encapsulate fetching of favicons in a new
    WebFeedFaviconFetcher class, which is used for the
    menu item and the follow management surface.
    
    I also fixed a bug with the layout of the follow
    management UI: the delayed load of favicons affected
    the follow item layout, making the items taller
    after the favicons load. Instead, now the favicon
    ImageView has a fixed width and height.
    
    Bug: b/197266800
    Change-Id: I043cf338a395b20f7c5f19c13afb12359851bedc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119219
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915756}

--- components/feed/core/v2/public/types.h
+++ components/feed/core/v2/public/types.h
@@ -151,6 +151,7 @@ struct WebFeedMetadata {
   WebFeedMetadata();
   WebFeedMetadata(const WebFeedMetadata&);
   WebFeedMetadata(WebFeedMetadata&&);
+  ~WebFeedMetadata();
   WebFeedMetadata& operator=(const WebFeedMetadata&);
   WebFeedMetadata& operator=(WebFeedMetadata&&);
 
@@ -165,6 +166,7 @@ struct WebFeedMetadata {
   GURL publisher_url;
   WebFeedSubscriptionStatus subscription_status =
       WebFeedSubscriptionStatus::kUnknown;
+  GURL favicon_url;
 };
 std::ostream& operator<<(std::ostream& out, const WebFeedMetadata& value);
 
