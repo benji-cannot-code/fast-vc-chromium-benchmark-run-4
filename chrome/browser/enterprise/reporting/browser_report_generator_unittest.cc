368a153533c26c9cc0d71ff38ef45447f0646992
Hidehiko Abe <hidehiko@chromium.org>
2021-08-26 15:01:26 +0000

    Fix Lacros official build unittests breakage.
    With crrev.com/c/3031561 BrowserReporterGeneratorTest is enabled for Lacros.
    However, because of ifdef condition mismatch, build break found.
    This CL fixes it.
    
    Bug: 1228835
    Test: Ran unittests for Lacros locally.
    Change-Id: I22b7b74e3237b6fc78e6f95d7185b6b28b297610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3118309
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915578}

--- chrome/browser/enterprise/reporting/browser_report_generator_unittest.cc
+++ chrome/browser/enterprise/reporting/browser_report_generator_unittest.cc
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && !defined(OS_CHROMEOS)
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && !BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/test/base/scoped_channel_override.h"
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING) && !defined(OS_CHROMEOS)
 
