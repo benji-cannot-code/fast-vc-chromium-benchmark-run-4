a658edccb8510381d3a35d1e3b3a5f0084dcf764
Muhammad Hasan Khan <mhasank@chromium.org>
2021-08-27 20:47:21 +0000

    arc: Remove deprecated GetIcingGlobalQueryResults api
    There is no code using this api.
    
    BUG=818902, b:196426538
    TEST=cq
    
    Change-Id: I7e3ef56d3e560422e5b4adbfb2406e9466dd4a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122686
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Commit-Queue: Muhammad Hasan Khan <mhasank@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916099}

--- components/arc/test/fake_app_instance.h
+++ components/arc/test/fake_app_instance.h
@@ -164,10 +164,6 @@ class FakeAppInstance : public mojom::AppInstance {
       const std::string& query,
       int32_t max_results,
       GetRecentAndSuggestedAppsFromPlayStoreCallback callback) override;
-  void GetIcingGlobalQueryResultsDeprecated(
-      const std::string& query,
-      int32_t max_results,
-      GetIcingGlobalQueryResultsDeprecatedCallback callback) override;
   void GetAppShortcutGlobalQueryItems(
       const std::string& query,
       int32_t max_results,
