2a5eb7ddff6587235ac57c3d5a32662837a75a7b
Sushma Venkatesh Reddy <sushma.venkatesh.reddy@intel.com>
2021-08-27 20:28:23 +0000

    Ensure encode tasks are executed on the encoder thread
    Some encoder related tasks were not executed on the encoder thread which
    led to the crashes when sequence check in CL:2793005 was applied. This
    patch helps fix those issues by performing all the necessary tasks on
    encoder thread.
    
    BUG=b:183515581
    TEST=VP9 encode shouldn't cause any crashes
    
    Signed-off-by: Sushma Venkatesh Reddy <sushma.venkatesh.reddy@intel.com>
    Change-Id: I4547758a3704181b8f902e2846b645ee3d1d9330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2976427
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916092}

--- media/gpu/vaapi/vaapi_video_encode_accelerator.h
+++ media/gpu/vaapi/vaapi_video_encode_accelerator.h
@@ -88,6 +88,8 @@ class MEDIA_GPU_EXPORT VaapiVideoEncodeAccelerator
   //
   void InitializeTask(const Config& config);
 
+  bool AttemptedInitialization() const { return !!client_ptr_factory_; }
+
   // Enqueues |frame| onto the queue of pending inputs and attempts to continue
   // encoding.
   void EncodeTask(scoped_refptr<VideoFrame> frame, bool force_keyframe);
@@ -205,7 +207,8 @@ class MEDIA_GPU_EXPORT VaapiVideoEncodeAccelerator
 
   // VaapiWrapper is the owner of all HW resources (surfaces and buffers)
   // and will free them on destruction.
-  scoped_refptr<VaapiWrapper> vaapi_wrapper_;
+  scoped_refptr<VaapiWrapper> vaapi_wrapper_
+      GUARDED_BY_CONTEXT(encoder_sequence_checker_);
 
   // The expected coded size of incoming video frames when |native_input_mode_|
   // is false.
@@ -274,7 +277,8 @@ class MEDIA_GPU_EXPORT VaapiVideoEncodeAccelerator
 
   // VaapiWrapper for VPP (Video Pre Processing). This is used for scale down
   // for the picture send to vaapi encoder.
-  scoped_refptr<VaapiWrapper> vpp_vaapi_wrapper_;
+  scoped_refptr<VaapiWrapper> vpp_vaapi_wrapper_
+      GUARDED_BY_CONTEXT(encoder_sequence_checker_);
 
   // The completion callback of the Flush() function.
   FlushCallback flush_callback_;
