3420c62e038f9dade2892729ff8dbb99f1ed0940
Brian Sheedy <bsheedy@chromium.org>
2021-08-27 22:25:42 +0000

    Add Gold local output directory flag
    Updates the Python Skia Gold code to accept
    --skia-gold-local-png-write-directory, which causes local diff images
    to be saved to subdirectories of the given path instead of temporary
    directories. This makes it easier for users to look through results
    since they are all in one place instead of scattered across randomly
    named temporary directories.
    
    Bug: 1243813
    Change-Id: Ia603fe7d522243822d5c7c7aa4ca436f730bedcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3125815
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916139}

--- build/skia_gold_common/skia_gold_session.py
+++ build/skia_gold_common/skia_gold_session.py
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import logging
 import os
 import shutil
-import subprocess
 import sys
 import tempfile
+import time
 
 CHROMIUM_SRC = os.path.realpath(
     os.path.join(os.path.dirname(__file__), '..', '..'))
@@ -74,6 +74,8 @@ class SkiaGoldSession(object):
     self._corpus = corpus
     self._instance = instance
     self._bucket = bucket
+    self._local_png_directory = (self._gold_properties.local_png_directory
+                                 or tempfile.mkdtemp())
     self._triage_link_file = tempfile.NamedTemporaryFile(suffix='.txt',
                                                          dir=working_dir,
                                                          delete=False).name
@@ -369,7 +371,7 @@ class SkiaGoldSession(object):
           '--bypass-skia-gold-functionality is not supported when running '
           'tests locally.')
 
-    output_dir = self._CreateDiffOutputDir()
+    output_dir = self._CreateDiffOutputDir(name)
     # TODO(skbug.com/10611): Remove this temporary work dir and instead just use
     # self._working_dir once `goldctl diff` stops clobbering the auth files in
     # the provided work directory.
@@ -504,7 +506,10 @@ class SkiaGoldSession(object):
     """
     open(self._triage_link_file, 'w').close()
 
-  def _CreateDiffOutputDir(self):
+  def _CreateDiffOutputDir(self, _):
+    # We don't use self._local_png_directory here since we want it to be
+    # automatically cleaned up with the working directory. Any subclasses that
+    # want to keep it around can override this method.
     return tempfile.mkdtemp(dir=self._working_dir)
 
   def _GetDiffGoldInstance(self):
