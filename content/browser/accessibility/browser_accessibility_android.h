e9db8ea45c61a4c1526a3aa676a8e884e575cba9
Xiaocheng Hu <xiaochengh@chromium.org>
2021-08-27 02:33:15 +0000

    [accessibility] Ignore AXObject descendants of list markers
    As discussed in [1], the text descendants of a list marker do not carry
    extra information than the marker itself, and hence, should be ignored
    in the AX tree.
    
    Most of this patch are test expectation updates. The real changes are:
    - blink/renderer/modules/accessibility: Make text descendants of list
      markers ignored by acccessibility
    - ui/accessibility/ax_node.cc: Set IsText() to tree for list markers
    - ui/accessibility/ax_position.h: Make positions anchored to list
      markers leaf positions
    - And some other platform-specific minor changes
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3061476/comments/cd6abde4_acaed71c
    
    Bug: 1166766
    Change-Id: I586d37dc3b6fd6f71774032fda6dd15946f4d03c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072884
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915811}

--- content/browser/accessibility/browser_accessibility_android.h
+++ content/browser/accessibility/browser_accessibility_android.h
@@ -203,6 +203,7 @@ class CONTENT_EXPORT BrowserAccessibilityAndroid : public BrowserAccessibility {
 
   bool HasOnlyTextChildren() const;
   bool HasOnlyTextAndImageChildren() const;
+  bool HasListMarkerChild() const;
   bool ShouldExposeValueAsName() const;
   int CountChildrenWithRole(ax::mojom::Role role) const;
 
