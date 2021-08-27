e53d2253564fc75b65baf9411d4fe522648c9de3
Eric Willigers <ericwilligers@chromium.org>
2021-08-27 02:36:02 +0000

    Fix WebAppRegistrar includes
    app_icon_factory.h makes use of SquareSizePx
    web_app_registrar.h makes use of SkColor
    
    Each file now includes the relevant header.
    
    Bug: 1144877
    Change-Id: Id58cb743f88f8b13f74070608d0cba409ec6041f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122689
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915814}

--- chrome/browser/web_applications/web_app_registrar.h
+++ chrome/browser/web_applications/web_app_registrar.h
@@ -21,6 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/protocol_handler_info.h"
 #include "components/services/app_service/public/cpp/url_handler_info.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/skia/include/core/SkColor.h"
+
+namespace apps {
+struct ShareTarget;
+}  // namespace apps
 
 namespace web_app {
 
