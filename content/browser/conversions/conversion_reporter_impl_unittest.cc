8a51a8fdb3599035902ef70c1356a5ce74ab819e
Andrew Paseltiner <apaseltiner@chromium.org>
2021-08-27 21:34:31 +0000

    [attribution_reporting] Add report-sent time and highlight HTTP errors in internals UI
    Screenshot of changes available in crbug.com/1243313#c5.
    
    We change SentReportInfo to store the entire ConversionReport, since we
    may wish to expose additional fields in the sent reports table in the
    future.
    
    Bug: 1243313
    Change-Id: Ic5f779ebae390ccc9bab226e5c167962586f6816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3118988
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916122}

--- content/browser/conversions/conversion_reporter_impl_unittest.cc
+++ content/browser/conversions/conversion_reporter_impl_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_browser_context.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace content {
 
@@ -45,13 +46,11 @@ class MockNetworkSender : public ConversionReporterImpl::NetworkSender {
  public:
   MockNetworkSender() = default;
 
-  void SendReport(const ConversionReport& conversion_report,
+  void SendReport(ConversionReport report,
                   ReportSentCallback sent_callback) override {
-    last_sent_report_id_ = *conversion_report.conversion_id;
+    last_sent_report_id_ = *report.conversion_id;
     num_reports_sent_++;
-    SentReportInfo info = GetBlankSentReportInfo();
-    info.conversion_id = *conversion_report.conversion_id;
-    info.original_report_time = conversion_report.original_report_time;
+    SentReportInfo info = GetBlankSentReportInfo(std::move(report));
     info.http_response_code = 200;
     std::move(sent_callback).Run(std::move(info));
   }
@@ -90,7 +89,7 @@ class ConversionReporterImplTest : public testing::Test {
 
   const base::Clock& clock() { return *task_environment_.GetMockClock(); }
 
-  const SentReportInfo& last_sent_report_info() const {
+  const absl::optional<SentReportInfo>& last_sent_report_info() const {
     return last_sent_report_info_;
   }
 
@@ -103,7 +102,7 @@ class ConversionReporterImplTest : public testing::Test {
   MockNetworkSender* sender_;
 
  private:
-  SentReportInfo last_sent_report_info_ = GetBlankSentReportInfo();
+  absl::optional<SentReportInfo> last_sent_report_info_;
 
   void OnReportSent(SentReportInfo info) {
     last_sent_report_info_ = std::move(info);
@@ -119,8 +118,8 @@ TEST_F(ConversionReporterImplTest,
   // sent.
   task_environment_.FastForwardBy(base::TimeDelta());
   EXPECT_EQ(1, *sender_->last_sent_report_id());
-  EXPECT_EQ(1L, *last_sent_report_info().conversion_id);
-  EXPECT_EQ(200, last_sent_report_info().http_response_code);
+  EXPECT_EQ(1L, *last_sent_report_info()->report.conversion_id.value());
+  EXPECT_EQ(200, last_sent_report_info()->http_response_code);
 }
 
 TEST_F(ConversionReporterImplTest,
@@ -133,8 +132,8 @@ TEST_F(ConversionReporterImplTest,
   // sent.
   task_environment_.FastForwardBy(base::TimeDelta());
   EXPECT_EQ(1, *sender_->last_sent_report_id());
-  EXPECT_EQ(1L, *last_sent_report_info().conversion_id);
-  EXPECT_EQ(200, last_sent_report_info().http_response_code);
+  EXPECT_EQ(1L, *last_sent_report_info()->report.conversion_id.value());
+  EXPECT_EQ(200, last_sent_report_info()->http_response_code);
 }
 
 TEST_F(ConversionReporterImplTest,
@@ -197,7 +196,8 @@ TEST_F(ConversionReporterImplTest, ManyReportsAddedAtOnce_SentInOrder) {
 
     EXPECT_EQ(static_cast<size_t>(i), sender_->num_reports_sent());
     EXPECT_EQ(static_cast<int64_t>(i), *sender_->last_sent_report_id());
-    EXPECT_EQ(static_cast<int64_t>(i), *last_sent_report_info().conversion_id);
+    EXPECT_EQ(static_cast<int64_t>(i),
+              *last_sent_report_info()->report.conversion_id.value());
   }
 }
 
@@ -215,7 +215,8 @@ TEST_F(ConversionReporterImplTest, ManyReportsAddedSeparately_SentInOrder) {
 
     EXPECT_EQ(static_cast<size_t>(i), sender_->num_reports_sent());
     EXPECT_EQ(static_cast<int64_t>(i), *sender_->last_sent_report_id());
-    EXPECT_EQ(static_cast<int64_t>(i), *last_sent_report_info().conversion_id);
+    EXPECT_EQ(static_cast<int64_t>(i),
+              *last_sent_report_info()->report.conversion_id.value());
   }
 }
 
@@ -230,9 +231,9 @@ TEST_F(ConversionReporterImplTest, EmbedderDisallowsConversions_ReportNotSent) {
   // sent.
   task_environment_.FastForwardBy(base::TimeDelta());
   EXPECT_EQ(0u, sender_->num_reports_sent());
-  EXPECT_EQ(1L, *last_sent_report_info().conversion_id);
+  EXPECT_EQ(1L, *last_sent_report_info()->report.conversion_id.value());
   // Verify that the report was not sent to the NetworkSender.
-  EXPECT_EQ(0, last_sent_report_info().http_response_code);
+  EXPECT_EQ(0, last_sent_report_info()->http_response_code);
   SetBrowserClientForTesting(old_browser_client);
 }
 
@@ -286,9 +287,9 @@ TEST_F(ConversionReporterImplTest, EmbedderDisallowedContext_ReportNotSent) {
         << "impression_origin; " << test_case.impression_origin
         << ", conversion_origin: " << test_case.conversion_origin
         << ", reporting_origin: " << test_case.reporting_origin;
-    EXPECT_EQ(1L, *last_sent_report_info().conversion_id);
+    EXPECT_EQ(1L, *last_sent_report_info()->report.conversion_id.value());
     EXPECT_EQ(test_case.report_allowed ? 200 : 0,
-              last_sent_report_info().http_response_code);
+              last_sent_report_info()->http_response_code);
 
     sender_->Reset();
   }
--- content/browser/conversions/sent_report_info.cc
+++ content/browser/conversions/sent_report_info.cc
@@ -7,14 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-SentReportInfo::SentReportInfo(ConversionReport::Id conversion_id,
-                               base::Time original_report_time,
+SentReportInfo::SentReportInfo(ConversionReport report,
                                GURL report_url,
                                std::string report_body,
                                int http_response_code,
                                bool should_retry)
-    : conversion_id(conversion_id),
-      original_report_time(original_report_time),
+    : report(std::move(report)),
       report_url(std::move(report_url)),
       report_body(std::move(report_body)),
       http_response_code(http_response_code),
