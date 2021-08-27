e4fceb5327b53182836c44add8d494d80507f240
Ken Buchanan <kenrb@chromium.org>
2021-08-27 18:27:29 +0000

    [CredMan] Add UseCounters for PasswordCredential and FederatedCredential
    This adds UseCounters for each of the get, store and create operations
    for PasswordCredential and FederatedCredential.
    
    Change-Id: Iaf52c973add781c8d9e6081e4d7ca3684bfa7537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123877
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916043}

--- third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
@@ -820,6 +820,14 @@ ScriptPromise CredentialsContainer::get(
     return promise;
   }
 
+  if (options->hasFederated()) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialManagerGetFederatedCredential);
+  } else if (options->hasPassword()) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialManagerGetPasswordCredential);
+  }
+
   if (options->hasPublicKey()) {
     auto cryptotoken_origin = SecurityOrigin::Create(KURL(kCryptotokenOrigin));
     if (!cryptotoken_origin->IsSameOriginWith(
@@ -1042,6 +1050,14 @@ ScriptPromise CredentialsContainer::store(ScriptState* script_state,
     return promise;
   }
 
+  if (credential->IsFederatedCredential()) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialManagerStoreFederatedCredential);
+  } else if (credential->IsPasswordCredential()) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialManagerStorePasswordCredential);
+  }
+
   const KURL& url =
       credential->IsFederatedCredential()
           ? static_cast<const FederatedCredential*>(credential)->iconURL()
@@ -1088,6 +1104,8 @@ ScriptPromise CredentialsContainer::create(
   }
 
   if (options->hasPassword()) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialManagerCreatePasswordCredential);
     resolver->Resolve(
         options->password()->IsPasswordCredentialData()
             ? PasswordCredential::Create(
@@ -1099,6 +1117,8 @@ ScriptPromise CredentialsContainer::create(
     return promise;
   }
   if (options->hasFederated()) {
+    UseCounter::Count(resolver->GetExecutionContext(),
+                      WebFeature::kCredentialManagerCreateFederatedCredential);
     resolver->Resolve(
         FederatedCredential::Create(options->federated(), exception_state));
     return promise;
