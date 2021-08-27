aa043b33473ad471cbc449a89880740fde4c18ec
Joel Hockey <joelhockey@chromium.org>
2021-08-27 04:27:32 +0000

    Fix ifdef for IsTerminalSystemWebAppNaclPage() decl
    Function currently is only for IS_CHROMEOS_ASH, but I hadn't realized
    that it is also nested inside ENABLE_NACL and ENABLE_EXTENSIONS.
    
    Bug: 1241262
    Change-Id: I0d6c3ba63a83d1616c3d2ce0a0bbcdf18067f383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3121777
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#915834}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -348,7 +348,8 @@ void MaybeEnableWebShare() {
 #endif
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(ENABLE_NACL) && BUILDFLAG(ENABLE_EXTENSIONS) && \
+    BUILDFLAG(IS_CHROMEOS_ASH)
 bool IsTerminalSystemWebAppNaClPage(GURL url) {
   url::Replacements<char> replacements;
   replacements.ClearQuery();
