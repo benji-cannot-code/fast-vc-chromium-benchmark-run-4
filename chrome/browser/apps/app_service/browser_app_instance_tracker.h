8b893b19c843ecabdabdea84f696bd75834ed89d
Alexander Bolodurin <alexbn@chromium.org>
2021-08-27 09:50:43 +0000

    [lacros shelf] Use ActivationClient instead of BrowserList
    There are differences in Aura and Desktop Aura, in some cases causing
    inconsistency between browser window object not yet being active when
    BrowserList::OnBrowserSetLastActive() runs, so BrowserList API is not
    suitable to use in Lacros.
    
    Fall back to using wm::ActivationClient directly, but in a manner
    compatible with Lacros: track multiple activation clients corresponding
    to each window's root window. Observe new activation clients as they are
    encountered, and unobserve them when the last window associated with it
    is removed.
    
    Bug: 1203992
    Change-Id: Idc711dcb3c18defc0bdd4e87e1a9a575d452e7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3120905
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915882}

--- chrome/browser/apps/app_service/browser_app_instance_tracker.h
+++ chrome/browser/apps/app_service/browser_app_instance_tracker.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
+#include "ui/wm/public/activation_change_observer.h"
+#include "ui/wm/public/activation_client.h"
 
 class Browser;
 class Profile;
@@ -41,6 +43,7 @@ class BrowserAppInstanceObserver;
 class BrowserAppInstanceTracker : public TabStripModelObserver,
                                   public BrowserTabStripTrackerDelegate,
                                   public aura::WindowObserver,
+                                  public wm::ActivationChangeObserver,
                                   public AppRegistryCache::Observer,
                                   public BrowserListObserver {
  public:
@@ -99,10 +102,13 @@ class BrowserAppInstanceTracker : public TabStripModelObserver,
   void OnWindowVisibilityChanged(aura::Window* window, bool visible) override;
   void OnWindowDestroying(aura::Window* window) override;
 
+  // wm::ActivationChangeObserver overrides:
+  void OnWindowActivated(ActivationReason reason,
+                         aura::Window* gained_active,
+                         aura::Window* lost_active) override;
+
   // BrowserListObserver overrides:
   void OnBrowserAdded(Browser* browser) override;
-  void OnBrowserSetLastActive(Browser* browser) override;
-  void OnBrowserNoLongerActive(Browser* browser) override;
   void OnBrowserRemoved(Browser* browser) override;
 
   // apps::AppRegistryCache::Observer:
@@ -179,6 +185,7 @@ class BrowserAppInstanceTracker : public TabStripModelObserver,
 
   bool IsBrowserTracked(Browser* browser) const;
   bool IsWindowTracked(aura::Window* window) const;
+  bool IsActivationClientTracked(wm::ActivationClient* client) const;
 
   // Removes the instance given a map (app or browser), if it exists, and
   // notifies observers.
@@ -199,6 +206,11 @@ class BrowserAppInstanceTracker : public TabStripModelObserver,
   // A set of observed browser windows.
   base::ScopedMultiSourceObservation<aura::Window, aura::WindowObserver>
       browser_window_observations_{this};
+
+  // A set of observed activation clients for all browser's windows.
+  base::ScopedMultiSourceObservation<wm::ActivationClient,
+                                     wm::ActivationChangeObserver>
+      activation_client_observations_{this};
 
   BrowserTabStripTracker browser_tab_strip_tracker_;
 
