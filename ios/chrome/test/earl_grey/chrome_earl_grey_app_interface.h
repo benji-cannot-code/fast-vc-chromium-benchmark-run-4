dbaa11d6ee1feeb871cc9b4b05a9e968d47f693c
Ewann <ewannpv@google.com>
2021-08-27 12:54:56 +0000

    [iOS] Adds isContextMenuActionsRefreshEnabled to EG2
    This CL adds isContextMenuActionsRefreshEnabled to EG2 in order to test
    the ContextMenuActionsRefresh feature.
    
    Bug: 1241012
    Change-Id: I91deff0178bba1b73ad872ce149ba9a12b32c2bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122596
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915933}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -509,6 +509,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // can, open multiple windows.
 + (BOOL)areMultipleWindowsSupported;
 
+// Returns whether the ContextMenuActionsRefresh feature is enabled.
++ (BOOL)isContextMenuActionsRefreshEnabled;
+
 #pragma mark - Popup Blocking
 
 // Gets the current value of the popup content setting preference for the
--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -1020,6 +1020,10 @@ NSString* SerializedPref(const PrefService::Preference* pref) {
   return base::ios::IsMultipleScenesSupported();
 }
 
++ (BOOL)isContextMenuActionsRefreshEnabled {
+  return IsContextMenuActionsRefreshEnabled();
+}
+
 #pragma mark - ScopedBlockPopupsPref
 
 + (ContentSetting)popupPrefValue {
