be5cd346b587c4ed12b6b2c21f2de79a83f411d4
Justin Novosad <junov@chromium.org>
2021-08-27 15:22:55 +0000

    Remove overdraw detection logic from 2d canvas
    Historical context: Overdraw optimizations were originally
    implemented to improve Chrome's score on legacy benchmarks that were
    used by the press for comparing browser performance.  Overdraw
    optimization refers to the logic that discards all pending draw
    operations whenever a single draw operation occludes the entire canvas.
    Typically, this optimization only affects animations that are not
    synchronized with the display, which was the norm before the
    introduction of the requestAnimationFrame API.
    
    The logic for determining whether a draw operation occludes the canvas
    is cumbersome. Even in the simplest early-exit scenarios it includes
    non-inlinable function calls and conditional branches on every single
    draw call. This resulted in measurable overhead that brought no benefit
    to well designed JS animation code.
    
    This CL proposes a compromise: it still performs overdraw optimization
    when the canvas is explicitly cleared, but it avoids overdraw detection
    logic on every single draw call. This way, most animations that are not
    synchronized with the display (do not use rAF) can still benefit from
    the optimization because they reset the canvas at the start of each
    animation frame. All the while, the runtime of canvas draw methods is
    slightly reduced.  As a result of this change the performance a few old
    or poorly written web pages may suffer, but code that respects best
    practices will benefit.
    
    With this change, the Canvas Lines test of the MotionMark benchmark
    (which is CPU-bound on the renderer main thread) runs about 3% faster.
    
    Bug: 1232600
    Change-Id: I30bbae3e3e579996fa9d1f8796f5b4ebae703664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114397
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915974}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -969,8 +969,7 @@ void CanvasRenderingContext2D::fillFormattedText(
       wrap_width, bounds);
   Draw([recording](cc::PaintCanvas* c, const PaintFlags* flags)  // draw lambda
        { c->drawPicture(recording); },
-       [](const SkIRect& rect) { return false; }, bounds,
-       CanvasRenderingContext2DState::PaintType::kFillPaintType,
+       bounds, CanvasRenderingContext2DState::PaintType::kFillPaintType,
        CanvasRenderingContext2DState::kNoImage,
        CanvasPerformanceMonitor::DrawType::kText);
 }
@@ -1085,8 +1084,6 @@ void CanvasRenderingContext2D::DrawTextInternal(
                                         Font::kUseFallbackIfFontNotReady,
                                         kCDeviceScaleFactor, *flags);
       },
-      [](const SkIRect& rect)  // overdraw test lambda
-      { return false; },
       bounds, paint_type, CanvasRenderingContext2DState::kNoImage,
       CanvasPerformanceMonitor::DrawType::kText);
 }
