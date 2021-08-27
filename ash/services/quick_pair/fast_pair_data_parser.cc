f8675f42c784f6c9276681b325c429980e2edb23
Shane Fitzpatrick <shanefitz@google.com>
2021-08-27 22:48:27 +0000

    [FastPair] Move decryption logic into ash/services/quick_pair
    This removes a circular dependency that can happen when code in
    ash/quick_pair tries to use ash/services/quick_pair.
    
    Fixed: 1242982
    Change-Id: I6afbc5dcb8d76ba81140da041d85d659eeefdea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3124024
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#916145}

--- ash/services/quick_pair/fast_pair_data_parser.cc
+++ ash/services/quick_pair/fast_pair_data_parser.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/quick_pair/common/fast_pair/fast_pair_decoder.h"
 #include "ash/quick_pair/common/logging.h"
-#include "ash/quick_pair/pairing/fast_pair/fast_pair_encryption.h"
+#include "ash/services/quick_pair/fast_pair_decryption.h"
 #include "ash/services/quick_pair/public/mojom/fast_pair_data_parser.mojom.h"
 #include "crypto/openssl_util.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -83,7 +83,7 @@ void FastPairDataParser::ParseDecryptedResponse(
   ConvertVectorsToArrays(aes_key_bytes, encrypted_response_bytes, key, bytes);
 
   std::move(callback).Run(
-      fast_pair_encryption::ParseDecryptedResponse(key, bytes));
+      fast_pair_decryption::ParseDecryptedResponse(key, bytes));
 }
 
 void FastPairDataParser::ParseDecryptedPasskey(
@@ -100,7 +100,7 @@ void FastPairDataParser::ParseDecryptedPasskey(
   ConvertVectorsToArrays(aes_key_bytes, encrypted_passkey_bytes, key, bytes);
 
   std::move(callback).Run(
-      fast_pair_encryption::ParseDecryptedPasskey(key, bytes));
+      fast_pair_decryption::ParseDecryptedPasskey(key, bytes));
 }
 
 }  // namespace quick_pair
