4508bedac9410217b788f258b6d3f37b9c9661c9
Junbo Ke <juke@chromium.org>
2021-08-27 23:57:27 +0000

    [Chromecast] Introduce CastAudioRenderer.
    This CL introduces a new media::AudioRenderer implementation gated by a
    buildflag. CastAudioRenderer replaces the default media::Renderer
    implementation in RendererImpl on Cast when the buildflag is turned on.
    CastAudioRenderer communicates with the audio output service (in browser
    process) via sockets (unix domain socket or TCP). The newly introduced
    IPC path can help decouple the cast service (i.e. CMA and multiroom
    service) from the browser process.
    
    Bug: b:173250111
    Test: fling YT on desktop cast_shell
    Change-Id: Ieebd88b58f0f40b605d099b0627f4d2091491458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3105156
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Doug Steedman <dougsteed@chromium.org>
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916170}

--- chromecast/browser/cast_browser_interface_binders.cc
+++ chromecast/browser/cast_browser_interface_binders.cc
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "chromecast/browser/application_media_capabilities.h"
+#include "chromecast/browser/application_media_info_manager.h"
+#include "chromecast/browser/cast_navigation_ui_data.h"
 #include "chromecast/browser/cast_web_contents.h"
 #include "chromecast/common/mojom/application_media_capabilities.mojom.h"
 #include "components/network_hints/browser/simple_network_hints_handler_impl.h"
 #include "components/network_hints/common/network_hints.mojom.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
+#include "media/mojo/mojom/cast_application_media_info_manager.mojom.h"
 #include "media/mojo/mojom/remoting.mojom.h"
 
 namespace chromecast {
@@ -51,6 +54,21 @@ void BindMediaRemotingRemotee(
     return;
   mojo::GenericPendingReceiver generic_receiver(std::move(receiver));
   cast_web_contents->TryBindReceiver(generic_receiver);
+}
+
+void BindApplicationMediaInfoManager(
+    content::RenderFrameHost* frame_host,
+    mojo::PendingReceiver<::media::mojom::CastApplicationMediaInfoManager>
+        receiver) {
+  auto* web_contents = content::WebContents::FromRenderFrameHost(frame_host);
+  if (!web_contents) {
+    return;
+  }
+  auto application_session_id =
+      CastNavigationUIData::GetSessionIdForWebContents(web_contents);
+  media::CreateApplicationMediaInfoManager(
+      frame_host, std::move(application_session_id),
+      /*mixer_audio_enabled=*/false, std::move(receiver));
 }
 
 // Some Cast internals still dynamically set up interface binders after
@@ -97,6 +115,8 @@ void PopulateCastFrameBinders(
       base::BindRepeating(&BindApplicationMediaCapabilities));
   binder_map->Add<::media::mojom::Remotee>(
       base::BindRepeating(&BindMediaRemotingRemotee));
+  binder_map->Add<::media::mojom::CastApplicationMediaInfoManager>(
+      base::BindRepeating(&BindApplicationMediaInfoManager));
 
   binder_map->SetDefaultBinderDeprecated(
       base::BindRepeating(&HandleGenericReceiver));
