42e049937e76a7b0a6ef377aacbd962934d4fec1
Melissa Zhang <melzhang@google.com>
2021-08-27 05:19:46 +0000

    [App Discovery] Rename AppsFetcherService to AppDiscoveryService.
    Rename AppsFetcherService to AppDiscoveryService and also remove all the
    's's to avoid confusion later on.
    
    Bug: 1223321
    Change-Id: I31a443fbb15afc8c31cbeeb1503c7bdd885bd9f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3120914
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915843}

--- chrome/browser/apps/app_discovery_service/recommended_arc_app_fetcher.cc
+++ chrome/browser/apps/app_discovery_service/recommended_arc_app_fetcher.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/apps/apps_fetcher_service/recommended_arc_apps_fetcher.h"
+#include "chrome/browser/apps/app_discovery_service/recommended_arc_app_fetcher.h"
 
 #include "base/values.h"
 
 namespace apps {
 
-void RecommendedArcAppsFetcher::GetApps(ResultCallback callback) {
+void RecommendedArcAppFetcher::GetApps(ResultCallback callback) {
   // TODO(crbug.com/1223321) : Implement.
   std::move(callback).Run(base::Value());
 }
