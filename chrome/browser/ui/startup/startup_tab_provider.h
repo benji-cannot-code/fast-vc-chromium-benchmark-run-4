6ee4a1fdba18b3cf5133d16ae641d8814ab9c062
Hidehiko Abe <hidehiko@chromium.org>
2021-08-27 19:15:11 +0000

    Support startup_urls in BrowserInitParams.
    So, when lacros is not running, ash-chrome can launch it
    to open a specific URL.
    
    Bug: 1236859
    Test: Ran locally with modifying ash to forcibly set the startup_urls.
    Change-Id: I6fdbaec3a8d06defcbb073cd5d12221f730d6c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3104165
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916062}

--- chrome/browser/ui/startup/startup_tab_provider.h
+++ chrome/browser/ui/startup/startup_tab_provider.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/ui/startup/startup_tab.h"
 #include "url/gurl.h"
 
@@ -74,11 +75,16 @@ class StartupTabProvider {
   virtual StartupTabs GetCommandLineTabs(const base::CommandLine& command_line,
                                          const base::FilePath& cur_dir,
                                          Profile* profile) const = 0;
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  // Returns the URLs given via the crosapi BrowserInitParams with
+  // kOpenWindowWithUrls action.
+  virtual StartupTabs GetCrosapiTabs() const = 0;
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
 #if !defined(OS_ANDROID)
   // Returns tabs related to the What's New UI (if applicable).
   virtual StartupTabs GetNewFeaturesTabs(bool whats_new_enabled) const = 0;
-#endif
+#endif  // !defined(OS_ANDROID)
 };
 
 class StartupTabProviderImpl : public StartupTabProvider {
@@ -194,9 +200,13 @@ class StartupTabProviderImpl : public StartupTabProvider {
                                  const base::FilePath& cur_dir,
                                  Profile* profile) const override;
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  StartupTabs GetCrosapiTabs() const override;
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
 #if !defined(OS_ANDROID)
   StartupTabs GetNewFeaturesTabs(bool whats_new_enabled) const override;
-#endif
+#endif  // !defined(OS_ANDROID)
 };
 
 #endif  // CHROME_BROWSER_UI_STARTUP_STARTUP_TAB_PROVIDER_H_
