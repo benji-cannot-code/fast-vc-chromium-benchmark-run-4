f1ec6846a53f475fe7f1fa31f5816d61473d3e75
kouhei <kouhei@chromium.org>
2021-08-27 16:22:37 +0000

    ScriptRunner: Remove unused code
    This CL removes unused methods, and inlines
    `ScriptRunner::ScheduleReadyInOrderScripts()`, since the member func is
    now only called from NotifyScriptReady.
    
    Bug: 1239245
    Change-Id: I944b6b8c7a85a4ab8a28fedf3575e41b3bcd7322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3124720
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Auto-Submit: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#915995}

--- third_party/blink/renderer/core/script/script_runner.h
+++ third_party/blink/renderer/core/script/script_runner.h
@@ -52,10 +52,6 @@ class CORE_EXPORT ScriptRunner final
   ScriptRunner& operator=(const ScriptRunner&) = delete;
 
   void QueueScriptForExecution(PendingScript*);
-  bool HasPendingScripts() const {
-    return !pending_in_order_scripts_.IsEmpty() ||
-           !pending_async_scripts_.IsEmpty();
-  }
   void NotifyScriptReady(PendingScript*);
 
   static void MovePendingScript(Document&, Document&, ScriptLoader*);
@@ -68,11 +64,8 @@ class CORE_EXPORT ScriptRunner final
   const char* NameInHeapSnapshot() const override { return "ScriptRunner"; }
 
  private:
-  class Task;
-
   void MovePendingScript(ScriptRunner*, PendingScript*);
   bool RemovePendingInOrderScript(PendingScript*);
-  void ScheduleReadyInOrderScripts();
 
   // Execute the given pending script.
   void ExecutePendingScript(PendingScript*);
