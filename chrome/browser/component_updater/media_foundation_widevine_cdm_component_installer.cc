bf63cb5b2ae0b0c9c2f2f3645b008f6564a4668b
Xiaohan Wang <xhwang@chromium.org>
2021-08-28 04:22:46 +0000

    media: Only register MediaFoundationWidevineCdm when supported
    On older systems where MediaFoundationCdm is not supported (e.g. on
    older versions of Windows), we should not even register the
    MediaFoundationWidevineCdm since we are sure it cannot be used.
    
    Bug: 1225681
    Change-Id: I7fb7bc6fc848d096b1e72b085b541ee037367d17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116868
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916212}

--- chrome/browser/component_updater/media_foundation_widevine_cdm_component_installer.cc
+++ chrome/browser/component_updater/media_foundation_widevine_cdm_component_installer.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/cdm_registry.h"
 #include "content/public/common/cdm_info.h"
 #include "media/base/media_switches.h"
+#include "media/cdm/win/media_foundation_cdm.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/widevine/cdm/widevine_cdm_common.h"
 
@@ -146,7 +147,8 @@ MediaFoundationWidevineCdmComponentInstallerPolicy::GetInstallerAttributes()
 
 void RegisterMediaFoundationWidevineCdmComponent(
     component_updater::ComponentUpdateService* cus) {
-  if (base::FeatureList::IsEnabled(media::kHardwareSecureDecryption)) {
+  if (base::FeatureList::IsEnabled(media::kHardwareSecureDecryption) &&
+      media::MediaFoundationCdm::IsAvailable()) {
     VLOG(1) << "Registering Media Foundation Widevine CDM component.";
     auto installer = base::MakeRefCounted<ComponentInstaller>(
         std::make_unique<MediaFoundationWidevineCdmComponentInstallerPolicy>());
