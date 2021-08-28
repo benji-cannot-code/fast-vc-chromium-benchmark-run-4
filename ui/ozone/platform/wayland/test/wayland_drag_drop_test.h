c7cf9442e81f218bbbdee6302cc4ffedeb3a6cba
Nick Diego Yamane <nickdiego@igalia.com>
2021-08-28 01:11:26 +0000

    ozone/wayland: Fix serial handling when starting drag
    So far, drag controllers have been using standard WaylandConnection's
    serial() getter to retrieve the serial for wl_data_device.start_drag
    request, which may wrongly use serial values other than the ones
    corresponding to mouse and touch press events, e.g: key press in cases
    as reported in crbug.com/1175083. This CL fixes it by switching to
    SerialTracker, explicitly selecting a (kTouchPress | kMousePress)
    serial, as well as adds a regression test to prevent similar regressions
    from being re-introduced.
    
    R=msisov@igalia.com, tonikitoo@igalia.com
    
    Bug: 1211874, 1175083
    Test: Covered by ozone_unittests.
    Change-Id: Ia990bae2ebed6f240c44102931332156db8151f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122116
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#916188}

--- ui/ozone/platform/wayland/test/wayland_drag_drop_test.h
+++ ui/ozone/platform/wayland/test/wayland_drag_drop_test.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_OZONE_PLATFORM_WAYLAND_TEST_WAYLAND_DRAG_DROP_TEST_H_
 #define UI_OZONE_PLATFORM_WAYLAND_TEST_WAYLAND_DRAG_DROP_TEST_H_
 
+#include <cstdint>
+
 #include "base/callback_forward.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/ozone/platform/wayland/test/test_data_device.h"
 #include "ui/ozone/platform/wayland/test/test_data_source.h"
 #include "ui/ozone/platform/wayland/test/wayland_test.h"
@@ -33,6 +36,7 @@ class WaylandDragDropTest : public WaylandTest,
   WaylandDragDropTest();
   WaylandDragDropTest(const WaylandDragDropTest&) = delete;
   WaylandDragDropTest& operator=(const WaylandDragDropTest&) = delete;
+  ~WaylandDragDropTest() override;
 
   // These are public for convenience, as they must be callable from lambda
   // functions, usually posted to task queue while the drag loop runs.
@@ -86,6 +90,9 @@ class WaylandDragDropTest : public WaylandTest,
   wl::TestDataSource* data_source_;
   wl::MockPointer* pointer_;
   wl::TestTouch* touch_;
+
+  // Latest serial generated for pointer/touch press events.
+  absl::optional<uint32_t> latest_pointer_press_serial_;
 };
 
 }  // namespace ui
