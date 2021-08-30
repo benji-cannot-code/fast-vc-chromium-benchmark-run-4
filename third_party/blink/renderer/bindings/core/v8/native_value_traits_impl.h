76c8a69cdad34759d8e6a47193c268295a9da1ab
Yuki Shiino <yukishiino@chromium.org>
2021-08-30 11:53:58 +0000

    binding: Remove unused NVT::NativeValue(ScriptIterator)
    Removes unused NativeValueTraits<T>::NativeValue(ScriptIterator).
    
    Change-Id: I314463d97e06e3f6a71b06b4952f1f27700e02cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3129200
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916378}

--- third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h
+++ third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h
@@ -822,13 +822,6 @@ struct NativeValueTraits<IDLSequence<T>>
   static ImplType NativeValue(v8::Isolate* isolate,
                               v8::Local<v8::Value> value,
                               ExceptionState& exception_state);
-
-  // https://heycam.github.io/webidl/#es-sequence
-  // This is a special case, used when converting an IDL union that contains a
-  // sequence or frozen array type.
-  static ImplType NativeValue(v8::Isolate* isolate,
-                              ScriptIterator script_iterator,
-                              ExceptionState& exception_state);
 };
 
 namespace bindings {
@@ -963,17 +956,6 @@ NativeValueTraits<IDLSequence<T>>::NativeValue(
         "The object must have a callable @@iterator property.");
     return ImplType();
   }
-  return bindings::CreateIDLSequenceFromIterator<T>(
-      isolate, std::move(script_iterator), exception_state);
-}
-
-template <typename T>
-typename NativeValueTraits<IDLSequence<T>>::ImplType
-NativeValueTraits<IDLSequence<T>>::NativeValue(
-    v8::Isolate* isolate,
-    ScriptIterator script_iterator,
-    ExceptionState& exception_state) {
-  DCHECK(!script_iterator.IsNull());
   return bindings::CreateIDLSequenceFromIterator<T>(
       isolate, std::move(script_iterator), exception_state);
 }
