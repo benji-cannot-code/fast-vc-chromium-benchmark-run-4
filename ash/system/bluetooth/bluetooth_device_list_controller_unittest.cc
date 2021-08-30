bbb1f5f6fe0ff9d744620b4ea314adf5cbcf7c75
Chad Duffin <chadduffin@chromium.org>
2021-08-30 18:44:43 +0000

    [CrOS Bluetooth] Add Bluetooth detailed view classes.
    This change introduces the classes used to represent and managed the
    Bluetooth detailed view page. The classes are mostly skeletons with some
    functionality to show their relationship with each other class. These
    classes combined handle the enabled, disabled, device list, and device
    list item views.
    
    Example screenshot at https://screenshot.googleplex.com/9SWwvjLxDNgjR7K.
    
    Bug: 1010321
    Test: Built unit tests and chrome, ran locally.
    
    Change-Id: I90f70d908fde9194abd4654bf59c7097dc406ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098651
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916481}

---
+++ ash/system/bluetooth/bluetooth_device_list_controller_unittest.cc
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/bluetooth/bluetooth_device_list_controller.h"
+
+#include "ash/constants/ash_features.h"
+#include "ash/test/ash_test_base.h"
+#include "base/test/scoped_feature_list.h"
+
+namespace ash {
+
+class BluetoothDeviceListControllerTest : public AshTestBase {
+ public:
+  void SetUp() override {
+    AshTestBase::SetUp();
+
+    feature_list_.InitAndEnableFeature(features::kBluetoothRevamp);
+  }
+
+  void TearDown() override { AshTestBase::TearDown(); }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+TEST_F(BluetoothDeviceListControllerTest, CanConstruct) {
+  BluetoothDeviceListController bluetooth_device_list_controller(
+      /*delegate=*/nullptr);
+}
+
+}  // namespace ash
---
+++ ash/system/bluetooth/bluetooth_disabled_detailed_view_unittest.cc
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/bluetooth/bluetooth_disabled_detailed_view.h"
+
+#include "ash/constants/ash_features.h"
+#include "ash/test/ash_test_base.h"
+#include "base/test/scoped_feature_list.h"
+
+namespace ash {
+
+class BluetoothDisabledDetailedViewTest : public AshTestBase {
+ public:
+  void SetUp() override {
+    AshTestBase::SetUp();
+
+    feature_list_.InitAndEnableFeature(features::kBluetoothRevamp);
+  }
+
+  void TearDown() override { AshTestBase::TearDown(); }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+TEST_F(BluetoothDisabledDetailedViewTest, CanConstruct) {
+  BluetoothDisabledDetailedView bluetooth_disabled_detailed_view;
+  bluetooth_disabled_detailed_view.SetVisible(true);
+}
+
+}  // namespace ash
