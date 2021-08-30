8fb2452b43bf368f3f3e2a0aa5899fb489ffaa7a
Yue Li <updowndota@google.com>
2021-08-30 18:38:17 +0000

    Quick Answers V2: Fix settings logic
    The eligibility check in QuickAnswersState should reflect feature availability.
    Moving settings related logics out of it.
    
    Bug: b/197891222
    Test: Manual Test
    Change-Id: Icf0fb4dd9361c215fea7a78b55badfe1005ef7e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123440
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916476}

--- ash/quick_answers/quick_answers_controller_impl.cc
+++ ash/quick_answers/quick_answers_controller_impl.cc
@@ -58,6 +58,18 @@ bool IsProcessedRequest(const QuickAnswersRequest& request) {
           chromeos::quick_answers::IntentType::kUnknown);
 }
 
+bool ShouldShowQuickAnswers() {
+  if (!ash::QuickAnswersState::Get()->is_eligible())
+    return false;
+
+  bool should_show_consent =
+      ash::QuickAnswersState::Get()->consent_status() ==
+      chromeos::quick_answers::prefs::ConsentStatus::kUnknown;
+  bool settings_enabled = ash::QuickAnswersState::Get()->settings_enabled();
+
+  return should_show_consent || settings_enabled;
+}
+
 }  // namespace
 
 namespace ash {
@@ -80,9 +92,8 @@ void QuickAnswersControllerImpl::MaybeShowQuickAnswers(
     const gfx::Rect& anchor_bounds,
     const std::string& title,
     const Context& context) {
-  if (!QuickAnswersState::Get()->is_eligible()) {
+  if (!ShouldShowQuickAnswers())
     return;
-  }
 
   if (visibility_ == QuickAnswersVisibility::kClosed)
     return;
