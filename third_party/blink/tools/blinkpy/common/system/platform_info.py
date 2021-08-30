70e9ad7ec9adb475af7160bec1ef10aa3244caed
Preethi Mohan <preethim@google.com>
2021-08-30 21:29:03 +0000

    PY3 Migration(blinkpy unit tests)- migrate blinkpy.common tests
    fixed many failures. 8 failures remaining.
    
    vpython3 ./run_blinkpy_tests.py blinkpy.common
    
    Bug: 1218641
    Change-Id: I50e224d77c26c5651308a971071bf9a097ea2fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123144
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/main@{#916554}

--- third_party/blink/tools/blinkpy/common/system/platform_info.py
+++ third_party/blink/tools/blinkpy/common/system/platform_info.py
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import re
 import sys
+from six.moves import map
 
 
 class PlatformInfo(object):
@@ -110,7 +111,7 @@ class PlatformInfo(object):
 
     def total_bytes_memory(self):
         if self.is_mac():
-            return long(
+            return int(
                 self._executive.run_command(['sysctl', '-n', 'hw.memsize']))
         return None
 
