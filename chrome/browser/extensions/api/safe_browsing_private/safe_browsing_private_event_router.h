d0475d383f0ee1d9c75e4f0232189cb636d76aaf
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2021-08-30 21:01:33 +0000

    Add support for the Password Leak events in SB private event router
    Bug: 1238577
    Change-Id: Ib008e79a66c004770b1d9d5b91c2b45ac8ff08a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3125954
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916511}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
@@ -89,6 +89,9 @@ class SafeBrowsingPrivateEventRouter
   static const char kKeyScanId[];
   static const char kKeyIsFederated[];
   static const char kKeyFederatedOrigin[];
+  static const char kKeyPasswordBreachIdentities[];
+  static const char kKeyPasswordBreachIdentitiesUrl[];
+  static const char kKeyPasswordBreachIdentitiesUsername[];
 
   static const char kKeyPasswordReuseEvent[];
   static const char kKeyPasswordChangedEvent[];
@@ -97,7 +100,8 @@ class SafeBrowsingPrivateEventRouter
   static const char kKeySensitiveDataEvent[];
   static const char kKeyUnscannedFileEvent[];
   static const char kKeyLoginEvent[];
-  static const char* kAllEvents[7];
+  static const char kKeyPasswordBreachEvent[];
+  static const char* kAllEvents[8];
 
   static const char kKeyUnscannedReason[];
 
@@ -222,6 +226,10 @@ class SafeBrowsingPrivateEventRouter
   void OnLoginEvent(const GURL& url,
                     bool is_federated,
                     const GURL& federated_origin);
+
+  void OnPasswordBreach(
+      const std::string& trigger,
+      const std::vector<std::pair<GURL, std::string>>& identities);
 
   // Returns true if enterprise real-time reporting should be initialized,
   // checking both the feature flag. This function is public so that it can
--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.h
@@ -140,6 +140,12 @@ class EventReportValidator {
                         const std::string& expected_federated_origin,
                         const std::string& expected_username);
 
+  void ExpectPasswordBreachEvent(
+      const std::string& expected_trigger,
+      const std::vector<std::pair<std::string, std::string>>&
+          expected_identities,
+      const std::string& expected_username);
+
   void ExpectNoReport();
 
   // Closure to run once all expected events are validated.
@@ -147,6 +153,7 @@ class EventReportValidator {
 
  private:
   void ValidateReport(base::Value* report);
+  void ValidateIdentities(base::Value* value);
   void ValidateMimeType(base::Value* value);
   void ValidateDlpVerdict(
       base::Value* value,
@@ -168,7 +175,7 @@ class EventReportValidator {
   policy::MockCloudPolicyClient* client_;
 
   std::string event_key_;
-  std::string url_;
+  absl::optional<std::string> url_;
   absl::optional<std::string> trigger_ = absl::nullopt;
   absl::optional<std::string> threat_type_ = absl::nullopt;
   absl::optional<std::string> unscanned_reason_ = absl::nullopt;
@@ -177,6 +184,8 @@ class EventReportValidator {
   std::string username_;
   absl::optional<bool> is_federated_ = absl::nullopt;
   absl::optional<std::string> federated_origin_ = absl::nullopt;
+  absl::optional<std::vector<std::pair<std::string, std::string>>>
+      password_breach_identities_ = absl::nullopt;
 
   // When multiple files generate events, we don't necessarily know in which
   // order they will be reported. As such, we use maps to ensure all of them
