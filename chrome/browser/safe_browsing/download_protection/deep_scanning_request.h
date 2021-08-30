181eb7e1a6f7a5092bbf1cea158917371262e879
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-08-30 17:59:11 +0000

    Delay event reports until DeepScanningRequest::FinishRequest is called
    This addresses an issue in crrev.com/c/3107892, which is that by
    reporting immediately as the scanning verdict is obtained, the actual
    Chrome UX the download will have as captured by event_result will be
    inaccurate since a subsequent verdict may make it change. This is
    fixed by simply storing these report calls in a list that eventually
    reports everything once every scanning verdict is known.
    
    This really only affects save package scans which are still WIP since
    single-file scans will call FinishRequest shortly after getting a scan
    response.
    
    Bug: 1230577
    Change-Id: I95d8cfd3822be4b1b60ce87057a911b879342e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3129920
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916464}

--- chrome/browser/safe_browsing/download_protection/deep_scanning_request.h
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_request.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback_list.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 #include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
+#include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/file_analysis_request.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/file_opening_job.h"
 #include "chrome/browser/safe_browsing/download_protection/download_protection_util.h"
@@ -211,6 +213,10 @@ class DeepScanningRequest : public download::DownloadItem::Observer {
   // Cached danger type for the download to be used by reporting in case
   // scanning is skipped for any reason.
   download::DownloadDangerType pre_scan_danger_type_;
+
+  // Cached callbacks to report scanning results until the final `event_result_`
+  // is known. The callbacks in this list should be called in FinishRequest.
+  base::OnceCallbackList<void(EventResult result)> report_callbacks_;
 
   base::WeakPtrFactory<DeepScanningRequest> weak_ptr_factory_;
 };
