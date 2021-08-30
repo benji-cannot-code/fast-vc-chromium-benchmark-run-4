9a7c63219e182f166a5090efb9ef1f23de779ed9
Hirokazu Honda <hiroh@chromium.org>
2021-08-30 21:18:17 +0000

    media/gpu/vaapi/H264VaapiVED: Support temporal layer encoding
    This CL adds H264 temporal layer encoding support in
    H264VaapiVideoEncoderDelegate. It enables for
    VaapiVideoEncodeAccelerator to produce H264 temporal layer stream.
    However, intel VA-API drivers don't support temporal layer bitrate
    control, so the bitrate specified by a client is split to the
    temporal layers based on the number of frames in each layer.
    For instance, the bitrate allocation is [25%, 25%, 50%] in the case
    of three temporal layers. We require a software bitrate controller
    to achieve per temporal layer bitrate control. (b/195407733)
    
    Bug: b:194265793
    Test: video.EncodeAccel.h264*temporal* on atlas and volteer
    Change-Id: I8e2bc3dde9f810ceb384fa3801b6b060ddd60f28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3061966
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Zhaoliang Ma <zhaoliang.ma@intel.com>
    Cr-Commit-Position: refs/heads/main@{#916535}

--- media/gpu/h264_dpb.h
+++ media/gpu/h264_dpb.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/codec_picture.h"
 #include "media/gpu/media_gpu_export.h"
 #include "media/video/h264_parser.h"
+#include "media/video/video_encode_accelerator.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace media {
@@ -91,6 +92,8 @@ class MEDIA_GPU_EXPORT H264Picture : public CodecPicture {
 
   // Position in DPB (i.e. index in DPB).
   int dpb_position;
+
+  absl::optional<H264Metadata> metadata_for_encoding;
 
  protected:
   ~H264Picture() override;
