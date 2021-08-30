6f77fea4c6de88fd3c45e54a804664d785a5996d
Pi-Hsun Shih <pihsun@chromium.org>
2021-08-30 07:15:37 +0000

    CCA: Add search parameter to control default mode
    Add a search parameter for CCA URL: ?mode={video,photo,square} to
    control what the default mode to use, and fallback to photo mode if the
    search parameter is not specified.
    
    Bug: b:189723935
    Test: chrome://camera-app/views/main.html?mode=video in Chrome
    Test: tast run <ip> camera.CCAUISmoke* camera.CCAUIStress* camera.CCAUIIntent
    Change-Id: I1e5a6d1ecb5d448635c73a7e8da0ae1a69ba1d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3121877
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916331}

--- chromeos/components/camera_app_ui/resources/js/intent.js
+++ chromeos/components/camera_app_ui/resources/js/intent.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import * as metrics from './metrics.js';
 import {ChromeHelper} from './mojo/chrome_helper.js';
+// eslint-disable-next-line no-unused-vars
 import {Mode} from './type.js';
 
 /**
@@ -54,8 +55,9 @@ export class Intent {
     /**
      * Capture mode of intent.
      * @const {!Mode}
+     * @private
      */
-    this.mode = mode;
+    this.mode_ = mode;
 
     /**
      * Whether the intent should return with the captured result.
@@ -142,7 +144,7 @@ export class Intent {
    */
   logResult(result) {
     metrics.sendIntentEvent({
-      mode: this.mode,
+      mode: this.mode_,
       result,
       shouldHandleResult: this.shouldHandleResult,
       shouldDownScale: this.shouldDownScale,
@@ -152,24 +154,19 @@ export class Intent {
 
   /**
    * @param {!URL} url Url passed along with app launch event.
+   * @param {!Mode} mode Mode for the intent
    * @return {!Intent} Created intent object. Returns null if input is not a
    *     valid intent url.
    * @throws {!ParseError}
    */
-  static create(url) {
+  static create(url, mode) {
     const params = url.searchParams;
     const getBool = (key) => params.get(key) === '1';
-    let param = params.get('intentId');
+    const param = params.get('intentId');
     if (param === null) {
       throw new ParseError(url);
     }
     const intentId = parseInt(param, 10);
-
-    param = params.get('mode');
-    if (param === null || !Object.values(Mode).includes(param)) {
-      throw new ParseError(url);
-    }
-    const mode = /** @type {!Mode} */ (param);
 
     return new Intent(
         url, intentId, mode, getBool('shouldHandleResult'),
