67b653b0ff8d76ae9c62f7f92a56787b54fafa52
Kevin Marshall <kmarshall@chromium.org>
2021-08-30 21:40:59 +0000

    [fuchsia] Enable the use of PartitionAlloc for general purpose heap allocation.
    Allows PartitionAlloc to be optionally used as the general purpose heap
    allocator for Fuchsia builds.
    
    To enable PartitionAlloc, simply add the following GN args:
      use_allocator = "partition"
      use_allocator_shim = true
    
    
    Bug: 1178937
    Change-Id: I76f325ec77b2605ffd2fa22882cc9bf801379334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697971
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916560}

--- base/allocator/allocator_shim_internals.h
+++ base/allocator/allocator_shim_internals.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(__GNUC__)
 
+#if defined(OS_POSIX)
 #include <sys/cdefs.h>  // for __THROW
+#endif
 
 #ifndef __THROW  // Not a glibc system
 #ifdef _NOEXCEPT  // LLVM libc++ uses noexcept instead
