35364ff0a8192eeb6e40a09747e6846e76f71be6
Matt Jones <mdjones@chromium.org>
2021-08-30 13:22:11 +0000

    Deflake testCloseTabWithManyTabs
    This patch adds an @Before to TabStripTest to reset whether tabs
    should cascade (true by default). This appears to have fixed the
    flake locally.
    
    Bug: 1244161
    Change-Id: Id12bbaddad45cc80c020059cb41c6503e477b09c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3125798
    Commit-Queue: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916392}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
@@ -12,6 +12,7 @@ import androidx.test.filters.LargeTest;
 
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -58,6 +59,11 @@ public class TabStripTest {
     @Rule
     public BlankCTATabInitialStateRule mBlankCTATabInitialStateRule =
             new BlankCTATabInitialStateRule(sActivityTestRule, false);
+
+    @Before
+    public void setUp() throws ExecutionException {
+        setShouldCascadeTabsAndCheckTabStrips(true);
+    }
 
     /**
      * Tests that the initial state of the system is good.  This is so the default TabStrips match
