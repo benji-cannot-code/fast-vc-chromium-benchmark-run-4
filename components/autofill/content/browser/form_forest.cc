8f7156330aa1b153095420c76825b842f12816d7
Christoph Schwering <schwering@google.com>
2021-08-30 10:27:18 +0000

    [Autofill] Fixed browser crash in UnsetKeyPressListener().
    This CL fixes that due to race conditions with renderer events, the
    Autofill popup may be still open at the time RenderFrameDeleted() and/or
    DidFinishNavigation() in ContentAutofillDriverFactory are called.
    
    It also removes old comments referring to crbug/1190640, which has been
    fixed now.
    
    Bug: 1187842, 1218890, 1240246
    Change-Id: I61fcb711633bd130f2d064609d0d579b76cc022f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3126560
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916364}

--- components/autofill/content/browser/form_forest.cc
+++ components/autofill/content/browser/form_forest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define AFCHECK(condition, ...)           \
   {                                       \
     if (!(condition)) {                   \
-      DCHECK(false);                      \
       base::debug::DumpWithoutCrashing(); \
       __VA_ARGS__;                        \
     }                                     \
