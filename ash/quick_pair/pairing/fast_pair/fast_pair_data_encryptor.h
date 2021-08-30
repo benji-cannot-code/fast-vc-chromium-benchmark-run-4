37fe1eace8efe9797cf8932d2b041ef8b8b1f25d
Juliet Levesque <julietlevesque@google.com>
2021-08-30 22:37:57 +0000

    [Fast Pair] Add decrypt functions to FastPairDataEncryptor.
    Change-Id: I727b273d4020644b2b66143c88258b7fba2418e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114306
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#916579}

--- ash/quick_pair/pairing/fast_pair/fast_pair_data_encryptor.h
+++ ash/quick_pair/pairing/fast_pair/fast_pair_data_encryptor.h
@@ -9,6 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "ash/services/quick_pair/public/cpp/decrypted_passkey.h"
+#include "ash/services/quick_pair/public/cpp/decrypted_response.h"
+#include "base/callback.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
 #include <array>
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -31,6 +36,18 @@ class FastPairDataEncryptor {
       const std::array<uint8_t, kBlockSizeBytes>& bytes_to_encrypt) = 0;
 
   virtual const absl::optional<std::array<uint8_t, 64>>& GetPublicKey() = 0;
+
+  // Decrypt and parse decrypted response bytes with the stored secret key.
+  virtual void ParseDecryptedResponse(
+      const std::vector<uint8_t>& encrypted_response_bytes,
+      base::OnceCallback<void(const absl::optional<DecryptedResponse>&)>
+          callback) = 0;
+
+  // Decrypt and parse decrypted passkey bytes with the stored secret key.
+  virtual void ParseDecryptedPasskey(
+      const std::vector<uint8_t>& encrypted_passkey_bytes,
+      base::OnceCallback<void(const absl::optional<DecryptedPasskey>&)>
+          callback) = 0;
 
   virtual ~FastPairDataEncryptor() = default;
 };
