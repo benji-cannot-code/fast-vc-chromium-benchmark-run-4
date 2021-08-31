77a070df361c8809da9b108994598a93a2b4952b
Ehimare Okoyomon <eokoyomon@chromium.org>
2021-08-31 22:13:56 +0000

    [Android] Fix openUrl in PageInfoHistory
    Pass in Tab supplier to pageinfohistory in order to be able to open urls
    from page info history.
    
    Bug: 1243629, 1173154
    Change-Id: Id9326d86c1e70149881720f66c19c41c58e1b84e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122600
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916993}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/ChromePageInfoControllerDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/ChromePageInfoControllerDelegate.java
@@ -220,7 +220,8 @@ public class ChromePageInfoControllerDelegate extends PageInfoControllerDelegate
     @Override
     public PageInfoSubpageController createHistoryController(
             PageInfoMainController mainController, PageInfoRowView rowView) {
-        return new PageInfoHistoryController(mainController, rowView, this);
+        final Tab tab = TabUtils.fromWebContents(mWebContents);
+        return new PageInfoHistoryController(mainController, rowView, this, () -> { return tab; });
     }
 
     /**
