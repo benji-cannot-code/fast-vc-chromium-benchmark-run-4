50db09838dd4fb3aee51f02215e64dd0e06ad6fa
Harry Cutts <hcutts@chromium.org>
2021-08-31 23:38:01 +0000

    Diagnostics: add switch for number pad presence
    This will be set by cros_config on devices that have a built-in number
    pad, for use in the keyboard tester.
    
    Bug: 1207678
    Test: add --has-number-pad to /etc/chrome_dev.conf, then check the value
    Test: of the hasNumberPad field for the internal keyboard.
    Change-Id: I6a3d278db1eb9bc11ff3e6d0bb5f0d348f221ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123730
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Harry Cutts <hcutts@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917034}

--- ash/webui/diagnostics_ui/resources/fake_data.js
+++ ash/webui/diagnostics_ui/resources/fake_data.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {BatteryChargeStatus, BatteryHealth, BatteryInfo, BatteryState, ConnectionType, CpuUsage, ExternalPowerSource, KeyboardInfo, MechanicalLayout, MemoryUsage, Network, NetworkGuidInfo, NetworkState, NetworkType, PhysicalLayout, PowerRoutineResult, RoutineType, SecurityType, StandardRoutineResult, SystemInfo, TouchDeviceInfo, TouchDeviceType, WiFiStateProperties} from './diagnostics_types.js'
+import {BatteryChargeStatus, BatteryHealth, BatteryInfo, BatteryState, ConnectionType, CpuUsage, ExternalPowerSource, KeyboardInfo, MechanicalLayout, MemoryUsage, Network, NetworkGuidInfo, NetworkState, NetworkType, NumberPadPresence, PhysicalLayout, PowerRoutineResult, RoutineType, SecurityType, StandardRoutineResult, SystemInfo, TouchDeviceInfo, TouchDeviceType, WiFiStateProperties} from './diagnostics_types.js'
 import {stringToMojoString16} from './mojo_utils.js';
 
 /** @type {!Array<!BatteryChargeStatus>} */
@@ -457,6 +457,7 @@ export let fakeKeyboards = [
     physicalLayout: PhysicalLayout.kChromeOS,
     mechanicalLayout: MechanicalLayout.kAnsi,
     hasAssistantKey: true,
+    numberPadPresent: NumberPadPresence.kPresent,
   },
 ];
 
--- chrome/test/data/webui/chromeos/diagnostics/input_list_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/input_list_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://diagnostics/input_list.js';
 
-import {ConnectionType, KeyboardInfo, MechanicalLayout, PhysicalLayout, TouchDeviceInfo, TouchDeviceType} from 'chrome://diagnostics/diagnostics_types.js';
+import {ConnectionType, KeyboardInfo, MechanicalLayout, NumberPadPresence, PhysicalLayout, TouchDeviceInfo, TouchDeviceType} from 'chrome://diagnostics/diagnostics_types.js';
 import {fakeKeyboards, fakeTouchDevices} from 'chrome://diagnostics/fake_data.js';
 import {FakeInputDataProvider} from 'chrome://diagnostics/fake_input_data_provider.js';
 import {setInputDataProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
@@ -70,6 +70,7 @@ export function inputListTestSuite() {
       physicalLayout: PhysicalLayout.kUnknown,
       mechanicalLayout: MechanicalLayout.kUnknown,
       hasAssistantKey: false,
+      numberPadPresent: NumberPadPresence.kUnknown,
     };
     return initializeInputList()
         .then(() => {
