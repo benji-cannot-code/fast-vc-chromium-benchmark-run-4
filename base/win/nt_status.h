858ea2d7a6c1cd882ad6a3864c7409d2ca525231
Ken Rockot <rockot@google.com>
2021-08-31 22:33:43 +0000

    Reland "Mojo: More CHECKs for invalid handles"
    This is a reland of edc95d055a166ae2cb6d24c16f85763269f2ce4b
    with additional safeguards to avoid CHECKing on some
    unavoidable DuplicateHandle failures, namely when the remote
    process has already begun termination.
    
    Original change's description:
    > Mojo: More CHECKs for invalid handles
    >
    > Bug: 1229671,1239934
    > Change-Id: I2e6a1cf3444e2217fbab4c7c8f636d2824bf66b6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3098360
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#913489}
    
    Bug: 1229671,1239934
    Change-Id: Iec2a4aefb7df69cea0aa4290c4fc09839ee5c17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131381
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917000}

---
+++ base/win/nt_status.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_WIN_NT_STATUS_H_
+#define BASE_WIN_NT_STATUS_H_
+
+#include <windows.h>
+#include <winternl.h>
+
+#include "base/base_export.h"
+
+namespace base {
+namespace win {
+
+// Returns the value of the most recent thread-local NTSTATUS value, i.e.
+// LastStatusValue from the Thread Environment Block (TEB). This may be used,
+// for example, to deduce more information about the outcome of an API call
+// where the meaning of GetLastError() is ambiguous.
+BASE_EXPORT NTSTATUS GetLastNtStatus();
+
+}  // namespace win
+}  // namespace base
+
+#endif  // BASE_WIN_NT_STATUS_H_
--- mojo/public/cpp/bindings/lib/buffer.h
+++ mojo/public/cpp/bindings/lib/buffer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/component_export.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/system/handle.h"
@@ -94,7 +95,7 @@ class COMPONENT_EXPORT(MOJO_CPP_BINDINGS_BASE) Buffer {
 
   // Serializes |handles| into the buffer object. Only valid to call when this
   // Buffer is backed by a message object.
-  void AttachHandles(std::vector<ScopedHandle>* handles);
+  bool AttachHandles(std::vector<ScopedHandle>* handles) WARN_UNUSED_RESULT;
 
   // Seals this Buffer so it can no longer be used for allocation, and ensures
   // the backing message object has a complete accounting of the size of the
