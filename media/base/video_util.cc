12e3329700901824fbac3620510c5a983571945e
Ren-Pei Zeng <kamesan@chromium.org>
2021-08-31 08:50:38 +0000

    chromeos_camera/VaapiMJDA: Enable cropping and downscaling decoded frame
    Enable VaapiMjpegDecodeAccelerator to crop/downscale the decoded frame
    by VPP or by libyuv when hardware is not capable. This is useful for
    camera pipeline to support outputting resolutions of different aspect
    ratio from the sensor.
    
    This functionality for V4L2MJDA will be implemented in a follow-up CL.
    
    Bug: b:182092931
    Test: jpeg_decode_accelerator_unittest, media_unittests
    Change-Id: Id418de1424403bfe408e989054448b219ef7de4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2823675
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Ren-Pei Zeng <kamesan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916729}

--- media/base/video_util.cc
+++ media/base/video_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 
 #include "base/bind.h"
+#include "base/bits.h"
 #include "base/callback_helpers.h"
 #include "base/check_op.h"
 #include "base/logging.h"
@@ -555,6 +556,28 @@ gfx::Size ScaleSizeToFitWithinTarget(const gfx::Size& size,
 gfx::Size ScaleSizeToEncompassTarget(const gfx::Size& size,
                                      const gfx::Size& target) {
   return ScaleSizeToTarget(size, target, false);
+}
+
+gfx::Rect CropSizeForScalingToTarget(const gfx::Size& size,
+                                     const gfx::Size& target,
+                                     size_t alignment) {
+  DCHECK_GT(alignment, 0u);
+  if (size.IsEmpty() || target.IsEmpty())
+    return gfx::Rect();
+
+  gfx::Rect crop(ScaleSizeToFitWithinTarget(target, size));
+  crop.set_width(base::checked_cast<int>(base::bits::AlignDown(
+      base::checked_cast<size_t>(crop.width()), alignment)));
+  crop.set_height(base::checked_cast<int>(base::bits::AlignDown(
+      base::checked_cast<size_t>(crop.height()), alignment)));
+  crop.set_x(base::checked_cast<int>(base::bits::AlignDown(
+      base::checked_cast<size_t>((size.width() - crop.width()) / 2),
+      alignment)));
+  crop.set_y(base::checked_cast<int>(base::bits::AlignDown(
+      base::checked_cast<size_t>((size.height() - crop.height()) / 2),
+      alignment)));
+  DCHECK(gfx::Rect(size).Contains(crop));
+  return crop;
 }
 
 gfx::Size GetRectSizeFromOrigin(const gfx::Rect& rect) {
