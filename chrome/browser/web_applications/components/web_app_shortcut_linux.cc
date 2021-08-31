bc4678f3c72a78124773d6ad7af7b4852e871322
Mandy Chen <mandy.chen@microsoft.com>
2021-08-31 17:49:10 +0000

    dpwas: Linux support for Shortcut Menus
    Implement support for showing shortcut menu specified in the web app
    manifest via the application dock.
    
    At app install, application actions are written to the .desktop file
    of the app [1].
    
    This change does not add icon support (marked as TODO). Browser tests
    are being looked into and will be added in a follow-up CL.
    
    How to test:
    1. Install
    https://mwjacksonmsft.github.io/pwa/display-standalone/manifest.webmanifest
    on Linux.
    2. Right click on the app icon in the application dock and there should
    be three actions. Click on each and verify the app is launched with the
    right link.
    
    [1] https://specifications.freedesktop.org/desktop-entry-spec/latest/ar01s11.html#extra-actions-identifier
    
    
    Bug: 1069293
    Change-Id: I1e2ed563d35441fdb3755fee66b580046c92255f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115510
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Mandy Chen <mandy.chen@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#916873}

--- chrome/browser/web_applications/components/web_app_shortcut_linux.cc
+++ chrome/browser/web_applications/components/web_app_shortcut_linux.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_shortcut_linux.h"
 
 #include <fcntl.h>
+#include <algorithm>
 
 #include "base/base_paths.h"
 #include "base/environment.h"
@@ -310,6 +311,29 @@ bool CreateShortcutInApplicationsMenu(base::Environment* env,
 
 namespace web_app {
 
+DesktopActionInfo::DesktopActionInfo() = default;
+
+DesktopActionInfo::DesktopActionInfo(const std::string& id,
+                                     const std::string& name,
+                                     const GURL& exec_launch_url)
+    : id(id), name(name), exec_launch_url(exec_launch_url) {
+#if DCHECK_IS_ON()
+  // Check that `id` only contains characters A-Za-z0-9-.
+  auto is_character_allowed = [](auto c) {
+    return base::IsAsciiAlpha(c) || base::IsAsciiDigit(c) || c == '-';
+  };
+  DCHECK(std::all_of(id.begin() + 1, id.end(), is_character_allowed));
+#endif  // DCHECK_IS_ON()
+}
+
+DesktopActionInfo::DesktopActionInfo(const DesktopActionInfo&) = default;
+
+bool DesktopActionInfo::operator<(const DesktopActionInfo& other) const {
+  return this->id < other.id;
+}
+
+DesktopActionInfo::~DesktopActionInfo() = default;
+
 void SetLaunchXdgUtilityForTesting(
     LaunchXdgUtilityForTesting launchXdgUtilityForTesting) {
   GetInstalledLaunchXdgUtilityForTesting() =
@@ -432,7 +456,8 @@ bool CreateDesktopShortcut(base::Environment* env,
     std::string contents = shell_integration_linux::GetDesktopFileContents(
         chrome_exe_path, app_name, shortcut_info.url,
         shortcut_info.extension_id, shortcut_info.title, icon_name,
-        shortcut_info.profile_path, "", "", false, "");
+        shortcut_info.profile_path, "", "", false, "",
+        std::move(shortcut_info.actions));
     success = CreateShortcutOnDesktop(shortcut_filename, contents);
   }
 
@@ -441,7 +466,8 @@ bool CreateDesktopShortcut(base::Environment* env,
     std::string contents = shell_integration_linux::GetDesktopFileContents(
         chrome_exe_path, app_name, shortcut_info.url,
         shortcut_info.extension_id, shortcut_info.title, icon_name,
-        shortcut_info.profile_path, "", "", false, kRunOnOsLoginModeWindowed);
+        shortcut_info.profile_path, "", "", false, kRunOnOsLoginModeWindowed,
+        std::move(shortcut_info.actions));
     success =
         CreateShortcutInAutoStart(env, shortcut_filename, contents) && success;
   }
@@ -482,7 +508,7 @@ bool CreateDesktopShortcut(base::Environment* env,
       shortcut_info.title, icon_name, shortcut_info.profile_path, "",
       base::JoinString(mime_types, ";"),
       creation_locations.applications_menu_location == APP_MENU_LOCATION_HIDDEN,
-      "");
+      "", std::move(shortcut_info.actions));
   success = CreateShortcutInApplicationsMenu(env, shortcut_filename, contents,
                                              directory_filename,
                                              directory_contents) &&
