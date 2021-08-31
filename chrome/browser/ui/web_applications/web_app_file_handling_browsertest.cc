7b082a07c1ddfac98edebd61d021740c48108c74
Evan Stade <estade@chromium.org>
2021-08-31 00:40:39 +0000

    File handling: close app when file access is not granted in prompt
    When the prompt shows and the user denies the permission (sticky or
    not), close the window/tab instead of leaving a broken app hanging
    around.
    
    This patch also changes the behavior when the permission has already
    been permanently denied before the app is launched to handle a file.
    (This can happen if the OS ignores a file handling de-registration
    -- see crbug.com/1205528.) In this case, open the app to its normal
    start url instead of the file handling URL.
    
    Bug: 1236075
    Change-Id: Ie92a5daf8de889545178f2740c1dbf1966f1c286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3124567
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916619}

--- chrome/browser/ui/web_applications/web_app_file_handling_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_file_handling_browsertest.cc
@@ -200,7 +200,9 @@ class WebAppFileHandlingTestBase : public WebAppControllerBrowserTest {
   void SetFileHandlingPermission(ContentSetting setting) {
     auto* map =
         HostContentSettingsMapFactory::GetForProfile(browser()->profile());
-    map->SetDefaultContentSetting(ContentSettingsType::FILE_HANDLING, setting);
+    map->SetContentSettingDefaultScope(GetSecureAppURL(), GetSecureAppURL(),
+                                       ContentSettingsType::FILE_HANDLING,
+                                       setting);
   }
 
   const AppId& app_id() { return app_id_; }
@@ -279,6 +281,8 @@ class WebAppFileHandlingBrowserTest : public WebAppFileHandlingTestBase {
     web_contents_ = LaunchApplication(
         profile(), app_id, expected_launch_url, launch_container,
         apps::mojom::AppLaunchSource::kSourceFileHandler, files);
+    destroyed_watcher_ =
+        std::make_unique<content::WebContentsDestroyedWatcher>(web_contents_);
   }
 
   void VerifyPwaDidReceiveFileLaunchParams(
@@ -309,12 +313,14 @@ class WebAppFileHandlingBrowserTest : public WebAppFileHandlingTestBase {
  protected:
   base::test::ScopedFeatureList scoped_feature_list_;
   content::WebContents* web_contents_ = nullptr;
+  std::unique_ptr<content::WebContentsDestroyedWatcher> destroyed_watcher_;
 };
 
 IN_PROC_BROWSER_TEST_F(WebAppFileHandlingBrowserTest,
                        LaunchConsumerIsNotTriggeredWithNoFiles) {
   InstallFileHandlingPWA();
   SetFileHandlingPermission(CONTENT_SETTING_ALLOW);
+  // The URL used is the normal start URL.
   LaunchWithFiles(app_id(), GetSecureAppURL(), {});
   VerifyPwaDidReceiveFileLaunchParams(false);
 }
@@ -334,7 +340,8 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingBrowserTest,
   InstallFileHandlingPWA();
   SetFileHandlingPermission(CONTENT_SETTING_BLOCK);
   base::FilePath test_file_path = NewTestFilePath("txt");
-  LaunchWithFiles(app_id(), GetTextFileHandlerActionURL(), {test_file_path});
+  // The URL used is the normal start URL.
+  LaunchWithFiles(app_id(), GetSecureAppURL(), {test_file_path});
 
   VerifyPwaDidReceiveFileLaunchParams(false);
 }
@@ -393,7 +400,6 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingBrowserTest,
 // Regression test for crbug.com/1126091
 IN_PROC_BROWSER_TEST_F(WebAppFileHandlingBrowserTest,
                        LaunchQueueSetOnRedirect) {
-  SetFileHandlingPermission(CONTENT_SETTING_ALLOW);
   // Install an app where the file handling action page redirects.
   auto web_app_info = std::make_unique<WebApplicationInfo>();
   web_app_info->start_url =
@@ -411,6 +417,10 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingBrowserTest,
 
   AppId app_id =
       WebAppControllerBrowserTest::InstallWebApp(std::move(web_app_info));
+
+  // Note this must be called after the app is installed, as installing an app
+  // with new file handlers resets it.
+  SetFileHandlingPermission(CONTENT_SETTING_ALLOW);
 
   // The redirect points to handle_files.html, so wait till that navigation is
   // finished.
@@ -680,7 +690,8 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest, BlockAlways) {
   InstallAndLaunchWebApp();
   FileHandlingPermissionRequestDialogTestApi::Resolve(/*checked=*/true,
                                                       /*accept=*/false);
-  VerifyPwaDidReceiveFileLaunchParams(false);
+  // Verify that the window is closed.
+  destroyed_watcher_->Wait();
   EXPECT_EQ(CONTENT_SETTING_BLOCK,
             GetFileHandlingPermission(GetSecureAppURL()));
 }
@@ -689,7 +700,8 @@ IN_PROC_BROWSER_TEST_F(WebAppFileHandlingPermissionDialogTest, BlockOnce) {
   InstallAndLaunchWebApp();
   FileHandlingPermissionRequestDialogTestApi::Resolve(/*checked=*/false,
                                                       /*accept=*/false);
-  VerifyPwaDidReceiveFileLaunchParams(false);
+  // Verify that the window is closed.
+  destroyed_watcher_->Wait();
   EXPECT_EQ(CONTENT_SETTING_ASK, GetFileHandlingPermission(GetSecureAppURL()));
 }
 
