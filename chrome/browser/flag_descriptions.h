ba2c76505f22112e20f0a3638fb5c0af80e0338d
Min Qin <qinmin@chromium.org>
2021-08-31 20:02:27 +0000

    Add flags for download dialog
    BUG=1234151
    
    Change-Id: I2678fb5124c0e19ddee7caff07e7229f5a3cd158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133747
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916940}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1765,6 +1765,12 @@ extern const char kEnableAndroidSpellcheckerDescription[];
 extern const char kEnableCommandLineOnNonRootedName[];
 extern const char kEnableCommandLineOnNoRootedDescription[];
 
+extern const char kEnableDangerousDownloadDialogName[];
+extern const char kEnableDangerousDownloadDialogDescription[];
+
+extern const char kEnableDuplicateDownloadDialogName[];
+extern const char kEnableDuplicateDownloadDialogDescription[];
+
 extern const char kEnableUseAaudioDriverName[];
 extern const char kEnableUseAaudioDriverDescription[];
 
