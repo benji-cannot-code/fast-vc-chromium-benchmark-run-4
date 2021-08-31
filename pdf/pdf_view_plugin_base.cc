ae8219797be3071d3f9e4ead9032dff30b1cc83c
Ankit Kumar 🌪️ <ankk@microsoft.com>
2021-08-31 20:40:29 +0000

    [unseasoned-pdf] Move PDFEngine find methods to PdfViewPluginBase
    Move the implementations of PDFEngine::Client find methods to
    PdfViewPluginBase to be shared between the two integration models.
    
    Bug: 1199999
    Change-Id: Ib1c1706b73f09871afe62c4e40dbd7ee10644ee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3127469
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916961}

--- pdf/pdf_view_plugin_base.cc
+++ pdf/pdf_view_plugin_base.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
+#include <iterator>
 #include <memory>
 #include <sstream>
 #include <string>
@@ -78,6 +79,9 @@ constexpr double kMinZoom = 0.01;
 // A delay to wait between each accessibility page to keep the system
 // responsive.
 constexpr base::TimeDelta kAccessibilityPageDelay =
+    base::TimeDelta::FromMilliseconds(100);
+
+constexpr base::TimeDelta kFindResultCooldown =
     base::TimeDelta::FromMilliseconds(100);
 
 constexpr char kChromePrintHost[] = "chrome://print/";
@@ -243,6 +247,40 @@ void PdfViewPluginBase::NavigateToDestination(int page,
   if (zoom)
     message.SetDoubleKey("zoom", *zoom);
   SendMessage(std::move(message));
+}
+
+void PdfViewPluginBase::UpdateTickMarks(
+    const std::vector<gfx::Rect>& tickmarks) {
+  float inverse_scale = 1.0f / device_scale_;
+  tickmarks_.clear();
+  tickmarks_.reserve(tickmarks.size());
+  std::transform(tickmarks.begin(), tickmarks.end(),
+                 std::back_inserter(tickmarks_),
+                 [inverse_scale](const gfx::Rect& t) -> gfx::Rect {
+                   return gfx::ScaleToEnclosingRect(t, inverse_scale);
+                 });
+}
+
+void PdfViewPluginBase::NotifyNumberOfFindResultsChanged(int total,
+                                                         bool final_result) {
+  // We don't want to spam the renderer with too many updates to the number of
+  // find results. Don't send an update if we sent one too recently. If it's the
+  // final update, we always send it though.
+  if (recently_sent_find_update_ && !final_result)
+    return;
+
+  NotifyFindResultsChanged(total, final_result);
+  NotifyFindTickmarks(tickmarks_);
+
+  if (final_result)
+    return;
+
+  recently_sent_find_update_ = true;
+  ScheduleTaskOnMainThread(
+      FROM_HERE,
+      base::BindOnce(&PdfViewPluginBase::ResetRecentlySentFindUpdate,
+                     GetWeakPtr()),
+      /*result=*/0, kFindResultCooldown);
 }
 
 void PdfViewPluginBase::NotifyTouchSelectionOccurred() {
@@ -941,6 +979,8 @@ void PdfViewPluginBase::SelectFindResult(bool forward) {
 
 void PdfViewPluginBase::StopFind() {
   engine_->StopFind();
+  tickmarks_.clear();
+  NotifyFindTickmarks(tickmarks_);
 }
 
 void PdfViewPluginBase::SetZoom(double scale) {
@@ -1511,6 +1551,11 @@ void PdfViewPluginBase::LoadAccessibility() {
       base::BindOnce(&PdfViewPluginBase::PrepareAndSetAccessibilityPageInfo,
                      GetWeakPtr()),
       /*result=*/0, kAccessibilityPageDelay);
+}
+
+void PdfViewPluginBase::ResetRecentlySentFindUpdate(
+    int32_t /*unused_but_required*/) {
+  recently_sent_find_update_ = false;
 }
 
 namespace {
