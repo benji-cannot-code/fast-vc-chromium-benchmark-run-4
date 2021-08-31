a85b731349509d210bd489adf2260e98af5f847c
Alexander Alekseev <alemate@chromium.org>
2021-08-31 20:23:28 +0000

    [chromeos] Fix flaky RestoreOnStartupTest
    RestoreOnStartupTest is loading chrome://help, which hits rewriting
    rule. This CL replaces the URL above with the stable
    chrome://settings/help.
    
    Bug: 1218161,1244533
    Change-Id: Ie982d14786a529d9cc0d3c7c3f1fcadf0c8845c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133182
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916954}

--- chrome/browser/ash/policy/handlers/restore_on_startup_browsertest.cc
+++ chrome/browser/ash/policy/handlers/restore_on_startup_browsertest.cc
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 
 namespace {
-const char kStartUpURL1[] = "chrome://help/";
+// We should not use "chrome://help/" here, because it will be rewritten into
+// "chrome://settings/help".
+const char kStartUpURL1[] = "chrome://settings/help";
 const char kStartUpURL2[] = "chrome://version/";
 }  // namespace
 
