2de617adef3852882339be2a46b73aa8294f0bf8
Elaine Chien <elainechien@chromium.org>
2021-08-31 23:01:05 +0000

    Replace InterpolatedTransform animation with gfx::Transform animation
    This CL does the following:
    - Replaces SetInterpolatedTransform with SetTransform and use
    CreateTransformElement rather than CreateInterpolatedTransformElement
    - Modifies < operator of AnimationBuilder::Value struct to support
    zero duration animation blocks (needed to set the initial transform).
    Previously zero duration animation blocks would be nondeterministically
    ordered either before or after non zero duration animation blocks
    of the same start time resulting in a potential DCHECK.
    - Refactors tray_background_view BounceInAnimation to use gfx::Transform
    animations rather than InterpolatedTransform Animations. Since
    gfx::Transform don't take a start value, the start value of the next
    Transform is the end value of the previous transform.
    
    Test:
    For BounceInAnimation (in addition to existing unit tests)
    
    1) Start a screen capture.
    2) Observe animation for both stop recording button and the holding space
    tray when the recording is stopped
    
    Recording:
    https://drive.google.com/file/d/1WBS6bj4UcXgUtx0TkDfRR81RygA7PZtk/view?usp=sharing
    
    
    
    Bug: 1236069
    Change-Id: Ic81f7e17de39a78735157916f2de593585d8c466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123925
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917010}

--- ui/views/animation/animation_sequence_block.h
+++ ui/views/animation/animation_sequence_block.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/transform.h"
 #include "ui/views/animation/animation_key.h"
 #include "ui/views/views_export.h"
 
@@ -27,7 +28,6 @@ class RoundedCornersF;
 }  // namespace gfx
 
 namespace ui {
-class InterpolatedTransform;
 class Layer;
 class LayerOwner;
 }  // namespace ui
@@ -105,13 +105,13 @@ class VIEWS_EXPORT AnimationSequenceBlock {
       ui::LayerOwner* target,
       float opacity,
       gfx::Tween::Type tween_type = gfx::Tween::LINEAR);
-  AnimationSequenceBlock& SetInterpolatedTransform(
+  AnimationSequenceBlock& SetTransform(
       ui::Layer* target,
-      std::unique_ptr<ui::InterpolatedTransform> interpolated_transform,
+      gfx::Transform transform,
       gfx::Tween::Type tween_type = gfx::Tween::LINEAR);
-  AnimationSequenceBlock& SetInterpolatedTransform(
+  AnimationSequenceBlock& SetTransform(
       ui::LayerOwner* target,
-      std::unique_ptr<ui::InterpolatedTransform> interpolated_transform,
+      gfx::Transform transform,
       gfx::Tween::Type tween_type = gfx::Tween::LINEAR);
   AnimationSequenceBlock& SetRoundedCorners(
       ui::Layer* target,
@@ -149,13 +149,12 @@ class VIEWS_EXPORT AnimationSequenceBlock {
   AnimationSequenceBlock& OnScheduled(base::OnceClosure callback);
 
  private:
-  using AnimationValue =
-      absl::variant<gfx::Rect,
-                    float,
-                    SkColor,
-                    std::unique_ptr<ui::InterpolatedTransform>,
-                    gfx::RoundedCornersF,
-                    bool>;
+  using AnimationValue = absl::variant<gfx::Rect,
+                                       float,
+                                       SkColor,
+                                       gfx::RoundedCornersF,
+                                       bool,
+                                       gfx::Transform>;
 
   // Data for the animation of a given AnimationKey.
   struct Element {
