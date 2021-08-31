1d0570f8d20ebbed938f7ce1f75331e6c3ebbb24
Daniel Cheng <dcheng@chromium.org>
2021-08-31 19:40:47 +0000

    Remove unused <memory> header from safe_browsing_triggered_popup_blocker.h
    Followup to lint errors found by https://crrev.com/c/3132241.
    
    Change-Id: I4ce028eaf68b79808f5210037976a175bb3c26b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131785
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916927}

--- components/blocked_content/safe_browsing_triggered_popup_blocker.h
+++ components/blocked_content/safe_browsing_triggered_popup_blocker.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_BLOCKED_CONTENT_SAFE_BROWSING_TRIGGERED_POPUP_BLOCKER_H_
 #define COMPONENTS_BLOCKED_CONTENT_SAFE_BROWSING_TRIGGERED_POPUP_BLOCKER_H_
 
-#include <memory>
-
 #include "base/feature_list.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
