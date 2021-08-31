9ba62d47e484cb5d6e7bb816562826d522a0dc40
jlebel <jlebel@chromium.org>
2021-08-31 21:25:05 +0000

    [iOS] Fix typo in UpgradeSigninLogger
    The method was never called since there is a typo in the name. Only the
    method from the super class is called.
    
    Update method name from:
    logSigninCompletedWithResult:addedAcount:advancedSettingsShown:
    to:
    logSigninCompletedWithResult:addedAccount:advancedSettingsShown:
    
    Fixed: 1245381
    Bug: 1245383
    Change-Id: Ic07e6f973a4b9719d6fc3079e50e6d5656d17b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133841
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Auto-Submit: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916973}

--- ios/chrome/browser/ui/authentication/signin/user_signin/logging/upgrade_signin_logger.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/logging/upgrade_signin_logger.mm
@@ -73,7 +73,7 @@ typedef NS_ENUM(NSUInteger, UserSigninPromoAction) {
 }
 
 - (void)logSigninCompletedWithResult:(SigninCoordinatorResult)signinResult
-                         addedAcount:(BOOL)addedAccount
+                        addedAccount:(BOOL)addedAccount
                advancedSettingsShown:(BOOL)advancedSettingsShown {
   [super logSigninCompletedWithResult:signinResult
                          addedAccount:addedAccount
