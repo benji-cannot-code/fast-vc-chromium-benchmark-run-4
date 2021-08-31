b4fff62f8d1bc7fcfba0957867817ad2f5471778
Eric Willigers <ericwilligers@chromium.org>
2021-08-31 15:34:54 +0000

    Call WebAppProvider::GetForLocalAppsUnchecked()
    SessionRestore is being implemented for both system web apps and user
    web apps.
    
    The method WebAppProviderFactory::GetForProfile() is being made
    private. We now call WebAppProvider::GetForLocalAppsUnchecked()
    
    Bug: 1239989
    Change-Id: Iee98812e7eeb92be745530602b6a75a70a660304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131653
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916817}

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -62,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
-#include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "chrome/common/extensions/extension_metrics.h"
 #include "chrome/common/url_constants.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
@@ -189,7 +188,7 @@ class SessionRestoreImpl : public BrowserListObserver {
       // properly. If we don't wait, it's possible that apps are restored in
       // an incoherent state.
       web_app::WebAppProvider* provider =
-          web_app::WebAppProviderFactory::GetForProfile(profile_);
+          web_app::WebAppProvider::GetForLocalAppsUnchecked(profile_);
       DCHECK(provider);
 
       provider->on_registry_ready().Post(
