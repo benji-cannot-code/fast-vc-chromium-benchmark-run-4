fd75f04dc174d4eab1238ef8ef20a1f5d23cb1d4
Aida Zolic <aidazolic@chromium.org>
2021-08-31 10:32:20 +0000

    Fix crash due to DCHECK when printing is blocked
    Remove one call to OnPrintPreviewDialogClosed(), as it is anyway called
    from PrintPreviewDone(). This method calls DispatchAfterPrintEvent(),
    which needs to be called after DispatchBeforePrintEvent(). If called
    twice, the DCHECKS will fail and cause a crash due to changed state
    variable is_in_printing_.
    
    Bug: 1236074
    Change-Id: I96d114cc6a57dd1cd33a8a0885de24d9fcf8c8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3124807
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916749}

--- chrome/browser/printing/print_view_manager.cc
+++ chrome/browser/printing/print_view_manager.cc
@@ -241,7 +241,6 @@ void PrintViewManager::OnPrintPreviewRequestRejected(int render_process_id,
   if (!rfh) {
     return;
   }
-  GetPrintRenderFrame(rfh)->OnPrintPreviewDialogClosed();
   PrintPreviewDone();
 }
 
