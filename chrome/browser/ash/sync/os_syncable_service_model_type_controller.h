29a3f56c4afa4c086eb72f4afc2fc0776266b258
Roman Sorokin <rsorokin@chromium.org>
2021-08-31 16:30:15 +0000

    [ash-chrome] Migrate OsSyncableServiceModelTypeController
    This CL migrates OsSyncableServiceModelTypeController from
    ModelTypeController to SyncableServiceBasedModelTypeController
    
    Bug: 1031549
    Change-Id: Ifc0fe6014121f20184d4372b800a3ce3c2b95fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133085
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916837}

--- chrome/browser/ash/sync/os_syncable_service_model_type_controller.h
+++ chrome/browser/ash/sync/os_syncable_service_model_type_controller.h
@@ -6,18 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_SYNC_OS_SYNCABLE_SERVICE_MODEL_TYPE_CONTROLLER_H_
 #define CHROME_BROWSER_ASH_SYNC_OS_SYNCABLE_SERVICE_MODEL_TYPE_CONTROLLER_H_
 
-#include <memory>
-
 #include "base/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "components/prefs/pref_change_registrar.h"
-#include "components/sync/driver/model_type_controller.h"
+#include "components/sync/driver/syncable_service_based_model_type_controller.h"
 #include "components/sync/model/model_type_store.h"
 
 class PrefService;
 
 namespace syncer {
-class ModelTypeSyncBridge;
 class SyncableService;
 class SyncService;
 }  // namespace syncer
@@ -25,10 +22,8 @@ class SyncService;
 // Controls syncing of Chrome OS ModelTypes that run in sync transport-only
 // mode and are tied to the OS sync feature consent (APP_LIST, OS_PREFERENCES,
 // and OS_PRIORITY_PREFERENCES).
-// TODO(http://crbug.com/1031549): This should derive from
-// SyncableServiceBasedModelTypeController when it supports transport-mode.
 class OsSyncableServiceModelTypeController
-    : public syncer::ModelTypeController {
+    : public syncer::SyncableServiceBasedModelTypeController {
  public:
   OsSyncableServiceModelTypeController(
       syncer::ModelType type,
@@ -52,7 +47,6 @@ class OsSyncableServiceModelTypeController
   // Callback for changes to the OS sync feature enabled pref.
   void OnUserPrefChanged();
 
-  std::unique_ptr<syncer::ModelTypeSyncBridge> bridge_;
   PrefService* const pref_service_;
   syncer::SyncService* const sync_service_;
 
