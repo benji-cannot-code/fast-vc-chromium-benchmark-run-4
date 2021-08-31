359c4021d25a5902f4903aa27d60f337befa5e5f
Avi Drissman <avi@chromium.org>
2021-08-31 23:26:20 +0000

    Use GetLastCommittedOrigin in Javascript dialogs
    GetLastCommittedOrigin more accurately allows us to track the origins
    of the alerting frame and report it to the user.
    
    Bug: 1241497
    Change-Id: Ie62462d3bccd5de7c3d9881781082795f43e3fb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3107569
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917027}

--- components/javascript_dialogs/extensions_client.h
+++ components/javascript_dialogs/extensions_client.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-class GURL;
-
 namespace content {
 class WebContents;
+}
+
+namespace url {
+class Origin;
 }
 
 namespace javascript_dialogs {
@@ -20,7 +22,7 @@ namespace javascript_dialogs {
 // that opened a JavaScript dialog.
 class ExtensionsClient {
  public:
-  virtual ~ExtensionsClient() {}
+  virtual ~ExtensionsClient() = default;
 
   // Called when the extension associated with |web_contents| opened
   // a dialog.
@@ -34,7 +36,7 @@ class ExtensionsClient {
   // |web_contents| in the |name_out| if there is one, returning true;
   // returns false otherwise.
   virtual bool GetExtensionName(content::WebContents* web_contents,
-                                const GURL& alerting_frame_url,
+                                const url::Origin& alerting_frame_origin,
                                 std::string* name_out) = 0;
 };
 
--- extensions/components/javascript_dialog_extensions_client/javascript_dialog_extension_client_impl.cc
+++ extensions/components/javascript_dialog_extensions_client/javascript_dialog_extension_client_impl.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/components/javascript_dialog_extensions_client/javascript_dialog_extension_client_impl.h"
 
-#include "base/macros.h"
-#include "base/memory/ptr_util.h"
+#include <memory>
+
 #include "components/javascript_dialogs/app_modal_dialog_manager.h"
 #include "components/javascript_dialogs/extensions_client.h"
 #include "content/public/browser/web_contents.h"
@@ -39,6 +39,10 @@ class JavaScriptDialogExtensionsClientImpl
  public:
   JavaScriptDialogExtensionsClientImpl() = default;
   ~JavaScriptDialogExtensionsClientImpl() override = default;
+  JavaScriptDialogExtensionsClientImpl(
+      const JavaScriptDialogExtensionsClientImpl&) = delete;
+  JavaScriptDialogExtensionsClientImpl& operator=(
+      const JavaScriptDialogExtensionsClientImpl&) = delete;
 
   // JavaScriptDialogExtensionsClient:
   void OnDialogOpened(content::WebContents* web_contents) override {
@@ -66,27 +70,24 @@ class JavaScriptDialogExtensionsClientImpl
           web_contents->GetLastCommittedURL().spec());
   }
   bool GetExtensionName(content::WebContents* web_contents,
-                        const GURL& alerting_frame_url,
+                        const url::Origin& alerting_frame_origin,
                         std::string* name_out) override {
     const Extension* extension = GetExtensionForWebContents(web_contents);
     if (extension &&
-        url::IsSameOriginWith(alerting_frame_url,
-                              web_contents->GetLastCommittedURL())) {
+        alerting_frame_origin.IsSameOriginWith(
+            web_contents->GetMainFrame()->GetLastCommittedOrigin())) {
       *name_out = extension->name();
       return true;
     }
     return false;
   }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(JavaScriptDialogExtensionsClientImpl);
 };
 
 }  // namespace
 
 void InstallClient() {
   javascript_dialogs::AppModalDialogManager::GetInstance()->SetExtensionsClient(
-      base::WrapUnique(new JavaScriptDialogExtensionsClientImpl));
+      std::make_unique<JavaScriptDialogExtensionsClientImpl>());
 }
 
 }  // namespace javascript_dialog_extensions_client
