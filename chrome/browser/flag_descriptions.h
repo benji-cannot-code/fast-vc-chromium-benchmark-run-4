a799bf7ccf3fa148bdda0363b3f092e85ac39374
Alan Cutter <alancutter@chromium.org>
2021-08-31 21:27:17 +0000

    Add web app launch handler to about:flags
    This exposes the WebAppEnabledLaunchHandler flag to about:flags as:
    chrome://flags#enable-desktop-pwas-launch-handler
    
    Bug: 1231886
    Change-Id: Id219c5632c715190f58d9e77395e792391f1d42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131673
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916976}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -461,6 +461,9 @@ extern const char kDesktopPWAsFlashAppNameInsteadOfOriginDescription[];
 extern const char kDesktopPWAsNotificationIconAndTitleName[];
 extern const char kDesktopPWAsNotificationIconAndTitleDescription[];
 
+extern const char kDesktopPWAsLaunchHandlerName[];
+extern const char kDesktopPWAsLaunchHandlerDescription[];
+
 extern const char kDesktopPWAsLinkCapturingName[];
 extern const char kDesktopPWAsLinkCapturingDescription[];
 
