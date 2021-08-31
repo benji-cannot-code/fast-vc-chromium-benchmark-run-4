9cfee0f0086dab0b42df7cb53a218f31656979ec
Ian Struiksma <ianstruiksma@google.com>
2021-08-31 19:02:57 +0000

    [ReplayTests] Give time for WPR to finish writing archive.
    Add a wait time when shutting down so that the WPR archive write is not
    interrupted.
    
    Bug: 1099009
    Change-Id: Id57a3578efebebc86ee5e96cb72e64c5950f5246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133511
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Commit-Queue: Ian Struiksma <ianstruiksma@google.com>
    Cr-Commit-Position: refs/heads/main@{#916911}

--- tools/captured_sites/control.py
+++ tools/captured_sites/control.py
@@ -60,6 +60,7 @@ import os
 import signal
 import subprocess
 import sys
+import time
 
 # Checking for environment variables.
 _HOME_DIR = os.environ['HOME']
@@ -372,6 +373,8 @@ def _handle_signal(sig, _):
 
   # https://tldp.org/LDP/abs/html/exitcodes.html:
   # Exit code 128+n -> Fatal error signal "n".
+  print('Signal to quit received, waiting for potential WPR write to complete')
+  time.sleep(1)
   sys.exit(128 + sig)
 
 
