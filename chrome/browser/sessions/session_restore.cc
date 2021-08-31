817d08abc80cc4547eb8fd78d32d65fd647957b2
Nancy Wang <nancylingwang@chromium.org>
2021-08-31 03:01:40 +0000

    Don't restore web apps when launch browser after crash.
    When launch browser, web apps opened in a standalone browser window
    should not be restored. So modify RestoreSessionAfterCrash, don't
    restore web apps if the full restore feature is enabled. Web apps
    should be restored from the full restore only in Chrome OS.
    
    BUG=1240848
    
    Change-Id: Ie3141211433f1c68dc31363e5631c724acf84757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113357
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916664}

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/chromeos/boot_times_recorder.h"
+#include "components/full_restore/features.h"
 #endif
 
 #if BUILDFLAG(ENABLE_APP_SESSION_SERVICE)
@@ -1032,8 +1033,16 @@ void SessionRestore::RestoreSessionAfterCrash(Browser* browser) {
            : 0);
 
 #if BUILDFLAG(ENABLE_APP_SESSION_SERVICE)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // In Chrome OS, apps are restored by full restore only. This function is
+  // called when the chrome browser is launched after crash, so only browser
+  // tabs are restored, apps are not restroed.
+  if (!full_restore::features::IsFullRestoreEnabled())
+    behavior |= SessionRestore::RESTORE_APPS;
+#else
   // Apps should always be restored on crash restore.
   behavior |= SessionRestore::RESTORE_APPS;
+#endif
 #endif
   SessionRestore::RestoreSession(profile, browser, behavior,
                                  std::vector<GURL>());
