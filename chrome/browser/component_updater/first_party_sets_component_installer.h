be2a9434498a44ab41dadc01fddf75604315ef9d
cfredric <cfredric@chromium.org>
2021-08-31 16:00:20 +0000

    Add a browsertest to verify FirstPartySets are reconfigured after crashes.
    This is a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/3119868, after
    addressing flakiness.
    
    Bug: 1129506, 1243938
    Change-Id: I7d3026219fb9ee43caabe2ef997abad72ff70dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123275
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916823}

--- chrome/browser/component_updater/first_party_sets_component_installer.h
+++ chrome/browser/component_updater/first_party_sets_component_installer.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/strings/string_piece.h"
 #include "base/values.h"
 #include "components/component_updater/component_installer.h"
 
@@ -46,6 +47,11 @@ class FirstPartySetsComponentInstallerPolicy : public ComponentInstallerPolicy {
   static void ResetForTesting();
 
   static const char kDogfoodInstallerAttributeName[];
+
+  // Seeds a component at `install_dir` with the given `contents`. Only to be
+  // used in testing.
+  static void WriteComponentForTesting(const base::FilePath& install_dir,
+                                       base::StringPiece contents);
 
  private:
   FRIEND_TEST_ALL_PREFIXES(FirstPartySetsComponentInstallerTest,
