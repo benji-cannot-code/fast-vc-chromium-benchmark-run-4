08fa66cc8ecb6a24705992c9f2c48d4f45f6e75b
Austin Sullivan <asully@chromium.org>
2021-08-31 23:10:46 +0000

    IndexedDB: Fix readyState() check failure
    The existing DCHECK is incorrect, since readyState() can be called with
    ready_state_ == kEarlyDeath if the accessor is hit after the execution
    context is destroyed.
    
    Bug: 1243946
    Change-Id: Ia5a56b232b8c9e9d2ca142fb1c487286aa22f77b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3125334
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917017}

--- third_party/blink/renderer/modules/indexeddb/idb_request.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_request.cc
@@ -207,6 +207,11 @@ const IDBRequest::Source* IDBRequest::source(ScriptState* script_state) const {
 }
 
 const String& IDBRequest::readyState() const {
+  if (!GetExecutionContext()) {
+    DCHECK(ready_state_ == DONE || ready_state_ == kEarlyDeath);
+    return indexed_db_names::kDone;
+  }
+
   DCHECK(ready_state_ == PENDING || ready_state_ == DONE);
 
   if (ready_state_ == PENDING)
