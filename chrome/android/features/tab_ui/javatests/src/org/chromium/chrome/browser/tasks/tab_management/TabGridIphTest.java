b244d84f91a9470c5212d3ac673204f3e5d2a6a1
Finnur Thorarinsson <finnur@chromium.org>
2021-08-31 20:18:48 +0000

    Disable TabGridIphTest.testSwipeToDismiss_IPH (flaky)
    Bug: 1245260
    Change-Id: I11074c29a313c63c484d974929a01339e5924048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133042
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916949}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphTest.java
@@ -337,6 +337,7 @@ public class TabGridIphTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1245260")
     public void testSwipeToDismiss_IPH() {
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         enterTabSwitcher(cta);
