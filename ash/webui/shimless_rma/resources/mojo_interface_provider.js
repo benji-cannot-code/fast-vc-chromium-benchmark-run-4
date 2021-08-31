6350859f4e9c2e7793d4bc7039851571abf4a579
Gavin Dodd <gavindodd@google.com>
2021-08-31 23:33:32 +0000

    shimless: Make cancel button call abortRma()
    Bug: 1198187
    Test: Run local build.
          Run Shimless* browser tests.
    
    Change-Id: I33271ff0d1814f02b74eaab0e4219443ec51983f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122759
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917030}

--- ash/webui/shimless_rma/resources/mojo_interface_provider.js
+++ ash/webui/shimless_rma/resources/mojo_interface_provider.js
@@ -41,6 +41,8 @@ function setupFakeShimlessRmaService_() {
 
   service.setStates(fakeStates);
 
+  service.setAbortRmaResult(RmadErrorCode.kOk);
+
   service.setGetCurrentOsVersionResult(fakeChromeVersion[0]);
   service.setCheckForOsUpdatesResult(true);
   service.setUpdateOsResult(false);
