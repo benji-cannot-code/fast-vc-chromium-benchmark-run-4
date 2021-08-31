82acb9baad859baca6269a07da528fa11580d940
Gayane Petrosyan <gayane@google.com>
2021-08-31 18:55:47 +0000

    Use WindowAndroid implementation of  AndroidPermissionDelegate
    Use WindowAndroid instead of ActivityAndroidPermissionDelegate. Unclear
    how ActivityAndroidPermissionDelegate.requestPermissionsFromRequester
    was supposed to communicate back the permission result as
    handlePermissionResult is called only from WindowAndroid.
    
    Bug: 1244014
    Change-Id: Id7ef8aaee7b87c731f3b5cac646b6e4af01be06a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133703
    Commit-Queue: sebsg <sebsg@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916906}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/screenshot/ScreenshotShareSheetCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/screenshot/ScreenshotShareSheetCoordinator.java
@@ -5,19 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.share.screenshot;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 
 import org.chromium.base.Callback;
 import org.chromium.chrome.browser.share.share_sheet.ChromeOptionShareCallback;
 import org.chromium.chrome.browser.tab.Tab;
-import org.chromium.ui.base.ActivityAndroidPermissionDelegate;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -50,9 +47,8 @@ public class ScreenshotShareSheetCoordinator {
         mModel = new PropertyModel(allProperties);
 
         mModel.set(ScreenshotShareSheetViewProperties.SCREENSHOT_BITMAP, screenshot);
-        mSaveDelegate = new ScreenshotShareSheetSaveDelegate(context, mModel, closeDialogRunnable,
-                new ActivityAndroidPermissionDelegate(
-                        new WeakReference<Activity>((Activity) context)));
+        mSaveDelegate = new ScreenshotShareSheetSaveDelegate(
+                context, mModel, closeDialogRunnable, tab.getWindowAndroid());
         mMediator = new ScreenshotShareSheetMediator(context, mModel, closeDialogRunnable,
                 mSaveDelegate::save, tab, shareUrl, shareSheetCallback, installCallback);
 
