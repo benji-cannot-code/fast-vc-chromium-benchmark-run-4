88f9bf513c79b6b874317ef658de633c1447c130
Aaron Leventhal <aleventhal@google.com>
2021-08-31 01:12:58 +0000

    Don't expose  kValue string where there are numeric values already
    Avoid redundant information, and allow browser side to determine
    whether aria-valuetext was used.
    
    This will enable CL:3125463 to update the Android mappings so that
    something using aria-valuetext can override the value, and not
    have a percentage spoken (which wouldn't make sense with valuetext).
    
    Bug: 1165927, b/176216946
    Change-Id: I9d2312353a3162b8c326615e60c850f178853faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123896
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916631}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -2713,8 +2713,7 @@ String AXNodeObject::GetValueForControl() const {
     String aria_value_text =
         GetAOMPropertyOrARIAAttribute(AOMStringProperty::kValueText)
             .GetString();
-    if (!aria_value_text.IsEmpty())
-      return aria_value_text;
+    return aria_value_text.IsEmpty() ? String() : aria_value_text;
   }
 
   if (GetLayoutObject() && GetLayoutObject()->IsFileUploadControl())
