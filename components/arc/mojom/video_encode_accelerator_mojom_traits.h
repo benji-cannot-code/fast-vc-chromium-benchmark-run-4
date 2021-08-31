16646a8cfca4e1fb618e1124861ced7118ad1041
David Staessens <dstaessens@google.com>
2021-08-31 10:57:19 +0000

    gpu_arc_vea: Add support for configuring the bitrate mode.
    This CL adds support for changing the bitrate mode and associated peak
    bitrate to the libvda encoder. All bitrate-related variables are packed
    into a new Bitrate structure similar to the changes made to the Chrome
    VEA in crrev.com/c/2986204.
    
    The bitrate can be configured during initialization using the new
    'bitrate' field in the mojo VideoEncodeAcceleratorConfig, and
    dynamically using the new 'RequestEncodingParametersChange' mojo call
    that uses the new Bitrate structure.
    
    BUG=b:190336806,b:181514834
    TEST=arc.VideoEncodeAccel.h264_192p_i420_vm
    
    Change-Id: I33ab5cdca7c398d6b6bba04962fd057097703c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3026783
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#916755}

--- components/arc/mojom/video_encode_accelerator_mojom_traits.h
+++ components/arc/mojom/video_encode_accelerator_mojom_traits.h
@@ -9,6 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/mojom/video_encode_accelerator.mojom-shared.h"
 #include "media/base/bitrate.h"
 #include "media/video/video_encode_accelerator.h"
+#include "mojo/public/cpp/bindings/enum_traits.h"
+#include "mojo/public/cpp/bindings/union_traits.h"
+
+// Needs to be included after 'video_encode_accelerator.h'.
+#include "components/arc/mojom/video_encode_accelerator.mojom.h"
 
 namespace mojo {
 
@@ -59,6 +64,28 @@ struct StructTraits<arc::mojom::VideoEncodeProfileDataView,
   }
 };
 
+// TODO(b/198127993): Convert directly to media::Bitrate.
+template <>
+struct StructTraits<arc::mojom::ConstantBitrateDataView,
+                    arc::mojom::ConstantBitrate> {
+  static uint32_t target(const arc::mojom::ConstantBitrate& input);
+};
+
+template <>
+struct StructTraits<arc::mojom::VariableBitrateDataView,
+                    arc::mojom::VariableBitrate> {
+  static uint32_t target(const arc::mojom::VariableBitrate& input);
+  static uint32_t peak(const arc::mojom::VariableBitrate& input);
+};
+
+template <>
+struct UnionTraits<arc::mojom::BitrateDataView, media::Bitrate> {
+  static arc::mojom::BitrateDataView::Tag GetTag(const media::Bitrate& input);
+  static arc::mojom::ConstantBitrate constant(const media::Bitrate& input);
+  static arc::mojom::VariableBitrate variable(const media::Bitrate& input);
+  static bool Read(arc::mojom::BitrateDataView input, media::Bitrate* output);
+};
+
 template <>
 struct StructTraits<arc::mojom::VideoEncodeAcceleratorConfigDataView,
                     media::VideoEncodeAccelerator::Config> {
@@ -77,9 +104,7 @@ struct StructTraits<arc::mojom::VideoEncodeAcceleratorConfigDataView,
     return input.output_profile;
   }
 
-  // TODO(clarissagarvey): update arc Mojo to use the new
-  // VideoEncodeAccelerator::Config API in transport
-  static uint32_t initial_bitrate(
+  static uint32_t initial_bitrate_deprecated(
       const media::VideoEncodeAccelerator::Config& input) {
     return input.bitrate.target();
   }
@@ -124,6 +149,11 @@ struct StructTraits<arc::mojom::VideoEncodeAcceleratorConfigDataView,
       case media::VideoEncodeAccelerator::Config::StorageType::kGpuMemoryBuffer:
         return arc::mojom::VideoFrameStorageType::DMABUF;
     }
+  }
+
+  static const media::Bitrate& bitrate(
+      const media::VideoEncodeAccelerator::Config& input) {
+    return input.bitrate;
   }
 
   static bool Read(arc::mojom::VideoEncodeAcceleratorConfigDataView input,
