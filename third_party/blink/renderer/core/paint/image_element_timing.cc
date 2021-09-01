3e9692248f9918a47232f9a18dbc773e23401420
Nicolás Peña Moreno <npm@chromium.org>
2021-09-01 18:08:11 +0000

    Remove incorrect DCHECK in ImageElementTiming
    Bug: 1244353
    Change-Id: Ia3949a54321d4f1b233ae3eb928afa4dbe2b22e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135530
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917306}

--- third_party/blink/renderer/core/paint/image_element_timing.cc
+++ third_party/blink/renderer/core/paint/image_element_timing.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/paint/image_element_timing.h"
 
+#include "base/time/time.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/loader/resource/image_resource_content.h"
@@ -207,7 +208,13 @@ void ImageElementTiming::NotifyBackgroundImagePainted(
     return;
 
   auto it = background_image_timestamps_.find(&background_image);
-  DCHECK(it != background_image_timestamps_.end());
+  if (it == background_image_timestamps_.end()) {
+    // TODO(npm): investigate how this could happen. For now, we set the load
+    // time as the current time.
+    background_image_timestamps_.insert(&background_image,
+                                        base::TimeTicks::Now());
+    it = background_image_timestamps_.find(&background_image);
+  }
 
   ImageInfo& info =
       images_notified_
