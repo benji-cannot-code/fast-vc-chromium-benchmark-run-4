0819687b0c1c9205b1f123530d9d36e02fe375e4
Gavin Williams <gavinwill@chromium.org>
2021-09-01 23:45:19 +0000

    scanning: Show cancel button for multi-page scans
    While doing a multi-page scan, show the Cancel button and hide the Save
    and Scan buttons
    
    screenshot: http://screen/4d9vHtFNLjkqyMD
    
    Bug: 1210271
    Change-Id: I86c7020121979d7f908fe52c31371dbb366b6b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3134527
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917451}

--- chrome/test/data/webui/chromeos/scanning/multi_page_scan_test.js
+++ chrome/test/data/webui/chromeos/scanning/multi_page_scan_test.js
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://scanning/multi_page_scan.js';
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+import {AppState} from 'chrome://scanning/scanning_app_types.js';
 import {ScanningBrowserProxyImpl} from 'chrome://scanning/scanning_browser_proxy.js';
 
-import {assertEquals, assertTrue} from '../../chai_assert.js';
-import {flushTasks} from '../../test_util.js';
+import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
+import {flushTasks, isVisible} from '../../test_util.js';
 
 import {TestScanningBrowserProxy} from './test_scanning_browser_proxy.js';
 
@@ -75,5 +77,31 @@ export function multiPageScanTest() {
 
     multiPageScan.$$('#saveButton').click();
     assertTrue(completeMultiPageScanEventFired);
+  });
+
+  // Verify the cancel button and progress text show while scanning.
+  test('cancelButtonShowsWhileScanning', () => {
+    const scanButton =
+        /** @type {!HTMLElement} */ (multiPageScan.$$('#scanButton'));
+    const cancelButton =
+        /** @type {!HTMLElement} */ (multiPageScan.$$('#cancelButton'));
+    const pageNumber = 4;
+
+    // Scan button should be visible.
+    multiPageScan.appState = AppState.MULTI_PAGE_NEXT_ACTION;
+    assertTrue(isVisible(scanButton));
+    assertFalse(isVisible(cancelButton));
+    assertEquals(
+        loadTimeData.getString('multiPageScanInstructionsText'),
+        multiPageScan.$$('#multiPageScanText').innerText.trim());
+
+    // Cancel button should be visible while scanning.
+    multiPageScan.pageNumber = pageNumber;
+    multiPageScan.appState = AppState.MULTI_PAGE_SCANNING;
+    assertFalse(isVisible(scanButton));
+    assertTrue(isVisible(cancelButton));
+    assertEquals(
+        loadTimeData.getStringF('multiPageScanProgressText', pageNumber),
+        multiPageScan.$$('#multiPageScanText').innerText.trim());
   });
 }
