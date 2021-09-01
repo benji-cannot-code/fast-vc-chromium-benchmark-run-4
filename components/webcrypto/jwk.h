221b8686c280809596c4d994d92dc7320cb96b6b
Thomas Guilbert <tguilbert@chromium.org>
2021-09-01 21:17:20 +0000

    [Code Health] Update base::Value uses in webcrypto::Jwk*
    This CL removes uses of GetListAs() from jwk.cc. It also modernizes some
    other deprecated base::Value calls, such as using a base::Value directly
    instead of a base::DictionaryValue.
    
    There should be no functional changes.
    
    Bug: 1187009
    Change-Id: I75cfe7496bccbd4bfa7de4a11f78c7e18ea738e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3134722
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917389}

--- components/webcrypto/jwk.h
+++ components/webcrypto/jwk.h
@@ -100,7 +100,7 @@ class JwkReader {
   Status VerifyAlg(const std::string& expected_alg) const;
 
  private:
-  base::DictionaryValue dict_;
+  base::Value dict_;
 };
 
 // Helper class for building the JSON for a JWK.
@@ -125,7 +125,7 @@ class JwkWriter {
   void ToJson(std::vector<uint8_t>* utf8_bytes) const;
 
  private:
-  base::DictionaryValue dict_;
+  base::Value dict_;
 };
 
 // Converts a JWK "key_ops" array to the corresponding WebCrypto usages. Used by
