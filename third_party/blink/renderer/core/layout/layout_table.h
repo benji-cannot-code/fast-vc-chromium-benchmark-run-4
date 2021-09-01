ca77740e495bf32a0701cb7e9286f3f6ece30b3e
Yuki Yamada <yukiy@chromium.org>
2021-09-01 05:38:22 +0000

    Layout: Convert TableLayoutAlgorithm to GCed
    Convert TableLayoutAlgorithm and related classes to GCed to replace
    UntracedMembers in them added by https://crrev.com/c/2465602 to Members.
    
    Bug: 1030176
    Change-Id: I3d429136b2034f34272c372d00d7211413098261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3117306
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917123}

--- third_party/blink/renderer/core/layout/layout_table.h
+++ third_party/blink/renderer/core/layout/layout_table.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_LAYOUT_TABLE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_LAYOUT_TABLE_H_
 
-#include <memory>
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/layout/layout_block.h"
@@ -604,7 +603,7 @@ class CORE_EXPORT LayoutTable final : public LayoutBlock,
   //
   // As the algorithm is dependent on the style, this field is nullptr before
   // the first style is applied in styleDidChange().
-  std::unique_ptr<TableLayoutAlgorithm> table_layout_;
+  Member<TableLayoutAlgorithm> table_layout_;
 
   // Collapsed borders are SUPER EXPENSIVE to compute. The reason is that we
   // need to compare a cells border against all the adjoining cells, rows,
--- third_party/blink/renderer/core/layout/table_layout_algorithm_auto.cc
+++ third_party/blink/renderer/core/layout/table_layout_algorithm_auto.cc
@@ -179,7 +179,7 @@ void TableLayoutAlgorithmAuto::FullRecalc() {
   unsigned n_eff_cols = table_->NumEffectiveColumns();
   layout_struct_.resize(n_eff_cols);
   layout_struct_.Fill(Layout());
-  span_cells_.Fill(nullptr);
+  span_cells_.clear();
 
   Length group_logical_width;
   unsigned current_column = 0;
@@ -928,4 +928,10 @@ void TableLayoutAlgorithmAuto::ShrinkColumnWidth(
     }
   }
 }
+
+void TableLayoutAlgorithmAuto::Trace(Visitor* visitor) const {
+  visitor->Trace(span_cells_);
+  TableLayoutAlgorithm::Trace(visitor);
+}
+
 }  // namespace blink
