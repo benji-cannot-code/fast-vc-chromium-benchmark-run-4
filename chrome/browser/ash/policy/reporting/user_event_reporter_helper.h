7568917b7044e4718e9c3d1f841569c2011d9ba5
Ahmed Nasr <anasr@google.com>
2021-09-01 18:00:35 +0000

    Refactor UserEventReporterBase
    Refactor UserEventReporterBase and make it
    a helper class instead of a base class.
    
    Bug: b:197976589
    Change-Id: Ifd5874ab48a1560bc63883026248ce6e6ad964d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122889
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#917292}

--- chrome/browser/ash/policy/reporting/user_event_reporter_helper.h
+++ chrome/browser/ash/policy/reporting/user_event_reporter_helper.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_REPORTING_USER_EVENT_REPORTER_BASE_H_
-#define CHROME_BROWSER_ASH_REPORTING_USER_EVENT_REPORTER_BASE_H_
+#ifndef CHROME_BROWSER_ASH_POLICY_REPORTING_USER_EVENT_REPORTER_HELPER_H_
+#define CHROME_BROWSER_ASH_POLICY_REPORTING_USER_EVENT_REPORTER_HELPER_H_
 
 #include <memory>
 
@@ -14,19 +14,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace reporting {
 
-class UserEventReporterBase {
+class UserEventReporterHelper {
  public:
-  explicit UserEventReporterBase(::reporting::Destination destination);
-  UserEventReporterBase(const UserEventReporterBase& other) = delete;
-  UserEventReporterBase& operator=(const UserEventReporterBase& other) = delete;
-  virtual ~UserEventReporterBase();
-
- protected:
+  explicit UserEventReporterHelper(Destination destination);
   // Allows test classes to pass in user defined report queues for testing.
-  explicit UserEventReporterBase(
-      ::reporting::Destination destination,
+  explicit UserEventReporterHelper(
       std::unique_ptr<::reporting::ReportQueue, base::OnTaskRunnerDeleter>
           report_queue);
+  UserEventReporterHelper(const UserEventReporterHelper& other) = delete;
+  UserEventReporterHelper& operator=(const UserEventReporterHelper& other) =
+      delete;
+  virtual ~UserEventReporterHelper();
 
   // Returns whether the user email can be included in the report. By default,
   // only affiliated user emails are included. Function can accept
@@ -37,17 +35,15 @@ class UserEventReporterBase {
   virtual bool ReportingEnabled(const std::string& policy_path) const;
 
   // Reports an event to the queue.
-  virtual void ReportEvent(base::StringPiece record,
-                           ::reporting::Priority priority);
+  virtual void ReportEvent(const google::protobuf::MessageLite* record,
+                           Priority priority);
 
+ private:
   // Returns the device DM token.
   static policy::DMToken GetDMToken();
 
-  std::unique_ptr<::reporting::ReportQueue, base::OnTaskRunnerDeleter>
-      report_queue_;
-
-  const ::reporting::Destination destination_;
+  std::unique_ptr<ReportQueue, base::OnTaskRunnerDeleter> report_queue_;
 };
 }  // namespace reporting
 
-#endif  // CHROME_BROWSER_ASH_POLICY_REPORTING_USER_EVENT_REPORTER_BASE_H_
+#endif  // CHROME_BROWSER_ASH_POLICY_REPORTING_USER_EVENT_REPORTER_HELPER_H_
