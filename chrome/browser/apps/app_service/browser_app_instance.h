9a75a261c87b202a077fdde4fa2b7f35d6414269
Alexander Bolodurin <alexbn@chromium.org>
2021-09-01 12:22:10 +0000

    [lacros shelf] Make BrowserAppInstance ID a generated token
    Replace a combination of PID, WebContentsId, and window with a generated
    token. PID is removed as ID namespacing is no longer necessary.
    
    Bug: 1203992
    Change-Id: I5994a76d086b8f7eab8f08dc233479d2985516a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135214
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917195}

--- chrome/browser/apps/app_service/browser_app_instance.h
+++ chrome/browser/apps/app_service/browser_app_instance.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/process/process_handle.h"
-#include "base/types/id_type.h"
+#include "base/unguessable_token.h"
 
 class Browser;
 
@@ -23,9 +23,7 @@ class WebContents;
 
 namespace apps {
 
-struct WebContentsIdTypeMarker {};
-
-typedef base::IdTypeU32<WebContentsIdTypeMarker> WebContentsId;
+typedef base::UnguessableToken BrowserAppInstanceId;
 
 // An instance of a browser-based app. Can represent either of:
 // - apps running inside Browser->WebContents (in a tab or in a window),
@@ -42,11 +40,10 @@ struct BrowserAppInstance {
   BrowserAppInstance(const BrowserAppInstance&) = delete;
   BrowserAppInstance& operator=(const BrowserAppInstance&) = delete;
 
-  std::string app_id;
+  BrowserAppInstanceId id;
   Type type;
-  base::ProcessId process_id;
+  std::string app_id;
   aura::Window* window;
-  WebContentsId web_contents_id;
   bool visible;
   bool active;
 };
