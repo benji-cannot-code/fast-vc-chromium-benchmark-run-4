bab31b9471f15b3bbdeb2caaaa15ba1f229f5203
yugeeklab <yugeeklab@gmail.com>
2021-09-01 10:47:59 +0000

    Improve text matching for datalist
    Before modification, it verified that the input sentence matches
    in the target sentences. This causes strict text matching.
    After modification, It verifies that the input words matches in
    the target sentences. This allows for more flexible text matching.
    
    Bug: 1194436
    Change-Id: I6f315c38d0ad22c652a1e15b83ed015f4bbf9e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3110795
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917177}

--- third_party/blink/renderer/core/editing/visible_units_word.cc
+++ third_party/blink/renderer/core/editing/visible_units_word.cc
@@ -50,11 +50,6 @@ static bool IsLineBreak(UChar ch) {
   return ch == kNewlineCharacter || ch == kCarriageReturnCharacter;
 }
 
-static bool IsWordBreak(UChar ch) {
-  return (WTF::unicode::IsPrintableChar(ch) && !IsWhitespace(ch)) ||
-         U16_IS_SURROGATE(ch) || IsLineBreak(ch) || ch == kLowLineCharacter;
-}
-
 PositionInFlatTree EndOfWordPositionInternal(const PositionInFlatTree& position,
                                              WordSide side) {
   class Finder final : public TextSegments::Finder {
@@ -340,4 +335,8 @@ Position StartOfWordPosition(const Position& position, WordSide side) {
       StartOfWordPosition(ToPositionInFlatTree(position), side));
 }
 
+bool IsWordBreak(UChar ch) {
+  return (WTF::unicode::IsPrintableChar(ch) && !IsWhitespace(ch)) ||
+         U16_IS_SURROGATE(ch) || IsLineBreak(ch) || ch == kLowLineCharacter;
+}
 }  // namespace blink
