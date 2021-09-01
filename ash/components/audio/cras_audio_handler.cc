dda2608e071b75e77fb310f28202f1099abfa7c8
Hsin-Yu Chao <hychao@google.com>
2021-09-01 04:46:13 +0000

    Sets FlossEnabled to CRAS if feature flag is set
    Calling dBus method SetFlossEnabled makes CRAS switch to
    use Floss as Bluetooth stack. Expose this control to Chrome
    side so eventually a Chrome flag can toggle it.
    
    R=abhishekpandit, yuhsuan
    
    Bug: 195084852
    Test: manual change the value of kFlossEnabled
    Change-Id: Ic45afcdad4bac7b833383030029a1ed6675c9490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3068786
    Auto-Submit: Hsinyu Chao <hychao@chromium.org>
    Commit-Queue: Hsinyu Chao <hychao@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917112}

--- ash/components/audio/cras_audio_handler.cc
+++ ash/components/audio/cras_audio_handler.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "base/system/system_monitor.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "device/bluetooth/floss/floss_features.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 namespace ash {
@@ -1114,6 +1115,10 @@ void CrasAudioHandler::InitializeAudioAfterCrasServiceAvailable(
         base::BindOnce(&CrasAudioHandler::HandleGetDeprioritizeBtWbsMic,
                        weak_ptr_factory_.GetWeakPtr()));
   }
+
+  // Sets Floss enabled based on feature flag.
+  CrasAudioClient::Get()->SetFlossEnabled(
+      base::FeatureList::IsEnabled(floss::features::kFlossEnabled));
 
   input_muted_by_microphone_mute_switch_ = IsMicrophoneMuteSwitchOn();
   if (input_muted_by_microphone_mute_switch_)
