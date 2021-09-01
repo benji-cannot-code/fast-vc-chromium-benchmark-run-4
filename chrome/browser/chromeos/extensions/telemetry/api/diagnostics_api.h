0836e30bdd718ccebc4227ea90487727a70a7e0f
Oleh Lamzin <lamzin@google.com>
2021-09-01 18:04:51 +0000

    [Telemetry] Introduce getAvailableRoutines
    Introduce new chrome.os.diagnostics.getAvailableRoutines API
    function to get available routines.
    
    Bug: b:188897824
    Change-Id: I26b7a6d33d6d53882edbc50618b5ecc40d50704c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086683
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Mahmoud Gawad <mgawad@google.com>
    Cr-Commit-Position: refs/heads/main@{#917300}

--- chrome/browser/chromeos/extensions/telemetry/api/diagnostics_api.h
+++ chrome/browser/chromeos/extensions/telemetry/api/diagnostics_api.h
@@ -14,8 +14,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
+class DiagnosticsApiFunctionBase : public ExtensionFunction {
+ public:
+  DiagnosticsApiFunctionBase();
+
+  DiagnosticsApiFunctionBase(const DiagnosticsApiFunctionBase&) = delete;
+  DiagnosticsApiFunctionBase& operator=(const DiagnosticsApiFunctionBase&) =
+      delete;
+
+ protected:
+  ~DiagnosticsApiFunctionBase() override;
+
+  mojo::Remote<ash::health::mojom::DiagnosticsService>
+      remote_diagnostics_service_;
+
+ private:
+  DiagnosticsService diagnostics_service_;
+};
+
+class OsDiagnosticsGetAvailableRoutinesFunction
+    : public DiagnosticsApiFunctionBase {
+ public:
+  DECLARE_EXTENSION_FUNCTION("os.diagnostics.getAvailableRoutines",
+                             OS_DIAGNOSTICS_GETAVAILABLEROUTINES)
+
+  OsDiagnosticsGetAvailableRoutinesFunction();
+  OsDiagnosticsGetAvailableRoutinesFunction(
+      const OsDiagnosticsGetAvailableRoutinesFunction&) = delete;
+  OsDiagnosticsGetAvailableRoutinesFunction& operator=(
+      const OsDiagnosticsGetAvailableRoutinesFunction&) = delete;
+
+ private:
+  ~OsDiagnosticsGetAvailableRoutinesFunction() override;
+
+  // ExtensionFunction:
+  ResponseAction Run() override;
+
+  void OnResult(
+      const std::vector<ash::health::mojom::DiagnosticRoutineEnum>& routines);
+};
+
 class OsDiagnosticsRunBatteryCapacityRoutineFunction
-    : public ExtensionFunction {
+    : public DiagnosticsApiFunctionBase {
  public:
   DECLARE_EXTENSION_FUNCTION("os.diagnostics.runBatteryCapacityRoutine",
                              OS_DIAGNOSTICS_RUNBATTERYCAPACITYROUTINE)
@@ -33,11 +73,6 @@ class OsDiagnosticsRunBatteryCapacityRoutineFunction
   ResponseAction Run() override;
 
   void OnResult(ash::health::mojom::RunRoutineResponsePtr ptr);
-
-  mojo::Remote<ash::health::mojom::DiagnosticsService>
-      remote_diagnostics_service_;
-
-  DiagnosticsService diagnostics_service_;
 };
 
 }  // namespace chromeos
