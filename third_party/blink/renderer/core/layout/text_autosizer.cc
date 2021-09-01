5e5ba14817cc23c4d846ff0bb183b69ce20cc941
Yuki Yamada <yukiy@chromium.org>
2021-09-01 04:37:59 +0000

    Layout: Remove UntracedMembers in TextAutosizer
    Convert Supercluster and Cluster to GCed to replace UntracedMembers in
    TextAutosizer to Members.
    
    This is a follow-up for https://crrev.com/c/2465602.
    
    Bug: 1030176
    Change-Id: I185b84e64354882704504966db69a86cf0833b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3121643
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917110}

--- third_party/blink/renderer/core/layout/text_autosizer.cc
+++ third_party/blink/renderer/core/layout/text_autosizer.cc
@@ -344,7 +344,7 @@ void TextAutosizer::PrepareClusterStack(LayoutObject* layout_object) {
     blocks_that_have_begun_layout_.insert(block);
 #endif
     if (Cluster* cluster = MaybeCreateCluster(block))
-      cluster_stack_.push_back(base::WrapUnique(cluster));
+      cluster_stack_.push_back(cluster);
   }
 }
 
@@ -364,7 +364,7 @@ void TextAutosizer::BeginLayout(LayoutBlock* block,
     did_check_cross_site_use_count_ = false;
 
   if (Cluster* cluster = MaybeCreateCluster(block))
-    cluster_stack_.push_back(base::WrapUnique(cluster));
+    cluster_stack_.push_back(cluster);
 
   DCHECK(!cluster_stack_.IsEmpty());
 
@@ -898,7 +898,7 @@ TextAutosizer::Cluster* TextAutosizer::MaybeCreateCluster(LayoutBlock* block) {
     return nullptr;
 
   bool is_new_entry = false;
-  Cluster* cluster = new Cluster(
+  Cluster* cluster = MakeGarbageCollected<Cluster>(
       block, flags, parent_cluster,
       fingerprint_mapper_.CreateSuperclusterIfNeeded(block, is_new_entry));
   return cluster;
@@ -917,13 +917,13 @@ TextAutosizer::FingerprintMapper::CreateSuperclusterIfNeeded(
     return nullptr;
 
   SuperclusterMap::AddResult add_result =
-      superclusters_.insert(fingerprint, std::unique_ptr<Supercluster>());
+      superclusters_.insert(fingerprint, nullptr);
   is_new_entry = add_result.is_new_entry;
   if (!add_result.is_new_entry)
-    return add_result.stored_value->value.get();
+    return add_result.stored_value->value;
 
-  Supercluster* supercluster = new Supercluster(roots);
-  add_result.stored_value->value = base::WrapUnique(supercluster);
+  Supercluster* supercluster = MakeGarbageCollected<Supercluster>(roots);
+  add_result.stored_value->value = supercluster;
   return supercluster;
 }
 
@@ -1302,9 +1302,13 @@ bool TextAutosizer::IsWiderOrNarrowerDescendant(Cluster* cluster) {
   return false;
 }
 
+void TextAutosizer::Supercluster::Trace(Visitor* visitor) const {
+  visitor->Trace(roots_);
+}
+
 TextAutosizer::Cluster* TextAutosizer::CurrentCluster() const {
   SECURITY_DCHECK(!cluster_stack_.IsEmpty());
-  return cluster_stack_.back().get();
+  return cluster_stack_.back();
 }
 
 TextAutosizer::Cluster::Cluster(const LayoutBlock* root,
@@ -1321,6 +1325,13 @@ TextAutosizer::Cluster::Cluster(const LayoutBlock* root,
       has_table_ancestor_(root->IsTableCell() ||
                           (parent_ && parent_->has_table_ancestor_)) {}
 
+void TextAutosizer::Cluster::Trace(Visitor* visitor) const {
+  visitor->Trace(root_);
+  visitor->Trace(deepest_block_containing_all_text_);
+  visitor->Trace(parent_);
+  visitor->Trace(supercluster_);
+}
+
 #if DCHECK_IS_ON()
 void TextAutosizer::FingerprintMapper::AssertMapsAreConsistent() {
   // For each fingerprint -> block mapping in m_blocksForFingerprint we should
@@ -1330,12 +1341,9 @@ void TextAutosizer::FingerprintMapper::AssertMapsAreConsistent() {
            blocks_for_fingerprint_.begin();
        fingerprint_it != end; ++fingerprint_it) {
     Fingerprint fingerprint = fingerprint_it->key;
-    BlockSet* blocks = fingerprint_it->value.get();
-    for (BlockSet::iterator block_it = blocks->begin();
-         block_it != blocks->end(); ++block_it) {
-      const LayoutBlock* block = (*block_it);
+    BlockSet* blocks = fingerprint_it->value;
+    for (auto& block : *blocks)
       DCHECK_EQ(fingerprints_.at(block), fingerprint);
-    }
   }
 }
 #endif
@@ -1356,9 +1364,9 @@ void TextAutosizer::FingerprintMapper::AddTentativeClusterRoot(
   Add(block, fingerprint);
 
   ReverseFingerprintMap::AddResult add_result =
-      blocks_for_fingerprint_.insert(fingerprint, std::unique_ptr<BlockSet>());
+      blocks_for_fingerprint_.insert(fingerprint, nullptr);
   if (add_result.is_new_entry)
-    add_result.stored_value->value = std::make_unique<BlockSet>();
+    add_result.stored_value->value = MakeGarbageCollected<BlockSet>();
   add_result.stored_value->value->insert(block);
 #if DCHECK_IS_ON()
   AssertMapsAreConsistent();
@@ -1384,7 +1392,7 @@ bool TextAutosizer::FingerprintMapper::Remove(LayoutObject* layout_object) {
         superclusters_.find(fingerprint);
 
     if (supercluster_iter != superclusters_.end()) {
-      Supercluster* supercluster = supercluster_iter->value.get();
+      Supercluster* supercluster = supercluster_iter->value;
       potentially_inconsistent_superclusters_.erase(supercluster);
       superclusters_.erase(supercluster_iter);
     }
@@ -1521,7 +1529,7 @@ float TextAutosizer::ComputeAutosizedFontSize(float computed_size,
 }
 
 void TextAutosizer::CheckSuperclusterConsistency() {
-  HashSet<Supercluster*>& potentially_inconsistent_superclusters =
+  HeapHashSet<Member<Supercluster>>& potentially_inconsistent_superclusters =
       fingerprint_mapper_.GetPotentiallyInconsistentSuperclusters();
   if (potentially_inconsistent_superclusters.IsEmpty())
     return;
@@ -1557,11 +1565,18 @@ void TextAutosizer::CheckSuperclusterConsistency() {
 void TextAutosizer::Trace(Visitor* visitor) const {
   visitor->Trace(document_);
   visitor->Trace(first_block_to_begin_layout_);
+#if DCHECK_IS_ON()
+  visitor->Trace(blocks_that_have_begun_layout_);
+#endif
+  visitor->Trace(cluster_stack_);
   visitor->Trace(fingerprint_mapper_);
 }
 
 void TextAutosizer::FingerprintMapper::Trace(Visitor* visitor) const {
   visitor->Trace(fingerprints_);
+  visitor->Trace(blocks_for_fingerprint_);
+  visitor->Trace(superclusters_);
+  visitor->Trace(potentially_inconsistent_superclusters_);
 }
 
 }  // namespace blink
