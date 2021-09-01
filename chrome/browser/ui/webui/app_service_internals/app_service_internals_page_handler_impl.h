6ff1885f69d46caab0882b0fe51e8f1e66469a4a
Tim Sergeant <tsergeant@chromium.org>
2021-09-01 02:08:07 +0000

    [App Service] Add preferred apps information to app-service-internals
    This displays the information contained in the PreferredAppsList on the
    chrome://app-service-internals page. This information is hard to find
    elsewhere ("view" intents will be shown in App Management but not other
    types of intents which might eventually be used for things like file
    handling), which makes it a good candidate for displaying on the
    internals page.
    
    Bug: 1227531
    Change-Id: Id32dfe41710c72a178dc91b635c44de4d057b67f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3127465
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917080}

--- chrome/browser/ui/webui/app_service_internals/app_service_internals_page_handler_impl.h
+++ chrome/browser/ui/webui/app_service_internals/app_service_internals_page_handler_impl.h
@@ -21,6 +21,7 @@ class AppServiceInternalsPageHandlerImpl
 
   // mojom::app_service_internals::AppServiceInternalsPageHandler:
   void GetApps(GetAppsCallback callback) override;
+  void GetPreferredApps(GetPreferredAppsCallback callback) override;
 
  private:
   Profile* profile_;
