14512bc84e5085a261cdbf217ae31ffeb910f725
Sylvain Defresne <sdefresne@chromium.org>
2021-09-01 13:27:04 +0000

    [ios] Expose `voice_search` API as free function
    Add a free function exposing the `voice_search` API with the goal
    to replace `VoiceSearchProviderProvider` class. Do not convert the
    code to use the functions as it has no internal implementation
    yet.
    
    This is part of a multi-CL change:
     => https://crrev.com/c/3133123
      - https://crrev.com/i/4095470
      - https://crrev.com/c/3132986
      - https://crrev.com/i/4095471
      - https://crrev.com/c/3133083
    
    Bug: 1201182
    Change-Id: I54b9b15558e4c659dfc2582ca3b0c062c7d14e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133123
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917215}

---
+++ ios/public/provider/chrome/browser/voice_search/voice_search_api.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_VOICE_SEARCH_VOICE_SEARCH_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_VOICE_SEARCH_VOICE_SEARCH_API_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/voice/voice_search_language.h"
+#import "ios/public/provider/chrome/browser/voice_search/voice_search_controller.h"
+
+class Browser;
+
+namespace ios {
+namespace provider {
+
+// Returns whether Voice Search is enabled.
+bool IsVoiceSearchEnabled();
+
+// Returns the list of available Voice Search languages.
+NSArray<VoiceSearchLanguage*>* GetAvailableLanguages();
+
+// Creates a new VoiceSearchController instance.
+id<VoiceSearchController> CreateVoiceSearchController(Browser* browser);
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_VOICE_SEARCH_VOICE_SEARCH_API_H_
