e7738bb43fed7cf4ad8e31ab2d5957ed3c628d2b
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-09-01 23:25:26 +0000

    [CrOS Bluetooth] Move fake_bluetooth_config to cr_components/*
    Bug: 1010321
    Change-Id: Ia0638eba78386213a99bfcca75f69762096c7c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133584
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#917435}

--- chrome/test/data/webui/settings/chromeos/os_bluetooth_device_battery_info_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_bluetooth_device_battery_info_tests.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // #import {flush, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {assertTrue} from '../../../chai_assert.js';
-// #import {createDefaultBluetoothDevice} from './fake_bluetooth_config.m.js';
+// #import {createDefaultBluetoothDevice} from 'chrome://test/cr_components/chromeos/bluetooth/fake_bluetooth_config.js';
 // clang-format on
 
 suite('OsBluetoothDeviceBatteryInfoTest', function() {
--- chrome/test/data/webui/settings/chromeos/os_paired_bluetooth_list_item_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_paired_bluetooth_list_item_tests.js
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {flush, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {assertTrue, assertEquals} from '../../../chai_assert.js';
 // #import {eventToPromise} from 'chrome://test/test_util.m.js';
-// #import {createDefaultBluetoothDevice} from './fake_bluetooth_config.m.js';
+// #import {createDefaultBluetoothDevice} from 'chrome://test/cr_components/chromeos/bluetooth/fake_bluetooth_config.js';
 // #import {Router, Route, routes} from 'chrome://os-settings/chromeos/os_settings.js';
 // clang-format on
 
