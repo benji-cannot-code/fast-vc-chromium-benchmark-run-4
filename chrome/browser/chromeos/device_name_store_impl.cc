ff1612940f0170e53691e8a54f0fbf17926c477e
jahchuen <ahchuen@google.com>
2021-09-01 19:46:51 +0000

    [CrOS Device Name] Fetch owner privileges properly after oobe
    This CL fixes a bug whereby the user's owner privileges would not be
    loaded properly right after oobe. We now observe ProfileManager instead
    of UserManager in DeviceNameStoreImpl and use a callback function to
    update the device name state once the owner privileges are loaded for
    the current user.
    
    Bug: 126802
    Change-Id: I4dc0ad0183ab951e371031863b7cfb4f8d706010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3129981
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jason Ah Chuen <ahchuen@google.com>
    Cr-Commit-Position: refs/heads/main@{#917346}

--- chrome/browser/chromeos/device_name_store_impl.cc
+++ chrome/browser/chromeos/device_name_store_impl.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/device_name_store_impl.h"
 
+#include "chrome/browser/ash/ownership/owner_settings_service_ash.h"
+#include "chrome/browser/ash/ownership/owner_settings_service_ash_factory.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/device_name_applier_impl.h"
 #include "chrome/browser/chromeos/device_name_validator.h"
 #include "chrome/common/pref_names.h"
@@ -32,7 +35,9 @@ DeviceNameStoreImpl::DeviceNameStoreImpl(
       handler_(handler),
       device_name_applier_(std::move(device_name_applier)) {
   policy_handler_observation_.Observe(handler_);
-  user_manager_observation_.Observe(user_manager::UserManager::Get());
+
+  if (g_browser_process->profile_manager())
+    profile_manager_observer_.Observe(g_browser_process->profile_manager());
 
   // Gets the device name according to the device name policy set. If empty, the
   // name in prefs is not set yet and hence we set it to the default name.
@@ -79,7 +84,7 @@ bool DeviceNameStoreImpl::CannotModifyBecauseNotDeviceOwner() const {
 
   // If device is unmanaged, then user has to be the device owner to be able to
   // change the name.
-  if (user_manager::UserManager::Get()->IsCurrentUserOwner())
+  if (is_user_owner_)
     return false;
 
   return true;
@@ -161,9 +166,33 @@ void DeviceNameStoreImpl::OnHostnamePolicyChanged() {
   AttemptDeviceNameUpdate(ComputeDeviceName());
 }
 
-void DeviceNameStoreImpl::ActiveUserChanged(user_manager::User* active_user) {
-  device_name_state_ = ComputeDeviceNameState();
+void DeviceNameStoreImpl::AttemptDeviceNameStateUpdate(bool is_user_owner) {
+  is_user_owner_ = is_user_owner;
+  DeviceNameStore::DeviceNameState new_state = ComputeDeviceNameState();
+  if (device_name_state_ == new_state)
+    return;
+
+  device_name_state_ = new_state;
   NotifyDeviceNameMetadataChanged();
+}
+
+void DeviceNameStoreImpl::OnProfileAdded(Profile* profile) {
+  DCHECK(profile);
+
+  ash::OwnerSettingsServiceAsh* service =
+      ash::OwnerSettingsServiceAshFactory::GetForBrowserContext(profile);
+  if (service) {
+    service->IsOwnerAsync(
+        base::BindOnce(&DeviceNameStoreImpl::AttemptDeviceNameStateUpdate,
+                       weak_factory_.GetWeakPtr()));
+  } else {
+    VLOG(1) << "Owner settings service unavailable for added profile, will "
+               "not update device name state.";
+  }
+}
+
+void DeviceNameStoreImpl::OnProfileManagerDestroying() {
+  profile_manager_observer_.Reset();
 }
 
 }  // namespace chromeos
