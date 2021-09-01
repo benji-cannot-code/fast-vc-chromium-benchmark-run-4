823e37f19c1f3909d1a63fbc1fbab38a84c29d89
Hiroki Nakagawa <nhiroki@chromium.org>
2021-09-01 06:39:26 +0000

    Revert "Convert ExtensionMsg_UpdateBrowserWindowId to mojo"
    This reverts commit b382797c811519688a8daa4cf406a28da56db888.
    
    Reason for revert:
    Speculative revert. This could be failing the following tests on the
    Win7 Tests (dbg) bot.
    
    - ExtensionApiTabTest.GetViewsOfCreatedWindow
    - ExtensionApiTest.GetViews
    - ExtensionApiTabTest.GetViewsOfCreatedPopup
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win7%20Tests%20(dbg)(1)/90572/overview
    
    Original change's description:
    > Convert ExtensionMsg_UpdateBrowserWindowId to mojo
    >
    > Bug: 1215550
    > Change-Id: I3cac79615da9a145ad4a8a13893a668863b4f2b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3102545
    > Commit-Queue: Carlos IL <carlosil@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Emily Stark <estark@chromium.org>
    > Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#916693}
    
    Bug: 1215550
    Change-Id: Ib0c719271f706cd9b8bf9ec22f3abb832d5fd1cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135073
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917134}

--- extensions/browser/extension_web_contents_observer.cc
+++ extensions/browser/extension_web_contents_observer.cc
@@ -36,9 +36,8 @@ namespace extensions {
 // static
 ExtensionWebContentsObserver* ExtensionWebContentsObserver::GetForWebContents(
     content::WebContents* web_contents) {
-  auto* client = ExtensionsBrowserClient::Get();
-  return client ? client->GetExtensionWebContentsObserver(web_contents)
-                : nullptr;
+  return ExtensionsBrowserClient::Get()->GetExtensionWebContentsObserver(
+      web_contents);
 }
 
 // static
