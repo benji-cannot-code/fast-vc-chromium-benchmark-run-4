e75b7058323856f97505d1f2cf44380a3247f20c
Olivier Li <olivierli@chromium.org>
2021-09-01 14:28:53 +0000

    Disable HangWatcher in RenderProcessHostTest.KeepAliveRendererProcess_Hung
    It's been seen making HangWatcher changes flake on the CQ:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win%207%20Tests%20x64%20(1)/79981/overview
    
    Since RenderProcessHostTest.* is unrelated to HangWatcher there
    is not really any gain to have it run at the same time anyway.
    
    Bug: 3068019
    Change-Id: I2984445349a4353f663f557a67ca2103105041f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3130240
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917233}

--- content/browser/renderer_host/render_process_host_browsertest.cc
+++ content/browser/renderer_host/render_process_host_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/test/test_timeouts.h"
+#include "base/threading/hang_watcher.h"
 #include "build/build_config.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
@@ -1118,6 +1119,10 @@ IN_PROC_BROWSER_TEST_F(RenderProcessHostTest,
 #endif
 IN_PROC_BROWSER_TEST_F(RenderProcessHostTest,
                        MAYBE_KeepAliveRendererProcess_Hung) {
+  // Disable HangWatcher so it doesn't interfere with this test when hangs take
+  // place.
+  base::HangWatcher::StopMonitoringForTesting();
+
   // The test assumes that the render process exits after 1 second. But this
   // will be prevented if the process still hosts a bfcached page. So disable
   // BFCache for this test.
@@ -1170,6 +1175,10 @@ IN_PROC_BROWSER_TEST_F(RenderProcessHostTest,
 #endif
 IN_PROC_BROWSER_TEST_F(RenderProcessHostTest,
                        MAYBE_FetchKeepAliveRendererProcess_Hung) {
+  // Disable HangWatcher so it doesn't interfere with this test when hangs take
+  // place.
+  base::HangWatcher::StopMonitoringForTesting();
+
   // The test assumes that the render process exits after 1 second. But this
   // will be prevented if the process still hosts a bfcached page. So disable
   // BFCache for this test.
