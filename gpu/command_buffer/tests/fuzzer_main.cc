2cace5469f20bb6d561efa38f840a817eccf5dc2
Maksim Sisov <msisov@igalia.com>
2021-09-02 11:46:20 +0000

    fixup! ozone: fix libfuzzer initialization.
    It turned out Ozone initialization was lost in the nested
    ifdefs. Place this into the beginning so that every ozone platform
    has chance to initialize.
    
    Bug: 1243209, 1245905
    Change-Id: I813fe8eb023cc276661ae413186ac9d63edbce8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138796
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#917620}

--- gpu/command_buffer/tests/fuzzer_main.cc
+++ gpu/command_buffer/tests/fuzzer_main.cc
@@ -333,6 +333,11 @@ class CommandBufferSetup {
     auto* command_line = base::CommandLine::ForCurrentProcess();
     ALLOW_UNUSED_LOCAL(command_line);
 
+#if defined(USE_OZONE)
+    if (features::IsUsingOzonePlatform())
+      ui::OzonePlatform::InitializeForGPU(ui::OzonePlatform::InitParams());
+#endif
+
 #if defined(GPU_FUZZER_USE_ANGLE)
     command_line->AppendSwitchASCII(switches::kUseGL,
                                     gl::kGLImplementationANGLEName);
@@ -342,10 +347,6 @@ class CommandBufferSetup {
 #else
     command_line->AppendSwitchASCII(switches::kUseANGLE,
                                     gl::kANGLEImplementationNullName);
-#endif
-#if defined(USE_OZONE)
-    if (features::IsUsingOzonePlatform())
-      ui::OzonePlatform::InitializeForGPU(ui::OzonePlatform::InitParams());
 #endif
 
     CHECK(gl::init::InitializeStaticGLBindingsImplementation(
