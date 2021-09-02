605c74da76236e46c7a793f034841ba05e6cf0b1
Adrienne Walker <enne@chromium.org>
2021-09-02 23:20:15 +0000

    appcache: remove/disable appcache web tests
    Chromium-only tests are deleted (along with virtual test suites).
    WPT tests are added to NeverFixTests and are disabled.
    
    This is an exact reland of:
    https://chromium-review.googlesource.com/c/chromium/src/+/3116859
    
    This was reverted because of fuzzer references in:
    https://chromium-review.googlesource.com/c/chromium/src/+/3116859
    
    Fuzzer references removed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/3136794
    
    Bug: 582750,835943,940797,1218726,901056
    Change-Id: Ia27c3b1c36d28acd817e114bb9584f1025c340f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140550
    Auto-Submit: enne <enne@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917874}

--- third_party/blink/web_tests/http/tests/devtools/appcache/resources/with-versioned-manifest.php
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<html manifest="versioned-manifest.php">
-<head>
-</head>
-<body>
-Page with versioned manifest
-</body>
-</html>
