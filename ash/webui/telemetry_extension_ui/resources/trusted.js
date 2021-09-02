cdfa4f527c8ae1fb8342369d8e206973ab965b1a
Oleh Lamzin <lamzin@google.com>
2021-09-02 11:04:16 +0000

    [Telemetry] Add memory routine to Mojo services
    Add memory routine to Mojo services to be used for Chrome OS telemetry
    extensions.
    
    Bug: b:188897824
    Change-Id: Ia08d4946d0200c7bf1e6d04ba92f9332cd820060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3127704
    Reviewed-by: Mahmoud Gawad <mgawad@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#917612}

--- ash/webui/telemetry_extension_ui/resources/trusted.js
+++ ash/webui/telemetry_extension_ui/resources/trusted.js
@@ -77,6 +77,7 @@ class DiagnosticsProxy {
       [routineEnum.kPrimeSearch, 'prime-search'],
       [routineEnum.kBatteryDischarge, 'battery-discharge'],
       [routineEnum.kBatteryCharge, 'battery-charge'],
+      [routineEnum.kMemory, 'memory'],
     ]);
 
     if (this.enumToRoutineName_.size !== routineEnum.MAX_VALUE + 1) {
--- ash/webui/telemetry_extension_ui/services/diagnostics_service.h
+++ ash/webui/telemetry_extension_ui/services/diagnostics_service.h
@@ -76,6 +76,7 @@ class DiagnosticsService : public health::mojom::DiagnosticsService {
       uint32_t length_seconds,
       uint32_t minimum_charge_percent_required,
       RunBatteryChargeRoutineCallback callback) override;
+  void RunMemoryRoutine(RunMemoryRoutineCallback callback) override;
 
   // Pointer to real implementation.
   mojo::Remote<cros_healthd::mojom::CrosHealthdDiagnosticsService> service_;
