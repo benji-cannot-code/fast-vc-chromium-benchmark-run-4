30c47a8686407d7b7db3673f1938510c8ceb9246
Salvador Guerrero <salg@google.com>
2021-09-02 01:15:49 +0000

    Implement bytecode rewriter for trace event injection in Android Views
    Trace events are useful to diagnose performance issues, but they require
    instrumentation code to be inserted in performance-critical methods. As
    part of an effort to simplify native Android UI performance measurements
    we designed a bytecode rewriter that automatically adds instrumentation
    calls to all Android Views (more details on
    go/clank-java-tracing-annotation).
    
    This CL contains a tool that rewrites Java classes to find Android Views
    and wraps their draw, onMeasure and onLayout methods in TraceEvent.begin
    and TraceEvent.end calls.
    
    A follow-up CL contains the build changes that enables this tool on all
    Android libraries.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/3116421
    
    Change-Id: I192eacd3a104fe3fe9e108ffa0244d94cf3036d3
    Bug: 1186449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2708628
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Salvador Guerrero <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#917482}

---
+++ build/android/bytecode/java/org/chromium/bytecode/MethodDescription.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.bytecode;
+
+class MethodDescription {
+    public final String methodName;
+    public final String description;
+    public final int access;
+    public Boolean shouldCreateOverride;
+
+    public MethodDescription(String methodName, String description, int access) {
+        this.methodName = methodName;
+        this.description = description;
+        this.access = access;
+        // A null value means we haven't checked the method.
+        this.shouldCreateOverride = null;
+    }
+}
