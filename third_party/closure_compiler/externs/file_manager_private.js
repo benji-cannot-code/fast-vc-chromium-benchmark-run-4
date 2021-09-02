dc5d202ceca6db84d56b87055cd33776b89240e8
Nina Satragno <nsatragno@chromium.org>
2021-09-02 16:44:10 +0000

    Revert "Files SWA: Add private API to open new window"
    This reverts commit 57ca23d6d784b4a72262eeeb89d15304723187da.
    
    Reason for revert: making tests timeout crbug.com/1246023
    
    Bug: 1246023
    
    Original change's description:
    > Files SWA: Add private API to open new window
    >
    > Add a new method `openWindow()` to fileManagerPrivate to allow users to
    > open a new window for SWA.
    >
    > Change the UI code to call this API for the SWA.
    >
    > Integration tests coming in follow up CL, since it requires deeper
    > changes to be able to handle multiple windows.
    >
    > Test: Manually tested using linux-chromeos.
    > Bug: 194150318
    > Change-Id: Ica6eb91f558c070565b622416471d705b80e415c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3128983
    > Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Bo Majewski <majewski@chromium.org>
    > Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#917565}
    
    Bug: 194150318
    Change-Id: Ic574a3f6b853068bc2141f7b7fd7beb515500763
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138442
    Owners-Override: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917700}

--- third_party/closure_compiler/externs/file_manager_private.js
+++ third_party/closure_compiler/externs/file_manager_private.js
@@ -626,14 +626,6 @@ chrome.fileManagerPrivate.HoldingSpaceState;
 
 /**
  * @typedef {{
- *   currentDirectoryURL: (string|undefined),
- *   selectionURL: (string|undefined)
- * }}
- */
-chrome.fileManagerPrivate.OpenWindowParams;
-
-/**
- * @typedef {{
  *   volumeId: string,
  *   writable: boolean,
  * }}
@@ -1277,14 +1269,6 @@ chrome.fileManagerPrivate.isTabletModeEnabled = function(callback) {};
  * @param {chrome.fileManagerPrivate.DriveDialogResult} result
  */
 chrome.fileManagerPrivate.notifyDriveDialogResult = function(result) {};
-
-/**
- * Creates a new Files app window in the directory provided in `params`.
- * @param {!chrome.fileManagerPrivate.OpenWindowParams} params
- * @param {function(boolean): void} callback |result| Boolean result returned by
- *     the invoked function.
- */
-chrome.fileManagerPrivate.openWindow = function(params, callback) {};
 
 /** @type {!ChromeEvent} */
 chrome.fileManagerPrivate.onMountCompleted;
