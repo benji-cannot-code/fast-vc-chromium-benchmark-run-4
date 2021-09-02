c9ae8b52e3bc72f25288b69c8284e65c1ca01cda
Clark DuVall <cduvall@chromium.org>
2021-09-02 00:57:26 +0000

    [Code Health] Remove ListValue::GetSize in remaining directories
    This was an automated change done with a slightly modified copy of
    //tools/clang/value_cleanup.
    
    The larger top level directories were split into separate CLs.
    
    Bug: 1187064
    Change-Id: I74397e01c8a30ad066f72a32e4634ce2dc7d4764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138756
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917476}

--- ios/chrome/browser/ui/settings/block_popups_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/block_popups_table_view_controller.mm
@@ -135,7 +135,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   }
 
   if ([self popupsCurrentlyBlocked] &&
-      (_exceptions.GetSize() || _allowPopupsByPolicy.GetSize())) {
+      (_exceptions.GetList().size() || _allowPopupsByPolicy.GetList().size())) {
     [self populateExceptionsItems];
   }
 }
@@ -145,7 +145,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
 }
 
 - (BOOL)editButtonEnabled {
-  return _exceptions.GetSize() > 0;
+  return _exceptions.GetList().size() > 0;
 }
 
 // Override.
@@ -220,7 +220,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   [self deleteItemAtIndexPaths:@[ indexPath ]];
   if (![self.tableViewModel
           hasSectionForSectionIdentifier:SectionIdentifierExceptions] ||
-      !_exceptions.GetSize()) {
+      !_exceptions.GetList().size()) {
     self.navigationItem.rightBarButtonItem.enabled = NO;
   }
 }
@@ -388,7 +388,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   [model setHeader:header forSectionWithIdentifier:SectionIdentifierExceptions];
 
   // Populate the exception items set by the user.
-  for (size_t i = 0; i < _exceptions.GetSize(); ++i) {
+  for (size_t i = 0; i < _exceptions.GetList().size(); ++i) {
     std::string allowed_url;
     _exceptions.GetString(i, &allowed_url);
     TableViewDetailTextItem* item =
@@ -398,7 +398,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   }
 
   // Populate the exception items set by the policy.
-  for (size_t l = 0; l < _allowPopupsByPolicy.GetSize(); ++l) {
+  for (size_t l = 0; l < _allowPopupsByPolicy.GetList().size(); ++l) {
     std::string allowed_url_by_policy;
     _allowPopupsByPolicy.GetString(l, &allowed_url_by_policy);
     TableViewDetailTextItem* item = [[TableViewDetailTextItem alloc]
@@ -409,7 +409,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
 }
 
 - (void)layoutSections:(BOOL)blockPopupsIsOn {
-  BOOL hasExceptions = _exceptions.GetSize() || _allowPopupsByPolicy.GetSize();
+  BOOL hasExceptions =
+      _exceptions.GetList().size() || _allowPopupsByPolicy.GetList().size();
   BOOL exceptionsListShown = [self.tableViewModel
       hasSectionForSectionIdentifier:SectionIdentifierExceptions];
 
--- ios/components/webui/sync_internals/sync_internals_message_handler.mm
+++ ios/components/webui/sync_internals/sync_internals_message_handler.mm
@@ -149,7 +149,7 @@ void SyncInternalsMessageHandler::HandleRequestIncludeSpecificsInitialState(
 
 void SyncInternalsMessageHandler::HandleGetAllNodes(
     const base::ListValue* args) {
-  DCHECK_EQ(1U, args->GetSize());
+  DCHECK_EQ(1U, args->GetList().size());
   std::string callback_id;
   bool success = args->GetString(0, &callback_id);
   DCHECK(success);
@@ -164,13 +164,13 @@ void SyncInternalsMessageHandler::HandleGetAllNodes(
 
 void SyncInternalsMessageHandler::HandleSetIncludeSpecifics(
     const base::ListValue* args) {
-  DCHECK_EQ(1U, args->GetSize());
+  DCHECK_EQ(1U, args->GetList().size());
   include_specifics_ = args->GetList()[0].GetBool();
 }
 
 void SyncInternalsMessageHandler::HandleRequestStart(
     const base::ListValue* args) {
-  DCHECK_EQ(0U, args->GetSize());
+  DCHECK_EQ(0U, args->GetList().size());
 
   syncer::SyncService* service = GetSyncService();
   if (!service) {
@@ -187,7 +187,7 @@ void SyncInternalsMessageHandler::HandleRequestStart(
 
 void SyncInternalsMessageHandler::HandleRequestStopKeepData(
     const base::ListValue* args) {
-  DCHECK_EQ(0U, args->GetSize());
+  DCHECK_EQ(0U, args->GetList().size());
 
   syncer::SyncService* service = GetSyncService();
   if (!service) {
@@ -199,7 +199,7 @@ void SyncInternalsMessageHandler::HandleRequestStopKeepData(
 
 void SyncInternalsMessageHandler::HandleRequestStopClearData(
     const base::ListValue* args) {
-  DCHECK_EQ(0U, args->GetSize());
+  DCHECK_EQ(0U, args->GetList().size());
 
   syncer::SyncService* service = GetSyncService();
   if (!service) {
