7cc16bae6c6933eeea459abed073652d7fc5b4ee
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-09-02 16:45:14 +0000

    Reuse reporting code for scanning request reporting metadata
    This simplifies the logic in ConnectorsService to get a ClientMetadata
    object by reusing the logic in reporting_util. Since this is done by
    implementing a proto version of reporting::GetContext, assertions are
    added to tests so that the 2 implementations are known to return the
    same thing.
    
    Bug: b/194493854
    Change-Id: I0ac3fd8e41a3230cff9cfab212b2227e8141e53d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131081
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917702}

--- chrome/browser/profiles/reporting_util.h
+++ chrome/browser/profiles/reporting_util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PROFILES_REPORTING_UTIL_H_
 #define CHROME_BROWSER_PROFILES_REPORTING_UTIL_H_
 
+#include "components/enterprise/common/proto/connectors.pb.h"
+
 class Profile;
 
 namespace base {
@@ -18,6 +20,11 @@ namespace reporting {
 // returns corresponding info to a Device, Browser and Profile protos defined in
 // google3/google/internal/chrome/reporting/v1/chromereporting.proto.
 base::Value GetContext(Profile* profile);
+
+// Fetches the same information as GetContext, but in a protobuf instead of a
+// Value.
+enterprise_connectors::ClientMetadata GetContextAsClientMetadata(
+    Profile* profile);
 
 }  // namespace reporting
 
