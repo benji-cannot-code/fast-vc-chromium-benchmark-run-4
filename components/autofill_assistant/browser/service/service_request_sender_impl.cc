209626516b9f89fd9018da6f4ccf9ac09f56ec10
sandromaggi <sandromaggi@google.com>
2021-09-02 14:28:25 +0000

    [Autofill Assistant] Log Server URL in DEBUG
    Bug: None
    Change-Id: If683defaa8b5a89fe7098dad79e1ffb011d0a87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139911
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Auto-Submit: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#917659}

--- components/autofill_assistant/browser/service/service_request_sender_impl.cc
+++ components/autofill_assistant/browser/service/service_request_sender_impl.cc
@@ -73,7 +73,7 @@ void SendRequestImpl(
   auto loader =
       loader_factory->CreateLoader(std::move(request), kTrafficAnnotation);
   loader->AttachStringForUpload(request_body, "application/x-protobuffer");
-#ifdef DEBUG
+#ifndef NDEBUG
   loader->SetAllowHttpErrorResults(true);
 #endif
   auto* const loader_ptr = loader.get();
@@ -107,7 +107,11 @@ void SendRequestNoAuth(
   add_key.SetQueryStr(query_str);
   GURL modified_url = url.ReplaceComponents(add_key);
 
+#ifdef NDEBUG
   VLOG(2) << "Sending request with api key to backend";
+#else
+  VLOG(2) << "Sending request with api key to backend: " << modified_url;
+#endif
   SendRequestImpl(CreateResourceRequest(modified_url), request_body, context,
                   loader_factory, std::move(callback));
 }
@@ -194,7 +198,11 @@ void ServiceRequestSenderImpl::SendRequestAuth(const GURL& url,
                               weak_ptr_factory_.GetWeakPtr(), url, access_token,
                               request_body, std::move(callback));
   }
+#ifdef NDEBUG
   VLOG(2) << "Sending request with access token to backend";
+#else
+  VLOG(2) << "Sending request with access token to backend: " << url;
+#endif
   SendRequestImpl(std::move(resource_request), request_body, context_,
                   loader_factory_.get(), std::move(callback));
 }
