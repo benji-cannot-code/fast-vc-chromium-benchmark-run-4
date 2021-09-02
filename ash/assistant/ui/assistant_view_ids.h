96501bf9aec4e4bb11f42f46d47381fb6c6fe10c
Yuki Awano <yawano@google.com>
2021-09-02 08:51:55 +0000

    Make AssistantOnboardingSuggestionView dark and light mode aware
    Bug: 1176919
    Change-Id: I8ee46bda24e1a66e2bb15f4de6b29abd9b2d8dba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3109294
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#917585}

--- ash/assistant/ui/assistant_view_ids.h
+++ ash/assistant/ui/assistant_view_ids.h
@@ -44,7 +44,10 @@ enum AssistantViewID {
   kLowConfidenceLabel,
 
   // UIs in UiElementContainer
-  kOverflowIndicator
+  kOverflowIndicator,
+
+  // UIs in AssistantOnboardingSuggestionView
+  kAssistantOnboardingSuggestionViewLabel
 };
 
 }  // namespace ash
