216827dce9be20b76a6d9dd43111e19b0cebfc84
Jiewei Qian <qjw@chromium.org>
2021-09-02 06:20:35 +0000

    Reland "sample-swa: Adds example Mojo interface to the Sample System Web App"
    This is a reland of a71e674fdfe9c052576070d87c45a07c3ccfb358
    
    The revert was caused by generate_grd("build_mojo_grdp)" using "//" as
    root of input_files_base_dir instead of using "$root_build_dir".
    
    Original change's description:
    > sample-swa: Adds example Mojo interface to the Sample System Web App
    >
    > Based on: https://crrev.com/c/2538862
    >
    > This example shows how to:
    >
    >   - Get a value from the browser process.
    >   - Set a value on the browser process.
    >   - Request the browser process to do something.
    >   - Have the browser notify the page about an event
    >
    > Change-Id: Ibcee09b7258a48a5c404a40d04f887f76bd1155a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2960339
    > Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Reviewed-by: calamity <calamity@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Bo Majewski <majewski@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#910696}
    
    Change-Id: Ibf18de3fc2cded4aa76adcc7923c749256263fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135193
    Auto-Submit: Jiewei Qian  <qjw@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917554}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -247,6 +247,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH) && !defined(OFFICIAL_BUILD)
+#include "ash/webui/sample_system_web_app_ui/mojom/sample_system_web_app_ui.mojom.h"
+#include "ash/webui/sample_system_web_app_ui/sample_system_web_app_ui.h"
 #include "ash/webui/telemetry_extension_ui/mojom/diagnostics_service.mojom.h"  // nogncheck crbug.com/1125897
 #include "ash/webui/telemetry_extension_ui/mojom/probe_service.mojom.h"  // nogncheck crbug.com/1125897
 #include "ash/webui/telemetry_extension_ui/mojom/system_events_service.mojom.h"  // nogncheck crbug.com/1125897
@@ -945,6 +947,10 @@ void PopulateChromeWebUIFrameBinders(
         chromeos::mojom::demo_mode::PageHandlerFactory,
         chromeos::DemoModeAppUI>(map);
   }
+
+  RegisterWebUIControllerInterfaceBinder<
+      ash::mojom::sample_swa::PageHandlerFactory, ash::SampleSystemWebAppUI>(
+      map);
 
   if (base::FeatureList::IsEnabled(chromeos::features::kTelemetryExtension)) {
     RegisterWebUIControllerInterfaceBinder<
