a1c264c2206ccde3a7aa008d2737c6059d1916b3
Maggie Cai <mxcai@google.com>
2021-09-02 05:56:45 +0000

    [Lacros] Add launch interface for Lacros app controller.
    This CL adds the launch interface for Lacros crospi api to allow sending
    launch command to Lacros to launch app.
    
    BUG=1144877
    
    Change-Id: I3cf70c16303094deddb2fb3f6158e573aaf8db08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3121876
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917548}

--- chrome/browser/lacros/lacros_extension_apps_controller.cc
+++ chrome/browser/lacros/lacros_extension_apps_controller.cc
@@ -137,3 +137,9 @@ void LacrosExtensionAppsController::SetWindowMode(
   // standalone window.
   NOTIMPLEMENTED();
 }
+
+void LacrosExtensionAppsController::Launch(
+    crosapi::mojom::LaunchParamsPtr launch_params,
+    LaunchCallback callback) {
+  NOTIMPLEMENTED();
+}
