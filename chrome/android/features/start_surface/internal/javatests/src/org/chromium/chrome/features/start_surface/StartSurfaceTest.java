a8c99b7d16ecd0f8dae10de9e8bf2c50d69182b3
Xi Han <hanxi@google.com>
2021-09-02 16:50:39 +0000

    [Start] Add two new variations.
    Candidate A:
    1. Thin last tab card
    2. Always have reduced Most Visited tiles from two row to one row
    3. No Home button in Tab Switcher
    4. ‘New tab’ leads to Start surface
    
    Candidate B:
    1. Thin last tab card
    2. Always have reduced Most Visited tiles from two row to one row
    3. No Home button in Tab Switcher
    4. “New Tab” opens old New Tab Page after startup
    
    Tab count button is shown on the Start surface for both. A new Finch
    parameter is added to control the showing of Tab count button.
    
    Bug: 1245985
    Change-Id: I9bb90c15a5099c814f94cbd4537051477f289d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138597
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917708}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -1526,7 +1526,8 @@ public class StartSurfaceTest {
     @Feature({"StartSurface"})
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single/exclude_mv_tiles/false"
-            + "/new_home_surface_from_home_button/hide_mv_tiles_and_tab_switcher"})
+            + "/new_home_surface_from_home_button/hide_mv_tiles_and_tab_switcher"
+            + "/tab_count_button_on_start_surface/true"})
     public void testNewSurfaceFromHomeButton(){
         // clang-format on
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
@@ -1560,7 +1561,8 @@ public class StartSurfaceTest {
     @Feature({"StartSurface"})
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single/exclude_mv_tiles/false"
-            + "/new_home_surface_from_home_button/hide_tab_switcher_only"})
+            + "/new_home_surface_from_home_button/hide_tab_switcher_only"
+            + "/tab_count_button_on_start_surface/true"})
     public void testNewSurfaceHideTabOnlyFromHomeButton() {
         // clang-format on
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
