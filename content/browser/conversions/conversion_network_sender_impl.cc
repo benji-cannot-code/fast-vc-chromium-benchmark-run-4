5e67c60f935764381b12f5c4cb604cae989cd340
Andrew Paseltiner <apaseltiner@chromium.org>
2021-09-02 22:44:47 +0000

    [attribution_reporting] Use transient IsolationInfo for conversion reports
    This avoids any kind of state persistence at the network layer that
    could be used to link reports together.
    
    Bug: 1246007
    Change-Id: I779a9d67c75dbf20461db08df1e9a9b23ff5e35b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140491
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917860}

--- content/browser/conversions/conversion_network_sender_impl.cc
+++ content/browser/conversions/conversion_network_sender_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/conversions/conversion_report.h"
 #include "content/browser/conversions/sent_report_info.h"
 #include "content/public/browser/storage_partition.h"
+#include "net/base/isolation_info.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
 #include "net/base/schemeful_site.h"
@@ -91,6 +92,9 @@ void ConversionNetworkSenderImpl::SendReport(ConversionReport report,
   resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
   resource_request->load_flags =
       net::LOAD_DISABLE_CACHE | net::LOAD_BYPASS_CACHE;
+  resource_request->trusted_params = network::ResourceRequest::TrustedParams();
+  resource_request->trusted_params->isolation_info =
+      net::IsolationInfo::CreateTransient();
 
   // TODO(https://crbug.com/1058018): Update the "policy" field in the traffic
   // annotation when a setting to disable the API is properly
