874510d21fb79edb8d603a90d2587f7892260aca
Sinan Sahin <sinansahin@google.com>
2021-09-02 18:49:00 +0000

    [GMNext] Refactor Theme.BrowserUI to have base color overlay attributes
    This CL adds a base theme that includes all the required color
    attributes that we otherwise apply using an overlay. This will make it
    possible to use the theme without needing to apply an extra overlay.
    
    The next step is to remove the color overlay application from places
    where to context already has a theme that inherits from Theme.BrowserUI.
    
    Bug: 1236789
    Change-Id: I4c36b47c48592f90117397df7fb585bb949329c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139074
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#917764}

--- components/infobars/android/java/src/org/chromium/components/infobars/InfoBarControlLayoutTest.java
+++ components/infobars/android/java/src/org/chromium/components/infobars/InfoBarControlLayoutTest.java
@@ -40,7 +40,7 @@ public class InfoBarControlLayoutTest {
     @Before
     public void setUp() {
         mContext = InstrumentationRegistry.getTargetContext();
-        mContext.setTheme(R.style.Theme_BrowserUI);
+        mContext.setTheme(R.style.Theme_BrowserUI_DayNight);
     }
 
     /**
