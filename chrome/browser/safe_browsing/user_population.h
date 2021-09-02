27c7d5eb119a558a5a5fbc14549e3a4896b31333
Colin Blundell <blundell@chromium.org>
2021-09-02 17:28:16 +0000

    [Safe Browsing] Rename //chrome's GetUserPopulation(Profile*)
    //weblayer currently inlines a forked version of //chrome's
    GetUserPopulation(Profile*) in a couple places. I plan to instead have
    it reuse this code by pulling it down into //components, taking in all
    the values that it currently obtains internally from //chrome's Profile.
    
    When I do that, I will leave a //chrome wrapper that takes in the
    Profile for ergonomics (there are ~20 callsites in //chrome). This CL
    prepares for that structure by renaming the existing method (which will
    stay in place as the wrapper) to GetUserPopulationForProfile().
    
    Bug: 1226567
    Change-Id: Iea5c1d54c228c371e8d6028986f59e41c3a3f995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135377
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917729}

--- chrome/browser/safe_browsing/user_population.h
+++ chrome/browser/safe_browsing/user_population.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace safe_browsing {
 
 // Creates a ChromeUserPopulation proto for the given |profile|.
-ChromeUserPopulation GetUserPopulation(Profile* profile);
+ChromeUserPopulation GetUserPopulationForProfile(Profile* profile);
 
 }  // namespace safe_browsing
 
