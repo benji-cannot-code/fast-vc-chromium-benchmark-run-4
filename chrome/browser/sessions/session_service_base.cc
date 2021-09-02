531eec95d2fcc30e970a5413609ff3fbcdfbeec0
Scott Violet <sky@chromium.org>
2021-09-02 17:38:30 +0000

    sessions: add debugging code to help identify loss of data
    Some ChromeOS logs indicate that SessionService is getting created
    multiple times. That the SessionService is created multiple times
    isn't itself a problem, what is a problem is if it's recreated at a
    time we don't expect (perhaps shutdown), so that the session data ends
    up getting clobbered. This adds logging and crash-without-dump to
    help track down what is happening.
    
    This also moves logging of the start event to SessionService. This is
    done as StartupBrowserCreator isn't always created in some situations
    (namely chromeos startup).
    
    BUG=1245816
    TEST=none
    
    Change-Id: I6331c6ca0b67982f5796420ba303b7031351aaca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139174
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917739}

--- chrome/browser/sessions/session_service_base.cc
+++ chrome/browser/sessions/session_service_base.cc
@@ -643,6 +643,7 @@ void SessionServiceBase::ScheduleCommand(
       !is_closing_command) {
     ScheduleResetCommands();
   }
+  DidScheduleCommand();
 }
 
 bool SessionServiceBase::ShouldTrackChangesToWindow(
