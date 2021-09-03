8d2259585eb63a53845b47b144b1fe9d0b53b52e
Pi-Hsun Shih <pihsun@chromium.org>
2021-09-03 09:17:22 +0000

    Mojo: Generate better types for JS module interface types
    Currently parameters and return types of methods on an interface is
    generated by param_type_in_js_module, which only generates "number" for
    enum types, and doesn't generate correct nullability.
    
    Change it to use the similar way of non-JS module counterpart
    (interface_definition.tmpl), and use type_in_js_module_with_nullability
    instead. Also remove several methods that are no longer used in
    mojom_js_generator.py.
    
    Also fix an error caught by having more strict nullability.
    
    Bug: 1004256,b:198116882
    Change-Id: I187101e36e9ed871de114b8241097481bebca5c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3132741
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918026}

--- chrome/browser/resources/chromeos/audio/input_page.ts
+++ chrome/browser/resources/chromeos/audio/input_page.ts
@@ -50,7 +50,7 @@ export class InputPage extends Page {
   updateActiveInputDevice() {
     const handler = AudioBroker.getInstance().handler;
     handler.getActiveInputDeviceName().then(({deviceName}) => {
-      $('active-input').innerHTML = deviceName;
+      $('active-input').innerHTML = deviceName ?? 'No active input device';
     });
   }
 
