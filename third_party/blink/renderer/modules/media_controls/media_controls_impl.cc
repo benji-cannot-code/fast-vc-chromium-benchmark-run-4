a95e57d339292db687ff6ea212cf639a64ba5e7f
Yiren Wang <yrw@chromium.org>
2021-09-03 17:33:54 +0000

    Hide the buffering spinner when the video is playing
    We should hide the spinner when the ready state gets to HAVE_FUTURE_DATA
    or HAVA_ENOUGH_DATA, so that we will not see the spinner over playing
    videos.
    
    Bug: 1221358
    Change-Id: Id24d53466295728cf7d9b09ec12fa9ee401f7488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140631
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918159}

--- third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
+++ third_party/blink/renderer/modules/media_controls/media_controls_impl.cc
@@ -856,7 +856,7 @@ MediaControlsImpl::ControlsState MediaControlsImpl::State() const {
                                        : ControlsState::kLoadingMetadataPlaying;
       }
       if (!MediaElement().paused() &&
-          ready_state != HTMLMediaElement::kHaveEnoughData) {
+          ready_state < HTMLMediaElement::kHaveFutureData) {
         return ControlsState::kBuffering;
       }
       break;
