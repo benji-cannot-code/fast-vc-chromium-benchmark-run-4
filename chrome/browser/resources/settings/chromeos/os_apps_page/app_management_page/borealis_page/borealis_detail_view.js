cae8a10664545262a2e50534c486258e08c02698
Jeevan Shikaram <jshikaram@chromium.org>
2021-09-03 05:03:16 +0000

    [App Management] Convert Borealis page to Polymer3.
    This CL converts the Borealis detail view page to Polymer3.
    
    Bug: 1180979
    Change-Id: I53876ed22a9f06b0787ee3dc2c5daf35a7a03125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3134537
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917969}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/borealis_page/borealis_detail_view.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/borealis_page/borealis_detail_view.js
@@ -5,11 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const kBorealisMainAppId = 'epfhbkiklgmlkhfpbcdleadnhcfdjfmo';
 
+import {Polymer, html} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import '../icons.m.js';
+import '../permission_item.m.js';
+import '../pin_to_shelf_item.m.js';
+import '../shared_style.m.js';
+import '//resources/cr_elements/icons.m.js';
+
+import {AppManagementStoreClient} from '../store_client.m.js';
+import {getSelectedApp} from '../util.m.js';
+import {routes} from '../../../os_route.m.js';
+import {Router} from '../../../../router.js';
+
 Polymer({
+  _template: html`{__html_template__}`,
   is: 'app-management-borealis-detail-view',
 
   behaviors: [
-    app_management.AppManagementStoreClient,
+    AppManagementStoreClient,
   ],
 
   properties: {
@@ -22,7 +36,7 @@ Polymer({
   attached() {
     // When the state is changed, get the new selected app and assign it to
     // |app_|
-    this.watch('app_', state => app_management.util.getSelectedApp(state));
+    this.watch('app_', state => getSelectedApp(state));
     this.updateFromStore();
   },
 
