e3910a26698afe1d9a2b43ec45e599f3ac06d61c
Maksim Sisov <msisov@igalia.com>
2021-09-03 05:24:55 +0000

    ozone: fix parent-child chain in DWTHPlatform
    It's possible that Ozone uses params.context as a parent window
    if it's set instead of params.parent, which depends on
    OzonePlatform::PlatformProperty::set_parent_for_non_top_level_windows.
    
    DWTHPlatform used to be storing itself as a child only if params.parent
    was set, which is invalid now, and could lead to a potentially undesired
    behaviour when a parent is destroyed before its children.
    
    Thus, to fix that, add a helper method that would return a
    parent DWTHPlatform (either from params.parent or params.context), which
    can be then used to make parent-child relationship.
    
    Bug: 1078328
    Change-Id: I54acfc9339cd99b069e8db5cacbc2a8ecdc5ab2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3117826
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#917979}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_platform.h
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_platform.h
@@ -155,6 +155,8 @@ class VIEWS_EXPORT DesktopWindowTreeHostPlatform
  private:
   FRIEND_TEST_ALL_PREFIXES(DesktopWindowTreeHostPlatformTest,
                            UpdateWindowShapeFromWindowMask);
+  FRIEND_TEST_ALL_PREFIXES(DesktopWindowTreeHostPlatformTest,
+                           MakesParentChildRelationship);
 
   void ScheduleRelayout();
 
