4a628d964e42f08ef61a1db65bdd5054c970ae31
Daniel Hosseinian <dhoss@chromium.org>
2021-09-03 21:44:16 +0000

    [printing] Filter non-job attributes from CUPS printer options
    crrev.com/782198 enabled passing of printer options to job settings, but
    that caused trouble for some users when non-job attributes were being
    passed as well. Those extraneous attributes are passed by CUPS, so they
    should be filtered.
    
    Bug: 1156941
    Change-Id: I5d2729974813a8b1f7f7b9f1d34c4d4b7e749cdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137654
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918253}

--- printing/print_settings_conversion.cc
+++ printing/print_settings_conversion.cc
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/containers/contains.h"
+#include "base/containers/fixed_flat_set.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -222,8 +225,13 @@ std::unique_ptr<PrintSettings> PrintSettingsFromJobSettings(
   const base::Value* advanced_settings =
       job_settings.FindDictKey(kSettingAdvancedSettings);
   if (advanced_settings) {
-    for (const auto item : advanced_settings->DictItems())
-      settings->advanced_settings().emplace(item.first, item.second.Clone());
+    for (const auto item : advanced_settings->DictItems()) {
+      static constexpr auto kNonJobAttributes =
+          base::MakeFixedFlatSet<base::StringPiece>(
+              {"printer-info", "printer-make-and-model", "system_driverinfo"});
+      if (!base::Contains(kNonJobAttributes, item.first))
+        settings->advanced_settings().emplace(item.first, item.second.Clone());
+    }
   }
 #endif  // defined(OS_CHROMEOS) || (defined(OS_LINUX) && defined(USE_CUPS))
 
--- printing/print_settings_conversion_unittest.cc
+++ printing/print_settings_conversion_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "printing/print_settings_conversion.h"
 
+#include "base/containers/contains.h"
 #include "base/json/json_reader.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -93,5 +94,28 @@ TEST(PrintSettingsConversionTest, ConversionTest_DontSendUsername) {
   EXPECT_EQ("", settings->username());
 }
 #endif
+
+#if defined(OS_CHROMEOS) || (defined(OS_LINUX) && defined(USE_CUPS))
+TEST(PrintSettingsConversionTest, FilterNonJobSettings) {
+  absl::optional<base::Value> value = base::JSONReader::Read(kPrinterSettings);
+  ASSERT_TRUE(value.has_value());
+
+  {
+    base::Value advanced_attributes(base::Value::Type::DICTIONARY);
+    advanced_attributes.SetStringKey("printer-info", "yada");
+    advanced_attributes.SetStringKey("printer-make-and-model", "yada");
+    advanced_attributes.SetStringKey("system_driverinfo", "yada");
+    advanced_attributes.SetStringKey("Foo", "Bar");
+    value->SetKey(kSettingAdvancedSettings, std::move(advanced_attributes));
+  }
+
+  std::unique_ptr<PrintSettings> settings =
+      PrintSettingsFromJobSettings(value.value());
+  ASSERT_TRUE(settings);
+  EXPECT_EQ(settings->advanced_settings().size(), 1u);
+  ASSERT_TRUE(base::Contains(settings->advanced_settings(), "Foo"));
+  EXPECT_EQ(settings->advanced_settings().at("Foo"), base::Value("Bar"));
+}
+#endif  // defined(OS_CHROMEOS) || (defined(OS_LINUX) && defined(USE_CUPS))
 
 }  // namespace printing
