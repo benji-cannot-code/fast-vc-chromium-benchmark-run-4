937bd660ab13cf3b4e4e49557839a5e9517a6593
Jared Saul <jsaul@google.com>
2021-09-03 00:04:56 +0000

    [AF] Avoid creating different TestBrowserAutofillManager classes
    I noticed that CreditCardAccessoryControllerTest is creating its own
    TestBrowserAutofillManager when it doesn't need to be. Even if it needed
    additional functionality, it would be better to keep it all in one
    class. This also allows the removal of the CreditCardAccessManager
    parameter from BrowserAutofillManager's test-based constructor.
    
    This CL is diffbased off of http://crrev.com/c/3123927, which takes a
    similar approach.
    
    Change-Id: I3f7cc1c3cc5ea1369c3d44d730d7d05cd1a50948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131955
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#917891}

--- chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
+++ chrome/browser/autofill/credit_card_accessory_controller_impl_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/test_autofill_client.h"
 #include "components/autofill/core/browser/test_autofill_driver.h"
+#include "components/autofill/core/browser/test_browser_autofill_manager.h"
 #include "components/autofill/core/browser/test_personal_data_manager.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/autofill/core/common/form_data.h"
@@ -35,34 +36,6 @@ const std::u16string kFirstTwelveDigits = u"411111111111";
 
 namespace autofill {
 namespace {
-
-class TestBrowserAutofillManager : public BrowserAutofillManager {
- public:
-  TestBrowserAutofillManager(
-      AutofillDriver* driver,
-      AutofillClient* client,
-      PersonalDataManager* personal_data,
-      std::unique_ptr<CreditCardAccessManager> cc_access_manager = nullptr)
-      // Force to use the constructor designated for unit test.
-      : BrowserAutofillManager(
-            driver,
-            client,
-            personal_data,
-            "en-US",
-            AutofillManager::DISABLE_AUTOFILL_DOWNLOAD_MANAGER,
-            std::move(cc_access_manager)) {}
-
-  ~TestBrowserAutofillManager() override = default;
-
-  const FormData& last_query_form() const override { return last_form_; }
-
-  void SetLastForm(FormData form) { last_form_ = std::move(form); }
-
- private:
-  FormData last_form_;
-
-  DISALLOW_COPY_AND_ASSIGN(TestBrowserAutofillManager);
-};
 
 AccessorySheetData::Builder CreditCardAccessorySheetDataBuilder() {
   return AccessorySheetData::Builder(
@@ -111,12 +84,11 @@ class CreditCardAccessoryControllerTest
     : public ChromeRenderViewHostTestHarness {
  public:
   CreditCardAccessoryControllerTest()
-      : af_manager_(&mock_af_driver_,
-                    &client_,
-                    &data_manager_,
-                    std::make_unique<TestAccessManager>(&mock_af_driver_,
-                                                        &client_,
-                                                        &data_manager_)) {}
+      : af_manager_(&mock_af_driver_, &client_, &data_manager_) {
+    af_manager_.set_credit_card_access_manager_for_test(
+        std::make_unique<TestAccessManager>(&mock_af_driver_, &client_,
+                                            &data_manager_));
+  }
 
   void SetUp() override {
     scoped_feature_list_.InitWithFeatures(
@@ -152,7 +124,6 @@ class CreditCardAccessoryControllerTest
     form.action = origin;
     form.main_frame_origin = url::Origin::Create(origin);
     client_.set_form_origin(origin);
-    af_manager_.SetLastForm(std::move(form));
   }
 
  protected:
