a8c94298214778da7e253da156934bfef00f569a
Yue Li <updowndota@google.com>
2021-09-03 22:34:54 +0000

    Quick Answers V2: Fix Assistant dependency
    Bug: 1246243
    Test: Manual Test
    Change-Id: Iee8ed4bd3affa3771b8ee03045386ca45c762518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139508
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918276}

--- chrome/browser/ui/ash/assistant/assistant_browser_delegate_impl.cc
+++ chrome/browser/ui/ash/assistant/assistant_browser_delegate_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/assistant/assistant_interface_binder.h"
 #include "ash/public/cpp/assistant/controller/assistant_interaction_controller.h"
@@ -82,8 +83,11 @@ void AssistantBrowserDelegateImpl::MaybeInit(Profile* profile) {
   service_->Init();
 
   assistant_setup_ = std::make_unique<AssistantSetup>();
-  assistant_web_view_factory_ =
-      std::make_unique<AssistantWebViewFactoryImpl>(profile_);
+
+  if (!assistant_web_view_factory_) {
+    assistant_web_view_factory_ =
+        std::make_unique<AssistantWebViewFactoryImpl>(profile_);
+  }
 }
 
 void AssistantBrowserDelegateImpl::MaybeStartAssistantOptInFlow() {
@@ -203,9 +207,19 @@ void AssistantBrowserDelegateImpl::OnUserSessionStarted(bool is_primary_user) {
 
 void AssistantBrowserDelegateImpl::OnAssistantFeatureAllowedChanged(
     chromeos::assistant::AssistantAllowedState allowed_state) {
-  if (allowed_state != chromeos::assistant::AssistantAllowedState::ALLOWED)
-    return;
-
   Profile* profile = ProfileManager::GetActiveUserProfile();
+
+  if (allowed_state != chromeos::assistant::AssistantAllowedState::ALLOWED) {
+    // This is a short term workaround since Quick Answers also use the webview
+    // factory.
+    // TODO(b/198811694): Refactor AssistantWebViewFactoryImpl.
+    if (chromeos::features::IsQuickAnswersV2Enabled() &&
+        !assistant_web_view_factory_) {
+      assistant_web_view_factory_ =
+          std::make_unique<AssistantWebViewFactoryImpl>(profile);
+    }
+    return;
+  }
+
   MaybeInit(profile);
 }
