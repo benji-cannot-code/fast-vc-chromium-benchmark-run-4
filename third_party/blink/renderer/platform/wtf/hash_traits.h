083ce2cd31cc6754f59ac073ddb532e6d9400656
Stephan Hartmann <stha09@googlemail.com>
2021-09-03 21:48:38 +0000

    IWYU: add string.h for memset in hash_traits
    Bug: None
    Change-Id: Ic90032da0307e3e87484300815893fbede154f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141557
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#918258}

--- third_party/blink/renderer/platform/wtf/hash_traits.h
+++ third_party/blink/renderer/platform/wtf/hash_traits.h
@@ -23,10 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_HASH_TRAITS_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_HASH_TRAITS_H_
 
+#include <string.h>
+
 #include <limits>
 #include <memory>
 #include <type_traits>
 #include <utility>
+
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/hash_functions.h"
