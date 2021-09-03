0547a16490a8434421107a9ddb3d1885264ff682
Elad Alon <eladalon@chromium.org>
2021-09-03 17:29:24 +0000

    [Conditional Focus] Use WeakPersistent for MediaDevices reference
    Bug: 1245881
    Change-Id: I032a63ff38cd16da3054fda5a6e8b6387439133f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141555
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Auto-Submit: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918157}

--- third_party/blink/renderer/modules/mediastream/media_devices.h
+++ third_party/blink/renderer/modules/mediastream/media_devices.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/gtest_prod_util.h"
-#include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/mediastream/media_devices.mojom-blink.h"
@@ -149,8 +148,6 @@ class MODULES_EXPORT MediaDevices final
   EnumerateDevicesTestCallback enumerate_devices_test_callback_;
   base::OnceClosure connection_error_test_callback_;
   base::OnceClosure device_change_test_callback_;
-
-  base::WeakPtrFactory<MediaDevices> weak_factory_{this};
 };
 
 }  // namespace blink
