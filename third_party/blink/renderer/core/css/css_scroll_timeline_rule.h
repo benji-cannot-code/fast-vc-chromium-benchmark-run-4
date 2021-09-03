4c67501a849fe42a8429671e91edb2ff08ab7efe
Yuki Yamada <yukiy@chromium.org>
2021-09-03 06:02:36 +0000

    Revert "Integrated Progress based animations with CSS Animations"
    This reverts commit 149290ad09de842c169957a8a773ba9b07e75b50.
    
    Reason for revert: Speculative revert for consistent failure of
    at-scroll-timeline-dynamic.tentative.html on Linux Tests (dbg)(1).
    Blamelist of the first failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/98587/blamelist
    
    Original change's description:
    > Integrated Progress based animations with CSS Animations
    >
    > Timeline duration can now be stored on an AnimationEffect.
    > This was needed since for CSS animations, InertEffects with
    > no owning animation are used until later in the pipeline.
    > Normally the owning animation of an effect is used to gain
    > access to the corresponding timeline which provides the timeline
    > duration needed for timing normalization math, but in some
    > cases the duration was needed before an effect had an owner
    > assigned. As such it is now possible to create an InertEffect
    > and pass in a timeline duration if needed.
    >
    > Added logic for when CSS animations swap from progress based
    > timeline to a time based timeline. This new logic preserves
    > the current progress of the animation even when current times
    > do not match up.
    >
    > I also removed time range from some CSS tests since they are
    > now supported by progress based animations.
    >
    > Bug: 1216655, 1140602
    > Change-Id: I7dde1f117968c6465eb7c4c0f4b8b66d9371776f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3130703
    > Reviewed-by: Kevin Ellis <kevers@chromium.org>
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Jordan Taylor <jortaylo@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#917801}
    
    Bug: 1216655, 1140602
    Change-Id: I5d993ec0ee6de610347916f8b1fd09d0ea10e34f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140514
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yuki Yamada <yukiy@chromium.org>
    Auto-Submit: Yuki Yamada <yukiy@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917986}

--- third_party/blink/renderer/core/css/css_scroll_timeline_rule.h
+++ third_party/blink/renderer/core/css/css_scroll_timeline_rule.h
@@ -29,6 +29,7 @@ class CSSScrollTimelineRule final : public CSSRule {
   String orientation() const;
   String start() const;
   String end() const;
+  String timeRange() const;
 
   void Trace(Visitor*) const override;
 
