099977bd84f09e17c08b622c6d353b786e11f7ef
Colin Blundell <blundell@chromium.org>
2021-09-03 07:58:03 +0000

    [Safe Browsing] Componentize impl of GetUserPopulationForProfile()
    This CL componentizes the bulk of the implementation of //chrome's
    safe_browsing::GetUserPopulationForProfile() method for reuse by
    //weblayer. We do so by expanding the signature of the function to
    explicitly take in all the state that it needs to construct the user
    population proto, rather than just taking in a Profile.
    
    We leave the //chrome-level function as a wrapper for convenience at the
    //chrome level.
    
    In followup CLs we will:
    - Rename the //chrome files to chrome_user_population_helper*
    - Separate the //chrome-level unittest into //components-level and
      //chrome-level parts
    
    Bug: 1226567
    Change-Id: I3ba1c3c3359b4f4028837b4f1e0fe54d7c44e380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139993
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918004}

--- chrome/browser/safe_browsing/user_population.h
+++ chrome/browser/safe_browsing/user_population.h
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
-// Creates a ChromeUserPopulation proto for the given |profile|.
+// A convenience function that creates a ChromeUserPopulation proto for the
+// given |profile|.
 ChromeUserPopulation GetUserPopulationForProfile(Profile* profile);
 
 }  // namespace safe_browsing
