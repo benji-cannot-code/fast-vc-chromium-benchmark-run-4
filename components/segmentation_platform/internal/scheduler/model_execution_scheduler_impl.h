5c8bcf5d472ba451e3712184cfa1e6519a2832ba
Shakti Sahu <shaktisahu@chromium.org>
2021-09-03 05:06:29 +0000

    [segmentation_platform] Fixed segment selector |is_ready|
    Earlier we only wrote valid segment to prefs. Any UNKNOWN result i.e.
    near zero score was skipped. In this CL we are saving the UNKNOWN as
    well so that |is_ready| can identify the state having no data correctly.
    The UNKNOWN is handled at the UI layer to correctly show the button state.
    
    Bug: 1245430
    Change-Id: Ic621a44a0f151529f1922f62518482d325bfb62b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3134552
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917975}

--- components/segmentation_platform/internal/scheduler/model_execution_scheduler_impl.h
+++ components/segmentation_platform/internal/scheduler/model_execution_scheduler_impl.h
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/execution/model_execution_manager.h"
 #include "components/segmentation_platform/internal/execution/model_execution_status.h"
 
+namespace base {
+class Clock;
+}  // namespace base
+
 namespace segmentation_platform {
 
 namespace proto {
@@ -28,7 +32,8 @@ class ModelExecutionSchedulerImpl : public ModelExecutionScheduler {
   ModelExecutionSchedulerImpl(Observer* observer,
                               SegmentInfoDatabase* segment_database,
                               SignalStorageConfig* signal_storage_config,
-                              ModelExecutionManager* model_execution_manager);
+                              ModelExecutionManager* model_execution_manager,
+                              base::Clock* clock);
   ~ModelExecutionSchedulerImpl() override;
 
   // Disallow copy/assign.
@@ -67,6 +72,9 @@ class ModelExecutionSchedulerImpl : public ModelExecutionScheduler {
 
   // The class that executes the models.
   ModelExecutionManager* model_execution_manager_;
+
+  // The time provider.
+  base::Clock* clock_;
 
   // In-flight model execution requests. Will be killed if we get a model
   // update.
--- components/segmentation_platform/internal/scheduler/model_execution_scheduler_unittest.cc
+++ components/segmentation_platform/internal/scheduler/model_execution_scheduler_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/scheduler/model_execution_scheduler_impl.h"
 
 #include "base/run_loop.h"
+#include "base/test/simple_test_clock.h"
 #include "base/test/task_environment.h"
 #include "components/segmentation_platform/internal/database/mock_signal_storage_config.h"
 #include "components/segmentation_platform/internal/database/segment_info_database.h"
@@ -48,13 +49,15 @@ class ModelExecutionSchedulerTest : public testing::Test {
   ~ModelExecutionSchedulerTest() override = default;
 
   void SetUp() override {
+    clock_.SetNow(base::Time::Now());
     segment_database_ = std::make_unique<test::TestSegmentInfoDatabase>();
     model_execution_scheduler_ = std::make_unique<ModelExecutionSchedulerImpl>(
         &observer_, segment_database_.get(), &signal_storage_config_,
-        &model_execution_manager_);
+        &model_execution_manager_, &clock_);
   }
 
   base::test::TaskEnvironment task_environment_;
+  base::SimpleTestClock clock_;
   MockModelExecutionObserver observer_;
   MockSignalStorageConfig signal_storage_config_;
   MockModelExecutionManager model_execution_manager_;
@@ -92,7 +95,7 @@ TEST_F(ModelExecutionSchedulerTest, OnNewModelInfoReady) {
   auto* prediction_result = segment_info->mutable_prediction_result();
   prediction_result->set_result(0.9);
   prediction_result->set_timestamp_us(
-      base::Time::Now().ToDeltaSinceWindowsEpoch().InMicroseconds());
+      clock_.Now().ToDeltaSinceWindowsEpoch().InMicroseconds());
   EXPECT_CALL(model_execution_manager_,
               ExecuteModel(kTestOptimizationTarget, _))
       .Times(0);
@@ -102,7 +105,7 @@ TEST_F(ModelExecutionSchedulerTest, OnNewModelInfoReady) {
 
   // If we have a non-fresh, but not expired result, we SHOULD NOT try to
   // execute the model.
-  base::Time not_expired_timestamp = base::Time::Now() -
+  base::Time not_expired_timestamp = clock_.Now() -
                                      base::TimeDelta::FromDays(1) +
                                      base::TimeDelta::FromHours(1);
   prediction_result->set_result(0.9);
@@ -114,7 +117,7 @@ TEST_F(ModelExecutionSchedulerTest, OnNewModelInfoReady) {
   model_execution_scheduler_->OnNewModelInfoReady(*segment_info);
 
   // If we have an expired result, we SHOULD try to execute the model.
-  base::Time just_expired_timestamp = base::Time::Now() -
+  base::Time just_expired_timestamp = clock_.Now() -
                                       base::TimeDelta::FromDays(1) -
                                       base::TimeDelta::FromHours(1);
   prediction_result->set_result(0.9);
