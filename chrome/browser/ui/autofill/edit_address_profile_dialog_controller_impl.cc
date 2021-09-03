586aa7a28f3ccfeddb994a3da8b9fe6be2fd4886
Mohamed Amir Yosef <mamir@chromium.org>
2021-09-03 00:29:16 +0000

    [Autofill] Fix crash when closing window while address edit dialog open
    More details are in the linked bug.
    
    Bug: 1240884
    Change-Id: If27c7ac4c5030ee8b96bcc77ee73944992c597cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139926
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917903}

--- chrome/browser/ui/autofill/edit_address_profile_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/edit_address_profile_dialog_controller_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/edit_address_profile_dialog_controller_impl.h"
 
 #include "base/stl_util.h"
+#include "chrome/browser/ui/autofill/autofill_bubble_base.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_handler.h"
 #include "chrome/browser/ui/autofill/save_update_address_profile_bubble_controller_impl.h"
 #include "chrome/browser/ui/browser.h"
@@ -26,7 +27,9 @@ EditAddressProfileDialogControllerImpl::EditAddressProfileDialogControllerImpl(
 }
 
 EditAddressProfileDialogControllerImpl::
-    ~EditAddressProfileDialogControllerImpl() = default;
+    ~EditAddressProfileDialogControllerImpl() {
+  HideDialog();
+}
 
 void EditAddressProfileDialogControllerImpl::OfferEdit(
     const AutofillProfile& profile,
@@ -38,8 +41,9 @@ void EditAddressProfileDialogControllerImpl::OfferEdit(
   address_profile_save_prompt_callback_ =
       std::move(address_profile_save_prompt_callback);
   Browser* browser = chrome::FindBrowserWithWebContents(web_contents());
-  browser->window()->GetAutofillBubbleHandler()->ShowEditAddressProfileDialog(
-      web_contents(), this);
+  dialog_view_ = browser->window()
+                     ->GetAutofillBubbleHandler()
+                     ->ShowEditAddressProfileDialog(web_contents(), this);
 }
 
 std::u16string EditAddressProfileDialogControllerImpl::GetWindowTitle() const {
@@ -86,6 +90,18 @@ void EditAddressProfileDialogControllerImpl::OnDialogClosed() {
     OnUserDecision(
         AutofillClient::SaveAddressProfileOfferUserDecision::kIgnored,
         address_profile_to_edit_);
+  }
+  dialog_view_ = nullptr;
+}
+
+void EditAddressProfileDialogControllerImpl::WebContentsDestroyed() {
+  HideDialog();
+}
+
+void EditAddressProfileDialogControllerImpl::HideDialog() {
+  if (dialog_view_) {
+    dialog_view_->Hide();
+    dialog_view_ = nullptr;
   }
 }
 
