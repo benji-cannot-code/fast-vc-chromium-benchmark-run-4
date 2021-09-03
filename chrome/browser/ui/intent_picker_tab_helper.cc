aa376940a4398187d86a908aaf19c0dc81beba10
Eric Willigers <ericwilligers@chromium.org>
2021-09-03 01:39:26 +0000

    Cache app registrar in IntentPickerTabHelper
    IntentPickerTabHelper::OnWebAppWillBeUninstalled() previously called
    WebAppProvider::GetDeprecated() to obtain the WebAppProvider, and thus
    the WebAppRegistrar.
    
    IntentPickerTabHelper's constructor now calls
    WebAppProvider::GetForWebContents(), and caches the WebAppRegistrar
    obtained from the WebAppProvider.
    
    BUG=1239989
    
    Change-Id: I0833af14e68de47e353db54425a98d19a6e9cdb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138318
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#917930}

--- chrome/browser/ui/intent_picker_tab_helper.cc
+++ chrome/browser/ui/intent_picker_tab_helper.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "content/public/browser/navigation_handle.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/models/image_model.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/image/image.h"
@@ -42,6 +43,14 @@ apps::mojom::AppType GetAppType(apps::PickerEntryType picker_entry_type) {
   return app_type;
 }
 
+web_app::WebAppRegistrar* MaybeGetWebAppRegistrar(
+    content::WebContents* web_contents) {
+  // Profile for web contents might not contain a web app provider. eg. kiosk
+  // profile in Chrome OS.
+  auto* provider = web_app::WebAppProvider::GetForWebContents(web_contents);
+  return provider ? &provider->registrar() : nullptr;
+}
+
 }  // namespace
 
 IntentPickerTabHelper::~IntentPickerTabHelper() = default;
@@ -61,15 +70,10 @@ void IntentPickerTabHelper::SetShouldShowIcon(
 }
 
 IntentPickerTabHelper::IntentPickerTabHelper(content::WebContents* web_contents)
-    : content::WebContentsObserver(web_contents) {
-  Profile* profile =
-      Profile::FromBrowserContext(web_contents->GetBrowserContext());
-  auto* provider = web_app::WebAppProvider::GetForLocalAppsUnchecked(profile);
-
-  // Profile might not contain a web app provider. eg. kiosk profile in
-  // Chrome OS.
-  if (provider)
-    registrar_observation_.Observe(&provider->registrar());
+    : content::WebContentsObserver(web_contents),
+      registrar_(MaybeGetWebAppRegistrar(web_contents)) {
+  if (registrar_)
+    registrar_observation_.Observe(registrar_);
 }
 
 // static
@@ -148,15 +152,10 @@ void IntentPickerTabHelper::DidFinishNavigation(
 
 void IntentPickerTabHelper::OnWebAppWillBeUninstalled(
     const web_app::AppId& app_id) {
-  Profile* profile =
-      Profile::FromBrowserContext(web_contents()->GetBrowserContext());
-
   // WebAppTabHelper has an app_id but it is reset during
   // OnWebAppWillBeUninstalled so using FindAppWithUrlInScope.
-  auto local_app_id =
-      web_app::WebAppProvider::GetDeprecated(profile)
-          ->registrar()
-          .FindAppWithUrlInScope(web_contents()->GetLastCommittedURL());
+  absl::optional<web_app::AppId> local_app_id =
+      registrar_->FindAppWithUrlInScope(web_contents()->GetLastCommittedURL());
   if (app_id == local_app_id)
     SetShouldShowIcon(web_contents(), false);
 }
--- chrome/browser/ui/intent_picker_tab_helper.h
+++ chrome/browser/ui/intent_picker_tab_helper.h
@@ -63,6 +63,8 @@ class IntentPickerTabHelper
   void OnWebAppWillBeUninstalled(const web_app::AppId& app_id) override;
   void OnAppRegistrarDestroyed() override;
 
+  web_app::WebAppRegistrar* const registrar_;
+
   bool should_show_icon_ = false;
 
   base::ScopedObservation<web_app::WebAppRegistrar,
