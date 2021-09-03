9930675747174c88a1a6795dcf778e854ce96e9e
Xiaocheng Hu <xiaochengh@chromium.org>
2021-09-03 17:08:57 +0000

    [@layer] Invalidate style for layer changes
    There isn't much to invalidate for layer changes, since
    InvalidateForRuleSetChanges() should already mark style dirty on all
    affected elements.
    
    This patch also invalidates MatchedPropertiesCache so that it doesn't
    store any MatchedResult with stale layer order numbers.
    
    Bug: 1095765
    Change-Id: Idf8e6cbb2d35245bd6ca644e797b473de58aa526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138303
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918147}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -1552,7 +1552,8 @@ void StyleEngine::ApplyUserRuleSetChanges(
     user_cascade_layer_map_ =
         MakeGarbageCollected<CascadeLayerMap>(new_style_sheets);
 
-    // TODO(crbug.com/1095765): Invalidate user style.
+    if (resolver_)
+      resolver_->InvalidateMatchedPropertiesCache();
   }
 
   if (changed_rule_flags & (kPropertyRules | kScrollTimelineRules)) {
@@ -1669,7 +1670,8 @@ void StyleEngine::ApplyRuleSetChanges(
       // sub-layer can precede an original layer in the final ordering.
       resolver.RebuildCascadeLayerMap(new_style_sheets);
 
-      // TODO(crbug.com/1095765): Invalidate style.
+      if (resolver_)
+        resolver_->InvalidateMatchedPropertiesCache();
     }
 
     resolver.AppendActiveStyleSheets(append_start_index, new_style_sheets);
