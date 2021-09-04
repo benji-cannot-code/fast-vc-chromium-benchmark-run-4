7b646640141919a0e086f7b0c4371153cbe02f51
Andrey Kosyakov <caseq@chromium.org>
2021-09-04 07:09:48 +0000

    DevTools: add an integration test for reloading of a SW-based extension
    Related front-end change: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3115448
    
    Bug: 1213714, 1159283
    Change-Id: I4d27fa1909c34fe574a00b89f4aaa72405087c7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115848
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918356}

--- chrome/test/data/devtools/extensions/service_worker/service_worker.js
+++ chrome/test/data/devtools/extensions/service_worker/service_worker.js
@@ -2,5 +2,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-console.log('Hello world!');
+console.log('PASS');
 chrome.test.sendMessage('WORKER_ACTIVATED');
