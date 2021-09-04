314ff8f0349b35a99eebf320268a88c52f76caa1
Ali Stanfield <stanfield@google.com>
2021-09-04 00:23:54 +0000

    Enable side panel for Lens image search.
    This change is protected behind a feature param.
    
    Bug: 1234592
    Change-Id: Ibd8c731d678f3533ded0b92ba86d0b8ed99fb2e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139950
    Commit-Queue: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918317}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -3298,7 +3298,8 @@ void RenderViewContextMenu::ExecSearchLensForImage() {
 
   core_tab_helper->SearchWithLensInNewTab(
       render_frame_host, params().src_url,
-      lens::EntryPoint::CHROME_SEARCH_WITH_GOOGLE_LENS_CONTEXT_MENU_ITEM);
+      lens::EntryPoint::CHROME_SEARCH_WITH_GOOGLE_LENS_CONTEXT_MENU_ITEM,
+      lens::features::kEnableSidePanelForLensImageSearch.Get());
 }
 
 void RenderViewContextMenu::ExecLensRegionSearch() {
