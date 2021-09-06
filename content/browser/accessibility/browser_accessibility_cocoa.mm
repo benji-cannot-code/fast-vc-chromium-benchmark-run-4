9dbced8aec80a5ff14257597794256d6afeb1b32
Alexander Surkov <asurkov@igalia.com>
2021-09-06 15:58:28 +0000

    ax_mac_merge: implement accessibilityLabel on BrowserAccessibilityCocoa
    accessibilityLabel is NSAccessibility method for AXDescription
    attribute. If accessibilityLabel and accessibilityTitle are both
    the same thing (which is currently the case for AXPlatformNodeCocoa),
    then it prevents AXDescription from being computed.
    
    Change-Id: Iaf46901385f178efed64377a9ea7993e8fd11989
    Bug: 1237915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141730
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918604}

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -1203,6 +1203,10 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
   return table;
 }
 
+- (NSString*)AXDescription {
+  return [self descriptionForAccessibility];
+}
+
 - (NSString*)descriptionForAccessibility {
   if (![self instanceActive])
     return nil;
