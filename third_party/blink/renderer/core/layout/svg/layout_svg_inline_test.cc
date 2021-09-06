0fed162b0e7e6aec0c48cf73699a99d9cc8f3c4a
Kent Tamura <tkent@chromium.org>
2021-09-06 06:39:58 +0000

    SVG: Do not allow nested <textPath>
    We have rejected <textPath><textPath></textPath></textPath> in layout
    trees. However we failed to reject <textPath><a><textPath>...
    
    NGSvgTextLayoutAttributesBuilder has an assumption that <textPath> can
    not be nested, and had a crash.
    
    Bug: 1246627
    Change-Id: Ib8ac869ae5301e26a614d5b87ff724e5057458bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140801
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918495}

---
+++ third_party/blink/renderer/core/layout/svg/layout_svg_inline_test.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/layout/svg/layout_svg_inline.h"
+
+#include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
+
+namespace blink {
+
+class LayoutSVGInlineTest : public RenderingTest {};
+
+TEST_F(LayoutSVGInlineTest, IsChildAllowed) {
+  SetBodyInnerHTML(R"HTML(
+<svg>
+<text>
+<textPath><a id="anchor"><textPath />)HTML");
+  GetDocument().UpdateStyleAndLayoutTree();
+
+  auto* a = GetLayoutObjectByElementId("anchor");
+  // The second <textPath> is not added.
+  EXPECT_FALSE(a->SlowFirstChild());
+}
+
+}  // namespace blink
