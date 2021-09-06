6d13fa0edc5f4cd187e982fe3d31b907cc3f54dd
Ossama Mahmoud <osamafathy@google.com>
2021-09-06 14:36:05 +0000

    OOBE: Retire HidDetection histograms
    Retire the following histograms:
    HIDDetection.OOBEDevicesDetectedOnContinuePressed
    HIDDetection.OOBEDialogShown
    
    Bug: 1235402
    Change-Id: Ifc96acbbdc4857255e15f5b8d0c9d4f0ad1f54b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140382
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Ossama Mahmoud <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#918591}

--- chrome/browser/ash/login/screens/hid_detection_screen.cc
+++ chrome/browser/ash/login/screens/hid_detection_screen.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/default_tick_clock.h"
@@ -112,17 +111,6 @@ void HIDDetectionScreen::OverrideInputDeviceManagerBinderForTesting(
 }
 
 void HIDDetectionScreen::OnContinueButtonClicked() {
-  ContinueScenarioType scenario_type;
-  if (!pointing_device_id_.empty() && !keyboard_device_id_.empty())
-    scenario_type = All_DEVICES_DETECTED;
-  else if (pointing_device_id_.empty())
-    scenario_type = KEYBOARD_DEVICE_ONLY_DETECTED;
-  else
-    scenario_type = POINTING_DEVICE_ONLY_DETECTED;
-
-  UMA_HISTOGRAM_ENUMERATION("HIDDetection.OOBEDevicesDetectedOnContinuePressed",
-                            scenario_type, CONTINUE_SCENARIO_TYPE_SIZE);
-
   CleanupOnExit();
   Exit(Result::NEXT);
 }
@@ -586,8 +574,6 @@ void HIDDetectionScreen::OnGetInputDevicesListForCheck(
   // Screen is not required if both devices are present.
   const bool all_devices_autodetected =
       !pointing_device_id.empty() && !keyboard_device_id.empty();
-  UMA_HISTOGRAM_BOOLEAN("HIDDetection.OOBEDialogShown",
-                        !all_devices_autodetected);
 
   std::move(on_check_done).Run(!all_devices_autodetected);
 }
