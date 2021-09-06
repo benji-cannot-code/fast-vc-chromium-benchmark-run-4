8f01b5e9633dcfafd6b41e452d0e328b6d417116
Dan Elphick <delphick@chromium.org>
2021-09-06 14:44:06 +0000

    Add missing v8-script.h include
    Fix v8_inspector_string.h using v8-script.h via a transitive include
    (which prevents removing the v8-script.h include from v8-inspector.h).
    
    Bug: v8:11965
    Change-Id: Iaf95f5dd813a752b1c6e19bb1d3fea764ed5555f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141443
    Auto-Submit: Dan Elphick <delphick@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918593}

--- third_party/blink/renderer/core/inspector/v8_inspector_string.h
+++ third_party/blink/renderer/core/inspector/v8_inspector_string.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/inspector_protocol/crdtp/serializable.h"
 #include "third_party/inspector_protocol/crdtp/serializer_traits.h"
 #include "v8/include/v8-inspector.h"
+#include "v8/include/v8-script.h"
 
 namespace blink {
 
