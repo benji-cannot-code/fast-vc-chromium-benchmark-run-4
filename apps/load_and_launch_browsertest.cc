8562f6b99afcbc42af617ca33d3639f17637d32b
Eric Willigers <ericwilligers@chromium.org>
2021-09-06 00:30:40 +0000

    [apps] Include chromeos_buildflags
    load_and_launch_browsertest.cc uses IS_CHROMEOS_LACROS and now
    includes the relevant header.
    
    Bug: 1246565
    Change-Id: I7c90c70559780bba6d6ce09aad6d55ab6aa8d31a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141315
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918455}

--- apps/load_and_launch_browsertest.cc
+++ apps/load_and_launch_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_timeouts.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/apps/platform_apps/app_browsertest_util.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/load_error_reporter.h"
