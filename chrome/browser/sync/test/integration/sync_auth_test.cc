24b90b2c605aa06bb61b9af02a93363c09d7aa72
Victor Vianna <victorvianna@google.com>
2021-09-06 11:22:58 +0000

    Disable SyncAuthTest.TokenExpiry on Lacros
    Bug: 1245180
    Change-Id: I772ed717b3750a843e568aa1a76f0fe3420067e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141702
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918557}

--- chrome/browser/sync/test/integration/sync_auth_test.cc
+++ chrome/browser/sync/test/integration/sync_auth_test.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/buildflag.h"
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -275,7 +276,13 @@ IN_PROC_BROWSER_TEST_F(SyncAuthTest, RetryInitialSetupWithTransientError) {
 }
 
 // Verify that SyncServiceImpl fetches a new token when an old token expires.
-IN_PROC_BROWSER_TEST_F(SyncAuthTest, TokenExpiry) {
+// TODO(crbug.com/1245180): Flaky on Lacros.
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_TokenExpiry DISABLED_TokenExpiry
+#else
+#define MAYBE_TokenExpiry TokenExpiry
+#endif
+IN_PROC_BROWSER_TEST_F(SyncAuthTest, MAYBE_TokenExpiry) {
   // Initial sync succeeds with a short lived OAuth2 Token.
   ASSERT_TRUE(SetupClients());
   GetFakeServer()->ClearHttpError();
