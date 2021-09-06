a3e6b196c4cd1516c9c9583730da07e9527ac8c8
Joel Hockey <joelhockey@chromium.org>
2021-09-06 09:27:33 +0000

    Do not filter mime types when 'text/uri-list' is a custom type
    Type 'text/uri-list' can be set when either files are available, or if
    it is set as a custom web type.
    
    When files are available, we do not include other types such as
    'text/plain' since it contains the full file path on some platforms
    (http://crbug.com/1214108).
    
    But if 'text/uri-list' is set as a custom web type such as by CrOS
    FilesApp which sets 'text/uri-list' along with others types such as
    'fs/sources', then we should not drop any other types.
    
    Rather than calling ReadAvailableTypes() on the clipboard and checking
    for 'text/uri-list', we can call IsFormatAvailable(FilenamesType()) to
    determine if files are available.
    
    Bug: 1241671
    Change-Id: Ia4cef4480feaa6e444628ed217649ee8eb235815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3136034
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918523}

--- ui/base/clipboard/test/test_clipboard.cc
+++ ui/base/clipboard/test/test_clipboard.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/clipboard/clipboard_constants.h"
 #include "ui/base/clipboard/clipboard_monitor.h"
+#include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 #include "ui/base/data_transfer_policy/data_transfer_policy_controller.h"
 #include "ui/base/ui_base_features.h"
@@ -100,7 +101,8 @@ std::vector<std::u16string> TestClipboard::GetStandardFormats(
     ClipboardBuffer buffer,
     const DataTransferEndpoint* data_dst) const {
   std::vector<std::u16string> types;
-  if (!IsReadAllowed(GetStore(buffer).data_src.get(), data_dst))
+  const DataStore& store = GetStore(buffer);
+  if (!IsReadAllowed(store.data_src.get(), data_dst))
     return types;
 
   if (IsFormatAvailable(ClipboardFormatType::PlainTextType(), buffer,
@@ -129,6 +131,11 @@ std::vector<std::u16string> TestClipboard::GetStandardFormats(
     types.push_back(base::UTF8ToUTF16(kMimeTypePNG));
   if (IsFormatAvailable(ClipboardFormatType::FilenamesType(), buffer, data_dst))
     types.push_back(base::UTF8ToUTF16(kMimeTypeURIList));
+
+  auto it = store.data.find(ClipboardFormatType::WebCustomDataType());
+  if (it != store.data.end())
+    ReadCustomDataTypes(it->second.c_str(), it->second.size(), &types);
+
   return types;
 }
 
@@ -443,6 +450,7 @@ void TestClipboard::DataStore::Clear() {
   url_title.clear();
   html_src_url.clear();
   png.clear();
+  filenames.clear();
   data_src.reset();
 }
 
