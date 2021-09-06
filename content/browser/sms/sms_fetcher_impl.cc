a0f0d745361c5e78457547ebb79549449144a025
Robert Lin <robertlin@chromium.org>
2021-09-06 02:03:30 +0000

    prerender: add DCHECK in WebOTPService::Receive
    This DCHECK is added to make sure that the frame is not prerendering and
    OK to call WebContents::FromRenderFrameHost to go from RenderFrameHost
    to WebContents.
    
    Bug: 1244477
    Change-Id: I63cf80b5362b735e130cf20f481cfbc0c38bdc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3127468
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918461}

--- content/browser/sms/sms_fetcher_impl.cc
+++ content/browser/sms/sms_fetcher_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/sms/sms_parser.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/content_browser_client.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
 
@@ -57,6 +58,14 @@ void SmsFetcherImpl::Subscribe(const OriginList& origin_list,
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(subscriber);
   DCHECK(render_frame_host);
+  // This function cannot get called during prerendering because
+  // WebOTPService::Receive() calls this, but WebOTPService is deferred during
+  // prerendering by MojoBinderPolicyApplier. This DCHECK proves we don't have
+  // to worry about prerendering when using WebContents::FromRenderFrameHost()
+  // below (see function comments for WebContents::FromRenderFrameHost() for
+  // more details).
+  DCHECK_NE(render_frame_host->GetLifecycleState(),
+            RenderFrameHost::LifecycleState::kPrerendering);
   // Should not be called multiple times for the same subscriber.
   DCHECK(!remote_cancel_callbacks_.count(subscriber));
   DCHECK(!subscribers_.HasSubscriber(origin_list, subscriber));
