9991925b5a29f8699330ce327fa3c8db0436383b
Timothy Loh <timloh@chromium.org>
2021-09-07 07:21:45 +0000

    Plugin VM installer: Show network error if download size is smaller than expected
    Normally a failed download should result in DOWNLOAD_FAILED_NETWORK or
    a similar error, but in some cases we only detect this when hashing the
    download. If the hash doesn't match but the downloaded image was smaller
    than expected, show a network error instead of a hash error.
    
    Bug: b/197278759
    Change-Id: I364b5b5848b8ef162e6e4a0a05885d21e4d41dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139735
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/main@{#918703}

--- chrome/browser/ash/plugin_vm/plugin_vm_installer.cc
+++ chrome/browser/ash/plugin_vm/plugin_vm_installer.cc
@@ -207,6 +207,17 @@ void PluginVmInstaller::OnDownloadStarted() {}
 void PluginVmInstaller::OnDownloadProgressUpdated(uint64_t bytes_downloaded,
                                                   int64_t content_length) {
   DCHECK_EQ(installing_state_, InstallingState::kDownloadingImage);
+
+  if (expected_image_size_ == kImageSizeUnknown) {
+    if (content_length > 0) {
+      expected_image_size_ = content_length;
+    }
+  } else if (expected_image_size_ > 0) {
+    if (content_length != expected_image_size_) {
+      expected_image_size_ = kImageSizeError;
+    }
+  }
+
   if (observer_)
     observer_->OnDownloadProgressUpdated(bytes_downloaded, content_length);
 
@@ -227,7 +238,14 @@ void PluginVmInstaller::OnDownloadCompleted(
     downloaded_image_ = downloaded_image_for_testing_.value();
 
   if (!VerifyDownload(info.hash256)) {
-    OnDownloadFailed(FailureReason::HASH_MISMATCH);
+    LOG(ERROR) << "Expected image size: " << expected_image_size_
+               << ", downloaded image size: " << downloaded_image_size_;
+    if (expected_image_size_ == kImageSizeUnknown ||
+        expected_image_size_ == downloaded_image_size_) {
+      OnDownloadFailed(FailureReason::HASH_MISMATCH);
+    } else {
+      OnDownloadFailed(FailureReason::DOWNLOAD_SIZE_MISMATCH);
+    }
     return;
   }
 
@@ -554,6 +572,8 @@ void PluginVmInstaller::StartDownload() {
     return;
   }
 
+  expected_image_size_ = kImageSizeUnknown;
+  downloaded_image_size_ = kImageSizeUnknown;
   absl::optional<std::string> drive_id = GetIdFromDriveUrl(url);
   using_drive_download_service_ = drive_id.has_value();
 
@@ -994,7 +1014,6 @@ void PluginVmInstaller::OnTemporaryImageRemoved(bool success) {
                << downloaded_image_.value() << " failed to be deleted";
     return;
   }
-  downloaded_image_size_ = -1;
   downloaded_image_.clear();
   creating_new_vm_ = false;
 }
