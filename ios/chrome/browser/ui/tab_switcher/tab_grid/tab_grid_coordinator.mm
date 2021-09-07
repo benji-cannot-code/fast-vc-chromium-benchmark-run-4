b6f5c0e3df1eb62179f97b407760d2bd26636a54
Olivier Robin <olivierrobin@google.com>
2021-09-07 08:38:19 +0000

    Add todo: Call TabGridCoordinator setIncognitoBrowser from |start|
    TabGridCoordinator does the same incognito init in start and
    setIncognitoBrowser so it would be cleaner to call setIncognitoBrowser
    from start instead to have duplicate code.
    
    Note that this requires reordering the operations in start as some
    operation require that incognito browser is already init.
    
    Change-Id: I5ebf6bf4e209cb375ea9ca783f25b9e5299d552d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141608
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918719}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -566,6 +566,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeCoordinator
 
 - (void)start {
+  // TODO(crbug.com/1246931): refactor to call setIncognitoBrowser from this
+  // function.
   IncognitoReauthSceneAgent* reauthAgent = [IncognitoReauthSceneAgent
       agentFromScene:SceneStateBrowserAgent::FromBrowser(_incognitoBrowser)
                          ->GetSceneState()];
