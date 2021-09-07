1d68887febe71aad3fe289251296cf84534dbfe6
Nan Lin <linnan@chromium.org>
2021-09-07 20:16:51 +0000

    Use transient IsolationInfo for aggregatable reports
    This avoids any kind of state persistence at the network layer that
    could be used to link reports together.
    
    Bug: 1246399
    Change-Id: I6dbd55ddbe92e1393386822430645fcbfa8b4412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137619
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918916}

--- content/browser/aggregation_service/aggregatable_report_sender.cc
+++ content/browser/aggregation_service/aggregatable_report_sender.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "content/public/browser/storage_partition.h"
+#include "net/base/isolation_info.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_request_headers.h"
@@ -52,6 +53,9 @@ void AggregatableReportSender::SendReport(const GURL& url,
   resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
   resource_request->load_flags =
       net::LOAD_DISABLE_CACHE | net::LOAD_BYPASS_CACHE;
+  resource_request->trusted_params = network::ResourceRequest::TrustedParams();
+  resource_request->trusted_params->isolation_info =
+      net::IsolationInfo::CreateTransient();
 
   // TODO(crbug.com/1238343): Update the "policy" field in the traffic
   // annotation when a setting to disable the API is properly
