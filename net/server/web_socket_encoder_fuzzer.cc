92cdc0d872c79c61db5f1f181856c517e90cf708
Shiho Noda <shihonoda@google.com>
2021-09-07 04:11:16 +0000

    Add a fuzzing test for the decoder function of web socket
    Add a fuzzing test for DecodeFrame() in WebSocketEncoder.
    
    Change-Id: I4777a31b18ae917ae22f8a969585bd83a2a9e707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3143215
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Shiho Noda <shihonoda@google.com>
    Cr-Commit-Position: refs/heads/main@{#918678}

---
+++ net/server/web_socket_encoder_fuzzer.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#include <stddef.h>
+#include <stdint.h>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include <memory>
+#include <string>
+
+#include "net/server/web_socket_encoder.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider fuzzed_data_provider(data, size);
+  auto server = net::WebSocketEncoder::CreateServer();
+  int bytes_consumed;
+  std::string decoded;
+
+  while (fuzzed_data_provider.remaining_bytes() > 0) {
+    size_t chunk_size = fuzzed_data_provider.ConsumeIntegralInRange(1, 125);
+    std::string chunk = fuzzed_data_provider.ConsumeBytesAsString(chunk_size);
+    server->DecodeFrame(chunk, &bytes_consumed, &decoded);
+  }
+  return 0;
+}
