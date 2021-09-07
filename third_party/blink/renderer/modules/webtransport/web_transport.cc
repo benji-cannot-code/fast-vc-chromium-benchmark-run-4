b4e55e4624cf6d26e527bb3ea4afda3f3d9eb9ed
Adam Rice <ricea@chromium.org>
2021-09-07 05:44:26 +0000

    Correctly handle simultaneous reads
    WebTransport datagrams reader would hit a DCHECK if multiple reads were
    started simultaneously while the queue was empty, due to an unexpected
    call to pull(). Fix it.
    
    BUG=1246335
    
    Change-Id: I0329788e5fe30c14d82a53f1eea5685321c7ac8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3143835
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918693}

--- third_party/blink/renderer/modules/webtransport/web_transport.cc
+++ third_party/blink/renderer/modules/webtransport/web_transport.cc
@@ -206,6 +206,14 @@ class WebTransport::DatagramUnderlyingSource final
   // Implementation of UnderlyingSourceBase.
   ScriptPromise pull(ScriptState* script_state) override {
     DVLOG(1) << "DatagramUnderlyingSource::pull()";
+
+    if (waiting_for_datagrams_) {
+      // This can happen if a second read is issued while a read is already
+      // pending.
+      DCHECK(queue_.empty());
+      return ScriptPromise::CastUndefined(script_state);
+    }
+
     // If high water mark is reset to 0 and then read() is called, it should
     // block waiting for a new datagram. So we may need to discard datagrams
     // here.
@@ -219,7 +227,6 @@ class WebTransport::DatagramUnderlyingSource final
         return ScriptPromise::CastUndefined(script_state);
       }
 
-      DCHECK(!waiting_for_datagrams_);
       waiting_for_datagrams_ = true;
       return ScriptPromise::CastUndefined(script_state);
     }
