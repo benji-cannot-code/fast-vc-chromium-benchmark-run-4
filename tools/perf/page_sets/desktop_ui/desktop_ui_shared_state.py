54da7f8336b1684d086f47616c010201f68d3fb4
Ryan Heise <heiserya@google.com>
2021-09-07 21:06:00 +0000

    Use separate browser instances for desktop_ui tests, regardless of platform
    Bug: 1246062
    Change-Id: I10bf02ef60abec59d8e0b41c1bc7daf69ccc355c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145674
    Commit-Queue: Ryan Heise <heiserya@google.com>
    Auto-Submit: Ryan Heise <heiserya@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918945}

---
+++ tools/perf/page_sets/desktop_ui/desktop_ui_shared_state.py
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry.page import shared_page_state
+
+
+class DesktopUISharedState(shared_page_state.SharedPageState):
+  """ Ensures the browser is restarted for each test, for all platforms. """
+
+  def ShouldReuseBrowserForAllStoryRuns(self):
+    return False
