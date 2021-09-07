5e5c731b1fd182de4e2b44ad10a83b6c669c7f1c
Ankit Kumar 🌪️ <ankk@microsoft.com>
2021-09-07 01:39:36 +0000

    [unseasoned-pdf] Enable PdfAccessibilityContextMenuAction
    Enables PdfAccessibilityContextMenuAction browser_test in unseasoned
    mode.
    
    Bug: 1144444, 1229631
    Change-Id: I774266adacbe1b696fce8ec6b906f23f141e60b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3126254
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918661}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -1739,8 +1739,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionTestWithoutUnseasonedOverride,
   EXPECT_EQ(ax::mojom::Role::kRegion, region->data().role);
 }
 
-IN_PROC_BROWSER_TEST_F(PDFExtensionTestWithoutUnseasonedOverride,
-                       PdfAccessibilityContextMenuAction) {
+IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfAccessibilityContextMenuAction) {
   // Validate the context menu arguments for PDF selection when context menu is
   // invoked via accessibility tree.
   const char kExepectedPDFSelection[] =
@@ -1775,7 +1774,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionTestWithoutUnseasonedOverride,
 
   auto context_menu_interceptor =
       std::make_unique<content::ContextMenuInterceptor>(
-          guest_contents->GetMainFrame());
+          GetPluginFrame(guest_contents));
 
   ContextMenuWaiter menu_waiter;
   // Invoke kShowContextMenu accessibility action on the node with the kPdfRoot
