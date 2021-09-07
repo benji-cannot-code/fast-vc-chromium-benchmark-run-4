289e2f92b2ef116f6403481fc0e60abac16525c5
Adrienne Walker <enne@chromium.org>
2021-09-07 21:00:16 +0000

    Window Placement: update visual properties when any screen changes
    Previously, UpdateScreenInfo would only send updates when the current
    screen changed.  Due to the current screen having isExtended, this
    would also support updating after adding a second display or removing
    the second display.
    
    Fix by comparing the entire display lists and not just the current
    display.
    
    Bug: 1246545
    Change-Id: I2b6d937c4af61b228edfc4982f0ca6aeb4e2cc5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140786
    Auto-Submit: enne <enne@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918942}

--- ui/display/display_list.cc
+++ ui/display/display_list.cc
@@ -53,6 +53,10 @@ bool DisplayList::operator==(const DisplayList& other) const {
          current_id_ == other.current_id_;
 }
 
+bool DisplayList::operator!=(const DisplayList& other) const {
+  return !(*this == other);
+}
+
 void DisplayList::AddObserver(DisplayObserver* observer) {
   observers_.AddObserver(observer);
 }
