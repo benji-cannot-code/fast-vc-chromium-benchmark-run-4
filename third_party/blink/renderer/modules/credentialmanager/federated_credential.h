7e53c92096aa92ae2d644a9829e22596e7041f34
Sam Goto <goto@chromium.org>
2021-09-07 08:45:31 +0000

    [webid] Add the API surface for the WebID revocation API
    Use case: https://wicg.github.io/WebID/#rp-revocation
    
    Example: https://wicg.github.io/WebID/#examples-rp-revocation
    Spec: https://wicg.github.io/WebID/#FederatedCredential
    Change-Id: Ifa37cef08c01b7424bb757a470691c2e84930764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135033
    Commit-Queue: Mike West <mkwst@chromium.org>
    Auto-Submit: Sam Goto <goto@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918722}

--- third_party/blink/renderer/modules/credentialmanager/federated_credential.h
+++ third_party/blink/renderer/modules/credentialmanager/federated_credential.h
@@ -62,6 +62,7 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
   }
 
   static ScriptPromise logout(ScriptState*, const Vector<String>&);
+  static ScriptPromise revoke(ScriptState*, const String);
 
  private:
   const scoped_refptr<const SecurityOrigin> provider_;
