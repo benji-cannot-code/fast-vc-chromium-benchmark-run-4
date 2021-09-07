ea7c47b2c3ecb3e18fa7b703686103ce6620a23f
Michael Lippautz <mlippautz@chromium.org>
2021-09-07 12:04:43 +0000

    Blink: Fix compilation of cppgc_enable_object_names
    With `cppgc_enable_object_names = true` GCInfo objects are not merged
    into their base classes but kept as leaves to aid debugging. This
    requires that types are destroyed through their leave classes.
    
    Bug: chromium:1056170
    Change-Id: I13ae76a594d342e2d37739a6fb4251d982f6fb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141722
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918754}

--- third_party/blink/renderer/modules/webcodecs/audio_data.cc
+++ third_party/blink/renderer/modules/webcodecs/audio_data.cc
@@ -204,6 +204,8 @@ AudioData::AudioData(scoped_refptr<media::AudioBuffer> buffer)
     close();
 }
 
+AudioData::~AudioData() = default;
+
 AudioData* AudioData::clone(ExceptionState& exception_state) {
   if (!data_) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
