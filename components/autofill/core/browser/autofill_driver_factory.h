3000d866c3efbce3d489fe8ab79f879024313f17
Christoph Schwering <schwering@google.com>
2021-09-07 09:43:06 +0000

    [Autofill] Fixed circular dependency in driver destruction.
    This CL fixes a circular dependency: ~ContentAutofillDriverFactory()
    destroys ContentAutofillDriverFactory::router_ and afterwards calls
    ~AutofillDriverFactory(), which implicitly deletes all drivers and thus
    calls ~ContentAutofillDriver(), which again refers to
    ContentAutofillDriverFactory::router_.
    
    This CL also eliminates ContentAutofillRouter::Reset() to simplify
    the destruction API.
    
    Bug: 1187842
    Change-Id: I2c904292a771b757db806a24de732768251e4fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137732
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#918733}

--- components/autofill/core/browser/autofill_driver_factory.h
+++ components/autofill/core/browser/autofill_driver_factory.h
@@ -55,6 +55,9 @@ class AutofillDriverFactory {
   // Deletes the AutofillDriver for |key|.
   void DeleteForKey(void* key);
 
+  // Deletes all AutofillDrivers.
+  void DeleteAllAutofillDrivers();
+
  private:
   AutofillClient* const client_;
 
