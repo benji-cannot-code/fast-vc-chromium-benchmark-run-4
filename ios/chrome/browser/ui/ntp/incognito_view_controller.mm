e8470d77248a0ae00178806ba7c711388eb48afe
Side Yilmaz <sideyilmaz@chromium.org>
2021-09-07 13:26:19 +0000

    [IncNTP] Add RevampedIncognitoView and use when flag is enabled.
    This CL add new RevampedIncognitoView for incongito ntp revamp project
    (go/incognito-ntp). New UI will be shown when incognito-ntp-revamp flag
    is enabled. This CL copy-paste the legacy incognito_view, as follow-up
    CLs will update the UI.
    
    Bug: 1240087
    Change-Id: I00a64eb9a58fa6f8668fedc4819811c3ce76f75a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097507
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918774}

--- ios/chrome/browser/ui/ntp/incognito_view_controller.mm
+++ ios/chrome/browser/ui/ntp/incognito_view_controller.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
 #import "ios/chrome/browser/ui/ntp/incognito_view.h"
+#import "ios/chrome/browser/ui/ntp/revamped_incognito_view.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/url_loading/url_loading_browser_agent.h"
@@ -23,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface IncognitoViewController ()
 
 // The scrollview containing the actual views.
-@property(nonatomic, strong) IncognitoView* incognitoView;
+@property(nonatomic, strong) UIScrollView* incognitoView;
 
 @end
 
@@ -43,8 +44,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)viewDidLoad {
   self.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
 
-  self.incognitoView = [[IncognitoView alloc] initWithFrame:self.view.bounds
-                                                  URLLoader:_URLLoader];
+  if (base::FeatureList::IsEnabled(kIncognitoNtpRevamp)) {
+    self.incognitoView =
+        [[RevampedIncognitoView alloc] initWithFrame:self.view.bounds];
+  } else {
+    self.incognitoView = [[IncognitoView alloc] initWithFrame:self.view.bounds
+                                                    URLLoader:_URLLoader];
+  }
+
   [self.incognitoView setAutoresizingMask:UIViewAutoresizingFlexibleHeight |
                                           UIViewAutoresizingFlexibleWidth];
   self.incognitoView.backgroundColor = [UIColor colorNamed:kBackgroundColor];
