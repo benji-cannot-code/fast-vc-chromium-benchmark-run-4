902ea673deb6aed48cdbd393820ef0f2e77163b2
ckitagawa <ckitagawa@chromium.org>
2021-09-07 21:12:21 +0000

    [Zucchini] DEX Version 39 Support
    DEX Version 39 added:
    * const-method-handle containing a method_handle@BBBB reference
    * const-method-type containing a proto@BBBB reference
    
    This CL
    * Updates CodeToProtoId for const-method-type
    * Adds CodeToMethodHandle and WriteMethodHandle
    
    Fuzzed about 500k iterations locally and uploaded new samples to
    the clusterfuzz bucket. 97% coverage.
    
    Manually tested on hand-written dex files using smali as well as the
    dexdump test corpus.
    
    Bug: 1231885
    Change-Id: Id8ab09ac8d3331902c5e6f92ac39ebd26d36e79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060660
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918948}

--- components/zucchini/disassembler_dex.h
+++ components/zucchini/disassembler_dex.h
@@ -34,7 +34,7 @@ class DisassemblerDex : public Disassembler {
     kMethodId,
     // kClassDef,  // Unused
     kCallSiteId,
-    // kMethodHandle,  // Unused
+    kMethodHandle,
     kTypeList,
     kAnnotationSetRefList,
     kAnnotionSet,
@@ -83,6 +83,8 @@ class DisassemblerDex : public Disassembler {
     kAnnotationsDirectoryToParameterMethodId,
 
     kCodeToCallSiteId,  // kCallSiteId
+
+    kCodeToMethodHandle,  // kMethodHandle
 
     kProtoIdToParametersTypeList,  // kTypeList
     kClassDefToInterfacesTypeList,
@@ -221,6 +223,8 @@ class DisassemblerDex : public Disassembler {
                                                             offset_t hi);
   std::unique_ptr<ReferenceReader> MakeReadCodeToCallSiteId16(offset_t lo,
                                                               offset_t hi);
+  std::unique_ptr<ReferenceReader> MakeReadCodeToMethodHandle16(offset_t lo,
+                                                                offset_t hi);
   std::unique_ptr<ReferenceReader> MakeReadCodeToRelCode8(offset_t lo,
                                                           offset_t hi);
   std::unique_ptr<ReferenceReader> MakeReadCodeToRelCode16(offset_t lo,
@@ -240,6 +244,8 @@ class DisassemblerDex : public Disassembler {
   std::unique_ptr<ReferenceWriter> MakeWriteMethodId16(MutableBufferView image);
   std::unique_ptr<ReferenceWriter> MakeWriteMethodId32(MutableBufferView image);
   std::unique_ptr<ReferenceWriter> MakeWriteCallSiteId16(
+      MutableBufferView image);
+  std::unique_ptr<ReferenceWriter> MakeWriteMethodHandle16(
       MutableBufferView image);
   std::unique_ptr<ReferenceWriter> MakeWriteRelCode8(MutableBufferView image);
   std::unique_ptr<ReferenceWriter> MakeWriteRelCode16(MutableBufferView image);
---
+++ components/zucchini/testdata/const-method-type-min.smali
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Tests const-method-type added in DEX version 39.
+
+# Compile using smali: https://github.com/JesusFreke/smali
+# java -jar smali.jar assemble const-method-type.smali --api 28
+
+.class public LConstMethodTypeTest;
+.super Ljava/lang/Object;
+
+.method public test(I)V
+    .registers 4
+    const-method-type v0, ()I
+    const-method-type v1, (C)V
+    const-method-type v2, (I)V
+    const-method-type v3, (I)I
+    return-void
+.end method
