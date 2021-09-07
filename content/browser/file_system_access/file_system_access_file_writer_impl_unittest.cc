47542659c61a3a663ff45fa516dbb6c0ee4f22ac
Emanuel Krivoy <krivoy@google.com>
2021-09-07 13:08:41 +0000

    Switch to refcounted locks, add shared locks for writable streams
    This CL changes the scope of the WriteLockManager, making it responsible
    for the creation of locks, instead of owning access handle and file
    writer receivers. This simplifies error handling and avoids unnecessary
    IO operations when taking a lock is not possible. This new locks are
    refcounted, when there are no more references to them, their
    destructor releases the lock on the URL.
    
    Creating access handles requires an exclusive lock, which prevent any
    other lock from being taken on the URL. Creating writable streams
    requires a shared lock, which prevent the taking of exclusive locks, but
    allow the taking of shared ones.
    
    Bug: 1218431
    Change-Id: I9c21b901ed0f5f331682c8a6b75db5e28674e5a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3100189
    Commit-Queue: Emanuel Krivoy <krivoy@google.com>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918766}

--- content/browser/file_system_access/file_system_access_file_writer_impl_unittest.cc
+++ content/browser/file_system_access/file_system_access_file_writer_impl_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/gmock_callback_support.h"
 #include "base/test/task_environment.h"
+#include "content/browser/file_system_access/file_system_access_write_lock_manager.h"
 #include "content/browser/file_system_access/fixed_file_system_access_permission_grant.h"
 #include "content/browser/file_system_access/mock_file_system_access_permission_context.h"
 #include "content/public/test/browser_task_environment.h"
@@ -155,10 +156,15 @@ class FileSystemAccessFileWriterImplTest : public testing::Test {
           quarantine_receivers_.Add(&quarantine_, std::move(receiver));
         });
 
+    auto lock = manager_->TakeWriteLock(
+        test_file_url_,
+        FileSystemAccessWriteLockManager::WriteLockType::kShared);
+    ASSERT_TRUE(lock.has_value());
+
     handle_ = manager_->CreateFileWriter(
         FileSystemAccessManagerImpl::BindingContext(kTestStorageKey, kTestURL,
                                                     kFrameId),
-        test_file_url_, test_swap_url_,
+        test_file_url_, test_swap_url_, std::move(lock.value()),
         FileSystemAccessManagerImpl::SharedHandleState(permission_grant_,
                                                        permission_grant_),
         remote_.InitWithNewPipeAndPassReceiver(),
@@ -683,16 +689,20 @@ TEST_F(FileSystemAccessFileWriterAfterWriteChecksTest,
             storage::AsyncFileTestHelper::CreateFile(file_system_context_.get(),
                                                      test_swap_url_));
 
+  auto lock = manager_->TakeWriteLock(
+      test_file_url_, FileSystemAccessWriteLockManager::WriteLockType::kShared);
+  ASSERT_TRUE(lock.has_value());
+
   mojo::PendingRemote<blink::mojom::FileSystemAccessFileWriter> remote;
-  handle_ =
-      manager_->CreateFileWriter(FileSystemAccessManagerImpl::BindingContext(
-                                     kTestStorageKey, kTestURL, kFrameId),
-                                 test_file_url_, test_swap_url_,
-                                 FileSystemAccessManagerImpl::SharedHandleState(
-                                     permission_grant_, permission_grant_),
-                                 remote.InitWithNewPipeAndPassReceiver(),
-                                 /*has_transient_user_activation=*/false,
-                                 /*auto_close=*/false, quarantine_callback_);
+  handle_ = manager_->CreateFileWriter(
+      FileSystemAccessManagerImpl::BindingContext(kTestStorageKey, kTestURL,
+                                                  kFrameId),
+      test_file_url_, test_swap_url_, std::move(lock.value()),
+      FileSystemAccessManagerImpl::SharedHandleState(permission_grant_,
+                                                     permission_grant_),
+      remote.InitWithNewPipeAndPassReceiver(),
+      /*has_transient_user_activation=*/false,
+      /*auto_close=*/false, quarantine_callback_);
 
   uint64_t bytes_written;
   FileSystemAccessStatus result = WriteSync(0, "foo", &bytes_written);
