c3d95e438cf80eb05833709e64bd26cf49fa848d
Joe Downing <joedow@google.com>
2021-09-07 21:34:01 +0000

    Addressing a few DCHECKs that occur during the connection process
    This CL adds a couple of allowances for sync primitives used in and
    around the WebRTC PeerConnection class.  Note that this is related
    to the discussion in crbug.com/793486 (same root cause) however I've
    left that bug as-is because it looks like it is tracking the decision
    on whether to use a separate thread for our WebRTC PeerConnection
    usage.  At this point in time, I think it is reasonable just to
    allow sync primitives in the handful of function calls which require
    them and we can look at splitting threads in the future if there is
    a stronger justification for it.
    
    Bug: 1198501
    Change-Id: Ib63e321fa8fa35a944089bbbe15abc0f7ac82fdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139510
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918953}

--- remoting/protocol/webrtc_transport.cc
+++ remoting/protocol/webrtc_transport.cc
@@ -57,6 +57,9 @@ namespace protocol {
 class ScopedAllowThreadJoinForWebRtcTransport
     : public base::ScopedAllowBaseSyncPrimitivesOutsideBlockingScope {};
 
+class ScopedAllowSyncPrimitivesForWebRtcTransport
+    : public base::ScopedAllowBaseSyncPrimitivesOutsideBlockingScope {};
+
 namespace {
 
 using DataChannelState = webrtc::DataChannelInterface::DataState;
@@ -326,18 +329,28 @@ class WebrtcTransport::PeerConnectionWrapper
   ~PeerConnectionWrapper() override {
     thread_join_watchdog_->Arm();
 
-    // PeerConnection creates threads internally, which are joined when the
-    // connection is closed. See crbug.com/660081.
-    ScopedAllowThreadJoinForWebRtcTransport allow_thread_join;
-    peer_connection_->Close();
-    peer_connection_ = nullptr;
-    peer_connection_factory_ = nullptr;
+    {
+      // |peer_connection_| creates threads internally, which are joined when
+      // the connection is closed. See crbug.com/660081.
+      ScopedAllowThreadJoinForWebRtcTransport allow_thread_join;
+      peer_connection_->Close();
+      peer_connection_ = nullptr;
+      peer_connection_factory_ = nullptr;
+    }
+
     audio_module_ = nullptr;
 
     if (before_disarm_thread_join_watchdog_callback_) {
       std::move(before_disarm_thread_join_watchdog_callback_).Run();
     }
     thread_join_watchdog_->Disarm();
+
+    {
+      // |thread_join_watchdog_| uses a lock internally so we need to allow sync
+      // primitives when destroying the watchdog.
+      ScopedAllowSyncPrimitivesForWebRtcTransport allow_sync_primitives;
+      thread_join_watchdog_.reset();
+    }
   }
 
   WebrtcAudioModule* audio_module() {
@@ -563,12 +576,15 @@ bool WebrtcTransport::ProcessTransportInfo(XmlElement* transport_info) {
       return false;
     }
 
-    peer_connection()->SetRemoteDescription(
-        SetSessionDescriptionObserver::Create(base::BindOnce(
-            &WebrtcTransport::OnRemoteDescriptionSet,
-            weak_factory_.GetWeakPtr(),
-            type == webrtc::SessionDescriptionInterface::kOffer)),
-        webrtc_session_description.release());
+    {
+      ScopedAllowThreadJoinForWebRtcTransport allow_wait;
+      peer_connection()->SetRemoteDescription(
+          SetSessionDescriptionObserver::Create(base::BindOnce(
+              &WebrtcTransport::OnRemoteDescriptionSet,
+              weak_factory_.GetWeakPtr(),
+              type == webrtc::SessionDescriptionInterface::kOffer)),
+          webrtc_session_description.release());
+    }
 
     // SetRemoteDescription() might overwrite any bitrate caps previously set,
     // so (re)apply them here. This might happen if ICE state were already
@@ -1218,6 +1234,7 @@ void WebrtcTransport::StartRtcEventLogging() {
 void WebrtcTransport::StopRtcEventLogging() {
   DCHECK(thread_checker_.CalledOnValidThread());
   if (peer_connection()) {
+    ScopedAllowThreadJoinForWebRtcTransport allow_wait;
     peer_connection()->StopRtcEventLog();
   }
 }
