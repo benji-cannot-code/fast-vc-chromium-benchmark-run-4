b00d4e0f8418f85784dc93faf5a59c43797058e0
Xing Liu <xingliu@chromium.org>
2021-09-07 19:49:20 +0000

    Price drop: Conditionally track UMA in PriceDropNotificationManager.
    The new code path with local notification tracks UMA internally. This
    CL adds a boolean flag and some comments to inform the reader that
    only Chime code path should call these.
    
    Bug: 1241666
    Change-Id: I7ccfa817f2e0690446d3b06744b6d7f4cfac0adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3124936
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#918896}

--- chrome/browser/commerce/price_tracking/android/javatests/src/org/chromium/chrome/browser/price_tracking/PriceDropNotifierUnitTest.java
+++ chrome/browser/commerce/price_tracking/android/javatests/src/org/chromium/chrome/browser/price_tracking/PriceDropNotifierUnitTest.java
@@ -10,6 +10,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import android.app.PendingIntent;
 import android.graphics.Bitmap;
@@ -45,6 +46,7 @@ import org.chromium.chrome.browser.notifications.NotificationUmaTracker.ActionTy
 import org.chromium.chrome.browser.price_tracking.PriceDropNotifier.ActionData;
 import org.chromium.chrome.browser.price_tracking.PriceDropNotifier.NotificationData;
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxy;
+import org.chromium.components.browser_ui.notifications.NotificationWrapper;
 import org.chromium.components.browser_ui.notifications.NotificationWrapperBuilder;
 import org.chromium.components.browser_ui.notifications.PendingIntentProvider;
 import org.chromium.components.image_fetcher.ImageFetcher;
@@ -105,6 +107,8 @@ public class PriceDropNotifierUnitTest {
     private NotificationManagerProxy mNotificationManagerProxy;
     @Mock
     private ChromeBrowserInitializer mChromeInitializer;
+    @Mock
+    private NotificationWrapper mNotificationWrapper;
 
     @Captor
     ArgumentCaptor<Callback<Bitmap>> mBitmapCallbackCaptor;
@@ -120,6 +124,7 @@ public class PriceDropNotifierUnitTest {
         mPriceDropNotifier = new PriceDropNotifier(ContextUtils.getApplicationContext(),
                 mImageFetcher, mNotificationBuilder, mNotificationManagerProxy);
         ChromeBrowserInitializer.setForTesting(mChromeInitializer);
+        when(mNotificationBuilder.buildNotificationWrapper()).thenReturn(mNotificationWrapper);
     }
 
     @After
