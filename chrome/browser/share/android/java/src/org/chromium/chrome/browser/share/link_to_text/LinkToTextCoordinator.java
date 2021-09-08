14b048e21f43a82621a5dbe9d826f655119bb18c
Cheick Cisse <cheickcisse@google.com>
2021-09-08 15:47:18 +0000

    Add Null checks in LinkToTextCoordinator before requesting canonicalUrl
    Change-Id: Ia72088f9be690ec98de3efec6592449691c052ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145180
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Sophey <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919273}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/link_to_text/LinkToTextCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/link_to_text/LinkToTextCoordinator.java
@@ -18,6 +18,7 @@ import org.chromium.chrome.browser.share.ChromeShareExtras;
 import org.chromium.chrome.browser.share.share_sheet.ChromeOptionShareCallback;
 import org.chromium.chrome.browser.share.share_sheet.ShareSheetLinkToggleCoordinator.LinkToggleState;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
+import org.chromium.chrome.browser.tab.SadTab;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabHidingType;
 import org.chromium.components.browser_ui.share.ShareParams;
@@ -158,6 +159,12 @@ public class LinkToTextCoordinator extends EmptyTabObserver {
     private void reshareHighlightedText() {
         PostTask.postDelayedTask(UiThreadTaskTraits.DEFAULT, () -> timeout(), TIMEOUT_MS);
         setTextFragmentReceiver();
+
+        if (mProducer == null) {
+            onSelectorReady(INVALID_SELECTOR);
+            return;
+        }
+
         mProducer.extractTextFragmentsMatches(
                 new TextFragmentReceiver.ExtractTextFragmentsMatchesResponse() {
                     @Override
@@ -217,6 +224,12 @@ public class LinkToTextCoordinator extends EmptyTabObserver {
         }
 
         setTextFragmentReceiver();
+
+        if (mProducer == null) {
+            onSelectorReady(INVALID_SELECTOR);
+            return;
+        }
+
         mProducer.requestSelector(new TextFragmentReceiver.RequestSelectorResponse() {
             @Override
             public void call(String selector) {
@@ -245,23 +258,43 @@ public class LinkToTextCoordinator extends EmptyTabObserver {
     }
 
     private void requestSelectorForCanonicalUrl() {
-        mTab.getWebContents().getMainFrame().getCanonicalUrlForSharing(new Callback<GURL>() {
-            @Override
-            public void onResult(GURL result) {
-                if (!result.isEmpty()) {
-                    mShareUrl = result.getSpec();
+        if (shouldRequestCanonicalUrl()) {
+            mTab.getWebContents().getMainFrame().getCanonicalUrlForSharing(new Callback<GURL>() {
+                @Override
+                public void onResult(GURL result) {
+                    if (!result.isEmpty()) {
+                        mShareUrl = result.getSpec();
+                    }
+                    requestSelector();
                 }
-                requestSelector();
-            }
-        });
+            });
+        } else {
+            requestSelector();
+        }
+    }
+
+    private boolean shouldRequestCanonicalUrl() {
+        if (mTab.getWebContents() == null) return false;
+        if (mTab.getWebContents().getMainFrame() == null) return false;
+        if (mTab.getUrl().isEmpty()) return false;
+        if (mTab.isShowingErrorPage() || SadTab.isShowing(mTab)) {
+            return false;
+        }
+        return true;
     }
 
     private void setTextFragmentReceiver() {
-        mProducer = mChromeShareExtras.getRenderFrameHost() != null
-                ? mChromeShareExtras.getRenderFrameHost().getInterfaceToRendererFrame(
-                        TextFragmentReceiver.MANAGER)
-                : mTab.getWebContents().getMainFrame().getInterfaceToRendererFrame(
-                        TextFragmentReceiver.MANAGER);
+        if (mChromeShareExtras.getRenderFrameHost() != null
+                && mChromeShareExtras.getRenderFrameHost().isRenderFrameCreated()) {
+            mProducer = mChromeShareExtras.getRenderFrameHost().getInterfaceToRendererFrame(
+                    TextFragmentReceiver.MANAGER);
+            return;
+        }
+
+        if (mTab.getWebContents() != null && mTab.getWebContents().getMainFrame() != null) {
+            mProducer = mTab.getWebContents().getMainFrame().getInterfaceToRendererFrame(
+                    TextFragmentReceiver.MANAGER);
+        }
     }
 
     private void cleanup() {
