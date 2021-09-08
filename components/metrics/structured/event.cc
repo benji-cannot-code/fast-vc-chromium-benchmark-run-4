40967c2aff7e2aeb0cfbc48620632f0d510c4096
Greg Thompson <grt@chromium.org>
2021-09-08 12:44:46 +0000

    Revert "Follow-up CL to clean-up code."
    This reverts commit b5d130030a9133ecdca830fa90033391b15b73c0.
    
    Reason for revert: MemorySanitizer: use-of-uninitialized-value
    See https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/24844/overview
    
    Original change's description:
    > Follow-up CL to clean-up code.
    >
    > Comments were made after commit was merged:
    > https://chromium-review.googlesource.com/c/chromium/src/+/3139554.
    >
    > Change-Id: Ibbcf0fa235c2b4cfa3691ab64eeae48e232a9fed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3144243
    > Reviewed-by: Tony Yeoman <tby@chromium.org>
    > Commit-Queue: Jong Ahn <jongahn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#919019}
    
    Change-Id: Icee1472ada69d3daa6814be5e1079be139d7072c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3148054
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Owners-Override: Greg Thompson <grt@google.com>
    Cr-Commit-Position: refs/heads/main@{#919223}

--- components/metrics/structured/event.cc
+++ components/metrics/structured/event.cc
@@ -27,7 +27,6 @@ bool Event::MetricValue::operator==(const Event::MetricValue& rhs) const {
 
 Event::MetricValue::~MetricValue() = default;
 
-Event::Event() = default;
 Event::Event(const std::string& project_name, const std::string& event_name)
     : project_name_(project_name), event_name_(event_name) {}
 
@@ -46,6 +45,7 @@ Event& Event::operator=(Event&& other) {
   return *this;
 }
 
+Event::Event() = default;
 Event::~Event() = default;
 
 const std::string& Event::project_name() const {
