0702d5783ff17932a8074ac24283c3db81a10bda
Erik Chen <erikchen@chromium.org>
2021-09-08 19:47:18 +0000

    Lacros: Basic ash-side plumbing for shelf integration.
    This CL should have no functional effect for users since all behavior is
    gated behind a lacros-side Finch flag that defaults to off.
    
    This CL creates a new class ChromeAppWindowTrackerAsh. It's responsible
    for: listening to the creation of new windows, and launching of chrome
    app instances. It muxes the data streams and creates instances of
    StandaloneBrowserExtensionAppShelfItemController as necessary.
    
    StandaloneBrowserExtensionAppShelfItemController is responsible for
    setting properties on the corresponding shelf item, as well as tracking
    the running instances/windows -- which are synonymous.
    
          launch a chrome app. Confirm that a temporary icon shows up in the
          shelf. Confirm that the icon goes away once the app window is
          closed.
    
    Test: Run Lacros with --enable-features=LacrosChromeApps. Install and
    Change-Id: I2daa28da169fac77cca3ad08eb7234ca64d7eaf6
    Bug: 1225848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133906
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919391}

--- chrome/browser/ash/crosapi/crosapi_ash.cc
+++ chrome/browser/ash/crosapi/crosapi_ash.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/browser_service_host_ash.h"
 #include "chrome/browser/ash/crosapi/browser_version_service_ash.h"
 #include "chrome/browser/ash/crosapi/cert_database_ash.h"
+#include "chrome/browser/ash/crosapi/chrome_app_window_tracker_ash.h"
 #include "chrome/browser/ash/crosapi/clipboard_ash.h"
 #include "chrome/browser/ash/crosapi/clipboard_history_ash.h"
 #include "chrome/browser/ash/crosapi/content_protection_ash.h"
@@ -107,6 +108,8 @@ CrosapiAsh::CrosapiAsh()
       browser_version_service_ash_(std::make_unique<BrowserVersionServiceAsh>(
           g_browser_process->component_updater())),
       cert_database_ash_(std::make_unique<CertDatabaseAsh>()),
+      chrome_app_window_tracker_ash_(
+          std::make_unique<ChromeAppWindowTrackerAsh>()),
       clipboard_ash_(std::make_unique<ClipboardAsh>()),
       clipboard_history_ash_(std::make_unique<ClipboardHistoryAsh>()),
       content_protection_ash_(std::make_unique<ContentProtectionAsh>()),
@@ -219,7 +222,7 @@ void CrosapiAsh::BindChromeAppPublisher(
 
 void CrosapiAsh::BindChromeAppWindowTracker(
     mojo::PendingReceiver<mojom::AppWindowTracker> receiver) {
-  // TODO(https://crbug.com/1225848): Implement this method.
+  chrome_app_window_tracker_ash_->BindReceiver(std::move(receiver));
 }
 
 void CrosapiAsh::BindFileManager(
