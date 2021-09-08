367a0c95207dfeebc5f87c7d7b0f806f85c8f428
Colin Blundell <blundell@chromium.org>
2021-09-08 12:32:11 +0000

    [WebLayer] Rationalize WL's ad-hoc population of ChromeUserPopulation
    Both Chrome and WebLayer's safe browsing code populate instances of the
    ChromeUserPopulation proto. WebLayer's code consisted of inline forks of
    the //chrome-level function. In addition to being duplicated code, these
    forks were inconsistent in how they set state on the proto.
    
    This CL rationalizes WebLayer's population of this proto by having it
    layer on top of the now-componentized GetUserPopulation() function that
    //chrome uses. This results in the following behavioral changes, all of
    which are positive:
    
    - The UrlLookupServiceFactory callsite now checks whether safe browsing
      is enabled before setting proto state to SAFE_BROWSING
    - The UrlLookupServiceFactory callsite now sets whether MBB is enabled
    - The WebLayerCSDServiceDelegate callsite now explicitly sets
      is_under_advanced_management to false
    
    In addition, this CL ensures that WebLayer will remaining consistent
    with Chrome in the setting of this state going forward.
    
    Other than the above changes, the behavior is identical to what it was
    before.
    
    Bug: 1226567
    Change-Id: If78952a69f74424fcb043de187fdca0baa4e6c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141554
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919220}

---
+++ weblayer/browser/safe_browsing/weblayer_user_population_helper.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBLAYER_BROWSER_SAFE_BROWSING_WEBLAYER_USER_POPULATION_HELPER_H_
+#define WEBLAYER_BROWSER_SAFE_BROWSING_WEBLAYER_USER_POPULATION_HELPER_H_
+
+#include "components/safe_browsing/core/common/proto/csd.pb.h"
+
+namespace content {
+class BrowserContext;
+}
+
+namespace weblayer {
+
+// A convenience function that creates a ChromeUserPopulation proto for the
+// given |browser_context| and populates it appropriately for WebLayer.
+safe_browsing::ChromeUserPopulation GetUserPopulationForBrowserContext(
+    content::BrowserContext* browser_context);
+
+}  // namespace weblayer
+
+#endif  // WEBLAYER_BROWSER_SAFE_BROWSING_WEBLAYER_USER_POPULATION_HELPER_H_
