70b4f54763293e9c4aaecab089133e58d830f4e4
Nick Diego Yamane <nickdiego@igalia.com>
2021-09-08 23:21:45 +0000

    wayland: add a custom impl of OSExchangeDataProvider for Wayland
    Having a custom implementation of OSExchangeDataProvider allows us,
    amongst other things, to build up the mime types list, required to offer
    data for drag-and-drop, in a generic way. To do so, this adds a new
    class that inherits from OSExchangeDataProviderNonBacked and can inspect
    its pickled data map and build mime types list out of it.
    
    More data utility code will be moved into WaylandExchangeDataProvider in
    upcoming CL(s).
    
    R=msisov@igalia.com, sky@chromium.org
    
    Bug: 1247063
    Test: Covered by ozone_unittests.
    Change-Id: Ia7f2279d1cdf091b3ad20c32256af68a0b5e39d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145735
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#919514}

--- ui/ozone/platform/wayland/ozone_platform_wayland.cc
+++ ui/ozone/platform/wayland/ozone_platform_wayland.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "ui/base/buildflags.h"
 #include "ui/base/cursor/cursor_factory.h"
+#include "ui/base/dragdrop/os_exchange_data_provider_factory_ozone.h"
 #include "ui/base/ime/linux/input_method_auralinux.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/events/devices/device_data_manager.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/host/wayland_buffer_manager_connector.h"
 #include "ui/ozone/platform/wayland/host/wayland_buffer_manager_host.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
+#include "ui/ozone/platform/wayland/host/wayland_exchange_data_provider.h"
 #include "ui/ozone/platform/wayland/host/wayland_input_method_context_factory.h"
 #include "ui/ozone/platform/wayland/host/wayland_menu_utils.h"
 #include "ui/ozone/platform/wayland/host/wayland_output_manager.h"
@@ -75,7 +77,8 @@ namespace ui {
 
 namespace {
 
-class OzonePlatformWayland : public OzonePlatform {
+class OzonePlatformWayland : public OzonePlatform,
+                             public OSExchangeDataProviderFactoryOzone {
  public:
   OzonePlatformWayland()
       : old_synthesize_key_repeat_enabled_(
@@ -337,6 +340,11 @@ class OzonePlatformWayland : public OzonePlatform {
       base::OnceCallback<void()> shutdown_cb) override {
     DCHECK(connection_);
     connection_->SetShutdownCb(std::move(shutdown_cb));
+  }
+
+  // OSExchangeDataProviderFactoryOzone:
+  std::unique_ptr<OSExchangeDataProvider> CreateProvider() override {
+    return std::make_unique<WaylandExchangeDataProvider>();
   }
 
  private:
