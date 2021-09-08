f5bbbce7c33c26e03be9656ec2f8536d42a10805
Colin Blundell <blundell@chromium.org>
2021-09-08 08:13:53 +0000

    Remove more unused includes in //chrome's safe_browsing_service.cc
    I'm analyzing this file for potential componentization, and removing
    unused //chrome includes helps to clarify the task (besides being good
    hygiene in any case).
    
    Bug: 1226567
    Change-Id: If5d1d4c5ccb08981fffc1577e99d5bf6044f0093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145417
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919147}

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -77,9 +77,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(FULL_SAFE_BROWSING)
-#include "chrome/browser/safe_browsing/download_protection/download_protection_service.h"
 #include "chrome/browser/safe_browsing/incident_reporting/binary_integrity_analyzer.h"
-#include "chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.h"
 #endif
 
 using content::BrowserThread;
