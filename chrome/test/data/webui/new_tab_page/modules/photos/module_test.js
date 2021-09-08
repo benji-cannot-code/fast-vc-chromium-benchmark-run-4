55f7bfbfbd84c359a4bc28bda8199fd400ef9d35
Tibor Goldschwendt <tiborg@chromium.org>
2021-09-08 20:41:16 +0000

    [ntp][modules][photos] Enable JS unit tests
    Adds boilerplate to make the photos JS unit tests run and fixes an
    import.
    
    Bug: 1230867
    Change-Id: I3da7ea51b6f78390800b055707472660e13bc9a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145930
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919429}

--- chrome/test/data/webui/new_tab_page/modules/photos/module_test.js
+++ chrome/test/data/webui/new_tab_page/modules/photos/module_test.js
@@ -3,9 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {$$, photosDescriptor, PhotosProxy} from 'chrome://new-tab-page/new_tab_page.js';
+import {photosDescriptor, PhotosProxy} from 'chrome://new-tab-page/new_tab_page.js';
 import {installMock} from 'chrome://test/new_tab_page/test_support.js';
 import {TestBrowserProxy} from 'chrome://test/test_browser_proxy.js';
+import {isVisible} from 'chrome://test/test_util.js';
 
 suite('NewTabPageModulesPhotosModuleTest', () => {
   /** @type {!TestBrowserProxy} */
