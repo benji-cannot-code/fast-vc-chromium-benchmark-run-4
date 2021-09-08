079a0c316c1d4a92dad4238b6748b654af8c79a4
Friedrich Horschig <fhorschig@chromium.org>
2021-09-08 17:44:18 +0000

    [Android][Passwords] Create proxy for shadow traffic to GMS
    The feature won't run before the local backend can be instantiated as
    added in https://crrev.com/c/3135655. It will compile but crash.
    
    This CL introduces a proxy class that allows for multiple store backends
    which will be used to ensure stability of Android backends before they
    are becoming default.
    Their purpose is to relay Chrome's requests to its local backend that
    fulfills them as usual and an Android backend that executes the same
    request. Unlike the response from the local backend, the Android backend
    response is only used by the proxy to compare results and run times and
    record the difference(s).
    
    Bug: 1229654
    Change-Id: I2f8cdba2f4d1c23f5da500a0e8dd147c1e208122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3129240
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919326}

--- chrome/browser/password_manager/password_store_android_backend_factory.cc
+++ chrome/browser/password_manager/password_store_android_backend_factory.cc
@@ -9,20 +9,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/android/password_store_android_backend.h"
 #include "chrome/browser/password_manager/android/password_store_android_backend_bridge.h"
 #include "components/password_manager/core/browser/login_database.h"
+#include "components/password_manager/core/browser/password_store_proxy_backend.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 
 namespace password_manager {
 
 std::unique_ptr<PasswordStoreBackend> PasswordStoreBackend::Create(
     std::unique_ptr<LoginDatabase> login_db) {
-  if (!base::FeatureList::IsEnabled(
+  if (base::FeatureList::IsEnabled(
           password_manager::features::kUnifiedPasswordManagerAndroid)) {
-    // TODO(crbug.com/1217071): Once PasswordStoreImpl does not implement the
-    // PasswordStore abstract class anymore, return a local backend.
-    return nullptr;
+    return std::make_unique<PasswordStoreAndroidBackend>(
+        PasswordStoreAndroidBackendBridge::Create());
   }
-  return std::make_unique<PasswordStoreAndroidBackend>(
-      PasswordStoreAndroidBackendBridge::Create());
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kUnifiedPasswordManagerShadowAndroid)) {
+    return std::make_unique<PasswordStoreProxyBackend>(
+        /*TODO(crbug.com/1229654): Create a local backend instead of */ nullptr,
+        std::make_unique<PasswordStoreAndroidBackend>(
+            PasswordStoreAndroidBackendBridge::Create()));
+  }
+  // TODO(crbug.com/1217071): Once PasswordStoreImpl does not implement the
+  // PasswordStore abstract class anymore, return a local backend.
+  return nullptr;
 }
 
 }  // namespace password_manager
