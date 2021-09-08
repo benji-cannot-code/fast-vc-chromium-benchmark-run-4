deb98474e6fa889ef91dafe77ccb026124ece3bd
Daniel Hosseinian <dhoss@chromium.org>
2021-09-08 02:20:47 +0000

    [unseasoned-pdf] Add is_pdf field to NavigationRequest
    Add a field to distinguish navigations for PDF content in a renderer.
    
    Bug: 1231763
    Change-Id: I38c585d794eb5b69106fa2310ba36c73cf79cda1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116737
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919054}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/callback_forward.h"
 #include "base/debug/crash_logging.h"
-#include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string_util.h"
@@ -200,7 +199,8 @@ class CONTENT_EXPORT NavigationRequest
       NavigationEntryImpl* entry,
       const scoped_refptr<network::ResourceRequestBody>& post_body,
       std::unique_ptr<NavigationUIData> navigation_ui_data,
-      const absl::optional<blink::Impression>& impression);
+      const absl::optional<blink::Impression>& impression,
+      bool is_pdf);
 
   // Creates a request for a renderer-initiated navigation.
   static std::unique_ptr<NavigationRequest> CreateRendererInitiated(
@@ -251,6 +251,9 @@ class CONTENT_EXPORT NavigationRequest
   // ReloadType::NONE.
   static ReloadType NavigationTypeToReloadType(
       blink::mojom::NavigationType type);
+
+  NavigationRequest(const NavigationRequest&) = delete;
+  NavigationRequest& operator=(const NavigationRequest&) = delete;
 
   ~NavigationRequest() override;
 
@@ -953,7 +956,8 @@ class CONTENT_EXPORT NavigationRequest
       std::unique_ptr<WebBundleHandleTracker> web_bundle_handle_tracker,
       RenderFrameHostImpl* rfh_restored_from_back_forward_cache,
       int initiator_process_id,
-      bool was_opener_suppressed);
+      bool was_opener_suppressed,
+      bool is_pdf);
 
   // Checks if this navigation may activate a prerendered page. If it's
   // possible, schedules to start running CommitDeferringConditions for
@@ -1921,9 +1925,10 @@ class CONTENT_EXPORT NavigationRequest
   // the time when this NavigationRequest was created.
   int initiator_commit_navigation_sent_counter_ = -1;
 
-  base::WeakPtrFactory<NavigationRequest> weak_factory_{this};
+  // Indicates that this navigation is for PDF content in a renderer.
+  bool is_pdf_ = false;
 
-  DISALLOW_COPY_AND_ASSIGN(NavigationRequest);
+  base::WeakPtrFactory<NavigationRequest> weak_factory_{this};
 };
 
 }  // namespace content
