9bc1ded284afea733ee5b0df60f440391955538d
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2021-09-08 16:56:13 +0000

    Call LoginEvent reporting code from PasswordManager
    Bug: 1238577
    Change-Id: Ifd2f68d8da5403e650fd517b45eb88ad5babe7b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137574
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919301}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
@@ -225,7 +225,7 @@ class SafeBrowsingPrivateEventRouter
 
   void OnLoginEvent(const GURL& url,
                     bool is_federated,
-                    const GURL& federated_origin);
+                    const url::Origin& federated_origin);
 
   void OnPasswordBreach(
       const std::string& trigger,
