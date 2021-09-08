21dc67f2e3b083ce906906c8566e7cc8e3799364
Kevin Marshall <kmarshall@chromium.org>
2021-09-08 20:49:45 +0000

    Reland "[fuchsia] Enable the use of PartitionAlloc for general purpose heap allocation."
    This is a reland of 6ae1d86c81f262ed6c565df688f23e93f27f5697
    
    * Adds a public_config entry to automatically propagate a "lib" entry
      on Zircon libsync to the dependents of //base:base.
    * Removes assert to allow PA on component builds under Fuchsia.
    * Addresses post-review feedback.
    
    Original change's description:
    > [fuchsia] Enable the use of PartitionAlloc for general purpose heap allocation.
    >
    > Allows PartitionAlloc to be optionally used as the general purpose heap
    > allocator for Fuchsia builds.
    >
    > To enable PartitionAlloc, simply add the following GN args:
    >   use_allocator = "partition"
    >   use_allocator_shim = true
    >
    >
    > Bug: 1178937
    > Change-Id: I76f325ec77b2605ffd2fa22882cc9bf801379334
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2697971
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Reviewed-by: Benoit L <lizeb@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#916560}
    
    Bug: 1178937
    Change-Id: Ib73627e8be11dff938bca3df8cd5091b26071501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131720
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919434}

--- base/allocator/partition_allocator/partition_tls.h
+++ base/allocator/partition_allocator/partition_tls.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
-#if defined(OS_POSIX)
+#if defined(OS_POSIX) || defined(OS_FUCHSIA)
 typedef pthread_key_t PartitionTlsKey;
 
 ALWAYS_INLINE bool PartitionTlsCreate(PartitionTlsKey* key,
