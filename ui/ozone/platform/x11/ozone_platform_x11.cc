b7b1409fbaa7272d1d488d81779b67fab7ef89fb
Nick Diego Yamane <nickdiego@igalia.com>
2021-09-08 05:39:32 +0000

    ozone/x11: Get rid of X11OSExchangeDataProviderOzone
    As of now, ozone/x11's X11OSExchangeDataProviderOzone and ui/base's
    OSExchangeDataProviderX11 contains basically the same code, so
    deduplicate them and prepare to start moving X code down to
    platform_window/x11 (or ozone/platform/x11 if possible), now that
    Aura/X11 is deprecated.
    
    R=thomasanderson@chromium.org
    
    Bug: None
    Change-Id: I3062e6c8b83cf68822675d06228a1fb56224805e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141883
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#919115}

--- ui/ozone/platform/x11/ozone_platform_x11.cc
+++ ui/ozone/platform/x11/ozone_platform_x11.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "ui/base/buildflags.h"
 #include "ui/base/cursor/cursor_factory.h"
-#include "ui/base/dragdrop/os_exchange_data_provider_factory.h"
 #include "ui/base/dragdrop/os_exchange_data_provider_factory_ozone.h"
 #include "ui/base/ime/linux/linux_input_method_context_factory.h"
 #include "ui/base/linux/linux_ui_delegate.h"
@@ -56,8 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/dragdrop/os_exchange_data_provider_non_backed.h"
 #include "ui/base/ime/chromeos/input_method_chromeos.h"
 #else
+#include "ui/base/dragdrop/os_exchange_data_provider_x11.h"
 #include "ui/base/ime/linux/input_method_auralinux.h"
-#include "ui/ozone/platform/x11/x11_os_exchange_data_provider_ozone.h"
 #endif
 
 namespace ui {
@@ -74,7 +73,7 @@ class LinuxUiDelegateX11 : public LinuxUiDelegate {
 
 // Singleton OzonePlatform implementation for X11 platform.
 class OzonePlatformX11 : public OzonePlatform,
-                         public ui::OSExchangeDataProviderFactoryOzone {
+                         public OSExchangeDataProviderFactoryOzone {
  public:
   OzonePlatformX11() { SetInstance(this); }
 
@@ -186,7 +185,7 @@ class OzonePlatformX11 : public OzonePlatform,
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     return std::make_unique<OSExchangeDataProviderNonBacked>();
 #else
-    return std::make_unique<X11OSExchangeDataProviderOzone>();
+    return std::make_unique<OSExchangeDataProviderX11>();
 #endif
   }
 
