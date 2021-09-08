7e47e48f0484baed7cfe29c25a5ccb2a3117ee60
Aaron Leventhal <aleventhal@google.com>
2021-09-08 10:33:28 +0000

    Fix a couple of post-review nits
    Bug: None
    NOTRY: true
    Change-Id: I5d255dce98edb69ad0e39046f833e9508301de3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141610
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919195}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "ax_object.h"
 #include "base/auto_reset.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_macros.h"
