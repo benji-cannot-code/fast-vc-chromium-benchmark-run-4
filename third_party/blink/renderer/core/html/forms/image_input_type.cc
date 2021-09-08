348c25b7a8a728caf4f8d3b1b2cb3d15631fbe38
Juanmi Huertas <juanmihd@chromium.org>
2021-09-08 17:12:18 +0000

    Revert "Force DisplayLocks in <input type=image> layout APIs"
    This reverts commit 96b0fd22d085b82fb64f5fdef57adcfeeb9a8a94.
    
    Reason for revert: This newly added test is failing in several bots https://bugs.chromium.org/p/chromium/issues/detail?id=1247714
    
    Original change's description:
    > Force DisplayLocks in <input type=image> layout APIs
    >
    > Without this patch, the width and height properties of image input types
    > return zero inside c-v:hidden subtrees due to layout being skipped for
    > DisplayLocking.
    >
    > Bug: 1247417
    > Change-Id: I2fb4e2f9388bbf351011efab525237f6a203323b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145723
    > Auto-Submit: Joey Arhar <jarhar@chromium.org>
    > Commit-Queue: vmpstr <vmpstr@chromium.org>
    > Reviewed-by: vmpstr <vmpstr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#919246}
    
    Bug: 1247417
    Change-Id: I436e7994615b0ab411c81a03616500a472902655
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149552
    Auto-Submit: Juanmi Huertas <juanmihd@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Owners-Override: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919305}

--- third_party/blink/renderer/core/html/forms/image_input_type.cc
+++ third_party/blink/renderer/core/html/forms/image_input_type.cc
@@ -181,8 +181,8 @@ unsigned ImageInputType::Height() const {
     }
   }
 
-  GetElement().GetDocument().UpdateStyleAndLayoutForNode(
-      &GetElement(), DocumentUpdateReason::kJavaScript);
+  GetElement().GetDocument().UpdateStyleAndLayout(
+      DocumentUpdateReason::kJavaScript);
 
   LayoutBox* box = GetElement().GetLayoutBox();
   return box ? AdjustForAbsoluteZoom::AdjustInt(box->ContentHeight().ToInt(),
@@ -207,8 +207,8 @@ unsigned ImageInputType::Width() const {
     }
   }
 
-  GetElement().GetDocument().UpdateStyleAndLayoutForNode(
-      &GetElement(), DocumentUpdateReason::kJavaScript);
+  GetElement().GetDocument().UpdateStyleAndLayout(
+      DocumentUpdateReason::kJavaScript);
 
   LayoutBox* box = GetElement().GetLayoutBox();
   return box ? AdjustForAbsoluteZoom::AdjustInt(box->ContentWidth().ToInt(),
