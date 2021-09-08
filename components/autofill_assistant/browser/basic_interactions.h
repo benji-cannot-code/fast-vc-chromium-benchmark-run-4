f7a091b67f2d0d9e661ef73469aa5613866d14a3
jeyashankher@google.com <jeyashankher@google.com>
2021-09-08 14:45:03 +0000

    [Autofill Assistant] Set InfoPopup's Close message with display_strings_util.
    Using display_strings_util to set InfoPopup's fallback Close message.
    This will ensure we use the right locale for the string.
    
    Bug: b/197935389
    Change-Id: Ifd1a1fcad38abca4bd7af4416f91f785ac228264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139870
    Commit-Queue: Jeyashankher Ramamirtham <jeyashankher@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/main@{#919254}

--- components/autofill_assistant/browser/basic_interactions.h
+++ components/autofill_assistant/browser/basic_interactions.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
+#include "components/autofill_assistant/browser/client_settings.h"
 #include "components/autofill_assistant/browser/client_status.h"
 #include "components/autofill_assistant/browser/generic_ui.pb.h"
 
@@ -27,6 +28,9 @@ class BasicInteractions {
   BasicInteractions& operator=(const BasicInteractions&) = delete;
 
   base::WeakPtr<BasicInteractions> GetWeakPtr();
+
+  // Returns ClientSettings from the ScriptExecutorDelegate.
+  const ClientSettings& GetClientSettings();
 
   // Performs the computation specified by |proto| and writes the result to
   // |user_model_|. Returns true on success, false on error.
