32097c35ba604445ca64b8433dc8506b85ea45aa
ananubis <anqing@google.com>
2021-09-08 22:44:21 +0000

    Block new windows in the web Kiosk session
    The new class lacros/app_mode/kiosk_session_service_lacros.* will be
    introduced in lacros-chrome to manage the Kiosk sessions and related
    resources.
    
    The instance is owned by ChromeBrowserMainExtraPartsLacros, but only be
    initialized in the web Kiosk session. In contrast, the existing ash
    version web Kiosk initializes and keeps a WebKioskAppManager instance
    in the ChromeBrowserMainChromeos.
    
    The AppSession class is reused in both ash and lacros to block the
    new windows.
    
    Bug: 1232794
    Change-Id: I8c12dd5ea0d4989265d46bea774d59480bbca8f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3132780
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919497}

--- chrome/browser/lacros/chrome_browser_main_extra_parts_lacros.cc
+++ chrome/browser/lacros/chrome_browser_main_extra_parts_lacros.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lacros/chrome_browser_main_extra_parts_lacros.h"
 
 #include "base/feature_list.h"
+#include "chrome/browser/lacros/app_mode/kiosk_session_service_lacros.h"
 #include "chrome/browser/lacros/automation_manager_lacros.h"
 #include "chrome/browser/lacros/browser_service_lacros.h"
 #include "chrome/browser/lacros/download_controller_client_lacros.h"
@@ -35,6 +36,7 @@ void ChromeBrowserMainExtraPartsLacros::PostBrowserStart() {
   download_controller_client_ =
       std::make_unique<DownloadControllerClientLacros>();
   task_manager_provider_ = std::make_unique<crosapi::TaskManagerLacros>();
+  kiosk_session_service_ = std::make_unique<KioskSessionServiceLacros>();
   web_page_info_provider_ =
       std::make_unique<crosapi::WebPageInfoProviderLacros>();
 
