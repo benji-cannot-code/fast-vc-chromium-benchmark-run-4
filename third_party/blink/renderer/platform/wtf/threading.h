46fe7be948bbe84b2ab75894288eb69d634c4373
yangxiaobing <yangxiaobing@qianxin.com>
2021-09-08 14:47:00 +0000

    Use GetCurrentThreadId() in WTF::CurrentThread On Windows
    The API GetCurrentThreadId() fetch current thread id value directly
    from TEB FS:0x24(x86). It's user mode and thread local already.
    See https://en.wikipedia.org/wiki/Win32_Thread_Information_Block
    
    Switch back to API and we save a thread_local variable on OS_WIN.
    
    R=dtapuska@chromium.org, haraken@chromium.org, yutak@chromium.org
    
    Bug: 1218710
    Change-Id: I83d668133f2335a4e13d24fc52d60f9e07305518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2962175
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919256}

--- third_party/blink/renderer/platform/wtf/threading.h
+++ third_party/blink/renderer/platform/wtf/threading.h
@@ -44,14 +44,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_WIN)
 WTF_EXPORT base::PlatformThreadId CurrentThread();
 #else
 // On Android gettid(3) uses a faster TLS model than thread_local.
+// On Windows GetCurrentThreadId() directly pick TID from TEB.
 inline base::PlatformThreadId CurrentThread() {
   return base::PlatformThread::CurrentId();
 }
-#endif  // !defined(OS_ANDROID)
+#endif  // !defined(OS_ANDROID) && !defined(OS_WIN)
 
 #if DCHECK_IS_ON()
 WTF_EXPORT bool IsBeforeThreadCreated();
