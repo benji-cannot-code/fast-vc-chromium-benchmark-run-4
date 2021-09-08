2674f81e6d5591718d7b5fbc0d9a6b326f6c02ec
Kent Tamura <tkent@chromium.org>
2021-09-08 10:27:48 +0000

    SVG Text NG: Allow mix-blend-mode CSS property
    Bug: 1247491
    Change-Id: Icd2bb203397a0f1f0c901875e5693b9fa8567e34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3146931
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919194}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -1293,6 +1293,7 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
     NOT_DESTROYED();
     return !IsSVG() || IsSVGShape() || IsSVGImage() || IsSVGText() ||
            IsSVGInline() || IsSVGRoot() || IsSVGForeignObject() ||
+           IsNGSVGText() ||
            // Blending does not apply to non-renderable elements such as
            // patterns (see: https://github.com/w3c/fxtf-drafts/issues/309).
            (IsSVGContainer() && !IsSVGHiddenContainer());
