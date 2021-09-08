8152ca72fd4d6d6d3e71eea94f20bce78858f612
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-09-08 20:10:50 +0000

    [Sheriff] Disable flaky CorsFileOriginBrowserTest test
    CorsFileOriginBrowserTest.UniversalAccessFromFileUrls is flaky on
    Lacros. Disable it.
    
    Bug: 1247748
    Change-Id: I4569b32ca8f9703819fa52f1ffd90aebcf1c51c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149693
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Owners-Override: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919410}

--- content/browser/loader/cors_file_origin_browsertest.cc
+++ content/browser/loader/cors_file_origin_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/test/scoped_command_line.h"
 #include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_paths.h"
@@ -244,7 +245,8 @@ IN_PROC_BROWSER_TEST_F(CorsFileOriginBrowserTest, AccessToAnotherFileUrl) {
 
 // TODO(lukasza, nasko): https://crbug.com/981018: Enable this test on Macs
 // after understanding what makes it flakily fail on the mac-rel trybot.
-#if defined(OS_MAC)
+// Also flaky on Lacros: https://crbug.com/1247748.
+#if defined(OS_MAC) || BUILDFLAG(IS_CHROMEOS_LACROS)
 #define MAYBE_UniversalAccessFromFileUrls DISABLED_UniversalAccessFromFileUrls
 #else
 #define MAYBE_UniversalAccessFromFileUrls UniversalAccessFromFileUrls
