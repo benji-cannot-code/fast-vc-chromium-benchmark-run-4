0baa15ec0b3d5cec62f546051718a43d9e833683
Joey Arhar <jarhar@chromium.org>
2021-09-08 14:18:45 +0000

    Unlock DisplayLocks in getBBox()
    Without this patch, getBBox() returns zero for all dimensions because
    layout it skipped for DisplayLocking.
    
    Bug: 1247417
    Change-Id: Id77947c39f1860627f90ffce95dabefe466a1231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145394
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919247}

--- third_party/blink/renderer/core/svg/svg_graphics_element.cc
+++ third_party/blink/renderer/core/svg/svg_graphics_element.cc
@@ -184,7 +184,8 @@ FloatRect SVGGraphicsElement::GetBBox() {
 }
 
 SVGRectTearOff* SVGGraphicsElement::getBBoxFromJavascript() {
-  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kJavaScript);
+  GetDocument().UpdateStyleAndLayoutForNode(this,
+                                            DocumentUpdateReason::kJavaScript);
 
   // FIXME: Eventually we should support getBBox for detached elements.
   FloatRect boundingBox;
