cddbd9939b25c53e03453feaabb3a046f426369c
Scott Violet <sky@chromium.org>
2021-09-08 19:49:31 +0000

    weblayer: adds UrlBarController#showPageInfo()
    As requested by Alihan.
    
    BUG=1233526
    TEST=showPageInfo
    
    Change-Id: Ib92e2de230328f9749c45b2d425abc6067e7e19e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131377
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919394}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/PageInfoTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/PageInfoTest.java
@@ -28,6 +28,7 @@ import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.CriteriaNotSatisfiedException;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.weblayer.PageInfoDisplayOptions;
 import org.chromium.weblayer.TestWebLayer;
 import org.chromium.weblayer.shell.InstrumentationActivity;
 
@@ -67,8 +68,25 @@ public class PageInfoTest {
         int buttonId = ResourceUtil.getIdentifier(remoteContext, "id/security_button", packageName);
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            StrictModeContext ignored = StrictModeContext.allowDiskReads();
-            EventUtils.simulateTouchCenterOfView(activity.findViewById(buttonId));
+            try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
+                EventUtils.simulateTouchCenterOfView(activity.findViewById(buttonId));
+            }
+        });
+        onViewWaiting(withText(CONNECTION_IS_NOT_SECURE_TEXT));
+    }
+
+    @Test
+    @SmallTest
+    public void testShowPageInfo() {
+        InstrumentationActivity activity = mActivityTestRule.launchShellWithUrl(
+                mActivityTestRule.getTestDataURL("simple_page.html"));
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
+                PageInfoDisplayOptions options =
+                        PageInfoDisplayOptions.builder().showPublisherUrl().build();
+                activity.getBrowser().getUrlBarController().showPageInfo(options);
+            }
         });
         onViewWaiting(withText(CONNECTION_IS_NOT_SECURE_TEXT));
     }
@@ -82,8 +100,9 @@ public class PageInfoTest {
                 mActivityTestRule.getTestDataURL("simple_page.html"), extras);
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            StrictModeContext ignored = StrictModeContext.allowDiskReads();
-            EventUtils.simulateTouchCenterOfView(activity.getUrlBarView());
+            try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
+                EventUtils.simulateTouchCenterOfView(activity.getUrlBarView());
+            }
         });
         onViewWaiting(withText(CONNECTION_IS_NOT_SECURE_TEXT));
     }
@@ -97,8 +116,9 @@ public class PageInfoTest {
                 mActivityTestRule.getTestDataURL("simple_page.html"), extras);
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            StrictModeContext ignored = StrictModeContext.allowDiskReads();
-            EventUtils.simulateTouchCenterOfView(activity.getUrlBarView());
+            try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
+                EventUtils.simulateTouchCenterOfView(activity.getUrlBarView());
+            }
         });
         onViewWaiting(withText(CONNECTION_IS_NOT_SECURE_TEXT)).perform(click());
         onViewWaiting(withText("The identity of this website has not been verified."));
@@ -114,8 +134,9 @@ public class PageInfoTest {
                 mActivityTestRule.getTestDataURL("simple_page.html"), extras);
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            StrictModeContext ignored = StrictModeContext.allowDiskReads();
-            EventUtils.simulateTouchCenterOfView(activity.getUrlBarView());
+            try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
+                EventUtils.simulateTouchCenterOfView(activity.getUrlBarView());
+            }
         });
         onViewWaiting(withText("Cookies")).perform(click());
         onViewWaiting(withText("0 cookies in use"));
