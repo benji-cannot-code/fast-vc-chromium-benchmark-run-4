b6992a8f26f3bb3298fea82a058a7adb15f62d9a
David Jean <djean@chromium.org>
2021-09-08 07:59:18 +0000

    [ios] Replace managed sync switches by info icon
    In Settings => Sync, when a type is managed (i.e not possible
    for user to enable it), replace it by a Off / Info icon, just
    like Sync Everything item.
    
    https://drive.google.com/file/d/1UhHvGhnlHPPtr2dFu2hrO75ePGn4gNcG/view?usp=sharing
    
    https://drive.google.com/file/d/1WvFTwrEYyLUA8G7LZnYTEHFxwl38kKOl/view?usp=sharing
    
    Bug: 1237849
    Change-Id: I0fa7f1c14b6827bc7ad3ef83c91f6753c786df2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137712
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919140}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator_unittest.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator_unittest.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_consumer.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_table_view_controller.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_image_item.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_info_button_item.h"
 #import "ios/chrome/browser/ui/table_view/table_view_model.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/signin/fake_chrome_identity.h"
@@ -434,12 +435,14 @@ TEST_F(ManageSyncSettingsMediatorTest,
     if (item.type == SyncEverythingItemType) {
       ASSERT_FALSE([item isKindOfClass:[SyncSwitchItem class]]);
       continue;
+    } else if (item.type == BookmarksDataTypeItemType ||
+               item.type == PasswordsDataTypeItemType) {
+      ASSERT_TRUE([item isKindOfClass:[TableViewInfoButtonItem class]]);
+      continue;
     }
     SyncSwitchItem* switch_item =
         base::mac::ObjCCastStrict<SyncSwitchItem>(item);
-    if (switch_item.type == BookmarksDataTypeItemType ||
-        switch_item.type == PasswordsDataTypeItemType ||
-        switch_item.type == AutocompleteWalletItemType) {
+    if (switch_item.type == AutocompleteWalletItemType) {
       ASSERT_FALSE(switch_item.enabled);
     } else {
       ASSERT_TRUE(switch_item.enabled);
