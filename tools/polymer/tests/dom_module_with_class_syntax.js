86386c95af7fa63e9116768a11ed7de793596ebf
Renato Silva <rrsilva@google.com>
2021-09-09 16:35:49 +0000

    Tools - Polymer - OOBE - Extend dom-module type
    Add support for ES6 class syntax in the polymer_modulizer()
    GN directive. Instead of relying on the Polymer() factory method,
    OOBE screens are going to migrate directly to ES6 class syntax and
    this was a necessary change.
    
    Bug: 1184731
    Change-Id: I32fea6472e15183140b73d21ee0ff5cf88ca7b0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139507
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#919833}

---
+++ tools/polymer/tests/dom_module_with_class_syntax.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/* #js_imports_placeholder */
+
+class CrTestFoo extends PolymerElement {
+  static get is() { return 'cr-test-foo'; }
+
+  /* #html_template_placeholder */
+
+}
