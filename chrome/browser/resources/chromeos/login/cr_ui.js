6a40c4e382876df8e7370eb4df68274c4cd184e1
Renato Silva <rrsilva@google.com>
2021-09-09 16:51:51 +0000

    OOBE - Add Polymer3 entry point
    Adds a top level document for the Polymer3 version of OOBE.
    Also adds the necessary feature flag. Changes the import of
    some elements to a relative import.
    
    Bug: 1184731
    Change-Id: Ica9ea26a417fe0366de949e7fbbb193b470a629d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3147672
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#919838}

--- chrome/browser/resources/chromeos/login/cr_ui.js
+++ chrome/browser/resources/chromeos/login/cr_ui.js
@@ -356,7 +356,6 @@ cr.define('cr.ui', function() {
      */
     static initialize() {
       Oobe.getInstance().initialize();
-
       chrome.send('screenStateInitialize');
     }
 
---
+++ chrome/browser/resources/chromeos/login/oobe_polymer3.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import {loadTimeData} from './i18n_setup.js';
+import {DebuggerUI} from './debug/debug.m.js';
+import {Oobe} from './cr_ui.m.js';
+
+import 'chrome://oobe/screens/common/marketing_opt_in.m.js';
+
+function initializeDebugger() {
+  if (document.readyState === 'loading')
+    return;
+  document.removeEventListener('DOMContentLoaded', initializeDebugger);
+  DebuggerUI.getInstance().register(document.body);
+}
+
+// Create the global values attached to `window` that are used
+// for accessing OOBE controls from the browser side.
+function prepareGlobalValues(globalValue) {
+    console.log('Preparing global values.');
+    if (globalValue.cr == undefined) {
+        globalValue.cr = {};
+    }
+    if (globalValue.cr.ui == undefined) {
+        globalValue.cr.ui = {};
+    }
+
+    globalValue.cr.ui.Oobe = Oobe;
+    globalValue.Oobe = Oobe;
+}
+
+(function (root) {
+    prepareGlobalValues(window);
+    Oobe.initialize();
+
+    // Initialize debugger.
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', initializeDebugger);
+      } else {
+        initializeDebugger();
+    }
+
+    // Make the WebUI visible.
+    chrome.send('loginVisible', ['oobe']);
+})(window);
