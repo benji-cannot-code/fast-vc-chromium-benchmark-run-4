64b294b96ff8900128a48b113139ece5118aaf4f
Titouan Rigoudy <titouan@chromium.org>
2021-09-09 18:23:46 +0000

    [Private Network Access] Record UMA histogram for each check result.
    At the same time, stop recording WebFeature usage when a request is
    blocked. This will show us the impact of the launch in the declining
    WebFeature usage, as well as in the increasing `kBlockedByPolicyBlock`
    histogram counts.
    
    Bug: chromium:1247571
    Change-Id: Ifc50f2739f69a33482abfa628b421a7d59b3a821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150014
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919883}

--- chrome/browser/net/private_network_access_browsertest.cc
+++ chrome/browser/net/private_network_access_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/file_util.h"
 #include "base/strings/string_piece.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/dom_distiller/tab_utils.h"
 #include "chrome/browser/dom_distiller/test_distillation_observers.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/dom_distiller/core/url_constants.h"
 #include "components/embedder_support/switches.h"
 #include "components/error_page/content/browser/net_error_auto_reloader.h"
+#include "components/metrics/content/subprocess_metrics_provider.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/url_constants.h"
@@ -592,15 +594,19 @@ IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
       feature_histogram_tester.GetNonZeroCounts(AllAddressSpaceFeatures()),
       IsEmpty());
 
+  base::HistogramTester base_histogram_tester;
+
   EXPECT_EQ(true, content::EvalJs(web_contents(), R"(
     fetch("/defaultresponse").catch(() => true)
   )"));
 
-  feature_histogram_tester.ExpectCounts(AddFeatureCounts(
-      AllZeroFeatureCounts(AllAddressSpaceFeatures()),
-      {
-          {WebFeature::kAddressSpacePublicNonSecureContextEmbeddedLocal, 1},
-      }));
+  metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+  base_histogram_tester.ExpectBucketCount(
+      "Security.PrivateNetworkAccess.CheckResult", 5, 1);
+
+  EXPECT_THAT(
+      feature_histogram_tester.GetNonZeroCounts(AllAddressSpaceFeatures()),
+      IsEmpty());
 }
 
 IN_PROC_BROWSER_TEST_F(PrivateNetworkAccessWithFeatureEnabledBrowserTest,
