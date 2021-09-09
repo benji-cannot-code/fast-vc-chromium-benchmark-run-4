a18bcfb7b34d6a44d5e159baf8ac3fb4e7efed29
Anders Hartvoll Ruud <andruud@chromium.org>
2021-09-09 05:09:49 +0000

    Make CompositablePaintAnimationChanged actually invalidate paint
     - In UpdatePropertySpecificDifferences, we need to check the
       new style ('other'), not the old style ('this').
     - In Element::RecalcOwnStyle, we need to add a check as
       well, otherwise we can call LayoutObject::SetStyle with
       ApplyStyleChanges::kNo, and we'll never actually reach
       UpdatePropertySpecificDifferences.
    
    The new unit test covers both of these cases.
    
    Bug: 1246826
    Change-Id: I6062f768268ac00d6354f6d83bfc36fcf28590a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141439
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919639}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -1031,10 +1031,11 @@ void ComputedStyle::UpdatePropertySpecificDifferences(
     diff.SetCompositingReasonsChanged();
   }
 
+  // TODO(crbug.com/1246826): Remove CompositablePaintAnimationChanged.
   if (RuntimeEnabledFeatures::CompositeBGColorAnimationEnabled() &&
       (HasCurrentBackgroundColorAnimation() !=
            other.HasCurrentBackgroundColorAnimation() ||
-       CompositablePaintAnimationChanged())) {
+       other.CompositablePaintAnimationChanged())) {
     diff.SetCompositablePaintEffectChanged();
   }
 }
