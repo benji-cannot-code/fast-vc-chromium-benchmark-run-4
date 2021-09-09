2afd694228d4a40ab0796dab1ac7b6a5064aa53c
Peng Huang <penghuang@chromium.org>
2021-09-09 18:08:48 +0000

    Fix incorrect resource format in blink for webgl
    For WebGL context created with alpha:false, it may create shared image
    backings in RGBX_8888 format. However the
    ImageLayerBridge::PrepareTransferableResource() always assume the image
    is in RGBA_8888 format, and set it into the TransferableResource.
    And then SkiaRenderer will receive the mismatched resource, it will
    cause DCHECK() error in skia. Fix the problem by get resource format
    from the StaticBitmapImage.
    
    Bug: 1230587,1200808
    Change-Id: Iafbc11da0e4c28b4f1244fd03d2c6ff8b8324df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3148710
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919877}

--- third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.cc
+++ third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.cc
@@ -150,4 +150,8 @@ bool UnacceleratedStaticBitmapImage::CopyToResourceProvider(
                                         /*x=*/0, /*y=*/0);
 }
 
+SkColorType UnacceleratedStaticBitmapImage::GetSkColorType() const {
+  return paint_image_.GetSkImageInfo().colorType();
+}
+
 }  // namespace blink
