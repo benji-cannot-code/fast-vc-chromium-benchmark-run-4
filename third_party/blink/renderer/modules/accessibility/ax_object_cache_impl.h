d0eb98f1b3a8baa07b3d67363fd35e713d22cf50
Mark Schillaci <mschillaci@google.com>
2021-09-09 07:48:27 +0000

    Revert "Simplification of dirty object serialization"
    This reverts commit 6cb40bfdc94c9ff0d038fcdc6b9a6645507011ca.
    
    Reason for revert: Causing crash on Android due to
    nodes not being wiped from cache, crbug.com/1084165
    
    Original change's description:
    > Simplification of dirty object serialization
    >
    > Now that we only mark included nodes dirty, we can simplify
    > RenderAccessibilityImpl::SerializeUpdatesAndEvents().
    > As it turns out, serializing an ignored but included node is
    > enough -- no need to serialize ancestors up to the unignored
    > node as well. As a result, we can remove the complex inner loop
    > from RenderAccessibilityImpl::SerializeUpdatesAndEvents().
    >
    > Bug: None
    > NOTRY: true
    > Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    > Change-Id: I38aafd0f40fc47851d37a6b7daf7b6a8fb43430e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3015448
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#902520}
    
    Bug: None
    Change-Id: Ib0cecbe0f2576d0e2adbbcb784a20e13f40c4cc0
    Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149360
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919675}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
@@ -234,7 +234,6 @@ class MODULES_EXPORT AXObjectCacheImpl
   void ChildrenChangedWithCleanLayout(Node* optional_node_for_relation_update,
                                       AXObject*);
 
-  void MarkAXObjectDirty(AXObject*);
   void MarkAXObjectDirtyWithCleanLayout(AXObject*);
   void MarkAXSubtreeDirtyWithCleanLayout(AXObject*);
 
@@ -443,6 +442,7 @@ class MODULES_EXPORT AXObjectCacheImpl
   ax::mojom::blink::EventFrom ComputeEventFrom();
 
   void MarkAXObjectDirtyWithCleanLayoutHelper(AXObject* obj, bool subtree);
+  void MarkAXObjectDirty(AXObject*);
   void MarkAXSubtreeDirty(AXObject*);
   void MarkElementDirty(const Node*);
   void MarkElementDirtyWithCleanLayout(const Node*);
