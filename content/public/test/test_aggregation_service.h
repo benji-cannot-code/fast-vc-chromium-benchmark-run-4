963458dacb71214dedfd584d170b0ebf5e077bcd
Nan Lin <linnan@chromium.org>
2021-09-09 23:58:52 +0000

    Output aggregatable report from aggregation service tool
    This CL implements the aggregatable report output from the aggregation
    service tool, either to save to disk or send over network to the
    reporting endpoint.
    
    Also refactored the main function.
    
    Bug: 1218124, 1233537
    Change-Id: I806bf70bf14981e306c4a49e16ae775b3af2e2d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3092108
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920023}

--- content/public/test/test_aggregation_service.h
+++ content/public/test/test_aggregation_service.h
@@ -9,8 +9,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/callback.h"
-#include "url/origin.h"
+#include "base/callback_forward.h"
+
+class GURL;
+
+template <class T>
+class scoped_refptr;
+
+namespace base {
+class Value;
+}  // namespace base
+
+namespace network {
+class SharedURLLoaderFactory;
+}  // namespace network
+
+namespace url {
+class Origin;
+}  // namespace url
 
 namespace content {
 
@@ -29,6 +45,19 @@ class TestAggregationService {
   virtual void SetPublicKeys(const url::Origin& origin,
                              const std::string& json_string,
                              base::OnceCallback<void(bool)> callback) = 0;
+
+  // Sets the provided URL loader factory. This will be called by the
+  // aggregation service tool to inject a network::mojom::URLLoaderFactory to
+  // send an aggregatable report over network.
+  virtual void SetURLLoaderFactory(
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory) = 0;
+
+  // Sends the aggregatable report to the specified reporting endpoint `url`.
+  // `callback` will be run once completed which returns whether the report was
+  // sent successfully.
+  virtual void SendReport(const GURL& url,
+                          const base::Value& contents,
+                          base::OnceCallback<void(bool)> callback) = 0;
 };
 
 }  // namespace content
