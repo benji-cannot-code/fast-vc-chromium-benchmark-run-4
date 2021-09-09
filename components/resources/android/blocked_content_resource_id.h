000d901cf95eaaf55882414ccfedb8060093bbd9
Aishwarya Rajesh <aishwaryarj@google.com>
2021-09-09 18:31:56 +0000

    [Messages] Migrate Ads Blocked infobar to a message - phase 3
    This CL includes changes to add Ads Blocked message icons and to use the AdsBlockedDialog implemented in crrev.com/c/3107487 to link to the Ads Blocked dialog from the message. Also accounts for the implementation of the message and dialog action callbacks and associated tests.
    
    This CL also moves the gear icon defined at //chrome/android/java/res/drawable/ic_settings_black.xml to //components for use by messages defined in both //chrome and //components.
    
    This CL additionally accounts for the requirement that the dialog is dismissed on touching outside on the scrim.
    
    During the course of testing this implementation, identified crbug.com/1245060, which has also been fixed.
    
    Bug: 1233696, 1245060
    Change-Id: I78a399bfaecbcd20a7c9c64ce96975e9cb967871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3111387
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919888}

--- components/resources/android/blocked_content_resource_id.h
+++ components/resources/android/blocked_content_resource_id.h
@@ -26,3 +26,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // InfoBar resources.
 DECLARE_RESOURCE_ID(IDR_ANDROID_INFOBAR_BLOCKED_POPUPS,
                     R.drawable.infobar_blocked_popups)
+
+DECLARE_RESOURCE_ID(IDR_ANDROID_SETTINGS, R.drawable.ic_settings_black)
--- components/subresource_filter/android/ads_blocked_dialog.cc
+++ components/subresource_filter/android/ads_blocked_dialog.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/check.h"
 #include "base/memory/ptr_util.h"
@@ -21,8 +22,9 @@ AdsBlockedDialogBase::~AdsBlockedDialogBase() = default;
 // static
 std::unique_ptr<AdsBlockedDialogBase> AdsBlockedDialog::Create(
     content::WebContents* web_contents,
-    AllowAdsClickedCallback allow_ads_clicked_callback,
-    LearnMoreClickedCallback learn_more_clicked_callback) {
+    base::OnceClosure allow_ads_clicked_callback,
+    base::OnceClosure learn_more_clicked_callback,
+    base::OnceClosure dialog_dismissed_callback) {
   DCHECK(web_contents);
 
   ui::WindowAndroid* window_android = web_contents->GetTopLevelNativeWindow();
@@ -30,15 +32,18 @@ std::unique_ptr<AdsBlockedDialogBase> AdsBlockedDialog::Create(
     return nullptr;
   return base::WrapUnique(new AdsBlockedDialog(
       window_android->GetJavaObject(), std::move(allow_ads_clicked_callback),
-      std::move(learn_more_clicked_callback)));
+      std::move(learn_more_clicked_callback),
+      std::move(dialog_dismissed_callback)));
 }
 
 AdsBlockedDialog::AdsBlockedDialog(
     base::android::ScopedJavaLocalRef<jobject> j_window_android,
-    AllowAdsClickedCallback allow_ads_clicked_callback,
-    LearnMoreClickedCallback learn_more_clicked_callback)
+    base::OnceClosure allow_ads_clicked_callback,
+    base::OnceClosure learn_more_clicked_callback,
+    base::OnceClosure dialog_dismissed_callback)
     : allow_ads_clicked_callback_(std::move(allow_ads_clicked_callback)),
-      learn_more_clicked_callback_(std::move(learn_more_clicked_callback)) {
+      learn_more_clicked_callback_(std::move(learn_more_clicked_callback)),
+      dialog_dismissed_callback_(std::move(dialog_dismissed_callback)) {
   JNIEnv* env = base::android::AttachCurrentThread();
   java_ads_blocked_dialog_ = Java_AdsBlockedDialog_create(
       env, reinterpret_cast<intptr_t>(this), j_window_android);
@@ -67,5 +72,6 @@ void AdsBlockedDialog::OnLearnMoreClicked(JNIEnv* env) {
 }
 
 void AdsBlockedDialog::OnDismissed(JNIEnv* env) {
-  // TODO(aishwaryarj): destroy the dialog instance on dismissal.
+  java_ads_blocked_dialog_.Reset();
+  std::move(dialog_dismissed_callback_).Run();
 }
