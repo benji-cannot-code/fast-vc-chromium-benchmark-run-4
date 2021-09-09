20922aa7d577219d8c7615610bf4876fee9e3cf5
Danil Somsikov <dsv@chromium.org>
2021-09-09 08:45:19 +0000

    Enable back copy-network-request.js test and update it according to crrev.com/c/3059011
    Bug: 3059011
    Change-Id: I68223a4ba0a1730d57efdb20aaa76a0a66077a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3148354
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919691}

--- third_party/blink/web_tests/http/tests/devtools/copy-network-request.js
+++ third_party/blink/web_tests/http/tests/devtools/copy-network-request.js
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.showPanel('network');
 
   var logView = UI.panels.network.networkLogView;
+  const BROWSER = 0;
+  const NODE_JS = 1;
 
   function newRequest(isBlob, headers, data, opt_url, method = null) {
     var request = SDK.NetworkRequest.create(
@@ -35,8 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var curlWin = await logView.generateCurlCommand(request, 'win');
     var curlUnix = await logView.generateCurlCommand(request, 'unix');
     var powershell = await logView.generatePowerShellCommand(request);
-    var fetchForBrowser = await logView.generateFetchCall(request, false);
-    var fetchForNodejs = await logView.generateFetchCall(request, true);
+    var fetchForBrowser = await logView.generateFetchCall(request, BROWSER);
+    var fetchForNodejs = await logView.generateFetchCall(request, NODE_JS);
     TestRunner.addResult(`cURL Windows:\n${curlWin}\n\n`);
     TestRunner.addResult(`cURL Unix:\n${curlUnix}\n\n`);
     TestRunner.addResult(`Powershell:\n${powershell}\n\n`);
@@ -52,8 +54,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var allCurlWin = await logView.generateAllCurlCommand(allRequests, 'win');
     var allCurlUnix = await logView.generateAllCurlCommand(allRequests, 'unix');
     var allPowershell = await logView.generateAllPowerShellCommand(allRequests);
-    var allFetchForBrowser = await logView.generateAllFetchCall(allRequests, false);
-    var allFetchForNodejs = await logView.generateAllFetchCall(allRequests, true);
+    var allFetchForBrowser = await logView.generateAllFetchCall(allRequests, BROWSER);
+    var allFetchForNodejs = await logView.generateAllFetchCall(allRequests, NODE_JS);
     TestRunner.addResult(`cURL Windows:\n${allCurlWin}\n\n`);
     TestRunner.addResult(`cURL Unix:\n${allCurlUnix}\n\n`);
     TestRunner.addResult(`Powershell:\n${allPowershell}\n\n`);
@@ -85,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dumpRequest({'Cookie': '_x=fdsfs; aA=fdsfdsf; FOO=ID=BAR:BAZ=FOO:F=d:AO=21.212.2.212-:A=dsadas8d9as8d9a8sd9sa8d9a; AAA=117'});
   await dumpRequest({}, null, null, '|evilcommand|');
   await dumpRequest({'Content-Type':'application/x-www-form-urlencoded'}, '@/etc/passwd');
+  await dumpRequest({'Referer' : 'https://example.com'});
 
   await dumpMultipleRequests([]);
   await dumpMultipleRequests([true]);
