c0a4ed65293962041b6ae6a7478ee177124b104f
Andreea Costinas <acostinas@google.com>
2021-09-09 09:49:41 +0000

    [Sheriff] Disable TSan DecoderTemplateTest.CodecReclamation test
    Bug: 1247967
    Change-Id: I8fdbd8ed50cf57874d35606aaae598005d03aeaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149853
    Reviewed-by: Victor-Gabriel Savu <vsavu@google.com>
    Reviewed-by: Andreea-Elena Costinas <acostinas@google.com>
    Commit-Queue: Andreea-Elena Costinas <acostinas@google.com>
    Owners-Override: Andreea-Elena Costinas <acostinas@google.com>
    Auto-Submit: Andreea-Elena Costinas <acostinas@google.com>
    Cr-Commit-Position: refs/heads/main@{#919712}

--- third_party/blink/renderer/modules/webcodecs/decoder_template_test.cc
+++ third_party/blink/renderer/modules/webcodecs/decoder_template_test.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_function.h"
@@ -164,8 +165,14 @@ TYPED_TEST(DecoderTemplateTest, ResetDuringFlush) {
   }
 }
 
+#if defined(OS_LINUX) && defined(THREAD_SANITIZER)
+// https://crbug.com/1247967
+#define MAYBE_CodecReclamation DISABLED_CodecReclamation
+#else
+#define MAYBE_CodecReclamation CodecReclamation
+#endif
 // Ensures codecs can be reclaimed in a configured or unconfigured state.
-TYPED_TEST(DecoderTemplateTest, CodecReclamation) {
+TYPED_TEST(DecoderTemplateTest, MAYBE_CodecReclamation) {
   V8TestingScope v8_scope;
 
   // Create a decoder.
