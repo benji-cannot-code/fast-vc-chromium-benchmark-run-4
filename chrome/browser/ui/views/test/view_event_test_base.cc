231287f49801a71e5745c30b71612e9d0009ee2f
Maksim Sisov <msisov@igalia.com>
2021-09-09 18:39:31 +0000

    X11: remove screen
    Non-Ozone/X11 is no longer in use and all the non-Ozone/X11
    code shall be removed.
    
    Bug: 1096425
    Change-Id: I153917217b571ccb7427565de7dcf1238d6076ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140088
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#919894}

--- chrome/browser/ui/views/test/view_event_test_base.cc
+++ chrome/browser/ui/views/test/view_event_test_base.cc
@@ -23,10 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/screen.h"
 #include "ui/views/widget/desktop_aura/desktop_screen.h"
 
-#if defined(USE_X11)
-#include "ui/views/test/test_desktop_screen_x11.h"
-#endif  // defined(USE_X11)
-
 #if (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) && defined(USE_OZONE)
 #include "ui/views/test/test_desktop_screen_ozone.h"
 #endif  // (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) &&
@@ -103,10 +99,6 @@ ViewEventTestBase::ViewEventTestBase() {
   // insufficient for these tests, then either bolster/replace it or fix the
   // tests.
   DCHECK(!display::Screen::GetScreen());
-#if defined(USE_X11)
-  if (!features::IsUsingOzonePlatform())
-    views::test::TestDesktopScreenX11::GetInstance();
-#endif  // defined(USE_X11)
 #if (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) && defined(USE_OZONE)
   if (!display::Screen::GetScreen())
     display::Screen::SetScreenInstance(
--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -112,10 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/test/event_generator.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
-#if !BUILDFLAG(IS_CHROMEOS_ASH) && defined(USE_X11)
-#include "ui/views/test/test_desktop_screen_x11.h"
-#endif
-
 #if defined(TOOLKIT_VIEWS)
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
@@ -512,16 +508,6 @@ void InProcessBrowserTest::AddTabAtIndex(int index,
 
 bool InProcessBrowserTest::SetUpUserDataDirectory() {
   return true;
-}
-
-void InProcessBrowserTest::SetScreenInstance() {
-#if defined(USE_X11) && !BUILDFLAG(IS_CHROMEOS_ASH)
-  if (!features::IsUsingOzonePlatform()) {
-    DCHECK(!display::Screen::GetScreen());
-    display::Screen::SetScreenInstance(
-        views::test::TestDesktopScreenX11::GetInstance());
-  }
-#endif
 }
 
 #if !defined(OS_MAC)
