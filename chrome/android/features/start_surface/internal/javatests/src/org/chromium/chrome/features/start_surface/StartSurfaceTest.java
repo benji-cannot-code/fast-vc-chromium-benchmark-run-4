9e5a94e3fd5eebe3cd8b3e11b0b8b2d7ad8f9f22
bttk <bttk@chromium.org>
2021-09-09 23:25:48 +0000

    sheriff: disable flaky tests in StartSurfaceTest
    Bug: 1246457
    Change-Id: I7904c5a83382158f639a8ad03d60cbd8608b83c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152389
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: bttk <bttk@chromium.org>
    Owners-Override: bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920008}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -479,6 +479,8 @@ public class StartSurfaceTest {
     @Feature({"StartSurface"})
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single"})
+    @DisableIf.
+        Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "Flaky, see crbug.com/1246457")
     public void testShow_SingleAsHomepage_FromResumeShowStart() throws Exception {
         // clang-format on
         if (!mImmediateReturn) {
@@ -1027,8 +1029,12 @@ public class StartSurfaceTest {
     @Test
     @MediumTest
     @Feature({"StartSurface"})
+    // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single"})
+    @DisableIf.
+        Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "Flaky, see crbug.com/1246457")
     public void testShow_SingleAsHomepage_BackButtonWithTabSwitcher() {
+        // clang-format on
         singleAsHomepage_BackButtonWithTabSwitcher();
     }
 
@@ -1037,6 +1043,8 @@ public class StartSurfaceTest {
     @Feature({"StartSurface"})
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single/show_last_active_tab_only/true"})
+    @DisableIf.
+        Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "Flaky, see crbug.com/1246457")
     public void testShow_SingleAsHomepageV2_BackButtonWithTabSwitcher() {
         // clang-format on
         singleAsHomepage_BackButtonWithTabSwitcher();
@@ -1146,6 +1154,8 @@ public class StartSurfaceTest {
     @EnableFeatures(ChromeFeatureList.TAB_GROUPS_ANDROID)
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single"})
+    @DisableIf.
+        Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "Flaky, see crbug.com/1246457")
     public void testShow_SingleAsHomepage_BackButtonOnCarouselTabSwitcher() {
         // clang-format on
         if (!mImmediateReturn) {
@@ -1801,8 +1811,12 @@ public class StartSurfaceTest {
     @Test
     @MediumTest
     @Feature({"StartSurface"})
+    // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/single"})
+    @DisableIf.
+        Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "Flaky, see crbug.com/1246457")
     public void testSwipeBackOnStartSurfaceHomePage() throws ExecutionException {
+        // clang-format on
         // TODO(https://crbug.com/1093632): Requires 2 back press/gesture events now. Make this
         // work with a single event.
         Assume.assumeFalse(mImmediateReturn);
