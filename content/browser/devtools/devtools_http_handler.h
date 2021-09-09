ea44486d3884f71d6ef49218ff52b9b5e565b32e
Simon Zünd <szuend@chromium.org>
2021-09-09 05:52:04 +0000

    [cleanup] Replace deprecated usage of DictionaryValue in http handler
    Bug: 1187061
    Change-Id: I3985b6eb5ddc67d422be11a7c434fa4f35551bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3147331
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919654}

--- content/browser/devtools/devtools_http_handler.h
+++ content/browser/devtools/devtools_http_handler.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_status_code.h"
 
 namespace base {
-class DictionaryValue;
 class Thread;
 class Value;
 }
@@ -102,9 +101,8 @@ class DevToolsHttpHandler {
       const std::string& target_id,
       const std::string& host);
 
-  std::unique_ptr<base::DictionaryValue> SerializeDescriptor(
-      scoped_refptr<DevToolsAgentHost> agent_host,
-      const std::string& host);
+  base::Value SerializeDescriptor(scoped_refptr<DevToolsAgentHost> agent_host,
+                                  const std::string& host);
 
   // The thread used by the devtools handler to run server socket.
   std::unique_ptr<base::Thread> thread_;
