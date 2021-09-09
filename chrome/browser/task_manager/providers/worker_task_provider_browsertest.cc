320025e8be5201bf7688932d5094cad2c10e6944
Gayane Petrosyan <gayane@google.com>
2021-09-09 19:49:52 +0000

    Revert "Fix WorkerTaskProviderBrowserTest.CreateTasksForMultiProfiles"
    This reverts commit 51ececfa5124274d699e69b6bf9ad47704b73f05.
    
    Reason for revert: The test still fails 
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac11-arm64-rel-tests/16/overview
    
    Original change's description:
    > Fix WorkerTaskProviderBrowserTest.CreateTasksForMultiProfiles
    >
    > This test is flaky because sometimes the NavigateToURL() fails to wait
    > until the kCreateServiceWorkerURL finishes to load before returning.
    >
    > The theory is that the mechanism that wait for a navigation actually
    > catches the navigation to the new tab page initiated by opening a new
    > browser window.
    >
    > This race condition is fixed by ensuring the newly created WebContents
    > has finished loading before trying to load the test URL.
    >
    > Bug: 1244009
    > Change-Id: Iea0312a87cbfe0952df0713fc4de03d6599ffbf9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150212
    > Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    > Auto-Submit: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#919847}
    
    Bug: 1244009
    Change-Id: Idb055939f7da735cf885cff77f2f8405d6b4ca65
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149953
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Owners-Override: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#919927}

--- chrome/browser/task_manager/providers/worker_task_provider_browsertest.cc
+++ chrome/browser/task_manager/providers/worker_task_provider_browsertest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "content/public/test/test_navigation_observer.h"
 #include "net/dns/mock_host_resolver.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -105,15 +104,6 @@ class WorkerTaskProviderBrowserTest : public InProcessBrowserTest,
                               base::DoNothing());
     BrowserList* browser_list = BrowserList::GetInstance();
     return *browser_list->begin_browsers_ordered_by_activation();
-  }
-
-  void EnsureInitialNavigationFinished(Browser* browser) {
-    content::WebContents* web_contents =
-        browser->tab_strip_model()->GetActiveWebContents();
-    if (web_contents->IsLoading()) {
-      content::TestNavigationObserver test_navigation_observer(web_contents);
-      test_navigation_observer.Wait();
-    }
   }
 
   content::ServiceWorkerContext* GetServiceWorkerContext(Browser* browser) {
@@ -253,7 +243,7 @@ IN_PROC_BROWSER_TEST_F(WorkerTaskProviderBrowserTest,
 // simultaneously, the WorkerTaskProvider can still works.
 // Flaky on all platforms. https://crrev.com/1244009.
 IN_PROC_BROWSER_TEST_F(WorkerTaskProviderBrowserTest,
-                       CreateTasksForMultiProfiles) {
+                       DISABLED_CreateTasksForMultiProfiles) {
   StartUpdating();
 
   EXPECT_TRUE(tasks().empty());
@@ -262,7 +252,6 @@ IN_PROC_BROWSER_TEST_F(WorkerTaskProviderBrowserTest,
       "/service_worker/create_service_worker.html");
 
   Browser* browser_1 = CreateNewProfileAndSwitch();
-  EnsureInitialNavigationFinished(browser_1);
   content::RenderFrameHost* render_frame_host_1 =
       ui_test_utils::NavigateToURL(browser_1, kCreateServiceWorkerURL);
   ASSERT_EQ(render_frame_host_1->GetLastCommittedURL(),
@@ -272,7 +261,6 @@ IN_PROC_BROWSER_TEST_F(WorkerTaskProviderBrowserTest,
   WaitUntilTaskCount(1);
 
   Browser* browser_2 = CreateNewProfileAndSwitch();
-  EnsureInitialNavigationFinished(browser_1);
   content::RenderFrameHost* render_frame_host_2 =
       ui_test_utils::NavigateToURL(browser_2, kCreateServiceWorkerURL);
   ASSERT_EQ(render_frame_host_2->GetLastCommittedURL(),
