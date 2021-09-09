b17c2f9749292f93064480e564de5050032e6793
Olga Gerchikov <gerchiko@microsoft.com>
2021-09-09 02:59:48 +0000

    [CompositeClipPathAnimations] Support for CSS Transitions
    - Implemented handling transition keyframes.
    - Simplified input for ClipPathPaintWorkletInput() - use BasicShape
      instead of ShapeClipPathOperation.
    
    Bug: 1223975
    Change-Id: Ic238778515dd0b76b0355dd66ab9c5edfd6becf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145771
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Olga Gerchikov <gerchiko@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#919589}

--- third_party/blink/renderer/core/animation/path_interpolation_functions.h
+++ third_party/blink/renderer/core/animation/path_interpolation_functions.h
@@ -37,6 +37,9 @@ class CORE_EXPORT PathInterpolationFunctions {
 
   static bool PathsAreCompatible(const NonInterpolableValue& start,
                                  const NonInterpolableValue& end);
+
+  static bool IsPathNonInterpolableValue(const NonInterpolableValue& value);
+
   static PairwiseInterpolationValue MaybeMergeSingles(
       InterpolationValue&& start,
       InterpolationValue&& end);
