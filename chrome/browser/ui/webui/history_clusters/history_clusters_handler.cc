35a553ecc51b819609749d0ee348c3f65478e8ab
mcrouse <mcrouse@google.com>
2021-09-09 20:30:21 +0000

    [Memories] Add UKM event for HistoryClusters interactions.
    This adds a UKM event that captures the initial state of how the
    HistoryClusters UI is arrived at and the result of that navigation. It
    also captures how many queries occur during that interaction and whether
    the user toggled to the original history page.
    
    UKM Privacy doc: https://docs.google.com/document/d/1F3qGIlG1XygM2zjwddvDnIPjkQ-t3yE8KgFTpZiwtag/edit
    
    Bug: 1243049
    Change-Id: Ieca75bba1e2d8bfeec30c6d275bd71a863f6e1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3116691
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919945}

--- chrome/browser/ui/webui/history_clusters/history_clusters_handler.cc
+++ chrome/browser/ui/webui/history_clusters/history_clusters_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/strings/string_number_conversions.h"
+#include "chrome/browser/history_clusters/history_clusters_metrics_logger.h"
 #include "chrome/browser/history_clusters/history_clusters_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -209,6 +210,15 @@ void HistoryClustersHandler::QueryClusters(mojom::QueryParamsPtr query_params) {
         << "Page called handler with non-null but invalid end_time.";
     end_time = *(query_params->end_time);
   }
+
+  if (!query.empty()) {
+    // If the query string is not empty, we assume that this clusters query
+    // is user generated.
+    HistoryClustersMetricsLogger::GetOrCreateForPage(
+        web_contents_->GetPrimaryPage())
+        ->increment_query_count();
+  }
+
   auto result_callback =
       base::BindOnce(&HistoryClustersHandler::OnClustersQueryResult,
                      weak_ptr_factory_.GetWeakPtr(), std::move(query_params));
