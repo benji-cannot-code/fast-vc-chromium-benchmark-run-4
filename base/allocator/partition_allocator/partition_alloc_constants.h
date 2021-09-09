70e985c7830c47d943b6e0e7448740492f690edf
Anton Bikineev <bikineev@chromium.org>
2021-09-09 11:19:49 +0000

    *Scan: Switch to StateBitmap
    This CL entirely removes QuarantineBitmaps and instead uses StateBitmap.
    The scanning logic is kept roughly the same, only the threshold for
    skipping entries is lowered to make sure that discarded free slots don't
    get faulted back.
    
    Bug: 1237828
    Change-Id: If942af9b4f89d0dfca8dcb781e9976ac7031c8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3071188
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919726}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -121,7 +121,7 @@ MaxRegularSlotSpanSize() {
 //     | Guard page (4 KiB)    |
 //     | Metadata page (4 KiB) |
 //     | Guard pages (8 KiB)   |
-//     | QuarantineBitmaps     |
+//     | *Scan State Bitmap    |
 //     | Slot span             |
 //     | Slot span             |
 //     | ...                   |
@@ -129,7 +129,7 @@ MaxRegularSlotSpanSize() {
 //     | Guard pages (16 KiB)  |
 //     +-----------------------+
 //
-// QuarantineBitmaps are inserted for partitions that may have PCScan enabled.
+// State Bitmap is inserted for partitions that may have quarantine enabled.
 //
 // If refcount_at_end_allocation is enabled, RefcountBitmap(4KiB) is inserted
 // after the Metadata page for BackupRefPtr. The guard pages after the bitmap
