c2fb44887d4c9f3a31466eb4d28aa78b3e63ce1f
Yuzu Saijo <yuzus@chromium.org>
2021-09-09 17:46:27 +0000

    [bfcache] Report other reasons along with CacheControlNoStore
    This CL fixes a bug where CacheControlNoStore related feature is not
    reported with other reasons.
    
    This bug was caused because of two reasons:
    1) Upon other eviction when a page is in bfcache, not recording
      CacheControlNoStore reasons.
    2) Upon other eviction OnPageLifecycleChangedAck, not recording
      CacheControlNoStore reasons.
    
    This CL fixes both of them in MaybeEvictFromBackForwardCache() and
    EvictFromBackForwardCacheWithReason().
    (These two functions both call EvictFromBackForwardCacheWithReasons(),
    and at this point the reasons cannot change.)
    
    Note that we have to make a special case for OnPageLifecycleChangedAck.
    Upon getting an ack from renderer to enter bfcache, we do not want to
    evict the entry if the only reason evicting is CacheControlNoStore. We
    want the pages to enter bfcache instead. But when there are other reasons
    at that point, we want to evict with them, as well as
    CacheControlNoStore.
    
    Design doc: https://docs.google.com/document/d/1RSJqWqxd3tZUESPQTNt7CdVaW-hbUQOK3ojfpeoG8-o/edit#
    
    
    Bug: 1240064
    Change-Id: I3206db5cbd2bb55fd8a4f196396970fdcc88c04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3096892
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919863}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -5122,7 +5122,7 @@ RenderFrameHostImpl::GetBackForwardCanStoreNowDebugStringForTesting() {
   return frame_tree()
       ->controller()
       .GetBackForwardCache()
-      .CanStorePageNow(GetMainFrame())
+      .CanRestorePageNowForTesting(GetMainFrame())  // IN-TEST
       .ToString();
 }
 
@@ -5753,7 +5753,9 @@ void RenderFrameHostImpl::EvictFromBackForwardCache(
 
 void RenderFrameHostImpl::EvictFromBackForwardCacheWithReason(
     BackForwardCacheMetrics::NotRestoredReason reason) {
-  BackForwardCacheCanStoreDocumentResult can_store;
+  auto can_store =
+      frame_tree()->controller().GetBackForwardCache().CanStorePageNow(
+          GetMainFrame());
   can_store.No(reason);
   EvictFromBackForwardCacheWithReasons(can_store);
 }
@@ -11941,17 +11943,16 @@ void RenderFrameHostImpl::MaybeEvictFromBackForwardCache() {
   RenderFrameHostImpl* top_document = this;
   while (RenderFrameHostImpl* parent = top_document->GetParent())
     top_document = parent;
-
-  auto can_store =
+  BackForwardCacheCanStoreDocumentResult can_store =
       frame_tree()->controller().GetBackForwardCache().CanStorePageNow(
           top_document);
+
   TRACE_EVENT("navigation",
               "RenderFrameHostImpl::MaybeEvictFromBackForwardCache",
               "render_frame_host", this, "can_store", can_store.ToString());
 
   if (can_store)
     return;
-
   EvictFromBackForwardCacheWithReasons(can_store);
 }
 
