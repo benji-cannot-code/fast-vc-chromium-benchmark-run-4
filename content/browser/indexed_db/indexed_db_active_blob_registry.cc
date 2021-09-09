e64d0141c60573945b2c88c40fdb05e57ad8af41
Alec Petridis <alecthechop@gmail.com>
2021-09-09 01:26:49 +0000

    Use 'auto' instead of 'const auto&' for iterators in IndexedDB.
    Using 'const auto&' for iterators is unnecessary and less clear than
    'auto'.
    
    Bug: 944290
    Change-Id: I0d0d0d7bf2097a6d1c15a3a3664890531bbc7a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3146296
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Alec Petridis <alecthechop@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#919548}

--- content/browser/indexed_db/indexed_db_active_blob_registry.cc
+++ content/browser/indexed_db/indexed_db_active_blob_registry.cc
@@ -28,7 +28,7 @@ bool IndexedDBActiveBlobRegistry::MarkDatabaseDeletedAndCheckIfReferenced(
     int64_t database_id) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(KeyPrefix::IsValidDatabaseId(database_id));
-  const auto& db_pair = blob_reference_tracker_.find(database_id);
+  auto db_pair = blob_reference_tracker_.find(database_id);
   if (db_pair == blob_reference_tracker_.end())
     return false;
 
@@ -42,7 +42,7 @@ bool IndexedDBActiveBlobRegistry::MarkBlobInfoDeletedAndCheckIfReferenced(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK_NE(blob_number, DatabaseMetaDataKey::kAllBlobsNumber);
   DCHECK(KeyPrefix::IsValidDatabaseId(database_id));
-  const auto& db_pair = blob_reference_tracker_.find(database_id);
+  auto db_pair = blob_reference_tracker_.find(database_id);
   if (db_pair == blob_reference_tracker_.end())
     return false;
 
@@ -112,7 +112,7 @@ void IndexedDBActiveBlobRegistry::MarkBlobInactive(int64_t database_id,
 
   DCHECK(KeyPrefix::IsValidDatabaseId(database_id));
   DCHECK(DatabaseMetaDataKey::IsValidBlobNumber(blob_number));
-  const auto& db_pair = blob_reference_tracker_.find(database_id);
+  auto db_pair = blob_reference_tracker_.find(database_id);
   if (db_pair == blob_reference_tracker_.end()) {
     NOTREACHED();
     return;
@@ -124,7 +124,7 @@ void IndexedDBActiveBlobRegistry::MarkBlobInactive(int64_t database_id,
     return;
   }
   bool delete_blob_in_backend = false;
-  const auto& deleted_database_it = deleted_dbs_.find(database_id);
+  auto deleted_database_it = deleted_dbs_.find(database_id);
   bool db_marked_for_deletion = deleted_database_it != deleted_dbs_.end();
   // Don't bother deleting the file if we're going to delete its whole
   // database directory soon.
