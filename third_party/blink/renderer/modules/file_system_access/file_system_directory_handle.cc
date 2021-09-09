4a7c23b2c5bd62b1281d5421caeb7253cd6add29
Austin Sullivan <asully@chromium.org>
2021-09-09 15:43:36 +0000

    Add FileSystemHandle::move() and FileSystemHandle::rename() methods
    Currently, it is not possible to move or rename a file or directory
    without creating a new file/directory, copying over data (recursively,
    in the case of a directory), and removing the original.
    
    This CL allows for the atomic moving of a file or directory on the
    local file system without needing to duplicate data.
    
    Moves to non-local file systems will are not guaranteed to be atomic
    and will involve duplicating data.
    
    PR: https://github.com/WICG/file-system-access/pull/317
    
    Bug: 1140805
    Change-Id: I774ed1d9616249b6ecc80783db48a7bfee915aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2984739
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919810}

--- third_party/blink/renderer/modules/file_system_access/file_system_directory_handle.cc
+++ third_party/blink/renderer/modules/file_system_access/file_system_directory_handle.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
@@ -240,6 +241,33 @@ void FileSystemDirectoryHandle::RequestPermissionImpl(
   }
 
   mojo_ptr_->RequestPermission(writable, std::move(callback));
+}
+
+void FileSystemDirectoryHandle::RenameImpl(
+    const String& new_entry_name,
+    base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr)> callback) {
+  if (!mojo_ptr_.is_bound()) {
+    std::move(callback).Run(mojom::blink::FileSystemAccessError::New(
+        mojom::blink::FileSystemAccessStatus::kInvalidState,
+        base::File::Error::FILE_ERROR_FAILED, "Context Destroyed"));
+    return;
+  }
+
+  mojo_ptr_->Rename(new_entry_name, std::move(callback));
+}
+
+void FileSystemDirectoryHandle::MoveImpl(
+    mojo::PendingRemote<mojom::blink::FileSystemAccessTransferToken> dest,
+    const String& new_entry_name,
+    base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr)> callback) {
+  if (!mojo_ptr_.is_bound()) {
+    std::move(callback).Run(mojom::blink::FileSystemAccessError::New(
+        mojom::blink::FileSystemAccessStatus::kInvalidState,
+        base::File::Error::FILE_ERROR_FAILED, "Context Destroyed"));
+    return;
+  }
+
+  mojo_ptr_->Move(std::move(dest), new_entry_name, std::move(callback));
 }
 
 void FileSystemDirectoryHandle::RemoveImpl(
--- third_party/blink/renderer/modules/file_system_access/file_system_file_handle.cc
+++ third_party/blink/renderer/modules/file_system_access/file_system_file_handle.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/file_metadata.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
@@ -192,6 +193,33 @@ void FileSystemFileHandle::RequestPermissionImpl(
   }
 
   mojo_ptr_->RequestPermission(writable, std::move(callback));
+}
+
+void FileSystemFileHandle::RenameImpl(
+    const String& new_entry_name,
+    base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr)> callback) {
+  if (!mojo_ptr_.is_bound()) {
+    std::move(callback).Run(mojom::blink::FileSystemAccessError::New(
+        mojom::blink::FileSystemAccessStatus::kInvalidState,
+        base::File::Error::FILE_ERROR_FAILED, "Context Destroyed"));
+    return;
+  }
+
+  mojo_ptr_->Rename(new_entry_name, std::move(callback));
+}
+
+void FileSystemFileHandle::MoveImpl(
+    mojo::PendingRemote<mojom::blink::FileSystemAccessTransferToken> dest,
+    const String& new_entry_name,
+    base::OnceCallback<void(mojom::blink::FileSystemAccessErrorPtr)> callback) {
+  if (!mojo_ptr_.is_bound()) {
+    std::move(callback).Run(mojom::blink::FileSystemAccessError::New(
+        mojom::blink::FileSystemAccessStatus::kInvalidState,
+        base::File::Error::FILE_ERROR_FAILED, "Context Destroyed"));
+    return;
+  }
+
+  mojo_ptr_->Move(std::move(dest), new_entry_name, std::move(callback));
 }
 
 void FileSystemFileHandle::RemoveImpl(
