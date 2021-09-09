c673cc9d351f959fb68a93e3e1b06180d0e850c0
Kristi Park <kristipark@chromium.org>
2021-09-09 01:25:45 +0000

    [CrOSSharingHub] Close sharesheet if tab is closed
    Fix issue where sharesheet remains open if the associated tab is closed.
    
    Bug: 1234050
    Change-Id: Icf5fb86853f4f4d1889b4b5d52c81f7b47e7aa0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149505
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919546}

--- chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller.h
+++ chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller.h
@@ -22,6 +22,10 @@ namespace views {
 class Button;
 }  // namespace views
 
+namespace sharesheet {
+class SharesheetController;
+}  // namespace sharesheet
+
 namespace sharing_hub {
 
 class SharingHubBubbleView;
@@ -81,6 +85,7 @@ class SharingHubBubbleController
   void OnSharesheetClosed(views::Widget::ClosedReason reason);
 
   views::ViewTracker highlighted_button_tracker_;
+  sharesheet::SharesheetController* sharesheet_controller_ = nullptr;
 #endif
 
   // The web_contents associated with this controller.
