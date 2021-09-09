b568651cb034aa62719c30522775917cae518100
Joey Arhar <jarhar@chromium.org>
2021-09-09 18:03:41 +0000

    Revert ScopedForcedUpdate Range changes
    This reverts two patches:
    http://crrev.com/919277
    http://crrev.com/919407
    The first patch caused a memory regression, and the second patch is
    based on it so it must also be reverted.
    The memory regression probably won't be too hard to fix, but since it's
    this close to the branch I'm just going to revert them.
    
    Bug: 1236774
    Fixed: 1247842
    Change-Id: I5c5bb3c3a0a53ba4f6c2ba87a28f0b00fe6e0294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152055
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919874}

--- third_party/blink/renderer/core/dom/range.cc
+++ third_party/blink/renderer/core/dom/range.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/range.h"
 
-#include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/dom/character_data.h"
 #include "third_party/blink/renderer/core/dom/container_node.h"
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
@@ -1592,7 +1591,6 @@ void Range::expand(const String& unit, ExceptionState& exception_state) {
 }
 
 DOMRectList* Range::getClientRects() const {
-  DisplayLockUtilities::ScopedForcedUpdate force_locks(this);
   owner_document_->UpdateStyleAndLayout(DocumentUpdateReason::kJavaScript);
 
   Vector<FloatQuad> quads;
@@ -1714,7 +1712,6 @@ void Range::GetBorderAndTextQuads(Vector<FloatQuad>& quads) const {
 }
 
 FloatRect Range::BoundingRect() const {
-  DisplayLockUtilities::ScopedForcedUpdate force_locks(this);
   owner_document_->UpdateStyleAndLayout(DocumentUpdateReason::kJavaScript);
 
   Vector<FloatQuad> quads;
