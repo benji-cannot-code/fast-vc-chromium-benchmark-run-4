9926234638f37ffa89466970fc56c5eb377d25fa
Lei Zhang <thestig@chromium.org>
2021-09-09 17:04:44 +0000

    Add kPrintWithPostScriptType42Fonts feature.
    Add an off by default base::Feature for controlling whether PostScript
    printing generates Type 42 fonts on Windows. This will speed up printing
    for some printers that support PostScript level 3 and have a TrueType
    rasterizer. Since this feature is experimental, having a base::Feature
    toggle will allow the feature to be rolled out in a controlled manner.
    
    This CL includes:
    - Plumbing for the browser to tell the PdfToEmfConverter service to use
      the new printing mode.
    - New Printing.ConversionSize metrics to see how the new printing modes
      perform size-wise.
    - A very basic sanity test.
    - Control for the feature in chrome://flags.
    - Control for the feature via an enterprise policy.
    
    Also fix another Windows-only printing enterprise policy to use
    "MS_WIN_NAME" when referring to Windows.
    
    Bug: 1232526
    Change-Id: I4f3a333e6c83fb56d4482c8aa3af4fcc6b8533dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150776
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919845}

--- chrome/services/printing/public/mojom/pdf_render_settings_mojom_traits.h
+++ chrome/services/printing/public/mojom/pdf_render_settings_mojom_traits.h
@@ -35,6 +35,8 @@ struct EnumTraits<printing::mojom::PdfRenderSettings_Mode,
         return MojomMode::EMF_WITH_REDUCED_RASTERIZATION;
       case PrintMode::EMF_WITH_REDUCED_RASTERIZATION_AND_GDI_TEXT:
         return MojomMode::EMF_WITH_REDUCED_RASTERIZATION_AND_GDI_TEXT;
+      case PrintMode::POSTSCRIPT_LEVEL3_WITH_TYPE42_FONTS:
+        return MojomMode::POSTSCRIPT_LEVEL3_WITH_TYPE42_FONTS;
 #endif
     }
     NOTREACHED() << "Unknown mode " << static_cast<int>(mode);
@@ -67,6 +69,9 @@ struct EnumTraits<printing::mojom::PdfRenderSettings_Mode,
         return true;
       case MojomMode::EMF_WITH_REDUCED_RASTERIZATION_AND_GDI_TEXT:
         *output = PrintMode::EMF_WITH_REDUCED_RASTERIZATION_AND_GDI_TEXT;
+        return true;
+      case MojomMode::POSTSCRIPT_LEVEL3_WITH_TYPE42_FONTS:
+        *output = PrintMode::POSTSCRIPT_LEVEL3_WITH_TYPE42_FONTS;
         return true;
 #endif
     }
