0ea77496db2c0ab205bd24a3603ac0aa47e89184
Chad Duffin <chadduffin@chromium.org>
2021-09-09 18:22:59 +0000

    [CrOS Bluetooth] Use IsBluetoothEnabledOrEnabling helper.
    This change updates two callsites that check whether Bluetooth is
    enabled or enabling to use the newly added IsBluetoothEnabledOrEnabling
    helper function.
    
    Bug: 1010321
    Test: Built chrome and executed related unit tests.
    Change-Id: I99728ba44f6c41703586b896fbd055f78b3d8d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145446
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919882}

--- chromeos/services/bluetooth_config/device_cache.cc
+++ chromeos/services/bluetooth_config/device_cache.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/bluetooth_config/device_cache.h"
 
 #include "chromeos/services/bluetooth_config/adapter_state_controller.h"
+#include "chromeos/services/bluetooth_config/public/cpp/cros_bluetooth_config_util.h"
 
 namespace chromeos {
 namespace bluetooth_config {
@@ -62,8 +63,8 @@ void DeviceCache::NotifyUnpairedDevicesListChanged() {
 bool DeviceCache::IsBluetoothEnabledOrEnabling() const {
   const mojom::BluetoothSystemState adapter_state =
       adapter_state_controller_->GetAdapterState();
-  return adapter_state == mojom::BluetoothSystemState::kEnabled ||
-         adapter_state == mojom::BluetoothSystemState::kEnabling;
+  return ::chromeos::bluetooth_config::IsBluetoothEnabledOrEnabling(
+      adapter_state);
 }
 
 }  // namespace bluetooth_config
