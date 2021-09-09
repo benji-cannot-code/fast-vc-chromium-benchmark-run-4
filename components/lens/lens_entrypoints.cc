79a7977807fdee4d5f8edc081a31b8c355bd5899
Ali Stanfield <stanfield@google.com>
2021-09-09 23:52:16 +0000

    Add surface query param for side panel requests.
    Bug: 1234592
    Change-Id: I18c4bfffd4320969a0cac770725ea913ee43ff43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149614
    Commit-Queue: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ben Goldberger <benwgold@google.com>
    Cr-Commit-Position: refs/heads/main@{#920021}

--- components/lens/lens_entrypoints.cc
+++ components/lens/lens_entrypoints.cc
@@ -7,28 +7,49 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/strcat.h"
 
-namespace lens {
+namespace {
 
 // Entry point string names.
-constexpr char kEntryPointQueryParameter[] = "ep=";
+constexpr char kEntryPointQueryParameter[] = "ep";
 constexpr char kChromeRegionSearchMenuItem[] = "crs";
 constexpr char kChromeSearchWithGoogleLensContextMenuItem[] = "ccm";
 
-std::string GetQueryParameterFromEntryPoint(EntryPoint ep) {
-  std::string query_parameter = std::string(kEntryPointQueryParameter);
+constexpr char kSurfaceQueryParameter[] = "s";
+constexpr char kSidePanel[] = "csp";
+
+void AppendQueryParam(std::string* query_string,
+                      const char name[],
+                      const char value[]) {
+  if (!query_string->empty()) {
+    base::StrAppend(query_string, {"&"});
+  }
+  base::StrAppend(query_string, {name, "=", value});
+}
+
+}  // namespace
+
+namespace lens {
+
+std::string GetQueryParametersForLensRequest(EntryPoint ep,
+                                             bool is_side_panel_request) {
+  std::string query_string;
   switch (ep) {
     case CHROME_REGION_SEARCH_MENU_ITEM:
-      base::StrAppend(&query_parameter, {kChromeRegionSearchMenuItem});
+      AppendQueryParam(&query_string, kEntryPointQueryParameter,
+                       kChromeRegionSearchMenuItem);
       break;
     case CHROME_SEARCH_WITH_GOOGLE_LENS_CONTEXT_MENU_ITEM:
-      base::StrAppend(&query_parameter,
-                      {kChromeSearchWithGoogleLensContextMenuItem});
+      AppendQueryParam(&query_string, kEntryPointQueryParameter,
+                       kChromeSearchWithGoogleLensContextMenuItem);
       break;
     default:
       // Empty strings are ignored when query parameters are built.
-      return std::string();
+      break;
   }
-  return query_parameter;
+  if (is_side_panel_request) {
+    AppendQueryParam(&query_string, kSurfaceQueryParameter, kSidePanel);
+  }
+  return query_string;
 }
 
 }  // namespace lens
