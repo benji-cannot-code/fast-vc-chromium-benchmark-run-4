894f040bf0303f0bbee217086d4f90d8e486cb8a
Colin Blundell <blundell@chromium.org>
2021-09-09 07:18:30 +0000

    [Safe Browsing] Modify NavigationObserverManager interface
    SafeBrowsingNavigationObserverManager::IsEnabledAndReady() takes in a
    SafeBrowsingServiceInterface*. However, //weblayer's SafeBrowsingService
    doesn't implement SafeBrowsingServiceInterface. Moreover, it can't
    easily be made to at this point. This fact hinders upcoming bringup of
    SafeBrowsingNavigationObserverManager in WebLayer.
    
    Fortunately, all that SafeBrowsingNavigationObserverManager needs to
    know is whether the SafeBrowsingService exists in the embedder. This CL
    changes the interface in question to taken in a boolean.
    
    Bug: 1231997
    Change-Id: I72be3b55b87cf93a18f5547b4cea0f553aed62b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3147334
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919669}

--- chrome/browser/safe_browsing/download_protection/download_request_maker.cc
+++ chrome/browser/safe_browsing/download_protection/download_request_maker.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
 #include "components/safe_browsing/core/common/utils.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_item_utils.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
