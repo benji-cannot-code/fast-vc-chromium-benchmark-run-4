6e297b9f2f056954cee53b0a4a62b0e2fa434448
Robin Lewis <wrlewis@google.com>
2021-09-09 22:02:05 +0000

    Revert "Reland "Add view for Lens side panel.""
    This reverts commit 5647685014cd0fd663c5a6d022b98d14cb5b74e0.
    
    Reason for revert: Breaks Mac compile: https://ci.chromium.org/ui/p/chrome/builders/ci/mac64/30386/overview
    
    
    Original change's description:
    > Reland "Add view for Lens side panel."
    >
    > This is a reland of 156e573b5b8778a04d240ac21494b74b7db30bf1
    >
    > Original change's description:
    > > Add view for Lens side panel.
    > >
    > > This view includes a header for the side panel and a WebView underneath it contained in a FlexLayout.
    > > Due to using a branded icon, this change also guards the feature on Google Chrome Branding.
    > >
    > > Screenshot: https://screenshot.googleplex.com/C3CnYy68aXBGedG.png (Red box is WebView)
    > >
    > > Bug: 1234592, b/198482172
    > > Change-Id: I64662cbb82c01cdf5592cfb4f2e6588feda3d18b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3106037
    > > Commit-Queue: Juan Mojica <juanmojica@google.com>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Reviewed-by: Evan Stade <estade@chromium.org>
    > > Reviewed-by: Ben Goldberger <benwgold@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#919496}
    >
    > Bug: 1234592, b/198482172
    > Change-Id: Ia219e9ccf09b2108ee125c613e61d7527534951b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149449
    > Reviewed-by: Ben Goldberger <benwgold@google.com>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    > Commit-Queue: Juan Mojica <juanmojica@google.com>
    > Cr-Commit-Position: refs/heads/main@{#919911}
    
    Bug: 1234592, b/198482172
    Change-Id: I11db518594c22e2bfed1be13e296d14e698e9722
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150815
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Robin Lewis <wrlewis@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Owners-Override: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919978}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -231,8 +231,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #endif
 
-#if (defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)) && \
-    BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)
 #include "chrome/browser/lens/region_search/lens_region_search_controller.h"
 #endif
 
@@ -985,8 +984,7 @@ void RenderViewContextMenu::InitMenu() {
     AppendCurrentExtensionItems();
   }
 
-#if (defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)) && \
-    BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)
   if (content_type_->SupportsGroup(
           ContextMenuContentType::ITEM_GROUP_LENS_REGION_SEARCH)) {
     if (IsLensRegionSearchEnabled()) {
@@ -3057,14 +3055,10 @@ bool RenderViewContextMenu::IsQRCodeGeneratorEnabled() const {
 }
 
 bool RenderViewContextMenu::IsLensRegionSearchEnabled() const {
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   return base::FeatureList::IsEnabled(lens::features::kLensRegionSearch) &&
          search::DefaultSearchProviderIsGoogle(GetProfile()) &&
          GetPrefs(browser_context_)
              ->GetBoolean(prefs::kLensRegionSearchEnabled);
-#else
-  return false;
-#endif
 }
 
 void RenderViewContextMenu::AppendQRCodeGeneratorItem(bool for_image,
@@ -3309,8 +3303,7 @@ void RenderViewContextMenu::ExecSearchLensForImage() {
 }
 
 void RenderViewContextMenu::ExecLensRegionSearch() {
-#if (defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)) && \
-    BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)
   if (!lens_region_search_controller_)
     lens_region_search_controller_ =
         std::make_unique<lens::LensRegionSearchController>(
--- chrome/browser/renderer_context_menu/render_view_context_menu_unittest.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
@@ -711,8 +710,7 @@ TEST_F(RenderViewContextMenuPrefsTest, ShowAllPasswordsIncognito) {
 }
 
 // TODO(crbug/1229334): Add Mac support for Lens Region Search feature.
-#if (defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS)) && \
-    BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS)
 // Verify that the Lens Region Search menu item is displayed when the feature
 // is enabled.
 TEST_F(RenderViewContextMenuPrefsTest, LensRegionSearch) {
