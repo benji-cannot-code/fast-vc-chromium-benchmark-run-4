f40b057c4e364bb74cc05adc06d5d8b3b986c941
Hiroki Nakagawa <nhiroki@chromium.org>
2021-09-09 14:36:54 +0000

    Prerender: Make "Purpose: prefetch" header CORS-exempted
    Requests from prerendered pages have the "Purpose: prefetch" header to
    let servers know the intention of the requests. Before this CL, the
    header was added to regular header lists, but this caused CORS preflight
    requests for cross-origin subresources that should be avoided in this
    case. To fix it, this CL adds the header to the CORS-exempted header
    lists instead
    
    The tests were originally written as wpt_internals using service
    workers, but this CL moves them to content_browsertests. This is
    because it seems that service workers cannot see the CORS-exempted
    header on the FetchEvent. Alternatively we could rewrite them as WPT's
    custom request henaders, but that would take longer time as the test
    setup is complicated: it needs to coordinate various subresource
    requests and nested iframe loading in a prerendered page isolated from
    the main test page. In addition to that, it has to initiate the requests
    before and after activation. Therefore, as a stopgap, this CL uses the
    browser test infra instead.
    
    Bug: 1239500
    Change-Id: I0ea93953d4c7c9976f162372f3965a6fd8cf0c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3142387
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#919788}

--- content/public/test/prerender_test_util.cc
+++ content/public/test/prerender_test_util.cc
@@ -406,7 +406,9 @@ net::test_server::HttpRequest::HeaderMap PrerenderTestHelper::GetRequestHeaders(
     const GURL& url) {
   EXPECT_TRUE(content::BrowserThread::CurrentlyOn(BrowserThread::UI));
   base::AutoLock auto_lock(lock_);
-  return request_headers_by_path_[url.PathForRequest()];
+  std::string path = url.PathForRequest();
+  DCHECK(base::Contains(request_headers_by_path_, path)) << path;
+  return request_headers_by_path_[path];
 }
 
 void PrerenderTestHelper::WaitForRequest(const GURL& url, int count) {
