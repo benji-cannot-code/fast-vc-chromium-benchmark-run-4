2ab05a86d1d1ef546abb9d6fd5f2e02e98a018a4
L. David Baron <dbaron@chromium.org>
2021-09-10 01:57:22 +0000

    Make TransformState::Move accumulate into existing transform more often.
    The substantive change here is the removal of the test of
    accumulating_transform_ in TransformState::Move.  At least when
    direction_ is kApplyTransformDirection, optimizing away the modification
    to the existing transform into a move is not safe.
    
    The remainder of the code changes are removing accumulating_transform_,
    since its remaining uses are all in DCHECK()s and its meaning is
    somewhat unclear.  It's not clear whether it's intended to be an
    indicator for whether accumulated_transform_ would be non-null were it
    not for the caching optimization in
    TransformState::FlattenWithTransform, or whether it's an indication of
    the state of flattening or preserve-3d.
    
    Without the code change here,
    virtual/transform-interop-disabled/external/wpt/css/css-transforms/transform-getBoundingClientRect-001.html
    fails (with 75 rather than 125), but
    external/wpt/css/css-transforms/transform-getBoundingClientRect-001.html
    passes both with and without the change.
    
    Bug: 1247858
    Change-Id: Id32a11b2bf66757b0dbbab251c598222f9d120a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152179
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920070}

--- third_party/blink/renderer/core/layout/geometry/transform_state.cc
+++ third_party/blink/renderer/core/layout/geometry/transform_state.cc
@@ -36,7 +36,6 @@ TransformState& TransformState::operator=(const TransformState& other) {
     last_planar_point_ = other.last_planar_point_;
   if (map_quad_)
     last_planar_quad_ = other.last_planar_quad_;
-  accumulating_transform_ = other.accumulating_transform_;
   force_accumulating_transform_ = other.force_accumulating_transform_;
   direction_ = other.direction_;
 
@@ -78,7 +77,7 @@ void TransformState::Move(const PhysicalOffset& offset,
     accumulated_offset_ += offset;
   } else {
     ApplyAccumulatedOffset();
-    if (accumulating_transform_ && accumulated_transform_) {
+    if (accumulated_transform_) {
       // If we're accumulating into an existing transform, apply the
       // translation.
       TranslateTransform(offset);
@@ -87,7 +86,6 @@ void TransformState::Move(const PhysicalOffset& offset,
       TranslateMappedCoordinates(offset);
     }
   }
-  accumulating_transform_ = accumulate == kAccumulateTransform;
 }
 
 void TransformState::ApplyAccumulatedOffset() {
@@ -147,8 +145,6 @@ void TransformState::ApplyTransform(
       FlattenWithTransform(*final_transform);
     }
   }
-  accumulating_transform_ =
-      accumulate == kAccumulateTransform || force_accumulating_transform_;
 }
 
 void TransformState::Flatten() {
@@ -157,7 +153,6 @@ void TransformState::Flatten() {
   ApplyAccumulatedOffset();
 
   if (!accumulated_transform_) {
-    accumulating_transform_ = false;
     return;
   }
 
@@ -193,7 +188,6 @@ FloatQuad TransformState::MappedQuad() const {
 
 const TransformationMatrix& TransformState::AccumulatedTransform() const {
   DCHECK(force_accumulating_transform_);
-  DCHECK(accumulating_transform_);
   return *accumulated_transform_;
 }
 
@@ -217,7 +211,6 @@ void TransformState::FlattenWithTransform(const TransformationMatrix& t) {
   // preserve-3d and flat elements.
   if (accumulated_transform_)
     accumulated_transform_->MakeIdentity();
-  accumulating_transform_ = false;
 }
 
 }  // namespace blink
