0b380184737ab8498c6721f82e7f6167397030b7
Mila Green <milagreen@chromium.org>
2021-09-10 17:38:23 +0000

    Updater: Install the new `Keystone` bundle with the Updater.
    This change creates a new app bundle named "GoogleSoftwareUpdate".
    The bundle contains the replacement for the "ksadmin" tool, used by
    Keystone's registration framework. The structure is as follows:
    GoogleUpdater
      Contents
        Helpers
          ksadmin
        Info.plist
        MacOS
          GoogleSoftwareUpdate
        PkgInfo
    
    Note: As there are many references to GoogleSoftwareUpdate.bundle and
    ultimately we are trying to provide an in-place replacement for it,
    I had to change the "mac_app_bundle" template to support the production
    of a ".bundle".
    
    Bug: 1247725
    Change-Id: Ib717aab5682a04d84e50334f5b63f458c98c8a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149833
    Commit-Queue: Mila Green <milagreen@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920275}

--- chrome/updater/mac/keystone/ksadmin.h
+++ chrome/updater/mac/keystone/ksadmin.h
@@ -3,8 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/updater/mac/ks_admin/ks_admin.h"
+#ifndef CHROME_UPDATER_MAC_KEYSTONE_KSADMIN_H_
+#define CHROME_UPDATER_MAC_KEYSTONE_KSADMIN_H_
 
-int main(int argc, char* argv[]) {
-  return updater::KSAdminAppMain(argc, argv);
-}
+namespace updater {
+
+int KSAdminAppMain(int argc, char* argv[]);
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_MAC_KEYSTONE_KSADMIN_H_
--- chrome/updater/mac/keystone/ksadmin_main.cc
+++ chrome/updater/mac/keystone/ksadmin_main.cc
@@ -3,13 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_UPDATER_MAC_KS_ADMIN_KS_ADMIN_H_
-#define CHROME_UPDATER_MAC_KS_ADMIN_KS_ADMIN_H_
+#include "chrome/updater/mac/keystone/ksadmin.h"
 
-namespace updater {
-
-int KSAdminAppMain(int argc, char* argv[]);
-
-}  // namespace updater
-
-#endif  // CHROME_UPDATER_MAC_KS_ADMIN_KS_ADMIN_H_
+int main(int argc, char* argv[]) {
+  return updater::KSAdminAppMain(argc, argv);
+}
