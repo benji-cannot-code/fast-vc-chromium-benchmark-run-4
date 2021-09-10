a14c09df33c90a59b20d64e2188fccf578ff02c3
Juan Mojica <juanmojica@google.com>
2021-09-10 02:59:18 +0000

    Reland "Add view for Lens side panel."
    This is a reland of 156e573b5b8778a04d240ac21494b74b7db30bf1
    
    Original change's description:
    > Add view for Lens side panel.
    >
    > This view includes a header for the side panel and a WebView underneath it contained in a FlexLayout.
    > Due to using a branded icon, this change also guards the feature on Google Chrome Branding.
    >
    > Screenshot: https://screenshot.googleplex.com/C3CnYy68aXBGedG.png (Red box is WebView)
    >
    > Bug: 1234592, b/198482172
    > Change-Id: I64662cbb82c01cdf5592cfb4f2e6588feda3d18b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3106037
    > Commit-Queue: Juan Mojica <juanmojica@google.com>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Ben Goldberger <benwgold@google.com>
    > Cr-Commit-Position: refs/heads/main@{#919496}
    
    Bug: 1234592, b/198482172
    Change-Id: I34e69bb9de56831e25986b497a7e7d8f63c6af38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152755
    Reviewed-by: Ben Goldberger <benwgold@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#920096}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -231,7 +231,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #endif
 
-#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)
+#if (defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)) && \
+    BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #include "chrome/browser/lens/region_search/lens_region_search_controller.h"
 #endif
 
@@ -1654,7 +1655,8 @@ void RenderViewContextMenu::AppendPageItems() {
                                   IDS_CONTENT_CONTEXT_SAVEPAGEAS);
   menu_model_.AddItemWithStringId(IDC_PRINT, IDS_CONTENT_CONTEXT_PRINT);
   AppendMediaRouterItem();
-#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)
+#if (defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)) && \
+    BUILDFLAG(GOOGLE_CHROME_BRANDING)
   if (IsLensRegionSearchEnabled()) {
     AppendLensRegionSearchItem();
   }
@@ -3058,10 +3060,14 @@ bool RenderViewContextMenu::IsQRCodeGeneratorEnabled() const {
 }
 
 bool RenderViewContextMenu::IsLensRegionSearchEnabled() const {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   return base::FeatureList::IsEnabled(lens::features::kLensRegionSearch) &&
          search::DefaultSearchProviderIsGoogle(GetProfile()) &&
          GetPrefs(browser_context_)
              ->GetBoolean(prefs::kLensRegionSearchEnabled);
+#else
+  return false;
+#endif
 }
 
 void RenderViewContextMenu::AppendQRCodeGeneratorItem(bool for_image,
@@ -3306,7 +3312,8 @@ void RenderViewContextMenu::ExecSearchLensForImage() {
 }
 
 void RenderViewContextMenu::ExecLensRegionSearch() {
-#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)
+#if (defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_LINUX)) && \
+    BUILDFLAG(GOOGLE_CHROME_BRANDING)
   if (!lens_region_search_controller_)
     lens_region_search_controller_ =
         std::make_unique<lens::LensRegionSearchController>(
--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -36,7 +36,7 @@ const base::FeatureParam<bool> kEnableUKMLoggingForImageSearch{
     &kLensStandalone, "enable-ukm-logging", false};
 
 const base::FeatureParam<bool> kEnableSidePanelForLensRegionSearch{
-    &kLensRegionSearch, "enable-side-panel", false};
+    &kLensRegionSearch, "enable-side-panel", true};
 
 const base::FeatureParam<bool> kEnableSidePanelForLensImageSearch{
     &kLensStandalone, "enable-side-panel", false};
