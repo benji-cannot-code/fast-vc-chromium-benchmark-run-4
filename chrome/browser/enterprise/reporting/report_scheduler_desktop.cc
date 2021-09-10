17b49b89ab550eac6529160f527f7de82f717e96
Owen Min <zmin@chromium.org>
2021-09-10 16:18:41 +0000

    Update Extension Workflow experimental config
    Extension request is now uploaded with ERP by default.
    
    Bug: 1247355
    Change-Id: If2470a2d51a4ac771d69d84829732b617fde59d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149580
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#920241}

--- chrome/browser/enterprise/reporting/report_scheduler_desktop.cc
+++ chrome/browser/enterprise/reporting/report_scheduler_desktop.cc
@@ -47,7 +47,7 @@ bool IsRealTimePipielineEnabled() {
   return reporting::ReportQueueProvider::
              IsEncryptedReportingPipelineEnabled() &&
          base::GetFieldTrialParamByFeatureAsBool(
-             features::kEnterpriseRealtimeExtensionRequest, "with_erp", false);
+             features::kEnterpriseRealtimeExtensionRequest, "with_erp", true);
 }
 
 }  // namespace
