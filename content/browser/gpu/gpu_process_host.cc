8efe3c95022d3517ae97b3c7ddc2887538ebf05c
Jeffrey Kardatzke <jkardatzke@google.com>
2021-09-10 00:19:27 +0000

    lacros: Add support for protected AV1 content
    This uses a similar mechanism we used for enabling other protected
    content features in Lacros chrome for handling the AV1 capability.
    
    BUG=b:155509025
    TEST=GPU/renderer on Lacros chrome on volteer gets the av1 flag
    
    Change-Id: I889df67c5c45619344839693c351dd586d6b0294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150254
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/main@{#920030}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -318,6 +318,7 @@ static const char* const kSwitchNames[] = {
     switches::kLacrosEnablePlatformEncryptedHevc,
     switches::kLacrosEnablePlatformHevc,
     switches::kLacrosUseChromeosProtectedMedia,
+    switches::kLacrosUseChromeosProtectedAv1,
 #endif
 };
 
