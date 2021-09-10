3ba5b3f1f3172af3869cc8bb714207b5321a3bc9
Mahmoud Gawad <mgawad@google.com>
2021-09-10 22:01:57 +0000

    [Telemetry] add features chromeos_system_extension
    Enable chromeos_system_extension type of extensions to define the
    following manifest key: externally_connectable.
    
    Also allow the extension type to access the api: chrome.runtime.*.
    
    Bug: b:188897824, 1229585
    Test: added browser_test in telemetry_extension_capabilities_browser_test.cc
    Test: browser_tests --gtest_filter="*CanReceiveMessageExternal*" Green
    
    Change-Id: Ie2127565bb6d7084b5d5ff1ff27818381380ef21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3114300
    Commit-Queue: Mahmoud Gawad <mgawad@google.com>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#920394}

--- chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_browser_test.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_browser_test.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/extensions/telemetry/api/base_telemetry_extension_browser_test.h"
 
+#include <string>
+
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "components/user_manager/user_manager.h"
 #include "extensions/test/result_catcher.h"
@@ -12,6 +14,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
+
+namespace {
+
+constexpr char kManifest[] = R"(
+      {
+        // Sample telemetry extension public key. Currently, this is the only
+        // allowed extension to declare "chromeos_system_extension" key.
+        // See //chrome/common/chromeos/extensions/api/_manifest_features.json
+        "key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt2CwI94nqAQzLTBHSIwtkMlkoRyhu27rmkDsBneMprscOzl4524Y0bEA+0RSjNZB+kZgP6M8QAZQJHCpAzULXa49MooDDIdzzmqQswswguAALm2FS7XP2N0p2UYQneQce4Wehq/C5Yr65mxasAgjXgDWlYBwV3mgiISDPXI/5WG94TM2Z3PDQePJ91msDAjN08jdBme3hAN976yH3Q44M7cP1r+OWRkZGwMA6TSQjeESEuBbkimaLgPIyzlJp2k6jwuorG5ujmbAGFiTQoSDFBFCjwPEtywUMLKcZjH4fD76pcIQIdkuuzRQCVyuImsGzm1cmGosJ/Z4iyb80c1ytwIDAQAB",
+        "name": "Test Telemetry Extension",
+        "version": "1",
+        "manifest_version": 3,
+        "chromeos_system_extension": {},
+        "background": {
+          "service_worker": "sw.js"
+        },
+        "permissions": [ "os.telemetry" ],
+        "externally_connectable": {
+          "ids": [
+            "*"
+          ]
+        }
+      }
+    )";
+
+}  // namespace
 
 BaseTelemetryExtensionBrowserTest::BaseTelemetryExtensionBrowserTest() =
     default;
@@ -29,33 +57,31 @@ void BaseTelemetryExtensionBrowserTest::SetUpOnMainThread() {
   user_manager->SetOwnerId(regular_user);
 }
 
-void BaseTelemetryExtensionBrowserTest::CreateExtensionAndRunServiceWorker(
+const extensions::Extension*
+BaseTelemetryExtensionBrowserTest::LoadExtensionWithManifestAndServiceWorker(
+    const std::string& manifest_content,
     const std::string& service_worker_content) {
   extensions::TestExtensionDir test_dir;
-  test_dir.WriteManifest(R"(
-      {
-        // Sample telemetry extension public key. Currently, this is the only
-        // allowed extension to declare "chromeos_system_extension" key.
-        // See //chrome/common/chromeos/extensions/api/_manifest_features.json
-        "key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt2CwI94nqAQzLTBHSIwtkMlkoRyhu27rmkDsBneMprscOzl4524Y0bEA+0RSjNZB+kZgP6M8QAZQJHCpAzULXa49MooDDIdzzmqQswswguAALm2FS7XP2N0p2UYQneQce4Wehq/C5Yr65mxasAgjXgDWlYBwV3mgiISDPXI/5WG94TM2Z3PDQePJ91msDAjN08jdBme3hAN976yH3Q44M7cP1r+OWRkZGwMA6TSQjeESEuBbkimaLgPIyzlJp2k6jwuorG5ujmbAGFiTQoSDFBFCjwPEtywUMLKcZjH4fD76pcIQIdkuuzRQCVyuImsGzm1cmGosJ/Z4iyb80c1ytwIDAQAB",
-        "name": "Test Telemetry Extension",
-        "version": "1",
-        "manifest_version": 3,
-        "chromeos_system_extension": {},
-        "background": {
-          "service_worker": "sw.js"
-        },
-        "permissions": [ "os.telemetry" ]
-      }
-    )");
+  test_dir.WriteManifest(manifest_content);
   test_dir.WriteFile(FILE_PATH_LITERAL("sw.js"), service_worker_content);
 
+  return LoadExtension(test_dir.UnpackedPath());
+}
+
+const extensions::Extension*
+BaseTelemetryExtensionBrowserTest::LoadExtensionWithServiceWorker(
+    const std::string& service_worker_content) {
+  return LoadExtensionWithManifestAndServiceWorker(kManifest,
+    service_worker_content);
+}
+
+void BaseTelemetryExtensionBrowserTest::CreateExtensionAndRunServiceWorker(
+    const std::string& service_worker_content) {
   // Must be initialised before loading extension.
   extensions::ResultCatcher result_catcher;
 
-  const extensions::Extension* extension =
-      LoadExtension(test_dir.UnpackedPath());
-  ASSERT_TRUE(extension);
+  ASSERT_TRUE(LoadExtensionWithManifestAndServiceWorker(kManifest,
+    service_worker_content));
 
   EXPECT_TRUE(result_catcher.GetNextResult()) << result_catcher.message();
 }
