6df94f5c2633f1a51dc9dd405c05bd9d3e0f473f
Alan Cutter <alancutter@chromium.org>
2021-09-10 00:22:34 +0000

    Remove feature flags for default Chat/Meet apps
    This feature has shipped in M92 and these flags are no longer needed.
    
    Bug: 1203138
    Change-Id: I188f1449aad03c4f81efd8b937c58cfb948d3e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149802
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920035}

--- chrome/browser/web_applications/preinstalled_web_apps/google_meet.cc
+++ chrome/browser/web_applications/preinstalled_web_apps/google_meet.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/web_applications/preinstalled_web_apps/google_meet.h"
 
-#include "chrome/browser/web_applications/preinstalled_app_install_features.h"
-
 namespace web_app {
 
 ExternalInstallOptions GetConfigForGoogleMeet() {
@@ -18,7 +16,6 @@ ExternalInstallOptions GetConfigForGoogleMeet() {
 
   // Exclude managed users until we have a way for admins to block the app.
   options.user_type_allowlist = {"unmanaged", "child"};
-  options.gate_on_feature = kDefaultMeetWebApp.name;
   options.only_for_new_users = true;
   options.add_to_quick_launch_bar = false;
 
