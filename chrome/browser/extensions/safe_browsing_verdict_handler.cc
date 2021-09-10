d15c8d888c5be4a545a32f43ff7d14491db66213
Xinghui Lu <xinghuilu@chromium.org>
2021-09-10 21:32:31 +0000

    Add histograms for greylist extensions.
    Add the greylist version of ExtensionBlacklist.BlacklistInstalled and
    ExtensionBlacklist.UnblacklistInstalled.
    
    Histograms added:
    Extensions.Greylist.Disabled
    Extensions.Greylist.Enabled
    
    Bug: 1193695
    Change-Id: I8f4549cf940dec5b4eb6561c5d7d988b42fcabda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3146210
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920385}

--- chrome/browser/extensions/safe_browsing_verdict_handler.cc
+++ chrome/browser/extensions/safe_browsing_verdict_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/safe_browsing_verdict_handler.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/trace_event/trace_event.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -80,7 +81,6 @@ void SafeBrowsingVerdictHandler::Partition(const ExtensionIdSet& before,
   *no_longer = base::STLSetDifference<ExtensionIdSet>(*no_longer, unchanged);
 }
 
-// TODO(crbug.com/1193695): UMA logging
 void SafeBrowsingVerdictHandler::UpdateGreylistedExtensions(
     const ExtensionIdSet& greylist,
     const ExtensionIdSet& unchanged,
@@ -104,6 +104,8 @@ void SafeBrowsingVerdictHandler::UpdateGreylistedExtensions(
         extension_prefs_);
     extension_service_->ClearGreylistedAcknowledgedStateAndMaybeReenable(
         extension->id());
+    UMA_HISTOGRAM_ENUMERATION("Extensions.Greylist.Enabled",
+                              extension->location());
   }
 
   // Iterate over `greylist` instead of `not_yet_greylisted`, because the
@@ -126,6 +128,8 @@ void SafeBrowsingVerdictHandler::UpdateGreylistedExtensions(
         extension->id(), bitmap_greylist_state, extension_prefs_);
     extension_service_->MaybeDisableGreylistedExtension(id,
                                                         bitmap_greylist_state);
+    UMA_HISTOGRAM_ENUMERATION("Extensions.Greylist.Disabled",
+                              extension->location());
   }
 }
 
