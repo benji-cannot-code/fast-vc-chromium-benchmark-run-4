61a970ab085b6ffb9aaaebf9f33ac67cf27085fd
Elizabeth Popova <lizapopova@google.com>
2021-09-10 14:11:01 +0000

    [Autofill] Fix using wrong PopupHidingReason
    kOverlappingWithAnotherPrompt was introduced in [1], but
    kInsufficientSpace was mistakenly used instead.
    
    [1]https://chromium-review.googlesource.com/c/chromium/src/+/2953407
    
    Bug: 1204722
    Change-Id: Ia851b5f88d10188a1f7ea34620e3a00dcaef9099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151843
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/main@{#920210}

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -1427,7 +1427,7 @@ bool AutofillPopupViewNativeViews::DoUpdateBoundsAndRedrawPopup() {
 
   if (BoundsOverlapWithAnyOpenPrompt(popup_bounds,
                                      controller_->GetWebContents())) {
-    controller_->Hide(PopupHidingReason::kInsufficientSpace);
+    controller_->Hide(PopupHidingReason::kOverlappingWithAnotherPrompt);
     return false;
   }
 
