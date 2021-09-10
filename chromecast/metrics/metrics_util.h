43122e6a0e8b257b58d933b539054d48d625dd94
Ryan Keane <rwkeane@google.com>
2021-09-10 19:11:03 +0000

    Upstream CastEventBuilderImpl
    A previous CL upstreamed the CastEventBuilder interface, but left the
    implementation for a future time. Now is that future!
    
    These files have been unchanged from their upstream versions.
    Making large changes to these files is outside the scope of this CL.
    
    Bug: b/182482912, b/194439829
    Change-Id: I822ace3fb548263e2972c30402077526f23c60bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3091487
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920315}

---
+++ chromecast/metrics/metrics_util.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_METRICS_METRICS_UTIL_H_
+#define CHROMECAST_METRICS_METRICS_UTIL_H_
+
+#include "net/base/ip_address.h"
+
+namespace chromecast {
+
+// Pack last two bytes of IPv4 or IPv6 address into value used for logging
+// partial sender IP fragments (e.g. discovery code and virtual connection
+// details). If the address is empty or not valid IPv4/IPv6 then zeros will
+// be filled into the packed fragment.
+uint32_t GetIPAddressFragmentForLogging(const net::IPAddressBytes& sender_ip);
+
+}  // namespace chromecast
+
+#endif  // CHROMECAST_METRICS_METRICS_UTIL_H_
