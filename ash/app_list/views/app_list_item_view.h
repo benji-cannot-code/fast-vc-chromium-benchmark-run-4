b7b12fc9fae82f1e765b591077de0c90878de3d7
James Cook <jamescook@chromium.org>
2021-09-10 17:23:50 +0000

    app_list: Port more AppsGridView tests to support feature AppListBubble
    In particular, support drag-and-drop tests to get more coverage of
    dragging into and out of folders.
    
    - Move drag helper methods to AppsGridViewTest base class
    - Fix issue where ScrollableAppsGridView has size 0 at the start of
      tests.
    - Convert "cardified" state tests to run in tablet mode with
      AppListBubble, since clamshell mode doesn't use pages/cards.
    - Switch some other tests to run in tablet mode for the same reason.
    
    Bug: 1211608, 1214064
    Change-Id: I1ae2e5a51d6492dab99dbf396583eb1964851c67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149618
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920264}

--- ash/app_list/views/app_list_item_view.h
+++ ash/app_list/views/app_list_item_view.h
@@ -42,7 +42,7 @@ class AppListMenuModelAdapter;
 class AppListViewDelegate;
 
 namespace test {
-class AppsGridViewDragAndDropTestBase;
+class AppsGridViewTest;
 class AppListMainViewTest;
 }  // namespace test
 
@@ -209,7 +209,7 @@ class ASH_EXPORT AppListItemView : public views::Button,
   GridDelegate* grid_delegate_for_test() { return grid_delegate_; }
 
  private:
-  friend class test::AppsGridViewDragAndDropTestBase;
+  friend class test::AppsGridViewTest;
   friend class test::AppListMainViewTest;
 
   class IconImageView;
