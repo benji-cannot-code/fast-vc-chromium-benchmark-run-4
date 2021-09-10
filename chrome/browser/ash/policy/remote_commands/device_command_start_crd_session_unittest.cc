719ec7f0d0969d8265c6a8b2fc0a1554bde0befc
Jeroen Dhollander <jeroendh@google.com>
2021-09-10 07:56:42 +0000

    Ignore TerminateUponInput flag for CRD Managed User Sessions
    For managed user sessions the host user has to confirm the CRD
    connection request, which would immediately terminate the session if we
    respected TerminateUponInput.
    
    See go/remotecrdusersession-DD.
    
    Fixed: b/198134801
    Test: unit_test --gtest_filter="*CRD*" and manually deployed.
    Change-Id: Ie815d5e2d0998a20004e86d7d751a0810bb847f8
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3129701
    Reviewed-by: Michael Ershov <miersh@google.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920153}

--- chrome/browser/ash/policy/remote_commands/device_command_start_crd_session_unittest.cc
+++ chrome/browser/ash/policy/remote_commands/device_command_start_crd_session_unittest.cc
@@ -634,7 +634,7 @@ TEST_F(DeviceCommandStartCRDSessionJobTest,
 }
 
 TEST_F(DeviceCommandStartCRDSessionJobTest,
-       ShouldPassTerminateUponInputTrueToDelegate) {
+       ShouldPassTerminateUponInputTrueToDelegateForKioskUsers) {
   LogInAsAutoLaunchedKioskAppUser();
   SetOAuthToken(kTestOAuthToken);
 
@@ -777,6 +777,34 @@ TEST_F(DeviceCommandStartCRDSessionJobWithCRDForUserSessionsFeatureTest,
 
   EXPECT_EQ(true,
             crd_host_delegate().session_parameters().show_confirmation_dialog);
+}
+
+TEST_F(DeviceCommandStartCRDSessionJobWithCRDForUserSessionsFeatureTest,
+       ShouldPassTerminateUponInputFalseToDelegateForAffiliatedUser) {
+  LogInAsAffiliatedUser();
+  SetOAuthToken(kTestOAuthToken);
+
+  SetTerminateUponInput(true);
+
+  Result result = RunJobAndWaitForResult();
+  EXPECT_SUCCESS(result);
+
+  EXPECT_EQ(false,
+            crd_host_delegate().session_parameters().terminate_upon_input);
+}
+
+TEST_F(DeviceCommandStartCRDSessionJobWithCRDForUserSessionsFeatureTest,
+       ShouldPassTerminateUponInputFalseToDelegateForManagedGuestUser) {
+  LogInAsManagedGuestSessionUser();
+  SetOAuthToken(kTestOAuthToken);
+
+  SetTerminateUponInput(true);
+
+  Result result = RunJobAndWaitForResult();
+  EXPECT_SUCCESS(result);
+
+  EXPECT_EQ(false,
+            crd_host_delegate().session_parameters().terminate_upon_input);
 }
 
 }  // namespace policy
