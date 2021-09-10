3ad16ebbaf8acf76ab4340c612a95244bf26aaf8
dpapad <dpapad@chromium.org>
2021-09-10 07:24:10 +0000

    Settings TypeScript: Migrate privacy_page/ folder, part 1.
    Migrating security-keys-subpage and its dependencies.
    
    Also checking in TypeScript definitions for
    cr_fingerprint_progress_arc.js
    
    Bug: 1234307
    Change-Id: If3014050b417847fee106b9c2390aea9c2675160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151471
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920144}

---
+++ ui/webui/resources/cr_elements/cr_fingerprint/cr_fingerprint_progress_arc.m.d.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {LegacyElementMixin} from 'chrome://resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
+
+interface CrFingerprintProgressArcElement extends LegacyElementMixin,
+                                                  HTMLElement {
+  setProgress(
+      prevPercentComplete: number, currPercentComplete: number,
+      isComplete: boolean): void;
+  reset(): void;
+}
+
+export {CrFingerprintProgressArcElement};
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'cr-fingerprint-progress-arc': CrFingerprintProgressArcElement;
+  }
+}
