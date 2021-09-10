b4d9d15f678b359c44b7b70a06694ee9090cfed6
Scott Violet <sky@chromium.org>
2021-09-10 15:52:45 +0000

    weblayer: disables two tests
    These two tests continue to be flaky on the bots.
    
    BUG=1248183, 1248187
    TEST=test only change
    
    Change-Id: I4f2a0bead5e7dacb193d1b691b44805cb0708afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150340
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920233}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/FindInPageTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/FindInPageTest.java
@@ -14,6 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.FindInPageCallback;
 import org.chromium.weblayer.Tab;
@@ -144,6 +145,7 @@ public class FindInPageTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1248187")
     public void testHideOnNewTab() {
         setUp("new_browser.html");
 
