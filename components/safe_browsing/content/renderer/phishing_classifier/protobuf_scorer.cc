19daeb578309dd8be1bb3e8de1f48db689fc6eba
Robert Ogden <robertogden@chromium.org>
2021-09-10 18:49:36 +0000

    Fix tflite headers in phishing detection scorers
    The previous CL (crrev/917787) missed a couple of headers that can be
    removed or need to be ifdef'd, which should fix the build on Win Arm64.
    
    Bug: 1242884
    Change-Id: I886f253b2781b093076a90b3ebf81304cef5c727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152202
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920301}

--- components/safe_browsing/content/renderer/phishing_classifier/protobuf_scorer.cc
+++ components/safe_browsing/content/renderer/phishing_classifier/protobuf_scorer.cc
@@ -24,10 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_thread.h"
 #include "crypto/sha2.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "third_party/tflite-support/src/tensorflow_lite_support/cc/task/core/task_api_factory.h"
-#include "third_party/tflite-support/src/tensorflow_lite_support/cc/task/vision/image_classifier.h"
-#include "third_party/tflite/src/tensorflow/lite/kernels/builtin_op_kernels.h"
-#include "third_party/tflite/src/tensorflow/lite/op_resolver.h"
 
 namespace safe_browsing {
 
