3f130249d5a9d1ec21e413ede29442907a763f81
Matt Falkenhagen <falken@chromium.org>
2021-09-10 01:01:33 +0000

    payments: Simplify now that service worker is on the UI thread (pt 1).
    The "service worker core thread" is just the UI thread, so
    RunOrPostTaskOnThread just invokes the function directly.
    
    Bug: 1138155
    Change-Id: Iac3f929b822fe62855fe719bd06a02cad5bc0d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151232
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920051}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -1139,9 +1139,6 @@ StoragePartitionImpl::~StoragePartitionImpl() {
   if (GetBackgroundSyncContext())
     GetBackgroundSyncContext()->Shutdown();
 
-  if (GetPaymentAppContext())
-    GetPaymentAppContext()->Shutdown();
-
   if (GetBackgroundFetchContext())
     GetBackgroundFetchContext()->Shutdown();
 
