1ee3fe312fb37dc64d5b07489c086a34b13fe6fe
Kurt Catti-Schmidt (SCHMIDT) <kschmi@microsoft.com>
2021-09-11 00:20:06 +0000

    [GridNG] Fixing NGDisableSideEffectsScope in ComputeMinMaxSizes
    In https://chromium-review.googlesource.com/c/chromium/src/+/2991438,
    an NGDisableSideEffectsScope element was added, intended only for the
    ComputeMinMaxPhase. However, it was gated on
    "sizing_constraint != SizingConstraint::kLayout", which is not correct,
    since the row computation in ComputeMinMaxSizes is correctly using a
    sizing_constraint of SizingConstraint::kLayout.
    
    The fix here is to plumb a boolean down to indicate whether we're in
    the ComputeMinMaxSizes stage.
    
    Bug: 1248162
    Change-Id: I83c9268311f1e1b343d32e07f1cc1195ceab9447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154996
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920442}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
@@ -341,11 +341,11 @@ struct GridItemIndices {
     // Returns the size that a grid item will distribute across the tracks with
     // an intrinsic sizing function it spans in the relevant track direction.
     LayoutUnit ContributionSizeForGridItem(
-        SizingConstraint sizing_constraint,
         const GridGeometry& grid_geometry,
         const GridItemData& grid_item,
         GridTrackSizingDirection track_direction,
         GridItemContributionType contribution_type,
+        bool is_min_max_pass,
         bool* needs_additional_pass,
         bool* has_block_size_dependent_item) const;
 
@@ -404,6 +404,7 @@ struct GridItemIndices {
         SizingConstraint sizing_constraint,
         const GridGeometry& grid_geometry,
         const NGGridProperties& grid_properties,
+        const bool is_min_max_pass,
         NGGridLayoutAlgorithmTrackCollection* track_collection,
         GridItems* grid_items,
         bool* needs_additional_pass,
@@ -413,20 +414,20 @@ struct GridItemIndices {
     // size resolution defined in
     // https://drafts.csswg.org/css-grid-2/#algo-content.
     void ResolveIntrinsicTrackSizes(
-        SizingConstraint sizing_constraint,
         const GridGeometry& grid_geometry,
+        bool is_min_max_pass,
         NGGridLayoutAlgorithmTrackCollection* track_collection,
         GridItems* grid_items,
         bool* needs_additional_pass,
         bool* has_block_size_dependent_item) const;
 
     void IncreaseTrackSizesToAccommodateGridItems(
-        SizingConstraint sizing_constraint,
         const GridGeometry& grid_geometry,
         GridItems::Iterator group_begin,
         GridItems::Iterator group_end,
         const bool is_group_spanning_flex_track,
         GridItemContributionType contribution_type,
+        bool is_min_max_pass,
         NGGridLayoutAlgorithmTrackCollection* track_collection,
         bool* needs_additional_pass,
         bool* has_block_size_dependent_item) const;
@@ -442,6 +443,7 @@ struct GridItemIndices {
     void ExpandFlexibleTracks(
         SizingConstraint sizing_constraint,
         const GridGeometry& grid_geometry,
+        bool is_min_max_pass,
         NGGridLayoutAlgorithmTrackCollection* track_collection,
         GridItems* grid_items,
         bool* needs_additional_pass,
