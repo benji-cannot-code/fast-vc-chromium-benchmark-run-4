0016d3345242f601d829d81504c0c47d684ea60a
Gavin Williams <gavinwill@chromium.org>
2021-09-11 05:05:43 +0000

    scanning: Show scan progress over scanned images
    - This CL makes changes to the preview area to support showing the
      scan progress area over the existing scanned images
    
    - The CSS changes for multi-page scan are gated by
      |multi-page-scanning_|
    
    - For the whole preview area:
      - Hide the overflow when scanning to prevent the user from scrolling
    
    - For the scan progress:
      - Set position to fixed so the progress bar always shows in the same
        position even when scrolled down
      - Set |--multi-page-scan-progress-height| so the scan progress covers
        the same area as the scanned images
    
    screenshot: http://screen/98wXABuyaBDoYan
    
    Bug: 1210271
    Change-Id: Ifa84c9bc6d4c6405f28328e95e2c85fdc0e0158f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139787
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920495}

--- chrome/test/data/webui/chromeos/scanning/scan_preview_test.js
+++ chrome/test/data/webui/chromeos/scanning/scan_preview_test.js
@@ -167,6 +167,7 @@ export function scanPreviewTest() {
   test('positionActionToolbarOnSubsequentScans', () => {
     const scannedImagesDiv =
         /** @type {!HTMLElement} */ (scanPreview.$$('#scannedImages'));
+    scanPreview.objectUrls = ['image'];
     scanPreview.multiPageScanChecked = true;
     scanPreview.appState = AppState.MULTI_PAGE_SCANNING;
     return flushTasks()
@@ -353,6 +354,7 @@ export function scanPreviewTest() {
   test('resizingWindowRepositionsActionToolbar', () => {
     const scannedImagesDiv =
         /** @type {!HTMLElement} */ (scanPreview.$$('#scannedImages'));
+    scanPreview.objectUrls = ['image'];
     scanPreview.multiPageScanChecked = true;
     scanPreview.appState = AppState.MULTI_PAGE_SCANNING;
     return flushTasks()
