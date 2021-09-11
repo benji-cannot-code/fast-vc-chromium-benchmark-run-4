835ae0b22c36a785c8c85817b5dbe24ac74da473
Qasid Sadiq <qasid@google.com>
2021-09-11 02:17:30 +0000

    personalization: Refresh on disconnect and reconnect.
    The app doesn't work when offline. We should refresh the page so we can
    display the correct offline message when disconnected. And we should
    refresh on online so the collections/images can be loaded and displayed.
    
    Bug: b/195323333
    Test: By hand.
    Change-Id: I624a5459f3129e62ec174c584f35278f3bfec84a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138292
    Commit-Queue: Qasid Sadiq <qasid@google.com>
    Auto-Submit: Qasid Sadiq <qasid@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920472}

--- chromeos/components/personalization_app/resources/trusted/personalization_app.js
+++ chromeos/components/personalization_app/resources/trusted/personalization_app.js
@@ -26,3 +26,9 @@ import {PersonalizationStore} from './personalization_store.js';
 PersonalizationStore.getInstance().init(emptyState());
 
 window.addEventListener('message', onMessageReceived);
+
+function reload(event) {
+  window.location.reload();
+}
+// Reload when online, in case any images are not loaded.
+window.addEventListener('online', reload);
