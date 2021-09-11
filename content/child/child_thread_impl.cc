3cca1188ad96dc84ac85513eaa3e2d69ef209f28
Sebastien Marchand <sebmarchand@chromium.org>
2021-09-11 15:22:36 +0000

    [PGO] Allow setting the PGO profile file on Android
    This is a follow up on
    https://chromium-review.googlesource.com/c/chromium/src/+/3110569 which
    made Chrome create the proper profiling file, the file is now passed to
    the profiling runtime library.
    
    It's only enabled for PGO builds because it's hard to tell if it could
    break the coverage build.
    
    Please ignore the android-arm(.*)-pgo bot failure, this CL gets us
    closer to getting some green bots but we're not there yet.
    
    Bug: 1227766
    Change-Id: I7003431c666b9b19dd6a4b892d3baf6ab86838c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150734
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920527}

--- content/child/child_thread_impl.cc
+++ content/child/child_thread_impl.cc
@@ -97,6 +97,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(CLANG_PROFILING_INSIDE_SANDBOX)
 #include <stdio.h>
 #include "base/test/clang_profiling.h"
+#include "build/config/compiler/compiler_buildflags.h"
 #if defined(OS_WIN)
 #include <io.h>
 #endif
@@ -378,7 +379,7 @@ class ChildThreadImpl::IOThreadState
 #if BUILDFLAG(CLANG_PROFILING_INSIDE_SANDBOX)
   void SetProfilingFile(base::File file) override {
     // TODO(crbug.com/985574) Remove Android check when possible.
-#if defined(OS_POSIX) && !defined(OS_ANDROID)
+#if defined(OS_POSIX) && (!defined(OS_ANDROID) || defined(CLANG_PGO))
     // Take the file descriptor so that |file| does not close it.
     int fd = file.TakePlatformFile();
     FILE* f = fdopen(fd, "r+b");
