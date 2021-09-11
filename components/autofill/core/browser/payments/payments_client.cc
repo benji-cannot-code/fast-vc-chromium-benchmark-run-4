a576f4169cf85b99d315c9b332e7ff7233b96eb0
Sujie Zhu <sujiezhu@google.com>
2021-09-11 01:56:39 +0000

    [VCN] Enhance UnmaskCardRequest for new VCN request and response
    For unmask request, adding otp and context_token. Also a new request
    format for otp auth.
    
    For unmask response, adding
     - idv_challenge_options
     - flow_status
     - context_token
    
    Also add more test coveraget to cover different scenarios
    
    Bug: 1243475
    Change-Id: Id651836d3a3721fa1ed0c2e72e82acd309367a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154439
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Sujie Zhu <sujiezhu@google.com>
    Cr-Commit-Position: refs/heads/main@{#920463}

--- components/autofill/core/browser/payments/payments_client.cc
+++ components/autofill/core/browser/payments/payments_client.cc
@@ -57,10 +57,13 @@ const char kGetUnmaskDetailsRequestPath[] =
 const char kUnmaskCardRequestPath[] =
     "payments/apis-secure/creditcardservice/getrealpan?s7e_suffix=chromewallet";
 const char kUnmaskCardRequestFormat[] =
+    "requestContentType=application/json; charset=utf-8&request=%s";
+const char kUnmaskCardRequestFormatWithCvc[] =
     "requestContentType=application/json; charset=utf-8&request=%s"
     "&s7e_13_cvc=%s";
-const char kUnmaskCardRequestFormatWithoutCvc[] =
-    "requestContentType=application/json; charset=utf-8&request=%s";
+const char kUnmaskCardRequestFormatWithOtp[] =
+    "requestContentType=application/json; charset=utf-8&request=%s"
+    "&s7e_263_otp=%s";
 
 const char kOptChangeRequestPath[] =
     "payments/apis/chromepaymentsservice/updateautofilluserpreference";
@@ -379,6 +382,11 @@ class UnmaskCardRequest : public PaymentsRequest {
                                base::Value(full_sync_enabled_));
     request_dict.SetKey("chrome_user_context", std::move(chrome_user_context));
 
+    if (!request_details_.context_token.empty()) {
+      request_dict.SetKey("context_token",
+                          base::Value(request_details_.context_token));
+    }
+
     int value = 0;
     if (base::StringToInt(request_details_.user_response.exp_month, &value))
       request_dict.SetKey("expiration_month", base::Value(value));
@@ -389,14 +397,17 @@ class UnmaskCardRequest : public PaymentsRequest {
         "opt_in_fido_auth",
         base::Value(request_details_.user_response.enable_fido_auth));
 
-    // Either FIDO assertion info is set or CVC is set, never both.
     bool is_cvc_auth = !request_details_.user_response.cvc.empty();
+    bool is_otp_auth = !request_details_.otp.empty();
     bool is_fido_auth = request_details_.fido_assertion_info.has_value();
 
-    DCHECK_NE(is_cvc_auth, is_fido_auth);
+    // At most, only one of these auth methods can be provided.
+    DCHECK_LE(is_cvc_auth + is_fido_auth + is_otp_auth, 1);
     if (is_cvc_auth) {
       request_dict.SetKey("encrypted_cvc", base::Value("__param:s7e_13_cvc"));
-    } else {
+    } else if (is_otp_auth) {
+      request_dict.SetKey("otp", base::Value("__param:s7e_263_otp"));
+    } else if (is_fido_auth) {
       request_dict.SetKey(
           "fido_assertion_info",
           std::move(request_details_.fido_assertion_info.value()));
@@ -417,20 +428,29 @@ class UnmaskCardRequest : public PaymentsRequest {
     std::string request_content;
     if (is_cvc_auth) {
       request_content = base::StringPrintf(
-          kUnmaskCardRequestFormat,
+          kUnmaskCardRequestFormatWithCvc,
           net::EscapeUrlEncodedData(json_request, true).c_str(),
           net::EscapeUrlEncodedData(
               base::UTF16ToASCII(request_details_.user_response.cvc), true)
               .c_str());
-    } else {
+    } else if (is_otp_auth) {
       request_content = base::StringPrintf(
-          kUnmaskCardRequestFormatWithoutCvc,
+          kUnmaskCardRequestFormatWithOtp,
+          net::EscapeUrlEncodedData(json_request, true).c_str(),
+          net::EscapeUrlEncodedData(base::UTF16ToASCII(request_details_.otp),
+                                    true)
+              .c_str());
+    } else {
+      // If neither cvc nor otp request, use the normal request format.
+      request_content = base::StringPrintf(
+          kUnmaskCardRequestFormat,
           net::EscapeUrlEncodedData(json_request, true).c_str());
     }
 
     // Payments is reporting receiving blank or non-standard-length CVCs.
     // Log CVC length being sent to gauge how often this is happening.
-    if (request_details_.reason == AutofillClient::UNMASK_FOR_AUTOFILL) {
+    if (request_details_.reason == AutofillClient::UNMASK_FOR_AUTOFILL &&
+        is_cvc_auth) {
       base::UmaHistogramCounts1000("Autofill.CardUnmask.CvcLength.ForAutofill",
                                    request_details_.user_response.cvc.length());
     } else if (request_details_.reason ==
@@ -463,6 +483,7 @@ class UnmaskCardRequest : public PaymentsRequest {
         response_details_.expiration_year = base::NumberToString(year.value());
     }
 
+    // TODO(crbug.com/1248268): Clean up unused fido_creation_options.
     const base::Value* creation_options = response.FindKeyOfType(
         "fido_creation_options", base::Value::Type::DICTIONARY);
     if (creation_options)
@@ -473,9 +494,22 @@ class UnmaskCardRequest : public PaymentsRequest {
     if (request_options)
       response_details_.fido_request_options = request_options->Clone();
 
-    const std::string* token =
+    const base::Value* idv_challenge_options = response.FindKeyOfType(
+        "idv_challenge_options", base::Value::Type::LIST);
+    if (idv_challenge_options)
+      response_details_.idv_challenge_options = idv_challenge_options->Clone();
+
+    const std::string* card_authorization_token =
         response.FindStringKey("card_authorization_token");
-    response_details_.card_authorization_token = token ? *token : std::string();
+    response_details_.card_authorization_token =
+        card_authorization_token ? *card_authorization_token : std::string();
+
+    const std::string* context_token = response.FindStringKey("context_token");
+    response_details_.context_token =
+        context_token ? *context_token : std::string();
+
+    const std::string* flow_status = response.FindStringKey("flow_status");
+    response_details_.flow_status = flow_status ? *flow_status : std::string();
 
     if (request_details_.card.record_type() == CreditCard::VIRTUAL_CARD) {
       response_details_.card_type =
@@ -496,10 +530,15 @@ class UnmaskCardRequest : public PaymentsRequest {
       case AutofillClient::PaymentsRpcCardType::SERVER_CARD:
         return !response_details_.real_pan.empty();
       case AutofillClient::PaymentsRpcCardType::VIRTUAL_CARD:
-        return !response_details_.real_pan.empty() &&
-               !response_details_.expiration_month.empty() &&
-               !response_details_.expiration_year.empty() &&
-               !response_details_.dcvv.empty();
+        // When pan is returned, it has to contain pan + expiry + cvv.
+        // When pan is not returned, it has to contain context token to indicate
+        // success.
+        return (response_details_.real_pan.empty() &&
+                !response_details_.context_token.empty()) ||
+               (!response_details_.real_pan.empty() &&
+                !response_details_.expiration_month.empty() &&
+                !response_details_.expiration_year.empty() &&
+                !response_details_.dcvv.empty());
     }
   }
 
@@ -1080,6 +1119,8 @@ PaymentsClient::UnmaskRequestDetails::UnmaskRequestDetails(
   } else {
     fido_assertion_info.reset();
   }
+  context_token = other.context_token;
+  otp = other.otp;
   last_committed_url_origin = other.last_committed_url_origin;
 }
 PaymentsClient::UnmaskRequestDetails::~UnmaskRequestDetails() = default;
@@ -1103,7 +1144,14 @@ operator=(const PaymentsClient::UnmaskResponseDetails& other) {
   } else {
     fido_request_options.reset();
   }
+  if (other.idv_challenge_options.has_value()) {
+    idv_challenge_options = other.idv_challenge_options->Clone();
+  } else {
+    idv_challenge_options.reset();
+  }
   card_authorization_token = other.card_authorization_token;
+  flow_status = other.flow_status;
+  context_token = other.context_token;
   return *this;
 }
 
