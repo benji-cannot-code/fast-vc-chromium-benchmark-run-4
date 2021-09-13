aa07a13f223cb5bc5c26823ee2b677c1347bd29f
Morten Stenshorne <mstensho@chromium.org>
2021-09-13 08:24:14 +0000

    Do not re-use anonymous multicol child block for continuations.
    We need to keep the multicol anonymous block wrapper child (which was
    introduced in CL:2823033) when inserting blocks into inlines.
    
    LayoutInline::SplitFlow() & co will establish a continuation chain by
    wrapping all inlines inside anonymous blocks (and the blocks in inlines
    will be wrapped inside another sibling anonymous block). The code will
    attempt to re-use any anonymous block that's already there, but this
    won't work if an inline is a direct DOM child of a multicol container.
    We need to leave the dedicated multicol anonymous block wrapper child
    alone, as this one will be responsible for laying out any out-of-flow
    positioned descendants that have inlines as their containing block.
    See LayoutObject::FindNonAnonymousContainingBlock(). Without this fix,
    that method would return the flow thread, which should be invisible to
    LayoutNG (it's not an NG object).
    
    Bug: 1246602
    Change-Id: I69c374576af6f3f8b0b1a1254345df71d3403b12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3153658
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920677}

--- third_party/blink/renderer/core/layout/layout_block_flow.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow.cc
@@ -3165,6 +3165,10 @@ void LayoutBlockFlow::RemoveChild(LayoutObject* old_child) {
   }
 }
 
+bool LayoutBlockFlow::CreatesAnonymousWrapper() const {
+  return IsLayoutFlowThread() && Parent()->IsLayoutNGObject();
+}
+
 void LayoutBlockFlow::MoveAllChildrenIncludingFloatsTo(
     LayoutBlock* to_block,
     bool full_remove_insert) {
