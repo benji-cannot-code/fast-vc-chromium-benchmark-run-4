a074309f9ec47074605c7a1974042465eef419fb
Ali Beyad <abeyad@chromium.org>
2021-09-13 21:54:54 +0000

    Reland "Add support for the User-Agent Reduction Origin Trial to Service Workers"
    This is a reland of e760f6dc7d44ae4efd0b696c2f27f0acf7bc3f32
    
    The original change was reverted because the
    UserAgentServiceWorkerBrowserTest did not have the PlzServiceWorker
    feature enabled, which caused the test to fail on the release build CI
    (where it was off by default).
    
    When the PlzServiceWorker feature is off because
    ServiceWorkerVersion.origin_trial_tokens_ is not populated before
    ServiceWorkerVersion::StartWorkerInternal() is invoked (where the user
    agent string is obtained).
    
    The only change in this CL from the original is that the
    UserAgentServiceWorkerBrowserTest turns on the PlzServiceWorker feature
    in its SetUp function.
    
    The PlzServiceWorker full rollout is on schedule for M94, and the
    UserAgentReduction Origin Trial is scheduled for M95, so the dependency
    on PlzServiceWorker should not be an issue.
    
    Bug: 1222742, 1248048
    Fixed: 1248048
    
    Original change's description:
    > Add support for the User-Agent Reduction Origin Trial to Service Workers
    >
    > This CL ensures that the WorkerGlobalScope and WorkletGlobalScope
    > execution contexts have both the full and reduced user agent strings, so
    > that we can return the correct one for the Navigator JavaScript APIs,
    > depending on a valid UserAgentReduction origin trial token.
    >
    > As part of this change, the GetReducedUserAgent() function was
    > overridden in ShellContentBrowserClient.  This resulted in many web
    > tests failing expectations with a reduced user agent string when the
    > experimental features were turned on, since the UserAgentReduction
    > feature was marked with "experimental" status.  Since we only want this
    > feature activated by a valid origin trial token, the "experimental"
    > status was removed.  'ch-ua-reduced' was also removed from the
    > expected values in the feature-policy-features.html web test since it's
    > no longer an experimental feature.
    >
    > Feature: https://chromestatus.com/feature/5704553745874944
    >
    > Bug: 1222742
    > Change-Id: I4151b65795d922f05a7ad051d3f1a581db30eac7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137615
    > Auto-Submit: Ali Beyad <abeyad@chromium.org>
    > Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: Ben Kelly <wanderview@chromium.org>
    > Commit-Queue: Mike West <mkwst@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#919667}
    
    Bug: 1222742
    Change-Id: I21969968512f781e32cc0dcf7528e5f570d32dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154196
    Commit-Queue: Ali Beyad <abeyad@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920938}

---
+++ content/test/data/service_worker/user_agent.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+self.addEventListener('fetch', (event) => {
+  // Responds to requests containing "user_agent_sw" with the value of
+  // navigator.userAgent.
+  if (event.request.url.includes('user_agent_sw')) {
+    event.respondWith(new Response(navigator.userAgent));
+  }
+});
