b9907cf9a06daea27f2a15ed354d381332477f38
Willie Koomson <wvk@google.com>
2021-09-13 22:22:49 +0000

    Revert "Add autotest extension API to control Chrome tracing"
    This reverts commit 0d5eed67a0e02b8bcb3fe6db7f8912383fdbd720. Tast now
    uses the CDP functions to control tracing, so this is unused.
    
    BUG=b:187792732
    Test=Chrome builds
    
    Change-Id: Iecc7556026bb9675fbb0d6d94e883ba473710393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154974
    Auto-Submit: Willie Koomson <wvk@google.com>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Willie Koomson <wvk@google.com>
    Cr-Commit-Position: refs/heads/main@{#920954}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_apitest.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_apitest.cc
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
-#include "services/tracing/public/cpp/perfetto/perfetto_traced_process.h"
 #include "ui/aura/window.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/test/event_generator.h"
@@ -327,30 +326,6 @@ IN_PROC_BROWSER_TEST_F(AutotestPrivateArcPerformanceTracing, Basic) {
 
   ASSERT_TRUE(RunExtensionTest("autotest_private",
                                {.custom_arg = "arcPerformanceTracing"},
-                               {.load_as_component = true}))
-      << message_;
-}
-
-class AutotestPrivateStartStopTracing : public AutotestPrivateApiTest {
- public:
-  AutotestPrivateStartStopTracing() = default;
-  ~AutotestPrivateStartStopTracing() override = default;
-  AutotestPrivateStartStopTracing(const AutotestPrivateStartStopTracing&) =
-      delete;
-  AutotestPrivateStartStopTracing& operator=(
-      const AutotestPrivateStartStopTracing&) = delete;
-
- protected:
-  // AutotestPrivateApiTest:
-  void SetUpOnMainThread() override {
-    AutotestPrivateApiTest::SetUpOnMainThread();
-    tracing::PerfettoTracedProcess::Get()->ClearDataSourcesForTesting();
-  }
-};
-
-IN_PROC_BROWSER_TEST_F(AutotestPrivateStartStopTracing, StartStopTracing) {
-  ASSERT_TRUE(RunExtensionTest("autotest_private",
-                               {.custom_arg = "startStopTracing"},
                                {.load_as_component = true}))
       << message_;
 }
