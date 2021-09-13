9e8237fb170fca51f2541077edd649ede2569565
Robert Ogden <robertogden@chromium.org>
2021-09-13 19:23:58 +0000

    Missing header when TFLite isn't built in
    Change-Id: I4e89f7b8f2d67343e219a1935dbdcb64682a5df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158501
    Auto-Submit: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920867}

--- components/optimization_guide/content/browser/page_content_annotations_service.cc
+++ components/optimization_guide/content/browser/page_content_annotations_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/optimization_guide/core/optimization_guide_enums.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
