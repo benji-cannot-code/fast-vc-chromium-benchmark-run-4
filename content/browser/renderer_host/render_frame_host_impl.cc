50f120dec75a4be9abc5fda36268ff068ab1d221
Rakina Zata Amni <rakina@chromium.org>
2021-09-13 22:48:00 +0000

    Remove intended_as_new_entry from DidCommitParams
    We've proven that this is always the same as the CommitNavigationParams
    value, so we can just use that value directly when needed.
    
    DwCs link:
    https://crash.corp.google.com/browse?q=expanded_custom_data.ChromeCrashProto.magic_signature_1.name%3D%27%5BDump+without+crash%5D+content%3A%3ARenderFrameHostImpl%3A%3AVerifyThatBrowserAndRendererCalculatedDidCommitParamsMatch%27+AND+%28%28EXISTS+%28SELECT+1+FROM+UNNEST%28productdata%29+WHERE+key%3D%27VerifyDidCommit-intended_browser%27+AND+value%3D%27true%27%29%29+AND+%28EXISTS+%28SELECT+1+FROM+UNNEST%28productdata%29+WHERE+key%3D%27VerifyDidCommit-intended_renderer%27+AND+value%3D%27false%27%29%29+OR+%28EXISTS+%28SELECT+1+FROM+UNNEST%28productdata%29+WHERE+key%3D%27VerifyDidCommit-intended_browser%27+AND+value%3D%27false%27%29%29+AND+%28EXISTS+%28SELECT+1+FROM+UNNEST%28productdata%29+WHERE+key%3D%27VerifyDidCommit-intended_renderer%27+AND+value%3D%27true%27%29%29%29&engine=dremel#samplereports
    
    Bug: 1131832
    Change-Id: Iff9825b22adc02185e794d1d9b69df7f49b34615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3153262
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920966}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -11524,7 +11524,6 @@ void RenderFrameHostImpl::
 #endif
   // Check if these values from DidCommitProvisionalLoadParams sent by the
   // renderer can be calculated entirely in the browser side:
-  // - intended_as_new_entry
   // - method
   // - url_is_unreachable
   // - post_id
@@ -11610,10 +11609,7 @@ void RenderFrameHostImpl::
   const bool browser_history_list_was_cleared =
       request->commit_params().should_clear_history_list;
 
-  if ((!ShouldVerify("intended_as_new_entry") ||
-       request->commit_params().intended_as_new_entry ==
-           params.intended_as_new_entry) &&
-      (!ShouldVerify("method") || browser_method == params.method) &&
+  if ((!ShouldVerify("method") || browser_method == params.method) &&
       (!ShouldVerify("url_is_unreachable") ||
        browser_url_is_unreachable == params.url_is_unreachable) &&
       (!ShouldVerify("post_id") || browser_post_id == params.post_id) &&
@@ -11656,10 +11652,8 @@ void RenderFrameHostImpl::
       "VerifyDidCommit", "history_url_fdu_type",
       GetURLTypeForCrashKey(request->common_params().history_url_for_data_url));
 
-  SCOPED_CRASH_KEY_BOOL("VerifyDidCommit", "intended_browser",
+  SCOPED_CRASH_KEY_BOOL("VerifyDidCommit", "intended_as_new_entry",
                         request->commit_params().intended_as_new_entry);
-  SCOPED_CRASH_KEY_BOOL("VerifyDidCommit", "intended_renderer",
-                        params.intended_as_new_entry);
 
   SCOPED_CRASH_KEY_STRING32("VerifyDidCommit", "method_browser",
                             browser_method);
@@ -11834,8 +11828,6 @@ void RenderFrameHostImpl::
 
   // These DCHECKs ensure that tests will fail if we got here, as
   // DumpWithoutCrashing won't fail tests.
-  DCHECK_EQ(request->commit_params().intended_as_new_entry,
-            params.intended_as_new_entry);
   DCHECK_EQ(browser_method, params.method);
   DCHECK_EQ(browser_url_is_unreachable, params.url_is_unreachable);
   DCHECK_EQ(browser_post_id, params.post_id);
@@ -11853,11 +11845,6 @@ void RenderFrameHostImpl::
 
   // Log histograms to trigger Chrometto slow reports, allowing us to see traces
   // to analyze what happened in these navigations.
-  if (request->commit_params().intended_as_new_entry !=
-      params.intended_as_new_entry) {
-    LogVerifyDidCommitParamsDifference(
-        VerifyDidCommitParamsDifference::kIntendedAsNewEntry);
-  }
   if (browser_method != params.method) {
     LogVerifyDidCommitParamsDifference(
         VerifyDidCommitParamsDifference::kMethod);
