46f59aef41b8a60862c17b3d052357b766b8805d
François Degros <fdegros@chromium.org>
2021-09-13 06:11:41 +0000

    [Files App] Ensure ZipTask can be cancelled at any time
    Also added comprehensive unit tests for ZipTask.
    
    BUG=chromium:912236
    TEST=browser_tests --gtest_filter="FileManagerJsTest.FileOperationManagerTest"
    
    Change-Id: I6cf9b5fb00530bf262a33e05cff6d025c9d46695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3144750
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920658}

--- ui/file_manager/file_manager/background/js/file_operation_util.js
+++ ui/file_manager/file_manager/background/js/file_operation_util.js
@@ -567,7 +567,7 @@ fileOperationUtil.Task = class {
     this.processingEntries = null;
 
     /**
-     * Total number of bytes to be processed. Filled in initialize().
+     * Total number of bytes to be processed. Filled in initialize() or run().
      * Use 1 as an initial value to indicate that the task is not completed.
      * @type {number}
      */
@@ -630,7 +630,9 @@ fileOperationUtil.Task = class {
   /**
    * @param {function()} callback When entries resolved.
    */
-  initialize(callback) {}
+  initialize(callback) {
+    callback();
+  }
 
   /**
    * Requests cancellation of this task.
@@ -1155,16 +1157,6 @@ fileOperationUtil.ZipTask = class extends fileOperationUtil.Task {
     this.zipBaseDirEntry = zipBaseDirEntry;
   }
 
-
-  /**
-   * Initializes the ZipTask.
-   * @param {function()} callback Called when the initialize is completed.
-   */
-  initialize(callback) {
-    this.totalBytes = this.sourceEntries.length;
-    callback();
-  }
-
   /**
    * Runs a zip file creation task.
    *
@@ -1193,6 +1185,10 @@ fileOperationUtil.ZipTask = class extends fileOperationUtil.Task {
         const destPath = await fileOperationUtil.deduplicatePath(
             this.targetDirEntry, destName + '.zip');
 
+        if (this.cancelRequested_) {
+          throw util.createDOMError(util.FileError.ABORT_ERR);
+        }
+
         // Start ZIP operation.
         const {zipId, totalBytes} = await new Promise(
             (resolve, reject) => chrome.fileManagerPrivate.zipSelection(
@@ -1204,8 +1200,15 @@ fileOperationUtil.ZipTask = class extends fileOperationUtil.Task {
         this.totalBytes = totalBytes;
         this.speedometer_.setTotalBytes(this.totalBytes);
 
-        // Set up cancellation callback.
-        this.cancelCallback_ = () => chrome.fileManagerPrivate.cancelZip(zipId);
+        if (this.cancelRequested_) {
+          // Cancellation was requested while fileManagerPrivate.zipSelection()
+          // was running.
+          chrome.fileManagerPrivate.cancelZip(zipId);
+        } else {
+          // Set up cancellation callback.
+          this.cancelCallback_ = () =>
+              chrome.fileManagerPrivate.cancelZip(zipId);
+        }
 
         // Monitor progress.
         while (true) {
