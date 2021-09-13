df6813fa7cb126bfe641cddcd252f9a022db0009
Hiroshige Hayashizaki <hiroshige@chromium.org>
2021-09-13 22:07:33 +0000

    [WPT] Check base URLs in worker-interception-redirect.https.html
    This CL
    
    - Changes the response URL of a worker top-level script to
      `.../subdir/...` when intercepted by a service worker,
      to make worker's base URL directory different depending on
      whether the response URL or the current request URL is used.
    - Makes worker-interception-redirect-webworker.js/py
      to `fetch()`/`importScripts()` relative to its
      settings object's base URL, not
      `/service-workers/service-worker/resources/`, and checks
      the URLs intercepted by serviceworkers.
    
    This checks the settings object's base URL of the workers,
    and thus exposes that Chromium uses current request URLs
    instead of response URLs as base URL, which will be fixed by
    https://chromium-review.googlesource.com/c/chromium/src/+/3145772.
    
    Bug: 861564
    Change-Id: I32dbb4aae55aebd12c75b26bd4a503ce366b57a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3146235
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920944}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/subdir/import-scripts-echo.py
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(req, res):
+    return ([
+        (b'Cache-Control', b'no-cache, must-revalidate'),
+        (b'Pragma', b'no-cache'),
+        (b'Content-Type', b'application/javascript')],
+      b'echo_output = "%s (subdir/)";\n' % req.GET[b'msg'])
