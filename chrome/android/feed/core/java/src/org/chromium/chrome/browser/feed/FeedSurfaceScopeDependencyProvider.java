b3e7ee2bd61790fd80d70b992de0b7696b53cda7
Ian Wells <iwells@chromium.org>
2021-09-13 23:16:36 +0000

    Revert "Feed reliability logging: Split FeedLoggingDependencyProvider into surface and process scopes"
    This reverts commit 2cb74ca77bb31c420b68569fa8eb6e83a789a73b.
    
    Reason for revert: This change breaks the internal build.
    
    Original change's description:
    > Feed reliability logging: Split FeedLoggingDependencyProvider into surface and process scopes
    >
    > In FeedLoggingDependencyProvider, only isActivityLoggingEnabled() depends on a surface. The rest of FeedLoggingDependencyProvider could be process-scope.
    >
    > This CL:
    > * puts surface-related methods (just isActivityLoggingEnabled()) in their own interface (FLDP.SurfaceScope), and has SurfaceScopeDependencyProvider extend the new interface
    > * has ProcessScopeDependencyProvider extend FeedLoggingDependencyProvider
    > * moves some logging-related methods from ProcessSDP to FLDP
    > * copies implementations of FLDP methods from SurfaceSDP to ProcessSDP
    >
    > A google3 CL will make use of the newly process-scope logging dependencies.
    >
    > Finally, there will be another Chromium CL to clean up the logging code that no longer belongs in SurfaceSDP.
    >
    > Bug: 1195940
    > Change-Id: I8d7a6ead465d10da0ee95e8b98d77ca68028a8cb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3130706
    > Commit-Queue: Ian Wells <iwells@chromium.org>
    > Reviewed-by: Cathy Li <chili@chromium.org>
    > Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#920866}
    
    Bug: 1195940
    Change-Id: Ic134c65f31bb951ab45eb2ebb4bd5127ed265875
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3157459
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920977}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceScopeDependencyProvider.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceScopeDependencyProvider.java
@@ -84,7 +84,6 @@ class FeedSurfaceScopeDependencyProvider implements SurfaceScopeDependencyProvid
 
     /**
      * FeedLoggingDependencyProvider implementation.
-     * TODO(iwells): Remove these methods after callers switch to the process scope version.
      */
 
     @Override
