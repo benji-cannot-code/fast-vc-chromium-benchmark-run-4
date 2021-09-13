8edd4ac51b35fdc946a963795c4d976854a94838
James Cook <jamescook@chromium.org>
2021-09-13 22:39:49 +0000

    app_list: Update folder colors for AppListBubble
    Update colors to spec, which is similar to dark-light mode. Set blur
    filter quality for consistency with other UI (and a potential small
    performance benefit).
    
    Screenshots:
    https://screenshot.googleplex.com/9hE3scptKUe6wgh
    https://screenshot.googleplex.com/5qBcRJjs6fRz9Zz
    
    Bug: 1214064
    Change-Id: I30a06c1d29f9e2c155a1d09bcf00ba7b1dbd181c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3155602
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920964}

--- ash/app_list/views/app_list_folder_view.cc
+++ ash/app_list/views/app_list_folder_view.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_features.h"
 #include "ash/public/cpp/metrics_util.h"
 #include "ash/public/cpp/pagination/pagination_model.h"
+#include "ash/public/cpp/style/color_provider.h"
 #include "base/barrier_closure.h"
 #include "base/bind.h"
 #include "base/check.h"
@@ -117,16 +118,12 @@ class BackgroundAnimation : public AppListFolderView::Animation,
     to_rect -= background_view_->bounds().OffsetFromOrigin();
     const SkColor background_color =
         AppListColorProvider::Get()->GetFolderBackgroundColor();
-    const SkColor from_color =
-        show_ ? AppListColorProvider::Get()->GetFolderBubbleColor()
-              : background_color;
-    const SkColor to_color =
-        show_ ? background_color
-              : AppListColorProvider::Get()->GetFolderBubbleColor();
+    const SkColor bubble_color =
+        AppListColorProvider::Get()->GetFolderBubbleColor();
+    const SkColor from_color = show_ ? bubble_color : background_color;
+    const SkColor to_color = show_ ? background_color : bubble_color;
 
     background_view_->layer()->SetColor(from_color);
-    background_view_->layer()->SetBackgroundBlur(
-        AppListColorProvider::Get()->GetFolderBackgrounBlurSigma());
     background_view_->layer()->SetClipRect(from_rect);
     background_view_->layer()->SetRoundedCornerRadius(
         gfx::RoundedCornersF(from_radius));
@@ -529,8 +526,11 @@ AppListFolderView::AppListFolderView(AppListFolderController* folder_controller,
   // such changes.
   background_view_ = AddChildView(std::make_unique<views::View>());
   background_view_->SetPaintToLayer(ui::LAYER_SOLID_COLOR);
-  background_view_->layer()->SetColor(SK_ColorTRANSPARENT);
   background_view_->layer()->SetFillsBoundsOpaquely(false);
+  background_view_->layer()->SetBackgroundBlur(
+      ColorProvider::kBackgroundBlurSigma);
+  background_view_->layer()->SetBackdropFilterQuality(
+      ColorProvider::kBackgroundBlurQuality);
 
   contents_container_ = AddChildView(std::make_unique<views::View>());
   contents_container_->SetPaintToLayer(ui::LAYER_NOT_DRAWN);
