a19176290ea4ab2b61343ba6698e9957965e1a62
Maksim Ivanov <emaxx@chromium.org>
2021-09-13 16:54:54 +0000

    Fix use-after-move crashes in //ash/quick_pair/pairing/
    Fix potential crashes in pairer_broker_impl.cc caused by using a
    scoped_refptr after moving from it. This was found by the clang-tidy
    bugprone-use-after-move diagnostic:
    
      pairer_broker_impl.cc:77: 'device' used after it was moved
      pairer_broker_impl.cc:77: move occurred here
      the use happens in a later loop iteration than the move
    
      pairer_broker_impl.cc:86: 'device' used after it was moved
      pairer_broker_impl.cc:86: move occurred here
      the use happens in a later loop iteration than the move
    
    Bug: 1122844
    Change-Id: I73812c02bf1a352aad7744bbe348a44a41917eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3156665
    Auto-Submit: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#920799}

--- ash/quick_pair/pairing/pairer_broker_impl.cc
+++ ash/quick_pair/pairing/pairer_broker_impl.cc
@@ -75,7 +75,7 @@ void PairerBrokerImpl::OnFastPairDevicePaired(scoped_refptr<Device> device) {
   QP_LOG(INFO) << __func__ << ": Device=" << device;
 
   for (auto& observer : observers_) {
-    observer.OnDevicePaired(std::move(device));
+    observer.OnDevicePaired(device);
   }
 }
 
@@ -84,7 +84,7 @@ void PairerBrokerImpl::OnFastPairPairingFailure(scoped_refptr<Device> device,
   QP_LOG(INFO) << __func__ << ": Device=" << device << ", Failure=" << failure;
 
   for (auto& observer : observers_) {
-    observer.OnPairFailure(std::move(device), failure);
+    observer.OnPairFailure(device, failure);
   }
 }
 
