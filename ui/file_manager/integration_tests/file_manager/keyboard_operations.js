671216ac38133139f6b2eb586c90c8f6562211b1
Luciano Pacheco <lucmult@chromium.org>
2021-09-13 00:34:28 +0000

    Reland "Files SWA: Add appID and change browser test to identify Files SWA"
    This is a reland of 1c22febc3b64c14de23262aa963c9f39d7ccace7
    
    Original change's description:
    > Files SWA: Add appID and change browser test to identify Files SWA
    >
    > Add appID for Files SWA using the chrome extensions API chrome.windows
    > getCurrent.  This ID is guaranteed to be unique within a user session,
    > but isn't unique across restarts.
    >
    > Add GetAllWebContents() for the integration test to be able to identify
    > all Files SWA WebContents. This is based on
    > WebContentsImpl::GetAllWebContents(), using only the public interfaces
    > from //content/.
    >
    > Enable the test `keyboardOpenNewWindow` to test the private API added on
    > CL:3137810.
    >
    > Enable the test `saveFileDialogDownloads` to confirm that we can
    > identify the dialog's WebContent using the GetAllWebContents().
    >
    > Test
    >
    > Bug: b:194150318 b:194969976
    > Change-Id: Icfa214eb81c9a3c9b43ce8a2323baa5284c72a06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137835
    > Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Bo Majewski <majewski@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#919341}
    
    Bug: b:194150318 b:194969976
    Change-Id: I11245db884cf86c13a4dbf66907bb1ff293289b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3153004
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920621}

--- ui/file_manager/integration_tests/file_manager/keyboard_operations.js
+++ ui/file_manager/integration_tests/file_manager/keyboard_operations.js
@@ -483,10 +483,8 @@ testcase.keyboardOpenNewWindow = async () => {
       await setupAndWaitUntilReady(RootPath.DOWNLOADS, [ENTRIES.hello], []);
 
   // Grab the current open windows.
-  const initialWindows =
-      await remoteCall.callRemoteTestUtil('getWindows', null, []);
+  const initialWindows = await remoteCall.getWindows();
   const initialWindowsCount = Object.keys(initialWindows).length;
-  console.log(JSON.stringify(initialWindows));
 
   // Send Ctrl+N to open a new window.
   const key = ['#file-list', 'n', true, false, false];
@@ -496,8 +494,7 @@ testcase.keyboardOpenNewWindow = async () => {
   // Wait for the new window to appear.
   return repeatUntil(async () => {
     const caller = getCaller();
-    const currentWindows =
-        await remoteCall.callRemoteTestUtil('getWindows', null, []);
+    const currentWindows = await remoteCall.getWindows();
     const currentWindowsIds = Object.keys(currentWindows);
     if (initialWindowsCount < currentWindowsIds.length) {
       return true;
