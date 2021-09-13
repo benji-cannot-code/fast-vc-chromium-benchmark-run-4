af80a09023bcfef4d9645b2d751e0851ef02e00a
sisidovski <sisidovski@google.com>
2021-09-13 10:21:58 +0000

    Apply lazy-loading to iframe if the url is in the allowlist
    This CL will expand the scope of lazy-load iframes. By having the
    allowlist of urls which will impact to Core Web Vitals, browser
    selectively applies lazy-loading to iframe regardless of the loading
    attribute setting.
    
    The target urls in the allowlist are selected manually for now, we will
    consider more effective and maintainable approaches like using
    heuristics after the experiment.
    
    This is an extension of previous efforts to lazy-load iframes for
    data-saver users.
    https://blog.chromium.org/2019/10/automatically-lazy-loading-offscreen.html
    
    Design doc: https://docs.google.com/document/d/1xlERn2N3CaPWrDlnTRi_4Cdq_Cx8xGzTdWtWlZxpY7Y/edit?usp=sharing
    
    Change-Id: I299054645f968438d8af67b82b8543ed8b6913c1
    Bug: 1247131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3131382
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@google.com>
    Cr-Commit-Position: refs/heads/main@{#920708}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace features {
 
+// Apply lazy-loading to frames which have embeds likely impacting Core Web
+// Vitals.
+const base::Feature kAutomaticLazyFrameLoadingToEmbeds{
+    "AutomaticLazyFrameLoadingToEmbeds", base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enable intervention for download that was initiated from or occurred in an ad
 // frame without user activation.
 const base::Feature kBlockingDownloadsInAdFrameWithoutUserActivation{
