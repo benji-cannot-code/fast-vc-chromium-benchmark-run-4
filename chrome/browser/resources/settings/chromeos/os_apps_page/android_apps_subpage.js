17c650051ee43057632fceb2bc22f37179915c17
Jimmy Gong <jimmyxgong@google.com>
2021-09-13 20:16:34 +0000

    [CrOS Settings] Check in Polymer3 os_apps_page
    Bug: 1180979
    Change-Id: Id50992116ffa94d4058208dd979b2af47b2cc5ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3153045
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920899}

--- chrome/browser/resources/settings/chromeos/os_apps_page/android_apps_subpage.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/android_apps_subpage.js
@@ -8,14 +8,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * 'android-apps-subpage' is the settings subpage for managing android apps.
  */
 
+import '//resources/cr_elements/cr_button/cr_button.m.js';
+import '//resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import '//resources/cr_elements/cr_link_row/cr_link_row.js';
+import '../../settings_shared_css.js';
+
+import {assert, assertNotReached} from '//resources/js/assert.m.js';
+import {focusWithoutInk} from '//resources/js/cr/ui/focus_without_ink.m.js';
+import {I18nBehavior} from '//resources/js/i18n_behavior.m.js';
+import {afterNextRender, flush, html, Polymer, TemplateInstanceBase, Templatizer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {loadTimeData} from '../../i18n_setup.js';
+import {PrefsBehavior} from '../../prefs/prefs_behavior.js';
+import {Route, RouteObserverBehavior, Router} from '../../router.js';
+import {DeepLinkingBehavior} from '../deep_linking_behavior.m.js';
+import {routes} from '../os_route.m.js';
+
+import {AndroidAppsBrowserProxyImpl, AndroidAppsInfo} from './android_apps_browser_proxy.js';
+
 Polymer({
+  _template: html`{__html_template__}`,
   is: 'settings-android-apps-subpage',
 
   behaviors: [
     DeepLinkingBehavior,
     I18nBehavior,
     PrefsBehavior,
-    settings.RouteObserverBehavior,
+    RouteObserverBehavior,
   ],
 
   properties: {
@@ -58,12 +77,12 @@ Polymer({
   },
 
   /**
-   * @param {!settings.Route} route
-   * @param {!settings.Route} oldRoute
+   * @param {!Route} route
+   * @param {!Route} oldRoute
    */
   currentRouteChanged(route, oldRoute) {
     // Does not apply to this page.
-    if (route !== settings.routes.ANDROID_APPS_DETAILS) {
+    if (route !== routes.ANDROID_APPS_DETAILS) {
       return;
     }
 
@@ -73,9 +92,9 @@ Polymer({
   /** @private */
   onPlayStoreEnabledChanged_(enabled) {
     if (!enabled &&
-        settings.Router.getInstance().getCurrentRoute() ===
-            settings.routes.ANDROID_APPS_DETAILS) {
-      settings.Router.getInstance().navigateToPreviousRoute();
+        Router.getInstance().getCurrentRoute() ===
+            routes.ANDROID_APPS_DETAILS) {
+      Router.getInstance().navigateToPreviousRoute();
     }
   },
 
@@ -126,7 +145,7 @@ Polymer({
 
   /** @private */
   onConfirmDisableDialogClose_() {
-    cr.ui.focusWithoutInk(assert(this.$$('#remove')));
+    focusWithoutInk(assert(this.$$('#remove')));
   },
 
   /**
@@ -136,7 +155,7 @@ Polymer({
   onManageAndroidAppsTap_(event) {
     // |event.detail| is the click count. Keyboard events will have 0 clicks.
     const isKeyboardAction = event.detail === 0;
-    settings.AndroidAppsBrowserProxyImpl.getInstance().showAndroidAppsSettings(
+    AndroidAppsBrowserProxyImpl.getInstance().showAndroidAppsSettings(
         isKeyboardAction);
   },
 });
