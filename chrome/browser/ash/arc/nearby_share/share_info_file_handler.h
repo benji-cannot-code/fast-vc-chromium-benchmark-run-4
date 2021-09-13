eb79a745f5f3b8b5076394639f0bd2e9eeb1aa7e
Alan Ding <alanding@google.com>
2021-09-13 20:45:24 +0000

    Reland "[ARC Nearby Share] Reduce lifetime of session and decouple files cleanup"
    This is a reland of f443fa867b9461b0964c55cb12025205f1e47499 after flaky
    unit tests fix. Issue was the updated ARC cleanup callback runs in a
    base::Threadpool which requires a full fledged TaskEnvironment[1]
    rather than base::RunLoop().
    
    [1]https://chromium.googlesource.com/chromium/src/+/HEAD/docs/threading_and_tasks_testing.md#full-fledged-base_test_taskenvironment
    
    Original change's description:
    > [ARC Nearby Share] Reduce lifetime of session and decouple files cleanup
    >
    > With https://crrev.com/c/3120239 (Close reason fix), we've extended the
    > lifetime of the ARC Nearby Share session object. This causes UI race
    > conditions if session could later be cleaned up from OnNearbyShareClosed
    > and from CleanupSession callback passed to NearbyShare. Currently, if
    > the latter is called first then the Android activity is not closed.
    >
    > Other changes:
    > - Always cleanup the session object immediately after bubble is closed.
    > - Added ARC cleanup support if the utility process stops or crashes.
    > - Consolidate all cleanup into one function: CleanupSession.
    > - This CL minimizes the session object lifetime by decoupling cache
    > files cleanup so that they can be deleted any time after cleaning up the
    > session object. This way the cache files lifetime is not dependent on
    > the session staying alive.
    >
    > BUG=b:198845773, b:184720732
    > TEST= Verified with Photos app.
    >
    > Change-Id: I482dd7da9eca927cb9a692af1d4d2370f82103f7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151422
    > Commit-Queue: Alan Ding <alanding@chromium.org>
    > Reviewed-by: Josh Nohle <nohle@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#920564}
    
    Bug: b:198845773, b:184720732
    Test: ./testing/xvfb.py ./out/Default/unit_tests --gtest_filter=*NearbySharingServiceImplTest.SendPayloadWithArcCallback* --gtest_repeat=20
    Change-Id: Id56e5dbc289043dc7e80172099b3b5789cb37f90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3157756
    Commit-Queue: Alan Ding <alanding@chromium.org>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920912}

--- chrome/browser/ash/arc/nearby_share/share_info_file_handler.h
+++ chrome/browser/ash/arc/nearby_share/share_info_file_handler.h
@@ -61,7 +61,9 @@ class ShareInfoFileHandler
 
   // |profile| is the current user profile.
   // |share_info| represents the data being shared.
-  // |directory| is the top level share directory.
+  // |directory| is the top level share directory. The owner of this class
+  // object will be required to clean up |directory| including any
+  // subdirectories and files within it.
   // |task_runner| is used for any cleanup which requires disk IO.
   ShareInfoFileHandler(Profile* profile,
                        mojom::ShareIntentInfo* share_info,
@@ -101,14 +103,11 @@ class ShareInfoFileHandler
 
   ~ShareInfoFileHandler();
 
-  // Create local share path in scoped directory for cache files.
-  bool CreateShareDirectory(std::list<base::ScopedTempDir>::iterator it);
+  // Create local unique share directory for cache files.
+  base::FilePath CreateShareDirectory();
 
-  // Called when temp directory for Nearby Share cached files is created and
-  // started streaming files.
-  void OnCreatedDirectoryAndStartStreaming(
-      std::list<base::ScopedTempDir>::iterator it_dir,
-      bool result);
+  // Called when share directory path is created and can start streaming files.
+  void OnShareDirectoryPathCreated(base::FilePath share_dir);
 
   // Create file with create and write flags and return scoped fd.
   base::ScopedFD CreateFileForWrite(const base::FilePath& file_path);
@@ -162,7 +161,6 @@ class ShareInfoFileHandler
 
   std::list<scoped_refptr<ShareInfoFileStreamAdapter>> file_stream_adapters_;
   std::list<scoped_refptr<storage::FileSystemContext>> contexts_;
-  std::list<base::ScopedTempDir> scoped_temp_dirs_;
   FileShareConfig file_config_;
   StartedCallback started_callback_;
   CompletedCallback completed_callback_;
