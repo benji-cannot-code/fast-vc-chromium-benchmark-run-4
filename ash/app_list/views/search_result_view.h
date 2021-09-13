9a6e3a00974a910c8ed5aa2b146b5465fda3e89e
Yulun Wu <yulunwu@chromium.org>
2021-09-13 19:48:54 +0000

    Replace Render Text with Style Label in SearchResultView.
    Render Text is being replaced with StyledLabel in SearchResultView
    because we should be using ash_typography for font styling instead of
    directly manipulating render text..
    
    Additionally, layout/spacing logic has been moved from
    PaintButtonContents to Layout().
    
    Follow up CLs will add SearchResultViewType specific fonts as well as
    a layout manager for the search result view.
    
    Bug: 792785
    Change-Id: Iad3c8fe2010f5428f8099468a68e6efb667e87e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3120332
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920883}

--- ash/app_list/views/search_result_view.h
+++ ash/app_list/views/search_result_view.h
@@ -21,12 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "ui/views/context_menu_controller.h"
 
-namespace gfx {
-class RenderText;
-}
-
 namespace views {
 class ImageView;
+class StyledLabel;
+class Label;
 }  // namespace views
 
 namespace ash {
@@ -80,9 +78,8 @@ class ASH_EXPORT SearchResultView : public SearchResultBaseView,
   void UpdateTitleText();
   void UpdateDetailsText();
 
-  // Creates title/details render text.
-  void CreateTitleRenderText();
-  void CreateDetailsRenderText();
+  void StyleTitleLabel();
+  void StyleDetailsLabel();
 
   // Callback for query suggstion removal confirmation.
   void OnQueryRemovalAccepted(bool accepted);
@@ -97,6 +94,7 @@ class ASH_EXPORT SearchResultView : public SearchResultBaseView,
   void OnMouseExited(const ui::MouseEvent& event) override;
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
   void VisibilityChanged(View* starting_from, bool is_visible) override;
+  void OnThemeChanged() override;
 
   // ui::EventHandler overrides:
   void OnGestureEvent(ui::GestureEvent* event) override;
@@ -136,10 +134,11 @@ class ASH_EXPORT SearchResultView : public SearchResultBaseView,
 
   AppListViewDelegate* view_delegate_;
 
-  MaskedImageView* icon_;         // Owned by views hierarchy.
-  views::ImageView* badge_icon_;  // Owned by views hierarchy.
-  std::unique_ptr<gfx::RenderText> title_text_;
-  std::unique_ptr<gfx::RenderText> details_text_;
+  MaskedImageView* icon_ = nullptr;              // Owned by views hierarchy.
+  views::ImageView* badge_icon_ = nullptr;       // Owned by views hierarchy.
+  views::StyledLabel* title_label_ = nullptr;    // Owned by view hierarchy.
+  views::StyledLabel* details_label_ = nullptr;  // Owned by view hierarchy.
+  views::Label* separator_label_ = nullptr;      // Owned by view hierarchy.
 
   std::unique_ptr<AppListMenuModelAdapter> context_menu_;
 
