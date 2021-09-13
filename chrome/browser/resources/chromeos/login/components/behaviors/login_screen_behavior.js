65c3f9e48fd0a02be824bf5b1987f80f8a45f185
Renato Silva <rrsilva@google.com>
2021-09-13 15:46:58 +0000

    OOBE - Polymer3 - Marketing opt in
    Move the marketing screen to its appropriate folder.
    Replace the Polymer() factory method with the proper
    ES6 class syntax. Add modularisation directives and
    closure compilation for the modularised version.
    Add an interface to LoginScreenBehavior for the closure
    compilation.
    
    Change-Id: I6a70bc362c3b0bc8c17872eb5c6cdf849da822b9
    Bug: 1184731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3141014
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920770}

--- chrome/browser/resources/chromeos/login/components/behaviors/login_screen_behavior.js
+++ chrome/browser/resources/chromeos/login/components/behaviors/login_screen_behavior.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// #import {OOBE_UI_STATE} from '../display_manager_types.m.js';
+// #import {OOBE_UI_STATE, DisplayManagerScreenAttributes} from '../display_manager_types.m.js';
 // #import {OobeTypes} from '../oobe_types.m.js';
 // #import {Oobe} from '../../cr_ui.m.js';
 
@@ -151,3 +151,12 @@ const CALLBACK_USER_ACTED = 'userActed';
  * }}
  */
 LoginScreenBehavior.Proto;
+
+/** @interface */
+/* #export */ class LoginScreenBehaviorInterface {
+  /**
+   * @param {string} screenName
+   * @param {DisplayManagerScreenAttributes} attributes
+   */
+  initializeLoginScreen(screenName, attributes) {}
+}
