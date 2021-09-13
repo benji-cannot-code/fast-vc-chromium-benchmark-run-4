6aca48559c17155ae95fa06efe65e943c1fb298f
Allen Bauer <kylixrd@chromium.org>
2021-09-13 23:01:16 +0000

    Convert TouchCalibratorView to use AnimationBuilder for animations.
    Using the new AnimationBuilder eliminates the need to manage the observer while also making the animation construction simpler.
    
    Bug: 1236069
    Change-Id: Iae3634570ba7e6bf3b8264ce39430a1fca70bb19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158791
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#920973}

--- ash/display/touch_calibrator_view.h
+++ ash/display/touch_calibrator_view.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "base/macros.h"
 #include "cc/paint/paint_flags.h"
-#include "ui/compositor/layer_animation_observer.h"
 #include "ui/display/display.h"
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/views/animation/animation_delegate_views.h"
@@ -26,10 +25,6 @@ namespace gfx {
 class Animation;
 }  // namespace gfx
 
-namespace ui {
-class LayerAnimationSequence;
-}  // namespace ui
-
 namespace ash {
 
 class CircularThrobberView;
@@ -42,8 +37,7 @@ class HintBox;
 // |TouchCalibratorView| acts as a state machine and has an API to toggle its
 // state or get the current state.
 class ASH_EXPORT TouchCalibratorView : public views::View,
-                                       public views::AnimationDelegateViews,
-                                       public ui::LayerAnimationObserver {
+                                       public views::AnimationDelegateViews {
  public:
   // Different states of |TouchCalibratorView| in order.
   enum State {
@@ -86,12 +80,6 @@ class ASH_EXPORT TouchCalibratorView : public views::View,
   void AnimationProgressed(const gfx::Animation* animation) override;
   void AnimationCanceled(const gfx::Animation* animation) override;
 
-  // ui::LayerAnimationObserver
-  void OnLayerAnimationStarted(ui::LayerAnimationSequence* sequence) override;
-  void OnLayerAnimationEnded(ui::LayerAnimationSequence* sequence) override;
-  void OnLayerAnimationAborted(ui::LayerAnimationSequence* sequence) override;
-  void OnLayerAnimationScheduled(ui::LayerAnimationSequence* sequence) override;
-
   // Moves the touch calibrator view to its next state.
   void AdvanceToNextState();
 
@@ -115,6 +103,15 @@ class ASH_EXPORT TouchCalibratorView : public views::View,
                       bool is_primary_view);
 
   void InitViewContents();
+
+  void OnStateAnimationEnded();
+
+  // Animate the given |view|'s layer to |end_position| and an optional
+  // |opacity|.
+  void AnimateLayerToPosition(views::View* view,
+                              base::TimeDelta duration,
+                              gfx::Point end_position,
+                              float opacity = 1.f);
 
   // The target display on which this view is rendered on.
   const display::Display display_;
@@ -157,8 +154,6 @@ class ASH_EXPORT TouchCalibratorView : public views::View,
   views::View* touch_point_view_ = nullptr;
 
   State state_ = UNKNOWN;
-
-  DISALLOW_COPY_AND_ASSIGN(TouchCalibratorView);
 };
 
 }  // namespace ash
