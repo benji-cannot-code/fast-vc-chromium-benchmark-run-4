a03f5ff93b2ba979da7926c8c81f7798cf1cb9ab
Cheick Cisse <cheickcisse@google.com>
2021-09-14 14:05:15 +0000

    [SH] Show IPH on PageLoadFinished
    This CL uses LinkToTextIPH to show the message when a user loads a link-to-text
    
    Bug: 1244605
    Change-Id: I0cb5fe4d61eeecb384f2dafbc2b0899ba68afbf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150476
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#921196}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -75,6 +75,7 @@ import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.read_later.ReadLaterIPHController;
 import org.chromium.chrome.browser.share.ShareDelegate;
+import org.chromium.chrome.browser.share.link_to_text.LinkToTextIPHController;
 import org.chromium.chrome.browser.share.scroll_capture.ScrollCaptureManager;
 import org.chromium.chrome.browser.signin.SyncConsentActivityLauncherImpl;
 import org.chromium.chrome.browser.signin.ui.SigninPromoUtil;
@@ -141,6 +142,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
     private UrlFocusChangeListener mUrlFocusChangeListener;
     private @Nullable ToolbarButtonInProductHelpController mToolbarButtonInProductHelpController;
     private AddToHomescreenIPHController mAddToHomescreenIPHController;
+    private LinkToTextIPHController mLinkToTextIPHController;
     private AddToHomescreenMostVisitedTileClickObserver mAddToHomescreenMostVisitedTileObserver;
     private AppBannerInProductHelpController mAppBannerInProductHelpController;
     private PwaBottomSheetController mPwaBottomSheetController;
@@ -623,6 +625,8 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                 R.id.add_to_homescreen_id, () -> {
                     return mToolbarManager.getMenuButtonView();
                 }, MessageDispatcherProvider.from(mWindowAndroid));
+        mLinkToTextIPHController =
+                new LinkToTextIPHController(mActivityTabProvider, mTabModelSelectorSupplier.get());
         mAddToHomescreenMostVisitedTileObserver = new AddToHomescreenMostVisitedTileClickObserver(
                 mActivityTabProvider, mAddToHomescreenIPHController);
         mAppBannerInProductHelpController =
