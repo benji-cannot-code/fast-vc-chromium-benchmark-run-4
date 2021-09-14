28b7b2844355a5380d729f9680ec61fd6471603d
Leonard Grey <lgrey@chromium.org>
2021-09-14 16:09:47 +0000

    Commander: add placeholder and no-results view
    Bug: 1014639
    Change-Id: Ifc52b20d174deed78a1dc997c7f9960401f03c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158404
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921246}

--- chrome/browser/ui/webui/commander/commander_ui.cc
+++ chrome/browser/ui/webui/commander/commander_ui.cc
@@ -24,6 +24,10 @@ CommanderUI::CommanderUI(content::WebUI* web_ui)
 
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(chrome::kChromeUICommanderHost);
+  // TODO(lgrey): Localize when these are no longer temp.
+  source->AddString("placeholder", "Type to search Chrome Commands…");
+  source->AddString("noResults", "No Chrome Commands found.");
+  source->AddString("pageTitle", "Commander");
   webui::SetupWebUIDataSource(
       source, base::make_span(kCommanderResources, kCommanderResourcesSize),
       IDR_COMMANDER_COMMANDER_HTML);
