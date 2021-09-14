1e49bba1358896b0a1ccff2f79b7af58bbadb83e
Jeremy Selier <jerem@google.com>
2021-09-14 17:57:46 +0000

    [ntp][modules][photos] Add support for info button on module
    Show info button on module. Show dialog about Google Photos Memories when clicked. screenshot/7xFzTvs9BFqK79C
    
    Bug: 1230867
    Change-Id: Ibe1c77af4891f7030f44da81e3a491546e2999f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159265
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Jeremy Selier <jerem@google.com>
    Cr-Commit-Position: refs/heads/main@{#921307}

--- chrome/browser/resources/new_tab_page/modules/photos/module.js
+++ chrome/browser/resources/new_tab_page/modules/photos/module.js
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../module_header.js';
 import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
+import 'chrome://resources/cr_elements/cr_lazy_render/cr_lazy_render.m.js';
 
 import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {I18nBehavior, loadTimeData} from '../../i18n_setup.js';
+import {InfoDialogElement} from '../info_dialog.js';
 import {ModuleDescriptor} from '../module_descriptor.js';
 import {PhotosProxy} from './photos_module_proxy.js';
 
@@ -32,6 +34,12 @@ class PhotosModuleElement extends mixinBehaviors
       /** @type {Array<!photos.mojom.Memory>} */
       memories: Array,
     };
+  }
+
+  /** @private */
+  onInfoButtonClick_() {
+    /** @type {InfoDialogElement} */ (this.$.infoDialogRender.get())
+        .showModal();
   }
 }
 
