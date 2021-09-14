fbd0e47970f6fc17bdd569c1645c8225d1ccba15
Jan Kosiaty <jkosiaty@opera.com>
2021-09-14 17:36:08 +0000

    Remove unused observer
    Dead code since
    https://chromium-review.googlesource.com/c/chromium/src/+/1469650
    
    R=bsep@chromium.org, pkasting@chromium.org, sky@chromium.org
    
    Change-Id: Ibba408217fb61cc5e5e0fe5ae596fdab25189574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3150016
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921295}

--- chrome/browser/ui/views/frame/glass_browser_caption_button_container.cc
+++ chrome/browser/ui/views/frame/glass_browser_caption_button_container.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/glass_browser_frame_view.h"
 #include "chrome/browser/ui/views/frame/windows_10_caption_button.h"
 #include "chrome/browser/ui/views/frame/windows_10_tab_search_caption_button.h"
