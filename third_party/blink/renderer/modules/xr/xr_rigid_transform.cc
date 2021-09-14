a2b90cfcdc2c49d2f5a9ae73823abeec2c80d8b0
Alexander Cooper <alcooper@chromium.org>
2021-09-14 16:07:50 +0000

    Validate XRRigidTransform Constructor Arguments
    Per the spec, XRRigidTransform's created with DOMPoint's which contain
    non-finite (e.g. NaN/Infinity) should throw a type error and fail to
    be created. While not doing so seems to work fine on production builds
    (seeming to default to using identity for the component which had the
    invalid portion), on DCHECK builds, these poorly constructed objects
    start to hit DCHECKs when methods on the object are attempted to be
    used.
    
    This fixes the XRRigidTransform::Create method to appropriately throw
    per the spec and thus should avoid the later DCHECKs. This change also
    adds a WPT to validate this specced behavior.
    
    Fixed: 1246156
    Change-Id: I5d13784868056896f15b8c803ce4b6abb69f03fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159085
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921244}

--- third_party/blink/renderer/modules/xr/xr_rigid_transform.cc
+++ third_party/blink/renderer/modules/xr/xr_rigid_transform.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/xr/xr_rigid_transform.h"
 
+#include <cmath>
 #include <utility>
 
 #include "third_party/blink/renderer/bindings/core/v8/v8_dom_point_init.h"
@@ -14,6 +15,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/transforms/transformation_matrix.h"
 
 namespace blink {
+
+namespace {
+
+bool IsComponentValid(DOMPointInit* point) {
+  DCHECK(point);
+  return std::isfinite(point->x()) && std::isfinite(point->y()) &&
+         std::isfinite(point->z()) && std::isfinite(point->w());
+}
+}  // anonymous namespace
 
 // makes a deep copy of transformationMatrix
 XRRigidTransform::XRRigidTransform(
@@ -62,6 +72,13 @@ XRRigidTransform* XRRigidTransform::Create(DOMPointInit* position,
                                            ExceptionState& exception_state) {
   if (position && position->w() != 1.0) {
     exception_state.ThrowTypeError("W component of position must be 1.0");
+    return nullptr;
+  }
+
+  if ((position && !IsComponentValid(position)) ||
+      (orientation && !IsComponentValid(orientation))) {
+    exception_state.ThrowTypeError(
+        "Position and Orientation must consist of only finite values");
     return nullptr;
   }
 
