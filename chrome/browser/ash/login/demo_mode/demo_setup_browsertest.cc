8bde1e14218389905b8869dfe98febf904c32b2b
Maggie Cai <mxcai@google.com>
2021-09-14 07:54:37 +0000

    [Sheriff] Disable tests in demo_setup_browsertests.cc
    This CL disables tests:
    DemoSetupArcSupportedTest.OnlineSetupFlowErrorDefault
    DemoSetupArcSupportedTest.OnlineSetupFlowSuccessWithCountryCustomization
    DemoSetupFRETest.NonEnterpriseDevice
    DemoSetupFRETest.DeviceWithFRE
    in ASAN because they cause adress santitizer failure in Linux Chromium
    OS ASan LSan Tests (1). See linked bug for more details,
    
    BUG=1249355
    
    Change-Id: I4e18c5aa3a03ead71b0cb55bc8eb487c4903b61d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159940
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Owners-Override: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921091}

--- chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
@@ -474,8 +474,16 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
 }
 
+// Disabled due to test failure. http://crbug.com/1249355
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_OnlineSetupFlowSuccessWithCountryCustomization \
+  DISABLED_OnlineSetupFlowSuccessWithCountryCustomization
+#else
+#define MAYBE_OnlineSetupFlowSuccessWithCountryCustomization \
+  OnlineSetupFlowSuccessWithCountryCustomization
+#endif
 IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
-                       OnlineSetupFlowSuccessWithCountryCustomization) {
+                       MAYBE_OnlineSetupFlowSuccessWithCountryCustomization) {
   // Simulate successful online setup.
   enrollment_helper_.ExpectEnrollmentMode(
       policy::EnrollmentConfig::MODE_ATTESTATION);
@@ -530,7 +538,14 @@ IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
 }
 
-IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest, OnlineSetupFlowErrorDefault) {
+// Disabled due to test failure. http://crbug.com/1249355
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_OnlineSetupFlowErrorDefault DISABLED_OnlineSetupFlowErrorDefault
+#else
+#define MAYBE_OnlineSetupFlowErrorDefault OnlineSetupFlowErrorDefault
+#endif
+IN_PROC_BROWSER_TEST_F(DemoSetupArcSupportedTest,
+                       MAYBE_OnlineSetupFlowErrorDefault) {
   // Simulate online setup failure.
   enrollment_helper_.ExpectEnrollmentMode(
       policy::EnrollmentConfig::MODE_ATTESTATION);
@@ -1088,7 +1103,13 @@ IN_PROC_BROWSER_TEST_F(DemoSetupFRETest, DeviceFromFactory) {
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
 }
 
-IN_PROC_BROWSER_TEST_F(DemoSetupFRETest, NonEnterpriseDevice) {
+// Disabled due to test failure. http://crbug.com/1249355
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_NonEnterpriseDevice DISABLED_NonEnterpriseDevice
+#else
+#define MAYBE_NonEnterpriseDevice NonEnterpriseDevice
+#endif
+IN_PROC_BROWSER_TEST_F(DemoSetupFRETest, MAYBE_NonEnterpriseDevice) {
   // Simulating device that was never set for enterprise:
   // * "active_date" is set
   // * "check_enrollment" and "block_devmode" flags are set to false.
@@ -1151,7 +1172,13 @@ IN_PROC_BROWSER_TEST_F(DemoSetupFRETest, MAYBE_LegacyDemoModeDevice) {
   EXPECT_TRUE(StartupUtils::IsDeviceRegistered());
 }
 
-IN_PROC_BROWSER_TEST_F(DemoSetupFRETest, DeviceWithFRE) {
+// Disabled due to test failure. http://crbug.com/1249355
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_DeviceWithFRE DISABLED_DeviceWithFRE
+#else
+#define MAYBE_DeviceWithFRE DeviceWithFRE
+#endif
+IN_PROC_BROWSER_TEST_F(DemoSetupFRETest, MAYBE_DeviceWithFRE) {
   // Simulating device that requires FRE. "check_enrollment", "block_devmode"
   // and "ActivateDate" flags are set.
   statistics_provider_.SetMachineStatistic(system::kActivateDateKey, "2018-01");
