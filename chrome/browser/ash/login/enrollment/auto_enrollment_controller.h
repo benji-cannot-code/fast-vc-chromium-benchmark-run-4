f57bf075a102f17820e60f621086c4a9aba0be6e
Pavol Marko <pmarko@google.com>
2021-09-14 10:15:00 +0000

    Extract system clock sync waiting logic into SystemClockSyncObservation
    This way the code is reusable across multiple sites.
    
    Bug: none
    Change-Id: I91a841655f82a5f38c9b89b2867e480c48288b57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3080885
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921121}

--- chrome/browser/ash/login/enrollment/auto_enrollment_controller.h
+++ chrome/browser/ash/login/enrollment/auto_enrollment_controller.h
@@ -26,6 +26,8 @@ class CommandLine;
 
 namespace ash {
 
+class SystemClockSyncObservation;
+
 // Drives the forced re-enrollment check (for historical reasons called
 // auto-enrollment check), running an AutoEnrollmentClient if appropriate to
 // make a decision.
@@ -146,8 +148,6 @@ class AutoEnrollmentController {
       policy::AutoEnrollmentClient::Factory* auto_enrollment_client_factory);
 
  private:
-  class SystemClockSyncWaiter;
-
   // Determines the FRE and Initial Enrollment requirement and starts initial
   // enrollment if necessary. If Initial Enrollment would be skipped and the
   // system clock has not been synchronized yet, triggers waiting for system
@@ -185,7 +185,7 @@ class AutoEnrollmentController {
 
   // Called when the system clock has been synchronized or a timeout has been
   // reached while waiting for the system clock sync.
-  void OnSystemClockSyncResult(SystemClockSyncState system_clock_sync_state);
+  void OnSystemClockSyncResult(bool system_clock_synchronized);
 
   // Starts the auto-enrollment client for initial enrollment.
   void StartClientForInitialEnrollment();
@@ -275,7 +275,7 @@ class AutoEnrollmentController {
       AutoEnrollmentCheckType::kNone;
 
   // Utility for waiting until the system clock has been synchronized.
-  std::unique_ptr<SystemClockSyncWaiter> system_clock_sync_waiter_;
+  std::unique_ptr<SystemClockSyncObservation> system_clock_sync_observation_;
 
   // Current system clock sync state. This is only modified in
   // `OnSystemClockSyncResult` after `system_clock_sync_wait_requested_` has
