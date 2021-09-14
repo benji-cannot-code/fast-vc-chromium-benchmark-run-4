fd54892040aa5f9eefb133911eb77bfc0ae43e22
Felipe Andrade <fsandrade@chromium.org>
2021-09-14 14:56:46 +0000

    Refactor enrollment ID upload manager
    Rename EnrollmentPolicyObserver to EnrollmentIdUploadManager. The old
    name was too generic, and didn't represent the actual task performed by
    this class. Additionally, rename some methods that are only used for
    testing, and remove usage of the deprecated DISALLOW_* macro.
    
    Bug: 1248117
    Change-Id: I69cfd6b99255b4e6c7260bc7a80c221eec0de94d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151869
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921212}

--- chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.cc
+++ chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/ash/attestation/attestation_policy_observer.h"
 #include "chrome/browser/ash/attestation/enrollment_certificate_uploader_impl.h"
-#include "chrome/browser/ash/attestation/enrollment_policy_observer.h"
+#include "chrome/browser/ash/attestation/enrollment_id_upload_manager.h"
 #include "chrome/browser/ash/attestation/machine_certificate_uploader_impl.h"
 #include "chrome/browser/ash/login/enrollment/auto_enrollment_controller.h"
 #include "chrome/browser/ash/login/reporting/login_logout_reporter.h"
@@ -206,8 +206,8 @@ void DeviceCloudPolicyManagerAsh::StartConnection(
   enrollment_certificate_uploader_ =
       std::make_unique<ash::attestation::EnrollmentCertificateUploaderImpl>(
           client());
-  enrollment_policy_observer_ =
-      std::make_unique<ash::attestation::EnrollmentPolicyObserver>(
+  enrollment_id_upload_manager_ =
+      std::make_unique<ash::attestation::EnrollmentIdUploadManager>(
           client(), enrollment_certificate_uploader_.get());
   lookup_key_uploader_ = std::make_unique<LookupKeyUploader>(
       device_store(), g_browser_process->local_state(),
