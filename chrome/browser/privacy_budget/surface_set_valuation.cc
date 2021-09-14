5e746d701cf5fa390d53804a27ddfb7f4f716c5e
Asanka Herath <asanka@chromium.org>
2021-09-14 18:46:53 +0000

    [privacy_budget] Refactor large initializers into separate functions.
    T'was a bit hard on the eyes.
    
    Bug: 1224752
    Change-Id: Iac73713ade41eafae5479cdd66ffc53e03f4b4a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3156352
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921338}

--- chrome/browser/privacy_budget/surface_set_valuation.cc
+++ chrome/browser/privacy_budget/surface_set_valuation.cc
@@ -24,15 +24,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/privacy_budget/types.h"
 #include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
 
+namespace {
+
+IdentifiableSurfaceCostMap GetPerSurfaceCostsFromFieldTrial() {
+  return DecodeIdentifiabilityFieldTrialParam<IdentifiableSurfaceCostMap>(
+      features::kIdentifiabilityStudyPerHashCost.Get());
+}
+
+IdentifiableSurfaceTypeCostMap GetPerTypeCostsFromFieldTrial() {
+  return DecodeIdentifiabilityFieldTrialParam<IdentifiableSurfaceTypeCostMap>(
+      features::kIdentifiabilityStudyPerTypeCost.Get());
+}
+
+}  // namespace
+
 SurfaceSetValuation::SurfaceSetValuation(
     const SurfaceSetEquivalence& surface_set_equivalence)
     : equivalence_sets_(surface_set_equivalence),
-      per_surface_costs_(
-          DecodeIdentifiabilityFieldTrialParam<IdentifiableSurfaceCostMap>(
-              features::kIdentifiabilityStudyPerHashCost.Get())),
-      per_type_costs_(
-          DecodeIdentifiabilityFieldTrialParam<IdentifiableSurfaceTypeCostMap>(
-              features::kIdentifiabilityStudyPerTypeCost.Get())) {}
+      per_surface_costs_(GetPerSurfaceCostsFromFieldTrial()),
+      per_type_costs_(GetPerTypeCostsFromFieldTrial()) {}
 
 SurfaceSetValuation::~SurfaceSetValuation() = default;
 
