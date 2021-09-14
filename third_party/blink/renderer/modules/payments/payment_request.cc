1aa0a7f377b4ea38abce2372a31d21dbf212f5cb
Rouslan Solomakhin <rouslan@chromium.org>
2021-09-14 21:13:35 +0000

    [Web Payment] Add UserCounters for completing PaymentRequest.
    Before this patch, it was possible to monitor PaymentRequest completion
    rates only in the browser process, which may miss some issues in code
    that cause an abort before the measurement code is reached.
    
    This patch adds UseCounters for PaymentReqhest.show() resolving and
    PaymentRequest.complete() being called in Blink.
    
    After this patch, even if the completion rate measurement code in the
    browser process is not reached due to some issue, the Blink UseCounters
    can be used as a backup.
    
    Bug: 1203257
    Change-Id: I21c735277c54ddbd92ceab3c4325de4c6e865439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3160859
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921407}

--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -1105,6 +1105,8 @@ ScriptPromise PaymentRequest::Complete(ScriptState* script_state,
     return ScriptPromise();
   }
 
+  UseCounter::Count(GetExecutionContext(), WebFeature::kPaymentRequestComplete);
+
   complete_timer_.Stop();
 
   // The payment provider should respond in PaymentRequest::OnComplete().
@@ -1501,6 +1503,8 @@ void PaymentRequest::OnPaymentResponse(PaymentResponsePtr response) {
     ClearResolversAndCloseMojoConnection();
     return;
   }
+
+  UseCounter::Count(GetExecutionContext(), WebFeature::kPaymentRequestResponse);
 
   // If the website does not call complete() 60 seconds after show() has been
   // resolved, then behave as if the website called complete("fail").
