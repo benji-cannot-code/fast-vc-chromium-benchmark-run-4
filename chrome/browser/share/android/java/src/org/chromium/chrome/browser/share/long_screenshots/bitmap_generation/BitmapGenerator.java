755dd862ee0c83b9252da7e24ff94b895e700031
ckitagawa <ckitagawa@chromium.org>
2021-09-14 15:35:08 +0000

    [Scroll Capture] Fix scaling bug
    Fix a bug where scale factor is assumed to be 1 for compositing when
    the value should be (viewport width / content width) to ensure auto
    width works correctly.
    
    Bug: 1246509
    Change-Id: If3dba3c8efbc6d03c147efd5e435c7bfb8af5813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154822
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fred Mello <fredmello@google.com>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921232}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/long_screenshots/bitmap_generation/BitmapGenerator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/long_screenshots/bitmap_generation/BitmapGenerator.java
@@ -31,6 +31,7 @@ public class BitmapGenerator implements LongScreenshotsTabService.CaptureProcess
 
     protected GeneratorCallBack mGeneratorCallBack;
     private ScreenshotBoundsManager mBoundsManager;
+    private float mScaleFactor;
 
     /**
      * Users of the {@link LongScreenshotsEntry} class have to implement and pass this interface in
@@ -70,6 +71,7 @@ public class BitmapGenerator implements LongScreenshotsTabService.CaptureProcess
         }
         mTabService.setCaptureProcessor(this);
         mTabService.captureTab(mTab, mBoundsManager.getCaptureBounds(), inMemory);
+        mScaleFactor = 0f;
     }
 
     /**
@@ -103,8 +105,12 @@ public class BitmapGenerator implements LongScreenshotsTabService.CaptureProcess
             Rect rect, Runnable errorCallback, Callback<Bitmap> onBitmapGenerated) {
         // Check if the compositor is ready and whether the rect is within the bounds of the
         // the capture.
+        if (mScaleFactor == 0f) {
+            mScaleFactor =
+                    mBoundsManager.getBitmapScaleFactorFromCompositedWidth(mCompositor.getWidth());
+        }
         return mCompositor.requestBitmap(mBoundsManager.calculateBoundsRelativeToCapture(rect),
-                errorCallback, onBitmapGenerated);
+                mScaleFactor, errorCallback, onBitmapGenerated);
     }
 
     /**
