818dda0e00d984e50cdf7812977f46a2ef5b55ad
Xianzhu Wang <wangxianzhu@chromium.org>
2021-09-14 19:19:45 +0000

    Update comment for change of PaintedOutputInvisible()
    This is based on the review comment at
    https://chromium-review.googlesource.com/c/chromium/src/+/3119761/4/third_party/blink/renderer/core/paint/paint_layer.cc#1491
    
    Bug: 1242746
    Change-Id: Ie8c33729639ad694ec1fda40eb56d27f935a0039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159324
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921346}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -1489,7 +1489,8 @@ void PaintLayer::RemoveOnlyThisLayerAfterStyleChange(
 
     if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled() &&
         PaintLayerPainter::PaintedOutputInvisible(*old_style)) {
-      // This layer is removed because opacity becomes 1. Do the same as
+      // PaintedOutputInvisible() was true because opacity was near zero, and
+      // this layer is to be removed because opacity becomes 1. Do the same as
       // StyleDidChange() on change of PaintedOutputInvisible().
       GetLayoutObject().SetSubtreeShouldDoFullPaintInvalidation();
     }
