8e439de3e85a1245f5895843047a5590365ad0e2
David Roger <droger@chromium.org>
2021-09-14 12:50:33 +0000

    [Lacros][profiles] Enable account consistency in secondary profiles
    When chrome://flags/#multi-profile-account-consistency is enabled,
    account consistency is turned on in all Lacros profiles.
    
    If the flag is disabled (default), this CL does not change anything.
    
    Bug: 1233933
    Change-Id: Ide73fa170fefefe3538bbeb6229bc71efe3ac975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3156638
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921172}

--- chrome/browser/lacros/account_manager/account_manager_util.cc
+++ chrome/browser/lacros/account_manager/account_manager_util.cc
@@ -7,11 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profiles_state.h"
+#include "chrome/browser/signin/signin_features.h"
 
 bool IsAccountManagerAvailable(const Profile* profile) {
   // Account Manager / Mirror is only enabled on Lacros's Main Profile for now.
   if (!profile->IsMainProfile())
-    return false;
+    return base::FeatureList::IsEnabled(kMultiProfileAccountConsistency);
 
   // TODO(anastasiian): check for Web kiosk mode.
 
