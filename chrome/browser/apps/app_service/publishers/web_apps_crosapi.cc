1939db4d976ac29eb89cd712420a66b79673aebc
Raymes Khoury <raymes@chromium.org>
2021-09-14 08:36:38 +0000

    Implement StopApp for web apps
    This implements the AppService's StopApp function for web apps, both
    when they are running in Lacros and Ash. This function will be used
    to implement the ability to close web apps from the shelf.
    
    Change-Id: I9771c9daa00d950d0b874d03b105bad0d38390af
    Bug: 1247474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3144256
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921104}

--- chrome/browser/apps/app_service/publishers/web_apps_crosapi.cc
+++ chrome/browser/apps/app_service/publishers/web_apps_crosapi.cc
@@ -203,6 +203,10 @@ void WebAppsCrosapi::UnpauseApp(const std::string& app_id) {
   controller_->UnpauseApp(app_id);
 }
 
+void WebAppsCrosapi::StopApp(const std::string& app_id) {
+  controller_->StopApp(app_id);
+}
+
 void WebAppsCrosapi::OpenNativeSettings(const std::string& app_id) {
   controller_->OpenNativeSettings(app_id);
 }
