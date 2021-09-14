9243d38aa3755e0c9d4622f05607903e830e7686
Karandeep Bhatia <karandeepb@chromium.org>
2021-09-14 22:26:05 +0000

    Extensions: Introduce a new feature to allow mv3 extensions to use wasm.
    Implement a new feature "AllowWasmInMV3" to allow Manifest V3 extensions
    to use web assembly. When this feature is enabled:
    
    - Extensions which don't specify an extension_pages CSP will get a
    default CSP of "script-src 'self' 'wasm-eval'; object-src 'self';".
    
    - Extensions which specify a CSP will be able to use 'wasm-eval' as part
    of their extension pages CSP.
    
    Bug: 1248599
    Change-Id: Ia0d550f5567b5951ca38bc00ec5819aa79dfbbd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152352
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921441}

--- extensions/common/extension_features.h
+++ extensions/common/extension_features.h
@@ -31,6 +31,8 @@ extern const base::Feature kAllowSharedArrayBuffersUnconditionally;
 
 extern const base::Feature kU2FSecurityKeyAPI;
 
+extern const base::Feature kAllowWasmInMV3;
+
 }  // namespace extensions_features
 
 #endif  // EXTENSIONS_COMMON_EXTENSION_FEATURES_H_
