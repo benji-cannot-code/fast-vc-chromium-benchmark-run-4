dbaca310b1d60ee6892c993330f4e7e190265b08
David Benjamin <davidben@chromium.org>
2021-09-14 01:20:50 +0000

    Invalid ECHConfigLists should not be ERR_UNEXPECTED
    Most of the Init() errors are just internal malloc failures and thus
    ERR_UNEXPECTED. However, SSL_set1_ech_config_list can fail if we got a
    syntax error out of DNS. Return an error code for this. Also, in
    anticipation of having to debug these in the future, add the
    ECHConfigList to NetLog.
    
    (Unsupported ECHConfigs aren't an error. BoringSSL will just internally
    skip them over. This function failing is about the whole ECHConfigList
    structure not parsing.)
    
    Bug: 1091403
    Change-Id: I1e305fb126abf71f98df1d573cef9f42802d1008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158566
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921030}

--- net/log/net_log_values.h
+++ net/log/net_log_values.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/containers/span.h"
 #include "base/strings/string_piece_forward.h"
 #include "net/base/net_export.h"
 
@@ -48,6 +49,7 @@ NET_EXPORT base::Value NetLogStringValue(base::StringPiece raw);
 // serializing them.
 //
 // This wrapper encodes |bytes| as a Base64 encoded string.
+NET_EXPORT base::Value NetLogBinaryValue(base::span<const uint8_t> bytes);
 NET_EXPORT base::Value NetLogBinaryValue(const void* bytes, size_t length);
 
 // Creates a base::Value() to represent integers, including 64-bit ones.
