21d9d26184a48d02f0e01f4c4e30d599e3643058
Torne (Richard Coles) <torne@google.com>
2021-09-14 17:28:46 +0000

    android: handle unusual classloaders correctly.
    We were assuming that the application and Chromium classloaders are both
    subclasses of BaseDexClassLoader in ClassLoaderContextWrapperFactory,
    used by WebView and WebLayer. This isn't always true (e.g. instant apps
    have an unusual classloader type), so can cause crashes.
    
    Instead just wrap the classloader without casting it, and only attempt
    to cast it during findLibrary if it's actually the correct type. This
    means we may not be able to load native libraries from splits
    successfully if we're in this situation, but this is much better than
    crashing on startup, and WebView should not need to do this in the first
    place.
    
    Bug: 1249483
    Change-Id: I9e53dfc6bd862574fa36da4db3322060eb15132b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3160856
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921289}

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/application/ClassLoaderContextWrapperFactory.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/application/ClassLoaderContextWrapperFactory.java
@@ -14,8 +14,6 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.view.LayoutInflater;
 
-import dalvik.system.BaseDexClassLoader;
-
 import org.chromium.base.ContextUtils;
 import org.chromium.base.WrappedClassLoader;
 
@@ -81,11 +79,7 @@ public class ClassLoaderContextWrapperFactory {
             final ClassLoader appCl = getBaseContext().getClassLoader();
             final ClassLoader chromiumCl = ClassLoaderContextWrapper.class.getClassLoader();
 
-            assert appCl instanceof BaseDexClassLoader;
-            assert chromiumCl instanceof BaseDexClassLoader;
-
-            return new WrappedClassLoader(
-                    (BaseDexClassLoader) chromiumCl, (BaseDexClassLoader) appCl);
+            return new WrappedClassLoader(chromiumCl, appCl);
         }
 
         @Override
