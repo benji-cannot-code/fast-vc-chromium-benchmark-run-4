d6489df4d4a394171fcc8950bc9222767e5b0a6e
Michael Checo <michaelcheco@google.com>
2021-09-14 02:03:48 +0000

    [navigation_view] Add media queries and support for hiding nav panel
    Bug: 1205539
    Change-Id: I74457f00846cc20b6d273405997acd579125b643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3158582
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921050}

--- ash/webui/common/resources/navigation_view_panel.js
+++ ash/webui/common/resources/navigation_view_panel.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import './navigation_shared_vars.js';
 import './page_toolbar.js';
 
@@ -80,6 +81,11 @@ export class NavigationViewPanelElement extends PolymerElement {
       showToolBar: {
         type: Boolean,
         value: false,
+      },
+
+      /** @protected {boolean} */
+      showNav: {
+        type: Boolean,
       },
     }
   }
