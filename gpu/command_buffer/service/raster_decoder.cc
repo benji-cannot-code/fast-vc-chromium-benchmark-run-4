c7efe12429450300de9984d13dcd124eb04695ae
Vasiliy Telezhnikov <vasilyt@chromium.org>
2021-09-14 13:35:49 +0000

    Always reset ContextState after RasterDecoder
    RasterDecoder uses skia which alters GL Context state. To make sure that
    GLES2 decoders restore this state we set |need_context_state_reset_|
    flag in SharedContextState.
    
    This CLs makes sure that we always do reset by setting this flag in
    MakeCurrent() instead of individual commands, so we don't miss one (e.g
    DoConvertYUVMailboxesToRGBINTERNAL was missing one).
    
    Bug: 1248549
    Change-Id: I5291f3538800ecff01bfcc5fb1fed22c171aa69b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154804
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921182}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -1219,6 +1219,9 @@ bool RasterDecoderImpl::MakeCurrent() {
   // Rebind textures if the service ids may have changed.
   RestoreAllExternalTextureBindingsIfNeeded();
 
+  // We're going to use skia, so make sure we reset context afterwards.
+  shared_context_state_->set_need_context_state_reset(true);
+
   return true;
 }
 
@@ -2453,8 +2456,6 @@ void RasterDecoderImpl::DoCopySubTextureINTERNALSkia(
   std::vector<GrBackendSemaphore> begin_semaphores;
   std::vector<GrBackendSemaphore> end_semaphores;
 
-  shared_context_state_->set_need_context_state_reset(true);
-
   // Allow uncleared access, as we manually handle clear tracking.
   std::unique_ptr<SharedImageRepresentationSkia::ScopedWriteAccess>
       dest_scoped_access = dest_shared_image->BeginScopedWriteAccess(
@@ -3503,7 +3504,6 @@ void RasterDecoderImpl::DoBeginRasterCHROMIUM(GLuint sk_color,
 
   DCHECK(locked_handles_.empty());
   DCHECK(!raster_canvas_);
-  shared_context_state_->set_need_context_state_reset(true);
 
   SkColorType sk_color_type = viz::ResourceFormatToClosestSkColorType(
       /*gpu_compositing=*/true, shared_image_->format());
@@ -3618,7 +3618,6 @@ void RasterDecoderImpl::DoRasterCHROMIUM(GLuint raster_shm_id,
     return;
   }
   DCHECK(transfer_cache());
-  shared_context_state_->set_need_context_state_reset(true);
 
   if (font_shm_size > 0) {
     // Deserialize fonts before raster.
@@ -3718,7 +3717,6 @@ void RasterDecoderImpl::DoEndRasterCHROMIUM() {
     return;
   }
 
-  shared_context_state_->set_need_context_state_reset(true);
   raster_canvas_ = nullptr;
 
   if (use_ddl_in_current_raster_session_) {
@@ -3826,9 +3824,6 @@ void RasterDecoderImpl::DoCreateTransferCacheEntryINTERNAL(
   GrDirectContext* context_for_entry =
       cc::ServiceTransferCacheEntry::UsesGrContext(entry_type) ? gr_context()
                                                                : nullptr;
-  if (context_for_entry)
-    shared_context_state_->set_need_context_state_reset(true);
-
   if (!transfer_cache()->CreateLockedEntry(
           ServiceTransferCache::EntryKey(raster_decoder_id_, entry_type,
                                          entry_id),
