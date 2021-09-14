030ca76dd570c1addee05a226a89a49477501b91
Mike Dougherty <michaeldo@chromium.org>
2021-09-14 00:25:48 +0000

    Migrate ExecuteJavaScript to CallJavaScriptFunction
    These JavaScript calls in tests can be migrated to use the now
    recommended CallJavaScriptFunction.
    
    Change-Id: Idf01cb7c6201b895281d68c2f7f101322b14a533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3157521
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921013}

--- ios/web/public/test/web_test_with_web_state.mm
+++ ios/web/public/test/web_test_with_web_state.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/navigation/wk_navigation_util.h"
 #include "ios/web/public/deprecated/url_verification_constants.h"
 #import "ios/web/public/test/js_test_util.h"
+#import "ios/web/public/test/web_view_interaction_test_util.h"
 #import "ios/web/public/web_client.h"
 #include "ios/web/public/web_state_observer.h"
 #import "ios/web/web_state/ui/crw_web_controller.h"
@@ -207,6 +208,12 @@ bool WebTestWithWebState::WaitUntilLoaded() {
     WaitForBackgroundTasks();
     return !web_state()->IsLoading();
   });
+}
+
+std::unique_ptr<base::Value> WebTestWithWebState::CallJavaScriptFunction(
+    const std::string& function,
+    const std::vector<base::Value>& parameters) {
+  return web::test::CallJavaScriptFunction(web_state(), function, parameters);
 }
 
 id WebTestWithWebState::ExecuteJavaScript(NSString* script) {
--- ios/web/web_state/js/message_js_unittest.mm
+++ ios/web/web_state/js/message_js_unittest.mm
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-// JavaScript to return a frame's frameId.
-NSString* const kGetFrameIdJs = @"__gCrWeb.message.getFrameId();";
+// JavaScript function to return a frame's frameId.
+const char kGetFrameIdJsFunction[] = "message.getFrameId";
 }  // namespace
 
 namespace web {
@@ -27,26 +27,26 @@ typedef web::WebTestWithWebState MessageJsTest;
 TEST_F(MessageJsTest, FrameId) {
   ASSERT_TRUE(LoadHtml("<p>"));
 
-  NSString* frame_id = ExecuteJavaScript(kGetFrameIdJs);
+  auto result = CallJavaScriptFunction(kGetFrameIdJsFunction, {});
   // Validate frameId.
-  EXPECT_TRUE(frame_id);
-  EXPECT_TRUE([frame_id isKindOfClass:[NSString class]]);
-  EXPECT_GT(frame_id.length, 0ul);
+  ASSERT_TRUE(result);
+  ASSERT_TRUE(result->is_string());
+  EXPECT_GT(result->GetString().length(), 0ul);
 }
 
 // Tests that the frameId is unique between two page loads.
 TEST_F(MessageJsTest, UniqueFrameID) {
   ASSERT_TRUE(LoadHtml("<p>"));
-  NSString* frame_id = ExecuteJavaScript(kGetFrameIdJs);
+  auto frame_id1 = CallJavaScriptFunction(kGetFrameIdJsFunction, {});
 
   ASSERT_TRUE(LoadHtml("<p>"));
-  NSString* frame_id2 = ExecuteJavaScript(kGetFrameIdJs);
+  auto frame_id2 = CallJavaScriptFunction(kGetFrameIdJsFunction, {});
   // Validate second frameId.
-  EXPECT_TRUE(frame_id2);
-  EXPECT_TRUE([frame_id2 isKindOfClass:[NSString class]]);
-  EXPECT_GT(frame_id2.length, 0ul);
+  ASSERT_TRUE(frame_id2);
+  ASSERT_TRUE(frame_id2->is_string());
+  EXPECT_GT(frame_id2->GetString().length(), 0ul);
 
-  EXPECT_NSNE(frame_id, frame_id2);
+  EXPECT_NE(frame_id1->GetString(), frame_id2->GetString());
 }
 
 }  // namespace web
