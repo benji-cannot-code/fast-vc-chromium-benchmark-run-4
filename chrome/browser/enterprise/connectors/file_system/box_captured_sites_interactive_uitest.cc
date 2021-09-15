507779377231decb0ab2740fe00d608b46a79596
uwyiming <uwyiming@google.com>
2021-09-15 17:03:30 +0000

    Minor fixes for the FSC integration tests.
    Point the capture dir at the correct folder.
    Adapt the tests to new upload completion behavior in the box uploader
    (upload counts as succeeded if user cancels the upload).
    
    Bug: 1214825
    Change-Id: If73993cc6b52e432710e65bf68fc94ee0f804d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161380
    Reviewed-by: Alice Gong <alicego@google.com>
    Commit-Queue: Alice Gong <alicego@google.com>
    Cr-Commit-Position: refs/heads/main@{#921707}

--- chrome/browser/enterprise/connectors/file_system/box_captured_sites_interactive_uitest.cc
+++ chrome/browser/enterprise/connectors/file_system/box_captured_sites_interactive_uitest.cc
@@ -208,7 +208,7 @@ bool GetWPRCaptureDir(base::FilePath* capture_dir) {
                      .AppendASCII("enterprise")
                      .AppendASCII("connectors")
                      .AppendASCII("file_system")
-                     .AppendASCII("captures");
+                     .AppendASCII("captured_sites");
   return true;
 }
 
@@ -1203,7 +1203,7 @@ IN_PROC_BROWSER_TEST_F(BoxCapturedSitesInteractiveTest,
   ASSERT_NO_FATAL_FAILURE(
       SetCloudFSCPolicy(GetAllAllowedTestPolicy("797972721")));
   ASSERT_NO_FATAL_FAILURE(
-      StartWprUsingFSCCaptureDir("box.com.large_download.wpr"));
+      StartWprUsingFSCCaptureDir("box.com.large.download.wpr"));
 
   StartDownloadByNavigatingToEmbeddedServerUrl(
       "/enterprise/connectors/file_system/downloads/cipd/"
@@ -1317,7 +1317,7 @@ IN_PROC_BROWSER_TEST_F(BoxCapturedSitesInteractiveTest,
   download_item_observer.WaitForSignInConfirmationDialog();
   ASSERT_NO_FATAL_FAILURE(
       download_item_observer.sign_in_observer()->CancelBoxSignInConfirmation());
-  EXPECT_FALSE(
+  EXPECT_TRUE(
       download_item_observer.upload_observer()->WaitForUploadCompletion());
   EXPECT_TRUE(
       download_item_observer.upload_observer()->WaitForTmpFileDeletion());
@@ -1353,7 +1353,7 @@ IN_PROC_BROWSER_TEST_F(BoxCapturedSitesInteractiveTest, ExitSignInDialog) {
       download_item_observer.sign_in_observer()->SubmitInvalidSignInCredentials(
           GetBoxAccountUserName(), GetBoxAccountPassword()));
   download_item_observer.sign_in_observer()->CloseSignInWidget();
-  EXPECT_FALSE(
+  EXPECT_TRUE(
       download_item_observer.upload_observer()->WaitForUploadCompletion());
   EXPECT_TRUE(
       download_item_observer.upload_observer()->WaitForTmpFileDeletion());
@@ -1593,7 +1593,7 @@ IN_PROC_BROWSER_TEST_F(BoxCapturedSitesInteractiveTest,
   EXPECT_EQ(kLogin, login_val);
 
   download_item_observer.sign_in_observer()->CloseSignInWidget();
-  EXPECT_FALSE(
+  EXPECT_TRUE(
       download_item_observer.upload_observer()->WaitForUploadCompletion());
   EXPECT_TRUE(
       download_item_observer.upload_observer()->WaitForTmpFileDeletion());
