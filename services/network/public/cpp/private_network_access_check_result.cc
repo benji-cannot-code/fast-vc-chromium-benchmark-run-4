26cfb4b6895c301dd3d5db9af71025ad78028557
Titouan Rigoudy <titouan@chromium.org>
2021-09-15 15:17:13 +0000

    [Private Network Access] Extract check, improve metric test coverage.
    Bug: chromium:1247571
    Change-Id: I2beb3bef34795214d6bc5282e0cfb5790a43d953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152016
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921665}

---
+++ services/network/public/cpp/private_network_access_check_result.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/public/cpp/private_network_access_check_result.h"
+
+namespace network {
+
+bool PrivateNetworkAccessCheckResultIsAllowed(
+    PrivateNetworkAccessCheckResult result) {
+  switch (result) {
+    case PrivateNetworkAccessCheckResult::kAllowedMissingClientSecurityState:
+    case PrivateNetworkAccessCheckResult::kAllowedNoLessPublic:
+    case PrivateNetworkAccessCheckResult::kAllowedByPolicyAllow:
+    case PrivateNetworkAccessCheckResult::kAllowedByPolicyWarn:
+      return true;
+    case PrivateNetworkAccessCheckResult::kBlockedByLoadOption:
+    case PrivateNetworkAccessCheckResult::kBlockedByPolicyBlock:
+      return false;
+  }
+}
+
+}  // namespace network
