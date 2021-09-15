8d3a4b767852ab646fe78beed5662b061dfe0c2c
Alison Maher <almaher@microsoft.com>
2021-09-15 20:54:48 +0000

    [LayoutNG] OOF with inline CB relative offset
    This CL updates how relative offsets are handled for OOFs with inline
    containing blocks when block fragmented. Instead of applying an
    accumulated relative offset for OOFs (CL:3039319) when
    MoveOutOfFlowDescendantCandidatesToDescendants() is called on the CB,
    apply the relative offsets via PropagateChildData().
    
    As a result of this change, the following test now passes with
    NGBlockFragmentation and NGBlockInInline enabled:
      external/wpt/css/css-break/out-of-flow-in-multicolumn-059.html
    
    Bug: 716930
    Change-Id: I5093a3c58ed3ab14dbaa9e559e2d4aea9111d1b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161073
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921847}

--- third_party/blink/renderer/core/layout/ng/ng_container_fragment_builder.cc
+++ third_party/blink/renderer/core/layout/ng/ng_container_fragment_builder.cc
@@ -270,8 +270,8 @@ void NGContainerFragmentBuilder::TransferOutOfFlowCandidates(
   oof_positioned_candidates_.clear();
 }
 
-void NGContainerFragmentBuilder::MoveOutOfFlowDescendantCandidatesToDescendants(
-    LogicalOffset relative_offset) {
+void NGContainerFragmentBuilder::
+    MoveOutOfFlowDescendantCandidatesToDescendants() {
   DCHECK(oof_positioned_descendants_.IsEmpty());
   std::swap(oof_positioned_candidates_, oof_positioned_descendants_);
 
@@ -288,7 +288,8 @@ void NGContainerFragmentBuilder::MoveOutOfFlowDescendantCandidatesToDescendants(
     if (!candidate.inline_container.container &&
         IsInlineContainerForNode(candidate.Node(), layout_object_)) {
       candidate.inline_container = NGInlineContainer<LogicalOffset>(
-          To<LayoutInline>(layout_object_), relative_offset);
+          To<LayoutInline>(layout_object_),
+          /* relative_offset */ LogicalOffset());
     }
 
     // Ensure that the inline_container is a continuation root.
