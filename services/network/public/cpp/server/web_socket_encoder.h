6f22ff3376896b95a8aed03801f657069e6a5f8d
Shiho Noda <shihonoda@google.com>
2021-09-15 13:13:29 +0000

    Add unit tests for WebSocketEncoder
    Add unit tests for WebSocketEncoder which is the version in the network
    service. These tests are copied verbatim from
    //net/server/web_socket_encoder_unittest.cc.
    
    Change-Id: Ia86dbb9e6d48cd79534c1562b3775491520ccc62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159790
    Commit-Queue: Shiho Noda <shihonoda@google.com>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921595}

--- services/network/public/cpp/server/web_socket_encoder.h
+++ services/network/public/cpp/server/web_socket_encoder.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/component_export.h"
 #include "base/macros.h"
 #include "base/strings/string_piece.h"
 #include "net/websockets/websocket_deflater.h"
@@ -24,7 +25,7 @@ namespace network {
 
 namespace server {
 
-class WebSocketEncoder final {
+class COMPONENT_EXPORT(NETWORK_CPP) WebSocketEncoder final {
  public:
   static const char kClientExtensions[];
 
