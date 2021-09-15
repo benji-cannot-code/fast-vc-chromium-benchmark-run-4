7121153dfc92398486688ff2cfd179cdcb039c35
Joel Hockey <joelhockey@chromium.org>
2021-09-15 04:12:27 +0000

    Allow dragged image File to be dropped in any renderer
    We currently support drag-drop of images from browser to native
    file managers in Windows and Mac, and kind of support it for any other
    platform using ozone (x11, wayland). This work is to enable a similar
    feature for CrOS FilesApp.
    
    Since CrOS FilesApp is a web app, adding support for it to receive
    the dragged image File object via OSExchangeData also adds this support
    for Windows and Linux so that any image can drag-drop between pages.
    
    Also adds support for DropData::Metadata::BINARY.
    
    Bug: 1247873
    
    Change-Id: I0a3f330caff07f326941f3ca91e00047bd16e4ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3146940
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921525}

--- content/browser/renderer_host/data_transfer_util.cc
+++ content/browser/renderer_host/data_transfer_util.cc
@@ -80,7 +80,6 @@ blink::mojom::DragDataPtr DropDataToDragData(
     int child_id) {
   // These fields are currently unused when dragging into Blink.
   DCHECK(drop_data.download_metadata.empty());
-  DCHECK(drop_data.file_contents.empty());
   DCHECK(drop_data.file_contents_content_disposition.empty());
 
   std::vector<blink::mojom::DragItemPtr> items;
@@ -119,7 +118,16 @@ blink::mojom::DragDataPtr DropDataToDragData(
     item->file_system_id = file_system_file.filesystem_id;
     items.push_back(blink::mojom::DragItem::NewFileSystemFile(std::move(item)));
   }
-  for (const std::pair<std::u16string, std::u16string> data :
+  if (drop_data.file_contents_source_url.is_valid()) {
+    blink::mojom::DragItemBinaryPtr item = blink::mojom::DragItemBinary::New();
+    item->data = mojo_base::BigBuffer(
+        base::as_bytes(base::make_span(drop_data.file_contents)));
+    item->source_url = drop_data.file_contents_source_url;
+    item->filename_extension =
+        base::FilePath(drop_data.file_contents_filename_extension);
+    items.push_back(blink::mojom::DragItem::NewBinary(std::move(item)));
+  }
+  for (const std::pair<const std::u16string, std::u16string>& data :
        drop_data.custom_data) {
     blink::mojom::DragItemStringPtr item = blink::mojom::DragItemString::New();
     item->string_type = base::UTF16ToUTF8(data.first);
@@ -180,6 +188,14 @@ blink::mojom::DragDataPtr DropMetaDataToDragData(
       item->url = meta_data_item.file_system_url;
       items.push_back(
           blink::mojom::DragItem::NewFileSystemFile(std::move(item)));
+      continue;
+    }
+
+    if (meta_data_item.kind == DropData::Kind::BINARY) {
+      blink::mojom::DragItemBinaryPtr item =
+          blink::mojom::DragItemBinary::New();
+      item->source_url = meta_data_item.file_contents_url;
+      items.push_back(blink::mojom::DragItem::NewBinary(std::move(item)));
       continue;
     }
   }
