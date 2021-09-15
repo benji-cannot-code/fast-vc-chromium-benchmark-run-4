45c6b5089b8996d3c28cba0767488b4fcfee7f71
Ari Chivukula <arichiv@chromium.org>
2021-09-15 22:20:28 +0000

    [Re-Factor Client Hints] (5) Remove (unlaunched) `lang` client hint
    Now that we've cleaned up the array indexing that was blocking this,
    let's move forward with removing this unused client hint.
    
    This is a part of a series of CLs to re-factor the client hints code:
    (1) Label hints with legacy names
    (2) Fix the naming of (unlaunched) `lang` client hint
    (3) Consolidate client hint name lists
    (4) Migrate from array to map indexing for client hint names
    (5) Remove (unlaunched) `lang` client hint
    
    Bug: 1227043
    Change-Id: I57404516cd1f39f738f0fbd7c202de7adf063d98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3160286
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921893}

--- third_party/blink/renderer/platform/loader/fetch/client_hints_preferences_test.cc
+++ third_party/blink/renderer/platform/loader/fetch/client_hints_preferences_test.cc
@@ -20,7 +20,6 @@ class ClientHintsPreferencesTest : public testing::Test {
   ClientHintsPreferencesTest() {
     scoped_feature_list_.InitWithFeatures(
         /*enabled_features=*/{blink::features::kUserAgentClientHint,
-                              blink::features::kLangClientHintHeader,
                               blink::features::
                                   kPrefersColorSchemeClientHintHeader},
         /*disabled_features=*/{});
@@ -39,7 +38,6 @@ TEST_F(ClientHintsPreferencesTest, BasicSecure) {
     bool expectation_rtt;
     bool expectation_downlink;
     bool expectation_ect;
-    bool expectation_lang;
     bool expectation_ua;
     bool expectation_ua_arch;
     bool expectation_ua_platform;
@@ -48,34 +46,34 @@ TEST_F(ClientHintsPreferencesTest, BasicSecure) {
     bool expectation_prefers_color_scheme;
   } cases[] = {
       {"width, dpr, viewportWidth", true, true, false, false, false, false,
-       false, false, false, false, false, false, false},
-      {"WiDtH, dPr, viewport-width, rtt, downlink, ect, lang, "
+       false, false, false, false, false, false},
+      {"WiDtH, dPr, viewport-width, rtt, downlink, ect, "
        "sec-ch-prefers-color-scheme",
-       true, true, true, true, true, true, true, false, false, false, false,
-       false, true},
+       true, true, true, true, true, true, false, false, false, false, false,
+       true},
       {"WiDtH, dPr, viewport-width, rtt, downlink, effective-connection-type",
        true, true, true, true, true, false, false, false, false, false, false,
-       false, false},
+       false},
       {"WIDTH, DPR, VIWEPROT-Width", true, true, false, false, false, false,
-       false, false, false, false, false, false, false},
+       false, false, false, false, false, false},
       {"VIewporT-Width, wutwut, width", true, false, true, false, false, false,
-       false, false, false, false, false, false, false},
+       false, false, false, false, false, false},
       {"dprw", false, false, false, false, false, false, false, false, false,
-       false, false, false, false},
+       false, false, false},
       {"DPRW", false, false, false, false, false, false, false, false, false,
+       false, false, false},
+      {"sec-ch-ua", false, false, false, false, false, false, true, false,
        false, false, false, false},
-      {"sec-ch-ua", false, false, false, false, false, false, false, true,
-       false, false, false, false, false},
-      {"sec-ch-ua-arch", false, false, false, false, false, false, false, false,
-       true, false, false, false, false},
+      {"sec-ch-ua-arch", false, false, false, false, false, false, false, true,
+       false, false, false, false},
       {"sec-ch-ua-platform", false, false, false, false, false, false, false,
-       false, false, true, false, false, false},
+       false, true, false, false, false},
       {"sec-ch-ua-model", false, false, false, false, false, false, false,
-       false, false, false, true, false, false},
+       false, false, true, false, false},
       {"sec-ch-ua, sec-ch-ua-arch, sec-ch-ua-platform, sec-ch-ua-model, "
        "sec-ch-ua-full-version",
-       false, false, false, false, false, false, false, true, true, true, true,
-       true, false},
+       false, false, false, false, false, false, true, true, true, true, true,
+       false},
   };
 
   for (const auto& test_case : cases) {
@@ -104,9 +102,6 @@ TEST_F(ClientHintsPreferencesTest, BasicSecure) {
     EXPECT_EQ(test_case.expectation_ect,
               preferences.ShouldSend(
                   network::mojom::WebClientHintsType::kEct_DEPRECATED));
-    EXPECT_EQ(
-        test_case.expectation_lang,
-        preferences.ShouldSend(network::mojom::WebClientHintsType::kLang));
     EXPECT_EQ(test_case.expectation_ua,
               preferences.ShouldSend(network::mojom::WebClientHintsType::kUA));
     EXPECT_EQ(
@@ -174,8 +169,6 @@ TEST_F(ClientHintsPreferencesTest, SecureEnabledTypesMerge) {
       network::mojom::WebClientHintsType::kDownlink_DEPRECATED));
   EXPECT_FALSE(preferences.ShouldSend(
       network::mojom::WebClientHintsType::kEct_DEPRECATED));
-  EXPECT_FALSE(
-      preferences.ShouldSend(network::mojom::WebClientHintsType::kLang));
   EXPECT_FALSE(preferences.ShouldSend(network::mojom::WebClientHintsType::kUA));
   EXPECT_FALSE(
       preferences.ShouldSend(network::mojom::WebClientHintsType::kUAArch));
@@ -197,8 +190,6 @@ TEST_F(ClientHintsPreferencesTest, SecureEnabledTypesMerge) {
       network::mojom::WebClientHintsType::kDownlink_DEPRECATED));
   EXPECT_FALSE(preferences.ShouldSend(
       network::mojom::WebClientHintsType::kEct_DEPRECATED));
-  EXPECT_FALSE(
-      preferences.ShouldSend(network::mojom::WebClientHintsType::kLang));
   EXPECT_FALSE(preferences.ShouldSend(network::mojom::WebClientHintsType::kUA));
   EXPECT_FALSE(
       preferences.ShouldSend(network::mojom::WebClientHintsType::kUAArch));
@@ -220,8 +211,6 @@ TEST_F(ClientHintsPreferencesTest, SecureEnabledTypesMerge) {
       network::mojom::WebClientHintsType::kDownlink_DEPRECATED));
   EXPECT_FALSE(preferences.ShouldSend(
       network::mojom::WebClientHintsType::kEct_DEPRECATED));
-  EXPECT_FALSE(
-      preferences.ShouldSend(network::mojom::WebClientHintsType::kLang));
   EXPECT_FALSE(preferences.ShouldSend(network::mojom::WebClientHintsType::kUA));
   EXPECT_FALSE(
       preferences.ShouldSend(network::mojom::WebClientHintsType::kUAArch));
@@ -243,8 +232,6 @@ TEST_F(ClientHintsPreferencesTest, SecureEnabledTypesMerge) {
       network::mojom::WebClientHintsType::kDownlink_DEPRECATED));
   EXPECT_FALSE(preferences.ShouldSend(
       network::mojom::WebClientHintsType::kEct_DEPRECATED));
-  EXPECT_FALSE(
-      preferences.ShouldSend(network::mojom::WebClientHintsType::kLang));
   EXPECT_FALSE(preferences.ShouldSend(network::mojom::WebClientHintsType::kUA));
   EXPECT_FALSE(
       preferences.ShouldSend(network::mojom::WebClientHintsType::kUAArch));
@@ -281,7 +268,6 @@ TEST_F(ClientHintsPreferencesTest, ParseHeaders) {
     bool expect_rtt;
     bool expect_downlink;
     bool expect_ect;
-    bool expect_lang;
     bool expect_ua;
     bool expect_ua_arch;
     bool expect_ua_platform;
@@ -289,25 +275,25 @@ TEST_F(ClientHintsPreferencesTest, ParseHeaders) {
     bool expect_ua_full_version;
     bool expect_prefers_color_scheme;
   } test_cases[] = {
-      {"width, dpr, viewportWidth, lang, sec-ch-prefers-color-scheme", false,
-       true, true, false, false, false, false, true, false, false, false, false,
-       false, true},
+      {"width, dpr, viewportWidth, sec-ch-prefers-color-scheme", false, true,
+       true, false, false, false, false, false, false, false, false, false,
+       true},
       {"width, dpr, viewportWidth", false, true, true, false, false, false,
-       false, false, false, false, false, false, false, false},
+       false, false, false, false, false, false, false},
       {"width, dpr, viewportWidth", false, true, true, false, false, false,
-       false, false, false, false, false, false, false, false},
+       false, false, false, false, false, false, false},
       {"width, dpr, viewportWidth", false, true, true, false, false, false,
-       false, false, false, false, false, false, false, false},
+       false, false, false, false, false, false, false},
       {"width, dpr, rtt, downlink, ect", false, true, true, false, true, true,
-       true, false, false, false, false, false, false, false},
+       true, false, false, false, false, false, false},
       {"device-memory", true, false, false, false, false, false, false, false,
-       false, false, false, false, false, false},
-      {"dpr rtt", false, false, false, false, false, false, false, false, false,
        false, false, false, false, false},
+      {"dpr rtt", false, false, false, false, false, false, false, false, false,
+       false, false, false, false},
       {"sec-ch-ua, sec-ch-ua-arch, sec-ch-ua-platform, sec-ch-ua-model, "
        "sec-ch-ua-full-version",
-       false, false, false, false, false, false, false, false, true, true, true,
-       true, true, false},
+       false, false, false, false, false, false, false, true, true, true, true,
+       true, false},
   };
 
   for (const auto& test : test_cases) {
@@ -327,8 +313,6 @@ TEST_F(ClientHintsPreferencesTest, ParseHeaders) {
         network::mojom::WebClientHintsType::kDownlink_DEPRECATED));
     EXPECT_FALSE(enabled_types.IsEnabled(
         network::mojom::WebClientHintsType::kEct_DEPRECATED));
-    EXPECT_FALSE(
-        enabled_types.IsEnabled(network::mojom::WebClientHintsType::kLang));
     EXPECT_FALSE(
         enabled_types.IsEnabled(network::mojom::WebClientHintsType::kUA));
     EXPECT_FALSE(
@@ -370,8 +354,6 @@ TEST_F(ClientHintsPreferencesTest, ParseHeaders) {
     EXPECT_EQ(test.expect_ect,
               enabled_types.IsEnabled(
                   network::mojom::WebClientHintsType::kEct_DEPRECATED));
-    EXPECT_EQ(test.expect_lang, enabled_types.IsEnabled(
-                                    network::mojom::WebClientHintsType::kLang));
     EXPECT_EQ(test.expect_ua,
               enabled_types.IsEnabled(network::mojom::WebClientHintsType::kUA));
     EXPECT_EQ(
@@ -383,9 +365,6 @@ TEST_F(ClientHintsPreferencesTest, ParseHeaders) {
     EXPECT_EQ(
         test.expect_ua_model,
         enabled_types.IsEnabled(network::mojom::WebClientHintsType::kUAModel));
-    EXPECT_EQ(test.expect_lang,
-              enabled_types.IsEnabled(
-                  network::mojom::WebClientHintsType::kPrefersColorScheme));
   }
 }
 
--- third_party/blink/web_tests/external/wpt/client-hints/resources/stale-echo-client-hints.py
+++ third_party/blink/web_tests/external/wpt/client-hints/resources/stale-echo-client-hints.py
@@ -17,7 +17,6 @@ def main(request, response):
         b"rtt",
         b"downlink",
         b"ect",
-        b"lang",
         b"sec-ch-ua",
         b"sec-ch-ua-arch",
         b"sec-ch-ua-platform",
