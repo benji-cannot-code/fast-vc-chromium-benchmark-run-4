783ae2e44ef9d0561f5d31dd77167020c09daf7f
Benoit Lize <lizeb@chromium.org>
2021-09-15 08:42:23 +0000

    [base/allocator] Support max alloc size with use_allocator = "none".
    On Linux with `use_allocator = "none"`, the shims dispatch to
    glibc. There is no way to restrict the maximum allocation size in this
    case, which is both not desirable, and causes issues with tests
    expecting that.
    
    This CL adds support for limiting allocation sizes in the shim. It has a
    runtime cost, but this should be very small, and also not matter for
    shipping releases, since we currently don't ship these configurations
    widely.
    
    Bug: 1248470, 169327
    Change-Id: Idca9c477a566de36727838135b937ddd7e19ed03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3157563
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921558}

--- components/cronet/native/test/buffer_test.cc
+++ components/cronet/native/test/buffer_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
+#include "build/build_config.h"
 #include "components/cronet/native/test/test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -75,12 +76,16 @@ TEST_F(BufferTest, TestInitWithAlloc) {
   ASSERT_FALSE(on_destroy_called());
 }
 
-#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
-    defined(THREAD_SANITIZER) || defined(OS_FUCHSIA) ||        \
-    BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
-// ASAN and MSAN malloc by default triggers crash instead of returning null on
-// failure. Fuchsia malloc() also crashes on allocation failure in some kernel
-// builds. PartitionAlloc malloc also crashes on allocation failure by design.
+#if defined(ARCH_CPU_64_BITS) &&                                              \
+    (defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) ||               \
+     defined(THREAD_SANITIZER) || BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC) || \
+     defined(OS_CHROMEOS) || defined(OS_LINUX) || defined(OS_FUCHSIA))
+// - ASAN and MSAN malloc by default triggers crash instead of returning null on
+//   failure.
+// - PartitionAlloc malloc also crashes on allocation failure by design.
+// - Fuchsia malloc() also crashes on allocation failure in some kernel builds.
+// - On Linux and Chrome OS, the allocator shims crash for large allocations, on
+//   purpose.
 #define MAYBE_TestInitWithHugeAllocFails DISABLED_TestInitWithHugeAllocFails
 #else
 #define MAYBE_TestInitWithHugeAllocFails TestInitWithHugeAllocFails
