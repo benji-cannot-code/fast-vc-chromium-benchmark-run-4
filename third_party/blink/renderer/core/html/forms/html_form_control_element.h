1bc8e0b8c3b9a34889c91e89fcc1d4684f1ac782
Matthias Körber <koerber@google.com>
2021-09-15 18:29:18 +0000

    [Autofill] In preview mode, only highlight changed fields
    Currently, Autofill highlights all fields that are either already
    autofilled or a filling is currently previewed.
    
    With this CL the behavior is changed:
    
    a) If Autofill is NOT in preview mode, all autofilled fields are
    highlighted which is the current behavior.
    
    b) If Autofill is in preview mode, only the fields that would change
    by accepting the filling are highlighted.
    
    To achieve b), fields that are already in the autofilled state must not
    be highlighted in preview mode. For this a binary state for form control
    elements is introduced that indicates if a
    WebAutofillState::kAutofilled field should be highlighted.
    This field is evaluated for selecting the css pseudo class and for
    painting the fields.
    
    Change-Id: I0d633bf4c480b061d6c8bcb45405514306ab7fd9
    Bug: 1248585, 1247978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151850
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921756}

--- third_party/blink/renderer/core/html/forms/html_form_control_element.h
+++ third_party/blink/renderer/core/html/forms/html_form_control_element.h
@@ -108,7 +108,16 @@ class CORE_EXPORT HTMLFormControlElement : public HTMLElement,
   bool IsAutofilled() const {
     return autofill_state_ != WebAutofillState::kNotFilled;
   }
+  bool HighlightAutofilled() const {
+    return autofill_state_ == WebAutofillState::kPreviewed ||
+           (autofill_state_ == WebAutofillState::kAutofilled &&
+            !PreventHighlightingOfAutofilledFields());
+  }
   void SetAutofillState(WebAutofillState = WebAutofillState::kAutofilled);
+  void SetPreventHighlightingOfAutofilledFields(bool prevent_highlighting);
+  bool PreventHighlightingOfAutofilledFields() const {
+    return prevent_highlighting_of_autofilled_fields_;
+  }
 
   // The autofill section to which this element belongs (e.g. billing address,
   // shipping address, .. .)
@@ -166,6 +175,7 @@ class CORE_EXPORT HTMLFormControlElement : public HTMLElement,
 
   WebString autofill_section_;
   enum WebAutofillState autofill_state_;
+  bool prevent_highlighting_of_autofilled_fields_ : 1;
 
   bool blocks_form_submission_ : 1;
 };
