120e00f4db774bab2bde5f39c084c439ab4b8e88
Peter Boström <pbos@chromium.org>
2021-09-15 06:35:54 +0000

    Use OnWidgetDestroying() in WidgetFadeAnimator
    OnWidgetClosing() is going away as an API (WidgetObservers expect this
    to always be called, but it's only called if an explicit Close() request
    is the reason for its closure, so it's commonly a UAF risk).
    
    This is maybe(???) also a speculative fix for crbug.com/1245948 as it's
    possible that something is requesting the closure of this widget on the
    same stack that it's created but before it's been used. If that's the
    case then it's possible that WidgetFadeAnimator's `widget_` is cleared
    out between its construction and first call to ::FadeIn().
    
    Bug: 1240365, 1245948
    Change-Id: I6bb28511086ed528442dba7dedd028b8f48b2f1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3160852
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921540}

--- ui/views/animation/widget_fade_animator.cc
+++ ui/views/animation/widget_fade_animator.cc
@@ -95,7 +95,7 @@ void WidgetFadeAnimator::AnimationEnded(const gfx::Animation* animation) {
   fade_complete_callbacks_.Notify(this, animation_type);
 }
 
-void WidgetFadeAnimator::OnWidgetClosing(Widget* widget) {
+void WidgetFadeAnimator::OnWidgetDestroying(Widget* widget) {
   widget_observation_.Reset();
   fade_animation_.End();
   animation_type_ = FadeType::kNone;
