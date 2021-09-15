52eda16395bb010e9ceaf5fa0f42313f16262ec5
Akihiro Ota <akihiroota@chromium.org>
2021-09-15 18:29:54 +0000

    Offline dictation: Remove period from string
    AX-Relnotes: N/A
    Bug: 1173135
    Change-Id: Ic36df18fe04a164b226eb4b6cdf55daeb1dd0ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161200
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921757}

--- chrome/test/data/webui/settings/chromeos/manage_accessibility_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/manage_accessibility_page_tests.js
@@ -313,7 +313,7 @@ suite('ManageAccessibilityPageTests', function() {
     assertTrue(!!dictationLocaleMenuSubtitle);
     assertEquals('Language', dictationLocaleMenuLabel.innerText);
     assertEquals(
-        'English (United States) is processed locally and works offline.',
+        'English (United States) is processed locally and works offline',
         dictationLocaleMenuSubtitle.innerText);
 
     // Fake a request to change the dictation locale menu subtitle.
@@ -350,7 +350,7 @@ suite('ManageAccessibilityPageTests', function() {
     page.dictationLocaleSubtitleOverride_ = 'Testing';
     Polymer.dom.flush();
     assertEquals(
-        'English (United States) is processed locally and works offline.',
+        'English (United States) is processed locally and works offline',
         page.computeDictationLocaleSubtitle_());
 
     // Changing the Dictation locale pref should change the subtitle
