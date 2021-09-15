eb29b0d2ad52d04d2402c50ad587368f903237da
Anders Hartvoll Ruud <andruud@chromium.org>
2021-09-15 13:17:47 +0000

    Make CSSParserTokenStream::AtEnd take boundaries into account
     - AtEnd() now considers the boundary token EOF. This is consistent
       with kBlockEnd also being treated as EOF.
     - Support multiple additive boundaries. Otherwise it would be
       possible to "escape" the current boundary by setting a more
       liberal (inner) boundary.
     - ConsumeUntilPeekedBoundary not needed anymore, and instead we
       can just call ConsumeUntilPeekedTypeIs<>.
    
    Bug: 962417
    Change-Id: I39e4b972752e8a89c45e03dc29f111b675857b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3156636
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#921602}

--- third_party/blink/renderer/core/css/parser/css_parser_token_stream.h
+++ third_party/blink/renderer/core/css/parser/css_parser_token_stream.h
@@ -59,16 +59,23 @@ class CORE_EXPORT CSSParserTokenStream {
     CSSParserTokenStream& stream_;
   };
 
-  // Instantiate this to set a short-term boundary for range extraction.
-  class RangeBoundary {
+  static constexpr uint64_t FlagForTokenType(CSSParserTokenType token_type) {
+    return 1ull << static_cast<uint64_t>(token_type);
+  }
+
+  // The specified token type will be treated as kEOF while the Boundary is on
+  // the stack.
+  class Boundary {
+    STACK_ALLOCATED();
+
    public:
-    RangeBoundary(CSSParserTokenStream& stream,
-                  CSSParserTokenType boundary_type)
-        : auto_reset_(&stream.boundary_type_, boundary_type) {}
-    ~RangeBoundary() = default;
+    Boundary(CSSParserTokenStream& stream, CSSParserTokenType boundary_type)
+        : auto_reset_(&stream.boundaries_,
+                      stream.boundaries_ | FlagForTokenType(boundary_type)) {}
+    ~Boundary() = default;
 
    private:
-    base::AutoReset<CSSParserTokenType> auto_reset_;
+    base::AutoReset<uint64_t> auto_reset_;
   };
 
   // We found that this value works well empirically by printing out the
@@ -135,7 +142,8 @@ class CORE_EXPORT CSSParserTokenStream {
 
   inline bool UncheckedAtEnd() const {
     DCHECK(HasLookAhead());
-    return next_.IsEOF() || next_.GetBlockType() == CSSParserToken::kBlockEnd;
+    return (boundaries_ & FlagForTokenType(next_.GetType())) ||
+           next_.GetBlockType() == CSSParserToken::kBlockEnd;
   }
 
   // Get the index of the character in the original string to be consumed next.
@@ -158,7 +166,8 @@ class CORE_EXPORT CSSParserTokenStream {
   // token and return false.
   bool ConsumeCommentOrNothing();
 
-  // Invalidates any ranges created by previous calls to ConsumeUntil*()
+  // Invalidates any ranges created by previous calls to
+  // ConsumeUntilPeekedTypeIs()
   template <CSSParserTokenType... Types>
   CSSParserTokenRange ConsumeUntilPeekedTypeIs() {
     EnsureLookAhead();
@@ -168,17 +177,6 @@ class CORE_EXPORT CSSParserTokenStream {
            !detail::IsTokenTypeOneOf<Types...>(UncheckedPeek().GetType())) {
       ConsumeTokenOrBlockAndAppendToBuffer();
     }
-
-    return CSSParserTokenRange(buffer_);
-  }
-
-  // Invalidates any ranges created by previous calls to ConsumeUntil*()
-  CSSParserTokenRange ConsumeUntilPeekedBoundary() {
-    EnsureLookAhead();
-
-    buffer_.Shrink(0);
-    while (!UncheckedAtEnd() && UncheckedPeek().GetType() != boundary_type_)
-      ConsumeTokenOrBlockAndAppendToBuffer();
 
     return CSSParserTokenRange(buffer_);
   }
@@ -228,7 +226,7 @@ class CORE_EXPORT CSSParserTokenStream {
   CSSParserToken next_;
   wtf_size_t offset_ = 0;
   bool has_look_ahead_ = false;
-  CSSParserTokenType boundary_type_ = kEOFToken;
+  uint64_t boundaries_ = FlagForTokenType(kEOFToken);
 };
 
 }  // namespace blink
