9b2565c94af186e7dd314dd735ea542598091d3b
Li Lin <llin@chromium.org>
2021-09-15 19:31:25 +0000

    Add Projector App externs js and message types.
    Bug: b/193579885
    Change-Id: I3c2f3722df84d082037cbc2a3401a6cec54a9d85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161378
    Commit-Queue: Li Lin <llin@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921793}

---
+++ chromeos/components/projector_app/resources/communication/message_types.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * Message definitions passed over the Projector app privileged/unprivileged
+ * pipe.
+ */
+
+/**
+ * Enum for tool types supported in the Ink engine.
+ *
+ * See http://go/ink-tools for details on each tool.
+ *
+ * @enum {string}
+ */
+/* #export */ const AnnotatorToolType = {
+  MARKER: 'marker',
+  PEN: 'pen',
+  HIGHLIGHTER: 'highlighter',
+  ERASER: 'eraser',
+};
