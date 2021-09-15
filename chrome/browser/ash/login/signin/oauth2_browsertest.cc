646efdd36dd4d02b1b2c9988fc90008165b93d17
Matthias Körber <koerber@google.com>
2021-09-15 15:29:32 +0000

    Revert "Disable OAuth2Test.MergeSession on Linux"
    This reverts commit 5d2a755d0ba7d3ccb9231c0cc3b73daaa15366cc.
    
    Reason for revert: Messed up header
    
    Original change's description:
    > Disable OAuth2Test.MergeSession on Linux
    >
    > Bug: 1249863
    > Change-Id: I1f6ee72fc9355ac956300404c8d04b799be3da8c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162162
    > Auto-Submit: Matthias Körber <koerber@google.com>
    > Commit-Queue: Matthias Körber <koerber@google.com>
    > Reviewed-by: Christoph Schwering <schwering@google.com>
    > Owners-Override: Matthias Körber <koerber@google.com>
    > Cr-Commit-Position: refs/heads/main@{#921669}
    
    Bug: 1249863
    Change-Id: Ic8d78e432b0e0483b5320292b3213a180c947db6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162600
    Auto-Submit: Matthias Körber <koerber@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#921671}

--- chrome/browser/ash/login/signin/oauth2_browsertest.cc
+++ chrome/browser/ash/login/signin/oauth2_browsertest.cc
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include <map>
@@ -16,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/strings/stringprintf.h"
 #include "base/synchronization/waitable_event.h"
-#include "build/build_config.h"
 #include "chrome/browser/ash/login/existing_user_controller.h"
 #include "chrome/browser/ash/login/signin/oauth2_login_manager.h"
 #include "chrome/browser/ash/login/signin/oauth2_login_manager_factory.h"
@@ -566,13 +566,7 @@ IN_PROC_BROWSER_TEST_F(OAuth2Test, PRE_MergeSession) {
 // MergeSession test is attempting to merge session for an existing profile
 // that was generated in PRE_PRE_MergeSession test. This attempt should fail
 // since FakeGaia instance isn't configured to return relevant tokens/cookies.
-// TODO(crbug.com/1249863): Test is flaky on Linux
-#if defined(OS_LINUX)
-#define MAYBE_MergeSession DISABLED_MergeSession
-#else
-#define MAYBE_MergeSession MergeSession
-#endif
-IN_PROC_BROWSER_TEST_F(OAuth2Test, MAYBE_MergeSession) {
+IN_PROC_BROWSER_TEST_F(OAuth2Test, MergeSession) {
   SimulateNetworkOnline();
 
   EXPECT_EQ(1, LoginScreenTestApi::GetUsersCount());
