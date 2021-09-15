a133ca68c8a15e08ae7a8805c5d3f050ee169b82
Gyuyoung Kim <gyuyoung@igalia.com>
2021-09-15 02:19:01 +0000

    MPArch: Ensure that Prerendering doesn't record lite video decision
    Most uses of IsInMainFrame have been converted to IsInPrimaryMainFrame
    for MPArch. As one of ensuring that the conversion is correct, this
    CL ensures prerendering does not record the lite video decision
    in LiteVideoDecider object through
    "LiteVideo.CanApplyLiteVideo.UserBlocklist.MainFrame" historgram.
    
    To do that, this CL adds an early return to
    LiteVideoDecider::CanApplyLiteVideo method to avoid recording the
    metrics. Additionally, this CL restores IsInMainFrame with
    IsInPrimaryMainFrame because it was an incorrect conversion.
    
    Bug: 1218946
    Change-Id: Ia9e8216c4cb80086b2b6aaed4a8b4adfb730efde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138169
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#921503}

--- chrome/browser/lite_video/lite_video_decider.cc
+++ chrome/browser/lite_video/lite_video_decider.cc
@@ -164,16 +164,19 @@ void LiteVideoDecider::CanApplyLiteVideo(
     return;
   }
 
-  // TODO(https://crbug.com/1218946): With MPArch there may be multiple main
-  // frames. This caller was converted automatically to the primary main frame
-  // to preserve its semantics. Follow up to confirm correctness.
-  const bool is_in_primary_main_frame =
-      navigation_handle->IsInPrimaryMainFrame();
+  if (navigation_handle->IsInPrerenderedMainFrame()) {
+    std::move(callback).Run(
+        absl::nullopt, blocklist_reason,
+        optimization_guide::OptimizationGuideDecision::kFalse);
+    return;
+  }
+
+  const bool is_in_main_frame = navigation_handle->IsInMainFrame();
 
   if (url.has_host() && IsHostPermanentlyBlockedlisted(url.host())) {
     blocklist_reason = LiteVideoBlocklistReason::kHostPermanentlyBlocklisted;
-    ScopedLiteVideoDecisionRecorder scoped_decision_recorder(
-        blocklist_reason, is_in_primary_main_frame);
+    ScopedLiteVideoDecisionRecorder scoped_decision_recorder(blocklist_reason,
+                                                             is_in_main_frame);
     std::move(callback).Run(
         absl::nullopt, blocklist_reason,
         optimization_guide::OptimizationGuideDecision::kFalse);
@@ -191,8 +194,8 @@ void LiteVideoDecider::CanApplyLiteVideo(
     blocklist_reason = is_reload
                            ? LiteVideoBlocklistReason::kNavigationReload
                            : LiteVideoBlocklistReason::kNavigationForwardBack;
-    ScopedLiteVideoDecisionRecorder scoped_decision_recorder(
-        blocklist_reason, is_in_primary_main_frame);
+    ScopedLiteVideoDecisionRecorder scoped_decision_recorder(blocklist_reason,
+                                                             is_in_main_frame);
     std::move(callback).Run(
         absl::nullopt, blocklist_reason,
         optimization_guide::OptimizationGuideDecision::kFalse);
@@ -204,7 +207,7 @@ void LiteVideoDecider::CanApplyLiteVideo(
 
   if (opt_guide_decider_) {
     // This relies on the optimization guide for hints.
-    if (is_in_primary_main_frame) {
+    if (is_in_main_frame) {
       opt_guide_decider_->CanApplyOptimizationAsync(
           navigation_handle, optimization_guide::proto::LITE_VIDEO,
           base::BindOnce(&LiteVideoDecider::OnOptimizationGuideHintAvailable,
@@ -236,8 +239,8 @@ void LiteVideoDecider::CanApplyLiteVideo(
 
     UpdateBlocklists(navigation_handle, blocklist_reason);
 
-    ScopedLiteVideoDecisionRecorder scoped_decision_recorder(
-        blocklist_reason, is_in_primary_main_frame);
+    ScopedLiteVideoDecisionRecorder scoped_decision_recorder(blocklist_reason,
+                                                             is_in_main_frame);
     if (hint)
       scoped_decision_recorder.set_has_hint_for_host(true);
 
@@ -247,8 +250,8 @@ void LiteVideoDecider::CanApplyLiteVideo(
 
   absl::optional<LiteVideoHint> hint =
       hint_cache_->GetHintForNavigationURL(url);
-  ScopedLiteVideoDecisionRecorder scoped_decision_recorder(
-      blocklist_reason, is_in_primary_main_frame);
+  ScopedLiteVideoDecisionRecorder scoped_decision_recorder(blocklist_reason,
+                                                           is_in_main_frame);
 
   if (hint)
     scoped_decision_recorder.set_has_hint_for_host(true);
