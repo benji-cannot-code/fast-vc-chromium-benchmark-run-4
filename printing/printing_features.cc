dc41bec86e8fecbabe4e84d06f2b96a498b2f4f1
Roberto Carrillo <robertocn@chromium.org>
2021-09-15 00:04:41 +0000

    Revert "Use build flag for OOP printing"
    This reverts commit d8170454da4cdaab0a8ced84476fcd3857ccaf36.
    
    Reason for revert: Tree-closing compile failure https://ci.chromium.org/ui/p/chromium/builders/ci/android-archive-dbg/9592/overview
    
    Original change's description:
    > Use build flag for OOP printing
    >
    > As out-of-process printing support gets filled in there is an increasing
    > number of places which require compile-time conditioning for the feature
    > implementation.  It is more concise and clear if these code blocks are
    > enclosed by a build flag, rather than to continue repeating the
    > condition based upon the list of Windows, macOS, Linux, and ChromeOS
    > platforms which this feature is currently supported on.
    >
    > Bug: 809738
    > Change-Id: I5b9bda291c3daf15b44b9e4ab699cda7eafcf99f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154758
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Commit-Queue: Alan Screen <awscreen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#921439}
    
    Bug: 809738
    Change-Id: I4852747b6f856955ce59d58c9ab8acca3738bad3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161249
    Auto-Submit: Roberto Carrillo <robertocn@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>
    Owners-Override: Roberto Carrillo <robertocn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921466}

--- printing/printing_features.cc
+++ printing/printing_features.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/printing_features.h"
 
 #include "build/chromeos_buildflags.h"
-#include "printing/buildflags/buildflags.h"
 
 #if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
     defined(OS_CHROMEOS)
@@ -55,7 +54,8 @@ bool ShouldPrintUsingXps(bool source_is_pdf) {
 }
 #endif  // defined(OS_WIN)
 
-#if BUILDFLAG(ENABLE_OOP_PRINTING)
+#if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
+    defined(OS_CHROMEOS)
 // Enables printing interactions with the operating system to be performed
 // out-of-process.
 const base::Feature kEnableOopPrintDrivers{"EnableOopPrintDrivers",
@@ -63,7 +63,8 @@ const base::Feature kEnableOopPrintDrivers{"EnableOopPrintDrivers",
 
 const base::FeatureParam<bool> kEnableOopPrintDriversJobPrint{
     &kEnableOopPrintDrivers, "JobPrint", false};
-#endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
+#endif  // defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) ||
+        // defined(OS_CHROMEOS)
 
 }  // namespace features
 }  // namespace printing
--- printing/printing_features.h
+++ printing/printing_features.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "printing/buildflags/buildflags.h"
 
 namespace printing {
 namespace features {
@@ -43,12 +42,14 @@ COMPONENT_EXPORT(PRINTING_BASE)
 bool ShouldPrintUsingXps(bool source_is_pdf);
 #endif  // defined(OS_WIN)
 
-#if BUILDFLAG(ENABLE_OOP_PRINTING)
+#if defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) || \
+    defined(OS_CHROMEOS)
 COMPONENT_EXPORT(PRINTING_BASE)
 extern const base::Feature kEnableOopPrintDrivers;
 COMPONENT_EXPORT(PRINTING_BASE)
 extern const base::FeatureParam<bool> kEnableOopPrintDriversJobPrint;
-#endif  // BUILDFLAG(ENABLE_OOP_PRINTING)
+#endif  // defined(OS_WIN) || defined(OS_MAC) || defined(OS_LINUX) ||
+        // defined(OS_CHROMEOS)
 
 }  // namespace features
 }  // namespace printing
