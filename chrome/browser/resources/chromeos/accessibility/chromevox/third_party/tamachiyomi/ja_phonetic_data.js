ab302a81ad6a399681b7ce4210156dc6900be20e
Chiaki Ushimaru <uchiaki@google.com>
2021-09-15 09:40:43 +0000

    ChromeVox: Change Object to Map
    This CL is to change Objects to Maps so that it clearly indicates that
    they are used as maps.
    Specifically, JaPhoneticMap.MAP and JA_TEST_MAP have been changed.
    
    Bug: 1195393
    Test: ChromeVoxPhoneticDataTest
    Change-Id: I887e37923a7410c31e51a51b9dbd8a914b722c63
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3156312
    Commit-Queue: Chiaki Ushimaru <uchiaki@google.com>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921571}

--- chrome/browser/resources/chromeos/accessibility/chromevox/third_party/tamachiyomi/ja_phonetic_data.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/third_party/tamachiyomi/ja_phonetic_data.js
@@ -13,12 +13,12 @@ JaPhoneticData = class {
 
   /**
    * Initialize phoneticMap_ by |map|.
-   * @param {Object<string, string>} map
+   * @param {Map<string, string>} map
    */
   static init(map) {
     /**
-     * An object containing phonetic disambiguation data for Japanese.
-     * @private {Object<string, string>}
+     * A map containing phonetic disambiguation data for Japanese.
+     * @private {Map<string, string>}
      */
     this.phoneticMap_ = map;
   }
@@ -34,7 +34,7 @@ JaPhoneticData = class {
       const prefix = JaPhoneticData.getDefaultPrefix(characterSet);
       return prefix + ' ' + JaPhoneticData.maybeGetLargeLetterKana(char);
     }
-    return JaPhoneticData.phoneticMap_[char] || char;
+    return JaPhoneticData.phoneticMap_.get(char) || char;
   }
 
   /**
@@ -63,7 +63,7 @@ JaPhoneticData = class {
           }
         }
       } else {
-        result.push(JaPhoneticData.phoneticMap_[char] || char);
+        result.push(JaPhoneticData.phoneticMap_.get(char) || char);
       }
 
       lastCharacterSet = currentCharacterSet;
