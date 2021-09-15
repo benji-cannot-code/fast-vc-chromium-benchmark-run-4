ff0d5ce0e52cd3816914705bdba096173f64f292
yilkal <yilkal@chromium.org>
2021-09-15 21:44:31 +0000

    [Projector] Support Duplex Comm for Annotator
    This cl introduces the following:
    1. WebUIListenerBehavior for trusted annotator
    to listen to calls from the browser process.
    2. UntrustedAnnotatorClient and TrustedAnnotatorRequestHandler
    in the trusted annotator content.
    3. TrustedAnnotatorClient and UntrustedAnnotatorRequestHandler
    in the untrusted annotator content.
    
    Bug: b/196245932
    Change-Id: I9d6c8f470b47f64db13ebe41be466c7c8cdabcec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3153685
    Reviewed-by: Li Lin <llin@chromium.org>
    Reviewed-by: Dan S <danan@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921876}

--- chromeos/components/projector_app/resources/annotator/annotator_embedder_impl.js
+++ chromeos/components/projector_app/resources/annotator/annotator_embedder_impl.js
@@ -5,19 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {WebUIListenerBehavior} from 'chrome://resources/js/web_ui_listener_behavior.m.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {ProjectorBrowserProxyImpl} from '../../communication/projector_browser_proxy.js';
+
+import {AnnotatorTrustedCommFactory, UntrustedAnnotatorClient} from './trusted/trusted_annotator_comm_factory.js';
 
 Polymer({
   is: 'annotator-embedder-impl',
 
   behaviors: [WebUIListenerBehavior],
 
-  properties: {},
-
-  // TODO(b/194915295): Add WebUIListener methods here.
-
   /** @override */
   ready() {
-    // TODO(b/194915295): Register WebUIListener methods here.
-  },
+    let client = AnnotatorTrustedCommFactory.getPostMessageAPIClient();
 
+    this.addWebUIListener('undo', () => {
+      client.undo();
+    });
+
+    this.addWebUIListener('redo', () => {
+      client.redo();
+    });
+
+    this.addWebUIListener('clear', () => {
+      client.clear();
+    });
+
+    this.addWebUIListener('setTool', async (tool) => {
+      const success = await client.setTool(tool);
+      if (success) {
+        ProjectorBrowserProxyImpl.getInstance().onToolSet(tool);
+      }
+    });
+  },
 });
