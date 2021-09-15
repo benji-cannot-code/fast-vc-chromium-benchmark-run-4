64608c391baf5288e572275ff6e2e3c4e94187ec
Ari Chivukula <arichiv@chromium.org>
2021-09-15 22:16:19 +0000

    Revert "[Origin -> StorageKey] third_party/blink/public/mojom/dom_storage/dom_storage.mojom"
    This reverts commit a379c4f577fede7e6e0c42f87ba73b157b3b63de.
    
    Reason for revert: 1249777, 1249758
    
    Original change's description:
    > [Origin -> StorageKey] third_party/blink/public/mojom/dom_storage/dom_storage.mojom
    >
    > This is part of a larger effort to migrate dom_storage to use
    > storage_key in preparation for storage partitioning.
    > Design Doc: https://docs.google.com/document/d/1goqlnZic0GZEen3byAbL2I57JnjhX0AQetjIKSQ6Lb0/edit
    >
    > Bug: 1212808
    > Change-Id: Iaa5e9a0e021fa216864b729c2b112e390338792b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3122751
    > Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    > Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#921218}
    
    Bug: 1212808
    Change-Id: Idea2546ef8ae5022d1ed42bff055c6e38a9797e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163109
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921890}

--- content/browser/storage_partition_impl.h
+++ content/browser/storage_partition_impl.h
@@ -43,20 +43,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/network/public/mojom/cookie_manager.mojom.h"
 #include "services/network/public/mojom/network_context.mojom.h"
-#include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/mojom/dom_storage/dom_storage.mojom.h"
 
 namespace leveldb_proto {
 class ProtoDatabaseProvider;
-}  // namespace leveldb_proto
+}
 
 namespace net {
 class IsolationInfo;
-}  // namespace net
-
-namespace url {
-class Origin;
-}  // namespace url
+}
 
 namespace content {
 
@@ -234,16 +229,14 @@ class CONTENT_EXPORT StoragePartitionImpl
 
   // blink::mojom::DomStorage interface.
   void OpenLocalStorage(
-      const url::Origin& local_frame_origin,
-      const blink::LocalFrameToken& local_frame_token,
+      const url::Origin& origin,
       mojo::PendingReceiver<blink::mojom::StorageArea> receiver) override;
   void BindSessionStorageNamespace(
       const std::string& namespace_id,
       mojo::PendingReceiver<blink::mojom::SessionStorageNamespace> receiver)
       override;
   void BindSessionStorageArea(
-      const url::Origin& local_frame_origin,
-      const blink::LocalFrameToken& local_frame_token,
+      const url::Origin& origin,
       const std::string& namespace_id,
       mojo::PendingReceiver<blink::mojom::StorageArea> receiver) override;
 
