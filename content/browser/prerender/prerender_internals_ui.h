1f0fb7683c40690afd1bbaa556a7609d265080df
Yohei Maeda <yoheimaeda@google.com>
2021-09-15 23:47:26 +0000

    Prerender: Create an internal page for prerendering
    This CL introduces a placeholder internal page for the experimental
    prerendering feature (chrome://prerender-internals). Contents of
    the page will be implemented by subsequent CLs.
    
    Bug: 1246787
    Change-Id: I67b0da669c2eec896dff5e41436d95712f7d7ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135301
    Commit-Queue: Yohei Maeda <yoheimaeda@google.com>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921921}

---
+++ content/browser/prerender/prerender_internals_ui.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_PRERENDER_PRERENDER_INTERNALS_UI_H_
+#define CONTENT_BROWSER_PRERENDER_PRERENDER_INTERNALS_UI_H_
+
+#include "content/public/browser/web_ui_controller.h"
+
+namespace content {
+
+// The WebUI for chrome://prerender-internals.
+class PrerenderInternalsUI : public WebUIController {
+ public:
+  explicit PrerenderInternalsUI(WebUI* web_ui);
+  PrerenderInternalsUI(const PrerenderInternalsUI&) = delete;
+  PrerenderInternalsUI& operator=(const PrerenderInternalsUI&) = delete;
+  ~PrerenderInternalsUI() override;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_PRERENDER_PRERENDER_INTERNALS_UI_H_
--- content/browser/webui/content_web_ui_controller_factory.cc
+++ content/browser/webui/content_web_ui_controller_factory.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/media/media_internals_ui.h"
 #include "content/browser/metrics/histograms_internals_ui.h"
 #include "content/browser/net/network_errors_listing_ui.h"
+#include "content/browser/prerender/prerender_internals_ui.h"
 #include "content/browser/process_internals/process_internals_ui.h"
 #include "content/browser/service_worker/service_worker_internals_ui.h"
 #include "content/browser/tracing/tracing_ui.h"
@@ -44,6 +45,7 @@ WebUI::TypeID ContentWebUIControllerFactory::GetWebUIType(
       url.host_piece() == kChromeUIServiceWorkerInternalsHost ||
       url.host_piece() == kChromeUIAppCacheInternalsHost ||
       url.host_piece() == kChromeUINetworkErrorsListingHost ||
+      url.host_piece() == kChromeUIPrerenderInternalsHost ||
       url.host_piece() == kChromeUIProcessInternalsHost ||
       url.host_piece() == kChromeUIConversionInternalsHost ||
       url.host_piece() == kChromeUIUkmHost) {
@@ -82,6 +84,8 @@ ContentWebUIControllerFactory::CreateWebUIControllerForURL(WebUI* web_ui,
 #endif
   if (url.host_piece() == kChromeUIWebRTCInternalsHost)
     return std::make_unique<WebRTCInternalsUI>(web_ui);
+  if (url.host_piece() == kChromeUIPrerenderInternalsHost)
+    return std::make_unique<PrerenderInternalsUI>(web_ui);
   if (url.host_piece() == kChromeUIProcessInternalsHost)
     return std::make_unique<ProcessInternalsUI>(web_ui);
   if (url.host_piece() == kChromeUIConversionInternalsHost)
