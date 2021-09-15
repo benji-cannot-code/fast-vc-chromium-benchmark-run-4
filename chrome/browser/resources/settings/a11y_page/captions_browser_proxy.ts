7df2cd4eece46682ba1d8f1d36302553d1ad97b4
dpapad <dpapad@chromium.org>
2021-09-15 18:21:40 +0000

    Settings TypeScript: Migrate a11y_page/ folder.
    Bug: 1234307
    Change-Id: I7870bfc6698a94ff9d3e86169f29a5bf6202feca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3157389
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921751}

--- chrome/browser/resources/settings/a11y_page/captions_browser_proxy.ts
+++ chrome/browser/resources/settings/a11y_page/captions_browser_proxy.ts
@@ -8,26 +8,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * interact with the browser. Used on operating system that is not Chrome OS.
  */
 
-// clang-format off
-import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
-// clang-format on
-
-/** @interface */
-export class CaptionsBrowserProxy {
+export interface CaptionsBrowserProxy {
   /**
    * Open the native captions system dialog.
    */
-  openSystemCaptionsDialog() {}
+  openSystemCaptionsDialog(): void;
 }
 
-/**
- * @implements {CaptionsBrowserProxy}
- */
-export class CaptionsBrowserProxyImpl {
-  /** @override */
+export class CaptionsBrowserProxyImpl implements CaptionsBrowserProxy {
   openSystemCaptionsDialog() {
     chrome.send('openSystemCaptionsDialog');
   }
+
+  static getInstance(): CaptionsBrowserProxy {
+    return instance || (instance = new CaptionsBrowserProxyImpl());
+  }
+
+  static setInstance(obj: CaptionsBrowserProxy) {
+    instance = obj;
+  }
 }
 
-addSingletonGetter(CaptionsBrowserProxyImpl);
+let instance: CaptionsBrowserProxy|null = null;
