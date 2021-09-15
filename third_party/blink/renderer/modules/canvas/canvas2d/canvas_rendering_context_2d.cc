cdcb25057083ab75428814d66c448c33b8a53a0f
Justin Novosad <junov@chromium.org>
2021-09-15 23:01:44 +0000

    Complete overhaul of 2D canvas overdraw optimizations.
    Context
    =======
    
    The overdraw optimization discards all previously recorded
    draw calls without rasterizing them when a new draw call is made that
    overwrites the entire canvas. This optimization adds measurable overhead
    to all draw calls in order to determine whether they occlude the canvas.
    In an attempt to reduce overhead, we recently tried removing the
    overdraw optimization, which led to performance regressions, so we put
    it back.
    See: https://chromium-review.googlesource.com/c/chromium/src/+/3114397
    The revert also caused numerous performance regression, thus proving
    that removing the optimization is not a bad idea.  This CL aims to reach
    a compromise that benefits all use cases.
    
    Objectives
    ==========
    
    * Reduce the amount of overhead associated with the overdraw
      optimization, without modifying existing behavior.
    * Gather data to support decisions to modify existing behavior in order
      to further reduce overhead in the future, without significantly
      compromising the effectiveness of overdraw optimizations.
    
    Metrics
    =======
    
    * Blink.UseCounter.Feature has a new bucket named
      CanvasRenderingContext2DHasOverdraw which counts page visits that
      activate the overdraw optimization.
    * New histogram Blink.Canvas.OverdrawOp which counts the types and
      characteristics of draw operations that trigger the overdraw
      optimization.
    
    Testing
    =======
    
    * Fixed missing test coverage for setFilter.
    * Fixed missing test coverage for path rendering (triggers overdraw when
      'copy' composite op is used).
    * Modified all overdraw tests to make them verify histogram logging.
    * Tests refactored: Even though the behavior of the overdraw
      optimization was not changed, the tests had to be refactored to
      validate histogram logging. To hit two birds with one stone, the
      architecture of the tests was improved at the same time.  The old
      macro-based implementation was hard to debug. The new implementation
      is not as compact (compactness was the reason for the original macro
      approach). The tests now use the traditional test fixture approach,
      which makes it a lot easier to investigate test failures.
    
    Performance Optimizations
    =========================
    
    * The CheckOverdraw method now has the draw op type as a template
      argument.  This allows static branches (resolved at compile time) to
      skip logic that is not relevant to the current draw operation.
    * Branch prediction hints in CheckOverdraw.
    * CheckOverdraw is now always inlined.  This makes sense now that the
      function is a template, since each template instance has a unique
      call site. Also the generated code is much smaller now due to all
      the static branches.
    * CheckOverdraw now has a static early exit at the top which ensures
      that the call is completely optimized out of draw ops that are not
      candidates for overdraw detection. This means that the overhead is
      reduced to zero for path and text rendering.
    
    Results
    =======
    
    Javascript execution time reduced by ~3% for code that calls
    rect() and fill() in a tight loop. Measured on an 2016 MacBook Pro.
    
    Bug: 1244575
    
    Change-Id: Idaf15d986deea9f11dfd1b1a9daa96d047548ea7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137620
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921906}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -986,12 +986,13 @@ void CanvasRenderingContext2D::fillFormattedText(
   sk_sp<PaintRecord> recording = formatted_text->PaintFormattedText(
       canvas()->GetDocument(), GetState().GetFontDescription(), x, y,
       wrap_width, bounds);
-  Draw([recording](cc::PaintCanvas* c, const PaintFlags* flags)  // draw lambda
-       { c->drawPicture(recording); },
-       [](const SkIRect& rect) { return false; }, bounds,
-       CanvasRenderingContext2DState::PaintType::kFillPaintType,
-       CanvasRenderingContext2DState::kNoImage,
-       CanvasPerformanceMonitor::DrawType::kText);
+  Draw<OverdrawOp::kNone>(
+      [recording](cc::PaintCanvas* c, const PaintFlags* flags)  // draw lambda
+      { c->drawPicture(recording); },
+      [](const SkIRect& rect) { return false; }, bounds,
+      CanvasRenderingContext2DState::PaintType::kFillPaintType,
+      CanvasRenderingContext2DState::kNoImage,
+      CanvasPerformanceMonitor::DrawType::kText);
 }
 
 void CanvasRenderingContext2D::DrawTextInternal(
@@ -1092,7 +1093,7 @@ void CanvasRenderingContext2D::DrawTextInternal(
     location.SetX(location.X() / clampTo<float>(width / font_width));
   }
 
-  Draw(
+  Draw<OverdrawOp::kNone>(
       [this, text = std::move(text), direction, bidi_override, location](
           cc::PaintCanvas* c, const PaintFlags* flags)  // draw lambda
       {
--- third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
@@ -718,7 +718,7 @@ void OffscreenCanvasRenderingContext2D::DrawTextInternal(
     location = FloatPoint();
   }
 
-  Draw(
+  Draw<OverdrawOp::kNone>(
       [this, text = std::move(text), direction, location](
           cc::PaintCanvas* paint_canvas,
           const PaintFlags* flags) /* draw lambda */ {
