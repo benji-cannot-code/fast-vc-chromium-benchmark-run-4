8342b304e5dfef8802918680ee597be1423d8a20
Alan Ding <alanding@google.com>
2021-09-15 21:52:16 +0000

    [ARC Nearby Share] "Preparing file" UI string for sharing single file
    Per CrOS UX requirement from UI Strings office hours, we need to show
    different strings "Preparing file" vs. "Preparing files" for sharing
    single file vs. multiple files respectively.
    
    BUG=b:199949712,b:195465354,b:184720732
    TEST=Verified sharing single vs. multiple files.
    
    Change-Id: I51e54646ee96ff7497f1d32cbdd97fdd71d792e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161254
    Commit-Queue: Alan Ding <alanding@chromium.org>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921879}

--- chrome/browser/ash/arc/nearby_share/ui/progress_bar_dialog_view.h
+++ chrome/browser/ash/arc/nearby_share/ui/progress_bar_dialog_view.h
@@ -21,7 +21,7 @@ namespace arc {
 
 class ProgressBarDialogView : public views::BoxLayoutView {
  public:
-  ProgressBarDialogView();
+  explicit ProgressBarDialogView(bool is_multiple_files);
   ProgressBarDialogView(const ProgressBarDialogView&) = delete;
   ProgressBarDialogView& operator=(const ProgressBarDialogView&) = delete;
   ~ProgressBarDialogView() override;
@@ -48,6 +48,9 @@ class ProgressBarDialogView : public views::BoxLayoutView {
 
   // Message label for the progress bar.
   views::Label* message_label_ = nullptr;
+
+  // Indicates whether multiple files are being shared for UI string.
+  const bool is_multiple_files_;
 };
 
 }  // namespace arc
