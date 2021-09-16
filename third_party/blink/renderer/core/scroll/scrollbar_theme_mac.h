ebf1a1ac9f3c639c721e88b56ab9f3e231b284f3
Jayson Adams <shrike@chromium.org>
2021-09-16 20:13:04 +0000

    [ScrollbarThemeMac] Cache Mac scrollbar metrics
    In my performance work, for some sites the renderer could spend nearly
    4% of main thread time calling ScrollbarThemeMac::ScrollbarThickness()
    to retrieve the scrollbar thickness. Most of this time involved
    dynamically looking up the NSScrollerImp class and creating an instance
    (and later tearing it down). This cl caches scroller thicknesses to
    avoid the repeated lookup and instantiation costs.
    
    Bug: 1245426
    
    Test: out/Release/blink_unittests --gtest_filter=ScrollbarThemeMacTest*
    
    Change-Id: I7fe445434416aca501f6aa13d942a884b916cdac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3146374
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922278}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_mac.h
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_mac.h
@@ -38,7 +38,7 @@ namespace blink {
 
 class Pattern;
 
-class PLATFORM_EXPORT ScrollbarThemeMac : public ScrollbarTheme {
+class CORE_EXPORT ScrollbarThemeMac : public ScrollbarTheme {
  public:
   ScrollbarThemeMac();
   ~ScrollbarThemeMac() override;
