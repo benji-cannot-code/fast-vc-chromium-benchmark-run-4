408e8f38b37c341c2255be4378a4aa9b69be5d9a
Gavin Williams <gavinwill@chromium.org>
2021-09-16 18:05:27 +0000

    scanning: Record multi-page action toolbar actions
    Bug: 1210271
    Change-Id: Ic7be2f713d48735e702c443fed262e30db4b0395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159195
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922212}

--- chrome/browser/ash/scanning/scan_service_unittest.cc
+++ chrome/browser/ash/scanning/scan_service_unittest.cc
@@ -974,6 +974,9 @@ TEST_F(ScanServiceTest, MultiPageScanRemoveWithTwoPages) {
   histogram_tester.ExpectUniqueSample("Scanning.NumPagesScanned", 1, 1);
   histogram_tester.ExpectUniqueSample("Scanning.MultiPageScan.NumPagesScanned",
                                       1, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Scanning.MultiPageScan.ToolbarAction",
+      scanning::ScanMultiPageToolbarAction::kRemovePage, 1);
 }
 
 // Test that a page can be removed from a multi-page scan with three scanned
@@ -1025,6 +1028,9 @@ TEST_F(ScanServiceTest, MultiPageScanRemoveWithThreePages) {
   histogram_tester.ExpectUniqueSample("Scanning.NumPagesScanned", 2, 1);
   histogram_tester.ExpectUniqueSample("Scanning.MultiPageScan.NumPagesScanned",
                                       2, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Scanning.MultiPageScan.ToolbarAction",
+      scanning::ScanMultiPageToolbarAction::kRemovePage, 1);
 }
 
 // Test that if there's only one page available, the page is removed and the
@@ -1067,6 +1073,9 @@ TEST_F(ScanServiceTest, MultiPageScanRemoveLastPage) {
   histogram_tester.ExpectUniqueSample("Scanning.NumPagesScanned", 1, 1);
   histogram_tester.ExpectUniqueSample("Scanning.MultiPageScan.NumPagesScanned",
                                       1, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Scanning.MultiPageScan.ToolbarAction",
+      scanning::ScanMultiPageToolbarAction::kRemovePage, 1);
 }
 
 // Test that a page can be rescanned and replaced from a multi-page scan with
@@ -1110,6 +1119,9 @@ TEST_F(ScanServiceTest, MultiPageScanRescanWithOnePage) {
   histogram_tester.ExpectUniqueSample("Scanning.NumPagesScanned", 1, 1);
   histogram_tester.ExpectUniqueSample("Scanning.MultiPageScan.NumPagesScanned",
                                       1, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Scanning.MultiPageScan.ToolbarAction",
+      scanning::ScanMultiPageToolbarAction::kRescanPage, 1);
 }
 
 // Test that a page can be rescanned and replaced from a multi-page scan with
@@ -1167,6 +1179,9 @@ TEST_F(ScanServiceTest, MultiPageScanRescanWithThreePages) {
   histogram_tester.ExpectUniqueSample("Scanning.NumPagesScanned", 3, 1);
   histogram_tester.ExpectUniqueSample("Scanning.MultiPageScan.NumPagesScanned",
                                       3, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Scanning.MultiPageScan.ToolbarAction",
+      scanning::ScanMultiPageToolbarAction::kRescanPage, 1);
 }
 
 }  // namespace ash
