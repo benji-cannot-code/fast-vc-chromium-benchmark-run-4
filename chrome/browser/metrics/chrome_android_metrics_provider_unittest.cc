9d8d931d18a2d68a61aa9633b6fb778ec4104340
Peter E Conn <peconn@chromium.org>
2021-09-16 12:53:34 +0000

    🌓 Record Dark Mode state with metrics on Android.
    Bug: 1248375
    Change-Id: I19d05fa94df4bc96b5a1b28724d9c53db3e9633a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3149470
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922101}

--- chrome/browser/metrics/chrome_android_metrics_provider_unittest.cc
+++ chrome/browser/metrics/chrome_android_metrics_provider_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/metrics_proto/chrome_user_metrics_extension.pb.h"
 
 class ChromeAndroidMetricsProviderTest : public testing::Test {
  public:
@@ -30,12 +31,23 @@ TEST_F(ChromeAndroidMetricsProviderTest, OnDidCreateMetricsLog_CustomTabs) {
 
 TEST_F(ChromeAndroidMetricsProviderTest,
        ProvideCurrentSessionData_MultiWindowMode) {
-  metrics_provider_.ProvideCurrentSessionData(nullptr /* uma_proto */);
+  metrics::ChromeUserMetricsExtension uma_proto;
+  metrics_provider_.ProvideCurrentSessionData(&uma_proto);
   histogram_tester_.ExpectTotalCount("Android.MultiWindowMode.Active", 1);
 }
 
 TEST_F(ChromeAndroidMetricsProviderTest,
        ProvideCurrentSessionData_AppNotifications) {
-  metrics_provider_.ProvideCurrentSessionData(nullptr /* uma_proto */);
+  metrics::ChromeUserMetricsExtension uma_proto;
+  metrics_provider_.ProvideCurrentSessionData(&uma_proto);
   histogram_tester_.ExpectTotalCount("Android.AppNotificationStatus", 1);
+}
+
+TEST_F(ChromeAndroidMetricsProviderTest,
+       ProvideCurrentSessionData_DarkModeState) {
+  metrics::ChromeUserMetricsExtension uma_proto;
+  ASSERT_FALSE(uma_proto.system_profile().os().has_dark_mode_state());
+
+  metrics_provider_.ProvideCurrentSessionData(&uma_proto);
+  ASSERT_TRUE(uma_proto.system_profile().os().has_dark_mode_state());
 }
