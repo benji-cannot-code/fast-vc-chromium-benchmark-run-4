8480d93cc2384f3ce88fb1591363b927ebce3edb
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-09-16 20:01:41 +0000

    Import wpt@c9a9e8d2ef4ad99a34d573244f82c3be82b36eb6
    Using wpt-import in Chromium 52484573fd2991e5f43ee05a9351023c623ebf08.
    With Chromium commits locally applied on WPT:
    15251d1ae5 "[WPT] Move/merge COEP/COOP dispatcher framework to /common"
    e399ab4be9 "[Re-Factor Client Hints] (5) Remove (unlaunched) `lang` client hint"
    791d40eacd "App history: change navigation method return values"
    59641cef1f "Pre-paint: Avoid inlines that aren't present in a fragmentainer."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    cbiesinger@chromium.org:
      external/wpt/css/css-flexbox
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    
    NOAUTOREVERT=true
    
    No-Export: true
    Change-Id: I13b94fa9d87ab14a72619b90fa84e660a345d39d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3165336
    Reviewed-by: Kyle Ju <kyleju@chromium.org>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#922273}

--- third_party/blink/web_tests/external/wpt/wasm/jsapi/table/grow.any.js
+++ third_party/blink/web_tests/external/wpt/wasm/jsapi/table/grow.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window,dedicatedworker,jsshell
+// META: script=/wasm/jsapi/wasm-module-builder.js
 // META: script=assertions.js
 
 function nulls(n) {
