e7adc3e04d6b35006bbcd50fd8bd0ea7c72398bb
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-09-16 14:52:35 +0000

    [Traffic Annotations] Add Java support to extractor.py
    Make extractor.py able to tokenize/scan Java source files for traffic
    annotation definitions. Add a unit test with Java code examples.
    
    auditor.py still only scans .cc/.mm files, so this doesn't affect the
    Auditor's behavior yet.
    
    Bug: 1231780
    Change-Id: I9af4afa04fbbeaf5b5dd44f5cd9d3b9fd3a8f09b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139691
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922133}

--- tools/traffic_annotation/scripts/annotation_tokenizer.py
+++ tools/traffic_annotation/scripts/annotation_tokenizer.py
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 A tokenizer for traffic annotation definitions.
 """
 
-from collections import namedtuple
+from typing import NamedTuple, Optional
 
 import re
 
@@ -20,10 +20,19 @@ import re
 TOKEN_REGEXEN = [
     # Comma for separating args.
     ('comma', re.compile(r'(,)')),
-    # String literal. "string" or R"(string)".
-    ('string_literal',
-     re.compile(r'"((?:[^"]|\\.)*?)"|R"\((.*?)\)"', re.DOTALL)),
-    # C++ identifier.
+    # String literal. "string" or R"(string)". In Java, this will incorrectly
+    # accept R-strings, which aren't part of the language's syntax. But since
+    # that wouldn't compile anyways, we can just ignore this issue.
+    ('string_literal', re.compile(r'"((?:[^"]|\\.)*?)"|R"\((.*?)\)"',
+                                  re.DOTALL)),
+    # The '+' operator, for string concatenation. Java doesn't have multi-line
+    # string literals, so this is the only way to keep long strings readable. It
+    # doesn't incur a runtime cost, since the Java compiler is smart enough to
+    # concat the string literals at compile time. See "constant expressions" in
+    # the JLS:
+    # https://docs.oracle.com/javase/specs/jls/se8/html/jls-15.html#jls-15.28
+    ('plus', re.compile(r'(\+)')),
+    # C++ or Java identifier.
     ('symbol', re.compile(r'([a-zA-Z_][a-zA-Z_0-9]*)')),
     # Left parenthesis.
     ('left_paren', re.compile(r'(\()')),
@@ -34,11 +43,15 @@ TOKEN_REGEXEN = [
 # Number of characters to include in the context (for error reporting).
 CONTEXT_LENGTH = 20
 
-Token = namedtuple('Token', ['type', 'value', 'pos'])
+
+class Token(NamedTuple):
+  type: str
+  value: str
+  pos: int
 
 
-class CppParsingError(Exception):
-  """An error during C++ parsing/tokenizing."""
+class SourceCodeParsingError(Exception):
+  """An error during C++ or Java parsing/tokenizing."""
 
   def __init__(self, expected_type, body, pos, file_path, line_number):
     context = body[pos:pos + CONTEXT_LENGTH]
@@ -66,8 +79,8 @@ class Tokenizer:
       return
     # Skip whitespace to make the error message more useful.
     pos = self._skip_whitespace()
-    raise CppParsingError(expected_type, self.body, pos, self.file_path,
-                          self.line_number)
+    raise SourceCodeParsingError(expected_type, self.body, pos, self.file_path,
+                                 self.line_number)
 
   def _skip_whitespace(self):
     """Return the position of the first non-whitespace character from here."""
@@ -91,7 +104,7 @@ class Tokenizer:
 
     return token
 
-  def maybe_advance(self, expected_type):
+  def maybe_advance(self, expected_type: str) -> Optional[str]:
     """Advance the tokenizer by one token if it has |expected_type|.
 
     Args:
@@ -107,7 +120,7 @@ class Tokenizer:
       return token.value
     return None
 
-  def advance(self, expected_type):
+  def advance(self, expected_type: str) -> str:
     """Advance the tokenizer by one token, asserting its type.
 
     Throws an error if the token at point has the wrong type.
