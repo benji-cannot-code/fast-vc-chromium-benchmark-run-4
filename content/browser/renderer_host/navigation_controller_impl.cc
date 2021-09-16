a348f783dfc0b1bbe83390e0453ce5d019d6defc
Harkiran Bolaria <hbolaria@google.com>
2021-09-16 19:03:26 +0000

    [MPArch] Replace RFHI::FTN::IsMainFrame with RFHI::is_main_frame
    All instances where RenderFrameHostImpl::frame_tree_node_ is used
    to call IsMainFrame() should be replaced with calls to the
    is_main_frame() method of RenderFrameHostImpl directly.
    
    As part of the MPArch project, eventually the  frame_tree_ and
    frame_tree_node_ class members will be fully removed from
    RenderFrameHostImpl.
    
    Bug: 1179502
    Change-Id: Icf96f68dfb0800a82653f2500052380fb021865e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3165197
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Cr-Commit-Position: refs/heads/main@{#922248}

--- content/browser/renderer_host/navigation_controller_impl.cc
+++ content/browser/renderer_host/navigation_controller_impl.cc
@@ -2458,7 +2458,7 @@ void NavigationControllerImpl::NavigateFromFrameProxy(
   // directly, passing them to the destination RenderFrameHost.  See
   // https://crbug.com/536906.
   std::unique_ptr<NavigationEntryImpl> entry;
-  if (!node->IsMainFrame()) {
+  if (!render_frame_host->is_main_frame()) {
     // Subframe case: create FrameNavigationEntry.
     if (GetLastCommittedEntry()) {
       entry = GetLastCommittedEntry()->Clone();
