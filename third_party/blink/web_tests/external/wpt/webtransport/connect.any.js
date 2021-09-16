8ec9d24164ee1f3bb9b372a588d9d7ede993e47a
Yutaka Hirano <yhirano@chromium.org>
2021-09-16 06:15:42 +0000

    Use WebTransportError for errors during session establishment
    This is for https://github.com/w3c/webtransport/issues/344.
    Test coverage is not great, but we are adding more web platform tests.
    
    Bug: 1011392
    Change-Id: Ie84fce58936931bf14ba3e8c9d096cdf73e12d3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162458
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922011}

--- third_party/blink/web_tests/external/wpt/webtransport/connect.any.js
+++ third_party/blink/web_tests/external/wpt/webtransport/connect.any.js
@@ -6,30 +6,45 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 promise_test(async t => {
   const wt = new WebTransport(webtransport_url('custom-response.py?:status=200'));
   await wt.ready;
-}, 'WebTransport connection succeeds with status code 200');
+}, 'WebTransport session is established with status code 200');
 
 promise_test(async t => {
   const wt = new WebTransport(webtransport_url('custom-response.py?:status=204'));
   await wt.ready;
-}, 'WebTransport connection succeeds with status code 204');
+}, 'WebTransport session is established with status code 204');
 
 promise_test(async t => {
   const wt = new WebTransport(webtransport_url('custom-response.py?:status=301'));
-  await promise_rejects_js(t, TypeError, wt.ready, 'ready promise shoud be rejected');
-  await promise_rejects_js(t, TypeError, wt.closed, 'closed promise should be rejected');
-}, 'WebTransport connection fails with status code 301');
+  // Sadly we cannot use promise_rejects_dom as the error constructor is
+  // WebTransportError rather than DOMException. Ditto below.
+  // We get a possible error, and then make sure wt.closed is rejected with it.
+  const e = await wt.ready.catch(e => e);
+  await promise_rejects_exactly(t, e, wt.closed, 'closed promise should be rejected');
+  await promise_rejects_exactly(t, e, wt.ready, 'ready promise shoud be rejected');
+  assert_true(e instanceof WebTransportError);
+  assert_equals(e.source, 'session', 'source');
+  assert_equals(e.applicationProtocolCode, null, 'applicationProtocolCode');
+}, 'WebTransport session establishment fails with status code 301');
 
 promise_test(async t => {
   const  wt = new WebTransport(webtransport_url('custom-response.py?:status=401'));
-  await promise_rejects_js(t, TypeError, wt.ready, 'ready promise should be rejected');
-  await promise_rejects_js(t, TypeError, wt.closed, 'closed promise should be rejected');
-}, 'WebTransport connection fails with status code 401');
+  const e = await wt.ready.catch(e => e);
+  await promise_rejects_exactly(t, e, wt.closed, 'closed promise should be rejected');
+  await promise_rejects_exactly(t, e, wt.ready, 'ready promise shoud be rejected');
+  assert_true(e instanceof WebTransportError);
+  assert_equals(e.source, 'session', 'source');
+  assert_equals(e.applicationProtocolCode, null, 'applicationProtocolCode');
+}, 'WebTransport session establishment with status code 401');
 
 promise_test(async t => {
   const wt = new WebTransport(webtransport_url('custom-response.py?:status=404'));
-  await promise_rejects_js(t, TypeError, wt.ready, 'ready promise should be rejected');
-  await promise_rejects_js(t, TypeError, wt.closed, 'closed promise should be rejected');
-}, 'WebTransport connection fails with status code 404');
+  const e = await wt.ready.catch(e => e);
+  await promise_rejects_exactly(t, e, wt.closed, 'closed promise should be rejected');
+  await promise_rejects_exactly(t, e, wt.ready, 'ready promise shoud be rejected');
+  assert_true(e instanceof WebTransportError);
+  assert_equals(e.source, 'session', 'source');
+  assert_equals(e.applicationProtocolCode, null, 'applicationProtocolCode');
+}, 'WebTransport session establishment fails with status code 404');
 
 promise_test(async t => {
   // Create WebTransport session.
