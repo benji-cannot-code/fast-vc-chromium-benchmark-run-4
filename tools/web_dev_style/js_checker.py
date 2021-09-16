0bdd88445b73e29b5d9efbdd300660a79b02a9b2
dpapad <dpapad@chromium.org>
2021-09-16 11:35:33 +0000

    WebUI: Apply arrow function style check only to inline functions.
    Previously the following was causing a PRESUBMIT failure
    this.addWebUIListener('foo-changed', this.onFooChanged_.bind(this));
    
    Modifying the check to only complain for inline functions as follows:
    this.addWebUIListener('foo-changed', (function(foo) {...}).bind(this));
    
    The previous check resulted in having to change the code to a more
    verbose version only to satisfy the PRESUBMIT. For example the code
    would be changed to:
    
    this.addWebUIListener('foo-changed', foo => this.onFooChanged_(foo));
    
    Especially with the use of TS the latter is more verbose (need to
    re-declare function parameter types) and does not seem to add much
    value.
    
    Bug: None
    Change-Id: Idac82e2de9d4ab8ab2bdd04ed90ba259357588bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3160537
    Auto-Submit: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922085}

--- tools/web_dev_style/js_checker.py
+++ tools/web_dev_style/js_checker.py
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 See chrome/browser/PRESUBMIT.py
 """
 
-from . import regex_check
+import regex_check
 
 
 class JSChecker(object):
@@ -22,7 +22,8 @@ class JSChecker(object):
         self.input_api.re, line_number, line, regex, message)
 
   def BindThisCheck(self, i, line):
-    return self.RegexCheck(i, line, r"(\.bind\(this)[^)]*\)",
+    """Checks for usages of bind(this) with inlined functions."""
+    return self.RegexCheck(i, line, r"\)(\.bind\(this)[^)]*\)",
                            "Prefer arrow (=>) functions over bind(this)")
 
   def ChromeSendCheck(self, i, line):
