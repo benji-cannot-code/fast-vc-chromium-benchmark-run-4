549fb196aefaab92507204be6cce390323f9094d
dpapad <dpapad@chromium.org>
2021-09-16 12:59:27 +0000

    Settings: Fix CrSettingsSafetyCheckChromeCleanerTest.
    This test was relying on the existence of the ChromeCleanupProxy class
    at runtime, but after converting the prod code to TypeScript
    ChromeCleanupProxy is just an interface and is stripped from runtime.
    
    Fixing by not using ChromeCleanupProxy at runtime anymore.
    
    Bug: 1241370
    Change-Id: I20160aa76c6b16969f16292770a29fc536c2867e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3164189
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rainhard <rainhard@chromium.org>
    Commit-Queue: Rainhard <rainhard@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922102}

--- chrome/test/data/webui/settings/safety_check_chrome_cleaner_test.js
+++ chrome/test/data/webui/settings/safety_check_chrome_cleaner_test.js
@@ -7,12 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {ChromeCleanupProxy, ChromeCleanupProxyImpl} from 'chrome://settings/lazy_load.js';
+import {ChromeCleanupProxyImpl} from 'chrome://settings/lazy_load.js';
 import {MetricsBrowserProxyImpl, Router, routes, SafetyCheckCallbackConstants, SafetyCheckChromeCleanerStatus, SafetyCheckIconStatus, SafetyCheckInteractions, SettingsSafetyCheckChromeCleanerChildElement} from 'chrome://settings/settings.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 import {TestBrowserProxy} from '../test_browser_proxy.js';
 
+import {TestChromeCleanupProxy} from './test_chrome_cleanup_proxy.js';
 import {TestMetricsBrowserProxy} from './test_metrics_browser_proxy.js';
 
 // clang-format on
@@ -97,9 +98,7 @@ suite('SafetyCheckChromeCleanerUiTests', function() {
   let page;
 
   setup(function() {
-    chromeCleanupBrowserProxy = TestBrowserProxy.fromClass(ChromeCleanupProxy);
-    chromeCleanupBrowserProxy.setResultFor(
-        'restartComputer', Promise.resolve(0));
+    chromeCleanupBrowserProxy = new TestChromeCleanupProxy();
     ChromeCleanupProxyImpl.setInstance(chromeCleanupBrowserProxy);
 
     metricsBrowserProxy = new TestMetricsBrowserProxy();
