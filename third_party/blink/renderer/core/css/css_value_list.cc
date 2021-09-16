4875f75f6854fc6715fae6e04bb51b348e86ddf7
Keishi Hattori <keishi@chromium.org>
2021-09-16 14:45:18 +0000

    Show error for Vector<Member> and Vector<WeakMember>
    Show errors for Vector<Member> and Vector<WeakMember> by adding a static_assert
    
    Change-Id: I5958fc12324aa621740ac9d2ef915ecdccd121f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159772
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922132}

--- third_party/blink/renderer/core/css/css_value_list.cc
+++ third_party/blink/renderer/core/css/css_value_list.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 struct SameSizeAsCSSValueList : CSSValue {
-  Vector<Member<CSSValue>, 4> list_values;
+  HeapVector<Member<CSSValue>, 4> list_values;
 };
 ASSERT_SIZE(CSSValueList, SameSizeAsCSSValueList);
 
