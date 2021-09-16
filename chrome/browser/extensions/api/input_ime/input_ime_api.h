b6639e6e20acb73fb8fb7b36b9e3c3f6da836ee3
Darren Shen <shend@chromium.org>
2021-09-16 04:31:48 +0000

    ime: Ensure all pending key events are resolved when removing listener.
    There is a bug where if the extension removes the key event listener,
    then the pending key event will never get resolved, neither by the
    extension nor Chromium.
    
    Listen for the listener removed event and resolve all pending key
    events when that happens.
    
    Bug: b:184007339
    Change-Id: Ib48cc2031f57d9e7f999e1519545b757d049a373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3080043
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921986}

--- chrome/browser/extensions/api/input_ime/input_ime_api.h
+++ chrome/browser/extensions/api/input_ime/input_ime_api.h
@@ -198,6 +198,7 @@ class InputImeAPI : public BrowserContextKeyedAPI,
 
   // EventRouter::Observer implementation.
   void OnListenerAdded(const EventListenerInfo& details) override;
+  void OnListenerRemoved(const EventListenerInfo& details) override;
 
  private:
   friend class BrowserContextKeyedAPIFactory<InputImeAPI>;
