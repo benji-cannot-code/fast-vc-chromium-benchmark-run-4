54d22eaa28de7c48f56789ac3f74b260b31a47aa
Yulun Wu <yulunwu@chromium.org>
2021-09-16 20:57:40 +0000

    Add search result view font via ash typography.
    Search result view uses Google Sans at different font sizes depending
    on the type of view being shown. This CL addresses that using
    ash_typography.
    
    Bug: 792785
    Change-Id: Ic2fa23d6bd4b641d7e4b232f39fc8a383bc1613e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159024
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922302}

--- ash/app_list/views/search_result_view.cc
+++ ash/app_list/views/search_result_view.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/image_model_utils.h"
+#include "ui/views/style/typography.h"
 
 namespace ash {
 
@@ -145,7 +146,9 @@ SearchResultView::SearchResultView(SearchResultListView* list_view,
   details_label_->SetDisplayedOnBackgroundColor(SK_ColorTRANSPARENT);
   details_label_->SetVisible(false);
 
-  separator_label_ = AddChildView(std::make_unique<views::Label>());
+  separator_label_ = AddChildView(std::make_unique<views::Label>(
+      l10n_util::GetStringUTF16(IDS_ASH_SEARCH_RESULT_SEPARATOR),
+      CONTEXT_SEARCH_RESULT_VIEW, STYLE_PRODUCTIVITY_LAUNCHER));
   separator_label_->SetBackgroundColor(SK_ColorTRANSPARENT);
   separator_label_->SetVisible(false);
   separator_label_->SetText(
@@ -214,6 +217,17 @@ void SearchResultView::UpdateDetailsText() {
 void SearchResultView::StyleTitleLabel() {
   title_label_->ClearStyleRanges();
   views::StyledLabel::RangeStyleInfo title_style;
+
+  switch (view_type_) {
+    case SearchResultViewType::kClassic:
+      title_label_->SetTextContext(CONTEXT_SEARCH_RESULT_VIEW);
+      title_label_->SetDefaultTextStyle(STYLE_CLASSIC_LAUNCHER);
+      break;
+    case SearchResultViewType::kInlineAnswer:
+    case SearchResultViewType::kDefault:
+      title_label_->SetTextContext(CONTEXT_SEARCH_RESULT_VIEW);
+      title_label_->SetDefaultTextStyle(STYLE_PRODUCTIVITY_LAUNCHER);
+  }
   title_style.override_color =
       AppListColorProvider::Get()->GetSearchBoxTextColor(
           kDeprecatedSearchBoxTextDefaultColor);
@@ -244,6 +258,20 @@ void SearchResultView::StyleTitleLabel() {
 void SearchResultView::StyleDetailsLabel() {
   details_label_->ClearStyleRanges();
   views::StyledLabel::RangeStyleInfo details_style;
+  switch (view_type_) {
+    case SearchResultViewType::kClassic:
+      details_label_->SetTextContext(CONTEXT_SEARCH_RESULT_VIEW);
+      details_label_->SetDefaultTextStyle(STYLE_CLASSIC_LAUNCHER);
+      break;
+    case SearchResultViewType::kInlineAnswer:
+      details_label_->SetTextContext(
+          CONTEXT_SEARCH_RESULT_VIEW_INLINE_ANSWER_DETAILS);
+      details_label_->SetDefaultTextStyle(STYLE_PRODUCTIVITY_LAUNCHER);
+      break;
+    case SearchResultViewType::kDefault:
+      details_label_->SetTextContext(CONTEXT_SEARCH_RESULT_VIEW);
+      details_label_->SetDefaultTextStyle(STYLE_PRODUCTIVITY_LAUNCHER);
+  }
   details_style.override_color =
       AppListColorProvider::Get()->GetSearchBoxSecondaryTextColor(
           kDeprecatedSearchBoxTextDefaultColor);
