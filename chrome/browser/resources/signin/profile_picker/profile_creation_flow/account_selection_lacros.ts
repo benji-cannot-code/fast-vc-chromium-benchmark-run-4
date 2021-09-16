e53f9ba82159a2fc6a9be59e72a61a871d892cb8
David Roger <droger@chromium.org>
2021-09-16 15:51:43 +0000

    [Lacros] Layout for the account selection screen
    Screenshot (normal case):
    https://drive.google.com/file/d/1swKcVLYN8mMuVL-BaHUBcPYTfhX98zCk/view?usp=sharing&resourcekey=0-GbpS2rzU_m7JzN_YxSwlVA
    
    Screenshot (edge cases with ellipsis and scrollbar):
    https://drive.google.com/file/d/1l87sNdp5Tl_S9fAnE4dZUF9kSaEV4V8g/view?usp=sharing&resourcekey=0-90OcDpXz7DjfDGfy9GjotA
    
    Notes:
    - the page can be scrolled if there are more accounts.
    - icons are missing and replaced by temporary placeholders
    
    Bug: 1226050
    Change-Id: I07f0b70ee0a22680625ffd15887d8eec34c50089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154175
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922151}

--- chrome/browser/resources/signin/profile_picker/profile_creation_flow/account_selection_lacros.ts
+++ chrome/browser/resources/signin/profile_picker/profile_creation_flow/account_selection_lacros.ts
@@ -26,6 +26,7 @@ interface RepeaterEvent extends CustomEvent {
 export interface AccountSelectionLacrosElement {
   $: {
     backButton: HTMLElement,
+    'product-logo': HTMLElement,
   };
 }
 
@@ -85,6 +86,17 @@ export class AccountSelectionLacrosElement extends
   private getBackButtonAriaLabel_(): string {
     return this.i18n(
         'backButtonAriaLabel', this.i18n('accountSelectionLacrosTitle'));
+  }
+
+  private onProductLogoTap_() {
+    this.$['product-logo'].animate(
+        {
+          transform: ['none', 'rotate(-10turn)'],
+        },
+        {
+          duration: 500,
+          easing: 'cubic-bezier(1, 0, 0, 1)',
+        });
   }
 
   private onAccountClick_(e: RepeaterEvent) {
