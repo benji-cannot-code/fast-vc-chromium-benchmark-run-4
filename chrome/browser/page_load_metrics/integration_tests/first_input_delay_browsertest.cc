131ec6e3ccddfbe76e5a0af58575402b0feca942
SebSG <sebsg@chromium.org>
2021-09-16 16:20:26 +0000

    [Sheriff] Disable MetricIntegrationTest.FirstInputDelay on CrOS.
    Test is flaky.
    
    Bug: 1250315
    Change-Id: Ie91bdc24cb09fa751e623f594022bbfbdc7fc479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3164817
    Commit-Queue: sebsg <sebsg@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Auto-Submit: sebsg <sebsg@chromium.org>
    Owners-Override: SebSG <sebsg@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#922163}

--- chrome/browser/page_load_metrics/integration_tests/first_input_delay_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/first_input_delay_browsertest.cc
@@ -13,7 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using ukm::builders::PageLoad;
 
-IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, FirstInputDelay) {
+#if defined(OS_CHROMEOS)
+#define MAYBE_FirstInputDelay DISABLED_FirstInputDelay
+#else
+#define MAYBE_FirstInputDelay FirstInputDelay
+#endif
+IN_PROC_BROWSER_TEST_F(MetricIntegrationTest, MAYBE_FirstInputDelay) {
   LoadHTML(R"HTML(
     <p>Sample website</p>
     <script>
