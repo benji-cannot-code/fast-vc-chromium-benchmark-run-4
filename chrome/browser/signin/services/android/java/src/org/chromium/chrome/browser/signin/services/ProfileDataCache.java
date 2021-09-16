34b711acdd8b7fdc67d847ca1229482243a0690c
Tanmoy Mollik <triploblastic@chromium.org>
2021-09-16 10:32:11 +0000

    [Signin][FRE] Add child account badge for supervised accounts
    This cl adds support for dynamically adding badge to user avatar in
    ProfileDataCache and uses it in the new FRE welcome screen for
    supervised accounts.
    
    Screenshot: https://crbug.com/1227314#c7
    Bug: 1227314
    Change-Id: I76446562d4fccdcf834ab7a116dccfa7826f0386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3133080
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922074}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCache.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCache.java
@@ -65,7 +65,6 @@ public class ProfileDataCache implements AccountInfoService.Observer {
         private final @Px int mBorderSize;
 
         private BadgeConfig(Context context, @DrawableRes int badgeResId) {
-            assert badgeResId != 0 : "badgeResId shouldn't be 0!";
             Resources resources = context.getResources();
             mBadge = AppCompatResources.getDrawable(context, badgeResId);
             mBadgeSize = resources.getDimensionPixelSize(R.dimen.badge_size);
@@ -95,7 +94,7 @@ public class ProfileDataCache implements AccountInfoService.Observer {
 
     private final Context mContext;
     private final int mImageSize;
-    private @Nullable final BadgeConfig mBadgeConfig;
+    private @Nullable BadgeConfig mBadgeConfig;
     private final Drawable mPlaceholderImage;
     private final ObserverList<Observer> mObservers = new ObserverList<>();
     private final Map<String, DisplayableProfileData> mCachedProfileData = new HashMap<>();
@@ -122,8 +121,7 @@ public class ProfileDataCache implements AccountInfoService.Observer {
 
     /**
      * @param context Context of the application to extract resources from.
-     * @param badgeResId Resource id of the badge to be attached. If it is 0 then no badge is
-     *         attached.
+     * @param badgeResId Resource id of the badge to be attached.
      * @return A {@link ProfileDataCache} object with default image size(R.dimen.user_picture_size)
      *         and a badge of given badgeResId provided
      */
@@ -156,6 +154,18 @@ public class ProfileDataCache implements AccountInfoService.Observer {
             return new DisplayableProfileData(accountEmail, mPlaceholderImage, null, null);
         }
         return profileData;
+    }
+
+    /**
+     * Sets a {@link BadgeConfig} and then populates the cache with the new Badge.
+     * @param badgeResId Resource id of the badge to be attached. If 0 then the current Badge is
+     * removed.
+     */
+    public void setBadge(@DrawableRes int badgeResId) {
+        if (badgeResId == 0 && mBadgeConfig == null) return;
+        mBadgeConfig = badgeResId == 0 ? null : new BadgeConfig(mContext, badgeResId);
+        mCachedProfileData.clear();
+        AccountInfoServiceProvider.getPromise().then(this::populateCache);
     }
 
     /**
