9cefdb8b62d8bf40c1e9d3a4395f9aa5ca70ae6f
Omar Morsi <omorsi@google.com>
2021-09-16 13:02:16 +0000

    [Sheriff] Disable flaky DownloadExtensionTests
    The tests have been flaky when run by Win7 (32) Tests builder [*]. Other
    tests in this class have also been flaky and disabled by previous
    sheriffs.
    
    [*] https://ci.chromium.org/p/chromium/builders/ci/Win7%20(32)%20Tests
    
    Bug: 1249757
    Change-Id: Ib697aaecdde19a81e42526c984311671524a256a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162206
    Owners-Override: Omar Morsi <omorsi@google.com>
    Auto-Submit: Omar Morsi <omorsi@google.com>
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922103}

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -2426,8 +2426,16 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
 // chrome fails to propagate them back to the server.  This tests both that
 // testserver.py does not succeed when it should fail as well as how the
 // downloads extension api exposes the failure to extensions.
+// TODO(https://crbug.com/1249757): DownloadExtensionTest's are flaky
+#if defined(OS_WIN)
+#define MAYBE_DownloadExtensionTest_Download_Headers_Fail \
+  DISABLED_DownloadExtensionTest_Download_Headers_Fail
+#else
+#define MAYBE_DownloadExtensionTest_Download_Headers_Fail \
+  DownloadExtensionTest_Download_Headers_Fail
+#endif
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
-                       DownloadExtensionTest_Download_Headers_Fail) {
+                       MAYBE_DownloadExtensionTest_Download_Headers_Fail) {
   LoadExtension("downloads_split");
   ASSERT_TRUE(StartEmbeddedTestServer());
   std::string download_url =
@@ -2575,8 +2583,16 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
 // server. This tests both that testserver.py does not succeed when it should
 // fail, and this tests how the downloads extension api exposes the failure to
 // extensions.
+// TODO(https://crbug.com/1249757): DownloadExtensionTest's are flaky
+#if defined(OS_WIN)
+#define MAYBE_DownloadExtensionTest_Download_Post_Get \
+  DISABLED_DownloadExtensionTest_Download_Post_Get
+#else
+#define MAYBE_DownloadExtensionTest_Download_Post_Get \
+  DownloadExtensionTest_Download_Post_Get
+#endif
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
-                       DownloadExtensionTest_Download_Post_Get) {
+                       MAYBE_DownloadExtensionTest_Download_Post_Get) {
   LoadExtension("downloads_split");
   ASSERT_TRUE(StartEmbeddedTestServer());
   std::string download_url = embedded_test_server()
@@ -2616,8 +2632,16 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
 // server. This tests both that testserver.py does not succeed when it should
 // fail, and this tests how the downloads extension api exposes the failure to
 // extensions.
+// TODO(https://crbug.com/1249757): DownloadExtensionTest's are flaky
+#if defined(OS_WIN)
+#define MAYBE_DownloadExtensionTest_Download_Post_NoBody \
+  DISABLED_DownloadExtensionTest_Download_Post_NoBody
+#else
+#define MAYBE_DownloadExtensionTest_Download_Post_NoBody \
+  DownloadExtensionTest_Download_Post_NoBody
+#endif
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
-                       DownloadExtensionTest_Download_Post_NoBody) {
+                       MAYBE_DownloadExtensionTest_Download_Post_NoBody) {
   LoadExtension("downloads_split");
   ASSERT_TRUE(StartEmbeddedTestServer());
   std::string download_url = embedded_test_server()
