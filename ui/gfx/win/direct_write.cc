d3eb8c171fe0149e027e0d9d7e1d5c6c69acf71c
Etienne Bergeron <etienneb@chromium.org>
2021-09-16 16:22:36 +0000

    Add UMA metric for DWrite startup initialization time
    This CL is adding a UMA metric to measure the impact in the long
    tail (high percentiles) for DWrite initialisation. We observed that
    for slow-startup, DWrite initialisation is a often the cause of 30% of
    the execution time.
    
    R=gab@chromium.org
    
    Bug: 1249873
    Change-Id: If9bf9920874c52a8362316f85baeced457d67457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163019
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922165}

--- ui/gfx/win/direct_write.cc
+++ ui/gfx/win/direct_write.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/alias.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/trace_event.h"
 #include "base/win/windows_version.h"
@@ -55,6 +56,7 @@ void InitializeDirectWrite() {
   tried_dwrite_initialize = true;
 
   TRACE_EVENT0("fonts", "gfx::InitializeDirectWrite");
+  SCOPED_UMA_HISTOGRAM_LONG_TIMER("DirectWrite.Fonts.Gfx.InitializeTime");
 
   Microsoft::WRL::ComPtr<IDWriteFactory> factory;
   CreateDWriteFactory(&factory);
