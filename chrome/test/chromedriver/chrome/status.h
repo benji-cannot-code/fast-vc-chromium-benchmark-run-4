0422953a6d336b92eccd6994f6dff6eb5db7b71c
Jesus David Garcia Gomez <jegarc@microsoft.com>
2021-09-16 19:47:12 +0000

    Implement shadow root endpoints
    This CL implements the following shadow root commands:
    
    - Get Element Shadow Root: https://w3c.github.io/webdriver/#get-element-shadow-root
    - Find Element From Shadow Root: https://w3c.github.io/webdriver/#find-element-from-shadow-root
    - Find Elements From Shadow Root: https://w3c.github.io/webdriver/#find-elements-from-shadow-root
    
    This CL was reverted because wpt tools needs to be updated before merge it, so the
    webdriver tests are updated and doesn't break anything.
    
    Bug: chromedriver:3715
    Change-Id: I7c3b4d89b0eaf7c65979ff483c1544c8c3076db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119807
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Jesus David Garcia Gomez <jegarc@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#922260}

--- chrome/test/chromedriver/chrome/status.h
+++ chrome/test/chromedriver/chrome/status.h
@@ -35,6 +35,8 @@ enum StatusCode {
   kInvalidArgument = 61,
   kNoSuchCookie = 62,
   kElementClickIntercepted = 64,
+  kNoSuchShadowRoot = 65,
+  kDetachedShadowRoot = 66,
   kUnsupportedOperation = 405,
   // Chrome-specific status codes.
   kChromeNotReachable = 100,
