ce9aa568a4816241ebfab4ea65c4ee5b532eeab8
Joel Hockey <joelhockey@chromium.org>
2021-09-16 05:29:25 +0000

    Return window type app for an app browser in chrome.windows API
    In crrev.com/c/2189893 chrome.windows.create({type: 'popup'}) was
    changed to create a browser of TYPE_APP_POPUP, rather than TYPE_APP, so
    we can now safely return both 'app' and 'popup' as a window type in
    chrome.windows API.
    
    This change will allow SSH extension (nassh) to detect when it is
    being run inside the CrOS Terminal System App browser.
    
     chrome.window.getCurrent(w => w.type === 'app')
    
    Bug: 990158,1241262
    Change-Id: I20ec6536957c494f7c6825f4671d1257ab9fd5ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159768
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922001}

---
+++ chrome/test/data/extensions/api_test/windows/create/test.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function typeNormal() {
+    chrome.windows.create({'type': 'normal'}, chrome.test.callbackPass(w => {
+      chrome.test.assertEq('normal', w.type);
+    }));
+  },
+  function typePopup() {
+    chrome.windows.create({'type': 'popup'}, chrome.test.callbackPass(w => {
+      chrome.test.assertEq('popup', w.type);
+    }));
+  },
+]);
