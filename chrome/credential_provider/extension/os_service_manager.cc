09fa639fc9bd3d1ac3851df83972cb534d73497b
Yusuf Sengul <yusufsn@google.com>
2021-09-16 21:11:47 +0000

    Quote GCPW extension binary path
    Bug: 1250455
    Change-Id: Idd447187cecf4cb3801d0658ef7b4647b2f9c8ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3166098
    Reviewed-by: Robin Lewis <wrlewis@google.com>
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/main@{#922310}

--- chrome/credential_provider/extension/os_service_manager.cc
+++ chrome/credential_provider/extension/os_service_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/credential_provider/extension/os_service_manager.h"
 
+#include "base/command_line.h"
 #include "chrome/credential_provider/extension/extension_strings.h"
 #include "chrome/credential_provider/gaiacp/logging.h"
 
@@ -38,19 +39,22 @@ DWORD OSServiceManager::InstallService(
   if (!scm_handle.IsValid())
     return ::GetLastError();
 
+  base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
+  command_line.SetProgram(service_binary_path);
+
   *sc_handle = ScopedScHandle(::CreateService(
-      scm_handle.Get(),                     // SCM database
-      kGCPWExtensionServiceName,            // name of service
-      kGCPWExtensionServiceDisplayName,     // service name to display
-      SERVICE_ALL_ACCESS,                   // desired access
-      SERVICE_WIN32_OWN_PROCESS,            // service type
-      SERVICE_AUTO_START,                   // start type
-      SERVICE_ERROR_NORMAL,                 // error control type
-      service_binary_path.value().c_str(),  // path to service's binary
-      nullptr,                              // no load ordering group
-      nullptr,                              // no tag identifier
-      nullptr,                              // no dependencies
-      nullptr,                              // LocalSystem account
+      scm_handle.Get(),                             // SCM database
+      kGCPWExtensionServiceName,                    // name of service
+      kGCPWExtensionServiceDisplayName,             // service name to display
+      SERVICE_ALL_ACCESS,                           // desired access
+      SERVICE_WIN32_OWN_PROCESS,                    // service type
+      SERVICE_AUTO_START,                           // start type
+      SERVICE_ERROR_NORMAL,                         // error control type
+      command_line.GetCommandLineString().c_str(),  // path to service's binary
+      nullptr,                                      // no load ordering group
+      nullptr,                                      // no tag identifier
+      nullptr,                                      // no dependencies
+      nullptr,                                      // LocalSystem account
       nullptr));
 
   if (!sc_handle->IsValid())
