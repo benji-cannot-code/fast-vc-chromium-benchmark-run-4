963e8dee26776514b4d551ce2ef2312427d5ef8b
Maggie Cai <mxcai@google.com>
2021-09-16 05:01:51 +0000

    [IntentHandling] Create intent filters for shortcut apps.
    Currently the intent filter creation for web apps are depends on the
    scope() API for WebApp. However for shortcut apps, sometimes this field
    is not filled. Update to use the WebAppRegistrar::GetAppScope() API to
    generate the intent filters, until WebApp provide API to get the url
    scope.
    
    BUG=1249896
    
    Change-Id: I88239bce8e52c1352e285389d444c67d2ce78891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3164000
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#921993}

--- chrome/browser/apps/app_service/intent_util_unittest.cc
+++ chrome/browser/apps/app_service/intent_util_unittest.cc
@@ -169,17 +169,6 @@ TEST_F(IntentUtilsTest, CreateShareIntentFromText) {
             apps_util::CreateLaunchIntent("com.android.vending", intent));
 }
 
-TEST_F(IntentUtilsTest, CreateWebAppIntentFilters_ShortcutApp_NoUrlFilter) {
-  auto web_app = web_app::test::CreateWebApp();
-  // Ensure scope is empty, as in a "Create Shortcut" app.
-  web_app->SetScope(GURL());
-
-  std::vector<IntentFilterPtr> filters =
-      apps_util::CreateWebAppIntentFilters(*web_app.get());
-
-  EXPECT_EQ(filters.size(), 0);
-}
-
 TEST_F(IntentUtilsTest, CreateWebAppIntentFilters_WebApp_HasUrlFilter) {
   auto web_app = web_app::test::CreateWebApp();
   DCHECK(web_app->start_url().is_valid());
@@ -187,7 +176,7 @@ TEST_F(IntentUtilsTest, CreateWebAppIntentFilters_WebApp_HasUrlFilter) {
   web_app->SetScope(scope);
 
   std::vector<IntentFilterPtr> filters =
-      apps_util::CreateWebAppIntentFilters(*web_app.get());
+      apps_util::CreateWebAppIntentFilters(*web_app.get(), scope);
 
   ASSERT_EQ(filters.size(), 1);
   IntentFilterPtr& filter = filters[0];
@@ -256,7 +245,7 @@ TEST_F(IntentUtilsTest, CreateWebAppIntentFilters_FileHandlers) {
   web_app->SetFileHandlers(file_handlers);
 
   std::vector<IntentFilterPtr> filters =
-      apps_util::CreateWebAppIntentFilters(*web_app.get());
+      apps_util::CreateWebAppIntentFilters(*web_app.get(), scope);
 
   ASSERT_EQ(filters.size(), 2);
   // 1st filter is URL filter.
@@ -291,7 +280,7 @@ TEST_F(IntentUtilsTest, CreateWebAppIntentFilters_NoteTakingApp) {
   web_app->SetNoteTakingNewNoteUrl(new_note_url);
 
   std::vector<IntentFilterPtr> filters =
-      apps_util::CreateWebAppIntentFilters(*web_app.get());
+      apps_util::CreateWebAppIntentFilters(*web_app.get(), scope);
 
   ASSERT_EQ(filters.size(), 2);
 
