9a5922ba178e61428fb8356f0ce155eebc3fb513
Benoit Lize <lizeb@chromium.org>
2021-09-17 09:23:55 +0000

    [PartitionAlloc] Report syscall count and duration.
    On non official builds, count how many memory management system calls
    are made, and report their total duration. The aim of this is to
    diagnose cases where PartitionAlloc is making too many syscalls.
    
    For instance, on Linux, running Speedometer 2.0 makes >100k system
    calls, which is a lot, particularly on operating systems with slower
    memory management calls than Linux. This is important for PartitionAlloc
    beyond its malloc() use case.
    
    As the instrumentation is per-PartitionRoot, we do not instrument
    PageAllocator, but the points where calls to PageAllocator (or
    AddressPoolManager) are made.
    
    Bug: 1250288
    Change-Id: I79d9e505ac2c6941eec64a8953131178bff18042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3164538
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922474}

--- base/allocator/partition_allocator/partition_page.cc
+++ base/allocator/partition_allocator/partition_page.cc
@@ -74,6 +74,7 @@ ALWAYS_INLINE void PartitionDirectUnmap(
   // expected to be very rare though, and likely preferable to holding the lock
   // while releasing the address space.
   ScopedUnlockGuard<thread_safe> unlock{root->lock_};
+  ScopedSyscallTimer<thread_safe> timer{root};
   UnmapNow(reservation_start, reservation_size, root->ChoosePool());
 }
 
