ede4916c89a9be40ae204557b55fe945fabeecee
Dylan Cutler <dylancutler@google.com>
2021-09-17 00:28:34 +0000

    Make CookiePartitionKey constructor private.
    This way, any CookiePartitionKey instantiated with a value needs to have
    been created with FromNetworkIsolationKey or FromURLForTesting. This
    makes it easier for us to keep track of/control the logic of how
    CookiePartitionKeys are instantiated.
    
    This CL also adds a new factory, FromWire, meant for creating
    CookiePartitionKey instances from an existing partition key for Mojo.
    
    Bug: 1225444
    Change-Id: Ib8133e0a51fdf49c0cf95e8b28c5613931cd9d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163538
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922370}

--- net/cookies/cookie_partition_key_fuzzer.cc
+++ net/cookies/cookie_partition_key_fuzzer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fuzzer/FuzzedDataProvider.h>
 
 #include "net/cookies/cookie_partition_key.h"
+#include "url/origin.h"
 
 namespace net {
 
@@ -20,22 +21,19 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (!url.is_valid())
     return 0;
 
-  SchemefulSite site(url::Origin::Create(url));
   absl::optional<CookiePartitionKey> partition_key =
-      absl::make_optional(CookiePartitionKey(site));
+      absl::make_optional(CookiePartitionKey::FromURLForTesting(url));
 
-  bool result = CookiePartitionKey::Deserialize(url_str, partition_key);
-  if (site.opaque())
-    CHECK(!result);
-  else
-    CHECK(result);
-
+  bool is_opaque = url::Origin::Create(url).opaque();
   std::string tmp;
-  result = CookiePartitionKey::Serialize(partition_key, tmp);
-  if (site.opaque())
-    CHECK(!result);
-  else
-    CHECK(result);
+  CHECK_NE(is_opaque, CookiePartitionKey::Serialize(partition_key, tmp));
+
+  CHECK_NE(is_opaque, CookiePartitionKey::Deserialize(url_str, partition_key));
+
+  if (!is_opaque) {
+    CHECK(absl::make_optional(CookiePartitionKey::FromURLForTesting(url)) ==
+          partition_key);
+  }
 
   return 0;
 }
