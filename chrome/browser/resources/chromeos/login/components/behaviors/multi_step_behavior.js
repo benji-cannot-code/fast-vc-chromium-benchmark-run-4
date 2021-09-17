5633254f641a5d4fc7251d7cd4f8b71eaaed54d8
Renato Silva <rrsilva@google.com>
2021-09-17 15:25:20 +0000

    OOBE - Polymer3 - Migrate AD Password Change
    Migrate the ActiveDirectory password change screen to ES6 class
    syntax and add directives to generate its Polymer3 version.
    
    Also fix an issue with the variables names that was caught by
    the closure compiler.
    
    Bug: 1184731
    Change-Id: If3e1034ea762e122873db1a5b303ec2a8ef6d2c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3165137
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#922543}

--- chrome/browser/resources/chromeos/login/components/behaviors/multi_step_behavior.js
+++ chrome/browser/resources/chromeos/login/components/behaviors/multi_step_behavior.js
@@ -194,3 +194,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * }}
  */
 MultiStepBehavior.Proto;
+
+/** @interface */
+/* #export */ class MultiStepBehaviorInterface {
+  setUIStep(step) {}
+}
--- chrome/browser/resources/chromeos/login/structure/components_login.js
+++ chrome/browser/resources/chromeos/login/structure/components_login.js
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // <include src="../screens/login/gaia_password_changed.js">
 // <include src="../screens/login/management_transition.js">
-// <include src="../screens/login/active_directory_password_change.js">
 // <include src="../screens/login/encryption_migration.js">
 // <include src="../screens/login/update_required_card.js">
 // <include src="../screens/login/offline_login.js">
