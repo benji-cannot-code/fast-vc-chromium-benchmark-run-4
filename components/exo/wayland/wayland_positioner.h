3d903af2aee633d5c599afa22a7472912d59494b
jonahwilliams <jonahwilliams@google.com>
2021-09-17 01:01:53 +0000

    [exo] remove popup flip
    Client will already attempt to flip popup, which can currently result in double flipping.
    
    bug: crbug.com/1245976
    
    Change-Id: Ibbc5346a5d0f79e54cdd337462ee285f97ec4dc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163073
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Jonah Williams <jonahwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#922376}

--- components/exo/wayland/wayland_positioner.h
+++ components/exo/wayland/wayland_positioner.h
@@ -23,8 +23,6 @@ class WaylandPositioner {
   struct Result {
     gfx::Point origin;
     gfx::Size size;
-    bool x_flipped;
-    bool y_flipped;
   };
 
   // Represents the 1-dimensional projection of the gravity/anchor values.
@@ -37,9 +35,7 @@ class WaylandPositioner {
   WaylandPositioner(Version v) : version_(v) {}
 
   // Calculate and return bounds from current state.
-  Result CalculateBounds(const gfx::Rect& work_area,
-                         bool flip_x,
-                         bool flip_y) const;
+  Result CalculateBounds(const gfx::Rect& work_area) const;
 
   void SetSize(gfx::Size size) { size_ = std::move(size); }
 
