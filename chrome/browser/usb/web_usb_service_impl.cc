f9aaf0562b57319791cd117fb42d2e05818d8a87
Maksim Ivanov <emaxx@chromium.org>
2021-09-17 09:07:57 +0000

    Allowlist USB for Smart Card Connector extension
    Exempt the zero-party Smart Card Connector extension (the migrated
    version of the legacy Chrome App) from the restriction of talking to USB
    devices with the smart card device class. This allows the extension to
    function via the WebUSB API, meanwhile the legacy app was using the
    Apps-specific chrome.usb API that never had this restriction.
    
    Do the same for its Beta version, to simplify the QA testing before the
    future rollout.
    
    Note that this change doesn't actually grant the permission to connect
    to those devices, since that is controlled by the upper layer of the
    WebUSB implementation (e.g., the permission can be granted by the user).
    
    Bug: 1233881
    Change-Id: Ife495ac97728a99ea56cb2f6cbf636280d417315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3155802
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922472}

--- chrome/browser/usb/web_usb_service_impl.cc
+++ chrome/browser/usb/web_usb_service_impl.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/public/mojom/usb_device.mojom.h"
 #include "services/device/public/mojom/usb_enumeration_options.mojom.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "base/containers/fixed_flat_set.h"
 #include "extensions/common/constants.h"
 #endif
@@ -136,7 +136,7 @@ std::vector<uint8_t> WebUsbServiceImpl::GetProtectedInterfaceClasses() const {
 #if BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(IS_CHROMEOS_ASH)
   // These extensions can claim the protected HID interface class (example: used
   // as badge readers)
-  static constexpr auto kPrivilegedExtensionIds =
+  static constexpr auto kHidPrivilegedExtensionIds =
       base::MakeFixedFlatSet<base::StringPiece>({
           // Imprivata Extensions, see crbug.com/1065112 and crbug.com/995294.
           "baobpecgllpajfeojepgedjdlnlfffde",
@@ -176,10 +176,26 @@ std::vector<uint8_t> WebUsbServiceImpl::GetProtectedInterfaceClasses() const {
       });
 
   if (origin_.scheme() == extensions::kExtensionScheme &&
-      base::Contains(kPrivilegedExtensionIds, origin_.host())) {
+      base::Contains(kHidPrivilegedExtensionIds, origin_.host())) {
     base::Erase(classes, device::mojom::kUsbHidClass);
   }
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS) && BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  // These extensions can claim the smart card USB class
+  static constexpr auto kSmartCardPrivilegedExtensionIds =
+      base::MakeFixedFlatSet<base::StringPiece>({
+          // Smart Card Connector Extension and its Beta version, see
+          // crbug.com/1233881.
+          "khpfeaanjngmcnplbdlpegiifgpfgdco",
+          "mockcojkppdndnhgonljagclgpkjbkek",
+      });
+
+  if (origin_.scheme() == extensions::kExtensionScheme &&
+      base::Contains(kSmartCardPrivilegedExtensionIds, origin_.host())) {
+    base::Erase(classes, device::mojom::kUsbSmartCardClass);
+  }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
   return classes;
 }
