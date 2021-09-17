b6c5b253403938a8f0beb732d70546d37695807b
Eva <evih@google.com>
2021-09-17 15:09:30 +0000

    [Autofill] Use GetFormRendererId() and GetFieldRendererId()
    To improve readability, this change replaces the use cases of
    getting the form's/field's renderer id and creating a
    FormRendererId/FieldRendererId object from it by calling the
    GetFormRendererId()/GetFieldRendererId() function.
    
    This change preserves equivalence.
    
    Bug: 1007974
    Change-Id: I3c04d3b28576932e1d9e622d4abf6dca0e79416a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151881
    Commit-Queue: Eva Herencsárová <evih@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#922538}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1032,7 +1032,7 @@ std::vector<WebFormControlElement>::iterator SearchInSortedVector(
     const FormFieldData& field,
     std::vector<WebFormControlElement>& sorted_elements) {
   auto get_field_renderer_id = [](const WebFormControlElement& e) {
-    return FieldRendererId(e.UniqueRendererFormControlId());
+    return GetFieldRendererId(e);
   };
   // Find the first element whose unique renderer ID is greater or equal to
   // |fields|.
@@ -1040,8 +1040,7 @@ std::vector<WebFormControlElement>::iterator SearchInSortedVector(
       sorted_elements.begin(), sorted_elements.end(), field.unique_renderer_id,
       base::ranges::less{}, get_field_renderer_id);
   if (it == sorted_elements.end() ||
-      FieldRendererId(it->UniqueRendererFormControlId()) !=
-          field.unique_renderer_id) {
+      GetFieldRendererId(*it) != field.unique_renderer_id) {
     return sorted_elements.end();
   }
   return it;
@@ -1395,8 +1394,7 @@ void MatchLabelsAndFields(
       if (form_control.FormControlTypeForAutofill() == *kHidden)
         continue;
       // Typical case: look up |field_data| in |field_set|.
-      auto iter = field_set.find(
-          FieldRendererId(form_control.UniqueRendererFormControlId()));
+      auto iter = field_set.find(GetFieldRendererId(form_control));
       if (iter == field_set.end())
         continue;
       field_data = iter->first;
@@ -1961,8 +1959,7 @@ void WebFormControlElementToFormField(
   field->name = element.NameForAutofill().Utf16();
   field->id_attribute = element.GetIdAttribute().Utf16();
   field->name_attribute = element.GetAttribute(*kName).Utf16();
-  field->unique_renderer_id =
-      FieldRendererId(element.UniqueRendererFormControlId());
+  field->unique_renderer_id = GetFieldRendererId(element);
   field->host_form_id = form_renderer_id;
   field->form_control_ax_id = element.GetAxId();
   field->form_control_type = element.FormControlTypeForAutofill().Utf8();
@@ -2100,8 +2097,8 @@ void WebFormControlElementToFormField(
   if (field_data_manager &&
       field->properties_mask & (FieldPropertiesFlags::kUserTyped |
                                 FieldPropertiesFlags::kAutofilled)) {
-    const std::u16string user_input = field_data_manager->GetUserInput(
-        FieldRendererId(element.UniqueRendererFormControlId()));
+    const std::u16string user_input =
+        field_data_manager->GetUserInput(GetFieldRendererId(element));
 
     // The typed value is preserved for all passwords. It is also preserved for
     // potential usernames, as long as the |value| is not deemed acceptable.
@@ -2125,8 +2122,7 @@ bool WebFormElementToFormData(
     return false;
 
   form->name = GetFormIdentifier(form_element);
-  form->unique_renderer_id =
-      FormRendererId(form_element.UniqueRendererFormId());
+  form->unique_renderer_id = GetFormRendererId(form_element);
   form->action = GetCanonicalActionForForm(form_element);
   form->is_action_empty =
       form_element.Action().IsNull() || form_element.Action().IsEmpty();
@@ -2477,7 +2473,7 @@ bool InferLabelForElementForTesting(const WebFormControlElement& element,
 WebFormElement FindFormByUniqueRendererId(const WebDocument& doc,
                                           FormRendererId form_renderer_id) {
   for (const auto& form : doc.Forms()) {
-    if (FormRendererId(form.UniqueRendererFormId()) == form_renderer_id)
+    if (GetFormRendererId(form) == form_renderer_id)
       return form;
   }
   return WebFormElement();
@@ -2511,10 +2507,8 @@ WebFormControlElement FindFormControlElementByUniqueRendererId(
       if (!element.IsFormControlElement())
         continue;
       WebFormControlElement control = element.To<WebFormControlElement>();
-      if (queried_form_control ==
-          FieldRendererId(control.UniqueRendererFormControlId())) {
+      if (queried_form_control == GetFieldRendererId(control))
         return control;
-      }
     }
     return WebFormControlElement();
   }
@@ -2569,8 +2563,7 @@ std::vector<WebFormControlElement> FindFormControlElementsByUniqueRendererId(
       if (!element.IsFormControlElement())
         continue;
       WebFormControlElement control = element.To<WebFormControlElement>();
-      auto it = renderer_id_to_index_map.find(
-          FieldRendererId(control.UniqueRendererFormControlId()));
+      auto it = renderer_id_to_index_map.find(GetFieldRendererId(control));
       if (it == renderer_id_to_index_map.end())
         continue;
       result[it->second] = control;
@@ -2591,8 +2584,7 @@ std::vector<WebFormControlElement> FindFormControlElementsByUniqueRendererId(
   auto renderer_id_to_index_map = BuildRendererIdToIndex(queried_form_controls);
 
   for (const auto& field : form.GetFormControlElements()) {
-    auto it = renderer_id_to_index_map.find(
-        FieldRendererId(field.UniqueRendererFormControlId()));
+    auto it = renderer_id_to_index_map.find(GetFieldRendererId(field));
     if (it == renderer_id_to_index_map.end())
       continue;
     result[it->second] = field;
