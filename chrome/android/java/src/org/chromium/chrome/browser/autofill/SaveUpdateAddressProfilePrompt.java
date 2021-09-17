242c2443df6e4c1788b10406d339300991f5dedb
Min Qin <qinmin@chromium.org>
2021-09-17 08:49:25 +0000

    Allow negative button to be filled for modal dialog
    Sometimes negative button is a more "appropriate" action for user to
    take. For example, "cancel" is more appropriate for dangerous download
    dialog. Currently only primary button can be filled, this CL makes
    it possible to fill the negative button.
    
    BUG=1250127
    
    Change-Id: I823b39895bbc45e0683a11c376fd7cc09f5d18d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163887
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922468}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/SaveUpdateAddressProfilePrompt.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/SaveUpdateAddressProfilePrompt.java
@@ -67,7 +67,8 @@ public class SaveUpdateAddressProfilePrompt {
                         .with(ModalDialogProperties.CONTROLLER,
                                 new SimpleModalDialogController(
                                         modalDialogManager, this::onDismiss))
-                        .with(ModalDialogProperties.PRIMARY_BUTTON_FILLED, true)
+                        .with(ModalDialogProperties.BUTTON_STYLES,
+                                ModalDialogProperties.ButtonStyles.PRIMARY_FILLED_NEGATIVE_OUTLINE)
                         .with(ModalDialogProperties.CUSTOM_VIEW, mDialogView);
         mDialogModel = builder.build();
 
