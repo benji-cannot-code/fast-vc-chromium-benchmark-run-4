e95ef909b4f2a4d8c52ed198212c14d3d9a9236a
Rohit Agarwal <roagarwal@chromium.org>
2021-09-17 09:30:37 +0000

    [Sheriff] [Android] Disable Animations for PriceAlertsMessageCardTest.
    The test testSwipeMessage is flaky due to animations delay. Disabling
    the animations explicitly.
    
    Bug: 1201547
    Change-Id: Ia4e95d5f2e0c2bfbaa3c5a09629f7e9366d92745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168211
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Rohit Agarwal <roagarwal@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Owners-Override: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922477}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/PriceAlertsMessageCardTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/PriceAlertsMessageCardTest.java
@@ -43,6 +43,7 @@ import androidx.test.filters.MediumTest;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -64,6 +65,7 @@ import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.components.browser_ui.notifications.MockNotificationManagerProxy;
+import org.chromium.ui.test.util.DisableAnimationsTestRule;
 import org.chromium.ui.test.util.UiRestriction;
 
 import java.io.IOException;
@@ -84,6 +86,10 @@ public class PriceAlertsMessageCardTest {
             "android.settings.APP_NOTIFICATION_SETTINGS";
     private MockNotificationManagerProxy mMockNotificationManager;
     private PriceDropNotificationManager mPriceDropNotificationManager;
+
+    @ClassRule
+    public static DisableAnimationsTestRule sDisableAnimationsTestRule =
+            new DisableAnimationsTestRule();
 
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
