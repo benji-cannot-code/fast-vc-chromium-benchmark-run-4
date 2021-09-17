81cb1388797d964a471470d2b56c421093daf020
Ben Reich <benreich@chromium.org>
2021-09-17 05:09:46 +0000

    [FilesBanner] Update sharePathWithCrostini test for new banner framework
    The new banners framework ports the Shared with Crostini banner to the
    framework to ensure it is appropriately prioritised and only shown when
    no other banner is showing (it is lower priority but persistent). This
    ensures the browser test sharePathWithCrostini work with the flag
    enabled and disabled.
    
    Bug: 1228128
    Test: ui/file_managaer:closure_compile
    Test: browser_tests --gtest_filter=*FilesApp*
    Change-Id: I4a2f3865e8df622e152d96a2434b506134112f99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3164856
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922432}

--- ui/file_manager/integration_tests/file_manager/crostini.js
+++ ui/file_manager/integration_tests/file_manager/crostini.js
@@ -46,11 +46,20 @@ testcase.sharePathWithCrostini = async () => {
       '[command="#share-with-linux"]:not([hidden]):not([disabled])';
   const menuNoShareWithLinux = '#file-context-menu:not([hidden]) ' +
       '[command="#share-with-linux"][hidden][disabled="disabled"]';
-  const shareMessageShown = '#files-message:not([hidden])';
   const shareMessageHidden = '#files-message[hidden]';
+  let shareMessageShown = '#files-message:not([hidden])';
 
   const appId =
       await setupAndWaitUntilReady(RootPath.DOWNLOADS, [ENTRIES.photos], []);
+
+  const isBannersFrameworkEnabled =
+      await sendTestMessage({name: 'isBannersFrameworkEnabled'}) === 'true';
+  if (isBannersFrameworkEnabled) {
+    await remoteCall.isolateBannerForTesting(
+        appId, 'shared-with-crostini-pluginvm-banner');
+    shareMessageShown =
+        '#banners > shared-with-crostini-pluginvm-banner:not([hidden])';
+  }
 
   // Ensure fake Linux files root is shown.
   await remoteCall.waitForElement(appId, FAKE_LINUX_FILES);
@@ -79,7 +88,12 @@ testcase.sharePathWithCrostini = async () => {
   await remoteCall.waitForElement(appId, menuNoShareWithLinux);
 
   // Click 'photos' to go in photos directory, ensure share message is shown.
-  await remoteCall.waitForElement(appId, shareMessageHidden);
+  if (isBannersFrameworkEnabled) {
+    await remoteCall.waitForElementLost(
+        appId, '#banners > shared-with-crostini-pluginvm-banner');
+  } else {
+    await remoteCall.waitForElement(appId, shareMessageHidden);
+  }
   remoteCall.callRemoteTestUtil('fakeMouseDoubleClick', appId, [photos]);
   await remoteCall.waitForElement(appId, shareMessageShown);
 };
