7e7becdceeacea0285f4e2faf0ed5b395756efb1
Vignesh Shenvi <vshenvi@google.com>
2021-09-17 19:29:55 +0000

    Initialize missive client in Lacros
    This change initializes the missive client to enable client side reporting in Lacros.
    
    Bug: b:190862120
    Change-Id: I206fee5c432ff754e9b7416309e6857fe2dac0ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163670
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#922594}

--- chromeos/lacros/lacros_dbus_helper.cc
+++ chromeos/lacros/lacros_dbus_helper.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/lacros/lacros_dbus_helper.h"
 
 #include "chromeos/dbus/init/initialize_dbus_client.h"
+#include "chromeos/dbus/missive/missive_client.h"
 #include "chromeos/dbus/permission_broker/permission_broker_client.h"
 #include "chromeos/lacros/lacros_dbus_thread_manager.h"
 
@@ -22,10 +23,14 @@ void LacrosInitializeDBus() {
   dbus::Bus* bus = LacrosDBusThreadManager::Get()->GetSystemBus();
 
   InitializeDBusClient<PermissionBrokerClient>(bus);
+
+  InitializeDBusClient<MissiveClient>(bus);
 }
 
 void LacrosShutdownDBus() {
   // Shut down D-Bus clients in reverse order of initialization.
+  MissiveClient::Shutdown();
+
   PermissionBrokerClient::Shutdown();
 
   LacrosDBusThreadManager::Shutdown();
