10ed17225b32d7e2f894915273990c0ee2011db9
Christoph Schwering <schwering@google.com>
2021-09-17 01:39:04 +0000

    [Autofill] Fixed creation of ContentAutofillDrivers for deleted RFHs.
    This CL fixes that ContentAutofillDriverFactory may create drivers for
    deleted RenderFrameHosts in the following scenario:
    
    1. First, ContentAutofillDriverFactory::RenderFrameDeleted(rfh) destroys
       |rfh|'s ContentAutofillDriver.
    2. Next, Another RenderFrameDeleted(rfh) handler calls
       ContentAutofillDriverFactory::DriverForFactory(rfh), which creates
       a new ContentAutofillDriver for |rfh|.
    
    This CL fixes this by checking that |rfh| is in the "created" state.
    
    Bug: 1187842, 1248847
    Change-Id: I74082463f69fb615650444c7435318e469a4c669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3164243
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922383}

--- components/autofill/content/browser/content_autofill_driver_factory.cc
+++ components/autofill/content/browser/content_autofill_driver_factory.cc
@@ -110,8 +110,7 @@ void ContentAutofillDriverFactory::BindAutofillDriver(
   if (!factory)
     return;
 
-  ContentAutofillDriver* driver = factory->DriverForFrame(render_frame_host);
-  if (driver)
+  if (auto* driver = factory->DriverForFrame(render_frame_host))
     driver->BindPendingReceiver(std::move(pending_receiver));
 }
 
@@ -135,7 +134,7 @@ ContentAutofillDriver* ContentAutofillDriverFactory::DriverForFrame(
   AutofillDriver* driver = DriverForKey(render_frame_host);
 
   // ContentAutofillDriver are created on demand here.
-  if (!driver) {
+  if (!driver && render_frame_host->IsRenderFrameCreated()) {
     AddForKey(
         render_frame_host,
         base::BindRepeating(CreateDriver, render_frame_host, client(),
@@ -191,7 +190,8 @@ void ContentAutofillDriverFactory::DidStartNavigation(
     content::RenderFrameHost* render_frame_host =
         content::RenderFrameHost::FromID(id);
     if (render_frame_host) {
-      DriverForFrame(render_frame_host)->ProbablyFormSubmitted();
+      if (auto* driver = DriverForFrame(render_frame_host))
+        driver->ProbablyFormSubmitted();
     }
   }
 }
@@ -201,11 +201,12 @@ void ContentAutofillDriverFactory::DidFinishNavigation(
   if (navigation_handle->HasCommitted() &&
       (navigation_handle->IsInMainFrame() ||
        navigation_handle->HasSubframeNavigationEntryCommitted())) {
-    ContentAutofillDriver* driver =
-        DriverForFrame(navigation_handle->GetRenderFrameHost());
-    NavigationFinished(AutofillDriverFactory::HideUi(
-        !navigation_handle->IsInPrerenderedMainFrame()));
-    driver->DidNavigateFrame(navigation_handle);
+    if (auto* driver =
+            DriverForFrame(navigation_handle->GetRenderFrameHost())) {
+      NavigationFinished(AutofillDriverFactory::HideUi(
+          !navigation_handle->IsInPrerenderedMainFrame()));
+      driver->DidNavigateFrame(navigation_handle);
+    }
   }
 }
 
@@ -235,11 +236,8 @@ void ContentAutofillDriverFactory::ReadyToCommitNavigation(
       content::RenderFrameHost::LifecycleState::kPrerendering) {
     return;
   }
-  AutofillDriver* driver = DriverForFrame(render_frame_host);
-  if (!driver)
-    return;
-  static_cast<ContentAutofillDriver*>(driver)
-      ->MaybeReportAutofillWebOTPMetrics();
+  if (auto* driver = DriverForFrame(render_frame_host))
+    driver->MaybeReportAutofillWebOTPMetrics();
 }
 
 }  // namespace autofill
