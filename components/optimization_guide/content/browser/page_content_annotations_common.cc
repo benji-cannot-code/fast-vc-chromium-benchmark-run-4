27da9b18c07d1f24e1015f6bbdf633a428ad2b6c
Robert Ogden <robertogden@chromium.org>
2021-09-17 06:03:38 +0000

    Unimplemented OnDemand Page Annotations API Surface
    Lands the API surface and applicable subtypes. The API has a no-op
    implementation now and abundant use of TODOs, but it does move the
    History code to private/friend class.
    
    Bug: 1249632
    Change-Id: I07b4b52c8c725b0bf61e8559688e715287b0cb50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161075
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922439}

---
+++ components/optimization_guide/content/browser/page_content_annotations_common.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/optimization_guide/content/browser/page_content_annotations_common.h"
+
+#include "base/check_op.h"
+#include "base/strings/stringprintf.h"
+
+namespace optimization_guide {
+
+WeightedString::WeightedString(const std::string& value, double weight)
+    : value(value), weight(weight) {
+  DCHECK_GE(weight, 0.0);
+  DCHECK_LE(weight, 1.0);
+}
+WeightedString::~WeightedString() = default;
+
+BatchAnnotationResult::BatchAnnotationResult(const std::string& input)
+    : input(input) {}
+BatchAnnotationResult::BatchAnnotationResult(const BatchAnnotationResult&) =
+    default;
+BatchAnnotationResult::~BatchAnnotationResult() = default;
+
+}  // namespace optimization_guide
