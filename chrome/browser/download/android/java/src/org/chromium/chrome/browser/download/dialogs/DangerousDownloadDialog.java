40e41977d45c2411e164459cf5675678b455572f
Min Qin <qinmin@chromium.org>
2021-09-17 22:47:18 +0000

    Use tab modal dialog for dangerous download
    Use tabmodal instead of app modal
    based on feedback from 1240609
    
    BUG=1234151
    
    Change-Id: Iccf708a1f6be486e8b7690971f8df3ea54440361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115509
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922676}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DangerousDownloadDialog.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DangerousDownloadDialog.java
@@ -125,7 +125,7 @@ public class DangerousDownloadDialog {
                                                   .PRIMARY_OUTLINE_NEGATIVE_OUTLINE)
                         .build();
 
-        modalDialogManager.showDialog(propertyModel, ModalDialogManager.ModalDialogType.APP);
+        modalDialogManager.showDialog(propertyModel, ModalDialogManager.ModalDialogType.TAB);
         recordDangerousDownloadDialogEvent(
                 DangerousDownloadDialogEvent.DANGEROUS_DOWNLOAD_DIALOG_SHOW);
     }
