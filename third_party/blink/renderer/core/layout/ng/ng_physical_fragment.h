28d46996528ccce227fba1c23aedd97f43643543
Koji Ishii <kojii@chromium.org>
2021-09-17 05:08:48 +0000

    [block-in-inline] Do not include opaque fragments for outlilne
    This patch stops including opaque fragments[1] for outline.
    
    This makes LayoutNGBlockInInline match the current NG/legacy
    behavior, fixes the following test when LayoutNGBlockInInline
    is enabled:
      paint/invalidation/outline/focus-ring-on-continuation-move.html
    
    The test in this CL fails in legacy by drawing outlines
    differently by a few pixels though.
    
    [1] https://docs.google.com/document/d/15kgdIHhb9EVNup6Ir5NWwJxpzY5GH0ED7Ld3iMW3HlA/edit#bookmark=id.uywtiaww1ovd
    
    Bug: 716930
    Change-Id: I236264aceda5158097cded85fc5ae57a2470d40c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3167110
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922431}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
@@ -186,6 +186,7 @@ class CORE_EXPORT NGPhysicalFragment
            layout_object_->IsRubyBase();
   }
 
+  bool IsSvg() const { return layout_object_->IsSVG(); }
   bool IsSvgText() const { return layout_object_->IsNGSVGText(); }
 
   bool IsTableNGPart() const { return is_table_ng_part_; }
