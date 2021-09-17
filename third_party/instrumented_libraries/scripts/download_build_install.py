976b771279ae2329751262b6dec93cd66bd2c98c
Tom Anderson <thomasanderson@chromium.org>
2021-09-17 22:42:52 +0000

    Xenial instrumented libraries: Fix build in all configurations
    Fixes the following package builds:
    * libgnutls30
    * libcurl3-gnutls
      - Removed as it's not needed
    * libgdk-pixbuf2.0-0
    * libglib2.0-0
    * libwayland-client0
    
    Also fixed an issue where build failures were not decoded before being
    printed in python3.
    
    R=thestig
    
    Bug: 1244199
    Change-Id: I19a887ddb18adfc41f300972de98a710904b88d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169687
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922667}

--- third_party/instrumented_libraries/scripts/download_build_install.py
+++ third_party/instrumented_libraries/scripts/download_build_install.py
@@ -113,7 +113,7 @@ class InstrumentedPackageBuilder(object):
     child = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
         env=env, shell=True, cwd=cwd)
-    stdout, stderr = child.communicate()
+    stdout = child.communicate()[0].decode('utf-8')
     if ignore_ret_code:
       if self._verbose:
         print(stdout)
@@ -339,13 +339,6 @@ class DebianBuilder(InstrumentedPackageBuilder):
     return deb_files
 
 
-class LibcurlBuilder(DebianBuilder):
-  def build_and_install(self):
-    DebianBuilder.build_and_install(self)
-    self.shell_call('ln -rsf %s/libcurl-gnutls.so.4 %s/libcurl.so' %
-                    (self.dest_libdir(), self.dest_libdir()))
-
-
 class LibcapBuilder(InstrumentedPackageBuilder):
   def build_and_install(self):
     # libcap2 doesn't have a configure script
@@ -526,8 +519,6 @@ def main():
     builder = NSSBuilder(args, clobber)
   elif args.build_method == 'custom_libcap':
     builder = LibcapBuilder(args, clobber)
-  elif args.build_method == 'custom_libcurl':
-    builder = LibcurlBuilder(args, clobber)
   elif args.build_method == 'custom_libpci3':
     builder = Libpci3Builder(args, clobber)
   elif args.build_method == 'debian':
