53541f629700edb3dba974c70cc357c5d6935d59
Munira Tursunova <moonira@google.com>
2021-09-17 14:52:50 +0000

    Implemented font-synthesis-small-caps functionality
    Implement of CSS property font-synthesis-small-caps functionality, which
    controls synthesized small-caps. Added tests for fonts which support and
    do not support small-caps.
    
    Bug: 509989
    Change-Id: Ia996dfa2d54e021b5934e901301a5fa896be9c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162064
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922513}

--- third_party/blink/renderer/platform/fonts/opentype/open_type_caps_support.cc
+++ third_party/blink/renderer/platform/fonts/opentype/open_type_caps_support.cc
@@ -41,7 +41,6 @@ bool activationSelectorPresent(
 
 OpenTypeCapsSupport::OpenTypeCapsSupport()
     : harfbuzz_face_(nullptr),
-      requested_caps_(FontDescription::kCapsNormal),
       font_support_(FontSupport::kFull),
       caps_synthesis_(CapsSynthesis::kNone),
       font_format_(FontFormat::kUndetermined) {}
@@ -49,9 +48,11 @@ OpenTypeCapsSupport::OpenTypeCapsSupport()
 OpenTypeCapsSupport::OpenTypeCapsSupport(
     const HarfBuzzFace* harfbuzz_face,
     FontDescription::FontVariantCaps requested_caps,
+    FontDescription::FontSynthesisSmallCaps font_synthesis_small_caps,
     hb_script_t script)
     : harfbuzz_face_(harfbuzz_face),
       requested_caps_(requested_caps),
+      font_synthesis_small_caps_(font_synthesis_small_caps),
       font_support_(FontSupport::kFull),
       caps_synthesis_(CapsSynthesis::kNone),
       font_format_(FontFormat::kUndetermined) {
@@ -80,7 +81,8 @@ FontDescription::FontVariantCaps OpenTypeCapsSupport::FontFeatureToUse(
 bool OpenTypeCapsSupport::NeedsRunCaseSplitting() {
   // Lack of titling case support is ignored, titling case is not synthesized.
   return font_support_ != FontSupport::kFull &&
-         requested_caps_ != FontDescription::kTitlingCaps;
+         requested_caps_ != FontDescription::kTitlingCaps &&
+         SyntheticSmallCapsAllowed();
 }
 
 bool OpenTypeCapsSupport::NeedsSyntheticFont(
@@ -89,6 +91,9 @@ bool OpenTypeCapsSupport::NeedsSyntheticFont(
     return false;
 
   if (requested_caps_ == FontDescription::kTitlingCaps)
+    return false;
+
+  if (!SyntheticSmallCapsAllowed())
     return false;
 
   if (font_support_ == FontSupport::kNone) {
@@ -111,7 +116,7 @@ CaseMapIntend OpenTypeCapsSupport::NeedsCaseChange(
     SmallCapsIterator::SmallCapsBehavior run_case) {
   CaseMapIntend case_map_intend = CaseMapIntend::kKeepSameCase;
 
-  if (font_support_ == FontSupport::kFull)
+  if (font_support_ == FontSupport::kFull || !SyntheticSmallCapsAllowed())
     return case_map_intend;
 
   switch (run_case) {
@@ -279,6 +284,11 @@ void OpenTypeCapsSupport::DetermineFontSupport(hb_script_t script) {
     default:
       NOTREACHED();
   }
+}
+
+bool OpenTypeCapsSupport::SyntheticSmallCapsAllowed() const {
+  return font_synthesis_small_caps_ ==
+         FontDescription::kAutoFontSynthesisSmallCaps;
 }
 
 }  // namespace blink
