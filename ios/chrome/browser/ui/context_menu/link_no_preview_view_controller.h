d7496004eb52fda1341cd0f6c0b1312ecce810ea
Gauthier Ambard <gambard@chromium.org>
2021-09-17 14:53:49 +0000

    [iOS] Add favicon to ContextMenu link
    This CL adds the attributes of the favicon when showing the Context Menu
    UI for a link, without preview.
    The fetch of the favicon is done BrowserViewController as it is creating
    the ViewController, which has no mediator nor coordinator.
    
    Fixed: 1237929
    Change-Id: I3012b1c7dd26829ebc4fd4230af9f0da30956568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162601
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922514}

--- ios/chrome/browser/ui/context_menu/link_no_preview_view_controller.h
+++ ios/chrome/browser/ui/context_menu/link_no_preview_view_controller.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+@class FaviconAttributes;
+
 // View Controller showing the information for a link when a preview of the
 // destination is not displayed.
 @interface LinkNoPreviewViewController : UIViewController
@@ -21,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (instancetype)initWithNibName:(NSString*)nibNameOrNil
                          bundle:(NSBundle*)nibBundleOrNil NS_UNAVAILABLE;
+
+// Configures the Favicon with |attributes|.
+- (void)configureFaviconWithAttributes:(FaviconAttributes*)attributes;
 
 @end
 
