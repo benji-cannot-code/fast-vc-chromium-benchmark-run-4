aff85edbfcf97e3fbbb71483d2281412108f4c7e
David Black <dmblack@google.com>
2021-09-20 19:12:48 +0000

    Fix dark mode colors in cellular_eid_dialog.
    NOTE: In dark mode a light background is applied to the QR code canvas
    as some QR code scanners cannot read an inverted QR code in which the
    background is darker than the foreground.
    
    Before (dark):  http://shortn/_bK1pvfqlLG
    After (dark):   http://shortn/_X13i65O6qg
    Before (light): http://shortn/_hODzmyiPtN
    After (light):  http://shortn/_XMMDzkjEFB
    
    Bug: 1250941
    Change-Id: I37f6373063ecc560ad91b1171631382c155e6f29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3170494
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#923014}

--- ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_eid_dialog.js
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_eid_dialog.js
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // The size of each tile in pixels.
 const QR_CODE_TILE_SIZE = 5;
-// Styling for filled tiles in the QR code.
-const QR_CODE_FILL_STYLE = '#000000';
 
 Polymer({
   is: 'cellular-eid-dialog',
@@ -66,7 +64,8 @@ Polymer({
     Polymer.dom.flush();
     const context = this.getCanvasContext_();
     context.clearRect(0, 0, this.canvasSize_, this.canvasSize_);
-    context.fillStyle = QR_CODE_FILL_STYLE;
+    context.fillStyle = getComputedStyle(this.$.qrCodeCanvas)
+                            .getPropertyValue('--cros-dark-icon-color-primary');
     let index = 0;
     for (let x = 0; x < response.qrCode.size; x++) {
       for (let y = 0; y < response.qrCode.size; y++) {
