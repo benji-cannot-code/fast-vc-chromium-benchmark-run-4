e4ddbf96d75f3397ea576016bdeff0859bd8ccc5
Eric Willigers <ericwilligers@chromium.org>
2021-09-20 08:58:15 +0000

    FeedbackHandler: Use defined(OS_CHROMEOS)
    Instead of testing for Ash or Lacros, we simply test for
    defined(OS_CHROMEOS).
    
    This also avoids the need to include build/chromeos_buildflags.h
    
    Bug: 1246565
    Change-Id: Ib6370542bf9e3455c27997dbe73930768dc58197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168526
    Commit-Queue: calamity <calamity@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922864}

--- chrome/browser/ui/webui/feedback/feedback_handler.cc
+++ chrome/browser/ui/webui/feedback/feedback_handler.cc
@@ -56,7 +56,7 @@ void FeedbackHandler::RegisterMessages() {
   web_ui()->RegisterDeprecatedMessageCallback(
       "showDialog", base::BindRepeating(&FeedbackHandler::HandleShowDialog,
                                         base::Unretained(this)));
-#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_CHROMEOS)
   web_ui()->RegisterDeprecatedMessageCallback(
       "showAssistantLogsInfo",
       base::BindRepeating(&FeedbackHandler::HandleShowAssistantLogsInfo,
@@ -65,7 +65,7 @@ void FeedbackHandler::RegisterMessages() {
       "showBluetoothLogsInfo",
       base::BindRepeating(&FeedbackHandler::HandleShowBluetoothLogsInfo,
                           base::Unretained(this)));
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#endif  // defined(OS_CHROMEOS)
   web_ui()->RegisterDeprecatedMessageCallback(
       "showSystemInfo",
       base::BindRepeating(&FeedbackHandler::HandleShowSystemInfo,
@@ -79,7 +79,7 @@ void FeedbackHandler::HandleShowDialog(const base::ListValue* args) {
   dialog_->Show();
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_CHROMEOS)
 void FeedbackHandler::HandleShowAssistantLogsInfo(const base::ListValue* args) {
   ShowChildPage(dialog_, ChildPageURL("html/assistant_logs_info.html"),
                 std::u16string(),
@@ -92,7 +92,7 @@ void FeedbackHandler::HandleShowBluetoothLogsInfo(const base::ListValue* args) {
                 /*dialog_width=*/400, /*dialog_height=*/120,
                 /*can_resize=*/false, /*can_minimize=*/false);
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#endif  // defined(OS_CHROMEOS)
 
 void FeedbackHandler::HandleShowSystemInfo(const base::ListValue* args) {
   ShowChildPage(dialog_, ChildPageURL("html/sys_info.html"),
