1eaaedf3545357222fb01ee3a9905afdd3979e72
Ashley Prasad <ashleydp@google.com>
2021-09-20 23:41:40 +0000

    Diagnostics: Display network details for connecting state
    - Updates condition to display network details for connecting state
      to avoid displaying an empty card below network-card header.
    - Tests display expectation in connecting state.
    
    Screenshot: http://shortn/_71JJxZsgSD
    
    Bug: 1197335
    Test: ./out/Default/browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: Ibecc1abe91cae7af9339062b2131ef418b8c0b32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171524
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923133}

--- chrome/test/data/webui/chromeos/diagnostics/network_card_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/network_card_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://diagnostics/network_card.js';
 
-import {fakeCellularNetwork, fakeDisconnectedEthernetNetwork, fakeDisconnectedWifiNetwork, fakeEthernetNetwork, fakeNetworkGuidInfoList, fakePortalWifiNetwork, fakeWifiNetwork, fakeWifiNetworkDisabled} from 'chrome://diagnostics/fake_data.js';
+import {fakeCellularNetwork, fakeConnectingEthernetNetwork, fakeDisconnectedEthernetNetwork, fakeDisconnectedWifiNetwork, fakeEthernetNetwork, fakeNetworkGuidInfoList, fakePortalWifiNetwork, fakeWifiNetwork, fakeWifiNetworkDisabled} from 'chrome://diagnostics/fake_data.js';
 import {FakeNetworkHealthProvider} from 'chrome://diagnostics/fake_network_health_provider.js';
 import {setNetworkHealthProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
 
@@ -51,6 +51,8 @@ export function networkCardTestSuite() {
     provider.setFakeNetworkState(
         'wifiDisconnectedGuid', [fakeDisconnectedWifiNetwork]);
     provider.setFakeNetworkState('wifiPortalGuid', [fakePortalWifiNetwork]);
+    provider.setFakeNetworkState(
+        'ethernetConnectingGuid', [fakeConnectingEthernetNetwork]);
     // Add the network info to the DOM.
     networkCardElement = /** @type {!NetworkCardElement} */ (
         document.createElement('network-card'));
@@ -162,6 +164,17 @@ export function networkCardTestSuite() {
       assertTrue(isVisible(getTroubleConnectingElement()));
       assertFalse(isVisible(getNetworkInfoElement()));
       assertFalse(isVisible(getIpConfigDrawerElement()));
+    });
+  });
+
+  test('NetworkConnectingHideTroubleShooting', () => {
+    return initializeNetworkCard('ethernetConnectingGuid').then(() => {
+      dx_utils.assertElementContainsText(
+          networkCardElement.$$('#cardTitle'),
+          'Ethernet [81:C5:A6:30:3F:33] (Connecting)');
+      assertFalse(isVisible(getTroubleConnectingElement()));
+      assertTrue(isVisible(getNetworkInfoElement()));
+      assertTrue(isVisible(getIpConfigDrawerElement()));
     });
   });
 
