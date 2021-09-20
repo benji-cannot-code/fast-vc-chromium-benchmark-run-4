f15930c9ed9fafbcbb0e498c263cd245d7810584
Scott Violet <sky@chromium.org>
2021-09-20 18:23:22 +0000

    [reland] views: fixes menus repeatedly triggering painting
    This differs from the first patch in so far as updating of state
    is moved to paint. I did the for two reasons:
    . Updating state from CreateSubmenu() is problematic as some models
      don't expect this (AppMenuModel).
    . During paint, MenuItemView is guaranteed to be a Widget, so that
      colors can be looked up.
    
    This means an additional unnecessary paint may occur when shown
    (because paint may update the image, triggering another paint), but
    this is easier to deal with then worrying about whether state can
    be updated at a particular point because the MenuItemView isn't in
    a widget, or the delegate is in a weird state.
    
    While the show may generate an additional unnecessary paint, only
    one will be generated, not a continuous stream as could happen before.
    
    
    BUG=1245854
    TEST=MenuItemViewPaintUnitTest.DontSchedulePaintFromOnPaint
    
    Change-Id: I55d7c1629f6acd60ea55a388380e7727c6cda20c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163439
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922973}

--- ui/views/controls/menu/menu_item_view.h
+++ ui/views/controls/menu/menu_item_view.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/models/menu_separator_types.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/image/image_skia.h"
@@ -285,6 +286,9 @@ class VIEWS_EXPORT MenuItemView : public View {
   }
   const std::u16string& accessible_name() const { return accessible_name_; }
 
+  // Called when the drop status of this item changes.
+  void OnDropStatusChanged();
+
   // Paints the menu item.
   void OnPaint(gfx::Canvas* canvas) override;
 
@@ -380,6 +384,9 @@ class VIEWS_EXPORT MenuItemView : public View {
 
   // View:
   void ChildPreferredSizeChanged(View* child) override;
+  void OnThemeChanged() override;
+  void ViewHierarchyChanged(
+      const ViewHierarchyChangedDetails& details) override;
 
   // Returns the preferred size (and padding) of any children.
   virtual gfx::Size GetChildPreferredSize() const;
@@ -394,7 +401,14 @@ class VIEWS_EXPORT MenuItemView : public View {
   friend class test::TestMenuItemViewNotShown;  // for access to |submenu_|;
   friend class TestMenuItemView;  // For access to AddEmptyMenus();
 
-  enum class PaintButtonMode { kNormal, kForDrag };
+  enum class PaintMode { kNormal, kForDrag };
+
+  // The set of colors used in painting the MenuItemView.
+  struct Colors {
+    SkColor fg_color = SK_ColorTRANSPARENT;
+    SkColor icon_color = SK_ColorTRANSPARENT;
+    SkColor minor_fg_color = SK_ColorTRANSPARENT;
+  };
 
   // Calculates all sizes that we can from the OS.
   //
@@ -429,15 +443,17 @@ class VIEWS_EXPORT MenuItemView : public View {
   // necessary.
   void AdjustBoundsForRTLUI(gfx::Rect* rect) const;
 
-  // Actual paint implementation. If mode is kForDrag, portions of the menu are
-  // not rendered.
-  void PaintButton(gfx::Canvas* canvas, PaintButtonMode mode);
+  // Paints the MenuItemView for a drag operation.
+  void PaintForDrag(gfx::Canvas* canvas);
 
-  // Helper function for PaintButton(), draws the background for the button if
-  // appropriate.
+  // Actual paint implementation.
+  void OnPaintImpl(gfx::Canvas* canvas, PaintMode mode);
+
+  // Helper function for OnPaintImpl() that is responsible for drawing the
+  // background.
   void PaintBackground(gfx::Canvas* canvas,
-                       PaintButtonMode mode,
-                       bool render_selection);
+                       PaintMode mode,
+                       bool paint_as_selected);
 
   // Paints the right-side icon and text.
   void PaintMinorIconAndText(gfx::Canvas* canvas, SkColor color);
@@ -455,7 +471,10 @@ class VIEWS_EXPORT MenuItemView : public View {
 
   // Returns the text color for the current state.  |minor| specifies if the
   // minor text or the normal text is desired.
-  SkColor GetTextColor(bool minor, bool render_selection) const;
+  SkColor GetTextColor(bool minor, bool paint_as_selected) const;
+
+  // Returns the colors used in painting.
+  Colors CalculateColors(bool paint_as_selected) const;
 
   // Returns the accessible name for this menu item.
   std::u16string GetAccessibleName() const;
@@ -507,6 +526,15 @@ class VIEWS_EXPORT MenuItemView : public View {
   void invalidate_dimensions() { dimensions_.height = 0; }
   bool is_dimensions_valid() const { return dimensions_.height > 0; }
 
+  // Calls UpdateSelectionBasedState() if the the selection state changed.
+  void UpdateSelectionBasedStateIfChanged(PaintMode mode);
+
+  // Updates any state that may changed based on the selection state.
+  void UpdateSelectionBasedState(bool should_paint_as_selected);
+
+  // Returns true if the MenuItemView should be painted as selected.
+  bool ShouldPaintAsSelected(PaintMode mode) const;
+
   // The delegate. This is only valid for the root menu item. You shouldn't
   // use this directly, instead use GetDelegate() which walks the tree as
   // as necessary.
@@ -527,6 +555,8 @@ class VIEWS_EXPORT MenuItemView : public View {
 
   // Whether we're selected.
   bool selected_ = false;
+
+  bool last_paint_as_selected_ = false;
 
   // Whether the submenu area of an ACTIONABLE_SUBMENU is selected.
   bool submenu_area_of_actionable_submenu_selected_ = false;
