e2ec04492a37b82001f28c7adf5f71fb251c65c5
Alice Wang <aliceywang@chromium.org>
2021-09-20 12:19:49 +0000

    [Android][FRE] Support sign-out on the sync consent screen
    This CL allows the sync consent screen to support the sign-out. This
    can happen during the FRE, when user first signed in without sync
    consent from the welcomee screen and then back-pressed and signed out.
    The sync consent should also reflect this change.
    
    Bug: 1250185
    Change-Id: I008cf3dd1f10182147cbfb64e80bd219a2afe216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3165036
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922887}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SyncConsentFragmentBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SyncConsentFragmentBase.java
@@ -317,6 +317,7 @@ public abstract class SyncConsentFragmentBase
         mAccountManagerFacade.getAccounts().then(this::updateAccounts);
     }
 
+    /** Implements {@link SigninManager.SignInStateObserver}. */
     @Override
     public void onSignedIn() {
         final CoreAccountInfo primaryAccount =
@@ -327,6 +328,16 @@ public abstract class SyncConsentFragmentBase
                 && mSigninAccessPoint == SigninAccessPoint.START_PAGE && primaryAccount != null);
         if (mIsSignedInWithoutSync) {
             mSelectedAccountName = primaryAccount.getEmail();
+            mAccountManagerFacade.getAccounts().then(this::updateAccounts);
+        }
+    }
+
+    /** Implements {@link SigninManager.SignInStateObserver}. */
+    @Override
+    public void onSignedOut() {
+        if (FREMobileIdentityConsistencyFieldTrial.isEnabled()) {
+            mIsSignedInWithoutSync = false;
+            mSelectedAccountName = null;
             mAccountManagerFacade.getAccounts().then(this::updateAccounts);
         }
     }
