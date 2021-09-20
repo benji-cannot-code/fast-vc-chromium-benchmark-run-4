ffeb0b0e7587c0989f2c6df31f848d31b7f12d92
Moe Ahmadi <mahmadi@chromium.org>
2021-09-20 21:08:26 +0000

    [memories] Makes entire row clickable and opens links in a new tab
    Rather than using target="_blank" attribute, click events are explicitly
    handled in JS for the following reasons:
    
    1) adding histogram logging in JS.
    2) potentially having to move the navigation logic to the browser
       side if needed (e.g., related searches requiring a different
       navigation type).
    
    Bug: 1173908
    Change-Id: I787acc7a8f5f7a6251792affdb8d6e9099d5b338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163454
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923047}

---
+++ chrome/browser/resources/history/history_clusters/open_window_proxy.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview This file provides an abstraction layer for Window.open() for
+ * for mocking in tests.
+ * TODO(http://crbug.com/1250487): Refactor this file and similar files into
+ * ui/webui/resources/js/
+ */
+
+export class OpenWindowProxy {
+  open(url: string) {
+    window.open(url);
+  }
+
+  static getInstance(): OpenWindowProxy {
+    return instance || (instance = new OpenWindowProxy());
+  }
+
+  static setInstance(obj: OpenWindowProxy) {
+    instance = obj;
+  }
+}
+
+let instance: OpenWindowProxy|null = null;
