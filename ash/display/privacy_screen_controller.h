2f0a841b3a1015cc2ec32a70cfb0f21161456c78
Alvin Lee <alvinlee@google.com>
2021-09-20 12:09:54 +0000

    Fix DLP eprivacy screen enforcement logic for unsupported devices
    Previously, DlpContentManager attempted to enforce a privacy screen
    (if configured and triggered) regardless if the device supported a
    privacy screen. This led to erroneous eprivacy events to be reported.
    This CL exposes the PrivacyScreenController::IsSupported() function
    to the parent PrivacyScreenDlpHelper. This is then used by the
    DlpContentManager to only attempt to enforce a privacy screen in if
    the running device supports it.
    
    Bug: 1246385
    Change-Id: I2eaee63f11e1d163237c5043df3fa273c7b9e459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162056
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Alvin Lee <alvinlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#922886}

--- ash/display/privacy_screen_controller.h
+++ ash/display/privacy_screen_controller.h
@@ -55,8 +55,6 @@ class ASH_EXPORT PrivacyScreenController
 
   static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
-  // Whether or not the privacy screen feature is supported by the device.
-  bool IsSupported() const;
   // Whether or not the privacy screen feature is enforced by policy.
   bool IsManaged() const;
   // Get the PrivacyScreen settings stored in the current active user prefs.
@@ -68,6 +66,7 @@ class ASH_EXPORT PrivacyScreenController
   void RemoveObserver(Observer* observer);
 
   // PrivacyScreenDlpHelper:
+  bool IsSupported() const override;
   void SetEnforced(bool enforced) override;
 
   // SessionObserver:
