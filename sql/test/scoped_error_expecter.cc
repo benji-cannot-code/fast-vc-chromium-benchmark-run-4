b13574280f0005c37d6adbae84a9e9070a8b8d55
Victor Costan <pwnall@chromium.org>
2021-09-20 21:02:49 +0000

    sql: Remove ScopedErrorExpecter::SQLiteLibVersionNumber().
    Chrome links its own SQLite build on all platforms. Code that uses //sql
    should never need to check the SQLite version, as it does not vary.
    
    Change-Id: I3991ca396fe422f4f46a3ef748517c1e9e2cdc5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3170452
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923037}

--- sql/test/scoped_error_expecter.cc
+++ sql/test/scoped_error_expecter.cc
@@ -11,11 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sql {
 namespace test {
 
-// static
-int ScopedErrorExpecter::SQLiteLibVersionNumber() {
-  return sqlite3_libversion_number();
-}
-
 ScopedErrorExpecter::ScopedErrorExpecter()
     : checked_(false) {
   callback_ = base::BindRepeating(&ScopedErrorExpecter::ErrorSeen,
