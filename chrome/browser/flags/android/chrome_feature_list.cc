0087d453e9d4fb73d96c8a342d3f70ce24923117
Sky Malice <skym@chromium.org>
2021-09-20 18:43:07 +0000

    Enable ThemeRefactorAndroid by default.
    Bug: 1251194
    Change-Id: I431b812cb8082500753efb807ef39752762c43b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171224
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#922990}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -749,7 +749,7 @@ const base::Feature kTestDefaultEnabled{"TestDefaultEnabled",
                                         base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kThemeRefactorAndroid{"ThemeRefactorAndroid",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kToolbarIphAndroid{"ToolbarIphAndroid",
                                        base::FEATURE_ENABLED_BY_DEFAULT};
