f34df2fb9c108fb5322f54ba6e087e081b34dc13
Chloe Pelling <cpelling@google.com>
2021-09-20 08:55:44 +0000

    Revert "[Fast Pair] Disable fast pair when screen is off"
    This reverts commit 846710ff02768d258866e96c16efe580b9dd91dd.
    
    Reason for revert: New tests fail ASAN; see https://crbug.com/1251051
    
    Original change's description:
    > [Fast Pair] Disable fast pair when screen is off
    >
    > Adds an enabled provider to fast pair that detects
    > when there are no screens on (i.e. the laptop lid
    > is closed or brightness is 0). Had to move the creation
    > of the fast pair mediator in order for this provider
    > to observe the DisplayConfigurator.
    >
    > Change-Id: I6a04ef57574cc9f00be7d6100618472686cb4e79
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152045
    > Commit-Queue: Daniel Classon <dclasson@google.com>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#922645}
    
    Change-Id: I1dce339b251674a74d6e1df8c1b6fedb5a5a7112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168987
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Chloe Pelling <cpelling@google.com>
    Owners-Override: Chloe Pelling <cpelling@google.com>
    Cr-Commit-Position: refs/heads/main@{#922859}

--- ash/quick_pair/feature_status_tracker/fast_pair_enabled_provider.h
+++ ash/quick_pair/feature_status_tracker/fast_pair_enabled_provider.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/feature_status_tracker/bluetooth_enabled_provider.h"
 #include "ash/quick_pair/feature_status_tracker/google_api_key_availability_provider.h"
 #include "ash/quick_pair/feature_status_tracker/logged_in_user_enabled_provider.h"
-#include "ash/quick_pair/feature_status_tracker/screen_state_enabled_provider.h"
 #include "base/memory/weak_ptr.h"
 
 namespace ash {
@@ -24,7 +23,6 @@ class FastPairEnabledProvider : public BaseEnabledProvider {
       std::unique_ptr<BluetoothEnabledProvider> bluetooth_enabled_provider,
       std::unique_ptr<LoggedInUserEnabledProvider>
           logged_in_user_enabled_provider,
-      std::unique_ptr<ScreenStateEnabledProvider> screen_state_enabled_provider,
       std::unique_ptr<GoogleApiKeyAvailabilityProvider>
           google_api_key_availability_provider);
   ~FastPairEnabledProvider() override;
@@ -35,7 +33,6 @@ class FastPairEnabledProvider : public BaseEnabledProvider {
 
   std::unique_ptr<BluetoothEnabledProvider> bluetooth_enabled_provider_;
   std::unique_ptr<LoggedInUserEnabledProvider> logged_in_user_enabled_provider_;
-  std::unique_ptr<ScreenStateEnabledProvider> screen_state_enabled_provider_;
   std::unique_ptr<GoogleApiKeyAvailabilityProvider>
       google_api_key_availability_provider_;
   base::WeakPtrFactory<FastPairEnabledProvider> weak_factory_{this};
--- ash/quick_pair/feature_status_tracker/fast_pair_enabled_provider_unittest.cc
+++ ash/quick_pair/feature_status_tracker/fast_pair_enabled_provider_unittest.cc
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/feature_status_tracker/mock_bluetooth_enabled_provider.h"
 #include "ash/quick_pair/feature_status_tracker/mock_google_api_key_availability_provider.h"
 #include "ash/quick_pair/feature_status_tracker/mock_logged_in_user_enabled_provider.h"
-#include "ash/quick_pair/feature_status_tracker/mock_screen_state_enabled_provider.h"
-#include "ash/quick_pair/feature_status_tracker/screen_state_enabled_provider.h"
-#include "ash/test/ash_test_base.h"
 #include "base/callback.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/scoped_refptr.h"
@@ -26,15 +23,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/scoped_user_manager.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "testing/gtest/include/gtest/gtest-param-test.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
 namespace quick_pair {
 
-class FastPairEnabledProviderTest : public AshTestBase {
+class FastPairEnabledProviderTest : public testing::Test {
  public:
   void SetUp() override {
-    AshTestBase::SetUp();
-
     adapter_ = base::MakeRefCounted<FakeBluetoothAdapter>();
     device::BluetoothAdapterFactory::SetAdapterForTesting(adapter_);
     scoped_user_manager_ = std::make_unique<user_manager::ScopedUserManager>(
@@ -56,10 +52,6 @@ TEST_F(FastPairEnabledProviderTest, ProviderCallbackIsInvokedOnBTChanges) {
   ON_CALL(*logged_in_user_enabled_provider, is_enabled)
       .WillByDefault(testing::Return(true));
 
-  auto* screen_state_enabled_provider = new MockScreenStateEnabledProvider();
-  ON_CALL(*screen_state_enabled_provider, is_enabled)
-      .WillByDefault(testing::Return(true));
-
   auto* google_api_key_availability_provider =
       new MockGoogleApiKeyAvailabilityProvider();
   ON_CALL(*google_api_key_availability_provider, is_enabled)
@@ -68,7 +60,6 @@ TEST_F(FastPairEnabledProviderTest, ProviderCallbackIsInvokedOnBTChanges) {
   auto provider = std::make_unique<FastPairEnabledProvider>(
       std::make_unique<BluetoothEnabledProvider>(),
       base::WrapUnique(logged_in_user_enabled_provider),
-      base::WrapUnique(screen_state_enabled_provider),
       base::WrapUnique(google_api_key_availability_provider));
 
   provider->SetCallback(callback.Get());
@@ -77,8 +68,8 @@ TEST_F(FastPairEnabledProviderTest, ProviderCallbackIsInvokedOnBTChanges) {
 }
 
 // Represents: <is_flag_enabled, is_bt_enabled, is_user_logged_in,
-//              is_screen_state_on, is_google_api_keys_available>
-using TestParam = std::tuple<bool, bool, bool, bool, bool>;
+//              is_google_api_keys_available>
+using TestParam = std::tuple<bool, bool, bool, bool>;
 
 class FastPairEnabledProviderTestWithParams
     : public FastPairEnabledProviderTest,
@@ -88,8 +79,7 @@ TEST_P(FastPairEnabledProviderTestWithParams, IsEnabledWhenExpected) {
   bool is_flag_enabled = std::get<0>(GetParam());
   bool is_bt_enabled = std::get<1>(GetParam());
   bool is_user_logged_in = std::get<2>(GetParam());
-  bool is_screen_state_on = std::get<3>(GetParam());
-  bool is_google_api_keys_available = std::get<4>(GetParam());
+  bool is_google_api_keys_available = std::get<3>(GetParam());
 
   base::test::ScopedFeatureList feature_list;
   feature_list.InitWithFeatureState(features::kFastPair, is_flag_enabled);
@@ -102,10 +92,6 @@ TEST_P(FastPairEnabledProviderTestWithParams, IsEnabledWhenExpected) {
   ON_CALL(*logged_in_user_enabled_provider, is_enabled)
       .WillByDefault(testing::Return(is_user_logged_in));
 
-  auto* screen_state_enabled_provider = new MockScreenStateEnabledProvider();
-  ON_CALL(*screen_state_enabled_provider, is_enabled)
-      .WillByDefault(testing::Return(is_screen_state_on));
-
   auto* google_api_key_availability_provider =
       new MockGoogleApiKeyAvailabilityProvider();
   ON_CALL(*google_api_key_availability_provider, is_enabled)
@@ -115,13 +101,10 @@ TEST_P(FastPairEnabledProviderTestWithParams, IsEnabledWhenExpected) {
       std::unique_ptr<BluetoothEnabledProvider>(bluetooth_enabled_provider),
       std::unique_ptr<LoggedInUserEnabledProvider>(
           logged_in_user_enabled_provider),
-      std::unique_ptr<ScreenStateEnabledProvider>(
-          screen_state_enabled_provider),
       base::WrapUnique(google_api_key_availability_provider));
 
   bool all_are_enabled = is_flag_enabled && is_bt_enabled &&
-                         is_user_logged_in && is_screen_state_on &&
-                         is_google_api_keys_available;
+                         is_user_logged_in && is_google_api_keys_available;
 
   EXPECT_EQ(provider->is_enabled(), all_are_enabled);
 }
@@ -129,7 +112,6 @@ TEST_P(FastPairEnabledProviderTestWithParams, IsEnabledWhenExpected) {
 INSTANTIATE_TEST_SUITE_P(FastPairEnabledProviderTestWithParams,
                          FastPairEnabledProviderTestWithParams,
                          testing::Combine(testing::Bool(),
-                                          testing::Bool(),
                                           testing::Bool(),
                                           testing::Bool(),
                                           testing::Bool()));
