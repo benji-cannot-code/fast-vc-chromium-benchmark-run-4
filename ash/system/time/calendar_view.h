971fff8dcecee21daacc08eabeb96e316c75b87f
Jiaming Cheng <jiamingc@chromium.org>
2021-09-20 19:42:45 +0000

    Add year to the month label
    If the month view is not in the current year, a year label is added to
    the month label.
    
    Also fixed the color of the label text.
    
    Bug: 1236276
    
    Change-Id: Ic619fb85913101a1ce0817e865e8c723553ac61c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169825
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923031}

--- ash/system/time/calendar_view.h
+++ ash/system/time/calendar_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/unified/unified_system_tray_controller.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/scroll_view.h"
+#include "ui/views/view.h"
 
 namespace views {
 
@@ -56,6 +57,15 @@ class ASH_EXPORT CalendarView : public CalendarViewController::Observer,
                                   int info_accessible_name_id) override;
 
  private:
+  // The header of each month view which shows the month's name. If the year of
+  // this month is not the same as the current month, the year is also shown in
+  // this view.
+  class MonthYearHeaderView;
+
+  // The types to create the `MonthYearHeaderView` which are in corresponding to
+  // the 3 months: `previous_month_`, `current_month_` and `next_month_`.
+  enum LabelType { PREVIOUS, CURRENT, NEXT };
+
   friend class CalendarViewTest;
 
   // Assigns month views and labels based on the current date on screen.
@@ -68,8 +78,7 @@ class ASH_EXPORT CalendarView : public CalendarViewController::Observer,
   int PositionOfToday();
 
   // Adds a month label.
-  views::Label* AddLabelWithId(std::u16string label_string,
-                               bool add_at_front = false);
+  views::View* AddLabelWithId(LabelType type, bool add_at_front = false);
 
   // Adds a `CalendarMonthView`.
   CalendarMonthView* AddMonth(base::Time month_first_date,
@@ -108,9 +117,9 @@ class ASH_EXPORT CalendarView : public CalendarViewController::Observer,
 
   // The following is owned by `CalendarView`.
   views::ScrollView* scroll_view_ = nullptr;
-  views::Label* current_label_ = nullptr;
-  views::Label* previous_label_ = nullptr;
-  views::Label* next_label_ = nullptr;
+  views::View* current_label_ = nullptr;
+  views::View* previous_label_ = nullptr;
+  views::View* next_label_ = nullptr;
   CalendarMonthView* previous_month_ = nullptr;
   CalendarMonthView* current_month_ = nullptr;
   CalendarMonthView* next_month_ = nullptr;
