e241407341e58e3f805971734582230862748f3e
Ashley Prasad <ashleydp@google.com>
2021-09-20 18:59:09 +0000

    Diagnostics: Conditionally render network details
    - Hides network-info and ip-config-info-drawer when device is not in
      connected or online state.  In these states the fields for these
      elements would be empty except for MAC Address.
    - Updates tests to reflect expected UI visibility.
    
    Screenshot (WiFi Connected/Ethernet & Cellular Disconnected):
    http://shortn/_l9WwzdClGe
    
    Screenshot (Ethernet Connected/WiFi Online/Cellular Disconnected):
    http://shortn/_efg1dQgtk7
    
    Bug: 1197335
    Test: ./out/Default/browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I8affc167a0bafea47ba0fa452324245bb159ab56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169493
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923001}

--- chrome/test/data/webui/chromeos/diagnostics/network_card_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/network_card_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://diagnostics/network_card.js';
 
-import {fakeCellularNetwork, fakeDisconnectedEthernetNetwork, fakeDisconnectedWifiNetwork, fakeEthernetNetwork, fakeNetworkGuidInfoList, fakeWifiNetwork, fakeWifiNetworkDisabled} from 'chrome://diagnostics/fake_data.js';
+import {fakeCellularNetwork, fakeDisconnectedEthernetNetwork, fakeDisconnectedWifiNetwork, fakeEthernetNetwork, fakeNetworkGuidInfoList, fakePortalWifiNetwork, fakeWifiNetwork, fakeWifiNetworkDisabled} from 'chrome://diagnostics/fake_data.js';
 import {FakeNetworkHealthProvider} from 'chrome://diagnostics/fake_network_health_provider.js';
 import {setNetworkHealthProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
 
@@ -50,6 +50,7 @@ export function networkCardTestSuite() {
         'ethernetDisconnectedGuid', [fakeDisconnectedEthernetNetwork]);
     provider.setFakeNetworkState(
         'wifiDisconnectedGuid', [fakeDisconnectedWifiNetwork]);
+    provider.setFakeNetworkState('wifiPortalGuid', [fakePortalWifiNetwork]);
     // Add the network info to the DOM.
     networkCardElement = /** @type {!NetworkCardElement} */ (
         document.createElement('network-card'));
@@ -68,12 +69,45 @@ export function networkCardTestSuite() {
         networkCardElement.$$('#networkTroubleshooting'));
   }
 
+  /** @return {!Element} */
+  function getIpConfigDrawerElement() {
+    assertTrue(!!networkCardElement);
+
+    return /** @type {!Element} */ (
+        networkCardElement.$$('#ipConfigInfoDrawer'));
+  }
+
+  /** @return {!Element} */
+  function getNetworkInfoElement() {
+    assertTrue(!!networkCardElement);
+
+    return /** @type {!Element} */ (networkCardElement.$$('network-info'));
+  }
+
+  /** @return {!Element} */
+  function getEthernetInfoElement() {
+    const networkInfoElement = getNetworkInfoElement();
+    assertTrue(!!networkInfoElement);
+
+    return dx_utils.getEthernetInfoElement(networkInfoElement);
+  }
+
+  /** @return {!Element} */
+  function getWifiInfoElement() {
+    const networkInfoElement = getNetworkInfoElement();
+    assertTrue(!!networkInfoElement);
+
+    return dx_utils.getWifiInfoElement(networkInfoElement);
+  }
+
   test('CardTitleWiFiConnectedInitializedCorrectly', () => {
     return initializeNetworkCard('wifiGuid').then(() => {
       dx_utils.assertElementContainsText(
           networkCardElement.$$('#cardTitle'),
           'Wi-Fi [84:C5:A6:30:3F:31] (Connected)');
       assertFalse(isVisible(getTroubleConnectingElement()));
+      assertTrue(isVisible(getWifiInfoElement()));
+      assertTrue(isVisible(getIpConfigDrawerElement()));
     });
   });
 
@@ -83,6 +117,8 @@ export function networkCardTestSuite() {
           networkCardElement.$$('#cardTitle'),
           'Wi-Fi [84:C5:A6:30:3F:31] (Disabled)');
       assertTrue(isVisible(getTroubleConnectingElement()));
+      assertFalse(isVisible(getNetworkInfoElement()));
+      assertFalse(isVisible(getIpConfigDrawerElement()));
     });
   });
 
@@ -92,6 +128,19 @@ export function networkCardTestSuite() {
           networkCardElement.$$('#cardTitle'),
           'Wi-Fi [84:C5:A6:30:3F:31] (Not Connected)');
       assertTrue(isVisible(getTroubleConnectingElement()));
+      assertFalse(isVisible(getNetworkInfoElement()));
+      assertFalse(isVisible(getIpConfigDrawerElement()));
+    });
+  });
+
+  test('WifiPortalShowTroubleShooting', () => {
+    return initializeNetworkCard('wifiPortalGuid').then(() => {
+      dx_utils.assertElementContainsText(
+          networkCardElement.$$('#cardTitle'),
+          'Wi-Fi [84:C5:A6:30:3F:31] (Portal)');
+      assertTrue(isVisible(getTroubleConnectingElement()));
+      assertTrue(isVisible(getNetworkInfoElement()));
+      assertTrue(isVisible(getIpConfigDrawerElement()));
     });
   });
 
@@ -101,6 +150,7 @@ export function networkCardTestSuite() {
           networkCardElement.$$('#cardTitle'),
           'Ethernet [81:C5:A6:30:3F:31] (Online)');
       assertFalse(isVisible(getTroubleConnectingElement()));
+      assertTrue(isVisible(getEthernetInfoElement()));
     });
   });
 
@@ -110,6 +160,8 @@ export function networkCardTestSuite() {
           networkCardElement.$$('#cardTitle'),
           'Ethernet [81:C5:A6:30:3F:32] (Not Connected)');
       assertTrue(isVisible(getTroubleConnectingElement()));
+      assertFalse(isVisible(getNetworkInfoElement()));
+      assertFalse(isVisible(getIpConfigDrawerElement()));
     });
   });
 
