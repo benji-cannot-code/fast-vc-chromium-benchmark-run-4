7831c27e8a4836d4c2df407bd47072a0d1518eff
Ali Juma <ajuma@chromium.org>
2021-09-20 21:25:23 +0000

    [iOS] Build PartitionAlloc on iOS simulator
    This builds PartitionAlloc on iOS simulator and enables PartitionAlloc
    unit tests.
    
    Building and running on device is more complicated and left for another
    CL. Specifically, an extra entitlement is needed in order to use a
    large enough address space on iOS devices (otherwise, mmap-ing a large
    region, as PartitionAlloc does, fails).
    
    Change-Id: Ib2971206d3bb4bd5a42e9a249bb2c407989d58a7
    Bug: 1250788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163082
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923070}

--- base/allocator/partition_allocator/page_allocator_internals_posix.h
+++ base/allocator/partition_allocator/page_allocator_internals_posix.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MAP_ANONYMOUS MAP_ANON
 #endif
 
-#if defined(OS_APPLE)
+#if defined(OS_MAC)
 
 // SecTaskGetCodeSignStatus is marked as unavailable on macOS, although it’s
 // available on iOS and other Apple operating systems. It is, in fact, present
@@ -55,7 +55,7 @@ uint32_t SecTaskGetCodeSignStatus(SecTaskRef task)
     API_AVAILABLE(macos(10.12));
 #pragma clang diagnostic pop
 
-#endif  // OS_APPLE
+#endif  // OS_MAC
 
 namespace base {
 
@@ -85,7 +85,7 @@ const char* PageTagToName(PageTag tag) {
 }
 #endif  // defined(OS_ANDROID)
 
-#if defined(OS_APPLE)
+#if defined(OS_MAC)
 // Tests whether the version of macOS supports the MAP_JIT flag and if the
 // current process is signed with the hardened runtime and the allow-jit
 // entitlement, returning whether MAP_JIT should be used to allocate regions
@@ -132,7 +132,7 @@ bool UseMapJit() {
 
   return mac::CFCast<CFBooleanRef>(jit_entitlement.get()) == kCFBooleanTrue;
 }
-#endif  // defined(OS_APPLE)
+#endif  // defined(OS_MAC)
 
 }  // namespace
 
@@ -159,7 +159,7 @@ void* SystemAllocPagesInternal(void* hint,
   int access_flag = GetAccessFlags(accessibility);
   int map_flags = MAP_ANONYMOUS | MAP_PRIVATE;
 
-#if defined(OS_APPLE)
+#if defined(OS_MAC)
   // On macOS 10.14 and higher, executables that are code signed with the
   // "runtime" option cannot execute writable memory by default. They can opt
   // into this capability by specifying the "com.apple.security.cs.allow-jit"
