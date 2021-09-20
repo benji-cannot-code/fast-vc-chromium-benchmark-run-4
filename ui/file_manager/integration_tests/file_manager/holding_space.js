a544be2f925406aefd8b63b8e9cba1a7e1fa5599
Ben Reich <benreich@chromium.org>
2021-09-20 07:45:40 +0000

    [FilesBanner] Fix holding space tablet mode display incorrectly
    The holding space banner has different text based on whether it is
    displaying in tablet mode or not. When resizing the banner in tablet
    mode to 1/4 of the screen, the banner text was splitting lines
    incorrectly [1].
    
    THis removes some of the values hardcoded in to ensure the gear menu
    icon was in the middle and moves to more semantically correct
    vertical-align value instead. The results are pleasing [2].
    
    [1] https://screenshot.googleplex.com/6HEHGCBebEZmrWJ.png
    [2] https://screenshot.googleplex.com/9mHoQ9g5NS6LNbW.png
    
    Bug: 1228128
    Test: Compile chromeos-on-linux and visually inspect in tablet mode
    Change-Id: I2add45d3aa7af67d1521b07c9f2952c2e351d18a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3167918
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922852}

--- ui/file_manager/integration_tests/file_manager/holding_space.js
+++ ui/file_manager/integration_tests/file_manager/holding_space.js
@@ -161,7 +161,6 @@ testcase.holdingSpaceWelcomeBannerOnTabletModeChanged = async () => {
   let text = '.holding-space-welcome-text:not(.tablet-mode-enabled)';
   let textInTabletMode = '.holding-space-welcome-text.tablet-mode-enabled';
   let expectedTextDisplayValue = 'block';
-  let expectedTextInTabletModeDisplayValue = 'block';
   if (isBannersFrameworkEnabled) {
     await remoteCall.isolateBannerForTesting(
         appId, 'holding-space-welcome-banner');
@@ -174,7 +173,6 @@ testcase.holdingSpaceWelcomeBannerOnTabletModeChanged = async () => {
       'educational-banner > span[slot="subtitle"].tablet-mode-enabled'
     ];
     expectedTextDisplayValue = 'inline';
-    expectedTextInTabletModeDisplayValue = 'flex';
   }
   // Check: `text` should be displayed but `textInTabletMode` should not.
   await waitForElementWithDisplay(text, expectedTextDisplayValue);
@@ -187,8 +185,7 @@ testcase.holdingSpaceWelcomeBannerOnTabletModeChanged = async () => {
 
   // Check: `textInTabletMode` should be displayed but `text` should not.
   await waitForElementWithDisplay(text, 'none');
-  await waitForElementWithDisplay(
-      textInTabletMode, expectedTextInTabletModeDisplayValue);
+  await waitForElementWithDisplay(textInTabletMode, 'block');
 
   // Perform and check: disable tablet mode.
   await sendTestMessage({name: 'disableTabletMode'}).then((result) => {
