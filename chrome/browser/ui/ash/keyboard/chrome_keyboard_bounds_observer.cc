86be67f20655291ecfdeadc9e47843139975245f
Curtis McMullan <curtismcmullan@chromium.org>
2021-09-20 02:22:16 +0000

    chos: fix chrome keyboard bounds crash
    There's a number of crashes occuring at
    ChromeKeyboardBoundsObserver::OnKeyboardVisibleBoundsChanged(). See the
    linked bug for crash report links.
    
    It seems there are two points where we may encounter a nullptr, with the
    call to `hosts->GetNextHost()` or `host->GetView()`. Seeing as
    `host->GetNextHost()` nullptr value is handled in the while loop, then
    it leaves `host->GetView()` as the most likely culprit.
    
    Indeed reading the comment at the link below, it seems views returned by
    `RenderWidgetHost::GetView()` could be null.
    https://source.chromium.org/chromium/chromium/src/+/main:content/public/browser/render_widget_host.h;drc=00a8ca06f8f356e7e25ad9eac59364d51884af30;l=192
    
    This CL then adds a nullptr check for the views returned by
    `RenderWidgetHost::GetView()`.
    
    Bug: 1250063
    Change-Id: I7a4ec18863e1879e99060f12fe381f7b505f0f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3167632
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#922816}

--- chrome/browser/ui/ash/keyboard/chrome_keyboard_bounds_observer.cc
+++ chrome/browser/ui/ash/keyboard/chrome_keyboard_bounds_observer.cc
@@ -71,7 +71,8 @@ void ChromeKeyboardBoundsObserver::OnKeyboardVisibleBoundsChanged(
 
     while (content::RenderWidgetHost* host = hosts->GetNextHost()) {
       content::RenderWidgetHostView* view = host->GetView();
-      view->NotifyVirtualKeyboardOverlayRect(screen_bounds);
+      if (view)
+        view->NotifyVirtualKeyboardOverlayRect(screen_bounds);
     }
   }
 }
