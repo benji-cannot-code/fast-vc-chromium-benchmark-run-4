54e179f2b1460884812ba65be1db3b5b5e9589d9
Elaine Chien <elainechien@chromium.org>
2021-09-21 02:13:29 +0000

    Correctly track sequences for all blocks in AnimationBuilder::Observer
    After callbacks were moved to the AnimationBuilder the observer only
    observed the LayerAnimationSequences created from the animations in the
    first AnimationSequenceBlock. When callbacks were on the
    AnimationSequenceBlock the observer only observed the
    LayerAnimationSequences created in the AnimationSequenceBlock that
    called onXXX. Both of these behaviors are conceptually incorrect because
    these callbacks are meant to run on the entire animation (for each
    .Once() or .Repeatedly call). Therefore there were some sequences
    that weren't tracked by the observer. However, because every
    LayerAnimationSequence we create no matter which block it is in has the
    duration of the entire animation (synchronized with pause elements)
    the previous implementation effectively still had the correct behavior
    as two sequences of the same duration *should* start and end at the same
    time.
    
    This CL modifies AnimationBuilder such that the observer behaves as an
    observer for the whole animation. This CL stays consistent with the
    requirement that callbacks must be set before creating the first
    AnimationSequenceBlock. A call such as the one below will be DCHECKed.
    
    AnimationBuilder b;
    b.Once().SetOpacity(..);
    b.OnStarted(..);
    
    
    Bug: 1236069
    Change-Id: Ie9f07bc0f6160a7a8b0a3303135635435f6e7fcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3167301
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923185}

--- ui/views/animation/animation_builder_unittest.cc
+++ ui/views/animation/animation_builder_unittest.cc
@@ -336,7 +336,10 @@ TEST_F(AnimationBuilderTest, CheckStartEndCallbacks) {
         .SetOpacity(first_animating_view, 0.4f)
         .Offset(base::TimeDelta())
         .SetDuration(kDelay * 2)
-        .SetOpacity(second_animating_view, 0.9f);
+        .SetOpacity(second_animating_view, 0.9f)
+        .Then()
+        .SetDuration(kDelay)
+        .SetOpacity(second_animating_view, 0.4f);
   }
 
   // Only one Observer should have been created in the above block. Make sure
@@ -347,6 +350,8 @@ TEST_F(AnimationBuilderTest, CheckStartEndCallbacks) {
 
   EXPECT_TRUE(started);
   Step(kDelay * 2);
+  EXPECT_FALSE(ended);
+  Step(kDelay);
   EXPECT_TRUE(ended);
 }
 
