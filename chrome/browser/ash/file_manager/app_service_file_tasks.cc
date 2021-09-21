1e6872213fdd245ce6f2bb8d651f33e4bc6d821b
Peter Marshall <petermarshall@chromium.org>
2021-09-21 06:40:48 +0000

    [IntentHandling] Remove FindFileHandlerTasks
    Move some remaining functionality into the App Service implementation
    and move some tests over as well.
    
    Bug: 1240018
    Change-Id: I58b836965b1297ecacd102a4c2888b0ac9975f09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168581
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923264}

--- chrome/browser/ash/file_manager/app_service_file_tasks.cc
+++ chrome/browser/ash/file_manager/app_service_file_tasks.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/launch_utils.h"
+#include "chrome/browser/ash/crostini/crostini_features.h"
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
 #include "chrome/browser/ash/file_manager/filesystem_api_util.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
@@ -75,7 +76,22 @@ TaskType GetTaskType(apps::mojom::AppType app_type) {
   }
 }
 
+const char kImportCrostiniImageHandlerId[] = "import-crostini-image";
+const char kInstallLinuxPackageHandlerId[] = "install-linux-package";
+
 }  // namespace
+
+bool FileHandlerIsEnabled(Profile* profile,
+                          const std::string& file_handler_id) {
+  // Crostini deb files and backup files can be disabled by policy.
+  if (file_handler_id == kInstallLinuxPackageHandlerId) {
+    return crostini::CrostiniFeatures::Get()->IsRootAccessAllowed(profile);
+  }
+  if (file_handler_id == kImportCrostiniImageHandlerId) {
+    return crostini::CrostiniFeatures::Get()->IsExportImportUIAllowed(profile);
+  }
+  return true;
+}
 
 void FindAppServiceTasks(Profile* profile,
                          const std::vector<extensions::EntryInfo>& entries,
@@ -168,6 +184,9 @@ void FindAppServiceTasks(Profile* profile,
     if (app_type == apps::mojom::AppType::kExtension) {
       if (profile->IsOffTheRecord() &&
           !extensions::util::IsIncognitoEnabled(launch_entry.app_id, profile))
+        continue;
+      if (!FileHandlerIsEnabled(maybe_original_profile,
+                                launch_entry.activity_name))
         continue;
     }
 
