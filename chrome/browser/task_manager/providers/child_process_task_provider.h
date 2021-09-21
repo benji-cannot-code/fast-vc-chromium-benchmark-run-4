2c650cbbf51cc419742a30c1a053ba394aec96d1
John Abd-El-Malek <jam@chromium.org>
2021-09-21 23:49:59 +0000

    Simplify task_manager code now that ProcessHostOnUI is the only path.
    Bug: 904556
    Change-Id: Ib8a2407f33ef7ac735148aa09f2f5e630ef28758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171458
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923645}

--- chrome/browser/task_manager/providers/child_process_task_provider.h
+++ chrome/browser/task_manager/providers/child_process_task_provider.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
-#include "base/memory/weak_ptr.h"
 #include "chrome/browser/task_manager/providers/task_provider.h"
 #include "content/public/browser/browser_child_process_observer.h"
 
@@ -50,13 +49,6 @@ class ChildProcessTaskProvider
   void StartUpdating() override;
   void StopUpdating() override;
 
-  // The pre-existing child processes data will be collected on the IO thread.
-  // When that is done, we will be notified on the UI thread by receiving a call
-  // to this method.
-  void ChildProcessDataCollected(
-      std::unique_ptr<const std::vector<content::ChildProcessData>>
-          child_processes);
-
   // Creates a ChildProcessTask from the given |data| and notifies the observer
   // of its addition.
   void CreateTask(const content::ChildProcessData& data);
@@ -75,10 +67,6 @@ class ChildProcessTaskProvider
 
   // A map to track ChildProcessTask's by their child process unique ids.
   base::flat_map<int, ChildProcessTask*> tasks_by_child_id_;
-
-  // Always keep this the last member of this class to make sure it's the
-  // first thing to be destructed.
-  base::WeakPtrFactory<ChildProcessTaskProvider> weak_ptr_factory_{this};
 };
 
 }  // namespace task_manager
