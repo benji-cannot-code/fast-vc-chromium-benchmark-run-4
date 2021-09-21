83225b4b1a94d7eb75da3d96b7094843a15276a0
Moe Ahmadi <mahmadi@chromium.org>
2021-09-21 03:02:36 +0000

    [omnibox] Rename and refactor Autocomplete methods for setting AQS param
    This change improves readability, simplifies the code, and likely
    improves performance by not creating the AQS param in the match
    construction phase. The final match URL with the AQS param is created
    once the match is selected in the following locations:
    OmniboxEditModel::OpenMatch()
    RealboxHandler::OpenAutocompleteMatch()
    
    Bug: 1247846
    Change-Id: Ied270d603e2efa5ed650c2174e3537a856643fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162828
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923205}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteController.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteController.java
@@ -275,10 +275,12 @@ public class AutocompleteController {
         if (!mAutocompleteResult.verifyCoherency(selectedIndex, VerificationPoint.UPDATE_MATCH)) {
             return null;
         }
-        return AutocompleteControllerJni.get().updateMatchDestinationURLWithQueryFormulationTime(
-                mNativeController, selectedIndex, elapsedTimeSinceInputChange, newQueryText,
-                newQueryParams == null ? null
-                                       : newQueryParams.toArray(new String[newQueryParams.size()]));
+        return AutocompleteControllerJni.get()
+                .updateMatchDestinationURLWithAdditionalAssistedQueryStats(mNativeController,
+                        selectedIndex, elapsedTimeSinceInputChange, newQueryText,
+                        newQueryParams == null
+                                ? null
+                                : newQueryParams.toArray(new String[newQueryParams.size()]));
     }
 
     /**
@@ -324,7 +326,7 @@ public class AutocompleteController {
         void onOmniboxFocused(long nativeAutocompleteControllerAndroid, String omniboxText,
                 String currentUrl, int pageClassification, String currentTitle);
         void deleteSuggestion(long nativeAutocompleteControllerAndroid, int selectedIndex);
-        GURL updateMatchDestinationURLWithQueryFormulationTime(
+        GURL updateMatchDestinationURLWithAdditionalAssistedQueryStats(
                 long nativeAutocompleteControllerAndroid, int selectedIndex,
                 long elapsedTimeSinceInputChange, String newQueryText, String[] newQueryParams);
         Tab findMatchingTabWithUrl(long nativeAutocompleteControllerAndroid, GURL url);
