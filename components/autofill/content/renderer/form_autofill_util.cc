cc7fe5f5acb84ca6a0ca2ceae484cb2cecac339d
Eva <evih@google.com>
2021-09-21 17:45:24 +0000

    [Autofill] Delete AreFormContentsVisible() and IsSomeControlElementVisible()
    The IsSomeControlElementVisible() can be simplified to a one line
    function call, therefore, we replace it on the callsites.
    The AreFormContentsVisible() just calls the
    IsSomeControlElementVisible() function on the fields of the form that's
    passed as a parameter. As the AreFormContentsVisible name is misleading
    and again the function can be replaced with a one line function call, we
    replace it on the callsites.
    
    Bug: 1007974
    Change-Id: Ic56512cde9124d50f53f06683eb99a36bf988718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172613
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Eva Herencsárová <evih@google.com>
    Cr-Commit-Position: refs/heads/main@{#923475}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -1755,15 +1755,6 @@ bool ExtractFormData(const WebFormElement& form_element,
 }
 
 bool IsSomeControlElementVisible(
-    const WebVector<WebFormControlElement>& control_elements) {
-  for (const WebFormControlElement& control_element : control_elements) {
-    if (IsWebElementVisible(control_element))
-      return true;
-  }
-  return false;
-}
-
-bool IsSomeControlElementVisible(
     blink::WebLocalFrame* frame,
     const std::set<FieldRendererId>& control_elements) {
   SCOPED_UMA_HISTOGRAM_TIMER_MICROS(
@@ -1813,10 +1804,6 @@ bool IsSomeControlElementVisible(
     }
     return false;
   }
-}
-
-bool AreFormContentsVisible(const WebFormElement& form) {
-  return IsSomeControlElementVisible(form.GetFormControlElements());
 }
 
 GURL GetCanonicalActionForForm(const WebFormElement& form) {
