a6e629268c1029cdae419aca6fc85464ae1d9d12
Cattalyya Nuengsigkapian <cattalyya@chromium.org>
2021-09-21 02:40:42 +0000

    vsplit: snap_state: Add Vertical Snap to chrome://flags
    Bug: 1250548
    Test: Manual
    Change-Id: I14fff2ecd929ca229bf248ba4e5793e1c2e9fdfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3167531
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Cattalyya Nuengsigkapian <cattalyya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923199}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1525,6 +1525,9 @@ extern const char kUseSearchClickForRightClickDescription[];
 extern const char kV8VmFutureName[];
 extern const char kV8VmFutureDescription[];
 
+extern const char kVerticalSnapName[];
+extern const char kVerticalSnapDescription[];
+
 extern const char kWalletServiceUseSandboxName[];
 extern const char kWalletServiceUseSandboxDescription[];
 
