452ad1100ed937be8440f68eecd5e9f73f0d91c2
Dan Harrington <harringtond@chromium.org>
2021-09-21 19:51:47 +0000

    webfeed: add histogram for content order
    Adds the
    ContentSuggestions.Feed.ContentOrderOnRefresh.WebFeed
    histogram.
    
    Bug: b/199960490
    Change-Id: I20e59184c05274822fc162bd846396999dbdd880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163390
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923529}

--- components/feed/core/v2/api_test/feed_api_stream_unittest.cc
+++ components/feed/core/v2/api_test/feed_api_stream_unittest.cc
@@ -2656,6 +2656,7 @@ TEST_F(FeedApiTest, ForYouContentOrderUnset) {
 }
 
 TEST_F(FeedApiTest, ContentOrderIsGroupedByDefault) {
+  base::HistogramTester histograms;
   network_.InjectListWebFeedsResponse({MakeWireWebFeed("cats")});
   response_translator_.InjectResponse(MakeTypicalInitialModelState());
   TestWebFeedSurface surface(stream_.get());
@@ -2665,6 +2666,9 @@ TEST_F(FeedApiTest, ContentOrderIsGroupedByDefault) {
   EXPECT_EQ(
       feedwire::FeedQuery::ContentOrder::FeedQuery_ContentOrder_GROUPED,
       network_.query_request_sent->feed_request().feed_query().order_by());
+  histograms.ExpectUniqueSample(
+      "ContentSuggestions.Feed.WebFeed.RefreshContentOrder",
+      ContentOrder::kGrouped, 1);
 }
 
 TEST_F(FeedApiTest, SetContentOrderReloadsContent) {
@@ -2673,6 +2677,7 @@ TEST_F(FeedApiTest, SetContentOrderReloadsContent) {
   TestWebFeedSurface surface(stream_.get());
   WaitForIdleTaskQueue();
 
+  base::HistogramTester histograms;
   response_translator_.InjectResponse(MakeTypicalInitialModelState());
   stream_->SetContentOrder(kWebFeedStream, ContentOrder::kReverseChron);
   WaitForIdleTaskQueue();
@@ -2684,6 +2689,9 @@ TEST_F(FeedApiTest, SetContentOrderReloadsContent) {
       network_.query_request_sent->feed_request().feed_query().order_by());
   EXPECT_EQ(ContentOrder::kReverseChron,
             stream_->GetContentOrder(kWebFeedStream));
+  histograms.ExpectUniqueSample(
+      "ContentSuggestions.Feed.WebFeed.RefreshContentOrder",
+      ContentOrder::kReverseChron, 1);
 }
 
 TEST_F(FeedApiTest, SetContentOrderIsSavedeNotRefreshedIfUnchanged) {
