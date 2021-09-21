08966197eed41226d2f1eadb4feabb93965bea3c
Liquan (Max) Gu <maxlg@chromium.org>
2021-09-21 18:27:30 +0000

    [BasicCardDeprecation] Warn the deprecation in console
    This CL adds the basic-card deprecation to the Reporting API. It has
    the following effects:
    * the Reporting API will be able to capture a web page's use of
      basic-card and generate a report for the Reporting API.
    * the console will give a warning to the console when a web page uses
      basic-card, no matter whether the page calls Reporting API or not.
      The console warning will read: "[Deprecation] The 'basic-card'
      payment method is deprecated and will be removed in M100, around
      March 2022. See
      https://www.chromestatus.com/features/5730051011117056 for more
      details."
    
    Bug: 1250135
    Change-Id: I25284d60cacc0623c9903a49d22c24e21a260602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169471
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923488}

--- third_party/blink/renderer/modules/payments/payment_request.cc
+++ third_party/blink/renderer/modules/payments/payment_request.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/event_queue.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
+#include "third_party/blink/renderer/core/frame/deprecation.h"
 #include "third_party/blink/renderer/core/frame/frame_owner.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -442,6 +443,8 @@ void StringifyAndParseMethodSpecificData(ExecutionContext& execution_context,
   if (RuntimeEnabledFeatures::PaymentRequestBasicCardEnabled(
           &execution_context) &&
       supported_method == "basic-card") {
+    Deprecation::CountDeprecation(&execution_context,
+                                  WebFeature::kPaymentRequestBasicCard);
     BasicCardHelper::ParseBasiccardData(input, output->supported_networks,
                                         exception_state);
   } else if (supported_method == kSecurePaymentConfirmationMethod &&
