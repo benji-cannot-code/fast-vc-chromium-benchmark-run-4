f943463e5735fa0addd0041246ad5bcb90cadd32
Zentaro Kavanagh <zentaro@chromium.org>
2021-09-21 22:22:19 +0000

    Session Log: Refactor RoutineLog to use AsyncLog
    - Reuse functionality from AsyncLog
    
    Bug: 1125150
    Test: ash_webui_unittests
    Change-Id: Idc591b36187eef441bd4e25ec68d5169fbd03a87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171272
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923612}

--- ash/webui/diagnostics_ui/backend/routine_log.h
+++ ash/webui/diagnostics_ui/backend/routine_log.h
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "ash/webui/diagnostics_ui/backend/async_log.h"
 #include "ash/webui/diagnostics_ui/mojom/system_routine_controller.mojom.h"
-#include "base/files/file_path.h"
-#include "base/sequenced_task_runner.h"
+
+namespace base {
+class FilePath;
+}
 
 namespace ash {
 namespace diagnostics {
@@ -39,18 +42,7 @@ class RoutineLog {
   std::string GetContents() const;
 
  private:
-  // Performs the file operations to ensure the log file exists and appends
-  // provided content.
-  // Expectation is this will be called from a thread which allows blocking.
-  void LogContentToFile(const std::string& content);
-
-  void AppendToLog(const std::string& content);
-
-  void CreateFile();
-
-  const base::FilePath routine_log_file_path_;
-  // Blockable task runner to enable I/O operations.
-  scoped_refptr<base::SequencedTaskRunner> sequenced_task_runner_;
+  AsyncLog log_;
 };
 
 }  // namespace diagnostics
