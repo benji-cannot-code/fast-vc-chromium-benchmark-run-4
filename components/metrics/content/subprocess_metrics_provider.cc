5d7713be63b0eceacab2dc8f72661055de255dd7
John Abd-El-Malek <jam@chromium.org>
2021-09-21 20:39:24 +0000

    Simplify SubprocessMetricsProvider now that ProcessHostOnUI is the only path.
    Bug: 904556
    Change-Id: I130d194fb8f85e186d8b1c8e261c61747a543895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173785
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923564}

--- components/metrics/content/subprocess_metrics_provider.cc
+++ components/metrics/content/subprocess_metrics_provider.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/bind.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_base.h"
 #include "base/metrics/histogram_macros.h"
@@ -15,10 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/persistent_memory_allocator.h"
 #include "components/metrics/metrics_service.h"
 #include "content/public/browser/browser_child_process_host.h"
-#include "content/public/browser/browser_task_traits.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/child_process_data.h"
-#include "content/public/common/content_features.h"
 
 namespace metrics {
 namespace {
@@ -127,22 +123,21 @@ void SubprocessMetricsProvider::BrowserChildProcessLaunchedAndConnected(
     const content::ChildProcessData& data) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
-  if (base::FeatureList::IsEnabled(features::kProcessHostOnUI)) {
-    RegisterSubprocessAllocator(
-        data.id, GetSubprocessHistogramAllocatorOnProcessThread(data.id));
-  } else {
-    // It's necessary to access the BrowserChildProcessHost object that is
-    // managing the child in order to extract the metrics memory from it.
-    // Unfortunately, the required lookup can only be performed on the IO
-    // thread so do the necessary dance.
-    content::GetIOThreadTaskRunner({})->PostTaskAndReplyWithResult(
-        FROM_HERE,
-        base::BindOnce(&SubprocessMetricsProvider::
-                           GetSubprocessHistogramAllocatorOnProcessThread,
-                       data.id),
-        base::BindOnce(&SubprocessMetricsProvider::RegisterSubprocessAllocator,
-                       weak_ptr_factory_.GetWeakPtr(), data.id));
-  }
+  // See if the new process has a memory allocator and take control of it if so.
+  // This call can only be made on the browser's IO thread.
+  content::BrowserChildProcessHost* host =
+      content::BrowserChildProcessHost::FromID(data.id);
+  if (!host)
+    return;
+
+  std::unique_ptr<base::PersistentMemoryAllocator> allocator =
+      host->TakeMetricsAllocator();
+  if (!allocator)
+    return;
+
+  RegisterSubprocessAllocator(
+      data.id, std::make_unique<base::PersistentHistogramAllocator>(
+                   std::move(allocator)));
 }
 
 void SubprocessMetricsProvider::BrowserChildProcessHostDisconnected(
@@ -206,26 +201,6 @@ void SubprocessMetricsProvider::RenderProcessHostDestroyed(
 
   DeregisterSubprocessAllocator(host->GetID());
   scoped_observations_.RemoveObservation(host);
-}
-
-// static
-std::unique_ptr<base::PersistentHistogramAllocator>
-SubprocessMetricsProvider::GetSubprocessHistogramAllocatorOnProcessThread(
-    int id) {
-  // See if the new process has a memory allocator and take control of it if so.
-  // This call can only be made on the browser's IO thread.
-  content::BrowserChildProcessHost* host =
-      content::BrowserChildProcessHost::FromID(id);
-  if (!host)
-    return nullptr;
-
-  std::unique_ptr<base::PersistentMemoryAllocator> allocator =
-      host->TakeMetricsAllocator();
-  if (!allocator)
-    return nullptr;
-
-  return std::make_unique<base::PersistentHistogramAllocator>(
-      std::move(allocator));
 }
 
 }  // namespace metrics
