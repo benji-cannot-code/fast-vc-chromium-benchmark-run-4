9fe5c476daccac3716bc30e0251d9fbc10b3bc98
Timothy Gu <timothygu@chromium.org>
2021-09-21 05:28:48 +0000

    url: more spec-compliant file: URL host handling
    Currently on Windows, the file: URL parser removes any host if the path
    begins with a Windows drive letter:
    
        GURL(file://localhost/c:/hello.txt) = file:///C:/hello.txt
        GURL(file://otherhost/c:/hello.txt) = file:///C:/hello.txt
    
    However, the host-removal step functions inconsistently, and is
    generally not activated upon a ReplaceComponents call. This means that
    GURL::ReplaceComponents does not roundtrip properly:
    
        GURL(file://localhost/foo.txt)
            .ReplaceComponents(PATH, /c:/hello.txt)
                = file://localhost/C:/hello.txt
    
    ~~~~~~
    
    This CL provides two fixes. First, it moves the host-stripping code from
    the file URL parser (url_parse_file.cc) to the canonicalizer
    (url_canon_fileurl.cc). The canonicalizer is run after any parsing/
    replacing step, and so this fix resolves the consistency issue
    identified earlier. This fixes bug 1214098. Correspondingly, the work-
    around added in Ib92f4bd503766fadc1dd80fd31d94714c174f14a is removed.
    
    Second, it changes the conditions required for removing host from
    
          Windows-only,    any host,    path has drive letter
                                  to
             any OS,    localhost-only, path has drive letter
    
    This is a step towards compliance with the WHATWG URL Standard, and
    partially fixes bug 688961. For reference, the spec and Safari do host
    removal with:
             any OS,    localhost-only,       any path
    while Firefox uses:
             any OS,       any host,          any path
    
    If this CL is backed out, a less ambitious attempt is
    https://crrev.com/c/3088261; it only includes the first fix mentioned
    here.
    
    Bug: 688961, 1214098
    Change-Id: I41057fa0766762557c0b9dd8d772969648d51d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3088038
    Commit-Queue: Timothy Gu <timothygu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923244}

--- url/url_file.h
+++ url/url_file.h
@@ -14,14 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace url {
 
-#ifdef WIN32
-
 // We allow both "c:" and "c|" as drive identifiers.
 inline bool IsWindowsDriveSeparator(char16_t ch) {
   return ch == ':' || ch == '|';
 }
-
-#endif  // WIN32
 
 // Returns the index of the next slash in the input after the given index, or
 // spec_len if the end of the input is reached.
@@ -32,8 +28,6 @@ inline int FindNextSlash(const CHAR* spec, int begin_index, int spec_len) {
     idx++;
   return idx;
 }
-
-#ifdef WIN32
 
 // DoesContainWindowsDriveSpecUntil returns the least number between
 // start_offset and max_offset such that the spec has a valid drive
@@ -74,6 +68,8 @@ inline bool DoesBeginWindowsDriveSpec(const CHAR* spec,
   return DoesContainWindowsDriveSpecUntil(spec, start_offset, start_offset,
                                           spec_len) == start_offset;
 }
+
+#ifdef WIN32
 
 // Returns true if the start_offset in the given text looks like it begins a
 // UNC path, for example "\\". This function explicitly handles start_offset
