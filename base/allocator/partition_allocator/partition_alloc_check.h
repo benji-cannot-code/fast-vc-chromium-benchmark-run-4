2e45daefd7de59d95ceaafd03f145dd2ca15f24c
Anton Bikineev <bikineev@chromium.org>
2021-09-21 17:00:07 +0000

    *Scan: Make expensive *Scan dchecks no-ops on non-debug
    *Scan (especially with instrumentation enabled) can cause slowdowns and
    timeouts on try-bots. This CL enables *Scan dchecks only on debug
    builds.
    
    Bug: 1247061
    Change-Id: I0d74decb97fe353b70291f84b0544979bb62a918
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3148413
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923447}

--- base/allocator/partition_allocator/partition_alloc_check.h
+++ base/allocator/partition_allocator/partition_alloc_check.h
@@ -60,6 +60,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PA_PCHECK(condition) PCHECK(condition)
 #endif  // BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 
+// Expensive dchecks that run within *Scan. These checks are only enabled in
+// debug builds with dchecks enabled.
+#if !defined(NDEBUG)
+#define PA_SCAN_DCHECK_IS_ON() DCHECK_IS_ON()
+#else
+#define PA_SCAN_DCHECK_IS_ON() 0
+#endif
+
+#if PA_SCAN_DCHECK_IS_ON()
+#define PA_SCAN_DCHECK(expr) PA_DCHECK(expr)
+#else
+#define PA_SCAN_DCHECK(expr) EAT_CHECK_STREAM_PARAMS(!(expr))
+#endif
+
 #if defined(PAGE_ALLOCATOR_CONSTANTS_ARE_CONSTEXPR)
 
 // Use this macro to assert on things that are conditionally constexpr as
