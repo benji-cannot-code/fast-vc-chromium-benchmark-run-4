ab365d1024b2d541fe25efc13ba1afc42c827932
Tomasz Moniuszko <tmoniuszko@opera.com>
2021-09-21 16:45:35 +0000

    Forward declare BrowserContext in content namespace
    Change-Id: I075e81e5c3f81edfa52287ddcbdf08c963a1946f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172511
    Auto-Submit: Tomasz Moniuszko <tmoniuszko@opera.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923439}

--- chrome/browser/extensions/api/web_authentication_proxy/web_authentication_proxy_service.h
+++ chrome/browser/extensions/api/web_authentication_proxy/web_authentication_proxy_service.h
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/web_authentication_request_proxy.h"
 
+namespace content {
 class BrowserContext;
+}  // namespace content
 
 namespace extensions {
 
