6e4b980258797e1ce66cc6c1f045c147a7bd195f
Shimi Zhang <ctzsm@chromium.org>
2021-09-21 00:26:37 +0000

    [Android] Enable by default for Swipe To Move Cursor
    The experiment was pushed to 100% already, M95 is the last milestone
    to enable the feature via finch.
    
    Bug: 1126778
    Change-Id: Ifa2fa467d1fd402c11813be5b59a9a22dc7eacdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3163021
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923151}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2063,9 +2063,6 @@ extern const char kVoiceButtonInTopToolbarDescription[];
 extern const char kInlineUpdateFlowName[];
 extern const char kInlineUpdateFlowDescription[];
 
-extern const char kSwipeToMoveCursorName[];
-extern const char kSwipeToMoveCursorDescription[];
-
 extern const char kWalletRequiresFirstSyncSetupCompleteName[];
 extern const char kWalletRequiresFirstSyncSetupCompleteDescription[];
 
