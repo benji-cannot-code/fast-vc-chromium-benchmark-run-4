72edca47c80aaf0d4aa937889660305320b5f6c1
Alexey Baskakov <loyso@chromium.org>
2021-09-21 05:00:55 +0000

    Sheriff: Disable AddAnimationWhileAnimating and MultipleAnimations tests.
    Disable
    TotalAnimationThroughputReporterTest.AddAnimationWhileAnimating
    TotalAnimationThroughputReporterTest.MultipleAnimations
    
    Flaky on Win and other OSes.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64%20(dbg)/25075/overview
    
    This reverts 3 CLs:
    https://chromium-review.googlesource.com/c/chromium/src/+/3089800
    https://chromium-review.googlesource.com/c/chromium/src/+/3089396
    https://chromium-review.googlesource.com/c/chromium/src/+/3083164
    
    Bug: 1217783, 1216715
    Change-Id: Ied0abbd326119f7a55f01193fec3e6e40767ec83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172054
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Owners-Override: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923238}

--- ui/compositor/total_animation_throughput_reporter_unittest.cc
+++ ui/compositor/total_animation_throughput_reporter_unittest.cc
@@ -24,24 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
     defined(THREAD_SANITIZER) || defined(LEAK_SANITIZER) ||    \
     defined(UNDEFINED_SANITIZER)
-#define SANITIZER_ENABLED 1
-#endif
-
-// TODO(crbug.com/1217783, crbug.com/1216715): Time check is flaky on fuchia and
-// lacros, linux ozone and sanitizer build.
-#if defined(OS_FUCHSIA) || defined(OS_LINUX) ||                    \
-    BUILDFLAG(IS_CHROMEOS_LACROS) || defined(ADDRESS_SANITIZER) || \
-    defined(MEMORY_SANITIZER) || defined(THREAD_SANITIZER) ||      \
-    defined(LEAK_SANITIZER) || defined(UNDEFINED_SANITIZER)
-#define TIME_CHECK_ENABLED 0
-#else
-#define TIME_CHECK_ENABLED 1
+#define SANITIZER_ENABLED
 #endif
 
 namespace ui {
 namespace {
 
-#if TIME_CHECK_ENABLED
+#if !defined(SANITIZER_ENABLED)
 // Returns the delta from current time to the (start + duration) time.
 // This is used to compute how long it should wait from now to reach
 // the `start + duration` time.
@@ -99,7 +88,8 @@ TEST_F(TotalAnimationThroughputReporterTest, StopAnimation) {
 }
 
 // Tests the longest animation will trigger the report.
-TEST_F(TotalAnimationThroughputReporterTest, MultipleAnimations) {
+// TODO(crbug.com/1217783): Test is flaky.
+TEST_F(TotalAnimationThroughputReporterTest, DISABLED_MultipleAnimations) {
   Layer layer1;
   layer1.SetOpacity(0.5f);
   root_layer()->Add(&layer1);
@@ -125,7 +115,7 @@ TEST_F(TotalAnimationThroughputReporterTest, MultipleAnimations) {
     settings.SetTransitionDuration(base::TimeDelta::FromMilliseconds(96));
     layer2.SetOpacity(1.0f);
   }
-#if TIME_CHECK_ENABLED
+#if !defined(SANITIZER_ENABLED)
   auto start = base::TimeTicks::Now();
 #endif
   Advance(base::TimeDelta::FromMilliseconds(32));
@@ -133,7 +123,7 @@ TEST_F(TotalAnimationThroughputReporterTest, MultipleAnimations) {
 
   // The following check may fail on sanitizer builds which
   // runs slwer.
-#if TIME_CHECK_ENABLED
+#if !defined(SANITIZER_ENABLED)
   auto sixty_four_ms_from_start = DeltaFromNowToTarget(start, 64);
   ASSERT_TRUE(sixty_four_ms_from_start > base::TimeDelta());
   Advance(sixty_four_ms_from_start);
@@ -173,7 +163,9 @@ TEST_F(TotalAnimationThroughputReporterTest, MultipleAnimationsOnSingleLayer) {
 }
 
 // Tests adding new animation will extends the duration.
-TEST_F(TotalAnimationThroughputReporterTest, AddAnimationWhileAnimating) {
+// TODO(crbug.com/1216715): Test is flaky.
+TEST_F(TotalAnimationThroughputReporterTest,
+       DISABLED_AddAnimationWhileAnimating) {
   Layer layer1;
   layer1.SetOpacity(0.5f);
   root_layer()->Add(&layer1);
@@ -188,7 +180,7 @@ TEST_F(TotalAnimationThroughputReporterTest, AddAnimationWhileAnimating) {
     settings.SetTransitionDuration(base::TimeDelta::FromMilliseconds(48));
     layer1.SetOpacity(1.0f);
   }
-#if TIME_CHECK_ENABLED
+#if !defined(SANITIZER_ENABLED)
   base::TimeTicks start = base::TimeTicks::Now();
 #endif
   Advance(base::TimeDelta::FromMilliseconds(32));
@@ -209,7 +201,7 @@ TEST_F(TotalAnimationThroughputReporterTest, AddAnimationWhileAnimating) {
 
   // The following check may fail on sanitizer builds which
   // runs slwer.
-#if TIME_CHECK_ENABLED
+#if !defined(SANITIZER_ENABLED)
   // The animation time is extended by 32ms.
   auto sixty_four_ms_from_start = DeltaFromNowToTarget(start, 64);
   ASSERT_TRUE(sixty_four_ms_from_start > base::TimeDelta());
