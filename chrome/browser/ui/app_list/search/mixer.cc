f986ba7ed440c5b18ad5eff97e0d03151246cb8d
Rachel Wong <wrong@chromium.org>
2021-09-21 02:50:40 +0000

    [suggested files] Gate suggested Drive files behind a different flag.
    Suggested Drive files are now gated behind a different flag
    (enable-suggested-drive-files) to distinguish it from suggested local
    files (enable-suggested-files).
    
    The CL also extends the expiry milestone for the flag.
    
    Bug: 1237317
    Change-Id: I64f8d4c1a27971cfe2e0ff41a14a2546277b5bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3162683
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923201}

--- chrome/browser/ui/app_list/search/mixer.cc
+++ chrome/browser/ui/app_list/search/mixer.cc
@@ -126,7 +126,8 @@ void Mixer::InitializeRankers(Profile* profile) {
   search_result_ranker_ = std::make_unique<SearchResultRanker>(profile);
   search_result_ranker_->InitializeRankers(search_controller_);
 
-  if (app_list_features::IsSuggestedFilesEnabled()) {
+  if (app_list_features::IsSuggestedFilesEnabled() ||
+      app_list_features::IsSuggestedDriveFilesEnabled()) {
     chip_ranker_ = std::make_unique<ChipRanker>(profile);
   }
 }
