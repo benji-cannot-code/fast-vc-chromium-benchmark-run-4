3370a7bab4ff67a1522a63d6816a69af0f560b4c
Gauthier Ambard <gambard@chromium.org>
2021-09-21 16:07:55 +0000

    [iOS] Extract Context Menu handling out of BVC
    This is a refactoring to clean up BVC.
    
    Bug: 1250689
    Change-Id: I2937fd85be930640f2161631f92e8b732b5fa344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168597
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923426}

---
+++ ios/chrome/browser/ui/context_menu/context_menu_configuration_provider.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_CONTEXT_MENU_CONTEXT_MENU_CONFIGURATION_PROVIDER_H_
+#define IOS_CHROME_BROWSER_UI_CONTEXT_MENU_CONTEXT_MENU_CONFIGURATION_PROVIDER_H_
+
+#import <UIKit/UIKit.h>
+
+namespace web {
+struct ContextMenuParams;
+class WebState;
+}
+
+class Browser;
+
+// Object creating the configuration (action items...) for the context menu.
+@interface ContextMenuConfigurationProvider : NSObject
+
+// Instantiates with a |browser|.
+- (instancetype)initWithBrowser:(Browser*)browser;
+
+// Returns a configuration for a context menu, based on its associated
+// |webState|, |params| and |baseViewController|.
+- (UIContextMenuConfiguration*)
+    contextMenuConfigurationForWebState:(web::WebState*)webState
+                                 params:(const web::ContextMenuParams&)params
+                     baseViewController:(UIViewController*)baseViewController;
+
+// DEPRECATED.
+// Displays a context menu using an action sheet on |baseViewController|.
+- (void)showLegacyContextMenuForWebState:(web::WebState*)webState
+                                  params:(const web::ContextMenuParams&)params
+                      baseViewController:(UIViewController*)baseViewController;
+
+// DEPRECATED.
+// Dismisses the context menu shown above.
+- (void)dismissLegacyContextMenu;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_CONTEXT_MENU_CONTEXT_MENU_CONFIGURATION_PROVIDER_H_
