f1277db750e4ccfb0bbe4d09d91149b265438ecf
Ari Chivukula <arichiv@chromium.org>
2021-09-21 00:16:26 +0000

    [DOM Storage + StorageKey] (1) Refactor session and local storage checks
    This CL consolidates the checks performed for session and local storage
    IPC in dom_storage_context_wrapper. This enables unit testing in
    in addition to browser testing, and cuts lines from the already
    expansive storage_partition_impl. It'll be easier to add new checks
    and prevent divergence between callsites now.
    
    The prior attempt at this change (https://crrev.com/c/3122751). Took a
    quick and dirty approach which required a revert. This code is on a
    critical path, has a lot of quick fixes layered, and is under-tested.
    Let's take a slower approach as follows:
    (1) Refactor session and local storage checks
    (2) Add StorageKey to IPC
    (3) Add LocalFrameToken to IPC and verify
    
    Bug: 1212808
    Change-Id: I7d8cd8d0c78d5f79855b983a08573a48812d6207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3165851
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923144}

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/sequenced_task_runner.h"
 #include "base/single_thread_task_runner.h"
-#include "base/syslog_logging.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/sequence_local_storage_slot.h"
 #include "base/time/default_clock.h"
@@ -1689,17 +1688,14 @@ void StoragePartitionImpl::OpenLocalStorage(
     const url::Origin& origin,
     mojo::PendingReceiver<blink::mojom::StorageArea> receiver) {
   DCHECK(initialized_);
-  const auto& security_policy_handle = dom_storage_receivers_.current_context();
-  if (!security_policy_handle->CanAccessDataForOrigin(origin)) {
-    SYSLOG(WARNING) << "Killing renderer: illegal localStorage request.";
-    dom_storage_receivers_.ReportBadMessage(
-        "Access denied for localStorage request");
-    return;
-  }
+  ChildProcessSecurityPolicyImpl::Handle security_policy_handle =
+      dom_storage_receivers_.current_context()->Duplicate();
   dom_storage_context_->OpenLocalStorage(
       // TODO(https://crbug.com/1199077): Pass the real StorageKey
       // when StoragePartitionImpl is converted.
-      blink::StorageKey(origin), std::move(receiver));
+      blink::StorageKey(origin), std::move(receiver),
+      std::move(security_policy_handle),
+      dom_storage_receivers_.GetBadMessageCallback());
 }
 
 void StoragePartitionImpl::BindSessionStorageNamespace(
@@ -1719,11 +1715,11 @@ void StoragePartitionImpl::BindSessionStorageArea(
   ChildProcessSecurityPolicyImpl::Handle security_policy_handle =
       dom_storage_receivers_.current_context()->Duplicate();
   dom_storage_context_->BindStorageArea(
-      std::move(security_policy_handle),
       // TODO(https://crbug.com/1199077): Pass the real StorageKey
       // when StoragePartitionImpl is converted.
-      blink::StorageKey(origin), namespace_id,
-      dom_storage_receivers_.GetBadMessageCallback(), std::move(receiver));
+      blink::StorageKey(origin), namespace_id, std::move(receiver),
+      std::move(security_policy_handle),
+      dom_storage_receivers_.GetBadMessageCallback());
 }
 
 void StoragePartitionImpl::OnAuthRequired(
@@ -2830,10 +2826,10 @@ void StoragePartitionImpl::OpenLocalStorageForProcess(
     const blink::StorageKey& storage_key,
     mojo::PendingReceiver<blink::mojom::StorageArea> receiver) {
   DCHECK(initialized_);
-  DCHECK(ChildProcessSecurityPolicyImpl::GetInstance()
-             ->CreateHandle(process_id)
-             .CanAccessDataForOrigin(storage_key.origin()));
-  dom_storage_context_->OpenLocalStorage(storage_key, std::move(receiver));
+  auto handle =
+      ChildProcessSecurityPolicyImpl::GetInstance()->CreateHandle(process_id);
+  dom_storage_context_->OpenLocalStorage(storage_key, std::move(receiver),
+                                         std::move(handle), base::DoNothing());
 }
 
 void StoragePartitionImpl::BindSessionStorageAreaForProcess(
@@ -2844,10 +2840,9 @@ void StoragePartitionImpl::BindSessionStorageAreaForProcess(
   DCHECK(initialized_);
   auto handle =
       ChildProcessSecurityPolicyImpl::GetInstance()->CreateHandle(process_id);
-  DCHECK(handle.CanAccessDataForOrigin(storage_key.origin()));
-  dom_storage_context_->BindStorageArea(std::move(handle), storage_key,
-                                        namespace_id, base::DoNothing(),
-                                        std::move(receiver));
+  dom_storage_context_->BindStorageArea(storage_key, namespace_id,
+                                        std::move(receiver), std::move(handle),
+                                        base::DoNothing());
 }
 
 void StoragePartitionImpl::
