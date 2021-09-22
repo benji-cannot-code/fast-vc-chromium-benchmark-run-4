979a2d12936ea7874b2781b0d9ee127f9d695397
Jesse Doherty <jwd@google.com>
2021-09-22 20:31:42 +0000

    Validate experiment probability overflow.
    We preivously had a check that the accumulated weight didn't overflow. This assumed that the sum didn't overflow a uint32, which can happen if an experiment's weight is near enough to max uint32.
    
    Bug: 1250927
    Change-Id: Ic7442e887685a558f332a828991d2899abc2b3d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3175940
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924018}

--- components/variations/processed_study.cc
+++ components/variations/processed_study.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/variations/processed_study.h"
 
+#include <cstdint>
 #include <set>
 #include <string>
 
@@ -29,7 +30,8 @@ enum class InvalidStudyReason {
   kTotalProbabilityOverflow = 6,
   kMissingDefaultExperimentInList = 7,
   kBlankStudyName = 8,
-  kMaxValue = kBlankStudyName,
+  kExperimentProbabilityOverflow = 9,
+  kMaxValue = kExperimentProbabilityOverflow,
 };
 
 void LogInvalidReason(InvalidStudyReason reason) {
@@ -52,15 +54,15 @@ bool ValidateStudyAndComputeTotalProbability(
   if (study.filter().has_min_version() &&
       !base::Version::IsValidWildcardString(study.filter().min_version())) {
     LogInvalidReason(InvalidStudyReason::kInvalidMinVersion);
-    DVLOG(1) << study.name() << " has invalid min version: "
-             << study.filter().min_version();
+    DVLOG(1) << study.name()
+             << " has invalid min version: " << study.filter().min_version();
     return false;
   }
   if (study.filter().has_max_version() &&
       !base::Version::IsValidWildcardString(study.filter().max_version())) {
     LogInvalidReason(InvalidStudyReason::kInvalidMaxVersion);
-    DVLOG(1) << study.name() << " has invalid max version: "
-             << study.filter().max_version();
+    DVLOG(1) << study.name()
+             << " has invalid max version: " << study.filter().max_version();
     return false;
   }
   if (study.filter().has_min_os_version() &&
@@ -120,6 +122,16 @@ bool ValidateStudyAndComputeTotalProbability(
       if (divisor != 0)
         multiple_assigned_groups = true;
 
+      if (experiment.probability_weight() >
+          std::numeric_limits<base::FieldTrial::Probability>::max()) {
+        LogInvalidReason(InvalidStudyReason::kExperimentProbabilityOverflow);
+        DVLOG(1) << study.name() << " has an experiment (" << experiment.name()
+                 << ") with a probability weight of "
+                 << experiment.probability_weight()
+                 << " that exceeds the maximum supported value";
+        return false;
+      }
+
       if (divisor + experiment.probability_weight() >
           std::numeric_limits<base::FieldTrial::Probability>::max()) {
         LogInvalidReason(InvalidStudyReason::kTotalProbabilityOverflow);
@@ -158,7 +170,6 @@ bool ValidateStudyAndComputeTotalProbability(
   return true;
 }
 
-
 }  // namespace
 
 // static
@@ -169,8 +180,7 @@ ProcessedStudy::ProcessedStudy() {}
 
 ProcessedStudy::ProcessedStudy(const ProcessedStudy& other) = default;
 
-ProcessedStudy::~ProcessedStudy() {
-}
+ProcessedStudy::~ProcessedStudy() = default;
 
 bool ProcessedStudy::Init(const Study* study, bool is_expired) {
   base::FieldTrial::Probability total_probability = 0;
