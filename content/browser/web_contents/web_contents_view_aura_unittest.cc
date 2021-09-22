3bf783b864cd22fbe7fb01c363da0f03d2f5bc76
Joel Hockey <joelhockey@chromium.org>
2021-09-22 20:48:29 +0000

    Reenable WebContentsViewAura contents_unittest for CHROMEOS_ASH
    Test was changed in crrev.com/c/2534271. The description states that
    the intention was to do a nop and replace defined(OS_CHROMEOS) with
    BUILDFLAG(IS_CHROMEOS_ASH), however the replacement was
    BUILDFLAG(IS_CHROMEOS_LACROS).
    
    Bug: 1052397, 1247873, 1251482
    Change-Id: Ia51b97a9a16148ea382d9fa829c80c80575be749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174611
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924029}

--- content/browser/web_contents/web_contents_view_aura_unittest.cc
+++ content/browser/web_contents/web_contents_view_aura_unittest.cc
@@ -49,9 +49,7 @@ using ::ui::mojom::DragOperation;
 constexpr gfx::Rect kBounds = gfx::Rect(0, 0, 20, 20);
 constexpr gfx::PointF kClientPt = {5, 10};
 
-// TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
-// complete.
-#if (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) || defined(OS_WIN)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_WIN)
 constexpr gfx::PointF kScreenPt = {17, 3};
 #endif
 
@@ -234,9 +232,7 @@ TEST_F(WebContentsViewAuraTest, OccludeView) {
   EXPECT_EQ(web_contents()->GetVisibility(), Visibility::VISIBLE);
 }
 
-// TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
-// complete.
-#if (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) || defined(OS_WIN)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_WIN)
 TEST_F(WebContentsViewAuraTest, DragDropFiles) {
   WebContentsViewAura* view = GetView();
   auto data = std::make_unique<ui::OSExchangeData>();
@@ -274,9 +270,7 @@ TEST_F(WebContentsViewAuraTest, DragDropFiles) {
   view->OnDragEntered(event);
   ASSERT_NE(nullptr, view->current_drop_data_);
 
-// TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
-// complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
   // By design, Linux implementations return an empty string if file data
   // is also present.
   EXPECT_TRUE(!view->current_drop_data_->text ||
@@ -312,9 +306,7 @@ TEST_F(WebContentsViewAuraTest, DragDropFiles) {
 
   CheckDropData(view);
 
-// TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
-// complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
   // By design, Linux implementations returns an empty string if file data
   // is also present.
   EXPECT_TRUE(!drop_complete_data_->drop_data.text ||
@@ -360,7 +352,8 @@ TEST_F(WebContentsViewAuraTest, DragDropFilesOriginateFromRenderer) {
   data->SetFilenames(test_file_infos);
 
   // Simulate the drag originating in the renderer process, in which case
-  // any file data should be filtered out (anchor drag scenario).
+  // any file data should be filtered out (anchor drag scenario) except in
+  // CHROMEOS_ASH.
   data->MarkOriginatedFromRenderer();
 
   ui::DropTargetEvent event(*data.get(), kClientPt, kScreenPt,
@@ -371,9 +364,7 @@ TEST_F(WebContentsViewAuraTest, DragDropFilesOriginateFromRenderer) {
   view->OnDragEntered(event);
   ASSERT_NE(nullptr, view->current_drop_data_);
 
-// TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
-// complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
   // By design, Linux implementations return an empty string if file data
   // is also present.
   EXPECT_TRUE(!view->current_drop_data_->text ||
@@ -382,7 +373,12 @@ TEST_F(WebContentsViewAuraTest, DragDropFilesOriginateFromRenderer) {
   EXPECT_EQ(string_data, view->current_drop_data_->text);
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // CHROMEOS_ASH always returns false for DidOriginateFromRenderer().
+  ASSERT_FALSE(view->current_drop_data_->filenames.empty());
+#else
   ASSERT_TRUE(view->current_drop_data_->filenames.empty());
+#endif
 
   // Simulate drop.
   auto callback = base::BindOnce(&WebContentsViewAuraTest::OnDropComplete,
@@ -397,9 +393,7 @@ TEST_F(WebContentsViewAuraTest, DragDropFilesOriginateFromRenderer) {
 
   CheckDropData(view);
 
-// TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
-// complete.
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
   // By design, Linux implementations returns an empty string if file data is
   // also present.
   EXPECT_TRUE(!drop_complete_data_->drop_data.text ||
@@ -408,7 +402,12 @@ TEST_F(WebContentsViewAuraTest, DragDropFilesOriginateFromRenderer) {
   EXPECT_EQ(string_data, drop_complete_data_->drop_data.text);
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // CHROMEOS_ASH always returns false for DidOriginateFromRenderer().
+  ASSERT_FALSE(drop_complete_data_->drop_data.filenames.empty());
+#else
   ASSERT_TRUE(drop_complete_data_->drop_data.filenames.empty());
+#endif
 }
 #endif
 
