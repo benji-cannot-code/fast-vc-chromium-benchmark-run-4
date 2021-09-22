cb29bc9b24ff5485af0292e21f6aaba2c2ce3013
Justin Novosad <junov@chromium.org>
2021-09-22 20:53:18 +0000

    Adjust the OOPR-Canvas auto-flushing heuristics.
    This change relies on the number of bytes consumed by serialized
    PaintOps rather than the number of ops.  This new heuristic is based
    on the assumption that the number of serialized bytes is a better
    proxy for the computational load associated with rasterizing a batch
    of PaintOps. For example, the weight of rasterizing a path is
    proportional to the number of verbs and points.
    
    This change is being made in response to a regression in the Canvas
    Lines test of the MotionMark suite. This tests generate a large number
    of lightweight paint ops (line segments), and was therefore flushing too
    often under the old heristic.
    
    BUG=1251190
    
    Change-Id: I5b833cac2de9dbda393af0c7ff7632debdd2d0e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174231
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924032}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
@@ -1617,12 +1617,16 @@ TEST_P(CanvasRenderingContext2DTest, AutoFlush) {
   Context2D()->fillRect(0, 0, 1, 1);  // Ensure resource provider is created.
   const size_t initial_op_count =
       CanvasElement().ResourceProvider()->TotalOpCount();
-  for (size_t i = initial_op_count;
-       i <= CanvasResourceProvider::kMaxRecordedOpsPerCanvas; i++) {
+
+  while (CanvasElement().ResourceProvider()->TotalOpBytesUsed() <=
+         CanvasResourceProvider::kMaxRecordedOpBytes) {
     Context2D()->fillRect(0, 0, 1, 1);
-    ASSERT_EQ(CanvasElement().ResourceProvider()->TotalOpCount(), i + 1);
+    // Verify that auto-flush did not happen
+    ASSERT_GT(CanvasElement().ResourceProvider()->TotalOpCount(),
+              initial_op_count);
   }
   Context2D()->fillRect(0, 0, 1, 1);
+  // Verify that auto-flush happened
   ASSERT_EQ(CanvasElement().ResourceProvider()->TotalOpCount(),
             initial_op_count);
 }
@@ -1641,16 +1645,15 @@ TEST_P(CanvasRenderingContext2DTest, AutoFlushPinnedImages) {
   constexpr unsigned int kImageSize = 1024;
   constexpr unsigned int kBytesPerImage = 4 * kImageSize * kImageSize;
 
-  size_t initial_op_count = CanvasElement().ResourceProvider()->TotalOpCount();
+  const size_t initial_op_count =
+      CanvasElement().ResourceProvider()->TotalOpCount();
 
   // We repeat the test twice to verify that the state was properly
   // reset by the Flush.
   for (int repeat = 0; repeat < 2; ++repeat) {
     size_t expected_op_count = initial_op_count;
-    // The for loop below uses "<" rather than "<=" to account for the bytes
-    // used By the paint ops, which also count against pinned memory budget.
     for (size_t pinned_bytes = 0;
-         pinned_bytes < CanvasResourceProvider::kMaxPinnedMemoryPerCanvas;
+         pinned_bytes <= CanvasResourceProvider::kMaxPinnedImageBytes;
          pinned_bytes += kBytesPerImage) {
       FakeImageSource unique_image(IntSize(kImageSize, kImageSize),
                                    kOpaqueBitmap);
@@ -1711,10 +1714,8 @@ TEST_P(CanvasRenderingContext2DTest, AutoFlushSameImage) {
 
   FakeImageSource image(IntSize(kImageSize, kImageSize), kOpaqueBitmap);
 
-  // The for loop below uses "<" rather than "<=" to account for the bytes used
-  // By the paint ops, which also count against pinned memory budget.
   for (size_t pinned_bytes = 0;
-       pinned_bytes < 2 * CanvasResourceProvider::kMaxPinnedMemoryPerCanvas;
+       pinned_bytes <= 2 * CanvasResourceProvider::kMaxPinnedImageBytes;
        pinned_bytes += kBytesPerImage) {
     NonThrowableExceptionState exception_state;
     Context2D()->drawImage(GetScriptState(), &image, 0, 0, 1, 1, 0, 0, 1, 1,
@@ -1737,10 +1738,11 @@ TEST_P(CanvasRenderingContext2DTest, AutoFlushDelayedByLayer) {
   Context2D()->beginLayer();
   const size_t initial_op_count =
       CanvasElement().ResourceProvider()->TotalOpCount();
-  for (size_t i = initial_op_count;
-       i <= CanvasResourceProvider::kMaxRecordedOpsPerCanvas + 10; i++) {
+  while (CanvasElement().ResourceProvider()->TotalOpBytesUsed() <=
+         CanvasResourceProvider::kMaxRecordedOpBytes + 1024) {
     Context2D()->fillRect(0, 0, 1, 1);
-    ASSERT_EQ(CanvasElement().ResourceProvider()->TotalOpCount(), i + 1);
+    ASSERT_GT(CanvasElement().ResourceProvider()->TotalOpCount(),
+              initial_op_count);
   }
   // Closing the layer means next op can trigger auto flush
   Context2D()->endLayer();
