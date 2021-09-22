c571767dac15898c053c3af5488aab2230856df2
Mirko Bonadei <mbonadei@chromium.org>
2021-09-22 08:58:36 +0000

    Roll abseil_revision b2dc72c17a..de71511109
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/b2dc72c17a..de71511109
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/b2dc72c17a..de71511109
    
    Bug: None
    Change-Id: Ic3cf2a0b7e9559867f8694142beddae65596dc49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173136
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923780}

--- third_party/abseil-cpp/absl/container/internal/raw_hash_set.h
+++ third_party/abseil-cpp/absl/container/internal/raw_hash_set.h
@@ -1070,6 +1070,8 @@ class raw_hash_set {
     // past that we simply deallocate the array.
     if (capacity_ > 127) {
       destroy_slots();
+
+      infoz().RecordClearedReservation();
     } else if (capacity_) {
       for (size_t i = 0; i != capacity_; ++i) {
         if (IsFull(ctrl_[i])) {
@@ -1383,14 +1385,20 @@ class raw_hash_set {
     if (n == 0 && size_ == 0) {
       destroy_slots();
       infoz().RecordStorageChanged(0, 0);
+      infoz().RecordClearedReservation();
       return;
     }
+
     // bitor is a faster way of doing `max` here. We will round up to the next
     // power-of-2-minus-1, so bitor is good enough.
     auto m = NormalizeCapacity(n | GrowthToLowerboundCapacity(size()));
     // n == 0 unconditionally rehashes as per the standard.
     if (n == 0 || m > capacity_) {
       resize(m);
+
+      // This is after resize, to ensure that we have completed the allocation
+      // and have potentially sampled the hashtable.
+      infoz().RecordReservation(n);
     }
   }
 
@@ -1398,6 +1406,10 @@ class raw_hash_set {
     if (n > size() + growth_left()) {
       size_t m = GrowthToLowerboundCapacity(n);
       resize(NormalizeCapacity(m));
+
+      // This is after resize, to ensure that we have completed the allocation
+      // and have potentially sampled the hashtable.
+      infoz().RecordReservation(n);
     }
   }
 
@@ -1639,6 +1651,7 @@ class raw_hash_set {
         PolicyTraits::destroy(&alloc_ref(), slots_ + i);
       }
     }
+
     // Unpoison before returning the memory to the allocator.
     SanitizerUnpoisonMemoryRegion(slots_, sizeof(slot_type) * capacity_);
     Deallocate<alignof(slot_type)>(
