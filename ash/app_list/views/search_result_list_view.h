e0686104349f7502c70769e6412a1dd735259744
Yulun Wu <yulunwu@chromium.org>
2021-09-22 20:34:23 +0000

    Add Best Match title label to search result list view.
    This CL adds the Best Match title label to search result list view.
    Additional labeled search result views  will be added as categorical
    labels become available for the search result types.
    
    Bug: 1216097
    Change-Id: I854d6c0981b9a9c715139dd380c6902a1026cf1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159194
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924019}

--- ash/app_list/views/search_result_list_view.h
+++ ash/app_list/views/search_result_list_view.h
@@ -16,7 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "ui/views/view.h"
 
+namespace views {
+class Label;
+}  // namespace views
+
 namespace ash {
+
 namespace test {
 class SearchResultListViewTest;
 }
@@ -28,13 +33,28 @@ class AppListViewDelegate;
 // SearchResultView.
 class ASH_EXPORT SearchResultListView : public SearchResultContainerView {
  public:
+  enum class SearchResultListType {
+    // kUnified list view contains all search results with the display type
+    // SearchResultDisplayType::kList. No category labels are shown. This should
+    // be used when productivity launcher is disabled.
+    kUnified,
+    // kBestMatch list view contains the results that are the best match for the
+    // current query. This category should be used when productivity launcher is
+    // enabled. All search results will show up under this category until search
+    // metadata is updated with the other category labels.
+    // TODO(crbug/1216097) add other SearchResultListTypes.
+    kBestMatch,
+  };
+
   SearchResultListView(AppListMainView* main_view,
                        AppListViewDelegate* view_delegate);
 
   SearchResultListView(const SearchResultListView&) = delete;
   SearchResultListView& operator=(const SearchResultListView&) = delete;
-
   ~SearchResultListView() override;
+
+  // Updates the type of search results the list view shows.
+  void SetListType(SearchResultListType list_type);
 
   void SearchResultActivated(SearchResultView* view,
                              int event_flags,
@@ -67,6 +87,7 @@ class ASH_EXPORT SearchResultListView : public SearchResultContainerView {
   // Overridden from views::View:
   void Layout() override;
   int GetHeightForWidth(int w) const override;
+  void OnThemeChanged() override;
 
   // Logs the set of recommendations (impressions) that were shown to the user
   // after a period of time.
@@ -85,6 +106,9 @@ class ASH_EXPORT SearchResultListView : public SearchResultContainerView {
 
   std::vector<SearchResultView*> search_result_views_;  // Not owned.
 
+  // The SearchResultListViewType dictates what kinds of results will be shown.
+  SearchResultListType list_type_ = SearchResultListType::kUnified;
+  views::Label* title_label_ = nullptr;  // Owned by view hierarchy.
   // Used for logging impressions shown to users.
   base::OneShotTimer impression_timer_;
 };
