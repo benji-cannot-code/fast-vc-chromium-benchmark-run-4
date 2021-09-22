5cca4388dced67ea943095be3093ef3764443025
Juliet Levesque <julietlevesque@google.com>
2021-09-22 17:09:42 +0000

    [Fast Pair] Add SFUL metrics.
    This change adds a feature use metrics logger to the quick pair metrics
    logger class to implement support for Fast Pair SFUL metrics.
    
    Change-Id: Ifc327a39f8b5d1b8b3b45c4ca054c17bf5daf0ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168694
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#923911}

--- ash/quick_pair/keyed_service/quick_pair_mediator_unittest.cc
+++ ash/quick_pair/keyed_service/quick_pair_mediator_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/ui/ui_broker.h"
 #include "ash/services/quick_pair/quick_pair_process_manager_impl.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/test/task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -71,6 +72,7 @@ class MediatorTest : public testing::Test {
   MockPairerBroker* mock_pairer_broker_;
   MockUIBroker* mock_ui_broker_;
   std::unique_ptr<Mediator> mediator_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 };
 
 TEST_F(MediatorTest, TogglesScanningWhenFastPairEnabledChanges) {
