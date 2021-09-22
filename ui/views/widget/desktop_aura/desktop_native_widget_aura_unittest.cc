b20924943126a812f0e7d4bfab83157eae7f9e28
Wez <wez@chromium.org>
2021-09-22 21:54:54 +0000

    [fuchsia] Temporarily home incidental windows under the browser window.
    Disable reparenting of full-screen and menu windows to their own
    top-level platform windows, under Fuchsia, so that they will instead
    run within the root-window's Aura window tree.
    
    This is a temporary workaround until Fuchsia provides the necessary
    window-management APIs.
    
    Bug: 1236997
    Change-Id: Ib7e1f2b32b0334073d3a2a720551a81197d71b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168502
    Auto-Submit: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Benjamin Lerman <qsr@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924058}

--- ui/views/widget/desktop_aura/desktop_native_widget_aura_unittest.cc
+++ ui/views/widget/desktop_aura/desktop_native_widget_aura_unittest.cc
@@ -371,6 +371,12 @@ TEST_F(DesktopNativeWidgetAuraTest, WidgetCanBeDestroyedFromNestedLoop) {
   run_loop.Run();
 }
 
+using DesktopAuraWidgetTest = DesktopWidgetTest;
+
+#if !defined(OS_FUCHSIA)
+// TODO(crbug.com/1236997): Under Fuchsia pop-up and fullscreen windows are not
+// reparented to be top-level, so the following tests are not valid.
+
 // This class provides functionality to create fullscreen and top level popup
 // windows. It additionally tests whether the destruction of these windows
 // occurs correctly in desktop AURA without crashing.
@@ -473,8 +479,6 @@ class DesktopAuraTopLevelWindowTest : public aura::WindowObserver {
   bool use_async_mode_ = true;
 };
 
-using DesktopAuraWidgetTest = DesktopWidgetTest;
-
 TEST_F(DesktopAuraWidgetTest, FullscreenWindowDestroyedBeforeOwnerTest) {
   DesktopAuraTopLevelWindowTest fullscreen_window;
   ASSERT_NO_FATAL_FAILURE(
@@ -544,6 +548,8 @@ TEST_F(DesktopAuraWidgetTest, TopLevelOwnedPopupRepositionTest) {
 
   ASSERT_NO_FATAL_FAILURE(popup_window.DestroyOwnedWindow());
 }
+
+#endif  // !defined(OS_FUCHSIA)
 
 // The following code verifies we can correctly destroy a Widget from a mouse
 // enter/exit. We could test move/drag/enter/exit but in general we don't run
