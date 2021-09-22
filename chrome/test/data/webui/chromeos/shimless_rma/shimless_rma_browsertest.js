172663950ab3576d2dfc7d10dc1793651c277c57
Gavin Dodd <gavindodd@google.com>
2021-09-22 18:52:40 +0000

    shimless: Implement run calibration page skeleton
    Screenshot: https://screenshot.googleplex.com/6Wahug6nuSskmAL
    
          Run browser tests for shimless
          autoninja -C out/Default/ ash/webui/shimless_rma/resources:closure_compile_module
          autoninja -C out/Default/ chrome/test/data/webui/chromeos/shimless_rma:closure_compile
    
    Bug: 1198187
    Test: Run local build
    Change-Id: I46b0a5aae08ef699cb637db3fe230009df0e500f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174494
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923966}

--- chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_browsertest.js
+++ chrome/test/data/webui/chromeos/shimless_rma/shimless_rma_browsertest.js
@@ -50,6 +50,7 @@ const debug_suites_list = [
   'OnboardingUpdatePageTest',
   'OnboardingWaitForManualWpDisablePageTest',
   'ReimagingCalibrationPageTest',
+  'ReimagingCalibrationRunPageTest',
   'ReimagingCalibrationSetupPageTest',
   'ReimagingFirmwareUpdatePageTest',
   'ReimagingDeviceInformationPageTest',
