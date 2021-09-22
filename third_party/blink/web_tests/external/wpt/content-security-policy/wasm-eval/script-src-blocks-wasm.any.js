9e78398095aee288cc19b425c6dde88a1cbb77df
Fergus Dall <sidereal@google.com>
2021-09-22 01:36:39 +0000

    Revert "[CSP] Fix wasm-eval check from workers"
    This reverts commit fda85ae58f7d3483b57f02973015f0bdbc597f19.
    
    Reason for revert: Broke arc.TextToSpeech and a11y.ChromevoxHint tast tests
    
    Original change's description:
    > [CSP] Fix wasm-eval check from workers
    >
    > For workers, we were not gating wasm instantiation/compilation behind
    > Content Security Policy. This CL fixes that and adds tests.
    >
    > Bug: 1248289
    > Change-Id: Ib2cdb2ba4dd5b5e2d289a5fbe4700521dea9265f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152118
    > Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    > Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#923308}
    
    Bug: 1248289
    Bug: 1251580
    Change-Id: Ib478f1c81a0c296aef1fe7367af11c86bcc04669
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173036
    Auto-Submit: Fergus Dall <sidereal@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/main@{#923685}

--- third_party/blink/web_tests/external/wpt/content-security-policy/wasm-eval/script-src-blocks-wasm.any.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// META: global=window,worker
-
-promise_test(t => {
-  return promise_rejects_js(
-      t, WebAssembly.CompileError,
-      WebAssembly.instantiate(
-          new Uint8Array([0, 0x61, 0x73, 0x6d, 0x1, 0, 0, 0])));
-});
