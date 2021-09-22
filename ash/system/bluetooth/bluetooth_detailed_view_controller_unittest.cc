68aa17329ef7399e6af64c838d5df39d57b25eda
Chad Duffin <chadduffin@chromium.org>
2021-09-22 15:10:51 +0000

    [CrOS Bluetooth] Implement "pair new device" button.
    This change implements the "pair new device" button within the Quick
    Settings, and hooks the button up so that the Bluetooth pairing dialog
    is opened when pressed. Tests are included to verify functionality.
    
    Enabled:  https://screenshot.googleplex.com/7HGnu9YcNEcw8jG.png
    Disabled: https://screenshot.googleplex.com/3ed5qJUQjk3YEjK.png
    
    Bug: 1010321
    Test: Verified on-device and locally with unit tests.
    Change-Id: Id0fc0668b5fff098ec21500f1faeb1b30d56f49a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152964
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923858}

--- ash/system/bluetooth/bluetooth_detailed_view_controller_unittest.cc
+++ ash/system/bluetooth/bluetooth_detailed_view_controller_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/constants/ash_features.h"
+#include "ash/public/cpp/test/test_system_tray_client.h"
 #include "ash/system/bluetooth/bluetooth_detailed_view.h"
 #include "ash/system/bluetooth/bluetooth_device_list_controller.h"
 #include "ash/system/bluetooth/fake_bluetooth_detailed_view.h"
@@ -191,6 +192,13 @@ TEST_F(BluetoothDetailedViewControllerTest,
   bluetooth_detailed_view_delegate()->OnToggleClicked(/*new_state=*/true);
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(BluetoothSystemState::kEnabling, GetBluetoothAdapterState());
+}
+
+TEST_F(BluetoothDetailedViewControllerTest,
+       OnPairNewDeviceRequestedOpensBluetoothDialog) {
+  EXPECT_EQ(0, GetSystemTrayClient()->show_bluetooth_pairing_dialog_count());
+  bluetooth_detailed_view_delegate()->OnPairNewDeviceRequested();
+  EXPECT_EQ(1, GetSystemTrayClient()->show_bluetooth_pairing_dialog_count());
 }
 
 }  // namespace tray
