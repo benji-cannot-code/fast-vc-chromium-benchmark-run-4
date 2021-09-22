8108ac39d00cf988eaea7b8c194652c091282055
Dominic Battre <battre@chromium.org>
2021-09-22 16:43:50 +0000

    Improve debug output for accessibility events we are waiting for
    This CL does two things:
    
    1) It moves log output form LOG(INFO) to VLOG(1) to reduce the default
    spammyness of browsertest output. Use
    --vmodule=accessibility_notification_waiter=1 to to see the VLOG(1)
    output (this will happen via a ScopedCommandLine in the test).
    
    2) Improve log output when necessary:
    
    AccessibilityNotificationWaiter currently logs accessibility events
    in OnAccessibilityEvent(). However in tests that wait for multiple
    events, this is not particularly useful output.
    
    Before the CL, the following output would be generated:
    
    [..._waiter.cc(158)] OnAccessibilityEvent kChildrenChanged
    [..._waiter.cc(158)] OnAccessibilityEvent kLayoutComplete
    [..._waiter.cc(158)] OnAccessibilityEvent kLoadComplete
    [..._waiter.cc(158)] OnAccessibilityEvent kTextSelectionChanged
    [..._waiter.cc(158)] OnAccessibilityEvent kDocumentSelectionChanged
    [..._waiter.cc(158)] OnAccessibilityEvent kFocus
    
    With this CL, it is possible to understand which accessibility event
    is awaited in case a flaky test times out.
    
    [..._waiter.cc(81)] Waiting for AccessibilityEvent kLoadComplete
    [..._waiter.cc(158)] OnAccessibilityEvent kChildrenChanged
    [..._waiter.cc(158)] OnAccessibilityEvent kLayoutComplete
    [..._waiter.cc(158)] OnAccessibilityEvent kLoadComplete
    [..._waiter.cc(81)] Waiting for AccessibilityEvent kNone
    [..._waiter.cc(158)] OnAccessibilityEvent kTextSelectionChanged
    [..._waiter.cc(158)] OnAccessibilityEvent kDocumentSelectionChanged
    [..._waiter.cc(158)] OnAccessibilityEvent kFocus
    
    https://ci.chromium.org/ui/p/chromium/builders/try/linux-rel/797210/overview
    is an example of useless debug output.
    
    Bug: 1240687
    Change-Id: I3e005427033f1dc4178ada201f74c0427d48c3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168782
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923897}

--- content/public/test/accessibility_notification_waiter.cc
+++ content/public/test/accessibility_notification_waiter.cc
@@ -78,6 +78,8 @@ AccessibilityNotificationWaiter::~AccessibilityNotificationWaiter() = default;
 
 void AccessibilityNotificationWaiter::ListenToAllFrames(
     WebContents* web_contents) {
+  if (event_to_wait_for_)
+    VLOG(1) << "Waiting for AccessibilityEvent " << *event_to_wait_for_;
   WebContentsImpl* web_contents_impl =
       static_cast<WebContentsImpl*>(web_contents);
   FrameTree* frame_tree = web_contents_impl->GetFrameTree();
@@ -154,7 +156,7 @@ void AccessibilityNotificationWaiter::OnAccessibilityEvent(
   if (IsAboutBlank())
     return;
 
-  LOG(INFO) << "OnAccessibilityEvent " << event_type;
+  VLOG(1) << "OnAccessibilityEvent " << event_type;
 
   if (event_to_wait_for_ == ax::mojom::Event::kNone ||
       event_to_wait_for_ == event_type) {
