d7fd6434e56b05fccaa3509b55c8d9c748020bb1
Olivier Li <olivierli@chromium.org>
2021-09-22 16:32:42 +0000

    Remove expired histogram Mojo.Channel.WriteQueued
    Bug: 1209951
    Change-Id: I99ae5234aa755e5a19b373697b02fdb64e39aeae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173933
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923893}

--- mojo/core/channel_posix.cc
+++ mojo/core/channel_posix.cc
@@ -159,7 +159,6 @@ void ChannelPosix::Write(MessagePtr message) {
                            message->NumHandlesForTransit());
 
   bool write_error = false;
-  bool queued = false;
   {
     base::AutoLock lock(write_lock_);
     if (reject_writes_)
@@ -170,7 +169,6 @@ void ChannelPosix::Write(MessagePtr message) {
     } else {
       outgoing_messages_.emplace_back(std::move(message), 0);
     }
-    queued = !outgoing_messages_.empty();
   }
   if (write_error) {
     // Invoke OnWriteError() asynchronously on the IO thread, in case Write()
@@ -179,7 +177,6 @@ void ChannelPosix::Write(MessagePtr message) {
                               base::BindOnce(&ChannelPosix::OnWriteError, this,
                                              Error::kDisconnected));
   }
-  UMA_HISTOGRAM_BOOLEAN("Mojo.Channel.WriteQueued", queued);
 }
 
 void ChannelPosix::LeakHandle() {
