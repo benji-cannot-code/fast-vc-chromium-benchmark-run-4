1a69c7be3657fe06f87fd3559457f308c11bb392
Theodore Tefera <ttefera@google.com>
2021-09-22 02:47:49 +0000

    ArcNearbyShare finding ARC Window UMA metrics
    UMA metrics for how long it takes to find ARC Window with
    ArcNearbyShare. If ARC Window is not found, the timeout time will be displayed in the histogram.
    
    Bug: 191232397
    Change-Id: I8765a7082f7f07f787c137c4da2ebb4ee0a5d702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115107
    Auto-Submit: Theodore Tefera <ttefera@google.com>
    Reviewed-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Alan Ding <alanding@chromium.org>
    Commit-Queue: Theodore Tefera <ttefera@google.com>
    Cr-Commit-Position: refs/heads/main@{#923711}

---
+++ chrome/browser/ash/arc/nearby_share/arc_nearby_share_uma.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_ARC_NEARBY_SHARE_ARC_NEARBY_SHARE_UMA_H_
+#define CHROME_BROWSER_ASH_ARC_NEARBY_SHARE_ARC_NEARBY_SHARE_UMA_H_
+
+namespace arc {
+
+void UpdateNearbyShareWindowFound(bool found);
+
+}  // namespace arc
+
+#endif  // CHROME_BROWSER_ASH_ARC_NEARBY_SHARE_ARC_NEARBY_SHARE_UMA_H_
