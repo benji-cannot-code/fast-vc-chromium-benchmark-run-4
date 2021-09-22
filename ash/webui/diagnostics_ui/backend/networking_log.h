62a78b9fcd11915efc42e9be79ab93b233e51240
Zentaro Kavanagh <zentaro@chromium.org>
2021-09-22 00:10:46 +0000

    Session Log: Show all valid networks in log
    - Previously only the active network was shown
    
    Bug: 1125150
    Change-Id: I11a099fedda996b7f4e4faba2cc37760caafe383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171273
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923657}

--- ash/webui/diagnostics_ui/backend/networking_log.h
+++ ash/webui/diagnostics_ui/backend/networking_log.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WEBUI_DIAGNOSTICS_UI_BACKEND_NETWORKING_LOG_H_
 
 #include <string>
+#include <vector>
 
 #include "ash/webui/diagnostics_ui/mojom/network_health_provider.mojom.h"
 
@@ -24,10 +25,18 @@ class NetworkingLog {
 
   // Returns the networking log as a string.
   std::string GetContents() const;
-  void UpdateContents(mojom::NetworkPtr latest_network_info);
+
+  // Updates the list of valid networks and which is active.
+  void UpdateNetworkList(const std::vector<std::string>& observer_guids,
+                         std::string active_guid);
+
+  // Update the state of the network. Networks can be identified by
+  // the `observer_guid` property.
+  void UpdateNetworkState(mojom::NetworkPtr network);
 
  private:
-  mojom::NetworkPtr latest_network_info_;
+  std::string active_guid_;
+  base::flat_map<std::string, mojom::NetworkPtr> latest_network_states_;
 };
 
 }  // namespace diagnostics
