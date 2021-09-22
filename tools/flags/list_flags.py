a6414bb23844683280179d5af404e3b3609106c3
Lei Zhang <thestig@chromium.org>
2021-09-22 21:18:12 +0000

    Fix list_flags.py -u.
    Currently, running this command fails with:
    
    ValueError: binary mode doesn't take an encoding argument
    
    Fix by not reading in binary mode. Also change the shebang to use
    python3 explicitly, as this script no longer runs with Python 2.
    
    Change-Id: I80023bf0a79e636c6a74b0ff54f508fcc6d0ea8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176305
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924043}

--- tools/flags/list_flags.py
+++ tools/flags/list_flags.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2019 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -90,7 +90,7 @@ def find_unused(flags):
       'chrome/browser/about_flags.cc',
       'ios/chrome/browser/flags/about_flags.mm',
   ]
-  flag_files_data = [open(f, 'rb', encoding='utf-8').read() for f in FLAG_FILES]
+  flag_files_data = [open(f, 'r', encoding='utf-8').read() for f in FLAG_FILES]
   unused_flags = []
   for flag in flags:
     # Search for the name in quotes.
