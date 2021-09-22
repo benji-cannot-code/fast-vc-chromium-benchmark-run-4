4f3960826f2ddee59533c073475acbdab4009de3
Gabriel Charette <gab@chromium.org>
2021-09-22 18:08:55 +0000

    [startup] Add trace events and metrics to DoUpgradeTasks
    R=chromium-metrics-reviews@google.com, etienneb@chromium.org
    
    Bug: 1252004
    Change-Id: I3450373eb1d5bdee57f1520c341df75377fe9028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3175861
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923946}

--- chrome/browser/first_run/upgrade_util_win.cc
+++ chrome/browser/first_run/upgrade_util_win.cc
@@ -23,11 +23,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/process/launch.h"
 #include "base/process/process_handle.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "base/win/registry.h"
 #include "base/win/windows_version.h"
 #include "build/branding_buildflags.h"
@@ -57,6 +60,8 @@ bool GetNewerChromeFile(base::FilePath* path) {
 
 bool InvokeGoogleUpdateForRename() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  TRACE_EVENT0("startup", "upgrade_util::InvokeGoogleUpdateForRename");
+
   Microsoft::WRL::ComPtr<IProcessLauncher> ipl;
   HRESULT hr = ::CoCreateInstance(__uuidof(ProcessLauncherClass), nullptr,
                                   CLSCTX_ALL, IID_PPV_ARGS(&ipl));
@@ -100,6 +105,8 @@ bool InvokeGoogleUpdateForRename() {
 namespace upgrade_util {
 
 bool RelaunchChromeBrowserImpl(const base::CommandLine& command_line) {
+  TRACE_EVENT0("startup", "upgrade_util::RelaunchChromeBrowserImpl");
+
   base::FilePath chrome_exe;
   if (!base::PathService::Get(base::FILE_EXE, &chrome_exe)) {
     NOTREACHED();
@@ -122,6 +129,7 @@ bool RelaunchChromeBrowserImpl(const base::CommandLine& command_line) {
 }
 
 bool IsUpdatePendingRestart() {
+  TRACE_EVENT0("startup", "upgrade_util::IsUpdatePendingRestart");
   base::FilePath new_chrome_exe;
   if (!GetNewerChromeFile(&new_chrome_exe))
     return false;
@@ -131,6 +139,8 @@ bool IsUpdatePendingRestart() {
 bool SwapNewChromeExeIfPresent() {
   if (!IsUpdatePendingRestart())
     return false;
+
+  TRACE_EVENT0("startup", "upgrade_util::SwapNewChromeExeIfPresent");
 
   // If this is a system-level install, ask Google Update to launch an elevated
   // process to rename Chrome executables.
@@ -182,6 +192,7 @@ bool SwapNewChromeExeIfPresent() {
 }
 
 bool IsRunningOldChrome() {
+  TRACE_EVENT0("startup", "upgrade_util::IsRunningOldChrome");
   // This figures out the actual file name that the section containing the
   // mapped exe refers to. This is used instead of GetModuleFileName because the
   // .exe may have been renamed out from under us while we've been running which
@@ -200,12 +211,23 @@ bool IsRunningOldChrome() {
 }
 
 bool DoUpgradeTasks(const base::CommandLine& command_line) {
-  if (!SwapNewChromeExeIfPresent() && !IsRunningOldChrome())
+  TRACE_EVENT0("startup", "upgrade_util::DoUpgradeTasks");
+  const auto begin_time = base::TimeTicks::Now();
+  if (!SwapNewChromeExeIfPresent() && !IsRunningOldChrome()) {
+    UMA_HISTOGRAM_MEDIUM_TIMES("Startup.DoUpgradeTasks.NoRelaunch",
+                               base::TimeTicks::Now() - begin_time);
     return false;
+  }
   // At this point the chrome.exe has been swapped with the new one.
-  if (!RelaunchChromeBrowser(command_line)) {
-    // The re-launch fails. Feel free to panic now.
+  if (RelaunchChromeBrowser(command_line)) {
+    UMA_HISTOGRAM_MEDIUM_TIMES("Startup.DoUpgradeTasks.RelaunchSucceeded",
+                               base::TimeTicks::Now() - begin_time);
+  } else {
+    // The re-launch failed. Feel free to panic now.
     NOTREACHED();
+    // Log a metric anyways to see if this is at fault in crbug.com/1252004
+    UMA_HISTOGRAM_MEDIUM_TIMES("Startup.DoUpgradeTasks.RelaunchFailed",
+                               base::TimeTicks::Now() - begin_time);
   }
   return true;
 }
