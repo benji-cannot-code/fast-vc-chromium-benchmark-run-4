d43fc92ad8eec2a683cc0bfd2ad5e381b75b2f46
Scott Violet <sky@chromium.org>
2021-09-22 17:27:24 +0000

    views: remove MenuItemView::Init()
    And instead uses delegated constructors.
    
    Note that the declaration/definition are rather out of order in this
    file, so I put the new constructor in a spot to minimize diffs.
    
    BUG=none
    TEST=none
    
    Change-Id: I538ea971d66f90d5da5883b5a5e2c2314620ae0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161386
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923920}

--- ui/views/controls/menu/menu_item_view.h
+++ ui/views/controls/menu/menu_item_view.h
@@ -410,13 +410,15 @@ class VIEWS_EXPORT MenuItemView : public View {
     SkColor minor_fg_color = SK_ColorTRANSPARENT;
   };
 
+  MenuItemView(MenuItemView* parent,
+               int command,
+               Type type,
+               MenuDelegate* delegate);
+
   // Calculates all sizes that we can from the OS.
   //
   // This is invoked prior to Running a menu.
   void UpdateMenuPartSizes();
-
-  // Called by the two constructors to initialize this menu item.
-  void Init(MenuItemView* parent, int command, MenuItemView::Type type);
 
   // The RunXXX methods call into this to set up the necessary state before
   // running. |is_first_menu| is true if no menus are currently showing.
@@ -551,7 +553,7 @@ class VIEWS_EXPORT MenuItemView : public View {
 
   // Type of menu. NOTE: MenuItemView doesn't itself represent SEPARATOR,
   // that is handled by an entirely different view class.
-  Type type_ = Type::kSubMenu;
+  const Type type_;
 
   // Whether we're selected.
   bool selected_ = false;
