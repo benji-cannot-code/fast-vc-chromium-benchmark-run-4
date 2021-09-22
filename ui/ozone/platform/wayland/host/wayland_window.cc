af276eca0e7de1bd851fb70c82e0314bad59a8ab
Peter McNeeley <petermcneeley@chromium.org>
2021-09-22 18:10:26 +0000

    Final propagation of buffer opaqueness for ozone wayland
    This change is to support LaCros delegated compositing. With this
    change buffers that are considered opaque (non-blending) on the LaCros
    side will end up being flagged as non blending on the Ash side.
    
    Example Differences with and without this change can be found at:
    https://docs.google.com/document/
    d/1o6a-JTx42jKN21JuvIeEaWuqkmNVGdo33ogTseUkTpk/
    edit?pli=1&resourcekey=0-hTc9nPF2GiFPEqZjzBPpYg
    #heading=h.hb18pt1opdtx
    
    
    Bug: 1239042
    Change-Id: I62c569dfe8f9a75a648c316953bd335ee886afd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173514
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923947}

--- ui/ozone/platform/wayland/host/wayland_window.cc
+++ ui/ozone/platform/wayland/host/wayland_window.cc
@@ -800,6 +800,7 @@ bool WaylandWindow::CommitOverlays(
         (*iter)->wayland_surface()->SetViewportSource(
             (*overlay_iter)->crop_rect);
         (*iter)->wayland_surface()->SetOpacity((*overlay_iter)->opacity);
+        (*iter)->wayland_surface()->SetBlending((*overlay_iter)->enable_blend);
         (*iter)->wayland_surface()->SetViewportDestination(
             (*overlay_iter)->bounds_rect.size());
         connection_->buffer_manager_host()->CommitBufferInternal(
@@ -837,6 +838,7 @@ bool WaylandWindow::CommitOverlays(
         (*iter)->wayland_surface()->SetViewportSource(
             (*overlay_iter)->crop_rect);
         (*iter)->wayland_surface()->SetOpacity((*overlay_iter)->opacity);
+        (*iter)->wayland_surface()->SetBlending((*overlay_iter)->enable_blend);
         (*iter)->wayland_surface()->SetViewportDestination(
             (*overlay_iter)->bounds_rect.size());
         connection_->buffer_manager_host()->CommitBufferInternal(
@@ -887,6 +889,7 @@ bool WaylandWindow::CommitOverlays(
     primary_subsurface_->wayland_surface()->SetViewportSource(
         (*split)->crop_rect);
     primary_subsurface_->wayland_surface()->SetOpacity((*split)->opacity);
+    primary_subsurface_->wayland_surface()->SetBlending((*split)->enable_blend);
     primary_subsurface_->wayland_surface()->SetViewportDestination(
         (*split)->crop_rect == gfx::RectF(1.f, 1.f)
             ? gfx::Size()
