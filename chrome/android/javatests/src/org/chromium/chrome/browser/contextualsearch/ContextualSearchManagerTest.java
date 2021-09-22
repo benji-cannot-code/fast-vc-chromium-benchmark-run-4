24d97f754cf02dafe03228fb60b47dab6ad3627b
Donn Denman <donnd@chromium.org>
2021-09-22 16:19:18 +0000

    [TTS][RSearches] Fix flaky tests on release bots
    Since release bots don't pick up the testing config we need to
    set all features that we're testing. We were not doing that for
    two Related Searches tests, but now we do.
    
    BUG=1251592,1245740
    
    Change-Id: I38d8eda22636ed54c46b48518550ecb69ffdf6cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174292
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923888}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -3979,6 +3979,7 @@ public class ContextualSearchManagerTest {
     @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "crbug.com/1240342")
     @Feature({"ContextualSearch"})
     public void testRelatedSearchesItemNotSelected() throws Exception {
+        FeatureList.setTestFeatures(ENABLE_RELATED_SEARCHES_IN_BAR);
         mPolicy.overrideAllowSendingPageUrlForTesting(true);
         FakeResolveSearch fakeSearch = simulateResolveSearch("intelligence");
         Assert.assertFalse("Related Searches should have been requested but were not!",
@@ -4000,6 +4001,7 @@ public class ContextualSearchManagerTest {
     @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.O, message = "crbug.com/1182040")
     @DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "crbug.com/1240342")
     public void testRelatedSearchesItemSelected() throws Exception {
+        FeatureList.setTestFeatures(ENABLE_RELATED_SEARCHES_IN_BAR);
         mFakeServer.reset();
         FakeResolveSearch fakeSearch = simulateResolveSearch("intelligence");
         ResolvedSearchTerm resolvedSearchTerm = fakeSearch.getResolvedSearchTerm();
