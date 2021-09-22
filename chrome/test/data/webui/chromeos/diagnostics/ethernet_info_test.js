151ae9a1348e50453e05dcfb978b2ff065a74abc
Michael Checo <michaelcheco@google.com>
2021-09-22 15:27:56 +0000

    Diagnostics: Remove link speed and reorder data points
    Screenshot: http://shortn/_qkDpV5SPyK
    
    Bug: 1125150
    Change-Id: Ib5d2c58cedc38f59664546c38323b1146b951fbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173341
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#923863}

--- chrome/test/data/webui/chromeos/diagnostics/ethernet_info_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/ethernet_info_test.js
@@ -59,15 +59,6 @@ export function ethernetInfoTestSuite() {
     });
   }
 
-  test('EthernetInfoPopulated', () => {
-    return initializeEthernetInfo(fakeEthernetNetwork).then(() => {
-      // Element expected on screen but data currently missing in api.
-      // TODO(ashleydp): Update test when link speed data-point value provided.
-      assertTextContains(
-          getDataPointValue(ethernetInfoElement, '#linkSpeed'), '');
-    });
-  });
-
   test('EthernetInfoIpAddressBasedOnNetwork', () => {
     return initializeEthernetInfo(fakeEthernetNetwork).then(() => {
       const expectedHeader = ethernetInfoElement.i18n('networkIpAddressLabel');
@@ -103,14 +94,5 @@ export function ethernetInfoTestSuite() {
               ethernetInfoElement, '#authentication', expectedHeader,
               expectedValue);
         });
-  });
-
-  test('EthernetInfoLinkSpeedBasedOnNetwork', () => {
-    return initializeEthernetInfo(fakeEthernetNetwork).then(() => {
-      const expectedHeader = ethernetInfoElement.i18n('networkLinkSpeedLabel');
-      // TODO(ashleydp): Update test when Ethernet link speed data provided.
-      assertDataPointHasExpectedHeaderAndValue(
-          ethernetInfoElement, '#linkSpeed', expectedHeader, '');
-    });
   });
 }
