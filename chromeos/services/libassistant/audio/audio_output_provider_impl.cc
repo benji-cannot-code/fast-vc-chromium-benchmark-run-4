2fda0ee097472e63f3f755609257cf8ffc55f10a
wutao <wutao@chromium.org>
2021-09-22 07:26:17 +0000

    assistant: Start audio decoder service as needed
    Will start audio decoder service when assistant client created and
    stop the service when assistant client destroyed.
    
    Bug: 1179766
    Test: manual
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I791edf1dd7e7dacf503d1f4da991de61200fe4ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3140782
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#923755}

--- chromeos/services/libassistant/audio/audio_output_provider_impl.cc
+++ chromeos/services/libassistant/audio/audio_output_provider_impl.cc
@@ -166,8 +166,6 @@ void AudioOutputProviderImpl::Bind(
     mojo::PendingRemote<mojom::AudioOutputDelegate> audio_output_delegate,
     mojom::PlatformDelegate* platform_delegate) {
   platform_delegate_ = platform_delegate;
-  platform_delegate_->BindAudioDecoderFactory(
-      audio_decoder_factory_.BindNewPipeAndPassReceiver());
 
   audio_output_delegate_.Bind(std::move(audio_output_delegate));
 
@@ -227,6 +225,15 @@ assistant_client::VolumeControl& AudioOutputProviderImpl::GetVolumeControl() {
 void AudioOutputProviderImpl::RegisterAudioEmittingStateCallback(
     AudioEmittingStateCallback callback) {
   // TODO(muyuanli): implement.
+}
+
+void AudioOutputProviderImpl::BindAudioDecoderFactory() {
+  platform_delegate_->BindAudioDecoderFactory(
+      audio_decoder_factory_.BindNewPipeAndPassReceiver());
+}
+
+void AudioOutputProviderImpl::UnBindAudioDecoderFactory() {
+  audio_decoder_factory_.reset();
 }
 
 void AudioOutputProviderImpl::BindStreamFactory(
--- chromeos/services/libassistant/platform_api.h
+++ chromeos/services/libassistant/platform_api.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "chromeos/services/libassistant/grpc/assistant_client_observer.h"
 #include "chromeos/services/libassistant/network_provider_impl.h"
 #include "chromeos/services/libassistant/public/mojom/audio_output_delegate.mojom.h"
 #include "chromeos/services/libassistant/public/mojom/platform_delegate.mojom.h"
@@ -27,7 +28,8 @@ class SystemProviderImpl;
 // Implementation of the Libassistant PlatformApi.
 // The components that haven't been migrated to this mojom service will still be
 // implemented chromeos/service/assistant/platform (and simply be exposed here).
-class PlatformApi : public assistant_client::PlatformApi {
+class PlatformApi : public assistant_client::PlatformApi,
+                    public AssistantClientObserver {
  public:
   PlatformApi();
   PlatformApi(const PlatformApi&) = delete;
@@ -40,13 +42,17 @@ class PlatformApi : public assistant_client::PlatformApi {
 
   PlatformApi& SetAudioInputProvider(assistant_client::AudioInputProvider*);
 
-  // assistant_client::PlatformApi implementation:
+  // assistant_client::PlatformApi:
   assistant_client::AudioInputProvider& GetAudioInputProvider() override;
   assistant_client::AudioOutputProvider& GetAudioOutputProvider() override;
   assistant_client::AuthProvider& GetAuthProvider() override;
   assistant_client::FileProvider& GetFileProvider() override;
   assistant_client::NetworkProvider& GetNetworkProvider() override;
   assistant_client::SystemProvider& GetSystemProvider() override;
+
+  // AssistantClientObserver:
+  void OnAssistantClientCreated(AssistantClient* assistant_client) override;
+  void OnAssistantClientDestroyed() override;
 
  private:
   // This is owned by |AudioInputController|.
