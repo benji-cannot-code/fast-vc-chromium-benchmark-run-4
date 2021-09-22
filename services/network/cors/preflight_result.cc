b917790ee5b4ab2d6b67b165761864c1f7a13a54
ainozaki <ainozaki@google.com>
2021-09-22 07:17:58 +0000

    CORS: Logging preflight result
    This is one of the patches of logging CORS preflight request in
    NetLog. design doc (Internal):
    https://docs.google.com/document/d/1VambUhLsBYXPg9tOXPtTmYgUHXXuArIbBOQAOw8YcKU/edit?usp=sharing
    
    In this CL, the result of CORS preflight is logged. PreflightController
    gets the result in two ways: from CORS preflight request or from CORS
    preflight cache, so we take logs in the two places.
    
    Bug: 124451
    Change-Id: Ida38ef4fc6ee57f431bca4372a45a998f3a6ed90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3161097
    Commit-Queue: Ai Nozaki <ainozaki@google.com>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923752}

--- services/network/cors/preflight_result.cc
+++ services/network/cors/preflight_result.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/cors/preflight_result.h"
 
+#include <string>
+#include <vector>
+
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
@@ -13,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
+#include "base/values.h"
 #include "net/http/http_request_headers.h"
 #include "net/http/http_util.h"
 #include "services/network/cors/cors_util.h"
@@ -245,6 +249,17 @@ bool PreflightResult::HasAuthorizationCoveredByWildcard(
 
   return has_wildcard && headers.HasHeader(kAuthorization) &&
          !headers_.contains(kAuthorization);
+}
+
+base::Value PreflightResult::NetLogParams() {
+  base::Value dict(base::Value::Type::DICTIONARY);
+  std::vector<std::string> methods(methods_.begin(), methods_.end());
+  std::vector<std::string> headers(headers_.begin(), headers_.end());
+  dict.SetStringKey("access-control-allow-methods",
+                    base::JoinString(methods, ","));
+  dict.SetStringKey("access-control-allow-headers",
+                    base::JoinString(headers, ","));
+  return dict;
 }
 
 }  // namespace cors
--- services/network/public/cpp/cors/cors.cc
+++ services/network/public/cpp/cors/cors.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
-#include "base/containers/flat_set.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_piece.h"
