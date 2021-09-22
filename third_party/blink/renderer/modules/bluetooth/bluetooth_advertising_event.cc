d26b38077ef1b11a2db7546323e4c5c7c059515d
Reilly Grant <reillyg@chromium.org>
2021-09-22 02:08:33 +0000

    Remove unused headers from bluetooth_advertising_event.*
    Follow-up on crrev/c/3172034 as suggested by yukishiino@.
    
    Bug: 1187331
    Change-Id: I164ac645833d7d08030bd5d08309f1f68cc7b62e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173989
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923695}

--- third_party/blink/renderer/modules/bluetooth/bluetooth_advertising_event.cc
+++ third_party/blink/renderer/modules/bluetooth/bluetooth_advertising_event.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/bluetooth/bluetooth_advertising_event.h"
 
-#include "third_party/blink/renderer/bindings/core/v8/v8_union_string_unsignedlong.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_bluetooth_advertising_event_init.h"
-#include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
 #include "third_party/blink/renderer/modules/bluetooth/bluetooth_device.h"
 #include "third_party/blink/renderer/modules/bluetooth/bluetooth_manufacturer_data_map.h"
--- third_party/blink/renderer/modules/bluetooth/bluetooth_advertising_event.h
+++ third_party/blink/renderer/modules/bluetooth/bluetooth_advertising_event.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_BLUETOOTH_BLUETOOTH_ADVERTISING_EVENT_H_
 
 #include "third_party/blink/public/mojom/bluetooth/web_bluetooth.mojom-blink-forward.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 
 namespace blink {
