c5ae0544e31201a13414199b6aee8171707687fa
dpapad <dpapad@chromium.org>
2021-09-22 08:36:00 +0000

    Settings TypeScript: Migrate languages_page/ folder, part 1.
    Migrating all files that are not shared with CrOS Settings. Also adding
    TypeScript definitions for the chrome.languageSettingsPrivate API.
    
    Bug: 1234307
    Change-Id: I573daad28ad8fead1e779816d9d2c84dbf26836c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3167042
    Auto-Submit: dpapad <dpapad@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923775}

--- chrome/browser/resources/settings/languages_page/languages_browser_proxy.js
+++ chrome/browser/resources/settings/languages_page/languages_browser_proxy.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // clang-format off
-import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
 // clang-format on
 
   /** @interface */
@@ -65,8 +65,17 @@ export class LanguagesBrowserProxyImpl {
     return /** @type {!InputMethodPrivate} */ (chrome.inputMethodPrivate);
   }
   // </if>
+
+  /** @return {!LanguagesBrowserProxy} */
+  static getInstance() {
+    return instance || (instance = new LanguagesBrowserProxyImpl());
+  }
+
+  /** @param {!LanguagesBrowserProxy} obj */
+  static setInstance(obj) {
+    instance = obj;
+  }
 }
 
-  // The singleton instance_ is replaced with a test version of this wrapper
-  // during testing.
-addSingletonGetter(LanguagesBrowserProxyImpl);
+/** @type {?LanguagesBrowserProxy} */
+let instance = null;
