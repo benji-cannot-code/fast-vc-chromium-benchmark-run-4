03206d9116c0e8039148c0c56ed6f263b4637299
Emily Shack <emshack@chromium.org>
2021-09-22 18:49:09 +0000

    Don't close the side panel on ESC
    Adds a parameter to BubbleContentsWrapper to allow specific instances to
    bypass the default ESC behavior. While most bubble use cases are
    ephemeral and should close on ESC, the side panel is a larger, more
    persistent surface and therefore should not close on ESC.
    
    Bug: 1250483
    Change-Id: I0043b2f67756dd21f71d2ab407af67cd7c857a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173717
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#923963}

--- chrome/browser/ui/views/bubble/bubble_contents_wrapper.h
+++ chrome/browser/ui/views/bubble/bubble_contents_wrapper.h
@@ -50,7 +50,8 @@ class BubbleContentsWrapper : public content::WebContentsDelegate,
   BubbleContentsWrapper(content::BrowserContext* browser_context,
                         int task_manager_string_id,
                         bool enable_extension_apis,
-                        bool webui_resizes_host);
+                        bool webui_resizes_host,
+                        bool esc_closes_ui);
   ~BubbleContentsWrapper() override;
 
   // content::WebContentsDelegate:
@@ -92,6 +93,8 @@ class BubbleContentsWrapper : public content::WebContentsDelegate,
   // If true will allow the wrapped WebContents to automatically resize its
   // RenderWidgetHostView and send back updates to `Host` for the new size.
   const bool webui_resizes_host_;
+  // If true will cause the ESC key to close the UI during pre-handling.
+  const bool esc_closes_ui_;
   base::WeakPtr<BubbleContentsWrapper::Host> host_;
   std::unique_ptr<content::WebContents> web_contents_;
 };
@@ -107,11 +110,13 @@ class BubbleContentsWrapperT : public BubbleContentsWrapper {
                          content::BrowserContext* browser_context,
                          int task_manager_string_id,
                          bool enable_extension_apis = false,
-                         bool webui_resizes_host = true)
+                         bool webui_resizes_host = true,
+                         bool esc_closes_ui = true)
       : BubbleContentsWrapper(browser_context,
                               task_manager_string_id,
                               enable_extension_apis,
-                              webui_resizes_host),
+                              webui_resizes_host,
+                              esc_closes_ui),
         webui_url_(webui_url) {}
 
   void ReloadWebContents() override {
