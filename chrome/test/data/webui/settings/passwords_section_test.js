dddadefaf1c253052e34ae2e21e04d817c5c65da
Elizabeth Popova <lizapopova@google.com>
2021-09-23 09:54:24 +0000

    [Passwords] Remove ShowPasswordMixin from password_edit_dialog
    Cleaned up and simplified the logic when the password is a federation
    credential. This CL does NOT introduce any behavioral change.
    
    Initially, the dialog was opened in the view mode for both federation
    credentians and regular passwords. Later on, the edit functionality has
    been implemented for regular passwords and guarded as part of
    |isEditDialog_| boolean.
    
    Now the edit mode is the default behavior for regular passwords, so
    ShowPasswordMixin methods are called only for the federation credential
    and sometimes are even unreachable. The logic can be simplified
    without using ShowPasswordMixin. Also, since we always require user
    authentication before opening the edit dialog, it is always opened with
    the plaintext password available.
    
    The removal of ShowPasswordMixin also unblocks the implementation of the
    add functionality, as now |entry| can be made optional.
    
    Bug: 1236053
    Change-Id: I424991d982c310b6385bb5b9a708a598328afc1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138807
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924238}

--- chrome/test/data/webui/settings/passwords_section_test.js
+++ chrome/test/data/webui/settings/passwords_section_test.js
@@ -206,19 +206,16 @@ function detailsDialogPartsAreShownCorrectly(passwordDialog) {
  */
 async function changeSavedPasswordTestHelper(
     editDialog, entryIds, passwordManager) {
-  const PASSWORD1 = 'hello_world';
-  const USERNAME1 = 'new_username';
-  editDialog.set('entry.password', PASSWORD1);
-  assertEquals(PASSWORD1, editDialog.$.passwordInput.value);
+  const NEW_USERNAME = 'new_username';
+  const NEW_PASSWORD = 'new_password';
 
-  // Empty password should be consider invalid and disables the save button.
+  // Empty password should be considered invalid and disable the save button.
   editDialog.$.passwordInput.value = '';
   assertTrue(editDialog.$.passwordInput.invalid);
   assertTrue(editDialog.$.actionButton.disabled);
 
-  const PASSWORD2 = 'hello_world_2';
-  editDialog.$.usernameInput.value = USERNAME1;
-  editDialog.$.passwordInput.value = PASSWORD2;
+  editDialog.$.usernameInput.value = NEW_USERNAME;
+  editDialog.$.passwordInput.value = NEW_PASSWORD;
   assertFalse(editDialog.$.passwordInput.invalid);
   assertFalse(editDialog.$.actionButton.disabled);
 
@@ -227,8 +224,8 @@ async function changeSavedPasswordTestHelper(
   // Check that the changeSavedPassword is called with the right arguments.
   const {ids, newUsername, newPassword} =
       await passwordManager.whenCalled('changeSavedPassword');
-  assertEquals(USERNAME1, newUsername);
-  assertEquals(PASSWORD2, newPassword);
+  assertEquals(NEW_USERNAME, newUsername);
+  assertEquals(NEW_PASSWORD, newPassword);
 
   assertEquals(entryIds.length, ids.length);
   entryIds.forEach(entryId => assertTrue(ids.includes(entryId)));
@@ -1146,11 +1143,14 @@ suite('PasswordsSection', function() {
   });
 
   test('editDialogChangeUsernameFailsWhenReused', async function() {
-
-    const accountEntry = createMultiStorePasswordEntry(
-        {url: 'goo.gl', username: 'bart', accountId: 0});
-    const editDialog = elementFactory.createPasswordEditDialog(accountEntry);
-    editDialog.usernamesForSameOrigin = new Set(['mark', 'bart']);
+    const accountPasswords = [
+      createMultiStorePasswordEntry(
+          {url: 'goo.gl', username: 'bart', accountId: 0}),
+      createMultiStorePasswordEntry(
+          {url: 'goo.gl', username: 'mark', accountId: 0})
+    ];
+    const editDialog = elementFactory.createPasswordEditDialog(
+        accountPasswords[0], accountPasswords);
 
     editDialog.$.usernameInput.value = 'mark';
     assertTrue(editDialog.$.usernameInput.invalid);
@@ -1161,7 +1161,7 @@ suite('PasswordsSection', function() {
     assertFalse(editDialog.$.actionButton.disabled);
 
     changeSavedPasswordTestHelper(
-        editDialog, [accountEntry.accountId], passwordManager);
+        editDialog, [accountPasswords[0].accountId], passwordManager);
   });
 
   test('editDialogChangeUsernameWhenReusedForDifferentStore', async function() {
