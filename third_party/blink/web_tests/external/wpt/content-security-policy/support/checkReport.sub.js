02ff4d46398b6f257401ecb148a4559640e68b40
Rodney Ding <rodneyding@google.com>
2021-09-23 21:59:55 +0000

    Add wpt tests for Document-Reporting Endpoints header
    - These tests describe the desired behaviour according to the spec
    - This includes the following cases:
      1) Sending reports to default endpoint
      2) Setting Reporting-Endpoints header with path-absolute url
      3) Sending reports to multiple named endpoints
      4) Endpoint with the same name gets overridden
      5) Document has both Report-To and Reporting-Endpoints header
      6) Reports are not batched for same endpoint, same url in two frame
      7) Reports are not sent after navigating from document with
         Reporting-Endpoints header to same url without header
    - Update uuid used in various Reporting-Endpoints header's reportID so
      they're actually unique among tests.
    
    Bug: 1062359
    Change-Id: I42f268e8421407d3f9b745b7d762c4775f247500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3060743
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924487}

--- third_party/blink/web_tests/external/wpt/content-security-policy/support/checkReport.sub.js
+++ third_party/blink/web_tests/external/wpt/content-security-policy/support/checkReport.sub.js
@@ -127,7 +127,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       reportCountReport.onload = reportCountTest.step_func(function () {
         var data = JSON.parse(reportCountReport.responseText);
 
-        assert_equals(data.report_count, reportCount, "Report count was not what was expected.");
+        assert_equals(data.report_count, +reportCount, "Report count was not what was expected.");
 
         reportCountTest.done();
       });
