8c758eac3c40fe1efff32c789fa6529fd4c9c56a
Danil Somsikov <dsv@chromium.org>
2021-09-23 10:59:38 +0000

    Add UMA metric to measure "show CORS errors in console" setting usage.
    Bug: 1251176
    Change-Id: I44b3f3b0f400f6c7a5d43045fa502152c1c5fe85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168345
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924245}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -151,6 +151,8 @@ static const char kDevToolsLinearMemoryInspectorRevealedFromHistogram[] =
 static const char kDevToolsLinearMemoryInspectorTargetHistogram[] =
     "DevTools.LinearMemoryInspector.Target";
 static const char kDevToolsLanguageHistogram[] = "DevTools.Language";
+static const char kDevToolsConsoleShowsCorsErrorsHistogram[] =
+    "DevTools.ConsoleShowsCorsErrors";
 
 static const char kRemotePageActionInspect[] = "inspect";
 static const char kRemotePageActionReload[] = "reload";
@@ -1331,7 +1333,8 @@ void DevToolsUIBindings::RecordEnumeratedHistogram(const std::string& name,
       name == kDevToolsDeveloperResourceSchemeHistogram ||
       name == kDevToolsLinearMemoryInspectorRevealedFromHistogram ||
       name == kDevToolsLinearMemoryInspectorTargetHistogram ||
-      name == kDevToolsLanguageHistogram)
+      name == kDevToolsLanguageHistogram ||
+      name == kDevToolsConsoleShowsCorsErrorsHistogram)
     base::UmaHistogramExactLinear(name, sample, boundary_value);
   else
     frontend_host_->BadMessageReceived();
