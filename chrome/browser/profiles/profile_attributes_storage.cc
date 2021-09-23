75666245bd1515dd373ae78a822508dfd60870d5
David Roger <droger@chromium.org>
2021-09-23 09:18:26 +0000

    [profiles] Cleanup DCHECK usage in profile_attributes_storage.cc
    Change-Id: I025786f0d11b3fbdc00a8ff883ecccfe748d9db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172526
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924235}

--- chrome/browser/profiles/profile_attributes_storage.cc
+++ chrome/browser/profiles/profile_attributes_storage.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/cxx17_backports.h"
 #include "base/files/file_util.h"
@@ -760,7 +761,7 @@ void ProfileAttributesStorage::NotifyProfileUserManagementAcceptanceChanged(
 
 std::string ProfileAttributesStorage::StorageKeyFromProfilePath(
     const base::FilePath& profile_path) const {
-  DCHECK(user_data_dir_ == profile_path.DirName());
+  DCHECK_EQ(user_data_dir_, profile_path.DirName());
   return profile_path.BaseName().AsUTF8Unsafe();
 }
 
