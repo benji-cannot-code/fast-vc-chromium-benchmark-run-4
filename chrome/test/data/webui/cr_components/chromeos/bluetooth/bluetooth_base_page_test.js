f43a1f53896bc867814a8b45e6c7d4f351ddfd29
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-09-23 00:54:20 +0000

    [CrOS Bluetooth] Add buttons to bluetooth base page
    This CL adds done, confirm buttons  and also updates the title
    to bluetooth base page.
    
    Screenshot: https://screenshot.googleplex.com/BcMsUYMf8YeVZS3.png
    
    Bug: 1010321
    Change-Id: Ifadfa8e1227eb3106bf5ff902ff0e6896d216993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171980
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#924124}

--- chrome/test/data/webui/cr_components/chromeos/bluetooth/bluetooth_base_page_test.js
+++ chrome/test/data/webui/cr_components/chromeos/bluetooth/bluetooth_base_page_test.js
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://bluetooth-pairing/strings.m.js';
 
 import {SettingsBluetoothBasePageElement} from 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_base_page.js';
+import {ButtonState} from 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_types.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {assertEquals, assertTrue} from '../../../chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from '../../../chai_assert.js';
 // clang-format on
 
 suite('CrComponentsBluetoothBasePageTest', function() {
@@ -22,11 +23,57 @@ suite('CrComponentsBluetoothBasePageTest', function() {
     flush();
   });
 
+  /**
+   * @param {!HTMLElement} button
+   * @return {boolean}
+   */
+  function isButtonShownAndEnabled(button) {
+    return !button.hidden && !button.disabled;
+  }
+
+  /**
+   * @param {!HTMLElement} button
+   * @return {boolean}
+   */
+  function isButtonShownAndDisabled(button) {
+    return !button.hidden && button.disabled;
+  }
+
+  /**
+   * @param {!ButtonState} state
+   */
+  function setStateForAllButtons(state) {
+    bluetoothBasePage.buttonBarState = {
+      cancel: state,
+      pair: state,
+    };
+    flush();
+  }
+
   test('Title is shown', function() {
     const title = bluetoothBasePage.shadowRoot.querySelector('#title');
     assertTrue(!!title);
     assertEquals(
         bluetoothBasePage.i18n('bluetoothPairNewDevice'),
         title.textContent.trim());
+  });
+
+  test('Button states', function() {
+    const getCancelButton = () =>
+        bluetoothBasePage.shadowRoot.querySelector('#cancel');
+    const getPairButton = () =>
+        bluetoothBasePage.shadowRoot.querySelector('#pair');
+
+    setStateForAllButtons(ButtonState.ENABLED);
+    assertTrue(isButtonShownAndEnabled(getCancelButton()));
+    assertTrue(isButtonShownAndEnabled(getPairButton()));
+
+    setStateForAllButtons(ButtonState.DISABLED);
+    assertTrue(isButtonShownAndDisabled(getCancelButton()));
+    assertTrue(isButtonShownAndDisabled(getPairButton()));
+
+    setStateForAllButtons(ButtonState.HIDDEN);
+    assertFalse(!!getCancelButton());
+    assertFalse(!!getPairButton());
   });
 });
---
+++ ui/webui/resources/cr_components/chromeos/bluetooth/bluetooth_types.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @enum {number} */
+export const ButtonState = {
+  ENABLED: 1,
+  DISABLED: 2,
+  HIDDEN: 3,
+};
+
+/** @enum {number} */
+export const ButtonName = {
+  CANCEL: 1,
+  PAIR: 2,
+};
+
+/**
+ * @typedef {{
+ *   cancel: (!ButtonState),
+ *   pair: (!ButtonState),
+ * }}
+ */
+export let ButtonBarState;
