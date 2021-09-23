7eac87ae7d158dc5478ead584370d89d934ed304
Ben Pastene <bpastene@chromium.org>
2021-09-23 17:38:54 +0000

    Disable result_adapter for all runs of chrome_sizes on perf bots.
    ResultDB integration in the sizes script was added in
    crrev.com/c/3146192.
    
    Consequently, result_adapter is no longer needed to stream its results.
    And can cause problems when the RDB-results experiment is enabled, eg:
    https://ci.chromium.org/p/chrome/builders/ci/linux-builder-perf/345947?
    
    So signaling to the recipe that chrome_sizes has rdb integration and
    doesn't need result-adapter should fix that.
    
    Bug: 1135718
    Change-Id: I92defc2abead587b236546ded661dfc42ff8c3ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176366
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Chan Li <chanli@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#924368}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -470,6 +470,9 @@ BUILDERS = {
             'name': 'chrome_sizes',
             'isolate': 'chrome_sizes',
             'type': TEST_TYPES.GENERIC,
+            'resultdb': {
+                'has_native_resultdb_integration': True,
+            },
         }],
         'dimension': {
             'cpu': 'x86-64',
@@ -488,6 +491,9 @@ BUILDERS = {
             'name': 'chrome_sizes',
             'isolate': 'chrome_sizes',
             'type': TEST_TYPES.GENERIC,
+            'resultdb': {
+                'has_native_resultdb_integration': True,
+            },
         }],
         'dimension': {
             'cpu': 'x86-64',
@@ -503,6 +509,9 @@ BUILDERS = {
             'name': 'chrome_sizes',
             'isolate': 'chrome_sizes',
             'type': TEST_TYPES.GENERIC,
+            'resultdb': {
+                'has_native_resultdb_integration': True,
+            },
         }],
         'dimension': {
             'cpu': 'x86',
@@ -518,6 +527,9 @@ BUILDERS = {
             'name': 'chrome_sizes',
             'isolate': 'chrome_sizes',
             'type': TEST_TYPES.GENERIC,
+            'resultdb': {
+                'has_native_resultdb_integration': True,
+            },
         }],
         'dimension': {
             'cpu': 'x86',
@@ -533,6 +545,9 @@ BUILDERS = {
             'name': 'chrome_sizes',
             'isolate': 'chrome_sizes',
             'type': TEST_TYPES.GENERIC,
+            'resultdb': {
+                'has_native_resultdb_integration': True,
+            },
         }],
         'dimension': {
             'cpu': 'x86-64',
@@ -1492,9 +1507,14 @@ def generate_performance_test(tester_config, test, builder_name):
     ]
   }
 
-  # Enable Result DB on all perf test bots. Builders with names including
-  # "builder-perf" are used for compiling only, and do not run perf tests.
-  if 'builder-perf' not in builder_name:
+  if test.get('resultdb'):
+    result['resultdb'] = test['resultdb'].copy()
+  elif 'builder-perf' not in builder_name:
+    # Enable Result DB on all perf test bots. Builders with names including
+    # "builder-perf" are used for compiling only, and do not run perf tests.
+    # TODO(crbug.com/1135718): Replace the following line by specifying either
+    # "result_format" for GTests, or "has_native_resultdb_integration" for all
+    # other tests.
     result['resultdb'] = {'enable': True}
 
   # For now we either get shards from the number of devices specified
