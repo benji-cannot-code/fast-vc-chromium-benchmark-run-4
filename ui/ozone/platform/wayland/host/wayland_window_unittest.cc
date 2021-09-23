e4a6a9020d033659b4bff033e956f259a00cfdc5
Alexander Dunaev <adunaev@igalia.com>
2021-09-23 09:44:06 +0000

    [ozone/wayland] Postponed setting the geometry to the next frame update.
    This is a follow-up to [1] and to our discussion with fangzhough@ in
    which we agreed to do this.
    
    In this CL, an instant call to SetWindowGeometry() that was done in
    WaylandToplevelWindow::UpdateDecorations() is replaced by setting a flag
    that will trigger setting the geometry in the next frame update, which
    happens via WaylandToplevelWindow::UpdateVisualSize().
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3157883
    
    Bug: 1223005
    Change-Id: Ia5096571563b744fbea687d9acdfdb62a2c69f29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171058
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#924236}

--- ui/ozone/platform/wayland/host/wayland_window_unittest.cc
+++ ui/ozone/platform/wayland/host/wayland_window_unittest.cc
@@ -325,6 +325,11 @@ TEST_P(WaylandWindowTest, UpdateVisualSizeConfiguresWaylandWindow) {
 // some sides of insets divides by 2 with remainder.
 TEST_P(WaylandWindowTest, SetDecorationInsets) {
   const auto kNormalBounds = gfx::Rect{0, 0, 956, 556};
+  const auto kHiDpiScale = 2;
+  const auto kHiDpiBounds = gfx::ScaleToRoundedRect(kNormalBounds, kHiDpiScale);
+
+  window_->SetBounds(kNormalBounds);
+
   uint32_t serial = 0;
   auto state = InitializeWlArrayWithActivatedState();
 
@@ -339,10 +344,7 @@ TEST_P(WaylandWindowTest, SetDecorationInsets) {
 
   Sync();
 
-  // window_->set_update_visual_size_immediately(false);
-  window_->SetBounds(kNormalBounds);
-  // auto* mock_surface = server_.GetObject<wl::MockSurface>(
-  //     window_->root_surface()->GetSurfaceId());
+  // Set insets for normal DPI.
   const gfx::Insets kDecorationInsets = {24, 28, 32, 28};
   auto bounds_with_insets = kNormalBounds;
   bounds_with_insets.Inset(kDecorationInsets);
@@ -352,6 +354,10 @@ TEST_P(WaylandWindowTest, SetDecorationInsets) {
                                 bounds_with_insets.width(),
                                 bounds_with_insets.height()));
   window_->SetDecorationInsets(kDecorationInsets);
+  // Setting the decoration insets does not trigger the immediate update of the
+  // window geometry.  Emulate updating the visual size (sending the frame
+  // update) for that.
+  window_->UpdateVisualSize(kNormalBounds.size());
 
   Sync();
 
@@ -365,10 +371,9 @@ TEST_P(WaylandWindowTest, SetDecorationInsets) {
 
   Sync();
 
-  // Change scale. WaylandToplevelWindow should still configure pending bounds
-  // correctly.
-  EXPECT_CALL(delegate_, OnBoundsChanged(_)).Times(1);
-  output->SetScale(2);
+  // Change scale.  This is the only time when we expect the bounds to change.
+  EXPECT_CALL(delegate_, OnBoundsChanged(Eq(kHiDpiBounds))).Times(1);
+  output->SetScale(kHiDpiScale);
   output->Flush();
 
   Sync();
@@ -380,6 +385,10 @@ TEST_P(WaylandWindowTest, SetDecorationInsets) {
                                 bounds_with_insets.width(),
                                 bounds_with_insets.height()));
   window_->SetDecorationInsets(kDecorationInsets_2x);
+  // Setting the decoration insets does not trigger the immediate update of the
+  // window geometry.  Emulate updating the visual size (sending the frame
+  // update) for that.
+  window_->UpdateVisualSize(kHiDpiBounds.size());
 
   Sync();
 
