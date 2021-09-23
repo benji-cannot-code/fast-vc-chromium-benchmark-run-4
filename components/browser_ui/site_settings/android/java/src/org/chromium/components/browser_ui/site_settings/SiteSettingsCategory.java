00ee61fe58f31cf21e306ab80556bb9e721ec477
Shu Yang <shuyng@google.com>
2021-09-23 22:58:10 +0000

    [DesktopSite] Request Desktop Site Global Setting
    Add content setting REQUEST_DESKTOP_SITE;
    Add the content setting to the Android SiteSettings UI;
    Hook up to TabImpl to set the user agent when creating a tab.
    
    Bug: 1243758
    Change-Id: I54ccd098a635f5325c959a707488b019803605bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169455
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924518}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/SiteSettingsCategory.java
@@ -43,7 +43,7 @@ public class SiteSettingsCategory {
             Type.CLIPBOARD, Type.COOKIES, Type.IDLE_DETECTION, Type.DEVICE_LOCATION,
             Type.JAVASCRIPT, Type.MICROPHONE, Type.NFC, Type.NOTIFICATIONS, Type.POPUPS,
             Type.PROTECTED_MEDIA, Type.SENSORS, Type.SOUND, Type.USB, Type.VIRTUAL_REALITY,
-            Type.USE_STORAGE, Type.AUTO_DARK_WEB_CONTENT})
+            Type.USE_STORAGE, Type.AUTO_DARK_WEB_CONTENT, Type.REQUEST_DESKTOP_SITE})
     @Retention(RetentionPolicy.SOURCE)
     public @interface Type {
         // All updates here must also be reflected in {@link #preferenceKey(int)
@@ -72,10 +72,11 @@ public class SiteSettingsCategory {
         int VIRTUAL_REALITY = 21;
         int USE_STORAGE = 22;
         int AUTO_DARK_WEB_CONTENT = 23;
+        int REQUEST_DESKTOP_SITE = 24;
         /**
          * Number of handled categories used for calculating array sizes.
          */
-        int NUM_ENTRIES = 24;
+        int NUM_ENTRIES = 25;
     }
 
     private final BrowserContextHandle mBrowserContextHandle;
@@ -149,7 +150,7 @@ public class SiteSettingsCategory {
     /**
      * Convert Type into {@link ContentSettingsType}.
      */
-    public static int contentSettingsType(@Type int type) {
+    public static @ContentSettingsType int contentSettingsType(@Type int type) {
         // This switch statement is ordered by types alphabetically.
         switch (type) {
             case Type.ADS:
@@ -172,6 +173,8 @@ public class SiteSettingsCategory {
                 return ContentSettingsType.CLIPBOARD_READ_WRITE;
             case Type.COOKIES:
                 return ContentSettingsType.COOKIES;
+            case Type.REQUEST_DESKTOP_SITE:
+                return ContentSettingsType.REQUEST_DESKTOP_SITE;
             case Type.DEVICE_LOCATION:
                 return ContentSettingsType.GEOLOCATION;
             case Type.IDLE_DETECTION:
@@ -199,7 +202,7 @@ public class SiteSettingsCategory {
             // case Type.ALL_SITES
             // case Type.USE_STORAGE
             default:
-                return -1; // Conversion unavailable.
+                return ContentSettingsType.DEFAULT; // Conversion unavailable.
         }
     }
 
@@ -246,6 +249,8 @@ public class SiteSettingsCategory {
                 return "clipboard";
             case Type.COOKIES:
                 return "cookies";
+            case Type.REQUEST_DESKTOP_SITE:
+                return "request_desktop_site";
             case Type.DEVICE_LOCATION:
                 return "device_location";
             case Type.IDLE_DETECTION:
