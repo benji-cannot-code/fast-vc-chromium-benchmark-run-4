9852e3fa42e103c38c9af41a1a32579b9557ad58
John Abd-El-Malek <jam@chromium.org>
2021-09-23 00:45:24 +0000

    Simplify service process code now that ProcessHostOnUI is on by default.
    Bug: 904556
    Change-Id: I9e0d097bbe66311cb5548174f9b8511ff6adde98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176193
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Quick-Run: John Abd-El-Malek <jam@chromium.org>
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#924121}

--- content/browser/service_process_host_impl.cc
+++ content/browser/service_process_host_impl.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/service_process_host.h"
-#include "content/public/common/content_features.h"
 #include "mojo/public/cpp/bindings/generic_pending_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
@@ -26,11 +25,10 @@ namespace content {
 
 namespace {
 
-// Internal helper to track running service processes. Usage of this class is
-// split across the IO thread and UI thread.
+// Internal helper to track running service processes.
 class ServiceProcessTracker {
  public:
-  ServiceProcessTracker() : ui_task_runner_(GetUIThreadTaskRunner({})) {}
+  ServiceProcessTracker() = default;
 
   ServiceProcessTracker(const ServiceProcessTracker&) = delete;
   ServiceProcessTracker& operator=(const ServiceProcessTracker&) = delete;
@@ -39,65 +37,33 @@ class ServiceProcessTracker {
 
   ServiceProcessInfo AddProcess(const base::Process& process,
                                 const std::string& service_interface_name) {
-    // TODO(jam): remove this class or at least the locks once we only have UI
-    // thread mode.
-    DCHECK_CURRENTLY_ON(base::FeatureList::IsEnabled(features::kProcessHostOnUI)
-                            ? BrowserThread::UI
-                            : BrowserThread::IO);
-    base::AutoLock lock(processes_lock_);
+    DCHECK_CURRENTLY_ON(BrowserThread::UI);
     auto id = GenerateNextId();
     ServiceProcessInfo& info = processes_[id];
     info.service_process_id = id;
     info.pid = process.Pid();
     info.service_interface_name = service_interface_name;
-    if (base::FeatureList::IsEnabled(features::kProcessHostOnUI)) {
-      for (auto& observer : observers_)
-        observer.OnServiceProcessLaunched(info);
-    } else {
-      ui_task_runner_->PostTask(
-          FROM_HERE,
-          base::BindOnce(&ServiceProcessTracker::NotifyLaunchOnUIThread,
-                         base::Unretained(this), info));
-    }
+    for (auto& observer : observers_)
+      observer.OnServiceProcessLaunched(info);
     return info;
   }
 
   void NotifyTerminated(ServiceProcessId id) {
-    DCHECK_CURRENTLY_ON(base::FeatureList::IsEnabled(features::kProcessHostOnUI)
-                            ? BrowserThread::UI
-                            : BrowserThread::IO);
-    base::AutoLock lock(processes_lock_);
+    DCHECK_CURRENTLY_ON(BrowserThread::UI);
     auto iter = processes_.find(id);
     DCHECK(iter != processes_.end());
 
-    if (base::FeatureList::IsEnabled(features::kProcessHostOnUI)) {
-      for (auto& observer : observers_)
-        observer.OnServiceProcessTerminatedNormally(iter->second);
-    } else {
-      ui_task_runner_->PostTask(
-          FROM_HERE,
-          base::BindOnce(&ServiceProcessTracker::NotifyTerminatedOnUIThread,
-                         base::Unretained(this), iter->second));
-    }
+    for (auto& observer : observers_)
+      observer.OnServiceProcessTerminatedNormally(iter->second);
     processes_.erase(iter);
   }
 
   void NotifyCrashed(ServiceProcessId id) {
-    DCHECK_CURRENTLY_ON(base::FeatureList::IsEnabled(features::kProcessHostOnUI)
-                            ? BrowserThread::UI
-                            : BrowserThread::IO);
-    base::AutoLock lock(processes_lock_);
+    DCHECK_CURRENTLY_ON(BrowserThread::UI);
     auto iter = processes_.find(id);
     DCHECK(iter != processes_.end());
-    if (base::FeatureList::IsEnabled(features::kProcessHostOnUI)) {
-      for (auto& observer : observers_)
-        observer.OnServiceProcessCrashed(iter->second);
-    } else {
-      ui_task_runner_->PostTask(
-          FROM_HERE,
-          base::BindOnce(&ServiceProcessTracker::NotifyCrashedOnUIThread,
-                         base::Unretained(this), iter->second));
-    }
+    for (auto& observer : observers_)
+      observer.OnServiceProcessCrashed(iter->second);
     processes_.erase(iter);
   }
 
@@ -115,7 +81,6 @@ class ServiceProcessTracker {
 
   std::vector<ServiceProcessInfo> GetProcesses() {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    base::AutoLock lock(processes_lock_);
     std::vector<ServiceProcessInfo> processes;
     for (const auto& entry : processes_)
       processes.push_back(entry.second);
@@ -139,16 +104,12 @@ class ServiceProcessTracker {
   }
 
   ServiceProcessId GenerateNextId() {
-    DCHECK_CURRENTLY_ON(base::FeatureList::IsEnabled(features::kProcessHostOnUI)
-                            ? BrowserThread::UI
-                            : BrowserThread::IO);
+    DCHECK_CURRENTLY_ON(BrowserThread::UI);
     return service_process_id_generator_.GenerateNextId();
   }
 
-  const scoped_refptr<base::TaskRunner> ui_task_runner_;
   ServiceProcessId::Generator service_process_id_generator_;
 
-  base::Lock processes_lock_;
   std::map<ServiceProcessId, ServiceProcessInfo> processes_;
 
   // Observers are owned and used exclusively on the UI thread.
@@ -238,13 +199,10 @@ void ServiceProcessHost::RemoveObserver(Observer* observer) {
 void ServiceProcessHost::Launch(mojo::GenericPendingReceiver receiver,
                                 Options options) {
   DCHECK(receiver.interface_name().has_value());
-  auto task_runner = base::FeatureList::IsEnabled(features::kProcessHostOnUI)
-                         ? GetUIThreadTaskRunner({})
-                         : GetIOThreadTaskRunner({});
-  if (task_runner->BelongsToCurrentThread()) {
+  if (GetUIThreadTaskRunner({})->BelongsToCurrentThread()) {
     LaunchServiceProcess(std::move(receiver), std::move(options));
   } else {
-    task_runner->PostTask(
+    GetUIThreadTaskRunner({})->PostTask(
         FROM_HERE, base::BindOnce(&LaunchServiceProcess, std::move(receiver),
                                   std::move(options)));
   }
