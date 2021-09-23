d60aaceb6379494a31b47fc7f4184fe1fbeac691
Ashley Prasad <ashleydp@google.com>
2021-09-23 22:36:21 +0000

    Diagnostics: Add skeleton diagnostics-sticky-banner component
    - Adds skeleton for banner.
    - Tests banner can be added to DOM.
    
    Bug: 1197335
    Test: ./out/Default/browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I65e78f39f8d43257fc5419074d7e6facf5ddd2ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177067
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924507}

---
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_sticky_banner_test.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {DiagnosticsStickyBannerElement} from 'chrome://diagnostics/diagnostics_sticky_banner.js';
+
+import {assertFalse, assertTrue} from '../../chai_assert.js';
+import {flushTasks} from '../../test_util.js';
+
+export function diagnosticsStickyBannerTestSuite() {
+  /** @type {?DiagnosticsStickyBannerElement} */
+  let diagnosticsStickyBannerElement = null;
+
+  setup(() => {
+    document.body.innerHTML = '';
+  });
+
+  teardown(() => {
+    diagnosticsStickyBannerElement.remove();
+    diagnosticsStickyBannerElement = null;
+  });
+
+  function initializeDiagnosticsStickyBanner() {
+    assertFalse(!!diagnosticsStickyBannerElement);
+
+    // Add the sticky banner to the DOM.
+    diagnosticsStickyBannerElement =
+        /** @type {!DiagnosticsStickyBannerElement} */ (
+            document.createElement('diagnostics-sticky-banner'));
+    assertTrue(!!diagnosticsStickyBannerElement);
+    document.body.appendChild(diagnosticsStickyBannerElement);
+
+    return flushTasks();
+  }
+
+  test('BannerInitializedCorrectly', () => {
+    return initializeDiagnosticsStickyBanner().then(() => {
+      assertTrue(!!diagnosticsStickyBannerElement);
+    });
+  });
+}
