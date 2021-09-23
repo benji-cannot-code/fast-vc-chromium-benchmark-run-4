dc5d1780042f03419c24833d84043ed0bec04ad1
Ashley Prasad <ashleydp@google.com>
2021-09-23 23:55:38 +0000

    Diagnostics: Add window event listeners for custom events
    - Adds custom event listeners for 'show-caution-banner' and 'dismiss-
      caution-banner' events.
    - Tests for showing and hiding of banner triggered by custom events.
    
    Bug: 1197335
    Test: ./out/Default/browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I9c34f69e8e4446a0d8f7b708206b49cb56b2f829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176904
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924548}

--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_sticky_banner_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_sticky_banner_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {DiagnosticsStickyBannerElement} from 'chrome://diagnostics/diagnostics_sticky_banner.js';
 
-import {assertFalse, assertTrue} from '../../chai_assert.js';
+import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 import {flushTasks, isVisible} from '../../test_util.js';
 
 import * as dx_utils from './diagnostics_test_utils.js';
@@ -64,6 +64,35 @@ export function diagnosticsStickyBannerTestSuite() {
     return flushTasks();
   }
 
+  /**
+   * Triggers 'dismiss-caution-banner' custom event.
+   * @return {!Promise}
+   */
+  function triggerDismissBannerEvent() {
+    window.dispatchEvent(new CustomEvent('dismiss-caution-banner', {
+      bubbles: true,
+      composed: true,
+    }));
+
+    return flushTasks();
+  }
+
+  /**
+   * Triggers 'show-caution-banner' custom event with correctly configured event
+   * detail object based on provided message.
+   * @param {string} message
+   * @return {!Promise}
+   */
+  function triggerShowBannerEvent(message) {
+    window.dispatchEvent(new CustomEvent('show-caution-banner', {
+      bubbles: true,
+      composed: true,
+      detail: {message},
+    }));
+
+    return flushTasks();
+  }
+
   test('BannerInitializedCorrectly', () => {
     return initializeDiagnosticsStickyBanner().then(() => {
       assertFalse(isVisible(getBanner()));
@@ -80,6 +109,41 @@ export function diagnosticsStickyBannerTestSuite() {
         })
         .then(() => setBannerMessage(''))
         .then(() => {
+          assertFalse(isVisible(getBanner()));
+          dx_utils.assertElementDoesNotContainText(getBannerMsg(), testMessage);
+        });
+  });
+
+  test('BannerHandlesShowCautionBannerEvent', () => {
+    const bannerText1 = 'Infomational banner 1';
+    const bannerText2 = 'Infomational banner 2';
+    return initializeDiagnosticsStickyBanner()
+        .then(() => triggerShowBannerEvent(bannerText1))
+        .then(() => {
+          assertEquals(
+              bannerText1, diagnosticsStickyBannerElement.bannerMessage);
+          assertTrue(isVisible(getBanner()));
+          dx_utils.assertElementContainsText(getBannerMsg(), bannerText1);
+        })
+        .then(() => triggerShowBannerEvent(bannerText2))
+        .then(() => {
+          assertEquals(
+              bannerText2, diagnosticsStickyBannerElement.bannerMessage);
+          assertTrue(isVisible(getBanner()));
+          dx_utils.assertElementContainsText(getBannerMsg(), bannerText2);
+        });
+  });
+
+  test('BannerHandlesDismissCautionBannerEvent', () => {
+    const testMessage = 'Infomational banner';
+    return initializeDiagnosticsStickyBanner()
+        .then(() => triggerShowBannerEvent(testMessage))
+        .then(() => {
+          dx_utils.assertElementContainsText(getBannerMsg(), testMessage);
+        })
+        .then(() => triggerDismissBannerEvent())
+        .then(() => {
+          assertEquals('', diagnosticsStickyBannerElement.bannerMessage);
           assertFalse(isVisible(getBanner()));
           dx_utils.assertElementDoesNotContainText(getBannerMsg(), testMessage);
         });
