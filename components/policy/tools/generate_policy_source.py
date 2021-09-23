2797e89f91db4b831b7d5e50a102036ec5444f89
Jana Grill <janagrill@google.com>
2021-09-23 22:35:32 +0000

    Remove strange side-effect of _WritePolicyProto
    No longer pass the fields array as a parameter to _WritePolicyProto and
    instead add items to it directly.
    
    Additionally implement a test for _WritePolicyProto.
    
    This CL is preparation for a larger change including refactoring
    ChromeSettingsProto message into smaller sub-messages and is intended to
    make sure that nothing is broken when the larger change is made.
    
    Bug: 1237044
    Test: python3 generate_policy_source_test.py
    Change-Id: Ieb7988a39c88f78959b46b72996bd3f9dbe97f66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179005
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924506}

--- components/policy/tools/generate_policy_source.py
+++ components/policy/tools/generate_policy_source.py
@@ -1464,7 +1464,7 @@ import "policy_common_definitions{full_runtime_suffix}.proto";
 RESERVED_IDS = 2
 
 
-def _WritePolicyProto(f, policy, fields):
+def _WritePolicyProto(f, policy):
   _OutputComment(f, policy.caption + '\n\n' + policy.desc)
   if policy.items is not None:
     _OutputComment(f, '\nValid values:')
@@ -1485,10 +1485,6 @@ def _WritePolicyProto(f, policy, fields):
   f.write('  optional PolicyOptions policy_options = 1;\n')
   f.write('  optional %s %s = 2;\n' % (policy.protobuf_type, policy.name))
   f.write('}\n\n')
-  fields += [
-      '  optional %sProto %s = %s;\n' % (policy.name, policy.name,
-                                         policy.id + RESERVED_IDS)
-  ]
 
 
 def _WriteChromeSettingsProtobuf(policies,
@@ -1511,7 +1507,13 @@ def _WriteChromeSettingsProtobuf(policies,
     # Note: This protobuf also gets the unsupported policies, since it's an
     # exhaustive list of all the supported user policies on any platform.
     if not policy.is_device_only:
-      _WritePolicyProto(f, policy, fields)
+      # Write the individual policy proto into the file
+      _WritePolicyProto(f, policy)
+      # Add to |fields| in order to eventually add to ChromeSettingsProto
+      fields += [
+          '  optional %sProto %s = %s;\n' %
+          (policy.name, policy.name, policy.id + RESERVED_IDS)
+      ]
 
   f.write('// --------------------------------------------------\n'
           '// Big wrapper PB containing the above groups.\n\n'
