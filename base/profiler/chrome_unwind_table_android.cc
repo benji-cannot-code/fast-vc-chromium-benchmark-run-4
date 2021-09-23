ecb76602804be51fc32e9c1c062fc9cc443a8e37
Charlie Hu <chenleihu@google.com>
2021-09-23 23:25:19 +0000

    [Android Unwinder] Implement ChromeUnwindInfoAndroid
    This CL implements the parsing of android unwind info v2.
    
    Design Doc:
    https://docs.google.com/document/d/1IYTmGCJZoiQ242xPUZX1fATD6ivsjU1TAt_fPv74ocs/edit?usp=sharing
    
    Bug: 1240698
    Change-Id: I9bc75fa1accd813dc444040150a0f2bb07d4c3cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3115141
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/main@{#924530}

--- base/profiler/chrome_unwind_table_android.cc
+++ base/profiler/chrome_unwind_table_android.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/notreached.h"
 #include "base/numerics/checked_math.h"
+#include "base/profiler/chrome_unwind_info_android.h"
 
 namespace base {
 namespace {
--- base/profiler/chrome_unwind_table_android_unittest.cc
+++ base/profiler/chrome_unwind_table_android_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/profiler/chrome_unwind_table_android.h"
 
+#include "base/profiler/chrome_unwind_info_android.h"
 #include "base/test/gtest_util.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
