38303d0efd51e24c6491c54a71e36a03a13844e1
rbpotter <rbpotter@chromium.org>
2021-09-23 20:28:36 +0000

    Print Preview: Migrate destination dialog and deps to TypeScript
    destination_dialog.js
    destination_dialog_cros.js
    provisional_destination_resolver.js
    destination_dialog_css.js
    destination_list.js
    destination_list_item.js
    
    Bug: 1245834
    Change-Id: I27699ad4b2935b15a3cf2889b03f0592cdf1717f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3175052
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924444}

--- chrome/browser/resources/print_preview/ui/destination_list.ts
+++ chrome/browser/resources/print_preview/ui/destination_list.ts
@@ -11,22 +11,24 @@ import './print_preview_vars_css.js';
 import '../strings.m.js';
 import './throbber_css.js';
 
-import {ListPropertyUpdateBehavior, ListPropertyUpdateBehaviorInterface} from 'chrome://resources/js/list_property_update_behavior.m.js';
+import {ListPropertyUpdateBehavior} from 'chrome://resources/js/list_property_update_behavior.m.js';
+import {IronListElement} from 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
 import {afterNextRender, html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {Destination} from '../data/destination.js';
 
 const DESTINATION_ITEM_HEIGHT = 32;
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {ListPropertyUpdateBehaviorInterface}
- */
-const PrintPreviewDestinationListElementBase =
-    mixinBehaviors([ListPropertyUpdateBehavior], PolymerElement);
+export interface PrintPreviewDestinationListElement {
+  $: {
+    list: IronListElement,
+  };
+}
 
-/** @polymer */
+const PrintPreviewDestinationListElementBase =
+    mixinBehaviors([ListPropertyUpdateBehavior], PolymerElement) as
+    {new (): ListPropertyUpdateBehavior & PolymerElement};
+
 export class PrintPreviewDestinationListElement extends
     PrintPreviewDestinationListElementBase {
   static get is() {
@@ -39,37 +41,30 @@ export class PrintPreviewDestinationListElement extends
 
   static get properties() {
     return {
-      /** @type {Array<!Destination>} */
       destinations: Array,
 
-      /** @type {?RegExp} */
       searchQuery: Object,
 
-      /** @type {boolean} */
       loadingDestinations: {
         type: Boolean,
         value: false,
       },
 
-      /** @private {!Array<!Destination>} */
       matchingDestinations_: {
         type: Array,
         value: () => [],
       },
 
-      /** @private {boolean} */
       hasDestinations_: {
         type: Boolean,
         value: true,
       },
 
-      /** @private {boolean} */
       throbberHidden_: {
         type: Boolean,
         value: false,
       },
 
-      /** @private */
       hideList_: {
         type: Boolean,
         value: false,
@@ -77,22 +72,23 @@ export class PrintPreviewDestinationListElement extends
     };
   }
 
+  destinations: Destination[];
+  searchQuery: RegExp|null;
+  loadingDestinations: boolean;
+  private matchingDestinations_: Destination[];
+  private hasDestinations_: boolean;
+  private throbberHidden_: boolean;
+  private hideList_: boolean;
+
+  private boundUpdateHeight_: ((e: Event) => void)|null = null;
+
   static get observers() {
     return [
       'updateMatchingDestinations_(' +
           'destinations.*, searchQuery, loadingDestinations)',
-
     ];
   }
 
-  constructor() {
-    super();
-
-    /** @private {?function(Event)} */
-    this.boundUpdateHeight_ = null;
-  }
-
-  /** @override */
   connectedCallback() {
     super.connectedCallback();
 
@@ -100,11 +96,10 @@ export class PrintPreviewDestinationListElement extends
     window.addEventListener('resize', this.boundUpdateHeight_);
   }
 
-  /** @override */
   disconnectedCallback() {
     super.disconnectedCallback();
 
-    window.removeEventListener('resize', this.boundUpdateHeight_);
+    window.removeEventListener('resize', this.boundUpdateHeight_!);
     this.boundUpdateHeight_ = null;
   }
 
@@ -115,17 +110,12 @@ export class PrintPreviewDestinationListElement extends
    * (a known iron-list issue). The event needs to be fired while the list is
    * visible, so firing it immediately when the change occurs does not always
    * work.
-   * @private
    */
-  forceIronResize_() {
+  private forceIronResize_() {
     this.$.list.fire('iron-resize');
   }
 
-  /**
-   * @param {number=} numDestinations
-   * @private
-   */
-  updateHeight_(numDestinations) {
+  private updateHeight_(numDestinations?: number) {
     const count = numDestinations === undefined ?
         this.matchingDestinations_.length :
         numDestinations;
@@ -150,15 +140,13 @@ export class PrintPreviewDestinationListElement extends
         `${DESTINATION_ITEM_HEIGHT}px`;
   }
 
-  /** @private */
-  updateMatchingDestinations_() {
+  private updateMatchingDestinations_() {
     if (this.destinations === undefined) {
       return;
     }
 
     const matchingDestinations = this.searchQuery ?
-        this.destinations.filter(
-            d => d.matches(/** @type {!RegExp} */ (this.searchQuery))) :
+        this.destinations.filter(d => d.matches(this.searchQuery!)) :
         this.destinations.slice();
 
     // Update the height before updating the list.
@@ -170,11 +158,7 @@ export class PrintPreviewDestinationListElement extends
     this.forceIronResize_();
   }
 
-  /**
-   * @param {!KeyboardEvent} e Event containing the destination and key.
-   * @private
-   */
-  onKeydown_(e) {
+  private onKeydown_(e: KeyboardEvent) {
     if (e.key === 'Enter') {
       this.onDestinationSelected_(e);
       e.stopPropagation();
@@ -182,11 +166,10 @@ export class PrintPreviewDestinationListElement extends
   }
 
   /**
-   * @param {!Event} e Event containing the destination that was selected.
-   * @private
+   * @param e Event containing the destination that was selected.
    */
-  onDestinationSelected_(e) {
-    if (e.composedPath()[0].tagName === 'A') {
+  private onDestinationSelected_(e: Event) {
+    if ((e.composedPath()[0] as HTMLElement).tagName === 'A') {
       return;
     }
 
@@ -197,11 +180,8 @@ export class PrintPreviewDestinationListElement extends
 
   /**
    * Returns a 1-based index for aria-rowindex.
-   * @param {number} index
-   * @return {number}
-   * @private
    */
-  getAriaRowindex_(index) {
+  private getAriaRowindex_(index: number): number {
     return index + 1;
   }
 }
