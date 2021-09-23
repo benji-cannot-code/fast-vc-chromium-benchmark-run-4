2454b982e9d9c4b534e5e050bff0644669b7980b
dpapad <dpapad@chromium.org>
2021-09-23 08:54:42 +0000

    Settings TypeScript: Migrate privacy_sandbox/.
    With this change privacy_sandbox:build_grdp is only used for
    optimize_webui=true. For the "false" case there is no need to use
    a separate grdp file, as the relevant files are already included in the
    build, since they are listed in the tsconfig.manifest generated from
    the ts_library() target.
    
    Bug: 1234307
    Change-Id: Iae6c4f6f4be7bbaf7ec6c1e78c077ebed4f88955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3175141
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#924228}

--- chrome/test/data/webui/settings/privacy_sandbox_test.js
+++ chrome/test/data/webui/settings/privacy_sandbox_test.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PrivacySandboxAppElement} from 'chrome://settings/privacy_sandbox/app.js';
-import {PrivacySandboxBrowserProxy, PrivacySandboxBrowserProxyImpl} from 'chrome://settings/privacy_sandbox/privacy_sandbox_browser_proxy.js';
+import {PrivacySandboxBrowserProxyImpl} from 'chrome://settings/privacy_sandbox/privacy_sandbox_browser_proxy.js';
 import {CrSettingsPrefs, HatsBrowserProxyImpl, loadTimeData, MetricsBrowserProxyImpl, TrustSafetyInteraction} from 'chrome://settings/settings.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
@@ -15,6 +15,26 @@ import {flushTasks, isChildVisible} from '../test_util.js';
 
 import {TestHatsBrowserProxy} from './test_hats_browser_proxy.js';
 import {TestMetricsBrowserProxy} from './test_metrics_browser_proxy.js';
+
+class TestPrivacySandboxBrowserProxy extends TestBrowserProxy {
+  constructor() {
+    super(['getFlocId', 'resetFlocId']);
+  }
+
+  getFlocId() {
+    this.methodCalled('getFlocId');
+    return Promise.resolve({
+      trialStatus: 'test-trial-status',
+      cohort: 'test-id',
+      nextUpdate: 'test-time',
+      canReset: true,
+    });
+  }
+
+  resetFlocId() {
+    this.methodCalled('resetFlocId');
+  }
+}
 
 suite('PrivacySandbox', function() {
   /** @type {!PrivacySandboxAppElement} */
@@ -32,15 +52,6 @@ suite('PrivacySandbox', function() {
    */
   let testPrivacySandboxBrowserProxy;
 
-  function setDefaultFlocID() {
-    testPrivacySandboxBrowserProxy.setResultFor('getFlocId', Promise.resolve({
-      trialStatus: 'test-trial-status',
-      cohort: 'test-id',
-      nextUpdate: 'test-time',
-      canReset: true,
-    }));
-  }
-
   setup(function() {
     testHatsBrowserProxy = new TestHatsBrowserProxy();
     HatsBrowserProxyImpl.setInstance(testHatsBrowserProxy);
@@ -48,13 +59,10 @@ suite('PrivacySandbox', function() {
     metricsBrowserProxy = new TestMetricsBrowserProxy();
     MetricsBrowserProxyImpl.setInstance(metricsBrowserProxy);
 
-    testPrivacySandboxBrowserProxy =
-        TestBrowserProxy.fromClass(PrivacySandboxBrowserProxy);
-    PrivacySandboxBrowserProxyImpl.instance_ = testPrivacySandboxBrowserProxy;
+    testPrivacySandboxBrowserProxy = new TestPrivacySandboxBrowserProxy();
+    PrivacySandboxBrowserProxyImpl.setInstance(testPrivacySandboxBrowserProxy);
 
     CrSettingsPrefs.deferInitialization = true;
-
-    setDefaultFlocID();
 
     document.body.innerHTML = '';
     page = /** @type {!PrivacySandboxAppElement} */
@@ -152,7 +160,7 @@ suite('PrivacySandbox', function() {
 
     // When the FLoC generated preference is changed, the page should re-query
     // for the FLoC id.
-    setDefaultFlocID();
+    testPrivacySandboxBrowserProxy.resetResolver('getFlocId');
     page.set('prefs.generated.floc_enabled.value', false);
     await testPrivacySandboxBrowserProxy.whenCalled('getFlocId');
   });
