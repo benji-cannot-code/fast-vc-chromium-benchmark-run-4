64b209cfc5a4e827bb505c10669aa303bc87d61c
Fabio Tirelo <ftirelo@chromium.org>
2021-09-23 15:11:23 +0000

    Reland "[Clank CBCM] Add device model and brand to register request"
    This reverts commit e744365d96dfc6aaa143ff36a3b9c412abaa1d2b.
    
    Reason for revert: error fixed in patch #2
    
    Suggestion for reviewer:
     - Patch #1 is the original CL (crrev.com/c/3166072)
     - Patch #2 is the fix, easier to review if compared with Patch #1
    
    The original CL (crrev.com/c/3166072) failed on Win7 bots because with
    the error below:
    
    ----------------------------------------------------------------------
    Check failed: false. CoCreateInstance calls in Chromium require
    explicit COM initialization via base::CreateCOMSTATaskRunner() or
    ScopedCOMInitializer. See the comment in DCheckedCoCreateInstance
    for more details
    ----------------------------------------------------------------------
    
    Fixing that requires us to add a ScopedCOMInitializer in the affected
    tests (Windows only) similar to what is in
    `SysInfoTest.GetHardwareInfoWMIMatchRegistry` [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:base/system/sys_info_unittest.cc;l=250;drc=8f95b5eab2797a3e26bba299f3b0df85bfc98bf5
    
    Bug: 1210118
    Fixes: 1210118
    
    Original change's description:
    > Revert "[Clank CBCM] Add device model and brand to register request"
    >
    > This reverts commit 30b8b43e7d8b0c71ef613e30156c2fc8672bad79.
    >
    > Reason for revert: Fails unit_tests on Win7 Dbg:
    >
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Win7%20Tests%20(dbg)(1)/90845
    >
    > Original change's description:
    > > [Clank CBCM] Add device model and brand to register request
    > >
    > > Device model and manufacturer are provided by async calls to
    > > base::SysInfo::GetHardwareInfo. Using the function required us to
    > > make a few function calls in the report proto generation async as
    > > well.
    > >
    > > Note: GetDeviceManufacturer() and GetDeviceModel() are still used by
    > > CommonSignalsDecorator::Decorate(), but they will return empty string
    > > on Desktop. A follow-up CL will delete the method calls and the
    > > functions.
    > >
    > > Bug: 1210118
    > > Fixes: 1210118
    > >
    > > Change-Id: Ib81ada4d511af9d13fd2eb52bfcd790322bc1983
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3166072
    > > Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    > > Reviewed-by: Owen Min <zmin@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#922745}
    >
    > Bug: 1210118
    > Change-Id: I8f203bee8f26dac7c58e925cd1ba16c2376264dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168718
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    > Owners-Override: Alexey Baskakov <loyso@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#923203}
    
    Bug: 1210118
    Change-Id: I8254bd49f0afbca76b445e4ddae09695c0abbc6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172916
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Auto-Submit: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924311}

--- components/policy/core/common/cloud/client_data_delegate.h
+++ components/policy/core/common/cloud/client_data_delegate.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_POLICY_CORE_COMMON_CLOUD_CLIENT_DATA_DELEGATE_H_
 #define COMPONENTS_POLICY_CORE_COMMON_CLOUD_CLIENT_DATA_DELEGATE_H_
 
+#include "base/callback_forward.h"
+
 namespace enterprise_management {
 class RegisterBrowserRequest;
 }  // namespace enterprise_management
@@ -21,7 +23,8 @@ class ClientDataDelegate {
   virtual ~ClientDataDelegate() = default;
 
   virtual void FillRegisterBrowserRequest(
-      enterprise_management::RegisterBrowserRequest* request) const = 0;
+      enterprise_management::RegisterBrowserRequest* request,
+      base::OnceClosure callback) const = 0;
 };
 
 }  // namespace policy
