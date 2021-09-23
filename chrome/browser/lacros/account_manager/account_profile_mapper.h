ce34889efae227dff1a9963f3d32d18d120e4e6e
David Roger <droger@chromium.org>
2021-09-23 11:52:46 +0000

    [Lacros] Add account cache to AccountProfileMapper
    This architecture avoids creating account_manager::Account instances in
    chrome. This will be more robust to future changes in the Account
    structure, and avoids conversions.
    
    Bug: 1226045
    Change-Id: I8c551ec982036866fbff1d7d627724400cd886dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3175980
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924260}

--- chrome/browser/lacros/account_manager/account_profile_mapper.h
+++ chrome/browser/lacros/account_manager/account_profile_mapper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_forward.h"
+#include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -88,8 +89,7 @@ class AccountProfileMapper
  private:
   // Computes the stale accounts (accounts that are in Lacros but no longer in
   // the system) and removes them from the `ProfileAttributesStorage`.
-  std::vector<std::pair<base::FilePath, std::string>> RemoveStaleAccounts(
-      const base::flat_set<std::string>& system_gaia_ids);
+  std::vector<std::pair<base::FilePath, std::string>> RemoveStaleAccounts();
 
   // Computes the new accounts (accounts that are in the system but not in
   // Lacros) and adds them to `entry_for_new_accounts`. If the entry is nullptr,
@@ -120,6 +120,9 @@ class AccountProfileMapper
   base::ScopedObservation<account_manager::AccountManagerFacade,
                           account_manager::AccountManagerFacade::Observer>
       account_manager_facade_observation_{this};
+
+  // Map of account_manager::Account keyed by GaiaID.
+  base::flat_map<std::string, account_manager::Account> account_cache_;
 
   base::WeakPtrFactory<AccountProfileMapper> weak_factory_{this};
 };
