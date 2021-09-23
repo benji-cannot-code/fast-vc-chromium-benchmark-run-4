d6fbf776345fb64cbd5182e54ccb6224930c0fd0
Austin Sullivan <asully@chromium.org>
2021-09-23 01:19:51 +0000

    Clipboard: Migrate use of BitmapType in x11
    This is a no-op since both PngType and BitmapType map to kMimeTypePNG.
    BitmapType is planned for removal.
    
    Bug: 1223849
    Change-Id: I94ffd3693531fb2bc92185acdbee57f1e387054d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174090
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924134}

--- ui/base/x/x11_clipboard_helper.cc
+++ ui/base/x/x11_clipboard_helper.cc
@@ -221,7 +221,7 @@ std::vector<std::string> XClipboardHelper::GetAvailableTypes(
     available_types.push_back(kMimeTypeSvg);
   if (target_list.ContainsFormat(ClipboardFormatType::RtfType()))
     available_types.push_back(kMimeTypeRTF);
-  if (target_list.ContainsFormat(ClipboardFormatType::BitmapType()))
+  if (target_list.ContainsFormat(ClipboardFormatType::PngType()))
     available_types.push_back(kMimeTypePNG);
   if (target_list.ContainsFormat(ClipboardFormatType::FilenamesType()))
     available_types.push_back(kMimeTypeURIList);
