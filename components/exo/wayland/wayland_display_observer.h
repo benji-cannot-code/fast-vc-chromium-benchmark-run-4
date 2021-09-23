6c0b99e770da371ed610d15e0730ccd2e8ccbb46
yjliu <yjliu@chromium.org>
2021-09-23 15:39:56 +0000

    Reland "Fixed crash of parallels screen on multiple displays"
    This is a reland of 5fd69d15f15944f5ab9d0ae976c92e7ab7d693b0 without the
    newly-added unit test. The test will be in a separate CL.
    
    Original change's description:
    > Fixed crash of parallels screen on multiple displays
    >
    > Bug: b/183896845
    > Test: covered by unit test
    > Change-Id: I8967da6e37024efbe5cc69e27ee8652de1202b0e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3086404
    > Commit-Queue: Jun Liu <yjliu@chromium.org>
    > Auto-Submit: Jun Liu <yjliu@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#922371}
    
    Bug: b/183896845
    Change-Id: I0dc10c26d35cf2d5d50afb9ea6db290a20329c72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177720
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Jun Liu <yjliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924317}

--- components/exo/wayland/wayland_display_observer.h
+++ components/exo/wayland/wayland_display_observer.h
@@ -45,6 +45,7 @@ class WaylandDisplayHandler : public display::DisplayObserver,
   WaylandDisplayHandler& operator=(const WaylandDisplayHandler&) = delete;
 
   ~WaylandDisplayHandler() override;
+  void Initialize();
   void AddObserver(WaylandDisplayObserver* observer);
   int64_t id() const;
 
@@ -57,6 +58,9 @@ class WaylandDisplayHandler : public display::DisplayObserver,
   void OnXdgOutputCreated(wl_resource* xdg_output_resource);
   // Unset the xdg output object.
   void UnsetXdgOutputResource();
+
+ protected:
+  wl_resource* output_resource() const { return output_resource_; }
 
  private:
   // Overridden from WaylandDisplayObserver:
