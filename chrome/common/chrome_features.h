4c07eef27b8d27cb7d64c275ec1f2b224a3057e7
Eric Willigers <ericwilligers@chromium.org>
2021-09-23 04:15:55 +0000

    [Desktop PWAs] Retire attention badging flag
    We retire the enable-desktop-pwas-attention-badging-cros flag and
    associated switches.
    
    The flag expired in M93.
    
    The shipped behavior (API overrides notifications) can no longer be
    changed using flags. The Badging API controls if badges are shown.
    Notifications can also lead to a badge being shown, but only if the
    web app has not used the Badging API in the past 14 days.
    
    Bug: 1157273
    Change-Id: I3c20521607677eeda1689d12d30aa8497c609755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172992
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924177}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -159,14 +159,6 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsAppIconShortcutsMenuUI;
 #endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
-COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::Feature kDesktopPWAsAttentionBadgingCrOS;
-COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::FeatureParam<std::string>
-    kDesktopPWAsAttentionBadgingCrOSParam;
-#endif
-
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsAdditionalWindowingControls;
 
