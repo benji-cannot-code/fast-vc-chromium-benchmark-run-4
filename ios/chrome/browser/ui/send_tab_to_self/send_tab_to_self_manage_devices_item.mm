4b79afe78797711e42bd65b79989f3e2723810f7
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-09-24 20:38:21 +0000

    [butter stts] Adjust avatar size and alignment on iOS
    Use the same margins and icon size as the device rows, i.e.
    SendTabToSelfImageDetailTextItem [1][2]. Also make the link
    background transparent, otherwise it looks off in dark mode.
    
    Screenshots
    Before: crbug.com/1246488#c1
    After: crbug.com/1246488#c5
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_image_detail_text_item.mm;l=21;drc=3a2fb61fd4f4e2160b0797c8d132932fd4f57ebb
    [2] https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/settings/cells/settings_image_detail_text_cell.mm;l=106;drc=d9f4442d0d5bf1a9480675a114d3aa4f593f7400
    
    Bug: 1246488
    Change-Id: Ia200fddb6dba0fbd1f730e62522db2438b4d0f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3182960
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924891}

--- ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_manage_devices_item.mm
+++ ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_manage_devices_item.mm
@@ -20,6 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+namespace {
+
+const CGFloat kAvatarSize = 24;
+
+}  // namespace
+
 #pragma mark - SendTabtoSelfManageDevicesCell
 
 // Cell class for SendTabToSelfManageDevicesItem.
@@ -47,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _avatarBadge = [[UIImageView alloc] init];
   _avatarBadge.translatesAutoresizingMaskIntoConstraints = NO;
   _avatarBadge.contentMode = UIViewContentModeScaleAspectFit;
-  _avatarBadge.layer.cornerRadius = kTableViewIconImageSize / 2;
+  _avatarBadge.layer.cornerRadius = kAvatarSize / 2;
   _avatarBadge.clipsToBounds = YES;
   [self.contentView addSubview:_avatarBadge];
 
@@ -58,30 +64,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _linkAndEmailTextView.font =
       [UIFont preferredFontForTextStyle:kTableViewSublabelFontStyle];
   _linkAndEmailTextView.textColor = [UIColor colorNamed:kTextSecondaryColor];
+  _linkAndEmailTextView.backgroundColor = [UIColor clearColor];
+  // Remove built-in padding.
+  _linkAndEmailTextView.textContainer.lineFragmentPadding = 0;
+  [_linkAndEmailTextView setTextContainerInset:UIEdgeInsetsZero];
   _linkAndEmailTextView.delegate = self;
   [self.contentView addSubview:_linkAndEmailTextView];
 
   [NSLayoutConstraint activateConstraints:@[
     [_avatarBadge.centerYAnchor
         constraintEqualToAnchor:self.contentView.centerYAnchor],
-    [_avatarBadge.heightAnchor
-        constraintEqualToConstant:kTableViewIconImageSize],
-    [_avatarBadge.widthAnchor
-        constraintEqualToConstant:kTableViewIconImageSize],
+    [_avatarBadge.heightAnchor constraintEqualToConstant:kAvatarSize],
+    [_avatarBadge.widthAnchor constraintEqualToConstant:kAvatarSize],
     [_avatarBadge.leftAnchor
         constraintEqualToAnchor:self.contentView.leftAnchor
                        constant:kTableViewHorizontalSpacing],
+    [_avatarBadge.topAnchor
+        constraintGreaterThanOrEqualToAnchor:self.contentView.topAnchor
+                                    constant:
+                                        kTableViewTwoLabelsCellVerticalSpacing],
     [_linkAndEmailTextView.centerYAnchor
         constraintEqualToAnchor:_avatarBadge.centerYAnchor],
     [_linkAndEmailTextView.leftAnchor
         constraintEqualToAnchor:_avatarBadge.rightAnchor
-                       constant:kTableViewSubViewHorizontalSpacing],
+                       constant:kTableViewImagePadding],
     [_linkAndEmailTextView.rightAnchor
         constraintEqualToAnchor:self.contentView.rightAnchor
                        constant:-kTableViewHorizontalSpacing],
     [_linkAndEmailTextView.topAnchor
-        constraintEqualToAnchor:self.contentView.topAnchor
-                       constant:kTableViewVerticalSpacing]
+        constraintGreaterThanOrEqualToAnchor:self.contentView.topAnchor
+                                    constant:
+                                        kTableViewTwoLabelsCellVerticalSpacing]
   ]];
 
   return self;
