745d44f7901402ff8c291f5f53c9a204dee385bd
Palak Agarwal <agpalak@chromium.org>
2021-09-24 08:33:45 +0000

    External device should have the same group ID for input and output on Mac.
    When a headset is plugged in, it creates a new entry in the device_ids vector even though the headset's last entry might still exist there. Then, when you call GetRelatedDeviceIDs(), which returns this vector, you get the deviceID of not only of the current plugged-in device, but also of the last few times it was plugged in. This will end up in returning duplicate device IDs by GetRelatedDeviceIDs() function.
    
    Bug: 1186125
    Change-Id: I4b5db217c3b9e6cc15b6a5c763dee6d47571c304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179201
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924686}

--- media/audio/mac/audio_manager_mac.cc
+++ media/audio/mac/audio_manager_mac.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/containers/flat_set.h"
 #include "base/mac/mac_logging.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
@@ -671,10 +672,12 @@ std::string AudioManagerMac::GetAssociatedOutputDeviceID(
   std::vector<AudioObjectID> related_device_ids =
       core_audio_mac::GetRelatedDeviceIDs(input_device_id);
 
-  std::vector<AudioObjectID> related_output_device_ids;
+  // Defined as a set as device IDs might be duplicated in
+  // GetRelatedDeviceIDs().
+  base::flat_set<AudioObjectID> related_output_device_ids;
   for (AudioObjectID device_id : related_device_ids) {
     if (core_audio_mac::GetNumStreams(device_id, false /* is_input */) > 0)
-      related_output_device_ids.push_back(device_id);
+      related_output_device_ids.insert(device_id);
   }
 
   // Return the device ID if there is only one associated device.
@@ -683,7 +686,7 @@ std::string AudioManagerMac::GetAssociatedOutputDeviceID(
   // to an endpoint, so we cannot randomly pick a device.
   if (related_output_device_ids.size() == 1) {
     absl::optional<std::string> related_unique_id =
-        core_audio_mac::GetDeviceUniqueID(related_output_device_ids[0]);
+        core_audio_mac::GetDeviceUniqueID(*related_output_device_ids.begin());
     if (related_unique_id)
       return std::move(*related_unique_id);
   }
