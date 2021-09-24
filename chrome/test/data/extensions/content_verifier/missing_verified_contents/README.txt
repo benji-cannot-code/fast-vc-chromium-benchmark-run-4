303a73c97a6dfc4b6cc345b48992eb823e7f298f
Swapnil <swapnilgupta@google.com>
2021-09-24 08:23:46 +0000

    [Extensions Refactoring] Cleanup CWS test extension from browsertest
    Some browsertests related to content verification used a test extension
    from CWS to verify the test. These tests were added in
    https://crrev.com/c/2717827. We can use already existing extension
    archives in the test directory to avoid usage of actual test extensions
    from CWS. This CL removes the test extension and updates the
    browsertests to use existing extension directories.
    
    Bug: b/187925129
    Change-Id: I9000a3702a2060779cf2fb2369d28afbae35caaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3178911
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Cr-Commit-Position: refs/heads/main@{#924682}

--- chrome/test/data/extensions/content_verifier/missing_verified_contents/README.txt
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-The files in this directory came from
-extensions/test/data/content_hash_fetcher/missing_verified_contents. Refer to
-README.txt of above directory for more details.
--- chrome/test/data/extensions/content_verifier/missing_verified_contents/source/script.js
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
-// trivial change for version 0.8
-
-var hello = document.createElement("div");
-hello.innerText = "hello world";
-document.body.insertBefore(hello, document.body.firstChild);
