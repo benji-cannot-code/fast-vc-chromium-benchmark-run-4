056a6322aef47f3585c5fc2fc1d5e9c950e313d2
Charlie Hu <chenleihu@google.com>
2021-09-24 21:57:39 +0000

    [Android Unwinder] Rename chrome_unwind_table_android
    In anticipation of using the contained functions for the
    implementation of ChromeUnwinderAndroidV2, this CL renames
    chrome_unwind_table_android to chrome_unwinder_android_v2.
    
    Bug: 1240698
    Change-Id: I96a148477190d03a1d482f2ccb51498cd292c5e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3170059
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924934}

--- base/profiler/chrome_unwinder_android_v2.h
+++ base/profiler/chrome_unwinder_android_v2.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_PROFILER_CHROME_UNWIND_TABLE_ANDROID_H_
-#define BASE_PROFILER_CHROME_UNWIND_TABLE_ANDROID_H_
+#ifndef BASE_PROFILER_CHROME_UNWINDER_ANDROID_V2_H_
+#define BASE_PROFILER_CHROME_UNWINDER_ANDROID_V2_H_
 
 #include <stdint.h>
 
@@ -84,4 +84,4 @@ GetFunctionTableIndexFromInstructionOffset(
 
 }  // namespace base
 
-#endif  // BASE_PROFILER_CHROME_UNWIND_TABLE_ANDROID_H_
+#endif  // BASE_PROFILER_CHROME_UNWINDER_ANDROID_V2_H_
