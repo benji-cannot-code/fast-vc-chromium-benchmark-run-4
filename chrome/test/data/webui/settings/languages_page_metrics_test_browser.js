5c48447576965afa906034325870cdccfc60c41e
dpapad <dpapad@chromium.org>
2021-09-24 08:37:33 +0000

    Settings TypeScript: Migrate languages_page/ folder, part 2.
    Migrate remaining files that were previously shared with CrOS Settings
    (no longer the case).
    
    Bug: 1234307
    Change-Id: Ic35c5d8482aa084639264f3389a2ba7d2512f0b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176181
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924687}

--- chrome/test/data/webui/settings/languages_page_metrics_test_browser.js
+++ chrome/test/data/webui/settings/languages_page_metrics_test_browser.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {LanguageHelper, LanguagesBrowserProxyImpl, LanguageSettingsActionType, LanguageSettingsMetricsProxyImpl, LanguageSettingsPageImpressionType, SettingsLanguagesSubpageElement} from 'chrome://settings/lazy_load.js';
+import {LanguagesBrowserProxyImpl, LanguageSettingsActionType, LanguageSettingsMetricsProxyImpl, LanguageSettingsPageImpressionType, SettingsLanguagesSubpageElement} from 'chrome://settings/lazy_load.js';
 import {CrSettingsPrefs} from 'chrome://settings/settings.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
