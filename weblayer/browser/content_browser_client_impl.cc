13e76d424f022f002f60af81bf7c8c4e462048dd
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2021-09-24 09:16:17 +0000

    Wire up Web Bluetooth API in WebLayer.
    Bug: 1076462
    Change-Id: I081f1bcb12a72a69075d2f125970fca418c9cf90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3048437
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924698}

--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -129,6 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/embedder_support/android/metrics/android_metrics_service_client.h"
 #include "components/media_router/browser/presentation/presentation_service_delegate_impl.h"  // nogncheck
 #include "components/navigation_interception/intercept_navigation_delegate.h"
+#include "components/permissions/bluetooth_delegate_impl.h"
 #include "components/safe_browsing/core/browser/realtime/policy_engine.h"  // nogncheck
 #include "components/safe_browsing/core/browser/realtime/url_lookup_service.h"  // nogncheck
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
@@ -140,6 +141,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/browser/android/metrics/weblayer_metrics_navigation_throttle.h"
 #include "weblayer/browser/android/metrics/weblayer_metrics_service_client.h"
 #include "weblayer/browser/android_descriptors.h"
+#include "weblayer/browser/bluetooth/weblayer_bluetooth_delegate_impl_client.h"
 #include "weblayer/browser/browser_context_impl.h"
 #include "weblayer/browser/devtools_manager_delegate_android.h"
 #include "weblayer/browser/http_auth_handler_impl.h"
@@ -1174,6 +1176,14 @@ bool ContentBrowserClientImpl::
   // embedded in a scrollable container and we need to update the position of
   // any DialogOverlays.
   return true;
+}
+
+content::BluetoothDelegate* ContentBrowserClientImpl::GetBluetoothDelegate() {
+  if (!bluetooth_delegate_) {
+    bluetooth_delegate_ = std::make_unique<permissions::BluetoothDelegateImpl>(
+        std::make_unique<WebLayerBluetoothDelegateImplClient>());
+  }
+  return bluetooth_delegate_.get();
 }
 
 #endif  // OS_ANDROID
--- weblayer/browser/content_browser_client_impl.h
+++ weblayer/browser/content_browser_client_impl.h
@@ -29,6 +29,10 @@ namespace net {
 class SiteForCookies;
 }  // namespace net
 
+namespace permissions {
+class BluetoothDelegateImpl;
+}
+
 namespace weblayer {
 
 class FeatureListCreator;
@@ -215,6 +219,7 @@ class ContentBrowserClientImpl : public content::ContentBrowserClient {
   std::unique_ptr<content::TtsEnvironmentAndroid> CreateTtsEnvironmentAndroid()
       override;
   bool ShouldObserveContainerViewLocationForDialogOverlays() override;
+  content::BluetoothDelegate* GetBluetoothDelegate() override;
 #endif  // OS_ANDROID
   content::SpeechRecognitionManagerDelegate*
   CreateSpeechRecognitionManagerDelegate() override;
@@ -233,6 +238,8 @@ class ContentBrowserClientImpl : public content::ContentBrowserClient {
 
 #if defined(OS_ANDROID)
   SafeBrowsingService* GetSafeBrowsingService();
+
+  std::unique_ptr<permissions::BluetoothDelegateImpl> bluetooth_delegate_;
 #endif
 
   MainParams* params_;
