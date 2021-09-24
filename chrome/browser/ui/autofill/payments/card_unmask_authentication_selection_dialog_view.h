f2531362d2f85f652b0cbe5945f0cf32bede5d86
Vinny Persky <vinnypersky@google.com>
2021-09-24 01:07:24 +0000

    Card Unmask Authentication Selection Dialog
    This CL is to create the dialog for Card Unmask Authentication Selection.
    The current dialog that gets rendered by this CL can be found here: https://screenshot.googleplex.com/8dxSHGt8F2tJdLM
    Original Dialog Sample Here: https://screenshot.googleplex.com/4Yo389pmwiE56Hx
    
    There is no triggering logic implemented for this dialog, so it will have no effect on current autofill functionality.
    
    Complete list of changes in this CL:
    - All controller & view files needed for the dialog.
    - A new vector icon for the challenge option text icon.
    - Browser tests to ensure the dialog can be rendered, and correctly handles tab closing and browser closing. Manual testing was also done to ensure correct functionality.
    - .sha files for the correct image of the dialog (and strings), and strings added to .grdp
    - Updated BUILD files for the vector icon, dialog, and browser tests.
    
    Link to implementation tracking: https://bugs.chromium.org/p/chromium/issues/detail?id=1243475#c1
    
    Bug: 1247347,1247351,1243475
    Change-Id: I2e9f7a327e2d96e9be604df675d0909aa6a2b350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3145273
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924581}

---
+++ chrome/browser/ui/autofill/payments/card_unmask_authentication_selection_dialog_view.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_CARD_UNMASK_AUTHENTICATION_SELECTION_DIALOG_VIEW_H_
+#define CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_CARD_UNMASK_AUTHENTICATION_SELECTION_DIALOG_VIEW_H_
+
+namespace autofill {
+
+class CardUnmaskAuthenticationSelectionDialogController;
+
+class CardUnmaskAuthenticationSelectionDialogView {
+ public:
+  // Creates a dialog and displays it as a modal on top of the web contents of
+  // CardUnmaskAuthenticationSelectionDialogController.
+  static CardUnmaskAuthenticationSelectionDialogView* CreateAndShow(
+      CardUnmaskAuthenticationSelectionDialogController* controller);
+
+  // Method to safely close this dialog when the controller is destroyed by
+  // unlinking the controller and closing the widget that owns this dialog view.
+  virtual void OnControllerDestroying() = 0;
+};
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_CARD_UNMASK_AUTHENTICATION_SELECTION_DIALOG_VIEW_H_
