134a5639ebcdcd42a7426b270548d3faf3ed827d
Chad Duffin <chadduffin@chromium.org>
2021-09-24 23:52:25 +0000

    [CrOS Bluetooth] Implement BluetoothDeviceListControllerImpl.
    This change implements the BluetoothDeviceListControllerImpl class,
    which manages the Bluetooth device list within the detailed page of
    Quick Settings. This class manages creating, removing, and rearranging
    the items and headers, and ensuring the correct separators exist.
    Updating the individual device list items to match the updated Bluetooth
    UI spec, and will be covered in a follow-up change.
    
    Screenshots of no paired devices, connected devices, previously
    connected devices, and both connected/previously connected devices:
     - https://screenshot.googleplex.com/SvDTgsekAHMRgjA.png
     - https://screenshot.googleplex.com/8UbtL6jjKmM6M6U.png
     - https://screenshot.googleplex.com/Bun5N4FvSS5n4Zs.png
     - https://screenshot.googleplex.com/vvsT2EtAGf8mUm5.png
    
    Bug: 1010321
    Test: Locally, on-device, unit tests.
    Change-Id: I68d8af0c1045b9c63d01649f7117a8437175a085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171395
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924974}

--- ash/system/bluetooth/bluetooth_device_list_item_view.cc
+++ ash/system/bluetooth/bluetooth_device_list_item_view.cc
@@ -27,4 +27,8 @@ void BluetoothDeviceListItemView::UpdateDeviceProperties(
   device_properties_ = mojo::Clone(device_properties);
 }
 
+const char* BluetoothDeviceListItemView::GetClassName() const {
+  return "BluetoothDeviceListItemView";
+}
+
 }  // namespace ash
