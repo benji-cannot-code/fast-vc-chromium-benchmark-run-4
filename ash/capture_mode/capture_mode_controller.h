0940cd95dd9c5ff6a56e2c64999e7c5ed77490f1
Ahmed Fakhry <afakhry@chromium.org>
2021-09-24 02:36:19 +0000

    capture_mode_settings: Implement backend of the save-to dialog
    This CL adds support for setting a custom save location for the
    captured images and videos.
    This will later be hooked to the new settings menu UI which is hidden
    behind a flag.
    This CL does not change the current behavior.
    
    Later follow-up will handle edge cases if an arbitrary selected
    folder becomes unavailable.
    
    BUG=1250885
    TEST=Manually, added new tests.
    
    Change-Id: I17e09b20e4344d998565eadbefd48dca02ff16d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177404
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924609}

--- ash/capture_mode/capture_mode_controller.h
+++ ash/capture_mode/capture_mode_controller.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/services/recording/public/mojom/recording_service.mojom.h"
 #include "base/callback_forward.h"
+#include "base/files/file_path.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -27,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/image/image.h"
+
+class PrefRegistrySimple;
 
 namespace base {
 class FilePath;
@@ -45,6 +48,16 @@ class ASH_EXPORT CaptureModeController
       public SessionObserver,
       public chromeos::PowerManagerClient::Observer {
  public:
+  // Contains info about the folder used for saving the captured images and
+  // videos.
+  struct CaptureFolder {
+    // The absolute path of the folder used for saving the captures.
+    base::FilePath path;
+
+    // True if the above |path| is the default "Downloads" folder on the device.
+    bool is_default_downloads_folder = false;
+  };
+
   explicit CaptureModeController(std::unique_ptr<CaptureModeDelegate> delegate);
   CaptureModeController(const CaptureModeController&) = delete;
   CaptureModeController& operator=(const CaptureModeController&) = delete;
@@ -53,6 +66,8 @@ class ASH_EXPORT CaptureModeController
   // Convenience function to get the controller instance, which is created and
   // owned by Shell.
   static CaptureModeController* Get();
+
+  static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
   CaptureModeType type() const { return type_; }
   CaptureModeSource source() const { return source_; }
@@ -88,6 +103,17 @@ class ASH_EXPORT CaptureModeController
   // Sets the user capture region. If it's non-empty and changed by the user,
   // update |last_capture_region_update_time_|.
   void SetUserCaptureRegion(const gfx::Rect& region, bool by_user);
+
+  // Sets the given |path| as the custom save location of captured images and
+  // videos for the currently logged in user (i.e. can only be called when user
+  // is logged in). Setting an empty |path| clears any custom selected folder
+  // resulting in using the default downloads folder.
+  void SetCustomCaptureFolder(const base::FilePath& path);
+
+  // Returns the folder in which all taken screenshots and videos will be saved.
+  // It can be the temp directory if the user is not logged in, the default
+  // "Downloads" folder, or a user-selected custom location.
+  CaptureFolder GetCurrentCaptureFolder() const;
 
   // Full screen capture for each available display if no restricted
   // content exists on that display, each capture is saved as an individual
