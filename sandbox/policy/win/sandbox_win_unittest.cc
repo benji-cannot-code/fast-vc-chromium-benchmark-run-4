79d204984037d59bf984cbd01bc01ba27aea9ec6
Will Harris <wfh@chromium.org>
2021-09-24 21:13:28 +0000

    Move ability to create jobless sandbox target into policy object.
    Prior to this CL the ability to create sandbox targets with no
    sandbox job was smuggled within the command line of the child
    process as it was passed around within content and sandbox.
    
    This CL moves this rule into a concrete attribute of the sandbox
    policy for the target.
    
    This CL contains no functional changes.
    
    BUG=None
    
    Change-Id: I3dd74ce11f60fc6dbab99ac39b1a3ba575dfc4b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3182784
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924908}

--- sandbox/policy/win/sandbox_win_unittest.cc
+++ sandbox/policy/win/sandbox_win_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/ref_counted.h"
+#include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/scoped_native_library.h"
 #include "base/strings/strcat.h"
@@ -141,6 +142,8 @@ class TestTargetPolicy : public TargetPolicy {
   }
 
   std::unique_ptr<PolicyInfo> GetPolicyInfo() override { return nullptr; }
+  void SetAllowNoSandboxJob() override { NOTREACHED(); }
+  bool GetAllowNoSandboxJob() override { return false; }
 
  private:
   std::vector<std::wstring> blocklisted_dlls_;
