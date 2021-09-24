65f16ea717127282aa552f791cc7592ea2c5001b
Juha Vainio <juha.j.vainio@intel.com>
2021-09-24 10:30:23 +0000

    [SensorProviderProxyImpl] Fixing the crash of callback destroyed before its execution
    Changing the order of fields in class definition to prevent situation
    where callback was destroyed before its execution.
    
    Also added unittest to test this situation.
    
    Bug: 1240814
    Change-Id: I33fdd1c8a2fb93aa6ebf23c2eba4aa78bd5ab49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173359
    Reviewed-by: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#924712}

--- services/device/public/cpp/test/fake_sensor_and_provider.h
+++ services/device/public/cpp/test/fake_sensor_and_provider.h
@@ -69,6 +69,7 @@ class FakeSensorProvider : public mojom::SensorProvider {
   void GetSensor(mojom::SensorType type, GetSensorCallback callback) override;
 
   void Bind(mojo::PendingReceiver<mojom::SensorProvider> receiver);
+  bool is_bound() const;
 
   void set_ambient_light_sensor_is_available(
       bool ambient_light_sensor_is_available) {
