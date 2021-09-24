0f037d51686b7c0a9d64390292f7af50c4ec51d8
Avi Drissman <avi@chromium.org>
2021-09-24 15:26:48 +0000

    Remove expired and unused flags
    - android-multiple-display
    - android-partner-customization-phenotype
    - autofill-save-card-infobar-edit-support
    - autofill-show-all-profiles-on-prefilled-forms
    - edit-bookmarks-ios
    - enable-ambient-mode
    - enable-android-night-mode-tab-reparenting
    - enable-assistant-media-session-integration
    - enable-assistant-timers-v2
    - enable-autofill-password-reauth-ios
    - enable-defer-all-script
    - enable-defer-all-script-without-optimization-hints
    - enable-mark-http-as
    - enable-md-rounded-corners-on-dialogs
    - enable-previews-coin-flip
    - enhanced_clipboard_simple_render
    - extension-checkup
    - ios-lookalike-url-navigation-suggestions-ui
    - managed-bookmarks-ios
    - new-tabstrip-animation
    - omnibox-local-zero-suggest-frecency-ranking
    - omnibox-on-focus-suggestions
    - omnibox-search-engine-logo
    - omnibox-search-ready-incognito
    - omnibox-ui-hide-steady-state-url-path-query-and-ref-on-interaction
    - omnibox-ui-reveal-steady-state-url-path-query-and-ref-on-hover
    - omnibox-ui-sometimes-elide-to-registrable-domain
    - page-info-performance-hints
    - report-feed-user-actions
    - send-uma-cellular
    - snapshot-draw-view
    - texture-layer-skip-wait-for-activation
    - use-lookalikes-for-navigation-suggestions
    
    Fixed: 1110677
    Fixed: 1110693
    Fixed: 1110724
    Fixed: 1129142
    Fixed: 1143325
    Fixed: 1143335
    Fixed: 1143358
    Fixed: 1159078
    Fixed: 1176835
    Fixed: 1176836
    Fixed: 1176843
    Fixed: 1176845
    Fixed: 1188797
    Fixed: 1188799
    Fixed: 1188801
    Fixed: 1188802
    Fixed: 1188803
    Fixed: 1207459
    Fixed: 1207476
    Fixed: 1207479
    Fixed: 1207484
    Fixed: 1207485
    Fixed: 1207493
    Fixed: 1207503
    Fixed: 1207519
    Fixed: 1207528
    Fixed: 1207534
    Fixed: 1207536
    Fixed: 1207537
    Fixed: 1207539
    Fixed: 1207547
    Fixed: 1207554
    Fixed: 1207557
    Change-Id: Ib1d3af1f141182bf723d71983adbe1ffb81544fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180660
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924772}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -137,9 +137,6 @@ extern const char kU2FPermissionPromptDescription[];
 extern const char kU2FSecurityKeyAPIName[];
 extern const char kU2FSecurityKeyAPIDescription[];
 
-extern const char kUseLookalikesForNavigationSuggestionsName[];
-extern const char kUseLookalikesForNavigationSuggestionsDescription[];
-
 extern const char kUseStorkSmdsServerAddressName[];
 extern const char kUseStorkSmdsServerAddressDescription[];
 
@@ -970,9 +967,6 @@ extern const char kDestroyProfileOnBrowserCloseDescription[];
 
 extern const char kNewUsbBackendName[];
 extern const char kNewUsbBackendDescription[];
-
-extern const char kNewTabstripAnimationName[];
-extern const char kNewTabstripAnimationDescription[];
 
 extern const char kNotificationIndicatorName[];
 extern const char kNotificationIndicatorDescription[];
