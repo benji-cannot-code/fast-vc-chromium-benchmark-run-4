aaf3da0bf0497f2a827c45d312677f1267caf97f
Chris Whelan <chwhelan@google.com>
2021-09-24 15:21:12 +0000

    Limit GCM keystore leveldb log size to 16KB
    Bug: b/199952827
    Test: Build
    Change-Id: Id7e0b933817a8412267a62641bb9e1b25fb0152f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179529
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Christopher Whelan <chwhelan@google.com>
    Cr-Commit-Position: refs/heads/main@{#924770}

--- components/gcm_driver/crypto/gcm_key_store.cc
+++ components/gcm_driver/crypto/gcm_key_store.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/leveldb_proto/public/proto_database_provider.h"
 #include "components/leveldb_proto/public/shared_proto_database_client_list.h"
 #include "crypto/random.h"
+#include "third_party/leveldatabase/env_chromium.h"
 
 namespace gcm {
 
@@ -32,6 +33,9 @@ using EntryVectorType =
 // authentication secret. Must be at least 16 bytes.
 const size_t kAuthSecretBytes = 16;
 
+// Size cap for the leveldb log file before compression.
+const size_t kDatabaseWriteBufferSizeBytes = 16 * 1024;
+
 std::string DatabaseKey(const std::string& app_id,
                         const std::string& authorized_entity) {
   DCHECK_EQ(std::string::npos, app_id.find(','));
@@ -40,6 +44,14 @@ std::string DatabaseKey(const std::string& app_id,
   return authorized_entity.empty()
              ? app_id  // No comma, for compatibility with existing keys.
              : app_id + ',' + authorized_entity;
+}
+
+leveldb_env::Options CreateLevelDbOptions() {
+  leveldb_env::Options options;
+  options.create_if_missing = true;
+  options.max_open_files = 0;  // Use minimum.
+  options.write_buffer_size = kDatabaseWriteBufferSizeBytes;
+  return options;
 }
 
 }  // namespace
@@ -293,7 +305,7 @@ void GCMKeyStore::LazyInitialize(base::OnceClosure done_closure) {
       blocking_task_runner_);
 
   database_->Init(
-      leveldb_proto::CreateSimpleOptions(),
+      CreateLevelDbOptions(),
       base::BindOnce(&GCMKeyStore::DidInitialize, weak_factory_.GetWeakPtr()));
 }
 
