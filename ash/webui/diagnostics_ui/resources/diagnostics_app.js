6fd05667bc6de7172a11149f3196e040d8ebbcc0
Ashley Prasad <ashleydp@google.com>
2021-09-24 00:22:32 +0000

    Diagnostics: Use banner slot when navigation enabled
    - Adds diagnostics-stick-banner to banner slot when networking enabled.
    - Ensures existing system banner does not activate if networking
      enabled to avoid duplicate banners.
    - Updates tests to check on window networking state.
    - Adds tests for banner visibility event handling for diagnostics-app.
    
    Screenshot (with navigation enabled):
    https://screenshot.googleplex.com/9x6b5xbwzgouFmQ.png
    
    Screenshot (with navigation disabled):
    https://screenshot.googleplex.com/LdHbtyjeRsXcE55
    
    Bug: 1197335
    Test: ./out/Default/browser_tests --gtest_filter=DiagnosticsApp*
    Change-Id: I4d05ca0523ae04b0ea9ca488a4499ba0f81dbe2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177243
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924561}

--- ash/webui/diagnostics_ui/resources/diagnostics_app.js
+++ ash/webui/diagnostics_ui/resources/diagnostics_app.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/ash/common/navigation_view_panel.js';
 import 'chrome://resources/ash/common/page_toolbar.js';
 import 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
+import './diagnostics_sticky_banner.js';
 import './input_list.js';
 import './network_list.js';
 import './strings.m.js';
@@ -35,6 +36,17 @@ Polymer({
   browserProxy_: null,
 
   properties: {
+    /**
+     * Used in navigation-view-panel to set show-banner when banner is expected
+     * to be shown.
+     * @protected
+     * @type {string}
+     */
+    bannerMessage_: {
+      type: Boolean,
+      value: '',
+    },
+
     /** @private {boolean} */
     showNavPanel_: {
       type: Boolean,
