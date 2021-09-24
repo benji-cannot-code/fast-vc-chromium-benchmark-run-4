a45e2ecf3d41d879ea0593d7cf6f73843eec76b1
Maria Kazinova <kazinova@google.com>
2021-09-24 07:22:45 +0000

    Migrate BrowsingDataCounterWrapper to PasswordStoreInterface.
    PasswordStoreInterface is an interface that should be used by other
    classes instead of interacting directly with PasswordStore
    
    Bug: 1218413
    Change-Id: If358fa55efe0eb784f9dfa99ee2bc869c1535ec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179210
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#924668}

--- ios/chrome/browser/browser_state/chrome_browser_state_manager_impl.cc
+++ ios/chrome/browser/browser_state/chrome_browser_state_manager_impl.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/scoped_blocking_call.h"
-#include "components/password_manager/core/browser/password_store.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/ios/browser/active_state_manager.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
--- ios/chrome/browser/browsing_data/browsing_data_counter_wrapper.cc
+++ ios/chrome/browser/browsing_data/browsing_data_counter_wrapper.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browsing_data/core/counters/passwords_counter.h"
 #include "components/browsing_data/core/pref_names.h"
 #include "components/keyed_service/core/service_access_type.h"
-#include "components/password_manager/core/browser/password_store.h"
+#include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/prefs/pref_service.h"
 #include "components/sync/driver/sync_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
@@ -48,7 +48,7 @@ CreateCounterForBrowserStateAndPref(ChromeBrowserState* browser_state,
 
   if (pref_name == browsing_data::prefs::kDeletePasswords) {
     return std::make_unique<browsing_data::PasswordsCounter>(
-        IOSChromePasswordStoreFactory::GetForBrowserState(
+        IOSChromePasswordStoreFactory::GetInterfaceForBrowserState(
             browser_state, ServiceAccessType::EXPLICIT_ACCESS),
         /*account_store=*/nullptr,
         SyncServiceFactory::GetForBrowserState(browser_state));
