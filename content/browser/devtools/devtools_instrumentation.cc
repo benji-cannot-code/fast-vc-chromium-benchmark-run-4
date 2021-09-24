14ae3bdd19742b7b74233178c666ef2478872eb9
Kateryna Prokopenko <kprokopenko@chromium.org>
2021-09-24 09:17:23 +0000

    Update protocol on the back-end side to include GenericIssue and add issue reporting for cross-origin portal message errorType
    Doc: https://docs.google.com/document/d/13spbxTDTiSpAjFw7K3Qem8BCxPDbdwEM3NrTOXF9LU0
    Bug: 1246855
    Change-Id: I6568bc67af5e37fc4fdcf45b59e14db098712cff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3144151
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924700}

--- content/browser/devtools/devtools_instrumentation.cc
+++ content/browser/devtools/devtools_instrumentation.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/browser/web_package/signed_exchange_envelope.h"
 #include "content/public/browser/browser_context.h"
+#include "devtools_instrumentation.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "net/base/load_flags.h"
@@ -1216,6 +1217,61 @@ void ApplyNetworkContextParamsOverrides(
                                                          context_params);
     }
   }
+}
+
+protocol::Audits::GenericIssueErrorType GenericIssueErrorTypeToProtocol(
+    blink::mojom::GenericIssueErrorType error_type) {
+  switch (error_type) {
+    case (blink::mojom::GenericIssueErrorType::
+              kCrossOriginPortalPostMessageError):
+      return protocol::Audits::GenericIssueErrorTypeEnum::
+          CrossOriginPortalPostMessageError;
+  }
+}
+
+namespace {
+struct GenericIssueInfo {
+  GenericIssueInfo() = default;
+  ~GenericIssueInfo() = default;
+  GenericIssueInfo(const GenericIssueInfo& info) = default;
+
+  blink::mojom::GenericIssueErrorType error_type;
+  absl::optional<std::string> frame_id;
+};
+
+void BuildAndReportGenericIssue(RenderFrameHostImpl* render_frame_host_impl,
+                                const GenericIssueInfo& issue_info) {
+  auto generic_issue_details =
+      protocol::Audits::GenericIssueDetails::Create()
+          .SetErrorType(GenericIssueErrorTypeToProtocol(issue_info.error_type))
+          .Build();
+
+  if (issue_info.frame_id) {
+    generic_issue_details->SetFrameId(*issue_info.frame_id);
+  }
+
+  auto issue =
+      protocol::Audits::InspectorIssue::Create()
+          .SetCode(protocol::Audits::InspectorIssueCodeEnum::GenericIssue)
+          .SetDetails(
+              protocol::Audits::InspectorIssueDetails::Create()
+                  .SetGenericIssueDetails(std::move(generic_issue_details))
+                  .Build())
+          .Build();
+
+  ReportBrowserInitiatedIssue(render_frame_host_impl, issue.get());
+}
+}  // namespace
+
+void DidRejectCrossOriginPortalMessage(
+    RenderFrameHostImpl* render_frame_host_impl) {
+  GenericIssueInfo issue_info;
+  issue_info.error_type =
+      blink::mojom::GenericIssueErrorType::kCrossOriginPortalPostMessageError;
+  issue_info.frame_id =
+      render_frame_host_impl->GetDevToolsFrameToken().ToString();
+
+  BuildAndReportGenericIssue(render_frame_host_impl, issue_info);
 }
 
 }  // namespace devtools_instrumentation
