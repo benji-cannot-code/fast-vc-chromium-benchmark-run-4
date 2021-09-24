ef6951bdded0bf2b4ee5b0ebb13067f5406c06ca
Ari Chivukula <arichiv@chromium.org>
2021-09-24 06:52:01 +0000

    [Legacy client hints modernization] (1) Add `sec-ch-` hint constants
    This CL adds the boilerplate constants needed to implement our four new
    variants (`sec-ch-` prefixes for device-memory, dpr, width, and
    viewport-width). There are a lot of side effects before even adding
    implementation, so that's saved for a later CL. All of this is gated
    behind disabled by default features so there isn't an external facing
    change.
    
    NOTE: This does not add new client hints, just new names for existing
    ones as discussed here:
    https://docs.google.com/document/d/1yhVLyEIpDhhDQf698WkvXBiPcLwxEgCBI4o1FjvXwfM/edit
    
    This series of CLs seeks to add `sec-ch-` prefixes to client hints that
    are missing them by deprecating the old versions and adding new ones:
    (1) Add `sec-ch-` hint constants
    (2) Implement `sec-ch-` device-memory, dpr, width, and viewport-width
    
    Bug: 1227043
    Change-Id: I672850e4aeda1bbbe33ef1969f838d53f87b963a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173916
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924660}

--- third_party/blink/renderer/core/loader/frame_client_hints_preferences_context.cc
+++ third_party/blink/renderer/core/loader/frame_client_hints_preferences_context.cc
@@ -60,6 +60,13 @@ ClientHintToWebFeatureMap MakeClientHintToWebFeatureMap() {
        WebFeature::kClientHintsUAReduced},
       {network::mojom::WebClientHintsType::kViewportHeight,
        WebFeature::kClientHintsViewportHeight},
+      {network::mojom::WebClientHintsType::kDeviceMemory,
+       WebFeature::kClientHintsDeviceMemory},
+      {network::mojom::WebClientHintsType::kDpr, WebFeature::kClientHintsDPR},
+      {network::mojom::WebClientHintsType::kResourceWidth,
+       WebFeature::kClientHintsResourceWidth},
+      {network::mojom::WebClientHintsType::kViewportWidth,
+       WebFeature::kClientHintsViewportWidth},
   };
 }
 
