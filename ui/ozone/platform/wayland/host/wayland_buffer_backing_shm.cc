c9ba7a0deb4389f17cdd59af7e6352279bfc225d
Kramer Ge <fangzhoug@chromium.org>
2021-09-24 20:42:30 +0000

    [Ozone/Wayland]Refactor WaylandBuffer out of WaylandBufferManagerHost
    A WaylandBuffer represents a buffer that can be attached to
    WaylandSurfaces, created with Create(Dmabuf/Shm)BasedBuffer IPCs from
    the gpu process.
    
    It dynamically requests wl_buffer objects through Wayland to represent
    the same backing buffer. This avoids the undefined behaviour of
    wl_buffer releases in the case of re-using wl_buffers on multiple
    wl_surfaces.Requested wl_buffer objects are wrapped in a
    WaylandBufferHandle managed by the WaylandBuffer.
    
    This is 1/? of the WaylandBufferManagerHost refactoring CLs
    Proposed design: https://docs.google.com/document/d/1KCl_gsa9qfPP60i66L_Q8Ekpgb9LNFtdK7p5mcBi9Ys/edit?usp=sharing
    
    Change-Id: I271969cdf2e252834a883c0f569397b9f19b4970
    Bug: 1203920, 1203921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3138536
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#924894}

---
+++ ui/ozone/platform/wayland/host/wayland_buffer_backing_shm.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/ozone/platform/wayland/host/wayland_buffer_backing_shm.h"
+
+#include "ui/ozone/platform/wayland/host/wayland_connection.h"
+#include "ui/ozone/platform/wayland/host/wayland_shm.h"
+
+namespace ui {
+
+WaylandBufferBackingShm::WaylandBufferBackingShm(
+    const WaylandConnection* connection,
+    base::ScopedFD fd,
+    uint64_t length,
+    const gfx::Size& size,
+    uint32_t buffer_id)
+    : WaylandBufferBacking(connection, buffer_id, size),
+      fd_(std::move(fd)),
+      length_(length) {}
+
+WaylandBufferBackingShm::~WaylandBufferBackingShm() = default;
+
+void WaylandBufferBackingShm::RequestBufferHandle(
+    base::OnceCallback<void(wl::Object<wl_buffer>)> callback) {
+  DCHECK(!callback.is_null());
+  std::move(callback).Run(
+      connection_->shm()->CreateBuffer(fd_, length_, size()));
+}
+
+}  // namespace ui
--- ui/ozone/platform/wayland/host/wayland_drm.cc
+++ ui/ozone/platform/wayland/host/wayland_drm.cc
@@ -64,7 +64,7 @@ bool WaylandDrm::SupportsDrmPrime() const {
   return authenticated_ && !!wl_drm_;
 }
 
-void WaylandDrm::CreateBuffer(base::ScopedFD fd,
+void WaylandDrm::CreateBuffer(const base::ScopedFD& fd,
                               const gfx::Size& size,
                               const std::vector<uint32_t>& strides,
                               const std::vector<uint32_t>& offsets,
