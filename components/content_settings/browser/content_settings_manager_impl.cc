9b3f80bcfba38fad748644ee1dd928db0cef0336
Matt Falkenhagen <falken@chromium.org>
2021-09-24 10:30:16 +0000

    content: Remove remaining uses of content::RunOrPostTaskOnThread.
    This helper was created in
    commit 7b509e56dc6d9ae0428690080211e645c8c94f5e to expedite the UI/IO
    thread simplification effort, and particularly the service worker thread
    migration which is now complete. It was not intended as a long-term
    function for general use, as it is a strange layer over the normal
    threading and tasks functions in base/ and encourages a typically
    unrecommended pattern of "maybe async, maybe sync". There are just a
    few targeted uses of it today, and these can directly use the usual
    functions without this helper.
    
    Bug: 1138155, 1187753
    Change-Id: I7c82f0909656a802ec103b2af40bceb436e26e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173372
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924711}

--- components/content_settings/browser/content_settings_manager_impl.cc
+++ components/content_settings/browser/content_settings_manager_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
+#include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
@@ -125,6 +126,17 @@ void OnContentBlockedOnUI(int render_process_id,
     settings->OnContentBlocked(type);
 }
 
+// We may or may not be on the UI thread depending on whether the
+// NavigationThreadingOptimizations feature is enabled.
+// TODO(https://crbug.com/1187753): Clean this up once the feature is
+// shipped and the code path is removed.
+void RunOrPostTaskOnUI(const base::Location& location, base::OnceClosure task) {
+  if (content::BrowserThread::CurrentlyOn(content::BrowserThread::UI))
+    std::move(task).Run();
+  else
+    content::GetUIThreadTaskRunner({})->PostTask(location, std::move(task));
+}
+
 }  // namespace
 
 ContentSettingsManagerImpl::~ContentSettingsManagerImpl() = default;
@@ -178,8 +190,8 @@ void ContentSettingsManagerImpl::AllowStorageAccess(
     return;
   }
 
-  content::RunOrPostTaskOnThread(
-      FROM_HERE, content::BrowserThread::UI,
+  RunOrPostTaskOnUI(
+      FROM_HERE,
       base::BindOnce(&NotifyStorageAccess, render_process_id_, render_frame_id,
                      storage_type, url, top_frame_origin, allowed));
 
@@ -189,10 +201,9 @@ void ContentSettingsManagerImpl::AllowStorageAccess(
 void ContentSettingsManagerImpl::OnContentBlocked(int32_t render_frame_id,
                                                   ContentSettingsType type) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  content::RunOrPostTaskOnThread(
-      FROM_HERE, content::BrowserThread::UI,
-      base::BindOnce(&OnContentBlockedOnUI, render_process_id_, render_frame_id,
-                     type));
+  RunOrPostTaskOnUI(FROM_HERE,
+                    base::BindOnce(&OnContentBlockedOnUI, render_process_id_,
+                                   render_frame_id, type));
 }
 
 ContentSettingsManagerImpl::ContentSettingsManagerImpl(
