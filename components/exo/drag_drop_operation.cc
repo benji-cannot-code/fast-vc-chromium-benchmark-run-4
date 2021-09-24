bb11504283f459a786bb538057de817ec30705a2
Antonio Gomes <tonikitoo@igalia.com>
2021-09-24 03:20:54 +0000

    [exo] Add initial support to 'chromium/x-web-custom-data' mime type
    This CL adds a initial support in Exo to chromium's custom data
    mime types. Particularly, it also adds support to Ozone/Wayland
    (ie Lacros) to make use of this support, and offer them, upon
    request.
    
    This CL is the basis for supporting "WebUI Tab Dragging" with
    both touch and mouse to trigger work ChromeOS.
    
    BUG=1236708
    
    Change-Id: Ibf000ae32915f75b9bb959e49e70e4c480317c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3166178
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#924624}

--- components/exo/drag_drop_operation.cc
+++ components/exo/drag_drop_operation.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/barrier_closure.h"
 #include "base/check.h"
+#include "base/pickle.h"
 #include "base/strings/string_split.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "build/chromeos_buildflags.h"
@@ -227,6 +228,8 @@ DragDropOperation::DragDropOperation(
                      origin->window()),
       base::BindOnce(&DragDropOperation::OnFileContentsRead,
                      weak_ptr_factory_.GetWeakPtr()),
+      base::BindOnce(&DragDropOperation::OnWebCustomDataRead,
+                     weak_ptr_factory_.GetWeakPtr()),
       counter_);
 }
 
@@ -287,6 +290,17 @@ void DragDropOperation::OnFileContentsRead(const std::string& mime_type,
   DCHECK(os_exchange_data_);
   os_exchange_data_->SetFileContents(filename,
                                      std::string(data.begin(), data.end()));
+  mime_type_ = mime_type;
+  counter_.Run();
+}
+
+void DragDropOperation::OnWebCustomDataRead(const std::string& mime_type,
+                                            const std::vector<uint8_t>& data) {
+  DCHECK(os_exchange_data_);
+  base::Pickle pickle(reinterpret_cast<const char*>(data.data()), data.size());
+  os_exchange_data_->SetPickledData(
+      ui::ClipboardFormatType::WebCustomDataType(), pickle);
+
   mime_type_ = mime_type;
   counter_.Run();
 }
