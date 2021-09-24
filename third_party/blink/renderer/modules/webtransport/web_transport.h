2f86fb60b9441b55c33f5372714e9f8d4f8d001d
Yutaka Hirano <yhirano@chromium.org>
2021-09-24 03:05:26 +0000

    [WebTransport] Improve session termination handling
     - Define WebTransport::Cleanup which is corresponding to
       https://w3c.github.io/webtransport/#webtransport-cleanup.
     - Datagrams are now errored when close() is called.
    
    Bug: 1011392
    Change-Id: I67acea071df27f17c0c01ec788d247a70b9bc872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3167921
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924618}

--- third_party/blink/renderer/modules/webtransport/web_transport.h
+++ third_party/blink/renderer/modules/webtransport/web_transport.h
@@ -87,6 +87,8 @@ class MODULES_EXPORT WebTransport final
   void OnDatagramReceived(base::span<const uint8_t> data) override;
   void OnIncomingStreamClosed(uint32_t stream_id, bool fin_received) override;
 
+  void OnClosed(const absl::optional<WebTransportCloseInfo>&);
+
   // Implementation of ExecutionContextLifecycleObserver
   void ContextDestroyed() final;
 
@@ -120,8 +122,11 @@ class MODULES_EXPORT WebTransport final
   void ResetAll();
 
   void Dispose();
+  void Cleanup(v8::Local<v8::Value> reason,
+               v8::Local<v8::Value> error,
+               bool abruptly);
   void OnConnectionError();
-  void RejectPendingStreamResolvers();
+  void RejectPendingStreamResolvers(v8::Local<v8::Value> error);
   void OnCreateSendStreamResponse(ScriptPromiseResolver*,
                                   mojo::ScopedDataPipeProducerHandle,
                                   bool succeeded,
@@ -131,8 +136,6 @@ class MODULES_EXPORT WebTransport final
                                            mojo::ScopedDataPipeConsumerHandle,
                                            bool succeeded,
                                            uint32_t stream_id);
-
-  bool cleanly_closed_ = false;
 
   Member<DatagramDuplexStream> datagrams_;
 
