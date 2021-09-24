0b6d3c34af7e1167e9ff235285bed847982fa97b
Toshiki Kikuchi <toshikikikuchi@google.com>
2021-09-24 03:28:22 +0000

    Consider divider width in splitview carrying over
    This CL fixes the snapped bounds carry over logic considering the
    divider width in to-tablet/to-clamshell transitions.
    The center of the tablet splitscreen should be the center of the divider
    instead of the edge of the primary/secondary window.
    
    * Tablet mode
      |       left      | divider |      right     |
                         <==>|<==>
                             |
                     the center of divider
    * Clamshell mode
     <Correct position with this CL>
      |       left           |        right        |
     <Wrong position>
      |       left      |             right        |
    
    TEST=ClamshellTabletTransitionDividerPositionTest
    BUG=b:198998320
    
    Change-Id: I9f0e63864214df226934b45caa9611133ba3cc34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172996
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#924627}

--- ash/wm/tablet_mode/tablet_mode_window_manager_unittest.cc
+++ ash/wm/tablet_mode/tablet_mode_window_manager_unittest.cc
@@ -1741,8 +1741,7 @@ TEST_F(TabletModeWindowManagerTest,
             1200 * 0.33 - kSplitviewDividerShortSideLength / 2);
   // Exit tablet mode and verify the window stays in the same position.
   DestroyTabletModeWindowManager();
-  EXPECT_EQ(window->bounds().width(),
-            1200 * 0.33 - kSplitviewDividerShortSideLength / 2);
+  EXPECT_EQ(window->bounds().width(), 1200 * 0.33);
 
   // Now test the 2 windows case.
   std::unique_ptr<aura::Window> window2(
@@ -1765,8 +1764,7 @@ TEST_F(TabletModeWindowManagerTest,
             1200 - window->bounds().width() - kSplitviewDividerShortSideLength);
   // Exit tablet mode and verify the windows stay in the same position.
   DestroyTabletModeWindowManager();
-  EXPECT_EQ(window->bounds().width(),
-            1200 * 0.33 - kSplitviewDividerShortSideLength / 2);
+  EXPECT_EQ(window->bounds().width(), 1200 * 0.33);
   EXPECT_EQ(window2->bounds().width(), 1200 - window->bounds().width());
 }
 
