19ee34d77f589b4d0e2cc46d8589cfabbcec91f2
Zentaro Kavanagh <zentaro@chromium.org>
2021-09-25 01:53:40 +0000

    Firmware Updater: Add flag for feature
    Bug: 1252981
    Test: N/A
    Change-Id: I61271d3620b18cd6527f1a7a3329d45600b08c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3183622
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925004}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -5745,6 +5745,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableShortcutCustomizationAppDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(features::kShortcutCustomizationApp)},
 
+    {"enable-firmware-updater-app",
+     flag_descriptions::kEnableFirmwareUpdaterAppName,
+     flag_descriptions::kEnableFirmwareUpdaterAppDescription, kOsCrOS,
+     FEATURE_VALUE_TYPE(ash::features::kFirmwareUpdaterApp)},
+
     {"enhanced-network-voices", flag_descriptions::kEnhancedNetworkVoicesName,
      flag_descriptions::kEnhancedNetworkVoicesDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(features::kEnhancedNetworkVoices)},
