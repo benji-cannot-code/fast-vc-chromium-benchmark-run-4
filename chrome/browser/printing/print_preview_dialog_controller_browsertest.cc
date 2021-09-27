e147e51ae9c3541846764f25ad6f0c2a26ef6d92
K. Moon <kmoon@chromium.org>
2021-09-27 22:52:23 +0000

    [unseasoned-pdf] Test PrintPreviewPdfAccessibility in both modes
    Adds a feature override of PrintPreviewDialogControllerBrowserTest in
    order to test PrintPreviewPdfAccessibility in both Pepper and unseasoned
    PDF plugin modes.
    
    Fixed: 1144444
    Change-Id: Iac755d36b4cbec70bce4b6210d79c16f26208023
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3187587
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925474}

--- chrome/browser/printing/print_preview_dialog_controller_browsertest.cc
+++ chrome/browser/printing/print_preview_dialog_controller_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/with_feature_override.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/plugins/chrome_plugin_service_filter.h"
@@ -42,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
 #include "ipc/ipc_message_macros.h"
+#include "pdf/pdf_features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_switches.h"
 #include "url/gurl.h"
@@ -159,6 +161,14 @@ class PrintPreviewDialogControllerBrowserTest : public InProcessBrowserTest {
       cloned_tab_observer_;
   printing::TestPrintViewManagerForRequestPreview* test_print_view_manager_;
   WebContents* initiator_ = nullptr;
+};
+
+class PrintPreviewDialogControllerBrowserTestWithUnseasonedOverride
+    : public base::test::WithFeatureOverride,
+      public PrintPreviewDialogControllerBrowserTest {
+ public:
+  PrintPreviewDialogControllerBrowserTestWithUnseasonedOverride()
+      : base::test::WithFeatureOverride(chrome_pdf::features::kPdfUnseasoned) {}
 };
 
 // Test to verify that when a initiator navigates, we can create a new preview
@@ -348,8 +358,9 @@ IN_PROC_BROWSER_TEST_F(PrintPreviewDialogControllerBrowserTest,
   PrintPreviewDone();
 }
 
-IN_PROC_BROWSER_TEST_F(PrintPreviewDialogControllerBrowserTest,
-                       PrintPreviewPdfAccessibility) {
+IN_PROC_BROWSER_TEST_P(
+    PrintPreviewDialogControllerBrowserTestWithUnseasonedOverride,
+    PrintPreviewPdfAccessibility) {
   content::BrowserAccessibilityState::GetInstance()->EnableAccessibility();
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(),
                                            GURL("data:text/html,HelloWorld")));
@@ -359,3 +370,6 @@ IN_PROC_BROWSER_TEST_F(PrintPreviewDialogControllerBrowserTest,
 
   PrintPreviewDone();
 }
+
+INSTANTIATE_FEATURE_OVERRIDE_TEST_SUITE(
+    PrintPreviewDialogControllerBrowserTestWithUnseasonedOverride);
