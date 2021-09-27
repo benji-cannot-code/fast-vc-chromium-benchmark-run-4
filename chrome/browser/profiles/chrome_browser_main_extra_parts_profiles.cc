9c64d3c00058eaf2c45f17e6fbaefa11a59a99ee
Olesia Marukhno <olesiamarukhno@google.com>
2021-09-27 14:17:45 +0000

    [about this site] Initial setup for the data service
    Add AboutThisSite service and service factory. For now, it returns
    a test string instead of the actual data. Also, add optimization type
    ABOUT_THIS_SITE.
    
    Bug: 1250653
    Change-Id: I76c4b56c269c141defa29a53cbfaac57e958e0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169719
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925251}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notifier_state_tracker_factory.h"
 #include "chrome/browser/optimization_guide/model_validator_keyed_service_factory.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
+#include "chrome/browser/page_info/about_this_site_service_factory.h"
 #include "chrome/browser/page_load_metrics/observers/https_engagement_metrics/https_engagement_service_factory.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_memory_tracker_factory.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
@@ -254,6 +255,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #endif
 
   AboutSigninInternalsFactory::GetInstance();
+  AboutThisSiteServiceFactory::GetInstance();
   AccessContextAuditServiceFactory::GetInstance();
   AccountConsistencyModeManagerFactory::GetInstance();
   AccountInvestigatorFactory::GetInstance();
--- components/optimization_guide/core/hints_processing_util.cc
+++ components/optimization_guide/core/hints_processing_util.cc
@@ -61,6 +61,8 @@ std::string GetStringNameForOptimizationType(
       return "PriceTracking";
     case proto::OptimizationType::BLOOM_FILTER_VALIDATION:
       return "BloomFilterValidation";
+    case proto::OptimizationType::ABOUT_THIS_SITE:
+      return "AboutThisSite";
   }
   NOTREACHED();
   return std::string();
