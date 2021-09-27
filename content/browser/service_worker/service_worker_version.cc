e4c0e0933d7acc647819280c7bc157658cba2ce9
Matt Falkenhagen <falken@chromium.org>
2021-09-27 05:38:14 +0000

    service worker: Obsolete "WorkerStopped" histogram.
    This histogram expired and we probably won't use it. At the time of
    removal, on Windows approx 99.9% of stops are NORMAL, 0.07% are
    DETACH_BY_REGISTRY, and 0.08% are TIMEOUT. On Android, 65% NORMAL, 35%
    DETACH_BY_REGISTRY, 0.5% TIMEOUT.  This is likely because of process
    kills on Android even when the service worker is running.
    
    Bug: 1224551, 824628
    Change-Id: I6417f9c5eae32da2620685d4b7a94c11ac7472c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179697
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925149}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -1257,10 +1257,6 @@ void ServiceWorkerVersion::OnStopping() {
 }
 
 void ServiceWorkerVersion::OnStopped(EmbeddedWorkerStatus old_status) {
-  if (IsInstalled(status())) {
-    ServiceWorkerMetrics::RecordWorkerStopped(
-        ServiceWorkerMetrics::StopStatus::NORMAL);
-  }
   if (!stop_time_.is_null())
     ServiceWorkerMetrics::RecordStopWorkerTime(GetTickDuration(stop_time_));
 
@@ -1268,10 +1264,6 @@ void ServiceWorkerVersion::OnStopped(EmbeddedWorkerStatus old_status) {
 }
 
 void ServiceWorkerVersion::OnDetached(EmbeddedWorkerStatus old_status) {
-  if (IsInstalled(status())) {
-    ServiceWorkerMetrics::RecordWorkerStopped(
-        ServiceWorkerMetrics::StopStatus::DETACH_BY_REGISTRY);
-  }
   OnStoppedInternal(old_status);
 }
 
@@ -2003,10 +1995,6 @@ void ServiceWorkerVersion::OnTimeoutTimer() {
   // Stopping the worker hasn't finished within a certain period.
   if (GetTickDuration(stop_time_) > kStopWorkerTimeout) {
     DCHECK_EQ(EmbeddedWorkerStatus::STOPPING, running_status());
-    if (IsInstalled(status())) {
-      ServiceWorkerMetrics::RecordWorkerStopped(
-          ServiceWorkerMetrics::StopStatus::TIMEOUT);
-    }
     ReportError(blink::ServiceWorkerStatusCode::kErrorTimeout,
                 "DETACH_STALLED_IN_STOPPING");
 
