f730bb4885c368b49e12f98c79f9e3f6aa86120b
Maksim Sisov <msisov@igalia.com>
2021-09-27 12:44:18 +0000

    ozone: //components/embedder_support clean up from USE_X11
    USE_X11 is deprecated and Ozone is default on Linux.
    
    Bug: 1096425
    Change-Id: I793ad2d55f9fed5ab9a3be96e4e431c4cddeed43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3186392
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#925232}

--- components/embedder_support/user_agent_utils_unittest.cc
+++ components/embedder_support/user_agent_utils_unittest.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "third_party/re2/src/re2/re2.h"
 
-#if defined(USE_X11) || defined(USE_OZONE)
+#if defined(USE_OZONE)
 #include <sys/utsname.h>
 #endif
 
@@ -129,7 +129,7 @@ void CheckUserAgentStringOrdering(bool mobile_device) {
   ASSERT_LE(0, value);
   ASSERT_TRUE(base::StringToInt(pieces[2], &value));
   ASSERT_LE(0, value);
-#elif defined(USE_X11) || defined(USE_OZONE)
+#elif defined(USE_OZONE)
   // X11; Linux x86_64
   // X11; CrOS armv7l 4537.56.0
   struct utsname unixinfo;
