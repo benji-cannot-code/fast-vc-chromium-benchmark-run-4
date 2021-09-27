b7a2d79e6e000138534ddce46adbf6e95c741924
Chris Davis (EDGE) <chrdavis@microsoft.com>
2021-09-27 23:23:16 +0000

    Ensure occlusion recalculation on resume or login
    When resuming windows or logging back in we sometimes do not run a
    recalculation of the occlusion state.  This is because of 2 issues.
    First, after unlocking the PC we may not get a foreground event that
    will trigger a recalculation.  Second, if we do go down the path of
    recalculation we may hit a timing issue where the occlusion timer is
    running, blocking us from recalculating.  Both of these are fixed with
    this code change.  Since timers can be unreliable going into and out
    of sleep code is added to disable timers at those times.
    
    Bug: 1252551
    Change-Id: I9d6b727a094eb6697dd354814b6d99785d748383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180265
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Chris Davis <chrdavis@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#925487}

--- ui/base/win/power_setting_change_listener.h
+++ ui/base/win/power_setting_change_listener.h
@@ -14,6 +14,8 @@ namespace ui {
 class COMPONENT_EXPORT(UI_BASE) PowerSettingChangeListener {
  public:
   virtual void OnDisplayStateChanged(bool display_on) = 0;
+  virtual void OnResume() = 0;
+  virtual void OnSuspend() = 0;
 
  protected:
   virtual ~PowerSettingChangeListener() = default;
