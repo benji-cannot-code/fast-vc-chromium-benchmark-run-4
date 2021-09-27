ada4433484af98e64793e909ae88714bd9cf1e8e
Kasjan Siwek <kasiwek@microsoft.com>
2021-09-27 21:50:23 +0000

    Add CHROME_ROOT_STORE_SUPPORTED flag check to unit tests that depend on this flag.
    https://crrev.com/c/3143874 (https://crbug.com/1239258) has added few unit tests,
    which depend on sources, which get built only when CHROME_ROOT_STORE_SUPPORTED flag
    is enabled. This change simply adds this check.
    
    Change-Id: Ie4bcae542eedeedf2f3b2045de16fb09a2c31059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3187875
    Commit-Queue: Nicolas Arciniega <niarci@microsoft.com>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925444}

--- net/cert/internal/path_builder_unittest.cc
+++ net/cert/internal/path_builder_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/internal/verify_certificate_chain.h"
 #include "net/cert/pem.h"
 #include "net/der/input.h"
+#include "net/net_buildflags.h"
 #include "net/test/test_certificate_data.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -582,7 +583,7 @@ TEST_F(PathBuilderMultiRootTest, TestTrivialDeadline) {
   }
 }
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) && BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
 
 void AddToStoreWithEKURestriction(HCERTSTORE store,
                                   const scoped_refptr<ParsedCertificate>& cert,
@@ -681,7 +682,7 @@ TEST_F(PathBuilderMultiRootTest, TrustStoreWinNoPathEKURestrictions) {
   auto result = path_builder.Run();
   ASSERT_FALSE(result.HasValidPath());
 }
-#endif  // OS_WIN
+#endif  // defined(OS_WIN) && BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
 
 class PathBuilderKeyRolloverTest : public ::testing::Test {
  public:
