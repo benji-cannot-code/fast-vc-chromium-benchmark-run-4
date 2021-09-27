52458f5a31af268877a5748f1721bd1b6d24529a
Aida Zolic <aidazolic@chromium.org>
2021-09-27 08:29:16 +0000

    Add warn to screenshot restrictions in extensions API
    For V1, we treat block and warn Data Leak Prevention restriction the
    same in case of "tabs.captureVisibleTab" extensions API and do not allow
    taking screenshots.
    
    Bug: 1222814
    Change-Id: I0f267aea34ff49b91032186caf1fa9d833dc4108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176243
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925184}

--- chrome/browser/ash/policy/dlp/dlp_content_manager.h
+++ chrome/browser/ash/policy/dlp/dlp_content_manager.h
@@ -60,6 +60,9 @@ class DlpContentManager : public DlpWindowObserver::Delegate {
   // Returns whether screenshots should be restricted.
   virtual bool IsScreenshotRestricted(const ScreenshotArea& area) const;
 
+  // Returns whether screenshots should be restricted for extensions API.
+  virtual bool IsScreenshotApiRestricted(const ScreenshotArea& area) const;
+
   // Returns whether video capture should be restricted.
   bool IsVideoCaptureRestricted(const ScreenshotArea& area) const;
 
