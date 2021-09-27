64fba777f454b69726e56f58e1afa6d6b1bdc5a7
Nancy Wang <nancylingwang@chromium.org>
2021-09-27 23:44:01 +0000

    Refactor app icon factory part 1.
    This CL is used to prepare for CL:3185339, which moves the class
    IconLoadingPipeline to a separate file.
    
    kInvalidIconResource and GetScaleToSize are used by both the class
    IconLoadingPipeline and other functions in app icon factory. So move
    kInvalidIconResource and GetScaleToSize out of the empty namespace, so
    that IconLoadingPipeline can use them after moving to a separate file.
    
    This is pure refactor. No function change.
    
    BUG=1253250
    
    Change-Id: I8c907ad6b6966c14e080308de621d10c7305527e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3185420
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925502}

--- chrome/browser/apps/app_service/app_icon_factory.cc
+++ chrome/browser/apps/app_service/app_icon_factory.cc
@@ -66,8 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-static const int kInvalidIconResource = 0;
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 
 // Copy from Android code, all four sides of the ARC foreground and background
@@ -117,18 +115,6 @@ const gfx::ImageSkiaRep& GetMaskAsImageSkiaRep(float scale,
                                     size_hint_in_dip, size_hint_in_dip),
       scale);
   return image_rep;
-}
-
-apps::ScaleToSize GetScaleToSize(const gfx::ImageSkia& image_skia) {
-  apps::ScaleToSize scale_to_size;
-  if (image_skia.image_reps().empty()) {
-    scale_to_size[1.0f] = image_skia.size().width();
-  } else {
-    for (const auto& rep : image_skia.image_reps()) {
-      scale_to_size[rep.scale()] = rep.pixel_width();
-    }
-  }
-  return scale_to_size;
 }
 
 bool IsConsistentPixelSize(const gfx::ImageSkiaRep& rep,
@@ -586,7 +572,7 @@ void IconLoadingPipeline::ApplyIconEffects(apps::IconEffects icon_effects,
 
   gfx::ImageSkia mask_image;
   if (icon_effects & apps::IconEffects::kCrOsStandardMask) {
-    mask_image = apps::LoadMaskImage(GetScaleToSize(iv->uncompressed));
+    mask_image = apps::LoadMaskImage(apps::GetScaleToSize(iv->uncompressed));
     mask_image.MakeThreadSafe();
   }
 
@@ -828,7 +814,7 @@ void IconLoadingPipeline::LoadIconFromResource(int icon_resource) {
   }
 #endif
 
-  if (icon_resource == kInvalidIconResource) {
+  if (icon_resource == apps::kInvalidIconResource) {
     MaybeLoadFallbackOrCompleteEmpty();
     return;
   }
@@ -1164,10 +1150,10 @@ void IconLoadingPipeline::MaybeLoadFallbackOrCompleteEmpty() {
     return;
   }
 
-  if (fallback_icon_resource_ != kInvalidIconResource) {
+  if (fallback_icon_resource_ != apps::kInvalidIconResource) {
     int icon_resource = fallback_icon_resource_;
     // Resetting default icon resource to ensure no infinite loops.
-    fallback_icon_resource_ = kInvalidIconResource;
+    fallback_icon_resource_ = apps::kInvalidIconResource;
     LoadIconFromResource(icon_resource);
     return;
   }
@@ -1178,6 +1164,18 @@ void IconLoadingPipeline::MaybeLoadFallbackOrCompleteEmpty() {
 }  // namespace
 
 namespace apps {
+
+apps::ScaleToSize GetScaleToSize(const gfx::ImageSkia& image_skia) {
+  apps::ScaleToSize scale_to_size;
+  if (image_skia.image_reps().empty()) {
+    scale_to_size[1.0f] = image_skia.size().width();
+  } else {
+    for (const auto& rep : image_skia.image_reps()) {
+      scale_to_size[rep.scale()] = rep.pixel_width();
+    }
+  }
+  return scale_to_size;
+}
 
 base::OnceCallback<void(std::vector<uint8_t> compressed_data)>
 CompressedDataToImageSkiaCallback(
