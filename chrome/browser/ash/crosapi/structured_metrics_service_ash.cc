ae762b2a0095be4e99ff2c0597f016f92a276047
Jonghyun Ahn <jongahn@google.com>
2021-09-27 21:26:33 +0000

    [Structured Metrics] Refactor ash service to use Recorder explicitly.
    This refactor is needed to feature guard the usage of the mojo pipes
    behind StructuredMetricsClient.
    
    Bug: 1238197
    Change-Id: I0e4ef465a90a1bd66f64c3190a47d9e2bf73a5ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179882
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925431}

--- chrome/browser/ash/crosapi/structured_metrics_service_ash.cc
+++ chrome/browser/ash/crosapi/structured_metrics_service_ash.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/structured_metrics_service.mojom.h"
 #include "components/metrics/structured/event.h"
 #include "components/metrics/structured/event_base.h"
+#include "components/metrics/structured/recorder.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 
@@ -27,7 +28,8 @@ void StructuredMetricsServiceAsh::Record(
     auto event_base = metrics::structured::EventBase::FromEvent(event);
     // Only record the event if it is valid.
     if (event_base.has_value())
-      event_base.value().Record();
+      metrics::structured::Recorder::GetInstance()->Record(
+          std::move(event_base.value()));
   }
 }
 
