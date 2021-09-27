0c53eb498da073e630cd3cf7f46ca98edf6ccceb
Yue Zhang <yuezhanggg@chromium.org>
2021-09-27 17:53:49 +0000

    [ChromeCart][Coupon] Don't fetch when no cart from partner merchants
    Bug: 1252223
    Change-Id: Ia92ffe3b733d12ce1bc2bdb173c91f8be2ba2cd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177652
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925299}

--- chrome/browser/cart/fetch_discount_worker.cc
+++ chrome/browser/cart/fetch_discount_worker.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/no_destructor.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/cart/cart_discount_fetcher.h"
@@ -23,11 +24,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_constants.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "third_party/re2/src/re2/re2.h"
 
 namespace {
 const char kOauthName[] = "rbd";
 const char kOauthScopes[] = "https://www.googleapis.com/auth/chromememex";
 const char kEmptyToken[] = "";
+
+const re2::RE2& GetPartnerMerchantPattern() {
+  re2::RE2::Options options;
+  options.set_case_sensitive(false);
+  static base::NoDestructor<re2::RE2> instance(
+      cart_features::kPartnerMerchantPattern.Get(), options);
+  return *instance;
+}
+
+bool IsPartnerMerchant(const GURL& url) {
+  const std::string& url_string = url.spec();
+  return RE2::PartialMatch(
+      re2::StringPiece(url_string.data(), url_string.size()),
+      GetPartnerMerchantPattern());
+}
 }  // namespace
 
 CartServiceDelegate::CartServiceDelegate(CartService* cart_service)
@@ -139,6 +156,19 @@ void FetchDiscountWorker::ReadyToFetch(
                      weak_ptr_factory_.GetWeakPtr());
 
   cart_service_delegate_->RecordFetchTimestamp();
+  // If there is no partner merchant cart, don't fetch immediately; instead,
+  // post another delayed fetch.
+  bool has_partner_merchant = false;
+  for (auto pair : proto_pairs) {
+    if (IsPartnerMerchant(GURL(pair.second.merchant_cart_url()))) {
+      has_partner_merchant = true;
+      break;
+    }
+  }
+  if (!has_partner_merchant) {
+    Start(cart_features::kDiscountFetchDelayParam.Get());
+    return;
+  }
   backend_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(
