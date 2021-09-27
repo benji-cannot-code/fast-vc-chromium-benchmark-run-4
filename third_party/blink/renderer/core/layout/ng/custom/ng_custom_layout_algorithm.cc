78381e1b9d933bd10310c770e053905775a19b8a
Morten Stenshorne <mstensho@chromium.org>
2021-09-27 22:47:54 +0000

    Treat custom layout objects as monolithic.
    Custom layout block fragmentation isn't implemented, so just avoid it.
    
    This fixes
    external/wpt/css/css-layout-api/multicol-child-crash.https.html when
    LayoutNGBlockFragmentation is enabled.
    
    Also update a DCHECK to allow breaks *before* a custom layout object,
    and added a test that would otherwise crash with
    LayoutNGBlockFragmentation enabled.
    
    Bug: 829028
    Change-Id: I5a433a5214e5e435a844e3cb7dbf1d427d846b5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3186453
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925472}

--- third_party/blink/renderer/core/layout/ng/custom/ng_custom_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/custom/ng_custom_layout_algorithm.cc
@@ -80,7 +80,7 @@ MinMaxSizesResult NGCustomLayoutAlgorithm::ComputeMinMaxSizes(
 }
 
 scoped_refptr<const NGLayoutResult> NGCustomLayoutAlgorithm::Layout() {
-  DCHECK(!BreakToken());
+  DCHECK(!IsResumingLayout(BreakToken()));
 
   if (!Node().IsCustomLayoutLoaded())
     return FallbackLayout();
