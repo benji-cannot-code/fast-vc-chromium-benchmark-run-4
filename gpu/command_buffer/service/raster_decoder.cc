33cc72d04467acec4c41b42a6f585619de209d8f
Justin Novosad <junov@chromium.org>
2021-09-27 22:30:23 +0000

    Add UMA Histogram buckets for PaintOp deserialization errors.
    This change adds histogram buckets to track reasons why SharedImage
    retrieval failed while decoding a PaintOp.
    
    BUG=1019634, 1245368
    
    Change-Id: I6afbd0d29ac7ff9a1ca36242f3b5ce33e35e43bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3187492
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925463}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -245,10 +245,13 @@ class SharedImageProviderImpl final : public cc::SharedImageProvider {
 
   ~SharedImageProviderImpl() override { read_accessors_.clear(); }
 
-  sk_sp<SkImage> OpenSharedImageForRead(const gpu::Mailbox& mailbox) override {
+  sk_sp<SkImage> OpenSharedImageForRead(const gpu::Mailbox& mailbox,
+                                        Error& error) override {
     auto it = read_accessors_.find(mailbox);
-    if (it != read_accessors_.end())
+    error = Error::kNoError;
+    if (it != read_accessors_.end()) {
       return it->second.read_access_sk_image;
+    }
 
     auto shared_image_skia =
         shared_image_factory_->ProduceSkia(mailbox, shared_context_state_);
@@ -258,6 +261,7 @@ class SharedImageProviderImpl final : public cc::SharedImageProvider {
                               ("Attempting to operate on unknown mailbox:" +
                                mailbox.ToDebugString())
                                   .c_str());
+      error = Error::kUnknownMailbox;
       return nullptr;
     }
 
@@ -272,6 +276,7 @@ class SharedImageProviderImpl final : public cc::SharedImageProvider {
                               ("Couldn't access shared image for mailbox:" +
                                mailbox.ToDebugString())
                                   .c_str());
+      error = Error::kNoAccess;
       return nullptr;
     }
 
@@ -288,6 +293,7 @@ class SharedImageProviderImpl final : public cc::SharedImageProvider {
       ERRORSTATE_SET_GL_ERROR(error_state_, GL_INVALID_OPERATION,
                               "SharedImageProviderImpl::OpenSharedImageForRead",
                               "Couldn't create output SkImage.");
+      error = Error::kSkImageCreationFailed;
       return nullptr;
     }
 
