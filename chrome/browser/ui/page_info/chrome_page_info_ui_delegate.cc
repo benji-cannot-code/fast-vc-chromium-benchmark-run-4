b41a2e18c357bb6df9c322b4aeb51cdc9cf1fdd3
Olesia Marukhno <olesiamarukhno@google.com>
2021-09-27 14:26:55 +0000

    [about this site] Initial setup for desktop page info UI
    Add "About this site" button to the page info. It has a separator on top
    and bottom. It shows short description as a subtitle and opens the
    subpage with more information.
    
    The button is only shown if there is data available from the service.
    
    Bug: 1250653
    Change-Id: I79a70f6a65ac41a05993123a41d8c68e0f1c4571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168889
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925253}

--- chrome/browser/ui/page_info/chrome_page_info_ui_delegate.cc
+++ chrome/browser/ui/page_info/chrome_page_info_ui_delegate.cc
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "chrome/browser/content_settings/chrome_content_settings_utils.h"
+#include "chrome/browser/page_info/about_this_site_service_factory.h"
 #include "chrome/browser/permissions/permission_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/page_info/about_this_site_service.h"
 #include "components/permissions/permission_manager.h"
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
@@ -89,6 +91,14 @@ std::u16string ChromePageInfoUiDelegate::GetAutomaticallyBlockedReason(
       break;
   }
 
+  return std::u16string();
+}
+
+std::u16string ChromePageInfoUiDelegate::GetAboutThisSiteDescription() {
+  if (auto* service =
+          AboutThisSiteServiceFactory::GetForProfile(GetProfile())) {
+    return service->GetAboutThisSiteDescription(site_url_);
+  }
   return std::u16string();
 }
 
--- chrome/browser/ui/views/page_info/page_info_main_view.cc
+++ chrome/browser/ui/views/page_info/page_info_main_view.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/page_info/permission_toggle_row_view.h"
 #include "chrome/browser/vr/vr_tab_helper.h"
 #include "chrome/common/url_constants.h"
+#include "components/page_info/features.h"
 #include "components/permissions/permission_util.h"
 #include "components/strings/grit/components_chromium_strings.h"
 #include "components/strings/grit/components_strings.h"
@@ -86,6 +87,15 @@ PageInfoMainView::PageInfoMainView(
   permissions_view_ = layout->AddView(std::make_unique<views::View>());
   permissions_view_->SetLayoutManager(std::make_unique<views::FlexLayout>())
       ->SetOrientation(views::LayoutOrientation::kVertical);
+
+  if (base::FeatureList::IsEnabled(page_info::kPageInfoAboutThisSite)) {
+    std::u16string description = ui_delegate_->GetAboutThisSiteDescription();
+    if (!description.empty()) {
+      layout->StartRow(views::GridLayout::kFixedSize, kColumnId);
+      about_this_site_section_ =
+          layout->AddView(CreateAboutThisSiteSection(description));
+    }
+  }
 
   layout->StartRow(views::GridLayout::kFixedSize, kColumnId);
   site_settings_view_ = layout->AddView(CreateContainerView());
@@ -264,7 +274,11 @@ void PageInfoMainView::SetPermissionInfo(
   // show reset button.
   reset_button_->SetVisible(false);
   UpdateResetButton(permission_info_list);
-  permissions_view_->AddChildView(PageInfoViewFactory::CreateSeparator());
+  // 'About this site' section has separators on top and bottom. If it is shown,
+  // bottom separator here isn't needed anymore.
+  if (!about_this_site_section_) {
+    permissions_view_->AddChildView(PageInfoViewFactory::CreateSeparator());
+  }
 
   PreferredSizeChanged();
 }
@@ -485,4 +499,30 @@ std::unique_ptr<views::View> PageInfoMainView::CreateBubbleHeaderView() {
   header->AddChildView(close_button.release());
 
   return header;
+}
+
+std::unique_ptr<views::View> PageInfoMainView::CreateAboutThisSiteSection(
+    std::u16string description) {
+  auto about_this_site_section = std::make_unique<views::View>();
+  about_this_site_section
+      ->SetLayoutManager(std::make_unique<views::FlexLayout>())
+      ->SetOrientation(views::LayoutOrientation::kVertical);
+  about_this_site_section->AddChildView(PageInfoViewFactory::CreateSeparator());
+
+  // TODO(crbug.com/1250653): Update with the actual strings.
+  auto* about_this_site_button = about_this_site_section->AddChildView(
+      std::make_unique<PageInfoHoverButton>(
+          base::BindRepeating(
+              [](PageInfoMainView* view) {
+                // TODO(crbug.com/1250653): Open 'About this site' subpage.
+              },
+              this),
+          PageInfoViewFactory::GetAboutThisSiteIcon(), 0, std::u16string(),
+          PageInfoViewFactory::VIEW_ID_PAGE_INFO_ABOUT_THIS_SITE_BUTTON,
+          std::u16string(), description,
+          PageInfoViewFactory::GetOpenSubpageIcon()));
+  about_this_site_button->SetTitleText(u"About this site");
+
+  about_this_site_section->AddChildView(PageInfoViewFactory::CreateSeparator());
+  return about_this_site_section;
 }
