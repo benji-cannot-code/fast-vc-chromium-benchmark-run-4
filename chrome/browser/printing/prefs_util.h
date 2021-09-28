48ab41625e0ab0f59c40875c9c299df86b367a21
Lei Zhang <thestig@chromium.org>
2021-09-28 03:38:25 +0000

    Move prefs_util.h from components/ back to chrome/browser/printing/.
    In https://crrev.com/875064, code was extracted out of
    chrome/browser/ui/webui/print_preview/print_preview_handler.cc into
    components/printing/browser/prefs_util.h for use elsewhere. It turns out
    that elsewhere is also inside chrome/browser/. So move prefs_util.h back
    into chrome/browser/. This removes the duplicate pref name constant and
    some dependencies.
    
    Change-Id: Ib6d5d085105041e35cac4c84e135080a459a508a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174374
    Reviewed-by: Pranav Batra <batrapranav@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925608}

--- chrome/browser/printing/prefs_util.h
+++ chrome/browser/printing/prefs_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_PRINTING_BROWSER_PREFS_UTIL_H_
-#define COMPONENTS_PRINTING_BROWSER_PREFS_UTIL_H_
+#ifndef CHROME_BROWSER_PRINTING_PREFS_UTIL_H_
+#define CHROME_BROWSER_PRINTING_PREFS_UTIL_H_
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/geometry/size.h"
@@ -18,4 +18,4 @@ absl::optional<gfx::Size> ParsePaperSizeDefault(const PrefService& prefs);
 
 }  // namespace printing
 
-#endif  // COMPONENTS_PRINTING_BROWSER_PREFS_UTIL_H_
+#endif  // CHROME_BROWSER_PRINTING_PREFS_UTIL_H_
