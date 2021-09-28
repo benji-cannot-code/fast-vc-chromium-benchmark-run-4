d9b924e3b12d79c04f8b9a6b461281bf73d56435
Moe Ahmadi <mahmadi@chromium.org>
2021-09-28 20:10:11 +0000

    [memories] Show history list/clusters pages as tabs in chrome://history
    This CL embeds <history-list> and <history-clusters> inside two nested
    <iron-pages> elements with the outer one controlling the selected page
    between <history-synced-device-manager> and either of the history pages
    and inner one controlling the selected history page only.
    
    screenshot/BmpdpZSvMWVHkH7
    
    Bug: 1173908
    Change-Id: I744fd07ae62ba1479cf7e4d8ed5e6376a9f0a16d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3184081
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925906}

--- chrome/browser/resources/history/side_bar.ts
+++ chrome/browser/resources/history/side_bar.ts
@@ -20,6 +20,7 @@ import {PaperRippleElement} from 'chrome://resources/polymer/v3_0/paper-ripple/p
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BrowserService} from './browser_service.js';
+import {Page, TABBED_PAGES} from './router.js';
 
 export type FooterInfo = {
   managed: boolean,
@@ -43,14 +44,18 @@ export class HistorySideBarElement extends PolymerElement {
 
   static get properties() {
     return {
+      footerInfo: Object,
+
+      /* The id of the currently selected page. */
       selectedPage: {
         type: String,
         notify: true,
       },
 
-      guestSession_: Boolean,
+      /* The index of the currently selected tab. */
+      selectedTab: Number,
 
-      footerInfo: Object,
+      guestSession_: Boolean,
 
       historyClustersEnabled_: {
         type: Boolean,
@@ -68,8 +73,12 @@ export class HistorySideBarElement extends PolymerElement {
     };
   }
 
-  private guestSession_ = loadTimeData.getBoolean('isGuestSession');
   footerInfo: FooterInfo;
+  selectedPage: Page;
+  selectedTab: number;
+  private guestSession_ = loadTimeData.getBoolean('isGuestSession');
+  private historyClustersEnabled_: boolean;
+  private showFooter_: boolean;
 
   /** @override */
   ready() {
@@ -109,6 +118,30 @@ export class HistorySideBarElement extends PolymerElement {
    */
   private onItemClick_(e: Event) {
     e.preventDefault();
+  }
+
+  /**
+   * @returns The url to navigate to when the history menu item is clicked. It
+   *     reflects the currently selected tab.
+   */
+  private getHistoryItemHref_(
+      _selectedHistoryTab: number, _historyClustersEnabled: boolean): string {
+    return this.historyClustersEnabled_ &&
+            TABBED_PAGES[this.selectedTab] === Page.HISTORY_CLUSTERS ?
+        '/' + Page.HISTORY_CLUSTERS :
+        '/';
+  }
+
+  /**
+   * @returns The path that determines if the history menu item is selected. It
+   *     reflects the currently selected tab.
+   */
+  private getHistoryItemPath_(
+      _selectedHistoryTab: number, _historyClustersEnabled: boolean): string {
+    return this.historyClustersEnabled_ &&
+            TABBED_PAGES[this.selectedTab] === Page.HISTORY_CLUSTERS ?
+        Page.HISTORY_CLUSTERS :
+        Page.HISTORY;
   }
 
   private computeShowFooter_(
