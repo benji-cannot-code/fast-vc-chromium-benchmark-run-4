e6a1feddb2b8fa302e25efbabe9941f7fd966dc5
Peter E Conn <peconn@chromium.org>
2021-09-28 15:12:23 +0000

    🕸️ Create WebLayer::SiteEngagementServiceFactory.
    The added class corresponds to Chrome's SiteEngagementServiceFactory.
    It isn't plugged in yet - GetInstance() needs to be called from
    browser_main_parts_impl.cc.
    
    Change-Id: Ib9690d734532812c7488c44217eafa9b2a1817d0
    Bug: 1101999
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3135315
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925766}

---
+++ weblayer/browser/site_engagement/site_engagement_service_factory.cc
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "weblayer/browser/site_engagement/site_engagement_service_factory.h"
+
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+
+using site_engagement::SiteEngagementService;
+
+namespace weblayer {
+
+// static
+SiteEngagementService* SiteEngagementServiceFactory::GetForBrowserContext(
+    content::BrowserContext* browser_context) {
+  return static_cast<SiteEngagementService*>(
+      GetInstance()->GetServiceForBrowserContext(browser_context,
+                                                 /*create=*/true));
+}
+
+// static
+SiteEngagementServiceFactory* SiteEngagementServiceFactory::GetInstance() {
+  static base::NoDestructor<SiteEngagementServiceFactory> factory;
+  return factory.get();
+}
+
+SiteEngagementServiceFactory::SiteEngagementServiceFactory()
+    : BrowserContextKeyedServiceFactory(
+          "SiteEngagementService",
+          BrowserContextDependencyManager::GetInstance()) {
+  SiteEngagementService::SetServiceProvider(this);
+}
+
+SiteEngagementServiceFactory::~SiteEngagementServiceFactory() {
+  SiteEngagementService::ClearServiceProvider(this);
+}
+
+KeyedService* SiteEngagementServiceFactory::BuildServiceInstanceFor(
+    content::BrowserContext* browser_context) const {
+  return new SiteEngagementService(browser_context);
+}
+
+content::BrowserContext* SiteEngagementServiceFactory::GetBrowserContextToUse(
+    content::BrowserContext* context) const {
+  return context;
+}
+
+SiteEngagementService* SiteEngagementServiceFactory::GetSiteEngagementService(
+    content::BrowserContext* browser_context) {
+  return GetForBrowserContext(browser_context);
+}
+
+}  // namespace weblayer
