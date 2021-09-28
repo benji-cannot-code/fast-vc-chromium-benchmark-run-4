2694a2a428e08fbc7760ab027bbc3c154cdadb6f
Sigurdur Asgeirsson <siggi@chromium.org>
2021-09-28 13:33:41 +0000

    Reland "[battery] Fix CPU time scaling on M1 hardware."
    This is a reland of fafa4ef6544f54f3ea0ec1bcd5c2123c50061ff8
    
    Original change's description:
    > [battery] Fix CPU time scaling on M1 hardware.
    >
    > Bug: 1248442
    > Change-Id: I2c6ad9d84453a67cc231849c68cf3e2fb72b61c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179502
    > Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    > Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#925335}
    
    Bug: 1248442
    Change-Id: I5605a6075204444dbfc806fdbf050689f060b8e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3187491
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925729}

--- chrome/browser/performance_monitor/process_metrics_recorder_util.cc
+++ chrome/browser/performance_monitor/process_metrics_recorder_util.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#if defined(OS_MAC)
 #include "chrome/browser/performance_monitor/resource_coalition_mac.h"
+#endif
 
 namespace performance_monitor {
 
