e150f32600923af8b6041956280b412e49aced92
dpapad <dpapad@chromium.org>
2021-09-28 04:55:10 +0000

    WebUI: Check-in Polymer3 version of cr_policy_pref_behavior.js.
    This is in preparation of migrating it to a Mixin and then to
    TypeScript.
    
    Bug: 1184053
    Change-Id: Ic0c3e502c5b94adb6c9710833ca1e0f19e9bbde5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3185739
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925627}

--- chrome/browser/resources/settings/controls/settings_boolean_control_behavior.js
+++ chrome/browser/resources/settings/controls/settings_boolean_control_behavior.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-import {CrPolicyPrefBehavior} from 'chrome://resources/cr_elements/policy/cr_policy_pref_behavior.m.js';
+import {CrPolicyPrefBehavior} from 'chrome://resources/cr_elements/policy/cr_policy_pref_behavior.js';
 import {assert} from 'chrome://resources/js/assert.m.js';
 
 import {PrefControlBehavior, PrefControlBehaviorInterface} from './pref_control_behavior.js';
--- ui/webui/resources/cr_elements/policy/cr_policy_pref_behavior.js
+++ ui/webui/resources/cr_elements/policy/cr_policy_pref_behavior.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 /** @polymerBehavior */
-/* #export */ const CrPolicyPrefBehavior = {
+export const CrPolicyPrefBehavior = {
   properties: {
     /**
      * Showing that an extension is controlling a pref is sometimes done with a
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 };
 
 /** @interface */
-/* #export */ class CrPolicyPrefBehaviorInterface {
+export class CrPolicyPrefBehaviorInterface {
   /** @param {boolean} enabled */
   set noExtensionIndicator(enabled) {}
 
