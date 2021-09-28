22f4cc4abeb0f8cab8a1144c31e6d0b4a97297bf
Qasid Sadiq <qasid@google.com>
2021-09-28 20:49:02 +0000

    personalization: Disable Old Picker Background Updates
    When the new SWA Wallpaper Picker is enabled, the old picker should not
    receive updates.
    Without this, syncing from the old picker on other devices happens,
    and it's impossible to disable daily refresh if it was set on the old
    picker, and the user has moved to the new picker.
    
    Test: by hand, with the feature flag enabled/disabled.
    Bug: b/197900797
    Change-Id: I8e4b6fab964e795a9776f229bf793fd2d2deaf73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3174601
    Auto-Submit: Qasid Sadiq <qasid@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925925}

--- chrome/test/data/chromeos/wallpaper_manager/unit_tests/api_mock.js
+++ chrome/test/data/chromeos/wallpaper_manager/unit_tests/api_mock.js
@@ -290,6 +290,9 @@ var chrome = {
     getSurpriseMeImage: function(collectionId, resumeToken, callback) {
       callback(
           {imageUrl: TestConstants.wallpaperUrl}, null /*nextResumeToken=*/);
+    },
+    isSwaEnabled: function(callback) {
+      callback(false);
     }
   },
   runtime: {lastError: null}
