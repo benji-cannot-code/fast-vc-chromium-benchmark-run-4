c208eaaf4822b719d7540407a1bb96b76b46720d
Maksim Sisov <msisov@igalia.com>
2021-09-28 05:26:03 +0000

    ozone: //webplayer remove USE_X11
    USE_X11 is deprecated and Linux uses Ozone by default, which
    can switch between x11/wayland/headless backends on runtime
    via --ozone-platform=backend switch.
    
    Bug: 1096425
    Change-Id: I149dda830643e791a6b2d61c97714b88a5891f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3186832
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#925632}

--- weblayer/browser/browser_main_parts_impl.cc
+++ weblayer/browser/browser_main_parts_impl.cc
@@ -76,10 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/common/features.h"
 #endif
 
-#if defined(USE_AURA) && defined(USE_X11)
-#include "ui/base/ui_base_features.h"
-#include "ui/events/devices/x11/touch_factory_x11.h"  // nogncheck
-#endif
 // TODO(crbug.com/1052397): Revisit once build flag switch of lacros-chrome is
 // complete.
 #if defined(USE_AURA) && (defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS))
@@ -193,13 +189,6 @@ int BrowserMainPartsImpl::PreCreateThreads() {
 #endif
 
   return content::RESULT_CODE_NORMAL_EXIT;
-}
-
-void BrowserMainPartsImpl::PreCreateMainMessageLoop() {
-#if defined(USE_AURA) && defined(USE_X11)
-  if (!features::IsUsingOzonePlatform())
-    ui::TouchFactory::SetTouchDeviceListFromCommandLine();
-#endif
 }
 
 int BrowserMainPartsImpl::PreEarlyInitialization() {
