743d3aa2e310c2cf2696afabfaa1cfd2c106481d
Evan Stade <estade@chromium.org>
2021-09-28 15:18:53 +0000

    Chrome OS: launch file handling app in existing browser window
    When in tabbed mode, the file handling app should launch in an existing
    browser window when possible. This changes LaunchAppWithFiles() to pass
    the display for new windows when launching an app. Effectively, this
    will match against existing browser windows on the focused display.
    
    This also updates WebAppPublisherHelper::Launch() similarly, since it
    is closely related.
    
    Bug: 1251453
    Change-Id: I81a633051e33ca43eb62d6a05602c993b79e236e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189072
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925769}

--- chrome/browser/web_applications/app_service/web_app_publisher_helper.cc
+++ chrome/browser/web_applications/app_service/web_app_publisher_helper.cc
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/clear_site_data_utils.h"
 #include "third_party/blink/public/mojom/manifest/capture_links.mojom.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 #include "ui/display/types/display_constants.h"
 #include "url/gurl.h"
 #include "url/origin.h"
@@ -496,7 +498,9 @@ content::WebContents* WebAppPublisherHelper::Launch(
 
   apps::AppLaunchParams params = apps::CreateAppIdLaunchParamsWithEventFlags(
       web_app->app_id(), event_flags, apps::GetAppLaunchSource(launch_source),
-      window_info ? window_info->display_id : display::kInvalidDisplayId,
+      window_info
+          ? window_info->display_id
+          : display::Screen::GetScreen()->GetDisplayForNewWindows().id(),
       /*fallback_container=*/
       ConvertDisplayModeToAppLaunchContainer(display_mode));
 
@@ -512,7 +516,7 @@ content::WebContents* WebAppPublisherHelper::LaunchAppWithFiles(
   DisplayMode display_mode = registrar().GetAppEffectiveDisplayMode(app_id);
   apps::AppLaunchParams params = apps::CreateAppIdLaunchParamsWithEventFlags(
       app_id, event_flags, apps::GetAppLaunchSource(launch_source),
-      display::kDefaultDisplayId,
+      display::Screen::GetScreen()->GetDisplayForNewWindows().id(),
       /*fallback_container=*/
       ConvertDisplayModeToAppLaunchContainer(display_mode));
   if (file_paths) {
