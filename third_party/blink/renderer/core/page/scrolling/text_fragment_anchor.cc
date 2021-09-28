5e4d5b48f82eade2c21db82619b3cdd20356526d
Joey Arhar <jarhar@chromium.org>
2021-09-28 19:18:26 +0000

    Auto-expand details and hidden=until-found for text fragments
    This patch makes ScrollToTextFragment search for text in and
    automatically expand closed details elements and elements with the
    hidden=until-found HTML attribute.
    
    Bug: 1055002, 1185950
    Change-Id: Icca5c44966e6d864f83d6d60dd86adc7f2aa2663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171393
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925885}

--- third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.cc
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/shared_highlighting/core/common/shared_highlighting_features.h"
 #include "components/shared_highlighting/core/common/text_fragments_utils.h"
+#include "third_party/blink/renderer/core/display_lock/display_lock_document_state.h"
 #include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/visible_units.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/html/html_details_element.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
@@ -328,7 +330,7 @@ void TextFragmentAnchor::Trace(Visitor* visitor) const {
 }
 
 void TextFragmentAnchor::DidFindMatch(
-    const EphemeralRangeInFlatTree& range,
+    const RangeInFlatTree& range,
     const TextFragmentAnchorMetrics::Match match_metrics,
     bool is_unique) {
   if (search_finished_)
@@ -344,7 +346,8 @@ void TextFragmentAnchor::DidFindMatch(
   if (!frame_->GetDocument()
            ->Markers()
            .MarkersIntersectingRange(
-               range, DocumentMarker::MarkerTypes::TextFragment())
+               range.ToEphemeralRange(),
+               DocumentMarker::MarkerTypes::TextFragment())
            .IsEmpty()) {
     return;
   }
@@ -369,13 +372,19 @@ void TextFragmentAnchor::DidFindMatch(
 
   // Apply :target to the first match
   if (!did_find_match_) {
-    ApplyTargetToCommonAncestor(range);
+    ApplyTargetToCommonAncestor(range.ToEphemeralRange());
     needs_style_and_layout = true;
   }
 
+  // TODO(crbug.com/1252872): Only |first_node| is considered for the below
+  // ancestor expanding code, but we should be considering the entire |range|
+  // for ancestor unlocking as well.
+  Node& first_node = *range.ToEphemeralRange().Nodes().begin();
+
   // Activate any find-in-page activatable display-locks in the ancestor
   // chain.
-  if (DisplayLockUtilities::ActivateFindInPageMatchRangeIfNeeded(range)) {
+  if (DisplayLockUtilities::ActivateFindInPageMatchRangeIfNeeded(
+          range.ToEphemeralRange())) {
     // Since activating a lock dirties layout, we need to make sure it's clean
     // before computing the text rect below.
     needs_style_and_layout = true;
@@ -384,6 +393,19 @@ void TextFragmentAnchor::DidFindMatch(
     // should really yield until the next frame to give script an opportunity
     // to run.
   }
+
+  // If the active match is hidden inside a <details> element, then we should
+  // expand it so we can scroll to it.
+  needs_style_and_layout |=
+      RuntimeEnabledFeatures::AutoExpandDetailsElementEnabled() &&
+      HTMLDetailsElement::ExpandDetailsAncestors(first_node);
+
+  // If the active match is hidden inside a hidden=until-found element, then we
+  // should reveal it so we can scroll to it.
+  needs_style_and_layout |=
+      RuntimeEnabledFeatures::BeforeMatchEventEnabled(
+          first_node.GetExecutionContext()) &&
+      DisplayLockUtilities::RevealHiddenUntilFoundAncestors(first_node);
 
   if (needs_style_and_layout) {
     frame_->GetDocument()->UpdateStyleAndLayout(
@@ -396,20 +418,19 @@ void TextFragmentAnchor::DidFindMatch(
   if (first_match_needs_scroll_) {
     first_match_needs_scroll_ = false;
 
-    PhysicalRect bounding_box(ComputeTextRect(range));
+    PhysicalRect bounding_box(ComputeTextRect(range.ToEphemeralRange()));
 
     // Set the bounding box height to zero because we want to center the top of
     // the text range.
     bounding_box.SetHeight(LayoutUnit());
 
-    DCHECK(range.Nodes().begin() != range.Nodes().end());
+    DCHECK(range.ToEphemeralRange().Nodes().begin() !=
+           range.ToEphemeralRange().Nodes().end());
 
-    Node& node = *range.Nodes().begin();
-
-    DCHECK(node.GetLayoutObject());
+    DCHECK(first_node.GetLayoutObject());
 
     PhysicalRect scrolled_bounding_box =
-        node.GetLayoutObject()->ScrollRectToVisible(
+        first_node.GetLayoutObject()->ScrollRectToVisible(
             bounding_box, ScrollAlignment::CreateScrollIntoViewParams(
                               ScrollAlignment::CenterAlways(),
                               ScrollAlignment::CenterAlways(),
@@ -417,7 +438,7 @@ void TextFragmentAnchor::DidFindMatch(
     did_scroll_into_view_ = true;
 
     if (AXObjectCache* cache = frame_->GetDocument()->ExistingAXObjectCache())
-      cache->HandleScrolledToAnchor(&node);
+      cache->HandleScrolledToAnchor(&first_node);
 
     metrics_->DidScroll();
 
--- third_party/blink/renderer/core/page/scrolling/text_fragment_selector_generator.h
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_selector_generator.h
@@ -103,7 +103,7 @@ class CORE_EXPORT TextFragmentSelectorGenerator final
   };
 
   // TextFragmentFinder::Client interface
-  void DidFindMatch(const EphemeralRangeInFlatTree& match,
+  void DidFindMatch(const RangeInFlatTree& match,
                     const TextFragmentAnchorMetrics::Match match_metrics,
                     bool is_unique) override;
   void NoMatchFound() override;
