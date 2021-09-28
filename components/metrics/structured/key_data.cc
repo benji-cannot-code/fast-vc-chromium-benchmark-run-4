9df9dbc2f78f824f2ca6c9e0c4c142878eda4b68
Jonghyun Ahn <jongahn@google.com>
2021-09-28 01:40:30 +0000

    [Structured Metrics] Refactor from EventBase to Event components/metrics
    This refactor is needed for StructuredMetrics to be used in Lacros.
    
    If the feature flag is off, this should no-op as the Client will use
    the Recorder interface directly.
    
    Bug: 1238197
    Change-Id: I7b7641df606278d8524ebfcd9ae54c40794117a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180211
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925563}

--- components/metrics/structured/key_data.cc
+++ components/metrics/structured/key_data.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "components/metrics/structured/histogram_util.h"
-#include "components/metrics/structured/structured_events.h"
 #include "crypto/hmac.h"
 #include "crypto/sha2.h"
 
--- components/metrics/structured/key_data_unittest.cc
+++ components/metrics/structured/key_data_unittest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/structured/histogram_util.h"
 #include "components/metrics/structured/recorder.h"
 #include "components/metrics/structured/storage.pb.h"
-#include "components/metrics/structured/structured_events.h"
 #include "components/prefs/persistent_pref_store.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
