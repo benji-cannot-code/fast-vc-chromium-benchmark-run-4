4140946b55cfcdcc3944670f9ab677edf6a83e1e
Maks Orlovich <morlovich@chromium.org>
2021-09-28 20:42:36 +0000

    RestrictedCookieManager: remove state mismatch DwoC.
    I got enough data from the canary to get at least rough picture of
    places where this goes wrong, so remove this in advance of M96 branch
    to not waste resources.
    
    Bug: 1251118
    Change-Id: If0118e08207066ac3d36bede36685262a0de70fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3191271
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925924}

--- services/network/restricted_cookie_manager.cc
+++ services/network/restricted_cookie_manager.cc
@@ -11,9 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"  // for FALLTHROUGH;
-#include "base/debug/alias.h"
-#include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
@@ -626,31 +623,6 @@ bool RestrictedCookieManager::ValidateAccessToCookiesAt(
                         site_for_cookies_ok);
   UMA_HISTOGRAM_BOOLEAN("Net.RestrictedCookieManager.TopFrameOriginOK",
                         top_frame_origin_ok);
-
-  if (!top_frame_origin_ok || !site_for_cookies_ok) {
-    base::debug::Alias(&top_frame_origin_ok);
-    base::debug::Alias(&site_for_cookies_ok);
-    static bool reported = false;
-    if (!reported) {
-      reported = true;
-      SCOPED_CRASH_KEY_STRING256("RCM", "rcm-site_for_cookies",
-                                 BoundSiteForCookies().ToDebugString());
-      SCOPED_CRASH_KEY_STRING256("RCM", "render-site_for_cookies",
-                                 site_for_cookies.ToDebugString());
-
-      SCOPED_CRASH_KEY_STRING256("RCM", "rcm-top_frame_origin",
-                                 BoundTopFrameOrigin().GetDebugString());
-      SCOPED_CRASH_KEY_STRING256("RCM", "render-top_frame_origin",
-                                 top_frame_origin.GetDebugString());
-
-      SCOPED_CRASH_KEY_STRING256("RCM", "rcm-origin", origin_.GetDebugString());
-      // Only origin here, since url is probably way too sensitive.
-      SCOPED_CRASH_KEY_STRING256("RCM", "render-origin",
-                                 url::Origin::Create(url).GetDebugString());
-      base::debug::DumpWithoutCrashing();
-    }
-    return false;
-  }
 
   // Don't allow setting cookies on other domains. See crbug.com/996786.
   if (cookie_being_set && !cookie_being_set->IsDomainMatch(url.host())) {
