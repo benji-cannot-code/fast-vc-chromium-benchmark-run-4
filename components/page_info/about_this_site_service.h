d1d86a09fe1f9ac2e22a17bb5705a0dd557f477c
David Baron <dbaron@chromium.org>
2021-09-28 16:39:31 +0000

    Revert "[reland] [about this site] Add proto for "About this site" info"
    This reverts commit 6d1bf03335e43af249d02cc866bc0c6bc13ec17e.
    
    Reason for revert: build failure in https://ci.chromium.org/p/chromium/builders/ci/android-archive-dbg/9694
    
    Original change's description:
    > [reland] [about this site] Add proto for "About this site" info
    >
    > Create a proto and replace the test strings with proto objects in the
    > service and desktop UI.
    >
    > Bug: 1250653
    > Change-Id: If3489f9509e52f0875f3aef8065ff6c3c89256c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172842
    > Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/main@{#925285}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189716
    > Cr-Commit-Position: refs/heads/main@{#925791}
    
    Bug: 1250653
    Change-Id: Iec4a4685e5bad4a127b70053bcc67a93d0e7e305
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3190930
    Auto-Submit: David Baron <dbaron@chromium.org>
    Owners-Override: David Baron <dbaron@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#925802}

--- components/page_info/about_this_site_service.h
+++ components/page_info/about_this_site_service.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/core/optimization_guide_decision.h"
 #include "components/optimization_guide/core/optimization_metadata.h"
-#include "components/page_info/proto/about_this_site_metadata.pb.h"
 
 class GURL;
 
@@ -40,8 +39,7 @@ class AboutThisSiteService : public KeyedService {
   AboutThisSiteService& operator=(const AboutThisSiteService&) = delete;
 
   // Returns "About this site" information for the website with |url|.
-  absl::optional<page_info::proto::SiteInfo> GetAboutThisSiteInfo(
-      const GURL& url) const;
+  std::u16string GetAboutThisSiteDescription(const GURL& url) const;
 
  private:
   std::unique_ptr<Client> client_;
