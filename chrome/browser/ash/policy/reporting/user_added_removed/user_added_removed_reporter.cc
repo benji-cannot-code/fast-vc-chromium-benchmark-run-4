b0ea732f48ff8dbbae9f2415bc18d9b686b64494
Zach Trudo <zatrudo@google.com>
2021-09-28 18:46:24 +0000

    Create SessionAffiliatedUser proto
    Currently SessionAffiliatedUser is defined in two locations,
    login_logout_record.proto and user_added_removed_record.proto. This CL
    creates one place for both protos to inherit from.
    
    Change-Id: I12603ba698b6b2b1ac2d9878eef4b7f26643b1ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3166073
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/main@{#925864}

--- chrome/browser/ash/policy/reporting/user_added_removed/user_added_removed_reporter.cc
+++ chrome/browser/ash/policy/reporting/user_added_removed/user_added_removed_reporter.cc
@@ -55,7 +55,7 @@ void UserAddedRemovedReporter::OnLogin(Profile* profile) {
   UserAddedRemovedRecord record;
   record.mutable_user_added_event();
   if (helper_->ShouldReportUser(email)) {
-    record.mutable_user()->set_email(email);
+    record.mutable_affiliated_user()->set_user_email(email);
   }
   record.set_event_timestamp_sec(base::Time::Now().ToTimeT());
 
@@ -96,7 +96,7 @@ void UserAddedRemovedReporter::OnUserRemoved(
   UserAddedRemovedRecord record;
   record.mutable_user_removed_event()->set_reason(UserRemovalReason(reason));
   if (is_affiliated_user) {
-    record.mutable_user()->set_email(account_id.GetUserEmail());
+    record.mutable_affiliated_user()->set_user_email(account_id.GetUserEmail());
   }
   record.set_event_timestamp_sec(base::Time::Now().ToTimeT());
 
@@ -113,7 +113,7 @@ void UserAddedRemovedReporter::ProcessRemoveUserCache() {
     record.mutable_user_removed_event()->set_reason(
         UserRemovalReason(user.second));
     if (user.first != "") {
-      record.mutable_user()->set_email(user.first);
+      record.mutable_affiliated_user()->set_user_email(user.first);
     }
 
     helper_->ReportEvent(&record, ::reporting::Priority::IMMEDIATE);
