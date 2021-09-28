64d4b5423d13dc6572c08da6e363e4a54de13dee
Alexander Surkov <asurkov@igalia.com>
2021-09-28 16:57:56 +0000

    ax_mac_merge: merge accessibilityPlaceholderValue implementations
    Change-Id: I189f1c4f0baeefddef5a6ff51a1ca9699a8c715c
    Bug: 1237915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3182163
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#925813}

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -865,7 +865,6 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
       {NSAccessibilityOrientationAttribute, @"orientation"},
       {NSAccessibilityOwnsAttribute, @"owns"},
       {NSAccessibilityParentAttribute, @"parent"},
-      {NSAccessibilityPlaceholderValueAttribute, @"placeholderValue"},
       {NSAccessibilityPositionAttribute, @"position"},
       {NSAccessibilityRequiredAttributeChrome, @"required"},
       {NSAccessibilityRoleAttribute, @"role"},
@@ -1693,23 +1692,6 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
   if (![self instanceActive])
     return nil;
   return @(GetState(_owner, ax::mojom::State::kMultiselectable));
-}
-
-- (NSString*)AXPlaceholderValue {
-  return [self placeholderValue];
-}
-
-- (NSString*)placeholderValue {
-  if (![self instanceActive])
-    return nil;
-  ax::mojom::NameFrom nameFrom = static_cast<ax::mojom::NameFrom>(
-      _owner->GetIntAttribute(ax::mojom::IntAttribute::kNameFrom));
-  if (nameFrom == ax::mojom::NameFrom::kPlaceholder) {
-    return base::SysUTF8ToNSString(_owner->GetName());
-  }
-
-  return NSStringForStringAttribute(_owner,
-                                    ax::mojom::StringAttribute::kPlaceholder);
 }
 
 - (NSString*)language {
@@ -2826,7 +2808,7 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
   if (selector)
     return [self performSelector:selector];
 
-  return nil;
+  return [super accessibilityAttributeValue:attribute];
 }
 
 - (id)AXStringForRange:(id)parameter {
--- ui/accessibility/platform/ax_platform_node_cocoa.mm
+++ ui/accessibility/platform/ax_platform_node_cocoa.mm
@@ -834,7 +834,7 @@ bool IsAXSetter(SEL selector) {
 }
 
 - (NSString*)AXPlaceholderValue {
-  return [self getStringAttribute:ax::mojom::StringAttribute::kPlaceholder];
+  return [self accessibilityPlaceholderValue];
 }
 
 - (NSString*)AXMenuItemMarkChar {
@@ -1129,10 +1129,13 @@ bool IsAXSetter(SEL selector) {
 }
 
 - (NSString*)accessibilityPlaceholderValue {
-  if (!_node)
+  if (![self instanceActive])
     return nil;
 
-  return [self AXPlaceholderValue];
+  if (_node->GetNameFrom() == ax::mojom::NameFrom::kPlaceholder)
+    return base::SysUTF8ToNSString(_node->GetName());
+
+  return [self getStringAttribute:ax::mojom::StringAttribute::kPlaceholder];
 }
 
 - (NSString*)accessibilitySelectedText {
