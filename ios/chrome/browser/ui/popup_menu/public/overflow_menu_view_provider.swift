0400e90ac1079a948229e6b39a1c8a758326ee38
Robbie Gibson <rkgibson@google.com>
2021-09-28 14:42:45 +0000

    [iOS] Add initial Overflow Menu view
    For the view, it is in SwiftUI, currently just a temporary red square.
    The provider is necessary because SwiftUI types are not exposed to
    Objective C, so we need a Swift wrapper to expose a plain
    UIViewController. Similarly, free Swift functions are also not exposed
    to Objective C, so it needs to be wrapped in a class.
    
    Bug: 1253367
    Change-Id: Id0ccff3b7b35394e2711ab30963cb6c90380cfda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3186852
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#925755}

---
+++ ios/chrome/browser/ui/popup_menu/public/overflow_menu_view_provider.swift
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import UIKit
+import SwiftUI
+
+// A provider to provide the SwiftUI OverflowMenuView to Objective C. This is
+// necessary because Objective C can't see SwiftUI types.
+@objcMembers public class OverflowMenuViewProvider: NSObject {
+  public static func makeViewController() -> UIViewController {
+    return UIHostingController(rootView: OverflowMenuView())
+  }
+}
