b7dd692da24ab3ab1d2e034e57d2715744351794
Justin Ouellette <justinoue@google.com>
2021-09-28 19:35:30 +0000

    Fill TPM features in device status reports.
    This adds additional information about TPM devices from tpm_manager
    to the device_management_backend proto and populates it in
    device_status_collector.
    
    Bug: b:198311620
    Change-Id: I71192a5dbd03c1146606ba259d6d6d6e969c9831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172271
    Commit-Queue: Justin Ouellette <justinoue@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925892}

--- chrome/browser/ash/policy/status_collector/tpm_status_combiner.h
+++ chrome/browser/ash/policy/status_collector/tpm_status_combiner.h
@@ -49,6 +49,10 @@ class TpmStatusCombiner : public base::RefCounted<TpmStatusCombiner> {
   // `AttestationClient::GetDictionaryAttackInfoReply()`.
   void OnGetDictionaryAttackInfo(
       const ::tpm_manager::GetDictionaryAttackInfoReply& reply);
+  // Designed to be the callback of
+  // `TpmManagerClient::OnGetSupportedFeatures()`.
+  void OnGetSupportedFeatures(
+      const ::tpm_manager::GetSupportedFeaturesReply& reply);
 
  private:
   // `RefCounted` subclass requires the destructor to be non-public.
@@ -69,6 +73,7 @@ class TpmStatusCombiner : public base::RefCounted<TpmStatusCombiner> {
   bool has_tpm_status_ = false;
   bool has_enrollment_status_ = false;
   bool has_dictionary_attack_info_ = false;
+  bool has_supported_features_ = false;
 };
 
 }  // namespace policy
