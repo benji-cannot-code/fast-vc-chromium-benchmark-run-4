f4ff9fdb15582c578e49925e41b08a6db3868a38
Lei Zhang <thestig@chromium.org>
2021-09-28 20:30:58 +0000

    [unseasoned-pdf] Handle IME input.
    Port applicable PepperWebPluginImpl and PepperPluginInstanceImpl logic
    to PdfViewWebPlugin. This provides the same basic level of IME support
    as the PPAPI-based PDF Viewer.
    
    Bug: 1230168
    Change-Id: I9611c48eb61e527421ee8cf9d0c073de48acabab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3184085
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925914}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_op.h"
+#include "base/i18n/char_iterator.h"
 #include "base/i18n/string_search.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
@@ -70,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/cursor/cursor.h"
 #include "ui/base/cursor/mojom/cursor_type.mojom-shared.h"
 #include "ui/display/screen_info.h"
+#include "ui/events/base_event_utils.h"
 #include "ui/events/blink/blink_event_util.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/gfx/geometry/point.h"
@@ -596,8 +598,38 @@ void PdfViewWebPlugin::RotateView(blink::WebPlugin::RotationType type) {
   }
 }
 
+bool PdfViewWebPlugin::ShouldDispatchImeEventsToPlugin() {
+  return true;
+}
+
 blink::WebTextInputType PdfViewWebPlugin::GetPluginTextInputType() {
   return text_input_type_;
+}
+
+gfx::Rect PdfViewWebPlugin::GetPluginCaretBounds() {
+  return caret_rect_;
+}
+
+void PdfViewWebPlugin::ImeSetCompositionForPlugin(
+    const blink::WebString& text,
+    const std::vector<ui::ImeTextSpan>& /*ime_text_spans*/,
+    const gfx::Range& /*replacement_range*/,
+    int /*selection_start*/,
+    int /*selection_end*/) {
+  composition_text_ = text;
+}
+
+void PdfViewWebPlugin::ImeCommitTextForPlugin(
+    const blink::WebString& text,
+    const std::vector<ui::ImeTextSpan>& /*ime_text_spans*/,
+    const gfx::Range& /*replacement_range*/,
+    int /*relative_cursor_pos*/) {
+  HandleImeCommit(text);
+}
+
+void PdfViewWebPlugin::ImeFinishComposingTextForPlugin(
+    bool /*keep_selection*/) {
+  HandleImeCommit(composition_text_);
 }
 
 void PdfViewWebPlugin::UpdateCursor(ui::mojom::CursorType new_cursor_type) {
@@ -611,6 +643,11 @@ void PdfViewWebPlugin::NotifySelectedFindResultChanged(int current_find_index) {
   DCHECK_GE(current_find_index, -1);
   container_wrapper_->ReportFindInPageSelection(find_identifier_,
                                                 current_find_index + 1);
+}
+
+void PdfViewWebPlugin::CaretChanged(const gfx::Rect& caret_rect) {
+  caret_rect_ = gfx::ScaleToEnclosingRectSafe(
+      caret_rect + available_area().OffsetFromOrigin(), device_to_css_scale_);
 }
 
 void PdfViewWebPlugin::Alert(const std::string& message) {
@@ -956,6 +993,32 @@ bool PdfViewWebPlugin::Redo() {
 
   engine()->Redo();
   return true;
+}
+
+void PdfViewWebPlugin::HandleImeCommit(const blink::WebString& text) {
+  if (text.IsEmpty())
+    return;
+
+  std::u16string text16 = text.Utf16();
+  composition_text_.Reset();
+
+  size_t i = 0;
+  for (base::i18n::UTF16CharIterator iterator(text16); iterator.Advance();) {
+    blink::WebKeyboardEvent char_event(blink::WebInputEvent::Type::kChar,
+                                       blink::WebInputEvent::kNoModifiers,
+                                       ui::EventTimeForNow());
+    char_event.windows_key_code = text16[i];
+    char_event.native_key_code = text16[i];
+
+    for (const size_t char_start = i; i < iterator.array_pos(); ++i) {
+      char_event.text[i - char_start] = text16[i];
+      char_event.unmodified_text[i - char_start] = text16[i];
+    }
+
+    blink::WebCoalescedInputEvent input_event(char_event, ui::LatencyInfo());
+    ui::Cursor dummy_cursor_info;
+    HandleInputEvent(input_event, &dummy_cursor_info);
+  }
 }
 
 void PdfViewWebPlugin::OnInvokePrintDialog(int32_t /*result*/) {
