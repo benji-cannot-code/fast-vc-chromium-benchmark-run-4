312a153764b2e8f893f477a80959534136bb12db
Jérôme Lebel <jlebel@chromium.org>
2021-09-28 12:02:22 +0000

    [iOS] Remove dead code
    Remove:
    -[ChromeIdentityInteractionManager
        addAccountWithPresentingViewController:completion:]
    -[FakeAddAccountViewController
        addAccountWithPresentingViewController:completion:]
    
    Replaced by:
    -[ChromeIdentityInteractionManager
        addAccountWithPresentingViewController:userEmail:completion:]
    
    * crrev.com/i/4097710 Remove unused method in downstream
    =>crrev.com/c/3136373 Remove unused method in upstream
    * crrev.com/i/4098568 Cleanup
    
    Bug: 1245648
    Change-Id: Ifdc8b1a6154293e4cab65c343ed8e6e062740eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3136373
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925709}

--- ios/public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
+++ ios/public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
@@ -107,6 +107,7 @@ static ChromeIdentity* _identity = nil;
 }
 
 - (void)addAccountWithPresentingViewController:(UIViewController*)viewController
+                                     userEmail:(NSString*)userEmail
                                     completion:
                                         (SigninCompletionCallback)completion {
   self.completionCallback = completion;
@@ -118,14 +119,6 @@ static ChromeIdentity* _identity = nil;
                              completion:^() {
                                weakSelf.viewControllerPresented = YES;
                              }];
-}
-
-- (void)addAccountWithPresentingViewController:(UIViewController*)viewController
-                                     userEmail:(NSString*)userEmail
-                                    completion:
-                                        (SigninCompletionCallback)completion {
-  [self addAccountWithPresentingViewController:viewController
-                                    completion:completion];
 }
 
 - (void)cancelAddAccountAnimated:(BOOL)animated
