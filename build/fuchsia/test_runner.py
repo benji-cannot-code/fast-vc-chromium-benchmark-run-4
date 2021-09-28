40adb1628f4e38221271f2a36fee73bc5c64f4e5
Zijie He <zijiehe@google.com>
2021-09-28 22:17:58 +0000

    [fuchsia] Add gtest_also_run_disabled_tests in fuchsia/test_runner.py
    Bug: 1253618
    Change-Id: I45c7bd6e55f626927c1de5d8cfd7faa9386a4eac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3188650
    Commit-Queue: Zijie He <zijiehe@chromium.org>
    Reviewed-by: Rohan Pavone <rohpavone@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925973}

--- build/fuchsia/test_runner.py
+++ build/fuchsia/test_runner.py
@@ -111,6 +111,10 @@ def AddTestExecutionArgs(arg_parser):
   test_args.add_argument('--child-arg',
                          action='append',
                          help='Arguments for the test process.')
+  test_args.add_argument('--gtest_also_run_disabled_tests',
+                         default=False,
+                         action='store_true',
+                         help='Run tests prefixed with DISABLED_')
   test_args.add_argument('child_args',
                          nargs='*',
                          help='Arguments for the test process.')
@@ -181,6 +185,8 @@ def main():
   if args.isolated_script_test_perf_output:
     child_args.append('--isolated-script-test-perf-output=' +
                       TEST_PERF_RESULT_PATH)
+  if args.gtest_also_run_disabled_tests:
+    child_args.append('--gtest_also_run_disabled_tests')
 
   if args.child_arg:
     child_args.extend(args.child_arg)
