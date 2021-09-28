e426be7eabc8658081fde32b88bf6715fd8912bc
dpapad <dpapad@chromium.org>
2021-09-28 19:35:51 +0000

    CrOS Settings: Use Closure externs for controls shared with Browser Settings.
    This is in preparation of migrating such controls to TypeScript, and
    therefore directly referring to such files from js_library() targets
    will not be possible anymore. Using Closure externs allows to keep
    reusing the code instead of forking c/b/r/settings/controls for CrOS
    Settings.
    
    Bug: 1234307
    Change-Id: I87fa7c8a4fab80d84537af6be6f069d1cc2f13e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3191113
    Commit-Queue: dpapad <dpapad@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925893}

--- chrome/browser/resources/settings/chromeos/date_time_page/timezone_selector.js
+++ chrome/browser/resources/settings/chromeos/date_time_page/timezone_selector.js
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import '../../settings_shared_css.js';
+import '../../controls/settings_dropdown_menu.js';
 
 import {addWebUIListener, removeWebUIListener, sendWithPromise, WebUIListener} from '//resources/js/cr.m.js';
 import {afterNextRender, flush, html, Polymer, TemplateInstanceBase, Templatizer} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {DropdownMenuOptionList} from '../../controls/settings_dropdown_menu.js';
 import {loadTimeData} from '../../i18n_setup.js';
 import {PrefsBehavior} from '../../prefs/prefs_behavior.js';
 import {CrSettingsPrefs} from '../../prefs/prefs_types.js';
