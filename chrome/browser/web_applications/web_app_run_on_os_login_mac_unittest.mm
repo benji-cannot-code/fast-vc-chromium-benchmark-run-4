90f7ec00eaed479e650c0255a17e0eb5ec1e8013
Daniel Murphy <dmurph@chromium.org>
2021-09-28 22:52:00 +0000

    Reland "[dPWA Testing] Improve shortcut override to be scoped"
    This is a reland of 85b5ae53461beb0479fcc67832d02356c1f62b48
    
    The fix was to remove the folder creation code from DCHECKs, as these
    are removed in release builds and thus the folders were never created.
    
    Original change's description:
    > [dPWA Testing] Improve shortcut override to be scoped
    >
    > This change removes a lot of the cruft around customizing the shortcut
    > directories during testing for web apps, and also makes the
    > customization scoped, forcing the developer to uninstall any installed
    > apps during the customization, ensuring os integration cleanup on the
    > bots.
    >
    > Bug: 1232635
    > Change-Id: I1328c93219e42de6bc8d137ba02b432ff3b770a5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3178722
    > Reviewed-by: Phillis Tang <phillis@chromium.org>
    > Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#925860}
    
    Bug: 1232635
    Change-Id: I47ec065635f46fda02f690208947228ff9d0962e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192310
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925990}

--- chrome/browser/web_applications/web_app_run_on_os_login_mac_unittest.mm
+++ chrome/browser/web_applications/web_app_run_on_os_login_mac_unittest.mm
@@ -79,9 +79,10 @@ class WebAppRunOnOsLoginMacTest : public WebAppTest {
     WebAppTest::SetUp();
     base::mac::SetBaseBundleID(kFakeChromeBundleId);
 
-    EXPECT_TRUE(temp_destination_dir_.CreateUniqueTempDir());
+    shortcut_override_ = OverrideShortcutsForTesting();
+    destination_dir_ = shortcut_override_->chrome_apps_folder.GetPath();
+
     EXPECT_TRUE(temp_user_data_dir_.CreateUniqueTempDir());
-    destination_dir_ = temp_destination_dir_.GetPath();
     user_data_dir_ = temp_user_data_dir_.GetPath();
     // Recreate the directory structure as it would be created for the
     // ShortcutInfo created in the above GetShortcutInfo.
@@ -98,10 +99,6 @@ class WebAppRunOnOsLoginMacTest : public WebAppTest {
     user_data_dir_ = base::MakeAbsoluteFilePath(user_data_dir_);
     app_data_dir_ = base::MakeAbsoluteFilePath(app_data_dir_);
 
-    ShortcutOverrideForTesting shortcut_override;
-    shortcut_override.chrome_apps_folder = destination_dir_;
-    web_app::SetShortcutOverrideForTesting(shortcut_override);
-
     info_ = GetShortcutInfo();
     base::FilePath shim_base_name =
         base::FilePath(base::UTF16ToUTF8(info_->title) + ".app");
@@ -113,7 +110,12 @@ class WebAppRunOnOsLoginMacTest : public WebAppTest {
 
   void TearDown() override {
     WebAppAutoLoginUtil::SetInstanceForTesting(nullptr);
-    web_app::SetShortcutOverrideForTesting(absl::nullopt);
+    // To prevent OS hooks from sticking around on bots, destroying the shortcut
+    // override DCHECK fails if the directories are not empty. To bypass this in
+    // this unittest, we manually delete it.
+    // TODO: If these unittests leave OS hook artifacts on bots, undo that here.
+    EXPECT_TRUE(shortcut_override_->chrome_apps_folder.Delete());
+    shortcut_override_.reset();
     WebAppShortcutCreator::ResetHaveLocalizedAppDirNameForTesting();
     WebAppTest::TearDown();
   }
@@ -131,7 +133,6 @@ class WebAppRunOnOsLoginMacTest : public WebAppTest {
   }
 
  protected:
-  base::ScopedTempDir temp_destination_dir_;
   base::ScopedTempDir temp_user_data_dir_;
   base::FilePath app_data_dir_;
   base::FilePath destination_dir_;
@@ -140,6 +141,7 @@ class WebAppRunOnOsLoginMacTest : public WebAppTest {
   std::unique_ptr<WebAppAutoLoginUtilMock> auto_login_util_mock_;
   std::unique_ptr<ShortcutInfo> info_;
   base::FilePath shim_path_;
+  std::unique_ptr<ScopedShortcutOverrideForTesting> shortcut_override_;
 };
 
 TEST_F(WebAppRunOnOsLoginMacTest, Register) {
