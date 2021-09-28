58bef03922ffb1ed2be7c947805854f4d9a619c3
Christian Dullweber <dullweber@chromium.org>
2021-09-28 11:14:23 +0000

    PageInfo: Hide sound permission if no sound was played
    Currently the sound permission is always shown in the permission
    subpage. On desktop it only shows up if sound was played and we already
    don't include it in the permission string on the pageinfo main page on
    Android. This hides the permission on the subpage if it is not shown
    on the main page.
    
    Bug: 1168527
    Change-Id: If93b184cde498d4f26bd66442b64b9ec77b42c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3172792
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925700}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoPermissionsController.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoPermissionsController.java
@@ -35,6 +35,7 @@ public class PageInfoPermissionsController
         implements PageInfoSubpageController, SingleWebsiteSettings.Observer {
     /**  Parameters to represent a single permission. */
     public static class PermissionObject {
+        public @ContentSettingsType int type;
         public CharSequence name;
         public CharSequence nameMidSentence;
         public boolean allowed;
@@ -46,6 +47,7 @@ public class PageInfoPermissionsController
     private final PageInfoControllerDelegate mDelegate;
     private final String mTitle;
     private final String mPageUrl;
+    private boolean mHasSoundPermission;
     private boolean mDataIsStale;
     private SingleWebsiteSettings mSubPage;
     @ContentSettingsType
@@ -90,6 +92,8 @@ public class PageInfoPermissionsController
         if (fragmentManager.isStateSaved()) return null;
 
         Bundle fragmentArgs = SingleWebsiteSettings.createFragmentArgsForSite(mPageUrl);
+        fragmentArgs.putBoolean(SingleWebsiteSettings.EXTRA_SHOW_SOUND, mHasSoundPermission);
+
         mSubPage = (SingleWebsiteSettings) Fragment.instantiate(
                 mRowView.getContext(), SingleWebsiteSettings.class.getName(), fragmentArgs);
         mSubPage.setSiteSettingsDelegate(mDelegate.getSiteSettingsDelegate());
@@ -126,6 +130,14 @@ public class PageInfoPermissionsController
             rowParams.rowTint = mHighlightColor;
         }
         mRowView.setParams(rowParams);
+
+        mHasSoundPermission = false;
+        for (PermissionObject permission : permissions) {
+            if (permission.type == ContentSettingsType.SOUND) {
+                mHasSoundPermission = true;
+                break;
+            }
+        }
     }
 
     /**
@@ -208,7 +220,7 @@ public class PageInfoPermissionsController
             mMainController.refreshPermissions();
         }
         mDataIsStale = false;
-    };
+    }
 
     // SingleWebsiteSettings.Observer methods
 
