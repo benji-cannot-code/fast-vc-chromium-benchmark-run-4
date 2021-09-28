19c7e6fa582d0a39285c8602c549cadf14d03421
Siyu An <siyua@chromium.org>
2021-09-28 21:33:14 +0000

    [Downstream Refactor] Clean up CreditCardAccessManager
    Bug: 1249665
    Change-Id: I712389877c1b775426d7fd7c9648a2262748f192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180153
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925951}

--- components/autofill/core/browser/payments/payments_client.cc
+++ components/autofill/core/browser/payments/payments_client.cc
@@ -1160,6 +1160,11 @@ PaymentsClient::UnmaskDetails& PaymentsClient::UnmaskDetails::operator=(
 PaymentsClient::UnmaskRequestDetails::UnmaskRequestDetails() = default;
 PaymentsClient::UnmaskRequestDetails::UnmaskRequestDetails(
     const UnmaskRequestDetails& other) {
+  *this = other;
+}
+PaymentsClient::UnmaskRequestDetails&
+PaymentsClient::UnmaskRequestDetails::operator=(
+    const PaymentsClient::UnmaskRequestDetails& other) {
   billing_customer_number = other.billing_customer_number;
   reason = other.reason;
   card = other.card;
@@ -1173,6 +1178,7 @@ PaymentsClient::UnmaskRequestDetails::UnmaskRequestDetails(
   context_token = other.context_token;
   otp = other.otp;
   last_committed_url_origin = other.last_committed_url_origin;
+  return *this;
 }
 PaymentsClient::UnmaskRequestDetails::~UnmaskRequestDetails() = default;
 
