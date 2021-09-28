376e2e67ffc3b32348b46a978bb905341cdab3b5
Matthias Körber <koerber@google.com>
2021-09-28 18:39:15 +0000

    [Autofill] Generalize update prompt metrics
    Currently the histograms
    "Autofill.ProfileImport.UpdateProfileAffectedType" and
    "Autofill.ProfileImport.UpdateProfileNumberOfAffectedFields" are only
    recorded in the case the prompt is accepted by the user without
    additional edits.
    
    To allow for a better understanding of the users' behavior,
    these histogram are recorded for all of the possible user decisions.
    For this, a suffix indicating the users' decision is added to the
    histogram name. For convenience, additional ".Any" histograms are
    recorded for a decision independent view on the signal.
    
    Bug: 1253798
    Change-Id: Iba37258d23e585b738a2be1105d39a04adc6e307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3190250
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925862}

--- components/autofill/core/browser/address_profile_save_manager_unittest.cc
+++ components/autofill/core/browser/address_profile_save_manager_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/address_profile_save_manager.h"
 
+#include "base/strings/strcat.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
@@ -329,6 +330,23 @@ void AddressProfileSaveManagerTest::TestImportScenario(
            test_scenario.expected_affeceted_types_in_merge_for_metrics) {
         histogram_tester.ExpectBucketCount(kProfileUpdateAffectedTypesHistogram,
                                            changed_type, 1);
+        std::string changed_histogram_suffix;
+        switch (test_scenario.user_decision) {
+          case UserDecision::kAccepted:
+            changed_histogram_suffix = ".Accepted";
+            break;
+
+          case UserDecision::kDeclined:
+            changed_histogram_suffix = ".Declined";
+            break;
+
+          default:
+            NOTREACHED() << "Decision not covered by test logic.";
+        }
+        histogram_tester.ExpectBucketCount(
+            base::StrCat({kProfileUpdateAffectedTypesHistogram,
+                          changed_histogram_suffix}),
+            changed_type, 1);
       }
 
       histogram_tester.ExpectUniqueSample(
