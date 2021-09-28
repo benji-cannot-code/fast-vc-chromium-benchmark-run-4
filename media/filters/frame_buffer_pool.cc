7c795e325ee999817cc2afb80a44636649709dcb
Andrey Logvinov <alogvinov@yandex-team.ru>
2021-09-28 21:55:28 +0000

    Add unique suffix to memory dumps created by FrameBufferPool
    Multiple instances of FrameBufferPool may record memory dumps but they
    use the same absolute name which triggers a DCHECK in
    ProcessMemoryDump::AddAllocatorDumpInternal. This CL adds a unique
    suffix to each dump, similar to other implementations of
    MemoryDumpProvider.
    
    Bug: 1253874
    Change-Id: Ifba63b8d14f2ec1c6c862c148288a19b414e378b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3190478
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Andrey Logvinov <alogvinov@yandex-team.ru>
    Cr-Commit-Position: refs/heads/main@{#925960}

--- media/filters/frame_buffer_pool.cc
+++ media/filters/frame_buffer_pool.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/free_deleter.h"
 #include "base/process/memory.h"
 #include "base/sequenced_task_runner.h"
+#include "base/strings/stringprintf.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/trace_event/memory_allocator_dump.h"
 #include "base/trace_event/memory_dump_manager.h"
@@ -142,9 +143,13 @@ bool FrameBufferPool::OnMemoryDump(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   base::trace_event::MemoryAllocatorDump* memory_dump =
-      pmd->CreateAllocatorDump("media/frame_buffers/memory_pool");
+      pmd->CreateAllocatorDump(
+          base::StringPrintf("media/frame_buffers/memory_pool/0x%" PRIXPTR,
+                             reinterpret_cast<uintptr_t>(this)));
   base::trace_event::MemoryAllocatorDump* used_memory_dump =
-      pmd->CreateAllocatorDump("media/frame_buffers/memory_pool/used");
+      pmd->CreateAllocatorDump(
+          base::StringPrintf("media/frame_buffers/memory_pool/used/0x%" PRIXPTR,
+                             reinterpret_cast<uintptr_t>(this)));
 
   pmd->AddSuballocation(memory_dump->guid(),
                         base::trace_event::MemoryDumpManager::GetInstance()
