1c5334f0dfb0b2a4e0a4a2f9f8512d9274856126
Alexander Bolodurin <alexbn@chromium.org>
2021-09-28 02:48:25 +0000

    [lacros shellf] Getters by instance ID in BrowserAppInstanceRegistry
    - Add getters by ID to BrowserAppInstanceRegistry and
      BrowserAppInstanceTracker.
    - Move some methods out of line for consistency.
    - Fix up invalid comments and misspelled function names.
    - Remove unused headers.
    
    Bug: 1203992
    Change-Id: Icc1d67d3c928522b8348d22b61c475a19957ffec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189351
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925591}

--- chrome/browser/apps/app_service/browser_app_instance_tracker.cc
+++ chrome/browser/apps/app_service/browser_app_instance_tracker.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
-#include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "ui/aura/window.h"
 
@@ -203,6 +202,15 @@ const BrowserAppInstance* BrowserAppInstanceTracker::GetAppInstanceById(
 const BrowserWindowInstance* BrowserAppInstanceTracker::GetWindowInstance(
     Browser* browser) const {
   return GetInstance(window_instances_, browser);
+}
+
+const BrowserWindowInstance*
+BrowserAppInstanceTracker::GetBrowserWindowInstanceById(
+    base::UnguessableToken id) const {
+  return FindInstanceIf(window_instances_,
+                        [&id](const BrowserWindowInstance& instance) {
+                          return instance.id == id;
+                        });
 }
 
 void BrowserAppInstanceTracker::ActivateTabInstance(base::UnguessableToken id) {
