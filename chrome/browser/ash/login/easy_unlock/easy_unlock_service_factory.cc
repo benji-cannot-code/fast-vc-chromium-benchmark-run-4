9fecfab30741cb970fc1a65e5ff1255d91d8534b
Henrique Ferreiro <hferreiro@igalia.com>
2021-09-28 10:14:14 +0000

    Remove chromeos suffix from files in c/b/ash/login/easy_unlock/
    This CL removes the chromeos suffix from some files to match its class
    names.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: Ie518eac6a36eca68afab4a9c0bf13b367c4358da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3185760
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#925681}

--- chrome/browser/ash/login/easy_unlock/easy_unlock_service_factory.cc
+++ chrome/browser/ash/login/easy_unlock/easy_unlock_service_factory.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/device_sync/device_sync_client_factory.h"
 #include "chrome/browser/ash/login/easy_unlock/easy_unlock_service.h"
 #include "chrome/browser/ash/login/easy_unlock/easy_unlock_service_regular.h"
-#include "chrome/browser/ash/login/easy_unlock/easy_unlock_service_signin_chromeos.h"
+#include "chrome/browser/ash/login/easy_unlock/easy_unlock_service_signin.h"
 #include "chrome/browser/ash/login/easy_unlock/easy_unlock_tpm_key_manager_factory.h"
 #include "chrome/browser/ash/multidevice_setup/multidevice_setup_client_factory.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
