d270e2b2c38b909e9d6816519a3944e0a56a7ffd
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-09-29 17:09:50 +0000

    Import wpt@68709a211b91f91b338614d1e41456f77a0b6ac4
    Using wpt-import in Chromium c25492646c2c7bb2e4667018a11ea617c8a54401.
    With Chromium commits locally applied on WPT:
    e2dc11a997 "Fix flaky coop reporting tests."
    c482ebfa21 "Reland "[CSP] Fix wasm-eval check from workers""
    04601bb016 "Added WPT tests for wasm-unsafe-eval"
    8ec9246cbf "Add Datagrams WPTs for WebTransport"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    meade@chromium.org:
      external/wpt/web-animations
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Change-Id: I8e988779ea83d86d01efd7af1197a96adaaa9329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192496
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#926337}

--- third_party/blink/web_tests/external/wpt/web-animations/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/web-animations/idlharness.window.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 idl_test(
-  ['web-animations'],
+  ['web-animations', 'web-animations-2'],
   ['dom', 'html'],
   idl_array => {
     idl_array.add_objects({
