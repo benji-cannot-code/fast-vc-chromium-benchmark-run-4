bbe3bdb63dfbf04851c80b40f0b662563bde468c
John Z Wu <jzw@chromium.org>
2021-09-29 19:35:21 +0000

    Refactor keychain code in password_util into ios/components
    This is so it can be shared with //ios/web_view, as //ios/web_view
    cannot depend on anything in //ios/chrome.
    
    Bug: 1253663
    Change-Id: I68c7ae98b30124cd8468dc0ea2a393cf89d9a95c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3190928
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926427}

---
+++ ios/components/credential_provider_extension/password_util.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_COMPONENTS_CREDENTIAL_PROVIDER_EXTENSION_PASSWORD_UTIL_H_
+#define IOS_COMPONENTS_CREDENTIAL_PROVIDER_EXTENSION_PASSWORD_UTIL_H_
+
+#import <Foundation/Foundation.h>
+
+namespace credential_provider_extension {
+
+// Queries Keychain Services for the passed identifier password.
+// Returns nil if |identifier| is nil or if no password found for |identifier|.
+NSString* PasswordWithKeychainIdentifier(NSString* identifier);
+
+// Stores |password| in Keychain Services using |identifier| as its identifier
+// for later query. Returns |YES| if saving was successful and |NO| otherwise.
+BOOL StorePasswordInKeychain(NSString* password, NSString* identifier);
+
+}  // namespace credential_provider_extension
+
+#endif  // IOS_COMPONENTS_CREDENTIAL_PROVIDER_EXTENSION_PASSWORD_UTIL_H_
--- ios/components/credential_provider_extension/password_util_unittest.mm
+++ ios/components/credential_provider_extension/password_util_unittest.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-#include "ios/chrome/credential_provider_extension/password_util.h"
+#include "ios/components/credential_provider_extension/password_util.h"
 
 #import <Foundation/Foundation.h>
 #import <Security/Security.h>
