f8173dc61e2ad43df658173d7b6896316f412235
Claudio DeSouza <cdesouza@igalia.com>
2021-09-29 23:25:25 +0000

    [MPArch] Delay ConversionHost events when prerendering
    This CL adds changes on blink to prevent observable effects to
    impressions/conversions when pages are being prerendered. With these
    changes, attributes registered during pre-rendering are held back until
    the page with those attributes is brought to the fore.
    
    Bug: 1218946
    Change-Id: I835eba98061b0c75e86f663c7d313383ebb214b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3168868
    Commit-Queue: Claudio DeSouza <cdesouza@igalia.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926526}

--- third_party/blink/renderer/core/frame/attribution_reporting.h
+++ third_party/blink/renderer/core/frame/attribution_reporting.h
@@ -39,6 +39,8 @@ class CORE_EXPORT AttributionReporting final
                                           ExceptionState& exception_state);
 
  private:
+  void RegisterImpression(blink::Impression impression);
+
   HeapMojoAssociatedRemote<mojom::blink::ConversionHost> conversion_host_;
 };
 
