f05efbcc45c1102b51145f6accdb9ddc7881c9be
Shakti Sahu <shaktisahu@chromium.org>
2021-09-29 18:06:23 +0000

    [Video Tutorials] Fetch fresh data if accept languages have changed
    This CL adds support for re-requesting fresh data from server if the
    accept languages have changed. This is checked at startup and anytime
    after startup whenever the language changes. The new data fetched won't
    be available to use until next chrome session. Also restructured the
    manager class a bit to allow caching of the calls before initialization.
    
    Bug: 1254109
    Change-Id: I9c73a5ada4d7532a431b7250e2a3148f2c96d044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194110
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926371}

--- chrome/browser/video_tutorials/internal/tutorial_manager_impl.h
+++ chrome/browser/video_tutorials/internal/tutorial_manager_impl.h
@@ -31,6 +31,8 @@ class TutorialManagerImpl : public TutorialManager {
 
  private:
   // TutorialManager implementation.
+  void Initialize(SuccessCallback callback) override;
+  bool IsInitialized() override;
   void GetTutorials(MultipleItemCallback callback) override;
   void GetTutorial(FeatureType feature_type,
                    SingleItemCallback callback) override;
@@ -39,9 +41,10 @@ class TutorialManagerImpl : public TutorialManager {
       FeatureType feature_type) override;
   absl::optional<std::string> GetPreferredLocale() override;
   void SetPreferredLocale(const std::string& locale) override;
+  std::string GetTextLocale() override;
   void SaveGroups(std::unique_ptr<proto::VideoTutorialGroups> groups) override;
 
-  void OnDataLoaded(MultipleItemCallback callback,
+  void OnDataLoaded(SuccessCallback callback,
                     bool success,
                     std::unique_ptr<proto::VideoTutorialGroups> loaded_groups);
   void OnGroupsSaved(bool success);
@@ -56,7 +59,7 @@ class TutorialManagerImpl : public TutorialManager {
   std::unique_ptr<proto::VideoTutorialGroups> tutorial_groups_;
 
   // The initialization status of the database.
-  absl::optional<bool> init_success_;
+  bool initialized_{false};
 
   base::WeakPtrFactory<TutorialManagerImpl> weak_ptr_factory_{this};
 };
