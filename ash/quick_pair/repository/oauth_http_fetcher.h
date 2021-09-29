fc30c224628a5c97086290dfebf4d77fa524fa19
Jon Mann <jonmann@chromium.org>
2021-09-29 19:19:22 +0000

    Fast Pair: Support POST requests in OAuthHttpFetcher.
    Change-Id: Ibb262e20f36288598121249ea8fc918115593966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179769
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#926415}

--- ash/quick_pair/repository/oauth_http_fetcher.h
+++ ash/quick_pair/repository/oauth_http_fetcher.h
@@ -33,10 +33,12 @@ class OAuthHttpFetcher : public HttpFetcher, public OAuth2ApiCallFlow {
   OAuthHttpFetcher& operator=(const OAuthHttpFetcher&) = delete;
   ~OAuthHttpFetcher() override;
 
-  // Performs a GET request to the desired URL and returns the response, if
-  // available, as a string to the provided |callback|.
+  // HttpFetcher::
   void ExecuteGetRequest(const GURL& url,
                          FetchCompleteCallback callback) override;
+  void ExecutePostRequest(const GURL& url,
+                          const std::string& body,
+                          FetchCompleteCallback callback) override;
 
  protected:
   // Reduce the visibility of OAuth2ApiCallFlow::Start() to avoid exposing
@@ -53,8 +55,10 @@ class OAuthHttpFetcher : public HttpFetcher, public OAuth2ApiCallFlow {
                              std::unique_ptr<std::string> body) override;
   net::PartialNetworkTrafficAnnotationTag GetNetworkTrafficAnnotationTag()
       override;
+  std::string GetRequestTypeForBody(const std::string& body) override;
 
  private:
+  void StartRequest(const GURL& url, FetchCompleteCallback callback);
   void OnAccessTokenFetched(GoogleServiceAuthError error,
                             signin::AccessTokenInfo access_token_info);
 
@@ -63,6 +67,8 @@ class OAuthHttpFetcher : public HttpFetcher, public OAuth2ApiCallFlow {
 
   bool has_call_started_ = false;
   GURL url_;
+  std::string body_;
+  RequestType request_type_;
   FetchCompleteCallback callback_;
 
   std::unique_ptr<signin::PrimaryAccountAccessTokenFetcher>
