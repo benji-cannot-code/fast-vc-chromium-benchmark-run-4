e50167ba4aa92e51e9353c490360fc1c4de05c27
Eric Willigers <ericwilligers@chromium.org>
2021-09-29 20:51:31 +0000

    [Lacros] Consistency with Ash for default-installed apps
    For Windows/Mac/Linux, we do not migrate user uninstalled default apps.
    https://chromium-review.googlesource.com/c/chromium/src/+/2685434
    
    For Lacros, we keep the same behavior as Ash.
    
    Bug: 1174667,1252277
    Change-Id: Iecfc3ce758dc3efdaf56bdfe6b9e0c9101e4fa7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180645
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926466}

--- chrome/browser/web_applications/extensions/extension_status_utils.cc
+++ chrome/browser/web_applications/extensions/extension_status_utils.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/one_shot_event.h"
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/preinstalled_apps.h"
 #include "chrome/browser/profiles/profile.h"
@@ -111,12 +110,12 @@ void OnExtensionSystemReady(content::BrowserContext* context,
 }
 
 bool DidPreinstalledAppsPerformNewInstallation(Profile* profile) {
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
+#if !defined(OS_CHROMEOS)
   return preinstalled_apps::Provider::DidPerformNewInstallationForProfile(
       profile);
 #else
   return false;
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // defined(OS_CHROMEOS)
 }
 
 bool IsPreinstalledAppId(const std::string& app_id) {
