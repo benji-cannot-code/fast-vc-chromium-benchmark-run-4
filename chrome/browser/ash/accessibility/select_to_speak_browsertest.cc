3486629f091b14e1928313c5eb99554f1ab17c95
Devlin Cronin <rdevlin.cronin@chromium.org>
2021-09-29 00:41:33 +0000

    [Extensions] Shorten a couple ExtensionHostTestHelper methods
    The ExtensionHostTestHelper class's methods were a little verbose.
    Rename them now before they're too widespread:
    WaitForExtensionHostCompletedFirstLoad -> WaitForHostCompletedFirstLoad
    WaitForExtensionHostDestroyed -> WaitForHostDestroyed
    
    Given the class name, the "ExtensionHost" is implied. Note that we don't
    remove "Host" entirely here (unlike in WaitForDocumentElementAvailable
    and WaitForRenderProcessReady) because these methods are more
    action-based than state-based (and "WaitForDestroyed" and
    "WaitForCompletedFirstLoad" sound clunky).
    
    Bug: None
    Change-Id: I63ce729919ecfba4a72cd66651f7778dd5d00423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3190248
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Auto-Submit: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926040}

--- chrome/browser/ash/accessibility/select_to_speak_browsertest.cc
+++ chrome/browser/ash/accessibility/select_to_speak_browsertest.cc
@@ -79,7 +79,7 @@ class SelectToSpeakTest : public InProcessBrowserTest {
     extensions::ExtensionHostTestHelper host_helper(
         browser()->profile(), extension_misc::kSelectToSpeakExtensionId);
     AccessibilityManager::Get()->SetSelectToSpeakEnabled(true);
-    host_helper.WaitForExtensionHostCompletedFirstLoad();
+    host_helper.WaitForHostCompletedFirstLoad();
 
     aura::Window* root_window = Shell::Get()->GetPrimaryRootWindow();
     generator_ = std::make_unique<ui::test::EventGenerator>(root_window);
--- chrome/browser/ash/accessibility/switch_access_browsertest.cc
+++ chrome/browser/ash/accessibility/switch_access_browsertest.cc
@@ -48,7 +48,7 @@ class SwitchAccessTest : public InProcessBrowserTest {
     extensions::ExtensionHostTestHelper host_helper(
         manager->profile(), extension_misc::kSwitchAccessExtensionId);
     manager->SetSwitchAccessEnabled(true);
-    host_helper.WaitForExtensionHostCompletedFirstLoad();
+    host_helper.WaitForHostCompletedFirstLoad();
 
     manager->SetSwitchAccessKeysForTest(
         select_key_codes,
