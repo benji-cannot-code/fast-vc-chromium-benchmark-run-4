492f0f3333d63334a0bd6701d5f96b66f6ab95a3
Guillaume Jenkins <gujen@google.com>
2021-09-29 15:35:24 +0000

    [LR] Hook up toolbar controls
    Makes the LightweightReactionsCoordinatorImpl implement the new
    reaction ToolbarControlsDelegate to handle the Cancel and Done buttons.
    For now, Done simply dismisses the dialog.
    
    Also makes the LightweightReactionsCoordinatorImpl create and hold a
    ToolbarCoordinator. The LR coordinator is responsible for observing
    its dialog view creation, and initializing the toolbar coordinator
    when the root dialog view is ready. The toolbar coordinator is then
    able to hook up event handlers on the toolbar buttons to the LR
    coordinator, which is the delegate for the toolbar controls.
    
    Fixed: 1253541
    Change-Id: I362a5745399516acdfa4c6bd3211d7565a6bf46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3188032
    Reviewed-by: Sophey <sophey@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#926273}

--- chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsCoordinatorImpl.java
+++ chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsCoordinatorImpl.java
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.content_creation.reactions;
 
 import android.app.Activity;
+import android.view.View;
 
 import androidx.fragment.app.FragmentActivity;
 
+import org.chromium.chrome.browser.content_creation.reactions.toolbar.ToolbarControlsDelegate;
+import org.chromium.chrome.browser.content_creation.reactions.toolbar.ToolbarCoordinator;
 import org.chromium.chrome.browser.share.BaseScreenshotCoordinator;
 import org.chromium.chrome.browser.share.share_sheet.ChromeOptionShareCallback;
 import org.chromium.chrome.browser.tab.Tab;
@@ -17,9 +20,11 @@ import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 /**
  * Responsible for reactions main UI and its subcomponents.
  */
-public class LightweightReactionsCoordinatorImpl
-        extends BaseScreenshotCoordinator implements LightweightReactionsCoordinator {
+public class LightweightReactionsCoordinatorImpl extends BaseScreenshotCoordinator
+        implements LightweightReactionsCoordinator, ToolbarControlsDelegate {
     private final LightweightReactionsDialog mDialog;
+    private ToolbarCoordinator mToolbarCoordinator;
+
     /**
      * Constructs a new LightweightReactionsCoordinatorImpl which initializes and displays the
      * Lightweight Reactions scene.
@@ -37,15 +42,37 @@ public class LightweightReactionsCoordinatorImpl
         mDialog = new LightweightReactionsDialog();
     }
 
+    /**
+     * Initializes the toolbar after the root dialog view is ready.
+     * @param view The root {@link View} of the dialog.
+     */
+    private void onViewCreated(View view) {
+        mToolbarCoordinator = new ToolbarCoordinator(view, this);
+    }
+
+    // LightweightReactionsCoordinator implementation.
     @Override
     public void showDialog() {
         FragmentActivity fragmentActivity = (FragmentActivity) mActivity;
         mDialog.show(fragmentActivity.getSupportFragmentManager(), null);
     }
 
+    // BaseScreenshotCoordinator implementation.
     @Override
     protected void handleScreenshot() {
-        mDialog.init(mScreenshot);
+        mDialog.init(mScreenshot, this::onViewCreated);
         showDialog();
+    }
+
+    // ToolbarControlsDelegate implementation.
+    @Override
+    public void cancelButtonTapped() {
+        mDialog.dismiss();
+    }
+
+    @Override
+    public void doneButtonTapped() {
+        // For now, simply dismiss the dialog.
+        mDialog.dismiss();
     }
 }
