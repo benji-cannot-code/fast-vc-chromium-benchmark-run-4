e54e0de6d56677966353d0813cb94f1ccb7ccdcd
Scott Violet <sky@chromium.org>
2021-09-29 21:23:26 +0000

    win: converts feature to using feature params
    This removes the feature kEvictRootSurfaceWhenHidden, and instead
    uses a feature param for kApplyNativeOcclusionToCompositor. Three
    variants are supported (as before, but now controlled via params):
    1. Apply to compositor only. When the HWND is occluded the compositor is
       hidden.
    2. Apply and evict. Same as 1, but in addition evicts the root surface
       when hidden. This immediately frees up more gpu resources.
    3. Don't apply, but evict when hidden.
    
    BUG=1248235
    TEST=none
    
    Change-Id: If312ffec3e9fb4078026a651fa24dc2eecc21cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194788
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926480}

--- ui/aura/window_tree_host.cc
+++ ui/aura/window_tree_host.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
 #include "build/chromeos_buildflags.h"
@@ -52,7 +53,16 @@ const char kWindowTreeHostForAcceleratedWidget[] =
 
 bool ShouldEvictRootSurfaceWhenHidden() {
 #if defined(OS_WIN)
-  return base::FeatureList::IsEnabled(features::kEvictRootSurfaceWhenHidden);
+  if (!base::FeatureList::IsEnabled(
+          features::kApplyNativeOcclusionToCompositor)) {
+    return false;
+  }
+
+  const std::string type = base::GetFieldTrialParamValueByFeature(
+      features::kApplyNativeOcclusionToCompositor,
+      features::kApplyNativeOcclusionToCompositorType);
+  return type == features::kApplyNativeOcclusionToCompositorTypeApplyAndEvict ||
+         type == features::kApplyNativeOcclusionToCompositorTypeEvictOnly;
 #else
   return false;
 #endif
