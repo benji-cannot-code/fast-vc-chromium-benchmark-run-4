6c4c06a3c547c0146748d6f1174c53767da01a81
Maria Kazinova <kazinova@google.com>
2021-09-29 16:54:01 +0000

    Migrate chrome/browser/browsing_data to PasswordStoreInterface.
    PasswordStoreInterface is an interface that should be used by other
    classes instead of interacting directly with PasswordStore.
    
    Bug: 1218413
    Change-Id: I41c99d2a6e4d5565624487c1085f520eb7a3c9c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193541
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926323}

--- chrome/browser/browsing_data/counters/passwords_counter_browsertest.cc
+++ chrome/browser/browsing_data/counters/passwords_counter_browsertest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/browsing_data/core/counters/passwords_counter.h"
 #include "components/browsing_data/core/pref_names.h"
 #include "components/password_manager/core/browser/password_form.h"
-#include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/browser/password_store_interface.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_thread.h"
@@ -35,7 +34,7 @@ class PasswordsCounterTest : public InProcessBrowserTest {
     finished_ = false;
     time_ = base::Time::Now();
     times_used_ = 0;
-    store_ = PasswordStoreFactory::GetForProfile(
+    store_ = PasswordStoreFactory::GetInterfaceForProfile(
                  browser()->profile(), ServiceAccessType::IMPLICIT_ACCESS)
                  .get();
     SetPasswordsDeletionPref(true);
@@ -167,9 +166,9 @@ IN_PROC_BROWSER_TEST_F(PasswordsCounterTest, SameDomain) {
 
   Profile* profile = browser()->profile();
   browsing_data::PasswordsCounter counter(
-      PasswordStoreFactory::GetForProfile(profile,
-                                          ServiceAccessType::EXPLICIT_ACCESS),
-      AccountPasswordStoreFactory::GetForProfile(
+      PasswordStoreFactory::GetInterfaceForProfile(
+          profile, ServiceAccessType::EXPLICIT_ACCESS),
+      AccountPasswordStoreFactory::GetInterfaceForProfile(
           profile, ServiceAccessType::EXPLICIT_ACCESS),
       SyncServiceFactory::GetForProfile(profile));
   counter.Init(profile->GetPrefs(),
@@ -190,9 +189,9 @@ IN_PROC_BROWSER_TEST_F(PasswordsCounterTest, blocklisted) {
 
   Profile* profile = browser()->profile();
   browsing_data::PasswordsCounter counter(
-      PasswordStoreFactory::GetForProfile(profile,
-                                          ServiceAccessType::EXPLICIT_ACCESS),
-      AccountPasswordStoreFactory::GetForProfile(
+      PasswordStoreFactory::GetInterfaceForProfile(
+          profile, ServiceAccessType::EXPLICIT_ACCESS),
+      AccountPasswordStoreFactory::GetInterfaceForProfile(
           profile, ServiceAccessType::EXPLICIT_ACCESS),
       SyncServiceFactory::GetForProfile(profile));
 
@@ -215,9 +214,9 @@ IN_PROC_BROWSER_TEST_F(PasswordsCounterTest, PrefChanged) {
 
   Profile* profile = browser()->profile();
   browsing_data::PasswordsCounter counter(
-      PasswordStoreFactory::GetForProfile(profile,
-                                          ServiceAccessType::EXPLICIT_ACCESS),
-      AccountPasswordStoreFactory::GetForProfile(
+      PasswordStoreFactory::GetInterfaceForProfile(
+          profile, ServiceAccessType::EXPLICIT_ACCESS),
+      AccountPasswordStoreFactory::GetInterfaceForProfile(
           profile, ServiceAccessType::EXPLICIT_ACCESS),
       SyncServiceFactory::GetForProfile(profile));
   counter.Init(profile->GetPrefs(),
@@ -237,9 +236,9 @@ IN_PROC_BROWSER_TEST_F(PasswordsCounterTest, StoreChanged) {
 
   Profile* profile = browser()->profile();
   browsing_data::PasswordsCounter counter(
-      PasswordStoreFactory::GetForProfile(profile,
-                                          ServiceAccessType::EXPLICIT_ACCESS),
-      AccountPasswordStoreFactory::GetForProfile(
+      PasswordStoreFactory::GetInterfaceForProfile(
+          profile, ServiceAccessType::EXPLICIT_ACCESS),
+      AccountPasswordStoreFactory::GetInterfaceForProfile(
           profile, ServiceAccessType::EXPLICIT_ACCESS),
       SyncServiceFactory::GetForProfile(profile));
   counter.Init(profile->GetPrefs(),
@@ -273,9 +272,9 @@ IN_PROC_BROWSER_TEST_F(PasswordsCounterTest, PeriodChanged) {
 
   Profile* profile = browser()->profile();
   browsing_data::PasswordsCounter counter(
-      PasswordStoreFactory::GetForProfile(profile,
-                                          ServiceAccessType::EXPLICIT_ACCESS),
-      AccountPasswordStoreFactory::GetForProfile(
+      PasswordStoreFactory::GetInterfaceForProfile(
+          profile, ServiceAccessType::EXPLICIT_ACCESS),
+      AccountPasswordStoreFactory::GetInterfaceForProfile(
           profile, ServiceAccessType::EXPLICIT_ACCESS),
       SyncServiceFactory::GetForProfile(profile));
   counter.Init(profile->GetPrefs(),
@@ -322,9 +321,9 @@ IN_PROC_BROWSER_TEST_F(PasswordsCounterTest, MostCommonDomains) {
 
   Profile* profile = browser()->profile();
   browsing_data::PasswordsCounter counter(
-      PasswordStoreFactory::GetForProfile(profile,
-                                          ServiceAccessType::EXPLICIT_ACCESS),
-      AccountPasswordStoreFactory::GetForProfile(
+      PasswordStoreFactory::GetInterfaceForProfile(
+          profile, ServiceAccessType::EXPLICIT_ACCESS),
+      AccountPasswordStoreFactory::GetInterfaceForProfile(
           profile, ServiceAccessType::EXPLICIT_ACCESS),
       SyncServiceFactory::GetForProfile(profile));
   counter.Init(profile->GetPrefs(),
