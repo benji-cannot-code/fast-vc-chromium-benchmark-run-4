9eb4f37934c41da58787e3f9d1b114e879711dcc
Nancy Wang <nancylingwang@chromium.org>
2021-09-29 00:52:18 +0000

    Add InstallSource to apps.mojom::App.
    This CL adds install_source to apps.mojom::App to record where the
    app is installed from, e.g. play store, chrome web store, etc.
    
    Doc: https://docs.google.com/document/d/1h2aSTCJJ815ORzEPzkr7CJIWiQYnBYUOn_lQqDInPJ0
    
    BUG=1250971
    
    Change-Id: Ib2a72a603ecb5d6f4963ee42fa362e08e45500fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3184840
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926047}

--- chrome/browser/apps/app_service/publishers/arc_apps.cc
+++ chrome/browser/apps/app_service/publishers/arc_apps.cc
@@ -1339,7 +1339,7 @@ void ArcApps::LoadPlayStoreIcon(apps::mojom::IconType icon_type,
                        is_placeholder_icon, icon_effects, std::move(callback));
 }
 
-apps::mojom::InstallReason GetInstallSource(
+apps::mojom::InstallReason GetInstallReason(
     const ArcAppListPrefs* prefs,
     const std::string& app_id,
     const ArcAppListPrefs::AppInfo& app_info) {
@@ -1368,11 +1368,12 @@ apps::mojom::AppPtr ArcApps::Convert(ArcAppListPrefs* prefs,
                                      const std::string& app_id,
                                      const ArcAppListPrefs::AppInfo& app_info,
                                      bool update_icon) {
+  auto install_reason = GetInstallReason(prefs, app_id, app_info);
   apps::mojom::AppPtr app = PublisherBase::MakeApp(
       apps::mojom::AppType::kArc, app_id,
       app_info.suspended ? apps::mojom::Readiness::kDisabledByPolicy
                          : apps::mojom::Readiness::kReady,
-      app_info.name, GetInstallSource(prefs, app_id, app_info));
+      app_info.name, install_reason);
 
   app->publisher_id = app_info.package_name;
 
@@ -1387,6 +1388,9 @@ apps::mojom::AppPtr ArcApps::Convert(ArcAppListPrefs* prefs,
 
   app->last_launch_time = app_info.last_launch_time;
   app->install_time = app_info.install_time;
+  app->install_source = install_reason == apps::mojom::InstallReason::kSystem
+                            ? apps::mojom::InstallSource::kSystem
+                            : apps::mojom::InstallSource::kPlayStore;
 
   auto show = ShouldShow(app_info) ? apps::mojom::OptionalBool::kTrue
                                    : apps::mojom::OptionalBool::kFalse;
