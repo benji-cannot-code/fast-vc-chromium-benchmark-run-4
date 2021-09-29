97721fb2f2baddfa61fc291b899b59db8cd9ed63
Shubham Aggarwal <shuagga@microsoft.com>
2021-09-29 21:48:16 +0000

    Add bool to indicate whether ProxyConfig was created using system settings
    To decide between whether to use the new WindowsProxyResolutionService
    or ConfiguredProxyResolutionService, we need to know whether the current
    ProxyConfig object comes from the OS. This change adds a |from_system_|
    member to ProxyConfig which is set by the relevant ProxyConfigService
    when the ProxyConfig object is created.
    
    Even though this information is only needed in the Windows case, for
    consistency this change adds it for all supported OSes.
    
    Bug: 1032820
    Change-Id: Iec6a219e8d5e1dc138e20fba85bcdc55649b0d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3165457
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Nicolas Arciniega <niarci@microsoft.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Shubham Aggarwal <shuagga@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#926498}

--- net/proxy_resolution/proxy_config_service_linux.cc
+++ net/proxy_resolution/proxy_config_service_linux.cc
@@ -1048,6 +1048,7 @@ bool ProxyConfigServiceLinux::Delegate::GetProxyFromSettings(
 absl::optional<ProxyConfigWithAnnotation>
 ProxyConfigServiceLinux::Delegate::GetConfigFromSettings() {
   ProxyConfig config;
+  config.set_from_system(true);
 
   std::string mode;
   if (!setting_getter_->GetString(SettingGetter::PROXY_MODE, &mode)) {
