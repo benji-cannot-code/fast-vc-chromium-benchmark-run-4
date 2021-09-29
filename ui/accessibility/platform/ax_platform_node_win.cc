043ba89ce675a1690eca4bd7bf0801fb8217eb56
Fernando Fiori <ffiori@microsoft.com>
2021-09-29 15:23:20 +0000

    Highlight API: Add support for HighlightMarkers in UIA
    This CL sets an AnnotationType_Highlighted to the content surrounded
    by a HighlightMarker.
    
    https://docs.microsoft.com/en-us/windows/win32/winauto/uiauto-annotation-type-identifiers
    
    AX-Relnotes: n/a
    Change-Id: I578c4e604efe3919b46fe96fe190b04484a2c8d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3187276
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Fernando Fiori <ffiori@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#926270}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -4940,19 +4940,22 @@ HRESULT AXPlatformNodeWin::GetAnnotationTypesAttribute(
     const absl::optional<int>& end_offset,
     base::win::VariantVector* result) {
   base::win::VariantVector variant_vector;
-
   MarkerTypeRangeResult grammar_result = MarkerTypeRangeResult::kNone;
   MarkerTypeRangeResult spelling_result = MarkerTypeRangeResult::kNone;
+  MarkerTypeRangeResult highlight_result = MarkerTypeRangeResult::kNone;
 
   if (IsText() || IsAtomicTextField()) {
     grammar_result = GetMarkerTypeFromRange(start_offset, end_offset,
                                             ax::mojom::MarkerType::kGrammar);
     spelling_result = GetMarkerTypeFromRange(start_offset, end_offset,
                                              ax::mojom::MarkerType::kSpelling);
+    highlight_result = GetMarkerTypeFromRange(
+        start_offset, end_offset, ax::mojom::MarkerType::kHighlight);
   }
 
   if (grammar_result == MarkerTypeRangeResult::kMixed ||
-      spelling_result == MarkerTypeRangeResult::kMixed) {
+      spelling_result == MarkerTypeRangeResult::kMixed ||
+      highlight_result == MarkerTypeRangeResult::kMixed) {
     Microsoft::WRL::ComPtr<IUnknown> mixed_attribute_value;
     HRESULT hr = ::UiaGetReservedMixedAttributeValue(&mixed_attribute_value);
     if (SUCCEEDED(hr))
@@ -4964,6 +4967,8 @@ HRESULT AXPlatformNodeWin::GetAnnotationTypesAttribute(
     result->Insert<VT_I4>(AnnotationType_SpellingError);
   if (grammar_result == MarkerTypeRangeResult::kMatch)
     result->Insert<VT_I4>(AnnotationType_GrammarError);
+  if (highlight_result == MarkerTypeRangeResult::kMatch)
+    result->Insert<VT_I4>(AnnotationType_Highlighted);
 
   return S_OK;
 }
