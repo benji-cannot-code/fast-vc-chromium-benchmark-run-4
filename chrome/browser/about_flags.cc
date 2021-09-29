c3084693632b766de3735e13b0ee1f7436da4ea2
Leonard Grey <lgrey@chromium.org>
2021-09-29 14:30:57 +0000

    Mac: Remove ScreenTime integration (hopefully temporary)
    This is currently blocked on Issue 1202440. The flag is expired, and a
    user who inadvertently toggles it can end up in a difficult to recover
    state (as chrome://flags is not exempted).
    
    Bug: 1154287
    Change-Id: Ie7877ef30b92ebe2c3fc0d4dc2a12f6d2d2df886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3188915
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926254}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -242,7 +242,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_MAC)
 #include "chrome/browser/ui/browser_dialogs.h"
-#include "chrome/browser/ui/cocoa/screentime/screentime_features.h"
 #endif  // OS_MAC
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -6236,9 +6235,6 @@ const FeatureEntry kFeatureEntries[] = {
     {"metal", flag_descriptions::kMetalName,
      flag_descriptions::kMetalDescription, kOsMac,
      FEATURE_VALUE_TYPE(features::kMetal)},
-    {"screentime", flag_descriptions::kScreenTimeName,
-     flag_descriptions::kScreenTimeDescription, kOsMac,
-     FEATURE_VALUE_TYPE(screentime::kScreenTime)},
 #endif
 
     {"enable-de-jelly", flag_descriptions::kEnableDeJellyName,
