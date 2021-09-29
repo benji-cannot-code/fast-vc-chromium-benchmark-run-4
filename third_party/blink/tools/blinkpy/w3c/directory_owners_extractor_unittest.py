3bdccdce41361588129ffb4c53673d30ca18f291
Nodir Turakulov <nodir@chromium.org>
2021-09-29 18:55:59 +0000

    [dirmd] Migrate to "read" subcommand
    "dirmd compute" were merged into "dirmd read", distinguished by -form flag.
    Use `dirmd read -form sparse` instead of "dirmd compute".
    
    The -root flag was removed: repo root is used instead.
    Update the code to expect paths relative to the repo root.
    
    Change-Id: Ia6800e80919fbe2dda186bd0afb2adb8d9812662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2911765
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Commit-Queue: Nodir Turakulov <nodir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926402}

--- third_party/blink/tools/blinkpy/w3c/directory_owners_extractor_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/directory_owners_extractor_unittest.py
@@ -229,8 +229,7 @@ class DirectoryOwnersExtractorTest(unittest.TestCase):
                                                         'a/b/OWNERS')
 
         self.assertEqual(self.host.executive.full_calls[0].args, [
-            'dirmd', 'compute', '-root', MOCK_WEB_TESTS_WITHOUT_SLASH,
-            MOCK_WEB_TESTS + 'a/b'
+            'dirmd', 'read', '-form', 'sparse', MOCK_WEB_TESTS + 'a/b'
         ])
         self.assertEqual(wpt_dir_metadata.team_email, 'bar')
         self.assertEqual(wpt_dir_metadata.should_notify, True)
@@ -244,8 +243,7 @@ class DirectoryOwnersExtractorTest(unittest.TestCase):
                                                         'a/b/OWNERS')
 
         self.assertEqual(self.host.executive.full_calls[0].args, [
-            'dirmd', 'compute', '-root', MOCK_WEB_TESTS_WITHOUT_SLASH,
-            MOCK_WEB_TESTS + 'a/b'
+            'dirmd', 'read', '-form', 'sparse', MOCK_WEB_TESTS + 'a/b'
         ])
         self.assertEqual(wpt_dir_metadata, None)
 
