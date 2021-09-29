5e7f8d0de3a208bda049412af0c17e1ef3635629
Changhao Han <changhaohan@chromium.org>
2021-09-29 09:11:38 +0000

    Add Isolation Mode to DevTools inspector overlay
    This adds a new type of persistent overlay: isolation mode, to inspector
    overlays. When it is turned on for an element, the overlay will:
    1. find the given element's current width, height, and content box;
    2. and use these information to position three resizers for width,
    height, and both directions (similar to DevTools' device preview mode);
    3. send these information and the positions of the three resizers
    to the overlay frontend to draw.
    
    When the overlay frontend draws the resizers, it will watch mouse events
    on the resizers and send resizing messages to the overlay backend, which
    will decode the message and adjust the element's width and/or height
    accordingly.
    
    There will be some follow-ups to solve some niche cases, e.g. when the
    element is rotated, etc. These niche cases will be collected and we need
    more UX work to determine what to do in these cases.
    
    Design doc: https://goo.gle/devtools-container-queries
    
    frontend CL: https://crrev.com/c/3175289
    
    screencast: https://imgur.com/a/61Tvewc
    
    Bug: chromium:1146422
    Change-Id: I1b072d3ba255a37fac05b0f71e5d59529c566d2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173212
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926181}

--- third_party/blink/renderer/core/inspector/inspector_overlay_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_overlay_agent.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "third_party/blink/public/platform/web_input_event_result.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/core/accessibility/ax_context.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -111,7 +112,7 @@ class CORE_EXPORT InspectTool : public GarbageCollected<InspectTool> {
   virtual bool ForwardEventsToOverlay();
   virtual bool SupportsPersistentOverlays();
   virtual void Draw(float scale) {}
-  virtual void Dispatch(const String& message) {}
+  virtual void Dispatch(const ScriptValue& message) {}
   virtual void Trace(Visitor* visitor) const;
   virtual bool HideOnHideHighlight();
   virtual bool HideOnMouseMove();
@@ -158,6 +159,9 @@ class CORE_EXPORT InspectorOverlayAgent final
       protocol::Overlay::ContainerQueryContainerHighlightConfig*);
   static std::unique_ptr<InspectorFlexItemHighlightConfig>
   ToFlexItemHighlightConfig(protocol::Overlay::FlexItemHighlightConfig*);
+  static std::unique_ptr<InspectorIsolationModeHighlightConfig>
+  ToIsolationModeHighlightConfig(
+      protocol::Overlay::IsolationModeHighlightConfig*);
   static absl::optional<LineStyle> ToLineStyle(protocol::Overlay::LineStyle*);
   static absl::optional<BoxStyle> ToBoxStyle(protocol::Overlay::BoxStyle*);
   static std::unique_ptr<InspectorHighlightConfig> ToHighlightConfig(
@@ -246,6 +250,10 @@ class CORE_EXPORT InspectorOverlayAgent final
       std::unique_ptr<
           protocol::Array<protocol::Overlay::ContainerQueryHighlightConfig>>
           container_query_highlight_configs) override;
+  protocol::Response setShowIsolatedElements(
+      std::unique_ptr<
+          protocol::Array<protocol::Overlay::IsolatedElementHighlightConfig>>
+          isolated_element_highlight_configs) override;
 
   // InspectorBaseAgent overrides.
   void Restore() override;
@@ -278,7 +286,7 @@ class CORE_EXPORT InspectorOverlayAgent final
   class InspectorPageOverlayDelegate;
 
   // InspectorOverlayHost::Delegate implementation.
-  void Dispatch(const String& message) override;
+  void Dispatch(const ScriptValue& message) override;
 
   bool IsEmpty();
 
--- third_party/blink/renderer/core/inspector/inspector_overlay_host.cc
+++ third_party/blink/renderer/core/inspector/inspector_overlay_host.cc
@@ -34,7 +34,7 @@ namespace blink {
 InspectorOverlayHost::InspectorOverlayHost(Delegate* delegate)
     : delegate_(delegate) {}
 
-void InspectorOverlayHost::send(const String& message) {
+void InspectorOverlayHost::send(const ScriptValue& message) {
   if (delegate_)
     delegate_->Dispatch(message);
 }
