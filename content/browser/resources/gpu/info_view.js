286a8e9f33bc98ecede3c51fc99547e0a67386a3
Peng Huang <penghuang@chromium.org>
2021-09-29 20:43:01 +0000

    RawDraw: add raw draw in chrome://gpu page
    Bug: 3191733
    Change-Id: I30970227196caa54d1420dcec186ba60652c3df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193290
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926462}

--- content/browser/resources/gpu/info_view.js
+++ content/browser/resources/gpu/info_view.js
@@ -279,6 +279,7 @@ export function makeInfoView(browserBridge) {
         'webgl2': 'WebGL2',
         'skia_renderer': 'Skia Renderer',
         'canvas_oop_rasterization': 'Canvas out-of-process rasterization',
+        'raw_draw': 'Raw Draw',
       };
 
       const statusMap = {
