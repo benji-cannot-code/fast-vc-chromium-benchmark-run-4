5cc597d820c700d8696fcf22404c9cb81ce611d1
Justin Cohen <justincohen@google.com>
2021-09-29 17:38:09 +0000

    ios: Optimize calls to reloadInputViews in autofill.
    Only call reloadInputViews when the field type has changed, and after
    ignoring things like blur, change or form_changed. Include a kill switch
    as this will likely be cherry picked.
    
    Bug: 1253561
    Change-Id: If794bacb63c2e6bc2810ea7e2c108c186a9d47b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193170
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926352}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_mediator.mm
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_mediator.mm
@@ -49,6 +49,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using base::UmaHistogramEnumeration;
 
+namespace {
+
+// Kill switch guarding a workaround for keyboard flicker, see crbug.com/1253561
+const base::Feature kFormInputKeyboardReloadInputViews{
+    "FormInputKeyboardReloadInputViews", base::FEATURE_ENABLED_BY_DEFAULT};
+
+}  // namespace
+
 @interface FormInputAccessoryMediator () <FormActivityObserver,
                                           FormInputAccessoryViewDelegate,
                                           CRWWebStateObserver,
@@ -300,7 +308,11 @@ using base::UmaHistogramEnumeration;
   }
 
   self.validActivityForAccessoryView = YES;
-  [GetFirstResponder() reloadInputViews];
+  static bool form_input_keyboard_reload_input_views_workaround =
+      base::FeatureList::IsEnabled(kFormInputKeyboardReloadInputViews);
+  if (!form_input_keyboard_reload_input_views_workaround) {
+    [GetFirstResponder() reloadInputViews];
+  }
 
   NSString* frameID;
   if (frame) {
@@ -315,6 +327,11 @@ using base::UmaHistogramEnumeration;
   if (params.type == "blur" || params.type == "change" ||
       params.type == "form_changed") {
     return;
+  }
+
+  if (form_input_keyboard_reload_input_views_workaround &&
+      _lastSeenParams.field_type != params.field_type) {
+    [GetFirstResponder() reloadInputViews];
   }
   _lastSeenParams = params;
   _hasLastSeenParams = YES;
