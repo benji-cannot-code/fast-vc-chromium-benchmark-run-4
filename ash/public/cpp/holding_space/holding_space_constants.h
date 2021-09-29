2a0a3a09bb095487fa301dd61132d9a93ee9bc71
David Black <dmblack@google.com>
2021-09-29 20:17:14 +0000

    Restrict pause/resume/cancel visibility for holding space items.
    For now, these commands will only be visible for in-progress *download*
    type holding space items. We can open visibility back up later in the
    future if required for additional holding space item types.
    
    Bug: 1251353
    Change-Id: I13802113d13e6d9c472e2b6bcf51040a6d7b4292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3183520
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926448}

--- ash/public/cpp/holding_space/holding_space_constants.h
+++ ash/public/cpp/holding_space/holding_space_constants.h
@@ -37,7 +37,8 @@ constexpr int kHoldingSpaceTrayIconSize = 20;
 
 // Context menu commands.
 enum class HoldingSpaceCommandId {
-  kCancelItem,
+  kMinValue = 1,  // NOTE: Zero is used when command id is unset.
+  kCancelItem = kMinValue,
   kCopyImageToClipboard,
   kHidePreviews,
   kRemoveItem,
@@ -47,6 +48,7 @@ enum class HoldingSpaceCommandId {
   kShowInFolder,
   kShowPreviews,
   kUnpinItem,
+  kMaxValue = kUnpinItem,
 };
 
 // View IDs.
