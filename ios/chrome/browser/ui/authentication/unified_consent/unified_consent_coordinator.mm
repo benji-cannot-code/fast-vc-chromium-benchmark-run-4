cc891af0b502bff092943cbd594d47f052c8ca71
David Jean <djean@chromium.org>
2021-09-29 07:36:38 +0000

    [ios] Add restricted accounts message to user sign in vc
    https://drive.google.com/file/d/1yE2o0UH1G7gR4HNrO8Cx6Y6ndHEDhktA/view?usp=sharing
    
    Bug: 1243681
    Change-Id: I6a2cf755c551616ca9fd9000f71d04859ba50fe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3156634
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926154}

--- ios/chrome/browser/ui/authentication/unified_consent/unified_consent_coordinator.mm
+++ ios/chrome/browser/ui/authentication/unified_consent/unified_consent_coordinator.mm
@@ -110,6 +110,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return HasManagedSyncDataType(self.browser->GetBrowserState());
 }
 
+- (BOOL)hasRestrictedAccounts {
+  return ChromeAccountManagerServiceFactory::GetForBrowserState(
+             self.browser->GetBrowserState())
+      ->HasRestrictedIdentities();
+}
+
 #pragma mark - Private
 
 // Opens the identity chooser dialog with an animation from |point|.
