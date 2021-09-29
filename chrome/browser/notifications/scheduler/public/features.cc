c127142969221b63024d0cc34649b9cd72d94bfc
Xing Liu <xingliu@chromium.org>
2021-09-29 18:40:25 +0000

    Notification: Enable scheduling service by default.
    Enable the notification scheduling service feature flag.
    
    Bug: NONE
    Change-Id: I0fa697eafce91f5995d8a7e8182e02d749a32b25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169632
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926391}

--- chrome/browser/notifications/scheduler/public/features.cc
+++ chrome/browser/notifications/scheduler/public/features.cc
@@ -9,7 +9,7 @@ namespace notifications {
 namespace features {
 
 const base::Feature kNotificationScheduleService{
-    "NotificationScheduleService", base::FEATURE_DISABLED_BY_DEFAULT};
+    "NotificationScheduleService", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 
