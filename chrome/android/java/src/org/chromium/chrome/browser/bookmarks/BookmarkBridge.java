b51084d636a5c835bed78069acf189e82b5b130d
Matt Jones <mdjones@chromium.org>
2021-09-29 13:48:35 +0000

    [Shopping] Use byte arrays to pass data across JNI
    This patch changes the means by which proto data is passed to and
    from native and java. Rather than using strings with a specific
    encoding, a raw byte array is now passed. This avoids issues like
    data truncation (resulting in invalid protos).
    
    Bug: 1205448
    Change-Id: I4042f4c84acebdf14052cbe1dba86727f8085393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179581
    Commit-Queue: Matt Jones <mdjones@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926239}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
@@ -31,7 +31,6 @@ import org.chromium.components.url_formatter.UrlFormatter;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.url.GURL;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -642,13 +641,13 @@ public class BookmarkBridge {
      * @return The meta or null if none exists.
      */
     public PowerBookmarkMeta getPowerBookmarkMeta(BookmarkId id) {
-        String protoBytes = BookmarkBridgeJni.get().getPowerBookmarkMeta(
+        byte[] protoBytes = BookmarkBridgeJni.get().getPowerBookmarkMeta(
                 mNativeBookmarkBridge, this, id.getId(), id.getType());
 
-        if (TextUtils.isEmpty(protoBytes)) return null;
+        if (protoBytes == null) return null;
 
         try {
-            return PowerBookmarkMeta.parseFrom(protoBytes.getBytes(StandardCharsets.UTF_8));
+            return PowerBookmarkMeta.parseFrom(protoBytes);
         } catch (InvalidProtocolBufferException ex) {
             deletePowerBookmarkMeta(id);
             return null;
@@ -662,10 +661,9 @@ public class BookmarkBridge {
      * @param meta The meta to store.
      */
     public void setPowerBookmarkMeta(BookmarkId id, PowerBookmarkMeta meta) {
-        String protoBytes =
-                meta != null ? new String(meta.toByteArray(), StandardCharsets.UTF_8) : null;
-        BookmarkBridgeJni.get().setPowerBookmarkMeta(
-                mNativeBookmarkBridge, BookmarkBridge.this, id.getId(), id.getType(), protoBytes);
+        if (meta == null) return;
+        BookmarkBridgeJni.get().setPowerBookmarkMeta(mNativeBookmarkBridge, BookmarkBridge.this,
+                id.getId(), id.getType(), meta.toByteArray());
     }
 
     /**
@@ -1178,10 +1176,10 @@ public class BookmarkBridge {
                 long nativeBookmarkBridge, BookmarkBridge caller, long id, int type, String title);
         void setBookmarkUrl(
                 long nativeBookmarkBridge, BookmarkBridge caller, long id, int type, GURL url);
-        String getPowerBookmarkMeta(
+        byte[] getPowerBookmarkMeta(
                 long nativeBookmarkBridge, BookmarkBridge caller, long id, int type);
         void setPowerBookmarkMeta(
-                long nativeBookmarkBridge, BookmarkBridge caller, long id, int type, String meta);
+                long nativeBookmarkBridge, BookmarkBridge caller, long id, int type, byte[] meta);
         void deletePowerBookmarkMeta(
                 long nativeBookmarkBridge, BookmarkBridge caller, long id, int type);
         boolean doesBookmarkExist(
