56ea52aae0d2537428f03128c755553712b96759
Ben Wagner <bungeman@chromium.org>
2021-09-29 20:08:39 +0000

    Make PDF accessibility tests more specific.
    Revert "Re-enable 4 PDF accessibility tests by making them more robust."
    Revert "Disable 4 PDFExtensionTest test cases as they fail on multiple
    bots."
    
    These tests were modified to work when the subset "character" \014 was
    not actually available to map to "fi" or any useful glyph. This could
    only happen when the Type1 font embedded in the PDF could not be read.
    This happened when FreeType was misconfigured not to support Type1 fonts
    when they were needed for PDF support. Changing these tests back both
    simplifies the tests and prevents any future regressions.
    
    Bug: chromium:701427
    Change-Id: Id2c49ee0e7319a61dfa4a5725d2d88479249ad12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194730
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926446}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
-#include "base/strings/pattern.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
@@ -174,13 +173,12 @@ void WaitForLoadStart(WebContents* web_contents) {
 }
 }  // namespace
 
-// Check if the |actual| string matches the string or the string pattern in
-// |pattern| and print a readable message if it does not match.
-#define ASSERT_MULTILINE_STR_MATCHES(pattern, actual) \
-  ASSERT_TRUE(base::MatchPattern(actual, pattern))    \
-      << "Expected match pattern:\n"                  \
-      << pattern << "\n\nActual:\n"                   \
-      << actual
+// Using ASSERT_TRUE deliberately instead of ASSERT_EQ or ASSERT_STREQ
+// in order to print a more readable message if the strings differ.
+#define ASSERT_MULTILINE_STREQ(expected, actual) \
+    ASSERT_TRUE(expected == actual) \
+        << "Expected:\n" << expected \
+        << "\n\nActual:\n" << actual
 
 class PDFExtensionTestWithoutUnseasonedOverride
     : public extensions::ExtensionApiTest {
@@ -1515,10 +1513,7 @@ std::string DumpPdfAccessibilityTree(const ui::AXTreeUpdate& ax_tree) {
   return ax_tree_dump;
 }
 
-// This is a pattern with a few wildcards due to a PDF bug where the
-// fi ligature is not parsed correctly on some systems.
-// http://crbug.com/701427
-constexpr char kExpectedPDFAXTreePattern[] =
+constexpr char kExpectedPDFAXTree[] =
     "pdfRoot 'PDF document containing 3 pages'\n"
     "  region 'Page 1'\n"
     "    paragraph\n"
@@ -1526,8 +1521,8 @@ constexpr char kExpectedPDFAXTreePattern[] =
     "        inlineTextBox '1 '\n"
     "        inlineTextBox 'First Section'\n"
     "    paragraph\n"
-    "      staticText 'This is the *rst section.'\n"
-    "        inlineTextBox 'This is the *rst section.'\n"
+    "      staticText 'This is the first section.'\n"
+    "        inlineTextBox 'This is the first section.'\n"
     "    paragraph\n"
     "      staticText '1'\n"
     "        inlineTextBox '1'\n"
@@ -1537,8 +1532,8 @@ constexpr char kExpectedPDFAXTreePattern[] =
     "        inlineTextBox '1.1 '\n"
     "        inlineTextBox 'First Subsection'\n"
     "    paragraph\n"
-    "      staticText 'This is the *rst subsection.'\n"
-    "        inlineTextBox 'This is the *rst subsection.'\n"
+    "      staticText 'This is the first subsection.'\n"
+    "        inlineTextBox 'This is the first subsection.'\n"
     "    paragraph\n"
     "      staticText '2'\n"
     "        inlineTextBox '2'\n"
@@ -1565,7 +1560,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfAccessibility) {
   ui::AXTreeUpdate ax_tree = GetAccessibilityTreeSnapshotForPdf(guest_contents);
   std::string ax_tree_dump = DumpPdfAccessibilityTree(ax_tree);
 
-  ASSERT_MULTILINE_STR_MATCHES(kExpectedPDFAXTreePattern, ax_tree_dump);
+  ASSERT_MULTILINE_STREQ(kExpectedPDFAXTree, ax_tree_dump);
 }
 
 IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfAccessibilityEnableLater) {
@@ -1581,7 +1576,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfAccessibilityEnableLater) {
                                                 "1 First Section\r\n");
   ui::AXTreeUpdate ax_tree = GetAccessibilityTreeSnapshotForPdf(guest_contents);
   std::string ax_tree_dump = DumpPdfAccessibilityTree(ax_tree);
-  ASSERT_MULTILINE_STR_MATCHES(kExpectedPDFAXTreePattern, ax_tree_dump);
+  ASSERT_MULTILINE_STREQ(kExpectedPDFAXTree, ax_tree_dump);
 }
 
 // Flaky, see crbug.com/1228762
@@ -1605,7 +1600,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionTest, MAYBE_PdfAccessibilityInIframe) {
 
   ui::AXTreeUpdate ax_tree = GetAccessibilityTreeSnapshotForPdf(guest_contents);
   std::string ax_tree_dump = DumpPdfAccessibilityTree(ax_tree);
-  ASSERT_MULTILINE_STR_MATCHES(kExpectedPDFAXTreePattern, ax_tree_dump);
+  ASSERT_MULTILINE_STREQ(kExpectedPDFAXTree, ax_tree_dump);
 }
 
 IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfAccessibilityInOOPIF) {
@@ -1623,7 +1618,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfAccessibilityInOOPIF) {
 
   ui::AXTreeUpdate ax_tree = GetAccessibilityTreeSnapshotForPdf(guest_contents);
   std::string ax_tree_dump = DumpPdfAccessibilityTree(ax_tree);
-  ASSERT_MULTILINE_STR_MATCHES(kExpectedPDFAXTreePattern, ax_tree_dump);
+  ASSERT_MULTILINE_STREQ(kExpectedPDFAXTree, ax_tree_dump);
 }
 
 IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfAccessibilityWordBoundaries) {
