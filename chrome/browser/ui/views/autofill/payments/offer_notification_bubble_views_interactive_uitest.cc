aa985ac85079a80a57f8662663a02f4cb4054f25
Marc Treib <treib@chromium.org>
2021-09-29 09:30:37 +0000

    Disable OfferNotificationBubbleViewsInteractiveUiTest.Logging* on Linux
    These tests are flaky. 2 of the 3 were already disabled on Mac for
    being flaky.
    
    Bug: 1186169
    Change-Id: Ifb66b6977c6063fd034b4b1685236a0a29357e51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192673
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926183}

--- chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_interactive_uitest.cc
+++ chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_interactive_uitest.cc
@@ -157,8 +157,14 @@ IN_PROC_BROWSER_TEST_F(OfferNotificationBubbleViewsInteractiveUiTest,
   EXPECT_FALSE(IsIconVisible());
 }
 
+#if defined(OS_LINUX)
+// Flaky: https://crbug.com/1186169.
+#define MAYBE_Logging_Shown DISABLED_Logging_Shown
+#else
+#define MAYBE_Logging_Shown Logging_Shown
+#endif
 IN_PROC_BROWSER_TEST_F(OfferNotificationBubbleViewsInteractiveUiTest,
-                       Logging_Shown) {
+                       MAYBE_Logging_Shown) {
   base::HistogramTester histogram_tester;
   ShowBubbleAndVerify();
 
@@ -179,8 +185,8 @@ IN_PROC_BROWSER_TEST_F(OfferNotificationBubbleViewsInteractiveUiTest,
       1);
 }
 
-#if defined(OS_MAC)
-// OS_MAC: Flaky: https://crbug.com/1186169.
+#if defined(OS_LINUX) || defined(OS_MAC)
+// Flaky: https://crbug.com/1186169.
 #define MAYBE_Logging_Acknowledged DISABLED_Logging_Acknowledged
 #else
 #define MAYBE_Logging_Acknowledged Logging_Acknowledged
@@ -216,8 +222,8 @@ IN_PROC_BROWSER_TEST_F(OfferNotificationBubbleViewsInteractiveUiTest,
       1);
 }
 
-#if defined(OS_MAC)
-// OS_MAC: Flaky: https://crbug.com/1186164.
+#if defined(OS_LINUX) || defined(OS_MAC)
+// Flaky: https://crbug.com/1186169.
 #define MAYBE_Logging_Closed DISABLED_Logging_Closed
 #else
 #define MAYBE_Logging_Closed Logging_Closed
