a98ea1ff53b05128bdd06d3ea5934b36eb3aed97
Gabriel Charette <gab@chromium.org>
2021-09-29 17:44:17 +0000

    [metrics] Temporary NOINLINE rules for SampleVectorBase::Accumulate
    As discussed @ https://bugs.chromium.org/p/chromium/issues/detail?id=1254354#c4
    
    R=asvitkine@chromium.org
    
    Bug: 1254354
    Change-Id: Idd19a0a7cb818a554ade1442f0e4278066a7ade5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194650
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926357}

--- base/metrics/histogram_samples.h
+++ base/metrics/histogram_samples.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/atomicops.h"
+#include "base/compiler_specific.h"
 #include "base/metrics/histogram_base.h"
 
 namespace base {
@@ -64,7 +65,8 @@ class BASE_EXPORT HistogramSamples {
     // Adds a given count to the held bucket. If not possible, it returns false
     // and leaves the parts unchanged. Once extracted/disabled, this always
     // returns false. This in an "acquire/release" operation.
-    bool Accumulate(size_t bucket, HistogramBase::Count count);
+    // TODO(crbug.com/1254354): Remove NOINLINE after diagnosis.
+    bool NOINLINE Accumulate(size_t bucket, HistogramBase::Count count);
 
     // Returns if the sample has been "disabled" (via Extract) and thus not
     // allowed to accept further accumulation.
@@ -188,12 +190,14 @@ class BASE_EXPORT HistogramSamples {
   // Accumulates to the embedded single-sample field if possible. Returns true
   // on success, false otherwise. Sum and redundant-count are also updated in
   // the success case.
-  bool AccumulateSingleSample(HistogramBase::Sample value,
-                              HistogramBase::Count count,
-                              size_t bucket);
+  // TODO(crbug.com/1254354): Remove NOINLINE after diagnosis.
+  bool NOINLINE AccumulateSingleSample(HistogramBase::Sample value,
+                                       HistogramBase::Count count,
+                                       size_t bucket);
 
   // Atomically adjust the sum and redundant-count.
-  void IncreaseSumAndCount(int64_t sum, HistogramBase::Count count);
+  // TODO(crbug.com/1254354): Remove NOINLINE after diagnosis.
+  void NOINLINE IncreaseSumAndCount(int64_t sum, HistogramBase::Count count);
 
   // Record a negative-sample observation and the reason why.
   void RecordNegativeSample(NegativeSampleReason reason,
