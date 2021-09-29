746152dba79cb3f66edf28dab9507910d11124a4
Eugene Zemtsov <eugene@chromium.org>
2021-09-29 21:13:52 +0000

    webcodecs: Render video frames according to their display size
    1. Special handling for VideoFrames in base_rendering_context_2d.cc
       It allows to avoid double scaling when frame has display size
       and resized by drawImage()
    
    2. Caching canvas resource provider between frame render calls.
    
    3. Accurate handling of color spaces in VideoImageGenerator.
    
    Bug: 1244034
    Change-Id: I61f547d90cec0dfdccb609fd52ed8ad3d8f648f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3099249
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926475}

--- third_party/blink/web_tests/external/wpt/webcodecs/image-decoder-utils.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/image-decoder-utils.js
@@ -1,4 +1,23 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const kYellow = 0xFFFF00FF;
+const kRed = 0xFF0000FF;
+const kBlue = 0x0000FFFF;
+const kGreen = 0x00FF00FF;
+
+function getColorName (color) {
+  switch (color) {
+    case kYellow:
+      return "Yellow";
+    case kRed:
+      return "Red";
+    case kBlue:
+      return "Blue";
+    case kGreen:
+      return "Green";
+  }
+  return "#" + color.toString(16);
+}
+
 function toUInt32(pixelArray) {
   let p = pixelArray.data;
   return ((p[0] << 24) + (p[1] << 16) + (p[2] << 8) + p[3]) >>> 0;
@@ -100,8 +119,8 @@ function testFourColorDecodeWithExifOrientation(orientation, canvas) {
           ctx.drawImage(result.image, 0, 0);
 
           let matrix = [
-            [0xFFFF00FF, 0xFF0000FF],  // yellow, red
-            [0x0000FFFF, 0x00FF00FF],  // blue, green
+            [kYellow, kRed],
+            [kBlue, kGreen],
           ];
           if (respectOrientation) {
             switch (orientation) {
@@ -145,8 +164,8 @@ function testFourColorDecodeWithExifOrientation(orientation, canvas) {
 function verifyFourColorsImage(width, height, ctx, matrix) {
   if (!matrix) {
     matrix = [
-      [0xFFFF00FF, 0xFF0000FF],  // yellow, red
-      [0x0000FFFF, 0x00FF00FF],  // blue, green
+      [kYellow, kRed],
+      [kBlue, kGreen],
     ];
   }
 
@@ -160,8 +179,12 @@ function verifyFourColorsImage(width, height, ctx, matrix) {
   let bottomLeft = toUInt32(ctx.getImageData(0, height - 1, 1, 1));
   let bottomRight = toUInt32(ctx.getImageData(width - 1, height - 1, 1, 1));
 
-  assert_equals(topLeft, expectedTopLeft, 'top left corner');
-  assert_equals(topRight, expectedTopRight, 'top right corner');
-  assert_equals(bottomLeft, expectedBottomLeft, 'bottom left corner');
-  assert_equals(bottomRight, expectedBottomRight, 'bottom right corner');
+  assert_equals(getColorName(topLeft), getColorName(expectedTopLeft),
+                            'top left corner');
+  assert_equals(getColorName(topRight), getColorName(expectedTopRight),
+                            'top right corner');
+  assert_equals(getColorName(bottomLeft), getColorName(expectedBottomLeft),
+                            'bottom left corner');
+  assert_equals(getColorName(bottomRight), getColorName(expectedBottomRight),
+                            'bottom right corner');
 }
