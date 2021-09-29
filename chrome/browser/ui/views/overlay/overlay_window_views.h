bf2144d46e3ff4ec1281cf312a960d4f038e7f3e
Tommy Steimel <steimel@chromium.org>
2021-09-29 17:33:21 +0000

    PiP: Ensure controls show up on focus
    Bug: 1214013
    Change-Id: Id2ac96132b6c065281b4aa136ee8a7ae5f9014d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3175242
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#926349}

--- chrome/browser/ui/views/overlay/overlay_window_views.h
+++ chrome/browser/ui/views/overlay/overlay_window_views.h
@@ -71,6 +71,7 @@ class OverlayWindowViews : public content::OverlayWindow,
   // views::Widget:
   bool IsActive() const override;
   bool IsVisible() const override;
+  void OnNativeFocus() override;
   void OnNativeBlur() override;
   void OnNativeWidgetDestroyed() override;
   gfx::Size GetMinimumSize() const override;
