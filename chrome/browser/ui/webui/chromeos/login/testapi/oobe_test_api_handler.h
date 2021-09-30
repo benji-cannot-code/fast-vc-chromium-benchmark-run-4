8093a21825b6a8a5ba2bd919f42d5b5189d0009a
Roman Sorokin <rsorokin@chromium.org>
2021-09-30 13:26:22 +0000

    [oobe test api] Add loginWithPin function
    The function helps automate authentication with PIN from the login
    screen. It's not easy at the moment to launch autotest extension on the
    login screen for the tauto due to signin profile restrictions.
    Also see http://crrev.com/c/3181060 for the usage
    
    BUG=b:197667947
    BUG=b:197667852
    BUG=b:197262920
    
    Change-Id: I5a49f5cd74aa3aa6e192ea548ee30be0c58f5b39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3196111
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#926725}

--- chrome/browser/ui/webui/chromeos/login/testapi/oobe_test_api_handler.h
+++ chrome/browser/ui/webui/chromeos/login/testapi/oobe_test_api_handler.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,8 +21,12 @@ class OobeTestAPIHandler : public BaseWebUIHandler {
   // WebUIMessageHandler implementation:
   void DeclareLocalizedValues(
       ::login::LocalizedValuesBuilder* builder) override;
+  void DeclareJSCallbacks() override;
   void Initialize() override;
   void GetAdditionalParameters(base::DictionaryValue* dict) override;
+
+ private:
+  void LoginWithPin(const std::string& username, const std::string& pin);
 };
 
 }  // namespace chromeos
