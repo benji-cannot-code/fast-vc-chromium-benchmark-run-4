699f040034dc7652657c579befb60faa074cb898
Nancy Wang <nancylingwang@chromium.org>
2021-09-30 23:14:32 +0000

    Rename InstallSource to InstallReason (part 5).
    Doc: https://docs.google.com/document/d/1h2aSTCJJ815ORzEPzkr7CJIWiQYnBYUOn_lQqDInPJ0
    
    InstallSource is renamed to InstallReason, so that we can add a new
    InstallSource to define where the app is installed, e.g. from play
    store, chrome web store, etc.
    
    This is the part 5 to rename InstallSource to InstallReason in
    crosapi mojom, and managed_apps_test.
    
    This CL is pure renaming/refactor, no function change.
    
    BUG=1250971
    
    Change-Id: I733a56d4912af2c2b8d44ac8320e9a77892b6384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195431
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926994}

--- chromeos/crosapi/mojom/app_service_types_mojom_traits.h
+++ chromeos/crosapi/mojom/app_service_types_mojom_traits.h
@@ -71,7 +71,7 @@ struct StructTraits<crosapi::mojom::AppDataView, apps::mojom::AppPtr> {
     return r->install_time;
   }
 
-  static const apps::mojom::InstallReason& install_source(
+  static const apps::mojom::InstallReason& install_reason(
       const apps::mojom::AppPtr& r) {
     return r->install_reason;
   }
@@ -161,10 +161,10 @@ struct StructTraits<crosapi::mojom::IconKeyDataView, apps::mojom::IconKeyPtr> {
 };
 
 template <>
-struct EnumTraits<crosapi::mojom::InstallSource, apps::mojom::InstallReason> {
-  static crosapi::mojom::InstallSource ToMojom(
+struct EnumTraits<crosapi::mojom::InstallReason, apps::mojom::InstallReason> {
+  static crosapi::mojom::InstallReason ToMojom(
       apps::mojom::InstallReason input);
-  static bool FromMojom(crosapi::mojom::InstallSource input,
+  static bool FromMojom(crosapi::mojom::InstallReason input,
                         apps::mojom::InstallReason* output);
 };
 
