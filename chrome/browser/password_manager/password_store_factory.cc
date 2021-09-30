e53d0faa1c5c938df04f58baecedcb7154108d12
Chris Davis (EDGE) <chrdavis@microsoft.com>
2021-09-30 15:46:42 +0000

    Remove the blank password code from password_manager_util_win
    Code was added to check if the currently logged on user has a blank
    password and logs a histogram.  This results in excessive audit
    failures.  The resulting histogram doesn't seem to be used so the code
    should be removed.
    
    Bug: 1132052
    Change-Id: If4742836c2e441cde15307f1194341aa7afcbb83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3178675
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Chris Davis <chrdavis@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#926774}

--- chrome/browser/password_manager/password_store_factory.cc
+++ chrome/browser/password_manager/password_store_factory.cc
@@ -38,10 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
-#if defined(OS_WIN)
-#include "chrome/browser/password_manager/password_manager_util_win.h"
-#endif
-
 using password_manager::PasswordStore;
 using password_manager::PasswordStoreInterface;
 
@@ -88,9 +84,6 @@ PasswordStoreFactory::~PasswordStoreFactory() = default;
 scoped_refptr<RefcountedKeyedService>
 PasswordStoreFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
-#if defined(OS_WIN)
-  password_manager_util_win::DelayReportOsPassword();
-#endif
   Profile* profile = static_cast<Profile*>(context);
 
   std::unique_ptr<password_manager::LoginDatabase> login_db(
