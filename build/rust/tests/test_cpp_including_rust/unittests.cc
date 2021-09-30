61b96e8144731be236a4f96aee40819fd2f3edf2
Lukasz Anforowicz <lukasza@chromium.org>
2021-09-30 18:07:31 +0000

    [rust] Cover test_cpp_including_rust_unittests test suite on bots.
    After this CL the test_cpp_including_rust_unittests test suite will
    be run on the linux-rust-x64-rel and android-rust-arm-rel bots.
    
    Manual testing: manually request the two bots above to run on the
    CL at https://crrev.com/c/3195215 and inspect the run results to
    verify that the test_cpp_including_rust_unittests step is present
    and that it's json.output contains the expected test results.
    
    Bug: 1245778
    Change-Id: I24b98e74d380af229211841acbae252da25dc044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195215
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926839}

--- build/rust/tests/test_cpp_including_rust/unittests.cc
+++ build/rust/tests/test_cpp_including_rust/unittests.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/allocator/buildflags.h"
+#include "base/allocator/partition_allocator/address_pool_manager_bitmap.h"
 #include "base/allocator/partition_allocator/partition_address_space.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
