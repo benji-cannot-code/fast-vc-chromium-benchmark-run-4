375a16ea31301faf71cf48e5e4e21da00bdc8bae
Frank Liberato <liberato@chromium.org>
2021-09-30 18:24:01 +0000

    Add ReuseMediaPlayer switch.
    When enabled, HTMLMediaElement will try to re-use the player across
    document movement.
    
    This is not at all safe to enable right now.  It's intended to
    allow incremental implementation of player reuse.
    
    Bug: 1252550
    Change-Id: I20ac59340e99fd02732174866fa5cfa27c0b53f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180280
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926851}

--- third_party/blink/renderer/platform/testing/empty_web_media_player.h
+++ third_party/blink/renderer/platform/testing/empty_web_media_player.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_TESTING_EMPTY_WEB_MEDIA_PLAYER_H_
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "third_party/blink/public/platform/web_media_player.h"
 
 namespace cc {
@@ -19,7 +20,8 @@ namespace blink {
 // An empty WebMediaPlayer used only for tests. This class defines the methods
 // of WebMediaPlayer so that mock WebMediaPlayers don't need to redefine them if
 // they don't care their behavior.
-class EmptyWebMediaPlayer : public WebMediaPlayer {
+class EmptyWebMediaPlayer : public WebMediaPlayer,
+                            public base::SupportsWeakPtr<EmptyWebMediaPlayer> {
  public:
   ~EmptyWebMediaPlayer() override = default;
 
@@ -72,7 +74,9 @@ class EmptyWebMediaPlayer : public WebMediaPlayer {
   void Paint(cc::PaintCanvas*, const gfx::Rect&, cc::PaintFlags&) override {}
   scoped_refptr<media::VideoFrame> GetCurrentFrame() override;
   bool HasAvailableVideoFrame() const override { return false; }
-  base::WeakPtr<WebMediaPlayer> AsWeakPtr() override { return nullptr; }
+  base::WeakPtr<WebMediaPlayer> AsWeakPtr() override {
+    return base::SupportsWeakPtr<EmptyWebMediaPlayer>::AsWeakPtr();
+  }
 };
 
 }  // namespace blink
