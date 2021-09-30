2210e58413217ae9b2791cf59f8aa15843825aa9
Claudius van der Merwe <cvandermerwe@google.com>
2021-09-30 18:50:02 +0000

    [Nearby] Make multidevice subpage accessible pre-onboarding
    This CL
    1. Makes the Nearby Subpage accessible pre-onboarding if background
    scanning is enabled. To do so an arrow is shown pre-onboarding on the
    row and clicking the row redirects to the subpage instead of initiating
    onboarding.
    2. Only downloads contacts in the Nearby Subpage if onboarding is
    already complete and the feature is enabled.
    
    Screenshot: https://screenshot.googleplex.com/42aQK7jKM5tWcdZ
    
    Bug: 1223888
    Change-Id: I0ed73ea7d0928d88658cad56051f87dd7d129361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3159552
    Commit-Queue: Claude van der Merwe <cvandermerwe@google.com>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#926861}

--- chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_subpage.js
+++ chrome/browser/resources/settings/chromeos/nearby_share_page/nearby_share_subpage.js
@@ -94,7 +94,7 @@ Polymer({
     },
 
     /** @private */
-    isBackgroundScanningEnabled_: {
+    shouldShowFastInititationNotificationToggle_: {
       type: Boolean,
       value: () =>
           loadTimeData.getBoolean('isNearbyShareBackgroundScanningEnabled')
@@ -106,6 +106,7 @@ Polymer({
   /** @private {?nearbyShare.mojom.ReceiveObserverReceiver} */
   receiveObserver_: null,
 
+  /** @override */
   attached() {
     // TODO(b/166779043): Check whether the Account Manager is enabled and fall
     // back to profile name, or just hide the row. This is not urgent because
@@ -122,11 +123,20 @@ Polymer({
         });
     this.receiveObserver_ = nearby_share.observeReceiveManager(
         /** @type {!nearbyShare.mojom.ReceiveObserverInterface} */ (this));
+  },
 
-    // Trigger a contact sync whenever the Nearby subpage is opened to improve
-    // consistency. This should help avoid scenarios where a share is attempted
-    // and contacts are stale on the receiver.
-    nearby_share.getContactManager().downloadContacts();
+  /**
+   * Called when component is attached and all settings values have been
+   * retrieved.
+   */
+  onSettingsRetrieved() {
+    if (this.settings.enabled) {
+      // Trigger a contact sync whenever the Nearby subpage is opened and
+      // onboarding is complete to improve consistency. This should help avoid
+      // scenarios where a share is attempted and contacts are stale on the
+      // receiver.
+      nearby_share.getContactManager().downloadContacts();
+    }
   },
 
   /** @private */
