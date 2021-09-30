2a82f1a3e59741b2309b1ebd6067dc22cd2f9527
Jiewei Qian <qjw@chromium.org>
2021-09-30 09:15:51 +0000

    blink/mojo: support MojoJSInterfaceBroker in MojoInterfaceInterceptor
    This CL adds logic to make MojoInterfaceInterceptor work with
    MojoJSInterfaceBroker by introducing a new "context_js" scope.
    
    Fixed: 1247166
    Change-Id: I66654f6e9514457370e4e635a7cb4c0244be0f1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194018
    Commit-Queue: Jiewei Qian  <qjw@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#926680}

--- third_party/blink/renderer/core/mojo/test/mojo_interface_interceptor.h
+++ third_party/blink/renderer/core/mojo/test/mojo_interface_interceptor.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/strong_alias.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_mojo_interface_interceptor_scope.h"
 #include "third_party/blink/renderer/core/dom/events/event_listener.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
@@ -33,14 +34,15 @@ class MojoInterfaceInterceptor final
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  using Scope = V8MojoInterfaceInterceptorScope;
   static MojoInterfaceInterceptor* Create(ExecutionContext*,
                                           const String& interface_name,
-                                          const String& scope,
+                                          const Scope& scope,
                                           ExceptionState&);
 
   MojoInterfaceInterceptor(ExecutionContext*,
                            const String& interface_name,
-                           bool process_scope);
+                           Scope::Enum scope);
   ~MojoInterfaceInterceptor() override;
 
   void start(ExceptionState&);
@@ -66,7 +68,7 @@ class MojoInterfaceInterceptor final
 
   const String interface_name_;
   bool started_ = false;
-  bool process_scope_ = false;
+  Scope::Enum scope_ = Scope::Enum::kContext;
 };
 
 }  // namespace blink
