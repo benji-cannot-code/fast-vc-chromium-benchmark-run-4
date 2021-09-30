a35dc10c0aa9dd560bc8dad0bede675d3628f47b
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-09-30 03:25:24 +0000

    Revert "appcache: remove appcache inspector agent"
    This reverts commit 8236dc0922d42a440e80daa604aacf1837954756.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 8236dc0922d42a440e80daa604aacf1837954756 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzgyMzZkYzA5MjJkNDJhNDQwZTgwZGFhNjA0YWFjZjE4Mzc5NTQ3NTYM
    
    Sample Failed Build: https://ci.chromium.org/b/8834706556036244193
    
    Sample Failed Step: compile
    
    Original change's description:
    > appcache: remove appcache inspector agent
    > 
    > AppCache is disabled by default and is being removed.
    > 
    > Bug: 582750, 1084190
    > Change-Id: I0b3da78bf8255d50aa7048cafe085d81cf7a7573
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192227
    > Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#926560}
    
    
    Change-Id: I1f80c8f7999883456a4ad2061f757b837b874bb0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 582750, 1084190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194167
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926580}

--- third_party/blink/renderer/core/exported/web_dev_tools_agent_impl.cc
+++ third_party/blink/renderer/core/exported/web_dev_tools_agent_impl.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/devtools_session.h"
 #include "third_party/blink/renderer/core/inspector/inspected_frames.h"
 #include "third_party/blink/renderer/core/inspector/inspector_animation_agent.h"
+#include "third_party/blink/renderer/core/inspector/inspector_application_cache_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_audits_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_css_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_dom_agent.h"
@@ -254,6 +255,9 @@ void WebDevToolsAgentImpl::AttachSession(DevToolsSession* session,
       inspected_frames, css_agent, session->V8Session()));
 
   session->Append(MakeGarbageCollected<InspectorMemoryAgent>(inspected_frames));
+
+  session->Append(
+      MakeGarbageCollected<InspectorApplicationCacheAgent>(inspected_frames));
 
   auto* page_agent = MakeGarbageCollected<InspectorPageAgent>(
       inspected_frames, this, resource_content_loader_.Get(),
