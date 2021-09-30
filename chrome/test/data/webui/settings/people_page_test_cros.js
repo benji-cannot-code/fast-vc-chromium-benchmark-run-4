ab41073768ee4eeea71153b94e4f96a7a66a0cb8
Roman Sorokin <rsorokin@chromium.org>
2021-09-30 14:54:58 +0000

    Remove splitSettingsSyncEnabled mention from the settings_ui
    Looks like JS part is migrated.
    
    Bug: 1227694
    Change-Id: I96636eff8f478da9ef1742852946c344b32b9393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195894
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926753}

--- chrome/test/data/webui/settings/people_page_test_cros.js
+++ chrome/test/data/webui/settings/people_page_test_cros.js
@@ -204,7 +204,6 @@ suite('Chrome OS with account manager disabled', function() {
 suite('Chrome OS with UseBrowserSyncConsent', function() {
   suiteSetup(function() {
     loadTimeData.overrideValues({
-      splitSettingsSyncEnabled: true,
       useBrowserSyncConsent: true,
     });
   });
