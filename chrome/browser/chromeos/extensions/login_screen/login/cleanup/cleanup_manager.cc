40ccc4539d2b740945b6f83b75bcff7062e770e5
Jit Yao Yap <jityao@google.com>
2021-09-30 16:14:29 +0000

    Add ClipboardCleanupHandler
    This CL adds the clipboard cleanup handler for the
    chrome.login.endSession API.
    
    Bug: b:186737512
    
    Change-Id: Idf87c46b84899a979354a0888553d1aab0e2190e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3196132
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Jit Yao Yap <jityao@google.com>
    Cr-Commit-Position: refs/heads/main@{#926788}

--- chrome/browser/chromeos/extensions/login_screen/login/cleanup/cleanup_manager.cc
+++ chrome/browser/chromeos/extensions/login_screen/login/cleanup/cleanup_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/browsing_data_cleanup_handler.h"
+#include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/clipboard_cleanup_handler.h"
 #include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/files_cleanup_handler.h"
 #include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/open_windows_cleanup_handler.h"
 
@@ -72,6 +73,7 @@ void CleanupManager::InitializeCleanupHandlers() {
       std::make_unique<BrowsingDataCleanupHandler>());
   cleanup_handlers_.emplace_back(std::make_unique<OpenWindowsCleanupHandler>());
   cleanup_handlers_.emplace_back(std::make_unique<FilesCleanupHandler>());
+  cleanup_handlers_.emplace_back(std::make_unique<ClipboardCleanupHandler>());
 }
 
 void CleanupManager::OnCleanupHandlerDone(
