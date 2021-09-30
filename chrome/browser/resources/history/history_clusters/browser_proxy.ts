6ee7f8b7d88a4deb98a0980fc48a5ee153211e51
Moe Ahmadi <mahmadi@chromium.org>
2021-09-30 00:03:31 +0000

    Reland "[memories] Separates interface and implementation of proxy classes"
    This reverts commit e95bb8bdec1693ea8d69306a83867254c0c06ae4.
    
    Reason for revert: fixed compilation failure.
    
    Original change's description:
    > Revert "[memories] Separates interface and implementation of proxy classes"
    >
    > This reverts commit 370d2b36619b78cfff569515b4139dc4266eca26.
    >
    > Reason for revert: Causing compile failure. Sample failed build:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/GPU%20Linux%20Builder/249368/overview
    >
    > Original change's description:
    > > [memories] Separates interface and implementation of proxy classes
    > >
    > > Bug: 1173908
    > > Change-Id: I0342015399893cf54b07b5b663fa21325ed12f81
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193040
    > > Reviewed-by: dpapad <dpapad@chromium.org>
    > > Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#926362}
    >
    > Bug: 1173908
    > Change-Id: Id0c1b579e6301dea86c6135796857136c8fcacc3
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194731
    > Owners-Override: Tommy Martino <tmartino@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Tommy Martino <tmartino@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#926366}
    
    Bug: 1173908
    Change-Id: Ia0615bcb7df2346f33c1aa1db7b2ee06cac0670e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194778
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926547}

--- chrome/browser/resources/history/history_clusters/browser_proxy.ts
+++ chrome/browser/resources/history/history_clusters/browser_proxy.ts
@@ -11,7 +11,12 @@ import {PageCallbackRouter, PageHandler, PageHandlerRemote} from './history_clus
  * the browser.
  */
 
-export class BrowserProxy {
+export interface BrowserProxy {
+  handler: PageHandlerRemote;
+  callbackRouter: PageCallbackRouter;
+}
+
+export class BrowserProxyImpl implements BrowserProxy {
   handler: PageHandlerRemote;
   callbackRouter: PageCallbackRouter;
 
@@ -28,7 +33,7 @@ export class BrowserProxy {
     const handler = PageHandler.getRemote();
     const callbackRouter = new PageCallbackRouter();
     handler.setPage(callbackRouter.$.bindNewPipeAndPassRemote());
-    return instance = new BrowserProxy(handler, callbackRouter);
+    return instance = new BrowserProxyImpl(handler, callbackRouter);
   }
 
   static setInstance(obj: BrowserProxy) {
