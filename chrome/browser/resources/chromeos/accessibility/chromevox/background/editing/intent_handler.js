d601d22f1581789e41647de095b6604bf5f5fd46
David Tseng <dtseng@google.com>
2021-09-30 00:51:19 +0000

    Provide better speech feedback for editable paragraph nav in ChromeVox
    When pressing Ctrl+Up or Ctrl+Down, ChromeVox should describe the entire
    paragraph surrounding the caret.
    
    R=akihiroota@chromium.org
    
    AX-Relnotes: ChromeVox will speak the entire paragraph within a multi-line editable text field when pressing Control+Up or Control+Down.
    Bug: none
    Test: cq/browser tests
    Change-Id: I268f9e117b02720a6dd247e906118dcd5c5adaa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3191770
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926570}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/intent_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/intent_handler.js
@@ -21,6 +21,7 @@ const IntentCommandType = chrome.automation.IntentCommandType;
 const IntentTextBoundaryType = chrome.automation.IntentTextBoundaryType;
 const Movement = cursors.Movement;
 const Range = cursors.Range;
+const RoleType = chrome.automation.RoleType;
 const Unit = cursors.Unit;
 
 /**
@@ -142,6 +143,28 @@ IntentHandler = class {
         cur.speakLine(prev);
         return true;
 
+      case IntentTextBoundaryType.PARAGRAPH_START: {
+        let node = cur.startContainer;
+
+        if (node.role === RoleType.LINE_BREAK) {
+          return false;
+        }
+
+        while (node && AutomationPredicate.text(node)) {
+          node = node.parent;
+        }
+
+        if (!node || node.role === RoleType.TEXT_FIELD) {
+          return false;
+        }
+
+        new Output()
+            .withRichSpeechAndBraille(
+                cursors.Range.fromNode(node), null, OutputEventType.NAVIGATE)
+            .go();
+        return true;
+      }
+
       case IntentTextBoundaryType.WORD_END:
       case IntentTextBoundaryType.WORD_START: {
         const shouldMoveToPreviousWord =
@@ -166,7 +189,6 @@ IntentHandler = class {
       case IntentTextBoundaryType.PAGE_START:
       case IntentTextBoundaryType.PAGE_START_OR_END:
       case IntentTextBoundaryType.PARAGRAPH_END:
-      case IntentTextBoundaryType.PARAGRAPH_START:
       case IntentTextBoundaryType.PARAGRAPH_START_OR_END:
       case IntentTextBoundaryType.SENTENCE_END:
       case IntentTextBoundaryType.SENTENCE_START:
