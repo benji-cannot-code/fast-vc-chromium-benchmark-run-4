e3d8d8f5d891c7089e60962ebff27fac1c95c74b
Austin Sullivan <asully@chromium.org>
2021-09-30 20:23:12 +0000

    Fix URL debug string for filesystems which are not GURL serializable
    Calling DebugString() on a URL pointing the local file system, for
    example, used to result in a DCHECK.
    
    Bug: N/A
    Change-Id: I3e9cf9fb927dbc319f0ad9d91199cc83665cc8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171500
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926906}

--- storage/browser/file_system/file_system_url.cc
+++ storage/browser/file_system/file_system_url.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/strings/string_util.h"
 #include "net/base/escape.h"
+#include "storage/common/file_system/file_system_types.h"
 #include "storage/common/file_system/file_system_util.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "url/origin.h"
@@ -135,7 +136,20 @@ std::string FileSystemURL::DebugString() const {
   if (!is_valid_)
     return "invalid filesystem: URL";
   std::ostringstream ss;
-  ss << GetFileSystemRootURI(storage_key_.origin().GetURL(), mount_type_);
+  switch (mount_type_) {
+    // Include GURL if GURL serialization is possible.
+    case kFileSystemTypeTemporary:
+    case kFileSystemTypePersistent:
+    case kFileSystemTypeExternal:
+    case kFileSystemTypeIsolated:
+    case kFileSystemTypeTest:
+      ss << "{ uri: ";
+      ss << GetFileSystemRootURI(storage_key_.origin().GetURL(), mount_type_);
+      break;
+    // Otherwise list the origin and path separately.
+    default:
+      ss << "{ path: ";
+  }
 
   // filesystem_id_ will be non empty for (and only for) cracked URLs.
   if (!filesystem_id_.empty()) {
@@ -147,6 +161,8 @@ std::string FileSystemURL::DebugString() const {
   } else {
     ss << path_.value();
   }
+  ss << ", storage key: " << storage_key_.GetDebugString();
+  ss << " }";
   return ss.str();
 }
 
