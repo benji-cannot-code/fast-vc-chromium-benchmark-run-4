089bc15a74f117ec7778d6f63587b5a85a3b14ed
Brandon Wylie <wylieb@chromium.org>
2021-09-30 21:38:34 +0000

    [Read Later] Add context menu option to feed
    Bug: 1254832
    Change-Id: Icafb2ce14f42ea2309106f35317d5e48e12d0be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198344
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926946}

--- chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/SurfaceActionsHandler.java
+++ chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/SurfaceActionsHandler.java
@@ -45,6 +45,9 @@ public interface SurfaceActionsHandler {
      */
     default void downloadLink(String url) {}
 
+    /** Add the url to the reading list and make it available offline. */
+    default void addToReadingList(String title, String url) {}
+
     /**
      * Open a bottom sheet with the view as contents.
      * @param view The bottom sheet contents view.
