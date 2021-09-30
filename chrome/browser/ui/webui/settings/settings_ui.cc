a09e721a57088c1796d6c657e5d3aabcf93920a7
Lu Huang <luhua@microsoft.com>
2021-09-30 16:22:15 +0000

    Remove unused headers in settings_ui.cc
    Removes:
    web_app_provider.h
    web_app_registrar.h
    url_handler_prefs.h
    
    
    Change-Id: I018ad254f7c91319f883cda1d02aae2f1ddc7f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195219
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Lu Huang <luhua@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#926791}

--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -141,9 +141,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN) || defined(OS_MAC) || \
     (defined(OS_LINUX) && !BUILDFLAG(IS_CHROMEOS_LACROS))
 #include "chrome/browser/ui/webui/settings/url_handlers_handler.h"
-#include "chrome/browser/web_applications/url_handler_prefs.h"
-#include "chrome/browser/web_applications/web_app_provider.h"
-#include "chrome/browser/web_applications/web_app_registrar.h"
 #endif
 
 namespace settings {
