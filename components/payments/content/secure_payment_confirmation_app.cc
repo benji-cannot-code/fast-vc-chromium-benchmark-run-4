97dbf15f188b7544f46726e1b9d15aec7622f4c9
Liquan (Max) Gu <maxlg@chromium.org>
2021-09-30 19:02:00 +0000

    [AndroidSPC] Cancelling Fido prompt should return NotAllowedError
    Before the change, canceling the Fido prompt resulted in the error
    "AbortError: Authenticator returned NOT_ALLOWED_ERROR". This is not
    desired because it exposes the fact that the user has a credential in
    their browser.
    
    After the change, canceling the Fido prompt result in the generic error
    "NotAllowedError: The operation either timed out or was not allowed.
    See: https://www.w3.org/TR/webauthn-2/#sctn-privacy-considerations-client"
    This is desirable because it's indistinguishable from the error of no
    credential available.
    
    Bug: 1239859
    Change-Id: Id686aa502aa4b334bd9866110a3e4f2e8132918c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192576
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926866}

--- components/payments/content/secure_payment_confirmation_app.cc
+++ components/payments/content/secure_payment_confirmation_app.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/payments/content/secure_payment_confirmation_app.h"
 
-#include <sstream>
 #include <utility>
 
 #include "base/base64.h"
@@ -16,11 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
-#include "base/strings/strcat.h"
-#include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "components/payments/content/payment_request_spec.h"
+#include "components/payments/core/error_strings.h"
 #include "components/payments/core/method_strings.h"
 #include "components/payments/core/payer_data.h"
 #include "components/webauthn/core/browser/internal_authenticator.h"
@@ -243,10 +241,8 @@ void SecurePaymentConfirmationApp::OnGetAssertion(
     return;
 
   if (status != blink::mojom::AuthenticatorStatus::SUCCESS || !response) {
-    std::stringstream status_string_stream;
-    status_string_stream << status;
-    delegate->OnInstrumentDetailsError(base::StringPrintf(
-        "Authenticator returned %s.", status_string_stream.str().c_str()));
+    delegate->OnInstrumentDetailsError(
+        errors::kWebAuthnOperationTimedOutOrNotAllowed);
     RecordSystemPromptResult(
         SecurePaymentConfirmationSystemPromptResult::kCanceled);
     return;
