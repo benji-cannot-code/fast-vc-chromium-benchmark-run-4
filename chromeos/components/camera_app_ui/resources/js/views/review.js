71547c5efe84c77ea8396b934ca62db9de4dd27f
Kuo Jen Wei <inker@google.com>
2021-09-30 09:01:29 +0000

    CCA: Add share button to gif reivew page
    Bug: b:191950622
    Test: Manually
    
    Change-Id: Ib1258595bd08c38628cc31bd4356e242cfe23a13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193457
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926676}

--- chromeos/components/camera_app_ui/resources/js/views/review.js
+++ chromeos/components/camera_app_ui/resources/js/views/review.js
@@ -20,19 +20,28 @@ import {View} from './view.js';
 export class Option {
   /**
    * @param {!I18nString} text Text string show on the option button.
-   * @param {!T} value Value returned if the user select this option after
-   *   review.
+   * @param {{
+   *   exitValue: (!T|undefined),
+   *   callback: (function()|undefined),
+   * }} handlerParams Sets |exitValue| if the review page will exit with this
+   *   value when option selected. Sets |callback| for the function get executed
+   *   when option selected.
    */
-  constructor(text, value) {
+  constructor(text, {exitValue, callback}) {
     /**
      * @const {!I18nString}
      */
     this.text = text;
 
     /**
-     * @const {!T}
+     * @const {?T}
      */
-    this.value = value;
+    this.exitValue = exitValue ?? null;
+
+    /**
+     * @const {?function()}
+     */
+    this.callback = callback || null;
   }
 }
 
@@ -133,7 +142,7 @@ export class Review extends View {
      * @param {!Option<!T>} option
      * @param {boolean} isPrimary
      */
-    const addButton = ({text, value}, isPrimary) => {
+    const addButton = ({text, exitValue, callback}, isPrimary) => {
       const templ = instantiateTemplate('#text-button-template');
       const btn = dom.getFrom(templ, 'button', HTMLButtonElement);
       btn.setAttribute('i18n-text', text);
@@ -144,7 +153,12 @@ export class Review extends View {
         btn.classList.add('secondary');
       }
       btn.onclick = () => {
-        onSelected.signal(value);
+        if (callback !== null) {
+          callback();
+        }
+        if (exitValue !== null) {
+          onSelected.signal(exitValue);
+        }
       };
       this.btnGroups_.appendChild(templ);
     };
