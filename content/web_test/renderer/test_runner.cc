c4937b4db821ce4aa309d1a8e4321cd9d8040cff
Austin Sullivan <asully@chromium.org>
2021-09-30 15:14:26 +0000

    Clipboard: Migrate test_runner to ReadPng
    The ReadImage path is being deprecated. This is effectively a no-op,
    since decoding from PNG to bitmap is being done in the current
    ReadImage call anyways.
    
    Bug: 1223849
    Change-Id: I1a3fc1902b6c5039d1a76c35415bc33ace63a210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195085
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#926762}

--- content/web_test/renderer/test_runner.cc
+++ content/web_test/renderer/test_runner.cc
@@ -80,6 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_view_observer.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
@@ -1959,8 +1960,10 @@ void TestRunnerBindings::CopyImageThen(int x,
                                         &sequence_number_after);
   }
 
+  mojo_base::BigBuffer png_data;
+  remote_clipboard->ReadPng(ui::ClipboardBuffer::kCopyPaste, &png_data);
   SkBitmap bitmap;
-  remote_clipboard->ReadImage(ui::ClipboardBuffer::kCopyPaste, &bitmap);
+  gfx::PNGCodec::Decode(png_data.data(), png_data.size(), &bitmap);
 
   v8::Isolate* isolate = blink::MainThreadIsolate();
   v8::HandleScope handle_scope(isolate);
