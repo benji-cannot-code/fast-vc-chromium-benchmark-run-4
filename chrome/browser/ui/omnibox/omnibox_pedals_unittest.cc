f87de179ca258f19c3e8a1532855788c6f90f3db
Orin Jaworski <orinj@google.com>
2021-09-30 20:18:46 +0000

    [omnibox] Make the manage passwords pedal require only "passwords"
    This CL makes the edit/change/manage synonym group optional,
    which has the effect of making the password/passwords synonym
    group the only required group. This will significantly open
    up the triggering for the pedal. The "credentials" synonym
    is removed to narrow it slightly.
    
    Bug: 893183
    Change-Id: I469418c5a7ed6df6c5654b6f5dc668db0170f617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194759
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@google.com>
    Cr-Commit-Position: refs/heads/main@{#926902}

--- chrome/browser/ui/omnibox/omnibox_pedals_unittest.cc
+++ chrome/browser/ui/omnibox/omnibox_pedals_unittest.cc
@@ -169,7 +169,7 @@ void TestDataLoadsForAllLocales(bool with_translation_console) {
       { "en",
         {
           "delete information",
-          "manager credentials",
+          "passwords",
           "change credit card information",
           "incognito window",
           "change language this page",
