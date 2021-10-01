50bbaf1e787093e53041aaa27a1f2c5c8339052c
Gayane Petrosyan <gayane@google.com>
2021-10-01 17:16:07 +0000

    [Stylize] 'New' label for 'Create card' option in Sharing Hub.
    Bug: 1243752
    Change-Id: I8644bbe5030ff0c0819913955976cf064fd2f494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193911
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927259}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -86,6 +86,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHSharedHighlightingReceiverFeature,
     &kIPHStartSurfaceTabSwitcherHomeButton,
     &kIPHUpdatedConnectionSecurityIndicatorsFeature,
+    &kIPHSharingHubWebnotesStylizeFeature,
 #endif  // defined(OS_ANDROID)
 #if defined(OS_IOS)
     &kIPHBottomToolbarTipFeature,
