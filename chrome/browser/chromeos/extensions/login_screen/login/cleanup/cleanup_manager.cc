a6571eefcfc4067f783864fd53dff7448c7678cd
Jit Yao Yap <jityao@google.com>
2021-10-01 08:51:17 +0000

    Add PrintJobsCleanupHandler
    This CL adds the print jobs cleanup handler for the
    chrome.login.endSession API.
    
    Bug: b:186737512
    
    Change-Id: Iabdebe3b4ff91a10fd7836bd64c75c12ec4b4401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195892
    Commit-Queue: Jit Yao Yap <jityao@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927143}

--- chrome/browser/chromeos/extensions/login_screen/login/cleanup/cleanup_manager.cc
+++ chrome/browser/chromeos/extensions/login_screen/login/cleanup/cleanup_manager.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/clipboard_cleanup_handler.h"
 #include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/files_cleanup_handler.h"
 #include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/open_windows_cleanup_handler.h"
+#include "chrome/browser/chromeos/extensions/login_screen/login/cleanup/print_jobs_cleanup_handler.h"
 
 #include "base/logging.h"
 
@@ -74,6 +75,7 @@ void CleanupManager::InitializeCleanupHandlers() {
   cleanup_handlers_.emplace_back(std::make_unique<OpenWindowsCleanupHandler>());
   cleanup_handlers_.emplace_back(std::make_unique<FilesCleanupHandler>());
   cleanup_handlers_.emplace_back(std::make_unique<ClipboardCleanupHandler>());
+  cleanup_handlers_.emplace_back(std::make_unique<PrintJobsCleanupHandler>());
 }
 
 void CleanupManager::OnCleanupHandlerDone(
