6f0a1795d40d8c5c1014739be6b5774c3b997d4c
Vladimir Levin <vmpstr@chromium.org>
2021-10-01 18:21:40 +0000

    DL: Store a StyleRecalcChange instead of custom dirty bits
    This patch stores the StyleRecalcChange from blocked style walks
    and accumulates that, instead of setting custom dirty bits, since
    that is bug prone (e.g. we can miss some new dirty bits).
    
    This also simplifies the code quite a bit.
    
    R=futhark@chromium.org, andruud@chromium.org
    Bug: 1202618
    
    Change-Id: Idc8d67ba746c018dc80db44152977cc5540932af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198134
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927297}

--- third_party/blink/renderer/core/display_lock/display_lock_context.h
+++ third_party/blink/renderer/core/display_lock/display_lock_context.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DISPLAY_LOCK_DISPLAY_LOCK_CONTEXT_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/css/style_recalc.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/scroll/scroll_types.h"
 #include "third_party/blink/renderer/core/style/computed_style_base_constants.h"
@@ -19,7 +20,6 @@ namespace blink {
 
 class Document;
 class Element;
-class StyleRecalcChange;
 
 enum class DisplayLockActivationReason {
   // Accessibility driven activation
@@ -68,15 +68,6 @@ class CORE_EXPORT DisplayLockContext final
     : public GarbageCollected<DisplayLockContext>,
       public LocalFrameView::LifecycleNotificationObserver {
  public:
-  // The type of style that was blocked by this display lock.
-  enum StyleType {
-    kStyleUpdateNotRequired,
-    kStyleUpdateSelf,
-    kStyleUpdatePseudoElements,
-    kStyleUpdateChildren,
-    kStyleUpdateDescendants
-  };
-
   enum class ForcedPhase { kStyleAndLayoutTree, kLayout, kPrePaint };
 
   explicit DisplayLockContext(Element*);
@@ -102,9 +93,9 @@ class CORE_EXPORT DisplayLockContext final
   bool ShouldPaintChildren() const;
 
   // Returns true if the last style recalc traversal was blocked at this
-  // element, either for itself, its children or its descendants.
-  bool StyleTraversalWasBlocked() {
-    return blocked_style_traversal_type_ != kStyleUpdateNotRequired;
+  // element.
+  bool StyleTraversalWasBlocked() const {
+    return !blocked_child_recalc_change_.IsEmpty();
   }
 
   // Returns true if the contents of the associated element should be visible
@@ -138,13 +129,13 @@ class CORE_EXPORT DisplayLockContext final
 
   // Inform the display lock that it prevented a style change. This is used to
   // invalidate style when we need to update it in the future.
-  void NotifyStyleRecalcWasBlocked(StyleType type) {
-    blocked_style_traversal_type_ =
-        std::max(blocked_style_traversal_type_, type);
+  void NotifyChildStyleRecalcWasBlocked(const StyleRecalcChange& change) {
+    blocked_child_recalc_change_ = blocked_child_recalc_change_.Combine(change);
   }
 
   void NotifyReattachLayoutTreeWasBlocked() {
-    reattach_layout_tree_was_blocked_ = true;
+    blocked_child_recalc_change_ =
+        blocked_child_recalc_change_.ForceReattachLayoutTree();
   }
 
   void NotifyChildLayoutWasBlocked() { child_layout_was_blocked_ = true; }
@@ -397,11 +388,7 @@ class CORE_EXPORT DisplayLockContext final
 
   UpdateForcedInfo forced_info_;
 
-  StyleType blocked_style_traversal_type_ = kStyleUpdateNotRequired;
-  // Signifies whether we've blocked a layout tree reattachment on |element_|'s
-  // descendants or not, so that we can mark |element_| for reattachment when
-  // needed.
-  bool reattach_layout_tree_was_blocked_ = false;
+  StyleRecalcChange blocked_child_recalc_change_;
 
   bool needs_effective_allowed_touch_action_update_ = false;
   bool needs_blocking_wheel_event_handler_update_ = false;
