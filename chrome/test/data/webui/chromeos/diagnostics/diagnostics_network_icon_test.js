838dca2fb0078264d0a54b9674aa838e77be6a35
Ashley Prasad <ashleydp@google.com>
2021-10-01 20:07:56 +0000

    Diagnostics: Add connection-state conversion to adapter
    - Sets connectionState based on network state.
    - Tests connection state conversion.
    
      --gtest_filter=DiagnosticsApp*
    
    Bug: 1197335
    Test: testing/xvfb.py out/Default/browser_tests
    Change-Id: I2cd03f4e0a6187260766f1376634004edad0d6c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3191112
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927360}

--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_network_icon_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_network_icon_test.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {CrosNetworkType, DiagnosticsNetworkIconElement, networkToNetworkStateAdapter} from 'chrome://diagnostics/diagnostics_network_icon.js';
-import {fakeCellularNetwork, fakeEthernetNetwork, fakeWifiNetwork} from 'chrome://diagnostics/fake_data.js';
+import {ConnectionStateType, CrosNetworkType, DiagnosticsNetworkIconElement, networkToNetworkStateAdapter} from 'chrome://diagnostics/diagnostics_network_icon.js';
+import {fakeCellularNetwork, fakeConnectingEthernetNetwork, fakeDisconnectedEthernetNetwork, fakeEthernetNetwork, fakePortalWifiNetwork, fakeWifiNetwork, fakeWifiNetworkDisabled} from 'chrome://diagnostics/fake_data.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 import {flushTasks, isVisible} from '../../test_util.js';
@@ -42,7 +42,7 @@ export function diagnosticsNetworkIconTestSuite() {
     });
   });
 
-  test('NetworkToNetworkStateAdapter', () => {
+  test('NetworkToNetworkStateAdapter_NetworkType', () => {
     assertEquals(
         CrosNetworkType.kEthernet,
         networkToNetworkStateAdapter(fakeEthernetNetwork).type);
@@ -52,5 +52,29 @@ export function diagnosticsNetworkIconTestSuite() {
     assertEquals(
         CrosNetworkType.kCellular,
         networkToNetworkStateAdapter(fakeCellularNetwork).type);
+  });
+
+  test('NetworkToNetworkStateAdapterTypes_ConnectionState', () => {
+    assertEquals(
+        ConnectionStateType.kOnline,
+        networkToNetworkStateAdapter(fakeEthernetNetwork).connectionState);
+    assertEquals(
+        ConnectionStateType.kConnected,
+        networkToNetworkStateAdapter(fakeCellularNetwork).connectionState);
+    assertEquals(
+        ConnectionStateType.kPortal,
+        networkToNetworkStateAdapter(fakePortalWifiNetwork).connectionState);
+    assertEquals(
+        ConnectionStateType.kConnecting,
+        networkToNetworkStateAdapter(fakeConnectingEthernetNetwork)
+            .connectionState);
+    // NotConnected states: Disabled, NotConnected.
+    assertEquals(
+        ConnectionStateType.kNotConnected,
+        networkToNetworkStateAdapter(fakeWifiNetworkDisabled).connectionState);
+    assertEquals(
+        ConnectionStateType.kNotConnected,
+        networkToNetworkStateAdapter(fakeDisconnectedEthernetNetwork)
+            .connectionState);
   });
 }
