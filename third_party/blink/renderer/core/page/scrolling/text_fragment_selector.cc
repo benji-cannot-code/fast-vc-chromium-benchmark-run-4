0d566f44f9211c85d55ac7e4e4da88aa1a4cbe81
Gayane Petrosyan <gayane@google.com>
2021-10-01 17:51:14 +0000

    Escape text fragment special characters.
    The documentation says that hyphens, ampersands and commas should be
    escaped https://wicg.github.io/scroll-to-text-fragment/#:~:text=The%20text%20parameters%20are%20percent-decoded%20before%20matching.%20Dash%20(-)%2C%20ampersand%20(%26)%2C%20and%20comma%20(%2C)%20characters%20in%20text%20parameters%20must%20be%20percent-encoded%20to%20avoid%20being%20interpreted%20as%20part%20of%20the%20text%20directive%20syntax.
    
    but when using standard url encoding hyphen is not escaped.
    
    Bug: 1245669
    Change-Id: Ie9bcefdf75bd302d219fda7137ca7de866164d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197798
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#927288}

--- third_party/blink/renderer/core/page/scrolling/text_fragment_selector.cc
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_selector.cc
@@ -40,6 +40,14 @@ String ExtractSuffix(String* target_text) {
   return "";
 }
 
+// Escapes special chars that can be part of text fragment directive, including
+// hyphen (-), ampersand (&), and comma (,).
+String EscapeSelectorSpecialCharacters(const String& target_text) {
+  String escaped_str = EncodeWithURLEscapeSequences(target_text);
+  escaped_str.Replace("-", "%2D");
+  return escaped_str;
+}
+
 }  // namespace
 
 TextFragmentSelector TextFragmentSelector::Create(String target_text) {
@@ -86,22 +94,22 @@ TextFragmentSelector::TextFragmentSelector(SelectorType type) : type_(type) {}
 String TextFragmentSelector::ToString() const {
   StringBuilder selector;
   if (!prefix_.IsEmpty()) {
-    selector.Append(EncodeWithURLEscapeSequences(prefix_));
+    selector.Append(EscapeSelectorSpecialCharacters(prefix_));
     selector.Append("-,");
   }
 
   if (!start_.IsEmpty()) {
-    selector.Append(EncodeWithURLEscapeSequences(start_));
+    selector.Append(EscapeSelectorSpecialCharacters(start_));
   }
 
   if (!end_.IsEmpty()) {
     selector.Append(",");
-    selector.Append(EncodeWithURLEscapeSequences(end_));
+    selector.Append(EscapeSelectorSpecialCharacters(end_));
   }
 
   if (!suffix_.IsEmpty()) {
     selector.Append(",-");
-    selector.Append(EncodeWithURLEscapeSequences(suffix_));
+    selector.Append(EscapeSelectorSpecialCharacters(suffix_));
   }
 
   return selector.ToString();
