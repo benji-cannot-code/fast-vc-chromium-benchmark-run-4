84348f51b31022658152b6e1b177b069116f5adc
rbpotter <rbpotter@chromium.org>
2021-10-01 19:32:38 +0000

    What's New: Account for multiple milestones
    Account for the possibility of different versions of What's New per
    milestone. Specifically:
    - Append "mN" to the URL requested, for all milestones N >= 96 (the
      first milestone where the server will be expecting this extra path).
    - Don't track the max What's New version as we will now be able to tell
      if there is a What's New page for the desired version based on the
      response of the server.
    - If the server responds with 404 but the user tried to open the page
      manually, try to open it for a few previous milestones. We want to
      try to show the user the most recent What's New page, even if it
      isn't for their exact milestone, if the user is manually navigating
      there.
    
    Bug: 1219381
    Change-Id: I1973321aaadcecc5c9a3109be802bd8b1efa8d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192197
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927333}

--- chrome/browser/ui/webui/whats_new/whats_new_handler.h
+++ chrome/browser/ui/webui/whats_new/whats_new_handler.h
@@ -30,6 +30,7 @@ class WhatsNewHandler : public content::WebUIMessageHandler {
  private:
   void HandleInitialize(const base::ListValue* args);
   typedef base::OnceCallback<void(bool success,
+                                  bool page_not_found,
                                   std::unique_ptr<std::string> body)>
       OnFetchResultCallback;
   void Fetch(const GURL& url, OnFetchResultCallback on_result);
@@ -39,6 +40,7 @@ class WhatsNewHandler : public content::WebUIMessageHandler {
   void OnFetchResult(const std::string& callback_id,
                      bool is_auto,
                      bool success,
+                     bool page_not_found,
                      std::unique_ptr<std::string> body);
 
   // content::WebUIMessageHandler:
@@ -46,6 +48,7 @@ class WhatsNewHandler : public content::WebUIMessageHandler {
   void OnJavascriptAllowed() override;
   void OnJavascriptDisallowed() override;
 
+  int num_retries_ = 0;
   std::unordered_map<const network::SimpleURLLoader*,
                      std::unique_ptr<network::SimpleURLLoader>>
       loader_map_;
