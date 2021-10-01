66c402edf2ad8c39259e9378d12bdc85c4b53c2b
Tommy Li <tommycli@chromium.org>
2021-10-01 19:58:35 +0000

    [memories] Make Related Searches group accessible
    This CL adds aria labels, roles, as well as keyboard event handlers to
    bring Related Searches up to the same accessibility standards as the
    rest of the WebUI surface.
    
    Bug: 1249963
    Change-Id: I9aed9cc0e0b9367486eb81e60da2228a873e30dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200144
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927351}

--- chrome/browser/resources/history/history_clusters/url_visit.ts
+++ chrome/browser/resources/history/history_clusters/url_visit.ts
@@ -106,18 +106,6 @@ class VisitRowElement extends PolymerElement {
   // Event handlers
   //============================================================================
 
-  private onKeydown_(e: KeyboardEvent) {
-    // To be consistent with <history-list>, only handle Enter, and not Space.
-    if (e.key !== 'Enter') {
-      return;
-    }
-
-    // To record metrics.
-    this.onAuxClick_();
-
-    OpenWindowProxyImpl.getInstance().open(this.visit.normalizedUrl.url);
-  }
-
   private onActionMenuButtonClick_(event: MouseEvent) {
     this.$.actionMenu.get().showAt(this.$.actionMenuButton);
     event.preventDefault();  // Prevent default browser action (navigation).
@@ -141,6 +129,18 @@ class VisitRowElement extends PolymerElement {
     }
 
     event.preventDefault();  // Prevent default browser action (navigation).
+
+    // To record metrics.
+    this.onAuxClick_();
+
+    OpenWindowProxyImpl.getInstance().open(this.visit.normalizedUrl.url);
+  }
+
+  private onKeydown_(e: KeyboardEvent) {
+    // To be consistent with <history-list>, only handle Enter, and not Space.
+    if (e.key !== 'Enter') {
+      return;
+    }
 
     // To record metrics.
     this.onAuxClick_();
