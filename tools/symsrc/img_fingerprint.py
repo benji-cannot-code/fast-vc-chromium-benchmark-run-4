997759f9c3694399d2e9058d81e32d89ab570443
Bruce Dawson <brucedawson@chromium.org>
2021-10-01 20:42:16 +0000

    Revert "Migrate Clang's package.py and tools/symsrc/ to Python3"
    This reverts commit 96c964b469e3791079f8c3f732030e35538ae4ac.
    
    Reason for revert: It's breaking symbol upload, see crbug.com/1254992
    
    Original change's description:
    > Migrate Clang's package.py and tools/symsrc/ to Python3
    >
    > This should remove the last dependency on  third_party/pefile
    > which is Python 2.
    >
    > (This co-depends on a CL to switch the Clang packaging buildbots
    > to run package.py under Python 3: https://crrev.com/c/3194137)
    >
    > Bug: 1213186
    > Change-Id: Ia5fa42ec2012900058a6ce11bc75cbded15ad052
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194354
    > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Hans Wennborg <hans@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#926673}
    
    Bug: 1213186, 1254992
    Change-Id: Ia577f817e39eb6c74f8fb795645d7a60805f8273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198497
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927376}

--- tools/symsrc/img_fingerprint.py
+++ tools/symsrc/img_fingerprint.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -19,7 +19,7 @@ import sys
 # Assume this script is under tools/symsrc/
 _SCRIPT_DIR = os.path.dirname(__file__)
 _ROOT_DIR = os.path.join(_SCRIPT_DIR, os.pardir, os.pardir)
-_PEFILE_DIR = os.path.join(_ROOT_DIR, 'third_party', 'pefile_py3')
+_PEFILE_DIR = os.path.join(_ROOT_DIR, 'third_party', 'pefile')
 
 sys.path.insert(1, _PEFILE_DIR)
 
