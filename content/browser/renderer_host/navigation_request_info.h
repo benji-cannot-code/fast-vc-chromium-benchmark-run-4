07526430aa2023304ee83c81fe5a1e279cee4199
Daniel Hosseinian <dhoss@chromium.org>
2021-10-01 05:13:47 +0000

    [unseasoned-pdf] Allow PDF blob navigations to be intercepted
    Fix crashes by allowing PDF blob navigations to be intercepted, changing
    the assumption that no requests to blob navigations need intercepting.
    
    Within the PDF loading dance, the browser throttles PDF stream URL
    navigations and redirects them using the original PDF content URL. The
    new navigation gets intercepted and its loader is replaced with a page
    with an in-process plugin.
    
    When the original PDF content URL has a blob: scheme, the interceptor
    was skipped, thus invoking another PDF extension embedded inside the
    first one. The consequent nested WebContents then led to weird behavior,
    causing crashes.
    
    Add tests that load PDF blob URLs to avoid future regressions.
    
    Credit to alexmos@ and kmoon@ for playing critical roles in debugging
    the issue.
    
    Fixed: 1253314
    Change-Id: Ieb0071bac6ffc61ad69a419ace4469a1a576bafa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194567
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927104}

--- content/browser/renderer_host/navigation_request_info.h
+++ content/browser/renderer_host/navigation_request_info.h
@@ -43,7 +43,8 @@ struct CONTENT_EXPORT NavigationRequestInfo {
       net::HttpRequestHeaders cors_exempt_headers,
       network::mojom::ClientSecurityStatePtr client_security_state,
       const absl::optional<std::vector<net::SourceStream::SourceType>>&
-          devtools_accepted_stream_types);
+          devtools_accepted_stream_types,
+      bool is_pdf);
   NavigationRequestInfo(const NavigationRequestInfo& other) = delete;
   ~NavigationRequestInfo();
 
@@ -99,6 +100,9 @@ struct CONTENT_EXPORT NavigationRequestInfo {
   // decoding any non-listed stream types.
   absl::optional<std::vector<net::SourceStream::SourceType>>
       devtools_accepted_stream_types;
+
+  // Indicates that this navigation is for PDF content in a renderer.
+  const bool is_pdf;
 };
 
 }  // namespace content
