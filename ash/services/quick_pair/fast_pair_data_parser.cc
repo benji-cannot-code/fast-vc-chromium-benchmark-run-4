e60c8ffad3eb2a0336bc6a36f84a6235a7a353e2
Shane Fitzpatrick <shanefitz@google.com>
2021-10-01 01:47:28 +0000

    [FastPair] Parse battery information from advertisement
    This changes adds the logic to parse the optional battery information
    from the BT advertisement in our utility process.
    
    Tested via unit tests and integration tests on my local machine.
    
    Change-Id: I13b4caed59733ff6541b76595212912d4b31be45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198342
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927048}

--- ash/services/quick_pair/fast_pair_data_parser.cc
+++ ash/services/quick_pair/fast_pair_data_parser.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/common/fast_pair/fast_pair_decoder.h"
 #include "ash/quick_pair/common/logging.h"
 #include "ash/services/quick_pair/fast_pair_decryption.h"
+#include "ash/services/quick_pair/public/cpp/battery_notification.h"
 #include "ash/services/quick_pair/public/cpp/not_discoverable_advertisement.h"
 #include "ash/services/quick_pair/public/mojom/fast_pair_data_parser.mojom.h"
 #include "base/containers/flat_map.h"
@@ -29,6 +30,8 @@ constexpr int kFieldLengthOffset = 4;
 constexpr int kFieldTypeAccountKeyFilter = 0;
 constexpr int kFieldTypeAccountKeyFilterSalt = 1;
 constexpr int kFieldTypeAccountKeyFilterNoNotification = 2;
+constexpr int kFieldTypeBattery = 3;
+constexpr int kFieldTypeBatteryNoNotification = 4;
 
 bool ValidateInputSizes(const std::vector<uint8_t>& aes_key_bytes,
                         const std::vector<uint8_t>& encrypted_bytes) {
@@ -175,6 +178,18 @@ void FastPairDataParser::ParseNotDiscoverableAdvertisement(
                    &salt_bytes);
   }
 
+  std::vector<uint8_t> battery_bytes;
+  bool show_ui_for_battery = false;
+  if (field_indices.contains(kFieldTypeBattery)) {
+    CopyFieldBytes(service_data, field_indices, kFieldTypeBattery,
+                   &battery_bytes);
+    show_ui_for_battery = true;
+  } else if (field_indices.contains(kFieldTypeBatteryNoNotification)) {
+    CopyFieldBytes(service_data, field_indices, kFieldTypeBatteryNoNotification,
+                   &battery_bytes);
+    show_ui_for_battery = false;
+  }
+
   if (account_key_filter_bytes.empty()) {
     std::move(callback).Run(absl::nullopt);
   } else if (salt_bytes.size() != 1) {
@@ -183,7 +198,10 @@ void FastPairDataParser::ParseNotDiscoverableAdvertisement(
     std::move(callback).Run(absl::nullopt);
   } else {
     std::move(callback).Run(NotDiscoverableAdvertisement(
-        std::move(account_key_filter_bytes), show_ui, salt_bytes[0]));
+        std::move(account_key_filter_bytes), show_ui, salt_bytes[0],
+        battery_bytes.size() == 3
+            ? BatteryNotification::FromBytes(battery_bytes, show_ui_for_battery)
+            : absl::nullopt));
   }
 }
 
