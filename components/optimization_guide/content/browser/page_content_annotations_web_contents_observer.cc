3020ce9248a7f8cd47d00292d9fb55a3ae250ac2
Sophie Chang <sophiechang@chromium.org>
2021-10-01 04:37:52 +0000

    Annotate title text instead of waiting for content if same document navigation
    Bug: 1177102
    Change-Id: I731de8cbb28a841b67a206b18327f66a574517d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199190
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927098}

--- components/optimization_guide/content/browser/page_content_annotations_web_contents_observer.cc
+++ components/optimization_guide/content/browser/page_content_annotations_web_contents_observer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/content/browser/page_content_annotations_web_contents_observer.h"
 
 #include "base/bind.h"
+#include "base/strings/utf_string_conversions.h"
 #include "components/google/core/common/google_util.h"
 #include "components/optimization_guide/content/browser/page_content_annotations_service.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
@@ -43,6 +44,23 @@ PageContentAnnotationsWebContentsObserver::
 
 void PageContentAnnotationsWebContentsObserver::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInPrimaryMainFrame() ||
+      !navigation_handle->HasCommitted()) {
+    return;
+  }
+
+  optimization_guide::HistoryVisit history_visit = optimization_guide::
+      PageContentAnnotationsService::CreateHistoryVisitFromWebContents(
+          web_contents(), navigation_handle->GetNavigationId());
+
+  // TODO(crbug/1177102): Remove this title hack once the PageTextObserver works
+  // for same-document navigations.
+  if (navigation_handle->IsSameDocument()) {
+    // Annotate the title instead.
+    page_content_annotations_service_->Annotate(
+        history_visit, base::UTF16ToUTF8(web_contents()->GetTitle()));
+  }
+
   if (!optimization_guide::features::ShouldExtractRelatedSearches()) {
     return;
   }
@@ -51,16 +69,8 @@ void PageContentAnnotationsWebContentsObserver::DidFinishNavigation(
     return;
   }
 
-  if (!navigation_handle->IsInPrimaryMainFrame() ||
-      !navigation_handle->HasCommitted()) {
-    return;
-  }
-
-  page_content_annotations_service_->ExtractRelatedSearches(
-      optimization_guide::PageContentAnnotationsService::
-          CreateHistoryVisitFromWebContents(
-              web_contents(), navigation_handle->GetNavigationId()),
-      web_contents());
+  page_content_annotations_service_->ExtractRelatedSearches(history_visit,
+                                                            web_contents());
 }
 
 std::unique_ptr<PageTextObserver::ConsumerTextDumpRequest>
@@ -75,7 +85,8 @@ PageContentAnnotationsWebContentsObserver::MaybeRequestFrameTextDump(
   if (!navigation_handle->GetURL().SchemeIsHTTPOrHTTPS())
     return nullptr;
 
-  // TODO(crbug/1177102): Figure out how to deal with same document navigations.
+  if (navigation_handle->IsSameDocument())
+    return nullptr;
 
   std::unique_ptr<PageTextObserver::ConsumerTextDumpRequest> request =
       std::make_unique<PageTextObserver::ConsumerTextDumpRequest>();
