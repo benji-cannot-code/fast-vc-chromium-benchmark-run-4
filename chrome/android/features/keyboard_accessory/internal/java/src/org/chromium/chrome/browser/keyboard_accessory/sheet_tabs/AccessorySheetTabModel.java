7a9bbe5b34d6ffd4d75b1487c8345e474894798b
Vishwas Uppoor <vishwasuppoor@google.com>
2021-10-01 02:55:02 +0000

    Promo code offers in manual fallback
    This CL adds promo code offers to the manual fallback in the payments
    tab. The design doc and the UI can be found here
    (https://docs.google.com/document/d/1UdrHp4FkeERdP2EhhaQFv1rWw0muogdQS-9mXPXrz3c/edit?pli=1)
    
    ** The UX team decided to not show the `no_promo_codes_message`. If promo codes are unavailable, no change is made to the Payments tab. **
    
    Mocks(Icon next to promo code should be G logo instead of GPay): https://drive.google.com/file/d/1ynv0qEDVUogQhB4EmtSbgCf8drffNKNg/view?usp=sharing&resourcekey=0-pR899i2WRpBUzNil78t1-A
    Implementation: https://drive.google.com/file/d/14uaAi6jY4gI_CrdpzoLqaSCSO3unxw7J/view?usp=sharing&resourcekey=0-zP-LRrZSOh6tOo53cWPMxw
    
    Change-Id: Id408aca23fa13a69230aa72dc1cf2c2648b28011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3056580
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Maxim Kolosovskiy  <kolos@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#927084}

--- chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/AccessorySheetTabModel.java
+++ chrome/android/features/keyboard_accessory/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/AccessorySheetTabModel.java
@@ -26,7 +26,8 @@ class AccessorySheetTabModel extends ListModel<AccessorySheetTabModel.AccessoryS
      */
     static class AccessorySheetDataPiece {
         @IntDef({Type.TITLE, Type.PASSWORD_INFO, Type.ADDRESS_INFO, Type.CREDIT_CARD_INFO,
-                Type.TOUCH_TO_FILL_INFO, Type.FOOTER_COMMAND, Type.WARNING, Type.OPTION_TOGGLE})
+                Type.TOUCH_TO_FILL_INFO, Type.FOOTER_COMMAND, Type.WARNING, Type.OPTION_TOGGLE,
+                Type.PROMO_CODE_INFO})
         @Retention(RetentionPolicy.SOURCE)
         @interface Type {
             /**
@@ -63,6 +64,10 @@ class AccessorySheetTabModel extends ListModel<AccessorySheetTabModel.AccessoryS
              * previously disabled.
              */
             int OPTION_TOGGLE = 8;
+            /**
+             * A section containing a promo code info.
+             */
+            int PROMO_CODE_INFO = 9;
         }
 
         private Object mDataPiece;
