dd6d037c9d3f5f730185d8b464ad09160ec6ffce
Igor Ruvinov <igorruvinov@chromium.org>
2021-10-01 18:14:26 +0000

    Add manifest version to the extension info reporting proto.
    Each extension's manifest version is included in the extension info
    report sent to the DMServer.
    
    This change is implemented behind a feature flag which will be removed
    once server-side work is complete.
    
    Bug: 1204508, b/201559564
    Change-Id: I240c091ed317400a449e72d9212aaf7d8a1a493c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197990
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927294}

--- chrome/browser/enterprise/reporting/extension_info_unittest.cc
+++ chrome/browser/enterprise/reporting/extension_info_unittest.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise/reporting/extension_info.h"
 
+#include "base/feature_list.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_service_test_base.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/testing_profile.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/manifest_constants.h"
@@ -29,6 +31,7 @@ const char kPermission1[] = "alarms";
 const char kPermission2[] = "idle";
 const char kPermission3[] = "*://*.example.com/*";
 const char kAppLaunchUrl[] = "https://www.example.com/";
+const int kManifestVersion = 2;
 
 }  // namespace
 
@@ -52,6 +55,8 @@ class ExtensionInfoTest : public extensions::ExtensionServiceTestBase {
         .SetVersion(kVersion)
         .SetManifestKey(extensions::manifest_keys::kDescription, kDescription)
         .SetManifestKey(extensions::manifest_keys::kHomepageURL, kHomepage)
+        .SetManifestKey(extensions::manifest_keys::kManifestVersion,
+                        kManifestVersion)
         .SetLocation(location)
         .AddPermission(kPermission1)
         .AddPermission(kPermission2)
@@ -67,6 +72,8 @@ class ExtensionInfoTest : public extensions::ExtensionServiceTestBase {
     service()->AddExtension(extension.get());
     return extension;
   }
+
+  base::test::ScopedFeatureList feature_list_;
 };
 
 TEST_F(ExtensionInfoTest, ExtensionReport) {
@@ -82,6 +89,9 @@ TEST_F(ExtensionInfoTest, ExtensionReport) {
   EXPECT_EQ(kName, actual_extension_report.name());
   EXPECT_EQ(kVersion, actual_extension_report.version());
   EXPECT_EQ(kDescription, actual_extension_report.description());
+  // Manifest version reporting flag is not enabled, so the manifest version is
+  // not passed into the extension info proto.
+  EXPECT_EQ(0, actual_extension_report.manifest_version());
 
   EXPECT_EQ(em::Extension_ExtensionType_TYPE_EXTENSION,
             actual_extension_report.app_type());
@@ -98,6 +108,26 @@ TEST_F(ExtensionInfoTest, ExtensionReport) {
   EXPECT_EQ(kPermission2, actual_extension_report.permissions(1));
   EXPECT_EQ(1, actual_extension_report.host_permissions_size());
   EXPECT_EQ(kPermission3, actual_extension_report.host_permissions(0));
+}
+
+TEST_F(ExtensionInfoTest, ExtensionReport_ManifestVersionEnabled) {
+  // Enable the manifest version reporting flag so that the actual manifest
+  // version is passed into the extension info proto.
+  feature_list_.InitAndEnableFeature(
+      features::kEnterpriseReportingExtensionManifestVersion);
+  auto extension = BuildExtension();
+
+  em::ChromeUserProfileInfo info;
+  AppendExtensionInfoIntoProfileReport(profile(), &info);
+
+  EXPECT_EQ(1, info.extensions_size());
+  const em::Extension actual_extension_report = info.extensions(0);
+
+  EXPECT_EQ(kId, actual_extension_report.id());
+  EXPECT_EQ(kName, actual_extension_report.name());
+  EXPECT_EQ(kVersion, actual_extension_report.version());
+  EXPECT_EQ(kDescription, actual_extension_report.description());
+  EXPECT_EQ(kManifestVersion, actual_extension_report.manifest_version());
 }
 
 TEST_F(ExtensionInfoTest, MultipleExtensions) {
