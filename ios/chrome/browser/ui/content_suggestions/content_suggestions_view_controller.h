d1c040d95bfdbd1fd7ccfe254e88c4cee062d90d
Sergio Collazos <sczs@chromium.org>
2021-10-01 22:54:01 +0000

    Reland "[ios] Removes Zine implementation 4/N"
    Removes Overscroll, ThumbStripSupporting and ContentSuggestionsCollectionControlling
    usage from ContentSuggestionsVC.
    
    Bug: 1200303
    Change-Id: Ie78e18a9930721ef4282636411750545e61303e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197940
    Reviewed-by: Adam Trudeau-Arcaro <adamta@google.com>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927428}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.h
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.h
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/ui/collection_view/collection_view_controller.h"
-#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_controlling.h"
-#import "ios/chrome/browser/ui/thumb_strip/thumb_strip_supporting.h"
 
 @class BubblePresenter;
 @class ContentSuggestionsSectionInformation;
@@ -22,16 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol ContentSuggestionsViewControllerAudience;
 @protocol DiscoverFeedHeaderChanging;
 @protocol DiscoverFeedMenuCommands;
-@protocol OverscrollActionsControllerDelegate;
 @protocol SnackbarCommands;
 @protocol SuggestedContent;
 @protocol ThemeChangeDelegate;
-@class ViewRevealingVerticalPanHandler;
 
 // CollectionViewController to display the suggestions items.
-@interface ContentSuggestionsViewController
-    : CollectionViewController <ContentSuggestionsCollectionControlling,
-                                ThumbStripSupporting>
+@interface ContentSuggestionsViewController : CollectionViewController
 
 // Inits view controller with |style|.
 - (instancetype)initWithStyle:(CollectionViewControllerStyle)style
@@ -50,9 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, readonly)
     CollectionViewModel<CollectionViewItem<SuggestedContent>*>*
         collectionViewModel;
-// Delegate for the overscroll actions.
-@property(nonatomic, weak) id<OverscrollActionsControllerDelegate>
-    overscrollDelegate;
 // Delegate for handling theme changes (dark/light theme).
 @property(nonatomic, weak) id<ThemeChangeDelegate> themeChangeDelegate;
 @property(nonatomic, weak) id<DiscoverFeedMenuCommands> discoverFeedMenuHandler;
@@ -71,10 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Provider of menu configurations for the contentSuggestions component.
 @property(nonatomic, weak) id<ContentSuggestionsMenuProvider> menuProvider;
 
-// The pan gesture handler to notify of scroll events happening in this view
-// controller.
-@property(nonatomic, weak) ViewRevealingVerticalPanHandler* panGestureHandler;
-
 // Bubble presenter for displaying IPH bubbles relating to the NTP.
 @property(nonatomic, strong) BubblePresenter* bubblePresenter;
 
@@ -90,10 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)addSuggestions:
             (NSArray<CollectionViewItem<SuggestedContent>*>*)suggestions
          toSectionInfo:(ContentSuggestionsSectionInformation*)sectionInfo;
-// Updates the constraints of the collection.
-- (void)updateConstraints;
-// Clear the overscroll actions.
-- (void)clearOverscroll;
 
 @end
 
