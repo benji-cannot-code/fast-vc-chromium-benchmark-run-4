c54e07da50364b810be49b4ae40c4ce66162818e
Nico Weber <thakis@chromium.org>
2021-10-01 20:06:59 +0000

    win: Replace INSTANTIATE_TEST_CASE_P with INSTANTIATE_TEST_SUITE_P
    They do the same thing, but the former is deprecated.
    
    Bug: 1255114,925652
    Change-Id: I6bef10f16bb44d7dbe6e28d58074a277e1ae6059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198739
    Owners-Override: Nico Weber <thakis@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927359}

--- sandbox/win/src/app_container_test.cc
+++ sandbox/win/src/app_container_test.cc
@@ -952,7 +952,7 @@ TEST_P(SocketBrokerTest, SocketBrokerTestTCP) {
                           .c_str()));
 }
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     AppContainerBrokered,
     SocketBrokerTest,
     ::testing::Combine(
@@ -960,7 +960,7 @@ INSTANTIATE_TEST_CASE_P(
         /* using socket brokering */ ::testing::Values(true),
         /* connect to real adapter */ ::testing::Values(true, false),
         /* add brokering rule */ ::testing::Values(true, false)));
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     AppContainerNonBrokered,
     SocketBrokerTest,
     ::testing::Combine(
@@ -968,7 +968,7 @@ INSTANTIATE_TEST_CASE_P(
         /* using socket brokering */ ::testing::Values(false),
         /* connect to real adapter */ ::testing::Values(true, false),
         /* add brokering rule */ ::testing::Values(true, false)));
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     NoAppContainerNonBrokered,
     SocketBrokerTest,
     ::testing::Combine(
