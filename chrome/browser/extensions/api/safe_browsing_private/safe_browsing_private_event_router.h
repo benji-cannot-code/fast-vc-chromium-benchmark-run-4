983a3bad9c8bdaf85daadfea4a48ac71cf534711
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2021-10-01 16:27:13 +0000

    Add login username field to enterprise login event reporting
    This field was mistakenly omitted in https://chromium-review.googlesource.com/c/chromium/src/+/3086144 and https://chromium-review.googlesource.com/c/chromium/src/+/3137574
    
    Bug: 1238577
    Change-Id: I8b938c799fb534101c3472b26a228d45808cab52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197993
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: anthonyvd <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927236}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
@@ -89,6 +89,7 @@ class SafeBrowsingPrivateEventRouter
   static const char kKeyScanId[];
   static const char kKeyIsFederated[];
   static const char kKeyFederatedOrigin[];
+  static const char kKeyLoginUserName[];
   static const char kKeyPasswordBreachIdentities[];
   static const char kKeyPasswordBreachIdentitiesUrl[];
   static const char kKeyPasswordBreachIdentitiesUsername[];
@@ -230,7 +231,8 @@ class SafeBrowsingPrivateEventRouter
 
   void OnLoginEvent(const GURL& url,
                     bool is_federated,
-                    const url::Origin& federated_origin);
+                    const url::Origin& federated_origin,
+                    const std::u16string& username);
 
   void OnPasswordBreach(
       const std::string& trigger,
