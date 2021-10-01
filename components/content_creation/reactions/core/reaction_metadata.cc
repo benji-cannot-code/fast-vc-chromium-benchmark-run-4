c6a282f757e3b1ce7b4afb707ae04fa39008ce78
Guillaume Jenkins <gujen@google.com>
2021-10-01 12:35:34 +0000

    [LR] Add metadata classes
    Adds metadata classes to hold information about a Lightweight Reaction,
    both on the native side and the Java side.
    
    Also adds the enum that will hold the possible reaction types, with
    some build rules to generate the equivalent @IntDef values on the
    Java side.
    
    Bug: 1253535
    Change-Id: I0f9e42fa74e0f6a5092b06c7dffdd181999bdfa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193315
    Reviewed-by: Sophey <sophey@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927181}

---
+++ components/content_creation/reactions/core/reaction_metadata.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/content_creation/reactions/core/reaction_metadata.h"
+
+namespace content_creation {
+
+ReactionMetadata::ReactionMetadata(ReactionType type,
+                                   const std::string& localized_name,
+                                   const std::string& thumbnail_url,
+                                   const std::string& asset_url)
+    : type_(type),
+      localized_name_(localized_name),
+      thumbnail_url_(thumbnail_url),
+      asset_url_(asset_url) {}
+
+ReactionMetadata::~ReactionMetadata() = default;
+
+}  // namespace content_creation
