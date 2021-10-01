390a99c01737e78f0183ed31fda11095f86329d3
Guillaume Jenkins <gujen@google.com>
2021-10-01 18:16:22 +0000

    [LR] Add basic conversion bridge for reaction metadata
    Adds the skeleton of the native and Java conversion bridges for the new
    ReactionMetadata class. For now the bridges aren't used and they are not
    very useful because the feature will never convert reaction metadata
    objects individually.
    
    Bug: 1253535
    Change-Id: I6a679eb85bc55f49dbbee5c3a4e3d8f17565a4e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197458
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Sophey <sophey@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927296}

---
+++ components/content_creation/reactions/android/reaction_metadata_conversion_bridge.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/content_creation/reactions/android/reaction_metadata_conversion_bridge.h"
+
+#include "base/android/jni_string.h"
+#include "components/content_creation/reactions/android/jni_headers/ReactionMetadataConversionBridge_jni.h"
+#include "components/content_creation/reactions/core/reaction_types.h"
+
+namespace content_creation {
+
+using base::android::ConvertUTF8ToJavaString;
+using base::android::ScopedJavaLocalRef;
+
+// static
+ScopedJavaLocalRef<jobject>
+ReactionMetadataConversionBridge::CreateJavaReactionMetadata(
+    JNIEnv* env,
+    const ReactionMetadata& metadata) {
+  return Java_ReactionMetadataConversionBridge_createReactionMetadata(
+      env, static_cast<uint32_t>(metadata.type()),
+      ConvertUTF8ToJavaString(env, metadata.localized_name()),
+      ConvertUTF8ToJavaString(env, metadata.thumbnail_url()),
+      ConvertUTF8ToJavaString(env, metadata.asset_url()));
+}
+
+}  // namespace content_creation
