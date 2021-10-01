3d8722e5036e3ac6d5c883c952d6c6f3e2363ddd
Etienne Pierre-doray <etiennep@chromium.org>
2021-10-01 16:26:27 +0000

    [ThreadPool]: Replace CONTINUE_ON_SHUTDOWN workers on shutdown.
    This is ensuring there will be an enough worker in the
    thread pool if there are still pending BLOCK_SHUTDOWN tasks.
    
    The problem we observed is that CONTINUE_ON_SHUTDOWN tasks are
    scheduled before the shutdown is started. Then, when shutdown is
    initiated, there is no available worker to run any BLOCK_SHUTDOWN
    tasks and the CONTINUE_ON_SHUTDOWN tasks needs to continue until
    their completion.
    
    The solution is to replace workers running CONTINUE_ON_SHUTDOWN tasks
    (similar to handling WILL_BLOCK). Such a task will allow
    max_tasks/max_best_effort_tasks to increase and create new workers for
    pending BLOCK_SHUTDOWN tasks. Note that we don't explicitly consider
    shutdown in GetDesiredNumAwakeWorkersLockRequired(), so we'd actually
    create workers for all pending tasks, limited to max_tasks.
    
    This aim to solve the same problem as
    https://chromium-review.googlesource.com/c/chromium/src/+/3085974
    but this solution is preferred because it only acts during shutdown
    and covers tasks that don't have a ScopedBlockingCall. That CL is
    effectively reverted here to implement the preferred solution
    
    Bug: 1227930
    Change-Id: I98a24bfc8264833f20ef9f679a8b5458925b836e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3072339
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927235}

--- base/task/thread_pool/thread_group_native.cc
+++ base/task/thread_pool/thread_group_native.cc
@@ -182,5 +182,7 @@ void ThreadGroupNative::DidUpdateCanRunPolicy() {
   EnsureEnoughWorkersLockRequired(&executor);
 }
 
+void ThreadGroupNative::OnShutdownStarted() {}
+
 }  // namespace internal
 }  // namespace base
