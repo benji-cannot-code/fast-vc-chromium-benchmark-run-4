2205c919960d0321a2c87cb291080c447643115a
Sammie Quon <sammiequon@chromium.org>
2021-10-01 00:27:18 +0000

    desks: Expose desks trackpad experiment to chrome://flags.
    Bug: 1191545
    Change-Id: I667678caddc795d46e28906ce378afffb26c0922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198706
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927025}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2370,6 +2370,9 @@ extern const char kButtonARCNetworkDiagnosticsDescription[];
 extern const char kCalendarViewName[];
 extern const char kCalendarViewDescription[];
 
+extern const char kDesksTrackpadSwipeImprovementsName[];
+extern const char kDesksTrackpadSwipeImprovementsDescription[];
+
 extern const char kPreferConstantFrameRateName[];
 extern const char kPreferConstantFrameRateDescription[];
 
