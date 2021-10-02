fb2459011234f58ca8ffc6c28d46cbb5e19527dc
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-10-02 00:08:26 +0000

    Import wpt@46bb74d1b82ce7daf2cc101eaa2032833291272c
    Using wpt-import in Chromium a3679119428db9d09810f66e9207d26450acd523.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Change-Id: I5268874d9259425fe564c4633925a95f2edcdb15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199539
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#927463}

---
+++ third_party/blink/web_tests/external/wpt/xhr/status.h2.window.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// See also /fetch/api/basic/status.h2.any.js
+
+[
+  200,
+  210,
+  400,
+  404,
+  410,
+  500,
+  502
+].forEach(status => {
+  async_test(t => {
+    const client = new XMLHttpRequest();
+    client.open("GET", "/xhr/resources/status.py?code=" + status);
+    client.send();
+    client.onload = t.step_func_done(() => {
+      assert_equals(client.status, status, "status should be " + status);
+      assert_equals(client.statusText, "", "statusText should be the empty string");
+    });
+  }, "statusText over H2 for status " + status + " should be the empty string");
+});
