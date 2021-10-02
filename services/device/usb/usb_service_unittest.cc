5696269ec2919dc02872c3258acb46e4a5860ba4
Reilly Grant <reillyg@chromium.org>
2021-10-02 01:19:39 +0000

    Reland "Remove libusb dependency from Windows"
    This reverts commit 0b6b7ef9940d7c3c7dcfb4f8c07069a1c529a66b.
    
    Reason for revert: Fixed DLL loading issues.
    
    Original change's description:
    > Revert "Remove libusb dependency from Windows"
    >
    > This reverts commit c36de0a232f6b13867c10c80a82d1fd400c48685.
    >
    > Reason for revert: Suspect for crbug.com/1236291
    >
    > Original change's description:
    > > Remove libusb dependency from Windows
    > >
    > > The kNewUsbBackend flag has been enabled by default since M-87 and the
    > > about:flags entry has been removed in M-92. Now that M-93 has branched
    > > this code can be removed.
    > >
    > > Bug: 637404
    > > Change-Id: I18242aa752316716e33173db3a0a888bf38470b2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2531186
    > > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > > Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#907715}
    >
    > Bug: 637404, 1236291
    > Change-Id: I65291ec071887383908c4ffdc4b621a183d5027a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069733
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Austin Eng <enga@google.com>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Owners-Override: Austin Eng <enga@google.com>
    > Auto-Submit: Austin Eng <enga@google.com>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#908302}
    
    Bug: 637404, 1236291
    Change-Id: I58bf48341db0b6c0c399c56c47256068b62270e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198944
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927489}

--- services/device/usb/usb_service_unittest.cc
+++ services/device/usb/usb_service_unittest.cc
@@ -55,7 +55,7 @@ TEST_F(UsbServiceTest, GetDevices) {
   }
 }
 
-#if defined(OS_WIN)
+#if defined(OS_MAC)
 TEST_F(UsbServiceTest, GetDevicesNewBackend) {
   base::test::ScopedFeatureList features;
   features.InitAndEnableFeature(device::kNewUsbBackend);
@@ -67,7 +67,7 @@ TEST_F(UsbServiceTest, GetDevicesNewBackend) {
     loop.Run();
   }
 }
-#endif  // defined(OS_WIN)
+#endif  // defined(OS_MAC)
 
 TEST_F(UsbServiceTest, ClaimGadget) {
   if (!UsbTestGadget::IsTestEnabled() || !usb_service_)
