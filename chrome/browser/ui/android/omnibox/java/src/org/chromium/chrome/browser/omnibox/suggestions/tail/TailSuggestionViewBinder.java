1435751c86ec0a3ef10686caee5c9daa3641aabc
Sinan Sahin <sinansahin@google.com>
2021-10-02 18:37:22 +0000

    [GMNext] Fix crash in TailSuggestionViewBinder
    The result of OmniboxResourceProvider#getSuggestionPrimaryTextColor, a
    ColorInt, was being passed to Resources#getColor(), expects ColorRes,
    even though it should be used directly. This CL fixes this by removing
    the getColor() part.
    
    Bug: 1255299
    Change-Id: I1af8c65e2b6caef1ceacc8c4bda31aeb33e051ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200559
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#927529}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/tail/TailSuggestionViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/tail/TailSuggestionViewBinder.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.omnibox.suggestions.tail;
 
 import androidx.annotation.ColorInt;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.browser.omnibox.styles.OmniboxResourceProvider;
 import org.chromium.chrome.browser.omnibox.suggestions.SuggestionCommonProperties;
 import org.chromium.ui.modelutil.PropertyKey;
@@ -26,7 +25,7 @@ public class TailSuggestionViewBinder {
         } else if (propertyKey == SuggestionCommonProperties.OMNIBOX_THEME) {
             final @ColorInt int color = OmniboxResourceProvider.getSuggestionPrimaryTextColor(
                     view.getContext(), model.get(SuggestionCommonProperties.OMNIBOX_THEME));
-            view.setTextColor(ApiCompatibilityUtils.getColor(view.getResources(), color));
+            view.setTextColor(color);
         }
     }
 }
