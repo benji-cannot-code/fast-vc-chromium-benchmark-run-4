0d0abdc9dacfdc352c5c9a0b04fa00002a15bd97
Nate Fischer <ntfschr@chromium.org>
2021-10-02 02:00:43 +0000

    Python: fix print statements in stack.py
    No change to logic. This fixes an issue introduced in
    https://crrev.com/c/3065476. That CL converted all python2 print
    statements to use parentheses, however `print()` will literally print
    `()` (it's a print statement with an empty tuple as an argument). As a
    quick fix, import the actual print_function to get the correct behavior.
    This will have no effect on python3 since the print function is builtin.
    
    Bug: 1223892
    Test: third_party/android_platform/development/scripts/stack --help
    Change-Id: I1e2e7eb58c77b7ab4449e28932ff05a9b3598a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200060
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927499}

--- third_party/android_platform/development/scripts/stack.py
+++ third_party/android_platform/development/scripts/stack.py
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """stack symbolizes native crash dumps."""
 
+from __future__ import print_function
+
 import getopt
 import glob
 import logging
