ee74abd4dfb028145de449264e33eb7f8b042c58
Weizhong Xia <weizhong@google.com>
2021-10-03 03:59:08 +0000

    Make wpt-importer python3 compatible
    Unit tests are also converted
    
    Bug: 1254879
    Change-Id: I6ff43f5cc82549ab3b425ced87cce2abe7e8510b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198329
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Kyle Ju <kyleju@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/main@{#927551}

--- third_party/blink/tools/blinkpy/w3c/monorail_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/monorail_unittest.py
@@ -32,20 +32,20 @@ class MonorailIssueTest(unittest.TestCase):
             'chromium',
             summary=u'test',
             status='Untriaged',
-            description=u'ABC~‾¥≈¤･・•∙·☼★星🌟星★☼·∙•・･¤≈¥‾~XYZ',
+            description='ABC~‾¥≈¤･・•∙·☼★星🌟星★☼·∙•・･¤≈¥‾~XYZ',
             cc=['foo@chromium.org', 'bar@chromium.org'],
             labels=['Flaky'],
             components=['Infra'])
-        self.assertEqual(type(unicode(issue)), unicode)
+        self.assertEqual(type(str(issue)), str)
         self.assertEqual(
-            unicode(issue),
-            (u'Monorail issue in project chromium\n'
-             u'Summary: test\n'
-             u'Status: Untriaged\n'
-             u'CC: foo@chromium.org, bar@chromium.org\n'
-             u'Components: Infra\n'
-             u'Labels: Flaky\n'
-             u'Description:\nABC~‾¥≈¤･・•∙·☼★星🌟星★☼·∙•・･¤≈¥‾~XYZ\n'))
+            str(issue),
+            ('Monorail issue in project chromium\n'
+             'Summary: test\n'
+             'Status: Untriaged\n'
+             'CC: foo@chromium.org, bar@chromium.org\n'
+             'Components: Infra\n'
+             'Labels: Flaky\n'
+             'Description:\nABC~‾¥≈¤･・•∙·☼★星🌟星★☼·∙•・･¤≈¥‾~XYZ\n'))
 
     def test_init_unknown_fields(self):
         with self.assertRaises(AssertionError):
