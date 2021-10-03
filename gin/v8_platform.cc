b1dc45c73f3a27748f629481e9d727d7e68647b5
Anton Bikineev <bikineev@chromium.org>
2021-10-03 12:26:27 +0000

    gin: Provide custom non-quarantinable allocator for V8 zones
    PCScan-in-the-renderer project avoids quarantining and scanning V8
    zones. That CL implements a custom ZoneBackingAllocator that wires
    allocations to PA's non-quarantinable partition.
    
    Bug: 1249550
    Change-Id: I053d6ee5c2f9b4fadf66e301006701e7d6aca534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177660
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927557}

--- gin/v8_platform.cc
+++ gin/v8_platform.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/debug/stack_trace.h"
 #include "base/location.h"
+#include "base/memory/nonscannable_memory.h"
 #include "base/rand_util.h"
 #include "base/system/sys_info.h"
 #include "base/task/post_job.h"
@@ -374,6 +375,16 @@ void V8Platform::OnCriticalMemoryPressure() {
 #if defined(OS_WIN) && defined(ARCH_CPU_32_BITS)
   base::ReleaseReservation();
 #endif
+}
+
+v8::ZoneBackingAllocator* V8Platform::GetZoneBackingAllocator() {
+  static struct Allocator final : v8::ZoneBackingAllocator {
+    MallocFn GetMallocFn() const override {
+      return &base::AllocNonQuarantinable;
+    }
+    FreeFn GetFreeFn() const override { return &base::FreeNonQuarantinable; }
+  } allocator;
+  return &allocator;
 }
 #endif  // BUILDFLAG(USE_PARTITION_ALLOC)
 
