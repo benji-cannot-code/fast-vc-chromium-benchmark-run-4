5f9785dfad59118e557e08a96b9b3b67607ed217
Vincent Boisselle <vincb@google.com>
2021-10-03 01:18:48 +0000

    [iOS][ForcedSignIn]🏢 Present forced sign-in screen on active view ctl
    Currently, the forced sign-in screen can only be presented on the
    browser view controllers (incognito tab or regular tab), because
    interfaceProvider.currentInterface.viewController only provides the
    browser view controller used on the current browser interface.
    
    TabGridViewCoordinator.activeViewController provides the view controller
    that is really active. For example, activeViewController will provide
    TabGridViewController when the active view is the regular tab switcher.
    
    Tested on each surface: incognito tab, incognito tab switcher, regular
    tab, regular tab switcher, and recent tabs. See
    http://shortn/_fBkQ0PBzFB.
    
    Bug: 1250761
    Change-Id: I3008535b6f715a704c2d6592de6609cb4d58ee21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3188491
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#927547}

--- ios/chrome/browser/ui/main/scene_controller.h
+++ ios/chrome/browser/ui/main/scene_controller.h
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // YES if the scene is presenting the signin view.
 @property(nonatomic, readonly) BOOL isPresentingSigninView;
 
+// The view controller that is active. Can be either a BrowserViewController or
+// TabGridViewController.
+@property(nonatomic, readonly) UIViewController* activeViewController;
+
 // Handler for the UIWindowSceneDelegate callback with the same selector.
 - (void)performActionForShortcutItem:(UIApplicationShortcutItem*)shortcutItem
                    completionHandler:
