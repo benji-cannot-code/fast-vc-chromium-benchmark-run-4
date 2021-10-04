1a9bbad1e17c6d337d4d9a360c7108763430aeee
Rune Lillesveen <futhark@chromium.org>
2021-10-04 12:43:57 +0000

    Content property on html/body creates LayoutImage
    We support replacing real elements with an image from the content
    property. This affects whether a LayoutObject reports having containment
    applied or not. Add this check to the ComputedStyle based containment
    application check.
    
    Fixes DCHECK failure in BODY/HTML propagation.
    
    Bug: 1254076
    Change-Id: Ie6bbd0f87ccbb676bbc88739a15a67312ff1b03a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199929
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927649}

--- third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/text.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/html/html_style_element.h"
+#include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 
@@ -1531,6 +1532,22 @@ TEST_F(StyleResolverTest, CascadeLayersAndPageRules) {
 
   // The layered declaraion should win the cascading.
   EXPECT_EQ(100, description.margin_top);
+}
+
+TEST_F(StyleResolverTest, BodyPropagationLayoutImageContain) {
+  ScopedCSSContainedBodyPropagationForTest enable_scope(true);
+  GetDocument().documentElement()->setAttribute(
+      html_names::kStyleAttr,
+      "contain:size; display:inline-table; content:url(img);");
+  GetDocument().body()->SetInlineStyleProperty(CSSPropertyID::kBackgroundColor,
+                                               "red");
+
+  // Should not trigger DCHECK
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_EQ(Color::kTransparent,
+            GetDocument().GetLayoutView()->StyleRef().VisitedDependentColor(
+                GetCSSPropertyBackgroundColor()));
 }
 
 }  // namespace blink
