087b0730d3ea9ac8fa7eb45d3a1ea82e4f76da00
Julie Jeongeun Kim <jkim@igalia.com>
2021-10-04 06:39:56 +0000

    [mojo] Keep ScopedDepthTracker only at ValidateStruct()
    This CL removes ScopedDepthTracker from ValidateInlinedUnion(),
    ValidateContainer(), and ValidateNonInlinedUnion() in order to
    avoid redundant recursive depth checking. If the validation
    runs with base::Value type, it increases `stack_depth_` from
    ValidationContext redundantly.
    
    Bug: 1246320
    Change-Id: Ifbf400cb7a44ce18310a078a04e4b95799ae4cd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3154109
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927602}

--- mojo/public/cpp/bindings/lib/validation_util.h
+++ mojo/public/cpp/bindings/lib/validation_util.h
@@ -185,8 +185,6 @@ template <typename T>
 bool ValidateContainer(const Pointer<T>& input,
                        ValidationContext* validation_context,
                        const ContainerValidateParams* validate_params) {
-  ValidationContext::ScopedDepthTracker depth_tracker(validation_context);
-
   return ValidateParams(input, validation_context) &&
          T::Validate(input.Get(), validation_context, validate_params);
 }
@@ -203,7 +201,6 @@ bool ValidateStruct(const Pointer<T>& input,
 template <typename T>
 bool ValidateInlinedUnion(const T& input,
                           ValidationContext* validation_context) {
-  ValidationContext::ScopedDepthTracker depth_tracker(validation_context);
   if (validation_context->ExceedsMaxDepth()) {
     ReportValidationError(validation_context,
                           VALIDATION_ERROR_MAX_RECURSION_DEPTH);
@@ -215,8 +212,6 @@ bool ValidateInlinedUnion(const T& input,
 template <typename T>
 bool ValidateNonInlinedUnion(const Pointer<T>& input,
                              ValidationContext* validation_context) {
-  ValidationContext::ScopedDepthTracker depth_tracker(validation_context);
-
   return ValidateParams(input, validation_context) &&
          T::Validate(input.Get(), validation_context, false);
 }
