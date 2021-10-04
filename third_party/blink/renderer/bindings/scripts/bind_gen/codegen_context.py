31e3b91e88101515462f9da86e6e3a86f5925ca4
Yuki Shiino <yukishiino@chromium.org>
2021-10-04 05:39:28 +0000

    binding: Implement ObservableArray<T> (3 of N)
    Adds basic support of observable arrays in bind_gen/.  This is
    mostly just a preparation for the coming
    bind_gen/observable_array.py in a follow-up patch.
    
    Bug: 1201744
    Change-Id: I874c699f896633f7b4c8cce5e221e3ffd6351372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198015
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927594}

--- third_party/blink/renderer/bindings/scripts/bind_gen/codegen_context.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/codegen_context.py
@@ -60,6 +60,7 @@ class CodeGenContext(object):
             "enumeration": None,
             "interface": None,
             "namespace": None,
+            "observable_array": None,
             "typedef": None,
             "union": None,
 
