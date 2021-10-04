6266b815b4524929690785afc98c5c20ad3f1283
dpapad <dpapad@chromium.org>
2021-10-04 18:17:30 +0000

    Certificate Manager: Migrate from I18nBehavior to I18nMixin.
    Bug: 1184053
    Change-Id: I7454eafecca91bb1d89170a071fccb0197162d4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199673
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927752}

--- ui/webui/resources/cr_components/certificate_manager/certificate_password_decryption_dialog.ts
+++ ui/webui/resources/cr_components/certificate_manager/certificate_password_decryption_dialog.ts
@@ -12,10 +12,10 @@ import '../../cr_elements/cr_dialog/cr_dialog.m.js';
 import '../../cr_elements/cr_input/cr_input.m.js';
 import './certificate_shared_css.js';
 
-import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {CrDialogElement} from '../../cr_elements/cr_dialog/cr_dialog.m.js';
-import {I18nBehavior} from '../../js/i18n_behavior.m.js';
+import {I18nMixin} from '../../js/i18n_mixin.js';
 
 import {CertificatesBrowserProxy, CertificatesBrowserProxyImpl} from './certificates_browser_proxy.js';
 
@@ -26,8 +26,7 @@ export interface CertificatePasswordDecryptionDialogElement {
 }
 
 const CertificatePasswordDecryptionDialogElementBase =
-    mixinBehaviors([I18nBehavior], PolymerElement) as
-    {new (): PolymerElement & I18nBehavior};
+    I18nMixin(PolymerElement);
 
 export class CertificatePasswordDecryptionDialogElement extends
     CertificatePasswordDecryptionDialogElementBase {
