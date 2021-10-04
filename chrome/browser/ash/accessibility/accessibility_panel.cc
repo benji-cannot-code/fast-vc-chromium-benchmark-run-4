693c5a7fae92133c3fd9fb84110d2514b8c76ea5
Sean Kau <skau@chromium.org>
2021-10-04 16:55:34 +0000

    Make the Window for ChromeVoxPanel clip to bounds
    Setting the ChromeVoxPanel to clip to bounds causes it to not
    participate in window targeting when the height is set to 0. Otherwise,
    the children of the panel can still be selected when the panel is
    occluded in full screen. We do not use Hide() because we rely on
    the ChromeVoxPanel being able to receive keyboard events.
    
    Bug: 1251129
    Change-Id: Iff768961efa48d79815d3d915f70d4ba3f982158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200183
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927712}

--- chrome/browser/ash/accessibility/accessibility_panel.cc
+++ chrome/browser/ash/accessibility/accessibility_panel.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/view_type_utils.h"
 #include "extensions/common/mojom/view_type.mojom.h"
+#include "ui/compositor/layer.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/views/controls/webview/webview.h"
@@ -79,6 +80,10 @@ AccessibilityPanel::AccessibilityPanel(content::BrowserContext* browser_context,
   params.name = widget_name;
   params.shadow_elevation = wm::kShadowElevationInactiveWindow;
   widget_->Init(std::move(params));
+  // We rely on being able to set the bounds of the panel to control when it
+  // captures input rather than hiding the widget because we need to continue to
+  // receive key events. crbug.com/1251129.
+  widget_->GetNativeWindow()->layer()->SetMasksToBounds(true);
 }
 
 AccessibilityPanel::~AccessibilityPanel() = default;
