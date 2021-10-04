8274b777e131251dfd9ddc7e3320d90fd75bfb24
Harry Cutts <hcutts@chromium.org>
2021-10-04 19:17:34 +0000

    Diagnostics: add a simple keyboard diagram
    This is the beginning of an interactive keyboard diagram to be used for
    internal keyboards. In future it will show the appropriate keys and
    labels for the device's physical keyboard and toggle their pressed state
    when the user presses keys, but this CL lays out the basic grid-based
    diagram.
    
    Screenshot: http://shortn/_b5piuj30fb
    (The "backspace" text overflowing is a known issue — not worth fixing
    until the dialogue and diagram sizing is worked out later on.)
    
    Bug: 1207678
    Change-Id: Iea1a1594bacf803704be0e82b1520476b7abec1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3193505
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927779}

---
+++ ash/webui/common/resources/keyboard_diagram.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './keyboard_key.js'
+
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+/**
+ * @fileoverview
+ * 'keyboard-diagram' displays a diagram of a CrOS-style keyboard.
+ */
+
+export class KeyboardDiagramElement extends PolymerElement {
+  static get is() {
+    return 'keyboard-diagram';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+}
+
+customElements.define(KeyboardDiagramElement.is, KeyboardDiagramElement);
