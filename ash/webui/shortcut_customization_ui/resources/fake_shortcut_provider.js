eb50c031d9a610ce8dac1963edc9221f2b8a18a3
Jimmy Gong <jimmyxgong@google.com>
2021-10-04 20:02:14 +0000

    shortcuts: Add RemoveAccelerator to FakeShortcutProvider
    Bug: 1060690
    Test: --gtest_filter=ShortcutCustomizationAppBrowserTest.\
    MANUAL_FakeShortcut*
    
    Change-Id: I8543fc445c41547b0329c3c4cd825f4cb560f8f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177682
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927798}

--- ash/webui/shortcut_customization_ui/resources/fake_shortcut_provider.js
+++ ash/webui/shortcut_customization_ui/resources/fake_shortcut_provider.js
@@ -23,6 +23,7 @@ export class FakeShortcutProvider {
     this.methods_.register('isMutable');
     this.methods_.register('addUserAccelerator');
     this.methods_.register('replaceAccelerator');
+    this.methods_.register('removeAccelerator');
   }
 
   /**
@@ -71,6 +72,18 @@ export class FakeShortcutProvider {
     this.methods_.setResult(
         'replaceAccelerator', AcceleratorConfigResult.kSuccess);
     return this.methods_.resolveMethod('replaceAccelerator');
+  }
+
+  /**
+   * @param {!AcceleratorSource} source
+   * @param {number} action
+   * @param {!AcceleratorKeys} accelerator
+   */
+  removeAccelerator(source, action, accelerator) {
+    // Always return kSuccess in this fake.
+    this.methods_.setResult(
+        'removeAccelerator', AcceleratorConfigResult.kSuccess);
+    return this.methods_.resolveMethod('removeAccelerator');
   }
 
   /**
