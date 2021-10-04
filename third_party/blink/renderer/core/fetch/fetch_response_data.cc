20a96e676318bd42e5ca86088d5cd1f0a794271c
Ben Kelly <wanderview@chromium.org>
2021-10-04 22:54:58 +0000

    CacheStorage: Store WASM code cache.
    This CL enables storing WASM code cache in cache_storage.  Changes
    include:
    
    * Plumbing the cache_storage cache name through the various layers in
      order to send the compiled data back to cache_storage.
    * Making service workers load the cache_storage side data blob for
      responses with a WASM mime type.
    * Fixing a race condition in v8_wasm_response_extension.cc where it
      could check for code cache before it was finished being loaded from
      disk.
    * Adds a test verifying the code cache was correctly generated and
      used via devtools tracing.  (Unfortunately the test hits a flaky
      issue within the devtools framework and is marked SKIP for now.)
    
    Bug: 1252950
    Change-Id: Icf655e2c8b0792d6799d339e3753a56c2ab84436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199655
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927872}

--- third_party/blink/renderer/core/fetch/fetch_response_data.cc
+++ third_party/blink/renderer/core/fetch/fetch_response_data.cc
@@ -339,6 +339,7 @@ void FetchResponseData::InitFromResourceResponse(
   SetMimeType(response.MimeType());
   SetRequestMethod(request_method);
   SetResponseTime(response.ResponseTime());
+  SetCacheStorageCacheName(response.CacheStorageCacheName());
 
   if (response.WasCached()) {
     SetResponseSource(network::mojom::FetchResponseSource::kHttpCache);
