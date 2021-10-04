1a361252d0a8a67e3807140048c86296410e2add
Side Yilmaz <sideyilmaz@chromium.org>
2021-10-04 17:38:48 +0000

    Always use 'New Tab' when saving NTP to bookmarks for NTP.
    This CL forces using "New Tab" title when saving NTP to bookmarks even
    in Incognito mode. Bookmarks are shared between regular and incognito
    mode, so "New Incognito Tab" should not be used for bookmark name
    suggestion.
    
    Bug: 1255032
    Change-Id: I2fedcfdcc7d25af687180dc3dba0092682494ba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199910
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927732}

--- chrome/browser/ui/bookmarks/bookmark_utils.cc
+++ chrome/browser/ui/bookmarks/bookmark_utils.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/reading_list/features/reading_list_switches.h"
 #include "components/search/search.h"
+#include "components/strings/grit/components_strings.h"
 #include "components/url_formatter/url_formatter.h"
 #include "components/user_prefs/user_prefs.h"
 #include "components/vector_icons/vector_icons.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/drop_target_event.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/pointer/touch_ui_controller.h"
 
 #if defined(TOOLKIT_VIEWS)
@@ -109,6 +111,11 @@ bool GetURLAndTitleToBookmark(content::WebContents* web_contents,
   } else {
     *title = web_contents->GetTitle();
   }
+
+  // Use "New tab" as title if the current page is NTP even in incognito mode.
+  if (u == GURL(chrome::kChromeUINewTabURL))
+    *title = l10n_util::GetStringUTF16(IDS_NEW_TAB_TITLE);
+
   return true;
 }
 
