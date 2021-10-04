485e1825b18d064b3e1f5732970aae573e16f30d
Ali Stanfield <stanfield@google.com>
2021-10-04 17:48:51 +0000

    Add region search support for non-Google default search providers.
    Bug: 1255702, b/198833501
    Change-Id: I16ee168c7f114c408c9279174ebef915d02ef80e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199698
    Commit-Queue: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Ben Goldberger <benwgold@google.com>
    Cr-Commit-Position: refs/heads/main@{#927739}

--- chrome/browser/lens/region_search/lens_region_search_controller.cc
+++ chrome/browser/lens/region_search/lens_region_search_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "chrome/browser/image_editor/screenshot_flow.h"
 #include "chrome/browser/lens/metrics/lens_metrics.h"
+#include "chrome/browser/search/search.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/lens/lens_side_panel_helper.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
@@ -171,10 +172,15 @@ void LensRegionSearchController::OnCaptureCompleted(
         lens::LensRegionSearchCaptureResult::FAILED_TO_OPEN_TAB);
     return;
   }
-  core_tab_helper->SearchWithLensInNewTab(
-      image, captured_image.Size(),
-      lens::EntryPoint::CHROME_REGION_SEARCH_MENU_ITEM,
-      lens::features::kEnableSidePanelForLensRegionSearch.Get());
+
+  if (search::DefaultSearchProviderIsGoogle(browser_->profile())) {
+    core_tab_helper->SearchWithLensInNewTab(
+        image, captured_image.Size(),
+        lens::EntryPoint::CHROME_REGION_SEARCH_MENU_ITEM,
+        lens::features::kEnableSidePanelForLensRegionSearch.Get());
+  } else {
+    core_tab_helper->SearchByImageInNewTab(image, captured_image.Size());
+  }
   RecordCaptureResult(lens::LensRegionSearchCaptureResult::SUCCESS);
 }
 
