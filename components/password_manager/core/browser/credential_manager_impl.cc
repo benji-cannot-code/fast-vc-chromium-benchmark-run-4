c0e7425e5f11f3a4926a4eca661d20720d1f5460
Maria Kazinova <kazinova@google.com>
2021-10-04 15:18:16 +0000

    Clean up CredentialManagerImpl to use PasswordStoreInterface.
    PasswordStoreInterface is an interface that should be used by other
    classes instead of interacting directly with PasswordStore.
    This class is already refactored, this CL does a cleanup of the
    remaining PasswordStore usages.
    
    Bug: 1218413
    Change-Id: I4957a5c9d18a156ccbfac73ecffe0ee31ac3dc27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199780
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#927685}

--- components/password_manager/core/browser/credential_manager_impl.cc
+++ components/password_manager/core/browser/credential_manager_impl.cc
@@ -227,11 +227,11 @@ PasswordManagerClient* CredentialManagerImpl::client() const {
 }
 
 PasswordStoreInterface* CredentialManagerImpl::GetProfilePasswordStore() {
-  return client_ ? client_->GetProfilePasswordStore() : nullptr;
+  return client_ ? client_->GetProfilePasswordStoreInterface() : nullptr;
 }
 
 PasswordStoreInterface* CredentialManagerImpl::GetAccountPasswordStore() {
-  return client_ ? client_->GetAccountPasswordStore() : nullptr;
+  return client_ ? client_->GetAccountPasswordStoreInterface() : nullptr;
 }
 
 void CredentialManagerImpl::DoneRequiringUserMediation() {
