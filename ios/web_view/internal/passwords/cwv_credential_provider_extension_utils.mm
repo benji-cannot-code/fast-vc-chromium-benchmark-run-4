298f40c1c8841fac461dd232ea4c0991af88ea8f
John Z Wu <jzw@chromium.org>
2021-10-04 17:14:28 +0000

    Add API for retrieving and storing password from the keychain
    Passwords in iOS are stored in the keychain for security. This API will
    enable extensions to interact with this store in a secure way.
    
    Bug: 1253663
    Change-Id: Ife9defbc2c7b45baafd65b89c12333323180ad46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189083
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927720}

---
+++ ios/web_view/internal/passwords/cwv_credential_provider_extension_utils.mm
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/web_view/public/cwv_credential_provider_extension_utils.h"
+
+#import "ios/components/credential_provider_extension/password_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation CWVCredentialProviderExtensionUtils
+
++ (nullable NSString*)retrievePasswordForKeychainIdentifier:
+    (NSString*)keychainIdentifier {
+  return credential_provider_extension::PasswordWithKeychainIdentifier(
+      keychainIdentifier);
+}
+
++ (BOOL)storePasswordForKeychainIdentifier:(NSString*)keychainIdentifier
+                                  password:(NSString*)password {
+  return credential_provider_extension::StorePasswordInKeychain(
+      password, keychainIdentifier);
+}
+
+@end
