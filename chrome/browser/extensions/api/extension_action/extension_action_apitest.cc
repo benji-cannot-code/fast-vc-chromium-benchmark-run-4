cd5511b63df5ed5e40a0768fd556b5a28fe7091a
Theodore Olsauskas-Warren <sauski@google.com>
2021-10-05 14:00:29 +0000

    Sheriff: Disable SessionStorageDoesNotPersistBetweenOpenings on Linux
    Bug: 1256760
    Change-Id: I65bf894b651026940a895ece5694f435e5a1aa99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205118
    Owners-Override: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#928136}

--- chrome/browser/extensions/api/extension_action/extension_action_apitest.cc
+++ chrome/browser/extensions/api/extension_action/extension_action_apitest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/api/extension_action/test_extension_action_api_observer.h"
 #include "chrome/browser/extensions/api/extension_action/test_icon_image_observer.h"
 #include "chrome/browser/extensions/extension_apitest.h"
@@ -554,8 +555,16 @@ IN_PROC_BROWSER_TEST_P(MultiActionAPITest, PopupCreation) {
 
 // Tests that sessionStorage does not persist between closing and opening of a
 // popup.
+// TODO(crbug/1256760): Flaky on Linux.
+#if defined(OS_LINUX)
+#define MAYBE_SessionStorageDoesNotPersistBetweenOpenings \
+  DISABLED_SessionStorageDoesNotPersistBetweenOpenings
+#else
+#define MAYBE_SessionStorageDoesNotPersistBetweenOpenings \
+  SessionStorageDoesNotPersistBetweenOpenings
+#endif
 IN_PROC_BROWSER_TEST_P(MultiActionAPITest,
-                       SessionStorageDoesNotPersistBetweenOpenings) {
+                       MAYBE_SessionStorageDoesNotPersistBetweenOpenings) {
   constexpr char kManifestTemplate[] =
       R"({
            "name": "Test sessionStorage",
