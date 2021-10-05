a1fabf7320aeb2c325db36ea83d856d846e04136
Jeremy Selier <jerem@google.com>
2021-10-05 23:31:59 +0000

    Reland "[ntp][modules][photos] Adding metrics for the photos module"
    This is a reland of cbd56854da01ea53fbccf44a89c7162e53cf511e
    Changed the test to ensure browser does not navigate away when
    clicking a memory
    
    Original change's description:
    > [ntp][modules][photos] Adding metrics for the photos module
    >
    > See go/photos-exp-chrome-ntp and Metrics section of go/ntp-memories-eng-design for more details.
    >
    > Bug: 1230867
    > Change-Id: I550ea79b1f7db3e1af8b09717157471246136a4f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195175
    > Commit-Queue: Jeremy Selier <jerem@google.com>
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#927796}
    
    Bug: 1230867
    Change-Id: I7749d91b72b7009fd127a840e35370693802da73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205985
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Jeremy Selier <jerem@google.com>
    Cr-Commit-Position: refs/heads/main@{#928414}

--- chrome/test/data/webui/new_tab_page/modules/photos/module_test.js
+++ chrome/test/data/webui/new_tab_page/modules/photos/module_test.js
@@ -7,6 +7,7 @@ import {$$, photosDescriptor, PhotosProxy} from 'chrome://new-tab-page/new_tab_p
 import {assert} from 'chrome://resources/js/assert.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://test/chai_assert.js';
+import {fakeMetricsPrivate, MetricsTracker} from 'chrome://test/new_tab_page/metrics_test_support.js';
 import {installMock} from 'chrome://test/new_tab_page/test_support.js';
 import {TestBrowserProxy} from 'chrome://test/test_browser_proxy.js';
 import {isVisible} from 'chrome://test/test_util.js';
@@ -15,10 +16,14 @@ suite('NewTabPageModulesPhotosModuleTest', () => {
   /** @type {!TestBrowserProxy} */
   let handler;
 
+  /** @type {!MetricsTracker} */
+  let metrics;
+
   setup(() => {
     document.body.innerHTML = '';
     handler =
         installMock(photos.mojom.PhotosHandlerRemote, PhotosProxy.setHandler);
+    metrics = fakeMetricsPrivate();
   });
 
   test('module appears on render', async () => {
@@ -44,6 +49,10 @@ suite('NewTabPageModulesPhotosModuleTest', () => {
     document.body.append(module);
     await handler.whenCalled('getMemories');
     await handler.whenCalled('shouldShowOptInScreen');
+    assertEquals(
+        0,
+        metrics.count('NewTabPage.Photos.ModuleShownWithOptInScreen', false));
+    module.dispatchEvent(new Event('detect-impression'));
 
     // Assert.
     const items =
@@ -55,6 +64,9 @@ suite('NewTabPageModulesPhotosModuleTest', () => {
         'Title 1', items[0].querySelector('.memory-title').textContent);
     assertEquals(
         'Title 2', items[1].querySelector('.memory-title').textContent);
+    assertEquals(
+        1,
+        metrics.count('NewTabPage.Photos.ModuleShownWithOptInScreen', false));
   });
 
   test('module does not show without data', async () => {
@@ -260,9 +272,14 @@ suite('NewTabPageModulesPhotosModuleTest', () => {
     document.body.append(module);
     await handler.whenCalled('getMemories');
     await handler.whenCalled('shouldShowOptInScreen');
+    assertEquals(
+        0, metrics.count('NewTabPage.Photos.ModuleShownWithOptInScreen', true));
+    module.dispatchEvent(new Event('detect-impression'));
 
     // Asserts.
     assertTrue(!!$$(module, '#optInCard'));
+    assertEquals(
+        1, metrics.count('NewTabPage.Photos.ModuleShownWithOptInScreen', true));
 
     // Act.
     const disable = {event: null};
@@ -277,6 +294,7 @@ suite('NewTabPageModulesPhotosModuleTest', () => {
             'disableModuleToastMessage',
             loadTimeData.getString('modulesPhotosMemoriesDisabled')),
         disable.event.detail.message);
+    assertEquals(1, metrics.count('NewTabPage.Photos.UserOptIn', false));
   });
 
   test('UI is updated and backend notified when user opt in', async () => {
@@ -295,9 +313,14 @@ suite('NewTabPageModulesPhotosModuleTest', () => {
     document.body.append(module);
     await handler.whenCalled('getMemories');
     await handler.whenCalled('shouldShowOptInScreen');
+    assertEquals(
+        0, metrics.count('NewTabPage.Photos.ModuleShownWithOptInScreen', true));
+    module.dispatchEvent(new Event('detect-impression'));
 
     // Asserts.
     assertTrue(!!$$(module, '#optInCard'));
+    assertEquals(
+        1, metrics.count('NewTabPage.Photos.ModuleShownWithOptInScreen', true));
 
     // Act.
     $$(module, '#optInButton').click();
@@ -312,5 +335,33 @@ suite('NewTabPageModulesPhotosModuleTest', () => {
     const items =
         Array.from(module.shadowRoot.querySelectorAll('#memories > .memory'));
     assertEquals(1, items.length);
+    assertEquals(1, metrics.count('NewTabPage.Photos.UserOptIn', true));
+  });
+
+  test('click on memory trigger proper logging', async () => {
+    // Arrange.
+    const data = {
+      memories: [{
+        title: 'Title 1',
+        id: 'key1',
+        coverUrl: {url: 'https://fakeurl.com/1?token=foo'},
+        itemUrl: {url: '#'}
+      }]
+    };
+    handler.setResultFor('getMemories', Promise.resolve(data));
+    handler.setResultFor(
+        'shouldShowOptInScreen', Promise.resolve({showOptInScreen: false}));
+    const module = assert(await photosDescriptor.initialize(0));
+    document.body.append(module);
+    await handler.whenCalled('getMemories');
+    await handler.whenCalled('shouldShowOptInScreen');
+
+    // Act.
+    const usage = {event: null};
+    module.addEventListener('usage', (e) => usage.event = e);
+    $$(module, '#memories > .memory').click();
+
+    // Assert.
+    assertTrue(!!usage.event);
   });
 });
