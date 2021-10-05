daefc7752b6b93696b322e5f815bfeba6083629a
Gordon Seto <gordonseto@google.com>
2021-10-05 18:34:32 +0000

    [CrOS Bluetooth] Update DeviceNameManager to validate device_id exists.
    Update DeviceNameManager to check if a device with identifier
    |device_id| exists in the Bluetooth adapter before setting the device's
    nickname. If no device does not exist, the nickname is not saved. This
    is done because we have no hard-limit on number of device nicknames
    that can be saved. In order to add some constraint, we are only allowing
    devices that have been found to have nicknames set. This limits the
    number of nicknames a user can set.
    
    Bug: 1010321
    Change-Id: Icdef8ff4f0725a9b77230792ee37bf23e402c833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3202576
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928251}

--- chromeos/services/bluetooth_config/device_name_manager_impl.h
+++ chromeos/services/bluetooth_config/device_name_manager_impl.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/services/bluetooth_config/device_name_manager.h"
 
+#include "device/bluetooth/bluetooth_adapter.h"
+#include "device/bluetooth/bluetooth_device.h"
+
 class PrefRegistrySimple;
 class PrefService;
 
@@ -19,7 +22,9 @@ class DeviceNameManagerImpl : public DeviceNameManager {
  public:
   static void RegisterPrefs(PrefRegistrySimple* registry);
 
-  explicit DeviceNameManagerImpl(PrefService* pref_service);
+  DeviceNameManagerImpl(
+      scoped_refptr<device::BluetoothAdapter> bluetooth_adapter,
+      PrefService* pref_service);
   ~DeviceNameManagerImpl() override;
 
   // DeviceNameManager:
@@ -29,6 +34,11 @@ class DeviceNameManagerImpl : public DeviceNameManager {
                          const std::string& nickname) override;
 
  private:
+  // Returns true if a BluetoothDevice* with identifier |device_id| exists in
+  // |bluetooth_adapter_|, else false.
+  bool DoesDeviceExist(const std::string& device_id) const;
+
+  scoped_refptr<device::BluetoothAdapter> bluetooth_adapter_;
   PrefService* pref_service_;
 };
 
