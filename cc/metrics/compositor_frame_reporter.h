54834a069793afc6bc637a7dc9142bb3beaa2408
Prathik Kaundinya <kprathik@google.com>
2021-10-05 18:47:22 +0000

    Adding arguments to trace event to display breakdowns under SendBeginMainFrameToCommit.
    The change in this CL shows the contributions of individual parts within the SendBeginMainframeToCommit section to the overall latency. These metrics are shown in UMA but were previously not shown in the trace viewer.
    
    Bug: chromium:1247291
    Change-Id: I636f9df049ca1fa33d5b8535145cf93704cf1c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198591
    Auto-Submit: Prathik Kaundinya <kprathik@google.com>
    Commit-Queue: Prathik Kaundinya <kprathik@google.com>
    Reviewed-by: Behdadb <behdadb@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928266}

--- cc/metrics/compositor_frame_reporter.h
+++ cc/metrics/compositor_frame_reporter.h
@@ -349,7 +349,6 @@ class CC_EXPORT CompositorFrameReporter {
       base::TimeDelta time_delta) const;
 
   void ReportEventLatencyHistograms() const;
-
   void ReportCompositorLatencyTraceEvents() const;
   void ReportEventLatencyTraceEvents() const;
 
