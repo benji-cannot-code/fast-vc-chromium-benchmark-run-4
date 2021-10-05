5c67dfe5266087dd64bd78a9be0fce600d3dbc1d
Rohit Bhatia <bhatiarohit@google.com>
2021-10-05 05:46:08 +0000

    Extract and move safe_browsing_metrics_collector
    Extract safe_browsing_metrics_collector from components/safe_browsing/content/browser/ and move to components/safe_browsing/core/browser/
    
    Bug: 1240291
    Change-Id: I7fa4011b07b88316e28657b1a9e48750cdadbd03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198534
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928018}

--- chrome/browser/safe_browsing/download_protection/download_reporter.cc
+++ chrome/browser/safe_browsing/download_protection/download_reporter.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_item.h"
 #include "components/download/public/common/simple_download_manager_coordinator.h"
 #include "components/safe_browsing/content/browser/download/download_stats.h"
-#include "components/safe_browsing/content/browser/safe_browsing_metrics_collector.h"
+#include "components/safe_browsing/core/browser/safe_browsing_metrics_collector.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_item_utils.h"
 #include "url/url_constants.h"
--- components/safe_browsing/core/browser/safe_browsing_metrics_collector_unittest.cc
+++ components/safe_browsing/core/browser/safe_browsing_metrics_collector_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/safe_browsing/content/browser/safe_browsing_metrics_collector.h"
+#include "components/safe_browsing/core/browser/safe_browsing_metrics_collector.h"
 #include <memory>
 #include <utility>
 
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
-#include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -73,7 +72,7 @@ class SafeBrowsingMetricsCollectorTest : public ::testing::Test {
   }
 
   std::unique_ptr<SafeBrowsingMetricsCollector> metrics_collector_;
-  content::BrowserTaskEnvironment task_environment_{
+  base::test::TaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   TestingPrefServiceSimple pref_service_;
 
