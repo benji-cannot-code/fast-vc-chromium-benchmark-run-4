3ccfaf268bdfbf3e45d7696b95f01c5fe861e4d2
Alex Ilin <alexilin@chromium.org>
2021-10-05 14:21:53 +0000

    [profiles] Fix TestingProfileManager::DeleteAllTestingProfiles()
    The current implementation of DeleteAllTestingProfiles() doesn't remove
    profiles from an underlying ProfileManager. ProfileManager keeps
    profiles in the ProfileInfoMap.
    
    All other Delete*Profile() methods in TestingProfileManager do actually
    delete a profile by erasing a ProfileInfo entry in the ProfileManager.
    This CL applies the same method to DeleteAllTestingProfiles().
    
    This CL also fixes tests that start failing because
    DeleteAllTestingProfiles() actually deletes profiles now.
    
    Change-Id: I81c45b55701f2393246a68fc07c09e1fb7ad21d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198888
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928144}

--- chrome/browser/metrics/chrome_metrics_extensions_helper_unittest.cc
+++ chrome/browser/metrics/chrome_metrics_extensions_helper_unittest.cc
@@ -44,5 +44,6 @@ TEST(ChromeMetricsExtensionsHelperTest, Basic) {
                                                site_instance->GetId());
   EXPECT_TRUE(extensions_helper.IsExtensionProcess(host));
 #endif
+  rph_factory.reset();
   profile_manager.DeleteAllTestingProfiles();
 }
