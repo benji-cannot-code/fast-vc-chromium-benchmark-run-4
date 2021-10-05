ffb707493f416a36b534c8c391eec0cc4edb5c48
Pi-Hsun Shih <pihsun@chromium.org>
2021-10-05 03:08:23 +0000

    Revert "Choose OzoneBacking over GLImageBacking for CrOS Ash"
    This reverts commit 3bc1f7fe4654c11af43fc003eefb8d5e51de0777.
    
    Reason for revert: Breaks video playing on ChromeOS
    
    Original change's description:
    > Choose OzoneBacking over GLImageBacking for CrOS Ash
    >
    > This change completes a TODO where the OzoneFactory is chosen over
    > GLImage since it supports creating shared images with both GMBs and
    > pixel upload support. It also adds a check for device creation failures
    > for x11 in ozone and fallbacks to GPU_READ when SCANOUT is not
    > supported for creating pixmaps. It also fixes null image issue in
    > SharedImageRepresentationGLTextureOzone.
    >
    > Bug: 1241537
    > Change-Id: If352cf3efde248ceacb12b64174ed6134749d6f0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3187214
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#927402}
    
    Bug: 1241537
    Change-Id: I819ea697a89310253a80ee6465927febb045ac13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198022
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927983}

--- ui/gfx/linux/gpu_memory_buffer_support_x11.cc
+++ ui/gfx/linux/gpu_memory_buffer_support_x11.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/debug/crash_logging.h"
-#include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/buffer_types.h"
@@ -120,10 +119,7 @@ std::unique_ptr<GbmBuffer> GpuMemoryBufferSupportX11::CreateBuffer(
     gfx::BufferFormat format,
     const gfx::Size& size,
     gfx::BufferUsage usage) {
-  if (!device_) {
-    LOG(ERROR) << "Device could not be created.";
-    return nullptr;
-  }
+  DCHECK(device_);
   DCHECK(base::Contains(supported_configs_,
                         gfx::BufferUsageAndFormat(usage, format)));
 
