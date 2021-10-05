748336b39550cf984508031e82f524ad34bcb54a
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-10-05 21:56:40 +0000

    [CrOS Bluetooth] Add request pin functionality
    Implements pairing to a device when request PIN or request passKey
    is required for authorization. It also adds some a11y to
    bluetooth_pairing_request_code_page.html.
    
    Bug: 1010321
    Change-Id: I5b668f626f9ed2db27ab58329073aecd3e464f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179662
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#928367}

--- chrome/test/data/webui/settings/chromeos/os_bluetooth_pairing_dialog_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_bluetooth_pairing_dialog_tests.js
@@ -36,7 +36,7 @@ suite('OsBluetoothPairingDialogTest', function() {
     Polymer.dom.flush();
   });
 
-  test('Cancel event is fired on close', async function() {
+  test('Finished event is fired on close', async function() {
     const pairingDialog =
         bluetoothPairingDialog.shadowRoot.querySelector('bluetooth-pairing-ui');
     assertTrue(!!pairingDialog);
@@ -44,7 +44,7 @@ suite('OsBluetoothPairingDialogTest', function() {
     const closeBluetoothPairingUiPromise =
         test_util.eventToPromise('close', bluetoothPairingDialog);
 
-    pairingDialog.dispatchEvent(new CustomEvent('cancel'));
+    pairingDialog.dispatchEvent(new CustomEvent('finished'));
 
     await closeBluetoothPairingUiPromise;
   });
