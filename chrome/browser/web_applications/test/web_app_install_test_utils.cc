bdf71cae89bbaca9f9fac0fcf6f7fe7e7ecaa80b
Glen Robertson <glenrob@chromium.org>
2021-10-05 02:33:50 +0000

    NoteTaking: Use AppService not WebApp Registrar so it works in LaCros
    Under LaCros note_taking_helper will exist in Ash-Chrome while the
    note-taking web apps will exist in Lacros-Chrome. Ash-Chrome will not
    have access to the Lacros-Chrome Web App registrar so they must
    communicate via App Service and intents.
    
    Also observes updates to apps in AppService so list of note-taking apps
    is updated dynamically (previously you had to reopen the settings page
    to see a change after installing a note-taking web app).
    
    Bug: 1185678,1194370
    Change-Id: I74479d81548c28c54ba65a0dd1f42e46c062c571
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3089279
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927969}

--- chrome/browser/web_applications/test/web_app_install_test_utils.cc
+++ chrome/browser/web_applications/test/web_app_install_test_utils.cc
@@ -54,10 +54,7 @@ void AwaitStartWebAppProviderAndSubsystems(Profile* profile) {
   provider->SetSystemWebAppManager(
       std::make_unique<web_app::TestSystemWebAppManager>(profile));
   provider->Start();
-  // Await registry ready.
-  base::RunLoop run_loop;
-  provider->on_registry_ready().Post(FROM_HERE, run_loop.QuitClosure());
-  run_loop.Run();
+  WaitUntilReady(provider);
 }
 
 AppId InstallDummyWebApp(Profile* profile,
@@ -88,7 +85,8 @@ AppId InstallDummyWebApp(Profile* profile,
             run_loop.Quit();
           }));
   run_loop.Run();
-
+  // Allow updates to be published to App Service listeners.
+  base::RunLoop().RunUntilIdle();
   return app_id;
 }
 
@@ -116,6 +114,8 @@ AppId InstallWebApp(Profile* profile,
           }));
 
   run_loop.Run();
+  // Allow updates to be published to App Service listeners.
+  base::RunLoop().RunUntilIdle();
   return app_id;
 }
 
@@ -146,6 +146,8 @@ AppId InstallWebAppWithUrlHandlers(
         run_loop.Quit();
       }));
   run_loop.Run();
+  // Allow updates to be published to App Service listeners.
+  base::RunLoop().RunUntilIdle();
   return app_id;
 }
 #endif
@@ -163,6 +165,8 @@ void UninstallWebApp(Profile* profile, const AppId& app_id) {
       }));
 
   run_loop.Run();
+  // Allow updates to be published to App Service listeners.
+  base::RunLoop().RunUntilIdle();
 }
 
 }  // namespace test
