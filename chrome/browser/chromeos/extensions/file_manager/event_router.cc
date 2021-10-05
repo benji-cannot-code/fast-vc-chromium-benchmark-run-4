ccc27947fed8902e6fde76ec872a0d7b936c4f91
Ben Reich <benreich@chromium.org>
2021-10-05 04:12:31 +0000

    [FilesSWA] Record FSP mount UMA in event router when SWA enabled
    The background page currently records
    FileBrowser.FileSystemProviderMounted and in the SWA world the
    background code will exist on every window. This ports the code to C++
    specifically in the event_router.cc and guards it to only emit if the
    SWA flag is enabled.
    
    There is a special use case where the SWA flag might be enabled but the
    Chrome app is still running (thus a background page still starts). In
    this case, favour the event router implementation over the background
    page.
    
    Furthermore, the mount_metrics.js enum for FSPs had the ZIP_ARCHIVER and
    ZIP_UNPACKER removed. When the JS API implementation fires first it sets
    up the Histogram to contain the num of values in the array (i.e. 16) but
    the C++ API requires explicit values (skipping them throws an error).
    There is also guidance to keep the the
    tools/metrics/histograms/enums.xml file up to date with the
    implementation.
    
    Bug: 1254243
    Test: browser_tests --gtest_filter=*deduplicatedUmaMetricForFile*
    Test: Compile, flash to device and install Dropbox FSP
    Change-Id: I79ee60b7978843f558f9f6dd649c1dffe9eca5b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3195849
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927997}

--- chrome/browser/chromeos/extensions/file_manager/event_router.cc
+++ chrome/browser/chromeos/extensions/file_manager/event_router.cc
@@ -12,11 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/components/drivefs/drivefs_host.h"
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/tablet_mode.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/files/file_util.h"
+#include "base/metrics/histogram.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "chrome/browser/app_mode/app_mode_utils.h"
@@ -29,11 +32,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_manager/open_util.h"
 #include "chrome/browser/ash/file_manager/path_util.h"
 #include "chrome/browser/ash/file_manager/volume_manager.h"
+#include "chrome/browser/ash/file_system_provider/provided_file_system_info.h"
 #include "chrome/browser/ash/login/lock/screen_locker.h"
 #include "chrome/browser/ash/login/ui/login_display_host.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_features.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_pref_names.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_util.h"
+#include "chrome/browser/chromeos/extensions/file_manager/file_system_provider_metrics_util.h"
 #include "chrome/browser/chromeos/extensions/file_manager/private_api_util.h"
 #include "chrome/browser/extensions/api/file_system/chrome_file_system_delegate.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -433,6 +438,28 @@ class DriveFsEventRouterImpl : public DriveFsEventRouter {
   const std::map<base::FilePath, std::unique_ptr<FileWatcher>>* const
       file_watchers_;
 };
+
+// Records mounted File System Provider type if known otherwise UNKNOWN.
+void RecordFileSystemProviderMountMetrics(const Volume& volume) {
+  const ash::file_system_provider::ProviderId& provider_id =
+      volume.provider_id();
+  if (provider_id.GetType() != ash::file_system_provider::ProviderId::INVALID) {
+    using FileSystemProviderMountedTypeMap =
+        std::unordered_map<std::string, FileSystemProviderMountedType>;
+
+    const std::string fsp_key = provider_id.ToString();
+    FileSystemProviderMountedTypeMap fsp_sample_map =
+        GetUmaForFileSystemProvider();
+    FileSystemProviderMountedTypeMap::iterator sample =
+        fsp_sample_map.find(fsp_key);
+    if (sample != fsp_sample_map.end())
+      UMA_HISTOGRAM_ENUMERATION(kFileSystemProviderMountedMetricName,
+                                sample->second);
+    else
+      UMA_HISTOGRAM_ENUMERATION(kFileSystemProviderMountedMetricName,
+                                FileSystemProviderMountedType::UNKNOWN);
+  }
+}
 
 }  // namespace
 
@@ -879,6 +906,11 @@ void EventRouter::OnVolumeMounted(chromeos::MountError error_code,
   DispatchMountCompletedEvent(
       file_manager_private::MOUNT_COMPLETED_EVENT_TYPE_MOUNT, error_code,
       volume);
+
+  // If the Files SWA is enabled, record the UMA metrics for mounted FSPs.
+  if (ash::features::IsFileManagerSwaEnabled()) {
+    RecordFileSystemProviderMountMetrics(volume);
+  }
 
   // TODO(mtomasz): Move VolumeManager and part of the event router outside of
   // file_manager, so there is no dependency between File System API and the
