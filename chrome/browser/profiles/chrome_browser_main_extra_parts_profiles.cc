036f96637b414c28c776dfe937100678db5138e6
sauski <sauski@google.com>
2021-10-05 12:19:14 +0000

    Privacy Metrics: Add Privacy Metrics service
    Add a new BrowserContextKeyedService to record privacy metrics along
    with profile startup. Creating a new service allows for consolidation
    of privacy metrics being recorded elsewhere (included in this CL) as
    well as allows the collection of more complex Privacy metrics, such
    as the interaction of Sync with Privacy Settings (future CL).
    
    Bug: 1228614
    Change-Id: Iaec188cdc6525e7d4e0bbc6902aa72958c283cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199779
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#928106}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefetch/no_state_prefetch/no_state_prefetch_link_manager_factory.h"
 #include "chrome/browser/prefetch/no_state_prefetch/no_state_prefetch_manager_factory.h"
 #include "chrome/browser/prefs/pref_metrics_service.h"
+#include "chrome/browser/privacy/privacy_metrics_service_factory.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/renderer_updater_factory.h"
 #include "chrome/browser/safe_browsing/certificate_reporting_service_factory.h"
@@ -423,6 +424,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   predictors::PredictorDatabaseFactory::GetInstance();
   prerender::NoStatePrefetchLinkManagerFactory::GetInstance();
   prerender::NoStatePrefetchManagerFactory::GetInstance();
+  PrivacyMetricsServiceFactory::GetInstance();
   PrivacySandboxSettingsFactory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   ProfileAccountManagerFactory::GetInstance();
--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/chrome_pref_service_factory.h"
 #include "chrome/browser/prefs/pref_service_syncable_util.h"
 #include "chrome/browser/prefs/profile_pref_store_manager.h"
+#include "chrome/browser/privacy/privacy_metrics_service_factory.h"
 #include "chrome/browser/profiles/bookmark_model_loaded_observer.h"
 #include "chrome/browser/profiles/chrome_version_service.h"
 #include "chrome/browser/profiles/pref_service_builder_utils.h"
@@ -817,6 +818,9 @@ void ProfileImpl::DoFinalInit(CreateMode create_mode) {
   // The creation of FlocIdProvider should align with the start of a browser
   // profile session, so initialize it here.
   federated_learning::FlocIdProviderFactory::GetForProfile(this);
+
+  // The Privacy Metrics service should start alongside each profile session.
+  PrivacyMetricsServiceFactory::GetForProfile(this);
 
   AnnouncementNotificationServiceFactory::GetForProfile(this)
       ->MaybeShowNotification();
