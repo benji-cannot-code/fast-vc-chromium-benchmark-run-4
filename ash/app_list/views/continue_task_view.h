de2f1671c52ae6ffafcceb471136e844074e3a49
Ana Salazar <anasalazar@chromium.org>
2021-10-05 22:40:26 +0000

    [launcher]: Add context menu to continue tasks
    This CL adds a context menu for 'ContinueTaskView'. Right now, the
    context menu will appear when long pressed or with a right mouse click.
    It has two available actions:
       - Open: Launches the task.
       - Remove Suggestion: Prevents the suggestion from further showing in
         the Continue Section (To be implemented).
    This CL also defines strings and icons for the menu items.
    Screenshot: https://screenshot.googleplex.com/645GFirMkun9siW.png
    
    Bug: 1216084
    Change-Id: I43850e06533a3bebe5c21f4edc3a592cb27303e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3169394
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928387}

--- ash/app_list/views/continue_task_view.h
+++ ash/app_list/views/continue_task_view.h
@@ -6,21 +6,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_APP_LIST_VIEWS_CONTINUE_TASK_VIEW_H_
 #define ASH_APP_LIST_VIEWS_CONTINUE_TASK_VIEW_H_
 
+#include <memory>
+
 #include "ash/app_list/model/search/search_result.h"
 #include "ash/app_list/model/search/search_result_observer.h"
 #include "ash/ash_export.h"
+#include "ui/base/models/simple_menu_model.h"
+#include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/button.h"
 
 namespace views {
 class ImageView;
 class Label;
-}
+class MenuRunner;
+}  // namespace views
 
 namespace ash {
 class AppListViewDelegate;
 
+enum ContinueTaskCommandId {
+  // Context Menu option to open the selected suggestion.
+  kOpenResult = 0,
+  // Context Menu option to prevent the suggestion from showing.
+  kRemoveResult = 1,
+};
+
 // A view with a suggested task for the "Continue" section.
 class ASH_EXPORT ContinueTaskView : public views::Button,
+                                    public views::ContextMenuController,
+                                    public ui::SimpleMenuModel::Delegate,
                                     public SearchResultObserver {
  public:
   METADATA_HEADER(ContinueTaskView);
@@ -39,8 +53,15 @@ class ASH_EXPORT ContinueTaskView : public views::Button,
 
   void SetResult(SearchResult* result);
 
+  // Returns true if the context menu for this task is showing.
+  bool IsMenuShowing() const;
+
   void set_index_in_container(size_t index) { index_in_container_ = index; }
   SearchResult* result() const { return result_; }
+  int index_in_container() const { return index_in_container_.value_or(-1); }
+
+  // SimpleMenuModel::Delegate:
+  void ExecuteCommand(int command_id, int event_flags) override;
 
  private:
   void SetIcon(const gfx::ImageSkia& icon);
@@ -48,6 +69,20 @@ class ASH_EXPORT ContinueTaskView : public views::Button,
   void UpdateResult();
 
   void OnButtonPressed(const ui::Event& event);
+
+  // views::ContextMenuController:
+  void ShowContextMenuForViewImpl(views::View* source,
+                                  const gfx::Point& point,
+                                  ui::MenuSourceType source_type) override;
+
+  // Opens the search result related to the view.
+  void OpenResult(int event_flags);
+
+  // Builds and returns a raw pointer to `context_menu_model_`.
+  ui::SimpleMenuModel* BuildMenuModel();
+
+  // Closes the context menu for this view if it is running.
+  void CloseContextMenu();
 
   // The index of this view within a |SearchResultContainerView| that holds it.
   absl::optional<int> index_in_container_;
@@ -57,6 +92,9 @@ class ASH_EXPORT ContinueTaskView : public views::Button,
   views::Label* subtitle_ = nullptr;
   views::ImageView* icon_ = nullptr;
   SearchResult* result_ = nullptr;  // Owned by SearchModel::SearchResults.
+
+  std::unique_ptr<ui::SimpleMenuModel> context_menu_model_;
+  std::unique_ptr<views::MenuRunner> context_menu_runner_;
 
   base::ScopedObservation<SearchResult, SearchResultObserver>
       search_result_observation_{this};
