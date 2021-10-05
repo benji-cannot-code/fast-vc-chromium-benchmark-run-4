5e3a14677040f14363e23eebd387263bcc604c43
Alex Turner <alexmt@chromium.org>
2021-10-05 23:33:03 +0000

    Move trustworthy origin check from key fetcher to ReportRequest::Create
    This will ensure that APIs do not create requests with such origins,
    surfacing the error sooner. Some unittests are also updated for
    consistency.
    
    Bug: 1254117
    Change-Id: I4567f7498da925c9e568906947ed42dd9b3a6eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197378
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928416}

--- content/browser/aggregation_service/aggregation_service_key_fetcher.cc
+++ content/browser/aggregation_service/aggregation_service_key_fetcher.cc
@@ -30,11 +30,7 @@ AggregationServiceKeyFetcher::~AggregationServiceKeyFetcher() = default;
 
 void AggregationServiceKeyFetcher::GetPublicKey(const url::Origin& origin,
                                                 FetchCallback callback) {
-  if (!network::IsOriginPotentiallyTrustworthy(origin)) {
-    std::move(callback).Run(absl::nullopt,
-                            PublicKeyFetchStatus::kUntrustworthyOrigin);
-    return;
-  }
+  DCHECK(network::IsOriginPotentiallyTrustworthy(origin));
 
   base::circular_deque<FetchCallback>& pending_callbacks =
       origin_callbacks_[origin];
