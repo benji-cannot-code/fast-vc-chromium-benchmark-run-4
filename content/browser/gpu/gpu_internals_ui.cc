e049daa02b6a03e70697b1ba1bbd66fb35b88d68
yugeeklab <yugeeklab@gmail.com>
2021-10-05 22:27:13 +0000

    CodeHealth: Modernize base::Value code in gpu_internals_ui
    Remove ListValue::Get(). Existing usages should be replaced
    with Value::GetList()[].
    
    Bug: 1187084
    Change-Id: Id9807ad473f2ef7f2f8253feb3fa72962e63dfcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199528
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928381}

--- content/browser/gpu/gpu_internals_ui.cc
+++ content/browser/gpu/gpu_internals_ui.cc
@@ -750,9 +750,7 @@ void GpuMessageHandler::OnCallAsync(const base::ListValue* args) {
   DCHECK_GE(args->GetList().size(), static_cast<size_t>(2));
   // unpack args into requestId, submessage and submessageArgs
   bool ok;
-  const base::Value* requestId;
-  ok = args->Get(0, &requestId);
-  DCHECK(ok);
+  const base::Value& requestId = args->GetList()[0];
 
   std::string submessage;
   ok = args->GetString(1, &submessage);
@@ -760,11 +758,9 @@ void GpuMessageHandler::OnCallAsync(const base::ListValue* args) {
 
   auto submessageArgs = std::make_unique<base::ListValue>();
   for (size_t i = 2; i < args->GetList().size(); ++i) {
-    const base::Value* arg;
-    ok = args->Get(i, &arg);
-    DCHECK(ok);
+    const base::Value& arg = args->GetList()[i];
 
-    submessageArgs->Append(arg->Clone());
+    submessageArgs->Append(arg.Clone());
   }
 
   // call the submessage handler
@@ -781,10 +777,10 @@ void GpuMessageHandler::OnCallAsync(const base::ListValue* args) {
   // call BrowserBridge.onCallAsyncReply with result
   if (ret) {
     web_ui()->CallJavascriptFunctionUnsafe("browserBridge.onCallAsyncReply",
-                                           *requestId, *ret);
+                                           requestId, *ret);
   } else {
     web_ui()->CallJavascriptFunctionUnsafe("browserBridge.onCallAsyncReply",
-                                           *requestId);
+                                           requestId);
   }
 }
 
