f99f929e8e32100748eb3836a6ade19cd58d4ca3
Ayu Ishii <ayui@chromium.org>
2021-10-05 22:10:50 +0000

    StorageBuckets: New BucketLocator object
    This change adds a new BucketLocator object to represent
    a Storage Bucket. This includes a bucket's main identifying
    properties (id, storage_key, type, is_default) to be passed between
    Quota and Storage APIs. Unlike the BucketInfo object, all
    included properties for the bucket should never change until
    the bucket is deleted from the QuotaDatabase. This is expected
    to be used in most cases where a Bucket is retrieved when
    BucketInfo is too much but BucketId is not sufficient
    (ex. QuotaClient, some methods on QuotaManagerProxy).
    
    Change-Id: I42a0b99aaff51d6706350ee17d159c2b9860b491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3183259
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928373}

--- components/services/storage/public/cpp/buckets/bucket_info.h
+++ components/services/storage/public/cpp/buckets/bucket_info.h
@@ -15,6 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace storage {
 
+// Snapshot of a bucket's information in the quota database.
+//
+// Properties that can be updated by the Storage Buckets API, like
+// `expiration` and `quota`, may get out of sync with the database. The
+// database is the source of truth.
 struct COMPONENT_EXPORT(STORAGE_SERVICE_BUCKETS_SUPPORT) BucketInfo {
   BucketInfo();
   BucketInfo(BucketId bucket_id,
