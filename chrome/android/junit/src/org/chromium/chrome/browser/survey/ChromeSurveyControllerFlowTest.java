2916474231cce3bd6c19acbea3169ca3e87d0e49
Aishwarya Rajesh <aishwaryarj@google.com>
2021-10-05 02:20:10 +0000

    [Messages][HaTS] Add tab observer for HaTS message
    This CL includes updates to the HaTS message implementation to keep in line with the existing infobar behavior on events associated with the tab where the prompt is displayed.
    
    The primary issue that is being targeted here is that the message prompt currently shows up even after the survey has expired. To avoid this, the message will be dismissed when the original tab where the message was displayed is hidden for any reason.
    
    Bug: 1254789
    Change-Id: Ida34c9f4c2c3e0a428468b5b861fceaa9053d0d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198140
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#927964}

--- chrome/android/junit/src/org/chromium/chrome/browser/survey/ChromeSurveyControllerFlowTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/survey/ChromeSurveyControllerFlowTest.java
@@ -58,6 +58,7 @@ import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.survey.ChromeSurveyController.InfoBarClosingState;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.TabHidingType;
 import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
@@ -622,6 +623,20 @@ public class ChromeSurveyControllerFlowTest {
         }
     }
 
+    @Test
+    public void testMessages_Dismiss_OnTabHidden() {
+        presentMessages();
+        PropertyModel messageModel = mMessagePropertyCaptor.getValue();
+
+        mTabObserver.onHidden(mMockTab, TabHidingType.ACTIVITY_HIDDEN);
+        verify(mMessageDispatcher).dismissMessage(messageModel, DismissReason.TAB_SWITCHED);
+
+        // Simulate the invocation of the message dismissal callback.
+        messageModel.get(MessageBannerProperties.ON_DISMISSED).onResult(DismissReason.TAB_SWITCHED);
+        assertInfoBarDisplayedRecorded();
+        assertInfoBarClosingStateRecorded(InfoBarClosingState.UNKNOWN);
+    }
+
     private void initializeChromeSurveyController() {
         ChromeSurveyController.initialize(
                 mMockModelSelector, mMockLifecycleDispatcher, mActivity, mMessageDispatcher);
@@ -654,6 +669,7 @@ public class ChromeSurveyControllerFlowTest {
         mockTabReady();
         mTestSurveyController.onDownloadSuccessRunnable.run();
         assertSurveyMessagesEnqueued();
+        Assert.assertNotNull("mTabObserver is null.", mTabObserver);
     }
 
     private void mockTabReady() {
@@ -713,6 +729,9 @@ public class ChromeSurveyControllerFlowTest {
     }
 
     private void assertInfoBarDisplayedRecorded() {
+        if (mTabObserver != null) {
+            verify(mMockTab).removeObserver(mTabObserver);
+        }
         Assert.assertTrue("SharedPreference for InfoBarShown is not recorded.",
                 SharedPreferencesManager.getInstance().contains(mPrefKeyPromptShown));
     }
