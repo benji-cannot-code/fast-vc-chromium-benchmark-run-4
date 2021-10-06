b534cf6d70bd7452420da074ea926f1444a1fa48
David Maunder <davidjm@chromium.org>
2021-10-06 21:45:30 +0000

    No decimal places for prices above $10 in price drops
    We have a UX requirement to use 2 decimal places for prices below $10
    and no decimal places above $10 for the formatted price when displaying
    price drops in the Tab Switching UI.
    
    Bug: 1245026
    Change-Id: I90a9c8035f0e6818b1bd78a0f0286c0a75ad4ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209113
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928888}

--- ios/chrome/browser/commerce/shopping_persisted_data_tab_helper.h
+++ ios/chrome/browser/commerce/shopping_persisted_data_tab_helper.h
@@ -20,29 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-namespace {
-
-const int kUnitsToMicros = 1000000;
-const int kMinimumDropThresholdAbsolute = 2 * kUnitsToMicros;
-const int kMinimumDropThresholdRelative = 10;
-
-// Not all price drops should be made available - only when there is an absolute
-// price drop of 2 units or more and a relative price drop greater than 10%.
-BOOL IsQualifyingPriceDrop(int64_t current_price_micros,
-                           int64_t previous_price_micros) {
-  if (previous_price_micros - current_price_micros <
-      kMinimumDropThresholdAbsolute) {
-    return false;
-  }
-  if ((100 * current_price_micros) / previous_price_micros >
-      (100 - kMinimumDropThresholdRelative)) {
-    return false;
-  }
-  return true;
-}
-
-}  // namespace
-
 // This class acquires pricing data corresponding to the WebState's
 // URL - should the URL be for a shopping website with an offer.
 class ShoppingPersistedDataTabHelper
@@ -83,8 +60,21 @@ class ShoppingPersistedDataTabHelper
  private:
   SEQUENCE_CHECKER(sequence_checker_);
   friend class web::WebStateUserData<ShoppingPersistedDataTabHelper>;
+  friend class ShoppingPersistedDataTabHelperTest;
 
   explicit ShoppingPersistedDataTabHelper(web::WebState* web_state);
+
+  // Not all price drops should be made available - only when there is an
+  // absolute price drop of 2 units or more and a relative price drop greater
+  // than 10%.
+  static BOOL IsQualifyingPriceDrop(int64_t current_price_micros,
+                                    int64_t previous_price_micros);
+
+  // Converts price from micros to a string according to the
+  // |currency_formatter| currency code and locale.
+  static std::u16string FormatPrice(
+      payments::CurrencyFormatter* currency_formatter,
+      long price_micros);
 
   // web::WebStateObserver overrides:
   void DidFinishNavigation(web::WebState* web_state,
