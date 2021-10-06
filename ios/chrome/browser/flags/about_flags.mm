1e8943babe75a177c3bf0324836783faedb10150
Aliona DANGLA <alionadangla@google.com>
2021-10-06 15:47:11 +0000

    [iOS] Add flags for testing default browser screen
    This CL adds flags to test all scenarios for FRE's default browser
    screen experiment and puts better naming.
    
    Test plan can be found here:
    https://docs.google.com/document/d/10qtceZtSfGZiSCh1nuSVo2yYax9Y6BJ_iUX6Y1u3gkk/edit?usp=sharing&resourcekey=0-qK9au20x8D97atz-yFPrOg
    
    Bug: 1238811
    Change-Id: Ia80798d86c3de7bfea53e0f2d8a4238f98802cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3127715
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Cr-Commit-Position: refs/heads/main@{#928682}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/browser/ui/default_promo/default_browser_utils.h"
 #import "ios/chrome/browser/ui/download/features.h"
+#include "ios/chrome/browser/ui/first_run/default_browser_promo_field_trial.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_features.h"
 #import "ios/chrome/browser/ui/ntp/new_tab_page_feature.h"
 #import "ios/chrome/browser/ui/overlays/infobar_banner/infobar_banner_features.h"
@@ -298,6 +299,23 @@ const FeatureEntry::FeatureVariation kStartSurfaceVariations[] = {
     {"1h:Hide Shortcuts and show Return to Recent Tab tile",
      kStartSurfaceOneHourHideShortcutsReturnToRecentTab,
      base::size(kStartSurfaceOneHourHideShortcutsReturnToRecentTab), nullptr},
+};
+
+const FeatureEntry::FeatureParam kFREDefaultPromoTestingDefaultDelay[] = {
+    {kFREDefaultPromoTestingDefaultDelayParam, "true"}};
+const FeatureEntry::FeatureParam kFREDefaultPromoTestingFREOnly[] = {
+    {kFREDefaultPromoTestingFREOnlyParam, "true"}};
+const FeatureEntry::FeatureParam kFREDefaultPromoTestingShortDelay[] = {
+    {kFREDefaultPromoTestingShortDelayParam, "true"}};
+const FeatureEntry::FeatureVariation kFREDefaultPromoTestingVariations[] = {
+    {"Wait 14 days after FRE default browser promo",
+     kFREDefaultPromoTestingDefaultDelay,
+     base::size(kFREDefaultPromoTestingDefaultDelay), nullptr},
+    {"FRE default browser promo only", kFREDefaultPromoTestingFREOnly,
+     base::size(kFREDefaultPromoTestingFREOnly), nullptr},
+    {"Wait 3 days after FRE default browser promo",
+     kFREDefaultPromoTestingShortDelay,
+     base::size(kFREDefaultPromoTestingShortDelay), nullptr},
 };
 
 // To add a new entry, add to the end of kFeatureEntries. There are four
@@ -755,7 +773,14 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableDiscoverFeedDiscoFeedEndpointName,
      flag_descriptions::kEnableDiscoverFeedDiscoFeedEndpointDescription,
      flags_ui::kOsIos,
-     FEATURE_VALUE_TYPE(kEnableDiscoverFeedDiscoFeedEndpoint)}};
+     FEATURE_VALUE_TYPE(kEnableDiscoverFeedDiscoFeedEndpoint)},
+    {"enable-fre-default-browser-screen-testing",
+     flag_descriptions::kEnableFREDefaultBrowserScreenTestingName,
+     flag_descriptions::kEnableFREDefaultBrowserScreenTestingDescription,
+     flags_ui::kOsIos,
+     FEATURE_WITH_PARAMS_VALUE_TYPE(kEnableFREDefaultBrowserScreenTesting,
+                                    kFREDefaultPromoTestingVariations,
+                                    "EnableFREDefaultBrowserScreenTesting")}};
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
   return false;
