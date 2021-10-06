e269893a5e24c3c0a6e5f7841f17d23d52c65663
Yohei Maeda <yoheimaeda@google.com>
2021-10-06 02:45:07 +0000

    Prerender: Show information of prerendered pages
    This CL added a function to an internal page for prerendering.
    We'll be able to access the page using chrome://prerender-internals
    (This page will show no information if Prerender2 is not enabled.)
    
    Procedures below are implemented in this CL.
    - PrerenderInternalsHandlerImpl gets the information: the URL of
      the prerendered page, the URL of the page that triggers prerendering,
      and the FinalStatus.
    - pass the information from the browser process to the renderer process
      using mojo.
    - list the information in HTML using JS.
    
    Bug: 1246787
    Change-Id: I0851a8af8d532c51395dc1b802cf66a5c4f025df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3157702
    Commit-Queue: Yohei Maeda <yoheimaeda@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928498}

--- content/browser/prerender/prerender_internals_ui.h
+++ content/browser/prerender/prerender_internals_ui.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_PRERENDER_PRERENDER_INTERNALS_UI_H_
 #define CONTENT_BROWSER_PRERENDER_PRERENDER_INTERNALS_UI_H_
 
+#include "content/browser/prerender/prerender_internals.mojom-forward.h"
 #include "content/public/browser/web_ui_controller.h"
 
 namespace content {
@@ -17,6 +18,16 @@ class PrerenderInternalsUI : public WebUIController {
   PrerenderInternalsUI(const PrerenderInternalsUI&) = delete;
   PrerenderInternalsUI& operator=(const PrerenderInternalsUI&) = delete;
   ~PrerenderInternalsUI() override;
+
+  void WebUIRenderFrameCreated(RenderFrameHost* render_frame_host) override;
+
+  void BindPrerenderInternalsHandler(
+      mojo::PendingReceiver<mojom::PrerenderInternalsHandler> receiver);
+
+ private:
+  std::unique_ptr<mojom::PrerenderInternalsHandler> ui_handler_;
+
+  WEB_UI_CONTROLLER_TYPE_DECL();
 };
 
 }  // namespace content
---
+++ content/browser/resources/prerender/prerender_internals.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {$} from 'chrome://resources/js/util.m.js';
+import {PrerenderInternalsHandler} from './prerender_internals.mojom-webui.js';
+
+let pageHandler;
+
+function addDivElement(li, text) {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(text));
+  li.appendChild(div);
+}
+
+document.addEventListener('DOMContentLoaded', _ => {
+  pageHandler = PrerenderInternalsHandler.getRemote();
+
+  pageHandler.getPrerenderInfo().then((response) => {
+    for (const info of response.infos) {
+      const li = document.createElement('li');
+      for (const pageInfo of info.prerenderedPageInfos) {
+        addDivElement(li, pageInfo.triggerPageUrl.url);
+        addDivElement(li, pageInfo.url.url);
+        addDivElement(li, pageInfo.finalStatus);
+      }
+      $('prerender-info').appendChild(li);
+    }
+  });
+});
