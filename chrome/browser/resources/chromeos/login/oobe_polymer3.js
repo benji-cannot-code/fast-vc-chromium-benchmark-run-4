392e79816192aebdbdf149b59f391a0c560676ac
Renato Silva <rrsilva@google.com>
2021-10-06 16:01:00 +0000

    OOBE - Polymer3 migration - App Downloading Screen
    Migrate the App Downloading screen to ES6 class syntax and
    add directives to generate its Polymer3 version.
    
    Bug: 1184731, 1251576
    Change-Id: I8a6dd964fcfe738115127b9c84575525c680e458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3202592
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#928690}

--- chrome/browser/resources/chromeos/login/oobe_polymer3.js
+++ chrome/browser/resources/chromeos/login/oobe_polymer3.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://oobe/screens/common/app_downloading.m.js';
 import 'chrome://oobe/screens/common/fingerprint_setup.m.js';
 import 'chrome://oobe/screens/common/gesture_navigation.m.js';
 import 'chrome://oobe/screens/common/marketing_opt_in.m.js';
