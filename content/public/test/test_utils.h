d40b6e67b70e0302656c793a27026eb235950fa7
Dave Tapuska <dtapuska@chromium.org>
2021-10-06 19:06:12 +0000

    Rewrite more tests that are using GetAllFrames.
    - Replace GetAllFrames with ChildFrameAt
    - Remove unused GetAllFrameUrls method
    - Adjust before unload prep method to work on only the main frame where
    before unload is dispatched.
    
    BUG=1208438
    
    Change-Id: Id16fb253763b8e59ed5154b8aac2bbd27242ca58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199500
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928800}

--- content/public/test/test_utils.h
+++ content/public/test/test_utils.h
@@ -391,6 +391,8 @@ class RenderFrameHostWrapper {
   RenderFrameHost& operator*() const;
   RenderFrameHost* operator->() const;
 
+  explicit operator bool() const { return get() != nullptr; }
+
  private:
   const GlobalRenderFrameHostId rfh_id_;
 
