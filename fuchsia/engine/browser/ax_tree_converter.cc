1fb7ebf187488fee5ee42f226b21746883593000
Lucas Radaelli <lucasradaelli@google.com>
2021-10-06 16:24:37 +0000

    [fuchsia][a11y] Translates invisible and focusable states.
    This change translates two AxNode states: invisible and focusable, to
    their Fuchsia equivalent.
    
    Test: AXTreeConverterTest
    Change-Id: I4a5472c4479399ef620ac54794ec787cd6371af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206868
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Lucas Radaelli <lucasradaelli@google.com>
    Cr-Commit-Position: refs/heads/main@{#928701}

--- fuchsia/engine/browser/ax_tree_converter.cc
+++ fuchsia/engine/browser/ax_tree_converter.cc
@@ -189,7 +189,7 @@ fuchsia::accessibility::semantics::States ConvertStates(
   }
 
   // Indicates if the node is hidden.
-  states.set_hidden(node.IsIgnored());
+  states.set_hidden(node.IsIgnored() || node.IsInvisible());
 
   // The user entered value of the node, if applicable.
   if (node.HasStringAttribute(ax::mojom::StringAttribute::kValue)) {
@@ -211,6 +211,9 @@ fuchsia::accessibility::semantics::States ConvertStates(
       node.GetIntAttribute(ax::mojom::IntAttribute::kScrollY);
   if (x_scroll_offset || y_scroll_offset)
     states.set_viewport_offset({x_scroll_offset, y_scroll_offset});
+
+  if (node.HasState(ax::mojom::State::kFocusable))
+    states.set_focusable(true);
 
   return states;
 }
