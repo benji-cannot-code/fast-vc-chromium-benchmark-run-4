1ada9da5cbf00f67f000706f526a28d823be7baa
Xiaoling Bao <xiaolingbao@chromium.org>
2021-10-06 21:42:38 +0000

    Try to fix test config on UAC-enabled bots.
    1. Move 'updater_tests.exe' argument to test definition in
       gn_isolate_map.pyl, as suggested by dpranke@.
    2. Use 'run_telemetry_as_googletest.py' as test run script,
       as as suggested by dpranke@. This script is actually general
       enough to run any executable.
    3. Add "python3" attribute to 'updater_tests_win_uac' so the test
       is run with the correct interpreter.
    4. Tweaked run_command_as_standard_user.py to make the command
       argument passing more flexible (the test infra could adjust the
       arguments so 'updater_tests.exe' may no longer be the 1st arg.
    5. Make ":updater_tests" a data DEP of 'updater_tests_win_uac', so
       the binaries are copied to bots to run.
    
    Bug: 1233164
    Change-Id: I84d3a979af9b5af3cc3e9c0a6c42bcedadbba59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3192195
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928884}

--- chrome/updater/test/service/win/run_command_as_standard_user.py
+++ chrome/updater/test/service/win/run_command_as_standard_user.py
@@ -15,13 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 All arguments provided to this program will be used to reconstruct the command
 line for the child process. For example,
-    vpython run_command_as_standard_user.py notepad "hello world.txt"
+    vpython run_command_as_standard_user.py --command notepad "hello world.txt"
 will launch a process with command line:
     notepad "hello world.txt"
 
 This command must be run as an elevated user.
 """
 
+import argparse
 import distutils
 import logging
 import os
@@ -32,13 +33,26 @@ import rpc_client
 import updater_test_service_control
 
 
+
+def ParseCommandLine():
+  """Parse the command line arguments."""
+  cmd_parser = argparse.ArgumentParser(description='Run command as user')
+
+  cmd_parser.add_argument(
+      '--command',
+      dest='command',
+      type=str,
+      help='The command to run.')
+  return cmd_parser.parse_known_args()
+
+
 def LogToSTDERR(title, output):
   if not output:
     return
 
   logging.error('%s %s starts %s', '=' * 30, title, '=' * 30)
 
-  # Directly dump the output to STDER so we don't have logging prefix each
+  # Directly dump the output to STDERR so we don't have logging prefix each
   # line, to make it easier to read.
   sys.stderr.write(output)
 
@@ -46,16 +60,18 @@ def LogToSTDERR(title, output):
 
 
 def main():
-  if len(sys.argv) < 2:
+  flags, remaining_args = ParseCommandLine()
+
+  if not flags.command:
     logging.error('Must specify a command to run.')
     sys.exit(-1)
 
   # Find the location of the command. shutil.which() looks suitable for this,
   # only if https://bugs.python.org/issue24505 is closed. For now, use the
   # one from distutils module.
-  command = distutils.spawn.find_executable(sys.argv[1])
+  command = distutils.spawn.find_executable(flags.command)
   if not command:
-    logging.error('Cannot find command: %s', sys.argv[1])
+    logging.error('Cannot find command: %s', flags.command)
     sys.exit(-2)
 
   # Command may be in relative path. Make it absolute so that the RPC server
@@ -67,7 +83,7 @@ def main():
   # already processed by shell. It is possible that the reconstructed command
   # line is skewed (for example, expansion of environment variable), but
   # hopefully this works well enough in all real scenarios.
-  command_line = subprocess.list2cmdline([command] + sys.argv[2:])
+  command_line = subprocess.list2cmdline([command] + remaining_args)
   logging.error('Full command line: %s', command_line)
   with updater_test_service_control.OpenService():
     pid, exit_code, stdout, stderr = rpc_client.RunAsStandardUser(command_line)
