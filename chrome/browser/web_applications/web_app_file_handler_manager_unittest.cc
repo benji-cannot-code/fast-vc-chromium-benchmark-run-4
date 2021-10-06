e04293bc5e0fc73ea8b157a754a027ebc7a28e08
David Bienvenu <davidbienvenu@chromium.org>
2021-10-06 16:06:24 +0000

    Add GetFile{Extensions/MimeTypes}FromFileHandler
    This addresses TODO's from crbug.com/938103 and will be useful for
    supporting per-file handler display names (crbug.com/1247824)
    
    Bug: 938103, 1247824
    Change-Id: Ib5b72376585285349ef32c5cd27646502d3906c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199059
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928691}

--- chrome/browser/web_applications/web_app_file_handler_manager_unittest.cc
+++ chrome/browser/web_applications/web_app_file_handler_manager_unittest.cc
@@ -23,6 +23,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
+TEST(FileHandlerUtilsTest, GetFileExtensionsFromFileHandler) {
+  apps::FileHandler file_handler;
+  file_handler.action = GURL("https://app.site/open-foo");
+  {
+    apps::FileHandler::AcceptEntry accept_entry;
+    accept_entry.mime_type = "application/foo";
+    accept_entry.file_extensions.insert(".foo");
+    file_handler.accept.push_back(accept_entry);
+  }
+  {
+    apps::FileHandler::AcceptEntry accept_entry;
+    accept_entry.mime_type = "application/foobar";
+    accept_entry.file_extensions.insert(".foobar");
+    file_handler.accept.push_back(accept_entry);
+  }
+
+  std::set<std::string> file_extensions =
+      GetFileExtensionsFromFileHandler(file_handler);
+
+  EXPECT_EQ(2u, file_extensions.size());
+  EXPECT_THAT(file_extensions,
+              testing::UnorderedElementsAre(".foo", ".foobar"));
+}
+
 TEST(FileHandlerUtilsTest, GetFileExtensionsFromFileHandlers) {
   apps::FileHandlers file_handlers;
 
@@ -63,6 +87,28 @@ TEST(FileHandlerUtilsTest, GetFileExtensionsFromFileHandlers) {
   EXPECT_EQ(4u, file_extensions.size());
   EXPECT_THAT(file_extensions,
               testing::UnorderedElementsAre(".foo", ".foobar", ".bar", ".baz"));
+}
+
+TEST(FileHandlerUtilsTest, GetMimeTypesFromFileHandler) {
+  apps::FileHandler file_handler;
+  file_handler.action = GURL("https://app.site/open-foo");
+  {
+    apps::FileHandler::AcceptEntry accept_entry;
+    accept_entry.mime_type = "application/foo";
+    accept_entry.file_extensions.insert(".foo");
+    file_handler.accept.push_back(accept_entry);
+  }
+  {
+    apps::FileHandler::AcceptEntry accept_entry;
+    accept_entry.mime_type = "application/foobar";
+    accept_entry.file_extensions.insert(".foobar");
+    file_handler.accept.push_back(accept_entry);
+  }
+  std::set<std::string> mime_types = GetMimeTypesFromFileHandler(file_handler);
+
+  EXPECT_EQ(2u, mime_types.size());
+  EXPECT_THAT(mime_types, testing::UnorderedElementsAre("application/foo",
+                                                        "application/foobar"));
 }
 
 TEST(FileHandlerUtilsTest, GetMimeTypesFromFileHandlers) {
