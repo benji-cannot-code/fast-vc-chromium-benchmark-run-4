f60435e19db2dbcd235459a7298c4b690e39a62b
Curtis McMullan <curtismcmullan@chromium.org>
2021-10-06 08:30:52 +0000

    lp: update language packs mojom module name
    The module name for the language packs mojom is conflicting with a
    parameter name within the generated mojom js bindings. Update the
    module name to stop conflict.
    
    Bug: 1194688
    Change-Id: I1fd952e007b64be1825cb1fce5e40c5a88b81979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205146
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928587}

--- chromeos/language/language_packs/language_packs_impl.cc
+++ chromeos/language/language_packs/language_packs_impl.cc
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace language_packs {
 
-using ::language::mojom::FeatureId;
-using ::language::mojom::LanguagePackInfo;
-using ::language::mojom::LanguagePacks;
-using ::language::mojom::PackState;
+using ::chromeos::language::mojom::FeatureId;
+using ::chromeos::language::mojom::LanguagePackInfo;
+using ::chromeos::language::mojom::LanguagePacks;
+using ::chromeos::language::mojom::PackState;
 
 namespace {
 
@@ -57,7 +57,7 @@ void OnOperationComplete(LanguagePacksImpl::GetPackInfoCallback mojo_callback,
 }  // namespace
 
 LanguagePacksImpl::LanguagePacksImpl(
-    mojo::PendingReceiver<language::mojom::LanguagePacks> receiver)
+    mojo::PendingReceiver<chromeos::language::mojom::LanguagePacks> receiver)
     : receiver_(this, std::move(receiver)) {}
 LanguagePacksImpl::~LanguagePacksImpl() = default;
 
