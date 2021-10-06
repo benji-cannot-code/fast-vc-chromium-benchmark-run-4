3e345c905a1efcd929b574e53e9baaf54e70d77f
Renato Silva <rrsilva@google.com>
2021-10-06 14:55:50 +0000

    OOBE - Polymer3 Migration - WebView Misc
    Move web_view_helper and web_view_loader to the components
    folder and add instructions to generate their modularised
    versions. Also add support files for them and cr_ui.js and
    also dependencies in display_manager.
    
    Bug: 1184731
    Change-Id: Idc9944d91ed098eda68bb74ba59df7a106ab3793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205361
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#928663}

--- chrome/browser/resources/chromeos/login/components/web_view_helper.js
+++ chrome/browser/resources/chromeos/login/components/web_view_helper.js
@@ -3,12 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// #import {assert, assertNotReached} from '//resources/js/assert.m.js';
+
 /**
  * @fileoverview Web view helper.
  */
 
 /** Web view helper shared between OOBE screens. */
-class WebViewHelper {
+/* #export */ class WebViewHelper {
   /**
    * Loads content of the given url into the given web view.
    * The content is loaded via XHR and is sent to web view via data url so that
