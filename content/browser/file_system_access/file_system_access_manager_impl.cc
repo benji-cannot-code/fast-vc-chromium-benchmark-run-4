1c6866789be036e5c28e4175a534fd6a8c84eb06
Austin Sullivan <asully@chromium.org>
2021-10-06 22:07:42 +0000

    FSA: Add SafeMoveDelegate
    Pulls out the existing FileWriter code into a helper which:
    - runs safe browsing checks
    - moves the file
    - adds the mark of the web
    
    This helper will eventually be used by FileSystemHandle::move()
    to safely move files and directories.
    
    Bug: 1247850
    Change-Id: I301547091d154ce4bd658d4c1db8c73a395302aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180202
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928900}

--- content/browser/file_system_access/file_system_access_manager_impl.cc
+++ content/browser/file_system_access/file_system_access_manager_impl.cc
@@ -1494,4 +1494,9 @@ void FileSystemAccessManagerImpl::DidCleanupAccessHandleCapacityAllocation(
   DCHECK_EQ(1u, count_removed);
 }
 
+base::WeakPtr<FileSystemAccessManagerImpl>
+FileSystemAccessManagerImpl::AsWeakPtr() {
+  return weak_factory_.GetWeakPtr();
+}
+
 }  // namespace content
