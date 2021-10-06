d18abddde9c287c63ddf873a2274575b09a8971d
John Abd-El-Malek <jam@chromium.org>
2021-10-06 21:57:38 +0000

    Add histogram for time to load extensions enterprise settings.
    Bug: 1256913
    Change-Id: I92e0ed60afc63893a75365b4d499fec92473abe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209315
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928895}

--- chrome/browser/extensions/extension_management.cc
+++ chrome/browser/extensions/extension_management.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/syslog_logging.h"
@@ -406,6 +407,7 @@ bool ExtensionManagement::CheckMinimumVersion(
 
 void ExtensionManagement::Refresh() {
   TRACE_EVENT0("browser,startup", "ExtensionManagement::Refresh");
+  SCOPED_UMA_HISTOGRAM_TIMER("Extensions.Management_Refresh");
   // Load all extension management settings preferences.
   const base::ListValue* allowed_list_pref =
       static_cast<const base::ListValue*>(LoadPreference(
