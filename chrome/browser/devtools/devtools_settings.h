5ca7f73ca2aaba36c7bedeb7e57d00d21d71822e
Simon Zünd <szuend@chromium.org>
2021-10-06 07:21:25 +0000

    Move synced DevTools settings between the two backing dictionaries
    This CL adds a change listener for the "Sync DevTools settings" toggle.
    When it is changed, we need to move all synced settings between the
    two dictionaries where one is synced and the other is not.
    (See https://bit.ly/devtools-settings-sync)
    
    It is enough to only listen for changes during DevTools sessions, as
    the frontend is the only client able to change the setting. The
    added method is also idempotent. As long as the method is not run
    concurrently, multiple DevToolsUIBinding instances for the same
    profile can't interfere with each other.
    
    R=caseq@chromium.org
    
    Bug: 1245541
    Change-Id: I2a35ce6d470a566ba11dd7a41bf5c2a8f9aec08d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189195
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928570}

--- chrome/browser/devtools/devtools_settings.h
+++ chrome/browser/devtools/devtools_settings.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/flat_set.h"
+#include "components/prefs/pref_change_registrar.h"
 
 class Profile;
 
@@ -42,6 +43,7 @@ class DevToolsSettings {
  private:
   const char* GetDictionaryNameForSettingsName(const std::string& name) const;
   const char* GetDictionaryNameForSyncedPrefs() const;
+  void DevToolsSyncPreferencesChanged();
 
   Profile* const profile_;
 
@@ -49,6 +51,12 @@ class DevToolsSettings {
   // The DevTools frontend *must* call `Register` for each setting prior to
   // use, which guarantees that this set must not be persisted.
   base::flat_set<std::string> synced_setting_names_;
+
+  // Settings pref observer that moves synced settings between their two
+  // dictionaries depending on the state of kDevToolsSyncPreferences.
+  // kDevToolsSyncPreferences is only changed via the frontend, so it is
+  // sufficient to only listen for changes during a DevTools session.
+  PrefChangeRegistrar pref_change_registrar_;
 };
 
 #endif  // CHROME_BROWSER_DEVTOOLS_DEVTOOLS_SETTINGS_H_
