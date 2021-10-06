b6a6ce075c7564601a6fa9ba097b1e925e96e54b
Egor Pasko <pasko@chromium.org>
2021-10-06 17:09:49 +0000

    build/android: fix some test_runner.py failures in py3
    Applying a few fixes. This allowed me to run a command like:
    
    > build/android/test_runner.py junit \
      --output-directory out/A \
      --test-suite base_junit_tests \
      --resource-apk out/A/gen/base/base_junit_tests.ap_ \
      --robolectric-runtime-deps-dir \
      out/A/lib.java/third_party/robolectric \
      '--gtest_filter=*.LinkerTest*' -v
    
    Since this functionality is still often run under py2, an attempt was
    made to make it work on both.
    
    Bug: 1257206
    Change-Id: Ieb2038c58c8f9ae959433573b35c0027a8df8c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208808
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928723}

--- build/android/pylib/local/machine/local_machine_junit_test_run.py
+++ build/android/pylib/local/machine/local_machine_junit_test_run.py
@@ -156,7 +156,8 @@ class LocalMachineJunitTestRun(test_run.TestRun):
       procs = [
           subprocess.Popen(cmd,
                            stdout=subprocess.PIPE,
-                           stderr=subprocess.STDOUT) for cmd in cmd_list
+                           stderr=subprocess.STDOUT,
+                           universal_newlines=True) for cmd in cmd_list
       ]
       PrintProcessesStdout(procs)
 
