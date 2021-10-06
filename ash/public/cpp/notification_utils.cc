47a73fb0e90c9f17f71ed33854567713c612391b
Alex Newcomer <newcomer@chromium.org>
2021-10-06 00:26:56 +0000

    cros:Remove unused util function
    It's not used anywhere, removing it.
    
    Change-Id: I21425ef24e780a0fbe6c6f44bd16f83148977b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3204191
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928438}

--- ash/public/cpp/notification_utils.cc
+++ ash/public/cpp/notification_utils.cc
@@ -11,28 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 std::unique_ptr<message_center::Notification> CreateSystemNotification(
-    const std::string& notification_id,
-    const std::u16string& title,
-    const std::u16string& message,
-    const std::string& system_component_id,
-    const base::RepeatingClosure& click_callback) {
-  DCHECK(!click_callback.is_null());
-  std::unique_ptr<message_center::Notification> notification =
-      CreateSystemNotification(
-          message_center::NOTIFICATION_TYPE_SIMPLE, notification_id, title,
-          message, std::u16string() /* display_source */, GURL(),
-          message_center::NotifierId(
-              message_center::NotifierType::SYSTEM_COMPONENT,
-              system_component_id),
-          message_center::RichNotificationData(),
-          new message_center::HandleNotificationClickDelegate(click_callback),
-          gfx::kNoneIcon,
-          message_center::SystemNotificationWarningLevel::CRITICAL_WARNING);
-  notification->SetSystemPriority();
-  return notification;
-}
-
-std::unique_ptr<message_center::Notification> CreateSystemNotification(
     message_center::NotificationType type,
     const std::string& id,
     const std::u16string& title,
