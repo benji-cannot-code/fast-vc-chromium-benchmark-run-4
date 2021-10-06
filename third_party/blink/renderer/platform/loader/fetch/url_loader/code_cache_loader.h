ee76fdccee398fd327efc9fad28ed2d3eb1fe789
Leszek Swirski <leszeks@chromium.org>
2021-10-06 13:34:57 +0000

    Reland "[code cache] Use context specific interface for clearing cached code"
    This is a reland of commit 0314d69e5c35606f91bd69085a90612f464201ca.
    
    Reland fixes:
     * Rebase on top of https://crrev.com/c/3162165 which introduces a
       wrapper CodeCacheHost to allow CodeCacheLoader to store WeakRef to
       the mojo::Remote<CodeCacheHost>, so that it knows when the Remote is
       reset (from e.g disconnecting).
    
    Original change's description:
    > [code cache] Use context specific interface for clearing cached code
    >
    > Code caches are migrated to use NetworkIsolationKey and to use context
    > specific (frame / worker) interface to perform operations on code
    > caches. This CL migrates clearing of the cached code to use context
    > specific interface. This extends CodeCacheLoader to also add support
    > for clearing code cache entries and uses this to clear cached code.
    >
    > Bug: chromium:1213818
    > Change-Id: I091a65c01faeae098e2d4d61ee1c91578a302d89
    > Reviewed-on:
    https://chromium-review.googlesource.com/c/chromium/src/+/3122177
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: Mythri Alle <mythria@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#916751}
    
    Bug: chromium:1213818, chromium:1245526
    Change-Id: Ic5100b50104a27613d3cc22ab3574ac2ee4c188e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152025
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928643}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/code_cache_loader.h
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/code_cache_loader.h
@@ -32,6 +32,9 @@ class BLINK_PLATFORM_EXPORT CodeCacheLoader : public WebCodeCacheLoader {
                           const WebURL& url,
                           FetchCodeCacheCallback callback) override;
 
+  void ClearCodeCacheEntry(mojom::CodeCacheType cache_type,
+                           const WebURL& url) override;
+
  private:
   bool ShouldUsePerProcessInterface() const;
 
