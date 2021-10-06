94d0d967230615cfa4401602ae47e06dd0ccd17d
Dirk Pranke <dpranke@google.com>
2021-10-06 22:40:18 +0000

    Make --isolated-script-test-output optional where possible.
    As part of the work to unify how we invoke tests, we want
    people to be able to reliably run the out/Default/bin/run_*
    wrappers either locally or on bots and have them do the right
    thing.
    
    Historically, we expected tests that used the //testing/scripts
    wrappers to *only* by run on the bots, and so we thought that
    having `--isolated-script-test-output` be a required argument
    was a good idea (it would catch programmatic errors).
    
    However, if it is required, then devs have to specify it when
    they want to run the script locally. That is often confusing
    (devs don't know what to set it to) and unnecessary, because they
    often don't even want the results.
    
    This change makes the flag optional to nearly all of the script
    wrappers. There are still a few telemetry/perf-based wrappers where
    the flag will still be required, because we need to know where
    to write the other perf output files.
    
    Bug: 816629
    Change-Id: Ib2ee9939316bd55d82d4cf43197b29282e2eb8cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200552
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#928912}

--- testing/scripts/run_devtools_check.py
+++ testing/scripts/run_devtools_check.py
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Runs a python script under an isolate
 
 This script attempts to emulate the contract of gtest-style tests
-invoked via recipes. The main contract is that the caller passes the
-argument:
+invoked via recipes.
 
-  --isolated-script-test-output=[FILENAME]
-
-json is written to that file in the format produced by
-common.parse_common_test_results.
+If optional argument --isolated-script-test-output=[FILENAME] is passed
+to the script, json is written to that file in the format detailed in
+//docs/testing/json-test-results-format.md.
 
 This script is intended to be the base command invoked by the isolate,
 followed by a subsequent Python script."""
@@ -33,8 +31,7 @@ import xvfb
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--isolated-script-test-output', type=str,
-                      required=True)
+  parser.add_argument('--isolated-script-test-output', type=str)
   args, rest_args = parser.parse_known_args()
   # Remove the isolated script extra args this script doesn't care about.
   should_ignore_arg = lambda arg: any(to_ignore in arg for to_ignore in (
@@ -45,9 +42,10 @@ def main():
   rest_args = [arg for arg in rest_args if not should_ignore_arg(arg)]
 
   ret = common.run_command([sys.executable] + rest_args)
-  with open(args.isolated_script_test_output, 'w') as fp:
-    json.dump({'valid': True,
-               'failures': ['failed'] if ret else []}, fp)
+  if args.isolated_Script_test_output:
+    with open(args.isolated_script_test_output, 'w') as fp:
+      json.dump({'valid': True,
+                 'failures': ['failed'] if ret else []}, fp)
   return ret
 
 
--- testing/scripts/run_flatbuffers_unittests.py
+++ testing/scripts/run_flatbuffers_unittests.py
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Runs a python script under an isolate
 
 This script attempts to emulate the contract of gtest-style tests
-invoked via recipes. The main contract is that the caller passes the
-argument:
+invoked via recipes.
 
-  --isolated-script-test-output=[FILENAME]
-
-json is written to that file in the format produced by
-common.parse_common_test_results.
+If optional argument --isolated-script-test-output=[FILENAME] is passed
+to the script, json is written to that file in the format detailed in
+//docs/testing/json-test-results-format.md.
 
 This script is intended to be the base command invoked by the isolate,
 followed by a subsequent Python script."""
@@ -31,8 +29,7 @@ import xvfb
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--isolated-script-test-output', type=str,
-                      required=True)
+  parser.add_argument('--isolated-script-test-output', type=str)
   args, _ = parser.parse_known_args()
 
   if sys.platform == 'win32':
@@ -52,8 +49,9 @@ def main():
       if output != "ALL TESTS PASSED\n":
         failures = [output]
 
-  with open(args.isolated_script_test_output, 'w') as fp:
-    json.dump({'valid': True,'failures': failures}, fp)
+  if args.isolated_script_test_output:
+    with open(args.isolated_script_test_output, 'w') as fp:
+      json.dump({'valid': True,'failures': failures}, fp)
 
   return rc
 
