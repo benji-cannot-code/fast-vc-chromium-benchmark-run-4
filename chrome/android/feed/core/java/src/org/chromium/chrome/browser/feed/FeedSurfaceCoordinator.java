8f75892ed26678ab642967458422ce389653590e
Cathy Li <chili@chromium.org>
2021-10-06 16:15:52 +0000

    Prevent call-after-destroy in FeedSurfaceCoordinator.
    Bug: 1247675, 1229179
    Change-Id: I03e273b8fe62c01ca7432028e1b6b11028cc5bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3207377
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928697}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -384,7 +384,6 @@ public class FeedSurfaceCoordinator implements FeedSurfaceProvider, FeedBubbleDe
             mSwipeRefreshLayout = null;
         }
         stopBubbleTriggering();
-        mMediator.destroy();
         if (mFeedSurfaceLifecycleManager != null) mFeedSurfaceLifecycleManager.destroy();
         mFeedSurfaceLifecycleManager = null;
         if (mEnhancedProtectionPromoController != null) {
@@ -396,6 +395,9 @@ public class FeedSurfaceCoordinator implements FeedSurfaceProvider, FeedBubbleDe
             mSectionHeaderModel.get(SectionHeaderListProperties.SECTION_HEADERS_KEY)
                     .removeObserver(mSectionHeaderListModelChangeProcessor);
         }
+        // Destroy mediator after all other related controller/processors are destroyed.
+        mMediator.destroy();
+
         maybeClearImageMemoryCache();
         FeedSurfaceTracker.getInstance().untrackSurface(this);
         if (mHybridListRenderer != null) {
