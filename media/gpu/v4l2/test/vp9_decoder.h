75feba48c886c0bcc3ad7e558a66efe6d88db4d8
Steve Cho <stevecho@google.com>
2021-10-06 16:54:02 +0000

    media/gpu/v4l2: initialization for v4l2 stateless decoder
    This CL does initialization for v4l2 stateless decoder. This
    includes setting formats, allocating buffers, allocating
    requests, and starting streaming for OUTPUT queue and
    CAPTURE queue.
    
    Bug: b:190733055
    TEST: v4l2_stateful_decoder --video crowd_run_256X144_fr15_bd8_8buf_l1_20210312.ivf on asurada
    Change-Id: I466228ddb34f1325ecc52bae21abdde581a81e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3123749
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928716}

--- media/gpu/v4l2/test/vp9_decoder.h
+++ media/gpu/v4l2/test/vp9_decoder.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/v4l2/test/v4l2_ioctl_shim.h"
 
+#include "base/files/memory_mapped_file.h"
+#include "media/filters/ivf_parser.h"
 #include "media/filters/vp9_parser.h"
 
 namespace media {
@@ -26,11 +28,18 @@ class Vp9Decoder {
   // Creates a Vp9Decoder after verifying that the underlying implementation
   // supports VP9 stateless decoding.
   static std::unique_ptr<Vp9Decoder> Create(
-      std::unique_ptr<IvfParser> ivf_parser);
+      std::unique_ptr<IvfParser> ivf_parser,
+      const media::IvfFileHeader& file_header);
+
+  // Initializes setup needed for decoding.
+  // https://www.kernel.org/doc/html/v5.10/userspace-api/media/v4l/dev-stateless-decoder.html#initialization
+  bool Initialize();
 
  private:
   Vp9Decoder(std::unique_ptr<IvfParser> ivf_parser,
-             std::unique_ptr<V4L2IoctlShim> v4l2_ioctl);
+             std::unique_ptr<V4L2IoctlShim> v4l2_ioctl,
+             std::unique_ptr<V4L2Queue> OUTPUT_queue,
+             std::unique_ptr<V4L2Queue> CAPTURE_queue);
 
   // Reads next frame from IVF stream and its size into |vp9_frame_header|
   // and |size| respectively.
@@ -45,6 +54,12 @@ class Vp9Decoder {
 
   // Wrapper for V4L2 ioctl requests.
   const std::unique_ptr<V4L2IoctlShim> v4l2_ioctl_;
+
+  // OUTPUT_queue needed for compressed (encoded) input.
+  std::unique_ptr<V4L2Queue> OUTPUT_queue_;
+
+  // CAPTURE_queue needed for uncompressed (decoded) output.
+  std::unique_ptr<V4L2Queue> CAPTURE_queue_;
 };
 
 }  // namespace v4l2_test
