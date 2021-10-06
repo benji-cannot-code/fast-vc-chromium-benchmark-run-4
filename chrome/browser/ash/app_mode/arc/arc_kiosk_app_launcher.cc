ef8ff72b929e27ae56bf093fec0bc10f7224a12a
Sean Kau <skau@chromium.org>
2021-10-06 16:10:32 +0000

    Migrate arc_kiosk_app_launcher away from kWindowPinTypeKey
    kWindowPinTypeKey is redundant in Ash. It's being removed in
    favor of interacting with WindowState directly through helper
    functions.
    
    Bug: 1249678
    Change-Id: I8fb2b34a3d2eabd300b9daf1227a0f5a10293eba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206098
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928694}

--- chrome/browser/ash/app_mode/arc/arc_kiosk_app_launcher.cc
+++ chrome/browser/ash/app_mode/arc/arc_kiosk_app_launcher.cc
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
-#include "chromeos/ui/base/window_pin_type.h"
-#include "chromeos/ui/base/window_properties.h"
+#include "chrome/browser/ui/ash/window_pin_util.h"
 #include "components/arc/arc_util.h"
 #include "components/arc/metrics/arc_metrics_constants.h"
 #include "components/exo/shell_surface_util.h"
@@ -77,8 +76,7 @@ bool ArcKioskAppLauncher::CheckAndPinWindow(aura::Window* const window) {
     return false;
   // Stop observing as target window is already found.
   StopObserving();
-  window->SetProperty(chromeos::kWindowPinTypeKey,
-                      chromeos::WindowPinType::kTrustedPinned);
+  PinWindow(window, /*trusted=*/true);
   if (delegate_)
     delegate_->OnAppWindowLaunched();
   return true;
