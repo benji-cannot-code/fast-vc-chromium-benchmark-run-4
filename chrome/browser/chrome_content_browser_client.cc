c0478dbfd33da08080e30a59f256aaa348d8da41
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-10-06 01:46:26 +0000

    Revert "Add enterprise policy for enabling or disabling network sandbox."
    This reverts commit a5f1ceaa8aa4e57ed3d78f67fcb405a3af5600be.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 928449 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2E1ZjFjZWFhOGFhNGU1N2VkM2Q3OGY2N2ZjYjQwNWEzYWY1NjAwYmUM
    
    Sample Failed Build: https://ci.chromium.org/b/8834164055623906449
    
    Sample Failed Step: compile
    
    Original change's description:
    > Add enterprise policy for enabling or disabling network sandbox.
    >
    > This CL adds a new enterprise policy `NetworkServiceSandboxEnabled`
    > for force enabling or disabling the network service sandbox.
    >
    > This allows enterprises to force enable now to get feedback on any
    > potential future compatibility issues during rollout, and will also
    > permit opting out of the sandbox if any third party compatibility
    > issues are encountered after rollout.
    >
    > If this policy is not set, then the default from content will be
    > used.
    >
    > This CL also removes some DCHECKs in sandbox as control of whether
    > network service sandbox is enabled is now fully in content.
    >
    > BUG=841001
    > BYPASS_POLICY_COMPATIBILITY_CHECK=No suitable default exists.
    >
    > Change-Id: Icdbbadd71a7b93fab947a00e1a14e6bedbb8c8f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198860
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#928449}
    
    
    BUG=841001
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I882ff253998a41297b8901b577412a8bdb2cbed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3207369
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Owners-Override: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928461}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -5680,10 +5680,6 @@ bool ChromeContentBrowserClient::ShouldSandboxAudioService() {
   return IsAudioServiceSandboxEnabled();
 }
 
-bool ChromeContentBrowserClient::ShouldSandboxNetworkService() {
-  return SystemNetworkContextManager::IsNetworkSandboxEnabled();
-}
-
 void ChromeContentBrowserClient::LogWebFeatureForCurrentPage(
     content::RenderFrameHost* render_frame_host,
     blink::mojom::WebFeature feature) {
