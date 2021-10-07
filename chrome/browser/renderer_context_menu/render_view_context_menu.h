f4a8137aeb9b305a619610402daa8f7b82f6b94c
Juan Mojica <juanmojica@google.com>
2021-10-07 22:07:12 +0000

    Reland "Add region search support for non-Google default search providers.".
    This is a reland of 23f829c698968c11a75e96bdcd3d7735d97179af
    
    This change was reverted due to an uncaught bug in a unit test. This pertained to the IsRegionSearchEnabled function not checking if the TemplateURLService was NULL.
    Thus, a NULL check was added for the service in the function.
    
    Other changes are:
    Instead of disabling the menu item, we are going to continue to remove it from the menu if it is not enabled. This is to keep in line with how the current Search Image on Web menu item works. See https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/renderer_context_menu/render_view_context_menu.cc;l=883;drc=5200e82d9d7290bd01292961ecdca5656a6295b7.
    
    Original change's description:
    > Reland "Add region search support for non-Google default search providers.".
    >
    > This an attempted reland of the following reverted change:
    > https://chromium-review.googlesource.com/c/chromium/src/+/3199698
    >
    > PS1 is identical to the original change with a few minor modifications to resolve merge conflicts.
    >
    > The changes from the original is the adding of an instance variable to LensRegionSearchController representing whether the default search engine is Google. This could not be used in the controller directly due to an include rule. This is now instead passed as part of the Start() function through render_view_context_menu.
    >
    >
    > Bug: 1255702,1255897, b/198833501
    > Change-Id: I83c4b0a5728ab3ba122d248c05c2f27b76a05a4b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3203970
    > Commit-Queue: Juan Mojica <juanmojica@google.com>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Ben Goldberger <benwgold@google.com>
    > Cr-Commit-Position: refs/heads/main@{#928974}
    
    Bug: 1255702,1255897, b/198833501
    Change-Id: Ie338b9b2ae53d526db11c8df7d8d506ef4cbb410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3212158
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Ben Goldberger <benwgold@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929439}

--- chrome/browser/renderer_context_menu/render_view_context_menu.h
+++ chrome/browser/renderer_context_menu/render_view_context_menu.h
@@ -225,7 +225,7 @@ class RenderViewContextMenu : public RenderViewContextMenuBase,
   void AppendClickToCallItem();
 #endif
   void AppendSharedClipboardItem();
-  void AppendLensRegionSearchItem();
+  void AppendRegionSearchItem();
   void AppendQRCodeGeneratorItem(bool for_image, bool draw_icon);
 
   std::unique_ptr<ui::DataTransferEndpoint> CreateDataEndpoint(
@@ -247,7 +247,7 @@ class RenderViewContextMenu : public RenderViewContextMenuBase,
   bool IsRouteMediaEnabled() const;
   bool IsOpenLinkOTREnabled() const;
   bool IsSearchWebForEnabled() const;
-  bool IsLensRegionSearchEnabled() const;
+  bool IsRegionSearchEnabled() const;
 
   // Command execution functions.
   void ExecOpenWebApp();
@@ -261,7 +261,8 @@ class RenderViewContextMenu : public RenderViewContextMenuBase,
   void ExecCopyLinkText();
   void ExecCopyImageAt();
   void ExecSearchLensForImage();
-  void ExecLensRegionSearch(int event_flags);
+  void ExecRegionSearch(int event_flags,
+                        bool is_google_default_search_provider);
   void ExecSearchWebForImage();
   void ExecLoadImage();
   void ExecPlayPause();
