8090db8893a824b304ed338d50c283d6880d751f
Shakti Sahu <shaktisahu@chromium.org>
2021-10-07 17:25:42 +0000

    Download Home: Removed prefetch user settings
    Removed user settings "Download articles for you on WiFi". Corresponding
    string resources were removed.
    
    Bug: 1211724
    Change-Id: I8fa4887014ab8acfbf9986cc46efbba7daa0c5a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206864
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929276}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/settings/DownloadSettingsTest.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/settings/DownloadSettingsTest.java
@@ -104,7 +104,6 @@ public class DownloadSettingsTest {
         assertPreference(DownloadSettings.PREF_LOCATION_CHANGE);
         assertPreference(DownloadSettings.PREF_LOCATION_PROMPT_ENABLED);
         assertPreference(DownloadSettings.PREF_DOWNLOAD_LATER_PROMPT_ENABLED);
-        assertPreference(DownloadSettings.PREF_PREFETCHING_ENABLED);
         mSettingsActivityTestRule.getActivity().finish();
     }
 
@@ -117,7 +116,6 @@ public class DownloadSettingsTest {
         assertPreference(DownloadSettings.PREF_LOCATION_CHANGE);
         assertPreference(DownloadSettings.PREF_LOCATION_PROMPT_ENABLED);
         assertPreference(DownloadSettings.PREF_DOWNLOAD_LATER_PROMPT_ENABLED, Matchers.nullValue());
-        assertPreference(DownloadSettings.PREF_PREFETCHING_ENABLED);
         mSettingsActivityTestRule.getActivity().finish();
     }
 
