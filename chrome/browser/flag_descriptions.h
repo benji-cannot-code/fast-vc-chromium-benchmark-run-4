b752a74a61ed154d2b28b64131c258604d89063a
Harry Cutts <hcutts@chromium.org>
2021-10-07 15:00:38 +0000

    Add a flag to enable libinput to handle touchpad
    Add a new flag to chromium to allow uses libinput to handle touchpad
    instead of the gestures library.
    
    Bug: b:192669240
    Change-Id: Ib96d8dc3274133e9a7005c47499a28b41af16c45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3166630
    Reviewed-by: Harry Cutts <hcutts@chromium.org>
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sean O'Brien <seobrien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929195}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2567,6 +2567,9 @@ extern const char kEnableInputNoiseCancellationUiDescription[];
 extern const char kEnableLauncherSearchNormalizationName[];
 extern const char kEnableLauncherSearchNormalizationDescription[];
 
+extern const char kEnableLibinputToHandleTouchpadName[];
+extern const char kEnableLibinputToHandleTouchpadDescription[];
+
 extern const char kNewDragSpecInLauncherName[];
 extern const char kNewDragSpecInLauncherDescription[];
 
