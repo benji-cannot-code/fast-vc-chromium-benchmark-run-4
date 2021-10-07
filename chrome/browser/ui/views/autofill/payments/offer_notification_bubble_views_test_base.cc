4f539022ef286597be1c00d7ed871e86fc1ed525
Yue Zhang <yuezhanggg@chromium.org>
2021-10-07 18:30:38 +0000

    [Coupon] Add IsUrlEligible function to CouponService
    This CL adds IsIsUrlEligible to CouponService and expose it to
    AutofillOfferManager to check eligibility for a URL in CouponService.
    After this CL, the basic wiring for supporting FreeListing coupons
    to show in infobar bubble is finished, and a browser test is added
    to verify this.
    
    Bug: 1248524
    Change-Id: Ic32f5311d9d77b79b1ad98beec36bf8b8fadf257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209127
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929309}

--- chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_test_base.cc
+++ chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_test_base.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autofill/autofill_uitest_util.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
+#include "chrome/browser/commerce/coupons/coupon_service.h"
+#include "chrome/browser/commerce/coupons/coupon_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -44,6 +46,7 @@ void OfferNotificationBubbleViewsTestBase::SetUpOnMainThread() {
 
   personal_data_ =
       PersonalDataManagerFactory::GetForProfile(browser()->profile());
+  coupon_service_ = CouponServiceFactory::GetForProfile(browser()->profile());
 
   // Wait for Personal Data Manager to be fully loaded to prevent that
   // spurious notifications deceive the tests.
@@ -133,6 +136,19 @@ void OfferNotificationBubbleViewsTestBase::
   personal_data_->AddOfferDataForTest(
       CreatePromoCodeOfferDataWithDomains(domains));
   personal_data_->NotifyPersonalDataObserver();
+}
+
+void OfferNotificationBubbleViewsTestBase::
+    SetUpFreeListingCouponOfferDataForCouponService(
+        std::unique_ptr<AutofillOfferData> offer) {
+  coupon_service_->DeleteAllFreeListingCoupons();
+  base::flat_map<GURL,
+                 std::vector<std::unique_ptr<autofill::AutofillOfferData>>>
+      coupon_map;
+  for (auto origin : offer->merchant_origins) {
+    coupon_map[origin].emplace_back(std::move(offer));
+  }
+  coupon_service_->UpdateFreeListingCoupons(coupon_map);
 }
 
 void OfferNotificationBubbleViewsTestBase::NavigateTo(
