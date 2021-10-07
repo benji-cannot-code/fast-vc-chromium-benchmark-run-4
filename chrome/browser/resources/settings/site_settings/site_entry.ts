1735ee76221230825f68a9c8621793e74e5748e6
sauski <sauski@google.com>
2021-10-07 10:20:42 +0000

    Site Storage Controls: Fix title on remove buttons
    The title associated with the remove buttons, which is used by screen
    readers, was not being correctly built.
    
    This CL fixes some naming errors and moves the logic into the TS file
    to ensure that the bindings run correctly.
    
    Bug: 1238757
    Change-Id: Ic3213e4350eb0af3879df0997eec6ec16c9a736b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211127
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Auto-Submit: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929088}

--- chrome/browser/resources/settings/site_settings/site_entry.ts
+++ chrome/browser/resources/settings/site_settings/site_entry.ts
@@ -20,6 +20,7 @@ import {CrLazyRenderElement} from 'chrome://resources/cr_elements/cr_lazy_render
 import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
 import {FocusRowBehavior} from 'chrome://resources/js/cr/ui/focus_row_behavior.m.js';
 import {EventTracker} from 'chrome://resources/js/event_tracker.m.js';
+import {I18nMixin, I18nMixinInterface} from 'chrome://resources/js/i18n_mixin.js';
 import {IronCollapseElement} from 'chrome://resources/polymer/v3_0/iron-collapse/iron-collapse.js';
 import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -53,8 +54,11 @@ interface SiteEntryElement {
 
 const SiteEntryElementBase =
     mixinBehaviors(
-        [FocusRowBehavior], BaseMixin(SiteSettingsMixin(PolymerElement))) as
-    {new (): PolymerElement & SiteSettingsMixinInterface & BaseMixinInterface};
+        [FocusRowBehavior],
+        BaseMixin(SiteSettingsMixin(I18nMixin(PolymerElement)))) as {
+      new (): PolymerElement & I18nMixinInterface & SiteSettingsMixinInterface &
+      BaseMixinInterface
+    };
 
 class SiteEntryElement extends SiteEntryElementBase {
   static get is() {
@@ -421,6 +425,11 @@ class SiteEntryElement extends SiteEntryElementBase {
    */
   private getClassForIndex_(index: number): string {
     return index > 0 ? 'hr' : '';
+  }
+
+  private getRemoveOriginButtonTitle_(origin: string): string {
+    return this.i18n(
+        'siteSettingsCookieRemoveSite', this.originRepresentation(origin));
   }
 
   /**
