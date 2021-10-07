77a4698e15dc827c961b2bf40c1196f82b23ab6f
Marlon Facey <mfacey@chromium.org>
2021-10-07 21:07:56 +0000

    [realbox] Add Pedals to Realbox UI
    Create the Action polymer element and connect it with the
    backend method to execute the actions. Also place it behind feature
    flag. The aim of this feature is to close the gap between the omnibox
    and realbox.
    
    Focus out: screenshot/6ibiHEQ2dTwrRLi
    Focus in: screenshot/55tJ5VyJhsPxkAE
    
    Bug: 1236284
    Change-Id: If806a2550c2fa601ede23270c257ce9a87b25e07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3177603
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929405}

--- chrome/browser/resources/new_tab_page/realbox/realbox_match.js
+++ chrome/browser/resources/new_tab_page/realbox/realbox_match.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './realbox_icon.js';
+import './realbox_action.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 import 'chrome://resources/cr_elements/cr_icons_css.m.js';
 import 'chrome://resources/cr_elements/hidden_style_css.m.js';
@@ -11,7 +12,8 @@ import 'chrome://resources/cr_elements/hidden_style_css.m.js';
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {loadTimeData} from '../i18n_setup.js';
-import {decodeString16} from '../utils.js';
+import {decodeString16, mojoTimeTicks} from '../utils.js';
+import {RealboxBrowserProxy} from './realbox_browser_proxy.js';
 
 // clang-format off
 /**
@@ -117,6 +119,15 @@ class RealboxMatchElement extends PolymerElement {
       },
 
       /**
+       * @type {boolean}
+       * @private
+       */
+      actionIsVisible_: {
+        type: Boolean,
+        computed: `computeActionIsVisible_(match)`,
+      },
+
+      /**
        * Remove button's 'aria-label' attribute.
        * @type {string}
        * @private
@@ -156,6 +167,12 @@ class RealboxMatchElement extends PolymerElement {
     };
   }
 
+  constructor() {
+    super();
+    /** @private {realbox.mojom.PageHandlerRemote} */
+    this.pageHandler_ = RealboxBrowserProxy.getInstance().handler;
+  }
+
   ready() {
     super.ready();
 
@@ -166,6 +183,39 @@ class RealboxMatchElement extends PolymerElement {
   //============================================================================
   // Event handlers
   //============================================================================
+
+  /**
+   * @param {!Event} e Event
+   * containing index of the match that was removed as well as modifier key
+   * presses.
+   * @private
+   */
+  executeAction_(e) {
+    this.pageHandler_.executeAction(
+        this.matchIndex, mojoTimeTicks(Date.now()), e.button || 0, e.altKey,
+        e.ctrlKey, e.metaKey, e.shiftKey);
+  }
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onActionClick_(e) {
+    this.executeAction_(e);
+
+    e.preventDefault();   // Prevents default browser action (navigation).
+    e.stopPropagation();  // Prevents <iron-selector> from selecting the match.
+  }
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onActionKeyDown_(e) {
+    if (e.key && (e.key === 'Enter' || e.key === ' ')) {
+      this.onActionClick_(e);
+    }
+  }
 
   /**
    * @param {!Event} e
@@ -314,6 +364,14 @@ class RealboxMatchElement extends PolymerElement {
    */
   computeHasImage_() {
     return this.match && !!this.match.imageUrl;
+  }
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeActionIsVisible_() {
+    return this.match && !!this.match.action;
   }
 
   /**
--- chrome/browser/ui/webui/realbox/realbox_handler_unittest.cc
+++ chrome/browser/ui/webui/realbox/realbox_handler_unittest.cc
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/realbox/realbox_handler.h"
 
 #include <string>
+#include <unordered_map>
 
 #include <gtest/gtest.h>
 #include "base/check.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/test/scoped_feature_list.h"
+#include "components/omnibox/browser/actions/omnibox_pedal.h"
+#include "components/omnibox/browser/actions/omnibox_pedal_implementations.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/omnibox/browser/suggestion_answer.h"
@@ -70,5 +74,15 @@ TEST_P(RealboxHandlerIconTest, VectorIcons) {
       ASSERT_FALSE(svg_name.empty());
       ASSERT_NE("search.svg", svg_name);
     }
+  }
+
+  std::unordered_map<OmniboxPedalId, scoped_refptr<OmniboxPedal>> pedals =
+      GetPedalImplementations(true, true);
+  for (auto const& it : pedals) {
+    const scoped_refptr<OmniboxPedal> pedal = it.second;
+    const gfx::VectorIcon& vector_icon = pedal->GetVectorIcon();
+    const std::string& svg_name =
+        RealboxHandler::PedalVectorIconToResourceName(vector_icon);
+    ASSERT_FALSE(svg_name.empty());
   }
 }
