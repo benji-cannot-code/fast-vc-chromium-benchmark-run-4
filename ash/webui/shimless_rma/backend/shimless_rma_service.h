4f0bb4ccfa7fea95b11283a67d1e7954ab818008
Gavin Dodd <gavindodd@google.com>
2021-10-07 23:36:30 +0000

    shimless: Clean up names to match coding standard
    Rename parameters and member variables to C++ standard.
    
    Bug: 1198187
    Change-Id: I455056d69e6cf8ffd51310842f7b549adbf38fd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208923
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#929490}

--- ash/webui/shimless_rma/backend/shimless_rma_service.h
+++ ash/webui/shimless_rma/backend/shimless_rma_service.h
@@ -130,7 +130,7 @@ class ShimlessRmaService : public mojom::ShimlessRmaService,
   // RmadClient::Observer interface.
   void Error(rmad::RmadErrorCode error) override;
   void CalibrationProgress(
-      const rmad::CalibrationComponentStatus& componentStatus) override;
+      const rmad::CalibrationComponentStatus& component_status) override;
   void CalibrationOverallProgress(
       rmad::CalibrationOverallStatus status) override;
   void ProvisioningProgress(rmad::ProvisionDeviceState::ProvisioningStep step,
@@ -138,7 +138,7 @@ class ShimlessRmaService : public mojom::ShimlessRmaService,
   void HardwareWriteProtectionState(bool enabled) override;
   void PowerCableState(bool plugged_in) override;
   void HardwareVerificationResult(const rmad::HardwareVerificationResult&
-                                      hardwareVerificationResult) override;
+                                      hardware_verification_result) override;
 
   void OsUpdateProgress(update_engine::Operation operation, double progress);
 
--- chromeos/dbus/rmad/rmad_client.h
+++ chromeos/dbus/rmad/rmad_client.h
@@ -34,7 +34,7 @@ class COMPONENT_EXPORT(RMAD) RmadClient {
 
     // Called when calibration progress is updated.
     virtual void CalibrationProgress(
-        const rmad::CalibrationComponentStatus& componentStatus) {}
+        const rmad::CalibrationComponentStatus& component_status) {}
 
     // Called when overall calibration progress is updated.
     virtual void CalibrationOverallProgress(
@@ -53,7 +53,7 @@ class COMPONENT_EXPORT(RMAD) RmadClient {
 
     // Called when hardware verification completes.
     virtual void HardwareVerificationResult(
-        const rmad::HardwareVerificationResult& hardwareVerificationResult) {}
+        const rmad::HardwareVerificationResult& hardware_verification_result) {}
   };
 
   // Creates and initializes a global instance. |bus| must not be null.
