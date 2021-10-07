81805b0efd396a3acd30a14e0ef657785e5760d4
Neil Coronado <nemco@google.com>
2021-10-07 21:13:50 +0000

    [AutoDark] Add dialog when user disables feature per-site multiple times
    - Adds disabling auto dark per-site through the app menu to the feature
    engagement system
    - When the feature has been disabled per-site enough times, a dialog
    will inform the user on how to disable the feature globally.
    
    Note: The feature engagement config uses placeholder values (e.g. how
    much the use has to disable the feature, etc.)
    
    Screenshot: https://screenshot.googleplex.com/4UzCwcKAjwBP3wF.png
    
    Bug: 1251737
    Change-Id: I1644bc6d57eb21a79205082064be4deb8a9b3c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173987
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#929411}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -22,6 +22,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHAdaptiveButtonInTopToolbarCustomizationVoiceSearchFeature,
     &kIPHAddToHomescreenMessageFeature,
     &kIPHAddToHomescreenTextBubbleFeature,
+    &kIPHAutoDarkOptOutFeature,
     &kIPHAutoDarkUserEducationMessageFeature,
     &kIPHDataSaverDetailFeature,
     &kIPHDataSaverMilestonePromoFeature,
