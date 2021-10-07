cc962962d2b5ae38a6604b120f601291e4fcc080
Wez <wez@chromium.org>
2021-10-07 11:46:51 +0000

    [fuchsia] Read and merge all JSON config-data in LoadPackageConfig().
    Read and merge all *.json files from /config/data, and merge them into
    a single config dictionary, while verifying that there are no duplicate
    values.
    
    Bug: fuchsia:85649, 1257533
    Change-Id: I89ef118d03ba6a4cc41fa6d3ff7ad934bd4e2791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3207186
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929138}

--- fuchsia/base/agent_impl_unittest.cc
+++ fuchsia/base/agent_impl_unittest.cc
@@ -34,6 +34,10 @@ class AccumulatingTestInterfaceImpl : public base::testfidl::TestInterface {
  public:
   AccumulatingTestInterfaceImpl() = default;
 
+  AccumulatingTestInterfaceImpl(const AccumulatingTestInterfaceImpl&) = delete;
+  AccumulatingTestInterfaceImpl& operator=(
+      const AccumulatingTestInterfaceImpl&) = delete;
+
   // TestInterface implementation:
   void Add(int32_t a, int32_t b, AddCallback callback) override {
     accumulated_ += a + b;
@@ -42,8 +46,6 @@ class AccumulatingTestInterfaceImpl : public base::testfidl::TestInterface {
 
  private:
   int32_t accumulated_ = 0;
-
-  DISALLOW_COPY_AND_ASSIGN(AccumulatingTestInterfaceImpl);
 };
 
 class AccumulatorComponentState : public AgentImpl::ComponentStateBase {
@@ -80,6 +82,9 @@ class AgentImplTest : public ::testing::Test {
     services_client_ =
         std::make_unique<sys::ServiceDirectory>(std::move(directory));
   }
+
+  AgentImplTest(const AgentImplTest&) = delete;
+  AgentImplTest& operator=(const AgentImplTest&) = delete;
 
   fuchsia::modular::AgentPtr CreateAgentAndConnect() {
     DCHECK(!agent_impl_);
@@ -124,8 +129,6 @@ class AgentImplTest : public ::testing::Test {
   // Set only if a keep-alive component was connected, to allow the test to
   // forcibly teardown the ComponentState for it.
   base::OnceClosure disconnect_clients_and_teardown_;
-
-  DISALLOW_COPY_AND_ASSIGN(AgentImplTest);
 };
 
 }  // namespace
