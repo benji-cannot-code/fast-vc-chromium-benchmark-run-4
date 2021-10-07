6d5c2d24a96cf85456b3a6a4390de9a33e107d46
Theresa Sullivan <twellington@chromium.org>
2021-10-07 03:47:05 +0000

    Fix NTP LogoView clickable/focusable settings
    Previously the LogoView was clickable as long as it displayed something
    other than the default logo, while technically should only be clickable
    if clicking the View will do something (i.e. there is a logo target url
    or animated logo url).
    
    If the logo is not clickable, but does have alt text, it should be
    focusable for a11y.
    
    Bug: 1257397
    Change-Id: I7afeb24dfa8669706441a6211dac49b27b833db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3210616
    Auto-Submit: Theresa  <twellington@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929030}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/LogoView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/LogoView.java
@@ -27,6 +27,7 @@ import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
@@ -217,10 +218,16 @@ public class LogoView extends FrameLayout implements OnClickListener {
         String contentDescription = TextUtils.isEmpty(logo.altText)
                 ? null
                 : getResources().getString(R.string.accessibility_google_doodle, logo.altText);
-        updateLogo(logo.image, contentDescription, false);
+        updateLogo(
+                logo.image, contentDescription, /* isDefaultLogo = */ false, isLogoClickable(logo));
     }
 
-    private void updateLogo(Bitmap logo, final String contentDescription, boolean isDefaultLogo) {
+    private static boolean isLogoClickable(Logo logo) {
+        return !TextUtils.isEmpty(logo.animatedLogoUrl) || !TextUtils.isEmpty(logo.onClickUrl);
+    }
+
+    private void updateLogo(Bitmap logo, final String contentDescription, boolean isDefaultLogo,
+            boolean isClickable) {
         assert logo != null;
 
         if (mFadeAnimation != null) mFadeAnimation.end();
@@ -256,7 +263,8 @@ public class LogoView extends FrameLayout implements OnClickListener {
                 mTransitionAmount = 0f;
                 mFadeAnimation = null;
                 setContentDescription(contentDescription);
-                setClickable(!mNewLogoIsDefault);
+                setClickable(isClickable);
+                setFocusable(isClickable || !TextUtils.isEmpty(contentDescription));
             }
 
             @Override
@@ -275,7 +283,7 @@ public class LogoView extends FrameLayout implements OnClickListener {
     private boolean maybeShowDefaultLogo() {
         Bitmap defaultLogo = getDefaultLogo();
         if (defaultLogo != null) {
-            updateLogo(defaultLogo, null, true);
+            updateLogo(defaultLogo, null, /* isDefaultLogo = */ true, /* isClickable = */ false);
             return true;
         }
         return false;
@@ -411,5 +419,10 @@ public class LogoView extends FrameLayout implements OnClickListener {
         if (view == this && mDelegate != null && !isTransitioning()) {
             mDelegate.onLogoClicked(isAnimatedLogoShowing());
         }
+    }
+
+    @VisibleForTesting
+    void endAnimationsForTesting() {
+        mFadeAnimation.end();
     }
 }
