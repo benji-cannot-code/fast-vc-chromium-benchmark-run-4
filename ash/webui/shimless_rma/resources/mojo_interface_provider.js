b09514f26f4d03b8da686adc137fcb3ea8498648
Gavin Dodd <gavindodd@google.com>
2021-10-07 00:16:46 +0000

    shimless: Display RSU code in rows.
    Split RSU code into rows with 4 x 4 characters in each row.
    
    Screenshot: https://screenshot.googleplex.com/5fixaBfV2CyGHNc
    
    Bug: 1198187
    Change-Id: I643807bdffdfe9567281668b68f65b8ef2dd6eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206105
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#928960}

--- ash/webui/shimless_rma/resources/mojo_interface_provider.js
+++ ash/webui/shimless_rma/resources/mojo_interface_provider.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from 'chrome://resources/js/assert.m.js';
 
-import {fakeCalibrationComponents, fakeChromeVersion, fakeComponents, fakeDeviceRegions, fakeDeviceSkus, fakeRsuChallengeQrCode, fakeStates} from './fake_data.js';
+import {fakeCalibrationComponents, fakeChromeVersion, fakeComponents, fakeDeviceRegions, fakeDeviceSkus, fakeRsuChallengeCode, fakeRsuChallengeQrCode, fakeStates} from './fake_data.js';
 import {FakeShimlessRmaService} from './fake_shimless_rma_service.js'
 import {CalibrationSetupInstruction, NetworkConfigServiceInterface, RmadErrorCode, ShimlessRmaService, ShimlessRmaServiceInterface} from './shimless_rma_types.js';
 
@@ -53,7 +53,7 @@ function setupFakeShimlessRmaService_() {
   service.automaticallyTriggerDisableWriteProtectionObservation();
   service.automaticallyTriggerCalibrationObservation();
 
-  service.setGetRsuDisableWriteProtectChallengeResult('###challenge-code###')
+  service.setGetRsuDisableWriteProtectChallengeResult(fakeRsuChallengeCode)
   service.setGetRsuDisableWriteProtectHwidResult('### hwid ###')
   service.setGetRsuDisableWriteProtectChallengeQrCodeResponse(
       fakeRsuChallengeQrCode);
