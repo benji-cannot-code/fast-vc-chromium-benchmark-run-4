68de5fcb92779423de0ffdacadb365c692ed6315
S. Ganesh <ganesh@chromium.org>
2021-10-07 22:08:46 +0000

    Add utility function `RunElevated` for non-elevated Per-system Setup.
    Bug: 1254126
    Change-Id: I7ee023807c17c4789bdd27e95013d0994e48bc2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208857
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Auto-Submit: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929440}

--- chrome/updater/win/win_util.h
+++ chrome/updater/win/win_util.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_types.h"
 #include "chrome/updater/updater_scope.h"
 
+namespace base {
+class FilePath;
+}
+
 namespace updater {
 
 // Returns the last error as an HRESULT or E_FAIL if last error is NO_ERROR.
@@ -154,6 +158,14 @@ std::wstring GetTaskDisplayName(UpdaterScope scope);
 // Returns `KEY_WOW64_32KEY | access`. All registry access under the Updater key
 // should use `Wow6432(access)` as the `REGSAM`.
 REGSAM Wow6432(REGSAM access);
+
+// Starts a new elevated process. `file_path` specifies the program to be run.
+// `parameters` can be an empty string.
+// The function waits until the spawned process has completed. The exit code of
+// the process is returned in `exit_code`.
+HRESULT RunElevated(const base::FilePath& file_path,
+                    const std::wstring& parameters,
+                    DWORD* exit_code);
 
 }  // namespace updater
 
