faadd23d7988320448ee33f77e6f3e0d52f79bc3
danakj <danakj@chromium.org>
2021-10-07 20:31:42 +0000

    Add support for dependency chains with multiple crate versions
    If a crate depends on Foo version X, but it also depends transitively on
    Foo version Y, then Rust will no longer compile, unless you give each of
    these Foo libraries different metadata via the -Cmetadata command line
    switch.
    
    Commonly the metadata is a hash of the crate's contents, but for Chrome
    each version would be a different GN rule, so we can specify the
    metadata from there, as the version epoch. In the future, we could hash
    the path to avoid the need for BUILD.gn to specify anything.
    
    R=adetaylor@chromium.org
    
    Bug: 1069271
    Change-Id: I4177e301be8eeb05994528574b0030ae85f74e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3212687
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929383}

---
+++ build/rust/tests/test_rust_multiple_dep_versions_exe/main.rs
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+fn main() {
+    test_lib::say_hello_from_v1();
+    transitive_v2::transitively_say_hello();
+}
