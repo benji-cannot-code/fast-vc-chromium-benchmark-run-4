35280b4621de1c3497e1b4e96d0b23b14790c396
Ashley Prasad <ashleydp@google.com>
2021-10-07 17:56:57 +0000

    Diagnostics: Announce diagnostics completion state
    - Adds 'Diagnostics completed' announce to when all routine-
      executor completes.
    - Tests that announcement occurs when routine execution is
      completed.
    - Tests that announcement does not occur when routines execution
      is cancelled.
    
    Bug: 1197335
    Change-Id: I6e9bef3e939ac7ce790b1991f69e64b2c871f660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209749
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929294}

--- ash/webui/diagnostics_ui/resources/routine_section.js
+++ ash/webui/diagnostics_ui/resources/routine_section.js
@@ -16,6 +16,7 @@ import './strings.m.js';
 import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+import {IronA11yAnnouncer} from 'chrome://resources/polymer/v3_0/iron-a11y-announcer/iron-a11y-announcer.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {PowerRoutineResult, RoutineType, StandardRoutineResult, SystemRoutineControllerInterface} from './diagnostics_types.js';
@@ -54,6 +55,16 @@ Polymer({
   systemRoutineController_: null,
 
   properties: {
+    /**
+     * Added to support testing of announce behavior.
+     * @private
+     * @type {string}
+     */
+    announcedText_: {
+      type: String,
+      value: '',
+    },
+
     /** @type {!Array<RoutineGroup|RoutineType>} */
     routines: {
       type: Array,
@@ -212,6 +223,11 @@ Polymer({
     'onActivePageChanged_(isActive)'
   ],
 
+  /** @override */
+  attached() {
+    IronA11yAnnouncer.requestAvailability();
+  },
+
   /**
    * @param {string} buttonText
    * @return {string}
@@ -328,6 +344,13 @@ Polymer({
     }
   },
 
+  /** @private */
+  announceRoutinesComplete_() {
+    this.announcedText_ =
+        loadTimeData.getString('testOnRoutinesCompletedText');
+    this.fire('iron-announce', {text: `${this.announcedText_}`});
+  },
+
   /** @param {!ExecutionProgress} status */
   handleRoutinesCompletedStatus_(status) {
     this.executionStatus_ = status;
@@ -344,6 +367,7 @@ Polymer({
       this.badgeText_ = this.failedTest_ ?
           loadTimeData.getString('testFailedBadgeText') :
           loadTimeData.getString('testSucceededBadgeText');
+      this.announceRoutinesComplete_();
     }
   },
 
