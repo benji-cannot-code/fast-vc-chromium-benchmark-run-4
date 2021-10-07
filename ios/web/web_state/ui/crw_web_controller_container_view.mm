18d24e763b303b76dad0c00201bd56c2d5882cb3
Gauthier Ambard <gambard@chromium.org>
2021-10-07 13:47:08 +0000

    [iOS] Remove transient leftovers
    When the transient items/views were removed, the code wasn't fully
    cleaned up. This CL removes some leftovers.
    
    Fixed: 570114
    Change-Id: Ie0216ef38af730f6f9e63f1c0b5ccf776e977508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211488
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929173}

--- ios/web/web_state/ui/crw_web_controller_container_view.mm
+++ ios/web/web_state/ui/crw_web_controller_container_view.mm
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Redefine properties as readwrite.
 @property(nonatomic, strong, readwrite)
     CRWWebViewContentView* webViewContentView;
-@property(nonatomic, strong, readwrite) CRWContentView* transientContentView;
 
 // Convenience getter for the proxy object.
 @property(nonatomic, weak, readonly) CRWWebViewProxyImpl* contentViewProxy;
@@ -31,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation CRWWebControllerContainerView
 @synthesize webViewContentView = _webViewContentView;
-@synthesize transientContentView = _transientContentView;
 @synthesize delegate = _delegate;
 
 - (instancetype)initWithDelegate:
@@ -80,13 +78,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
-- (void)setTransientContentView:(CRWContentView*)transientContentView {
-  if (![_transientContentView isEqual:transientContentView]) {
-    [_transientContentView removeFromSuperview];
-    _transientContentView = transientContentView;
-  }
-}
-
 - (CRWWebViewProxyImpl*)contentViewProxy {
   return [_delegate contentViewProxyForContainerView:self];
 }
@@ -109,20 +100,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // layout updates need to occur despite the bounds size staying constant.
   self.webViewContentView.frame = self.bounds;
   [self.webViewContentView setNeedsLayout];
-
-  // TODO(crbug.com/570114): Move adding of the following subviews to another
-  // place.
-  // transientContentView layout.
-  if (self.transientContentView) {
-    if (!self.transientContentView.superview)
-      [self addSubview:self.transientContentView];
-    [self bringSubviewToFront:self.transientContentView];
-    self.transientContentView.frame = self.bounds;
-  }
 }
 
 - (BOOL)isViewAlive {
-  return self.webViewContentView || self.transientContentView;
+  return self.webViewContentView;
 }
 
 - (void)willMoveToWindow:(UIWindow*)newWindow {
@@ -158,29 +139,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)resetContent {
   self.webViewContentView = nil;
-  self.transientContentView = nil;
   self.contentViewProxy.contentView = nil;
 }
 
 - (void)displayWebViewContentView:(CRWWebViewContentView*)webViewContentView {
   DCHECK(webViewContentView);
   self.webViewContentView = webViewContentView;
-  self.transientContentView = nil;
   self.contentViewProxy.contentView = self.webViewContentView;
   [self updateWebViewContentViewForContainerWindow:self.window];
   [self setNeedsLayout];
-}
-
-- (void)displayTransientContent:(CRWContentView*)transientContentView {
-  DCHECK(transientContentView);
-  self.transientContentView = transientContentView;
-  self.contentViewProxy.contentView = self.transientContentView;
-  [self setNeedsLayout];
-}
-
-- (void)clearTransientContentView {
-  self.transientContentView = nil;
-  self.contentViewProxy.contentView = self.webViewContentView;
 }
 
 #pragma mark UIView (printing)
