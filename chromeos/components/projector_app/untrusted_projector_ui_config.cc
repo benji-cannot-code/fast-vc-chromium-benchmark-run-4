383ac2997260e2c55b2829e7ffa833953f3eef6d
Zork <zork@google.com>
2021-10-07 01:51:07 +0000

    Add base infrastructure for sending feature flags to Projector App.
    Bug: b/201666699
    Change-Id: Iddf2e68d2f91c8f119643f33987af66b6bb2636f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208925
    Commit-Queue: Zork <zork@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928990}

--- chromeos/components/projector_app/untrusted_projector_ui_config.cc
+++ chromeos/components/projector_app/untrusted_projector_ui_config.cc
@@ -61,6 +61,10 @@ content::WebUIDataSource* CreateProjectorHTMLSource() {
 
   source->AddFrameAncestor(GURL(kChromeUITrustedProjectorUrl));
 
+  // TODO(b/201666699): Move this into a delegate, and populate with real data.
+  source->AddBoolean("isDevChannel", true);
+  source->UseStringsJs();
+
   return source;
 }
 
