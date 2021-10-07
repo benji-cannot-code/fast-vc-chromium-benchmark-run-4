90ae559f3777e45edd8ee401b650448a394cb96d
Kent Tamura <tkent@chromium.org>
2021-10-07 10:14:06 +0000

    Layout: Do not refer to FontBaseline of inline items with dominant-baseline:auto
    crrev.com/888805 affected non-SVG elements in an orthogonal
    writing-mode.
    
    This CL reverts the most part of crrev.com/888805, and applies the
    FontBaseline of an item only if its dominant-baseline is not "auto".
    
    Bug: 1241356
    Change-Id: Id749fc583c1f47a9b5d7ffc7840bd20b2f8b4b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3210291
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929076}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_box_state.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_box_state.cc
@@ -115,8 +115,12 @@ void NGInlineBoxState::ResetStyle(const ComputedStyle& style_ref,
 }
 
 void NGInlineBoxState::ComputeTextMetrics(const ComputedStyle& styleref,
-                                          const Font& fontref) {
-  const auto baseline_type = styleref.GetFontBaseline();
+                                          const Font& fontref,
+                                          FontBaseline ifc_baseline) {
+  const auto baseline_type =
+      styleref.CssDominantBaseline() == EDominantBaseline::kAuto
+          ? ifc_baseline
+          : styleref.GetFontBaseline();
   if (const SimpleFontData* font_data = fontref.PrimaryFont()) {
     if (is_svg_text) {
       text_metrics =
@@ -154,9 +158,10 @@ void NGInlineBoxState::ResetTextMetrics() {
 }
 
 void NGInlineBoxState::EnsureTextMetrics(const ComputedStyle& styleref,
-                                         const Font& fontref) {
+                                         const Font& fontref,
+                                         FontBaseline ifc_baseline) {
   if (text_metrics.IsEmpty())
-    ComputeTextMetrics(styleref, fontref);
+    ComputeTextMetrics(styleref, fontref, ifc_baseline);
 }
 
 void NGInlineBoxState::AccumulateUsedFonts(
@@ -240,7 +245,8 @@ NGInlineBoxState* NGInlineLayoutStateStack::OnBeginPlaceItems(
     // line height properties) as the initial metrics for the line box.
     // https://drafts.csswg.org/css2/visudet.html#strut
     if (!line_height_quirk) {
-      line_box_state.ComputeTextMetrics(line_style, *line_box_state.font);
+      line_box_state.ComputeTextMetrics(line_style, *line_box_state.font,
+                                        baseline_type);
     }
   }
 
