2e1ebb848f0714dea441c07421e5dbbd35dc55e4
K. Moon <kmoon@chromium.org>
2021-10-07 16:57:36 +0000

    Partially reverts "[unseasoned-pdf] Let chrome://print create in-process plugin"
    Partially reverts commit c93c531c91d7fbbfe3b1c9076b4440141edca6d1.
    
    More work is required to actually move the unseasoned plugin out of the
    Print Preview process, but this change goes back to just checking the
    origin of the creator is either the PDF viewer or Print Preview, rather
    than checking frame relationships.
    
    The original change made the assumption that loading plugin content into
    the chrome://print origin was equivalent in both Pepper and unseasoned
    modes. Unlike a Pepper plugin process, however, chrome://print renderers
    have more privileges than normal.
    
    Bug: 1253984
    Change-Id: I1c686c1b74687e7a6e563869320b606d40825472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3191123
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929252}

--- components/pdf/renderer/pdf_internal_plugin_delegate.h
+++ components/pdf/renderer/pdf_internal_plugin_delegate.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PDF_RENDERER_PDF_INTERNAL_PLUGIN_DELEGATE_H_
 #define COMPONENTS_PDF_RENDERER_PDF_INTERNAL_PLUGIN_DELEGATE_H_
 
-namespace blink {
-class WebFrame;
-}  // namespace blink
+namespace url {
+class Origin;
+}  // namespace url
 
 namespace pdf {
 
@@ -19,8 +19,10 @@ class PdfInternalPluginDelegate {
   PdfInternalPluginDelegate();
   virtual ~PdfInternalPluginDelegate();
 
-  // Returns `true` if the frame is allowed to create the internal PDF plugin.
-  virtual bool IsAllowedFrame(const blink::WebFrame& frame) const;
+  // Returns `true` if the origin is allowed to create the internal PDF plugin.
+  // Note that this applies to the origin of the parent of the frame that
+  // contains the in-process plugin.
+  virtual bool IsAllowedOrigin(const url::Origin& origin) const;
 };
 
 }  // namespace pdf
