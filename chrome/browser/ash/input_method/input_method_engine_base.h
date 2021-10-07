cf6f2c78128c2f03cd2a531a1068b9eaf4233af8
John Palmer <jopalmer@chromium.org>
2021-10-07 03:10:15 +0000

    Add API for getting Text field bounds via inputmethodprivate
    Bug: b:201024029
    Change-Id: I79126129e0cf3dfd2f508f009148a711a3aebe75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3180630
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929021}

--- chrome/browser/ash/input_method/input_method_engine_base.h
+++ chrome/browser/ash/input_method/input_method_engine_base.h
@@ -206,6 +206,7 @@ class InputMethodEngineBase : virtual public ui::IMEEngineHandlerInterface,
   gfx::Range GetAutocorrectRange(int context_id, std::string* error);
 
   gfx::Rect GetAutocorrectCharacterBounds(int context_id, std::string* error);
+  gfx::Rect GetTextFieldBounds(int context_id, std::string* error);
 
   bool SetAutocorrectRange(int context_id,
                            const gfx::Range& range,
@@ -284,6 +285,7 @@ class InputMethodEngineBase : virtual public ui::IMEEngineHandlerInterface,
   virtual gfx::Range GetAutocorrectRange() = 0;
 
   virtual gfx::Rect GetAutocorrectCharacterBounds() = 0;
+  virtual gfx::Rect GetTextFieldBounds() = 0;
 
   // Notifies the InputContextHandler that the autocorrect range should
   // be updated and the autocorrect text has updated.
