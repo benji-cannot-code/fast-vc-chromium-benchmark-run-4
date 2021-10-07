0bbd86a9373189226e8e783e4d1e71b1c0d459e7
Alexander Timin <altimin@chromium.org>
2021-10-07 01:31:39 +0000

    [content/test] Add test name to RunTestOnMainThread
    Capture test name and location in RunTestOnMainThread to make debugging easier.
    
    R=kuanhuang@google.com
    
    Change-Id: Idb45952bdfb2039be398ccd2e3c123f5dd61718d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3210406
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#928986}

--- content/public/test/browser_test_base.cc
+++ content/public/test/browser_test_base.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
+#include "base/trace_event/typed_macros.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "components/tracing/common/tracing_switches.h"
@@ -868,7 +869,10 @@ void BrowserTestBase::ProxyRunTestOnMainThreadLoop() {
     InitializeNetworkProcess();
 
     {
-      TRACE_EVENT0("test", "RunTestOnMainThread");
+      auto* test = ::testing::UnitTest::GetInstance()->current_test_info();
+      TRACE_EVENT("test", "RunTestOnMainThread", "test_name",
+                  test->test_suite_name() + std::string(".") + test->name(),
+                  "file", test->file(), "line", test->line());
       base::ScopedDisallowBlocking disallow_blocking;
       RunTestOnMainThread();
     }
