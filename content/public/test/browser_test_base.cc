b1eb0cd821ac6b9590dba8a86101a5eb0acb1063
Scott Violet <sky@chromium.org>
2021-10-07 19:56:13 +0000

    content: remove unnecessary test code
    As content-browser doesn't use v8, this is not necessary.
    
    BUG=764576
    
    Change-Id: If8401e9608321313a569ddcbc4b613edd46300ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209636
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Scott Violet <sky@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929359}

--- content/public/test/browser_test_base.cc
+++ content/public/test/browser_test_base.cc
@@ -102,10 +102,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_paths.h"
 #include "testing/android/native_test/native_browser_test_support.h"
 #include "ui/base/ui_base_paths.h"
-
-#ifdef V8_USE_EXTERNAL_STARTUP_DATA
-#include "gin/v8_initializer.h"  // nogncheck
-#endif
 #endif
 
 #if defined(OS_MAC)
@@ -586,10 +582,6 @@ void BrowserTestBase::SetUp() {
 
   base::i18n::AllowMultipleInitializeCallsForTesting();
   base::i18n::InitializeICU();
-
-#ifdef V8_USE_EXTERNAL_STARTUP_DATA
-  gin::V8Initializer::LoadV8Snapshot();
-#endif
 
   ContentMainDelegate* delegate = GetContentMainDelegateForTesting();
   // The delegate should have been set by JNI_OnLoad for the test target.
