d75d030bcd1f79883d46b50a6b152cc484779f17
Curtis McMullan <curtismcmullan@chromium.org>
2021-10-07 04:54:28 +0000

    lp: add LanguagePacksImpl resolver for extensions
    LanguagePacks will be used from an extension, and as such we need to
    route any js requests for LanguagePacks receivers to the correct
    implementation. This CL adds the required logic to resolve
    mojo::PendingReceiver<language::mojom::LanguagePacks> to
    LanguagePacksImpl.
    
    Bug: 1194688
    Change-Id: I3c2a21d9ea4389de0611978978c9d71a84eb7e83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205096
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929042}

--- chrome/browser/extensions/chrome_extensions_browser_interface_binders.cc
+++ chrome/browser/extensions/chrome_extensions_browser_interface_binders.cc
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/camera_app_ui/camera_app_ui.h"
 #include "chromeos/components/chromebox_for_meetings/buildflags/buildflags.h"
 #include "chromeos/components/remote_apps/mojom/remote_apps.mojom.h"
+#include "chromeos/language/language_packs/language_packs_impl.h"
+#include "chromeos/language/public/mojom/language_packs.mojom.h"
 #include "chromeos/services/media_perception/public/mojom/media_perception.mojom.h"
 #include "chromeos/services/tts/public/mojom/tts_service.mojom.h"
 #include "extensions/browser/api/extensions_api_client.h"
@@ -62,6 +64,13 @@ void BindInputEngineManager(
     content::RenderFrameHost* render_frame_host,
     mojo::PendingReceiver<chromeos::ime::mojom::InputEngineManager> receiver) {
   ash::input_method::InputMethodManager::Get()->ConnectInputEngineManager(
+      std::move(receiver));
+}
+
+void BindLanguagePacks(
+    content::RenderFrameHost* render_frame_host,
+    mojo::PendingReceiver<chromeos::language::mojom::LanguagePacks> receiver) {
+  chromeos::language_packs::LanguagePacksImpl::GetInstance().BindReceiver(
       std::move(receiver));
 }
 
@@ -123,6 +132,8 @@ void PopulateChromeFrameBindersForExtension(
   if (extension->id() == ash::extension_ime_util::kXkbExtensionId) {
     binder_map->Add<chromeos::ime::mojom::InputEngineManager>(
         base::BindRepeating(&BindInputEngineManager));
+    binder_map->Add<chromeos::language::mojom::LanguagePacks>(
+        base::BindRepeating(&BindLanguagePacks));
     binder_map->Add<chromeos::machine_learning::mojom::MachineLearningService>(
         base::BindRepeating(&BindMachineLearningService));
   }
