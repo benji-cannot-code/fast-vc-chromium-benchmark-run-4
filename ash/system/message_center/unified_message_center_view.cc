a423892b5dfb2184622147274102148475ad4bbf
Ahmed Mehfooz <amehfooz@chromium.org>
2021-10-07 22:53:09 +0000

    Add rounded corners for message center scroll view
    The scroll_view is now painted to a layer and has rounded corners.
    We won't add rounded corners to individual notifications in
    UnifiedMessageListView anymore.
    
    Bug: 1243889
    Change-Id: I4e7f9126f25ca10a3049962c51fa474420a6e095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208679
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929461}

--- ash/system/message_center/unified_message_center_view.cc
+++ ash/system/message_center/unified_message_center_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/metrics/user_metrics.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/compositor/layer.h"
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/views/message_view.h"
@@ -108,6 +109,11 @@ void UnifiedMessageCenterView::Init() {
   scroller_->SetBackgroundColor(absl::nullopt);
   scroller_->SetVerticalScrollBar(base::WrapUnique(scroll_bar_));
   scroller_->SetDrawOverflowIndicator(false);
+  if (is_notifications_refresh_enabled_) {
+    scroller_->SetPaintToLayer();
+    scroller_->layer()->SetRoundedCornerRadius(
+        gfx::RoundedCornersF{kMessageCenterScrollViewCornerRadius});
+  }
   AddChildView(scroller_);
 
   notification_bar_->Update(
@@ -239,9 +245,11 @@ void UnifiedMessageCenterView::Layout() {
       scroller_bottom_inset += kMessageCenterBottomPadding;
 
     gfx::Rect scroller_bounds(GetContentsBounds());
-    scroller_bounds.Inset(
-        gfx::Insets(notification_bar_height - notification_bar_offset, 0,
-                    scroller_bottom_inset, 0));
+    int scroller_side_padding =
+        is_notifications_refresh_enabled_ ? kMessageCenterSidePadding : 0;
+    scroller_bounds.Inset(gfx::Insets(
+        notification_bar_height - notification_bar_offset,
+        scroller_side_padding, scroller_bottom_inset, scroller_side_padding));
     scroller_->SetBoundsRect(scroller_bounds);
   } else {
     scroller_->SetBoundsRect(GetContentsBounds());
