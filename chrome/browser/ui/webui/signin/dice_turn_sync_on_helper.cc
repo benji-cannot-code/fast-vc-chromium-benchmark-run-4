19c52f2fa90149f6d91165ca834fa9ba4e6cb4b3
Jan Krcal <jkrcal@chromium.org>
2021-10-07 12:44:53 +0000

    [Dent] Make turn sync on helper compile with mirror
    This CL is the first step towards reusing (Dice)TurnSyncOnHelper on
    lacros. The CL only lifts a compile-time restriction, moves the build
    target to all desktop platforms and marks platform dependent pieces of
    code with #ifdefs and NOTIMPLEMENTED().
    
    While touching the chrome/test/BUILD.gn file, the CL makes minor
    independent changes - including the profile picker tests on lacros.
    
    Bug: 1248047
    Change-Id: Ia2d6c845639cc8151e555ca1eef75771e54aee31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199757
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929152}

--- chrome/browser/ui/webui/signin/dice_turn_sync_on_helper.cc
+++ chrome/browser/ui/webui/signin/dice_turn_sync_on_helper.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile_metrics.h"
 #include "chrome/browser/signin/account_id_from_account_info.h"
-#include "chrome/browser/signin/dice_signed_in_profile_creator.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_features.h"
 #include "chrome/browser/signin/signin_util.h"
@@ -56,6 +55,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/driver/sync_user_settings.h"
 #include "components/unified_consent/unified_consent_service.h"
 #include "content/public/browser/storage_partition.h"
+
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+#include "chrome/browser/signin/dice_signed_in_profile_creator.h"
+#endif
 
 namespace {
 
@@ -443,6 +446,7 @@ void DiceTurnSyncOnHelper::OnProviderUpdatePropagated(
 }
 
 void DiceTurnSyncOnHelper::CreateNewSignedInProfile() {
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   DCHECK(!dice_signed_in_profile_creator_);
   // Unretained is fine because the profile creator is owned by this.
   dice_signed_in_profile_creator_ =
@@ -452,6 +456,9 @@ void DiceTurnSyncOnHelper::CreateNewSignedInProfile() {
           /*use_guest=*/false,
           base::BindOnce(&DiceTurnSyncOnHelper::OnNewSignedInProfileCreated,
                          base::Unretained(this)));
+#else
+  NOTIMPLEMENTED() << "Creating profiles is not yet supported on lacros.";
+#endif
 }
 
 syncer::SyncService* DiceTurnSyncOnHelper::GetSyncService() {
@@ -461,6 +468,7 @@ syncer::SyncService* DiceTurnSyncOnHelper::GetSyncService() {
 }
 
 void DiceTurnSyncOnHelper::OnNewSignedInProfileCreated(Profile* new_profile) {
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   DCHECK(dice_signed_in_profile_creator_);
   dice_signed_in_profile_creator_.reset();
   ProfileMetrics::LogProfileAddNewUser(ProfileMetrics::ADD_NEW_USER_SYNC_FLOW);
@@ -485,6 +493,9 @@ void DiceTurnSyncOnHelper::OnNewSignedInProfileCreated(Profile* new_profile) {
     // No policy to load - simply complete the signin process.
     SigninAndShowSyncConfirmationUI();
   }
+#else
+  NOTIMPLEMENTED() << "Creating profiles is not yet supported for lacros.";
+#endif
 }
 
 void DiceTurnSyncOnHelper::SigninAndShowSyncConfirmationUI() {
@@ -614,12 +625,18 @@ void DiceTurnSyncOnHelper::FinishSyncSetupAndDelete(
       break;
     }
     case LoginUIService::ABORT_SYNC: {
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
       auto* primary_account_mutator =
           identity_manager_->GetPrimaryAccountMutator();
       DCHECK(primary_account_mutator);
       primary_account_mutator->RevokeSyncConsent(
           signin_metrics::ABORT_SIGNIN,
           signin_metrics::SignoutDelete::kIgnoreMetric);
+#else
+      // TODO(crbug.com/1248047): We should support this flow by disabling all
+      // data types instead of revoking the sync consent.
+      NOTIMPLEMENTED() << "Not syncing is not yet supported on lacros.";
+#endif
       AbortAndDelete();
       return;
     }
@@ -677,6 +694,7 @@ void DiceTurnSyncOnHelper::AbortAndDelete() {
     policy::UserPolicySigninServiceFactory::GetForProfile(profile_)
         ->ShutdownUserCloudPolicyManager();
   }
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   if (signin_aborted_mode_ == SigninAbortedMode::REMOVE_ACCOUNT) {
     if (base::FeatureList::IsEnabled(kAccountPoliciesLoadedWithoutSync)) {
       policy::UserPolicySigninServiceFactory::GetForProfile(profile_)
@@ -690,6 +708,10 @@ void DiceTurnSyncOnHelper::AbortAndDelete() {
         signin_metrics::SourceForRefreshTokenOperation::
             kDiceTurnOnSyncHelper_Abort);
   }
+#else
+  NOTIMPLEMENTED()
+      << "Profiles without accounts are not yet supported on lacros.";
+#endif
 
   delete this;
 }
--- chrome/browser/ui/webui/signin/dice_turn_sync_on_helper.h
+++ chrome/browser/ui/webui/signin/dice_turn_sync_on_helper.h
@@ -22,13 +22,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/identity_manager/account_info.h"
 
-#if !BUILDFLAG(ENABLE_DICE_SUPPORT)
-#error "This file should only be included if DICE support is enabled"
+#if !BUILDFLAG(ENABLE_DICE_SUPPORT) && !BUILDFLAG(ENABLE_MIRROR)
+#error "This file should only be included if DICE support / mirror is enabled"
 #endif
 
 class Browser;
-class DiceSignedInProfileCreator;
 class SigninUIError;
+
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+class DiceSignedInProfileCreator;
+#endif
 
 namespace signin {
 class IdentityManager;
@@ -41,6 +44,8 @@ class SyncSetupInProgressHandle;
 
 // Handles details of setting the primary account with IdentityManager and
 // turning on sync for an account for which there is already a refresh token.
+// TODO(crbug.com/1248047): Rename this to TurnSyncOnHelper to reflect this can
+// also be used with mirror.
 class DiceTurnSyncOnHelper
     : public SyncStartupTracker::Observer,
       public policy::PolicyService::ProviderUpdateObserver {
@@ -280,7 +285,9 @@ class DiceTurnSyncOnHelper
   base::ScopedClosureRunner scoped_callback_runner_;
 
   std::unique_ptr<SyncStartupTracker> sync_startup_tracker_;
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   std::unique_ptr<DiceSignedInProfileCreator> dice_signed_in_profile_creator_;
+#endif
   base::CallbackListSubscription shutdown_subscription_;
   bool enterprise_account_confirmed_ = false;
 
