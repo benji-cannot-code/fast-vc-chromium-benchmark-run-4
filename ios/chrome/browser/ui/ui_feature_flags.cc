8e30da52fae7af627fcf2ab6edb96f04e6c26c36
Aliona DANGLA <alionadangla@google.com>
2021-10-08 17:29:45 +0000

    [iOS] Remove kEnableFREDefaultBrowserScreen flag
    As fre_field_trial and default_browser_field_trial have been merged in
    crrev.com/c/chromium/src/+/3211960 the flag is not needed any more.
    
    Fixed: 1257195
    Change-Id: I2fbdc7cf125387a635e15abe560ec872f68999a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3214031
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929752}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -14,9 +14,6 @@ const base::Feature kTestFeature{"TestFeature",
 const base::Feature kSharedHighlightingIOS{"SharedHighlightingIOS",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kEnableFREDefaultBrowserScreen{
-    "EnableFREDefaultBrowserScreen", base::FEATURE_DISABLED_BY_DEFAULT};
-
 const base::Feature kEnableFREDefaultBrowserScreenTesting{
     "EnableFREDefaultBrowserScreenTesting", base::FEATURE_DISABLED_BY_DEFAULT};
 
