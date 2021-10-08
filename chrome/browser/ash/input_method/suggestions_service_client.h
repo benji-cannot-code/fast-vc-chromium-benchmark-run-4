3e8d248b994aa9fba4a150934b157534175470a2
Curtis McMullan <curtismcmullan@chromium.org>
2021-10-08 03:58:23 +0000

    multiword: enable ime with predictive writing pref
    The code in this CL was reverted due to a use after free error found in
    the asan build bot - see crrev/c/3212533.
    
    The issue was a dangling ptr dereference found in
    suggestions_service_client.cc.
    
    This CL fixes the asan issue and adds the reverted code back.
    
    Bug: 1146266
    Change-Id: I84119b02c8dfe7a1c3e31987ffbc555074d5d0fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211342
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929559}

--- chrome/browser/ash/input_method/suggestions_service_client.h
+++ chrome/browser/ash/input_method/suggestions_service_client.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/input_method/suggestions_source.h"
 #include "chromeos/services/ime/public/cpp/suggestions.h"
@@ -43,6 +44,8 @@ class SuggestionsServiceClient : public AsyncSuggestionsSource {
   mojo::Remote<chromeos::machine_learning::mojom::TextSuggester>
       text_suggester_;
   bool text_suggester_loaded_ = false;
+
+  base::WeakPtrFactory<SuggestionsServiceClient> weak_ptr_factory_{this};
 };
 
 }  // namespace input_method
