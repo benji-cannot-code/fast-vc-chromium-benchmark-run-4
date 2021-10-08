e1f299135a503ea6c6afb25dc696215b9414553f
Elly Fong-Jones <ellyjones@chromium.org>
2021-10-08 23:05:09 +0000

    desktop-share-menu: wire into omnibox context menu
    Currently, the omnibox context menu conditionally includes a
    send-tab-to-self item (or submenu). This change, when the
    share submenu is enabled, replaces that item with the share submenu
    instead, which offers both the existing STTS functionality as well
    as the other share options (generate QR code, copy link, and the
    various 3P share options).
    
    Bug: 1250494
    Change-Id: Icfcfc1260f10c009c55b0897f4b8481c7221d283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211928
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929909}

--- chrome/browser/share/share_submenu_model.cc
+++ chrome/browser/share/share_submenu_model.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/send_tab_to_self/send_tab_to_self_desktop_util.h"
 #include "chrome/browser/send_tab_to_self/send_tab_to_self_util.h"
+#include "chrome/browser/share/share_features.h"
 #include "chrome/browser/share/share_metrics.h"
 #include "chrome/browser/sharing_hub/sharing_hub_model.h"
 #include "chrome/browser/sharing_hub/sharing_hub_service.h"
@@ -66,6 +67,12 @@ const base::Feature kShareMenu{
     "ShareMenu",
     base::FEATURE_DISABLED_BY_DEFAULT,
 };
+
+// static
+bool ShareSubmenuModel::IsEnabled() {
+  return base::FeatureList::IsEnabled(kShareMenu) ||
+         share::AreUpcomingSharingFeaturesEnabled();
+}
 
 ShareSubmenuModel::ShareSubmenuModel(
     Browser* browser,
--- chrome/browser/ui/views/omnibox/omnibox_view_views.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "chrome/browser/share/share_submenu_model.h"
 #include "chrome/browser/ui/send_tab_to_self/send_tab_to_self_sub_menu_model.h"
 #include "components/omnibox/browser/omnibox_view.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -319,6 +320,10 @@ class OmniboxViewViews
   void PerformDrop(const ui::DropTargetEvent& event,
                    ui::mojom::DragOperation& output_drag_op);
 
+  // Helper methods to construct parts of the context menu.
+  void MaybeAddShareSubmenu(ui::SimpleMenuModel* menu_contents);
+  void MaybeAddSendTabToSelfItem(ui::SimpleMenuModel* menu_contents);
+
   // When true, the location bar view is read only and also is has a slightly
   // different presentation (smaller font size). This is used for popups.
   bool popup_window_mode_;
@@ -408,7 +413,9 @@ class OmniboxViewViews
   base::ScopedObservation<TemplateURLService, TemplateURLServiceObserver>
       scoped_template_url_service_observation_{this};
 
-  // Send tab to self submenu.
+  // Send tab to self submenu & share submenu - only one of these is populated
+  // at a time.
+  std::unique_ptr<share::ShareSubmenuModel> share_submenu_model_;
   std::unique_ptr<send_tab_to_self::SendTabToSelfSubMenuModel>
       send_tab_to_self_sub_menu_model_;
 
