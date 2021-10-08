df4107e0e8d78b938ac28d72b51cd07185ddea38
Joey Arhar <jarhar@chromium.org>
2021-10-08 06:41:06 +0000

    Don't always unlock DisplayLocks in CanContainRangeEndPoint
    SelectionModifier::Modify updates the lifecycle to PrePaintClean in
    order to get paint information it needs. Within this function,
    HTMLMeterElement::CanContainRangeEndpoint may be called, which means
    that we can't unlock DisplayLocks because that would dirty style and hit
    DCHECKs with lifecycle updates since we are already at PrePaintClean.
    
    Ideally we would try to unlock DisplayLocks before the lifecycle update
    to PrePaintClean, but in this case where the selection range is being
    extended to include other nodes, it doesn't really make sense - we don't
    know which node(s) we want to extend to and unlock until after the
    update to PrePaintClean, but at that point its too late to unlock
    DisplayLocks for that node...
    
    Fixed: 1251369
    Change-Id: I25baafb85419deb399042f4c30c0089b56f4b241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3184901
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929599}

--- third_party/blink/renderer/core/html/html_meter_element.cc
+++ third_party/blink/renderer/core/html/html_meter_element.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/html/html_meter_element.h"
 
+#include "third_party/blink/renderer/core/display_lock/display_lock_utilities.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
@@ -221,7 +222,13 @@ void HTMLMeterElement::UpdateValueAppearance(double percentage) {
 }
 
 bool HTMLMeterElement::CanContainRangeEndPoint() const {
-  GetDocument().UpdateStyleAndLayoutTreeForNode(this);
+  if (DisplayLockUtilities::LockedAncestorPreventingPaint(*this)) {
+    // If this element is DisplayLocked, then we can't access GetComputedStyle.
+    // Even with GetComputedStyle's scoped unlock, this function may be called
+    // during selection modification which prevents lifecycle updates that the
+    // unlock would incur.
+    return false;
+  }
   return GetComputedStyle() && !GetComputedStyle()->HasEffectiveAppearance();
 }
 
