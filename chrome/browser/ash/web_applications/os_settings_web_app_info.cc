26a941bf9cdb30d23ed8e20d2a8fa8bc9c9b76ba
David Black <dmblack@google.com>
2021-10-08 03:11:20 +0000

    Provide dark mode theme color in Settings web app info.
    This will cause the CrOS settings app to use the correct frame color
    in dark mode, whereas previously it would use white regardless of color
    scheme.
    
    Note that this does not address the issue of jank during theme
    transitions while the app is open. UX for that transition is still
    under discussion: http://shortn/_7eeE0RmHiO
    
    Before (dark):  http://shortn/_wa4diipyZW
    After (dark):   http://shortn/_Je0SJu57u2
    Before (light): http://shortn/_ZfxcE7Re78
    After (light):  http://shortn/_OUJrPX5lZ6
    
    Bug: 1256883
    Change-Id: Id530b0eb59ec2dd73c0203a9bd95b36188429b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206651
    Reviewed-by: Dominic Schulz <dominicschulz@google.com>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#929521}

--- chrome/browser/ash/web_applications/os_settings_web_app_info.cc
+++ chrome/browser/ash/web_applications/os_settings_web_app_info.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
+#include "base/feature_list.h"
 #include "chrome/browser/ash/web_applications/system_web_app_install_utils.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/browser/web_applications/web_application_info.h"
@@ -16,6 +18,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/os_settings_resources.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/chromeos/styles/cros_styles.h"
+
+namespace {
+
+SkColor GetBgColor(bool use_dark_mode) {
+  return cros_styles::ResolveColor(
+      cros_styles::ColorName::kBgColor, use_dark_mode,
+      base::FeatureList::IsEnabled(
+          ash::features::kSemanticColorsDebugOverride));
+}
+
+}  // namespace
 
 std::unique_ptr<WebApplicationInfo>
 CreateWebAppInfoForOSSettingsSystemWebApp() {
@@ -31,8 +45,9 @@ CreateWebAppInfoForOSSettingsSystemWebApp() {
 
       },
       *info);
-  info->theme_color = 0xffffffff;
-  info->background_color = 0xffffffff;
+  info->theme_color = GetBgColor(/*use_dark_mode=*/false);
+  info->dark_mode_theme_color = GetBgColor(/*use_dark_mode=*/true);
+  info->background_color = info->theme_color;
   info->display_mode = blink::mojom::DisplayMode::kStandalone;
   info->user_display_mode = blink::mojom::DisplayMode::kStandalone;
   return info;
