bdeb71c8e162d7580509d0d9e5e84704149b4159
Yoichi Osato <yoichio@chromium.org>
2021-10-08 03:08:54 +0000

    Send a quic version header by OnBeforeSendHeaders
    This CL is a follow-up of crrev.com/c/3206436
    ("Add a header indicating the version of the WebTransport draft used")
    Since the header is not a pseudo header and no another way
    for extension authors to know the header, we should send it.
    
    Bug: 1240935
    Change-Id: I32161553b9c79375b3c43a9952fa48e1d4efcd35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3210527
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929515}

--- chrome/test/data/extensions/api_test/webrequest/test_webtransport.js
+++ chrome/test/data/extensions/api_test/webrequest/test_webtransport.js
@@ -204,7 +204,9 @@ chrome.tabs.getCurrent(function(tab) {
           let isOnBeforeSendHeadersCalled = false;
           const onBeforeSendHeaders = callbackPass((details) => {
             const headers = details.requestHeaders;
-            chrome.test.assertEq([], headers);
+            chrome.test.assertEq(
+                [{name: 'sec-webtransport-http3-draft02', value: '1'}],
+                headers);
             headers.push({name: 'foo', value: 'bar'});
             isOnBeforeSendHeadersCalled = true;
 
@@ -218,7 +220,9 @@ chrome.tabs.getCurrent(function(tab) {
           const onSendHeaders = callbackPass((details) => {
             const headers = details.requestHeaders;
             // Header mutation in onBeforeSendHeaders is ignored.
-            chrome.test.assertEq([], headers);
+            chrome.test.assertEq(
+                [{name: 'sec-webtransport-http3-draft02', value: '1'}],
+                headers);
             isOnSendHeadersCalled = true;
 
             chrome.webRequest.onSendHeaders.removeListener(onSendHeaders);
