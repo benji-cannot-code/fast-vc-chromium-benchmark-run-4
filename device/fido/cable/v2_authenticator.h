f6dbc4718a7bf366ce014ec3081850e0345ca21b
Adam Langley <agl@chromium.org>
2021-10-08 19:58:08 +0000

    webauthn: tweak caBLEv2 meterics.
    We continue to investigate caBLEv2 metrics. This CL includes changes to
    try and shine light on some oddities:
    
       * We see quite a high number of activity stops while a Play Services
         assertion request is outstanding. Therefore measure the amount of
         time these are taking (perhaps it's really slow?) and count the
         number of invocations and responses (perhaps requests are getting
         lost?).
       * The overall success rate measured by Chrome is much lower than
         the end-to-end measurement. So record when a transaction completes
         on a connection in case there's somehow a difference in
         measurements between that and success.
    
    Change-Id: I843e5eea920ea3a1e75c51587b3ed9612c6ec8ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3206243
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#929856}

--- device/fido/cable/v2_authenticator.h
+++ device/fido/cable/v2_authenticator.h
@@ -43,6 +43,7 @@ class Platform {
     HANDSHAKE_COMPLETE = 2,
     REQUEST_RECEIVED = 3,
     CTAP_ERROR = 4,
+    FIRST_TRANSACTION_DONE = 5,
   };
 
   enum class Error {
@@ -63,6 +64,7 @@ class Platform {
     NO_SCREENLOCK = 110,
     NO_BLUETOOTH_PERMISSION = 111,
     QR_URI_ERROR = 112,
+    EOF_WHILE_PROCESSING = 113,
   };
 
   using MakeCredentialCallback =
