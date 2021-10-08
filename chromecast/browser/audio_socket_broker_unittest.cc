1ed22ddc6af0feffb2404ae8b84389766133bb2c
Junbo Ke <juke@chromium.org>
2021-10-08 17:39:52 +0000

    [Chromecast] Fix a potential race condition in AudioSocketBrokerTest.
    The socket FD is default to non-blocking, which makes recvmsg return -1
    immediately if there are no pending incoming messages. Make the accepted
    FD blocking to make sure the message sent with sendmsg is guaranteed to
    reach the receiving end.
    
    Bug: b:200081217
    Test: cast_shell_unittests
    Change-Id: I8fb1358ee846358cd0cf2cd50cf62fddb1bef86b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213514
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929769}

--- chromecast/browser/audio_socket_broker_unittest.cc
+++ chromecast/browser/audio_socket_broker_unittest.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/browser/audio_socket_broker.h"
 
+#include <fcntl.h>
+#include <sys/socket.h>
+
 #include <cstring>
 #include <memory>
 #include <string>
@@ -13,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/location.h"
+#include "base/posix/eintr_wrapper.h"
 #include "base/posix/unix_domain_socket.h"
 #include "base/run_loop.h"
 #include "base/threading/thread.h"
@@ -90,6 +94,10 @@ class AudioSocketBrokerTest : public content::RenderViewHostTestHarness {
     EXPECT_EQ(result, net::OK);
     char buffer[16];
     std::vector<base::ScopedFD> fds;
+    const int flags = fcntl(accepted_descriptor_, F_GETFL);
+    ASSERT_NE(
+        HANDLE_EINTR(fcntl(accepted_descriptor_, F_SETFL, flags & ~O_NONBLOCK)),
+        -1);
     EXPECT_EQ(static_cast<size_t>(base::UnixDomainSocket::RecvMsg(
                   accepted_descriptor_, buffer, sizeof(buffer), &fds)),
               sizeof(kSocketMsg));
