82fdae0980480f8337ed133c6a548fbf87243d27
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-10-08 18:22:53 +0000

    Import wpt@0bfbf52dcbd2433cd42a7ca43ab57c5b3e6f93a0
    Using wpt-import in Chromium 259214a17024cb5a98686a89f14fd14b43c42796.
    With Chromium commits locally applied on WPT:
    8acc6e6155 "[anonymous-iframe] WPT fix newAnonymousIframe."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    garykac@chromium.org:
      external/wpt/keyboard-map
    garykac@chromium.org, joedow@chromium.org:
      external/wpt/keyboard-lock
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Change-Id: I84a1414eff0100075d79e6b0d2f099c0e947a088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3214411
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#929802}

--- third_party/blink/web_tests/external/wpt/keyboard-lock/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/keyboard-lock/idlharness.https.window.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['keyboard-lock'],
-  ['html'],
+  ['html', 'dom'],
   idl_array => {
     idl_array.add_objects({
       Navigator: ['navigator'],
