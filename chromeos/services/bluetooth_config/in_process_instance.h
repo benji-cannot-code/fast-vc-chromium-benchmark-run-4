09bef7aaed0152834d72db110c5791ff92ffe812
Gordon Seto <gordonseto@google.com>
2021-10-08 18:24:29 +0000

    [CrOS Bluetooth] Update CrosBluetoothConfig initialization.
    Update CrosBluetoothConfig to initialize early in
    ChromeBrowserMainPartsAsh. Previously, it was lazy-initialized by the
    first client to bind to it. Because CrosBluetoothConfg will have
    services that use prefs, including local device prefs, this needs to be
    updated to be initialized at earlier because it needs access to prefs
    before any client binds to it. Classes that will use the prefs will be
    added in follow-up CLs.
    
    Bug: 1010321
    Change-Id: I26d9900647fa32c5222f25f0d1307b07032a4c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3202980
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#929803}

--- chromeos/services/bluetooth_config/in_process_instance.h
+++ chromeos/services/bluetooth_config/in_process_instance.h
@@ -10,10 +10,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/bluetooth_config/public/mojom/cros_bluetooth_config.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 
+class PrefService;
+
 namespace chromeos {
 namespace bluetooth_config {
 
 class Initializer;
+
+COMPONENT_EXPORT(IN_PROCESS_BLUETOOTH_CONFIG)
+void Initialize();
+
+COMPONENT_EXPORT(IN_PROCESS_BLUETOOTH_CONFIG)
+void Shutdown();
+
+// Updates the PrefServices used by CrosBluetoothConfig.
+COMPONENT_EXPORT(IN_PROCESS_BLUETOOTH_CONFIG)
+void SetPrefs(PrefService* logged_in_profile_prefs, PrefService* device_prefs);
 
 // Binds to an instance of CrosBluetoothConfig from within the browser process.
 COMPONENT_EXPORT(IN_PROCESS_BLUETOOTH_CONFIG)
