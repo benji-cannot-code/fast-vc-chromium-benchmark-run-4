0df3d9085c487f3dbb3c57053bea27d8c80b4c97
Joe Downing <joedow@google.com>
2021-10-08 19:08:37 +0000

    Update remoting::HostConfig to use non-deprecated JSON methods
    Bug: NO_BUG
    Change-Id: I9eb09f684d8051c7a38af801a1ddfbf81061c40d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213389
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929825}

--- remoting/host/daemon_process_win.cc
+++ remoting/host/daemon_process_win.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/win/security_descriptor.h"
 #include "remoting/host/win/unprivileged_process_delegate.h"
 #include "remoting/host/win/worker_process_launcher.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 using base::win::ScopedHandle;
 
@@ -288,16 +289,14 @@ void DaemonProcessWin::SendHostConfigToNetworkProcess(
     LOG_IF(ERROR, !remoting_host_control_.is_connected())
         << "IPC channel not connected. HostConfig message will be dropped.";
 
-    std::unique_ptr<base::DictionaryValue> config(
-        HostConfigFromJson(serialized_config));
-    if (!config) {
+    absl::optional<base::Value> config(HostConfigFromJson(serialized_config));
+    if (!config.has_value()) {
       LOG(ERROR) << "Invalid host config, shutting down.";
       OnPermanentError(kInvalidHostConfigurationExitCode);
       return;
     }
 
-    remoting_host_control_->ApplyHostConfig(
-        base::Value::FromUniquePtrValue(std::move(config)));
+    remoting_host_control_->ApplyHostConfig(std::move(config.value()));
   }
 }
 
