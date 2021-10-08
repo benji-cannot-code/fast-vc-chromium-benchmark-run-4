9ee2b5d086318aeaf55c152e181b2762221dd26d
Tobias Soppa <tobias.soppa@code.berlin>
2021-10-08 19:18:21 +0000

    Fix ClosedTabCache issues with beforeunload events
    When a single WebContents is closed and the beforeunload event
    fires, an empty vector will be passed to
    BrowserTabStripModelDelegate::CacheWebContents().
    
    This causes the browser to crash because there are no checks
    if the passed vector is empty or not.
    
    Bug: 1240170, 1253261
    Change-Id: Ifeda002fc624ee75dabc9eb5cfeb041a3da328a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3097836
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Commit-Queue: Tobias Soppa <tobias.soppa@code.berlin>
    Cr-Commit-Position: refs/heads/main@{#929832}

--- chrome/browser/ui/browser_tab_strip_model_delegate.cc
+++ chrome/browser/ui/browser_tab_strip_model_delegate.cc
@@ -242,6 +242,8 @@ void BrowserTabStripModelDelegate::CacheWebContents(
     return;
   }
 
+  DCHECK(!web_contents.empty());
+
   ClosedTabCache& cache =
       ClosedTabCacheServiceFactory::GetForProfile(browser_->profile())
           ->closed_tab_cache();
