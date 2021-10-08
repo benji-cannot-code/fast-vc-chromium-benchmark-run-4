4eca601e33af722f439a6594dbfbb45aef7803b8
Michael Checo <michaelcheco@google.com>
2021-10-08 20:48:52 +0000

    Diagnostics: Update settings icon
    - This change also fixes the formatting of the input icons.
    
    Screenshot: http://shortn/_GCNhUpVyNe
    
    Bug: 1197335
    Change-Id: I6829596b3d8306deffa32f6fc612b40ec2c19a0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3214632
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#929875}

--- ash/webui/diagnostics_ui/resources/network_list.js
+++ ash/webui/diagnostics_ui/resources/network_list.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './connectivity_card.js';
 import './diagnostics_fonts_css.js';
 import './diagnostics_shared_css.js';
+import './icons.js';
 import './network_card.js';
 
 import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
