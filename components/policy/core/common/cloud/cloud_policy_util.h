c610b9f1c210ea5157b9ee799126351468e43f51
Sebastien Lalancette <seblalancette@chromium.org>
2021-10-08 13:11:44 +0000

    Remove Unused Cloud Policy Utility Functions
    Removed GetDeviceManufacturer and GetDeviceModel.
    
    Change-Id: I3373f93a058d27cd502a621a2afd1b5eea1d3855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208111
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929677}

--- components/policy/core/common/cloud/cloud_policy_util.h
+++ components/policy/core/common/cloud/cloud_policy_util.h
@@ -15,13 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace policy {
 
-// Returns the model of the device. This function is platform specific.
-POLICY_EXPORT std::string GetDeviceModel();
-
-// Returns the brand/manufacturer of the device. This function is platform
-// specific.
-POLICY_EXPORT std::string GetDeviceManufacturer();
-
 // Returns the name of the machine. This function is platform specific.
 POLICY_EXPORT std::string GetMachineName();
 
