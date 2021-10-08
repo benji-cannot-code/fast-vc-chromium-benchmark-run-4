5183f226c3ecaaef87c602edde01216a3f1e9927
Vidhan Jain <vidhanj@google.com>
2021-10-08 12:21:37 +0000

    [iOS][Passwords] Update PasswordDetailsTableViewController on duplicates
    With this CL, when manually creating a new credential from settings,
    if a duplicate credential exists with the same username and website
    combination, an alert section is added to the tableViewModel informing
    the user about the existing credential.
    
    Subsequent CL would implement the functionality to view the existing
    credential if the user decides to tap on "View Password" in the alert
    section.
    
    Mocks: https://docs.google.com/presentation/d/1X79wp1xX9QTwCiqnHingswAfaiwmdysNTI7VZ313IJA/edit?resourcekey=0--WwLl01uAB8RlH1aIixU6A#slide=id.gf0812f595f_0_0
    
    Bug: 1226006
    Change-Id: I674c0649ff415f4a928cf636c958b298fd19d5b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194356
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#929659}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_table_view_controller_unittest.mm
@@ -227,6 +227,12 @@ class PasswordDetailsTableViewControllerTest
     EXPECT_NSEQ(expected_text, cell.textFieldValue);
   }
 
+  void SetEditCellText(NSString* text, int section, int item) {
+    TableViewTextEditItem* cell =
+        static_cast<TableViewTextEditItem*>(GetTableViewItem(section, item));
+    cell.textFieldValue = text;
+  }
+
   void CheckDetailItemTextWithId(int expected_detail_text_id,
                                  int section,
                                  int item) {
@@ -405,9 +411,7 @@ TEST_F(PasswordDetailsTableViewControllerTest, TestEditPasswordConfirmed) {
   EXPECT_FALSE(delegate().password);
   EXPECT_TRUE(passwordDetails.tableView.editing);
 
-  TableViewTextEditItem* cell =
-      static_cast<TableViewTextEditItem*>(GetTableViewItem(0, 2));
-  cell.textFieldValue = @"new_password";
+  SetEditCellText(@"new_password", 0, 2);
 
   [passwordDetails editButtonPressed];
   EXPECT_TRUE(handler().editingCalled);
@@ -430,9 +434,7 @@ TEST_F(PasswordDetailsTableViewControllerTest, TestEditPasswordCancel) {
   EXPECT_FALSE(delegate().password);
   EXPECT_TRUE(passwordDetails.tableView.editing);
 
-  TableViewTextEditItem* cell =
-      static_cast<TableViewTextEditItem*>(GetTableViewItem(0, 2));
-  cell.textFieldValue = @"new_password";
+  SetEditCellText(@"new_password", 0, 2);
 
   [passwordDetails editButtonPressed];
   EXPECT_FALSE(delegate().password);
@@ -601,10 +603,34 @@ TEST_F(PasswordDetailsTableViewControllerTest, TestSectionsInAdd) {
       static_cast<PasswordDetailsTableViewController*>(controller());
   [passwords_controller loadModel];
 
-  EXPECT_EQ(2, NumberOfSections());
+  EXPECT_EQ(3, NumberOfSections());
   EXPECT_EQ(1, NumberOfItemsInSection(0));
   EXPECT_EQ(2, NumberOfItemsInSection(1));
   // TODO(crbug.com/1226006): Use i18n string.
   CheckSectionFooter(
-      @"Make sure you're saving your current password for this site", 1);
+      @"Make sure you're saving your current password for this site", 2);
+}
+
+// Tests the layout of the view controller when adding a new credential with
+// duplicate website/username combination.
+TEST_F(PasswordDetailsTableViewControllerTest, TestSectionsInAddDuplicated) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(
+      password_manager::features::kSupportForAddPasswordsInSettings);
+  SetCredentialType(CredentialTypeNew);
+  SetPassword();
+
+  PasswordDetailsTableViewController* passwords_controller =
+      static_cast<PasswordDetailsTableViewController*>(controller());
+  [passwords_controller loadModel];
+
+  SetEditCellText(@"http://www.example.com/", 0, 0);
+  SetEditCellText(@"test@egmail.com", 1, 0);
+
+  [passwords_controller onDuplicateCheckCompletion:YES];
+
+  EXPECT_EQ(4, NumberOfSections());
+  EXPECT_EQ(1, NumberOfItemsInSection(0));
+  EXPECT_EQ(2, NumberOfItemsInSection(1));
+  EXPECT_EQ(2, NumberOfItemsInSection(2));
 }
