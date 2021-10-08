a6eaceb80624345e375205088a70eb6ede255572
Preethi Mohan <preethim@google.com>
2021-10-08 19:57:26 +0000

    Temporary additional logging to debug gtest failures on win7 dbg.
    Bug: 1235878
    Change-Id: Id3381334ee9f7e9388e1b86919a2d8ef7f2d3eaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213441
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/main@{#929855}

--- chrome/test/base/browser_tests_main.cc
+++ chrome/test/base/browser_tests_main.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "base/logging.h"
 #include "base/test/launcher/test_launcher.h"
 #include "base/test/test_switches.h"
 #include "build/build_config.h"
@@ -17,10 +18,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_WIN)
 
 int main(int argc, char** argv) {
+  // TODO(preethim): Debugging for crbug.com/1235878.
+  // Will revert within a day or two after reproing in CI.
+  LOG(INFO) << "Starting browser_tests.\n";
   base::CommandLine::Init(argc, argv);
   size_t parallel_jobs = base::NumParallelJobs(/*cores_per_job=*/2);
-  if (parallel_jobs == 0U)
+  if (parallel_jobs == 0U) {
+    // TODO(preethim): Debugging for crbug.com/1235878.
+    // Will revert within a day or two after reproing in CI.
+    LOG(INFO) << "parallel_jobs is 0. Exiting.\n";
     return 1;
+  }
 
 #if defined(OS_WIN)
   // Many tests validate code that requires user32.dll to be loaded. Loading it,
