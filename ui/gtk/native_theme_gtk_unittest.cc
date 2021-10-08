2360b464dc57ede66288b156ef24149a99b16d48
Maksim Sisov <msisov@igalia.com>
2021-10-08 14:39:39 +0000

    ozone: fix gtk_unittests
    These tests fail as OzonePlatform is not initialized
    if only ozone platform wayland is enabled. Otherwise,
    they fallback to X11 as delegate is not set, but
    USE_X11 is defined.
    
    Cq-Include-Trybots: luci.chromium.try:linux-wayland-rel
    Bug: 1257977
    Change-Id: I4a3ec37e2bb458d1f3cf4039c0255863354a6456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211734
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#929704}

--- ui/gtk/native_theme_gtk_unittest.cc
+++ ui/gtk/native_theme_gtk_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gtk/gtk_ui_factory.h"
 #include "ui/native_theme/native_theme_color_id.h"
 #include "ui/native_theme/test/color_utils.h"
+#include "ui/ozone/public/ozone_platform.h"
 #include "ui/views/linux_ui/linux_ui.h"
 
 namespace gtk {
@@ -26,7 +27,12 @@ class NativeThemeGtkRedirectedEquivalenceTest
     : public testing::TestWithParam<
           std::tuple<ui::NativeTheme::ColorScheme, ui::NativeTheme::ColorId>> {
  public:
-  NativeThemeGtkRedirectedEquivalenceTest() { gtk_ui_ = BuildGtkUi(); }
+  NativeThemeGtkRedirectedEquivalenceTest() {
+    ui::OzonePlatform::InitParams params;
+    params.single_process = true;
+    ui::OzonePlatform::InitializeForUI(params);
+    gtk_ui_ = BuildGtkUi();
+  }
 
   static std::string ParamInfoToString(
       ::testing::TestParamInfo<
