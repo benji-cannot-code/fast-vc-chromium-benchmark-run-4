61788c06b2e64cbb9582597f4dda925af46fa461
Scott Violet <sky@chromium.org>
2021-10-08 15:23:57 +0000

    viz: don't update Display's SurfaceId if a frame is submitted for evicted id
    This ensures the following sequence:
    
    . EvictSurface(id)
    . SubmitCompositorFrame(id)
    
    Doesn't result in Display referencing a SurfaceId that has been
    evicted.
    
    BUG=1257319
    TEST=FrameSinkManagerTest.SubmitCompositorFrameWithEvictedSurfaceId
    
    Change-Id: Id7859c62d2060edf8740c69065ed12869f0cf646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3212502
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#929710}

--- components/viz/service/frame_sinks/compositor_frame_sink_support.cc
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.cc
@@ -1108,14 +1108,6 @@ bool CompositorFrameSinkSupport::ShouldSendBeginFrame(
   return true;
 }
 
-bool CompositorFrameSinkSupport::IsEvicted(
-    const LocalSurfaceId& local_surface_id) const {
-  return local_surface_id.embed_token() ==
-             last_evicted_local_surface_id_.embed_token() &&
-         local_surface_id.parent_sequence_number() <=
-             last_evicted_local_surface_id_.parent_sequence_number();
-}
-
 void CompositorFrameSinkSupport::CheckPendingSurfaces() {
   if (pending_surfaces_.empty())
     return;
@@ -1135,6 +1127,14 @@ void CompositorFrameSinkSupport::OnCompositorFrameTransitionDirectiveProcessed(
     uint32_t sequence_id) {
   if (client_)
     client_->OnCompositorFrameTransitionDirectiveProcessed(sequence_id);
+}
+
+bool CompositorFrameSinkSupport::IsEvicted(
+    const LocalSurfaceId& local_surface_id) const {
+  return local_surface_id.embed_token() ==
+             last_evicted_local_surface_id_.embed_token() &&
+         local_surface_id.parent_sequence_number() <=
+             last_evicted_local_surface_id_.parent_sequence_number();
 }
 
 void CompositorFrameSinkSupport::DestroySelf() {
