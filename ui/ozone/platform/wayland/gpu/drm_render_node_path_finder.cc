4d03b55fac08c98a754b11dffe182b4ddb5aca87
Stephan Hartmann <stha09@googlemail.com>
2021-10-11 09:14:06 +0000

    IWYU: add string.h for memcmp in ui::DrmRenderNodePathFinder
    Bug: None
    Change-Id: I9b97dfa1f14f8c7cf586d0de953eb93d6243beb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3212038
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#930076}

--- ui/ozone/platform/wayland/gpu/drm_render_node_path_finder.cc
+++ ui/ozone/platform/wayland/gpu/drm_render_node_path_finder.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/gpu/drm_render_node_path_finder.h"
 
 #include <fcntl.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
