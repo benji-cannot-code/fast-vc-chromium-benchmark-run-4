477fe3944a0a37fdf6d462dca56b0b73f6bd1769
Gang Wu <gangwu@chromium.org>
2021-10-11 17:42:56 +0000

    Revert "[Autofill Assistant] Add AutofillAssistantAgent mojoms"
    This reverts commit 6f96eb2c46ba7dab5b1a0ddf18eb128b7af4e095.
    
    Reason for revert: https://ci.chromium.org/p/chromium/builders/ci/android-pie-arm64-rel/11916?
    
    
    Original change's description:
    > [Autofill Assistant] Add AutofillAssistantAgent mojoms
    >
    > This adds a new mojom for the Autofill Assistant. This is a pre-step,
    > the current use-case of suppressing the keyboard is a bit more
    > involved than expected (see crrev/c/3160636).
    >
    > This mojom will eventually be used in a project to interact with the
    > webpage on Autofill Assistant's behalf.
    >
    > Bug: b/199856285
    > Change-Id: If1e45f35c59b4032b65d4dbd2677e077127645b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197817
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Clemens Arbesser <arbesser@google.com>
    > Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#930181}
    
    Bug: b/199856285
    Change-Id: Ibe8f312a9a9ff1ae54ee0fba54846e09a09a99c1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217891
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Owners-Override: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930192}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/renderer/autofill_assistant_agent.h"
 #include "components/autofill/content/renderer/password_autofill_agent.h"
 #include "components/autofill/content/renderer/password_generation_agent.h"
-#include "components/autofill_assistant/content/renderer/autofill_assistant_agent.h"
 #include "components/content_capture/common/content_capture_features.h"
 #include "components/content_capture/renderer/content_capture_sender.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
@@ -255,6 +254,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using autofill::AutofillAgent;
+using autofill::AutofillAssistantAgent;
 using autofill::PasswordAutofillAgent;
 using autofill::PasswordGenerationAgent;
 using base::ASCIIToUTF16;
@@ -639,16 +639,11 @@ void ChromeContentRendererClient::RenderFrameCreated(
   PasswordGenerationAgent* password_generation_agent =
       new PasswordGenerationAgent(render_frame, password_autofill_agent,
                                   associated_interfaces);
-  autofill::AutofillAssistantAgent* autofill_assistant_agent =
-      new autofill::AutofillAssistantAgent(render_frame);
+  AutofillAssistantAgent* autofill_assistant_agent =
+      new AutofillAssistantAgent(render_frame);
   new AutofillAgent(render_frame, password_autofill_agent,
                     password_generation_agent, autofill_assistant_agent,
                     associated_interfaces);
-
-#if defined(OS_ANDROID)
-  new autofill_assistant::AutofillAssistantAgent(render_frame,
-                                                 associated_interfaces);
-#endif
 
   if (content_capture::features::IsContentCaptureEnabled()) {
     new content_capture::ContentCaptureSender(render_frame,
