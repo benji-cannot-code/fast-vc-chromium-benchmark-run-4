5988ec78b7b7800dc3a4f35def481084e89acdee
Robbie Gibson <rkgibson@google.com>
2021-10-11 17:55:51 +0000

    [iOS] Add overflow menu destination views
    This CL adds two simple views to display the destination items (one
    for the item and one for the list of item).
    
    It also adds some icons for the items.
    
    Bug: 1254379
    Change-Id: I42b1015c71701eac7d7c1e767bea0da145051486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208036
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930203}

---
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_destination_list.swift
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import SwiftUI
+
+/// A view displaying a list of destinations.
+struct OverflowMenuDestinationList: View {
+  /// The destinations for this view.
+  var destinations: [OverflowMenuDestination]
+
+  var body: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      LazyHStack(spacing: 0) {
+        ForEach(destinations) { destination in
+          OverflowMenuDestinationView(destination: destination, iconSpacing: 13)
+        }
+      }
+    }
+  }
+}
--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_view.swift
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_view.swift
@@ -6,9 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import SwiftUI
 
 struct OverflowMenuView: View {
+  enum Dimensions {
+    static let destinationListHeight: CGFloat = 129
+  }
+
   @EnvironmentObject var model: OverflowMenuModel
   var body: some View {
     Group {
+      OverflowMenuDestinationList(destinations: model.destinations)
+        .frame(height: Dimensions.destinationListHeight)
+      Divider()
       OverflowMenuActionList(actions: model.actions)
     }.background(Color(.systemGroupedBackground).edgesIgnoringSafeArea(.top))
   }
