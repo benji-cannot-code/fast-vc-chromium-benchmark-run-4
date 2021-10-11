ac190dd63ebb50a5f71d743238b70ba93c6cfaae
Sean Harrison <harrisonsean@chromium.org>
2021-10-11 11:39:08 +0000

    [Privacy Review] Add HaTS feature
    Add feature for Privacy Review HaTS
    
    
    Bug: 1215630
    Change-Id: I19a58a9de198cc6595572c0a0a91ad0529e667b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213469
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#930115}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -299,6 +299,9 @@ extern const base::FeatureParam<bool>
     kHappinessTrackingSurveysForDesktopSettingsPrivacyNoSandbox;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kHappinessTrackingSurveysForDesktopPrivacyReview;
+
+COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kHappinessTrackingSurveysForDesktopNtpModules;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
