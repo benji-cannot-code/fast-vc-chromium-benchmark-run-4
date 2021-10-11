cdeb08a586f63595855951f2f633c29496887542
Gauthier Ambard <gambard@chromium.org>
2021-10-11 12:40:48 +0000

    [iOS] Remove unused flag
    The experiment has been launched and the flag wasn't cleaned up.
    
    Bug: None
    Change-Id: I9e047db3a470c8c0f749aa7800182606dc8c49b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3216692
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930122}

--- ios/chrome/browser/ui/first_run/legacy_first_run_egtest.mm
+++ ios/chrome/browser/ui/first_run/legacy_first_run_egtest.mm
@@ -67,7 +67,6 @@ id<GREYMatcher> SkipSigninButton() {
 
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
-  config.features_disabled.push_back(kLocationPermissionsPrompt);
   config.features_disabled.push_back(kEnableFREUIModuleIOS);
 
   // Show the First Run UI at startup.
