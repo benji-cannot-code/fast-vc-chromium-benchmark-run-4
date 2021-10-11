03979ffa66ef2f34f7b6332fa85db77bcc7e26f1
dpapad <dpapad@chromium.org>
2021-10-11 08:40:24 +0000

    Settings: Move RouteObserverBehavior to OS Settings, only used there.
    Browser Settings no longer uses RouteObserverBehavior, as it has been
    migrated to RouteObserverMixin. Moving it to OS Settings is a
    preparatory step for migrating the shared router.js file to TypeScript.
    
    Bug: 1234307
    Change-Id: I2a8c0e2aa5c23891e1ab3d68e625a0c4c6160de1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213516
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930070}

--- chrome/browser/resources/settings/chromeos/route_origin_behavior.js
+++ chrome/browser/resources/settings/chromeos/route_origin_behavior.js
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// #import {RouteObserverBehavior, Route, Router} from '../router.js';
+// #import {Route, Router} from '../router.js';
+// #import {RouteObserverBehavior} from './route_observer_behavior.js';
 // #import {assert} from 'chrome://resources/js/assert.m.js';
 
 cr.define('settings', function() {
