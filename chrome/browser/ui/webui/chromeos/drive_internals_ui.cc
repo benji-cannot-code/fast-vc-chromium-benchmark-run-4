f269903e7742724415ad1b4bba89599df8c787b0
Lei Zhang <thestig@chromium.org>
2021-10-11 23:59:20 +0000

    Fix some nits in CrOS download code.
    - Use range-based for-loops.
    - Mark char arrays as truly const.
    
    Change-Id: I460584d1f2b7fcc1e05f0ae2d10f2a3aaf00ac2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218355
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930352}

--- chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
+++ chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
@@ -450,13 +450,12 @@ class DriveInternalsWebUIHandler : public content::WebUIMessageHandler {
                      integration_service->GetMountPointPath().AsUTF8Unsafe());
     }
 
-    const char* kPathPreferences[] = {
+    const char* const kPathPreferences[] = {
         prefs::kSelectFileLastDirectory,
         prefs::kSaveFileDefaultDirectory,
         prefs::kDownloadDefaultDirectory,
     };
-    for (size_t i = 0; i < base::size(kPathPreferences); ++i) {
-      const char* const key = kPathPreferences[i];
+    for (const char* key : kPathPreferences) {
       AppendKeyValue(&paths, key,
                      profile()->GetPrefs()->GetFilePath(key).AsUTF8Unsafe());
     }
@@ -530,7 +529,7 @@ class DriveInternalsWebUIHandler : public content::WebUIMessageHandler {
   void UpdateDriveRelatedPreferencesSection() {
     SetSectionEnabled("drive-related-preferences-section", true);
 
-    const char* kDriveRelatedPreferences[] = {
+    const char* const kDriveRelatedPreferences[] = {
         drive::prefs::kDisableDrive,
         drive::prefs::kDisableDriveOverCellular,
         drive::prefs::kDriveFsWasLaunchedAtLeastOnce,
@@ -541,11 +540,10 @@ class DriveInternalsWebUIHandler : public content::WebUIMessageHandler {
     PrefService* pref_service = profile()->GetPrefs();
 
     base::ListValue preferences;
-    for (size_t i = 0; i < base::size(kDriveRelatedPreferences); ++i) {
-      const std::string key = kDriveRelatedPreferences[i];
+    for (const char* key : kDriveRelatedPreferences) {
       // As of now, all preferences are boolean.
       const std::string value =
-          (pref_service->GetBoolean(key.c_str()) ? "true" : "false");
+          (pref_service->GetBoolean(key) ? "true" : "false");
       AppendKeyValue(&preferences, key, value);
     }
 
