b69d4258195f76c741d051d33f7da387f42e0c8e
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2021-10-11 17:09:54 +0000

    sensors: Use base::ranges::any_of() in PlatformSensorFusion's constructor.
    Simplify the code a little bit by using the ranges version of any_of().
    While here, stop unnecessarily making Factory a friend class and fix the
    grammar in the class' description.
    
    Change-Id: If48acaf2b6ca6cb5d66ab74b60eb42415a6a627e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217450
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930189}

--- services/device/generic_sensor/platform_sensor_fusion.cc
+++ services/device/generic_sensor/platform_sensor_fusion.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
+#include "base/ranges/algorithm.h"
 #include "services/device/generic_sensor/platform_sensor_fusion_algorithm.h"
 #include "services/device/generic_sensor/platform_sensor_provider.h"
 #include "services/device/generic_sensor/platform_sensor_util.h"
@@ -117,11 +118,10 @@ PlatformSensorFusion::PlatformSensorFusion(
 
   fusion_algorithm_->set_fusion_sensor(this);
 
-  if (std::any_of(source_sensors_.begin(), source_sensors_.end(),
-                  [](const SourcesMapEntry& pair) {
-                    return pair.second->GetReportingMode() ==
-                           mojom::ReportingMode::ON_CHANGE;
-                  })) {
+  if (base::ranges::any_of(source_sensors_, [](const auto& pair) {
+        return pair.second->GetReportingMode() ==
+               mojom::ReportingMode::ON_CHANGE;
+      })) {
     reporting_mode_ = mojom::ReportingMode::ON_CHANGE;
   }
 }
