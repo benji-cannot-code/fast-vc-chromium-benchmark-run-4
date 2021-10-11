15f7ebb20f6faf169eb19b7bdcb58c453eaec466
Avi Drissman <avi@chromium.org>
2021-10-11 23:05:56 +0000

    Add formatOriginForSecurityDisplay to Java
    Bug: 1255713
    Change-Id: I1ec7c6cb143342c12c31c6bb0d6872a12495fe4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3216454
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930320}

--- components/url_formatter/url_formatter_android.cc
+++ components/url_formatter/url_formatter_android.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_formatter/url_formatter.h"
 #include "url/android/gurl_android.h"
 #include "url/gurl.h"
+#include "url/origin.h"
 
 using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
@@ -104,6 +105,18 @@ static ScopedJavaLocalRef<jstring> JNI_UrlFormatter_FormatUrlForSecurityDisplay(
   return base::android::ConvertUTF16ToJavaString(
       env, url_formatter::FormatUrlForSecurityDisplay(
                *gurl, static_cast<SchemeDisplay>(scheme_display)));
+}
+
+static ScopedJavaLocalRef<jstring>
+JNI_UrlFormatter_FormatOriginForSecurityDisplay(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& j_origin,
+    jint scheme_display) {
+  DCHECK(j_origin);
+  url::Origin origin = url::Origin::FromJavaObject(j_origin);
+  return base::android::ConvertUTF16ToJavaString(
+      env, url_formatter::FormatOriginForSecurityDisplay(
+               origin, static_cast<SchemeDisplay>(scheme_display)));
 }
 
 static ScopedJavaLocalRef<jstring>
