a7e9b8c7feccfe21f314f313b73c2cb55dc24a7e
Fergal Daly <fergal@chromium.org>
2021-10-11 02:30:47 +0000

    Switch location of counting persisted page-show in the browser.
    In PageLifecycleStateManager we can't tell whether we're dealing with
    a main frame's RenderViewHost which leads to over-counting. Instead,
    switch to counting in
    BackForwardCacheImpl::WillCommitNavigationToCachedEntry which is
    further away from where the IPC is made but should also be 1-1 with
    renderer and will give us useful signal.
    
    I tried to fix the existing counter but it was not straight-forward, I
    will follow up to fix it. I've deleted it because the new test will
    not pass if it's left in place.
    
    Bug: 1234634
    Change-Id: Icfd6b675c63c3de5666b4ca4255dffd51333abd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205142
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930024}

--- content/browser/renderer_host/back_forward_cache_impl.cc
+++ content/browser/renderer_host/back_forward_cache_impl.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_headers.h"
 #include "net/http/http_status_code.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/public/common/frame/event_page_show_persisted.h"
 #include "third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h"
 #include "third_party/blink/public/mojom/frame/sudden_termination_disabler_type.mojom-shared.h"
 #if defined(OS_ANDROID)
@@ -998,6 +999,10 @@ std::unique_ptr<BackForwardCacheImpl::Entry> BackForwardCacheImpl::RestoreEntry(
     int navigation_entry_id,
     blink::mojom::PageRestoreParamsPtr page_restore_params) {
   TRACE_EVENT0("navigation", "BackForwardCache::RestoreEntry");
+  blink::RecordUMAEventPageShowPersisted(
+      blink::EventPageShowPersisted::
+          kYesInBrowser_BackForwardCache_RestoreEntry_Attempt);
+
   // Select the RenderFrameHostImpl matching the navigation entry.
   auto matching_entry =
       std::find_if(entries_.begin(), entries_.end(),
@@ -1030,6 +1035,9 @@ std::unique_ptr<BackForwardCacheImpl::Entry> BackForwardCacheImpl::RestoreEntry(
 
   RestoreBrowserControlsState(entry->render_frame_host());
 
+  blink::RecordUMAEventPageShowPersisted(
+      blink::EventPageShowPersisted::
+          kYesInBrowser_BackForwardCache_RestoreEntry_Succeed);
   return entry;
 }
 
@@ -1263,6 +1271,9 @@ void BackForwardCacheImpl::WillCommitNavigationToCachedEntry(
           },
           base::TimeTicks::Now(), std::move(done_callback)));
 
+  blink::RecordUMAEventPageShowPersisted(
+      blink::EventPageShowPersisted::
+          kYesInBrowser_BackForwardCache_WillCommitNavigationToCachedEntry);
   for (auto* rvh : bfcache_entry.render_view_hosts()) {
     rvh->PrepareToLeaveBackForwardCache(cb);
   }
