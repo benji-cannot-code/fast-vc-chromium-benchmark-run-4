391ce46b73f378362ce32abf8f28a77dc49249fa
Steven Bingler <bingler@chromium.org>
2021-10-12 19:49:45 +0000

    Check service worker's key before adding matching registration
    Add in a check for the container_host's key before matching
    registrations that have a matching scope. This is necessary because
    when storage partitioning is enabled it is possible to have two service
    worker instances that match the same scope but are on different
    partitions.
    
    This CL also makes a similar change to dedicated workers' checks.
    
    Bug: 1246549
    Change-Id: Ida0a438bfb16d198f71599d51ae6b7eaa660ef72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3216348
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930737}

--- content/browser/service_worker/service_worker_container_host.cc
+++ content/browser/service_worker/service_worker_container_host.cc
@@ -481,6 +481,7 @@ void ServiceWorkerContainerHost::AddMatchingRegistration(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(blink::ServiceWorkerScopeMatches(registration->scope(),
                                           GetUrlForScopeMatch()));
+  DCHECK(registration->key() == key());
   if (!IsEligibleForServiceWorkerController())
     return;
   size_t key = registration->scope().spec().size();
@@ -1200,7 +1201,7 @@ void ServiceWorkerContainerHost::SyncMatchingRegistrations() {
   const auto& registrations = context_->GetLiveRegistrations();
   for (const auto& key_registration : registrations) {
     ServiceWorkerRegistration* registration = key_registration.second;
-    if (!registration->is_uninstalled() &&
+    if (!registration->is_uninstalled() && registration->key() == key() &&
         blink::ServiceWorkerScopeMatches(registration->scope(),
                                          GetUrlForScopeMatch())) {
       AddMatchingRegistration(registration);
