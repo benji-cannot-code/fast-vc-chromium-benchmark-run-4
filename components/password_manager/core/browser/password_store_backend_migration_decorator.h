705c17bc9aeb2f4b50931d2030441bdc8b27fd7a
Viktor Semeniuk <vsemeniuk@google.com>
2021-10-12 16:13:00 +0000

    [Passwords] Introduce BuiltInBackendToAndroidBackendMigrator
    This CL is introducing the skeleton of the migrator without any actual
    code.
    Follow-up CLs will add more content gradually.
    
    Bug: 1252443
    Change-Id: I44b71cffef7783da59a9176d5c16b0d6775b9078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211948
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930577}

--- components/password_manager/core/browser/password_store_backend_migration_decorator.h
+++ components/password_manager/core/browser/password_store_backend_migration_decorator.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager {
 
+class BuiltInBackendToAndroidBackendMigrator;
+
 // This is the backend that should be used on Android platform until the full
 // migration to the Android backend is launched. Internally, this backend
 // owns two backends: the built-in and the Android backend. In addition
@@ -70,11 +72,16 @@ class PasswordStoreBackendMigrationDecorator : public PasswordStoreBackend {
   std::unique_ptr<syncer::ProxyModelTypeControllerDelegate>
   CreateSyncControllerDelegateFactory() override;
 
+  // Creates 'migrator_' and starts migration process.
+  void StartMigration();
+
   std::unique_ptr<PasswordStoreBackend> built_in_backend_;
   std::unique_ptr<PasswordStoreBackend> android_backend_;
 
   // Proxy backend to which all responsibilities are being delegated.
   std::unique_ptr<PasswordStoreBackend> active_backend_;
+
+  std::unique_ptr<BuiltInBackendToAndroidBackendMigrator> migrator_;
 };
 
 }  // namespace password_manager
