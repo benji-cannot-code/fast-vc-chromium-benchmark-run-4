539d2ff2c1148eead39271abeb6483b3bc0a1408
Patrick Monette <pmonette@chromium.org>
2021-10-12 17:27:23 +0000

    Move task-related files from base/ to base/task/
    The forward headers are updated to point to the new location, and the
    header guards were fixed.
    
    Bug: 1255932
    Change-Id: Id84a0dd71720efee6d222a0fc0a89c1de8c1d72c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3214950
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Owners-Override: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930627}

--- base/task/task_runner_util.h
+++ base/task/task_runner_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_TASK_RUNNER_UTIL_H_
-#define BASE_TASK_RUNNER_UTIL_H_
+#ifndef BASE_TASK_TASK_RUNNER_UTIL_H_
+#define BASE_TASK_TASK_RUNNER_UTIL_H_
 
 #include <memory>
 #include <utility>
@@ -54,4 +54,4 @@ bool PostTaskAndReplyWithResult(TaskRunner* task_runner,
 
 }  // namespace base
 
-#endif  // BASE_TASK_RUNNER_UTIL_H_
+#endif  // BASE_TASK_TASK_RUNNER_UTIL_H_
