e126804e167a95386559ce97e77d1c8d9da0e796
Roman Sorokin <rsorokin@chromium.org>
2021-10-12 18:55:44 +0000

    [ChromeOS] OOBE: Deprecate OOBE.WebUIToViewsSwitch.Duration
    Fixed: 1258686
    Change-Id: I6690b5092fa4c9ccd27572b781a7ecc3fc462896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218895
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930705}

--- chrome/browser/ash/login/ui/login_display_host_webui.cc
+++ chrome/browser/ash/login/ui/login_display_host_webui.cc
@@ -27,9 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/task/single_thread_task_runner_forward.h"
-#include "base/threading/thread_restrictions.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
@@ -1287,37 +1284,9 @@ void ShowLoginWizard(OobeScreenId first_screen) {
   TriggerShowLoginWizardFinish(locale, std::move(data));
 }
 
-class WebUIToViewsSwitchMetricsReporter
-    : public session_manager::SessionManagerObserver {
- public:
-  WebUIToViewsSwitchMetricsReporter() {
-    session_observation_.Observe(session_manager::SessionManager::Get());
-  }
-
-  // session_manager::SessionManagerObserver:
-  void OnLoginOrLockScreenVisible() override {
-    if (LoginDisplayHost::default_host()->GetOobeUI()) {
-      DCHECK_EQ(OobeUI::kGaiaSigninDisplay,
-                LoginDisplayHost::default_host()->GetOobeUI()->display_type());
-    }
-    base::UmaHistogramTimes("OOBE.WebUIToViewsSwitch.Duration",
-                            timer_.Elapsed());
-    base::SequencedTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
-  }
-
- private:
-  base::ScopedObservation<session_manager::SessionManager,
-                          session_manager::SessionManagerObserver>
-      session_observation_{this};
-  base::ElapsedTimer timer_;
-};
-
 void SwitchWebUItoMojo() {
   DCHECK_EQ(LoginDisplayHost::default_host()->GetOobeUI()->display_type(),
             OobeUI::kOobeDisplay);
-
-  // The object deletes itself.
-  new WebUIToViewsSwitchMetricsReporter();
 
   // This replaces WebUI host with the Mojo (views) host.
   ShowLoginWizard(OobeScreen::SCREEN_UNKNOWN);
