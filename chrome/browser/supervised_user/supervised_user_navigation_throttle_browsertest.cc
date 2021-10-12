5ec8c7545acae435d3a420757ec900bee42fdbbe
Francois Doray <fdoray@chromium.org>
2021-10-12 16:07:44 +0000

    Disable SupervisedUserNavigationThrottleTest.DisallowPrerendering on ChromeOS.
    R=vollick@chromium.org
    
    Bug: 1259146
    Change-Id: I718254cff6b7d970f6b9e2de443e470af7746367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217255
    Auto-Submit: François Doray <fdoray@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930574}

--- chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
+++ chrome/browser/supervised_user/supervised_user_navigation_throttle_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/ash/login/test/logged_in_user_mixin.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_key.h"
@@ -222,8 +223,14 @@ void SupervisedUserNavigationThrottleTest::SetUpOnMainThread() {
 }
 
 // Tests that prerendering fails in supervised user mode.
+#if defined(OS_CHROMEOS)
+// TODO(crbug.com/1259146): Flaky on ChromeOS.
+#define MAYBE_DisallowPrerendering DISABLED_DisallowPrerendering
+#else
+#define MAYBE_DisallowPrerendering DisallowPrerendering
+#endif
 IN_PROC_BROWSER_TEST_F(SupervisedUserNavigationThrottleTest,
-                       DisallowPrerendering) {
+                       MAYBE_DisallowPrerendering) {
   base::HistogramTester histogram_tester;
   const GURL initial_url = embedded_test_server()->GetURL("/simple.html");
   const GURL allowed_url =
