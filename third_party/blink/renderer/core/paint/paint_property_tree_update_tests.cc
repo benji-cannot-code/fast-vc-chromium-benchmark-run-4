23661fbe2b85427eb8131a430c2f29b5bf4bca3e
Xianzhu Wang <wangxianzhu@chromium.org>
2021-10-12 00:12:27 +0000

    ScrollPaintPropertyNode::ContentsRect() instead of ContentsSize()
    It returns gfx::Rect(container_rect.origin, contents_size), which
    can help prevent errors caused by assuming the contents rect's origin
    is zero.
    
    Change-Id: I0cbbc93df1d315617f7800ff13541302a6b82ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3214937
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930358}

--- third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
@@ -794,7 +794,7 @@ TEST_P(PaintPropertyTreeUpdateTest, ScrollBoundsChange) {
                           ->ScrollTranslation()
                           ->ScrollNode();
   EXPECT_EQ(gfx::Rect(0, 0, 100, 100), scroll_node->ContainerRect());
-  EXPECT_EQ(gfx::Size(200, 200), scroll_node->ContentsSize());
+  EXPECT_EQ(gfx::Rect(0, 0, 200, 200), scroll_node->ContentsRect());
 
   GetDocument().getElementById("content")->setAttribute(
       html_names::kStyleAttr, "width: 200px; height: 300px");
@@ -804,7 +804,7 @@ TEST_P(PaintPropertyTreeUpdateTest, ScrollBoundsChange) {
                              ->ScrollTranslation()
                              ->ScrollNode());
   EXPECT_EQ(gfx::Rect(0, 0, 100, 100), scroll_node->ContainerRect());
-  EXPECT_EQ(gfx::Size(200, 300), scroll_node->ContentsSize());
+  EXPECT_EQ(gfx::Rect(0, 0, 200, 300), scroll_node->ContentsRect());
 }
 
 // The scrollbars are attached to the visual viewport but created by (and have
@@ -826,8 +826,8 @@ TEST_P(PaintPropertyTreeUpdateTest,
 
   EXPECT_EQ(gfx::Rect(0, 0, 800, 600),
             visual_viewport.GetScrollNode()->ContainerRect());
-  EXPECT_EQ(gfx::Size(800, 600),
-            visual_viewport.GetScrollNode()->ContentsSize());
+  EXPECT_EQ(gfx::Rect(0, 0, 800, 600),
+            visual_viewport.GetScrollNode()->ContentsRect());
 }
 
 TEST_P(PaintPropertyTreeUpdateTest, ViewportAddRemoveDeviceEmulationNode) {
