4af94f904362ee0f90e40c7b83a643d59b92fd77
Eric Willigers <ericwilligers@chromium.org>
2021-10-12 06:18:34 +0000

    Desktop PWA browser tests: Wait for system ready
    We override SetUpOnMainThread() to wait for the web app provider to
    signal ready.
    
    This is to eliminate a potential source of test flakiness.
    
    Bug: 1257413
    Change-Id: Ic94e6e919c7664d345bf0d98e7f94666f63dff98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218421
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930435}

--- chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
+++ chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/web_applications/web_app_dialog_utils.h"
 #include "chrome/browser/web_applications/install_bounce_metric.h"
 #include "chrome/browser/web_applications/os_integration_manager.h"
+#include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_install_finalizer.h"
 #include "chrome/browser/web_applications/web_app_prefs_utils.h"
@@ -167,6 +168,8 @@ class PwaInstallViewBrowserTest : public extensions::ExtensionBrowserTest {
     web_contents_ = GetCurrentTab();
     app_banner_manager_ =
         webapps::TestAppBannerManagerDesktop::FromWebContents(web_contents_);
+    web_app::test::WaitUntilReady(
+        web_app::WebAppProvider::GetForTest(browser()->profile()));
   }
 
   std::unique_ptr<net::test_server::HttpResponse> RequestInterceptor(
