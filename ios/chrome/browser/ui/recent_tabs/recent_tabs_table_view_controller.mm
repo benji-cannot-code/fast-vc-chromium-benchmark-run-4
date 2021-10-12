03cb3e4f14bbaaea94d111c7e5634881af894797
Ewann <ewannpv@google.com>
2021-10-12 07:11:39 +0000

    [1/N][iOS][Enterprise] Fixes SyncTypesListDisabled policy
    This CL fixes the recent tabs screen.
    When sync tabs is disabled by a policy, |isSyncDisabledByAdministrator|
    should return true.
    
    Bug: 1258623
    Change-Id: I87a3ca049365200ba507ce5a3cac52f66f209911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217637
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930448}

--- ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
+++ ios/chrome/browser/ui/recent_tabs/recent_tabs_table_view_controller.mm
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/authentication/cells/signin_promo_view_configurator.h"
 #import "ios/chrome/browser/ui/authentication/cells/signin_promo_view_consumer.h"
 #import "ios/chrome/browser/ui/authentication/cells/table_view_signin_promo_item.h"
+#import "ios/chrome/browser/ui/authentication/enterprise/enterprise_utils.h"
 #import "ios/chrome/browser/ui/authentication/signin/signin_utils.h"
 #import "ios/chrome/browser/ui/authentication/signin_presenter.h"
 #import "ios/chrome/browser/ui/authentication/signin_promo_view_mediator.h"
@@ -251,8 +252,11 @@ const int kRecentlyClosedTabsSectionIndex = 0;
 // Returns YES if the user cannot turn on sync for enterprise policy reasons.
 - (BOOL)isSyncDisabledByAdministrator {
   DCHECK(self.syncService);
-  return self.syncService->GetDisableReasons().Has(
+  bool syncDisabledPolicy = self.syncService->GetDisableReasons().Has(
       syncer::SyncService::DISABLE_REASON_ENTERPRISE_POLICY);
+  bool syncTypesDisabledPolicy =
+      IsManagedSyncDataType(self.browserState, SyncSetupService::kSyncOpenTabs);
+  return syncDisabledPolicy || syncTypesDisabledPolicy;
 }
 
 #pragma mark - SyncObserverModelBridge
