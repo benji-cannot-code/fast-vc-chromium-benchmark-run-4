7a1b5d7aaa99b420cb7b0223458e503cbd4e1c47
Nancy Wang <nancylingwang@chromium.org>
2021-10-12 19:54:55 +0000

    Remove restore data when Play Store is disabled.
    BUG=1243716
    
    Change-Id: If2bea5254b6aef715912d6a97af0d271fa6655bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3216290
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930743}

--- chrome/browser/ash/app_restore/app_restore_arc_task_handler.cc
+++ chrome/browser/ash/app_restore/app_restore_arc_task_handler.cc
@@ -116,6 +116,8 @@ void AppRestoreArcTaskHandler::OnArcPlayStoreEnabledChanged(bool enabled) {
     if (handler)
       handler->OnArcPlayStoreEnabledChanged(enabled);
   }
+
+  GetAppRestoreArcInfo()->NotifyPlayStoreEnabledChanged(enabled);
 }
 
 void AppRestoreArcTaskHandler::OnShelfReady() {
--- components/app_restore/arc_save_handler.cc
+++ components/app_restore/arc_save_handler.cc
@@ -218,6 +218,11 @@ void ArcSaveHandler::OnTaskDestroyed(int32_t task_id) {
   task_id_to_app_id_.erase(task_id);
 }
 
+void ArcSaveHandler::OnArcPlayStoreEnabledChanged(bool enabled) {
+  if (!enabled)
+    task_id_to_app_id_.clear();
+}
+
 void ArcSaveHandler::OnTaskThemeColorUpdated(int32_t task_id,
                                              uint32_t primary_color,
                                              uint32_t status_bar_color) {
