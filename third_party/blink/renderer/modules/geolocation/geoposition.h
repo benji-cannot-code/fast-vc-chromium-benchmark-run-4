b87990a68d50378a51b28f80214e0212b84f4c0f
Reilly Grant <reillyg@chromium.org>
2021-10-12 17:37:57 +0000

    Introduce EpochTimeStamp and use it in Geolocation
    This change introduces a replacement for DOMTimeStamp called
    EpochTimeStamp and a new helper function for directly converting from a
    base::Time as that is what all callers of ConvertSecondsToDOMTimeStamp()
    are using.
    
    The implementation of the GeolocationPosition interface is updated to
    use the new type.
    
    Bug: 1258466,1258483
    Change-Id: I01be5b304f00f83b1a825f0ec481aab4ff966064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217846
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930636}

--- third_party/blink/renderer/modules/geolocation/geoposition.h
+++ third_party/blink/renderer/modules/geolocation/geoposition.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_GEOLOCATION_GEOPOSITION_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_GEOLOCATION_GEOPOSITION_H_
 
-#include "third_party/blink/renderer/core/dom/dom_time_stamp.h"
+#include "third_party/blink/renderer/core/timing/epoch_time_stamp.h"
 #include "third_party/blink/renderer/modules/event_modules.h"
 #include "third_party/blink/renderer/modules/geolocation/geolocation_coordinates.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
@@ -39,7 +39,7 @@ class Geoposition final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  Geoposition(GeolocationCoordinates* coordinates, DOMTimeStamp timestamp)
+  Geoposition(GeolocationCoordinates* coordinates, EpochTimeStamp timestamp)
       : coordinates_(coordinates), timestamp_(timestamp) {
     DCHECK(coordinates_);
   }
@@ -49,12 +49,12 @@ class Geoposition final : public ScriptWrappable {
     ScriptWrappable::Trace(visitor);
   }
 
-  DOMTimeStamp timestamp() const { return timestamp_; }
+  EpochTimeStamp timestamp() const { return timestamp_; }
   GeolocationCoordinates* coords() const { return coordinates_; }
 
  private:
   Member<GeolocationCoordinates> coordinates_;
-  DOMTimeStamp timestamp_;
+  EpochTimeStamp timestamp_;
 };
 
 }  // namespace blink
