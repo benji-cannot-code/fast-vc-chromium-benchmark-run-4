ca6877511738609fb67c16d14b6589c2f399587a
minch <minch@chromium.org>
2021-10-12 19:05:25 +0000

    dark_mode: Building up PillButton
    - Building up class PillButton to include more properties of this
      button. Not only colors, button size etc as Decorate*PillButtons did.
    - Migrating "Sign out" button to this newly built PillButton.
    
    TODOs:
    - Migrate more buttons to PillButton. E.g, "Today" button in the new
      calendar. Remove RoundedLabelButton after all the clients got
      migrated.
    - Going to build up more button styles inside button_style,
      e.g, IconButtons.
    - Remove ash/element_style after all the button styles got built up
      in button_style and all the clients got migrated.
    - Unify font size, weight of these button styles.
    - Remove styles related functions inside TrayPopupUtils and migrate
      the clients to call corresponding APIs from ash/style.
    
    Bug: 1258935
    Change-Id: If8fd3783f2f15c82193f1cf30ba4a33bafb357e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218382
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930711}

--- ash/system/unified/top_shortcuts_view.cc
+++ ash/system/unified/top_shortcuts_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shutdown_controller_impl.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
+#include "ash/style/button_style.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/system/unified/collapse_button.h"
@@ -200,12 +201,14 @@ TopShortcutsView::TopShortcutsView(UnifiedSystemTrayController* controller) {
         UserChooserDetailedViewController::IsUserChooserEnabled());
     container_->AddUserAvatarButton(user_avatar_button_);
 
-    sign_out_button_ = new RoundedLabelButton(
+    sign_out_button_ = new PillButton(
         base::BindRepeating(&UnifiedSystemTrayController::HandleSignOutAction,
                             base::Unretained(controller)),
         user::GetLocalizedSignOutStringForStatus(
             Shell::Get()->session_controller()->login_status(),
-            /*multiline=*/false));
+            /*multiline=*/false),
+        PillButton::Type::kIconless,
+        /*icon=*/nullptr);
     container_->AddSignOutButton(sign_out_button_);
   }
 
