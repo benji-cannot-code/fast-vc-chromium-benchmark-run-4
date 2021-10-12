4fe95473d9db699ef8e4164b1ca87ce87052b5d8
Devlin <rdevlin.cronin@chromium.org>
2021-10-12 18:05:20 +0000

    Revert "[Extensions] Replace NOTIFICATION_EXTENSION_REMOVED in ExternalInstallManager"
    This reverts commit e7c77a1de6301faed4fd8fc3e82203e7516abe19.
    
    Reason for revert: Likely caused crbug.com/1259043
    
    Original change's description:
    > [Extensions] Replace NOTIFICATION_EXTENSION_REMOVED in ExternalInstallManager
    >
    > NOTIFICATION_EXTENSION_REMOVED has issues:
    > - It's deprecated (all legacy NOTIFICATION_-style events are)
    > - It's unpredictable: it can be fired either when an extension is
    >   unloaded (which can happen for any number of reasons) *or* as part of
    >   uninstallation for crashed extensions (but not other extensions).
    >
    > ExternalInstallManager uses the notification to check if the removed
    > extension was related to any of the active external install warnings
    > shown to the user, and remove that warning if so. ExternalInstallManager
    > also already observes ExtensionRegistryObserver::OnExtensionUninstalled,
    > which is sufficient for this use. Move the handling to remove the error
    > for the uninstalled extension to OnExtensionUninstalled, and stop
    > listening for NOTIFICATION_EXTENSION_REMOVED.
    >
    > Code History:
    > Why did the class observe these two methods? It originally observed
    > NOTIFICATION_EXTENSION_REMOVED because it didn't listen for
    > OnExtensionUninstalled. It at one point was going to listen for
    > OnExtensionUnloaded, but that was insufficient (because disabled
    > extensions were already unloaded), so it relied on the old notification.
    > This was done in https://codereview.chromium.org/300853008/.
    >
    > Then, the class was refactored to pull in more logic from
    > ExtensionService, and it began listening to OnExtensionUninstalled in
    > https://codereview.chromium.org/308473002. The naive Chromium developer*
    > who made those changes didn't realize that the notification could now be
    > cleaned up, and it subsequently listened to both.
    >
    > *It was me.
    >
    > Bug: 1174736
    > Change-Id: I5c83b19e90e913b33deedf3104ea179296f1c79f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209807
    > Reviewed-by: Tim Judkins <tjudkins@chromium.org>
    > Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#928972}
    
    Bug: 1174736
    Change-Id: I148600f71a9e36db70794d89204849cd4e1a6287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219344
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930660}

--- chrome/browser/extensions/external_install_manager.h
+++ chrome/browser/extensions/external_install_manager.h
@@ -11,12 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/scoped_observation.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_observer.h"
 #include "extensions/common/extension_id.h"
 
 namespace content {
 class BrowserContext;
+class NotificationDetails;
+class NotificationSource;
 }
 
 namespace extensions {
@@ -24,7 +28,8 @@ class Extension;
 class ExtensionPrefs;
 class ExternalInstallError;
 
-class ExternalInstallManager : public ExtensionRegistryObserver {
+class ExternalInstallManager : public ExtensionRegistryObserver,
+                               public content::NotificationObserver {
  public:
   ExternalInstallManager(content::BrowserContext* browser_context,
                          bool is_first_run);
@@ -81,6 +86,11 @@ class ExternalInstallManager : public ExtensionRegistryObserver {
                               const Extension* extension,
                               extensions::UninstallReason reason) override;
 
+  // content::NotificationObserver implementation.
+  void Observe(int type,
+               const content::NotificationSource& source,
+               const content::NotificationDetails& details) override;
+
   // Adds a global error informing the user that an external extension was
   // installed. If |is_new_profile| is true, then this error is from the first
   // time our profile checked for new extensions.
@@ -112,6 +122,8 @@ class ExternalInstallManager : public ExtensionRegistryObserver {
 
   // The error that is currently showing an alert dialog/bubble.
   ExternalInstallError* currently_visible_install_alert_;
+
+  content::NotificationRegistrar registrar_;
 
   base::ScopedObservation<ExtensionRegistry, ExtensionRegistryObserver>
       extension_registry_observation_{this};
