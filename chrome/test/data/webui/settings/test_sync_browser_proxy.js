1b7fb44c85e302eac7a149dd324a28a401129e59
dpapad <dpapad@chromium.org>
2021-10-12 09:11:56 +0000

    Settings TypeScript: Migrate sync_browser_proxy.js
    Adding minimal Closure externs in settings_controls_types.js for
    SyncBrowserProxy to allow chrome://os-settings to keep reusing the same
    file (instead of having to fork).
    
    Bug: 1234307
    Change-Id: If1aebb6eb7c84374883f587564882b630c516325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211847
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930474}

--- chrome/test/data/webui/settings/test_sync_browser_proxy.js
+++ chrome/test/data/webui/settings/test_sync_browser_proxy.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {isChromeOS} from 'chrome://resources/js/cr.m.js';
-import {PageStatus, StoredAccount, SyncBrowserProxy, SyncStatus} from 'chrome://settings/settings.js';
+import {PageStatus} from 'chrome://settings/settings.js';
 
 import {TestBrowserProxy} from '../test_browser_proxy.js';
 // clang-format on
