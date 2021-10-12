9f9ecc4866503af76f49f673333a92a40fdd9e58
Mason Freed <masonf@chromium.org>
2021-10-12 01:46:53 +0000

    Add bug number to two disabled SXG tests
    These two tests were disabled as part of [1], and this CL just
    adds a bug number to keep track.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/3187376
    
    Bug: 1258886
    Change-Id: I9eefd78feb274034cefcc0da5b0ec8c0c6169739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218125
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930369}

--- content/browser/web_package/signed_exchange_subresource_prefetch_browsertest.cc
+++ content/browser/web_package/signed_exchange_subresource_prefetch_browsertest.cc
@@ -1344,6 +1344,8 @@ IN_PROC_BROWSER_TEST_F(SignedExchangeSubresourcePrefetchBrowserTest,
       1 /* expected_script_fetch_count */);
 }
 
+// TODO(crbug.com/1258886): Fails flakily on all platforms with Synchronous HTML
+// Parsing enabled.
 IN_PROC_BROWSER_TEST_F(SignedExchangeSubresourcePrefetchBrowserTest,
                        DISABLED_ImageSrcsetAndSizes) {
   const char* prefetch_path = "/prefetch.html";
