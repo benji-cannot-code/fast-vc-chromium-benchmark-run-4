b65984cbfd8b751c70a58c9c05c96edcc362ff4b
Aleks Totic <atotic@chromium.org>
2021-10-12 05:38:26 +0000

    Port MARQEE/METER elements to manual slotting
    Bug: 1179356
    Change-Id: I5bdb42258ec0e65b77f27b4ea4d8bec593e597e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218309
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Auto-Submit: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930426}

--- third_party/blink/renderer/core/html/html_marquee_element.cc
+++ third_party/blink/renderer/core/html/html_marquee_element.cc
@@ -57,7 +57,7 @@ namespace blink {
 HTMLMarqueeElement::HTMLMarqueeElement(Document& document)
     : HTMLElement(html_names::kMarqueeTag, document) {
   UseCounter::Count(document, WebFeature::kHTMLMarqueeElement);
-  EnsureUserAgentShadowRoot();
+  EnsureUserAgentShadowRoot().EnableNameBasedSlotAssignment();
 }
 
 void HTMLMarqueeElement::DidAddUserAgentShadowRoot(ShadowRoot& shadow_root) {
@@ -74,8 +74,7 @@ void HTMLMarqueeElement::DidAddUserAgentShadowRoot(ShadowRoot& shadow_root) {
   auto* mover = MakeGarbageCollected<HTMLDivElement>(GetDocument());
   shadow_root.AppendChild(mover);
 
-  mover->AppendChild(
-      HTMLSlotElement::CreateUserAgentDefaultSlot(GetDocument()));
+  mover->AppendChild(MakeGarbageCollected<HTMLSlotElement>(GetDocument()));
   mover_ = mover;
 }
 
