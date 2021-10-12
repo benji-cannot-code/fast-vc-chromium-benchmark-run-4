a9f77d141b99433b331a043b029d7fdc817817ea
Jason Sun <jasonsun@google.com>
2021-10-12 15:09:08 +0000

    [CameraRoll] Implement item data transfer protocol
    Add parsing logic for request/response messages needed to complete the
    file transfer of a camera roll item.
    
    Bug: 1221297
    Change-Id: I38419d6fcc32e9fcbbf9ddd9dbb3a5801296ae12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217647
    Commit-Queue: Jason Sun <jasonsun@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930550}

--- chromeos/components/phonehub/message_receiver.cc
+++ chromeos/components/phonehub/message_receiver.cc
@@ -37,5 +37,11 @@ void MessageReceiver::NotifyFetchCameraRollItemsResponseReceived(
     observer.OnFetchCameraRollItemsResponseReceived(response);
 }
 
+void MessageReceiver::NotifyFetchCameraRollItemDataResponseReceived(
+    const proto::FetchCameraRollItemDataResponse& response) {
+  for (auto& observer : observer_list_)
+    observer.OnFetchCameraRollItemDataResponseReceived(response);
+}
+
 }  // namespace phonehub
 }  // namespace chromeos
