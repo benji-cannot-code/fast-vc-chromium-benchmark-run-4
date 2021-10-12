9c00679e5c6b13a9c5d4acd136a08b71105d901d
Xi Han <hanxi@google.com>
2021-10-12 15:00:18 +0000

    [Start] Fix StartSurfaceTest#testStartSurfaceMenu* test on L.
    The tests fail on L due to an extra update menu item: "Chrome can't
    updated (android version unsupported)" is shown on L. Update the test
    to increase the menu size if the update menu item exists.
    
    Bug: 1259047
    Change-Id: I8dab59f8b41eb53dbef52bbf3299bbd8de5f9a34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217656
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930547}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -2275,9 +2275,13 @@ public class StartSurfaceTest {
         assertNull(AppMenuTestSupport.getMenuItemPropertyModel(
                 mActivityTestRule.getAppMenuCoordinator(), R.id.close_all_tabs_menu_id));
 
+        boolean hasUpdateMenuItem =
+                AppMenuTestSupport.getMenuItemPropertyModel(
+                        mActivityTestRule.getAppMenuCoordinator(), R.id.update_menu_id)
+                != null;
         ModelList menuItemsModelList =
                 AppMenuTestSupport.getMenuModelList(mActivityTestRule.getAppMenuCoordinator());
-        assertEquals(11, menuItemsModelList.size());
+        assertEquals(hasUpdateMenuItem ? 12 : 11, menuItemsModelList.size());
     }
 
     /**
