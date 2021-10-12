7957e7daa92c2eb205552a8a69ea233a1ada6260
Robbie Gibson <rkgibson@google.com>
2021-10-12 14:24:04 +0000

    [iOS] Add SwiftUI wrapper for Chrome colors
    Bug: 1254379
    Change-Id: Idfecb663441299045924c860b7ec4bdd69119e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217744
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Javier Flores <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930532}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_destination_view.swift
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_destination_view.swift
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import SwiftUI
+import ios_chrome_common_ui_colors_swift
 
 /// A view displaying a single destination.
 struct OverflowMenuDestinationView: View {
@@ -26,7 +27,7 @@ struct OverflowMenuDestinationView: View {
     VStack {
       ZStack(alignment: .center) {
         Rectangle()
-          .foregroundColor(.white)
+          .foregroundColor(.cr_groupedSecondaryBackground)
           .frame(width: Dimensions.iconSize, height: Dimensions.iconSize)
           .cornerRadius(Dimensions.cornerRadius)
         destination.image
---
+++ ios/chrome/common/ui/colors/Color+Chrome.swift
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import SwiftUI
+
+// Adds easy SwiftUI access to the Chrome color palette
+extension Color {
+  /// The secondary grouped background color
+  public static var cr_groupedSecondaryBackground: Color {
+    return Color(kGroupedSecondaryBackgroundColor)
+  }
+}
