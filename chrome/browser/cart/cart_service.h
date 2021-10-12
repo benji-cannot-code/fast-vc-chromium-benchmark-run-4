2cba4e295a37fb078b264a4872c413605417dc71
Mei Liang <meiliang@chromium.org>
2021-10-12 04:36:16 +0000

    [Coupon] Add browser test for FetchDiscountWorker
    This CL adds browser test to cover the FL codeless discount code path,
    and fix a bug.
    
    Change-Id: I0e8446712461135f9329c53bb25be770933a9bf5
    Bug: 1240341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3214926
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930415}

--- chrome/browser/cart/cart_service.h
+++ chrome/browser/cart/cart_service.h
@@ -131,6 +131,7 @@ class CartService : public history::HistoryServiceObserver,
   friend class CartServiceBrowserDiscountTest;
   friend class CartServiceDiscountFetchTest;
   friend class CartServiceCouponTest;
+  friend class FetchDiscountWorkerBrowserTest;
   FRIEND_TEST_ALL_PREFIXES(CartHandlerNtpModuleFakeDataTest,
                            TestEnableFakeData);
 
