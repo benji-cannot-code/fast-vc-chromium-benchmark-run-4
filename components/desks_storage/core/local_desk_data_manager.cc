1eee615ed2b6ff8babd7140e9d4d7cce7bbe3c4b
Yanzhu Du <yzd@google.com>
2021-10-12 17:29:52 +0000

    Remove deprecated desks_storage::DeskTemplate.
    desks_storage::DeskTemplate has been replaced by ash::DeskTemplate.
    
    Bug: 1177413
    Change-Id: If674ec20c84534ee3adbee5966cc039574943fb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205969
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Yanzhu Du <yzd@google.com>
    Cr-Commit-Position: refs/heads/main@{#930630}

--- components/desks_storage/core/local_desk_data_manager.cc
+++ components/desks_storage/core/local_desk_data_manager.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/app_restore/restore_data.h"
 #include "components/desks_storage/core/desk_model.h"
-#include "components/desks_storage/core/desk_template.h"
 #include "components/sync/protocol/workspace_desk_specifics.pb.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/re2/src/re2/re2.h"
--- components/desks_storage/core/local_desks_data_manager_unittests.cc
+++ components/desks_storage/core/local_desks_data_manager_unittests.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "components/desks_storage/core/desk_template.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace desks_storage {
