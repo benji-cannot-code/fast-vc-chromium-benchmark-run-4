578ed5136769b576623e2556eb1dd451378eaeb2
Sophey <sophey@chromium.org>
2021-10-12 18:42:13 +0000

    [LR] Keep track of the active reaction and implement (de)selecting.
    - When a new reaction is added, the new reaction is active and the
    previous reaction is no longer active.
    - When the background is pressed, the active reaction is no longer
    active
    - Active status is triggered via clicking on the ImageView
    
    Fixed: 1258879, 1258878
    Change-Id: I1afdf2d76c0a8a4a8f9ca2dec83ce0bd022db68c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218122
    Commit-Queue: Sophey <sophey@chromium.org>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930692}

--- chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/scene/ReactionLayout.java
+++ chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/scene/ReactionLayout.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -17,13 +18,19 @@ import org.chromium.chrome.browser.content_creation.reactions.internal.R;
 import org.chromium.ui.widget.ChromeImageButton;
 
 class ReactionLayout extends RelativeLayout {
+    private final int mReactionPadding;
+
     private ChromeImageButton mCopyButton;
+    private ChromeImageButton mDeleteButton;
+    private ChromeImageButton mAdjustButton;
     private Drawable mDrawable;
     private ImageView mReaction;
     private SceneEditorDelegate mSceneEditorDelegate;
+    private boolean mIsActive;
 
     public ReactionLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mReactionPadding = context.getResources().getDimensionPixelSize(R.dimen.reaction_padding);
     }
 
     /**
@@ -34,9 +41,8 @@ class ReactionLayout extends RelativeLayout {
     void init(Drawable drawable, SceneEditorDelegate sceneEditorDelegate) {
         mDrawable = drawable;
         mSceneEditorDelegate = sceneEditorDelegate;
-        if (mReaction != null) {
-            mReaction.setImageDrawable(mDrawable);
-        }
+        mIsActive = true;
+        setUpReactionView();
     }
 
     Drawable getReaction() {
@@ -47,10 +53,32 @@ class ReactionLayout extends RelativeLayout {
     public void onFinishInflate() {
         super.onFinishInflate();
         mReaction = findViewById(R.id.reaction_view);
-        if (mDrawable != null) {
-            mReaction.setImageDrawable(mDrawable);
-        }
         setUpCopyButton();
+        mDeleteButton = findViewById(R.id.delete_button);
+        mAdjustButton = findViewById(R.id.adjust_button);
+    }
+
+    void setActive(boolean isActive) {
+        if (mIsActive == isActive) return;
+
+        mIsActive = isActive;
+        int visibility = mIsActive ? View.VISIBLE : View.GONE;
+        mCopyButton.setVisibility(visibility);
+        mDeleteButton.setVisibility(visibility);
+        mAdjustButton.setVisibility(visibility);
+        if (!mIsActive) {
+            mReaction.setBackground(null);
+        } else {
+            mReaction.setBackgroundResource(R.drawable.border_inset);
+            mReaction.setPadding(
+                    mReactionPadding, mReactionPadding, mReactionPadding, mReactionPadding);
+        }
+    }
+
+    private void setUpReactionView() {
+        mReaction.setImageDrawable(mDrawable);
+        mReaction.setOnClickListener(
+                view -> mSceneEditorDelegate.markActiveStatus(this, !mIsActive));
     }
 
     private void setUpCopyButton() {
