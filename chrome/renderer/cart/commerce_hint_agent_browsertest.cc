8e81945bcbb835d45a8e047399dc956ff88d7a22
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2021-10-12 01:51:10 +0000

    [ChromeCart] Fix cart-extraction-max-count
    Cart parameter cart-extraction-max-count was introduced in
    https://crrev.com/c/3205509, but the counter was incremented and checked
    inconsistently, leading to potentially more extractions than allowed.
    
    Bug: 1258855, 1257901
    Change-Id: Idddc1eb509f5fff9c0bf3ef44eaad6b5715f0d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211774
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930377}

--- chrome/renderer/cart/commerce_hint_agent_browsertest.cc
+++ chrome/renderer/cart/commerce_hint_agent_browsertest.cc
@@ -243,7 +243,7 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
       run_loop.Run();
       if (satisfied_)
         break;
-      base::PlatformThread::Sleep(TestTimeouts::tiny_timeout());
+      base::PlatformThread::Sleep(TestTimeouts::tiny_timeout() * 10);
     }
   }
 
@@ -264,8 +264,8 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
                   expected[i].second.merchant_cart_url());
       }
     } else {
-      VLOG(3) << "Found " << found.size() << " but expecting "
-              << expected.size();
+      LOG(INFO) << "WaitForCartCount() is expecting " << expected.size()
+                << " but found " << found.size();
     }
     std::move(closure).Run();
   }
@@ -281,7 +281,7 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
       run_loop.Run();
       if (satisfied_)
         break;
-      base::PlatformThread::Sleep(TestTimeouts::tiny_timeout());
+      base::PlatformThread::Sleep(TestTimeouts::tiny_timeout() * 10);
     }
   }
 
@@ -300,6 +300,9 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
                           .ReplaceComponents(remove_port)
                           .spec() == expected[i].second.merchant_cart_url();
       }
+    } else {
+      LOG(INFO) << "WaitForCarts() is expecting " << expected.size()
+                << " but found " << found.size();
     }
     std::move(closure).Run();
   }
@@ -315,7 +318,7 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
       run_loop.Run();
       if (satisfied_)
         break;
-      base::PlatformThread::Sleep(TestTimeouts::tiny_timeout());
+      base::PlatformThread::Sleep(TestTimeouts::tiny_timeout() * 10);
     }
   }
 
@@ -325,8 +328,11 @@ class CommerceHintAgentTest : public PlatformBrowserTest {
                              ShoppingCarts found) {
     bool fail = false;
     bool same_size = found.size() == expected.size();
-    if (!same_size)
+    if (!same_size) {
       fail = true;
+      LOG(INFO) << "WaitForProductCount() is expecting " << expected.size()
+                << " but found " << found.size();
+    }
     for (size_t i = 0; i < std::min(found.size(), expected.size()); i++) {
       EXPECT_EQ(found[i].first, expected[i].first);
       GURL::Replacements remove_port;
@@ -847,7 +853,8 @@ class CommerceHintTimeoutTest : public CommerceHintAgentTest {
   void SetUpInProcessBrowserTestFixture() override {
     scoped_feature_list_.InitWithFeaturesAndParameters(
         {{ntp_features::kNtpChromeCartModule,
-          {{"cart-extraction-timeout", "0"}}}},
+          {{"cart-extraction-max-count", "1"},
+           {"cart-extraction-timeout", "0"}}}},
         {optimization_guide::features::kOptimizationHints});
   }
 
@@ -883,9 +890,7 @@ class CommerceHintMaxCountTest : public CommerceHintAgentTest {
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-// Flaky on Linux: https://crbug.com/1257964.
-// See definition of MAYBE_ExtractCart above.
-IN_PROC_BROWSER_TEST_F(CommerceHintMaxCountTest, MAYBE_ExtractCart) {
+IN_PROC_BROWSER_TEST_F(CommerceHintMaxCountTest, ExtractCart) {
   NavigateToURL("https://www.guitarcenter.com/cart.html");
 
   WaitForUmaBucketCount("Commerce.Carts.ExtractionTimedOut", 0, 1);
