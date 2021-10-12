44572919d193f6ed7b4f2488ca870e95d103952d
Eric Willigers <ericwilligers@chromium.org>
2021-10-12 23:49:35 +0000

    AppListSortBrowserTest: LacrosSupport expectations
    When the LacrosSupport feature is enabled, AppListSortBrowserTest now
    expects one additional default app (the Lacros browser).
    
    Bug: 1258445
    Change-Id: I382b8eb5e3bae7ed932cd05f4e269bddb98eff77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217431
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930854}

--- chrome/browser/ui/app_list/app_list_sort_browsertest.cc
+++ chrome/browser/ui/app_list/app_list_sort_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/test/app_list_test_api.h"
 #include "ash/public/cpp/test/shell_test_api.h"
 #include "ash/shell.h"
+#include "base/feature_list.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/app_list/app_list_model_updater.h"
@@ -48,8 +49,15 @@ class AppListSortBrowserTest : public extensions::ExtensionBrowserTest {
     ash::AcceleratorController::Get()->PerformActionIfEnabled(
         ash::TOGGLE_APP_LIST_FULLSCREEN, {});
 
-    // Assume that there are two default apps.
-    ASSERT_EQ(2, app_list_test_api_.GetTopListItemCount());
+    const int default_app_count = app_list_test_api_.GetTopListItemCount();
+
+    if (base::FeatureList::IsEnabled(chromeos::features::kLacrosSupport)) {
+      // Assume that there are three default apps, one being the Lacros browser.
+      ASSERT_EQ(3, app_list_test_api_.GetTopListItemCount());
+    } else {
+      // Assume that there are two default apps.
+      ASSERT_EQ(2, app_list_test_api_.GetTopListItemCount());
+    }
 
     app1_id_ = LoadExtension(test_data_dir_.AppendASCII("app1"))->id();
     ASSERT_FALSE(app1_id_.empty());
@@ -59,7 +67,7 @@ class AppListSortBrowserTest : public extensions::ExtensionBrowserTest {
     // app in this test.
     app3_id_ = LoadExtension(test_data_dir_.AppendASCII("app4"))->id();
     ASSERT_FALSE(app3_id_.empty());
-    EXPECT_EQ(5, app_list_test_api_.GetTopListItemCount());
+    EXPECT_EQ(default_app_count + 3, app_list_test_api_.GetTopListItemCount());
 
     event_generator_ = std::make_unique<ui::test::EventGenerator>(
         ash::Shell::GetPrimaryRootWindow());
