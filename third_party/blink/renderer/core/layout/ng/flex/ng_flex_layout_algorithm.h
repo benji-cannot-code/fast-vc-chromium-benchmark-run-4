c0ece0fef7267ae02f4836873dd937075e6fd724
Alison Maher <almaher@microsoft.com>
2021-10-12 19:21:54 +0000

    [FlexNG] Refactor for constraint space
    This CL moves the logic for setting up the flex item constraint space
    during layout to a new helper method: BuildSpaceForLayout().
    
    Bug: 660611
    Change-Id: If1f99341428db6a4ae9b4a00c04e08d409b74a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219432
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#930723}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.h
@@ -58,6 +58,7 @@ class CORE_EXPORT NGFlexLayoutAlgorithm
   NGConstraintSpace BuildSpaceForFlexBasis(const NGBlockNode& flex_item) const;
   NGConstraintSpace BuildSpaceForIntrinsicBlockSize(
       const NGBlockNode& flex_item) const;
+  NGConstraintSpace BuildSpaceForLayout(const FlexItem& flex_item) const;
   void ConstructAndAppendFlexItems();
   void ApplyStretchAlignmentToChild(FlexItem& flex_item);
   bool GiveLinesAndItemsFinalPositionAndSize();
