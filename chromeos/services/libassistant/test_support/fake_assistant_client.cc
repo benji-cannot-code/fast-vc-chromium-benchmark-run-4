5e60fca007720d7aee810be3dfc2e8b4539e1fe2
Meilin Wang <meilinw@google.com>
2021-10-12 19:21:24 +0000

    [LibassistantV2] Rename OnDisplayRequest() to SendDisplayRequest()
    Rename to avoid confusion and stick with the naming pattern used by
    other APIs in the same file.
    
    Bug: NA.
    Test: run unittests
    Change-Id: I83e1e1f1b63db26d9782a750a2e6a3fc6c5db3f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3215002
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930722}

--- chromeos/services/libassistant/test_support/fake_assistant_client.cc
+++ chromeos/services/libassistant/test_support/fake_assistant_client.cc
@@ -59,7 +59,7 @@ void FakeAssistantClient::GetSpeakerIdEnrollmentInfo(
 
 void FakeAssistantClient::ResetAllDataAndShutdown() {}
 
-void FakeAssistantClient::OnDisplayRequest(
+void FakeAssistantClient::SendDisplayRequest(
     const OnDisplayRequestRequest& request) {}
 
 void FakeAssistantClient::AddDisplayEventObserver(
