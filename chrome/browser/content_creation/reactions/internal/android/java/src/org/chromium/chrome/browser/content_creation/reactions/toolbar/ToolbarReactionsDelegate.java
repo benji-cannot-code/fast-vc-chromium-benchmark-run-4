265e294c3aa09844dad7c25aba02dc1b6053b3f9
Guillaume Jenkins <gujen@google.com>
2021-10-12 22:38:40 +0000

    [LR] Create TolbarReactionsDelegate
    - Creates the ToolbarReactionsDelegate interface, which
      defines the API that the SceneCoordinator must implement
      to handle the reaction taps from the toolbar.
    - Makes the SceneDelegate implement this new delegate, with
      empty methods for now.
    - Makes the LightweightReactionsCoordinator pass the scene
      coordinator to the toolbar coordinator on creation as its
      reaction delegate.
    
    Bug: 1253538
    Change-Id: Icabdae558b713f687f180c42f56d918cb8ebbb0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213206
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Sophey <sophey@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930815}

---
+++ chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/toolbar/ToolbarReactionsDelegate.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.content_creation.reactions.toolbar;
+
+import org.chromium.components.content_creation.reactions.ReactionMetadata;
+
+/**
+ * Interface used by the toolbar to delegate action handlers for tapping reactions.
+ */
+public interface ToolbarReactionsDelegate {
+    /**
+     * Invoked when the user taps a reaction.
+     */
+    public void onToolbarReactionTapped(ReactionMetadata reactionData);
+}
