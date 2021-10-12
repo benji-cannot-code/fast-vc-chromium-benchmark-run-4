a08908273b96c6803d68d2012297cc6816bbaad1
Kevin McNee <mcnee@chromium.org>
2021-10-12 21:42:22 +0000

    Introduce RenderFrameHost::GetOutermostMainFrame
    This method iterates through parent frames and outer documents (in the
    case of fenced frames and portals) until no further ancestor is found.
    It is the inner frame tree aware analogue of GetMainFrame.
    
    We also rename a content internal method to
    GetOutermostMainFrameOrEmbedder, as this is the version that escapes
    GuestViews.
    
    Bug: 1196715, 1061899
    Change-Id: I65050faddbe7ea72d83fe97b4d5cb23cf85335ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3210026
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930783}

--- content/browser/renderer_host/cross_process_frame_connector.cc
+++ content/browser/renderer_host/cross_process_frame_connector.cc
@@ -47,7 +47,7 @@ CrossProcessFrameConnector::CrossProcessFrameConnector(
   // be on the correct display. All subsequent updates to |screen_infos_|
   // ultimately come from the root, so it makes sense to do it here as well.
   screen_infos_ = current_child_frame_host()
-                      ->GetOutermostMainFrame()
+                      ->GetOutermostMainFrameOrEmbedder()
                       ->GetRenderWidgetHost()
                       ->GetScreenInfos();
 }
@@ -455,7 +455,7 @@ CrossProcessFrameConnector::GetRootRenderWidgetHostView() {
     return nullptr;
 
   RenderFrameHostImpl* root =
-      current_child_frame_host()->GetOutermostMainFrame();
+      current_child_frame_host()->GetOutermostMainFrameOrEmbedder();
   return static_cast<RenderWidgetHostViewBase*>(root->GetView());
 }
 
