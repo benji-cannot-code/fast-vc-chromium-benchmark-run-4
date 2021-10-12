9eeab0e2a802a072a13259e43db63e050fe4e0d3
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-10-12 17:21:25 +0000

    [send-tab-to-self] Fix target device list not being updated often enough
    Before:
    SendTabToSelfBridge::{GetTargetDeviceInfoSortedList,HasValidTargetDevice}
    would recompute (sort+dedupe) the target device list or not, based on
    ShouldUpdateTargetDeviceInfoList(). The predicate turned out to return
    false more often than it should, causing the bridge to expose an
    outdated list.
    [1] is the thread where this design got decided .
    
    Now:
    1. SendTabToSelfBridge observes DeviceInfoTracker and recomputes the
    device list on every OnDeviceInfoChange().
    
    2. {GetTargetDeviceInfoSortedList,HasValidTargetDevice} handle device
    expiration. Note that (1) doesn't handle this: the "last used"
    timestamps might stay the same, but will eventually be considered too
    old.
    This change means the worst-case time complexity of these methods is
    now better.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1585944/comments/264f963d_33bde0f3
    
    Fixed: 1257573
    Change-Id: I017d9ce69a759332096fbb72d4fe14afb353a247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217642
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930622}

--- components/sync_device_info/fake_device_info_tracker.cc
+++ components/sync_device_info/fake_device_info_tracker.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync_device_info/fake_device_info_tracker.h"
 
+#include <algorithm>
+
 #include "base/check.h"
 #include "base/notreached.h"
 #include "components/sync_device_info/device_info.h"
@@ -82,6 +84,16 @@ bool FakeDeviceInfoTracker::IsRecentLocalCacheGuid(
 
 void FakeDeviceInfoTracker::Add(const DeviceInfo* device) {
   devices_.push_back(device);
+  for (auto& observer : observers_)
+    observer.OnDeviceInfoChange();
+}
+
+void FakeDeviceInfoTracker::Replace(const DeviceInfo* old_device,
+                                    const DeviceInfo* new_device) {
+  std::vector<const DeviceInfo*>::iterator it =
+      std::find(devices_.begin(), devices_.end(), old_device);
+  DCHECK(devices_.end() != it) << "Tracker doesn't contain device";
+  *it = new_device;
   for (auto& observer : observers_)
     observer.OnDeviceInfoChange();
 }
