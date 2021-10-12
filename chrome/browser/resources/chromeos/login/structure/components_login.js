97df5e7cf658aa864764391f6e9664ddd505e6ce
Danila Kuzmin <dkuzmin@google.com>
2021-10-12 12:36:03 +0000

    OOBE: Migrate lacros-data-migration to Polymer3
    Bug: 1258569
    Change-Id: Id63aa21e920cb16c7f0c90caee0de61d7a5bf7fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218070
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#930510}

--- chrome/browser/resources/chromeos/login/structure/components_login.js
+++ chrome/browser/resources/chromeos/login/structure/components_login.js
@@ -8,4 +8,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="../screens/login/encryption_migration.js">
 // <include src="../screens/login/update_required_card.js">
 // <include src="../screens/login/offline_login.js">
-// <include src="../screens/login/lacros_data_migration.js">
