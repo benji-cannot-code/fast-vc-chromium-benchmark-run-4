23a087659cbc6aa2478b5cfd4745db1da77e448d
Sebastien Lalancette <seblalancette@chromium.org>
2021-10-13 20:17:54 +0000

    [CAS] Create the DeviceTrustConnectorEnabled feature flag
    This feature flag will be used to control the client-side portion of the
    Device Trust enterprise connector.
    
    Bug: b/202438904
    Change-Id: I13a4d344bfb9f8c9a43b58b922a61855c29ff1df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220861
    Auto-Submit: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931198}

---
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_features.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_DEVICE_TRUST_FEATURES_H_
+#define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_DEVICE_TRUST_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace enterprise_connectors {
+
+// Controls whether the Device Trust connector client code is enabled or not.
+extern const base::Feature kDeviceTrustConnectorEnabled;
+
+}  // namespace enterprise_connectors
+
+#endif  // CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_DEVICE_TRUST_FEATURES_H_
