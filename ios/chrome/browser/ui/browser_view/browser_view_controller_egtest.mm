0bc9f5162c6fd66fd4776916b549f66b49859cbc
Peter Kasting <pkasting@chromium.org>
2021-10-13 15:06:31 +0000

    Fix -Wunusued-but-set-variable: /ios/chrome/browser/ui/browser_view
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 1203071
    Change-Id: I87ef64fa6f3702ec958d13786005cb596f1c1346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219334
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931062}

--- ios/chrome/browser/ui/browser_view/browser_view_controller_egtest.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller_egtest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #import "base/ios/ios_util.h"
+#include "base/macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/ui/start_surface/start_surface_features.h"
@@ -53,8 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                                             block:^BOOL {
                                                               return NO;
                                                             }];
-    BOOL success = [myCondition waitWithTimeout:0.05];
-    success = NO;
+    ignore_result([myCondition waitWithTimeout:0.05]);
 
     [ChromeEarlGrey openNewTab];
   }  // End of the sync disabler scope.
