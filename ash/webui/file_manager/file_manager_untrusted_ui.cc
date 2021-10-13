3a1d734a4405d097bc8bc40f1b0dc31bea238afc
Jérémie Boulic <jboulic@chromium.org>
2021-10-13 17:26:38 +0000

    Reland "[Files app] Separate chrome-untrusted resources"
    This is a reland of 2288a6bf377b2a627d669cd8296552fddb67b373
    
    Include paths for moved resources have been updated.
    
    Original change's description:
    > [Files app] Separate chrome-untrusted resources
    >
    > The resources in u/f/file_manager/foreground/elements/sandboxed/
    > are meant to be loaded in a safe environment with restricted
    > permissions (either <webview> or an iframed chrome-untrusted page).
    >
    > Separate these resources from the main ui/file_manager code to better
    > reflect the fact that chrome-untrusted://file-manager is now its own
    > separate page with its own resources.
    >
    > Legacy Files app now "borrows" these resources from the chrome-untrusted
    > WebUI, to load them in <webview> tags. This code is meant to be removed
    > once Files app fully migrates to a SWA.
    >
    > Test: browser_tests --gtest_filter=QuickView/FilesAppBrowserTest.Test/*
    > Bug: b:192894681, b:201363705, b:197601728
    > Change-Id: I3490672beb5a72d814cd5fb8c7d908fe8678472d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189577
    > Reviewed-by: calamity <calamity@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Bo Majewski <majewski@chromium.org>
    > Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#930868}
    
    Bug: b:192894681, b:201363705, b:197601728
    Change-Id: Ibdfb3783d650dda05576768f85421b3ed6d3dd07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219021
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Jeremie Boulic <jboulic@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931116}

--- ash/webui/file_manager/file_manager_untrusted_ui.cc
+++ ash/webui/file_manager/file_manager_untrusted_ui.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/file_manager/file_manager_untrusted_ui.h"
 
-#include "ash/webui/file_manager/resources/grit/file_manager_untrusted_resources.h"
-#include "ash/webui/file_manager/resources/grit/file_manager_untrusted_resources_map.h"
+#include "ash/webui/file_manager/untrusted_resources/grit/file_manager_untrusted_resources.h"
+#include "ash/webui/file_manager/untrusted_resources/grit/file_manager_untrusted_resources_map.h"
 #include "ash/webui/file_manager/url_constants.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
