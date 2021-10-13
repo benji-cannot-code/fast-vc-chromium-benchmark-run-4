456a7c34cf766c17d8224fe964f2a8256a01238c
Stephan Hartmann <stha09@googlemail.com>
2021-10-13 23:33:53 +0000

    IWYU: add string.h for memcpy in web_package
    Bug: None
    Change-Id: Ie23278547db7a570da06f3092449bb461399f50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220636
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931301}

--- components/web_package/web_bundle_builder.cc
+++ components/web_package/web_bundle_builder.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/web_package/web_bundle_builder.h"
 
+#include <string.h>
+
 #include <ostream>
 
 #include "base/big_endian.h"
