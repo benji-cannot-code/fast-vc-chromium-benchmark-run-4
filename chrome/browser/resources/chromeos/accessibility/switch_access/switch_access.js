bfe612a9e0cecdea96e3e2933f0d895c0bab3df8
Akihiro Ota <akihiroota@chromium.org>
2021-10-13 16:11:49 +0000

    Accessibility sprint: Add quick commands sub menu
    This change adds a quick commands sub menu to Switch Access. This will
    allow quick commands to be easily added to Switch Access and is hidden
    behind the flag:
    
    enable-experimental-accessibility-switch-access-multistep-automation
    
    AX-Relnotes: N/A
    Bug: 1258921
    Change-Id: I5206f92687860e33fe0dbe2528d2ffc1660739d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218520
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931077}

--- chrome/browser/resources/chromeos/accessibility/switch_access/switch_access.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/switch_access.js
@@ -62,9 +62,18 @@ export class SwitchAccess {
      */
     this.enableImprovedTextInput_ = false;
 
+    /** @private {boolean} */
+    this.enableMultistepAutomationFeatures_ = false;
+
     chrome.commandLinePrivate.hasSwitch(
         'enable-experimental-accessibility-switch-access-text', (result) => {
           this.enableImprovedTextInput_ = result;
+        });
+
+    chrome.commandLinePrivate.hasSwitch(
+        'enable-experimental-accessibility-switch-access-multistep-automation',
+        (enabled) => {
+          this.enableMultistepAutomationFeatures_ = enabled;
         });
 
     /* @private {!SAConstants.Mode} */
@@ -78,6 +87,11 @@ export class SwitchAccess {
    */
   improvedTextInputEnabled() {
     return this.enableImprovedTextInput_;
+  }
+
+  /** @return {boolean} */
+  multistepAutomationFeaturesEnabled() {
+    return this.enableMultistepAutomationFeatures_;
   }
 
   /** @return {!SAConstants.Mode} */
