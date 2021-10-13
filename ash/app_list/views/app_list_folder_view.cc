679f6818995907b83efa2a3c22cadf66c8da84e8
James Cook <jamescook@chromium.org>
2021-10-13 17:25:52 +0000

    app_list: Clean up scroll event forwarding to folders
    Update how scroll and mouse wheel events are handled:
    - AppListView no longer forwards them to an open folder
    - AppListFolderView handles the events directly
    
    This makes a small UI improvement. Scroll events and mouse wheel
    events in the folder view header area now scroll the folder.
    
    For ProductivityLauncher, ScrollableAppsGridView now handles scroll
    events by scrolling vertically. This addresses a TODO.
    
    Bug: 1214064
    Test: ash_unittests
    Change-Id: I84414f21199c6baf6493474ca1912f2121e99c27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219563
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931115}

--- ash/app_list/views/app_list_folder_view.cc
+++ ash/app_list/views/app_list_folder_view.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/events/event.h"
+#include "ui/events/types/event_type.h"
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/point.h"
@@ -914,6 +915,20 @@ void AppListFolderView::OnTabletModeChanged(bool started) {
   folder_header_view()->set_tablet_mode(started);
   if (page_switcher_)
     page_switcher_->set_is_tablet_mode(started);
+}
+
+void AppListFolderView::OnScrollEvent(ui::ScrollEvent* event) {
+  items_grid_view_->HandleScrollFromParentView(
+      gfx::Vector2d(event->x_offset(), event->y_offset()), event->type());
+  event->SetHandled();
+}
+
+void AppListFolderView::OnMouseEvent(ui::MouseEvent* event) {
+  if (event->type() == ui::ET_MOUSEWHEEL) {
+    items_grid_view_->HandleScrollFromParentView(
+        event->AsMouseWheelEvent()->offset(), ui::ET_MOUSEWHEEL);
+    event->SetHandled();
+  }
 }
 
 bool AppListFolderView::IsDragPointOutsideOfFolder(
