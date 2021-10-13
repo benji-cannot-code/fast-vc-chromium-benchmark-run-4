7e4909c73a45dde3c79ebd772ac1d85ec63acec4
Hirokazu Honda <hiroh@chromium.org>
2021-10-13 08:18:17 +0000

    media/gpu/VP9VaapiVED: Call VaapiWrapper::SubmitBuffer directly
    VP9VaapiVideoEncoderDelegate submits VA-API structures through
    EncodeJob's setup execute callbacks. This calls
    VaapiWrapper::SubmitBuffer directly in VP9VaapiVideoEncoderDelegate.
    This works because VaapiVideoEncoderDelegate::Encode() executes
    VaapiWrapper::ExecuteAndDestroyPendingBuffers() immediately after
    PrepareJob().
    
    Bug: b:202337642
    Test: media_unittest
    Test: video_encode_accelerator_tests
    Change-Id: I9331f00e3f5ff3ff988fa1402decf53d0afc18b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213979
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930971}

--- media/gpu/vaapi/vp9_vaapi_video_encoder_delegate_unittest.cc
+++ media/gpu/vaapi/vp9_vaapi_video_encoder_delegate_unittest.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <numeric>
 #include <tuple>
 
+#include <va/va.h>
+
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/cxx17_backports.h"
@@ -257,9 +259,15 @@ MATCHER_P3(MatchFrameParam,
          arg.spatial_layer_id == spatial_layer_id;
 }
 
+MATCHER_P2(MatchVABufferDescriptor, va_buffer_type, va_buffer_size, "") {
+  return arg.type == va_buffer_type && arg.size == va_buffer_size &&
+         arg.data != nullptr;
+}
+
 class MockVaapiWrapper : public VaapiWrapper {
  public:
   MockVaapiWrapper() : VaapiWrapper(kEncodeConstantQuantizationParameter) {}
+  MOCK_METHOD1(SubmitBuffer_Locked, bool(const VABufferDescriptor&));
 
  protected:
   ~MockVaapiWrapper() override = default;
@@ -442,7 +450,16 @@ void VP9VaapiVideoEncoderDelegateTest::
   EXPECT_CALL(*mock_rate_ctrl_, GetLoopfilterLevel())
       .WillOnce(Return(kDefaultLoopFilterLevel));
 
-  // TODO(mcasas): Consider setting expectations on MockVaapiWrapper calls.
+  EXPECT_CALL(*mock_vaapi_wrapper_,
+              SubmitBuffer_Locked(MatchVABufferDescriptor(
+                  VAEncSequenceParameterBufferType,
+                  sizeof(VAEncSequenceParameterBufferVP9))))
+      .WillOnce(Return(true));
+  EXPECT_CALL(*mock_vaapi_wrapper_,
+              SubmitBuffer_Locked(MatchVABufferDescriptor(
+                  VAEncPictureParameterBufferType,
+                  sizeof(VAEncPictureParameterBufferVP9))))
+      .WillOnce(Return(true));
 
   EXPECT_TRUE(encoder_->PrepareEncodeJob(*encode_job.get()));
 
