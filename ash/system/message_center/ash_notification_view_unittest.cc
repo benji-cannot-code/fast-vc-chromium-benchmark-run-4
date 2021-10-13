dd97f2040fa5545bd2d587b591fecfc6e0f6fc31
Andre Le <leandre@chromium.org>
2021-10-13 17:33:53 +0000

    NotificationRevamp: Make all notifications expandable.
    We want every notification in the new UI to be expandable, unless we are
    in inline settings UI. This CL added that change to AshNotificationView
    and made some refactor in NotificationViewBase and NotificationView.
    
    Bug: 1233654
    Change-Id: Iee44de83f996f89af24b3839cb842cf2715f0b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217414
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931122}

--- ash/system/message_center/ash_notification_view_unittest.cc
+++ ash/system/message_center/ash_notification_view_unittest.cc
@@ -51,6 +51,12 @@ std::unique_ptr<Notification> CreateTestNotification(bool has_image = false) {
   return notification;
 }
 
+class DummyEvent : public ui::Event {
+ public:
+  DummyEvent() : Event(ui::ET_UNKNOWN, base::TimeTicks(), 0) {}
+  ~DummyEvent() override = default;
+};
+
 }  // namespace
 
 class AshNotificationViewTest : public AshTestBase, public views::ViewObserver {
@@ -85,6 +91,11 @@ class AshNotificationViewTest : public AshTestBase, public views::ViewObserver {
     task_environment()->RunUntilIdle();
   }
 
+  // Toggle inline settings with a dummy event.
+  void ToggleInlineSettings() {
+    notification_view_->ToggleInlineSettings(DummyEvent());
+  }
+
  protected:
   AshNotificationView* GetFirstGroupedChildNotificationView() {
     if (!notification_view_->grouped_notifications_container_->children()
@@ -108,6 +119,7 @@ class AshNotificationViewTest : public AshTestBase, public views::ViewObserver {
     return notification_view_->header_row();
   }
   views::View* left_content() { return notification_view_->left_content(); }
+  views::View* content_row() { return notification_view_->content_row(); }
   views::View* title_row() { return notification_view_->title_row_; }
   views::Label* title_view() {
     return notification_view_->title_row_->title_view_;
@@ -179,8 +191,6 @@ TEST_F(AshNotificationViewTest, CreateOrUpdateTitle) {
 }
 
 TEST_F(AshNotificationViewTest, UpdatesTimestampOverTime) {
-  EXPECT_FALSE(timestamp_in_collapsed_view()->GetVisible());
-
   auto notification = CreateTestNotification(/*has_image=*/true);
   notification_view()->UpdateWithNotification(*notification);
   notification_view()->SetExpanded(false);
@@ -300,13 +310,24 @@ TEST_F(AshNotificationViewTest, GroupedNotificationExpandState) {
 }
 
 TEST_F(AshNotificationViewTest, ExpandButtonVisibility) {
-  // Expand button should not be shown when it is not expandable.
-  EXPECT_FALSE(expand_button()->GetVisible());
+  // Expand button should be shown in any type of notification and hidden in
+  // inline settings UI.
+  auto notification1 = CreateTestNotification();
+  notification_view()->UpdateWithNotification(*notification1);
+  EXPECT_TRUE(expand_button()->GetVisible());
 
-  // Expand button should be shown when it is expandable (i.e. there is an
-  // image).
-  auto notification = CreateTestNotification(/*has_image=*/true);
-  notification_view()->UpdateWithNotification(*notification);
+  auto notification2 = CreateTestNotification(/*has_image=*/true);
+  notification_view()->UpdateWithNotification(*notification2);
+  EXPECT_TRUE(expand_button()->GetVisible());
+
+  ToggleInlineSettings();
+  // `content_row()` should be hidden, which also means expand button should be
+  // hidden here.
+  EXPECT_FALSE(content_row()->GetVisible());
+
+  // Toggle back.
+  ToggleInlineSettings();
+  EXPECT_TRUE(content_row()->GetVisible());
   EXPECT_TRUE(expand_button()->GetVisible());
 }
 
