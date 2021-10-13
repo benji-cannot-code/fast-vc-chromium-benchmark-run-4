7b05694000d470b17eb1381a244a9f555f0da8a0
Curt Clemens <cclem@google.com>
2021-10-13 20:02:25 +0000

    [SmartLock] Get tap/click events from AuthIconView
    We're currently getting tap events from LoginAuthFactorsView and
    calling OnTapEvent() on the AuthFactorModels. This CL instead collects
    both tap and click events from AuthIconView and routes them to the
    AuthFactorModel. The idea is that once there are multiple
    AuthFactorIcons, then clicking/tapping the respective icons will allow
    interacting with the different AuthFactorModels.
    
    Bug: 1233614
    Change-Id: I6bff543198e579bf754ce52dbcc164b11fd3741d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213513
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931194}

--- ash/login/ui/auth_icon_view.h
+++ ash/login/ui/auth_icon_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_LOGIN_UI_AUTH_ICON_VIEW_H_
 
 #include "ash/login/ui/animated_rounded_image_view.h"
+#include "base/callback.h"
 
 namespace gfx {
 struct VectorIcon;
@@ -27,6 +28,17 @@ class AuthIconView : public AnimatedRoundedImageView {
 
   // Show a static icon.
   void SetIcon(const gfx::VectorIcon& icon);
+
+  void set_on_tap_or_click_callback(base::RepeatingClosure on_tap_or_click) {
+    on_tap_or_click_callback_ = on_tap_or_click;
+  }
+
+  // views::View:
+  void OnGestureEvent(ui::GestureEvent* event) override;
+  bool OnMousePressed(const ui::MouseEvent& event) override;
+
+ private:
+  base::RepeatingClosure on_tap_or_click_callback_;
 };
 
 }  // namespace ash
