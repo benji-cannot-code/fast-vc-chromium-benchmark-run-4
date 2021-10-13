245c5c4619c608ab0fc4740860c520cd78c99d5a
Harkiran Bolaria <hbolaria@google.com>
2021-10-13 21:42:45 +0000

    Mark RenderFrameHost::GetFrameTreeNodeId as const
    Also fix the return type of the const RenderFrameHostImpl::GetParent
    to return const RenderFrameHostImpl*.
    
    Bug: 1179502
    Change-Id: I179430b3546d15622f5dae02a5df2b249476bf3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171056
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931242}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -1864,7 +1864,7 @@ RenderFrameHostImpl* RenderFrameHostImpl::GetParent() {
   return parent_;
 }
 
-RenderFrameHostImpl* RenderFrameHostImpl::GetParent() const {
+const RenderFrameHostImpl* RenderFrameHostImpl::GetParent() const {
   return parent_;
 }
 
@@ -2002,7 +2002,7 @@ void RenderFrameHostImpl::ForEachRenderFrameHostImpl(
   }
 }
 
-int RenderFrameHostImpl::GetFrameTreeNodeId() {
+int RenderFrameHostImpl::GetFrameTreeNodeId() const {
   return frame_tree_node_->frame_tree_node_id();
 }
 
