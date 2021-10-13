1c8fb9285f7973618311d85e33b707d6aee7a232
Michael Checo <michaelcheco@google.com>
2021-10-13 00:33:50 +0000

    Diagnostics: Make gateway test non-blocking
    - This change also moves the ARC Ping test to the Local Network
      group.
    
    Bug: 1197335
    Change-Id: Id5f58b31d5e01dc5c7cdf29cd8c036f6e6d606fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218356
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#930876}

--- ash/webui/diagnostics_ui/resources/diagnostics_utils.js
+++ ash/webui/diagnostics_ui/resources/diagnostics_utils.js
@@ -126,7 +126,7 @@ export function createRoutine(routine, blocking) {
 export function getRoutineGroups(type, isArcEnabled) {
   let localNetworkGroup = new RoutineGroup(
       [
-        createRoutine(RoutineType.kGatewayCanBePinged, true),
+        createRoutine(RoutineType.kGatewayCanBePinged, false),
         createRoutine(RoutineType.kLanConnectivity, true),
       ],
       'localNetworkGroupLabel');
@@ -158,8 +158,7 @@ export function getRoutineGroups(type, isArcEnabled) {
     // Add ARC routines to their corresponding groups.
     nameResolutionGroup.addRoutine(
         (createRoutine(RoutineType.kArcDnsResolution, false)));
-    internetConnectivityGroup.addRoutine(
-        (createRoutine(RoutineType.kArcPing, false)));
+    localNetworkGroup.addRoutine((createRoutine(RoutineType.kArcPing, false)));
     internetConnectivityGroup.addRoutine(
         (createRoutine(RoutineType.kArcHttp, false)));
   }
