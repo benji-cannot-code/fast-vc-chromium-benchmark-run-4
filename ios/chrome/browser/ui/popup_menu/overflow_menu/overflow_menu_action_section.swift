b8049cb425858eb5196bb6ca126c2fd704187db4
Robbie Gibson <rkgibson@google.com>
2021-10-13 14:49:17 +0000

    [iOS] Split overflow menu actions into groups for menu subsections
    Originally, the Actions were represented as one list of actions.
    However, the model should actually be a list of Action sections, each
    one with a list of Actions, as the UI is a grouped list.
    
    This CL introduces a model class and view for the sections because that
    makes responding to changes in the sections or any individual section
    easier in SwiftUI.
    
    Bug: 1254379
    Change-Id: Ie538a1f1911dff5f07242d78ce3fd21d02dc1328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3211474
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#931050}

---
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_action_section.swift
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import SwiftUI
+
+/// A SwiftUI view for the overflow menu displaying a subsection of the actions list.
+struct OverflowMenuActionSection: View {
+  var actionGroup: OverflowMenuActionGroup
+
+  var body: some View {
+    Section {
+      ForEach(actionGroup.actions) { action in
+        OverflowMenuActionRow(action: action)
+      }
+    }
+    .background(Color.white)
+  }
+}
