8544e389856fd3b5f3c92cd67956dcabd505dc24
Akihiro Ota <akihiroota@chromium.org>
2021-10-13 22:43:26 +0000

    Accessibility sprint: Add switch access menu action to leave group
    This behavior is hidden behind the flag:
    enable-experimental-accessibility-switch-access-multistep-automation
    
    AX-Relnotes: N/A
    Bug: 1258921
    Change-Id: I0d9fe5eaa7107b1d41a69b92c523e2eb7209b4ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219852
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931276}

--- third_party/closure_compiler/externs/accessibility_private.js
+++ third_party/closure_compiler/externs/accessibility_private.js
@@ -151,6 +151,7 @@ chrome.accessibilityPrivate.SwitchAccessMenuAction = {
   SCREENSHOT: 'screenshot',
   VOLUME_UP: 'volumeUp',
   VOLUME_DOWN: 'volumeDown',
+  LEAVE_GROUP: 'leaveGroup',
 };
 
 /**
