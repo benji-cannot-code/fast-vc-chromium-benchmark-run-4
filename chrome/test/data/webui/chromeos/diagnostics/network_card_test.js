c05899036848027bb80dca5ba3aca67f3b3eddfe
Ashley Prasad <ashleydp@google.com>
2021-10-13 15:22:34 +0000

    Diagnostics: Update network disconnected string
    - Changes message for disconnected network device to "No {network_type}
      network available".
    - Changes troubeshooting link text to "Try troubleshooting steps".
    - Tests for correct troubleshooting link and header.
    - Updates SHA for related strings.
    
    Strings Diagnostics app link: http://go/cros-diagnostics-strings
    Screenshot: http://shortn/_2vk7PweMPx
    
      --gtest_filter=DiagnosticsApp*'
    
    Bug: 1197335
    Test: testing/xvfb.py out/Default/browser_tests
    Change-Id: I29c24259b129107af7eac95e4d784d3f68326ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219856
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931065}

--- chrome/test/data/webui/chromeos/diagnostics/network_card_test.js
+++ chrome/test/data/webui/chromeos/diagnostics/network_card_test.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://diagnostics/network_card.js';
 
-import {fakeCellularDisabledNetwork, fakeCellularNetwork, fakeCellularWithIpConfigNetwork, fakeConnectingEthernetNetwork, fakeDisconnectedEthernetNetwork, fakeDisconnectedWifiNetwork, fakeEthernetNetwork, fakeNetworkGuidInfoList, fakePortalWifiNetwork, fakeWifiNetwork, fakeWifiNetworkDisabled, fakeWifiNetworkInvalidNameServers, fakeWifiNetworkNoIpAddress} from 'chrome://diagnostics/fake_data.js';
+import {fakeCellularDisabledNetwork, fakeCellularDisconnectedNetwork, fakeCellularNetwork, fakeCellularWithIpConfigNetwork, fakeConnectingEthernetNetwork, fakeDisconnectedEthernetNetwork, fakeDisconnectedWifiNetwork, fakeEthernetNetwork, fakeNetworkGuidInfoList, fakePortalWifiNetwork, fakeWifiNetwork, fakeWifiNetworkDisabled, fakeWifiNetworkInvalidNameServers, fakeWifiNetworkNoIpAddress} from 'chrome://diagnostics/fake_data.js';
 import {FakeNetworkHealthProvider} from 'chrome://diagnostics/fake_network_health_provider.js';
 import {setNetworkHealthProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
@@ -63,6 +63,8 @@ export function networkCardTestSuite() {
         'cellularWithIpConfigGuid', [fakeCellularWithIpConfigNetwork]);
     provider.setFakeNetworkState(
         'cellularDisabledGuid', [fakeCellularDisabledNetwork]);
+    provider.setFakeNetworkState(
+        'cellularDisconnectedGuid', [fakeCellularDisconnectedNetwork]);
     // Add the network info to the DOM.
     networkCardElement = /** @type {!NetworkCardElement} */ (
         document.createElement('network-card'));
@@ -205,12 +207,19 @@ export function networkCardTestSuite() {
   });
 
   test('WifiDisconnectedShowTroubleShooting', () => {
+    const networkType = 'Wi-Fi';
     return initializeNetworkCard('wifiDisconnectedGuid').then(() => {
       dx_utils.assertElementContainsText(
-          networkCardElement.$$('#cardTitle'), 'Wi-Fi');
+          networkCardElement.$$('#cardTitle'), networkType);
       assertTrue(isVisible(getTroubleConnectingElement()));
       assertFalse(isVisible(getNetworkInfoElement()));
       assertFalse(isVisible(getIpConfigDrawerElement()));
+      assertEquals(
+          networkCardElement.i18n('troubleshootingText', networkType),
+          getTroubleshootingHeader());
+      assertEquals(
+          networkCardElement.i18n('troubleConnecting'),
+          getTroubleshootingLinkText());
     });
   });
 
@@ -239,13 +248,20 @@ export function networkCardTestSuite() {
   });
 
   test('EthernetDisconnectedShowTroubleShooting', () => {
+    const networkType = 'Ethernet';
     return initializeNetworkCard('ethernetDisconnectedGuid').then(() => {
       dx_utils.assertElementContainsText(
-          networkCardElement.$$('#cardTitle'), 'Ethernet');
+          networkCardElement.$$('#cardTitle'), networkType);
       assertTrue(isVisible(getNetworkIcon()));
       assertTrue(isVisible(getTroubleConnectingElement()));
       assertFalse(isVisible(getNetworkInfoElement()));
       assertFalse(isVisible(getIpConfigDrawerElement()));
+      assertEquals(
+          networkCardElement.i18n('troubleshootingText', networkType),
+          getTroubleshootingHeader());
+      assertEquals(
+          networkCardElement.i18n('troubleConnecting'),
+          getTroubleshootingLinkText());
     });
   });
 
@@ -347,6 +363,23 @@ export function networkCardTestSuite() {
       assertFalse(isVisible(getIpConfigDrawerElement()));
       assertEquals(
           networkCardElement.i18n('reconnectLinkText'),
+          getTroubleshootingLinkText());
+    });
+  });
+
+  test('CardTitleCellularDisconnectedInitializedCorrectly', () => {
+    const networkType = 'Mobile data';
+    return initializeNetworkCard('cellularDisconnectedGuid').then(() => {
+      dx_utils.assertElementContainsText(
+          networkCardElement.$$('#cardTitle'), networkType);
+      assertTrue(isVisible(getTroubleConnectingElement()));
+      assertFalse(isVisible(getNetworkInfoElement()));
+      assertFalse(isVisible(getIpConfigDrawerElement()));
+      assertEquals(
+          networkCardElement.i18n('troubleshootingText', networkType),
+          getTroubleshootingHeader());
+      assertEquals(
+          networkCardElement.i18n('troubleConnecting'),
           getTroubleshootingLinkText());
     });
   });
