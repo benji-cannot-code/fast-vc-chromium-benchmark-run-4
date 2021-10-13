3ec2bfdf432fee62e7f136a11283032ae728ec54
Wei Lee <wtlee@google.com>
2021-10-13 06:18:18 +0000

    CCA: Cleanup logic for enabling document mode
    Now we enable document mode on all devices if it is supported on
    platform side.
    
    Bug: b:197721293
    Test: Manually
    Change-Id: I83592fa99772a981b4112d7dff9662e6529eeb20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3119386
    Reviewed-by: Inker Kuo <inker@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930954}

--- chromeos/components/camera_app_ui/resources/js/device/camera3_device_info.js
+++ chromeos/components/camera_app_ui/resources/js/device/camera3_device_info.js
@@ -26,11 +26,9 @@ export class Camera3DeviceInfo {
    *     resolutions and maximal capture fps of the video device.
    * @param {!FpsRangeList} fpsRanges Supported fps ranges of the video device.
    * @param {boolean} supportPTZ Is supported PTZ controls.
-   * @param {boolean} supportDocumentScan Is supported document scanning.
    */
   constructor(
-      deviceInfo, facing, photoResols, videoResolFpses, fpsRanges, supportPTZ,
-      supportDocumentScan) {
+      deviceInfo, facing, photoResols, videoResolFpses, fpsRanges, supportPTZ) {
     /**
      * @const {string}
      * @public
@@ -73,12 +71,6 @@ export class Camera3DeviceInfo {
      */
     this.supportPTZ = supportPTZ;
 
-    /**
-     * @const {boolean}
-     * @public
-     */
-    this.supportDocumentScan = supportDocumentScan;
-
     videoResolFpses.filter(({maxFps}) => maxFps >= 24)
         .forEach(({width, height, maxFps}) => {
           const r = new Resolution(width, height);
@@ -113,10 +105,9 @@ export class Camera3DeviceInfo {
     const filteredVideoConfigs = videoConfigs.filter(videoConfigFilter);
     const supportedFpsRanges =
         await deviceOperator.getSupportedFpsRanges(deviceId);
-    const supportDocumentScan = true;
 
     return new Camera3DeviceInfo(
         deviceInfo, facing, photoResolution, filteredVideoConfigs,
-        supportedFpsRanges, supportPTZ, supportDocumentScan);
+        supportedFpsRanges, supportPTZ);
   }
 }
--- chromeos/components/camera_app_ui/resources/js/views/camera/document_corner_overlay.js
+++ chromeos/components/camera_app_ui/resources/js/views/camera/document_corner_overlay.js
@@ -229,13 +229,6 @@ export class DocumentCornerOverlay {
   }
 
   /**
-   * @return {string}
-   */
-  getDeviceId() {
-    return assertString(this.deviceId_);
-  }
-
-  /**
    * Attaches to camera with specified device id.
    * @param {string} deviceId
    */
