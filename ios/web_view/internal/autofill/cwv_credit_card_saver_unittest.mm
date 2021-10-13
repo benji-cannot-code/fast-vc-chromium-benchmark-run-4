a6739dd0cc97c21988027026137e1327a246d28f
Peter Kasting <pkasting@chromium.org>
2021-10-13 15:03:53 +0000

    Fix -Wunusued-but-set-variable: /ios/web_view
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 1203071
    Change-Id: I280ac9775c1ce0bb0b1add27cfc216847b582608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219328
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931060}

--- ios/web_view/internal/autofill/cwv_credit_card_saver_unittest.mm
+++ ios/web_view/internal/autofill/cwv_credit_card_saver_unittest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/bind.h"
+#include "base/compiler_specific.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #import "base/test/ios/wait_util.h"
@@ -90,6 +91,7 @@ TEST_F(CWVCreditCardSaverTest, Ignore) {
                                   savePromptCallback:std::move(callback)];
   // Force -[CWVCreditCardSaver dealloc].
   credit_card_saver = nil;
+  ALLOW_UNUSED_LOCAL(credit_card_saver);
 
   EXPECT_TRUE(callback_called);
 }
