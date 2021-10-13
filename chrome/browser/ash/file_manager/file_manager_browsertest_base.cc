45d112bb518adf6c409297c5ef3f01652d7224b4
François Degros <fdegros@chromium.org>
2021-10-13 03:18:46 +0000

    [Files App] Fix ZIP browser tests
    Fixed and re-enabled the browser tests that used to work with the old
    ZIP mounting and creation systems. Ensured that they work with the new
    systems.
    
    Removed some of the ZIP mounting tests that don't make sense anymore
    with the new ZIP mounting system, since only a "fake mount" can be
    achieved in browser tests.
    
    BUG=chromium:1240426
    TEST=browser_tests --gtest_filter="ZipFiles/*"
    TEST=browser_tests --gtest_filter="*ContextMenuZip*"
    
    Change-Id: I5a09e0299af6ec41c05d795e67e73127f00b7101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218800
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930915}

--- chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
@@ -2224,11 +2224,6 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
     return;
   }
 
-  // TODO(crbug.com/1240426) Remove that.
-  if (name == "zipArchiverLoaded") {
-    return;
-  }
-
   if (name == "getRootPaths") {
     // Obtain the root paths.
     const auto downloads_root =
