44606840a91fa429d7eeecdfcca7bcc39d46232c
Andre Le <leandre@chromium.org>
2021-10-13 01:08:50 +0000

    NotificationRevamp: Add message view in expanded mode.
    Add a new message view in expanded state mentioned in this design here:
    go/cros-notification-newui-detailed-sketch#heading=h.5bfym4t4yovi
    
    Bug: 1233654, 1233658
    Change-Id: I3c119bab3970b927e3ba5f77d1867a3691caa92e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3212914
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930890}

--- ui/message_center/views/notification_view_base.h
+++ ui/message_center/views/notification_view_base.h
@@ -259,7 +259,8 @@ class MESSAGE_CENTER_EXPORT NotificationViewBase
   FRIEND_TEST_ALL_PREFIXES(NotificationViewBaseTest, AppNameWebNotification);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewBaseTest, AppNameWebAppNotification);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewBaseTest, CreateOrUpdateTest);
-  FRIEND_TEST_ALL_PREFIXES(NotificationViewBaseTest, ExpandLongMessage);
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewBaseTest,
+                           ManuallyExpandedOrCollapsed);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewBaseTest, InlineSettings);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewBaseTest,
                            InlineSettingsInkDropAnimation);
@@ -290,6 +291,7 @@ class MESSAGE_CENTER_EXPORT NotificationViewBase
   FRIEND_TEST_ALL_PREFIXES(NotificationViewTest, InlineSettingsNotBlock);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewTest, InlineSettingsBlockAll);
   FRIEND_TEST_ALL_PREFIXES(NotificationViewTest, TestAccentColor);
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewTest, ExpandLongMessage);
 
   friend class NotificationViewBaseTest;
 
