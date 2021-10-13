38a0bfee3af3136039e5aae97c1e206c8f116add
Erik Chen <erikchen@chromium.org>
2021-10-13 22:34:12 +0000

    Lacros chrome apps require lacros to always be running.
    This is a short term solution while a longer term solution is worked out.
    
    Bug: 1225848
    Change-Id: I680f1d4adf983a94b3c1f21fc0a4ddb0606c7590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209149
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931266}

--- chrome/browser/apps/app_service/publishers/standalone_browser_extension_apps.h
+++ chrome/browser/apps/app_service/publishers/standalone_browser_extension_apps.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHERS_STANDALONE_BROWSER_EXTENSION_APPS_H_
 #define CHROME_BROWSER_APPS_APP_SERVICE_PUBLISHERS_STANDALONE_BROWSER_EXTENSION_APPS_H_
 
+#include <memory>
+
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/ash/crosapi/browser_manager.h"
 #include "chromeos/crosapi/mojom/app_service.mojom.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/services/app_service/public/cpp/publisher_base.h"
@@ -51,6 +54,10 @@ class StandaloneBrowserExtensionApps : public KeyedService,
   // to publish chrome apps to the app service in ash-chrome.
   void RegisterChromeAppsCrosapiHost(
       mojo::PendingReceiver<crosapi::mojom::AppPublisher> receiver);
+
+  // Registers the keep alive, as the current implementation relies on the
+  // assumption that Lacros is always running.
+  void RegisterKeepAlive();
 
  private:
   // apps::PublisherBase:
@@ -99,6 +106,8 @@ class StandaloneBrowserExtensionApps : public KeyedService,
 
   // Used to send chrome app publisher actions to Lacros.
   mojo::Remote<crosapi::mojom::AppController> controller_;
+
+  std::unique_ptr<crosapi::BrowserManager::ScopedKeepAlive> keep_alive_;
 
   base::WeakPtrFactory<StandaloneBrowserExtensionApps> weak_factory_{this};
 };
