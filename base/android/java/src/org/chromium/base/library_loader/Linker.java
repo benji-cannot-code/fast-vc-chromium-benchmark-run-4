136315dda41ab24a11ba99dbc4596cb9de261b71
Egor Pasko <pasko@chromium.org>
2021-10-13 19:37:14 +0000

    Reland "android: Use the RELRO from the App Zygote in the browser process"
    This is a reland of a493b8f3f267dc423bb95c966f27e3dbff1ea386
    
    What is new since the original change:
    
    1. Set the classloader on the bundle before attempting to unparcel
       Linker.LibInfo to avoid BadParcelableException
    
    2. Remove the (now unnecessary) call to set the classloader from
       ChildProcessService.class
    
    3. Change the classloader from
       ContextUtils.getApplicationContext().getClassLoader() to the
       classloader of the Linker.class (to reduce what Linker depends on)
    
    I'm not very confident in the robustness of the new classloader choice
    in (3).  At least this does not prevent unloading any classloaders
    (Linker is not going to be unloaded anyway, so its classloader cannot be
    unloaded too).
    
    Original change's description:
    > android: Use the RELRO from the App Zygote in the browser process
    >
    > Our design assumes that the browser process takes the RELRO from the App
    > Zygote, and we made sure no third-party code runs in the Zygote before
    > this message is received. Still somehow forgot to actually do it.
    >
    > The fix is easy. Works locally and invisibly well.
    >
    > Bug: 1154224
    > Change-Id: Iada335612eba143b715fdae7d291af586378c5a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218167
    > Reviewed-by: Bo <boliu@chromium.org>
    > Commit-Queue: Egor Pasko <pasko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#930726}
    
    Bug: 1154224
    Change-Id: I26b120637f1d31e0ce5f7bef81a99c626dff2ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220298
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931186}

--- base/android/java/src/org/chromium/base/library_loader/Linker.java
+++ base/android/java/src/org/chromium/base/library_loader/Linker.java
@@ -661,6 +661,7 @@ abstract class Linker {
         }
 
         public static LibInfo fromBundle(Bundle bundle) {
+            bundle.setClassLoader(Linker.class.getClassLoader());
             return bundle.getParcelable(EXTRA_LINKER_LIB_INFO);
         }
 
--- base/android/java/src/org/chromium/base/process_launcher/ChildProcessService.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildProcessService.java
@@ -221,7 +221,6 @@ public class ChildProcessService {
 
         @Override
         public void consumeRelroBundle(Bundle bundle) {
-            bundle.setClassLoader(ContextUtils.getApplicationContext().getClassLoader());
             mDelegate.consumeRelroBundle(bundle);
         }
     };
