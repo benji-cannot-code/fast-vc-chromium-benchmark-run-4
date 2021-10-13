ae58bf3fda8be11be642a155861dc4e28300d97d
ainozaki <ainozaki@google.com>
2021-10-13 04:41:03 +0000

    Remove Platform::IsExcludedHeaderForServiceWorkerFetchEvent.
    This was originally needed to prevent X-Client-Data from leaking to
    service workers, since it was added as a normal header.
    
    But later |cors_exempt_headers| was introduced, and X-Client-Data is
    added to |cors_exempt_headers|, which do not leak to service workers.
    This is because the converter from ResourceRequest to FetchApiRequest in
    content/common/fetch/fetch_request_type_converters.cc ignores
    |cors_exempt_headers|.
    
    Therefore, this CL:
    - Removes Platform::IsExcludedHeaderForServiceWorkerFetchEvent.
    - Adds a test for the desired behavior, which passes before and
      after this CL.
    
    There is a potential bug, disposing the ThrottlingURLLoader on redirects
    without applying removed_headers_, etc calculated in ThrottlingURLLoader. So fixed it as well.
    
    This CL takes over https://chromium-review.googlesource.com/c/chromium/src/+/2307014
    
    Bug: 963260
    Change-Id: If95dea63a9be67eb1dbf17a894719cc79559c548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189210
    Commit-Queue: Ai Nozaki <ainozaki@google.com>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#930935}

--- third_party/blink/common/loader/throttling_url_loader.cc
+++ third_party/blink/common/loader/throttling_url_loader.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/loader/throttling_url_loader.h"
 
+#include <vector>
+
 #include "base/bind.h"
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_functions.h"
@@ -318,11 +320,13 @@ ThrottlingURLLoader::~ThrottlingURLLoader() {
 }
 
 void ThrottlingURLLoader::FollowRedirectForcingRestart() {
-  ResetForFollowRedirect();
+  url_loader_.ResetWithReason(
+      network::mojom::URLLoader::kClientDisconnectReason,
+      kFollowRedirectReason);
   client_receiver_.reset();
   CHECK(throttle_will_redirect_redirect_url_.is_empty());
 
-  UpdateRequestHeaders();
+  UpdateRequestHeaders(start_info_->url_request);
 
   removed_headers_.clear();
   modified_headers_.Clear();
@@ -331,7 +335,17 @@ void ThrottlingURLLoader::FollowRedirectForcingRestart() {
   StartNow();
 }
 
-void ThrottlingURLLoader::ResetForFollowRedirect() {
+void ThrottlingURLLoader::ResetForFollowRedirect(
+    network::ResourceRequest& resource_request,
+    const std::vector<std::string>& removed_headers,
+    const net::HttpRequestHeaders& modified_headers,
+    const net::HttpRequestHeaders& modified_cors_exempt_headers) {
+  MergeRemovedHeaders(&removed_headers_, removed_headers);
+  modified_headers_.MergeFrom(modified_headers);
+  modified_cors_exempt_headers_.MergeFrom(modified_cors_exempt_headers);
+  // Call UpdateRequestHeaders() after headers are merged.
+  UpdateRequestHeaders(resource_request);
+
   url_loader_.ResetWithReason(
       network::mojom::URLLoader::kClientDisconnectReason,
       kFollowRedirectReason);
@@ -360,7 +374,7 @@ void ThrottlingURLLoader::FollowRedirect(
   if (!throttle_will_start_redirect_url_.is_empty()) {
     throttle_will_start_redirect_url_ = GURL();
     // This is a synthesized redirect, so no need to tell the URLLoader.
-    UpdateRequestHeaders();
+    UpdateRequestHeaders(start_info_->url_request);
     StartNow();
     return;
   }
@@ -965,14 +979,14 @@ void ThrottlingURLLoader::UpdateDeferredRequestHeaders(
   }
 }
 
-void ThrottlingURLLoader::UpdateRequestHeaders() {
+void ThrottlingURLLoader::UpdateRequestHeaders(
+    network::ResourceRequest& resource_request) {
   for (const std::string& header : removed_headers_) {
-    start_info_->url_request.headers.RemoveHeader(header);
-    start_info_->url_request.cors_exempt_headers.RemoveHeader(header);
+    resource_request.headers.RemoveHeader(header);
+    resource_request.cors_exempt_headers.RemoveHeader(header);
   }
-  start_info_->url_request.headers.MergeFrom(modified_headers_);
-  start_info_->url_request.cors_exempt_headers.MergeFrom(
-      modified_cors_exempt_headers_);
+  resource_request.headers.MergeFrom(modified_headers_);
+  resource_request.cors_exempt_headers.MergeFrom(modified_cors_exempt_headers_);
 }
 
 void ThrottlingURLLoader::UpdateDeferredResponseHead(
