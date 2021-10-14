1b7b7f65b234eca68a3c1f514f4c0e120cf22017
dpapad <dpapad@chromium.org>
2021-10-14 16:31:57 +0000

    WebUI: Stop using extraLibraries in CrElementsActionMenuTest.All test.
     - Migrate CrElementsActionMenuTest from a "chrome://test?module="
       URL to "chrome://test/test_loader.html?module="
     - Fix test logic relating to document.body.clientHeight/clientWidth [1]
       in cr_action_menu_test.js that only worked in "Quirks Mode" [2], as
       the test is now loaded in "Standards Mode" due to the presence of
       <!DOCTYPE html> in test_loader.html.
    
    [1] https://developer.mozilla.org/en-US/docs/Web/API/Element/clientHeight
    [2] https://developer.mozilla.org/en-US/docs/Web/HTML/Quirks_Mode_and_Standards_Mode
    
    Bug: 1158471
    Change-Id: If0bb165ffe92bb901dda9fc80cc3e8a685812b15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222680
    Auto-Submit: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931540}

--- chrome/test/data/webui/cr_elements/cr_action_menu_test.js
+++ chrome/test/data/webui/cr_elements/cr_action_menu_test.js
@@ -577,8 +577,10 @@ suite('CrActionMenu', function() {
     // overridden so that no scrolling happens.
     test('onscreen forces anchor change', function() {
       const rect = dots.getBoundingClientRect();
-      document.body.scrollLeft = rect.right - document.body.clientWidth + 10;
-      document.body.scrollTop = rect.bottom - document.body.clientHeight + 10;
+      document.documentElement.scrollLeft =
+          rect.right - document.documentElement.clientWidth + 10;
+      document.documentElement.scrollTop =
+          rect.bottom - document.documentElement.clientHeight + 10;
 
       menu.showAt(dots, {anchorAlignmentX: AnchorAlignment.AFTER_START});
       const buttonWidth = dots.offsetWidth;
@@ -591,8 +593,8 @@ suite('CrActionMenu', function() {
     });
 
     test('scroll position maintained for showAtPosition', function() {
-      document.body.scrollLeft = 500;
-      document.body.scrollTop = 1000;
+      document.documentElement.scrollLeft = 500;
+      document.documentElement.scrollTop = 1000;
       menu.showAtPosition({top: 50, left: 50});
       assertEquals(550, dialog.offsetLeft);
       assertEquals(1050, dialog.offsetTop);
