06b5ae94439530c458f11e0dc42b7046d49b0a07
llin <llin@chromium.org>
2021-10-14 00:58:34 +0000

    Couple minor Projector updates.
    - Use "projector" as Projector metadata file extension. It is used to
    identify Projector screencasts at processing pending screencasts and
    fetching screencast list. Product branding is in progress. We might
    need to update the extension name once finalize.
    - Update a few tracking bugs to the ones in buganizer.
    - Add TODO to track error handling.
    
    Bug: b/200960422
    Change-Id: Ib166d2930112ab07157856ddb539e55b2ff93389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221451
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toby Huang <tobyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931327}

--- ash/projector/projector_metadata_controller.cc
+++ ash/projector/projector_metadata_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/projector/projector_metadata_controller.h"
 
 #include "ash/projector/projector_controller_impl.h"
+#include "ash/public/cpp/projector/projector_controller.h"
 #include "base/bind.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -66,8 +67,9 @@ void ProjectorMetadataController::RecordKeyIdea() {
 void ProjectorMetadataController::SaveMetadata(
     const base::FilePath& video_file_path) {
   DCHECK(metadata_);
-  // TODO(crbug.com/1165439): Finalize on the metadata file naming convention.
-  const base::FilePath path = video_file_path.ReplaceExtension(".txt");
+  // TODO(b/200330118): Finalize on the metadata file naming convention.
+  const base::FilePath path =
+      video_file_path.AddExtension(kProjectorMetadataFileExtension);
 
   metadata_->SetName(
       video_file_path.RemoveExtension().BaseName().AsUTF8Unsafe());
@@ -75,17 +77,16 @@ void ProjectorMetadataController::SaveMetadata(
   // Save metadata.
   auto metadata_str = metadata_->Serialize();
 
-  // TODO(crbug.com/1165439): Update after finalizing on the storage strategy.
+  // TODO(b/203000496): Update after finalizing on the storage strategy.
   blocking_task_runner_->PostTaskAndReplyWithResult(
       FROM_HERE, base::BindOnce(&SaveFile, metadata_str, path),
       base::BindOnce(
           [](const base::FilePath& path, bool success) {
             if (!success) {
               LOG(ERROR) << "Failed to save the metadata file: " << path;
+              // TODO(b/200846160): notify user with the failure.
               return;
             }
-
-            // TODO(crbug.com/1165439): Make screencast metadata indexable.
           },
           path));
 }
