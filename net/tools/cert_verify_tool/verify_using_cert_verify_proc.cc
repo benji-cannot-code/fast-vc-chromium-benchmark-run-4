1557a9f8a9ee2dd531bf70da8eb68fd520076af1
Hubert Chao <hchao@chromium.org>
2021-10-14 21:12:30 +0000

    Add the beginnings of cert verify comparison tool that compares results from platform and builtin cert verifier in bulk.
    Tool is based on the cert_verify_tool code, with many features stripped
    out, and the older version of this tool that was never checked in
    (https://crrev.com/c/1028991).
    
    Goal for this CL is to be able to read the dump file and to output
    results manually. Future CLs will make stats more aggregated and
    introduce better comparisons similar to what is done in the trial
    cert verifier.
    
    
    Bug: b/200835768
    Change-Id: Id19b8203941bf17ff3339105f18cd2a81ddd9c28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200451
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931696}

--- net/tools/cert_verify_tool/verify_using_cert_verify_proc.cc
+++ net/tools/cert_verify_tool/verify_using_cert_verify_proc.cc
@@ -79,6 +79,8 @@ void PrintCertStatus(int cert_status) {
   }
 }
 
+}  // namespace
+
 void PrintCertVerifyResult(const net::CertVerifyResult& result) {
   PrintDebugData(&result);
   PrintCertStatus(result.cert_status);
@@ -109,8 +111,6 @@ void PrintCertVerifyResult(const net::CertVerifyResult& result) {
     }
   }
 }
-
-}  // namespace
 
 bool VerifyUsingCertVerifyProc(
     net::CertVerifyProc* cert_verify_proc,
