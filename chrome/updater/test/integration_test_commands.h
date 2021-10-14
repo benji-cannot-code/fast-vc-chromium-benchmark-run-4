feed6ab640e3877aaad626d2d97c46896676da32
Sorin Jianu <sorin@chromium.org>
2021-10-14 15:16:37 +0000

    Tests that update checks and pings include "ismachine".
    Fixed: 1255143
    Change-Id: I74c44648969c0fc2261d75f3df4a2689c0fd501f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222019
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931504}

--- chrome/updater/test/integration_test_commands.h
+++ chrome/updater/test/integration_test_commands.h
@@ -14,8 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 
+namespace base {
+class Version;
+}
+
 namespace updater {
 namespace test {
+
+class ScopedServer;
 
 class IntegrationTestCommands
     : public base::RefCountedThreadSafe<IntegrationTestCommands> {
@@ -30,6 +36,10 @@ class IntegrationTestCommands
   virtual void ExpectActiveUpdater() const = 0;
   virtual void ExpectActive(const std::string& app_id) const = 0;
   virtual void ExpectNotActive(const std::string& app_id) const = 0;
+  virtual void ExpectUpdateSequence(ScopedServer* test_server,
+                                    const std::string& app_id,
+                                    const base::Version& from_version,
+                                    const base::Version& to_version) const = 0;
   virtual void ExpectVersionActive(const std::string& version) const = 0;
   virtual void ExpectVersionNotActive(const std::string& version) const = 0;
   virtual void Uninstall() const = 0;
