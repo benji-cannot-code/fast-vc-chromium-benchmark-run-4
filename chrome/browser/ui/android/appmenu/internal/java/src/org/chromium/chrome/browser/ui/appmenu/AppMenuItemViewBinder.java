1cbfe2a1424592e95cad1796b12f105d40d80d63
Gang Wu <gangwu@chromium.org>
2021-10-14 04:09:28 +0000

    [AppMenu] Support tint the menu item icon to blue
    Bug:1259490
    
    Change-Id: I3a8762e4fc0e71479fece66bbc4d7a68327a5092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219955
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931368}

--- chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemViewBinder.java
+++ chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemViewBinder.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.ColorRes;
 import androidx.appcompat.content.res.AppCompatResources;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -61,6 +62,16 @@ class AppMenuItemViewBinder {
             ChromeImageView imageView = (ChromeImageView) view.findViewById(R.id.menu_item_icon);
             imageView.setImageDrawable(icon);
             imageView.setVisibility(icon == null ? View.GONE : View.VISIBLE);
+
+            // tint the icon
+            @ColorRes
+            int colorResId = model.get(AppMenuItemProperties.ICON_COLOR_RES);
+            if (colorResId == 0) {
+                // If there is no color assigned to the icon, use the default color.
+                colorResId = R.color.default_icon_color_secondary_tint_list;
+            }
+            ApiCompatibilityUtils.setImageTintList(imageView,
+                    AppCompatResources.getColorStateList(imageView.getContext(), colorResId));
         } else if (key == AppMenuItemProperties.CLICK_HANDLER) {
             view.setOnClickListener(
                     v -> model.get(AppMenuItemProperties.CLICK_HANDLER).onItemClick(model));
