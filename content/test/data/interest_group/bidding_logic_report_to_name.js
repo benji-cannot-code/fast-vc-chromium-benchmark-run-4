9094914027d80c2dcd1cefb53806b32301d80f8c
Matt Menke <mmenke@chromium.org>
2021-10-14 16:36:33 +0000

    FLEDGE:  Add private network protections to reports.
    This copies the ClientSecurityState from the RenderFrameHost running an
    auction to the report fetch, so it inherits the same protections as if
    it were made from the frame.
    
    Bug: 1249868
    Change-Id: I0d71bd9cbafa98a5b2af934116c081ef3d9068c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173431
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931544}

---
+++ content/test/data/interest_group/bidding_logic_report_to_name.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function generateBid(
+    interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals,
+    browserSignals) {
+  const ad = interestGroup.ads[0];
+  return {'ad': ad, 'bid': 1, 'render': ad.renderUrl};
+}
+
+function reportWin(
+  auctionSignals, perBuyerSignals, sellerSignals, browserSignals) {
+
+  // Smuggle the report-to destination in the InterestGroup name, so caller can
+  // control the destination.
+  sendReportTo(browserSignals.interestGroupName);
+}
