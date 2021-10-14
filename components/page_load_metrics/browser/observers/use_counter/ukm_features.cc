04d2685433373f47c94063e2ebd075dc7ddbf9b6
Ari Chivukula <arichiv@chromium.org>
2021-10-14 18:59:48 +0000

    Track actual usage of WebSQL
    The previous counter, `kOpenWebDatabase`, would increment whenever the
    database was opened. The new counter, `kReadOrWriteWebDatabase`, will
    increment whenever the database is actually read from or written to.
    The goal is to get a more accurate usage count.
    Inspired by: https://chromium-review.googlesource.com/c/chromium/src/+/3108519
    
    Bug: 695592
    Change-Id: I2ab144ee1aaab72b33e70190530d46344f444f54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221123
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931645}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -217,6 +217,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kBlobStoreAccessAcrossAgentClustersInResolveForNavigation,
           WebFeature::kReadOrWriteWebDatabaseThirdPartyContext,
           WebFeature::kSearchEventFired,
+          WebFeature::kReadOrWriteWebDatabase,
       }));
   return *opt_in_features;
 }
