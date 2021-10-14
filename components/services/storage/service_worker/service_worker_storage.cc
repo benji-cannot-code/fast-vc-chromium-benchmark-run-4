c5f0ac46ac6d533766aca9907861afea4f874f42
Steven Bingler <bingler@chromium.org>
2021-10-14 19:52:57 +0000

    Correctly pass through StorageKey in WriteRegistrationInDB
    The StorageKey was accidentally being recreated which dropped top-level
    site information from the key. This CL corrects that.
    
    Bug: 1246549
    Change-Id: Ic2cae1f22982b3a77d8a28e1f0ae158f730513c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217419
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931664}

--- components/services/storage/service_worker/service_worker_storage.cc
+++ components/services/storage/service_worker/service_worker_storage.cc
@@ -1655,9 +1655,7 @@ void ServiceWorkerStorage::WriteRegistrationInDB(
   ServiceWorkerDatabase::Status status =
       database->WriteRegistration(*registration, resources, &deleted_version);
   original_task_runner->PostTask(
-      FROM_HERE, base::BindOnce(std::move(callback),
-                                blink::StorageKey(
-                                    url::Origin::Create(registration->script)),
+      FROM_HERE, base::BindOnce(std::move(callback), registration->key,
                                 deleted_version, status));
 }
 
