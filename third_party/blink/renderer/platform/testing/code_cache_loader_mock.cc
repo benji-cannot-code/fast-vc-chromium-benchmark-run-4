103da9b09996cb42e1b2fec722683b423a76cc3b
Leszek Swirski <leszeks@chromium.org>
2021-10-14 15:26:09 +0000

    Reland^2 "[code cache] Use context specific interface for clearing cached code"
    This is a reland of 28cadc9006238d6ef2b4b6d48aadcd71f163e7d8
    Reland fixes:
      * Rebase on top of https://crrev.com/c/3211984, which is a reland of
        https://crrev.com/c/3162165 described below.
    
    Original change's description:
    > Reland "[code cache] Use context specific interface for clearing cached code"
    >
    > This is a reland of commit 0314d69e5c35606f91bd69085a90612f464201ca.
    >
    > Reland fixes:
    >  * Rebase on top of https://crrev.com/c/3162165 which introduces a
    >    wrapper CodeCacheHost to allow CodeCacheLoader to store WeakRef to
    >    the mojo::Remote<CodeCacheHost>, so that it knows when the Remote is
    >    reset (from e.g disconnecting).
    >
    > Original change's description:
    > > [code cache] Use context specific interface for clearing cached code
    > >
    > > Code caches are migrated to use NetworkIsolationKey and to use context
    > > specific (frame / worker) interface to perform operations on code
    > > caches. This CL migrates clearing of the cached code to use context
    > > specific interface. This extends CodeCacheLoader to also add support
    > > for clearing code cache entries and uses this to clear cached code.
    > >
    > > Bug: chromium:1213818
    > > Change-Id: I091a65c01faeae098e2d4d61ee1c91578a302d89
    > > Reviewed-on:
    > https://chromium-review.googlesource.com/c/chromium/src/+/3122177
    > > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Commit-Queue: Mythri Alle <mythria@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#916751}
    >
    > Bug: chromium:1213818, chromium:1245526
    > Change-Id: Ic5100b50104a27613d3cc22ab3574ac2ee4c188e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3152025
    > Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#928643}
    
    Bug: chromium:1213818, chromium:1245526
    Change-Id: I40223d50f8596a5f670c7c190cb0290317197e84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222674
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931508}

--- third_party/blink/renderer/platform/testing/code_cache_loader_mock.cc
+++ third_party/blink/renderer/platform/testing/code_cache_loader_mock.cc
@@ -21,6 +21,10 @@ void CodeCacheLoaderMock::FetchFromCodeCache(
   }
 }
 
+void CodeCacheLoaderMock::ClearCodeCacheEntry(
+    blink::mojom::CodeCacheType cache_type,
+    const WebURL& url) {}
+
 void CodeCacheLoaderMock::Controller::DelayResponse() {
   delayed_ = true;
 }
