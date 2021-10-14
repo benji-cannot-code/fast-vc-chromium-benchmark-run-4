20814a98dac9d20efc3eca6576dd2ccc4d361d7a
Saifuddin Hitawala <hitawala@chromium.org>
2021-10-14 15:53:17 +0000

    Modify device_ check while creating X11 buffer
    Fox X11, device cannot be created for tests on CrOS-on-linux bots. This
    is probably due to missing support for dri3 api on some X11. This CL
    fixes crash by adding a check and returning gracefully if device is not
    supported.
    
    Bug: 1249766
    Change-Id: I22236804ea95e3ae6a5a249ad060a1981d4bdf02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205293
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931524}

--- ui/gfx/linux/gpu_memory_buffer_support_x11.cc
+++ ui/gfx/linux/gpu_memory_buffer_support_x11.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/debug/crash_logging.h"
+#include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/buffer_types.h"
@@ -33,12 +34,16 @@ namespace {
 std::unique_ptr<ui::GbmDevice> CreateX11GbmDevice() {
   auto* connection = x11::Connection::Get();
   // |connection| may be nullptr in headless mode.
-  if (!connection)
+  if (!connection) {
+    LOG(ERROR) << "Could not create x11 connection.";
     return nullptr;
+  }
 
   auto& dri3 = connection->dri3();
-  if (!dri3.present())
+  if (!dri3.present()) {
+    LOG(ERROR) << "dri3 extension not supported.";
     return nullptr;
+  }
 
   // Let the X11 server know the DRI3 client version. This is required to use
   // the DRI3 extension. We don't care about the returned server version because
@@ -119,9 +124,17 @@ std::unique_ptr<GbmBuffer> GpuMemoryBufferSupportX11::CreateBuffer(
     gfx::BufferFormat format,
     const gfx::Size& size,
     gfx::BufferUsage usage) {
-  DCHECK(device_);
-  DCHECK(base::Contains(supported_configs_,
-                        gfx::BufferUsageAndFormat(usage, format)));
+  if (!device_) {
+    LOG(ERROR) << "Can't create buffer -- gbm  device is missing.";
+    return nullptr;
+  }
+  if (!base::Contains(supported_configs_,
+                      gfx::BufferUsageAndFormat(usage, format))) {
+    LOG(ERROR) << "Can't create buffer -- unsupported config: usage="
+               << gfx::BufferUsageToString(usage)
+               << ", format=" << gfx::BufferFormatToString(format);
+    return nullptr;
+  }
 
   static base::debug::CrashKeyString* crash_key_string =
       base::debug::AllocateCrashKeyString("buffer_usage_and_format",
