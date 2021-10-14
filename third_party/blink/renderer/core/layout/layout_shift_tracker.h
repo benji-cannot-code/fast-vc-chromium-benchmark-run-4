cddfe3d3258817181d6d9094cb398830118cce6c
Lan Wei <lanwei@chromium.org>
2021-10-14 19:50:15 +0000

    Set hadRecentInput for history navigation initiated from browser
    We have several ways to start history back/forward navigation, one is
    clicking the left or right arrow on the toolbar, the other way is to
    use the History API's forward or back function. We have an attribute
    "is_browser_initiated" in CommitNavigationParams, which indicates
    if the history navigation is initiated from browser side, like clicking
    the navigation buttons on the toolbar.
    
    There is attribute "has_user_gesture" in CommonNavigationParams, which
    seems a more appropriate way to indicate if there is any user
    interaction, but right now it is not set correctly when there is a user
    activation. Also, the value we use to set "has_user_gesture" is from
    HasTransientUserActivation in FrameTreeNode, which is not set to be true
    when there is a user activation to the browser toolbar, only to the web
    content.
    
    We need this flag to set hadRecentInput for the navigations that come
    from user interactions with the browser, so that we do not count the
    layout shift score for the same-document-history navigation started
    from the browser.
    
    Bug: 1178747
    Change-Id: I2bb4df62376b4ffd46ba4cfebd0e2e7933f73707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021664
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931663}

--- third_party/blink/renderer/core/layout/layout_shift_tracker.h
+++ third_party/blink/renderer/core/layout/layout_shift_tracker.h
@@ -81,6 +81,7 @@ class CORE_EXPORT LayoutShiftTracker final
   void NotifyFindInPageInput();
   void NotifyChangeEvent();
   void NotifyZoomLevelChanged();
+  void NotifyBrowserInitiatedSameDocumentNavigation();
   bool IsActive() const { return is_active_; }
   double Score() const { return score_; }
   double WeightedScore() const { return weighted_score_; }
