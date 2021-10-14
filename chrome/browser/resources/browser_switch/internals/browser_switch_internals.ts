e060ff61aabdaee3400beeb3d9138f7b8c124178
Sneha Nagpaul <snehanagpaul@google.com>
2021-10-14 21:40:07 +0000

    Changed checking for http:// to ://
    This change makes other protocols possible to check besides http://.
    Part of a change to make the interface usable on
    chrome://browser-switch/internals
    
    Bug: 1258133
    Change-Id: I55a13957a3d70eacbc9144b069b44e33991ecc33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3223560
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Sneha Nagpaul <snehanagpaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#931711}

--- chrome/browser/resources/browser_switch/internals/browser_switch_internals.ts
+++ chrome/browser/resources/browser_switch/internals/browser_switch_internals.ts
@@ -109,7 +109,7 @@ function checkUrl() {
     $('output').innerText = '';
     return;
   }
-  if (!/^http[s]?:\/\//g.test(url)) {
+  if (!url.includes('://')) {
     url = 'http://' + url;
   }
   sendWithPromise('getDecision', url)
