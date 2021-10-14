139b036994fac392550bd50e1d258b8fdc1fc7e2
Martin Kreichgauer <martinkr@google.com>
2021-10-14 23:21:15 +0000

    u2f: unbreak canMakeU2fApiRequest invocations during Chrome OS sign-in
    canMakeU2fApiRequest has non-optional args for the tabId and frameId
    making the original U2F API request. They are used to determine origin
    trial enrollment status and show the deprecation prompt. Cryptotoken's
    JS gets these values from the MessageSender object. But they can be
    undefined if the request does not come from a tab.
    
    This is the case if U2F API requests are made on the Chrome OS login
    screen, as some SAML SSO providers do. In these cases, the
    canMakeU2fApiRequest fails outright due to invalid arguments, and
    the entire U2F request can't succeed as a result.
    
    To address this, make the corresponding WebRequestSender fields
    non-optional, filling dummy values if the original MessageSender values
    are undefined. (This is a behavior change that affects things other
    than canMakeU2fApiRequestmore, but I convinced myself that this is safe.)
    
    On the browser side, if a request comes from a Chrome OS sign-in
    context, immediately resolve canMakeU2fApiRequest with true. (This was
    the intended effect of CL:3209886; but that CL by itself was not enough
    to unbreak Chrome OS SAML sign-in due to the undefined parameters.)
    
    Bug: 1257293,1224886
    Change-Id: Ib3d148d6d8aff66f117661102db48a8181b54931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222182
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931751}

--- chrome/browser/resources/cryptotoken/webrequestsender.js
+++ chrome/browser/resources/cryptotoken/webrequestsender.js
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @typedef {{
  *   origin: string,
- *   tabId: (number|undefined)
- *   frameId: (number|undefined)
+ *   tabId: number,
+ *   frameId: number,
  * }}
  */
 var WebRequestSender;
@@ -34,6 +34,13 @@ function createSenderFromMessageSender(messageSender) {
   if (messageSender.tab) {
     sender.tabId = messageSender.tab.id;
     sender.frameId = messageSender.frameId;
+  } else {
+    // The MessageSender tab and frameId properties are unset if the
+    // `chrome.tabs` API is unavailable because the request was not sent from a
+    // tab. This is the case for requests made on the Chrome OS sign-in screen
+    // during SAML SSO device login.
+    sender.tabId = chrome.tabs.TAB_ID_NONE;
+    sender.frameId = -1;
   }
   return sender;
 }
