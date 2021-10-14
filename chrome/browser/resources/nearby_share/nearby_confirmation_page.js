c2605950ef9dcbc75c97ee4f03094d02a59978cd
Crisrael Lucero <crisrael@google.com>
2021-10-14 23:39:40 +0000

    [Nearby] Nearby Share discovery and send dark mode
    This fix addresses dark mode compatibility with nearby share's discovery
    and send confirmation dialog pages when sending files. This addresses
    general formatting such as page backgrounds, text colors, icon colors,
    etc. This also addresses lottie animations (pulse and progress bar).
    
    Before Pictures
    Discovery page:
    https://screenshot.googleplex.com/5QaRzwHWEAteG7t
    Discovery page with device selected:
    https://screenshot.googleplex.com/7Y67GbdSUSgEEpK
    Send confirmation page:
    https://screenshot.googleplex.com/3TJaRUFM8npFpFY
    Send confirmation page with error:
    https://screenshot.googleplex.com/BCzzwxyFvb2Pi3n
    
    Light After Pictures
    Discovery page searching:
    https://screenshot.googleplex.com/KmFYcUp3pQdL9nq
    Discovery page device found:
    https://screenshot.googleplex.com/5BYc6NoCfAveLiF
    Discovery page with device selected:
    https://screenshot.googleplex.com/75k8qVusjLtHzf3
    Send confirmation page:
    https://screenshot.googleplex.com/5ujUXtjDa63WLsi
    Send confirmation page with error:
    https://screenshot.googleplex.com/6jdLjH9J5eBMKMk
    
    Dark After Pictures
    Discovery page searching:
    https://screenshot.googleplex.com/6Kw9quVdo5WqqvX
    Discovery page device found:
    https://screenshot.googleplex.com/9z6cpmSkm4ZRGQs
    Discovery page with device selected:
    https://screenshot.googleplex.com/5zJkgvfp7tNKamR
    Send confirmation page:
    https://screenshot.googleplex.com/8yD8MFLkSrhmWXv
    Send confirmation page with error:
    https://screenshot.googleplex.com/8ZhHNHjRYb4YyUZ
    
    Fixed: 1254907
    Change-Id: I37fb6576fa8fe0009c64d0712adab04ea30521c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3199704
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931759}

--- chrome/browser/resources/nearby_share/nearby_confirmation_page.js
+++ chrome/browser/resources/nearby_share/nearby_confirmation_page.js
@@ -55,6 +55,18 @@ class TransferUpdateListener {
   }
 }
 
+/**
+ * The progress bar asset URL for light mode
+ * @type {string}
+ */
+const PROGRESS_BAR_URL_LIGHT = 'nearby_share_progress_bar_light.json';
+
+/**
+ * The progress bar asset URL for dark mode
+ * @type {string}
+ */
+const PROGRESS_BAR_URL_DARK = 'nearby_share_progress_bar_dark.json';
+
 Polymer({
   is: 'nearby-confirmation-page',
 
@@ -152,6 +164,15 @@ Polymer({
     lastTransferStatus_: {
       type: nearbyShare.mojom.TransferStatus,
       value: null,
+    },
+
+    /**
+     * Whether the confirmation page is being rendered in dark mode.
+     * @private {boolean}
+     */
+    isDarkModeActive_: {
+      type: Boolean,
+      value: false,
     },
   },
 
@@ -323,4 +344,13 @@ Polymer({
         this.payloadPreview.description :
         'Unknown file';
   },
+
+  /**
+   * Returns the URL for the asset that defines a file transfer's animated
+   * progress bar.
+   */
+  getAnimationUrl_() {
+    return this.isDarkModeActive_ ? PROGRESS_BAR_URL_DARK :
+                                    PROGRESS_BAR_URL_LIGHT;
+  }
 });
