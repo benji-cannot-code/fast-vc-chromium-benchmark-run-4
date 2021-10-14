9c3dc3db8540226ad7201d6c2990e014f9994f21
Kevin Babbitt <kbabbitt@microsoft.com>
2021-10-14 16:01:36 +0000

    Remove extra StringImpl AddRef from StringResourceBase
    When initialized from an AtomicString, StringResourceBase would take two
    references to the underlying StringImpl: one in plain_string_ and one in
    atomic_string_. This CL adjusts usage of the two member variables so
    that a StringResourceBase initialized from an AtomicString only needs to
    hold a single reference. To ensure correct usage, the member variables
    are made private and new protected accessor is added for derived classes
    to use.
    
    While I was in the area, I also wrapped the parkable_string_ member
    variable in a protected accessor so it could be made private.
    
    Bug: 1083392
    Change-Id: I35cff32d3c49f7eea6e1a8f044ae01f6ed89ae3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221808
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931529}

--- third_party/blink/renderer/bindings/modules/v8/v8_element_test.cc
+++ third_party/blink/renderer/bindings/modules/v8/v8_element_test.cc
@@ -44,7 +44,7 @@ TEST_F(V8ElementTest, SetAttributeOperationCallback) {
 
 #if DCHECK_IS_ON()
   AtomicString test_attribute("test-attribute");
-  EXPECT_EQ(test_attribute.Impl()->RefCountChangeCountForTesting(), 17u);
+  EXPECT_EQ(test_attribute.Impl()->RefCountChangeCountForTesting(), 16u);
   AtomicString test_value("test-value");
   EXPECT_EQ(test_value.Impl()->RefCountChangeCountForTesting(), 11u);
 #endif
@@ -65,7 +65,7 @@ TEST_F(V8ElementTest, GetAttributeOperationCallback_NonExisting) {
 
 #if DCHECK_IS_ON()
   AtomicString test_attribute("test-attribute");
-  EXPECT_EQ(test_attribute.Impl()->RefCountChangeCountForTesting(), 10u);
+  EXPECT_EQ(test_attribute.Impl()->RefCountChangeCountForTesting(), 9u);
 #endif
 
   // Trigger a low memory notification. This will signal V8 to clear its
