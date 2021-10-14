77aa5ec6476a4634a125a435bc997d7ba1752a18
John Williams <jrw@chromium.org>
2021-10-14 17:57:02 +0000

    Revert "Reland "[PartitionAlloc] Limit waste due to empty slot spans."""
    This reverts commit eeed9fc7618a94fb27af92a6b8a0d7c91656ed08.
    
    Reason for revert: This appears to cause a test failure: https://ci.chromium.org/ui/p/chromium/builders/ci/mac11-arm64-rel-tests/831/test-results
    
    Original change's description:
    > Reland "[PartitionAlloc] Limit waste due to empty slot spans.""
    >
    > This reverts commit ba37fa78d603a9b3cd5507c19b71ba8e44684878.
    >
    > Reason for reland: fixed the test on ARM64 macOS.
    >
    > Original change's description:
    > > [PartitionAlloc] Limit waste due to empty slot spans.
    > >
    > > When a slot span becomes empty, rather than decommitting it right away,
    > > it is added to a per-root global empty slot span ring buffer. This helps
    > > avoid syscalls when memory is allocated / deallocated in a loop. For
    > > instance, any single-slot slot span allocation (>=64kiB with 4k pages)
    > > will create an empty slot span at free() time, and we can avoid many
    > > system calls this way.
    > >
    > > However, this may end up wasting committed (dirty) memory. To avoid
    > > that, cap the relative waste. This was already partially done by
    > > MemoryReclaimer, but it may never run, or be arbitrarily delayed.
    > >
    > > This will allow to increase the size of the ring without increasing peak
    > > committed memory too much.
    > >
    > > Bug: 1250288
    > > Change-Id: I4cdd521b191cc3a30cf71a5ada570e5ac47ce9c9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217737
    > > Commit-Queue: Benoit L <lizeb@chromium.org>
    > > Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#931133}
    >
    > Bug: 1250288
    > Change-Id: Id0a8065bb7c41b84599d08f9d14b6ffb83ca3cfc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222583
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    > Commit-Queue: Benoit L <lizeb@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#931493}
    
    Bug: 1250288
    Change-Id: I0d2b31b1e3b7ac8dd4dd1315614879f4344f071a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3223136
    Auto-Submit: John Williams <jrw@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: John Williams <jrw@chromium.org>
    Owners-Override: John Williams <jrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931593}

--- base/allocator/partition_allocator/starscan/pcscan_unittest.cc
+++ base/allocator/partition_allocator/starscan/pcscan_unittest.cc
@@ -61,9 +61,6 @@ class PartitionAllocPCScanTest : public testing::Test {
                      PartitionOptions::Cookie::kDisallowed,
                      PartitionOptions::BackupRefPtr::kDisabled,
                      PartitionOptions::UseConfigurablePool::kNo});
-
-    allocator_.root()->UncapEmptySlotSpanMemoryForTesting();
-
     PCScan::RegisterScannableRoot(allocator_.root());
   }
   ~PartitionAllocPCScanTest() override {
@@ -460,7 +457,6 @@ TEST_F(PartitionAllocPCScanTest, DanglingInterPartitionReference) {
        PartitionOptions::Cookie::kAllowed,
        PartitionOptions::BackupRefPtr::kDisabled,
        PartitionOptions::UseConfigurablePool::kNo});
-  source_root.UncapEmptySlotSpanMemoryForTesting();
   ThreadSafePartitionRoot value_root(
       {PartitionOptions::AlignedAlloc::kDisallowed,
        PartitionOptions::ThreadCache::kDisabled,
@@ -468,7 +464,6 @@ TEST_F(PartitionAllocPCScanTest, DanglingInterPartitionReference) {
        PartitionOptions::Cookie::kAllowed,
        PartitionOptions::BackupRefPtr::kDisabled,
        PartitionOptions::UseConfigurablePool::kNo});
-  value_root.UncapEmptySlotSpanMemoryForTesting();
 
   PCScan::RegisterScannableRoot(&source_root);
   PCScan::RegisterScannableRoot(&value_root);
@@ -491,7 +486,6 @@ TEST_F(PartitionAllocPCScanTest, DanglingReferenceToNonScannablePartition) {
        PartitionOptions::Cookie::kAllowed,
        PartitionOptions::BackupRefPtr::kDisabled,
        PartitionOptions::UseConfigurablePool::kNo});
-  source_root.UncapEmptySlotSpanMemoryForTesting();
   ThreadSafePartitionRoot value_root(
       {PartitionOptions::AlignedAlloc::kDisallowed,
        PartitionOptions::ThreadCache::kDisabled,
@@ -499,7 +493,6 @@ TEST_F(PartitionAllocPCScanTest, DanglingReferenceToNonScannablePartition) {
        PartitionOptions::Cookie::kAllowed,
        PartitionOptions::BackupRefPtr::kDisabled,
        PartitionOptions::UseConfigurablePool::kNo});
-  value_root.UncapEmptySlotSpanMemoryForTesting();
 
   PCScan::RegisterScannableRoot(&source_root);
   PCScan::RegisterNonScannableRoot(&value_root);
@@ -522,7 +515,6 @@ TEST_F(PartitionAllocPCScanTest, DanglingReferenceFromNonScannablePartition) {
        PartitionOptions::Cookie::kAllowed,
        PartitionOptions::BackupRefPtr::kDisabled,
        PartitionOptions::UseConfigurablePool::kNo});
-  source_root.UncapEmptySlotSpanMemoryForTesting();
   ThreadSafePartitionRoot value_root(
       {PartitionOptions::AlignedAlloc::kDisallowed,
        PartitionOptions::ThreadCache::kDisabled,
@@ -530,7 +522,6 @@ TEST_F(PartitionAllocPCScanTest, DanglingReferenceFromNonScannablePartition) {
        PartitionOptions::Cookie::kAllowed,
        PartitionOptions::BackupRefPtr::kDisabled,
        PartitionOptions::UseConfigurablePool::kNo});
-  value_root.UncapEmptySlotSpanMemoryForTesting();
 
   PCScan::RegisterNonScannableRoot(&source_root);
   PCScan::RegisterScannableRoot(&value_root);
