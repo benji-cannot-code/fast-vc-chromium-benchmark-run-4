97f8411e5afb556bdf939f8060a3418183bd8f64
Amanda Deacon <amandadeacon@chromium.org>
2021-10-15 05:02:08 +0000

    [Launcher] Add metric for read status errors in score normalizer.
    Bug: 1254607
    Test: logged to histogram from an arbitrary place and observed in
    chrome://histograms
    
    Change-Id: I8b163f459c4ff1e7b7e237f6b0db6a07d5592e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220839
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931872}

--- chrome/browser/ui/app_list/search/ranking/persistent_proto.h
+++ chrome/browser/ui/app_list/search/ranking/persistent_proto.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/sequence_checker.h"
 #include "base/task/post_task.h"
 #include "base/task/sequenced_task_runner_forward.h"
@@ -31,6 +32,7 @@ enum class ReadStatus {
   kMissing = 1,
   kReadError = 2,
   kParseError = 3,
+  kMaxValue = kParseError,
 };
 
 // The result of writing a backing file to disk. These values persist to logs.
@@ -39,6 +41,7 @@ enum class WriteStatus {
   kOk = 0,
   kWriteError = 1,
   kSerializationError = 2,
+  kMaxValue = kSerializationError,
 };
 
 namespace {
@@ -210,6 +213,8 @@ class PersistentProto {
 
  private:
   void OnReadComplete(std::pair<ReadStatus, std::unique_ptr<T>> result) {
+    base::UmaHistogramEnumeration("Apps.AppList.PersistentProto.ReadStatus",
+                                  result.first);
     if (result.first == ReadStatus::kOk) {
       proto_ = std::move(result.second);
     } else {
@@ -228,7 +233,11 @@ class PersistentProto {
     std::move(on_read_).Run(result.first);
   }
 
-  void OnWriteComplete(const WriteStatus status) { on_write_.Run(status); }
+  void OnWriteComplete(const WriteStatus status) {
+    base::UmaHistogramEnumeration("Apps.AppList.PersistentProto.WriteStatus",
+                                  status);
+    on_write_.Run(status);
+  }
 
   void OnQueueWrite() {
     // Reset the queued flag before posting the task. Last-moment updates to
