e48574399ef41b8de86c65602ea1c9696d807a44
Wen-Chien Wang <wcwang@chromium.org>
2021-10-15 17:08:05 +0000

    Home button should stay for at least 2 minutes to show launcher nudge.
    Consider the case when the time has passed long enough for the nudge to
    be shown and the user just logged in. The nudge was implemented to be
    shown right after the home button shows up, which is not a wanted
    behavior.
    With this cl, the launcher nudge is designed to ensure that it is shown
    after the home button has been shown for enough time (currently 2 min,
    can be adjusted). Currently two specific cases that the home button
    shows up are considered:
    (1) The user logs in
    (2) The user changes from tablet mode to clamshell mode.
    A unittest is added explicitly to test this logic.
    
    Bug: 1226093
    Change-Id: I2048a7c56e6232887b4c737d3d48b6da7af0bb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224221
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932078}

--- ash/shelf/launcher_nudge_controller.h
+++ ash/shelf/launcher_nudge_controller.h
@@ -12,12 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/tablet_mode_observer.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/scoped_observation.h"
+#include "base/time/time.h"
 
 namespace base {
 class Clock;
 class TickClock;
-class Time;
-class TimeDelta;
 class WallClockTimer;
 }  // namespace base
 
@@ -36,6 +35,13 @@ class ASH_EXPORT LauncherNudgeController : public SessionObserver,
  public:
   // Maximum number of times that the nudge will be shown to the users.
   static constexpr int kMaxShownCount = 3;
+
+  // To prevent showing the nudge to users right after the home button appears
+  // (e.g. at the moment when the user logs in or change to the clamshell mode),
+  // a minimum time delta is used here to guarantee the nudge is shown after a
+  // certain amount of time since the home button shows up.
+  static constexpr base::TimeDelta kMinIntervalAfterHomeButtonAppears =
+      base::Minutes(2);
 
   explicit LauncherNudgeController();
   LauncherNudgeController(const LauncherNudgeController&) = delete;
@@ -96,6 +102,10 @@ class ASH_EXPORT LauncherNudgeController : public SessionObserver,
 
   // The timer that keeps track of when should the nudge be shown next time.
   std::unique_ptr<base::WallClockTimer> show_nudge_timer_;
+
+  // The earliest available time for the nudge to be shown next time. Could be
+  // null or earlier than GetNow().
+  base::Time earliest_available_time_;
 
   ScopedSessionObserver session_observer_{this};
   base::ScopedObservation<TabletModeController, TabletModeObserver>
