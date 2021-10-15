279b1546c52a3f3f132806f6ec28f2b4db248d8d
Reilly Grant <reillyg@chromium.org>
2021-10-15 02:47:36 +0000

    serial: Detect disconnection on Linux
    Linux seems to use EIO instead of ENXIO as macOS does to indicate
    that a write() has failed because the device has disconnected.
    
    Change-Id: Ia4175257b654eef48549123a6aa85fd2fa5c07f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224668
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931835}

--- services/device/serial/serial_io_handler_posix.cc
+++ services/device/serial/serial_io_handler_posix.cc
@@ -386,7 +386,7 @@ void SerialIoHandlerPosix::OnFileCanWriteWithoutBlocking() {
                                            pending_write_buffer().data(),
                                            pending_write_buffer().size()));
     if (bytes_written < 0) {
-      if (errno == ENXIO) {
+      if (errno == EIO || errno == ENXIO) {
         WriteCompleted(0, mojom::SerialSendError::DISCONNECTED);
         StopWatchingFileWrite();
       } else {
