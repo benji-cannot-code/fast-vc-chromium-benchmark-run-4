ebe2be85c9287d01ffb933d083dfad5054e6798c
Mitch McDermott <mcdermottm@google.com>
2021-10-15 02:51:25 +0000

    Add a flag for chrome://media-app to handle audio.
    This is a re-land of http://crrev/c/3086046 with changes to the
    underlying implementation to use the existing MediaApp rather than
    create a clone.
    
    Enables multi-window mode (and so absorbs the flag) to support the use
    case of opening audio files alongside other images/videos.
    
    Flag is initially disabled, so the SWA is not presented to users.
    
    This CL adds the app manifest with file handlers, and skips install of
    the old Chrome app to prevent it being added as a file handler.
    
    Extends the mock app to handle audio files; which also changes its
    implementation away from the "file is video, otherwise it is an image".
    A side effect of this is needing to implement error states-- so some
    incorrectly mocked tests had to be updated.
    
    Bug: b/201351958, b/202360041, b/197184322
    Change-Id: Ie415c388c5f7d0223e634ce1cb4dd39d0e0d9347
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3189576
    Reviewed-by: dstockwell <dstockwell@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Mitchell McDermott <mcdermottm@google.com>
    Cr-Commit-Position: refs/heads/main@{#931838}

--- chrome/browser/ash/web_applications/media_app/media_web_app_info.cc
+++ chrome/browser/ash/web_applications/media_app/media_web_app_info.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ash/web_applications/media_app/media_web_app_info.h"
+#include "base/containers/span.h"
 
 #include <memory>
 #include <string>
@@ -22,9 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+using FileHandlerConfig = std::pair<const char*, const char*>;
+
 // FileHandler configuration.
 // See https://github.com/WICG/file-handling/blob/main/explainer.md.
-constexpr std::pair<const char*, const char*> kFileHandlers[] = {
+constexpr FileHandlerConfig kFileHandlers[] = {
     {"image/*", ""},
     {"video/*", ""},
 
@@ -60,13 +63,31 @@ constexpr std::pair<const char*, const char*> kFileHandlers[] = {
     {"application/pdf", ".pdf"},
 };
 
-// Converts the kFileHandlers constexpr into the type needed to populate the
+constexpr FileHandlerConfig kAudioFileHandlers[] = {
+    {"audio/*", ""},
+
+    // More audio formats.
+    {"audio/flac", ".flac"},
+    {"audio/m4a", ".m4a"},
+    {"audio/mpeg", ".mp3"},
+    {"audio/ogg", ".oga,.ogg,.opus"},
+    {"audio/wav", ".wav"},
+    {"audio/webm", "weba"},
+
+    // Note: some extensions appear twice. See mime_util.cc.
+    {"audio/mp3", "mp3"},
+    {"audio/x-m4a", "m4a"},
+};
+
+// Converts a FileHandlerConfig constexpr into the type needed to populate the
 // WebApplicationInfo's `accept` property.
-std::vector<apps::FileHandler::AcceptEntry> MakeHandlerAccept() {
+std::vector<apps::FileHandler::AcceptEntry> MakeFileHandlerAccept(
+    base::span<const FileHandlerConfig> config) {
   std::vector<apps::FileHandler::AcceptEntry> result;
+  result.reserve(config.size());
 
   const std::string separator = ",";
-  for (const auto& handler : kFileHandlers) {
+  for (const auto& handler : config) {
     result.emplace_back();
     result.back().mime_type = handler.first;
     auto file_extensions = base::SplitString(
@@ -114,10 +135,19 @@ std::unique_ptr<WebApplicationInfo> CreateWebAppInfoForMediaWebApp() {
   info->display_mode = blink::mojom::DisplayMode::kStandalone;
   info->user_display_mode = blink::mojom::DisplayMode::kStandalone;
 
-  apps::FileHandler file_handler;
-  file_handler.action = GURL(ash::kChromeUIMediaAppURL);
-  file_handler.accept = MakeHandlerAccept();
-  info->file_handlers.push_back(std::move(file_handler));
+  // Add handlers for image+video and audio. We keep them separate since their
+  // UX are sufficiently different (we don't want audio files to have a carousel
+  // since this would be a second layer of navigation in conjunction with the
+  // play queue). Order matters here; the Files app will prefer earlier
+  // handlers.
+  apps::FileHandler image_video_handler;
+  image_video_handler.action = GURL(ash::kChromeUIMediaAppURL);
+  image_video_handler.accept = MakeFileHandlerAccept(kFileHandlers);
+  info->file_handlers.push_back(std::move(image_video_handler));
+  apps::FileHandler audio_handler;
+  audio_handler.action = GURL(ash::kChromeUIMediaAppURL);
+  audio_handler.accept = MakeFileHandlerAccept(kAudioFileHandlers);
+  info->file_handlers.push_back(std::move(audio_handler));
   return info;
 }
 
@@ -139,7 +169,8 @@ bool MediaSystemAppDelegate::ShouldShowInSearch() const {
 }
 
 bool MediaSystemAppDelegate::ShouldShowNewWindowMenuOption() const {
-  return base::FeatureList::IsEnabled(chromeos::features::kMediaAppMultiWindow);
+  return base::FeatureList::IsEnabled(
+      chromeos::features::kMediaAppHandlesAudio);
 }
 
 bool MediaSystemAppDelegate::ShouldBeSingleWindow() const {
