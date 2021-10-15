12cccb507ff1c2ef008323886f13f984a178a74b
John Z Wu <jzw@chromium.org>
2021-10-15 06:21:54 +0000

    Mark iOS Credential Provider Extension APIs available
    Bug: 1253663
    Change-Id: I80ed2411e8015b224027d0dbe3b187dea1ae8587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3223979
    Auto-Submit: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931899}

--- ios/web_view/public/cwv_defines.h
+++ ios/web_view/public/cwv_defines.h
@@ -35,4 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // based flows.
 #define IOS_WEB_VIEW_SUPPORTS_NEW_CREDIT_CARD_SAVE_APIS 1
 
+// Supports APIs used to implement the iOS credential provider extension.
+#define IOS_WEB_VIEW_SUPPORTS_CREDENTIAL_EXTENSION_PROVIDER_APIS 1
+
 #endif  // IOS_WEB_VIEW_PUBLIC_CWV_DEFINES_H_
