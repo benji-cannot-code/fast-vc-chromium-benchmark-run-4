16846cc97c84d5a2ce2205760aa88e93a2141e40
Alexander Brusher <abrusher@google.com>
2021-10-15 22:07:25 +0000

    [fuchsia][a11y] Add AccessibilityBridgeFuchsia interface.
    This change introduces the interface for the
    BrowserAccessibility*Fuchisa -> accessibility bridge surface.
    
    AX-Relnotes: n/a
    Test: n/a
    Bug: fuchsia:82791
    Change-Id: I6fc91ac570e07ff25bec2003b8cba1d117174b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221266
    Commit-Queue: Alexander Brusher <abrusher@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932227}

---
+++ ui/accessibility/platform/fuchsia/fuchsia_types.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/accessibility/platform/fuchsia/fuchsia_types.h"
+
+namespace ui {
+
+NodeID::NodeID() = default;
+NodeID::~NodeID() = default;
+
+NodeUpdate::NodeUpdate() = default;
+NodeUpdate::~NodeUpdate() = default;
+
+}  // namespace ui
