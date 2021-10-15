8bdf461df383cb60a657bc98ce9bbb23f544fcc1
Greg Guterman <guterman@google.com>
2021-10-15 21:14:36 +0000

    Fix style issues in command_line related CL
    The last CL, https://crrev.com/c/3111330, needed some followup, mostly
    style related fixes, which this CL should cover.
    
    Bug: 1241896
    Change-Id: I1f306076729815ad9c94e0701ac7ad768f35d4cb
    R: thestig@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219618
    Commit-Queue: Gregory Guterman <guterman@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Gregory Guterman <guterman@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932208}

--- base/command_line.h
+++ base/command_line.h
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-class FilePath;
 class DuplicateSwitchHandler;
+class FilePath;
 
 class BASE_EXPORT CommandLine {
  public:
--- base/test/launcher/unit_test_launcher.h
+++ base/test/launcher/unit_test_launcher.h
@@ -173,10 +173,11 @@ class UnitTestLauncherDelegate : public TestLauncherDelegate {
 // duplicate
 class MergeTestFilterSwitchHandler : public DuplicateSwitchHandler {
  public:
+  ~MergeTestFilterSwitchHandler() override;
+
   void ResolveDuplicate(base::StringPiece key,
                         CommandLine::StringPieceType new_value,
                         CommandLine::StringType& out_value) override;
-  ~MergeTestFilterSwitchHandler() override;
 };
 
 }   // namespace base
