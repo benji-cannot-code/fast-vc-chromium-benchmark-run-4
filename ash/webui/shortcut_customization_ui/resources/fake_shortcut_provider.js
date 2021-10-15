2c9af1cdc47187b7e164e3925038b3f7091b4a59
Jimmy Gong <jimmyxgong@google.com>
2021-10-15 23:28:31 +0000

    shortcuts: Add RestoreAllDefaults and RestoreActionDefault to FakeShortcutProvider
    Bug: 1060690
    Test: --gtest_filter=ShortcutCustomizationAppBrowserTest.\
    MANUAL_FakeShortcut*
    
    Change-Id: I2fa009e79fd8be2bbd6a575bd82dcda52c18b19d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198974
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932249}

--- ash/webui/shortcut_customization_ui/resources/fake_shortcut_provider.js
+++ ash/webui/shortcut_customization_ui/resources/fake_shortcut_provider.js
@@ -24,6 +24,8 @@ export class FakeShortcutProvider {
     this.methods_.register('addUserAccelerator');
     this.methods_.register('replaceAccelerator');
     this.methods_.register('removeAccelerator');
+    this.methods_.register('restoreAllDefaults');
+    this.methods_.register('restoreActionDefaults');
   }
 
   /**
@@ -84,6 +86,24 @@ export class FakeShortcutProvider {
     this.methods_.setResult(
         'removeAccelerator', AcceleratorConfigResult.kSuccess);
     return this.methods_.resolveMethod('removeAccelerator');
+  }
+
+  restoreAllDefaults() {
+    // Always return kSuccess in this fake.
+    this.methods_.setResult(
+        'restoreAllDefaults', AcceleratorConfigResult.kSuccess);
+    return this.methods_.resolveMethod('restoreAllDefaults');
+  }
+
+  /**
+   * @param {!AcceleratorSource} source
+   * @param {number} action
+   */
+  restoreActionDefaults(source, action) {
+    // Always return kSuccess in this fake.
+    this.methods_.setResult(
+        'restoreActionDefaults', AcceleratorConfigResult.kSuccess);
+    return this.methods_.resolveMethod('restoreActionDefaults');
   }
 
   /**
