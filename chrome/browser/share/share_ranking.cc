5ee5cf7dd10616b0844161001ae33d12add90a2f
Elly Fong-Jones <ellyjones@chromium.org>
2021-10-15 17:56:42 +0000

    [smartsorting] don't block UI thread for default locale
    Surprisingly, l10n_util::GetApplicationLocale() blocks the calling
    thread: it checks whether string packs are present for every
    candidate locale, which requires synchronously hitting disk. This
    is obviously not a good idea on the UI thread, especially on Android.
    
    Fortunately, we do not care whether string packs are present - we're
    using the locale as a key to pick which ranking to use. Just use the
    system locale, which is available without blocking.
    
    This fixes a DCHECK in smartsorting.
    
    Fixed: 1260175
    Change-Id: I224770deab2bf19de542767bd8318e0d1d3d3b28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225785
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sophey <sophey@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Sophey <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932108}

--- chrome/browser/share/share_ranking.cc
+++ chrome/browser/share/share_ranking.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/callback_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
+#include "base/android/locale_utils.h"
 #include "chrome/browser/profiles/profile_android.h"
 
 #include "chrome/browser/share/jni_headers/ShareRankingBridge_jni.h"
@@ -498,7 +499,16 @@ void ShareRanking::OnRankGetOldRankingDone(
 
 ShareRanking::Ranking ShareRanking::GetDefaultInitialRankingForType(
     const std::string& type) {
+#if defined(OS_ANDROID)
+  // On Android, just use the app's default locale string - we don't have a pref
+  // locale to consult regardless, and l10n_util::GetApplicationLocale can do
+  // blocking disk IO (!) while it checks whether we have a string pack for the
+  // various eligible locales. We don't care about strings here, so just go with
+  // what the system is set to, and don't block on the UI thread.
+  std::string locale = base::android::GetDefaultLocaleString();
+#else
   std::string locale = l10n_util::GetApplicationLocale("", false);
+#endif
   return initial_ranking_for_test_.value_or(
       DefaultRankingForLocaleAndType(locale, type));
 }
