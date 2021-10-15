8d060878ee222f8f4857789e4ced24d9d8a99a83
Fergal Daly <fergal@chromium.org>
2021-10-15 15:40:50 +0000

    Record EventPageShowPersisted from RenderFrameHostManager::CommitPending.
    This is what leads to the browser-side of the IPC that triggers
    persisted pageshow. https://crrev.com/c/3205142 added it at a different
    point in the state machine.
    
    Bug: 1234634
    Change-Id: Ie7af190aa35d8aa9e4788aff1a709fa276bab0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222540
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932039}

--- content/browser/renderer_host/render_frame_host_manager.cc
+++ content/browser/renderer_host/render_frame_host_manager.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/chrome_debug_urls.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/public/common/frame/event_page_show_persisted.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/mojom/frame/frame_owner_properties.mojom.h"
 #include "third_party/blink/public/mojom/frame/fullscreen.mojom.h"
@@ -3327,6 +3328,9 @@ void RenderFrameHostManager::CommitPending(
         std::move(pending_stored_page->render_view_hosts);
     if (prev_state ==
         RenderFrameHostImpl::LifecycleStateImpl::kInBackForwardCache) {
+      blink::RecordUMAEventPageShowPersisted(
+          blink::EventPageShowPersisted::
+              kYesInBrowser_RenderFrameHostManager_CommitPending);
       for (RenderViewHostImpl* rvh : render_view_hosts_to_restore) {
         rvh->LeaveBackForwardCache(
             pending_stored_page->page_restore_params.Clone());
