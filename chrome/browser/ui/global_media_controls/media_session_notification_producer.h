41fcf19760262fb930e1a5070241e78e2ca3a9bd
Tommy Steimel <steimel@chromium.org>
2021-10-15 18:14:59 +0000

    GMC: Remove dep on Presentation from MediaSessionNotificationProducer
    This CL removes the dependency on StartPresentationContext from
    MediaSessionNotificationProducer since it will be moved to a
    component that can't depend on media_router or //chrome.
    
    Bug: 1203842
    Change-Id: I87e753712ad6d7002da09139575b84fef870b297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3205963
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932116}

--- chrome/browser/ui/global_media_controls/media_session_notification_producer.h
+++ chrome/browser/ui/global_media_controls/media_session_notification_producer.h
@@ -28,11 +28,6 @@ class MediaItemManager;
 class MediaItemUI;
 }  // namespace global_media_controls
 
-namespace media_router {
-class CastDialogController;
-class StartPresentationContext;
-}  // namespace media_router
-
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
 enum class GlobalMediaControlsDismissReason {
@@ -87,10 +82,10 @@ class MediaSessionNotificationProducer
   void OnMediaItemUIDismissed(const std::string& id) override;
 
   bool HasSession(const std::string& id) const;
-  std::unique_ptr<media_router::CastDialogController>
-  CreateCastDialogControllerForSession(const std::string& id);
+
   bool HasActiveControllableSessionForWebContents(
       content::WebContents* web_contents) const;
+
   // Returns the notification id of the session associated with |web_contents|.
   // There is at most one session per WebContents.
   std::string GetActiveControllableSessionForWebContents(
@@ -106,9 +101,6 @@ class MediaSessionNotificationProducer
   RegisterIsAudioOutputDeviceSwitchingSupportedCallback(
       const std::string& id,
       base::RepeatingCallback<void(bool)> callback) override;
-
-  void OnStartPresentationContextCreated(
-      std::unique_ptr<media_router::StartPresentationContext> context);
 
   void set_device_provider_for_testing(
       std::unique_ptr<MediaNotificationDeviceProvider> device_provider);
@@ -276,8 +268,6 @@ class MediaSessionNotificationProducer
 
   std::unique_ptr<MediaNotificationDeviceProvider> device_provider_;
 
-  // Used to initialize a MediaRouterUI.
-  std::unique_ptr<media_router::StartPresentationContext> context_;
   base::WeakPtrFactory<MediaSessionNotificationProducer> weak_ptr_factory_{
       this};
 };
