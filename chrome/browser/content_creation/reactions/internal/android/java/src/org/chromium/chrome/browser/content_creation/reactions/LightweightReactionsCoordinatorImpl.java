d4162ed5cd49e7b1d5da7a307af506b4646e4ac2
Guillaume Jenkins <gujen@google.com>
2021-10-15 00:10:44 +0000

    [LR] Create LR Mediator and basic image fetching
    Creates the LightweightReactionMediator class that is responsible for
    fetching assets of the model via the component cached image fetcher.
    
    The mediator is owned by the LR coordinator. The coordinator
    temporarily uses the mediator to fetch the first image asset returned
    by the ReactionService.
    
    Bug: 1259235
    Change-Id: Ifa572cff34793d292a0f5768ffa4d7c181afad2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221833
    Auto-Submit: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Sophey <sophey@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931777}

--- chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsCoordinatorImpl.java
+++ chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsCoordinatorImpl.java
@@ -13,12 +13,16 @@ import androidx.fragment.app.FragmentActivity;
 import org.chromium.chrome.browser.content_creation.reactions.scene.SceneCoordinator;
 import org.chromium.chrome.browser.content_creation.reactions.toolbar.ToolbarControlsDelegate;
 import org.chromium.chrome.browser.content_creation.reactions.toolbar.ToolbarCoordinator;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.share.BaseScreenshotCoordinator;
 import org.chromium.chrome.browser.share.share_sheet.ChromeOptionShareCallback;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.content_creation.reactions.ReactionMetadata;
 import org.chromium.components.content_creation.reactions.ReactionService;
+import org.chromium.components.image_fetcher.ImageFetcher;
+import org.chromium.components.image_fetcher.ImageFetcherConfig;
+import org.chromium.components.image_fetcher.ImageFetcherFactory;
 
 import java.util.List;
 
@@ -28,6 +32,7 @@ import java.util.List;
 public class LightweightReactionsCoordinatorImpl extends BaseScreenshotCoordinator
         implements LightweightReactionsCoordinator, ToolbarControlsDelegate {
     private final ReactionService mReactionService;
+    private final LightweightReactionsMediator mMediator;
     private final LightweightReactionsDialog mDialog;
     private final SceneCoordinator mSceneCoordinator;
 
@@ -52,7 +57,16 @@ public class LightweightReactionsCoordinatorImpl extends BaseScreenshotCoordinat
         mReactionService = reactionService;
         mDialog = new LightweightReactionsDialog();
         mSceneCoordinator = new SceneCoordinator(activity);
-        mReactionService.getReactions((reactions) -> { mAvailableReactions = reactions; });
+
+        Profile profile = Profile.fromWebContents(tab.getWebContents());
+        ImageFetcher imageFetcher = ImageFetcherFactory.createImageFetcher(
+                ImageFetcherConfig.DISK_CACHE_ONLY, profile.getProfileKey());
+        mMediator = new LightweightReactionsMediator(imageFetcher);
+        mReactionService.getReactions((reactions) -> {
+            mAvailableReactions = reactions;
+            mMediator.getBitmapForUrl(
+                    reactions.get(0).thumbnailUrl, (bitmap) -> { assert bitmap != null; });
+        });
     }
 
     /**
