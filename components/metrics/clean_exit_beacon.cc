0e9012bd9ca06e85cebde44059585af78bda305d
Caitlin Fischer <caitlinfischer@google.com>
2021-10-15 16:02:06 +0000

    Revert the timing of the Local State write to what it was previously.
    I suspect that part of a recent change (crrev.com/c/3210328) is causing
    one test to fail and another to be flaky.
    
    BEFORE that change landed, clients in the SignalAndWriteViaFileUtil
    group updated beacon in Local State (in memory) the first time
    WriteBeaconValue() was called. The next time it was called, which is the
    status quo point at which Chrome monitors for crashes, a write was
    scheduled.
    
    AFTER that change, SignalAndWriteViaFileUtil clients now do the
    following the first time WriteBeaconValue() is called:
    (a) Do not update the beacon in Local State since this can be done the
    second time WriteBeaconValue() is called and
    (b) Schedule a Local State write (since WriteBeaconValue() happens to
    update kStabilityBrowserLastLiveTimeStamp).
    The second time WriteBeaconValue() is called, which is the status quo
    point at which Chrome monitors for crashes, the beacon is updated in
    Local State and a Local Statewrite is scheduled.
    
    This change has Chrome go back scheduling a Local State write the second
    time WriteBeaconValue() is called for SignalAndWriteViaFileUtil clients.
    
    Additionally, it schedules a Local State write in a test class,
    ShellContentBrowserClient, and re-enables the browser test.
    
    Bug: 1259550, 1259996, 1260246
    Change-Id: I01bb44049b6e1b04733213a403405942a3ee241b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3223901
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#932044}

--- components/metrics/clean_exit_beacon.cc
+++ components/metrics/clean_exit_beacon.cc
@@ -308,6 +308,7 @@ void CleanExitBeacon::WriteBeaconValue(bool exited_cleanly,
     WriteBeaconFile(exited_cleanly);
   } else {
     local_state_->SetBoolean(prefs::kStabilityExitedCleanly, exited_cleanly);
+    local_state_->CommitPendingWrite();  // Schedule a write.
     if (group_name == kSignalAndWriteViaFileUtilGroup) {
       // Clients in this group write to the Variations Safe Mode file whenever
       // |kStabilityExitedCleanly| is updated. The file is kept in sync with the
@@ -315,7 +316,6 @@ void CleanExitBeacon::WriteBeaconValue(bool exited_cleanly,
       WriteBeaconFile(exited_cleanly);
     }
   }
-  local_state_->CommitPendingWrite();  // Schedule a write.
 
 #if defined(OS_WIN)
   base::win::RegKey regkey;
