af74e54da55a3808afac80f9815e5030804a56fd
Alvin Lee <alvinlee@google.com>
2021-10-15 15:05:53 +0000

    Move DLP Policy Constants, Histogram Helper & Policy Event to /chromeos.
    Part of a larger change to move the DLP stack into a shared location
    for use in both ash and lacros chrome. This change moves the code
    relating to DLP constants, histogram helper and policy event proto
    from the ash specific directory into a new chromeos/policy/dlp
    directory. Subsequent CLs will modify these DLP files to be compiled
    and work with lacros.
    
    Bug: 1254326
    Change-Id: Ia26a607fd9927afe4679f080f8f5b69f733e4cfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218053
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932019}

--- chrome/browser/chromeos/policy/dlp/dlp_reporting_manager_test_helper.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_reporting_manager_test_helper.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "chrome/browser/ash/policy/dlp/dlp_policy_event.pb.h"
+#include "chrome/browser/chromeos/policy/dlp/dlp_policy_event.pb.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_reporting_manager.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
 #include "components/reporting/client/mock_report_queue.h"
