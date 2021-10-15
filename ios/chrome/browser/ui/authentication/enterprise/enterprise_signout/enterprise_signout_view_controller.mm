64b4aa8cc2bfbefa20330a438c881f7c800bfea4
Ewann <ewannpv@google.com>
2021-10-15 16:38:17 +0000

    [iOS][Enterprise] Updates restrictAccounts alert
    This CL updates strings and fixes the bottom padding.
    
    screenshot: https://drive.google.com/file/d/1Ef3bT-U97R0bkXMwOJqKAWOvf7DsnSiQ/view?usp=sharing&resourcekey=0-t_8kX6w_anlBZCyBlDlgrQ
    
    Fixed: 1260359
    Change-Id: I8c8eaae2f65454e4630ef00a716a6a889d28042f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226331
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932060}

--- ios/chrome/browser/ui/authentication/enterprise/enterprise_signout/enterprise_signout_view_controller.mm
+++ ios/chrome/browser/ui/authentication/enterprise/enterprise_signout/enterprise_signout_view_controller.mm
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.customSpacingAfterImage = 30;
 
   self.primaryActionAvailable = YES;
-  self.secondaryActionAvailable = YES;
+  self.secondaryActionAvailable = NO;
   self.showDismissBarButton = NO;
   self.titleString = l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT);
   self.subtitleString = l10n_util::GetNSString(
