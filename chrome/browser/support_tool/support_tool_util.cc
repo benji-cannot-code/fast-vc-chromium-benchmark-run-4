36566261c80f0eda29a5157ffc8a6d883402369e
Irem Uguz <iremuguz@google.com>
2021-10-15 07:55:21 +0000

    support-tool: Create DataCollector interface
    Create DataCollector interface and one example implementation class,
    UiHierarchyDataCollector. Create SupportToolHandler class which will
    contain the list of DataCollectors and will run the functions of
    DataCollectors. Add support_tool_util.cc, which will contain the
    function to create SupportToolHandler and add required DataCollectors to
    it, based on the data collection module requested by the user. For now,
    it only creates a SupportToolHandler for Chrome OS and adds one data
    collector in it. The functions are empty for now, and will be filled in
    next CL. This CL only declared the function names and defined the
    general workflow of the Support Tool.
    For more context please see: go/support-tool-v1-design
    
    Bug: b:200510719
    Test: None for now.
    Change-Id: Ia3f35262d567b82cfced96369df5c4545c8fef9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3197152
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Irem Uğuz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#931917}

---
+++ chrome/browser/support_tool/support_tool_util.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/support_tool/support_tool_util.h"
+
+#include <memory>
+
+#include "chrome/browser/support_tool/support_tool_handler.h"
+#include "chrome/browser/support_tool/ui_hierarchy_data_collector.h"
+
+std::unique_ptr<SupportToolHandler> GetSupportToolHandler(bool chrome_os,
+                                                          bool chrome_browser) {
+  std::unique_ptr<SupportToolHandler> handler =
+      std::make_unique<SupportToolHandler>();
+  if (chrome_os) {
+    handler->AddDataCollector(std::make_unique<UiHierarchyDataCollector>());
+  }
+  return handler;
+}
---
+++ chrome/browser/support_tool/ui_hierarchy_data_collector.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SUPPORT_TOOL_UI_HIERARCHY_DATA_COLLECTOR_H_
+#define CHROME_BROWSER_SUPPORT_TOOL_UI_HIERARCHY_DATA_COLLECTOR_H_
+
+#include "base/callback_forward.h"
+#include "chrome/browser/support_tool/data_collector.h"
+
+class UiHierarchyDataCollector : public DataCollector {
+ public:
+  UiHierarchyDataCollector();
+  ~UiHierarchyDataCollector() override;
+
+  // Overrides from DataCollector.
+  std::string GetName() const override;
+
+  std::string GetDescription() const override;
+
+  const PIIMap& GetDetectedPII() override;
+
+  void CollectDataAndDetectPII(
+      base::OnceCallback<void()> on_data_collected_callback) override;
+
+ private:
+  PIIMap pii_map_;
+};
+
+#endif  // CHROME_BROWSER_SUPPORT_TOOL_UI_HIERARCHY_DATA_COLLECTOR_H_
