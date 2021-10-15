704ec3f0c3cef1a3906c283bcc67497c4cefbf2b
Brian Sheedy <bsheedy@chromium.org>
2021-10-15 20:37:39 +0000

    Reduce unexpected pass finder processing time
    Reduces the amount of time that the unexpected pass finder takes to
    process data returned by BigQuery. This is done by changing the approach
    for adding results to first group identically named results together and
    perform an initial comparison based off that test name.
    
    In practice, this basically eliminates processing time as a significant
    factor in the runtime of the script. Using the WebGL 1 conformance tests
    with 100 samples and the Blink web tests with 20 samples as tests, we
    went from the following:
    
    GPU: ~5 minutes runtime, up to ~25 seconds spent processing results per
        builder
    Blink: ~45 minute runtime, up to ~1300 seconds spent processing results
        per builder
    
    To:
    
    GPU: ~4 minutes runtime, up to 1.5 seconds spent processing results per
        builder
    Blink: ~15 minute runtime, up to ~15 seconds spent processing results
        per builder
    
    Bug: 1222827
    Change-Id: I60ff4a87a9051c301088b136f749c014f4bb865e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224516
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932192}

--- testing/unexpected_passes_common/data_types_unittest.py
+++ testing/unexpected_passes_common/data_types_unittest.py
@@ -623,7 +623,7 @@ class TestExpectationMapAddResultListUnittest(unittest.TestCase):
     self.assertEqual(expectation_map, expected_expectation_map)
 
 
-class TestExpectationMapAddResultUnittest(unittest.TestCase):
+class TestExpectationMapAddGroupedResultsUnittest(unittest.TestCase):
   def testResultMatchPassingNew(self):
     """Test adding a passing result when no results for a builder exist."""
     r = data_types.Result('some/test/case', ['win', 'win10'], 'Pass',
@@ -635,8 +635,12 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
             e: data_types.BuilderStepMap(),
         }),
     })
-    found_matching = expectation_map._AddResult(r, 'builder', None)
-    self.assertTrue(found_matching)
+    grouped_results = {
+        'some/test/case': [r],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', None)
+    self.assertEqual(matched_results, set([r]))
     stats = data_types.BuildStats()
     stats.AddPassedBuild()
     expected_expectation_map = {
@@ -661,8 +665,12 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
             e: data_types.BuilderStepMap(),
         }),
     })
-    found_matching = expectation_map._AddResult(r, 'builder', None)
-    self.assertTrue(found_matching)
+    grouped_results = {
+        'some/test/case': [r],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', None)
+    self.assertEqual(matched_results, set([r]))
     stats = data_types.BuildStats()
     stats.AddFailedBuild('build_id')
     expected_expectation_map = {
@@ -695,8 +703,12 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
             }),
         }),
     })
-    found_matching = expectation_map._AddResult(r, 'builder', None)
-    self.assertTrue(found_matching)
+    grouped_results = {
+        'some/test/case': [r],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', None)
+    self.assertEqual(matched_results, set([r]))
     stats = data_types.BuildStats()
     stats.AddFailedBuild('build_id')
     stats.AddPassedBuild()
@@ -730,8 +742,12 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
             }),
         }),
     })
-    found_matching = expectation_map._AddResult(r, 'builder', None)
-    self.assertTrue(found_matching)
+    grouped_results = {
+        'some/test/case': [r],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', None)
+    self.assertEqual(matched_results, set([r]))
     stats = data_types.BuildStats()
     stats.AddFailedBuild('build_id')
     stats.AddPassedBuild()
@@ -759,8 +775,12 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
             e2: data_types.BuilderStepMap(),
         }),
     })
-    found_matching = expectation_map._AddResult(r, 'builder', None)
-    self.assertTrue(found_matching)
+    grouped_results = {
+        'some/test/case': [r],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', None)
+    self.assertEqual(matched_results, set([r]))
     stats = data_types.BuildStats()
     stats.AddPassedBuild()
     expected_expectation_map = {
@@ -790,8 +810,12 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
             e: data_types.BuilderStepMap(),
         })
     })
-    found_matching = expectation_map._AddResult(r, 'builder', None)
-    self.assertFalse(found_matching)
+    grouped_results = {
+        'some/test/case': [r],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', None)
+    self.assertEqual(matched_results, set())
     expected_expectation_map = {'expectation_file': {e: {}}}
     self.assertEqual(expectation_map, expected_expectation_map)
 
@@ -806,9 +830,12 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
         'bar_expectations':
         data_types.ExpectationBuilderMap({e: data_types.BuilderStepMap()}),
     })
-    found_matching = expectation_map._AddResult(r, 'builder',
-                                                ['bar_expectations'])
-    self.assertTrue(found_matching)
+    grouped_results = {
+        'some/test/case': [r],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', ['bar_expectations'])
+    self.assertEqual(matched_results, set([r]))
     stats = data_types.BuildStats()
     stats.AddPassedBuild()
     expected_expectation_map = {
@@ -821,6 +848,61 @@ class TestExpectationMapAddResultUnittest(unittest.TestCase):
                     'pixel_tests': stats,
                 }
             }
+        }
+    }
+    self.assertEqual(expectation_map, expected_expectation_map)
+
+  def testMultipleResults(self):
+    """Tests that behavior is as expected when multiple results are given."""
+    r1 = data_types.Result('some/test/case', ['win'], 'Pass', 'pixel_tests',
+                           'build_id')
+    r2 = data_types.Result('some/test/case', ['linux'], 'Pass', 'pixel_tests',
+                           'build_id')
+    r3 = data_types.Result('some/other/test', ['win'], 'Pass', 'pixel_tests',
+                           'build_id')
+    r4 = data_types.Result('some/other/test', ['linux'], 'Pass', 'pixel_tests',
+                           'build_id')
+    r5 = data_types.Result('some/other/other/test', ['win'], 'Pass',
+                           'pixel_tests', 'build_id')
+    r6 = data_types.Result('some/other/other/test', ['linux'], 'Pass',
+                           'pixel_tests', 'build_id')
+    e1 = data_types.Expectation('some/test/case', [], 'Failure')
+    e2 = data_types.Expectation('some/other/test', ['win'], 'Failure')
+    e3 = data_types.Expectation('some/other/other/test', ['mac'], 'Failure')
+    expectation_map = data_types.TestExpectationMap({
+        'expectation_file':
+        data_types.ExpectationBuilderMap({
+            e1: data_types.BuilderStepMap(),
+            e2: data_types.BuilderStepMap(),
+            e3: data_types.BuilderStepMap(),
+        })
+    })
+    grouped_results = {
+        'some/test/case': [r1, r2],
+        'some/other/test': [r3, r4],
+        'some/other/other/test': [r5, r6],
+    }
+    matched_results = expectation_map._AddGroupedResults(
+        grouped_results, 'builder', None)
+    self.assertEqual(matched_results, set([r1, r2, r3]))
+    stats1 = data_types.BuildStats()
+    stats1.AddPassedBuild()
+    stats1.AddPassedBuild()
+    stats2 = data_types.BuildStats()
+    stats2.AddPassedBuild()
+    expected_expectation_map = {
+        'expectation_file': {
+            e1: {
+                'builder': {
+                    'pixel_tests': stats1,
+                },
+            },
+            e2: {
+                'builder': {
+                    'pixel_tests': stats2,
+                },
+            },
+            e3: {},
         }
     }
     self.assertEqual(expectation_map, expected_expectation_map)
