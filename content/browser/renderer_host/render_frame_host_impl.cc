578bfe8293cd603521b12b953d6f5460f20531d8
Daniel Cheng <dcheng@chromium.org>
2021-10-15 20:42:46 +0000

    Consistently invalidate Mojo connections when render frame is deleted.
    Bug: 1260007, 1260134
    Change-Id: I2ae77fcbf04b557f7f6e68b55d6c2905708fc220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225563
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932196}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -2760,7 +2760,6 @@ void RenderFrameHostImpl::RenderProcessExited(
   // Reset state for the current RenderFrameHost once the FrameTreeNode has been
   // reset.
   RenderFrameDeleted();
-  InvalidateMojoConnection();
   broker_receiver_.reset();
   SetLastCommittedUrl(GURL());
   renderer_url_info_ = RendererURLInfo();
@@ -3138,6 +3137,7 @@ void RenderFrameHostImpl::RenderFrameDeleted() {
   if (was_created) {
     delegate_->RenderFrameDeleted(this);
   }
+  InvalidateMojoConnection();
 
   if (web_ui_) {
     web_ui_->RenderFrameDeleted();
