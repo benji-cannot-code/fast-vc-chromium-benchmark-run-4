03e8323c03c6b7ad1b27b02bc27f898f9b48b16e
Dominik Röttsches <drott@chromium.org>
2021-10-15 12:42:15 +0000

    Revert "Use last committed URL to open new tab links in side panel."
    This reverts commit 112a25c1db921056ef5d7d8dcc97612750bb7db3.
    
    Reason for revert: Suspected to cause chrome os failures, see https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/18065/test-results and https://bugs.chromium.org/p/chromium/issues/detail?id=1260340
    
    Original change's description:
    > Use last committed URL to open new tab links in side panel.
    >
    > Bug: 1255214, b:201790284, b:201789654, b:201802516
    > Change-Id: I2ee5697a4b8ecfdbed2bc5a9946d0fa3ad58dcb9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3198114
    > Reviewed-by: Adam Langley <agl@chromium.org>
    > Reviewed-by: Ben Goldberger <benwgold@google.com>
    > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > Commit-Queue: Juan Mojica <juanmojica@google.com>
    > Cr-Commit-Position: refs/heads/main@{#931785}
    
    Bug: 1255214, b:201790284, b:201789654, b:201802516
    Change-Id: I7d708c94008a077c158cfee41ef0ee48dcdbd9f5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225888
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931982}

--- components/lens/lens_entrypoints.h
+++ components/lens/lens_entrypoints.h
@@ -8,12 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-class GURL;
-
 namespace lens {
-
-// Query parameter for the payload.
-constexpr char kPayloadQueryParameter[] = "p";
 
 // Lens entry points for LWD.
 enum EntryPoint {
@@ -23,15 +18,6 @@ enum EntryPoint {
   UNKNOWN
 };
 
-// Returns a modified GURL with appended or replaced parameters depending on the
-// entrypoint and other parameters.
-extern GURL AppendOrReplaceQueryParametersForLensRequest(
-    const GURL& url,
-    EntryPoint ep,
-    bool is_side_panel_request);
-
-// Returns a query string with all relevant query parameters. Needed for when a
-// GURL is unavailable to append to.
 extern std::string GetQueryParametersForLensRequest(EntryPoint entry_point,
                                                     bool is_side_panel_request);
 
