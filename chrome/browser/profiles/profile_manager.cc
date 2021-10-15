2e8c5264bd769459adfeb2d163288c58027b0535
Alex Ilin <alexilin@chromium.org>
2021-10-15 09:37:24 +0000

    [Lacros] Do not create AccountProfileMapper if the feature is off
    AccountProfileMapper should only be created if the
    kMultiProfileAccountConsistency feature is enabled. Otherwise, it might
    have negative side-effects like deleting user profiles.
    
    To add a DCHECK(kMultiProfileAccountConsistency) to
    AccountProfileMapper,
    this CL also enables kMultiProfileAccountConsistency in all related
    unit tests. This appears to be a non-trivial task because ProfileManager
    will now create its own instance of AccountProfileMapper that doesn't
    use an AccountManagerFacade mock.
    
    This CL adds a TestingProfileManager::SetAccountProfileMapper() methods
    to inject a fake instance for testing.
    
    Bug: 1226045
    Change-Id: I69f74aed3f4bd9bdb107912d5e92872d53691a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222809
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931942}

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/signin/account_reconcilor_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/signin/signin_features.h"
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
@@ -1020,7 +1021,8 @@ ProfileShortcutManager* ProfileManager::profile_shortcut_manager() {
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 AccountProfileMapper* ProfileManager::GetAccountProfileMapper() {
-  if (!account_profile_mapper_) {
+  if (!account_profile_mapper_ &&
+      base::FeatureList::IsEnabled(kMultiProfileAccountConsistency)) {
     account_profile_mapper_ = std::make_unique<AccountProfileMapper>(
         GetAccountManagerFacade(/*profile_path=*/std::string()),
         &GetProfileAttributesStorage());
