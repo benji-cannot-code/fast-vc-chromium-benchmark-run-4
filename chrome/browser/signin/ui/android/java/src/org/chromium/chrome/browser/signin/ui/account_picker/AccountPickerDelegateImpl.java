08da0b3d2c8285acba69081041e358fb8081f7b2
Alice Wang <aliceywang@chromium.org>
2021-10-15 10:35:20 +0000

    [Android] Update comments in AccountPickerDelegateImpl
    This CL updates the comments in AccountPickerDelegateImpl and removes
    an outdated todo.
    
    Bug: None
    Change-Id: I14393a1229072b5609c04f5326b012726028c10c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225881
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#931958}

--- chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerDelegateImpl.java
+++ chrome/browser/signin/ui/android/java/src/org/chromium/chrome/browser/signin/ui/account_picker/AccountPickerDelegateImpl.java
@@ -67,15 +67,11 @@ public class AccountPickerDelegateImpl implements AccountPickerDelegate {
                 Profile.getLastUsedRegularProfile());
     }
 
-    /** Releases resources used by this class. */
     @Override
     public void destroy() {
         destroyWebSigninBridge();
     }
 
-    /**
-     * Signs the user into the given account.
-     */
     @Override
     public void signIn(
             String accountEmail, Callback<GoogleServiceAuthError> onSignInErrorCallback) {
@@ -108,11 +104,6 @@ public class AccountPickerDelegateImpl implements AccountPickerDelegate {
         });
     }
 
-    /**
-     * Notifies when the user clicked the "add account" button.
-     *
-     * TODO(https//crbug.com/1117000): Change the callback argument to CoreAccountInfo
-     */
     @Override
     public void addAccount(Callback<String> callback) {
         AccountManagerFacadeProvider.getInstance().createAddAccountIntent((@Nullable Intent intent)
@@ -135,9 +126,6 @@ public class AccountPickerDelegateImpl implements AccountPickerDelegate {
         });
     }
 
-    /**
-     * Updates credentials of the given account name.
-     */
     @Override
     public void updateCredentials(
             String accountName, Callback<Boolean> onUpdateCredentialsCallback) {
