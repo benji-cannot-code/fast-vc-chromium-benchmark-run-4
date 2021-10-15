bd51e9ed16c8f69e329276341cb9d5fe0ead3437
Luciano Pacheco <lucmult@chromium.org>
2021-10-15 07:23:58 +0000

    Reland "[Files SWA]: Enable Files SWA to replace the legacy File Manager."
    This is a reland of ba4c6e4ab130a7bb1ac127e856ef1e79ad7cd18b
    
    Original change's description:
    > [Files SWA]: Enable Files SWA to replace the legacy File Manager.
    >
    > This CL adjusts
    > * image loader code imported by Files SWA to have no dependencies
    >   on hhaomji... extension.
    > * changes how Files SWA loads various utilities and the background
    >   code, by loading it from file-manager resources rather than
    >   hhaomji... extension.
    > * Uses regular File Manager Icons
    > * Changes the color of the title bar to be #FFFFFF
    > * Protects against initializing load time data multiple times
    > * Adds a message in an extra big font informing the user that
    >   they are running File System Web App
    >
    > Bug: b/199452030
    > Change-Id: I348fc152f53442d2e4e4880740d3613e5b1aeca3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3216295
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Commit-Queue: Bo Majewski <majewski@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#931807}
    
    Bug: b/199452030
    Change-Id: If13c2f4357522332db8cd009b72b32db9ac24d31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224971
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#931910}

--- ash/webui/file_manager/resources/runtime_loaded_test_util_swa.js
+++ ash/webui/file_manager/resources/runtime_loaded_test_util_swa.js
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // runtime_loaded_test_util.js loads the functions on window.test namespace.
 // runtime_loaded_test_util_swa.js only exists for rollup to have the secondary
 // entry point (in addition to main.js) to generate the bundles.
-import 'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj/background/js/runtime_loaded_test_util.js';
+import 'chrome://file-manager/background/js/runtime_loaded_test_util.js';
--- ui/file_manager/file_manager/background/js/background_base.js
+++ ui/file_manager/file_manager/background/js/background_base.js
@@ -32,7 +32,9 @@ export class BackgroundBaseImpl {
           console.error(chrome.runtime.lastError.message);
           return;
         }
-        loadTimeData.data = assert(stringData);
+        if (!loadTimeData.isInitialized()) {
+          loadTimeData.data = assert(stringData);
+        }
         fulfill(stringData);
       });
     });
