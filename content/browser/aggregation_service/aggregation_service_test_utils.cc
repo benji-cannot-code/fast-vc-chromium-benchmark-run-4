32619bdbe4cc352fabea4995cda0d0267526eb85
Alex Turner <alexmt@chromium.org>
2021-10-15 17:28:21 +0000

    Rename kCountValueHistogram to kHierarchicalHistogram
    AggregationServicePayloadContents::Operation should be updated to match
    the wording in the explainer:
    https://github.com/WICG/conversion-measurement-api/blob/main/AGGREGATE.md
    
    Bug: 1260379
    Change-Id: If87c53b942925319a15e8733c397bc09358b6bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226980
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932093}

--- content/browser/aggregation_service/aggregation_service_test_utils.cc
+++ content/browser/aggregation_service/aggregation_service_test_utils.cc
@@ -165,7 +165,7 @@ AggregatableReportRequest CreateExampleRequest() {
               url::Origin::Create(GURL("https://b.example"))},
              AggregationServicePayloadContents(
                  AggregationServicePayloadContents::Operation::
-                     kCountValueHistogram,
+                     kHierarchicalHistogram,
                  /*bucket=*/123, /*value=*/456,
                  AggregationServicePayloadContents::ProcessingType::kTwoParty,
                  url::Origin::Create(GURL("https://reporting.example"))),
@@ -289,8 +289,8 @@ std::ostream& operator<<(
     std::ostream& out,
     const AggregationServicePayloadContents::Operation& operation) {
   switch (operation) {
-    case AggregationServicePayloadContents::Operation::kCountValueHistogram:
-      return out << "kCountValueHistogram";
+    case AggregationServicePayloadContents::Operation::kHierarchicalHistogram:
+      return out << "kHierarchicalHistogram";
   }
 }
 
