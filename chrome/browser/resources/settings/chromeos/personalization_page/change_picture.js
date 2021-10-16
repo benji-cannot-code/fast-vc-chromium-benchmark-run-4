e0dbe51348a84c906faf42a9f0fd22253ec97fe6
Yue Li <updowndota@google.com>
2021-10-16 23:54:12 +0000

    Avatar Refresh: Fix regression on discard old image button
    Bug: 1259687
    Test: Manual Test
    Change-Id: Ic32dcb3dab5fb3f353fe718818406a6b3664d328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227692
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932306}

--- chrome/browser/resources/settings/chromeos/personalization_page/change_picture.js
+++ chrome/browser/resources/settings/chromeos/personalization_page/change_picture.js
@@ -187,6 +187,7 @@ Polymer({
   receiveOldImage_(imageUrl) {
     this.oldImageLabel_ = this.i18n(
         cr.png.isEncodedPngDataUrlAnimated(imageUrl) ? 'oldVideo' : 'oldPhoto');
+    this.oldImagePending_ = false;
     this.pictureList_.setOldImageUrl(imageUrl);
   },
 
