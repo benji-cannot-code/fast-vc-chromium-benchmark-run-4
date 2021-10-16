686edbdb09c88ff8d082154bd8154ad00bf832c6
Gabriel Szlechtman <gabrielsz@google.com>
2021-10-16 13:25:39 +0000

    [cros][ash] Add Performance and Stability surveys.
    TEST=Manual validation, go/paste/5623459003498496
    
    Bug: b/202990935 b/202990752
    Change-Id: Ib23a8aaf44e5d62746615cd70dcbe642588516b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222174
    Commit-Queue: Gabriel Szlechtman <gabrielsz@google.com>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932282}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -350,6 +350,12 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kHappinessTrackingSystem;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kHappinessTrackingSystemStability;
+
+COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kHappinessTrackingSystemPerformance;
+
+COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kHappinessTrackingSystemOnboarding;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
