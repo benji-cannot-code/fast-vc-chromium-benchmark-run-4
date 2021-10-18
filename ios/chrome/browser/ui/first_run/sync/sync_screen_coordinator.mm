b36984286ff231b2670a81ef14b46ddbcbd95115
Veronique Nguyen <veronguyen@google.com>
2021-10-18 19:50:58 +0000

    [ForcedSignIn]: Don't log FRE histogram in sync screen outside FRE.
    Bug: 1255192
    Change-Id: I8464127fd2afbb41d928db727db24ec9be0034c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227159
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932639}

--- ios/chrome/browser/ui/first_run/sync/sync_screen_coordinator.mm
+++ ios/chrome/browser/ui/first_run/sync/sync_screen_coordinator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/metrics/histogram_functions.h"
 #include "components/sync/driver/sync_service.h"
+#import "ios/chrome/app/application_delegate/app_state.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/first_run/first_run_metrics.h"
 #include "ios/chrome/browser/main/browser.h"
@@ -29,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/first_run/sync/sync_screen_mediator.h"
 #import "ios/chrome/browser/ui/first_run/sync/sync_screen_mediator_delegate.h"
 #import "ios/chrome/browser/ui/first_run/sync/sync_screen_view_controller.h"
+#import "ios/chrome/browser/ui/main/scene_state.h"
+#import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
 #import "ios/chrome/browser/unified_consent/unified_consent_service_factory.h"
 #include "ios/chrome/grit/ios_strings.h"
 
@@ -66,6 +69,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Coordinator that handles the advanced settings sign-in UI.
 @property(nonatomic, strong)
     SigninCoordinator* advancedSettingsSigninCoordinator;
+
+// YES if this coordinator is currently used in the First Run context.
+@property(nonatomic, assign) BOOL firstRun;
 
 @end
 
@@ -119,6 +125,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
+  // Determine if it is currently in the First Run context.
+  SceneState* sceneState =
+      SceneStateBrowserAgent::FromBrowser(self.browser)->GetSceneState();
+  AppState* appState = sceneState.appState;
+  self.firstRun = appState.initStage == InitStageFirstRun;
+
   self.viewController = [[SyncScreenViewController alloc] init];
   self.viewController.delegate = self;
   self.viewController.syncTypesRestricted =
@@ -141,7 +153,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.mediator.delegate = self;
   self.mediator.consumer = self.viewController;
 
-  base::UmaHistogramEnumeration("FirstRun.Stage", first_run::kSyncScreenStart);
+  if (self.firstRun) {
+    base::UmaHistogramEnumeration("FirstRun.Stage",
+                                  first_run::kSyncScreenStart);
+  }
 
   BOOL animated = self.baseNavigationController.topViewController != nil;
   [self.baseNavigationController setViewControllers:@[ self.viewController ]
@@ -179,8 +194,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)didTapSecondaryActionButton {
-  base::UmaHistogramEnumeration("FirstRun.Stage",
-                                first_run::kSyncScreenCompletionWithoutSync);
+  if (self.firstRun) {
+    base::UmaHistogramEnumeration("FirstRun.Stage",
+                                  first_run::kSyncScreenCompletionWithoutSync);
+  }
   syncer::SyncService* syncService =
       SyncServiceFactory::GetForBrowserState(self.browser->GetBrowserState());
   // Call StopAndClear() to clear the encryption passphrase, in case the
@@ -207,8 +224,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // interactions on the advanced settings button.
     [self showAdvancedSettings];
   } else {
-    base::UmaHistogramEnumeration("FirstRun.Stage",
-                                  first_run::kSyncScreenCompletionWithSync);
+    if (self.firstRun) {
+      base::UmaHistogramEnumeration("FirstRun.Stage",
+                                    first_run::kSyncScreenCompletionWithSync);
+    }
     [self.delegate willFinishPresenting];
   }
 }
