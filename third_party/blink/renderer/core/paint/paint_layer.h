c555d33522801b70ba2730b8f41a7458bfe6e1ff
Yuki Yamada <yukiy@chromium.org>
2021-10-18 02:34:49 +0000

    Layout: Make PaintLayer::layout_object_ a Member
    This is a follow-up for https://crrev.com/c/2440003/48/third_party/blink/renderer/core/paint/paint_layer.h#1458
    
    Converting PaintLayer::layout_object_ to a Member is safe because
    PaintLayer doesn't outlive LayoutObject.
    
    Change-Id: I31983f29f100f965799913256e0783dda95d8752
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224899
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932401}

--- third_party/blink/renderer/core/paint/paint_layer.h
+++ third_party/blink/renderer/core/paint/paint_layer.h
@@ -1444,7 +1444,7 @@ class CORE_EXPORT PaintLayer : public GarbageCollected<PaintLayer>,
   bool is_destroyed_ = false;
 #endif
 
-  const WeakMember<LayoutBoxModelObject> layout_object_;
+  const Member<LayoutBoxModelObject> layout_object_;
 
   Member<PaintLayer> parent_;
   Member<PaintLayer> previous_;
