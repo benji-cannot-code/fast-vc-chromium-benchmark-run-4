e16a397f1f16234d76909aeb76835e553ea1bc76
Michael Hansen <hansenmichael@google.com>
2021-10-18 22:51:13 +0000

    [Nearby] Ensure Nearby subpage reloads with timestamp.
    Clicking the "Device nearby is sharing" notification will open the
    Nearby Share receive dialog and enable high visibility mode. This change
    appends a timestamp to the receive dialog URL to ensure that if you
    click another instance of the "Device nearby is sharing" notification
    while the receive dialog is already open, the page will reload and
    restart high visibility mode.
    
    Bug: 1258640
    Change-Id: Ia5e31011afb0fc8b4aa2f4d0bd4a0e505518c254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227610
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#932721}

--- chrome/browser/nearby_sharing/nearby_notification_manager.cc
+++ chrome/browser/nearby_sharing/nearby_notification_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -68,6 +69,11 @@ message_center::Notification CreateNearbyNotification(const std::string& id) {
       message_center::SettingsButtonHandler::DELEGATE);
 
   return notification;
+}
+
+std::string GetTimestampString() {
+  return base::NumberToString(
+      base::Time::Now().ToDeltaSinceWindowsEpoch().InMicroseconds());
 }
 
 FileAttachment::Type GetCommonFileAttachmentType(
@@ -1033,7 +1039,8 @@ void NearbyNotificationManager::OnNearbyDeviceTryingToShareClicked() {
 
   std::string path =
       std::string(chromeos::settings::mojom::kNearbyShareSubpagePath) +
-      "?receive&entrypoint=notification";
+      "?receive&entrypoint=notification&";
+  path += "&time=" + GetTimestampString();
   settings_opener_->ShowSettingsPage(profile_, path);
 }
 
