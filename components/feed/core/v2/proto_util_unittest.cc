3bfb62cbce4050d3aa20dc85adaaed58e8ff04d8
Ian Wells <iwells@chromium.org>
2021-10-18 20:05:09 +0000

    Add feature flag to control sending OPEN_IN_TAB capability for feed requests from Start Surface
    After this change, the server logic leaving out "open in new tab" from Start Surface feeds can be removed and instead only check whether the OPEN_IN_TAB capability is present.
    
    Bug: 1225676
    Change-Id: Ieb54bd472d75e24aa46513d14de1359fcd68d1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224347
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932646}

--- components/feed/core/v2/proto_util_unittest.cc
+++ components/feed/core/v2/proto_util_unittest.cc
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace feed {
 namespace {
 
+using ::testing::Contains;
+using ::testing::Not;
+
 TEST(ProtoUtilTest, CreateClientInfo) {
   RequestMetadata request_metadata;
   request_metadata.chrome_info.version = base::Version({1, 2, 3, 4});
@@ -57,10 +60,12 @@ TEST(ProtoUtilTest, ClientInfoStartSurface) {
 }
 
 TEST(ProtoUtilTest, DefaultCapabilities) {
+  RequestMetadata metadata;
+  metadata.chrome_info.start_surface = false;
   feedwire::FeedRequest request =
       CreateFeedQueryRefreshRequest(kForYouStream,
                                     feedwire::FeedQuery::MANUAL_REFRESH,
-                                    /*request_metadata=*/{},
+                                    /*request_metadata=*/metadata,
                                     /*consistency_token=*/std::string())
           .feed_request();
 
@@ -92,10 +97,13 @@ TEST(ProtoUtilTest, HeartsEnabled) {
           .feed_request();
 
   ASSERT_THAT(request.client_capability(),
-              testing::Contains(feedwire::Capability::HEART));
+              Contains(feedwire::Capability::HEART));
 }
 
 TEST(ProtoUtilTest, DisableCapabilitiesWithFinch) {
+  RequestMetadata metadata;
+  metadata.chrome_info.start_surface = false;
+
   // Try to disable BASE_UI and _INFINITE_FEED. BASE_UI is not an experimental
   // capability, and should not be affected.
   base::test::ScopedFeatureList scoped_feature_list;
@@ -107,7 +115,7 @@ TEST(ProtoUtilTest, DisableCapabilitiesWithFinch) {
   feedwire::FeedRequest request =
       CreateFeedQueryRefreshRequest(kForYouStream,
                                     feedwire::FeedQuery::MANUAL_REFRESH,
-                                    /*request_metadata=*/{},
+                                    /*request_metadata=*/metadata,
                                     /*consistency_token=*/std::string())
           .feed_request();
 
@@ -164,9 +172,9 @@ TEST(ProtoUtilTest, AutoplayEnabled) {
           .feed_request();
 
   ASSERT_THAT(request.client_capability(),
-              testing::Contains(feedwire::Capability::INLINE_VIDEO_AUTOPLAY));
+              Contains(feedwire::Capability::INLINE_VIDEO_AUTOPLAY));
   ASSERT_THAT(request.client_capability(),
-              testing::Contains(feedwire::Capability::OPEN_VIDEO_COMMAND));
+              Contains(feedwire::Capability::OPEN_VIDEO_COMMAND));
 }
 
 TEST(ProtoUtilTest, StampEnabled) {
@@ -180,11 +188,72 @@ TEST(ProtoUtilTest, StampEnabled) {
           .feed_request();
 
   ASSERT_THAT(request.client_capability(),
-              testing::Contains(feedwire::Capability::SILK_AMP_OPEN_COMMAND));
+              Contains(feedwire::Capability::SILK_AMP_OPEN_COMMAND));
   ASSERT_THAT(request.client_capability(),
-              testing::Contains(feedwire::Capability::AMP_STORY_PLAYER));
+              Contains(feedwire::Capability::AMP_STORY_PLAYER));
   ASSERT_THAT(request.client_capability(),
-              testing::Contains(feedwire::Capability::AMP_GROUP_DATASTORE));
+              Contains(feedwire::Capability::AMP_GROUP_DATASTORE));
+}
+
+TEST(ProtoUtilTest, OpenInNewTabFromStartSurface) {
+  RequestMetadata metadata;
+  metadata.chrome_info.start_surface = true;
+
+  {
+    feedwire::FeedRequest request =
+        CreateFeedQueryRefreshRequest(kForYouStream,
+                                      feedwire::FeedQuery::MANUAL_REFRESH,
+                                      /*request_metadata=*/metadata,
+                                      /*consistency_token=*/std::string())
+            .feed_request();
+
+    ASSERT_THAT(request.client_capability(),
+                Not(Contains(feedwire::Capability::OPEN_IN_TAB)));
+  }
+  {
+    base::test::ScopedFeatureList scoped_feature_list;
+    scoped_feature_list.InitWithFeatures(
+        {kEnableOpenInNewTabFromStartSurfaceFeed}, {});
+    feedwire::FeedRequest request =
+        CreateFeedQueryRefreshRequest(kForYouStream,
+                                      feedwire::FeedQuery::MANUAL_REFRESH,
+                                      /*request_metadata=*/metadata,
+                                      /*consistency_token=*/std::string())
+            .feed_request();
+
+    ASSERT_THAT(request.client_capability(),
+                Contains(feedwire::Capability::OPEN_IN_TAB));
+  }
+}
+
+TEST(ProtoUtilTest, OpenInNewTabFromNTP) {
+  RequestMetadata metadata;
+  metadata.chrome_info.start_surface = false;
+  {
+    feedwire::FeedRequest request =
+        CreateFeedQueryRefreshRequest(kForYouStream,
+                                      feedwire::FeedQuery::MANUAL_REFRESH,
+                                      /*request_metadata=*/metadata,
+                                      /*consistency_token=*/std::string())
+            .feed_request();
+
+    ASSERT_THAT(request.client_capability(),
+                Contains(feedwire::Capability::OPEN_IN_TAB));
+  }
+  {
+    base::test::ScopedFeatureList scoped_feature_list;
+    scoped_feature_list.InitWithFeatures(
+        {kEnableOpenInNewTabFromStartSurfaceFeed}, {});
+    feedwire::FeedRequest request =
+        CreateFeedQueryRefreshRequest(kForYouStream,
+                                      feedwire::FeedQuery::MANUAL_REFRESH,
+                                      /*request_metadata=*/metadata,
+                                      /*consistency_token=*/std::string())
+            .feed_request();
+
+    ASSERT_THAT(request.client_capability(),
+                Contains(feedwire::Capability::OPEN_IN_TAB));
+  }
 }
 
 }  // namespace
