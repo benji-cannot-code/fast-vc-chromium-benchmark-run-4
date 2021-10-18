0c60010cf12714ba4f7439295a879b7b9178b21d
Koji Ishii <kojii@chromium.org>
2021-10-18 06:24:44 +0000

    [AsyncFontAccess] Prewarm the default family for `<input>`
    This patch prewarms the default font family for `<input
    type="text">`.
    
    This patch has no behavior changes because `PrewarmFamily` is
    still no-op.
    
    Bug: 1256946
    Change-Id: If9735280f7d71b2aa0682208f5971edeaa3841c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226536
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932433}

--- third_party/blink/renderer/core/html/forms/html_input_element.cc
+++ third_party/blink/renderer/core/html/forms/html_input_element.cc
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/input_type_names.h"
 #include "third_party/blink/renderer/core/layout/adjust_for_absolute_zoom.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
+#include "third_party/blink/renderer/core/layout/layout_theme_font_provider.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/platform/bindings/exception_messages.h"
@@ -95,6 +96,8 @@ namespace blink {
 namespace {
 
 const unsigned kMaxEmailFieldLength = 254;
+
+static bool is_default_font_prewarmed_ = false;
 
 }  // namespace
 
@@ -409,6 +412,15 @@ void HTMLInputElement::InitializeTypeInParsing() {
     input_type_->WarnIfValueIsInvalid(default_value);
 
   input_type_view_->UpdateView();
+
+  // Prewarm the default font family. Do this while parsing because the style
+  // recalc calls |TextControlInnerEditorElement::CreateInnerEditorStyle| which
+  // needs the primary font.
+  if (!is_default_font_prewarmed_ && new_type_name == input_type_names::kText) {
+    FontCache::PrewarmFamily(LayoutThemeFontProvider::SystemFontFamily(
+        CSSValueID::kWebkitSmallControl));
+    is_default_font_prewarmed_ = true;
+  }
 }
 
 void HTMLInputElement::UpdateType() {
