a760f4a42878d88f6c95a787bb359ffa64e60276
Bo Liu <boliu@chromium.org>
2021-10-18 17:36:59 +0000

    weblayer: Workaround SurfaceView attach glitch
    Workaround a SurfaceView visual glitch which draws the hole immediately
    after attach before there is surface below has content.
    
    Bug: b/202507554
    Change-Id: Ib1eca109860565f17d016dfe8da10c7bef209aef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3228458
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932573}

--- weblayer/browser/content_view_render_view.cc
+++ weblayer/browser/content_view_render_view.cc
@@ -156,7 +156,9 @@ void ContentViewRenderView::SurfaceChanged(
 }
 
 void ContentViewRenderView::SetNeedsRedraw(JNIEnv* env) {
-  compositor_->SetNeedsRedraw();
+  if (compositor_) {
+    compositor_->SetNeedsRedraw();
+  }
 }
 
 base::android::ScopedJavaLocalRef<jobject>
