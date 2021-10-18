f0471ec1379e9888e7f076f73d79482e27b2d1a3
rbpotter <rbpotter@chromium.org>
2021-10-18 22:40:42 +0000

    WebUI: Migrate CrContainerShadowMixin to TypeScript
    This mixin is only used in UIs that are already migrated to TypeScript,
    so it will be more convenient to have the mixin itself also migrated.
    
    Bug: 1189595
    Change-Id: Ie5c14ffa8f8f3e56c926a8541f3accabb3c395e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227195
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932720}

--- chrome/browser/resources/extensions/error_page.ts
+++ chrome/browser/resources/extensions/error_page.ts
@@ -82,8 +82,7 @@ interface ExtensionsErrorPageElement {
   };
 }
 
-const ExtensionsErrorPageElementBase =
-    CrContainerShadowMixin(PolymerElement) as {new (): PolymerElement};
+const ExtensionsErrorPageElementBase = CrContainerShadowMixin(PolymerElement);
 
 class ExtensionsErrorPageElement extends ExtensionsErrorPageElementBase {
   static get is() {
