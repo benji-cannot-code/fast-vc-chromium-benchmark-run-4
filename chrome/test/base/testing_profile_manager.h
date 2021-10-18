65e79d5aa78b45f30a4e95637ee2a2cd35d7d753
Alex Ilin <alexilin@chromium.org>
2021-10-18 11:33:13 +0000

    Reland "[Lacros] Do not create AccountProfileMapper if the feature is off"
    This is a reland of 7db902da0f7ad90b013f69e005957e22909ac13e with a fix
    for a crash.
    
    The reason of crash is that an AccountProfileMapper could be destroyed
    before a profile that this has dependency on it. This problem is
    addressed by changing the order of class member declaration in
    profile_manager.h.
    
    Original change's description:
    > [Lacros] Do not create AccountProfileMapper if the feature is off
    >
    > AccountProfileMapper should only be created if the
    > kMultiProfileAccountConsistency feature is enabled. Otherwise, it might
    > have negative side-effects like deleting user profiles.
    >
    > To add a DCHECK(kMultiProfileAccountConsistency) to
    > AccountProfileMapper,
    > this CL also enables kMultiProfileAccountConsistency in all related
    > unit tests. This appears to be a non-trivial task because ProfileManager
    > will now create its own instance of AccountProfileMapper that doesn't
    > use an AccountManagerFacade mock.
    >
    > This CL adds a TestingProfileManager::SetAccountProfileMapper() methods
    > to inject a fake instance for testing.
    >
    > Bug: 1226045
    > Change-Id: I69f74aed3f4bd9bdb107912d5e92872d53691a07
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222809
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Alex Ilin <alexilin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#931942}
    
    Bug: 1226045
    Change-Id: I3a61f396e3f3c6ea423b0845525e3fbfdbd1258d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225895
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932477}

--- chrome/test/base/testing_profile_manager.h
+++ chrome/test/base/testing_profile_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/scoped_multi_source_observation.h"
 #include "base/test/scoped_path_override.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile_observer.h"
 #include "chrome/test/base/scoped_testing_local_state.h"
 #include "chrome/test/base/testing_profile.h"
@@ -26,6 +27,10 @@ class TestingBrowserProcess;
 namespace sync_preferences {
 class PrefServiceSyncable;
 }
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+class AccountProfileMapper;
+#endif
 
 // The TestingProfileManager is a TestingProfile factory for a multi-profile
 // environment. It will bring up a full ProfileManager and attach it to the
@@ -112,6 +117,10 @@ class TestingProfileManager : public ProfileObserver {
 
   // Sets the last used profile; also sets the active time to now.
   void UpdateLastUser(Profile* last_active);
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  void SetAccountProfileMapper(std::unique_ptr<AccountProfileMapper> mapper);
+#endif
 
   // Helper accessors.
   const base::FilePath& profiles_dir();
