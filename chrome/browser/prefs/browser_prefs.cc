4b52cc8959ab21d4948724b504aae22a0d68b75a
yilkal <yilkal@chromium.org>
2021-10-18 21:59:00 +0000

    [Projector] Define and register Projector preferences.
    This cl adds and defines the ash.projector.creationFlowEnabled
    preference.
    The creationFlowEnabled preference is set when the user goes through
    the Projector onboarding.
    
    Bug: b/200205765
    Change-Id: I19b28f07ea4fc33d0957815d9ab008f5cc30a9d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200012
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932706}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -195,6 +195,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/settings/stats_reporting_controller.h"
 #include "chrome/browser/chromeos/extensions/extensions_permissions_tracker.h"
 #include "chrome/browser/component_updater/metadata_table_chromeos.h"
+#include "chrome/browser/ui/ash/projector/projector_app_client_impl.h"
 #include "chrome/browser/ui/webui/signin/inline_login_handler_chromeos.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
@@ -1376,6 +1377,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   borealis::prefs::RegisterProfilePrefs(registry);
   ash::ChromeScanningAppDelegate::RegisterProfilePrefs(registry);
   policy::PolicyCertService::RegisterProfilePrefs(registry);
+  ProjectorAppClientImpl::RegisterProfilePrefs(registry);
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
