13d32028d5d1c88b279746d638718818e3afd261
Jinsuk Kim <jinsukkim@chromium.org>
2021-10-18 23:35:32 +0000

    Deflake OmniboxTest
    This CL deflakes OmniboxTest#testSecurityIconOnHTTPSFocusAndBack and
    re-enables it.
    
    The issue was that the back button event was sometimes generated
    before the EditText widget is ready (or while temporarily not ready
    yet), therefore gets lost. This could be verified by giving enough
    delay (as much as 5000ms), which makes the problem
    disappear. Interestingly this happens only on the emulator running
    with --avd-config=tools/android/avd/proto/generic_android28.textpb.
    Not observed on other config or on real devices.
    
    But there was a much bigger problem that made the test very flaky:
    Espresso ViewActions#click has a bug.
    
    For this specific test, roughly 1 out of 50 times,
    |onView(withId(R.id.url_bar)).perform(click())| generated a long click
    instead of a click. This caused the text selection to be activated on
    the EditText widget. Back press was only consumed to deactivate it but
    failed to make the URL bar lose focus and get the security lock icon
    back.
    
    It is a known issue documented in the Espresso repo:
    https://bit.ly/3BHMhEC
    
    Found from here:
    https://stackoverflow.com/questions/32330671/android-espresso-
    performs-longclick-instead-of-click
    
    This is allegedly a very hard problem to solve. The approach taken in
    this CL to work around it is to replace Espresso APIs with a test util
    method that can work to the same effect of click()/backPress() by
    giving/taking way the focus from URL bar.
    
    Bug: 1195124
    Change-Id: Ic5d98760334d55f70e99ed03b4de5560573a0d84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220763
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932736}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxTest.java
@@ -5,16 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
 import android.annotation.SuppressLint;
 import android.support.test.InstrumentationRegistry;
 import android.view.KeyEvent;
 import android.widget.ImageView;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.filters.MediumTest;
 import androidx.test.filters.SmallTest;
 
@@ -31,7 +26,6 @@ import org.chromium.base.test.params.SkipCommandLineParameterization;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.EnormousTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
@@ -269,7 +263,6 @@ public class OmniboxTest {
     @Test
     @MediumTest
     @SkipCommandLineParameterization
-    @DisabledTest(message = "https://crbug.com/1195124")
     public void testSecurityIconOnHTTPSFocusAndBack() throws Exception {
         setNonDefaultSearchEngine();
 
@@ -301,11 +294,12 @@ public class OmniboxTest {
                     locationBar.getStatusCoordinatorForTesting();
             final int firstIcon = statusCoordinator.getSecurityIconResourceIdForTesting();
 
-            onView(withId(R.id.url_bar)).perform(click());
+            UrlBar urlBar = (UrlBar) mActivityTestRule.getActivity().findViewById(R.id.url_bar);
+            TestThreadUtils.runOnUiThreadBlocking(() -> urlBar.requestFocus());
             CriteriaHelper.pollUiThread(
                     () -> statusCoordinator.getSecurityIconResourceIdForTesting() != firstIcon);
             final int secondIcon = statusCoordinator.getSecurityIconResourceIdForTesting();
-            Espresso.pressBack();
+            TestThreadUtils.runOnUiThreadBlocking(() -> urlBar.clearFocus());
             CriteriaHelper.pollUiThread(
                     () -> statusCoordinator.getSecurityIconResourceIdForTesting() != secondIcon);
 
