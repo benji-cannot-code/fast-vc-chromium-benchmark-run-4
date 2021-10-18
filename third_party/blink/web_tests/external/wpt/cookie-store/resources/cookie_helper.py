0f68b4a0e29829d2b73d89ff97e1b61a766966c7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-10-18 20:07:17 +0000

    Import wpt@7e060aadcfe0d7080d069ed0c686036db5d89201
    Using wpt-import in Chromium fbcb4c4b5418eff5baa92d9429af8f910d356be7.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    ayui@chromium.org, mek@chromium.org, pwnall@chromium.org, jsbell@chromium.org:
      external/wpt/cookie-store
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    R=foolip@google.com
    
    No-Export: true
    Change-Id: I2e931480fc2cb214918050cb3b138e86e4b7c6bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229185
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#932649}

--- third_party/blink/web_tests/external/wpt/cookie-store/resources/cookie_helper.py
+++ third_party/blink/web_tests/external/wpt/cookie-store/resources/cookie_helper.py
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # Active wptserve handler for cookie operations.
--- third_party/blink/web_tests/external/wpt/xhr/resources/access-control-basic-allow-no-credentials.py
+++ third_party/blink/web_tests/external/wpt/xhr/resources/access-control-basic-allow-no-credentials.py
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
 def main(request, response):
     response.headers.set(b"Content-Type", b"text/plain")
     response.headers.set(b"Access-Control-Allow-Origin", request.headers.get(b"origin"))
