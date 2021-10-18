4d78236989ba45d592e0ee7f4fb94beccb3a0588
Felipe Andrade <fsandrade@chromium.org>
2021-10-18 23:44:06 +0000

    Fix typos in some policy files
    These typos were found while working on the associated issue. Fixings
    then in a separate CL, to avoid confusion.
    
    Bug: 1248117
    Change-Id: I100485c9f00c47493aed1b0c50bd464e7e95346f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220617
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932742}

--- chrome/browser/ash/attestation/enrollment_certificate_uploader_impl.h
+++ chrome/browser/ash/attestation/enrollment_certificate_uploader_impl.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/attestation/enrollment_certificate_uploader.h"
-// TODO(https://crbug.com/1164001): forward declare AttestatoinFlow
+// TODO(https://crbug.com/1164001): forward declare AttestationFlow
 // after //chromeos/attestation is moved to ash.
 #include "chromeos/attestation/attestation_flow.h"
 #include "chromeos/dbus/constants/attestation_constants.h"
--- chrome/browser/ash/attestation/machine_certificate_uploader_impl.h
+++ chrome/browser/ash/attestation/machine_certificate_uploader_impl.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/attestation/machine_certificate_uploader.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-// TODO(https://crbug.com/1164001): forward declare AttestatoinFlow
+// TODO(https://crbug.com/1164001): forward declare AttestationFlow
 // after //chromeos/attestation is moved to ash.
 #include "chromeos/attestation/attestation_flow.h"
 #include "chromeos/dbus/attestation/interface.pb.h"
