19639ff02af4a50b7b4e3942d210247a9dd42b3a
Allen Bauer <kylixrd@chromium.org>
2021-10-18 21:02:26 +0000

    Remove HasCustomColor from DownloadShelfView code.
    This also pushes the calculation of the colors into the ThemeHelper. It will slightly change the themed color calculations while keeping the colors the same for non-themed cases.
    
    Provided ColorProvider color mapping and calculation recipes.
    
    Bug: 1056953
    Change-Id: I83afd3fbb127668babbeeac8a0a24b52fe06bb63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227286
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932677}

--- chrome/browser/themes/browser_theme_pack.cc
+++ chrome/browser/themes/browser_theme_pack.cc
@@ -1049,6 +1049,7 @@ void BrowserThemePack::AddColorMixers(
     int property_id;
     int color_id;
   } kThemePropertiesMap[] = {
+      {TP::COLOR_DOWNLOAD_SHELF, kColorDownloadShelf},
       {TP::COLOR_TOOLBAR, kColorToolbar},
       {TP::COLOR_OMNIBOX_TEXT, kColorOmniboxText},
       {TP::COLOR_OMNIBOX_BACKGROUND, kColorOmniboxBackground},
