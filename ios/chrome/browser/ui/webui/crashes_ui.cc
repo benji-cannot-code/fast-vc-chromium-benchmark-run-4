ab0b85790a6c8a5074ea4dfea67fb58cad8a2ca1
Justin Cohen <justincohen@google.com>
2021-10-18 13:52:31 +0000

    ios: Enable manualUploads on chrome://crashes with Crashpad.
    manualUploads works with Crashpad.
    
    Change-Id: Ib23412e7322dc03b51b531695a81597e5f3c9261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225440
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932504}

--- ios/chrome/browser/ui/webui/crashes_ui.cc
+++ ios/chrome/browser/ui/webui/crashes_ui.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "base/values.h"
 #include "components/crash/core/browser/crashes_ui_util.h"
+#include "components/crash/core/common/reporter_running_ios.h"
 #include "components/grit/dev_ui_components_resources.h"
 #include "components/strings/grit/components_chromium_strings.h"
 #include "components/strings/grit/components_strings.h"
@@ -137,7 +138,7 @@ void CrashesDOMHandler::UpdateUI() {
   base::Value result(base::Value::Type::DICTIONARY);
   result.SetBoolPath("enabled", crash_reporting_enabled);
   result.SetBoolPath("dynamicBackend", false);
-  result.SetBoolPath("manualUploads", false);
+  result.SetBoolPath("manualUploads", crash_reporter::IsCrashpadRunning());
   result.SetPath("crashes", std::move(crash_list));
   result.SetStringPath("version", version_info::GetVersionNumber());
   result.SetStringPath("os", base::SysInfo::OperatingSystemName() + " " +
