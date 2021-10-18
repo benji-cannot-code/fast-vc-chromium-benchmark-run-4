f4e033e84e18bfbc4defc7db73ff98c15682e5d1
Ossama Mahmoud <osamafathy@google.com>
2021-10-18 18:53:39 +0000

    OOBE: Add usage stats opt-in to guest ToS screen
    Bug: 1259929
    Change-Id: I658d8b65d722b40b6f01cc99f3d78d74debbb09e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222748
    Commit-Queue: Ossama Mahmoud <osamafathy@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932607}

--- chrome/browser/ash/login/screens/guest_tos_screen.cc
+++ chrome/browser/ash/login/screens/guest_tos_screen.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
-constexpr const char kUserActionAcceptClicked[] = "accept-button";
 constexpr const char kUserActionBackClicked[] = "back-button";
 constexpr const char kUserActionCancelClicked[] = "cancel";
 
@@ -71,9 +70,7 @@ void GuestTosScreen::ShowImpl() {
 void GuestTosScreen::HideImpl() {}
 
 void GuestTosScreen::OnUserAction(const std::string& action_id) {
-  if (action_id == kUserActionAcceptClicked) {
-    exit_callback_.Run(Result::ACCEPT);
-  } else if (action_id == kUserActionBackClicked) {
+  if (action_id == kUserActionBackClicked) {
     exit_callback_.Run(Result::BACK);
   } else if (action_id == kUserActionCancelClicked) {
     exit_callback_.Run(Result::CANCEL);
@@ -82,4 +79,8 @@ void GuestTosScreen::OnUserAction(const std::string& action_id) {
   }
 }
 
+void GuestTosScreen::OnAccept(bool enable_usage_stats) {
+  // TODO(crbug.com/1241468): Handle usage stats reporting for guest session.
+  exit_callback_.Run(Result::ACCEPT);
+}
 }  // namespace ash
--- chrome/browser/resources/chromeos/login/guest_tos.js
+++ chrome/browser/resources/chromeos/login/guest_tos.js
@@ -27,7 +27,12 @@ Polymer({
 
   behaviors: [OobeI18nBehavior, MultiStepBehavior, LoginScreenBehavior],
 
-  properties: {},
+  properties: {
+    usageChecked: {
+      type: Boolean,
+      value: true,
+    },
+  },
 
   ready() {
     this.initializeLoginScreen('GuestTosScreen', {
@@ -90,6 +95,10 @@ Polymer({
     return terms.innerHTML;
   },
 
+  getUsageLearnMoreText_(locale) {
+    return this.i18nAdvanced('guestTosUsageOptInLearnMore');
+  },
+
   onGoogleEulaLinkClick_() {
     this.setUIStep(UIState.GOOGLE_EULA);
     this.$.googleEulaOkButton.focus();
@@ -106,13 +115,17 @@ Polymer({
     }
   },
 
+  onUsageLearnMoreClick_() {
+    this.$.usageLearnMorePopUp.showDialog();
+  },
+
   onTermsStepOkClick_() {
     this.setUIStep(UIState.LOADED);
     this.$.acceptButton.focus();
   },
 
   onAcceptClick_() {
-    this.userActed('accept-button');
+    chrome.send('GuestToSAccept', [this.usageChecked]);
   },
 
   onBackClick_() {
