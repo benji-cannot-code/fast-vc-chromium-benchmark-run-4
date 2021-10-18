b7604ffc11b4abca6247e5f429c0ad33d6f4b147
ainozaki <ainozaki@google.com>
2021-10-18 00:50:31 +0000

    Move variations::IsVariationsHeader
    variations::IsVariationsHeader is used only in
    chrome/browser/prefetch/prefetch_browsertest.cc, so move it into the test code.
    
    This is a clean-up CL of https://chromium-review.googlesource.com/c/chromium/src/+/3189210.
    
    Bug: 963260
    Change-Id: Id65b78d159ecd0155ace8522b66f6c8c5cb4a446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218927
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Ai Nozaki <ainozaki@google.com>
    Cr-Commit-Position: refs/heads/main@{#932386}

--- chrome/browser/prefetch/prefetch_browsertest.cc
+++ chrome/browser/prefetch/prefetch_browsertest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/prefs/pref_service.h"
-#include "components/variations/net/variations_http_headers.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
@@ -41,11 +40,7 @@ const char kPrefetchCachingPeriodUrl[] = "/prefetch_caching_period";
 
 bool HasVariationsHeader(
     const net::test_server::HttpRequest::HeaderMap& headers) {
-  for (const auto& pair : headers) {
-    if (variations::IsVariationsHeader(pair.first))
-      return true;
-  }
-  return false;
+  return headers.find("X-Client-Data") != headers.end();
 }
 
 class MockNetworkChangeNotifierWIFI : public NetworkChangeNotifier {
