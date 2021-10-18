f52fd62cc765dad0195ce1ce8f597a2ba4189091
Dan Harrington <harringtond@chromium.org>
2021-10-18 00:59:19 +0000

    Reland "feed modularization: FeedStream"
    This is a reland of 70331b911314dc35fb1793894c6e43ceb637c67e
    
    Original change's description:
    > feed modularization: FeedStream
    >
    > * Moved several other classes to the module
    > * Introduced FeedActionDelegate to delegate some actions
    >   out to the app / containing surface.
    >
    > Bug: 1254437
    > Change-Id: I1dce3a37c0c1d2a1ac37b54e519e0100ee78256f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221891
    > Commit-Queue: Dan H <harringtond@chromium.org>
    > Reviewed-by: Theresa  <twellington@chromium.org>
    > Reviewed-by: Xi Han <hanxi@chromium.org>
    > Reviewed-by: Cathy Li <chili@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#932242}
    
    Bug: 1254437
    Change-Id: I5c77efa19a5b8afd04e96dce4dac6b10144be0cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227720
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932387}

--- chrome/android/java/src/org/chromium/chrome/browser/app/feed/feedmanagement/FeedManagementTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/feed/feedmanagement/FeedManagementTest.java
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-package org.chromium.chrome.browser.app.feedmanagement;
+package org.chromium.chrome.browser.app.feed.feedmanagement;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.intent.Intents.intended;
