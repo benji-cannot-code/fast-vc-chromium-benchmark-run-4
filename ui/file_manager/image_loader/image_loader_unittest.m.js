6d7297e046c00e4c6b5a47134da7432f0dc3818f
Bo Majewski <majewski@chromium.org>
2021-10-18 05:21:01 +0000

    [Files SWA]: Follow-up cleanup for crrev.com/c/3224971
    Fix paths for DialogType. Use copy of image_loader.
    
    Bug: b/199452030
    Change-Id: I6561b0dd2e8e4d74053fbf5b97f579c944a0c184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225750
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932428}

--- ui/file_manager/image_loader/image_loader_unittest.m.js
+++ ui/file_manager/image_loader/image_loader_unittest.m.js
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {ImageOrientation} from 'chrome-extension://hhaomjibdihmijegdhdafkllkbggdgoj/foreground/js/metadata/image_orientation.js';
 import {assertEquals} from 'chrome://test/chai_assert.js';
 
 import {ImageLoaderUtil} from './image_loader_util.js';
+import {ImageOrientation} from './image_orientation.js';
 import {LoadImageRequest} from './load_image_request.js';
 
 
