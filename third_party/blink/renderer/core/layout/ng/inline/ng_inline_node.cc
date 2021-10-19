25c066b7ad061087b4cb9252853ee37d80d436ac
Kent Tamura <tkent@chromium.org>
2021-10-19 06:45:50 +0000

    Deferred Shaping: Add UMA histograms for times spent for font shaping
    This CL has no user-visible behavior changes.
    
    Bug: 1259085
    Change-Id: I2d85f48d937b183276c00dd092f515ccc9035a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229281
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932884}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/svg/ng_svg_text_layout_attributes_builder.h"
 #include "third_party/blink/renderer/core/layout/ng/svg/svg_inline_node_data.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/platform/fonts/font_performance.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/harfbuzz_shaper.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_spacing.h"
@@ -525,8 +526,12 @@ void NGInlineNode::PrepareLayout(NGInlineNodeData* previous_data) const {
   DCHECK(data);
   CollectInlines(data, previous_data);
   SegmentText(data);
-  ShapeText(data, previous_data ? &previous_data->text_content : nullptr);
-  ShapeTextForFirstLineIfNeeded(data);
+  {
+    base::ElapsedTimer shaping_timer;
+    ShapeText(data, previous_data ? &previous_data->text_content : nullptr);
+    ShapeTextForFirstLineIfNeeded(data);
+    FontPerformance::AddShapingTime(shaping_timer.Elapsed());
+  }
   AssociateItemsWithInlines(data);
   DCHECK_EQ(data, MutableData());
 
@@ -934,8 +939,12 @@ bool NGInlineNode::SetTextWithOffset(LayoutText* layout_text,
   // Relocates |ShapeResult| in |previous_data| after |offset|+|length|
   editor.Run();
   node.SegmentText(data);
-  node.ShapeText(data, &previous_data->text_content, &previous_data->items);
-  node.ShapeTextForFirstLineIfNeeded(data);
+  {
+    base::ElapsedTimer shaping_timer;
+    node.ShapeText(data, &previous_data->text_content, &previous_data->items);
+    node.ShapeTextForFirstLineIfNeeded(data);
+    FontPerformance::AddShapingTime(shaping_timer.Elapsed());
+  }
   node.AssociateItemsWithInlines(data);
   return true;
 }
--- third_party/blink/renderer/platform/fonts/font_performance.h
+++ third_party/blink/renderer/platform/fonts/font_performance.h
@@ -22,6 +22,7 @@ class PLATFORM_EXPORT FontPerformance {
     primary_font_ = base::TimeDelta();
     primary_font_in_style_ = base::TimeDelta();
     system_fallback_ = base::TimeDelta();
+    shaping_ = base::TimeDelta();
   }
 
   // The aggregated time spent in |DeterminePrimarySimpleFontData|.
@@ -46,6 +47,8 @@ class PLATFORM_EXPORT FontPerformance {
     system_fallback_ += time;
   }
 
+  static void AddShapingTime(base::TimeDelta time) { shaping_ += time; }
+
   static void MarkFirstContentfulPaint();
   static void MarkDomContentLoaded();
 
@@ -62,6 +65,7 @@ class PLATFORM_EXPORT FontPerformance {
   static base::TimeDelta primary_font_;
   static base::TimeDelta primary_font_in_style_;
   static base::TimeDelta system_fallback_;
+  static base::TimeDelta shaping_;
   static unsigned in_style_;
 };
 
