67bd6518954c35dd6af9eed5ceeacbb34d8dfadf
Brian Sheedy <bsheedy@chromium.org>
2021-10-19 18:01:03 +0000

    Support virtual tests in stale expectation remover
    Adds support for virtual test suites in the web test stale expectation
    remover. Previously, results from such tests were treated as unused, as
    there was no mechanism for falling back to expectations for the
    non-virtual version of the test.
    
    As part of this work, also adds support to the shared code for custom
    data type extensions.
    
    Bug: 1222827
    Change-Id: I1cb6e09036100cc259b882b64c78b32d523a1120
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227698
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933084}

--- testing/unexpected_passes_common/data_types.py
+++ testing/unexpected_passes_common/data_types.py
@@ -17,8 +17,26 @@ FULL_PASS = 1
 NEVER_PASS = 2
 PARTIAL_PASS = 3
 
+# Allow different unexpected pass finder implementations to register custom
+# data types if necessary. These are set to the base versions at the end of the
+# file.
+Expectation = None
+Result = None
 
-class Expectation(object):
+
+def SetExpectationImplementation(impl):
+  global Expectation
+  assert issubclass(impl, BaseExpectation)
+  Expectation = impl
+
+
+def SetResultImplementation(impl):
+  global Result
+  assert issubclass(impl, BaseResult)
+  Result = impl
+
+
+class BaseExpectation(object):
   """Container for a test expectation.
 
   Similar to typ's expectations_parser.Expectation class, but with unnecessary
@@ -41,12 +59,12 @@ class Expectation(object):
     # slower (~40x from rough testing) than a straight comparison, so only use
     # it if necessary.
     if '*' in test:
-      self._comp = lambda r: fnmatch.fnmatch(r, self.test)
+      self._comp = self._CompareWildcard
     else:
-      self._comp = lambda r: r == self.test
+      self._comp = self._CompareNonWildcard
 
   def __eq__(self, other):
-    return (isinstance(other, Expectation) and self.test == other.test
+    return (isinstance(other, BaseExpectation) and self.test == other.test
             and self.tags == other.tags
             and self.expected_results == other.expected_results
             and self.bug == other.bug)
@@ -56,6 +74,12 @@ class Expectation(object):
 
   def __hash__(self):
     return hash((self.test, self.tags, self.expected_results, self.bug))
+
+  def _CompareWildcard(self, result_test_name):
+    return fnmatch.fnmatch(result_test_name, self.test)
+
+  def _CompareNonWildcard(self, result_test_name):
+    return result_test_name == self.test
 
   def AppliesToResult(self, result):
     """Checks whether this expectation should have applied to |result|.
@@ -70,7 +94,7 @@ class Expectation(object):
     Returns:
       True if |self| applies to |result|, otherwise False.
     """
-    assert isinstance(result, Result)
+    assert isinstance(result, BaseResult)
     return (self._comp(result.test) and self.tags <= result.tags)
 
   def MaybeAppliesToTest(self, test_name):
@@ -85,7 +109,7 @@ class Expectation(object):
     return self._comp(test_name)
 
 
-class Result(object):
+class BaseResult(object):
   """Container for a test result.
 
   Contains the minimal amount of data necessary to describe/identify a result
@@ -109,7 +133,7 @@ class Result(object):
     self.build_id = build_id
 
   def __eq__(self, other):
-    return (isinstance(other, Result) and self.test == other.test
+    return (isinstance(other, BaseResult) and self.test == other.test
             and self.tags == other.tags
             and self.actual_result == other.actual_result
             and self.step == other.step and self.build_id == other.build_id)
@@ -487,7 +511,7 @@ class ExpectationBuilderMap(BaseTypedMap):
   """Typed map for Expectation -> BuilderStepMap."""
 
   def __setitem__(self, key, value):
-    assert isinstance(key, Expectation)
+    assert isinstance(key, BaseExpectation)
     assert isinstance(value, self._value_type())
     super(ExpectationBuilderMap, self).__setitem__(key, value)
 
@@ -557,3 +581,7 @@ class StepBuildStatsMap(BaseTypedMap):
 
 def IsStringType(s):
   return isinstance(s, six.string_types)
+
+
+Expectation = BaseExpectation
+Result = BaseResult
--- testing/unexpected_passes_common/data_types_unittest.py
+++ testing/unexpected_passes_common/data_types_unittest.py
@@ -93,6 +93,14 @@ class ExpectationUnittest(unittest.TestCase):
                                'Pass')
     self.assertFalse(e.AppliesToResult(r))
 
+  def testCustomImplementation(self):
+    class CustomExpectation(data_types.BaseExpectation):
+      pass
+
+    data_types.SetExpectationImplementation(CustomExpectation)
+    expectation = data_types.Expectation('test', ['tag1', 'tag2'], 'Pass')
+    self.assertIsInstance(expectation, CustomExpectation)
+
 
 class ResultUnittest(unittest.TestCase):
   def testEquality(self):
@@ -121,6 +129,15 @@ class ResultUnittest(unittest.TestCase):
   def testHashability(self):
     r = GENERIC_RESULT
     _ = {r}
+
+  def testCustomImplementation(self):
+    class CustomResult(data_types.BaseResult):
+      pass
+
+    data_types.SetResultImplementation(CustomResult)
+    result = data_types.Result('test', ['tag1', 'tag2'], 'Pass', 'pixel_tests',
+                               'build_id')
+    self.assertIsInstance(result, CustomResult)
 
 
 class BuildStatsUnittest(unittest.TestCase):
