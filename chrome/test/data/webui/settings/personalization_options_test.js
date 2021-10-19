5a36b192080ea3422846761c176ba21ad3c92a58
David Roger <droger@chromium.org>
2021-10-19 21:59:42 +0000

    [Lacros] Fix signout buttons in settings
    Signout is not fully supported yet. This CL disables the signout buttons
    when it's not supported:
    - "Turn off sync" button is removed in non-Dice profiles
    - "Allow Chrome Signin" toggle is disabled in all profiles. It would be
      technically possible to keep it in Dice profiles, but Dice profiles
      are not intended to be supported in the long term, so this CL goes for
      simpler code and always disables it.
    
    Bug: 1226067, 1217645
    Change-Id: Ie96538635b26908feb3d9c8a340f228202416b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226075
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933191}

--- chrome/test/data/webui/settings/personalization_options_test.js
+++ chrome/test/data/webui/settings/personalization_options_test.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // clang-format off
 import 'chrome://settings/lazy_load.js';
 
-import {isChromeOS} from 'chrome://resources/js/cr.m.js';
+import {isChromeOS, isLacros} from 'chrome://resources/js/cr.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PrivacyPageBrowserProxyImpl, StatusAction, SyncBrowserProxyImpl} from 'chrome://settings/settings.js';
 import {TestPrivacyPageBrowserProxy} from 'chrome://test/settings/test_privacy_page_browser_proxy.js';
@@ -78,7 +78,7 @@ suite('PersonalizationOptionsTests_AllBuilds', function() {
     assertFalse(!!testElement.shadowRoot.querySelector('#driveSuggestControl'));
   });
 
-  if (!isChromeOS) {
+  if (!isChromeOS && !isLacros) {
     test('signinAllowedToggle', function() {
       const toggle = testElement.$.signinAllowedToggle;
       assertTrue(isVisible(toggle));
