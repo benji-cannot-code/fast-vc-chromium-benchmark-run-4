ea0ce68eb3748b3540f201f7517741f38acb92e2
Shakti Sahu <shaktisahu@chromium.org>
2021-10-19 20:24:39 +0000

    Download Infobar and Messages: Cleanup code for old download path
    This CL removes the old download path from download infobar and download
    message UI.
    
    Bug: 1260391
    Change-Id: I0f15adece0e249a0e68fa7d5d6d98fec08c83359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229208
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933157}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -402,11 +402,7 @@ public class DownloadManagerService implements DownloadController.Observer,
         }
     }
 
-    private void updateDownloadInfoBar(DownloadItem item) {
-        DownloadMessageUiController infobarController =
-                getInfoBarController(item.getDownloadInfo().getOTRProfileId());
-        if (infobarController != null) infobarController.onDownloadItemUpdated(item);
-    }
+    private void updateDownloadInfoBar(DownloadItem item) {}
 
     /**
      * Broadcast that a download was successful.
@@ -1015,8 +1011,6 @@ public class DownloadManagerService implements DownloadController.Observer,
             removeDownloadProgress(id.id);
         } else {
             mDownloadNotifier.notifyDownloadCanceled(id);
-            DownloadMessageUiController infoBarController = getInfoBarController(otrProfileID);
-            if (infoBarController != null) infoBarController.onDownloadItemRemoved(id);
         }
     }
 
@@ -1500,12 +1494,6 @@ public class DownloadManagerService implements DownloadController.Observer,
     // Deprecated after new download backend.
     @CalledByNative
     private void onDownloadItemRemoved(String guid, OTRProfileID otrProfileID) {
-        DownloadMessageUiController infobarController = getInfoBarController(otrProfileID);
-        if (infobarController != null) {
-            infobarController.onDownloadItemRemoved(
-                    LegacyHelpers.buildLegacyContentId(false, guid));
-        }
-
         for (DownloadObserver adapter : mDownloadObservers) {
             adapter.onDownloadItemRemoved(guid);
         }
