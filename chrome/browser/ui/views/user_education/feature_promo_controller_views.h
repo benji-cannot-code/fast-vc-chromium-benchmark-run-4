d5578091a0b8f1a03c9dcda1c35b7d2cc37c6439
Dana Fried <dfried@chromium.org>
2021-10-19 17:10:05 +0000

    Gate reading of the help message for the focus help bubble accelerator.
    Note: I also considered naming this CL "yo dawg, heard you liked
          promos!" or possibly just "promoception".
    
    Appends a promo for the accelerator to focus a help bubble to the screen
    reader text for help bubbles. Promo goes away after being read 10 times
    (only counts if there is a screen reader present) or when the
    accelerator is used to actually focus something.
    
    The promo itself uses the Feature Engagement backend. However, since
    demo mode blocks other FE promos, we bypass the FE backend when demoing
    an IPH (the accelerator promo will always play, assuming a screen reader
    is enabled).
    
    Bug: 1212149
    Change-Id: Ic40fdc89fe3d031248ec7bcdab3c0f140bf8a0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218213
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933058}

--- chrome/browser/ui/views/user_education/feature_promo_controller_views.h
+++ chrome/browser/ui/views/user_education/feature_promo_controller_views.h
@@ -130,7 +130,8 @@ class FeaturePromoControllerViews : public FeaturePromoController {
   void FinishContinuedPromo() override;
 
   bool ShowPromoBubbleImpl(const FeaturePromoBubbleParams& params,
-                           views::View* anchor_view);
+                           views::View* anchor_view,
+                           bool screen_reader_promo);
 
   void HandleBubbleClosed();
 
@@ -140,6 +141,11 @@ class FeaturePromoControllerViews : public FeaturePromoController {
 
   // Returns whether we can play a screen reader prompt for the "focus help
   // bubble" promo.
+  // TODO(crbug.com/1258216): This must be called *before* we ask if the bubble
+  // will show because a limitation in the current FE backend causes
+  // ShouldTriggerHelpUI() to always return false if another promo is being
+  // displayed. Once we have machinery to allow concurrency in the FE system
+  // all of this logic can be rewritten.
   bool CheckScreenReaderPromptAvailable() const;
 
   // The browser window this instance is responsible for.
