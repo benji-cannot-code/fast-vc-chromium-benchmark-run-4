ce42d92a4adcb9ed4b6a03dd0accf12d5f6eb962
Bowen Li <lbowen@google.com>
2021-10-19 20:19:41 +0000

    Hide PWAs in the launcher and search if the device is entering the guest managed session with no internet connection for demo mode.
    - Before change
    Launcher:
    https://screenshot.googleplex.com/4WDNJ9q7ZKnnud8
    
    Show results while searching PWAs(Youtube):
    https://screenshot.googleplex.com/7XioqrPtwWVVMy3
    
    - After change
    Launcher:
    https://screenshot.googleplex.com/7hYxzqFmvuuLSey
    
    Hide results while searching PWAs(Youtube):
    https://screenshot.googleplex.com/SPFXBD5rBQ8CdrJ
    
    R=lbowen@google.com
    
    Bug: b:199298082
    Change-Id: I6b2d27aaf9d4a4b01027acf342780e2aa802762a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3179526
    Commit-Queue: Yves Arrouye <drcrash@chromium.org>
    Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933153}

--- chrome/browser/ui/app_list/app_service/app_service_app_model_builder_unittest.cc
+++ chrome/browser/ui/app_list/app_service/app_service_app_model_builder_unittest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/ash/guest_os/guest_os_registry_service.h"
 #include "chrome/browser/ash/guest_os/guest_os_registry_service_factory.h"
+#include "chrome/browser/ash/login/demo_mode/demo_mode_test_helper.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_features.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_test_helper.h"
 #include "chrome/browser/extensions/chrome_app_icon.h"
@@ -73,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/uninstall_reason.h"
 #include "extensions/common/extension_set.h"
 #include "extensions/common/manifest.h"
+#include "services/network/test/test_network_connection_tracker.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -643,6 +645,54 @@ TEST_F(WebAppBuilderTest, LoadGeneratedIcon) {
   WaitForIconUpdates(item);
 
   VerifyIcon(src_image_skia, item->icon());
+}
+
+class WebAppBuilderDemoModeTest : public WebAppBuilderTest {
+ protected:
+  ~WebAppBuilderDemoModeTest() override = default;
+
+  void SetUp() override {
+    WebAppBuilderTest::SetUp();
+    CreateBuilder();
+
+    // Fake Demo Mode.
+    demo_mode_test_helper_ = std::make_unique<ash::DemoModeTestHelper>();
+    demo_mode_test_helper_->InitializeSession();
+
+    app_service_test_.SetUp(profile_.get());
+    RemoveApps(apps::mojom::AppType::kWeb, profile(), model_updater_.get());
+  }
+
+  void TearDown() override {
+    demo_mode_test_helper_.reset();
+
+    WebAppBuilderTest::TearDown();
+  }
+
+ private:
+  std::unique_ptr<ash::DemoModeTestHelper> demo_mode_test_helper_;
+};
+
+// This test adds a web app to the app list for demo mode when online.
+TEST_F(WebAppBuilderDemoModeTest, WebAppListOnline) {
+  network::TestNetworkConnectionTracker::GetInstance()->SetConnectionType(
+      network::mojom::ConnectionType::CONNECTION_ETHERNET);
+
+  const std::string kAppName = "https://test.com";
+  CreateWebApp(kAppName);
+
+  EXPECT_EQ(1u, model_updater_->ItemCount());
+}
+
+// This test adds a web app to the app list for demo mode when offline.
+TEST_F(WebAppBuilderDemoModeTest, WebAppListOffline) {
+  network::TestNetworkConnectionTracker::GetInstance()->SetConnectionType(
+      network::mojom::ConnectionType::CONNECTION_NONE);
+
+  const std::string kAppName = "https://test.com";
+  CreateWebApp(kAppName);
+
+  EXPECT_EQ(0u, model_updater_->ItemCount());
 }
 
 class CrostiniAppTest : public AppServiceAppModelBuilderTest {
--- chrome/browser/ui/app_list/extension_app_utils.cc
+++ chrome/browser/ui/app_list/extension_app_utils.cc
@@ -25,7 +25,7 @@ namespace app_list {
 
 bool ShouldShowInLauncher(const extensions::Extension* extension,
                           content::BrowserContext* context) {
-  return ash::DemoSession::ShouldDisplayInAppLauncher(extension->id()) &&
+  return ash::DemoSession::ShouldShowExtensionInAppLauncher(extension->id()) &&
          extensions::ui_util::ShouldDisplayInAppLauncher(extension, context);
 }
 
