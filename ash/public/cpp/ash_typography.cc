632bb06bcdcff96b7d29e4e6a96944d587a928a9
Melissa Zhang <melzhang@google.com>
2021-10-19 23:39:22 +0000

    [Sharesheet] Add sharesheet context for file enumeration label.
    Fixes a dcheck failure where sharesheet style should only be used for
    sharesheet contexts.
    
    Bug: 1177554
    Change-Id: I4461fe305385cadd8568bd8e10387f77bc2c4d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229404
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933230}

--- ash/public/cpp/ash_typography.cc
+++ ash/public/cpp/ash_typography.cc
@@ -13,6 +13,9 @@ void ApplyAshFontStyles(int context,
   switch (context) {
     case CONTEXT_SEARCH_RESULT_VIEW_INLINE_ANSWER_DETAILS:
       break;
+    case CONTEXT_SHARESHEET_BUBBLE_SMALL:
+      details.size_delta = -2;
+      break;
     case CONTEXT_SHARESHEET_BUBBLE_BODY_SECONDARY:
       details.size_delta = 1;
       break;
@@ -23,8 +26,6 @@ void ApplyAshFontStyles(int context,
     case CONTEXT_LAUNCHER_BUTTON:
     case CONTEXT_SHARESHEET_BUBBLE_BODY:
     case CONTEXT_CALENDAR_DATE:
-      details.size_delta = 2;
-      break;
     case CONTEXT_SEARCH_RESULT_VIEW:
       details.size_delta = 2;
       break;
@@ -54,7 +55,8 @@ void ApplyAshFontStyles(int context,
     case STYLE_SHARESHEET:
       DCHECK(context == CONTEXT_SHARESHEET_BUBBLE_TITLE ||
              context == CONTEXT_SHARESHEET_BUBBLE_BODY ||
-             context == CONTEXT_SHARESHEET_BUBBLE_BODY_SECONDARY);
+             context == CONTEXT_SHARESHEET_BUBBLE_BODY_SECONDARY ||
+             context == CONTEXT_SHARESHEET_BUBBLE_SMALL);
       details.weight = gfx::Font::Weight::MEDIUM;
       break;
     case STYLE_CLASSIC_LAUNCHER:
