0a4d251dae728ab610bc74506c7ceb7ea76451c0
Preethi Mohan <preethim@google.com>
2021-10-19 02:08:23 +0000

    reverting the additional logging to debug crbug/1235878 on "win7 dbg Tests"
    Original CL to add logging - https://chromium-review.googlesource.com/c/chromium/src/+/3213441
    
    Bug: 1235878
    Change-Id: I4ec3068f6ebce66c1ea55f645208b1d9ca871b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3230228
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932808}

--- chrome/test/base/browser_tests_main.cc
+++ chrome/test/base/browser_tests_main.cc
@@ -17,15 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // defined(OS_WIN)
 
 int main(int argc, char** argv) {
-  // TODO(preethim): Debugging for crbug.com/1235878.
-  // Will revert within a day or two after reproing in CI.
-  fprintf(stdout, "Starting browser_tests.\n");
   base::CommandLine::Init(argc, argv);
   size_t parallel_jobs = base::NumParallelJobs(/*cores_per_job=*/2);
   if (parallel_jobs == 0U) {
-    // TODO(preethim): Debugging for crbug.com/1235878.
-    // Will revert within a day or two after reproing in CI.
-    fprintf(stdout, "parallel_jobs is 0. Exiting.\n");
     return 1;
   }
 
