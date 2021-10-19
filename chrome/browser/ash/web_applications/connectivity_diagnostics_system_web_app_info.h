c1e7bb68a52067ea6d63b24e6182b576a6332f6a
Henrique Ferreiro <hferreiro@igalia.com>
2021-10-19 05:41:48 +0000

    Move {chromeos/components => ash/webui}/connectivity_diagnostics/
    Also, the moved files are migrated from namespace chromeos to ash.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Bug: 1164001
    Change-Id: I2112df0577619832a8851e851342cc71ada42f75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226076
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#932871}

--- chrome/browser/ash/web_applications/connectivity_diagnostics_system_web_app_info.h
+++ chrome/browser/ash/web_applications/connectivity_diagnostics_system_web_app_info.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_WEB_APPLICATIONS_CONNECTIVITY_DIAGNOSTICS_SYSTEM_WEB_APP_INFO_H_
 #define CHROME_BROWSER_ASH_WEB_APPLICATIONS_CONNECTIVITY_DIAGNOSTICS_SYSTEM_WEB_APP_INFO_H_
 
+#include "ash/webui/connectivity_diagnostics/url_constants.h"
 #include "chrome/browser/web_applications/system_web_apps/system_web_app_delegate.h"
 #include "chrome/browser/web_applications/system_web_apps/system_web_app_types.h"
-#include "chromeos/components/connectivity_diagnostics/url_constants.h"
 
 struct WebApplicationInfo;
 
--- chrome/browser/ui/webui/chromeos/connectivity_diagnostics_dialog.cc
+++ chrome/browser/ui/webui/chromeos/connectivity_diagnostics_dialog.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "chromeos/components/connectivity_diagnostics/url_constants.h"
+#include "ash/webui/connectivity_diagnostics/url_constants.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 
