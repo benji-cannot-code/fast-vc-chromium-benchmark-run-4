1bbda17431cfe4aa1b9baac113ca71eb50467332
Yuki Shiino <yukishiino@chromium.org>
2021-10-19 19:00:34 +0000

    bindings: Fix 'length' property of observable array
    blink::bindings::CreatePropertyDescriptorObject needs 'enumerable'
    and 'configurable' to be set explicitly, plus ES Proxy has a
    restriction that the target object's property must be
    {configurable: false} if the trap of getOwnPropertyDescriptor
    returns {configurable: false}.
    
    Bug: 1201744
    Change-Id: Ic54ecf7680e0ee800c32f7978edb793d8559fdfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3230670
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#933110}

--- third_party/blink/renderer/bindings/core/v8/observable_array_exotic_object_handler.h
+++ third_party/blink/renderer/bindings/core/v8/observable_array_exotic_object_handler.h
@@ -249,6 +249,8 @@ class ObservableArrayExoticObjectHandler {
               V8AtomicString(isolate, "length"))) {
         v8::PropertyDescriptor prop_desc(
             v8::Integer::NewFromUnsigned(isolate, backing_list.size()), true);
+        prop_desc.set_configurable(false);
+        prop_desc.set_enumerable(false);
         V8SetReturnValue(info, prop_desc);
         return;
       }
