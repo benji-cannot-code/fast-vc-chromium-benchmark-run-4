25cc5026b7763a3a568d0694bdbe2d63f49c5f2c
Mark Schillaci <mschillaci@google.com>
2021-10-19 00:01:35 +0000

    Accessibility sprint: Add focus ring color change when recording macros
    This CL adds some logic to switch the colors of the primary and preview
    focus rings of Switch Access on ChromeOS while recording a macro. We
    add Material red colors and toggle the focus ring colors during the
    action recorder start and stop methods.
    
    AX-Relnotes: N/A
    Bug: 1258921
    Change-Id: I64638a1935ceb3d7667c54f9e988a6ca7a822a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227525
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#932756}

--- chrome/browser/resources/chromeos/accessibility/switch_access/action_recorder.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/action_recorder.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {FocusRingManager} from './focus_ring_manager.js';
 import {NodeIdentifier} from './node_identifier.js';
 import {SwitchAccess} from './switch_access.js';
 
@@ -33,11 +34,13 @@ export class ActionRecorder {
   start() {
     this.recording_ = true;
     this.macro_ = [];
+    FocusRingManager.setIsRecording(true);
   }
 
   /** Stops recording actions */
   stop() {
     this.recording_ = false;
+    FocusRingManager.setIsRecording(false);
   }
 
   /** @param {!chrome.automation.AutomationNode} node */
