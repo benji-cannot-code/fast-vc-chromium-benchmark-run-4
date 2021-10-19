718ad17cac454a5cf997293950a8f30308119427
Nancy Wang <nancylingwang@chromium.org>
2021-10-19 17:30:59 +0000

    Replace mojom::IconValuePtr with IconValue for app icon loader(part 1).
    DD: https://docs.google.com/document/d/1TCWKi0rqxhWI-DT6vZHsSia7_M_8_vQ1lCwGrpdnEII
    
    This is the preparation to remove mojom in AppService. This CL will be
    submitted with CL:3224903 to remove AppService mojom in app icon
    loader, to reuse app icon loader for both mojom and non-mojom
    AppService. With these 2 CLs, the icon loading process will be:
    Client: LoadIcon
      -> AppService
        -> Publisher::LoadIcon
          -> app icon loading(apps::IconValue)
            -> (new)Convert apps::IconValue -> apps::mojom::IconValue
              -> mojom OnIconLoadCallback(apps::mojom::IconValue)
    
    "Convert apps::IconValue -> apps::mojom::IconValue" is a new call to
    reuse non-mojom app icon loader.
    
    The final goal is removing mojom for icon loading, which will be done
    with several follow up CLs.
    
    This CL replace some apps::mojom::IconValuePtr with apps::IconValue in
    app icon loader, to reuse app icon loader for icon loading with both
    mojom and non-mojom AppService.
    
    This CL is pure refactor, and no functionn change.
    
    BUG=1253250
    
    Change-Id: Id7b26cab18a868975f4bbf26fb7e12306f6eb9fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225843
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933070}

--- chrome/browser/ash/remote_apps/remote_apps_manager_browsertest.cc
+++ chrome/browser/ash/remote_apps/remote_apps_manager_browsertest.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/login/auth/user_context.h"
 #include "components/account_id/account_id.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
+#include "components/services/app_service/public/cpp/icon_types.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
@@ -315,8 +316,8 @@ IN_PROC_BROWSER_TEST_F(RemoteAppsManagerBrowsertest, AddApp) {
 
   base::RunLoop run_loop;
   apps::mojom::IconValuePtr output_data = apps::mojom::IconValue::New();
-  apps::mojom::IconValuePtr iv = apps::mojom::IconValue::New();
-  iv->icon_type = apps::mojom::IconType::kStandard;
+  std::unique_ptr<apps::IconValue> iv = std::make_unique<apps::IconValue>();
+  iv->icon_type = apps::IconType::kStandard;
   iv->uncompressed = icon;
   iv->is_placeholder_icon = true;
   apps::ApplyIconEffects(icon_effects, 64, std::move(iv),
