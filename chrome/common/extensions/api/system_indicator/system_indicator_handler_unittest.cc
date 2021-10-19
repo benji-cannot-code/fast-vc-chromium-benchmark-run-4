c26c56cd33f18d23c5a250e6627e45ac244a3538
David Bertoni <dbertoni@chromium.org>
2021-10-19 21:58:25 +0000

    [Extensions] Only enable the systemIndicator API on supported platforms.
    This API broke on ChromeOS in 2019 and test coverage was disabled.
    Since this API is abandoned, and we'd like to get rid of it, this
    CL limits its use to the platforms where it still works. It also
    disables the API in the Dev channel, limiting it to only the extensions
    that were previously allowed in stable.
    
    The tests were tested for flakiness and passed without issue, so they
    are now all enabled only on those platforms.
    
    Bug: 497643
    Change-Id: Ifc551d105624d5686129fbb15684c4355eccdf81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3221806
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933189}

--- chrome/common/extensions/api/system_indicator/system_indicator_handler_unittest.cc
+++ chrome/common/extensions/api/system_indicator/system_indicator_handler_unittest.cc
@@ -5,21 +5,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/extensions/api/system_indicator/system_indicator_handler.h"
 
+#include "base/strings/stringprintf.h"
 #include "base/test/values_test_util.h"
-#include "components/version_info/channel.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_icon_set.h"
-#include "extensions/common/features/feature_channel.h"
 #include "extensions/common/manifest_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 namespace extensions {
 
+namespace {
+
+// The manifest permissions for system_indicator have an allowlist. This is the
+// test key.
+constexpr char kKey[] =
+    "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9fDu8apG3Dz72XTT3Ym1SfGt06tdowTlYQ+"
+    "3lGlCbVpfnMOmewgRgYxzUtUPso9aQERZcmI2+7UtbWjtk6/usl9Hr7a1JBQwfaUoUygEe56aj"
+    "UeZhe/ErkH5CXT84U0pokfPr5vMvc7RVPduU+UBiF0DnGb/hSpzz/1UhJ5H9AwIDAQAB";
+
+}  // anonymous namespace
+
 using SystemIndicatorHandlerTest = ManifestTest;
 
 TEST_F(SystemIndicatorHandlerTest, BasicTests) {
-  ScopedCurrentChannel current_channel(version_info::Channel::DEV);
-
   // Simple icon path.
   {
     constexpr char kManifest[] =
@@ -27,10 +35,12 @@ TEST_F(SystemIndicatorHandlerTest, BasicTests) {
              "name": "System Indicator",
              "manifest_version": 2,
              "version": "0.1",
+             "key": "%s",
              "system_indicator": { "default_icon": "icon.png" }
            })";
     scoped_refptr<const Extension> extension = LoadAndExpectSuccess(
-        ManifestData(base::test::ParseJson(kManifest), "icon"));
+        ManifestData(base::test::ParseJson(base::StringPrintf(kManifest, kKey)),
+                     "icon"));
     ASSERT_TRUE(extension);
     const ExtensionIconSet* icon =
         SystemIndicatorHandler::GetSystemIndicatorIcon(*extension);
@@ -50,6 +60,7 @@ TEST_F(SystemIndicatorHandlerTest, BasicTests) {
              "name": "System Indicator",
              "manifest_version": 2,
              "version": "0.1",
+             "key": "%s",
              "system_indicator": {
                "default_icon": {
                  "24": "icon24.png",
@@ -59,7 +70,8 @@ TEST_F(SystemIndicatorHandlerTest, BasicTests) {
              }
            })";
     scoped_refptr<const Extension> extension = LoadAndExpectSuccess(
-        ManifestData(base::test::ParseJson(kManifest), "icon"));
+        ManifestData(base::test::ParseJson(base::StringPrintf(kManifest, kKey)),
+                     "icon"));
     ASSERT_TRUE(extension);
     const ExtensionIconSet* icon =
         SystemIndicatorHandler::GetSystemIndicatorIcon(*extension);
@@ -80,10 +92,12 @@ TEST_F(SystemIndicatorHandlerTest, BasicTests) {
              "name": "System Indicator",
              "manifest_version": 2,
              "version": "0.1",
+             "key": "%s",
              "system_indicator": {}
            })";
     scoped_refptr<const Extension> extension = LoadAndExpectSuccess(
-        ManifestData(base::test::ParseJson(kManifest), "icon"));
+        ManifestData(base::test::ParseJson(base::StringPrintf(kManifest, kKey)),
+                     "icon"));
     ASSERT_TRUE(extension);
     const ExtensionIconSet* icon =
         SystemIndicatorHandler::GetSystemIndicatorIcon(*extension);
