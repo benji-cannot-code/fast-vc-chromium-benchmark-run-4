6881d2cf896b1c358ecad0493d164830731e5ad2
Ari Chivukula <arichiv@chromium.org>
2021-10-20 15:46:42 +0000

    [Legacy client hints modernization] (3a) Implement `sec-ch-` device-memory, dpr, width, and viewport-width
    This CL contains a fix for the way client hints were implemented in:
    https://chromium-review.googlesource.com/c/chromium/src/+/3205978
    by switching from the legacy handling model to the standard one.
    
    This series of CLs seeks to add `sec-ch-` prefixes to client hints that
    are missing them by deprecating the old versions and adding new ones:
    (1) Add `sec-ch-` hint constants
    (2) Add feature to gate legacy hints for future deprecation
    (3) Implement `sec-ch-` device-memory, dpr, width, and viewport-width
    (4) Add deprecation warning for legacy hints
    (5) Enable `sec-ch-` client hints by default
    
    Bug: 1227043
    Change-Id: I504092db86ea20f1390aa8f66a1e3824533f4eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3225782
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933481}

--- third_party/blink/renderer/core/loader/frame_fetch_context_test.cc
+++ third_party/blink/renderer/core/loader/frame_fetch_context_test.cc
@@ -627,14 +627,12 @@ TEST_F(FrameFetchContextHintsTest, MonitorDeviceMemorySecureTransport) {
 #if defined(OS_ANDROID)
   ExpectHeader("https://www.someother-example.com/1.gif", "Device-Memory", true,
                "4");
-  ExpectHeader("https://www.someother-example.com/1.gif",
-               "Sec-CH-Device-Memory", true, "4");
 #else
   ExpectHeader("https://www.someother-example.com/1.gif", "Device-Memory",
                false, "");
+#endif
   ExpectHeader("https://www.someother-example.com/1.gif",
                "Sec-CH-Device-Memory", false, "");
-#endif
 }
 
 // Verify that client hints are not attached when the resources do not belong to
@@ -1094,14 +1092,12 @@ TEST_F(FrameFetchContextHintsTest, MonitorSomeHintsPermissionsPolicy) {
 #if defined(OS_ANDROID)
   ExpectHeader("https://www.someother-example.com/1.gif", "Device-Memory", true,
                "4");
-  ExpectHeader("https://www.someother-example.com/1.gif",
-               "Sec-CH-Device-Memory", true, "4");
 #else
   ExpectHeader("https://www.someother-example.com/1.gif", "Device-Memory",
                false, "");
+#endif
   ExpectHeader("https://www.someother-example.com/1.gif",
                "Sec-CH-Device-Memory", false, "");
-#endif
   // `Sec-CH-UA` is special.
   ExpectHeader("https://www.example.net/1.gif", "Sec-CH-UA", true, "");
 
@@ -1112,11 +1108,10 @@ TEST_F(FrameFetchContextHintsTest, MonitorSomeHintsPermissionsPolicy) {
   // Policy delegation.
 #if defined(OS_ANDROID)
   ExpectHeader("https://www.example.net/1.gif", "DPR", true, "1");
-  ExpectHeader("https://www.example.net/1.gif", "Sec-CH-DPR", true, "1");
 #else
   ExpectHeader("https://www.example.net/1.gif", "DPR", false, "");
-  ExpectHeader("https://www.example.net/1.gif", "Sec-CH-DPR", false, "");
 #endif
+  ExpectHeader("https://www.example.net/1.gif", "Sec-CH-DPR", false, "");
   ExpectHeader("https://www.example.net/1.gif", "Sec-CH-UA-Arch", false, "");
   ExpectHeader("https://www.example.net/1.gif", "Sec-CH-UA-Platform-Version",
                false, "");
