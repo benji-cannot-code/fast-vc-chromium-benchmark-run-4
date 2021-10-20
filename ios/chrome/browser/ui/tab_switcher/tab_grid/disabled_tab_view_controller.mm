d5d8e2d8c853c27366792d2414cfefb711157391
Tina Wang <tinazwang@google.com>
2021-10-20 19:09:43 +0000

    [ios] Add a helper method for height constraint
    Add help method for height constraint to constraints_ui_util.
    They will be used by DisabledTabViewController and
    TabGridEmptyStateView.
    
    This is a follow up cl of
    https://chromium-review.googlesource.com/c/chromium/src/+/2683714
    
    Bug: 1184079
    Change-Id: I6e1bf0d88ccc816f41fe90e8e449edee81c40024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229143
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933576}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/disabled_tab_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/disabled_tab_view_controller.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/string_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "net/base/mac/url_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -130,13 +131,9 @@ NSAttributedString* GetBodyString(TabGridPage page) {
   [scrollView addSubview:container];
   [self.view addSubview:scrollView];
 
-  NSLayoutConstraint* scrollViewHeightConstraint = [scrollView.heightAnchor
-      constraintEqualToAnchor:container.heightAnchor
-                     constant:(self.scrollViewContentInsets.top +
-                               self.scrollViewContentInsets.bottom)];
-  scrollViewHeightConstraint.priority = UILayoutPriorityDefaultLow;
-  scrollViewHeightConstraint.active = YES;
-  self.scrollViewHeight = scrollViewHeightConstraint;
+  self.scrollViewHeight = VerticalConstraintsWithInset(
+      container, scrollView,
+      self.scrollViewContentInsets.top + self.scrollViewContentInsets.bottom);
 
   [NSLayoutConstraint activateConstraints:@[
     [topLabel.topAnchor constraintEqualToAnchor:container.topAnchor
