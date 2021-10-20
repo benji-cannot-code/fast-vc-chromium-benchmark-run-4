c1a934399d22b706410d7c09c4e3fcd9766a649c
Daniel Hosseinian <dhoss@chromium.org>
2021-10-20 07:08:46 +0000

    [unseasoned-pdf] Isolate PDF content with data: URLs from the extension
    PDF content loaded inside the PDF viewer extension with a data: URL
    causes it to share a SiteInstance with the extension. Add an extra check
    in RenderFrameHostManager::CanUseSourceSiteInstance() to prevent this.
    
    Add security checks in the renderer when the in-process plugin is being
    created to catch similar regressions.
    
    Add browser tests for process isolation.
    
    Fixed: 1259635
    Change-Id: I3bc6252a1d83930e6a71832b7ae5063b47a289bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227467
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933366}

--- components/pdf/renderer/internal_plugin_renderer_helpers.cc
+++ components/pdf/renderer/internal_plugin_renderer_helpers.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/check.h"
 #include "base/feature_list.h"
 #include "components/pdf/renderer/pdf_internal_plugin_delegate.h"
 #include "components/pdf/renderer/pdf_view_web_plugin_client.h"
@@ -48,13 +49,23 @@ blink::WebPlugin* CreateInternalPlugin(
   }
 
   // The in-process plugin should only be created if the parent frame has an
-  // allowed origin.
-  blink::WebFrame* parent_frame = render_frame->GetWebFrame()->Parent();
+  // origin allowed to hold an internal plugin.
+  blink::WebFrame* frame = render_frame->GetWebFrame();
+  blink::WebFrame* parent_frame = frame->Parent();
   if (!parent_frame)
     return nullptr;
 
   if (!delegate->IsAllowedOrigin(parent_frame->GetSecurityOrigin()))
     return nullptr;
+
+  // The plugin should always be externally handled from origins allowed to
+  // hold the internal plugin.
+  DCHECK(!delegate->IsAllowedOrigin(frame->GetSecurityOrigin()));
+
+  // The internal plugin frame must not share a process with the parent frame.
+  // This could potentially happen if the source SiteInstance is incorrectly
+  // reused (see crbug.com/1259635).
+  CHECK(parent_frame->IsWebRemoteFrame());
 
   mojo::AssociatedRemote<pdf::mojom::PdfService> pdf_service_remote;
   render_frame->GetRemoteAssociatedInterfaces()->GetInterface(
