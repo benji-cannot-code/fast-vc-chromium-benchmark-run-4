f655ddbf585a55590509066d204e07e8b7f1b7e7
David Tseng <dtseng@google.com>
2021-10-20 21:25:06 +0000

    Exclude icon classification from Fuchsia
    R=rohpavone@chromium.org
    
    Fixed: 1261684
    Change-Id: Ifd39462f545a1a6460ad12309acb28f258d7d751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235528
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Rohan Pavone <rohpavone@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933639}

--- content/renderer/accessibility/ax_image_annotator.cc
+++ content/renderer/accessibility/ax_image_annotator.cc
@@ -40,7 +40,7 @@ namespace content {
 namespace {
 
 int GetMessageIdForIconEnum(const std::string& icon_type) {
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_FUCHSIA)
   return 0;
 #else
   static constexpr auto kIconTypeToMessageIdMap =
