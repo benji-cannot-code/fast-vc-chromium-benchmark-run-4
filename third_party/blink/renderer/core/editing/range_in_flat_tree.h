75b431ed1448b67818d35864ad7722bdca9ed4ae
David Bokan <bokan@chromium.org>
2021-10-20 22:47:15 +0000

    Add API for dealing with fragment directives
    This CL implements, behind a flag, the fragment directive API described
    in https://github.com/WICG/scroll-to-text-fragment/issues/160, allowing
    authors to read back directives specified in the :~: portion of the URL
    fragment, as well as create text-directives URLs using a Range or
    Selection.
    
    Because the feature was previously specific to text directives, the
    parsing of the URL fragment directive is currently spread across
    Document, TextFragmentAnchor and TextFragmentSelector. The semantics of
    the new API would make it more convenient to move all this logic into
    FragmentDirective itself but this is a sizeable change so we do this in
    a separate CL: https://crrev.com/c/3216206.
    
    Bug: 1214791
    Change-Id: I189363a5fe9843ac1e55c7078609d61aa33bc26d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209166
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933676}

--- third_party/blink/renderer/core/editing/range_in_flat_tree.h
+++ third_party/blink/renderer/core/editing/range_in_flat_tree.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // This is a wrapper class for a range in flat tree that is relocatable by
-// relacating the start and end positions in DOM tree.
+// relocating the start and end positions in DOM tree.
 class CORE_EXPORT RangeInFlatTree final
     : public GarbageCollected<RangeInFlatTree> {
  public:
--- third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.h
+++ third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.h
@@ -23,6 +23,7 @@ namespace blink {
 class DocumentLoader;
 class LocalFrame;
 class KURL;
+class TextDirective;
 
 constexpr char kFragmentDirectivePrefix[] = ":~:";
 // Subtract 1 because base::size includes the \0 string terminator.
@@ -108,6 +109,7 @@ class CORE_EXPORT TextFragmentAnchor final : public FragmentAnchor,
   bool HasSearchEngineSource();
 
   HeapVector<Member<TextFragmentFinder>> text_fragment_finders_;
+  HeapVector<Member<TextDirective>> dom_text_directives_;
 
   Member<LocalFrame> frame_;
 
@@ -140,6 +142,8 @@ class CORE_EXPORT TextFragmentAnchor final : public FragmentAnchor,
   // Whether we performed a non-zero scroll to scroll a match into view. Used
   // to determine whether the user subsequently scrolls back to the top.
   bool did_non_zero_scroll_ = false;
+  // Whether PerformPreRafActions should run at the next rAF.
+  bool needs_perform_pre_raf_actions_ = false;
 
   // Whether a text fragment finder was run.
   bool has_performed_first_text_search_ = false;
