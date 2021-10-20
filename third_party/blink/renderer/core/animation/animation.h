5da72396ebb40f0cf6c96d0b62f6365098f0733d
Jordan Taylor <jortaylo@microsoft.com>
2021-10-20 22:36:05 +0000

    Change effect phase calculation to be end inclusive for scroll animation
    This fixes a problem where when scrolled all the way to the end of a
    scroller, the animation would "finish" which meant the effect was no
    longer applied, which is strange behavior for scroll based animations.
    
    This adds an additional condition to the effect phase calculation that
    allows it to account for whether or not it is at a timeline boundary
    point.
    
    I also added a few test cases related to phases and refactored the other
    parts of the effect phase calculation to be more in line with the spec.
    
    Bug: 1079300
    Change-Id: I391df62da425b737500af6b66d83b596e7295e91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3176729
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Jordan Taylor <jortaylo@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#933672}

--- third_party/blink/renderer/core/animation/animation.h
+++ third_party/blink/renderer/core/animation/animation.h
@@ -314,6 +314,7 @@ class CORE_EXPORT Animation : public EventTargetWithInlineData,
     return compositor_property_animations_have_no_effect_;
   }
   bool AnimationHasNoEffect() const { return animation_has_no_effect_; }
+  bool AtScrollTimelineBoundary();
 
  protected:
   DispatchEventResult DispatchEventInternal(Event&) override;
--- third_party/blink/renderer/modules/animationworklet/worklet_animation.cc
+++ third_party/blink/renderer/modules/animationworklet/worklet_animation.cc
@@ -422,8 +422,8 @@ void WorkletAnimation::cancel() {
   // update the value in the next frame.
   if (IsActive(play_state_)) {
     for (auto& effect : effects_) {
-      effect->UpdateInheritedTime(absl::nullopt, absl::nullopt, playback_rate_,
-                                  kTimingUpdateOnDemand);
+      effect->UpdateInheritedTime(absl::nullopt, absl::nullopt, false,
+                                  playback_rate_, kTimingUpdateOnDemand);
     }
   }
   SetPlayState(Animation::kIdle);
@@ -509,7 +509,7 @@ void WorkletAnimation::Update(TimingUpdateReason reason) {
         local_times_[i]
             ? absl::make_optional(AnimationTimeDelta(local_times_[i].value()))
             : absl::nullopt,
-        absl::nullopt, playback_rate_, reason);
+        absl::nullopt, false, playback_rate_, reason);
   }
 }
 
