af51610ebf2a351b19adce795bb20e0ebbe53cc0
Takashi Toyoshima <toyoshim@chromium.org>
2021-10-20 21:36:23 +0000

    Kaleidoscope: Remove unused base::Feature flags and menu items
    The feature was already turned down by
    https://chromium-review.googlesource.com/c/chromium/src/+/2596439,
    and some base::Features and menu item handling code are remaining.
    
    This patch cleans them up all.
    
    Change-Id: If6afebb6d657e4384506f5d80b1ad8737fcb624c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233541
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933642}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -225,7 +225,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_SHOW_FULL_URLS             40259
 #define IDC_CARET_BROWSING_TOGGLE      40260
 #define IDC_TOGGLE_COMMANDER     40261
-#define IDC_SHOW_KALEIDOSCOPE          40262
 #define IDC_CHROME_TIPS                40263
 #define IDC_CHROME_WHATS_NEW           40264
 
