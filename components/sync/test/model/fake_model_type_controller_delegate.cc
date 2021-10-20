70955f45d5be470ae5a2a562fd16159a5206c492
Mikel Astiz <mastiz@chromium.org>
2021-10-20 08:47:25 +0000

    [sync] Allow controller delegates to skip engine connection
    Prior to this patch, sync controllers (subclasses of DataTypeController)
    have the ability to skip a full activation (that is, "connecting" or
    enabling the datatype in the sync engine via
    SyncEngineImpl::ConnectDataType), which was in fact leveraged by
    ProxyTabsDataTypeController. This approach was somewhat subtle, because
    the controller skipped the MODEL_LOADED state and transitioned directly
    to RUNNING, to effectively trick DataTypeManagerImpl and avoid
    SyncEngineImpl::ConnectDataType.
    
    This patch redesigns this approach by instead introducing an explicit
    boolean in DataTypeActivationResponse, which is specifically honored
    in DataTypeManagerImpl without otherwise subtly bypassing any states in
    the controller's state machine.
    
    A side effect of this, and in fact the motivating reason, is that this
    allows controller *delegates* (ModelTypeControllerDelegate) to also
    influence this behavior. This extra flexibility will be leveraged in
    future patches to avoid sparse feature-guarded logic across controller
    and model (controller delegate).
    
    Change-Id: I7b2d2d60ad5a535d4341e1f0e48b3c6e9a833518
    Bug: 1260837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229275
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933387}

--- components/sync/test/model/fake_model_type_controller_delegate.cc
+++ components/sync/test/model/fake_model_type_controller_delegate.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/run_loop.h"
-#include "components/sync/engine/data_type_activation_response.h"
 #include "components/sync/model/data_type_activation_request.h"
 #include "components/sync/model/type_entities_count.h"
 
@@ -22,7 +21,12 @@ FakeModelTypeControllerDelegate::~FakeModelTypeControllerDelegate() = default;
 
 void FakeModelTypeControllerDelegate::SetModelTypeStateForActivationResponse(
     const sync_pb::ModelTypeState& model_type_state) {
-  model_type_state_for_activation_response_ = model_type_state;
+  activation_response_.model_type_state = model_type_state;
+}
+
+void FakeModelTypeControllerDelegate::
+    EnableSkipEngineConnectionForActivationResponse() {
+  activation_response_.skip_engine_connection = true;
 }
 
 void FakeModelTypeControllerDelegate::EnableManualModelStart() {
@@ -105,7 +109,9 @@ FakeModelTypeControllerDelegate::GetWeakPtr() {
 std::unique_ptr<DataTypeActivationResponse>
 FakeModelTypeControllerDelegate::MakeActivationResponse() const {
   auto response = std::make_unique<DataTypeActivationResponse>();
-  response->model_type_state = model_type_state_for_activation_response_;
+  response->model_type_state = activation_response_.model_type_state;
+  response->skip_engine_connection =
+      activation_response_.skip_engine_connection;
   return response;
 }
 
--- components/sync/test/model/fake_model_type_controller_delegate.h
+++ components/sync/test/model/fake_model_type_controller_delegate.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "components/sync/base/model_type.h"
+#include "components/sync/engine/data_type_activation_response.h"
 #include "components/sync/model/model_error.h"
 #include "components/sync/model/model_type_controller_delegate.h"
 #include "components/sync/protocol/model_type_state.pb.h"
@@ -28,8 +29,14 @@ class FakeModelTypeControllerDelegate : public ModelTypeControllerDelegate {
 
   ~FakeModelTypeControllerDelegate() override;
 
+  // Determines the ModelTypeState returned in Connect() as part of
+  // DataTypeActivationResponse.
   void SetModelTypeStateForActivationResponse(
       const sync_pb::ModelTypeState& model_type_state);
+
+  // Influences the bit |skip_engine_connection| returned in Connect() as part
+  // of DataTypeActivationResponse.
+  void EnableSkipEngineConnectionForActivationResponse();
 
   // By default, this delegate (model) completes startup automatically when
   // OnSyncStarting() is invoked. For tests that want to manually control the
@@ -64,7 +71,7 @@ class FakeModelTypeControllerDelegate : public ModelTypeControllerDelegate {
   const ModelType type_;
   bool manual_model_start_enabled_ = false;
   int clear_metadata_call_count_ = 0;
-  sync_pb::ModelTypeState model_type_state_for_activation_response_;
+  DataTypeActivationResponse activation_response_;
   absl::optional<ModelError> model_error_;
   StartCallback start_callback_;
   ModelErrorHandler error_handler_;
