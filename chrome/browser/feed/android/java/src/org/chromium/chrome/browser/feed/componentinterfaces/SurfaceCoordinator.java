013ed641f818fb16da60bc347a68fa831a4b2944
Pete Williamson <petewil@chromium.org>
2021-10-20 22:51:33 +0000

    Web Feed - Turn the feed back on if the user follows a page.
    If the feed is turned off, and the user follows a page, then turn the feed
    back on, and set it to come up in the "Following" tab the next time the
    user visits the NTP.
    
    Bug: b:202757270
    Change-Id: Idc60f480faf9a7123405350290ada8f0bfb9641e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232472
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933677}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/componentinterfaces/SurfaceCoordinator.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/componentinterfaces/SurfaceCoordinator.java
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.feed.componentinterfaces;
 
+import androidx.annotation.IntDef;
+
 /** Interface for referencing FeedSurfaceCoordinator in this library. */
 public interface SurfaceCoordinator {
     /** Observes the SurfaceCoordinator. */
@@ -16,4 +18,11 @@ public interface SurfaceCoordinator {
     void onSurfaceClosed();
     void onSurfaceOpened();
     boolean isActive();
+
+    @IntDef({StreamTabId.DEFAULT, StreamTabId.FOR_YOU, StreamTabId.FOLLOWING})
+    public @interface StreamTabId {
+        int DEFAULT = -1;
+        int FOR_YOU = 0;
+        int FOLLOWING = 1;
+    };
 }
