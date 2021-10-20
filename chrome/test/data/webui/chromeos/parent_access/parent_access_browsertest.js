7d115dd5c79f87ffd34f782d010aaa02bc696717
Danan S <danan@chromium.org>
2021-10-20 19:22:22 +0000

    [Parent Access] Create ParentAccessController (PostMessageAPIServer)
    This CL creates the ParentAccessController, a PostMessageAPIServer
    that communicates with the parent access web widget hosted in the
    ChromeOS Parent Access UI.
    
    
    Bug: b/199752908
    Change-Id: Ie85df7066aa95263ca1d1d7b951fca226aaae231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3137534
    Commit-Queue: Dan S <danan@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933581}

---
+++ chrome/test/data/webui/chromeos/parent_access/parent_access_browsertest.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Runs the Parent Access flow tests. */
+
+GEN('#include "content/public/test/browser_test.h"');
+
+var ParentAccessControllerTest = class extends testing.Test {
+  /** @override */
+  get browsePreload() {
+    return 'chrome://parent-access/test_loader.html?module=' +
+        'chromeos/parent_access/parent_access_controller_test.js';
+  }
+
+  /** @override */
+  get isAsync() {
+    return true;
+  }
+
+  /** @param {string} testName The name of the test to run. */
+  runMochaTest(testName) {
+    runMochaTest(parent_access_controller_tests.suiteName, testName);
+  }
+};
+
+TEST_F('ParentAccessControllerTest', 'ParentAccessResultFnCalled', function() {
+  this.runMochaTest(
+      parent_access_controller_tests.TestNames.ParentAccessResultFnCalled);
+});
---
+++ chrome/test/data/webui/chromeos/parent_access/parent_access_test_client.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PostMessageAPIClient} from 'chrome://resources/js/post_message_api_client.m.js';
+
+
+const serverOriginURLFilter = 'chrome://parent-access/';
+
+class TestParentAccessAPIClient extends PostMessageAPIClient {
+  constructor() {
+    super(serverOriginURLFilter, null);
+  }
+
+  /**
+   * @override
+   */
+  onInitialized() {
+    this.callApiFn('onParentAccessResult', ['1234567890']);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const parentAccessTestClient = new TestParentAccessAPIClient();
+});
