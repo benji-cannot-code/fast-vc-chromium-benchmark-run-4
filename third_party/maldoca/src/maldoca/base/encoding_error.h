e7129976267cb5ab6353de45a51770521a529878
Olabode Anise <anise@chromium.org>
2021-10-20 21:03:19 +0000

    Fix glibc dependency addition by using static keyword
    Previously, encoding_error.h used |thread_local| which was unnecessary
    because a new sandboxed process is created each time a document is
    scanned. Changing to static to avoid bringing in new glibc version.
    
    Bug: 1261617
    Change-Id: I4cb5714224d467f6baaf66bcc0cbd742a0250c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235432
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Olabode O. Anise II <anise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933631}

--- third_party/maldoca/src/maldoca/base/encoding_error.h
+++ third_party/maldoca/src/maldoca/base/encoding_error.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace maldoca {
 static inline std::string& GetFailedEncoding() {
-  thread_local std::string failed_encoding;
+  static std::string failed_encoding;
   return failed_encoding;
 }
 
