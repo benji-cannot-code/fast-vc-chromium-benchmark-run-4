6d82f577519586dba8c8f19a98b01746d20e7a9d
Moe Ahmadi <mahmadi@chromium.org>
2021-10-20 15:40:52 +0000

    [memories] Shows header and empty state message appropriately
    - Shows header and iff both query and list of clusters are non-empty
    - Shows placeholder empty state message when there are no clusters.
    - Scrolls to the top only when a new set of clusters is received.
    - s/Cluster/cluster
    
    screenshot/PcECEDXiFzx76qB
    screenshot/3FFNiJGteYVGeSR
    
    Bug: 1261236
    Change-Id: Iff077103d15b771db2da7df9253eb45fefc7a579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229968
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933479}

--- chrome/browser/resources/history/history_clusters/shared_style.ts
+++ chrome/browser/resources/history/history_clusters/shared_style.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '../shared_style.js';
 import './shared_vars.js';
 import 'chrome://resources/cr_elements/hidden_style_css.m.js';
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
