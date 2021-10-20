8864dd756de38962d27f259764ca1289a59f2418
dpapad <dpapad@chromium.org>
2021-10-20 16:45:06 +0000

    Settings: Hide external icon when proxy controlled by extension.
    The external icon button was a no-op and should not be shown, unless the
    proxy settings are not controlled neither by an extension or a policy.
    
    Fixed: 1236057
    Change-Id: I658781921fc743e30d690ff03b661846eb48c533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234397
    Auto-Submit: dpapad <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933505}

--- chrome/test/data/webui/settings/system_page_tests.js
+++ chrome/test/data/webui/settings/system_page_tests.js
@@ -124,7 +124,7 @@ suite('settings system page', function() {
     // settings.
     expectFalse(control.hasAttribute('actionable'));
     expectEquals(null, control.querySelector('cr-policy-pref-indicator'));
-    expectFalse(showProxyButton.hidden);
+    expectTrue(showProxyButton.hidden);
 
     systemPage.set('prefs.proxy', {
       key: 'proxy',
