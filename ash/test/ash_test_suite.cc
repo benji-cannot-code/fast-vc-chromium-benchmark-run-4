809865da6612e861c8fd0ab978272e7270f0922d
Avery Musbach <amusbach@chromium.org>
2021-10-20 16:10:50 +0000

    phone hub: Migrate images from PNG to Lottie
    Bug: 1127996
    Test: manual
    Change-Id: I5d77c85cb23277b5ec74e0af8a4b665209c3c57b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3171913
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933489}

--- ash/test/ash_test_suite.cc
+++ ash/test/ash_test_suite.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display_switches.h"
 #include "ui/gl/gl_switches.h"
 #include "ui/gl/test/gl_surface_test_support.h"
+#include "ui/lottie/resource.h"
 
 namespace ash {
 
@@ -40,6 +41,9 @@ void AshTestSuite::Initialize() {
   // Force unittests to run using en-US so if we test against string output,
   // it'll pass regardless of the system language.
   base::i18n::SetICUDefaultLocale("en_US");
+
+  ui::ResourceBundle::SetParseLottieAsStillImage(
+      &lottie::ParseLottieAsStillImage);
 
   // Load ash test resources and en-US strings; not 'common' (Chrome) resources.
   base::FilePath path;
