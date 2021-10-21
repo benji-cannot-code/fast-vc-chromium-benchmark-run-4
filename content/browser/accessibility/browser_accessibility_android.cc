f054e6a4498b4d16c6978f3dfce763c4fb0da241
Mark Schillaci <mschillaci@google.com>
2021-10-21 18:33:00 +0000

    Update accessibility attributes for date and time pickers on Android
    This CL updates some attributes for date and time pickers on Android.
    
    With this CL we update the classname assigned to the
    AccessibilityNodeInfo objects on Android for some pickers from a
    generic View to a Spinner to match expectations. We also update the
    time type pickers to expose their value as a name (text), so
    chosen values by a user are announced in Talkback.
    
    AX-Relnotes: Fixed bug where certain date or time pickers would not announce their selected value on Android.
    Bug: 1259813
    Change-Id: Icc903895578ef4125ba424c925909e6db173ba26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234658
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#933963}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -2403,6 +2403,7 @@ bool BrowserAccessibilityAndroid::ShouldExposeValueAsName() const {
   switch (GetRole()) {
     case ax::mojom::Role::kDate:
     case ax::mojom::Role::kDateTime:
+    case ax::mojom::Role::kInputTime:
       return true;
     case ax::mojom::Role::kColorWell:
       return false;
