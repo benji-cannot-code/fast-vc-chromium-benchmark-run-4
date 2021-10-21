60a48f97367faeac77f3ff6cb3be751d9791350b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-10-21 22:34:00 +0000

    Import wpt@8347f5b483e3b713443d06638d3662457107b3b0
    Using wpt-import in Chromium 37d607dea3082f8f0230bb96a80b7c79eade5af5.
    With Chromium commits locally applied on WPT:
    246215b8fb "[GridFragmentation] Support basic grid-item fragmentation."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    mek@chromium.org:
      external/wpt/webmessaging
    rego@igalia.com:
      external/wpt/css/css-ui
    
    NOAUTOREVERT=true
    R=foolip@google.com
    
    No-Export: true
    Change-Id: I0b5cdf0385446202a4e8f41b99a5580c63a5a71f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237747
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#934070}

--- third_party/blink/web_tests/external/wpt/web-locks/resources/helpers.js
+++ third_party/blink/web_tests/external/wpt/web-locks/resources/helpers.js
@@ -58,4 +58,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     });
   };
 
+  /**
+   * Request a lock and hold it until the subtest ends.
+   * @param {*} t test runner object
+   * @param {string} name lock name
+   * @param {LockOptions=} options lock options
+   * @returns
+   */
+  self.requestLockAndHold = (t, name, options = {}) => {
+    return navigator.locks.request(name, options, () => {
+      return new Promise(resolve => t.add_cleanup(resolve));
+    });
+  };
+
+  self.makePromiseAndResolveFunc = () => {
+    let resolve;
+    const promise = new Promise(r => { resolve = r; });
+    return [promise, resolve];
+  };
+
 })();
--- third_party/blink/web_tests/external/wpt/web-locks/signal.tentative.https.any.js
+++ third_party/blink/web_tests/external/wpt/web-locks/signal.tentative.https.any.js
@@ -5,12 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-function makePromiseAndResolveFunc() {
-  let resolve;
-  const promise = new Promise(r => { resolve = r; });
-  return [promise, resolve];
-}
-
 promise_test(async t => {
   const res = uniqueName(t);
 
@@ -40,8 +34,7 @@ promise_test(async t => {
   const res = uniqueName(t);
 
   // Grab a lock and hold it until this subtest completes.
-  const never_settled = new Promise(resolve => t.add_cleanup(resolve));
-  navigator.locks.request(res, lock => never_settled);
+  requestLockAndHold(t, res);
 
   const controller = new AbortController();
 
@@ -69,8 +62,7 @@ promise_test(async t => {
   const res = uniqueName(t);
 
   // Grab a lock and hold it until this subtest completes.
-  const never_settled = new Promise(resolve => t.add_cleanup(resolve));
-  navigator.locks.request(res, lock => never_settled);
+  requestLockAndHold(t, res);
 
   const controller = new AbortController();
 
@@ -199,3 +191,19 @@ promise_test(async t => {
                 'Lock released promise should not reject');
 
 }, 'Abort signaled after lock released');
+
+promise_test(async t => {
+  const res = uniqueName(t);
+
+  const controller = new AbortController();
+  const first = requestLockAndHold(t, res, { signal: controller.signal });
+  const next = navigator.locks.request(res, () => "resolved");
+  controller.abort();
+
+  await promise_rejects_dom(t, "AbortError", first, "Request should abort");
+  assert_equals(
+    await next,
+    "resolved",
+    "The next request is processed after abort"
+  );
+}, "Abort should process the next pending lock request");
