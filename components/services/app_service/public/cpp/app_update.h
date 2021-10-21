e9fa33f364b2a82776cc200e218e607e5701200f
Nancy Wang <nancylingwang@chromium.org>
2021-10-21 04:49:13 +0000

    Rename state and delta in AppUpdate
    This is the preparation to remove mojom in AppService. Rename state_
    delta_ in AppUpdate, so that we can add non-mojom App struct in
    AppUpdate to let AppUpdate represent for both mojom and non-mojom App
    struct.
    
    This CL is pure refactor, and no function change.
    
    BUG=1253250
    
    Change-Id: I65625966529d82841098b3b758cdc1159251c6cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235727
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933765}

--- components/services/app_service/public/cpp/app_update.h
+++ components/services/app_service/public/cpp/app_update.h
@@ -45,6 +45,8 @@ namespace apps {
 // remain valid for the lifetime of the AppUpdate.
 //
 // See components/services/app_service/README.md for more details.
+//
+// TODO(crbug.com/1253250): Remove all apps::mojom related code.
 class COMPONENT_EXPORT(APP_UPDATE) AppUpdate {
  public:
   // Modifies |state| by copying over all of |delta|'s known fields: those
@@ -156,8 +158,8 @@ class COMPONENT_EXPORT(APP_UPDATE) AppUpdate {
   const ::AccountId& AccountId() const;
 
  private:
-  const apps::mojom::App* state_;
-  const apps::mojom::App* delta_;
+  const apps::mojom::App* mojom_state_;
+  const apps::mojom::App* mojom_delta_;
 
   const ::AccountId& account_id_;
 };
