41a019d8e9a26ab965cc2df09d79339925339f5c
Brian Begnoche <bcb@chromium.org>
2021-10-21 14:37:41 +0000

    Replace deprecated ListValue::GetString() in chrome/browser/ui/webui/settings.
    Bug: 1187090
    Change-Id: Iae2747922b712d649e75a24329117f8b14d46316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3212216
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Brian Begnoche <bcb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933887}

--- chrome/browser/ui/webui/settings/site_settings_handler.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler.cc
@@ -661,9 +661,7 @@ void SiteSettingsHandler::OnZoomLevelChanged(
 void SiteSettingsHandler::HandleFetchUsageTotal(const base::ListValue* args) {
   AllowJavascript();
   CHECK_EQ(1U, args->GetList().size());
-  std::string host;
-  CHECK(args->GetString(0, &host));
-  usage_host_ = host;
+  usage_host_ = args->GetList()[0].GetString();
 
   update_site_details_ = true;
   if (cookies_tree_model_ && !send_sites_list_) {
@@ -675,8 +673,7 @@ void SiteSettingsHandler::HandleFetchUsageTotal(const base::ListValue* args) {
 
 void SiteSettingsHandler::HandleClearUsage(const base::ListValue* args) {
   CHECK_EQ(1U, args->GetList().size());
-  std::string origin;
-  CHECK(args->GetString(0, &origin));
+  const std::string& origin = args->GetList()[0].GetString();
   GURL url(origin);
   if (!url.is_valid())
     return;
@@ -692,10 +689,8 @@ void SiteSettingsHandler::HandleClearUsage(const base::ListValue* args) {
 void SiteSettingsHandler::HandleSetDefaultValueForContentType(
     const base::ListValue* args) {
   CHECK_EQ(2U, args->GetList().size());
-  std::string content_type;
-  CHECK(args->GetString(0, &content_type));
-  std::string setting;
-  CHECK(args->GetString(1, &setting));
+  const std::string& content_type = args->GetList()[0].GetString();
+  const std::string& setting = args->GetList()[1].GetString();
   ContentSetting default_setting;
   CHECK(content_settings::ContentSettingFromString(setting, &default_setting));
   ContentSettingsType type =
@@ -732,8 +727,7 @@ void SiteSettingsHandler::HandleGetDefaultValueForContentType(
 
   CHECK_EQ(2U, args->GetList().size());
   const base::Value& callback_id = args->GetList()[0];
-  std::string type;
-  CHECK(args->GetString(1, &type));
+  const std::string& type = args->GetList()[1].GetString();
 
   ContentSettingsType content_type =
       site_settings::ContentSettingsTypeFromGroupName(type);
@@ -951,8 +945,7 @@ void SiteSettingsHandler::HandleGetExceptionList(const base::ListValue* args) {
 
   CHECK_EQ(2U, args->GetList().size());
   const base::Value& callback_id = args->GetList()[0];
-  std::string type;
-  CHECK(args->GetString(1, &type));
+  const std::string& type = args->GetList()[1].GetString();
   ContentSettingsType content_type =
       site_settings::ContentSettingsTypeFromGroupName(type);
 
@@ -987,8 +980,7 @@ void SiteSettingsHandler::HandleGetChooserExceptionList(
 
   CHECK_EQ(2U, args->GetList().size());
   const base::Value& callback_id = args->GetList()[0];
-  std::string type;
-  CHECK(args->GetString(1, &type));
+  const std::string& type = args->GetList()[1].GetString();
   const site_settings::ChooserTypeNameEntry* chooser_type =
       site_settings::ChooserTypeFromGroupName(type);
   CHECK(chooser_type);
@@ -1122,14 +1114,10 @@ void SiteSettingsHandler::HandleSetOriginPermissions(
 void SiteSettingsHandler::HandleResetCategoryPermissionForPattern(
     const base::ListValue* args) {
   CHECK_EQ(4U, args->GetList().size());
-  std::string primary_pattern_string;
-  CHECK(args->GetString(0, &primary_pattern_string));
-  std::string secondary_pattern_string;
-  CHECK(args->GetString(1, &secondary_pattern_string));
-  std::string type;
-  CHECK(args->GetString(2, &type));
-  CHECK(args->GetList()[3].is_bool());
-  bool incognito = args->GetList()[3].GetBool();
+  const std::string& primary_pattern_string = args->GetList()[0].GetString();
+  const std::string& secondary_pattern_string = args->GetList()[1].GetString();
+  const std::string& type = args->GetList()[2].GetString();
+  const bool& incognito = args->GetList()[3].GetBool();
 
   ContentSettingsType content_type =
       site_settings::ContentSettingsTypeFromGroupName(type);
@@ -1186,16 +1174,11 @@ void SiteSettingsHandler::HandleResetCategoryPermissionForPattern(
 void SiteSettingsHandler::HandleSetCategoryPermissionForPattern(
     const base::ListValue* args) {
   CHECK_EQ(5U, args->GetList().size());
-  std::string primary_pattern_string;
-  CHECK(args->GetString(0, &primary_pattern_string));
-  std::string secondary_pattern_string;
-  CHECK(args->GetString(1, &secondary_pattern_string));
-  std::string type;
-  CHECK(args->GetString(2, &type));
-  std::string value;
-  CHECK(args->GetString(3, &value));
-  CHECK(args->GetList()[4].is_bool());
-  bool incognito = args->GetList()[4].GetBool();
+  const std::string& primary_pattern_string = args->GetList()[0].GetString();
+  const std::string& secondary_pattern_string = args->GetList()[1].GetString();
+  const std::string& type = args->GetList()[2].GetString();
+  const std::string& value = args->GetList()[3].GetString();
+  const bool& incognito = args->GetList()[4].GetBool();
 
   ContentSettingsType content_type =
       site_settings::ContentSettingsTypeFromGroupName(type);
@@ -1259,19 +1242,16 @@ void SiteSettingsHandler::HandleResetChooserExceptionForSite(
     const base::ListValue* args) {
   CHECK_EQ(4U, args->GetList().size());
 
-  std::string chooser_type_str;
-  CHECK(args->GetString(0, &chooser_type_str));
+  const std::string& chooser_type_str = args->GetList()[0].GetString();
   const site_settings::ChooserTypeNameEntry* chooser_type =
       site_settings::ChooserTypeFromGroupName(chooser_type_str);
   CHECK(chooser_type);
 
-  std::string origin_str;
-  CHECK(args->GetString(1, &origin_str));
+  const std::string& origin_str = args->GetList()[1].GetString();
   GURL requesting_origin(origin_str);
   CHECK(requesting_origin.is_valid());
 
-  std::string embedding_origin_str;
-  CHECK(args->GetString(2, &embedding_origin_str));
+  const std::string& embedding_origin_str = args->GetList()[2].GetString();
   GURL embedding_origin(embedding_origin_str);
   CHECK(embedding_origin.is_valid());
 
@@ -1285,8 +1265,7 @@ void SiteSettingsHandler::HandleIsOriginValid(const base::ListValue* args) {
   AllowJavascript();
   CHECK_EQ(2U, args->GetList().size());
   const base::Value& callback_id = args->GetList()[0];
-  std::string origin_string;
-  CHECK(args->GetString(1, &origin_string));
+  const std::string& origin_string = args->GetList()[1].GetString();
 
   ResolveJavascriptCallback(callback_id,
                             base::Value(GURL(origin_string).is_valid()));
@@ -1297,10 +1276,8 @@ void SiteSettingsHandler::HandleIsPatternValidForType(
   AllowJavascript();
   CHECK_EQ(3U, args->GetList().size());
   const base::Value& callback_id = args->GetList()[0];
-  std::string pattern_string;
-  CHECK(args->GetString(1, &pattern_string));
-  std::string type;
-  CHECK(args->GetString(2, &type));
+  const std::string& pattern_string = args->GetList()[1].GetString();
+  const std::string& type = args->GetList()[2].GetString();
 
   std::string reason;
   bool is_valid =
@@ -1407,8 +1384,7 @@ void SiteSettingsHandler::SendZoomLevels() {
 void SiteSettingsHandler::HandleRemoveZoomLevel(const base::ListValue* args) {
   CHECK_EQ(1U, args->GetList().size());
 
-  std::string origin;
-  CHECK(args->GetString(0, &origin));
+  std::string origin = args->GetList()[0].GetString();
 
   if (origin ==
       l10n_util::GetStringUTF8(IDS_ZOOMLEVELS_CHROME_ERROR_PAGES_LABEL)) {
@@ -1583,8 +1559,7 @@ void SiteSettingsHandler::GetOriginCookies(
 void SiteSettingsHandler::HandleClearEtldPlus1DataAndCookies(
     const base::ListValue* args) {
   CHECK_EQ(1U, args->GetList().size());
-  std::string etld_plus1_string;
-  CHECK(args->GetString(0, &etld_plus1_string));
+  const std::string& etld_plus1_string = args->GetList()[0].GetString();
 
   AllowJavascript();
   CookieTreeNode* parent = cookies_tree_model_->GetRoot();
