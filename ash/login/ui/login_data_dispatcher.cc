cebfd7fdb1b7a6e4d08c7d4ba3b8d551a0256d9d
Brittany Hartmire <bhartmire@google.com>
2021-10-21 17:24:49 +0000

    [SmartLock] Add Plumbing for UI Revamp State Management
    The UI Revamp requires adding plumbing from EasyUnlockService to
    SmartLockAuthModel.
    
    - Add plumbing for LoginScreenModel::SetSmartLockState() ->
      LoginDataDispatcher::SetSmartLockState() override ->
      LoginDataDispatcher::Observer::OnSmartLockStateChanged() ->
      LockContentsView::OnSmartLockStateChanged() override ->
      LoginAuthUserView::SetSmartLockState()
    - Add plumbing for LoginScreenModel::NotifySmartLockAuthResult() ->
      LoginDataDispatcher::NotifySmartLockAuthResult() override ->
      LoginDataDispatcher::Observer::OnSmartLockAuthResult() ->
      LockContentsView::OnSmartLockAuthResult() override ->
      LoginAuthUserView::NotifySmartLockAuthResult()
    - Add new SetSmartLockState() and NotifySmartLockAuthResult() methods
      to TestLoginScreenModel.
    
    Design doc: go/cros-smartlock-ui-revamp
    
    Bug: 1233614
    Change-Id: I40ebf7b3e24fccd861fb2d1a5a1942e9b0cb8467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235867
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933932}

--- ash/login/ui/login_data_dispatcher.cc
+++ ash/login/ui/login_data_dispatcher.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/login/ui/login_data_dispatcher.h"
+#include "ash/constants/ash_features.h"
 
 namespace ash {
 
@@ -29,6 +30,14 @@ void LoginDataDispatcher::Observer::OnFingerprintStateChanged(
     FingerprintState state) {}
 
 void LoginDataDispatcher::Observer::OnFingerprintAuthResult(
+    const AccountId& account_id,
+    bool successful) {}
+
+void LoginDataDispatcher::Observer::OnSmartLockStateChanged(
+    const AccountId& user,
+    SmartLockState state) {}
+
+void LoginDataDispatcher::Observer::OnSmartLockAuthResult(
     const AccountId& account_id,
     bool successful) {}
 
@@ -129,16 +138,16 @@ void LoginDataDispatcher::SetChallengeResponseAuthEnabledForUser(
     observer.OnChallengeResponseAuthEnabledForUserChanged(user, enabled);
 }
 
-void LoginDataDispatcher::SetFingerprintState(const AccountId& account_id,
-                                              FingerprintState state) {
-  for (auto& observer : observers_)
-    observer.OnFingerprintStateChanged(account_id, state);
-}
-
 void LoginDataDispatcher::SetAvatarForUser(const AccountId& account_id,
                                            const UserAvatar& avatar) {
   for (auto& observer : observers_)
     observer.OnUserAvatarChanged(account_id, avatar);
+}
+
+void LoginDataDispatcher::SetFingerprintState(const AccountId& account_id,
+                                              FingerprintState state) {
+  for (auto& observer : observers_)
+    observer.OnFingerprintStateChanged(account_id, state);
 }
 
 void LoginDataDispatcher::NotifyFingerprintAuthResult(
@@ -146,6 +155,12 @@ void LoginDataDispatcher::NotifyFingerprintAuthResult(
     bool successful) {
   for (auto& observer : observers_)
     observer.OnFingerprintAuthResult(account_id, successful);
+}
+
+void LoginDataDispatcher::NotifySmartLockAuthResult(const AccountId& account_id,
+                                                    bool successful) {
+  for (auto& observer : observers_)
+    observer.OnSmartLockAuthResult(account_id, successful);
 }
 
 void LoginDataDispatcher::EnableAuthForUser(const AccountId& account_id) {
@@ -186,8 +201,17 @@ void LoginDataDispatcher::SetLockScreenNoteState(mojom::TrayActionState state) {
 void LoginDataDispatcher::ShowEasyUnlockIcon(
     const AccountId& user,
     const EasyUnlockIconInfo& icon_info) {
+  if (base::FeatureList::IsEnabled(ash::features::kSmartLockUIRevamp))
+    return;
+
   for (auto& observer : observers_)
     observer.OnShowEasyUnlockIcon(user, icon_info);
+}
+
+void LoginDataDispatcher::SetSmartLockState(const AccountId& user,
+                                            SmartLockState state) {
+  for (auto& observer : observers_)
+    observer.OnSmartLockStateChanged(user, state);
 }
 
 void LoginDataDispatcher::UpdateWarningMessage(const std::u16string& message) {
--- ash/login/ui/login_data_dispatcher.h
+++ ash/login/ui/login_data_dispatcher.h
@@ -68,6 +68,14 @@ class ASH_EXPORT LoginDataDispatcher : public LoginScreenModel {
     virtual void OnFingerprintAuthResult(const AccountId& account_id,
                                          bool successful);
 
+    // Called when smart lock state is changed.
+    virtual void OnSmartLockStateChanged(const AccountId& user,
+                                         SmartLockState state);
+
+    // Called after a smart lock authentication attempt.
+    virtual void OnSmartLockAuthResult(const AccountId& account_id,
+                                       bool successful);
+
     // Called when auth should be enabled for |user|. By default, auth should be
     // enabled.
     virtual void OnAuthEnabledForUser(const AccountId& user);
@@ -94,7 +102,9 @@ class ASH_EXPORT LoginDataDispatcher : public LoginScreenModel {
     // Called when the lock screen note state changes.
     virtual void OnLockScreenNoteStateChanged(mojom::TrayActionState state);
 
-    // Called when an easy unlock icon should be displayed.
+    // TODO(https://crbug.com/1233614): Delete this method in favor of
+    // OnSmartLockStateChanged once SmartLock UI revamp is enabled. Called when
+    // an easy unlock icon should be displayed.
     virtual void OnShowEasyUnlockIcon(const AccountId& user,
                                       const EasyUnlockIconInfo& icon_info);
 
@@ -169,12 +179,15 @@ class ASH_EXPORT LoginDataDispatcher : public LoginScreenModel {
   void SetPinEnabledForUser(const AccountId& user, bool enabled) override;
   void SetChallengeResponseAuthEnabledForUser(const AccountId& user,
                                               bool enabled) override;
-  void SetFingerprintState(const AccountId& account_id,
-                           FingerprintState state) override;
   void SetAvatarForUser(const AccountId& account_id,
                         const UserAvatar& avatar) override;
+  void SetFingerprintState(const AccountId& account_id,
+                           FingerprintState state) override;
   void NotifyFingerprintAuthResult(const AccountId& account_id,
                                    bool successful) override;
+  void SetSmartLockState(const AccountId& user, SmartLockState state) override;
+  void NotifySmartLockAuthResult(const AccountId& account_id,
+                                 bool successful) override;
   void EnableAuthForUser(const AccountId& account_id) override;
   void DisableAuthForUser(const AccountId& account_id,
                           const AuthDisabledData& auth_disabled_data) override;
@@ -185,6 +198,8 @@ class ASH_EXPORT LoginDataDispatcher : public LoginScreenModel {
                                     bool enabled) override;
   void ForceOnlineSignInForUser(const AccountId& user) override;
   void SetLockScreenNoteState(mojom::TrayActionState state);
+  // TODO(https://crbug.com/1233614): Delete ShowEasyUnlockIcon in favor of
+  // SetSmartLockState once SmartLock UI revamp is enabled.
   void ShowEasyUnlockIcon(const AccountId& user,
                           const EasyUnlockIconInfo& icon_info) override;
   void UpdateWarningMessage(const std::u16string& message) override;
