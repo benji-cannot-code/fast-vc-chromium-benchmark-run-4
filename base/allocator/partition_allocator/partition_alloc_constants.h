b56e28e55d5701d7a886e1396dc78bf285995453
Jose Dapena Paz <jdapena@igalia.com>
2021-10-21 10:59:18 +0000

    IWYU: missing include for numeric limits used in partition alloc constants.
    Fix build:
        ../../base/allocator/partition_allocator/partition_alloc_constants.h:355:43: error: ‘numeric_limits’ is not a member of ‘std’
          355 | constexpr size_t kMaxFreeableSpans = std::numeric_limits<int8_t>::max();
              |                                           ^~~~~~~~~~~~~~
        ../../base/allocator/partition_allocator/partition_alloc_constants.h:355:64: error: expected primary-expression before ‘>’ token
          355 | constexpr size_t kMaxFreeableSpans = std::numeric_limits<int8_t>::max();
              |                                                                ^
        ../../base/allocator/partition_allocator/partition_alloc_constants.h:355:67: error: ‘::max’ has not been declared; did you mean ‘std::max’?
          355 | constexpr size_t kMaxFreeableSpans = std::numeric_limits<int8_t>::max();
              |                                                                   ^~~
              |                                                                   std::max
    
    Bug: 957519
    Change-Id: I0a2f839589650901c5abf1feae5f3a3ef18fefc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3231561
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#933811}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 
 #include <algorithm>
+#include <limits>
 
 #include "base/allocator/partition_allocator/address_pool_manager_types.h"
 #include "base/allocator/partition_allocator/page_allocator_constants.h"
