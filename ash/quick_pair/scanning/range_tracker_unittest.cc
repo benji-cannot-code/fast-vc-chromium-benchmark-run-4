3282ff2ef499de01df648bc8bb1b3689dbaf18bb
Shane Fitzpatrick <shanefitz@google.com>
2021-10-21 17:31:31 +0000

    [FastPair] Add timeout to RangeTracker
    This change introduces a timeout to RangeTracker which will fire when
    the device fails to report rssi and tx power values. Without this, we
    can get into a state where we are indefinitely waiting for these.
    
    Under normal circumstances, these values should be available
    immediately, but it is feasible that some devices do not do this and
    we don't want to give an unnecessary poor UX if that's the case.
    
    Change-Id: I25a18c5af0fd10d6420af5046d75d5967e78d869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233118
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933937}

--- ash/quick_pair/scanning/range_tracker_unittest.cc
+++ ash/quick_pair/scanning/range_tracker_unittest.cc
@@ -9,12 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/test/mock_callback.h"
+#include "base/test/task_environment.h"
 #include "device/bluetooth/bluetooth_adapter.h"
+#include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/test/mock_bluetooth_adapter.h"
 #include "device/bluetooth/test/mock_bluetooth_device.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
+
+const std::string kAddress = "Test address";
 
 class FakeBluetoothAdapter
     : public testing::NiceMock<device::MockBluetoothAdapter> {
@@ -24,7 +28,7 @@ class FakeBluetoothAdapter
   }
 
  private:
-  ~FakeBluetoothAdapter() = default;
+  ~FakeBluetoothAdapter() override = default;
 };
 
 }  // namespace
@@ -40,9 +44,12 @@ class RangeTrackerTest : public testing::Test {
         static_cast<scoped_refptr<device::BluetoothAdapter>>(adapter_));
     device_ = std::make_unique<device::MockBluetoothDevice>(
         adapter_.get(),
-        /*bluetooth_class=*/0, "Test device name", "Test address",
+        /*bluetooth_class=*/0, "Test device name", kAddress,
         /*paired=*/false,
         /*connected=*/false);
+
+    ON_CALL(*(adapter_.get()), GetDevice(kAddress))
+        .WillByDefault(testing::Return(device_.get()));
   }
 
  protected:
@@ -53,6 +60,8 @@ class RangeTrackerTest : public testing::Test {
         .WillOnce(testing::Return(tx_power));
   }
 
+  base::test::TaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   std::unique_ptr<device::MockBluetoothDevice> device_;
   scoped_refptr<FakeBluetoothAdapter> adapter_;
   std::unique_ptr<RangeTracker> tracker_;
@@ -119,6 +128,12 @@ TEST_F(RangeTrackerTest, StopTracking) {
 
   tracker_->StopTracking(device_.get());
   adapter_->NotifiyDeviceChanged(device_.get());
+}
+
+TEST_F(RangeTrackerTest, Timeout) {
+  EXPECT_CALL(callback_, Run(device_.get()));
+  tracker_->Track(device_.get(), 3, callback_.Get());
+  task_environment_.FastForwardUntilNoTasksRemain();
 }
 
 }  // namespace quick_pair
