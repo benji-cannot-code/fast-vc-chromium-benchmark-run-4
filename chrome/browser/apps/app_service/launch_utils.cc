cb74974d25f4147661fd567f8f653430c94018d2
Evan Stade <estade@chromium.org>
2021-10-21 16:44:48 +0000

    dPWA: don't treat URLs as files when handling launch events.
    With the exception of file:// URLs, other URLs with schemes like
    http:// or mailto:// should not ever be treated as file launches.
    
    Bug: none
    Change-Id: I8410fe64c109a8c1a410cae94226dc041a69a88a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229812
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933913}

--- chrome/browser/apps/app_service/launch_utils.cc
+++ chrome/browser/apps/app_service/launch_utils.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "build/build_config.h"
 #include "chrome/browser/apps/app_service/file_utils.h"
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -38,13 +39,19 @@ std::vector<base::FilePath> GetLaunchFilesFromCommandLine(
     return launch_files;
   }
 
-  // Assume all args passed were intended as files to pass to the app.
   launch_files.reserve(command_line.GetArgs().size());
   for (const auto& arg : command_line.GetArgs()) {
-    base::FilePath path(arg);
-    if (path.empty()) {
+#if defined(OS_WIN)
+    GURL url(base::AsStringPiece16(arg));
+#else
+    GURL url(arg);
+#endif
+    if (url.is_valid() && !url.SchemeIsFile())
       continue;
-    }
+
+    base::FilePath path(arg);
+    if (path.empty())
+      continue;
 
     launch_files.push_back(path);
   }
