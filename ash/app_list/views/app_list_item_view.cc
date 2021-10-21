9709179a569ed316d1967b6059762db2147cb562
Wen-Chien Wang <wcwang@chromium.org>
2021-10-21 20:29:06 +0000

    Add enum value to AppListMenuModelAdapter for bubble launcher metrics
    In AppListMenuModelAdapter, the AppListViewAppType enum helps recording
    the metrics of which type of app activated the context menu. This cl
    adds new enum values to AppListMenuModelAdapter for the bubble launcher
    that considers the cases that didn't exist, such as Continue Apps.
    
    Also added a new version of Apps.ContextMenuShowSource histogram and did
    some refactoring on histogram suffix according to the guideline
    mentioned by Tricium.
    
    Bug: 1221314
    Change-Id: I07f82e87bb5912a42b5409e3486f165fc78cc2fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227588
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934008}

--- ash/app_list/views/app_list_item_view.cc
+++ ash/app_list/views/app_list_item_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/model/app_list_folder_item.h"
 #include "ash/app_list/model/app_list_item.h"
 #include "ash/app_list/views/app_list_menu_model_adapter.h"
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/app_list/app_list_color_provider.h"
 #include "ash/public/cpp/app_list/app_list_config.h"
 #include "ash/public/cpp/app_list/app_list_switches.h"
@@ -265,7 +266,8 @@ class AppListItemView::IconImageView : public views::ImageView {
 AppListItemView::AppListItemView(const AppListConfig* app_list_config,
                                  GridDelegate* grid_delegate,
                                  AppListItem* item,
-                                 AppListViewDelegate* view_delegate)
+                                 AppListViewDelegate* view_delegate,
+                                 Context context)
     : views::Button(
           base::BindRepeating(&GridDelegate::OnAppListItemViewActivated,
                               base::Unretained(grid_delegate),
@@ -274,7 +276,8 @@ AppListItemView::AppListItemView(const AppListConfig* app_list_config,
       is_folder_(item->GetItemType() == AppListFolderItem::kItemType),
       item_weak_(item),
       grid_delegate_(grid_delegate),
-      view_delegate_(view_delegate) {
+      view_delegate_(view_delegate),
+      context_(context) {
   DCHECK(app_list_config_);
   // TODO(crbug.com/1218186): Remove this, this is in place temporarily to be
   // able to submit accessibility checks. This crashes if fetching a11y node
@@ -650,9 +653,23 @@ void AppListItemView::OnContextMenuModelReceived(
       AppListLaunchedFrom::kLaunchedFromGrid};
   view_delegate_->GetAppLaunchedMetricParams(&metric_params);
 
+  // Assign the correct app type to `context_menu_` according to the parent view
+  // of the app list item view.
+  AppListMenuModelAdapter::AppListViewAppType app_type;
+  switch (context_) {
+    case Context::kAppsGridView:
+      app_type = features::IsProductivityLauncherEnabled()
+                     ? AppListMenuModelAdapter::PRODUCTIVITY_LAUNCHER_APP_GRID
+                     : AppListMenuModelAdapter::FULLSCREEN_APP_GRID;
+      break;
+    case Context::kRecentAppsView:
+      app_type = AppListMenuModelAdapter::PRODUCTIVITY_LAUNCHER_RECENT_APP;
+      break;
+  }
+
   context_menu_ = std::make_unique<AppListMenuModelAdapter>(
       item_weak_->GetMetadata()->id, std::move(menu_model), GetWidget(),
-      source_type, metric_params, AppListMenuModelAdapter::FULLSCREEN_APP_GRID,
+      source_type, metric_params, app_type,
       base::BindOnce(&AppListItemView::OnMenuClosed,
                      weak_ptr_factory_.GetWeakPtr()),
       view_delegate_->IsInTabletMode());
