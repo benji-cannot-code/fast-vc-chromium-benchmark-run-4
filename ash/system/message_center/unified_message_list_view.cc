024f15da4605585664a02eb69a3023429fac39a1
Owen Wiggins <owx@chromium.org>
2021-10-21 21:05:24 +0000

    Close Message Center Bubble asynchronously in destructor.
    The Message Center and Quick Settings UI are peers owned by the Unified
    System Tray. Previously, when all notifications in the Message Center
    were removed, the Message Center would be in an unusual state - it would
    be hidden, but still active and focused. When the Esc accelerator is
    processed by the focus manager of the Unified System Tray, it would free
    the Message Center Bubble and close the widget synchronously from the
    destructor, causing NativeWidgetAura to fail when the Window properties
    are accessed after it has been freed from the heap.
    
    This fix changes the Message Center Bubble destructor to asynchronously
    close its widget. This retains the Widget after the
    NativeWidgetAura::Hide() call in NativeWidgetAura::Close(), preventing
    the window_ pointer from becoming invalid. The remaining logic for
    next/previous focusable already handles this state correctly -it won't
    focus the Message Center with Tab or Shift+Tab.
    
    Bug: 1251351
    Change-Id: I924c222c4bbfe08ea7a3e620531cee92d70596b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3207232
    Commit-Queue: Owen Wiggins <owx@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934031}

--- ash/system/message_center/unified_message_list_view.cc
+++ ash/system/message_center/unified_message_list_view.cc
@@ -279,7 +279,8 @@ UnifiedMessageListView::UnifiedMessageListView(
 }
 
 UnifiedMessageListView::~UnifiedMessageListView() {
-  model_->ClearNotificationChanges();
+  if (model_)
+    model_->ClearNotificationChanges();
   for (auto* view : children())
     AsMVC(view)->StoreExpandedState(model_);
 }
@@ -512,6 +513,10 @@ void UnifiedMessageListView::OnNotificationSlidOut() {
   state_ = State::MOVE_DOWN;
   UpdateBounds();
   StartAnimation();
+}
+
+void UnifiedMessageListView::OnShutdown() {
+  model_ = nullptr;
 }
 
 void UnifiedMessageListView::OnNotificationUpdated(const std::string& id) {
