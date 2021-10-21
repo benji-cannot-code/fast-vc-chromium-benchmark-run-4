7fc1df39139c99a725620ffc67da1741079ece7c
Josh Hilke <jrhilke@google.com>
2021-10-21 17:08:28 +0000

    Delete event protos from old location. Point references to synced/ dir.
    BUG=b:178634580
    
    Change-Id: I68bd3f0797e4a58b5f951deea78d4c076d1c872f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224579
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933921}

--- chrome/browser/ash/policy/reporting/user_added_removed/user_added_removed_reporter.h
+++ chrome/browser/ash/policy/reporting/user_added_removed/user_added_removed_reporter.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/scoped_observation.h"
-#include "chrome/browser/ash/policy/reporting/user_added_removed/user_added_removed_record.pb.h"
 #include "chrome/browser/ash/policy/reporting/user_event_reporter_helper.h"
 #include "chrome/browser/ash/policy/status_collector/managed_session_service.h"
+#include "chrome/browser/policy/messaging_layer/proto/synced/user_added_removed_record.pb.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/reporting/client/report_queue_provider.h"
 #include "components/reporting/proto/record_constants.pb.h"
