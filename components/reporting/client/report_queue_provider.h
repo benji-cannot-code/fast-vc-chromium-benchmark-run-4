c97024d676a40b4cec696fc0a6e0b9e763895318
Patrick Monette <pmonette@chromium.org>
2021-10-21 01:32:45 +0000

    Remove temporary forward headers that were used for mass migration.
    Also contains a few last-minute header replacements.
    
    Bug: 1255932
    Change-Id: Id5fdc9c527323c2de48bdc446777aeb390835de5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235096
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Owners-Override: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933739}

--- components/reporting/client/report_queue_provider.h
+++ components/reporting/client/report_queue_provider.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/task/sequenced_task_runner_forward.h"
+#include "base/task/sequenced_task_runner.h"
 #include "components/reporting/client/report_queue.h"
 #include "components/reporting/client/report_queue_configuration.h"
 #include "components/reporting/proto/record_constants.pb.h"
