f5ae7ac8bc7cb7f76cd99fd88e14451c2d1d8175
Eric Sum <esum@google.com>
2021-10-21 00:42:46 +0000

    Add ResourceProvider to be used by SkottieWrapper.
    This will be integrated into SkottieWrapper in the next CL. It serves
    2 purposes in the context of the overall design:
    
    1) It owns/maintains a SkottieResourceMetadataMap. This will be used
    at "paint time" to identify all assets in the animation and generate a
    cc::PaintImage for each one.
    
    2) Allows the caller to set a (GPU-backed in ChromeOS's case) SkImage
    for each asset before rendering each frame in the animation.
    
    Change-Id: I4ddedacf475d48722b0c36f7164f3ee7e3286b96
    Bug: b:203584064
    Test: cc_unittests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227614
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933728}

--- cc/paint/skottie_resource_metadata.h
+++ cc/paint/skottie_resource_metadata.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
 #include "base/strings/string_piece.h"
+#include "base/types/id_type.h"
 #include "cc/paint/paint_export.h"
 
 namespace cc {
@@ -45,7 +46,10 @@ class CC_PAINT_EXPORT SkottieResourceMetadataMap {
 
 // For performance reasons, the resource_id can be hashed, and the caller can
 // circulate the resulting integer throughout the system.
-size_t CC_PAINT_EXPORT HashSkottieResourceId(base::StringPiece resource_id);
+using SkottieResourceIdHash =
+    base::IdType<SkottieResourceMetadataMap, size_t, 0>;
+SkottieResourceIdHash CC_PAINT_EXPORT
+HashSkottieResourceId(base::StringPiece resource_id);
 
 }  // namespace cc
 
