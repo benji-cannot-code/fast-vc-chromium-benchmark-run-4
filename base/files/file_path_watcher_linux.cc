f9a24c40e6e0cc92a50f748f7af5fa55da069bde
Marshall Greenblatt <marshall@chromium.org>
2021-10-21 23:34:04 +0000

    Fix crash when there aren't enough file descriptors
    FD_SETSIZE is 1024 on Linux, which is too low for Chromium-based apps
    that use a lot of file descriptors. Use poll() instead of select() to
    work around this limitation.
    
    Change-Id: I0316e2650605a0c7eb4d473f71bd367fdd4f3fce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237729
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Marshall Greenblatt <marshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934094}

--- base/files/file_path_watcher_linux.cc
+++ base/files/file_path_watcher_linux.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path_watcher.h"
 
 #include <errno.h>
+#include <poll.h>
 #include <stddef.h>
 #include <string.h>
 #include <sys/inotify.h>
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 
 #include <algorithm>
+#include <array>
 #include <fstream>
 #include <map>
 #include <memory>
@@ -274,20 +276,13 @@ LazyInstance<InotifyReader>::Leaky g_inotify_reader = LAZY_INSTANCE_INITIALIZER;
 void InotifyReaderThreadDelegate::ThreadMain() {
   PlatformThread::SetName("inotify_reader");
 
-  // Make sure the file descriptors are good for use with select().
-  CHECK_LE(0, inotify_fd_);
-  CHECK_GT(FD_SETSIZE, inotify_fd_);
+  std::array<pollfd, 1> fdarray{{{inotify_fd_, POLLIN, 0}}};
 
   while (true) {
-    fd_set rfds;
-    FD_ZERO(&rfds);
-    FD_SET(inotify_fd_, &rfds);
-
     // Wait until some inotify events are available.
-    int select_result =
-        HANDLE_EINTR(select(inotify_fd_ + 1, &rfds, nullptr, nullptr, nullptr));
-    if (select_result < 0) {
-      DPLOG(WARNING) << "select failed";
+    int poll_result = HANDLE_EINTR(poll(fdarray.data(), fdarray.size(), -1));
+    if (poll_result < 0) {
+      DPLOG(WARNING) << "poll failed";
       return;
     }
 
