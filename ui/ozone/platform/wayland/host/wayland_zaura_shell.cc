0d05a50d98242b4aa26968e8009b4360305d433c
yjliu <yjliu@chromium.org>
2021-10-21 19:32:34 +0000

    [lacros] fixed version skew between the server and the client.
    Bug: 1261254
    Change-Id: I2c3c77c65c802bd1ef1bf1205f9c0163e6443fbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233117
    Auto-Submit: Jun Liu <yjliu@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933987}

--- ui/ozone/platform/wayland/host/wayland_zaura_shell.cc
+++ ui/ozone/platform/wayland/host/wayland_zaura_shell.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
+#include "ui/ozone/common/features.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
 #include "ui/ozone/platform/wayland/host/wayland_output_manager.h"
 #include "ui/ozone/platform/wayland/host/wayland_screen.h"
@@ -58,10 +59,11 @@ WaylandZAuraShell::WaylandZAuraShell(zaura_shell* aura_shell,
       &OnActivated,
   };
   zaura_shell_add_listener(obj_.get(), &zaura_shell_listener, this);
-  if (zaura_shell_get_version(wl_object()) >=
-      ZAURA_SHELL_SURFACE_SUBMISSION_IN_PIXEL_COORDINATES_SINCE_VERSION) {
-    if (connection->surface_submission_in_pixel_coordinates())
-      zaura_shell_surface_submission_in_pixel_coordinates(obj_.get());
+  if (IsWaylandSurfaceSubmissionInPixelCoordinatesEnabled() &&
+      zaura_shell_get_version(wl_object()) >=
+          ZAURA_SHELL_SURFACE_SUBMISSION_IN_PIXEL_COORDINATES_SINCE_VERSION) {
+    zaura_shell_surface_submission_in_pixel_coordinates(obj_.get());
+    connection->set_surface_submission_in_pixel_coordinates(true);
   }
 }
 
