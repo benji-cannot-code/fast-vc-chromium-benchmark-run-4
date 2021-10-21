d5f247aed563896c47ee7fb8d028d17468e05ae5
Nancy Wang <nancylingwang@chromium.org>
2021-10-21 00:15:45 +0000

    Add non-mojom simplified app types.
    DD: https://docs.google.com/document/d/1TCWKi0rqxhWI-DT6vZHsSia7_M_8_vQ1lCwGrpdnEII
    
    This is the preparation to remove mojom in AppService. Add a simplified
    app struct to replace app::mojom::App.
    
    There will be follow up CLs to add/move more field from app::mojom::App
    to apps::App structure.
    
    BUG=1253250
    
    Change-Id: I483b82f114ad091ce19269bd2d14e9f32efa4de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219017
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#933717}

---
+++ components/services/app_service/public/cpp/app_types.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/services/app_service/public/cpp/app_types.h"
+
+namespace apps {
+
+App::App(AppType app_type, const std::string& app_id)
+    : app_type(app_type), app_id(app_id) {}
+
+App::~App() = default;
+
+}  // namespace apps
