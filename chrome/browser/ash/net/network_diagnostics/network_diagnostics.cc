0aa166822694942252648d2f91e70b5448061dca
Ahmed Nasr <anasr@google.com>
2021-10-22 01:17:23 +0000

    Add latency ms  value to network diagnostics
    Add new field result_value to mojom and set the latency
    in ms value of the https latency routine needed for
    network telemetry reporting.
    
    Bug: b:177847653
    Change-Id: Ic7a2df1598326b3efdb0c37acb90b641a6cf15e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3194824
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kartik Hegde <khegde@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#934121}

--- chrome/browser/ash/net/network_diagnostics/network_diagnostics.cc
+++ chrome/browser/ash/net/network_diagnostics/network_diagnostics.cc
@@ -31,16 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace network_diagnostics {
 
-namespace {
-
-mojom::RoutineResultPtr CreateResult(mojom::RoutineVerdict verdict,
-                                     mojom::RoutineProblemsPtr problems) {
-  return mojom::RoutineResult::New(verdict, std::move(problems),
-                                   base::Time::Now());
-}
-
-}  // namespace
-
 NetworkDiagnostics::NetworkDiagnostics(
     chromeos::DebugDaemonClient* debug_daemon_client) {
   DCHECK(debug_daemon_client);
