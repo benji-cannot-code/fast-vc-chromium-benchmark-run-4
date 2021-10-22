76f3eed0891c52e61764dd05a56e53e659ddb2ea
Ahmed Nasr <anasr@google.com>
2021-10-22 00:42:47 +0000

    Telemetry: Change Sampler to have a single Collect method.
    In this change the Telemetry Sampler interface is updated
    to have only a single Collect method instead of one for
    info and one for telemetry. The main purpose is to
    decouple the info and telemetry reporting since not
    all metrics have the 2 types, and for those who have
    them they can be managed by different policies.
    
    Bug: 177847653
    Change-Id: Iea54f5789ee722be2b4332e19dfb53d5729af0c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232335
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934113}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/https_latency_sampler.h
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/https_latency_sampler.h
@@ -41,7 +41,7 @@ class HttpsLatencySampler : public Sampler {
 
   ~HttpsLatencySampler() override;
 
-  void CollectTelemetry(TelemetryCallback callback) override;
+  void Collect(MetricCallback callback) override;
 
   void SetHttpsLatencyRoutineGetterForTest(
       HttpsLatencyRoutineGetter https_latency_routine_getter);
@@ -53,7 +53,7 @@ class HttpsLatencySampler : public Sampler {
 
   HttpsLatencyRoutineGetter https_latency_routine_getter_;
   std::unique_ptr<HttpsLatencyRoutine> https_latency_routine_;
-  base::queue<TelemetryCallback> telemetry_callbacks_;
+  base::queue<MetricCallback> metric_callbacks_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_telemetry_sampler.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_telemetry_sampler.cc
@@ -3,8 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/policy/reporting/metrics_reporting/network/network_sampler.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/network/network_telemetry_sampler.h"
 
+#include "base/logging.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
@@ -29,8 +30,8 @@ NetworkConnectionState GetTelemetryonnectionStateType(
   return NetworkConnectionState::NOT_CONNECTED;
 }
 
-void OnHttpsLatencySamplerCompleted(TelemetryCallback callback,
-                                    TelemetryData telemetry_data) {
+void OnHttpsLatencySamplerCompleted(MetricCallback callback,
+                                    MetricData metric_data) {
   chromeos::NetworkStateHandler::NetworkStateList network_state_list;
   chromeos::NetworkStateHandler* const network_state_handler =
       chromeos::NetworkHandler::Get()->network_state_handler();
@@ -41,9 +42,16 @@ void OnHttpsLatencySamplerCompleted(TelemetryCallback callback,
       /*limit=*/0,  // no limit to number of results
       &network_state_list);
 
+  if (!metric_data.has_telemetry_data()) {
+    DVLOG(1)
+        << "Metric data is expected to contain HttpsLatency telemetry data, "
+        << " but telemetry data is empty.";
+  }
+
+  auto* const telemetry_data = metric_data.mutable_telemetry_data();
   for (const chromeos::NetworkState* network : network_state_list) {
     auto* const network_telemetry =
-        telemetry_data.mutable_networks_telemetry()->add_network_telemetry();
+        telemetry_data->mutable_networks_telemetry()->add_network_telemetry();
     network_telemetry->set_guid(network->guid());
     network_telemetry->set_connection_state(
         GetTelemetryonnectionStateType(network));
@@ -52,17 +60,17 @@ void OnHttpsLatencySamplerCompleted(TelemetryCallback callback,
     }
   }
 
-  std::move(callback).Run(telemetry_data);
+  std::move(callback).Run(metric_data);
 }
 }  // namespace
 
-NetworkSampler::NetworkSampler(Sampler* https_latency_sampler)
+NetworkTelemetrySampler::NetworkTelemetrySampler(Sampler* https_latency_sampler)
     : https_latency_sampler_(https_latency_sampler) {}
 
-NetworkSampler::~NetworkSampler() = default;
+NetworkTelemetrySampler::~NetworkTelemetrySampler() = default;
 
-void NetworkSampler::CollectTelemetry(TelemetryCallback callback) {
-  https_latency_sampler_->CollectTelemetry(
+void NetworkTelemetrySampler::Collect(MetricCallback callback) {
+  https_latency_sampler_->Collect(
       base::BindOnce(OnHttpsLatencySamplerCompleted, std::move(callback)));
 }
 }  // namespace reporting
