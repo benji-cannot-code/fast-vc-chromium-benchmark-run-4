909e13cc4ec70a8c1666493aacda6b2a5f72c3ca
Yuki Shiino <yukishiino@chromium.org>
2021-10-25 04:47:14 +0000

    Revert "Camera Roll: Prevent settings item from showing when flag is disabled"
    This reverts commit 13e13236374fb9507cf5aeffbebe0de986b1d7d9.
    
    Reason for revert: Suspicious about causing failures of
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/40825/overview
    
    Original change's description:
    > Camera Roll: Prevent settings item from showing when flag is disabled
    >
    > Change-Id: Ic24fefc30cd93d0155d59177d1659a25fb2469fe
    > Bug: 1260191
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227628
    > Auto-Submit: Jianbing Wu <jianbing@google.com>
    > Commit-Queue: Jon Mann <jonmann@chromium.org>
    > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#934124}
    
    Bug: 1260191
    Change-Id: I0d0ec10d1733a29bd010ca3ef77ad9e17556c898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3238607
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934408}

--- chromeos/services/multidevice_setup/feature_state_manager_impl_unittest.cc
+++ chromeos/services/multidevice_setup/feature_state_manager_impl_unittest.cc
@@ -8,10 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
-#include "ash/constants/ash_features.h"
 #include "base/containers/contains.h"
 #include "base/macros.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "chromeos/components/multidevice/remote_device_test_util.h"
 #include "chromeos/services/device_sync/public/cpp/fake_device_sync_client.h"
@@ -712,9 +710,6 @@ TEST_F(MultiDeviceSetupFeatureStateManagerImplTest, PhoneHubForSecondaryUsers) {
 }
 
 TEST_F(MultiDeviceSetupFeatureStateManagerImplTest, PhoneHub) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(ash::features::kPhoneHubCameraRoll);
-
   SetupFeatureStateManager();
 
   const std::vector<mojom::Feature> kAllPhoneHubFeatures{
@@ -782,8 +777,6 @@ TEST_F(MultiDeviceSetupFeatureStateManagerImplTest, PhoneHub) {
                      mojom::Feature::kPhoneHubNotifications);
   VerifyFeatureState(mojom::FeatureState::kUnavailableTopLevelFeatureDisabled,
                      mojom::Feature::kPhoneHubTaskContinuation);
-  VerifyFeatureState(mojom::FeatureState::kUnavailableTopLevelFeatureDisabled,
-                     mojom::Feature::kPhoneHubCameraRoll);
   VerifyFeatureStateChange(7u /* expected_index */, mojom::Feature::kPhoneHub,
                            mojom::FeatureState::kDisabledByUser);
 
@@ -802,34 +795,6 @@ TEST_F(MultiDeviceSetupFeatureStateManagerImplTest, PhoneHub) {
   }
   VerifyFeatureStateChange(9u /* expected_index */, mojom::Feature::kPhoneHub,
                            mojom::FeatureState::kProhibitedByPolicy);
-}
-
-TEST_F(MultiDeviceSetupFeatureStateManagerImplTest,
-       PhoneHubWithCameraRollFeatureDisabled) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndDisableFeature(ash::features::kPhoneHubCameraRoll);
-  const std::vector<mojom::Feature> kAllPhoneHubFeatures{
-      mojom::Feature::kPhoneHub, mojom::Feature::kPhoneHubCameraRoll,
-      mojom::Feature::kPhoneHubNotifications,
-      mojom::Feature::kPhoneHubTaskContinuation};
-
-  SetupFeatureStateManager();
-  SetVerifiedHost();
-  SetSoftwareFeatureState(true /* use_local_device */,
-                          multidevice::SoftwareFeature::kPhoneHubClient,
-                          multidevice::SoftwareFeatureState::kSupported);
-  test_pref_service()->SetBoolean(kPhoneHubEnabledPrefName, true);
-  test_pref_service()->SetBoolean(kPhoneHubCameraRollEnabledPrefName, true);
-  test_pref_service()->SetBoolean(kPhoneHubNotificationsEnabledPrefName, true);
-  SetSoftwareFeatureState(false /* use_local_device */,
-                          multidevice::SoftwareFeature::kPhoneHubHost,
-                          multidevice::SoftwareFeatureState::kEnabled);
-  for (const auto& phone_hub_feature : kAllPhoneHubFeatures) {
-    if (phone_hub_feature == mojom::Feature::kPhoneHubCameraRoll) {
-      VerifyFeatureState(mojom::FeatureState::kNotSupportedByChromebook,
-                         phone_hub_feature);
-    }
-  }
 }
 
 TEST_F(MultiDeviceSetupFeatureStateManagerImplTest, WifiSync) {
