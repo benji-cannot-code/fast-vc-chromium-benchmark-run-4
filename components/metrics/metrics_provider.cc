286cdd58c7f663397ab8db49aaf243523eaabbe3
Robert Kaplow <rkaplow@chromium.org>
2021-10-25 17:04:16 +0000

    Provide session data from providers in UKM report.
    Bug: 1249960
    
    Change-Id: Ibc81050d9e1de38915918ddc7fca5ee5ab0ccc19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3218674
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934564}

--- components/metrics/metrics_provider.cc
+++ components/metrics/metrics_provider.cc
@@ -74,6 +74,8 @@ void MetricsProvider::ProvideCurrentSessionData(
   ProvideStabilityMetrics(uma_proto->mutable_system_profile());
 }
 
+void MetricsProvider::ProvideCurrentSessionUKMData() {}
+
 void MetricsProvider::ProvideStabilityMetrics(
     SystemProfileProto* system_profile_proto) {
 }
