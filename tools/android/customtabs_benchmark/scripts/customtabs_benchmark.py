312f433031aad3bde0e70f8f43d9ad66e96553a2
Egor Pasko <pasko@chromium.org>
2021-10-25 12:06:29 +0000

    customtabs_benchmark: kill zygote, make pinning optional
    Kill the app zygote process between runs. This is useful for
    benchmarking zygote startup characteristics, and is somewhat more
    similar to Pre-Q behavior.
    
    The pinning_benchmark part of the customtabs_benchmark is likely not
    going to be needed. It was intended to be optional, but with a config
    that disables it an assert is hit.
    
    Move setting the defaults for this type of config to an earlier stage,
    just before the assertion. This was easier than cleanly removing all
    the remnants. I did not try a git revert though.
    
    Bug: 1154224
    Change-Id: I9fafe2d056c14a9c48b3468f7b961e19906cb347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234529
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934491}

--- tools/android/customtabs_benchmark/scripts/customtabs_benchmark.py
+++ tools/android/customtabs_benchmark/scripts/customtabs_benchmark.py
@@ -35,6 +35,7 @@ import chrome_setup
 
 # Local build of Chrome (not Chromium).
 _CHROME_PACKAGE = 'com.google.android.apps.chrome'
+_ZYGOTE_PACKAGE = 'com.google.android.apps.chrome.chrome_zygote'
 _COMMAND_LINE_FILE = 'chrome-command-line'
 _TEST_APP_PACKAGE_NAME = 'org.chromium.customtabs.test'
 _INVALID_VALUE = -1
@@ -108,6 +109,7 @@ def RunOnce(device, url, speculated_url, parallel_url, warmup,
     logcat_monitor = device.GetLogcatMonitor(clear=True)
     logcat_monitor.Start()
     device.ForceStop(_CHROME_PACKAGE)
+    device.ForceStop(_ZYGOTE_PACKAGE)
     device.ForceStop(_TEST_APP_PACKAGE_NAME)
 
     if reset_chrome_state:
@@ -187,16 +189,19 @@ def LoopOnDevice(device, configs, output_filename, once=False,
                        config['url'],
                        config.get('speculated_url', config['url']),
                        config.get('parallel_url', ''),
-                       config['warmup'], config['skip_launcher_activity'],
+                       config['warmup'],
+                       config['skip_launcher_activity'],
                        config['speculation_mode'],
                        config['delay_to_may_launch_url'],
-                       config['delay_to_launch_url'], config['cold'],
-                       config.get('pinning_benchmark', False),
-                       config.get('pin_filename', ''),
-                       config.get('pin_offset', -1),
-                       config.get('pin_length', -1),
-                       config.get('extra_brief_memory_mb', 0),
-                       chrome_args, reset_chrome_state=True)
+                       config['delay_to_launch_url'],
+                       config['cold'],
+                       config['pinning_benchmark'],
+                       config['pin_filename'],
+                       config['pin_offset'],
+                       config['pin_length'],
+                       config['extra_brief_memory_mb'],
+                       chrome_args,
+                       reset_chrome_state=True)
       if result is not None:
         out.write(result + '\n')
         out.flush()
