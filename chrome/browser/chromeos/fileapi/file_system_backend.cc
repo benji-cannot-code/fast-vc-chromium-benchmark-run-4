8129666daed263ce1d08aab2c51f5f6c3f957069
Josh Simmons <simmonsjosh@google.com>
2021-10-25 05:15:27 +0000

    [COIL] Change non-inclusive terminology in chromeos::FileSystemBackend
    Changes a non-inclusive comment. No functional change.
    
    Bug: 1198019
    Change-Id: I0ecc988901f4da6c21f995c37fe3661ea1063f15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237496
    Reviewed-by: Josh Simmons <simmonsjosh@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Josh Simmons <simmonsjosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#934419}

--- chrome/browser/chromeos/fileapi/file_system_backend.cc
+++ chrome/browser/chromeos/fileapi/file_system_backend.cc
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace {
 
-// TODO(mtomasz): Remove this hacky whitelist.
+// TODO(mtomasz): Remove this hacky allowlist.
 // See: crbug.com/271946
 const char* kOemAccessibleExtensions[] = {
     "mlbmkoenclnokonejhlfakkeabdlmpek",  // TimeScapes,
