52dbd82b7f7cebe51918463e848536c4386858f3
Joshua Pawlicki <waffles@chromium.org>
2021-10-25 20:09:34 +0000

    component updater: Support disabling component updates.
    Bug: 1222416
    Change-Id: Ifd27875b8a1a2af522e1b0a0fdc25abe6766e579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242682
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934650}

--- android_webview/nonembedded/component_updater/installer_policies/aw_package_names_allowlist_component_installer_policy.cc
+++ android_webview/nonembedded/component_updater/installer_policies/aw_package_names_allowlist_component_installer_policy.cc
@@ -58,7 +58,7 @@ void RegisterWebViewAppsPackageNamesAllowlistComponent(
 
 bool AwPackageNamesAllowlistComponentInstallerPolicy::
     SupportsGroupPolicyEnabledComponentUpdates() const {
-  return false;
+  return true;
 }
 
 bool AwPackageNamesAllowlistComponentInstallerPolicy::
