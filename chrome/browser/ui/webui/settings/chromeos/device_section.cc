e464174a2f571177fe8ee3dad93a62595ffa191e
Gavin Williams <gavinwill@chromium.org>
2021-10-25 20:42:54 +0000

    Input: Add haptic click sensitivity to settings search
    screenshot: http://screen/4f7CpccSf5YGViy
    
    Bug: 1257318
    Change-Id: Id88090e491b534a5b25dd8fa81485e162b3be8f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237393
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934675}

--- chrome/browser/ui/webui/settings/chromeos/device_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_section.cc
@@ -209,6 +209,18 @@ const std::vector<SearchConcept>& GetTouchpadHapticFeedback() {
   return *tags;
 }
 
+const std::vector<SearchConcept>& GetTouchpadHapticClickSensitivity() {
+  static const base::NoDestructor<std::vector<SearchConcept>> tags({
+      {IDS_OS_SETTINGS_TAG_TOUCHPAD_HAPTIC_CLICK_SENSITIVITY,
+       mojom::kPointersSubpagePath,
+       mojom::SearchResultIcon::kLaptop,
+       mojom::SearchResultDefaultRank::kMedium,
+       mojom::SearchResultType::kSetting,
+       {.setting = mojom::Setting::kTouchpadHapticClickSensitivity}},
+  });
+  return *tags;
+}
+
 const std::vector<SearchConcept>& GetMouseScrollAccelerationSearchConcepts() {
   static const base::NoDestructor<std::vector<SearchConcept>> tags({
       {IDS_OS_SETTINGS_TAG_MOUSE_SCROLL_ACCELERATION,
@@ -929,6 +941,7 @@ void DeviceSection::RegisterHierarchy(HierarchyGenerator* generator) const {
       mojom::Setting::kTouchpadScrollAcceleration,
       mojom::Setting::kTouchpadSpeed,
       mojom::Setting::kTouchpadHapticFeedback,
+      mojom::Setting::kTouchpadHapticClickSensitivity,
       mojom::Setting::kPointingStickSwapPrimaryButtons,
       mojom::Setting::kPointingStickSpeed,
       mojom::Setting::kPointingStickAcceleration,
@@ -1020,6 +1033,7 @@ void DeviceSection::TouchpadExists(bool exists) {
   updater.RemoveSearchTags(GetTouchpadSearchConcepts());
   updater.RemoveSearchTags(GetTouchpadScrollAccelerationSearchConcepts());
   updater.RemoveSearchTags(GetTouchpadHapticFeedback());
+  updater.RemoveSearchTags(GetTouchpadHapticClickSensitivity());
 
   if (exists) {
     updater.AddSearchTags(GetTouchpadSearchConcepts());
@@ -1030,6 +1044,10 @@ void DeviceSection::TouchpadExists(bool exists) {
     if (base::FeatureList::IsEnabled(
             ::features::kAllowDisableTouchpadHapticFeedback)) {
       updater.AddSearchTags(GetTouchpadHapticFeedback());
+    }
+    if (base::FeatureList::IsEnabled(
+            ::features::kAllowTouchpadHapticClickSettings)) {
+      updater.AddSearchTags(GetTouchpadHapticClickSensitivity());
     }
   }
 }
