bcc89b41ba5ff721664823653e67ec43653181eb
Nancy Wang <nancylingwang@chromium.org>
2021-10-25 05:01:39 +0000

    Modify AppUpdate to use non-mojom App struct.
    This is the preparation to remove mojom in AppService. Add functions to
    use the non-mojom App struct.
    
    This CL is pure refactor, and no function change.
    
    BUG=1253250
    
    Change-Id: I06a80b689df406c3719cf23ef817fbfb1397b2db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237010
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934413}

--- components/services/app_service/public/cpp/icon_types.h
+++ components/services/app_service/public/cpp/icon_types.h
@@ -20,6 +20,8 @@ struct COMPONENT_EXPORT(APP_UPDATE) IconKey {
 
   IconKey(const IconKey&) = delete;
   IconKey& operator=(const IconKey&) = delete;
+  IconKey(IconKey&&) = default;
+  IconKey& operator=(IconKey&&) = default;
 
   ~IconKey();
 
