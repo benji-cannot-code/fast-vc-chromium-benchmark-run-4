b08f164989ce4b67bab0ddebb2917b70e6abe96e
kyraseevers <kyraseevers@chromium.org>
2021-10-25 17:33:26 +0000

    Pipe in third-party StorageKey to BrowserTestUtils and callers
    For storage partitioning, the BrowserTestUtils class must use a web-
    exposed (third-party) StorageKey value piped in from the renderer.
    From a previous refactor, this value was the empty StorageKey as a
    placeholder, and now contains a value that will allow browsertests
    to pass once storage partitioning is enabled for the LegacyFileSystem.
    
    Bug: 1259676
    Change-Id: I01a6fb661a6d7be7e46bf5d23a2ad3fa6e903435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229186
    Commit-Queue: Kyra Seevers <kyraseevers@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Ali Beyad <abeyad@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934582}

--- content/public/test/browser_test_utils.h
+++ content/public/test/browser_test_utils.h
@@ -69,22 +69,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 class Point;
-}
+}  // namespace gfx
 
 namespace net {
 class CanonicalCookie;
 namespace test_server {
 class EmbeddedTestServer;
-}
+}  // namespace test_server
+
 // TODO(svaldez): Remove typedef once EmbeddedTestServer has been migrated
 // out of net::test_server.
 using test_server::EmbeddedTestServer;
-}
+}  // namespace net
 
 namespace ui {
 class AXPlatformNodeDelegate;
 class AXTreeID;
-}
+}  // namespace ui
 
 #if defined(OS_WIN)
 namespace Microsoft {
@@ -105,8 +106,9 @@ typedef int PROPERTYID;
 // content\test\content_browser_test_utils.h.
 
 namespace blink {
+class StorageKey;
 struct FrameVisualProperties;
-}
+}  // namespace blink
 
 namespace content {
 
@@ -1776,14 +1778,16 @@ class PwnMessageHelper {
                                GURL path,
                                bool exclusive,
                                bool is_directory,
-                               bool recursive);
+                               bool recursive,
+                               const blink::StorageKey& storage_key);
 
   // Calls Write method in FileSystemHost Mojo interface.
   static void FileSystemWrite(RenderProcessHost* process,
                               int request_id,
                               GURL file_path,
                               std::string blob_uuid,
-                              int64_t position);
+                              int64_t position,
+                              const blink::StorageKey& storage_key);
 
   // Calls OpenURL method in FrameHost Mojo interface.
   static void OpenURL(RenderFrameHost* render_frame_host, const GURL& url);
