6d6fd09bd5934e078450bf5bfdd173b72987d92a
Samuel Huang <huangs@chromium.org>
2021-10-25 15:10:31 +0000

    [QuickUnlockPrivate] Add chrome.quickUnlockPrivate.getAuthToken() stub to Lacros.
    This CL makes chrome.quickUnlockPrivate.getAuthToken() available to
    Lacros. The Lacros extension impl is currently a stub, which will be
    populated once the required crosapi is added. Since the Lacros impl
    (using crosapi) will be significantly different from Ash impl, the new
    code is added to separate *_lacros.{cc,h} files.
    
    Bug: 1227546
    Change-Id: I57a01a62f282f32697e87b0e9ae5ac2e60170231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234844
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934530}

---
+++ chrome/browser/extensions/api/quick_unlock_private/quick_unlock_private_api_lacros.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/api/quick_unlock_private/quick_unlock_private_api_lacros.h"
+
+namespace extensions {
+
+// quickUnlockPrivate.getAuthToken
+
+QuickUnlockPrivateGetAuthTokenFunction::
+    QuickUnlockPrivateGetAuthTokenFunction() {}
+
+QuickUnlockPrivateGetAuthTokenFunction::
+    ~QuickUnlockPrivateGetAuthTokenFunction() {}
+
+ExtensionFunction::ResponseAction
+QuickUnlockPrivateGetAuthTokenFunction::Run() {
+  // TODO(crbug.com/1227546): Call Ash implementation via crosapi when
+  // available.
+  return RespondNow(Error("AuthenticateProfileHandler crosapi unavailable."));
+}
+
+}  // namespace extensions
