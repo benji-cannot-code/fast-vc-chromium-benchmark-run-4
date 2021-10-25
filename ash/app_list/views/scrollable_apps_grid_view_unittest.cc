9365d35281b14c4aa041cbee4ba91f331697f531
Matthew Mourgos <mmourgos@chromium.org>
2021-10-25 23:18:54 +0000

    CrOS AppList: calculate variable row size for grid pages
    For tablet mode productivity launcher, calculate the number of rows to
    show on each page based on the height available in the apps grid using
    the following rules:
    
    - By default have 4 rows for landscape and 5 rows for portrait mode.
    - If the spacing between rows is greater than 96, add another row.
    - If the spacing between rows is less than 8, remove a row.
    - The final # of rows will be spaced out an amount between 8 and 96.
    
    The first page can have a different number of rows and different spacing
    between rows to account for the continue section and recent apps.
    
    Bug: 1234064
    Change-Id: I0a1d2f763bd5fd99b0f0b8e985351011f3a5d3d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3209874
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934722}

--- ash/app_list/views/scrollable_apps_grid_view_unittest.cc
+++ ash/app_list/views/scrollable_apps_grid_view_unittest.cc
@@ -203,7 +203,7 @@ TEST_F(ScrollableAppsGridViewTest, DragApp) {
   StartDragOnItemViewAt(0);
 
   // Drag to the right of the second item.
-  gfx::Size tile_size = apps_grid_view_->GetTotalTileSize();
+  gfx::Size tile_size = apps_grid_view_->GetTotalTileSize(/*page=*/0);
   auto* generator = GetEventGenerator();
   generator->MoveMouseBy(tile_size.width() * 2, 0);
   generator->ReleaseLeftButton();
@@ -310,7 +310,7 @@ TEST_F(ScrollableAppsGridViewTest, DragAppAfterScrollingDown) {
   generator->MoveMouseTo(item->GetBoundsInScreen().CenterPoint());
   generator->PressLeftButton();
   item->FireMouseDragTimerForTest();
-  gfx::Size tile_size = apps_grid_view->GetTotalTileSize();
+  gfx::Size tile_size = apps_grid_view->GetTotalTileSize(/*page=*/0);
   generator->MoveMouseBy(tile_size.width() * 2, 0);
   generator->ReleaseLeftButton();
 
@@ -470,7 +470,7 @@ TEST_F(ScrollableAppsGridViewTest, DragItemIntoEmptySpaceWillReorderToEnd) {
 
   // Drag and drop the first item straight down below the first row.
   StartDragOnItemViewAt(0);
-  gfx::Size tile_size = apps_grid_view_->GetTotalTileSize();
+  gfx::Size tile_size = apps_grid_view_->GetTotalTileSize(/*page=*/0);
   auto* generator = GetEventGenerator();
   generator->MoveMouseBy(0, tile_size.height());
   generator->ReleaseLeftButton();
