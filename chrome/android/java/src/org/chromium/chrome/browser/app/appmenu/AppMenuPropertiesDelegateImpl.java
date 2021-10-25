3fb11a0a81db6a0bc74d179162db43fb4a21401f
Xi Han <hanxi@google.com>
2021-10-25 16:41:39 +0000

    [Start] Fix a crash due to empty tab in app menu.
    Bug: 1263020
    Change-Id: I632cf54d105362f2d241ea2ccfe1ad160e2ef55c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3241229
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934554}

--- chrome/android/java/src/org/chromium/chrome/browser/app/appmenu/AppMenuPropertiesDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/appmenu/AppMenuPropertiesDelegateImpl.java
@@ -1006,7 +1006,7 @@ public class AppMenuPropertiesDelegateImpl implements AppMenuPropertiesDelegate 
                 && mBookmarkBridgeSupplier.get().isEditBookmarksEnabled();
         bookmarkMenuItemShortcut.setEnabled(editEnabled);
 
-        if (shouldCheckBookmarkStar(currentTab)) {
+        if (currentTab != null && shouldCheckBookmarkStar(currentTab)) {
             bookmarkMenuItemShortcut.setIcon(R.drawable.btn_star_filled);
             bookmarkMenuItemShortcut.setChecked(true);
             bookmarkMenuItemShortcut.setTitleCondensed(mContext.getString(R.string.edit_bookmark));
