6e1d9cbb39fb4ba7b30b99ab61375f0121690130
Joe Downing <joedow@google.com>
2021-10-25 16:50:22 +0000

    Trigger policy updates when Registry keys are updated
    The CRD host relies on the PolicyLoader component in Chromium which
    listens for GPO notifications before informing the host that the
    policies have changed. This works well if the notifications are sent
    but the policies are not applied until it is restarted if not.
    
    This CL hooks up a registry watcher for the Chrome policy key which
    asks the policy service to reload it's view when a change is detected.
    The same filtering and restart logic we had before is preserved, we
    just apply it for consistently. If both a registry key change and GPO
    notification event occur and the CRD policies are the same, the second
    notification will be dropped since our PolicyWatching only triggers
    updates when one of our policies has changed.
    
    Bug: b:203468803
    Change-Id: I353ad1a19b6c3fbf42d9a3f3b5c3c72b7c5f5f24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3236776
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934557}

--- remoting/host/policy_watcher.h
+++ remoting/host/policy_watcher.h
@@ -12,7 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
+#include "build/build_config.h"
 #include "components/policy/core/common/policy_service.h"
+
+#if defined(OS_WIN)
+#include "base/win/registry.h"
+#endif
 
 namespace base {
 class DictionaryValue;
@@ -152,6 +157,10 @@ class PolicyWatcher : public policy::PolicyService::Observer {
                        const policy::PolicyMap& current) override;
   void OnPolicyServiceInitialized(policy::PolicyDomain domain) override;
 
+#if defined(OS_WIN)
+  void WatchForRegistryChanges();
+#endif
+
   PolicyUpdatedCallback policy_updated_callback_;
   PolicyErrorCallback policy_error_callback_;
 
@@ -174,6 +183,13 @@ class PolicyWatcher : public policy::PolicyService::Observer {
   std::unique_ptr<policy::SchemaRegistry> owned_schema_registry_;
   std::unique_ptr<policy::ConfigurationPolicyProvider> owned_policy_provider_;
   std::unique_ptr<policy::PolicyService> owned_policy_service_;
+
+#if defined(OS_WIN)
+  // |policy_key_| relies on |policy_service_| to notify the host of policy
+  // changes. Make sure |policy_key_| is destroyed to prevent any notifications
+  // from firing while the above objects are being torn down.
+  base::win::RegKey policy_key_;
+#endif
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
