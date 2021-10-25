552a69bb01b42545828dac6834c4ed306d8aa6bd
Ramin Halavati <rhalavati@chromium.org>
2021-10-25 06:45:07 +0000

    Pass ax::mojom::InvalidState through Automation API.
    ax::mojom::InvalidState is passed through Automation API.
    
    Bug: 1260894
    AX-Relnotes: n/a.
    Change-Id: I7a737b74be7a6b7ef7da83c93f218635bd614a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226175
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934433}

--- extensions/renderer/resources/automation/automation_node.js
+++ extensions/renderer/resources/automation/automation_node.js
@@ -440,6 +440,14 @@ const GetAriaCurrentState = natives.GetAriaCurrentState;
 /**
  * @param {string} axTreeID The id of the accessibility tree.
  * @param {number} nodeID The id of a node.
+ * @return {automation.InvalidState}
+ */
+const GetInvalidState = natives.GetInvalidState;
+
+
+/**
+ * @param {string} axTreeID The id of the accessibility tree.
+ * @param {number} nodeID The id of a node.
  * @param {string} searchStr
  * @param {boolean} backward
  * @return {{treeId: string, nodeId: number}}
@@ -897,6 +905,10 @@ AutomationNodeImpl.prototype = {
 
   get ariaCurrentState() {
     return GetAriaCurrentState(this.treeID, this.id);
+  },
+
+  get invalidState() {
+    return GetInvalidState(this.treeID, this.id);
   },
 
   get tableCellColumnHeaders() {
@@ -2097,6 +2109,7 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
         'htmlAttributes',
         'imageAnnotation',
         'indexInParent',
+        'invalidState',
         'isRootNode',
         'italic',
         'lastChild',
