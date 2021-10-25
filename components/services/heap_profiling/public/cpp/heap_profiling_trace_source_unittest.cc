8cf91ec7a781ba99935fd539ece5ab50955eb861
Sami Kyostila <skyostil@chromium.org>
2021-10-25 09:50:13 +0000

    services/tracing: Make sure tracing always gets torn down in tests
    Return a handle from PerfettoTracedProcess::SetupForTesting() so the
    caller can't accidentally fail to tear down tracing infrastructure for
    the process, potentially leading to a hard-to-debug failure in an
    unrelated test.
    
    Bug: b:189825391
    Change-Id: Iaefd018cdb3a14706f42598b92b3394543dba4d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237248
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934465}

--- components/services/heap_profiling/public/cpp/heap_profiling_trace_source_unittest.cc
+++ components/services/heap_profiling/public/cpp/heap_profiling_trace_source_unittest.cc
@@ -19,8 +19,7 @@ namespace {
 class HeapProfilingProtoExporterTest : public testing::Test {
  public:
   void SetUp() override {
-    tracing::PerfettoTracedProcess::ResetTaskRunnerForTesting();
-    tracing::PerfettoTracedProcess::GetTaskRunner()->GetOrCreateTaskRunner();
+    test_handle_ = tracing::PerfettoTracedProcess::SetupForTesting();
 
     auto perfetto_wrapper = std::make_unique<base::tracing::PerfettoTaskRunner>(
         task_environment_.GetMainThreadTaskRunner());
@@ -32,7 +31,6 @@ class HeapProfilingProtoExporterTest : public testing::Test {
   void TearDown() override {
     // Be sure there is no pending/running tasks.
     task_environment_.RunUntilIdle();
-    tracing::PerfettoTracedProcess::TearDownForTesting();
   }
 
   void BeginTrace() {
@@ -54,6 +52,7 @@ class HeapProfilingProtoExporterTest : public testing::Test {
 
  private:
   base::test::TaskEnvironment task_environment_;
+  std::unique_ptr<tracing::PerfettoTracedProcess::TestHandle> test_handle_;
 };
 
 using Sample = base::SamplingHeapProfiler::Sample;
