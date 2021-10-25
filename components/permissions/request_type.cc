969094d7344c556280a04a6e0f1ae853d07f66a2
Ravjit <ravjit@chromium.org>
2021-10-25 13:49:20 +0000

    Added code to debug crash in permissions::ContentSettingsTypeToRequestType
    Added SCOPED_CRASH_KEY_NUMBER to see which ContentSettingsType is causing a crash in the permissions::ContentSettingsTypeToRequestType method.
    
    Bug: 1260684
    Change-Id: I8c9a7170e895716467b312a57f82f44d898c7ce2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3236667
    Commit-Queue: Ravjit Singh Uppal <ravjit@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934511}

--- components/permissions/request_type.cc
+++ components/permissions/request_type.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/permissions/request_type.h"
 
 #include "base/check.h"
+#include "base/debug/crash_logging.h"
 #include "base/notreached.h"
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/permissions/permissions_client.h"
@@ -177,6 +178,8 @@ RequestType ContentSettingsTypeToRequestType(
       return RequestType::kWindowPlacement;
 #endif
     default:
+      SCOPED_CRASH_KEY_NUMBER("RequestType", "content_settings_type",
+                              static_cast<int>(content_settings_type));
       CHECK(false);
       return RequestType::kGeolocation;
   }
