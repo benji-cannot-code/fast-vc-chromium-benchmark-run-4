7497e4e72fb64f0fdf09883c285cd56a0e6ad958
Matt Mueller <mattm@chromium.org>
2021-10-25 21:09:09 +0000

    PathBuilder: return partial paths that did not reach a trust anchor
    Change-Id: I98fa110e7a3d0f4f6881294cf7911764947857dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233115
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934693}

--- net/cert/internal/common_cert_errors.cc
+++ net/cert/internal/common_cert_errors.cc
@@ -9,6 +9,7 @@ namespace net {
 
 namespace cert_errors {
 
+DEFINE_CERT_ERROR_ID(kInternalError, "Internal error");
 DEFINE_CERT_ERROR_ID(kValidityFailedNotAfter, "Time is after notAfter");
 DEFINE_CERT_ERROR_ID(kValidityFailedNotBefore, "Time is before notBefore");
 DEFINE_CERT_ERROR_ID(kDistrustedByTrustStore, "Distrusted by trust store");
@@ -60,6 +61,9 @@ DEFINE_CERT_ERROR_ID(kCertificateRevoked, "Certificate is revoked");
 DEFINE_CERT_ERROR_ID(kNoRevocationMechanism,
                      "Certificate lacks a revocation mechanism");
 DEFINE_CERT_ERROR_ID(kUnableToCheckRevocation, "Unable to check revocation");
+DEFINE_CERT_ERROR_ID(kNoIssuersFound, "No matching issuer found");
+DEFINE_CERT_ERROR_ID(kDeadlineExceeded, "Deadline exceeded");
+DEFINE_CERT_ERROR_ID(kIterationLimitExceeded, "Iteration limit exceeded");
 
 }  // namespace cert_errors
 
