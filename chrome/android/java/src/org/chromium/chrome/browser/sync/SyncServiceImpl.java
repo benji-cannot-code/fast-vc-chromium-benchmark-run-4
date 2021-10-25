d894e810cc3fb89baf8e2c13328713c6705648c2
Marc Treib <treib@chromium.org>
2021-10-25 14:13:55 +0000

    Sync cleanup: Replace 'is primary' terminology in Android/Java code
    Since this code was written, the terminology around the 'primary'
    account has changed, so this wasn't in line with IdentityManager's use
    of the term anymore.
    Instead of 'is primary', it's now 'has sync consent'.
    
    Bug: 1254280
    Change-Id: Ie27f1ba0be73b5ede5d9f0915dcd810b259af1c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3240970
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934516}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncServiceImpl.java
@@ -123,13 +123,13 @@ public class SyncServiceImpl extends SyncService {
     }
 
     @Override
-    public @Nullable CoreAccountInfo getAuthenticatedAccountInfo() {
-        return SyncServiceImplJni.get().getAuthenticatedAccountInfo(mSyncServiceAndroidBridge);
+    public @Nullable CoreAccountInfo getAccountInfo() {
+        return SyncServiceImplJni.get().getAccountInfo(mSyncServiceAndroidBridge);
     }
 
     @Override
-    public boolean isAuthenticatedAccountPrimary() {
-        return SyncServiceImplJni.get().isAuthenticatedAccountPrimary(mSyncServiceAndroidBridge);
+    public boolean hasSyncConsent() {
+        return SyncServiceImplJni.get().hasSyncConsent(mSyncServiceAndroidBridge);
     }
 
     @Override
@@ -432,8 +432,8 @@ public class SyncServiceImpl extends SyncService {
         void setDecoupledFromAndroidMasterSync(long nativeSyncServiceAndroidBridge);
         boolean getDecoupledFromAndroidMasterSync(long nativeSyncServiceAndroidBridge);
         @Nullable
-        CoreAccountInfo getAuthenticatedAccountInfo(long nativeSyncServiceAndroidBridge);
-        boolean isAuthenticatedAccountPrimary(long nativeSyncServiceAndroidBridge);
+        CoreAccountInfo getAccountInfo(long nativeSyncServiceAndroidBridge);
+        boolean hasSyncConsent(long nativeSyncServiceAndroidBridge);
         boolean isPassphrasePromptMutedForCurrentProductVersion(
                 long nativeSyncServiceAndroidBridge);
         void markPassphrasePromptMutedForCurrentProductVersion(long nativeSyncServiceAndroidBridge);
