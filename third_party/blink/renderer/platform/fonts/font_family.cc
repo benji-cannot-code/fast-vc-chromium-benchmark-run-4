54bc2279d193e2242d78f2314978b4bbf56f2ef9
Koji Ishii <kojii@chromium.org>
2021-10-25 00:33:41 +0000

    [AsyncFontAccess] Prewarm the first font attached to |LayoutText|
    THis patch prewarms the first family name in |WillUseFontData|.
    
    |LayoutText| calls |WillUseFontData| to start loading web
    fonts earlier in the style phase.
    
    This patch has no behavior changes except some additional CPU
    cycles because |Prewarm| is not hooked up yet.
    
    Bug: 1256946
    Change-Id: I638860adcf50b655b18bd724b8a1fc83fae28697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3238625
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934378}

--- third_party/blink/renderer/platform/fonts/font_family.cc
+++ third_party/blink/renderer/platform/fonts/font_family.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/font_family.h"
 
 #include "third_party/blink/renderer/platform/font_family_names.h"
+#include "third_party/blink/renderer/platform/fonts/font_cache.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
@@ -53,6 +54,20 @@ void FontFamily::AppendFamily(AtomicString family_name, Type family_type) {
   scoped_refptr<SharedFontFamily> appended_family = SharedFontFamily::Create();
   appended_family->SetFamily(family_name, family_type);
   AppendFamily(appended_family);
+}
+
+void FontFamily::Prewarm(const FontDescription& font_description) const {
+  DCHECK(!FamilyName().IsEmpty());
+  DCHECK(!is_prewarmed_);
+  is_prewarmed_ = true;
+  switch (family_type_) {
+    case Type::kFamilyName:
+      FontCache::PrewarmFamily(FamilyName());
+      break;
+    case Type::kGenericFamily:
+      // TODO(kojii): Support generic family.
+      break;
+  }
 }
 
 String FontFamily::ToString() const {
--- third_party/blink/renderer/platform/fonts/font_family.h
+++ third_party/blink/renderer/platform/fonts/font_family.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class FontDescription;
 class SharedFontFamily;
 
 class PLATFORM_EXPORT FontFamily {
@@ -64,6 +65,8 @@ class PLATFORM_EXPORT FontFamily {
   void AppendFamily(AtomicString family_name, Type family_type);
   scoped_refptr<SharedFontFamily> ReleaseNext();
 
+  void PrewarmIfNeeded(const FontDescription&) const;
+
   // Returns this font family's name followed by all subsequent linked
   // families separated ", " (comma and space). Font family names are never
   // quoted nor escaped. For web-exposed serialization, please rely instead on
@@ -78,9 +81,12 @@ class PLATFORM_EXPORT FontFamily {
   static Type InferredTypeFor(const AtomicString& family_name);
 
  private:
+  void Prewarm(const FontDescription&) const;
+
   AtomicString family_name_;
   scoped_refptr<SharedFontFamily> next_;
   Type family_type_ = Type::kFamilyName;
+  mutable bool is_prewarmed_ = false;
 };
 
 class PLATFORM_EXPORT SharedFontFamily : public FontFamily,
@@ -121,6 +127,12 @@ inline void FontFamily::AppendFamily(scoped_refptr<SharedFontFamily> family) {
 
 inline scoped_refptr<SharedFontFamily> FontFamily::ReleaseNext() {
   return std::move(next_);
+}
+
+inline void FontFamily::PrewarmIfNeeded(
+    const FontDescription& font_description) const {
+  if (!is_prewarmed_)
+    Prewarm(font_description);
 }
 
 }  // namespace blink
