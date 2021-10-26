64c2c139c3fb2c8fed60f42b7b581a8525c563c7
Tim van der Lippe <tvanderlippe@chromium.org>
2021-10-26 10:52:12 +0000

    Use loadLegacyModule in data_grid tests
    In https://crrev.com/c/3242142 we are removing the module.json file, so
    we have to use loadLegacyModule to load in the DataGrid symbols. We
    have to disable the tests first, as the test expectations bake in the
    warning about missing module.json files, which will only happen after
    DevTools has rolled.
    
    R=szuend@chromium.org
    
    Bug: 1127902
    Change-Id: I430713b0f863a344a5bb84769c549ba2661515ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242084
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934898}

--- third_party/blink/web_tests/http/tests/devtools/components/viewport-datagrid.js
+++ third_party/blink/web_tests/http/tests/devtools/components/viewport-datagrid.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (async function() {
   TestRunner.addResult(`Tests ViewportDataGrid.\n`);
-  await TestRunner.loadModule('data_grid'); await TestRunner.loadTestModule('data_grid_test_runner');
+  await TestRunner.loadLegacyModule('data_grid'); await TestRunner.loadTestModule('data_grid_test_runner');
 
   function attach(parent, child, index) {
     var parentName = parent === root ? 'root' : parent.data.id;
--- third_party/blink/web_tests/http/tests/devtools/unit/datagrid-items-attached-to-dom.js
+++ third_party/blink/web_tests/http/tests/devtools/unit/datagrid-items-attached-to-dom.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function() {
-  await TestRunner.loadModule('data_grid');
+  await TestRunner.loadLegacyModule('data_grid');
 
   TestRunner.addResult("This tests viewport datagrid.");
 
