e92944edeb7b2a34a6dc7d7c9366c5518d0a0845
Nancy Wang <nancylingwang@chromium.org>
2021-10-26 17:16:18 +0000

    Create the browser session restore crash lock when restore from crash.
    CL:3200880 adds ability to disable saving of commands for the browser
    session restore to address the browser sessions/tabs not restored issue.
    
    CL:3229806 change logic to not write the browser session data until the
    user closes the bubble, or creates a new browser.
    
    This CL modifies the full restore service to implement the same logic
    as the browser session restore to create the crash lock to not write
    the browser session restore data until a new browser is created.
    
    BUG=294444, 1146900
    
    Change-Id: I339d61ceb532a8363296397651e4036fc493db67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244708
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935017}

--- chrome/browser/ash/app_restore/full_restore_service.h
+++ chrome/browser/ash/app_restore/full_restore_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/accelerators.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
+#include "chrome/browser/sessions/exit_type_service.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "content/public/browser/notification_observer.h"
@@ -163,6 +164,10 @@ class FullRestoreService : public KeyedService,
   std::unique_ptr<message_center::Notification> notification_;
 
   content::NotificationRegistrar notification_registrar_;
+
+  // Browser session restore exit type service lock. This is created when the
+  // system is restored from crash to help set the browser saving flag.
+  std::unique_ptr<ExitTypeService::CrashedLock> crashed_lock_;
 
   base::ScopedObservation<ash::AcceleratorController,
                           ash::AcceleratorController::Observer>
