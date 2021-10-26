56470d9aea8ad1f395862dabfd70abee1bcc2531
Tim van der Lippe <tvanderlippe@chromium.org>
2021-10-26 11:17:34 +0000

    Fix object_ui layout tests
    R=szuend@chromium.org
    
    Bug: 1127902
    Change-Id: I10f0fc41e84cc80a5bf4a0141770b4ce8a0987e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3243945
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934905}

--- third_party/blink/web_tests/http/tests/devtools/local-object-properties-section.js
+++ third_party/blink/web_tests/http/tests/devtools/local-object-properties-section.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (async function() {
   TestRunner.addResult(`Test that ObjectPropertiesSection works with local remote objects.\n`);
-  await TestRunner.loadModule('ui/legacy/components/object_ui');
   await TestRunner.loadLegacyModule('ui/legacy/components/object_ui');
 
   var d = [];
