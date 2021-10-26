df055c4eb681ec7c0ec5554de35a19d6dd282626
Joey Arhar <jarhar@chromium.org>
2021-10-26 20:00:35 +0000

    Fix use-after-poison in DetachLayoutTree
    This bug looks very similar to crbug.com/1255464, so I tried to look for
    other places that a similar fix could be applied by looking for other
    uses of IsFlatTreeTraversalForbidden in accessibility. I found one
    without a corresponding HasPendingSlotAssignmentRecalc in
    AXObject::ClearChildren, and adding a HasPendingSlotAssignmentRecalc
    there appears to fix the bug.
    
    I wasn't able to make a minimal web_test to repro the crash, but I
    verified with the clusterfuzz repro tool that this fixes the crash.
    
    Fixed: 1258932
    Change-Id: Idea6a048ebefa2ddc7d5ffbd36c9f25fc7435a0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245451
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935108}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/simulated_click_options.h"
 #include "third_party/blink/renderer/core/dom/focus_params.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
+#include "third_party/blink/renderer/core/dom/slot_assignment_engine.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
@@ -4752,7 +4753,10 @@ void AXObject::ClearChildren() const {
   if (!node)
     return;
 
-  if (GetDocument()->IsFlatTreeTraversalForbidden()) {
+  if (GetDocument()->IsFlatTreeTraversalForbidden() ||
+      node->GetDocument()
+          .GetSlotAssignmentEngine()
+          .HasPendingSlotAssignmentRecalc()) {
     // Cannot use layout tree builder traversal now, will have to rely on
     // RepairParent() at a later point.
     return;
