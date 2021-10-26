60267c4af2ebae9a9b2d3fcda7bbffbcabdd0124
Michael Checo <michaelcheco@google.com>
2021-10-26 19:28:46 +0000

    Firmware UI: Add skeleton mojo provider
    - Adds boilerplate code for mojo_interface_provider/types file
    - A follow-up CL will implement observePeripheralUpdates on the
      updateProvider interface.
    
    Bug: 1252981
    Change-Id: I385106f1b83b0e3c3ba54b4c1e42f3b07ea7b472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3231197
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935087}

--- chrome/test/data/webui/chromeos/firmware_update/firmware_update_test.js
+++ chrome/test/data/webui/chromeos/firmware_update/firmware_update_test.js
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {FirmwareUpdateAppElement} from 'chrome://accessory-update/firmware_update_app.js';
+import {UpdateProviderInterface} from 'chrome://accessory-update/firmware_update_types.js';
+import {getUpdateProvider, setUpdateProviderForTesting} from 'chrome://accessory-update/mojo_interface_provider.js';
 
 import {assertEquals} from '../../chai_assert.js';
 
@@ -28,5 +30,13 @@ export function firmwareUpdateAppTest() {
     assertEquals(
         'Firmware Update',
         page.shadowRoot.querySelector('#header').textContent);
+  });
+
+  test('SettingGettingTestProvider', () => {
+    // TODO(michaelcheco): Replace with fake when built.
+    let fake_provider =
+        /** @type {!UpdateProviderInterface} */ (new Object());
+    setUpdateProviderForTesting(fake_provider);
+    assertEquals(fake_provider, getUpdateProvider());
   });
 }
