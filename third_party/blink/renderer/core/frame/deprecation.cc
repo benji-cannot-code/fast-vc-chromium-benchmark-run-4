edebfede5acfde1a1aac47d337372a95eff51bc0
Ari Chivukula <arichiv@chromium.org>
2021-10-26 03:45:14 +0000

    [Deprecation Console->Issue] (4) Add features for console/issue deprecation
    This CL prepares for the next few parts which implement and launch the
    DevTools Issue deprecation message. This CL is split out to reduce the
    chances of rebase issues on the next one, which will take a while to
    review.
    
    This is a part of a set of CLs:
    (1) Remove legacy CSS Deprecation path
    (2) Consolidate DeprecationInfo
    (3) Streamline redundant Deprecation messages
    (4) Add features for console/issue deprecation
    (5) Fix feature status detail link
    (6) Implement Issue Deprecation
    (7) Launch Issue Deprecation
    (8) Remove Console Deprecation
    
    Bug: 1248484
    Change-Id: I5686f258799cd1443f6284d31a337b6dd0951a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237194
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934817}

--- third_party/blink/renderer/core/frame/deprecation.cc
+++ third_party/blink/renderer/core/frame/deprecation.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/deprecation.h"
 
+#include "base/feature_list.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy.mojom-blink.h"
 #include "third_party/blink/public/mojom/reporting/reporting.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
@@ -798,10 +800,16 @@ void Deprecation::CountDeprecation(ExecutionContext* context,
 
   // Send the deprecation message to the console as a warning.
   DCHECK(!info.message_.IsEmpty());
-  auto* console_message = MakeGarbageCollected<ConsoleMessage>(
-      mojom::blink::ConsoleMessageSource::kDeprecation,
-      mojom::blink::ConsoleMessageLevel::kWarning, info.message_);
-  context->AddConsoleMessage(console_message);
+  if (base::FeatureList::IsEnabled(features::kDeprecationWillLogToConsole)) {
+    auto* console_message = MakeGarbageCollected<ConsoleMessage>(
+        mojom::blink::ConsoleMessageSource::kDeprecation,
+        mojom::blink::ConsoleMessageLevel::kWarning, info.message_);
+    context->AddConsoleMessage(console_message);
+  }
+  if (base::FeatureList::IsEnabled(
+          features::kDeprecationWillLogToDevToolsIssue)) {
+    // TODO(crbug.com/1248484): Implement DevTools Issue logging.
+  }
 
   Report* report = CreateReportInternal(context->Url(), info);
 
