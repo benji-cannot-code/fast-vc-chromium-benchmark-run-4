b057721fddf169a014c1b227856733eafb8a1374
Kushagra Sinha <sinhak@chromium.org>
2021-10-26 08:34:08 +0000

    cros: Separate error message for token handle errors
    Add a separate error message for token handle errors, to distinguish
    them from other types of errors shown by `SigninErrorNotifier`.
    
    Additionally fix a bug in TokenHandle error handling where we may
    display the error message associated with a Secondary Account, instead
    of a TokenHandle specific error.
    
    A screenshot is attached in https://crbug.com/1257161#c4
    
    Fixed: 1257161
    Test: unit_tests --gtest_filter="*SigninErrorNotifierTest*"
    Change-Id: Ibf55e2d7718d61e5a0b8f463ce8495ce818727f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3208059
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934870}

--- chrome/browser/ash/login/signin/signin_error_notifier.h
+++ chrome/browser/ash/login/signin/signin_error_notifier.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/login/signin/token_handle_util.h"
@@ -51,9 +52,15 @@ class SigninErrorNotifier : public SigninErrorController::Observer,
   void OnErrorChanged() override;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(SigninErrorNotifierTest, TokenHandleTest);
+  FRIEND_TEST_ALL_PREFIXES(SigninErrorNotifierTest,
+                           TokenHandleErrorsDoNotDisplaySecondaryAccountErrors);
+
   // Handles errors for the Device Account.
-  // Displays a notification asking the user to Sign Out.
-  void HandleDeviceAccountError();
+  // Displays a notification with `error_message`, asking the user to Sign Out -
+  // as opposed to `HandleSecondaryAccountError` which asks the user to
+  // re-authenticate in-session.
+  void HandleDeviceAccountError(const std::u16string& error_message);
 
   // Handles errors for Secondary Accounts.
   // Displays a notification that allows users to open crOS Account Manager UI.
@@ -74,8 +81,6 @@ class SigninErrorNotifier : public SigninErrorController::Observer,
   // `message_center::HandleNotificationClickDelegate`.
   void HandleSecondaryAccountReauthNotificationClick(
       absl::optional<int> button_index);
-
-  std::u16string GetMessageBody(bool is_secondary_account_error) const;
 
   // The error controller to query for error details.
   SigninErrorController* error_controller_;
