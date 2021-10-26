26ca65d314598737f6979be065ab0f5eca1ca7e4
Peter Boström <pbos@chromium.org>
2021-10-26 19:06:43 +0000

    Repaint side-panel border on theme updates
    This prevents the border from getting out of sync with the rest of the
    browser.
    
    Bug: 1263373
    Change-Id: Ib7d5d1d3b98174cacfe7266b21699eef3a3f0435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244281
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Connie Wan <connily@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935075}

--- chrome/browser/ui/views/side_panel.cc
+++ chrome/browser/ui/views/side_panel.cc
@@ -100,6 +100,11 @@ class BorderView : public views::View {
 
     SetBoundsRect(bounds);
   }
+
+  void OnThemeChanged() override {
+    SchedulePaint();
+    View::OnThemeChanged();
+  }
 };
 
 }  // namespace
