cdf5ac3d75539b9d840c42c44da45093e4a01a0d
tby <tby@chromium.org>
2021-10-26 04:33:28 +0000

    [Launcher] Control ItemSuggest multi-query with Finch
    This allows us to do a slow rollout of the 'query more than once
    per session" bug fix for non-suggested-files users.
    
    Bug: 1263257
    Change-Id: I5a7471cefde1636cece1b61a97d555cbb0dedc50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242904
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934829}

--- chrome/browser/ui/app_list/search/files/item_suggest_cache.h
+++ chrome/browser/ui/app_list/search/files/item_suggest_cache.h
@@ -113,6 +113,12 @@ class ItemSuggestCache {
   static constexpr base::FeatureParam<int> kMinMinutesBetweenUpdates{
       &kExperiment, "min_minutes_between_updates", 15};
 
+  // Whether ItemSuggest should be queried more than once per session. Multiple
+  // queries are issued if either this param is true or the suggested files
+  // experiment is enabled.
+  static constexpr base::FeatureParam<bool> kMultipleQueriesPerSession{
+      &kExperiment, "multiple_queries_per_session", false};
+
   // Returns the body for the itemsuggest request. Affected by |kExperiment|.
   std::string GetRequestBody();
 
@@ -139,6 +145,8 @@ class ItemSuggestCache {
   const bool enabled_;
   const GURL server_url_;
   const base::TimeDelta min_time_between_updates_;
+  // Whether we should query item suggest more than once per session.
+  const bool multiple_queries_per_session_;
 
   Profile* profile_;
   std::unique_ptr<signin::PrimaryAccountAccessTokenFetcher> token_fetcher_;
