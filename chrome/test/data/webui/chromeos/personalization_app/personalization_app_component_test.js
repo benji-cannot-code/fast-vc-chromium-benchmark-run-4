4fae66defd1861f3660d6f4458e1d1a212797c63
David Black <dmblack@google.com>
2021-10-26 19:59:06 +0000

    personalization: Stub in space for Google Photos albums and photos.
    This CL stubs in space for Google Photos albums and photos to appear.
    Eventually these spaces will be iframes, with actual UIs implemented in
    untrusted similar to other places in the personalization app.
    
    Bug: b:203830585
    Change-Id: Ifb092bda569fac0eb4d08fa98a58fc794a9d2ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3238081
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#935105}

--- chrome/test/data/webui/chromeos/personalization_app/personalization_app_component_test.js
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_app_component_test.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {GooglePhotosTest} from './google_photos_element_test.js';
 import {LocalImagesTest} from './local_images_element_test.js';
 import {PersonalizationToastTest} from './personalization_toast_element_test.js';
 import {WallpaperBreadcrumbTest} from './wallpaper_breadcrumb_element_test.js';
@@ -16,6 +17,7 @@ import {WallpaperSelectedTest} from './wallpaper_selected_element_test.js';
 window.console.warn = () => {};
 
 const testCases = [
+  GooglePhotosTest,
   LocalImagesTest,
   PersonalizationToastTest,
   WallpaperBreadcrumbTest,
