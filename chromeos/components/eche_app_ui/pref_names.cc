6f473ca4df27e666882b26b34e743e06ecf3b845
mavishsu <mavishsu@google.com>
2021-10-26 01:59:22 +0000

    Reland "Adds AppsAccessManager for apps streaming setup flow."
    This is a reland of I53a7f1e49c7b5e886962f974ac10ec8304b887bf
    
    The original change have been reverted because MSan builder failures:
    https://chromium-review.googlesource.com/c/chromium/src/+/3234479
    
    We need to initialize current_feature_status_ in apps_access_manager_impl.cc.
    for fixing uninitialized memory access problem.
    
    Original change's description:
    > Adds AppsAccessManager for apps streaming setup flow.
    >
    > ninja -C out/Default chromeos_components_unittests
    > ./out/Default/chromeos_components_unittests --gtest_filter="AppsAccessManagerImplTest*"
    >
    > Bug: b/184137843
    > Test: Pass test cases
    > Change-Id: I53a7f1e49c7b5e886962f974ac10ec8304b887bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220831
    > Commit-Queue: Mavis Hsu <mavishsu@google.com>
    > Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#933331}
    
    Bug: b/184137843
    Test: ./out/Default/chromeos_components_unittests --gtest_filter="AppsAccessManagerImplTest*"
    Change-Id: I1e0b14527ddfa2b7450cff8a2b8ed5bb7d5d5e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234270
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Commit-Queue: Mavis Hsu <mavishsu@google.com>
    Cr-Commit-Position: refs/heads/main@{#934789}

---
+++ chromeos/components/eche_app_ui/pref_names.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/eche_app_ui/pref_names.h"
+
+namespace chromeos {
+namespace eche_app {
+namespace prefs {
+// The last provided apps access status provided by the phone. This pref
+// stores the numerical value associated with the
+// AppsAccessManager::AccessStatus enum.
+const char kAppsAccessStatus[] = "cros.echeapps.apps_access_status";
+}  // namespace prefs
+}  // namespace eche_app
+}  // namespace chromeos
