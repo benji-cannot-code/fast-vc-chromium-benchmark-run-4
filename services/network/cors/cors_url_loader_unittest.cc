0f7dbe4e2f5ee45933551b96d3bf57033bc08a94
ainozaki <ainozaki@google.com>
2021-10-26 04:40:21 +0000

    Revert "CORS: Change the timing of PreflightLoader calling completion callback"
    This reverts commit f603b77df469526167d18839857e626ad8acb626.
    
    Reason for revert: We find alternative way to solve the problem without this patch.
    
    Original change's description:
    > CORS: Change the timing of PreflightLoader calling completion callback
    >
    > PreflightLoader currently calls completion callback once it receives
    > response header and finish handling response. In this CL, we make the
    > timing later and wait SimpleURLLoaderImpl::FinishWithResult. Only the
    > timing is changed and the other behavior is not changed in this CL.
    >
    > Bug: N/A
    > Change-Id: Ia36161a451baaa792f1c87ea078ba7ed2b5d35ec
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220430
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    > Commit-Queue: Ai Nozaki <ainozaki@google.com>
    > Cr-Commit-Position: refs/heads/main@{#932794}
    
    Bug: N/A
    Change-Id: I481a0c8fe8e46044bfba4898197b89dc609c5cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244652
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Ai Nozaki <ainozaki@google.com>
    Cr-Commit-Position: refs/heads/main@{#934831}

--- services/network/cors/cors_url_loader_unittest.cc
+++ services/network/cors/cors_url_loader_unittest.cc
@@ -67,7 +67,7 @@ constexpr char kTestCorsExemptHeader[] = "x-test-cors-exempt";
 
 class TestURLLoaderFactory : public mojom::URLLoaderFactory {
  public:
-  TestURLLoaderFactory() = default;
+  TestURLLoaderFactory() {}
 
   TestURLLoaderFactory(const TestURLLoaderFactory&) = delete;
   TestURLLoaderFactory& operator=(const TestURLLoaderFactory&) = delete;
@@ -1268,7 +1268,6 @@ TEST_F(CorsURLLoaderTest,
   NotifyLoaderClientOnReceiveResponse(
       {{"Access-Control-Allow-Origin", "https://example.com"},
        {"Access-Control-Allow-Methods", "PATCH"}});
-  NotifyLoaderClientOnComplete(net::OK);
   RunUntilCreateLoaderAndStartCalled();
 
   // the actual request
@@ -1299,7 +1298,6 @@ TEST_F(CorsURLLoaderTest,
   NotifyLoaderClientOnReceiveResponse(
       {{"Access-Control-Allow-Origin", "null"},
        {"Access-Control-Allow-Methods", "PATCH"}});
-  NotifyLoaderClientOnComplete(net::OK);
   RunUntilCreateLoaderAndStartCalled();
 
   // the second actual request
@@ -1945,7 +1943,6 @@ TEST_F(CorsURLLoaderTest, RevalidationAndPreflight) {
   NotifyLoaderClientOnReceiveResponse(
       {{"Access-Control-Allow-Origin", "https://example.com"},
        {"Access-Control-Allow-Headers", "foo"}});
-  NotifyLoaderClientOnComplete(net::OK);
   RunUntilCreateLoaderAndStartCalled();
 
   // the actual request
@@ -2770,7 +2767,6 @@ TEST_F(CorsURLLoaderTest, NetLogBasic) {
       {{"Access-Control-Allow-Origin", "https://example.com"},
        {"Access-Control-Allow-Headers", "Apple"},
        {"Access-Control-Allow-Methods", "GET"}});
-  NotifyLoaderClientOnComplete(net::OK);
 
   // Continue the actual request.
   RunUntilCreateLoaderAndStartCalled();
