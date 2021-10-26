f7a4376273b7629a653dae4a644e44799030f10e
Christian Dullweber <dullweber@chromium.org>
2021-10-26 13:48:11 +0000

    ContentSettings: Migrate content settings observers
    Multiple content settings providers use ContentSettingsType::DEFAULT
    to indicate that any setting might have changes. Many observers don't
    handle this case correctly.
    
    We added a wrapper class for ContentSettingsType that enforces
    correctly checking whether the type an observer cares about is affected.
    
    Bug: 1245927
    Change-Id: I40d7594c2aee0571efe0c5885e3454334fe03fe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3139994
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934933}

--- components/signin/core/browser/account_reconcilor.cc
+++ components/signin/core/browser/account_reconcilor.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
+#include "components/content_settings/core/browser/content_settings_observer.h"
 #include "components/signin/core/browser/account_reconcilor_delegate.h"
 #include "components/signin/public/base/account_consistency_method.h"
 #include "components/signin/public/base/signin_client.h"
@@ -304,9 +305,9 @@ void AccountReconcilor::RemoveObserver(Observer* observer) {
 void AccountReconcilor::OnContentSettingChanged(
     const ContentSettingsPattern& primary_pattern,
     const ContentSettingsPattern& secondary_pattern,
-    ContentSettingsType content_type) {
+    ContentSettingsTypeSet content_type_set) {
   // If this is not a change to cookie settings, just ignore.
-  if (content_type != ContentSettingsType::COOKIES)
+  if (!content_type_set.Contains(ContentSettingsType::COOKIES))
     return;
 
   // If this does not affect GAIA, just ignore. The secondary pattern is not
--- components/signin/core/browser/account_reconcilor_unittest.cc
+++ components/signin/core/browser/account_reconcilor_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/mock_timer.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "components/content_settings/core/browser/content_settings_observer.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/account_reconcilor.h"
@@ -384,9 +385,9 @@ void AccountReconcilorTest::SimulateLogOutFromCookieCompleted(
 void AccountReconcilorTest::SimulateCookieContentSettingsChanged(
     content_settings::Observer* observer,
     const ContentSettingsPattern& primary_pattern) {
-  observer->OnContentSettingChanged(primary_pattern,
-                                    ContentSettingsPattern::Wildcard(),
-                                    ContentSettingsType::COOKIES);
+  observer->OnContentSettingChanged(
+      primary_pattern, ContentSettingsPattern::Wildcard(),
+      ContentSettingsTypeSet(ContentSettingsType::COOKIES));
 }
 
 void AccountReconcilorTest::SetAccountConsistency(
