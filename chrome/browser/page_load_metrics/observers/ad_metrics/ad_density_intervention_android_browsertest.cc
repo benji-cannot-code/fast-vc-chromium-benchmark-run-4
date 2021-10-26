6baaae85a71d64bf7d9a130d4afa9289f422347b
Aishwarya Rajesh <aishwaryarj@google.com>
2021-10-26 10:39:07 +0000

    Update fieldtrial_testing_config.json for Ads Blocked message experiment
    Rewrite relevant Java instrumentation tests for Ads Blocked.
    TODO: rewrite C++ instrumentation tests in a subsequent CL.
    
    Bug: 1261346
    Change-Id: I70a67480c61205370bb7affea7f4bf3ac9af0e08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3203271
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934895}

--- chrome/browser/page_load_metrics/observers/ad_metrics/ad_density_intervention_android_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/ad_metrics/ad_density_intervention_android_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar.h"
 #include "components/infobars/core/infobar_delegate.h"
 #include "components/infobars/core/infobar_manager.h"
+#include "components/messages/android/messages_feature.h"
 #include "components/page_load_metrics/browser/observers/ad_metrics/ad_intervention_browser_test_utils.h"
 #include "components/page_load_metrics/browser/page_load_metrics_test_waiter.h"
 #include "components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.h"
@@ -45,7 +46,9 @@ class AdDensityViolationBrowserTest
     std::vector<base::Feature> enabled = {
         subresource_filter::kAdTagging,
         subresource_filter::kAdsInterventionsEnforced};
-    std::vector<base::Feature> disabled = {};
+    // TODO (crbug.com/1261346): Rewrite tests for messages.
+    std::vector<base::Feature> disabled = {
+        messages::kMessagesForAndroidAdsBlocked};
 
     feature_list_.InitWithFeatures(enabled, disabled);
     subresource_filter::SubresourceFilterBrowserTest::SetUp();
--- components/messages/android/internal/java/src/org/chromium/components/messages/SingleActionMessage.java
+++ components/messages/android/internal/java/src/org/chromium/components/messages/SingleActionMessage.java
@@ -168,6 +168,11 @@ public class SingleActionMessage implements MessageStateHandler {
         return mMessageDismissed;
     }
 
+    @VisibleForTesting
+    PropertyModel getModelForTesting() {
+        return mModel;
+    }
+
     @Override
     @MessageIdentifier
     public int getMessageIdentifier() {
