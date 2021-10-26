40a561d97be8ac423dfed4eff26dff2f56c34949
Koji Ishii <kojii@chromium.org>
2021-10-26 01:29:07 +0000

    [AsyncFontAccess] Support generic families and web fonts
    This patch moves the prewarm logic to |CSSFontSelectorBase|,
    because it is where we can resolve generic font families, and
    where we can identify whether it is a web font or not.
    
    |Font::WillUseFontData| also supports non-DOM usages (that
    don't have |FontSelector|,) treating all fonts as local.
    Generic families and web fonts are not supported for non-DOM
    usages.
    
    Bug: 1256946
    Change-Id: Ibfc44f5c5f03fa7e3c93bdbd626778390290910d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3241122
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934776}

--- third_party/blink/renderer/platform/fonts/font_family.h
+++ third_party/blink/renderer/platform/fonts/font_family.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class FontDescription;
 class SharedFontFamily;
 
 class PLATFORM_EXPORT FontFamily {
@@ -65,7 +64,8 @@ class PLATFORM_EXPORT FontFamily {
   void AppendFamily(AtomicString family_name, Type family_type);
   scoped_refptr<SharedFontFamily> ReleaseNext();
 
-  void PrewarmIfNeeded(const FontDescription&) const;
+  bool IsPrewarmed() const { return is_prewarmed_; }
+  void SetIsPrewarmed() const { is_prewarmed_ = true; }
 
   // Returns this font family's name followed by all subsequent linked
   // families separated ", " (comma and space). Font family names are never
@@ -81,8 +81,6 @@ class PLATFORM_EXPORT FontFamily {
   static Type InferredTypeFor(const AtomicString& family_name);
 
  private:
-  void Prewarm(const FontDescription&) const;
-
   AtomicString family_name_;
   scoped_refptr<SharedFontFamily> next_;
   Type family_type_ = Type::kFamilyName;
@@ -127,12 +125,6 @@ inline void FontFamily::AppendFamily(scoped_refptr<SharedFontFamily> family) {
 
 inline scoped_refptr<SharedFontFamily> FontFamily::ReleaseNext() {
   return std::move(next_);
-}
-
-inline void FontFamily::PrewarmIfNeeded(
-    const FontDescription& font_description) const {
-  if (!is_prewarmed_)
-    Prewarm(font_description);
 }
 
 }  // namespace blink
