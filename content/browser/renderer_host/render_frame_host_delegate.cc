bce99b4ce94628b41f6d3144c193f37c208e2fd1
Adrienne Walker <enne@chromium.org>
2021-10-26 21:41:20 +0000

    Window Placement: Require user activation checks for fullscreen swap
    See notes on issue 1254804.  Having no user activation requirement for
    fullscreen swaps creates a large number of potential abusable scenarios
    for multi-screen window placement.  This will make some use cases harder
    in the short term, and we will followup with some compromise solution
    such as requiring an activation (somewhere) or using capability
    delegation.
    
    This reverts the following two patches:
    
    "Skip requestFullscreen checks on current fullscreen element"
    This reverts commit c17358d64ea9ee12cce4d136ac41dc7b1b90421c.
    
    "fullscreen: Don't consume user activation if already fullscreen"
    This reverts commit d6581323abee1bda138956d53c39ea766b751f91.
    
    Bug: 1218483,1254804
    Change-Id: Icb740012bbe9431c415fc9962cafc796e9b508af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232391
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935150}

--- content/browser/renderer_host/render_frame_host_delegate.cc
+++ content/browser/renderer_host/render_frame_host_delegate.cc
@@ -82,10 +82,6 @@ bool RenderFrameHostDelegate::CanEnterFullscreenMode() {
   return true;
 }
 
-bool RenderFrameHostDelegate::HasEnteredFullscreenMode() {
-  return false;
-}
-
 void RenderFrameHostDelegate::FullscreenStateChanged(
     RenderFrameHostImpl* rfh,
     bool is_fullscreen,
