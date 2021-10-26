88ab49b0f3be4daaef23a745b6b5216d422f575e
Elaine Chien <elainechien@chromium.org>
2021-10-26 00:50:54 +0000

    Color Provider: Extract Chrome Color Id names
    To support Color Pipeline in WebUI we will need to translate color Ids
    to CSS. This CL adds a function to get the chrome color Ids needed for
    that translation.
    
    Bug: 1261912
    Change-Id: I151fec25d1af71f52e697db1169169e5baca8b33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235926
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934751}

---
+++ chrome/browser/ui/color/chrome_color_provider_utils_unittest.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome_color_provider_utils.h"
+
+#include "base/test/gtest_util.h"
+#include "chrome/browser/ui/color/chrome_color_id.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/color/color_id.h"
+
+using ChromeColorProviderUtilsTest = ::testing::Test;
+
+TEST_F(ChromeColorProviderUtilsTest, ChromeColorIdNameTest) {
+  EXPECT_EQ("kColorOmniboxBackground",
+            ChromeColorIdName(kColorOmniboxBackground));
+}
