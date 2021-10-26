bbd26ede3d75fce2bf9757f92ae845dee6c8a210
Wen-Chien Wang <wcwang@chromium.org>
2021-10-26 20:13:30 +0000

    Add short interval feature flag to launcher nudge
    As the default time delta between each launcher nudge showing is too
    long for testing (currently one day), this cl added a ash and chrome
    feature flags that uses short intervals for testing if the flags are
    enabled.
    
    Bug: 1226093
    Change-Id: Icac3b631b2eff2076eb3cd4c2ffd1335098b8a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245876
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935113}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -273,6 +273,8 @@ extern const base::Feature kLanguageSettingsUpdate2;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kLauncherAppSort;
 COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const base::Feature kLauncherNudgeShortInterval;
+COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kLicensePackagedOobeFlow;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kLockScreenHideSensitiveNotificationsSupport;
@@ -524,6 +526,7 @@ bool IsInstantTetheringBackgroundAdvertisingSupported();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsKeyboardBasedDisplayArrangementInSettingsEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLauncherAppSortEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLauncherNudgeShortIntervalEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsLicensePackagedOobeFlowEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsLockScreenHideSensitiveNotificationsSupported();
