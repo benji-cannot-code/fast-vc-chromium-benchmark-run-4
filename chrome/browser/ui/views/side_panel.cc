7967bfe9132a7d960831499d9eb635e292f61c04
Peter Boström <pbos@chromium.org>
2021-10-26 17:50:14 +0000

    Don't handle events inside the SidePanel border
    This View is purely decorational and should not receive any events (less
    so process them).
    
    This is also a speculative fix for buttons inside the Lens panel not
    receiving events with the side-panel border enabled.
    
    Bug: None
    Change-Id: Ic769ac140fd6fa50a5a00ec9d039346a1f87666d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245954
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Connie Wan <connily@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935043}

--- chrome/browser/ui/views/side_panel.cc
+++ chrome/browser/ui/views/side_panel.cc
@@ -88,6 +88,8 @@ class BorderView : public views::View {
     SetPaintToLayer();
     layer()->SetFillsBoundsOpaquely(false);
     SetBorder(std::make_unique<SidePanelBorder>());
+    // Don't allow the view to process events.
+    SetCanProcessEventsWithinSubtree(false);
   }
 
   void Layout() override {
