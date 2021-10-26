93c5a8f4847f4e38ddf4d46c535377b4d8cdb646
Michael Bai <michaelbai@chromium.org>
2021-10-26 17:54:44 +0000

    Remove unused AndroidCellularSignalStrength
    This API seems not be used anywhere.
    
    Bug: 1262271
    Change-Id: Ia4c664cce0219ea31517cd02d8a89eaac1b92b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3238023
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ian Swett <ianswett@chromium.org>
    Commit-Queue: Ian Swett <ianswett@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935045}

--- net/nqe/network_quality_estimator.cc
+++ net/nqe/network_quality_estimator.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if defined(OS_ANDROID)
-#include "net/android/cellular_signal_strength.h"
 #include "net/android/network_library.h"
 #endif  // OS_ANDROID
 
