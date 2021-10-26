8ec3e8715d15c49b3045ed5902d0439e5d940d20
Thomas Guilbert <tguilbert@chromium.org>
2021-10-26 01:08:45 +0000

    Fix null deref in OnError()
    When a DeviceListenerOutputStream receives an error from its underlying
    OutputStream, we delay reporting the error for 1s. This helps swallow
    errors during device changes, when the DLOS would destroyed anyways.
    
    If we stop() the DLOS while its call to OnError() is pending, we run
    into a null-deref, from having cleared the callback.
    
    This CL fixes the issue.
    
    Bug: 1262893
    Test: Added a UT, and A/B tested that the UT crashes for unfixed code.
    Change-Id: Ia756049617fa317bc360a0c883340b62bd02eff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3243664
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#934762}

--- services/audio/device_listener_output_stream.cc
+++ services/audio/device_listener_output_stream.cc
@@ -108,7 +108,8 @@ void DeviceListenerOutputStream::OnError(ErrorType type) {
 void DeviceListenerOutputStream::ReportError(ErrorType type) {
   DCHECK(task_runner_->BelongsToCurrentThread());
   DCHECK_NE(type, ErrorType::kDeviceChange);
-  source_callback_->OnError(type);
+  if (source_callback_)
+    source_callback_->OnError(type);
 }
 
 }  // namespace audio
