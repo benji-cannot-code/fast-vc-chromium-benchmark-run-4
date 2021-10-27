c25077b6c8efd30838b98bd2ee534566bad0e842
Christian Dullweber <dullweber@chromium.org>
2021-10-27 11:37:01 +0000

    ContentSettings: Migrate content settings observers
    Multiple content settings providers use ContentSettingsType::DEFAULT
    to indicate that any setting might have changes. Many observers don't
    handle this case correctly.
    
    We added a wrapper class for ContentSettingsType that enforces
    correctly checking whether the type an observer cares about is affected.
    
    Bug: 1245927
    Change-Id: Ia14d0c1318e877d61c823bf3d9d67991dfd89467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3243948
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935380}

--- chrome/browser/notifications/notification_channels_provider_android_unittest.cc
+++ chrome/browser/notifications/notification_channels_provider_android_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/content_settings_mock_observer.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/content_settings/core/browser/content_settings_observer.h"
 #include "components/content_settings/core/browser/content_settings_pref.h"
 #include "components/content_settings/core/browser/content_settings_rule.h"
 #include "components/content_settings/core/browser/content_settings_utils.h"
