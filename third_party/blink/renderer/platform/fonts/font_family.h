3ffa19c7d6bfff4c1afe19f6a8737396d5fd8a3c
Koji Ishii <kojii@chromium.org>
2021-10-27 05:59:44 +0000

    Revert "[AsyncFontAccess] Support generic families and web fonts"
    This reverts commit fbdf0b2be29a1253612ee53b19d57bab1166f8e8.
    
    Reason for revert: crbug.com/1263423
    
    Original change's description:
    > [AsyncFontAccess] Support generic families and web fonts
    >
    > This patch moves the prewarm logic to |CSSFontSelectorBase|,
    > because it is where we can resolve generic font families, and
    > where we can identify whether it is a web font or not.
    >
    > |Font::WillUseFontData| also supports non-DOM usages (that
    > don't have |FontSelector|,) treating all fonts as local.
    > Generic families and web fonts are not supported for non-DOM
    > usages.
    >
    > Bug: 1256946
    > Change-Id: Ibfc44f5c5f03fa7e3c93bdbd626778390290910d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3241122
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Auto-Submit: Koji Ishii <kojii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#934776}
    
    Bug: 1263423, 1256946
    Change-Id: Ibded4a912c97fdd5f96e71158ee83053d0a92d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245168
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935307}

--- third_party/blink/renderer/platform/fonts/font_family.h
+++ third_party/blink/renderer/platform/fonts/font_family.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class FontDescription;
 class SharedFontFamily;
 
 class PLATFORM_EXPORT FontFamily {
@@ -64,8 +65,7 @@ class PLATFORM_EXPORT FontFamily {
   void AppendFamily(AtomicString family_name, Type family_type);
   scoped_refptr<SharedFontFamily> ReleaseNext();
 
-  bool IsPrewarmed() const { return is_prewarmed_; }
-  void SetIsPrewarmed() const { is_prewarmed_ = true; }
+  void PrewarmIfNeeded(const FontDescription&) const;
 
   // Returns this font family's name followed by all subsequent linked
   // families separated ", " (comma and space). Font family names are never
@@ -81,6 +81,8 @@ class PLATFORM_EXPORT FontFamily {
   static Type InferredTypeFor(const AtomicString& family_name);
 
  private:
+  void Prewarm(const FontDescription&) const;
+
   AtomicString family_name_;
   scoped_refptr<SharedFontFamily> next_;
   Type family_type_ = Type::kFamilyName;
@@ -125,6 +127,12 @@ inline void FontFamily::AppendFamily(scoped_refptr<SharedFontFamily> family) {
 
 inline scoped_refptr<SharedFontFamily> FontFamily::ReleaseNext() {
   return std::move(next_);
+}
+
+inline void FontFamily::PrewarmIfNeeded(
+    const FontDescription& font_description) const {
+  if (!is_prewarmed_)
+    Prewarm(font_description);
 }
 
 }  // namespace blink
