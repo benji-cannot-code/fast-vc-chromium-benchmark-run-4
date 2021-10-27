06dd6cb7edd393014e80273f3168020586ad28b9
Sophey <sophey@chromium.org>
2021-10-27 14:52:35 +0000

    [LR] Implement resize/rotate button.
    - Rotates/resizes around a fixed center point
    - Records the current state when the user touches the button, then
    calculates the new width/height/location/angle when the touch point is
    moved
    - Also adjusts the duplicate method to copy the rotation angle when
    duplicated as well
    
    Fixed: 1263025
    Change-Id: Id47bad723241c7c694642ac62760d2eb70857445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245884
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Sophey <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935422}

--- chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/scene/SceneCoordinator.java
+++ chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/scene/SceneCoordinator.java
@@ -106,6 +106,7 @@ public class SceneCoordinator implements SceneEditorDelegate, ToolbarReactionsDe
         int offsetPx = ViewUtils.dpToPx(mActivity, REACTION_OFFSET_DP);
         newLayoutParams.leftMargin = oldLayoutParams.leftMargin + offsetPx;
         newLayoutParams.topMargin = oldLayoutParams.topMargin + offsetPx;
+        newReactionLayout.setRotation(reactionLayout.getRotation());
 
         addReaction(newReactionLayout, newLayoutParams);
     }
