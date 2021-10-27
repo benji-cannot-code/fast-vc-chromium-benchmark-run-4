7f2d3fc0dfe613308e062e9a32909aa1ad5c7cd9
Ahmed Mehfooz <amehfooz@chromium.org>
2021-10-27 23:42:34 +0000

    Refactor `AshNotificationView` construction
    This CL replaces the current views construction in
    `AshNotificationView` to use views::Builder.
    
    This also requires modifying constructors of various
    classes used in `AshNotificationView` to have no special inputs
    to their constructors.
    
    Bug: 1241983
    Change-Id: I3f94085959523f299e51a67013d4058849192e4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3240145
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935661}

--- ui/message_center/views/notification_input_container.h
+++ ui/message_center/views/notification_input_container.h
@@ -36,7 +36,8 @@ class MESSAGE_CENTER_EXPORT NotificationInputContainer
     : public views::View,
       public views::TextfieldController {
  public:
-  explicit NotificationInputContainer(NotificationInputDelegate* delegate);
+  explicit NotificationInputContainer(
+      NotificationInputDelegate* delegate = nullptr);
   NotificationInputContainer(const NotificationInputContainer&) = delete;
   NotificationInputContainer& operator=(const NotificationInputContainer&) =
       delete;
