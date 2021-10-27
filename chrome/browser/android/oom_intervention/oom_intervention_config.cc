8bfe6caa252a07159eea1c0f67b4874834879d97
Caitlin Fischer <caitlinfischer@google.com>
2021-10-27 01:20:44 +0000

    Clean up some dead code in oom_intervention_config.cc.
    Change-Id: I10c5cd94ac19bf15b391e6a0a72946967f3a815d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245204
    Auto-Submit: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935248}

--- chrome/browser/android/oom_intervention/oom_intervention_config.cc
+++ chrome/browser/android/oom_intervention/oom_intervention_config.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/process/process_metrics.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/system/sys_info.h"
@@ -100,15 +99,6 @@ bool GetSwapFreeThreshold(uint64_t* threshold) {
   *threshold = static_cast<uint64_t>(memory_info.swap_total * threshold_ratio);
   return true;
 }
-
-// These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
-enum class OomInterventionBrowserMonitorStatus {
-  kEnabledWithValidConfig = 0,
-  kDisabledWithInvalidParam = 1,
-  kEnabledWithNoSwap = 2,
-  kMaxValue = kEnabledWithNoSwap
-};
 
 }  // namespace
 
