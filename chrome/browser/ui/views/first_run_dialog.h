af854f4f0634ca64fdf376f40f6c847e922a4816
Peter Kasting <pkasting@chromium.org>
2021-10-27 00:31:11 +0000

    Remove GridLayout from first_run_dialog.cc.
    Also adds a simple UI test to show the dialog.
    
    Bug: 1261301
    Change-Id: Ic04105a2221cfa9b66fcdd863886a66015ee66b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3246339
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935229}

--- chrome/browser/ui/views/first_run_dialog.h
+++ chrome/browser/ui/views/first_run_dialog.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/window/dialog_delegate.h"
 
-class Profile;
-
 namespace views {
 class Checkbox;
 }
@@ -25,10 +23,12 @@ class FirstRunDialog : public views::DialogDelegateView {
   FirstRunDialog& operator=(const FirstRunDialog&) = delete;
 
   // Displays the first run UI for reporting opt-in, import data etc.
-  static void Show(Profile* profile);
+  static void Show(base::RepeatingClosure learn_more_callback,
+                   base::RepeatingClosure quit_runloop);
 
  private:
-  explicit FirstRunDialog(Profile* profile);
+  FirstRunDialog(base::RepeatingClosure learn_more_callback,
+                 base::RepeatingClosure quit_runloop);
   ~FirstRunDialog() override;
 
   // This terminates the nested message-loop.
