4d77eb3271d856660baacaca4106239e113d4fc3
Stephan Hartmann <stha09@googlemail.com>
2021-10-27 18:23:19 +0000

    IWYU: add string for std::string in viz::SharedElementResourceId
    Bug: 957519
    Change-Id: I57a276a9704a521be205fa5a8eeacb5a1c6883d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244417
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#935497}

--- components/viz/common/shared_element_resource_id.h
+++ components/viz/common/shared_element_resource_id.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_COMMON_SHARED_ELEMENT_RESOURCE_ID_H_
 #define COMPONENTS_VIZ_COMMON_SHARED_ELEMENT_RESOURCE_ID_H_
 
+#include <string>
 #include <vector>
 
 #include "components/viz/common/viz_common_export.h"
