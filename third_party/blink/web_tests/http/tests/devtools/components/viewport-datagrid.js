eaeaf8c977375b33e80f311b3aefb861770816eb
Tim van der Lippe <tvanderlippe@chromium.org>
2021-10-27 15:26:28 +0000

    Re-enable data_grid layout tests
    They can be enabled after https://crrev.com/c/3242142 rolled into
    Chromium.
    
    R=szuend@chromium.org
    
    Bug: 1127902
    Change-Id: Ida62975d07da05039117df82718ef9724fe1d060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242482
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935434}

--- third_party/blink/web_tests/http/tests/devtools/components/viewport-datagrid.js
+++ third_party/blink/web_tests/http/tests/devtools/components/viewport-datagrid.js
@@ -68,16 +68,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var b = new DataGrid.ViewportDataGridNode({id: 'b'});
 
   var root = dataGrid.rootNode();
+  var widget = dataGrid.asWidget();
+  widget.markAsRoot();
 
   var containerElement = document.body.createChild('div');
   containerElement.style.position = 'absolute';
   containerElement.style.width = '300px';
   containerElement.style.height = '300px';
   containerElement.style.overflow = 'hidden';
-  containerElement.appendChild(dataGrid.element);
-  dataGrid.wasShown();
+  widget.show(containerElement);
   dataGrid.element.style.width = '100%';
   dataGrid.element.style.height = '100%';
+  widget.element.style.width = '100%';
+  widget.element.style.height = '100%';
 
   TestRunner.addResult('Building tree.');
 
--- third_party/blink/web_tests/http/tests/devtools/unit/viewport-datagrid-items-expandable-attached-to-dom.js
+++ third_party/blink/web_tests/http/tests/devtools/unit/viewport-datagrid-items-expandable-attached-to-dom.js
@@ -9,8 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   var columns = [{id: "id", title: "ID column", width: "250px"}];
   var dataGrid = new DataGrid.ViewportDataGrid({displayName: 'Test', columns});
-  div.appendChild(dataGrid.element);
+  var widget = dataGrid.asWidget();
+  widget.show(div);
+  dataGrid.element.style.width = '100%';
   dataGrid.element.style.height = '150px';
+  widget.element.style.width = '100%';
+  widget.element.style.height = '100%';
 
   var rootNode = dataGrid.rootNode();
   var nodes = [];
