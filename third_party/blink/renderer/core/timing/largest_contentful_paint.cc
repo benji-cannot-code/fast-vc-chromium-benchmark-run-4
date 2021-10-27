38825a64255efd8a1957e4d46a885cc1b3e6807c
Mikel Astiz <mastiz@chromium.org>
2021-10-27 09:13:47 +0000

    Revert "[LCP] Add animated image support"
    This reverts commit b7d510c06e0436cfb4bd7260175cd460b949225c.
    
    Reason for revert: speculative revert for new flakiness in
    PageLoadMetricsBrowserTestWithAnimatedLCPFlag, see first failing
    build
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29/99853
    
    Original change's description:
    > [LCP] Add animated image support
    >
    > This CL adds support for better handling of animated images in LCP:
    > * A new attribute is exposing the first animated frame's paint time
    > (behind a flag).
    > * `startTime` is not changed.
    > * The PageLoadMetrics reported for LCP are set to that first frame paint
    > time for animated images (behind another flag).
    > * Entries are not emitted until the image is loaded.
    >
    > Relevant spec issue:
    > https://github.com/WICG/largest-contentful-paint/issues/83
    >
    > Change-Id: I6bb01eacb4f200f9c032ffcfcd9a1a41126a7773
    > Bug: 1260953
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226157
    > Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    > Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#935133}
    
    Bug: 1260953
    Change-Id: I00878689de95de38645195da81c6baf577d868a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247071
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Owners-Override: Mikel Astiz <mastiz@google.com>
    Cr-Commit-Position: refs/heads/main@{#935350}

--- third_party/blink/renderer/core/timing/largest_contentful_paint.cc
+++ third_party/blink/renderer/core/timing/largest_contentful_paint.cc
@@ -12,20 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-LargestContentfulPaint::LargestContentfulPaint(
-    double start_time,
-    base::TimeDelta render_time,
-    uint64_t size,
-    base::TimeDelta load_time,
-    base::TimeDelta first_animated_frame_time,
-    const AtomicString& id,
-    const String& url,
-    Element* element)
+LargestContentfulPaint::LargestContentfulPaint(double start_time,
+                                               base::TimeDelta render_time,
+                                               uint64_t size,
+                                               base::TimeDelta load_time,
+                                               const AtomicString& id,
+                                               const String& url,
+                                               Element* element)
     : PerformanceEntry(g_empty_atom, start_time, start_time),
       size_(size),
       render_time_(render_time),
       load_time_(load_time),
-      first_animated_frame_time_(first_animated_frame_time),
       id_(id),
       url_(url),
       element_(element) {}
@@ -57,8 +54,6 @@ void LargestContentfulPaint::BuildJSONValue(V8ObjectBuilder& builder) const {
   builder.Add("size", size_);
   builder.Add("renderTime", render_time_.InMillisecondsF());
   builder.Add("loadTime", load_time_.InMillisecondsF());
-  builder.Add("firstAnimatedFrameTime",
-              first_animated_frame_time_.InMillisecondsF());
   builder.Add("id", id_);
   builder.Add("url", url_);
 }
