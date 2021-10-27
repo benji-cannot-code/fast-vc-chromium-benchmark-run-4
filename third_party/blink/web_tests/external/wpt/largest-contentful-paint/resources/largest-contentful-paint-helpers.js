2c62c5f1ebca5f5587cf2ecb1254154fb86b9fb4
Yoav Weiss <yoavweiss@chromium.org>
2021-10-27 22:41:42 +0000

    Reland "[LCP] Add animated image support"
    This is a reland of b7d510c06e0436cfb4bd7260175cd460b949225c
    
    Original change's description:
    > [LCP] Add animated image support
    >
    > This CL adds support for better handling of animated images in LCP:
    > * A new attribute is exposing the first animated frame's paint time
    > (behind a flag).
    > * `startTime` is not changed.
    > * The PageLoadMetrics reported for LCP are set to that first frame paint
    > time for animated images (behind another flag).
    > * Entries are not emitted until the image is loaded.
    >
    > Relevant spec issue:
    > https://github.com/WICG/largest-contentful-paint/issues/83
    >
    > Change-Id: I6bb01eacb4f200f9c032ffcfcd9a1a41126a7773
    > Bug: 1260953
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3226157
    > Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    > Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#935133}
    
    Bug: 1260953
    Change-Id: I5eaaf0cfd1daa7fb905e68aed4994cb931dc7750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247449
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935635}

--- third_party/blink/web_tests/external/wpt/largest-contentful-paint/resources/largest-contentful-paint-helpers.js
+++ third_party/blink/web_tests/external/wpt/largest-contentful-paint/resources/largest-contentful-paint-helpers.js
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const image_delay = 1000;
+const delay_pipe_value = image_delay / 1000;
+
 // Receives an image LargestContentfulPaint |entry| and checks |entry|'s attribute values.
 // The |timeLowerBound| parameter is a lower bound on the loadTime value of the entry.
 // The |options| parameter may contain some string values specifying the following:
@@ -34,4 +37,33 @@ function checkImage(entry, expectedUrl, expectedID, expectedSize, timeLowerBound
   } else {
     assert_equals(entry.size, expectedSize);
   }
+  if (options.includes('animated')) {
+    assert_greater_than(entry.loadTime, entry.firstAnimatedFrameTime,
+      'firstAnimatedFrameTime should be smaller than loadTime');
+    assert_greater_than(entry.renderTime, entry.firstAnimatedFrameTime,
+      'firstAnimatedFrameTime should be smaller than renderTime');
+    assert_less_than(entry.firstAnimatedFrameTime, image_delay,
+      'firstAnimatedFrameTime should be smaller than the delay applied to the second frame');
+    assert_greater_than(entry.firstAnimatedFrameTime, 0,
+      'firstAnimatedFrameTime should be larger than 0');
+  }
+  if (options.includes('animated-zero')) {
+    assert_equals(entry.firstAnimatedFrameTime, 0, 'firstAnimatedFrameTime should be 0');
+  }
 }
+
+const load_and_observe = url => {
+  return new Promise(resolve => {
+    (new PerformanceObserver(entryList => {
+      for (let entry of entryList.getEntries()) {
+        if (entry.url == url) {
+          resolve(entryList.getEntries()[0]);
+        }
+      }
+    })).observe({type: 'largest-contentful-paint', buffered: true});
+    const img = new Image();
+    img.id = 'image_id';
+    img.src = url;
+    document.body.appendChild(img);
+  });
+};
