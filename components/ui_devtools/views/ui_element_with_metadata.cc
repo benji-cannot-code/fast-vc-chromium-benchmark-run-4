8e7766f67b000aeacff2f13664f5c2c88b86b3de
Andrey Kosyakov <caseq@chromium.org>
2021-10-27 21:10:57 +0000

    IWYU logging.h in .../ui_devtools/views/ui_element_with_metadata.cc
    ... right now, it depeonds on an include from inspector_protocol, where it's not
    actually needed.
    
    Change-Id: I49f543f9eff73714533b780a06a2f02cce49b44e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248638
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935579}

--- components/ui_devtools/views/ui_element_with_metadata.cc
+++ components/ui_devtools/views/ui_element_with_metadata.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/ui_devtools/views/ui_element_with_metadata.h"
 
+#include "base/logging.h"
 #include "base/notreached.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
