e305d170e248751a4a2b0f92ba223e5e93a6b365
Morten Stenshorne <mstensho@chromium.org>
2021-10-27 13:58:27 +0000

    Avoid cyclic dependency between ng_link and ng_physical_fragment.
    ng_link.h included ng_physical_fragment.h, and ng_physical_fragment.h
    included ng_link.h.
    
    Forward-declare NGPhysicalFragment in ng_link.h instead.
    
    Change-Id: I5dd659ba4dc8efc86db5f75900a3fa30d936b333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247152
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935411}

--- third_party/blink/renderer/core/layout/ng/ng_link.h
+++ third_party/blink/renderer/core/layout/ng/ng_link.h
@@ -8,9 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_offset.h"
-#include "third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h"
 
 namespace blink {
+
+class NGPhysicalFragment;
 
 // Class representing the offset of a child fragment relative to the
 // parent fragment. Fragments themselves have no position information
