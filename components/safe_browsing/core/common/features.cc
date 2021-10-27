0fd1d408595b09d65b533d2ec76b6fd12f356727
Rohit Bhatia <bhatiarohit@google.com>
2021-10-27 00:59:43 +0000

    Clean up GAIA-keying PhishGuard launch
    Fixed: 1261976
    Change-Id: I006a9cd9985784acddb75c65b5616bc6451d5004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3243794
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Rohit Bhatia <bhatiarohit@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935239}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -94,10 +94,6 @@ const base::Feature kPasswordProtectionForSignedInUsers {
 #endif
 };
 
-const base::Feature kPasswordProtectionWithToken{
-    "SafeBrowsingPasswordProtectionRequestWithToken",
-    base::FEATURE_ENABLED_BY_DEFAULT};
-
 const base::Feature kPromptEsbForDeepScanning{
     "SafeBrowsingPromptEsbForDeepScanning", base::FEATURE_ENABLED_BY_DEFAULT};
 
@@ -173,7 +169,6 @@ constexpr struct {
     {&kFileTypePoliciesTag, true},
     {&kOmitNonUserGesturesFromReferrerChain, true},
     {&kPasswordProtectionForSignedInUsers, true},
-    {&kPasswordProtectionWithToken, true},
     {&kRealTimeUrlLookupReferrerChain, true},
     {&kRealTimeUrlLookupReferrerChainForEnterprise, true},
     {&kSafeBrowsingPageLoadToken, true},
