dc227f309e58684958a9de38256650f70d24bfe0
Gavin Dodd <gavindodd@google.com>
2021-10-27 01:31:45 +0000

    shimless: Check for OS updates.
    Check Chrome OS updates are available in Welcome or WiFi connect states
    before choosing the next state.
    Only switch to 'Update Chrome OS' state if an update is available.
    
    Change-Id: Ie7c7bc6a0bedd727a56774cddd9e5df4a562c642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235606
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#935255}

--- ash/webui/shimless_rma/resources/onboarding_update_page.js
+++ ash/webui/shimless_rma/resources/onboarding_update_page.js
@@ -68,12 +68,6 @@ export class OnboardingUpdatePageElement extends PolymerElement {
       },
 
       /** @protected */
-      checkInProgress_: {
-        type: Boolean,
-        value: false,
-      },
-
-      /** @protected */
       updateInProgress_: {
         type: Boolean,
         value: false,
@@ -83,6 +77,12 @@ export class OnboardingUpdatePageElement extends PolymerElement {
       updateAvailable_: {
         type: Boolean,
         value: false,
+      },
+
+      /** @protected */
+      updateVersion_: {
+        type: String,
+        value: '',
       }
     };
   }
@@ -108,6 +108,7 @@ export class OnboardingUpdatePageElement extends PolymerElement {
   ready() {
     super.ready();
     this.getCurrentVersionText_();
+    this.checkForUdpates_();
     this.dispatchEvent(new CustomEvent(
         'disable-next-button',
         {bubbles: true, composed: true, detail: false},
@@ -125,12 +126,12 @@ export class OnboardingUpdatePageElement extends PolymerElement {
     // TODO(joonbug): i18n string
   }
 
-  /** @protected */
-  onUpdateCheckButtonClicked_() {
-    this.checkInProgress_ = true;
+  /** @private */
+  checkForUdpates_() {
     this.shimlessRmaService_.checkForOsUpdates().then((res) => {
-      if (res.updateAvailable) {
+      if (res && res.updateAvailable) {
         this.updateAvailable_ = true;
+        this.updateVersion_ = res.version;
         // TODO(joonbug): i18n string
         this.currentVersionText_ =
             `Current version ${this.currentVersion_} is out of date`;
@@ -139,18 +140,25 @@ export class OnboardingUpdatePageElement extends PolymerElement {
         this.currentVersionText_ =
             `Current version ${this.currentVersion_} is up to date`;
       }
-      this.checkInProgress_ = false;
     });
   }
 
   /** @protected */
   onUpdateButtonClicked_() {
     this.updateInProgress_ = true;
+    this.dispatchEvent(new CustomEvent(
+        'disable-next-button',
+        {bubbles: true, composed: true, detail: false},
+        ));
     this.shimlessRmaService_.updateOs().then((res) => {
       if (!res.updateStarted) {
         // TODO(gavindodd): i18n string
         this.updateProgressMessage_ = 'OS update failed';
         this.updateInProgress_ = false;
+        this.dispatchEvent(new CustomEvent(
+            'disable-next-button',
+            {bubbles: true, composed: true, detail: false},
+            ));
       }
     });
   }
@@ -173,8 +181,19 @@ export class OnboardingUpdatePageElement extends PolymerElement {
    * @param {number} progress
    */
   onOsUpdateProgressUpdated(operation, progress) {
-    if (operation === OsUpdateOperation.kIdle) {
+    // Ignore progress when not updating, it is just the update available check.
+    if (!this.updateInProgress_) {
+      return;
+    }
+    if (operation === OsUpdateOperation.kIdle ||
+        operation === OsUpdateOperation.kReportingErrorEvent ||
+        operation === OsUpdateOperation.kNeedPermissionToUpdate ||
+        operation === OsUpdateOperation.kDisabled) {
       this.updateInProgress_ = false;
+      this.dispatchEvent(new CustomEvent(
+          'disable-next-button',
+          {bubbles: true, composed: true, detail: false},
+          ));
     }
     // TODO(gavindodd): i18n string
     this.updateProgressMessage_ = 'OS update progress received ' +
--- chrome/test/data/webui/chromeos/shimless_rma/onboarding_update_page_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/onboarding_update_page_test.js
@@ -43,7 +43,7 @@ export function onboardingUpdatePageTest() {
 
     // Initialize the fake data.
     service.setGetCurrentOsVersionResult(version);
-    service.setCheckForOsUpdatesResult(updateAvailable);
+    service.setCheckForOsUpdatesResult(updateAvailable, 'fake version');
     service.setUpdateOsResult(updateAvailable);
 
     component = /** @type {!OnboardingUpdatePageElement} */ (
@@ -51,16 +51,6 @@ export function onboardingUpdatePageTest() {
     assertTrue(!!component);
     document.body.appendChild(component);
 
-    return flushTasks();
-  }
-
-  /**
-   * @return {!Promise}
-   */
-  function clickCheckUpdateButton() {
-    const checkUpdateButton =
-        component.shadowRoot.querySelector('#checkUpdate');
-    checkUpdateButton.click();
     return flushTasks();
   }
 
@@ -99,13 +89,10 @@ export function onboardingUpdatePageTest() {
         .then(() => {
           const networkUnavailable =
               component.shadowRoot.querySelector('#networkUnavailable');
-          const checkUpdateButton =
-              component.shadowRoot.querySelector('#checkUpdate');
           const updateButton =
               component.shadowRoot.querySelector('#performUpdate');
 
           assertFalse(networkUnavailable.hidden);
-          assertTrue(checkUpdateButton.hidden);
           assertTrue(updateButton.hidden);
         });
   });
@@ -119,7 +106,6 @@ export function onboardingUpdatePageTest() {
           component.networkAvailable = true;
           return flushTasks();
         })
-        .then(() => clickCheckUpdateButton())
         .then(() => {
           const versionComponent =
               component.shadowRoot.querySelector('#versionInfo');
@@ -139,7 +125,6 @@ export function onboardingUpdatePageTest() {
           component.networkAvailable = true;
           return flushTasks();
         })
-        .then(() => clickCheckUpdateButton())
         .then(() => {
           const versionComponent =
               component.shadowRoot.querySelector('#versionInfo');
@@ -164,7 +149,6 @@ export function onboardingUpdatePageTest() {
           component.networkAvailable = true;
           return flushTasks();
         })
-        .then(() => clickCheckUpdateButton())
         .then(() => {
           const versionComponent =
               component.shadowRoot.querySelector('#versionInfo');
@@ -194,6 +178,7 @@ export function onboardingUpdatePageTest() {
     const progressComponent =
         component.shadowRoot.querySelector('#progressMessage');
     assertEquals('', progressComponent.textContent);
+    await clickPerformUpdateButton();
 
     service.triggerOsUpdateObserver(OsUpdateOperation.kDownloading, 0.5, 0);
     await flushTasks();
