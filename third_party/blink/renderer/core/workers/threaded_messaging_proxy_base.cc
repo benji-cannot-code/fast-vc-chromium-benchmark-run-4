9b2d1a5e5f04bed040dce0731245481de6388543
Hajime Hoshi <hajimehoshi@chromium.org>
2021-10-28 10:51:40 +0000

    Add a new feature flag to enable DedicatedWorker for BFcache
    This flag is needed to add tests to make pages with DedicatedWorker
    eligible for back-forward cache.
    
    Design Doc: https://docs.google.com/document/d/1CJfDNqA2tEp_dmxK5kaBHHqGjx7cK2_jhTpsCwSCa3A/edit?usp=sharing
    
    Bug: 1146955
    Change-Id: I4269086f243c08c2de5ead93b19f41f28ba49d3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244915
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935821}

--- third_party/blink/renderer/core/workers/threaded_messaging_proxy_base.cc
+++ third_party/blink/renderer/core/workers/threaded_messaging_proxy_base.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/workers/threaded_messaging_proxy_base.h"
 
+#include "base/feature_list.h"
 #include "base/synchronization/waitable_event.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_worker_fetch_context.h"
 #include "third_party/blink/renderer/bindings/core/v8/source_location.h"
@@ -37,9 +39,12 @@ ThreadedMessagingProxyBase::ThreadedMessagingProxyBase(
           base::WaitableEvent::ResetPolicy::MANUAL,
           base::WaitableEvent::InitialState::NOT_SIGNALED),
       feature_handle_for_scheduler_(
-          execution_context->GetScheduler()->RegisterFeature(
-              SchedulingPolicy::Feature::kDedicatedWorkerOrWorklet,
-              {SchedulingPolicy::DisableBackForwardCache()})),
+          base::FeatureList::IsEnabled(
+              features::kBackForwardCacheDedicatedWorker)
+              ? FrameOrWorkerScheduler::SchedulingAffectingFeatureHandle()
+              : execution_context->GetScheduler()->RegisterFeature(
+                    SchedulingPolicy::Feature::kDedicatedWorkerOrWorklet,
+                    {SchedulingPolicy::DisableBackForwardCache()})),
       keep_alive_(PERSISTENT_FROM_HERE, this) {
   DCHECK(IsParentContextThread());
   g_live_messaging_proxy_count++;
