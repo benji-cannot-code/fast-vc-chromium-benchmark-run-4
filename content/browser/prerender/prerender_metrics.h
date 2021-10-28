ae44331a34ab20e6e36616e60bffc198f8f627f3
Robert Lin <robertlin@chromium.org>
2021-10-28 12:50:55 +0000

    prerender: Remove the dependency on RenderFrameHost from PrerenderHost.
    Currently, PrerenderHost and PrerenderHostRegistry depends on
    RenderFrameHost to start. However, there is no initiator RFH from
    Omnibox triggered prerendering, so it is desired to remove such
    dependency.
    
    Bug: 1166085
    Change-Id: Ib9b4e7af568fa52e33821e2d6b4db1eea576c5cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3236248
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935846}

--- content/browser/prerender/prerender_metrics.h
+++ content/browser/prerender/prerender_metrics.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-namespace content {
+#include "services/metrics/public/cpp/ukm_source_id.h"
 
-class RenderFrameHostImpl;
+namespace content {
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
@@ -26,7 +26,7 @@ enum class PrerenderCancelledInterface {
 
 void RecordPrerenderCancelledInterface(const std::string& interface_name);
 
-void RecordPrerenderTriggered(RenderFrameHostImpl& initiator_render_frame_host);
+void RecordPrerenderTriggered(ukm::SourceId ukm_id);
 
 }  // namespace content
 
