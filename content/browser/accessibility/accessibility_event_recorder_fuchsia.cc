d39182b09edd0e15cab931b5212129b14914bf2f
Alexander Brusher <abrusher@google.com>
2021-10-28 20:25:48 +0000

    [fuchsia][a11y] Add platform accessibility support for fuchsia.
    This change adds skeletons for the fuchsia classes that constitute
    platform accessibility support, and flips the has_platform_accessibility
    flag to include is_fuchsia.
    
    AX-Relnotes: n/a
    Test: content_unittests
    Bug: 1261576
    Change-Id: Idcdc1819bb41a51f2ff31b0341b2beb66a6a56bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232602
    Commit-Queue: Alexander Brusher <abrusher@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936069}

---
+++ content/browser/accessibility/accessibility_event_recorder_fuchsia.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/accessibility/accessibility_event_recorder_fuchsia.h"
+
+namespace content {
+
+// static
+AccessibilityEventRecorderFuchsia*
+    AccessibilityEventRecorderFuchsia::instance_ = nullptr;
+
+AccessibilityEventRecorderFuchsia::AccessibilityEventRecorderFuchsia(
+    BrowserAccessibilityManager* manager,
+    base::ProcessId pid,
+    const ui::AXTreeSelector& selector)
+    : AccessibilityEventRecorder(manager) {
+  CHECK(!instance_) << "There can be only one instance of"
+                    << " AccessibilityEventRecorder at a time.";
+  instance_ = this;
+}
+
+AccessibilityEventRecorderFuchsia::~AccessibilityEventRecorderFuchsia() {
+  instance_ = nullptr;
+}
+
+}  // namespace content
