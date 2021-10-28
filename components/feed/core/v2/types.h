03c0860ee9011e3b0eab334cf5be1b36b317223c
Jian Li <jianli@chromium.org>
2021-10-28 19:42:36 +0000

    Reland: Track the interaction of the feed notice
    This is the reland of patch
    https://chromium-review.googlesource.com/c/chromium/src/+/3219421
    
    Track the interaction, i.e. viewed or clicked of the feed
    notice in order to determine its acknowledgement state.
    
    Bug: 1258202
    Change-Id: I9b54fbbb89805b95706a2fc4c78768db25d69121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251643
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936041}

--- components/feed/core/v2/types.h
+++ components/feed/core/v2/types.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <iosfwd>
 #include <string>
+#include <vector>
 
 #include "base/containers/flat_set.h"
 #include "base/time/time.h"
@@ -54,6 +55,7 @@ struct RequestMetadata {
   ContentOrder content_order = ContentOrder::kUnspecified;
   bool notice_card_acknowledged = false;
   bool autoplay_enabled = false;
+  std::vector<std::string> acknowledged_notice_keys;
 };
 
 // Data internal to MetricsReporter which is persisted to Prefs.
