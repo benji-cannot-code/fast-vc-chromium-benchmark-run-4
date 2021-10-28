078da1683ae707d19471f82e4c3c91ff8b388194
Dan McArdle <dmcardle@chromium.org>
2021-10-28 13:57:24 +0000

    liburlpattern: Add missing check for U8_NEXT return to Tokenizer
    The ICU macro U8_NEXT's documentation [0] indicates that it may return a
    negative codepoint on failure.
    
    This CL annotates the Tokenizer::Next() and NextAt() methods with
    WARN_UNUSED_RESULT, and adds appropriate error-handling to affected call
    sites.
    
    Prior to this change, the original input (below) was parsed and
    canonicalized to a string that failed the canonicalization idempotency
    property check. With this change, the original input is rejected at
    position 1 because 0xcd is an illegal UTF8 codepoint.
    
        original :
           *      *   {   }   ?   *   ?
          2a  cd  2a  7b  7d  3f  2a  3f
        canonical:
           *      *   *   ?
          2a  cd  2a  2a  3f
    
    [0]: https://source.chromium.org/chromium/chromium/src/+/main:third_party/icu/source/common/unicode/utf8.h;l=352;drc=f90543d272e2e2edc5b3cdf8ead0b5b3eebceef5
    
    Bug: 1263594
    Change-Id: Ib1c66cb322f0c48db2ae7e264cdfd5818975ac9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248041
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935863}

--- third_party/liburlpattern/tokenize.cc
+++ third_party/liburlpattern/tokenize.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/liburlpattern/tokenize.h"
 
+#include "base/compiler_specific.h"
 #include "third_party/abseil-cpp/absl/strings/str_format.h"
 #include "third_party/icu/source/common/unicode/uchar.h"
 #include "third_party/icu/source/common/unicode/utf8.h"
@@ -54,7 +55,10 @@ class Tokenizer {
       if (!status_.ok())
         return std::move(status_);
 
-      NextAt(index_);
+      if (!NextAt(index_)) {
+        Error(absl::StrFormat("Invalid UTF-8 codepoint at index %d.", index_));
+        continue;
+      }
       if (codepoint_ == '*') {
         AddToken(TokenType::kAsterisk);
         continue;
@@ -74,7 +78,12 @@ class Tokenizer {
           continue;
         }
         size_t escaped_i = next_index_;
-        Next();
+        if (!Next()) {
+          Error(absl::StrFormat("Invalid UTF-8 codepoint at index %d.",
+                                next_index_));
+          continue;
+        }
+
         AddToken(TokenType::kEscapedChar, next_index_, escaped_i);
         continue;
       }
@@ -95,7 +104,12 @@ class Tokenizer {
 
         // Iterate over codepoints until we find the first non-name codepoint.
         while (pos < pattern_.size()) {
-          NextAt(pos);
+          if (!status_.ok())
+            return std::move(status_);
+          if (!NextAt(pos)) {
+            Error(absl::StrFormat("Invalid UTF-8 codepoint at index %d.", pos));
+            continue;
+          }
           if (!IsNameCodepoint(codepoint_, pos == name_start))
             break;
           pos = next_index_;
@@ -118,7 +132,11 @@ class Tokenizer {
         bool error = false;
 
         while (j < pattern_.size()) {
-          NextAt(j);
+          if (!NextAt(j)) {
+            Error(absl::StrFormat("Invalid UTF-8 codepoint at index %d.", j));
+            error = true;
+            break;
+          }
 
           if (!IsASCII(codepoint_)) {
             Error(absl::StrFormat(
@@ -150,7 +168,12 @@ class Tokenizer {
               break;
             }
             size_t escaped_j = next_index_;
-            Next();
+            if (!Next()) {
+              Error(absl::StrFormat("Invalid UTF-8 codepoint at index %d.",
+                                    next_index_));
+              error = true;
+              break;
+            }
             if (!IsASCII(codepoint_)) {
               Error(absl::StrFormat(
                         "Invalid non-ASCII character 0x%02x at index %d.",
@@ -178,7 +201,12 @@ class Tokenizer {
               break;
             }
             size_t tmp_j = next_index_;
-            Next();
+            if (!Next()) {
+              Error(absl::StrFormat("Invalid UTF-8 codepoint at index %d.",
+                                    next_index_));
+              error = true;
+              break;
+            }
             // Require the the first character after an open paren is `?`.  This
             // permits assertions, named capture groups, and non-capturing
             // groups. It blocks, however, unnamed capture groups.
@@ -230,17 +258,20 @@ class Tokenizer {
  private:
   // Read the codepoint at `next_index_` in `pattern_` and store it in
   // `codepoint_`.  In addition, `next_index_` is updated to the codepoint to be
-  // read next.
-  void Next() {
+  // read next.  Returns true iff the codepoint was read successfully. On
+  // success, `codepoint_` is non-negative.
+  bool Next() WARN_UNUSED_RESULT {
     U8_NEXT(pattern_.data(), next_index_, pattern_.size(), codepoint_);
+    return codepoint_ >= 0;
   }
 
   // Read the codepoint at the specified `index` in `pattern_` and store it in
   // `codepoint_`.  In addition, `next_index_` is updated to the codepoint to be
-  // read next.
-  void NextAt(size_t index) {
+  // read next.  Returns true iff the codepoint was read successfully. On
+  // success, `codepoint_` is non-negative.
+  bool NextAt(size_t index) WARN_UNUSED_RESULT {
     next_index_ = index;
-    Next();
+    return Next();
   }
 
   // Append a Token to our list of the given `type` and with a value consisting
