65ab63564ad97d608bdd75ff8bf53b65cbc61016
Jose Dapena Paz <jdapena@igalia.com>
2021-10-28 19:06:31 +0000

    IWYU: missing cstdint include for usage of uint8_t in clipboard test util.
    Fix build with libstdc++:
    ../../ui/base/clipboard/test/clipboard_test_util.h:19:13: error: ‘uint8_t’ was not declared in this scope
       19 | std::vector<uint8_t> ReadPng(Clipboard* clipboard);
          |             ^~~~~~~
    
    Bug: 957519
    Change-Id: I753ecfbc128ba91b67dde35f28a0a6f085ff8b39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251241
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#936019}

--- ui/base/clipboard/test/clipboard_test_util.h
+++ ui/base/clipboard/test/clipboard_test_util.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_CLIPBOARD_TEST_CLIPBOARD_TEST_UTIL_H_
 #define UI_BASE_CLIPBOARD_TEST_CLIPBOARD_TEST_UTIL_H_
 
+#include <cstdint>
 #include <vector>
 
 class SkBitmap;
