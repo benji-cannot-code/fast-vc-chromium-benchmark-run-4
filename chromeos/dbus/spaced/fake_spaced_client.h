e0452743cbac929006b6f74cdee2cb8074c1346c
Jeremie Boulic <jboulic@chromium.org>
2021-10-28 00:10:17 +0000

    Revert "[Storage management] Use Spaced service for total and free disk space"
    This reverts commit 3e0d2bbeef15f645d64230df6d397b810f47287b.
    
    Reason for revert: CL involves changes that have not been yet agreed upon.
    
    Original change's description:
    > [Storage management] Use Spaced service for total and free disk space
    >
    > Use the new Spaced DBus method GetRootDeviceSize that returns the size
    > of the disk instead of the size of the current partition.
    >
    > Use GetTotalDiskSpaceBlocking (statvfs) when the Spaced method above
    > fails (e.g. when the Fake client is used), and for guest profiles where
    > disk spaces are not relevant (in-memory temporary filesystem).
    >
    > Stop rounding the total disk space to the next power of 2.
    >
    > Refactoring: the observer StorageHandler::OnSizeCalculated is now
    > responsible for updating the map of items sizes
    > (storage_items_total_bytes_) and marking the correct item as calculated
    > (calculation_state_). It then calls dedicated functions to update
    > the UI (UpdateSizeStat, UpdateStorageItem, UpdateSystemSize).
    >
    > Test: unit_tests --gtest_filter=StorageHandlerTest.*
    > Bug: b:173086361, chromium:1262859
    > Change-Id: I0ca5f6490bef44616ed5f743b07f010fb44c758d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3216578
    > Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#934374}
    
    Bug: b:173086361, chromium:1262859
    Change-Id: I4701c86e36de5dc0b5ff3cbbb411f72bbb29edc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247558
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935675}

--- chromeos/dbus/spaced/fake_spaced_client.h
+++ chromeos/dbus/spaced/fake_spaced_client.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/files/file_path.h"
-#include "base/memory/weak_ptr.h"
 #include "chromeos/dbus/spaced/spaced_client.h"
 
 namespace chromeos {
@@ -31,8 +30,6 @@ class COMPONENT_EXPORT(SPACED_CLIENT) FakeSpacedClient : public SpacedClient {
                          GetSizeCallback callback) override;
 
   void GetRootDeviceSize(GetSizeCallback callback) override;
-
-  base::WeakPtrFactory<FakeSpacedClient> weak_ptr_factory_{this};
 };
 
 }  // namespace chromeos
