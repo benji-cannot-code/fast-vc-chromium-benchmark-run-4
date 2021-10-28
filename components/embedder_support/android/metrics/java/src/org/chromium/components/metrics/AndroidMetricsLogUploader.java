c63d5131c5ad3604adcc0bd70269652ffe17cec7
Hazem Ashmawy <hazems@chromium.org>
2021-10-28 21:15:00 +0000

    [AW] Add blocking metrics log method to PlatformServiceBridge
    The new method should block until the metrics logging operation finishes
    before returning. This is useful in the new MetricsUploadService that
    shouldn't return to the IPC caller until it makes sure the data is
    logged.
    
    Bug: 1248039
    Test: run webview instrumentation tests
    Change-Id: I2599dd32f37ca5bfc65bd254ffcecb4d4c4478a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247113
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936094}

--- components/embedder_support/android/metrics/java/src/org/chromium/components/metrics/AndroidMetricsLogUploader.java
+++ components/embedder_support/android/metrics/java/src/org/chromium/components/metrics/AndroidMetricsLogUploader.java
@@ -26,6 +26,7 @@ public class AndroidMetricsLogUploader {
         sUploader = uploader;
     }
 
+    // TODO(crbug.com/1264425): return status code of the logging operation.
     @CalledByNative
     public static void uploadLog(byte[] data) {
         final Consumer<byte[]> uploader = sUploader;
