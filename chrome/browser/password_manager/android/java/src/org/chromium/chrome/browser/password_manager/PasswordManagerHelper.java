d74181605d1db4958d4166cada574c71c8908809
Ioana Pandele <ioanap@chromium.org>
2021-10-28 13:40:59 +0000

    [CredManager] Pass the referrer when launching the Credential Manager
    This is used for metrics collection on the Credential Manager side.
    
    Bug: 1255038
    Change-Id: I2d692b6201d33a975dc6249bb1acfe5f79ee8a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217645
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935860}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerHelper.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerHelper.java
@@ -42,7 +42,7 @@ public class PasswordManagerHelper {
                     CredentialManagerLauncherFactory.getInstance().createLauncher();
 
             if (credentialManagerLauncher != null) {
-                credentialManagerLauncher.launchCredentialManager();
+                credentialManagerLauncher.launchCredentialManager(referrer);
                 return;
             }
         }
