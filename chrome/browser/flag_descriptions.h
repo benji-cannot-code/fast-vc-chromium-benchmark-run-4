790a87a86410e62ce7d47b5a239f69edc4f5fd97
danielng <danielng@google.com>
2021-10-28 06:42:48 +0000

    borealis: enable big gl for borealis
    Added a flag for Big GL enablement for Borealis. Currently Big Gl
    causes Borealis to fail on my Hatch (potentially on an outdated
    version). Flag is disabled by default, but if i can verify that that
    borealis is failing to launch because of an outdated patch, i may enable
    this flag by default.
    
    Bug: b/203007397
    Change-Id: I33e2171ff12b3d9db85812284e7843f13e9ecfc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245021
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Daniel Ng <danielng@google.com>
    Cr-Commit-Position: refs/heads/main@{#935709}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -263,6 +263,9 @@ extern const char kDragWindowToNewDeskDescription[];
 extern const char kBiometricReauthForPasswordFillingName[];
 extern const char kBiometricReauthForPasswordFillingDescription[];
 
+extern const char kBorealisBigGlName[];
+extern const char kBorealisBigGlDescription[];
+
 extern const char kBorealisDiskManagementName[];
 extern const char kBorealisDiskManagementDescription[];
 
