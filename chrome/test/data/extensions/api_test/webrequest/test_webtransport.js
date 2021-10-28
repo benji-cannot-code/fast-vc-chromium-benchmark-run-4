8e6fc849952ad26dc519aaae91c8062c4a567b5d
Yoichi Osato <yoichio@chromium.org>
2021-10-28 07:01:20 +0000

    Add WebRequest API test for WebTransport in service worker
    In the previous CL for supporting shared worker (crrev.com/c/3194228),
    Service worker got to be supported.
    Thus this CL just adds such tests.
    
    Bug: 1243518
    Change-Id: Ibd4d34dce209aef8c0e081e0d4d403f89525396f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233714
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935741}

--- chrome/test/data/extensions/api_test/webrequest/test_webtransport.js
+++ chrome/test/data/extensions/api_test/webrequest/test_webtransport.js
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 const callbackPass = chrome.test.callbackPass;
-const hasFrame = !('isSharedWorkerTest' in self);
+const hasFrame =
+    !('isSharedWorkerTest' in self) && !('isServiceWorkerTest' in self);
 
 chrome.tabs.getCurrent(function(tab) {
   runTestsForTab(
