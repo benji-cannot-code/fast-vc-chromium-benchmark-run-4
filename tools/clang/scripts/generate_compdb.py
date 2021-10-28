e1959ef33b1b4a52582a70a4815b665f150545c1
Daniel Cheng <dcheng@chromium.org>
2021-10-28 06:59:55 +0000

    Improve the compile DB generation script.
    - allow additional arguments to be filtered
    - filter frontend-specific arguments
    - defer argument filtering until after response file processing
    - some miscellaneous cleanup to alphabetize things
    
    Change-Id: I61e199fa57ee62f353fd3a04923b3d4d32e32240
    Cq-Include-Trybots: luci.chromium.codesearch:gen-android-try,gen-chromiumos-try,gen-fuchsia-try,gen-lacros-try,gen-linux-try,gen-win-try
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237169
    Reviewed-by: Will Cassella <cassew@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935738}

--- tools/clang/scripts/generate_compdb.py
+++ tools/clang/scripts/generate_compdb.py
@@ -3,12 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""
-Helper for generating compile DBs for clang tooling. On non-Windows platforms,
-this is pretty straightforward. On Windows, the tool does a bit of extra work to
-integrate the content of response files, force clang tooling to run in clang-cl
-mode, etc.
-"""
+"""Generates compile DBs that are more amenable for clang tooling."""
 
 import argparse
 import json
@@ -24,10 +19,14 @@ from clang import compile_db
 
 def main(argv):
   parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--filter_arg',
+      nargs='*',
+      help='Additional argument(s) to filter out from compilation database.')
+  parser.add_argument(
+      '-o',
+      help='File to write the compilation database to. Defaults to stdout')
   parser.add_argument('-p', required=True, help='Path to build directory')
-  parser.add_argument('targets',
-                      nargs='*',
-                      help='Additional targets to pass to ninja')
   parser.add_argument(
       '--target_os',
       choices=[
@@ -42,14 +41,15 @@ def main(argv):
       ],
       help='Target OS - see `gn help target_os`. Set to "win" when ' +
       'cross-compiling Windows from Linux or another host')
-  parser.add_argument(
-      '-o',
-      help='File to write the compilation database to. Defaults to stdout')
+  parser.add_argument('targets',
+                      nargs='*',
+                      help='Additional targets to pass to ninja')
 
   args = parser.parse_args()
 
-  compdb_text = json.dumps(compile_db.ProcessCompileDatabaseIfNeeded(
-      compile_db.GenerateWithNinja(args.p, args.targets), args.target_os),
+  compdb_text = json.dumps(compile_db.ProcessCompileDatabase(
+      compile_db.GenerateWithNinja(args.p, args.targets), args.filter_arg,
+      args.target_os),
                            indent=2)
   if args.o is None:
     print(compdb_text)
