9ab41fc39836aac19a665a0793ffdcbfae214a01
Peter Boström <pbos@chromium.org>
2021-10-28 23:57:55 +0000

    Add theme-background support for side-panel border
    This makes TopContainerBackground usable externally through a static
    method which is used for painting the side-panel border after setting a
    clipping region.
    
    Coordinate conversion has to be done for the side panel to be painted
    correctly here (tested in RTL with both left and right side panels).
    This same conversion is not correct when done from the toolbar or
    bookmarks bar (possibly because of transforms? unclear?). A TODO is left
    in place to figure out a way for this code to unconditionally figure out
    what transform should be done between `view` and `browser_view` to get
    the right offsets.
    
    Bug: 1260151
    Change-Id: I7fed14452b22cee5eb0cd6dcb8b7127862876d6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251715
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936165}

--- chrome/browser/ui/views/side_panel.h
+++ chrome/browser/ui/views/side_panel.h
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 #include "ui/views/view_observer.h"
 
+class BrowserView;
+
 class SidePanel : public views::View, public views::ViewObserver {
  public:
   METADATA_HEADER(SidePanel);
-  SidePanel();
+  explicit SidePanel(BrowserView* browser_view);
   SidePanel(const SidePanel&) = delete;
   SidePanel& operator=(const SidePanel&) = delete;
   ~SidePanel() override;
