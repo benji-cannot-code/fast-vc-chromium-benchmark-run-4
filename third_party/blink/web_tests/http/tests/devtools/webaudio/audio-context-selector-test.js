ed23268ca40958f6825b08d7c181fe680576d708
Tim van der Lippe <tvanderlippe@chromium.org>
2021-10-28 14:06:13 +0000

    Fix webaudio layout test
    R=szuend@chromium.org
    
    Bug: 1127902
    Change-Id: I4c5a126d8af4108de9d192b1a991806ed24ac2c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3250886
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#935868}

--- third_party/blink/web_tests/http/tests/devtools/webaudio/audio-context-selector-test.js
+++ third_party/blink/web_tests/http/tests/devtools/webaudio/audio-context-selector-test.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (async function() {
   TestRunner.addResult(`Tests the AudioContextSelector.`);
-  await TestRunner.loadModule('web_audio');
   await TestRunner.loadLegacyModule('web_audio');
 
   /** @type {!Protocol.WebAudio.BaseAudioContext} */
