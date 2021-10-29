8db1c5de4254654d8cd69624809f9034e09a3850
Michael Checo <michaelcheco@google.com>
2021-10-29 23:59:53 +0000

    Firmware UI: Add shared fonts and styles
    Bug: 1252981
    Change-Id: Ib60ee960a6a13d60e14dc45183e8273d587d9448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242697
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936614}

--- ash/webui/firmware_update_ui/resources/update_card.js
+++ ash/webui/firmware_update_ui/resources/update_card.js
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './firmware_shared_css.js';
+import './firmware_shared_fonts.js';
+
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {FirmwareUpdate, UpdatePriority} from './firmware_update_types.js';
 
