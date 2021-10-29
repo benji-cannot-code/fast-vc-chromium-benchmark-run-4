69a9df65df0cf1b8e513c348b301b6bb5a8bf9ca
Mengqi Guo <mqg@chromium.org>
2021-10-29 01:39:10 +0000

    Reland "ash: add dbus for privacy screen setting"
    This is a reland of 28b8f930314ff6babcbc9919c9069bf667f2ed3b
    The initial version of this CL crashed ash::Shell dtor, and is fixed
    in the new version.
    
    Original change's description:
    > ash: add dbus for privacy screen setting
    >
    > This allows systems daemons / userspace to query privacy screen
    > setting.
    >
    > TEST=the following commands
    > dbus-send --system --dest=org.chromium.PrivacyScreenService \
    > --print-reply --type=method_call \
    > /org/chromium/PrivacyScreenService \
    > org.chromium.PrivacyScreenService.GetPrivacyScreenSetting
    >
    > dbus-monitor --system \
    > "type='signal',interface='org.chromium.PrivacyScreenService'"
    >
    > on device with and without privacy screen.
    >
    > Bug: b:194326235
    > Change-Id: I79c77495f0da19bb7baccad9a708445609643d4b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3210059
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Reviewed-by: Gil Dekel <gildekel@chromium.org>
    > Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    > Commit-Queue: Mengqi Guo <mqg@chromium.org>
    > Auto-Submit: Mengqi Guo <mqg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#932662}
    
    Bug: b:194326235
    Change-Id: I81696503709d912c439c96fff338ebcde6998b5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3246330
    Commit-Queue: Mengqi Guo <mqg@chromium.org>
    Auto-Submit: Mengqi Guo <mqg@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936215}

--- ash/dbus/ash_dbus_services.cc
+++ ash/dbus/ash_dbus_services.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/dbus/display_service_provider.h"
 #include "ash/dbus/gesture_properties_service_provider.h"
 #include "ash/dbus/liveness_service_provider.h"
+#include "ash/dbus/privacy_screen_service_provider.h"
 #include "ash/dbus/url_handler_service_provider.h"
 #include "ash/dbus/user_authentication_service_provider.h"
 #include "base/feature_list.h"
@@ -37,6 +38,11 @@ AshDBusServices::AshDBusServices(dbus::Bus* system_bus) {
       dbus::ObjectPath(chromeos::kLivenessServicePath),
       chromeos::CrosDBusService::CreateServiceProviderList(
           std::make_unique<LivenessServiceProvider>()));
+  privacy_screen_service_ = chromeos::CrosDBusService::Create(
+      system_bus, privacy_screen::kPrivacyScreenServiceName,
+      dbus::ObjectPath(privacy_screen::kPrivacyScreenServicePath),
+      chromeos::CrosDBusService::CreateServiceProviderList(
+          std::make_unique<PrivacyScreenServiceProvider>()));
   url_handler_service_ = chromeos::CrosDBusService::Create(
       system_bus, chromeos::kUrlHandlerServiceName,
       dbus::ObjectPath(chromeos::kUrlHandlerServicePath),
@@ -53,6 +59,7 @@ AshDBusServices::~AshDBusServices() {
   display_service_.reset();
   gesture_properties_service_.reset();
   liveness_service_.reset();
+  privacy_screen_service_.reset();
   url_handler_service_.reset();
   user_authentication_service_.reset();
 }
