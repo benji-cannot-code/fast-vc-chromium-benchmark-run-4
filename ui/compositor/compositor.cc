2a3a4cee42b8f60684b47cf3f9ca7ea147f70a53
Mitsuru Oshima <oshima@chromium.org>
2021-10-29 07:42:32 +0000

    Reset ComopsitorAnimationObsever's start time upon resume
    So that it won't trigger logging/dcheck when the compositor has been
    suspended.
    
    Bug: 1237981
    Test: covered by unittests
    Change-Id: Idb4d42d50b63cf59c7b9bcc6a8d8e3acf2e6a39d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220823
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936298}

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/power_monitor/power_monitor.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -266,6 +267,9 @@ Compositor::Compositor(const viz::FrameSinkId& frame_sink_id,
   // See: http://crbug.com/956264.
   host_->SetVisible(true);
 
+  if (base::PowerMonitor::IsInitialized())
+    base::PowerMonitor::AddPowerSuspendObserver(this);
+
   if (command_line->HasSwitch(switches::kUISlowAnimations)) {
     slow_animations_ = std::make_unique<ScopedAnimationDurationScaleMode>(
         ScopedAnimationDurationScaleMode::SLOW_DURATION);
@@ -274,6 +278,8 @@ Compositor::Compositor(const viz::FrameSinkId& frame_sink_id,
 
 Compositor::~Compositor() {
   TRACE_EVENT0("shutdown,viz", "Compositor::destructor");
+  if (base::PowerMonitor::IsInitialized())
+    base::PowerMonitor::RemovePowerSuspendObserver(this);
 
   for (auto& observer : observer_list_)
     observer.OnCompositingShuttingDown(this);
@@ -821,6 +827,12 @@ void Compositor::CancelThroughtputTracker(TrackerId tracker_id) {
 
   if (should_stop)
     animation_host_->StopThroughputTracking(tracker_id);
+}
+
+void Compositor::OnResume() {
+  // Restart the time upon resume.
+  for (auto& obs : animation_observer_list_)
+    obs.ResetIfActive();
 }
 
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
--- ui/compositor/compositor.h
+++ ui/compositor/compositor.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/observer_list.h"
+#include "base/power_monitor/power_observer.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -142,7 +143,8 @@ class COMPOSITOR_EXPORT ContextFactory {
 // displayable form of pixels comprising a single widget's contents. It draws an
 // appropriately transformed texture for each transformed view in the widget's
 // view hierarchy.
-class COMPOSITOR_EXPORT Compositor : public cc::LayerTreeHostClient,
+class COMPOSITOR_EXPORT Compositor : public base::PowerSuspendObserver,
+                                     public cc::LayerTreeHostClient,
                                      public cc::LayerTreeHostSingleThreadClient,
                                      public viz::HostFrameSinkClient,
                                      public ThroughputTrackerHost {
@@ -382,6 +384,9 @@ class COMPOSITOR_EXPORT Compositor : public cc::LayerTreeHostClient,
       ThroughputTrackerHost::ReportCallback callback) override;
   bool StopThroughtputTracker(TrackerId tracker_id) override;
   void CancelThroughtputTracker(TrackerId tracker_id) override;
+
+  // base::PowerSuspendObserver:
+  void OnResume() override;
 
 // TODO(crbug.com/1052397): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
