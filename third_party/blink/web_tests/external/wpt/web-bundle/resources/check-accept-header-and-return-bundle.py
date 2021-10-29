a397f2a561b9ad47f6b5fce603a9df3676844cfc
Kunihiko Sakamoto <ksakamoto@chromium.org>
2021-10-29 06:56:31 +0000

    [Subresource WebBundles] Accept bundle format b2
    Now we prefer the new version of the WebBundle format ("b2"), while the
    old format ("b1") is still supported. This CL reflects that in the
    Accept: header value for WebBundle requests, i.e. we request b2 bundles
    with q=1 (default), and request b1 bundles with q=0.8.
    
    Bug: 1247139
    Change-Id: I016eb5dd8c5a4c0b8dc1192f004c48b4d589fd99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252464
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936289}

--- third_party/blink/web_tests/external/wpt/web-bundle/resources/check-accept-header-and-return-bundle.py
+++ third_party/blink/web_tests/external/wpt/web-bundle/resources/check-accept-header-and-return-bundle.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 
 # Returns the content of ./wbn/subresource.wbn if the request has an "Accept"
-# header including "application/webbundle;v=b1" type, otherwise returns an
+# header including "application/webbundle;v=b2" type, otherwise returns an
 # empty body with status code 400.
 
 def main(request, response):
@@ -12,7 +12,7 @@ def main(request, response):
     ]
 
     accept_values = request.headers.get(b"accept", b"").split(b",")
-    if b"application/webbundle;v=b1" in accept_values:
+    if b"application/webbundle;v=b2" in accept_values:
         with open(
             os.path.join(os.path.dirname(__file__), "./wbn/subresource.wbn"),
             "rb",
