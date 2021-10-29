dd40df0217018642460866cd206a91464ca232e0
Anders Hartvoll Ruud <andruud@chromium.org>
2021-10-29 08:46:38 +0000

    [mq-4] Add MediaQueryExpNode::CollectExpressions
    This function makes it possible to collect all the MediaQueryExp
    objects within a MediaQueryExpNode tree. This is useful, since
    some clients (InspectorCSSAgent::BuildMediaObject) will continue
    to model media queries as a flat list of expressions.
    
    For now the function is unused (but tested).
    
    Bug: 962417
    Change-Id: I3681b2ba5dacd8b810ff822cd253db90022c01ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245097
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936314}

--- third_party/blink/renderer/core/css/media_query_exp.h
+++ third_party/blink/renderer/core/css/media_query_exp.h
@@ -275,6 +275,7 @@ class CORE_EXPORT MediaQueryExpNode {
   virtual Type GetType() const = 0;
   virtual PhysicalAxes QueriedAxes() const = 0;
   virtual void SerializeTo(StringBuilder&) const = 0;
+  virtual void CollectExpressions(Vector<MediaQueryExp>&) const = 0;
   virtual std::unique_ptr<MediaQueryExpNode> Copy() const = 0;
 };
 
@@ -289,6 +290,7 @@ class CORE_EXPORT MediaQueryFeatureExpNode : public MediaQueryExpNode {
   Type GetType() const override { return Type::kFeature; }
   PhysicalAxes QueriedAxes() const override;
   void SerializeTo(StringBuilder&) const override;
+  void CollectExpressions(Vector<MediaQueryExp>&) const override;
   std::unique_ptr<MediaQueryExpNode> Copy() const override;
 
  private:
@@ -305,6 +307,7 @@ class CORE_EXPORT MediaQueryUnaryExpNode : public MediaQueryExpNode {
   }
 
   PhysicalAxes QueriedAxes() const override;
+  void CollectExpressions(Vector<MediaQueryExp>&) const override;
   const MediaQueryExpNode& Operand() const { return *operand_; }
 
  private:
@@ -347,6 +350,7 @@ class CORE_EXPORT MediaQueryCompoundExpNode : public MediaQueryExpNode {
   }
 
   PhysicalAxes QueriedAxes() const override;
+  void CollectExpressions(Vector<MediaQueryExp>&) const override;
   const MediaQueryExpNode& Left() const { return *left_; }
   const MediaQueryExpNode& Right() const { return *right_; }
 
