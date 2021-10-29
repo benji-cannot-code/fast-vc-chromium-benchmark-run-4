56a1f5acfaf19a121628a06a11ac43534cc419bd
Preethi Mohan <preethim@google.com>
2021-10-29 00:39:45 +0000

    PY3 migration(blinkpy unit tests) - Fix remaining failures in blinkpy.common
    All tests in this suite pass now.
    >vpython3 blinkpy.common
    
    Bug: 1218641
    
    Change-Id: Iac21700a7cd41c0f5815c16fdb28c6a590e72f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3246347
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/main@{#936184}

--- third_party/blink/tools/blinkpy/common/net/results_fetcher_test.py
+++ third_party/blink/tools/blinkpy/common/net/results_fetcher_test.py
@@ -96,18 +96,18 @@ class BuilderTest(LoggingTestCase):
             urls={
                 'https://test-results.appspot.com/testfile?buildnumber=123&'
                 'callback=ADD_RESULTS&builder=builder&name=full_results.json':
-                'ADD_RESULTS(%s);' % (json.dumps(
+                b'ADD_RESULTS(' + json.dumps(
                     [{
                         "TestType": "blink_web_tests on Intel GPU (with patch)"
                     }, {
                         "TestType": "base_unittests (with patch)"
-                    }])),
+                    }]).encode('utf8', 'replace') + b');',
                 'https://test-results.appspot.com/data/layout_results/builder/123/'
                 'blink_web_tests%20on%20Intel%20GPU%20%28with%20patch%29/'
                 'layout-test-results/failing_results.json':
                 json.dumps({
                     'passed': True
-                }),
+                }).encode('utf8', 'replace')
             })
         results = fetcher.fetch_results(Build('builder', 123))
         self.assertEqual(
@@ -126,8 +126,8 @@ class BuilderTest(LoggingTestCase):
             urls={
                 'https://test-results.appspot.com/testfile?buildnumber=5&'
                 'callback=ADD_RESULTS&builder=foo&name=full_results.json':
-                'ADD_RESULTS(%s);' %
-                (json.dumps([{
+                b'ADD_RESULTS(' +
+                json.dumps([{
                     "TestType": "blink_web_tests (with patch)"
                 }, {
                     "TestType":
@@ -136,7 +136,7 @@ class BuilderTest(LoggingTestCase):
                     "TestType": "blink_web_tests (retry with patch)"
                 }, {
                     "TestType": "base_unittests (with patch)"
-                }]))
+                }]).encode('utf8', 'replace') + b');'
             })
         step_name = fetcher.get_layout_test_step_name(Build('foo', 5))
         self.assertEqual(step_name, 'blink_web_tests (with patch)')
@@ -149,14 +149,14 @@ class BuilderTest(LoggingTestCase):
             urls={
                 'https://test-results.appspot.com/testfile?buildnumber=5&'
                 'callback=ADD_RESULTS&builder=foo&name=full_results.json':
-                'ADD_RESULTS(%s);' %
+                b'ADD_RESULTS(' +
                 (json.dumps([{
                     "TestType": "wpt_tests_suite (with patch)"
                 }, {
                     "TestType": "wpt_tests_suite (retry with patch)"
                 }, {
                     "TestType": "base_unittests (with patch)"
-                }]))
+                }])).encode('utf8', 'replace') + b');'
             })
         step_name = fetcher.get_layout_test_step_name(Build('foo', 5))
         self.assertEqual(step_name, 'wpt_tests_suite (with patch)')
@@ -205,7 +205,7 @@ class BuilderTest(LoggingTestCase):
                 'name=full_results.json':
                 json.dumps({
                     'passed': True
-                }),
+                }).encode('utf8', 'replace'),
             })
         results = fetcher.fetch_webdriver_test_results(
             Build('bar-rel', 123), 'foo.chrome')
