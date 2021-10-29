34697b60fa5c1452bb0cc87dd95092a4c3d98a6b
Trevor Perrier <perrier@chromium.org>
2021-10-29 16:25:55 +0000

    [ULP] Initial LanguageProfile classes
    This CL lands the initial LanguageProfile classes. Future CLs will use
    and a downstream version and JNI calls to access language profile values.
    
    Bug: 1258255
    Change-Id: I70556314bc7b80c4bf50ec024c25e58c2c67c236
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251354
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936421}

---
+++ components/language/android/java/src/org/chromium/components/language/LanguageProfileDelegate.java
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.language;
+
+import java.util.List;
+
+/**
+ * Interface to get language profile data for device.
+ */
+public interface LanguageProfileDelegate {
+    /**
+     * Helper class to hold language preference values.
+     */
+    public static class LanguagePreference {
+        private final String mLanguageName;
+        private final float mPreference;
+
+        public LanguagePreference(String languageName, float preference) {
+            mLanguageName = languageName;
+            mPreference = preference;
+        }
+
+        public String getLanguage() {
+            return mLanguageName;
+        }
+
+        public float getPreference() {
+            return mPreference;
+        }
+    }
+
+    /**
+     * @return True if ULP is currently available.
+     */
+    public boolean isULPAvailable();
+
+    /**
+     * @param accountName Account to get profile or null if the default profile should be returned.
+     * @return A list of language preferences for |accountName|
+     */
+    public List<LanguagePreference> getLanguagePreferences(String accountName);
+}
---
+++ components/language/android/java/src/org/chromium/components/language/LanguageProfileDelegateImpl.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.language;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Interface to get ULP data from GSM Core.
+ */
+public class LanguageProfileDelegateImpl implements LanguageProfileDelegate {
+    /**
+     * @return True if ULP is currently available.
+     */
+    @Override
+    public boolean isULPAvailable() {
+        // ULP is not available in the default implementation.
+        return false;
+    }
+
+    /**
+     * The default implementation always returns an empty list.
+     * @param accountName Account to get profile or null if the default profile should be returned.
+     * @return A list of language preferences for |accountName|
+     */
+    @Override
+    public List<LanguageProfileDelegate.LanguagePreference> getLanguagePreferences(
+            String accountName) {
+        return new ArrayList<LanguageProfileDelegate.LanguagePreference>();
+    }
+}
