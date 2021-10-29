dd91a664ac698938f118887896b17ff0d603ab8b
Sylvain Defresne <sdefresne@chromium.org>
2021-10-29 08:07:01 +0000

    [ios] Sort WebStateObserverBridge and CRWWebStateObserver methods
    Sort the WebStateObserverBridge and CRWWebStateObserver methods to
    be in the same order as the methods of WebStateObserver. Sort them
    in the implementation too.
    
    Also fix naming of Objective-C method parameter to conform to the
    style guide.
    
    Bug: none
    Change-Id: If3277bf94677d465d8e800d7be73126aae855801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3250674
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936304}

--- ios/web/web_state/web_state_observer_bridge.mm
+++ ios/web/web_state/web_state_observer_bridge.mm
@@ -36,6 +36,15 @@ void WebStateObserverBridge::DidStartNavigation(
   }
 }
 
+void WebStateObserverBridge::DidRedirectNavigation(
+    web::WebState* web_state,
+    web::NavigationContext* navigation_context) {
+  SEL selector = @selector(webState:didRedirectNavigation:);
+  if ([observer_ respondsToSelector:selector]) {
+    [observer_ webState:web_state didRedirectNavigation:navigation_context];
+  }
+}
+
 void WebStateObserverBridge::DidFinishNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
@@ -145,15 +154,6 @@ void WebStateObserverBridge::WebStateDestroyed(web::WebState* web_state) {
     // |webStateDestroyed:| may delete |this|, so don't expect |this| to be
     // valid afterwards.
     [observer_ webStateDestroyed:web_state];
-  }
-}
-
-void WebStateObserverBridge::DidRedirectNavigation(
-    web::WebState* web_state,
-    web::NavigationContext* navigation_context) {
-  SEL selector = @selector(webState:didRedirectNavigation:);
-  if ([observer_ respondsToSelector:selector]) {
-    [observer_ webState:web_state didRedirectNavigation:navigation_context];
   }
 }
 
