860001e21777b58896eecca2bd335ab5b1bb5941
David Maunder <davidjm@chromium.org>
2021-10-29 00:20:34 +0000

    Add accessibility string to chip displaying price drops
    We display price drops in the Tab Switching UI when they are
    identified for shopping websites. This CL adds the accessibility
    string for that piece of UI.
    
    Bug: 1264080
    Change-Id: I5d211583d9c863ae9990b81a250ad676c4898f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248645
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936177}

--- ios/chrome/browser/ui/commerce/price_card/price_card_view.mm
+++ ios/chrome/browser/ui/commerce/price_card/price_card_view.mm
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/commerce/price_card/price_card_view.h"
 
+#include "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/ui/commerce/price_card/price_card_constants.h"
 #import "ios/chrome/browser/ui/commerce/price_card/resources/semantic_color_names.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/grit/ios_strings.h"
+#include "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -100,7 +103,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                       attributes:attributes];
   self.previousPriceLabel.attributedText = attrText;
   _previousPrice = previousPrice;
+  self.accessibilityLabel = l10n_util::GetNSStringF(
+      IDS_IOS_TAB_SWITCHER_PRICE_CARD, base::SysNSStringToUTF16(currentPrice),
+      base::SysNSStringToUTF16(previousPrice));
   self.hidden = FALSE;
+}
+
+- (BOOL)isAccessibilityElement {
+  return YES;
 }
 
 @end
