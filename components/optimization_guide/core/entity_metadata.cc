1f6241ed50765e24f9f7357a99a99270e4fdabab
Robert Ogden <robertogden@chromium.org>
2021-10-29 00:34:16 +0000

    Add ctors for ScoredEntityMetadata and EntityMetadata
    Needed for src-internal change
    
    Bug: 1249632
    Change-Id: I572369682afc5c0eb7fbc54c0b19da1708b58356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252287
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936182}

--- components/optimization_guide/core/entity_metadata.cc
+++ components/optimization_guide/core/entity_metadata.cc
@@ -8,7 +8,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace optimization_guide {
 
 EntityMetadata::EntityMetadata() = default;
-EntityMetadata::~EntityMetadata() = default;
+EntityMetadata::EntityMetadata(
+    const std::string& entity_id,
+    const std::string& human_readable_name,
+    const base::flat_map<std::string, float>& human_readable_categories)
+    : entity_id(entity_id),
+      human_readable_name(human_readable_name),
+      human_readable_categories(human_readable_categories) {}
 EntityMetadata::EntityMetadata(const EntityMetadata&) = default;
+EntityMetadata::~EntityMetadata() = default;
+
+ScoredEntityMetadata::ScoredEntityMetadata() = default;
+ScoredEntityMetadata::ScoredEntityMetadata(float score,
+                                           const EntityMetadata& md)
+    : metadata(md), score(score) {}
+ScoredEntityMetadata::ScoredEntityMetadata(const ScoredEntityMetadata&) =
+    default;
+ScoredEntityMetadata::~ScoredEntityMetadata() = default;
 
 }  // namespace optimization_guide
