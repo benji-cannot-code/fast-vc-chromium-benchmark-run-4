85b28eb19ed151ad14a28b8963482128ec140259
Shashank Jain <jainshashank@google.com>
2021-10-29 12:40:33 +0000

    [Autofill Assistant] Replace TtsEventDelegate With Callback
    Use Callback with WeakPtr to pass the TtsEvent from
    AATtsController to Controller. This will avoid any
    errors due to invalid pointers.
    
    Bug: b/203664402
    Change-Id: I7e671558c1daeac871c3c54e45df21ca9162a74e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247393
    Commit-Queue: Shashank Jain <jainshashank@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/main@{#936358}

--- components/autofill_assistant/browser/autofill_assistant_tts_controller.cc
+++ components/autofill_assistant/browser/autofill_assistant_tts_controller.cc
@@ -47,7 +47,7 @@ void AutofillAssistantTtsController::Stop() {
 }
 
 void AutofillAssistantTtsController::SetTtsEventDelegate(
-    TtsEventDelegate* tts_event_delegate) {
+    base::WeakPtr<TtsEventDelegate> tts_event_delegate) {
   // Ensure that it is set only once
   DCHECK(!tts_event_delegate_);
 
@@ -60,7 +60,7 @@ void AutofillAssistantTtsController::OnTtsEvent(
     int char_index,
     int char_length,
     const std::string& error_message) {
-  if (tts_event_delegate_ == nullptr) {
+  if (!tts_event_delegate_) {
     VLOG(1) << "AssistantAutofillTtsController: No TtsEventDelegate set.";
     return;
   }
