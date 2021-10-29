565bd9ed5c2681a512247f7509dcc74c2f43bd8c
Francois Doray <fdoray@chromium.org>
2021-10-29 12:49:22 +0000

    [image decoding] Use base::span as argument to image decoding functions.
    The image decoding functions take a std::vector<uint8_t> as argument.
    This forces a copy when the encoded images is stored in a std::string
    at the call site. This CL changes the argument type to
    base::span<const uint8_t> to eliminate copies.
    
    Change-Id: Id02d279a660b101bb54cc83d34ec5f2f2c6873b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245556
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936360}

--- services/data_decoder/public/cpp/decode_image.cc
+++ services/data_decoder/public/cpp/decode_image.cc
@@ -54,7 +54,7 @@ void OnDecodeImages(mojo::Remote<mojom::ImageDecoder> decoder,
 }
 
 void DecodeImageUsingServiceProcess(DataDecoder* data_decoder,
-                                    const std::vector<uint8_t>& encoded_bytes,
+                                    base::span<const uint8_t> encoded_bytes,
                                     mojom::ImageCodec codec,
                                     bool shrink_to_fit,
                                     uint64_t max_size_in_bytes,
@@ -83,7 +83,7 @@ void DecodeImageUsingServiceProcess(DataDecoder* data_decoder,
 
 }  // namespace
 
-void DecodeImageIsolated(const std::vector<uint8_t>& encoded_bytes,
+void DecodeImageIsolated(base::span<const uint8_t> encoded_bytes,
                          mojom::ImageCodec codec,
                          bool shrink_to_fit,
                          uint64_t max_size_in_bytes,
@@ -106,7 +106,7 @@ void DecodeImageIsolated(const std::vector<uint8_t>& encoded_bytes,
 }
 
 void DecodeImage(DataDecoder* data_decoder,
-                 const std::vector<uint8_t>& encoded_bytes,
+                 base::span<const uint8_t> encoded_bytes,
                  mojom::ImageCodec codec,
                  bool shrink_to_fit,
                  uint64_t max_size_in_bytes,
@@ -119,7 +119,7 @@ void DecodeImage(DataDecoder* data_decoder,
 }
 
 void DecodeAnimationIsolated(
-    const std::vector<uint8_t>& encoded_bytes,
+    base::span<const uint8_t> encoded_bytes,
     bool shrink_to_fit,
     uint64_t max_size_in_bytes,
     mojom::ImageDecoder::DecodeAnimationCallback callback) {
@@ -138,7 +138,7 @@ void DecodeAnimationIsolated(
 }
 
 void DecodeAnimation(DataDecoder* data_decoder,
-                     const std::vector<uint8_t>& encoded_bytes,
+                     base::span<const uint8_t> encoded_bytes,
                      bool shrink_to_fit,
                      uint64_t max_size_in_bytes,
                      mojom::ImageDecoder::DecodeAnimationCallback callback) {
