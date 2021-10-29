3a735684a3812ced2b19cdbbe3caa1ce6e69b0ed
Richard Chui <richui@chromium.org>
2021-10-29 21:06:01 +0000

    Desk Templates: Expose feature flag to chrome://flags
    Fixed: 1263324
    Change-Id: I544bf0a6ff88d582f4b26c8be2c992e63cb3b13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3243824
    Commit-Queue: Richard Chui <richui@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936556}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2362,6 +2362,9 @@ extern const char kButtonARCNetworkDiagnosticsDescription[];
 extern const char kCalendarViewName[];
 extern const char kCalendarViewDescription[];
 
+extern const char kDesksTemplatesName[];
+extern const char kDesksTemplatesDescription[];
+
 extern const char kDesksTrackpadSwipeImprovementsName[];
 extern const char kDesksTrackpadSwipeImprovementsDescription[];
 
