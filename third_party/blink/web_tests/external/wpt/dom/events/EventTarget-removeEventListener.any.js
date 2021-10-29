7efbbf3c20067b466a1519a5a41351379a36c024
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-10-29 22:10:35 +0000

    Import wpt@7e53f8df9e06f8a7fe2be4f7fcbbe5078efe080f
    Using wpt-import in Chromium 8be8b9145c89537d0e8a3eb113aa6f9870d2e4fb.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Change-Id: I87880082c041830280c0462290ca645faa53861d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252222
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#936575}

--- third_party/blink/web_tests/external/wpt/dom/events/EventTarget-removeEventListener.any.js
+++ third_party/blink/web_tests/external/wpt/dom/events/EventTarget-removeEventListener.any.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Step 1.
 test(function() {
-  assert_equals(document.removeEventListener("x", null, false), undefined);
-  assert_equals(document.removeEventListener("x", null, true), undefined);
-  assert_equals(document.removeEventListener("x", null), undefined);
+  assert_equals(globalThis.removeEventListener("x", null, false), undefined);
+  assert_equals(globalThis.removeEventListener("x", null, true), undefined);
+  assert_equals(globalThis.removeEventListener("x", null), undefined);
 }, "removing a null event listener should succeed");
