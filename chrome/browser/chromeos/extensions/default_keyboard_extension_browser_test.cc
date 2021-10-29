375ebf7ec3f8b93f7bbcba92989fcbc40209cc9a
dpapad <dpapad@chromium.org>
2021-10-29 10:47:24 +0000

    ChromeOS: Fork pre-JS Module mock_timer.js and mock_controller.js
     - mock_timer.js is only used in CrOS virtual keyboard extension tests
       and is moved to the corresponding folder.
     - mock_controller.js is used by CrOS virtual keyboard extension tests
       and wallpaper manager tests and is moved to c/t/data/webui/chromeos/
    
    These pre-JS modules files are blocking migrating WebUI tests to
    TypeScript, and forking is required to unblock it.
    
    Bug: 1263610
    Change-Id: Iddeed22a0af2075a3c8b139e970d427ae61f098f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247876
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936340}

--- chrome/browser/chromeos/extensions/default_keyboard_extension_browser_test.cc
+++ chrome/browser/chromeos/extensions/default_keyboard_extension_browser_test.cc
@@ -28,16 +28,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/input_method.h"
 
 namespace {
-const base::FilePath::CharType kWebuiTestDir[] = FILE_PATH_LITERAL("webui");
+const base::FilePath::CharType kWebuiTestDir[] =
+    FILE_PATH_LITERAL("webui/chromeos");
 
 const base::FilePath::CharType kMockController[] =
     FILE_PATH_LITERAL("mock_controller.js");
 
-const base::FilePath::CharType kMockTimer[] =
-    FILE_PATH_LITERAL("mock_timer.js");
-
 const char kVirtualKeyboardExtensionTestDir[] =
     "chromeos/virtual_keyboard/default_extension";
+
+const base::FilePath::CharType kMockTimer[] =
+    FILE_PATH_LITERAL("mock_timer.js");
 
 const char kBaseKeyboardExtensionTestFramework[] =
     "virtual_keyboard_test_base.js";
@@ -78,7 +79,8 @@ void DefaultKeyboardExtensionBrowserTest::RunTest(
   // Inject testing scripts.
   InjectJavascript(base::FilePath(kWebuiTestDir),
                    base::FilePath(kMockController));
-  InjectJavascript(base::FilePath(kWebuiTestDir), base::FilePath(kMockTimer));
+  InjectJavascript(base::FilePath(config.test_dir_),
+                   base::FilePath(kMockTimer));
   InjectJavascript(base::FilePath(config.test_dir_),
                    base::FilePath(config.base_framework_));
   InjectJavascript(base::FilePath(config.test_dir_), file);
