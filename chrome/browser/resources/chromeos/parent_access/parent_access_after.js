7f1b226fb0cd863f6e636958275079696d84a1a8
Courtney Wong <courtneywong@chromium.org>
2021-10-29 20:14:40 +0000

    Add a skeleton for implementing the parent access after screen
    Tested: Tried switching to after view and saw the after template render
    Change-Id: Ifcaf087d76c8c72a040499338b29bc4d9f854e08
    Bug: b/199753153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248253
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Dan S <danan@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936528}

---
+++ chrome/browser/resources/chromeos/parent_access/parent_access_after.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+Polymer({
+  is: 'parent-access-after',
+
+  _template: html`{__html_template__}`,
+
+  /** @override */
+  ready() {
+    // TODO(b/199753153): Implement handlers for deny and approve buttons.
+  },
+});
