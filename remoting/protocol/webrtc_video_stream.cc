1c4e33f5f571209555ae821dc08a5f3fd3eb1fe6
Lambros Lambrou <lambroslambrou@chromium.org>
2021-10-29 15:42:40 +0000

    [remoting host] More cleanups from encode-pipeline refactoring.
    * Removed unused timestamps from WebrtcVideoEncoder.
    * Removed FrameProcessingTimeEstimator::StartFrame().
    * Make |frame| const in OnFrameEncoded() notification.
    
    Bug: 1192865
    Change-Id: I6aafc79ca7249d7cc4f92d8f4228c3d349b2af6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252720
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936400}

--- remoting/protocol/webrtc_video_stream.cc
+++ remoting/protocol/webrtc_video_stream.cc
@@ -208,7 +208,7 @@ void WebrtcVideoStream::CaptureNextFrame() {
 
 void WebrtcVideoStream::OnFrameEncoded(
     WebrtcVideoEncoder::EncodeResult encode_result,
-    WebrtcVideoEncoder::EncodedFrame* frame) {
+    const WebrtcVideoEncoder::EncodedFrame* frame) {
   DCHECK(thread_checker_.CalledOnValidThread());
 
   scheduler_->OnFrameEncoded(encode_result, frame);
