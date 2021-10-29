0620df47ff8eb2043b4cb7f0db1dadbf919212bc
Sammie Quon <sammiequon@chromium.org>
2021-10-29 02:43:02 +0000

    desk_templates: Allow launching ARC apps p12.
    Move FetchRestoreWindowId to app restore utils as it is used for desks
    templates now.
    
    Test: trybots
    Bug: 1251403
    Change-Id: I64b944e5c304f56cfb8f852a95467101ab9ed151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3249458
    Auto-Submit: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936241}

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/base/window_state_type.h"
 #include "chromeos/ui/frame/immersive/immersive_fullscreen_controller.h"
 #include "components/app_restore/app_restore_utils.h"
-#include "components/app_restore/full_restore_utils.h"
 #include "components/app_restore/window_properties.h"
 #include "components/session_manager/core/session_manager.h"
 #include "extensions/browser/app_window/app_delegate.h"
@@ -142,7 +141,7 @@ void ChromeNativeAppWindowViewsAuraAsh::OnBeforeWidgetInit(
   }
 
   const int32_t restore_window_id =
-      full_restore::FetchRestoreWindowId(app_window()->extension_id());
+      app_restore::FetchRestoreWindowId(app_window()->extension_id());
   init_params->init_properties_container.SetProperty(
       app_restore::kWindowIdKey, app_window()->session_id().id());
   init_params->init_properties_container.SetProperty(
