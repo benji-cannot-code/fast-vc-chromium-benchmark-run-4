54e13928b2eeb140d84fc2b70c729cd5dbd1d7d0
Evan Stade <estade@chromium.org>
2021-10-29 19:04:07 +0000

    dPWA: Reorganize Web App startup logic into a class.
    This hopefully improves legibility by:
    * making keep alive lifetime clearer
    * reducing the number of parameters passed around (storing them as
      member vars)
    
    The only behavioral difference is to create the keep alives earlier in
    the process of parsing the command line, as discussed in
    crrev.com/c/3221410
    
    Bug: none
    Change-Id: I6063843e4a8554b3140dede18b628d2a24f8d333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237052
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Mike Jackson <mjackson@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#936497}

--- chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
+++ chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
@@ -2710,6 +2710,7 @@ IN_PROC_BROWSER_TEST_F(
   // The waiter will get the dialog when it shows up and accepts it.
   waiter.WaitIfNeededAndGet()->CloseWithReason(
       views::Widget::ClosedReason::kCancelButtonClicked);
+  base::RunLoop().RunUntilIdle();
 
   WebAppProtocolHandlerIntentPickerView::SetDefaultRememberSelectionForTesting(
       false);
