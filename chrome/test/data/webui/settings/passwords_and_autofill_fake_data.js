fbc28fac06762d876a7eb9f1a107f47246f2d526
Elizabeth Popova <lizapopova@google.com>
2021-10-29 19:02:48 +0000

    [Passwords] Check for duplicates in add password dialog
    Now, on every website or username input change, the pair origin+username
    is checked for being already used. In this case the error message is
    shown and the saving is blocked.
    
    Previously, |usernamesForSameOrigin_| were computed for the fast check.
    Now, since origin can also change, usernames for all origins are
    precalculated and stored in |usernamesByOrigin_|.
    
    Note that in EDIT mode entries considered duplicates only if in the same
    store, but in ADD mode duplication is prevented irrespective of the
    store.
    
    Bug: 1236053
    Change-Id: I07610f18a95fbb664c4febcdb75ead38265fcdfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3250639
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#936495}

--- chrome/test/data/webui/settings/passwords_and_autofill_fake_data.js
+++ chrome/test/data/webui/settings/passwords_and_autofill_fake_data.js
@@ -382,7 +382,7 @@ export class PasswordSectionElementFactory {
    * @param {Array<!MultiStorePasswordUiEntry>=} passwords
    * @return {!Object}
    */
-  createPasswordEditDialog(passwordEntry = null, passwords = []) {
+  createPasswordEditDialog(passwordEntry = null, passwords) {
     const passwordDialog = this.document.createElement('password-edit-dialog');
     passwordDialog.existingEntry = passwordEntry;
     if (passwordEntry && !passwordEntry.federationText) {
@@ -391,7 +391,8 @@ export class PasswordSectionElementFactory {
       // edit dialog.
       passwordDialog.existingEntry.password = 'password';
     }
-    passwordDialog.savedPasswords = passwords;
+    passwordDialog.savedPasswords =
+        passwords || (passwordEntry ? [passwordEntry] : []);
     this.document.body.appendChild(passwordDialog);
     flush();
     return passwordDialog;
