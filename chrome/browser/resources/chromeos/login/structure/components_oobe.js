d77f71ba0a373db597c31496e604d52d94ba45c0
Renato Silva <rrsilva@google.com>
2021-10-29 22:35:47 +0000

    OOBE - Polymer3 - Welcome Screen
    Migrate the welcome screen to ES6 class syntax and add directives
    to generate its modularised version.
    
    Bug: 1184731, 1252778
    Change-Id: I46bc14a259d1243f513890a5f387c1cf78b99d72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222951
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#936585}

--- chrome/browser/resources/chromeos/login/structure/components_oobe.js
+++ chrome/browser/resources/chromeos/login/structure/components_oobe.js
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // clang-format off
 
 // <include src="../screens/oobe/enable_debugging.js">
-// <include src="../oobe_welcome_dialog.js">
-// <include src="../oobe_welcome.js">
 // <include src="../screens/oobe/oobe_network.js">
 // <include src="../oobe_update.js">
 // <include src="../enterprise_enrollment.js">
