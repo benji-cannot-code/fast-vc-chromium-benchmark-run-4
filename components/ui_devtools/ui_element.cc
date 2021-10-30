a8efe3d746f4a206b6bc366c0961d691ba155fad
Yuheng Huang <yuhengh@chromium.org>
2021-10-30 00:53:50 +0000

    UIDevtool: Support query by ID
    This change is mainly used for supporting Desktop UI benchmark to query
    element by id. Previously we used class name to find elements, which
    is not ideal as discussed in the doc.
    
    We will gradually migrate to query element by id since it's more robust
    to code refactor.
    
    Doc: https://docs.google.com/document/d/1zcVPqvatmAyNZQZOT_yL8Goj1xckt0eUfAGEGEoOf9g/edit?usp=sharing&resourcekey=0-RJywpBq5u1XqweomTY-NWA
    Bug: 1201243
    Change-Id: I95488d622381fe56b64c316d07fc8dfa4843a54f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253505
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936628}

--- components/ui_devtools/ui_element.cc
+++ components/ui_devtools/ui_element.cc
@@ -150,6 +150,10 @@ std::vector<UIElement::Source> UIElement::GetSources() {
   return sources_;
 }
 
+int UIElement::GetBackingElementID() {
+  return 0;
+}
+
 bool UIElement::DispatchMouseEvent(protocol::DOM::MouseEvent* event) {
   return false;
 }
