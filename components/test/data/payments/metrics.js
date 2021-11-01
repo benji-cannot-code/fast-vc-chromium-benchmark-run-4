892186e1f0f1679faac1a1c2ea594c24fc6170cb
Liquan (Max) Gu <maxlg@chromium.org>
2021-11-01 20:53:17 +0000

    Add migrated tests to PaymentRequestMetricsTest
    This CL migrates the tests of PaymentRequestMetricsTest that depend on
    basic-card and retains the original tests in PaymentRequestMetricsTest.
    
    Bug: 1221559
    Change-Id: If7bb3b01087e1ee4a45a598e3f92de163f30902f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3223563
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937036}

--- components/test/data/payments/metrics.js
+++ components/test/data/payments/metrics.js
@@ -9,10 +9,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var request;
 
+const bobPayMethod = Object.freeze({
+  supportedMethods: 'https://bobpay.com',
+});
+
+const kylePayMethod = Object.freeze({
+  supportedMethods: 'https://kylepay.com/webpay',
+});
+
+/**
+ * Launches the PaymentRequest UI that accepts url payment methods.
+ */
+function buyWithUrlMethods() { // eslint-disable-line no-unused-vars
+  buyWithMethods([bobPayMethod, kylePayMethod]);
+}
+
 /**
  * Launches the PaymentRequest UI that accepts credit cards.
  */
 function ccBuy() { // eslint-disable-line no-unused-vars
+  buyWithMethods([{
+    supportedMethods: 'basic-card',
+    data: {supportedNetworks: ['visa']},
+  }]);
+}
+
+/**
+ * Launches the PaymentRequest UI that accepts the given methods.
+ * @param {Array<Object>} methods An array of payment method objects.
+ */
+ function buyWithMethods(methods) {
   try {
     var details = {
       total: {
@@ -33,10 +59,7 @@ function ccBuy() { // eslint-disable-line no-unused-vars
       }],
     };
     request = new PaymentRequest(
-        [{
-          supportedMethods: 'basic-card',
-          data: {supportedNetworks: ['visa']},
-        }],
+        methods,
         {
           total: {
             label: 'Total',
