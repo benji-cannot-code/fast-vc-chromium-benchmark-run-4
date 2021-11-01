01f8f4241fa3e8cbdc3c4a85727dac55ed458ec2
Volodymyr Riazantsev <riazantsevv@google.com>
2021-11-01 20:33:20 +0000

    cast_core: Propagate VideoPlaneController instance
    Propagate pointer to a VideoPlaneController into
    CastRuntimeService builders. This is required to
    provide access to screen resolution change notifiers.
    
    Bug: b/204269370(internal)
    Signed-off-by: Volodymyr Riazantsev <riazantsevv@google.com>
    Change-Id: I89c61f485ba99c1bd95953ecf1f12dd9065eacec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3255630
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#937028}

--- chromecast/cast_core/cast_runtime_service.h
+++ chromecast/cast_core/cast_runtime_service.h
@@ -36,6 +36,7 @@ class CastWebService;
 
 namespace media {
 class MediaPipelineBackendManager;
+class VideoPlaneController;
 }  // namespace media
 
 namespace receiver {
@@ -58,7 +59,8 @@ class CastRuntimeService
       CastWindowManager* window_manager,
       media::MediaPipelineBackendManager* media_pipeline_backend_manager,
       NetworkContextGetter network_context_getter,
-      PrefService* pref_service);
+      PrefService* pref_service,
+      media::VideoPlaneController* video_plane_controller);
 
   // Returns current instance of CastRuntimeService in the browser process.
   static CastRuntimeService* GetInstance();
