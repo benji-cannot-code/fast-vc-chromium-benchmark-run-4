34e12e51ff738d7bf92c70026f1e5651ab0d2ec8
Ashley Prasad <ashleydp@google.com>
2021-11-01 21:48:39 +0000

    Diagnostics: Add metric handler skeleton
    - Adds skeleton WebUIMessageHandler for process UI triggered metric
      events.
    - Tests handler can be registered.
    
    Bug: 1197335
    Test: ./out/Default/ash_webui_unittests
    Change-Id: I11355b1ff654e2a084ad3f4595235a6f02f3a638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248355
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937062}

---
+++ ash/webui/diagnostics_ui/diagnostics_metrics_message_handler_unittest.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/webui/diagnostics_ui/diagnostics_metrics_message_handler.h"
+
+#include "content/public/test/test_web_ui.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ash {
+namespace diagnostics {
+namespace metrics {
+namespace {
+class DiagnosticsMetricsMessageHandlerTest : public testing::Test {
+ public:
+  DiagnosticsMetricsMessageHandlerTest() : web_ui_() {}
+  ~DiagnosticsMetricsMessageHandlerTest() override = default;
+
+ protected:
+  content::TestWebUI web_ui_;
+};
+}  // namespace
+
+TEST_F(DiagnosticsMetricsMessageHandlerTest, AbleToRegisterMessages) {
+  auto handler_ = std::make_unique<DiagnosticsMetricsMessageHandler>();
+  handler_->SetWebUiForTesting(&web_ui_);
+  EXPECT_NO_FATAL_FAILURE(handler_->RegisterMessages());
+}
+}  // namespace metrics
+}  // namespace diagnostics
+}  // namespace ash
