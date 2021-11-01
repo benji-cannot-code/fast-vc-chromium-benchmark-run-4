b69a0beae873dce4720f61cfd7688d23bcbbac80
Dominick Ng <dominickn@chromium.org>
2021-11-01 22:01:41 +0000

    Deflake InstallableManager browser tests.
    This CL converts GetAllInstallabilityErrorsWithPlayAppManifest to use
    a page that has no service worker. This allows the test to be
    deparameterised, removing it from the suite that tests the new
    offline capability checking (which remains unlaunched).
    
    GetAllInstallabilityErrorsNoErrors does not appear to have been
    previously flaky, so it is re-enabled.
    
    This CL also adds a readable operator<< for
    content::InstallabilityError, allowing failed test output to be more
    cleanly understandable.
    
    Bug: 1256938
    Change-Id: Ic8882e74e90d0cb788ae282a06a99e8d113ca76d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251816
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937068}

--- content/public/browser/installability_error.h
+++ content/public/browser/installability_error.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_INSTALLABILITY_ERROR_H_
 #define CONTENT_PUBLIC_BROWSER_INSTALLABILITY_ERROR_H_
 
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -34,6 +35,9 @@ struct CONTENT_EXPORT InstallabilityError {
   std::string error_id;
   std::vector<InstallabilityErrorArgument> installability_error_arguments;
 };
+
+CONTENT_EXPORT std::ostream& operator<<(std::ostream& os,
+                                        const InstallabilityError& error);
 
 }  // namespace content
 
