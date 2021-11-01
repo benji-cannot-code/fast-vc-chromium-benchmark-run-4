1a39f85b8139bed506d17265fb6ccfd9ab8c432e
Josh Hilke <jrhilke@google.com>
2021-11-01 18:52:40 +0000

    Rename class/variable to match server side
    BUG=b:204361512
    
    Change-Id: I5956460e88e3340dfb9926c8643326708a5a1b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252484
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Cr-Commit-Position: refs/heads/main@{#936980}

--- chrome/browser/policy/messaging_layer/upload/upload_provider_unittest.cc
+++ chrome/browser/policy/messaging_layer/upload/upload_provider_unittest.cc
@@ -124,7 +124,7 @@ class EncryptedReportingUploadProviderTest : public ::testing::Test {
  public:
   MOCK_METHOD(void,
               ReportSuccessfulUpload,
-              (reporting::SequencingInformation, bool),
+              (reporting::SequenceInformation, bool),
               ());
   MOCK_METHOD(void,
               EncryptionKeyCallback,
@@ -140,10 +140,10 @@ class EncryptedReportingUploadProviderTest : public ::testing::Test {
 
     record_.set_encrypted_wrapped_record("TEST_DATA");
 
-    auto* sequencing_information = record_.mutable_sequencing_information();
-    sequencing_information->set_sequencing_id(42);
-    sequencing_information->set_generation_id(1701);
-    sequencing_information->set_priority(reporting::Priority::SLOW_BATCH);
+    auto* sequence_information = record_.mutable_sequence_information();
+    sequence_information->set_sequencing_id(42);
+    sequence_information->set_generation_id(1701);
+    sequence_information->set_priority(reporting::Priority::SLOW_BATCH);
   }
 
   Status CallRequestUploadEncryptedRecord(
@@ -174,7 +174,7 @@ class EncryptedReportingUploadProviderTest : public ::testing::Test {
 
 TEST_F(EncryptedReportingUploadProviderTest, SuccessfullyUploadsRecord) {
   EXPECT_CALL(*this, ReportSuccessfulUpload(
-                         EqualsProto(record_.sequencing_information()), _))
+                         EqualsProto(record_.sequence_information()), _))
       .Times(1);
   EXPECT_CALL(cloud_policy_client_, UploadEncryptedReport(_, _, _))
       .WillOnce(WithArgs<0, 2>(
