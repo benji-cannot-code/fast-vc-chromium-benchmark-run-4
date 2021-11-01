5094ed5b85f873c5db35c3f9d3db296c79345699
Ari Chivukula <arichiv@chromium.org>
2021-11-01 20:55:35 +0000

    [Deprecation Console->Issue] (6) Implement Deprecation Issue (Chromium)
    This implements the deprecation protocol code per:
    https://docs.google.com/document/d/1gU2caqBEek2x8AeKop9SrSxqlO51ipealPMV4aisQNE/edit
    https://docs.google.com/document/d/1Jot9P30LFW9TQtc6eaQP9wRBwjLpUn-hIaxRJRUBFvI/edit
    
    This is a part of a set of CLs:
    (1) Remove legacy CSS Deprecation path
    (2) Consolidate DeprecationInfo
    (3) Streamline redundant Deprecation messages
    (4) Add features for console/issue deprecation
    (5) Fix feature status detail link
    (6) Implement Deprecation Issue (Chromium)
    (7) Move DeprecationIssue.ts code
    (8) Implement Deprecation Issue (DevTools)
    (9) Remove Console Deprecation
    
    Bug: 1248484
    Change-Id: I9c862f5b599d0ee2af51e3952feb9f737257dd0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245951
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937039}

--- third_party/blink/renderer/core/frame/deprecation.cc
+++ third_party/blink/renderer/core/frame/deprecation.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/report.h"
 #include "third_party/blink/renderer/core/frame/reporting_context.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
+#include "third_party/blink/renderer/core/inspector/inspector_audits_issue.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/origin_trials/origin_trial_context.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -808,7 +809,7 @@ void Deprecation::CountDeprecation(ExecutionContext* context,
   }
   if (base::FeatureList::IsEnabled(
           features::kDeprecationWillLogToDevToolsIssue)) {
-    // TODO(crbug.com/1248484): Implement DevTools Issue logging.
+    AuditsIssue::ReportDeprecationIssue(context, info.message_);
   }
 
   Report* report = CreateReportInternal(context->Url(), info);
--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/sab-postMessage-issue.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/sab-postMessage-issue.js
@@ -7,10 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.Audits.enable();
   session.evaluate(`postMessage(new (new WebAssembly.Memory(
          { shared:true, initial:0, maximum:0 }).buffer.constructor)());`);
-  const issues = await Promise.all(
-      [dp.Audits.onceIssueAdded(), dp.Audits.onceIssueAdded()]);
-
-  testRunner.log(issues[0].params, 'Creation issue: ');
-  testRunner.log(issues[1].params, 'Transfer issue: ');
+  const issue1 = await dp.Audits.onceIssueAdded();
+  const issue2 = await dp.Audits.onceIssueAdded();
+  const issue3 = await dp.Audits.onceIssueAdded();
+  testRunner.log(issue1.params, 'Creation issue: ');
+  testRunner.log(issue2.params, 'Deprecation issue: ');
+  testRunner.log(issue3.params, 'Transfer issue: ');
   testRunner.completeTest();
 })
