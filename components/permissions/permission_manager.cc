1ae78e22b500b9ed701fc4bd5596e57a8eb79af8
Ravjit <ravjit@chromium.org>
2021-11-01 18:32:16 +0000

    Remove unnecessary SCOPED_CRASH_KEY_NUMBER in PermissionManager
    We added the SCOPED_CRASH_KEY_NUMBER to debug a crash in PermissionSubscription (crbug.com/1223407)
    The crash has been fixed hence we don't need this anymore.
    
    Bug: 1262096
    Change-Id: I4773e8552a4d323adf13ec95afe9e0b1552b2253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3236586
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Ravjit Singh Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936966}

--- components/permissions/permission_manager.cc
+++ components/permissions/permission_manager.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
-#include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -658,9 +656,6 @@ void PermissionManager::UnsubscribePermissionStatusChange(
   if (!subscription)
     return;
 
-  if (is_processing_permission_change_)
-    base::debug::DumpWithoutCrashing();
-
   ContentSettingsType type = subscription->permission;
   subscriptions_.Remove(subscription_id);
   auto type_count = subscription_type_counts_.find(type);
@@ -689,10 +684,6 @@ void PermissionManager::OnPermissionChanged(
 
   std::vector<base::OnceClosure> callbacks;
   callbacks.reserve(subscriptions_.size());
-  base::AutoReset<bool> reset(&is_processing_permission_change_, true);
-  SCOPED_CRASH_KEY_NUMBER(
-      "PermissionManager", "content_type",
-      static_cast<int>(content_type_set.GetTypeOrDefault()));
   for (SubscriptionsMap::iterator iter(&subscriptions_); !iter.IsAtEnd();
        iter.Advance()) {
     Subscription* subscription = iter.GetCurrentValue();
