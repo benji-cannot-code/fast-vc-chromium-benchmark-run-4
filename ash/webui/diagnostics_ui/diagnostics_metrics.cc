c1f057d264266568ca2a6cbc93044ddd387eb659
Ashley Prasad <ashleydp@google.com>
2021-11-01 18:14:37 +0000

    Diagnostics: Enable recording Usetime metric
    - Adds functionality for recording `Usetime` of feature for
      `ChromeOS.FeatureUsage.Diagnostics.Usetime` metric.
    - Tests matching usetime result recorded when `RecordUsage(true)` +
      `StopSuccessfulUsage` triggered.
    
    Bug: 1197335
    Test: ./out/Default/ash_webui_unittests
    Change-Id: I05ef220d4ec4a22cf31bd8b82686dd50c7c16379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232271
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936958}

--- ash/webui/diagnostics_ui/diagnostics_metrics.cc
+++ ash/webui/diagnostics_ui/diagnostics_metrics.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/diagnostics_ui/diagnostics_metrics.h"
 
 #include "ash/constants/ash_features.h"
+#include "chromeos/components/feature_usage/feature_usage_metrics.h"
 
 namespace ash {
 namespace diagnostics {
@@ -15,7 +16,8 @@ const char kDiagnosticsUmaFeatureName[] = "DiagnosticsUi";
 }
 
 DiagnosticsMetrics::DiagnosticsMetrics()
-    : feature_metrics_(kDiagnosticsUmaFeatureName, this) {}
+    : feature_metrics_(kDiagnosticsUmaFeatureName, this),
+      successful_usage_started_(false) {}
 
 bool DiagnosticsMetrics::IsEligible() const {
   return features::IsDiagnosticsAppEnabled();
@@ -28,6 +30,31 @@ bool DiagnosticsMetrics::IsEnabled() const {
 // Helper function for feature_usage::FeatureUsageMetrics RecordUsage.
 void DiagnosticsMetrics::RecordUsage(bool success) {
   feature_metrics_.RecordUsage(success);
+
+  // Start successful usage recording only when usage was successful and not
+  // currently recording a successful usage.
+  // See {@link feature_usage::FeatureUsageMetrics} for rationale behind
+  // starting and stopping usetime tracking.
+  if (success) {
+    DCHECK(!successful_usage_started_);
+    feature_metrics_.StartSuccessfulUsage();
+    successful_usage_started_ = true;
+  }
+}
+
+void DiagnosticsMetrics::StopSuccessfulUsage() {
+  // Exit early if usage was not started.
+  if (!successful_usage_started_) {
+    return;
+  }
+
+  feature_metrics_.StopSuccessfulUsage();
+  successful_usage_started_ = false;
+}
+
+// Test helpers to check state of `successful_usage_started`.
+bool DiagnosticsMetrics::GetSuccessfulUsageStartedForTesting() {
+  return successful_usage_started_;
 }
 }  // namespace metrics
 }  // namespace diagnostics
