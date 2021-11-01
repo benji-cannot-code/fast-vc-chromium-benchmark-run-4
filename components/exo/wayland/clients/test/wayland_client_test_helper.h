8d3165fae7e512935007e2b1bb307a863dfdbd9f
Nicholas Hollingum <hollingum@google.com>
2021-11-01 04:18:13 +0000

    exo: Clean up redundant wayland_watcher in test helper
    Since the creation of this test helper, the wayland watcher has been:
     - factored into its own file
     - turned into a member of the wayland::Server
    
    As a result, these definitions are now redundant and we can remove them.
    
    Bug: b/200896773
    Change-Id: I30319c082c22af7bcfb3f7d0a87caf241b69d1d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252626
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#936809}

--- components/exo/wayland/clients/test/wayland_client_test_helper.h
+++ components/exo/wayland/clients/test/wayland_client_test_helper.h
@@ -45,8 +45,6 @@ class WaylandClientTestHelper {
   virtual void SetUpOnUIThread(base::WaitableEvent* event);
 
  private:
-  class WaylandWatcher;
-
   void TearDownOnUIThread(base::WaitableEvent* event);
 
   // Below objects can only be accessed from UI thread.
@@ -55,7 +53,6 @@ class WaylandClientTestHelper {
   std::unique_ptr<WMHelper> wm_helper_;
   std::unique_ptr<Display> display_;
   std::unique_ptr<wayland::Server> wayland_server_;
-  std::unique_ptr<WaylandWatcher> wayland_watcher_;
 };
 
 }  // namespace exo
