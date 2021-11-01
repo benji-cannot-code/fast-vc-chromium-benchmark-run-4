3bb7fd2f9d3b623cc598a6ee7885122cad4f9396
Michael Martis <martis@chromium.org>
2021-11-01 04:37:42 +0000

    [Snooping protection] Fix icon visibility bug.
    The icon was incorrectly shown on login as follows:
     1) The view was created and defaulted to visible.
    
     2) When updating visibility, the state booleans remained at their
        initial values, triggering the short-circuit logic and not hiding
        the icon even though it should be hidden.
    
    A fix is to initialize the icon to invisible (matching the initial
    values of the state booleans).
    
    Bug: chromium:1262869
    Change-Id: Iff55590f81c06431c619cea300cfd3074cf4f75f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251813
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936812}

--- ash/system/unified/hps_notify_view.cc
+++ ash/system/unified/hps_notify_view.cc
@@ -32,12 +32,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 HpsNotifyView::HpsNotifyView(Shelf* shelf) : TrayItemView(shelf) {
+  CreateImageView();
+  SetVisible(false);
+
   SessionControllerImpl* session_controller =
       Shell::Get()->session_controller();
 
-  CreateImageView();
-
   OnSessionStateChanged(session_controller->GetSessionState());
+  session_observation_.Observe(session_controller);
 
   // Poll the current preference state if the pref service is already loaded for
   // an active user. Then, from now on, observe changes to the active user pref
@@ -46,7 +48,6 @@ HpsNotifyView::HpsNotifyView(Shelf* shelf) : TrayItemView(shelf) {
   PrefService* pref_service =
       session_controller->GetUserPrefServiceForUser(account_id);
   OnActiveUserPrefServiceChanged(pref_service);
-  session_observation_.Observe(session_controller);
 
   // Poll the current HPS notify state if the daemon is active. Then, from now
   // on, observe changes to the HPS notify signal.
