e3a95332a10e393a3a13ee9d0e14854f2c3ff18f
Yoshifumi Inoue <yosin@chromium.org>
2021-11-01 08:52:56 +0000

    Reduce AtomicString trashing in CaseMappingHarfBuzzBufferFiller
    This patch changes |CaseMappingHarfBuzzBufferFiller| to take reference
    instead of value for reducing ref counting of |StringImpl|.
    
    This patch is similar to the CL[1].
    
    [1] http://crrev.com/c/3232099: Reduce AtomicString trashing by passing
    them by reference in css code.
    
    Bug: 1083392
    Change-Id: Iceaaceccc15e568a9f570b4368dd1ee0cb610972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3255004
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936843}

--- third_party/blink/renderer/platform/fonts/shaping/case_mapping_harfbuzz_buffer_filler.h
+++ third_party/blink/renderer/platform/fonts/shaping/case_mapping_harfbuzz_buffer_filler.h
@@ -22,7 +22,7 @@ class CaseMappingHarfBuzzBufferFiller {
 
  public:
   CaseMappingHarfBuzzBufferFiller(CaseMapIntend,
-                                  AtomicString locale,
+                                  const AtomicString& locale,
                                   hb_buffer_t* harfbuzz_buffer,
                                   const String& text,
                                   unsigned start_index,
@@ -30,7 +30,7 @@ class CaseMappingHarfBuzzBufferFiller {
 
  private:
   void FillSlowCase(CaseMapIntend,
-                    AtomicString locale,
+                    const AtomicString& locale,
                     const UChar* buffer,
                     unsigned buffer_length,
                     unsigned start_index,
