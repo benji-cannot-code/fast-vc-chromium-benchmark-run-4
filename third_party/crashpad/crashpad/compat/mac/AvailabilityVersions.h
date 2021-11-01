b507244e9b189364e3212ba7650954522c20467d
Nico Weber <thakis@chromium.org>
2021-11-01 15:02:59 +0000

    mac: Make crashpad build with the 12.0 SDK
    The size of dyld_all_image_infos didn't change, but the last 128 bit
    of `reserved` moved out of `reserved` and got names.
    
    On 64-bit, that's straightforward -- just move two uint64s out of the
    array into fields.
    
    On 32-bit, it's fairly awkward: It's 4 of the uintptr_t `reserved`
    array, which means `UIntPtr, reserved, [4]` would have to diverge
    between 32-bit and 64-bit, and also shared_cache_fs_obj_id would
    straddle the `end_v14` boundary. Since macOS 12.0 doesn't support
    32-bit builds, just punt on this for 32-bit. (This might possibly
    mean that crashpad-on-apple-watch might not compile, but that's
    a build configuration that doesn't exist.)
    
    See comment 9 and 6 on the bug.
    
    Bug: chromium:1265179
    Change-Id: I5b2d34878bbe5729eab98dc53cf364e019d7e000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253742
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#936886}

--- third_party/crashpad/crashpad/compat/mac/AvailabilityVersions.h
+++ third_party/crashpad/crashpad/compat/mac/AvailabilityVersions.h
@@ -88,4 +88,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define __MAC_11_0 110000
 #endif
 
+// 12.0 SDK
+
+#ifndef __MAC_12_0
+#define __MAC_12_0 120000
+#endif
+
 #endif  // CRASHPAD_COMPAT_MAC_AVAILABILITYVERSIONS_H_
