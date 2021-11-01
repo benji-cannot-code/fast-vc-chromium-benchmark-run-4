9e1c4216fc5ce424c47eb8887e5377b292627d56
Peter Kvitek <kvitekp@chromium.org>
2021-11-01 21:50:46 +0000

    [headless] Removed obsolete --export-tagged-pdf switch.
    Since https://crbug.com/607777 was closed, the headful Chrome always
    generates tagged PDFs. Headless Chrome should follow the trend and
    remove support for non tagged PDFs.
    
    Bug: 1224704
    Change-Id: I80b03d65c1ab40511828a0bf9ac43db9ab37dbb1
    Cq-Include-Trybots: luci.chromium.try:linux-headless-shell-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3255921
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937063}

--- headless/lib/renderer/headless_print_render_frame_helper_delegate.cc
+++ headless/lib/renderer/headless_print_render_frame_helper_delegate.cc
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "headless/lib/renderer/headless_print_render_frame_helper_delegate.h"
-#include "base/command_line.h"
-#include "headless/app/headless_shell_switches.h"
-
 #include "third_party/blink/public/web/web_element.h"
 
 namespace headless {
@@ -27,8 +24,8 @@ bool HeadlessPrintRenderFrameHelperDelegate::IsPrintPreviewEnabled() {
 }
 
 bool HeadlessPrintRenderFrameHelperDelegate::ShouldGenerateTaggedPDF() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      headless::switches::kExportTaggedPDF);
+  // Always generate tagged PDF, see: https://crbug.com/607777
+  return true;
 }
 
 bool HeadlessPrintRenderFrameHelperDelegate::OverridePrint(
