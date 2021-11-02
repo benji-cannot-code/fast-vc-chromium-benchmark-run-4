929f207239c18809405f42927b97c5e84679e7c9
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-11-02 05:15:18 +0000

    [CrOS Bluetooth] Show keyboard and mouse subpages when link is clicked
    This CL add functionality to route the user to mouse or keyboard
    settings pages when either is clicked in paired device details page.
    
    Bug: 1010321
    Change-Id: I6e0ad7eb78c23d4efd38528d5cc05681b95ffddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3255787
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937217}

--- chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_device_detail_subpage.js
+++ chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_bluetooth_device_detail_subpage.js
@@ -430,6 +430,16 @@ class SettingsBluetoothDeviceDetailSubpageElement extends
   getIsDeviceConnectedForTest() {
     return this.isDeviceConnected_;
   }
+
+  /** @private */
+  onMouseRowClick_() {
+    Router.getInstance().navigateTo(routes.POINTERS);
+  }
+
+  /** @private */
+  onKeyboardRowClick_() {
+    Router.getInstance().navigateTo(routes.KEYBOARD);
+  }
 }
 
 customElements.define(
