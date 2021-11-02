33fc5671dee1cdc8d7ee21dc4756b6b3e07187d6
Peter Kasting <pkasting@chromium.org>
2021-11-02 18:01:26 +0000

    Fix -Wshadow instances: components/
    Bug: 794619
    Change-Id: I02fbe70ddb2d3448af54a6033f0076f7e0075aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257901
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937450}

--- components/segmentation_platform/internal/segmentation_platform_service_impl.h
+++ components/segmentation_platform/internal/segmentation_platform_service_impl.h
@@ -82,7 +82,7 @@ class SegmentationPlatformServiceImpl : public SegmentationPlatformService {
       PrefService* pref_service,
       const scoped_refptr<base::SequencedTaskRunner>& task_runner,
       base::Clock* clock,
-      std::vector<std::unique_ptr<Config>> config);
+      std::vector<std::unique_ptr<Config>> configs);
 
   ~SegmentationPlatformServiceImpl() override;
 
