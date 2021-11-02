4c2f4ad04a6bfbe17da7cc4a25c3b37cce5c600f
Etienne Pierre-doray <etiennep@chromium.org>
2021-11-02 15:33:31 +0000

    [Battery]: Export to json file after |sample_count|
    Add a simple SampleCounter to count sample and exit after
    |sample_count|. Use JsonExporter to dump a json file if
    |json-output-file| is provided.
    
    Bug: 1254332
    Change-Id: Idc8ec371c9b60255f2c4faa037ec9b4fd79d6750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3254363
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937349}

---
+++ tools/mac/power/power_sampler/sample_counter.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tools/mac/power/power_sampler/sample_counter.h"
+
+namespace power_sampler {
+
+SampleCounter::SampleCounter(size_t max_sample_count)
+    : sample_count_(max_sample_count) {
+  DCHECK_GT(sample_count_, 0U);
+}
+
+bool SampleCounter::OnSample(base::TimeTicks sample_time,
+                             const DataRow& data_row) {
+  DCHECK_GT(sample_count_, 0U);
+  --sample_count_;
+  return sample_count_ == 0;
+}
+
+}  // namespace power_sampler
---
+++ tools/mac/power/power_sampler/sample_counter.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_MAC_POWER_POWER_SAMPLER_SAMPLE_COUNTER_H_
+#define TOOLS_MAC_POWER_POWER_SAMPLER_SAMPLE_COUNTER_H_
+
+#include <cstdio>
+
+#include "tools/mac/power/power_sampler/monitor.h"
+
+namespace power_sampler {
+
+// Counts Monitor notifications and causes exit after |max_sample_count|.
+class SampleCounter : public Monitor {
+ public:
+  SampleCounter(size_t max_sample_count);
+  ~SampleCounter() override = default;
+
+  void OnStartSession(const DataColumnKeyUnits& data_columns_units) override {}
+  bool OnSample(base::TimeTicks sample_time, const DataRow& data_row) override;
+  void OnEndSession() override {}
+
+ private:
+  size_t sample_count_;
+};
+
+}  // namespace power_sampler
+
+#endif  // TOOLS_MAC_POWER_POWER_SAMPLER_SAMPLE_COUNTER_H_
