e718f8443626ea9204bba2f8888fb46969315f47
Sam Zackrisson <saza@chromium.org>
2021-11-02 14:20:35 +0000

    Move analog mic level transformations into MediaStreamAudioProcessor
    The WebRtcAudioDeviceImpl microphone volume code is also removed, because it is unused: Audio processing runs outside of WebRTC.
    
    These changes are not necessary for running APM (webrtc::AudioProcessing) in the audio process, but it makes the associated code a little more straightforward:
    - dead code is removed (volume passing in WebRtcAudioDeviceImpl and ProcessedLocalAudioSource).
    - an atomic is removed.
    - the integer format for analog gain levels is isolated to the code interacting with APM, in MediaStreamAudioProcessor, and becomes easier to share between APM in the renderer and (future) APM in the audio process.
    
    Bug: chromium:1215061
    Change-Id: I298c3f631dba84f3d6624128dc5839faad704107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251174
    Reviewed-by: Alessio Bazzica <alessiob@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937324}

--- third_party/blink/renderer/modules/mediastream/processed_local_audio_source.h
+++ third_party/blink/renderer/modules/mediastream/processed_local_audio_source.h
@@ -93,11 +93,6 @@ class MODULES_EXPORT ProcessedLocalAudioSource final
     return level_calculator_.level();
   }
 
-  // Thread-safe volume accessors used by WebRtcAudioDeviceImpl.
-  void SetVolume(int volume);
-  int Volume() const;
-  int MaxVolume() const;
-
   void SetOutputDeviceForAec(const std::string& output_device_id);
 
  protected:
@@ -125,6 +120,9 @@ class MODULES_EXPORT ProcessedLocalAudioSource final
                              base::TimeTicks audio_capture_time,
                              double volume,
                              bool key_pressed);
+
+  // Update the device (source) mic volume.
+  void SetVolume(double volume);
 
   // Helper function to get the source buffer size based on whether audio
   // processing will take place.
@@ -155,10 +153,6 @@ class MODULES_EXPORT ProcessedLocalAudioSource final
 
   // The device created by the AudioDeviceFactory in EnsureSourceIsStarted().
   scoped_refptr<media::AudioCapturerSource> source_;
-
-  // Stores latest microphone volume received in a CaptureData() callback.
-  // Range is [0, 255].
-  base::subtle::Atomic32 volume_;
 
   // Used to calculate the signal level that shows in the UI.
   blink::MediaStreamAudioLevelCalculator level_calculator_;
