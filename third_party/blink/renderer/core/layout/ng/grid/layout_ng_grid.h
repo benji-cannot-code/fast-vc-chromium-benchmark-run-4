c01da8f7c3a73a17f595db9de3c4f9948b944650
Ethan Jimenez <ethavar@microsoft.com>
2021-11-02 03:17:42 +0000

    [GridNG] Separate NGGridData into placement and layout data
    Previously, some information from `NGGridPlacement` and the resolved
    positions for grid items were stored into the layout result with the
    purpose of reusing it if the items didn't changed their placement.
    
    For subgrid, this cached data will be very useful since placement of
    grid items in a subgridded axis needs to happen before the subgrid's
    parent starts its sizing algorithm. However, storing the cache in the
    layout result implies that the sizing algorithm needs to finish first.
    
    In order to break the circular dependency, this change separates the
    data that we cache into two categories: placement and layout.
    
    Placement data needs to be available before any grid container tries to
    resolve its track sizes, thus, this change moves it into `LayoutNGGrid`.
    On the other hand, layout data will remain stored in the layout result.
    
    This change shouldn't have any behavioral impact.
    
    Bug: 618969
    Change-Id: If8aa762eb4140dc68a8623df7de1852165c1892a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252262
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937197}

--- third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.h
+++ third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid_interface.h"
 #include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_data.h"
-#include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_properties.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_block.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h"
 
@@ -26,27 +25,25 @@ class CORE_EXPORT LayoutNGGrid : public LayoutNGBlock,
 
   const LayoutNGGridInterface* ToLayoutNGGridInterface() const final;
 
-  bool HasCachedPlacements(wtf_size_t column_auto_repetitions,
-                           wtf_size_t row_auto_repetitions) const;
-  const NGGridPlacementProperties& GetCachedPlacementProperties();
-  void SetCachedPlacementProperties(NGGridPlacementProperties&& properties,
-                                    wtf_size_t column_auto_repetitions,
-                                    wtf_size_t row_auto_repetitions);
+  bool HasCachedPlacementData() const;
+  const NGGridPlacementData& CachedPlacementData() const;
+  void SetCachedPlacementData(NGGridPlacementData&& placement_data);
 
-  wtf_size_t ExplicitGridStartForDirection(
-      GridTrackSizingDirection direction) const final;
-  wtf_size_t ExplicitGridEndForDirection(
-      GridTrackSizingDirection direction) const final;
   wtf_size_t AutoRepeatCountForDirection(
-      GridTrackSizingDirection direction) const final;
-  LayoutUnit GridGap(GridTrackSizingDirection) const final;
-  LayoutUnit GridItemOffset(GridTrackSizingDirection) const final;
+      const GridTrackSizingDirection track_direction) const final;
+  wtf_size_t ExplicitGridStartForDirection(
+      const GridTrackSizingDirection track_direction) const final;
+  wtf_size_t ExplicitGridEndForDirection(
+      const GridTrackSizingDirection track_direction) const final;
+  LayoutUnit GridGap(
+      const GridTrackSizingDirection track_direction) const final;
+  LayoutUnit GridItemOffset(
+      const GridTrackSizingDirection track_direction) const final;
   Vector<LayoutUnit, 1> TrackSizesForComputedStyle(
-      GridTrackSizingDirection direction) const final;
+      const GridTrackSizingDirection track_direction) const final;
+
   Vector<LayoutUnit> RowPositions() const final;
   Vector<LayoutUnit> ColumnPositions() const final;
-
-  absl::optional<wtf_size_t> GetPreviousGridItemsSizeForReserveCapacity();
 
  protected:
   bool IsOfType(LayoutObjectType type) const override {
@@ -55,21 +52,20 @@ class CORE_EXPORT LayoutNGGrid : public LayoutNGBlock,
   }
 
  private:
-  const NGGridData* GetGridData() const;
-  Vector<LayoutUnit> ComputeTrackSizesInRange(
-      const NGGridLayoutAlgorithmTrackCollection::Range& range,
-      GridTrackSizingDirection direction) const;
+  const NGGridLayoutData* GridLayoutData() const;
+
+  Vector<LayoutUnit> ComputeTrackSizeRepeaterForRange(
+      const NGGridLayoutData::TrackCollectionGeometry& geometry,
+      const NGGridLayoutData::RangeData& range) const;
   Vector<LayoutUnit> ComputeExpandedPositions(
-      GridTrackSizingDirection direction) const;
+      const GridTrackSizingDirection track_direction) const;
 
   void AddChild(LayoutObject* new_child,
                 LayoutObject* before_child = nullptr) override;
   void RemoveChild(LayoutObject*) override;
   void StyleDidChange(StyleDifference, const ComputedStyle*) override;
 
-  NGGridPlacementProperties cached_placement_properties_;
-  wtf_size_t cached_column_auto_repetitions_;
-  wtf_size_t cached_row_auto_repetitions_;
+  absl::optional<NGGridPlacementData> cached_placement_data_;
 };
 
 // wtf/casting.h helper.
--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_geometry.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_geometry.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-using SetOffsetData = NGGridData::SetData;
+using SetOffsetData = NGGridLayoutData::SetData;
 
 // Contains the information about the start offset of the tracks, as well as
 // the gutter size between them, for a given direction.
