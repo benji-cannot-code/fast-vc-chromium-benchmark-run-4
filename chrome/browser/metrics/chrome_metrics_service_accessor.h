50ab6e55c918feaaf47474bab083277014f33cd6
Adam Langley <agl@chromium.org>
2021-11-02 23:35:07 +0000

    webauthn: enable server-link logging, guarded by metrics reporting.
    Logging of server-link transactions (i.e. sign-ins to
    accounts.google.com) can happen if metric & crash reporting is enabled.
    Therefore this change plumbs that boolean into the caBLEv2 code and
    connects up the logging call.
    
    ChromeMetricsServiceAccessor only allows its friends to ask whether
    metrics & crash reporting is enabled, and a static function (as all JNI
    callbacks are) cannot be a friend of a class in another compilation
    unit. Since all of ChromeMetricsServiceAccessor's other friends are
    classes, we proxy the call via a tiny class to be consistent.
    
    Change-Id: I012492beb386b08ec9f1116377320a3102032214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251795
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937621}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -90,6 +90,12 @@ namespace browser_sync {
 class DeviceInfoSyncClientImpl;
 }  // namespace browser_sync
 
+namespace webauthn {
+namespace authenticator {
+class IsMetricsAndCrashReportingEnabled;
+}
+}  // namespace webauthn
+
 // This class limits and documents access to metrics service helper methods.
 // Since these methods are private, each user has to be explicitly declared
 // as a 'friend' below.
@@ -149,6 +155,7 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class feed::FeedServiceBridge;
   friend class feed::WebFeedSubscriptionCoordinator;
   friend class HttpsFirstModeService;
+  friend class webauthn::authenticator::IsMetricsAndCrashReportingEnabled;
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   friend class ChromeCameraAppUIDelegate;
