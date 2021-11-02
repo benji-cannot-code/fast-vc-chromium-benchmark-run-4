7b73364a76c16d33705fc6eb2235e2c28454c588
Ahmed Nasr <anasr@google.com>
2021-11-02 04:42:31 +0000

    Telemetry: Add Cros Settings wrapper to control reporting and collection
    Bug: b:177847653
    Change-Id: I9bc2f4bd65f0d628a911500f688f5f835f025b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3254852
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937213}

---
+++ chrome/browser/ash/policy/reporting/metrics_reporting/cros_reporting_settings.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_CROS_REPORTING_SETTINGS_H_
+#define CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_CROS_REPORTING_SETTINGS_H_
+
+#include <string>
+
+#include "base/callback_forward.h"
+#include "base/callback_list.h"
+#include "components/reporting/metrics/reporting_settings.h"
+
+namespace reporting {
+
+// Wrapper of `ash::CrosSettings` methods needed for telemetry collection and
+// reporting.
+class CrosReportingSettings : public ReportingSettings {
+ public:
+  CrosReportingSettings() = default;
+
+  ~CrosReportingSettings() override = default;
+
+  base::CallbackListSubscription AddSettingsObserver(
+      const std::string& path,
+      base::RepeatingClosure callback) override;
+
+  bool PrepareTrustedValues(base::OnceClosure callback) override;
+
+  bool GetBoolean(const std::string& path, bool* out_value) const override;
+  bool GetInteger(const std::string& path, int* out_value) const override;
+};
+}  // namespace reporting
+
+#endif  // CHROME_BROWSER_ASH_POLICY_REPORTING_METRICS_REPORTING_CROS_REPORTING_SETTINGS_H_
