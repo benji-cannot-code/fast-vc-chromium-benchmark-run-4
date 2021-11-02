03cf32774bd32a4265ae1f4c98e9fc5702a18f3d
Nicolas Dossou-Gbete <dgn@chromium.org>
2021-11-02 17:38:35 +0000

    Stop calling PrefMetricsService on browser creation
    That service is already getting created with BrowserContext, making the
    explicit call on browser creation redundant for the general case and
    unwanted for the case where we show the profile picker on start.
    
    Bug: 1150326
    Change-Id: I957f38166c42538337e3ae71b175e96b751603ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3250827
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937433}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -78,7 +78,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/prefs/chrome_command_line_pref_store.h"
 #include "chrome/browser/prefs/chrome_pref_service_factory.h"
-#include "chrome/browser/prefs/pref_metrics_service.h"
 #include "chrome/browser/process_singleton.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
@@ -1639,9 +1638,6 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
 #if BUILDFLAG(ENABLE_NACL)
   nacl::NaClProcessHost::EarlyStartup();
 #endif  // BUILDFLAG(ENABLE_NACL)
-
-  // Make sure initial prefs are recorded
-  PrefMetricsService::Factory::GetForProfile(profile_);
 
   PreBrowserStart();
 
