ecad8f2d38af34c8189d8c2f80c85537c06e0ccd
Jose Dapena Paz <jdapena@igalia.com>
2021-11-02 17:13:57 +0000

    GCC: put attributes before function name in several failing function definitions in test_future.h
    In GCC, in an inconsistent way, attributes are not allowed to be added
    after function name in a function definition, if it is a template inside
    a class. This breaks compilation in GCC.
    
    Fixes build error:
      ../../base/compiler_specific.h:117:28: error: attributes are not allowed on a function-definition
        117 | #define WARN_UNUSED_RESULT __attribute__((warn_unused_result))
            |                            ^~~~~~~~~~~~~
      ../../base/test/test_future.h:156:75: note: in expansion of macro ‘WARN_UNUSED_RESULT’
        156 |   const typename std::tuple_element<I, std::tuple<Types...>>::type& Get() WARN_UNUSED_RESULT {
            |                                                                           ^~~~~~~~~~~~~~~~~~
    
    This has been reported at https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60625
    
    Bug: 819294
    Change-Id: I45de6af764739cc3b1628694425c67645c912e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257941
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#937421}

--- base/test/test_future.h
+++ base/test/test_future.h
@@ -206,7 +206,7 @@ class TestFuture {
   //
   // Will DCHECK if a timeout happens.
   template <typename U = T, internal::EnableIfSingleValue<U> = true>
-  const FirstType& Get() WARN_UNUSED_RESULT {
+  const FirstType& WARN_UNUSED_RESULT Get() {
     return std::get<0>(GetTuple());
   }
 
@@ -214,7 +214,7 @@ class TestFuture {
   //
   // Will DCHECK if a timeout happens.
   template <typename U = T, internal::EnableIfSingleValue<U> = true>
-  FirstType Take() WARN_UNUSED_RESULT {
+  FirstType WARN_UNUSED_RESULT Take() {
     return std::get<0>(TakeTuple());
   }
 
@@ -226,7 +226,7 @@ class TestFuture {
   //
   // Will DCHECK if a timeout happens.
   template <typename U = T, internal::EnableIfMultiValue<U> = true>
-  const std::tuple<Types...>& Get() WARN_UNUSED_RESULT {
+  const std::tuple<Types...>& WARN_UNUSED_RESULT Get() {
     return GetTuple();
   }
 
@@ -234,7 +234,7 @@ class TestFuture {
   //
   // Will DCHECK if a timeout happens.
   template <typename U = T, internal::EnableIfMultiValue<U> = true>
-  std::tuple<Types...> Take() WARN_UNUSED_RESULT {
+  std::tuple<Types...> WARN_UNUSED_RESULT Take() {
     return TakeTuple();
   }
 
