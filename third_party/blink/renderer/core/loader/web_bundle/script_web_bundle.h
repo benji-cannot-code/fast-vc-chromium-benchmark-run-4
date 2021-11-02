0504821a7f1f9126f4072b112e93320ecc5811d5
Miras Myrzakerey <myrzakereyms@google.com>
2021-11-02 08:13:17 +0000

    Add support for load/error events for ScriptWebBundle.
    This CL makes it so we fire load/error events appropriately for
    ScriptWebBundle:
    1. Bundle loading finished successfully => script.onload
    2. Bundle loading failed => script.onerror
    With these events properly fired, we resolve many failing WPTs that were
    actively waiting on them to proceed.
    
    In a follow-up CL we will also add window.onerror event in case of
    JSON parsing failure.
    
    Bug: 1263783
    Change-Id: I465643b35e4728caedd3d1ec13477a80902bfa52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245756
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Miras Myrzakerey <myrzakereyms@google.com>
    Cr-Commit-Position: refs/heads/main@{#937243}

--- third_party/blink/renderer/core/loader/web_bundle/script_web_bundle.h
+++ third_party/blink/renderer/core/loader/web_bundle/script_web_bundle.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom-blink.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/loader/web_bundle/script_web_bundle_rule.h"
+#include "third_party/blink/renderer/core/script/script_element_base.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
@@ -32,10 +33,12 @@ class CORE_EXPORT ScriptWebBundle final
     : public GarbageCollected<ScriptWebBundle>,
       public SubresourceWebBundle {
  public:
-  static ScriptWebBundle* CreateOrReuseInline(Document& element_document,
+  static ScriptWebBundle* CreateOrReuseInline(ScriptElementBase&,
                                               const String& inline_text);
 
-  ScriptWebBundle(Document& element_document, const ScriptWebBundleRule& rule);
+  ScriptWebBundle(ScriptElementBase& element,
+                  Document& element_document,
+                  const ScriptWebBundleRule& rule);
 
   void Trace(Visitor* visitor) const override;
 
@@ -44,7 +47,7 @@ class CORE_EXPORT ScriptWebBundle final
   String GetCacheIdentifier() const override;
   const KURL& GetBundleUrl() const override;
   const base::UnguessableToken& WebBundleToken() const override;
-  void NotifyLoaded() override;
+  void NotifyLoadingFinished() override;
   void OnWebBundleError(const String& message) const override;
   bool IsScriptWebBundle() const override;
   bool WillBeReleased() const override;
@@ -54,14 +57,17 @@ class CORE_EXPORT ScriptWebBundle final
   void ReleaseBundleLoaderAndUnregister();
 
   void WillReleaseBundleLoaderAndUnregister();
-  void CancelRelease() { will_be_released_ = false; }
-
-  void SetRule(ScriptWebBundleRule rule) { rule_ = std::move(rule); }
-
+  void ReusedWith(ScriptElementBase& element, ScriptWebBundleRule rule);
   class ReleaseResourceTask;
 
  private:
   bool will_be_released_ = false;
+  // We store both |element_| and |element_document_| here,
+  // because we need |element_| for dispatching load/error events
+  // and creating/releasing WebBundleLoader we want to be extra
+  // careful that we're dealing with the correct document, so
+  // we explicitly capture it at the time of PrepareScript().
+  WeakMember<ScriptElementBase> element_;
   WeakMember<Document> element_document_;
   ScriptWebBundleRule rule_;
   Member<WebBundleLoader> bundle_loader_;
