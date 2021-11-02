6d4bd8be8f44736efb05b0c3ffb81d8b26641e4d
Tim van der Lippe <tvanderlippe@chromium.org>
2021-11-02 07:23:37 +0000

    Fix accessibility layout test
    R=szuend@chromium.org
    
    Bug: 1127902
    Change-Id: I521323866192b4253ec8f37bcdb846dab4794623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253378
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937236}

--- third_party/blink/web_tests/http/tests/devtools/elements/accessibility/edit-aria-attributes.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/accessibility/edit-aria-attributes.js
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 (async function() {
   TestRunner.addResult(`Tests that writing an ARIA attribute causes the accessibility node to be updated.\n`);
-  await TestRunner.loadModule('elements'); await TestRunner.loadTestModule('elements_test_runner');
-  await TestRunner.loadModule('panels/accessibility');
+  await TestRunner.loadLegacyModule('elements'); await TestRunner.loadTestModule('elements_test_runner');
+  await TestRunner.loadLegacyModule('panels/accessibility');
   await TestRunner.loadTestModule('accessibility_test_runner');
   await TestRunner.showPanel('elements');
   await TestRunner.loadHTML(`
