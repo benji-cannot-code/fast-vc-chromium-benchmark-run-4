2250d1dc77fb3ab3677cc9d9d6c81dfe73166de3
Piotr Bialecki <bialpio@chromium.org>
2021-11-02 01:19:20 +0000

    SkiaRenderer: add BlitRequest support for NV12
    This CL continues on top of the previous changes to CopyOutputRequest
    API and extends it with a capability to append BlitRequests to it.
    BlitRequest allows the consumer of CopyOutputRequest API to provide a
    collection of textures to which the results of the request will be
    blitted. The caller retains ownership of the textures. The API allows
    the callers to specify which region of their textures will be blitted
    into.
    
    Major changes:
    - add new struct, BlitRequest, which can optionally be appended to a
      CopyOutputRequest to ask viz/ to populate a region of caller-owned
      textures
    - `skia::BlitRGBAToYUVA()` now accepts a new parameter, `dst_region`,
      describing a subregion which should be populated with the results of
      the blit; the existing `dst_yuva_info` now describes the entire image
      that is passed in via `dst_surfaces`
    - `SkiaOutputSurfaceImplOnGpu` has a new helper,
      `ImportSurfacesForNV12Planes()`, roughly equivalent to
      `CreateSurfacesForNV12Planes()` except that the produced
      `PlaneAccessData`s are backed by caller-provided textures
    - add handling of BlitRequests in
      `SkiaOutputSurfaceImplOnGpu::CopyOutputNV12()` - bulk of the logic is
      taken care of by the helper mentioned above, with one notable change
      that if a blit request is present, we will not take the ownership of
      the caller-provided textures just prior to sending the result
    - add unit tests to cover the newly added functionality
    
    Minor changes:
    - tweaked `rgb_to_yuv_rec601_limited` in `skia::BlitRGBAToYUVA()` so
      that it's consistently rounded to 3 decimal places
    - added `GLScalerTestUtil::ConvertRGBAColorToYUV()` helper, which is a
      single color equivalent of `ConvertRGBABitmapToYUV()` (which operates
      on an entire bitmap)
    - `SkiaOutputSurfaceImpl` grabs sync tokens from mailbox holders passed
      in via a blit request before enqueuing a GPU task to copy the output
    
    Bug: b/196633661
    Change-Id: I21b7ff7c194856705c2925ed41c4bf8dcefc202d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3235102
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#937156}

---
+++ components/viz/common/frame_sinks/blit_request.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/viz/common/frame_sinks/blit_request.h"
+
+#include "base/strings/stringprintf.h"
+
+namespace viz {
+
+BlitRequest::BlitRequest(
+    const gfx::Point& destination_region_offset,
+    const std::array<gpu::MailboxHolder, CopyOutputResult::kMaxPlanes>&
+        mailboxes)
+    : destination_region_offset(destination_region_offset),
+      mailboxes(mailboxes) {}
+
+BlitRequest::BlitRequest(const BlitRequest& other) = default;
+BlitRequest& BlitRequest::operator=(const BlitRequest& other) = default;
+
+BlitRequest::~BlitRequest() = default;
+
+std::string BlitRequest::ToString() const {
+  return base::StringPrintf("blit to %s",
+                            destination_region_offset.ToString().c_str());
+}
+
+}  // namespace viz
