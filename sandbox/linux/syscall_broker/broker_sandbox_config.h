5361915f5e81b665e6da5bef9a5eea98b1bf4809
Jose Dapena Paz <jdapena@igalia.com>
2021-11-02 17:12:32 +0000

    IWYU: missing include for vector and unique_ptr usage in broker_sandbox_config.h
    Fix several build errors with libstdc++ due to missing includes for std::vector
    and std::unique_ptr.
    
    Bug: 957519
    Change-Id: I74dd4acb02cd1b0029da3f8794d174d3df2f7eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257961
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#937420}

--- sandbox/linux/syscall_broker/broker_sandbox_config.h
+++ sandbox/linux/syscall_broker/broker_sandbox_config.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SANDBOX_LINUX_SYSCALL_BROKER_BROKER_SANDBOX_CONFIG_H_
 #define SANDBOX_LINUX_SYSCALL_BROKER_BROKER_SANDBOX_CONFIG_H_
 
+#include <memory>
+#include <vector>
+
 #include "sandbox/linux/syscall_broker/broker_command.h"
 #include "sandbox/linux/syscall_broker/broker_file_permission.h"
 #include "sandbox/sandbox_export.h"
