e854d9a661115a230c9034bb6446d2ff4e3f1b87
Robert Ogden <robertogden@chromium.org>
2021-11-02 21:54:01 +0000

    Add debugging/test helpers to PCA Common classes
    Adds a bunch of ostream and ToString() and sorting to the output result.
    
    Bug: 1249632
    Change-Id: I0796a066517e81d9c0964945be1986f79afb81b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256651
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937552}

--- components/optimization_guide/core/page_content_annotations_common.h
+++ components/optimization_guide/core/page_content_annotations_common.h
@@ -56,6 +56,10 @@ enum class AnnotationType {
   kPageEntities,
 };
 
+std::string ExecutionStatusToString(ExecutionStatus status);
+
+std::string AnnotationTypeToString(AnnotationType type);
+
 // A weighted string value.
 class WeightedString {
  public:
@@ -66,7 +70,12 @@ class WeightedString {
   std::string value() const { return value_; }
   double weight() const { return weight_; }
 
+  std::string ToString() const;
+
   bool operator==(const WeightedString& other) const;
+
+  friend std::ostream& operator<<(std::ostream& stream,
+                                  const WeightedString& ws);
 
  private:
   std::string value_;
@@ -113,7 +122,12 @@ class BatchAnnotationResult {
   }
   absl::optional<double> visibility_score() const { return visibility_score_; }
 
+  std::string ToString() const;
+
   bool operator==(const BatchAnnotationResult& other) const;
+
+  friend std::ostream& operator<<(std::ostream& stream,
+                                  const BatchAnnotationResult& result);
 
  private:
   BatchAnnotationResult();
