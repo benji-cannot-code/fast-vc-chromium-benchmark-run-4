10df0e80ddb4a0f7817167031e53e9808dd695c5
conniekxu <conniekxu@chromium.org>
2021-11-02 20:37:08 +0000

    capture_mode_settings: Add chrome flag for capture advanced settings view
    This CL adds a chrome flag for enabling the advanced settings view of
    screen capture mode.
    
    
    Fixed: 1264514
    Change-Id: I1348a3c5fa4c97abdd620f827efd77e2f38d6a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251849
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937524}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2778,6 +2778,9 @@ extern const char kLacrosSupportDescription[];
 extern const char kLimitShelfItemsToActiveDeskName[];
 extern const char kLimitShelfItemsToActiveDeskDescription[];
 
+extern const char kImprovedScreenCaptureSettingsName[];
+extern const char kImprovedScreenCaptureSettingsDescription[];
+
 extern const char kListAllDisplayModesName[];
 extern const char kListAllDisplayModesDescription[];
 
