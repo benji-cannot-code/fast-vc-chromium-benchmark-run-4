2c9362c43aed66f039a21fd03946f4b0a1bc280d
Aaron Krajeski <aaronhk@chromium.org>
2021-11-02 17:11:56 +0000

    Fix mis-labed usecounters for the new Canvas2DAPI
    Bug: 1201359
    Change-Id: I13e3d5d966e1ced8d2d2e0f9b0802c513fadc791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257982
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937419}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -357,7 +357,7 @@ void BaseRenderingContext2D::ResetInternal() {
 
 void BaseRenderingContext2D::reset() {
   UseCounter::Count(GetCanvasRenderingContextHost()->GetTopExecutionContext(),
-                    WebFeature::kCanvasRenderingContext2DRoundRect);
+                    WebFeature::kCanvasRenderingContext2DReset);
   ResetInternal();
 }
 
