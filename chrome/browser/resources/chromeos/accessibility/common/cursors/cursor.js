1e0867eff262cfa940f82114815f4efd4878ab17
danielng <danielng@google.com>
2021-11-02 21:17:56 +0000

    Typo fix
    Change-Id: I90083497018c8ece4b97272f6acd90abcd892a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256603
    Auto-Submit: Daniel Ng <danielng@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937537}

--- chrome/browser/resources/chromeos/accessibility/common/cursors/cursor.js
+++ chrome/browser/resources/chromeos/accessibility/common/cursors/cursor.js
@@ -394,7 +394,7 @@ cursors.Cursor = class {
               }
             }
             if (goog.isDef(start)) {
-              // Succesfully found the next word stop within the same text
+              // Successfully found the next word stop within the same text
               // node.
               newIndex = start;
             } else {
