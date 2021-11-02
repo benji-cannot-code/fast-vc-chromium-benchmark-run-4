8cbc460fb704e7546ca38c9fb9f2e8def1d9de09
Ali Beyad <abeyad@chromium.org>
2021-11-02 19:34:06 +0000

    Send the devtools User-Agent override on redirects
    This CL fixes a regression introduced by the User-Agent Reduction origin
    trial CL crrev.com/c/3078124.  If devtools overrode the User-Agent
    request header, it would get applied on the initial request but would
    get reset back to the original User-Agent header on a redirected
    request.
    
    This CL fixes the issue by only applying User-Agent reduction logic on
    redirects if devtools has not overridden the User-Agent request header.
    
    Bug: 1222742, 1264251, 1263639
    Change-Id: Ibb68fb5bc095ecce6c84162064099a609c647c72
    Fixed: 1264251, 1263639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251922
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Commit-Queue: Ali Beyad <abeyad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937502}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/emulation/resources/redirect.php
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header('HTTP/1.1 307 Temporary Redirect');
+header('Pragma: no-cache');
+header('Location: echo-headers.php');
+?>
