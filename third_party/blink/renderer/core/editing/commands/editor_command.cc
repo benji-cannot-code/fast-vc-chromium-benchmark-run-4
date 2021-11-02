ba4b1ee8327aba73c0aeee0716e72d62f357cb11
Joey Arhar <jarhar@chromium.org>
2021-11-02 02:44:39 +0000

    Fix DCHECK in editor FindString command
    This bug and fix is very similar to crbug.com/1251072. I made the same
    fix as that bug, I didn't know that there was another place in editing
    that could run Editor::FindString like window.find() does for the
    mentioned bug.
    
    I verified that this change fixes the clusterfuzz test case, and I also
    added a similar test case to the one I added in crbug.com/1251072.
    
    Fixed: 1264507
    Change-Id: I24067700444d9a0ea79211c4324845d453fc9d89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253512
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937186}

--- third_party/blink/renderer/core/editing/commands/editor_command.cc
+++ third_party/blink/renderer/core/editing/commands/editor_command.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/css_value_keywords.h"
+#include "third_party/blink/renderer/core/display_lock/display_lock_document_state.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/tag_collection.h"
 #include "third_party/blink/renderer/core/editing/commands/clipboard_commands.h"
@@ -1986,6 +1987,18 @@ bool EditorCommand::Execute(const String& parameter,
           break;
       }
     }
+  }
+
+  // We need to force unlock activatable DisplayLocks for Editor::FindString
+  // before the following call to UpdateStyleAndLayout. Otherwise,
+  // ExecuteFindString/Editor::FindString will hit bad style/layout data.
+  absl::optional<DisplayLockDocumentState::ScopedForceActivatableDisplayLocks>
+      forced_locks;
+  if (command_->command_type == EditingCommandType::kFindString) {
+    forced_locks = GetFrame()
+                       .GetDocument()
+                       ->GetDisplayLockDocumentState()
+                       .GetScopedForceActivatableLocks();
   }
 
   GetFrame().GetDocument()->UpdateStyleAndLayout(
