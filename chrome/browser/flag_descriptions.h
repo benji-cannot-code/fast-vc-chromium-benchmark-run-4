9082f9ceff7394246630b6642c46bc03e48f7bd3
Jason Thai <jasontt@chromium.org>
2021-11-02 22:46:07 +0000

    personalization: add new flag for personalization hub
    Bug: b/202889314
    Test: locally run and see flag in chrome://flags
    Change-Id: Ie7ef55d321528784f24dd0c2c955ffaa6bae966b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256363
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937585}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1163,6 +1163,9 @@ extern const char kPermissionQuietChipDescription[];
 extern const char kPersistentQuotaIsTemporaryQuotaName[];
 extern const char kPersistentQuotaIsTemporaryQuotaDescription[];
 
+extern const char kPersonalizationHubName[];
+extern const char kPersonalizationHubDescription[];
+
 extern const char kPlaybackSpeedButtonName[];
 extern const char kPlaybackSpeedButtonDescription[];
 
