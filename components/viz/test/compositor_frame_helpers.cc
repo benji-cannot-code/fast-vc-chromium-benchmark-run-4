20bc744b1f526087dd7d8d833ebf9efea1d26596
kylechar <kylechar@chromium.org>
2021-11-02 18:02:51 +0000

    Use RenderPassDrawQuad::rect for backdrop filter damage
    Backdrop filters sample from the area behind the RenderPassDrawQuad.
    CompositorRenderPass::output_rect doesn't necessarily correspond to the
    CompositorRenderPassDrawQuad::rect, since it can be scaled or
    transformed arbitrarily, so use |rect| when computing possible
    intersections with a backdrop filter.
    
    In practice |output_rect| usually equals the quad |rect| so this
    shouldn't be a big change. This CL fixes some unit tests where that
    wasn't true by fixing the quad |rect|.
    
    Fixed: 1263494
    Change-Id: I75e6a53c4ebfdbd139a359a4664d2e8124496b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242698
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937452}

--- components/viz/test/compositor_frame_helpers.cc
+++ components/viz/test/compositor_frame_helpers.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <set>
 #include <utility>
+#include "components/viz/common/quads/compositor_render_pass.h"
 #include "components/viz/common/quads/compositor_render_pass_draw_quad.h"
 #include "components/viz/common/quads/solid_color_draw_quad.h"
 #include "components/viz/common/quads/surface_draw_quad.h"
@@ -21,6 +22,10 @@ constexpr gfx::Rect kDefaultOutputRect(20, 20);
 constexpr gfx::Rect kDefaultDamageRect(0, 0);
 
 }  // namespace
+
+RenderPassBuilder::RenderPassBuilder(CompositorRenderPassId id,
+                                     const gfx::Size& output_size)
+    : RenderPassBuilder(id, gfx::Rect(output_size)) {}
 
 RenderPassBuilder::RenderPassBuilder(CompositorRenderPassId id,
                                      const gfx::Rect& output_rect)
@@ -49,6 +54,18 @@ RenderPassBuilder& RenderPassBuilder::SetCacheRenderPass(bool val) {
 RenderPassBuilder& RenderPassBuilder::SetHasDamageFromContributingContent(
     bool val) {
   pass_->has_damage_from_contributing_content = val;
+  return *this;
+}
+
+RenderPassBuilder& RenderPassBuilder::AddFilter(
+    const cc::FilterOperation& filter) {
+  pass_->filters.Append(filter);
+  return *this;
+}
+
+RenderPassBuilder& RenderPassBuilder::AddBackdropFilter(
+    const cc::FilterOperation& filter) {
+  pass_->backdrop_filters.Append(filter);
   return *this;
 }
 
@@ -317,8 +334,22 @@ CompositorFrame CompositorFrameBuilder::MakeInitCompositorFrame() const {
   return frame;
 }
 
+CompositorRenderPassList CopyRenderPasses(
+    const CompositorRenderPassList& render_pass_list) {
+  CompositorRenderPassList copy_list;
+  for (auto& render_pass : render_pass_list)
+    copy_list.push_back(render_pass->DeepCopy());
+  return copy_list;
+}
+
 CompositorFrame MakeDefaultCompositorFrame() {
   return CompositorFrameBuilder().AddDefaultRenderPass().Build();
+}
+
+CompositorFrame MakeCompositorFrame(CompositorRenderPassList render_pass_list) {
+  return CompositorFrameBuilder()
+      .SetRenderPassList(std::move(render_pass_list))
+      .Build();
 }
 
 AggregatedFrame MakeDefaultAggregatedFrame(size_t num_render_passes) {
