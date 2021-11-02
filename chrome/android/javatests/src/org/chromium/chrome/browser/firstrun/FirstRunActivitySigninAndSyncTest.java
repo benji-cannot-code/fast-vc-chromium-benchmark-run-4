8b9fe91c4a36c7ce8e931a623db49e0f65704f93
Alice Wang <aliceywang@chromium.org>
2021-11-02 16:48:02 +0000

    [Android][Test] Fix flaky tests in FirstRunActivitySigninAndSyncTest
    This CL fixes the flaky tests in FirstRunActivitySigninAndSyncTest by
    mocking DataReductionProxySettings, because this singleton behaves
    differently in official and non-official environments.
    
    Bug: 1260822
    Change-Id: I27c27e216407e6060396edfb4949237f91ba6f0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257404
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937398}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunActivitySigninAndSyncTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunActivitySigninAndSyncTest.java
@@ -32,9 +32,9 @@ import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.signin.SigninFirstRunFragment;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
@@ -63,15 +63,21 @@ public class FirstRunActivitySigninAndSyncTest {
     @Mock
     private ExternalAuthUtils mExternalAuthUtilsMock;
 
+    @Mock
+    private DataReductionProxySettings mDataReductionProxySettingsMock;
+
     @Before
     public void setUp() {
+        when(mDataReductionProxySettingsMock.isDataReductionProxyManaged()).thenReturn(false);
+        when(mDataReductionProxySettingsMock.isDataReductionProxyFREPromoAllowed())
+                .thenReturn(true);
+        DataReductionProxySettings.setInstanceForTesting(mDataReductionProxySettingsMock);
         when(mExternalAuthUtilsMock.canUseGooglePlayServices()).thenReturn(true);
         ExternalAuthUtils.setInstanceForTesting(mExternalAuthUtilsMock);
     }
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1260822")
     public void dismissButtonClickSkipsSyncConsentPageWhenNoAccountsAreOnDevice() {
         launchFirstRunActivity();
         onView(withId(R.id.signin_fre_selected_account)).check(matches(not(isDisplayed())));
@@ -83,7 +89,6 @@ public class FirstRunActivitySigninAndSyncTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1260822")
     public void dismissButtonClickSkipsSyncConsentPageWhenOneAccountIsOnDevice() {
         mAccountManagerTestRule.addAccount(TEST_EMAIL);
         launchFirstRunActivity();
@@ -122,7 +127,6 @@ public class FirstRunActivitySigninAndSyncTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1260822")
     public void continueButtonClickSkipsSyncConsentPageWhenCannotUseGooglePlayServices() {
         when(mExternalAuthUtilsMock.canUseGooglePlayServices()).thenReturn(false);
         launchFirstRunActivity();
