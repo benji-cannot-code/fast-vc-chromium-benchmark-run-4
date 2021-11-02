cb617410a5806335cb065cda45c6d17fe4e4d430
Tyler Garrett <tylergarrett@chromium.org>
2021-11-02 16:23:27 +0000

    Reporting: Fix healthd callback not being called
    Due to a recent change the callback from fetching data from the health
    daemon is not being called. This change ensures it is.
    
    Bug: b:204843574
    Change-Id: Id7307caee2cac56145f1c31286eb860230890217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256870
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Tyler Garrett <tylergarrett@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937380}

--- chrome/browser/ash/policy/status_collector/device_status_collector.cc
+++ chrome/browser/ash/policy/status_collector/device_status_collector.cc
@@ -2032,6 +2032,8 @@ void DeviceStatusCollector::SampleDischargeRate(
       it->second.set_charge_rate(percent);
     }
   }
+
+  AddDataSample(std::move(sample), std::move(callback));
 }
 
 void DeviceStatusCollector::ReceiveCPUTemperature(
