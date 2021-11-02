ce85745eb4bdb799c70b553cf97ddf552944ba84
Richard Chui <richui@chromium.org>
2021-11-02 20:50:39 +0000

    Desk Templates: Hide instead of close template grid
    Change the `overview_grid` logic to hide the desks templates grid when
    we don't want to show it anymore instead of closing it, so that we can
    reuse the created widget.
    
    This CL also contains a typo and comment cleanup.
    
    Fixed: 1265733
    Test: manual
    Change-Id: I977c9955824c3345124d71490506d7571fcb04e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256227
    Commit-Queue: Richard Chui <richui@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937529}

--- ash/wm/desks/templates/desks_templates_unittest.cc
+++ ash/wm/desks/templates/desks_templates_unittest.cc
@@ -490,7 +490,7 @@ TEST_F(DesksTemplatesTest, HideOverviewItemsOnTemplateGridShow) {
   EXPECT_EQ(1.0f, test_window->layer()->opacity());
 }
 
-// Tests that when thetemplates grid is shown and the active desk is closed,
+// Tests that when the templates grid is shown and the active desk is closed,
 // overview items stay hidden.
 TEST_F(DesksTemplatesTest, OverviewItemsStayHiddenInTemplateGridOnDeskClose) {
   AddEntry(base::GUID::GenerateRandomV4(), "template_1", base::Time::Now());
@@ -1032,7 +1032,8 @@ TEST_F(DesksTemplatesTest, ShowingTemplatesGridToTabletMode) {
   aura::Window* root_window = Shell::GetPrimaryRootWindow();
   ASSERT_TRUE(GetOverviewSession()
                   ->GetGridWithRootWindow(root_window)
-                  ->desks_templates_grid_widget());
+                  ->desks_templates_grid_widget()
+                  ->IsVisible());
 
   // Tests that after transitioning, we remain in overview mode and the grid is
   // hidden.
@@ -1040,7 +1041,8 @@ TEST_F(DesksTemplatesTest, ShowingTemplatesGridToTabletMode) {
   ASSERT_TRUE(GetOverviewSession());
   EXPECT_FALSE(GetOverviewSession()
                    ->GetGridWithRootWindow(root_window)
-                   ->desks_templates_grid_widget());
+                   ->desks_templates_grid_widget()
+                   ->IsVisible());
 }
 
 }  // namespace ash
