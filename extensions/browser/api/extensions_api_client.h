c6546279721801cf705681d5d0388d98b983dfa1
Francois Doray <fdoray@chromium.org>
2021-11-03 16:25:16 +0000

    [image decoding] Avoid copy when invoking ImageDecoder from ClipboardExtensionHelper.
    An rvalue reference is passed to ImageDecoder to transfer ownership of
    the existing memory buffer instead of copying it.
    
    Change-Id: If3bd8ab76bec34fd9098ae147704affcad9d685a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252004
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937922}

--- extensions/browser/api/extensions_api_client.h
+++ extensions/browser/api/extensions_api_client.h
@@ -214,7 +214,7 @@ class ExtensionsAPIClient {
 #if defined(OS_CHROMEOS)
   // Saves image data on clipboard.
   virtual void SaveImageDataToClipboard(
-      const std::vector<char>& image_data,
+      std::vector<uint8_t> image_data,
       api::clipboard::ImageType type,
       AdditionalDataItemList additional_items,
       base::OnceClosure success_callback,
