b3df4a3d0ef02b82f7182325ff5830c484e22ef6
Sunny Sachanandani <sunnyps@chromium.org>
2021-11-03 15:48:20 +0000

    capture: webrtc: webcodecs: Enable GMB VideoFrame readback on Windows
    This enables the shared memory GpuMemoryBuffer readback path for Canvas
    Capture, WebCodecs, and WebRTC frames.
    
    Bug: 1247414
    Change-Id: I42af3551e7ae328056daa1c849556e54c03719c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3255724
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937899}

--- third_party/blink/renderer/modules/webcodecs/video_encoder.cc
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "base/trace_event/trace_event.h"
+#include "build/build_config.h"
 #include "build/os_buildflags.h"
 #include "components/viz/common/gpu/raster_context_provider.h"
 #include "gpu/GLES2/gl2extchromium.h"
@@ -370,7 +371,7 @@ VideoEncoderConfig* CopyConfig(const VideoEncoderConfig& config) {
 
 const base::Feature kWebCodecsEncoderGpuMemoryBufferReadback {
   "WebCodecsEncoderGpuMemoryBufferReadback",
-#if BUILDFLAG(IS_MAC) || \
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || \
     (BUILDFLAG(IS_CHROMEOS) && defined(ARCH_CPU_X86_FAMILY))
       base::FEATURE_ENABLED_BY_DEFAULT
 #else
