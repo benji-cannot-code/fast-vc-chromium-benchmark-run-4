d8ad1dab899c0309d42c26098e9ea4313e8b73cb
Mark Cogan <marq@google.com>
2021-11-03 18:59:45 +0000

    [iOS] Clean up BVC imports and dependencies.
    Removes ~24 unneeded includes from the BVC, and, where possible, the
    corresponding deps in BUILD.gn
    
    
    Change-Id: Ic29411dd8dedb8fc709145ac214434f481e7b557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229545
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937980}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.h
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/ios/block_types.h"
 #import "ios/chrome/browser/ui/find_bar/find_bar_coordinator.h"
-#import "ios/chrome/browser/ui/gestures/view_revealing_vertical_pan_handler.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_consumer.h"
 #import "ios/chrome/browser/ui/ntp/logo_animation_controller.h"
 #import "ios/chrome/browser/ui/page_info/requirements/page_info_presentation.h"
