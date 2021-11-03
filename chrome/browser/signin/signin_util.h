83768fadb01d129b5d1581ca12056e983571905c
Yann Dago <ydago@chromium.org>
2021-11-03 16:30:37 +0000

    Refactor ProfileSeparationEnforcedByPolicy to support user cloud policy
    Bug: 1261474
    Change-Id: I41a15109df91b6a9ec37042ba0e437143309ea6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3231636
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937924}

--- chrome/browser/signin/signin_util.h
+++ chrome/browser/signin/signin_util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SIGNIN_SIGNIN_UTIL_H_
 #define CHROME_BROWSER_SIGNIN_SIGNIN_UTIL_H_
 
+#include <string>
+
 #include "build/build_config.h"
 
 class Profile;
@@ -57,11 +59,14 @@ void EnsurePrimaryAccountAllowedForProfile(Profile* profile);
 
 #if !defined(OS_ANDROID)
 // Returns true if profile separation is enforced by policy.
-bool ProfileSeparationEnforcedByPolicy(Profile* profile);
+bool ProfileSeparationEnforcedByPolicy(
+    Profile* profile,
+    const std::string& intercepted_account_level_policy_value);
 
 // Records a UMA metric if the user accepts or not to create an enterprise
 // profile.
-void RecordEnterpriseProfileCreationUserChoice(Profile* profile, bool created);
+void RecordEnterpriseProfileCreationUserChoice(bool enforced_by_policy,
+                                               bool created);
 #endif
 
 }  // namespace signin_util
