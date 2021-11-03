b169bb9bde00ed357bc684a11c35cda11942644c
Ana Salazar <anasalazar@chromium.org>
2021-11-03 22:22:51 +0000

    [launcher]: Only show apps grid separator if needed
    Between the Continue Section/Recent Apps and the Apps Grid Container,
    we show a Separator view for a more organized launcher.
    If the Continue Section and the Recent Apps are both empty, there will
    be no point on showing the Separator.
    
    This CL assigns visibility to the Separator view in accordance to the
    visibility of both sections.
    
    Before: https://screenshot.googleplex.com/45gWtDJmoCNEmb7.png
    After: https://screenshot.googleplex.com/6LJwuMi5FmeYayF.png
    
    Bug: 1216085, 1216084
    Change-Id: Ifb3058db9520420754ed6597600ca88cac625441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3254780
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938039}

--- ash/app_list/views/app_list_bubble_apps_page.h
+++ ash/app_list/views/app_list_bubble_apps_page.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
 
+namespace views {
+class Separator;
+}  // namespace views
+
 namespace ash {
 
 class AppListConfig;
@@ -54,6 +58,7 @@ class ASH_EXPORT AppListBubbleAppsPage : public views::View,
 
   // views::View:
   void Layout() override;
+  void ChildVisibilityChanged(views::View* child) override;
 
   // AppListModelProvider::Observer:
   void OnActiveAppListModelsChanged(AppListModel* model,
@@ -72,12 +77,16 @@ class ASH_EXPORT AppListBubbleAppsPage : public views::View,
   }
 
   RecentAppsView* recent_apps_for_test() { return recent_apps_; }
+  views::Separator* separator_for_test() { return separator_; }
 
  private:
   friend class AppListTestHelper;
 
+  void UpdateSeparatorVisibility();
+
   ContinueSectionView* continue_section_ = nullptr;
   RecentAppsView* recent_apps_ = nullptr;
+  views::Separator* separator_ = nullptr;
   views::ScrollView* scroll_view_ = nullptr;
   ScrollableAppsGridView* scrollable_apps_grid_view_ = nullptr;
 
