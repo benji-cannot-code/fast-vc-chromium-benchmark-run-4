5e4b43bb2dbf0f93c44033c5814ec9f894cdd977
Joey Arhar <jarhar@chromium.org>
2021-11-03 21:15:37 +0000

    ComputeContainerNode -> AnchorNode for ScopedForcedUpdate
    In crrev.com/921268 I replaced the usage of AnchorNode with a Range
    based unlock. In crrev.com/937171 I re-added the removed single node
    unlock, but instead of using AnchorNode like I should have, I used
    ComputeContainerNode.
    
    ComputeContainerNode is used for the Range based unlock because that's
    what Range uses in its constructor and it must be non-null for Ranges.
    
    The test case is unminimized so I didn't include a test in this patch,
    but I verified with the clusterfuzz repro tool that this fixes the bug.
    
    Fixed: 1266366
    Change-Id: I8416d1c28ebad3ff6fc5341ca8e8ea740133eb83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260206
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938025}

--- third_party/blink/renderer/core/editing/frame_selection.cc
+++ third_party/blink/renderer/core/editing/frame_selection.cc
@@ -170,8 +170,7 @@ VisibleSelection FrameSelection::ComputeVisibleSelectionInDOMTreeDeprecated()
         DisplayLockContext::ForcedPhase::kLayout);
   } else {
     force_locks = DisplayLockUtilities::ScopedForcedUpdate(
-        base.ComputeContainerNode(),
-        DisplayLockContext::ForcedPhase::kLayout);
+        base.AnchorNode(), DisplayLockContext::ForcedPhase::kLayout);
   }
   GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kSelection);
   return ComputeVisibleSelectionInDOMTree();
