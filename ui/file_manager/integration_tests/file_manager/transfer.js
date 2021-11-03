b9ee73a33b7c2bc9770175f4f31aa237c6d56e11
Luciano Pacheco <lucmult@chromium.org>
2021-11-03 04:49:45 +0000

    Files SWA: Add move, delete and zip operations to feedback panels.
    Refactor the translation messages to be managed only in the
    ProgressCenterPanel for these operations.
    
    Bug: b:204253360, b:204253094, b:204253085, b:204836025
    Change-Id: Ib7a730e0c3938a40ee569abaf073c9c5e1ebde66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257945
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937707}

--- ui/file_manager/integration_tests/file_manager/transfer.js
+++ ui/file_manager/integration_tests/file_manager/transfer.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {ENTRIES, EntryType, RootPath, sendTestMessage, TestEntryInfo} from '../test_util.js';
+import {ENTRIES, EntryType, getCaller, pending, repeatUntil, RootPath, sendTestMessage, TestEntryInfo} from '../test_util.js';
 import {testcase} from '../testcase.js';
 
 import {expandTreeItem, isSinglePartitionFormat, navigateWithDirectoryTree, remoteCall, setupAndWaitUntilReady} from './background.js';
@@ -1014,11 +1014,22 @@ testcase.transferDeletedFile = async () => {
       await remoteCall.callRemoteTestUtil('execCommand', appId, ['paste']));
 
   // Check that the error appears in the feedback panel.
-  const element = await remoteCall.waitForElement(
-      appId, ['#progress-panel', 'xf-panel-item']);
-  chrome.test.assertEq(
-      `Whoops, ${entry.nameText} no longer exists.`,
-      element.attributes['primary-text']);
+  let element = {};
+  const caller = getCaller();
+  await repeatUntil(async () => {
+    element = await remoteCall.waitForElement(
+        appId, ['#progress-panel', 'xf-panel-item']);
+    const expectedMsg = `Whoops, ${entry.nameText} no longer exists.`;
+    const actualMsg = element.attributes['primary-text'];
+
+    if (actualMsg === expectedMsg) {
+      return;
+    }
+
+    return pending(
+        caller,
+        `Expected feedback panel msg: "${expectedMsg}", got "${actualMsg}"`);
+  });
 
   // Check that only one line of text is shown.
   chrome.test.assertFalse(!!element.attributes['secondary-text']);
