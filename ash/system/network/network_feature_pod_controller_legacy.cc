74698550af116e3aaa760706500e560d82ab8fd7
Chad Duffin <chadduffin@chromium.org>
2021-11-03 21:09:32 +0000

    [CrOS Network] Implement NetworkFeaturePodController.
    This change implements the NetworkFeaturePodController class responsible
    for handling all of the logic for updating the NetworkFeaturePodButton.
    The implementation is largely a combination of the previous controller
    and button implementations with minor cleanup changes for readability.
    
    The only functional differences this change makes are to the tooltips.
    Previously, when the feature pod button was enabled, the tooltip for the
    icon would suggest that pressing the icon would toggle the state of the
    default network type and the tooltip for the label would suggest that
    pressing the label would only navigate to the detailed view. However,
    if the default network type was ethernet then it could not be toggled by
    the icon, and if the default network type was WiFi (and it was disabled)
    then it would be toggled on by the label.
    
    Ethernet
     - Connected:         https://screenshot.googleplex.com/6oith9ddfUjbMjh.png
     - Icon tooltip:      https://screenshot.googleplex.com/9ikRdcFYuCa8u2v.png
     - Label tooltip:     https://screenshot.googleplex.com/5hLhmhmqEAZnPHT.png
    
    WiFi
     - Connected:         https://screenshot.googleplex.com/TXLsDWviPqtLD5z.png
     - Icon tooltip:      https://screenshot.googleplex.com/4Rrfin44yMcyXkZ.png
     - Label tooltip:     https://screenshot.googleplex.com/88AJGfS4w3Txpa5.png
     - Sub-Label tooltip: https://screenshot.googleplex.com/53nw3cRz9mciVTZ.png
    
    Mobile
     - Connected:         https://screenshot.googleplex.com/72tPZDTT4LqNiWz.png
     - Icon tooltip:      https://screenshot.googleplex.com/6fbrNY53RDhpwvT.png
     - Label tooltip:     https://screenshot.googleplex.com/6yviQstzi65XDDR.png
    
    Tether
     - Connected:         https://screenshot.googleplex.com/Bfo2TE5Zk3sd9P3.png
     - Icon tooltip:      https://screenshot.googleplex.com/3433gH2w4aS6zjV.png
     - Label tooltip:     https://screenshot.googleplex.com/A6ahxVJswp45qV9.png
     - Sub-Label tooltip: https://screenshot.googleplex.com/8pjgYjqMJPsVg8k.png
    
    Enabled
     - Toggled:           https://screenshot.googleplex.com/7FQXfgqGxYs94WY.png
     - Not toggled:       https://screenshot.googleplex.com/C5Z7D8zcinhdtzp.png
     - Icon tooltip:      https://screenshot.googleplex.com/6HtxegErT7qukHb.png
     - Label tooltip:     https://screenshot.googleplex.com/ArohB8ccuAtvEpN.png
    
    Locked
     - Icon/Label:        https://screenshot.googleplex.com/4xPbaqNT3GRbs42.png
     - Icon tooltip:      https://screenshot.googleplex.com/iVrWveNmqpu47hN.png
     - Sub-Label tooltip: https://screenshot.googleplex.com/9xvcqCUkNw2iJva.png
    
    Bug: 1169479
    Test: Unit tests and verified on device.
    
    Change-Id: Ic26d71c50a7ec093138120f2490937da74d1d0d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227597
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938023}

--- ash/system/network/network_feature_pod_controller_legacy.cc
+++ ash/system/network/network_feature_pod_controller_legacy.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/network/network_feature_pod_controller_legacy.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
@@ -49,7 +50,9 @@ bool SetNetworkEnabled(bool enabled) {
 
 NetworkFeaturePodControllerLegacy::NetworkFeaturePodControllerLegacy(
     UnifiedSystemTrayController* tray_controller)
-    : tray_controller_(tray_controller) {}
+    : tray_controller_(tray_controller) {
+  DCHECK(!ash::features::IsQuickSettingsNetworkRevampEnabled());
+}
 
 NetworkFeaturePodControllerLegacy::~NetworkFeaturePodControllerLegacy() =
     default;
