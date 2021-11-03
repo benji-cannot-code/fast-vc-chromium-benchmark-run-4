4d8ae9f7e3070cff4378a11fa321b430850b5594
Allen Bauer <kylixrd@chromium.org>
2021-11-03 19:03:01 +0000

    Add support for child-index in View Builder.
    Bug: 1130078
    Change-Id: Ia418c8af3975fed16d60e0849a4b5a1242c85388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3255781
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937986}

--- ui/views/metadata/view_factory_internal.h
+++ ui/views/metadata/view_factory_internal.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/cxx17_backports.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/base/class_property.h"
 #include "ui/base/metadata/base_type_conversion.h"
 #include "ui/views/views_export.h"
@@ -155,7 +156,10 @@ class VIEWS_EXPORT ViewBuilderCore {
   virtual std::unique_ptr<ViewBuilderCore> Release() WARN_UNUSED_RESULT = 0;
 
  protected:
-  using ChildList = std::vector<std::unique_ptr<ViewBuilderCore>>;
+  // Vector of child view builders. If the optional index is included it will be
+  // passed to View::AddChildViewAt().
+  using ChildList = std::vector<
+      std::pair<std::unique_ptr<ViewBuilderCore>, absl::optional<size_t>>>;
   using PropertyList = std::vector<std::unique_ptr<PropertySetterBase>>;
 
   void AddPropertySetter(std::unique_ptr<PropertySetterBase> setter);
