1b4d9ddaadbfdc6b9d7ad91fc2f6640eba53a1e3
Adrian Taylor <adetaylor@chromium.org>
2021-11-03 15:09:25 +0000

    [rust] Support cxx bindings
    The second in a series of two commits related to inclusion of
    the 'cxx' Rust/C++ interop layer. See the previous commit for
    general discussion.
    
    This CL:
    * Adds a rust_cxx.gni template which can be used to explicitly
      build the C++ side of some cxx bindings
    * Adds instructions to rust_source_set, mixed_source_set and
      similar which will trigger those rules as necessary.
    * Simplifies the C++/Rust interop within our various toy test
      binaries to use cxx instead of hand-crafted bindings.
    
    For these simple examples, the benefits of cxx are not
    immediately visible, but as soon as you deal with passing
    objects between Rust and C++, or passing references in any way,
    vast amounts of fiddly (and therefore risky) boilerplate code
    falls away.
    
    Change-Id: Ic9eef67064170833e7b8f7052c2bace2f6d7b668
    Bug: 1069271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162138
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Auto-Submit: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937887}

--- build/rust/tests/test_mixed_source_set/test_mixed_source_set.h
+++ build/rust/tests/test_mixed_source_set/test_mixed_source_set.h
@@ -6,6 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BUILD_RUST_TESTS_TEST_MIXED_SOURCE_SET_TEST_MIXED_SOURCE_SET_H_
 #define BUILD_RUST_TESTS_TEST_MIXED_SOURCE_SET_TEST_MIXED_SOURCE_SET_H_
 
+#include <stdint.h>
+#include "build/rust/tests/test_mixed_source_set/src/lib.rs.h"
+
+uint32_t cpp_addition(uint32_t a, uint32_t b);
+void cpp_callback();
+
 void say_hello_via_callbacks();
 
 #endif  // BUILD_RUST_TESTS_TEST_MIXED_SOURCE_SET_TEST_MIXED_SOURCE_SET_H_
