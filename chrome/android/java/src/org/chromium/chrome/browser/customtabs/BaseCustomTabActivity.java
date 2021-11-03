ea0295b880527b768ffdf9407b71443a28f46397
Peter E Conn <peconn@chromium.org>
2021-11-03 17:28:26 +0000

    Let UsageStatsService care about whether it's enabled.
    UsageStatsService.createPageViewObserver is used in two places, and in
    both cases the caller checks for UsageStatsService.isEnabled().
    
    This change adds a new method that performs the enabled check itself
    and makes the old method private so it's less likely that a caller
    will forget to check themselves.
    
    Change-Id: I12f01c6eb2934ef4ccbd8cab61b9ed9954c94d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259043
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937949}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
@@ -311,8 +311,8 @@ public abstract class BaseCustomTabActivity extends ChromeActivity<BaseCustomTab
 
     @Override
     public void finishNativeInitialization() {
-        if (isTaskRoot() && UsageStatsService.isEnabled()) {
-            UsageStatsService.getInstance().createPageViewObserver(
+        if (isTaskRoot()) {
+            UsageStatsService.createPageViewObserverIfEnabled(
                     this, getActivityTabProvider(), getTabContentManagerSupplier());
         }
         if (!getIntentDataProvider().isWebappOrWebApkActivity()) {
