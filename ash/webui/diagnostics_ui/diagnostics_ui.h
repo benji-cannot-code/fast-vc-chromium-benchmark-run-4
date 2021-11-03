1891029dbcf157b549f2223a4d3a01f527de83b8
Ashley Prasad <ashleydp@google.com>
2021-11-03 19:52:31 +0000

    Diagnostics: Enable internal navigation metric message handler
    - Adds diagnostics_metric_message_handler to diagnostics_ui.
    - Tests expected metrics triggered when 'recordNavigation' triggered
      from JavaScript.
    
    Bug: 1197335
    Test: testing/xvfb.py out/Default/browser_tests --gtest_filter=*DiagnosticsAppIntegrationTest*
    Change-Id: I994b9c7025cff08d6a84b847ddfe15687f06cb48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253449
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938004}

--- ash/webui/diagnostics_ui/diagnostics_ui.h
+++ ash/webui/diagnostics_ui/diagnostics_ui.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/common/backend/plural_string_handler.h"
 #include "ash/webui/diagnostics_ui/backend/session_log_handler.h"
 #include "ash/webui/diagnostics_ui/diagnostics_metrics.h"
+#include "ash/webui/diagnostics_ui/diagnostics_metrics_message_handler.h"
 #include "ash/webui/diagnostics_ui/mojom/input_data_provider.mojom-forward.h"
 #include "ash/webui/diagnostics_ui/mojom/network_health_provider.mojom-forward.h"
 #include "ash/webui/diagnostics_ui/mojom/system_data_provider.mojom-forward.h"
@@ -70,6 +71,6 @@ class DiagnosticsDialogUI : public ui::MojoWebDialogUI {
       diagnostics_metrics_;
 };
 
-}  // namespace chromeos
+}  // namespace ash
 
 #endif  // ASH_WEBUI_DIAGNOSTICS_UI_DIAGNOSTICS_UI_H_
