1f15c3ba1d326072e22832f284fa11dc7cdb8f73
Michael Checo <michaelcheco@google.com>
2021-11-03 00:32:21 +0000

    Firmware UI: Add device preparation dialog
    - A follow-up CL will add the update dialog and implement a mocked
      version of 'startUpdate'
    - Styles added are temporary and will be updated when the spec
      is released
    
    Screenshot:
     - http://shortn/_cqybxkyUvF
    
    Bug: 1252981
    Change-Id: I82a061db5b4c7d12f44d208f77f63a48bdf0d2ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248640
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937642}

--- ash/webui/firmware_update_ui/resources/update_card.js
+++ ash/webui/firmware_update_ui/resources/update_card.js
@@ -4,11 +4,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
 import './firmware_shared_css.js';
 import './firmware_shared_fonts.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {FirmwareUpdate, UpdatePriority} from './firmware_update_types.js';
+
+/** @enum {number} */
+export const DialogState = {
+  CLOSED: 0,
+  DEVICE_PREP: 1,
+};
 
 /**
  * @fileoverview
@@ -29,6 +36,12 @@ export class UpdateCardElement extends PolymerElement {
       update: {
         type: Object,
       },
+
+      /** @protected {!DialogState} */
+      dialogState_: {
+        type: Number,
+        value: DialogState.CLOSED,
+      },
     };
   }
 
@@ -38,6 +51,33 @@ export class UpdateCardElement extends PolymerElement {
    */
   isCriticalUpdate_() {
     return this.update.priority === UpdatePriority.kCritical;
+  }
+
+  /** @protected */
+  onUpdateButtonClicked_() {
+    if (this.update.updateModeInstructions) {
+      this.dialogState_ = DialogState.DEVICE_PREP;
+    }
+    // TODO(michaelcheco): Show update dialog immediately if no instructions
+    // are provided.
+  }
+
+  /** @protected */
+  closeDialog_() {
+    this.dialogState_ = DialogState.CLOSED;
+  }
+
+  /** @protected */
+  startUpdate_() {
+    // TODO(michaelcheco): Add implementation.
+  }
+
+  /**
+   * @protected
+   * @return {boolean}
+   */
+  shouldShowDevicePrepDialog_() {
+    return this.dialogState_ === DialogState.DEVICE_PREP;
   }
 }
 
