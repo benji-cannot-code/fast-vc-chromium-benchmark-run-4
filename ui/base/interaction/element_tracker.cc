40ff4519eb8c3d99f4b67e8a1fbe2227987d6484
Dana Fried <dfried@chromium.org>
2021-11-03 20:02:25 +0000

    Move temporary element identifier to ElementTracker, make more generic.
    This is a necessary prerequisite for changes we need for IPH.
    
    We've determined that there are cases where we want to assign a
    temporary/default ID to an element that aren't associated with named
    interaction sequence elements. For that reason, the temporary ID is
    moved down to the ElementTracker class, and ElementTrackerViews class
    now has a version of GetElementForView() which can optionally assign a
    default identifier (default is not to assign one and return null).
    
    This also correctly implements the const version of GetElementForView()
    which was previously not implemented (though it was unused), and a test
    for the new functionality is added.
    
    Change-Id: I02f249453509aefa634cd734aff64c41b325f872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257897
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938008}

--- ui/base/interaction/element_tracker.cc
+++ ui/base/interaction/element_tracker.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(ElementTracker, kTemporaryIdentifier);
+
 class ElementTracker::ElementData {
  public:
   ElementData(ElementTracker* tracker,
