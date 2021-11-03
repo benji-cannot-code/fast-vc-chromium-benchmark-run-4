9164a1ee261e0c1e00a782db49fb1a2f3de49178
Austin Tankiang <austinct@chromium.org>
2021-11-03 05:42:34 +0000

    Hook up cancelIOTask to feedback panel
    Bug: b:204835080
    Change-Id: I9da5295da1fa186a37294d875b826b458b15928b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3258730
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937720}

--- ui/file_manager/file_manager/background/js/file_operation_handler.js
+++ ui/file_manager/file_manager/background/js/file_operation_handler.js
@@ -71,6 +71,9 @@ export class FileOperationHandler {
       item.id = taskId;
       item.type = getTypeFromIOTaskType_(event.type);
       item.itemCount = event.itemCount;
+      item.cancelCallback = () => {
+        chrome.fileManagerPrivate.cancelIOTask(event.taskId);
+      };
     }
     item.message = getMessageFromProgressEvent_(event);
 
@@ -80,11 +83,6 @@ export class FileOperationHandler {
         item.destinationMessage = event.destinationName;
         item.progressMax = event.totalBytes;
         item.progressValue = event.bytesTransferred;
-        item.cancelCallback = () => {
-          const taskId = String(event.taskId);
-          console.log(`TODO: Cancel IOTask, taskId: ${taskId}`);
-          // chrome.fileManagerPrivate.cancelIOTask(event.taskId);
-        };
         item.remainingTime = event.remainingSeconds;
         break;
 
