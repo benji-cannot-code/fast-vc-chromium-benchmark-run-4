51ac7d67a3df3f065b4720e46807aced3e3ef061
Timothy Loh <timloh@chromium.org>
2021-11-03 10:15:15 +0000

    Revert "Reporting: Fix healthd callback not being called"
    This reverts commit 2e7607b2ebe8a0c69fd3d77f2b81bac4df6552f3.
    
    Reason for revert: Causes Chrome on my device to crash constantly
    
    I verified reverting this stop the crashes. Stack trace: gpaste/5578485629190144
    
    Original change's description:
    > Reporting: Fix healthd callback not being called
    >
    > Due to a recent change the callback from fetching data from the health
    > daemon is not being called. This change ensures it is.
    >
    > Bug: b:204843574
    > Change-Id: Id7307caee2cac56145f1c31286eb860230890217
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256870
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Commit-Queue: Tyler Garrett <tylergarrett@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#937380}
    
    Bug: b:204843574
    Change-Id: I254896c3d46586fd6ab852bafc3c067f3fe59d1c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257949
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937797}

--- chrome/browser/ash/policy/status_collector/device_status_collector.cc
+++ chrome/browser/ash/policy/status_collector/device_status_collector.cc
@@ -2032,8 +2032,6 @@ void DeviceStatusCollector::SampleDischargeRate(
       it->second.set_charge_rate(percent);
     }
   }
-
-  AddDataSample(std::move(sample), std::move(callback));
 }
 
 void DeviceStatusCollector::ReceiveCPUTemperature(
