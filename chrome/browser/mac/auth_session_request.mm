9364302be6936054b7ceb0fa0d7e1f3348e56cc1
Avi Drissman <avi@chromium.org>
2021-11-03 09:46:03 +0000

    Don't use an ephemeral session if prohibited by policy
    If policy doesn't allow for ephemeral sessions, don't use them for
    ASWebAuthenticationSession. The request for one is advisory, and
    returning an error instead would mean that the user literally would be
    unable to log in without changing their default browser.
    
    Fixed: 1263682
    Change-Id: I2a8dedadfa1df4de4e7ec0189542edae3456dd7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257289
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937788}

--- chrome/browser/mac/auth_session_request.mm
+++ chrome/browser/mac/auth_session_request.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "base/strings/sys_string_conversions.h"
+#include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
@@ -208,8 +209,22 @@ Browser* AuthSessionRequest::CreateBrowser(
   if (!profile)
     return nullptr;
 
-  if (request.shouldUseEphemeralSession)
+  bool ephemeral_sessions_allowed_by_policy =
+      IncognitoModePrefs::GetAvailability(profile->GetPrefs()) !=
+      IncognitoModePrefs::Availability::kDisabled;
+
+  // As per the documentation for `shouldUseEphemeralSession`: "Whether the
+  // request is honored depends on the user’s default web browser." If policy
+  // does not allow for the use of an ephemeral session, the options would be
+  // either to use a non-ephemeral session, or to error out. However, erroring
+  // out would leave any app that uses `ASWebAuthenticationSession` unable to do
+  // any sign-in at all via this API. Given that the docs do not actually
+  // provide a guarantee of an ephemeral session if requested, take advantage of
+  // that to not block the user's ability to sign in.
+  if (request.shouldUseEphemeralSession &&
+      ephemeral_sessions_allowed_by_policy) {
     profile = profile->GetPrimaryOTRProfile(/*create_if_needed=*/true);
+  }
   if (!profile)
     return nullptr;
 
