a01b450d20acc630a2d1effe126489e86713d557
Viktor Semeniuk <vsemeniuk@google.com>
2021-11-03 09:57:29 +0000

    Clean up after fixing crbug.com/1223022
    Even though crbug.com/1223022 was fixed there is left code, which should
    be removed.
    
    Bug: 1223022
    Change-Id: If05776b4a692494d72a3a635d30cfc06f25b84ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3250700
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937792}

--- ios/chrome/browser/ui/settings/password/passwords_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/password/passwords_table_view_controller_unittest.mm
@@ -188,12 +188,6 @@ class PasswordsTableViewControllerTest : public ChromeTableViewControllerTest {
           {InsecureType::kLeaked,
            password_manager::InsecurityMetadata(
                base::Time::Now(), password_manager::IsMuted(false))}};
-    } else {
-      // TODO(crbug.com/1223022): Once all places that operate changes on forms
-      // via UpdateLogin properly set |password_issues|, setting them to an
-      // empty map should be part of the default constructor.
-      form->password_issues =
-          base::flat_map<InsecureType, password_manager::InsecurityMetadata>();
     }
     AddPasswordForm(std::move(form));
   }
@@ -211,11 +205,6 @@ class PasswordsTableViewControllerTest : public ChromeTableViewControllerTest {
     form->signon_realm = "http://www.example2.com/";
     form->scheme = password_manager::PasswordForm::Scheme::kHtml;
     form->blocked_by_user = false;
-    // TODO(crbug.com/1223022): Once all places that operate changes on forms
-    // via UpdateLogin properly set |password_issues|, setting them to an empty
-    // map should be part of the default constructor.
-    form->password_issues =
-        base::flat_map<InsecureType, password_manager::InsecurityMetadata>();
     AddPasswordForm(std::move(form));
   }
 
@@ -233,11 +222,6 @@ class PasswordsTableViewControllerTest : public ChromeTableViewControllerTest {
     form->signon_realm = "http://www.secret.com/";
     form->scheme = password_manager::PasswordForm::Scheme::kHtml;
     form->blocked_by_user = true;
-    // TODO(crbug.com/1223022): Once all places that operate changes on forms
-    // via UpdateLogin properly set |password_issues|, setting them to an empty
-    // map should be part of the default constructor.
-    form->password_issues =
-        base::flat_map<InsecureType, password_manager::InsecurityMetadata>();
     AddPasswordForm(std::move(form));
   }
 
@@ -255,11 +239,6 @@ class PasswordsTableViewControllerTest : public ChromeTableViewControllerTest {
     form->signon_realm = "http://www.secret2.com/";
     form->scheme = password_manager::PasswordForm::Scheme::kHtml;
     form->blocked_by_user = true;
-    // TODO(crbug.com/1223022): Once all places that operate changes on forms
-    // via UpdateLogin properly set |password_issues|, setting them to an empty
-    // map should be part of the default constructor.
-    form->password_issues =
-        base::flat_map<InsecureType, password_manager::InsecurityMetadata>();
     AddPasswordForm(std::move(form));
   }
 
