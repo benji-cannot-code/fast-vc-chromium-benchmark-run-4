3653d97f119c2eb1c5a4357cae00d47c0955a550
Jinsuk Kim <jinsukkim@chromium.org>
2021-11-03 10:52:34 +0000

    gesture-nav: Reset UI when entering tab switcher
    Gesture navigation UI should be reset when tab switcher is entered.
    This was taken care of in rendered pages but not in native pages.
    This CL add an observer of the layout state and handles it accordingly.
    
    Bug: 1263920
    Change-Id: I315d4cce05fe4539da4ac5d71a6961627e11272b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3249650
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#937808}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -174,7 +174,7 @@ public class RootUiCoordinator
     private OverlayPanelManager.OverlayPanelManagerObserver mOverlayPanelManagerObserver;
 
     private OneshotSupplier<LayoutStateProvider> mLayoutStateProviderOneShotSupplier;
-    private LayoutStateProvider mLayoutStateProvider;
+    protected LayoutStateProvider mLayoutStateProvider;
     private LayoutStateProvider.LayoutStateObserver mLayoutStateObserver;
 
     /** A means of providing the theme color to different features. */
@@ -950,7 +950,7 @@ public class RootUiCoordinator
                         R.color.omnibox_focused_fading_background_color));
     }
 
-    private void setLayoutStateProvider(LayoutStateProvider layoutStateProvider) {
+    protected void setLayoutStateProvider(LayoutStateProvider layoutStateProvider) {
         assert layoutStateProvider != null;
         assert mLayoutStateProvider == null : "The LayoutStateProvider should set at most once.";
 
