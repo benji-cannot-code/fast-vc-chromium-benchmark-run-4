b8b06c202c7cb0bcab182223415ceb4dbd48c9fa
Fabio Tirelo <ftirelo@chromium.org>
2021-11-04 17:18:59 +0000

    [Clank CBCM] Use Android ID as CBCM ID on all Android versions
    This is a summary of the change:
    
     - Changes `CloudManagementAndroidConnection.getClientId` to return the
       raw Android ID instead of the obfuscated version
    
     - That's the function that generates the CBCM ID that will be sent in
       the `deviceid` request param and that seeds the DMToken
    
     - Remove the Android version check from
       `CloudManagementAndroidConnection.getGservicesAndroidId`
    
    Code cleanup will be sent in a follow-up CL.
    
    Bug: 1264463
    Change-Id: If961d2cf7024a146dca5a556ab59c4dc8ceeb23a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251004
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938322}

--- chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/CloudManagementAndroidConnection.java
+++ chrome/browser/policy/android/java/src/org/chromium/chrome/browser/policy/CloudManagementAndroidConnection.java
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.policy;
 
-import android.os.Build;
-
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.annotations.CalledByNative;
+
+import java.util.UUID;
 
 /**
  * Allows access to cloud management functionalities implemented downstream.
@@ -43,8 +43,13 @@ public class CloudManagementAndroidConnection {
             return clientId;
         }
 
-        // Generate a new ID and save it in Shared Preferences, so it can be reused.
-        String newClientId = getDelegate().generateClientId();
+        // Use the Gservices Android ID as the client ID. If the Android ID can't be obtained, then
+        // a randomly generated ID is used instead (e.g. for non-Chrome official builds).
+        // Save the ID in Shared Preferences, so it can be reused.
+        String newClientId = getDelegate().getGservicesAndroidId();
+        if (newClientId == null || newClientId.isEmpty()) {
+            newClientId = UUID.randomUUID().toString();
+        }
         CloudManagementSharedPreferences.saveClientId(newClientId);
 
         return newClientId;
@@ -59,9 +64,7 @@ public class CloudManagementAndroidConnection {
      */
     @CalledByNative
     public String getGservicesAndroidId() {
-        return Build.VERSION.SDK_INT <= Build.VERSION_CODES.S
-                ? getDelegate().getGservicesAndroidId()
-                : "";
+        return getDelegate().getGservicesAndroidId();
     }
 
     /** Overrides {@link mDelegate} if not null. */
