b8fe0af7628b25597283e059902b44cf2ad522fb
Christoph Schwering <schwering@google.com>
2021-11-04 09:57:47 +0000

    [Passwords] Call ChromeClient::PasswordFieldReset() only for empty fields.
    The only handler of ChromeClient::PasswordFieldReset() is
    PasswordAutofillAgent::InformAboutFieldClearing(), which DCHECKs
    that the given field's value is empty. This CL fixes a violation of this
    DCHECK.
    
    HTMLInputElement::setValue(value) triggers PasswordFieldReset() only if
    |value| is empty. However, there are situations where
      value.IsEmpty() && !value().IsEmpty()
    so that PasswordFieldReset() is called but !value().IsEmpty().
    
    For example, all conditions for PasswordFieldReset() are met if an
    <input type="password">
    1. changes its type to "range",
    2. changes its value to a non-default integer,
    3. changes its value to "".
    In Step 3, HTMLInputElement::setValue()'s |sanitized_value| and thus
    also value() are "50", not "". So the DCHECK in
    InformAboutFieldClearing() fails.
    
    To fix this issue, this CL checks if |sanitized_value| rather than
    |value| is empty.
    
    Bug: 1265130
    Change-Id: I037d0b04e1b17e948b401f5a3d64114196f3766e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3254466
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#938203}

--- third_party/blink/renderer/core/html/forms/html_input_element.cc
+++ third_party/blink/renderer/core/html/forms/html_input_element.cc
@@ -1226,7 +1226,8 @@ void HTMLInputElement::setValue(const String& value,
 
   if (value_changed) {
     NotifyFormStateChanged();
-    if (value.IsEmpty() && HasBeenPasswordField() && GetDocument().GetPage()) {
+    if (sanitized_value.IsEmpty() && HasBeenPasswordField() &&
+        GetDocument().GetPage()) {
       GetDocument().GetPage()->GetChromeClient().PasswordFieldReset(*this);
     }
   }
