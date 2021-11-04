d32a6268c0085881dad972b1f7d830680de18622
Yutaka Hirano <yhirano@chromium.org>
2021-11-04 05:25:10 +0000

    Remove SelfOwnedReceiver<CrossOriginEmbedderPolicyReporter>
    Instead use std::unique_ptr<content::CrossOriginEmbedderPolicyReporter>
    so that it doesn't outlive the associated StoragePartition.
    
    Change-Id: I20f99bbbde22a7526467703df2e52deb4b4577fc
    Bug: 1262516, 1262391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257942
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938152}

--- content/browser/service_worker/embedded_worker_instance.h
+++ content/browser/service_worker/embedded_worker_instance.h
@@ -47,7 +47,6 @@ class RenderProcessHost;
 class ServiceWorkerContentSettingsProxyImpl;
 class ServiceWorkerContextCore;
 class ServiceWorkerVersion;
-class CrossOriginEmbedderPolicyReporter;
 
 namespace service_worker_new_script_loader_unittest {
 class ServiceWorkerNewScriptLoaderTest;
@@ -390,8 +389,7 @@ class CONTENT_EXPORT EmbeddedWorkerInstance
   // COEP Reporter connected to the URLLoaderFactories that handles subresource
   // requests initiated from the service worker. The impl lives on the UI
   // thread, and |coep_reporter_| has the ownership of the impl instance.
-  mojo::Remote<network::mojom::CrossOriginEmbedderPolicyReporter>
-      coep_reporter_;
+  std::unique_ptr<CrossOriginEmbedderPolicyReporter> coep_reporter_;
 
   // A unique identifier for this service worker instance. This is unique across
   // the browser process, but not persistent across service worker restarts.
