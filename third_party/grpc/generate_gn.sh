98f3e81e59c8f721f54deee8bba8ed04eaba2655
Ming-Chuan Lin <mingcl@google.com>
2021-11-04 04:13:46 +0000

    Update gRPC version to v1.41.1+, with various fixes
    The commit updates gRPC's version to gRPC GitHub master at 2021-10-27
    
    1. GRPC_CALLBACK_API_NONEXPERIMENTAL already becomes defaults, remove it
    to avoid redefinition.
    2. upb now introduces 3 cc files with the same basename at the same
    time, so an additional "repeated" target is created to workaround the GN
    object file name conflict issue.
    3. Some source files is manually commented out before, now we comment
    them out in Mako template instead.
    4. A helper script is added for generating BUILD.gn
    
    Rationale of this change: We need some new gRPC features
    
    Bug: None
    Test: CQ; run some internal repo's unit tests that uses gRPC
    Change-Id: I1122cd09c11756f9b90064776a13bac6ccd1b32c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3246789
    Reviewed-by: Albert Chaulk <achaulk@chromium.org>
    Commit-Queue: Ming-Chuan Lin <mingcl@google.com>
    Auto-Submit: Ming-Chuan Lin <mingcl@google.com>
    Cr-Commit-Position: refs/heads/main@{#938141}

---
+++ third_party/grpc/generate_gn.sh
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+
+# This script generates BUILD.gn using template/BUILD.chromium.gn.template and
+# the gRPC repo in src/
+
+set -e
+
+# change directory to this script's directory
+cd `dirname "$0"`
+
+# copy template into grpc repo and run generate_projects in it
+cp template/BUILD.chromium.gn.template src/templates/BUILD.chromium.gn.template
+cd src
+./tools/buildgen/generate_projects.sh
+rm templates/BUILD.chromium.gn.template # clean up
+cd ..
+
+# move the generated GN file back to this directory
+mv src/BUILD.chromium.gn BUILD.gn
+
+gn format --inplace BUILD.gn
+
