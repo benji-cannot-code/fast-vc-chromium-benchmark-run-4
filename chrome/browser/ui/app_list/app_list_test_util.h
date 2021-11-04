02dcc4331c4da91dfecab76cff99899911f38a45
Andrew Xu <andrewxu@chromium.org>
2021-11-04 19:12:42 +0000

    [Launcher Sort] Temporary sort - part 2
    With this CL, when the app list under temporary sort receives the sync
    update coming from item move on a remote device, the temporary sort
    gets reverted.
    
    Bug: 1260447
    Change-Id: Ia042f7bf8f5b52f145f265dc9399bd31316d3bfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253654
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938397}

--- chrome/browser/ui/app_list/app_list_test_util.h
+++ chrome/browser/ui/app_list/app_list_test_util.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_APP_LIST_APP_LIST_TEST_UTIL_H_
 
 #include "chrome/browser/extensions/extension_service_test_base.h"
+#include "components/sync/model/sync_data.h"
+#include "components/sync/protocol/app_list_specifics.pb.h"
 
 namespace web_app {
 class TestWebAppUrlLoader;
@@ -32,6 +34,13 @@ class AppListTestBase : public extensions::ExtensionServiceTestBase {
   web_app::TestWebAppUrlLoader* url_loader_ = nullptr;
 };
 
+// Test util constants --------------------------------------------------------
+
+extern const char kUnset[];
+extern const char kDefault[];
+extern const char kOemAppName[];
+extern const char kSomeAppName[];
+
 // Test util functions ---------------------------------------------------------
 
 scoped_refptr<extensions::Extension> MakeApp(
@@ -42,5 +51,14 @@ scoped_refptr<extensions::Extension> MakeApp(
 // Creates next by natural sort ordering application id. Application id has to
 // have 32 chars each in range 'a' to 'p' inclusively.
 std::string CreateNextAppId(const std::string& app_id);
+
+syncer::SyncData CreateAppRemoteData(
+    const std::string& id,
+    const std::string& name,
+    const std::string& parent_id,
+    const std::string& item_ordinal,
+    const std::string& item_pin_ordinal,
+    sync_pb::AppListSpecifics_AppListItemType item_type =
+        sync_pb::AppListSpecifics_AppListItemType_TYPE_APP);
 
 #endif  // CHROME_BROWSER_UI_APP_LIST_APP_LIST_TEST_UTIL_H_
