82027f1c96e4ab938e2900c901740d03cc5a3952
Joshua Pawlicki <waffles@chromium.org>
2021-11-04 03:13:12 +0000

    Updater: mechanical refactor of update_client APIs.
    This is necessary to all chrome/updater to populate more members to
    transmit to the server when it calls these APIs.
    
    This CL is just a refactor and does not actually change what is sent.
    
    Bug: 1259972
    Change-Id: Iebf4528a0a4a273799b0ec1ed06a3c04ac68c060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251461
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938125}

--- components/update_client/task_send_registration_ping.h
+++ components/update_client/task_send_registration_ping.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/update_client/task.h"
 #include "components/update_client/update_client.h"
 
-namespace base {
-class Version;
-}
-
 namespace update_client {
 
 class UpdateEngine;
@@ -37,9 +33,7 @@ class TaskSendRegistrationPing : public Task {
   // |id| represents the CRX to send the ping for.
   // |callback| is posted when the task is done.
   TaskSendRegistrationPing(scoped_refptr<UpdateEngine> update_engine,
-                           const std::string& id,
-                           const base::Version& version,
-                           bool requires_network_encryption,
+                           const CrxComponent& crx_component,
                            Callback callback);
 
   void Run() override;
@@ -60,9 +54,7 @@ class TaskSendRegistrationPing : public Task {
 
   SEQUENCE_CHECKER(sequence_checker_);
   scoped_refptr<UpdateEngine> update_engine_;
-  const std::string id_;
-  const base::Version version_;
-  const bool requires_network_encryption_;
+  const CrxComponent crx_component_;
   Callback callback_;
 };
 
--- components/update_client/task_send_uninstall_ping.h
+++ components/update_client/task_send_uninstall_ping.h
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/update_client/task.h"
 #include "components/update_client/update_client.h"
 
-namespace base {
-class Version;
-}
-
 namespace update_client {
 
 class UpdateEngine;
@@ -31,15 +27,14 @@ class TaskSendUninstallPing : public Task {
   using Callback =
       base::OnceCallback<void(scoped_refptr<Task> task, Error error)>;
 
-  // |update_engine| is injected here to handle the task.
-  // |id| represents the CRX to send the ping for.
-  // |callback| is called to return the execution flow back to creator of
+  // `update_engine` is injected here to handle the task.
+  // `crx_component` represents the CRX to send the ping for.
+  // `reason` is the reason for the uninstall ping
+  // `callback` is called to return the execution flow back to creator of
   //    this task when the task is done.
   TaskSendUninstallPing(scoped_refptr<UpdateEngine> update_engine,
-                        const std::string& id,
-                        const base::Version& version,
+                        const CrxComponent& crx_component,
                         int reason,
-                        bool requires_network_encryption,
                         Callback callback);
 
   TaskSendUninstallPing(const TaskSendUninstallPing&) = delete;
@@ -60,10 +55,8 @@ class TaskSendUninstallPing : public Task {
 
   base::ThreadChecker thread_checker_;
   scoped_refptr<UpdateEngine> update_engine_;
-  const std::string id_;
-  const base::Version version_;
+  const CrxComponent crx_component_;
   const int reason_;
-  const bool requires_network_encryption_;
   Callback callback_;
 };
 
