ffa4152b4f4d6db9b9fd8bf2208e57ce0d9845d8
Hui Yingst <nigi@chromium.org>
2021-11-04 22:30:57 +0000

    Remove an extra comment from pdf_viewer.js.
    Remove an extra comment for a already deleted getter function.
    
    Change-Id: I1cfa7fa1f539e1db48bf8b12ef00aee1ddb7ca32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262539
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938551}

--- chrome/browser/resources/pdf/pdf_viewer.js
+++ chrome/browser/resources/pdf/pdf_viewer.js
@@ -654,7 +654,6 @@ export class PDFViewerElement extends PDFViewerBaseElement {
     }
   }
 
-  /** @return {!Viewport} The viewport. Used for testing. */
   /** @return {!Array<!Bookmark>} The bookmarks. Used for testing. */
   get bookmarks() {
     return this.bookmarks_;
