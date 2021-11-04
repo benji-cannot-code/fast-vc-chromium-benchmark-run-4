12cb1f5e0586493ddd2550b14c45804271e4cf96
Maksim Sisov <msisov@igalia.com>
2021-11-04 22:58:08 +0000

    ozone: //ui/compositor: clean up from USE_X11
    This is another CL that removes USE_X11 define and
    gn arg usage from //ui/compositor and also removes
    IsUsingOzonePlatform condition as it's no longer
    needed as it always resolves to true.
    
    Bug: 1096425
    Change-Id: I801ac1c361df9dba0a2a60e4728bd0d7974c0bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259634
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938563}

--- ui/compositor/test/test_suite.cc
+++ ui/compositor/test/test_suite.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/test/gl_surface_test_support.h"
 
 #if defined(USE_OZONE)
-#include "ui/base/ui_base_features.h"
 #include "ui/ozone/public/ozone_platform.h"
 #endif
 
@@ -44,11 +43,9 @@ void CompositorTestSuite::Initialize() {
   gl::GLSurfaceTestSupport::InitializeOneOff();
 
 #if defined(USE_OZONE)
-  if (features::IsUsingOzonePlatform()) {
-    OzonePlatform::InitParams params;
-    params.single_process = true;
-    OzonePlatform::InitializeForUI(params);
-  }
+  OzonePlatform::InitParams params;
+  params.single_process = true;
+  OzonePlatform::InitializeForUI(params);
 #endif
 
 #if defined(OS_WIN)
