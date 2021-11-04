386d7300db80b3ea33bd1a5dcc9ceb4ecbb20612
Lei Zhang <thestig@chromium.org>
2021-11-04 17:35:17 +0000

    Make FocusElementType naming in PDFiumEngine consistent.
    Currently, various code that stores / interacts with FocusElementType
    refers to the type as the item type. Change "item" to "element" to make
    the naming more consistent.
    
    Change-Id: I4a7c323d96b2797744303e41c054a7ebdae3e516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260913
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938338}

--- pdf/pdfium/pdfium_engine_unittest.cc
+++ pdf/pdfium/pdfium_engine_unittest.cc
@@ -690,7 +690,7 @@ class PDFiumEngineTabbingTest : public PDFiumTestBase {
   }
 
   PDFiumEngine::FocusElementType GetFocusedElementType(PDFiumEngine* engine) {
-    return engine->focus_item_type_;
+    return engine->focus_element_type_;
   }
 
   int GetLastFocusedPage(PDFiumEngine* engine) {
@@ -699,7 +699,7 @@ class PDFiumEngineTabbingTest : public PDFiumTestBase {
 
   PDFiumEngine::FocusElementType GetLastFocusedElementType(
       PDFiumEngine* engine) {
-    return engine->last_focused_item_type_;
+    return engine->last_focused_element_type_;
   }
 
   int GetLastFocusedAnnotationIndex(PDFiumEngine* engine) {
@@ -982,7 +982,7 @@ TEST_F(PDFiumEngineTabbingTest, TabbingWithModifiers) {
       HandleTabEvent(engine.get(), blink::WebInputEvent::Modifiers::kAltKey));
 }
 
-TEST_F(PDFiumEngineTabbingTest, NoFocusableItemTabbingTest) {
+TEST_F(PDFiumEngineTabbingTest, NoFocusableElementTabbingTest) {
   /*
    * Document structure
    * Document
