f38478e00f816d9d5966e68ff3690a43221e4710
David Black <dmblack@google.com>
2021-11-04 21:01:41 +0000

    personalization: Don't show tabs or albums content if albums is empty.
    Per change in product requirements, if the list of Google Photos albums
    is empty the tab strip should be hidden.
    
    Bug: b:205024475
    Change-Id: I94f885227c9b53c5fa3d34e1c01bad84a56f84ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260625
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938494}

--- ash/webui/personalization_app/resources/trusted/google_photos_element.js
+++ ash/webui/personalization_app/resources/trusted/google_photos_element.js
@@ -305,6 +305,15 @@ export class GooglePhotos extends WithPersonalizationStore {
   }
 
   /**
+   * Whether the list of albums is empty.
+   * @return {boolean}
+   * @private
+   */
+  isAlbumsEmpty_() {
+    return !isNonEmptyArray(this.albums_);
+  }
+
+  /**
    * Whether the albums tab is currently selected.
    * @return {boolean}
    * @private
