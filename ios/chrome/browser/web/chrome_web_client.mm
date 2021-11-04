1efd47ac63a3f9a9b2698b60c000e474ff3e607c
Justin Cohen <justincohen@google.com>
2021-11-04 21:12:13 +0000

    ios: Move URL cleanup from WebSessionStateTabHelper to WebClient.
    CleanupNativeRestoreURLs corrects missing NTP and reading list
    virtualURLs and titles. Native session restoration may not properly
    restore these items.
    
    Bug: 1266497
    Change-Id: I7b729e056c12d0bea4b883a29603594a3b660c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259971
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938501}

--- ios/chrome/browser/web/chrome_web_client.mm
+++ ios/chrome/browser/web/chrome_web_client.mm
@@ -31,7 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/browser/ios_chrome_main_parts.h"
 #import "ios/chrome/browser/link_to_text/link_to_text_java_script_feature.h"
+#import "ios/chrome/browser/ntp/new_tab_page_tab_helper.h"
 #import "ios/chrome/browser/reading_list/offline_page_tab_helper.h"
+#include "ios/chrome/browser/reading_list/offline_url_utils.h"
 #import "ios/chrome/browser/safe_browsing/password_protection_java_script_feature.h"
 #import "ios/chrome/browser/safe_browsing/safe_browsing_blocking_page.h"
 #import "ios/chrome/browser/safe_browsing/safe_browsing_error.h"
@@ -66,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/common/features.h"
 #include "ios/web/common/user_agent.h"
 #include "ios/web/public/navigation/browser_url_rewriter.h"
+#import "ios/web/public/navigation/navigation_item.h"
 #include "ios/web/public/navigation/navigation_manager.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_util.h"
@@ -449,4 +452,22 @@ web::UserAgentType ChromeWebClient::GetDefaultUserAgent(
 bool ChromeWebClient::RestoreSessionFromCache(web::WebState* web_state) const {
   return WebSessionStateTabHelper::FromWebState(web_state)
       ->RestoreSessionFromCache();
+}
+
+void ChromeWebClient::CleanupNativeRestoreURLs(web::WebState* web_state) const {
+  web::NavigationManager* navigationManager = web_state->GetNavigationManager();
+  for (int i = 0; i < navigationManager->GetItemCount(); i++) {
+    // The WKWebView URL underneath the NTP is about://newtab, which has no
+    // title. When restoring the NTP, be sure to re-add the title below.
+    web::NavigationItem* item = navigationManager->GetItemAtIndex(i);
+    NewTabPageTabHelper::UpdateItem(item);
+
+    // The WKWebView URL underneath a forced-offline page is chrome://offline,
+    // which has an embedded entry URL. Apply that entryURL to the virtualURL
+    // here.
+    if (item->GetVirtualURL().host() == kChromeUIOfflineHost) {
+      item->SetVirtualURL(
+          reading_list::EntryURLForOfflineURL(item->GetVirtualURL()));
+    }
+  }
 }
