a7c8e39ff9386d36b79f592613ac4b979dfec46c
Philipp Hancke <philipp.hancke@googlemail.com>
2021-11-04 15:18:04 +0000

    webrtc-internals: use spec names for more events
    BUG=chromium:1266063
    
    Change-Id: I0a99628b207aa1a1792a8e4a042ee09e929c409f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257461
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#938269}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
@@ -997,7 +997,7 @@ void PeerConnectionTracker::TrackAddIceCandidate(
 
   const char* event =
       (source == SOURCE_LOCAL)
-          ? "onIceCandidate"
+          ? "icecandidate"
           : (succeeded ? "addIceCandidate" : "addIceCandidateFailed");
 
   SendPeerConnectionUpdate(id, event, value);
@@ -1104,9 +1104,7 @@ void PeerConnectionTracker::TrackCreateDataChannel(
   String value = "label: " + String::FromUTF8(data_channel->label()) +
                  ", reliable: " + SerializeBoolean(data_channel->reliable());
   SendPeerConnectionUpdate(
-      id,
-      source == SOURCE_LOCAL ? "createLocalDataChannel" : "onRemoteDataChannel",
-      value);
+      id, source == SOURCE_LOCAL ? "createDataChannel" : "datachannel", value);
 }
 
 void PeerConnectionTracker::TrackClose(RTCPeerConnectionHandler* pc_handler) {
@@ -1124,7 +1122,7 @@ void PeerConnectionTracker::TrackSignalingStateChange(
   int id = GetLocalIDForHandler(pc_handler);
   if (id == -1)
     return;
-  SendPeerConnectionUpdate(id, "signalingStateChange",
+  SendPeerConnectionUpdate(id, "signalingstatechange",
                            GetSignalingStateString(state));
 }
 
@@ -1135,7 +1133,7 @@ void PeerConnectionTracker::TrackLegacyIceConnectionStateChange(
   int id = GetLocalIDForHandler(pc_handler);
   if (id == -1)
     return;
-  SendPeerConnectionUpdate(id, "legacyIceConnectionStateChange",
+  SendPeerConnectionUpdate(id, "iceconnectionstatechange (legacy)",
                            GetIceConnectionStateString(state));
 }
 
@@ -1146,7 +1144,7 @@ void PeerConnectionTracker::TrackIceConnectionStateChange(
   int id = GetLocalIDForHandler(pc_handler);
   if (id == -1)
     return;
-  SendPeerConnectionUpdate(id, "iceConnectionStateChange",
+  SendPeerConnectionUpdate(id, "iceconnectionstatechange",
                            GetIceConnectionStateString(state));
 }
 
@@ -1157,7 +1155,7 @@ void PeerConnectionTracker::TrackConnectionStateChange(
   int id = GetLocalIDForHandler(pc_handler);
   if (id == -1)
     return;
-  SendPeerConnectionUpdate(id, "connectionStateChange",
+  SendPeerConnectionUpdate(id, "connectionstatechange",
                            GetConnectionStateString(state));
 }
 
@@ -1168,7 +1166,7 @@ void PeerConnectionTracker::TrackIceGatheringStateChange(
   int id = GetLocalIDForHandler(pc_handler);
   if (id == -1)
     return;
-  SendPeerConnectionUpdate(id, "iceGatheringStateChange",
+  SendPeerConnectionUpdate(id, "icegatheringstatechange",
                            GetIceGatheringStateString(state));
 }
 
@@ -1229,7 +1227,7 @@ void PeerConnectionTracker::TrackOnRenegotiationNeeded(
   int id = GetLocalIDForHandler(pc_handler);
   if (id == -1)
     return;
-  SendPeerConnectionUpdate(id, "onRenegotiationNeeded", String(""));
+  SendPeerConnectionUpdate(id, "negotiationneeded", String(""));
 }
 
 void PeerConnectionTracker::TrackGetUserMedia(
