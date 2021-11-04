080c180324080a08ea7fe9f63f9dd58e36aa037a
Katie Dektar <katie@chromium.org>
2021-11-04 01:32:10 +0000

    Dictatation commands match Voice Access grammars.
    Update Dictation command names and strings to match Voice Access
    grammars, to allow for easier upgrade to using Voice Access strings
    in the future.
    
    Adds a "help" command to open the help center article.
    
    Bug: 1247299
    Change-Id: I9f7d206a551557c1e3b663572a3929ddeb5e5966
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256957
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938094}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
@@ -30,6 +30,9 @@ export class Dictation {
     /** @private {boolean} */
     this.commandsFeatureEnabled_ = false;
 
+    /** @private {string} */
+    this.localePref_ = '';
+
     /**
      * The state of Dictation.
      * @private {!Dictation.DictationState}
@@ -99,8 +102,8 @@ export class Dictation {
         chrome.accessibilityPrivate.AccessibilityFeature.DICTATION_COMMANDS,
         (result) => {
           this.commandsFeatureEnabled_ = result;
-          if (this.commandsFeatureEnabled_) {
-            this.commandParser_.setCommandsEnabled();
+          if (this.commandsFeatureEnabled_ && this.localePref_) {
+            this.commandParser_.setCommandsEnabled(this.localePref_);
           }
         });
   }
@@ -233,6 +236,9 @@ export class Dictation {
       const command = this.commandParser_.parse(transcript);
       if (this.commandsFeatureEnabled_) {
         if (command.execute()) {
+          if (command.endsDictation()) {
+            return;
+          }
           this.showCommandExecuted_(command);
         } else {
           this.showCommandExecutionFailed_();
@@ -309,6 +315,10 @@ export class Dictation {
           if (pref.value) {
             this.speechRecognitionOptions_.locale =
                 /** @type {string} */ (pref.value);
+            this.localePref_ = this.speechRecognitionOptions_.locale;
+            if (this.commandsFeatureEnabled_) {
+              this.commandParser_.setCommandsEnabled(this.localePref_);
+            }
           }
           break;
         case Dictation.SPOKEN_FEEDBACK_PREF:
