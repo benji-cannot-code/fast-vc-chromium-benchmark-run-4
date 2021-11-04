c40f5538df1c8b38bf900ca65196e24cfa7b491a
Vignesh Shenvi <vshenvi@google.com>
2021-11-04 02:44:17 +0000

    Move DM token retrieval to the Report Queue Provider - Part 3
    This change updates the Report Queue Provider to use an appropriate
    DM token retriever to retrieve DM tokens and configure them with the
    report queue for downstream processing.
    
    Next set of items to do that this CL does not cover yet:
    
    - Migrate pre-existing events over to not specify DM tokens before-hand
    and let the Report Queue Provider configure it on their behalf.
    - Eventually deprecate/delete old workflow that supported creating
    report queue configuration with DM tokens.
    
    Bug: b:201086200
    Change-Id: I1d7cd0ddf61e5cb8aedd5a0911ad7882cb6c766d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256088
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938116}

--- components/reporting/client/mock_report_queue_provider.h
+++ components/reporting/client/mock_report_queue_provider.h
@@ -42,6 +42,12 @@ class MockReportQueueProvider : public ReportQueueProvider {
 
   MOCK_METHOD(void, OnInitCompleted, (), ());
 
+  MOCK_METHOD(void,
+              ConfigureReportQueue,
+              (std::unique_ptr<ReportQueueConfiguration> configuration,
+               ReportQueueProvider::ReportQueueConfiguredCallback callback),
+              (override));
+
  private:
   scoped_refptr<StorageModuleInterface> storage_;
 };
