98965c9ba2b4c34a4f66747859d4a596699faa37
Marc Treib <treib@chromium.org>
2021-11-04 17:28:47 +0000

    Cleanup: Remove PasswordCheck.Observer.onCompromisedCredentialFound()
    This method was never called (except for the implementation in
    PasswordCheckMediator, which was called by some tests covering this
    implementation).
    
    Bug: 1102025
    Change-Id: I8bf49e63972acdad01180c3348231004066b87e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259870
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938332}

--- chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckImpl.java
+++ chrome/browser/password_check/android/internal/java/src/org/chromium/chrome/browser/password_check/PasswordCheckImpl.java
@@ -49,11 +49,6 @@ class PasswordCheckImpl implements PasswordCheck, PasswordCheckObserver {
     }
 
     @Override
-    public void onCompromisedCredentialFound(CompromisedCredential credential) {
-        for (Observer obs : mObserverList) obs.onCompromisedCredentialFound(credential);
-    }
-
-    @Override
     public void onCompromisedCredentialsFetched(int count) {
         mCompromisedCredentialsFetched = true;
         for (Observer obs : mObserverList) {
--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckMediator.java
@@ -21,7 +21,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.build.BuildConfig;
-import org.chromium.chrome.browser.password_check.CompromisedCredential;
 import org.chromium.chrome.browser.password_check.PasswordCheck;
 import org.chromium.chrome.browser.password_check.PasswordCheckFactory;
 import org.chromium.chrome.browser.password_check.PasswordCheckReferrer;
@@ -334,9 +333,6 @@ class SafetyCheckMediator implements PasswordCheck.Observer {
             determinePasswordStateOnLoadComplete();
         }
     }
-
-    @Override
-    public void onCompromisedCredentialFound(CompromisedCredential leakedCredential) {}
 
     @Override
     public void onPasswordCheckProgressChanged(int alreadyProcessed, int remainingInQueue) {}
