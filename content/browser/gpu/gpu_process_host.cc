ccfbf8e947126490cc304ee3c8fa304eb3a11115
Maksim Sisov <msisov@igalia.com>
2021-11-04 20:41:38 +0000

    ozone/x11: move x11 switches to //ui/gfx:gfx_switches
    There are 2 X11 switches that reside in //ui/gfx/x/x11_switches.h,
    which are used by GpuProcessHost to propagate these switches to
    the GPU process.
    
    This makes content/browser/gpu/gpu_process_host.cc include
    //ui/gfx/x/x11_switches.h, which is not we we want is X11 code
    will eventually go under //ui/ozone/platform/x11 and it won't
    be accessible outside. Thus, move these switches to a common
    place under //ui/gfx:gfx_switches to fix this problem.
    
    Bug: 1096425
    Change-Id: Ifbdbae2a31eb60972a53c96f78971ba91249d90b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257324
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#938476}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -99,8 +99,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/ozone_switches.h"
 #endif
 
-#if defined(USE_X11)
-#include "ui/gfx/x/x11_switches.h"  // nogncheck
+#if defined(OS_LINUX)
+#include "ui/gfx/switches.h"
 #endif
 
 #if BUILDFLAG(USE_ZYGOTE_HANDLE)
@@ -296,7 +296,7 @@ static const char* const kSwitchNames[] = {
     switches::kOzoneDumpFile,
     switches::kDisableBufferBWCompression,
 #endif
-#if defined(USE_X11)
+#if defined(OS_LINUX)
     switches::kX11Display,
     switches::kNoXshm,
 #endif
--- ui/base/x/x11_shm_image_pool.cc
+++ ui/base/x/x11_shm_image_pool.cc
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/platform/platform_event_dispatcher.h"
 #include "ui/events/platform/platform_event_source.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/switches.h"
 #include "ui/gfx/x/extension_manager.h"
-#include "ui/gfx/x/x11_switches.h"
 
 namespace ui {
 
