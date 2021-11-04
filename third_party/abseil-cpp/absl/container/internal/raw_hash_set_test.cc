cc8c0eab25a85c7f425e87e98b61849df25c8172
Mirko Bonadei <mbonadei@chromium.org>
2021-11-04 17:39:13 +0000

    Reland "Roll abseil_revision f70eadadd7..3b22e57740"
    This is a reland of 1becef9e78101e4187ca04aa1b9ead6e83a02538
    
    Original change's description:
    > Roll abseil_revision f70eadadd7..3b22e57740
    >
    > Change Log:
    > https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/f70eadadd7..3b22e57740
    > Full diff:
    > https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/f70eadadd7..3b22e57740
    >
    > Bug: None
    > Change-Id: I28427deea4e0e9e3df63bc8000aa93771529d3f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257270
    > Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    > Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#937927}
    
    Bug: None
    Change-Id: Ia76e30ca53eee85abf11f3e1ff6ccb9bb2da6f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259872
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938342}

--- third_party/abseil-cpp/absl/container/internal/raw_hash_set_test.cc
+++ third_party/abseil-cpp/absl/container/internal/raw_hash_set_test.cc
@@ -2076,6 +2076,7 @@ TEST(RawHashSamplerTest, Sample) {
           std::memory_order_relaxed)]++;
       reservations[info.max_reserve.load(std::memory_order_relaxed)]++;
     }
+    EXPECT_EQ(info.inline_element_size, sizeof(int64_t));
     ++end_size;
   });
 
