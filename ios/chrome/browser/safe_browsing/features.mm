a01d470ac9c41009a57075d46365a2491649fa00
Daniel White <danieltwhite@google.com>
2021-11-04 18:48:57 +0000

    Removed runtime conditional checks for iOS 14 in the ios/chrome/browser directory
    Bug: 1129484
    Change-Id: I490d26eb6427d434f9c1245cdc04ab1934ee979a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251221
    Commit-Queue: Daniel White <danieltwhite@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938379}

--- ios/chrome/browser/safe_browsing/features.mm
+++ ios/chrome/browser/safe_browsing/features.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 bool IsPasswordReuseDetectionEnabled() {
-  return base::ios::IsRunningOnIOS14OrLater() &&
-         base::FeatureList::IsEnabled(
-             password_manager::features::kPasswordReuseDetectionEnabled);
+  return base::FeatureList::IsEnabled(
+      password_manager::features::kPasswordReuseDetectionEnabled);
 }
