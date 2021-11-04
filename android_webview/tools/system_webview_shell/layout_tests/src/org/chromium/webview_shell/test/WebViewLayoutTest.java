69f924ddd4bfbcdcbdb2046c84e741405156a781
Xing Liu <xingliu@chromium.org>
2021-11-04 20:53:00 +0000

    Disable WebViewLayoutTest#testRequestMIDIAccess.
    WebViewLayoutTest#testRequestMIDIAccess is failing on multiple Android
    bots.
    
    Bug: 1266967
    Change-Id: Ic0aac84652f5df61d72a42cb0a73def1b727e60c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262602
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938485}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -246,6 +246,7 @@ public class WebViewLayoutTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1266967")
     public void testRequestMIDIAccess() throws Exception {
         runWebViewLayoutTest("blink-apis/webmidi/requestmidiaccess.html",
                 "blink-apis/webmidi/requestmidiaccess-expected.txt");
