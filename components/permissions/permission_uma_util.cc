f86a3777b177f80df4f293e39d14539d7ad69aff
Takashi Sakamoto <tasak@google.com>
2021-11-04 03:53:47 +0000

    Revert "Add separate disposition for a chip with auto popup prompt bubble."
    This reverts commit 6cf5f4036426a623ed68fc8ae70ab2c3b39af82b.
    
    Reason for revert: suspect causing All/PermissionPromptBubbleViewBrowserTest.AlertAccessibleEvent/1
     failure on multiple mac bots.
    
    First failing build:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.11%20Tests/66721/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.12%20Tests/47435/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac10.14%20Tests/19825/overview
    https://ci.chromium.org/p/chromium/builders/ci/Mac11%20Tests/b8831564240610172849
    
    Failure Reason:
    Expected equality of these values:
      2
      counter.GetCount(ax::mojom::Event::kAlert)
        Which is: 1
    
    Original change's description:
    > Add separate disposition for a chip with auto popup prompt bubble.
    >
    > Bug: 1253842
    > Change-Id: I35eb57d3acd6892550f6081e987cec26a8b82dce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259044
    > Commit-Queue: Illia Klimov <elklm@google.com>
    > Reviewed-by: Ravjit Singh Uppal <ravjit@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#937932}
    
    Bug: 1253842, 1266678
    Change-Id: Ied7bb81d453e0e961efe6fe2a057bc7977428ce8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260490
    Owners-Override: Takashi Sakamoto <tasak@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#938137}

--- components/permissions/permission_uma_util.cc
+++ components/permissions/permission_uma_util.cc
@@ -357,8 +357,6 @@ std::string GetPromptDispositionString(
       return "LocationBarLeftQuietChip";
     case PermissionPromptDisposition::LOCATION_BAR_LEFT_QUIET_ABUSIVE_CHIP:
       return "LocationBarLeftQuietAbusiveChip";
-    case PermissionPromptDisposition::LOCATION_BAR_LEFT_CHIP_AUTO_BUBBLE:
-      return "LocationBarLeftChipAutoBubble";
     case PermissionPromptDisposition::LOCATION_BAR_RIGHT_ANIMATED_ICON:
       return "LocationBarRightAnimatedIcon";
     case PermissionPromptDisposition::LOCATION_BAR_RIGHT_STATIC_ICON:
@@ -1056,7 +1054,6 @@ bool PermissionUmaUtil::IsPromptDispositionQuiet(
     case PermissionPromptDisposition::ANCHORED_BUBBLE:
     case PermissionPromptDisposition::MODAL_DIALOG:
     case PermissionPromptDisposition::LOCATION_BAR_LEFT_CHIP:
-    case PermissionPromptDisposition::LOCATION_BAR_LEFT_CHIP_AUTO_BUBBLE:
     case PermissionPromptDisposition::NONE_VISIBLE:
     case PermissionPromptDisposition::CUSTOM_MODAL_DIALOG:
     case PermissionPromptDisposition::NOT_APPLICABLE:
@@ -1071,7 +1068,6 @@ bool PermissionUmaUtil::IsPromptDispositionLoud(
     case PermissionPromptDisposition::ANCHORED_BUBBLE:
     case PermissionPromptDisposition::MODAL_DIALOG:
     case PermissionPromptDisposition::LOCATION_BAR_LEFT_CHIP:
-    case PermissionPromptDisposition::LOCATION_BAR_LEFT_CHIP_AUTO_BUBBLE:
       return true;
     case PermissionPromptDisposition::LOCATION_BAR_RIGHT_STATIC_ICON:
     case PermissionPromptDisposition::LOCATION_BAR_RIGHT_ANIMATED_ICON:
