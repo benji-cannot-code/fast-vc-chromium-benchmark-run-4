adb251f760f28da241b74d9fc10ded320ff9acac
Tomasz Wiszkowski <ender@google.com>
2021-11-04 20:01:39 +0000

    SwitchToTab: improve performance of the feature on Android platform.
    This change reduces the volume of JNI calls by batching the
    operation that matches suggestions against open tabs.
    
    The updated code builds a single map of GURL -> Matching Tabs and
    performs a lookup operation for each URL to identify matching Tab.
    
    The code still artificially builds on top of a (boolean + Tab) rather
    than directly using a Tab object, because it is explicitly disallowed
    for //components code to depend on //chrome targets. Until Tab is
    moved to //components the code will have to operate like this.
    
    The actual size of the change is much smaller than perceived by Gerrit
    A lot of the existing code in the tab_matcher_android.cc have just
    been moved (consolidated) to a single call.
    
    Bug: 1176768
    Change-Id: Iab99b3175535df023b4e6aa454961fada620d7fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256708
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938434}

---
+++ components/omnibox/browser/tab_matcher.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/tab_matcher.h"
+
+void TabMatcher::FindMatchingTabs(GURLToTabInfoMap* map,
+                                  const AutocompleteInput* input) const {
+  for (auto& gurl_to_tab_info : *map) {
+    gurl_to_tab_info.second.has_matching_tab =
+        IsTabOpenWithURL(gurl_to_tab_info.first, input);
+  }
+}
