2aa878aedb779a0067beb5790c1568f1edfa99a1
Matt Wolenetz <wolenetz@chromium.org>
2021-11-05 21:31:43 +0000

    MSE: Allow async append prep allocation to non-fatally fail
    Updates memory allocation for the conceptual "input buffer" of appended
    bytes to directly use PartitionAllocator's AllocFlags, which allows
    returning a nullptr instead of crashing if OOM condition occurs. The
    allocation for the input buffer is done during the synchronous portion
    of SourceBuffer appendBuffer(), which already has a mechanism for
    throwing a "QuotaExceededError" exception if the system rejects an
    attempted appendBuffer() due to resource constraints. This change adds
    the failed allocation attempt as another way of throwing that exception
    and should help avoid crashes on systems that have limited free
    contiguous memory.
    
    Note, WTF::Vector does not provide a mechanism for attempting such
    allocation, and there are multiple other similar cases in blink already
    for using AllocFlags this way. Previous usage of Vector for a
    SourceBuffer's input buffer in Chromium stems from legacy API and
    vector's append semantics are no longer required, just a basic backing
    buffer.
    
    Future work might break larger input buffers into pieces during this
    allocation (likely at the boundary used already in each async part).
    That might help if the increased rate of QuotaExceededError is undue to
    actual application usage, but due more to memory fragmentation over
    longer runs.
    
    BUG=1252469,1259261
    
    Change-Id: I96506727458aca4f5d335a1644875a19f25f777e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260568
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938944}

--- third_party/blink/renderer/modules/mediasource/source_buffer.cc
+++ third_party/blink/renderer/modules/mediasource/source_buffer.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 #include <utility>
 
+#include "base/allocator/partition_allocator/partition_alloc.h"
 #include "base/numerics/checked_math.h"
 #include "media/base/logging_override_if_enabled.h"
 #include "media/base/stream_parser_buffer.h"
@@ -75,6 +76,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/network/mime/content_type.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/partition_allocator.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/partitions.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
@@ -208,6 +211,7 @@ SourceBuffer::SourceBuffer(std::unique_ptr<WebSourceBuffer> web_source_buffer,
       append_window_start_(0),
       append_window_end_(std::numeric_limits<double>::infinity()),
       first_initialization_segment_received_(false),
+      pending_append_data_size_(0),
       pending_append_data_offset_(0),
       pending_remove_start_(-1),
       pending_remove_end_(-1) {
@@ -1164,8 +1168,8 @@ void SourceBuffer::AbortIfUpdating() {
 
   DCHECK(!append_encoded_chunks_resolver_);
   append_buffer_async_task_handle_.Cancel();
-  pending_append_data_.clear();
-  pending_append_data_offset_ = 0;
+  ClearPendingAppendData();
+
   // For the regular, non-promisified appendBuffer abort, use events to notify
   // result.
   // 4.3. Queue a task to fire a simple event named abort at this SourceBuffer
@@ -1858,8 +1862,7 @@ bool SourceBuffer::HasPendingActivity() const {
 
 void SourceBuffer::ContextDestroyed() {
   append_buffer_async_task_handle_.Cancel();
-  pending_append_data_.clear();
-  pending_append_data_offset_ = 0;
+  ClearPendingAppendData();
 
   append_encoded_chunks_async_task_handle_.Cancel();
   pending_chunks_to_buffer_.reset();
@@ -1891,6 +1894,30 @@ void SourceBuffer::ScheduleEvent(const AtomicString& event_name) {
   event->SetTarget(this);
 
   async_event_queue_->EnqueueEvent(FROM_HERE, *event);
+}
+
+bool SourceBuffer::AllocatePendingAppendData(wtf_size_t size) {
+  DCHECK(!pending_append_data_);
+  DCHECK(size);
+  DCHECK(!pending_append_data_size_);
+  DCHECK(!pending_append_data_offset_);
+
+  pending_append_data_.reset(static_cast<unsigned char*>(
+      WTF::Partitions::BufferPartition()->AllocFlags(
+          base::PartitionAllocReturnNull, size, "MsePendingAppend")));
+
+  if (!pending_append_data_)
+    return false;
+
+  pending_append_data_size_ = size;
+  pending_append_data_offset_ = 0;
+  return true;
+}
+
+void SourceBuffer::ClearPendingAppendData() {
+  pending_append_data_.reset();
+  pending_append_data_size_ = 0;
+  pending_append_data_offset_ = 0;
 }
 
 bool SourceBuffer::PrepareAppend(double media_time,
@@ -2017,6 +2044,9 @@ void SourceBuffer::AppendBufferInternal_Locked(
     MediaSourceAttachmentSupplement::ExclusiveKey /* passkey */) {
   DCHECK(source_);
   DCHECK(!updating_);
+  DCHECK(!pending_append_data_);
+  DCHECK(!pending_append_data_size_);
+  DCHECK(!pending_append_data_offset_);
   source_->AssertAttachmentsMutexHeldIfCrossThreadForDebugging();
 
   // Finish the prepare append algorithm begun by the caller.
@@ -2031,9 +2061,36 @@ void SourceBuffer::AppendBufferInternal_Locked(
 
   // 2. Add data to the end of the input buffer.
   DCHECK(data || size == 0);
-  if (data)
-    pending_append_data_.Append(data, base::checked_cast<wtf_size_t>(size));
-  pending_append_data_offset_ = 0;
+  if (data) {
+    // We are guaranteed to not have anything in |pending_append_data_|
+    // currently. Instead of adding the new data to an empty WTF::Vector, which
+    // could crash on OOM during the inherent allocation, we instead just
+    // attempt to allocate specifically the needed size and copy the input data
+    // to it. We do not need vector iterator semantics, just a basic byte
+    // buffer. This allows us to use the underlying allocator's ability to
+    // return null if the allocation fails, instead of causing OOM.
+    // TODO(crbug.com/1266639): Consider further optimizations.
+    if (!AllocatePendingAppendData(base::checked_cast<wtf_size_t>(size))) {
+      DCHECK(!pending_append_data_);
+      DCHECK(!pending_append_data_size_);
+      DCHECK(!pending_append_data_offset_);
+      MediaSource::LogAndThrowDOMException(
+          *exception_state, DOMExceptionCode::kQuotaExceededError,
+          "Unable to allocate space required to buffer appended media.");
+      TRACE_EVENT_NESTABLE_ASYNC_END0(
+          "media", "SourceBuffer::prepareAsyncAppend", TRACE_ID_LOCAL(this));
+      return;
+    }
+
+    // Copy |data| into |pending_append_data_| and continue.
+    static_assert(
+        !WTF::PartitionAllocator::kIsGarbageCollected,
+        "Ensure that we can use simple memcpy when using PartitionAllocator");
+    DCHECK(pending_append_data_);
+    DCHECK_EQ(pending_append_data_size_, base::checked_cast<wtf_size_t>(size));
+    DCHECK(!pending_append_data_offset_);
+    memcpy(pending_append_data_.get(), data, size);
+  }
 
   // 3. Set the updating attribute to true.
   updating_ = true;
@@ -2154,9 +2211,9 @@ void SourceBuffer::AppendBufferAsyncPart_Locked(
 
   // 1. Run the segment parser loop algorithm.
   // Step 2 doesn't apply since we run Step 1 synchronously here.
-  DCHECK_GE(pending_append_data_.size(), pending_append_data_offset_);
+  DCHECK_GE(pending_append_data_size_, pending_append_data_offset_);
   wtf_size_t append_size =
-      pending_append_data_.size() - pending_append_data_offset_;
+      pending_append_data_size_ - pending_append_data_offset_;
 
   // Impose an arbitrary max size for a single append() call so that an append
   // doesn't block the renderer event loop very long. This value was selected
@@ -2179,15 +2236,16 @@ void SourceBuffer::AppendBufferAsyncPart_Locked(
   // so that it can clear its end of stream state if necessary.
   unsigned char zero = 0;
   unsigned char* append_data = &zero;
-  if (append_size)
-    append_data = pending_append_data_.data() + pending_append_data_offset_;
+  if (append_size) {
+    DCHECK(pending_append_data_);
+    append_data = pending_append_data_.get() + pending_append_data_offset_;
+  }
 
   bool append_success =
       web_source_buffer_->Append(append_data, append_size, &timestamp_offset_);
 
   if (!append_success) {
-    pending_append_data_.clear();
-    pending_append_data_offset_ = 0;
+    ClearPendingAppendData();
     // Note that AppendError() calls NotifyDurationChanged, so a cross-thread
     // attachment will send updated buffered and seekable information to the
     // main thread here, too.
@@ -2195,7 +2253,7 @@ void SourceBuffer::AppendBufferAsyncPart_Locked(
   } else {
     pending_append_data_offset_ += append_size;
 
-    if (pending_append_data_offset_ < pending_append_data_.size()) {
+    if (pending_append_data_offset_ < pending_append_data_size_) {
       append_buffer_async_task_handle_ = PostCancellableTask(
           *GetExecutionContext()->GetTaskRunner(TaskType::kMediaElementEvent),
           FROM_HERE,
@@ -2210,8 +2268,7 @@ void SourceBuffer::AppendBufferAsyncPart_Locked(
 
     // 3. Set the updating attribute to false.
     updating_ = false;
-    pending_append_data_.clear();
-    pending_append_data_offset_ = 0;
+    ClearPendingAppendData();
 
     source_->SendUpdatedInfoToMainThreadCache();
 
