d12953cd92cde659efd6dd9271dbac0be4981c49
Dave Tapuska <dtapuska@chromium.org>
2021-11-05 13:46:20 +0000

    Remove usage of WebContentsObserver in favor of WeakPtr.
    The WebContentsObserver could have just used a WeakPtr instead and
    we avoid the intermediate class to clear the value.
    
    Change-Id: I21a2719b1f3821ef2db2b6ab83bde08f637787c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259417
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938751}

--- chrome/browser/ui/views/extensions/extension_install_friction_dialog_view.h
+++ chrome/browser/ui/views/extensions/extension_install_friction_dialog_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSION_INSTALL_FRICTION_DIALOG_VIEW_H_
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
@@ -42,20 +43,18 @@ class ExtensionInstallFrictionDialogView
 
   // Returns the parent web contents for the dialog. Returns nullptr if the web
   // contents have been destroyed.
-  content::WebContents* parent_web_contents() { return parent_web_contents_; }
+  content::WebContents* parent_web_contents() {
+    return parent_web_contents_.get();
+  }
 
   void ClickLearnMoreLinkForTesting();
 
  private:
-  class WebContentsDestructionObserver;
-
   std::unique_ptr<views::StyledLabel> CreateWarningLabel();
   void OnLearnMoreLinkClicked();
 
   Profile* profile_ = nullptr;
-  content::WebContents* parent_web_contents_ = nullptr;
-  std::unique_ptr<WebContentsDestructionObserver>
-      web_contents_destruction_observer_;
+  base::WeakPtr<content::WebContents> parent_web_contents_;
   base::OnceCallback<void(bool)> callback_;
 
   bool accepted_ = false;
