efad93018c75342a558d7e49578b24408b8f7103
Javier Fernández García-Boente <jfernandez@igalia.com>
2021-11-05 11:47:53 +0000

    Providing info about editing boundaries when determining caret bounds
    This CL adds an EditingBoundaryCrossingRule argument to several
    functions used to determine the local and absolute caret bounds.
    
    These logic to determine the caret bounds also use the
    ComputeInlineAdjustedPosition function to get an adjusted position,
    based on the downstream and upstream equivalent positions. Currently,
    we are always assuming kCanCrossEditingBoundary when calling to the
    MostForwardCaretPosition and MostBackwardCaretPosition to get the
    downstream and upstream positions respectively. This CL allows the
    callers to decide a different editing boundaries restriction.
    
    This CL should not cause any behavior change, since the new functions
    to determine the downstream/upstream equivalent positions are ignoring
    the editing boundaries rule, that's why I'm not adding any additional
    tests for this change.
    
    Bug: 688015
    
    Change-Id: I1a73437cbf640fb0e0a75d29d367cb534e4de8f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259402
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#938733}

--- third_party/blink/renderer/core/editing/inline_box_position.h
+++ third_party/blink/renderer/core/editing/inline_box_position.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_INLINE_BOX_POSITION_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/editing/editing_boundary.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
@@ -70,9 +71,12 @@ ComputeInlineBoxPosition(const PositionWithAffinity&);
 CORE_EXPORT InlineBoxPosition
 ComputeInlineBoxPosition(const PositionInFlatTreeWithAffinity&);
 
-PositionWithAffinity ComputeInlineAdjustedPosition(const PositionWithAffinity&);
+PositionWithAffinity ComputeInlineAdjustedPosition(
+    const PositionWithAffinity&,
+    EditingBoundaryCrossingRule = kCanCrossEditingBoundary);
 PositionInFlatTreeWithAffinity ComputeInlineAdjustedPosition(
-    const PositionInFlatTreeWithAffinity&);
+    const PositionInFlatTreeWithAffinity&,
+    EditingBoundaryCrossingRule = kCanCrossEditingBoundary);
 
 InlineBoxPosition ComputeInlineBoxPositionForInlineAdjustedPosition(
     const PositionWithAffinity&);
--- third_party/blink/renderer/core/editing/local_caret_rect.h
+++ third_party/blink/renderer/core/editing/local_caret_rect.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_LOCAL_CARET_RECT_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/editing/editing_boundary.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_rect.h"
 
@@ -39,18 +40,22 @@ struct LocalCaretRect {
 // TODO(xiaochengh): Get rid of the default parameter.
 CORE_EXPORT LocalCaretRect LocalCaretRectOfPosition(
     const PositionWithAffinity&,
-    LayoutUnit* /* extra_width_to_end_of_line */ = nullptr);
-CORE_EXPORT LocalCaretRect
-LocalCaretRectOfPosition(const PositionInFlatTreeWithAffinity&);
+    LayoutUnit* /* extra_width_to_end_of_line */ = nullptr,
+    EditingBoundaryCrossingRule = kCanCrossEditingBoundary);
+CORE_EXPORT LocalCaretRect LocalCaretRectOfPosition(
+    const PositionInFlatTreeWithAffinity&,
+    EditingBoundaryCrossingRule = kCanCrossEditingBoundary);
 
 LocalCaretRect LocalSelectionRectOfPosition(const PositionWithAffinity&);
 
 // Bounds of (possibly transformed) caret in absolute coords
-CORE_EXPORT IntRect
-AbsoluteCaretBoundsOf(const PositionWithAffinity&,
-                      LayoutUnit* extra_width_to_end_of_line = nullptr);
-CORE_EXPORT IntRect
-AbsoluteCaretBoundsOf(const PositionInFlatTreeWithAffinity&);
+CORE_EXPORT IntRect AbsoluteCaretBoundsOf(
+    const PositionWithAffinity&,
+    LayoutUnit* extra_width_to_end_of_line = nullptr,
+    EditingBoundaryCrossingRule rule = kCanCrossEditingBoundary);
+CORE_EXPORT IntRect AbsoluteCaretBoundsOf(
+    const PositionInFlatTreeWithAffinity&,
+    EditingBoundaryCrossingRule rule = kCanCrossEditingBoundary);
 
 CORE_EXPORT IntRect AbsoluteSelectionBoundsOf(const VisiblePosition&);
 CORE_EXPORT IntRect AbsoluteSelectionBoundsOf(const VisiblePositionInFlatTree&);
