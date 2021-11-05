6a7dffd8e207df74c53efaf5a43f2242fe478d4e
Caitlin Fischer <caitlinfischer@google.com>
2021-11-05 02:36:40 +0000

    Re-enable the Extended Variations Safe Mode experiment on iOS.
    It was temporarily disabled because an edge case was causing some tests
    to flake. The edge case was fixed by crrev.com/c/3171574.
    
    Bug: 1266503
    
    Change-Id: I05e50ad6ee618ed6fedf76156066d26da8524bb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3247613
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938632}

--- components/variations/service/variations_field_trial_creator_unittest.cc
+++ components/variations/service/variations_field_trial_creator_unittest.cc
@@ -71,7 +71,7 @@ const char kTestSeedSerializedData[] = "a serialized seed, 100% realistic";
 const char kTestSeedSignature[] = "a totally valid signature, I swear!";
 const int kTestSeedMilestone = 90;
 
-#if !defined(OS_ANDROID) && !defined(OS_IOS)
+#if !defined(OS_ANDROID)
 // The content of an empty prefs file.
 const char kEmptyPrefsFile[] = "{}";
 #endif
@@ -321,7 +321,7 @@ class TestVariationsFieldTrialCreator : public VariationsFieldTrialCreator {
     return was_maybe_extend_variations_safe_mode_called_;
   }
 
-#if !defined(OS_ANDROID) && !defined(OS_IOS)
+#if !defined(OS_ANDROID)
  protected:
   void MaybeExtendVariationsSafeMode(
       metrics::MetricsStateManager* metrics_state_manager) override {
@@ -329,7 +329,7 @@ class TestVariationsFieldTrialCreator : public VariationsFieldTrialCreator {
     VariationsFieldTrialCreator::MaybeExtendVariationsSafeMode(
         metrics_state_manager);
   }
-#endif  // !defined(OS_ANDROID) && !defined(OS_IOS)
+#endif  // !defined(OS_ANDROID)
 
  private:
   VariationsSeedStore* GetSeedStore() override { return &seed_store_; }
@@ -381,9 +381,8 @@ class FieldTrialCreatorTest : public ::testing::Test {
   std::unique_ptr<base::FeatureList> global_feature_list_;
 };
 
-#if !defined(OS_ANDROID) && !defined(OS_IOS)
-// TODO(crbug/1248239): Enable Extended Variations Safe Mode on Clank.
-// TODO(crbug/1255305): Re-enable it on iOS.
+#if !defined(OS_ANDROID)
+// TODO(crbug/1248239): Enable Extended Variations Safe Mode on Android Chrome.
 class FieldTrialCreatorSafeModeExperimentTest : public FieldTrialCreatorTest {
  public:
   FieldTrialCreatorSafeModeExperimentTest()
@@ -437,7 +436,7 @@ struct StartupVisibilityTestParams {
 class FieldTrialCreatorTestWithStartupVisibility
     : public FieldTrialCreatorSafeModeExperimentTest,
       public ::testing::WithParamInterface<StartupVisibilityTestParams> {};
-#endif  // !defined(OS_ANDROID) && !defined(OS_IOS)
+#endif  // !defined(OS_ANDROID)
 
 // Verify that unexpired seeds are used.
 TEST_F(FieldTrialCreatorTest, SetUpFieldTrials_ValidSeed_NotExpired) {
@@ -942,8 +941,8 @@ TEST_F(FieldTrialCreatorTest, ClientFilterableState_HardwareClass) {
 }
 #endif  // OS_ANDROID
 
-#if !defined(OS_ANDROID) && !defined(OS_IOS)
-// TODO(crbug/1248239): Enable Extended Variations Safe Mode on Clank.
+#if !defined(OS_ANDROID)
+// TODO(crbug/1248239): Enable Extended Variations Safe Mode on Android Chrome.
 // TODO(crbug/1255305): Re-enable it on iOS.
 TEST_F(FieldTrialCreatorSafeModeExperimentTest, OptOutOfExperiment) {
   std::unique_ptr<PrefService> pref_service(CreatePrefService());
@@ -1159,6 +1158,6 @@ TEST_F(FieldTrialCreatorSafeModeExperimentTest,
   ASSERT_TRUE(base::ReadFileToString(prefs_file(), &pref_file_contents));
   EXPECT_EQ(kEmptyPrefsFile, pref_file_contents);
 }
-#endif  // !defined(OS_ANDROID) && !defined(OS_IOS)
+#endif  // !defined(OS_ANDROID)
 
 }  // namespace variations
