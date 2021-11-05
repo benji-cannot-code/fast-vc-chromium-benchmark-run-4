aa3d676c4efbbecc7c504e1f1a891bbb142c1932
Kevin Lubick <kjlubick@chromium.org>
2021-11-05 12:59:35 +0000

    Guard include of GrVkTypes only on Vulkan builds.
    GrVkTypes indirectly includes vulkan headers.
    
    While searching for other unguarded includes of GrVkTypes,
    I found skia_output_device_x11.cc, which had some extra
    Skia includes that appear unused or needed for transitive
    includes. I replaced those with what appeared to be the
    correct direct includes.
    
    Change-Id: I95ac319563e484727dc37c399fce3352bb64674d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261267
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938743}

--- components/viz/service/display_embedder/skia_output_device_x11.cc
+++ components/viz/service/display_embedder/skia_output_device_x11.cc
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/ref_counted_memory.h"
-#include "third_party/skia/include/core/SkSurface.h"
-#include "third_party/skia/include/gpu/GrBackendSurface.h"
-#include "third_party/skia/include/gpu/vk/GrVkTypes.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
 #include "ui/base/x/x11_util.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/x/xproto.h"
--- gpu/command_buffer/service/skia_utils.h
+++ gpu/command_buffer/service/skia_utils.h
@@ -16,7 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrContextOptions.h"
 #include "third_party/skia/include/gpu/GrTypes.h"
+
+#if BUILDFLAG(ENABLE_VULKAN)
 #include "third_party/skia/include/gpu/vk/GrVkTypes.h"
+#endif
 
 // Forwardly declare a few GL types to avoid including GL header files.
 typedef int GLint;
