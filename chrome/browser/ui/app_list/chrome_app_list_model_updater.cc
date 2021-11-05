bef24e0be6951343b0e717c14c478bb19447eadf
Andrew Xu <andrewxu@chromium.org>
2021-11-05 21:43:50 +0000

    [Launcher] Fix OnAppListItemUpdated() not called after setting position
    Now `AppListModel::SetItemPosition()` does not always call
    `OnAppListItemUpdated()`. Meanwhile, `ChromeAppListModelUpdater` relies
    on `OnAppListItemUpdated()` to update `ChromeAppListItem`. This issue
    should exist even before recent refactoring.
    
    This CL is a quick fixing for this issue. In detail, this CL does the
    following things:
    
    (1) Make `AppListItemList::SetItemPosition()` return a boolean value
    that indicates whether the item's index in the sorted list changes
    after setting a new ordinal position.
    
    (2) If `AppListItemList::SetItemPosition()` returns false, call
    `OnAppListItemUpdated` explicitly to ensure that observers (actually
    on ChromeAppListModelUpdater) are notified of the position update.
    
    The real fixing is illustrated in https://crbug.com/1264839
    
    Bug: 1267231
    Change-Id: I6793cb6bb234eecc5a6f2c8410c23be100f3d73b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263220
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938956}

--- chrome/browser/ui/app_list/chrome_app_list_model_updater.cc
+++ chrome/browser/ui/app_list/chrome_app_list_model_updater.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_controller.h"
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ui/app_list/chrome_app_list_item.h"
 #include "chrome/browser/ui/app_list/chrome_app_list_item_manager.h"
@@ -490,11 +489,8 @@ void ChromeAppListModelUpdater::UpdateAppItemFromSyncItem(
 }
 
 void ChromeAppListModelUpdater::NotifyProcessSyncChangesFinished() {
-  if (is_active_) {
-    AppListClientImpl::GetInstance()
-        ->GetAppListController()
-        ->NotifyProcessSyncChangesFinished();
-  }
+  if (is_active_)
+    ash::AppListController::Get()->NotifyProcessSyncChangesFinished();
 }
 
 void ChromeAppListModelUpdater::AddObserver(
