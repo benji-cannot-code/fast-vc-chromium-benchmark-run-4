77a65dcee0ff134a919e7570858e1ed69b531d18
Alison Maher <almaher@microsoft.com>
2021-11-05 01:02:49 +0000

    [FlexNG] Make HasAutoMarginsInCrossAxis() static
    This is in preparation for a larger refactor meant to optimize flex
    fragmentation and should not result in any functional changes.
    
    Bug: 660611
    Change-Id: Ie5033a64a6d8193b1c454a6d53acb1f1e387aabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248974
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938610}

--- third_party/blink/renderer/core/layout/flexible_box_algorithm.h
+++ third_party/blink/renderer/core/layout/flexible_box_algorithm.h
@@ -173,8 +173,6 @@ class FlexItem {
 
   LayoutUnit AvailableAlignmentSpace() const;
 
-  bool HasAutoMarginsInCrossAxis() const;
-
   void UpdateAutoMarginsInMainAxis(LayoutUnit auto_margin_offset);
 
   // Computes the cross-axis size that a stretched item should have and stores
@@ -192,6 +190,9 @@ class FlexItem {
                                     LayoutUnit max_ascent,
                                     bool is_wrap_reverse,
                                     bool is_deprecated_webkit_box);
+
+  static bool HasAutoMarginsInCrossAxis(const ComputedStyle& item_style,
+                                        FlexLayoutAlgorithm* algorithm);
 
   void Trace(Visitor*) const;
 
