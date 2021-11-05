8cd9dec3a8ac6661bb623fcef66316df287aabd0
Patrick Monette <pmonette@chromium.org>
2021-11-05 21:37:53 +0000

    Fix missing copyright in chromecast/net/time_sync_tracker_fuchsia.cc
    Change-Id: Id6c5f02e532b44743772a4795bdfe3649350e1ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3217138
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Reviewed-by: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938951}

--- chromecast/net/time_sync_tracker_fuchsia.cc
+++ chromecast/net/time_sync_tracker_fuchsia.cc
@@ -1,4 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "chromecast/net/time_sync_tracker_fuchsia.h"
 
 #include <lib/zx/clock.h>
