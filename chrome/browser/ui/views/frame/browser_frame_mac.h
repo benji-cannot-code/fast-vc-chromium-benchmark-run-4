50a7cc0a4f9c2620a0ed0a80f0a73f539db6491b
Amanda Baker <ambake@microsoft.com>
2021-11-05 19:31:28 +0000

    dpwas: Announce state change when toggling WCO on Mac
    WCO = Window Controls Overlay
    
    When making screen reader announcements on Mac, they must be made against
    the active window, otherwise they will not be surfaced to the user. In
    the case of PWAs, the announcement must be made against the NSWindow
    associated with the app shim.
    
    This change surfaces the Window Controls Overlay toggle announcement
    against the proper NSWindow on Mac, so that the screen reader behavior
    is consistent across all platforms.
    
    Bug: 1266922, 1227251
    Change-Id: Id68b8623a18f87046b2759fbaeca7ceb77e58763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3257815
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Amanda Baker <ambake@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#938894}

--- chrome/browser/ui/views/frame/browser_frame_mac.h
+++ chrome/browser/ui/views/frame/browser_frame_mac.h
@@ -52,6 +52,7 @@ class BrowserFrameMac : public views::NativeWidgetMac,
   bool HandleKeyboardEvent(
       const content::NativeWebKeyboardEvent& event) override;
   bool ShouldRestorePreviousBrowserWidgetState() const override;
+  void AnnounceTextInInProcessWindow(const std::u16string& text) override;
 
  protected:
   ~BrowserFrameMac() override;
