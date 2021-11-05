76d2876efa6092e1700ed09be297764570623828
Eric Lawrence [MSFT] <ericlaw@microsoft.com>
2021-11-05 19:18:39 +0000

    Remove FTP from SavableSchemes
    Chrome no longer supports FTP, so the scenario of performing a Save Page
    operation on an FTP-sourced frame is no longer possible. Remove the FTP
    protocol from the list of supported schemes for saving pages.
    
    Bug: 333943
    Change-Id: Ib29ac314063b818e9caa08db80d4b0cd8922e20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261524
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#938888}

--- content/common/url_schemes.cc
+++ content/common/url_schemes.cc
@@ -28,7 +28,6 @@ const char* const kDefaultSavableSchemes[] = {
   url::kHttpsScheme,
   url::kFileScheme,
   url::kFileSystemScheme,
-  url::kFtpScheme,
   kChromeDevToolsScheme,
   kChromeUIScheme,
   url::kDataScheme
