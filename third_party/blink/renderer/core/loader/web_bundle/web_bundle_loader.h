a64a32b9b5a057015214846fd95cd43c4838878c
Hayato Ito <hayato@chromium.org>
2021-11-05 03:23:39 +0000

    Avoid an anonymous namespace in a header file
    A header file should not contain the anonymous namespace, which
    https://crrev.com/c/3245756 introduced.
    
    Note this CL also changes "(plain) enum" to "enum class".
    
    Bug: 1263783,1245166
    Change-Id: I7d44cb40068af5d2926b98182f77b3759d6e7268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260101
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938643}

--- third_party/blink/renderer/core/loader/web_bundle/web_bundle_loader.h
+++ third_party/blink/renderer/core/loader/web_bundle/web_bundle_loader.h
@@ -24,10 +24,6 @@ class SubresourceWebBundle;
 class Document;
 class ThreadableLoader;
 
-namespace {
-enum LoadState { kInProgress, kFailed, kSuccess };
-}
-
 // A loader which is used to load a resource from webbundle.
 class WebBundleLoader : public GarbageCollected<WebBundleLoader>,
                         public ThreadableLoaderClient,
@@ -40,8 +36,8 @@ class WebBundleLoader : public GarbageCollected<WebBundleLoader>,
 
   void Trace(Visitor* visitor) const override;
 
-  bool HasLoaded() const { return load_state_ == kSuccess; }
-  bool HasFailed() const { return load_state_ == kFailed; }
+  bool HasLoaded() const { return load_state_ == LoadState::kSuccess; }
+  bool HasFailed() const { return load_state_ == LoadState::kFailed; }
 
   // ThreadableLoaderClient
   void DidStartLoadingResponseBody(BytesConsumer& consumer) override;
@@ -66,11 +62,13 @@ class WebBundleLoader : public GarbageCollected<WebBundleLoader>,
   void ClearReceivers();
 
  private:
+  enum class LoadState { kInProgress, kFailed, kSuccess };
+
   void DidFailInternal();
 
   Member<SubresourceWebBundle> subresource_web_bundle_;
   Member<ThreadableLoader> loader_;
-  LoadState load_state_ = kInProgress;
+  LoadState load_state_ = LoadState::kInProgress;
   KURL url_;
   scoped_refptr<SecurityOrigin> security_origin_;
   base::UnguessableToken web_bundle_token_;
