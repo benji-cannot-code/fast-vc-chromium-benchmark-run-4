ce57fdad460dcf14d1d8ed4fb1f770f30f356f52
Olivier Li <olivierli@chromium.org>
2021-11-05 21:17:25 +0000

    Add History.Clusters.Backend.CollapseDuplicatesDuration metric
    Bug: 1267339
    Change-Id: Ia29593667f79bcf7c7caea212305f23b51843606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260237
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938934}

--- components/history_clusters/core/history_clusters_service.cc
+++ components/history_clusters/core/history_clusters_service.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "base/time/time_to_iso8601.h"
+#include "base/timer/elapsed_timer.h"
 #include "base/values.h"
 #include "components/history/core/browser/history_backend.h"
 #include "components/history/core/browser/history_database.h"
@@ -615,9 +616,14 @@ void HistoryClustersService::OnGotClusters(
   base::UmaHistogramTimes("History.Clusters.Backend.GetClustersLatency",
                           base::TimeTicks::Now() - cluster_start_time);
 
+  base::ElapsedTimer timer;
   auto filtered_raw_clusters = FilterClustersMatchingQuery(query, clusters);
   result.clusters = CollapseDuplicateVisits(filtered_raw_clusters);
   result.clusters = SortClusters(result.clusters);
+
+  base::TimeDelta clustering_duration = timer.Elapsed();
+  base::UmaHistogramLongTimes("History.Clusters.ProcessClustersDuration",
+                              clustering_duration);
 
   base::UmaHistogramCounts1000("History.Clusters.Backend.NumClustersReturned",
                                static_cast<int>(clusters.size()));
