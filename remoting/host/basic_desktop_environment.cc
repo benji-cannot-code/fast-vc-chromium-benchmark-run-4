c60b9f1c62343ab0e191c2956af45696af8c946b
Maksim Sisov <msisov@igalia.com>
2021-11-05 17:43:57 +0000

    ozone: remoting: rename use_x11 to remoting_use_x11
    Remoting doesn't have ozone implementation, but uses x11 on linux.
    Thus, given that use_x11 is being deprecated, rename use_x11
    to remoting_use_x11.
    
    Bug: 1096425
    Change-Id: I60c52307290c9acc609f333059ec1ca80a734db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263412
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#938847}

--- remoting/host/basic_desktop_environment.cc
+++ remoting/host/basic_desktop_environment.cc
@@ -30,15 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/win/evaluate_d3d.h"
 #endif
 
-#if defined(USE_X11)
+#if defined(REMOTING_USE_X11)
 #include "base/threading/watchdog.h"
 #include "remoting/host/linux/x11_util.h"
-#include "ui/base/ui_base_features.h"
 #endif
 
 namespace remoting {
 
-#if defined(USE_X11)
+#if defined(REMOTING_USE_X11)
 
 namespace {
 
@@ -68,7 +67,7 @@ class IgnoreXServerGrabsWatchdog : public base::Watchdog {
 
 }  // namespace
 
-#endif  // defined(USE_X11)
+#endif  // defined(REMOTING_USE_X11)
 
 BasicDesktopEnvironment::~BasicDesktopEnvironment() {
   DCHECK(caller_task_runner_->BelongsToCurrentThread());
@@ -170,16 +169,14 @@ BasicDesktopEnvironment::BasicDesktopEnvironment(
       client_session_control_(client_session_control),
       options_(options) {
   DCHECK(caller_task_runner_->BelongsToCurrentThread());
-#if defined(USE_X11)
-  if (!features::IsUsingOzonePlatform()) {
-    // TODO(yuweih): The watchdog is just to test the hypothesis.
-    // The IgnoreXServerGrabs() call should probably be moved to whichever
-    // thread that created desktop_capture_options().x_display().
-    IgnoreXServerGrabsWatchdog watchdog;
-    watchdog.Arm();
-    desktop_capture_options().x_display()->IgnoreXServerGrabs();
-    watchdog.Disarm();
-  }
+#if defined(REMOTING_USE_X11)
+  // TODO(yuweih): The watchdog is just to test the hypothesis.
+  // The IgnoreXServerGrabs() call should probably be moved to whichever
+  // thread that created desktop_capture_options().x_display().
+  IgnoreXServerGrabsWatchdog watchdog;
+  watchdog.Arm();
+  desktop_capture_options().x_display()->IgnoreXServerGrabs();
+  watchdog.Disarm();
 #elif defined(OS_WIN)
   // The options passed to this instance are determined by a process running in
   // Session 0.  Access to DirectX functions in Session 0 is limited so the
