49a242a2ee655348d03b0ee5d096faac95398026
Peter Boström <pbos@chromium.org>
2021-11-05 21:27:41 +0000

    Align tab-group-stroke thickness with FocusRing
    This changes stroke from 3dp to 2dp to match the focus ring.
    
    Bug: None
    Change-Id: I0ff67e8e98299ff90a5b500b2dde779768fd91f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2427568
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Edward Jung (EMEA) <edwardjung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938942}

--- chrome/browser/ui/views/tabs/tab_group_underline.h
+++ chrome/browser/ui/views/tabs/tab_group_underline.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/tab_groups/tab_group_id.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/view.h"
 
 class TabGroupViews;
@@ -19,7 +20,9 @@ class TabGroupUnderline : public views::View {
  public:
   METADATA_HEADER(TabGroupUnderline);
 
-  static constexpr int kStrokeThickness = 3;
+  static constexpr int kStrokeThickness =
+      views::FocusRing::kDefaultHaloThickness;
+
   static int GetStrokeInset();
 
   TabGroupUnderline(TabGroupViews* tab_group_views,
