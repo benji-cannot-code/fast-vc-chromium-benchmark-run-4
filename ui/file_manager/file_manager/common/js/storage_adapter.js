3f6d0c65b2b4fca59a345e4534ca78a3ae1f77e7
Luciano Pacheco <lucmult@chromium.org>
2021-11-05 00:59:27 +0000

    Files app and SWA: Change callers sites of xfm.storage to avoid chrome.runtime.lastError
    This fixes the issue that the caller sites were logging `lastError` in
    the SWA for errors from other API calls, because the xfm.storage in the
    SWA doesn't use an API that reports errors via `lastError`.
    
    Bug: b:205071156, chromium:1233372
    Change-Id: I06f8295d22a9901cb410fa5a89952288f2971322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260099
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938609}

--- ui/file_manager/file_manager/common/js/storage_adapter.js
+++ ui/file_manager/file_manager/common/js/storage_adapter.js
@@ -18,6 +18,12 @@ class StorageAreaAsync {
   constructor(storageArea) {
     this.storageArea_ = (storageArea) ? storageArea : this;
 
+    /**
+     * If the chrome.runtime.lastError should be checked.
+     * @protected {boolean}
+     */
+    this.checkLastError = true;
+
     this.get = this.storageArea_.get.bind(this.storageArea_);
     this.set = this.storageArea_.set.bind(this.storageArea_);
     this.remove = this.storageArea_.remove.bind(this.storageArea_);
@@ -32,7 +38,8 @@ class StorageAreaAsync {
   async getAsync(keys) {
     return new Promise((resolve, reject) => {
       this.get(keys, (values) => {
-        if (chrome && chrome.runtime && chrome.runtime.lastError) {
+        if (this.checkLastError && chrome && chrome.runtime &&
+            chrome.runtime.lastError) {
           const keysString = keys && keys.join(', ');
           reject(`Failed to retrieve keys [${keysString}] from browser storage:
               ${chrome.runtime.lastError.message}`);
@@ -51,7 +58,8 @@ class StorageAreaAsync {
   async setAsync(values) {
     return new Promise((resolve, reject) => {
       this.set(values, () => {
-        if (chrome && chrome.runtime && chrome.runtime.lastError) {
+        if (this.checkLastError && chrome && chrome.runtime &&
+            chrome.runtime.lastError) {
           const keysString = values && Object.keys(values).join(', ');
           reject(`Failed to update browser storage keys
               [${keysString}] with supplied values:
@@ -105,6 +113,8 @@ class StorageAreaSWAImpl extends StorageAreaAsync {
    */
   constructor(type) {
     super(/** storageArea */ null);
+
+    this.checkLastError = false;
 
     /** @private {boolean} */
     this.loaded_ = false;
--- ui/file_manager/file_manager/foreground/js/app_state_controller.js
+++ ui/file_manager/file_manager/foreground/js/app_state_controller.js
@@ -49,45 +49,37 @@ export class AppStateController {
   /**
    * @return {Promise}
    */
-  loadInitialViewOptions() {
+  async loadInitialViewOptions() {
     // Load initial view option.
-    return new Promise((fulfill, reject) => {
-             xfm.storage.local.get(this.viewOptionStorageKey_, values => {
-               if (chrome.runtime.lastError) {
-                 reject(
-                     'Failed to load view options: ' +
-                     chrome.runtime.lastError.message);
-               } else {
-                 fulfill(values);
-               }
-             });
-           })
-        .then(values => {
-          this.viewOptions_ = {};
-          const value = values[this.viewOptionStorageKey_];
-          if (!value) {
-            return;
-          }
+    try {
+      const values =
+          await xfm.storage.local.getAsync(this.viewOptionStorageKey_);
 
-          // Load the global default options.
-          try {
-            this.viewOptions_ = JSON.parse(value);
-          } catch (ignore) {
-          }
+      this.viewOptions_ = {};
 
-          // Override with window-specific options.
-          if (window.appState && window.appState.viewOptions) {
-            for (const key in window.appState.viewOptions) {
-              if (window.appState.viewOptions.hasOwnProperty(key)) {
-                this.viewOptions_[key] = window.appState.viewOptions[key];
-              }
-            }
+      const value = /** @type {string} */ (values[this.viewOptionStorageKey_]);
+      if (!value) {
+        return;
+      }
+
+      // Load the global default options.
+      try {
+        this.viewOptions_ = JSON.parse(value);
+      } catch (ignore) {
+      }
+
+      // Override with window-specific options.
+      if (window.appState && window.appState.viewOptions) {
+        for (const key in window.appState.viewOptions) {
+          if (window.appState.viewOptions.hasOwnProperty(key)) {
+            this.viewOptions_[key] = window.appState.viewOptions[key];
           }
-        })
-        .catch(error => {
-          this.viewOptions_ = {};
-          console.error(error);
-        });
+        }
+      }
+    } catch (error) {
+      this.viewOptions_ = {};
+      console.error(error);
+    }
   }
 
   /**
@@ -133,7 +125,7 @@ export class AppStateController {
   /**
    * Saves current view option.
    */
-  saveViewOptions() {
+  async saveViewOptions() {
     const prefs = {
       sortField: this.fileListSortField_,
       sortDirection: this.fileListSortDirection_,
@@ -147,12 +139,7 @@ export class AppStateController {
     // Save the global default.
     const items = {};
     items[this.viewOptionStorageKey_] = JSON.stringify(prefs);
-    xfm.storage.local.set(items, () => {
-      if (chrome.runtime.lastError) {
-        console.error(
-            'Failed to save view options: ' + chrome.runtime.lastError.message);
-      }
-    });
+    xfm.storage.local.setAsync(items);
 
     // Save the window-specific preference.
     if (window.appState) {
@@ -164,7 +151,7 @@ export class AppStateController {
   /**
    * @private
    */
-  onFileListSorted_() {
+  async onFileListSorted_() {
     const currentDirectory = this.directoryModel_.getCurrentDirEntry();
     if (!currentDirectory) {
       return;
@@ -183,7 +170,7 @@ export class AppStateController {
   /**
    * @private
    */
-  onFileFilterChanged_() {
+  async onFileFilterChanged_() {
     const isAllAndroidFoldersVisible =
         this.directoryModel_.getFileFilter().isAllAndroidFoldersVisible();
     if (this.viewOptions_.isAllAndroidFoldersVisible !==
