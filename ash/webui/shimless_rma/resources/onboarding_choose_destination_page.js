d8fe34fbde8ed0eb28da4149a56d6c41933f3f30
Gavin Dodd <gavindodd@google.com>
2021-11-05 18:38:51 +0000

    shimless: i18n strings for choose destination page
    Bug: 1198187
    Test: Run local build and confirm text is correct
    Change-Id: I2ed749da9ce865a9da17e6fdfd3612ffd5fdc8bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261634
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#938870}

--- ash/webui/shimless_rma/resources/onboarding_choose_destination_page.js
+++ ash/webui/shimless_rma/resources/onboarding_choose_destination_page.js
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './shimless_rma_shared_css.js';
 import './base_page.js';
-
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-
 import '//resources/cr_elements/cr_radio_button/cr_radio_button.m.js';
 import '//resources/cr_elements/cr_radio_group/cr_radio_group.m.js';
+
+import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
+import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getShimlessRmaService} from './mojo_interface_provider.js';
 import {ShimlessRmaServiceInterface, StateResult} from './shimless_rma_types.js';
@@ -19,7 +19,18 @@ import {ShimlessRmaServiceInterface, StateResult} from './shimless_rma_types.js'
  * 'onboarding-choose-destination-page' allows user to select between preparing
  * the device for return to the original owner or refurbishing for a new owner.
  */
-export class OnboardingChooseDestinationPageElement extends PolymerElement {
+
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ * @implements {I18nBehaviorInterface}
+ */
+const OnboardingChooseDestinationPageBase =
+    mixinBehaviors([I18nBehavior], PolymerElement);
+
+/** @polymer */
+export class OnboardingChooseDestinationPageElement extends
+    OnboardingChooseDestinationPageBase {
   static get is() {
     return 'onboarding-choose-destination-page';
   }
@@ -30,7 +41,7 @@ export class OnboardingChooseDestinationPageElement extends PolymerElement {
 
   static get properties() {
     return {
-      /** @private */
+      /** @protected */
       destinationOwner_: {
         type: String,
         value: '',
