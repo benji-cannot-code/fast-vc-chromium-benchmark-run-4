f2d255a428d232455a14f3c02817186c2b5827ec
Sammie Quon <sammiequon@chromium.org>
2021-11-05 22:05:27 +0000

    desk_templates: Adds a browser test for testing native UI.
    Will be updated as the UI gets updated.
    
    Test: browser_tests DesksClient.NativeUIBasic
    Bug: 1266945
    Change-Id: Ic22a3b78c86a5e4ea4bdd91ad2304b3e075f04f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262599
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938972}

---
+++ ash/wm/desks/templates/desks_templates_test_util.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_DESKS_TEMPLATES_DESKS_TEMPLATES_TEST_UTIL_H_
+#define ASH_WM_DESKS_TEMPLATES_DESKS_TEMPLATES_TEST_UTIL_H_
+
+namespace views {
+class Button;
+}
+
+namespace ash {
+
+// These buttons are the ones on the primary root window.
+views::Button* GetZeroStateDesksTemplatesButton();
+views::Button* GetSaveDeskAsTemplateButton();
+
+}  // namespace ash
+
+#endif  // ASH_WM_DESKS_TEMPLATES_DESKS_TEMPLATES_TEST_UTIL_H_
