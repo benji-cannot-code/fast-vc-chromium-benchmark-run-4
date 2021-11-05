4aebe0322948aa7d9f325dbfe0a48cdbdbfcd17f
Anatoliy Potapchuk <apotapchuk@chromium.org>
2021-11-05 19:23:10 +0000

    [ESim] Upload EUICC status
    This CL adds core logic to upload the EUICC status to DMServer. It sends
    this information every time either the number of euiccs changes or an
    eSim profile is being provisioned.
    
    Bug: 1231305
    Change-Id: I2957eac468b38d12406cf65e2a2b78317e6f9e1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3219292
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938890}

--- chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.h
+++ chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.h
@@ -50,6 +50,7 @@ class PrefService;
 namespace policy {
 
 class DeviceCloudPolicyStoreAsh;
+class EuiccStatusUploader;
 class ForwardingSchemaRegistry;
 class HeartbeatScheduler;
 class ManagedSessionService;
@@ -211,6 +212,7 @@ class DeviceCloudPolicyManagerAsh : public CloudPolicyManager {
       machine_certificate_uploader_;
   std::unique_ptr<ash::attestation::AttestationPolicyObserver>
       attestation_policy_observer_;
+  std::unique_ptr<EuiccStatusUploader> euicc_status_uploader_;
 
   // Uploader for remote server unlock related lookup keys.
   std::unique_ptr<LookupKeyUploader> lookup_key_uploader_;
