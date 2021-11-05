dcf749a765aebc02aff987786563953dfb46f4c6
Azeem Arshad <azeemarshad@chromium.org>
2021-11-05 19:13:37 +0000

    [CrOSCellular] Add OnEuiccReset observer.
    This CL adds an OnEuiccReset method to the HermesEuiccClient Observer.
    This will be used by EuiccStatusUploader to trigger an EUICC status
    upload.
    
    Bug: 1231305
    Change-Id: I8399bb9d10a21011aae889cbbf3d5bef9a673d22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263566
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938885}

--- chromeos/dbus/hermes/hermes_euicc_client_unittest.cc
+++ chromeos/dbus/hermes/hermes_euicc_client_unittest.cc
@@ -127,6 +127,25 @@ void CopyInstallResult(HermesResponseStatus* dest_status,
   }
 }
 
+// Test observer for HermesEuiccClient.
+class TestHermesEuiccClientObserver : public HermesEuiccClient::Observer {
+ public:
+  TestHermesEuiccClientObserver() = default;
+  TestHermesEuiccClientObserver(const TestHermesEuiccClientObserver&) = delete;
+  ~TestHermesEuiccClientObserver() override = default;
+
+  void OnEuiccReset(const dbus::ObjectPath& euicc_path) override {
+    on_euicc_reset_calls_.push_back(euicc_path);
+  }
+
+  const std::vector<dbus::ObjectPath>& on_euicc_reset_calls() {
+    return on_euicc_reset_calls_;
+  }
+
+ private:
+  std::vector<dbus::ObjectPath> on_euicc_reset_calls_;
+};
+
 }  // namespace
 
 class HermesEuiccClientTest : public HermesClientTestBase {
@@ -147,6 +166,7 @@ class HermesEuiccClientTest : public HermesClientTestBase {
 
     HermesEuiccClient::Initialize(GetMockBus());
     client_ = HermesEuiccClient::Get();
+    client_->AddObserver(&test_observer_);
 
     base::RunLoop().RunUntilIdle();
   }
@@ -159,6 +179,7 @@ class HermesEuiccClientTest : public HermesClientTestBase {
   scoped_refptr<dbus::MockObjectProxy> proxy_;
 
   HermesEuiccClient* client_;
+  TestHermesEuiccClientObserver test_observer_;
 };
 
 TEST_F(HermesEuiccClientTest, TestInstallProfileFromActivationCode) {
@@ -365,6 +386,8 @@ TEST_F(HermesEuiccClientTest, TestResetMemory) {
       .WillRepeatedly(Invoke(this, &HermesEuiccClientTest::OnMethodCalled));
 
   HermesResponseStatus status;
+  const std::vector<dbus::ObjectPath>& on_euicc_reset_calls =
+      test_observer_.on_euicc_reset_calls();
 
   // Verify that client makes corresponding dbus method call with
   // correct arguments.
@@ -374,6 +397,8 @@ TEST_F(HermesEuiccClientTest, TestResetMemory) {
       base::BindOnce(&hermes_test_utils::CopyHermesStatus, &status));
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(status, HermesResponseStatus::kSuccess);
+  EXPECT_EQ(1u, on_euicc_reset_calls.size());
+  EXPECT_EQ(test_euicc_path, on_euicc_reset_calls.front());
 
   // Verify that error responses are returned properly.
   std::unique_ptr<dbus::ErrorResponse> error_response =
@@ -385,6 +410,7 @@ TEST_F(HermesEuiccClientTest, TestResetMemory) {
       base::BindOnce(&hermes_test_utils::CopyHermesStatus, &status));
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(status, HermesResponseStatus::kErrorUnknown);
+  EXPECT_EQ(1u, on_euicc_reset_calls.size());
 }
 
 }  // namespace chromeos
