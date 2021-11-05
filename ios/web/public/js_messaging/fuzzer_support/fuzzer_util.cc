14421408fc0779ab92d992195c804376c9c7cdaa
Zhaoyang Li <zhaoyangli@chromium.org>
2021-11-05 04:49:07 +0000

    [iOS][libFuzzer] Create libFuzzer target for iOS autofill tab helper
    To fuzz autofill::FormActivityTabHelper::OnFormMessageReceived API.
    
    Bug: 1256485
    Change-Id: I61561c7398b3e3931cee5a41472ae967ec243504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252810
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938663}

--- ios/web/public/js_messaging/fuzzer_support/fuzzer_util.cc
+++ ios/web/public/js_messaging/fuzzer_support/fuzzer_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/js_messaging/fuzzer_support/fuzzer_util.h"
 
 #include "base/json/json_reader.h"
+#include "base/logging.h"
 #include "base/values.h"
 #include "ios/web/public/js_messaging/fuzzer_support/js_message.pb.h"
 #include "ios/web/public/js_messaging/script_message.h"
@@ -20,10 +21,21 @@ std::unique_ptr<web::ScriptMessage> ProtoToScriptMessage(
   if (proto.has_url()) {
     url = GURL(proto.url());
   }
-  return std::make_unique<web::ScriptMessage>(
+  auto script_message = std::make_unique<web::ScriptMessage>(
       (body ? base::Value::ToUniquePtrValue(std::move(*body))
             : std::make_unique<base::Value>()),
       proto.user_interacting(), proto.main_frame(), url);
+
+  if (getenv("LPM_DUMP_NATIVE_INPUT")) {
+    LOG(WARNING) << "Body: " << *script_message->body();
+    LOG(WARNING) << "is_user_interacting: "
+                 << script_message->is_user_interacting();
+    LOG(WARNING) << "is_main_frame: " << script_message->is_main_frame();
+    absl::optional<GURL> request_url = script_message->request_url();
+    LOG(WARNING) << "request_url: "
+                 << (request_url ? request_url.value() : GURL());
+  }
+  return script_message;
 }
 
 }  // namespace fuzzer
