bd778c254c93ccd96ed74fdefbc7ba056ef9e5dc
Alice Wang <aliceywang@chromium.org>
2021-11-05 08:30:34 +0000

    [Android][FRE] Skip data reduction page when user clicks settings link
    This CL skips the data reduction page when the user clicks the settings
    link on the sync consent page.
    
    Bug: 1260275
    Change-Id: I5537dbd485bfbe488383c0ec94f57e906de0780a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256560
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938702}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -1491,7 +1491,7 @@ public class FirstRunIntegrationTest {
         }
 
         @Override
-        public boolean shouldShowDataReductionPage() {
+        public boolean shouldShowDataReductionPage(boolean openAdvancedSyncSettings) {
             return mTestCase.showDataSaverPromo();
         }
 
--- chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencerTest.java
@@ -76,7 +76,7 @@ public class FirstRunFlowSequencerTest {
         public boolean shouldShowSearchEnginePage;
 
         @Override
-        public boolean shouldShowDataReductionPage() {
+        public boolean shouldShowDataReductionPage(boolean openAdvancedSyncSettings) {
             return shouldShowDataReductionPage;
         }
 
