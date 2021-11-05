7d7049ef78dcdc337eeebfb2e3975cdca07586ad
Matthias Körber <koerber@google.com>
2021-11-05 02:37:49 +0000

    Adds finch parameter to control popup placement specifics
    This CL adds two finch parameters to control both the maximum pixels the
    popup is moving towards the field's center, and also the percentage of
    the field's width that controls what is considered as the center.
    
    Bug: 1248339
    Change-Id: I35f285e912d423e1719b040c00a0778fe6d05919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259301
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#938633}

--- chrome/browser/ui/views/autofill/autofill_popup_view_utils.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_utils.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/extensions/extension_popup.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/permission_bubble/permission_prompt_bubble_view.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/common/constants.h"
 #include "ui/base/interaction/element_tracker.h"
@@ -409,7 +410,8 @@ BubbleBorder::Arrow GetOptimalBubblePlacement(
     const gfx::Size& bubble_preferred_size,
     bool right_to_left,
     int scrollbar_width,
-    int maximum_offset_to_center,
+    int maximum_pixel_offset_to_center,
+    int maximum_width_percentage_to_center,
     gfx::Rect& bubble_bounds) {
   // Determine the best side of the element to put the bubble and get a
   // corresponding arrow.
@@ -439,10 +441,11 @@ BubbleBorder::Arrow GetOptimalBubblePlacement(
 
   // Move the content bounds towards to center of the field.
   // Note that for |right_to_left|, this will be a negative value.
-  bubble_bounds.Offset(
-      std::min(maximum_offset_to_center, element_bounds.width() / 2) *
-          (right_to_left ? -1 : 1),
-      0);
+  bubble_bounds.Offset(std::min(maximum_pixel_offset_to_center,
+                                maximum_width_percentage_to_center *
+                                    element_bounds.width() / 100) *
+                           (right_to_left ? -1 : 1),
+                       0);
 
   // In case the bubble the exceeds the right edge of the view port, move it
   // back until it completely fits.
