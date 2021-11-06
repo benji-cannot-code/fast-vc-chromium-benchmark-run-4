39c15eff51af32bf6008c565a72178fefd9d8a00
Eric Willigers <ericwilligers@chromium.org>
2021-11-06 22:09:48 +0000

    Extract base class WebAppsSyncTestBase
    We extract common logic for Desktop PWA sync tests into a base class
    WebAppsSyncTestBase.
    
    Change-Id: I17537a17cdf8da37ebbf89bc09a73d83e32d9e42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3264104
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939125}

---
+++ chrome/browser/sync/test/integration/web_apps_sync_test_base.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SYNC_TEST_INTEGRATION_WEB_APPS_SYNC_TEST_BASE_H_
+#define CHROME_BROWSER_SYNC_TEST_INTEGRATION_WEB_APPS_SYNC_TEST_BASE_H_
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/sync/test/integration/sync_test.h"
+
+namespace web_app {
+
+class WebAppsSyncTestBase : public SyncTest {
+ public:
+  explicit WebAppsSyncTestBase(TestType test_type);
+  ~WebAppsSyncTestBase() override;
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+}  // namespace web_app
+
+#endif  // CHROME_BROWSER_SYNC_TEST_INTEGRATION_WEB_APPS_SYNC_TEST_BASE_H_
