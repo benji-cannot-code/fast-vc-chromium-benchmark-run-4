a8ad46e3b7de1a2cbe93b5e26869de892eb26cc5
Juanmi <juanmihd@chromium.org>
2021-11-08 14:33:45 +0000

    Fixing null derreference in CopyToResourceProvider
    A recent change introduced a potential null derreference.
    This CL fixes that.
    
    Bug: 1267684
    Change-Id: I50fa9056745027c6cc3408c32a3b66a2392e6354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3267257
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939350}

--- third_party/blink/renderer/modules/webgpu/gpu_swap_chain.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_swap_chain.cc
@@ -174,6 +174,9 @@ scoped_refptr<StaticBitmapImage> GPUSwapChain::SnapshotInternal(
 
 bool GPUSwapChain::CopyToResourceProvider(
     CanvasResourceProvider* resource_provider) const {
+  if (!texture_)
+    return false;
+
   return CopyTextureToResourceProvider(texture_->GetHandle(), Size(),
                                        resource_provider);
 }
@@ -187,9 +190,6 @@ bool GPUSwapChain::CopyTextureToResourceProvider(
   DCHECK(resource_provider->GetSharedImageUsageFlags() &
          gpu::SHARED_IMAGE_USAGE_WEBGPU);
   DCHECK(resource_provider->IsOriginTopLeft());
-
-  if (!texture)
-    return false;
 
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> shared_context_wrapper =
       SharedGpuContext::ContextProviderWrapper();
