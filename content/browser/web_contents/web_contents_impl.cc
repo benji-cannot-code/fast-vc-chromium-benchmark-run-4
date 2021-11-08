1a2c0728e65e90726d494d92088e3e9e41d2ae32
John Abd-El-Malek <jam@chromium.org>
2021-11-08 21:26:29 +0000

    Delay spare renderer creation on Android until after a page stops loading.
    Currently it's being created as soon as the previous spare is used up.
    Trials on Android didn't show an overall improvement, so this new
    iteration creates the spare after a page finishes loading to see if this
    performs better.
    
    Bug: 1247767
    Change-Id: I76d2a0ebeefc19f92dedca11dc58f9ee5845ad40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263644
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939526}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -7279,6 +7279,13 @@ void WebContentsImpl::DidStopLoading() {
   }
 
   LoadingStateChanged(true, details.get());
+
+#if defined(OS_ANDROID)
+  if (base::FeatureList::IsEnabled(features::kSpareRenderer)) {
+    RenderProcessHostImpl::NotifySpareManagerAboutRecentlyUsedBrowserContext(
+        GetBrowserContext());
+  }
+#endif
 }
 
 void WebContentsImpl::DidChangeLoadProgress() {
