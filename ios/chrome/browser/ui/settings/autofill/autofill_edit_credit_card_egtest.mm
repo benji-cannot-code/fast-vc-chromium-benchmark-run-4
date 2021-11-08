1d9b720596a293b149edcf9d0d53f806f68a847c
Vidhan Jain <vidhanj@google.com>
2021-11-08 12:41:47 +0000

    Deflake testEmptyNickname
    Not sure if this would fix the issue but worth the try. Maybe
    grey_replaceText would make the keyboard to show up.
    
    Bug: 1266113
    Change-Id: I99ec740ac375f03c854c63c0fdfd33eb3120131e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263871
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#939319}

--- ios/chrome/browser/ui/settings/autofill/autofill_edit_credit_card_egtest.mm
+++ ios/chrome/browser/ui/settings/autofill/autofill_edit_credit_card_egtest.mm
@@ -114,7 +114,7 @@ id<GREYMatcher> YearOfExpiryTextField() {
   [self typeNickname:@"To be removed"];
 
   [[EarlGrey selectElementWithMatcher:NicknameTextField()]
-      performAction:grey_clearText()];
+      performAction:grey_replaceText(@"")];
 
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
       assertWithMatcher:grey_allOf(grey_sufficientlyVisible(), grey_enabled(),
