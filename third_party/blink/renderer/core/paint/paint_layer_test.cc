584a84e1e19afb8ad3e6549a072e0da6357b3c86
Morten Stenshorne <mstensho@chromium.org>
2021-11-08 10:19:39 +0000

    Disallow subsequence caching for fragmented boxes.
    Legacy block fragentation disallowed subsequence caching for everything
    inside a multicol container (by checking EnclosingPaginationLayer()). In
    NG block fragmentation we'll allow caching as long as the box only
    creates one fragment, but not otherwise.
    
    Update PaintLayerTest.SubsequenceCachingMuticol to give the target
    element some height, so that it actually fragments, so that
    SupportsSubsequenceCaching() will return false.
    
    Bug: 1265412
    Change-Id: Ib06c60b36e9355370e5b5a6a5473cef8384d85c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263420
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939282}

--- third_party/blink/renderer/core/paint/paint_layer_test.cc
+++ third_party/blink/renderer/core/paint/paint_layer_test.cc
@@ -1098,7 +1098,7 @@ TEST_P(PaintLayerTest, SubsequenceCachingSVG) {
 TEST_P(PaintLayerTest, SubsequenceCachingMuticol) {
   SetBodyInnerHTML(R"HTML(
     <div style='columns: 2'>
-      <div id='target' style='position: relative'></div>
+      <div id='target' style='position: relative; height: 20px;'></div>
     </div>
   )HTML");
 
