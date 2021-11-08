77fee3246ebb20854e6e4789f572b151d2f2daf5
Moe Ahmadi <mahmadi@chromium.org>
2021-11-08 22:15:13 +0000

    [memories] Adds option to dropdown menu to open visits in a tab group
    screenshot/4APdJnXgft29nGK
    
    Bug: 1266903
    Change-Id: I514d460e2ff739177fa05a9ce41c480083f15818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261246
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939546}

--- chrome/browser/resources/history/history_clusters/metrics_proxy.ts
+++ chrome/browser/resources/history/history_clusters/metrics_proxy.ts
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 export enum ClusterAction {
   DELETED = 'Deleted',
+  OPENED_IN_TAB_GROUP = 'OpenedInTabGroup',
   RELATED_SEARCH_CLICKED = 'RelatedSearchClicked',
   RELATED_VISITS_VISIBILITY_TOGGLED = 'RelatedVisitsVisibilityToggled',
   VISIT_CLICKED = 'RelatedSearchClicked',
