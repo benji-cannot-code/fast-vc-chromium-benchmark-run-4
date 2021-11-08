c662bd937c1b293c7d4b59056a042c9e6ff125a1
Andre Le <leandre@chromium.org>
2021-11-08 23:01:59 +0000

    NotificationRevamp: Fix screenshot notification.
    With the new refactor to NotificationViewBase's image_container_view_,
    CaptureModeNotificationView now only needs to create extra view in ctor
    and there's no need to have view observer. Removing these view observers
    also fixes the layout issue with extra view.
    
    This CL also added rounded corners for the banner view in the new
    notification.
    
    Bug: 1266583
    Change-Id: I890c26756cb62e457de754f88ebf1f332cc69ac8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268169
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939569}

--- ash/capture_mode/capture_mode_notification_view.h
+++ ash/capture_mode/capture_mode_notification_view.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/capture_mode/capture_mode_types.h"
 #include "ui/message_center/views/notification_view.h"
-#include "ui/views/view_observer.h"
 
 namespace ash {
 
@@ -17,8 +16,7 @@ namespace ash {
 // notification by either showing a banner on top of the notification image for
 // image captures, or a play icon on top of the video thumbnail.
 class ASH_EXPORT CaptureModeNotificationView
-    : public message_center::NotificationView,
-      public views::ViewObserver {
+    : public message_center::NotificationView {
  public:
   CaptureModeNotificationView(const message_center::Notification& notification,
                               CaptureModeType capture_type);
@@ -43,11 +41,6 @@ class ASH_EXPORT CaptureModeNotificationView
 
   // message_center::NotificationView:
   void Layout() override;
-
-  // views::ViewObserver:
-  void OnViewVisibilityChanged(View* observed_view,
-                               View* starting_view) override;
-  void OnViewIsDeleting(View* observed_view) override;
 
  private:
   void CreateExtraView();
--- ash/capture_mode/capture_mode_util.cc
+++ ash/capture_mode/capture_mode_util.cc
@@ -24,8 +24,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/message_center/views/notification_background_painter.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/layout/box_layout.h"
+
+namespace {
+constexpr int kBannerViewTopRadius = 0;
+constexpr int kBannerViewBottomRadius = 8;
+}  // namespace
 
 namespace ash {
 
@@ -159,7 +165,14 @@ std::unique_ptr<views::View> CreateBannerView() {
           views::BoxLayout::Orientation::kHorizontal,
           gfx::Insets(kBannerVerticalInsetDip, kBannerHorizontalInsetDip),
           kBannerIconTextSpacingDip));
-  banner_view->SetBackground(views::CreateSolidBackground(background_color));
+
+  if (features::IsNotificationsRefreshEnabled()) {
+    banner_view->SetBackground(views::CreateBackgroundFromPainter(
+        std::make_unique<message_center::NotificationBackgroundPainter>(
+            kBannerViewTopRadius, kBannerViewBottomRadius, background_color)));
+  } else {
+    banner_view->SetBackground(views::CreateSolidBackground(background_color));
+  }
 
   views::ImageView* icon =
       banner_view->AddChildView(std::make_unique<views::ImageView>());
