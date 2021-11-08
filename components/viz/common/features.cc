52d68a40fbbc6893359d2169b7b973875ce248e6
kylechar <kylechar@chromium.org>
2021-11-08 16:31:32 +0000

    Add feature to clip prewalk damage
    During the SurfaceAggregator damage prewalk the damage contributed by
    embedded surfaces and render passes was not clipped by either the
    DrawQuad::visible_rect contributing the damage or the
    CompositorRenderPass::output_rect where the quad is drawn. Add a flag
    that intersects damage with both bounding rects to ensure damage doesn't
    grow beyond where it makes sense.
    
    This should be a no-op until the feature is enabled via finch.
    
    Bug: 1267895
    Change-Id: I9f18eafc777d0b2c3dd9789edbf7399d5f9231fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3021047
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939388}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -183,6 +183,13 @@ bool IsAdpfEnabled() {
   return base::FeatureList::IsEnabled(kAdpf);
 }
 
+bool IsClipPrewalkDamageEnabled() {
+  static constexpr base::Feature kClipPrewalkDamage{
+      "ClipPrewalkDamage", base::FEATURE_DISABLED_BY_DEFAULT};
+
+  return base::FeatureList::IsEnabled(kClipPrewalkDamage);
+}
+
 bool IsOverlayPrioritizationEnabled() {
   return base::FeatureList::IsEnabled(kEnableOverlayPrioritization);
 }
