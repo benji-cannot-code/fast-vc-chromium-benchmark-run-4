b5c9aee5f34da9f7a1c780be99f374debf3d20cf
Johan Bay <jobay@chromium.org>
2021-11-08 14:27:42 +0000

    Add parentId and frameId to CDP AXNodes
    Bug: 1226486
    Change-Id: I85f9d20fbd45c473e553a6d5e6a5bbd2bd411533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251071
    Commit-Queue: Johan Bay <jobay@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939347}

--- third_party/blink/web_tests/http/tests/inspector-protocol/accessibility/accessibility-query-axtree.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/accessibility/accessibility-query-axtree.js
@@ -85,7 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const response = await dp.Accessibility.queryAXTree(
         {objectId: documentObjId, accessibleName: 'title'});
     for (const axnode of response.result.nodes) {
-      testRunner.log(axnode, null, ['nodeId', 'backendDOMNodeId', 'childIds']);
+      testRunner.log(axnode, null, ['nodeId', 'backendDOMNodeId', 'childIds', 'parentId']);
     }
   }
 
