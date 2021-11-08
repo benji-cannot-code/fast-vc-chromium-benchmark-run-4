9fb560cc17fcdb977bc353f74cb4dab69906debe
Titouan Rigoudy <titouan@chromium.org>
2021-11-08 17:01:40 +0000

    [Sheriff] Disable diagnostics tests under ASAN.
    These are failing with a stack-use-after-scope error.
    
    Bug: chromium:1267299
    Change-Id: I8eb7b7e745532612672bbcaa180ce92d489b80b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3265108
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Owners-Override: Titouan Rigoudy <titouan@google.com>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939399}

--- chrome/browser/ash/web_applications/diagnostics_app_integration_browsertest.cc
+++ chrome/browser/ash/web_applications/diagnostics_app_integration_browsertest.cc
@@ -114,8 +114,16 @@ IN_PROC_BROWSER_TEST_P(DiagnosticsAppIntegrationTest,
                                        0, 1);
 }
 
+// TODO(https://crbug.com/1267299): Re-enable.
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_DiagnosticsAppRecordsInitialScreenSystem \
+  DISABLED_DiagnosticsAppRecordsInitialScreenSystem
+#else
+#define MAYBE_DiagnosticsAppRecordsInitialScreenSystem \
+  DiagnosticsAppRecordsInitialScreenSystem
+#endif
 IN_PROC_BROWSER_TEST_P(DiagnosticsAppIntegrationTest,
-                       DiagnosticsAppRecordsInitialScreenSystem) {
+                       MAYBE_DiagnosticsAppRecordsInitialScreenSystem) {
   // Launch Diagnostics at System UI.
   LaunchDiagnosticsApp("chrome://diagnostics/?system");
 
@@ -124,8 +132,16 @@ IN_PROC_BROWSER_TEST_P(DiagnosticsAppIntegrationTest,
                                        0, 1);
 }
 
+// TODO(https://crbug.com/1267299): Re-enable.
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_DiagnosticsAppRecordsInitialScreenConnectivity \
+  DISABLED_DiagnosticsAppRecordsInitialScreenConnectivity
+#else
+#define MAYBE_DiagnosticsAppRecordsInitialScreenConnectivity \
+  DiagnosticsAppRecordsInitialScreenConnectivity
+#endif
 IN_PROC_BROWSER_TEST_P(DiagnosticsAppIntegrationTest,
-                       DiagnosticsAppRecordsInitialScreenConnectivity) {
+                       MAYBE_DiagnosticsAppRecordsInitialScreenConnectivity) {
   // Launch Diagnostics at Connectivity UI.
   LaunchDiagnosticsApp("chrome://diagnostics/?connectivity");
 
@@ -134,8 +150,16 @@ IN_PROC_BROWSER_TEST_P(DiagnosticsAppIntegrationTest,
                                        1, 1);
 }
 
+// TODO(https://crbug.com/1267299): Re-enable.
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_DiagnosticsAppRecordsInitialScreenInput \
+  DISABLED_DiagnosticsAppRecordsInitialScreenInput
+#else
+#define MAYBE_DiagnosticsAppRecordsInitialScreenInput \
+  DiagnosticsAppRecordsInitialScreenInput
+#endif
 IN_PROC_BROWSER_TEST_P(DiagnosticsAppIntegrationTest,
-                       DiagnosticsAppRecordsInitialScreenInput) {
+                       MAYBE_DiagnosticsAppRecordsInitialScreenInput) {
   // Launch Diagnostics at Input UI.
   LaunchDiagnosticsApp("chrome://diagnostics/?input");
 
