056d82ca585d445ca19e0b711a4ba1da460f1c3a
Corentin Pescheloche <cpescheloche@fb.com>
2021-11-08 21:49:47 +0000

    Add CpuProfiler size to memory-infra.v8.code_stats
    The goal of the PR is to add to telemetry a metric estimating the space
    occupied by the codemap retained by a CpuProfiler and its underlying
    CodeObserver.
    
    This metric will help us understand better the space consumed
    by a profiler in that scenario and will also help detect memory leaks.
    
    Bug: chromium:1241491
    Change-Id: I7aca427c42ac5ff6f1ed91255609df346baa2c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3224584
    Auto-Submit: Corentin Pescheloche <cpescheloche@fb.com>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939536}

--- gin/v8_isolate_memory_dump_provider_unittest.cc
+++ gin/v8_isolate_memory_dump_provider_unittest.cc
@@ -160,6 +160,7 @@ TEST_F(V8MemoryDumpProviderTest, DumpCodeStatistics) {
   bool did_dump_bytecode_size = false;
   bool did_dump_code_size = false;
   bool did_dump_external_scripts_size = false;
+  bool did_dump_cpu_profiler_metadata_size = false;
 
   for (const auto& name_dump : allocator_dumps) {
     const std::string& name = name_dump.first;
@@ -172,6 +173,8 @@ TEST_F(V8MemoryDumpProviderTest, DumpCodeStatistics) {
           did_dump_code_size = true;
         } else if (entry.name == "external_script_source_size") {
           did_dump_external_scripts_size = true;
+        } else if (entry.name == "cpu_profiler_metadata_size") {
+          did_dump_cpu_profiler_metadata_size = true;
         }
       }
     }
@@ -181,6 +184,7 @@ TEST_F(V8MemoryDumpProviderTest, DumpCodeStatistics) {
   ASSERT_TRUE(did_dump_bytecode_size);
   ASSERT_TRUE(did_dump_code_size);
   ASSERT_TRUE(did_dump_external_scripts_size);
+  ASSERT_TRUE(did_dump_cpu_profiler_metadata_size);
 }
 
 // Tests that a deterministic memory dump request performs a GC.
