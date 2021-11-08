0d7ae226cdbc885d2df9a683cbbd74a0941a9bc5
Joshua Pawlicki <waffles@chromium.org>
2021-11-08 18:44:50 +0000

    component updater: Support disabling component updates.
    Bug: 1222416
    Change-Id: Iec4d2be8c4cd4364ddd843b2a3156ff33667c4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242749
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939446}

--- components/component_updater/installer_policies/safety_tips_component_installer.cc
+++ components/component_updater/installer_policies/safety_tips_component_installer.cc
@@ -61,7 +61,7 @@ SafetyTipsComponentInstallerPolicy::~SafetyTipsComponentInstallerPolicy() =
 
 bool SafetyTipsComponentInstallerPolicy::
     SupportsGroupPolicyEnabledComponentUpdates() const {
-  return false;
+  return true;
 }
 
 bool SafetyTipsComponentInstallerPolicy::RequiresNetworkEncryption() const {
