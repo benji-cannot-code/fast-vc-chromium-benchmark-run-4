c11bc2820a9559033fefd2cce26bb0e292e65443
Timothy Loh <timloh@chromium.org>
2021-11-08 05:05:52 +0000

    Add flags for Crostini IME and VK support
    These flags do nothing yet, but will let us guard changes and avoid
    impacting regular users. VK support will require more work on top of
    the changes for supporting keyboard-based IMEs, and the separate flag
    allows us to avoid accidentally surfacing a broken VK.
    
    Bug: 1247564
    Change-Id: Ic049c283607d5a216f25df7abc5e9b3920264760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3267668
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Auto-Submit: Timothy Loh <timloh@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#939230}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2455,6 +2455,12 @@ extern const char kCrostiniUseLxd4Description[];
 extern const char kCrostiniMultiContainerName[];
 extern const char kCrostiniMultiContainerDescription[];
 
+extern const char kCrostiniImeSupportName[];
+extern const char kCrostiniImeSupportDescription[];
+
+extern const char kCrostiniVirtualKeyboardSupportName[];
+extern const char kCrostiniVirtualKeyboardSupportDescription[];
+
 extern const char kDisableBufferBWCompressionName[];
 extern const char kDisableBufferBWCompressionDescription[];
 
