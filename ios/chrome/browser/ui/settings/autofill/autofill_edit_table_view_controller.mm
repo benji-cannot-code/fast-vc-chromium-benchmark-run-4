a048d6c2e3e0afc6257582fedc81865b63501b7b
Sylvain Defresne <sdefresne@chromium.org>
2021-11-08 11:49:59 +0000

    [ios] Ensure returned NSIndexPath* is valid
    The method -indexForCellPathWithOffset:fromPath: could return
    an NSIndexPath* pointing to the first item in an empty section.
    Accessing the corresponding item would result in an out-of-bound
    access in an NSArray in the ListModel implementation.
    
    Check that the section is not empty when the NSIndexPath* is
    created. This should ensure that NSIndexPath* is valid.
    
    Fixed: 1266938
    Change-Id: I42e0780bf708bca93b9dfadff221fd90fa283b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263833
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939311}

--- ios/chrome/browser/ui/settings/autofill/autofill_edit_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/autofill/autofill_edit_table_view_controller.mm
@@ -149,17 +149,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!cellPath || !offset)
     return nil;
 
-  NSInteger cellSection = [cellPath section];
-  NSInteger nextCellRow = [cellPath row] + offset;
-
-  if (nextCellRow >= 0 && nextCellRow < [self.tableView
-                                            numberOfRowsInSection:cellSection])
+  const NSInteger cellSection = [cellPath section];
+  const NSInteger nextCellRow = [cellPath row] + offset;
+  if ((0 <= nextCellRow) &&
+      (nextCellRow < [self.tableView numberOfRowsInSection:cellSection])) {
     return [NSIndexPath indexPathForRow:nextCellRow inSection:cellSection];
+  }
 
-  NSInteger nextCellSection = cellSection + offset;
-  if (nextCellSection >= 0 &&
-      nextCellSection < [self.tableView numberOfSections])
+  const NSInteger nextCellSection = cellSection + offset;
+  if ((0 <= nextCellSection) &&
+      (nextCellSection < [self.tableView numberOfSections]) &&
+      ([self.tableView numberOfRowsInSection:nextCellSection] > 0)) {
     return [NSIndexPath indexPathForRow:0 inSection:nextCellSection];
+  }
 
   return nil;
 }
