3e88d35152aed358691becbf896bf863b129e6b6
Daniel Vogelheim <vogelheim@chromium.org>
2021-11-08 11:45:53 +0000

    Adapt ExecCommandWithTrustedTypes use counter to count only strings.
    Currently, ExecCommandWithTrustedTypes counts usage of
    execCommand("insertHTML",...) when Trusted Types is enabled, regardless
    of whether the argument is a string or a Trusted Type. This CL counts
    the error cases more strictly, after the sites affected in crbug.com/1255546 have
    reported that they're now using Trusted Types with execCommand.
    
    This CL by itself should not change any behaviour. The purpose is
    to ensure that the planned behaviour change does not cause
    compatibility problems.
    
    More details on this change in the bug, specifically comment 16.
    
    Bug: 1230567
    Change-Id: I181a6699f45fd3b6877777e1a293ba6c146fd86a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259737
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939310}

--- third_party/blink/renderer/core/editing/commands/document_exec_command.cc
+++ third_party/blink/renderer/core/editing/commands/document_exec_command.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/metrics/histogram_functions.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_union_string_trustedhtml.h"
 #include "third_party/blink/renderer/core/dom/events/scoped_event_queue.h"
 #include "third_party/blink/renderer/core/editing/commands/editing_commands_utilities.h"
 #include "third_party/blink/renderer/core/editing/commands/editor_command.h"
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/forms/text_control_element.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
+#include "third_party/blink/renderer/core/trustedtypes/trusted_html.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_types_util.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
@@ -62,8 +64,16 @@ EditorCommand GetCommand(Document* document, const String& command_name) {
 }  // namespace
 
 bool Document::execCommand(const String& command_name,
-                           bool,
+                           bool unused_bool,
                            const String& value,
+                           ExceptionState& exception_state) {
+  V8UnionStringOrTrustedHTML tmp(value);
+  return execCommand(command_name, unused_bool, &tmp, exception_state);
+}
+
+bool Document::execCommand(const String& command_name,
+                           bool,
+                           const V8UnionStringOrTrustedHTML* value,
                            ExceptionState& exception_state) {
   if (!IsHTMLDocument() && !IsXHTMLDocument()) {
     exception_state.ThrowDOMException(
@@ -98,13 +108,22 @@ bool Document::execCommand(const String& command_name,
 
   // Count how often the error condition of crbug.com/1230567 occurs.
   if (CodeUnitCompareIgnoringASCIICase(command_name, "insertHTML") == 0 &&
+      value && value->IsString() &&
       RequireTrustedTypesCheck(GetExecutionContext())) {
     UseCounter::Count(*this, WebFeature::kExecCommandWithTrustedTypes);
   }
 
+  // Extract the (unchecked) string from value.
+  // TODO(vogelheim): Once crbug.com/1230567 is resolved, this can be removed.
+  String string_value;
+  if (value->IsTrustedHTML())
+    string_value = value->GetAsTrustedHTML()->toString();
+  else if (value->IsString())
+    string_value = value->GetAsString();
+
   base::UmaHistogramSparse("WebCore.Document.execCommand",
                            editor_command.IdForHistogram());
-  return editor_command.Execute(value);
+  return editor_command.Execute(string_value);
 }
 
 bool Document::queryCommandEnabled(const String& command_name,
