becefd628931d3bd97ffde083681300b0d0e51a6
Xiaoling Bao <xiaolingbao@chromium.org>
2021-11-08 19:25:11 +0000

    Read legacy Keystone tickets directly from ticket store during migration.
    Bug: 1250524
    Change-Id: I9e65536f8e65e5c0be8dded2132e4545a634cf8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3264071
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939464}

--- chrome/updater/mac/setup/ks_tickets.h
+++ chrome/updater/mac/setup/ks_tickets.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_UPDATER_MAC_KEYSTONE_KS_TICKETS_H_
-#define CHROME_UPDATER_MAC_KEYSTONE_KS_TICKETS_H_
+#ifndef CHROME_UPDATER_MAC_SETUP_KS_TICKETS_H_
+#define CHROME_UPDATER_MAC_SETUP_KS_TICKETS_H_
 
 #import <Foundation/Foundation.h>
 
@@ -49,4 +49,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // CHROME_UPDATER_MAC_KEYSTONE_KS_TICKETS_H_
+#endif  // CHROME_UPDATER_MAC_SETUP_KS_TICKETS_H_
