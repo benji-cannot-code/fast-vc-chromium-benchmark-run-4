05c715c7ad7a78fe0a987c325c10fa3952c56424
Stephan Hartmann <stha09@googlemail.com>
2021-11-08 06:33:33 +0000

    IWYU: add algorithm for std::find in ui::WaylandGLEGLUtility
    Bug: 957519
    Change-Id: Ib07e51c9bb39a4c0fa298751af43dc5c1a4a2c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263953
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#939244}

--- ui/ozone/platform/wayland/gpu/wayland_gl_egl_utility.cc
+++ ui/ozone/platform/wayland/gpu/wayland_gl_egl_utility.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/gpu/wayland_gl_egl_utility.h"
 
+#include <algorithm>
+
 // From ANGLE's egl/eglext.h. Follows the same approach as in
 // ui/gl/gl_surface_egl.cc
 #ifndef EGL_ANGLE_platform_angle_device_type_swiftshader
