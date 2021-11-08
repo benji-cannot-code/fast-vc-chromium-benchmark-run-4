e4b97bf6b933f80118695ac51a0dab67bcaafe94
Alison Maher <almaher@microsoft.com>
2021-11-08 03:23:56 +0000

    [FlexNG] Remove should_cache_result
    As a result of CL:3261166, we no longer re-run the entire flex layout
    algorithm in each fragmentainer. This means that we no longer need
    the should_cache_result flag added in CL:3212348.
    
    No functional changes intended.
    
    Bug: 660611
    Change-Id: I4f8d10ad029d1ff609fac621e2752a0653090631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259263
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939219}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -3310,9 +3310,6 @@ void LayoutBox::SetCachedLayoutResult(
   DCHECK(!result->PhysicalFragment().BreakToken());
   DCHECK(To<NGPhysicalBoxFragment>(result->PhysicalFragment()).IsOnlyForNode());
 
-  if (!result->GetConstraintSpaceForCaching().ShouldCacheResult())
-    return;
-
   if (result->GetConstraintSpaceForCaching().CacheSlot() ==
       NGCacheSlot::kMeasure) {
     // We don't early return here, when setting the "measure" result we also
@@ -3513,9 +3510,6 @@ scoped_refptr<const NGLayoutResult> LayoutBox::CachedLayoutResult(
     NGLayoutCacheStatus* out_cache_status) {
   NOT_DESTROYED();
   *out_cache_status = NGLayoutCacheStatus::kNeedsLayout;
-
-  if (!new_space.ShouldCacheResult())
-    return nullptr;
 
   const bool use_layout_cache_slot =
       new_space.CacheSlot() == NGCacheSlot::kLayout &&
