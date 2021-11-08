55ecfd227a3a691e2b80d4b782097720bfd0c780
Shakti Sahu <shaktisahu@chromium.org>
2021-11-08 19:19:03 +0000

    Fixed NPE in AddToHomescreenIPHController
    This CL fixes an NPE that was caused due to trying to show IPH after
    the chrome activity has been destroyed.
    
    Bug: 1265236
    Change-Id: I89777c2f459d87b0e56163ffe6f5c563440ffb19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268306
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939462}

--- chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenIPHController.java
+++ chrome/browser/webapps/android/java/src/org/chromium/chrome/browser/webapps/AddToHomescreenIPHController.java
@@ -51,10 +51,10 @@ public class AddToHomescreenIPHController {
     private static final String VARIATION_KEY_USE_TEXT_BUBBLE = "use_text_bubble";
     private static final String VARIATION_KEY_USE_MESSAGE = "use_message";
 
-    private final Activity mActivity;
+    private Activity mActivity;
+    private AppMenuHandler mAppMenuHandler;
     private final WindowAndroid mWindowAndroid;
     private final ModalDialogManager mModalDialogManager;
-    private final AppMenuHandler mAppMenuHandler;
     private final @IdRes int mHighlightMenuItemId;
     private final Supplier<View> mMenuButtonView;
     private final MessageDispatcher mMessageDispatcher;
@@ -94,6 +94,7 @@ public class AddToHomescreenIPHController {
      * @param tab The current tab.
      */
     public void showAddToHomescreenIPH(Tab tab) {
+        if (mActivity == null) return;
         if (!canShowAddToHomescreenMenuItem(mActivity, tab)) return;
 
         if (ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
@@ -105,6 +106,14 @@ public class AddToHomescreenIPHController {
                            false)) {
             showMessageIPH(tab);
         }
+    }
+
+    /**
+     * Called to notify that the activity is in the process of being destroyed.
+     */
+    public void destroy() {
+        mActivity = null;
+        mAppMenuHandler = null;
     }
 
     private static boolean canShowAddToHomescreenMenuItem(Context context, Tab tab) {
@@ -186,7 +195,7 @@ public class AddToHomescreenIPHController {
     }
 
     private void onMessageAddButtonClicked(Tab tab) {
-        if (tab.isDestroyed()) return;
+        if (tab.isDestroyed() || mActivity == null) return;
 
         Bundle menuItemData = new Bundle();
         // Used for UMA.
@@ -204,10 +213,12 @@ public class AddToHomescreenIPHController {
     }
 
     private void turnOnTextBubbleHighlightForMenuItem() {
+        if (mAppMenuHandler == null) return;
         mAppMenuHandler.setMenuHighlight(mHighlightMenuItemId);
     }
 
     private void turnOffTextBubbleHighlightForMenuItem() {
+        if (mAppMenuHandler == null) return;
         mAppMenuHandler.clearMenuHighlight();
     }
 }
