5c47cac6e3b81501e2c0fc25a4a210a495b7293d
Eric Lawrence [MSFT] <ericlaw@microsoft.com>
2021-11-09 20:23:54 +0000

    Remove FTP from UKM support
    Chrome no longer supports FTP. Because it's no longer possible to load
    content from FTP pages, it's no longer possible to collect UKM from
    ftp://-sourced content.
    
    Bug: 333943
    Change-Id: Ie2ff980824fd02f5d2da8659b20bfc49b8335170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268622
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#939978}

--- components/ukm/ukm_service_unittest.cc
+++ components/ukm/ukm_service_unittest.cc
@@ -1249,13 +1249,13 @@ TEST_F(UkmServiceTest, SupportedSchemes) {
   } test_cases[] = {
       {"http://google.ca/", true},
       {"https://google.ca/", true},
-      {"ftp://google.ca/", true},
       {"about:blank", true},
       {"chrome://version/", true},
       {"app://play/abcdefghijklmnopqrstuvwxyzabcdef/", true},
       // chrome-extension are controlled by TestIsWebstoreExtension, above.
       {"chrome-extension://bhcnanendmgjjeghamaccjnochlnhcgj/", true},
       {"chrome-extension://abcdefghijklmnopqrstuvwxyzabcdef/", false},
+      {"ftp://google.ca/", false},
       {"file:///tmp/", false},
       {"abc://google.ca/", false},
       {"www.google.ca/", false},
@@ -1308,12 +1308,12 @@ TEST_F(UkmServiceTest, SupportedSchemesNoExtensions) {
   } test_cases[] = {
       {"http://google.ca/", true},
       {"https://google.ca/", true},
-      {"ftp://google.ca/", true},
       {"about:blank", true},
       {"chrome://version/", true},
       {"app://play/abcdefghijklmnopqrstuvwxyzabcdef/", true},
       {"chrome-extension://bhcnanendmgjjeghamaccjnochlnhcgj/", false},
       {"chrome-extension://abcdefghijklmnopqrstuvwxyzabcdef/", false},
+      {"ftp://google.ca/", false},
       {"file:///tmp/", false},
       {"abc://google.ca/", false},
       {"www.google.ca/", false},
@@ -1389,7 +1389,6 @@ TEST_F(UkmServiceTest, SanitizeChromeUrlParams) {
       {"chrome://histograms/Variations", "chrome://histograms/Variations"},
       {"http://google.ca/?foo=bar", "http://google.ca/?foo=bar"},
       {"https://google.ca/?foo=bar", "https://google.ca/?foo=bar"},
-      {"ftp://google.ca/?foo=bar", "ftp://google.ca/?foo=bar"},
       {"chrome-extension://bhcnanendmgjjeghamaccjnochlnhcgj/foo.html?a=b",
        "chrome-extension://bhcnanendmgjjeghamaccjnochlnhcgj/"},
   };
