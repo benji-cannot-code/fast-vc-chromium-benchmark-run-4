51ac7262a5b1a856cfaa1e28ea70a21a4bd56836
Rafał Godlewski <rgod@google.com>
2021-11-09 22:22:22 +0000

    [Android] Make leak detection pref modifiable by signed out users
    Allows modifying leak detection toggle by signed out users.
    This is currently hidden by a feature flag.
    Keeps the same string logic:
    * signed out users with feature enabled (http://screen/4ZUAnBvEHkQgCob)
    * signed out users with feature disabled (http://screen/7DoVBAFnaviGpbP)
    
    Bug: 1261835
    Change-Id: Ida43ac65a74c8a10c061521b3c2ba766e9100ae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259475
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#940031}

--- chrome/browser/safe_browsing/android/safe_browsing_bridge.cc
+++ chrome/browser/safe_browsing/android/safe_browsing_bridge.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // can't depend on it.
 #include "chrome/browser/signin/identity_manager_factory.h"  // nogncheck
 #include "components/password_manager/core/browser/leak_detection/leak_detection_check_impl.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/content/common/file_type_policies.h"
@@ -83,6 +84,12 @@ static jboolean JNI_SafeBrowsingBridge_HasAccountForLeakCheckRequest(
           ProfileManager::GetLastUsedProfile());
   return password_manager::LeakDetectionCheckImpl::HasAccountForRequest(
       identity_manager);
+}
+
+static jboolean JNI_SafeBrowsingBridge_IsLeakDetectionUnauthenticatedEnabled(
+    JNIEnv* env) {
+  return base::FeatureList::IsEnabled(
+      password_manager::features::kLeakDetectionUnauthenticated);
 }
 
 }  // namespace safe_browsing
