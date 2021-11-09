27c06851d1dad99a25a2612dcc4b62c3666ead8a
Istiaque Ahmed <lazyboy@chromium.org>
2021-11-09 00:39:38 +0000

    [Extensions] Remove some BrowserContext* param from RunFunction*
    The *test_utils::RunFunction* methods that take
    ExtensionFunctionDispatcher as
    param already shouldn't require a separate BrowserContext* param.
    Moreover, those don't even use the param.
    
    This CL removes those params as that cleans up those methods a bit.
    
    Bug: None
    Change-Id: I41578407ed6f3a9d9b210fbd5e65aedaa7cf8220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3266628
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939627}

--- extensions/browser/api_test_utils.h
+++ extensions/browser/api_test_utils.h
@@ -86,7 +86,6 @@ std::unique_ptr<base::Value> RunFunctionWithDelegateAndReturnSingleResult(
 std::unique_ptr<base::Value> RunFunctionWithDelegateAndReturnSingleResult(
     scoped_refptr<ExtensionFunction> function,
     std::unique_ptr<base::ListValue> args,
-    content::BrowserContext* context,
     std::unique_ptr<ExtensionFunctionDispatcher> dispatcher,
     RunFunctionFlags flags);
 
@@ -128,12 +127,10 @@ bool RunFunction(ExtensionFunction* function,
                  content::BrowserContext* context);
 bool RunFunction(ExtensionFunction* function,
                  const std::string& args,
-                 content::BrowserContext* context,
                  std::unique_ptr<ExtensionFunctionDispatcher> dispatcher,
                  RunFunctionFlags flags);
 bool RunFunction(ExtensionFunction* function,
                  std::unique_ptr<base::ListValue> args,
-                 content::BrowserContext* context,
                  std::unique_ptr<ExtensionFunctionDispatcher> dispatcher,
                  RunFunctionFlags flags);
 
