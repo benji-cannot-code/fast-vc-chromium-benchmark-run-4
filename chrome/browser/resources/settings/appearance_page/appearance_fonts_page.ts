1288bb6d316207429a7507711115410e9180bb4a
dpapad <dpapad@chromium.org>
2021-11-09 19:13:37 +0000

    Settings TypeScript: Migrate appearance_page/ tests.
    Bug: 1263610
    Change-Id: I855605b2931a879e9c284de2ecb3768066466cc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268788
    Auto-Submit: dpapad <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939942}

--- chrome/browser/resources/settings/appearance_page/appearance_fonts_page.ts
+++ chrome/browser/resources/settings/appearance_page/appearance_fonts_page.ts
@@ -35,13 +35,17 @@ function ticksWithLabels(ticks: number[]): SliderTick[] {
  * settings.
  */
 
-interface SettingsAppearanceFontsPageElement {
+export interface SettingsAppearanceFontsPageElement {
   $: {
+    fixedFontPreview: HTMLElement,
     minimumSizeFontPreview: HTMLElement,
+    sansSerifFontPreview: HTMLElement,
+    serifFontPreview: HTMLElement,
+    standardFontPreview: HTMLElement,
   };
 }
 
-class SettingsAppearanceFontsPageElement extends PolymerElement {
+export class SettingsAppearanceFontsPageElement extends PolymerElement {
   static get is() {
     return 'settings-appearance-fonts-page';
   }
@@ -84,6 +88,7 @@ class SettingsAppearanceFontsPageElement extends PolymerElement {
     ];
   }
 
+  prefs: Object;
   private fontOptions_: DropdownMenuOptionList;
   private fontSizeRange_: SliderTick[];
   private minimumFontSizeRange_: SliderTick[];
@@ -114,6 +119,12 @@ class SettingsAppearanceFontsPageElement extends PolymerElement {
 
   private onMinimumSizeChange_() {
     this.$.minimumSizeFontPreview.hidden = this.computeMinimumFontSize_() <= 0;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'settings-appearance-fonts-page': SettingsAppearanceFontsPageElement;
   }
 }
 
--- chrome/browser/resources/settings/appearance_page/home_url_input.ts
+++ chrome/browser/resources/settings/appearance_page/home_url_input.ts
@@ -20,7 +20,7 @@ import {PrefControlMixin} from '../controls/pref_control_mixin.js';
 
 import {AppearanceBrowserProxy, AppearanceBrowserProxyImpl} from './appearance_browser_proxy.js';
 
-interface HomeUrlInputElement {
+export interface HomeUrlInputElement {
   $: {
     input: CrInputElement,
   };
@@ -30,7 +30,7 @@ const HomeUrlInputElementBase =
     CrPolicyPrefMixin(PrefControlMixin(PolymerElement)) as
     {new (): PolymerElement & CrPolicyPrefMixinInterface};
 
-class HomeUrlInputElement extends HomeUrlInputElementBase {
+export class HomeUrlInputElement extends HomeUrlInputElementBase {
   static get is() {
     return 'home-url-input';
   }
@@ -162,6 +162,12 @@ class HomeUrlInputElement extends HomeUrlInputElementBase {
     this.browserProxy_.validateStartupPage(this.value).then(isValid => {
       this.invalid = !isValid;
     });
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'home-url-input': HomeUrlInputElement;
   }
 }
 
