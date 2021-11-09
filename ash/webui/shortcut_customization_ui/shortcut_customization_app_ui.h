73060d5db32ef477a8694e6fad94e53460c61ffe
Jimmy Gong <jimmyxgong@google.com>
2021-11-09 07:51:49 +0000

    shortcuts: Add initial ShortcutCustomization mojom
    This mojom interface provides a message pipe between the Shortcut
    Customization SWA and the c++ handler. Followup CL will use the
    real mojom implementation instead of the fake mojom provide in
    the UI.
    
    Bug: 1060690
    Test: ash_webui_unittests
    Change-Id: Ic058edaea3616d663b245f856c70903790728c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3265146
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939727}

--- ash/webui/shortcut_customization_ui/shortcut_customization_app_ui.h
+++ ash/webui/shortcut_customization_ui/shortcut_customization_app_ui.h
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WEBUI_SHORTCUT_CUSTOMIZATION_UI_SHORTCUT_CUSTOMIZATION_APP_UI_H_
 #define ASH_WEBUI_SHORTCUT_CUSTOMIZATION_UI_SHORTCUT_CUSTOMIZATION_APP_UI_H_
 
+#include <memory>
+
+#include "ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.h"
+#include "ash/webui/shortcut_customization_ui/mojom/shortcut_customization.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/webui/mojo_web_ui_controller.h"
 
 namespace content {
@@ -21,6 +26,13 @@ class ShortcutCustomizationAppUI : public ui::MojoWebUIController {
   ShortcutCustomizationAppUI& operator=(const ShortcutCustomizationAppUI&) =
       delete;
   ~ShortcutCustomizationAppUI() override;
+
+ private:
+  void BindInterface(
+      mojo::PendingReceiver<
+          shortcut_customization::mojom::AcceleratorConfigurationProvider>
+          receiver);
+  std::unique_ptr<shortcut_ui::AcceleratorConfigurationProvider> provider_;
 };
 
 }  // namespace ash
