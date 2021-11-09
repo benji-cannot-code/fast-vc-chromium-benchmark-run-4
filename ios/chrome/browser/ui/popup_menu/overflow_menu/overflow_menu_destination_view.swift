5c93744582ec65c25f8259bacdea812ad9c6e697
Robbie Gibson <rkgibson@google.com>
2021-11-09 16:24:41 +0000

    [iOS] Adjust overflow menu destination item width based on menu width
    This CL uses a SwiftUI GeometryReader to read the view's width.
    
    Bug: 1254379
    Change-Id: I0a39d79aa8bb32c1df6aeb712b39017a8b80e768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268685
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939860}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_destination_view.swift
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_destination_view.swift
@@ -23,16 +23,15 @@ struct OverflowMenuDestinationView: View {
   /// The spacing to either side of each icon.
   var iconSpacing: CGFloat
 
+  /// The padding around the icon and inside the white background.
+  var iconPadding: CGFloat
+
   var body: some View {
     VStack {
-      ZStack(alignment: .center) {
-        Rectangle()
-          .foregroundColor(.cr_groupedSecondaryBackground)
-          .frame(width: Dimensions.iconSize, height: Dimensions.iconSize)
-          .cornerRadius(Dimensions.cornerRadius)
-        destination.image
-      }
-      .padding([.leading, .trailing], iconSpacing)
+      destination.image
+        .padding(iconPadding)
+        .background(iconBackground)
+        .padding([.leading, .trailing], iconSpacing)
 
       Text(destination.name)
         .font(.caption2)
@@ -40,5 +39,10 @@ struct OverflowMenuDestinationView: View {
     }
     .contentShape(Rectangle())
     .onTapGesture(perform: destination.handler)
+  }
+
+  var iconBackground: some View {
+    RoundedRectangle(cornerRadius: Dimensions.cornerRadius)
+      .foregroundColor(.cr_groupedSecondaryBackground)
   }
 }
