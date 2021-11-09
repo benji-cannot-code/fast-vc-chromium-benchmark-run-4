9586f6aa0fc90428944cde9a0331b01213dfadd3
David Tseng <dtseng@google.com>
2021-11-09 00:30:43 +0000

    Fixes Mac accessibility crash related to tables
    On Mac, extra nodes are added within tables due to Mac's desire for row
    and column nodes both pointing to the same cell node(s). This forms an
    undirected cyclic graph (though it is a directed acyclic graph).
    
    This leads to unexpected corner cases since the way in which the nodes
    are attached is via directly calling the last step of an
    AXTree::Unserialize (atomic update finished). This skips all other steps
    of unserialization, which in part, also calls
    AXTreeObserver::On*Changed. This is used by AXEventGenerator for firing
    generated events.
    
    The fix here is to skip AXEventGenerator::AddEvent by checking whether
    any associated event data is populated. This occurred as a result of
    https://chromium-review.googlesource.com/c/chromium/src/+/3263318 which
    also exposed the issue. Firing events on these nodes would happen twice
    if done correctly, which is something we would not want.
    
    R=abigailbklein@chromium.org
    
    Fixed: 1267772
    Change-Id: Ida488107814200a1c9737e711badfbb8d8426b76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268304
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939614}

--- ui/accessibility/ax_table_info.cc
+++ ui/accessibility/ax_table_info.cc
@@ -438,9 +438,8 @@ void AXTableInfo::UpdateExtraMacNodes() {
 
   changes.push_back(AXTreeObserver::Change(
       table_node_, AXTreeObserver::ChangeType::NODE_CHANGED));
-  for (AXTreeObserver& observer : tree_->observers()) {
-    observer.OnAtomicUpdateFinished(tree_, false, changes);
-  }
+  for (AXTreeObserver& observer : tree_->observers())
+    observer.OnAtomicUpdateFinished(tree_, /* root_changed= */ false, changes);
 }
 
 AXNode* AXTableInfo::CreateExtraMacColumnNode(size_t col_index) {
@@ -552,7 +551,7 @@ AXTableInfo::~AXTableInfo() {
     ClearExtraMacNodes();
     for (AXTreeObserver& observer : tree_->observers()) {
       observer.OnAtomicUpdateFinished(
-          tree_, false,
+          tree_, /* root_changed= */ false,
           {{table_node_, AXTreeObserver::ChangeType::NODE_CHANGED}});
     }
   }
