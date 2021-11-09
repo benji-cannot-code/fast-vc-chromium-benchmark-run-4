dab834938eb576c51c2858a338c28dc1dba14548
Danil Somsikov <dsv@chromium.org>
2021-11-09 21:01:23 +0000

    Propagate from_devtools_debugger in gestures.
    Bug: 1117173
    Change-Id: I1f093a97aec0bb898afca66d27404af79670f1ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259636
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940000}

--- content/browser/renderer_host/input/synthetic_touchpad_pinch_gesture.cc
+++ content/browser/renderer_host/input/synthetic_touchpad_pinch_gesture.cc
@@ -87,7 +87,10 @@ void SyntheticTouchpadPinchGesture::ForwardGestureEvents(
       target->DispatchInputEventToPlatform(
           blink::SyntheticWebGestureEventBuilder::Build(
               blink::WebGestureEvent::Type::kGesturePinchBegin,
-              blink::WebGestureDevice::kTouchpad));
+              blink::WebGestureDevice::kTouchpad,
+              params_.from_devtools_debugger
+                  ? blink::WebInputEvent::kFromDebugger
+                  : blink::WebInputEvent::kNoModifiers));
       state_ = IN_PROGRESS;
       break;
     case IN_PROGRESS: {
@@ -101,13 +104,19 @@ void SyntheticTouchpadPinchGesture::ForwardGestureEvents(
       target->DispatchInputEventToPlatform(
           blink::SyntheticWebGestureEventBuilder::BuildPinchUpdate(
               incremental_scale, params_.anchor.x(), params_.anchor.y(),
-              0 /* modifierFlags */, blink::WebGestureDevice::kTouchpad));
+              params_.from_devtools_debugger
+                  ? blink::WebInputEvent::kFromDebugger
+                  : blink::WebInputEvent::kNoModifiers,
+              blink::WebGestureDevice::kTouchpad));
 
       if (HasReachedTarget(event_timestamp)) {
         target->DispatchInputEventToPlatform(
             blink::SyntheticWebGestureEventBuilder::Build(
                 blink::WebGestureEvent::Type::kGesturePinchEnd,
-                blink::WebGestureDevice::kTouchpad));
+                blink::WebGestureDevice::kTouchpad,
+                params_.from_devtools_debugger
+                    ? blink::WebInputEvent::kFromDebugger
+                    : blink::WebInputEvent::kNoModifiers));
         state_ = DONE;
       }
       break;
