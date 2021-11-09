b05e2fff2a5cdb80ae7ca60a7a71487f059e4559
Ben Kelly <wanderview@chromium.org>
2021-11-09 15:48:30 +0000

    Fetch: Plumb navigation redirect chain through service workers
    Navigation redirection works differently than normal redirection.
    Navigation requests are made using "manual" redirect mode which means
    the redirect is not immediately followed.  Instead the redirect location
    is handed back up to the NavigationURLLoaderImpl which then manually
    follows the redirect.  This results in a new request being sent for each
    step in the redirect chain.
    
    This CL plumbs the redirect chain information from
    NavigationURLLoaderImpl down through each request so it can be included
    with requests proxied by a passthrough service worker.
    
    For more detailed information about these changes please see the
    internal design doc at:
    
    https://docs.google.com/document/d/1KZscujuV7bCFEnzJW-0DaCPU-I40RJimQKoCcI0umTQ/edit?usp=sharing
    
    We have rough consensus to make this change in this spec issue:
    
    https://github.com/whatwg/fetch/issues/1335
    
    Note, this CL includes some expected test failures.  These are due to
    the "lax-allowing-unsafe" intervention that is currently enabled.  See:
    
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/web_tests/TestExpectations;l=4635;drc=e8133cbf2469adb99c6610483ab78bcfb8cc4c76
    
    Bug: 1115847,1241188
    Change-Id: I2a2a17639e0bec3222684e0d444d6d98a21402ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3213310
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939851}

--- third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/redirect.py
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/redirect.py
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from wptserve.utils import isomorphic_decode
+
 def main(request, response):
     if b'Status' in request.GET:
         status = int(request.GET[b"Status"])
@@ -7,7 +9,7 @@ def main(request, response):
 
     headers = []
 
-    url = request.GET[b'Redirect']
+    url = isomorphic_decode(request.GET[b'Redirect'])
     headers.append((b"Location", url))
 
     if b"ACAOrigin" in request.GET:
