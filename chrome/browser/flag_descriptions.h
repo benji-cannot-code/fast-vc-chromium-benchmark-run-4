0527b43a4ac42a5c847b0aa9d9c3f18513b9e6cf
Shubham Gupta <shubh.gupta@samsung.com>
2021-11-09 05:32:29 +0000

    Remove expired flag from chrome://flags
    The flag treat-unsafe-downloads-as-active-content is expired.
    This CL removes them from chrome://flags.
    
    Bug: 1188811
    Change-Id: I35345006e6a90b2de586fa69d88a7b2d2ad40a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3266657
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939706}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1466,9 +1466,6 @@ extern const char kTranslateIntentDescription[];
 extern const char kTreatInsecureOriginAsSecureName[];
 extern const char kTreatInsecureOriginAsSecureDescription[];
 
-extern const char kTreatUnsafeDownloadsAsActiveName[];
-extern const char kTreatUnsafeDownloadsAsActiveDescription[];
-
 extern const char kTrustTokensName[];
 extern const char kTrustTokensDescription[];
 
