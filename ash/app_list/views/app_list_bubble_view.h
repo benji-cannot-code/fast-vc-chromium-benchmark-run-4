a0095d1f5f38b4956137d0bbbb50b4775d74ef84
James Cook <jamescook@chromium.org>
2021-11-09 22:45:12 +0000

    Reland "ash: Add hide animation to bubble launcher"
    This reverts commit 7b901ee77059e6200bcb3a5e796680978328dfc3.
    
    Fixed the failure by explicitly aborting animations during shutdown.
    Previously ~AppListBubblePresenter() would call CloseNow() on the
    widget, which would abort the hide animation, which would
    reentrantly call CloseNow() and cause the crash.
    
    Original change's description:
    > Revert "ash: Add hide animation to bubble launcher"
    >
    > This reverts commit 8de1b5dbc6a1bddf69e5e9233d5070f7f2c9c6de.
    >
    > Reason for revert: AppListBubbleViewTest.ShutdownDuringHideAnimationDoesNotCrash is failing, see
    >
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/52468/overview
    >
    > Original change's description:
    > > ash: Add hide animation to bubble launcher
    > >
    > > The hide animation is a opacity fade-out and bounds shrink. Keep the
    > > views::Widget alive until the end of the animation, then CloseNow().
    > > This means the launcher is considered "showing" until the hide
    > > animation completes, so attempts to Show() during the animation will
    > > be blocked.
    > >
    > > As an optimization, hide the scroll view gradient mask before the
    > > animation. On kevin, official branded release build, this improves
    > > smoothness from 47 to 65, P=0.01 for a difference. 65 is not great,
    > > but the animation looks OK in practice. I think there might be a
    > > single jank, and the animation is so short (100ms = 6 frames) that
    > > any dropped frame really hurts the number.
    > >
    > > Eliminate ScopedScrollViewGradientDisabler in favor of creating and
    > > deleting the entire ScrollViewGradientHelper class. This simplifies
    > > removing the gradient mask during hide. All the views that can
    > > naturally own a Disabler are deleted at the end of the animation.
    > > With this approach we don't have to worry about a Disabler going out
    > > of scope and re-creating the mask layer.
    > >
    > > Bug: 1264178
    > > Test: added to ash_unittests
    > > Change-Id: Icca0cecfead48e7897c496548f39fa309632ada3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260773
    > > Commit-Queue: James Cook <jamescook@chromium.org>
    > > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > > Reviewed-by: Tony Yeoman <tby@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#939567}
    >
    > Bug: 1264178
    > Change-Id: I9f87b5d7712396f1c5c7aab958f54eee2be7d995
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3267762
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    > Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#939665}
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1264178
    Change-Id: I84c5652abe69a62dc6fe06b1a0093d16ce641ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3270691
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940039}

--- ash/app_list/views/app_list_bubble_view.h
+++ ash/app_list/views/app_list_bubble_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/app_list_folder_controller.h"
 #include "ash/ash_export.h"
 #include "ash/search_box/search_box_view_delegate.h"
+#include "base/callback_forward.h"
 #include "ui/views/view.h"
 
 namespace ash {
@@ -42,6 +43,13 @@ class ASH_EXPORT AppListBubbleView : public views::View,
 
   // Starts the bubble show animation.
   void StartShowAnimation();
+
+  // Starts the bubble hide animation.
+  void StartHideAnimation(base::RepeatingClosure on_animation_ended);
+
+  // Aborts all layer animations started by StartShowAnimation() or
+  // StartHideAnimation(). This invokes their cleanup callbacks.
+  void AbortAllAnimations();
 
   // Handles back action if it we have a use for it besides dismissing.
   bool Back();
