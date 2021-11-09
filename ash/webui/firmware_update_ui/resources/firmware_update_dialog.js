f98da0fafc4c0b02243f8b1fa03dd81915868044
Michael Checo <michaelcheco@google.com>
2021-11-09 18:50:32 +0000

    Firmware UI: Add update dialog
    Screenshot: http://shortn/_6hs8G0wpFA
    
    Bug: 1252981
    Change-Id: I647e7e52c7c12bc51efeeed6add274d4892592e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3249490
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939934}

--- ash/webui/firmware_update_ui/resources/firmware_update_dialog.js
+++ ash/webui/firmware_update_ui/resources/firmware_update_dialog.js
@@ -14,6 +14,7 @@ import {FirmwareUpdate} from './firmware_update_types.js';
 export const DialogState = {
   CLOSED: 0,
   DEVICE_PREP: 1,
+  UPDATING: 2,
 };
 
 /**
@@ -57,6 +58,16 @@ export class FirmwareUpdateDialogElement extends PolymerElement {
       this.update = e.detail.update;
       this.dialogState = DialogState.DEVICE_PREP;
     };
+
+    /**
+     * Event callback for 'open-update-dialog'.
+     * @param {!Event} e
+     * @private
+     */
+    this.openUpdateDialog_ = (e) => {
+      this.update = e.detail.update;
+      this.dialogState = DialogState.UPDATING;
+    };
   }
 
   /** @override */
@@ -65,6 +76,9 @@ export class FirmwareUpdateDialogElement extends PolymerElement {
 
     window.addEventListener(
         'open-device-prep-dialog', (e) => this.openDevicePrepDialog_(e));
+
+    window.addEventListener(
+        'open-update-dialog', (e) => this.openUpdateDialog_(e));
   }
 
   /**
@@ -78,6 +92,61 @@ export class FirmwareUpdateDialogElement extends PolymerElement {
   /** @protected */
   closeDialog_() {
     this.dialogState = DialogState.CLOSED;
+  }
+
+  /** @protected */
+  startUpdate_() {
+    // TODO(michaelcheco): Start update.
+    this.dialogState = DialogState.UPDATING;
+  }
+
+  /**
+   * @protected
+   * @return {boolean}
+   */
+  shouldShowUpdateDialog_() {
+    // TODO(michaelchheco): Update when 'UPDATE_DONE' is added to the
+    // |DialogState| enum.
+    return this.isUpdateInProgress_();
+  }
+
+  /**
+   * @protected
+   * @return {number}
+   */
+  computePercentageValue_() {
+    // TODO(michaelcheco): Dynamically update this when 'onProgressChanged'
+    // observer is implemented.
+    return 0;
+  }
+
+  /**
+   * @protected
+   * @return {boolean}
+   */
+  isUpdateInProgress_() {
+    return this.dialogState === DialogState.UPDATING;
+  }
+
+  /**
+   * @protected
+   * @return {string}
+   */
+  computeUpdateDialogTitle_() {
+    if (this.isUpdateInProgress_()) {
+      return `Updating ${this.update.deviceName}`;
+    }
+    return '';
+  }
+
+  /**
+   * @protected
+   * @return {string}
+   */
+  computeProgressText_() {
+    // TODO(michaelcheco): Dynamically update this when 'onProgressChanged'
+    // observer is implemented.
+    return 'Installing (1%)';
   }
 }
 
--- chrome/test/data/webui/chromeos/firmware_update/update_card_test.js
+++ chrome/test/data/webui/chromeos/firmware_update/update_card_test.js
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
-
 import {FirmwareUpdate, UpdatePriority} from 'chrome://accessory-update/firmware_update_types.js';
 import {UpdateCardElement} from 'chrome://accessory-update/update_card.js';
 
