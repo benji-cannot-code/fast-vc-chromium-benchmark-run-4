e05d13082adfc5e3da96959f9780b76f675cae05
Juyoung Kim <chattank05@gmail.com>
2021-11-09 00:30:38 +0000

    Remove unused #include "base/memory/ptr_util.h"
    This removes #include "base/memory/ptr_util.h" from
    ash/webui/file_manager/file_manager_ui.cc.
    
    Bug: 1206140
    
    Change-Id: I9c16352eb60a65032e490b5e9f98de6a2905b6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3266656
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939613}

--- ash/webui/file_manager/file_manager_ui.cc
+++ ash/webui/file_manager/file_manager_ui.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/file_manager/resources/grit/file_manager_swa_resources.h"
 #include "ash/webui/file_manager/resources/grit/file_manager_swa_resources_map.h"
 #include "ash/webui/file_manager/url_constants.h"
-#include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
