6a3ad3f0489560dea3d0b1f7ef4eb842371c74f3
David Dorwin <ddorwin@chromium.org>
2021-11-09 00:22:44 +0000

    [base][win] Rename DIR_APP_DATA to DIR_ROAMING_APP_DATA
    This value is used in GetDefaultRoamingUserDataDirectory() and
    implemented using CSIDL_APPDATA, which
    https://docs.microsoft.com/en-us/windows/win32/shell/csidl#CSIDL_APPDATA
    documents as being replaced with FOLDERID_RoamingAppData.
    The new name is also more consistent with DIR_LOCAL_APP_DATA and
    DIR_COMMON_APP_DATA and less ambiguous.
    
    The Mac/iOS DIR_APP_DATA key is untouched.
    
    Bug: 1266477
    Change-Id: I8f541f1e739da8bcb0ddca1c3cbd27ca79117204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3262369
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#939603}

--- chrome/updater/updater.cc
+++ chrome/updater/updater.cc
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace updater {
 namespace {
 
-// The log file is created in DIR_LOCAL_APP_DATA or DIR_APP_DATA.
+// The log file is created in DIR_LOCAL_APP_DATA or DIR_ROAMING_APP_DATA.
 void InitLogging(UpdaterScope updater_scope) {
   logging::LoggingSettings settings;
   const absl::optional<base::FilePath> log_dir =
