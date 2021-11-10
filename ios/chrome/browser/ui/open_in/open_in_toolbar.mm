21e54ab0f51f8c481b5fd4ef56fb67944b15ff82
Ewann <ewannpv@google.com>
2021-11-10 08:51:27 +0000

    [iOS] Renames method
    This CL renames updateViewConstraints to updateToolbarConstraints
    because updateViewConstraints is an instance method.
    
    Fixed: 1268431
    Change-Id: Ie3d54e072e7d70f4452dea3b314afef97700ed19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268758
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940232}

--- ios/chrome/browser/ui/open_in/open_in_toolbar.mm
+++ ios/chrome/browser/ui/open_in/open_in_toolbar.mm
@@ -112,7 +112,7 @@ const CGFloat kTopBorderHeight = 0.5f;
 }
 
 // Helper to update constraints.
-- (void)updateViewConstraints {
+- (void)updateToolbarConstraints {
   if (!self.superview)
     return;
   NamedGuide* guide = [NamedGuide guideWithName:kSecondaryToolbarGuide
@@ -139,12 +139,12 @@ const CGFloat kTopBorderHeight = 0.5f;
 
 - (void)didMoveToSuperview {
   [super didMoveToSuperview];
-  [self updateViewConstraints];
+  [self updateToolbarConstraints];
 }
 
 - (void)didMoveToWindow {
   [super didMoveToWindow];
-  [self updateViewConstraints];
+  [self updateToolbarConstraints];
 }
 
 @end
