7e2eb0f248bda580f24e8cc288a36c5269847af4
Joey Arhar <jarhar@chromium.org>
2021-11-10 17:13:08 +0000

    Add flat tree traversal check in DisplayLocking top layer logic
    The top layer logic this patch modifies was added in
    http://crrev.com/933004. Similar fixes to very similar clusterfuzz
    crashes were made in http://crrev.com/929719 and
    http://crrev.com/935108.
    
    Fixed: 1267276
    Change-Id: I210681d9f49203621edb1cb78a783adc3476d94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268092
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940363}

--- third_party/blink/renderer/core/display_lock/display_lock_context.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context.cc
@@ -216,7 +216,8 @@ bool DisplayLockContext::NeedsLifecycleNotifications() const {
   return needs_deferred_not_intersecting_signal_ ||
          render_affecting_state_[static_cast<int>(
              RenderAffectingState::kAutoStateUnlockedUntilLifecycle)] ||
-         has_pending_subtree_checks_ || has_pending_clear_has_top_layer_;
+         has_pending_subtree_checks_ || has_pending_clear_has_top_layer_ ||
+         has_pending_top_layer_check_;
 }
 
 void DisplayLockContext::UpdateLifecycleNotificationRegistration() {
@@ -856,10 +857,14 @@ void DisplayLockContext::WillStartLifecycleUpdate(const LocalFrameView& view) {
         RenderAffectingState::kAutoStateUnlockedUntilLifecycle)]);
   }
 
+  if (has_pending_subtree_checks_ || has_pending_top_layer_check_) {
+    DetermineIfSubtreeHasTopLayerElement();
+    has_pending_top_layer_check_ = false;
+  }
+
   if (has_pending_subtree_checks_) {
     DetermineIfSubtreeHasFocus();
     DetermineIfSubtreeHasSelection();
-    DetermineIfSubtreeHasTopLayerElement();
 
     has_pending_subtree_checks_ = false;
     update_registration = true;
@@ -919,6 +924,12 @@ void DisplayLockContext::DetachLayoutTree() {
     SetRequestedState(EContentVisibility::kVisible);
     blocked_child_recalc_change_ = StyleRecalcChange();
   }
+}
+
+void DisplayLockContext::ScheduleTopLayerCheck() {
+  has_pending_top_layer_check_ = true;
+  UpdateLifecycleNotificationRegistration();
+  ScheduleAnimation();
 }
 
 void DisplayLockContext::ScheduleAnimation() {
