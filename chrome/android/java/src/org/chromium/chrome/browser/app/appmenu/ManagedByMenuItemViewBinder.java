3772769abcd33e8e1c2c621c929dff2656697c18
Fabio Tirelo <ftirelo@chromium.org>
2021-11-10 16:26:23 +0000

    [Clank CBCM] Open chrome://management from "Managed by..." menu
    Requires chrome://management page to be enabled to allow the menu item
    to be clickable.
    
    Bug: 1266543
    Fixed: 1266543
    Change-Id: I54f3d8a5abccfc2a9f7059e146da6f09dd676b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259976
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940334}

--- chrome/android/java/src/org/chromium/chrome/browser/app/appmenu/ManagedByMenuItemViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/appmenu/ManagedByMenuItemViewBinder.java
@@ -15,9 +15,7 @@ import org.chromium.chrome.browser.ui.appmenu.CustomViewBinder;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
-/**
- * A custom binder used to bind the managed by menu item.
- */
+/** A custom binder used to bind the managed by menu item. */
 class ManagedByMenuItemViewBinder implements CustomViewBinder {
     private static final int MANAGED_BY_ITEM_VIEW_TYPE = 0;
 
@@ -49,7 +47,13 @@ class ManagedByMenuItemViewBinder implements CustomViewBinder {
             assert id == R.id.managed_by_menu_id;
 
             view.setId(id);
-            view.setFocusable(false);
+        } else if (key == AppMenuItemProperties.ENABLED) {
+            boolean enabled = model.get(AppMenuItemProperties.ENABLED);
+            view.setEnabled(enabled);
+            view.setFocusable(enabled);
+        } else if (key == AppMenuItemProperties.CLICK_HANDLER) {
+            view.setOnClickListener(
+                    v -> model.get(AppMenuItemProperties.CLICK_HANDLER).onItemClick(model));
         }
     }
 
