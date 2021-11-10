ca5ab46feb7ac7d9dcfb559ddc91479016bcb037
adamta <adamta@google.com>
2021-11-10 19:48:23 +0000

    [iOS] Don't reload NTP for incognito
    Bug: 1189129
    Change-Id: I376556db1041b9e29de870132afdfb46982fc01c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271599
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940438}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -448,6 +448,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)reload {
+  if (self.browser->GetBrowserState()->IsOffTheRecord()) {
+    return;
+  }
   ios::GetChromeBrowserProvider().GetDiscoverFeedProvider()->RefreshFeed();
   [self reloadContentSuggestions];
 }
