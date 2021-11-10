055191266c77d2939b318044175d1e30a1ba0b56
Benjamin Lerman <qsr@chromium.org>
2021-11-10 14:25:31 +0000

    [fuchsia] Implement AudioInputStream for Fuchsia
    The implementation delegates to the already existing
    FuchsiaAudioCapturerSource implementation.
    
    Bug: 1259976
    Test: Go to a web site testing audio input. Checks that calls to
          audio-core are made.
    
    Change-Id: I6310679f51fe8f931ac28392b1f3511bbe53611f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245096
    Commit-Queue: Benjamin Lerman <qsr@chromium.org>
    Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940297}

--- media/audio/fuchsia/audio_manager_fuchsia.cc
+++ media/audio/fuchsia/audio_manager_fuchsia.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/fuchsia/scheduler.h"
+#include "media/audio/fuchsia/audio_input_stream_fuchsia.h"
 #include "media/audio/fuchsia/audio_output_stream_fuchsia.h"
 #include "media/base/audio_timestamp_helper.h"
 #include "media/base/media_switches.h"
@@ -122,16 +123,16 @@ AudioInputStream* AudioManagerFuchsia::MakeLinearInputStream(
     const AudioParameters& params,
     const std::string& device_id,
     const LogCallback& log_callback) {
-  NOTREACHED();
-  return nullptr;
+  DCHECK_EQ(AudioParameters::AUDIO_PCM_LINEAR, params.format());
+  return MakeInputStream(params, device_id);
 }
 
 AudioInputStream* AudioManagerFuchsia::MakeLowLatencyInputStream(
     const AudioParameters& params,
     const std::string& device_id,
     const LogCallback& log_callback) {
-  NOTREACHED();
-  return nullptr;
+  DCHECK_EQ(AudioParameters::AUDIO_PCM_LOW_LATENCY, params.format());
+  return MakeInputStream(params, device_id);
 }
 
 std::unique_ptr<AudioManager> CreateAudioManager(
@@ -141,4 +142,15 @@ std::unique_ptr<AudioManager> CreateAudioManager(
                                                audio_log_factory);
 }
 
+AudioInputStream* AudioManagerFuchsia::MakeInputStream(
+    const AudioParameters& params,
+    const std::string& device_id) {
+  if (!device_id.empty() &&
+      device_id != AudioDeviceDescription::kDefaultDeviceId &&
+      device_id != AudioDeviceDescription::kLoopbackInputDeviceId) {
+    return nullptr;
+  }
+
+  return new AudioInputStreamFuchsia(this, params, device_id);
+}
 }  // namespace media
