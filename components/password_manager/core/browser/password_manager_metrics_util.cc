2f3e6d22d5c6af1b4ec597da48bf104eac398907
Marc Treib <treib@chromium.org>
2021-11-10 16:30:36 +0000

    Leak warning: Prefer "Change automatically" if available
    The Leak warning dialog has a few different variants wrt to the actions
    recommended to the user: It may offer Password Checkup, or offer
    changing the password automatically, or recommend the user to change
    the password manually.
    Before this CL, Password Checkup was preferred (if available). This CL
    changes the logic so that "Change automatically" will be preferred, if
    both are available.
    
    Bug: 1264320
    Change-Id: I06204b3ae1d70eef588786f9eb05ae991a4fe6df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268688
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940341}

--- components/password_manager/core/browser/password_manager_metrics_util.cc
+++ components/password_manager/core/browser/password_manager_metrics_util.cc
@@ -125,6 +125,8 @@ void LogLeakDialogTypeAndDismissalReason(LeakDialogType type,
         return "Change";
       case LeakDialogType::kCheckupAndChange:
         return "CheckupAndChange";
+      case LeakDialogType::kChangeAutomatically:
+        return "ChangeAutomatically";
     }
   };
 
