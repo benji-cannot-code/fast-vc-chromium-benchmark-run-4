f63d835050a73f22ef3d0b0071b226631ae360de
Angela Yoeurng <yoangela@chromium.org>
2021-11-10 00:30:35 +0000

    [settings] Add interaction metrics to the Search Engines settings page
    This CL Adds metrics reporting for interactions on
    chrome://settings/searchEngines. The metrics in this CL report when a
    user activates a search engine, deactivates a search engine, or changes
    their keyboard shortcut setting.
    
    Bug: 1213659, 1258797
    Change-Id: I74b6e55c86ae72406c2924f30fc436ed2b6be0b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3258629
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940078}

--- chrome/test/data/webui/settings/test_search_engines_browser_proxy.ts
+++ chrome/test/data/webui/settings/test_search_engines_browser_proxy.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-import {SearchEngine, SearchEnginesBrowserProxy, SearchEnginesInfo} from 'chrome://settings/settings.js';
+import {SearchEngine, SearchEnginesBrowserProxy, SearchEnginesInfo, SearchEnginesInteractions} from 'chrome://settings/settings.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 // clang-format on
 
@@ -27,6 +27,7 @@ export class TestSearchEnginesBrowserProxy extends TestBrowserProxy implements
       'setDefaultSearchEngine',
       'setIsActiveSearchEngine',
       'validateSearchEngineInput',
+      'recordSearchEnginesPageHistogram',
     ]);
 
     this.searchEnginesInfo_ =
@@ -38,7 +39,7 @@ export class TestSearchEnginesBrowserProxy extends TestBrowserProxy implements
   }
 
   setIsActiveSearchEngine(modelIndex: number, isActive: boolean) {
-    this.methodCalled('setDefaultSearchEngine', [modelIndex, isActive]);
+    this.methodCalled('setIsActiveSearchEngine', [modelIndex, isActive]);
   }
 
   removeSearchEngine(modelIndex: number) {
@@ -69,6 +70,10 @@ export class TestSearchEnginesBrowserProxy extends TestBrowserProxy implements
     return Promise.resolve(true);
   }
 
+  recordSearchEnginesPageHistogram(interaction: SearchEnginesInteractions) {
+    this.methodCalled('recordSearchEnginesPageHistogram', interaction);
+  }
+
   /**
    * Sets the response to be returned by |getSearchEnginesList|.
    */
@@ -77,24 +82,25 @@ export class TestSearchEnginesBrowserProxy extends TestBrowserProxy implements
   }
 }
 
-export function createSampleSearchEngine(
-    canBeDefault: boolean, canBeEdited: boolean,
-    canBeRemoved: boolean): SearchEngine {
-  return {
-    canBeDefault: canBeDefault,
-    canBeEdited: canBeEdited,
-    canBeRemoved: canBeRemoved,
-    canBeActivated: false,
-    canBeDeactivated: false,
-    default: false,
-    displayName: 'Google',
-    iconURL: 'http://www.google.com/favicon.ico',
-    id: 0,
-    isOmniboxExtension: false,
-    keyword: 'google.com',
-    modelIndex: 0,
-    name: 'Google',
-    url: 'https://search.foo.com/search?p=%s',
-    urlLocked: false,
-  };
+export function createSampleSearchEngine(override?: Partial<SearchEngine>):
+    SearchEngine {
+  return Object.assign(
+      {
+        canBeDefault: false,
+        canBeEdited: false,
+        canBeRemoved: false,
+        canBeActivated: false,
+        canBeDeactivated: false,
+        default: false,
+        displayName: 'Google',
+        iconURL: 'http://www.google.com/favicon.ico',
+        id: 0,
+        isOmniboxExtension: false,
+        keyword: 'google.com',
+        modelIndex: 0,
+        name: 'Google',
+        url: 'https://search.foo.com/search?p=%s',
+        urlLocked: false,
+      },
+      override || {});
 }
