45114435dc511a00a7b4d573c3574568e70fd3ad
Jeremy Roman <jbroman@chromium.org>
2021-11-10 20:47:50 +0000

    WTF: Skip UTF-8 conversion for known ASCII in CreateCanonicalUUIDString.
    Change-Id: I9d6751d4dd32c31e36e85028aee6dd84f86b1755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271591
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940475}

--- third_party/blink/renderer/platform/wtf/uuid.cc
+++ third_party/blink/renderer/platform/wtf/uuid.cc
@@ -11,8 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WTF {
 
 String CreateCanonicalUUIDString() {
-  std::string uuid = base::GenerateGUID();
-  return String::FromUTF8(uuid.data(), uuid.length());
+  String uuid(base::GenerateGUID());
+  DCHECK(uuid.IsLowerASCII());
+  return uuid;
 }
 
 bool IsValidUUID(const String& uuid) {
