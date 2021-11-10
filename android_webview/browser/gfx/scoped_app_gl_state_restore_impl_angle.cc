b1a8e69adb4979dd136e288b5e95212b70b52ce2
Bo Liu <boliu@chromium.org>
2021-11-10 21:56:21 +0000

    aw: Ignore GL draw to R8 framebuffer
    Android HWUI has a bug in a feature unrelated to webview that can ask
    webview draw functor to draw to a R8 framebuffer (for a second time in a
    frame). Ideal fix is android not call draw functor at all. However the
    bug is already shipped.
    
    For GL, use glGet to get the fbo channel bits to check for R8, and
    ignore the draw in that case.
    
    Bug: 1268900
    Change-Id: I2061d87703f1646bcca7532a622911f32a2abce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272236
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940504}

--- android_webview/browser/gfx/scoped_app_gl_state_restore_impl_angle.cc
+++ android_webview/browser/gfx/scoped_app_gl_state_restore_impl_angle.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
 
+#include "base/android/build_info.h"
 #include "base/native_library.h"
 #include "base/threading/thread_restrictions.h"
 #include "ui/gl/gl_context.h"
@@ -72,6 +73,20 @@ ScopedAppGLStateRestoreImplAngle::ScopedAppGLStateRestoreImplAngle(
   egl_context_ = os::eglGetCurrentContextFn();
   DCHECK_NE(egl_context_, EGL_NO_CONTEXT) << " no native context is current.";
 #endif
+
+  if (base::android::BuildInfo::GetInstance()->sdk_int() ==
+      base::android::SDK_VERSION_S) {
+    GLint red_bits = 0;
+    GLint green_bits = 0;
+    GLint blue_bits = 0;
+    GLint alpha_bits = 0;
+    os::glGetIntegervFn(GL_RED_BITS, &red_bits);
+    os::glGetIntegervFn(GL_GREEN_BITS, &green_bits);
+    os::glGetIntegervFn(GL_BLUE_BITS, &blue_bits);
+    os::glGetIntegervFn(GL_ALPHA_BITS, &alpha_bits);
+    skip_draw_ =
+        red_bits == 8 && green_bits == 0 && blue_bits == 0 && alpha_bits == 0;
+  }
 
   // Query |stencil_state_| with native GL API.
   // Android should have made a native EGL context current, so we can call GL
