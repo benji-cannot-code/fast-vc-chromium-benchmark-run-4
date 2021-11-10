234f1ea02f7d003e4c8c140a77d56e9143dd2b4b
K. Moon <kmoon@chromium.org>
2021-11-10 00:24:04 +0000

    [unseasoned-pdf] Implement remaining SkiaGraphics operations
    Implements SetLayerTransform() and SetScale() for SkiaGraphics. In
    particular, pinch-zoom uses SetLayerTransform() between rasters, and
    PaintManager uses SetScale() to set the device scale on resize.
    
    Now that SetScale() is implemented, the device scale should no longer be
    updated directly in PdfViewWebPlugin::OnViewportChanged(). While
    removing this code, the PaintSnapshotsWithVariousDeviceScales test
    revealed an issue with setting the device scale to 1.0 from some other
    device scale: PaintManager incorrectly assumed that setting a device
    scale of 1.0 was always a no-op.
    
    Bug: 1099020
    Change-Id: I559f3c2fe5aa04e8e096951f799840ff6084930d
    Fixed: 1266849
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268583
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940075}

--- pdf/pdf_view_web_plugin.h
+++ pdf/pdf_view_web_plugin.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_plugin.h"
 #include "third_party/blink/public/web/web_plugin_container.h"
 #include "third_party/blink/public/web/web_plugin_params.h"
+#include "ui/gfx/geometry/vector2d_f.h"
 #include "v8/include/v8.h"
 
 namespace blink {
@@ -255,6 +256,9 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
 
   // SkiaGraphics::Client:
   void UpdateSnapshot(sk_sp<SkImage> snapshot) override;
+  void UpdateScale(float scale) override;
+  void UpdateLayerTransform(float scale,
+                            const gfx::Vector2dF& translate) override;
 
   // PdfAccessibilityActionHandler:
   void EnableAccessibility() override;
@@ -305,6 +309,9 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
 
   bool InitializeCommon(std::unique_ptr<ContainerWrapper> container_wrapper,
                         std::unique_ptr<PDFiumEngine> engine);
+
+  // Recalculates values that depend on scale factors.
+  void UpdateScaledValues();
 
   void OnViewportChanged(const gfx::Rect& plugin_rect_in_css_pixel,
                          float new_device_scale);
@@ -371,13 +378,23 @@ class PdfViewWebPlugin final : public PdfViewPluginBase,
   v8::Persistent<v8::Object> scriptable_receiver_;
   PostMessageSender post_message_sender_;
 
+  // The current image snapshot.
   cc::PaintImage snapshot_;
+
+  // Translate from snapshot to device pixels.
+  gfx::Vector2dF snapshot_translate_;
+
+  // Scale from snapshot to device pixels.
+  float snapshot_scale_ = 1.0f;
 
   // The viewport coordinates to DIP (device-independent pixel) ratio.
   float viewport_to_dip_scale_ = 1.0f;
 
   // The device pixel to CSS pixel ratio.
   float device_to_css_scale_ = 1.0f;
+
+  // Combined translate from snapshot to device to CSS pixels.
+  gfx::Vector2dF total_translate_;
 
   // The plugin rect in CSS pixels.
   gfx::Rect css_plugin_rect_;
