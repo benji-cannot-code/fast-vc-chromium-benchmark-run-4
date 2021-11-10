51091b137fa7d038fb3e550300bc52343972480e
Toni Barzic <tbarzic@chromium.org>
2021-11-10 03:19:03 +0000

    Convert InvalidateLayout in search UI code back to Layout
    The CL that switched Layout -> InvalidateLayout is causing issues where
    apps container view, and search result page do not get laid out properly
    when results are updated during page transition (where
    ContentsView::Layout() returns early). For now, revert that change.
    
    Also, change order of destruction for ash_test_helper_ and
    test_views_delegate_ in BrowserWithTestWindowTest, so test view delegate
    gets destroyed after ash UI is reset in ash_test_helper_.TearDown(),
    so the test layout provider gets reset after ash views are destroyed.
    (the CL otherwise crashes during ash test helper teardown - results in
    app list UI get cleared when test app list model is reset, which causes
    some labels to try to access layout provider)
    
    BUG=1267832
    
    Change-Id: Ieacf1da36a8aa815df1828c3e359a9ec20fcc141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3267930
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940161}

--- ash/app_list/views/suggestion_chip_container_view.cc
+++ ash/app_list/views/suggestion_chip_container_view.cc
@@ -171,7 +171,7 @@ int SuggestionChipContainerView::DoUpdate() {
                                    notifier_results);
   }
 
-  InvalidateLayout();
+  Layout();
   return std::min(kMaxSuggestionChips, display_results.size());
 }
 
