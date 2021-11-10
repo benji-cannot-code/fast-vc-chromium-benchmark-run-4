2b8bf1f835f302413aa3adc35f00363cb651d88d
Dave Tapuska <dtapuska@chromium.org>
2021-11-10 20:40:32 +0000

    Move WebShareService to be a DocumentService
    ShareServiceImpl does not need to be a WebContentsObserver it can be
    a DocumentService directly.
    
    BUG=1252046
    
    Change-Id: Id96f7ef30105cab0fc588cf6429345f249024e87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272237
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940468}

--- chrome/browser/webshare/share_service_impl.cc
+++ chrome/browser/webshare/share_service_impl.cc
@@ -32,13 +32,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // //third_party/blink/renderer/modules/webshare/FILE_TYPES.md
 // //components/browser_ui/webshare/android/java/src/org/chromium/components/browser_ui/webshare/ShareServiceImpl.java
 
-ShareServiceImpl::ShareServiceImpl(content::RenderFrameHost& render_frame_host)
-    : content::WebContentsObserver(
-          content::WebContents::FromRenderFrameHost(&render_frame_host)),
+ShareServiceImpl::ShareServiceImpl(
+    content::RenderFrameHost* render_frame_host,
+    mojo::PendingReceiver<blink::mojom::ShareService> receiver)
+    : content::DocumentService<blink::mojom::ShareService>(render_frame_host,
+                                                           std::move(receiver))
 #if defined(OS_CHROMEOS)
-      sharesheet_client_(web_contents()),
+      ,
+      sharesheet_client_(
+          content::WebContents::FromRenderFrameHost(render_frame_host))
 #endif
-      render_frame_host_(&render_frame_host) {
+{
   DCHECK(base::FeatureList::IsEnabled(features::kWebShare));
 }
 
@@ -48,9 +52,8 @@ ShareServiceImpl::~ShareServiceImpl() = default;
 void ShareServiceImpl::Create(
     content::RenderFrameHost* render_frame_host,
     mojo::PendingReceiver<blink::mojom::ShareService> receiver) {
-  mojo::MakeSelfOwnedReceiver(
-      std::make_unique<ShareServiceImpl>(*render_frame_host),
-      std::move(receiver));
+  DCHECK(render_frame_host);
+  new ShareServiceImpl(render_frame_host, std::move(receiver));
 }
 
 // static
@@ -152,7 +155,7 @@ void ShareServiceImpl::Share(const std::string& title,
   UMA_HISTOGRAM_ENUMERATION(kWebShareApiCountMetric, WebShareMethod::kShare);
 
   content::WebContents* const web_contents =
-      content::WebContents::FromRenderFrameHost(render_frame_host_);
+      content::WebContents::FromRenderFrameHost(render_frame_host());
   if (!web_contents) {
     VLOG(1) << "Cannot share after navigating away";
     std::move(callback).Run(blink::mojom::ShareError::PERMISSION_DENIED);
@@ -222,7 +225,7 @@ void ShareServiceImpl::OnSafeBrowsingResultReceived(
   safe_browsing_request_.reset();
 
   content::WebContents* const web_contents =
-      content::WebContents::FromRenderFrameHost(render_frame_host_);
+      content::WebContents::FromRenderFrameHost(render_frame_host());
   if (!web_contents) {
     VLOG(1) << "Cannot share after navigating away";
     std::move(callback).Run(blink::mojom::ShareError::PERMISSION_DENIED);
@@ -269,10 +272,4 @@ void ShareServiceImpl::OnSafeBrowsingResultReceived(
   NOTREACHED();
   std::move(callback).Run(blink::mojom::ShareError::INTERNAL_ERROR);
 #endif
-}
-
-void ShareServiceImpl::RenderFrameDeleted(
-    content::RenderFrameHost* render_frame_host) {
-  if (render_frame_host == render_frame_host_)
-    render_frame_host_ = nullptr;
 }
