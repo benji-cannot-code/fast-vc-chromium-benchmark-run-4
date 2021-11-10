a35949f019892b05ad5fe0f99be35ac39e953329
Michael Checo <michaelcheco@google.com>
2021-11-10 23:40:25 +0000

    Firmware UI: Add i18n strings
    Bug: 1252981
    Change-Id: I7e5d85651613d159f5c154f13fbc21ca3d1101ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256652
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940547}

--- ash/webui/firmware_update_ui/resources/firmware_update_dialog.js
+++ ash/webui/firmware_update_ui/resources/firmware_update_dialog.js
@@ -5,9 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './firmware_shared_css.js';
 import './firmware_shared_fonts.js';
+import './strings.m.js';
 
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import 'chrome://resources/polymer/v3_0/paper-progress/paper-progress.js';
+
+import {I18nBehavior, I18nBehaviorInterface} from 'chrome://resources/js/i18n_behavior.m.js';
+import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {FirmwareUpdate, InstallationProgress, UpdateControllerInterface} from './firmware_update_types.js';
 import {getUpdateController} from './mojo_interface_provider.js';
 
@@ -23,7 +27,18 @@ export const DialogState = {
  * @fileoverview
  * 'firmware-update-dialog' displays information related to a firmware update.
  */
-export class FirmwareUpdateDialogElement extends PolymerElement {
+
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ * @implements {I18nBehaviorInterface}
+ */
+const FirmwareUpdateDialogElementBase =
+    mixinBehaviors([I18nBehavior], PolymerElement);
+
+/** @polymer */
+export class FirmwareUpdateDialogElement extends
+    FirmwareUpdateDialogElementBase {
   static get is() {
     return 'firmware-update-dialog';
   }
