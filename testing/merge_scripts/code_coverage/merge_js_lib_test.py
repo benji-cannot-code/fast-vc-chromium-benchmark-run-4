4c1802dbeffd7b1a57d9c6e3901a7aaa5d848fef
Matt Reichhoff <mreichhoff@chromium.org>
2021-11-10 16:48:01 +0000

    Convert testing/merge_scripts/code_coverage to python3
    Also includes explicit run_on_python2 (and 3) args in
    testing/trigger_scripts/PRESUBMIT.py.
    
    Bug: 1262365
    Change-Id: If89e4c2ad286458b5f43c24749733e8dcb4c86ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271730
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940352}

--- testing/merge_scripts/code_coverage/merge_js_lib_test.py
+++ testing/merge_scripts/code_coverage/merge_js_lib_test.py
@@ -481,7 +481,7 @@ class MergeJSLibTest(unittest.TestCase):
         for file_name in files:
           actual_files.append(os.path.join(root, file_name))
 
-      self.assertItemsEqual(expected_files, actual_files)
+      self.assertCountEqual(expected_files, actual_files)
     finally:
       shutil.rmtree(scripts_dir)
 
@@ -515,7 +515,7 @@ class MergeJSLibTest(unittest.TestCase):
         for file_name in files:
           actual_files.append(os.path.join(root, file_name))
 
-      self.assertItemsEqual(expected_files, actual_files)
+      self.assertCountEqual(expected_files, actual_files)
     finally:
       shutil.rmtree(scripts_dir)
 
