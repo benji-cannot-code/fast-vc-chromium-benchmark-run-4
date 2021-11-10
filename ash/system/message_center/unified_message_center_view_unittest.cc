cab359146f26504ac4c74e19b00e0858bd44f42d
Andre Le <leandre@chromium.org>
2021-11-10 20:45:24 +0000

    NotificationRevamp: Implement new scroll bar for message center.
    This CL added the usage of RoundedScrollBar for the new message center.
    
    Bug: 1266433
    Change-Id: I87b7f7c9861fc7c1ba47f31d16be5f490409cad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259826
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940472}

--- ash/system/message_center/unified_message_center_view_unittest.cc
+++ ash/system/message_center/unified_message_center_view_unittest.cc
@@ -171,7 +171,7 @@ class UnifiedMessageCenterViewTest : public AshTestBase,
 
   views::ScrollView* GetScroller() { return message_center_view()->scroller_; }
 
-  MessageCenterScrollBar* GetScrollBar() {
+  views::ScrollBar* GetScrollBar() {
     return message_center_view()->scroll_bar_;
   }
 
