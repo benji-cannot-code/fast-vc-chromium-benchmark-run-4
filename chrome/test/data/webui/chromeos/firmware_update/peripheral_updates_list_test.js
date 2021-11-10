d864423cb1a4f69308217d1ddeef2bcf04abd408
Michael Checo <michaelcheco@google.com>
2021-11-10 20:48:24 +0000

    Firmware UI: Display message when no updates are available
    Screenshot: http://shortn/_bKKzKYCvrd
    
    Bug: 1252981
    Change-Id: I5fda6aa1b820301b35e1fdbf89a387c22a10dfb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3256225
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940476}

--- chrome/test/data/webui/chromeos/firmware_update/peripheral_updates_list_test.js
+++ chrome/test/data/webui/chromeos/firmware_update/peripheral_updates_list_test.js
@@ -11,7 +11,7 @@ import {PeripheralUpdateListElement} from 'chrome://accessory-update/peripheral_
 import {UpdateCardElement} from 'chrome://accessory-update/update_card.js';
 
 import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
-import {flushTasks} from '../../test_util.js';
+import {flushTasks, isVisible} from '../../test_util.js';
 
 export function peripheralUpdatesListTest() {
   /** @type {?PeripheralUpdateListElement} */
@@ -20,12 +20,9 @@ export function peripheralUpdatesListTest() {
   /** @type {?FakeUpdateProvider} */
   let provider = null;
 
-  suiteSetup(() => {
+  setup(() => {
     provider = new FakeUpdateProvider();
     setUpdateProviderForTesting(provider);
-  });
-
-  setup(() => {
     document.body.innerHTML = '';
   });
 
@@ -60,6 +57,15 @@ export function peripheralUpdatesListTest() {
   }
 
   /**
+   * @suppress {visibility}
+   * @return {!Promise}
+   */
+  function clearFirmwareUpdates() {
+    peripheralUpdateListElement.firmwareUpdates_ = [];
+    return flushTasks();
+  }
+
+  /**
    * @return {!Array<!UpdateCardElement>}
    */
   function getUpdateCards() {
@@ -74,5 +80,15 @@ export function peripheralUpdatesListTest() {
         assertEquals(u.deviceName, updateCards[i].$.name.innerText);
       });
     });
+  });
+
+  test('EmptyState', () => {
+    return initializeUpdateList()
+        .then(() => clearFirmwareUpdates())
+        .then(() => {
+          assertTrue(isVisible(/** @type {!HTMLDivElement} */ (
+              peripheralUpdateListElement.shadowRoot.querySelector(
+                  '#upToDateText'))));
+        });
   });
 }
