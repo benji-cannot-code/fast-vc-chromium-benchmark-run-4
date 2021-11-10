8893c268cda444afc257cfd9502d5edc8db6726d
Xiaocheng Hu <xiaochengh@chromium.org>
2021-11-10 18:12:37 +0000

    [@layer] Make 'revert-layer' work in keyframes
    As per the recent CSSWG resolution [1], 'revert-layer' in keyframes
    should revert from the animation origin. This patch implements that.
    
    [1] https://github.com/w3c/csswg-drafts/issues/6749#issuecomment-960303732
    
    Bug: 1095765
    Change-Id: I3f093548da61108c24c05c6a679e9b1d85207467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3232606
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940387}

--- third_party/blink/renderer/core/animation/css_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_interpolation_type.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_custom_property_declaration.h"
 #include "third_party/blink/renderer/core/css/css_inherited_value.h"
 #include "third_party/blink/renderer/core/css/css_initial_value.h"
+#include "third_party/blink/renderer/core/css/css_revert_layer_value.h"
 #include "third_party/blink/renderer/core/css/css_revert_value.h"
 #include "third_party/blink/renderer/core/css/css_unset_value.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
@@ -114,8 +115,14 @@ class ResolvedRegisteredCustomPropertyChecker
   scoped_refptr<CSSVariableData> resolved_tokens_;
 };
 
+template <typename RevertValueType>
 class RevertChecker : public CSSInterpolationType::ConversionChecker {
  public:
+  static_assert(
+      std::is_same<RevertValueType, cssvalue::CSSRevertValue>::value ||
+          std::is_same<RevertValueType, cssvalue::CSSRevertLayerValue>::value,
+      "RevertCheck only accepts CSSRevertValue and CSSRevertLayerValue");
+
   RevertChecker(const PropertyHandle& property_handle,
                 const CSSValue* resolved_value)
       : property_handle_(property_handle), resolved_value_(resolved_value) {
@@ -125,8 +132,8 @@ class RevertChecker : public CSSInterpolationType::ConversionChecker {
   bool IsValid(const InterpolationEnvironment& environment,
                const InterpolationValue&) const final {
     const auto& css_environment = To<CSSInterpolationEnvironment>(environment);
-    const CSSValue* current_resolved_value = css_environment.Resolve(
-        property_handle_, cssvalue::CSSRevertValue::Create());
+    const CSSValue* current_resolved_value =
+        css_environment.Resolve(property_handle_, RevertValueType::Create());
     return DataEquivalent(resolved_value_.Get(), current_resolved_value);
   }
 
@@ -191,7 +198,16 @@ InterpolationValue CSSInterpolationType::MaybeConvertSingleInternal(
     value = css_environment.Resolve(GetProperty(), value);
     DCHECK(value);
     conversion_checkers.push_back(
-        std::make_unique<RevertChecker>(GetProperty(), value));
+        std::make_unique<RevertChecker<cssvalue::CSSRevertValue>>(GetProperty(),
+                                                                  value));
+  }
+
+  if (value->IsRevertLayerValue()) {
+    value = css_environment.Resolve(GetProperty(), value);
+    DCHECK(value);
+    conversion_checkers.push_back(
+        std::make_unique<RevertChecker<cssvalue::CSSRevertLayerValue>>(
+            GetProperty(), value));
   }
 
   bool is_inherited = CssProperty().IsInherited();
@@ -221,7 +237,13 @@ InterpolationValue CSSInterpolationType::MaybeConvertCustomPropertyDeclaration(
 
   if (declaration.IsRevert()) {
     conversion_checkers.push_back(
-        std::make_unique<RevertChecker>(GetProperty(), value));
+        std::make_unique<RevertChecker<cssvalue::CSSRevertValue>>(GetProperty(),
+                                                                  value));
+  }
+  if (declaration.IsRevertLayer()) {
+    conversion_checkers.push_back(
+        std::make_unique<RevertChecker<cssvalue::CSSRevertLayerValue>>(
+            GetProperty(), value));
   }
   if (const auto* resolved_declaration =
           DynamicTo<CSSCustomPropertyDeclaration>(value)) {
--- third_party/blink/renderer/core/animation/string_keyframe.h
+++ third_party/blink/renderer/core/animation/string_keyframe.h
@@ -121,6 +121,7 @@ class CORE_EXPORT StringKeyframe : public Keyframe {
 
     bool IsNeutral() const final { return !value_; }
     bool IsRevert() const final;
+    bool IsRevertLayer() const final;
     Keyframe::PropertySpecificKeyframe* NeutralKeyframe(
         double offset,
         scoped_refptr<TimingFunction> easing) const final;
@@ -158,6 +159,7 @@ class CORE_EXPORT StringKeyframe : public Keyframe {
 
     bool IsNeutral() const final { return value_.IsNull(); }
     bool IsRevert() const final { return false; }
+    bool IsRevertLayer() const final { return false; }
     PropertySpecificKeyframe* NeutralKeyframe(
         double offset,
         scoped_refptr<TimingFunction> easing) const final;
