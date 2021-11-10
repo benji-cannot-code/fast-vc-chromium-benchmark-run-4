5d9b548ad9c9e4cc768eb023927088aa91f5bb0c
Avery Musbach <amusbach@chromium.org>
2021-11-10 01:49:17 +0000

    cc: Remove #include <memory> in skottie_wrapper.h
    Bug: None
    Test: CQ
    Change-Id: I8c1d20cb21dbd8b396ea98bfb48eccbf8040c67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271494
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940121}

--- cc/paint/skottie_wrapper.h
+++ cc/paint/skottie_wrapper.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_PAINT_SKOTTIE_WRAPPER_H_
 #define CC_PAINT_SKOTTIE_WRAPPER_H_
 
-#include <memory>
 #include <vector>
 
 #include "base/containers/span.h"
