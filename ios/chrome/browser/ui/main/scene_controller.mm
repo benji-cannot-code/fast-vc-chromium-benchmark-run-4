5ad77b947f7608069b54bf193cb5e5792220c986
Vincent Boisselle <vincb@google.com>
2021-11-10 13:26:11 +0000

    [iOS][ForcedSignIn]🏢 Fix dismissing modals with forced sign-in prompt
    Simpler version of this fix: http://crrev.com/c/3268484
    
    This CL fixes 2 dcheck crashes that are triggered when dismissing modals
    with a trigger that isn't from an interaction with the sign-in prompt:
      > The crash that happens when the
    -dismissModalsWithCompletion command is called while the forced sign-in
    prompt is presented. This doesn't happen when closing the app where the
    signin coordinator is interrupted without dismissal.
    
        Problematic DCHECK: http://shortn/_krVOzQcYEu
    
      > The crash that happens when showing the forced sign-in prompt
        after dismissing (externally) the sign-in prompt that is presented
        on the Settings menu.
    
        Problematic DCHECK: http://shortn/_DmTFP8PjQI
    
    These issues were caused by crrev.com/c/3254141 which purpose was
    to fix another issue.
    
    The problems are caused by the forced signin prompt being reshown
    immediately after dismissing the sign-in prompt (any of them), which
    will populate the |signinCoordinator| property of SceneController
    before the process that dismisses the modal is fully done, which will
    then trigger DCHECK errors because the property is populated before
    the process dismissing the sign-in prompt is formally done.
    
    A solution if to make sure that the force sign-in prompt is only
    reshown after the DCHECK.
    
    The best solution would be probably to have a signal when modals are
    dismissed but this would require more design and thinking. Filled
    http://crbug.com/1267900 to investigate that.
    
    ============
    Visualization of the time windows of sign-in signal VS dismiss modals.
    
    ~~~
    Problem - Misalignment of the time windows
    
    The signal (represented by 'x') is fired during dismiss modals.
    
                    YES
             |-------------|
    _________|             |x--------- NO
    SceneState.signinInProgress
    
                    |-------x-----|
    ________________|             |----------
    dismiss settings modal
    
    ---------------------------------->
    time
    
    ~~~
    Fix - Make sure signinInProgress covers dismiss modals
    
    The signal (represented by 'x') is fired after dismiss modals.
    
                          YES
             |------------------------|
    _________|                        |x--------- NO
    signinInProgress
    
                    |-------------|
    ________________|             |----x-----
    dismiss settings modal
    
    ---------------------------------->
    time
    ~~~~
    
    ============
    
    ============
    Note about the issue with -dismissModalsWithCompletion
    
    Normally we won't expect -dismissModalsWithCompletion to be called when
    the forced sign-in prompt is presented, but this can happen when testing
    where the command can be executed while forced sign-in prompt blocks
    the UI (I think normally this wouldn't be allowed).
    
    See crrev.com/c/3267844 as example of a context where
    -dismissModalsWithCompletion may be called while the forced sign-in
    prompt is presented.
    ============
    
    Bug: 1267723
    Change-Id: Ifbe427840aca3082b19fd570e8862ea04a79b046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3264702
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940281}

--- ios/chrome/browser/ui/main/scene_controller.mm
+++ ios/chrome/browser/ui/main/scene_controller.mm
@@ -253,6 +253,14 @@ bool IsSigninForcedByPolicy() {
 @property(nonatomic, strong)
     NSDictionary<NSString*, NSString*>* specificProductData;
 
+// YES if the process of dismissing the sign-in prompt is from an external
+// trigger and is currently ongoing. An external trigger isn't done from the
+// signin prompt itself (i.e., tapping a button in the sign-in prompt that
+// dismisses the prompt). For example, the -dismissModalDialogswithCompletion
+// command is considered as an external trigger because it comes from something
+// outside the sign-in prompt UI.
+@property(nonatomic, assign) BOOL dismissingSigninPromptFromExternalTrigger;
+
 @end
 
 @implementation SceneController
@@ -2663,6 +2671,19 @@ bool IsSigninForcedByPolicy() {
 
 - (void)closeSettingsOrSigninAnimated:(BOOL)animated
                            completion:(ProceduralBlock)completion {
+  __weak __typeof(self) weakSelf = self;
+  BOOL resetSigninState = self.signinCoordinator != nil;
+  completion = ^{
+    __typeof(self) strongSelf = weakSelf;
+    if (completion) {
+      completion();
+    }
+    if (resetSigninState) {
+      strongSelf.sceneState.signinInProgress = NO;
+    }
+    strongSelf.dismissingSigninPromptFromExternalTrigger = NO;
+  };
+
   if (self.settingsNavigationController) {
     ProceduralBlock dismissSettings = ^() {
       [self.settingsNavigationController cleanUpSettings];
@@ -2708,6 +2729,8 @@ bool IsSigninForcedByPolicy() {
   SigninCoordinatorInterruptAction action =
       animated ? SigninCoordinatorInterruptActionDismissWithAnimation
                : SigninCoordinatorInterruptActionDismissWithoutAnimation;
+
+  self.dismissingSigninPromptFromExternalTrigger = YES;
   [self.signinCoordinator interruptWithAction:action completion:completion];
 }
 
@@ -2741,10 +2764,17 @@ bool IsSigninForcedByPolicy() {
         strongSelf.signinCoordinator = nil;
         uiBlocker.reset();
 
-        weakSelf.sceneState.signinInProgress = NO;
-
         if (completion) {
           completion(result == SigninCoordinatorResultSuccess);
+        }
+
+        if (!weakSelf.dismissingSigninPromptFromExternalTrigger) {
+          // If the coordinator isn't stopped by an external trigger, sign-in
+          // is done. Otherwise, there might be extra steps to be done before
+          // considering sign-in as done. This is up to the handler that sets
+          // |self.dismissingSigninPromptFromExternalTrigger| to YES to set
+          // back |signinInProgress| to NO.
+          weakSelf.sceneState.signinInProgress = NO;
         }
 
         switch (info.signinCompletionAction) {
