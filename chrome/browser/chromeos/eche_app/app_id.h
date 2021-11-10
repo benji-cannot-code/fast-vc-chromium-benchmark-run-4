84bd1c6aa2be2fe848273fe278ed8087faf5693b
paulzhchen <paulzchen@google.com>
2021-11-10 23:42:49 +0000

    Prevent pinning of the Eche application to the shelf
    The Eche application cannot be invoked without context, if it is
    pinned to the shelf it does nothing when clicked so block doing so.
    
    Bug: b/204942993
    Test: unit tests pass, manually verified
    Change-Id: Id785320322d0a33035e35294cb63a8bc3df2fcbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268008
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Daniel Nishi <dhnishi@chromium.org>
    Commit-Queue: Paulz Chen <paulzchen@google.com>
    Cr-Commit-Position: refs/heads/main@{#940549}

---
+++ chrome/browser/chromeos/eche_app/app_id.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_ECHE_APP_APP_ID_H_
+#define CHROME_BROWSER_CHROMEOS_ECHE_APP_APP_ID_H_
+
+namespace chromeos {
+namespace eche_app {
+
+// The Eche's app ID.
+const char kEcheAppId[] = "pcmgemncoejjdplpoaekhfgkckffggcb";
+
+}  // namespace eche_app
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_ECHE_APP_APP_ID_H_
--- chrome/browser/ui/ash/shelf/chrome_shelf_controller_unittest.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_controller_unittest.cc
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/demo_mode/demo_mode_test_helper.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chrome/browser/chromeos/eche_app/app_id.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/test_extension_system.h"
 #include "chrome/browser/prefs/browser_prefs.h"
@@ -4880,7 +4881,8 @@ TEST_P(ChromeShelfControllerTest, UnpinnableComponentApps) {
   InitShelfController();
 
   const char* kPinnableApp = file_manager::kFileManagerAppId;
-  const char* kNoPinApps[] = {extension_misc::kFeedbackExtensionId};
+  const char* kNoPinApps[] = {extension_misc::kFeedbackExtensionId,
+                              chromeos::eche_app::kEcheAppId};
 
   EXPECT_EQ(AppListControllerDelegate::PIN_EDITABLE,
             GetPinnableForAppID(kPinnableApp, profile()));
