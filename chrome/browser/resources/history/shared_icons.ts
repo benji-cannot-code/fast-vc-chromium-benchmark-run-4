cc0dc09b65870e404cca130a63033c18fd66def8
Moe Ahmadi <mahmadi@chromium.org>
2021-11-11 16:25:03 +0000

    [memories] Fixes color of history clusters visibility toggle menu item
    <iron-icon> uses an <img> tag to display icons provided using the src
    attribute. In this case the '--iron-icon-fill-color' CSS var does not
    get applied to the svg. For the fill color to be correctly applied to
    <iron-icon> the icon needs to be provided as a member of an icon set
    using the icon attribute: icon="<iconset>:<icon>"
    
    Fixed: 1268282
    Change-Id: I1a03010bac6f78c86386266d916f85c5a7962259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3270653
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940786}

---
+++ chrome/browser/resources/history/shared_icons.ts
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/polymer/v3_0/iron-iconset-svg/iron-iconset-svg.js';
+
+import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+const template = html`{__html_template__}`;
+document.head.appendChild(template.content);
