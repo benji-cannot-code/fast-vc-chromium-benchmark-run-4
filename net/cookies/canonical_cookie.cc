785645d265ad1968448c0162d650a4db0e4c7e83
Steven Bingler <bingler@chromium.org>
2021-11-11 18:34:34 +0000

    Remove Cookie.SameSiteContextDowngrade* histograms
    Expire and remove the unnecessary histograms.
    
    Bug: 1253418
    Change-Id: I2a77387ba0ad4ee3b63330f131cb79b7234b6b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3273973
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940840}

--- net/cookies/canonical_cookie.cc
+++ net/cookies/canonical_cookie.cc
@@ -1078,11 +1078,6 @@ CookieAccessResult CanonicalCookie::IncludeForRequestURL(
     }
   }
 
-  if (status.ShouldRecordDowngradeMetrics()) {
-    UMA_HISTOGRAM_ENUMERATION("Cookie.SameSiteContextDowngradeRequest",
-                              status.GetBreakingDowngradeMetricsEnumValue(url));
-  }
-
   if (status.HasWarningReason(
           CookieInclusionStatus::
               WARN_CROSS_SITE_REDIRECT_DOWNGRADE_CHANGES_INCLUSION)) {
