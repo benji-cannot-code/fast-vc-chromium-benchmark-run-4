9bed332fbfbccc38d0bc3ea9e1a39d73bc69ad53
Vladimir Levin <vmpstr@chromium.org>
2021-11-11 18:59:59 +0000

    Style: Add StyleRecalcChange::ToString and mark two functions const
    This patch should have no functionality change, but adds a ToString
    to StyleRecalcChange which has been useful in my debugging.
    
    It also marks a couple of functions const that should be const.
    
    R=futhark@chromium.org
    
    Change-Id: If528aabc3903b0ca0bd13403c89fbbf5fb6733a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275642
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940859}

--- third_party/blink/renderer/core/css/style_recalc.cc
+++ third_party/blink/renderer/core/css/style_recalc.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/pseudo_element.h"
+#include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
 
@@ -53,6 +54,62 @@ bool StyleRecalcChange::ShouldUpdatePseudoElement(
     return true;
   return RecalcContainerQueryDependent() &&
          pseudo_element.ComputedStyleRef().DependsOnContainerQueries();
+}
+
+String StyleRecalcChange::ToString() const {
+  StringBuilder builder;
+  builder.Append("StyleRecalcChange{propagate=");
+  switch (propagate_) {
+    case kNo:
+      builder.Append("kNo");
+      break;
+    case kUpdatePseudoElements:
+      builder.Append("kUpdatePseudoElements");
+      break;
+    case kIndependentInherit:
+      builder.Append("kIndependentInherit");
+      break;
+    case kRecalcChildren:
+      builder.Append("kRecalcChildren");
+      break;
+    case kRecalcDescendants:
+      builder.Append("kRecalcDescendants");
+      break;
+  }
+  builder.Append(", flags=");
+  if (!flags_) {
+    builder.Append("kNoFlags");
+  } else {
+    Flags flags = flags_;
+    // Make sure we don't loop forever if we aren't handling some case.
+    Flags previous_flags = 0;
+    String separator = "";
+    while (flags && flags != previous_flags) {
+      previous_flags = flags;
+      builder.Append(separator);
+      separator = "|";
+      if (flags & kRecalcContainer) {
+        builder.Append("kRecalcContainer");
+        flags &= ~kRecalcContainer;
+      } else if (flags & kRecalcDescendantContainers) {
+        builder.Append("kRecalcDescendantContainers");
+        flags &= ~kRecalcDescendantContainers;
+      } else if (flags & kReattach) {
+        builder.Append("kReattach");
+        flags &= ~kReattach;
+      } else if (flags & kSuppressRecalc) {
+        builder.Append("kSuppressRecalc");
+        flags &= ~kSuppressRecalc;
+      }
+    }
+    if (flags) {
+      builder.Append(separator);
+      builder.Append("UnknownFlag=");
+      builder.Append(flags);
+    }
+  }
+  builder.Append("}");
+  return builder.ToString();
 }
 
 StyleRecalcChange::Flags StyleRecalcChange::FlagsForChildren(
--- third_party/blink/renderer/core/css/style_recalc.h
+++ third_party/blink/renderer/core/css/style_recalc.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
@@ -92,11 +93,11 @@ class CORE_EXPORT StyleRecalcChange {
   StyleRecalcChange SuppressRecalc() const {
     return {propagate_, static_cast<Flags>(flags_ | kSuppressRecalc)};
   }
-  StyleRecalcChange WithRecalcContainerFlags(StyleRecalcChange& from) {
+  StyleRecalcChange WithRecalcContainerFlags(StyleRecalcChange& from) const {
     return {propagate_,
             static_cast<Flags>(flags_ | (from.flags_ & kRecalcContainerFlags))};
   }
-  StyleRecalcChange Combine(const StyleRecalcChange& other) {
+  StyleRecalcChange Combine(const StyleRecalcChange& other) const {
     return {std::max(propagate_, other.propagate_),
             static_cast<Flags>(flags_ | other.flags_)};
   }
@@ -112,6 +113,8 @@ class CORE_EXPORT StyleRecalcChange {
   bool ShouldRecalcStyleFor(const Node&) const;
   bool ShouldUpdatePseudoElement(const PseudoElement&) const;
   bool IsSuppressed() const { return flags_ & kSuppressRecalc; }
+
+  String ToString() const;
 
  private:
   StyleRecalcChange(Propagate propagate, Flags flags)
