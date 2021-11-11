25ed79fed41214cc4fb6f07eed71606e9ec40804
Nancy Wang <nancylingwang@chromium.org>
2021-11-11 03:20:19 +0000

    Add interfaces in AppRegistryCache to access the non mojom App struct.
    Add ForAllApps and ForApp in AppRegistryCache to access the non mojom
    App struct, and they will be used by the follow up CLs to get the
    icon key.
    
    BUG=1253250
    
    Change-Id: I0056155c99a9547d571bbae8b553c9caa6aa6fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3270472
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940642}

--- components/services/app_service/public/cpp/types_util.h
+++ components/services/app_service/public/cpp/types_util.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Utility functions for App Service types.
 
+#include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 
 namespace apps_util {
 
 bool IsInstalled(apps::mojom::Readiness readiness);
+bool IsInstalled(apps::Readiness readiness);
 bool IsHumanLaunch(apps::mojom::LaunchSource launch_source);
 
 // Checks if an app of |app_type| runs in Browser/WebContents (web apps, hosted
