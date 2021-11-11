3e48009099f814b5963ac30749d60cae262f6551
Dylan Cutler <dylancutler@google.com>
2021-11-11 15:23:08 +0000

    Delete unnecessary method from UrlRequestJob
    Bug: None
    Change-Id: I9d2920b0abe6b73d391690d4f650faed1abfe723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3273544
    Auto-Submit: Dylan Cutler <dylancutler@google.com>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940771}

--- net/url_request/url_request_job.h
+++ net/url_request/url_request_job.h
@@ -292,9 +292,6 @@ class NET_EXPORT URLRequestJob {
   bool CanSetCookie(const net::CanonicalCookie& cookie,
                     CookieOptions* options) const;
 
-  // Delegates to URLRequest.
-  PrivacyMode privacy_mode() const;
-
   // Notifies the job that headers have been received.
   void NotifyHeadersComplete();
 
