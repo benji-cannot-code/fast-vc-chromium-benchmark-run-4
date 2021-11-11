83bc95589a1dbc74fceac3d08f025494f258859c
Peter E Conn <peconn@chromium.org>
2021-11-11 17:57:41 +0000

    🛃 Add some unit tests for Trusted CDN logic.
    Bug: 847341
    Change-Id: I530832ecf000c785425921988a940e35f11ad0ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275774
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940812}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/TrustedCdn.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/TrustedCdn.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.tab;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.UnownedUserData;
 import org.chromium.base.UnownedUserDataKey;
@@ -92,6 +93,11 @@ public class TrustedCdn extends TabWebContentsUserData {
         return trustedCdn;
     }
 
+    @VisibleForTesting
+    public static void setPublisherUrlForTesting(@NonNull Tab tab, @Nullable String publisherUrl) {
+        from(tab).setPublisherUrl(publisherUrl);
+    }
+
     private static TrustedCdn get(@Nullable Tab tab) {
         return tab != null ? tab.getUserDataHost().getUserData(USER_DATA_KEY) : null;
     }
@@ -140,7 +146,7 @@ public class TrustedCdn extends TabWebContentsUserData {
     }
 
     @NativeMethods
-    interface Natives {
+    public interface Natives {
         long init(TrustedCdn caller);
         void onDestroyed(long nativeTrustedCdn, TrustedCdn caller);
         void setWebContents(long nativeTrustedCdn, TrustedCdn caller, WebContents webContents);
