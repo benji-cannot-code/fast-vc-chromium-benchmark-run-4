489a6b933b529dbcd4faac1021cfc72bff7ba2ae
Thomas Lukaszewicz <tluk@chromium.org>
2021-11-11 21:41:03 +0000

    Revert "Remove unused refs to the webrtcAudioPrivate API"
    This reverts commit ff4d88c3d01f03688e892c8f789e17c935f3fb08.
    
    Reason for revert:
    CL is causing test failures for
    HangoutServicesBrowserTest.RunComponentExtensionTest
    on linux-chromeos-chrome bots
    
    First failing build below
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/18643/overview
    
    Original change's description:
    > Remove unused refs to the webrtcAudioPrivate API
    >
    > This API is no longer used since it was obsoleted by the introduction of
    > HTMLMediaElement.setSinkId to control audio output destinations.
    > To follow: deleting the actual API impl.
    >
    > Bug: 1267191
    > Change-Id: I547350b66687f93ba38ad8eb00c1524c51eac1fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275494
    > Reviewed-by: Elad Alon <eladalon@chromium.org>
    > Commit-Queue: Tony Herre <toprice@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#940729}
    
    TBR=toprice@chromium.org,eladalon@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: If3902af75a85a2bece6a2452edfdc24574c84db8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1267191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277053
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tom Lukaszewicz <tluk@google.com>
    Owners-Override: Tom Lukaszewicz <tluk@google.com>
    Cr-Commit-Position: refs/heads/main@{#940928}

--- chrome/browser/resources/hangout_services/thunk.js
+++ chrome/browser/resources/hangout_services/thunk.js
@@ -130,6 +130,14 @@ chrome.runtime.onMessageExternal.addListener(function(
     } else if (method == 'logging.discard') {
       chrome.webrtcLoggingPrivate.discard(requestInfo, origin, doSendResponse);
       return true;
+    } else if (method == 'getSinks') {
+      chrome.webrtcAudioPrivate.getSinks(doSendResponse);
+      return true;
+    } else if (method == 'getAssociatedSink') {
+      const sourceId = message['sourceId'];
+      chrome.webrtcAudioPrivate.getAssociatedSink(
+          origin, sourceId, doSendResponse);
+      return true;
     } else if (method == 'isExtensionEnabled') {
       // This method is necessary because there may be more than one
       // version of this extension, under different extension IDs. By
@@ -185,6 +193,20 @@ chrome.runtime.onMessageExternal.addListener(function(
     doSendResponse(null, e.name + ': ' + e.message);
   }
 });
+
+// If Hangouts connects with a port named 'onSinksChangedListener', we
+// will register a listener and send it a message {'eventName':
+// 'onSinksChanged'} whenever the event fires.
+function onSinksChangedPort(port) {
+  function clientListener() {
+    port.postMessage({'eventName': 'onSinksChanged'});
+  }
+  chrome.webrtcAudioPrivate.onSinksChanged.addListener(clientListener);
+
+  port.onDisconnect.addListener(function() {
+    chrome.webrtcAudioPrivate.onSinksChanged.removeListener(clientListener);
+  });
+}
 
 // This is a one-time-use port for calling chooseDesktopMedia.  The page
 // sends one message, identifying the requested source types, and the
@@ -286,7 +308,9 @@ function appendLastErrorMessage(errors) {
 }
 
 chrome.runtime.onConnectExternal.addListener(function(port) {
-  if (port.name == 'chooseDesktopMedia') {
+  if (port.name == 'onSinksChangedListener') {
+    onSinksChangedPort(port);
+  } else if (port.name == 'chooseDesktopMedia') {
     onChooseDesktopMediaPort(port);
   } else if (port.name == 'processCpu') {
     onProcessCpu(port);
