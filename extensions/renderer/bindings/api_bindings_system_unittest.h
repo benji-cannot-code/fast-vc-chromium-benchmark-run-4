68902453ee537356e8064344ce7dff1acd8e83b7
Tim Judkins <tjudkins@chromium.org>
2021-11-11 20:38:48 +0000

    [Extensions] Add to custom callback test for APIBindingsSystemTest
    This CL adds to the APIBindingSystemTest dealing with custom callbacks
    to add a promise case and add cases for how we currently handle errors
    being thrown inside custom callbacks.
    
    Bug: 1220910
    Change-Id: I1af967d5ee8c307dcd02d353912e91584b7bf96c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271885
    Commit-Queue: Tim Judkins <tjudkins@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940904}

--- extensions/renderer/bindings/api_bindings_system_unittest.h
+++ extensions/renderer/bindings/api_bindings_system_unittest.h
@@ -59,8 +59,8 @@ class APIBindingsSystemTest : public APIBindingTest {
       v8::Local<v8::Object>* secondary_parent);
 
   // Simulates logging an error to the console.
-  virtual void AddConsoleError(v8::Local<v8::Context> context,
-                               const std::string& error) {}
+  void AddConsoleError(v8::Local<v8::Context> context,
+                       const std::string& error);
 
   // Returns the DictionaryValue representing the schema with the given API
   // name.
@@ -95,6 +95,9 @@ class APIBindingsSystemTest : public APIBindingTest {
   }
   void reset_last_request() { last_request_.reset(); }
   APIBindingsSystem* bindings_system() { return bindings_system_.get(); }
+  const std::vector<std::string>& console_errors() const {
+    return console_errors_;
+  }
 
  private:
   // The API schemas for the fake APIs.
@@ -107,6 +110,9 @@ class APIBindingsSystemTest : public APIBindingTest {
   // The last request to be received from the APIBindingsSystem, or null if
   // there is none.
   std::unique_ptr<APIRequestHandler::Request> last_request_;
+
+  // A list for keeping track of simulated console errors.
+  std::vector<std::string> console_errors_;
 };
 
 }  // namespace extensions
