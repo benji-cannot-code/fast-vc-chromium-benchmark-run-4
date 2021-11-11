dba4ee21d36b5cce3648bfb240cc7b565818b115
Nan Lin <linnan@chromium.org>
2021-11-11 22:09:18 +0000

    [Code Health] Remove usage of DictionaryValue::GetBoolean() under components
    Bug: 1187033
    Change-Id: Ib40209840043f4c657129c49f776af5bda4a0bb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261897
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940940}

--- components/dom_distiller/core/page_features.cc
+++ components/dom_distiller/core/page_features.cc
@@ -163,14 +163,13 @@ std::vector<double> CalculateDerivedFeaturesFromJSON(
     return std::vector<double>();
   }
 
-  bool isOGArticle = false;
   std::string url, innerText, textContent, innerHTML;
   absl::optional<double> numElements = dict->FindDoubleKey("numElements");
   absl::optional<double> numAnchors = dict->FindDoubleKey("numAnchors");
   absl::optional<double> numForms = dict->FindDoubleKey("numForms");
-
-  if (!(dict->GetBoolean("opengraph", &isOGArticle) &&
-        dict->GetString("url", &url) && numElements && numAnchors && numForms &&
+  absl::optional<bool> isOGArticle = dict->FindBoolKey("opengraph");
+  if (!(isOGArticle.has_value() && dict->GetString("url", &url) &&
+        numElements && numAnchors && numForms &&
         dict->GetString("innerText", &innerText) &&
         dict->GetString("textContent", &textContent) &&
         dict->GetString("innerHTML", &innerHTML))) {
@@ -182,7 +181,7 @@ std::vector<double> CalculateDerivedFeaturesFromJSON(
     return std::vector<double>();
   }
 
-  return CalculateDerivedFeatures(isOGArticle, parsed_url, *numElements,
+  return CalculateDerivedFeatures(isOGArticle.value(), parsed_url, *numElements,
                                   *numAnchors, *numForms, innerText,
                                   textContent, innerHTML);
 }
