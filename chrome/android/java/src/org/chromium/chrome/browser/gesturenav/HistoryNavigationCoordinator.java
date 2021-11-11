19637d82d0b69db8a58161cab304de79f9d37665
Jinsuk Kim <jinsukkim@chromium.org>
2021-11-11 03:01:39 +0000

    gesture-nav: Fix crash at destruction phase
    NavigationHandler (TouchEventObserver) should have been removed from
    CompositorViewHolder at destruction to avoid UAF when remaining
    touch events are routed to it at destruction phase.
    
    Bug: 1268094
    Change-Id: I83106f7717a42707d109d5932651a869076da63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3269250
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940635}

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/HistoryNavigationCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/HistoryNavigationCoordinator.java
@@ -49,6 +49,7 @@ public class HistoryNavigationCoordinator
     private OverscrollGlowOverlay mOverscrollGlowOverlay;
 
     private Callback<TouchEventObserver> mInitCallback;
+    private Callback<TouchEventObserver> mDestroyCallback;
 
     /**
      * Creates the coordinator for gesture navigation and initializes internal objects.
@@ -61,6 +62,7 @@ public class HistoryNavigationCoordinator
      *        capabilities of the device.
      * @param backActionDelegate Delegate handling actions for back gesture.
      * @param initRunnable Runnable to run when Navigation Handler is initialized.
+     * @param destroyRunnable Runnable to run when Navigation Handler is destroyed.
      * @param layoutManager LayoutManager for handling overscroll glow effect as scene layer.
      * @return HistoryNavigationCoordinator object or null if not enabled via feature flag.
      */
@@ -68,10 +70,12 @@ public class HistoryNavigationCoordinator
             ActivityLifecycleDispatcher lifecycleDispatcher, ViewGroup parentView,
             Runnable requestRunnable, ObservableSupplier<Tab> tabSupplier,
             InsetObserverView insetObserverView, BackActionDelegate backActionDelegate,
-            Callback<TouchEventObserver> initCallback, LayoutManager layoutManager) {
+            Callback<TouchEventObserver> initCallback, Callback<TouchEventObserver> destroyCallback,
+            LayoutManager layoutManager) {
         HistoryNavigationCoordinator coordinator = new HistoryNavigationCoordinator();
         coordinator.init(window, lifecycleDispatcher, parentView, requestRunnable, tabSupplier,
-                insetObserverView, backActionDelegate, initCallback, layoutManager);
+                insetObserverView, backActionDelegate, initCallback, destroyCallback,
+                layoutManager);
         return coordinator;
     }
 
@@ -86,7 +90,8 @@ public class HistoryNavigationCoordinator
     private void init(WindowAndroid window, ActivityLifecycleDispatcher lifecycleDispatcher,
             ViewGroup parentView, Runnable requestRunnable, ObservableSupplier<Tab> tabSupplier,
             InsetObserverView insetObserverView, BackActionDelegate backActionDelegate,
-            Callback<TouchEventObserver> initCallback, LayoutManager layoutManager) {
+            Callback<TouchEventObserver> initCallback, Callback<TouchEventObserver> destroyCallback,
+            LayoutManager layoutManager) {
         mOverscrollGlowOverlay = new OverscrollGlowOverlay(window, parentView, requestRunnable);
         mNavigationLayout = new HistoryNavigationLayout(parentView.getContext(), this::isNativePage,
                 mOverscrollGlowOverlay, (direction) -> mNavigationHandler.navigate(direction));
@@ -118,6 +123,7 @@ public class HistoryNavigationCoordinator
                 });
 
         mInitCallback = initCallback;
+        mDestroyCallback = destroyCallback;
 
         // We wouldn't hear about the first tab until the content changed or we switched tabs
         // if tabProvider.get() != null. Do here what we do when tab switching happens.
@@ -290,6 +296,7 @@ public class HistoryNavigationCoordinator
         if (mNavigationHandler != null) {
             mNavigationHandler.setTab(null);
             mNavigationHandler.destroy();
+            mDestroyCallback.onResult(mNavigationHandler);
             mNavigationHandler = null;
         }
         if (mActivityLifecycleDispatcher != null) {
