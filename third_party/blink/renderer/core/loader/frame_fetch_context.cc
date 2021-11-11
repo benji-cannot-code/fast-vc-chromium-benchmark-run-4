70a39399b6a3deff4a1d076867766ba5fffc8a41
Hajime Hoshi <hajimehoshi@chromium.org>
2021-11-11 02:32:48 +0000

    Refactoring: Rename BackForwardCacheLoaderHelperForFrame to *Impl and add *Delegate
    This CL renames BackForwardCacheLoaderHelperForFrame to
    BackForwardCacheLoaderHelperImpl, and makes it depends on a new Delegate
    class instead of LocalFrame. This change makes it easier to use
    BackForwardCacheLoaderHelper for workers. WorkerOrWorkletGlobalScope will
    be able to inherit BackForwardCacheLoaderHelperImpl::Delegate.
    
    This is a preparation for making pages with dedicated workers eligible
    for back-forward cache.
    
    Design Doc: https://docs.google.com/document/d/1CJfDNqA2tEp_dmxK5kaBHHqGjx7cK2_jhTpsCwSCa3A/edit?usp=sharing
    
    Bug: 1146955
    Change-Id: I6e1882d91ff1c83f85d2b02556e6b855a331ef8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263670
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940627}

--- third_party/blink/renderer/core/loader/frame_fetch_context.cc
+++ third_party/blink/renderer/core/loader/frame_fetch_context.cc
@@ -79,7 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/inspector/inspector_audits_issue.h"
 #include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
-#include "third_party/blink/renderer/core/loader/back_forward_cache_loader_helper_for_frame.h"
+#include "third_party/blink/renderer/core/loader/back_forward_cache_loader_helper_impl.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/core/loader/frame_loader.h"
 #include "third_party/blink/renderer/core/loader/frame_resource_fetcher_properties.h"
@@ -240,7 +240,7 @@ ResourceFetcher* FrameFetchContext::CreateFetcherForCommittedDocument(
       frame->GetTaskRunner(TaskType::kNetworkingUnfreezable),
       MakeGarbageCollected<LoaderFactoryForFrame>(loader, *frame->DomWindow()),
       frame->DomWindow(),
-      MakeGarbageCollected<BackForwardCacheLoaderHelperForFrame>(*frame));
+      MakeGarbageCollected<BackForwardCacheLoaderHelperImpl>(*frame));
   init.use_counter =
       MakeGarbageCollected<DetachableUseCounter>(frame->DomWindow());
   init.console_logger = MakeGarbageCollected<DetachableConsoleLogger>(
