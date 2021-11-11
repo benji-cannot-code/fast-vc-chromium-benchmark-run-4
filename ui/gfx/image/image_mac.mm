a81dd66d116d0cd6aaf8ec6f8a056b97f4ee1e7b
Austin Sullivan <asully@chromium.org>
2021-11-11 01:00:07 +0000

    Add null checks in Get1xPNGBytesFromNSImage
    CHECKs that nsimage is never a nullptr, since if it is this would
    indicates a memory management issue.
    
    Also adds a DumpWithoutCrashing call to figure out what's going on
    when cg_image is null.
    
    Bug: 1253882
    Change-Id: I7c9a511d3d73ee96023b714067636acf243676bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3273573
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940590}

--- ui/gfx/image/image_mac.mm
+++ ui/gfx/image/image_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <AppKit/AppKit.h>
 #include <stddef.h>
 
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/mac/scoped_nsobject.h"
 #include "ui/gfx/geometry/size.h"
@@ -35,9 +36,16 @@ NSImage* GetErrorNSImage() {
 
 scoped_refptr<base::RefCountedMemory> Get1xPNGBytesFromNSImage(
     NSImage* nsimage) {
+  // TODO(crbug.com/1253882): Remove this once the root cause has been found.
+  CHECK(nsimage);
   CGImageRef cg_image = [nsimage CGImageForProposedRect:NULL
                                                 context:nil
                                                   hints:nil];
+  // TODO(crbug.com/1253882): Remove this once the root cause has been found.
+  if (!cg_image) {
+    base::debug::DumpWithoutCrashing();
+    return scoped_refptr<base::RefCountedMemory>();
+  }
   base::scoped_nsobject<NSBitmapImageRep> ns_bitmap(
       [[NSBitmapImageRep alloc] initWithCGImage:cg_image]);
   NSData* ns_data = [ns_bitmap representationUsingType:NSPNGFileType
