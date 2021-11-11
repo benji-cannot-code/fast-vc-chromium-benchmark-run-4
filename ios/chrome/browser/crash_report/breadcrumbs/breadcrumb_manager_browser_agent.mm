6a68b9017775e401fef9240875262bd8d8eda821
Jesse McKenna <jessemckenna@google.com>
2021-11-11 23:42:43 +0000

    breadcrumbs: create base browser agent class
    This change creates breadcrumbs::BreadcrumbManagerBrowserAgent, a base
    class from which the iOS- and desktop-specific implementations can
    inherit. It has been created by extracting from the iOS version any
    logic that will apply across platforms, specifically:
    * the browser agent's unique ID
    * the specific strings used for logging
    
    This work enables the addition of a desktop implementation, which will
    be added in a future CL.
    
    Bug: 1163762
    Change-Id: I37972a59253bddbd04f29b78188af73d333ade74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3273301
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#940976}

--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_browser_agent.mm
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_browser_agent.mm
@@ -47,9 +47,6 @@ BROWSER_USER_DATA_KEY_IMPL(BreadcrumbManagerBrowserAgent)
 
 BreadcrumbManagerBrowserAgent::BreadcrumbManagerBrowserAgent(Browser* browser)
     : browser_(browser) {
-  static int next_unique_id = 1;
-  unique_id_ = next_unique_id++;
-
   browser_->AddObserver(this);
   browser_->GetWebStateList()->AddObserver(this);
 
@@ -59,29 +56,15 @@ BreadcrumbManagerBrowserAgent::BreadcrumbManagerBrowserAgent(Browser* browser)
 
 BreadcrumbManagerBrowserAgent::~BreadcrumbManagerBrowserAgent() = default;
 
-bool BreadcrumbManagerBrowserAgent::IsLoggingEnabled() {
-  return logging_enabled_;
-}
-
-void BreadcrumbManagerBrowserAgent::SetLoggingEnabled(bool enabled) {
-  logging_enabled_ = enabled;
-}
-
 void BreadcrumbManagerBrowserAgent::BrowserDestroyed(Browser* browser) {
   browser_->GetWebStateList()->RemoveObserver(this);
   browser_->RemoveObserver(this);
 }
 
-void BreadcrumbManagerBrowserAgent::LogEvent(const std::string& event) {
-  if (!logging_enabled_) {
-    return;
-  }
-
-  breadcrumbs::BreadcrumbManagerKeyedService* breadcrumb_manager =
-      BreadcrumbManagerKeyedServiceFactory::GetInstance()->GetForBrowserState(
-          browser_->GetBrowserState());
-  breadcrumb_manager->AddEvent(
-      base::StringPrintf("Browser%d %s", unique_id_, event.c_str()));
+void BreadcrumbManagerBrowserAgent::PlatformLogEvent(const std::string& event) {
+  BreadcrumbManagerKeyedServiceFactory::GetInstance()
+      ->GetForBrowserState(browser_->GetBrowserState())
+      ->AddEvent(event);
 }
 
 void BreadcrumbManagerBrowserAgent::WebStateInsertedAt(
@@ -93,18 +76,14 @@ void BreadcrumbManagerBrowserAgent::WebStateInsertedAt(
     ++batch_operation_->insertion_count;
     return;
   }
-
-  const char* activating_string = activating ? "active" : "inactive";
-  LogEvent(base::StringPrintf("Insert %s Tab%d at %d", activating_string,
-                              GetTabId(web_state), index));
+  LogTabInsertedAt(GetTabId(web_state), index, activating);
 }
 
 void BreadcrumbManagerBrowserAgent::WebStateMoved(WebStateList* web_state_list,
                                                   web::WebState* web_state,
                                                   int from_index,
                                                   int to_index) {
-  LogEvent(base::StringPrintf("Moved Tab%d from %d to %d", GetTabId(web_state),
-                              from_index, to_index));
+  LogTabMoved(GetTabId(web_state), from_index, to_index);
 }
 
 void BreadcrumbManagerBrowserAgent::WebStateReplacedAt(
@@ -112,9 +91,7 @@ void BreadcrumbManagerBrowserAgent::WebStateReplacedAt(
     web::WebState* old_web_state,
     web::WebState* new_web_state,
     int index) {
-  LogEvent(base::StringPrintf("Replaced Tab%d with Tab%d at %d",
-                              GetTabId(old_web_state), GetTabId(new_web_state),
-                              index));
+  LogTabReplaced(GetTabId(old_web_state), GetTabId(new_web_state), index);
 }
 
 void BreadcrumbManagerBrowserAgent::WillCloseWebStateAt(
@@ -126,8 +103,7 @@ void BreadcrumbManagerBrowserAgent::WillCloseWebStateAt(
     ++batch_operation_->close_count;
     return;
   }
-
-  LogEvent(base::StringPrintf("Close Tab%d at %d", GetTabId(web_state), index));
+  LogTabClosedAt(GetTabId(web_state), index);
 }
 
 void BreadcrumbManagerBrowserAgent::WebStateActivatedAt(
@@ -136,20 +112,15 @@ void BreadcrumbManagerBrowserAgent::WebStateActivatedAt(
     web::WebState* new_web_state,
     int active_index,
     ActiveWebStateChangeReason reason) {
-  if (reason != ActiveWebStateChangeReason::Activated) {
+  if (reason != ActiveWebStateChangeReason::Activated)
     return;
-  }
-
-  std::vector<std::string> event = {"Switch"};
-  if (old_web_state) {
-    event.push_back(base::StringPrintf("from Tab%d", GetTabId(old_web_state)));
-  }
-  if (new_web_state) {
-    event.push_back(base::StringPrintf("to Tab%d at %d",
-                                       GetTabId(new_web_state), active_index));
-  }
-
-  LogEvent(base::JoinString(event, " "));
+  absl::optional<int> old_tab_id =
+      old_web_state ? absl::optional<int>(GetTabId(old_web_state))
+                    : absl::nullopt;
+  absl::optional<int> new_tab_id =
+      new_web_state ? absl::optional<int>(GetTabId(new_web_state))
+                    : absl::nullopt;
+  LogActiveTabChanged(old_tab_id, new_tab_id, active_index);
 }
 
 void BreadcrumbManagerBrowserAgent::WillBeginBatchOperation(
@@ -161,12 +132,10 @@ void BreadcrumbManagerBrowserAgent::BatchOperationEnded(
     WebStateList* web_state_list) {
   if (batch_operation_) {
     if (batch_operation_->insertion_count > 0) {
-      LogEvent(base::StringPrintf("Inserted %d tabs",
-                                  batch_operation_->insertion_count));
+      LogTabsInserted(batch_operation_->insertion_count);
     }
     if (batch_operation_->close_count > 0) {
-      LogEvent(
-          base::StringPrintf("Closed %d tabs", batch_operation_->close_count));
+      LogTabsClosed(batch_operation_->close_count);
     }
   }
   batch_operation_.reset();
