b06ed2d339731f3ae22fd2df813c5b72055377ab
manukh <manukh@chromium.org>
2021-11-11 16:42:49 +0000

    [memories] Accumulate keywords over 30 days of visits instead of 90.
    The ML model only provides keywords for 30 days of visits; processing
    more visits is potentially having a performance impact with no benefit.
    
    Bug: 1262016
    Change-Id: I423680b70e8cd2ad44c94f0ab29dfa81955b1e65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3269195
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940788}

--- components/history_clusters/core/history_clusters_service_unittest.cc
+++ components/history_clusters/core/history_clusters_service_unittest.cc
@@ -178,12 +178,16 @@ class HistoryClustersServiceTest : public testing::Test {
   }
 
   // Verifies that the hardcoded visits were passed to the clustering backend.
-  void AwaitAndVerifyTestClusteringBackendRequest() {
+  void AwaitAndVerifyTestClusteringBackendRequest(bool for_keywords = false) {
     test_clustering_backend_->WaitForGetClustersCall();
 
     std::vector<history::AnnotatedVisit> visits =
         test_clustering_backend_->LastClusteredVisits();
-    ASSERT_EQ(visits.size(), 2u);
+
+    // Keyword requests should not fetch visits older than 30 days; cluster
+    // requests should fetch all visits.
+    ASSERT_EQ(visits.size(), for_keywords ? 2u : 3u);
+
     auto& visit = visits[0];
     EXPECT_EQ(visit.visit_row.visit_id, 2);
     EXPECT_EQ(visit.visit_row.visit_time,
@@ -199,6 +203,11 @@ class HistoryClustersServiceTest : public testing::Test {
     EXPECT_EQ(visit.visit_row.visit_duration, base::Milliseconds(5600));
     EXPECT_EQ(visit.url_row.url(), "https://google.com/");
     EXPECT_EQ(visit.context_annotations.page_end_reason, 3);
+
+    if (!for_keywords) {
+      visit = visits[2];
+      EXPECT_EQ(visit.visit_row.visit_id, 4);
+    }
 
     // TODO(tommycli): Add back visit.referring_visit_id() check after updating
     //  the HistoryService test methods to support that field.
@@ -235,7 +244,8 @@ TEST_F(HistoryClustersServiceTest, ClusterAndVisitSorting) {
   AddHardcodedTestDataToHistoryService();
 
   history_clusters_service_->QueryClusters(
-      /*query=*/"", /*end_time=*/base::Time(), /* max_count=*/0,
+      /*query=*/"", /*begin_time=*/base::Time(), /*end_time=*/base::Time(),
+      /* max_count=*/0,
       // This "expect" block is not run until after the fake response is sent
       // further down in this method.
       base::BindLambdaForTesting([&](QueryClustersResult result) {
@@ -290,7 +300,7 @@ TEST_F(HistoryClustersServiceTest, ClusterAndVisitSorting) {
       "History.Clusters.Backend.NumClustersReturned",
       static_cast<int>(clusters.size()), 1);
   histogram_tester.ExpectUniqueSample(
-      "History.Clusters.Backend.NumVisitsToCluster", 2, 1);
+      "History.Clusters.Backend.NumVisitsToCluster", 3, 1);
   histogram_tester.ExpectUniqueSample(
       "History.Clusters.PercentClustersFilteredByQuery", 0, 1);
   histogram_tester.ExpectTotalCount(
@@ -301,7 +311,8 @@ TEST_F(HistoryClustersServiceTest, UnflattenDuplicatesIntegrationTest) {
   AddHardcodedTestDataToHistoryService();
 
   history_clusters_service_->QueryClusters(
-      /*query=*/"", /*end_time=*/base::Time(), /* max_count=*/0,
+      /*query=*/"", /*begin_time=*/base::Time(), /*end_time=*/base::Time(),
+      /* max_count=*/0,
       // This "expect" block is not run until after the fake response is sent
       // further down in this method.
       base::BindLambdaForTesting([&](QueryClustersResult result) {
@@ -433,7 +444,8 @@ TEST_F(HistoryClustersServiceTest, HardCapOnVisitsFetchedFromHistory) {
   history::BlockUntilHistoryProcessesPendingRequests(history_service_.get());
 
   history_clusters_service_->QueryClusters(
-      /*query=*/"", /*end_time=*/base::Time::Now(), /* max_count=*/0,
+      /*query=*/"", /*begin_time=*/base::Time(), /*end_time=*/base::Time::Now(),
+      /* max_count=*/0,
       base::DoNothing(),  // Only need to verify the correct request is sent.
       &task_tracker_);
 
@@ -464,7 +476,8 @@ TEST_F(HistoryClustersServiceTest, QueryClustersIncompleteAndPersistedVisits) {
                         // a non-visible page transition.
 
   history_clusters_service_->QueryClusters(
-      /*query=*/"", /*end_time=*/base::Time::Now(), /* max_count=*/0,
+      /*query=*/"", /*begin_time=*/base::Time(), /*end_time=*/base::Time::Now(),
+      /* max_count=*/0,
       base::DoNothing(),  // Only need to verify the correct request is sent.
       &task_tracker_);
 
@@ -524,7 +537,8 @@ TEST_F(HistoryClustersServiceTest, QueryClustersVariousQueries) {
     auto run_loop_quit = run_loop.QuitClosure();
 
     history_clusters_service_->QueryClusters(
-        test_data[i].query, /*end_time=*/base::Time(),
+        test_data[i].query, /*begin_time=*/base::Time(),
+        /*end_time=*/base::Time(),
         /* max_count=*/0,
         // This "expect" block is not run until after the fake response is sent
         // further down in this method.
@@ -618,7 +632,7 @@ TEST_F(HistoryClustersServiceTest, QueryClustersVariousQueries) {
   histogram_tester.ExpectBucketCount(
       "History.Clusters.Backend.NumClustersReturned", 2, base::size(test_data));
   histogram_tester.ExpectBucketCount(
-      "History.Clusters.Backend.NumVisitsToCluster", 2, base::size(test_data));
+      "History.Clusters.Backend.NumVisitsToCluster", 3, base::size(test_data));
   histogram_tester.ExpectBucketCount(
       "History.Clusters.PercentClustersFilteredByQuery", 0, 1);
   histogram_tester.ExpectBucketCount(
@@ -761,7 +775,9 @@ TEST_F(HistoryClustersServiceTest, DoesQueryMatchAnyCluster) {
   EXPECT_FALSE(history_clusters_service_->DoesQueryMatchAnyCluster("apples"));
 
   // Providing the response and running the task loop should populate the cache.
-  AwaitAndVerifyTestClusteringBackendRequest();
+  // This will also verify that visits older than 30 days are not included for
+  // keyword requests.
+  AwaitAndVerifyTestClusteringBackendRequest(true);
 
   std::vector<history::Cluster> clusters;
   clusters.push_back(
