bd99e0d61cf8233f63dd8ba464bcfc889b0b7ab6
Sky Malice <skym@chromium.org>
2021-11-11 02:10:29 +0000

    Revert "services/tracing: Clear data sources at test start"
    This reverts commit 269a8e12ce46779bfbd410e3c1a6b5484145d69c.
    
    Reason for revert: Caused EnablePerfettoSystemTracingDefaultState to start failing. See crbug.com/1262132
    
    Original change's description:
    > services/tracing: Clear data sources at test start
    >
    > Make sure the registered data sources are cleared when a test starts.
    > Otherwise the tracing service's built-in data sources might conflict
    > with the ones used by the test.
    >
    > We also enable SystemPerfettoTest.SystemTraceWhileLocalStartupTracing
    > which was previously disabled because of these failures.
    >
    > Bug: 1262132
    > Test: services_unittests --gtest_filter=SystemPerfettoTest.*:*Trac*
    > Change-Id: I75dda95ddab856cccb68119cd192a3346c92e85d
    > Cq-Include-Trybots: luci.chromium.try:linux-lacros-rel,linux-lacros-dbg
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268665
    > Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    > Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    > Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#939766}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1262132
    Change-Id: If02f7936fbeddb26b348930673fe47f52a55a687
    Cq-Include-Trybots: luci.chromium.try:linux-lacros-rel,linux-lacros-dbg
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3273702
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Owners-Override: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#940615}

--- services/tracing/perfetto/test_utils.cc
+++ services/tracing/perfetto/test_utils.cc
@@ -445,7 +445,6 @@ void TracingUnitTest::SetUp() {
   test_handle_ = PerfettoTracedProcess::SetupForTesting(
       base::ThreadTaskRunnerHandle::Get());
   PerfettoTracedProcess::Get()->OnThreadPoolAvailable();
-  PerfettoTracedProcess::Get()->ClearDataSourcesForTesting();
 
   // Wait for any posted construction tasks to execute.
   RunUntilIdle();
