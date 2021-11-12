e809b5c742ac128074e1a49d5af8c4b0c58d749d
Juliet Levesque <julietlevesque@google.com>
2021-11-12 23:45:29 +0000

    [Fast Pair] Provide battery info from not discoverable scanner
    Adds battery info from non discoverable advertisement to the bluetooth
    device.
    
    Change-Id: I33522fbe4e0f09ddf0897332561f217e4cc06bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278733
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#941382}

--- ash/quick_pair/scanning/fast_pair/fast_pair_not_discoverable_scanner.cc
+++ ash/quick_pair/scanning/fast_pair/fast_pair_not_discoverable_scanner.cc
@@ -37,7 +37,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace {
+
 constexpr int kMaxParseAdvertisementRetryCount = 5;
+
+device::BluetoothDevice::BatteryInfo GetBatteryInfo(
+    const ash::quick_pair::BatteryInfo& battery_info,
+    const device::BluetoothDevice::BatteryType& battery_type) {
+  return device::BluetoothDevice::BatteryInfo(
+      battery_type, battery_info.percentage,
+      battery_info.is_charging
+          ? device::BluetoothDevice::BatteryInfo::ChargeState::kCharging
+          : device::BluetoothDevice::BatteryInfo::ChargeState::kDischarging);
+}
+
+void SetBatteryInfo(
+    device::BluetoothDevice* device,
+    const ash::quick_pair::BatteryNotification& battery_notification) {
+  device::BluetoothDevice::BatteryInfo left_bud_info =
+      GetBatteryInfo(/*battery_info=*/battery_notification.left_bud_info,
+                     /*battery_type=*/device::BluetoothDevice::BatteryType::
+                         kLeftBudTrueWireless);
+  device->SetBatteryInfo(left_bud_info);
+
+  device::BluetoothDevice::BatteryInfo right_bud_info =
+      GetBatteryInfo(/*battery_info=*/battery_notification.right_bud_info,
+                     /*battery_type=*/device::BluetoothDevice::BatteryType::
+                         kRightBudTrueWireless);
+  device->SetBatteryInfo(right_bud_info);
+
+  device::BluetoothDevice::BatteryInfo case_info = GetBatteryInfo(
+      /*battery_info=*/battery_notification.case_info,
+      /*battery_type=*/device::BluetoothDevice::BatteryType::kCaseTrueWireless);
+  device->SetBatteryInfo(case_info);
+}
+
 }  // namespace
 
 namespace ash {
@@ -112,7 +145,15 @@ void FastPairNotDiscoverableScanner::OnAdvertisementParsed(
 
   advertisement_parse_attempts_.erase(it);
 
-  if (!advertisement || !advertisement->show_ui)
+  if (!advertisement)
+    return;
+
+  // Set the battery notification if the advertisement contains battery
+  // notification information
+  if (advertisement->battery_notification)
+    SetBatteryInfo(device, advertisement->battery_notification.value());
+
+  if (!advertisement->show_ui)
     return;
 
   auto filter_iterator =
