0383f3b7d486d37584db6a5bbd096e50dcbf474d
Florian Gauger <fga@google.com>
2021-11-12 12:01:51 +0000

    [Autofill Assistant] Remove dependency on //chrome/browser/ui/android/favicon:java
    Moving FaviconUtils to //components is not straightforward as it uses //chrome dependencies as public methods arguments.
    
    Bug: b/173103628
    Change-Id: I7c39b65b1df1de6f1369b6ae81a5cf51a44c4d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263941
    Commit-Queue: Florian Gauger <fga@google.com>
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#941129}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/generic_ui/AssistantDrawable.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/generic_ui/AssistantDrawable.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill_assistant.generic_ui;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -23,12 +24,12 @@ import org.chromium.base.annotations.JNINamespace;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill_assistant.AutofillAssistantUiController;
 import org.chromium.chrome.browser.autofill_assistant.drawable.AssistantDrawableIcon;
-import org.chromium.chrome.browser.ui.favicon.FaviconUtils;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
 import org.chromium.components.favicon.LargeIconBridge;
 import org.chromium.components.image_fetcher.ImageFetcher;
 import org.chromium.components.image_fetcher.ImageFetcherConfig;
 import org.chromium.components.image_fetcher.ImageFetcherFactory;
+import org.chromium.ui.base.ViewUtils;
 import org.chromium.url.GURL;
 
 /** Represents a view background. */
@@ -248,20 +249,34 @@ public abstract class AssistantDrawable {
             final LargeIconBridge iconBridge =
                     new LargeIconBridge(AutofillAssistantUiController.getProfile());
             iconBridge.getLargeIconForUrl(mUrl, mDiameterSizeInPixel,
-                    (Bitmap icon, int fallbackColor, boolean isFallbackColorDefault,
+                    (@Nullable Bitmap icon, int fallbackColor, boolean isFallbackColorDefault,
                             int iconType) -> {
-                        float fontSize = mDiameterSizeInPixel * 7f / 10f;
-                        RoundedIconGenerator roundedIconGenerator =
-                                new RoundedIconGenerator(mDiameterSizeInPixel, mDiameterSizeInPixel,
-                                        mDiameterSizeInPixel / 2,
-                                        ApiCompatibilityUtils.getColor(context.getResources(),
-                                                R.color.default_favicon_background_color),
-                                        fontSize);
-                        Drawable drawable = FaviconUtils.getIconDrawableWithoutFilter(
-                                mForceMonogram ? null : icon, mUrl, fallbackColor,
-                                roundedIconGenerator, context.getResources(), mDiameterSizeInPixel);
+                        Resources resources = context.getResources();
+                        boolean useMonogram = mForceMonogram || icon == null;
+
+                        Drawable drawable = useMonogram
+                                ? getMonogramDrawable(mUrl, fallbackColor, resources)
+                                : getDrawableFromFavicon(icon, resources, mDiameterSizeInPixel);
                         callback.onResult(drawable);
                     });
+        }
+
+        private Drawable getMonogramDrawable(GURL url, int fallbackColor, Resources resources) {
+            float fontSize = mDiameterSizeInPixel * 7f / 10f;
+            RoundedIconGenerator roundedIconGenerator = new RoundedIconGenerator(
+                    mDiameterSizeInPixel, mDiameterSizeInPixel, mDiameterSizeInPixel / 2,
+                    ApiCompatibilityUtils.getColor(
+                            resources, R.color.default_favicon_background_color),
+                    fontSize);
+            roundedIconGenerator.setBackgroundColor(fallbackColor);
+            Bitmap icon = roundedIconGenerator.generateIconForUrl(url);
+            return new BitmapDrawable(resources, icon);
+        }
+
+        private Drawable getDrawableFromFavicon(Bitmap icon, Resources resources, int iconSize) {
+            return ViewUtils.createRoundedBitmapDrawable(resources,
+                    Bitmap.createScaledBitmap(icon, iconSize, iconSize, false),
+                    resources.getDimensionPixelSize(R.dimen.default_favicon_corner_radius));
         }
     }
 }
