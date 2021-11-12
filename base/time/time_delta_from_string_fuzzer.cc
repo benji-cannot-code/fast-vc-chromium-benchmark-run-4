e0c1226fc6c923f1228ea22af7ee56a8f649a3db
Daniel Hosseinian <dhoss@chromium.org>
2021-11-12 15:12:30 +0000

    [base] Add some fuzz targets for //base/time/ parsing functions
    Add fuzzing for:
    - base::Time::FromString()
    - base::Time::FromUTCString()
    - base::Time::FromUTCExploded()
    - base::Time::FromLocalExploded()
    - base::TimeDeltaFromString()
    
    All of the above functions parse strings in production and/or perform
    calculations that are prone to overflows.
    
    Change-Id: I62ded616c52123025daeb942f44d2753171a4421
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277067
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941177}

---
+++ base/time/time_delta_from_string_fuzzer.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/strings/string_piece.h"
+#include "base/time/time.h"
+#include "base/time/time_delta_from_string.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::StringPiece input(reinterpret_cast<const char*>(data), size);
+  base::TimeDeltaFromString(input);
+  return 0;
+}
