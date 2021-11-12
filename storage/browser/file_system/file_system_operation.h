23c8a89e41e994235214641386df88ff533c7bfb
Jérémie Boulic <jboulic@chromium.org>
2021-11-12 00:12:25 +0000

    [Files app] Force cross-filesystem My files <-> Downloads copy/move
    Use the kForceCrossFilesystem CopyOrMoveOption to force cross-filesystem
    copy/move when:
    - The source and the destination are on different volumes
    - The copy/move happens between My files and Downloads.
    
    Add VolumeManager::FindVolumeFromPath.
    
    Fix CopyOrMoveOptionSet that didn't include kForceCrossFilesystem.
    
    Bug: b:204837171, 1200251
    Change-Id: I3e7cd9174264692787d4b2bc72733a7277f1c6ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3012438
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#940988}

--- storage/browser/file_system/file_system_operation.h
+++ storage/browser/file_system/file_system_operation.h
@@ -265,7 +265,7 @@ class FileSystemOperation {
     kForceCrossFilesystem,
 
     kFirst = kPreserveLastModified,
-    kLast = kPreserveDestinationPermissions
+    kLast = kForceCrossFilesystem
   };
 
   using CopyOrMoveOptionSet = base::EnumSet<CopyOrMoveOption,
