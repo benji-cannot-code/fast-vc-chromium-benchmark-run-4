9f48379ff9c52b2ff755dfa88430eb965935f052
Piotr Tworek <ptworek@vewd.com>
2021-11-12 12:40:38 +0000

    IWYU: ui/ozone/buildflags.h for OZONE_PLATFORM_X11.
    This is where OZONE_PLATFORM_X11 gets defined. The lack of ths include
    becomes visible when building chromedriver with use_ozone=false. With
    use_ozone=true required buildflag header gets pulled indirectly.
    
    Bug: None
    Change-Id: I115498f88689ccce57829db5d9543e4d17226378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263990
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/main@{#941138}

--- chrome/test/chromedriver/keycode_text_conversion_ozone.cc
+++ chrome/test/chromedriver/keycode_text_conversion_ozone.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
 #include "ui/events/ozone/layout/stub/stub_keyboard_layout_engine.h"
+#include "ui/ozone/buildflags.h"
 
 #if defined(USE_OZONE)
 #include "ui/base/ui_base_features.h"
