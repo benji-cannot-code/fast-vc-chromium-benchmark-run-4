2795fdd368d42ab542e2de5c4e9bc8de4622f53e
Rushan Suleymanov <rushans@google.com>
2021-11-12 19:05:19 +0000

    [Sync] Enable bookmarks reupload by default
    Bug: 1232951
    Change-Id: I34f3c809e0fd8f7c6bd8c8588446d93584e42f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278012
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#941244}

--- components/sync_bookmarks/switches.cc
+++ components/sync_bookmarks/switches.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 const base::Feature kSyncReuploadBookmarks{"SyncReuploadBookmarks",
-                                           base::FEATURE_DISABLED_BY_DEFAULT};
+                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSyncUseClientTagForBookmarkCommits{
     "SyncUseClientTagForBookmarkCommits", base::FEATURE_ENABLED_BY_DEFAULT};
