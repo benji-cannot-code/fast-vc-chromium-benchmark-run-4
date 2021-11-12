f00a791b6a7586515573e1821d5f096e4ce8c38a
Mark Yacoub <markyacoub@google.com>
2021-11-12 18:02:50 +0000

    drm: Disable not-preferred connected CRTCs to Connectors
    [Why]
    Devices such as Kevin choose a CRTC for tast test different than the preferred CRTC
    that Chrome would choose for the same connector, resulting in a connected
    CRTC that Chrome doesn't want, failing to modeset the preferred CRTC.
    
    [How]
    1. For security, the browser process can't modify the DRM, so create a parallel
    function to GetAvailableDisplayControllerInfos that modifies DRM and is
    called by the gpu.
    
    2. On getting available connectors, if the connector chooses a better
    CRTC but it currently has connected CRTC, disable that unpreferred CRTC.
    
    BUG=b:199333308
    TEST=cros_sdk tast run KEVIN_DUT graphics.DRM.drm_cursor_test graphics.Smoke.chrome
    
    Change-Id: I74418f7d35c07ab5a4e4dc12a1dfe33051b23d81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271634
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941224}

--- ui/ozone/platform/drm/gpu/drm_gpu_util.h
+++ ui/ozone/platform/drm/gpu/drm_gpu_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_OZONE_PLATFORM_DRM_GPU_DRM_GPU_UTIL_H_
 
 #include "base/trace_event/traced_value.h"
+#include "ui/ozone/platform/drm/common//drm_util.h"
 #include "ui/ozone/platform/drm/common/scoped_drm_types.h"
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
 
@@ -40,6 +41,11 @@ std::vector<display::GammaRampRGBEntry> ResampleLut(
 
 // Check DRM driver name match.
 bool IsDriverName(const char* device_file_name, const char* driver);
+
+// Returns the display infos parsed in
+// |GetDisplayInfosAndInvalidCrtcs| and disables the invalid CRTCs
+// that weren't picked as preferred CRTCs.
+HardwareDisplayControllerInfoList GetDisplayInfosAndUpdateCrtcs(int fd);
 
 void DrmAsValueIntoHelper(const drmModeModeInfo& mode_info,
                           base::trace_event::TracedValue* value);
