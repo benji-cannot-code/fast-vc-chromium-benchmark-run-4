1297098d48241f054b1d44e80f39b05faa98c7bd
dljames <dljames@google.com>
2021-11-12 21:48:59 +0000

    Removed Tab Groups Send Feedback flag
    Bug: 1269483
    Change-Id: I4e5a8d032f9982b212ff4cc69fbe6fc0135da101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279211
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941337}

--- chrome/browser/ui/ui_features.h
+++ chrome/browser/ui/ui_features.h
@@ -88,8 +88,6 @@ extern const base::Feature kTabGroupsAutoCreate;
 
 extern const base::Feature kTabGroupsCollapseFreezing;
 
-extern const base::Feature kTabGroupsFeedback;
-
 extern const base::Feature kTabGroupsNewBadgePromo;
 
 extern const base::Feature kTabGroupsSave;
