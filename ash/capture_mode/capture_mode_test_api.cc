732ce30a06ae35a5ae0e94e0b4e46b7a5176422d
Ahmed Fakhry <afakhry@chromium.org>
2021-11-12 02:02:10 +0000

    capture_mode: Support DLP warning mode at session init
    This is the first CL in a series of CLs that adds support
    for the DLP warning mode to capture mode.
    
    In this CL, a capture mode session initialization is continued only
    after DLP is checked for any restricted contents. Some of these
    contents may have a warning-only level restriction, in which case
    DLP will show a dialog to warn the user. The capture mode session
    initialization can either proceed or abort based on the user's
    interaction with the dialog.
    
    BUG=1253550
    TEST=Added browser tests
    
    Change-Id: I1f856acc46cb6c24ea206fcd07759b295f9d7e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277069
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941021}

--- ash/capture_mode/capture_mode_test_api.cc
+++ ash/capture_mode/capture_mode_test_api.cc
@@ -46,6 +46,10 @@ void CaptureModeTestApi::StartForRegion(bool for_video) {
   controller_->Start(CaptureModeEntryType::kQuickSettings);
 }
 
+bool CaptureModeTestApi::IsSessionActive() const {
+  return controller_->IsActive();
+}
+
 void CaptureModeTestApi::SetUserSelectedRegion(const gfx::Rect& region) {
   controller_->SetUserCaptureRegion(region, /*by_user=*/true);
 }
@@ -59,6 +63,10 @@ void CaptureModeTestApi::PerformCapture() {
 
 bool CaptureModeTestApi::IsVideoRecordingInProgress() const {
   return controller_->is_recording_in_progress();
+}
+
+bool CaptureModeTestApi::IsPendingDlpCheckOnSessionInit() const {
+  return controller_->pending_dlp_check_on_session_init_;
 }
 
 void CaptureModeTestApi::StopVideoRecording() {
