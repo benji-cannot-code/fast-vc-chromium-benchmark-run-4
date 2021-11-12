88840655486463d8e4f29ac7aa4e9e6edb09c5f9
bttk <bttk@google.com>
2021-11-12 19:38:27 +0000

    [Read Later] Android should fetch larger favicons
    When fetching favicons from Google, Chrome on Android always requested
    24dip. The 'dip' in the name of kDipForServerRequests created a false
    sense that the requested size would scale with the display density.
    
    Contents of ResourceBundle::InitSharedInstance suggest that the dip->px
    scaling factor on Android is always 1x and has nothing to do with the
    unit 'dp' used in Android.
    
    Feature LargeFaviconFromGoogle now makes it possible to set the
    requested icon size to 128px, which is equivalent to a 32dp drawable on
    a xxxhdpi display. In future the Android UI code could set the desired
    favicon size at runtime.
    
    Also, adding a DCHECK verifying that requests sent to the favicon
    service are supported size values.
    
    Bug: 1261876
    Change-Id: Ie9aa821b448106f9d220dbcb8d529f6a756a01a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268795
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941263}

--- chrome/browser/favicon/large_icon_service_factory.cc
+++ chrome/browser/favicon/large_icon_service_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/task/post_task.h"
 #include "build/build_config.h"
+#include "chrome/browser/browser_features.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
 #include "chrome/browser/image_fetcher/image_decoder_impl.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
@@ -24,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 #if defined(OS_ANDROID)
+// Seems like on Android `1 dip == 1 px`. The value of `kDipForServerRequests`
+// can be overridden by `features::kLargeFaviconFromGoogle`.
 const int kDipForServerRequests = 24;
 const favicon_base::IconType kIconTypeForServerRequests =
     favicon_base::IconType::kTouchIcon;
@@ -76,8 +79,16 @@ KeyedService* LargeIconServiceFactory::BuildServiceInstanceFor(
           std::make_unique<ImageDecoderImpl>(),
           profile->GetDefaultStoragePartition()
               ->GetURLLoaderFactoryForBrowserProcess()),
-      kDipForServerRequests, kIconTypeForServerRequests,
+      desired_size_in_dip_for_server_requests(), kIconTypeForServerRequests,
       kGoogleServerClientParam);
+}
+
+// static
+int LargeIconServiceFactory::desired_size_in_dip_for_server_requests() {
+  if (base::FeatureList::IsEnabled(features::kLargeFaviconFromGoogle)) {
+    return features::kLargeFaviconFromGoogleSizeInDip.Get();
+  }
+  return kDipForServerRequests;
 }
 
 bool LargeIconServiceFactory::ServiceIsNULLWhileTesting() const {
---
+++ chrome/browser/favicon/large_icon_service_factory_unittest.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/favicon/large_icon_service_factory.h"
+
+#include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
+#include "chrome/browser/browser_features.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class LargeIconServiceFactoryTest : public testing::Test {};
+
+TEST_F(LargeIconServiceFactoryTest, LargeFaviconFromGoogleDisabled) {
+  base::test::ScopedFeatureList features;
+  features.InitAndDisableFeature(features::kLargeFaviconFromGoogle);
+
+#if defined(OS_ANDROID)
+  const int expected = 24;
+#else
+  const int expected = 16;
+#endif
+
+  EXPECT_EQ(LargeIconServiceFactory::desired_size_in_dip_for_server_requests(),
+            expected);
+}
+
+TEST_F(LargeIconServiceFactoryTest, LargeFaviconFromGoogleEnabled) {
+  base::test::ScopedFeatureList features;
+  features.InitAndEnableFeatureWithParameters(features::kLargeFaviconFromGoogle,
+                                              {{"favicon_size_in_dip", "256"}});
+
+  EXPECT_EQ(LargeIconServiceFactory::desired_size_in_dip_for_server_requests(),
+            256);
+}
