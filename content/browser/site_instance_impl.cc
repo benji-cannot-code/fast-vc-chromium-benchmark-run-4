8885ac4ba052fd77ff69a85744d581c00fdb6930
W. James MacLean <wjmaclean@chromium.org>
2021-11-12 21:42:01 +0000

    Implement OAC Process Count Overhead Metric.
    This CL introduces a "process count overhead" metric, based on an
    estimate of how many additional processes are needed for
    OriginAgentCluster (OAC). It's an estimate since we can't control for
    all variables, like the coalescing of SiteInstances from multiple
    BrowsingInstances in a single RenderProcess, or the effects of all
    possible configurations of the SiteInfo parameters.
    
    The estimate is based on counting the total number of HTTPS
    SiteInstances (OAC is only used for HTTPS) by seeing how many unique
    SiteInfos they generate in each BrowsingInstance, then converting each
    SiteInfo to its non-OAC equivalent and counting again, and comparing
    the two counts. The difference is the overhead estimate. This value
    should only be reported for users with cross-process OAC enabled.
    
    Basically, this is a best effort approach because knowing the exact
    process count requires too much knowledge of process creation history.
    It ignores process sharing, which is usually (but not always) an upper
    bound on the actual overhead.
    
    Bug: 1171269
    Change-Id: I31781bc8dedcb7c4849776d82aacb2890f255142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3182871
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: W. James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941335}

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -1310,4 +1310,8 @@ void SiteInstanceImpl::WriteIntoTrace(
   proto->set_active_rfh_count(active_frame_count_);
 }
 
+int SiteInstanceImpl::EstimateOriginAgentClusterOverheadForMetrics() {
+  return browsing_instance_->EstimateOriginAgentClusterOverhead();
+}
+
 }  // namespace content
