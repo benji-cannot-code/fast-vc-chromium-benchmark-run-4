5e95a6419cc5452350e3bcfed4ad2dacbfa43be2
wutao <wutao@chromium.org>
2021-11-13 10:27:16 +0000

    Libassistant-v2: Clean up ServiceController
    This patch removes the public functions of assistant_manager() and
    assistant_manager_internal(), which are only used in test and can be
    accessed by assistant_client().
    
    Bug: b/189973552
    Test: Pass the test
    
    Change-Id: Ieb21e2529170745192147101d9420abb29a2c912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3245519
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941457}

--- chromeos/services/libassistant/service_controller.cc
+++ chromeos/services/libassistant/service_controller.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/libassistant/libassistant_factory.h"
 #include "chromeos/services/libassistant/settings_controller.h"
 #include "chromeos/services/libassistant/util.h"
-#include "libassistant/shared/internal_api/assistant_manager_internal.h"
+#include "libassistant/shared/public/assistant_manager.h"
 #include "services/network/public/cpp/cross_thread_pending_shared_url_loader_factory.h"
 #include "services/network/public/cpp/wrapper_shared_url_loader_factory.h"
 
@@ -266,16 +266,6 @@ bool ServiceController::IsRunning() const {
 
 AssistantClient* ServiceController::assistant_client() {
   return assistant_client_.get();
-}
-
-assistant_client::AssistantManager* ServiceController::assistant_manager() {
-  return assistant_client_ ? assistant_client_->assistant_manager() : nullptr;
-}
-
-assistant_client::AssistantManagerInternal*
-ServiceController::assistant_manager_internal() {
-  return assistant_client_ ? assistant_client_->assistant_manager_internal()
-                           : nullptr;
 }
 
 void ServiceController::OnAllServicesReady() {
--- chromeos/services/libassistant/service_controller_unittest.cc
+++ chromeos/services/libassistant/service_controller_unittest.cc
@@ -334,11 +334,12 @@ TEST_F(AssistantServiceControllerTest,
 
 TEST_F(AssistantServiceControllerTest,
        ShouldCreateAssistantManagerWhenCallingInitialize) {
-  EXPECT_EQ(nullptr, service_controller().assistant_manager());
+  EXPECT_EQ(nullptr, service_controller().assistant_client());
 
   Initialize();
 
-  EXPECT_NE(nullptr, service_controller().assistant_manager());
+  EXPECT_NE(nullptr,
+            service_controller().assistant_client()->assistant_manager());
 }
 
 TEST_F(AssistantServiceControllerTest, ShouldBeNoopWhenCallingStartTwice) {
@@ -374,7 +375,8 @@ TEST_F(AssistantServiceControllerTest, ShouldAllowStartAfterStop) {
   // The second Initialize() call should create the AssistantManager.
 
   Initialize();
-  EXPECT_NE(nullptr, service_controller().assistant_manager());
+  EXPECT_NE(nullptr,
+            service_controller().assistant_client()->assistant_manager());
 
   // The second Start() call should send out a state update.
 
@@ -385,18 +387,20 @@ TEST_F(AssistantServiceControllerTest, ShouldAllowStartAfterStop) {
 
   Start();
 
-  EXPECT_NE(nullptr, service_controller().assistant_manager());
+  EXPECT_NE(nullptr,
+            service_controller().assistant_client()->assistant_manager());
 }
 
 TEST_F(AssistantServiceControllerTest,
        ShouldDestroyAssistantManagerWhenCallingStop) {
   Initialize();
   Start();
-  EXPECT_NE(nullptr, service_controller().assistant_manager());
+  EXPECT_NE(nullptr,
+            service_controller().assistant_client()->assistant_manager());
 
   Stop();
 
-  EXPECT_EQ(nullptr, service_controller().assistant_manager());
+  EXPECT_EQ(nullptr, service_controller().assistant_client());
 }
 
 TEST_F(AssistantServiceControllerTest,
@@ -415,11 +419,13 @@ TEST_F(AssistantServiceControllerTest,
 
 TEST_F(AssistantServiceControllerTest,
        ShouldCreateButNotPublishAssistantManagerInternalWhenCallingInitialize) {
-  EXPECT_EQ(nullptr, service_controller().assistant_manager_internal());
+  EXPECT_EQ(nullptr, service_controller().assistant_client());
 
   Initialize();
 
-  EXPECT_NE(nullptr, service_controller().assistant_manager_internal());
+  EXPECT_NE(
+      nullptr,
+      service_controller().assistant_client()->assistant_manager_internal());
 }
 
 TEST_F(AssistantServiceControllerTest,
@@ -427,19 +433,23 @@ TEST_F(AssistantServiceControllerTest,
   Initialize();
   Start();
 
-  EXPECT_NE(nullptr, service_controller().assistant_manager_internal());
+  EXPECT_NE(
+      nullptr,
+      service_controller().assistant_client()->assistant_manager_internal());
 }
 
 TEST_F(AssistantServiceControllerTest,
        ShouldDestroyAssistantManagerInternalWhenCallingStop) {
   Initialize();
 
-  EXPECT_NE(nullptr, service_controller().assistant_manager_internal());
+  EXPECT_NE(
+      nullptr,
+      service_controller().assistant_client()->assistant_manager_internal());
 
   Start();
   Stop();
 
-  EXPECT_EQ(nullptr, service_controller().assistant_manager_internal());
+  EXPECT_EQ(nullptr, service_controller().assistant_client());
 }
 
 TEST_F(AssistantServiceControllerTest,
