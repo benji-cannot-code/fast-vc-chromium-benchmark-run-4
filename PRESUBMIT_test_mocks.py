a99411205d59178a47f62e65321e07fd8dab2faf
Kevin McNee <mcnee@chromium.org>
2021-11-15 16:09:29 +0000

    Add presubmit to cc an MPArch watchlist for uses of concerning APIs
    With the MPArch project which enables projects like bfcache,
    prerendering, and fenced frames, a number of assumptions in the code
    base relating to a tab only having one page at a time are no longer
    valid [1].
    
    In the interest of avoiding backsliding, let's set up a watchlist for
    uses of APIs which can contribute to these problematic assumptions.
    
    [1] https://docs.google.com/document/d/1NginQ8k0w3znuwTiJ5qjYmBKgZDekvEPC22q0I4swxQ/edit?usp=sharing
    
    Bug: 1190019
    Change-Id: If06c08a922a8d80129c59bd9ba03e726fed0f25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276815
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941694}

--- PRESUBMIT_test_mocks.py
+++ PRESUBMIT_test_mocks.py
@@ -170,7 +170,7 @@ class MockOutputApi(object):
     self.more_cc = []
 
   def AppendCC(self, more_cc):
-    self.more_cc.extend(more_cc)
+    self.more_cc.append(more_cc)
 
 
 class MockFile(object):
