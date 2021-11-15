3bd08b343175b82d54c854f78329d0eb874912c7
ckitagawa <ckitagawa@chromium.org>
2021-11-15 21:02:50 +0000

    [Paint Preview] Fix crash if closed before compositor ready
    Looks like this can crash is the feature is closed while a11y is
    initializing. Check if the objects are still live and abort otherwise.
    
    Bug: 1270424
    Change-Id: I4238fdd7cb77290cfed1d758235b8d1c6482d6ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282507
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941831}

--- components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameCoordinator.java
+++ components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameCoordinator.java
@@ -110,6 +110,8 @@ public class PlayerFrameCoordinator {
     }
 
     public PlayerFrameViewport getViewportForAccessibility() {
+        if (mMediator == null) return null;
+
         return mMediator.getViewport();
     }
 
