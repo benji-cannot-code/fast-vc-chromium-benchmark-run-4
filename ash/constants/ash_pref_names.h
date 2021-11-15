f4d52c03659c0cf9cc29be69266cf51e0f653ae2
Felipe Andrade <fsandrade@chromium.org>
2021-11-15 20:57:04 +0000

    Create ChromadToCloudMigrationEnabled policy
    Add a new device policy, called ChromadToCloudMigrationEnabled. It will
    be used to enable the migration of AD managed device into cloud
    management. This policy allows for a touchless migration of multiple
    devices in a company.
    
    Currently, the policy is just mapped info a local state pref. Later, it
    will be used to trigger a remote device powerwash, if the
    kEnrollmentIdUploadedOnChromad pref is also true.
    
    Bug: 1209246
    Change-Id: I99dd3fd86b7d9bdd89b59cf494dbe768eb8258b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282123
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941820}

--- ash/constants/ash_pref_names.h
+++ ash/constants/ash_pref_names.h
@@ -397,6 +397,8 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kPreconfiguredDeskTemplates[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kProjectorCreationFlowEnabled[];
 
+COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const char kChromadToCloudMigrationEnabled[];
 }  // namespace prefs
 }  // namespace ash
 
--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1282,6 +1282,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kQuickAnswersUnitConverstionEnabled,
     ash::quick_answers::prefs::kQuickAnswersUnitConverstionEnabled,
     base::Value::Type::BOOLEAN },
+  { key::kChromadToCloudMigrationEnabled,
+    ash::prefs::kChromadToCloudMigrationEnabled,
+    base::Value::Type::BOOLEAN },
 #endif // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if defined(OS_WIN)
