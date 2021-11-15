a2e3e81f2691f031d8096ee6f1c7376f7b9862b7
François Doray <fdoray@chromium.org>
2021-11-15 22:29:07 +0000

    [performance_manager] Support kLoadedBusy->kLoading transition in BackgroundTabLoadingPolicy.
    This transition can happen when a new load starts before the page
    reaches quiescence after the previous load. Supporting it is the last
    step before loading background tabs can be controlled from the
    Performance Manager.
    
    Bug: 1166745, 1059341
    Change-Id: I0472af8388733dfc7e922a2d11a633499927517d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271890
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Auto-Submit: François Doray <fdoray@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941867}

--- chrome/browser/resource_coordinator/tab_manager_resource_coordinator_signal_observer.h
+++ chrome/browser/resource_coordinator/tab_manager_resource_coordinator_signal_observer.h
@@ -40,7 +40,8 @@ class TabManager::ResourceCoordinatorSignalObserver
 
   // PageNode::ObserverDefaultImpl:
   // This function run on the performance manager sequence.
-  void OnLoadingStateChanged(const PageNode* page_node) override;
+  void OnLoadingStateChanged(const PageNode* page_node,
+                             PageNode::LoadingState previous_state) override;
 
   // GraphOwned implementation:
   void OnPassedToGraph(Graph* graph) override;
