d2fda0c6c9492b4d90d12fc52de1b61455f71ea4
Yuki Shiino <yukishiino@chromium.org>
2021-11-15 15:07:58 +0000

    bindings: Fix web_idl.Namespace.constants
    Fixes an unintentionally not-yet-implemented Python attribute
    of web_idl.Namespace.constants.
    
    Change-Id: I182a7ea69522525c9542501a2d0bf858f9c83725
    Bug: 1214607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3281685
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#941675}

--- third_party/blink/renderer/bindings/scripts/web_idl/namespace.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/namespace.py
@@ -135,7 +135,7 @@ class Namespace(UserDefinedType, WithExtendedAttributes, WithCodeGeneratorInfo,
     @property
     def constants(self):
         """Returns constants."""
-        return ()
+        return self._constants
 
     @property
     def constructors(self):
