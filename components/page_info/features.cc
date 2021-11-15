291c614cee0724976c61d3c82b20c2d5972aca78
Ehimare Okoyomon <eokoyomon@chromium.org>
2021-11-15 15:40:57 +0000

    [Android] Enable PageInfoHistory by default
    Bug: 1173154
    Change-Id: I92ed83cad310ca7a61cc2f81e5949fbd2c52b4bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275762
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941685}

--- components/page_info/features.cc
+++ components/page_info/features.cc
@@ -12,7 +12,7 @@ namespace page_info {
 
 #if defined(OS_ANDROID)
 const base::Feature kPageInfoHistory{"PageInfoHistory",
-                                     base::FEATURE_DISABLED_BY_DEFAULT};
+                                     base::FEATURE_ENABLED_BY_DEFAULT};
 const base::Feature kPageInfoStoreInfo{"PageInfoStoreInfo",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
