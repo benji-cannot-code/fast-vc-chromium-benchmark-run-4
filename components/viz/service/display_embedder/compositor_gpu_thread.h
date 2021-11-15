2b9fa6594bf4aafc4a06043b1c46bf19a2caebe5
Vikas Soni <vikassoni@chromium.org>
2021-11-15 17:32:35 +0000

    [Dr-Dc] CompositorGpuThread creates a VulkanContextProvider.
    CompositorGpuThread create a VulkanContextProvider which shared same
    vulkan device and queue as gpu main thread
    
    Bug: 1262385
    Change-Id: Idde7d161ca754f9ab4b8a5a10dede6c699d2fd4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251855
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941730}

--- components/viz/service/display_embedder/compositor_gpu_thread.h
+++ components/viz/service/display_embedder/compositor_gpu_thread.h
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 class GpuChannelManager;
+class VulkanImplementation;
+class VulkanDeviceQueue;
+class VulkanContextProvider;
 }  // namespace gpu
 
 namespace viz {
@@ -26,6 +29,8 @@ class VIZ_SERVICE_EXPORT CompositorGpuThread
  public:
   static std::unique_ptr<CompositorGpuThread> Create(
       gpu::GpuChannelManager* gpu_channel_manager,
+      gpu::VulkanImplementation* vulkan_implementation,
+      gpu::VulkanDeviceQueue* device_queue,
       bool enable_watchdog);
 
   // Disallow copy and assign.
@@ -36,6 +41,10 @@ class VIZ_SERVICE_EXPORT CompositorGpuThread
 
   scoped_refptr<gpu::SharedContextState> shared_context_state() const {
     return shared_context_state_;
+  }
+
+  VulkanContextProvider* vulkan_context_provider() const {
+    return vulkan_context_provider_.get();
   }
 
   // base::Thread implementation.
@@ -53,14 +62,18 @@ class VIZ_SERVICE_EXPORT CompositorGpuThread
   void OnForegrounded();
 
  private:
-  CompositorGpuThread(gpu::GpuChannelManager* gpu_channel_manager,
-                      bool enable_watchdog);
+  CompositorGpuThread(
+      gpu::GpuChannelManager* gpu_channel_manager,
+      scoped_refptr<VulkanContextProvider> vulkan_context_provider,
+      bool enable_watchdog);
 
   bool Initialize();
 
   gpu::GpuChannelManager* gpu_channel_manager_;
   const bool enable_watchdog_;
   bool init_succeded_ = false;
+
+  scoped_refptr<VulkanContextProvider> vulkan_context_provider_;
 
   // WatchdogThread to monitor CompositorGpuThread. Ensure that the members
   // which needs to be monitored by |watchdog_thread_| should be destroyed
