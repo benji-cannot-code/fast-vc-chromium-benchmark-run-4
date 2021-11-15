a6145338317256a45d269c79f3944737de3272d7
Shu Yang <shuyng@google.com>
2021-11-15 22:13:15 +0000

    [RDS] Add flag for Request Desktop Site per-site setting
    Add Finch Feature Flag REQUEST_DESKTOP_SITE_EXCEPTIONS;
    Add Chrome flag request-desktop-site-exceptions.
    
    Bug: 1269592
    Change-Id: Ib3859c1a92f6029f927c4de65b6fed0fefd7c3eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277440
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941839}

--- content/browser/android/content_feature_list.cc
+++ content/browser/android/content_feature_list.cc
@@ -26,6 +26,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kBindingManagementWaiveCpu,
     &features::kOnDemandAccessibilityEvents,
     &features::kProcessSharingWithStrictSiteInstances,
+    &features::kRequestDesktopSiteExceptions,
     &features::kRequestDesktopSiteGlobal,
     &features::kWebAuth,
     &features::kWebBluetoothNewPermissionsBackend,
