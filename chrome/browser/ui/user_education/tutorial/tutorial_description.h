a43859722f5f7c44ba57246af41537b91f70104d
David Pennington <dpenning@chromium.org>
2021-11-15 19:53:56 +0000

    Add Named Element steps to Tutorials
    Change-Id: I7b599f397e64d3a9825f8abb681ea2c92cd994a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3248677
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941792}

--- chrome/browser/ui/user_education/tutorial/tutorial_description.h
+++ chrome/browser/ui/user_education/tutorial/tutorial_description.h
@@ -37,6 +37,7 @@ struct TutorialDescription {
          absl::optional<std::u16string> body_text_,
          ui::InteractionSequence::StepType step_type_,
          ui::ElementIdentifier element_id_,
+         std::string element_name_,
          Arrow arrow_,
          absl::optional<bool> must_remain_visible_ = absl::nullopt);
     Step(const Step& step);
@@ -53,6 +54,10 @@ struct TutorialDescription {
 
     // the element used by interaction sequence to observe and attach a bubble.
     ui::ElementIdentifier element_id;
+
+    // the element, referred to by name, used by the interaction sequence
+    // to observe and potentially attach a bubble. must be non-empty.
+    std::string element_name;
 
     // the positioning of the bubble arrow
     Arrow arrow;
