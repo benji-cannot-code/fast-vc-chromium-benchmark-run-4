9f7bcb85330b80982e12d0ee60db73f20b21ed50
danielng <danielng@google.com>
2021-11-15 06:06:44 +0000

    borealis: add allowlist for external urls
    We want to restrict external URL handling to a set list of URLs (see
    bug) for Borealis.
    
    Bug: b/182861770
    Change-Id: I177bc30e30ba1456750d3446885167a869714126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3274392
    Commit-Queue: Daniel Ng <danielng@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Cr-Commit-Position: refs/heads/main@{#941579}

--- chrome/browser/ash/borealis/borealis_util.h
+++ chrome/browser/ash/borealis/borealis_util.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback_forward.h"
+#include "base/strings/string_piece.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/views/widget/widget.h"
 
@@ -29,6 +30,10 @@ extern const char kBorealisMainAppId[];
 extern const char kBorealisDlcName[];
 // The regex used for extracting the Borealis app ID of an application.
 extern const char kBorealisAppIdRegex[];
+// Base64-encoded allowed x-scheme for Borealis apps.
+extern const char kAllowedScheme[];
+// Base64-encoded allow list of URLs that can be handled by Borealis apps.
+extern const base::StringPiece kURLAllowlist[];
 
 // Shows the Borealis installer (borealis_installer_view).
 void ShowBorealisInstallerView(Profile* profile);
@@ -53,6 +58,10 @@ void FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
                      const std::string& app_id,
                      const std::string& window_title,
                      base::OnceCallback<void(GURL)> url_callback);
+
+// Checks that a given URL has the allowed scheme and that its contents starts
+// with one of the URLs in the allowlist.
+bool IsExternalURLAllowed(const GURL& url);
 
 }  // namespace borealis
 
--- chrome/browser/ash/guest_os/guest_os_external_protocol_handler.cc
+++ chrome/browser/ash/guest_os/guest_os_external_protocol_handler.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/ash/borealis/borealis_app_launcher.h"
 #include "chrome/browser/ash/borealis/borealis_service.h"
+#include "chrome/browser/ash/borealis/borealis_util.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/ash/guest_os/guest_os_registry_service_factory.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_files.h"
@@ -22,8 +23,13 @@ using VmType = guest_os::GuestOsRegistryService::VmType;
 
 bool AppHandlesProtocol(
     const guest_os::GuestOsRegistryService::Registration& app,
-    const std::string& scheme) {
-  return app.MimeTypes().count("x-scheme-handler/" + scheme) != 0;
+    const GURL& url) {
+  if (app.VmType() == guest_os::GuestOsRegistryService::VmType::
+                          ApplicationList_VmType_BOREALIS &&
+      !borealis::IsExternalURLAllowed(url)) {
+    return false;
+  }
+  return app.MimeTypes().count("x-scheme-handler/" + url.scheme()) != 0;
 }
 
 }  // namespace
@@ -42,7 +48,7 @@ absl::optional<GuestOsRegistryService::Registration> GetHandler(
   absl::optional<GuestOsRegistryService::Registration> result;
   for (auto& pair : registry_service->GetEnabledApps()) {
     auto& registration = pair.second;
-    if (AppHandlesProtocol(registration, url.scheme()) &&
+    if (AppHandlesProtocol(registration, url) &&
         (!result || registration.LastLaunchTime() > result->LastLaunchTime())) {
       result = std::move(registration);
     }
