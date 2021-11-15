ecbc35595ea35dfc2d9af9ef2e5fb414ba692d40
Geoff Lang <geofflang@chromium.org>
2021-11-15 17:27:25 +0000

    Reset draw buffers when clearing the WebGL drawing buffer
    If the user changes the drawing buffer's draw buffer state, the WebGL
    back buffer clearing logic will clear the wrong (or no) buffer.
    Reset the draw buffers state before and after clearing the drawing
    buffer and disable merging clears when the draw buffer state is not
    default.
    
    Bug: 1257538
    Change-Id: I384b79328d8f2d5b25507b17b4fdaecedc45341c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279360
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941724}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1489,7 +1489,8 @@ WebGLRenderingContextBase::ClearIfComposited(
 
   // Determine if it's possible to combine the clear the user asked for and this
   // clear.
-  bool combined_clear = mask && !scissor_enabled_;
+  bool combined_clear =
+      mask && !scissor_enabled_ && back_draw_buffer_ == GL_BACK;
 
   ContextGL()->Disable(GL_SCISSOR_TEST);
   if (combined_clear && (mask & GL_COLOR_BUFFER_BIT)) {
@@ -1528,6 +1529,7 @@ WebGLRenderingContextBase::ClearIfComposited(
   ContextGL()->ColorMask(
       true, true, true,
       !GetDrawingBuffer()->DefaultBufferRequiresAlphaChannelToBePreserved());
+
   {
     ScopedDisableRasterizerDiscard scoped_disable(this,
                                                   rasterizer_discard_enabled_);
@@ -8842,6 +8844,9 @@ GLint WebGLRenderingContextBase::MaxColorAttachments() {
 
 void WebGLRenderingContextBase::SetBackDrawBuffer(GLenum buf) {
   back_draw_buffer_ = buf;
+  if (GetDrawingBuffer()) {
+    GetDrawingBuffer()->SetDrawBuffer(buf);
+  }
 }
 
 void WebGLRenderingContextBase::SetFramebuffer(GLenum target,
