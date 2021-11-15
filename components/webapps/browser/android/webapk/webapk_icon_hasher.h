85ccc4079f416998cf16859ea15675757ef85c5c
Francois Beaufort <beaufort.francois@gmail.com>
2021-11-15 22:21:31 +0000

    Fix use-after-free security error in WebApkIconHasher
    This CL makes sure that the WebApkIconHasher::OnImageDownloaded callback
    is not called when WebApkIconHasher::OnDownloadTimedOut is called as
    it deletes WebApkIconHasher instance.
    
    Bug: 1269307
    Change-Id: If1204434052959c1ec6bfee2501e186f9b32eee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3280083
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Auto-Submit: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941853}

--- components/webapps/browser/android/webapk/webapk_icon_hasher.h
+++ components/webapps/browser/android/webapk/webapk_icon_hasher.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -105,6 +106,8 @@ class WebApkIconHasher {
 
   // Fails WebApkIconHasher if the download takes too long.
   base::OneShotTimer download_timeout_timer_;
+
+  base::WeakPtrFactory<WebApkIconHasher> weak_ptr_factory_{this};
 };
 
 }  // namespace webapps
