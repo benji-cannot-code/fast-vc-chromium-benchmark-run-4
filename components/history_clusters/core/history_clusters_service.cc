0d1aa4a7cd495396c289a26ba5c125c90d1ddced
Tommy Li <tommycli@chromium.org>
2021-11-15 19:44:12 +0000

    [memories] Skip omnibox action generation for low end devices.
    We want to avoid any possibility of creating omnibox jank on low end
    devices.
    
    Bug: 1262016, 1268570
    Change-Id: Iffd1f1291d90eb9b520df8a118e0a74cb35722f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3273581
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Auto-Submit: Tommy Li <tommycli@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941788}

--- components/history_clusters/core/history_clusters_service.cc
+++ components/history_clusters/core/history_clusters_service.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/system/sys_info.h"
 #include "base/time/time.h"
 #include "base/time/time_to_iso8601.h"
 #include "base/timer/elapsed_timer.h"
@@ -427,6 +428,10 @@ void HistoryClustersService::RemoveVisits(
 bool HistoryClustersService::DoesQueryMatchAnyCluster(
     const std::string& query) {
   if (!base::FeatureList::IsEnabled(kJourneys))
+    return false;
+
+  // We don't want any omnibox jank for low-end devices.
+  if (base::SysInfo::IsLowEndDevice())
     return false;
 
   // 2 hour threshold chosen arbitrarily for cache refresh time.
