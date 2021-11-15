f6be8f9368de34fbde1ed8e33a741b87aef641ba
Luciano Pacheco <lucmult@chromium.org>
2021-11-15 04:30:53 +0000

    Files app & SWA: Add more debug logs to distinguish legacy from SWA
    Increase the log to WARNING so it's displayed in the system logs.
    
    Add logs for the legacy version to be explicit that the legacy is
    running.
    
    Add logs to the FileManagerUI to guarantee a SWA log even if there is
    any failures in the JS.
    
    Bug: b:199452030
    Change-Id: I4879fe46e27e00241ce67d361cbc79ac034f2fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278840
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941571}

--- ash/webui/file_manager/file_manager_ui.cc
+++ ash/webui/file_manager/file_manager_ui.cc
@@ -32,6 +32,8 @@ FileManagerUI::FileManagerUI(content::WebUI* web_ui,
 
   // Add ability to request chrome-untrusted: URLs
   web_ui->AddRequestableScheme(content::kChromeUIUntrustedScheme);
+
+  LOG(WARNING) << "Starting FileManagerUI";
 }
 
 content::WebUIDataSource* FileManagerUI::CreateTrustedAppDataSource() {
