7bd6f65829ea88c509a3b21a8ce3ec5fb75b17ae
Tim Sergeant <tsergeant@chromium.org>
2021-11-15 00:52:23 +0000

    [Intents] Convert file name field in Intent structs to SafeBaseName
    This migrates the existing String field to a structured type which does
    not allow unsafe characters like path separators. As this is the first
    usage of SafeBaseName, this CL addresses a couple of issues with using
    the class from a mojom struct.
    
    Bug: 1238215
    Change-Id: I0370c08fe643fd7e882612c5dfdf06faeac54ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261501
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941545}

--- base/files/safe_base_name.cc
+++ base/files/safe_base_name.cc
@@ -27,4 +27,8 @@ absl::optional<SafeBaseName> SafeBaseName::Create(
 
 SafeBaseName::SafeBaseName(const FilePath& path) : path_(path) {}
 
+bool SafeBaseName::operator==(const SafeBaseName& that) const {
+  return path_ == that.path_;
+}
+
 }  // namespace base
--- chrome/browser/resources/settings/chromeos/os_apps_page/permission_constants.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/permission_constants.js
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
 
+import '/app-management/file_path.mojom-lite.js';
+import '/app-management/image.mojom-lite.js';
+import '/app-management/safe_base_name.mojom-lite.js';
 import '/app-management/types.mojom-lite.js';
 
 
