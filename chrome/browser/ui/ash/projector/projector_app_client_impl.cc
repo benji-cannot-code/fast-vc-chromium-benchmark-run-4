cb0f5613ce64ee58b5623636133a366da17e5bd9
Mohsen Izadi <mohsen@chromium.org>
2021-11-16 15:10:30 +0000

    Revert "projector: Add PendingScreencastManager"
    This reverts commit e93d0432ce8718b1463ad980d5025e95e27f0ef4.
    
    Reason for revert: Causing use-of-uninitialized-value on Linux ChromiumOS MSan Tests bot:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/25403/overview
    
    Original change's description:
    > projector: Add PendingScreencastManager
    >
    > Add PendingScreencastManager to process pending
    > screencasts by observing the syncing status change
    > of DriveFsHostObserver::OnSyncingStatusUpdate.
    >
    > Bug: b/201468756
    > Change-Id: I988c9c0086bf009c828d51bf85b5aa247667651f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3227489
    > Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Reviewed-by: Li Lin <llin@chromium.org>
    > Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#941838}
    
    Bug: b/201468756
    Change-Id: I6616b6a281ccc7fc6b00e1802d009d8762d3990a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282393
    Auto-Submit: Mohsen Izadi <mohsen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Owners-Override: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942159}

--- chrome/browser/ui/ash/projector/projector_app_client_impl.cc
+++ chrome/browser/ui/ash/projector/projector_app_client_impl.cc
@@ -22,10 +22,7 @@ void ProjectorAppClientImpl::RegisterProfilePrefs(
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 }
 
-ProjectorAppClientImpl::ProjectorAppClientImpl()
-    : pending_screencast_manager_(base::BindRepeating(
-          &ProjectorAppClientImpl::NotifyScreencastsPendingStatusChanged,
-          base::Unretained(this))) {}
+ProjectorAppClientImpl::ProjectorAppClientImpl() = default;
 ProjectorAppClientImpl::~ProjectorAppClientImpl() = default;
 
 signin::IdentityManager* ProjectorAppClientImpl::GetIdentityManager() {
@@ -57,9 +54,11 @@ void ProjectorAppClientImpl::OnNewScreencastPreconditionChanged(
     observer.OnNewScreencastPreconditionChanged(can_start);
 }
 
+// TODO(b/201468756): Implement a PendingScreencastManager to provide the set
+// of PendingScreencast.
 const std::set<ash::PendingScreencast>&
 ProjectorAppClientImpl::GetPendingScreencasts() const {
-  return pending_screencast_manager_.GetPendingScreencasts();
+  return pending_screencasts_;
 }
 
 void ProjectorAppClientImpl::NotifyScreencastsPendingStatusChanged(
--- chrome/browser/ui/ash/projector/projector_app_client_impl.h
+++ chrome/browser/ui/ash/projector/projector_app_client_impl.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/projector_app/projector_app_client.h"
 #include "base/observer_list.h"
-#include "chrome/browser/ui/ash/projector/pending_screencast_manager.h"
 
 namespace network {
 namespace mojom {
@@ -46,7 +45,9 @@ class ProjectorAppClientImpl : public ash::ProjectorAppClient {
       const std::set<ash::PendingScreencast>& pending_screencast);
 
   base::ObserverList<Observer> observers_;
-  PendingSreencastManager pending_screencast_manager_;
+
+  // TODO(b/201468756): Create a PendingScreencastManager to own this set.
+  std::set<ash::PendingScreencast> pending_screencasts_;
 };
 
 #endif  // CHROME_BROWSER_UI_ASH_PROJECTOR_PROJECTOR_APP_CLIENT_IMPL_H_
