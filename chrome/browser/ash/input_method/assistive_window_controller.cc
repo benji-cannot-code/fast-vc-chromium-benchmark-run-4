c737bda9be4c078249ebfeee7a3838289d59b250
Curtis McMullan <curtismcmullan@chromium.org>
2021-11-16 06:48:30 +0000

    multiword: use composition bounds when lacros disabled
    Currently we use the caret bounds when positioning the suggestion window
    ui for multi word suggestions. This is done to ensure the suggestion
    window is in the "approximately" right position when running the feature
    in a lacros or ash browser.
    
    However, the caret bounds are not always correct for all suggestions.
    This anchor will be slightly off for completion candidates. We should be
    using the current composition text bounds to anchor the suggestion ui.
    
    Given that ash browsers have no issues with composition text, we should
    use the composition bounds when lacros support has been disabled. This
    CL does exactly that.
    
    Bug: 1146266
    Change-Id: I634f2e2d8340f349c8d77e7d3e36964d59e87a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277257
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942046}

--- chrome/browser/ash/input_method/assistive_window_controller.cc
+++ chrome/browser/ash/input_method/assistive_window_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/wm/window_util.h"
@@ -35,6 +36,10 @@ gfx::NativeView GetParentView() {
                     : ash::Shell::GetRootWindowForNewWindows(),
       ash::kShellWindowId_VirtualKeyboardContainer);
   return parent;
+}
+
+bool IsLacrosEnabled() {
+  return base::FeatureList::IsEnabled(chromeos::features::kLacrosSupport);
 }
 
 }  // namespace
@@ -157,15 +162,13 @@ void AssistiveWindowController::SetBounds(const Bounds& bounds) {
   // TODO(crbug/1112982): Investigate getting bounds to suggester before sending
   // show suggestion request.
   if (suggestion_window_view_ && !tracking_last_suggestion_) {
-    // TODO(crbug/1146266): Composition text is no longer available which means
-    //     bounds.composition_text is always 0x0. For the moment we can just use
-    //     bounds.caret to at least position the suggestion window in the right
-    //     location. Although for multiword completion suggestions, this window
-    //     will always be to the right of the correct position. Imagine the
-    //     following suggestion `ho|` -> `how are you`, with bounds.caret the
-    //     left edge of the window will be under the caret, whereas it should
-    //     be placed underneath the left edge of the `h` character.
-    suggestion_window_view_->SetAnchorRect(bounds.caret);
+    // TODO(crbug/1146266): When running the multi word feature with lacros,
+    //     composition mode is unavailable, thus we need to use the caret
+    //     bounds instead. Investigate how we can position the window correctly
+    //     without composition bounds.
+    suggestion_window_view_->SetAnchorRect(
+        (confirmed_length_ != 0 && !IsLacrosEnabled()) ? bounds.composition_text
+                                                       : bounds.caret);
   }
   if (grammar_suggestion_window_) {
     grammar_suggestion_window_->SetBounds(bounds_.caret);
