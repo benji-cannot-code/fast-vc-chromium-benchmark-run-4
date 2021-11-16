098f0751fc17c8faf4df803c7b75c661d7073a99
Shengsong Tan <sstan@chromium.org>
2021-11-16 09:25:21 +0000

    app_restore: Enable ghost window on P if flag is enabled
    BUG=b:205207561
    
    Change-Id: If8d373129b23d7e90b386f2c43bd28950eb88e91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268039
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942081}

--- chrome/browser/ash/app_restore/arc_window_utils.cc
+++ chrome/browser/ash/app_restore/arc_window_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_restore/full_restore_prefs.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_features.h"
 #include "components/app_restore/features.h"
 #include "components/exo/wm_helper.h"
 #include "components/prefs/pref_service.h"
@@ -16,15 +17,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
+#include "ui/views/window/caption_button_layout_constants.h"
 
 namespace {
 
-void ScaleToRoundedRect(apps::mojom::Rect* rect, double scale_factor) {
+void ScaleToRoundedRectWithHeightInsets(apps::mojom::Rect* rect,
+                                        double scale_factor,
+                                        int height) {
   if (rect == nullptr)
     return;
 
-  auto res_rect = gfx::ScaleToRoundedRect(
-      gfx::Rect(rect->x, rect->y, rect->width, rect->height), scale_factor);
+  gfx::Rect bounds = gfx::Rect(rect->x, rect->y, rect->width, rect->height);
+  if (height)
+    bounds.Inset(0, height, 0, 0);
+  auto res_rect = gfx::ScaleToRoundedRect(bounds, scale_factor);
   rect->x = res_rect.x();
   rect->y = res_rect.y();
   rect->width = res_rect.width();
@@ -38,7 +44,13 @@ namespace full_restore {
 
 bool IsArcGhostWindowEnabled() {
   if (!::full_restore::features::IsArcGhostWindowEnabled() ||
-      !arc::IsArcVmEnabled() || !exo::WMHelper::HasInstance()) {
+      !exo::WMHelper::HasInstance()) {
+    return false;
+  }
+
+  // Returens false if the feature not enabled on ARC P specifically.
+  if (!arc::IsArcVmEnabled() &&
+      !base::FeatureList::IsEnabled(features::kArcPiGhostWindow)) {
     return false;
   }
 
@@ -79,7 +91,17 @@ apps::mojom::WindowInfoPtr HandleArcWindowInfo(
     return window_info;
   }
 
-  ScaleToRoundedRect(window_info->bounds.get(), scale_factor.value());
+  // For ARC P, the window bounds in launch parameters should minus caption
+  // height.
+  int extra_caption_height = 0;
+  if (!arc::IsArcVmEnabled()) {
+    extra_caption_height =
+        views::GetCaptionButtonLayoutSize(
+            views::CaptionButtonLayoutSize::kNonBrowserCaption)
+            .height();
+  }
+  ScaleToRoundedRectWithHeightInsets(
+      window_info->bounds.get(), scale_factor.value(), extra_caption_height);
   return window_info;
 }
 
