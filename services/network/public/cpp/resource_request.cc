bee3a3339bc89c219df5cf907024e7ec02b24ff8
Adrienne Walker <enne@chromium.org>
2021-11-16 23:32:20 +0000

    appcache: remove remaining network service references
    Bug: 582750
    Change-Id: Ida251c1a8620e05b9c2d89dc6bd406cb769fa2f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3286369
    Auto-Submit: enne <enne@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942322}

--- services/network/public/cpp/resource_request.cc
+++ services/network/public/cpp/resource_request.cc
@@ -217,7 +217,6 @@ bool ResourceRequest::EqualsForTesting(const ResourceRequest& request) const {
          resource_type == request.resource_type &&
          priority == request.priority &&
          devtools_stack_id == request.devtools_stack_id &&
-         should_reset_appcache == request.should_reset_appcache &&
          is_external_request == request.is_external_request &&
          cors_preflight_policy == request.cors_preflight_policy &&
          originated_from_service_worker ==
