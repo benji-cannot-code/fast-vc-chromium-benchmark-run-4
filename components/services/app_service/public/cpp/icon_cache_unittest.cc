18c350e41242fecc53bfa793147c6553b669d4cf
Nancy Wang <nancylingwang@chromium.org>
2021-11-16 05:09:25 +0000

    Modify tests for the non mojom LoadIconFromIconKey interface.
    Modify test case to implement the non mojom LoadIconFromIconKey
    interface. Also modify some test cases to test both the non mojom
    LoadIconFromIconKey interface and the mojom LoadIconFromIconKey
    interface.
    
    Bug: 1253250
    
    Change-Id: Ibc68b43269329f9eb120ce0f1dae12f29496663a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276214
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942021}

--- components/services/app_service/public/cpp/icon_cache_unittest.cc
+++ components/services/app_service/public/cpp/icon_cache_unittest.cc
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/callback_helpers.h"
+#include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/cpp/icon_cache.h"
+#include "components/services/app_service/public/cpp/icon_types.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image_skia_rep.h"
 
-class AppsIconCacheTest : public testing::Test {
+class AppsIconCacheTest : public testing::Test,
+                          public ::testing::WithParamInterface<bool> {
  protected:
   enum class HitOrMiss {
     kHit,
@@ -31,18 +34,18 @@ class AppsIconCacheTest : public testing::Test {
 
    private:
     std::unique_ptr<Releaser> LoadIconFromIconKey(
-        apps::mojom::AppType app_type,
+        apps::AppType app_type,
         const std::string& app_id,
-        apps::mojom::IconKeyPtr icon_key,
-        apps::mojom::IconType icon_Type,
+        const apps::IconKey& icon_key,
+        apps::IconType icon_type,
         int32_t size_hint_in_dip,
         bool allow_placeholder_icon,
-        apps::mojom::Publisher::LoadIconCallback callback) override {
+        apps::LoadIconCallback callback) override {
       num_load_calls_++;
 
-      auto iv = apps::mojom::IconValue::New();
-      if (icon_Type == apps::mojom::IconType::kUncompressed) {
-        iv->icon_type = apps::mojom::IconType::kUncompressed;
+      auto iv = std::make_unique<apps::IconValue>();
+      if (icon_type == apps::IconType::kUncompressed) {
+        iv->icon_type = apps::IconType::kUncompressed;
         iv->uncompressed =
             gfx::ImageSkia(gfx::ImageSkiaRep(gfx::Size(1, 1), 1.0f));
         iv->is_placeholder_icon = return_placeholder_icons_;
@@ -52,24 +55,49 @@ class AppsIconCacheTest : public testing::Test {
       return nullptr;
     }
 
+    std::unique_ptr<Releaser> LoadIconFromIconKey(
+        apps::mojom::AppType app_type,
+        const std::string& app_id,
+        apps::mojom::IconKeyPtr mojom_icon_key,
+        apps::mojom::IconType icon_type,
+        int32_t size_hint_in_dip,
+        bool allow_placeholder_icon,
+        apps::mojom::Publisher::LoadIconCallback callback) override {
+      auto icon_key = apps::ConvertMojomIconKeyToIconKey(mojom_icon_key);
+      return LoadIconFromIconKey(
+          apps::ConvertMojomAppTypToAppType(app_type), app_id, *icon_key,
+          apps::ConvertMojomIconTypeToIconType(icon_type), size_hint_in_dip,
+          allow_placeholder_icon,
+          apps::IconValueToMojomIconValueCallback(std::move(callback)));
+    }
+
     int num_load_calls_ = 0;
     bool return_placeholder_icons_ = false;
   };
+
+  bool IsLoadIconWithoutMojomEnabled() const { return GetParam(); }
 
   UniqueReleaser LoadIcon(apps::IconLoader* loader,
                           FakeIconLoader* fake,
                           const std::string& app_id,
                           HitOrMiss expect_hom,
                           bool allow_placeholder_icon = false) {
-    static constexpr auto app_type = apps::mojom::AppType::kWeb;
-    static constexpr auto icon_type = apps::mojom::IconType::kUncompressed;
     static constexpr int32_t size_hint_in_dip = 1;
 
     int before = fake->NumLoadIconFromIconKeyCalls();
 
-    UniqueReleaser releaser =
-        loader->LoadIcon(app_type, app_id, icon_type, size_hint_in_dip,
-                         allow_placeholder_icon, base::DoNothing());
+    UniqueReleaser releaser;
+    if (IsLoadIconWithoutMojomEnabled()) {
+      static constexpr auto app_type = apps::AppType::kWeb;
+      static constexpr auto icon_type = apps::IconType::kUncompressed;
+      releaser = loader->LoadIcon(app_type, app_id, icon_type, size_hint_in_dip,
+                                  allow_placeholder_icon, base::DoNothing());
+    } else {
+      static constexpr auto app_type = apps::mojom::AppType::kWeb;
+      static constexpr auto icon_type = apps::mojom::IconType::kUncompressed;
+      releaser = loader->LoadIcon(app_type, app_id, icon_type, size_hint_in_dip,
+                                  allow_placeholder_icon, base::DoNothing());
+    }
 
     int after = fake->NumLoadIconFromIconKeyCalls();
     HitOrMiss actual_hom = (after == before) ? kHit : kMiss;
@@ -207,7 +235,7 @@ class AppsIconCacheTest : public testing::Test {
   }
 };
 
-TEST_F(AppsIconCacheTest, Eager) {
+TEST_P(AppsIconCacheTest, Eager) {
   static constexpr apps::IconCache::GarbageCollectionPolicy gc_policy =
       apps::IconCache::GarbageCollectionPolicy::kEager;
 
@@ -216,7 +244,7 @@ TEST_F(AppsIconCacheTest, Eager) {
   TestAfterZeroRefcount(gc_policy);
 }
 
-TEST_F(AppsIconCacheTest, ExplicitSweepReleasedIcons) {
+TEST_P(AppsIconCacheTest, ExplicitSweepReleasedIcons) {
   static constexpr apps::IconCache::GarbageCollectionPolicy gc_policy =
       apps::IconCache::GarbageCollectionPolicy::kExplicit;
 
@@ -225,7 +253,7 @@ TEST_F(AppsIconCacheTest, ExplicitSweepReleasedIcons) {
   TestAfterZeroRefcount(gc_policy);
 }
 
-TEST_F(AppsIconCacheTest, ExplicitRemoveIcons) {
+TEST_P(AppsIconCacheTest, ExplicitRemoveIcons) {
   static constexpr apps::IconCache::GarbageCollectionPolicy gc_policy =
       apps::IconCache::GarbageCollectionPolicy::kExplicit;
 
@@ -233,3 +261,9 @@ TEST_F(AppsIconCacheTest, ExplicitRemoveIcons) {
   TestPlaceholder(gc_policy);
   TestAfterZeroRefcount(gc_policy, true /* remove_icon */);
 }
+
+// The parameter indicates whether the kAppServiceLoadIconWithoutMojom feature
+// is enabled.
+INSTANTIATE_TEST_SUITE_P(All,
+                         AppsIconCacheTest,
+                         ::testing::Values(true, false));
