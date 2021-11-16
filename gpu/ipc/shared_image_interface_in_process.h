fb994ae04882e8a16a75a795e5fb9ceb1ae18683
Piotr Bialecki <bialpio@chromium.org>
2021-11-16 21:25:24 +0000

    Refactor SharedImageInterfaceInProcess ctor to work with GpuServiceImpl
    Change-Id: If424f020145a2d6bba471667a33d672c15586da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276705
    Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942287}

--- gpu/ipc/shared_image_interface_in_process.h
+++ gpu/ipc/shared_image_interface_in_process.h
@@ -13,12 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gpu {
 class MailboxManager;
-class SyncPointClientState;
-struct SyncToken;
 class SharedContextState;
 class SharedImageFactory;
 class SharedImageManager;
 class SingleTaskSequence;
+class SyncPointClientState;
+struct SyncToken;
 
 // This is an implementation of the SharedImageInterface to be used on the viz
 // compositor thread. This class also implements the corresponding parts
@@ -30,10 +30,33 @@ class GL_IN_PROCESS_CONTEXT_EXPORT SharedImageInterfaceInProcess
  public:
   using CommandBufferHelper =
       InProcessCommandBuffer::SharedImageInterfaceHelper;
+  // The callers must guarantee that the instances passed via pointers are kept
+  // alive for as long as the instance of this class is alive. This can be
+  // achieved by ensuring that the ownership of the created
+  // SharedImageInterfaceInProcess is the same as the ownership of the passed in
+  // pointers.
   SharedImageInterfaceInProcess(
       SingleTaskSequence* task_sequence,
       DisplayCompositorMemoryAndTaskControllerOnGpu* display_controller,
       std::unique_ptr<CommandBufferHelper> command_buffer_helper);
+  // The callers must guarantee that the instances passed via pointers are kept
+  // alive for as long as the instance of this class is alive. This can be
+  // achieved by ensuring that the ownership of the created
+  // SharedImageInterfaceInProcess is the same as the ownership of the passed in
+  // pointers.
+  SharedImageInterfaceInProcess(
+      SingleTaskSequence* task_sequence,
+      SyncPointManager* sync_point_manager,
+      const GpuPreferences& gpu_preferences,
+      const GpuDriverBugWorkarounds& gpu_workarounds,
+      const GpuFeatureInfo& gpu_feature_info,
+      gpu::SharedContextState* context_state,
+      MailboxManager* mailbox_manager,
+      SharedImageManager* shared_image_manager,
+      ImageFactory* image_factory,
+      MemoryTracker* tracker,
+      bool is_for_display_compositor = false,
+      std::unique_ptr<CommandBufferHelper> command_buffer_helper = nullptr);
 
   SharedImageInterfaceInProcess(const SharedImageInterfaceInProcess&) = delete;
   SharedImageInterfaceInProcess& operator=(
@@ -152,10 +175,12 @@ class GL_IN_PROCESS_CONTEXT_EXPORT SharedImageInterfaceInProcess
       const gpu::Mailbox& mailbox) override;
 
  private:
-  struct SharedImageFactoryInput;
+  // Parameters needed to be passed in to set up the class on the GPU.
+  // Needed since we cannot pass refcounted instances (e.g.
+  // gpu::SharedContextState) to base::BindOnce as raw pointers.
+  struct SetUpOnGpuParams;
 
-  void SetUpOnGpu(
-      DisplayCompositorMemoryAndTaskControllerOnGpu* display_controller);
+  void SetUpOnGpu(std::unique_ptr<SetUpOnGpuParams> params);
   void DestroyOnGpu(base::WaitableEvent* completion);
 
   SyncToken MakeSyncToken(uint64_t release_id) {
@@ -235,9 +260,9 @@ class GL_IN_PROCESS_CONTEXT_EXPORT SharedImageInterfaceInProcess
   SharedImageManager* shared_image_manager_;
 
   // Accessed on GPU thread.
-  MailboxManager* mailbox_manager_;
   scoped_refptr<SharedContextState> context_state_;
-  // Created and only used by this SharedImageInterface.
+  // This is a raw pointer for now since the ownership of SyncPointManager would
+  // be the same as the SharedImageInterfaceInProcess.
   SyncPointManager* sync_point_manager_;
   scoped_refptr<SyncPointClientState> sync_point_client_state_;
   std::unique_ptr<SharedImageFactory> shared_image_factory_;
