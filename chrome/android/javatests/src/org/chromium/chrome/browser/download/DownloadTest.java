1b3dc9b863f9c55b4dc816d58e368af697d23f7a
Lijin Shen <lazzzis@google.com>
2021-11-16 23:14:24 +0000

    Disable DownloadTest#testDangerousDownload on M
    Bug: 1270871
    Change-Id: I8f7cefd8b12b055ace860e1ecd17de5ab5e302ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3286743
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#942308}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTest.java
@@ -32,6 +32,7 @@ import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
@@ -243,6 +244,7 @@ public class DownloadTest implements CustomMainActivityStart {
     @Test
     @MediumTest
     @Feature({"Downloads"})
+    @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.N, message = "crbug.com/1270871")
     public void testDangerousDownload() throws Exception {
         mDownloadTestRule.loadUrl(mTestServer.getURL(TEST_DOWNLOAD_DIRECTORY + "dangerous.html"));
         waitForFocus();
