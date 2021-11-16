a79538e8e19f99d0448eb7068b4650bd44dbb895
Sean Kau <skau@chromium.org>
2021-11-16 00:47:24 +0000

    Move c/b/lacros/immersive_context* to ui/lacros
    This class is clearly for ui so it should be in the ui directory.
    
    Bug: 1266496
    Change-Id: I35456bd395f1779e85d77bffa641a9b923d3e093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3259854
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941941}

--- chrome/browser/ui/lacros/immersive_context_lacros.h
+++ chrome/browser/ui/lacros/immersive_context_lacros.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_LACROS_IMMERSIVE_CONTEXT_LACROS_H_
-#define CHROME_BROWSER_LACROS_IMMERSIVE_CONTEXT_LACROS_H_
+#ifndef CHROME_BROWSER_UI_LACROS_IMMERSIVE_CONTEXT_LACROS_H_
+#define CHROME_BROWSER_UI_LACROS_IMMERSIVE_CONTEXT_LACROS_H_
 
 #include "chromeos/ui/frame/immersive/immersive_context.h"
 
@@ -33,4 +33,4 @@ class ImmersiveContextLacros : chromeos::ImmersiveContext {
   bool DoesAnyWindowHaveCapture() override;
 };
 
-#endif  // CHROME_BROWSER_LACROS_IMMERSIVE_CONTEXT_LACROS_H_
+#endif  // CHROME_BROWSER_UI_LACROS_IMMERSIVE_CONTEXT_LACROS_H_
