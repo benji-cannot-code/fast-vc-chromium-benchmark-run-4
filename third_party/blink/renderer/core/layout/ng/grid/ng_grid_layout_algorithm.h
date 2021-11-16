11b1b3a52486317a27087740f72fed2bb8d0f80b
Ethan Jimenez <ethavar@microsoft.com>
2021-11-16 05:05:10 +0000

    [GridNG] Fix "MinMax after Layout" crash during baseline calculation
    Fixing a bug where a grid item with baseline alignment needs to be laid
    out during `ComputeMinMaxSizes`, causing a "MinMax after Layout" crash.
    
    Refactoring all uses of `Layout` that could be affected within a MinMax
    pass of the grid layout algorithm to use `NGDisableSideEffectsScope`.
    
    See https://docs.google.com/document/d/1HkMwnfr-JbP2SE0ktYalkeAutwciuy-GftLs3TOnBcI
    
    Bug: 1266688
    Change-Id: I84e7f0a372d376ef3cfa4a6767df78ac729fc37f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276669
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#942018}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
@@ -21,8 +21,10 @@ class NGGridPlacement;
 struct GridItemOffsets;
 struct NGGridProperties;
 
-enum class AxisEdge : uint8_t { kStart, kCenter, kEnd, kBaseline };
-enum class ItemType : uint8_t { kInGridFlow, kOutOfFlow };
+enum class AxisEdge { kStart, kCenter, kEnd, kBaseline };
+enum class BaselineType { kMajor, kMinor };
+enum class ItemType { kInGridFlow, kOutOfFlow };
+enum class SizingConstraint { kLayout, kMinContent, kMaxContent };
 
 // This enum corresponds to each step used to accommodate grid items across
 // intrinsic tracks according to their min and max track sizing functions, as
@@ -34,11 +36,6 @@ enum class GridItemContributionType {
   kForIntrinsicMaximums,
   kForMaxContentMaximums,
   kForFreeSpace,
-};
-
-enum class BaselineType : uint8_t {
-  kMajor,
-  kMinor,
 };
 
 struct GridItemIndices {
@@ -247,8 +244,6 @@ struct GridItemIndices {
    private:
     friend class NGGridLayoutAlgorithmTest;
 
-    enum class SizingConstraint { kLayout, kMinContent, kMaxContent };
-
     LayoutUnit Baseline(const NGGridGeometry& grid_geometry,
                         const GridItemData& grid_item,
                         const GridTrackSizingDirection track_direction) const;
@@ -304,16 +299,11 @@ struct GridItemIndices {
         const NGGridLayoutAlgorithmTrackCollection& track_collection,
         GridItems* grid_items) const;
 
-    // Returns 'true' if it's possible to layout a grid item.
-    bool CanLayoutGridItem(
-        const GridItemData& grid_item,
-        const NGConstraintSpace& space,
-        const GridTrackSizingDirection track_direction) const;
-
     // Determines the major/minor alignment baselines for each row/column based
     // on each item in |grid_items|, and stores the results in |grid_geometry|.
     void CalculateAlignmentBaselines(
         const GridTrackSizingDirection track_direction,
+        const SizingConstraint sizing_constraint,
         NGGridGeometry* grid_geometry,
         GridItems* grid_items,
         bool* needs_additional_pass = nullptr) const;
