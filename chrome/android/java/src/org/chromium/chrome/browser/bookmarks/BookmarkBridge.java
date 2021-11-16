f698711339e0eac9909722d99bcb3ff11aa93f3e
Luciano Pacheco <lucmult@chromium.org>
2021-11-16 03:06:34 +0000

    Revert "[Power Bookmarks] Add a shopping-specific menu item row"
    This reverts commit 1ad3593b6ad873574861b3f3180fb213b9f27450.
    
    Reason for revert: Suspect of breaking the build and closing the tree:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1270592
    
    Original change's description:
    > [Power Bookmarks] Add a shopping-specific menu item row
    >
    > Bug: 1264685
    > Change-Id: Ie41ae3e4627dbc896224b97773c45adf161b3d1e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252831
    > Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    > Reviewed-by: Theresa  <twellington@chromium.org>
    > Reviewed-by: Filip Gorski <fgorski@chromium.org>
    > Reviewed-by: Matt Jones <mdjones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#941982}
    
    TBR=fgorski@chromium.org,twellington@chromium.org,mdjones@chromium.org,wylieb@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: I1eb3a45e7901f4562775150e6702daddec6c1ee9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1264685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283906
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Owners-Override: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#941997}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
@@ -42,7 +42,6 @@ import java.util.List;
  */
 public class BookmarkBridge {
     private final Profile mProfile;
-    private boolean mIsDestroyed;
     private boolean mIsDoingExtensiveChanges;
     private long mNativeBookmarkBridge;
     private boolean mIsNativeBookmarkModelLoaded;
@@ -298,7 +297,6 @@ public class BookmarkBridge {
      * Destroys this instance so no further calls can be executed.
      */
     public void destroy() {
-        mIsDestroyed = true;
         if (mNativeBookmarkBridge != 0) {
             BookmarkBridgeJni.get().destroy(mNativeBookmarkBridge, BookmarkBridge.this);
             mNativeBookmarkBridge = 0;
@@ -306,11 +304,6 @@ public class BookmarkBridge {
             mDelayedBookmarkCallbacks.clear();
         }
         mObservers.clear();
-    }
-
-    /** Returns whether the bridge has been destroyed. */
-    public boolean isDestroyed() {
-        return mIsDestroyed;
     }
 
     /**
@@ -686,8 +679,7 @@ public class BookmarkBridge {
      * @param id The {@link BookmarkId} of the bookmark to fetch the meta for.
      * @return The meta or null if none exists.
      */
-    public @Nullable PowerBookmarkMeta getPowerBookmarkMeta(@Nullable BookmarkId id) {
-        if (id == null || mNativeBookmarkBridge == 0) return null;
+    public PowerBookmarkMeta getPowerBookmarkMeta(BookmarkId id) {
         byte[] protoBytes = BookmarkBridgeJni.get().getPowerBookmarkMeta(
                 mNativeBookmarkBridge, this, id.getId(), id.getType());
 
