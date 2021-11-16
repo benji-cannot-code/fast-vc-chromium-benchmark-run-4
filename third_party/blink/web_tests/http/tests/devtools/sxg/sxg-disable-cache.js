9c645bf1a3c59fc1c586adc9c4c9143c84986dae
Danil Somsikov <dsv@chromium.org>
2021-11-16 19:14:23 +0000

    Update and enable sxg-disable-cache.js layout test
    Bug: 1203169
    Change-Id: Ibbf8b91efc9b312bb45271abe8d6bf7febc78922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3284987
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942249}

--- third_party/blink/web_tests/http/tests/devtools/sxg/sxg-disable-cache.js
+++ third_party/blink/web_tests/http/tests/devtools/sxg/sxg-disable-cache.js
@@ -39,9 +39,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   async function addPrefetchAndWait(prefetchUrl, waitUrl) {
     const promise = new Promise(resolve => {
         TestRunner.addSniffer(SDK.NetworkDispatcher.prototype, 'loadingFinished', loadingFinished);
-        function loadingFinished(requestId, finishTime, encodedDataLength) {
+        function loadingFinished(event) {
           var request = NetworkTestRunner.networkLog().requestByManagerAndId(
-              TestRunner.networkManager, requestId);
+              TestRunner.networkManager, event.requestId);
           if (request.url() == waitUrl) {
             resolve();
           } else {
