5d40130da90db8ce219556bb7d76d8f933c22343
Brandon Wylie <wylieb@chromium.org>
2021-11-16 01:49:43 +0000

    [Power Bookmarks] Add a shopping-specific menu item row
    Bug: 1264685
    Change-Id: Ie41ae3e4627dbc896224b97773c45adf161b3d1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3252831
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#941982}

---
+++ chrome/browser/commerce/shopping_list/android/java/src/org/chromium/chrome/browser/commerce/shopping_list/ShoppingFeatures.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.commerce.shopping_list;
+
+import org.chromium.base.FeatureList;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+/** Self-documenting feature class for shopping.  */
+public class ShoppingFeatures {
+    /** Returns whether shopping is enabled. */
+    public static boolean isShoppingListEnabled() {
+        return FeatureList.isInitialized()
+                && ChromeFeatureList.isEnabled(ChromeFeatureList.SHOPPING_LIST);
+    }
+}
