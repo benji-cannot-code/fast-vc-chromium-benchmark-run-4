8591dc79454ec1da513096ecf04c85113acc9ee5
Tsuyoshi Horo <horo@chromium.org>
2021-11-16 14:01:44 +0000

    MPArch: Deny PermissionRequests from fenced frames
    Currently CHECK_EQ() at permission_request_manager.cc line 196 fails
    when the permission is requested from a fenced frame. This is because
    IsInactiveAndDisallowActivation() returns false for fenced frames.
    
    This CL fix this by checking IsNestedWithinFencedFrame() in
    PermissionContextBase::GetPermissionStatus().
    
    Bug: 1190825, 1259837
    Change-Id: Ieed75c6939d861da2f6fd2396fdbab7a43eb37a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220941
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Ravjit Singh Uppal <ravjit@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942118}

--- content/web_test/browser/web_test_permission_manager.cc
+++ content/web_test/browser/web_test_permission_manager.cc
@@ -62,6 +62,10 @@ void WebTestPermissionManager::RequestPermission(
     bool user_gesture,
     base::OnceCallback<void(blink::mojom::PermissionStatus)> callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  if (render_frame_host->IsNestedWithinFencedFrame()) {
+    std::move(callback).Run(blink::mojom::PermissionStatus::DENIED);
+    return;
+  }
 
   std::move(callback).Run(
       GetPermissionStatus(permission, requesting_origin,
@@ -78,6 +82,11 @@ void WebTestPermissionManager::RequestPermissions(
     base::OnceCallback<void(const std::vector<blink::mojom::PermissionStatus>&)>
         callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  if (render_frame_host->IsNestedWithinFencedFrame()) {
+    std::move(callback).Run(std::vector<blink::mojom::PermissionStatus>(
+        permissions.size(), blink::mojom::PermissionStatus::DENIED));
+    return;
+  }
 
   std::vector<blink::mojom::PermissionStatus> result;
   result.reserve(permissions.size());
@@ -139,6 +148,8 @@ WebTestPermissionManager::GetPermissionStatusForFrame(
     PermissionType permission,
     content::RenderFrameHost* render_frame_host,
     const GURL& requesting_origin) {
+  if (render_frame_host->IsNestedWithinFencedFrame())
+    return blink::mojom::PermissionStatus::DENIED;
   return GetPermissionStatus(
       permission, requesting_origin,
       content::WebContents::FromRenderFrameHost(render_frame_host)
