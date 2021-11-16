eaecf9a5a1ff0cdbd8e3aaf4a29a47de2ee7502e
Alexei Svitkine <asvitkine@chromium.org>
2021-11-16 22:01:32 +0000

    Update callsites to use WIN_-prefixed base::File flags.
    The flags have been renamed to make it clear that they only
    apply to Windows (see associated bug).
    
    If you're reviewing this CL, please verify your code's usage
    of the flag to ensure that the behavior is as intended. Thanks!
    
    This CL was uploaded by git cl split.
    
    R=huangs@chromium.org
    
    Bug: 1244149
    Change-Id: I08d4f6e886a699d852470e08025edb85b0a695ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279224
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942295}

--- components/zucchini/mapped_file_unittest.cc
+++ components/zucchini/mapped_file_unittest.cc
@@ -34,7 +34,7 @@ TEST_F(MappedFileWriterTest, Keep) {
   {
     using base::File;
     File file(file_path_, File::FLAG_CREATE_ALWAYS | File::FLAG_READ |
-                              File::FLAG_WRITE | File::FLAG_SHARE_DELETE |
+                              File::FLAG_WRITE | File::FLAG_WIN_SHARE_DELETE |
                               File::FLAG_CAN_DELETE_ON_CLOSE);
     MappedFileWriter file_writer(file_path_, std::move(file), 10);
     EXPECT_FALSE(file_writer.HasError());
@@ -50,7 +50,7 @@ TEST_F(MappedFileWriterTest, DeleteOnClose) {
   {
     using base::File;
     File file(file_path_, File::FLAG_CREATE_ALWAYS | File::FLAG_READ |
-                              File::FLAG_WRITE | File::FLAG_SHARE_DELETE |
+                              File::FLAG_WRITE | File::FLAG_WIN_SHARE_DELETE |
                               File::FLAG_CAN_DELETE_ON_CLOSE);
     MappedFileWriter file_writer(file_path_, std::move(file), 10);
     EXPECT_FALSE(file_writer.HasError());
