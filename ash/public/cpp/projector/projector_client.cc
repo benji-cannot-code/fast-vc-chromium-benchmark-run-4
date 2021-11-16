a573162cfeb10f3575755e015aab7013fb52c081
yilkal <yilkal@chromium.org>
2021-11-16 23:54:08 +0000

    [Projector] Remove mocking Projector Controller in browser tests
    This cl removes mocking projector controller in browser tests.
    
    Bug: b/200205765
    Change-Id: Ic435477d2bf9a114e6475a721bace4f2eac5c28b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271151
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942335}

---
+++ ash/public/cpp/projector/projector_client.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/projector/projector_client.h"
+
+#include "base/check_op.h"
+
+namespace ash {
+
+namespace {
+ProjectorClient* g_instance_ = nullptr;
+}
+
+// static
+ProjectorClient* ProjectorClient::Get() {
+  return g_instance_;
+}
+
+ProjectorClient::ProjectorClient() {
+  DCHECK_EQ(g_instance_, nullptr);
+  g_instance_ = this;
+}
+
+ProjectorClient::~ProjectorClient() {
+  DCHECK_EQ(g_instance_, this);
+  g_instance_ = nullptr;
+}
+
+}  // namespace ash
