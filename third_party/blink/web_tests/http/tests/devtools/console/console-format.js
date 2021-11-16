50111287ce0552ce70818db825c8f7e237bf5000
Mathias Bynens <mathias@chromium.org>
2021-11-16 20:52:57 +0000

    [DevTools] Clarify console test expectations
    The recent work on improved display of properties removed the need
    for a restriction that prevented us from showing element properties
    on large TypedArrays before. The new behavior is strictly better,
    and the test content has been rewritten to clarify that this is now
    the expected and desired behavior.
    
    https://developer.chrome.com/blog/new-in-devtools-95/#properties
    
    Bug: chromium:1076820, chromium:1119900, chromium:1203169
    Change-Id: I0feb7e4c769a6ca5c83b68fdb5d9e99b4d1fd3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3284956
    Auto-Submit: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942274}

--- third_party/blink/web_tests/http/tests/devtools/console/console-format.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-format.js
@@ -80,7 +80,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       var smallTypedArray = new Uint8Array(new ArrayBuffer(400));
       smallTypedArray["foo"] = "bar";
       var bigTypedArray = new Uint8Array(new ArrayBuffer(400 * 1000 * 1000));
-      bigTypedArray["FAIL"] = "FAIL: Object.getOwnPropertyNames() should not have been run";
+      bigTypedArray.PASS = "Non-element properties should be displayed.";
       var namespace = {};
       namespace.longSubNamespace = {};
       namespace.longSubNamespace.x = {};
