1ed9362f0a7a6d93f3073787e0d727a6b1369260
Hazem Ashmawy <hazems@chromium.org>
2021-11-16 07:37:57 +0000

    Revert "Reenable SiteSettings#testProtectedContentAllowThenBlock"
    This reverts commit 8b7bf83359effbf8e3956537b739b7d62ed05dde.
    
    Reason for revert: test is still failing on builder https://ci.chromium.org/ui/p/chromium/builders/ci/android-11-x86-rel/1323
    
    Original change's description:
    > Reenable SiteSettings#testProtectedContentAllowThenBlock
    >
    > Restart testEmbargoedNotificationSiteSettings as it creates a OS level
    > settings activity, which causes the rest of the test suite flaky.
    >
    > Bug: 1269556
    > Change-Id: If42d5415154648c191f59a881659ba5e10ceeb41
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278488
    > Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    > Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#941629}
    
    TBR=andypaicu@chromium.org,chromium-scoped@luci-project-accounts.iam.gserviceaccount.com,wenyufu@chromium.org
    
    Change-Id: I4c464c58bc9716fc75a9fff6d6064993740289ea
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1269556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282148
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942055}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
@@ -34,7 +34,6 @@ import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.FlakyTest;
-import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.chrome.browser.browsing_data.BrowsingDataBridge;
 import org.chromium.chrome.browser.browsing_data.BrowsingDataType;
 import org.chromium.chrome.browser.browsing_data.TimePeriod;
@@ -1229,7 +1228,6 @@ public class SiteSettingsTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
-    @RequiresRestart("https://crbug.com/1269556")
     @DisableIf.
     Build(message = "Flaky, see crbug.com/1170671", sdk_is_less_than = Build.VERSION_CODES.Q)
     public void testEmbargoedNotificationSiteSettings() throws Exception {
@@ -1377,7 +1375,8 @@ public class SiteSettingsTest {
     @Test
     @MediumTest
     @Feature({"Preferences"})
-    @DisableIf.Build(message = "EME not working before M", sdk_is_less_than = Build.VERSION_CODES.M)
+    @DisableIf.Build(message = "EME not working before M", sdk_is_less_than = Build.VERSION_CODES.M,
+        sdk_is_greater_than = Build.VERSION_CODES.Q)
     @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1234530
     public void testProtectedContentAllowThenBlock() throws Exception {
         initializeUpdateWaiter(true /* expectGranted */);
