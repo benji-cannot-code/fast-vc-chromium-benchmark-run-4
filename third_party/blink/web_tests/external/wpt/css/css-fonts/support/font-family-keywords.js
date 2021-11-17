a8f33092ff8790b248237a8f4108f4c586aefba6
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2021-11-17 15:45:32 +0000

    Import wpt@55a5e683c80f1254f413f498ad828859cbe15dcc
    Using wpt-import in Chromium 0c2619553d42ac02aee52e40b8af02337d48d784.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    guidou@chromium.org, armax@chromium.org:
      external/wpt/mediacapture-streams
    smcgruer@chromium.org:
      external/wpt/css/css-position
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Change-Id: I1969da68cfb6669dd15baaa0e1488366167d4d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289503
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#942621}

--- third_party/blink/web_tests/external/wpt/css/css-fonts/support/font-family-keywords.js
+++ third_party/blink/web_tests/external/wpt/css/css-fonts/support/font-family-keywords.js
@@ -1,18 +1,28 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var kGenericFontFamilyKeywords = ["serif",
-                                  "sans-serif",
-                                  "cursive",
-                                  "fantasy",
-                                  "monospace",
-                                  "system-ui",
-                                  "emoji",
-                                  "math",
-                                  "fangsong",
-                                  "ui-serif",
-                                  "ui-sans-serif",
-                                  "ui-monospace",
-                                  "ui-rounded"];
+// <generic-family> keywords, as specified in
+// https://drafts.csswg.org/css-fonts/#generic-family-value
+var kGenericFontFamilyKeywords = [
+    "serif",
+    "sans-serif",
+    "cursive",
+    "fantasy",
+    "monospace",
+    "system-ui",
+    "emoji",
+    "math",
+    "fangsong",
+    "ui-serif",
+    "ui-sans-serif",
+    "ui-monospace",
+    "ui-rounded",
+];
 
-var kWebKitPrefixKeywords = ["-webkit-body",
-                             "-webkit-standard",
-                             "-webkit-pictograph"];
+// <family-name> values that had/have web-exposed behavior in some browsers, but
+// are not defined in the specification.
+var kNonGenericFontFamilyKeywords = [
+    "NonGenericFontFamilyName",
+    "-webkit-body",
+    "-webkit-standard",
+    "-webkit-pictograph",
+    "BlinkMacSystemFont",
+];
