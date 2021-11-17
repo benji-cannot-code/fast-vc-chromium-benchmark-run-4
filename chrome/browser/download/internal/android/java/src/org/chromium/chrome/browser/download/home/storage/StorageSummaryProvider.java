f4200f4fe450f7b2594b2890cc738235bb2e58b7
Xing Liu <xingliu@chromium.org>
2021-11-17 19:34:40 +0000

    Download: Fix a crash on Q+ when Android mount no storage.
    Fix an NPE when Android mount no storage when
    DownloadDirectoryProvider.getPrimaryDownloadDirectory() returns null.
    
    Bug=1270946
    
    Change-Id: I2c4068a0091aaf812fa69cb26a9e2ad6ab449629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289982
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942759}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/storage/StorageSummaryProvider.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/storage/StorageSummaryProvider.java
@@ -83,6 +83,8 @@ public class StorageSummaryProvider implements OfflineItemFilterObserver {
             @Override
             protected DirectoryOption doInBackground() {
                 File defaultDownloadDir = DownloadDirectoryProvider.getPrimaryDownloadDirectory();
+                if (defaultDownloadDir == null) return null;
+
                 DirectoryOption directoryOption = new DirectoryOption("",
                         defaultDownloadDir.getAbsolutePath(), defaultDownloadDir.getUsableSpace(),
                         defaultDownloadDir.getTotalSpace(),
