2b8853c371e799c7edcadc4038a65b15ea4f7c26
Xianzhu Wang <wangxianzhu@chromium.org>
2021-11-17 03:37:52 +0000

    Make (PointF|SizeF|Vector2dF)::ToString more concise
    Use %g instead of %f for the components to make the output shorter
    e.g. for integer values. This is the same as what blink geometry types
    do.
    
    Bug: 738465
    Change-Id: I96f0cb150472ccecf5dd50768185fcfd2086abc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3281428
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942447}

--- ui/gfx/geometry/vector2d_f.cc
+++ ui/gfx/geometry/vector2d_f.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 std::string Vector2dF::ToString() const {
-  return base::StringPrintf("[%f %f]", x_, y_);
+  return base::StringPrintf("[%g %g]", x_, y_);
 }
 
 bool Vector2dF::IsZero() const {
