7997affc4aac4323299a852ccdd9d0374ace048b
Ben Franz <bfranz@chromium.org>
2021-11-17 09:12:52 +0000

    Enforce kSystemFeaturesDisableMode in Lacros
    When Lacros starts owning the web apps managed by kSystemFeaturesDisableList, their hiding is no longer guaranteed as the code is currently limited to ash. This change enables the code in Lacros
    
    Bug: 1251490
    Test: WebAppPolicyManagerTest
    Change-Id: Ib25fd30e93bcbbe18dbfc69674e4233a847acd7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275972
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942517}

--- chrome/browser/web_applications/extensions/web_app_policy_manager_unittest.cc
+++ chrome/browser/web_applications/extensions/web_app_policy_manager_unittest.cc
@@ -43,9 +43,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
+#if defined(OS_CHROMEOS)
 #include "chrome/browser/policy/system_features_disable_list_policy_handler.h"
 #include "components/policy/core/common/policy_pref_names.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // defined(OS_CHROMEOS)
 
 using sync_preferences::TestingPrefServiceSyncable;
 
@@ -920,7 +923,7 @@ TEST_P(WebAppPolicyManagerTest, InstallResultHistogram) {
   }
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if defined(OS_CHROMEOS)
 TEST_P(WebAppPolicyManagerTest, DisableWebApps) {
   policy_manager().Start();
   base::RunLoop().RunUntilIdle();
@@ -952,7 +955,7 @@ TEST_P(WebAppPolicyManagerTest, DisableWebApps) {
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(policy_manager().IsDisabledAppsModeHidden());
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // defined(OS_CHROMEOS)
 
 TEST_P(WebAppPolicyManagerTest, WebAppSettingsDynamicRefresh) {
   if (ShouldSkipPWASpecificTest())
