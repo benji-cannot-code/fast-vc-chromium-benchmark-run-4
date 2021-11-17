06f95fa6e2caf40b4c9a62ae7f113230fedd0d2e
Andrew Xu <andrewxu@chromium.org>
2021-11-17 14:07:20 +0000

    [Applist Sync] Refactor the code that inits new item position
    This CL makes the following changes:
    
    (1) Delete `AppListModelUpdater::CalculatePositionForNewItem()`.
    In this CL, `AppListSyncableService` is responsible for initializing
    the position when a new item is added.
    
    (2) `ChromeAppListItem::CalculateDefaultPositionIfApplicable()` does
    not call `model_updater_` anymore. In addition, moving the logic of
    calculating so called "natural position" to a separated function named
    `CalculateDefaultPositionForTest()`. Because in
    `ChromeAppListItem::Ctor()`, `model_updater_` is null only in tests.
    
    (3) Move the logic that resets the sorting order when entropy is high
    to `AppListSyncableService` from `AppListReorderDelegate`. With this
    CL, all functions of `AppListReorderDelegate` are const.
    
    This CL should not bring any noticeable differences.
    
    Work Todo: move `AppListReorderDelegate`'s public member functions to
    an independent header file.
    
    Bug: 1261899
    Change-Id: I981dea44876a6ee98381fa97a9a35db0b629ef1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271925
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942582}

--- chrome/browser/ui/app_list/app_service/app_service_app_model_builder_unittest.cc
+++ chrome/browser/ui/app_list/app_service/app_service_app_model_builder_unittest.cc
@@ -183,6 +183,13 @@ void VerifyIcon(const gfx::ImageSkia& src, const gfx::ImageSkia& dst) {
   }
 }
 
+void InitAppPosition(ChromeAppListItem* new_item) {
+  if (new_item->position().IsValid())
+    return;
+
+  new_item->SetChromePosition(new_item->CalculateDefaultPositionForTest());
+}
+
 }  // namespace
 
 class AppServiceAppModelBuilderTest : public AppListTestBase {
@@ -205,6 +212,7 @@ class AppServiceAppModelBuilderTest : public AppListTestBase {
 
  protected:
   void ResetBuilder() {
+    scoped_callback_.reset();
     builder_.reset();
     controller_.reset();
     model_updater_.reset();
@@ -221,10 +229,16 @@ class AppServiceAppModelBuilderTest : public AppListTestBase {
         /*profile=*/nullptr, /*reorder_delegate=*/nullptr);
     controller_ = std::make_unique<test::TestAppListControllerDelegate>();
     builder_ = std::make_unique<AppServiceAppModelBuilder>(controller_.get());
+    scoped_callback_ = std::make_unique<
+        AppServiceAppModelBuilder::ScopedAppPositionInitCallbackForTest>(
+        builder_.get(), base::BindRepeating(&InitAppPosition));
     builder_->Initialize(nullptr, testing_profile(), model_updater_.get());
   }
 
   apps::AppServiceTest app_service_test_;
+  std::unique_ptr<
+      AppServiceAppModelBuilder::ScopedAppPositionInitCallbackForTest>
+      scoped_callback_;
   std::unique_ptr<AppServiceAppModelBuilder> builder_;
   std::unique_ptr<FakeAppListModelUpdater> model_updater_;
   std::unique_ptr<test::TestAppListControllerDelegate> controller_;
@@ -438,6 +452,9 @@ TEST_F(ExtensionAppTest, HideWebStore) {
   FakeAppListModelUpdater model_updater1(/*profile=*/nullptr,
                                          /*reorder_delegate=*/nullptr);
   AppServiceAppModelBuilder builder1(controller_.get());
+  auto scoped_callback1 = std::make_unique<
+      AppServiceAppModelBuilder::ScopedAppPositionInitCallbackForTest>(
+      &builder1, base::BindRepeating(&InitAppPosition));
   builder1.Initialize(nullptr, profile_.get(), &model_updater1);
   EXPECT_TRUE(model_updater1.FindItem(store->id()));
   EXPECT_TRUE(model_updater1.FindItem(enterprise_store->id()));
@@ -453,6 +470,9 @@ TEST_F(ExtensionAppTest, HideWebStore) {
   FakeAppListModelUpdater model_updater2(/*profile=*/nullptr,
                                          /*reorder_delegate=*/nullptr);
   AppServiceAppModelBuilder builder2(controller_.get());
+  auto scoped_callback2 = std::make_unique<
+      AppServiceAppModelBuilder::ScopedAppPositionInitCallbackForTest>(
+      &builder2, base::BindRepeating(&InitAppPosition));
   builder2.Initialize(nullptr, profile_.get(), &model_updater2);
   app_service_test_.FlushMojoCalls();
   EXPECT_FALSE(model_updater2.FindItem(store->id()));
@@ -464,6 +484,10 @@ TEST_F(ExtensionAppTest, HideWebStore) {
   // Now the web stores should have appeared.
   EXPECT_TRUE(model_updater2.FindItem(store->id()));
   EXPECT_TRUE(model_updater2.FindItem(enterprise_store->id()));
+
+  // Destroy scoped callbacks before model builders.
+  scoped_callback1.reset();
+  scoped_callback2.reset();
 }
 
 TEST_F(ExtensionAppTest, DisableAndEnable) {
@@ -974,6 +998,7 @@ class PluginVmAppTest : public testing::Test {
 
   // Destroys any existing builder in the correct order.
   void ResetBuilder() {
+    scoped_callback_.reset();
     builder_.reset();
     controller_.reset();
     model_updater_.reset();
@@ -990,6 +1015,9 @@ class PluginVmAppTest : public testing::Test {
         /*profile=*/nullptr, /*reorder_delegate=*/nullptr);
     controller_ = std::make_unique<test::TestAppListControllerDelegate>();
     builder_ = std::make_unique<AppServiceAppModelBuilder>(controller_.get());
+    scoped_callback_ = std::make_unique<
+        AppServiceAppModelBuilder::ScopedAppPositionInitCallbackForTest>(
+        builder_.get(), base::BindRepeating(&InitAppPosition));
     builder_->Initialize(nullptr, testing_profile_.get(), model_updater_.get());
 
     RemoveApps(apps::mojom::AppType::kPluginVm, testing_profile_.get(),
@@ -1010,6 +1038,9 @@ class PluginVmAppTest : public testing::Test {
 
   apps::AppServiceTest app_service_test_;
   std::unique_ptr<AppServiceAppModelBuilder> builder_;
+  std::unique_ptr<
+      AppServiceAppModelBuilder::ScopedAppPositionInitCallbackForTest>
+      scoped_callback_;
   std::unique_ptr<FakeAppListModelUpdater> model_updater_;
   std::unique_ptr<test::TestAppListControllerDelegate> controller_;
 };
@@ -1071,6 +1102,9 @@ class BorealisAppTest : public AppServiceAppModelBuilderTest {
         /*profile=*/nullptr, /*reorder_delegate=*/nullptr);
     controller_ = std::make_unique<test::TestAppListControllerDelegate>();
     builder_ = std::make_unique<AppServiceAppModelBuilder>(controller_.get());
+    scoped_callback_ = std::make_unique<
+        AppServiceAppModelBuilder::ScopedAppPositionInitCallbackForTest>(
+        builder_.get(), base::BindRepeating(&InitAppPosition));
     builder_->Initialize(nullptr, testing_profile_.get(), model_updater_.get());
 
     RemoveApps(apps::mojom::AppType::kBorealis, testing_profile_.get(),
