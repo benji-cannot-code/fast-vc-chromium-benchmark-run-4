c6441c0fc3440bf21cf1d60a670bc672d1587827
Nate Fischer <ntfschr@chromium.org>
2021-11-17 09:11:13 +0000

    Safe Browsing: remove unused constant
    This removes an unused string constant. The string was used when
    chrome://safe-browsing was still under construction, but this has since
    launched.
    
    Test: autoninja -C out/Default monochrome_apk (official build args)
    Test: load "chrome://safe-browsing/match?type=malware" in WebView & Chrome
    Test: load "http://testsafebrowsing.appspot.com/s/phishing.html" in WebView & Chrome
    Change-Id: I76fee5fe647191f7087dccc24d93cd05f2d12d24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283955
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942515}

--- components/safe_browsing/core/common/web_ui_constants.h
+++ components/safe_browsing/core/common/web_ui_constants.h
@@ -10,7 +10,6 @@ namespace safe_browsing {
 
 extern const char kChromeUISafeBrowsingURL[];
 extern const char kChromeUISafeBrowsingHost[];
-extern const char kSbUnderConstruction[];
 extern const char kChromeUISafeBrowsingMatchBillingUrl[];
 extern const char kChromeUISafeBrowsingMatchMalwareUrl[];
 extern const char kChromeUISafeBrowsingMatchPhishingUrl[];
