474955b4f347ea9e1dcaa80bce35d320960b197d
adamta <adamta@google.com>
2021-11-17 19:07:35 +0000

    [iOS] Re-enable disabled NTP tests
    testAccessibility: Was caused by a missing label in Mulder. Seems to be
    fixed now.
    
    testPositionRestoredWithOmniboxFocused: NTP improvements since August
    seem to have fixed the test on iPads.
    
    testToggleFeedEnabled: The flakes happened when the header was not
    visible but a reference to it in the tests caused it to be not nil.
    Changed condition to check visibility instead of nil-checking.
    
    Bug: 1237925, 1194106, 1243222
    Change-Id: I4f17548e4538e8b9318ca5de73ee4d7f456e604f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290003
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942736}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_egtest.mm
@@ -161,8 +161,7 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 #pragma mark - Tests
 
 // Tests that all items are accessible on the home page.
-// TODO(crbug.com/1237925): Re-enable once misisng accessibility label is fixed.
-- (void)DISABLED_testAccessibility {
+- (void)testAccessibility {
   [ChromeEarlGrey verifyAccessibilityForCurrentScreen];
 }
 
@@ -488,9 +487,7 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 // Tests that when navigating back to the NTP while having the omnibox focused
 // and moved up, the scroll position restored is the position before the omnibox
 // is selected.
-// Disable the test due to ios official build failure.
-// TODO(crbug.com/1243222): enable the test with fix.
-- (void)DISABLED_testPositionRestoredWithOmniboxFocused {
+- (void)testPositionRestoredWithOmniboxFocused {
   [self addMostVisitedTile];
 
   // Add suggestions to be able to scroll on iPad.
@@ -777,8 +774,7 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
 
 // Test to ensure that feed can be enabled/disabled and that feed header changes
 // accordingly.
-// TODO(crbug.com/1194106): Flaky on ios-simulator-noncq.
-- (void)DISABLED_testToggleFeedEnabled {
+- (void)testToggleFeedEnabled {
   [self
       testNTPInitialPositionAndContent:[NewTabPageAppInterface collectionView]];
 
@@ -800,7 +796,7 @@ id<GREYMatcher> OmniboxWidthBetween(CGFloat width, CGFloat margin) {
   [self
       testNTPInitialPositionAndContent:[NewTabPageAppInterface collectionView]];
   [[EarlGrey selectElementWithMatcher:chrome_test_util::DiscoverHeaderLabel()]
-      assertWithMatcher:grey_nil()];
+      assertWithMatcher:grey_notVisible()];
   [self checkIfNTPIsScrollable];
 
   // Re-enable feed.
