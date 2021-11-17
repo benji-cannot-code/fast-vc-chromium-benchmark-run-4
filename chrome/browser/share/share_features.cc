9cc678b122722b608e5ca82e9b3fcfe8d5ba2389
Elly Fong-Jones <ellyjones@chromium.org>
2021-11-17 21:27:28 +0000

    sharehub: add #persist-share-hub-on-app-switch
    This flag causes the sharing hub to persist across app pause/resume,
    for use in a canary experiment. We theorize that most of the time
    dismissing at app switch (ie, when the user has started to share
    but not actually shared) is not what users will desire, so we'll be
    tracking share hub abandon rate and success rate on either arm of this
    behavior.
    
    Bug: 1260487
    Change-Id: I61a524aeeca1ee17809d035befd6c26c124a2495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3286325
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942816}

--- chrome/browser/share/share_features.cc
+++ chrome/browser/share/share_features.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace share {
 
+const base::Feature kPersistShareHubOnAppSwitch{
+    "PersistShareHubOnAppSwitch", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kSharingDesktopScreenshotsEdit{
     "SharingDesktopScreenshotsEdit", base::FEATURE_DISABLED_BY_DEFAULT};
 const base::Feature kSwapAndroidShareHubRows{"SwapAndroidShareHubRows",
--- chrome/browser/share/share_features.h
+++ chrome/browser/share/share_features.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace share {
 
+extern const base::Feature kPersistShareHubOnAppSwitch;
 extern const base::Feature kSharingDesktopScreenshotsEdit;
 extern const base::Feature kSwapAndroidShareHubRows;
 extern const base::Feature kUpcomingSharingFeatures;
