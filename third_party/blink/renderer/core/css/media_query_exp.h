c1f5a7c4ac94f6dc0c6bc9842f1490ec46514244
Anders Hartvoll Ruud <andruud@chromium.org>
2021-11-17 10:54:46 +0000

    [mq-4] Move value parsing to MediaQueryExpValue::Consume
    Currently it's not possible to consume a MediaQueryExpValue
    (representing <mf-value>) without creating an entire
    MediaQueryExp (representing <mf-feature>). This is not convenient for
    future <mf-range> parsing, since it can require multiple
    MediaQueryExpValues per MediaQueryExp.
    
    This CL refactors the value parsing such that MediaQueryExpValue
    can be produced directly.
    
    Bug: 962417
    Change-Id: I311f0699865ed7bed6b3d394e7821525b9000447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3269114
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#942543}

--- third_party/blink/renderer/core/css/media_query_exp.h
+++ third_party/blink/renderer/core/css/media_query_exp.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_MEDIA_QUERY_EXP_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_MEDIA_QUERY_EXP_H_
 
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_primitive_value.h"
 #include "third_party/blink/renderer/core/css/css_value.h"
@@ -116,6 +117,16 @@ class CORE_EXPORT MediaQueryExpValue {
   bool operator!=(const MediaQueryExpValue& other) const {
     return !(*this == other);
   }
+
+  // Consume a MediaQueryExpValue for the provided feature, which must already
+  // be lower-cased.
+  //
+  // absl::nullopt is returned on errors.
+  static absl::optional<MediaQueryExpValue> Consume(
+      const String& lower_media_feature,
+      CSSParserTokenRange&,
+      const CSSParserContext&,
+      const ExecutionContext*);
 
  private:
   enum class Type { kInvalid, kId, kNumeric, kRatio };
