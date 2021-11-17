16c1b56efaf358687dd938d496b8f5c14528a07c
Yao Li <yaohuali@google.com>
2021-11-17 05:08:48 +0000

    arc: [13/N] Move //{ => ash}/components/arc/d*
    This CL moves part of //components/arc:arc target to
    //ash/components/arc:arc
    
    Move:
    { => ash}components/arc/(dark_theme|disk_quota)
    
    This is part of the LaCrOs directory migration
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE
    
    Bug: b:129295708
    Test: CI
    Change-Id: If77320d1401008c7d1e66a61cef1c3d8df86d8da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3284134
    Commit-Queue: Yao Li <yaohuali@google.com>
    Auto-Submit: Yao Li <yaohuali@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942462}

--- ash/components/arc/dark_theme/arc_dark_theme_bridge.h
+++ ash/components/arc/dark_theme/arc_dark_theme_bridge.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_ARC_DARK_THEME_ARC_DARK_THEME_BRIDGE_H_
-#define COMPONENTS_ARC_DARK_THEME_ARC_DARK_THEME_BRIDGE_H_
+#ifndef ASH_COMPONENTS_ARC_DARK_THEME_ARC_DARK_THEME_BRIDGE_H_
+#define ASH_COMPONENTS_ARC_DARK_THEME_ARC_DARK_THEME_BRIDGE_H_
 
 #include "ash/public/cpp/style/color_mode_observer.h"
 #include "base/threading/thread_checker.h"
@@ -60,4 +60,4 @@ class ArcDarkThemeBridge : public KeyedService,
 
 }  // namespace arc
 
-#endif  // COMPONENTS_ARC_DARK_THEME_ARC_DARK_THEME_BRIDGE_H_
+#endif  // ASH_COMPONENTS_ARC_DARK_THEME_ARC_DARK_THEME_BRIDGE_H_
