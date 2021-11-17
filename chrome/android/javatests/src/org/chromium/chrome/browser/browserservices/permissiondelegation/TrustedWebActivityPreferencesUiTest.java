68d48bc3058233a6e26521cf21fa3531f199c0ee
Alex Ilin <alexilin@chromium.org>
2021-11-17 13:02:44 +0000

    [sheriff] Disable a flaky TrustedWebActivityPreferencesUiTest test
    This CL unconditionally disables
    TrustedWebActivityPreferencesUiTest#testSingleCategoryManagedBy test
    
    Bug: 1202711
    Change-Id: Idd2a9fcdfeab94caf2e65aa779b91492dd3f4b74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289307
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Owners-Override: Alex Ilin <alexilin@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942567}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPreferencesUiTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/permissiondelegation/TrustedWebActivityPreferencesUiTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.browserservices.permissiondelegation;
 
-import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 
 import androidx.test.filters.SmallTest;
@@ -18,7 +17,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ChromeApplicationImpl;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -67,10 +66,8 @@ public class TrustedWebActivityPreferencesUiTest {
     @Test
     @SmallTest
     @Feature({"Preferences"})
-    @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.P,
-            message = "This test is disabled on Android O because of https://crbug.com/1202711")
-    public void
-    testSingleCategoryManagedBy() throws Exception {
+    @DisabledTest(message = "https://crbug.com/1202711")
+    public void testSingleCategoryManagedBy() throws Exception {
         final String site = "http://example.com";
         final Origin origin = Origin.create(site);
 
