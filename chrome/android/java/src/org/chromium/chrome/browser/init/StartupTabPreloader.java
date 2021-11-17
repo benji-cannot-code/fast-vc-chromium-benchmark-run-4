2685d5d4758942698677d0cfdcd58d0730ee19b0
Alex Ilin <alexilin@chromium.org>
2021-11-17 14:18:32 +0000

    Revert "[StartupTabPreloader] Record startup loading metrics when tab is dropped"
    This reverts commit 13e5fde1d2408f9abfbf11768fce5f63ca1a34d9.
    
    Reason for revert: StartupTabPreloaderTest#testStartupTabPreloaderStartupLoadingMetricsRecordedWhenTabTaken
    is failing on Android x86 bots.
    
    See failed builds:
    - https://ci.chromium.org/ui/p/chromium/builders/ci/android-11-x86-rel/1351/overview
    - https://ci.chromium.org/ui/p/chromium/builders/ci/android-pie-x86-rel/6889/overview
    
    Original change's description:
    > [StartupTabPreloader] Record startup loading metrics when tab is dropped
    >
    > Startup loading metrics are recorded only if a second navigation doesn't
    > happen before the initial navigation at startup commits; otherwise, it
    > is assumed that the user has taken some action that interacts with
    > startup [1]. However, startup tab preloading is an exception to this
    > case: when a tab is preloaded and then dropped, a second "true initial
    > navigation" is started without the first one committing.
    >
    > The current logic means that most startup loading metrics are not
    > recorded when a tab is preloaded and then dropped at startup. Some are
    > recorded but are unreliable (e.g., metrics that are a minimum of two
    > other metrics, one of which is recorded in this case and the other of
    > which isn't).
    >
    > This CL fixes this issue in the following way:
    > - We pass ActivityTabStartupMetricsTracker to StartupTabPreloader
    > - We extend ActivityTabStartupMetricsTracker with the ability to reset
    >   its initial navigation state
    > - We have StartupTabPreloader notify ActivityTabStartupMetricsTracker
    >   when a tab preload is dropped so that
    >   ActivityTabStartupMetricsTracker can reset its initial navigation
    >   state
    >
    > We also add a test of this case that fails without the fix.
    >
    > Note that another implementation approach would have been to add the
    > ability for ActivityTabStartupMetricsTracker to observe
    > StartupTabPreloader. The reason that I went in this direction is that
    > I'm going to need to have StartupTabPreloader know about
    > ActivityTabStartupMetricsTracker in followup CLs in any case to be
    > able to do detailed metrics on startup tab preloading that incorporate
    > state that ActivityTabStartupMetricsTracker tracks.
    >
    > Bug: 1268901
    > Change-Id: I652fd6fbcc569ae9ad29eb5d1798a51d767d7092
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272413
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#942531}
    
    Bug: 1268901
    Change-Id: I92853d8e5a769b33fc62c644fbe74b9c17670305
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289265
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alex Ilin <alexilin@google.com>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942592}

--- chrome/android/java/src/org/chromium/chrome/browser/init/StartupTabPreloader.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/StartupTabPreloader.java
@@ -20,7 +20,6 @@ import org.chromium.chrome.browser.WebContentsFactory;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.DestroyObserver;
-import org.chromium.chrome.browser.metrics.ActivityTabStartupMetricsTracker;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileManager;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
@@ -43,7 +42,6 @@ import org.chromium.url.GURL;
 public class StartupTabPreloader implements ProfileManager.Observer, DestroyObserver {
     public static final String EXTRA_DISABLE_STARTUP_TAB_PRELOADER =
             "org.chromium.chrome.browser.init.DISABLE_STARTUP_TAB_PRELOADER";
-    private static boolean sFailNextTabMatchForTesting;
 
     private final Supplier<Intent> mIntentSupplier;
     private final ActivityLifecycleDispatcher mActivityLifecycleDispatcher;
@@ -53,22 +51,15 @@ public class StartupTabPreloader implements ProfileManager.Observer, DestroyObse
     private LoadUrlParams mLoadUrlParams;
     private Tab mTab;
     private StartupTabObserver mObserver;
-    private ActivityTabStartupMetricsTracker mStartupMetricsTracker;
-
-    public static void failNextTabMatchForTesting() {
-        sFailNextTabMatchForTesting = true;
-    }
 
     public StartupTabPreloader(Supplier<Intent> intentSupplier,
             ActivityLifecycleDispatcher activityLifecycleDispatcher, WindowAndroid windowAndroid,
-            TabCreatorManager tabCreatorManager, IntentHandler intentHandler,
-            ActivityTabStartupMetricsTracker startupMetricsTracker) {
+            TabCreatorManager tabCreatorManager, IntentHandler intentHandler) {
         mIntentSupplier = intentSupplier;
         mActivityLifecycleDispatcher = activityLifecycleDispatcher;
         mWindowAndroid = windowAndroid;
         mTabCreatorManager = tabCreatorManager;
         mIntentHandler = intentHandler;
-        mStartupMetricsTracker = startupMetricsTracker;
 
         mActivityLifecycleDispatcher.register(this);
         ProfileManager.addObserver(this);
@@ -95,17 +86,12 @@ public class StartupTabPreloader implements ProfileManager.Observer, DestroyObse
         if (mTab == null) return null;
 
         boolean tabMatches = type == mTab.getLaunchType()
-                && doLoadUrlParamsMatchForWarmupManagerNavigation(mLoadUrlParams, loadUrlParams)
-                && !sFailNextTabMatchForTesting;
-
-        sFailNextTabMatchForTesting = false;
+                && doLoadUrlParamsMatchForWarmupManagerNavigation(mLoadUrlParams, loadUrlParams);
 
         RecordHistogram.recordBooleanHistogram(
                 "Startup.Android.StartupTabPreloader.TabTaken", tabMatches);
 
         if (!tabMatches) {
-            mStartupMetricsTracker.onStartupTabPreloadDropped();
-
             mTab.destroy();
             mTab = null;
             mLoadUrlParams = null;
