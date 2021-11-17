3110385223c19937b27383fa632c9547cc58ce14
Yang Guo <yangguo@chromium.org>
2021-11-17 15:36:29 +0000

    Correctly scale mouse drag end position
    Fixed: 935919
    Change-Id: Ie4321ec8776025af3fb14e2300dceef3c4db69de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3284803
    Auto-Submit: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942616}

--- third_party/blink/renderer/core/input/mouse_event_manager.cc
+++ third_party/blink/renderer/core/input/mouse_event_manager.cc
@@ -157,6 +157,7 @@ void MouseEventManager::Clear() {
   mouse_down_may_start_drag_ = false;
   captures_dragging_ = false;
   is_mouse_position_unknown_ = true;
+  last_known_mouse_position_in_root_frame_ = PhysicalOffset();
   last_known_mouse_position_ = FloatPoint();
   last_known_mouse_screen_position_ = FloatPoint();
   mouse_pressed_ = false;
@@ -167,7 +168,7 @@ void MouseEventManager::Clear() {
   mouse_down_timestamp_ = base::TimeTicks();
   mouse_down_ = WebMouseEvent();
   svg_pan_ = false;
-  drag_start_pos_ = PhysicalOffset();
+  drag_start_pos_in_root_frame_ = PhysicalOffset();
   hover_state_dirty_ = false;
   ResetDragSource();
   ClearDragDataTransfer();
@@ -651,6 +652,8 @@ FloatPoint MouseEventManager::LastKnownMouseScreenPosition() {
 void MouseEventManager::SetLastKnownMousePosition(const WebMouseEvent& event) {
   is_mouse_position_unknown_ =
       event.GetType() == WebInputEvent::Type::kMouseLeave;
+  last_known_mouse_position_in_root_frame_ =
+      PhysicalOffset(FlooredIntPoint(event.PositionInRootFrame()));
   last_known_mouse_position_ = FloatPoint(event.PositionInWidget());
   last_known_mouse_screen_position_ = FloatPoint(event.PositionInScreen());
 }
@@ -704,7 +707,7 @@ WebInputEventResult MouseEventManager::HandleMousePressEvent(
 
   mouse_press_node_ = inner_node;
   frame_->GetDocument()->SetSequentialFocusNavigationStartingPoint(inner_node);
-  drag_start_pos_ =
+  drag_start_pos_in_root_frame_ =
       PhysicalOffset(FlooredIntPoint(event.Event().PositionInRootFrame()));
 
   mouse_pressed_ = true;
@@ -738,7 +741,7 @@ WebInputEventResult MouseEventManager::HandleMouseReleaseEvent(
 
   return frame_->GetEventHandler()
                  .GetSelectionController()
-                 .HandleMouseReleaseEvent(event, drag_start_pos_)
+                 .HandleMouseReleaseEvent(event, drag_start_pos_in_root_frame_)
              ? WebInputEventResult::kHandledSystem
              : WebInputEventResult::kNotHandled;
 }
@@ -746,9 +749,8 @@ WebInputEventResult MouseEventManager::HandleMouseReleaseEvent(
 void MouseEventManager::UpdateSelectionForMouseDrag() {
   frame_->GetEventHandler()
       .GetSelectionController()
-      .UpdateSelectionForMouseDrag(
-          drag_start_pos_,
-          PhysicalOffset::FromFloatPointRound(last_known_mouse_position_));
+      .UpdateSelectionForMouseDrag(drag_start_pos_in_root_frame_,
+                                   last_known_mouse_position_in_root_frame_);
 }
 
 bool MouseEventManager::HandleDragDropIfPossible(
@@ -854,8 +856,8 @@ WebInputEventResult MouseEventManager::HandleMouseDraggedEvent(
   mouse_down_may_start_drag_ = false;
 
   frame_->GetEventHandler().GetSelectionController().HandleMouseDraggedEvent(
-      event, mouse_down_pos_, drag_start_pos_,
-      PhysicalOffset::FromFloatPointRound(last_known_mouse_position_));
+      event, mouse_down_pos_, drag_start_pos_in_root_frame_,
+      last_known_mouse_position_in_root_frame_);
 
   // The call into HandleMouseDraggedEvent may have caused a re-layout,
   // so get the LayoutObject again.
