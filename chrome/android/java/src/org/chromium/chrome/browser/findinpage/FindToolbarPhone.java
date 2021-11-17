02177c9d77c77e5963d74876bfc87f784b85b1fb
Neil Coronado <nemco@google.com>
2021-11-17 01:52:35 +0000

    [GMNext] Migrate divider_line_bg_color to dynamic colors
    Migrate divider_line_bg_color to dynamic colors
    
    divider_line_bg_color should route into ?attr/colorSurfaceVariant
    
    Bug: 1263122
    Change-Id: I292c49c7a01cd080146ce75c659db2490944c762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3246258
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#942411}

--- chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbarPhone.java
@@ -15,6 +15,7 @@ import androidx.appcompat.content.res.AppCompatResources;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.components.browser_ui.styles.ChromeColors;
+import org.chromium.components.browser_ui.styles.SemanticColorUtils;
 
 /**
  * A phone specific version of the {@link FindToolbar}.
@@ -46,7 +47,6 @@ public class FindToolbarPhone extends FindToolbar {
     protected void updateVisualsForTabModel(boolean isIncognito) {
         int queryTextColorId;
         int queryHintTextColorId;
-        int dividerColorId;
         if (isIncognito) {
             setBackgroundColor(ChromeColors.getDefaultThemeColor(getContext(), true));
             ColorStateList white = ChromeColors.getPrimaryIconTint(getContext(), true);
@@ -55,7 +55,7 @@ public class FindToolbarPhone extends FindToolbar {
             ApiCompatibilityUtils.setImageTintList(mCloseFindButton, white);
             queryTextColorId = R.color.find_in_page_query_white_color;
             queryHintTextColorId = R.color.find_in_page_query_incognito_hint_color;
-            dividerColorId = R.color.white_alpha_12;
+            mDivider.setBackgroundResource(R.color.white_alpha_12);
         } else {
             setBackgroundColor(ChromeColors.getDefaultThemeColor(getContext(), false));
             ColorStateList dark = ChromeColors.getPrimaryIconTint(getContext(), false);
@@ -64,13 +64,12 @@ public class FindToolbarPhone extends FindToolbar {
             ApiCompatibilityUtils.setImageTintList(mCloseFindButton, dark);
             queryTextColorId = R.color.default_text_color_list;
             queryHintTextColorId = R.color.find_in_page_query_default_hint_color;
-            dividerColorId = R.color.divider_line_bg_color;
+            mDivider.setBackgroundColor(SemanticColorUtils.getDividerLineBgColor(getContext()));
         }
         mFindQuery.setTextColor(
                 AppCompatResources.getColorStateList(getContext(), queryTextColorId));
         mFindQuery.setHintTextColor(
                 ApiCompatibilityUtils.getColor(getContext().getResources(), queryHintTextColorId));
-        mDivider.setBackgroundResource(dividerColorId);
     }
 
     @Override
