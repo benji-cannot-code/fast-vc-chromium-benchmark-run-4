1bfe92e610fb240a81d889aa0af68e9d4ad7791b
Aga Wronska <agawronska@chromium.org>
2021-11-17 15:27:33 +0000

    Fix focus on parent handoff OOBE dialog
    Focus the dialog on show. Otherwise the a11y focus remains on the
    element form the previous screen and there is no screen reader
    announcement.
    Also remove unused include from the related file.
    
    Bug: 1228955, 1269455
    Change-Id: Ia18080646e22acc9d77000e442c32fbaf645a291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283525
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942612}

--- chrome/browser/ash/login/screens/parental_handoff_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/parental_handoff_screen_browsertest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/wizard_controller_screen_exit_waiter.h"
 #include "chrome/browser/ash/login/wizard_context.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
-#include "chrome/browser/supervised_user/supervised_user_features/supervised_user_features.h"
 #include "chrome/browser/supervised_user/supervised_user_service.h"
 #include "chrome/browser/ui/webui/chromeos/login/assistant_optin_flow_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/parental_handoff_screen_handler.h"
