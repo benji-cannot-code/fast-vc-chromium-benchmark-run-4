214604450480441c879acf51506f76811d38be79
Benjamin Lerman <qsr@chromium.org>
2021-11-17 15:57:09 +0000

    [font] Add FontEnumerationCache for Fuchsia
    Fuchsia doesn't yet handle enumerating available font. For now, hardcode
    the available fonts.
    
    Test: FontAccessChooserControllerTest.*
    Bug: 1265639 fxbug.dev/8912
    Change-Id: I07db4c6b2c72f3952fdb8555bd280923bda1ba1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282183
    Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942628}

--- content/browser/font_access/font_enumeration_cache.h
+++ content/browser/font_access/font_enumeration_cache.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/font_access/font_access.mojom.h"
 
 #if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
-    defined(OS_MAC)
+    defined(OS_MAC) || defined(OS_FUCHSIA)
 #define PLATFORM_HAS_LOCAL_FONT_ENUMERATION_IMPL 1
 #endif
 
