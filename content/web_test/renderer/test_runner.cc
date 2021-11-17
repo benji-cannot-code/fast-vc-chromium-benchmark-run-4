6d22aa35b3490042c505e9a3ad92ea0a51d0d365
Hiroshige Hayashizaki <hiroshige@chromium.org>
2021-11-17 05:11:10 +0000

    Clean up WebScriptSource constructors
    - Remove a constructor with start_line that is never used
    - Make a 1-argument constructor explicit
    
    This CL doesn't change the behavior.
    
    Bug: 1111134
    Change-Id: Idbb4ae8437ce542dc1a73c1990dfbcc7826f5208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261873
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942466}

--- content/web_test/renderer/test_runner.cc
+++ content/web_test/renderer/test_runner.cc
@@ -488,7 +488,7 @@ void TestRunnerBindings::Install(TestRunner* test_runner,
   // because WPT reftests never access this object.
   if (is_wpt_test && is_main_test_window && !web_frame->Parent() &&
       !web_frame->Opener()) {
-    web_frame->ExecuteScript(blink::WebString(
+    web_frame->ExecuteScript(blink::WebScriptSource(blink::WebString(
         R"(if (!window.testRunner._wpt_reftest_setup) {
           window.testRunner._wpt_reftest_setup = true;
 
@@ -517,7 +517,7 @@ void TestRunnerBindings::Install(TestRunner* test_runner,
               document.fonts.ready.then(() => window.testRunner.notifyDone());
             }
           });
-        })"));
+        })")));
   }
 }
 
@@ -1105,7 +1105,7 @@ TestRunnerBindings::EvaluateScriptInIsolatedWorldAndReturnValue(
   if (invalid_ || world_id <= 0 || world_id >= (1 << 29))
     return {};
 
-  blink::WebScriptSource source = blink::WebString::FromUTF8(script);
+  blink::WebScriptSource source(blink::WebString::FromUTF8(script));
   return GetWebFrame()->ExecuteScriptInIsolatedWorldAndReturnValue(
       world_id, source, blink::BackForwardCacheAware::kAllow);
 }
@@ -1116,7 +1116,7 @@ void TestRunnerBindings::EvaluateScriptInIsolatedWorld(
   if (invalid_ || world_id <= 0 || world_id >= (1 << 29))
     return;
 
-  blink::WebScriptSource source = blink::WebString::FromUTF8(script);
+  blink::WebScriptSource source(blink::WebString::FromUTF8(script));
   GetWebFrame()->ExecuteScriptInIsolatedWorld(
       world_id, source, blink::BackForwardCacheAware::kAllow);
 }
