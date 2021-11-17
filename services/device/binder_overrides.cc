ea026ea176b682709152dc3e479cc2d0f7d0762f
Yeunjoo Choi <ychoi@igalia.com>
2021-11-17 06:08:24 +0000

    [mparch] Ensure that fenced frames cannot close NFC connection
    This CL adds a test to ensure that fenced frames cannot close the NFC
    connection in the primary main frame. ScopedNFCOverrider is implemented
    and fake NFC instances intercepts Mojo calls for testing as well.
    
    Bug: 1248508
    Change-Id: I802f2cdcbdce8a5563a9fa0271cfe83af722c1d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3244737
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#942481}

--- services/device/binder_overrides.cc
+++ services/device/binder_overrides.cc
@@ -15,5 +15,12 @@ GeolocationContextBinder& GetGeolocationContextBinderOverride() {
   return *binder;
 }
 
+#if defined(OS_ANDROID)
+NFCProviderBinder& GetNFCProviderBinderOverride() {
+  static base::NoDestructor<NFCProviderBinder> binder;
+  return *binder;
+}
+#endif
+
 }  // namespace internal
 }  // namespace device
--- services/device/binder_overrides.h
+++ services/device/binder_overrides.h
@@ -8,8 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/component_export.h"
+#include "build/build_config.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "services/device/public/mojom/geolocation_context.mojom.h"
+
+#if defined(OS_ANDROID)
+#include "services/device/public/mojom/nfc_provider.mojom.h"
+#endif
 
 namespace device {
 namespace internal {
@@ -18,6 +23,13 @@ using GeolocationContextBinder = base::RepeatingCallback<void(
     mojo::PendingReceiver<device::mojom::GeolocationContext>)>;
 COMPONENT_EXPORT(DEVICE_SERVICE_BINDER_OVERRIDES)
 GeolocationContextBinder& GetGeolocationContextBinderOverride();
+
+#if defined(OS_ANDROID)
+using NFCProviderBinder = base::RepeatingCallback<void(
+    mojo::PendingReceiver<device::mojom::NFCProvider>)>;
+COMPONENT_EXPORT(DEVICE_SERVICE_BINDER_OVERRIDES)
+NFCProviderBinder& GetNFCProviderBinderOverride();
+#endif
 
 }  // namespace internal
 }  // namespace device
