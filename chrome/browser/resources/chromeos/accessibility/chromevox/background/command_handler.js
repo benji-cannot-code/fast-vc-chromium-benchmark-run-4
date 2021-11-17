d1b6f33c98b9191817917a6ddbda419b316f0896
David Tseng <dtseng@google.com>
2021-11-17 01:37:40 +0000

    ChromeVox: introduce the idea of inferred intents
    R=josiahk@google.com
    
    Fixed: 1255974
    Test: cq
    Change-Id: I66753f3447f872ecec5b0bfa1fa451079bd4a7f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283886
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#942402}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/command_handler.js
@@ -407,6 +407,16 @@ CommandHandler.onCommand = function(command) {
       speechProps['phoneticCharacters'] = true;
       current = current.move(cursors.Unit.CHARACTER, dir);
       break;
+    case 'nativeNextCharacter':
+    case 'nativePreviousCharacter':
+      if (DesktopAutomationHandler.instance.textEditHandler) {
+        DesktopAutomationHandler.instance.textEditHandler.injectInferredIntents(
+            [{
+              command: chrome.automation.IntentCommandType.MOVE_SELECTION,
+              textBoundary: chrome.automation.IntentTextBoundaryType.CHARACTER
+            }]);
+      }
+      return true;
     case 'nextWord':
       shouldSetSelection = true;
       didNavigate = true;
@@ -418,6 +428,16 @@ CommandHandler.onCommand = function(command) {
       didNavigate = true;
       current = current.move(cursors.Unit.WORD, dir);
       break;
+    case 'nativeNextWord':
+    case 'nativePreviousWord':
+      if (DesktopAutomationHandler.instance.textEditHandler) {
+        DesktopAutomationHandler.instance.textEditHandler.injectInferredIntents(
+            [{
+              command: chrome.automation.IntentCommandType.MOVE_SELECTION,
+              textBoundary: chrome.automation.IntentTextBoundaryType.WORD_END
+            }]);
+      }
+      return true;
     case 'forward':
     case 'nextLine':
       didNavigate = true;
