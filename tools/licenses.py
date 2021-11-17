72bc3f17bfdc2524bff413672c96a2971cb5254b
Takuto Ikuta <tikuta@chromium.org>
2021-11-17 12:59:24 +0000

    tools/licenses.py: use tempfile.TemporaryDirectory
    This is to see what happens for failure like
    https://ci.chromium.org/ui/p/chromium/builders/ci/win-archive-rel/28176/overview
    
    Bug: 1208393
    Change-Id: Ifa99c238a72a146f514223f4f81fae70bb99a105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288249
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942561}

--- tools/licenses.py
+++ tools/licenses.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -613,9 +613,7 @@ def FindThirdPartyDeps(gn_out_dir, gn_target, target_os):
   # Current gn directory cannot be used when we run this script in a gn action
   # rule, because gn doesn't allow recursive invocations due to potential side
   # effects.
-  tmp_dir = None
-  try:
-    tmp_dir = tempfile.mkdtemp(dir=gn_out_dir)
+  with tempfile.TemporaryDirectory(dir=gn_out_dir) as tmp_dir:
     shutil.copy(os.path.join(gn_out_dir, "args.gn"), tmp_dir)
     subprocess.check_output([
         _GnBinary(), "gen", "--root=%s" % _REPOSITORY_ROOT, tmp_dir
@@ -626,9 +624,6 @@ def FindThirdPartyDeps(gn_out_dir, gn_target, target_os):
     ])
     if isinstance(gn_deps, bytes):
       gn_deps = gn_deps.decode("utf-8")
-  finally:
-    if tmp_dir and os.path.exists(tmp_dir):
-      shutil.rmtree(tmp_dir)
 
   return GetThirdPartyDepsFromGNDepsOutput(gn_deps, target_os)
 
