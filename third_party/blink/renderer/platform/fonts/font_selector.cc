7fe0e537bb51bd120845dba61056a5cde3414a70
Frédéric Wang <fwang@igalia.com>
2021-11-17 13:36:00 +0000

    Move -webkit-standard handling on Android into FamilyNameFromSettings
    Regression crbug.com/1252383 was not detected early due to the
    complexity of the code handling "-webkit-standard" on Android. Contrary
    to other platforms (where "-webkit-standard" is immediately converted in
    FamilyNameFromSettings by reading a family name from user settings), the
    "-webkit-standard" family name gets passed down to
    FontCache::CreateTypeface where it is finally resolved via a call to
    GetFallbackFontFamily.
    
    This CL instead moves the check for the "-webkit-standard" family name
    into FamilyNameFromSettings and removes any special handling present in
    FontCache. This is done by introducing a new parameter to
    FontCache::GetGenericFamilyNameForScript to specify a fallback to use.
    
    Additionally, this ensures the handling of "-webkit-standard" is at the
    same place as the corresponding use counters, giving more confidence
    about their accuracy.
    
    Bug: 1252383, 1241875, 1065354
    Change-Id: If73b7bb5d3a8944c9d8c6bab1406d5b845f321b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275574
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942574}

--- third_party/blink/renderer/platform/fonts/font_selector.cc
+++ third_party/blink/renderer/platform/fonts/font_selector.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom-blink.h"
+#include "third_party/blink/renderer/platform/fonts/alternate_font_family.h"
 #include "third_party/blink/renderer/platform/fonts/font_cache.h"
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
 #include "third_party/blink/renderer/platform/fonts/font_fallback_list.h"
@@ -54,19 +55,20 @@ AtomicString FontSelector::FamilyNameFromSettings(
 #if defined(OS_ANDROID)
   // TODO(crbug.com/1228189): Android does not have pre-installed math font.
   // https://github.com/googlefonts/noto-fonts/issues/330
-  if (font_description.GenericFamily() == FontDescription::kStandardFamily) {
+  if (font_description.GenericFamily() == FontDescription::kStandardFamily ||
+      generic_family_name == font_family_names::kWebkitStandard) {
     return FontCache::GetGenericFamilyNameForScript(
-        font_family_names::kWebkitStandard, font_description);
+        font_family_names::kWebkitStandard,
+        GetFallbackFontFamily(font_description), font_description);
   }
 
   if (generic_family_name == font_family_names::kSerif ||
       generic_family_name == font_family_names::kSansSerif ||
       generic_family_name == font_family_names::kCursive ||
       generic_family_name == font_family_names::kFantasy ||
-      generic_family_name == font_family_names::kMonospace ||
-      generic_family_name == font_family_names::kWebkitStandard) {
-    return FontCache::GetGenericFamilyNameForScript(generic_family_name,
-                                                    font_description);
+      generic_family_name == font_family_names::kMonospace) {
+    return FontCache::GetGenericFamilyNameForScript(
+        generic_family_name, generic_family_name, font_description);
   }
 #else   // !defined(OS_ANDROID)
   UScriptCode script = font_description.GetScript();
