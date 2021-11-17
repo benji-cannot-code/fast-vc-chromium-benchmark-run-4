1a55d2932580f04c68c3c05c650b46d1c64eafda
Erik Chen <erikchen@chromium.org>
2021-11-17 21:10:30 +0000

    lacros: Reserve system keys in ppapi.
    Ash reserves certain keys for e.g. brightness, etc. We should do the
    same in Lacros.
    
    Bug: 1235874
    Change-Id: I1dc49a041caa9009b3f12bf17972bdcab1074589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277339
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942804}

--- content/renderer/pepper/pepper_plugin_instance_impl.cc
+++ content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "cc/layers/texture_layer.h"
 #include "content/common/content_constants_internal.h"
 #include "content/public/common/content_constants.h"
@@ -134,7 +133,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/metafile_skia.h"          // nogncheck
 #endif
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if defined(OS_CHROMEOS)
 #include "ui/events/keycodes/keyboard_codes_posix.h"
 #endif
 
@@ -323,7 +322,7 @@ std::unique_ptr<const char* []> StringVectorToArgArray(
 // all keys sent to them. This can prevent keystrokes from working for things
 // like screen brightness and volume control.
 bool IsReservedSystemInputEvent(const blink::WebInputEvent& event) {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if defined(OS_CHROMEOS)
   if (event.GetType() != WebInputEvent::Type::kKeyDown &&
       event.GetType() != WebInputEvent::Type::kKeyUp)
     return false;
@@ -343,7 +342,7 @@ bool IsReservedSystemInputEvent(const blink::WebInputEvent& event) {
   }
 #else
   return false;
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // defined(OS_CHROMEOS)
 }
 
 void PrintPDFOutput(PP_Resource print_output,
