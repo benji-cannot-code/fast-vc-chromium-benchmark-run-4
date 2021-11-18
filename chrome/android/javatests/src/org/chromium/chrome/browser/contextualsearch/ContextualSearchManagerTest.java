bf940a7e013358f0523a9eda064512ac42308f05
Donn Denman <donnd@chromium.org>
2021-11-18 22:06:43 +0000

    Revert "[TTS][RSearches] Try to fix recent RS flakes"
    This reverts commit b967b7c265511657c2474e83c79b0c1318770837.
    
    Reason for revert: Seems to have made flakes worse instead of better.
    
    Original change's description:
    > [TTS][RSearches] Try to fix recent RS flakes
    >
    > Recent flakes of RelatedSearchesItem* tests are due to histograms
    > not being logged correctly, but it could be that we're just not
    > resetting the count correctly now that these tests are running
    > batched again.
    >
    > In this CL we try calling forgetHistograms directly.
    >
    > Change-Id: I5ce2ea34e20ab91786c7d13e0ade56cc4a78c5dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288748
    > Auto-Submit: Donn Denman <donnd@chromium.org>
    > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > Reviewed-by: Gang Wu <gangwu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#942924}
    
    Change-Id: Ie7829fcab60d0b60264ecc2eca226c1b66ff33f8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292114
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943272}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -342,7 +342,7 @@ public class ContextualSearchManagerTest {
         mActivityMonitor = null;
         mLatestSlowResolveSearch = null;
         if (mActionTester != null) mActionTester.tearDown();
-        forgetHistograms();
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(() -> forgetHistograms());
         FeatureList.setTestFeatures(null);
         CompositorAnimationHandler.setTestingMode(false);
     }
