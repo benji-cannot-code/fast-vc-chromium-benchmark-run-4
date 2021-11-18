55d5f9be85e5302d924ad7e20cbd2d65ab45df0c
Nate Fischer <ntfschr@google.com>
2021-11-18 01:33:56 +0000

    AW: fix and reenable testRequestMIDIAccess
    No change to logic. It seems like someone removed the additional console
    log related to web MIDI. The test passes again if I revert the change in
    https://crrev.com/c/2945199.
    
    Fixed: 1266967
    Test: run_system_webview_shell_layout_test_apk -f WebViewLayoutTest#testRequestMIDIAccess
    Change-Id: Ibf85aafbe5cf63fe14024ad429265833dfa064f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290657
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942903}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -246,7 +246,6 @@ public class WebViewLayoutTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/1266967")
     public void testRequestMIDIAccess() throws Exception {
         runWebViewLayoutTest("blink-apis/webmidi/requestmidiaccess.html",
                 "blink-apis/webmidi/requestmidiaccess-expected.txt");
