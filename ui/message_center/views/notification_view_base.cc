6cb5469e9f3d09b3f6b339b3ff213284c42ee279
Andre Le <leandre@chromium.org>
2021-11-18 02:02:54 +0000

    NotificationRevamp: Fix actions button padding.
    NotificationViewBase is currently using FillLayout for actions row. This
    caused the padding of the actions button to be incorrect. This CL added
    a way to use a different layout for actions button in the new notification.
    
    Fixed: 1264312
    Change-Id: Iecb498746b0d5833bb1c06df69143815b2fe3f3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3285447
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942918}

--- ui/message_center/views/notification_view_base.cc
+++ ui/message_center/views/notification_view_base.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
-#include "ui/views/layout/fill_layout.h"
 #include "ui/views/native_cursor.h"
 #include "ui/views/style/typography.h"
 #include "ui/views/widget/widget.h"
@@ -498,11 +497,12 @@ NotificationViewBase::CreateImageContainerBuilder() {
       .SetBorder(views::CreateEmptyBorder(kLargeImageContainerPadding));
 }
 
-std::unique_ptr<views::View> NotificationViewBase::CreateActionsRow() {
+std::unique_ptr<views::View> NotificationViewBase::CreateActionsRow(
+    std::unique_ptr<views::LayoutManager> layout_manager) {
   DCHECK(!actions_row_);
   auto actions_row = std::make_unique<views::View>();
   actions_row->SetVisible(false);
-  actions_row->SetLayoutManager(std::make_unique<views::FillLayout>());
+  actions_row->SetLayoutManager(std::move(layout_manager));
 
   // |action_buttons_row_| contains inline action buttons.
   DCHECK(!action_buttons_row_);
