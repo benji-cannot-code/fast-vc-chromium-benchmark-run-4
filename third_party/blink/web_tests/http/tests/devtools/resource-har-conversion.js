4818625ea06323b6cd367d4c264bc615b8050aa7
Danil Somsikov <dsv@chromium.org>
2021-11-18 14:55:15 +0000

    Update and enable resource-har-conversion.js layout test
    Bug: 1203169
    Change-Id: I15eef49ce2eb1a25511fa190702dba81d048d1da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289641
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943076}

--- third_party/blink/web_tests/http/tests/devtools/resource-har-conversion.js
+++ third_party/blink/web_tests/http/tests/devtools/resource-har-conversion.js
@@ -25,13 +25,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const c1 = new SDK.Cookie('a', 'b');
     c1.addAttribute('path', '/path');
     c1.addAttribute('domain', 'example.com');
-    request.includedRequestCookiesInternal = [
-      c1,
-      new SDK.Cookie('a1', 'b1'),
-      new SDK.Cookie('c1', 'd1'),
-    ];
+    request.addExtraRequestInfo({
+      includedRequestCookies: [
+        c1,
+        new SDK.Cookie('a1', 'b1'),
+        new SDK.Cookie('c1', 'd1'),
+      ],
+      blockedRequestCookies: [],
+      requestHeaders: [{name: 'version', value: 'HTTP/1.1'}],
+      connectTiming: {}
+    });
 
-    request.responseHeadersInternal = [{
+    request.responseHeaders = [{
       name: 'Set-Cookie',
       value:
           'x=y; Path=/path; Domain=example.com; Discard; httpOnly; Secure; Version=1\nx1=y1; SameSite=Strict\nz2=y2; SameSite=Lax'
