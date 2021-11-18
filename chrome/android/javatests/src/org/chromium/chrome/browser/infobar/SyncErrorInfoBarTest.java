384ea24f9240ac031170586c2eb7d6f4ed9b1381
Lijin Shen <lazzzis@google.com>
2021-11-18 18:24:40 +0000

    Re-enable sync error infobar render test
    Disable animation to fix flakiness.
    
    Bug: 1264954
    Change-Id: Iff258f03426c365244b38dafbeef9ce38ffae3bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290493
    Reviewed-by: Matt Jones <mdjones@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#943159}

--- chrome/android/javatests/src/org/chromium/chrome/browser/infobar/SyncErrorInfoBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/infobar/SyncErrorInfoBarTest.java
@@ -7,8 +7,6 @@ package org.chromium.chrome.browser.infobar;
 
 import static org.chromium.chrome.browser.preferences.ChromePreferenceKeys.SYNC_ERROR_PROMPT_SHOWN_AT_TIME;
 
-import android.os.Build;
-
 import androidx.test.filters.LargeTest;
 
 import org.junit.Assert;
@@ -19,7 +17,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.SyncFirstSetupCompleteSource;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -40,6 +37,7 @@ import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.infobars.InfoBar;
 import org.chromium.components.signin.base.GoogleServiceAuthError;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.ui.test.util.DisableAnimationsTestRule;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -95,6 +93,9 @@ public class SyncErrorInfoBarTest {
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus().setRevision(2).build();
+
+    @Rule
+    public DisableAnimationsTestRule mDisableAnimationsTestRule = new DisableAnimationsTestRule();
 
     @Before
     public void setUp() {
@@ -233,11 +234,7 @@ public class SyncErrorInfoBarTest {
     @Test
     @LargeTest
     @Feature("RenderTest")
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP_MR1,
-            sdk_is_less_than = Build.VERSION_CODES.N, supported_abis_includes = "x86",
-            message = "Flaky, see crbug.com/1264954")
-    public void
-    testSyncErrorInfoBarForAuthErrorView() throws IOException {
+    public void testSyncErrorInfoBarForAuthErrorView() throws IOException {
         mSyncTestRule.setUpAccountAndEnableSyncForTesting();
         mFakeSyncServiceImpl.setAuthError(GoogleServiceAuthError.State.INVALID_GAIA_CREDENTIALS);
         mSyncTestRule.loadUrl(UrlConstants.VERSION_URL);
@@ -248,11 +245,7 @@ public class SyncErrorInfoBarTest {
     @Test
     @LargeTest
     @Feature("RenderTest")
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP_MR1,
-            sdk_is_less_than = Build.VERSION_CODES.N, supported_abis_includes = "x86",
-            message = "Flaky, see crbug.com/1264954")
-    public void
-    testSyncErrorInfoBarForSyncSetupIncompleteView() throws IOException {
+    public void testSyncErrorInfoBarForSyncSetupIncompleteView() throws IOException {
         mSyncTestRule.setUpTestAccountAndSignInWithSyncSetupAsIncomplete();
         mSyncTestRule.loadUrl(UrlConstants.VERSION_URL);
         mRenderTestRule.render(mSyncTestRule.getInfoBarContainer().getContainerViewForTesting(),
@@ -262,11 +255,7 @@ public class SyncErrorInfoBarTest {
     @Test
     @LargeTest
     @Feature("RenderTest")
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP_MR1,
-            sdk_is_less_than = Build.VERSION_CODES.N, supported_abis_includes = "x86",
-            message = "Flaky, see crbug.com/1264954")
-    public void
-    testSyncErrorInfoBarForPassphraseRequiredView() throws IOException {
+    public void testSyncErrorInfoBarForPassphraseRequiredView() throws IOException {
         mSyncTestRule.setUpAccountAndEnableSyncForTesting();
         mFakeSyncServiceImpl.setEngineInitialized(true);
         mFakeSyncServiceImpl.setPassphraseRequiredForPreferredDataTypes(true);
@@ -278,11 +267,7 @@ public class SyncErrorInfoBarTest {
     @Test
     @LargeTest
     @Feature("RenderTest")
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP_MR1,
-            sdk_is_less_than = Build.VERSION_CODES.N, supported_abis_includes = "x86",
-            message = "Flaky, see crbug.com/1264954")
-    public void
-    testSyncErrorInfoBarForClientOutOfDateView() throws IOException {
+    public void testSyncErrorInfoBarForClientOutOfDateView() throws IOException {
         mSyncTestRule.setUpAccountAndEnableSyncForTesting();
         mFakeSyncServiceImpl.setRequiresClientUpgrade(true);
         mSyncTestRule.loadUrl(UrlConstants.VERSION_URL);
