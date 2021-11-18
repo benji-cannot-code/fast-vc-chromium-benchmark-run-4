f259cb2efe699c577058abc98d22b30b1102f68f
John Wu <jzw@chromium.org>
2021-11-18 22:24:53 +0000

    Reland "Add menu items for interacting with trusted vault"
    This is a reland of 68991cd6021336b25ad0c8862ad5b16902780a7c
    
    Original change's description:
    > Add menu items for interacting with trusted vault
    >
    > This now allows us to test a complete flow for:
    > - fetching trusted vault keys
    > - fixing degraded recoverability
    >
    > Bug: 1266130
    > Change-Id: I7bb8955e8b035399b574281795240bda5721ad09
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289968
    > Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    > Commit-Queue: John Wu <jzw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#943154}
    
    Bug: 1266130
    Change-Id: I0edf3a72c736a7aad60d7ef91800cd813aa65f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292054
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943283}

--- ios/web_view/shell/shell_trusted_vault_provider_fake.m
+++ ios/web_view/shell/shell_trusted_vault_provider_fake.m
@@ -15,6 +15,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return [super init];
 }
 
+- (void)showFetchKeysFlowForIdentity:(CWVIdentity*)identity
+                  fromViewController:(UIViewController*)viewController {
+  // No op.
+}
+
+- (void)showFixDegradedRecoverabilityFlowForIdentity:(CWVIdentity*)identity
+                                  fromViewController:
+                                      (UIViewController*)viewController {
+  // No op.
+}
+
 #pragma mark - CWVTrustedVaultProvider
 
 - (void)addTrustedVaultObserver:(CWVTrustedVaultObserver*)observer {
