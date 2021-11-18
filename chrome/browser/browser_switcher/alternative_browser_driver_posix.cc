5118a6138c43dd9f8099a38c88ecde604b79ae3a
Simon Hwang <sangmoonhwang@google.com>
2021-11-18 05:30:18 +0000

    Improve UI of the LBS internals
    Show "alternative browser" instead of an empty string when the
    alternative browser's name cannot be detected or determined.
    
    R=nicolaso@chromium.org
    
    Bug: 1258133
    Change-Id: I880ddc26d7079ba5565302632701d827d3c818c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288830
    Commit-Queue: Simon Hwang <sangmoonhwang@google.com>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#942967}

--- chrome/browser/browser_switcher/alternative_browser_driver_posix.cc
+++ chrome/browser/browser_switcher/alternative_browser_driver_posix.cc
@@ -264,7 +264,7 @@ void AlternativeBrowserDriverImpl::TryLaunch(const GURL& url,
 std::string AlternativeBrowserDriverImpl::GetBrowserName() const {
   std::string path = prefs_->GetAlternativeBrowserPath();
   const auto* mapping = FindBrowserMapping(path);
-  return mapping ? mapping->browser_name : std::string();
+  return mapping ? mapping->browser_name : "alternative browser";
 }
 
 BrowserType AlternativeBrowserDriverImpl::GetBrowserType() const {
