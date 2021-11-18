58d853d6f2eb189b5a41de6f2d829fd648371b52
Brian Sheedy <bsheedy@chromium.org>
2021-11-18 23:24:00 +0000

    Blink stale expectations: Add automated removal
    Adds the code necessary to support automated removal of stale
    expectations and semi-automated modification of semi-stale expectations
    in the Blink stale expectation remover.
    
    Bug: 1222827
    Change-Id: Ie58683247f89c516e2b24c3acb097bbdf53c1258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291925
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943324}

--- testing/unexpected_passes_common/unittest_utils.py
+++ testing/unexpected_passes_common/unittest_utils.py
@@ -161,7 +161,7 @@ class GenericExpectations(expectations.Expectations):
   def GetExpectationFilepaths(self):
     return []
 
-  def _GetExpectationFileTagHeader(self):
+  def _GetExpectationFileTagHeader(self, _):
     return """\
 # tags: [ linux mac win ]
 # results: [ Failure RetryOnFailure Skip Pass ]
--- third_party/blink/tools/blinkpy/web_tests/stale_expectation_removal/expectations_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/stale_expectation_removal/expectations_unittest.py
@@ -14,6 +14,7 @@ if six.PY3:
 
 from pyfakefs import fake_filesystem_unittest
 
+from blinkpy.web_tests.stale_expectation_removal import constants
 from blinkpy.web_tests.stale_expectation_removal import expectations
 
 
@@ -34,7 +35,7 @@ class GetExpectationFilepathsUnittest(fake_filesystem_unittest.TestCase):
         self.instance = expectations.WebTestExpectations()
         CreateFile(
             self.fs,
-            os.path.join(expectations.WEB_TEST_ROOT_DIR, 'FlagExpectations',
+            os.path.join(constants.WEB_TEST_ROOT_DIR, 'FlagExpectations',
                          'README.txt'))
 
     def testRealFilesCanBeFound(self):
@@ -55,7 +56,7 @@ class GetExpectationFilepathsUnittest(fake_filesystem_unittest.TestCase):
 
     def testFlagSpecificFiles(self):
         """Tests that flag-specific files are properly returned."""
-        flag_filepath = os.path.join(expectations.WEB_TEST_ROOT_DIR,
+        flag_filepath = os.path.join(constants.WEB_TEST_ROOT_DIR,
                                      'FlagExpectations', 'foo-flag')
         CreateFile(self.fs, flag_filepath)
         with mock.patch.object(self.instance,
@@ -66,7 +67,7 @@ class GetExpectationFilepathsUnittest(fake_filesystem_unittest.TestCase):
 
     def testAllExpectationFiles(self):
         """Tests that both top level and flag-specific files are returned."""
-        flag_filepath = os.path.join(expectations.WEB_TEST_ROOT_DIR,
+        flag_filepath = os.path.join(constants.WEB_TEST_ROOT_DIR,
                                      'FlagExpectations', 'foo-flag')
         CreateFile(self.fs, flag_filepath)
         with mock.patch.object(self.instance,
@@ -85,7 +86,8 @@ class GetExpectationFileTagHeaderUnittest(fake_filesystem_unittest.TestCase):
     def testRealContentsCanBeLoaded(self):
         """Tests that some sort of valid content can be read from the file."""
         with fake_filesystem_unittest.Pause(self):
-            header = self.instance._GetExpectationFileTagHeader()
+            header = self.instance._GetExpectationFileTagHeader(
+                expectations.MAIN_EXPECTATION_FILE)
         self.assertIn('tags', header)
         self.assertIn('results', header)
 
@@ -103,7 +105,8 @@ not a comment
         CreateFile(self.fs, expectations.MAIN_EXPECTATION_FILE)
         with open(expectations.MAIN_EXPECTATION_FILE, 'w') as f:
             f.write(header_contents)
-        header = self.instance._GetExpectationFileTagHeader()
+        header = self.instance._GetExpectationFileTagHeader(
+            expectations.MAIN_EXPECTATION_FILE)
         expected_header = """\
 # foo
 #   bar
