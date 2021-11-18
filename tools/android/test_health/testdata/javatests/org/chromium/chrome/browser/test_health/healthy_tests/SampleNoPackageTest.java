f7ad2a3121896fe4fa5cbac7ef041024d7bb2206
Andrew Heard <andrewheard@google.com>
2021-11-18 22:27:34 +0000

    Android: Add Java test health extraction module
    Added java_test_utils.py which contains utility functions to extract
    counts of test cases annotated with @DisabledTest, @DisableIf and
    @FlakyTest, as well as the Java package name, from the source code of a
    Java test file. These counts and the package names are surfaced in an
    internal Clank dashboard for test health.
    
    Bug: 1254072
    Change-Id: Ic3a76b410ffe69a0cc31ff79fbaa6856270b1967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3278356
    Commit-Queue: Andrew Heard <andrewheard@google.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943287}

---
+++ tools/android/test_health/testdata/javatests/org/chromium/chrome/browser/test_health/healthy_tests/SampleNoPackageTest.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseJUnit4ClassRunner;
+
+/** A sample Java test in the default Java package. */
+@SmallTest
+@RunWith(BaseJUnit4ClassRunner.class)
+public class SampleNoPackageTest {
+    @Test
+    public void testTrueIsTrue() {
+        Assert.assertTrue(true);
+    }
+}
