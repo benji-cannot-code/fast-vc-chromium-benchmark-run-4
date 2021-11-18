fac1c9e681831bac2cb4b6df4ca2de4d9eec7943
Sebastien Lalancette <seblalancette@chromium.org>
2021-11-18 15:33:20 +0000

    [ZT] Update DesktopAttestationService to use DTKeyManager
    Updates the attestation code to make use of the new DTKeyManager to
    do any signing key operation. Since DTKeyManager handles running
    expensive operations on the Thread Pool, this updates the sequence of
    operation to run DesktopAttestationService's own expensive operations
    on the thread pool as well. The whole attestation flow therefore
    consists of quite a few back-and-forth between the UI and background
    threads.
    
    Updated the unit test to capture the success scenario, and cleaned-up
    some unused protos.
    
    Fixed: b/205868950, b/203694133
    Change-Id: I874cab5cd53db2cd710c70f7f59dcbefac92d62f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289866
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943094}

---
+++ chrome/browser/enterprise/connectors/device_trust/key_management/browser/mock_device_trust_key_manager.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/browser/mock_device_trust_key_manager.h"
+
+namespace enterprise_connectors {
+namespace test {
+
+MockDeviceTrustKeyManager::MockDeviceTrustKeyManager() = default;
+MockDeviceTrustKeyManager::~MockDeviceTrustKeyManager() = default;
+
+}  // namespace test
+}  // namespace enterprise_connectors
