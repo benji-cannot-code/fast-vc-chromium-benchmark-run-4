ad94ee99283a5d6ad90c33417d2ea86020ac4270
Danil Somsikov <dsv@chromium.org>
2021-11-18 12:16:27 +0000

    Update and enable resource-har-headers.js layout tests
    Bug: 1203169
    Change-Id: I876600284ea223314e690683c56a6ac96f212516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289202
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943040}

--- third_party/blink/web_tests/http/tests/devtools/resource-har-headers.js
+++ third_party/blink/web_tests/http/tests/devtools/resource-har-headers.js
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     request.statusCode = 200;
     request.statusText = 'OK';
     request.resourceSize = 1000;
-    request.transferSizeInternal = 539;  // 39 = header size at the end of the day
+    request.setTransferSize(539);  // 39 = header size at the end of the day
     request.setPriority('VeryHigh');
     request.setResourceType(Common.resourceTypes.Fetch);
 
