36b8a47e39268f5116cd7a49c53186873eca3ba6
Kevin Radtke <kradtke@chromium.org>
2021-11-18 20:58:12 +0000

    Update notifier: change extension installed notification to toast.
    Modify the extension installed success notification to instead show a toast when a Chrome App is installed.
    
    Based on Notifier Framework Catalog "notifiers to be updated" list.
    go/notifier-catalogue
    
    Bug: 1254508
    Change-Id: I44f0f91a2912c7f56e193130a65e8ec0297887e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260806
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943248}

--- chrome/browser/ui/extensions/extension_install_ui_default.cc
+++ chrome/browser/ui/extensions/extension_install_ui_default.cc
@@ -32,7 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/ui/extensions/extension_installed_notification.h"
+#include "ash/public/cpp/toast_data.h"
+#include "ash/public/cpp/toast_manager.h"
+#include "chrome/grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #else
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/notification_service.h"
@@ -55,6 +58,19 @@ Browser* FindOrCreateVisibleBrowser(Profile* profile) {
     chrome::AddTabAt(browser, GURL(), -1, true);
   return browser;
 }
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+// Toast id and duration for extension install success.
+constexpr char kExtensionInstallSuccessToastId[] = "extension_install_success";
+// TODO(crbug.com/1270549): Use value from central location.
+constexpr int kToastDurationMs = 2500;
+
+void ShowToast(const std::string& id, const std::u16string& text) {
+  ash::ToastManager::Get()->Show(
+      ash::ToastData(id, text, kToastDurationMs,
+                     /*dismiss_text=*/absl::nullopt));
+}
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 }  // namespace
 
@@ -91,7 +107,9 @@ void ExtensionInstallUIDefault::OnInstallSuccess(
     }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-    ExtensionInstalledNotification::Show(extension.get(), current_profile);
+    ShowToast(kExtensionInstallSuccessToastId,
+              l10n_util::GetStringFUTF16(IDS_EXTENSION_NOTIFICATION_INSTALLED,
+                                         base::UTF8ToUTF16(extension->name())));
 #else   // BUILDFLAG(IS_CHROMEOS_ASH)
     OpenAppInstalledUI(extension->id());
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
