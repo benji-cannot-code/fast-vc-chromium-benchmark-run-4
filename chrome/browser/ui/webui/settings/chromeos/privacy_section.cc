f9260857488a5fb4c8b99da889a5e35d103f80d6
Jonghyun Ahn <jongahn@google.com>
2021-11-18 22:19:18 +0000

    Reland "Refactor metrics consent UI to handle more complex per-user logic."
    This is a reland of 8654d399e4a1256ab06a52d9d0298e1c8bce5797
    
    Original change's description:
    > Refactor metrics consent UI to handle more complex per-user logic.
    >
    > This refactor moves metrics consent logic (ie enable/disable, pref
    > changes) into a separate handler. It is in preparation to apply per-user
    > logic for controlling metrics consent, which is implemented and
    > controlled at the browser layer.
    >
    > This CL should no-op.
    >
    > Bug: 1181504
    > Change-Id: I8e39dca5ab59818631fa81161412e7957609c384
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268537
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    > Commit-Queue: Jong Ahn <jongahn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#942625}
    
    Bug: 1181504
    Change-Id: Id52267356e9abd6014eba7c2313337c360fe8603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290340
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943280}

--- chrome/browser/ui/webui/settings/chromeos/privacy_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/privacy_section.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_utils.h"
+#include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chrome/browser/ui/webui/settings/chromeos/metrics_consent_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/os_settings_features_util.h"
 #include "chrome/browser/ui/webui/settings/chromeos/peripheral_data_access_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/search/search_tag_registry.h"
@@ -248,6 +250,10 @@ PrivacySection::~PrivacySection() = default;
 void PrivacySection::AddHandlers(content::WebUI* web_ui) {
   web_ui->AddMessageHandler(
       std::make_unique<chromeos::settings::PeripheralDataAccessHandler>());
+
+  web_ui->AddMessageHandler(
+      std::make_unique<chromeos::settings::MetricsConsentHandler>(
+          profile(), user_manager::UserManager::Get()));
 
   if (IsSecureDnsAvailable())
     web_ui->AddMessageHandler(std::make_unique<::settings::SecureDnsHandler>());
