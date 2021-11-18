cf69c8c77a24462f827b68dea8b13939f0fea8d9
Adrienne Walker <enne@chromium.org>
2021-11-18 23:43:31 +0000

    appcache: remove wpt references to appcache
    This is mostly a removal, but required some modifications as well.
    Expectations are updated to remove references to removed tests.
    
    Bug: 582750
    Change-Id: I4e705f5888d3c8cd3d2cc4859a7a3816de5e88f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283191
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943332}

--- third_party/blink/web_tests/external/wpt/html/browsers/offline/appcache/resources/appcache-data.py
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-def main(request, response):
-    type = request.GET[b'type']
-    if request.GET[b'type'] == b'fallingback':
-        return 404, [(b'Content-Type', b'text/plain')], u"Page not found"
-    return [(b'Content-Type', b'text/plain')], type
