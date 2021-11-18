fb8d459908c649c779ecc6173d0ae1551a439138
Yutaka Hirano <yhirano@chromium.org>
2021-11-18 22:39:56 +0000

    Remove an unnecessary include in net/nqe/network_quality_estimator.cc
    This is the only defined(OS_ANDROID) usage in this file, so it's
    probably safe to remove.
    
    Bug: None
    Change-Id: I2e40fd3dcf2d3ebdf42a8201534bbe6d3e01be30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291247
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943291}

--- net/nqe/network_quality_estimator.cc
+++ net/nqe/network_quality_estimator.cc
@@ -49,10 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_context.h"
 #include "url/gurl.h"
 
-#if defined(OS_ANDROID)
-#include "net/android/network_library.h"
-#endif  // OS_ANDROID
-
 namespace net {
 
 namespace {
