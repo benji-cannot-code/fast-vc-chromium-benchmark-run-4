b0dbe207c455198f059f5f65c965fb21767b0bba
Arthur Hemery <ahemery@chromium.org>
2021-11-18 13:52:23 +0000

    [Fixit] Move some frameNavigation web tests to WPTs.
    Convert some older web tests into WPTs.
    
    Bug: 1187081
    Change-Id: I6b24765bb311608110646259ca6db0d4faebb8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3284525
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943060}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/browsing-the-web/navigating-across-documents/cross-origin-top-navigation-without-user-activation.window.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+
+async_test(t => {
+  addEventListener('message', t.step_func_done(e => {
+    assert_equals(e.data, 'Denied');
+  }));
+  const w = open("resources/page-with-top-navigating-iframe.html");
+  t.add_cleanup(() => {w.close()});
+
+}, "Cross-origin top navigation is blocked without user activation");
