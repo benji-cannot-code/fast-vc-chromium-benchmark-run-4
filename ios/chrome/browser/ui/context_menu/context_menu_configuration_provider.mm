a91d704c91f67ba681a949df155ce4bef9feddec
Jason Hu <hujasonx@google.com>
2021-11-19 05:56:04 +0000

    Add Lens availability UMA logging
    Logs are logged when the context menu is opened
    for an image, and Lens is enabled.
    
    Bug: 1234532
    Change-Id: I696f9f5883dcce1efc76c234ffdf59cdaf2391f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3287405
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#943425}

--- ios/chrome/browser/ui/context_menu/context_menu_configuration_provider.mm
+++ ios/chrome/browser/ui/context_menu/context_menu_configuration_provider.mm
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/image_util/image_saver.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_commands.h"
 #import "ios/chrome/browser/ui/incognito_reauth/incognito_reauth_scene_agent.h"
+#import "ios/chrome/browser/ui/lens/lens_availability.h"
 #import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
 #import "ios/chrome/browser/ui/menu/browser_action_factory.h"
 #import "ios/chrome/browser/ui/menu/menu_histograms.h"
@@ -283,14 +284,19 @@ const CGFloat kFaviconWidthHeight = 24;
                                                      completion:nil];
     [menuElements addObject:openImageInNewTab];
 
-    // Search by image or Search image with Lens.
+    // Search the image using Lens if Lens is enabled and available. Otherwise
+    // fall back to a standard search by image experience.
     TemplateURLService* service =
         ios::TemplateURLServiceFactory::GetForBrowserState(
             self.browser->GetBrowserState());
     __weak ContextMenuConfigurationProvider* weakSelf = self;
-    if (ios::provider::IsLensSupported() &&
-        base::FeatureList::IsEnabled(kUseLensToSearchForImage) &&
-        search_engines::SupportsSearchImageWithLens(service)) {
+
+    const BOOL lensEnabled =
+        ios::provider::IsLensSupported() &&
+        base::FeatureList::IsEnabled(kUseLensToSearchForImage);
+    const BOOL useLens =
+        lensEnabled && search_engines::SupportsSearchImageWithLens(service);
+    if (useLens) {
       UIAction* searchImageWithLensAction =
           [actionFactory actionToSearchImageUsingLensWithBlock:^{
             [weakSelf searchImageWithURL:imageURL
@@ -298,7 +304,14 @@ const CGFloat kFaviconWidthHeight = 24;
                                 referrer:referrer];
           }];
       [menuElements addObject:searchImageWithLensAction];
-    } else if (search_engines::SupportsSearchByImage(service)) {
+      UMA_HISTOGRAM_ENUMERATION(kIOSLensSupportStatusHistogram,
+                                LensSupportStatus::LensSearchSupported);
+    } else if (lensEnabled) {
+      UMA_HISTOGRAM_ENUMERATION(kIOSLensSupportStatusHistogram,
+                                LensSupportStatus::NonGoogleSearchEngine);
+    }
+
+    if (!useLens && search_engines::SupportsSearchByImage(service)) {
       const TemplateURL* defaultURL = service->GetDefaultSearchProvider();
       NSString* title =
           IsContextMenuActionsRefreshEnabled()
