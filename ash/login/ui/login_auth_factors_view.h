5202cb54aeae1878e0f083f4b6d2786559273b3e
Curt Clemens <cclem@google.com>
2021-11-19 23:37:43 +0000

    [SmartLock] Handle permanent errors
    - When an AuthFactorModel reports its state as kErrorPermanent, its
      error will be shown for 3 seconds, and then OnErrorTimeout() will
      be called. The AuthFactorModel is not required to reset its state.
      After the error timeout, permanent errors are shown alongside any
      auth factors in the kReady state, and if the icon/string needs to
      change, then UpdateIcon() and GetLabelId() can check the
      |permanent_error_timed_out_| variable to determine which to show.
    - Clicking/tapping on an auth factor in the kErrorPermanent state will
      run the timer again and show the error for 3 seconds.
    - When only a single auth factor is available and in the kErrorPermanent
      state, then its label is shown instead of the generic "enter your
      password" message.
    
    Screenshot:
    https://screenshot.googleplex.com/3d8rRtm9BtkLxDF.png
    
    Bug: 1233614
    Test: ash_unittests
    Change-Id: Ia879784f01a36854900cbd367dabb08cb31c87ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3246386
    Commit-Queue: Curt Clemens <cclem@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/main@{#943732}

--- ash/login/ui/login_auth_factors_view.h
+++ ash/login/ui/login_auth_factors_view.h
@@ -72,7 +72,7 @@ class ASH_EXPORT LoginAuthFactorsView : public views::View {
 
   void ShowArrowButton();
   void ShowSingleAuthFactor(AuthFactorModel* auth_factor);
-  void ShowReadyAuthFactors();
+  void ShowReadyAndDisabledAuthFactors();
   void ShowCheckmark();
 
   // Sets the text and accessible name of the label using the provided string
@@ -112,7 +112,7 @@ class ASH_EXPORT LoginAuthFactorsView : public views::View {
 
   /////////////////////////////////////////////////////////////////////////////
 
-  // The auth factor models that have been added by calling AddAuthFactor().
+  // The auth factor models that have been added by calling `AddAuthFactor`.
   // The order here should match the order in which they appear in the UI when
   // multiple are visible.
   std::vector<std::unique_ptr<AuthFactorModel>> auth_factors_;
