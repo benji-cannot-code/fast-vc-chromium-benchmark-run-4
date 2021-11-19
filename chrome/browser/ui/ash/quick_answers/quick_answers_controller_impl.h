09218d93ad08960dab816d77d27fd6087664390b
Yue Li <updowndota@google.com>
2021-11-19 22:01:57 +0000

    Reland "QA Lacros: Move //{ash => chromeos}/components/quick_answers"
    This reverts commit 3761ef5b66b437af30577740135a9c99e56e0578.
    
    Reason for revert: Fixed issue in tools/traffic_annotation/auditor/chromeos/safe_list.txt
    
    Original change's description:
    > Revert "QA Lacros: Move //{ash => chromeos}/components/quick_answers"
    >
    > This reverts commit 49d788e85d8566ef10a33f5300bc8b2b284f347f.
    >
    > Reason for revert: Failures in linux-chromeos-annotator-rel
    >
    > Original change's description:
    > > QA Lacros: Move //{ash => chromeos}/components/quick_answers
    > >
    > > Move the QuickAnswers source to chromeos since it will be shared by ash
    > > and lacros.
    > >
    > > DD: go/qa-lacros-alternatives
    > >
    > > Bug: b/206453061
    > > Test: Run existing tests
    > > Change-Id: I27d721284ddb8627b2de32f161dc93f17c840ef0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282642
    > > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > > Reviewed-by: Jason Thai <jasontt@chromium.org>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Commit-Queue: Yue Li <updowndota@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#943349}
    >
    > Bug: b/206453061
    > Change-Id: Ia33220372a141f34891871bbf70ba1c6b348a288
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293249
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Renato Silva <rrsilva@google.com>
    > Cr-Commit-Position: refs/heads/main@{#943569}
    
    Bug: b/206453061
    Change-Id: Iea58be3f8d50a3f45c793e232a951441a977c25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293762
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943683}

--- chrome/browser/ui/ash/quick_answers/quick_answers_controller_impl.h
+++ chrome/browser/ui/ash/quick_answers/quick_answers_controller_impl.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "ash/components/quick_answers/quick_answers_client.h"
-#include "ash/components/quick_answers/quick_answers_model.h"
 #include "ash/public/cpp/quick_answers/controller/quick_answers_controller.h"
 #include "chrome/browser/ui/ash/quick_answers/quick_answers_state_controller.h"
+#include "chromeos/components/quick_answers/quick_answers_client.h"
+#include "chromeos/components/quick_answers/quick_answers_model.h"
 #include "ui/gfx/geometry/rect.h"
 
 namespace ash {
