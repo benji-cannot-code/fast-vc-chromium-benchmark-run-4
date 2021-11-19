66a8f4ee62bfdb58e53fc39d63f63ceb3f064333
Nektarios Paisios <nektar@chromium.org>
2021-11-19 15:25:24 +0000

    Enables calculating line, word and sentence boundaries on the browser
    This patch enables the previously implemented functionality of calculating
    word, line and sentence boundaries on the browser.
    This functionality is only invoked when such boundaries are not provided
    by Blink, i.e. only in Views and the chrome.automation API.
    However, since Views is not using an AXTree of AXNodes yet,
    the computed offsets are still not used in Views for the
    time being.
    This patch enables these browser-side computations so that
    more testing can be performed and any inconsistencies
    ironed out until they are removed from Blink.
    
    This patch also changes some of the existing behavior whereby line start offsets did not include the
    start of the first line and always assumed that it would be
    at 0 in the chrome.automation API.
    I believe that it is better to explicitly provide the start offset of the first line
    because it helps prevent logic bugs, and because we might decide to start the first
    line after non-editable text in some cases, so the initial line may not be at offset 0.
    
    R=aleventhal@chromium.org
    
    AX-Relnotes: n/a.
    Bug: 1049261
    Change-Id: I7f33b2d419142347296bf3ac3fa0670496d4caa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289198
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943524}

--- content/browser/accessibility/browser_accessibility.cc
+++ content/browser/accessibility/browser_accessibility.cc
@@ -931,10 +931,6 @@ std::string BrowserAccessibility::GetLiveRegionText() const {
   return text;
 }
 
-std::vector<int> BrowserAccessibility::GetLineStartOffsets() const {
-  return node()->GetOrComputeLineStartOffsets();
-}
-
 BrowserAccessibility::AXPosition BrowserAccessibility::CreatePositionAt(
     int offset,
     ax::mojom::TextAffinity affinity) const {
