e3f83221fae539d5cdff1811c4892fec18f0c08e
Joel Hockey <joelhockey@chromium.org>
2021-11-19 05:14:58 +0000

    Consider inode/directory match to be non-generic
    This change is required for Terminal which can be used to open
    a directory.
    
    Bug: 1028898
    Change-Id: Ibfc46d76ba705519da24b9c8b938dbfaae565473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291251
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943420}

--- components/services/app_service/public/cpp/intent_util.cc
+++ components/services/app_service/public/cpp/intent_util.cc
@@ -42,6 +42,7 @@ const char kCategoriesKey[] = "categories";
 const char kDataKey[] = "data";
 const char kUiBypassedKey[] = "ui_bypassed";
 const char kExtrasKey[] = "extras";
+const char kMimeTypeInodeDirectory[] = "inode/directory";
 
 // Get the field/s from the |intent| that need to be checked/matched based on
 // |condition_type|. Most types return a single string but we return a vector
@@ -436,10 +437,11 @@ bool IsGenericFileHandler(const apps::mojom::IntentPtr& intent,
     }
   }
 
-  // We consider it a generic match if a directory is selected.
+  // If directory is selected, it is generic unless mime_types included
+  // 'inode/directory'.
   for (const auto& file : intent->files.value()) {
     if (file->is_directory == apps::mojom::OptionalBool::kTrue)
-      return true;
+      return mime_types.count(kMimeTypeInodeDirectory) == 0;
   }
   return false;
 }
