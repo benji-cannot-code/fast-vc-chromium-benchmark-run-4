c0bf2df879fe91c0cb38adf383aa25b69823dca8
Etienne Bergeron <etienneb@chromium.org>
2021-11-19 16:08:46 +0000

    Fix renderer/browser trace events scoped id.
    This CL is fixing passing to TRACE_ID_WITH_SCOPE a TRACE_ID_LOCAL.
    As described on crbug/1269403, we currently emit all the RendererMain
    trace event into the browser process, as the same context.
    
    The macro TRACE_ID_WITH_SCOPE is taking an ID which can be of three
    kind:
      1) Raw (raw integer) : You know what you are doing.
      2) LocalId : Use this for ids that are unique within a single process.
      3) GlobalId : Use this for ids that are unique across processes.
    
    The proposal is to create these events with a the local scope.
    
    R=eseckler,gab
    
    Bug: 1269403
    Change-Id: Ie2d16711163e4fff316da037e2323658643eb142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276036
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943535}

--- content/renderer/renderer_main.cc
+++ content/renderer/renderer_main.cc
@@ -110,9 +110,7 @@ std::unique_ptr<base::MessagePump> CreateMainThreadMessagePump() {
 int RendererMain(MainFunctionParams parameters) {
   // Don't use the TRACE_EVENT0 macro because the tracing infrastructure doesn't
   // expect synchronous events around the main loop of a thread.
-  TRACE_EVENT_NESTABLE_ASYNC_BEGIN1("startup", "RendererMain",
-                                    TRACE_ID_WITH_SCOPE("RendererMain", 0),
-                                    "zygote_child", false);
+  TRACE_EVENT_INSTANT0("startup", "RendererMain", TRACE_EVENT_SCOPE_THREAD);
 
   base::trace_event::TraceLog::GetInstance()->set_process_name("Renderer");
   base::trace_event::TraceLog::GetInstance()->SetProcessSortIndex(
@@ -237,9 +235,8 @@ int RendererMain(MainFunctionParams parameters) {
     // the tracing SMB on our behalf due to the zygote sandbox.
     if (parameters.zygote_child) {
       tracing::EnableStartupTracingIfNeeded();
-      TRACE_EVENT_NESTABLE_ASYNC_BEGIN1("startup", "RendererMain",
-                                        TRACE_ID_WITH_SCOPE("RendererMain", 0),
-                                        "zygote_child", true);
+      TRACE_EVENT_INSTANT1("startup", "RendererMain", TRACE_EVENT_SCOPE_THREAD,
+                           "zygote_child", true);
     }
 #endif  // OS_POSIX && !OS_ANDROID && !OS_MAC
 
@@ -260,15 +257,11 @@ int RendererMain(MainFunctionParams parameters) {
       if (pool)
         pool->Recycle();
 #endif
-      TRACE_EVENT_NESTABLE_ASYNC_BEGIN0(
-          "toplevel", "RendererMain.START_MSG_LOOP",
-          TRACE_ID_WITH_SCOPE("RendererMain.START_MSG_LOOP", 0));
+      TRACE_EVENT_INSTANT0("toplevel", "RendererMain.START_MSG_LOOP",
+                           TRACE_EVENT_SCOPE_THREAD);
       RenderThreadImpl::current()->set_run_loop_start_time(
           base::TimeTicks::Now());
       run_loop.Run();
-      TRACE_EVENT_NESTABLE_ASYNC_END0(
-          "toplevel", "RendererMain.START_MSG_LOOP",
-          TRACE_ID_WITH_SCOPE("RendererMain.START_MSG_LOOP", 0));
     }
 
 #if defined(LEAK_SANITIZER)
@@ -278,8 +271,6 @@ int RendererMain(MainFunctionParams parameters) {
 #endif
   }
   platform.PlatformUninitialize();
-  TRACE_EVENT_NESTABLE_ASYNC_END0("startup", "RendererMain",
-                                  TRACE_ID_WITH_SCOPE("RendererMain", 0));
   return 0;
 }
 
