de60ab27f4234691ab7880835cb25e81aa75e625
Dale Curtis <dalecurtis@chromium.org>
2021-11-19 23:25:52 +0000

    Disable Stop_Delayed_EnsureStarted_Immediate test.
    Flaky.
    
    R=thiabaud
    
    Bug: 1271419
    Change-Id: I0954172b1d6b55b1c4ebaf5f92bfeb95735a9815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293913
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Owners-Override: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#943723}

--- content/browser/compute_pressure/compute_pressure_sampler_unittest.cc
+++ content/browser/compute_pressure/compute_pressure_sampler_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/browser/compute_pressure/compute_pressure_sampler.h"
+#include "build/build_config.h"
 
 #include <cstddef>
 #include <memory>
@@ -178,7 +179,15 @@ TEST_F(ComputePressureSamplerTest, EnsureStarted_SkipsFirstSample) {
                             {.cpu_utilization = 0.4, .cpu_speed = 0.6})));
 }
 
-TEST_F(ComputePressureSamplerTest, Stop_Delayed_EnsureStarted_Immediate) {
+// TODO(crbug.com/1271419): Flaky.
+#if defined(OS_LINUX) || defined(OS_FUCHSIA)
+#define MAYBE_Stop_Delayed_EnsureStarted_Immediate \
+  DISABLED_Stop_Delayed_EnsureStarted_Immediate
+#else
+#define MAYBE_Stop_Delayed_EnsureStarted_Immediate \
+  Stop_Delayed_EnsureStarted_Immediate
+#endif
+TEST_F(ComputePressureSamplerTest, MAYBE_Stop_Delayed_EnsureStarted_Immediate) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   sampler_->EnsureStarted();
