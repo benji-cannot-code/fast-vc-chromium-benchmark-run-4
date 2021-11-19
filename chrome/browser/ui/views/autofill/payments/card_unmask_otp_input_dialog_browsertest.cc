4c9385609283d1be26d4096949b246c405dd518c
Vinny Persky <vinnypersky@google.com>
2021-11-19 11:33:01 +0000

    Enable Windows Browser Tests
    This CL is a follow-up to crrev.com/c/3203392. Based on this thread: https://groups.google.com/a/google.com/g/chrome-windows/c/HORfpPeE05U, this CL re-enables the Windows OS browser tests for Card Unmask OTP Input Dialog and Card Unmask Authentication Selection Dialog now that we have wired up the controller and views to the backend components.
    
    Change-Id: I1606a7af21bc88af771b72cf5c2109007f2c4cda
    Fixed: 1254686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282845
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#943468}

--- chrome/browser/ui/views/autofill/payments/card_unmask_otp_input_dialog_browsertest.cc
+++ chrome/browser/ui/views/autofill/payments/card_unmask_otp_input_dialog_browsertest.cc
@@ -61,16 +61,8 @@ class CardUnmaskOtpInputDialogBrowserTest : public DialogBrowserTest {
 };
 
 // Ensures the UI can be shown.
-#if defined(OS_WIN)
-// Triggering logic required for Windows OS runs: https://crbug.com/1254686
-#define MAYBE_InvokeUi_CardUnmaskOtpInputDialogDisplays \
-  DISABLED_InvokeUi_CardUnmaskOtpInputDialogDisplays
-#else
-#define MAYBE_InvokeUi_CardUnmaskOtpInputDialogDisplays \
-  InvokeUi_CardUnmaskOtpInputDialogDisplays
-#endif
 IN_PROC_BROWSER_TEST_F(CardUnmaskOtpInputDialogBrowserTest,
-                       MAYBE_InvokeUi_CardUnmaskOtpInputDialogDisplays) {
+                       InvokeUi_CardUnmaskOtpInputDialogDisplays) {
   base::HistogramTester histogram_tester;
 
   ShowAndVerifyUi();
@@ -83,15 +75,8 @@ IN_PROC_BROWSER_TEST_F(CardUnmaskOtpInputDialogBrowserTest,
 }
 
 // Ensures closing tab while dialog being visible is correctly handled.
-#if defined(OS_WIN)
-// Triggering logic required for Windows OS runs: https://crbug.com/1254686
-#define MAYBE_CanCloseTabWhileDialogShowing \
-  DISABLED_CanCloseTabWhileDialogShowing
-#else
-#define MAYBE_CanCloseTabWhileDialogShowing CanCloseTabWhileDialogShowing
-#endif
 IN_PROC_BROWSER_TEST_F(CardUnmaskOtpInputDialogBrowserTest,
-                       MAYBE_CanCloseTabWhileDialogShowing) {
+                       CanCloseTabWhileDialogShowing) {
   ShowUi("");
   VerifyUi();
   browser()->tab_strip_model()->GetActiveWebContents()->Close();
@@ -99,16 +84,8 @@ IN_PROC_BROWSER_TEST_F(CardUnmaskOtpInputDialogBrowserTest,
 }
 
 // Ensures closing browser while dialog being visible is correctly handled.
-#if defined(OS_WIN)
-// Triggering logic required for Windows OS runs: https://crbug.com/1254686
-#define MAYBE_CanCloseBrowserWhileDialogShowing \
-  DISABLED_CanCloseBrowserWhileDialogShowing
-#else
-#define MAYBE_CanCloseBrowserWhileDialogShowing \
-  CanCloseBrowserWhileDialogShowing
-#endif
 IN_PROC_BROWSER_TEST_F(CardUnmaskOtpInputDialogBrowserTest,
-                       MAYBE_CanCloseBrowserWhileDialogShowing) {
+                       CanCloseBrowserWhileDialogShowing) {
   ShowUi("");
   VerifyUi();
   browser()->window()->Close();
