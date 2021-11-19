49a87594d5e97e174d0f6934ed8558f8cdb404a3
Khem Raj <raj.khem@gmail.com>
2021-11-19 06:55:11 +0000

    ozone/wayland: Use stdint.h instead of bits/stdint-intn.h
    this header may not be available on all kind of systems e.g. musl/linux
    systems. including stdint.h should work across all
    
    Change-Id: If3ab4b2c51ee85ddc6726edb83318be555003193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291865
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Alexander Dunaev <adunaev@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#943435}

--- ui/ozone/platform/wayland/host/wayland_window.cc
+++ ui/ozone/platform/wayland/host/wayland_window.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/host/wayland_window.h"
 
-#include <bits/stdint-intn.h>
+#include <stdint.h>
 #include <wayland-cursor.h>
 #include <algorithm>
 #include <memory>
