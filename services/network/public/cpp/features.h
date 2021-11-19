72663d82873fab16066844d8a781e469fe18f148
Ari Chivukula <arichiv@chromium.org>
2021-11-19 01:11:22 +0000

    Revert "[Deprecation Issue for Legacy Client Hints] (1) Add Feature {Redux}"
    This reverts commit 75182b830e1e4e5ed2f94870272f00fa92f742fc.
    
    Reason for revert: We're not pursuing this approach anymore. Even if
    we need code like this in the future, we should revert and remove it
    until such time.
    
    Original change's description:
    > [Deprecation Issue for Legacy Client Hints] (1) Add Feature {Redux}
    >
    > I forgot that since we need to check for deprecation in the renderer and
    > browser we need to put shared code in services. This migrates that code
    > and adds a test for a core component (checking which hints are
    > deprecated and suggesting the alternate).
    >
    > This CL is part of a series, search for the bracketed prefix to find them:
    > (1) Add Feature
    > (2) Implement (Chromium + HTML)
    > (3) Implement (Chromium + HTTP)
    > (4) Implement (DevTools)
    > (5) Enable by default
    > (6) Bake in
    >
    > Bug: 1227043
    > Change-Id: I58d82785df4905b3cf5eb0a14ecb698eeefc4993
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3267826
    > Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    > Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    > Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#939932}
    
    Bug: 1227043
    Change-Id: I21f03157bd21524ecf422afd2e25cbbd4858ab03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291490
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943363}

--- services/network/public/cpp/features.h
+++ services/network/public/cpp/features.h
@@ -110,9 +110,6 @@ extern const base::Feature kCorsNonWildcardRequestHeadersSupport;
 COMPONENT_EXPORT(NETWORK_CPP)
 extern const base::Feature kURLLoaderSyncClient;
 
-COMPONENT_EXPORT(NETWORK_CPP)
-extern const base::Feature kClientHintDeprecationIssue;
-
 }  // namespace features
 }  // namespace network
 
