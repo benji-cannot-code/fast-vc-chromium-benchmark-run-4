58c1ffac3c23f803fca04033a1848e23e1e95e22
David Bienvenu <davidbienvenu@chromium.org>
2021-11-19 14:19:25 +0000

    unit_tests: fix flakiness due to null UserManager
    RemoteAppsManagerFactory depends on other keyed services which are
    base::Singletons. When unit tests use AtExitManager, all singletons are
    reset.
    
    Using base::Singleton in RemoteAppsManagerFactory allows it to be reset
    together with its dependencies, and properly re-inserted in the
    DependencyGraph. Otherwise it would be left in the graph with missing
    dependencies, which causes test flakiness in subsequent tests.
    
    Bug: 1269752
    Change-Id: Ic3834d2fa649f31bd61d1b2114a1a248f5d97b7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292340
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943504}

--- chrome/browser/ash/remote_apps/remote_apps_manager_factory.h
+++ chrome/browser/ash/remote_apps/remote_apps_manager_factory.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_REMOTE_APPS_REMOTE_APPS_MANAGER_FACTORY_H_
 #define CHROME_BROWSER_ASH_REMOTE_APPS_REMOTE_APPS_MANAGER_FACTORY_H_
 
-#include "base/no_destructor.h"
+#include "base/memory/singleton.h"
 #include "chrome/browser/ash/remote_apps/remote_apps_manager.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 
@@ -28,7 +28,7 @@ class RemoteAppsManagerFactory : public BrowserContextKeyedServiceFactory {
   static RemoteAppsManagerFactory* GetInstance();
 
  private:
-  friend base::NoDestructor<RemoteAppsManagerFactory>;
+  friend struct base::DefaultSingletonTraits<RemoteAppsManagerFactory>;
 
   RemoteAppsManagerFactory();
   RemoteAppsManagerFactory(const RemoteAppsManagerFactory&) = delete;
