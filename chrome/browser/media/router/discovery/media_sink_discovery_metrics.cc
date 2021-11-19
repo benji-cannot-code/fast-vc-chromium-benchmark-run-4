9455d0dcc90c40b132e2018d59f78877280f97b3
Takumi Fujimoto <takumif@chromium.org>
2021-11-19 23:03:56 +0000

    Add a metric for Cast sink name lengths
    Record the length whenever we open a Cast channel to a sink.
    
    Bug: 1271823
    Change-Id: I57d31d2869692cdb0a74eb14c9c51dfc1d8b26f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292488
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943710}

--- chrome/browser/media/router/discovery/media_sink_discovery_metrics.cc
+++ chrome/browser/media/router/discovery/media_sink_discovery_metrics.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/media/router/discovery/media_sink_discovery_metrics.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/time/default_clock.h"
 
@@ -83,6 +84,8 @@ const char CastAnalytics::kHistogramCastChannelConnectResult[] =
     "MediaRouter.Cast.Channel.ConnectResult";
 const char CastAnalytics::kHistogramCastChannelError[] =
     "MediaRouter.Cast.Channel.Error";
+const char CastAnalytics::kHistogramCastDeviceNameLength[] =
+    "MediaRouter.Cast.DeviceNameLength";
 const char CastAnalytics::kHistogramCastMdnsChannelOpenSuccess[] =
     "MediaRouter.Cast.Mdns.Channel.Open_Success";
 const char CastAnalytics::kHistogramCastMdnsChannelOpenFailure[] =
@@ -113,6 +116,11 @@ void CastAnalytics::RecordDeviceChannelOpenDuration(
   } else {
     UMA_HISTOGRAM_TIMES(kHistogramCastMdnsChannelOpenFailure, duration);
   }
+}
+
+// static
+void CastAnalytics::RecordDeviceNameLength(size_t length) {
+  base::UmaHistogramCounts100(kHistogramCastDeviceNameLength, length);
 }
 
 void WiredDisplayDeviceCountMetrics::RecordDeviceCounts(
