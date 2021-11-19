7515c3460b35fc0b9b9269296635237abdf0d7f8
Alex Moshchuk <alexmos@chromium.org>
2021-11-19 20:04:18 +0000

    Move <webview> guest site URL conversion code into SiteInfo.
    Currently, <webview> guests use specially constructed chrome-guest:
    site URLs that reflect a guest's StoragePartition configuration.  The
    translation between StoragePartitionConfigs and guest site URLs
    currently happens in the //extensions layer, inside WebViewGuest.
    
    As part of preparing to support site isolation in <webview>, this CL
    moves the specifics of guest site URLs into //content and hides guest
    site URL translations in SiteInfo.  //content already knows about
    guests and their special behavior (content::kGuestScheme is considered
    in process model decisions, SiteInstance already exposes IsGuest(),
    etc), so this wouldn't be any worse layering-wise.  The goal is to
    force all code that wants to interact with guests to do so via
    SiteInstance::IsGuest() and directly manipulating guest
    StoragePartitionConfigs (e.g., via
    SiteInstance::GetStoragePartitionConfig()), rather than via
    chrome-guest: site URLs; details about how a guest's
    StoragePartitionConfig is encoded in a site URL should be completely
    opaque to outside code. This will make it much easier to eventually
    convert guests to use real site URLs, which will be necessary for site
    isolation.
    
    More specifically, this CL makes the following changes:
    
    - Move GetSiteForGuestPartitionConfig and
      GetGuestPartitionConfigForSite from WebViewGuest into SiteInfo, and
      updates site <-> StoragePartitionConfig translations to happen
      within //content rather than outside it.  Note that WebViewGuest is
      still responsible for defining what a guest's StoragePartitionConfig
      is (i.e., assigning partition_domain to the embedder app's extension
      ID, and computing partition_name and in_memory based on <webview>'s
      partition attribute). This still happens in
      WebViewGuest::CreateWebContents(), and //content doesn't need to
      know about these semantics.
    
    - Updates SiteInstance::CreateForGuest to take a
      StoragePartitionConfig instead of a guest site URL, and lets
      SiteInfo::CreateForGuest generate the guest site URL internally.
    
    - Updates ExtensionNavigationThrottle and extensions ProcessManager to
      directly use StoragePartitionConfigs instead of guest site URLs.
    
    - The CreateForGuest() change required several updates to
      ServiceWorker and shared worker tests to be able to pass in an
      appropriate StoragePartition when workers are created in guests.  In
      particular, StoragePartitions for guests no longer track full guest
      site URLs and instead just store a bit that indicates that they're
      for guests.  These tests are also updated to take advantage of
      MockRenderProcessHost's support for StoragePartitions, which was
      recently added in https://crrev.com/c/3049856.
    
    This CL is based on acolwell@'s draft CL at
    https://chromium-review.googlesource.com/c/chromium/src/+/2750284.
    This is intended as a pure refactor, with no expected behavioral
    changes.
    
    Bug: 1267977, 1030932
    Change-Id: Ib1b8dcd7765f09cc9bc196640bd60a3fbe8563a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283604
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: W. James MacLean <wjmaclean@chromium.org>
    Commit-Queue: W. James MacLean <wjmaclean@chromium.org>
    Auto-Submit: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943621}

--- content/browser/site_instance_impl.h
+++ content/browser/site_instance_impl.h
@@ -37,6 +37,7 @@ class BrowsingInstance;
 class ProcessLock;
 class RenderProcessHostFactory;
 class SiteInstanceGroup;
+class StoragePartitionConfig;
 class StoragePartitionImpl;
 struct UrlInfo;
 
@@ -76,7 +77,7 @@ class CONTENT_EXPORT SiteInstanceImpl final : public SiteInstance,
       const UrlInfo& url_info);
   static scoped_refptr<SiteInstanceImpl> CreateForGuest(
       BrowserContext* browser_context,
-      const GURL& guest_site_url);
+      const StoragePartitionConfig& partition_config);
 
   // Creates a SiteInstance that will be use for a service worker.
   // `url_info` - The UrlInfo for the service worker. It contains the URL and
--- content/public/browser/site_instance.h
+++ content/public/browser/site_instance.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 class BrowserContext;
 class RenderProcessHost;
+class StoragePartitionConfig;
 
 using SiteInstanceId = base::IdType32<class SiteInstanceIdTag>;
 
@@ -217,11 +218,11 @@ class CONTENT_EXPORT SiteInstance : public base::RefCounted<SiteInstance> {
       const GURL& url);
 
   // Factory method to create a SiteInstance for a <webview> guest in a new
-  // BrowsingInstance.
-  // TODO(734722): Replace this method once SecurityPrincipal is available.
+  // BrowsingInstance. A guest requires a non-default StoragePartitionConfig
+  // which should be passed in via `partition_config`.
   static scoped_refptr<SiteInstance> CreateForGuest(
       BrowserContext* browser_context,
-      const GURL& guest_site_url);
+      const StoragePartitionConfig& partition_config);
 
   // Determine if a URL should "use up" a site.  URLs such as about:blank or
   // chrome-native:// leave the site unassigned.
