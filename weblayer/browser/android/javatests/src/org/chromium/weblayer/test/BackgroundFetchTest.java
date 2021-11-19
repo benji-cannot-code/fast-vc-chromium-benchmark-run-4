295f580c2bdda2b430b88a6891288607bfa5f738
Dale Curtis <dalecurtis@chromium.org>
2021-11-19 22:38:30 +0000

    Mark org.chromium.weblayer.test.BackgroundFetchTest#basic flaky.
    Bug: 1272010
    Change-Id: I5db948f311b31863dda76db95a57684e699d52ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288318
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Owners-Override: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#943700}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/BackgroundFetchTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/BackgroundFetchTest.java
@@ -26,6 +26,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.weblayer.Browser;
 import org.chromium.weblayer.OpenUrlCallback;
@@ -58,6 +59,7 @@ public class BackgroundFetchTest {
 
     @Test
     @LargeTest
+    @FlakyTest(message = "https://crbug.com/1272010")
     @MinAndroidSdkLevel(Build.VERSION_CODES.M)
     public void basic() throws Exception {
         Browser browser = mActivity.getBrowser();
