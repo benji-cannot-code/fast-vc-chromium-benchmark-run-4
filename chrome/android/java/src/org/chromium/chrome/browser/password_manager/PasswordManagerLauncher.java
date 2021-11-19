da9221150f0f556dbae316892a696608bf45cf71
Ioana Pandele <ioanap@chromium.org>
2021-11-19 17:24:40 +0000

    [CredManager] Add sync checks and use the new API
    This CL takes hidden dependencies out of PasswordManagerHelper, so that
    callers of the util methods have full knowledge of what is needed and
    don't need to mock internal implementation details in tests. These are
    the IdentityManager,the SyncService and the CredentialManagerLauncher.
    
    Bug:1255038
    
    Change-Id: Ic8d6f6f59d7365c54d2505520cfb390158185cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268679
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943572}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/PasswordManagerLauncher.java
@@ -13,9 +13,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
 import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.sync.SyncService;
-import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.signin.identitymanager.IdentityManager;
-import org.chromium.components.sync.ModelType;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.WindowAndroid;
 
@@ -34,11 +32,18 @@ public class PasswordManagerLauncher {
      */
     public static void showPasswordSettings(
             Activity activity, @ManagePasswordsReferrer int referrer) {
-        if (isSyncingPasswordsWithoutCustomPassphrase()
+        IdentityManager identityManager = IdentityServicesProvider.get().getIdentityManager(
+                Profile.getLastUsedRegularProfile());
+        SyncService syncService = SyncService.get();
+        if (PasswordManagerHelper.isSyncingPasswordsWithoutCustomPassphrase(
+                    identityManager, syncService)
                 && ChromeFeatureList.isEnabled(ChromeFeatureList.PASSWORD_SCRIPTS_FETCHING)) {
             PasswordScriptsFetcherBridge.prewarmCache();
         }
-        PasswordManagerHelper.showPasswordSettings(activity, referrer, new SettingsLauncherImpl());
+        CredentialManagerLauncher credentialManagerLauncher = null;
+        PasswordManagerHelper.showPasswordSettings(activity, referrer, new SettingsLauncherImpl(),
+                CredentialManagerLauncherFactory.getInstance().createLauncher(), identityManager,
+                syncService);
     }
 
     @CalledByNative
@@ -48,21 +53,5 @@ public class PasswordManagerLauncher {
         if (window == null) return;
         WeakReference<Activity> currentActivity = window.getActivity();
         showPasswordSettings(currentActivity.get(), referrer);
-    }
-
-    public static boolean isSyncingPasswordsWithoutCustomPassphrase() {
-        IdentityManager identityManager = IdentityServicesProvider.get().getIdentityManager(
-                Profile.getLastUsedRegularProfile());
-        if (!identityManager.hasPrimaryAccount(ConsentLevel.SYNC)) return false;
-
-        SyncService syncService = SyncService.get();
-        if (syncService == null
-                || !syncService.getActiveDataTypes().contains(ModelType.PASSWORDS)) {
-            return false;
-        }
-
-        if (syncService.isUsingExplicitPassphrase()) return false;
-
-        return true;
     }
 }
