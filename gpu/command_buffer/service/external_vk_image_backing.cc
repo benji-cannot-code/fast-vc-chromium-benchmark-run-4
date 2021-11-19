5e4c9c5306325f8d4035ff0b15c001afb6884dfc
Vikas Soni <vikassoni@chromium.org>
2021-11-19 21:37:23 +0000

    Mark ExternalVkImageBacking as cleared.
    Mark ExternalVkImageBacking as cleared to ensure that it is not treated
    as uninitialized.
    
    Change-Id: If4ce35a9def52900e8954d6d65498b4817ad3a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292243
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943673}

--- gpu/command_buffer/service/external_vk_image_backing.cc
+++ gpu/command_buffer/service/external_vk_image_backing.cc
@@ -226,6 +226,9 @@ std::unique_ptr<ExternalVkImageBacking> ExternalVkImageBacking::Create(
   if (!pixel_data.empty()) {
     size_t stride = BitsPerPixel(format) / 8 * size.width();
     backing->WritePixelsWithData(pixel_data, stride);
+
+    // Mark the backing as cleared.
+    backing->SetCleared();
   }
 
   return backing;
