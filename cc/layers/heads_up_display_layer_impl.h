ceb391b609382355d13073ea7bb87b28ea4fe090
Vladimir Levin <vmpstr@chromium.org>
2021-11-19 20:26:48 +0000

    cc: Adjust HUD layer to account for zoom-for-dsf.
    This patch updates the coordinate spaces of the HUD layer so that
    they work consistently with or without zoom-for-dsf, which uses
    painted_device_scale_factor and provides sizes in physical coordinates
    (ie layout coordinates) instead of DIPS.
    
    The HUD code paints things in DIPs, so this patch updates the drawing
    code to also be aware that there are two spaces at play here.
    
    R=szager@chromium.org
    
    Fixed: 1271475
    Change-Id: Ifc3aef4093699fca6007bf291c7d5d4e64992ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293481
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943632}

--- cc/layers/heads_up_display_layer_impl.h
+++ cc/layers/heads_up_display_layer_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/resources/memory_history.h"
 #include "cc/resources/resource_pool.h"
 #include "cc/trees/debug_rect_history.h"
+#include "cc/trees/layer_tree_impl.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 
 class SkTypeface;
@@ -163,6 +164,12 @@ class CC_EXPORT HeadsUpDisplayLayerImpl : public LayerImpl {
                                int left,
                                int top,
                                int width) const;
+
+  int bounds_width_in_dips() const {
+    // bounds() is specified in layout coordinates, which is painted dsf away
+    // from DIPs.
+    return bounds().width() / layer_tree_impl()->painted_device_scale_factor();
+  }
 
   ResourcePool::InUsePoolResource in_flight_resource_;
   std::unique_ptr<ResourcePool> pool_;
