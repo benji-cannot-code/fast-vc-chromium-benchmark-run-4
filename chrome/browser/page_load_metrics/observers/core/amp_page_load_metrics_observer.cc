f6a9d747a8eed21c5480fa5fbcadb8b7948418fa
Daniel Cheng <dcheng@chromium.org>
2021-11-19 11:34:43 +0000

    Use GURL::Replacements::SetQueryStr() in AMP page load metrics observer.
    This migrates the code to use a easier-to-use helper for setting a query
    string replacement.
    
    Bug: 1271865
    Change-Id: Id0593f77ef55364de0ec24dcc5bcc55afa9819d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292946
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Owners-Override: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943470}

--- chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "components/page_load_metrics/browser/observers/core/largest_contentful_paint_handler.h"
@@ -127,9 +128,9 @@ GURL GetViewerUrlFromCacheUrl(const GURL& url) {
   // (&viewerURL=<URL>). net::QueryIterator only operates on the query string,
   // so we copy the fragment into the query string, then iterate over the
   // parameters below.
-  std::string ref = url.ref();
+  base::StringPiece ref = url.ref_piece();
   GURL::Replacements replacements;
-  replacements.SetQuery(ref.c_str(), url::Component(0, ref.length()));
+  replacements.SetQueryStr(ref);
   GURL modified_url = url.ReplaceComponents(replacements);
   for (net::QueryIterator it(modified_url); !it.IsAtEnd(); it.Advance()) {
     if (it.GetKey() == "viewerUrl")
