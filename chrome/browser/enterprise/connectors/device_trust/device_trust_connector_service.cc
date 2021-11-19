2f349d929949df29443e88a057136cd3a3849e89
Roger Tawa <rogerta@chromium.org>
2021-11-19 16:12:06 +0000

    Make sure initial policy load is handled correctly in DTKeyManager.
    Bug: 207020513
    Change-Id: I93e58a1b57da230ee92f036d06faf2394c42deb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290244
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943538}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_connector_service.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_connector_service.cc
@@ -43,7 +43,20 @@ DeviceTrustConnectorService::DeviceTrustConnectorService(
     : profile_prefs_(profile_prefs),
       matcher_(std::make_unique<url_matcher::URLMatcher>()) {
   DCHECK(profile_prefs_);
+}
+
+DeviceTrustConnectorService::~DeviceTrustConnectorService() = default;
+
+bool DeviceTrustConnectorService::IsConnectorEnabled() const {
+  return DeviceTrustConnectorService::IsConnectorEnabled(profile_prefs_);
+}
+
+void DeviceTrustConnectorService::Initialize() {
   if (!IsDeviceTrustConnectorFeatureEnabled()) {
+    return;
+  }
+
+  if (!pref_observer_.IsEmpty()) {
     return;
   }
 
@@ -55,12 +68,6 @@ DeviceTrustConnectorService::DeviceTrustConnectorService(
 
   // Call once to initialize the watcher with the current pref's values.
   OnPolicyUpdated();
-}
-
-DeviceTrustConnectorService::~DeviceTrustConnectorService() = default;
-
-bool DeviceTrustConnectorService::IsConnectorEnabled() const {
-  return DeviceTrustConnectorService::IsConnectorEnabled(profile_prefs_);
 }
 
 bool DeviceTrustConnectorService::Watches(const GURL& url) const {
