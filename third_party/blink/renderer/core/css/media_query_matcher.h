ab8a6308ee040c4bc7c94aed4b82e94208ee7da2
Michael Lippautz <mlippautz@chromium.org>
2021-11-20 09:54:07 +0000

    heap: Use iwyu for HeapLinkedHashSet
    Bug: chromium:1269227
    Change-Id: I99278fa218a286252b44405f7582f1aea1d5588a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293006
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943819}

--- third_party/blink/renderer/core/css/media_query_matcher.h
+++ third_party/blink/renderer/core/css/media_query_matcher.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_MEDIA_QUERY_MATCHER_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_linked_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
--- third_party/blink/renderer/core/layout/hit_test_result.h
+++ third_party/blink/renderer/core/layout/hit_test_result.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/geometry/float_quad.h"
 #include "third_party/blink/renderer/platform/geometry/float_rect.h"
 #include "third_party/blink/renderer/platform/graphics/compositor_element_id.h"
+#include "third_party/blink/renderer/platform/heap/collection_support/heap_linked_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/text/text_direction.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
