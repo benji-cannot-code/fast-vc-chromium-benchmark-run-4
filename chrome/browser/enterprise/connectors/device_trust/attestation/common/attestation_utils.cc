8caab982c6ba3768a10661b088a240ad6df46c1c
Roger Tawa <rogerta@chromium.org>
2021-11-20 01:53:34 +0000

    Make JsonChallengeToProtobufChallenge more robust.
    JsonChallengeToProtobufChallenge() checks that the challenge returned
    by the origin has the correct fields but it was not checking the fiels
    types. It assumed a field was a string and would crash if not.
    
    Bug: b/207136898
    Change-Id: Id6be222a20d9e228e7da1c206325101b6a170e05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292076
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943779}

--- chrome/browser/enterprise/connectors/device_trust/attestation/common/attestation_utils.cc
+++ chrome/browser/enterprise/connectors/device_trust/attestation/common/attestation_utils.cc
@@ -14,18 +14,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_connectors {
 
-std::string JsonChallengeToProtobufChallenge(const std::string& challenge) {
+std::string JsonChallengeToProtobufChallenge(
+    const std::string& json_challenge) {
   absl::optional<base::Value> data = base::JSONReader::Read(
-      challenge, base::JSONParserOptions::JSON_ALLOW_TRAILING_COMMAS);
+      json_challenge, base::JSONParserOptions::JSON_ALLOW_TRAILING_COMMAS);
+  if (!data) {
+    LOG(ERROR) << "Error reading JSON challenge.";
+    return std::string();
+  }
 
   // If json is malformed or it doesn't include the needed field return
   // an empty string.
-  if (!data || !data.value().FindPath("challenge"))
+  const std::string* challenge = data.value().FindStringPath("challenge");
+  if (!challenge) {
+    LOG(ERROR) << "JSON challenge does not have required field.";
     return std::string();
+  }
 
   std::string serialized_signed_challenge;
-  if (!base::Base64Decode(data.value().FindPath("challenge")->GetString(),
-                          &serialized_signed_challenge)) {
+  if (!base::Base64Decode(*challenge, &serialized_signed_challenge)) {
     LOG(ERROR) << "Error during decoding base64 challenge.";
     return std::string();
   }
