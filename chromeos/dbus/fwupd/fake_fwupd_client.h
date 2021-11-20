5edf102db16c5f0ce4818252c3ba9b39747331b9
Zentaro Kavanagh <zentaro@chromium.org>
2021-11-20 00:46:24 +0000

    Firmware Updater: Get updates for all devices
    Bug: 1252981
    Test: ash_unittests, chromeos_unittests
    Change-Id: I5f2d9b47ee202dddab10bf75bb871a477783332f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290588
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943762}

--- chromeos/dbus/fwupd/fake_fwupd_client.h
+++ chromeos/dbus/fwupd/fake_fwupd_client.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-class COMPONENT_EXPORT(CHROMEOS_DBUS_FUWPD) FakeFwupdClient
+class COMPONENT_EXPORT(CHROMEOS_DBUS_FWUPD) FakeFwupdClient
     : public FwupdClient {
  public:
   FakeFwupdClient();
