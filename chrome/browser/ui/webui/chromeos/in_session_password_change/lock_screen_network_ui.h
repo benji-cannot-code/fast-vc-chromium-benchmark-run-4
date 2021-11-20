e9c2ed6b01a6f5171c60f6ee51561e1651ddb88e
Maciek Slusarczyk <mslus@chromium.org>
2021-11-20 19:24:43 +0000

    Browser tests for the lock screen network dialog.
    This CL adds browser tests for lock screen network dialog and simulates
    situations when the dialog should pop up (like wifi disconnect).
    
    Bug: 1237407
    Change-Id: Iae3d574aa6b116c4b48358ab0df69fff469d679b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3229272
    Commit-Queue: Maciek Slusarczyk <mslus@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943849}

--- chrome/browser/ui/webui/chromeos/in_session_password_change/lock_screen_network_ui.h
+++ chrome/browser/ui/webui/chromeos/in_session_password_change/lock_screen_network_ui.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_IN_SESSION_PASSWORD_CHANGE_LOCK_SCREEN_NETWORK_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_IN_SESSION_PASSWORD_CHANGE_LOCK_SCREEN_NETWORK_UI_H_
 
+#include "chrome/browser/ui/webui/chromeos/in_session_password_change/lock_screen_network_handler.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom-forward.h"
+
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
 #include "ui/webui/mojo_web_ui_controller.h"
@@ -21,8 +23,6 @@ namespace chromeos {
 class LockScreenNetworkUI : public ui::MojoWebDialogUI {
  public:
   explicit LockScreenNetworkUI(content::WebUI* web_ui);
-  LockScreenNetworkUI(LockScreenNetworkUI const&) = delete;
-  LockScreenNetworkUI& operator=(const LockScreenNetworkUI&) = delete;
   ~LockScreenNetworkUI() override;
 
   void GetLocalizedStrings(base::DictionaryValue* localized_strings);
@@ -32,7 +32,14 @@ class LockScreenNetworkUI : public ui::MojoWebDialogUI {
   void BindInterface(
       mojo::PendingReceiver<network_config::mojom::CrosNetworkConfig> receiver);
 
+  NetworkConfigMessageHandler* GetMainHandlerForTests() {
+    return main_handler_;
+  }
+
  private:
+  // The main message handler owned by the corresponding WebUI.
+  NetworkConfigMessageHandler* main_handler_;
+
   WEB_UI_CONTROLLER_TYPE_DECL();
 };
 
