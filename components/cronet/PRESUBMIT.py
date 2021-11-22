ed351abaa1eb297a789c0e18529187d88ba11fa7
Joshua Hood <jdh@chromium.org>
2021-11-22 22:37:59 +0000

    [CodeHealth] Migrate presubmit checks in components/cronet/ to Python3
    and fixed resulting errors.
    
    Bug: 1262285
    Change-Id: I5960d8eb23a4d879c94388a11adcf86d9b86ccba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282041
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Joshua Hood‎ <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944270}

--- components/cronet/PRESUBMIT.py
+++ components/cronet/PRESUBMIT.py
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
 """
-
 import os
+
+USE_PYTHON3 = True
 
 def _PyLintChecks(input_api, output_api):
   pylint_checks = input_api.canned_checks.GetPylint(input_api, output_api,
-          extra_paths_list=_GetPathsToPrepend(input_api), pylintrc='pylintrc')
+          extra_paths_list=_GetPathsToPrepend(input_api), pylintrc='pylintrc',
+          version='2.6')
   return input_api.RunTests(pylint_checks)
 
 
@@ -70,8 +72,7 @@ def _PackageChecks(input_api, output_api):
         'API classes must be in org.chromium.net package, and implementation\n'
         'classes must not be in org.chromium.net package.',
         problems)]
-  else:
-    return []
+  return []
 
 
 def _RunToolsUnittests(input_api, output_api):
@@ -79,7 +80,9 @@ def _RunToolsUnittests(input_api, output_api):
       input_api, output_api,
       '.',
       [ r'^tools_unittest\.py$'],
-      run_on_python3=True)
+      run_on_python2 = False,
+      run_on_python3 = True,
+      skip_shebang_check = True)
 
 
 def _ChangeAffectsCronetTools(change):
