b54743d4690efec853129264fc83d096e67f903e
Peter Boström <pbos@chromium.org>
2021-11-22 22:27:11 +0000

    Remove ExtensionsSidePanelController
    This concludes the kExtensionsSidePanel experiment.
    
    Bug: 1197555, 1270923
    Change-Id: I7a93f72acd9bbc82f73b818edda99860e939e16e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296393
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944269}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -64,7 +64,6 @@ class BookmarkBarView;
 class Browser;
 class ContentsLayoutManager;
 class ExclusiveAccessBubbleViews;
-class ExtensionsSidePanelController;
 class FeaturePromoControllerViews;
 class FullscreenControlHost;
 class InfoBarContainerView;
@@ -193,10 +192,6 @@ class BrowserView : public BrowserWindow,
 
   SidePanel* left_aligned_side_panel_for_testing() {
     return left_aligned_side_panel_;
-  }
-
-  ExtensionsSidePanelController* extensions_side_panel_controller() {
-    return extensions_side_panel_controller_.get();
   }
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -963,10 +958,6 @@ class BrowserView : public BrowserWindow,
   // The side panel aligned to the left side of the browser window.
   SidePanel* left_aligned_side_panel_ = nullptr;
   views::View* left_aligned_side_panel_separator_ = nullptr;
-
-  // A controller that handles extensions hosted in the left aligned side panel.
-  std::unique_ptr<ExtensionsSidePanelController>
-      extensions_side_panel_controller_;
 
   // The Lens side panel.
   SidePanel* lens_side_panel_ = nullptr;
--- chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bubble_view.h"
 #include "chrome/browser/ui/views/extensions/extension_popup.h"
-#include "chrome/browser/ui/views/extensions/extensions_side_panel_controller.h"
 #include "chrome/browser/ui/views/extensions/extensions_toolbar_button.h"
 #include "chrome/browser/ui/views/extensions/extensions_toolbar_container.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
@@ -301,11 +300,6 @@ void ToolbarView::Init() {
   }
 
   // Always add children in order from left to right, for accessibility.
-  if (browser_view_->extensions_side_panel_controller()) {
-    left_side_panel_button_ =
-        AddChildView(browser_view_->extensions_side_panel_controller()
-                         ->CreateToolbarButton());
-  }
   back_ = AddChildView(std::move(back));
   forward_ = AddChildView(std::move(forward));
   reload_ = AddChildView(std::move(reload));
