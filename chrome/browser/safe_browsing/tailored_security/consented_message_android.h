b59dae8524e821882016f9be0d6eb1aa0085b169
Rohit Bhatia <bhatiarohit@google.com>
2021-11-22 19:03:57 +0000

    Add Project ChAI UI for consented users on Android.
    When a signed-in synced user changes their Tailored
    Security account setting, we want to prompt them that
    Chrome Enhanced Safe Browsing was enabled.
    This Android message will prompt them.
    
    Added safety-check icon from chrome://settings/privacy.
    
    Screenshot at http://screen/6MvuRRSjRmwws9e.png
    
    Bug: 1257621
    Change-Id: I4dea97896f6bb4eec90dc702cdf787e23a2a41f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275315
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Cr-Commit-Position: refs/heads/main@{#944174}

---
+++ chrome/browser/safe_browsing/tailored_security/consented_message_android.h
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_CONSENTED_MESSAGE_ANDROID_H_
+#define CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_CONSENTED_MESSAGE_ANDROID_H_
+
+#include <memory>
+
+#include "components/messages/android/message_enums.h"
+#include "components/messages/android/message_wrapper.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+namespace safe_browsing {
+
+class TailoredSecurityConsentedModalAndroid {
+ public:
+  TailoredSecurityConsentedModalAndroid();
+  ~TailoredSecurityConsentedModalAndroid();
+
+  void DisplayMessage(content::WebContents* web_contents);
+  void DismissMessage(messages::DismissReason dismiss_reason);
+
+ private:
+  void HandleSettingsClicked();
+  void HandleMessageDismissed(messages::DismissReason dismiss_reason);
+
+  std::unique_ptr<messages::MessageWrapper> message_;
+  content::WebContents* web_contents_ = nullptr;
+};
+
+}  // namespace safe_browsing
+
+#endif  // CHROME_BROWSER_SAFE_BROWSING_TAILORED_SECURITY_CONSENTED_MESSAGE_ANDROID_H_
