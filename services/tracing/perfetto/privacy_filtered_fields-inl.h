15806f974990b5fb4614404b0eb1014c8ace36ec
ssid <ssid@chromium.org>
2021-11-22 18:40:44 +0000

    tracing: Migrate renderer visibility events to protos
    Also updates the privacy filter after cl/410283070
    
    Bug: b/204458150
    Change-Id: I578708db0b497e803917a27840dfc7cbabb1aad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283539
    Reviewed-by: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944164}

--- services/tracing/perfetto/privacy_filtered_fields-inl.h
+++ services/tracing/perfetto/privacy_filtered_fields-inl.h
@@ -183,7 +183,7 @@ constexpr MessageInfo kChromeApplicationStateInfo = {
     kChromeApplicationStateInfoIndices, nullptr};
 
 // Proto Message: ChromeRendererSchedulerState
-constexpr int kChromeRendererSchedulerStateIndices[] = {1, -1};
+constexpr int kChromeRendererSchedulerStateIndices[] = {1, 2, 3, -1};
 constexpr MessageInfo kChromeRendererSchedulerState = {
     kChromeRendererSchedulerStateIndices, nullptr};
 
