4232b0a3c10f8bc60765abab72903b9550374861
Matt Menke <mmenke@chromium.org>
2021-11-22 15:21:34 +0000

    [url] Add SchemeHostPort mojom file.
    url::Origin::Create() is slated to be deprecated to help protect against
    misuse. Per SchemeHostPort's docs, it should be used for to represent
    network authorities instead of url::Origins. Some consumers that are
    currently using url::Origins or GURLs to represent network authorities
    need to pass this information cross-process (e.g., HTTP auth and
    reporting), so these now need to be able to be passed over Mojo pipes.
    
    Bug: 1270878
    Change-Id: I90a541e5df690586e8c3538c89141502f8304777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288194
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944072}

---
+++ url/mojom/scheme_host_port_mojom_traits.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "url/mojom/scheme_host_port_mojom_traits.h"
+
+#include "base/strings/string_piece.h"
+#include "url/mojom/scheme_host_port.mojom-shared.h"
+#include "url/scheme_host_port.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<url::mojom::SchemeHostPortDataView, url::SchemeHostPort>::
+    Read(url::mojom::SchemeHostPortDataView data, url::SchemeHostPort* out) {
+  base::StringPiece scheme, host;
+  if (!data.ReadScheme(&scheme) || !data.ReadHost(&host))
+    return false;
+
+  *out = url::SchemeHostPort(scheme, host, data.port());
+
+  // Consider it an error if the output SchemeHostPort is not valid, but
+  // non-empty values were received over Mojo.
+  return out->IsValid() || (scheme.empty() && host.empty() && data.port() == 0);
+}
+
+}  // namespace mojo
