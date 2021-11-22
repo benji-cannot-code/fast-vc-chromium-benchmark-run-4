784635f896fa8ed6a83885a0905a200981bc2da4
Jason Jeremy Iman <jasongustaman@google.com>
2021-11-22 07:19:42 +0000

    arc: net: Implement AddPasspointCredentials
    Filled in the implementation to add passpoint credentials. This
    patch only have support for username and password EAP-TTLS.
    Certificate provisioning and EAP-TLS will be added in another
    patch.
    
    BUG=b:195262431
    TEST=Called shill manager's AddPaspointCredentials without error.
    
    Change-Id: Ibc898c11574c6f30e3dec93ef24b806524e76b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272121
    Commit-Queue: Jason Jeremy Iman <jasongustaman@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Cr-Commit-Position: refs/heads/main@{#943969}

--- ash/components/arc/net/arc_net_host_impl.h
+++ ash/components/arc/net/arc_net_host_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "base/values.h"
 #include "chromeos/network/network_connection_observer.h"
+#include "chromeos/network/network_profile_handler.h"
 #include "chromeos/network/network_state_handler_observer.h"
 #include "components/arc/mojom/net.mojom.h"
 #include "components/arc/session/connection_observer.h"
@@ -95,7 +96,12 @@ class ArcNetHostImpl : public KeyedService,
   std::unique_ptr<base::DictionaryValue> TranslateVpnConfigurationToOnc(
       const mojom::AndroidVpnConfiguration& cfg);
 
-  // Overriden from chromeos::NetworkStateHandlerObserver.
+  base::Value TranslateEapCredentialsToDict(const mojom::EapCredentials& cred);
+
+  base::Value TranslatePasspointCredentialsToDict(
+      const mojom::PasspointCredentials& cred);
+
+  // Overridden from chromeos::NetworkStateHandlerObserver.
   void ScanCompleted(const chromeos::DeviceState* /*unused*/) override;
   void OnShuttingDown() override;
   void NetworkConnectionStateChanged(
@@ -104,7 +110,7 @@ class ArcNetHostImpl : public KeyedService,
   void DeviceListChanged() override;
   void NetworkPropertiesUpdated(const chromeos::NetworkState* network) override;
 
-  // Overriden from chromeos::NetworkConnectionObserver.
+  // Overridden from chromeos::NetworkConnectionObserver.
   void DisconnectRequested(const std::string& service_path) override;
 
   // Overridden from ConnectionObserver<mojom::NetInstance>:
@@ -135,8 +141,16 @@ class ArcNetHostImpl : public KeyedService,
   // Convert a vector of strings, |string_list|, to a base::Value
   // that can be added to an ONC dictionary.  This is used for fields
   // like NameServers, SearchDomains, etc.
-  std::unique_ptr<base::Value> TranslateStringListToValue(
+  base::Value TranslateStringListToValue(
       const std::vector<std::string>& string_list);
+
+  // Convert a vector of uint64_t, |long_list|, to a base::Value of type list
+  // that can be passed to shill. This is because 64-bit integer values are not
+  // supported for base::Value.
+  // The translated values will be a list of decimal string and not a single
+  // string.
+  base::Value TranslateLongListToStringValue(
+      const std::vector<uint64_t>& long_list);
 
   // Ask Shill to connect to the Android VPN with name |service_path|.
   // |service_path| and |guid| are stored locally for future reference.
