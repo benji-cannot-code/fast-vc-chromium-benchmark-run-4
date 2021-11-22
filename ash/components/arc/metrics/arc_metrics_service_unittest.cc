9aa6dfb4eb78d6359aeb81fdfdf81e859a4b931e
Yury Khmel <khmel@chromium.org>
2021-11-22 22:01:49 +0000

    arc: Separate preANR handling per ANR type.
    Initial results show good results for preventing ContentProvider ANRs
    (>80%). However for Service ANR recovery rate ~20%. This might be due to
    fact that foreground service ANR timeout is 20s compare to 10s of
    ContentProvider. This also separate Service ANRs to Foreground and
    Background.
    
    BUG=b:204599606
    TEST=Locally + unit test
    
    Change-Id: Ia02d3e2783039e15bd33ca9a5b85b1455a74f89e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292245
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944260}

--- ash/components/arc/metrics/arc_metrics_service_unittest.cc
+++ ash/components/arc/metrics/arc_metrics_service_unittest.cc
@@ -404,10 +404,20 @@ TEST_F(ArcMetricsServiceTest, ArcAnr) {
   expectation[CreateAnrKey(kAppTypeSystemServer, mojom::AnrType::INPUT)] = 1;
   VerifyAnr(tester, expectation);
 
-  service()->ReportAnr(
-      GetAnr(mojom::AnrSource::SYSTEM_SERVER, mojom::AnrType::SERVICE));
-  expectation[CreateAnrKey(kAppOverall, mojom::AnrType::SERVICE)] = 1;
-  expectation[CreateAnrKey(kAppTypeSystemServer, mojom::AnrType::SERVICE)] = 1;
+  service()->ReportAnr(GetAnr(mojom::AnrSource::SYSTEM_SERVER,
+                              mojom::AnrType::FOREGROUND_SERVICE));
+  expectation[CreateAnrKey(kAppOverall, mojom::AnrType::FOREGROUND_SERVICE)] =
+      1;
+  expectation[CreateAnrKey(kAppTypeSystemServer,
+                           mojom::AnrType::FOREGROUND_SERVICE)] = 1;
+  VerifyAnr(tester, expectation);
+
+  service()->ReportAnr(GetAnr(mojom::AnrSource::SYSTEM_SERVER,
+                              mojom::AnrType::BACKGROUND_SERVICE));
+  expectation[CreateAnrKey(kAppOverall, mojom::AnrType::BACKGROUND_SERVICE)] =
+      1;
+  expectation[CreateAnrKey(kAppTypeSystemServer,
+                           mojom::AnrType::BACKGROUND_SERVICE)] = 1;
   VerifyAnr(tester, expectation);
 
   service()->ReportAnr(
@@ -436,11 +446,12 @@ TEST_F(ArcMetricsServiceTest, ArcAnr) {
   expectation[CreateAnrKey(kAppTypeArcOther, mojom::AnrType::INPUT)] = 1;
   VerifyAnr(tester, expectation);
 
-  service()->ReportAnr(
-      GetAnr(mojom::AnrSource::ARC_APP_LAUNCHER, mojom::AnrType::SERVICE));
-  expectation[CreateAnrKey(kAppOverall, mojom::AnrType::SERVICE)] = 2;
-  expectation[CreateAnrKey(kAppTypeArcAppLauncher, mojom::AnrType::SERVICE)] =
-      1;
+  service()->ReportAnr(GetAnr(mojom::AnrSource::ARC_APP_LAUNCHER,
+                              mojom::AnrType::FOREGROUND_SERVICE));
+  expectation[CreateAnrKey(kAppOverall, mojom::AnrType::FOREGROUND_SERVICE)] =
+      2;
+  expectation[CreateAnrKey(kAppTypeArcAppLauncher,
+                           mojom::AnrType::FOREGROUND_SERVICE)] = 1;
   VerifyAnr(tester, expectation);
 }
 
