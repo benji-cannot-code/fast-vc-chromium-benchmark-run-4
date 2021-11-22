58815e74a0e58b95a7a8528ad129017b00352c12
Simon Zünd <szuend@chromium.org>
2021-11-22 08:29:24 +0000

    Re-enable http/tests/devtools/cache-storage/cache-live-update-list.js
    This CL can be landed once https://crrev.com/c/3291153 has landed
    in the frontend.
    
    R=tvanderlippe@chromium.org
    
    Bug: 1222126
    Change-Id: I090c080ab9b210552f90a34cdd70f02f48aa1cac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289422
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943979}

--- third_party/blink/web_tests/http/tests/devtools/cache-storage/cache-live-update-list.js
+++ third_party/blink/web_tests/http/tests/devtools/cache-storage/cache-live-update-list.js
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   var cacheStorageModel = TestRunner.mainTarget.model(SDK.ServiceWorkerCacheModel);
   cacheStorageModel.enable();
-  cacheStorageModel.throttler._timeout = 0;
+  cacheStorageModel.setThrottlerSchedulesAsSoonAsPossibleForTest();
 
   await ApplicationTestRunner.clearAllCaches();
   await ApplicationTestRunner.dumpCacheTree();
