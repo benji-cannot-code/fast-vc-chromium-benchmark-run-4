fbb3cdef28a23ec39f4bbd5c1d2f7137ff71cfef
Eric Sum <esum@google.com>
2021-11-22 20:16:54 +0000

    Add SkottieFrameData provider for Skottie animations.
    Last CL for this specific project! This is the part that allows the
    application to insert its own logic for generating images for assets
    in the animation. They do so by overriding a "provider" interface that
    mirrors Skottie's ResourceProvider API. Where the image comes from and
    how often it changes from frame to frame is specific to each use case.
    When the images are provided, they are sent through that whole
    multi-process skottie rastering pipeline that was built in previous CLs.
    
    Bug: 1266051
    Bug: b:203584064
    Test: ui_base_unittests
    Change-Id: Id9a7c8552f53ca8e261e4309348247b30b568a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3265726
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#944216}

--- cc/paint/skottie_frame_data.h
+++ cc/paint/skottie_frame_data.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_PAINT_SKOTTIE_FRAME_DATA_H_
 
 #include "base/containers/flat_map.h"
+#include "cc/paint/paint_export.h"
 #include "cc/paint/paint_flags.h"
 #include "cc/paint/paint_image.h"
 #include "cc/paint/skottie_resource_metadata.h"
@@ -19,7 +20,7 @@ namespace cc {
 //
 // There's currently no use case for |skresources::ImageAsset::FrameData.matrix|
 // so it is omitted for now.
-struct SkottieFrameData {
+struct CC_PAINT_EXPORT SkottieFrameData {
   // PaintImage is preferable at the compositor layer instead of a "raw"
   // SkImage. It not only is more well supported for circulating through the
   // compositor/graphics pipeline, but also gives the client the most
@@ -30,6 +31,9 @@ struct SkottieFrameData {
   // image needs to be resized when rendering.
   PaintFlags::FilterQuality quality;
 };
+
+CC_PAINT_EXPORT bool operator==(const SkottieFrameData& frame_l,
+                                const SkottieFrameData& frame_r);
 
 // Map from asset id to the image to use for that asset.
 using SkottieFrameDataMap =
