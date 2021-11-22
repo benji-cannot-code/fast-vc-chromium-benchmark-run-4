16a50a3d17649a4967d8d4fe59f94c372f15954d
Guanru Lee <guanrulee@chromium.org>
2021-11-22 05:38:20 +0000

    Phone Hub: Update permissions dialog for screen lock
    - Finalize the strings.
    - Load current screen lock state from C++.
    - Move the position of subtext next to the radion button.
    
    Original: https://screenshot.googleplex.com/wj6MdVrLm9nyCEA.png
    New: https://screenshot.googleplex.com/9eDufGSEjHmMDKY.png
    
    "OSSettingsMultideviceNotificationAccessSetupDialogV3Test.*"
    
    Bug: b/184648164
    Test: ./out/Default/browser_tests --gtest_filter=
    Change-Id: Ie9c540a9fc423c7f0b04794cc63261866b620eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3233958
    Commit-Queue: Guanru Lee <guanrulee@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943959}

--- chrome/browser/ui/webui/settings/chromeos/multidevice_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/multidevice_section.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/chromeos/multidevice_section.h"
 
 #include "ash/components/phonehub/phone_hub_manager.h"
+#include "ash/components/phonehub/pref_names.h"
+#include "ash/components/phonehub/screen_lock_manager.h"
 #include "ash/components/phonehub/url_constants.h"
 #include "ash/constants/ash_features.h"
 #include "base/feature_list.h"
@@ -355,8 +357,6 @@ void MultiDeviceSection::AddLoadTimeData(
        IDS_SETTINGS_MULTIDEVICE_NOTIFICATION_ACCESS_SETUP_DIALOG_CONNECTING_TITLE},
       {"multideviceNotificationAccessSetupScreenLockTitle",
        IDS_SETTINGS_MULTIDEVICE_NOTIFICATION_ACCESS_SETUP_DIALOG_SCREEN_LOCK_TITLE},
-      {"multideviceNotificationAccessSetupScreenLockSubtitle",
-       IDS_SETTINGS_MULTIDEVICE_NOTIFICATION_ACCESS_SETUP_DIALOG_SCREEN_LOCK_SUBTITLE},
       {"multideviceNotificationAccessSetupScreenLockInstruction",
        IDS_SETTINGS_MULTIDEVICE_NOTIFICATION_ACCESS_SETUP_DIALOG_SCREEN_LOCK_INSTRUCTION},
       {"multideviceNotificationAccessSetupAwaitingResponseTitle",
@@ -412,6 +412,10 @@ void MultiDeviceSection::AddLoadTimeData(
       "multideviceNotificationAccessSetupCompletedSummary",
       ui::SubstituteChromeOSDeviceType(
           IDS_SETTINGS_MULTIDEVICE_NOTIFICATION_ACCESS_SETUP_DIALOG_COMPLETED_SUMMARY));
+  html_source->AddString(
+      "multideviceNotificationAccessSetupScreenLockSubtitle",
+      ui::SubstituteChromeOSDeviceType(
+          IDS_SETTINGS_MULTIDEVICE_NOTIFICATION_ACCESS_SETUP_DIALOG_SCREEN_LOCK_SUBTITLE));
   html_source->AddString(
       "multideviceForgetDeviceSummary",
       ui::SubstituteChromeOSDeviceType(
@@ -527,8 +531,12 @@ void MultiDeviceSection::AddLoadTimeData(
           base::FeatureList::IsEnabled(
               ::features::kNearbySharingBackgroundScanning));
   html_source->AddBoolean("isEcheAppEnabled", features::IsEcheSWAEnabled());
-  // TODO(crbug.com/1256644): Query the real value from pref.
-  html_source->AddBoolean("isPhoneScreenLockEnabled", false);
+  bool is_phone_screen_lock_enabled =
+      static_cast<phonehub::ScreenLockManager::LockStatus>(
+          pref_service_->GetInteger(phonehub::prefs::kScreenLockStatus)) ==
+      phonehub::ScreenLockManager::LockStatus::kLockedOn;
+  html_source->AddBoolean("isPhoneScreenLockEnabled",
+                          is_phone_screen_lock_enabled);
   const bool is_screen_lock_enabled =
       SessionControllerClientImpl::CanLockScreen() &&
       SessionControllerClientImpl::ShouldLockScreenAutomatically();
