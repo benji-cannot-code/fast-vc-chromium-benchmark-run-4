827e20b932f3957caebcdd984c9ff509c7e68b72
Sunggook Chue <sunggch@microsoft.com>
2021-11-22 21:29:04 +0000

    Adding waiting and error reporting in UpdateOsHooks
    PWA update currently doesn't wait on OS integration. So while it
    keeps the profile alive for the database modification, it can
    shut down the browser before the OS integration completes,
    resulting in os hook corruption.
    
    The change adds UpdateOsHooksCallback like InstallOsHooksCallback.
    UpdateOsHooks now calls the callback after waiting for all
    respective os hook operations to complete. While most of these
    will always report kOk, URL handlers will correctly report if it
    failed.
    
    Bug: 1247434
    Change-Id: I87a32706bc6e5669db5d2466ddbc308f470332d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288036
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944247}

--- chrome/browser/web_applications/web_app_constants.cc
+++ chrome/browser/web_applications/web_app_constants.cc
@@ -128,6 +128,8 @@ std::ostream& operator<<(std::ostream& os, InstallResultCode code) {
       return os << "kSuccessOfflineOnlyInstall";
     case InstallResultCode::kSuccessOfflineFallbackInstall:
       return os << "kSuccessOfflineFallbackInstall";
+    case InstallResultCode::kUpdateTaskFailed:
+      return os << "kUpdateTaskFailed";
   }
 }
 
