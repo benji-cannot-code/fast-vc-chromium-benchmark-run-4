d9aa58ac8395066666ae5490c676414f2da9b729
Nancy Wang <nancylingwang@chromium.org>
2021-11-22 04:35:31 +0000

    Fix the icon loading issue for disabled extensions.
    OnExtensionLoaded is not called for disabled extensions/Chrome apps. So
    Chrome app publisher can't load all Chrome apps, including the disabled
    extensions, during the system startup phase.
    
    Add a post function OnExtensionsReady, and when extensions are ready,
    call Chrome app publisher to load all Chrome apps/extensions during the
    system startup phase.
    
    BUG=1271332
    
    Change-Id: I636d15c1a0745cb5ee8e6ced4d872d566bf51dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288822
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#943950}

--- chrome/browser/apps/app_service/publishers/extension_apps_base.cc
+++ chrome/browser/apps/app_service/publishers/extension_apps_base.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_types.h"
 #include "base/callback.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/one_shot_event.h"
 #include "base/scoped_observation.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_factory.h"
@@ -339,10 +340,23 @@ ExtensionAppsBase::CallbackWrapper::~CallbackWrapper() {
 void ExtensionAppsBase::Initialize() {
   RegisterPublisher(AppType::kExtension);
 
+  prefs_observation_.Observe(extensions::ExtensionPrefs::Get(profile_));
+  registry_observation_.Observe(extensions::ExtensionRegistry::Get(profile_));
+
   DCHECK(profile_);
   PublisherBase::Initialize(proxy()->AppService(),
                             apps::mojom::AppType::kExtension);
 
+  // Publish apps after all extensions have been loaded, to include all apps
+  // including the disabled apps.
+  extensions::ExtensionSystem::Get(profile_)->ready().Post(
+      FROM_HERE, base::BindOnce(&ExtensionAppsBase::OnExtensionsReady,
+                                weak_factory_.GetWeakPtr()));
+
+  app_service_ = proxy()->AppService().get();
+}
+
+void ExtensionAppsBase::OnExtensionsReady() {
   std::vector<std::unique_ptr<App>> apps;
   extensions::ExtensionRegistry* registry =
       extensions::ExtensionRegistry::Get(profile_);
@@ -358,10 +372,6 @@ void ExtensionAppsBase::Initialize() {
   //
   // If making changes to which sets are consulted, also change ShouldShow,
   // OnHideWebStoreIconPrefChanged.
-
-  prefs_observation_.Observe(extensions::ExtensionPrefs::Get(profile_));
-  registry_observation_.Observe(extensions::ExtensionRegistry::Get(profile_));
-  app_service_ = proxy()->AppService().get();
 }
 
 void ExtensionAppsBase::LoadIcon(const std::string& app_id,
