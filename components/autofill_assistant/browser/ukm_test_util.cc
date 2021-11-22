7da26c433635ddee5a06b516b2e67a0079e04149
Luca Hunkeler <hluca@google.com>
2021-11-22 17:05:22 +0000

    [Autofill Assistant] Log contact fields metric in UKM
    Adds a metric that logs which fields are present in the contact selected
    by default by AutofillAssistant.
    The same metric for shipping and credit cards will be added in a follow-up
    
    Bug: b/204949438
    Change-Id: Ifbf0162d543dd7452d855e2113630315ca902ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288689
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#944115}

--- components/autofill_assistant/browser/ukm_test_util.cc
+++ components/autofill_assistant/browser/ukm_test_util.cc
@@ -56,6 +56,11 @@ GetUkmIncompleteContactProfilesCount(
   return ukm_recorder.GetEntries(kAutofillAssistantCollectContact,
                                  {kIncompleteContactProfilesCount});
 }
+std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
+GetUkmInitialContactFieldsStatus(ukm::TestAutoSetUkmRecorder& ukm_recorder) {
+  return ukm_recorder.GetEntries(kAutofillAssistantCollectContact,
+                                 {kInitialContactFieldsStatus});
+}
 std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry> GetUkmContactModified(
     ukm::TestAutoSetUkmRecorder& ukm_recorder) {
   return ukm_recorder.GetEntries(kAutofillAssistantCollectContact,
@@ -94,12 +99,12 @@ std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry> GetUkmShippingModified(
   return ukm_recorder.GetEntries(kAutofillAssistantCollectShippingAddress,
                                  {kShippingModified});
 }
+
 std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
 GetUkmCollectUserDataResult(ukm::TestAutoSetUkmRecorder& ukm_recorder) {
   return ukm_recorder.GetEntries(kAutofillAssistantCollectUserDataResult,
                                  {kResult});
 }
-
 std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry> GetUkmTimeTakenMs(
     ukm::TestAutoSetUkmRecorder& ukm_recorder) {
   return ukm_recorder.GetEntries(kAutofillAssistantCollectUserDataResult,
--- components/autofill_assistant/browser/ukm_test_util.h
+++ components/autofill_assistant/browser/ukm_test_util.h
@@ -47,6 +47,7 @@ const char kInChromeTriggerAction[] = "InChromeTriggerAction";
 const char kTriggerConditionTimingMs[] = "TriggerConditionEvaluationMs";
 const char kCompleteContactProfilesCount[] = "CompleteContactProfilesCount";
 const char kContactModified[] = "ContactModified";
+const char kInitialContactFieldsStatus[] = "InitialContactFieldsStatus";
 const char kIncompleteContactProfilesCount[] = "IncompleteContactProfilesCount";
 const char kCompleteCreditCardsCount[] = "CompleteCreditCardsCount";
 const char kIncompleteCreditCardsCount[] = "IncompleteCreditCardsCount";
@@ -75,6 +76,8 @@ std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
 GetUkmCompleteContactProfilesCount(ukm::TestAutoSetUkmRecorder& ukm_recorder);
 std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
 GetUkmIncompleteContactProfilesCount(ukm::TestAutoSetUkmRecorder& ukm_recorder);
+std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
+GetUkmInitialContactFieldsStatus(ukm::TestAutoSetUkmRecorder& ukm_recorder);
 std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry> GetUkmContactModified(
     ukm::TestAutoSetUkmRecorder& ukm_recorder);
 std::vector<ukm::TestUkmRecorder::HumanReadableUkmEntry>
