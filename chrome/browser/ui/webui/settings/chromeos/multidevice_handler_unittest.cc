59fdc4e858d3bc408b695165e3a920cc7ff0e726
Carlos Caballero <carlscab@google.com>
2021-11-23 17:25:09 +0000

    [CodeHealth] Remove DictionaryValue::GetBoolean()
    https://docs.google.com/document/d/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ
    
    This CL was uploaded by git cl split.
    
    R=hansberry@chromium.org
    
    Bug: 1187033
    Change-Id: I4c407583bdc5fb937ca7733590dc324813150a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298014
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944556}

--- chrome/browser/ui/webui/settings/chromeos/multidevice_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/chromeos/multidevice_handler_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_web_ui.h"
+#include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
@@ -34,6 +35,8 @@ namespace chromeos {
 namespace settings {
 
 namespace {
+
+using ::testing::Optional;
 
 class TestMultideviceHandler : public MultideviceHandler {
  public:
@@ -174,25 +177,15 @@ void VerifyPageContentDict(
         page_content_dict->GetString("hostDeviceName", &host_device_name));
   }
 
-  bool is_nearby_share_disallowed_by_policy;
-  EXPECT_TRUE(
-      page_content_dict->GetBoolean("isNearbyShareDisallowedByPolicy",
-                                    &is_nearby_share_disallowed_by_policy));
-  EXPECT_EQ(expected_is_nearby_share_disallowed_by_policy_,
-            is_nearby_share_disallowed_by_policy);
+  EXPECT_THAT(page_content_dict->FindBoolKey("isNearbyShareDisallowedByPolicy"),
+              Optional(expected_is_nearby_share_disallowed_by_policy_));
 
-  bool is_phone_hub_apps_access_granted;
-  EXPECT_TRUE(
-      page_content_dict->GetBoolean("isPhoneHubAppsAccessGranted",
-                                    &is_phone_hub_apps_access_granted));
-  EXPECT_EQ(expected_is_phone_hub_apps_access_granted_,
-            is_phone_hub_apps_access_granted);
+  EXPECT_THAT(page_content_dict->FindBoolKey("isPhoneHubAppsAccessGranted"),
+              Optional(expected_is_phone_hub_apps_access_granted_));
 
-  bool is_phone_hub_permissions_dialog_supported;
-  EXPECT_TRUE(page_content_dict->GetBoolean(
-      "isPhoneHubPermissionsDialogSupported",
-      &is_phone_hub_permissions_dialog_supported));
-  EXPECT_TRUE(is_phone_hub_permissions_dialog_supported);
+  EXPECT_THAT(
+      page_content_dict->FindBoolKey("isPhoneHubPermissionsDialogSupported"),
+      Optional(true));
 }
 
 }  // namespace
