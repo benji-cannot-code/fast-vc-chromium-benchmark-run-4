f2c23cbf12e722de6a0561e82f58740f4ec7cbc7
Xinghui Lu <xinghuilu@chromium.org>
2021-11-23 00:31:15 +0000

    Enable kRealTimeUrlLookupReferrerChainForEnterprise by default
    Bug: 1238092
    Change-Id: I4dcb925ccc816b15b31830b35c0aa04eb3cc607f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290497
    Auto-Submit: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Rohit Bhatia <bhatiarohit@google.com>
    Cr-Commit-Position: refs/heads/main@{#944318}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -115,7 +115,7 @@ const base::Feature kSafeBrowsingDisableConsumerCsdForEnterprise{
 
 const base::Feature kRealTimeUrlLookupReferrerChainForEnterprise{
     "SafeBrowsingRealTimeUrlLookupReferrerChainForEnterprise",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSafeBrowsingPageLoadToken{
     "SafeBrowsingPageLoadToken", base::FEATURE_DISABLED_BY_DEFAULT};
