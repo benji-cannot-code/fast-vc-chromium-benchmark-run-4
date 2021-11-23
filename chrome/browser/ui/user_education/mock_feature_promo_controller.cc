4e4285e98b36058e2ce9e628ba20bdf4a83080fe
Dana Fried <dfried@chromium.org>
2021-11-23 20:20:21 +0000

    UAF protection for promo launched from callback.
    Suspecting differing lifetime between tab helper and feature promo
    service in the attached bug (but can't be sure). Using a weak pointer at
    least eliminates this potential problem.
    
    Bug: 1271120
    Change-Id: I0697954660fdd73a4ab7a6469d228193e988e14d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297471
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944642}

--- chrome/browser/ui/user_education/mock_feature_promo_controller.cc
+++ chrome/browser/ui/user_education/mock_feature_promo_controller.cc
@@ -7,3 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 MockFeaturePromoController::MockFeaturePromoController() = default;
 MockFeaturePromoController::~MockFeaturePromoController() = default;
+
+base::WeakPtr<FeaturePromoController>
+MockFeaturePromoController::GetAsWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
+}
--- chrome/browser/ui/views/user_education/feature_promo_controller_views.cc
+++ chrome/browser/ui/views/user_education/feature_promo_controller_views.cc
@@ -300,6 +300,11 @@ FeaturePromoControllerViews::CloseBubbleAndContinuePromo(
   return PromoHandle(weak_ptr_factory_.GetWeakPtr());
 }
 
+base::WeakPtr<FeaturePromoController>
+FeaturePromoControllerViews::GetAsWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
+}
+
 // static
 void FeaturePromoControllerViews::BlockActiveWindowCheckForTesting() {
   active_window_check_blocked_for_testing = true;
