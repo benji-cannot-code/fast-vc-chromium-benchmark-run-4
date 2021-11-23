8e2c6dfa72240aab98adfb759aefde2d12663b10
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-11-23 19:28:14 +0000

    [send-tab-to-self desktop] Delete ShouldOfferFeatureForLink()
    Before this CL, ShouldOfferFeatureForLink() checked the scheme of the
    current URL besides that of the link to be shared. The reason is unknown
    and this is possibly a bug, since the current URL never makes it to the
    other client anyway. This CL deletes ShouldOfferFeatureForLink() and
    starts calling ShouldOfferFeatureForPage() with the link URL instead.
    The latter is renamed to ShouldOfferToSendUrl(), to better reflect its
    new use.
    
    This change is actually a no-op if the share::kShareMenu feature toggle
    is enabled, because that removes STTS for links, c.f. crbug.com/1273150.
    The true usefulness of this change is reducing the number of predicates
    of type "Is STTS allowed", so we can later replace them with a single
    method that tells in which of the 3 states the STTS bubble is:
    a) feature enabled, choose target device
    b) feature enabled but no target device
    c) feature disabled, offer sign-in promo
    
    We also use this opportunity for some code improvements:
    - Drop the IsIncognitoProfile() check in ShouldOfferFeatureForPage().
    This was redundant with the null check of SendTabToSelfSyncService.
    Worse, it didn't cover the Guest Profile case, where the service is
    also null. Now the method doesn't depend on profile and can be tested
    more easily.
    - Delete IsUserSyncTypeActive() and HasValidTargetDevice(), which
    were exposed in send_tab_to_self_util.h, but were never directly
    called in production code, only in tests.
    
    Bug: 1219433
    Change-Id: Iba5cfa4a2cf47942fd220e8660e7a57e97a0af14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295397
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944610}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sharing/shared_clipboard/shared_clipboard_context_menu_observer.h"
 #include "chrome/browser/sharing/shared_clipboard/shared_clipboard_utils.h"
 #include "chrome/browser/spellchecker/spellcheck_service.h"
+#include "chrome/browser/sync/send_tab_to_self_sync_service_factory.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
 #include "chrome/browser/translate/translate_service.h"
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
@@ -1408,9 +1409,11 @@ void RenderViewContextMenu::AppendLinkItems() {
     if (!ShouldUseShareMenu() && params_.has_image_contents)
       AppendQRCodeGeneratorItem(/*for_image=*/true, /*draw_icon=*/true);
 
-    if (browser && !ShouldUseShareMenu() &&
-        send_tab_to_self::ShouldOfferFeatureForLink(active_web_contents,
-                                                    params_.link_url)) {
+    const bool should_offer_to_share_url =
+        send_tab_to_self::ShouldOfferToShareUrl(
+            SendTabToSelfSyncServiceFactory::GetForProfile(profile),
+            params_.link_url);
+    if (browser && !ShouldUseShareMenu() && should_offer_to_share_url) {
       if (send_tab_to_self::GetValidDeviceCount(GetBrowser()->profile()) == 1) {
 #if defined(OS_MAC)
         menu_model_.AddItem(IDC_CONTENT_LINK_SEND_TAB_TO_SELF_SINGLE_TARGET,
@@ -2382,6 +2385,8 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
     case IDC_CONTENT_CONTEXT_LANGUAGE_SETTINGS:
     case IDC_SEND_TAB_TO_SELF:
     case IDC_SEND_TAB_TO_SELF_SINGLE_TARGET:
+    case IDC_CONTENT_LINK_SEND_TAB_TO_SELF:
+    case IDC_CONTENT_LINK_SEND_TAB_TO_SELF_SINGLE_TARGET:
       return true;
 
     case IDC_CONTENT_CONTEXT_LENS_REGION_SEARCH:
@@ -2395,11 +2400,6 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
 
     case IDC_CONTENT_CONTEXT_SHARING_SUBMENU:
       return true;
-
-    case IDC_CONTENT_LINK_SEND_TAB_TO_SELF:
-    case IDC_CONTENT_LINK_SEND_TAB_TO_SELF_SINGLE_TARGET:
-      return send_tab_to_self::AreContentRequirementsMet(
-          params_.link_url, GetBrowser()->profile());
 
     case IDC_CHECK_SPELLING_WHILE_TYPING:
       return prefs->GetBoolean(spellcheck::prefs::kSpellCheckEnable);
