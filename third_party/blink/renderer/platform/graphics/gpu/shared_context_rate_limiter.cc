2b6f5ddbe6ca5566577ca7d86f17552d4843ac5e
Justin Novosad <junov@chromium.org>
2021-11-23 23:14:13 +0000

    Fix rate limiter logic in Canvas2DLayerBridge
    The rate limiter was being reset whenever an overdraw optimization
    kicks in.  Such a reset only makes sense under the assumption that
    no recorded paint ops were flushed since the last compositor frame
    was submitted. That assumption may have been valid in the past, but
    it is not valid today since paint ops are flushed systematically in
    FinalizeFrame, which is called at the end of every script task that
    draw to the canvas (regardless of whether a compositor frame is
    being submitted).
    
    Bug: 1263368
    Change-Id: Iff9226d6d5c6fea5d4a450606ddc127e4e12447c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296565
    Auto-Submit: Justin Novosad <junov@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944722}

--- third_party/blink/renderer/platform/graphics/gpu/shared_context_rate_limiter.cc
+++ third_party/blink/renderer/platform/graphics/gpu/shared_context_rate_limiter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/ptr_util.h"
+#include "base/trace_event/trace_event.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/common/capabilities.h"
 #include "third_party/blink/public/platform/platform.h"
@@ -36,6 +37,7 @@ SharedContextRateLimiter::SharedContextRateLimiter(unsigned max_pending_ticks)
 }
 
 void SharedContextRateLimiter::Tick() {
+  TRACE_EVENT0("blink", "SharedContextRateLimiter::Tick");
   if (!context_provider_)
     return;
 
@@ -54,12 +56,15 @@ void SharedContextRateLimiter::Tick() {
   }
   if (queries_.size() > max_pending_ticks_) {
     if (can_use_sync_queries_) {
+      TRACE_EVENT0("blink",
+                   "GPU backpressure via GL_COMMANDS_COMPLETED_CHROMIUM");
       GLuint result;
       raster_interface->GetQueryObjectuivEXT(queries_.front(),
                                              GL_QUERY_RESULT_EXT, &result);
       raster_interface->DeleteQueriesEXT(1, &queries_.front());
       queries_.pop_front();
     } else {
+      TRACE_EVENT0("blink", "GPU backpressure via RasterInterface::Finish");
       raster_interface->Finish();
       Reset();
     }
