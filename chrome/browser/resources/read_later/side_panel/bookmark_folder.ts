71c5c115f1deaea625ab7b4af43882dd1068ccbc
John Lee <johntlee@chromium.org>
2021-11-23 06:52:12 +0000

    Side panel Bookmarks: Fix folders not keeping their open states
    There is a [restamp] property on the dom-repeat of bookmark folders,
    which causes the same elements to be used as bookmarks and folders
    get moved around during drag and drop. The |open_| property was only
    listening for changes on |openFolders|, and not |folder.id| which can
    actually change as the |folder| binding gets shifted with the re-use
    of the same elements for a new ordering.
    
    Bug: 1270913
    Change-Id: If1e13ca490ded698aea28e1d8fdcff4d0fefd493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297051
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944403}

--- chrome/browser/resources/read_later/side_panel/bookmark_folder.ts
+++ chrome/browser/resources/read_later/side_panel/bookmark_folder.ts
@@ -59,12 +59,10 @@ export class BookmarkFolderElement extends PolymerElement {
       open_: {
         type: Boolean,
         value: false,
+        computed: 'computeIsOpen_(openFolders, folder.id)'
       },
 
-      openFolders: {
-        type: Array,
-        observer: 'onOpenFoldersChanged_',
-      },
+      openFolders: Array,
     };
   }
 
@@ -161,13 +159,12 @@ export class BookmarkFolderElement extends PolymerElement {
       return;
     }
 
-    this.open_ = !this.open_;
     this.dispatchEvent(new CustomEvent(FOLDER_OPEN_CHANGED_EVENT, {
       bubbles: true,
       composed: true,
       detail: {
         id: this.folder.id,
-        open: this.open_,
+        open: !this.open_,
       }
     }));
 
@@ -176,9 +173,9 @@ export class BookmarkFolderElement extends PolymerElement {
                      'SidePanel.Bookmarks.FolderClose');
   }
 
-  private onOpenFoldersChanged_() {
-    this.open_ =
-        Boolean(this.openFolders) && this.openFolders.includes(this.folder.id);
+  private computeIsOpen_() {
+    return Boolean(this.openFolders) &&
+        this.openFolders.includes(this.folder.id);
   }
 
   private getFocusableRows_(): HTMLElement[] {
