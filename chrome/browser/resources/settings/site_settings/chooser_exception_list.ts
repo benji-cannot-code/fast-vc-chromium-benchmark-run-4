f47466d699c058772495772fe882b58c1976aa23
dpapad <dpapad@chromium.org>
2021-11-23 13:52:00 +0000

    Settings: Migrating site_settings/ tests to TypeScript, part5.
     - chooser_exception_list_entry_tests.ts
     - chooser_exception_list_tests.ts
     - settings_category_default_radio_group_tests.ts
    
    Bug: 1234307
    Change-Id: I0861fd6455589cf098d25381745c85f4086c0564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293760
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#944475}

--- chrome/browser/resources/settings/site_settings/chooser_exception_list.ts
+++ chrome/browser/resources/settings/site_settings/chooser_exception_list.ts
@@ -26,7 +26,7 @@ import {ChooserType, ContentSettingsTypes} from './constants.js';
 import {SiteSettingsMixin} from './site_settings_mixin.js';
 import {ChooserException, RawChooserException, SiteException} from './site_settings_prefs_browser_proxy.js';
 
-interface ChooserExceptionListElement {
+export interface ChooserExceptionListElement {
   $: {
     tooltip: PaperTooltipElement,
   };
@@ -35,7 +35,8 @@ interface ChooserExceptionListElement {
 const ChooserExceptionListElementBase = ListPropertyUpdateMixin(
     SiteSettingsMixin(WebUIListenerMixin(I18nMixin(PolymerElement))));
 
-class ChooserExceptionListElement extends ChooserExceptionListElementBase {
+export class ChooserExceptionListElement extends
+    ChooserExceptionListElementBase {
   static get is() {
     return 'chooser-exception-list';
   }
@@ -214,6 +215,12 @@ class ChooserExceptionListElement extends ChooserExceptionListElementBase {
         this.updateList(propertyPath, siteUidGetter, exception.sites);
       }, this);
     }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'chooser-exception-list': ChooserExceptionListElement;
   }
 }
 
--- chrome/browser/resources/settings/site_settings/chooser_exception_list_entry.ts
+++ chrome/browser/resources/settings/site_settings/chooser_exception_list_entry.ts
@@ -19,7 +19,13 @@ import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/poly
 
 import {ChooserException} from './site_settings_prefs_browser_proxy.js';
 
-class ChooserExceptionListEntryElement extends PolymerElement {
+export interface ChooserExceptionListEntryElement {
+  $: {
+    listContainer: HTMLElement,
+  };
+}
+
+export class ChooserExceptionListEntryElement extends PolymerElement {
   static get is() {
     return 'chooser-exception-list-entry';
   }
@@ -41,6 +47,12 @@ class ChooserExceptionListEntryElement extends PolymerElement {
 
   exception: ChooserException;
   private lastFocused_: HTMLElement|null;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'chooser-exception-list-entry': ChooserExceptionListEntryElement;
+  }
 }
 
 customElements.define(
