7ba1135b7ec45b1b7cbd61f1d9f3799ed851896a
Andrew Grieve <agrieve@chromium.org>
2021-11-23 17:15:59 +0000

    SuperSize: Move archive --relocations to a console command
    supersize archive -f Foo.apk foo.size
    supersize console foo.size
    > s = ReplanceWithRelocations(size_info)
    > SaveSizeInfo(size_info=s, to_file="relocs.size")
    
    Moving to a post-processing model simplifies the already very
    complicated archive.py logic.
    
    Bug: 1193507
    Change-Id: I2e7e348e2b5fc4b98cadceea6cae465021be9c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291630
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944554}

--- tools/binary_size/libsupersize/integration_test.py
+++ tools/binary_size/libsupersize/integration_test.py
@@ -263,7 +263,6 @@ class IntegrationTest(unittest.TestCase):
       container_list = []
       raw_symbols_list = []
       build_config = {}
-      opts = archive.ContainerArchiveOptions()
 
       # Override for testing. Lower the bar for compacting symbols, to allow
       # smaller test cases to be created.
@@ -280,7 +279,6 @@ class IntegrationTest(unittest.TestCase):
                                             output_directory=output_directory)
           container, raw_symbols = archive.CreateContainerAndSymbols(
               knobs=knobs,
-              opts=opts,
               container_name=container_name,
               metadata=metadata,
               apk_spec=apk_spec,
