34152c28a7ff3cfe1a770dc9c97a5e938cb93af8
Peter Boström <pbos@chromium.org>
2021-11-23 21:01:40 +0000

    Add basic versions of UnifiedSidePanel classes
    This adds SidePanelCoordinator, SidePanelRegistry and SidePanelEntry and
    instantiates them under the UnifiedSidePanel experiment. This setup
    likely breaks both side search and Lens, but the reading list and
    bookmarks work as part of the existing setup.
    
    Bug: 1270923
    Change-Id: Ib7c475a35f336667ac511b8357331d96cb181538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299074
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944664}

---
+++ chrome/browser/ui/views/side_panel/side_panel_registry.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_REGISTRY_H_
+#define CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_REGISTRY_H_
+
+#include <memory>
+#include <vector>
+
+class SidePanelEntry;
+
+class SidePanelRegistry final {
+ public:
+  SidePanelRegistry();
+  SidePanelRegistry(const SidePanelRegistry&) = delete;
+  SidePanelRegistry& operator=(const SidePanelRegistry&) = delete;
+  ~SidePanelRegistry();
+
+  void Register(std::unique_ptr<SidePanelEntry> entry);
+
+  std::vector<std::unique_ptr<SidePanelEntry>>& entries() { return entries_; }
+
+ private:
+  std::vector<std::unique_ptr<SidePanelEntry>> entries_;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_SIDE_PANEL_REGISTRY_H_
