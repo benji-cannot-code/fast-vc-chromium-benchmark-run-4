63a3bd77a8269149ef404bdd43d7d48fd430932a
Robert Sesek <rsesek@chromium.org>
2021-11-23 22:22:19 +0000

    android: Check all scheduling features for restricting sched_*affinity
    Also return EPERM from the calls rather than using baseline policy's
    "watched" disposition and crashing the process.
    
    Bug: 1271302, 1272481
    Change-Id: Id445544e2a67b373ee5c7239dc26d8b077cd7440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295904
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944698}

--- content/renderer/renderer_main_platform_delegate_android.cc
+++ content/renderer/renderer_main_platform_delegate_android.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/renderer_main_platform_delegate.h"
 
 #include "base/android/build_info.h"
+#include "base/cpu_affinity_posix.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
 #include "components/power_scheduler/power_scheduler_features.h"
@@ -40,7 +41,21 @@ bool RendererMainPlatformDelegate::EnableSandbox() {
 #if BUILDFLAG(USE_SECCOMP_BPF)
   bool allow_sched_affinity =
       base::FeatureList::IsEnabled(features::kBigLittleScheduling) ||
-      base::FeatureList::IsEnabled(power_scheduler::features::kPowerScheduler);
+      (base::HasBigCpuCores() &&
+       (base::FeatureList::IsEnabled(
+            power_scheduler::features::kPowerScheduler) ||
+        base::FeatureList::IsEnabled(
+            power_scheduler::features::kCpuAffinityRestrictToLittleCores) ||
+        base::FeatureList::IsEnabled(
+            power_scheduler::features::kPowerSchedulerThrottleIdle) ||
+        base::FeatureList::IsEnabled(
+            power_scheduler::features::
+                kPowerSchedulerThrottleIdleAndNopAnimation) ||
+        base::FeatureList::IsEnabled(
+            power_scheduler::features::
+                kWebViewCpuAffinityRestrictToLittleCores) ||
+        base::FeatureList::IsEnabled(
+            power_scheduler::features::kWebViewPowerSchedulerThrottleIdle)));
   starter.set_policy(
       std::make_unique<sandbox::BaselinePolicyAndroid>(allow_sched_affinity));
 #endif
