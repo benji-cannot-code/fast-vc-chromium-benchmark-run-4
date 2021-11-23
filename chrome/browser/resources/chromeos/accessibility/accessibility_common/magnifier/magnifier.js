590f35d80e2e271ad1c0b0d5b1db83114f7d1c59
Josiah K <josiahk@google.com>
2021-11-23 23:40:12 +0000

    [Magnifier] Move Magnifier on Automation API Selection events
    Previously, Fullscreen and Docked Magnifier would not move to the
    currently selected Chrome menu item when navigating with arrow keys.
    Now, we listen for chrome.automation.EventType.SELECTION events, which
    are sent when arrowing through the Chrome menu.
    
    Fixed: 1216044
    Test: browser_tests --gtest_filter=MagnifierE2ETest.MovesFullscreenMagnifierSelectionEvent - Also, manually tested: Enabled Docked Magnifier, open Chrome Menu, use arrow keys to move up and down, observed magnifier follows selected menu items, and did same with Fullscreen magnifier.
    Change-Id: I7bf97bb95530699dc94d61b0afff672001222581
    AX-Relnotes: N/A.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298788
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944738}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier.js
@@ -37,12 +37,18 @@ export class Magnifier {
 
     /** @private {!EventHandler} */
     this.focusHandler_ = new EventHandler(
-        [], chrome.automation.EventType.FOCUS, event => this.onFocus_(event));
+        [], chrome.automation.EventType.FOCUS,
+        event => this.onFocusOrSelectionChanged_(event));
 
     /** @private {!EventHandler} */
     this.activeDescendantHandler_ = new EventHandler(
         [], chrome.automation.EventType.ACTIVE_DESCENDANT_CHANGED,
         event => this.onActiveDescendantChanged_(event));
+
+    /** @private {!EventHandler} */
+    this.selectionHandler_ = new EventHandler(
+        [], chrome.automation.EventType.SELECTION,
+        event => this.onFocusOrSelectionChanged_(event));
 
     /** @private {!EventHandler} */
     this.onCaretBoundsChangedHandler = new EventHandler(
@@ -66,6 +72,7 @@ export class Magnifier {
   onMagnifierDisabled() {
     this.focusHandler_.stop();
     this.activeDescendantHandler_.stop();
+    this.selectionHandler_.stop();
     this.onCaretBoundsChangedHandler.stop();
     this.onMagnifierBoundsChangedHandler_.stop();
     this.updateFromPrefsHandler_.stop();
@@ -84,6 +91,8 @@ export class Magnifier {
       this.focusHandler_.start();
       this.activeDescendantHandler_.setNodes(desktop);
       this.activeDescendantHandler_.start();
+      this.selectionHandler_.setNodes(desktop);
+      this.selectionHandler_.start();
       this.onCaretBoundsChangedHandler.setNodes(desktop);
       this.onCaretBoundsChangedHandler.start();
     });
@@ -169,7 +178,7 @@ export class Magnifier {
    * @param {!chrome.automation.AutomationEvent} event
    * @private
    */
-  onFocus_(event) {
+  onFocusOrSelectionChanged_(event) {
     const {location} = event.target;
     if (!location || !this.shouldFollowFocus()) {
       return;
