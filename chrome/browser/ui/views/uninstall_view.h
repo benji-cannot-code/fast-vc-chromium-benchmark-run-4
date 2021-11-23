ebc20caf040f318e44ae59446118b33682d514b4
Allen Bauer <kylixrd@chromium.org>
2021-11-23 01:55:12 +0000

    Remove use of GridLayout on UninstallView.
    Bug: 1261301
    Change-Id: I86369dc642eac7260c183130cdfe5d6e17a8fdc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296341
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944348}

--- chrome/browser/ui/views/uninstall_view.h
+++ chrome/browser/ui/views/uninstall_view.h
@@ -12,12 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/models/combobox_model.h"
+#include "ui/views/metadata/view_factory.h"
 #include "ui/views/window/dialog_delegate.h"
 
 namespace views {
 class Checkbox;
 class Combobox;
-class Label;
 }
 
 // UninstallView implements the dialog that confirms Chrome uninstallation
@@ -46,13 +46,17 @@ class UninstallView : public views::DialogDelegateView,
   void OnDialogAccepted();
   void OnDialogCancelled();
 
-  views::Label* confirm_label_;
-  views::Checkbox* delete_profile_;
-  views::Checkbox* change_default_browser_;
-  views::Combobox* browsers_combo_;
+  views::Checkbox* delete_profile_ = nullptr;
+  views::Checkbox* change_default_browser_ = nullptr;
+  views::Combobox* browsers_combo_ = nullptr;
   std::unique_ptr<BrowsersMap> browsers_;
   int& user_selection_;
   base::RepeatingClosure quit_closure_;
 };
+
+BEGIN_VIEW_BUILDER(, UninstallView, views::DialogDelegateView)
+END_VIEW_BUILDER
+
+DEFINE_VIEW_BUILDER(, UninstallView)
 
 #endif  // CHROME_BROWSER_UI_VIEWS_UNINSTALL_VIEW_H_
