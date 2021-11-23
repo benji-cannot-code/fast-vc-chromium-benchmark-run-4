1615cd1ab1235f019b6a3a0a3e72fcbf593a554d
Mike Dougherty <michaeldo@chromium.org>
2021-11-23 15:43:50 +0000

    Remove empty all_frames_web_bundle JavaScript bundle.
    This bundle no longer injects any JavaScript and can be removed.
    
    Change-Id: Ia6b01953280fc0136f9ab245436c9e3e83019630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296671
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944511}

--- ios/web/js_features/context_menu/resources/main_frame_context_menu.js
+++ ios/web/js_features/context_menu/resources/main_frame_context_menu.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 goog.provide('__crWeb.mainFrameContextMenu');
 
-// Requires __crWeb.allFramesContextMenu provided by __crWeb.allFramesWebBundle.
+// Requires __crWeb.allFramesContextMenu
 
 /** Beginning of anonymous object */
 (function() {
--- ios/web/js_messaging/resources/setup_frame.js
+++ ios/web/js_messaging/resources/setup_frame.js
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 goog.provide('__crWeb.setupFrame');
 
-// Requires __crWeb.message provided by __crWeb.allFramesWebBundle.
+// Requires __crWeb.message
 
 /* Beginning of anonymous object. */
 (function() {
