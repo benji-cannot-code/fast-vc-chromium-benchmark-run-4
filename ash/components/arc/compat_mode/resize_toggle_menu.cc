a4fd8093653ae726dafa00d121ee3375f811befe
Yao Li <yaohuali@google.com>
2021-11-23 07:11:51 +0000

    arc: [24/N] Move //{ => ash}/components/arc/vector_icons
    This CL moves:
    { => ash}/components/arc/vector_icons
    
    This is part of the LaCrOs directory migration
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE
    
    Bug: b:129295708
    Test: CI
    Change-Id: I1e9a99a379118a35111ca71e1a6b590fc1426e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293849
    Commit-Queue: Yao Li <yaohuali@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944406}

--- ash/components/arc/compat_mode/resize_toggle_menu.cc
+++ ash/components/arc/compat_mode/resize_toggle_menu.cc
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/arc/compat_mode/overlay_dialog.h"
 #include "ash/components/arc/compat_mode/style/arc_color_provider.h"
+#include "ash/components/arc/vector_icons/vector_icons.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "base/bind.h"
 #include "base/check.h"
 #include "base/notreached.h"
-#include "components/arc/vector_icons/vector_icons.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
