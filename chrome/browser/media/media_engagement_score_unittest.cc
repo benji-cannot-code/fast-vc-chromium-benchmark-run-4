a9d0e144b782e1fba009b6b3278ca7c3008aa3d1
Carlos Caballero <carlscab@google.com>
2021-11-23 18:45:12 +0000

    [CodeHealth] Remove DictionaryValue::GetBoolean()
    https://docs.google.com/document/d/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ
    
    This CL was uploaded by git cl split.
    
    R=tguilbert@chromium.org
    
    Bug: 1187033
    Change-Id: I2aeeaf6d45b35a047d5a6418dbe364f1f18c5250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297977
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944594}

--- chrome/browser/media/media_engagement_score_unittest.cc
+++ chrome/browser/media/media_engagement_score_unittest.cc
@@ -19,10 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/site_engagement/content/site_engagement_service.h"
 #include "media/base/media_switches.h"
+#include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
 namespace {
+
+using ::testing::Optional;
 
 base::Time GetReferenceTime() {
   base::Time::Exploded exploded_reference_time;
@@ -270,7 +273,6 @@ TEST_F(MediaEngagementScoreTest, ContentSettings) {
   int stored_visits;
   int stored_media_playbacks;
   absl::optional<double> stored_last_media_playback_time;
-  bool stored_has_high_score;
   std::unique_ptr<base::DictionaryValue> values =
       base::DictionaryValue::From(settings_map->GetWebsiteSetting(
           origin.GetURL(), GURL(), ContentSettingsType::MEDIA_ENGAGEMENT,
@@ -280,8 +282,9 @@ TEST_F(MediaEngagementScoreTest, ContentSettings) {
                      &stored_media_playbacks);
   stored_last_media_playback_time =
       values->FindDoubleKey(MediaEngagementScore::kLastMediaPlaybackTimeKey);
-  values->GetBoolean(MediaEngagementScore::kHasHighScoreKey,
-                     &stored_has_high_score);
+
+  EXPECT_THAT(values->FindBoolPath(MediaEngagementScore::kHasHighScoreKey),
+              Optional(true));
   EXPECT_EQ(stored_visits, example_num_visits + 1);
   EXPECT_EQ(stored_media_playbacks, example_media_playbacks + 2);
   EXPECT_EQ(*stored_last_media_playback_time,
@@ -385,10 +388,8 @@ TEST_F(MediaEngagementScoreTest, HighScoreUpdated) {
             origin.GetURL(), GURL(), ContentSettingsType::MEDIA_ENGAGEMENT,
             nullptr));
 
-    bool stored_high_score = false;
-    dict->GetBoolean(MediaEngagementScore::kHasHighScoreKey,
-                     &stored_high_score);
-    EXPECT_FALSE(stored_high_score);
+    EXPECT_THAT(dict->FindBoolPath(MediaEngagementScore::kHasHighScoreKey),
+                Optional(false));
   }
 }
 
