69fcb69870d6af48676a1740f7234ea45fe359a8
Yi Chou <yich@google.com>
2021-11-23 22:10:09 +0000

    Redefine the system token software fallback
    Only doing the software fallback on system token would cause chromium
    to crash when it's trying to initialize the user token. Redefine the
    system slot software fallback to NSS slots software fallback could
    resolve this issue.
    Because the hardware NSS slots would only be available when the TPM is
    owned, we should use the fallback when the TPM isn't owned.
    
    BUG=b:204320629
    TEST=Build with nss_slots_software_fallback = true
    TEST=disallow the TPM on the device, and check chrome://flags
    
    Change-Id: Iac7ad4bdeb5905de3f1022943a652636983bff9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3173937
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>
    Cr-Commit-Position: refs/heads/main@{#944692}

--- chromeos/tpm/tpm_token_loader.cc
+++ chromeos/tpm/tpm_token_loader.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/task_runner_util.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "chromeos/tpm/buildflags.h"
 #include "chromeos/tpm/tpm_token_info_getter.h"
 #include "crypto/nss_util.h"
 
@@ -28,15 +27,6 @@ void PostResultToTaskRunner(scoped_refptr<base::SequencedTaskRunner> runner,
                             base::OnceCallback<void(bool)> callback,
                             bool success) {
   runner->PostTask(FROM_HERE, base::BindOnce(std::move(callback), success));
-}
-
-// Checks if the build flag system_slot_software_fallback is enabled.
-bool IsSystemSlotSoftwareFallbackEnabled() {
-#if BUILDFLAG(SYSTEM_SLOT_SOFTWARE_FALLBACK)
-  return true;
-#else
-  return false;
-#endif
 }
 
 }  // namespace
@@ -82,9 +72,6 @@ TPMTokenLoader::TPMTokenLoader(bool initialized_for_test)
           base::ThreadTaskRunnerHandle::Get())),
       tpm_token_slot_id_(-1),
       can_start_before_login_(false) {
-  tpm_token_info_getter_->SetSystemSlotSoftwareFallback(
-      IsSystemSlotSoftwareFallbackEnabled());
-
   if (!initialized_for_test_ && LoginState::IsInitialized())
     LoginState::Get()->AddObserver(this);
 
