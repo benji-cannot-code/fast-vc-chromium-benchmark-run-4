649b4e3e84870c5b3fa4d0a7d84157c62aeec21c
Owen Min <zmin@chromium.org>
2021-11-23 16:48:29 +0000

    Revert "[Profile picker] Enable browser tests on lacros"
    This reverts commit da9da2332e97b9e4e76fd34deb6b83ed87dcc57e.
    
    Reason for revert: Revert together with 
    https://chromium-review.googlesource.com/c/chromium/src/+/3297930
    
    Original change's description:
    > [Profile picker] Enable browser tests on lacros
    >
    > This CL enabled the first batch of browser tests for profile picker and
    > for profile creation flow that make sense on lacros. Follow-up CLs will
    > enable more tests and add lacros-specific tests.
    >
    > This CL makes a change in the turn sync on helper (disabling all data
    > types) that is needed for the tests that choose not to sync to detect
    > that everything works as intended.
    >
    > Bug: 1248047
    > Change-Id: I6d618800f499502ce8be80e17a0177366865aeeb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3234519
    > Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    > Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#944518}
    
    Bug: 1248047
    Change-Id: Id6f8ebd863cdc631bbb7681ace2c904f04e85ff0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298174
    Owners-Override: Owen Min <zmin@chromium.org>
    Auto-Submit: Owen Min <zmin@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#944537}

--- chrome/browser/ui/webui/signin/dice_turn_sync_on_helper_unittest.cc
+++ chrome/browser/ui/webui/signin/dice_turn_sync_on_helper_unittest.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
-#include "components/sync/base/user_selectable_type.h"
 #include "components/sync/driver/mock_sync_service.h"
 #include "components/sync/driver/sync_user_settings_mock.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
@@ -345,40 +344,6 @@ class DiceTurnSyncOnHelperTest : public testing::Test {
             syncer::SyncService::DISABLE_REASON_ENTERPRISE_POLICY)));
   }
 
-  void SetExpectationsForSyncAborted() {
-// TODO(crbug.com/1263553): Get rid of the lacros special casing once sync
-// disabled is fully supported on lacros.
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-    EXPECT_CALL(*GetMockSyncService()->GetMockUserSettings(),
-                SetSelectedTypes(/*sync_everything=*/false,
-                                 /*types=*/syncer::UserSelectableTypeSet()));
-    EXPECT_CALL(*GetMockSyncService()->GetMockUserSettings(),
-                SetFirstSetupComplete(
-                    syncer::SyncFirstSetupCompleteSource::BASIC_FLOW));
-#else
-    EXPECT_CALL(
-        *GetMockSyncService()->GetMockUserSettings(),
-        SetFirstSetupComplete(syncer::SyncFirstSetupCompleteSource::BASIC_FLOW))
-        .Times(0);
-#endif
-  }
-
-  void CheckSyncAborted() {
-// TODO(crbug.com/1263553): Get rid of the lacros special casing once sync
-// disabled is fully supported on lacros.
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-    // Disabling all data types is asserted in
-    // `SetExpectationsForSyncAborted()`.
-    EXPECT_TRUE(
-        identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync));
-    EXPECT_TRUE(identity_manager()->HasAccountWithRefreshToken(account_id()));
-#else
-    EXPECT_FALSE(
-        identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync));
-    EXPECT_FALSE(identity_manager()->HasAccountWithRefreshToken(account_id()));
-#endif
-  }
-
   void CheckDelegateCalls() {
     EXPECT_EQ(expected_login_error_, login_error_);
     EXPECT_EQ(expected_merge_data_previous_email_, merge_data_previous_email_);
@@ -645,7 +610,9 @@ TEST_F(DiceTurnSyncOnHelperTest, SyncDisabledAbortRemoveAccount) {
       DiceTurnSyncOnHelper::SigninAbortedMode::REMOVE_ACCOUNT);
   base::RunLoop().RunUntilIdle();
   // Check expectations.
-  CheckSyncAborted();
+  EXPECT_FALSE(
+      identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync));
+  EXPECT_FALSE(identity_manager()->HasAccountWithRefreshToken(account_id()));
   CheckDelegateCalls();
 }
 
@@ -666,7 +633,9 @@ TEST_F(DiceTurnSyncOnHelperTest, SyncDisabledAbortKeepAccount) {
       DiceTurnSyncOnHelper::SigninAbortedMode::KEEP_ACCOUNT);
   base::RunLoop().RunUntilIdle();
   // Check expectations.
-  CheckSyncAborted();
+  EXPECT_FALSE(
+      identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync));
+  EXPECT_FALSE(identity_manager()->HasAccountWithRefreshToken(account_id()));
   CheckDelegateCalls();
 }
 
@@ -751,7 +720,9 @@ TEST_F(DiceTurnSyncOnHelperTest, CrossAccountContinue) {
   CreateDiceTurnOnSyncHelper(
       DiceTurnSyncOnHelper::SigninAbortedMode::REMOVE_ACCOUNT);
   // Check expectations.
-  CheckSyncAborted();
+  EXPECT_FALSE(
+      identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync));
+  EXPECT_FALSE(identity_manager()->HasAccountWithRefreshToken(account_id()));
   CheckDelegateCalls();
 }
 
@@ -879,7 +850,10 @@ TEST_F(DiceTurnSyncOnHelperTest, UndoSync) {
   // Set expectations.
   expected_sync_confirmation_shown_ = true;
   SetExpectationsForSyncStartupCompleted(profile());
-  SetExpectationsForSyncAborted();
+  EXPECT_CALL(
+      *GetMockSyncService()->GetMockUserSettings(),
+      SetFirstSetupComplete(syncer::SyncFirstSetupCompleteSource::BASIC_FLOW))
+      .Times(0);
 
   // Signin flow.
   EXPECT_FALSE(
@@ -887,7 +861,9 @@ TEST_F(DiceTurnSyncOnHelperTest, UndoSync) {
   CreateDiceTurnOnSyncHelper(
       DiceTurnSyncOnHelper::SigninAbortedMode::REMOVE_ACCOUNT);
   // Check expectations.
-  CheckSyncAborted();
+  EXPECT_FALSE(
+      identity_manager()->HasPrimaryAccount(signin::ConsentLevel::kSync));
+  EXPECT_FALSE(identity_manager()->HasAccountWithRefreshToken(account_id()));
   CheckDelegateCalls();
 }
 
--- components/account_manager_core/chromeos/account_manager_facade_factory_lacros.cc
+++ components/account_manager_core/chromeos/account_manager_facade_factory_lacros.cc
@@ -49,7 +49,7 @@ class AccountManagerFacadeFactoryLacros {
         std::move(account_manager_ui));
   }
 
-  account_manager::AccountManagerFacade* GetAccountManagerFacade() {
+  account_manager::AccountManagerFacadeImpl* GetAccountManagerFacade() {
     if (!account_manager_facade_) {
       InitializeAccountManagerFacade();
     }
@@ -62,11 +62,6 @@ class AccountManagerFacadeFactoryLacros {
 
   account_manager::AccountManagerUI* MaybeGetAshAccountManagerUIForTests() {
     return account_manager_ui_;
-  }
-
-  crosapi::AccountManagerMojoService*
-  MaybeGetAshAccountManagerMojoServiceForTests() {
-    return account_manager_mojo_service_.get();
   }
 
   // Reset the pointers.
@@ -105,7 +100,7 @@ class AccountManagerFacadeFactoryLacros {
             /*account_manager_for_tests=*/nullptr);
   }
 
-  std::unique_ptr<account_manager::AccountManagerFacade>
+  std::unique_ptr<account_manager::AccountManagerFacadeImpl>
       account_manager_facade_;
 
   // Set only in tests:
@@ -149,10 +144,4 @@ account_manager::AccountManager* MaybeGetAshAccountManagerForTests() {
 account_manager::AccountManagerUI* MaybeGetAshAccountManagerUIForTests() {
   return GetAccountManagerFacadeFactoryLacros()
       ->MaybeGetAshAccountManagerUIForTests();  // IN-TEST
-}
-
-crosapi::AccountManagerMojoService*
-MaybeGetAshAccountManagerMojoServiceForTests() {
-  return GetAccountManagerFacadeFactoryLacros()
-      ->MaybeGetAshAccountManagerMojoServiceForTests();  // IN-TEST
 }
