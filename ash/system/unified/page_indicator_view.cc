99101db412458ed15d5504e4b79a889a14378022
James Cook <jamescook@chromium.org>
2021-11-23 20:19:16 +0000

    ash: Move app list and shelf strings out of //ui into //ash
    These strings are for UI elements that are implemented in //ash, so
    move them into ash_strings.grd.
    
    Some of the UI elements used to live in //ui, but were moved to //ash
    long ago.
    
    I moved the screenshot hashes for existing screenshots, but I did not
    take any new screenshots. There are no new strings here, just existing
    ones, and translation console should reuse the existing translations.
    
    No-Presubmit: true
    Bug: none
    Change-Id: I3788f7ee3447c5799a39bed75e8cdba2abe1bbf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293978
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944640}

--- ash/system/unified/page_indicator_view.cc
+++ ash/system/unified/page_indicator_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/app_list/app_list_metrics.h"
 #include "ash/public/cpp/pagination/pagination_model.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/style/style_util.h"
 #include "ash/system/tray/tray_popup_utils.h"
@@ -26,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/skia_conversions.h"
-#include "ui/strings/grit/ui_strings.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/button.h"
