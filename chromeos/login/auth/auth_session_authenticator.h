a34ba7da57a9f99c0f392dfcbb652104891f9e1c
Denis Kuznetsov <antrim@chromium.org>
2021-11-23 20:19:36 +0000

    AuthSession-based login flow for kiosk users
    Bug: 1179246
    Change-Id: I241ffe2093c730aa48540aaae57ab97b11351627
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3043564
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944641}

--- chromeos/login/auth/auth_session_authenticator.h
+++ chromeos/login/auth/auth_session_authenticator.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/cryptohome/UserDataAuth.pb.h"
 #include "chromeos/login/auth/authenticator.h"
 #include "chromeos/login/auth/safe_mode_delegate.h"
+#include "components/account_id/account_id.h"
+#include "components/user_manager/user_type.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 class AuthFailure;
@@ -213,6 +215,10 @@ class COMPONENT_EXPORT(CHROMEOS_LOGIN_AUTH) AuthSessionAuthenticator
                         ContextCallback continuation,
                         std::unique_ptr<UserContext> context,
                         absl::optional<user_data_auth::UnmountReply> reply);
+
+  void LoginAsKioskImpl(const AccountId& app_account_id,
+                        user_manager::UserType user_type,
+                        bool force_dircrypto);
 
   void PrepareForNewAttempt(const std::string& method_id,
                             const std::string& long_desc);
