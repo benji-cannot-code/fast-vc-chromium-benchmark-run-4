5151160c24faf4c4b227deb1a3c0802418d3972a
Dale Curtis <dalecurtis@chromium.org>
2021-11-24 03:15:23 +0000

    Treat HLG as GAMMA22 when D3D11VideoDecoder operates in SDR mode.
    8bit HDR content is handled via the gfx::ColorTransform, but since
    we can't seem to bind P010 in SDR mode (DCHECK in GrSurfaceProxy),
    we still need the P010 -> BGRA copying path.
    
    On the copying path we should treat HLG as GAMMA22 content when
    the VideoProcessor lacks support for tone mapping to get reasonable
    results.
    
    R=liberato, tmathmeyer
    
    Bug: 1216691
    Change-Id: I2412b8099e6266af5f5f03740ea6fa96e60edebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299071
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944811}

--- media/gpu/windows/d3d11_video_processor_proxy.h
+++ media/gpu/windows/d3d11_video_processor_proxy.h
@@ -59,6 +59,8 @@ class MEDIA_GPU_EXPORT VideoProcessorProxy
                                     UINT stream_count,
                                     D3D11_VIDEO_PROCESSOR_STREAM* streams);
 
+  bool supports_tone_mapping() const { return supports_tone_mapping_; }
+
  protected:
   virtual ~VideoProcessorProxy();
   friend class base::RefCounted<VideoProcessorProxy>;
@@ -69,6 +71,7 @@ class MEDIA_GPU_EXPORT VideoProcessorProxy
   ComD3D11VideoProcessor video_processor_;
   ComD3D11DeviceContext device_context_;
   ComD3D11VideoContext video_context_;
+  bool supports_tone_mapping_ = false;
 };
 
 }  // namespace media
