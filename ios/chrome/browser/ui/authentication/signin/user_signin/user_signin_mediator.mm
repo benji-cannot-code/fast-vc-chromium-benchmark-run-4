b9b411721080139e11bb3ee1348f2609a8a84a51
Jérôme Lebel <jlebel@chromium.org>
2021-11-24 18:12:19 +0000

    [iOS] Remove wrong DCHECK() in UserSigninMediator
    When turn on sync is cancelled, it is possible to be signed out, if the
    primary identity has been removed while the dialog was opened.
    
    This patch removes:
      DCHECK(self.authenticationService->GetPrimaryIdentity(
             signin::ConsentLevel::kSignin));
    in:
      -[UserSigninMediator cancelAndDismissAuthenticationFlowAnimated:
                                                           completion:].
    
    
    Fixed: 1273500
    Change-Id: If1ffc71673b9e381c40d38424fc17e91aa110b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297925
    Auto-Submit: Jérôme <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945037}

--- ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_mediator.mm
@@ -152,8 +152,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     case IdentitySigninStateSignedInWithSyncDisabled: {
       DCHECK(!self.authenticationService->GetPrimaryIdentity(
           signin::ConsentLevel::kSync));
-      DCHECK(self.authenticationService->GetPrimaryIdentity(
-          signin::ConsentLevel::kSignin));
       if ([self.authenticationService->GetPrimaryIdentity(
               signin::ConsentLevel::kSignin)
               isEqual:self.delegate.signinIdentityOnStart]) {
