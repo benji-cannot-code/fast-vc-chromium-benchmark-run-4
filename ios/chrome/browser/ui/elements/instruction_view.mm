3d1b6793767e8d6cd972ad067e28417e393cca36
ginny huang <ginnyhuang@chromium.org>
2021-11-24 16:34:14 +0000

    [iOS] Negate instruction label trailing margin constant
    Before the fix, the label goes slightly beyond the frame of the
    instruction (16px to be exact). This is caused by the right margin of
    the label set incorrectly - when used with trailing anchor,the method
    
    constraintEqualToAnchor:(NSLayoutAnchor<AnchorType> *)anchor
                   constant:(CGFloat)c
    
    sets the anchor point to "anchor.trailingAnchor + c", instead of minus c
    This CL fixes the issue by negating the constant parameter.
    
    Fixed: 1273256
    Change-Id: I4098e0ee6f939d72efd1c7bc7739f95b6074f1c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299488
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944995}

--- ios/chrome/browser/ui/elements/instruction_view.mm
+++ ios/chrome/browser/ui/elements/instruction_view.mm
@@ -162,7 +162,7 @@ constexpr CGFloat kIconLabelWidth = 30;
     [instructionLabel.topAnchor constraintEqualToAnchor:line.topAnchor
                                                constant:kVerticalMargin],
     [instructionLabel.trailingAnchor constraintEqualToAnchor:line.trailingAnchor
-                                                    constant:kTrailingMargin]
+                                                    constant:-kTrailingMargin]
   ]];
 
   return line;
