f854e15a0f0cf8cd61ac91d9cb5d2b8c27d951ef
Max Ihlenfeldt <mihlenfeldt@igalia.com>
2021-11-24 12:37:37 +0000

    SoftwareOutputSurface: set resize_based_on_root_surface capability
    This CL adds a new SupportsOverridePlatformSize() method to
    SoftwareOutputDevice and SurfaceOzoneCanvas. Its return value is used by
    SoftwareOutputSurface to set its resize_based_on_root_surface
    capability. All default implementations return false, except for
    WaylandCanvasSurface's override, which returns true. This approach is
    taken from viz::OutputPresenterGL and gl::GlSurface.
    
    This change prevents RootCompositorFrameSinkImpl::Resize() from updating
    the window size without updating the scale factor on Ozone/Wayland. Else
    the following order of function calls can happen in viz::Display:
    Resize() -> DrawFrame() -> SetLocalSurfaceId(). The Resize() call leads
    to DrawFrame() using the new window size, but the scale factor is only
    updated in SetLocalSurfaceId(). The next DrawFrame() call notices the
    changed scale factor, but the lower layers ignore Resize() calls with
    only a changed scale factor. Thus the scale factor may not be set
    correctly, e.g. when opening a new window.
    
    Bug: 1272478
    Change-Id: I044f2c4c188011128f48f073aa225b919286ce45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295395
    Commit-Queue: Max Ihlenfeldt <mihlenfeldt@igalia.com>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944928}

--- components/viz/service/display/software_output_device.cc
+++ components/viz/service/display/software_output_device.cc
@@ -65,4 +65,8 @@ int SoftwareOutputDevice::MaxFramesPending() const {
   return 1;
 }
 
+bool SoftwareOutputDevice::SupportsOverridePlatformSize() const {
+  return false;
+}
+
 }  // namespace viz
--- components/viz/service/display_embedder/software_output_device_ozone.cc
+++ components/viz/service/display_embedder/software_output_device_ozone.cc
@@ -62,4 +62,8 @@ int SoftwareOutputDeviceOzone::MaxFramesPending() const {
   return surface_ozone_->MaxFramesPending();
 }
 
+bool SoftwareOutputDeviceOzone::SupportsOverridePlatformSize() const {
+  return surface_ozone_->SupportsOverridePlatformSize();
+}
+
 }  // namespace viz
