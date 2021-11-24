c8fac68d91a3d76b9c136cd82b3e21ad9b84c133
Scott Violet <sky@chromium.org>
2021-11-24 21:15:15 +0000

    weblayer: removes no longer needed metric
    Interesting enough the code actually used a different name.
    Further proof this metric isn't really useful.
    
    BUG=1235484
    TEST=none
    
    Change-Id: Ic876bcea70a01aaf03c9e30866c87e2deda043c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298753
    Auto-Submit: Scott Violet <sky@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945112}

--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -138,7 +138,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "ui/base/resource/resource_bundle_android.h"
-#include "weblayer/browser/android/metrics/weblayer_metrics_navigation_throttle.h"
 #include "weblayer/browser/android/metrics/weblayer_metrics_service_client.h"
 #include "weblayer/browser/android_descriptors.h"
 #include "weblayer/browser/bluetooth/weblayer_bluetooth_delegate_impl_client.h"
@@ -795,11 +794,6 @@ ContentBrowserClientImpl::CreateThrottlesForNavigation(
       if (auto_reload_throttle)
         throttles.push_back(std::move(auto_reload_throttle));
     }
-
-#if defined(OS_ANDROID)
-    throttles.push_back(
-        std::make_unique<WebLayerMetricsNavigationThrottle>(handle));
-#endif
 
     // MetricsNavigationThrottle requires that it runs before
     // NavigationThrottles that may delay or cancel navigations, so only
