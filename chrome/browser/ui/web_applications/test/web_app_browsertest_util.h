f01fe720625673d0e00abe8a32ac716d407cdb26
Alan Cutter <alancutter@chromium.org>
2021-11-24 07:29:42 +0000

    Web apps: Implement navigate_existing_client: never behaviour for launch_handler
    This CL implements the non-navigation launch behaviour for web apps
    that specify
        "launch_handler": {
          "route_to": "existing-client",
          "navigate_existing_client": "never"
        }
    in their manifest.
    https://github.com/WICG/sw-launch/blob/main/launch_handler.md
    
    This is a reland of:
    https://chromium-review.googlesource.com/c/chromium/src/+/3287183
    which was reverted in:
    https://chromium-review.googlesource.com/c/chromium/src/+/3298270
    
    What's changed from the original?
     - Removed the use of raw_ptr which was tripping up the CFI bot.
       https://chromium-review.googlesource.com/c/chromium/src/+/3299097/1..2
    
    Bug: 1250223
    Change-Id: I8d432f4531f5782094f7155e238ec435541cf7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299097
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944869}

--- chrome/browser/ui/web_applications/test/web_app_browsertest_util.h
+++ chrome/browser/ui/web_applications/test/web_app_browsertest_util.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEB_APPLICATIONS_TEST_WEB_APP_BROWSERTEST_UTIL_H_
 #define CHROME_BROWSER_UI_WEB_APPLICATIONS_TEST_WEB_APP_BROWSERTEST_UTIL_H_
 
+#include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_application_info.h"
 #include "url/gurl.h"
@@ -75,8 +76,6 @@ Browser* FindWebAppBrowser(Profile* profile, const AppId& app_id);
 
 void CloseAndWait(Browser* browser);
 
-void WaitForBrowserToBeClosed(Browser* browser);
-
 bool IsBrowserOpen(const Browser* test_browser);
 
 void UninstallWebApp(Profile* profile, const AppId& app_id);
@@ -85,6 +84,31 @@ using UninstallWebAppCallback = base::OnceCallback<void(bool uninstalled)>;
 void UninstallWebAppWithCallback(Profile* profile,
                                  const AppId& app_id,
                                  UninstallWebAppCallback callback);
+
+// Helper class that lets you await one Browser added and one Browser removed
+// event. Optionally filters to a specific Browser with |filter|. Useful for
+// closing the web app window that appears after installation from page.
+class BrowserWaiter : public BrowserListObserver {
+ public:
+  explicit BrowserWaiter(Browser* filter = nullptr);
+  ~BrowserWaiter() override;
+
+  Browser* AwaitAdded();
+  Browser* AwaitRemoved();
+
+  // BrowserListObserver:
+  void OnBrowserAdded(Browser* browser) override;
+  void OnBrowserRemoved(Browser* browser) override;
+
+ private:
+  Browser* const filter_ = nullptr;
+
+  base::RunLoop added_run_loop_;
+  Browser* added_browser_ = nullptr;
+
+  base::RunLoop removed_run_loop_;
+  Browser* removed_browser_ = nullptr;
+};
 
 }  // namespace web_app
 
