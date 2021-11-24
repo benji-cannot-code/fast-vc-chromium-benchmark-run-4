61a84df61afa05b5e02358298e7810da0625a859
Dmitry Gozman <dgozman@chromium.org>
2021-11-24 19:42:48 +0000

    [DevTools] Dispatch existing targets upon Target.setAutoAttach
    When second client called Target.setAutoAttach with the same
    parameters as the first one, we returned immediately, instead of
    auto-attaching to existing targets.
    
    Fixed by calling UpdateAutoAttach in all cases and ensuring that
    it does report existing targets.
    
    Change-Id: Idde9236864e014ce44cc817a430fb9da95d5f4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299428
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945073}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/target/target-setAutoAttach-oopif-multisession-existing.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startURL(
+      'https://devtools.oopif-a.test:8443/inspector-protocol/resources/coi-with-iframe.php',
+      `Tests that the attachedToTarget message is dispatched to all attached sessions`);
+
+  // This will force the iframe to navigate out-of-process.
+  await session.evaluate(
+    'document.getElementById(\'iframe\').src=\'https://devtools.oopif-b.test:8443/inspector-protocol/resources/corp-iframe.php\'');
+
+  await dp.Target.enable();
+  const session1Attached = dp.Target.onceAttachedToTarget().then(
+    () => testRunner.log('session 1 attached'));
+  await dp.Target.setAutoAttach(
+      {autoAttach: true, waitForDebuggerOnStart: false, flatten: true});
+  await session1Attached;
+
+  const dp2 = (await page.createSession()).protocol;
+  const session2Attached = dp2.Target.onceAttachedToTarget().then(
+    () => testRunner.log('session 2 attached'));
+  await dp2.Target.setAutoAttach(
+      {autoAttach: true, waitForDebuggerOnStart: false, flatten: true});
+  await session2Attached;
+
+  testRunner.completeTest();
+})
