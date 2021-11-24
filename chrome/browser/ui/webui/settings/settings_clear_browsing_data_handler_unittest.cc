3c66c04fa4f327d3e54a0764052ed33eae471685
Carlos Caballero <carlscab@google.com>
2021-11-24 12:38:55 +0000

    [CodeHealth] Remove DictionaryValue::GetBoolean()
    https://docs.google.com/document/d/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ
    
    This CL was uploaded by git cl split.
    
    R=sauski@google.com
    
    Bug: 1187033
    Change-Id: Ie7926b38c5dd87372d30b33cd45c6d40fb4615d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297984
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#944929}

--- chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/settings_clear_browsing_data_handler_unittest.cc
@@ -16,10 +16,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_web_ui.h"
+#include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace settings {
+
+using ::testing::Optional;
 
 class TestingClearBrowsingDataHandler
     : public settings::ClearBrowsingDataHandler {
@@ -110,9 +113,8 @@ void ClearBrowsingDataHandlerUnitTest::VerifySearchHistoryWebUIUpdate(
     if (!data.arg2()->GetAsDictionary(&dictionary)) {
       continue;
     }
-    bool actual_is_non_google_dse;
-    dictionary->GetBoolean("isNonGoogleDse", &actual_is_non_google_dse);
-    ASSERT_EQ(expected_is_non_google_dse, actual_is_non_google_dse);
+    ASSERT_THAT(dictionary->FindBoolKey("isNonGoogleDse"),
+                Optional(expected_is_non_google_dse));
     if (expected_is_non_google_dse) {
       std::u16string actual_non_google_search_history_string;
       dictionary->GetString("nonGoogleSearchHistoryString",
