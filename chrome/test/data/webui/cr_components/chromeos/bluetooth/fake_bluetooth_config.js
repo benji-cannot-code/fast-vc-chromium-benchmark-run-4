a20804f079a81b00b4a0b48c9c28ae9ec785d2d5
Gordon Seto <gordonseto@google.com>
2021-11-24 22:23:15 +0000

    [CrOS Bluetooth] Add device-specific pairing to pairing dialog.
    Update <bluetooth-pairing-ui> to support pairing to a specific device
    based on address. This is used by ARC++ apps, which supply the dialog
    with an address as an argument.
    
    To pair with a device based on address, BluetoothDeviceProperties is
    updated with an address property which exposes the BluetoothDevice's
    address.
    
    DeviceConversionUtil is updated to pipe the BluetoothDevice's address
    to BluetoothDeviceProperties.
    
    The UI has not been updated for the device-specific case and will
    be added in a follow-up CL.
    
    Bug: 1010321
    Change-Id: I888beec1636663ab01fe4ee368637aee07c43d82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299477
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#945151}

--- chrome/test/data/webui/cr_components/chromeos/bluetooth/fake_bluetooth_config.js
+++ chrome/test/data/webui/cr_components/chromeos/bluetooth/fake_bluetooth_config.js
@@ -35,6 +35,7 @@ export function createDefaultBluetoothDevice(
   return {
     deviceProperties: {
       id: id,
+      address: id,
       publicName: stringToMojoString16(publicName),
       deviceType: opt_deviceType,
       audioCapability: opt_audioCapability,
@@ -137,8 +138,8 @@ export class FakeBluetoothConfig {
    */
   startDiscovery(delegate) {
     this.lastDiscoveryDelegate_ = delegate;
-    this.notifyDelegatesPropertiesUpdated_();
     this.notifyDiscoveryStarted_();
+    this.notifyDelegatesPropertiesUpdated_();
   }
 
   /**
--- ui/webui/resources/cr_components/chromeos/bluetooth/bluetooth_spinner_page.js
+++ ui/webui/resources/cr_components/chromeos/bluetooth/bluetooth_spinner_page.js
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * UI element which shows a loading spinner.
  */
 
+import 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
+
 import './bluetooth_base_page.js';
 import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {ButtonBarState, ButtonState} from './bluetooth_types.js';
