e4d1d7e04cec30c7618e631722cd800fa536ae4e
Tim Sergeant <tsergeant@chromium.org>
2021-11-24 22:42:11 +0000

    Link capturing: Add new default link capturing behavior for ARC apps
    This CL changes the behavior (behind a flag) of ARC apps so that they do
    not capture links clicked in the browser by default. This works by
    ignoring certain link capturing updates from the ARC side, deliberately
    allowing the ARC-side preference and the Ash-side preference to diverge.
    Any changes made to the preference on either side will still take effect
    and will put the two settings back in sync.
    
    Change-Id: I7d525cfdad721c15747c3ff920f9564f11eec510
    Bug: 1269526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291111
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945159}

--- components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/intent_helper/intent_constants.h"
 #include "components/arc/intent_helper/open_url_delegate.h"
 #include "components/arc/mojom/intent_helper.mojom-forward.h"
+#include "components/arc/mojom/intent_helper.mojom-shared.h"
 #include "components/arc/mojom/intent_helper.mojom.h"
 #include "mojo/public/cpp/bindings/clone_traits.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -214,7 +215,8 @@ TEST_F(ArcIntentHelperTest, TestObserver) {
         void,
         OnArcSupportedLinksChanged,
         (const std::vector<arc::mojom::SupportedLinksPtr>& added_packages,
-         const std::vector<arc::mojom::SupportedLinksPtr>& removed_packages),
+         const std::vector<arc::mojom::SupportedLinksPtr>& removed_packages,
+         arc::mojom::SupportedLinkChangeSource source),
         (override));
   };
 
@@ -259,8 +261,10 @@ TEST_F(ArcIntentHelperTest, TestObserver) {
   {
     // Observer should be called when supported links change.
     EXPECT_CALL(observer, OnArcSupportedLinksChanged);
-    instance_->OnSupportedLinksChanged(/*added_packages=*/{},
-                                       /*removed_packages=*/{});
+    instance_->OnSupportedLinksChanged(
+        /*added_packages=*/{},
+        /*removed_packages=*/{},
+        arc::mojom::SupportedLinkChangeSource::kArcSystem);
     testing::Mock::VerifyAndClearExpectations(&observer);
   }
 
@@ -269,9 +273,11 @@ TEST_F(ArcIntentHelperTest, TestObserver) {
   instance_->OnDownloadAdded(/*relative_path=*/"Download/foo/bar.pdf",
                              /*owner_package_name=*/"owner_package_name");
   instance_->OnIntentFiltersUpdated(/*filters=*/{});
-  instance_->OnPreferredAppsChangedDeprecated(/*added=*/{}, /*removed=*/{});
-  instance_->OnSupportedLinksChanged(/*added_packages=*/{},
-                                     /*removed_packages=*/{});
+  instance_->OnPreferredAppsChangedDeprecated(/*added=*/{}, /*deleted=*/{});
+  instance_->OnSupportedLinksChanged(
+      /*added_packages=*/{},
+      /*removed_packages=*/{},
+      arc::mojom::SupportedLinkChangeSource::kArcSystem);
 }
 
 // Tests that ShouldChromeHandleUrl returns true by default.
