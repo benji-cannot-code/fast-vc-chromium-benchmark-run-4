723676d879a17ff4c16a01273a3a3a9b29d5917c
Renato Pereyra <renatopereyra@google.com>
2021-11-24 03:47:30 +0000

    borealis: Add support for Proton and SLR versions in Borealis game reports
    BUG=b:198197365
    TEST=Verify that the correct versions show up in a Proton game report.
    
    Change-Id: I6eb6bff616845f1ea1f999bb9abb2282a60abd26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291863
    Auto-Submit: Renato Pereyra <renatopereyra@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Cr-Commit-Position: refs/heads/main@{#944824}

--- chrome/browser/ash/borealis/borealis_util.h
+++ chrome/browser/ash/borealis/borealis_util.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
-namespace guest_os {
-class GuestOsRegistryService;
-}
-
 namespace borealis {
 
 // This is used by the Borealis app and the Borealis installer.
@@ -54,7 +50,7 @@ void CloseBorealisSplashScreenView();
 // Creates a URL for a feedback form with prefilled app/device info, or an
 // invalid URL if we don't want to collect feedback for the given |app_id|. Will
 // invoke |url_callback| when the url is ready.
-void FeedbackFormUrl(const guest_os::GuestOsRegistryService* registry_service,
+void FeedbackFormUrl(Profile* const profile,
                      const std::string& app_id,
                      const std::string& window_title,
                      base::OnceCallback<void(GURL)> url_callback);
