9d981076ad43fb7c332fb85114a1493d5d50dff8
Alan Screen <awscreen@chromium.org>
2021-11-24 18:33:33 +0000

    Break PrintJobWorker OOP logic into separate class
    Extract the increasing amount of special-case out-of-process logic for
    PrintJobWorker out into a separate class.  This is to improve
    readability and maintenance.
    
    Update PrinterQuery to instantiate this class when appropriate.
    
    Logical behavior is unmodified for this change.
    
    Bug: 809738
    Change-Id: Iba0c90304f38163ad5422eeb5fe3344564b82c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3223975
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945043}

--- chrome/browser/printing/print_job_worker.h
+++ chrome/browser/printing/print_job_worker.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PRINTING_PRINT_JOB_WORKER_H_
 
 #include <memory>
-#include <string>
 
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -15,14 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/build_config.h"
 #include "content/public/browser/browser_thread.h"
-#include "printing/buildflags/buildflags.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/page_number.h"
 #include "printing/printing_context.h"
-
-#if BUILDFLAG(ENABLE_OOP_PRINTING)
-#include "chrome/services/printing/public/mojom/print_backend_service.mojom-forward.h"
-#endif
 
 namespace content {
 class WebContents;
@@ -115,6 +109,13 @@ class PrintJobWorker {
   content::WebContents* GetWebContents();
 
  protected:
+  // Reports settings back to |callback|.
+  void GetSettingsDone(SettingsCallback callback, mojom::ResultCode result);
+
+  // Called on the UI thread to update the print settings.
+  virtual void UpdatePrintSettings(base::Value new_settings,
+                                   SettingsCallback callback);
+
   // Retrieves the context for testing only.
   PrintingContext* printing_context() { return printing_context_.get(); }
 
@@ -124,13 +125,6 @@ class PrintJobWorker {
   // notification from the right thread. All NOTIFY_PRINT_JOB_EVENT
   // notifications are sent this way.
   class NotificationTask;
-
-#if BUILDFLAG(ENABLE_OOP_PRINTING)
-  // Local callback wrapper for Print Backend Service mojom call.
-  void OnDidUpdatePrintSettings(const std::string& device_name,
-                                SettingsCallback callback,
-                                mojom::PrintSettingsResultPtr print_settings);
-#endif
 
   // Posts a task to call OnNewPage(). Used to wait for pages/document to be
   // available.
@@ -163,18 +157,12 @@ class PrintJobWorker {
                          bool is_scripted,
                          SettingsCallback callback);
 
-  // Called on the UI thread to update the print settings.
-  void UpdatePrintSettings(base::Value new_settings, SettingsCallback callback);
-
 #if defined(OS_CHROMEOS)
   // Called on the UI thread to update the print settings.
   void UpdatePrintSettingsFromPOD(
       std::unique_ptr<printing::PrintSettings> new_settings,
       SettingsCallback callback);
 #endif
-
-  // Reports settings back to |callback|.
-  void GetSettingsDone(SettingsCallback callback, mojom::ResultCode result);
 
   // Use the default settings. When using GTK+ or Mac, this can still end up
   // displaying a dialog. So this needs to happen from the UI thread on these
