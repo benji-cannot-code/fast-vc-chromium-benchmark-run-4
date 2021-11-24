fd07882095c1f6c6503fa2763bf676d5186c1000
Steffen Seckler <sseckler@google.com>
2021-11-24 13:15:36 +0000

    DLP: Refactoring RunLoop -> TestFuture.
    Refactors one test to use TestFuture instead of RunLoop.
    https://source.chromium.org/chromium/chromium/src/+/main:base/test/test_future.h
    
    Fixes: 1271564
    Change-Id: If1c22f74eb4fb01accaf78f9647e0d6f341841eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298110
    Commit-Queue: Steffen Seckler <sseckler@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944935}

--- chrome/browser/ash/policy/dlp/dlp_content_manager_browsertest.cc
+++ chrome/browser/ash/policy/dlp/dlp_content_manager_browsertest.cc
@@ -3,12 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <functional>
 #include "base/bind.h"
 #include "chrome/browser/ash/policy/dlp/dlp_content_manager.h"
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/ash/policy/dlp/dlp_content_manager_test_helper.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_histogram_helper.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_policy_event.pb.h"
@@ -636,8 +638,7 @@ IN_PROC_BROWSER_TEST_F(DlpContentManagerBrowserTest, ScreenShareReporting) {
   // Setup screen sharing (media stream)
   auto picker_factory = std::make_unique<FakeDesktopMediaPickerFactory>();
   DesktopCaptureAccessHandler access_handler{std::move(picker_factory)};
-  blink::mojom::MediaStreamRequestResult result;
-  blink::MediaStreamDevices devices;
+
   const std::string id =
       content::DesktopStreamsRegistry::GetInstance()->RegisterStream(
           web_contents->GetMainFrame()->GetProcess()->GetID(),
@@ -656,25 +657,25 @@ IN_PROC_BROWSER_TEST_F(DlpContentManagerBrowserTest, ScreenShareReporting) {
       blink::mojom::MediaStreamType::GUM_DESKTOP_VIDEO_CAPTURE,
       /*disable_local_echo=*/false,
       /*request_pan_tilt_zoom_permission=*/false);
-  base::RunLoop wait_loop;
-  content::MediaResponseCallback callback = base::BindOnce(
-      [](base::RunLoop* wait_loop,
-         blink::mojom::MediaStreamRequestResult* request_result,
-         blink::MediaStreamDevices* devices_result,
-         const blink::MediaStreamDevices& devices,
-         blink::mojom::MediaStreamRequestResult result,
-         std::unique_ptr<content::MediaStreamUI> ui) {
-        *request_result = result;
-        *devices_result = devices;
-        wait_loop->Quit();
-      },
-      &wait_loop, &result, &devices);
+
+  base::test::TestFuture<
+      std::reference_wrapper<const blink::MediaStreamDevices>,
+      blink::mojom::MediaStreamRequestResult,
+      std::unique_ptr<content::MediaStreamUI>>
+      test_future;
 
   helper_.ChangeConfidentiality(web_contents, kScreenShareReported);
 
-  access_handler.HandleRequest(web_contents, request, std::move(callback),
-                               /*extension=*/nullptr);
-  wait_loop.Run();
+  access_handler.HandleRequest(
+      web_contents, request,
+      test_future.GetCallback<const blink::MediaStreamDevices&,
+                              blink::mojom::MediaStreamRequestResult,
+                              std::unique_ptr<content::MediaStreamUI>>(),
+      /*extension=*/nullptr);
+
+  ASSERT_TRUE(test_future.Wait()) << "Callback timed out.";
+
+  EXPECT_EQ(test_future.Get<1>(), blink::mojom::MediaStreamRequestResult::OK);
 
   CheckEvents(DlpRulesManager::Restriction::kScreenShare,
               DlpRulesManager::Level::kReport, 1u);
