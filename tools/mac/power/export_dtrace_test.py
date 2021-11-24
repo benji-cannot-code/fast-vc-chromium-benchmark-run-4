bd6768f6a2e8b357cd399f67369b0d68214f9868
Etienne Pierre-doray <etiennep@chromium.org>
2021-11-24 21:36:55 +0000

    [battery]: Rename collapse_profile/export_dtrace
    In follow up, export_dtrace is used to export to pprof directly.
    Since collapse is specific to the exported format, the name isn't
    meaningful if we're exporting to another format.
    
    Bug: 1224994
    Change-Id: I7a2284df6ca0bf08787cabd02c78378294530131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296546
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945122}

--- tools/mac/power/export_dtrace_test.py
+++ tools/mac/power/export_dtrace_test.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import unittest
-from collapse_profile import StackCollapser
+from export_dtrace import StackCollapser
 
 
 class DTraceReadTest(unittest.TestCase):
