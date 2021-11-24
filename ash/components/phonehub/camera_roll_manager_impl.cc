14496d2bf4947d8ff75eae8ade951a123bd31459
Jianbing <jianbing@google.com>
2021-11-24 23:25:10 +0000

    Camera Roll: Animate transition between UI states.
    Add a loading view for loading time between user just enable setting and
    there are new camera roll items received.
    
    Animation specs is referencing https://carbon.googleplex.com/cros-ux/pages/show-off/motion#97edf72b-040c-4cbb-a3a6-0ca55ec583ff
    
    Animation result can be checked here https://drive.google.com/file/d/1WqV_X5UGIchRMSPHG-e7BQfuCrVNImPl/view?usp=sharing&resourcekey=0-7FKZDsjnruOBkn4L-wd1kw
    
    UX review is done and approved by homerr@
    
    Change-Id: Id0336bd0d1dbbaa38eb29389811a4b174e146498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290332
    Commit-Queue: Jianbing Wu <jianbing@google.com>
    Auto-Submit: Jianbing Wu <jianbing@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945173}

--- ash/components/phonehub/camera_roll_manager_impl.cc
+++ ash/components/phonehub/camera_roll_manager_impl.cc
@@ -143,6 +143,7 @@ void CameraRollManagerImpl::OnPhoneStatusSnapshotReceived(
   if (!is_camera_roll_accessible_ || !IsCameraRollSettingEnabled()) {
     ClearCurrentItems();
     CancelPendingThumbnailRequests();
+    resetViewRefreshingFlagIfNeeded();
     return;
   }
 
@@ -156,6 +157,7 @@ void CameraRollManagerImpl::OnPhoneStatusUpdateReceived(
   if (!is_camera_roll_accessible_ || !IsCameraRollSettingEnabled()) {
     ClearCurrentItems();
     CancelPendingThumbnailRequests();
+    resetViewRefreshingFlagIfNeeded();
     return;
   }
 
@@ -189,6 +191,7 @@ void CameraRollManagerImpl::SendFetchCameraRollItemsRequest() {
 void CameraRollManagerImpl::OnItemThumbnailsDecoded(
     CameraRollThumbnailDecoder::BatchDecodeResult result,
     const std::vector<CameraRollItem>& items) {
+  resetViewRefreshingFlagIfNeeded();
   if (result == CameraRollThumbnailDecoder::BatchDecodeResult::kSuccess) {
     SetCurrentItems(items);
   }
@@ -203,6 +206,7 @@ void CameraRollManagerImpl::EnableCameraRollFeatureInSystemSetting() {
   multidevice_setup_client_->SetFeatureEnabledState(
       chromeos::multidevice_setup::mojom::Feature::kPhoneHubCameraRoll,
       /*enabled=*/true, /*auth_token=*/absl::nullopt, base::DoNothing());
+  is_refreshing_after_user_opt_in_ = true;
   // Re-compute and update ui immediately instead of waiting for the callback to
   // finish would hide the view on user's tap action, giving a indicator for the
   // user that the action is performed. When camera items are received, camera
@@ -216,6 +220,10 @@ bool CameraRollManagerImpl::IsCameraRollSettingEnabled() {
           chromeos::multidevice_setup::mojom::Feature::kPhoneHubCameraRoll);
   return camera_roll_feature_state ==
          chromeos::multidevice_setup::mojom::FeatureState::kEnabledByUser;
+}
+
+void CameraRollManagerImpl::resetViewRefreshingFlagIfNeeded() {
+  is_refreshing_after_user_opt_in_ = false;
 }
 
 void CameraRollManagerImpl::OnFeatureStatesChanged(
@@ -253,6 +261,8 @@ void CameraRollManagerImpl::ComputeAndUpdateUiState() {
         (pref_service_->GetBoolean(prefs::kHasDismissedCameraRollOnboardingUi))
             ? CameraRollUiState::SHOULD_HIDE
             : CameraRollUiState::CAN_OPT_IN;
+  } else if (is_refreshing_after_user_opt_in_) {
+    ui_state_ = CameraRollUiState::LOADING_VIEW;
   } else if (current_items().empty()) {
     ui_state_ = CameraRollUiState::SHOULD_HIDE;
   } else {
--- ash/system/phonehub/camera_roll_view.h
+++ ash/system/phonehub/camera_roll_view.h
@@ -38,9 +38,13 @@ class ASH_EXPORT CameraRollView : public views::View,
   // views::View:
   const char* GetClassName() const override;
 
+ protected:
+  bool should_disable_annimator_timer_for_test_ = false;
+
  private:
   FRIEND_TEST_ALL_PREFIXES(CameraRollViewTest, DisplayOptInView);
   FRIEND_TEST_ALL_PREFIXES(CameraRollViewTest, OptInAlready);
+  FRIEND_TEST_ALL_PREFIXES(CameraRollViewTest, RightAfterOptIn);
   FRIEND_TEST_ALL_PREFIXES(CameraRollViewTest, ViewLayout);
 
   class CameraRollItemsView : public views::View {
@@ -51,6 +55,7 @@ class ASH_EXPORT CameraRollView : public views::View,
     CameraRollItemsView operator=(CameraRollItemsView&) = delete;
 
     void AddCameraRollItem(views::View* camera_roll_item);
+    void AddLoadingAnimatedItem(bool disable_repeated_animation_for_test);
     void Reset();
 
     // views::View:
