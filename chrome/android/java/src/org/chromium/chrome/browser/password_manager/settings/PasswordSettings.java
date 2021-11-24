93fc6ccfcdbc85fb40aae2851892734e03809c6f
Ioana Pandele <ioanap@chromium.org>
2021-11-24 13:31:04 +0000

    [CredManager] Check chosen sync data types, not active ones
    Previously, the code was only checking if passwords were actively being
    syncing. However, it is more accurate to check whether the user has
    enabled sync and whether they chose to sync passwords. The difference is
    that the former check can return false in case of a temporary error,
    while the latter condition will stay true.
    
    Bug: 1255038
    Change-Id: I5245e545f8968d5cf4e574dd1873ec723794d6c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295368
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#944936}

--- chrome/android/java/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettings.java
@@ -40,7 +40,6 @@ import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.ChromeManagedPreferenceDelegate;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
-import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.sync.SyncService;
 import org.chromium.chrome.browser.sync.settings.SyncSettingsUtils;
 import org.chromium.components.browser_ui.settings.ChromeBasePreference;
@@ -50,7 +49,6 @@ import org.chromium.components.browser_ui.settings.SettingsLauncher;
 import org.chromium.components.browser_ui.settings.TextMessagePreference;
 import org.chromium.components.prefs.PrefService;
 import org.chromium.components.signin.base.CoreAccountInfo;
-import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.ui.text.SpanApplier;
 
@@ -577,11 +575,7 @@ public class PasswordSettings extends PreferenceFragmentCompat
     }
 
     private void displayManageAccountLink() {
-        IdentityManager identityManager = IdentityServicesProvider.get().getIdentityManager(
-                Profile.getLastUsedRegularProfile());
-        SyncService syncService = SyncService.get();
-        if (!PasswordManagerHelper.isSyncingPasswordsWithoutCustomPassphrase(
-                    identityManager, syncService)) {
+        if (!PasswordManagerHelper.isSyncingPasswordsWithNoCustomPassphrase(SyncService.get())) {
             return;
         }
         if (mSearchQuery != null && !mNoPasswords) {
--- chrome/browser/sync/android/java/src/org/chromium/chrome/browser/sync/SyncServiceImpl.java
+++ chrome/browser/sync/android/java/src/org/chromium/chrome/browser/sync/SyncServiceImpl.java
@@ -83,6 +83,11 @@ public class SyncServiceImpl extends SyncService {
     }
 
     @Override
+    public boolean isSyncFeatureEnabled() {
+        return SyncServiceImplJni.get().isSyncFeatureEnabled(mSyncServiceAndroidBridge);
+    }
+
+    @Override
     public boolean isSyncFeatureActive() {
         return SyncServiceImplJni.get().isSyncFeatureActive(mSyncServiceAndroidBridge);
     }
@@ -402,6 +407,7 @@ public class SyncServiceImpl extends SyncService {
         boolean canSyncFeatureStart(long nativeSyncServiceAndroidBridge);
         boolean isSyncAllowedByPlatform(long nativeSyncServiceAndroidBridge);
         void setSyncAllowedByPlatform(long nativeSyncServiceAndroidBridge, boolean allowed);
+        boolean isSyncFeatureEnabled(long nativeSyncServiceAndroidBridge);
         boolean isSyncFeatureActive(long nativeSyncServiceAndroidBridge);
         boolean isSyncDisabledByEnterprisePolicy(long nativeSyncServiceAndroidBridge);
         boolean isEngineInitialized(long nativeSyncServiceAndroidBridge);
