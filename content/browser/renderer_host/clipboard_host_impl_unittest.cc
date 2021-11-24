82efb8b40557f2e34ef79ec35e2bb7d709af7ebc
Austin Sullivan <asully@chromium.org>
2021-11-24 22:24:35 +0000

    Use CreateBitmap in tests needing a dummy bitmap
    Reduce boilerplate by migrating existing code to use this helper.
    
    This CL was uploaded by git cl split.
    
    R=nasko@chromium.org
    
    Bug: N/A
    Change-Id: I7fea5e0457571cda380ce495a1fbbb2df2a92f18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300759
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945152}

--- content/browser/renderer_host/clipboard_host_impl_unittest.cc
+++ content/browser/renderer_host/clipboard_host_impl_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/test/test_clipboard.h"
 #include "ui/base/data_transfer_policy/data_transfer_policy_controller.h"
 #include "ui/gfx/codec/png_codec.h"
+#include "ui/gfx/image/image_unittest_util.h"
 #include "ui/gfx/skia_util.h"
 
 namespace ui {
@@ -136,9 +137,7 @@ class ClipboardHostImplTest : public RenderViewHostTestHarness {
 };
 
 TEST_F(ClipboardHostImplTest, SimpleImage_ReadPng) {
-  SkBitmap bitmap;
-  bitmap.allocN32Pixels(3, 2);
-  bitmap.eraseARGB(255, 0, 255, 0);
+  SkBitmap bitmap = gfx::test::CreateBitmap(3, 2);
   mojo_clipboard()->WriteImage(bitmap);
   ui::ClipboardSequenceNumberToken sequence_number =
       system_clipboard()->GetSequenceNumber(ui::ClipboardBuffer::kCopyPaste);
