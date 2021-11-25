13182d2d51a5561334efc70278f9847b360227e5
Christian Biesinger <cbiesinger@chromium.org>
2021-11-25 19:32:05 +0000

    [FedCM] Implement Revoke in the FederatedAuthRequestImpl
    This implements Revoke in the mojo service for calling from DOM; that
    will be implemented in a later CL.
    
    Bug: 1263140
    Change-Id: I6704f278cb95f43545275878061d27b493904b11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289487
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945453}

--- third_party/blink/web_tests/wpt_internal/webid/resources/webid-mock.js
+++ third_party/blink/web_tests/wpt_internal/webid/resources/webid-mock.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import { RequestMode, RequestIdTokenStatus, LogoutStatus, FederatedAuthRequest, FederatedAuthRequestReceiver } from '/gen/third_party/blink/public/mojom/webid/federated_auth_request.mojom.m.js';
+import { RequestMode, RequestIdTokenStatus, LogoutStatus, RevokeStatus, FederatedAuthRequest, FederatedAuthRequestReceiver } from '/gen/third_party/blink/public/mojom/webid/federated_auth_request.mojom.m.js';
 
 function toMojoIdTokenStatus(status) {
   switch(status) {
@@ -30,6 +30,7 @@ export class MockFederatedAuthRequest {
     this.idToken_ = null;
     this.status_ = RequestIdTokenStatus.kError;
     this.logoutStatus_ = LogoutStatus.kError;
+    this.revokeStatus_ = RevokeStatus.kError;
   }
 
   // Causes the subsequent `navigator.id.get()` to resolve with the token.
@@ -61,10 +62,17 @@ export class MockFederatedAuthRequest {
     });
   }
 
+  async revoke(provider, client_id, account_id) {
+    return Promise.resolve({
+      status: this.revokeStatus_
+    });
+  }
+
   async reset() {
     this.idToken_ = null;
     this.status_ = RequestIdTokenStatus.kError;
     this.logoutStatus_ = LogoutStatus.kError;
+    this.revokeStatus_ = RevokeStatus.kError;
     this.receiver_.$.close();
     this.interceptor_.stop();
 
