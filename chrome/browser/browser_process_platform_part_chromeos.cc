56619bd08a246a12d955bc74324739c8f9db70c1
Sergiy Belozorov <sergiyb@chromium.org>
2021-11-25 11:19:57 +0000

    Use WallClockTimer to track time to reboot
    This avoids rebooting too late on some platforms that do not increment
    ticks while the device is suspended.
    
    TEST=deployed to DUT and verified that it reboots on time despite sleep
    
    Bug: 1180205
    Change-Id: Ife49853b3a20f4d8f63960743282028c1a021342
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288701
    Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945342}

--- chrome/browser/browser_process_platform_part_chromeos.cc
+++ chrome/browser/browser_process_platform_part_chromeos.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/check_op.h"
 #include "base/memory/singleton.h"
+#include "base/time/default_clock.h"
 #include "base/time/default_tick_clock.h"
 #include "base/time/tick_clock.h"
 #include "chrome/browser/ash/crosapi/browser_manager.h"
@@ -197,6 +198,7 @@ void BrowserProcessPlatformPart::InitializeAutomaticRebootManager() {
 
   automatic_reboot_manager_ =
       std::make_unique<ash::system::AutomaticRebootManager>(
+          base::DefaultClock::GetInstance(),
           base::DefaultTickClock::GetInstance());
 }
 
