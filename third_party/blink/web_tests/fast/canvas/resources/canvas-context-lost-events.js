e62ab12e671e076912bee86ebb07d3af4a5bd18b
Yi Xu <yiyix@chromium.org>
2021-11-25 21:15:40 +0000

    Add tests for oncontextlost/oncontextrestored
    Bug: 1267688
    
    Change-Id: Ie0e7f4e5b03f3c3eda0bd94c97099e40de55c3e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298180
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945483}

---
+++ third_party/blink/web_tests/fast/canvas/resources/canvas-context-lost-events.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if( 'undefined' === typeof window){
+    importScripts('../../../resources/js-test.js');
+    importScripts('../../../resources/testharness.js');
+    importScripts('../../../resources/testharnessreport.js');
+}
+
+function verifyContextLost(shouldBeLost, ctx) {
+    // Verify context loss experimentally as well as contextLost
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(0, 0, 1, 1);
+    contextLostTest = ctx.getImageData(0, 0, 1, 1).data[1] == 0;
+    if (shouldBeLost) {
+        assert_true(contextLostTest);
+        assert_true(ctx.isContextLost());
+    } else {
+        assert_false(contextLostTest);
+        assert_false(ctx.isContextLost());
+    }
+}
+
+function contextLost(lostEventHasFired, ctx) {
+    assert_false(lostEventHasFired, 'Graphics context lost event dispatched more than once.');
+    lostEventHasFired = true;
+    verifyContextLost(true, ctx);
+}
+
+function contextRestoredlostEventHasFired(lostEventHasFired, ctx) {
+    assert_true(lostEventHasFired, 'Context restored event dispatched after context lost.');
+    verifyContextLost(false, ctx);
+}
