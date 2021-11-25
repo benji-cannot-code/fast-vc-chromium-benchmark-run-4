2c4d72b937956293d7f062ded02766889a78b3ed
David Bienvenu <davidbienvenu@chromium.org>
2021-11-25 15:37:49 +0000

    Revert "unit_tests: fix flakes in ThemeServiceTest"
    This reverts commit 345f2fb2f5d37c78cce7060959d69e416274d072.
    
    Reason for revert: <crbug.com/1182630 has a workaround submitted>
    
    Original change's description:
    > unit_tests: fix flakes in ThemeServiceTest
    >
    > Make sure QuotaManager is finished initializing after loading theme,
    > so that we don't crash when it finishes initializing itself during test
    > shutdown.
    >
    > This should be rolled back once crbug.com/1182630 is fixed.
    >
    > Bug: 956719, 1169094
    > Change-Id: Ib6dafda03ca061792dd6fb814b780e3a9f909eba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2765407
    > Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#863976}
    
    Bug: 956719
    Bug: 1169094
    Change-Id: If362b8f7e5ca4138959f9b958c651198c48ffa8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2941648
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945392}

--- chrome/browser/themes/theme_service_unittest.cc
+++ chrome/browser/themes/theme_service_unittest.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_registry.h"
-#include "extensions/browser/extension_util.h"
 #include "extensions/browser/test_extension_registry_observer.h"
 #include "extensions/browser/uninstall_reason.h"
 #include "extensions/common/extension.h"
@@ -194,16 +193,9 @@ class ThemeServiceTest : public extensions::ExtensionServiceTestBase {
         extensions::UnpackedInstaller::Create(service_));
     extensions::TestExtensionRegistryObserver observer(registry_);
     installer->Load(temp_dir);
-    std::string extenson_id = observer.WaitForExtensionLoaded()->id();
-    scoper.set_extension_id(extenson_id);
+    scoper.set_extension_id(observer.WaitForExtensionLoaded()->id());
 
     waiter.WaitForThemeChanged();
-
-    // Make sure RegisterClient calls for storage are finished to avoid flaky
-    // crashes in QuotaManagerImpl::RegisterClient on test shutdown.
-    // TODO(crbug.com/1182630) : Remove this when 1182630 is fixed.
-    extensions::util::GetStoragePartitionForExtensionId(extenson_id, profile());
-    task_environment()->RunUntilIdle();
 
     return scoper;
   }
