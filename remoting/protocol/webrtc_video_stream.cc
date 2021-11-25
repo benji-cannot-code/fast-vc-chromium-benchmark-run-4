eb661a18ea7426907b6ac9f3be876ac9b061e8cf
Roman Sorokin [CET] <rsorokin@chromium.org>
2021-11-25 14:20:15 +0000

    Revert "[remoting host] Implement constant-framerate scheduler."
    This reverts commit fa02a5ce07e2d81cb7dea66ec118f5a38cd0d176.
    
    Reason for revert: Suspecting this CL made 	Webrtc/ConnectionTest.VideoWithSlowSignaling/0 flaky (see https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyTAsSBUZsYWtlIkFjaHJvbWl1bUByZW1vdGluZ191bml0dGVzdHNAQ29ubmVjdGlvblRlc3QuVmlkZW9XaXRoU2xvd1NpZ25hbGluZww&show_all_occurrences=1). Heavy flakiness started when this CL landed
    
    Original change's description:
    > [remoting host] Implement constant-framerate scheduler.
    >
    > This adds a new scheduler that captures the desktop at a fixed rate,
    > given by the value passed to SetMaxFramerateFps().
    >
    > Bug: 1268253
    > Change-Id: Ic90364ccd3ad9935ed7f56d70a922f073fef4b61
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293851
    > Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#945213}
    
    Bug: 1268253
    Change-Id: I1c86f570df2fc21dbc41029d9bda448c704eea84
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301473
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Roman Sorokin [CET] <rsorokin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#945375}

--- remoting/protocol/webrtc_video_stream.cc
+++ remoting/protocol/webrtc_video_stream.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/constants.h"
 #include "remoting/protocol/frame_stats.h"
 #include "remoting/protocol/host_video_stats_dispatcher.h"
-#include "remoting/protocol/webrtc_frame_scheduler_constant_rate.h"
+#include "remoting/protocol/webrtc_frame_scheduler_simple.h"
 #include "remoting/protocol/webrtc_transport.h"
 #include "remoting/protocol/webrtc_video_encoder_factory.h"
 #include "remoting/protocol/webrtc_video_frame_adapter.h"
@@ -91,7 +91,7 @@ void WebrtcVideoStream::Start(
 
   video_encoder_factory->SetVideoChannelStateObserver(
       weak_factory_.GetWeakPtr());
-  scheduler_ = std::make_unique<WebrtcFrameSchedulerConstantRate>();
+  scheduler_ = std::make_unique<WebrtcFrameSchedulerSimple>(session_options_);
   scheduler_->Start(base::BindRepeating(&WebrtcVideoStream::CaptureNextFrame,
                                         base::Unretained(this)));
 }
