cda48cf5e83386614e5d0ac7f0d88f784fb923d3
Jeroen Dhollander <jeroendh@google.com>
2021-11-25 16:42:27 +0000

    Stop screenshot in unified desktop mode from crashing
    The root cause of the crash was that the |CopyOutputRequest| was moved
    from the Surface/CompositorRenderPass to the AggregatedRenderPass
    generated for the unified display.
    This aggregated frame is never drawn though, and when the unified display
    skips drawing of this frame the moved-over |CopyOutputRequest| instances
    are destroyed.
    
    Bug: 1260249
    Test: Manually executed the Create Screenshot Device Action in a kiosk in Unified Desktop mode
    Change-Id: I4927c94b533153604e3cc5576b20623c7e25d43d
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295343
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#945418}

--- components/viz/service/display/surface_aggregator.cc
+++ components/viz/service/display/surface_aggregator.cc
@@ -702,7 +702,8 @@ void SurfaceAggregator::EmitSurfaceContent(
 
   // A map keyed by RenderPass id.
   Surface::CopyRequestsMap copy_requests;
-  surface->TakeCopyOutputRequests(&copy_requests);
+  if (take_copy_requests_)
+    surface->TakeCopyOutputRequests(&copy_requests);
 
   const CompositorRenderPassList& render_pass_list = frame.render_pass_list;
   if (!resolved_frame.is_valid()) {
@@ -1274,7 +1275,8 @@ void SurfaceAggregator::CopyPasses(const ResolvedFrameData& resolved_frame) {
   // off its render passes. This map contains a set of CopyOutputRequests
   // keyed by each RenderPass id.
   Surface::CopyRequestsMap copy_requests;
-  surface->TakeCopyOutputRequests(&copy_requests);
+  if (take_copy_requests_)
+    surface->TakeCopyOutputRequests(&copy_requests);
 
   const auto& source_pass_list = frame.render_pass_list;
   if (!resolved_frame.is_valid())
@@ -1928,7 +1930,7 @@ AggregatedFrame SurfaceAggregator::Aggregate(
   if (prewalk_result.frame_sinks_changed)
     manager_->AggregatedFrameSinksChanged();
 
-  frame.has_copy_requests = has_copy_requests_;
+  frame.has_copy_requests = has_copy_requests_ && take_copy_requests_;
   frame.video_capture_enabled = prewalk_result.video_capture_enabled;
   frame.may_contain_video = prewalk_result.may_contain_video;
   frame.content_color_usage = prewalk_result.content_color_usage;
