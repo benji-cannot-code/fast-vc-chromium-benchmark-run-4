2a0f636edb757467058ef6b10c96cd91dcbf9446
manukh <manukh@chromium.org>
2021-11-25 04:33:14 +0000

    [memories] Soft cap keywords cached.
    Both the primary and short caches will be soft capped. The cap if a
    feature param defaulted to 20k, which we don't expect to reach in
    typical cases but will avoid high memory usage in exceptional cases.
    We'll skip the continuation requests for more clusters if we've reached
    the cap.
    
    The cap may be exceeded because:
    - The 2 caches will be capped independently, so even if both are under
    the cap, collectively they may exceed the cap.
    - We don't partially cache a cluster. If we're under the cap, we'll
    cache all keywords of the next cluster even if doing so puts us beyond
    the cap.
    
    Each cluster can have multiple keyword phrases, each of which can
    consist of multiple words. E.g., a cluster containing google.com may
    have the 2 phrases "search engine" & "news", consisting of 3 words
    total. We cap the number of phrases, not individual words.
    
    This CL also adds 4 histograms to measure the cache sizes: words &
    phrases in the primary & short caches.
    
    Bug: 1262016
    Change-Id: I5b8f945ef9f98385783b1044c93d1d74e27ac526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291844
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945256}

--- components/history_clusters/core/memories_features.cc
+++ components/history_clusters/core/memories_features.cc
@@ -37,6 +37,11 @@ const base::FeatureParam<int> kMaxVisitsToCluster{
 const base::FeatureParam<int> kMaxDaysToCluster{&kJourneys,
                                                 "JourneysMaxDaysToCluster", 9};
 
+// 20k should be more than enough for most cases and should avoid consuming
+// large amounts of memory in extreme cases.
+const base::FeatureParam<int> kMaxKeywordPhrases{
+    &kJourneys, "JourneysMaxKeywordPhrases", 20000};
+
 const base::FeatureParam<bool> kPersistClustersInHistoryDb{
     &kJourneys, "JourneysPersistClustersInHistoryDb", false};
 
@@ -49,8 +54,8 @@ const base::FeatureParam<double> kMinScoreToAlwaysShowAboveTheFold{
 const base::FeatureParam<int> kNumVisitsToAlwaysShowAboveTheFold{
     &kJourneys, "JourneysNumVisitsToAlwaysShowAboveTheFold", 3};
 
-// Default to true, as this this new alternate action text was recommended by
-// our UX writers.
+// Default to true, as this new alternate action text was recommended by our UX
+// writers.
 const base::FeatureParam<bool> kAlternateOmniboxActionText{
     &kOmniboxAction, "JourneysAlternateOmniboxActionText", true};
 
