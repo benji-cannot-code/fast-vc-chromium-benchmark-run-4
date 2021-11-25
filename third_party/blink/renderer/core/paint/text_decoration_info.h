be15d403e26dadc4a4327ebcef1b5165418e632f
Kent Tamura <tkent@chromium.org>
2021-11-25 04:48:54 +0000

    SVG Text NG: Minimum underline/overline thickness should be 0
    SVG text is frequently scaled, and the minimum 1.0 pixel thickness of
    underline/overline is not reasonable.
    
    This CL adds a bool-like argument to the TextDecorationInfo constructor
    to distinguish SVG text and others.
    
    Bug: 1270336
    Change-Id: Ia506c49f117550f813e746b2181e8a4dc459c272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299740
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945260}

--- third_party/blink/renderer/core/paint/text_decoration_info.h
+++ third_party/blink/renderer/core/paint/text_decoration_info.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_TEXT_DECORATION_INFO_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_TEXT_DECORATION_INFO_H_
 
+#include "base/types/strong_alias.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_offset.h"
@@ -32,6 +33,8 @@ enum class ResolvedUnderlinePosition {
   kOver
 };
 
+using MinimumThickness1 = base::StrongAlias<class MinimumThickness1Tag, bool>;
+
 // Container for computing and storing information for text decoration
 // invalidation and painting. See also
 // https://www.w3.org/TR/css-text-decor-3/#painting-order
@@ -47,6 +50,7 @@ class CORE_EXPORT TextDecorationInfo {
       const Font& scaled_font,
       const absl::optional<AppliedTextDecoration> selection_text_decoration,
       const ComputedStyle* decorating_box_style,
+      MinimumThickness1 minimum_thickness1 = MinimumThickness1(true),
       float scaling_factor = 1.0f);
 
   // Set the decoration to use when painting and returning values.
@@ -122,6 +126,7 @@ class CORE_EXPORT TextDecorationInfo {
   const float scaling_factor_;
   ResolvedUnderlinePosition underline_position_;
   gfx::PointF local_origin_;
+  const bool minimum_thickness_is_one_;
   bool antialias_;
   Vector<float> applied_decorations_thickness_;
   int decoration_index_;
