e3830f655667dfc21fb000801051ce4c7dab19a2
Austin Eng <enga@chromium.org>
2021-11-25 01:49:44 +0000

    dawn_native::DeviceDescriptor -> dawn_native::DawnDeviceDescriptor
    This struct name is changing so Dawn can support DeviceDescriptor
    from upstream webgpu.h. The upstream struct name conflicts with this
    existing struct.
    
    Bug: dawn:160, dawn:689
    Change-Id: If8a0be13215d0c852946d64f744e88e55de54c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298680
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945218}

--- gpu/command_buffer/service/shared_image_backing_factory_d3d_unittest.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_d3d_unittest.cc
@@ -606,7 +606,7 @@ TEST_F(SharedImageBackingFactoryD3DTest, Dawn_SkiaGL) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
@@ -749,7 +749,7 @@ TEST_F(SharedImageBackingFactoryD3DTest, GL_Dawn_Skia_UnclearTexture) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
@@ -839,7 +839,7 @@ TEST_F(SharedImageBackingFactoryD3DTest, UnclearDawn_SkiaFails) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
@@ -1109,7 +1109,7 @@ TEST_F(SharedImageBackingFactoryD3DTest, Dawn_ReuseExternalImage) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
@@ -1237,7 +1237,7 @@ TEST_F(SharedImageBackingFactoryD3DTest, Dawn_HasLastRef) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
--- gpu/command_buffer/service/shared_image_backing_factory_iosurface_unittest.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_iosurface_unittest.cc
@@ -383,7 +383,7 @@ TEST_F(SharedImageBackingFactoryIOSurfaceTest, Dawn_SkiaGL) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
@@ -532,7 +532,7 @@ TEST_F(SharedImageBackingFactoryIOSurfaceTest, GL_Dawn_Skia_UnclearTexture) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
@@ -620,7 +620,7 @@ TEST_F(SharedImageBackingFactoryIOSurfaceTest, UnclearDawn_SkiaFails) {
       });
   ASSERT_NE(adapter_it, adapters.end());
 
-  dawn_native::DeviceDescriptor device_descriptor;
+  dawn_native::DawnDeviceDescriptor device_descriptor;
   // We need to request internal usage to be able to do operations with
   // internal methods that would need specific usages.
   device_descriptor.requiredFeatures.push_back("dawn-internal-usages");
