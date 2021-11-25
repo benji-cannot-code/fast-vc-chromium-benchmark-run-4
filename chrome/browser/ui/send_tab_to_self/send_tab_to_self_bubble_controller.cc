9f468894d86b456b6403fa1f142c4e5d79f996c1
Victor Hugo Vianna Silva <victorvianna@google.com>
2021-11-25 16:52:38 +0000

    [send-tab-to-self] Fix view test if SendTabToSelfManageDevicesLink on
    This is a preparation to enable the feature by default. The test
    used to call the real SendTabToSelfBubbleController::
    GetSharingAccountInfo(), which caused a null pointer dereference of
    its web_contents_. Instead, just override the method in the test double.
    
    Unrelated to this:
    - Remove an unused TestingProfile from the test.
    - Avoid some unnecessary usages of FindBrowserWithWebContents() in the
    controller. The profile can be directly obtained from WebContents via
    GetBrowserContext(), without having to resort to the browser window
    (which won't even be around if the tab is detached).
    
    Bug: 956722
    Change-Id: Idd565621a18cf7f26d44e60d3bae318e7a983ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301393
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945422}

--- chrome/browser/ui/send_tab_to_self/send_tab_to_self_bubble_controller.cc
+++ chrome/browser/ui/send_tab_to_self/send_tab_to_self_bubble_controller.cc
@@ -87,9 +87,8 @@ std::vector<TargetDeviceInfo> SendTabToSelfBubbleController::GetValidDevices()
 }
 
 AccountInfo SendTabToSelfBubbleController::GetSharingAccountInfo() const {
-  Browser* browser = chrome::FindBrowserWithWebContents(web_contents_);
   signin::IdentityManager* identity_manager =
-      IdentityManagerFactory::GetForProfile(browser->profile());
+      IdentityManagerFactory::GetForProfile(GetProfile());
   return identity_manager->FindExtendedAccountInfo(
       identity_manager->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin));
 }
@@ -107,8 +106,7 @@ void SendTabToSelfBubbleController::OnDeviceSelected(
 
 void SendTabToSelfBubbleController::OnManageDevicesClicked(
     const ui::Event& event) {
-  Browser* browser = chrome::FindBrowserWithWebContents(web_contents_);
-  NavigateParams params(browser->profile(),
+  NavigateParams params(GetProfile(),
                         GURL(chrome::kGoogleAccountDeviceActivityURL),
                         ui::PageTransition::PAGE_TRANSITION_LINK);
   // NEW_FOREGROUND_TAB is passed as the default below to avoid exiting the
