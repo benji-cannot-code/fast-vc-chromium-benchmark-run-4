e2b64402ae42d71e8fcf01f9ced44b3aef64d750
Francois Doray <fdoray@chromium.org>
2021-11-25 20:14:36 +0000

    Move code to compute macOS Energy Impact to //components/power_metrics.
    This allows reuse by the code that records histograms in Chrome and
    the "power_sampler" tool in //tools/mac/power.
    
    Bug: 1254332
    Change-Id: I9815af033848d4ea13ff39f724c68b2c66f41722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296547
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Auto-Submit: François Doray <fdoray@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945466}

---
+++ components/power_metrics/mach_time_mac.mm
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/power_metrics/mach_time_mac.h"
+
+#include "base/check.h"
+#include "base/mac/mach_logging.h"
+
+namespace power_metrics {
+
+uint64_t MachTimeToNs(uint64_t mach_time,
+                      const mach_timebase_info_data_t& mach_timebase) {
+  if (mach_timebase.numer == mach_timebase.denom)
+    return mach_time;
+
+  CHECK(!__builtin_umulll_overflow(mach_time, mach_timebase.numer, &mach_time));
+  return mach_time / mach_timebase.denom;
+}
+
+mach_timebase_info_data_t GetSystemMachTimeBase() {
+  mach_timebase_info_data_t info;
+  kern_return_t kr = mach_timebase_info(&info);
+  MACH_DCHECK(kr == KERN_SUCCESS, kr) << "mach_timebase_info";
+  DCHECK(info.numer);
+  DCHECK(info.denom);
+  return info;
+}
+
+}  // namespace power_metrics
