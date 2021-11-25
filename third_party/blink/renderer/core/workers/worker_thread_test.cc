7bdb5383bf787f0b2da99368510f6e5d0f43186e
Hajime Hoshi <hajimehoshi@chromium.org>
2021-11-25 05:26:35 +0000

    Evict the page when redirection happens in a fetch by a dedicated worker
    This CL propagates ExecutionContext::SetInBackForwardCache to dedicated
    workers and nested workers. The state of being in BFcache affects a
    resource loader's behavior: `MojoURLLoader::freeze_mode_` will be
    `WebLoaderFreezeMode::kBufferIncoming` when the page is in BFcache, and
    the page might be evicted when redirection happens or loading data
    reaches a limit.
    
    This CL also adds a test for the case when a page with a dedicatd worker
    fetches a resource before putting into BFcache, and then redirection
    response is sent after the page is put into BFcache.
    
    Design Doc: https://docs.google.com/document/d/1CJfDNqA2tEp_dmxK5kaBHHqGjx7cK2_jhTpsCwSCa3A/edit?usp=sharing
    
    Bug: 1146955
    Change-Id: Ie6bdc26a9169bb8051eac7727c3e4326083a358b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288508
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945270}

--- third_party/blink/renderer/core/workers/worker_thread_test.cc
+++ third_party/blink/renderer/core/workers/worker_thread_test.cc
@@ -555,7 +555,7 @@ TEST_F(WorkerThreadTest, MAYBE_TerminateFrozenScript) {
                           CrossThreadUnretained(&child_waitable)));
 
   // Freeze() enters a nested event loop where the kInternalTest should run.
-  worker_thread_->Freeze();
+  worker_thread_->Freeze(false /* is_in_back_forward_cache */);
   child_waitable.Wait();
 
   // Terminate() schedules a forcible termination task.
@@ -584,7 +584,7 @@ TEST_F(WorkerThreadTest, MAYBE_NestedPauseFreeze) {
 
   // Pause() enters a nested event loop where the kInternalTest should run.
   worker_thread_->Pause();
-  worker_thread_->Freeze();
+  worker_thread_->Freeze(false /* is_in_back_forward_cache */);
   child_waitable.Wait();
 
   // Resume Freeze.
@@ -629,7 +629,7 @@ TEST_F(WorkerThreadTest, MAYBE_NestedPauseFreezeNoInterrupts) {
 
   // Pause() enters a nested event loop where the kInternalTest should run.
   worker_thread_->Pause();
-  worker_thread_->Freeze();
+  worker_thread_->Freeze(false /* is_in_back_forward_cache */);
   child_waitable2.Wait();
 
   // Resume for Freeze.
