0e18f80861dd634c66f494fcd98415281846eafd
Olga Sharonova <olka@chromium.org>
2021-11-26 15:18:48 +0000

    Chrome-wide echo cancellation: Metrics for OutputDeviceMixer
    Bug: 1224845
    Change-Id: I6d536b92c835453e38fc5da11f4319a0977cf007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300364
    Reviewed-by: Alessio Bazzica <alessiob@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945633}

--- services/audio/output_device_mixer_impl.h
+++ services/audio/output_device_mixer_impl.h
@@ -78,6 +78,7 @@ class OutputDeviceMixerImpl final : public OutputDeviceMixer {
  private:
   class MixTrack;
   class MixableOutputStream;
+  class MixingStats;
 
   struct StreamAutoClose {
     void operator()(media::AudioOutputStream* stream) {
@@ -148,6 +149,10 @@ class OutputDeviceMixerImpl final : public OutputDeviceMixer {
   // Delays switching to unmixed playback in case a new listener is coming
   // soon (within kSwitchToIndependentPlaybackDelay).
   base::OneShotTimer switch_to_unmixed_playback_delay_timer_;
+
+  // Non-null when the playback is being mixed. Collects mixing statistics.
+  // Logs them upon the destruction when mixing stops.
+  std::unique_ptr<MixingStats> mixing_stats_;
 
 #if DCHECK_IS_ON()
   bool device_changed_ = false;
