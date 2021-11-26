8c65cfa4eacac1e348ab8e33c810fde2e78b381c
Alex Ilin <alexilin@chromium.org>
2021-11-26 15:49:10 +0000

    [Lacros] Save the list of Lacros accounts in local state
    This CL adds a new Lacros-only preference that is saved in local state.
    This new pref is managed by the AccountProfileMapper class.
    
    The full list of accounts allows Lacros to distinguish between newly
    added accounts and "unassigned" accounts that don't belong to any
    profile.
    
    This distinction is important to make because Lacros automatically
    assigns new accounts to the primary profile if the user has only one
    profile. However, Lacros should not automatically add old "unassigned"
    accounts to the primary profile.
    
    We could have stored only unassigned accounts instead of a full list of
    accounts. However, maintaining a list of unassigned accounts is more
    difficult than storing a list of all accounts and there is little
    benefit in doing so.
    
    Bug: 1260376
    Change-Id: I01fc5601d4d658281eba57f4731d025c3e184f2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296028
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945641}

--- chrome/browser/lacros/account_manager/account_manager_util_unittest.cc
+++ chrome/browser/lacros/account_manager/account_manager_util_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/account_manager_core/account.h"
 #include "components/account_manager_core/account_manager_facade.h"
 #include "components/account_manager_core/mock_account_manager_facade.h"
+#include "components/prefs/testing_pref_service.h"
 #include "content/public/test/browser_task_environment.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -99,8 +100,9 @@ class AccountManagerUtilTest : public testing::Test {
             [&accounts_in_facade](
                 base::OnceCallback<void(const std::vector<Account>&)>
                     callback) { std::move(callback).Run(accounts_in_facade); });
-    auto mapper = std::make_unique<AccountProfileMapper>(mock_facade(),
-                                                         attributes_storage());
+    auto mapper = std::make_unique<AccountProfileMapper>(
+        mock_facade(), attributes_storage(),
+        testing_profile_manager_.local_state()->Get());
     SetAccountsInStorage(accounts);
     return mapper;
   }
--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -428,6 +428,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/lacros/account_manager/account_cache.h"
 #include "chrome/browser/lacros/lacros_prefs.h"
 #include "chrome/browser/lacros/net/proxy_config_service_lacros.h"
 #endif
@@ -1060,6 +1061,7 @@ void RegisterLocalState(PrefRegistrySimple* registry) {
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
+  AccountCache::RegisterLocalStatePrefs(registry);
   lacros_prefs::RegisterLocalStatePrefs(registry);
 #endif
 
