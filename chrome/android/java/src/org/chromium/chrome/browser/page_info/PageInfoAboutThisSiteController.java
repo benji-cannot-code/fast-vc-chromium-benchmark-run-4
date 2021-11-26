8a8e29b10af54d4240180bfb5d95540a348a5f0a
Christian Dullweber <dullweber@chromium.org>
2021-11-26 19:24:39 +0000

    [about this site] Avoid crash for WebContents without Tab
    Pass in a WebContents instead of a Tab as the tab can be null.
    
    Bug: 1274167
    Change-Id: I25d29732e6d0f0740432627ffb2fcef7cb7bab21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304213
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945688}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/PageInfoAboutThisSiteController.java
@@ -13,8 +13,8 @@ import androidx.annotation.Nullable;
 import org.chromium.base.Log;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
+import org.chromium.chrome.browser.tab.TabUtils;
 import org.chromium.chrome.browser.tabmodel.document.TabDelegate;
 import org.chromium.components.page_info.PageInfoControllerDelegate;
 import org.chromium.components.page_info.PageInfoMainController;
@@ -37,15 +37,15 @@ public class PageInfoAboutThisSiteController implements PageInfoSubpageControlle
     private final PageInfoMainController mMainController;
     private final PageInfoRowView mRowView;
     private final PageInfoControllerDelegate mDelegate;
-    private final Tab mTab;
+    private final WebContents mWebContents;
     private @Nullable SiteInfo mSiteInfo;
 
     public PageInfoAboutThisSiteController(PageInfoMainController mainController,
-            PageInfoRowView rowView, PageInfoControllerDelegate delegate, Tab tab) {
+            PageInfoRowView rowView, PageInfoControllerDelegate delegate, WebContents webContents) {
         mMainController = mainController;
         mRowView = rowView;
         mDelegate = delegate;
-        mTab = tab;
+        mWebContents = webContents;
         setupRow();
     }
 
@@ -71,7 +71,7 @@ public class PageInfoAboutThisSiteController implements PageInfoSubpageControlle
             new TabDelegate(/*incognito=*/false)
                     .createNewTab(
                             new LoadUrlParams(mSiteInfo.getDescription().getSource().getUrl()),
-                            TabLaunchType.FROM_CHROME_UI, mTab);
+                            TabLaunchType.FROM_CHROME_UI, TabUtils.fromWebContents(mWebContents));
         });
         return view;
     }
@@ -111,8 +111,7 @@ public class PageInfoAboutThisSiteController implements PageInfoSubpageControlle
 
     private @Nullable SiteInfo getSiteInfo() {
         byte[] result = PageInfoAboutThisSiteControllerJni.get().getSiteInfo(
-                mMainController.getBrowserContext(), mMainController.getURL(),
-                mTab.getWebContents());
+                mMainController.getBrowserContext(), mMainController.getURL(), mWebContents);
         if (result == null) return null;
         SiteInfo info = null;
         try {
