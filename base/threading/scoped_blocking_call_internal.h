162d3b4e7c75878d6b7941cf41384fb19363f0b9
Gabriel Charette <gab@chromium.org>
2021-11-26 15:08:32 +0000

    Revert "[base] Only monitor IOJank on managed threads in ScopedBlockingCallIOJankMonitoringTest to avoid interference"
    This reverts commit 8e5f79fc2f512fda9db06908d276a2fbe0df15bb.
    
    Reason for revert: flakes TSAN
    https://luci-milo.appspot.com/ui/inv/build-8829505188895665761/test-results?q=ScopedBlockingCallIOJankMonitoringTest.MultiThreadedOverlappedWindows
    
    Original change's description:
    > [base] Only monitor IOJank on managed threads in ScopedBlockingCallIOJankMonitoringTest to avoid interference
    >
    > Bug: 1071166
    > Change-Id: I162d6c6651ef113fcffbbdb9294117156734d354
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3261128
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#945521}
    
    Bug: 1071166
    Change-Id: Id1901fb243dfd1faf72f28fc0c57362b01817752
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3303952
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#945630}

--- base/threading/scoped_blocking_call_internal.h
+++ base/threading/scoped_blocking_call_internal.h
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_THREADING_SCOPED_BLOCKING_CALL_INTERNAL_H_
 
 #include "base/base_export.h"
-#include "base/callback_forward.h"
 #include "base/debug/activity_tracker.h"
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
 #include "base/thread_annotations.h"
 #include "base/time/time.h"
-#include "base/types/strong_alias.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace base {
@@ -21,10 +19,7 @@ namespace base {
 // Forward-declare types from scoped_blocking_call.h to break cyclic dependency.
 enum class BlockingType;
 using IOJankReportingCallback = RepeatingCallback<void(int, int)>;
-using OnlyObservedThreadsForTest =
-    StrongAlias<class OnlyObservedThreadsTag, bool>;
-void BASE_EXPORT EnableIOJankMonitoringForProcess(IOJankReportingCallback,
-                                                  OnlyObservedThreadsForTest);
+void BASE_EXPORT EnableIOJankMonitoringForProcess(IOJankReportingCallback);
 
 // Implementation details of types in scoped_blocking_call.h and classes for a
 // few key //base types to observe and react to blocking calls.
@@ -106,9 +101,7 @@ class BASE_EXPORT IOJankMonitoringWindow
 
  private:
   friend class base::RefCountedThreadSafe<IOJankMonitoringWindow>;
-  friend void base::EnableIOJankMonitoringForProcess(
-      IOJankReportingCallback,
-      OnlyObservedThreadsForTest);
+  friend void base::EnableIOJankMonitoringForProcess(IOJankReportingCallback);
 
   // No-op if reporting_callback_storage() is null (i.e. unless
   // EnableIOJankMonitoringForProcess() was called).
