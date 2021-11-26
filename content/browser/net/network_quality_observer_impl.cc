48cc1932dc3ef74627f4f1613811a1355b18cf1a
dan sinclair <dsinclair@chromium.org>
2021-11-26 03:28:22 +0000

    [code health] Remove use of NotificationService
    This CL removes the use of the NotificationService in the
    NetworkQualityObserverImpl and replaces it with
    RenderProcessHostCreationObserver.
    
    More details can be seen in the bug.
    
    Bug: 357627
    Change-Id: I39daeb6a192c9623f8764a857b45a549ecb8480d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299490
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Auto-Submit: dsinclair <dsinclair@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945534}

--- content/browser/net/network_quality_observer_impl.cc
+++ content/browser/net/network_quality_observer_impl.cc
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/renderer.mojom.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/network_quality_observer_factory.h"
-#include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/notification_service.h"
-#include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_process_host.h"
 
 namespace {
@@ -62,8 +59,6 @@ NetworkQualityObserverImpl::NetworkQualityObserverImpl(
       last_notified_type_(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  registrar_.Add(this, NOTIFICATION_RENDERER_PROCESS_CREATED,
-                 NotificationService::AllSources());
   network_quality_tracker_->AddRTTAndThroughputEstimatesObserver(this);
   network_quality_tracker_->AddEffectiveConnectionTypeObserver(this);
 }
@@ -95,13 +90,9 @@ void NetworkQualityObserverImpl::OnEffectiveConnectionTypeChanged(
   }
 }
 
-void NetworkQualityObserverImpl::Observe(int type,
-                                         const NotificationSource& source,
-                                         const NotificationDetails& details) {
+void NetworkQualityObserverImpl::OnRenderProcessHostCreated(
+    content::RenderProcessHost* rph) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  DCHECK_EQ(NOTIFICATION_RENDERER_PROCESS_CREATED, type);
-
-  RenderProcessHost* rph = Source<RenderProcessHost>(source).ptr();
 
   // Notify the newly created renderer of the current network quality.
   rph->GetRendererInterface()->OnNetworkQualityChanged(
