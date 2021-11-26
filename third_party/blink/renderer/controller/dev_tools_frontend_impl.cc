59b84b08eae6f1760fbf08bd6eca60323182ee9c
Yang Guo <yangguo@chromium.org>
2021-11-26 09:16:55 +0000

    Disable forced dark mode for DevTools
    Fixed: 1243273
    Change-Id: I4d574e2410aa4e5a6349882e01eb91302a6f79de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301383
    Auto-Submit: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945576}

--- third_party/blink/renderer/controller/dev_tools_frontend_impl.cc
+++ third_party/blink/renderer/controller/dev_tools_frontend_impl.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/exported/web_view_impl.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/frame/web_frame_widget_impl.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
 #include "third_party/blink/renderer/core/inspector/dev_tools_host.h"
@@ -112,6 +113,7 @@ void DevToolsFrontendImpl::SetupDevToolsFrontend(
   DCHECK(frame->IsMainFrame());
   frame->GetWidgetForLocalRoot()->SetLayerTreeDebugState(
       cc::LayerTreeDebugState());
+  frame->GetPage()->GetSettings().SetForceDarkModeEnabled(false);
   api_script_ = api_script;
   host_.Bind(std::move(host),
              GetSupplementable()->GetTaskRunner(TaskType::kMiscPlatformAPI));
