956c4c48e2688390d7d1f55992d5e16abcada878
Alexander Bolodurin <alexbn@chromium.org>
2021-11-26 05:13:24 +0000

    [lacros shelf] Remove crash probes from BrowserAppInstanceTracker
    The crashes did not eventuate, so the probes can be removed.
    
    Bug: 1236273
    Change-Id: I5c68f1cf140dfd48988e23cb208d4ff48bfb6b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301575
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945546}

--- chrome/browser/apps/app_service/browser_app_instance_tracker.cc
+++ chrome/browser/apps/app_service/browser_app_instance_tracker.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/contains.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/process/process.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_service/browser_app_instance_map.h"
@@ -128,14 +127,8 @@ BrowserAppInstanceTracker::BrowserAppInstanceTracker(
 
 BrowserAppInstanceTracker::~BrowserAppInstanceTracker() {
   BrowserList::GetInstance()->RemoveObserver(this);
-  if (activation_client_observations_.GetSourcesCount() > 0) {
-    // TODO(crbug.com/1236273): Remove when confident it does not happen.
-    base::debug::DumpWithoutCrashing();
-  }
-  if (!tracked_browsers_.empty()) {
-    // TODO(crbug.com/1236273): Remove when confident it does not happen.
-    base::debug::DumpWithoutCrashing();
-  }
+  DCHECK_EQ(activation_client_observations_.GetSourcesCount(), 0u);
+  DCHECK(tracked_browsers_.empty());
   DCHECK(observers_.empty());
 }
 
@@ -224,17 +217,11 @@ void BrowserAppInstanceTracker::OnWindowActivated(ActivationReason reason,
 }
 
 void BrowserAppInstanceTracker::OnBrowserAdded(Browser* browser) {
-  // TODO(crbug.com/1236273): Remove when confident it does not happen.
-  if (base::Contains(tracked_browsers_, browser)) {
-    base::debug::DumpWithoutCrashing();
-  }
+  DCHECK(!base::Contains(tracked_browsers_, browser));
 }
 
 void BrowserAppInstanceTracker::OnBrowserRemoved(Browser* browser) {
-  // TODO(crbug.com/1236273): Remove when confident it does not happen.
-  if (base::Contains(tracked_browsers_, browser)) {
-    base::debug::DumpWithoutCrashing();
-  }
+  DCHECK(!base::Contains(tracked_browsers_, browser));
 }
 
 void BrowserAppInstanceTracker::OnAppUpdate(const AppUpdate& update) {
