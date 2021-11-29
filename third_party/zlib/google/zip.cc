3a1646bcd58df9f8d87bedfd1e468cd2c393e823
François Degros <fdegros@chromium.org>
2021-11-29 00:49:43 +0000

    [zip] Add ZipParams::continue_on_error
    BUG=chromium:1062158
    TEST=zlib_unittests
    
    Change-Id: I9300616a8c7724319e6835855625debcbf3b3246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304996
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945823}

--- third_party/zlib/google/zip.cc
+++ third_party/zlib/google/zip.cc
@@ -116,7 +116,8 @@ class DirectFileAccessor : public FileAccessor {
 
 std::ostream& operator<<(std::ostream& out, const Progress& progress) {
   return out << progress.bytes << " bytes, " << progress.files << " files, "
-             << progress.directories << " dirs";
+             << progress.directories << " dirs, " << progress.errors
+             << " errors";
 }
 
 bool Zip(const ZipParams& params) {
@@ -144,6 +145,7 @@ bool Zip(const ZipParams& params) {
   zip_writer->SetProgressCallback(params.progress_callback,
                                   params.progress_period);
   zip_writer->SetRecursive(params.recursive);
+  zip_writer->ContinueOnError(params.continue_on_error);
 
   if (!params.include_hidden_files || params.filter_callback)
     zip_writer->SetFilterCallback(base::BindRepeating(
