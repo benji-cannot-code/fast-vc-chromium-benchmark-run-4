32ab061f55467e8c03cb83d42598123a742ebfce
Michael Lippautz <mlippautz@chromium.org>
2021-11-29 13:59:51 +0000

    heap: Fix regression when querying sweeping state
    Avoid indirection through ThreadState when querying sweeping state.
    
    Bug: chromium:1273928
    Change-Id: I879bfc250d4678fd409b1c85d1b2801cf260bf96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3302810
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945939}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -117,6 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/property_tree_state.h"
 #include "third_party/blink/renderer/platform/graphics/touch_action.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
+#include "third_party/blink/renderer/platform/heap/thread_state_storage.h"
 #include "third_party/blink/renderer/platform/instrumentation/instance_counters.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
@@ -4056,7 +4057,7 @@ void LayoutObject::DestroyAndCleanupAnonymousWrappers(
 void LayoutObject::Destroy() {
   NOT_DESTROYED();
   CHECK(g_allow_destroying_layout_object_in_finalizer ||
-        !ThreadState::Current()->InAtomicSweepingPause());
+        !ThreadState::IsSweepingOnOwningThread(*ThreadStateStorage::Current()));
 
   // Mark as being destroyed to avoid trouble with merges in |RemoveChild()| and
   // other house keepings.
