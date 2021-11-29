538f500bfdf1d1804dc771694e4ac8dc151cd1c2
Greg Thompson <grt@chromium.org>
2021-11-29 11:09:12 +0000

    Revert "Initial support for text-decoration-line: spelling|grammar-error"
    This reverts commit 643df2d079fac73cb420e6cd074c79d5a739f63d.
    
    Reason for revert: text-decoration-line-grammar-error.html and text-decoration-line-spelling-error.html fail consistently on Mac10.12 Tests
    
    Original change's description:
    > Initial support for text-decoration-line: spelling|grammar-error
    >
    > This adds the initial support for spelling-error and grammar-error
    > values of text-decoration-line property.
    >
    > Main changes are in TextDecorationInfo, where we need to support
    > the new values and paint the decoration correctly.
    > In Mac platform we use a dotted decoration to match
    > the platform conventions.
    > In other platforms we use a wavy decoration, but we modify it
    > so it looks similar to the spelling and grammar markers
    > in Microsoft Word.
    >
    > It's important to note that this is just an intermediate step,
    > as this is different than how we currently paint
    > the spelling and grammar errors in DocumentMarkerPainter.
    > The idea would be make DocumentMarkerPainter use CSS text decorations
    > and then use this new code added here.
    >
    > There are some known issues:
    > * The underline offset is not the same than in Microsoft Word,
    >   but that's because of a problem with regular underlines
    >   not using the font metrics to compute that offset (crbug.com/1273042).
    > * We should allow to tweak the color with text-decoration-color,
    >   added a TODO about that.
    >
    > BUG=1163436
    >
    > Change-Id: I117ad38fe3fc805619eb47b1df2f48c9d7c9a351
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297885
    > Commit-Queue: Delan Azabani <dazabani@igalia.com>
    > Reviewed-by: Delan Azabani <dazabani@igalia.com>
    > Reviewed-by: Stephen Chenney <schenney@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#945876}
    
    Bug: 1163436
    Change-Id: I2622bde37f2f8a7f725a1a06be8d34acdf9f728b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306392
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Owners-Override: Greg Thompson <grt@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#945903}

--- third_party/blink/renderer/core/paint/text_decoration_info.cc
+++ third_party/blink/renderer/core/paint/text_decoration_info.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/build_config.h"
 #include "third_party/blink/renderer/core/paint/text_decoration_info.h"
 #include "third_party/blink/renderer/core/paint/text_paint_style.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
@@ -144,18 +143,8 @@ TextDecorationInfo::TextDecorationInfo(
 
   for (const AppliedTextDecoration& decoration :
        style_.AppliedTextDecorations()) {
-    if (EnumHasFlags(decoration.Lines(), TextDecorationLine::kSpellingError) ||
-        EnumHasFlags(decoration.Lines(), TextDecorationLine::kGrammarError)) {
-      // Spelling and grammar error thickness doesn't depend on the font size.
-#if defined(OS_MAC)
-      applied_decorations_thickness_.push_back(2.f);
-#else
-      applied_decorations_thickness_.push_back(1.f);
-#endif
-    } else {
-      applied_decorations_thickness_.push_back(ComputeUnderlineThickness(
-          decoration.Thickness(), decorating_box_style));
-    }
+    applied_decorations_thickness_.push_back(ComputeUnderlineThickness(
+        decoration.Thickness(), decorating_box_style));
   }
   DCHECK_EQ(style_.AppliedTextDecorations().size(),
             applied_decorations_thickness_.size());
@@ -174,8 +163,6 @@ void TextDecorationInfo::SetLineData(TextDecorationLine line,
   int wavy_offset_factor;
   switch (line) {
     case TextDecorationLine::kUnderline:
-    case TextDecorationLine::kSpellingError:
-    case TextDecorationLine::kGrammarError:
       double_offset = double_offset_from_thickness;
       wavy_offset_factor = 1;
       break;
@@ -197,7 +184,6 @@ void TextDecorationInfo::SetLineData(TextDecorationLine line,
       NOTREACHED();
   }
 
-  line_data_.line = line;
   line_data_.line_offset = line_offset;
   line_data_.double_offset = double_offset;
   line_data_.wavy_offset_factor = wavy_offset_factor;
@@ -216,25 +202,10 @@ void TextDecorationInfo::SetLineData(TextDecorationLine line,
 }
 
 ETextDecorationStyle TextDecorationInfo::DecorationStyle() const {
-  if (IsSpellingOrGrammarError()) {
-#if defined(OS_MAC)
-    return ETextDecorationStyle::kDotted;
-#else
-    return ETextDecorationStyle::kWavy;
-#endif
-  }
-
   return style_.AppliedTextDecorations()[decoration_index_].Style();
 }
 
 Color TextDecorationInfo::LineColor() const {
-  // TODO(rego): Allow customize the spelling and grammar error color with
-  // text-decoration-color property.
-  if (line_data_.line == TextDecorationLine::kSpellingError)
-    return LayoutTheme::GetTheme().PlatformSpellingMarkerUnderlineColor();
-  if (line_data_.line == TextDecorationLine::kGrammarError)
-    return LayoutTheme::GetTheme().PlatformGrammarMarkerUnderlineColor();
-
   // Find the matched normal and selection |AppliedTextDecoration|
   // and use the text-decoration-color from selection when it is.
   if (selection_text_decoration_ &&
@@ -346,9 +317,6 @@ float TextDecorationInfo::ControlPointDistanceFromResolvedThickness() const {
   // Distance between decoration's axis and Bezier curve's control points. The
   // height of the curve is based on this distance. Increases the curve's height
   // as strokeThickness increases to make the curve look better.
-  if (IsSpellingOrGrammarError())
-    return 5;
-
   return 3.5 * WavyDecorationSizing();
 }
 
@@ -356,9 +324,6 @@ float TextDecorationInfo::StepFromResolvedThickness() const {
   // Increment used to form the diamond shape between start point (p1), control
   // points and end point (p2) along the axis of the decoration. Makes the curve
   // wider as strokeThickness increases to make the curve look better.
-  if (IsSpellingOrGrammarError())
-    return 3;
-
   return 2.5 * WavyDecorationSizing();
 }
 
@@ -402,10 +367,6 @@ Path TextDecorationInfo::PrepareWavyStrokePath() const {
   float wave_offset = DoubleOffset() * line_data_.wavy_offset_factor;
 
   float control_point_distance = ControlPointDistanceFromResolvedThickness();
-  // For spelling and grammar errors we invert the control_point_distance to get
-  // a result closer to Microsoft Word circa 2021.
-  if (IsSpellingOrGrammarError())
-    control_point_distance = -control_point_distance;
   float step = StepFromResolvedThickness();
 
   gfx::PointF start_point = StartPoint();
@@ -414,15 +375,10 @@ Path TextDecorationInfo::PrepareWavyStrokePath() const {
   // AppliedDecorationPainter::StrokeWavyTextDecoration().
   // Offset the start point, so the beizer curve starts before the current line,
   // that way we can clip it exactly the same way in both ends.
-  // For spelling and grammar errors we offset an extra half step, to get a
-  // result closer to Microsoft Word circa 2021.
-  float start_offset = (IsSpellingOrGrammarError() ? -2.5 : -2) * step;
-  gfx::PointF p1(start_point + gfx::Vector2dF(start_offset, wave_offset));
+  gfx::PointF p1(start_point + gfx::Vector2dF(-2 * step, wave_offset));
   // Increase the width including the previous offset, plus an extra wave to be
   // painted after the line.
-  float extra_width = (IsSpellingOrGrammarError() ? 4.5 : 4) * step;
-  gfx::PointF p2(start_point +
-                 gfx::Vector2dF(width_ + extra_width, wave_offset));
+  gfx::PointF p2(start_point + gfx::Vector2dF(width_ + 4 * step, wave_offset));
 
   GraphicsContext::AdjustLineToPixelBoundaries(p1, p2, ResolvedThickness());
 
