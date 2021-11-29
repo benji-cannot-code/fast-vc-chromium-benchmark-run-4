eb8433fdf7168361dbf6243cfa40aebf86002701
Daniel Libby <dlibby@microsoft.com>
2021-11-29 21:30:10 +0000

    Always recurse for described-by text description
    Not propagating the aria_label_or_description_root into and through
    TextFromDescendants doesn't quite match the accname spec. Namely, we
    should recurse into children even if the node doesn't support
    name-from-contents.
    
    AXRelnotes: Name-from-contents algorithm when computing labelledby or
    description text will now recurse into elements that would otherwise be
    ineligible for contributing to the text alternative of an element, per
    step 2H under https://w3c.github.io/accname/#computation-steps
    
    R=aleventhal@chromium.org
    
    Bug: 1271756
    Change-Id: I5e7c383aeabc13f48cd7b98521a47410221b5181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292342
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946146}

--- content/browser/accessibility/dump_accessibility_node_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_node_browsertest.cc
@@ -303,6 +303,12 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest,
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest,
+                       DescFromContentOfDescribedbyElementRecursive) {
+  RunAccNameTest(FILE_PATH_LITERAL(
+      "desc-from-content-of-describedby-element-recursive.html"));
+}
+
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest,
                        DescImgAltDescribedbyHidden) {
   RunAccNameTest(FILE_PATH_LITERAL("desc-img-alt-describedby-hidden.html"));
 }
@@ -863,6 +869,12 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest, NameRadioTitle) {
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest, NameResetButton) {
   RunAccNameTest(FILE_PATH_LITERAL("name-reset-button.html"));
+}
+
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest,
+                       NameTextContentOfLabelledByElementRecursive) {
+  RunAccNameTest(FILE_PATH_LITERAL(
+      "name-text-content-of-labelledby-element-recursive.html"));
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityAccNameTest, NameTextCssAfterInLabel) {
