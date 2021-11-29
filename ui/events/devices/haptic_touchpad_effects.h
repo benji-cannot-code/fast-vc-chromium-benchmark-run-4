23473c5f7b7fc02cfe9eb1e61730ae1cd148293c
Sean O'Brien <seobrien@google.com>
2021-11-29 15:27:55 +0000

    Add haptic touchpad effects
    Add more effects for haptic touchpad.
    
    Bug: b/204903440
    Change-Id: Ibded9140f278c94ab912dc3d6f600aaaf73f8bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289666
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Sean O'Brien <seobrien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945963}

--- ui/events/devices/haptic_touchpad_effects.h
+++ ui/events/devices/haptic_touchpad_effects.h
@@ -9,11 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 enum class HapticTouchpadEffect {
-  kSnap = 0,    // UI feedback for snapping into place.
-  kKnock = 1,   // UI feedback for reaching a limit or boundary.
-  kTick = 2,    // UI feedback for discrete state changes.
-  kPress = 3,   // Standard touchpad button down effect.
-  kRelease = 4  // Standard touchpad button up effect.
+  kSnap = 0,         // UI feedback for snapping into place.
+  kSnapReverse = 1,  // UI feedback for snapping out of place.
+  kKnock = 2,        // UI feedback for reaching a limit or boundary.
+  kTick = 3,         // UI feedback for discrete state changes.
+  kToggleOn = 4,     // UI feedback for activating a feature.
+  kToggleOff = 5,    // UI feedback for deactivating a feature.
+  kPress = 6,        // Standard touchpad button down effect.
+  kRelease = 7,      // Standard touchpad button up effect.
+  kDeepPress = 8,    // Deeper (more force) touchpad button down effect.
+  kDeepRelease = 9   // Deeper (more force) touchpad button up effect.
 };
 
 // Modifies the intensity of effects.
