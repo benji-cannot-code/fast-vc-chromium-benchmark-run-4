eb4473377064ae1fe98a668cedaa65fb096da3a3
Lei Zhang <thestig@chromium.org>
2021-11-29 20:41:19 +0000

    Remove unneeded includes from mojo/public/c/system/functions.h.
    Move to functions.cc if needed.
    
    Bug: 242216
    Change-Id: I0fe40395621bb8f24ef6d24cc190f91da275b798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290946
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946119}

--- mojo/public/c/system/functions.h
+++ mojo/public/c/system/functions.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_PUBLIC_C_SYSTEM_FUNCTIONS_H_
 #define MOJO_PUBLIC_C_SYSTEM_FUNCTIONS_H_
 
-#include <stddef.h>
-#include <stdint.h>
-
-#include "mojo/public/c/system/invitation.h"
 #include "mojo/public/c/system/system_export.h"
 #include "mojo/public/c/system/types.h"
 
