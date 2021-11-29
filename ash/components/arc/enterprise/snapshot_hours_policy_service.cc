eb7682eee24c702f6699fe62d0a250d51753c959
calamity <calamity@chromium.org>
2021-11-29 02:53:25 +0000

    Revert "Move //{chromeos => ash/components}/policy"
    This reverts commit f12c2d551fcc48488a97e882c12796b00c92d34e.
    
    Reason for revert: Causing archive-rel bots to fail due to not being
    able to find chromeos/policy/OWNERS.
    Example: https://ci.chromium.org/ui/p/chromium/builders/ci/win32-archive-dbg/21367/overview
    
    Original change's description:
    > Move //{chromeos => ash/components}/policy
    >
    > This CL is part of the Chrome OS source code directory migration:
    > https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    >
    > Bug: 1164001
    > Change-Id: I796775ee37e3f17210adfe833a0ca4c6cf8dcdaa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301626
    > Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Owners-Override: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#945835}
    
    Bug: 1164001
    Change-Id: I76dfff04f317da8e460fa3f515e33bad8192eccd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3305645
    Auto-Submit: calamity <calamity@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: calamity <calamity@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945837}

--- ash/components/arc/enterprise/snapshot_hours_policy_service.cc
+++ ash/components/arc/enterprise/snapshot_hours_policy_service.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/components/arc/arc_prefs.h"
-#include "ash/components/policy/weekly_time/time_utils.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/ignore_result.h"
@@ -17,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_clock.h"
 #include "base/time/tick_clock.h"
 #include "base/values.h"
+#include "chromeos/policy/weekly_time/time_utils.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
 
