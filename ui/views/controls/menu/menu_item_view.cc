a4c31f2f3a6aba4dc3f8ca4f0ee46110835d8df9
Tom <tluk@chromium.org>
2021-11-29 17:28:46 +0000

    Only update selection based state for MenuItemViews in a menu
    This CL updates MenuItemView such that an item's selection based
    state is only updated if the item belongs to a views::Widget and
    is also not in a removed but not-yet-deleted state.
    
    Currently MenuItemView::RemoveAllMenuItems() will remove all of the
    first level menu items from their hosting Submenu view and add them
    to |removed_items_|. These items will be deleted in the next call to
    MenuItemView::ChildrenChanged().
    
    Between the call to RemoveAllMenuItems() and ChildrenChanged(), the
    menu's model can update (as is done by RecentTabsMenuModelDelegate).
    ChildrenChanged() will then clear selection from the menu items
    before they are finally deleted.
    
    Currently, updating selection of the removed but not-yet-deleted
    items can lead to state updates that query the now updated model.
    This can result in crashes when stale model references are used by
    the menu items.
    
    This CL addresses this by preventing updates to selection based state
    for menu items in the removed but not-yet-deleted state.
    
    Bug: 1259377
    Change-Id: Ida90a0cafdf71a6410022d2202195feb0d2d5292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301654
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946013}

--- ui/views/controls/menu/menu_item_view.cc
+++ ui/views/controls/menu/menu_item_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/containers/adapters.h"
+#include "base/containers/contains.h"
 #include "base/i18n/case_conversion.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/accessibility/ax_action_data.h"
@@ -158,7 +159,8 @@ void MenuItemView::ChildPreferredSizeChanged(View* child) {
 void MenuItemView::OnThemeChanged() {
   View::OnThemeChanged();
   // Force updating as the colors may have changed.
-  UpdateSelectionBasedState(ShouldPaintAsSelected(PaintMode::kNormal));
+  if (!IsScheduledForDeletion())
+    UpdateSelectionBasedState(ShouldPaintAsSelected(PaintMode::kNormal));
 }
 
 void MenuItemView::ViewHierarchyChanged(
@@ -1465,8 +1467,10 @@ bool MenuItemView::HasChecksOrRadioButtons() const {
 }
 
 void MenuItemView::UpdateSelectionBasedStateIfChanged(PaintMode mode) {
-  // Selection state depends upon NativeTheme.
-  if (!GetWidget())
+  // Selection state depends upon NativeTheme. Selection based state could also
+  // depend on the menu model so avoid the update if the item is scheduled to be
+  // deleted.
+  if (!GetWidget() || IsScheduledForDeletion())
     return;
 
   const bool paint_as_selected = ShouldPaintAsSelected(mode);
@@ -1507,6 +1511,12 @@ bool MenuItemView::ShouldPaintAsSelected(PaintMode mode) const {
   return (parent_menu_item_ && mode == PaintMode::kNormal && IsSelected() &&
           parent_menu_item_->GetSubmenu()->GetShowSelection(this) &&
           (NonIconChildViewsCount() == 0));
+}
+
+bool MenuItemView::IsScheduledForDeletion() const {
+  const MenuItemView* parent = GetParentMenuItem();
+  return parent && (base::Contains(parent->removed_items_, this) ||
+                    parent->IsScheduledForDeletion());
 }
 
 BEGIN_METADATA(MenuItemView, View)
