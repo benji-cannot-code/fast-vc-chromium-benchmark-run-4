86a52928523440569e4f19983eb2330b7b2c5a31
Rune Lillesveen <futhark@chromium.org>
2021-11-29 12:18:52 +0000

    [@container] Only allow query containers via container-type
    Deducing query containers via the contain property is not correct per
    spec.
    
    Bug: 1269197
    Change-Id: I69a5718c559d1ff6c75cf9fc5f813d87351249f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304212
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945918}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2080,9 +2080,7 @@ class ComputedStyle : public ComputedStyleBase,
   CORE_EXPORT bool ShouldApplyAnyContainment(const Element& element) const;
 
   bool IsContainerForContainerQueries() const {
-    return (StyleType() == kPseudoIdNone) &&
-           (ContainsStyle() && ContainsLayout() &&
-            (ContainsInlineSize() || ContainsBlockSize()));
+    return StyleType() == kPseudoIdNone && IsInlineOrBlockSizeContainer();
   }
 
   // Display utility functions.
