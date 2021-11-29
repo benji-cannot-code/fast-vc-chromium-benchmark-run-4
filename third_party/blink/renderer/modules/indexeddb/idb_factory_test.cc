1a89c45e1e9c7a43b2176db5f178d4941d0d75a5
Jinho Bang <zino@chromium.org>
2021-11-29 18:25:46 +0000

    Use NewScriptFunction instead of ScriptFunction in modules/indexeddb
    ScriptFunction was deprecated since https://crrev.com/c/2549321.
    So, this patch migrates it to NewScriptFunction.
    
    Bug: 1152249
    Change-Id: I5d3126c85176db2f1c0e059e3633b0aab428e3cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3302095
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946036}

--- third_party/blink/renderer/modules/indexeddb/idb_factory_test.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_factory_test.cc
@@ -32,24 +32,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace {
 
-class TestHelperFunction : public ScriptFunction {
+class TestHelperFunction : public NewScriptFunction::Callable {
  public:
-  static v8::Local<v8::Function> CreateFunction(ScriptState* script_state,
-                                                bool* called_flag) {
-    auto* self =
-        MakeGarbageCollected<TestHelperFunction>(script_state, called_flag);
-    return self->BindToV8Function();
-  }
+  explicit TestHelperFunction(bool* called_flag) : called_flag_(called_flag) {}
 
-  TestHelperFunction(ScriptState* script_state, bool* called_flag)
-      : ScriptFunction(script_state), called_flag_(called_flag) {}
-
- private:
-  ScriptValue Call(ScriptValue value) override {
+  ScriptValue Call(ScriptState* script_state, ScriptValue value) override {
     *called_flag_ = true;
     return value;
   }
 
+ private:
   bool* called_flag_;
 };
 
@@ -143,9 +135,12 @@ TEST_F(IDBFactoryTest, WebIDBGetDBInfoCallbacksResolvesPromise) {
 
   bool on_fulfilled = false;
   bool on_rejected = false;
-  promise.Then(
-      TestHelperFunction::CreateFunction(scope.GetScriptState(), &on_fulfilled),
-      TestHelperFunction::CreateFunction(scope.GetScriptState(), &on_rejected));
+  promise.Then(MakeGarbageCollected<NewScriptFunction>(
+                   scope.GetScriptState(),
+                   MakeGarbageCollected<TestHelperFunction>(&on_fulfilled)),
+               MakeGarbageCollected<NewScriptFunction>(
+                   scope.GetScriptState(),
+                   MakeGarbageCollected<TestHelperFunction>(&on_rejected)));
 
   EXPECT_FALSE(on_fulfilled);
   EXPECT_FALSE(on_rejected);
@@ -188,9 +183,12 @@ TEST_F(IDBFactoryTest, WebIDBGetDBNamesCallbacksRejectsPromise) {
 
   bool on_fulfilled = false;
   bool on_rejected = false;
-  promise.Then(
-      TestHelperFunction::CreateFunction(scope.GetScriptState(), &on_fulfilled),
-      TestHelperFunction::CreateFunction(scope.GetScriptState(), &on_rejected));
+  promise.Then(MakeGarbageCollected<NewScriptFunction>(
+                   scope.GetScriptState(),
+                   MakeGarbageCollected<TestHelperFunction>(&on_fulfilled)),
+               MakeGarbageCollected<NewScriptFunction>(
+                   scope.GetScriptState(),
+                   MakeGarbageCollected<TestHelperFunction>(&on_rejected)));
 
   EXPECT_FALSE(on_fulfilled);
   EXPECT_FALSE(on_rejected);
