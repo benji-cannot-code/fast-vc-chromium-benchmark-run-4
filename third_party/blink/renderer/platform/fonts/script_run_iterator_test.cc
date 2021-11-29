f8a0192d891efee2c2f83261146525a525ede3db
Koji Ishii <kojii@chromium.org>
2021-11-29 21:42:19 +0000

    Use the East Asian Width property when matching parentheses
    When r912274 crrev.com/c/3076598 normalized CJK scripts, it
    relied on the CJK punctuation characters to have CJK scripts
    in the script extension property.
    
    However, not all CJK punctuation characters have them.
    Characters in the fullwidth/halfwidth forms block must have
    the same properties as their canonical equivalent[1] code
    points.
    
    This patch uses the East Asian Width property[2] to normalize
    CJK scripts for such code points, to avoid such code points to
    be classified as the non-CJK scripts when preceded by the
    non-CJK scripts.
    
    [1]: https://unicode.org/reports/tr15/#Canon_Compat_Equivalence
    [2]: https://www.unicode.org/reports/tr11/
    
    Bug: 1252414
    Change-Id: I81713e1d2a937afb79f58e1cdde570b71edb8856
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3291121
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946156}

--- third_party/blink/renderer/platform/fonts/script_run_iterator_test.cc
+++ third_party/blink/renderer/platform/fonts/script_run_iterator_test.cc
@@ -566,6 +566,28 @@ TEST_F(ScriptRunIteratorTest, CJKConsecutiveParens10) {
                      {"」", USCRIPT_BOPOMOFO}});
 }
 
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParensLatin1) {
+  CHECK_SCRIPT_RUNS({{"「", USCRIPT_BOPOMOFO},  // See CJKConsecutiveParens4
+                     {"A", USCRIPT_LATIN},
+                     {"「", USCRIPT_BOPOMOFO},
+                     {"A", USCRIPT_LATIN},
+                     {"」」", USCRIPT_BOPOMOFO}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParensLatin2) {
+  CHECK_SCRIPT_RUNS({{"「", USCRIPT_BOPOMOFO},  // See CJKConsecutiveParens4
+                     {"A", USCRIPT_LATIN},
+                     {"（", USCRIPT_BOPOMOFO},
+                     {"A", USCRIPT_LATIN},
+                     {"）」", USCRIPT_BOPOMOFO}});
+}
+
+TEST_F(ScriptRunIteratorTest, CJKConsecutiveParensLatin3) {
+  CHECK_SCRIPT_RUNS({{"「", USCRIPT_BOPOMOFO},  // See CJKConsecutiveParens4
+                     {"A", USCRIPT_LATIN},
+                     {"（国）」", USCRIPT_HAN}});
+}
+
 // Emojies are resolved to the leading script.
 TEST_F(ScriptRunIteratorTest, EmojiCommon) {
   CHECK_SCRIPT_RUNS({{"百家姓🌱🌲🌳🌴", USCRIPT_HAN}});
