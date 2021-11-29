fe4f52aa88dd5e2653adb1320cb2bb7667a2444e
dpapad <dpapad@chromium.org>
2021-11-29 19:24:40 +0000

    ReadLater/SidePanel: Serve tests from chrome://webui-test/ data source.
    This is in preparation of migrating tests to TypeScript.
    
    Bug: 1271509,1260297
    Change-Id: Ia0027b73eceb582fcd635ccc6acce1f3fde0708a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300502
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946065}

--- chrome/test/data/webui/read_later/side_panel/side_panel_browsertest.js
+++ chrome/test/data/webui/read_later/side_panel/side_panel_browsertest.js
@@ -32,7 +32,7 @@ class SidePanelBrowserTest extends PolymerTest {
 var SidePanelAppTest = class extends SidePanelBrowserTest {
   /** @override */
   get browsePreload() {
-    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/side_panel_app_test.js';
+    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/side_panel_app_test.js&host=webui-test';
   }
 };
 
@@ -43,7 +43,7 @@ TEST_F('SidePanelAppTest', 'All', function() {
 var SidePanelBookmarksListTest = class extends SidePanelBrowserTest {
   /** @override */
   get browsePreload() {
-    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/bookmarks_list_test.js';
+    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/bookmarks_list_test.js&host=webui-test';
   }
 };
 
@@ -55,7 +55,7 @@ TEST_F('SidePanelBookmarksListTest', 'All', function() {
 var SidePanelBookmarkFolderTest = class extends SidePanelBrowserTest {
   /** @override */
   get browsePreload() {
-    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/bookmark_folder_test.js';
+    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/bookmark_folder_test.js&host=webui-test';
   }
 };
 
@@ -67,7 +67,7 @@ TEST_F('SidePanelBookmarkFolderTest', 'All', function() {
 var SidePanelBookmarksDragManagerTest = class extends SidePanelBrowserTest {
   /** @override */
   get browsePreload() {
-    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/bookmarks_drag_manager_test.js';
+    return 'chrome://read-later.top-chrome/test_loader.html?module=read_later/side_panel/bookmarks_drag_manager_test.js&host=webui-test';
   }
 };
 
