d29cf69c897fe7158fae64f1faacd3f409babe13
Ian Kilpatrick <ikilpatrick@chromium.org>
2021-11-29 20:20:57 +0000

    [flex] Clamp available sizes to zero.
    Scrollbars may be present on a box. When subtracting
    BorderScrollbarPadding we almost always need to clamp to zero, as the
    border-box size - bsp may be negative.
    
    Bug: 1274476
    Change-Id: I55322f5c5bc5a5bbe9fefc1eb1403677044b9443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307128
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946104}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -1027,9 +1027,11 @@ void NGFlexLayoutAlgorithm::ApplyFinalAlignmentAndReversals(
                               : line_contexts[0].cross_axis_offset_;
 
   LayoutUnit final_content_main_size =
-      container_builder_.InlineSize() - BorderScrollbarPadding().InlineSum();
+      (container_builder_.InlineSize() - BorderScrollbarPadding().InlineSum())
+          .ClampNegativeToZero();
   LayoutUnit final_content_cross_size =
-      total_block_size_ - BorderScrollbarPadding().BlockSum();
+      (total_block_size_ - BorderScrollbarPadding().BlockSum())
+          .ClampNegativeToZero();
   if (is_column_)
     std::swap(final_content_main_size, final_content_cross_size);
 
@@ -1062,10 +1064,12 @@ bool NGFlexLayoutAlgorithm::GiveItemsFinalPositionAndSize(
   LayoutUnit final_content_cross_size;
   if (is_column_) {
     final_content_cross_size =
-        container_builder_.InlineSize() - BorderScrollbarPadding().InlineSum();
+        (container_builder_.InlineSize() - BorderScrollbarPadding().InlineSum())
+            .ClampNegativeToZero();
   } else {
     final_content_cross_size =
-        total_block_size_ - BorderScrollbarPadding().BlockSum();
+        (total_block_size_ - BorderScrollbarPadding().BlockSum())
+            .ClampNegativeToZero();
   }
 
   absl::optional<LayoutUnit> fallback_baseline;
