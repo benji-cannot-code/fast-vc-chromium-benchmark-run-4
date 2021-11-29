dc8063dbc7291e7625792ccb0f139015189ccbb6
Bo Liu <boliu@chromium.org>
2021-11-29 19:54:39 +0000

    android: Treat detached tab as not interactable
    This avoids a crash that tries to access the null window when creating a
    popup window.
    
    Bug: 1268766
    Change-Id: I90bb837dafde0ba8572222ad0d663dd25ec16db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300865
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946085}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
@@ -346,6 +346,8 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
                 observer.onActivityAttachmentChanged(this, window);
             }
         }
+
+        updateInteractableState();
     }
 
     /**
@@ -1484,7 +1486,8 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
      */
     private void updateInteractableState() {
         boolean currentState = !mIsHidden && !isFrozen()
-                && (mIsViewAttachedToWindow || VrModuleProvider.getDelegate().isInVr());
+                && (mIsViewAttachedToWindow || VrModuleProvider.getDelegate().isInVr())
+                && !isDetached(this);
 
         if (currentState == mInteractableState) return;
 
