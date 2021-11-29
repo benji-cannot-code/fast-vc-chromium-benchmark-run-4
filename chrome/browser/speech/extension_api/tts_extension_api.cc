27855a348b34be9f45f9709338efd7f19a111453
Maks Orlovich <morlovich@chromium.org>
2021-11-29 21:01:12 +0000

    Code health: port away from deprecated ListValue::GetString
    This is a portion for /chrome/browser/speech/extension_api.
    (And in some cases also adjust away from other deprecated Value APIs,
     when convenient/nearby)
    
    This CL was uploaded by git cl split.
    
    R=dtseng@chromium.org
    
    Bug: 1187090
    Change-Id: I8d674fa937a37bb99a602bf31be4caf3ae5cd6cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306897
    Auto-Submit: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946128}

--- chrome/browser/speech/extension_api/tts_extension_api.cc
+++ chrome/browser/speech/extension_api/tts_extension_api.cc
@@ -260,10 +260,12 @@ ExtensionFunction::ResponseAction TtsSpeakFunction::Run() {
     base::ListValue* list;
     EXTENSION_FUNCTION_VALIDATE(
         options->GetList(constants::kRequiredEventTypesKey, &list));
-    for (size_t i = 0; i < list->GetList().size(); ++i) {
-      std::string event_type;
-      if (list->GetString(i, &event_type))
-        required_event_types.insert(TtsEventTypeFromString(event_type.c_str()));
+    for (const base::Value& i : list->GetList()) {
+      const std::string* event_type = i.GetIfString();
+      if (event_type) {
+        required_event_types.insert(
+            TtsEventTypeFromString(event_type->c_str()));
+      }
     }
   }
 
@@ -272,10 +274,10 @@ ExtensionFunction::ResponseAction TtsSpeakFunction::Run() {
     base::ListValue* list;
     EXTENSION_FUNCTION_VALIDATE(
         options->GetList(constants::kDesiredEventTypesKey, &list));
-    for (size_t i = 0; i < list->GetList().size(); ++i) {
-      std::string event_type;
-      if (list->GetString(i, &event_type))
-        desired_event_types.insert(TtsEventTypeFromString(event_type.c_str()));
+    for (const base::Value& i : list->GetList()) {
+      const std::string* event_type = i.GetIfString();
+      if (event_type)
+        desired_event_types.insert(TtsEventTypeFromString(event_type->c_str()));
     }
   }
 
