368cdf9d3c4d1c57da01380d7cb339cd0bcf9a2a
Yao Li <yaohuali@google.com>
2021-11-29 21:32:22 +0000

    Reland "arc: [25/N] Move //{ => ash}/components/arc/mojom : unit tests and small mojom targets"
    This is a reland of aead9e9600dbce33d3bf0695d5d1b2b27e28bbf3
    
    What's changed in reland:
    Fixed a few places of path updates.
    Less redirection in ash/components/arc/mojom/OWNERS.
    
    Original change's description:
    > arc: [25/N] Move //{ => ash}/components/arc/mojom : unit tests and small
    > mojom targets
    >
    > This CL moves smaller mojom targets and unit tests.
    > { => ash}/components/arc/mojom (partially)
    >
    > Bigger mojom targets will be moved in separate CLs.
    >
    > This is part of the LaCrOs directory migration
    > https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE
    >
    > Bug: b:129295708
    > Test: CI
    > Change-Id: Ic4bee431e05dbc8d635d71a9e76e5f4d539eb592
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296857
    > Commit-Queue: Yao Li <yaohuali@google.com>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Owners-Override: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#944608}
    
    Bug: b:129295708
    Change-Id: Iad7fb164356c15ced93750f171ea76f8391b42f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298686
    Commit-Queue: Yao Li <yaohuali@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946150}

--- ash/components/arc/session/arc_bridge_service.cc
+++ ash/components/arc/session/arc_bridge_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/session/arc_bridge_service.h"
 
 // These header is necessary for instantiation of ConnectionHolder.
+#include "ash/components/arc/mojom/oemcrypto.mojom.h"
 #include "components/arc/mojom/accessibility_helper.mojom.h"
 #include "components/arc/mojom/adbd.mojom.h"
 #include "components/arc/mojom/app.mojom.h"
@@ -40,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/mojom/nearby_share.mojom.h"
 #include "components/arc/mojom/net.mojom.h"
 #include "components/arc/mojom/obb_mounter.mojom.h"
-#include "components/arc/mojom/oemcrypto.mojom.h"
 #include "components/arc/mojom/payment_app.mojom.h"
 #include "components/arc/mojom/pip.mojom.h"
 #include "components/arc/mojom/policy.mojom.h"
