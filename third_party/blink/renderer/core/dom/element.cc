f608be5502a8f3a7f4303569ad53c647a9c99742
Rune Lillesveen <futhark@chromium.org>
2021-11-29 11:07:02 +0000

    [@container] Set up correct StyleRecalcContext for ::first-line
    Use originating element as the nearest possible container for
    ::first-line pseudo style.
    
    Bug: 1217976, 1273913
    Change-Id: I3681936c982ad18f73be9f798e9358270d77c168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298809
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945901}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -5695,9 +5695,8 @@ scoped_refptr<ComputedStyle> Element::UncachedStyleForPseudoElement(
   DCHECK(!RuntimeEnabledFeatures::HighlightInheritanceEnabled() ||
          !IsHighlightPseudoElement(request.pseudo_id));
 
-  // TODO(crbug.com/1145970): Use actual StyleRecalcContext.
-  StyleRecalcContext style_recalc_context;
-  return StyleForPseudoElement(style_recalc_context, request);
+  return StyleForPseudoElement(
+      StyleRecalcContext::FromInclusiveAncestors(*this), request);
 }
 
 scoped_refptr<ComputedStyle> Element::StyleForPseudoElement(
