06935dbf88a817e9cdf42a7c6c576aeaaf222ab0
Cici Ruan <cuicuiruan@google.com>
2021-11-29 20:10:20 +0000

    arc: Input overlay for supporting multi-display
    TouchInjector/EventRewriter is registered on the root window of the
    input-overlay enabled window. Once the window changes to a different
    display/root window, TouchInjector is unregistered from the original
    root window and is registered on the new root window.
    
    KeyEventSourceRewriter is added because when in multi-display mode, only
    primary root window can catch the key event. When the input-overlay
    enabled window is on the extended display, KeyEventSourceRewriter
    forwards the key event to the extended root window.
    
    Make some changes:
    Previously, InputMethodObserver is added when the first input overlay
    window shows up and is never removed. InputMethodObserver is only added
    when there is focused/registered input overlay window. The input overlay
    window is unregistered, the InputMethodObserver will be removed too.
    
    Bug: b:188452744
    Test: unittest
    Change-Id: I351536580c13c50db27ab6d2f0d2aff178b201e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3260883
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946096}

--- chrome/browser/ash/arc/input_overlay/touch_injector_unittest.cc
+++ chrome/browser/ash/arc/input_overlay/touch_injector_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "chrome/browser/ash/arc/input_overlay/actions/action_tap_key.h"
 #include "chrome/browser/ash/arc/input_overlay/input_overlay_resources_util.h"
+#include "chrome/browser/ash/arc/input_overlay/test/event_capturer.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/test/aura_test_base.h"
@@ -108,39 +109,6 @@ constexpr const char kValidJsonActionMoveKey[] =
     })json";
 }  // namespace
 
-// Records all key events for testing.
-class EventCapturer : public ui::EventHandler {
- public:
-  EventCapturer() = default;
-  EventCapturer(const EventCapturer&) = delete;
-  EventCapturer& operator=(const EventCapturer&) = delete;
-  ~EventCapturer() override = default;
-
-  void Clear() {
-    key_events_.clear();
-    touch_events_.clear();
-  }
-
-  std::vector<std::unique_ptr<ui::KeyEvent>>& key_events() {
-    return key_events_;
-  }
-  std::vector<std::unique_ptr<ui::TouchEvent>>& touch_events() {
-    return touch_events_;
-  }
-
- private:
-  void OnKeyEvent(ui::KeyEvent* event) override {
-    key_events_.emplace_back(std::make_unique<ui::KeyEvent>(*event));
-  }
-
-  void OnTouchEvent(ui::TouchEvent* event) override {
-    touch_events_.emplace_back(std::make_unique<ui::TouchEvent>(*event));
-  }
-
-  std::vector<std::unique_ptr<ui::KeyEvent>> key_events_;
-  std::vector<std::unique_ptr<ui::TouchEvent>> touch_events_;
-};
-
 class TouchInjectorTest : public views::ViewsTestBase {
  protected:
   TouchInjectorTest()
@@ -182,7 +150,7 @@ class TouchInjectorTest : public views::ViewsTestBase {
 
   aura::Window* root_window() { return GetContext(); }
 
-  EventCapturer event_capturer_;
+  input_overlay::test::EventCapturer event_capturer_;
   std::unique_ptr<ui::test::EventGenerator> event_generator_;
 
   std::unique_ptr<views::Widget> widget_;
