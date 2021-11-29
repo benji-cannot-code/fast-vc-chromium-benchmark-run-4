bab6c1193b848f88a5550863ee80914f8b45a18f
Michael Martis <martis@chromium.org>
2021-11-29 04:45:24 +0000

    [Snooping protection] Added user-facing flag.
    In preparation for rolling out user testing of snooping protection, this
    CL adds a Chrome flag that can be used to enable or disable the feature.
    
    Bug: chromium:1241704
    Change-Id: I302e7c958d5fde7d178f20e1d52077de6d806442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295293
    Reviewed-by: Charles Zhao <charleszhao@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#945852}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2934,6 +2934,9 @@ extern const char kShowTouchHudDescription[];
 extern const char kSmartLockUIRevampName[];
 extern const char kSmartLockUIRevampDescription[];
 
+extern const char kSnoopingProtectionName[];
+extern const char kSnoopingProtectionDescription[];
+
 extern const char kSpectreVariant2MitigationName[];
 extern const char kSpectreVariant2MitigationDescription[];
 
