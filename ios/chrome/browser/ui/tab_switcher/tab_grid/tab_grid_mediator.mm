f7010fca83ceb41183ed367721957e4302fcfbb2
David Maunder <davidjm@chromium.org>
2021-11-30 02:56:27 +0000

    Gate price drop tap through logging with flag
    CL 1245031 added metrics for price drop/no price drop annotation
    tap through in the Tab Grid Switcher. When I was reviewing it
    for a merge request, it occurred to me we should gate the call
    to LogPriceDropMetrics by the price drops annotations flag for
    additional safety.
    
    Bug: 1245031
    Change-Id: I252a50b9b4dcaf45e592d42fa8fb8fef29032b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307635
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946288}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_mediator.mm
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #import "ios/chrome/browser/chrome_url_util.h"
+#import "ios/chrome/browser/commerce/price_alert_util.h"
 #import "ios/chrome/browser/commerce/shopping_persisted_data_tab_helper.h"
 #import "ios/chrome/browser/drag_and_drop/drag_item_util.h"
 #include "ios/chrome/browser/main/browser.h"
@@ -370,7 +371,8 @@ web::WebState* GetWebStateWithId(WebStateList* web_state_list,
   if (index == WebStateList::kInvalidIndex)
     return;
 
-  LogPriceDropMetrics(self.webStateList->GetWebStateAt(index));
+  if (IsPriceAlertsEnabled())
+    LogPriceDropMetrics(self.webStateList->GetWebStateAt(index));
 
   // Don't attempt a no-op activation. Normally this is not an issue, but it's
   // possible that this method (-selectItemWithID:) is being called as part of
