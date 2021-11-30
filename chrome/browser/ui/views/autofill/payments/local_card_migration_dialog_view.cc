c34eba298c0e01eb498f50c437127f4c4a293d64
Siyu An <siyua@chromium.org>
2021-11-30 21:14:25 +0000

    Fix web_contents_ in LocalCardMigrationDialogView
    Make it a weak ptr instead of raw ptr and abort showing if web_contents_
    does not exist.
    
    Bug: 1273674
    Change-Id: I0905a81fafe294df7065e4fae6ffcaedfca83916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308220
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946658}

--- chrome/browser/ui/views/autofill/payments/local_card_migration_dialog_view.cc
+++ chrome/browser/ui/views/autofill/payments/local_card_migration_dialog_view.cc
@@ -389,7 +389,7 @@ END_METADATA
 LocalCardMigrationDialogView::LocalCardMigrationDialogView(
     LocalCardMigrationDialogController* controller,
     content::WebContents* web_contents)
-    : controller_(controller), web_contents_(web_contents) {
+    : controller_(controller), web_contents_(web_contents->GetWeakPtr()) {
   SetButtons(controller_->AllCardsInvalid()
                  ? ui::DIALOG_BUTTON_OK
                  : ui::DIALOG_BUTTON_OK | ui::DIALOG_BUTTON_CANCEL);
@@ -415,6 +415,13 @@ LocalCardMigrationDialogView::LocalCardMigrationDialogView(
 LocalCardMigrationDialogView::~LocalCardMigrationDialogView() {}
 
 void LocalCardMigrationDialogView::ShowDialog() {
+  if (!web_contents_) {
+    // If web_contents does not exist, delete this because at this step this
+    // View is not owned by any class.
+    delete this;
+    return;
+  }
+
   ConstructView();
   constrained_window::CreateBrowserModalDialogViews(
       this, web_contents_->GetTopLevelNativeWindow())
--- chrome/browser/ui/views/autofill/payments/local_card_migration_dialog_view.h
+++ chrome/browser/ui/views/autofill/payments/local_card_migration_dialog_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_PAYMENTS_LOCAL_CARD_MIGRATION_DIALOG_VIEW_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/autofill/payments/local_card_migration_dialog.h"
 #include "chrome/browser/ui/views/autofill/payments/dialog_view_ids.h"
 #include "components/autofill/core/browser/ui/payments/local_card_migration_dialog_controller.h"
@@ -59,7 +60,7 @@ class LocalCardMigrationDialogView : public LocalCardMigrationDialog,
 
   raw_ptr<LocalCardMigrationDialogController> controller_;
 
-  raw_ptr<content::WebContents> web_contents_;
+  base::WeakPtr<content::WebContents> web_contents_;
 
   // Pointer points to the LocalCardMigrationOfferView. Can be null when the
   // dialog is not in the 'offer' state.
