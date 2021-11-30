073de03e2cb4a4d7a1d24eaa8649ce3df603bcb1
Aya ElAttar <ayaelattar@chromium.org>
2021-11-30 16:59:24 +0000

    DLP: Rename DlpRulesManager::GetDisallowedTransfers
    Renamed GetDisallowedTransfers to GetDisallowedFileTransfers
    to avoid confusion as it's only used for files.
    
    Bug: 1259203
    Change-Id: I095f95d349c731d0788a25cae5368ba4dd8094b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306590
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946494}

--- chrome/browser/chromeos/policy/dlp/dlp_rules_manager_impl.h
+++ chrome/browser/chromeos/policy/dlp/dlp_rules_manager_impl.h
@@ -54,7 +54,7 @@ class DlpRulesManagerImpl : public DlpRulesManager {
                                   Restriction restriction,
                                   Level level) const override;
   size_t GetClipboardCheckSizeLimitInBytes() const override;
-  std::vector<uint64_t> GetDisallowedTransfers(
+  std::vector<uint64_t> GetDisallowedFileTransfers(
       const std::vector<FileMetadata>& transferred_files,
       const GURL& destination) const override;
 
