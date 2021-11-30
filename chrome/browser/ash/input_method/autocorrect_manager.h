6d78408f925c9cf4dfedd394a8b4780682e010e6
Leo Zhang <googleo@chromium.org>
2021-11-30 17:26:28 +0000

    Disable Autocorrect feature by rules.
    Text field contextual info will be fetched on focus.
    It will be used to check whether auto correct should be turned on/off.
    
    TEST=ut/Test/unit_tests --gtest_filter=NativeInputMethodEngineTest.*
    BUG=b:207587725
    
    Change-Id: Ic0cafe641b4c5aea91bfc1694031fe96c3426bd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306754
    Commit-Queue: Leo Zhang <googleo@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946510}

--- chrome/browser/ash/input_method/autocorrect_manager.h
+++ chrome/browser/ash/input_method/autocorrect_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/diacritics_insensitive_string_comparator.h"
 #include "chrome/browser/ash/input_method/input_method_engine_base.h"
 #include "chrome/browser/ash/input_method/suggestion_handler_interface.h"
+#include "chrome/browser/ash/input_method/text_field_contextual_info_fetcher.h"
 
 namespace ash {
 namespace input_method {
@@ -68,8 +69,13 @@ class AutocorrectManager {
 
   void UndoAutocorrect();
 
+  // Whether auto correction is disabled by some rule.
+  bool DisabledByRule();
+
  private:
   void LogAssistiveAutocorrectAction(AutocorrectActions action);
+
+  void OnTextFieldContextualInfoChanged(const TextFieldContextualInfo& info);
 
   SuggestionHandlerInterface* suggestion_handler_;
   int context_id_ = 0;
@@ -82,6 +88,8 @@ class AutocorrectManager {
   DiacriticsInsensitiveStringComparator
       diacritics_insensitive_string_comparator_;
   bool in_diacritical_autocorrect_session_ = false;
+
+  bool disabled_by_rule_ = false;
 };
 
 }  // namespace input_method
