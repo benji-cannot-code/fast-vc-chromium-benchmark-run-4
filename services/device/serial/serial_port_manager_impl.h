70a06935e4d89852abcb853bdf892811f79d9582
Chris Mumford <cmumford@chromium.org>
2021-11-30 23:00:26 +0000

    [serial] Add sequence checker to SerialPortManagerImpl.
    Properly sequencing use of SerialPortManagerImpl is non-trivial
    so add a sequence checker and some threading notes in the class
    comment to clarify use and catch misuse.
    
    Bug: none
    Change-Id: I47cf100a919408b0de1d6bbe76a614245874df91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307175
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/main@{#946727}

--- services/device/serial/serial_port_manager_impl.h
+++ services/device/serial/serial_port_manager_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/scoped_multi_source_observation.h"
+#include "base/sequence_checker.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
@@ -21,13 +22,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 class SingleThreadTaskRunner;
 class UnguessableToken;
-}
+}  // namespace base
 
 namespace device {
 
 // TODO(leonhsl): Merge this class with SerialDeviceEnumerator if/once
 // SerialDeviceEnumerator is exposed only via the Device Service.
 // crbug.com/748505
+//
+// Threading notes:
+// 1. Created on the UI thread.
+// 2. Used on the UI thread runner (macOS only), otherwise on a blocking task
+//    runner.
+// 3. Deleted on the same runner on which it is used *except* sometimes
+//    during shutdown when the runner threadpool is already shutdown.
+//    See crbug.com/1263149#c20 for details.
 class SerialPortManagerImpl : public mojom::SerialPortManager,
                               public SerialDeviceEnumerator::Observer {
  public:
@@ -74,6 +83,8 @@ class SerialPortManagerImpl : public mojom::SerialPortManager,
 
   mojo::ReceiverSet<SerialPortManager> receivers_;
   mojo::RemoteSet<mojom::SerialPortManagerClient> clients_;
+  // See threading notes above for guidelines for checking sequence.
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace device
