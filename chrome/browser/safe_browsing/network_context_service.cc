5342188f55fbe449804e005a68975431019d875b
Will Harris <wfh@chromium.org>
2021-11-30 00:44:46 +0000

    Enable network service data migration on Windows by default.
    This CL enables network service migration on Windows by
    default but leaves the feature for dynamic enabling on
    other platforms.
    
    Since it's enabled by default, the logic that previously
    connected the network service sandbox on Windows to
    network service data migration can be removed from the
    network context managers and also the tests.
    
    BUG=1173622
    
    Change-Id: I22e00580a3dd7503e1eb1db158655f21d12398f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307952
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946246}

--- chrome/browser/safe_browsing/network_context_service.cc
+++ chrome/browser/safe_browsing/network_context_service.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/safe_browsing/network_context_service.h"
 
+#include "base/feature_list.h"
 #include "chrome/browser/browser_features.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profiles/profile.h"
@@ -13,7 +14,8 @@ namespace safe_browsing {
 
 NetworkContextService::NetworkContextService(Profile* profile) {
   network_context_ = std::make_unique<SafeBrowsingNetworkContext>(
-      profile->GetPath(), features::ShouldTriggerNetworkDataMigration(),
+      profile->GetPath(),
+      base::FeatureList::IsEnabled(features::kTriggerNetworkDataMigration),
       base::BindRepeating(&NetworkContextService::CreateNetworkContextParams,
                           // This is safe because `this` owns
                           // `network_context_`.
