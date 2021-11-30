0777d3a42a408a4ca18b4eb4d0bdc2d9a8136153
Daniel Murphy <dmurph@chromium.org>
2021-11-30 00:41:12 +0000

    [dPWA] Add better logging for testing errors
    This patch prints out the files & folders that still exist after the
    integration tests complete.
    
    Bug: 1273568
    Change-Id: I0a29df33490d5707f319794230276d5336097bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307749
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946244}

--- chrome/browser/web_applications/web_app_shortcut.cc
+++ chrome/browser/web_applications/web_app_shortcut.cc
@@ -6,14 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_shortcut.h"
 
 #include <functional>
+#include <string>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/containers/span.h"
+#include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -99,6 +104,16 @@ GetMutableShortcutOverrideForTesting() {
   return g_shortcut_override;
 }
 
+std::string GetAllFilesInDir(const base::FilePath& file_path) {
+  std::vector<std::string> files_as_strs;
+  base::FileEnumerator files(file_path, true, base::FileEnumerator::FILES);
+  for (base::FilePath current = files.Next(); !current.empty();
+       current = files.Next()) {
+    files_as_strs.push_back(current.AsUTF8Unsafe());
+  }
+  return base::JoinString(base::make_span(files_as_strs), "\n  ");
+}
+
 }  // namespace
 
 ScopedShortcutOverrideForTesting::ScopedShortcutOverrideForTesting() = default;
@@ -124,10 +139,13 @@ ScopedShortcutOverrideForTesting::~ScopedShortcutOverrideForTesting() {
   directories = {&desktop};
 #endif
   for (base::ScopedTempDir* dir : directories) {
-    DCHECK(!dir->IsValid() || base::IsDirectoryEmpty(dir->GetPath()))
+    if (!dir->IsValid())
+      continue;
+    DCHECK(base::IsDirectoryEmpty(dir->GetPath()))
         << "Directory not empty: " << dir->GetPath().AsUTF8Unsafe()
         << ". Please uninstall all webapps that have been installed while "
-           "shortcuts were overriden.";
+           "shortcuts were overriden. Contents:\n"
+        << GetAllFilesInDir(dir->GetPath());
   }
   GetMutableShortcutOverrideForTesting() = absl::nullopt;  // IN-TEST
 }
