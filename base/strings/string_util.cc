75ac3ea0a7226e4a37c00c098b7a90a49aa1d5cd
Nico Weber <thakis@chromium.org>
2021-11-30 13:23:56 +0000

    Reland^2: Build with -std=c++17 on some platforms
    This is a reland of 41972c9047186a913d95378eb96879514adaafa1
    The reland changes WEB_CONTENTS_USER_DATA_KEY_DECL()
    to `static const int kUserDataKey = 0`. In C++17, class-level
    constexpr statics are implicitly inline variables (see bug),
    which means that they don't have unique addresses across shared
    libraries. WEB_CONTENTS_USER_DATA_KEY_DECL requires that, so
    just make the variable `const` instead of `constexpr` to make things
    work with C++17.
    
    Original change's description:
    > Reland: Build with -std=c++17 on some platforms
    >
    > In the reland, we keep using -std=c++14 for CFI builds for a short
    > time, see crbug.com/1273966.
    >
    > This is to find unknown things that break with -std=c++17.
    >
    > Do NOT use C++17 features anywhere yet. We'll likely have to revert
    > this again in a bit.
    >
    > This does NOT enable C++17 on iOS and macOS -- there are
    > a few (likely small) kinks we have to work out first on those
    > platforms.
    >
    > It does enable C++17 on Android, Cast, Chrome OS, Fuchsia,
    > (non-CFI) Linux.
    > C++17 was already enabled on Windows in an earlier change.
    >
    > This relands https://chromium-review.googlesource.com/c/chromium/src/+/3297210/
    > (but the change is much smaller now since much of that change landed
    > when we turned on C++17 on Windows, in
    > https://chromium-review.googlesource.com/c/chromium/src/+/3301748 )
    >
    > Bug: 752720,1273966
    > Cq-Include-Trybots: luci.chromium.try:linux_chromium_cfi_rel_ng
    > Change-Id: I4e4979f0af2052803154fb154edbe9797da5aa40
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304484
    > Auto-Submit: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Hans Wennborg <hans@chromium.org>
    > Reviewed-by: Hans Wennborg <hans@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#945796}
    
    Bug: 752720,1273966,1274431
    Change-Id: I62c98545544221284b6a01a682a13a991d22caa5
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_cfi_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306812
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946426}

--- base/strings/string_util.cc
+++ base/strings/string_util.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <vector>
 
+#include "base/cfi_buildflags.h"
 #include "base/check_op.h"
 #include "base/cxx17_backports.h"
 #include "base/no_destructor.h"
@@ -36,7 +37,8 @@ namespace base {
 bool IsWprintfFormatPortable(const wchar_t* format) {
   // This snippet of code checks that we can build C++17 code.
   // TODO(thakis): Enable this on all platforms, and then remove it again.
-#if defined(OS_WIN)
+#if !defined(OS_IOS) && !defined(OS_MAC) && !BUILDFLAG(CFI_ICALL_CHECK) && \
+    !BUILDFLAG(CFI_CAST_CHECK)
   if constexpr (constexpr int i = 0; i > 0) {
   }
 #endif
