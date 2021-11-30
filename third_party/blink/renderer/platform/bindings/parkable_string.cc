feed7d71fde9205e9ba74d9d15ad5dbe83e2b957
Olivier Li <olivierli@chromium.org>
2021-11-30 21:27:48 +0000

    Introduce the DelayFirstParkingOfStrings trial.
    The goal of the trial is to assess the performance impact of delaying
    the first parking of strings by 60 seconds.
    
    This delay was chosen by looking at UMA data and seeing that ~70%
    of renderers do not make it past 1 min of lifetime.
    
    The hope is that delaying the parking like this will not substantially
    increase memory use but will save CPU cycles by avoiding the parking
    of strings in renderers that are about to die.
    
    Bug: 1229682
    Change-Id: If425218afd942e1f8ebb85fc2d7985fbe1e97168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271249
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946661}

--- third_party/blink/renderer/platform/bindings/parkable_string.cc
+++ third_party/blink/renderer/platform/bindings/parkable_string.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/platform/bindings/parkable_string.h"
+#include "base/time/time.h"
 
 // parkable_string.h is a widely included header and its size impacts build
 // time. Try not to raise this limit unless necessary. See
