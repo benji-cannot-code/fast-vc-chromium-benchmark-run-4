5755b0f539bdccbd5ab4420e64b73414af0c75c0
Gavin Dodd <gavindodd@google.com>
2021-11-30 00:20:45 +0000

    shimless: Clean up i18n TODOs
    Remove unused data.
    Remove redundant TODO
    
          Run closure compile for shimless_rma and tests
    
    Bug: 1198187
    Test: Run browser tests for Shimless*
    Change-Id: I98b9f28b43b09ec1864cbbe47f8afadcac2e8b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300748
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#946230}

--- ash/webui/shimless_rma/resources/reimaging_calibration_run_page.js
+++ ash/webui/shimless_rma/resources/reimaging_calibration_run_page.js
@@ -67,7 +67,6 @@ export class ReimagingCalibrationRunPage extends
        */
       calibrationStatusMessage_: {
         type: String,
-        // TODO(gavindodd): update for i18n
         value: '',
       }
     };
