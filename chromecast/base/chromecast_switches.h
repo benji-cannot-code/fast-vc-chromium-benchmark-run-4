171bb9b6643ae81523a7da33116cd9d0f0054dcf
Junbo Ke <juke@chromium.org>
2021-11-30 23:43:55 +0000

    [Chromecast] Add mojom::CastAccessibilityService implementation.
    Changes are moved from the internal implementation with some fixes for
    build errors.
    
    Bug: b:190408515
    Test: Build Android apk and Linux cast_shell
    Change-Id: Ie908b4028f902a9762bd8aefede8042e53508220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299353
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946755}

--- chromecast/base/chromecast_switches.h
+++ chromecast/base/chromecast_switches.h
@@ -103,6 +103,9 @@ extern const char kForceUpdateRemoteUrl[];
 
 extern const char kSysInfoFilePath[];
 
+// Extension switches.
+extern const char kExtensionsDir[];
+
 }  // namespace switches
 
 namespace chromecast {
