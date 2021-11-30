a12600454832aef3b29c2348ad6170e136ce3e4a
Pei Zhang <ppz@chromium.org>
2021-11-30 07:42:26 +0000

    Port Indexeddb get-related tests to WPT tests
    Bug: 866164
    Change-Id: I5f0ad2307292e4d0e32d25d711b518579f121764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277288
    Commit-Queue: Pei Zhang <ppz@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946355}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbobjectstore_get3.any.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=IDBObjectStore.get() - key is a Date
+// META: script=support.js
+// @author Microsoft <https://www.microsoft.com>
+
+"use strict";
+
+let db;
+const t = async_test();
+const record = { key: new Date(), property: "data" };
+
+const open_rq = createdb(t);
+open_rq.onupgradeneeded = event => {
+  db = event.target.result;
+  db.createObjectStore("store", { keyPath: "key" })
+    .add(record);
+};
+
+open_rq.onsuccess = event => {
+  const rq = db.transaction("store")
+    .objectStore("store")
+    .get(record.key);
+
+  rq.onsuccess = t.step_func(event => {
+    assert_equals(event.target.result.key.valueOf(), record.key.valueOf());
+    assert_equals(event.target.result.property, record.property);
+    t.done();
+  });
+};
---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/idbobjectstore_get6.any.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=IDBObjectStore.get() - throw TransactionInactiveError on aborted transaction
+// META: script=support.js
+// @author YuichiNukiyama <https://github.com/YuichiNukiyama>
+
+"use strict";
+
+let db;
+const t = async_test();
+
+const open_rq = createdb(t);
+open_rq.onupgradeneeded = event => {
+  db = event.target.result;
+  db.createObjectStore("store", { keyPath: "key" });
+};
+
+open_rq.onsuccess = event => {
+  const store = db.transaction("store")
+    .objectStore("store");
+  store.transaction.abort();
+  assert_throws_dom("TransactionInactiveError", function () {
+    store.get(1);
+  }, "throw TransactionInactiveError on aborted transaction.");
+  t.done();
+};
