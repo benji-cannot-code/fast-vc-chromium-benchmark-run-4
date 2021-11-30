b84efb8b87a933fc8af8efbe18da9716d503fbc7
John Williams <jrw@google.com>
2021-11-30 03:22:58 +0000

    Created MediaDialogUiForTest to share code between GMC and Cast tests.
    Bug: 1229305, b/193073480
    Change-Id: Ia42f909c9bd188af125aaeb6736bd49e4a0b0f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3276721
    Commit-Queue: John Williams <jrw@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946304}

--- chrome/test/media_router/media_router_gmc_ui_for_test.h
+++ chrome/test/media_router/media_router_gmc_ui_for_test.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/ui/views/global_media_controls/media_dialog_ui_for_test.h"
 #include "chrome/browser/ui/views/media_router/cast_dialog_view.h"
 #include "chrome/browser/ui/views/media_router/media_router_dialog_controller_views.h"
 #include "chrome/test/media_router/media_router_ui_for_test_base.h"
@@ -15,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/media_router/common/media_source.h"
 #include "content/public/browser/web_contents_user_data.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+
+class Browser;
 
 namespace media_router {
 
@@ -59,6 +62,12 @@ class MediaRouterGmcUiForTest
       WatchType watch_type,
       absl::optional<std::string> sink_name = absl::nullopt) override;
 
+  Browser* browser() const { return browser_; }
+
+  Browser* const browser_;
+  MediaDialogUiForTest dialog_ui_{
+      base::BindRepeating(&MediaRouterGmcUiForTest::browser,
+                          base::Unretained(this))};
   base::WeakPtrFactory<MediaRouterGmcUiForTest> weak_factory_{this};
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
