5b50ca4c636647ddf44355d47e0eedefa5cf98ab
dpapad <dpapad@chromium.org>
2021-11-30 20:24:20 +0000

    Settings: Migrate autofill_page/ tests to TypeScript, part 1.
     - Extract PaymentsManager and PaymentsManagerImpl to their own file.
     - Extract AutofillManager and AutofillManagerImpl to their own file.
     - Migrate test_password_manager_proxy.js to TS.
     - Migrate passwords_and_autofill_fake_data.js to TS.
    
    Bug: 1263610
    Change-Id: I0b7a14e2c52cb5eae6d5a09071b313c4079329b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3305843
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946626}

--- chrome/browser/resources/settings/autofill_page/password_list_item.ts
+++ chrome/browser/resources/settings/autofill_page/password_list_item.ts
@@ -93,4 +93,10 @@ export class PasswordListItemElement extends PasswordListItemElementBase {
   }
 }
 
+declare global {
+  interface HTMLElementTagNameMap {
+    'password-list-item': PasswordListItemElement;
+  }
+}
+
 customElements.define(PasswordListItemElement.is, PasswordListItemElement);
--- chrome/browser/resources/settings/autofill_page/passwords_section.ts
+++ chrome/browser/resources/settings/autofill_page/passwords_section.ts
@@ -81,7 +81,7 @@ interface RepeaterEvent extends CustomEvent {
   };
 }
 
-interface PasswordsSectionElement {
+export interface PasswordsSectionElement {
   $: {
     exportImportMenu: CrActionMenuElement,
     passwordsListHandler: PasswordsListHandlerElement,
@@ -92,7 +92,7 @@ const PasswordsSectionElementBase = MergePasswordsStoreCopiesMixin(
     PrefsMixin(GlobalScrollTargetMixin(MergeExceptionsStoreCopiesMixin(
         WebUIListenerMixin(I18nMixin(PasswordCheckMixin(PolymerElement)))))));
 
-class PasswordsSectionElement extends PasswordsSectionElementBase {
+export class PasswordsSectionElement extends PasswordsSectionElementBase {
   static get is() {
     return 'passwords-section';
   }
@@ -690,6 +690,12 @@ class PasswordsSectionElement extends PasswordsSectionElementBase {
         }
       }));
     }, 0);
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'passwords-section': PasswordsSectionElement;
   }
 }
 
