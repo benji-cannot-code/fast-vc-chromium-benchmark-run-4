a829964aba7a1733e42db1338732940e4c25fdd0
Manuel Rego Casasnovas <rego@igalia.com>
2021-11-30 09:42:27 +0000

    Fix ::highlight & ::target-text in legacy layout
    We were always using the inactive search highlight color
    for ::target-text background in legacy layout.
    We can easily use the background color specified by the user
    in the pseudo-element.
    
    We were always using a grammar error marker for ::highlight
    in legacy layout.
    
    This patch simply calls
    HighlightPaintingUtils::HighlightBackgroundColor()
    in InlineTextBoxPainter::PaintTextMarkerBackground()
    to fix this issue.
    
    BUG=1136817,1164461
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng_disabled
    Change-Id: I2b353e98ceb26ac4d0695e6410fdb5cc3d1ad9b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306483
    Reviewed-by: Delan Azabani <dazabani@igalia.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#946378}

--- third_party/blink/renderer/core/paint/inline_text_box_painter.cc
+++ third_party/blink/renderer/core/paint/inline_text_box_painter.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/editor.h"
 #include "third_party/blink/renderer/core/editing/markers/composition_marker.h"
 #include "third_party/blink/renderer/core/editing/markers/document_marker_controller.h"
+#include "third_party/blink/renderer/core/editing/markers/highlight_pseudo_marker.h"
 #include "third_party/blink/renderer/core/editing/markers/text_match_marker.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/api/line_layout_api_shim.h"
@@ -56,7 +57,8 @@ std::pair<unsigned, unsigned> GetTextMatchMarkerPaintOffsets(
       text_box.Start() + text_box.GetLineLayoutItem().TextStartOffset();
 
   DCHECK(marker.GetType() == DocumentMarker::kTextMatch ||
-         marker.GetType() == DocumentMarker::kTextFragment);
+         marker.GetType() == DocumentMarker::kTextFragment ||
+         marker.GetType() == DocumentMarker::kHighlight);
   const unsigned start_offset = marker.StartOffset() > text_box_start
                                     ? marker.StartOffset() - text_box_start
                                     : 0U;
@@ -649,6 +651,7 @@ void InlineTextBoxPainter::PaintDocumentMarkers(
         inline_text_box_.PaintDocumentMarker(paint_info, box_origin, marker,
                                              style, font, true);
         break;
+      case DocumentMarker::kHighlight:
       case DocumentMarker::kTextFragment:
       case DocumentMarker::kTextMatch:
         if (marker_paint_phase == DocumentMarkerPaintPhase::kBackground) {
@@ -676,10 +679,6 @@ void InlineTextBoxPainter::PaintDocumentMarkers(
                                         styleable_marker, style, font);
         }
       } break;
-      case DocumentMarker::kHighlight:
-        inline_text_box_.PaintDocumentMarker(paint_info, box_origin, marker,
-                                             style, font, false);
-        break;
       default:
         // Marker is not painted, or painting code has not been added yet
         break;
@@ -966,11 +965,20 @@ void InlineTextBoxPainter::PaintTextMarkerBackground(
       GetTextMatchMarkerPaintOffsets(marker, inline_text_box_);
   TextRun run = inline_text_box_.ConstructTextRun(style);
 
-  Color color = LayoutTheme::GetTheme().PlatformTextSearchHighlightColor(
-      marker.GetType() == DocumentMarker::kTextMatch
-          ? To<TextMatchMarker>(marker).IsActiveMatch()
-          : false,
-      style.UsedColorScheme());
+  Color color;
+  if (marker.GetType() == DocumentMarker::kTextMatch) {
+    color = LayoutTheme::GetTheme().PlatformTextSearchHighlightColor(
+        To<TextMatchMarker>(marker).IsActiveMatch(), style.UsedColorScheme());
+  } else {
+    DCHECK(marker.GetType() == DocumentMarker::kHighlight ||
+           marker.GetType() == DocumentMarker::kTextFragment);
+    const auto& highlight_pseudo_marker = To<HighlightPseudoMarker>(marker);
+    auto layout_item = inline_text_box_.GetLineLayoutItem();
+    color = HighlightPaintingUtils::HighlightBackgroundColor(
+        layout_item.GetDocument(), layout_item.StyleRef(),
+        layout_item.GetNode(), highlight_pseudo_marker.GetPseudoId(),
+        highlight_pseudo_marker.GetPseudoArgument());
+  }
   GraphicsContext& context = paint_info.context;
   GraphicsContextStateSaver state_saver(context);
 
