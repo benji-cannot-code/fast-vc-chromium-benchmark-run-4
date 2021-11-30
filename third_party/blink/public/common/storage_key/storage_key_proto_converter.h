7c66d4c3d001dcbd3d8c88b260ef516fb85117e7
Aaron Tagliaboschi <aarontag@chromium.org>
2021-11-30 02:20:19 +0000

    Create fuzzing grammar proto and converter for StorageKey
    This can be used for fuzzing surfaces that are starting to use storage
    keys.
    
    Also implemented a small fuzzer for StorageKey itself, which takes
    untrusted input through its deserialization logic. It's incomplete (a
    true grammar for DeserializeForServiceWorker should be implemented) but
    the coverage should be able to start uncovering bugs if they exist
    
    Fixed: 1259682
    Bug: 1270350
    Change-Id: Icba430f5a613842abdd96b0d4c09a4bc6fd25a68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282444
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946272}

---
+++ third_party/blink/public/common/storage_key/storage_key_proto_converter.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_STORAGE_KEY_STORAGE_KEY_PROTO_CONVERTER_H_
+#define THIRD_PARTY_BLINK_PUBLIC_COMMON_STORAGE_KEY_STORAGE_KEY_PROTO_CONVERTER_H_
+
+#include "third_party/blink/public/common/storage_key/proto/storage_key.pb.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
+
+namespace storage_key_proto {
+
+blink::StorageKey Convert(const storage_key_proto::StorageKey& storage_key);
+
+}  // namespace storage_key_proto
+
+#endif  // THIRD_PARTY_BLINK_PUBLIC_COMMON_STORAGE_KEY_STORAGE_KEY_PROTO_CONVERTER_H_
