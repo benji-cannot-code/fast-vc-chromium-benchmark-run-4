46c5967ccc25db07f89422343428f3c258921ca4
Yi Gu <yigu@chromium.org>
2021-12-01 20:29:26 +0000

    [FedCM] Add wpt test for concurrent navigator.credentials.get
    When there's a pending request for id token, all new requests by `get`
    will be rejected. This patch adds a wpt test for it.
    
    Bug: 1274805
    Change-Id: I024abc5b2ff90c04d5c4d205b036a509d20943a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309110
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947168}

--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.js
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import { MockFederatedAuthRequest } from './fedcm-mock.js';
 
-export function fedcm_test(test_func, name, properties) {
+export function fedcm_test(test_func, name, exception, properties) {
   promise_test(async (t) => {
     assert_implements(navigator.credentials, 'missing navigator.credentials');
     const mock = new MockFederatedAuthRequest();
     try {
       await test_func(t, mock);
+    } catch (e) {
+      assert_equals(exception, e.message)
     } finally {
       await mock.reset();
     }
