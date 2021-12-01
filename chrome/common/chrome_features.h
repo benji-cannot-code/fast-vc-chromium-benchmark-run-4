b84ced43f2e0796305b9501196480844c33ea27a
Ivan Šandrk <isandrk@chromium.org>
2021-12-01 09:40:23 +0000

    [dPWA] Sub Apps - remove dead code flag
    Remove dead kDesktopPWAsSubApps flag in chrome_features.cc (since moved
    to third_party/blink/common/features.cc).
    
    Bug: 1171317
    Change-Id: I2d6ec00eabcae2253bf05c929d6ef67414e4212f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306460
    Auto-Submit: Ivan Šandrk <isandrk@chromium.org>
    Commit-Queue: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946917}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -209,9 +209,6 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsRunOnOsLogin;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::Feature kDesktopPWAsSubApps;
-
-COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsTabStripLinkCapturing;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
