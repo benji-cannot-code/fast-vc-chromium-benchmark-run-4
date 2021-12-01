ba2b3d4b6d591b08df50cde32de2160a2a69fe1d
Michael Martis <martis@chromium.org>
2021-12-01 00:16:20 +0000

    [Snooping protection] Made empty params a valid configuration.
    Creating a chrome://flags entry with variations always includes a
    "Default" variation with empty parameters. Hence, our feature should
    still do something useful when provided empty parameters. This CL
    ensures that a simple valid configuration is constructed when no feature
    params are specified.
    
    Bug: chromium:1241704
    Change-Id: Ib4c5fd2127dc8093ed965b53b85b6855ba376cbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3305913
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946773}

--- ash/system/hps/hps_configuration.cc
+++ ash/system/hps/hps_configuration.cc
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 // Used for indicating which oneof field to construct for `filter_config` inside
-// FeatureConfig.
-constexpr int kFilterConfigCaseDefault = 0;
+// FeatureConfig. We ensure that this is a valid configuration so that passing
+// empty params induces a working config (as required for, e.g., a
+// chrome:://flags entry).
+constexpr int kFilterConfigCaseDefault = 1;
 
 // Default value for `FeatureConfig.consecutive_results_filter_config.count`.
 // Default 1 means the change will be notified if the inference result is
