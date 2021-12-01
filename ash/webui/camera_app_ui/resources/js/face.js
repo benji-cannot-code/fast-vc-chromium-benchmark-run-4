c37068c02c365a0cb12ee3d0e5e44670f865fe44
Ren-Pei Zeng <kamesan@chromium.org>
2021-12-01 09:16:52 +0000

    CCA: Fix face rectangle orientation when screen is rotated
    Bug: b:199360764
    Test: Open CCA in tablet mode with different orientations.
    Change-Id: If47dbb8fc5e51afa35da0db78c90df0019dae0a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308300
    Commit-Queue: Ren-Pei Zeng <kamesan@chromium.org>
    Reviewed-by: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946912}

--- ash/webui/camera_app_ui/resources/js/face.js
+++ ash/webui/camera_app_ui/resources/js/face.js
@@ -40,9 +40,10 @@ function rotate(x, y, orientation) {
 export class FaceOverlay {
   /**
    * @param {!Resolution} activeArraySize
-   * @param {number} sensorOrientation
+   * @param {number} orientation Counter-clockwise angles to apply rotation to
+   *     the face rectangles.
    */
-  constructor(activeArraySize, sensorOrientation) {
+  constructor(activeArraySize, orientation) {
     /**
      * @const {!Resolution}
      * @private
@@ -53,7 +54,7 @@ export class FaceOverlay {
      * @const {number}
      * @private
      */
-    this.sensorOrientation_ = sensorOrientation;
+    this.orientation_ = orientation;
 
     /**
      * @const {!HTMLCanvasElement}
@@ -80,7 +81,7 @@ export class FaceOverlay {
 
     // TODO(b/178344897): Handle zoomed preview.
     // TODO(b/178344897): Handle cropped preview.
-    // TODO(b/178344897): Handle screen orientation.
+    // TODO(b/178344897): Handle screen orientation dynamically.
 
     this.ctx_.strokeStyle = RECT_COLOR;
     for (let i = 0; i < rects.length; i += 4) {
@@ -89,8 +90,8 @@ export class FaceOverlay {
       y1 /= this.activeArraySize_.height;
       x2 /= this.activeArraySize_.width;
       y2 /= this.activeArraySize_.height;
-      [x1, y1] = rotate(x1, y1, this.sensorOrientation_);
-      [x2, y2] = rotate(x2, y2, this.sensorOrientation_);
+      [x1, y1] = rotate(x1, y1, this.orientation_);
+      [x2, y2] = rotate(x2, y2, this.orientation_);
       x1 *= this.canvas_.width;
       y1 *= this.canvas_.height;
       x2 *= this.canvas_.width;
