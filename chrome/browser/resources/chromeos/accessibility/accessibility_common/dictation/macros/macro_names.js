2d47a0edb060e6321f4148c0ba350815fe2072e1
Akihiro Ota <akihiroota@chromium.org>
2021-12-01 17:24:51 +0000

    Dictation: Introduce ParseStrategy
    This change aims to clean up the logic in SpeechParser. The main
    observation is that SpeechParser has three different methods for
    converting text into macros:
    1. Using Pumpkin
    2. Using SimpleMacroFactory, if pumpkin is unavailable
    3. Returning the InputTextViewMacro (inputs text as-is) as a catch-all
    
    This change formalizes this in the code by introducing an abstract
    class, ParseStrategy, and extending it to implement the three behaviors
    listed above. In addition to readability, this change helps encapsulate
    all pumpkin loading and parsing logic into a separate class, which
    reduces code clutter.
    
    This is a pure refactor and should not result in any behavior change.
    
    AX-Relnotes: N/A
    Bug: 1216111
    Change-Id: I01ab593e54199a02cdda0cb794309cff05bdebb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3296609
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947075}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/macros/macro_names.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/macros/macro_names.js
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @enum {number}
  */
 export const MacroName = {
-  UNSPECIFID: 1,
+  UNSPECIFID: 0,
 
   // Simply input text into a text field.
   INPUT_TEXT_VIEW: 1,
