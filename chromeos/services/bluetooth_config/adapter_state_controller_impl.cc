7106d695580c26fd0d54f779e5f1de69dba416a9
Gordon Seto <gordonseto@google.com>
2021-12-01 00:33:23 +0000

    [CrOS Bluetooth] Fix AdapterStateController rapid enable toggling.
    Fix issue in AdapterStateController where if Bluetooth is enabled,
    then rapidly disabled, Bluetooth will re-enable itself. This is because
    when BluetoothAdapter::SetPowered() finishes and its success callback
    is fired, momentarily, the value it returns in
    BluetoothAdapter::IsPowered() is still the old value. Therefore, when
    AdapterStateController attempts to perform the next queued state change
    (disable) once the success callback finishes, it believes the adapter
    is already disabled because SetPowered() returns false, and performs
    no action, leaving the adapter in its current state (enabled). The UI
    updates to reflect the still-enabled-state, giving the impression that
    Bluetooth has re-enabled.
    
    This CL updates AdapterStateController to not attempt to perform the
    next queued state change when the success callback is fired, instead
    waiting until AdapterPoweredChanged() is called to ensure the adapter
    is in the correct state before performing the next state change.
    
    Bug: 1010321
    Change-Id: I0cdd871d5452730838f1e74b13de39ef663efc32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309686
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946787}

--- chromeos/services/bluetooth_config/adapter_state_controller_impl.cc
+++ chromeos/services/bluetooth_config/adapter_state_controller_impl.cc
@@ -51,6 +51,7 @@ void AdapterStateControllerImpl::AdapterPoweredChanged(
     device::BluetoothAdapter* adapter,
     bool powered) {
   NotifyAdapterStateChanged();
+  AttemptQueuedStateChange();
 }
 
 void AdapterStateControllerImpl::AttemptQueuedStateChange() {
@@ -114,7 +115,12 @@ void AdapterStateControllerImpl::OnSetPoweredSuccess(bool enabled) {
   BLUETOOTH_LOG(EVENT) << "Bluetooth " << (enabled ? "enabled" : "disabled")
                        << " successfully";
   in_progress_state_change_ = PowerStateChange::kNoChange;
-  AttemptQueuedStateChange();
+
+  // Adapter->IsPowered() won't immediately be updated to the new value when
+  // SetPowered() finishes and this method is called. Don't call
+  // AttemptQueuedStateChange() now because the adapter isn't in the correct
+  // state yet. Wait until AdapterPoweredChanged() is invoked and call
+  // AttemptQueuedStateChange() there.
 }
 
 void AdapterStateControllerImpl::OnSetPoweredError(bool enabled) {
