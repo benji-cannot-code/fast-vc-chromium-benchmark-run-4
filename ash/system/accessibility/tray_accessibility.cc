90fc0d37abb070fc37ed8cebec99b3e42446e106
Nico Weber <thakis@chromium.org>
2021-12-01 00:32:21 +0000

    Make AccessibilityDetailedView::kClassName const instead of constexpr
    In C++17, class-static constexprs are implicitly inline variables,
    and since we build with -fvisibility=hidden the (kind of senseless?)
    test TrayAccessibilityTest.GetClassName fails in component builds
    because it compares pointer equality:
    
      EXPECT_EQ(tray::AccessibilityDetailedView::kClassName,
                GetDetailedViewClassName());
    
    As fix, just make this const instead of constexpr.
    
    Bug: 1275292
    Change-Id: I26f963f4b969ac685df071cecc240a3179afd794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309238
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946786}

--- ash/system/accessibility/tray_accessibility.cc
+++ ash/system/accessibility/tray_accessibility.cc
@@ -87,7 +87,7 @@ namespace tray {
 ////////////////////////////////////////////////////////////////////////////////
 // ash::tray::AccessibilityDetailedView
 
-constexpr char AccessibilityDetailedView::kClassName[];
+const char AccessibilityDetailedView::kClassName[] = "AccessibilityDetailedView";
 
 AccessibilityDetailedView::AccessibilityDetailedView(
     DetailedViewDelegate* delegate)
--- ash/system/accessibility/tray_accessibility.h
+++ ash/system/accessibility/tray_accessibility.h
@@ -41,7 +41,7 @@ class ASH_EXPORT AccessibilityDetailedView
     : public TrayDetailedView,
       public speech::SodaInstaller::Observer {
  public:
-  static constexpr char kClassName[] = "AccessibilityDetailedView";
+  static const char kClassName[];
 
   explicit AccessibilityDetailedView(DetailedViewDelegate* delegate);
 
