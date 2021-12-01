0dee39d69e2594ab082ee506457b46f0b5f57fcb
Michael Martis <martis@chromium.org>
2021-12-01 01:57:37 +0000

    [Snooping protection] Introduced HPS notify controller.
    Snooping protection is now actively managing its preferences. Hence,
    this CL moves its backend logic into a controller class that pushes
    messages to its view. This has the added benefit of creating a
    per-session (v.s. per display) location where future initialization etc.
    logic can live.
    
    The new unit test is actually a migration of the old view unit test
    (since the controller is now responsible for most of the view's old
    behavior). I've not added in new view tests, since the view now only
    listens for colour and visibility changes. I can add in some tests for
    that minor logic if you feel strongly.
    
    Bug: chromium:1241704
    Change-Id: Ieafdb1cf63c2e5437f2071b6cc7cb175de807173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3277223
    Commit-Queue: Michael Martis <martis@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946819}

--- ash/ash_prefs.cc
+++ ash/ash_prefs.cc
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/power/power_prefs.h"
 #include "ash/system/session/logout_button_tray.h"
 #include "ash/system/session/logout_confirmation_controller.h"
-#include "ash/system/unified/hps_notify_view.h"
+#include "ash/system/unified/hps_notify_controller.h"
 #include "ash/system/unified/top_shortcuts_view.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
 #include "ash/touch/touch_devices_controller.h"
@@ -82,7 +82,7 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry, bool for_test) {
   GestureEducationNotificationController::RegisterProfilePrefs(registry,
                                                                for_test);
   holding_space_prefs::RegisterProfilePrefs(registry);
-  HpsNotifyView::RegisterProfilePrefs(registry);
+  HpsNotifyController::RegisterProfilePrefs(registry);
   LoginScreenController::RegisterProfilePrefs(registry, for_test);
   LogoutButtonTray::RegisterProfilePrefs(registry);
   LogoutConfirmationController::RegisterProfilePrefs(registry);
