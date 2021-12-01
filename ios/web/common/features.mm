4ef58c458c20ea89fc2d56641804eeae3a8925c3
Miriam Zimmerman <mutexlox@google.com>
2021-12-01 15:44:33 +0000

    Reland "base: Feature: check for disallowed prefixes."
    This is a reland of 39acd95ec27dacb146f82d324a94a4c36da4e332
    The change was speculatively reverted in
    https://chromium-review.googlesource.com/c/chromium/src/+/3309124
    but https://crbug.com/1275502 still happens, e.g. in
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20TSan%20Tests/71534/overview
    
    Original change's description:
    > base: Feature: check for disallowed prefixes.
    >
    > Creators of a base::Feature object can insert `CONSTINIT` at the
    > call site to ensure compile-time initialization (and skip this check at
    > runtime). In a future CL, we may attempt to apply this change across the
    > codebase.
    >
    > Also, clean up two base::Features that incorrectly failed to define a
    > default value.
    >
    > BUG=chromium:1263068
    > TEST=unit tests
    >
    > Change-Id: Ice6134d4e1c057353c8f5099d121e23bc04fcb1c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297126
    > Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Miriam Zimmerman <mutexlox@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#946696}
    
    Bug: chromium:1263068
    Change-Id: Iadf15b780329cf4442c583ce042b60ef14c9f3d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310638
    Owners-Override: Pavol Marko <pmarko@chromium.org>
    Auto-Submit: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947012}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -69,7 +69,8 @@ const base::Feature kEnableNewDownloadAPI{"EnableNewDownloadAPI",
 const base::Feature kSynthesizedRestoreSession{
     "SynthesizedRestoreSession", base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kEnableUnrealizedWebStates{"EnableUnrealizedWebStates"};
+const base::Feature kEnableUnrealizedWebStates{
+    "EnableUnrealizedWebStates", base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool UseWebClientDefaultUserAgent() {
   return base::FeatureList::IsEnabled(kUseDefaultUserAgentInWebClient);
