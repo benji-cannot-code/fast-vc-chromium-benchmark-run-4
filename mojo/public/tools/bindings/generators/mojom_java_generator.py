944b5ca10354213592d588a009c62d4808ca144f
Oksana Zhuravlova <oksamyt@chromium.org>
2021-12-01 23:28:21 +0000

    [mojo][java] Remove old names for response callbacks
    Bug: 1093146
    Change-Id: I3ed0f1d4471dea17ae6381a1e8e4f4764effc50b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3283950
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947251}

--- mojo/public/tools/bindings/generators/mojom_java_generator.py
+++ mojo/public/tools/bindings/generators/mojom_java_generator.py
@@ -149,11 +149,6 @@ def GetNameForElement(element):
 def GetInterfaceResponseName(method):
   return UpperCamelCase(method.name) + '_Response'
 
-
-# TODO(crbug.com/1093146): remove after migrating downstream references.
-def GetInterfaceResponseNameOld(method):
-  return UpperCamelCase(method.name + '_Response')
-
 def ParseStringAttribute(attribute):
   assert isinstance(attribute, basestring)
   return attribute
@@ -458,7 +453,6 @@ class Generator(generator.Generator):
         'has_method_without_response': HasMethodWithoutResponse,
         'has_method_with_response': HasMethodWithResponse,
         'interface_response_name': GetInterfaceResponseName,
-        'interface_response_name_old': GetInterfaceResponseNameOld,
         'is_array_kind': mojom.IsArrayKind,
         'is_any_handle_kind': mojom.IsAnyHandleKind,
         "is_enum_kind": mojom.IsEnumKind,
