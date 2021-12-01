213312729d465dd3c38e8a2f11cde4448880090e
Matt Jones <mdjones@chromium.org>
2021-12-01 22:36:40 +0000

    [Shopping] Sanitize JSON prior to parsing in the browser
    This patch adds sanitizes the JSON being returned from the in-page
    javascript prior to being handled in the browser process.
    
    Bug: 1274834
    Change-Id: I40693276d8ad795ea4f1ee532de77770e15cdea7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307366
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947222}

--- chrome/browser/commerce/shopping_list/shopping_data_provider.h
+++ chrome/browser/commerce/shopping_list/shopping_data_provider.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/content/browser/optimization_guide_decider.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "services/data_decoder/public/cpp/json_sanitizer.h"
 
 namespace base {
 class Value;
@@ -83,6 +84,9 @@ class ShoppingDataProvider
 
   // Handle the result of javascript on-page heuristics.
   void OnJavascriptExecutionCompleted(base::Value result);
+
+  // Handle the result of the JSON (from the on-page javascript) sanitization.
+  void OnJsonSanitizationCompleted(data_decoder::JsonSanitizer::Result result);
 
   // Whether the javascript heuristics should be run when the page has finished
   // loading.
