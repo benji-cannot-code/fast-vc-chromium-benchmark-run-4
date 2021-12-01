ad5f0daa0ba2dc31391bc31ce1bb11d1d579f190
Jack Hsieh <chengweih@chromium.org>
2021-12-01 20:30:26 +0000

    bluetooth: Send id raw bytes instead of encoded string over IPC.
    In the browser process side, modify WebBluetoothDeviceId class to hold
    array<uint8_t, 16> for device_id rather than storing base64 encoded
    string.  WebBluetoothDeviceId::str() method is modified to return base64
    encoded string as a way of serialization, which is used in multiple
    places of permission components for device lookup.
    
    In the Mojom interface, change WebBluetoothDeviceId mapping data type
    from WTF::String to ::blink::WebBluetoothDeviceId to get the benefit of
    the existing base64 encode helper.
    
    In the renderer process side, at needed call sites, explicitly uses
    ::blink::WebBluetoothDeviceId base64 encode helper to get base64 encoded
    device id.
    
    Bug: 720244
    Change-Id: I165c96cdd573e5d66624223ce830979634f5f605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3289979
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/main@{#947169}

--- third_party/blink/public/common/bluetooth/web_bluetooth_device_id_mojom_traits.h
+++ third_party/blink/public/common/bluetooth/web_bluetooth_device_id_mojom_traits.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_BLUETOOTH_WEB_BLUETOOTH_DEVICE_ID_MOJOM_TRAITS_H_
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_BLUETOOTH_WEB_BLUETOOTH_DEVICE_ID_MOJOM_TRAITS_H_
 
-#include <string>
-
 #include "third_party/blink/public/common/bluetooth/web_bluetooth_device_id.h"
 #include "third_party/blink/public/mojom/bluetooth/web_bluetooth.mojom-shared.h"
 
@@ -16,18 +14,16 @@ namespace mojo {
 template <>
 struct StructTraits<blink::mojom::WebBluetoothDeviceIdDataView,
                     blink::WebBluetoothDeviceId> {
-  static const std::string& device_id(
+  static const blink::WebBluetoothDeviceIdKey device_id(
       const blink::WebBluetoothDeviceId& device_id) {
-    return device_id.str();
+    return device_id.DeviceId();
   }
 
   static bool Read(blink::mojom::WebBluetoothDeviceIdDataView input,
                    blink::WebBluetoothDeviceId* output) {
-    std::string result;
+    blink::WebBluetoothDeviceIdKey result;
 
     if (!input.ReadDeviceId(&result))
-      return false;
-    if (!blink::WebBluetoothDeviceId::IsValid(result))
       return false;
 
     *output = blink::WebBluetoothDeviceId(std::move(result));
--- third_party/blink/renderer/platform/mojo/bluetooth_mojom_traits.cc
+++ third_party/blink/renderer/platform/mojo/bluetooth_mojom_traits.cc
@@ -10,13 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 // static
-bool StructTraits<::blink::mojom::WebBluetoothDeviceIdDataView, WTF::String>::
-    Read(::blink::mojom::WebBluetoothDeviceIdDataView data,
-         WTF::String* output) {
-  return data.ReadDeviceId(output);
-}
-
-// static
 bool StructTraits<bluetooth::mojom::UUIDDataView, WTF::String>::Read(
     bluetooth::mojom::UUIDDataView data,
     WTF::String* output) {
