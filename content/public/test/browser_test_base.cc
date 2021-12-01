95abcefe4a07daa0fa0809bff03ec93aa4c67865
Daniel Nicoara <dnicoara@chromium.org>
2021-12-01 20:35:54 +0000

    Only allow perfetto system backend init from browser process
    Currently every process that attempts to initialize Perfetto will try
    to also initialize the system backend (this backend tries to connect
    to Perfetto tracing service via sockets). This functionality is only
    relevant to the browser process. In other processes (GPU, renderer,
    utility) trying to initialize this backend is rejected by the sandbox
    policy by returning error in socket() calls or crashing in
    getsockopt().
    
    The change is passing down this restriction from //content so that
    processes don't even attempt to initialize the system backend if they
    aren't supposed to.
    
    Bug: 1274142
    Test: content_browsertests --gtest_filter=FakeSystemTracingDevToolsProtocolTest.TracingWithFakeSystemBackend
    Change-Id: Ibc9e7b587c5b5c34d20091f95dd7824185bfcd40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307319
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947175}

--- content/public/test/browser_test_base.cc
+++ content/public/test/browser_test_base.cc
@@ -620,7 +620,8 @@ void BrowserTestBase::SetUp() {
 
     StartBrowserThreadPool();
     BrowserTaskExecutor::PostFeatureListSetup();
-    tracing::InitTracingPostThreadPoolStartAndFeatureList();
+    tracing::InitTracingPostThreadPoolStartAndFeatureList(
+        /* enable_consumer */ true);
     InitializeBrowserMemoryInstrumentationClient();
   }
 
