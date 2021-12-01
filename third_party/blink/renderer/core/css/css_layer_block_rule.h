7be81780047f272e61abf010ee8eeaf90e13ad55
Xiaocheng Hu <xiaochengh@chromium.org>
2021-12-01 01:03:56 +0000

    [@layer] Implement CSSLayerBlockRule and CSSLayerStatementRule API
    Bug: 1095765
    Change-Id: I108721fa4f6cfdf750cf70226cca00b96e2b32dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3299588
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946795}

--- third_party/blink/renderer/core/css/css_layer_block_rule.h
+++ third_party/blink/renderer/core/css/css_layer_block_rule.h
@@ -20,6 +20,8 @@ class CSSLayerBlockRule final : public CSSGroupingRule {
   CSSLayerBlockRule(StyleRuleLayerBlock*, CSSStyleSheet*);
   ~CSSLayerBlockRule() override;
 
+  String name() const;
+
   void Reattach(StyleRuleBase*) override;
   String cssText() const override;
 
--- third_party/blink/renderer/core/css/css_layer_statement_rule.h
+++ third_party/blink/renderer/core/css/css_layer_statement_rule.h
@@ -21,6 +21,8 @@ class CSSLayerStatementRule final : public CSSRule {
   CSSLayerStatementRule(StyleRuleLayerStatement*, CSSStyleSheet*);
   ~CSSLayerStatementRule() override;
 
+  Vector<String> nameList() const;
+
   void Reattach(StyleRuleBase*) override;
   String cssText() const override;
 
