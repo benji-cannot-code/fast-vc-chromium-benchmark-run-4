ac73a8207a57ac606ea63694a1a65c36df2531b7
Nina Satragno <nsatragno@chromium.org>
2021-12-01 20:13:07 +0000

    Revert "Include cronet by default in ChromeWebView builds"
    This reverts commit a9a982dc31fda09f2560c83cefe254491545d543.
    
    Reason for revert: suspected of making the tree red
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-full-configs/35590/overview
    
    Original change's description:
    > Include cronet by default in ChromeWebView builds
    >
    > In practice Cronet was always included in ChromeWebView builds, so to
    > minimize configuration differences, we flip the default value to match
    > reality more closely.
    >
    > Bug: 1271692
    > Change-Id: I553577604cc37f5a1f781963819742a14023741e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292246
    > Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    > Commit-Queue: John Wu <jzw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#947155}
    
    Bug: 1271692
    Change-Id: I7f760ad6eba3d816370b827c8e293d9333199f44
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311242
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Owners-Override: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947158}

--- ios/web_view/test/web_view_inttest_base.mm
+++ ios/web_view/test/web_view_inttest_base.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/web_view/test/web_view_inttest_base.h"
 
-#import <CronetChromeWebView/CronetChromeWebView.h>
+#import <ChromeWebView/ChromeWebView.h>
 #import <Foundation/Foundation.h>
 
 #include "base/base64.h"
