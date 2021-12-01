e0326df6ebf9fc00aeaaeccbd4420dec8282eae4
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-12-01 18:02:14 +0000

    [CrOS Bluetooth] Add enterprise policy badge
    This CL adds an icon to paired devices list item to indicate
    that a Bluetooth device is currently being managed by enterprise
    policy.
    
    Screenshot: https://screenshot.googleplex.com/B5mc8WXLoeoKti9.png
    
    Bug: 1010321
    Change-Id: Ib618a1ef3f839a66c836be61657f3eb937b22bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308390
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#947091}

--- chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_paired_bluetooth_list_item.js
+++ chrome/browser/resources/settings/chromeos/os_bluetooth_page/os_paired_bluetooth_list_item.js
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../../settings_shared_css.js';
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+import '//resources/cr_elements/icons.m.js';
+import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
 import 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_icon.js';
 import 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_device_battery_info.js';
 
@@ -51,6 +53,7 @@ class SettingsPairedBluetoothListItemElement extends
        */
       device: {
         type: Object,
+        observer: 'onDeviceChanged_',
       },
 
       /** The index of this item in its parent list, used for its a11y label. */
@@ -62,6 +65,27 @@ class SettingsPairedBluetoothListItemElement extends
        */
       listSize: Number,
     };
+  }
+
+  /** @override */
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    // Fire an event in case the tooltip was previously showing for the managed
+    // icon in this item and this item is being removed.
+    this.fireTooltipStateChangeEvent_(/*showTooltip=*/ false);
+  }
+
+  /** @private */
+  onDeviceChanged_() {
+    if (!this.device) {
+      return;
+    }
+
+    if (!this.device.deviceProperties.isBlockedByPolicy) {
+      // Fire an event in case the tooltip was previously showing for this
+      // icon and this icon now is hidden.
+      this.fireTooltipStateChangeEvent_(/*showTooltip=*/ false);
+    }
   }
 
   /**
@@ -289,6 +313,27 @@ class SettingsPairedBluetoothListItemElement extends
     const deviceName = this.getDeviceName_(device);
     return this.i18n(
         'bluetoothPairedDeviceItemSubpageButtonA11yLabel', deviceName);
+  }
+
+  /** @private */
+  onShowTooltip_() {
+    this.fireTooltipStateChangeEvent_(/*showTooltip=*/ true);
+  }
+
+  /**
+   * @param {boolean} showTooltip
+   */
+  fireTooltipStateChangeEvent_(showTooltip) {
+    this.dispatchEvent(new CustomEvent('managed-tooltip-state-change', {
+      bubbles: true,
+      composed: true,
+      detail: {
+        address: this.device.deviceProperties.address,
+        show: showTooltip,
+        element: showTooltip ? this.shadowRoot.getElementById('managedIcon') :
+                               undefined,
+      }
+    }));
   }
 }
 
