98c946e4d2593aee319a8d23ed1f157dc8697155
Pavol Marko <pmarko@google.com>
2021-12-01 14:28:43 +0000

    Fix usage of ScopedFeatureList in ancestor_throttle_unittest.cc
    ScopedFeatureList must be initialized before creating other threads
    which the test is doing through RenderViewHostTestHarness::SetUp().
    
    Bug: 1275502
    Test: doesn't fail under TSan
    Change-Id: I9a29a15ee6ee1459cfe401155361dcaaa0a119f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309647
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Owners-Override: Pavol Marko <pmarko@chromium.org>
    Auto-Submit: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946986}

--- content/browser/renderer_host/ancestor_throttle_unittest.cc
+++ content/browser/renderer_host/ancestor_throttle_unittest.cc
@@ -25,12 +25,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-using AncestorThrottleNavigationTest = RenderViewHostTestHarness;
+class AncestorThrottleNavigationTest : public RenderViewHostTestHarness {
+ public:
+  AncestorThrottleNavigationTest() {
+    scoped_feature_list.InitAndEnableFeature(features::kEmbeddingRequiresOptIn);
+  }
+  ~AncestorThrottleNavigationTest() override = default;
+
+  AncestorThrottleNavigationTest(const AncestorThrottleNavigationTest& other) =
+      delete;
+  AncestorThrottleNavigationTest& operator=(
+      const AncestorThrottleNavigationTest& other) = delete;
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list;
+};
 
 TEST_F(AncestorThrottleNavigationTest, EmbeddingOptInRequirement) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(features::kEmbeddingRequiresOptIn);
-
   // Set up the main frame. We'll add nested frames to it in the tests below.
   NavigateAndCommit(GURL("https://www.example.org"));
   auto* main_frame = static_cast<TestRenderFrameHost*>(main_rfh());
