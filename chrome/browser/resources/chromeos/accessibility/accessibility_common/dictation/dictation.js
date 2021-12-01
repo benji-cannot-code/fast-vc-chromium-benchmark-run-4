b5c3cd1880104feef6594cefaa7a1780b496d4aa
Akihiro Ota <akihiroota@chromium.org>
2021-12-01 18:24:08 +0000

    Dictation: Hide prototyped UI behind feature
    This change does an audit of all places where we show the prototyped
    UI. It should be gated behind the `commandsFeatureEnabled_` member,
    which corresponds to the ExperimentalAccessibilityDictationCommands
    feature.
    
    Manually verified that the UI doesn't show up when using Dictation.
    
    AX-Relnotes: N/A
    Bug: 1216111
    Change-Id: I56d045d5568c603456aa0356b0a93eca85114c05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310997
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#947106}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
@@ -374,15 +374,16 @@ export class Dictation {
    * @private
    */
   setInterimText_(text) {
+    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
+      // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
+      return;
+    }
+
     // TODO(crbug.com/1252037): Need to find a way to show interim text that is
     // only whitespace. Google Cloud Speech can return a newline character
     // although SODA does not seem to do that. The newline character looks wrong
     // here.
     this.interimText_ = text;
-    if (this.chromeVoxEnabled_) {
-      // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
-      return;
-    }
     this.inputController_.showAnnotation(this.interimText_);
     if (this.clearUITextTimeoutId_) {
       clearTimeout(this.clearUITextTimeoutId_);
@@ -396,10 +397,11 @@ export class Dictation {
    * @private
    */
   clearInterimText_() {
-    if (this.chromeVoxEnabled_) {
+    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
       // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
       return;
     }
+
     this.interimText_ = '';
     this.inputController_.showAnnotation('....');
     if (this.clearUITextTimeoutId_) {
@@ -421,6 +423,7 @@ export class Dictation {
       // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
       return;
     }
+
     if (macro.getMacroName() === MacroName.INPUT_TEXT_VIEW ||
         macro.getMacroName() === MacroName.NEW_LINE) {
       // Return to the '....' UI.
@@ -449,6 +452,7 @@ export class Dictation {
       // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
       return;
     }
+
     this.interimText_ = '';
     // TODO(crbug.com/1252037): Finalize string and internationalization.
     this.inputController_.showAnnotation(`ⓘ Failed to execute: ` + transcript);
@@ -463,11 +467,12 @@ export class Dictation {
    * @private
    */
   hideCommandsUI_() {
-    if (this.chromeVoxEnabled_) {
+    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
       return;
     }
-    this.inputController_.hideAnnotation();
+
     this.interimText_ = '';
+    this.inputController_.hideAnnotation();
     if (this.clearUITextTimeoutId_) {
       clearTimeout(this.clearUITextTimeoutId_);
       this.clearUITextTimeoutId_ = null;
