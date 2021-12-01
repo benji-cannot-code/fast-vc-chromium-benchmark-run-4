187d549d049d224e9b91bf4a5ed7826767195f53
Weizhong Xia <weizhong@google.com>
2021-12-01 18:18:53 +0000

    Turn off buffering on stderr
    After disabling buffering, we regain the progress bar effect
    when running rwt on console.
    
    Bug: 1265947
    Change-Id: I84d41b54627047ce5436c41043b9f85673ddcb08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309999
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Brian Ryner <bryner@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#947102}

--- third_party/blink/tools/blinkpy/web_tests/run_web_tests.py
+++ third_party/blink/tools/blinkpy/web_tests/run_web_tests.py
@@ -61,6 +61,8 @@ def main(argv, stderr):
     else:
         host = Host()
 
+    if six.PY3 and stderr.isatty():
+        stderr.reconfigure(write_through=True)
     printer = printing.Printer(host, options, stderr)
 
     try:
