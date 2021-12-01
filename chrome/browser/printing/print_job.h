a8fccb757fb2792fa2feaac222b535e16ca08742
dan sinclair <dsinclair@chromium.org>
2021-12-01 00:28:50 +0000

    [code health] Remove JobEventDetails::PAGE_DONE.
    This CL removes the use of the notification service to send the
    PAGE_DONE job event. Instead the PrintJobWorker calls OnPageDone on the
    PrintJob directly.
    
    This removes use of the NotificationService from this code path.
    
    Bug: 796051
    Change-Id: Ibe6ff4c1d5d4ccf724c8637a3e95d5fe599d66dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3303834
    Auto-Submit: Dan Sinclair <dsinclair@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946782}

--- chrome/browser/printing/print_job.h
+++ chrome/browser/printing/print_job.h
@@ -85,6 +85,9 @@ class PrintJob : public base::RefCountedThreadSafe<PrintJob>,
   // of pages, because all PDF pages will be converted, but only the user's
   // selected pages should be sent to the printer. See https://crbug.com/823876.
   void ResetPageMapping();
+
+  // Called when |page| is done printing.
+  void OnPageDone(PrintedPage* page);
 #endif
 
   // content::NotificationObserver implementation.
@@ -255,20 +258,9 @@ class JobEventDetails : public base::RefCountedThreadSafe<JobEventDetails> {
     JOB_DONE,
 
     // An error occured. Printing is canceled.
-    FAILED,
-
-#if defined(OS_WIN)
-    // A page is done printing. Only used on Windows.
-    PAGE_DONE,
-#endif
+    FAILED
   };
 
-#if defined(OS_WIN)
-  JobEventDetails(Type type,
-                  int job_id,
-                  PrintedDocument* document,
-                  PrintedPage* page);
-#endif
   JobEventDetails(Type type, int job_id, PrintedDocument* document);
 
   JobEventDetails(const JobEventDetails&) = delete;
@@ -276,9 +268,6 @@ class JobEventDetails : public base::RefCountedThreadSafe<JobEventDetails> {
 
   // Getters.
   PrintedDocument* document() const;
-#if defined(OS_WIN)
-  PrintedPage* page() const;
-#endif
   Type type() const {
     return type_;
   }
@@ -290,9 +279,6 @@ class JobEventDetails : public base::RefCountedThreadSafe<JobEventDetails> {
   ~JobEventDetails();
 
   scoped_refptr<PrintedDocument> document_;
-#if defined(OS_WIN)
-  scoped_refptr<PrintedPage> page_;
-#endif
   const Type type_;
   int job_id_;
 };
