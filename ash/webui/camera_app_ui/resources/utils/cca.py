2fab3e8c2642af9b8d16c7546c2d48aec590af9d
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-01 05:27:27 +0000

    CCA: Don't decompress compressed file in cca deploy
    After CL:3295870 pak_util defaults to also decompress compressed
    resources. Since we don't need to decompress those for cca deploy, and
    decompressing those need the brotli binary, add '--raw' to avoid the
    decompressing.
    
    Bug: b:208523302
    Test: manually try `cca deploy`
    Change-Id: I997f6f5b11f8d8ca0ca137973c0644646c684b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307867
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946873}

--- ash/webui/camera_app_ui/resources/utils/cca.py
+++ ash/webui/camera_app_ui/resources/utils/cca.py
@@ -94,6 +94,7 @@ def deploy(args):
         extract_resources_pak_cmd = [
             pak_util_script,
             'extract',
+            '--raw',
             os.path.join(target_dir, 'resources.pak'),
             '-o',
             tmp_dir,
@@ -103,6 +104,7 @@ def deploy(args):
         extract_camera_pak_cmd = [
             pak_util_script,
             'extract',
+            '--raw',
             os.path.join(target_dir, 'gen/ash/ash_camera_app_resources.pak'),
             '-o',
             tmp_dir,
