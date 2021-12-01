8e3baf58fbbdec55230dd75b738d7e77481e4f46
Kuo Jen Wei <inker@google.com>
2021-12-01 08:37:25 +0000

    CCA: Fix focus not move onto document mode intro dialog bug
    Bug: b:208551560
    Test: Manually with chromevox
    Change-Id: If15140eb60c17e9c54ea278c566df6204590eab6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307871
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#946903}

--- ash/webui/camera_app_ui/resources/js/views/camera.js
+++ ash/webui/camera_app_ui/resources/js/views/camera.js
@@ -530,7 +530,8 @@ export class Camera extends View {
     // to take document photo with space key as shortcut. See b/196907822.
     const checkRefocus = () => {
       if (!state.get(state.State.CAMERA_CONFIGURING) && state.get(Mode.SCAN) &&
-          this.scanOptions_.isDocumentModeEanbled()) {
+          this.scanOptions_.isDocumentModeEanbled() &&
+          nav.isTopMostView(this.name)) {
         dom.getAll('button.shutter', HTMLButtonElement)
             .forEach((btn) => btn.offsetParent && btn.focus());
       }
