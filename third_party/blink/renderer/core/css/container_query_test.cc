d546b41a72eb8930e4a512b72e89761035554142
Rune Lillesveen <futhark@chromium.org>
2021-12-01 23:55:23 +0000

    Reland "[@container] Move container queries to test status"
    This is a reland of 890d69e3d6d2905fea37128a91518a8e5a9c9035
    
    Reland after test fix in:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/3307047
    
    Original change's description:
    > [@container] Move container queries to test status
    >
    > Remove virtual test suite and enable testing for status:test.
    >
    > Bug: 1145970
    > Change-Id: I44127d642102841168aa633de80886f72ae4ce8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3303692
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Reviewed-by: Stephen Chenney <schenney@chromium.org>
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#946940}
    
    Bug: 1145970
    Change-Id: I77377e00a8da12fbe2188c06e7fb79da63d9b97d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310605
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947265}

--- third_party/blink/renderer/core/css/container_query_test.cc
+++ third_party/blink/renderer/core/css/container_query_test.cc
@@ -24,9 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ContainerQueryTest : public PageTestBase,
-                           private ScopedCSSContainerQueriesForTest {
+                           private ScopedCSSContainerQueriesForTest,
+                           private ScopedLayoutNGForTest {
  public:
-  ContainerQueryTest() : ScopedCSSContainerQueriesForTest(true) {}
+  ContainerQueryTest()
+      : ScopedCSSContainerQueriesForTest(true), ScopedLayoutNGForTest(true) {}
 
   bool HasUnknown(StyleRuleContainer* rule) {
     return rule && rule->GetContainerQuery().Query().HasUnknown();
