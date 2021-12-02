90f223389348bf9bad42b301311394de0844b88e
Jonah <jonahwilliams@chromium.org>
2021-12-02 07:35:46 +0000

    [ash] take capture during webui tab drag
    The web ui tabstrip requires a capture to drag correctly in ash chrome,
    where there is no toplevel drag handler created. I observed that a
    longpress/gesture start events on the tab items would not start the
    drag reaction in the tabstrip if the target was not changed from the
    tab container. Not exactly sure, but to remove capture we might need
    to revise how the tabstrip responds to gestures.
    
    To avoid duplicating this code, move the capture/drag_tracker management
    into a new class that is shared by the tab_drag_drop_delegate and the
    extended_drag_source classes.
    
    Bug: 1270492
    Change-Id: I486219301126035fce85d29a2b8666ec62d95d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307416
    Commit-Queue: Jonah Williams <jonahwilliams@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947392}

--- ash/drag_drop/drag_drop_controller.cc
+++ ash/drag_drop/drag_drop_controller.cc
@@ -171,16 +171,19 @@ DragOperation DragDropController::StartDragAndDrop(
 
   operation_ = DragOperation::kNone;
   current_drag_event_source_ = source;
+  capture_delegate_ = nullptr;
 
   // When an extended drag is started, a capture window will be created to
   // handle moving gestures between different wl surfaces to support dragging
   // chrome tabs into and out of browsers.
   if (source == ui::mojom::DragEventSource::kTouch &&
       toplevel_window_drag_delegate_) {
-    using_drag_capture_ = toplevel_window_drag_delegate_->TakeCapture(
+    toplevel_window_drag_delegate_->TakeCapture(
         root_window, source_window,
         base::BindRepeating(&DragDropController::CancelIfInProgress,
-                            base::Unretained(this)));
+                            base::Unretained(this)),
+        ui::TransferTouchesBehavior::kCancel);
+    capture_delegate_ = toplevel_window_drag_delegate_;
   }
 
   drag_source_window_ = source_window;
@@ -217,6 +220,16 @@ DragOperation DragDropController::StartDragAndDrop(
         root_window, drag_source_window_, start_location_);
     static_cast<DragImageView*>(drag_image_widget_->GetContentsView())
         ->SetTouchDragOperationHintOff();
+    // Avoid taking capture twice if the toplevel drag delegate is being used.
+    if (!toplevel_window_drag_delegate_ &&
+        source == ui::mojom::DragEventSource::kTouch) {
+      tab_drag_drop_delegate_->TakeCapture(
+          root_window, source_window,
+          base::BindRepeating(&DragDropController::CancelIfInProgress,
+                              base::Unretained(this)),
+          ui::TransferTouchesBehavior::kDontCancel);
+      capture_delegate_ = tab_drag_drop_delegate_.get();
+    }
   }
 
   if (should_block_during_drag_drop_) {
@@ -392,9 +405,8 @@ void DragDropController::OnGestureEvent(ui::GestureEvent* event) {
   }
 
   aura::Window* translated_target;
-  if (using_drag_capture_) {
-    translated_target =
-        toplevel_window_drag_delegate_->GetTarget(touch_offset_event);
+  if (capture_delegate_) {
+    translated_target = capture_delegate_->GetTarget(touch_offset_event);
   } else {
     ui::Event::DispatcherApi(&touch_offset_event).set_target(event->target());
     translated_target = GetTarget(touch_offset_event);
@@ -407,9 +419,9 @@ void DragDropController::OnGestureEvent(ui::GestureEvent* event) {
   }
 
   ui::LocatedEvent* translated_event;
-  if (using_drag_capture_) {
-    translated_event = toplevel_window_drag_delegate_->ConvertEvent(
-        translated_target, touch_offset_event);
+  if (capture_delegate_) {
+    translated_event =
+        capture_delegate_->ConvertEvent(translated_target, touch_offset_event);
   } else {
     translated_event =
         ConvertEvent(translated_target, touch_offset_event).release();
@@ -429,11 +441,10 @@ void DragDropController::OnGestureEvent(ui::GestureEvent* event) {
       // drag drop is still in progress. The drag drop ends only when the nested
       // message loop ends. Due to this, we have to defer forwarding
       // the long tap.
-      if (using_drag_capture_) {
+      if (capture_delegate_) {
         pending_long_tap_ = std::make_unique<ui::GestureEvent>(
             *event,
-            static_cast<aura::Window*>(
-                toplevel_window_drag_delegate_->capture_window()),
+            static_cast<aura::Window*>(capture_delegate_->capture_window()),
             static_cast<aura::Window*>(drag_source_window_));
       } else {
         pending_long_tap_ = ui::Event::Clone(*event);
@@ -700,7 +711,7 @@ void DragDropController::Cleanup() {
   drag_data_.reset();
   allowed_operations_ = 0;
   tab_drag_drop_delegate_.reset();
-  using_drag_capture_ = false;
+  capture_delegate_ = nullptr;
 }
 
 void DragDropController::PerformDrop(
