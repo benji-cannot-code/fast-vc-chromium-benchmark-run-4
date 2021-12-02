74aa891aae232ff9275bdc9a906515c0ae86d8be
Nan Lin <linnan@chromium.org>
2021-12-02 22:31:56 +0000

    Fenced Frames: Addressing issues with invalid urn:uuid in fenced frames
    The following issues are addressed:
    1. In ShadowDOM implementation, the CanAccessDataForOrigin CHECK fails
    while committing error page since it's trying to commit
    initiator origin navigation into a process locked to urn: URL. This is
    fixed by allowing error page isolation for fenced frames.
    2. WebContentsObserverConsistencyChecker and
    RenderFrameHostImpl::FailedNavigation expect the navigation to be
    considered started before failing.
    3. OnRequestFailedInternal() should not skip throttle as throttles
    should get a chance to run on WillFailRequest() and override the error
    page behavior.
    
    Bug: 1264224
    Change-Id: I8598250077fc4681d6e932e78534bc30a379ee45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3285384
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947675}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -1812,9 +1812,10 @@ void NavigationRequest::BeginNavigationImpl() {
                                         pending_ad_components_map);
 
     if (!mapped_url) {
+      StartNavigation();
       OnRequestFailedInternal(
           network::URLLoaderCompletionStatus(net::ERR_INVALID_URL),
-          true /* skip_throttles */, absl::nullopt /* error_page_content*/,
+          false /* skip_throttles */, absl::nullopt /* error_page_content*/,
           false /* collapse_frame */);
       return;
     }
@@ -3541,8 +3542,7 @@ void NavigationRequest::OnRequestFailedInternal(
   // The check for WebUI should be performed only if error page isolation is
   // enabled for this failed navigation. It is possible for subframe error page
   // to be committed in a WebUI process as shown in https://crbug.com/944086.
-  if (SiteIsolationPolicy::IsErrorPageIsolationEnabled(
-          frame_tree_node_->IsMainFrame())) {
+  if (frame_tree_node_->IsErrorPageIsolationEnabled()) {
     if (!Navigator::CheckWebUIRendererDoesNotDisplayNormalURL(
             render_frame_host_, GetUrlInfo(),
             /* is_renderer_initiated_check */ false)) {
@@ -3566,10 +3566,8 @@ NavigationRequest::ErrorPageProcess NavigationRequest::ComputeErrorPageProcess(
     int net_error) {
   // By policy we can isolate all error pages from both the current and
   // destination processes.
-  if (SiteIsolationPolicy::IsErrorPageIsolationEnabled(
-          frame_tree_node_->IsMainFrame())) {
+  if (frame_tree_node_->IsErrorPageIsolationEnabled())
     return ErrorPageProcess::kIsolatedProcess;
-  }
 
   // Decide whether to leave the error page in the original process.
   // * If this was a renderer-initiated navigation, and the request is blocked
