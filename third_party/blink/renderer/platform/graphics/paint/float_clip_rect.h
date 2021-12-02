0a00e93053d6787306706384475ca62789d139ba
Lei Zhang <thestig@chromium.org>
2021-12-02 23:12:53 +0000

    Fix headers that include themselves.
    Remove these useless includes.
    
    Change-Id: Ida56c0c12090a796f6d81a57d5b742034c5afebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309224
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947714}

--- third_party/blink/renderer/platform/graphics/paint/float_clip_rect.h
+++ third_party/blink/renderer/platform/graphics/paint/float_clip_rect.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/geometry/float_rounded_rect.h"
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
-#include "third_party/blink/renderer/platform/graphics/paint/float_clip_rect.h"
 #include "third_party/blink/renderer/platform/transforms/transformation_matrix.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "ui/gfx/geometry/rect_f.h"
