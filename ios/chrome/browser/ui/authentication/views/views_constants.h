a2089c3affa9543029652b63ecfcc35367746109
Jérôme Lebel <jlebel@chromium.org>
2021-12-02 13:24:21 +0000

    [iOS] Add kIdentityPickerAddAccountIdentifier in web sign-in dialog
    kIdentityPickerAddAccountIdentifier is the accessibility string for
    the add account button in the web sign-in dialog.
    
    This patch is needed for crrev.com/i/4295394
    
    Change-Id: Ia1b28983ef148faec7e65c0a13a27a5138571c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310877
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947454}

--- ios/chrome/browser/ui/authentication/views/views_constants.h
+++ ios/chrome/browser/ui/authentication/views/views_constants.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Accessibility identifier for the identity picker view.
 extern NSString* const kIdentityButtonControlIdentifier;
+// Accessibility identifier for "Add Account" button in the identity picker
+// view.
+extern NSString* const kIdentityPickerAddAccountIdentifier;
 
 // Style for the identity view (modify the avatar size, font sizes and some
 // margins).
