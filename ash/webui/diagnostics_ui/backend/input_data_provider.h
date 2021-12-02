d4b3660092b4c8abc6dc8026cbbab8ffc88f63b6
Kenneth Albanowski <kenalba@google.com>
2021-12-02 19:31:27 +0000

    input: refactor input diagnostics for touch/keyboard
    Separate keyboard and touch device handling, to
    prepare for expanding their complexity.
    
    This is a pure refactor, and not intended to
    change any functionality, or active code.
    
    Bug: 1207678
    Test: ./out/Debug/ash_webui_unittests --gtest_filter=*InputDataProviderTest*
    Change-Id: Ieec8435683ec2c311f3c7d475f6ea034a0d6a247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279870
    Auto-Submit: Kenneth Albanowski <kenalba@google.com>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Harry Cutts <hcutts@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947611}

--- ash/webui/diagnostics_ui/backend/input_data_provider.h
+++ ash/webui/diagnostics_ui/backend/input_data_provider.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WEBUI_DIAGNOSTICS_UI_BACKEND_INPUT_DATA_PROVIDER_H_
 #define ASH_WEBUI_DIAGNOSTICS_UI_BACKEND_INPUT_DATA_PROVIDER_H_
 
+#include "ash/webui/diagnostics_ui/backend/input_data_provider_keyboard.h"
+#include "ash/webui/diagnostics_ui/backend/input_data_provider_touch.h"
 #include "ash/webui/diagnostics_ui/mojom/input_data_provider.mojom.h"
 #include "base/containers/flat_map.h"
 #include "base/files/file_path.h"
@@ -21,8 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ozone/device/device_event_observer.h"
 #include "ui/events/ozone/device/device_manager.h"
 #include "ui/events/ozone/evdev/event_device_info.h"
-#include "ui/events/ozone/layout/xkb/xkb_evdev_codes.h"
-#include "ui/events/ozone/layout/xkb/xkb_keyboard_layout_engine.h"
 
 namespace ash {
 namespace diagnostics {
@@ -76,8 +76,8 @@ class InputDataProvider : public mojom::InputDataProvider,
   void AddTouchDevice(int id, const ui::EventDeviceInfo* device_info);
   void AddKeyboard(int id, const ui::EventDeviceInfo* device_info);
 
-  void ProcessXkbLayout(GetKeyboardVisualLayoutCallback callback);
-  mojom::KeyGlyphSetPtr LookupGlyphSet(uint32_t evdev_code);
+  InputDataProviderKeyboard keyboard_helper_;
+  InputDataProviderTouch touch_helper_;
 
   base::flat_map<int, mojom::KeyboardInfoPtr> keyboards_;
   base::flat_map<int, mojom::TouchDeviceInfoPtr> touch_devices_;
@@ -87,9 +87,6 @@ class InputDataProvider : public mojom::InputDataProvider,
   mojo::Receiver<mojom::InputDataProvider> receiver_{this};
 
   std::unique_ptr<ui::DeviceManager> device_manager_;
-
-  ui::XkbEvdevCodes xkb_evdev_codes_;
-  ui::XkbKeyboardLayoutEngine xkb_layout_engine_;
 
   base::WeakPtrFactory<InputDataProvider> weak_factory_{this};
 };
