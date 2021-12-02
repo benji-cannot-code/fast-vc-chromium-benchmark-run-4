e5e3041d0e057f6b1ead07f6886a61e7c309c48d
Shane Fitzpatrick <shanefitz@google.com>
2021-12-02 18:38:38 +0000

    [FastPair] Cleanup some include statements
    Change-Id: Ia7a3c0d724f844b85b5f646ef94b3aeb0af8846d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312626
    Auto-Submit: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Reviewed-by: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#947580}

--- ash/quick_pair/fast_pair_handshake/fast_pair_data_encryptor.h
+++ ash/quick_pair/fast_pair_handshake/fast_pair_data_encryptor.h
@@ -9,13 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
+
 #include "ash/services/quick_pair/public/cpp/decrypted_passkey.h"
 #include "ash/services/quick_pair/public/cpp/decrypted_response.h"
 #include "base/callback.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
-
-#include <array>
-
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace {
