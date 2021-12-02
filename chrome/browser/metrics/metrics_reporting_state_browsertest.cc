aa99b141ccc30e88d4f4665ee57f811b05e5affb
Carlos Caballero <carlscab@google.com>
2021-12-02 01:19:10 +0000

    [CodeHealth] Remove DictionaryValue::GetBoolean()
    https://docs.google.com/document/d/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ
    
    This CL was uploaded by git cl split.
    
    R=holte@chromium.org
    
    Bug: 1187033
    Change-Id: I504b36dd156913c4c44287194cb8f531f9c2b93b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297913
    Auto-Submit: Carlos Caballero Grolimund <carlscab@google.com>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947309}

--- chrome/browser/metrics/metrics_reporting_state_browsertest.cc
+++ chrome/browser/metrics/metrics_reporting_state_browsertest.cc
@@ -27,12 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/metrics_pref_names.h"
 #include "components/metrics/metrics_service_accessor.h"
 #include "content/public/test/browser_test.h"
+#include "testing/gmock/include/gmock/gmock-matchers.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ash/settings/device_settings_cache.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #endif
+
+using ::testing::Optional;
 
 // ChromeBrowserMainExtraParts implementation that asserts the metrics and
 // reporting state matches a particular value in PreCreateThreads().
@@ -105,10 +108,9 @@ class MetricsReportingStateTest : public InProcessBrowserTest,
     ASSERT_TRUE(pref_values) << error_message;
     base::DictionaryValue* pref_dict_values = nullptr;
     ASSERT_TRUE(pref_values->GetAsDictionary(&pref_dict_values));
-    bool enabled = false;
-    ASSERT_TRUE(pref_dict_values->GetBoolean(
-        metrics::prefs::kMetricsReportingEnabled, &enabled));
-    EXPECT_EQ(!is_metrics_reporting_enabled_initial_value(), enabled);
+    EXPECT_THAT(pref_dict_values->FindBoolPath(
+                    metrics::prefs::kMetricsReportingEnabled),
+                Optional(!is_metrics_reporting_enabled_initial_value()));
     InProcessBrowserTest::TearDown();
   }
 
