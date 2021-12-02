24bc5561a0e9319b22bfeaf5ca89e3ffbe2b2996
Miyoung Shin <myid.shin@igalia.com>
2021-12-02 03:53:04 +0000

    [mparch] Do not disable PCScan when the prerendering page is navigated.
    StarScanLoadObserver disables PCScan in ReadyToCommitNavigation() and
    re-enables it in DidStopLoading(), but the navigation for the
    prerendering page doesn't invoke DidStopLoading().
    
    This CL changes not to disable PCScan during the prerendering.
    
    Bug: 1274551, 1231679
    Change-Id: Ie5de0acf5caf90adcfcea5d6b474a13006e8c4d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304369
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#947348}

--- content/browser/starscan_load_observer.cc
+++ content/browser/starscan_load_observer.cc
@@ -7,7 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/partition_allocator/starscan/pcscan.h"
 #include "base/logging.h"
+#include "content/browser/renderer_host/frame_tree.h"
+#include "content/browser/renderer_host/render_frame_host_impl.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/navigation_handle.h"
 
 namespace content {
 
@@ -27,6 +30,14 @@ StarScanLoadObserver::~StarScanLoadObserver() {
 void StarScanLoadObserver::ReadyToCommitNavigation(
     NavigationHandle* navigation_handle) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+
+  // We don't disable PCScan for a prerendering page's navigation since
+  // it doesn't invoke DidStopLoading.
+  RenderFrameHostImpl* rfh = static_cast<RenderFrameHostImpl*>(
+      navigation_handle->GetRenderFrameHost());
+  if (rfh->frame_tree()->type() == FrameTree::Type::kPrerender)
+    return;
+
   // Protect against ReadyToCommitNavigation() being called twice in a row.
   if (is_loading_)
     return;
