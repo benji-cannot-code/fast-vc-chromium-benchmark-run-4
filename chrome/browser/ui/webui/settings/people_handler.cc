3fa8c7db67d1deb2ef3d4b581505875339521650
David Roger <droger@chromium.org>
2021-12-02 13:01:11 +0000

    [Lacros] Fix signin-related logic in PeopleHandler
    Dice is not supported on Lacros. This CL stops running the Dice code on
    that platforms, and prepares for disabling ENABLE_DICE_SUPPORT on Lacros.
    
    Bug: 1198523
    Change-Id: I5a7dfb3d529d37ce5abf82508de58f61368662b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310885
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947447}

--- chrome/browser/ui/webui/settings/people_handler.cc
+++ chrome/browser/ui/webui/settings/people_handler.cc
@@ -264,15 +264,17 @@ void PeopleHandler::RegisterMessages() {
                                          base::Unretained(this)));
 #else
   web_ui()->RegisterDeprecatedMessageCallback(
+      "SyncSetupStartSignIn",
+      base::BindRepeating(&PeopleHandler::HandleStartSignin,
+                          base::Unretained(this)));
+#endif
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+  web_ui()->RegisterDeprecatedMessageCallback(
       "SyncSetupSignout", base::BindRepeating(&PeopleHandler::HandleSignout,
                                               base::Unretained(this)));
   web_ui()->RegisterDeprecatedMessageCallback(
       "SyncSetupPauseSync", base::BindRepeating(&PeopleHandler::HandlePauseSync,
                                                 base::Unretained(this)));
-  web_ui()->RegisterDeprecatedMessageCallback(
-      "SyncSetupStartSignIn",
-      base::BindRepeating(&PeopleHandler::HandleStartSignin,
-                          base::Unretained(this)));
 #endif
   web_ui()->RegisterDeprecatedMessageCallback(
       "SyncSetupGetStoredAccounts",
@@ -326,6 +328,7 @@ void PeopleHandler::DisplayGaiaLogin(signin_metrics::AccessPoint access_point) {
 
 void PeopleHandler::DisplayGaiaLoginInNewTabOrWindow(
     signin_metrics::AccessPoint access_point) {
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
   Browser* browser =
       chrome::FindBrowserWithWebContents(web_ui()->GetWebContents());
   if (!browser)
@@ -357,8 +360,12 @@ void PeopleHandler::DisplayGaiaLoginInNewTabOrWindow(
     browser->window()->ShowAvatarBubbleFromAvatarButton(
         BrowserWindow::AVATAR_BUBBLE_MODE_SIGNIN, access_point, false);
   }
-}
+#else
+  // TODO(https://crbug.com/1260291): Add support for Lacros.
+  NOTIMPLEMENTED();
 #endif
+}
+#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
 void PeopleHandler::OnDidClosePage(const base::ListValue* args) {
   // Don't mark setup as complete if "didAbort" is true, or if authentication
@@ -610,7 +617,9 @@ void PeopleHandler::HandleStartSignin(const base::ListValue* args) {
 
   DisplayGaiaLogin(signin_metrics::AccessPoint::ACCESS_POINT_SETTINGS);
 }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
 void PeopleHandler::HandleSignout(const base::ListValue* args) {
   bool delete_profile = false;
   if (args->GetList()[0].is_bool())
@@ -668,7 +677,7 @@ void PeopleHandler::HandlePauseSync(const base::ListValue* args) {
       ->InvalidateRefreshTokenForPrimaryAccount(
           signin_metrics::SourceForRefreshTokenOperation::kSettings_PauseSync);
 }
-#endif
+#endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
 void PeopleHandler::HandleStartKeyRetrieval(const base::ListValue* args) {
   Browser* browser =
