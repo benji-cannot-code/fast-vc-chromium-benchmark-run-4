017fdb59b31d4c1423a7c627bcdac07803fac924
Vishwas Uppoor <vishwasuppoor@google.com>
2021-12-02 01:54:32 +0000

    Add metrics for average time to fetch images for autofill_image_fetcher.
    Change-Id: Ibed1715697ecb8824ab7f3c4bfae8d8f24dce6ae
    Bug: 1262156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3273734
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947319}

--- components/autofill/core/browser/ui/autofill_image_fetcher.h
+++ components/autofill/core/browser/ui/autofill_image_fetcher.h
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace gfx {
 class Image;
@@ -49,7 +51,10 @@ class ImageFetchOperation : public base::RefCounted<ImageFetchOperation> {
   ImageFetchOperation& operator=(const ImageFetchOperation&) = delete;
 
   // Invoked when an image fetch is complete and data is returned.
-  void ImageFetched(const GURL& card_art_url, const gfx::Image& card_art_image);
+  void ImageFetched(
+      const GURL& card_art_url,
+      const gfx::Image& card_art_image,
+      const absl::optional<base::TimeTicks>& fetch_image_request_timestamp);
 
  private:
   friend class base::RefCounted<ImageFetchOperation>;
@@ -102,6 +107,7 @@ class AutofillImageFetcher : public KeyedService {
   static void OnCardArtImageFetched(
       const scoped_refptr<ImageFetchOperation>& operation,
       const GURL& card_art_url,
+      const absl::optional<base::TimeTicks>& fetch_image_request_timestamp,
       const gfx::Image& card_art_image,
       const image_fetcher::RequestMetadata& metadata);
 };
