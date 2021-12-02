a6ca07ff40cdf7e40b250b460c00887bd0b56930
Alan Screen <awscreen@chromium.org>
2021-12-02 18:44:43 +0000

    Add Metafile interface to get data as shared memory region
    Transferring print job data from the browser process to the Print
    Backend service is done using a shared memory region to improve
    performance.  Introduce a method to get the print job data from a
    metafile in this format for the browser (used by PrintJobWorker in
    https://crrev.com/c/3193950).  The Print Backend service will read from
    this shared region to then submit the job data to the system printing
    APIs.
    
    Included is a query to determine if cross-process Metafile users should
    make a copy of data from a shared memory region before initializing the
    metafile.  This is related to security concerns for operating directly
    on data from shared memory (crbug.com/1135729).  Callers need not make
    an extra copy for implementations which automatically make copies of
    the provided data.
    
    Bug: 809738
    Change-Id: Iae169dab77ea32053d37327ae064989bf0ae0c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3183192
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947585}

--- printing/metafile_skia.cc
+++ printing/metafile_skia.cc
@@ -47,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// `InitFromData()` should make a copy of data for the safety of all operations
+// which would then operate upon that.
+constexpr bool kInitFromDataCopyData = true;
+
 bool WriteAssetToBuffer(const SkStreamAsset* asset, void* buffer, size_t size) {
   // Calling duplicate() keeps original asset state unchanged.
   std::unique_ptr<SkStreamAsset> assetCopy(asset->duplicate());
@@ -121,7 +125,7 @@ void MetafileSkia::UtilizeTypefaceContext(
 // MetafileSkia does.
 bool MetafileSkia::InitFromData(base::span<const uint8_t> data) {
   data_->data_stream = std::make_unique<SkMemoryStream>(
-      data.data(), data.size(), /*copy_data=*/true);
+      data.data(), data.size(), kInitFromDataCopyData);
   return true;
 }
 
@@ -265,6 +269,11 @@ bool MetafileSkia::GetData(void* dst_buffer, uint32_t dst_buffer_size) const {
     return false;
   return WriteAssetToBuffer(data_->data_stream.get(), dst_buffer,
                             base::checked_cast<size_t>(dst_buffer_size));
+}
+
+bool MetafileSkia::ShouldCopySharedMemoryRegionData() const {
+  // When `InitFromData()` copies the data, the caller doesn't have to.
+  return !kInitFromDataCopyData;
 }
 
 mojom::MetafileDataType MetafileSkia::GetDataType() const {
