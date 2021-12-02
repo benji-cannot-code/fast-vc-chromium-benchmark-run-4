8b30f60e82234fbee8ee7b4b187ecd5bd5f84bad
Yue Zhang <yuezhanggg@chromium.org>
2021-12-02 17:30:58 +0000

    Revert "[RBD] Support product ID extraction for more sites (3)"
    This reverts commit 4bdc3c1b615c4ac563aedd954f88c7c6bc365f50.
    
    Reason for revert: Try to make the heuristics more generic since
    we'll need to merge this
    
    Original change's description:
    > [RBD] Support product ID extraction for more sites (3)
    >
    > This CL also hardcodes the add-to-cart detection for that site.
    >
    > Bug: 1200533
    > Change-Id: I980b08a39a1c8f14e7f1d94d943e3e2fc55f4b8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310539
    > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#947231}
    
    Bug: 1200533
    Change-Id: I6180ea97f7bcd9f3d0ff3c6d5c5f6fdaf2a3b76c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312896
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Auto-Submit: Yue Zhang <yuezhanggg@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947546}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1400,8 +1400,7 @@ const FeatureEntry::FeatureParam kNtpChromeCartModuleAbandonedCartDiscount[] = {
     {ntp_features::kNtpChromeCartModuleAbandonedCartDiscountUseUtmParam,
      "true"},
     {"partner-merchant-pattern",
-     "(electronicexpress.com|zazzle.com|wish.com|homesquare.com|iherb.com|"
-     "zappos.com)"}};
+     "(electronicexpress.com|zazzle.com|wish.com|homesquare.com|iherb.com)"}};
 const FeatureEntry::FeatureParam kNtpChromeCartModuleHeuristicsImprovement[] = {
     {ntp_features::kNtpChromeCartModuleHeuristicsImprovementParam, "true"}};
 const FeatureEntry::FeatureParam kNtpChromeCartModuleRBDAndCouponDiscount[] = {
