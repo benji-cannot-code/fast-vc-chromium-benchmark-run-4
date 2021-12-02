4997f550d36ff4bfe0495fe4d15b6a5b44cb57d6
Hirthanan Subenderan <hirthanan@google.com>
2021-12-02 19:22:17 +0000

    Plumb API key for Fresnel service authorization.
    API key will be used for authenticating device with Fresnel Oneplatform
    API. This provides one layer of protection against unauthenticated
    requests.
    
    Googlers: go/cros-device-actives-client - See Authorization section
    
    Bug: 1204630
    Change-Id: I9dc9ea0391edd1057bc666c2c66c95a196ad0dd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069422
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947610}

--- google_apis/google_api_keys.cc
+++ google_apis/google_api_keys.cc
@@ -98,6 +98,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GOOGLE_API_KEY_READ_ALOUD DUMMY_API_TOKEN
 #endif
 
+// API key for the Fresnel API.
+#if !defined(GOOGLE_API_KEY_FRESNEL)
+#define GOOGLE_API_KEY_FRESNEL DUMMY_API_TOKEN
+#endif
+
 // These are used as shortcuts for developers and users providing
 // OAuth credentials via preprocessor defines or environment
 // variables.  If set, they will be used to replace any of the client
@@ -154,6 +159,10 @@ class APIKeyCache {
         GOOGLE_API_KEY_READ_ALOUD,
         STRINGIZE_NO_EXPANSION(GOOGLE_API_KEY_READ_ALOUD), nullptr,
         std::string(), environment.get(), command_line, gaia_config);
+
+    api_key_fresnel_ = CalculateKeyValue(
+        GOOGLE_API_KEY_FRESNEL, STRINGIZE_NO_EXPANSION(GOOGLE_API_KEY_FRESNEL),
+        nullptr, std::string(), environment.get(), command_line, gaia_config);
 
     metrics_key_ = CalculateKeyValue(
         GOOGLE_METRICS_SIGNING_KEY,
@@ -222,6 +231,7 @@ class APIKeyCache {
   std::string api_key_sharing() const { return api_key_sharing_; }
   std::string api_key_soda() const { return api_key_soda_; }
   std::string api_key_read_aloud() const { return api_key_read_aloud_; }
+  std::string api_key_fresnel() const { return api_key_fresnel_; }
 
   std::string metrics_key() const { return metrics_key_; }
 
@@ -336,6 +346,7 @@ class APIKeyCache {
   std::string api_key_sharing_;
   std::string api_key_soda_;
   std::string api_key_read_aloud_;
+  std::string api_key_fresnel_;
   std::string metrics_key_;
   std::string client_ids_[CLIENT_NUM_ITEMS];
   std::string client_secrets_[CLIENT_NUM_ITEMS];
@@ -370,6 +381,10 @@ std::string GetSodaAPIKey() {
 
 std::string GetReadAloudAPIKey() {
   return g_api_key_cache.Get().api_key_read_aloud();
+}
+
+std::string GetFresnelAPIKey() {
+  return g_api_key_cache.Get().api_key_fresnel();
 }
 
 #if defined(OS_IOS) || defined(OS_FUCHSIA)
