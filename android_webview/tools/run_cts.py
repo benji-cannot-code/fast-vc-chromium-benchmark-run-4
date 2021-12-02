940cb4ca3e9fecc78254fc2f763205901417c75e
Takuto Ikuta <tikuta@chromium.org>
2021-12-02 15:34:37 +0000

    android_webview: use vpython3 for run_cts.py
    Bug: 1275016
    Change-Id: I1e9d7d1ae261b299d070283fbf74918e3cc605a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308149
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947489}

--- android_webview/tools/run_cts.py
+++ android_webview/tools/run_cts.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env vpython
+#!/usr/bin/env vpython3
 #
 # Copyright 2016 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -144,7 +144,7 @@ def RunCTS(test_runner_args, local_cts_dir, apk, json_results_file=None):
 
 def MergeTestResults(existing_results_json, additional_results_json):
   """Appends results in additional_results_json to existing_results_json."""
-  for k, v in additional_results_json.iteritems():
+  for k, v in additional_results_json.items():
     if k not in existing_results_json:
       existing_results_json[k] = v
     else:
