00ef9ee12cc26c6d673344b6965e9d33d562e13e
Xiaohan Wang <xhwang@chromium.org>
2021-12-03 22:20:11 +0000

    components: Add missing include in autofill_suggestion_generator.h
    Since std::u16string is used, add include <string>.
    
    Change-Id: Ife830fc09d3a9f948c2fe515dc8a2189189f1f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314171
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948192}

--- components/autofill/core/browser/autofill_suggestion_generator.h
+++ components/autofill/core/browser/autofill_suggestion_generator.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_AUTOFILL_SUGGESTION_GENERATOR_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_AUTOFILL_SUGGESTION_GENERATOR_H_
 
+#include <string>
 #include <vector>
 
 #include "base/gtest_prod_util.h"
