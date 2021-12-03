8912f6753debfdab5a67f5c6d168cfcebf30a379
Rushan Suleymanov <rushans@google.com>
2021-12-03 16:02:44 +0000

    [Sync] Add UMA histogram for sync engine reset reasons
    Bug: 1276394
    Change-Id: Ib1695a28d2aa315700872dda409166b527d4502c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314762
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#948015}

--- components/sync/driver/sync_service_impl.cc
+++ components/sync/driver/sync_service_impl.cc
@@ -369,7 +369,8 @@ void SyncServiceImpl::CredentialsChanged() {
     if (!engine_) {
       NotifyObservers();
     }
-    ResetEngine(ShutdownReason::STOP_SYNC_AND_KEEP_DATA);
+    ResetEngine(ShutdownReason::STOP_SYNC_AND_KEEP_DATA,
+                ResetEngineReason::kCredentialsChanged);
     return;
   }
 
@@ -492,7 +493,8 @@ void SyncServiceImpl::Shutdown() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   NotifyShutdown();
-  ResetEngine(ShutdownReason::BROWSER_SHUTDOWN_AND_KEEP_DATA);
+  ResetEngine(ShutdownReason::BROWSER_SHUTDOWN_AND_KEEP_DATA,
+              ResetEngineReason::kShutdown);
 
   DCHECK(!data_type_manager_);
   data_type_controllers_.clear();
@@ -512,17 +514,20 @@ void SyncServiceImpl::Shutdown() {
   auth_manager_.reset();
 }
 
-void SyncServiceImpl::ResetEngine(ShutdownReason reason) {
+void SyncServiceImpl::ResetEngine(ShutdownReason shutdown_reason,
+                                  ResetEngineReason reset_reason) {
+  base::UmaHistogramEnumeration("Sync.ResetEngineReason", reset_reason);
+
   if (!engine_) {
     // If the engine hasn't started or is already shut down when a DISABLE_SYNC
     // happens, the Directory needs to be cleaned up here.
-    if (reason == ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA) {
+    if (shutdown_reason == ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA) {
       sync_client_->GetSyncApiComponentFactory()->ClearAllTransportData();
     }
     return;
   }
 
-  switch (reason) {
+  switch (shutdown_reason) {
     case ShutdownReason::STOP_SYNC_AND_KEEP_DATA:
     case ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA:
       RemoveClientFromServer();
@@ -545,7 +550,7 @@ void SyncServiceImpl::ResetEngine(ShutdownReason reason) {
       // When aborting as part of shutdown, we should expect an aborted sync
       // configure result, else we'll dcheck when we try to read the sync error.
       expect_sync_configuration_aborted_ = true;
-      data_type_manager_->Stop(reason);
+      data_type_manager_->Stop(shutdown_reason);
     }
     data_type_manager_.reset();
   }
@@ -554,7 +559,7 @@ void SyncServiceImpl::ResetEngine(ShutdownReason reason) {
   // snapshot.
   migrator_.reset();
 
-  engine_->Shutdown(reason);
+  engine_->Shutdown(shutdown_reason);
   engine_.reset();
 
   sync_enabled_weak_factory_.InvalidateWeakPtrs();
@@ -573,7 +578,7 @@ void SyncServiceImpl::ResetEngine(ShutdownReason reason) {
   NotifyObservers();
 
   // Now that everything is shut down, try to start up again.
-  switch (reason) {
+  switch (shutdown_reason) {
     case ShutdownReason::STOP_SYNC_AND_KEEP_DATA:
     case ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA:
       // If Sync is being stopped (either temporarily or permanently),
@@ -720,7 +725,8 @@ void SyncServiceImpl::OnUnrecoverableErrorImpl(
   // Shut the Sync machinery down. The existence of
   // |unrecoverable_error_reason_| and thus |DISABLE_REASON_UNRECOVERABLE_ERROR|
   // will prevent Sync from starting up again (even in transport-only mode).
-  ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA);
+  ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA,
+              ResetEngineReason::kUnrecoverableError);
 }
 
 void SyncServiceImpl::DataTypePreconditionChanged(ModelType type) {
@@ -871,10 +877,12 @@ void SyncServiceImpl::OnActionableError(const SyncProtocolError& error) {
     case STOP_SYNC_FOR_DISABLED_ACCOUNT:
       // Sync disabled by domain admin. Stop syncing until next restart.
       sync_disabled_by_admin_ = true;
-      ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA);
+      ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA,
+                  ResetEngineReason::kDisabledAccount);
       break;
     case RESET_LOCAL_SYNC_DATA:
-      ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA);
+      ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA,
+                  ResetEngineReason::kResetLocalData);
       break;
     case UNKNOWN_ACTION:
       NOTREACHED();
@@ -1441,7 +1449,8 @@ void SyncServiceImpl::OnSyncRequestedPrefChange(bool is_sync_requested) {
     // This will notify the observers.
     // TODO(crbug.com/856179): Evaluate whether we can get away without a
     // full restart in this case (i.e. just reconfigure).
-    ResetEngine(ShutdownReason::STOP_SYNC_AND_KEEP_DATA);
+    ResetEngine(ShutdownReason::STOP_SYNC_AND_KEEP_DATA,
+                ResetEngineReason::kRequestedPrefChange);
   }
 }
 
@@ -1673,7 +1682,8 @@ void SyncServiceImpl::StopAndClear() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   ClearUnrecoverableError();
-  ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA);
+  ResetEngine(ShutdownReason::DISABLE_SYNC_AND_CLEAR_DATA,
+              ResetEngineReason::kStopAndClear);
   // Note: ResetEngine(DISABLE_SYNC_AND_CLEAR_DATA) does *not* clear prefs which
   // are directly user-controlled such as the set of selected types here, so
   // that if the user ever chooses to enable Sync again, they start off with
@@ -1701,7 +1711,8 @@ void SyncServiceImpl::SetSyncAllowedByPlatform(bool allowed) {
     // TODO(crbug.com/856179): Evaluate whether we can get away without a full
     // restart (i.e. just reconfigure). See also similar comment in
     // OnSyncRequestedPrefChange().
-    ResetEngine(ShutdownReason::STOP_SYNC_AND_KEEP_DATA);
+    ResetEngine(ShutdownReason::STOP_SYNC_AND_KEEP_DATA,
+                ResetEngineReason::kSetSyncAllowedByPlatform);
   }
 }
 
@@ -1763,7 +1774,8 @@ void SyncServiceImpl::OverrideNetworkForTest(
   if (engine_) {
     // Use BROWSER_SHUTDOWN_AND_KEEP_DATA to prevent the engine from immediately
     // restarting.
-    ResetEngine(ShutdownReason::BROWSER_SHUTDOWN_AND_KEEP_DATA);
+    ResetEngine(ShutdownReason::BROWSER_SHUTDOWN_AND_KEEP_DATA,
+                ResetEngineReason::kShutdown);
     restart = true;
   }
   DCHECK(!engine_);
