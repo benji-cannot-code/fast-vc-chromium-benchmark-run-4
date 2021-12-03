c91d8ff9fd044fd122159601ca76e61f9dd88826
Etienne Bergeron <etienneb@chromium.org>
2021-12-03 21:37:37 +0000

    Add trace events for investigating WebUI RSS compression
    This CL is adding trace events to investigate the impact of
    decompressing resources (WebUI resources) on the UI thread.
    
    The added trace events are to track the sequence of events:
      1) WebUIDataSourceImpl::StartDataRequest
      2) ResourceBundle::LoadDataResourceBytesForScale
         a) compression::GzipUncompress
         b) compression::BrotliDecompress
      3) WebUIURLLoader::DataAvailable
      4) WebUIURLLoader::ReadData
    
    Also, the goal is to look at expensive resources other than WebUI
    that may be uncompressed on the UI thread.
    
    R=gab@chromium.org
    CC=​dpapad@chromium.org
    
    Bug: 1191597
    Change-Id: I895eaa575334ad0c885357e3e2039f0b24cd667b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307480
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948172}

--- content/browser/webui/web_ui_data_source_impl.cc
+++ content/browser/webui/web_ui_data_source_impl.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/trace_event/trace_event.h"
 #include "content/grit/content_resources.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/content_client.h"
@@ -329,6 +330,8 @@ void WebUIDataSourceImpl::StartDataRequest(
     const WebContents::Getter& wc_getter,
     URLDataSource::GotDataCallback callback) {
   const std::string path = URLDataSource::URLToRequestPath(url);
+  TRACE_EVENT1("ui", "WebUIDataSourceImpl::StartDataRequest", "path", path);
+
   if (!should_handle_request_callback_.is_null() &&
       should_handle_request_callback_.Run(path)) {
     filter_callback_.Run(path, std::move(callback));
