17a7635db7cac915afea25a857d33b9f2dcdeb55
Hirthanan Subenderan <hirthanan@google.com>
2021-12-03 14:32:24 +0000

    Implement dbus method to retrieve PsmDeviceActiveSecret.
    Dbus method will be used to retrieve a derivative of the
    stable_device_secret_DO_NOT_SHARE VPD entry. This is done to avoid
    exposing the raw stable device secret in Chrome in the worst case
    scenario of a chrome data leak.
    
    BUG=chromium:1204630
    
    Change-Id: If42e393abab082cc62429e4b760472d063fbcab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3237550
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947990}

--- ash/components/device_activity/device_activity_client_unittest.cc
+++ ash/components/device_activity/device_activity_client_unittest.cc
@@ -23,6 +23,8 @@ namespace {
 
 constexpr char kWifiServiceGuid[] = "wifi_guid";
 
+constexpr char kFakePsmDeviceActiveSecret[] = "FAKE_PSM_DEVICE_ACTIVE_SECRET";
+
 }  // namespace
 
 class MockDeviceActivityClient : public DeviceActivityClient {
@@ -31,7 +33,10 @@ class MockDeviceActivityClient : public DeviceActivityClient {
       NetworkStateHandler* handler,
       PrefService* local_state,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory)
-      : DeviceActivityClient(handler, local_state, url_loader_factory) {}
+      : DeviceActivityClient(handler,
+                             local_state,
+                             url_loader_factory,
+                             kFakePsmDeviceActiveSecret) {}
 
   MOCK_METHOD(void, TransitionToHealthCheck, (), (override));
   MOCK_METHOD(void, TransitionToCheckMembershipOprf, (), (override));
--- ash/components/device_activity/device_activity_controller.cc
+++ ash/components/device_activity/device_activity_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/device_activity/fresnel_pref_names.h"
 #include "base/check_op.h"
 #include "base/time/time.h"
+#include "chromeos/dbus/session_manager/session_manager_client.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -47,18 +48,32 @@ DeviceActivityController::~DeviceActivityController() {
 }
 
 void DeviceActivityController::Start(
-    Trigger t,
+    Trigger trigger,
     PrefService* local_state,
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory) {
-  if (t == Trigger::kNetwork) {
+  // Wrap with callback from |psm_device_active_secret_| retrieval using
+  // |SessionManagerClient| DBus.
+  chromeos::SessionManagerClient::Get()->GetPsmDeviceActiveSecret(
+      base::BindOnce(&device_activity::DeviceActivityController::
+                         OnPsmDeviceActiveSecretFetched,
+                     weak_factory_.GetWeakPtr(), trigger, local_state,
+                     url_loader_factory));
+}
+
+void DeviceActivityController::OnPsmDeviceActiveSecretFetched(
+    Trigger trigger,
+    PrefService* local_state,
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+    const std::string& psm_device_active_secret) {
+  if (trigger == Trigger::kNetwork) {
     da_client_network_ = std::make_unique<DeviceActivityClient>(
         chromeos::NetworkHandler::Get()->network_state_handler(), local_state,
-        url_loader_factory);
+        url_loader_factory, psm_device_active_secret);
   }
 }
 
-void DeviceActivityController::Stop(Trigger t) {
-  if (t == Trigger::kNetwork && da_client_network_) {
+void DeviceActivityController::Stop(Trigger trigger) {
+  if (trigger == Trigger::kNetwork && da_client_network_) {
     da_client_network_.reset();
   }
 }
