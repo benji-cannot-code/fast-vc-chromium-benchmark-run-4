6991ee9b39f584cc3c967f6f2ba495666d5601cd
David Bertoni <dbertoni@chromium.org>
2021-12-03 00:04:34 +0000

    [Extensions] Move some constants to where they're referenced.
    Some content settings API constants were only referenced in the API's
    implementation file, so there's no point in having them in shared
    files. With previous modifications to these constants, the shared
    files are no longer needed, so this CL removes them.
    
    Bug: 883570
    Change-Id: Id1457170859aa4041731c7a337935ea4676441d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311580
    Reviewed-by: Ghazale Hosseinabadi <ghazale@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947755}

--- chrome/browser/extensions/api/content_settings/content_settings_apitest.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_apitest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/extensions/api/content_settings/content_settings_api.h"
-#include "chrome/browser/extensions/api/content_settings/content_settings_api_constants.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/permissions/permission_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
