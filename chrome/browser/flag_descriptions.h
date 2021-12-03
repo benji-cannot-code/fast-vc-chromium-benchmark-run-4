3b15a22ef9059544879b3d94966b306d7595329e
Elizabeth Popova <lizapopova@google.com>
2021-12-03 10:51:58 +0000

    Revert "Delete multipaste related flags."
    This reverts commit 210a45309cf3e8ebb444aa2965f8965bcaf6347d.
    
    Reason for revert: VirtualKeyboardMultipaste consistent failure crbug.com/1274517
    
    Original change's description:
    > Delete multipaste related flags.
    >
    > Bug: b:192507005
    > Change-Id: I13a362f3a4ac27e61d88408a0b4893e03c9be0f9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306075
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Darren Shen <shend@chromium.org>
    > Auto-Submit: Jing Wang <jiwan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#947816}
    
    Bug: b:192507005
    Change-Id: I1f48f98a18506657afbf5d1251be492197727f64
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314570
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Owners-Override: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/main@{#947950}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3000,6 +3000,12 @@ extern const char kVirtualKeyboardBorderedKeyDescription[];
 extern const char kVirtualKeyboardDisabledName[];
 extern const char kVirtualKeyboardDisabledDescription[];
 
+extern const char kVirtualKeyboardMultipasteName[];
+extern const char kVirtualKeyboardMultipasteDescription[];
+
+extern const char kVirtualKeyboardMultipasteSuggestionName[];
+extern const char kVirtualKeyboardMultipasteSuggestionDescription[];
+
 extern const char kWakeOnWifiAllowedName[];
 extern const char kWakeOnWifiAllowedDescription[];
 
