570ed26bd63ef92c79f56ac48a32f6389f0c6496
danakj <danakj@chromium.org>
2021-12-03 21:52:49 +0000

    Reland: Make PaymentRequest a DocumentService
    The PaymentRequest's lifetime is bounded by
    a) Its mojo receiver's connection
    b) The current document in the RenderFrameHost that spawned it
    c) It self-destroys on an error, which acts as a shortcut for a).
    
    We would like to:
    - avoid holding GlobalFrameRoutingIds, and doing null-checks,
    - avoid the fact that the mojo connection can slightly outlive
      the RenderFrameHost, and
    - avoid tracking the fragile-and-ever-changing WebContentsObserver
      API to manage the object's lifetime.
    
    DocumentService will ensure that the PaymentRequest (as its
    subclass) is destroyed appropriately, and never has a null, or
    invalid freed pointer back to the RenderFrameHost.
    
    This change allows us to remove most of the abstraction of the
    PaymentRequestWebContentsManager. The PaymentRequest's lifetime
    is now fully self-managed, and the object can just be created
    directly. But the PaymentRequestWebContentsManager has to
    stick around just as a place to hold state that the payments
    code wants to attach to the WebContents, that is the
    SPCTransactionMode.
    
    R=creis@chromium.org, rouslan@chromium.org
    
    Bug: 1251691, 1252046
    Change-Id: Ie90c881fbc80abf1f22fcf7c0a2a51c56d143a8e
    Cq-Include-Trybots: luci.chromium.try:linux-bfcache-rel,android-bfcache-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311758
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: danakj chromium <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948181}

--- chrome/browser/payments/payment_request_factory.cc
+++ chrome/browser/payments/payment_request_factory.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "chrome/browser/payments/chrome_payment_request_delegate.h"
+#include "components/payments/content/payment_request.h"
 #include "components/payments/content/payment_request_web_contents_manager.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
@@ -54,13 +55,22 @@ void CreatePaymentRequest(
                                            render_frame_host);
   }
 
-  PaymentRequestWebContentsManager::GetOrCreateForWebContents(
-      content::WebContents::FromRenderFrameHost(render_frame_host))
-      ->CreatePaymentRequest(
-          render_frame_host,
-          std::make_unique<ChromePaymentRequestDelegate>(render_frame_host),
-          std::move(receiver),
-          /*observer_for_testing=*/nullptr);
+  auto* web_contents =
+      content::WebContents::FromRenderFrameHost(render_frame_host);
+  CHECK(web_contents);
+  auto* web_contents_manager =
+      PaymentRequestWebContentsManager::GetOrCreateForWebContents(
+          *web_contents);
+
+  auto delegate =
+      std::make_unique<ChromePaymentRequestDelegate>(render_frame_host);
+  auto display_manager = delegate->GetDisplayManager()->GetWeakPtr();
+  // PaymentRequest is a DocumentService, whose lifetime is managed by the
+  // RenderFrameHost passed in here.
+  new PaymentRequest(render_frame_host, std::move(delegate),
+                     std::move(display_manager), std::move(receiver),
+                     web_contents_manager->transaction_mode(),
+                     /*observer_for_testing=*/nullptr);
 }
 
 void SetPaymentRequestFactoryForTesting(
--- chrome/browser/ui/views/payments/payment_request_browsertest_base.cc
+++ chrome/browser/ui/views/payments/payment_request_browsertest_base.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/ui/address_combobox_model.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/payments/content/payment_request.h"
-#include "components/payments/content/payment_request_web_contents_manager.h"
 #include "components/payments/core/payment_prefs.h"
 #include "components/prefs/pref_service.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
@@ -465,17 +464,14 @@ content::WebContents* PaymentRequestBrowserTestBase::GetActiveWebContents() {
 }
 
 const std::vector<PaymentRequest*>
-PaymentRequestBrowserTestBase::GetPaymentRequests(
-    content::WebContents* web_contents) {
-  PaymentRequestWebContentsManager* manager =
-      PaymentRequestWebContentsManager::GetOrCreateForWebContents(web_contents);
-  if (!manager)
-    return std::vector<PaymentRequest*>();
-
-  std::vector<PaymentRequest*> payment_requests_ptrs;
-  for (const auto& p : manager->payment_requests_)
-    payment_requests_ptrs.push_back(p.first);
-  return payment_requests_ptrs;
+PaymentRequestBrowserTestBase::GetPaymentRequests() {
+  std::vector<PaymentRequest*> ptrs;
+  ptrs.reserve(requests_.size());
+  for (const auto& weak : requests_) {
+    if (weak)
+      ptrs.push_back(&*weak);
+  }
+  return ptrs;
 }
 
 autofill::PersonalDataManager* PaymentRequestBrowserTestBase::GetDataManager() {
@@ -549,10 +545,11 @@ void PaymentRequestBrowserTestBase::CreatePaymentRequestForTest(
           render_frame_host, /*observer=*/GetWeakPtr(), &prefs_, is_incognito_,
           is_valid_ssl_, is_browser_window_active_, skip_ui_for_basic_card_);
   delegate_ = delegate.get();
-  PaymentRequestWebContentsManager::GetOrCreateForWebContents(
-      content::WebContents::FromRenderFrameHost(render_frame_host))
-      ->CreatePaymentRequest(render_frame_host, std::move(delegate),
-                             std::move(receiver), GetWeakPtr());
+  auto display_manager = delegate->GetDisplayManager()->GetWeakPtr();
+  auto* request = new PaymentRequest(
+      render_frame_host, std::move(delegate), std::move(display_manager),
+      std::move(receiver), SPCTransactionMode::NONE, GetWeakPtr());
+  requests_.push_back(request->GetWeakPtr());
 }
 
 void PaymentRequestBrowserTestBase::ClickOnDialogViewAndWait(
