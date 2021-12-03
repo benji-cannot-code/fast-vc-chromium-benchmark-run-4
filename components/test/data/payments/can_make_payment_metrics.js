700f4a9f96dba2d5bc633475a8cf32219e11b76e
Liquan (Max) Gu <maxlg@chromium.org>
2021-12-03 20:25:52 +0000

    Migrate 7 tests: payment_request_can_make_payment_metrics_browsertest
    This CL migrates 7 tests that depends on basic-card. The migrated
    tests are added as new tests so that the coverage for the old paths
    will be preserved.
    
    This CL also fixes the Called_False_NotShown (WithBasicCardDisabled)
    test which still used the basic-card method.
    
    Bug: 1221559
    Change-Id: I10d87ed3b7bf8f7fb9f6c45655cd86ef5b510208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313889
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948129}

--- components/test/data/payments/can_make_payment_metrics.js
+++ components/test/data/payments/can_make_payment_metrics.js
@@ -56,6 +56,8 @@ function noQueryShow() { // eslint-disable-line no-unused-vars, max-len
  * Do not query CanMakePayment before showing the Payment Request. This request
  * will be sent with the given methods.
  * @param {Array<Object>} methods An array of payment method objects.
+ * @return {string} 'success' if show() has been successfully called; otherwise,
+ *         return the error message.
  */
 function noQueryShowWithMethods(methods) { // eslint-disable-line no-unused-vars
   try {
@@ -73,8 +75,10 @@ function noQueryShowWithMethods(methods) { // eslint-disable-line no-unused-vars
         .catch(function(error) {
           print(error);
         });
+    return 'success';
   } catch (error) {
     print(error.message);
+    return error.message;
   }
 }
 
