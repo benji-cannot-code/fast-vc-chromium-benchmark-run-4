c6fdf04c319e386d29bd32a6ddb9ad437e9d0b89
David Bertoni <dbertoni@chromium.org>
2021-12-03 02:00:45 +0000

    [Extensions] Move some content settings helper functions.
    Bug: 883570
    Change-Id: I6af9779559f68c5989e5d3c5815b034d4a637d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312418
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947822}

--- chrome/browser/extensions/api/content_settings/content_settings_store.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_store.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
 #include "base/values.h"
-#include "chrome/browser/extensions/api/content_settings/content_settings_helpers.h"
 #include "components/content_settings/core/browser/content_settings_info.h"
 #include "components/content_settings/core/browser/content_settings_origin_identifier_value_map.h"
 #include "components/content_settings/core/browser/content_settings_registry.h"
@@ -28,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/permissions/features.h"
 #include "content/public/browser/browser_thread.h"
+#include "extensions/browser/api/content_settings/content_settings_helpers.h"
 
 using content::BrowserThread;
 using content_settings::ConcatenationIterator;
--- extensions/browser/api/content_settings/content_settings_helpers.h
+++ extensions/browser/api/content_settings/content_settings_helpers.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_API_CONTENT_SETTINGS_CONTENT_SETTINGS_HELPERS_H__
-#define CHROME_BROWSER_EXTENSIONS_API_CONTENT_SETTINGS_CONTENT_SETTINGS_HELPERS_H__
+#ifndef EXTENSIONS_BROWSER_API_CONTENT_SETTINGS_CONTENT_SETTINGS_HELPERS_H_
+#define EXTENSIONS_BROWSER_API_CONTENT_SETTINGS_CONTENT_SETTINGS_HELPERS_H_
 
 #include <string>
 
@@ -34,4 +34,4 @@ std::string ContentSettingsTypeToString(ContentSettingsType type);
 }  // namespace content_settings_helpers
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_API_CONTENT_SETTINGS_CONTENT_SETTINGS_HELPERS_H__
+#endif  // EXTENSIONS_BROWSER_API_CONTENT_SETTINGS_CONTENT_SETTINGS_HELPERS_H_
