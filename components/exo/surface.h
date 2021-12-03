12971b4f3933e18bfc22657172895d5221a08bf6
Maksim Sisov <msisov@igalia.com>
2021-12-03 08:30:18 +0000

    exo: set rounded corners bounds.
    In order to make delegated compositing work with rounded
    corners as they are supposed to work, the entire rounded
    corners rect for each quad must be set. Otherwise, they
    are applied to each quad instead of being applied to
    quads as if they are composited.
    
    See DirectRenderer::ShouldApplyRoundedCorner for more
    details.
    
    Moreover, the rounded corners are sent in dip and must
    be translated from dip to px.
    
    Bug: 1263496
    Change-Id: I9469cc179f08e26a4c51cf48cb4fcea65162cbf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295602
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#947919}

--- components/exo/surface.h
+++ components/exo/surface.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
+#include "ui/gfx/geometry/rrect_f.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/size_f.h"
 #include "ui/gfx/geometry/transform.h"
@@ -176,7 +177,7 @@ class Surface final : public ui::PropertyHandler {
   void PlaceSubSurfaceBelow(Surface* sub_surface, Surface* sibling);
   void OnSubSurfaceCommit();
 
-  void SetRoundedCorners(const gfx::RoundedCornersF& radii);
+  void SetRoundedCorners(const gfx::RRectF& rounded_corners_bounds);
   void SetOverlayPriorityHint(OverlayPriority hint);
 
   // This sets the surface viewport for scaling.
@@ -454,8 +455,8 @@ class Surface final : public ui::PropertyHandler {
 
     // The buffer that will become the content of surface.
     BufferAttachment buffer;
-    // The rounded corner for the surface.
-    gfx::RoundedCornersF radii;
+    // The rounded corners bounds for the surface.
+    gfx::RRectF rounded_corners_bounds;
     // The damage region to schedule paint for.
     cc::Region damage;
     // These lists contain the callbacks to notify the client when it is a good
