25a11a80ef88328c455ee09d1c8ba60df3dff258
Yao Li <yaohuali@google.com>
2021-12-03 03:23:25 +0000

    arc: [36/N] //{ => ash}/components/arc/ post-move deps clean-up
    This CL make users of //components/arc:arc point to
    //ash/components/arc:arc.
    
    It also cleans up unused deps of //components/arc:arc and
    //ash/components/arc:arc, after the move.
    
    It also does some clean-up on some meta-files, like DEPS.
    
    This is part of the LaCrOs directory migration
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE
    
    Bug: b:129295708
    Bug: 1164001
    Test: CI
    Change-Id: Idc9dfdcbdbc3d490c76a147583e94539efda6a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298437
    Commit-Queue: Yao Li <yaohuali@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947860}

--- ash/components/arc/test/fake_app_host.h
+++ ash/components/arc/test/fake_app_host.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 
-// For tests in //components/arc that cannot use the real AppHost implementation
-// in //chrome.
+// For tests in //ash/components/arc that cannot use the real AppHost
+// implementation in //chrome.
 class FakeAppHost : public mojom::AppHost {
  public:
   explicit FakeAppHost(
