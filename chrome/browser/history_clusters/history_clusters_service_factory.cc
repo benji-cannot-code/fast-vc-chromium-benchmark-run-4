c1c43ed32f2e1d4dddb36fa3c82cb9e2e4ff5be6
Tommy Li <tommycli@chromium.org>
2021-12-03 17:10:26 +0000

    [memories] Pass g_browser_process locale to HistoryClustersService
    Component has no concept of the locale, so we pass it via a string to
    the service from g_browser_process via the Factory.
    
    This is the same approach used by DriveFactory, WebDataServiceFactory,
    and others.
    
    Bug: 1275325
    Change-Id: Iadfc1a637ef875d2e1abb7a827d0c09757f7be7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313649
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948036}

--- chrome/browser/history_clusters/history_clusters_service_factory.cc
+++ chrome/browser/history_clusters/history_clusters_service_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/history_clusters/history_clusters_service_factory.h"
 
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/engagement/site_engagement_service_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/optimization_guide/page_content_annotations_service_factory.h"
@@ -57,7 +58,8 @@ KeyedService* HistoryClustersServiceFactory::BuildServiceInstanceFor(
   auto url_loader_factory = context->GetDefaultStoragePartition()
                                 ->GetURLLoaderFactoryForBrowserProcess();
   return new history_clusters::HistoryClustersService(
-      history_service, TemplateURLServiceFactory::GetForProfile(profile),
+      g_browser_process->GetApplicationLocale(), history_service,
+      TemplateURLServiceFactory::GetForProfile(profile),
       PageContentAnnotationsServiceFactory::GetForProfile(profile),
       url_loader_factory, site_engagement::SiteEngagementService::Get(profile));
 }
