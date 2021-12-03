47a8d2804e8e73d1349c3625c85f86c5fedc12fc
Luca Hunkeler <hluca@google.com>
2021-12-03 13:47:26 +0000

    [Autofill Assistant] Pass settings and web controller to ElementArea
    ElementArea now has a pointer to the settings and the web controller
    instead of getting them from ScriptExecutorDelegate.
    Same for BasicInteractions and the settings.
    These two classes will not have access to ScriptExecutorDelegate
    after the Controller refactoring.
    
    Bug: b/201261611
    Change-Id: Id5d39338b35725ccc936024a0ad13c6d093bca38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310104
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/main@{#947978}

--- components/autofill_assistant/browser/element_area.h
+++ components/autofill_assistant/browser/element_area.h
@@ -15,17 +15,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill_assistant/browser/client_settings.h"
 #include "components/autofill_assistant/browser/rectf.h"
 #include "components/autofill_assistant/browser/selector.h"
+#include "components/autofill_assistant/browser/web/web_controller.h"
 
 namespace autofill_assistant {
-class ScriptExecutorDelegate;
 
 // A helper that keeps track of the area on the screen that correspond to an
 // changeable set of elements.
 class ElementArea {
  public:
-  // |delegate| and |settings| must remain valid for the lifetime of this
+  // |settings| and |web_controller| must remain valid for the lifetime of this
   // instance.
-  explicit ElementArea(ScriptExecutorDelegate* delegate);
+  explicit ElementArea(ClientSettings* settings, WebController* web_controller);
 
   ElementArea(const ElementArea&) = delete;
   ElementArea& operator=(const ElementArea&) = delete;
@@ -131,7 +131,8 @@ class ElementArea {
   void OnGetVisualViewport(const ClientStatus& status, const RectF& rect);
   void ReportUpdate();
 
-  const raw_ptr<ScriptExecutorDelegate> delegate_;
+  const raw_ptr<ClientSettings> settings_;
+  const raw_ptr<WebController> web_controller_;
   std::vector<Rectangle> rectangles_;
 
   // If true, u pdate for the visual viewport position is currently scheduled.
