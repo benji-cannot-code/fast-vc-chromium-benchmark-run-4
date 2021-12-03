7b25cd9aa313ae36a16ad39e55b032875cc28e8f
Jing Wang <jiwan@chromium.org>
2021-12-03 01:55:43 +0000

    Delete multipaste related flags.
    Bug: b:192507005
    Change-Id: I13a362f3a4ac27e61d88408a0b4893e03c9be0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306075
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Auto-Submit: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947816}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3000,12 +3000,6 @@ extern const char kVirtualKeyboardBorderedKeyDescription[];
 extern const char kVirtualKeyboardDisabledName[];
 extern const char kVirtualKeyboardDisabledDescription[];
 
-extern const char kVirtualKeyboardMultipasteName[];
-extern const char kVirtualKeyboardMultipasteDescription[];
-
-extern const char kVirtualKeyboardMultipasteSuggestionName[];
-extern const char kVirtualKeyboardMultipasteSuggestionDescription[];
-
 extern const char kWakeOnWifiAllowedName[];
 extern const char kWakeOnWifiAllowedDescription[];
 
