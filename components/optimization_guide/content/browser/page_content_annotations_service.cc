4c9e26994318a44d753a83b73adccec68a19f16e
Robert Ogden <robertogden@chromium.org>
2021-12-03 22:12:23 +0000

    Connect PCA front and back ends
    Keeps the PageContentAnnotator in the service, but as a pointer that
    normally just points to model_manager_, but can be changed for testing.
    
    Bug: 1249632
    Change-Id: I7e29bf0aa3df88a0ee5ee37240b2c596fdad8a48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313678
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948187}

--- components/optimization_guide/content/browser/page_content_annotations_service.cc
+++ components/optimization_guide/content/browser/page_content_annotations_service.cc
@@ -85,6 +85,7 @@ PageContentAnnotationsService::PageContentAnnotationsService(
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
   model_manager_ = std::make_unique<PageContentAnnotationsModelManager>(
       optimization_guide_model_provider);
+  annotator_ = model_manager_.get();
 #endif
 }
 
@@ -109,8 +110,8 @@ void PageContentAnnotationsService::Annotate(const HistoryVisit& visit,
 }
 
 void PageContentAnnotationsService::OverridePageContentAnnotatorForTesting(
-    std::unique_ptr<PageContentAnnotator> annotator) {
-  annotator_ = std::move(annotator);
+    PageContentAnnotator* annotator) {
+  annotator_ = annotator;
 }
 
 void PageContentAnnotationsService::BatchAnnotate(
