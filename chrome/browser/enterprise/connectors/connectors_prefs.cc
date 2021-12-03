cf118afa63f25cd549764753908847dce68c6564
Dominique Fauteux-Chapleau <domfc@chromium.org>
2021-12-03 18:08:27 +0000

    Add OnPrintEnterpriseConnector policy
    This policy shares a similar scheme and description with other
    EnterpriseConnector polices, except it applies to printing and as such
    doesn't require certain fields like "minimum_data_size" or
    "block_password_protected".
    
    Bug: 1273922
    Change-Id: Ie108d2ff6b0edd1abc4dae24ee1a6178426b00a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3302869
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948063}

--- chrome/browser/enterprise/connectors/connectors_prefs.cc
+++ chrome/browser/enterprise/connectors/connectors_prefs.cc
@@ -25,6 +25,8 @@ const char kOnFileDownloadedPref[] = "enterprise_connectors.on_file_downloaded";
 
 const char kOnBulkDataEntryPref[] = "enterprise_connectors.on_bulk_data_entry";
 
+const char kOnPrintPref[] = "enterprise_connectors.on_print";
+
 const char kOnSecurityEventPref[] = "enterprise_connectors.on_security_event";
 
 const char kContextAwareAccessSignalsAllowlistPref[] =
@@ -40,6 +42,7 @@ const char kOnFileDownloadedScopePref[] =
     "enterprise_connectors.scope.on_file_downloaded";
 const char kOnBulkDataEntryScopePref[] =
     "enterprise_connectors.scope.on_bulk_data_entry";
+const char kOnPrintScopePref[] = "enterprise_connectors.scope.on_print";
 const char kOnSecurityEventScopePref[] =
     "enterprise_connectors.scope.on_security_event";
 
@@ -68,10 +71,12 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterListPref(kOnFileAttachedPref);
   registry->RegisterListPref(kOnFileDownloadedPref);
   registry->RegisterListPref(kOnBulkDataEntryPref);
+  registry->RegisterListPref(kOnPrintPref);
   registry->RegisterListPref(kOnSecurityEventPref);
   registry->RegisterIntegerPref(kOnFileAttachedScopePref, 0);
   registry->RegisterIntegerPref(kOnFileDownloadedScopePref, 0);
   registry->RegisterIntegerPref(kOnBulkDataEntryScopePref, 0);
+  registry->RegisterIntegerPref(kOnPrintScopePref, 0);
   registry->RegisterIntegerPref(kOnSecurityEventScopePref, 0);
   registry->RegisterListPref(kContextAwareAccessSignalsAllowlistPref);
 
