ab77a0a0e71d2d1d732c7b7fb4a1aa23a36513bd
Steve Cho <stevecho@google.com>
2021-12-03 23:06:01 +0000

    media/gpu/v4l2: VIDIOC_S_EXT_CTRLS ioctl call for request API
    This CL implements change to make VIDIOC_S_EXT_CTRLS ioctl needed for
    request API. This ioctl call is used to specify VP9 decoding parameters
    for each frame.
    
    Bug: b:202214561
    TEST: v4l2_stateless_decoder --video crowd_run_256X144_fr15_bd8_8buf_l1_20210312.ivf on asurada
    Change-Id: I469d201fd17590b82eb5860ac786d0c8d7e0cadd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3297120
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948214}

--- media/gpu/v4l2/test/v4l2_ioctl_shim.h
+++ media/gpu/v4l2/test/v4l2_ioctl_shim.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_V4L2_TEST_V4L2_IOCTL_SHIM_H_
 #define MEDIA_GPU_V4L2_TEST_V4L2_IOCTL_SHIM_H_
 
+#include <linux/media/vp9-ctrls.h>
 #include <linux/videodev2.h>
 
 #include "base/files/memory_mapped_file.h"
@@ -153,6 +154,12 @@ class V4L2IoctlShim {
 
   // Starts streaming |queue| (via VIDIOC_STREAMON).
   bool StreamOn(const enum v4l2_buf_type type) const WARN_UNUSED_RESULT;
+
+  // Sets the value of a control which specifies VP9 decoding parameters
+  // for each frame.
+  bool SetExtCtrls(const std::unique_ptr<V4L2Queue>& queue,
+                   v4l2_ctrl_vp9_frame_decode_params& frame_params) const
+      WARN_UNUSED_RESULT;
 
   // Allocates requests (likely one per OUTPUT buffer) via
   // MEDIA_IOC_REQUEST_ALLOC on the media device.
