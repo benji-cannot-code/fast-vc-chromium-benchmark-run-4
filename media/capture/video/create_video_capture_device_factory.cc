35f9e04b56198b322e7d2045a9306e167bb71cec
Ahmed Fakhry <afakhry@chromium.org>
2021-12-03 18:02:01 +0000

    media: Make VideoSourceProvider usable on chromeos-linux
    Using VideoSourceProvider on chromeos-linux builds
    (generally knows as the emulator) isn't possible due to
    the fact that it relies on VideoCaptureDeviceFactoryChromeOS
    which uses the camera hal that is built only for chrome os
    images installed on actual devices.
    
    This CL creates VideoCaptureDeviceFactoryLinux instead
    when we're not running on a device, so that using the
    emulator is possible to test camera-related features.
    
    This will be a huge productivity boost for many of our
    engineers.
    
    `VideoSourceProvider::GetSourceInfos()` is able to get the
    camera devices correctly on the emulator.
    
    Bug: 1276474
    Test: Tested manually that calling
    Change-Id: I47e05be94e5f9cff6d09d99c63d9f08d0a7dc70c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311677
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948058}

--- media/capture/video/create_video_capture_device_factory.cc
+++ media/capture/video/create_video_capture_device_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/create_video_capture_device_factory.h"
 
 #include "base/command_line.h"
+#include "base/system/sys_info.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "media/base/media_switches.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif BUILDFLAG(IS_CHROMEOS_ASH)
 #include "media/capture/video/chromeos/public/cros_features.h"
 #include "media/capture/video/chromeos/video_capture_device_factory_chromeos.h"
+#include "media/capture/video/linux/video_capture_device_factory_linux.h"
 #elif defined(OS_WIN)
 #include "media/capture/video/win/video_capture_device_factory_win.h"
 #elif defined(OS_MAC)
@@ -57,7 +59,9 @@ CreatePlatformSpecificVideoCaptureDeviceFactory(
 #if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
   return std::make_unique<VideoCaptureDeviceFactoryLinux>(ui_task_runner);
 #elif BUILDFLAG(IS_CHROMEOS_ASH)
-  return std::make_unique<VideoCaptureDeviceFactoryChromeOS>(ui_task_runner);
+  if (base::SysInfo::IsRunningOnChromeOS())
+    return std::make_unique<VideoCaptureDeviceFactoryChromeOS>(ui_task_runner);
+  return std::make_unique<VideoCaptureDeviceFactoryLinux>(ui_task_runner);
 #elif defined(OS_WIN)
   return std::make_unique<VideoCaptureDeviceFactoryWin>();
 #elif defined(OS_MAC)
