e064462beabab49b4c56ce75f739a3fd2d171ae5
Xing Liu <xingliu@chromium.org>
2021-12-03 22:21:33 +0000

    Disable testSigninFirstRunPages_WithCctPolicy*.
    These are failing on android 11 bots.
    
    Bug: 1276506
    Change-Id: I40ef0b148269af7de3040c1e2794253130333ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315340
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948193}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -16,6 +16,7 @@ import android.app.Instrumentation.ActivityMonitor;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.test.InstrumentationRegistry;
 import android.view.View;
@@ -50,6 +51,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.ScalableTimeout;
@@ -594,6 +596,8 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
+    @DisableIf.
+    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_AbsenceOfPromos() throws Exception {
@@ -602,6 +606,8 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
+    @DisableIf.
+    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_SearchPromo() throws Exception {
@@ -610,6 +616,8 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
+    @DisableIf.
+    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_SearchPromo_SigninPromo() throws Exception {
@@ -621,6 +629,8 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
+    @DisableIf.
+    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_SigninPromo() throws Exception {
