c6dacee844f978f086b20b96b4c0a0ba156b4eb9
Rachel Wong <wrong@chromium.org>
2021-12-03 02:57:19 +0000

    [answers] Add a skeleton Answer ranker.
    Ranking has temporarily been removed[1], so this CL does not include
    the corresponding AddRanker call.
    
    [1]
    https://chromium-review.googlesource.com/c/chromium/src/+/3307143
    
    Bug: 1275408
    Change-Id: Ib8009851bf9dca975362db993237c4c3f03fe0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313360
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#947851}

---
+++ chrome/browser/ui/app_list/search/ranking/answer_ranker.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/app_list/search/ranking/answer_ranker.h"
+
+namespace app_list {
+
+AnswerRanker::AnswerRanker() = default;
+AnswerRanker::~AnswerRanker() = default;
+
+void AnswerRanker::Start(const std::u16string& query,
+                         ResultsMap& results,
+                         CategoriesList& categories) {
+  // TODO(crbug.com/1275408): WIP.
+}
+
+void AnswerRanker::UpdateResultRanks(ResultsMap& results,
+                                     ProviderType provider) {
+  // TODO(crbug.com/1275408): WIP.
+}
+
+}  // namespace app_list
