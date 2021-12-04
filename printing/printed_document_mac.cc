414f59042409daa1aa7f2e7e745e89b62b161ba6
Alan Screen <awscreen@chromium.org>
2021-12-04 00:31:36 +0000

    Return error result from PrintedDocument render functions
    Modify the render functions of PrintedDocument to return the error
    status from rendering.  This is of larger interest since rendering is
    being moved into a sandboxed process, where the possibility of new
    errors is increased.
    
    Bug: 809738, 1252685
    Change-Id: I24b42c37cf3ccae65c63227fd9d841326f3d01a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3183829
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948250}

--- printing/printed_document_mac.cc
+++ printing/printed_document_mac.cc
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "printing/metafile.h"
+#include "printing/mojom/print.mojom.h"
 #include "printing/printing_context.h"
 
 namespace printing {
 
-bool PrintedDocument::RenderPrintedDocument(PrintingContext* context) {
+mojom::ResultCode PrintedDocument::RenderPrintedDocument(
+    PrintingContext* context) {
   DCHECK(context);
 
   const MetafilePlayer* metafile;
@@ -30,14 +32,19 @@ bool PrintedDocument::RenderPrintedDocument(PrintingContext* context) {
   size_t num_pages = expected_page_count();
   for (size_t metafile_page_number = 1; metafile_page_number <= num_pages;
        metafile_page_number++) {
-    if (context->NewPage() != mojom::ResultCode::kSuccess)
-      return false;
-    metafile->RenderPage(metafile_page_number, context->context(), paper_rect,
-                         /*autorotate=*/true, /*fit_to_page=*/false);
-    if (context->PageDone() != mojom::ResultCode::kSuccess)
-      return false;
+    mojom::ResultCode result = context->NewPage();
+    if (result != mojom::ResultCode::kSuccess)
+      return result;
+    if (!metafile->RenderPage(metafile_page_number, context->context(),
+                              paper_rect,
+                              /*autorotate=*/true, /*fit_to_page=*/false)) {
+      return mojom::ResultCode::kFailed;
+    }
+    result = context->PageDone();
+    if (result != mojom::ResultCode::kSuccess)
+      return result;
   }
-  return true;
+  return mojom::ResultCode::kSuccess;
 }
 
 }  // namespace printing
