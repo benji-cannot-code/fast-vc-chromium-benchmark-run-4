a1708434d1b4223ad46e26afbf23c34161fdcbfd
Kenichi Ishibashi <bashi@chromium.org>
2021-12-04 00:17:24 +0000

    Add a histogram to measure Android RadioUtils overhead
    A field experiment indicates that using RadioUtils in a hot path
    (e.g. resource loading) could regress Core Web Vitals. This CL
    adds a histogram to measure overheads of RadioUtils to narrow
    down the cause.
    
    Bug: 1232623
    Change-Id: I10ee3ed8a111cd7a647c0936bfe2e1cf24ee6b3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312541
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948246}

--- services/network/url_loader_unittest.cc
+++ services/network/url_loader_unittest.cc
@@ -7345,7 +7345,7 @@ TEST_F(URLLoaderTest, RecordRadioWakeupTrigger_IntervalTooShort) {
       base::android::RadioDataActivity::kDormant);
   RadioMonitorAndroid::GetInstance().OverrideRadioTypeForTesting(
       base::android::RadioConnectionType::kCell);
-  RadioMonitorAndroid::GetInstance().OverrideLastRecordTimeForTesting(
+  RadioMonitorAndroid::GetInstance().OverrideLastCheckTimeForTesting(
       base::TimeTicks::Now());
 
   LoadAndCompareFile("simple_page.html");
