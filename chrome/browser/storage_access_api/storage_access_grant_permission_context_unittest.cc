90aee8d9a737a4fdfc39e1238bd823aa1bbfaf4f
Mark Pearson <mpearson@chromium.org>
2021-12-05 21:46:40 +0000

    Refactor StorageAccessGrantPermissionContextTest to avoid race condition
    ScopedFeatureList must be initialized before creating other threads.
    
    Bug: 1275502
    Change-Id: I3fb6da3349de2042b4156344d1ae65431a6d8620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313554
    Auto-Submit: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948404}

--- chrome/browser/storage_access_api/storage_access_grant_permission_context_unittest.cc
+++ chrome/browser/storage_access_api/storage_access_grant_permission_context_unittest.cc
@@ -108,13 +108,10 @@ TEST_F(StorageAccessGrantPermissionContextTest, InsecureOriginsAreAllowed) {
       insecure_url, GetRequesterURL()));
 }
 
-// When the Storage Access API feature is disabled we should block the
-// permission request.
+// When the Storage Access API feature is disabled (the default) we
+// should block the permission request.
 TEST_F(StorageAccessGrantPermissionContextTest,
        PermissionBlockedWhenFeatureDisabled) {
-  base::test::ScopedFeatureList scoped_disable;
-  scoped_disable.InitAndDisableFeature(blink::features::kStorageAccessAPI);
-
   StorageAccessGrantPermissionContext permission_context(profile());
   permissions::PermissionRequestID fake_id = CreateFakeID();
 
@@ -125,13 +122,17 @@ TEST_F(StorageAccessGrantPermissionContextTest,
   EXPECT_EQ(CONTENT_SETTING_BLOCK, result);
 }
 
+class StorageAccessGrantPermissionContextAPIEnabledTest
+    : public StorageAccessGrantPermissionContextTest {
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_{
+      blink::features::kStorageAccessAPI};
+};
+
 // When the Storage Access API feature is enabled and we have a user gesture we
 // should get a decision.
-TEST_F(StorageAccessGrantPermissionContextTest,
+TEST_F(StorageAccessGrantPermissionContextAPIEnabledTest,
        PermissionDecidedWhenFeatureEnabled) {
-  base::test::ScopedFeatureList scoped_enable;
-  scoped_enable.InitAndEnableFeature(blink::features::kStorageAccessAPI);
-
   StorageAccessGrantPermissionContext permission_context(profile());
   permissions::PermissionRequestID fake_id = CreateFakeID();
 
@@ -161,11 +162,8 @@ TEST_F(StorageAccessGrantPermissionContextTest,
 }
 
 // No user gesture should force a permission rejection.
-TEST_F(StorageAccessGrantPermissionContextTest,
+TEST_F(StorageAccessGrantPermissionContextAPIEnabledTest,
        PermissionDeniedWithoutUserGesture) {
-  base::test::ScopedFeatureList scoped_enable;
-  scoped_enable.InitAndEnableFeature(blink::features::kStorageAccessAPI);
-
   StorageAccessGrantPermissionContext permission_context(profile());
   permissions::PermissionRequestID fake_id = CreateFakeID();
 
@@ -178,9 +176,6 @@ TEST_F(StorageAccessGrantPermissionContextTest,
 
 TEST_F(StorageAccessGrantPermissionContextTest,
        PermissionStatusBlockedWhenFeatureDisabled) {
-  base::test::ScopedFeatureList scoped_disable;
-  scoped_disable.InitAndDisableFeature(blink::features::kStorageAccessAPI);
-
   StorageAccessGrantPermissionContext permission_context(profile());
 
   EXPECT_EQ(CONTENT_SETTING_BLOCK,
@@ -190,11 +185,8 @@ TEST_F(StorageAccessGrantPermissionContextTest,
                 .content_setting);
 }
 
-TEST_F(StorageAccessGrantPermissionContextTest,
+TEST_F(StorageAccessGrantPermissionContextAPIEnabledTest,
        PermissionStatusAsksWhenFeatureEnabled) {
-  base::test::ScopedFeatureList scoped_enable;
-  scoped_enable.InitAndEnableFeature(blink::features::kStorageAccessAPI);
-
   StorageAccessGrantPermissionContext permission_context(profile());
 
   EXPECT_EQ(CONTENT_SETTING_ASK,
@@ -206,11 +198,8 @@ TEST_F(StorageAccessGrantPermissionContextTest,
 
 // Validate that each requesting origin has its own implicit grant limit. If
 // the limit for one origin is exhausted it should not affect another.
-TEST_F(StorageAccessGrantPermissionContextTest,
+TEST_F(StorageAccessGrantPermissionContextAPIEnabledTest,
        ImplicitGrantLimitPerRequestingOrigin) {
-  base::test::ScopedFeatureList scoped_enable;
-  scoped_enable.InitAndEnableFeature(blink::features::kStorageAccessAPI);
-
   base::HistogramTester histogram_tester;
   histogram_tester.ExpectTotalCount(kGrantIsImplicitHistogram, 0);
 
@@ -267,10 +256,7 @@ TEST_F(StorageAccessGrantPermissionContextTest,
                                      /*DISMISSED=*/2, 1);
 }
 
-TEST_F(StorageAccessGrantPermissionContextTest, ExplicitGrantDenial) {
-  base::test::ScopedFeatureList scoped_enable;
-  scoped_enable.InitAndEnableFeature(blink::features::kStorageAccessAPI);
-
+TEST_F(StorageAccessGrantPermissionContextAPIEnabledTest, ExplicitGrantDenial) {
   base::HistogramTester histogram_tester;
   histogram_tester.ExpectTotalCount(kGrantIsImplicitHistogram, 0);
   histogram_tester.ExpectTotalCount(kPromptResultHistogram, 0);
