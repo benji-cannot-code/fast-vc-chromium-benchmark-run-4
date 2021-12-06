f55e5824025aef898d5c6c4abf21ecd66acb68de
Joshua Pawlicki <waffles@chromium.org>
2021-12-06 19:11:22 +0000

    Updater: Add scope to mac installer API.
    Hopefully only the updater itself needs it.
    
    Bug: 1268555
    Change-Id: I85efca74b0f62769c2a719b63fcd17dcd29bd6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3317916
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948611}

--- chrome/updater/mac/install_from_archive.mm
+++ chrome/updater/mac/install_from_archive.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/updater/mac/mac_util.h"
+#include "chrome/updater/updater_scope.h"
 #include "chrome/updater/util.h"
 
 namespace updater {
@@ -120,6 +121,7 @@ bool IsInstallScriptExecutable(const base::FilePath& script_path) {
 int RunExecutable(const base::FilePath& existence_checker_path,
                   const std::string& ap,
                   const std::string& arguments,
+                  const UpdaterScope& scope,
                   const base::FilePath& unpacked_path) {
   if (!base::PathExists(unpacked_path)) {
     VLOG(1) << "File path (" << unpacked_path << ") does not exist.";
@@ -170,6 +172,7 @@ int RunExecutable(const base::FilePath& existence_checker_path,
         {"PATH", env_path},
         {"KS_TICKET_XC_PATH", existence_checker_path.value()},
         {"KS_TICKET_AP", ap},
+        {"UPDATE_IS_MACHINE", scope == UpdaterScope::kSystem ? "1" : "0"},
     };
     int exit_code = 0;
     if (!base::LaunchProcess(command, options).WaitForExit(&exit_code))
@@ -306,6 +309,7 @@ int InstallFromApp(const base::FilePath& app_file_path,
 int InstallFromArchive(const base::FilePath& file_path,
                        const base::FilePath& existence_checker_path,
                        const std::string& ap,
+                       const UpdaterScope& scope,
                        const std::string& arguments) {
   const std::map<std::string,
                  int (*)(const base::FilePath&,
@@ -320,7 +324,7 @@ int InstallFromArchive(const base::FilePath& file_path,
     if (base::PathExists(new_path)) {
       return entry.second(
           new_path, base::BindOnce(&RunExecutable, existence_checker_path, ap,
-                                   arguments));
+                                   arguments, scope));
     }
   }
 
