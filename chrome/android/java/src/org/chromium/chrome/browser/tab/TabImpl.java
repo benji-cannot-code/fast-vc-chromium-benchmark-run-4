f2cc8f0f2ec7e0a71bce19e15fcbc216d0439feb
Alex Ilin <alexilin@chromium.org>
2021-12-06 11:34:51 +0000

    Revert "[DesktopSite] Persist tab level user agent setting to disk"
    This reverts commit 4cbdc91d4470aa34d99cf6561a575cee19922260.
    
    Reason for revert: causes StrictModePolicyViolation test failures https://crbug.com/1276907
    
    Original change's description:
    > [DesktopSite] Persist tab level user agent setting to disk
    >
    > Add a new user agent field to TabState and CriticalPersistedTabData.
    > This is to preserve the tab level Request Desktop Site setting after
    > Clank restart.
    >
    > Bug: 1251794
    > Change-Id: Ic3fa701fbb9d75700a7c0f7f4aeed623a9e01353
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298954
    > Reviewed-by: David Maunder <davidjm@chromium.org>
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Commit-Queue: Shu Yang <shuyng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#947718}
    
    Bug: 1251794, 1276907
    Change-Id: I8349aa7dbad29963e016afc511eb7bcdc228501b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314913
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alex Ilin <alexilin@google.com>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948480}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.View.OnAttachStateChangeListener;
 import android.view.accessibility.AccessibilityEvent;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -70,6 +71,8 @@ import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.util.ColorUtils;
 import org.chromium.url.GURL;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.nio.ByteBuffer;
 
 /**
@@ -211,6 +214,22 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
     private final TabThemeColorHelper mThemeColorHelper;
     private int mThemeColor;
     private boolean mUsedCriticalPersistedTabData;
+
+    /** Tab level Request Desktop Site setting. */
+    private @TabUserAgent int mTabUserAgent;
+
+    // TODO(https://crbug.com/1251794): Determine if this should be defined somewhere like TabState
+    // for when we persist to disk.
+    /**
+     * Defines the tab level Request Desktop Site settings.
+     */
+    @IntDef({TabUserAgent.DEFAULT, TabUserAgent.MOBILE, TabUserAgent.DESKTOP})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface TabUserAgent {
+        int DEFAULT = 0; /* No tab level setting */
+        int MOBILE = 1; /* Tab level setting is mobile layout */
+        int DESKTOP = 2; /* Tab level setting is desktop layout */
+    }
 
     /**
      * Creates an instance of a {@link TabImpl}.
@@ -965,7 +984,6 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
         CriticalPersistedTabData.from(this).setLaunchTypeAtCreation(state.tabLaunchTypeAtCreation);
         CriticalPersistedTabData.from(this).setRootId(
                 state.rootId == Tab.INVALID_TAB_ID ? mId : state.rootId);
-        CriticalPersistedTabData.from(this).setUserAgent(state.userAgent);
     }
 
     /**
@@ -1635,27 +1653,13 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
     }
 
     private @UserAgentOverrideOption int calculateUserAgentOverrideOption() {
-        WebContents webContents = getWebContents();
-        boolean currentRequestDesktopSite = webContents == null
+        boolean currentRequestDesktopSite = getWebContents() == null
                 ? false
-                : webContents.getNavigationController().getUseDesktopUserAgent();
+                : getWebContents().getNavigationController().getUseDesktopUserAgent();
 
-        @TabUserAgent
-        int tabUserAgent = CriticalPersistedTabData.from(this).getUserAgent();
-        // TabUserAgent.UNSET means this is a pre-existing tab from an earlier build. In this case
-        // we set the TabUserAgent bit based on last committed entry's user agent. If webContents is
-        // null, this method is triggered too early, and we cannot read the last committed entry's
-        // user agent yet. We will skip for now and let the following call set the TabUserAgent bit.
-        if (webContents != null && tabUserAgent == TabUserAgent.UNSET) {
-            if (currentRequestDesktopSite) {
-                tabUserAgent = TabUserAgent.DESKTOP;
-            } else {
-                tabUserAgent = TabUserAgent.DEFAULT;
-            }
-            CriticalPersistedTabData.from(this).setUserAgent(tabUserAgent);
-        }
         // We only calculate the user agent when users did not manually choose one.
-        if (tabUserAgent == TabUserAgent.DEFAULT
+        // TODO(crbug.com/1251794): Desktop site setting in app menu does not persist after restart.
+        if (mTabUserAgent == TabUserAgent.DEFAULT
                 && ContentFeatureList.isEnabled(ContentFeatureList.REQUEST_DESKTOP_SITE_GLOBAL)) {
             // We only do the following logic to choose the desktop/mobile user agent if:
             // 1. User never manually made a choice in the app menu for requesting desktop site.
@@ -1688,6 +1692,10 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
 
         // INHERIT means use the same that was used last time.
         return UserAgentOverrideOption.INHERIT;
+    }
+
+    void setTabUserAgent(@TabUserAgent int tabUserAgent) {
+        mTabUserAgent = tabUserAgent;
     }
 
     private void switchUserAgentIfNeeded() {
