4a2886487a478859cd73dfc824191bc37340ba4d
Lei Zhang <thestig@chromium.org>
2021-12-06 19:46:38 +0000

    Make PdfPrinterHandlerPosixTest work independent of the system umask.
    As is, the test is written assuming the umask is 0022, and fails if the
    umask is set to some other value. Add a ScopedUmaskSetter to temporarily
    change the umask value to 0022 for the duration of the test, so it can
    consistently pass everywhere.
    
    Bug: 1035572
    Change-Id: I6c5005c4f6434b697c6baeb0118d14fe0ad85ef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313334
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948631}

--- chrome/browser/ui/webui/print_preview/pdf_printer_handler_posix_unittest.cc
+++ chrome/browser/ui/webui/print_preview/pdf_printer_handler_posix_unittest.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/print_preview/pdf_printer_handler.h"
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include <ios>
 #include <memory>
 
@@ -34,6 +37,21 @@ namespace {
 
 // Data content used as fake PDF doesn't matter, just can not be empty.
 constexpr uint8_t kDummyData[] = {'d', 'u', 'm', 'm', 'y'};
+
+// Set a umask and restore the old mask on destruction.  Cribbed from
+// sql/database_unittest.cc.
+class ScopedUmaskSetter {
+ public:
+  explicit ScopedUmaskSetter(mode_t target_mask)
+      : old_umask_(umask(target_mask)) {}
+  ~ScopedUmaskSetter() { umask(old_umask_); }
+
+  ScopedUmaskSetter(const ScopedUmaskSetter&) = delete;
+  ScopedUmaskSetter& operator=(const ScopedUmaskSetter&) = delete;
+
+ private:
+  const mode_t old_umask_;
+};
 
 class FakePdfPrinterHandler : public PdfPrinterHandler {
  public:
@@ -117,6 +135,8 @@ class PdfPrinterHandlerPosixTest : public BrowserWithTestWindowTest {
 };
 
 TEST_F(PdfPrinterHandlerPosixTest, SaveAsPdfFilePermissions) {
+  ScopedUmaskSetter permissive_umask(0022);
+
   // Saved PDF files are not executable files, and should be readable/writeable
   // for the user.  It should also have group readable permissions to match the
   // behavior seen for downloaded files.  Note that this is the desired case
