814618be66e07cca3f207ac333854908b80a868b
Victor Costan <pwnall@chromium.org>
2021-12-06 17:25:30 +0000

    WebSQL: Clean up WebDatabaseHostImpl access control.
    This CL makes WebDatabaseHostImpl's overridden method declarations
    public, whereas they were previously private. The private access control
    specifier is misleading, because WebDatabaseHostImpl uses public
    inheritance (as mandated by the Google C++ style guide), so outside code
    can always call into the overridden methods by creating a superclass
    pointer from a WebDatabaseHostImpl instance.
    
    Making the overridden methods public is sufficient for removing
    FRIEND_TEST_ALL_PREFIXES from FRIEND_TEST_ALL_PREFIXES, which unlocks a
    minor cleanup in testing code.
    
    Change-Id: I63e679d72c390be03590457aa4406311dbbdce55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3316976
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948571}

--- content/browser/web_database/web_database_host_impl.h
+++ content/browser/web_database/web_database_host_impl.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_WEB_DATABASE_WEB_DATABASE_HOST_IMPL_H_
 #define CONTENT_BROWSER_WEB_DATABASE_WEB_DATABASE_HOST_IMPL_H_
 
+#include <stdint.h>
+
 #include <string>
 
 #include "base/callback_forward.h"
-#include "base/gtest_prod_util.h"
+#include "base/memory/scoped_refptr.h"
 #include "build/build_config.h"
 #include "components/services/storage/public/cpp/quota_error_or.h"
 #include "content/common/content_export.h"
@@ -41,47 +43,32 @@ class CONTENT_EXPORT WebDatabaseHostImpl
       scoped_refptr<storage::DatabaseTracker> db_tracker,
       mojo::PendingReceiver<blink::mojom::WebDatabaseHost> receiver);
 
- private:
-  FRIEND_TEST_ALL_PREFIXES(WebDatabaseHostImplTest, OpenFileCreatesBucket);
-  FRIEND_TEST_ALL_PREFIXES(WebDatabaseHostImplTest, GetOrCreateBucketError);
-  FRIEND_TEST_ALL_PREFIXES(WebDatabaseHostImplTest, BadMessagesUnauthorized);
-  FRIEND_TEST_ALL_PREFIXES(WebDatabaseHostImplTest, BadMessagesInvalid);
-  FRIEND_TEST_ALL_PREFIXES(WebDatabaseHostImplTest, ProcessShutdown);
-
   // blink::mojom::WebDatabaseHost:
   void OpenFile(const std::u16string& vfs_file_name,
                 int32_t desired_flags,
                 OpenFileCallback callback) override;
-
   void DeleteFile(const std::u16string& vfs_file_name,
                   bool sync_dir,
                   DeleteFileCallback callback) override;
-
   void GetFileAttributes(const std::u16string& vfs_file_name,
                          GetFileAttributesCallback callback) override;
-
   void SetFileSize(const std::u16string& vfs_file_name,
                    int64_t expected_size,
                    SetFileSizeCallback callback) override;
-
   void GetSpaceAvailable(const url::Origin& origin,
                          GetSpaceAvailableCallback callback) override;
-
   void Opened(const url::Origin& origin,
               const std::u16string& database_name,
               const std::u16string& database_description) override;
-
   void Modified(const url::Origin& origin,
                 const std::u16string& database_name) override;
-
   void Closed(const url::Origin& origin,
               const std::u16string& database_name) override;
-
   void HandleSqliteError(const url::Origin& origin,
                          const std::u16string& database_name,
                          int32_t error) override;
 
-  // DatabaseTracker::Observer callbacks (tracker sequence)
+  // DatabaseTracker::Observer:
   void OnDatabaseSizeChanged(const std::string& origin_identifier,
                              const std::u16string& database_name,
                              int64_t database_size) override;
@@ -89,6 +76,7 @@ class CONTENT_EXPORT WebDatabaseHostImpl
       const std::string& origin_identifier,
       const std::u16string& database_name) override;
 
+ private:
   void DatabaseDeleteFile(const std::u16string& vfs_file_name,
                           bool sync_dir,
                           DeleteFileCallback callback,
--- content/browser/web_database/web_database_host_impl_unittest.cc
+++ content/browser/web_database/web_database_host_impl_unittest.cc
@@ -51,15 +51,11 @@ std::u16string ConstructVfsFileName(const url::Origin& origin,
   return base::UTF8ToUTF16(identifier) + u"/" + name + u"#" + suffix;
 }
 
-}  // namespace
-
 class WebDatabaseHostImplTest : public ::testing::Test {
  public:
+  WebDatabaseHostImplTest() = default;
   WebDatabaseHostImplTest(const WebDatabaseHostImplTest&) = delete;
   WebDatabaseHostImplTest& operator=(const WebDatabaseHostImplTest&) = delete;
-
- protected:
-  WebDatabaseHostImplTest() = default;
   ~WebDatabaseHostImplTest() override = default;
 
   void SetUp() override {
@@ -95,6 +91,7 @@ class WebDatabaseHostImplTest : public ::testing::Test {
     RunUntilIdle();
   }
 
+ protected:
   template <typename Callable>
   void CheckUnauthorizedOrigin(const Callable& func) {
     mojo::test::BadMessageObserver bad_message_observer;
@@ -376,5 +373,7 @@ TEST_F(WebDatabaseHostImplTest, ProcessShutdown) {
 
   mojo::SetDefaultProcessErrorHandler(base::NullCallback());
 }
+
+}  // namespace
 
 }  // namespace content
