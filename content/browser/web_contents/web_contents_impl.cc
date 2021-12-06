3ba6b4272ac9f2be9a52ee0549f49a4bb1fdfe7b
Joe Mason <joenotcharles@google.com>
2021-12-06 17:29:03 +0000

    Record Browser.Tabs.TabSwitchHasRWHV histogram
    This will validate that the difference in record counts in the
    TabSwitchMetrics2 experiment is due to tab switches without
    RenderWidgetHostView that are now being recorded.
    
    Bug: 1164477
    Change-Id: Ic571d06e36d13584255f0c5bcf23a588c2e3271d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313683
    Quick-Run: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948577}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/metrics/field_trial.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/no_destructor.h"
@@ -9154,6 +9155,24 @@ void WebContentsImpl::UpdateBrowserControlsState(
 
 void WebContentsImpl::SetTabSwitchStartTime(base::TimeTicks start_time,
                                             bool destination_is_loaded) {
+  // TODO(crbug.com/1164477): Remove this UMA once the TabSwitchMetrics2
+  // experiment ends.
+  //
+  // The experiment is showing a mix shift, with more records received in
+  // the experiment group. The control group should preserve the old
+  // behaviour where Browser.Tabs.* metrics were never recorded if there
+  // was no RenderWidgetHostView at the time of the tab switch. To verify
+  // that this accounts for the mix shift, expect that:
+  //
+  // 1. The difference in Browser.Tabs.* record count between the control
+  //    and enabled groups will match the count of
+  //    Browser.Tabs.TabSwitchHasRWHV == false in the enabled group.
+  //
+  // 2. The count of Browser.Tabs.TabSwitchHasRWHV in the control and
+  //    enabled groups are equal.
+  base::UmaHistogramBoolean("Browser.Tabs.TabSwitchHasRWHV",
+                            GetRenderWidgetHostView());
+
   auto* trigger = GetVisibleTimeRequestTrigger();
   if (!trigger)
     return;
