52ace88b0490e78e7ae595b69c7008aabc42f6fd
Lei Zhang <thestig@chromium.org>
2021-12-06 19:30:37 +0000

    Fix memory leak in CalendarKeyedServiceTest.
    This test class stores FakeChromeUserManager using a raw pointer, when
    it should have been using a std::unique_ptr.
    
    Bug: 974478
    Change-Id: I179df75e752681a21724e77825f4b5a06085a121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315896
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948625}

--- chrome/browser/ui/ash/calendar/calendar_keyed_service_unittest.cc
+++ chrome/browser/ui/ash/calendar/calendar_keyed_service_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/calendar/calendar_keyed_service.h"
 
+#include <memory>
 #include <vector>
 
 #include "ash/calendar/calendar_controller.h"
@@ -37,7 +38,8 @@ const char kTestUserAgent[] = "test-user-agent";
 
 class CalendarKeyedServiceTest : public BrowserWithTestWindowTest {
  public:
-  CalendarKeyedServiceTest() : fake_user_manager_(new FakeChromeUserManager) {}
+  CalendarKeyedServiceTest()
+      : fake_user_manager_(std::make_unique<FakeChromeUserManager>()) {}
 
   CalendarKeyedServiceTest(const CalendarKeyedServiceTest& other) = delete;
   CalendarKeyedServiceTest& operator=(const CalendarKeyedServiceTest& other) =
@@ -80,7 +82,7 @@ class CalendarKeyedServiceTest : public BrowserWithTestWindowTest {
   }
 
  private:
-  FakeChromeUserManager* fake_user_manager_;
+  std::unique_ptr<FakeChromeUserManager> fake_user_manager_;
 };
 
 class CalendarKeyedServiceIOTest : public testing::Test {
