80fc22d053e4f60e7c5ddcc04e6476e57117e784
Lei Zhang <thestig@chromium.org>
2021-12-06 19:31:09 +0000

    Fix memory leak in ExistingUserControllerBaseTest.
    This base test class stores MockUserManager using a raw pointer, when it
    should have been using a std::unique_ptr.
    
    Bug: 974478
    Change-Id: I668bcf1bb2910c6b07d7fedcd70f9c4530418ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314288
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948626}

--- chrome/browser/ash/login/existing_user_controller_base_test.h
+++ chrome/browser/ash/login/existing_user_controller_base_test.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_LOGIN_EXISTING_USER_CONTROLLER_BASE_TEST_H_
 #define CHROME_BROWSER_ASH_LOGIN_EXISTING_USER_CONTROLLER_BASE_TEST_H_
 
+#include <memory>
+
 #include "components/account_id/account_id.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -55,8 +57,8 @@ class ExistingUserControllerBaseTest : public ::testing::Test {
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
 
  private:
-  MockUserManager* const mock_user_manager_;
-  std::unique_ptr<user_manager::ScopedUserManager> scoped_user_manager_;
+  std::unique_ptr<MockUserManager> const mock_user_manager_;
+  std::unique_ptr<user_manager::ScopedUserManager> const scoped_user_manager_;
 };
 
 }  // namespace ash
