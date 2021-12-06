99ab33c3b132fcdeb7239a9c27a6986983540eb7
anina koehler <aninak@google.com>
2021-12-06 11:55:58 +0000

    Change full screen bubble design
    This CL updates the design of the bubble that is shown when the device
    returns from sleep, low brightness or the lock screen. It now uses a
    SubtleNotificationView so that it matches the design of the bubble that
    is shown in the browser when entering full screen mode.
    
    Bug: b:203501653
    Change-Id: I0686d0c1bea4096541c1efda44c3b4e5ad77dd59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3298026
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Anina Koehler <aninak@google.com>
    Cr-Commit-Position: refs/heads/main@{#948485}

--- ash/session/fullscreen_controller.h
+++ ash/session/fullscreen_controller.h
@@ -16,7 +16,7 @@ class PrefRegistrySimple;
 namespace ash {
 
 class SessionControllerImpl;
-class FullscreenAlertBubble;
+class FullscreenNotificationBubble;
 
 class FullscreenController : public chromeos::PowerManagerClient::Observer {
  public:
@@ -27,8 +27,6 @@ class FullscreenController : public chromeos::PowerManagerClient::Observer {
   ~FullscreenController() override;
 
   static void MaybeExitFullscreen();
-
-  void MaybeShowAlert();
 
   static void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
@@ -42,9 +40,11 @@ class FullscreenController : public chromeos::PowerManagerClient::Observer {
   void LidEventReceived(chromeos::PowerManagerClient::LidState state,
                         base::TimeTicks timestamp) override;
 
+  void MaybeShowNotification();
+
   const SessionControllerImpl* const session_controller_;
 
-  std::unique_ptr<FullscreenAlertBubble> bubble_;
+  std::unique_ptr<FullscreenNotificationBubble> bubble_;
 
   // Whether the screen brightness is low enough to make display dark.
   bool device_in_dark_ = false;
