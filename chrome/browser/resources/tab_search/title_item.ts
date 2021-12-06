c98f1788fd72ccd6811a4208c871012d6b95fe58
dpapad <dpapad@chromium.org>
2021-12-06 12:16:50 +0000

    TabSearch: Migrate to TypeScript part 2.
     - Remove js_type_check/js_library targets (remaining type coverage
       will be re-added in follow-up CLs)
     - Check-in TypeScript definitions for fuse.js
     - Migrate all non-UI files to TypeScript.
     - Remove unnecessary constructor param for BiMap (was only used in
       tests).
    
    UI files will be migrated in follow-up CLs.
    
    Bug: 1260300
    Change-Id: I6f71e940f4ed4e18eb6c8a174f43f2cccf29c8a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312883
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948490}

--- chrome/browser/resources/tab_search/title_item.ts
+++ chrome/browser/resources/tab_search/title_item.ts
@@ -4,19 +4,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 export class TitleItem {
-  /**
-   * @param {string} title
-   * @param {boolean} expandable
-   * @param {boolean} expanded
-   */
-  constructor(title, expandable = false, expanded = false) {
-    /** @type {string} */
+  title: string;
+  expandable: boolean;
+  expanded: boolean;
+
+  constructor(
+      title: string, expandable: boolean = false, expanded: boolean = false) {
     this.title = title;
-
-    /** @type {boolean} */
     this.expandable = expandable;
-
-    /** @type {boolean} */
     this.expanded = expanded;
   }
 }
