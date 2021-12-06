93fc78fae982e9909360b96e196e228a65187ca9
Rune Lillesveen <futhark@chromium.org>
2021-12-06 18:14:38 +0000

    [@container] Don't reattach fieldset while being laid out
    Fieldsets were fully re-attached when any child were re-attached to
    change the box structure for rendered legends. If the fieldset is a
    container for size container queries, this meant the LayoutObject for
    the fieldset would be destroyed while being laid out.
    
    Instead, re-attach all descendants of fieldset, not the fieldset itself.
    
    Bug: 1275087
    Change-Id: I4bee17065828e97f46f920eb1077581b33e78d7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312998
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948592}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/text.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
+#include "third_party/blink/renderer/core/html/forms/html_field_set_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/html_body_element.h"
 #include "third_party/blink/renderer/core/html/html_html_element.h"
@@ -2320,7 +2321,19 @@ void StyleEngine::UpdateStyleAndLayoutTreeForContainer(
   // with itself for its children.
   RecalcStyle(change, StyleRecalcContext());
 
-  if (container.ChildNeedsReattachLayoutTree()) {
+  if (UNLIKELY(container.NeedsReattachLayoutTree())) {
+    // Generally, the container itself should not be marked for re-attachment.
+    // In the case where we have a fieldset as a container, the fieldset itself
+    // is marked for re-attachment in HTMLFieldSetElement::DidRecalcStyle to
+    // make sure the rendered legend is appropriately placed in the layout tree.
+    // We cannot re-attach the fieldset itself in this case since we are in the
+    // process of laying it out. Instead we re-attach all children, which should
+    // be sufficient.
+    auto* fieldset = DynamicTo<HTMLFieldSetElement>(container);
+    DCHECK(fieldset)
+        << "Only fieldsets may be marked for re-attachment as query containers";
+    RebuildFieldSetContainer(*fieldset);
+  } else if (container.ChildNeedsReattachLayoutTree()) {
     DCHECK(layout_tree_rebuild_root_.GetRootNode());
     if (layout_tree_rebuild_root_.GetRootNode()->IsDocumentNode()) {
       // Avoid traversing from outside the container root. We know none of the
@@ -2395,6 +2408,15 @@ void StyleEngine::ClearEnsuredDescendantStyles(Element& root) {
   }
 }
 
+void StyleEngine::RebuildLayoutTreeForTraversalRootAncestors(Element* parent) {
+  for (auto* ancestor = parent; ancestor;
+       ancestor = ancestor->GetReattachParent()) {
+    ancestor->RebuildLayoutTreeForTraversalRootAncestor();
+    ancestor->ClearChildNeedsStyleRecalc();
+    ancestor->ClearChildNeedsReattachLayoutTree();
+  }
+}
+
 void StyleEngine::RebuildLayoutTree() {
   bool propagate_to_root = false;
   {
@@ -2412,13 +2434,8 @@ void StyleEngine::RebuildLayoutTree() {
       root_element.RebuildLayoutTree(whitespace_attacher);
     }
 
-    for (ContainerNode* ancestor = root_element.GetReattachParent(); ancestor;
-         ancestor = ancestor->GetReattachParent()) {
-      if (auto* ancestor_element = DynamicTo<Element>(ancestor))
-        ancestor_element->RebuildLayoutTreeForTraversalRootAncestor();
-      ancestor->ClearChildNeedsStyleRecalc();
-      ancestor->ClearChildNeedsReattachLayoutTree();
-    }
+    RebuildLayoutTreeForTraversalRootAncestors(
+        root_element.GetReattachParent());
     layout_tree_rebuild_root_.Clear();
     propagate_to_root = IsA<HTMLHtmlElement>(root_element) ||
                         IsA<HTMLBodyElement>(root_element);
@@ -2428,6 +2445,14 @@ void StyleEngine::RebuildLayoutTree() {
     if (NeedsLayoutTreeRebuild())
       RebuildLayoutTree();
   }
+}
+
+void StyleEngine::RebuildFieldSetContainer(HTMLFieldSetElement& fieldset) {
+  DCHECK(fieldset.NeedsReattachLayoutTree());
+  base::AutoReset<bool> rebuild_scope(&in_layout_tree_rebuild_, true);
+  fieldset.ReattachLayoutTreeChildren();
+  RebuildLayoutTreeForTraversalRootAncestors(&fieldset);
+  layout_tree_rebuild_root_.Clear();
 }
 
 void StyleEngine::UpdateStyleAndLayoutTree() {
--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/dcheck_is_on.h"
 #include "base/gtest_prod_util.h"
+#include "base/types/pass_key.h"
 #include "third_party/blink/public/common/input/pointer_id.h"
 #include "third_party/blink/public/common/metrics/document_update_reason.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_typedefs.h"
@@ -82,6 +83,7 @@ class ExceptionState;
 class FloatQuad;
 class FocusOptions;
 class GetInnerHTMLOptions;
+class HTMLFieldSetElement;
 class HTMLTemplateElement;
 class Image;
 class InputDeviceCapabilities;
@@ -1107,6 +1109,11 @@ class CORE_EXPORT Element : public ContainerNode, public Animatable {
   void AdjustForceLegacyLayout(const ComputedStyle*,
                                bool* should_force_legacy_layout);
 
+  // Reattach layout tree for all children but not the element itself. This is
+  // only used for reattaching fieldset children when the fieldset is a query
+  // container for size container queries.
+  void ReattachLayoutTreeChildren(base::PassKey<HTMLFieldSetElement>);
+
  private:
   friend class AXObject;
 
@@ -1193,6 +1200,29 @@ class CORE_EXPORT Element : public ContainerNode, public Animatable {
                                                     const StyleRecalcContext&);
   void AttachPseudoElement(PseudoId, AttachContext&);
   void DetachPseudoElement(PseudoId, bool performing_reattach);
+
+  void AttachPrecedingPseudoElements(AttachContext& context) {
+    AttachPseudoElement(kPseudoIdMarker, context);
+    AttachPseudoElement(kPseudoIdBefore, context);
+  }
+
+  void AttachSucceedingPseudoElements(AttachContext& context) {
+    AttachPseudoElement(kPseudoIdAfter, context);
+    AttachPseudoElement(kPseudoIdBackdrop, context);
+    UpdateFirstLetterPseudoElement(StyleUpdatePhase::kAttachLayoutTree);
+    AttachPseudoElement(kPseudoIdFirstLetter, context);
+  }
+
+  void DetachPrecedingPseudoElements(bool performing_reattach) {
+    DetachPseudoElement(kPseudoIdMarker, performing_reattach);
+    DetachPseudoElement(kPseudoIdBefore, performing_reattach);
+  }
+
+  void DetachSucceedingPseudoElements(bool performing_reattach) {
+    DetachPseudoElement(kPseudoIdAfter, performing_reattach);
+    DetachPseudoElement(kPseudoIdBackdrop, performing_reattach);
+    DetachPseudoElement(kPseudoIdFirstLetter, performing_reattach);
+  }
 
   ShadowRoot& CreateAndAttachShadowRoot(ShadowRootType);
 
