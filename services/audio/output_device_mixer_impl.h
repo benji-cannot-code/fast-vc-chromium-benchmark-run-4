5887ed81b76cb0b26c921e62b636fec74607b65d
Olga Sharonova <olka@chromium.org>
2021-12-06 19:12:22 +0000

    OutputDeviceMixer: ensure error reporting does not invalidate iterators.
    It does not, since the call stack
    OutputController::OnError() ->
    OutputStream::OnControllerError() ->
    OutputStream::OnError() --> posts OutputStream::CallDeleter()
    
    which ends up calling
    StreamFactory::DestroyOutputStream() ->
    OutputStream::~OutputStream() ->
    OutputController::Close() -> stopping and closing the stream.
    
    So Stop()/Close() is never done within the error reporting context.
    
    We are just adding a checker for that.
    
    Bug: 1224845
    Change-Id: I9b05366a54a07f5335bdb1fe2caf7973b16cb03a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311283
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948612}

--- services/audio/output_device_mixer_impl.h
+++ services/audio/output_device_mixer_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "media/audio/audio_io.h"
 #include "media/base/audio_parameters.h"
+#include "media/base/reentrancy_checker.h"
 #include "services/audio/mixing_graph.h"
 #include "services/audio/output_device_mixer.h"
 
@@ -168,6 +169,12 @@ class OutputDeviceMixerImpl final : public OutputDeviceMixer {
 #if DCHECK_IS_ON()
   bool device_changed_ = false;
 #endif
+
+  // A mixable stream operation cannot be invoked within a context of another
+  // such operation. In practice it means that AudioOutputStream created by the
+  // mixer cannot be stopped/closed synchronously from AudioSourceCallback
+  // provided to it on AudioOutputStream::Start().
+  REENTRANCY_CHECKER(reentrancy_checker_);
 
   // Supplies weak pointers to |this| for MixableOutputStream instances.
   base::WeakPtrFactory<OutputDeviceMixerImpl> weak_factory_{this};
