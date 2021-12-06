e99596e766149e49484c280b86e1c0f51195dcc3
Justin Novosad <junov@chromium.org>
2021-12-06 15:51:53 +0000

    Fix PDF viewer thumbnails not rendering after print preview
    The Document object was staying stuck in "printing" state after
    closing the print preview and/or system print dialogs.  This
    was causing web page compositing to malfunction. This fix restores
    the document's state to kNotPrinting after dispatching the
    afterprint event.
    
    BUG=1273342
    
    Change-Id: Iece9ba5c3cbffc39308c90c7780b03a64f92c11d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313449
    Auto-Submit: Justin Novosad <junov@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948540}

--- third_party/blink/renderer/core/page/print_context.h
+++ third_party/blink/renderer/core/page/print_context.h
@@ -116,6 +116,7 @@ class CORE_EXPORT PrintContext : public GarbageCollected<PrintContext> {
   friend class PrintContextTest;
 
   void OutputLinkedDestinations(GraphicsContext&, const gfx::Rect& page_rect);
+  bool IsFrameValid() const;
 
   Member<LocalFrame> frame_;
   Vector<gfx::Rect> page_rects_;
@@ -124,7 +125,6 @@ class CORE_EXPORT PrintContext : public GarbageCollected<PrintContext> {
   void ComputePageRectsWithPageSizeInternal(
       const gfx::SizeF& page_size_in_pixels);
   void CollectLinkedDestinations(Node*);
-  bool IsFrameValid() const;
 
   // Used to prevent misuses of BeginPrintMode() and EndPrintMode() (e.g., call
   // EndPrintMode() without BeginPrintMode()).
