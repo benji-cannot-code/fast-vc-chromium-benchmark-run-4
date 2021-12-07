789b919b16f2a189e7ec85358127ae42a8bed264
Yuzu Saijo <yuzus@chromium.org>
2021-12-07 05:24:43 +0000

    [bfcache] Fix UMA reporting name for AX events
    We added UMA for AX events here: https://crrev.com/c/3240642
    But we have not observed any case recorded.
    
    This seems to be happening because of the mismatch between the UMA name
    in back_forward_cache_metrics.cc and histograms.xml.
    This CL fixes the mismatch.
    
    AX-Relnotes: n/a (non user-facing change)
    Bug: 1263346
    Change-Id: I07998397541979c0bfa09063079d1592b0159665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3316099
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948848}

--- content/browser/renderer_host/back_forward_cache_metrics.cc
+++ content/browser/renderer_host/back_forward_cache_metrics.cc
@@ -437,6 +437,7 @@ void BackForwardCacheMetrics::RecordMetricsForHistoryNavigationCommit(
   for (const ax::mojom::Event event : page_store_result_->ax_events()) {
     base::UmaHistogramSparse(
         "BackForwardCache.HistoryNavigationOutcome."
+        "NotRestoredDueToAccessibility."
         "AXEventType",
         static_cast<int>(event));
   }
