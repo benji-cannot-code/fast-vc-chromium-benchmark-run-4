f4ba250feffddbf9d28c7662c67c065d365ea348
Olga Sharonova <olka@chromium.org>
2021-12-07 17:48:55 +0000

    OutputDeviceMixer UMA: Add default/non-default device subcategories
    Bug: 1224845
    Change-Id: Ia4c149e7b8705e879566a92cdf927c0d13b28b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320278
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949018}

--- services/audio/output_device_mixer_impl.cc
+++ services/audio/output_device_mixer_impl.cc
@@ -38,6 +38,12 @@ const char* LatencyToUmaSuffix(media::AudioLatency::LatencyType latency) {
   }
 }
 
+const char* DeviceIdToUmaSuffix(const std::string& device_id) {
+  if (device_id == "")
+    return ".Default";
+  return ".NonDefault";
+}
+
 // Do not change: used for UMA reporting, matches
 // AudioOutputDeviceMixerStreamStatus from enums.xml.
 enum class TrackError {
@@ -333,8 +339,11 @@ class OutputDeviceMixerImpl::MixableOutputStream final
 // playback starts, and destroyed when it ends.
 class OutputDeviceMixerImpl::MixingStats {
  public:
-  MixingStats(int active_track_count, int listener_count)
-      : active_track_count_(active_track_count),
+  MixingStats(const std::string& device_id,
+              int active_track_count,
+              int listener_count)
+      : suffix_(DeviceIdToUmaSuffix(device_id)),
+        active_track_count_(active_track_count),
         listener_count_(listener_count),
         start_(base::TimeTicks::Now()) {
     DCHECK_GT(active_track_count, 0);
@@ -347,18 +356,9 @@ class OutputDeviceMixerImpl::MixingStats {
     }
 
     DCHECK(!start_.is_null());
-    base::UmaHistogramLongTimes("Media.Audio.OutputDeviceMixer.MixingDuration",
-                                base::TimeTicks::Now() - start_);
-
-    constexpr int kMaxActiveStreamCount = 50;
-    constexpr int kMaxListeners = 20;
-
-    base::UmaHistogramExactLinear(
-        "Media.Audio.OutputDeviceMixer.MaxMixedStreamCount",
-        active_track_count_.GetMax(), kMaxActiveStreamCount);
-    base::UmaHistogramExactLinear(
-        "Media.Audio.OutputDeviceMixer.MaxListenerCount",
-        listener_count_.GetMax(), kMaxListeners);
+    base::TimeDelta duration = base::TimeTicks::Now() - start_;
+    LogPerDeviceUma(duration, suffix_);
+    LogPerDeviceUma(duration, "");  // Combined.
   }
 
   void AddListener() { listener_count_.Increment(); }
@@ -413,6 +413,24 @@ class OutputDeviceMixerImpl::MixingStats {
     noop_mixing_start_ = base::TimeTicks();
   }
 
+  void LogPerDeviceUma(base::TimeDelta duration, const char* suffix) {
+    constexpr int kMaxActiveStreamCount = 50;
+    constexpr int kMaxListeners = 20;
+
+    base::UmaHistogramLongTimes(
+        base::StrCat({"Media.Audio.OutputDeviceMixer.MixingDuration", suffix}),
+        duration);
+    base::UmaHistogramExactLinear(
+        base::StrCat(
+            {"Media.Audio.OutputDeviceMixer.MaxMixedStreamCount", suffix}),
+        active_track_count_.GetMax(), kMaxActiveStreamCount);
+    base::UmaHistogramExactLinear(
+        base::StrCat(
+            {"Media.Audio.OutputDeviceMixer.MaxListenerCount", suffix}),
+        listener_count_.GetMax(), kMaxListeners);
+  }
+
+  const char* const suffix_;
   MaxTracker active_track_count_;
   MaxTracker listener_count_;
   const base::TimeTicks start_;
@@ -769,7 +787,7 @@ void OutputDeviceMixerImpl::StartMixingGraphPlayback() {
 
   DCHECK(!mixing_stats_);
   mixing_stats_ = std::make_unique<MixingStats>(
-      active_tracks_.size(), TS_UNCHECKED_READ(listeners_).size());
+      device_id(), active_tracks_.size(), TS_UNCHECKED_READ(listeners_).size());
 
   for (MixTrack* mix_track : active_tracks_)
     mix_track->StartProvidingAudioToMixingGraph();
