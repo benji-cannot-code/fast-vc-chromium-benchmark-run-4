a4a2f8cf7ed490caf11470c69fc180fdde69d346
Caitlin Fischer <caitlinfischer@google.com>
2021-12-07 19:54:06 +0000

    Deprecate SystemProfileProto::Stability.incomplete_shutdown_count.
    Server-side change: cr/412602713
    
    Bug: 1274273
    Change-Id: Ib160be9c63f17c1463669dbec1d3742380d5ac14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3305034
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#949099}

--- components/metrics/stability_metrics_provider.h
+++ components/metrics/stability_metrics_provider.h
@@ -30,9 +30,6 @@ class StabilityMetricsProvider : public MetricsProvider {
 
   static void RegisterPrefs(PrefRegistrySimple* registry);
 
-  void CheckLastSessionEndCompleted();
-  void MarkSessionEndCompleted(bool end_completed);
-
   void LogCrash(base::Time last_live_timestamp);
   void LogLaunch();
 
