8e49ca651f057d24d631115b58d41cb09e3735c0
Brittany Hartmire <bhartmire@google.com>
2021-12-07 19:24:47 +0000

    [SmartLock] Arrow Button Nudge Animation
    Animation around arrow button icon when user needs to tap the icon to
    unlock Chromebook with Smart Lock.
    
    Video: https://bugs.chromium.org/p/chromium/issues/detail?id=1233614#c33
    Design Spec: https://carbon.googleplex.com/cr-os-motion-work/pages/nudge-live-captions/round-3
    
    Bug: 1233614
    Change-Id: I0f15d2b3c21de440f5296490be02708f95b8cde1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3300506
    Reviewed-by: Thomas Tellier <tellier@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#949085}

--- ash/login/ui/auth_icon_view.h
+++ ash/login/ui/auth_icon_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "ui/gfx/image/canvas_image_source.h"
 #include "ui/views/view.h"
 
 namespace gfx {
@@ -39,6 +40,9 @@ class ASH_EXPORT AuthIconView : public views::View {
   // Show a static icon.
   void SetIcon(const gfx::VectorIcon& icon, Color color = Color::kPrimary);
 
+  // Show a circle icon.
+  void SetCircleImage(int size, SkColor color);
+
   // Show a sequence of animation frames. |animation_resource_id| should refer
   // to an image with the frames of the animation layed out horizontally.
   // |duration| is the total duration of the animation. |num_frames| is the
@@ -51,11 +55,18 @@ class ASH_EXPORT AuthIconView : public views::View {
   // occurred.
   void RunErrorShakeAnimation();
 
+  // Cause the icon to repeatedly emit a circle that gradually scales up and
+  // fades out in order to nudge user to click.
+  void RunNudgeAnimation();
+
   // Starts a progress spinner animation if not already running.
   void StartProgressAnimation();
 
   // Stops the progress spinner animation if running.
   void StopProgressAnimation();
+
+  // Stops any existing animations.
+  void StopAnimating();
 
   void set_on_tap_or_click_callback(base::RepeatingClosure on_tap_or_click) {
     on_tap_or_click_callback_ = on_tap_or_click;
@@ -68,6 +79,20 @@ class ASH_EXPORT AuthIconView : public views::View {
   bool OnMousePressed(const ui::MouseEvent& event) override;
 
  private:
+  // Helper class to draw a circle that can be converted to "gfx::ImageSkia"
+  class CircleImageSource : public gfx::CanvasImageSource {
+   public:
+    explicit CircleImageSource(int size, SkColor color);
+    CircleImageSource(const CircleImageSource&) = delete;
+    CircleImageSource& operator=(const CircleImageSource&) = delete;
+    ~CircleImageSource() override = default;
+
+    void Draw(gfx::Canvas* canvas) override;
+
+   private:
+    SkColor color_;
+  };
+
   base::RepeatingClosure on_tap_or_click_callback_;
 
   AnimatedRoundedImageView* icon_;
--- ash/login/ui/login_auth_factors_view_unittest.cc
+++ ash/login/ui/login_auth_factors_view_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/events/event.h"
 #include "ui/views/accessibility/ax_event_manager.h"
 #include "ui/views/accessibility/ax_event_observer.h"
@@ -246,6 +247,7 @@ TEST_F(LoginAuthFactorsViewUnittest, SingleIconInAvailableState) {
   EXPECT_TRUE(test_api.auth_factor_icon_row()->GetVisible());
   EXPECT_FALSE(test_api.checkmark_icon()->GetVisible());
   EXPECT_FALSE(test_api.arrow_button()->GetVisible());
+  EXPECT_FALSE(test_api.arrow_nudge_animation()->GetVisible());
 
   // The number of icons should match the number of auth factors initialized.
   EXPECT_EQ(auth_factors_.size(),
@@ -269,6 +271,7 @@ TEST_F(LoginAuthFactorsViewUnittest, MultipleAuthFactorsInReadyState) {
   EXPECT_TRUE(test_api.auth_factor_icon_row()->GetVisible());
   EXPECT_FALSE(test_api.checkmark_icon()->GetVisible());
   EXPECT_FALSE(test_api.arrow_button()->GetVisible());
+  EXPECT_FALSE(test_api.arrow_nudge_animation()->GetVisible());
 
   // The number of icons should match the number of auth factors initialized.
   EXPECT_EQ(auth_factors_.size(),
@@ -284,19 +287,49 @@ TEST_F(LoginAuthFactorsViewUnittest, MultipleAuthFactorsInReadyState) {
 }
 
 TEST_F(LoginAuthFactorsViewUnittest, ClickRequired) {
+  ui::ScopedAnimationDurationScaleMode non_zero_duration_mode(
+      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+
   AddAuthFactors({AuthFactorType::kFingerprint, AuthFactorType::kSmartLock});
   LoginAuthFactorsView::TestApi test_api(view_);
   auth_factors_[0]->state_ = AuthFactorState::kReady;
   auth_factors_[1]->state_ = AuthFactorState::kClickRequired;
   test_api.UpdateState();
 
-  // Check that only the arrow button is shown and that the label has been
-  // updated.
+  // Check that the arrow button and arrow nudge animation is shown and that the
+  // label has been updated.
   EXPECT_TRUE(test_api.arrow_button()->GetVisible());
+  EXPECT_TRUE(test_api.arrow_nudge_animation()->GetVisible());
   EXPECT_FALSE(test_api.checkmark_icon()->GetVisible());
   EXPECT_FALSE(test_api.auth_factor_icon_row()->GetVisible());
   EXPECT_EQ(l10n_util::GetStringUTF16(IDS_AUTH_FACTOR_LABEL_CLICK_TO_ENTER),
             test_api.label()->GetText());
+}
+
+TEST_F(LoginAuthFactorsViewUnittest, ClickingArrowButton) {
+  ui::ScopedAnimationDurationScaleMode non_zero_duration_mode(
+      ui::ScopedAnimationDurationScaleMode::NORMAL_DURATION);
+
+  AddAuthFactors({AuthFactorType::kFingerprint, AuthFactorType::kSmartLock});
+  LoginAuthFactorsView::TestApi test_api(view_);
+  auth_factors_[0]->state_ = AuthFactorState::kReady;
+  auth_factors_[1]->state_ = AuthFactorState::kClickRequired;
+  test_api.UpdateState();
+
+  // Check that the arrow button and arrow nudge animation is shown.
+  EXPECT_TRUE(test_api.arrow_button()->GetVisible());
+  EXPECT_TRUE(test_api.arrow_nudge_animation()->GetVisible());
+
+  // Simulate clicking arrow nudge animation, which sits on top of arrow button
+  // and should relay arrow button click.
+  const gfx::Point point(0, 0);
+  test_api.arrow_nudge_animation()->OnMousePressed(ui::MouseEvent(
+      ui::ET_MOUSE_PRESSED, point, point, base::TimeTicks::Now(), 0, 0));
+
+  // Check that arrow button is still visible and that arrow nudge animation is
+  // no longer shown.
+  EXPECT_TRUE(test_api.arrow_button()->GetVisible());
+  EXPECT_FALSE(test_api.arrow_nudge_animation()->GetVisible());
 }
 
 TEST_F(LoginAuthFactorsViewUnittest, Authenticated) {
@@ -310,6 +343,7 @@ TEST_F(LoginAuthFactorsViewUnittest, Authenticated) {
   // updated.
   EXPECT_TRUE(test_api.checkmark_icon()->GetVisible());
   EXPECT_FALSE(test_api.arrow_button()->GetVisible());
+  EXPECT_FALSE(test_api.arrow_nudge_animation()->GetVisible());
   EXPECT_FALSE(test_api.auth_factor_icon_row()->GetVisible());
   EXPECT_EQ(l10n_util::GetStringUTF16(IDS_AUTH_FACTOR_LABEL_UNLOCKED),
             test_api.label()->GetText());
@@ -325,6 +359,7 @@ TEST_F(LoginAuthFactorsViewUnittest, ErrorTemporary) {
   EXPECT_TRUE(test_api.auth_factor_icon_row()->GetVisible());
   EXPECT_FALSE(test_api.checkmark_icon()->GetVisible());
   EXPECT_FALSE(test_api.arrow_button()->GetVisible());
+  EXPECT_FALSE(test_api.arrow_nudge_animation()->GetVisible());
 
   // Only the error should be visible for the first three seconds after the
   // state update.
@@ -346,6 +381,7 @@ TEST_F(LoginAuthFactorsViewUnittest, ErrorPermanent) {
   EXPECT_TRUE(test_api.auth_factor_icon_row()->GetVisible());
   EXPECT_FALSE(test_api.checkmark_icon()->GetVisible());
   EXPECT_FALSE(test_api.arrow_button()->GetVisible());
+  EXPECT_FALSE(test_api.arrow_nudge_animation()->GetVisible());
 
   // Only the error should be visible for the first three seconds after the
   // state update.
