1d16ed1bc3e8d0e9681b2a792152827ddbe79a35
Jiewei Qian <qjw@chromium.org>
2021-12-07 23:50:32 +0000

    Revert "[IOS] Fix New Search when Discover is hidden."
    This reverts commit 10a91ad84fb58202c3a0685d9a7b1b7a69f79696.
    
    Reason for revert: Breaking ios bot, https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/21895/overview
    
    Original change's description:
    > [IOS] Fix New Search when Discover is hidden.
    >
    > Fixes the issue where focusing the fakebox on the NTP when the Discover
    > feed is hidden doesn't show the omnibox (this happens when using the
    > 'New Search' option from the new tab menu).
    >
    > The fix is to change how the minimum NTP height is calculated -- this is
    > the 2-line change to ContentsSuggestionsLayout.
    >
    > The remainder of the CL is adding EG tests for the New Search menu
    > item, with and without enabling the Discover feed. This also adds
    > UI-driven support for showing and hiding the feed.
    >
    > Bug: 1273046, 1194106
    > Change-Id: Ie7d54cf65841644e0b2605d0436925fc618a771d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314898
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Mark Cogan <marq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#948974}
    
    Bug: 1273046, 1194106
    Change-Id: I757d4868ff4dbb639ebabe433f6d80b9d622d4ca
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320647
    Auto-Submit: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Owners-Override: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949248}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_layout.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_layout.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CGFloat ntpHeight = collectionViewHeight + headerHeight;
   CGFloat minimumHeight =
       ntpHeight - ntp_header::kScrolledToTopOmniboxBottomMargin;
+  CGFloat topSafeArea = self.collectionView.safeAreaInsets.top;
   if (!IsRegularXRegularSizeClass(self.collectionView)) {
     CGFloat toolbarHeight =
         IsSplitToolbarMode(self.collectionView)
@@ -37,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                         .preferredContentSizeCategory)
             : 0;
     CGFloat additionalHeight =
-        toolbarHeight + self.collectionView.contentInset.bottom;
+        toolbarHeight + topSafeArea + self.collectionView.contentInset.bottom;
     minimumHeight -= additionalHeight;
     ntpHeight += additionalHeight;
   }
--- ios/chrome/test/earl_grey/chrome_matchers.mm
+++ ios/chrome/test/earl_grey/chrome_matchers.mm
@@ -171,10 +171,6 @@ id<GREYMatcher> ToolsMenuButton() {
   return [ChromeMatchersAppInterface toolsMenuButton];
 }
 
-id<GREYMatcher> NewTabButton() {
-  return [ChromeMatchersAppInterface openNewTabButton];
-}
-
 id<GREYMatcher> ShareButton() {
   return [ChromeMatchersAppInterface shareButton];
 }
@@ -434,14 +430,6 @@ id<GREYMatcher> NTPCollectionView() {
 
 id<GREYMatcher> NTPIncognitoView() {
   return [ChromeMatchersAppInterface ntpIncognitoView];
-}
-
-id<GREYMatcher> NTPFeedMenuEnableButton() {
-  return [ChromeMatchersAppInterface ntpFeedMenuEnableButton];
-}
-
-id<GREYMatcher> NTPFeedMenuDisableButton() {
-  return [ChromeMatchersAppInterface ntpFeedMenuDisableButton];
 }
 
 id<GREYMatcher> WarningMessageView() {
