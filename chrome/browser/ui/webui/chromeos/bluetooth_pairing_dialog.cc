6792718095135baa4e8820372f4c03620ea786f5
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-12-07 19:12:22 +0000

    [CrOS Bluetooth] Update pairing dialog strings
    Brings Bluetooth pairing UI string up to spec.
    
    Screenshot: https://screenshot.googleplex.com/4bwmt8ST3icWdLr.png
                https://screenshot.googleplex.com/DoXNRbD8n6dWhLE.png
    
    Bug: b:207588508, b:209061678
    Change-Id: I43b6bb31c02b5eea10f4e0af4255cbbe5d0ce6b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314286
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#949082}

--- chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
+++ chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
@@ -34,7 +34,7 @@ namespace chromeos {
 namespace {
 
 constexpr int kBluetoothPairingDialogHeight = 375;
-constexpr int kBluetoothPairingDialogHeightWithFlag = 408;
+constexpr int kBluetoothPairingDialogHeightWithFlag = 424;
 
 void AddBluetoothStrings(content::WebUIDataSource* html_source) {
   struct {
