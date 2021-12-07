6c0131d917b8ce940f5da3cb136441e0f3b6f24b
Nidhi Jaju <nidhijaju@chromium.org>
2021-12-07 06:01:39 +0000

    AbortSignal: Use signal.reason in Streams implementation
    This CL follows https://github.com/whatwg/streams/pull/1182, which
    introduces custom abort reasons using the AbortSignal's abort reason
    in the ReadableStreamPipeTo function[1]. This change also updates
    WritableStreamAbort[2] to use the reason to signal abort.
    
    [1] https://streams.spec.whatwg.org/#readable-stream-pipe-to
    [2] https://streams.spec.whatwg.org/#writable-stream-abort
    
    Intent to Ship: https://groups.google.com/a/chromium.org/g/blink-dev/c/T6B5czAke1I
    
    Bug: 1267135, 1268515
    Change-Id: Ia9bdc0d0d46505f3e764d7a396a4fe19da843b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295823
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948860}

--- third_party/blink/renderer/core/streams/writable_stream_default_controller.h
+++ third_party/blink/renderer/core/streams/writable_stream_default_controller.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class AbortSignal;
 class ExceptionState;
 class QueueWithSizes;
 class ScriptState;
@@ -109,6 +110,9 @@ class CORE_EXPORT WritableStreamDefaultController final
                             WritableStreamDefaultController*,
                             v8::Local<v8::Value> error);
 
+  // IDL attributes
+  AbortSignal* signal() const { return signal_; }
+
   void Trace(Visitor*) const override;
 
  private:
@@ -142,6 +146,7 @@ class CORE_EXPORT WritableStreamDefaultController final
   // stored-as-is, and the `"close"` marker in the queue is represented by an
   // empty queue together with the |close_queued_| flag being set.
   Member<QueueWithSizes> queue_;
+  Member<AbortSignal> signal_;
   bool close_queued_ = false;
   bool started_ = false;
   double strategy_high_water_mark_ = 0.0;
