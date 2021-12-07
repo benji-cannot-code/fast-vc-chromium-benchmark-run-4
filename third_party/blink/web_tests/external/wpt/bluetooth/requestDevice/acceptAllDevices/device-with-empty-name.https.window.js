33dee3d6ed1141b3135fbf2bb37bd0759f124799
Jack Hsieh <chengweih@chromium.org>
2021-12-07 02:33:54 +0000

    bluetooth: bluetooth acceptAllDevices tests to standalone tests
    This change updates the tests in
    wpt/bluetooth/requestDevice/acceptAllDevices to be standalone window
    tests. This change allows the tests to be formatted with clang-format.
    
    Bug: 994756
    Change-Id: I633d5e4550d7784b9a98752590d19d5c609fd556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315347
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948795}

---
+++ third_party/blink/web_tests/external/wpt/bluetooth/requestDevice/acceptAllDevices/device-with-empty-name.https.window.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+// META: script=/bluetooth/resources/bluetooth-test.js
+// META: script=/bluetooth/resources/bluetooth-fake-devices.js
+'use strict';
+const test_desc = 'Device with empty name and no UUIDs nearby. Should be ' +
+    'found if acceptAllDevices is true.';
+
+bluetooth_test(async () => {
+  let { device } = await setUpPreconnectedFakeDevice({
+    fakeDeviceOptions: {
+      name: ''
+    },
+    requestDeviceOptions: {
+      acceptAllDevices: true
+    }
+  });
+  assert_equals(device.name, '');
+}, test_desc);
---
+++ third_party/blink/web_tests/external/wpt/bluetooth/requestDevice/acceptAllDevices/device-with-name.https.window.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+// META: script=/bluetooth/resources/bluetooth-test.js
+// META: script=/bluetooth/resources/bluetooth-fake-devices.js
+const test_desc =
+    'A device with name and no UUIDs nearby. Should be found if ' +
+    'acceptAllDevices is true.';
+const name = 'LE Device';
+
+bluetooth_test(async () => {
+  let { device } = await setUpPreconnectedFakeDevice({
+    fakeDeviceOptions: {
+      name: name
+    },
+    requestDeviceOptions: {
+      acceptAllDevices: true
+    }
+  });
+  assert_equals(device.name, name);
+}, test_desc);
