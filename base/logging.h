907b05b2b1271652e30c2fb730fe607a797110ac
Eric Willigers <ericwilligers@chromium.org>
2021-12-07 21:44:54 +0000

    base: include build/build_config.h
    Various source files use OS_... macros, and therefore now include
    build/build_config.h
    
    Change-Id: I5eb323759b702469755c043bd3c017e864d67ce2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320723
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949177}

--- base/logging.h
+++ base/logging.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/dcheck_is_on.h"
 #include "base/scoped_clear_last_error.h"
 #include "base/strings/string_piece_forward.h"
+#include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
--- base/process/internal_linux.cc
+++ base/process/internal_linux.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 
 // Not defined on AIX by default.
 #if defined(OS_AIX)
