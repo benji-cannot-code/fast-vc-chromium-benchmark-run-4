bd6de1b9409da9ba32deb6cc63e9418a409804cf
Simon Zünd <szuend@chromium.org>
2021-12-07 14:53:13 +0000

    [devtools] Move AsyncTaskContext into separate header
    AsyncTaskContext is included by a few blink/core headers. To reduce
    compilation time and issues, this CL moves AsyncTaskContext into
    its own header to prevent inclusion of local_frame.h and document.h
    
    Please note that because the implementation of AsyncTaskContext is
    shared currently with functions from core_probes.h, the implementation
    itself is not moved into its own .cc file. This can be done after the
    migration is complete.
    
    Bug: 1275875
    Change-Id: I73fd6ebb2f2afd43123ba36f02f72208a104f6cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320297
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948975}

--- third_party/blink/renderer/core/probe/core_probes.cc
+++ third_party/blink/renderer/core/probe/core_probes.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_probes_inl.h"
 #include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
 #include "third_party/blink/renderer/core/inspector/thread_debugger.h"
+#include "third_party/blink/renderer/core/probe/async_task_context.h"
 #include "third_party/blink/renderer/core/probe/async_task_id.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 
