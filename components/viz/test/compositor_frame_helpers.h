dbf0097645368f073c4220c8aa15aab4716a93a2
kylechar <kylechar@chromium.org>
2021-12-07 23:17:41 +0000

    Extend RenderPassBuilder + DrawQuad matchers
    I've added some more functionality to make sure both RenderPassBuilder
    and DrawQuad gmock matchers are more complete. Update more
    SurfaceAggregatorTests in the process.
    
    Bug: 1194082
    Change-Id: I6523a27ae8e8b9af21a91f64605495769d476857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311733
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949235}

--- components/viz/test/compositor_frame_helpers.h
+++ components/viz/test/compositor_frame_helpers.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/surfaces/surface_id.h"
 #include "components/viz/service/display/aggregated_frame.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/skia/include/core/SkBlendMode.h"
 #include "ui/latency/latency_info.h"
 
 namespace viz {
@@ -47,7 +48,7 @@ struct TextureQuadParams {
   bool needs_blending = false;
   bool premultiplied_alpha = false;
   SkColor background_color = SK_ColorGREEN;
-  float vertex_opacity[4] = {0.f, 0.f, 1.f, 1.f};
+  float vertex_opacity[4] = {1.0f, 1.0f, 1.0f, 1.0f};
   bool flipped = false;
   bool nearest_neighbor = false;
   bool secure_output_only = false;
@@ -150,6 +151,14 @@ class RenderPassBuilder {
   // Sets SharedQuadState::clip_rect for the last quad.
   RenderPassBuilder& SetQuadClipRect(absl::optional<gfx::Rect> clip_rect);
 
+  // Sets SharedQuadState::blend_mode for the last quad.
+  RenderPassBuilder& SetBlendMode(SkBlendMode blend_mode);
+
+  // Sets SharedQuadState::mask_filter_info and
+  // SharedQuadState::is_fast_rounded_corner for the last quad.
+  RenderPassBuilder& SetMaskFilter(const gfx::MaskFilterInfo& mask_filter_info,
+                                   bool is_fast_rounded_corner);
+
  private:
   // Appends and returns a new SharedQuadState for quad.
   SharedQuadState* AppendDefaultSharedQuadState(const gfx::Rect rect,
@@ -235,6 +244,10 @@ CompositorRenderPassList CopyRenderPasses(
 // Creates a CompositorFrame that has a render pass with 20x20 output_rect and
 // empty damage_rect. This CompositorFrame is valid and can be sent over IPC.
 CompositorFrame MakeDefaultCompositorFrame();
+
+// Creates a CompositorFrame with provided render pass.
+CompositorFrame MakeCompositorFrame(
+    std::unique_ptr<CompositorRenderPass> render_pass);
 
 // Creates a CompositorFrame with provided list of render passes.
 CompositorFrame MakeCompositorFrame(CompositorRenderPassList render_pass_list);
