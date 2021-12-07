00c4ba9a3678b872a23394ac2aea1346590064fb
Gauthier Ambard <gambard@chromium.org>
2021-12-07 17:58:02 +0000

    [iOS] Remove FRE Sync OldString feature
    This CL removes the flag that decided which strings were presented in
    the SyncScreen in the FRE.
    
    Bug: None
    Change-Id: I7913fb72d3c9abd65cf2cf2c7d587d5882e631b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320259
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949034}

--- ios/chrome/browser/ui/first_run/first_run_app_interface.h
+++ ios/chrome/browser/ui/first_run/first_run_app_interface.h
@@ -28,9 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Returns whether sync has finished its first run setup.
 + (BOOL)isSyncFirstSetupComplete;
 
-// Checks if the "old strings in new FRE" feature is enabled.
-+ (BOOL)isOldSyncStringInFREEnabled;
-
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_FIRST_RUN_APP_INTERFACE_H_
--- ios/chrome/browser/ui/first_run/first_run_app_interface.mm
+++ ios/chrome/browser/ui/first_run/first_run_app_interface.mm
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/first_run/welcome_to_chrome_view_controller.h"
 #import "ios/chrome/browser/ui/main/scene_controller.h"
 #import "ios/chrome/browser/ui/main/scene_controller_testing.h"
-#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -50,10 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return SyncSetupServiceFactory::GetForBrowserState(
              chrome_test_util::GetOriginalBrowserState())
       ->IsFirstSetupComplete();
-}
-
-+ (BOOL)isOldSyncStringInFREEnabled {
-  return base::FeatureList::IsEnabled(kOldSyncStringFRE);
 }
 
 @end
