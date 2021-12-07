958b23958002f4a071856bc45ee82eec0b7b9429
Yann Dago <ydago@chromium.org>
2021-12-07 21:54:33 +0000

    Update SecureConnect API url
    Bug: 1261474
    Change-Id: I7abc33daf18a85ce47dbc674681ce351f791d675
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318362
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949184}

--- components/policy/core/browser/signin/user_cloud_signin_restriction_policy_fetcher_unittest.cc
+++ components/policy/core/browser/signin/user_cloud_signin_restriction_policy_fetcher_unittest.cc
@@ -23,9 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kSecureConnectApiGetManagedAccountsSigninRestrictionsUrl[] =
-    "https://secureconnect-pa.googleapis.com/"
+    "https://secureconnect-pa.clients6.google.com/"
     "v1:getManagedAccountsSigninRestriction";
-
 }
 
 namespace policy {
