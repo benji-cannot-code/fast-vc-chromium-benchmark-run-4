78e0c01ad4d1fee0080a90de858983d2e8a21f54
minch <minch@chromium.org>
2021-12-07 01:43:57 +0000

    dark_mode: Remove element_style
    New desk button inside the zero state desks bar is a rounded rect
    button. Set its color directly instead of migrating it to IconButton,
    since IconButton is a circular button.
    
    All the clients of element_style have been moved, either removed or
    migrated. Removing element_style in this CL.
    
    Bug: 1268959
    Change-Id: Iaaf94e04018e17f8c8a7967e92913dee98012e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315737
    Commit-Queue: Min Chen <minch@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948775}

--- ash/clipboard/views/clipboard_history_delete_button.cc
+++ ash/clipboard/views/clipboard_history_delete_button.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/clipboard/views/clipboard_history_delete_button.h"
 
 #include "ash/clipboard/views/clipboard_history_item_view.h"
-#include "ash/style/element_style.h"
 #include "ash/style/style_util.h"
 #include "base/bind.h"
 #include "ui/base/l10n/l10n_util.h"
--- ash/wm/desks/persistent_desks_bar_button.cc
+++ ash/wm/desks/persistent_desks_bar_button.cc
@@ -8,13 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/style/ash_color_provider.h"
-#include "ash/style/element_style.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/wm/desks/desk.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "ash/wm/desks/persistent_desks_bar_context_menu.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ui/compositor/layer.h"
+#include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/controls/highlight_path_generator.h"
 
@@ -92,7 +92,12 @@ gfx::Size PersistentDesksBarCircularButton::CalculatePreferredSize() const {
 
 void PersistentDesksBarCircularButton::OnThemeChanged() {
   views::ImageButton::OnThemeChanged();
-  element_style::DecorateFloatingIconButton(this, icon_);
+
+  const SkColor enabled_icon_color =
+      AshColorProvider::Get()->GetContentLayerColor(
+          AshColorProvider::ContentLayerType::kButtonIconColor);
+  SetImage(views::Button::STATE_NORMAL,
+           gfx::CreateVectorIcon(icon_, enabled_icon_color));
 }
 
 // -----------------------------------------------------------------------------
