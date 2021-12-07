6775322086649b3bf97cb7e4992920f16f652d29
David Roger <droger@chromium.org>
2021-12-07 23:50:43 +0000

    [Lacros] Define ENABLE_MIRROR and undefine ENABLE_DICE_SUPPORT
    This CL fixes the #defines across the codebase to properly reflect
    that Lacros uses Mirror and not Dice.
    
    This CL should not change the behavior and is only a refactoring.
    
    Fixed: 1198523
    Change-Id: I4f7f360064134dcd86fd77af9f72c06da32f2783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318668
    Reviewed-by: Jan Krčál <jkrcal@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949249}

--- chrome/browser/signin/signin_util_unittest.cc
+++ chrome/browser/signin/signin_util_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/feature_list.h"
+#include "build/buildflag.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/browser/signin/signin_features.h"
@@ -44,6 +46,7 @@ TEST_F(SigninUtilTest, GetForceSigninPolicy) {
   EXPECT_FALSE(signin_util::IsForceSigninEnabled());
 }
 
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
 class SigninUtilEnterpriseTest : public BrowserWithTestWindowTest {
  public:
   SigninUtilEnterpriseTest()
@@ -122,3 +125,4 @@ TEST_F(SigninUtilEnterpriseTest, ProfileSeparationEnforcedByPolicy) {
   EXPECT_TRUE(signin_util::ProfileSeparationEnforcedByPolicy(
       profile.get(), "primary_account_strict"));
 }
+#endif
