17ef4f758af2d94d69d9109871971aa9a5db57fe
Evan Stade <estade@chromium.org>
2021-12-07 15:38:00 +0000

    Remove (dead) File Handling OT code from Blink.
    Bug: 1272000
    Change-Id: I1f39ea2b3e2981b4ff74a53659b9f56aea035e79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3317606
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948991}

--- third_party/blink/renderer/modules/modules_initializer.cc
+++ third_party/blink/renderer/modules/modules_initializer.cc
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/indexed_db_names.h"
 #include "third_party/blink/renderer/modules/indexeddb/inspector_indexed_db_agent.h"
 #include "third_party/blink/renderer/modules/installation/installation_service_impl.h"
-#include "third_party/blink/renderer/modules/launch/file_handling_expiry_impl.h"
 #include "third_party/blink/renderer/modules/launch/web_launch_service_impl.h"
 #include "third_party/blink/renderer/modules/manifest/manifest_manager.h"
 #include "third_party/blink/renderer/modules/media/audio/audio_renderer_sink_cache.h"
@@ -219,8 +218,6 @@ void ModulesInitializer::InitLocalFrame(LocalFrame& frame) const {
   }
   frame.GetInterfaceRegistry()->AddAssociatedInterface(WTF::BindRepeating(
       &WebLaunchServiceImpl::BindReceiver, WrapWeakPersistent(&frame)));
-  frame.GetInterfaceRegistry()->AddAssociatedInterface(WTF::BindRepeating(
-      &FileHandlingExpiryImpl::BindReceiver, WrapWeakPersistent(&frame)));
 
   frame.GetInterfaceRegistry()->AddInterface(WTF::BindRepeating(
       &InstallationServiceImpl::BindReceiver, WrapWeakPersistent(&frame)));
