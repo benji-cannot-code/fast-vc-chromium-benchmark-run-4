05284feb46b44d6157f96c5930ddd0babd792efa
John Palmer <jopalmer@chromium.org>
2021-12-07 01:45:03 +0000

    Add flag for VK dark mode
    Bug: b:203028258
    Change-Id: I239eda9744a1372595f31cbd7d56c2ccc8fa5382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3251802
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#948777}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2786,6 +2786,9 @@ extern const char kImeAssistMultiWordLacrosSupportDescription[];
 extern const char kImeAssistPersonalInfoName[];
 extern const char kImeAssistPersonalInfoDescription[];
 
+extern const char kVirtualKeyboardDarkModeName[];
+extern const char kVirtualKeyboardDarkModeDescription[];
+
 extern const char kImeEmojiSuggestAdditionName[];
 extern const char kImeEmojiSuggestAdditionDescription[];
 
