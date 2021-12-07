20d7d9b0282e41b11a09a390818ab1104fb979ef
Jérémie Boulic <jboulic@chromium.org>
2021-12-07 23:05:49 +0000

    [File system operations] Test removal of partially copied files
    Tests for CL:3306138.
    
    Test: storage_unittests --gtest_filter=CopyOrMoveOperationDelegateTest.*
    Bug: 498087
    Change-Id: Ie86c82aab0905c446d46a965c8d931af93a67cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313770
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949227}

--- storage/browser/file_system/copy_or_move_operation_delegate.cc
+++ storage/browser/file_system/copy_or_move_operation_delegate.cc
@@ -40,6 +40,10 @@ class CopyOrMoveOperationDelegate::CopyOrMoveImpl {
   virtual void Run(CopyOrMoveOperationDelegate::StatusCallback callback) = 0;
   virtual void Cancel() = 0;
 
+  // Force any file copy to result in an error. This affects copies or
+  // cross-filesystem moves.
+  void ForceCopyErrorForTest() { force_error_for_test_ = true; }
+
  protected:
   CopyOrMoveImpl(
       FileSystemOperationRunner* operation_runner,
@@ -159,6 +163,7 @@ class CopyOrMoveOperationDelegate::CopyOrMoveImpl {
   const FileSystemURL src_url_;
   const FileSystemURL dest_url_;
   const CopyOrMoveOperationDelegate::CopyOrMoveOptionSet options_;
+  bool force_error_for_test_ = false;
 
  private:
   const FileSystemOperation::CopyOrMoveProgressCallback progress_callback_;
@@ -364,6 +369,9 @@ class SnapshotCopyOrMoveImpl
   void RunAfterPostWriteValidation(
       CopyOrMoveOperationDelegate::StatusCallback callback,
       base::File::Error error) {
+    if (force_error_for_test_) {
+      error = base::File::FILE_ERROR_FAILED;
+    }
     if (cancel_requested_) {
       DidEndCopy(std::move(callback), base::File::FILE_ERROR_ABORT);
       return;
@@ -647,6 +655,9 @@ class StreamCopyOrMoveImpl
 
     NotifyOnModifyFile(dest_url_);
     NotifyOnEndUpdate(dest_url_);
+    if (force_error_for_test_) {
+      error = base::File::FILE_ERROR_FAILED;
+    }
     if (cancel_requested_)
       error = base::File::FILE_ERROR_ABORT;
 
@@ -964,9 +975,11 @@ void CopyOrMoveOperationDelegate::ProcessFile(const FileSystemURL& src_url,
   }
 
   // Register the running task.
-
   CopyOrMoveImpl* impl_ptr = impl.get();
   running_copy_set_[impl_ptr] = std::move(impl);
+  if (src_url == error_url_for_test_) {
+    impl_ptr->ForceCopyErrorForTest();  // IN-TEST
+  }
   impl_ptr->Run(base::BindOnce(&CopyOrMoveOperationDelegate::DidCopyOrMoveFile,
                                weak_factory_.GetWeakPtr(), std::move(callback),
                                impl_ptr));
