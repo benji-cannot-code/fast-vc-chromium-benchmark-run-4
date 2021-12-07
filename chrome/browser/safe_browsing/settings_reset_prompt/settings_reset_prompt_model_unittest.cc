91c15ce61cf413326bcd990613f2fe9196f3cd22
Maks Orlovich <morlovich@chromium.org>
2021-12-07 20:20:57 +0000

    Code health: port away from deprecated ListValue::GetString
    This is a portion for /chrome/browser/safe_browsing.
    (And in some cases also adjust away from other deprecated Value APIs,
     when convenient/nearby)
    
    This CL was uploaded by git cl split.
    
    R=vakh@chromium.org
    
    Bug: 1187090
    Change-Id: Ia9eb7b10425de10457c2e310d5af29adcecf2e51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307375
    Auto-Submit: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949118}

--- chrome/browser/safe_browsing/settings_reset_prompt/settings_reset_prompt_model_unittest.cc
+++ chrome/browser/safe_browsing/settings_reset_prompt/settings_reset_prompt_model_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_service_test_base.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/profile_resetter/profile_resetter_test_base.h"
@@ -57,9 +58,9 @@ bool ListValueContainsUrl(const base::ListValue* list, const GURL& url) {
   if (!list)
     return false;
 
-  for (size_t i = 0; i < list->GetList().size(); ++i) {
-    std::string url_text;
-    if (list->GetString(i, &url_text) && url == url_text)
+  for (const base::Value& i : list->GetList()) {
+    const std::string* url_text = i.GetIfString();
+    if (url_text && url == *url_text)
       return true;
   }
   return false;
