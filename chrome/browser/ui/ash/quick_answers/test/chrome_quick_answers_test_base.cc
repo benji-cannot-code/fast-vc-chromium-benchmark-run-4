38ff384bff7b8e308f24aad9600f43d8499c5377
Yue Li <updowndota@google.com>
2021-12-07 20:05:46 +0000

    QA Lacros: Update widget parent logics for Quick Answers related views
    Replaced the fallback parent with DCHECK since Quick Answers views
    should always be tied to a context menu instance. Also updated the
    test logics.
    
    Bug: b/208563257
    Change-Id: I86ce058b806000d06fcbabaa2cceda4d403e77f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312739
    Auto-Submit: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949106}

--- chrome/browser/ui/ash/quick_answers/test/chrome_quick_answers_test_base.cc
+++ chrome/browser/ui/ash/quick_answers/test/chrome_quick_answers_test_base.cc
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "chrome/browser/ui/ash/quick_answers/quick_answers_controller_impl.h"
+#include "ui/base/models/simple_menu_model.h"
+#include "ui/views/controls/label.h"
+#include "ui/views/controls/menu/menu_runner.h"
+#include "ui/views/widget/widget.h"
 
 ChromeQuickAnswersTestBase::ChromeQuickAnswersTestBase() = default;
 
@@ -27,5 +31,23 @@ void ChromeQuickAnswersTestBase::SetUp() {
 void ChromeQuickAnswersTestBase::TearDown() {
   quick_answers_controller_.reset();
 
+  // Menu.
+  menu_parent_.reset();
+  menu_runner_.reset();
+  menu_model_.reset();
+  menu_delegate_.reset();
+
   ChromeAshTestBase::TearDown();
+}
+
+void ChromeQuickAnswersTestBase::CreateAndShowBasicMenu() {
+  menu_delegate_ = std::make_unique<views::Label>();
+  menu_model_ = std::make_unique<ui::SimpleMenuModel>(menu_delegate_.get());
+  menu_model_->AddItem(0, u"Menu item");
+  menu_runner_ = std::make_unique<views::MenuRunner>(
+      menu_model_.get(), views::MenuRunner::CONTEXT_MENU);
+  menu_parent_ = CreateTestWidget();
+  menu_runner_->RunMenuAt(menu_parent_.get(), nullptr, gfx::Rect(),
+                          views::MenuAnchorPosition::kTopLeft,
+                          ui::MENU_SOURCE_MOUSE);
 }
