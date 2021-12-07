df696735c43007fffbaad8a1730c163764935fd6
Clark DuVall <cduvall@chromium.org>
2021-12-07 20:29:44 +0000

    Defer begin main frame requests during early navigation stages
    Android has a browser side compositor which can schedule begin main
    frame updates between navigation start and the start of the navigation
    request to the network. Local traces showed this could delay the
    navigation request. This change defers main frame updates for the
    browser compositor until the request has been sent.
    
    The CompositorImpl is taken from the current Android window. The
    compositor may change, but we will just let the lock timeout in this
    case.
    
    The base::Feature name for this change is generic since I may put some
    other related optimizations behind the same feature.
    
    Bug: 1276493
    Change-Id: I7ebc90379d9908bf260dab04a6fe5e977d507ee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315699
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949123}

--- content/browser/renderer_host/navigation_request.h
+++ content/browser/renderer_host/navigation_request.h
@@ -84,6 +84,10 @@ class ResourceRequestBody;
 struct URLLoaderCompletionStatus;
 }  // namespace network
 
+namespace ui {
+class CompositorLock;
+}  // namespace ui
+
 namespace content {
 
 class CrossOriginEmbedderPolicyReporter;
@@ -1937,6 +1941,10 @@ class CONTENT_EXPORT NavigationRequest
   // The suffix of a prerender embedder. This value is valid only when
   // PrerenderTriggerType is kEmbedder. Only used for metrics.
   std::string prerender_embedder_histogram_suffix_;
+
+  // Prevents the compositor from requesting main frame updates early in
+  // navigation.
+  std::unique_ptr<ui::CompositorLock> compositor_lock_;
 
   base::WeakPtrFactory<NavigationRequest> weak_factory_{this};
 };
