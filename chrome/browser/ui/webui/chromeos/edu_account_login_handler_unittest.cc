6c2ea3bcdcec35c7f8c5bba854aca2667437dd58
Andrey Davydov <andreydav@google.com>
2021-12-07 14:55:02 +0000

    Replace RegisterDeprecatedMessageCallback()
    This cl replaces deprecated method
    WebUI::RegisterDeprecatedMessageCallback() with
    WebUI::RegisterMessageCallback() in a few files. It also changes
    base::ListValue to std::vector<base::Value> in affected tests.
    
    Bug: 1243386
    Change-Id: I29b7f6b68aa605f446f9d166267b3ee3d6cdd597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301320
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Auto-Submit: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#948976}

--- chrome/browser/ui/webui/chromeos/edu_account_login_handler_unittest.cc
+++ chrome/browser/ui/webui/chromeos/edu_account_login_handler_unittest.cc
@@ -241,11 +241,11 @@ class EduAccountLoginHandlerTest : public testing::Test {
 TEST_F(EduAccountLoginHandlerTest, HandleGetParentsSuccess) {
   SetupNetwork();
   constexpr char callback_id[] = "handle-get-parents-callback";
-  base::ListValue list_args;
-  list_args.Append(callback_id);
+  std::vector<base::Value> list_args;
+  list_args.emplace_back(callback_id);
 
   EXPECT_CALL(*handler(), FetchFamilyMembers());
-  handler()->HandleGetParents(&list_args);
+  handler()->HandleGetParents(list_args);
 
   EXPECT_CALL(*handler(), FetchParentImages(_, GetFakeProfileImageUrlMap()));
   // Simulate successful fetching of family members -> expect FetchParentImages
@@ -266,11 +266,11 @@ TEST_F(EduAccountLoginHandlerTest, HandleGetParentsSuccess) {
 TEST_F(EduAccountLoginHandlerTest, HandleGetParentsFailure) {
   SetupNetwork();
   constexpr char callback_id[] = "handle-get-parents-callback";
-  base::ListValue list_args;
-  list_args.Append(callback_id);
+  std::vector<base::Value> list_args;
+  list_args.emplace_back(callback_id);
 
   EXPECT_CALL(*handler(), FetchFamilyMembers());
-  handler()->HandleGetParents(&list_args);
+  handler()->HandleGetParents(list_args);
 
   // Simulate failed fetching of family members.
   handler()->OnFailure(FamilyInfoFetcher::ErrorCode::kNetworkError);
@@ -285,14 +285,14 @@ TEST_F(EduAccountLoginHandlerTest, HandleParentSigninSuccess) {
   handler()->AllowJavascriptForTesting();
 
   constexpr char callback_id[] = "handle-parent-signin-callback";
-  base::ListValue list_args;
-  list_args.Append(callback_id);
-  list_args.Append(GetFakeParent());
-  list_args.Append(kFakeParentCredential);
+  std::vector<base::Value> list_args;
+  list_args.emplace_back(callback_id);
+  list_args.emplace_back(GetFakeParent());
+  list_args.emplace_back(kFakeParentCredential);
 
   EXPECT_CALL(*handler(),
               FetchAccessToken(kFakeParentGaiaId, kFakeParentCredential));
-  handler()->HandleParentSignin(&list_args);
+  handler()->HandleParentSignin(list_args);
 
   EXPECT_CALL(*handler(),
               FetchReAuthProofTokenForParent(
@@ -317,14 +317,14 @@ TEST_F(EduAccountLoginHandlerTest, HandleParentSigninAccessTokenFailure) {
   handler()->AllowJavascriptForTesting();
 
   constexpr char callback_id[] = "handle-parent-signin-callback";
-  base::ListValue list_args;
-  list_args.Append(callback_id);
-  list_args.Append(GetFakeParent());
-  list_args.Append(kFakeParentCredential);
+  std::vector<base::Value> list_args;
+  list_args.emplace_back(callback_id);
+  list_args.emplace_back(GetFakeParent());
+  list_args.emplace_back(kFakeParentCredential);
 
   EXPECT_CALL(*handler(),
               FetchAccessToken(kFakeParentGaiaId, kFakeParentCredential));
-  handler()->HandleParentSignin(&list_args);
+  handler()->HandleParentSignin(list_args);
 
   handler()->CreateReAuthProofTokenForParent(
       kFakeParentGaiaId, kFakeParentCredential,
@@ -343,14 +343,14 @@ TEST_F(EduAccountLoginHandlerTest, HandleParentSigninReAuthProofTokenFailure) {
   handler()->AllowJavascriptForTesting();
 
   constexpr char callback_id[] = "handle-parent-signin-callback";
-  base::ListValue list_args;
-  list_args.Append(callback_id);
-  list_args.Append(GetFakeParent());
-  list_args.Append(kFakeParentCredential);
+  std::vector<base::Value> list_args;
+  list_args.emplace_back(callback_id);
+  list_args.emplace_back(GetFakeParent());
+  list_args.emplace_back(kFakeParentCredential);
 
   EXPECT_CALL(*handler(),
               FetchAccessToken(kFakeParentGaiaId, kFakeParentCredential));
-  handler()->HandleParentSignin(&list_args);
+  handler()->HandleParentSignin(list_args);
 
   EXPECT_CALL(*handler(),
               FetchReAuthProofTokenForParent(
@@ -408,10 +408,10 @@ TEST_F(EduAccountLoginHandlerTest, ProfileImageFetcherTest) {
 TEST_F(EduAccountLoginHandlerTest, HandleIsNetworkReadyOffline) {
   SetupNetwork(/*network_status_online=*/false);
   constexpr char callback_id[] = "is-network-ready-callback";
-  base::ListValue list_args;
-  list_args.Append(callback_id);
+  std::vector<base::Value> list_args;
+  list_args.emplace_back(callback_id);
 
-  handler()->HandleIsNetworkReady(&list_args);
+  handler()->HandleIsNetworkReady(list_args);
 
   const content::TestWebUI::CallData& data = *web_ui()->call_data().back();
   VerifyJavascriptCallbackResolved(data, callback_id);
@@ -424,10 +424,10 @@ TEST_F(EduAccountLoginHandlerTest, HandleIsNetworkReadyOffline) {
 TEST_F(EduAccountLoginHandlerTest, HandleIsNetworkReadyOnline) {
   SetupNetwork(/*network_status_online=*/true);
   constexpr char callback_id[] = "is-network-ready-callback";
-  base::ListValue list_args;
-  list_args.Append(callback_id);
+  std::vector<base::Value> list_args;
+  list_args.emplace_back(callback_id);
 
-  handler()->HandleIsNetworkReady(&list_args);
+  handler()->HandleIsNetworkReady(list_args);
 
   const content::TestWebUI::CallData& data = *web_ui()->call_data().back();
   VerifyJavascriptCallbackResolved(data, callback_id);
