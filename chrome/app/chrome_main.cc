9157d6c78bf94ae5759fb9e639b36306d3c20123
Joshua Peraza <jperaza@chromium.org>
2021-12-07 22:05:55 +0000

    headless: enable crash reporter from chrome main
    headless_shell would previously automatically produce dumps for
    official, google-branded builds. However, there are no official
    distributions of headless_shell, so we cannot ensure that it is
    installed correctly to use crashpad (upcoming) unless it is part of
    chrome's official distribution.
    
    Users of headless_shell and embedders of headless_lib can still use
    crashpad by passing --enable-crash-reporter so long as they ensure
    crashpad_handler is also installed adjacent to their executable.
    
    Bug: 1176772
    Change-Id: I82a64acb2be928fa2208ff3b038e5af5baf558d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312911
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949196}

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/profiler/main_thread_stack_sampling_profiler.h"
 #include "content/public/app/content_main.h"
 #include "content/public/common/content_switches.h"
+#include "headless/app/headless_shell_switches.h"
 #include "headless/public/headless_shell.h"
 #include "ui/gfx/switches.h"
 
@@ -121,7 +122,7 @@ int ChromeMain(int argc, const char** argv) {
   base::CommandLine::Init(params.argc, params.argv);
 #endif  // defined(OS_WIN)
   base::CommandLine::Init(0, nullptr);
-  const base::CommandLine* command_line(base::CommandLine::ForCurrentProcess());
+  base::CommandLine* command_line(base::CommandLine::ForCurrentProcess());
   ALLOW_UNUSED_LOCAL(command_line);
 
 #if defined(OS_WIN)
@@ -148,8 +149,12 @@ int ChromeMain(int argc, const char** argv) {
   } else {
 #if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MAC) || \
     defined(OS_WIN)
-    if (command_line->HasSwitch(switches::kHeadless))
+    if (command_line->HasSwitch(switches::kHeadless)) {
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+      command_line->AppendSwitch(::headless::switches::kEnableCrashReporter);
+#endif
       return headless::HeadlessShellMain(std::move(params));
+    }
 #endif  // defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MAC) ||
         // defined(OS_WIN)
   }
