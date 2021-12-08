c0601a7bd51d5de196d20fcb1d825fb64f956d77
Michele Fan <michelefan@chromium.org>
2021-12-08 19:54:50 +0000

    capture_mode_settings: Show "Google Drive" when selecting the root of DriveFS
    At present, "root" is showing when selecting the root of DriveFS.
    Show "Google Drive" instead to be more readable. Fixing the root of
    DriveFS path by appending "root" to the save-to option metirc.
    
    Bug: 1277217
    Change-Id: I1d476722f9c82a8bf30acb28051d9a740bcada3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322267
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949674}

--- ash/capture_mode/capture_mode_advanced_settings_view.cc
+++ ash/capture_mode/capture_mode_advanced_settings_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/capture_mode/capture_mode_advanced_settings_view.h"
 
 #include <memory>
+#include <string>
 
 #include "ash/capture_mode/capture_mode_bar_view.h"
 #include "ash/capture_mode/capture_mode_constants.h"
@@ -130,8 +131,13 @@ void CaptureModeAdvancedSettingsView::OnCaptureFolderMayHaveChanged() {
     return;
   }
 
-  save_to_menu_group_->AddOrUpdateExistingOption(
-      custom_path.BaseName().AsUTF16Unsafe(), kCustomFolder);
+  const std::u16string folder_name =
+      controller->IsRootDriveFsPath(custom_path)
+          ? l10n_util::GetStringUTF16(
+                IDS_ASH_SCREEN_CAPTURE_SAVE_TO_GOOGLE_DRIVE)
+          : custom_path.BaseName().AsUTF16Unsafe();
+  save_to_menu_group_->AddOrUpdateExistingOption(folder_name, kCustomFolder);
+
   controller->CheckFolderAvailability(
       custom_path,
       base::BindOnce(
