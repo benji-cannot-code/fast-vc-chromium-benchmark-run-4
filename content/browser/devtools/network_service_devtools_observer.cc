1a5b6e3b125884beea6fcc8b3f18825c67a85537
Titouan Rigoudy <titouan@chromium.org>
2021-12-08 12:02:15 +0000

    [Private Network Access] Record WebFeature on preflight warnings.
    This will let us analyze prevalence of this case in the wild. The
    feature use is recorded in the browser process through the network
    service devtools observer, as it is conveniently already notified of
    failed preflights.
    
    The alternative here is to pipe the warning back to Blink in the
    renderer process and record the feature use there. That is the goal of
    crrev.com/c/3310846. While that works, it is significantly more complex
    than this approach.
    
    Bug: chromium:1277840
    Change-Id: Id8e1bb45a31b2c617576af8aa92f660e909b82e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307033
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949479}

--- content/browser/devtools/network_service_devtools_observer.cc
+++ content/browser/devtools/network_service_devtools_observer.cc
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/devtools/protocol/audits_handler.h"
 #include "content/browser/devtools/protocol/network_handler.h"
 #include "content/browser/devtools/render_frame_devtools_agent_host.h"
+#include "content/public/common/content_client.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "services/network/public/mojom/http_raw_headers.mojom.h"
+#include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 
 namespace content {
 
@@ -196,6 +198,18 @@ void NetworkServiceDevToolsObserver::OnCorsError(
   if (!ftn)
     return;
 
+  RenderFrameHostImpl* rfhi = ftn->current_frame_host();
+  if (!rfhi)
+    return;
+
+  // TODO(https://crbug.com/1268378): Remove this once enforcement is always
+  // enabled and warnings are no more.
+  if (is_warning) {
+    GetContentClient()->browser()->LogWebFeatureForCurrentPage(
+        rfhi,
+        blink::mojom::WebFeature::kPrivateNetworkAccessIgnoredPreflightError);
+  }
+
   std::unique_ptr<protocol::Audits::AffectedRequest> affected_request =
       protocol::Audits::AffectedRequest::Create()
           .SetRequestId(devtools_request_id ? *devtools_request_id : "")
@@ -228,8 +242,7 @@ void NetworkServiceDevToolsObserver::OnCorsError(
                    .SetDetails(std::move(details))
                    .SetIssueId(cors_error_status.issue_id.ToString())
                    .Build();
-  devtools_instrumentation::ReportBrowserInitiatedIssue(
-      ftn->current_frame_host(), issue.get());
+  devtools_instrumentation::ReportBrowserInitiatedIssue(rfhi, issue.get());
 }
 
 void NetworkServiceDevToolsObserver::OnSubresourceWebBundleMetadata(
