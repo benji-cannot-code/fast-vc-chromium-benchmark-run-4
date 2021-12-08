c10b9befe540ce56bde3f58028e1ad05d2da2b6b
sisidovski <sisidovski@google.com>
2021-12-08 14:57:24 +0000

    Do not allow Shadow DOM based fenced frame to trigger background fetch
    This CL will prevent fenced frames from triggering background fetch. Now
    the MPArch based fenced frame throws an error if background fetch is
    called, but the Shadow DOM based fenced frame implementation doesn't
    throw and could send a background fetch request. This CL will change the
    background fetch requests from fenced frames to be blocked in Blink.
    
    Change-Id: I61edcc8994629f4fc4a6f3af6ac52591be07225d
    Bug: 1271051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310158
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@google.com>
    Cr-Commit-Position: refs/heads/main@{#949521}

--- third_party/blink/renderer/modules/background_fetch/background_fetch_manager.cc
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/fetch/request.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/deprecation.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/modules/background_fetch/background_fetch_bridge.h"
 #include "third_party/blink/renderer/modules/background_fetch/background_fetch_icon_loader.h"
 #include "third_party/blink/renderer/modules/background_fetch/background_fetch_registration.h"
@@ -165,6 +166,14 @@ ScriptPromise BackgroundFetchManager::fetch(
   if (!registration_->active()) {
     exception_state.ThrowTypeError(
         "No active registration available on the ServiceWorkerRegistration.");
+    return ScriptPromise();
+  }
+
+  LocalDOMWindow* const window = LocalDOMWindow::From(script_state);
+  if (window && window->GetFrame()->IsInFencedFrameTree()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kNotAllowedError,
+        "backgroundFetch is not allowed in a fenced frame tree.");
     return ScriptPromise();
   }
 
@@ -360,6 +369,14 @@ ScriptPromise BackgroundFetchManager::get(ScriptState* script_state,
   if (!registration_->active())
     return ScriptPromise::CastUndefined(script_state);
 
+  LocalDOMWindow* const window = LocalDOMWindow::From(script_state);
+  if (window && window->GetFrame()->IsInFencedFrameTree()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kNotAllowedError,
+        "backgroundFetch is not allowed in a fenced frame tree.");
+    return ScriptPromise();
+  }
+
   ScriptState::Scope scope(script_state);
 
   if (id.IsEmpty()) {
@@ -497,7 +514,16 @@ void BackgroundFetchManager::DidGetRegistration(
   NOTREACHED();
 }
 
-ScriptPromise BackgroundFetchManager::getIds(ScriptState* script_state) {
+ScriptPromise BackgroundFetchManager::getIds(ScriptState* script_state,
+                                             ExceptionState& exception_state) {
+  LocalDOMWindow* const window = LocalDOMWindow::From(script_state);
+  if (window && window->GetFrame()->IsInFencedFrameTree()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kNotAllowedError,
+        "backgroundFetch is not allowed in a fenced frame tree.");
+    return ScriptPromise();
+  }
+
   // Creating a Background Fetch registration requires an activated worker, so
   // if |registration_| has not been activated we can skip the Mojo roundtrip.
   if (!registration_->active()) {
