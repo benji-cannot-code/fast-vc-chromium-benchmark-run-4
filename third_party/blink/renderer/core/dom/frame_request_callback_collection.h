8bec4d24f43dff1c71d2d509e4268ee11b55cf4a
Simon Zünd <szuend@chromium.org>
2021-12-08 07:47:29 +0000

    Migrate FrameRequestCallbackCollection to use AsyncTaskContext
    Doc: https://docs.google.com/document/d/1zxAw2SgBslG-aE8itQWhCYz3nqm3yACSYuPYscQX-tU/edit?usp=sharing
    
    Bug: 1275875
    Change-Id: I92e24cf7bd053a098022a2d923b631e343aebf63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3317343
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949414}

--- third_party/blink/renderer/core/dom/frame_request_callback_collection.h
+++ third_party/blink/renderer/core/dom/frame_request_callback_collection.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_frame_request_callback.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h"
-#include "third_party/blink/renderer/core/probe/async_task_id.h"
+#include "third_party/blink/renderer/core/probe/async_task_context.h"
 #include "third_party/blink/renderer/platform/bindings/name_client.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -37,7 +37,7 @@ class CORE_EXPORT FrameCallback : public GarbageCollected<FrameCallback>,
     use_legacy_time_base_ = use_legacy_time_base;
   }
 
-  probe::AsyncTaskId* async_task_id() { return &async_task_id_; }
+  probe::AsyncTaskContext* async_task_context() { return &async_task_context_; }
 
  protected:
   FrameCallback() = default;
@@ -46,7 +46,7 @@ class CORE_EXPORT FrameCallback : public GarbageCollected<FrameCallback>,
   int id_ = 0;
   bool is_cancelled_ = false;
   bool use_legacy_time_base_ = false;
-  probe::AsyncTaskId async_task_id_;
+  probe::AsyncTaskContext async_task_context_;
 };
 
 // |V8FrameCallback| is an adapter class for the conversion from
