4ff7b146ec90a6ef1648443b465f6b355be93154
Valerie Young <spectranaut@igalia.com>
2021-12-08 19:40:07 +0000

    ax_mac_merge: merge AXHasPopup and AXPopupValue
    Change-Id: I8be6aa5388f5e1115926b3fce31571ab2b20a7e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315753
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#949665}

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -71,8 +71,6 @@ NSString* const NSAccessibilityElementBusyAttribute = @"AXElementBusy";
 NSString* const NSAccessibilityFocusableAncestorAttribute =
     @"AXFocusableAncestor";
 NSString* const NSAccessibilityGrabbedAttribute = @"AXGrabbed";
-NSString* const NSAccessibilityHasPopupAttribute = @"AXHasPopup";
-NSString* const NSAccessibilityPopupValueAttribute = @"AXPopupValue";
 NSString* const NSAccessibilityHighestEditableAncestorAttribute =
     @"AXHighestEditableAncestor";
 NSString* const NSAccessibilityIsMultiSelectableAttribute =
@@ -793,8 +791,6 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
       {NSAccessibilityFocusedAttribute, @"focused"},
       {NSAccessibilityGrabbedAttribute, @"grabbed"},
       {NSAccessibilityHeaderAttribute, @"header"},
-      {NSAccessibilityHasPopupAttribute, @"hasPopup"},
-      {NSAccessibilityPopupValueAttribute, @"popupValue"},
       {NSAccessibilityHelpAttribute, @"help"},
       {NSAccessibilityHighestEditableAncestorAttribute,
        @"highestEditableAncestor"},
@@ -1209,34 +1205,6 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
     return @YES;
 
   return @NO;
-}
-
-- (NSNumber*)hasPopup {
-  if (![self instanceActive])
-    return nil;
-  return @(_owner->HasIntAttribute(ax::mojom::IntAttribute::kHasPopup));
-}
-
-- (NSString*)popupValue {
-  if (![self instanceActive])
-    return nil;
-  int hasPopup = _owner->GetIntAttribute(ax::mojom::IntAttribute::kHasPopup);
-  switch (static_cast<ax::mojom::HasPopup>(hasPopup)) {
-    case ax::mojom::HasPopup::kFalse:
-      return @"false";
-    case ax::mojom::HasPopup::kTrue:
-      return @"true";
-    case ax::mojom::HasPopup::kMenu:
-      return @"menu";
-    case ax::mojom::HasPopup::kListbox:
-      return @"listbox";
-    case ax::mojom::HasPopup::kTree:
-      return @"tree";
-    case ax::mojom::HasPopup::kGrid:
-      return @"grid";
-    case ax::mojom::HasPopup::kDialog:
-      return @"dialog";
-  }
 }
 
 - (id)header {
@@ -3222,12 +3190,6 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
   std::string grabbed;
   if (_owner->GetHtmlAttribute("aria-grabbed", &grabbed))
     [ret addObject:NSAccessibilityGrabbedAttribute];
-
-  if (_owner->HasIntAttribute(ax::mojom::IntAttribute::kHasPopup)) {
-    [ret addObjectsFromArray:@[
-      NSAccessibilityHasPopupAttribute, NSAccessibilityPopupValueAttribute
-    ]];
-  }
 
   if (_owner->HasBoolAttribute(ax::mojom::BoolAttribute::kSelected))
     [ret addObject:NSAccessibilitySelectedAttribute];
