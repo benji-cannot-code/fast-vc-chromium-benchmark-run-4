4faab2f009dad695e00c6e6d88edb5460f51bea9
Nohemi Fernandez <fernandex@chromium.org>
2021-12-08 11:37:31 +0000

    Migrate profile references in developer_private_api to Profile::IsChild.
    Follow-up to crrev.com/c/3317419.
    
    Bug: 1275169
    Change-Id: I2fccd8f4d0e19ce9aa2095f12ff8396caf1ea1c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320268
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949473}

--- chrome/browser/extensions/api/developer_private/developer_private_api.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_api.cc
@@ -896,7 +896,7 @@ DeveloperPrivateUpdateProfileConfigurationFunction::Run() {
   Profile* profile = Profile::FromBrowserContext(browser_context());
   PrefService* prefs = profile->GetPrefs();
   if (update.in_developer_mode) {
-    if (profile->IsSupervised())
+    if (profile->IsChild())
       return RespondNow(Error(kCannotUpdateSupervisedProfileSettingsError));
     prefs->SetBoolean(prefs::kExtensionsUIDeveloperMode,
                       *update.in_developer_mode);
@@ -1109,7 +1109,7 @@ ExtensionFunction::ResponseAction DeveloperPrivateLoadUnpackedFunction::Run() {
     return RespondNow(Error(kCouldNotFindWebContentsError));
 
   Profile* profile = Profile::FromBrowserContext(browser_context());
-  if (profile->IsSupervised()) {
+  if (profile->IsChild()) {
     return RespondNow(
         Error("Supervised users cannot load unpacked extensions."));
   }
@@ -1695,8 +1695,8 @@ DeveloperPrivateChoosePathFunction::~DeveloperPrivateChoosePathFunction() {}
 
 ExtensionFunction::ResponseAction
 DeveloperPrivateIsProfileManagedFunction::Run() {
-  return RespondNow(OneArgument(base::Value(
-      Profile::FromBrowserContext(browser_context())->IsSupervised())));
+  return RespondNow(OneArgument(
+      base::Value(Profile::FromBrowserContext(browser_context())->IsChild())));
 }
 
 DeveloperPrivateIsProfileManagedFunction::
