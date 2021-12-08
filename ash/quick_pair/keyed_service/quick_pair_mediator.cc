34ed06f2227a196c6b05caef4b2d1311b17730b6
Shane Fitzpatrick <shanefitz@google.com>
2021-12-08 23:28:29 +0000

    [FastPair] Erase handshake when device is lost
    Change-Id: I2160a4faebd7d45f11d3046f3b50244c44600fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312737
    Reviewed-by: Juliet Levesque <julietlevesque@google.com>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#949794}

--- ash/quick_pair/keyed_service/quick_pair_mediator.cc
+++ ash/quick_pair/keyed_service/quick_pair_mediator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/quick_pair/common/device.h"
 #include "ash/quick_pair/common/logging.h"
+#include "ash/quick_pair/fast_pair_handshake/fast_pair_handshake_lookup.h"
 #include "ash/quick_pair/feature_status_tracker/fast_pair_pref_enabled_provider.h"
 #include "ash/quick_pair/feature_status_tracker/quick_pair_feature_status_tracker.h"
 #include "ash/quick_pair/feature_status_tracker/quick_pair_feature_status_tracker_impl.h"
@@ -133,7 +134,8 @@ void Mediator::OnDeviceFound(scoped_refptr<Device> device) {
 
 void Mediator::OnDeviceLost(scoped_refptr<Device> device) {
   QP_LOG(INFO) << __func__ << ": " << device;
-  ui_broker_->RemoveNotifications(std::move(device));
+  ui_broker_->RemoveNotifications(device);
+  FastPairHandshakeLookup::GetInstance()->Erase(device);
 }
 
 void Mediator::OnRetroactivePairFound(scoped_refptr<Device> device) {
