3dcaca10304661c4ef87b9ff4184871a771c0a1e
Melissa Zhang <melzhang@google.com>
2021-12-08 03:40:42 +0000

    [Sharesheet] Fix label cut off in arabic.
    This CL removes manual height addition and gets the views to
    dynamically work out its own sizing, which solves the problem
    of the label being slightly cut off in arabic. We've had to
    reposition the targets in the View to ensure that all the
    targets still line up horizontally now that their heights
    can be different. Hence some of the padding has changed.
    
    Screenshots uploaded to bug.
    
    Bug: 1141648
    Change-Id: I1bf7f553564ad69eb455aadf1e76fa26cd1c5d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3319629
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Auto-Submit: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949360}

--- chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view.h
+++ chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view.h
@@ -94,7 +94,6 @@ class SharesheetBubbleView : public views::BubbleDialogDelegateView,
   void ShowWidgetWithAnimateFadeIn();
   void CloseWidgetWithAnimateFadeOut(views::Widget::ClosedReason closed_reason);
   void CloseWidgetWithReason(views::Widget::ClosedReason closed_reason);
-  int GetBubbleHeight();
   void RecordFormFactorMetric();
 
   // Owns this class.
