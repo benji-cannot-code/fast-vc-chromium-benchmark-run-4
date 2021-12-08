fa068735496c208db00c4a980e81656f29d93843
Mathias Bynens <mathias@chromium.org>
2021-12-08 07:53:34 +0000

    [devtools] Migrate SQLStatement to use AsyncTaskContext
    Doc: https://goo.gle/devtools-async-tasks
    
    Bug: chromium:1275875
    Change-Id: I388fe2f1a976acb7c8d18da872549736e8a9b005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320510
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949418}

--- third_party/blink/renderer/modules/webdatabase/sql_statement.cc
+++ third_party/blink/renderer/modules/webdatabase/sql_statement.cc
@@ -90,8 +90,8 @@ SQLStatement::SQLStatement(Database* database,
   DCHECK(IsMainThread());
 
   if (HasCallback() || HasErrorCallback()) {
-    probe::AsyncTaskScheduled(database->GetExecutionContext(), "SQLStatement",
-                              &async_task_id_);
+    async_task_context_.Schedule(database->GetExecutionContext(),
+                                 "SQLStatement");
   }
 }
 
@@ -124,7 +124,7 @@ bool SQLStatement::PerformCallback(SQLTransaction* transaction) {
   SQLErrorData* error = backend_->SqlError();
 
   probe::AsyncTask async_task(transaction->GetDatabase()->GetExecutionContext(),
-                              &async_task_id_);
+                              &async_task_context_);
 
   // Call the appropriate statement callback and track if it resulted in an
   // error, because then we need to jump to the transaction error callback.
