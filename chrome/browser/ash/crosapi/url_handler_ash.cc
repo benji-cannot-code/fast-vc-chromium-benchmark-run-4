7e5a5c7605c2ea83c2016dec9872b0434d3c6171
Stefan Kuhne <skuhne@google.com>
2021-12-08 17:25:26 +0000

    Lacros URL redirect: Avoiding duplication of remote launched apps
    When Lacros is launching system apps in Ash, only allow one app per
    type to be running. If the app runs already, activate it instead.
    
    BUG=1275776
    
    Change-Id: I142395b39ef0fd1b29e8d174fdcd992e456de14d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315543
    Auto-Submit: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949588}

--- chrome/browser/ash/crosapi/url_handler_ash.cc
+++ chrome/browser/ash/crosapi/url_handler_ash.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/web_applications/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
@@ -32,6 +33,14 @@ void ShowOsAppForProfile(Profile* profile,
   // screen profile) then bail out.
   if (Browser::GetCreationStatusForProfile(profile) !=
       Browser::CreationStatus::kOk) {
+    return;
+  }
+
+  Browser* browser = web_app::FindSystemWebAppBrowser(profile, app_type,
+                                                      Browser::TYPE_APP, gurl);
+  if (browser) {
+    // If there is a matching browser we simply activate it and be done!
+    browser->window()->Activate();
     return;
   }
 
