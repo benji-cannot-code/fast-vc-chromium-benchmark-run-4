0f267785a29f51f725103d2ffc29359f0465d39e
Hirthanan Subenderan <hirthanan@google.com>
2021-12-08 18:41:31 +0000

    Unit test PSM network requests for device active reporting.
    Mock network requests made by |shared_url_loader_factory| and generate
    mock protos for the private membership requests/responses.
    
    BUG=chromium:1263158, chromium:1268095
    
    Change-Id: I93666eeb0a0ba35ba532603e0619d80771ba3241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3242784
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#949630}

--- ash/components/device_activity/device_activity_controller.cc
+++ ash/components/device_activity/device_activity_controller.cc
@@ -9,16 +9,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/device_activity/fresnel_pref_names.h"
 #include "base/check_op.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "components/prefs/pref_registry_simple.h"
+#include "google_apis/google_api_keys.h"
+#include "third_party/private_membership/src/private_membership_rlwe_client.h"
+
+namespace psm_rlwe = private_membership::rlwe;
 
 namespace ash {
 namespace device_activity {
 
 namespace {
 DeviceActivityController* g_ash_device_activity_controller = nullptr;
+
+// Production edge server for reporting device actives.
+// TODO(https://crbug.com/1267432): Enable passing base url as a runtime flag.
+const char kFresnelBaseUrl[] =
+    "https://autopush-crosfresnel-pa.sandbox.googleapis.com";
+
+class PsmDelegateImpl : public PsmDelegate {
+ public:
+  PsmDelegateImpl() = default;
+  PsmDelegateImpl(const PsmDelegateImpl&) = delete;
+  PsmDelegateImpl& operator=(const PsmDelegateImpl&) = delete;
+  ~PsmDelegateImpl() override = default;
+
+  // PsmDelegate:
+  rlwe::StatusOr<std::unique_ptr<psm_rlwe::PrivateMembershipRlweClient>>
+  CreatePsmClient(
+      psm_rlwe::RlweUseCase use_case,
+      const std::vector<psm_rlwe::RlwePlaintextId>& plaintext_ids) override {
+    return psm_rlwe::PrivateMembershipRlweClient::Create(use_case,
+                                                         plaintext_ids);
+  }
+};
+
 }  // namespace
 
 DeviceActivityController* DeviceActivityController::Get() {
@@ -68,7 +96,9 @@ void DeviceActivityController::OnPsmDeviceActiveSecretFetched(
   if (trigger == Trigger::kNetwork) {
     da_client_network_ = std::make_unique<DeviceActivityClient>(
         chromeos::NetworkHandler::Get()->network_state_handler(), local_state,
-        url_loader_factory, psm_device_active_secret);
+        url_loader_factory, std::make_unique<PsmDelegateImpl>(),
+        std::make_unique<base::RepeatingTimer>(), kFresnelBaseUrl,
+        google_apis::GetFresnelAPIKey(), psm_device_active_secret);
   }
 }
 
