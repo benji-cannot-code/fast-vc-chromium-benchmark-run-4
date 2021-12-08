97767fdef4072e347819d0cbdee9b79cdc54f992
rbpotter <rbpotter@chromium.org>
2021-12-08 04:51:39 +0000

    Print Preview: Migrate policy and preview area/generation tests to TS
    Bug: 1245834
    Change-Id: I7c9376d5f6388a3e7e41946aec341092e33e6cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318610
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949379}

--- chrome/browser/resources/print_preview/data/model.ts
+++ chrome/browser/resources/print_preview/data/model.ts
@@ -94,6 +94,12 @@ export type PolicyEntry = {
   applyOnDestinationUpdate: boolean,
 };
 
+export type PolicyObjectEntry = {
+  defaultMode?: any,
+  allowedMode?: any,
+  value?: number,
+};
+
 export type PolicySettings = {
   headerFooter?: PolicyEntry,
   cssBackground?: PolicyEntry,
@@ -1190,8 +1196,7 @@ export class PrintPreviewModelElement extends PolymerElement {
     if (policies === undefined) {
       return;
     }
-    const policiesObject = policies as
-        {[key: string]: {defaultMode?: any, allowedMode?: any, value?: number}};
+    const policiesObject = policies as {[key: string]: PolicyObjectEntry};
     ['headerFooter', 'cssBackground', 'mediaSize'].forEach(settingName => {
       if (!policiesObject[settingName]) {
         return;
--- chrome/browser/resources/print_preview/ui/preview_area.ts
+++ chrome/browser/resources/print_preview/ui/preview_area.ts
@@ -34,7 +34,7 @@ import {MARGIN_KEY_MAP, MarginObject, PrintPreviewMarginControlContainerElement}
 import {PluginProxy, PluginProxyImpl} from './plugin_proxy.js';
 import {SettingsMixin} from './settings_mixin.js';
 
-type PreviewTicket = Ticket&{
+export type PreviewTicket = Ticket&{
   headerFooterEnabled: boolean;
   pageRange: Array<{to: number, from: number}>;
   pagesPerSheet: number;
@@ -146,6 +146,10 @@ export class PrintPreviewPreviewAreaElement extends
 
   private computePreviewLoaded_(): boolean {
     return this.documentReady_ && this.pluginLoadComplete_;
+  }
+
+  getLastTicketForTest(): PreviewTicket|null {
+    return this.lastTicket_;
   }
 
   previewLoaded(): boolean {
@@ -781,6 +785,12 @@ export class PrintPreviewPreviewAreaElement extends
       default:
         return '';
     }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'print-preview-preview-area': PrintPreviewPreviewAreaElement;
   }
 }
 
