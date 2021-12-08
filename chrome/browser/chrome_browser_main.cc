d582a2ce8b465a2b8134fd57ec3092fae9e05433
Shuran Huang <shuuran@chromium.org>
2021-12-08 17:10:47 +0000

    Reland "Read/write the serialized FirstPartySets from/to disk."
    This is a reland with modification of
    ea0afd04cc914edd77c3e28e34d8dfb5433d6b20
    (Patchset 1 is the original CL)
    
    This CL makes sure the reply callback sent to NetworkService is
    destroyed after the mojo pipe disconnects even if it's never run,
    and proceeds with reading/writing the serialized FirstPartySets
    from/to disk only if the Component Updater is enabled so that the
    First-Party Sets component will be installed.
    
    Original change's description:
    > Read/write the serialized FirstPartySets from/to disk.
    >
    > Add FirstPartySetsUtil class to handle read/write a serialized
    > First-Party Sets map as a string from disk. Have browser process own
    > an instance of it and read the string from the file on-disk during
    > startup, send the string to the NetworkService; NetworkService will
    > send back another string which the browser process will write to disk.
    > The process is implemented behind the flag
    > net::features::kFirstPartySets.
    >
    > Bug: 1219656
    > Change-Id: I637e0c123d5b31dc0f72581adf70e44be10188e5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3258066
    > Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Shuran Huang <shuuran@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#944496}
    
    Bug: 1219656
    Change-Id: I80d9722922b8786e41ca3b9f2f505be1e2433154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309537
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949580}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/component_updater/registration.h"
 #include "chrome/browser/defaults.h"
+#include "chrome/browser/first_party_sets/first_party_sets_util.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/language/url_language_histogram_factory.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
@@ -159,6 +160,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
@@ -181,6 +183,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/buildflags/buildflags.h"
 #include "printing/buildflags/buildflags.h"
 #include "rlz/buildflags/buildflags.h"
+#include "services/network/public/mojom/network_service.mojom.h"
 #include "services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/layout.h"
@@ -1646,6 +1649,21 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
     speech::SodaInstaller::GetInstance()->Init(profile_->GetPrefs(),
                                                browser_process_->local_state());
 #endif  // !defined(OS_ANDROID) && !BUILDFLAG(IS_CHROMEOS_ASH)
+
+    // Only read and update the persisted sets when First-Party Sets component
+    // will be installed.
+    if (base::FeatureList::IsEnabled(net::features::kFirstPartySets)) {
+      FirstPartySetsUtil::GetInstance()->SendAndUpdatePersistedSets(
+          user_data_dir_,
+          /*send_sets=*/
+          base::BindOnce(
+              [](base::OnceCallback<void(const std::string&)> callback,
+                 const std::string& sets) {
+                content::GetNetworkService()
+                    ->SetPersistedFirstPartySetsAndGetCurrentSets(
+                        sets, std::move(callback));
+              }));
+    }
   }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
