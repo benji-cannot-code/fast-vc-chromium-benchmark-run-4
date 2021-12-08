f874af6a688c6c4572a52a85bfc6caad0c83fb1f
Pavol Marko <pmarko@google.com>
2021-12-08 21:58:45 +0000

    Reland "reven: Don't block enrollment/device policy init on state keys"
    This is a reland of 8ad2b7f6bce1383a9bc2e4f71988921797c87ad9
    
    Original change's description:
    > reven: Don't block enrollment/device policy init on state keys
    >
    > Server-backed state keys are not available on reven, but reven also
    > doesn't support FRE/AutoRE, so don't block enterprise enrollment or
    > device policy initialization on the availability of server-backed state
    > keys.
    >
    > Bug: b:206775318
    > Change-Id: Ia3bc7e2ae7fed61bc0bc55070ddb1938cf44681f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310924
    > Commit-Queue: Pavol Marko <pmarko@chromium.org>
    > Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    > Reviewed-by: Roland Bock <rbock@google.com>
    > Reviewed-by: Igor <igorcov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#948910}
    
    Bug: b:206775318
    Change-Id: I700874468cfccffbd4bf506c28f73ff565eb0758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320328
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949737}

--- chrome/browser/ash/policy/enrollment/enrollment_handler.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/components/attestation/attestation_flow.h"
+#include "ash/constants/ash_switches.h"
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -313,6 +314,15 @@ void EnrollmentHandler::StartEnrollment() {
     return;
   }
 
+  // Currently reven devices don't support sever-backed state keys, but they
+  // also don't support FRE/AutoRE so don't block enrollment on the
+  // availablility of state keys.
+  // TODO(b/208705225): Remove this special case when reven supports state keys.
+  if (ash::switches::IsRevenBranding()) {
+    LOG(WARNING) << "Skipping state keys.";
+    HandleStateKeysResult({});
+    return;
+  }
   LOG(WARNING) << "Requesting state keys.";
   state_keys_broker_->RequestStateKeys(
       base::BindOnce(&EnrollmentHandler::HandleStateKeysResult,
@@ -456,7 +466,7 @@ void EnrollmentHandler::HandleStateKeysResult(
 
   // TODO(crbug.com/1271134): Logging as "WARNING" to make sure it's preserved
   // in the logs.
-  LOG(WARNING) << "State keys generated.";
+  LOG(WARNING) << "State keys generated, success=" << !state_keys.empty();
   SetStep(STEP_LOADING_STORE);
   StartRegistration();
 }
