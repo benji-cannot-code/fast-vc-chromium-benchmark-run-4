3aeb7a0c96a986e05bafd0d8480b5616716bcf7c
Viktor Semeniuk <vsemeniuk@google.com>
2021-12-08 11:33:18 +0000

    [Affiliation Matching] OnLoginsRetained implementation
    This change adds OnLoginsRetained implementation for
    AffiliatedMatchHelper. It prefetches newly added facets and removes
    missing facets.
    
    Bug: 1267912
    Change-Id: Idad3540fcb44390fbf3702dffa77cafdfeac8325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3288697
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#949472}

--- components/password_manager/core/browser/android_affiliation/affiliated_match_helper.cc
+++ components/password_manager/core/browser/android_affiliation/affiliated_match_helper.cc
@@ -16,6 +16,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager {
 
+namespace {
+
+bool IsFacetValidForAffiliation(const FacetURI& facet) {
+  return facet.IsValidAndroidFacetURI() ||
+         (facet.IsValidWebFacetURI() &&
+          base::FeatureList::IsEnabled(
+              password_manager::features::kFillingAcrossAffiliatedWebsites));
+}
+
+}  // namespace
+
 // static
 constexpr base::TimeDelta AffiliatedMatchHelper::kInitializationDelayOnStartup;
 
@@ -140,7 +151,16 @@ void AffiliatedMatchHelper::OnLoginsChanged(
 
 void AffiliatedMatchHelper::OnLoginsRetained(
     PasswordStoreInterface* /*store*/,
-    const std::vector<PasswordForm>& retained_passwords) {}
+    const std::vector<PasswordForm>& retained_passwords) {
+  std::vector<FacetURI> facets;
+  for (const auto& form : retained_passwords) {
+    FacetURI facet_uri =
+        FacetURI::FromPotentiallyInvalidSpec(form.signon_realm);
+    if (IsFacetValidForAffiliation(facet_uri))
+      facets.push_back(std::move(facet_uri));
+  }
+  affiliation_service_->KeepPrefetchForFacets(std::move(facets));
+}
 
 void AffiliatedMatchHelper::OnGetPasswordStoreResults(
     std::vector<std::unique_ptr<PasswordForm>> results) {
@@ -148,12 +168,7 @@ void AffiliatedMatchHelper::OnGetPasswordStoreResults(
   for (const auto& form : results) {
     FacetURI facet_uri =
         FacetURI::FromPotentiallyInvalidSpec(form->signon_realm);
-    if (facet_uri.IsValidAndroidFacetURI())
-      affiliation_service_->Prefetch(facet_uri, base::Time::Max());
-
-    if (facet_uri.IsValidWebFacetURI() &&
-        base::FeatureList::IsEnabled(
-            password_manager::features::kFillingAcrossAffiliatedWebsites))
+    if (IsFacetValidForAffiliation(facet_uri))
       affiliation_service_->Prefetch(facet_uri, base::Time::Max());
 
     facets.push_back(std::move(facet_uri));
