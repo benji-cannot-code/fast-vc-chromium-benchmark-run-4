cc7449f667eefa932c146cddfa5b4f919c10c996
Robert Lin <robertlin@chromium.org>
2021-12-08 22:58:30 +0000

    prerender: Cancel an old prerendering page when the predictor predicts another URL.
    This CL makes AutocompleteActionPredictor to cancel an old prerendering
    page when the predictor predicts another URL.
    
    Bug: 1166085
    Change-Id: Ie7d1ef52c05e454b3132b6ce594ed70dbf11bd47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306736
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949773}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -9215,7 +9215,8 @@ std::unique_ptr<PrerenderHandle> WebContentsImpl::StartPrerendering(
 
   if (frame_tree_node_id != FrameTreeNode::kFrameTreeNodeInvalidId) {
     return std::make_unique<PrerenderHandleImpl>(
-        GetPrerenderHostRegistry()->GetWeakPtr(), frame_tree_node_id);
+        GetPrerenderHostRegistry()->GetWeakPtr(), frame_tree_node_id,
+        prerendering_url);
   }
   return nullptr;
 }
