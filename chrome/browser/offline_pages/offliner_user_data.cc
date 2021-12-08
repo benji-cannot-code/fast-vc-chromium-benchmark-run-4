396e6e4e75e258e6266845064538000847533728
Dave Tapuska <dtapuska@chromium.org>
2021-12-08 15:09:21 +0000

    Fixup weird construction of OfflinerUserData.
    Use CreateForWebContents template function instead of the custom
    AddToWebContents one itself. The WebContents is always owned by the
    BackgroundLoaderOffliner so the check that it has already been
    created is fine to execute.
    
    BUG=1268914
    
    Change-Id: Ie6d908f78b5cc993cd03050209f69621e77bae63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321852
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949523}

--- chrome/browser/offline_pages/offliner_user_data.cc
+++ chrome/browser/offline_pages/offliner_user_data.cc
@@ -9,12 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace offline_pages {
 
-void OfflinerUserData::AddToWebContents(content::WebContents* webcontents,
-                                        BackgroundLoaderOffliner* offliner) {
-  DCHECK(offliner);
-  webcontents->SetUserData(UserDataKey(),
-                           std::make_unique<OfflinerUserData>(offliner));
-}
+OfflinerUserData::OfflinerUserData(content::WebContents* web_contents,
+                                   BackgroundLoaderOffliner* offliner)
+    : content::WebContentsUserData<OfflinerUserData>(*web_contents),
+      offliner_(offliner) {}
 
 // static - gets the data pointer as a BackgroundLoaderOffliner
 BackgroundLoaderOffliner* OfflinerUserData::OfflinerFromWebContents(
