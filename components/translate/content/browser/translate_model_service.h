8f6f18860ea69a732d6660bf5e6880078bc134f9
mcrouse <mcrouse@google.com>
2021-12-08 16:53:41 +0000

    [LangID] Use WeakPtr factory for model loading.
    This uses a weakptr rather than unretained for loading the model file.
    
    
    Bug: 1277487
    Change-Id: I286f290e26cdc8a70b3f38de3434b7547fa1ce19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321651
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949565}

--- components/translate/content/browser/translate_model_service.h
+++ components/translate/content/browser/translate_model_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/core/optimization_target_model_observer.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -82,6 +83,8 @@ class TranslateModelService
   std::vector<NotifyModelAvailableCallback> pending_model_requests_;
 
   scoped_refptr<base::SequencedTaskRunner> background_task_runner_;
+
+  base::WeakPtrFactory<TranslateModelService> weak_ptr_factory_{this};
 };
 
 }  //  namespace translate
