d1a7a307bdfbde7a46484f75358c9e8432856989
Ehimare Okoyomon <eokoyomon@chromium.org>
2021-12-08 14:15:59 +0000

    Privacy Review: Add custom completion link row
    Create custom link row to address UI polish issues for completion card.
    
    Screenshot: crbug.com/1215630#c80
    Bug: 1215630
    Change-Id: Ia577903ad1d50be38393032b16f8fcd29b9b9964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306628
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949512}

--- chrome/browser/resources/settings/privacy_page/privacy_review/privacy_review_completion_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_review/privacy_review_completion_fragment.ts
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import './privacy_review_completion_link_row.js';
 import './privacy_review_fragment_shared_css.js';
 
 import {WebUIListenerMixin} from 'chrome://resources/js/web_ui_listener_mixin.js';
