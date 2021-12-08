d3ffe0932787e60461e879dd900e19b1250d5ea9
Tom Anderson <thomasanderson@chromium.org>
2021-12-08 20:39:32 +0000

    Improve icon contrast in the tab group editor bubble
    Some custom themes (like the GTK Ambiance theme) use dark text on light
    windows for most UI, but reverse it to light text on dark backgrounds
    for menus.  This is problematic for the tab group editor which doesn't
    use the menu background color, but instead uses the default popup
    background color.
    
    This CL borrows the icon color from the label text color which ensures
    sufficient contrast against the background.
    
    Change-Id: Iacd8523286af10012e519a2935b2421e620863a9
    Fixed: 1250189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318613
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949701}

--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/pointer/touch_ui_controller.h"
 #include "ui/events/types/event_type.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/paint_vector_icon.h"
@@ -318,12 +319,14 @@ gfx::Rect TabGroupEditorBubbleView::GetAnchorRect() const {
 }
 
 void TabGroupEditorBubbleView::AddedToWidget() {
-  if (!move_menu_item_->GetEnabled()) {
-    const SkColor disabled_color = move_menu_item_->GetCurrentTextColor();
-    move_menu_item_->SetImageModel(
-        views::Button::STATE_DISABLED,
-        ui::ImageModel::FromVectorIcon(kMoveGroupToNewWindowIcon,
-                                       disabled_color));
+  for (auto menu_item : menu_items_) {
+    const bool enabled = menu_item->GetEnabled();
+    const SkColor text_color = menu_item->GetCurrentTextColor();
+    const SkColor icon_color =
+        enabled ? color_utils::DeriveDefaultIconColor(text_color) : text_color;
+    menu_item->SetImageModel(
+        enabled ? views::Button::STATE_NORMAL : views::Button::STATE_DISABLED,
+        ui::ImageModel::FromVectorIcon(kMoveGroupToNewWindowIcon, icon_color));
   }
 }
 
@@ -416,22 +419,23 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
       base::BindRepeating(&TabGroupEditorBubbleView::NewTabInGroupPressed,
                           base::Unretained(this)),
       &kNewTabInGroupIcon));
+  menu_items_.push_back(new_tab_menu_item);
 
-  AddChildView(CreateMenuItem(
+  menu_items_.push_back(AddChildView(CreateMenuItem(
       TAB_GROUP_HEADER_CXMENU_UNGROUP,
       l10n_util::GetStringUTF16(IDS_TAB_GROUP_HEADER_CXMENU_UNGROUP),
       base::BindRepeating(&TabGroupEditorBubbleView::UngroupPressed,
                           base::Unretained(this), header_view),
-      &kUngroupIcon));
+      &kUngroupIcon)));
 
-  AddChildView(CreateMenuItem(
+  menu_items_.push_back(AddChildView(CreateMenuItem(
       TAB_GROUP_HEADER_CXMENU_CLOSE_GROUP,
       l10n_util::GetStringUTF16(IDS_TAB_GROUP_HEADER_CXMENU_CLOSE_GROUP),
       base::BindRepeating(&TabGroupEditorBubbleView::CloseGroupPressed,
                           base::Unretained(this)),
-      &kCloseGroupIcon));
+      &kCloseGroupIcon)));
 
-  move_menu_item_ = AddChildView(
+  auto* move_menu_item = AddChildView(
       CreateMenuItem(TAB_GROUP_HEADER_CXMENU_MOVE_GROUP_TO_NEW_WINDOW,
                      l10n_util::GetStringUTF16(
                          IDS_TAB_GROUP_HEADER_CXMENU_MOVE_GROUP_TO_NEW_WINDOW),
@@ -439,9 +443,10 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
                          &TabGroupEditorBubbleView::MoveGroupToNewWindowPressed,
                          base::Unretained(this)),
                      &kMoveGroupToNewWindowIcon));
-  move_menu_item_->SetEnabled(
+  move_menu_item->SetEnabled(
       tab_strip_model->count() !=
       tab_strip_model->group_model()->GetTabGroup(group_)->tab_count());
+  menu_items_.push_back(move_menu_item);
 
   // Setting up the layout.
 
