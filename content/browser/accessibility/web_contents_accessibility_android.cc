3fb642b9ce584caaa7d2a7e4c07fafb07e8dfc28
Alexander Surkov <asurkov@igalia.com>
2021-12-08 18:25:38 +0000

    ax_cleanup: unify AXPlatformNodeDelegate GetBoundsRect derivative utils
    It devirtualizes AXPlatformNodeDelegate::GetClippedScreenBoundsRect
    implementation and makes code consistent.
    
    Change-Id: I7f28fe0a50ccb144feaea782c556014540de8d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3311297
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#949622}

--- content/browser/accessibility/web_contents_accessibility_android.cc
+++ content/browser/accessibility/web_contents_accessibility_android.cc
@@ -1034,7 +1034,7 @@ void WebContentsAccessibilityAndroid::ScrollToMakeNodeVisible(
   BrowserAccessibilityAndroid* node = GetAXFromUniqueID(unique_id);
   if (node) {
     node->manager()->ScrollToMakeVisible(
-        *node, gfx::Rect(node->GetClippedFrameBoundsRect().size()));
+        *node, gfx::Rect(node->GetUnclippedFrameBoundsRect().size()));
   }
 }
 
