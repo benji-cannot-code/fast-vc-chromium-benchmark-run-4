4731cfea81742541e232cadf66aec52e15ea7a0c
Tanmoy Mollik <triploblastic@chromium.org>
2021-12-08 14:12:24 +0000

    Revert "Disable testSigninFirstRunPages_WithCctPolicy*."
    This reverts commit e81db810956265792d9b71bf3ddd2937a6600a17.
    
    Reason for revert: The tests should be fixed because of this fix cl -
    https://crrev.com/c/3310853
    
    Original change's description:
    > Disable testSigninFirstRunPages_WithCctPolicy*.
    >
    > These are failing on android 11 bots.
    >
    > Bug: 1276506
    > Change-Id: I40ef0b148269af7de3040c1e2794253130333ca5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315340
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#948193}
    
    Bug: 1276506
    Change-Id: Id02c6d8d4c49e7c53e48341036fd7f778d93ba4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321587
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949510}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -16,7 +16,6 @@ import android.app.Instrumentation.ActivityMonitor;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.test.InstrumentationRegistry;
 import android.view.View;
@@ -51,7 +50,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.ScalableTimeout;
@@ -596,8 +594,6 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
-    @DisableIf.
-    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_AbsenceOfPromos() throws Exception {
@@ -606,8 +602,6 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
-    @DisableIf.
-    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_SearchPromo() throws Exception {
@@ -616,8 +610,6 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
-    @DisableIf.
-    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_SearchPromo_SigninPromo() throws Exception {
@@ -629,8 +621,6 @@ public class FirstRunIntegrationTest {
 
     @Test
     @MediumTest
-    @DisableIf.
-    Build(message = "https://crbug.com/1276506", sdk_is_greater_than = Build.VERSION_CODES.P)
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_DISABLE_SIGNIN_FRE})
     @CommandLineFlags.Add({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
     public void testSigninFirstRunPages_WithCctPolicy_SigninPromo() throws Exception {
