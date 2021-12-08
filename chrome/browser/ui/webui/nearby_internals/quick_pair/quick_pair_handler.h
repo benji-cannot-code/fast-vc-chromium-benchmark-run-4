430c037fb81dbd678e08bc6157be2647176058af
Juliet Levesque <julietlevesque@google.com>
2021-12-08 17:39:50 +0000

    [Fast Pair] Update UI Trigger tab for notifications.
    Updates the chrome://nearby-internals to show updated Fast
    Pair UI notifications as follows:
    1. New button layout: https://screenshot.googleplex.com/44GosdXqt7ubgCN
    2. Discovery notification:
       https://screenshot.googleplex.com/AafKjXQc7s4tsio
    3. Error notification: https://screenshot.googleplex.com/6y4dMxLxXsa4Xri
    4. Associate Account notification:
       https://screenshot.googleplex.com/5JDERw88KUWq3Jg
    5. Pairing notification:
       https://screenshot.googleplex.com/7V2gkXRckNPrHTw
    
    Change-Id: I580a949dc568b313946e94f7ca2d8792470d4743
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3309317
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#949596}

--- chrome/browser/ui/webui/nearby_internals/quick_pair/quick_pair_handler.h
+++ chrome/browser/ui/webui/nearby_internals/quick_pair/quick_pair_handler.h
@@ -9,14 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include "ash/quick_pair/common/log_buffer.h"
 #include "ash/quick_pair/common/logging.h"
+#include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/values.h"
 #include "content/public/browser/web_ui_message_handler.h"
+#include "ui/gfx/image/image.h"
 
 namespace ash {
 namespace quick_pair {
 class FastPairNotificationController;
+class FastPairImageDecoder;
 }  // namespace quick_pair
 }  // namespace ash
 
@@ -51,9 +54,16 @@ class QuickPairHandler : public content::WebUIMessageHandler,
   void NotifyFastPairError(const base::ListValue* args);
   void NotifyFastPairDiscovery(const base::ListValue* args);
   void NotifyFastPairPairing(const base::ListValue* args);
+  void NotifyFastPairAssociateAccountKey(const base::ListValue* args);
+
+  void OnImageDecodedFastPairError(gfx::Image image);
+  void OnImageDecodedFastPairDiscovery(gfx::Image image);
+  void OnImageDecodedFastPairPairing(gfx::Image image);
+  void OnImageDecodedFastPairAssociateAccountKey(gfx::Image image);
 
   std::unique_ptr<ash::quick_pair::FastPairNotificationController>
       fast_pair_notification_controller_;
+  std::unique_ptr<ash::quick_pair::FastPairImageDecoder> image_decoder_;
 
   base::ScopedObservation<ash::quick_pair::LogBuffer,
                           ash::quick_pair::LogBuffer::Observer>
