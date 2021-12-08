2db8962da0ebf26b376bd71a477e6a30880cfe40
Sergio Vila Prado <sergiovila@google.com>
2021-12-08 15:56:40 +0000

    [Autofill Assistant] Add CUP support for GetActions calls.
    Bug: b/203031699
    Change-Id: I4566c25b9d122392733a6bc34d3fc3c8092cb23c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3222581
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sergio Vila Prado <sergiovila@google.com>
    Cr-Commit-Position: refs/heads/main@{#949541}

---
+++ components/autofill_assistant/browser/service/rpc_type.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_SERVICE_RPC_TYPE_H_
+#define COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_SERVICE_RPC_TYPE_H_
+
+namespace autofill_assistant {
+
+enum class RpcType {
+  UNKNOWN,
+  GET_ACTIONS,
+  GET_TRIGGER_SCRIPTS,
+  SUPPORTS_SCRIPT,
+};
+
+}
+
+#endif  // COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_SERVICE_RPC_TYPE_H_
