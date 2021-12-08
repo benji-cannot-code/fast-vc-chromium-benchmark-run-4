63540e319be7c96d2f804dac892fda3c010294e3
dpapad <dpapad@chromium.org>
2021-12-08 21:01:43 +0000

    TabSearch: Migrate tests to TypeScript, part 2.
    Migrating all non-UI test files.
    
    Bug: 1260300
    Change-Id: Id196e036be38ad3949fb58ee75efab27afc690d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321303
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949709}

--- chrome/browser/resources/tab_search/app.ts
+++ chrome/browser/resources/tab_search/app.ts
@@ -130,7 +130,7 @@ export class TabSearchAppElement extends PolymerElement {
   private searchText_: string;
   private availableHeight_: number;
   filteredItems_: Array<TitleItem|TabData|TabGroupData>;
-  private fuzzySearchOptions_: FuzzySearchOptions;
+  private fuzzySearchOptions_: FuzzySearchOptions<TabData|TabGroupData>;
   private moveActiveTabToBottom_: boolean;
   private recentlyClosedDefaultItemDisplayCount_: number;
   private searchResultText_: string;
