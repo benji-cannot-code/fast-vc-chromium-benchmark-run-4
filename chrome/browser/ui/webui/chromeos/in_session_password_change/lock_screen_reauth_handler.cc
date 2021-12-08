4257f4236f864ea8ff4052f510f1dbdd92142871
Mohammed Abdon <mohammedabdon@google.com>
2021-12-08 21:56:47 +0000

    Fix wrong domain redirection on lockscreen reauth flow.
    This CL fixes a bug that happen on lockscreen online re-authentication
    flow when the user uses an account which doesn’t belong to same enrolled domain.
    The fix would use domain we get from gaia which is stored in the user's
    account info.
    
    Bug: 1237322
    Change-Id: I466bda3b8e7028f1b7e03177e08c38329b7e8028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3310902
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Mohammed Abdon <mohammedabdon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949735}

--- chrome/browser/ui/webui/chromeos/in_session_password_change/lock_screen_reauth_handler.cc
+++ chrome/browser/ui/webui/chromeos/in_session_password_change/lock_screen_reauth_handler.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/installer/util/google_update_settings.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/login/auth/challenge_response/cert_utils.h"
 #include "chromeos/login/auth/cryptohome_key_constants.h"
 #include "components/account_id/account_id.h"
+#include "components/signin/public/identity_manager/account_info.h"
 #include "components/user_manager/known_user.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/storage_partition.h"
@@ -63,15 +65,31 @@ bool ShouldDoSamlRedirect(const std::string& email) {
   return user && user->using_saml();
 }
 
-InSessionPasswordSyncManager* GetInSessionPasswordSyncManager() {
+Profile* GetActiveUserProfile() {
   const user_manager::User* user =
       user_manager::UserManager::Get()->GetActiveUser();
   Profile* profile = chromeos::ProfileHelper::Get()->GetProfileByUser(user);
+  return profile;
+}
 
+std::string GetHostedDomain(const std::string& gaia_id) {
+  Profile* profile = GetActiveUserProfile();
+  auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
+  if (!identity_manager) {
+    return std::string();
+  }
+  const AccountInfo account_info =
+      identity_manager->FindExtendedAccountInfoByGaiaId(gaia_id);
+  return account_info.hosted_domain;
+}
+
+InSessionPasswordSyncManager* GetInSessionPasswordSyncManager() {
+  Profile* profile = GetActiveUserProfile();
   return InSessionPasswordSyncManagerFactory::GetForProfile(profile);
 }
 
 const char kMainElement[] = "$(\'main-element\').";
+const char kIdpTestingDomain[] = "example.com";
 
 }  // namespace
 
@@ -178,16 +196,20 @@ void LockScreenReauthHandler::OnSetCookieForLoadGaiaWithPartition(
   params.SetBoolean("dontResizeNonEmbeddedPages", false);
   params.SetBoolean("enableGaiaActionButtons", false);
 
-  std::string enterprise_enrollment_domain(
-      g_browser_process->platform_part()
-          ->browser_policy_connector_ash()
-          ->GetEnterpriseEnrollmentDomain());
+  std::string hosted_domain = GetHostedDomain(context.gaia_id);
 
-  if (enterprise_enrollment_domain.empty()) {
-    enterprise_enrollment_domain = gaia::ExtractDomainName(context.email);
+  if (hosted_domain.empty()) {
+    LOG(ERROR) << "Couldn't get hosted_domain from account info.";
+    params.SetBoolean("doSamlRedirect", force_saml_redirect_for_testing_);
+  } else {
+    params.SetString(
+        "enterpriseEnrollmentDomain",
+        force_saml_redirect_for_testing_ ? kIdpTestingDomain : hosted_domain);
+    params.SetBoolean("doSamlRedirect",
+                      force_saml_redirect_for_testing_
+                          ? true
+                          : ShouldDoSamlRedirect(context.email));
   }
-
-  params.SetString("enterpriseEnrollmentDomain", enterprise_enrollment_domain);
 
   const std::string app_locale = g_browser_process->GetApplicationLocale();
   DCHECK(!app_locale.empty());
@@ -196,8 +218,6 @@ void LockScreenReauthHandler::OnSetCookieForLoadGaiaWithPartition(
   params.SetString("gaiaId", context.gaia_id);
   params.SetBoolean("extractSamlPasswordAttributes",
                     login::ExtractSamlPasswordAttributesEnabled());
-  params.SetBoolean("doSamlRedirect", force_saml_redirect_for_testing_ ?
-                    true : ShouldDoSamlRedirect(context.email));
   params.SetString("clientVersion", version_info::GetVersionNumber());
   params.SetBoolean("readOnlyEmail", true);
 
@@ -217,9 +237,8 @@ void LockScreenReauthHandler::UpdateOrientationAndWidth() {
   CallJavascript("setWidth", base::Value(width));
 }
 
-void LockScreenReauthHandler::CallJavascript(
-    const std::string& function,
-    const base::Value& params) {
+void LockScreenReauthHandler::CallJavascript(const std::string& function,
+                                             const base::Value& params) {
   CallJavascriptFunction(std::string(kMainElement) + function, params);
 }
 
