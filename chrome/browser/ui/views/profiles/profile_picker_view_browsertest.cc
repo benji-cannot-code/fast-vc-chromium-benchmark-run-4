f8522896fabe3dc63869b2744634ae30729c1e0e
Thomas Guilbert <tguilbert@chromium.org>
2021-12-08 19:03:55 +0000

    Revert "Record new metric for first render time on profile picker startup"
    This reverts commit 51e414a23651a301392db9fed77a6db3e7c566f2.
    
    Reason for revert: Failing/flaking on bots, e.g.:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests/108942/test-results?q=ExactID%3Aninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FProfilePickerCreationFlowBrowserTest.OpenProfileFromStartup+VHash%3A9db5781eb6173fa8
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac11-arm64-rel-tests/2282/test-results?q=ExactID%3Aninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FProfilePickerCreationFlowBrowserTest.OpenProfileFromStartup+VHash%3Af02fa9cbcfa036d0
    
    Original change's description:
    > Record new metric for first render time on profile picker startup
    >
    > This will allow covering a blind spot in startup performance monitoring
    > introduced by the rollout of showing the profile picker on startup.
    > (which happens when users have 2+ profiles and the option to show the
    > picker enabled)
    >
    > ProfilePicker.FirstProfileTime.FirstWebContentsNonEmptyPaint and
    > ProfilePicker.FirstProfileTime.FirstWebContentsFinishReason
    > are the 2 new metrics added for this.
    >
    > To record them, we are following a very similar pattern to the one used
    > for the affected metric, Startup.FirstWebContents.NonEmptyPaint3.This
    > CL refactors the profiler used to record it to cover both use cases.
    >
    >
    > Bug: 1273909
    > Change-Id: Id25f3e94cf77b5562067304425ebe45c255f0a97
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295409
    > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#949499}
    
    Bug: 1273909
    Change-Id: Ia7f3fb043a34fb14a398d6e75eddc08bbb233ca5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3323749
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Owners-Override: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949645}

--- chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view_browsertest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
-#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
@@ -19,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/interstitials/chrome_settings_page_helper.h"
-#include "chrome/browser/metrics/first_web_contents_profiler_base.h"
 #include "chrome/browser/policy/cloud/user_policy_signin_service.h"
 #include "chrome/browser/policy/cloud/user_policy_signin_service_factory.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
@@ -67,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/signin/public/identity_manager/primary_account_mutator.h"
-#include "components/startup_metric_utils/browser/startup_metric_utils.h"
 #include "components/sync/base/sync_prefs.h"
 #include "components/sync/driver/sync_driver_switches.h"
 #include "components/sync/driver/sync_service.h"
@@ -988,8 +985,6 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest, OpenProfile) {
-  base::HistogramTester histogram_tester;
-
   AvatarToolbarButton::SetIPHMinDelayAfterCreationForTesting(base::Seconds(0));
   FeaturePromoControllerViews::BlockActiveWindowCheckForTesting();
   ASSERT_EQ(1u, BrowserList::GetInstance()->size());
@@ -1008,43 +1003,6 @@ IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest, OpenProfile) {
   WaitForPickerClosed();
   // IPH is shown.
   EXPECT_TRUE(ProfileSwitchPromoHasBeenShown(new_browser));
-
-  // FirstProfileTime.* histograms aren't recorded because the picker
-  // is opened from the menu.
-  EXPECT_THAT(histogram_tester.GetTotalCountsForPrefix(
-                  "ProfilePicker.FirstProfileTime."),
-              testing::IsEmpty());
-}
-
-IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
-                       OpenProfileFromStartup) {
-  base::HistogramTester histogram_tester;
-
-  // Create a second profile.
-  base::FilePath other_path = CreateNewProfileWithoutBrowser();
-
-  // Open the picker.
-  ProfilePicker::Show(ProfilePicker::EntryPoint::kOnStartup);
-  EXPECT_TRUE(ProfilePicker::IsOpen());
-  WaitForLoadStop(GURL("chrome://profile-picker"));
-
-  // Open the new profile.
-  OpenProfileFromPicker(other_path, /*open_settings=*/false);
-
-  // Measurement of startup performance started.
-
-  // Browser for the profile is displayed.
-  Browser* new_browser = BrowserAddedWaiter(2u).Wait();
-  WaitForLoadStop(GURL("chrome://newtab/"),
-                  new_browser->tab_strip_model()->GetActiveWebContents());
-  EXPECT_EQ(new_browser->profile()->GetPath(), other_path);
-  WaitForPickerClosed();
-
-  histogram_tester.ExpectTotalCount(
-      "ProfilePicker.FirstProfileTime.FirstWebContentsNonEmptyPaint", 1);
-  histogram_tester.ExpectUniqueSample(
-      "ProfilePicker.FirstProfileTime.FirstWebContentsFinishReason",
-      metrics::StartupProfilingFinishReason::kDone, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(ProfilePickerCreationFlowBrowserTest,
