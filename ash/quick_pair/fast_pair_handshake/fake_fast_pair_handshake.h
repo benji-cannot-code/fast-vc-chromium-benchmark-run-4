a0cdefc3ad87e29234dc89109a7a5504d669508c
Shane Fitzpatrick <shanefitz@google.com>
2021-12-08 04:00:22 +0000

    [FastPair] FastPairHandshake class
    This changes add the FastPairHandshake class, a lookup class to manage
    instances of it and related test classes.
    
    The FastPairHandshake class performs the handshake logic that was
    previously in FastPairPairer. Moving the logic into this class enables
    sharing it between the Scanning and Pairing components.
    
    This CL is part of a chain of CLs to refactor this area to enable the
    handshake to be performed prior to showing the UX.
    
    Change-Id: I3295e5fad89f58c063e6e9d3dbe8a125a7b8e49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312015
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Auto-Submit: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#949366}

---
+++ ash/quick_pair/fast_pair_handshake/fake_fast_pair_handshake.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_QUICK_PAIR_FAST_PAIR_HANDSHAKE_FAKE_FAST_PAIR_HANDSHAKE_H_
+#define ASH_QUICK_PAIR_FAST_PAIR_HANDSHAKE_FAKE_FAST_PAIR_HANDSHAKE_H_
+
+#include <memory>
+
+#include "ash/quick_pair/fast_pair_handshake/fast_pair_handshake.h"
+
+#include "base/memory/scoped_refptr.h"
+
+namespace ash {
+namespace quick_pair {
+
+class FakeFastPairHandshake : public FastPairHandshake {
+ public:
+  FakeFastPairHandshake(
+      scoped_refptr<device::BluetoothAdapter> adapter,
+      scoped_refptr<Device> device,
+      OnCompleteCallback on_complete,
+      std::unique_ptr<FastPairDataEncryptor> data_encryptor = nullptr,
+      std::unique_ptr<FastPairGattServiceClient> gatt_service_client = nullptr);
+  FakeFastPairHandshake(const FakeFastPairHandshake&) = delete;
+  FakeFastPairHandshake& operator=(const FakeFastPairHandshake&) = delete;
+  ~FakeFastPairHandshake();
+
+  void InvokeCallback(absl::optional<PairFailure> failure = absl::nullopt);
+};
+
+}  // namespace quick_pair
+}  // namespace ash
+
+#endif  // ASH_QUICK_PAIR_FAST_PAIR_HANDSHAKE_FAKE_FAST_PAIR_HANDSHAKE_H_
