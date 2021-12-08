69eae8c5fce0946d4c19672957735f183cfca310
Vadym Kargin <vadym.kargin@accenture.com>
2021-12-08 15:09:32 +0000

    Bindings: Convert pseudo IDL namespaces into real IDL namespaces in the WEBGPU module.
    This update is part of work on converting pseudo IDL namespaces into real IDL namespaces.
    
    Issue: Convert pseudo IDL namespaces into real IDL namespaces
    Bug: 1214607
    Change-Id: I2481628806fbcca85f9342547ccd3e2ba49378f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315899
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Vadym Kargin <vadym.kargin@accenture.com>
    Cr-Commit-Position: refs/heads/main@{#949524}

--- third_party/blink/renderer/modules/webgpu/gpu_map_mode.h
+++ third_party/blink/renderer/modules/webgpu/gpu_map_mode.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_MAP_MODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_MAP_MODE_H_
 
+#include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_map_mode.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
 namespace blink {
@@ -15,8 +16,8 @@ class GPUMapMode : public ScriptWrappable {
 
  public:
   // gpu_map_mode.idl
-  static constexpr uint32_t kRead = 1;
-  static constexpr uint32_t kWrite = 2;
+  static constexpr uint32_t kRead = V8GPUMapMode::Constant::kRead;
+  static constexpr uint32_t kWrite = V8GPUMapMode::Constant::kWrite;
 
   GPUMapMode(const GPUMapMode&) = delete;
   GPUMapMode& operator=(const GPUMapMode&) = delete;
