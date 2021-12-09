0a95d4633dc0b48dcb5c72a828dc7730a8fb84bd
Peter McNeeley <petermcneeley@chromium.org>
2021-12-09 22:43:53 +0000

    Improve temporal tracking of overlay candidates
    Note: Candidates originating from the same surface with the same rect
    will have the same tracking id and thus the same
    |OverlayCandidateTemporalTracker|. The tracking  of all data for these
    candidates will be equivalent to the data of the first.
    
    This is a Reland as the original had two critical issues:
    The first issue was that the hash function was reading outside bounds.
    This was caused by a lack of testing after a review change.
    
    The second (more important) issue was that CFI triggered as a record
    for UMA would read an invalid iterator. This read did not generate a
    segfault but the underlying object was no longer a valid type and thus
    CFI triggered a SIGILL / ILL_ILLOPN.
    https://clang.llvm.org/docs/ControlFlowIntegrity.html
    gn Args:
    is_cfi = true
    use_thin_lto = true
    
    Original description:
    The motivation for this change is to ensure there is no regression
    for OverlayPrioritization on Android and improved content protection
    support on ChromeOS.
    
    Traditionally overlays are uniquely identified using |display_rect| of
    the overlay candidate. This identification is not stable for scrolling,
    pinch zoom, and window movement. This change now tracks overlays via
    surfaceID and (relative) quad |rect|.
    
    Damage tracking has also been simplified by moving to an exponential
    smoothing algorithm after N samples. Overlay promotion delays should
    now only be limited to at most one frame. The |damage_rate_threshold|
    still serves as the single control variable for the damage to energy
    heuristic.
    
    Bug: 1113306
    Change-Id: I32403c1abc3893339534e9b87c1ad1e22994020e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3182865
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950310}

--- components/viz/service/display/overlay_processor_using_strategy.h
+++ components/viz/service/display/overlay_processor_using_strategy.h
@@ -10,10 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <vector>
 
+#include "base/hash/hash.h"
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
 #include "components/viz/common/display/overlay_strategy.h"
 #include "components/viz/common/quads/aggregated_render_pass.h"
+#include "components/viz/service/display/display_resource_provider.h"
 #include "components/viz/service/display/output_surface.h"
 #include "components/viz/service/display/overlay_candidate.h"
 #include "components/viz/service/display/overlay_candidate_temporal_tracker.h"
@@ -269,11 +271,12 @@ class VIZ_SERVICE_EXPORT OverlayProcessorUsingStrategy
   void UpdateDownscalingCapabilities(float scale_factor, bool success);
 
   struct ProposedCandidateKey {
-    gfx::Rect rect;
+    OverlayCandidate::TrackingId tracking_id;
     OverlayStrategy strategy_id = OverlayStrategy::kUnknown;
 
     bool operator==(const ProposedCandidateKey& other) const {
-      return (rect == other.rect && strategy_id == other.strategy_id);
+      return (tracking_id == other.tracking_id &&
+              strategy_id == other.strategy_id);
     }
   };
 
@@ -289,7 +292,7 @@ class VIZ_SERVICE_EXPORT OverlayProcessorUsingStrategy
   std::unordered_map<ProposedCandidateKey,
                      OverlayCandidateTemporalTracker,
                      ProposedCandidateKeyHasher>
-      tracked_candidates;
+      tracked_candidates_;
 
   // These variables are used only for UMA purposes.
   void OnOverlaySwitchUMA(ProposedCandidateKey overlay_tracking_key);
