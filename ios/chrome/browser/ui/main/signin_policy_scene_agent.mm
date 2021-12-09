7270ec83a469838eb8816a01dd2184918a0212c8
Vincent Boisselle <vincb@google.com>
2021-12-09 15:52:54 +0000

    [iOS][ForcedSignIn]🏢 Fix double forced sign-in screen problem
    If the policy applied when there are multiple windows and the regular
    sign-in screen is shown, cancelling the sign-in screen might trigger
    multiple forced sign-in screens (one for each screen).
    
    Screencast of the problem: http://shortn/_w2rOtHEqGO
    
    Fix: http://shortn/_yHMqYypvzB
    
    This issue is more an aesthetic issue than a critical issue. Worst case,
    each window will show the forced sign-in prompt when the problem occurs,
    this will be a bit overwhelming but at least sign-in is still enforced.
    Also, the issue only happens when the following conditions are
    aligned: (1) there more than one window opened, (2) the regular
    sign-in prompt is shown in one window with UI blockers on the
    other windows, (3) the policy is applied while the prompt is shown,
    (4) the policy is enabled, and (5) sign-in is cancelled in the prompt.
    
    The likelihood of these conditions lining up is low, and the resulting
    problem doesn't look critical. This isn't a release blocker in my
    opinion.
    
    The problem happens because -sceneStateDidHideModalOverlay is handled
    before -signinDidEnd when the regular sign-in prompt is dismissed.
    The first scene to handle -sceneStateDidHideModalOverlay will show the
    forced sign-in screen. Usually this would block the other scenes from
    showing other replicas of the forced sign-in screen, but the UI blocker
    is deleted for each connected scene when the sign-in prompt is dismissed
    , which allows the forced sign-in screen to be shown in each window
    (removing the condition that usually blocks replicas of the sign-in
    screen).
    
    FIX: -sceneStateDidHideModalOverlay cannot be removed. The only option
    is to not show the sign-in prompt when there is ui blocker object in
    the app state. This fix also makes sure that there can be only one
    unique ui blocker target.
    
    Bug: 1277797
    Change-Id: Ibc5f5f199ff809e0034a7bb6b6bc96043b1d70ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321093
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#950087}

--- ios/chrome/browser/ui/main/signin_policy_scene_agent.mm
+++ ios/chrome/browser/ui/main/signin_policy_scene_agent.mm
@@ -258,9 +258,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return NO;
   }
 
-  if (self.sceneState.presentingModalOverlay) {
-    // Return NO when the scene cannot present views because it is blocked by
-    // the modal overlay.
+  if (self.sceneState.appState.currentUIBlocker) {
+    // Return NO when the scene cannot present views because it is blocked.
     return NO;
   }
 
