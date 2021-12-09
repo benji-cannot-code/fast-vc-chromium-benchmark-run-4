1e239db2b9ee31bd20a3edeef8e0cc60beac2fec
Adrian Taylor <adetaylor@chromium.org>
2021-12-09 19:31:05 +0000

    [rust] Add serde_jsonrc test executable.
    This ensures that serde_jsonrc is working fine
    independently of our integration into Chromium. We may well
    want to remove this later once we have Chromium integration.
    
    This CL uses a C++ unittest rather than a Rust unittest
    because some platforms are currently unable to build Rust
    unittests (e.g. Android) and we want to be sure the code
    builds OK there too.
    
    Bug: 1069271
    Change-Id: I1931893ddf0e3e144ba4ae68a24df4229047a5dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3265546
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950207}

---
+++ build/rust/tests/test_serde_jsonrc/unittests.cc
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "build/rust/tests/test_serde_jsonrc/lib.rs.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(RustTest, SerdeJsonTest) {
+  EXPECT_EQ(true, serde_works());
+}
