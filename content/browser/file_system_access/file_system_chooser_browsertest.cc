fc162cdafcefa3db56c5439a1cd4282d9bb2fe99
Austin Sullivan <asully@chromium.org>
2021-12-09 23:38:22 +0000

    FSA: Strip % chars from suggestedName
    not be allowed in a suggestedName.
    
    net: :GenerateFileName() does not strip these characters, but they should
    Change-Id: I90e413bac1cd66c7ac3b2bff8f8f3a2f71a243b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3324687
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950335}

--- content/browser/file_system_access/file_system_chooser_browsertest.cc
+++ content/browser/file_system_access/file_system_chooser_browsertest.cc
@@ -1574,9 +1574,9 @@ IN_PROC_BROWSER_TEST_F(FileSystemChooserBrowserTest, SuggestedName) {
                         "dangerous_extension.lnk", false});
 #endif
   // Invalid characters should be sanitized.
-  name_infos.push_back({R"(inv*l:d\\charבאמת!a<ters🤓.txt)",
+  name_infos.push_back({R"(inv*l:d\\ch%rבאמת!a<ters🤓.txt)",
                         ListValueOf(".txt"), true,
-                        R"(inv_l_d__charבאמת!a_ters🤓.txt)", true});
+                        R"(inv_l_d__ch_rבאמת!a_ters🤓.txt)", true});
 
   for (const auto& name_info : name_infos) {
     SCOPED_TRACE(name_info.suggested_name);
