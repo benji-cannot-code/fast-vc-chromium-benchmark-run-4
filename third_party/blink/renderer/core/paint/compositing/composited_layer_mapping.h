0bbd1cc4c60d3059dd88e6ba4e3468c863b37111
Philip Rogers <pdr@chromium.org>
2021-12-09 06:32:15 +0000

    Remove some pre-CompositeAfterPaint code from PaintLayer
    Bug: 1277748
    Change-Id: I5aa4dde88b15259504494e1ab79930d24dc4d004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3324943
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949952}

--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h
@@ -111,9 +111,6 @@ class CORE_EXPORT CompositedLayerMapping final
       const PaintLayer* compositing_container,
       HeapVector<Member<PaintLayer>>& layers_needing_paint_invalidation);
 
-  // Update whether layer needs blending.
-  void UpdateContentsOpaque();
-
   GraphicsLayer* MainGraphicsLayer() const { return graphics_layer_; }
 
   GraphicsLayer* ForegroundLayer() const { return foreground_layer_; }
@@ -328,7 +325,6 @@ class CORE_EXPORT CompositedLayerMapping final
   bool UpdateSquashingLayers(bool needs_squashing_layers);
   void UpdateDrawsContentAndPaintsHitTest();
   void UpdateCompositedBounds();
-  void UpdateGraphicsLayerContentsOpaque(bool should_check_children);
 
   // Also sets subpixelAccumulation on the layer.
   void ComputeBoundsOfOwningLayer(
