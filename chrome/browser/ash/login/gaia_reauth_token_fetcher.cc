b41120c1a4ae4e2c6040295a1688566c730c7c42
Yunke Zhou <yunkez@google.com>
2021-12-09 07:03:59 +0000

    Add timeout for GAIA reauth token fetcher
    Add metrics "Login.ReauthToken.FetchDuration.{Result}" to record fetch
    duration.
    
    Bug: b:197615068
    Change-Id: Ibd4fca7c0eb50affda4ef476f50130b8aa148741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322516
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/main@{#949960}

--- chrome/browser/ash/login/gaia_reauth_token_fetcher.cc
+++ chrome/browser/ash/login/gaia_reauth_token_fetcher.cc
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/constants/ash_switches.h"
+#include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
 #include "base/json/json_reader.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "google_apis/google_api_keys.h"
@@ -30,6 +33,8 @@ namespace {
 const char kGetReauthTokenUrl[] =
     "https://staging-chromeoslogin-pa.sandbox.googleapis.com/v1/rart";
 const char kApiKeyParameter[] = "key";
+
+constexpr base::TimeDelta kWaitTimeout = base::Seconds(5);
 
 GURL GetFetchReauthTokenUrl() {
   GURL url =
@@ -94,11 +99,13 @@ void GaiaReauthTokenFetcher::Fetch() {
   simple_url_loader_ = network::SimpleURLLoader::Create(
       std::move(resource_request), traffic_annotation);
   simple_url_loader_->SetAllowHttpErrorResults(true);
+  simple_url_loader_->SetTimeoutDuration(kWaitTimeout);
   simple_url_loader_->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
       g_browser_process->system_network_context_manager()
           ->GetURLLoaderFactory(),
       base::BindOnce(&GaiaReauthTokenFetcher::OnSimpleLoaderComplete,
                      weak_ptr_factory_.GetWeakPtr()));
+  fetch_timer_ = std::make_unique<base::ElapsedTimer>();
 }
 
 void GaiaReauthTokenFetcher::OnSimpleLoaderComplete(
@@ -118,6 +125,8 @@ void GaiaReauthTokenFetcher::OnSimpleLoaderComplete(
           message_value->FindStringKey("encodedReauthRequestToken");
       if (token != nullptr) {
         VLOG(1) << "Successfully fetched reauth request token.";
+        base::UmaHistogramTimes("Login.ReauthToken.FetchDuration.Success",
+                                fetch_timer_->Elapsed());
         std::move(callback_).Run(*token);
         return;
       } else {
@@ -130,6 +139,8 @@ void GaiaReauthTokenFetcher::OnSimpleLoaderComplete(
   } else {
     LOG(WARNING) << "Failed to fetch reauth request token.";
   }
+  base::UmaHistogramTimes("Login.ReauthToken.FetchDuration.Failure",
+                          fetch_timer_->Elapsed());
   std::move(callback_).Run(/*token=*/std::string());
 }
 
--- chrome/browser/ash/login/gaia_reauth_token_fetcher.h
+++ chrome/browser/ash/login/gaia_reauth_token_fetcher.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "base/timer/elapsed_timer.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 
 namespace ash {
@@ -34,13 +35,16 @@ class GaiaReauthTokenFetcher {
   void Fetch();
 
  private:
-  // Handles responses from the SimpleURLLoader
+  // Handles responses from the SimpleURLLoader.
   void OnSimpleLoaderComplete(std::unique_ptr<std::string> response_body);
 
   // Called at the end of Fetch().
   FetchCompleteCallback callback_;
 
   std::unique_ptr<network::SimpleURLLoader> simple_url_loader_;
+
+  // Used for metrics:
+  std::unique_ptr<base::ElapsedTimer> fetch_timer_;
 
   base::WeakPtrFactory<GaiaReauthTokenFetcher> weak_ptr_factory_{this};
 };
