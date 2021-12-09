45ef9b2e193b11ec0df6093561b79b901baba63f
Illia Klimov <elklm@google.com>
2021-12-09 15:26:23 +0000

    Fix a11y announcement for permission chip on Mac.
    Bug: 1278323
    Change-Id: I40af82cbb6f0cbc2c0c4ec66aa0e4db0025a504c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327225
    Reviewed-by: Olesia Marukhno <olesiamarukhno@google.com>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950076}

--- chrome/browser/ui/views/location_bar/permission_chip.cc
+++ chrome/browser/ui/views/location_bar/permission_chip.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/views/permission_bubble/permission_prompt_style.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/permissions/features.h"
@@ -124,6 +125,9 @@ void PermissionChip::AddedToWidget() {
   if (!should_start_open_) {
     GetViewAccessibility().AnnounceText(l10n_util::GetStringUTF16(
         IDS_PERMISSIONS_REQUESTED_SCREENREADER_ANNOUNCEMENT));
+#if defined(OS_MAC)
+    NotifyAccessibilityEvent(ax::mojom::Event::kAlert, true);
+#endif
   }
 }
 
