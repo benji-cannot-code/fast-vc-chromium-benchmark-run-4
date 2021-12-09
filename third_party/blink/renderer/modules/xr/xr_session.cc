75bccff8ebae47a75f73fa7fd9d75e517cc69f2f
Simon Zünd <szuend@chromium.org>
2021-12-09 11:23:30 +0000

    Migrate XRFrameRequestCallbackCollection to use AsyncTaskContext
    Doc: https://goo.gle/devtools-async-tasks
    
    Bug: 1275875
    Change-Id: I33f70795aebbf05be5bed8fc86ed75c37b43ffc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320247
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950025}

--- third_party/blink/renderer/modules/xr/xr_session.cc
+++ third_party/blink/renderer/modules/xr/xr_session.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
+#include "third_party/blink/renderer/core/probe/async_task_context.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer.h"
 #include "third_party/blink/renderer/core/resize_observer/resize_observer_entry.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
