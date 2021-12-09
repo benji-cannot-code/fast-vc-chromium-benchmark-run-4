9881ac6a1f262611f7d3e43dd9635e40cd5d1a21
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-09 04:35:40 +0000

    CCA: tsconfig: Enable noImplicitReturns
    Fix all places that is causing compile errors with noImplicitReturns,
    and enable the option.
    
    Bug: b:172340451
    Test: tsc compile pass
    Change-Id: I94f01dda5ac1bf57268969adbcb55df3bab7ceae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3281683
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949924}

--- ash/webui/camera_app_ui/resources/js/models/file_namer.js
+++ ash/webui/camera_app_ui/resources/js/models/file_namer.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {assertNotReached} from '../chrome_util.js';
 import {
   MimeType,
   VideoType,  // eslint-disable-line no-unused-vars
@@ -120,6 +121,7 @@ export class Filenamer {
         case MimeType.PDF:
           return 'pdf';
       }
+      assertNotReached(`Unknown type ${type}`);
     })();
     return DOCUMENT_PREFIX + timestampToDatetimeName(this.timestamp_) + '.' +
         ext;
