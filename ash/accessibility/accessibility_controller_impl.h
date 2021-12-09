d27d92af00267ac9849f97783afbd962d666f8c3
Akihiro Ota <akihiroota@chromium.org>
2021-12-09 20:27:16 +0000

    Dictation: Add view for new UI
    This change adds the controller and view necessary to implement
    the Dictation bubble UI (see the bug for mocks). This change only
    adds the new UI to the code base. Follow-up changes will add
    functionality that allows the Dictation extension to control this new
    UI.
    
    AX-Relnotes: N/A
    Bug: 1252037
    
    Change-Id: I56d7e366476b1613a05f3753d1802171f47e1117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3253676
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950243}

--- ash/accessibility/accessibility_controller_impl.h
+++ ash/accessibility/accessibility_controller_impl.h
@@ -44,6 +44,7 @@ class AccessibilityConfirmationDialog;
 class AccessibilityEventRewriter;
 class AccessibilityHighlightController;
 class AccessibilityObserver;
+class DictationBubbleController;
 class DictationNudgeController;
 class FloatingAccessibilityController;
 class PointScanController;
@@ -480,6 +481,10 @@ class ASH_EXPORT AccessibilityControllerImpl : public AccessibilityController,
     return dictation_soda_download_progress_;
   }
 
+  DictationBubbleController* GetDictationBubbleControllerForTest() {
+    return dictation_bubble_controller_.get();
+  }
+
  private:
   // Populate |features_| with the feature of the correct type.
   void CreateAccessibilityFeatures();
@@ -580,6 +585,9 @@ class ASH_EXPORT AccessibilityControllerImpl : public AccessibilityController,
   // with ShowDictationLanguageUpgradedNudge() and reset at Shutdown() or when
   // the Dictation feature is disabled.
   std::unique_ptr<DictationNudgeController> dictation_nudge_controller_;
+
+  // Used to control the Dictation bubble UI.
+  std::unique_ptr<DictationBubbleController> dictation_bubble_controller_;
 
   // True if ChromeVox should enable its volume slide gesture.
   bool enable_chromevox_volume_slide_gesture_ = false;
--- ash/wm/collision_detection/collision_detection_utils.h
+++ ash/wm/collision_detection/collision_detection_utils.h
@@ -45,7 +45,8 @@ class ASH_EXPORT CollisionDetectionUtils {
     kSwitchAccessMenu = 1,
     kAutomaticClicksMenu = 2,
     kAutomaticClicksScrollMenu = 3,
-    kDefault = 4,
+    kDictationBubble = 4,
+    kDefault = 5,
   };
 
   CollisionDetectionUtils() = delete;
