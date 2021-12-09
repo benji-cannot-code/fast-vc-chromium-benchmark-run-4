f963b825b5dcfd41b5a20e3437381f1e87539a9e
Xing Liu <xingliu@chromium.org>
2021-12-09 18:21:20 +0000

    Download: Improve range header validation in background download.
    Validates the range header when background download client starts a
    download. The client will get an error when range header is not valid.
    
    Bug: 1255769
    Change-Id: Ia90d6aab61364619106155ce419707b53d492e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3324864
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950172}

--- components/download/content/internal/download_driver_impl.cc
+++ components/download/content/internal/download_driver_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "components/download/internal/background_service/driver_entry.h"
+#include "components/download/network/download_http_utils.h"
 #include "components/download/public/common/download_features.h"
 #include "components/download/public/common/download_interrupt_reasons.h"
 #include "components/download/public/common/download_url_parameters.h"
@@ -24,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_byte_range.h"
 #include "net/http/http_response_headers.h"
 #include "net/http/http_status_code.h"
-#include "net/http/http_util.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace download {
@@ -73,24 +73,6 @@ FailureType FailureTypeFromInterruptReason(DownloadInterruptReason reason) {
 // Logs interrupt reason when download fails.
 void LogDownloadInterruptReason(download::DownloadInterruptReason reason) {
   base::UmaHistogramSparse("Download.Service.Driver.InterruptReason", reason);
-}
-
-absl::optional<net::HttpByteRange> ParseRangeHeader(
-    const net::HttpRequestHeaders& request_headers) {
-  std::vector<net::HttpByteRange> byte_ranges;
-  std::string range_header;
-  bool success =
-      request_headers.GetHeader(net::HttpRequestHeaders::kRange, &range_header);
-  if (!success)
-    return absl::nullopt;
-
-  success = net::HttpUtil::ParseRangeHeader(range_header, &byte_ranges);
-
-  // Multiple ranges are not supported.
-  if (!success || byte_ranges.empty() || byte_ranges.size() > 1)
-    return absl::nullopt;
-
-  return byte_ranges.front();
 }
 
 }  // namespace
@@ -218,9 +200,9 @@ void DownloadDriverImpl::Start(
             byte_range->last_byte_position());
       }
     } else {
-      // TODO(xingliu): Do input validation in ControllerImpl::StartDownload, so
-      // we don't need to return here.
+      // The request headers are validated in ControllerImpl::StartDownload.
       LOG(ERROR) << "Failed to parse Range request header.";
+      NOTREACHED();
       return;
     }
   }
