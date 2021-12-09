68c35f498ae27942e84227e88c27f5d323d10ede
Richard Chui <richui@chromium.org>
2021-12-09 02:36:37 +0000

    Desk Templates: Set Activation Window when hiding templates grid
    When hiding the grid, there were certain cases where the overview
    windows were not able to be restored by the FocusController due to the
    focus being set on the incorrect window/widget.
    
    This CL follows the previous convention of activating the overview focus
    window, which is what is expected when entering overview mode in the
    beginning.
    
    Fixed: 1275176
    Test: manual, added
    Change-Id: I3be8d4049a3abc230b5313fdb7e0a81deac34425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314280
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#949892}

--- ash/wm/desks/templates/desks_templates_unittest.cc
+++ ash/wm/desks/templates/desks_templates_unittest.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_test_base.h"
 #include "ash/wm/overview/overview_test_util.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "ash/wm/window_util.h"
 #include "base/callback_helpers.h"
 #include "base/guid.h"
 #include "base/strings/string_number_conversions.h"
@@ -1453,6 +1454,35 @@ TEST_F(DesksTemplatesTest, DesksTemplatesButtonBorderColor) {
   // active.
   SendKey(ui::VKEY_TAB, ui::EF_SHIFT_DOWN);
   EXPECT_EQ(active_color, get_border_color());
+}
+
+// Tests that if we save a template (and get dropped into the templates grid),
+// delete all the templates (and the templates grid gets hidden), the windows in
+// overview get activated and restored when selected.
+TEST_F(DesksTemplatesTest, WindowActivatableAfterSaveAndDeleteTemplate) {
+  // Create a test window.
+  auto test_window = CreateAppWindow();
+
+  // Open overview and save a template.
+  OpenOverviewAndSaveTemplate(Shell::Get()->GetPrimaryRootWindow());
+  std::vector<DeskTemplate*> entries = GetAllEntries();
+  ASSERT_EQ(1ul, entries.size());
+
+  // Delete the one and only template, which should hide the templates grid but
+  // remain in overview.
+  DeleteTemplate(entries[0]->uuid(), /*expected_current_item_count=*/1);
+  ASSERT_TRUE(InOverviewSession());
+
+  // Click on the `test_window` to activate it.
+  auto* event_generator = GetEventGenerator();
+  event_generator->MoveMouseToCenterOf(test_window.get());
+  event_generator->ClickLeftButton();
+
+  // Verify that we exit the overview session.
+  EXPECT_FALSE(InOverviewSession());
+
+  // Check that the window is active.
+  EXPECT_EQ(test_window.get(), window_util::GetActiveWindow());
 }
 
 }  // namespace ash
