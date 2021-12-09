8a5557159f559bd3fa5b12d6953e36017284b546
Amanda Baker <ambake@microsoft.com>
2021-12-09 02:04:48 +0000

    Rename windowControlsOverlay.getBoundingClientRect->getTitlebarAreaRect
    This change renames the existing JS API for Window Controls Overlay
    (WCO) from:
    navigator.windowControlsOverlay.getBoundingClientRect()
    to
    navigator.windowControlsOverlay.getTitlebarAreaRect()
    
    The new name addresses feedback about how the original API name was
    misleading since windowControlsOverlay.getBoundingClientRect() actually
    returns the available area *next to* the overlay.
    
    The new name--getTitlebarAreaRect--matches the CSS environment
    variables: `titlebar-area-x/y/width/height`
    
    Bug: 1276216
    Change-Id: I95e2bc680d124aaa69f0c12ea0ba41399b77185c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3319288
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Amanda Baker <ambake@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#949881}

--- third_party/blink/renderer/core/frame/window_controls_overlay.cc
+++ third_party/blink/renderer/core/frame/window_controls_overlay.cc
@@ -61,7 +61,7 @@ bool WindowControlsOverlay::visible() const {
       ->IsWindowControlsOverlayVisible();
 }
 
-DOMRect* WindowControlsOverlay::getBoundingClientRect() const {
+DOMRect* WindowControlsOverlay::getTitlebarAreaRect() const {
   if (!GetSupplementable()->DomWindow())
     return DOMRect::Create(0, 0, 0, 0);
 
