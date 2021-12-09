39c751b35c827fafb70822cca2e690024daeaa05
Harald Alvestrand <hta@chromium.org>
2021-12-09 13:03:21 +0000

    Disable SDES on all platforms except Fuchsia
    Bug: chromium:804275
    Change-Id: I27579b51b45c36fab0bf26c1b39c4483d3bd5c54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3326301
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950043}

--- third_party/blink/renderer/modules/mediastream/media_constraints_impl.cc
+++ third_party/blink/renderer/modules/mediastream/media_constraints_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/mediastream/media_constraints_impl.h"
 
+#include "build/os_buildflags.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/bindings/core/v8/array_value.h"
 #include "third_party/blink/renderer/bindings/core/v8/dictionary.h"
@@ -377,7 +378,11 @@ static void ParseOldStyleNames(
         Deprecation::CountDeprecation(
             context, WebFeature::kRTCConstraintEnableDtlsSrtpFalse);
       }
+#if BUILDFLAG(IS_FUCHSIA)
+      // Special dispensation for Fuchsia to run SDES in 2002
+      // TODO(crbug.com/804275): Delete when Fuchsia no longer depends on it.
       result.enable_dtls_srtp.SetExact(ToBoolean(constraint.value_));
+#endif
     } else if (constraint.name_.Equals(kEnableRtpDataChannels)) {
       // This constraint does not turn on RTP data channels, but we do not
       // want it to cause an error, so we parse it and ignore it.
