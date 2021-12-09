5e7a9c3098aeaa85db7766662b49d90a52b5fcfd
Maksim Moskvitin <mmoskvitin@google.com>
2021-12-09 17:46:15 +0000

    [TrustedVault] Avoid building desktop library on Android
    components/sync/trusted_vault contains desktop implementation of
    TrustedVaultClient and not needed on Android.
    
    This CL removes components/sync/trusted_vault from
    components/sync:sync group and adds direct dependencies to relevant
    targets.
    
    Bug: 1245981
    Change-Id: I4e500fbd8b862bfc8cdfef7d6e854d78474a463f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3263809
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#950140}

--- chrome/browser/sync/chrome_sync_client.cc
+++ chrome/browser/sync/chrome_sync_client.cc
@@ -86,7 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/forwarding_model_type_controller_delegate.h"
 #include "components/sync/model/model_type_store.h"
 #include "components/sync/model/model_type_store_service.h"
-#include "components/sync/trusted_vault/standalone_trusted_vault_client.h"
 #include "components/sync_bookmarks/bookmark_sync_service.h"
 #include "components/sync_preferences/pref_service_syncable.h"
 #include "components/sync_sessions/session_sync_service.h"
@@ -121,6 +120,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "chrome/browser/sync/trusted_vault_client_android.h"
+#else
+#include "components/sync/trusted_vault/standalone_trusted_vault_client.h"  // nogncheck
 #endif  // defined(OS_ANDROID)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
