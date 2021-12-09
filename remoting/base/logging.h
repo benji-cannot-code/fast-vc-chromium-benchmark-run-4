088d492a02251e1ebac870a4ec5ec253ca0625dc
Joe Downing <joedow@google.com>
2021-12-09 19:42:07 +0000

    Moving common internal logging init method to private header
    Bug: NO_BUG
    Change-Id: I7c7f1ced13834be5099c753ee6534f75707ef4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321430
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950217}

--- remoting/base/logging.h
+++ remoting/base/logging.h
@@ -35,9 +35,6 @@ constexpr GUID kRemotingHostLogProviderGuid = {
 // Initializes host logging.
 void InitHostLogging();
 
-// Common initialization for all platforms, called by InitHostLogging().
-void InitHostLoggingCommon();
-
 }  // namespace remoting
 
 #endif  // REMOTING_BASE_LOGGING_H_
--- remoting/base/logging_win.cc
+++ remoting/base/logging_win.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "remoting/base/logging.h"
+#include "remoting/base/logging_internal.h"
 
 #include <guiddef.h>
 
 #include "base/logging.h"
 #include "base/logging_win.h"
+#include "remoting/base/logging.h"
 
 namespace remoting {
 
