1cb59787e584f64f2bbeb7653f0ee23020557635
Aliona DANGLA <alionadangla@google.com>
2021-12-09 17:16:36 +0000

    [iOS] Update password breach alerts 'change' version
    This CL:
    * Manage secondary action button in mediator
    * Manage primary button to show password settings or dismiss screen
      depending on leak type
    * Center the stack view if there is a specific content view
    
    Result:
    https://drive.google.com/file/d/12y7i0pz7FN-7qpZwHPyD0M81XYxNQNhT/view?usp=sharing&resourcekey=0-DYynEIzdvN0wRmfyHH_PNA
    
    Fixed: 1268778
    Change-Id: I342b9a20d23fd553264a1b3671119bfb16f8eb4b
    No-Presubmit: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307067
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Cr-Commit-Position: refs/heads/main@{#950123}

--- ios/chrome/browser/ui/passwords/password_breach_consumer.h
+++ ios/chrome/browser/ui/passwords/password_breach_consumer.h
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Sets the respective state in the consumer.
 - (void)setTitleString:(NSString*)titleString
-         subtitleString:(NSString*)subtitleString
-    primaryActionString:(NSString*)primaryActionString;
+           subtitleString:(NSString*)subtitleString
+      primaryActionString:(NSString*)primaryActionString
+    secondaryActionString:(NSString*)secondaryActionString;
 
 @end
 
