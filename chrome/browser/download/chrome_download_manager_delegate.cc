6c9141699bf0e09658d3e8dbed05b9840dcae3bd
Mateusz Malon <mmalon@opera.com>
2021-12-09 07:59:27 +0000

    Determine whether download should be opened in browser during 'Open' call
    As should_prefer_opening_in_browser_ param is not available
    after browser relaunch (this is calculated only once during the download process)..
    
    Bug: 1273374
    Change-Id: If7c0267c473503bff62a3116229faa4738eead21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3301700
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Mateusz Malon <mmalon@opera.com>
    Cr-Commit-Position: refs/heads/main@{#949976}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/mime_util.h"
 #include "net/base/network_change_notifier.h"
 #include "ppapi/buildflags/buildflags.h"
+#include "third_party/blink/public/common/mime_util/mime_util.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if defined(OS_ANDROID)
@@ -844,7 +845,8 @@ void ChromeDownloadManagerDelegate::OpenDownload(DownloadItem* download) {
     return;
   }
 
-  if (!DownloadItemModel(download).ShouldPreferOpeningInBrowser()) {
+  if (!blink::IsSupportedMimeType(download->GetMimeType()) &&
+      !IsOpenInBrowserPreferreredForFile(download->GetTargetFilePath())) {
     RecordDownloadOpenMethod(DOWNLOAD_OPEN_METHOD_DEFAULT_PLATFORM);
     OpenDownloadUsingPlatformHandler(download);
     return;
