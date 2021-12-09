427ac2c4c4639b3408be81459913026f5633eabc
Hiroki Nakagawa <nhiroki@chromium.org>
2021-12-09 17:00:31 +0000

    ServiceWorker: Remove content/ dependency from NavigationPreloadRequest
    This is a preparation for moving NavigationPreloadRequest from
    content/renderer/service_worker/ to blink/ (onion soup).
    
    This CL replaces content::ServiceWorkerContextClient with
    blink::WebServiceWorkerContextClient to remove dependency on content/
    from NavigationPreloadRequest. content::ServiceWorkerContextClient is an
    implementation of blink::WebServiceWorkerContextClient, so this change
    doesn't affect functional behavior. To achieve this replacement, this CL
    moves some functions from content::ServiceWorkerContextClient to
    blink::WebServiceWorkerContextClient.
    
    Bug: 789857
    Change-Id: I79e11f26dd309fec50ea3355dbc05c51a11ccede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327441
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950110}

--- content/renderer/service_worker/navigation_preload_request.cc
+++ content/renderer/service_worker/navigation_preload_request.cc
@@ -7,17 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "content/renderer/service_worker/service_worker_context_client.h"
 #include "net/http/http_response_headers.h"
 #include "services/network/public/mojom/early_hints.mojom.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_error_type.mojom.h"
 #include "third_party/blink/public/platform/web_url_loader.h"
+#include "third_party/blink/public/web/modules/service_worker/web_service_worker_context_client.h"
 
 namespace content {
 
 NavigationPreloadRequest::NavigationPreloadRequest(
-    ServiceWorkerContextClient* owner,
+    blink::WebServiceWorkerContextClient* owner,
     int fetch_event_id,
     const blink::WebURL& url,
     mojo::PendingReceiver<network::mojom::URLLoaderClient>
--- third_party/blink/renderer/modules/service_worker/web_embedded_worker_impl_test.cc
+++ third_party/blink/renderer/modules/service_worker/web_embedded_worker_impl_test.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/service_worker/service_worker_object.mojom-blink.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_registration.mojom-blink.h"
 #include "third_party/blink/public/mojom/timing/worker_timing_container.mojom-blink.h"
+#include "third_party/blink/public/platform/modules/service_worker/web_service_worker_error.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/web_back_forward_cache_loader_helper.h"
 #include "third_party/blink/public/platform/web_content_settings_client.h"
@@ -236,6 +237,21 @@ class MockServiceWorkerContextClient final
   CreateWorkerFetchContextOnInitiatorThread() override {
     return base::MakeRefCounted<FakeWebServiceWorkerFetchContext>();
   }
+
+  void OnNavigationPreloadResponse(
+      int fetch_event_id,
+      std::unique_ptr<WebURLResponse> response,
+      mojo::ScopedDataPipeConsumerHandle data_pipe) override {}
+
+  void OnNavigationPreloadComplete(int fetch_event_id,
+                                   base::TimeTicks completion_time,
+                                   int64_t encoded_data_length,
+                                   int64_t encoded_body_length,
+                                   int64_t decoded_body_length) override {}
+
+  void OnNavigationPreloadError(
+      int fetch_event_id,
+      std::unique_ptr<WebServiceWorkerError> error) override {}
 
   void WorkerContextDestroyed() override { termination_event_.Signal(); }
 
