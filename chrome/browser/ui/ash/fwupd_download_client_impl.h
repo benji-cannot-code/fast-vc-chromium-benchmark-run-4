e42a4b73d85902e627f1544b48a4a2ed01c8114a
Jimmy Gong <jimmyxgong@google.com>
2021-12-10 04:24:35 +0000

    fwupd: Add FwupdDownloadClient
    FwupdDownloadClient provides Profile-based download helper functions
    for ash clients. Followup CL uses this class to load firmware download
    URLs.
    
    Bug: 1252981
    Change-Id: I8a27e3c884a7a6803685b354486fc23e53a18b89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3293852
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950427}

---
+++ chrome/browser/ui/ash/fwupd_download_client_impl.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ASH_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
+#define CHROME_BROWSER_UI_ASH_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
+
+#include "ash/public/cpp/fwupd_download_client.h"
+#include "base/memory/scoped_refptr.h"
+
+namespace network {
+class SharedURLLoaderFactory;
+}  // namespace network
+
+namespace ash {
+
+// Class to provide Profile based information to the ash client.
+class FwupdDownloadClientImpl : public ash::FwupdDownloadClient {
+ public:
+  FwupdDownloadClientImpl();
+  FwupdDownloadClientImpl(const FwupdDownloadClientImpl&) = delete;
+  FwupdDownloadClientImpl& operator=(const FwupdDownloadClientImpl&) = delete;
+  ~FwupdDownloadClientImpl() override;
+
+  // ash::FwupdDownloadClient:
+  scoped_refptr<network::SharedURLLoaderFactory> GetURLLoaderFactory() override;
+};
+
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_UI_ASH_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
