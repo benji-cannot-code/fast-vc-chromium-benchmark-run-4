7ac82db00c53be53508d4ef10f66c9b4af9b2518
Ethan Jimenez <ethavar@microsoft.com>
2021-12-10 04:59:33 +0000

    [GridNG] Move GridItemData out of ng_grid_layout_algorithm.h
    Refactoring `GridItemData` and `GridItems` to be contained within a new
    file `ng_grid_item.h`. This change shouldn't have any behavioral impact.
    
    Bug: 618969
    Change-Id: I7e75766d4c5530caf7ebe220f57ab54691edf086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3319426
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ana Sollano Kim <ansollan@microsoft.com>
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#950434}

--- third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
+++ third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_inline.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h"
+#include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_placement.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_line_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_box_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/legacy_layout_tree_walking.h"
@@ -374,11 +375,13 @@ NGOutOfFlowLayoutPart::GetContainingBlockInfo(
           candidate.Node(), container_style,
           default_writing_direction_.GetWritingMode());
 
+      NGGridPlacement grid_placement(
+          container_style,
+          To<LayoutNGGrid>(container_object)->CachedPlacementData());
+
       return {default_writing_direction_,
               NGGridLayoutAlgorithm::ComputeOutOfFlowItemContainingRect(
-                  container_style,
-                  To<LayoutNGGrid>(container_object)->CachedPlacementData(),
-                  container_builder_->GridLayoutData(),
+                  grid_placement, container_builder_->GridLayoutData(),
                   container_builder_->Borders(),
                   container_builder_->InitialBorderBoxSize(),
                   container_builder_->FragmentsTotalBlockSize(), &grid_item)};
