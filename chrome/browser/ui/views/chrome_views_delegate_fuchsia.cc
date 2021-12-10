aff75a18a976145b2bcedb743c5747e7a3db1e6b
Benjamin Lerman <qsr@chromium.org>
2021-12-10 13:01:58 +0000

    Revert "[fuchsia] Implement ChromeViewsDelegate::CreateNativeWidget()"
    This reverts commit 2908450d175db594d368fc3e7276be8cbde00c16.
    
    Reason for revert: This breaks all menu in chrome for Fuchsia: they are displayed as full screen windows.
    
    Original change's description:
    > [fuchsia] Implement ChromeViewsDelegate::CreateNativeWidget()
    >
    > In order to place widgets in the Aura widget tree, NativeWidgetAura
    > requires that either the new widget has a "parent", or that a "context"
    > is specified for it to be positioned under.
    >
    > UI elements such as the Profile Picker view are created with neither
    > of these set, because the Chrome implementation expects that its
    > ViewDelegate implementation will create a DesktopNativeWidgetAura, if
    > necessary, to contain the new view.
    >
    > Fuchsia now implements ChromeViewsDelegate::CreateNativeWidget(), using
    > the same logic as Linux.
    >
    > Bug: 1276451, 1234748
    > Bug: fuchsia:88142
    > Change-Id: I15554bd035394cf039e6e499a89e5a6cd8fcf77a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320244
    > Auto-Submit: Wez <wez@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#949569}
    
    Bug: 1276451, 1234748
    Bug: fuchsia:88142
    Change-Id: I34cba0bd57f748910fbda8b1821b9ac1dec962d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3329786
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    Commit-Queue: Benjamin Lerman <qsr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950511}

--- chrome/browser/ui/views/chrome_views_delegate_fuchsia.cc
+++ chrome/browser/ui/views/chrome_views_delegate_fuchsia.cc
@@ -5,15 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/chrome_views_delegate.h"
 
-#include "chrome/browser/ui/views/native_widget_factory.h"
-
 views::NativeWidget* ChromeViewsDelegate::CreateNativeWidget(
     views::Widget::InitParams* params,
     views::internal::NativeWidgetDelegate* delegate) {
-  NativeWidgetType native_widget_type =
-      (params->parent && params->type != views::Widget::InitParams::TYPE_MENU &&
-       params->type != views::Widget::InitParams::TYPE_TOOLTIP)
-          ? NativeWidgetType::NATIVE_WIDGET_AURA
-          : NativeWidgetType::DESKTOP_NATIVE_WIDGET_AURA;
-  return ::CreateNativeWidget(native_widget_type, params, delegate);
+  // TODO(crbug.com/1234748)
+  NOTIMPLEMENTED_LOG_ONCE();
+  return nullptr;
 }
