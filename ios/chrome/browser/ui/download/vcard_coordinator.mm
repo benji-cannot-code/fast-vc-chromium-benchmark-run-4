3a33f6f1b1fd02b21b496858c98d09cec11039b3
Ewann <ewannpv@google.com>
2021-12-10 12:34:57 +0000

    [iOS][Downloads][4/N] Adds Vcard support
    This CL allows users to edit or save the contact to their contact list.
    
    screenshots:
    - not in contact list:
    https://drive.google.com/file/d/1yqzT-tqGbfWVAiNMYGzpy-JDgPhPMENW/view?usp=sharing&resourcekey=0-eQHecnulT6133yq_x5DocA
    - already in contact list:
    https://drive.google.com/file/d/1895_sNUYAa_SEzWjyXrfCdYVySK19pos/view?usp=sharing&resourcekey=0-U5dSJ_RPA9Wtomk0N1qK7w
    
    Bug: 1244002
    Change-Id: Ie5ce869752a952e47bbc3df579bddca5c4c23dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322912
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950508}

--- ios/chrome/browser/ui/download/vcard_coordinator.mm
+++ ios/chrome/browser/ui/download/vcard_coordinator.mm
@@ -86,6 +86,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CNContactViewController* contactViewController =
       [CNContactViewController viewControllerForUnknownContact:contact];
 
+  contactViewController.allowsEditing = YES;
+  contactViewController.contactStore = [[CNContactStore alloc] init];
+
   UIBarButtonItem* dismissButton = [[UIBarButtonItem alloc]
       initWithBarButtonSystemItem:UIBarButtonSystemItemDone
                            target:self
