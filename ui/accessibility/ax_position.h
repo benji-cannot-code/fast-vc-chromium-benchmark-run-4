b905c84f4b0e6d79edb6a432ee109f8383e733c3
Jose Dapena Paz <jdapena@igalia.com>
2021-12-10 17:05:25 +0000

    GCC: use initializer list for NoDestructor construction in AXPosition.
    Fix build issue with GCC:
        ../../ui/accessibility/ax_position.h:4356:61: error: call of overloaded ‘NoDestructor(<brace-enclosed initializer list>)’ is ambiguous
         4356 |       static const base::NoDestructor<std::vector<int32_t>> embedded_word_ends(
              |                                                             ^~~~~~~~~~~~~~~~~~
    
    Bug: 819294
    Change-Id: I63233b3a201d969b9acabaee87f81a8b58d43453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3326825
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#950583}

--- ui/accessibility/ax_position.h
+++ ui/accessibility/ax_position.h
@@ -4325,7 +4325,7 @@ class AXPosition {
       // Using braces ensures that the vector will contain the given value, and
       // not create a vector of size 0.
       static const base::NoDestructor<std::vector<int32_t>>
-          embedded_word_starts({0});
+          embedded_word_starts{{0}};
       return *embedded_word_starts;
     }
 
@@ -4354,8 +4354,8 @@ class AXPosition {
     if (IsEmptyObjectReplacedByCharacter()) {
       // Using braces ensures that the vector will contain the given value, and
       // not create a vector of size 1.
-      static const base::NoDestructor<std::vector<int32_t>> embedded_word_ends(
-          {1});
+      static const base::NoDestructor<std::vector<int32_t>> embedded_word_ends{
+          {1}};
       return *embedded_word_ends;
     }
 
