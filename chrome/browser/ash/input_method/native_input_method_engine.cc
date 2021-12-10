e51a82903c5c20365ecbe3efc0e9b94583aaeda8
Keith Lee <keithlee@chromium.org>
2021-12-10 01:29:52 +0000

    Allow the decoder to recieve and handle Tab events
    R=shend@chromium.org
    
    Bug: b/209701204
    Change-Id: I7a9139581ada92dfe6ec62d31174db7576f868d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3329042
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950372}

--- chrome/browser/ash/input_method/native_input_method_engine.cc
+++ chrome/browser/ash/input_method/native_input_method_engine.cc
@@ -354,6 +354,8 @@ absl::optional<mojom::NamedDomKey> NamedDomKeyToMojom(
       return mojom::NamedDomKey::kPageDown;
     case ui::DomKey::PAGE_UP:
       return mojom::NamedDomKey::kPageUp;
+    case ui::DomKey::TAB:
+      return mojom::NamedDomKey::kTab;
     default:
       return absl::nullopt;
   }
