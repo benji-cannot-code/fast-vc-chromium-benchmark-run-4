10150c12f7712cf0478044e53ebc3791351f49ba
Ewann <ewannpv@google.com>
2021-12-10 13:46:07 +0000

    [iOS] Updates TableViewDetailIconCell constraint
    This CL updates the trailing padding of the stackView to be the same as
    the leading padding of the icon.
    
    Fixed: 1267397
    Change-Id: I57c8f26fe6c0be79d86f6cbd92ae33558d701034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327207
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950513}

--- ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.mm
@@ -165,7 +165,7 @@ const CGFloat kCellLabelsWidthProportion = 3.0f;
       // Text labels.
       [_textStackView.trailingAnchor
           constraintEqualToAnchor:contentView.trailingAnchor
-                         constant:-kTableViewTrailingContentPadding],
+                         constant:-kTableViewHorizontalSpacing],
       _iconHiddenConstraint,
 
       // Leading constraint for |customSeparator|.
