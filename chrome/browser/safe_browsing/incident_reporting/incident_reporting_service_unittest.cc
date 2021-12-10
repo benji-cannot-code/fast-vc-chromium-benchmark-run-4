f33599ac450a162bad552e89cef72c51d5f5f557
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2021-12-10 19:13:24 +0000

    [DestroyProfileOnBrowserClose] Log number of zombie profiles
    This complements metrics added in crrev.com/c/3289830, to track memory
    savings from Profile object destruction.
    
    Two new histograms let us track how many Profiles are in a "zombie"
    state (i.e., deleted from memory or still loaded but refcount=0):
    
    - Profile.ZombieProfileCount for zombies
    - Profile.LiveProfileCount for non-zombies
    
    This lets us know how often Profiles are destroyed, and how long they
    stay destroyed for (they could get re-loaded from disk later on).
    
    With LiveProfileCount, we can estimate the time users spend in the
    0-Profile state on macOS.
    
    Bug: 88586
    Change-Id: I77b8837a2bed58bbdeb20c97072eea6bb9b31cd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321267
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950621}

--- chrome/browser/safe_browsing/incident_reporting/incident_reporting_service_unittest.cc
+++ chrome/browser/safe_browsing/incident_reporting/incident_reporting_service_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/safe_browsing/incident_reporting/incident.h"
 #include "chrome/browser/safe_browsing/incident_reporting/incident_receiver.h"
 #include "chrome/browser/safe_browsing/incident_reporting/incident_report_uploader.h"
@@ -213,6 +214,7 @@ class IncidentReportingServiceTest : public testing::Test {
     ASSERT_TRUE(profile_manager_.SetUp());
     // Disable profile metrics reporting, otherwise the calls to
     // FastForwardUntilNoTasksRemain() never return.
+    profile_manager_.profile_manager()->DisableProfileMetricsForTesting();
     profile_manager_.profile_attributes_storage()
         ->DisableProfileMetricsForTesting();
   }
