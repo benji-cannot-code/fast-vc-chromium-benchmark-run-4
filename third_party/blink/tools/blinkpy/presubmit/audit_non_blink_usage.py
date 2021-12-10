fad7913de87e5bcd01fcc7e3ff286a32d7b2cad8
Hiroki Nakagawa <nhiroki@chromium.org>
2021-12-10 06:22:10 +0000

    ServiceWorker: Move NavigationPreloadRequest to blink/
    This CL moves NavigationPreloadRequest from
    content/renderer/service_worker/ to blink/. During the transition period
    of the onion soup, ServiceWorkerContextClient in content/ still needs to
    access NavigationPreloadRequest, so this CL introduces a public
    blink::WebNavigationPreloadRequest interface as a stopgap. This
    interface will be removed after the onion soup is completed.
    
    Bug: 789857
    Change-Id: Ia2344b0c71092ca1b74c231a89621b5c044347a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327523
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950444}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -1630,6 +1630,18 @@ _CONFIG = [
             'base::SingleSampleMetricsFactory',
         ],
     },
+    {
+        'paths': [
+            'third_party/blink/renderer/modules/service_worker/navigation_preload_request.cc',
+            'third_party/blink/renderer/modules/service_worker/navigation_preload_request.h',
+        ],
+        'allowed': [
+            'net::ERR_.+',
+            'net::HttpResponseHeaders',
+            'net::OK',
+            'net::RedirectInfo',
+        ],
+    },
 ]
 
 
