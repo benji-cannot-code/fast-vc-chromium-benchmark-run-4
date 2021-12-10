58c4deb7ecb677abd668218cf2811f0ddafb4052
Orin Jaworski <orinj@google.com>
2021-12-10 18:04:44 +0000

    [omnibox] Some minor refactor cleanup
    Bug: 1208026
    Change-Id: I242d65b8cc6b185721f28a6a74e40c58d729685d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3325185
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950608}

--- components/omnibox/browser/omnibox_edit_model.cc
+++ components/omnibox/browser/omnibox_edit_model.cc
@@ -1616,10 +1616,6 @@ void OmniboxEditModel::OnCurrentMatchChanged() {
                      is_keyword_hint, additional_text);
 }
 
-// static
-const char OmniboxEditModel::kCutOrCopyAllTextHistogram[] =
-    "Omnibox.CutOrCopyAllText";
-
 void OmniboxEditModel::SetAccessibilityLabel(const AutocompleteMatch& match) {
   view_->SetAccessibilityLabel(view_->GetText(), match, true);
 }
