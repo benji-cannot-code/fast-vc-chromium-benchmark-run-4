759b217c3aee9a605647f4922547cd05f02539cd
Bao-Duy Tran <tranbaoduy@chromium.org>
2021-12-10 02:00:29 +0000

    Uses better scoped crbug for FakeTextInputClient migration TODOs.
    Original TODOs were tagged with crbug/1148157 scoped to cleanup for
    NativeInputMethodEngine browser tests, which is not the only usage sites
    of DummyTextInputClient that needs replacing with FakeTextInputClient.
    
    The better scoped crbug will also help bring crbug/1148157 to closure
    once all other cleanups for NativeInputMethodEngine browser tests are
    deemed complete, without blocking on the more elaborate and tedious
    FakeTextInputClient migration.
    
    Bug: 1277388,1148157
    Change-Id: Ie41efe78ea3687622536cd8cda095b972b22fd30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3319482
    Auto-Submit: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950386}

--- ui/base/ime/dummy_text_input_client.h
+++ ui/base/ime/dummy_text_input_client.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 // Dummy implementation of TextInputClient. All functions do nothing.
-// TODO(crbug.com/1148157): Replace this class with FakeTextInputClient.
+// TODO(crbug.com/1277388): Replace this class with FakeTextInputClient.
 class DummyTextInputClient : public TextInputClient {
  public:
   DummyTextInputClient();
