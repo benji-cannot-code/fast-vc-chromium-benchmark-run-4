f40852b6593b266bd8c7ae3d4e2dbfb47261389d
Matthew Denton <mpdenton@chromium.org>
2021-12-10 12:13:37 +0000

    Linux OSCrypt: Add GetRawEncryptionKey() and SetRawEncryptionKey()
    See the attached bug for details on why this CL adds these functions.
    
    Adding these functions requires us to cache the crypto::SymmetricKeys
    themselves rather than the passwords used to derive the SymmetricKeys.
    This should be slightly more performant anyway as we don't need to
    re-derive the SymmetricKeys every time we want to encrypt or decrypt.
    
    This CL adds the function to os_crypt_posix.cc as well to avoid the
    unnecessary #if blocks everywhere, even though os_crypt_posix.cc
    only uses a hardcoded password.
    
    This CL modifies the test-only functions as well to make testing a
    little more clearly.
    
    Finally, this CL stops compiling os_crypt_linux.cc for lacros, and
    starts compiling os_crypt_posix.cc like it did for older chromeos
    ash. Encryption doesn't work and isn't necessary on Chrome OS so
    there's no need for lacros to compile it.
    
    Bug: 1277463
    Change-Id: Ib8e13c589a290ab0c08170aff936420fed99d033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3319715
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950502}

--- components/os_crypt/os_crypt_posix.cc
+++ components/os_crypt/os_crypt_posix.cc
@@ -141,3 +141,18 @@ bool OSCrypt::DecryptString(const std::string& ciphertext,
 
   return true;
 }
+
+// static
+bool OSCrypt::IsEncryptionAvailable() {
+  return false;
+}
+
+// static
+void OSCrypt::SetRawEncryptionKey(const std::string& raw_key) {
+  DCHECK(raw_key.empty());
+}
+
+// static
+std::string OSCrypt::GetRawEncryptionKey() {
+  return "";
+}
