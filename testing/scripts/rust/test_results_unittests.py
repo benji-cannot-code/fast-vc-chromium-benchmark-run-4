2e7269fc8973f530c6b490f4f18aa1902cf1ec9b
Lukasz Anforowicz <lukasza@chromium.org>
2021-12-10 15:28:24 +0000

    [rust] generate `out/.../bin/run_foo` script from rust_unit_test("foo").
    This CL tweaks GN templates so that `rust_unit_test(foo)` will not only
    generate a `out/dir/foo` executable with unit tests from "foo" crate,
    but also generate a `out/dir/bin/run_foo` Bash script that can be used
    to run the tests in a //docs/testing/test_executable_api.md-compatible
    way.
    
    Exactly 1 generated run_foo script is generated per 1
    rust_unit_test("foo") build target.  In the future, additional gni magic
    may be provided for generating a script that covers multiple crates
    (e.g. multiple Rust unit test executables).  To ensure that the test
    names are stable going forward, this CL preemptively tweaks
    //testing/scripts/rust to encode the name of a test executable in test
    names.
    
    Manual testing:
    
    1. $ ninja -C out/rust build/rust/tests/test_rust_unittests
       $ out/rust/bin/run_test_rust_unittests
    
    2. Checking that a follow-up CL can successfully execute Rust unit tests
       on a bot: https://crrev.com/c/3322199/3
    
    Bug: 1260120
    Change-Id: I2c963c12c343ff90a7d5132ee8b7136d24a79685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3268794
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950547}

--- testing/scripts/rust/test_results_unittests.py
+++ testing/scripts/rust/test_results_unittests.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import unittest
 
 from test_results import TestResult
+from test_results import _build_json_data
 
 
 class TestResultTests(unittest.TestCase):
@@ -36,6 +37,42 @@ class TestResultTests(unittest.TestCase):
         self.assertIn('PASS', fail_expected_repr)
         self.assertIn('FAIL', fail_expected_repr)
         self.assertIn('TestResult', fail_expected_repr)
+
+
+class BuildJsonDataTests(unittest.TestCase):
+    def test_grouping_of_tests(self):
+        t1 = TestResult('group1/foo', 'PASS')
+        t2 = TestResult('group1/bar', 'FAILED')
+        t3 = TestResult('group2/baz', 'FAILED')
+        actual_result = _build_json_data([t1, t2, t3], 123)
+        # yapf: disable
+        expected_result = {
+            'interrupted': False,
+            'path_delimiter': '/',
+            'seconds_since_epoch': 123,
+            'version': 3,
+            'tests': {
+                'group1': {
+                    'foo': {
+                        'expected': 'PASS',
+                        'actual': 'PASS'
+                    },
+                    'bar': {
+                        'expected': 'PASS',
+                        'actual': 'FAILED'
+                    }},
+                'group2': {
+                    'baz': {
+                        'expected': 'PASS',
+                        'actual': 'FAILED'
+                    }}},
+            'num_failures_by_type': {
+                'PASS': 1,
+                'FAILED': 2
+            }
+        }
+        # yapf: enable
+        self.assertEqual(actual_result, expected_result)
 
 
 if __name__ == '__main__':
