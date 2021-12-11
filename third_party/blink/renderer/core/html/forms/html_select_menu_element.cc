709deb49e8655d87d2b71258b2a28440b041ab1d
Ionel Popescu <iopopesc@microsoft.com>
2021-12-11 00:09:44 +0000

    [SelectMenu] Correctly update document for SelectMenu mutation observer.
    Prior to this CL, we were using SynchronousMutationObserver::DidMoveTreeToNewDocument
    to update the document for the SelectMenu mutation observer.
    
    This behavior is not correct, since DidMoveTreeToNewDocument is called
    from HeapObserverSet::ForEachObserver[1] that doesn't allow adding or
    removing observers while iterating.
    
    This CL fixes the issue by using instead Element::DidMoveToNewDocument
    to set the new document for the SelectMenu mutation observer.
    
    [1]: https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/heap_observer_set.h;l=65;
    
    Bug: 1277534, 1276592
    Change-Id: I3f69485c62ed32955d68801440e441d55029016b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3328906
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#950708}

--- third_party/blink/renderer/core/html/forms/html_select_menu_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_menu_element.cc
@@ -41,7 +41,6 @@ class HTMLSelectMenuElement::SelectMutationCallback
                         const QualifiedName& name,
                         const AtomicString& old_value,
                         const AtomicString& new_value) final;
-  void DidMoveTreeToNewDocument(const Node& root) final;
 
   void Trace(Visitor* visitor) const override;
 
@@ -123,12 +122,6 @@ void HTMLSelectMenuElement::SelectMutationCallback::AttributeChanged(
       SlotChanged(new_value);
     }
   }
-}
-
-void HTMLSelectMenuElement::SelectMutationCallback::DidMoveTreeToNewDocument(
-    const Node& root) {
-  if (root == select_)
-    SetDocument(&select_->GetDocument());
 }
 
 template <typename StringType>
@@ -305,6 +298,11 @@ void HTMLSelectMenuElement::DidAddUserAgentShadowRoot(ShadowRoot& root) {
   option_part_listener_ =
       MakeGarbageCollected<HTMLSelectMenuElement::OptionPartEventListener>(
           this);
+}
+
+void HTMLSelectMenuElement::DidMoveToNewDocument(Document& old_document) {
+  HTMLFormControlElementWithState::DidMoveToNewDocument(old_document);
+  select_mutation_callback_->SetDocument(&GetDocument());
 }
 
 String HTMLSelectMenuElement::value() const {
