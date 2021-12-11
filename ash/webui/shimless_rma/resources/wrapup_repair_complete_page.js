4a871f9c62a493c569b5725413e51108ef0e9549
Gavin Dodd <gavindodd@google.com>
2021-12-11 04:07:37 +0000

    shimless: repair complete buttons
    Screenshot: https://screenshot.googleplex.com/BHWFrfuREzaTyTV.png
    
    Add shutdown and reboot buttons to the repair complete page
    
           Run browser tests for Shimless*
           Run closure compile for shimless_rma and tests
    
    Bug: 1198187, b/210065680
    Fixes: b/210065680
    Test: Run local build and check page
    Change-Id: I3c3fee771d02a875cf09b74d6b6cafb9dd9a123d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331235
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#950734}

--- ash/webui/shimless_rma/resources/wrapup_repair_complete_page.js
+++ ash/webui/shimless_rma/resources/wrapup_repair_complete_page.js
@@ -76,7 +76,34 @@ export class WrapupRepairCompletePage extends WrapupRepairCompletePageBase {
   }
 
   /** @protected */
-  onShutdownClick_() {}
+  onShutDownButtonClick_(e) {
+    e.preventDefault();
+    this.dispatchEvent(new CustomEvent(
+        'transition-state',
+        {
+          bubbles: true,
+          composed: true,
+          detail: (() => {
+            return this.shimlessRmaService_.endRmaAndShutdown();
+          })
+        },
+        ));
+  }
+
+  /** @protected */
+  onRebootButtonClick_(e) {
+    e.preventDefault();
+    this.dispatchEvent(new CustomEvent(
+        'transition-state',
+        {
+          bubbles: true,
+          composed: true,
+          detail: (() => {
+            return this.shimlessRmaService_.endRmaAndReboot();
+          })
+        },
+        ));
+  }
 
   /** @protected */
   onRmaLogButtonClick_() {
