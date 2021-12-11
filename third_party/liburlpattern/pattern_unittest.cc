07ab18a2f1c6b061149f19fd0130a6a24cf186b7
Ben Kelly <wanderview@chromium.org>
2021-12-11 19:44:15 +0000

    URLPattern: Emit `{}` when plain text follows a `:foo` group.
    This CL fixes another problem case from:
    
    https://github.com/WICG/urlpattern/issues/145
    
    In this case we need to detect when fixed text following a `:foo` group
    could be mistaken as party of the name.  When this happens we should
    emit `{ }` braces around the group to isolate it.  For example,
    `{:foo}bar` instead of `:foobar`.
    
    Bug: 1263673
    Change-Id: I9c9bfb736c6e8880bceeae9280f0fc30f794cea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321520
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950813}

--- third_party/liburlpattern/pattern_unittest.cc
+++ third_party/liburlpattern/pattern_unittest.cc
@@ -384,6 +384,14 @@ TEST(PatternStringTest, NamedGroupWithCustomRegexpAndValidNameSuffix) {
   RunPatternStringTest("{:foo(baz)bar}", "{:foo(baz)bar}");
 }
 
+TEST(PatternStringTest, NamedGroupInGroupingFollowedByValidNameText) {
+  RunPatternStringTest("{:foo}bar", "{:foo}bar");
+}
+
+TEST(PatternStringTest, NamedGroupFollowedByEscapedValidNameText) {
+  RunPatternStringTest(":foo\\bar", "{:foo}bar");
+}
+
 struct DirectMatchCase {
   absl::string_view input;
   bool expected_match = true;
