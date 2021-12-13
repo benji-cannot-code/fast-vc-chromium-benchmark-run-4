b079f994d47b022ee78ef1b55eebda8a06ff9f93
Yeunjoo Choi <ychoi@igalia.com>
2021-12-13 01:40:50 +0000

    [mparch] Ensure that subframe navigation doesn't affect TabStatsTracker
    This CL is part of MPArch tasks. TabStatsTracker already blocks subframe
    navigation (i.e. iframes, fenced frames..) by treating only
    browser-initiated navigations as user interactions or making codes run
    in the primary main frame only. So, this CL adds a browsertest which
    ensures subframe navigation doesn't affect TabStatsTracker.
    
    Bug: 1275874
    Change-Id: I26b44eeda24b09a0d45a603498d47a57b8669623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308482
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#950905}

--- chrome/browser/metrics/tab_stats/tab_stats_tracker_browsertest.cc
+++ chrome/browser/metrics/tab_stats/tab_stats_tracker_browsertest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/fenced_frame_test_util.h"
 #include "content/public/test/prerender_test_util.h"
 #include "content/public/test/web_contents_tester.h"
 #include "media/base/media_switches.h"
@@ -56,13 +57,20 @@ class TestTabStatsObserver : public TabStatsObserver {
     --tab_count_;
   }
 
+  void OnTabInteraction(content::WebContents* web_contents) override {
+    ++interaction_count_;
+  }
+
   size_t tab_count() { return tab_count_; }
 
   size_t window_count() { return window_count_; }
 
+  size_t interaction_count() { return interaction_count_; }
+
  private:
   size_t tab_count_ = 0;
   size_t window_count_ = 0;
+  size_t interaction_count_ = 0;
 };
 
 using TabsStats = TabStatsDataStore::TabsStats;
@@ -123,6 +131,10 @@ class TabStatsTrackerBrowserTest : public InProcessBrowserTest {
   void SetUpOnMainThread() override {
     tab_stats_tracker_ = TabStatsTracker::GetInstance();
     ASSERT_TRUE(tab_stats_tracker_ != nullptr);
+  }
+
+  content::WebContents* GetWebContents() {
+    return browser()->tab_strip_model()->GetActiveWebContents();
   }
 
  protected:
@@ -380,7 +392,7 @@ class LenientMockTabStatsObserver : public TabStatsObserver {
   MOCK_METHOD2(OnMediaEffectivelyFullscreenChanged,
                void(content::WebContents*, bool));
 };
-using MockTabStatsObserver = testing::StrictMock<LenientMockTabStatsObserver>;
+using MockTabStatsObserver = testing::NiceMock<LenientMockTabStatsObserver>;
 
 }  // namespace
 
@@ -579,26 +591,6 @@ IN_PROC_BROWSER_TEST_F(TabStatsTrackerBrowserTest, AddObserverAudibleTab) {
   tab_stats_tracker_->RemoveObserver(&mock_observer);
 }
 
-namespace {
-
-class MockTabStatsObserverForPrerenderingTest : public TabStatsObserver {
- public:
-  MockTabStatsObserverForPrerenderingTest() = default;
-  ~MockTabStatsObserverForPrerenderingTest() override = default;
-  MockTabStatsObserverForPrerenderingTest(
-      const MockTabStatsObserverForPrerenderingTest& other) = delete;
-  MockTabStatsObserverForPrerenderingTest& operator=(
-      const MockTabStatsObserverForPrerenderingTest&) = delete;
-
-  MOCK_METHOD1(OnPrimaryMainFrameNavigationCommitted,
-               void(content::WebContents*));
-};
-
-using MockTabStatsPrerenderingObserver =
-    testing::StrictMock<MockTabStatsObserverForPrerenderingTest>;
-
-}  // namespace
-
 class TabStatsTrackerPrerenderBrowserTest : public TabStatsTrackerBrowserTest {
  public:
   TabStatsTrackerPrerenderBrowserTest()
@@ -627,10 +619,6 @@ class TabStatsTrackerPrerenderBrowserTest : public TabStatsTrackerBrowserTest {
     return prerender_helper_;
   }
 
-  content::WebContents* GetWebContents() {
-    return browser()->tab_strip_model()->GetActiveWebContents();
-  }
-
  private:
   content::test::PrerenderTestHelper prerender_helper_;
 };
@@ -647,7 +635,7 @@ IN_PROC_BROWSER_TEST_F(
   // OnPrimaryMainFrameNavigationCommitted() should not be called in
   // prerendering.
   {
-    MockTabStatsPrerenderingObserver mock_observer;
+    MockTabStatsObserver mock_observer;
     tab_stats_tracker_->AddObserverAndSetInitialState(&mock_observer);
     EXPECT_CALL(mock_observer,
                 OnPrimaryMainFrameNavigationCommitted(::testing::_))
@@ -661,7 +649,7 @@ IN_PROC_BROWSER_TEST_F(
 
   // OnPrimaryMainFrameNavigationCommitted() should be called after activating.
   {
-    MockTabStatsPrerenderingObserver mock_observer;
+    MockTabStatsObserver mock_observer;
     tab_stats_tracker_->AddObserverAndSetInitialState(&mock_observer);
     EXPECT_CALL(mock_observer,
                 OnPrimaryMainFrameNavigationCommitted(::testing::_))
@@ -670,6 +658,73 @@ IN_PROC_BROWSER_TEST_F(
     EXPECT_TRUE(host_observer->was_activated());
     tab_stats_tracker_->RemoveObserver(&mock_observer);
   }
+}
+
+class TabStatsTrackerSubFrameBrowserTest : public TabStatsTrackerBrowserTest {
+ public:
+  TabStatsTrackerSubFrameBrowserTest() = default;
+  ~TabStatsTrackerSubFrameBrowserTest() override = default;
+  TabStatsTrackerSubFrameBrowserTest(
+      const TabStatsTrackerSubFrameBrowserTest&) = delete;
+  TabStatsTrackerSubFrameBrowserTest& operator=(
+      const TabStatsTrackerSubFrameBrowserTest&) = delete;
+
+  void SetUpOnMainThread() override {
+    host_resolver()->AddRule("*", "127.0.0.1");
+    ASSERT_TRUE(embedded_test_server()->Start());
+    TabStatsTrackerBrowserTest::SetUpOnMainThread();
+  }
+
+ protected:
+  content::test::FencedFrameTestHelper fenced_frame_helper_;
+};
+
+// Ensure that subframe navigation cannot affect TabStatsTracker.
+IN_PROC_BROWSER_TEST_F(TabStatsTrackerSubFrameBrowserTest,
+                       VerifyBehaviorOnSubFrameNavigation) {
+  MockTabStatsObserver mock_observer;
+  TestTabStatsObserver count_observer;
+  tab_stats_tracker_->AddObserverAndSetInitialState(&mock_observer);
+  tab_stats_tracker_->AddObserverAndSetInitialState(&count_observer);
+
+  // Navigate to an initial page.
+  EXPECT_CALL(mock_observer,
+              OnPrimaryMainFrameNavigationCommitted(::testing::_))
+      .Times(1);
+  const GURL initial_url = embedded_test_server()->GetURL("/empty.html");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), initial_url));
+  EXPECT_EQ(1U, count_observer.interaction_count());
+  ::testing::Mock::VerifyAndClear(&mock_observer);
+
+  // Create an iframe and navigate inside the iframe.
+  EXPECT_CALL(mock_observer,
+              OnPrimaryMainFrameNavigationCommitted(::testing::_))
+      .Times(0);
+  ASSERT_TRUE(content::ExecJs(
+      GetWebContents(),
+      content::JsReplace("var iframe = document.createElement('iframe');"
+                         "iframe.src = $1;"
+                         "document.body.appendChild(iframe);",
+                         embedded_test_server()->GetURL("/title1.html"))));
+  WaitForLoadStop(GetWebContents());
+  ::testing::Mock::VerifyAndClear(&mock_observer);
+
+  // Create a fenced frame and navigate inside the fenced frame.
+  EXPECT_CALL(mock_observer,
+              OnPrimaryMainFrameNavigationCommitted(::testing::_))
+      .Times(0);
+  content::RenderFrameHost* fenced_frame_host =
+      fenced_frame_helper_.CreateFencedFrame(
+          GetWebContents()->GetMainFrame(),
+          embedded_test_server()->GetURL("/fenced_frames/title1.html"));
+  ASSERT_NE(nullptr, fenced_frame_host);
+  ::testing::Mock::VerifyAndClear(&mock_observer);
+
+  tab_stats_tracker_->RemoveObserver(&mock_observer);
+  tab_stats_tracker_->RemoveObserver(&count_observer);
+  // Ensure that subframe navigation doesn't increase the user interaction
+  // count.
+  EXPECT_EQ(1U, count_observer.interaction_count());
 }
 
 }  // namespace metrics
