364641b203ce675682586adbd59b341fa006bc6e
Sophey <sophey@chromium.org>
2021-12-13 20:03:52 +0000

    [SharingHub] Do not get ContentView when BottomSheet is null.
    Also removes the BottomSheetObserver when the share sheet is destroyed,
    which was causing the previous share sheet observers to be called when
    a new sheet is created.
    
    Fixed: 1266316
    Change-Id: Iaeb9817a6c1f16fa93348e88b84e4ee5036d4207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330705
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951158}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetCoordinator.java
@@ -146,6 +146,9 @@ public class ShareSheetCoordinator implements ActivityStateObserver, ChromeOptio
             @Override
             public void onSheetContentChanged(BottomSheetContent bottomSheet) {
                 super.onSheetContentChanged(bottomSheet);
+                if (mBottomSheet == null) {
+                    return;
+                }
                 if (bottomSheet == mBottomSheet) {
                     mBottomSheet.getContentView().addOnLayoutChangeListener(
                             ShareSheetCoordinator.this::onLayoutChange);
@@ -183,6 +186,9 @@ public class ShareSheetCoordinator implements ActivityStateObserver, ChromeOptio
         if (mLifecycleDispatcher != null) {
             mLifecycleDispatcher.unregister(this);
             mLifecycleDispatcher = null;
+        }
+        if (mBottomSheetController != null) {
+            mBottomSheetController.removeObserver(mBottomSheetObserver);
         }
     }
 
