68f6333a94c42b3717021751bff59b4b58f9cb3a
Benoit Lize <lizeb@chromium.org>
2021-12-13 10:41:03 +0000

    [PartitionAlloc] Allow double zone registration on mac for app_shim.
    Since the main binary of the app shim depends on //base, it registers
    its own malloc zone. Later, when the main framework is loaded, the code
    in InitializeDefaultMallocZoneWithPartitionAlloc() will find that there
    is already a zone names "PartitionAlloc", and not register a new one.
    
    However, base::UncheckedMalloc() will still go to the library instance
    of PartitionAlloc, as base::UncheckedMalloc() cannot call malloc()
    directly, since we configure it to crash rather than return nullptr. If
    a pointer which has been base::UncheckedMalloc()-ed in the library gets
    free()-ed, then the system free() does not know the zone it comes from,
    and panics.
    
    The ideal solution is to not depend on //base in the shim. A workaround
    is to allow the library to also register a zone.
    
    The second approach is taken in this CL: we change the zone name so that
    the library also registers it. We could remove the check for unicity in
    InitializeDefaultMallocZoneWithPartitionAlloc(), but we want to
    discourage multiple loading, so isolating the workaround makes more sense.
    
    Bug: 1274236
    Change-Id: I8f1f7833b016ad877e520d0f9435cfd4661460ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322973
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950976}

--- chrome/app_shim/app_mode_loader_mac.mm
+++ chrome/app_shim/app_mode_loader_mac.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
+#include "base/allocator/early_zone_registration_mac.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -199,6 +200,12 @@ int LoadFrameworkAndStart(int argc, char** argv) {
 
 __attribute__((visibility("default")))
 int main(int argc, char** argv) {
+  // The static constructor in //base will have registered PartitionAlloc as the
+  // default zone. Allow the //base instance in the main library to register it
+  // as well. Otherwise we end up passing memory to free() which was allocated
+  // by an unknown zone. See crbug.com/1274236 for details.
+  partition_alloc::AllowDoublePartitionAllocZoneRegistration();
+
   base::CommandLine::Init(argc, argv);
 
   // Exit instead of returning to avoid the the removal of |main()| from stack
