125b516de741abccb3dfd064831a1fd42d5eac30
Nate Fischer <ntfschr@chromium.org>
2021-12-13 22:53:20 +0000

    AW: remove Api21CompatibilityDelegate and add docs
    Now that our minSdkVersion is 23, we should be able to safely remove
    Api21CompatibilityDelegate, WebViewDelegateFactory, and some related
    code in the initialization path.
    
    Since I did some digging through the code history to verify this removal
    is OK, I thought it would also be helpful to document the history of the
    initialization path on different OS versions. I added this as class docs
    to WebViewChromiumFactoryProvider.
    
    Fixed: 1276584
    Test: launch WebView (system_webview_apk) on Android 23 emulator
    Test: launch WebView (system_webview_google_apk) on Android 24-28 emulator
    Test: launch WebView (trichrome_webview_google_apk) on Android 29+ emulator
    Change-Id: I1724d3bcec944f746b796df3f7453e9d066e05dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3315560
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951232}

--- android_webview/glue/java/src/com/android/webview/chromium/DrawGLFunctor.java
+++ android_webview/glue/java/src/com/android/webview/chromium/DrawGLFunctor.java
@@ -8,8 +8,7 @@ package com.android.webview.chromium;
 import android.graphics.Canvas;
 import android.os.Build;
 import android.view.View;
-
-import com.android.webview.chromium.WebViewDelegateFactory.WebViewDelegate;
+import android.webkit.WebViewDelegate;
 
 import org.chromium.android_webview.AwContents;
 import org.chromium.base.annotations.JniIgnoreNatives;
@@ -52,7 +51,9 @@ class DrawGLFunctor implements AwContents.NativeDrawGLFunctor {
         assert canvas != null;
         if (sSupportFunctorReleasedCallback) {
             assert releasedCallback != null;
-            mWebViewDelegate.callDrawGlFunction(canvas, mNativeDrawGLFunctor, releasedCallback);
+            GlueApiHelperForN.callDrawGlFunction(
+                    mWebViewDelegate, canvas, mNativeDrawGLFunctor, releasedCallback);
+
         } else {
             assert releasedCallback == null;
             mWebViewDelegate.callDrawGlFunction(canvas, mNativeDrawGLFunctor);
