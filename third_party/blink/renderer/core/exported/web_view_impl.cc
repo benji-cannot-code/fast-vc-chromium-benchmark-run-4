45618a0f76b3f64788d02f9c0f5368cf2aee6cd7
Dave Tapuska <dtapuska@chromium.org>
2021-12-13 17:45:00 +0000

    Do not apply page scale to fenced frame roots.
    Page scale should only be computed for the outermost main frame not
    fenced frame roots.
    
    BUG=1271351
    
    Change-Id: I8d1e3aadd30c9c948e15e0bf63bde23196a9fa36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330321
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951079}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -2647,7 +2647,7 @@ PageScaleConstraintsSet& WebViewImpl::GetPageScaleConstraintsSet() const {
 
 void WebViewImpl::RefreshPageScaleFactor() {
   if (!MainFrame() || !GetPage() || !GetPage()->MainFrame() ||
-      !GetPage()->MainFrame()->IsLocalFrame() ||
+      !GetPage()->MainFrame()->IsLocalFrame() || IsFencedFrameRoot() ||
       !GetPage()->DeprecatedLocalMainFrame()->View())
     return;
   UpdatePageDefinedViewportConstraints(MainFrameImpl()
