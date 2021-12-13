7b467d70d01b9d0e272e28c51b1e55c6ca83afb0
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-13 14:24:24 +0000

    CCA: Migrate flag.js to TypeScript
    Bug: b:172340451
    Test: tsc compile
    Change-Id: I2afb64965d56914aa8ea338991474150454d957a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3292481
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951014}

--- ash/webui/camera_app_ui/resources/js/flag.ts
+++ ash/webui/camera_app_ui/resources/js/flag.ts
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /**
  * Name of the chrome flags.
- * @enum {string}
  */
-export const Flag = {
-  DOCUMENT_MANUAL_CROP: 'cameraAppDocumentManualCrop',
-};
+export enum Flag {
+  DOCUMENT_MANUAL_CROP = 'cameraAppDocumentManualCrop',
+}
