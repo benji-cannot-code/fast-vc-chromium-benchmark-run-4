5346829c5fe37d62fb986ab43ceda769fb421840
Findit <findit-for-me@appspot.gserviceaccount.com>
2021-12-13 12:34:34 +0000

    Revert "dlp: Process confidential Lacros windows in Ash."
    This reverts commit 84ce36326b9e2985278ae1ac8095b4b85a0653a7.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision 84ce36326b9e2985278ae1ac8095b4b85a0653a7 as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8827964525852494017
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2F84ce36326b9e2985278ae1ac8095b4b85a0653a7&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+84ce36326b9e2985278ae1ac8095b4b85a0653a7
    
    Original change's description:
    > dlp: Process confidential Lacros windows in Ash.
    > 
    > When updated information about confidential content in Lacros windows
    > is received in Ash, it should be passed and correctly handled by
    > DlpContentManager.
    > It should track these restriction and apply to screenshot, screenshare
    > and privacy screen checks.
    > 
    > Bug: 1260467
    > Change-Id: I364d6d994d0fd652a6cd24f89351d3ed190b63dd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314865
    > Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    > Reviewed-by: Aida Zolic <aidazolic@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Commit-Queue: Sergey Poromov <poromov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#950982}
    
    
    Change-Id: I6fc34b60bd29e9ef2bcfc0dc5c2894114470f97d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1260467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3334563
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Owners-Override: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#950983}

--- chrome/browser/chromeos/policy/dlp/dlp_confidential_contents.h
+++ chrome/browser/chromeos/policy/dlp/dlp_confidential_contents.h
@@ -18,13 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 #include "url/gurl.h"
 
-namespace aura {
-class Window;
-}  // namespace aura
-
 namespace content {
 class WebContents;
-}  // namespace content
+}
 
 namespace policy {
 
@@ -32,15 +28,13 @@ namespace policy {
 // Used to cache and later show information about observed confidential contents
 // to the user.
 struct DlpConfidentialContent {
-  DlpConfidentialContent();
+  DlpConfidentialContent() = default;
   // Constructs DlpConfidentialContent from the title and icon obtained from
   // |web_contents|, which cannot be null.
   explicit DlpConfidentialContent(content::WebContents* web_contents);
-  // Constructs DlpConfidentialContent from the title and icon obtained from
-  // |window|, which cannot be null and |url|.
-  DlpConfidentialContent(aura::Window* window, const GURL& url);
-  DlpConfidentialContent(const DlpConfidentialContent& other);
-  DlpConfidentialContent& operator=(const DlpConfidentialContent& other);
+  DlpConfidentialContent(const DlpConfidentialContent& other) = default;
+  DlpConfidentialContent& operator=(const DlpConfidentialContent& other) =
+      default;
   ~DlpConfidentialContent() = default;
 
   // Contents with the same url are considered equal, ignoring the ref (part
@@ -77,16 +71,12 @@ class DlpConfidentialContents {
   // Converts |web_contents| to a DlpConfidentialContent and adds it to the
   // underlying container.
   void Add(content::WebContents* web_contents);
-  // Same for |window| and |url| pair.
-  void Add(aura::Window* window, const GURL& url);
 
   void Add(const DlpConfidentialContent& content);
 
   // Removes all stored confidential content, if there was any, and adds
   // |web_contents| converted to a DlpConfidentialContent.
   void ClearAndAdd(content::WebContents* web_contents);
-  // Same for |window| and |url| pair.
-  void ClearAndAdd(aura::Window* web_contents, const GURL& url);
 
   // Returns whether there is any content stored or not.
   bool IsEmpty() const;
