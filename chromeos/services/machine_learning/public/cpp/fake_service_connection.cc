528f78e2dcc8457356242ff44ea4712706c2964b
Andrew Moylan <amoylan@chromium.org>
2021-12-13 04:48:41 +0000

    Migrate to CreateGraphExecutorWithOptions
    CreateGraphExecutor callers now call CreateGraphExecutorWithOptions
    with a default Options struct.
    
    Bug: 1081597
    Change-Id: I7707e3a3b028f61dfaf3857b90963366302f8e0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2355162
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Andrew Moylan <amoylan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950923}

--- chromeos/services/machine_learning/public/cpp/fake_service_connection.cc
+++ chromeos/services/machine_learning/public/cpp/fake_service_connection.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/machine_learning/public/cpp/fake_service_connection.h"
 
 #include <utility>
+
 #include "base/bind.h"
+#include "base/notreached.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace chromeos {
@@ -68,20 +70,20 @@ void FakeServiceConnectionImpl::LoadFlatBufferModel(
       base::Unretained(this), std::move(receiver), std::move(callback)));
 }
 
-void FakeServiceConnectionImpl::CreateGraphExecutor(
+void FakeServiceConnectionImpl::REMOVED_0(
     mojo::PendingReceiver<mojom::GraphExecutor> receiver,
-    mojom::Model::CreateGraphExecutorCallback callback) {
-  ScheduleCall(base::BindOnce(
-      &FakeServiceConnectionImpl::HandleCreateGraphExecutorCall,
-      base::Unretained(this), std::move(receiver), std::move(callback)));
+    mojom::Model::REMOVED_0Callback callback) {
+  NOTIMPLEMENTED();
 }
 
-// Fake impl of CreateGrapHExecutorWithOptions just ignores `options`.
 void FakeServiceConnectionImpl::CreateGraphExecutorWithOptions(
     mojom::GraphExecutorOptionsPtr options,
     mojo::PendingReceiver<mojom::GraphExecutor> receiver,
-    mojom::Model::CreateGraphExecutorCallback callback) {
-  CreateGraphExecutor(std::move(receiver), std::move(callback));
+    mojom::Model::CreateGraphExecutorWithOptionsCallback callback) {
+  ScheduleCall(base::BindOnce(
+      &FakeServiceConnectionImpl::HandleCreateGraphExecutorWithOptionsCall,
+      base::Unretained(this), std::move(options), std::move(receiver),
+      std::move(callback)));
 }
 
 void FakeServiceConnectionImpl::LoadTextClassifier(
@@ -260,9 +262,10 @@ void FakeServiceConnectionImpl::HandleLoadFlatBufferModelCall(
   std::move(callback).Run(load_model_result_);
 }
 
-void FakeServiceConnectionImpl::HandleCreateGraphExecutorCall(
+void FakeServiceConnectionImpl::HandleCreateGraphExecutorWithOptionsCall(
+    mojom::GraphExecutorOptionsPtr options,
     mojo::PendingReceiver<mojom::GraphExecutor> receiver,
-    mojom::Model::CreateGraphExecutorCallback callback) {
+    mojom::Model::CreateGraphExecutorWithOptionsCallback callback) {
   if (create_graph_executor_result_ == mojom::CreateGraphExecutorResult::OK)
     graph_receivers_.Add(this, std::move(receiver));
 
