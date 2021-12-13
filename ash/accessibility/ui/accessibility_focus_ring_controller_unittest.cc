93432ab647d9348d50abfccd38585b0ff5db1edc
My Nguyen <myy@chromium.org>
2021-12-13 07:30:43 +0000

    Sheriff: Disable AccessibilityFocusRingControllerTest failing tests
    Bug: 1279278
    Change-Id: I01d0840fbbe55a2c9b7edd43ef09095fab89a5a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330441
    Owners-Override: My Nguyen <myy@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Auto-Submit: My Nguyen <myy@chromium.org>
    Commit-Queue: My Nguyen <myy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#950939}

--- ash/accessibility/ui/accessibility_focus_ring_controller_unittest.cc
+++ ash/accessibility/ui/accessibility_focus_ring_controller_unittest.cc
@@ -28,7 +28,8 @@ TEST_F(AccessibilityFocusRingControllerTest, CallingHideWhenEmpty) {
   controller->HideFocusRing("catsRCute");
 }
 
-TEST_F(AccessibilityFocusRingControllerTest, SetFocusRingCorrectRingGroup) {
+// Disabled due to failure. http://crbug.com/1279278
+TEST_F(AccessibilityFocusRingControllerTest, DISABLED_SetFocusRingCorrectRingGroup) {
   auto* controller = Shell::Get()->accessibility_focus_ring_controller();
   EXPECT_EQ(nullptr, controller->GetFocusRingGroupForTesting("catsRCute"));
   SkColor cat_color = SkColorSetARGB(0xFF, 0x42, 0x42, 0x42);
@@ -95,7 +96,8 @@ TEST_F(AccessibilityFocusRingControllerTest, CursorWorksOnMultipleDisplays) {
             50);
 }
 
-TEST_F(AccessibilityFocusRingControllerTest, FocusRingWorksOnMultipleDisplays) {
+// Disabled due to failure. http://crbug.com/1279278
+TEST_F(AccessibilityFocusRingControllerTest, DISABLED_FocusRingWorksOnMultipleDisplays) {
   UpdateDisplay("500x400,600x500");
   aura::Window::Windows root_windows = Shell::Get()->GetAllRootWindows();
   ASSERT_EQ(2u, root_windows.size());
