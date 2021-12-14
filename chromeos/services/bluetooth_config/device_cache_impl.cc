214f933a981badbd5042737dc2b276627a2c36bc
Gordon Seto <gordonseto@google.com>
2021-12-14 19:27:20 +0000

    [CrOS Bluetooth] Hide unknown devices from discovered device list.
    Update DeviceCache to omit adding devices with a device type of
    UNKNOWN to the list of unpaired devices.
    
    Fixed: b/209075547
    Change-Id: I8e66c3e36da7242322b8dc6a9e90da4560405c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331186
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#951600}

--- chromeos/services/bluetooth_config/device_cache_impl.cc
+++ chromeos/services/bluetooth_config/device_cache_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "chromeos/services/bluetooth_config/device_conversion_util.h"
+#include "device/bluetooth/chromeos/bluetooth_utils.h"
 
 namespace chromeos {
 namespace bluetooth_config {
@@ -157,8 +158,10 @@ void DeviceCacheImpl::OnDeviceNicknameChanged(const std::string& device_id,
 }
 
 void DeviceCacheImpl::FetchInitialDeviceLists() {
-  for (const device::BluetoothDevice* device :
-       bluetooth_adapter_->GetDevices()) {
+  device::BluetoothAdapter::DeviceList devices = FilterBluetoothDeviceList(
+      bluetooth_adapter_->GetDevices(), device::BluetoothFilterType::KNOWN,
+      /*max_devices=*/0);
+  for (const device::BluetoothDevice* device : devices) {
     if (device->IsPaired()) {
       paired_devices_.push_back(
           GeneratePairedBluetoothDeviceProperties(device));
@@ -205,7 +208,12 @@ bool DeviceCacheImpl::AttemptUpdatePairedDeviceMetadata(
         return paired_device->device_properties->id;
       });
 
-  // If device is not found in |paired_devices|, don't update.
+  // If device is not found in |paired_devices|, don't update. This is done
+  // because when a paired device is forgotten, it is removed from
+  // |paired_devices|, but then OnDeviceChanged() is called with
+  // device->IsPaired() == true. If we don't have this check here, the device
+  // will be incorrectly added back into |paired_devices|. See
+  // crrev.com/c/3287422.
   if (!device_found)
     return false;
 
@@ -229,6 +237,10 @@ void DeviceCacheImpl::SortPairedDeviceList() {
 bool DeviceCacheImpl::AttemptSetDeviceInUnpairedDeviceList(
     device::BluetoothDevice* device) {
   if (device->IsPaired())
+    return false;
+
+  // Check if the device should be added to the unpaired device list.
+  if (device::IsUnsupportedDevice(device))
     return false;
 
   // Remove the old (stale) properties, if they exist.
@@ -255,16 +267,6 @@ bool DeviceCacheImpl::RemoveFromUnpairedDeviceList(
 
 bool DeviceCacheImpl::AttemptUpdateUnpairedDeviceMetadata(
     device::BluetoothDevice* device) {
-  bool device_found =
-      base::Contains(unpaired_devices_, device->GetIdentifier(),
-                     [](const auto& unpaired_device) {
-                       return unpaired_device->device_properties->id;
-                     });
-
-  // If device is not found in |unpaired_devices|, don't update.
-  if (!device_found)
-    return false;
-
   // Remove existing metadata about |device|.
   bool updated = RemoveFromUnpairedDeviceList(device);
 
--- device/bluetooth/chromeos/bluetooth_utils.h
+++ device/bluetooth/chromeos/bluetooth_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DEVICE_BLUETOOTH_CHROMEOS_BLUETOOTH_UTILS_H_
 
 #include "device/bluetooth/bluetooth_adapter.h"
+#include "device/bluetooth/bluetooth_device.h"
 #include "device/bluetooth/bluetooth_export.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -93,6 +94,11 @@ DEVICE_BLUETOOTH_EXPORT device::BluetoothAdapter::DeviceList
 FilterBluetoothDeviceList(const BluetoothAdapter::DeviceList& devices,
                           BluetoothFilterType filter_type,
                           int max_devices);
+
+// Returns |true| if the device is unsupported and should not be known by the
+// UI.
+DEVICE_BLUETOOTH_EXPORT bool IsUnsupportedDevice(
+    const device::BluetoothDevice* device);
 
 // Record outcome of user attempting to pair to a device.
 DEVICE_BLUETOOTH_EXPORT void RecordPairingResult(
