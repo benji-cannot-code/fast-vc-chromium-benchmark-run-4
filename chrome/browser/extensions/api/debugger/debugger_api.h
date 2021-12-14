0e741d387cce819509933c89d7aa80e4565f10d7
Minoru Chikamune <chikamune@chromium.org>
2021-12-14 23:47:22 +0000

    [Code Health] Modernize Value API in debugger_api.h
    Bug: 1187061
    Change-Id: I228a32001622b89a749eb447e1f95bea2138643d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3334438
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951727}

--- chrome/browser/extensions/api/debugger/debugger_api.h
+++ chrome/browser/extensions/api/debugger/debugger_api.h
@@ -21,10 +21,6 @@ using extensions::api::debugger::Debuggee;
 
 // Base debugger function.
 
-namespace base {
-class DictionaryValue;
-}
-
 namespace extensions {
 class ExtensionDevToolsClientHost;
 
@@ -78,7 +74,7 @@ class DebuggerSendCommandFunction : public DebuggerFunction {
   DECLARE_EXTENSION_FUNCTION("debugger.sendCommand", DEBUGGER_SENDCOMMAND)
 
   DebuggerSendCommandFunction();
-  void SendResponseBody(base::DictionaryValue* result);
+  void SendResponseBody(base::Value result);
   void SendDetachedError();
 
  protected:
