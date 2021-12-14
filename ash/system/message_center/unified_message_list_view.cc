10ec5a7f91ee66c0c64ed174bf80a225d8daad4b
Alex Newcomer <newcomer@chromium.org>
2021-12-14 18:45:25 +0000

    speculative fix for crash
    It's possible that expand_or_collapsing_container_ does not
    exist when the animation is started. Use a default duration
    in that case.
    
    Bug: 1279592
    Change-Id: I92edbc5544081be27bf138b89287a2fb508560ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336862
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951575}

--- ash/system/message_center/unified_message_list_view.cc
+++ ash/system/message_center/unified_message_list_view.cc
@@ -947,7 +947,10 @@ void UnifiedMessageListView::StartAnimation() {
           kExpandOrCollapseAnimationSmoothnessHistogramName);
       DCHECK(expand_or_collapsing_container_);
       animation_duration =
-          expand_or_collapsing_container_->GetBoundsAnimationDuration();
+          expand_or_collapsing_container_
+              ? expand_or_collapsing_container_->GetBoundsAnimationDuration()
+              : base::Milliseconds(
+                    kLargeImageExpandAndCollapseAnimationDuration);
       break;
   }
 
