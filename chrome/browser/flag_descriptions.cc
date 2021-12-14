e47f318276abfc0164f15137879b9c0d5a502d99
Olesia Marukhno <olesiamarukhno@google.com>
2021-12-14 16:55:48 +0000

    [page info] Add history feature flag
    Change-Id: Ie13fc14380570fe20553136a9f7f7c20a5da27c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3332080
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#951532}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1929,6 +1929,10 @@ const char kPageInfoAboutThisSiteName[] =
 const char kPageInfoAboutThisSiteDescription[] =
     "Enable the 'About this site' section in the page info.";
 
+const char kPageInfoHistoryDesktopName[] = "Page info history";
+const char kPageInfoHistoryDesktopDescription[] =
+    "Enable a history section in the page info.";
+
 const char kParallelDownloadingName[] = "Parallel downloading";
 const char kParallelDownloadingDescription[] =
     "Enable parallel downloading to accelerate download speed.";
