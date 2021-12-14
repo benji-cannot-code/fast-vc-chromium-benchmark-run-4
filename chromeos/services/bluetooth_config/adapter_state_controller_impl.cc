38768b98878ac757f8a430c8880d37fc1025918f
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-12-14 23:15:43 +0000

    [CrOS Bluetooth] Add powered metric
    PoweredStateOperationResult was not included in test because
    SetPowered() result callbacks are mocked. It was manually verified
    instead.
    
    Bug: b:207588508
    Change-Id: I1d50087d2309a2f11f68bf082be4fd1d232b4e5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331185
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#951713}

--- chromeos/services/bluetooth_config/adapter_state_controller_impl.cc
+++ chromeos/services/bluetooth_config/adapter_state_controller_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "components/device_event_log/device_event_log.h"
+#include "device/bluetooth/chromeos/bluetooth_utils.h"
 
 namespace chromeos {
 namespace bluetooth_config {
@@ -105,8 +106,7 @@ void AdapterStateControllerImpl::AttemptSetEnabled(bool enabled) {
                      weak_ptr_factory_.GetWeakPtr(), enabled),
       base::BindOnce(&AdapterStateControllerImpl::OnSetPoweredError,
                      weak_ptr_factory_.GetWeakPtr(), enabled));
-  // TODO(gordonseto): Add power metric here.
-
+  device::RecordPoweredState(enabled);
   // State has changed to kEnabling or kDisabling; notify observers.
   NotifyAdapterStateChanged();
 }
@@ -115,6 +115,10 @@ void AdapterStateControllerImpl::OnSetPoweredSuccess(bool enabled) {
   BLUETOOTH_LOG(EVENT) << "Bluetooth " << (enabled ? "enabled" : "disabled")
                        << " successfully";
   in_progress_state_change_ = PowerStateChange::kNoChange;
+  device::PoweredStateOperation power_operation =
+      enabled ? device::PoweredStateOperation::kEnable
+              : device::PoweredStateOperation::kDisable;
+  device::RecordPoweredStateOperationResult(power_operation, /*success=*/true);
 
   // Adapter->IsPowered() won't immediately be updated to the new value when
   // SetPowered() finishes and this method is called. Don't call
@@ -126,7 +130,10 @@ void AdapterStateControllerImpl::OnSetPoweredSuccess(bool enabled) {
 void AdapterStateControllerImpl::OnSetPoweredError(bool enabled) {
   BLUETOOTH_LOG(ERROR) << "Error attempting to "
                        << (enabled ? "enable" : "disable") << " Bluetooth";
-  // TODO(gordonseto): Add power metric here.
+  device::PoweredStateOperation power_operation =
+      enabled ? device::PoweredStateOperation::kEnable
+              : device::PoweredStateOperation::kDisable;
+  device::RecordPoweredStateOperationResult(power_operation, /*success=*/false);
   in_progress_state_change_ = PowerStateChange::kNoChange;
 
   // State is no longer kEnabling or kDisabling; notify observers.
