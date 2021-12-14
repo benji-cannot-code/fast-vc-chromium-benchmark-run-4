c9567cd3971aef8314e5663fe600504c817efb0b
Lijin Shen <lazzzis@google.com>
2021-12-14 00:10:46 +0000

    Enable Andorid Popup Blocked Message by default
    Bug: 1179462
    Change-Id: Iff12c83512ac1526e231422773e15b1f90883247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3335583
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#951265}

--- components/messages/android/messages_feature.cc
+++ components/messages/android/messages_feature.cc
@@ -36,7 +36,7 @@ const base::Feature kMessagesForAndroidPermissionUpdate{
     "MessagesForAndroidPermissionUpdate", base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kMessagesForAndroidPopupBlocked{
-    "MessagesForAndroidPopupBlocked", base::FEATURE_DISABLED_BY_DEFAULT};
+    "MessagesForAndroidPopupBlocked", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kMessagesForAndroidReaderMode{
     "MessagesForAndroidReaderMode", base::FEATURE_ENABLED_BY_DEFAULT};
