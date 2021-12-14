655fb6b8ab10a0c7722c2f24b951e1082677b656
Florian Gauger <fga@google.com>
2021-12-14 09:52:03 +0000

    [Autofill Assistant] Pass in IsGsa function
    Removes IntentHandler Chrome dependency.
    
    We are in the process of moving all Autofill Assistant code to //components. For this we need to remove all dependencies on Chrome code. In this cl we pass in a generic interface without dependencies and supply the chrome-specific implementation at the call side.
    
    Bug: b/173103628
    Change-Id: Ic5dbfd15f512a51cb97efaa1bf04b50fd69b624e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3303813
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Auto-Submit: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#951427}

--- chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/Starter.java
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/Starter.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill_assistant;
 
-import android.content.Intent;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -14,8 +12,6 @@ import org.chromium.base.UserData;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
-import org.chromium.chrome.browser.IntentHandler;
-import org.chromium.chrome.browser.IntentHandler.ExternalAppId;
 import org.chromium.chrome.browser.autofill_assistant.metrics.FeatureModuleInstallation;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.services.UnifiedConsentServiceBridge;
@@ -36,6 +32,8 @@ import java.util.Map;
 public class Starter extends EmptyTabObserver implements UserData {
     /** The tab that this starter tracks. */
     private final Tab mTab;
+
+    private final AssistantIsGsaFunction mIsGsaFunction;
 
     /**
      * The WebContents associated with the Tab which this starter is monitoring, unless detached.
@@ -68,8 +66,9 @@ public class Starter extends EmptyTabObserver implements UserData {
      *
      * This will wait for dependencies to become available and then create the native-side starter.
      */
-    public Starter(Tab tab) {
+    public Starter(Tab tab, AssistantIsGsaFunction isGsaFunction) {
         mTab = tab;
+        mIsGsaFunction = isGsaFunction;
         detectWebContentsChange(tab);
     }
 
@@ -310,16 +309,7 @@ public class Starter extends EmptyTabObserver implements UserData {
 
     @CalledByNative
     private boolean getIsTabCreatedByGSA() {
-        // This can fail for certain tabs (e.g., hidden background tabs).
-        if (TabUtils.getActivity(mTab) == null) {
-            return false;
-        }
-        Intent intent = TabUtils.getActivity(mTab).getIntent();
-        if (intent == null) {
-            // This should never happen, this is just a failsafe.
-            return false;
-        }
-        return IntentHandler.determineExternalIntentSource(intent) == ExternalAppId.GSA;
+        return mIsGsaFunction.apply(TabUtils.getActivity(mTab));
     }
 
     @NativeMethods
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabHelpers.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabHelpers.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab;
 
 import org.chromium.chrome.browser.SwipeRefreshHandler;
+import org.chromium.chrome.browser.autofill_assistant.AssistantDependencyUtilsChrome;
 import org.chromium.chrome.browser.autofill_assistant.AutofillAssistantTabHelper;
 import org.chromium.chrome.browser.complex_tasks.TaskTabHelper;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchTabHelper;
@@ -38,7 +39,7 @@ public final class TabHelpers {
         TabBrowserControlsConstraintsHelper.createForTab(tab);
         ContinuousSearchTabHelper.createForTab(tab);
         if (ReaderModeManager.isEnabled()) ReaderModeManager.createForTab(tab);
-        AutofillAssistantTabHelper.createForTab(tab);
+        AutofillAssistantTabHelper.createForTab(tab, AssistantDependencyUtilsChrome::isGsa);
 
         // The following will start prefetching data for the price drops feature, so
         // we should only do it if the user is eligible for the feature (e.g. has sync enabled).
