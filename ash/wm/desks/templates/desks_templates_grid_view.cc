1b5ee8fea4030851ff06fe5013d33b02a1d49d7e
Yuki Shiino <yukishiino@chromium.org>
2021-12-14 01:59:07 +0000

    Revert "desk_templates: Animation for exiting overview while templates grid open"
    This reverts commit 4768602afcbc03c1aa3b76777e3515fa4e83c41d.
    
    Reason for revert: Suspicious about causing test failures of:
    DesksTemplatesTest.OverviewItemsStayHiddenInTemplateGridOnDeskClose
    DesksTemplatesTest.RecordDesksTemplateGridShows
    DesksTemplatesTest.DesksTemplatesButtonBorderColor
    DesksTemplatesTest.OverflowIconView
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/53363/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/53364/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/53365/overview
    
    Original change's description:
    > desk_templates: Animation for exiting overview while templates grid open
    >
    > This CL makes it so when a user exits overview with the desk templates
    > grid open, the desk templates grid first fades out then fades in the
    > overview items and blurs out the overview wallpaper.
    >
    > Bug: 1275232
    > Change-Id: I8141526cfb55d873586084dc8d4ba83623270b48
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3328303
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#951231}
    
    Bug: 1275232
    Change-Id: Idfed645656e367070b1e9672d425710d83f38102
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336820
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951310}

--- ash/wm/desks/templates/desks_templates_grid_view.cc
+++ ash/wm/desks/templates/desks_templates_grid_view.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/public/cpp/shell_window_ids.h"
-#include "ash/wm/desks/templates/desks_templates_animations.h"
 #include "ash/wm/desks/templates/desks_templates_item_view.h"
 #include "ash/wm/desks/templates/desks_templates_presenter.h"
 #include "ui/aura/window.h"
@@ -17,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer.h"
 #include "ui/events/event_handler.h"
 #include "ui/views/layout/table_layout.h"
+#include "ui/views/widget/unique_widget_ptr.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
@@ -65,8 +65,8 @@ DesksTemplatesGridView::DesksTemplatesGridView() = default;
 DesksTemplatesGridView::~DesksTemplatesGridView() = default;
 
 // static
-std::unique_ptr<views::Widget>
-DesksTemplatesGridView::CreateDesksTemplatesGridWidget(aura::Window* root) {
+views::UniqueWidgetPtr DesksTemplatesGridView::CreateDesksTemplatesGridWidget(
+    aura::Window* root) {
   DCHECK(root);
   DCHECK(root->IsRootWindow());
 
@@ -74,7 +74,6 @@ DesksTemplatesGridView::CreateDesksTemplatesGridWidget(aura::Window* root) {
       views::Widget::InitParams::TYPE_WINDOW_FRAMELESS);
   params.activatable = views::Widget::InitParams::Activatable::kYes;
   params.accept_events = true;
-  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   // The parent should be a container that covers all the windows but is below
   // some other system UI features such as system tray and capture mode and also
   // below the system modal dialogs.
@@ -83,7 +82,8 @@ DesksTemplatesGridView::CreateDesksTemplatesGridWidget(aura::Window* root) {
   params.parent = root->GetChildById(kShellWindowId_ShelfBubbleContainer);
   params.name = "DesksTemplatesGridWidget";
 
-  auto widget = std::make_unique<views::Widget>(std::move(params));
+  views::UniqueWidgetPtr widget(
+      std::make_unique<views::Widget>(std::move(params)));
   widget->SetContentsView(std::make_unique<DesksTemplatesGridView>());
 
   // Not opaque since we want to view the contents of the layer behind.
