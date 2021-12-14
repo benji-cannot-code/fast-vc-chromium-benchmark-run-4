122a541e03481148c930747556b9c11844e2f4c2
Maksim Ivanov <emaxx@chromium.org>
2021-12-14 18:09:32 +0000

    Fix use-after-move in CdmInfo
    Fix use-after-move issues in debug assertions in CdmInfo, as found by
    clang-tidy:
    
      content/common/media/cdm_info.cc:29:
      'capability' used after it was moved
      content/common/media/cdm_info.cc:22:
      move occurred here
    
      content/common/media/cdm_info.cc:40:
      'capability' used after it was moved
      content/common/media/cdm_info.cc:38:
      move occurred here
    
    Bug: 1122844
    Change-Id: If41e1af08cc38652ac3772c9051f2b1c2aadbf73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336056
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951556}

--- content/common/media/cdm_info.cc
+++ content/common/media/cdm_info.cc
@@ -27,7 +27,7 @@ CdmInfo::CdmInfo(const std::string& key_system,
       version(version),
       path(path),
       file_system_id(file_system_id) {
-  DCHECK(!capability || !capability->encryption_schemes.empty());
+  DCHECK(!this->capability || !this->capability->encryption_schemes.empty());
 }
 
 CdmInfo::CdmInfo(const std::string& key_system,
@@ -38,7 +38,7 @@ CdmInfo::CdmInfo(const std::string& key_system,
       robustness(robustness),
       capability(std::move(capability)),
       type(type) {
-  DCHECK(!capability || !capability->encryption_schemes.empty());
+  DCHECK(!this->capability || !this->capability->encryption_schemes.empty());
 }
 
 CdmInfo::CdmInfo(const CdmInfo& other) = default;
