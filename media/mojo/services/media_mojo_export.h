4d4ec96da801ebc9bc82619a38249788560623e5
Xiaohan Wang <xhwang@chromium.org>
2021-12-14 07:31:00 +0000

    media: Update MEDIA_MOJO_EXPORT
    Define it using COMPONENT_EXPORT which is recommended.
    
    Change-Id: I79c407d3de532c4b3cd082a78076a711e9a56f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3337087
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951396}

--- media/mojo/services/media_mojo_export.h
+++ media/mojo/services/media_mojo_export.h
@@ -6,28 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_MOJO_SERVICES_MEDIA_MOJO_EXPORT_H_
 #define MEDIA_MOJO_SERVICES_MEDIA_MOJO_EXPORT_H_
 
-// Define MEDIA_MOJO_EXPORT so that functionality implemented by the
-// media/mojo module can be exported to consumers.
+#include "base/component_export.h"
 
-#if defined(COMPONENT_BUILD)
-#if defined(WIN32)
-
-#if defined(MEDIA_MOJO_IMPLEMENTATION)
-#define MEDIA_MOJO_EXPORT __declspec(dllexport)
-#else
-#define MEDIA_MOJO_EXPORT __declspec(dllimport)
-#endif  // defined(MEDIA_IMPLEMENTATION)
-
-#else  // defined(WIN32)
-#if defined(MEDIA_MOJO_IMPLEMENTATION)
-#define MEDIA_MOJO_EXPORT __attribute__((visibility("default")))
-#else
-#define MEDIA_MOJO_EXPORT
-#endif
-#endif
-
-#else  // defined(COMPONENT_BUILD)
-#define MEDIA_MOJO_EXPORT
-#endif
+#define MEDIA_MOJO_EXPORT COMPONENT_EXPORT(MEDIA_MOJO)
 
 #endif  // MEDIA_MOJO_SERVICES_MEDIA_MOJO_EXPORT_H_
