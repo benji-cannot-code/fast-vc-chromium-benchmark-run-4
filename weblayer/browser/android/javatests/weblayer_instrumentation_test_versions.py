03513587630ccbe54229ad1b97cc8d148c9043db
Rakib M. Hasan <rmhasan@google.com>
2021-12-14 17:58:46 +0000

    WebLayer: Run skew tests with python3
    Bug: None
    Change-Id: Ia80f524b3a0a94fc6f172f2119d5ee11bc738bd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336866
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/main@{#951554}

--- weblayer/browser/android/javatests/weblayer_instrumentation_test_versions.py
+++ weblayer/browser/android/javatests/weblayer_instrumentation_test_versions.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env vpython
+#!/usr/bin/env vpython3
 #
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -114,7 +114,7 @@ def tests_to_skip(expectation_contents, impl_version='trunk',
             'Only "Skip" is supported in the skew test expectations.')
 
     # Iterate over the first (and only) item since can't index over a frozenset.
-    tag = iter(expectation.tags).next()
+    tag = next(iter(expectation.tags))
     if tag_matches(tag, impl_version, client_version):
       tests.append(expectation.test)
   return tests
