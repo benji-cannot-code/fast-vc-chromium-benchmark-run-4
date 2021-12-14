c941d37385e076702c35a4c45ac48aeb709ba480
Jack Hsieh <chengweih@chromium.org>
2021-12-14 20:19:34 +0000

    bluetooth: bluetooth server tests to standalone tests
    This change updates some of the tests in wpt/bluetooth/server to be
    standalone window tests. This change allows the tests to be formatted
    with clang-format.
    
    Additionally, fix
    wpt/bluetooth/server/device-same-object.https.window.html to compare
    with the correct device object.
    
    Bug: 994756
    Change-Id: I3276cdc221fded0c14656c806458096fa1a72093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3335577
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951620}

---
+++ third_party/blink/web_tests/external/wpt/bluetooth/server/getPrimaryServices/services-not-found.https.window.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testharness.js
+// META: script=/resources/testharnessreport.js
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+// META: script=/bluetooth/resources/bluetooth-test.js
+// META: script=/bluetooth/resources/bluetooth-fake-devices.js
+'use strict';
+const test_desc = 'Request for services in a device with no services. Reject ' +
+    'with NotFoundError.';
+const expected =
+    new DOMException('No Services found in device.', 'NotFoundError');
+
+bluetooth_test(async () => {
+  let {device} = await getEmptyHealthThermometerDevice();
+  return assert_promise_rejects_with_message(
+      device.gatt.getPrimaryServices(), expected);
+}, test_desc);
