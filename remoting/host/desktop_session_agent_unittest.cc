c4744006f49758d17a4c6e5e5c8acbedf6d9b6d3
Joe Downing <joedow@google.com>
2021-12-14 20:56:37 +0000

    Move DesktopEnvironmentOptions to //remoting/host/base
    This CL moves a common header+impl to the new base folder as it will
    be referenced in a follow-up IPC macro conversion CL. As part of this
    change I am removing the Windows dependency that class has as it calls
    IsD3DAvailable() in the c'tor which is a blocking call. It doesn't
    seem right that creating an instance should spin up another process
    and block on the results (especially for what appears to be a simple
    struct-like class).
    
    Change-Id: I01d1aea8ae6ee99df1f14bf7186dbabd8becf841
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336416
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951639}

--- remoting/host/desktop_session_agent_unittest.cc
+++ remoting/host/desktop_session_agent_unittest.cc
@@ -20,9 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_channel_proxy.h"
 #include "ipc/ipc_listener.h"
 #include "remoting/base/auto_thread_task_runner.h"
+#include "remoting/host/base/desktop_environment_options.h"
 #include "remoting/host/base/screen_resolution.h"
 #include "remoting/host/chromoting_messages.h"
-#include "remoting/host/desktop_environment_options.h"
 #include "remoting/host/fake_desktop_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
