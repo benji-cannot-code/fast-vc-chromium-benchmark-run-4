581bb9965fa7e9a65e4f62fb4b7023fa9b84ad1e
Emilia Paz <emiliapaz@chromium.org>
2021-12-14 02:34:56 +0000

    [Extensions c2s] Update installed extensions on tab changes
    Extension items can change in response to tab changes (such as the
    active tab, if a tab changes title or location, etc). Update the
    extensions menu in response to tab changes.
    
    Bug: 1263311
    Change-Id: I76bffe284bc6ada8b878e9c7fc42e7a34c32152d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331498
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951329}

--- chrome/browser/ui/views/extensions/extensions_tabbed_menu_view.h
+++ chrome/browser/ui/views/extensions/extensions_tabbed_menu_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/extensions/extension_action_view_controller.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
 #include "chrome/browser/ui/views/extensions/extensions_toolbar_button.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -24,6 +25,7 @@ class ExtensionsContainer;
 // TODO(crbug.com/1263311): Brief explanation of each tabs goal after
 // implementing them.
 class ExtensionsTabbedMenuView : public views::BubbleDialogDelegateView,
+                                 public TabStripModelObserver,
                                  public ToolbarActionsModel::Observer {
  public:
   METADATA_HEADER(ExtensionsTabbedMenuView);
@@ -67,6 +69,15 @@ class ExtensionsTabbedMenuView : public views::BubbleDialogDelegateView,
   // views::BubbleDialogDelegateView:
   std::u16string GetAccessibleWindowTitle() const override;
 
+  // TabStripModelObserver:
+  void TabChangedAt(content::WebContents* contents,
+                    int index,
+                    TabChangeType change_type) override;
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
+
   // ToolbarActionsModel::Observer:
   void OnToolbarActionAdded(
       const ToolbarActionsModel::ActionId& action_id) override;
@@ -80,6 +91,9 @@ class ExtensionsTabbedMenuView : public views::BubbleDialogDelegateView,
  private:
   // Initially creates the tabs.
   void Populate();
+
+  // Updates the menu.
+  void Update();
 
   // Adds a menu item in the installed extensions for a newly-added extension.
   void CreateAndInsertInstalledExtension(
