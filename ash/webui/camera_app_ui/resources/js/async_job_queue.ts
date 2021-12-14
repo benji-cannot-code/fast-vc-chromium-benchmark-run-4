c25fe4710ae065ca0852a577787a7db0a6f74f36
Pi-Hsun Shih <pihsun@chromium.org>
2021-12-14 05:23:25 +0000

    CCA: Migrate async_job_queue.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: I16b14757225777b6a921494662d3849f7211e911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3290992
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951371}

---
+++ ash/webui/camera_app_ui/resources/js/async_job_queue.ts
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Asynchronous job queue.
+ */
+export class AsyncJobQueue {
+  private promise: Promise<unknown> = Promise.resolve();
+  private clearing = false;
+
+  /**
+   * Pushes the given job into queue.
+   * @return Resolved with the job return value when the job is finished, or
+   *     null if the job is cleared.
+   */
+  push<T>(job: () => Promise<T>): Promise<T|null> {
+    const promise: Promise<T|null> = this.promise.then(() => {
+      if (this.clearing) {
+        return null;
+      }
+      return job();
+    });
+    this.promise = promise;
+    return promise;
+  }
+
+  /**
+   * Flushes the job queue.
+   * @return Resolved when all jobs in the queue are finished.
+   */
+  async flush(): Promise<void> {
+    await this.promise;
+  }
+
+  /**
+   * Clears all not-yet-scheduled jobs and waits for current job finished.
+   */
+  async clear(): Promise<void> {
+    this.clearing = true;
+    await this.flush();
+    this.clearing = false;
+  }
+}
