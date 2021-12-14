df93aaee55b91f0b4ffa26f675c146de75674ba7
Xinghui Lu <xinghuilu@chromium.org>
2021-12-14 03:13:59 +0000

    Enable SafeBrowsingPageLoadToken by default.
    Bug: 1240403
    Change-Id: Iab716d14af3fef008537d829b38dec47d1837b1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312435
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951344}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -120,7 +120,7 @@ const base::Feature kRealTimeUrlLookupReferrerChainForEnterprise{
     base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSafeBrowsingPageLoadToken{
-    "SafeBrowsingPageLoadToken", base::FEATURE_DISABLED_BY_DEFAULT};
+    "SafeBrowsingPageLoadToken", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature
     kSafeBrowsingPasswordCheckIntegrationForSavedPasswordsAndroid{
