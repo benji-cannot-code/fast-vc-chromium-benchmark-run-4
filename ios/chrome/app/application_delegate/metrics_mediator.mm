86c47eabbab6d06f9cfc49485706e01c33e419d8
Jérôme Lebel <jlebel@chromium.org>
2021-12-14 18:12:16 +0000

    [iOS] Add device restore metrics
    This patch adds histograms related to iOS device restore. This is based
    on the function: IsFirstSessionAfterDeviceRestore(), implemented in:
    crrev.com/c/3320241
    
    The following histograms are added:
    * Startup.IOSColdStartType
    Records session type for a cold start.
    
    * Signin.IOSDeviceRestoreSignedinState
     Records the sign-in state before and after an iOS device restore.
    
    * Signin.IOSDeviceRestoreIdentityCountBeforeUpgrade
     Records the number of identities before iOS device restore.
    
    * Signin.IOSDeviceRestoreIdentityCountAfterUpgrade
     Records the number of identities before iOS device restore.
    
    SigninSignoutProfile enum is updated to add:
     Sign-out forced because the account was removed from the device after
     a device restore.
    
    Fixed: 1266358
    Change-Id: I82992d9f0571fbbb2b5b8111b7f6764df6b68e92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320276
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Hao Dong <spdonghao@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951561}

--- ios/chrome/app/application_delegate/metrics_mediator.mm
+++ ios/chrome/app/application_delegate/metrics_mediator.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/metrics_service.h"
 #include "components/prefs/pref_service.h"
 #import "components/previous_session_info/previous_session_info.h"
+#import "components/signin/public/identity_manager/tribool.h"
 #include "components/ukm/ios/ukm_reporting_ios_util.h"
 #import "ios/chrome/app/application_delegate/metric_kit_subscriber.h"
 #import "ios/chrome/app/application_delegate/startup_information.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/main/browser.h"
 #include "ios/chrome/browser/metrics/first_user_action_recorder.h"
 #include "ios/chrome/browser/pref_names.h"
+#import "ios/chrome/browser/signin/signin_util.h"
 #include "ios/chrome/browser/system_flags.h"
 #import "ios/chrome/browser/ui/browser_view/browser_view_controller.h"
 #import "ios/chrome/browser/ui/default_promo/default_browser_utils.h"
@@ -77,6 +79,26 @@ const metrics_mediator::HistogramNameCountPair kHistogramsFromExtension[] = {
         @"IOS.CredentialExtension.NewCredentialUsername",
         static_cast<int>(CPENewCredentialUsername::kMaxValue) + 1,
     }};
+
+// Enum values for Startup.IOSColdStartType histogram.
+// Entries should not be renumbered and numeric values should never be reused.
+enum class ColdStartType : int {
+  // Regular cold start.
+  kRegular = 0,
+  // Cold start with FRE.
+  kFirstRun = 1,
+  // Cold start after a device restore.
+  kAfterDeviceRestore = 2,
+  // Cold start after a Chrome upgrade.
+  kAfterChromeUpgrade = 3,
+  // Cold start after a device restore and Chrome upgrade.
+  kAfterDeviceRestoreAndChromeUpgrade = 4,
+  // Unknown device restore.
+  kUnknownDeviceRestore = 5,
+  // Unknown device restore and Chrome upgrade.
+  kUnknownDeviceRestoreAndChromeUpgrade = 6,
+  kMaxValue = kUnknownDeviceRestoreAndChromeUpgrade,
+};
 
 // Returns time delta since app launch as retrieved from kernel info about
 // the current process.
@@ -426,6 +448,34 @@ using metrics_mediator::kAppDidFinishLaunchingConsecutiveCallsKey;
     [[NSUserDefaults standardUserDefaults]
         removeObjectForKey:kAppEnteredBackgroundDateKey];
   }
+  if (!startupInformation.isColdStart) {
+    return;
+  }
+  signin::Tribool device_restore = IsFirstSessionAfterDeviceRestore();
+  ColdStartType sessionType;
+  if (startupInformation.isFirstRun) {
+    sessionType = ColdStartType::kFirstRun;
+  } else {
+    bool afterUpgrade =
+        [PreviousSessionInfo sharedInstance].isFirstSessionAfterUpgrade;
+    switch (device_restore) {
+      case signin::Tribool::kUnknown:
+        sessionType = afterUpgrade
+                          ? ColdStartType::kUnknownDeviceRestoreAndChromeUpgrade
+                          : ColdStartType::kUnknownDeviceRestore;
+        break;
+      case signin::Tribool::kTrue:
+        sessionType = afterUpgrade
+                          ? ColdStartType::kAfterDeviceRestoreAndChromeUpgrade
+                          : ColdStartType::kAfterDeviceRestore;
+        break;
+      case signin::Tribool::kFalse:
+        sessionType = afterUpgrade ? ColdStartType::kAfterChromeUpgrade
+                                   : ColdStartType::kRegular;
+        break;
+    }
+  }
+  base::UmaHistogramEnumeration("Startup.IOSColdStartType", sessionType);
 }
 
 - (void)updateMetricsStateBasedOnPrefsUserTriggered:(BOOL)isUserTriggered {
