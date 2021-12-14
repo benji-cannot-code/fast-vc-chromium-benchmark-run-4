8ee4a59b4100651b857a64afdb525197d4d730d3
Howard Yang <hcyang@google.com>
2021-12-14 03:43:00 +0000

    fido/cros: Fix comments in authenticator.cc
    The logic to determine if USER_PRESENCE or USER_VERIFICATION request will
    be sent to u2fd currently doesn't match its comment. The behavior is
    correct but the comment isn't. The correct comment should be "Requests
    will be UserPresence if uv is discouraged and power button is enabled,
    and be UserVerification otherwise."
    
    Bug: None
    Test: None
    Change-Id: I17df97ba8c2d5ae25da02108fbce6df81e21428e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3329056
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Howard Yang <hcyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#951349}

--- device/fido/cros/authenticator.cc
+++ device/fido/cros/authenticator.cc
@@ -76,8 +76,9 @@ void ChromeOSAuthenticator::MakeCredential(
     MakeCredentialOptions request_options,
     MakeCredentialCallback callback) {
   u2f::MakeCredentialRequest req;
-  // Requests will be UserPresence if uv is not available or discouraged,
-  // and be UserVerification otherwise.
+  // Only allow skipping user verification if user presence checks via power
+  // button press have been configured. This is only the case when running with
+  // the "DeviceSecondFactorAuthentication" enterprise policy.
   req.set_verification_type(
       (request.user_verification == UserVerificationRequirement::kDiscouraged &&
        config_.power_button_enabled)
@@ -189,8 +190,9 @@ void ChromeOSAuthenticator::GetAssertion(CtapGetAssertionRequest request,
                                          CtapGetAssertionOptions options,
                                          GetAssertionCallback callback) {
   u2f::GetAssertionRequest req;
-  // Requests will be UserPresence if uv is not available or discouraged,
-  // and be UserVerification otherwise.
+  // Only allow skipping user verification if user presence checks via power
+  // button press have been configured. This is only the case when running with
+  // the "DeviceSecondFactorAuthentication" enterprise policy.
   req.set_verification_type(
       (request.user_verification == UserVerificationRequirement::kDiscouraged &&
        config_.power_button_enabled)
