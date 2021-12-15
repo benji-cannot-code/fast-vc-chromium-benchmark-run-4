d05d86fef5630a3ef8273f67e28ce4e8f14d81d0
Sammie Quon <sammiequon@chromium.org>
2021-12-15 21:58:27 +0000

    desk_templates: Fix hit test and alignment for the name view in RTL.
    - Use mirrored bounds for the view targeter.
    - Align right in RTL, by removing align left. RenderText will check
      which alignment to use if none is specified.
    
    Test: manual, ash_unittests DesksTemplatesTest.TemplatesNameHitTest
    Change-Id: I135faec3439524262ecd47854acb2983b68e73b0
    Fixed: 1278150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340056
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952107}

--- ash/wm/desks/desk_name_view.cc
+++ ash/wm/desks/desk_name_view.cc
@@ -43,8 +43,11 @@ bool IsDesksBarWidget(const views::Widget* widget) {
 }  // namespace
 
 DeskNameView::DeskNameView(DeskMiniView* mini_view) : mini_view_(mini_view) {
-  SetBorder(
-      views::CreateEmptyBorder(gfx::Insets(0, kDeskNameViewHorizontalPadding)));
+  views::Builder<DeskNameView>(this)
+      .SetBorder(views::CreateEmptyBorder(
+          gfx::Insets(0, kDeskNameViewHorizontalPadding)))
+      .SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_CENTER)
+      .BuildChildren();
 }
 
 DeskNameView::~DeskNameView() = default;
