69aeb9a073ab4ec111d1bae19aa18a018a5b20f3
Jérôme Lebel <jlebel@chromium.org>
2021-12-15 09:31:52 +0000

    [iOS] Fix TableCellCatalogViewController
    Fix DCHECK when scrolling through TableCellCatalogViewController.
    
    Change-Id: Ifce1ffdfdb5bb42fb1f0cb6675d7cb728fbf653c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3338360
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951866}

--- ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
+++ ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.h"
 
 #include "base/mac/foundation_util.h"
+#import "ios/chrome/browser/net/crurl.h"
 #import "ios/chrome/browser/signin/constants.h"
 #import "ios/chrome/browser/signin/signin_util.h"
 #import "ios/chrome/browser/ui/authentication/cells/signin_promo_view_configurator.h"
@@ -483,6 +484,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
       [[TableViewLinkHeaderFooterItem alloc] initWithType:ItemTypeLinkFooter];
   linkFooter.text =
       @"This is a footer text view with a BEGIN_LINKlinkEND_LINK in the middle";
+  linkFooter.urls =
+      @[ [[CrURL alloc] initWithGURL:GURL("http://www.example.com")] ];
   [model setFooter:linkFooter
       forSectionWithIdentifier:SectionIdentifierSettings];
 
