213c901dd02c1bc4b0b62e8fcbc388065742bc2b
Lei Zhang <thestig@chromium.org>
2021-12-15 03:26:58 +0000

    Move base::BrokenClampThatShouldNotBeUsed() to the file that uses it.
    Move it out of base/numerics/ranges.h and remove some unnecessary
    base/numerics/ranges.h includes / DEPS entries.
    
    Bug: 1231569
    Change-Id: I3690d6fecf8462cefea2d4aa160baeceef77ee4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331667
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951801}

--- chrome/browser/ash/power/auto_screen_brightness/gaussian_trainer.cc
+++ chrome/browser/ash/power/auto_screen_brightness/gaussian_trainer.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/numerics/ranges.h"
 #include "chrome/browser/ash/power/auto_screen_brightness/utils.h"
 
 namespace ash {
