6cdda77ac1ed3c791a8f61af38192daf6fea1456
Charlie Reis <creis@chromium.org>
2021-12-15 16:39:40 +0000

    Cleanup: Remove unnecessary url.is_valid() check in process reuse.
    This was necessary for extension logic in the function, which has
    since been removed.
    
    Bug: 1209417
    Change-Id: I637943a8d8335346037b8bf5a727653deada7ef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3339383
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Charles Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951971}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1933,11 +1933,6 @@ size_t ChromeContentBrowserClient::GetProcessCountToIgnoreForLimit() {
 bool ChromeContentBrowserClient::ShouldTryToUseExistingProcessHost(
     content::BrowserContext* browser_context,
     const GURL& url) {
-  // Only proceed for valid URLs.
-  // TODO(creis): This may no longer be needed.
-  if (!url.is_valid())
-    return false;
-
   // Top Chrome WebUI should try to share a RenderProcessHost with other
   // existing Top Chrome WebUI.
   if (IsTopChromeWebUIURL(url))
