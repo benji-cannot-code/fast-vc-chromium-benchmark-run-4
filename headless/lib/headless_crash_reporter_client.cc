ba88e7240e9464e6095395ee51ba32101c7a4410
Joshua Peraza <jperaza@chromium.org>
2021-12-15 03:03:38 +0000

    headless,linux: migrate from breakpad to crashpad
    When enabled, Breakpad would produce dumps but only upload them for
    official builds if no CHROME_HEADLESS environment variable existed.
    
    Crashpad maintains this behavior by overriding
    HeadlessCrashReporterClient::IsRunningUnattended(), and
    GetCollectStatsConsent(). GetCollectStatsConsent() is implied true by
    --enable-crash-reporter passed to headless_shell, or --headless passed
    to chrome. Crashpad will internally also only enable upload for
    official, google-branded builds.
    
    Removes use of kDisableBreakpad which previously would disable
    Breakpad/Linux or Crashpad/Mac, but not Crashpad/Windows. It is
    duplicative of --disable-crash-reporter. This patch also causes
    --disable-crash-reporter to function for mac, but still not windows.
    
    Bug: 1176772
    Change-Id: I651ae30e3c90ec90652743b92c55c63c84b53fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2893222
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951799}

--- headless/lib/headless_crash_reporter_client.cc
+++ headless/lib/headless_crash_reporter_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/environment.h"
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
@@ -32,23 +33,12 @@ HeadlessCrashReporterClient::~HeadlessCrashReporterClient() = default;
 
 #if defined(OS_POSIX) && !defined(OS_MAC)
 void HeadlessCrashReporterClient::GetProductNameAndVersion(
-    const char** product_name,
-    const char** version) {
-  *product_name = kChromeHeadlessProductName;
-  *version = PRODUCT_VERSION;
-}
-
-void HeadlessCrashReporterClient::GetProductNameAndVersion(
     std::string* product_name,
     std::string* version,
     std::string* channel) {
   *product_name = kChromeHeadlessProductName;
   *version = PRODUCT_VERSION;
   *channel = "";
-}
-
-base::FilePath HeadlessCrashReporterClient::GetReporterLogFilename() {
-  return base::FilePath(FILE_PATH_LITERAL("uploads.log"));
 }
 #endif  // defined(OS_POSIX) && !defined(OS_MAC)
 
@@ -65,6 +55,9 @@ bool HeadlessCrashReporterClient::GetCrashDumpLocation(
       !base::PathService::Get(base::DIR_MODULE, &crash_directory)) {
     return false;
   }
+  if (crash_dumps_dir_.empty()) {
+    crash_directory = crash_directory.Append(FILE_PATH_LITERAL("Crashpad"));
+  }
 #if defined(OS_WIN)
   *crash_dir = crash_directory.value();
 #else
@@ -73,12 +66,20 @@ bool HeadlessCrashReporterClient::GetCrashDumpLocation(
   return true;
 }
 
-bool HeadlessCrashReporterClient::EnableBreakpadForProcess(
-    const std::string& process_type) {
-  return process_type == ::switches::kRendererProcess ||
-         process_type == ::switches::kPpapiPluginProcess ||
-         process_type == ::switches::kZygoteProcess ||
-         process_type == ::switches::kGpuProcess;
+bool HeadlessCrashReporterClient::IsRunningUnattended() {
+  // CHROME_HEADLESS is not equivalent to running in headless mode. This
+  // environment variable is set in non-production environments which might be
+  // running with crash-dumping enabled. It is used to disable certain dialogs
+  // and in this particular usage disables crash report upload, while leaving
+  // dumping enabled.
+  std::unique_ptr<base::Environment> env(base::Environment::Create());
+  return env->HasVar("CHROME_HEADLESS");
+}
+
+bool HeadlessCrashReporterClient::GetCollectStatsConsent() {
+  // Headless has no way to configure this setting so consent is implied by
+  // the crash reporter being enabled.
+  return true;
 }
 
 }  // namespace content
