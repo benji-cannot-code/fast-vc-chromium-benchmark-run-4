841d6d177a61a9e4bbad2e5f920f3ce8a9f352d5
Etienne Bergeron <etienneb@chromium.org>
2021-12-15 20:40:11 +0000

    Remove unused NTPUserDataLogger from search_tab_helper
    The variable was introduced here:
      https://chromium-review.googlesource.com/c/chromium/src/+/2586281/12/chrome/browser/ui/search/search_tab_helper.cc#280
    
    But the function using the logger got removed here:
      https://chromium-review.googlesource.com/c/chromium/src/+/3193032/5/chrome/browser/ui/search/search_tab_helper.cc#b322
    
    This function no longer exists:
      SearchTabHelper::OnLogEvent(...) {
        if (logger_)
          logger_->LogEvent(event, time);
      }
    
    R=tiborg@chromium.org,aee@chromium.org
    R=gab@chromium.org
    
    Bug: 1279860
    Change-Id: Ia6046515d2b6229133795b2492679d32a7ddd0b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340012
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952064}

--- chrome/browser/ui/search/search_tab_helper.cc
+++ chrome/browser/ui/search/search_tab_helper.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
 #include "chrome/browser/ui/location_bar/location_bar.h"
-#include "chrome/browser/ui/search/ntp_user_data_logger.h"
 #include "chrome/browser/ui/search/omnibox_utils.h"
 #include "chrome/browser/ui/search/search_ipc_router_policy_impl.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
@@ -255,23 +254,8 @@ void SearchTabHelper::NavigationEntryCommitted(
   if (!load_details.is_main_frame)
     return;
 
-  if (search::IsInstantNTP(web_contents())) {
-    // We (re)create the logger here because
-    // 1. The logger tries to detect whether the NTP is being created at startup
-    //    or from the user opening a new tab, and if we wait until later, it
-    //    won't correctly detect this case.
-    // 2. There can be multiple navigations to NTPs in a single web contents.
-    //    The navigations can be user-triggered or automatic, e.g. we fall back
-    //    to the local NTP if a remote NTP fails to load. Since logging should
-    //    be scoped to the life time of a single NTP we reset the logger every
-    //    time we reach a new NTP.
-    logger_ = std::make_unique<NTPUserDataLogger>(
-        Profile::FromBrowserContext(web_contents()->GetBrowserContext()),
-        // We use the NavigationController's URL since it might differ from the
-        // WebContents URL which is usually chrome://newtab/.
-        web_contents()->GetController().GetVisibleEntry()->GetURL());
+  if (search::IsInstantNTP(web_contents()))
     ipc_router_.SetInputInProgress(IsInputInProgress());
-  }
 
   if (InInstantProcess(instant_service_, web_contents()))
     ipc_router_.OnNavigationEntryCommitted();
--- chrome/browser/ui/search/search_tab_helper.h
+++ chrome/browser/ui/search/search_tab_helper.h
@@ -41,7 +41,6 @@ class Image;
 
 class GURL;
 class InstantService;
-class NTPUserDataLogger;
 class Profile;
 class SearchIPCRouterTest;
 class SkBitmap;
@@ -129,8 +128,6 @@ class SearchTabHelper : public content::WebContentsObserver,
   raw_ptr<InstantService> instant_service_;
 
   bool is_setting_title_ = false;
-
-  std::unique_ptr<NTPUserDataLogger> logger_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 
