1679638385212065b16aa0388c414a93643c22c3
Daniel Rubery <drubery@chromium.org>
2021-12-15 22:56:28 +0000

    Completely disable TfLite classification on ChromeOS
    Due to frequent crashes, we want to completely turn off the TfLite model
    for Client Side Phishing Detection on ChromeOS.
    
    Bug: 1276702
    Change-Id: Ifd59a2c03880490548cd0ea91493c9f0502dc227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3343209
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952127}

--- components/safe_browsing/content/renderer/phishing_classifier/protobuf_scorer.h
+++ components/safe_browsing/content/renderer/phishing_classifier/protobuf_scorer.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/files/memory_mapped_file.h"
 #include "base/strings/string_piece.h"
+#include "build/build_config.h"
 #include "components/safe_browsing/content/renderer/phishing_classifier/scorer.h"
 #include "components/safe_browsing/core/common/proto/client_model.pb.h"
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
@@ -52,7 +53,9 @@ class ProtobufModelScorer : public Scorer {
       base::OnceCallback<void(std::unique_ptr<ClientPhishingRequest>)> callback)
       const override;
 
-#if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
+// TODO(crbug/1278502): This is disabled as a temporary measure due to crashes.
+#if BUILDFLAG(BUILD_WITH_TFLITE_LIB) && !defined(OS_CHROMEOS) && \
+    !BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_CHROMEOS_LACROS)
   void ApplyVisualTfLiteModel(
       const SkBitmap& bitmap,
       base::OnceCallback<void(std::vector<double>)> callback) override;
