7b8605876929db0dde1ffff6bbaae757c9c361bd
Marek Maslanka <mm@semihalf.com>
2021-12-15 12:21:53 +0000

    Remove checks for USE_LIBINPUT in EventDeviceInfo
    The code that checks if libinput needs to be used for touchpad, is not
    depending on the libinput.
    This commit completes changes from
    https://chromium-review.googlesource.com/c/chromium/src/+/3211447
    
    Bug: b:208192332
    Change-Id: I893cb1eaf0d8a0d0875368b45c4e80de76d46ba1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330203
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Harry Cutts <hcutts@chromium.org>
    Commit-Queue: Harry Cutts <hcutts@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951904}

--- ui/events/ozone/evdev/event_device_info.cc
+++ ui/events/ozone/evdev/event_device_info.cc
@@ -97,7 +97,6 @@ constexpr struct {
     {0x413c, 0x81d5},  // Dell Active Pen PN579X
 };
 
-#if defined(USE_LIBINPUT)
 // Certain devices need to be forced to use libinput in place of
 // evdev/libgestures
 constexpr struct {
@@ -118,7 +117,6 @@ bool IsForceLibinput(const EventDeviceInfo& devinfo) {
 
   return false;
 }
-#endif
 
 // Note: this is not SteelSeries's actual VID; the Stratus Duo just reports it
 // incorrectly over Bluetooth.
@@ -569,7 +567,6 @@ bool EventDeviceInfo::IsMicrophoneMuteSwitchDevice() const {
 
 bool EventDeviceInfo::UseLibinput() const {
   bool useLibinput = false;
-#if defined(USE_LIBINPUT)
   if (HasTouchpad()) {
     auto overridden_state =
         base::FeatureList::GetStateIfOverridden(ui::kLibinputHandleTouchpad);
@@ -580,7 +577,6 @@ bool EventDeviceInfo::UseLibinput() const {
                     IsSemiMultitouch() || IsForceLibinput(*this);
     }
   }
-#endif
 
   return useLibinput;
 }
