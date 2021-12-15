48ea7bb6f0825c3437e58c9741698362e422361b
Matt Menke <mmenke@chromium.org>
2021-12-15 00:53:28 +0000

    FLEDGE: Add class that can batch requests.
    The batching logic queues up signals requests until the consumer tells
    it to issue a network request, and then issues one network request for
    all batched requests.
    
    This doesn't wire up batching, which requires a bit more work (namely,
    logic to decide when to send requests for scoring signals. Bidder
    worklets additionally need to be reused before we can meaningfully use
    it for bidder worklets).
    
    Bug: 1276639
    Change-Id: Idf23cb9b91ab5e5801e03bc60cff1fc6866922a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3334501
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951752}

--- content/services/auction_worklet/bidder_worklet.cc
+++ content/services/auction_worklet/bidder_worklet.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/cxx17_backports.h"
 #include "base/logging.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
@@ -225,7 +226,9 @@ void BidderWorklet::GenerateBid(
       !trusted_bidding_signals_keys_->empty()) {
     generate_bid_task->trusted_bidding_signals =
         TrustedSignals::LoadBiddingSignals(
-            url_loader_factory_.get(), *trusted_bidding_signals_keys_,
+            url_loader_factory_.get(),
+            std::set<std::string>(trusted_bidding_signals_keys_->begin(),
+                                  trusted_bidding_signals_keys_->end()),
             top_window_origin.host(), *trusted_bidding_signals_url_, v8_helper_,
             base::BindOnce(&BidderWorklet::OnTrustedBiddingSignalsDownloaded,
                            base::Unretained(this), generate_bid_task));
@@ -387,7 +390,7 @@ void BidderWorklet::V8State::GenerateBid(
     const url::Origin& browser_signal_top_window_origin,
     const url::Origin& browser_signal_seller_origin,
     base::Time auction_start_time,
-    std::unique_ptr<TrustedSignals::Result> trusted_bidding_signals_result,
+    scoped_refptr<TrustedSignals::Result> trusted_bidding_signals_result,
     GenerateBidCallbackInternal callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(v8_sequence_checker_);
 
@@ -718,7 +721,7 @@ void BidderWorklet::OnScriptDownloaded(WorkletLoader::Result worklet_script,
 
 void BidderWorklet::OnTrustedBiddingSignalsDownloaded(
     GenerateBidTaskList::iterator task,
-    std::unique_ptr<TrustedSignals::Result> result,
+    scoped_refptr<TrustedSignals::Result> result,
     absl::optional<std::string> error_msg) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(user_sequence_checker_);
 
--- content/services/auction_worklet/seller_worklet.h
+++ content/services/auction_worklet/seller_worklet.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
 #include "content/services/auction_worklet/auction_v8_helper.h"
 #include "content/services/auction_worklet/public/mojom/auction_worklet_service.mojom-forward.h"
@@ -129,17 +130,16 @@ class SellerWorklet : public mojom::SellerWorklet {
 
     void SetWorkletScript(WorkletLoader::Result worklet_script);
 
-    void ScoreAd(
-        const std::string& ad_metadata_json,
-        double bid,
-        blink::mojom::AuctionAdConfigPtr auction_config,
-        std::unique_ptr<TrustedSignals::Result> trusted_scoring_signals,
-        const url::Origin& browser_signal_top_window_origin,
-        const url::Origin& browser_signal_interest_group_owner,
-        const GURL& browser_signal_render_url,
-        const std::vector<std::string>& browser_signal_ad_components,
-        uint32_t browser_signal_bidding_duration_msecs,
-        ScoreAdCallbackInternal callback);
+    void ScoreAd(const std::string& ad_metadata_json,
+                 double bid,
+                 blink::mojom::AuctionAdConfigPtr auction_config,
+                 scoped_refptr<TrustedSignals::Result> trusted_scoring_signals,
+                 const url::Origin& browser_signal_top_window_origin,
+                 const url::Origin& browser_signal_interest_group_owner,
+                 const GURL& browser_signal_render_url,
+                 const std::vector<std::string>& browser_signal_ad_components,
+                 uint32_t browser_signal_bidding_duration_msecs,
+                 ScoreAdCallbackInternal callback);
 
     void ReportResult(blink::mojom::AuctionAdConfigPtr auction_config,
                       const url::Origin& browser_signal_top_window_origin,
@@ -197,7 +197,7 @@ class SellerWorklet : public mojom::SellerWorklet {
   // V8 thread.
   void OnTrustedScoringSignalsDownloaded(
       ScoreAdTaskList::iterator task,
-      std::unique_ptr<TrustedSignals::Result> result,
+      scoped_refptr<TrustedSignals::Result> result,
       absl::optional<std::string> error_msg);
 
   void DeliverScoreAdCallbackOnUserThread(ScoreAdTaskList::iterator task,
