10448a8a3081817301b3734b18c57c81217f79aa
Scott Violet <sky@chromium.org>
2021-12-15 20:00:40 +0000

    blink: adds definition of FirstContentfulPaintRenderedButNotPresentedAsMonotonicTime
    https://chromium-review.googlesource.com/c/chromium/src/+/3339613/
    added the declaration, but I forgot to include the actual definition.
    
    BUG=1273276
    TEST=none
    
    Change-Id: I55a3b1ec670b3f8209602f2905d708dbaadc0d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342646
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Scott Violet <sky@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952043}

--- third_party/blink/renderer/core/exported/web_performance.cc
+++ third_party/blink/renderer/core/exported/web_performance.cc
@@ -188,6 +188,13 @@ base::TimeTicks WebPerformance::FirstContentfulPaintAsMonotonicTime() const {
   return private_->timing()->FirstContentfulPaintAsMonotonicTime();
 }
 
+base::TimeTicks
+WebPerformance::FirstContentfulPaintRenderedButNotPresentedAsMonotonicTime()
+    const {
+  return private_->timing()
+      ->FirstContentfulPaintRenderedButNotPresentedAsMonotonicTime();
+}
+
 double WebPerformance::FirstMeaningfulPaint() const {
   return MillisecondsToSeconds(private_->timing()->FirstMeaningfulPaint());
 }
