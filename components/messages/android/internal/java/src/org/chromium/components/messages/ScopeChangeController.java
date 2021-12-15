c6dc40455fd21ad32d2b90cd920e1d94f3a58de5
Lijin Shen <lazzzis@google.com>
2021-12-15 22:43:00 +0000

    Reland [Messages] Use webcontents focus/lostFocus to control navigation scope
    On start surface and tab switcher, #wasShown can be called by the
    webcontents of selected tabs, which leads to activate navigation scope
    by mistake.
    
    Use onWebContentsFocused/LostFocused to determine if the current
    webContents is user interactable or fully visible.
    
    Reland: The PortalTests failed because for unknown reason,
    the web contents is used before initialization.
    Make #getViewAndroidDelegate return null if it is invoked before
    initialization.
    See details: https://crbug.com/1278223#c19
    
    Bug: 1275714
    Change-Id: I931884935c877e4430510607c9d3c25127b9024f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3335193
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#952121}

--- components/messages/android/internal/java/src/org/chromium/components/messages/ScopeChangeController.java
+++ components/messages/android/internal/java/src/org/chromium/components/messages/ScopeChangeController.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.messages;
 
+import android.view.View;
+
 import androidx.annotation.Nullable;
 
 import org.chromium.base.ActivityState;
@@ -13,8 +15,10 @@ import org.chromium.content_public.browser.LoadCommittedDetails;
 import org.chromium.content_public.browser.NavigationController;
 import org.chromium.content_public.browser.NavigationEntry;
 import org.chromium.content_public.browser.Visibility;
+import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
 import org.chromium.ui.base.PageTransition;
+import org.chromium.ui.base.ViewAndroidDelegate;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.base.WindowAndroid.ActivityStateObserver;
 
@@ -80,22 +84,30 @@ class ScopeChangeController {
             super(scopeKey.webContents);
             mDelegate = delegate;
             mScopeKey = scopeKey;
-            mDelegate.onScopeChange(new MessageScopeChange(mScopeKey.scopeType, scopeKey,
-                    scopeKey.webContents.getVisibility() == Visibility.VISIBLE
-                            ? ChangeType.ACTIVE
-                            : ChangeType.INACTIVE));
+            int changeType = ChangeType.INACTIVE;
+            WebContents webContents = scopeKey.webContents;
+            if (webContents != null && webContents.getViewAndroidDelegate() != null
+                    && webContents.getVisibility() == Visibility.VISIBLE) {
+                ViewAndroidDelegate viewAndroidDelegate = webContents.getViewAndroidDelegate();
+                if (viewAndroidDelegate.getContainerView() != null
+                        && viewAndroidDelegate.getContainerView().getVisibility() == View.VISIBLE) {
+                    changeType = ChangeType.ACTIVE;
+                } else {
+                    changeType = ChangeType.INACTIVE;
+                }
+            }
+            mDelegate.onScopeChange(
+                    new MessageScopeChange(mScopeKey.scopeType, scopeKey, changeType));
         }
 
         @Override
-        public void wasShown() {
-            super.wasShown();
+        public void onWebContentsFocused() {
             mDelegate.onScopeChange(
                     new MessageScopeChange(mScopeKey.scopeType, mScopeKey, ChangeType.ACTIVE));
         }
 
         @Override
-        public void wasHidden() {
-            super.wasHidden();
+        public void onWebContentsLostFocus() {
             mDelegate.onScopeChange(
                     new MessageScopeChange(mScopeKey.scopeType, mScopeKey, ChangeType.INACTIVE));
         }
