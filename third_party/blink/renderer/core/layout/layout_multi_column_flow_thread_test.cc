e2ee55e80ece827b0dca9bd837e50867602973f7
Koji Ishii <kojii@chromium.org>
2021-12-15 14:19:04 +0000

    [block-in-inline] Rebaseline MultiColumnRenderingTest.Continuation
    Bug: 716930
    Change-Id: Ifae437f46e4cfe129e459c0e9bcd636b23b90361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341178
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#951926}

--- third_party/blink/renderer/core/layout/layout_multi_column_flow_thread_test.cc
+++ third_party/blink/renderer/core/layout/layout_multi_column_flow_thread_test.cc
@@ -1207,6 +1207,19 @@ LayoutBlockFlow DIV id="mc"
   +--LayoutMultiColumnSet (anonymous)
 )DUMP",
               ToSimpleLayoutTree(container));
+  } else if (RuntimeEnabledFeatures::LayoutNGBlockInInlineEnabled()) {
+    EXPECT_EQ(R"DUMP(
+LayoutNGBlockFlow DIV id="mc"
+  +--LayoutMultiColumnFlowThread (anonymous)
+  |  +--LayoutNGBlockFlow (anonymous)
+  |  |  +--LayoutInline SPAN
+  |  |  |  +--LayoutText #text "x"
+  |  |  |  +--LayoutNGBlockFlow (anonymous)
+  |  |  |  |  +--LayoutNGBlockFlow DIV id="inner"
+  |  |  |  +--LayoutText #text "y"
+  +--LayoutMultiColumnSet (anonymous)
+)DUMP",
+              ToSimpleLayoutTree(container));
   } else {
     EXPECT_EQ(R"DUMP(
 LayoutNGBlockFlow DIV id="mc"
@@ -1239,6 +1252,17 @@ LayoutBlockFlow DIV id="mc"
   +--LayoutMultiColumnSet (anonymous)
 )DUMP",
               ToSimpleLayoutTree(container));
+  } else if (RuntimeEnabledFeatures::LayoutNGBlockInInlineEnabled()) {
+    EXPECT_EQ(R"DUMP(
+LayoutNGBlockFlow DIV id="mc"
+  +--LayoutMultiColumnFlowThread (anonymous)
+  |  +--LayoutNGBlockFlow (anonymous)
+  |  |  +--LayoutInline SPAN
+  |  |  |  +--LayoutText #text "x"
+  |  |  |  +--LayoutText #text "y"
+  +--LayoutMultiColumnSet (anonymous)
+)DUMP",
+              ToSimpleLayoutTree(container));
   } else {
     EXPECT_FALSE(flow_thread.ChildrenInline());
     EXPECT_EQ(R"DUMP(
@@ -1265,6 +1289,17 @@ LayoutBlockFlow DIV id="mc"
   |  +--LayoutInline SPAN
   |  |  +--LayoutText #text "xy"
   |  +--LayoutInline SPAN
+  +--LayoutMultiColumnSet (anonymous)
+)DUMP",
+              ToSimpleLayoutTree(container));
+  } else if (RuntimeEnabledFeatures::LayoutNGBlockInInlineEnabled()) {
+    EXPECT_FALSE(flow_thread.ChildrenInline());
+    EXPECT_EQ(R"DUMP(
+LayoutNGBlockFlow DIV id="mc"
+  +--LayoutMultiColumnFlowThread (anonymous)
+  |  +--LayoutNGBlockFlow (anonymous)
+  |  |  +--LayoutInline SPAN
+  |  |  |  +--LayoutText #text "xy"
   +--LayoutMultiColumnSet (anonymous)
 )DUMP",
               ToSimpleLayoutTree(container));
