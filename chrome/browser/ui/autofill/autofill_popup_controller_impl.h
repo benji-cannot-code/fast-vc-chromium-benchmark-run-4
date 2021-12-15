ef0f666538e3b437bf862bf807483980870e4f44
Christoph Schwering <schwering@google.com>
2021-12-15 17:30:40 +0000

    [Autofill] Fixed invalidated WeakPtr<AutofillPopupControllerImpl> deref.
    Iif AutofillDelayPopupControllerDeletion is enabled,
    AutofillPopupControllerImpl deletes itself in a separate task. Before
    this CL, to cancel a pending deletion in GetOrCreate(), we invalidated
    the WeakPtrs.
    
    This also included WeakPtrs in AutofillPopupViewNativeViews which
    should not have been invalidated. Instead of invalidating WeakPtrs,
    this CL cancels the deletion task.
    
    Bug: 1280027, 1279971, 1280009
    Change-Id: Ibdcc73cc392bb00732f72ac592c88e8940866b61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340941
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#951988}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/task/delayed_task_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/autofill/autofill_popup_controller.h"
 #include "chrome/browser/ui/autofill/popup_controller_common.h"
@@ -215,6 +216,11 @@ class AutofillPopupControllerImpl : public AutofillPopupController {
   // The line that is currently selected by the user, null indicates that no
   // line is currently selected.
   absl::optional<int> selected_line_;
+
+  // AutofillPopupControllerImpl deletes itself. To simplify memory management,
+  // we delete the object asynchronously if AutofillDelayPopupControllerDeletion
+  // is enabled (TODO(crbug.com/1277218): Remove "if" clause).
+  base::DelayedTaskHandle self_deletion_task_handle_;
 
   base::WeakPtrFactory<AutofillPopupControllerImpl> weak_ptr_factory_{this};
 };
