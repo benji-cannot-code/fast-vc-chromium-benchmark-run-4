692dad5b77b52fc0926118e06e4f843c81263142
Simon Zünd <szuend@chromium.org>
2021-12-16 06:04:56 +0000

    [devtools] Inline AsyncTaskId into AsyncTaskContext
    This CL inlines the two fields from `AsyncTaskId` into
    `AsyncTaskContext`. This refactoring has been made possible by
    migrating all use-sites of `AsyncTaskId` to `AsyncTaskContext`.
    
    Inlining the fields should recover some of the memory regressions
    we have seen introduced by the migration itself.
    
    Note that half of this CL is a search&replace AsyncTaskId use-sites
    with AsyncTaskContext. The other half is inlining AsyncTaskScheduled
    into AsyncTaskContext::Schedule and removing now unneeded functions.
    
    Bug: 1275875,1278739,1278008
    Change-Id: I7c864b0f80ea0bc9a4467973b224135df7aa3f53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341056
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952252}

--- third_party/blink/renderer/modules/shared_storage/shared_storage.cc
+++ third_party/blink/renderer/modules/shared_storage/shared_storage.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
-#include "third_party/blink/renderer/core/probe/async_task_id.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/modules/shared_storage/shared_storage_worklet.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
