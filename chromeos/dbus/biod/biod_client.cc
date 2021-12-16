efed33f1b99577fac65c321ce9d2f61665cdcace
Patryk Duda <pdk@semihalf.com>
2021-12-16 12:47:47 +0000

    chromeos/dbus/biod: Add support for protobuf in signal
    Replace ScanResult enum with FingerprintMessage protobuf which will
    better capture errors.
    
    For now, FingerprintMessage will be converted to ScanResult due
    to compatibility with upper layers. Passing FingerprintMessage
    to upper layers will be implemented in next commit.
    
    To track AuthScanDone signal upgrade process, boolean metric
    was added.
    
    BUG=b:184843581 b:197959527
    TEST=autoninja -C out/Debug_chromeos/ chromeos_unittests
         ./out/Debug_chromeos/chromeos_unittests
    TEST=autoninja -C out/Debug_chromeos/ browser_tests
         ./out/Debug_chromeos/browser_tests
    
    Change-Id: I03a37b4f7d873986ed5976ae568e6bd90d249435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304058
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Patryk Duda <patrykd@google.com>
    Cr-Commit-Position: refs/heads/main@{#952326}

--- chromeos/dbus/biod/biod_client.cc
+++ chromeos/dbus/biod/biod_client.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/stringprintf.h"
 #include "chromeos/dbus/biod/fake_biod_client.h"
 #include "chromeos/dbus/biod/messages.pb.h"
@@ -26,6 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
+
+// Name of UMA boolean histogram used to count signals with FingerprintMessage
+// and ScanResult.
+const char* kUmaUpgradeToFingerprintMessage =
+    "Fingerprint.AuthScanDoneSignal.UpgradeToFingerprintMessage";
 
 BiodClient* g_instance = nullptr;
 
@@ -368,12 +374,29 @@ class BiodClientImpl : public BiodClient {
   void AuthScanDoneReceived(dbus::Signal* signal) {
     dbus::MessageReader signal_reader(signal);
     dbus::MessageReader array_reader(nullptr);
-    uint32_t scan_result;
     AuthScanMatches matches;
-    if (!signal_reader.PopUint32(&scan_result) ||
-        !signal_reader.PopArray(&array_reader)) {
-      LOG(ERROR) << "Error reading signal from biometrics: "
-                 << signal->ToString();
+    biod::FingerprintMessage msg;
+
+    if (!signal_reader.PopArrayOfBytesAsProto(&msg)) {
+      LOG(WARNING) << "Signal doesn't contain protobuf with authentication "
+                   << "result. Trying to extract ScanResult enum";
+      uint32_t scan_result;
+      if (!signal_reader.PopUint32(&scan_result)) {
+        LOG(ERROR) << "Can't read ScanResult from AuthScanDone signal";
+        return;
+      }
+      // Copy obtained ScanResult to FingerprintMessage protobuf.
+      msg.set_scan_result(static_cast<biod::ScanResult>(scan_result));
+
+      // AuthScanDone signal contains ScanResult enum.
+      base::UmaHistogramBoolean(kUmaUpgradeToFingerprintMessage, false);
+    } else {
+      // AuthScanDone signal contains FingerprintMessage protobuf.
+      base::UmaHistogramBoolean(kUmaUpgradeToFingerprintMessage, true);
+    }
+
+    if (!signal_reader.PopArray(&array_reader)) {
+      LOG(ERROR) << "Can't extract matches array from AuthScanDone signal";
       return;
     }
 
@@ -384,17 +407,17 @@ class BiodClientImpl : public BiodClient {
       if (!array_reader.PopDictEntry(&entry_reader) ||
           !entry_reader.PopString(&user_id) ||
           !entry_reader.PopArrayOfObjectPaths(&paths)) {
-        LOG(ERROR) << "Error reading signal from biometrics: "
-                   << signal->ToString();
+        LOG(ERROR) << "Can't read match data from AuthScanDone signal";
         return;
       }
 
       matches[user_id] = std::move(paths);
     }
 
-    for (auto& observer : observers_) {
-      observer.BiodAuthScanDoneReceived(
-          static_cast<biod::ScanResult>(scan_result), matches);
+    if (msg.msg_case() == biod::FingerprintMessage::MsgCase::kScanResult) {
+      for (auto& observer : observers_) {
+        observer.BiodAuthScanDoneReceived(msg.scan_result(), matches);
+      }
     }
   }
 
