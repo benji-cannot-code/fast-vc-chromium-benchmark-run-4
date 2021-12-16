1c03768962b8dd2fe8f576c85fd27f067a873526
Zhaoliang Ma <zhaoliang.ma@intel.com>
2021-12-16 05:49:47 +0000

    media/gpu/VEA: provide frame QP by VEA through BitstreamBufferMetadata
    webrtc would parse the encoded frame QP if not provided by the encoder,
    this CL reports the frame QP info for Vp8VaapiVED and Vp9VaapiVED and
    propagate QP through BitstreamBufferMetadata qp field.
    
    Bug: 1276221
    Change-Id: I6f1343352cddcd998fe154c02b65dcf07c939d61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3313005
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Zhaoliang Ma <zhaoliang.ma@intel.com>
    Cr-Commit-Position: refs/heads/main@{#952250}

--- media/mojo/mojom/video_encode_accelerator_mojom_traits.cc
+++ media/mojo/mojom/video_encode_accelerator_mojom_traits.cc
@@ -150,6 +150,7 @@ bool StructTraits<media::mojom::BitstreamBufferMetadataDataView,
   if (!data.ReadTimestamp(&metadata->timestamp)) {
     return false;
   }
+  metadata->qp = data.qp();
 
   return data.ReadCodecMetadata(metadata);
 }
