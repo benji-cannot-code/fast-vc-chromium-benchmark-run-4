b50fac00af88dcbd240ab7c55871aa76affbc6c2
Patryk Duda <pdk@semihalf.com>
2021-12-16 16:31:57 +0000

    fingerprint: Switch OnAuthScanDone argument to FingerprintMessage
    Replace ScanResult enum with FingerprintMessage union in
    OnAuthScanDone interface method to better capture information about
    failures.
    
    BUG=b:184843581 b:197959527
    TEST=autoninja -C out/Debug_chromeos/ services_unittests
         ./out/Debug_chromeos/services_unittests
         Files services/device/fingerprint/fingerprint_chromeos.cc
               services/device/fingerprint/fingerprint_chromeos_unittest.cc
               services/device/public/mojom/fingerprint.mojom
    
    TEST=autoninja -C out/Debug_chromeos/ ash_unittests
         ./out/Debug_chromeos/ash_unittests
         Files ash/ambient/ambient_controller.cc
               ash/ambient/ambient_controller.h
    
    TEST=autoninja -C out/Debug_chromeos/ browser_tests
         ./out/Debug_chromeos/browser_tests
      Files chrome/browser/ash/login/lock/screen_locker.cc
            chrome/browser/ash/login/lock/screen_locker.h
            chrome/browser/ash/login/screens/fingerprint_setup_screen.cc
            chrome/browser/ash/login/screens/fingerprint_setup_screen.h
            chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.cc
            chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h
            ui/chromeos/user_activity_power_manager_notifier.cc
            ui/chromeos/user_activity_power_manager_notifier.h
    
    TEST=autoninja -C out/Debug_chromeos/ unit_tests
         ./out/Debug_chromeos/unit_tests
         Files chrome/browser/ash/login/quick_unlock/fingerprint_storage.cc
               chrome/browser/ash/login/quick_unlock/fingerprint_storage.h
    
    Change-Id: Ia52417257959bb5252571251b0cbc25dee485b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3304060
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Patryk Duda <patrykd@google.com>
    Auto-Submit: Patryk Duda <patrykd@google.com>
    Cr-Commit-Position: refs/heads/main@{#952400}

--- chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h
+++ chrome/browser/ui/webui/settings/chromeos/fingerprint_handler.h
@@ -47,7 +47,7 @@ class FingerprintHandler : public ::settings::SettingsPageUIHandler,
                         bool enroll_session_complete,
                         int percent_complete) override;
   void OnAuthScanDone(
-      device::mojom::ScanResult scan_result,
+      const device::mojom::FingerprintMessagePtr msg,
       const base::flat_map<std::string, std::vector<std::string>>& matches)
       override;
   void OnSessionFailed() override;
