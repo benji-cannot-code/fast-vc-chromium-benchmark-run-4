449ec2efaad84ed250dacf4680637dfcb1de45f0
Elie Maamari <emaamari@google.com>
2021-12-16 21:11:03 +0000

    Implement native ui for password collection
    To be used by upcoming Javascript extension API for authentication.
    This is replacing the current webui implementation and lays foundations
    for the eventual overhaul of the authentication dialog that would
    include potentially auth factors other than password depending on what's
    been configured.
    
    Change-Id: I519b6c18653d7bc57cc491c1c8f23231d8662ca1
    Bug: 1271551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3275777
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#952512}

---
+++ ash/in_session_auth/authentication_dialog_unittest.cc
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/in_session_auth/authentication_dialog.h"
+
+#include "ash/test/ash_test_base.h"
+#include "base/test/bind.h"
+#include "ui/views/controls/textfield/textfield.h"
+
+namespace ash {
+namespace {
+
+class AuthenticationDialogTest : public AshTestBase {
+ public:
+  void SetUp() override {
+    AshTestBase::SetUp();
+    dialog_ = AuthenticationDialog::Show(base::BindLambdaForTesting(
+        [&](AuthenticationDialog::Result result, const std::u16string& token,
+            base::TimeDelta timeout) {
+          result_ = result;
+          called_ = true;
+        }));
+  }
+
+ protected:
+  bool called_ = false;
+  AuthenticationDialog::Result result_;
+  AuthenticationDialog* dialog_;
+};
+
+TEST_F(AuthenticationDialogTest, CallbackCalledOnCancel) {
+  dialog_->Cancel();
+  EXPECT_TRUE(called_);
+  EXPECT_EQ(result_, AuthenticationDialog::Result::kAborted);
+}
+
+TEST_F(AuthenticationDialogTest, CallbackCalledOnClose) {
+  dialog_->Close();
+  EXPECT_TRUE(called_);
+  EXPECT_EQ(result_, AuthenticationDialog::Result::kAborted);
+}
+
+}  // namespace
+}  // namespace ash
