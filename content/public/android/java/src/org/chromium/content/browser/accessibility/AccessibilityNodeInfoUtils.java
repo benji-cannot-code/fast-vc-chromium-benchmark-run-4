105dc40c4b7693bf5350fb6ebfcac59e2f52673e
Mark Schillaci <mschillaci@google.com>
2021-12-16 01:27:39 +0000

    Reland: "Add remaining html accessibility static tree tests on Android"
    This CL relands: crrev.com/c/3309668
    Reverted here: crrev.com/c/3338480
    
    To sheriffs: If a test fails/flakes, consider adding the "@DisabledTest"
                 annotation to the test in lieu of a full revert.
                 Thanks!
    
    For this reland, we have removed all instances of "offscreen", which
    can create flakes/failures in some cases. All failing tests had this
    in common, so (hopefully) this fixes all remaining issues.
    
    
    Original CL description:
    -------------------------------------------------
    
    This CL adds the remaining static tree dump tests to Android that are
    part of the content/test/data/accessibility/html directory, to
    continue the work on closing the gap on the "missing layer".
    
    We add a batch of tests with the generated output for each test
    as a txt file. We only include tests that are being run on another
    platform already (i.e., not a blink-exclusive test), and we skip
    all tests which have <script>'s as those could cause flakiness,
    and we skip any trivial tests that produce only a one-line output,
    such as a test with nodes that aren't important on Android. We batch
    the test suite for quicker run time.
    
    -------------------------------------------------
    
    AX-Relnotes: N/A
    Bug: 1258230
    Change-Id: I4472ac80827a41bd1646f4dc9a6d9bc77621feb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342212
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#952187}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/AccessibilityNodeInfoUtils.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/AccessibilityNodeInfoUtils.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.accessibility;
 
+import static org.chromium.content.browser.accessibility.WebContentsAccessibilityImpl.EXTRAS_KEY_OFFSCREEN;
 import static org.chromium.content.browser.accessibility.WebContentsAccessibilityImpl.EXTRAS_KEY_SUPPORTED_ELEMENTS;
 import static org.chromium.content.browser.accessibility.WebContentsAccessibilityImpl.EXTRAS_KEY_UNCLIPPED_BOTTOM;
 import static org.chromium.content.browser.accessibility.WebContentsAccessibilityImpl.EXTRAS_KEY_UNCLIPPED_TOP;
@@ -43,14 +44,18 @@ public class AccessibilityNodeInfoUtils {
         builder.append(classNameParts[classNameParts.length - 1]);
 
         // Print text unless it is empty (null is allowed).
-        if (node.getText() == null || !node.getText().toString().isEmpty()) {
-            builder.append(" text:\"").append(node.getText()).append("\"");
+        if (node.getText() == null) {
+            builder.append(" text:\"null\"");
+        } else if (!node.getText().toString().isEmpty()) {
+            builder.append(" text:\"")
+                    .append(node.getText().toString().replace("\n", "\\n"))
+                    .append("\"");
         }
 
         // Text properties - Only print when non-null.
         if (node.getContentDescription() != null) {
             builder.append(" contentDescription:\"")
-                    .append(node.getContentDescription())
+                    .append(node.getContentDescription().toString().replace("\n", "\\n"))
                     .append("\"");
         }
         if (node.getViewIdResourceName() != null) {
@@ -290,6 +295,12 @@ public class AccessibilityNodeInfoUtils {
             // For the special case of the supported HTML elements, which prints the same for the
             // rootWebArea on each test, assert consistency and suppress from results.
             if (key.equals(EXTRAS_KEY_SUPPORTED_ELEMENTS)) {
+                continue;
+            }
+
+            // To prevent flakiness or dependency on screensize/form factor, drop the "offscreen"
+            // Bundle extra.
+            if (key.equals(EXTRAS_KEY_OFFSCREEN)) {
                 continue;
             }
 
