126a7693b5909810c89b5ecada3ba127551293f6
Taylor Bergquist <tbergquist@chromium.org>
2021-12-16 23:30:16 +0000

    Add accessible names to the tab strip scroll buttons.
    Bug: 1253843
    Change-Id: Ia373b11397feff22c25c3d7013a2e11d5f207639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322136
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952568}

--- chrome/browser/ui/views/tabs/tab_strip_scroll_container.cc
+++ chrome/browser/ui/views/tabs/tab_strip_scroll_container.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
+#include "chrome/grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/canvas.h"
@@ -186,14 +188,19 @@ TabStripScrollContainer::TabStripScrollContainer(
       scroll_button_container->SetLayoutManager(
           std::make_unique<views::FlexLayout>());
   scroll_button_layout->SetOrientation(views::LayoutOrientation::kHorizontal);
+
   leading_scroll_button_ =
       scroll_button_container->AddChildView(CreateScrollButton(
           base::BindRepeating(&TabStripScrollContainer::ScrollTowardsLeadingTab,
                               base::Unretained(this))));
+  leading_scroll_button_->SetAccessibleName(
+      l10n_util::GetStringUTF16(IDS_ACCNAME_TAB_SCROLL_LEADING));
   trailing_scroll_button_ = scroll_button_container->AddChildView(
       CreateScrollButton(base::BindRepeating(
           &TabStripScrollContainer::ScrollTowardsTrailingTab,
           base::Unretained(this))));
+  trailing_scroll_button_->SetAccessibleName(
+      l10n_util::GetStringUTF16(IDS_ACCNAME_TAB_SCROLL_TRAILING));
 
   // The space in dips between the scroll buttons and the NTB.
   constexpr int kScrollButtonsTrailingMargin = 8;
