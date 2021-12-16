f459f53f0ebd721c907f14e1da16a880643a5abc
dpapad <dpapad@chromium.org>
2021-12-16 23:51:53 +0000

    WebUI: Migrate shared custom_element.js to TypeScript.
    All clients of that file are already migrated to TS.
    
    By migrating to TS, the signature of $() and $all() methods is improved
    to leverage generics and propagate types to the underlying
    querySelector/querySelectorAll methods.
    
    As part of this change discovered that the DownloadShelf UI was opting
    out of some strict TS checks (by setting "strictNullChecks": false).
    Removed the flag and fixed violations.
    
    Bug: 1189595
    Change-Id: I681d2506f6e6e9d6fb5e6bb46cf0b225ec42b7aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344388
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952580}

--- chrome/browser/resources/tab_strip/tab_list.ts
+++ chrome/browser/resources/tab_strip/tab_list.ts
@@ -389,13 +389,12 @@ export class TabListElement extends CustomElement implements
   }
 
   private findTabElement_(tabId: number): TabElement|null {
-    return this.$(`tabstrip-tab[data-tab-id="${tabId}"]`) as TabElement | null;
+    return this.$<TabElement>(`tabstrip-tab[data-tab-id="${tabId}"]`);
   }
 
   private findTabGroupElement_(groupId: string): TabGroupElement|null {
-    return this.$(`tabstrip-tab-group[data-group-id="${groupId}"]`) as
-        TabGroupElement |
-        null;
+    return this.$<TabGroupElement>(
+        `tabstrip-tab-group[data-group-id="${groupId}"]`);
   }
 
   private fetchAndUpdateColors_() {
@@ -404,8 +403,7 @@ export class TabListElement extends CustomElement implements
   }
 
   private fetchAndUpdateGroupData_() {
-    const tabGroupElements =
-        this.$all('tabstrip-tab-group') as NodeListOf<TabGroupElement>;
+    const tabGroupElements = this.$all<TabGroupElement>('tabstrip-tab-group');
     this.tabsApi_.getGroupVisualData().then(({data}) => {
       tabGroupElements.forEach(tabGroupElement => {
         tabGroupElement.updateVisuals(
@@ -421,7 +419,7 @@ export class TabListElement extends CustomElement implements
   }
 
   private getActiveTab_(): TabElement|null {
-    return this.$('tabstrip-tab[active]') as TabElement | null;
+    return this.$<TabElement>('tabstrip-tab[active]');
   }
 
   getIndexOfTab(tabElement: TabElement): number {
@@ -474,7 +472,7 @@ export class TabListElement extends CustomElement implements
     // document. When the tab strip first gains keyboard focus, no such event
     // exists yet, so the outline needs to be explicitly set to visible.
     this.focusOutlineManager_.visible = true;
-    (this.$('tabstrip-tab') as HTMLElement).focus();
+    this.$<TabElement>('tabstrip-tab')!.focus();
   }
 
   private onTabActivated_(tabId: number) {
@@ -489,7 +487,7 @@ export class TabListElement extends CustomElement implements
     // have updated a Tab to have an active state. For example, if a
     // tab is created with an already active state, there may be 2 active
     // TabElements: the newly created tab and the previously active tab.
-    (this.$all('tabstrip-tab[active]') as NodeListOf<TabElement>)
+    this.$all<TabElement>('tabstrip-tab[active]')
         .forEach((previouslyActiveTab) => {
           if (previouslyActiveTab.tab.id !== tabId) {
             previouslyActiveTab.tab = /** @type {!Tab} */ (
@@ -706,7 +704,7 @@ export class TabListElement extends CustomElement implements
       index++;
     }
 
-    let elementAtIndex = this.$all('tabstrip-tab')[index];
+    let elementAtIndex = this.$all('tabstrip-tab')[index]!;
     if (elementAtIndex && elementAtIndex.parentElement &&
         isTabGroupElement(elementAtIndex.parentElement)) {
       elementAtIndex = elementAtIndex.parentElement;
@@ -797,7 +795,8 @@ export class TabListElement extends CustomElement implements
           element, this.pinnedTabsElement_.childNodes[index]!);
     } else {
       let elementToInsert: TabElement|TabGroupElement = element;
-      let elementAtIndex = this.$all('tabstrip-tab').item(index);
+      let elementAtIndex: TabElement|TabGroupElement =
+          this.$all<TabElement>('tabstrip-tab').item(index);
       let parentElement = this.unpinnedTabsElement_;
 
       if (groupId) {
@@ -825,7 +824,7 @@ export class TabListElement extends CustomElement implements
         // TabElement is being sandwiched between two TabElements in a group, it
         // can be assumed that the tab will eventually be inserted into the
         // group as well.
-        elementAtIndex = elementAtIndex.parentElement;
+        elementAtIndex = elementAtIndex.parentElement as TabGroupElement;
       }
 
       if (elementAtIndex && elementAtIndex.parentElement === parentElement) {
