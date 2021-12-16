d4e5195394dac15942fa9db87518e318ad14fb0a
Sergio Vila Prado <sergiovila@google.com>
2021-12-16 14:52:52 +0000

    [Autofill Assistant] Add CLI override for ECDSA key
    Bug: b/203031699
    Change-Id: I9f44447042479569f2012e603e03cb2d079649d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3329614
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sergio Vila Prado <sergiovila@google.com>
    Cr-Commit-Position: refs/heads/main@{#952356}

--- components/autofill_assistant/browser/service/cup_impl_unittest.cc
+++ components/autofill_assistant/browser/service/cup_impl_unittest.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cup_impl.h"
 
+#include "base/command_line.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/autofill_assistant/browser/features.h"
 #include "components/autofill_assistant/browser/service.pb.h"
+#include "components/autofill_assistant/browser/switches.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 namespace {
@@ -80,6 +82,33 @@ TEST(CUPImplTest, IgnoresNonGetActionsResponse) {
   absl::optional<std::string> unpacked_response =
       cup_.UnpackResponse("a response");
   EXPECT_EQ(*unpacked_response, "a response");
+}
+
+TEST(CUPImplTest, OverridesEcdsaPublicKeyWithCLIValue) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      autofill_assistant::switches::kAutofillAssistantCupPublicKeyBase64,
+      "SGVsbG8=");
+  EXPECT_EQ(autofill_assistant::cup::CUPImpl::GetPublicKey(), "Hello");
+}
+
+TEST(CUPImplTest, HasValidEcdsaPublicKeyWithNotValidCLIValue) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      autofill_assistant::switches::kAutofillAssistantCupPublicKeyBase64,
+      "Not valid base64");
+  EXPECT_FALSE(autofill_assistant::cup::CUPImpl::GetPublicKey().empty());
+}
+
+TEST(CUPImplTest, OverridesEcdsaKeyVersionithCLIValue) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      autofill_assistant::switches::kAutofillAssistantCupKeyVersion, "15");
+  EXPECT_EQ(autofill_assistant::cup::CUPImpl::GetKeyVersion(), 15);
+}
+
+TEST(CUPImplTest, HasValidEcdsaKeyVersionWithNotValidCLIValue) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      autofill_assistant::switches::kAutofillAssistantCupKeyVersion,
+      "Not a number");
+  EXPECT_GT(autofill_assistant::cup::CUPImpl::GetKeyVersion(), -1);
 }
 
 }  // namespace
