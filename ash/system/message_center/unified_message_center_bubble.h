1d3eee4fd428d9f89f1bf1f9bf68c3a8e9b4adc6
Ahmed Mehfooz <amehfooz@chromium.org>
2021-12-16 00:14:52 +0000

    Fix message center artifacts when empty
    Hide widget when message center is invisible.
    Remove border drawn around the message center in the new UI.
    
    Bug: 1268182
    Change-Id: I688df35fa3cdfa102edd8878b10abefa87fcecdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3329082
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952161}

--- ash/system/message_center/unified_message_center_bubble.h
+++ ash/system/message_center/unified_message_center_bubble.h
@@ -89,6 +89,8 @@ class ASH_EXPORT UnifiedMessageCenterBubble
 
   // views::ViewObserver:
   void OnViewPreferredSizeChanged(views::View* observed_view) override;
+  void OnViewVisibilityChanged(views::View* observed_view,
+                               views::View* starting_view) override;
 
   // views::WidgetObserver:
   void OnWidgetDestroying(views::Widget* widget) override;
