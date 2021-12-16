c36aca75a648b0a6b8fafffc7611be216292dd1e
Nick Burris <nburris@chromium.org>
2021-12-16 20:04:16 +0000

    Migrate PaymentSheetViewControllerNoShippingTest
    This CL migrates the desktop PaymentSheetViewControllerNoShippingTest suite
    tests that depend on basic-card. The migrated tests use a URL based
    payment method instead of basic-card. They are added as a new test suite
    so that coverage for the old path is preserved.
    
    Bug: 1221559
    Change-Id: Ic07d53386a421f34335165ba344c372d3f6ad5ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318637
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952482}

--- components/test/data/payments/no_shipping.js
+++ components/test/data/payments/no_shipping.js
@@ -11,12 +11,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Launches the PaymentRequest UI that does not require a shipping address.
  */
 function buy() { // eslint-disable-line no-unused-vars
+  buyWithMethods([
+    {
+      supportedMethods: 'basic-card',
+      data: {supportedNetworks: ['visa', 'mastercard']},
+    },
+  ]);
+}
+
+/**
+ * Launches the PaymentRequest UI that does not require a shipping address.
+ * @param {String} methodData - An array of payment method objects.
+ */
+function buyWithMethods(methodData) {
   try {
     new PaymentRequest(
-        [{
-          supportedMethods: 'basic-card',
-          data: {supportedNetworks: ['visa', 'mastercard']},
-        }],
+      methodData,
         {
           total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}},
           displayItems: [
