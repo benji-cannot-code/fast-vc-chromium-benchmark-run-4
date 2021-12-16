5963616b75b1f1543a590cbd72e1342bd2f8078f
Sorin Jianu <sorin@chromium.org>
2021-12-16 00:13:32 +0000

    Replace head scratcher std::hash<IID> implementation.
    Fixed: 1280455
    Change-Id: Ib190a704e04688ac10824747f893cb5c35337a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3343446
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952160}

--- chrome/updater/win/win_util.h
+++ chrome/updater/win/win_util.h
@@ -8,10 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <winerror.h>
 
-#include <stdint.h>
-
+#include <cstdint>
 #include <string>
 
+#include "base/containers/span.h"
+#include "base/hash/hash.h"
 #include "base/process/process_iterator.h"
 #include "base/win/atl.h"
 #include "base/win/scoped_handle.h"
@@ -21,6 +22,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 class FilePath;
 }
+
+// Specialization for std::hash so that IID values can be stored in an
+// associative container.
+template <>
+struct std::hash<IID> {
+  size_t operator()(const IID& iid) const {
+    static_assert(sizeof(iid) == 16, "IID storage must be contiguous.");
+    return base::FastHash(base::span<const uint8_t>(
+        reinterpret_cast<const uint8_t*>(&iid), sizeof(iid)));
+  }
+};
 
 namespace updater {
 
