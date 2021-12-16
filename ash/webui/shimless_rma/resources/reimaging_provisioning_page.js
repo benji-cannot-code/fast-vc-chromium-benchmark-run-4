37466ff9faa9b8e6ceedb21b8f9a1ab1e8855b3a
Gavin Williams <gavinwill@chromium.org>
2021-12-16 23:44:45 +0000

    Shimless: Add retry provisioning button
    Also show spinner while provisioning
    
    screenshot: http://screen/5UAwg6KcprADnxm
    
    Bug: 1198187
    Change-Id: Ic52ebde7dd2166e5f9dc1f5cac2bad5e932fc1bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3343285
    Reviewed-by: Gavin Dodd <gavindodd@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952574}

--- ash/webui/shimless_rma/resources/reimaging_provisioning_page.js
+++ ash/webui/shimless_rma/resources/reimaging_provisioning_page.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import './shimless_rma_shared_css.js';
@@ -68,6 +69,18 @@ export class ReimagingProvisioningPage extends ReimagingProvisioningPageBase {
         type: String,
         computed: 'getStatusString_(status_, progress_)',
       },
+
+      /** @protected {boolean} */
+      shouldShowSpinner_: {
+        type: Boolean,
+        value: false,
+      },
+
+      /** @protected {boolean} */
+      shouldShowRetryButton_: {
+        type: Boolean,
+        value: false,
+      },
     };
   }
 
@@ -120,6 +133,10 @@ export class ReimagingProvisioningPage extends ReimagingProvisioningPageBase {
         'disable-next-button',
         {bubbles: true, composed: true, detail: disabled},
         ));
+    this.shouldShowSpinner_ = this.status_ === ProvisioningStatus.kInProgress;
+    this.shouldShowRetryButton_ =
+        this.status_ === ProvisioningStatus.kFailedBlocking ||
+        this.status_ === ProvisioningStatus.kFailedNonBlocking;
   }
 
   /** @return {!Promise<!StateResult>} */
@@ -130,6 +147,26 @@ export class ReimagingProvisioningPage extends ReimagingProvisioningPageBase {
     } else {
       return Promise.reject(new Error('Provisioning is not complete.'));
     }
+  }
+
+  /** @private */
+  onRetryProvsioningButtonClicked_() {
+    if (this.status_ !== ProvisioningStatus.kFailedBlocking &&
+        this.status_ !== ProvisioningStatus.kFailedNonBlocking) {
+      console.error('Provisioning has not failed.');
+      return;
+    }
+
+    this.dispatchEvent(new CustomEvent(
+        'transition-state',
+        {
+          bubbles: true,
+          composed: true,
+          detail: (() => {
+            return this.shimlessRmaService_.retryProvisioning();
+          })
+        },
+        ));
   }
 }
 
