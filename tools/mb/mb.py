ef9a97106942b45b4739a721d64149f0345db049
Dirk Pranke <dpranke@google.com>
2021-12-16 01:43:40 +0000

    Write .isolates as JSON literals, not Python literals.
    This fixes a bug with `mb run` and anything else that needed
    to be able to run the `isolate` command against an isolate
    generated by MB.
    
    It looks like there may be a newer version of the isolate
    binary that is expecting .isolate files to be JSON, not Python
    literals.
    
    Bug: 1280069
    Change-Id: I97e7235e087a28aa3c3657c80de6306bf3d63ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342248
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#952195}

--- tools/mb/mb.py
+++ tools/mb/mb.py
@@ -21,7 +21,6 @@ import json
 import os
 import pipes
 import platform
-import pprint
 import re
 import shutil
 import sys
@@ -1575,12 +1574,12 @@ class MetaBuildWrapper(object):
       return 1
 
     self.WriteFile(isolate_path,
-      pprint.pformat({
+      json.dumps({
         'variables': {
           'command': command,
           'files': files,
         }
-      }) + '\n')
+      }, sort_keys=True) + '\n')
 
     self.WriteJSON(
       {
