848db6c2403f77219bed9d9abbda4f79134e31dc
Vladimir Nechaev <nechaev@chromium.org>
2021-12-16 15:29:32 +0000

    Monitor Page.frameAttached in ChromeDriver
    DomTracker must monitor Page.frameAttached and update the mapping.
    
    Bug: 1233305
    Change-Id: I047dcfdd6605786367e5eec7704c5610e2ada1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341512
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Vladimir Nechaev <nechaev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952374}

--- chrome/test/chromedriver/chrome/dom_tracker.h
+++ chrome/test/chromedriver/chrome/dom_tracker.h
@@ -40,6 +40,7 @@ class DomTracker : public DevToolsEventListener {
  private:
   bool ProcessNodeList(const base::Value& nodes);
   bool ProcessNode(const base::Value& node);
+  Status RebuildMapping(DevToolsClient* client);
 
   std::map<int, std::string> node_to_frame_map_;
 };
