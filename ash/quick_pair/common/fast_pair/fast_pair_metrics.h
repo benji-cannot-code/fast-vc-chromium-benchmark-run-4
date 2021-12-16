9bbe06c72fc9d4a7513708af1161e6716f636135
Juliet Levesque <julietlevesque@google.com>
2021-12-16 17:39:19 +0000

    [Fast Pair] Log AccountKey failures and the result of account key write.
    Adds a metric to log the result of account key writes (success or
    failure) and adds a metric to log the account key failures.
    
    Change-Id: I8f4de33f62af1e7b13b95635df6bb93716a38d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3324521
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#952423}

--- ash/quick_pair/common/fast_pair/fast_pair_metrics.h
+++ ash/quick_pair/common/fast_pair/fast_pair_metrics.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_QUICK_PAIR_COMMON_FAST_PAIR_FAST_PAIR_METRICS_H_
 #define ASH_QUICK_PAIR_COMMON_FAST_PAIR_FAST_PAIR_METRICS_H_
 
+#include "ash/quick_pair/common/account_key_failure.h"
 #include "ash/quick_pair/common/pair_failure.h"
 #include "base/component_export.h"
 #include "base/time/time.h"
@@ -86,6 +87,13 @@ void RecordPairingFailureReason(const Device& device, PairFailure failure);
 
 COMPONENT_EXPORT(QUICK_PAIR_COMMON)
 void RecordPairingResult(const Device& device, bool success);
+
+COMPONENT_EXPORT(QUICK_PAIR_COMMON)
+void RecordAccountKeyFailureReason(const Device& device,
+                                   AccountKeyFailure failure);
+
+COMPONENT_EXPORT(QUICK_PAIR_COMMON)
+void RecordAccountKeyResult(const Device& device, bool success);
 
 }  // namespace quick_pair
 }  // namespace ash
