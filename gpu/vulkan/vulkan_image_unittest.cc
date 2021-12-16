89203f722c08deae3bd56466d0606940be6f9bf2
Corentin Wallez <cwallez@chromium.org>
2021-12-16 18:28:10 +0000

    VulkanImage: remove tracking of the current VkImageLayout
    The current layout was set to VK_IMAGE_LAYOUT_UNDEFINED and never set
    anywhere. Remove it to not mislead people into thinking that it tracks
    the last layout of the image.
    
    Bug: chromium:1279419
    Change-Id: Ia925e7f118677809509dad30c64ed2a424d49e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340716
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952445}

--- gpu/vulkan/vulkan_image_unittest.cc
+++ gpu/vulkan/vulkan_image_unittest.cc
@@ -46,7 +46,6 @@ TEST_F(VulkanImageTest, Create) {
     EXPECT_EQ(image->format(), format);
     EXPECT_GT(image->device_size(), 0u);
     EXPECT_EQ(image->image_tiling(), VK_IMAGE_TILING_OPTIMAL);
-    EXPECT_EQ(image->image_layout(), VK_IMAGE_LAYOUT_UNDEFINED);
     EXPECT_NE(image->image(), static_cast<VkImage>(VK_NULL_HANDLE));
     EXPECT_NE(image->device_memory(),
               static_cast<VkDeviceMemory>(VK_NULL_HANDLE));
@@ -75,7 +74,6 @@ TEST_F(VulkanImageTest, CreateWithExternalMemory) {
     EXPECT_EQ(image->format(), format);
     EXPECT_GT(image->device_size(), 0u);
     EXPECT_EQ(image->image_tiling(), VK_IMAGE_TILING_OPTIMAL);
-    EXPECT_EQ(image->image_layout(), VK_IMAGE_LAYOUT_UNDEFINED);
     EXPECT_NE(image->image(), static_cast<VkImage>(VK_NULL_HANDLE));
     EXPECT_NE(image->device_memory(),
               static_cast<VkDeviceMemory>(VK_NULL_HANDLE));
@@ -181,7 +179,6 @@ TEST_F(VulkanImageTest, CreateFromGpuMemoryBufferHandle) {
     EXPECT_EQ(image->format(), format.vk);
     EXPECT_GT(image->device_size(), 0u);
     EXPECT_EQ(image->image_tiling(), VK_IMAGE_TILING_OPTIMAL);
-    EXPECT_EQ(image->image_layout(), VK_IMAGE_LAYOUT_UNDEFINED);
     EXPECT_NE(image->image(), static_cast<VkImage>(VK_NULL_HANDLE));
     EXPECT_NE(image->device_memory(),
               static_cast<VkDeviceMemory>(VK_NULL_HANDLE));
