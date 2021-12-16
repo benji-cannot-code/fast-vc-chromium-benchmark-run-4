13f050411ec570ba230bcbfdf54c9b32bec65ef3
Vikas Soni <vikassoni@chromium.org>
2021-12-16 22:10:32 +0000

    [Dr-Dc] Thread safe vulkan Queue.
    1. Allow thread safe access to a vulkan queue from gpu main thread
    and drdc thread by using locks around all queue access. This
    includes using locks in all vkQueue* api calls.
    
    2. Use a map to generate lock per queue.
    
    3. Update gpu/vulkan/generate_bindings.py to autogenerate relevant code.
    
    Bug: 1262385
    Change-Id: Icd730759d863522981fd14ea6e39fadb67c70365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3339655
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952538}

--- gpu/vulkan/vulkan_function_pointers.h
+++ gpu/vulkan/vulkan_function_pointers.h
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/component_export.h"
+#include "base/containers/flat_map.h"
 #include "base/native_library.h"
+#include "base/synchronization/lock.h"
 #include "build/build_config.h"
 #include "ui/gfx/extension_set.h"
 
@@ -69,6 +71,12 @@ struct COMPONENT_EXPORT(VULKAN) VulkanFunctionPointers {
                                   const gfx::ExtensionSet& enabled_extensions);
 
   base::NativeLibrary vulkan_loader_library = nullptr;
+
+  // This is used to allow thread safe access to a given vulkan queue when
+  // multiple gpu threads are accessing it. Note that this map will be only
+  // accessed by multiple gpu threads concurrently to read the data, so it
+  // should be thread safe to use this map by multiple threads.
+  base::flat_map<VkQueue, std::unique_ptr<base::Lock>> per_queue_lock_map;
 
   template <typename T>
   class VulkanFunction;
@@ -960,10 +968,16 @@ ALWAYS_INLINE VkResult vkQueueSubmit(VkQueue queue,
                                      uint32_t submitCount,
                                      const VkSubmitInfo* pSubmits,
                                      VkFence fence) {
+  base::AutoLockMaybe auto_lock(
+      gpu::GetVulkanFunctionPointers()->per_queue_lock_map[queue].get());
+
   return gpu::GetVulkanFunctionPointers()->vkQueueSubmit(queue, submitCount,
                                                          pSubmits, fence);
 }
 ALWAYS_INLINE VkResult vkQueueWaitIdle(VkQueue queue) {
+  base::AutoLockMaybe auto_lock(
+      gpu::GetVulkanFunctionPointers()->per_queue_lock_map[queue].get());
+
   return gpu::GetVulkanFunctionPointers()->vkQueueWaitIdle(queue);
 }
 ALWAYS_INLINE VkResult vkResetCommandBuffer(VkCommandBuffer commandBuffer,
@@ -1149,6 +1163,9 @@ ALWAYS_INLINE VkResult vkGetSwapchainImagesKHR(VkDevice device,
 }
 ALWAYS_INLINE VkResult vkQueuePresentKHR(VkQueue queue,
                                          const VkPresentInfoKHR* pPresentInfo) {
+  base::AutoLockMaybe auto_lock(
+      gpu::GetVulkanFunctionPointers()->per_queue_lock_map[queue].get());
+
   return gpu::GetVulkanFunctionPointers()->vkQueuePresentKHR(queue,
                                                              pPresentInfo);
 }
--- gpu/vulkan/vulkan_util.cc
+++ gpu/vulkan/vulkan_util.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "gpu/config/gpu_info.h"  // nogncheck
 #include "gpu/config/vulkan_info.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GL_LAYOUT_DEPTH_ATTACHMENT_STENCIL_READ_ONLY_EXT 0x9531
 
 namespace gpu {
+
 namespace {
 
 #if defined(OS_ANDROID)
@@ -146,6 +148,25 @@ VkResult CreateGraphicsPipelinesHook(
       base::Time::Now()));
   return vkCreateGraphicsPipelines(device, pipelineCache, createInfoCount,
                                    pCreateInfos, pAllocator, pPipelines);
+}
+
+VkResult VulkanQueueSubmitHook(VkQueue queue,
+                               uint32_t submitCount,
+                               const VkSubmitInfo* pSubmits,
+                               VkFence fence) {
+  TRACE_EVENT0("gpu", "VulkanQueueSubmitHook");
+  return vkQueueSubmit(queue, submitCount, pSubmits, fence);
+}
+
+VkResult VulkanQueueWaitIdleHook(VkQueue queue) {
+  TRACE_EVENT0("gpu", "VulkanQueueWaitIdleHook");
+  return vkQueueWaitIdle(queue);
+}
+
+VkResult VulkanQueuePresentKHRHook(VkQueue queue,
+                                   const VkPresentInfoKHR* pPresentInfo) {
+  TRACE_EVENT0("gpu", "VulkanQueuePresentKHRHook");
+  return vkQueuePresentKHR(queue, pPresentInfo);
 }
 
 bool CheckVulkanCompabilities(const VulkanInfo& vulkan_info,
