0557ef4875e1a57bc2ca2d8467eb9c4d911643a4
Lijin Shen <lazzzis@google.com>
2021-12-16 17:20:33 +0000

    [Messages] Add presubmit warning for adding new Android InfoBars
    Add presubmit warnings for adding new Android InfoBars
    1. Check whether a new id is appended in infobar_delegate.h
    2. Check whether new files are created under android infobar
       related folders
    
    Bug: 1233156
    Change-Id: I0e9b0800cb806196e4432b0fbd5439c1d0dd937d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3328682
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#952415}

---
+++ tools/android/infobar_deprecation/PRESUBMIT.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Presubmit tests for /tools/android/infobar_deprecation.
+
+Runs Python unit tests in /tools/android/infobar_deprecation on upload.
+"""
+
+USE_PYTHON3 = True
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  result = []
+  result.extend(
+      input_api.canned_checks.RunUnitTests(input_api, output_api,
+                                           ['./infobar_deprecation_test.py']))
+  return result
