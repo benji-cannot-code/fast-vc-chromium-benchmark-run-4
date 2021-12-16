46dc6641ad1a9c6032a2c5aa9a13284ab2721856
Michael Martis <martis@chromium.org>
2021-12-16 02:31:25 +0000

    [Snooping protection] Don't cache stale daemon state.
    Since we weren't clearing the cached daemon state, we would erroneously
    report any previous execution's state while we were waiting for a new
    execution to respond. This was easy to miss in tests (where DBus calls
    return instantly) but very obvious on-device.
    
    Bug: chromium:1241704
    Change-Id: I63e7066d214ff3334a937ab825598e522e4ded6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341171
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952212}

--- ash/system/unified/hps_notify_controller.cc
+++ ash/system/unified/hps_notify_controller.cc
@@ -67,7 +67,8 @@ void HpsNotifyController::OnSessionStateChanged(
   const bool session_active =
       session_state == session_manager::SessionState::ACTIVE;
   ReconfigureHps(hps_available_, session_active, pref_enabled_);
-  UpdateIconVisibility(session_active, hps_state_, pref_enabled_);
+  UpdateIconVisibility(session_active, hps_state_ && session_active,
+                       pref_enabled_);
 }
 
 void HpsNotifyController::OnActiveUserPrefServiceChanged(
@@ -77,7 +78,8 @@ void HpsNotifyController::OnActiveUserPrefServiceChanged(
   const bool pref_enabled =
       pref_service->GetBoolean(prefs::kSnoopingProtectionEnabled);
   ReconfigureHps(hps_available_, session_active_, pref_enabled);
-  UpdateIconVisibility(session_active_, hps_state_, pref_enabled);
+  UpdateIconVisibility(session_active_, hps_state_ && pref_enabled,
+                       pref_enabled);
 
   // Re-subscribe to pref changes.
   pref_change_registrar_ = std::make_unique<PrefChangeRegistrar>();
@@ -93,6 +95,8 @@ void HpsNotifyController::OnHpsNotifyChanged(bool hps_state) {
 }
 
 void HpsNotifyController::OnRestart() {
+  DCHECK(!hps_state_);
+
   ReconfigureHps(/*hps_available_=*/true, session_active_, pref_enabled_);
 }
 
@@ -215,7 +219,8 @@ void HpsNotifyController::UpdatePrefState() {
   const bool pref_enabled = pref_change_registrar_->prefs()->GetBoolean(
       prefs::kSnoopingProtectionEnabled);
   ReconfigureHps(hps_available_, session_active_, pref_enabled);
-  UpdateIconVisibility(session_active_, hps_state_, pref_enabled);
+  UpdateIconVisibility(session_active_, hps_state_ && pref_enabled,
+                       pref_enabled);
 }
 
 }  // namespace ash
