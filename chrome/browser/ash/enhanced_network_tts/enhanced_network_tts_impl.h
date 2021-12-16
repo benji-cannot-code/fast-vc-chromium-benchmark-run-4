10fa2351476f207bb6e7f1f65f9f5c1ab4a08b6d
Robert Sesek <rsesek@chromium.org>
2021-12-16 19:35:35 +0000

    In EnhancedNetworkTts, use the DataDecoder C++ API
    The C++ API handles setting a disconnect handler and does not require
    manually binding the Mojo interface.
    
    Bug: 1217301, 1269888
    Change-Id: I988b37dd44c672fb1c4d7e2da94349d513e6704c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344446
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952471}

--- chrome/browser/ash/enhanced_network_tts/enhanced_network_tts_impl.h
+++ chrome/browser/ash/enhanced_network_tts/enhanced_network_tts_impl.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
-#include "services/data_decoder/public/mojom/json_parser.mojom.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -78,10 +77,6 @@ class EnhancedNetworkTtsImpl : public mojom::EnhancedNetworkTts {
   // List of ServerRequest.
   using ServerRequestList = std::list<ServerRequest>;
 
-  // Create or reuse a connection to the data decoder service for safe JSON
-  // parsing.
-  data_decoder::mojom::JsonParser* GetJsonParser();
-
   // Create a URL loader for a network request with an attached API key.
   std::unique_ptr<network::SimpleURLLoader> MakeRequestLoader();
 
@@ -102,8 +97,7 @@ class EnhancedNetworkTtsImpl : public mojom::EnhancedNetworkTts {
   // indicates if this is the last response we expect.
   void OnResponseJsonParsed(const int start_index,
                             const bool is_last_request,
-                            absl::optional<base::Value> json_data,
-                            const absl::optional<std::string>& error);
+                            data_decoder::DataDecoder::ValueOrError result);
 
   // Sends the response to the |mojom::AudioDataObserver|.
   void SendResponse(mojom::TtsResponsePtr response);
@@ -129,9 +123,6 @@ class EnhancedNetworkTtsImpl : public mojom::EnhancedNetworkTts {
 
   // Url loader factory to be loaded.
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
-
-  // Should not be used directly; GetJsonParser() should be called instead.
-  mojo::Remote<data_decoder::mojom::JsonParser> json_parser_;
 
   mojo::Receiver<mojom::EnhancedNetworkTts> receiver_{this};
 
