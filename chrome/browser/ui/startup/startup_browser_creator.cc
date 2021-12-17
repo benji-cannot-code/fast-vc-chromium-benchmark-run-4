6918e318345d71e33e0510fccea52f2bae7b8c6b
Wei4 Wang <wei4.wang@intel.com>
2021-12-17 03:10:24 +0000

    Add --list-apps switch.
    When launch chrome with --list-apps switch, we can output
    open/installed web apps for each profile to a file without launching a
    new browser window or tab. Pass a absolute file path to --list-apps
    switch to specify where to output the information. We can also use a
    optional --profile-base-name switch together to output open/installed
    web apps only for a given profile. Pass the basename of the profile
    directory to --profile-base-name switch to specify which profile to
    get information.
    
    Bug: 1259516
    Design-Doc: https://docs.google.com/document/d/1fzj-F_exyQxd2mkda5rt9nkI6S4nKeP74wJK5sLB5FI
    Change-Id: I00f73db43e3b5989850c3bfff743e9312ba5394c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3255221
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#952643}

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -130,6 +130,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/web_app_url_handling_startup_utils.h"
 #endif
 
+#if defined(OS_LINUX) || defined(OS_MAC) || defined(OS_WIN)
+#include "chrome/browser/ui/startup/web_app_info_recorder_utils.h"
+#endif
+
 using content::BrowserThread;
 using content::ChildProcessSecurityPolicy;
 
@@ -901,6 +905,21 @@ bool StartupBrowserCreator::ProcessCmdLineImpl(
     }
     silent_launch = true;
   }
+
+#if defined(OS_LINUX) || defined(OS_MAC) || defined(OS_WIN)
+  // Writes open and installed web apps to the specified file without
+  // launching a new browser window or tab.
+  if (command_line.HasSwitch(switches::kListApps)) {
+    base::FilePath output_file(
+        command_line.GetSwitchValuePath(switches::kListApps));
+    if (!output_file.empty() && output_file.IsAbsolute()) {
+      base::FilePath profile_base_name(
+          command_line.GetSwitchValuePath(switches::kProfileBaseName));
+      chrome::startup::WriteWebAppsToFile(output_file, profile_base_name);
+    }
+    return true;
+  }
+#endif  //  defined(OS_LINUX) || defined(OS_MAC) || defined(OS_WIN)
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_CHROMEOS_LACROS)
   if (base::FeatureList::IsEnabled(features::kOnConnectNative) &&
