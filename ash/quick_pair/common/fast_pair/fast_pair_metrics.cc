fc0a85ddb29b4c433638cc9d21f19a1f90f59c8d
Juliet Levesque <julietlevesque@google.com>
2021-12-17 00:10:14 +0000

    [Fast Pair] Log the result of creating a Gatt connection.
    Adds a boolean success metric to log the success or failure from
    creating a Gatt connection.
    
    Change-Id: Ie36b6a8c220b4f4617bab7237d027be02a4b9763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331622
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#952591}

--- ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
+++ ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
@@ -28,6 +28,8 @@ const char kRetroactivePairingResultMetric[] =
     "Bluetooth.ChromeOS.FastPair.RetroactivePairing.Result";
 const char kTotalGattConnectionTimeMetric[] =
     "Bluetooth.ChromeOS.FastPair.TotalGattConnectionTime";
+const char kGattConnectionResult[] =
+    "Bluetooth.ChromeOS.FastPair.GattConnection.Result";
 const char kFastPairPairFailureInitialMetric[] =
     "Bluetooth.ChromeOS.FastPair.PairFailure.InitialPairingProtocol";
 const char kFastPairPairFailureSubsequentMetric[] =
@@ -118,6 +120,10 @@ void RecordRetroactivePairingResult(bool success) {
 void RecordTotalGattConnectionTime(base::TimeDelta total_gatt_connection_time) {
   base::UmaHistogramTimes(kTotalGattConnectionTimeMetric,
                           total_gatt_connection_time);
+}
+
+void RecordGattConnectionResult(bool success) {
+  base::UmaHistogramBoolean(kGattConnectionResult, success);
 }
 
 void RecordPairingResult(const Device& device, bool success) {
