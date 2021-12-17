8d1174d216af730735e612ae7f21c889cc0f6cca
Matthew Denton <mpdenton@chromium.org>
2021-12-17 01:02:55 +0000

    Linux: Send OSCrypt raw encryption key to the Network Service
    This enables NetworkService::SetEncryptionKey() for Linux. See the
    referenced bug for details on why this is necessary.
    
    This will call OSCrypt::GetRawEncryptionKey() in the browser process,
    send it to the network service via NetworkService::SetEncryptionKey()
    and finally call OSCrypt::SetRawEncryptionKey() in the network service.
    
    This way the network service doesn't require access to KeyStorage,
    implemented by third party libraries (libsecret, kwallet, etc.), which
    makes it more easily and reliably sandboxable.
    
    This means that starting the network service requires the encrypted key
    to be retrieved from the platform-specific KeyStorage. So, an
    os_crypt::Config must already have been set before ever starting the
    network service. This means modifying the BrowserMainParts of chrome
    and headless to initialize the config early enough.
    
    This also allows us to remove some extra code that initialized os_crypt
    in the network service with a config, which now is unnecessary.
    
    Bug: 1277463
    Change-Id: I47c45a818fc579e094520ffbd1368af6a479b37b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320484
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952609}

--- services/network/network_service.h
+++ services/network/network_service.h
@@ -88,11 +88,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
 
   ~NetworkService() override;
 
-  // Call to inform the NetworkService that OSCrypt::SetConfig() has already
-  // been invoked, so OSCrypt::SetConfig() does not need to be called before
-  // encrypted storage can be used.
-  void set_os_crypt_is_configured();
-
   // Allows late binding if the mojo receiver wasn't specified in the
   // constructor.
   void Bind(mojo::PendingReceiver<mojom::NetworkService> receiver);
@@ -165,12 +160,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
       base::span<const uint8_t> crl_set,
       mojom::NetworkService::UpdateCRLSetCallback callback) override;
   void OnCertDBChanged() override;
-#if defined(OS_LINUX)
-  void SetCryptConfig(mojom::CryptConfigPtr crypt_config) override;
-#endif
-#if defined(OS_WIN) || defined(OS_MAC)
   void SetEncryptionKey(const std::string& encryption_key) override;
-#endif
   void AddAllowedRequestInitiatorForPlugin(
       int32_t process_id,
       const url::Origin& allowed_request_initiator) override;
@@ -251,8 +241,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
 #endif
 
   FirstPartySets* first_party_sets() const { return first_party_sets_.get(); }
-
-  bool os_crypt_config_set() const { return os_crypt_config_set_; }
 
   void set_host_resolver_factory_for_testing(
       std::unique_ptr<net::HostResolver::Factory> host_resolver_factory) {
@@ -369,8 +357,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
       raw_headers_access_origins_by_pid_;
 
   bool quic_disabled_ = false;
-
-  bool os_crypt_config_set_ = false;
 
   std::unique_ptr<CRLSetDistributor> crl_set_distributor_;
 
