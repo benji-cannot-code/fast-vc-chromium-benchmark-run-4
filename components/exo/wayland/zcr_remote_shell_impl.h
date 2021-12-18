2df90f509b5ad74e935364889dc502edd29a0f0c
Kazuki Takise <takise@google.com>
2021-12-18 09:36:49 +0000

    Ensure to call desktop_focus_state in remote_shell
    Since the wayland remote shell protocol was updated to v2,
    desktop_focus_state hasn't been called, which causes Android IME
    to be broken.
    
    This CL re-enables the calling of desktop_focus_state and makes
    sure it's called before the activation event in aura_shell. To
    achieve the latter, the priority observer in seat is used instead
    of the focus changed callback logic.
    
    Ideally, we want to move the desktop_focus_state event to
    aura_shell, but this Cl will be cherry picked into the release
    branches and the spec of the protocol hasn't been confirmed, so
    this Cl uses the existing protocol in remote_shell.
    
    BUG=208315972
    TEST=tast.arc.AndroidIMEInBrowser.vm
    
    Change-Id: I926b867e1451a313e7fb0adccef84fbd24896b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3343520
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952695}

--- components/exo/wayland/zcr_remote_shell_impl.h
+++ components/exo/wayland/zcr_remote_shell_impl.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/client_controlled_shell_surface.h"
 #include "components/exo/input_method_surface.h"
 #include "components/exo/notification_surface.h"
+#include "components/exo/seat.h"
+#include "components/exo/seat_observer.h"
 #include "components/exo/surface.h"
 #include "components/exo/toast_surface.h"
 #include "components/exo/wayland/wayland_display_observer.h"
@@ -47,7 +49,8 @@ class WaylandRemoteOutput : public WaylandDisplayObserver {
 // Implements remote shell interface and monitors workspace state needed
 // for the remote shell interface.
 class WaylandRemoteShell : public ash::TabletModeObserver,
-                           public display::DisplayObserver {
+                           public display::DisplayObserver,
+                           public SeatObserver {
  public:
   using OutputResourceProvider = base::RepeatingCallback<wl_resource*(int64_t)>;
 
@@ -97,6 +100,11 @@ class WaylandRemoteShell : public ash::TabletModeObserver,
   void OnTabletModeStarted() override;
   void OnTabletModeEnding() override;
   void OnTabletModeEnded() override;
+
+  // Overridden from SeatObserver:
+  void OnSurfaceFocused(Surface* gained_focus,
+                        Surface* lost_focus,
+                        bool has_focused_surface) override;
 
   WaylandRemoteShellEventMapping const event_mapping_;
 
@@ -164,7 +172,11 @@ class WaylandRemoteShell : public ash::TabletModeObserver,
 
   base::flat_map<wl_resource*, BoundsChangeData> pending_bounds_changes_;
 
+  bool last_has_focused_client_ = false;
+
   display::ScopedDisplayObserver display_observer_{this};
+
+  Seat* const seat_;
 
   base::WeakPtrFactory<WaylandRemoteShell> weak_ptr_factory_{this};
 
