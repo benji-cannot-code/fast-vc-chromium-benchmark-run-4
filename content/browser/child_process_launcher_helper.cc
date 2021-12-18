aa846727f583cb9aa9043391baeab04d905888f0
Will Harris <wfh@chromium.org>
2021-12-18 09:21:31 +0000

    Reland "Add launch failure notifications to BrowserChildProcessObserver"
    This is a reland of 893230151e96c29a556395cc3c3d44cced6ee600
    
    The previous version of this CL mistakenly removed a default
    case statement for an 'int' field, so an expected compile error
    was not generated but instead an early return was skipped.
    
    Original change's description:
    > Add launch failure notifications to BrowserChildProcessObserver
    >
    > Also, fix a bug where the Windows sandbox would return SBOX_ALL_OK
    > even if base::LaunchProcess failed, and launch_result was not
    > being set for elevated processes.
    >
    > Add reporting of GetLastError on Windows in the
    > ChildProcessTerminationInfo for failed launches.
    >
    > Also, clean up some switch statements to remove default cases.
    >
    > BUG=1280005
    >
    > Change-Id: I1001fc950b8456b78ef1a9a985ca07cf288e8a04
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340072
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#952182}
    
    Bug: 1280005, 1280541
    Change-Id: I81f3354e9870a455644e77144a40c4acbdf14ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3345720
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952667}

--- content/browser/child_process_launcher_helper.cc
+++ content/browser/child_process_launcher_helper.cc
@@ -145,7 +145,6 @@ void ChildProcessLauncherHelper::LaunchOnLauncherThread() {
                                       can_use_warm_up_connection_,
 #endif
                                       &is_synchronous_launch, &launch_result);
-
     AfterLaunchOnLauncherThread(process, options);
   }
 
@@ -157,6 +156,11 @@ void ChildProcessLauncherHelper::LaunchOnLauncherThread() {
 void ChildProcessLauncherHelper::PostLaunchOnLauncherThread(
     ChildProcessLauncherHelper::Process process,
     int launch_result) {
+#if defined(OS_WIN)
+  // The LastError is set on the launcher thread, but needs to be transferred to
+  // the Client thread.
+  DWORD last_error = ::GetLastError();
+#endif
   if (mojo_channel_)
     mojo_channel_->RemoteProcessLaunchAttempted();
 
@@ -190,14 +194,25 @@ void ChildProcessLauncherHelper::PostLaunchOnLauncherThread(
   client_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&ChildProcessLauncherHelper::PostLaunchOnClientThread,
-                     this, std::move(process), launch_result));
+                     this, std::move(process),
+#if defined(OS_WIN)
+                     last_error,
+#endif
+                     launch_result));
 }
 
 void ChildProcessLauncherHelper::PostLaunchOnClientThread(
     ChildProcessLauncherHelper::Process process,
+#if defined(OS_WIN)
+    DWORD last_error,
+#endif
     int error_code) {
   if (child_process_launcher_) {
-    child_process_launcher_->Notify(std::move(process), error_code);
+    child_process_launcher_->Notify(std::move(process),
+#if defined(OS_WIN)
+                                    last_error,
+#endif
+                                    error_code);
   } else if (process.process.IsValid() && terminate_on_shutdown_) {
     // Client is gone, terminate the process.
     ForceNormalProcessTerminationAsync(std::move(process));
