f59c19354c530b7a9fc0bc56480504e17eeae1eb
Ievgen Rysai <ievgen.rysai@grammarly.com>
2021-12-18 09:36:54 +0000

    Fix setting AXSelectedTextRange attribute.
    Once BrowserAccessibilityCocoa became a subclass of AXPlatformNodeCocoa
    the code path taken for setting AXSelectedTextRange has changed:
    instead of
    `-[BrowserAccessibilityCocoa accessibilitySetValue:forAttribute:]` OS
    invokes `-[AXPlatformNodeCocoa setAccessibilitySelectedTextRange:]`
    directly which does not work correctly on web text fields.
    
    This CL moves the correct text selection logic into
    `-[BrowserAccessibilityCocoa setAccessibilitySelectedTextRange:]`
    override.
    
    Bug: 1280252
    Change-Id: I2444ead9d3c9d4563d9be6d2f2c5fb4e711deb8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341535
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952696}

--- content/browser/accessibility/browser_accessibility_cocoa.mm
+++ content/browser/accessibility/browser_accessibility_cocoa.mm
@@ -1933,6 +1933,17 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
   return [NSValue valueWithRange:NSMakeRange(selStart, selLength)];
 }
 
+- (void)setAccessibilitySelectedTextRange:(NSRange)range {
+  if (![self instanceActive])
+    return;
+
+  BrowserAccessibilityManager* manager = _owner->manager();
+  manager->SetSelection(BrowserAccessibility::AXRange(
+      _owner->CreateTextPositionAt(range.location)->AsTextSelectionPosition(),
+      _owner->CreateTextPositionAt(NSMaxRange(range))
+          ->AsTextSelectionPosition()));
+}
+
 - (id)selectedTextMarkerRange {
   if (![self instanceActive])
     return nil;
@@ -2796,16 +2807,18 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
     return CGRectIsNull(rect) ? nil : [NSValue valueWithRect:rect];
   }
 
-  if ([attribute isEqualToString:
-                   NSAccessibilityUIElementCountForSearchPredicateParameterizedAttribute]) {
+  if ([attribute
+          isEqualToString:
+              NSAccessibilityUIElementCountForSearchPredicateParameterizedAttribute]) {
     OneShotAccessibilityTreeSearch search(_owner);
     if (InitializeAccessibilityTreeSearch(&search, parameter))
       return @(search.CountMatches());
     return nil;
   }
 
-  if ([attribute isEqualToString:
-                     NSAccessibilityUIElementsForSearchPredicateParameterizedAttribute]) {
+  if ([attribute
+          isEqualToString:
+              NSAccessibilityUIElementsForSearchPredicateParameterizedAttribute]) {
     OneShotAccessibilityTreeSearch search(_owner);
     if (InitializeAccessibilityTreeSearch(&search, parameter)) {
       size_t count = search.CountMatches();
@@ -3447,13 +3460,7 @@ id content::AXTextMarkerRangeFrom(id anchor_textmarker, id focus_textmarker) {
   }
   if ([attribute isEqualToString:NSAccessibilitySelectedTextRangeAttribute]) {
     if (content::IsNSRange(value)) {
-      NSRange range = [(NSValue*)value rangeValue];
-      BrowserAccessibilityManager* manager = _owner->manager();
-      manager->SetSelection(BrowserAccessibility::AXRange(
-          _owner->CreateTextPositionAt(range.location)
-              ->AsTextSelectionPosition(),
-          _owner->CreateTextPositionAt(NSMaxRange(range))
-              ->AsTextSelectionPosition()));
+      [self setAccessibilitySelectedTextRange:[(NSValue*)value rangeValue]];
     }
   }
   if ([attribute
