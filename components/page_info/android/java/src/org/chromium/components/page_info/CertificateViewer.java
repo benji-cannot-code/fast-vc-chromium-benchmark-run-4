d22a6e2336d23abd02584b8e4591799519250bfe
Christian Dullweber <dullweber@chromium.org>
2021-12-18 09:59:29 +0000

    PageInfo: Use AppCompatTextView
    Replace "new TextView" with "new AppCompatTextView" in page_info.
    
    Change-Id: Ic231921e20b6ee6cafe4f330c39dbdd283849225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3338350
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952731}

--- components/page_info/android/java/src/org/chromium/components/page_info/CertificateViewer.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/CertificateViewer.java
@@ -20,6 +20,7 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.view.ViewCompat;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -93,7 +94,7 @@ public class CertificateViewer implements OnItemSelectedListener {
         LinearLayout dialogContainer = new LinearLayout(mContext);
         dialogContainer.setOrientation(LinearLayout.VERTICAL);
 
-        TextView title = new TextView(mContext);
+        TextView title = new AppCompatTextView(mContext);
         title.setText(R.string.certtitle);
         title.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         ApiCompatibilityUtils.setTextAppearance(title, android.R.style.TextAppearance_Large);
@@ -208,7 +209,7 @@ public class CertificateViewer implements OnItemSelectedListener {
     }
 
     private TextView addLabel(LinearLayout certificateView, String label) {
-        TextView t = new TextView(mContext);
+        TextView t = new AppCompatTextView(mContext);
         t.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         t.setPadding(mPadding, mPadding / 2, mPadding, 0);
         t.setText(label);
@@ -218,7 +219,7 @@ public class CertificateViewer implements OnItemSelectedListener {
     }
 
     private void addValue(LinearLayout certificateView, String value) {
-        TextView t = new TextView(mContext);
+        TextView t = new AppCompatTextView(mContext);
         t.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         t.setText(value);
         t.setPadding(mPadding, 0, mPadding, mPadding / 2);
