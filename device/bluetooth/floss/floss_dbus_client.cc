17740dc1b982820761a0da47364a28fc45a0913b
Sonny Sasaka <sonnysasaka@chromium.org>
2021-12-18 09:57:27 +0000

    [Floss] Implement BluetoothDevice::Forget
    Also fixes the signature of CreateBond and CancelBondProcess.
    
    Bug: b:202334519
    Change-Id: Ice1c4646861329820144c8496f73c18892c6e289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3343276
    Reviewed-by: Abhishek Pandit-Subedi <abhishekpandit@google.com>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952729}

--- device/bluetooth/floss/floss_dbus_client.cc
+++ device/bluetooth/floss/floss_dbus_client.cc
@@ -21,6 +21,11 @@ template <typename T>
 bool ReadReturnFromResponse(dbus::MessageReader* reader, T* value);
 
 template <>
+bool ReadReturnFromResponse(dbus::MessageReader* reader, bool* value) {
+  return reader->PopBool(value);
+}
+
+template <>
 bool ReadReturnFromResponse(dbus::MessageReader* reader, uint8_t* value) {
   return reader->PopByte(value);
 }
@@ -77,6 +82,7 @@ const char kStartDiscovery[] = "StartDiscovery";
 const char kCancelDiscovery[] = "CancelDiscovery";
 const char kCreateBond[] = "CreateBond";
 const char kCancelBondProcess[] = "CancelBondProcess";
+const char kRemoveBond[] = "RemoveBond";
 const char kGetConnectionState[] = "GetConnectionState";
 const char kGetBondState[] = "GetBondState";
 const char kConnectAllEnabledProfiles[] = "ConnectAllEnabledProfiles";
@@ -202,6 +208,11 @@ void FlossDBusClient::DefaultResponseWithCallback(
       ErrorResponseToError(kErrorNoResponse, /*default_message=*/std::string(),
                            error_response));
 }
+
+template void FlossDBusClient::DefaultResponseWithCallback(
+    ResponseCallback<bool> callback,
+    dbus::Response* response,
+    dbus::ErrorResponse* error_response);
 
 template void FlossDBusClient::DefaultResponseWithCallback(
     ResponseCallback<uint8_t> callback,
