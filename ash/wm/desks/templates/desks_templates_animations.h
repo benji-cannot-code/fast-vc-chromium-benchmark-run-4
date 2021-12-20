db9ac1f391587d4a6c4e91af445e04d62ccb8845
chinsenj <chinsenj@chromium.org>
2021-12-20 23:23:56 +0000

    desk_templates: Add animation after last template is deleted
    Currently when the user deletes their last template, the template grid
    is instantly hidden and overview mode is returned to the active desk's
    overview grid.
    
    This CL makes it so the aforementioned process isn't instant but rather
    animated.
    
    Test: manual
    Bug: 1272063
    Change-Id: Ief4ec30f864effa66a7a278a478f4d029dd6a838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341643
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953058}

--- ash/wm/desks/templates/desks_templates_animations.h
+++ ash/wm/desks/templates/desks_templates_animations.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WM_DESKS_TEMPLATES_DESKS_TEMPLATES_ANIMATIONS_H_
 #define ASH_WM_DESKS_TEMPLATES_DESKS_TEMPLATES_ANIMATIONS_H_
 
+#include "base/callback_forward.h"
+#include "base/callback_helpers.h"
+
 namespace ui {
 class Layer;
 }  // namespace ui
@@ -17,7 +20,9 @@ namespace ash {
 void PerformFadeInLayer(ui::Layer* layer);
 
 // Animates linear fade out of overview items.
-void PerformFadeOutLayer(ui::Layer* layer);
+void PerformFadeOutLayer(
+    ui::Layer* layer,
+    base::OnceClosure on_animation_ended_callback = base::DoNothing());
 
 }  // namespace ash
 
