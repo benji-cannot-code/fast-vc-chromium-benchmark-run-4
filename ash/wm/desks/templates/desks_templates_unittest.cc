cd55700dbe1a0c51a3cd017bc45a7a7d9353c1f9
Sammie Quon <sammiequon@chromium.org>
2021-12-20 21:35:34 +0000

    desk_templates: Fix shutdown crash when the name view is focused.
    The session could be null when the view is blurred since the view can
    outlive the session for animation. The behavior of renaming is still
    not determined, so this patch just adds a regression test and prevents
    the crash for the meantime.
    
    Test: added regression test
    Change-Id: I725e74240f303b4dd731cbf433ee451548d543ef
    Fixed: 1281422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350793
    Reviewed-by: Richard Chui <richui@chromium.org>
    Reviewed-by: Jeremy Chinsen <chinsenj@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953033}

--- ash/wm/desks/templates/desks_templates_unittest.cc
+++ ash/wm/desks/templates/desks_templates_unittest.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/base/cursor/mojom/cursor_type.mojom-shared.h"
 #include "ui/compositor/layer.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/events/test/event_generator.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -312,6 +313,12 @@ class DesksTemplatesTest : public OverviewTestBase {
     scoped_feature_list_.InitAndEnableFeature(features::kDesksTemplates);
     OverviewTestBase::SetUp();
   }
+
+ protected:
+  // Tests should normally create a local `ScopedAnimationDurationScaleMode`.
+  // Create this object if a non zero scale mode needs to be used during test
+  // tear down.
+  std::unique_ptr<ui::ScopedAnimationDurationScaleMode> animation_scale_;
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
@@ -1815,6 +1822,36 @@ TEST_F(DesksTemplatesTest, EditTemplateNameWithKeyboardNoCrash) {
 
   // Verify that there is no crash after we tab again.
   SendKey(ui::VKEY_TAB);
+}
+
+// Tests that there is no crash when leaving the template name view focused with
+// a changed name during shutdown. Regression test for
+// https://crbug.com/1281422.
+TEST_F(DesksTemplatesTest, EditTemplateNameShutdownNoCrash) {
+  // The fade out animation of the desks templates grid must be enabled for this
+  // crash to have happened.
+  animation_scale_ = std::make_unique<ui::ScopedAnimationDurationScaleMode>(
+      ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
+
+  AddEntry(base::GUID::GenerateRandomV4(), "a", base::Time::Now());
+  AddEntry(base::GUID::GenerateRandomV4(), "b", base::Time::Now());
+
+  OpenOverviewAndShowTemplatesGrid();
+  DesksTemplatesNameView* name_view =
+      GetItemViewFromTemplatesGrid(0)->name_view();
+
+  // Tab until we focus the name view of the first template item.
+  SendKey(ui::VKEY_TAB);
+  SendKey(ui::VKEY_TAB);
+  ASSERT_EQ(name_view, GetHighlightedView());
+
+  // Rename template "a" to template "ddd".
+  SendKey(ui::VKEY_RETURN);
+  SendKey(ui::VKEY_D);
+  SendKey(ui::VKEY_D);
+  SendKey(ui::VKEY_D);
+
+  // Verify that there is no crash while the test tears down.
 }
 
 // Tests that the hovering over the templates name shows the expected cursor.
