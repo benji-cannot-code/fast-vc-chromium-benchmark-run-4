a19ccbb75fda7edf3756b5cf4dc9c6802015532c
Shu Yang <shuyng@google.com>
2021-12-20 18:01:17 +0000

    [DesktopSite] Update mUserAgent in CriticalPersistedTabData to int
    - Update mUserAgent field in CriticalPersistedTabData from Integer to
    int to avoid handling null pointer.
    - Remove USER_AGENT_UNKNOWN state from
    CriticalPersistedTabDataProto.UserAgentType to avoid confusion with
    UNSET state.
    - Update related tests.
    
    Bug: 1251794
    Change-Id: I2a73390c2a6ef008500fd863603492810a51b477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350184
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#952960}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabData.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabData.java
@@ -78,7 +78,7 @@ public class CriticalPersistedTabData extends PersistedTabData {
             new ObserverList<CriticalPersistedTabDataObserver>();
     private boolean mShouldSaveForTesting;
     /** Tab level Request Desktop Site setting. */
-    private @Nullable @TabUserAgent Integer mUserAgent;
+    private @TabUserAgent int mUserAgent;
 
     @VisibleForTesting
     protected CriticalPersistedTabData(Tab tab) {
@@ -356,7 +356,7 @@ public class CriticalPersistedTabData extends PersistedTabData {
     }
 
     @VisibleForTesting
-    static @Nullable @TabUserAgent Integer getUserAgentType(
+    static @TabUserAgent int getUserAgentType(
             CriticalPersistedTabDataProto.UserAgentType protoUserAgent) {
         switch (protoUserAgent) {
             case DEFAULT:
@@ -372,18 +372,13 @@ public class CriticalPersistedTabData extends PersistedTabData {
             default:
                 assert false : "Unexpected deserialization of UserAgentType: " + protoUserAgent;
                 // shouldn't happen
-                return null;
+                return TabUserAgent.DEFAULT;
         }
     }
 
     @VisibleForTesting
     static CriticalPersistedTabDataProto.UserAgentType getUserAgentType(
-            @Nullable @TabUserAgent Integer protoUserAgent) {
-        if (protoUserAgent == null) {
-            assert false : "TabUserAgent should never be null.";
-            // shouldn't happen
-            return CriticalPersistedTabDataProto.UserAgentType.USER_AGENT_UNKNOWN;
-        }
+            @TabUserAgent int protoUserAgent) {
         switch (protoUserAgent) {
             case TabUserAgent.DEFAULT:
                 return CriticalPersistedTabDataProto.UserAgentType.DEFAULT;
@@ -398,7 +393,7 @@ public class CriticalPersistedTabData extends PersistedTabData {
             default:
                 assert false : "Unexpected serialization of UserAgentType: " + protoUserAgent;
                 // shouldn't happen
-                return CriticalPersistedTabDataProto.UserAgentType.USER_AGENT_UNKNOWN;
+                return CriticalPersistedTabDataProto.UserAgentType.DEFAULT;
         }
     }
 
