8cbf0acc7c7501e1c7f02bbb7111529b68588fff
Roman Sorokin <rsorokin@chromium.org>
2021-12-20 17:00:50 +0000

    [ChromeOS][OOBE] Change force-hwid-check-failure-for-test semantics
    Changed the flag to force-hwid-check-result-for-test to be able to force
    success too.
    
    BUG=b:194038054
    
    Change-Id: I8b14cae977e2505126ecc71192a017532ff0c738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350578
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/main@{#952948}

--- chrome/browser/ash/login/hwid_checker.cc
+++ chrome/browser/ash/login/hwid_checker.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/system/sys_info.h"
@@ -151,8 +152,16 @@ bool IsHWIDCorrect(const std::string& hwid) {
 
 bool IsMachineHWIDCorrect() {
   base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
-  if (cmd_line->HasSwitch(switches::kForceHWIDCheckFailureForTest))
-    return false;
+  if (cmd_line->HasSwitch(switches::kForceHWIDCheckResultForTest)) {
+    const std::string check_result =
+        cmd_line->GetSwitchValueASCII(switches::kForceHWIDCheckResultForTest);
+    if (check_result == "failure")
+      return false;
+    if (check_result == "success")
+      return true;
+    NOTREACHED() << "Wrong " << switches::kForceHWIDCheckResultForTest
+                 << "value: " << check_result;
+  }
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   if (cmd_line->HasSwitch(::switches::kTestType))
     return true;
