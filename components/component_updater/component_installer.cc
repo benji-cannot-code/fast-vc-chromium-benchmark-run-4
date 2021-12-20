dc3889528f2b227fdf5c5ff3753f124e1f37d8ba
Ali Juma <ajuma@chromium.org>
2021-12-20 20:33:01 +0000

    [iOS][macOS] Prevent components from getting backed up
    Components installed by the Component Updater can be large (e.g.,
    larger than 1 MB). Backing up these components isn't useful, since
    they can be re-installed by the Component Updater as needed. This
    is particularly significant in the case where backups are saved in
    the cloud (e.g., iCloud backup on iOS), since large files use up
    bandwidth and cloud storage.
    
    This CL prevents backing up components on Apple platforms.
    
    Bug: 1265580
    Change-Id: I6fdc81f5e0818b66779dff9441da887a8a92d605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350846
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953008}

--- components/component_updater/component_installer.cc
+++ components/component_updater/component_installer.cc
@@ -41,6 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/update_client/utils.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
+#if defined(OS_APPLE)
+#include "base/mac/backup_util.h"
+#endif
+
 namespace component_updater {
 
 namespace {
@@ -164,6 +168,12 @@ Result ComponentInstaller::InstallHelper(const base::FilePath& unpack_path,
 
   DCHECK(!base::PathExists(unpack_path));
   DCHECK(base::PathExists(local_install_path));
+
+#if defined(OS_APPLE)
+  // Since components can be large and can be re-downloaded when needed, they
+  // are excluded from backups.
+  base::mac::SetBackupExclusion(local_install_path);
+#endif
 
   const Result result =
       installer_policy_->OnCustomInstall(local_manifest, local_install_path);
