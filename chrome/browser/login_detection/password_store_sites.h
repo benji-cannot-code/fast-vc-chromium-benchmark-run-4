2febd529d3c3635154f7c5712b5a2ab27c552376
Viktor Semeniuk <vsemeniuk@google.com>
2021-12-20 18:10:40 +0000

    [Passwords] Delay PasswordStoreSites GetAllLogins call
    On Android PasswordStoreSites calls PasswordStore::GetAllLogins during
    profile initialisation [1]. This cases a call to fail when Unified
    Password Manager experiment is enabled [2]. This happens because on Java
    side profile isn't initialised yet [3]. Posting a task which calls
    GetAllLogins fixes the issue.
    
    [1]https://screenshot.googleplex.com/5sVpY7uL5fYZeAH
    [2]https://uma.googleplex.com/p/chrome/variations?sid=6b03f3e917a86d392328a2d78351f8a1
    [3]https://source.corp.google.com/clank/java/src/com/google/android/apps/chrome/password_manager/PasswordStoreAndroidGoogleBackendImpl.java;l=166-168
    
    Change-Id: I634a11fdb44833433d4c559d997a3b00b57669bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3326942
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952963}

--- chrome/browser/login_detection/password_store_sites.h
+++ chrome/browser/login_detection/password_store_sites.h
@@ -33,6 +33,10 @@ class PasswordStoreSites
   bool IsSiteInPasswordStore(const GURL& url) const;
 
  private:
+  // Reads all logins from the password store and starts observing the store for
+  //  future changes.
+  void DoDeferredInitialization();
+
   // PasswordStoreInterface::Observer:
   void OnLoginsChanged(
       password_manager::PasswordStoreInterface* store,
