81f8787a2e2a3d2cf48d149f2d90c8da2f31eada
Raymes Khoury <raymes@chromium.org>
2021-12-20 01:16:16 +0000

    Add LockScreenService and implementation
    This adds the mojo interface LockScreenService which will allow a
    renderer to implement the lock screen API described at
    https://github.com/WICG/lock-screen. A test is added with it.
    
    Data is stored in a ValueStore in a similar way to the Chrome Apps
    lockscreen API, as described in:
    https://docs.google.com/document/d/1zy2cHMHF0gIdVXW0QWE2bX6Zfkw2h5QV5cBepl-B7L8/edit#heading=h.fwni50g0985m
    I2P thread: I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/006tkVwj1yw
    
    The data gets stored outside of the user's profile directory so that the
    lock screen profile can access it as described here:
    https://docs.google.com/document/d/1ocWgI51LZ3cjsTjVPgHlM2ymR6rwTceRlmYK0c8pGmo/edit#heading=h.bhrn8mszcke3
    We use a hash of the origin as the directory for storing the data.
    
    Code to encrypt this data will be added in a followup.
    
    Bug: 1268227
    Change-Id: Ief08511dbe12d3b7badf14289b2ddb88378596fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271971
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#952823}

--- chrome/browser/ash/lock_screen_apps/state_controller.cc
+++ chrome/browser/ash/lock_screen_apps/state_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/time/default_tick_clock.h"
@@ -31,7 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user.h"
+#include "content/public/browser/lock_screen_storage.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_features.h"
 #include "crypto/symmetric_key.h"
 #include "extensions/browser/api/lock_screen_data/lock_screen_item_storage.h"
 #include "extensions/browser/app_window/app_delegate.h"
@@ -150,6 +153,14 @@ void StateController::SetPrimaryProfile(Profile* profile) {
   }
 
   InitializeWithCryptoKey(profile, key);
+  if (base::FeatureList::IsEnabled(features::kWebLockScreenApi)) {
+    base::FilePath base_path;
+    base::PathService::Get(chrome::DIR_USER_DATA, &base_path);
+    base_path = base_path.AppendASCII("web_lock_screen_api_data");
+    base_path = base_path.Append(
+        chromeos::ProfileHelper::GetUserIdHashFromProfile(profile));
+    content::LockScreenStorage::GetInstance()->Init(profile, base_path);
+  }
 }
 
 void StateController::Shutdown() {
