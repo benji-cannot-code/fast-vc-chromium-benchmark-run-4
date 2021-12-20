3d7a90bdecec91c5b8924e5e825604876cf40490
Tyler Garrett <tylergarrett@chromium.org>
2021-12-20 21:59:14 +0000

    Reporting: Add healthd to NetworkTelemetrySampler
    This change adds healthd sampling to the network health telemetry
    sampler. this sampler samples the kNetworkInterfaces probe category.
    
    Change-Id: I897bf1f9439a58aaaa028e06a039ca04271ef757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3345964
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Tyler Garrett <tylergarrett@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953043}

--- chrome/browser/ash/policy/reporting/metrics_reporting/network/network_telemetry_sampler.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/network/network_telemetry_sampler.cc
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_metric_sampler.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "chromeos/network/network_type_pattern.h"
+#include "chromeos/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
 #include "components/reporting/proto/synced/metric_data.pb.h"
 #include "third_party/cros_system_api/dbus/shill/dbus-constants.h"
 
@@ -99,7 +101,11 @@ void OnHttpsLatencySamplerCompleted(MetricCallback callback,
     }
   }
 
-  std::move(callback).Run(metric_data);
+  CrosHealthdMetricSampler sampler(
+      chromeos::cros_healthd::mojom::ProbeCategoryEnum::kNetworkInterface,
+      ::reporting::CrosHealthdMetricSampler::MetricType::kTelemetry);
+  sampler.SetMetricData(std::move(metric_data));
+  sampler.Collect(std::move(callback));
 }
 }  // namespace
 
