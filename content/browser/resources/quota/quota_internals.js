9a0a9ede05bb2464267d646cb884fd08edcc421d
Christine Smith <christinesm@chromium.org>
2021-12-20 20:56:06 +0000

    Quota-Internals (chrome://quota-internals-2): Eviction Statistics
    Changes:
    - Connects GetStatistics to new Quota Internals mojo interface (set up
    in CL 3287686).
    - Displays data retrieved from GetStatistics to the Eviction Statistics section of the
    Quota Internals 2 UI.
    
    Bug: 1202165
    Change-Id: I0ccfccd1db232967ceff0b6527e53df620634a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3339618
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Christine Smith <christinesm@chromium.org>
    Commit-Queue: Christine Smith <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953016}

--- content/browser/resources/quota/quota_internals.js
+++ content/browser/resources/quota/quota_internals.js
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {$} from 'chrome://resources/js/util.m.js';
 import {QuotaInternalsHandler, QuotaInternalsHandlerRemote} from './quota_internals.mojom-webui.js';
 
-function RenderData(response) {
+/**
+ * @param response
+ */
+function renderDiskAvailability(response) {
   const rowTemplate = /** @type {!HTMLTemplateElement} */ ($('listener-row'));
   const tableBody = /** @type {!HTMLTableElement} */ ($('listeners-tbody'));
   const listenerRowTemplate =
@@ -28,13 +31,37 @@ function RenderData(response) {
   tableBody.append(listenerRow);
 }
 
+/**
+ * @param response
+ */
+function renderEvictionStats(response) {
+  const rowTemplate = /** @type {!HTMLTemplateElement} */ ($('eviction-row'));
+  const evictionRowTemplate =
+      /** @type {!HTMLTemplateElement} */ (rowTemplate.cloneNode(true));
+  const evictionRow = evictionRowTemplate.content;
+
+  const tableBody = /** @type {!HTMLTableElement} */ ($('eviction-tbody'));
+  tableBody.innerHTML = trustedTypes.emptyHTML;
+
+  evictionRow.querySelector('.errors-on-getting-usage-and-quota').textContent =
+      response.evictionStatistics['errors-on-getting-usage-and-quota'];
+  evictionRow.querySelector('.evicted-buckets').textContent =
+      response.evictionStatistics['evicted-buckets'];
+  evictionRow.querySelector('.eviction-rounds').textContent =
+      response.evictionStatistics['eviction-rounds'];
+  evictionRow.querySelector('.skipped-eviction-rounds').textContent =
+      response.evictionStatistics['skipped-eviction-rounds'];
+
+  tableBody.appendChild(evictionRow);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   /**
    * @type {!QuotaInternalsHandlerRemote}
    */
   const pageHandler = QuotaInternalsHandler.getRemote();
 
-  pageHandler.getDiskAvailability().then((response) => {
-    RenderData(response);
-  });
+  pageHandler.getDiskAvailability().then(renderDiskAvailability);
+  pageHandler.getStatistics().then(renderEvictionStats);
+
 });
