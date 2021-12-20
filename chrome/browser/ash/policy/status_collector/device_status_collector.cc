5daf10a0155697082670c6d2b917868266283113
Ahmed Nasr <anasr@google.com>
2021-12-20 18:46:18 +0000

    Add network info sampler.
    This matches the network info collection in device_status_collector.
    
    Bug: b:177847653
    Change-Id: I131b6b371e5253d823a3ecdd76d7c9b5a4738c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3324419
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#952974}

--- chrome/browser/ash/policy/status_collector/device_status_collector.cc
+++ chrome/browser/ash/policy/status_collector/device_status_collector.cc
@@ -2335,6 +2335,7 @@ bool DeviceStatusCollector::GetWriteProtectSwitch(
 
 bool DeviceStatusCollector::GetNetworkConfiguration(
     em::DeviceStatusReportRequest* status) {
+  // Note: keep in sync with `::reporting::NetworkInfoSampler`
   static const struct {
     const char* type_string;
     em::NetworkInterface::NetworkDeviceType type_constant;
