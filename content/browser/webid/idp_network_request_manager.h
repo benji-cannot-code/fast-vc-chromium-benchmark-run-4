985be646fea17b5ab07d573792d062f59ced6fb2
Yi Gu <yigu@chromium.org>
2021-12-21 23:51:44 +0000

    [FedCM] Refactor error message handling logic for fetching events
    This patch helps developers better understanding errors when calling the
    API. e.g. when there's an error due to fetching accounts list endpoint,
    an error message with detailed causes will be available to developers
    for debugging.
    
    This patch also applies the same logic to fetching client metadata.
    
    Bug: 1275825
    Change-Id: Ifc91e0a7b2973e7cb455302086dab4203aee4ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3351226
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953371}

--- content/browser/webid/idp_network_request_manager.h
+++ content/browser/webid/idp_network_request_manager.h
@@ -62,28 +62,16 @@ class CONTENT_EXPORT IdpNetworkRequestManager {
  public:
   enum class FetchStatus {
     kSuccess,
-    kWebIdNotSupported,
-    kFetchError,
+    kHttpNotFoundError,
+    kNoResponseError,
     kInvalidResponseError,
+    kInvalidRequestError,
   };
 
   enum class SigninResponse {
     kLoadIdp,
     kTokenGranted,
     kSigninError,
-    kInvalidResponseError,
-  };
-
-  enum class AccountsResponse {
-    kSuccess,
-    kNetError,
-    kInvalidResponseError,
-  };
-
-  enum class TokenResponse {
-    kSuccess,
-    kNetError,
-    kInvalidRequestError,
     kInvalidResponseError,
   };
 
@@ -124,9 +112,9 @@ class CONTENT_EXPORT IdpNetworkRequestManager {
   using SigninRequestCallback =
       base::OnceCallback<void(SigninResponse, const std::string&)>;
   using AccountsRequestCallback = base::OnceCallback<
-      void(AccountsResponse, AccountList, IdentityProviderMetadata)>;
+      void(FetchStatus, AccountList, IdentityProviderMetadata)>;
   using TokenRequestCallback =
-      base::OnceCallback<void(TokenResponse, const std::string&)>;
+      base::OnceCallback<void(FetchStatus, const std::string&)>;
   using RevokeCallback = base::OnceCallback<void(RevokeResponse)>;
   using LogoutCallback = base::OnceCallback<void()>;
 
