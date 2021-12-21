7e89036e2f4aa3262dcf335d1a7a77567acf2b09
Renato Silva <rrsilva@google.com>
2021-12-21 15:06:14 +0000

    OOBE - Polymer3 migration - notification_card
    Migrate notification_card to Polymer3 and ES6 syntax.
    
    Bug: 1184731
    Change-Id: I58bda53c9a57228efe072fc33fbac6cb42cd07c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3351975
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#953204}

--- chrome/browser/resources/chromeos/login/components/notification_card.js
+++ chrome/browser/resources/chromeos/login/components/notification_card.js
@@ -1,7 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2021 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+/* #js_imports_placeholder */
 
 /** @enum {string} */
 let NotificationCardType = {
@@ -9,16 +11,23 @@ let NotificationCardType = {
   SUCCESS: 'success',
 };
 
-Polymer({
-  is: 'notification-card',
+class NotificationCard extends Polymer.Element {
 
-  properties: {
-    buttonLabel: {type: String, value: ''},
+  static get is() {
+    return 'notification-card';
+  }
 
-    linkLabel: {type: String, value: ''},
+  /* #html_template_placeholder */
 
-    type: {type: String, value: ''}
-  },
+  static get properties() {
+    return {
+      buttonLabel: {type: String, value: ''},
+
+      linkLabel: {type: String, value: ''},
+
+      type: {type: String, value: ''}
+    };
+  }
 
   /**
    * @param {NotificationCardType} type
@@ -31,24 +40,28 @@ Polymer({
       return 'oobe-24:done';
     console.error('Unknown type "' + type + '".');
     return '';
-  },
+  }
 
   /** @private */
   buttonClicked_() {
-    this.fire('buttonclick');
-  },
+    this.dispatchEvent(new CustomEvent('buttonclick',
+        { bubbles: true, composed: true }));
+  }
 
   /**
    * @param {Event} e
    * @private
    */
   linkClicked_(e) {
-    this.fire('linkclick');
+    this.dispatchEvent(new CustomEvent('linkclick',
+        { bubbles: true, composed: true }));
     e.preventDefault();
-  },
+  }
 
   /** @type {Element} */
   get submitButton() {
     return this.$.submitButton;
   }
-});
+}
+
+customElements.define(NotificationCard.is, NotificationCard);
