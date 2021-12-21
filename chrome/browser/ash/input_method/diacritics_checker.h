c844d837ef2b487b859f6f3f1e066c4af4497c10
Bao-Duy Tran <tranbaoduy@chromium.org>
2021-12-21 02:21:00 +0000

    Migrate non-Autocorrect diacritic status metrics to System PK.
    These are meant for CrOS 1P multilingual experimental input methods.
    They used to be set up in 1P VK+IME extension based on legacy
    Extension-based physical keyboard (PK) code path when Latin System
    PK was work-in-progress (http://cl/373099876).
    
    Now that Latin System PK has graduated, its flag removed and Latin
    Extension-based PK is being dismantled (https://crrev.com/c/3267728),
    these experimental input methods got broken and have been migrated
    to use System PK (https://crrev.com/c/3344469), so these metrics
    should be migrated as well.
    
    NOTE: The above is only for the physical keyboard. Virtual keyboard
    (VK) still goes through the CrOS 1P VK+IME extension for these
    experimental input methods, so previous setup in the VK+IME extension
    (http://cl/373099876) shall remain for the VK.
    
    Bug: 1273302
    Change-Id: If7c36c61d1b34ec3b33eee38d3ab2773d865d7ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308293
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953088}

---
+++ chrome/browser/ash/input_method/diacritics_checker.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_INPUT_METHOD_DIACRITICS_CHECKER_H_
+#define CHROME_BROWSER_ASH_INPUT_METHOD_DIACRITICS_CHECKER_H_
+
+#include <string>
+
+namespace ash {
+namespace input_method {
+
+// Checks if the given UTF-16 text contains some accented letters.
+bool HasDiacritics(const std::u16string& text);
+
+}  // namespace input_method
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_INPUT_METHOD_DIACRITICS_CHECKER_H_
