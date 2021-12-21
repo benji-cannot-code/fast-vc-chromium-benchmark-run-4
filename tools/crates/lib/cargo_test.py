e14432f9a477b616530d31c9e79c0e7eff105f6e
danakj <danakj@chromium.org>
2021-12-21 16:20:36 +0000

    Add crates.py script to import third-party Rust crates
    The script has 2 commands:
    
    1) gen
    
    This will parse third_party/rust/third_party.toml to find the first-
    party rust dependencies, and from there use `cargo tree` to find all
    dependencies and needed features for all crates. It will do this for all
    supported build-target-platforms.
    
    Any missing crates will be asked to be downloaded with 2).
    
    Once all crates exist, it will use the completed graph of dependencies,
    features, and build-target-platforms, to generate BUILD.gn rules in
    third_party/rust/CRATE_NAME/vEPOCH/BUILD.gn for every crate in the
    dependency tree.
    
    2) download CRATE_NAME VERSION
    
    The version can be partially-specified (such as an epoch) or a full
    version. If partially-specified, this will find the newest matching
    version on crates.io. The tool will download the resulting version of
    the crate to third_party/rust/CRATE_NAME/vEPOCH/crate/ and write a
    readme file to third_party/rust/CRATE_NAME/vEPOCH/README.chromium.
    
    Change-Id: I2d6535a3c84563c89ef9735b773cc392eedd400b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3212915
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: danakj chromium <danakj@chromium.org>
    Auto-Submit: danakj chromium <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953230}

---
+++ tools/crates/lib/cargo_test.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# python3
+# Copyright 2021 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from lib import cargo
+
+
+class TestCargo(unittest.TestCase):
+    def test_output_to_usage(self):
+        self.assertEqual(cargo.CrateBuildOutput.NORMAL.as_dep_usage(),
+                         cargo.CrateUsage.FOR_NORMAL)
+        self.assertEqual(cargo.CrateBuildOutput.BUILDRS.as_dep_usage(),
+                         cargo.CrateUsage.FOR_BUILDRS)
+        self.assertEqual(cargo.CrateBuildOutput.TESTS.as_dep_usage(),
+                         cargo.CrateUsage.FOR_TESTS)
