6622fe99c869f466b9908a9b7ed986a705d32ab0
Jason Zhang <jiajunz@google.com>
2021-12-21 18:41:43 +0000

    [CrOS Cellular] Update reset euicc inhibit string
    This CL updates the reset euicc inhibit string in the cellular network
    list UI and the tooltip of add esim button when the device is under
    resetting eSIM.
    
    Bug: b/210536995
    Change-Id: Iff5d6b89926672645c002256843d05149f4d388b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347723
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#953272}

--- chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.js
+++ chrome/browser/resources/settings/chromeos/internet_page/cellular_networks_list.js
@@ -565,8 +565,7 @@ Polymer({
       case mojom.kRefreshingProfileList:
         return this.i18n('cellularNetworRefreshingProfileListProfile');
       case mojom.kResettingEuiccMemory:
-        // TODO(crbug.com/1231305) Fix string when finalized.
-        return this.i18n('cellularNetworkRemovingProfile');
+        return this.i18n('cellularNetworkResettingESim');
     }
 
     return '';
