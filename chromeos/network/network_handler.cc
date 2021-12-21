f7a65105714b2c193f7e054c058ac4a054a6680e
Regan Hsu <hsuregan@google.com>
2021-12-21 20:15:45 +0000

    [CrOS Network] Add the ConnectionInfoMetricsLogger.
    * Adds the ConnectionInfoMetricsLogger class which observes various
      network events needed to log general network metrics TBA.
    * Emits connection success of general network types to a patterned
      histogram.
    * Changes histogram names and adds to xml histograms.
    * Adds Shill Connect Result xml enums.
    
    Bug: b/207589664
    Change-Id: I32bf2fd101bb476a8c6a8da03ac3fccec19bafe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341072
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953301}

--- chromeos/network/network_handler.cc
+++ chromeos/network/network_handler.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/client_cert_resolver.h"
 #include "chromeos/network/geolocation_handler.h"
 #include "chromeos/network/managed_network_configuration_handler_impl.h"
+#include "chromeos/network/metrics/connection_info_metrics_logger.h"
 #include "chromeos/network/metrics/esim_policy_login_metrics_logger.h"
 #include "chromeos/network/network_activation_handler_impl.h"
 #include "chromeos/network/network_cert_loader.h"
@@ -60,6 +61,7 @@ NetworkHandler::NetworkHandler()
     esim_policy_login_metrics_logger_.reset(new ESimPolicyLoginMetricsLogger());
   }
   cellular_metrics_logger_.reset(new CellularMetricsLogger());
+  connection_info_metrics_logger_.reset(new ConnectionInfoMetricsLogger());
   if (NetworkCertLoader::IsInitialized()) {
     network_cert_migrator_.reset(new NetworkCertMigrator());
     client_cert_resolver_.reset(new ClientCertResolver());
@@ -118,6 +120,8 @@ void NetworkHandler::Init() {
   cellular_metrics_logger_->Init(network_state_handler_.get(),
                                  network_connection_handler_.get(),
                                  cellular_esim_profile_handler_.get());
+  connection_info_metrics_logger_->Init(network_state_handler_.get(),
+                                        network_connection_handler_.get());
   if (network_cert_migrator_)
     network_cert_migrator_->Init(network_state_handler_.get());
   if (client_cert_resolver_) {
