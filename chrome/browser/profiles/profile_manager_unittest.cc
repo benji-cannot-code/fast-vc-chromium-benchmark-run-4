benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 8f716497c091514a837b25493c73afe33f17304d

[Profiles] Wipe profile dir before creating a new multi profile
This CL adds additional checks when creating a new profile. This guards
against the situation that prefs::kProfilesNumCreated is out of sync
with profiles cache / disk. This can happen during a crash.

Bug: 1268945
Change-Id: I678eae1dbbe31ae4e479b4e19a5b1b22ffbfedbb
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3306361
Reviewed-by: David Roger <droger@chromium.org>
Commit-Queue: Jan Krčál <jkrcal@chromium.org>
Cr-Commit-Position: refs/heads/main@{#950007}

chrome/browser/profiles/profile_manager_unittest.cc
@@ -233,6 +233,8 @@ class ProfileManagerTest : public testing::Test {
     entry->SetIsEphemeral(true);
   }
 
+  TestingPrefServiceSimple* local_state() { return local_state_.Get(); }
+
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   void OnWillCreateBrowserContextServices(content::BrowserContext* context) {
     IdentityTestEnvironmentProfileAdaptor::
@@ -645,6 +647,97 @@ TEST_F(ProfileManagerTest, CreateMultiProfileAsyncMultipleRequests) {
   EXPECT_NE(profile2, profile3);
 }
 
+TEST_F(ProfileManagerTest,
+       CreateMultiProfilesAsyncWithBrokenPrefAndProfileInCache) {
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
+  ASSERT_TRUE(profile_manager);
+
+  // Simulate having "Profile 1" in the storage without properly incrementing
+  // the prefs::kProfilesNumCreated counter.
+  ProfileAttributesInitParams params_1;
+  params_1.profile_path =
+      profile_manager->user_data_dir().AppendASCII("Profile 1");
+  params_1.profile_name = u"name_1";
+  params_1.gaia_id = "12345";
+  params_1.is_consented_primary_account = true;
+  profile_manager->GetProfileAttributesStorage().AddProfile(
+      std::move(params_1));
+
+  MockObserver mock_observer2;
+  Profile* profile2 = nullptr;
+  EXPECT_CALL(mock_observer2, OnProfileCreated(testing::NotNull(), NotFail()))
+      .Times(testing::AtLeast(2))
+      .WillRepeatedly(testing::SaveArg<0>(&profile2));
+  CreateMultiProfileAsync(profile_manager, "Profile B", &mock_observer2);
+  content::RunAllTasksUntilIdle();
+
+  ASSERT_NE(profile2, nullptr);
+  EXPECT_EQ(profile2->GetPath().BaseName().value(),
+            FILE_PATH_LITERAL("Profile 2"));
+  ProfileAttributesEntry* entry2 =
+      profile_manager->GetProfileAttributesStorage()
+          .GetProfileAttributesWithPath(profile2->GetPath());
+  ASSERT_NE(entry2, nullptr);
+}
+
+TEST_F(ProfileManagerTest,
+       PRE_CreateMultiProfilesAsyncWithBrokenPrefAndProfileOnDisk) {
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
+  ASSERT_TRUE(profile_manager);
+
+  MockObserver mock_observer1;
+  Profile* profile1 = nullptr;
+  EXPECT_CALL(mock_observer1, OnProfileCreated(testing::NotNull(), NotFail()))
+      .Times(testing::AtLeast(2))
+      .WillRepeatedly(testing::SaveArg<0>(&profile1));
+  CreateMultiProfileAsync(profile_manager, "Profile A", &mock_observer1);
+  content::RunAllTasksUntilIdle();
+
+  ASSERT_NE(profile1, nullptr);
+  EXPECT_EQ(profile1->GetPath().BaseName().value(),
+            FILE_PATH_LITERAL("Profile 1"));
+  ProfileAttributesEntry* entry1 =
+      profile_manager->GetProfileAttributesStorage()
+          .GetProfileAttributesWithPath(profile1->GetPath());
+  ASSERT_NE(entry1, nullptr);
+
+  // Decrement next profile number to simulate it was never incremented.
+  EXPECT_EQ(local_state()->GetUserPref(prefs::kProfilesNumCreated)->GetInt(),
+            2);
+  local_state()->SetUserPref(prefs::kProfilesNumCreated,
+                             std::make_unique<base::Value>(1));
+  // Wipe the profile from profile attributes storage to simulate it got deleted
+  // but not wiped from disk.
+  profile_manager->GetProfileAttributesStorage().RemoveProfile(
+      profile1->GetPath());
+}
+
+// We need to restart Chrome in the mean-time to make sure the profile is not
+// loaded in memory.
+TEST_F(ProfileManagerTest,
+       CreateMultiProfilesAsyncWithBrokenPrefAndProfileOnDisk) {
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
+  ASSERT_TRUE(profile_manager);
+
+  MockObserver mock_observer2;
+  Profile* profile2 = nullptr;
+  EXPECT_CALL(mock_observer2, OnProfileCreated(testing::NotNull(), NotFail()))
+      .Times(testing::AtLeast(2))
+      .WillRepeatedly(testing::SaveArg<0>(&profile2));
+  CreateMultiProfileAsync(profile_manager, "Profile B", &mock_observer2);
+  content::RunAllTasksUntilIdle();
+
+  ASSERT_NE(profile2, nullptr);
+  // The profile uses the same base name as in the pre test.
+  EXPECT_EQ(profile2->GetPath().BaseName().value(),
+            FILE_PATH_LITERAL("Profile 1"));
+  ProfileAttributesEntry* entry2 =
+      profile_manager->GetProfileAttributesStorage()
+          .GetProfileAttributesWithPath(profile2->GetPath());
+  ASSERT_NE(entry2, nullptr);
+  EXPECT_EQ(base::UTF16ToUTF8(entry2->GetName()), "Profile B");
+}
+
 TEST_F(ProfileManagerTest, CreateHiddenProfileAsync) {
   Profile* profile = nullptr;
   MockObserver mock_observer;
@@ -843,14 +936,14 @@ TEST_F(ProfileManagerTest, AutoloadProfilesWithBackgroundApps) {
   ProfileManager* profile_manager = g_browser_process->profile_manager();
   ProfileAttributesStorage& storage =
       profile_manager->GetProfileAttributesStorage();
-  local_state_.Get()->SetUserPref(prefs::kBackgroundModeEnabled,
-                                  std::make_unique<base::Value>(true));
+  local_state()->SetUserPref(prefs::kBackgroundModeEnabled,
+                             std::make_unique<base::Value>(true));
 
   // Setting a pref which is not applicable to a system (i.e., Android in this
   // case) does not necessarily create it. Don't bother continuing with the
   // test if this pref doesn't exist because it will not load the profiles if
   // it cannot verify that the pref for background mode is enabled.
-  if (!local_state_.Get()->HasPrefPath(prefs::kBackgroundModeEnabled))
+  if (!local_state()->HasPrefPath(prefs::kBackgroundModeEnabled))
     return;
 
   EXPECT_EQ(0u, storage.GetNumberOfProfiles());
@@ -893,8 +986,8 @@ TEST_F(ProfileManagerTest, DoNotAutoloadProfilesIfBackgroundModeOff) {
   ProfileManager* profile_manager = g_browser_process->profile_manager();
   ProfileAttributesStorage& storage =
       profile_manager->GetProfileAttributesStorage();
-  local_state_.Get()->SetUserPref(prefs::kBackgroundModeEnabled,
-                                  std::make_unique<base::Value>(false));
+  local_state()->SetUserPref(prefs::kBackgroundModeEnabled,
+                             std::make_unique<base::Value>(false));
 
   EXPECT_EQ(0u, storage.GetNumberOfProfiles());
 

chrome/browser/browser_features.cc
@@ -31,6 +31,11 @@ const base::Feature kColorProviderRedirectionForThemeProvider = {
 const base::Feature kDestroyProfileOnBrowserClose{
     "DestroyProfileOnBrowserClose", base::FEATURE_DISABLED_BY_DEFAULT};
 
+// Nukes profile directory before creating a new profile using
+// ProfileManager::CreateMultiProfileAsync().
+const base::Feature kNukeProfileBeforeCreateMultiAsync{
+    "NukeProfileBeforeCreateMultiAsync", base::FEATURE_ENABLED_BY_DEFAULT};
+
 // Enables executing the browser commands sent by the NTP promos.
 const base::Feature kPromoBrowserCommands{"PromoBrowserCommands",
                                           base::FEATURE_ENABLED_BY_DEFAULT};

chrome/browser/browser_features.h
@@ -24,6 +24,8 @@ extern const base::Feature kColorProviderRedirectionForThemeProvider;
 
 extern const base::Feature kDestroyProfileOnBrowserClose;
 
+extern const base::Feature kNukeProfileBeforeCreateMultiAsync;
+
 extern const base::Feature kPromoBrowserCommands;
 extern const char kBrowserCommandIdParam[];
 

chrome/browser/profiles/profile_manager.cc
@@ -919,48 +919,68 @@ Profile* ProfileManager::GetProfileFromProfileKey(ProfileKey* profile_key) {
 }
 
 // static
-base::FilePath ProfileManager::CreateMultiProfileAsync(
-    const std::u16string& name,
-    size_t icon_index,
-    bool is_hidden,
-    const CreateCallback& callback) {
+void ProfileManager::CreateMultiProfileAsync(const std::u16string& name,
+                                             size_t icon_index,
+                                             bool is_hidden,
+                                             const CreateCallback& callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   DCHECK(!name.empty());
   DCHECK(profiles::IsDefaultAvatarIconIndex(icon_index));
 
   ProfileManager* profile_manager = g_browser_process->profile_manager();
 
-  base::FilePath new_path = profile_manager->GenerateNextProfileDirectoryPath();
-  DCHECK(profile_manager->CanCreateProfileAtPath(new_path));
-
   ProfileAttributesStorage& storage =
       profile_manager->GetProfileAttributesStorage();
-  ProfileAttributesEntry* entry =
-      storage.GetProfileAttributesWithPath(new_path);
-  if (entry) {
-    LOG(WARNING) << "Failed to generate a unique profile name for a new "
-                    "profile. Creation parameters will be ignored, loading an "
-                    "existing profile at path \""
-                 << new_path << "\" instead.";
-  } else {
-    // Add a storage entry early here to set up a new profile with user selected
-    // name and avatar.
-    // These parameters will be used to initialize profile's prefs in
-    // InitProfileUserPrefs(). AddProfileToStorage() will set any missing
-    // attributes after prefs are loaded.
-    // TODO(alexilin): consider using the user data to supply these parameters
-    // to profile.
-    ProfileAttributesInitParams init_params;
-    init_params.profile_path = new_path;
-    init_params.profile_name = name;
-    init_params.icon_index = icon_index;
-    init_params.is_ephemeral = is_hidden;
-    init_params.is_omitted = is_hidden;
-    storage.AddProfile(std::move(init_params));
+  base::FilePath new_path;
+  ProfileAttributesEntry* entry = nullptr;
+  // In some crashes, it can happen that next profile dir (backed up by local
+  // pref) is out-of-sync with profiles dir. Keep generating next profile path
+  // until that path is _not_ present in `storage`.
+  do {
+    new_path = profile_manager->GenerateNextProfileDirectoryPath();
+    // The generated path should be unused and free to use.
+    DCHECK_EQ(profile_manager->GetProfileByPath(new_path), nullptr);
+    DCHECK(profile_manager->CanCreateProfileAtPath(new_path));
+
+    entry = storage.GetProfileAttributesWithPath(new_path);
+  } while (entry != nullptr);
+
+  // Add a storage entry early here to set up a new profile with user selected
+  // name and avatar.
+  // These parameters will be used to initialize profile's prefs in
+  // InitProfileUserPrefs(). AddProfileToStorage() will set any missing
+  // attributes after prefs are loaded.
+  // TODO(alexilin): consider using the user data to supply these parameters
+  // to profile.
+  ProfileAttributesInitParams init_params;
+  init_params.profile_path = new_path;
+  init_params.profile_name = name;
+  init_params.icon_index = icon_index;
+  init_params.is_ephemeral = is_hidden;
+  init_params.is_omitted = is_hidden;
+  storage.AddProfile(std::move(init_params));
+
+  if (!base::FeatureList::IsEnabled(
+          features::kNukeProfileBeforeCreateMultiAsync)) {
+    profile_manager->CreateProfileAsync(new_path, callback);
+    return;
   }
 
-  profile_manager->CreateProfileAsync(new_path, callback);
-  return new_path;
+  // As another check, make sure the generated path is not present in the file
+  // system (there could be orphan profile dirs).
+  // TODO(crbug.com/1277948): There can be a theoretical race condition with a
+  // direct CreateProfileAsync() call that can create the directory before
+  // adding an entry to ProfileAttributesStorage. Creating a new
+  // ProfileAttributesEntry consistently before writing the profile folder to
+  // disk would resolve this.
+  base::ThreadPool::PostTask(
+      FROM_HERE,
+      {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
+      base::BindOnce(&NukeProfileFromDisk, new_path,
+                     base::BindOnce(&ProfileManager::CreateProfileAsync,
+                                    profile_manager->weak_factory_.GetWeakPtr(),
+                                    new_path, callback)));
 }
 
 // static

chrome/browser/profiles/profile_manager.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/threading/thread_checker.h"
 #include "build/build_config.h"
@@ -184,21 +185,19 @@ class ProfileManager : public Profile::Delegate {
   // otherwise return null.
   Profile* GetProfileByPath(const base::FilePath& path) const;
 
-  // Creates a new profile in the next available multiprofile directory.
-  // Directories are named "profile_1", "profile_2", etc., in sequence of
-  // creation. (Because directories can be removed, however, it may be the case
-  // that at some point the list of numbered profiles is not continuous.)
-  // |callback| may be invoked multiple times (for CREATE_STATUS_INITIALIZED
-  // and CREATE_STATUS_CREATED) so binding parameters with bind::Passed() is
-  // prohibited. Returns the file path to the profile that will be created
-  // asynchronously.
-  // If |is_hidden| is true, the new profile will be created as ephemeral
-  // (removed on the next startup) and omitted (not visible in the list of
-  // profiles).
-  static base::FilePath CreateMultiProfileAsync(const std::u16string& name,
-                                                size_t icon_index,
-                                                bool is_hidden,
-                                                const CreateCallback& callback);
+  // Asynchronously creates a new profile in the next available multiprofile
+  // directory. Directories are named "profile_1", "profile_2", etc., in
+  // sequence of creation. (Because directories can be removed, however, it may
+  // be the case that at some point the list of numbered profiles is not
+  // continuous.) |callback| may be invoked multiple times (for
+  // CREATE_STATUS_INITIALIZED and CREATE_STATUS_CREATED) so binding parameters
+  // with bind::Passed() is prohibited. If |is_hidden| is true, the new profile
+  // will be created as ephemeral (removed on the next startup) and omitted (not
+  // visible in the list of profiles).
+  static void CreateMultiProfileAsync(const std::u16string& name,
+                                      size_t icon_index,
+                                      bool is_hidden,
+                                      const CreateCallback& callback);
 
   // Returns the full path to be used for guest profiles.
   static base::FilePath GetGuestProfilePath();
@@ -595,6 +594,8 @@ class ProfileManager : public Profile::Delegate {
   // enough to do as part of the mass refactor CL which introduced
   // |thread_checker_|, ref. https://codereview.chromium.org/2907253003/#msg37.
   THREAD_CHECKER(thread_checker_);
+
+  base::WeakPtrFactory<ProfileManager> weak_factory_{this};
 };
 
 // Same as the ProfileManager, but doesn't initialize some services of the

commit 041d7d052bf0894f3e135d026eff05e443b418e7

[Profiles] Use RunLoop in unittests of CreateMultiProfileAsync()
This CL fixes test failures on Fuchsia, caused by previous CL 3306361.
That CL introduced more elaborate task posting in the process of
creating a new multi profile. That was not compatible with
content::RunAllTasksUntilIdle().

This CL replaces that by a proper RunLoop that gets quit when the
profile gets created.

Bug: 1280334
Change-Id: I7676bfbc5db4f8a28a9e53ac58ca21f65471bae4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347980
Reviewed-by: Alex Ilin <alexilin@chromium.org>
Commit-Queue: Jan Krčál <jkrcal@chromium.org>
Cr-Commit-Position: refs/heads/main@{#953128}

chrome/browser/profiles/profile_manager_unittest.cc
@@ -354,6 +354,14 @@ MATCHER(NotFail, "Profile creation failure status is not reported.") {
          arg == Profile::CREATE_STATUS_INITIALIZED;
 }
 
+MATCHER(Created, "Profile creation status is created.") {
+  return arg == Profile::CREATE_STATUS_CREATED;
+}
+
+MATCHER(Initialized, "Profile creation status is initialized.") {
+  return arg == Profile::CREATE_STATUS_INITIALIZED;
+}
+
 MATCHER(SameNotNull, "The same non-NULL value for all calls.") {
   if (!g_created_profile)
     g_created_profile = arg;
@@ -579,15 +587,17 @@ TEST_F(ProfileManagerTest, CreateMultiProfileAsync) {
 
   const std::string profile_name = "New Profile";
 
+  base::RunLoop run_loop;
   MockObserver mock_observer;
   Profile* profile = nullptr;
-  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(2))
-      .WillRepeatedly(testing::SaveArg<0>(&profile));
+  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), Created()))
+      .WillOnce(testing::SaveArg<0>(&profile));
+  EXPECT_CALL(mock_observer,
+              OnProfileCreated(testing::NotNull(), Initialized()))
+      .WillOnce([&run_loop] { run_loop.Quit(); });
 
   CreateMultiProfileAsync(profile_manager, profile_name, &mock_observer);
-
-  content::RunAllTasksUntilIdle();
+  run_loop.Run();
 
   // Check that the profile name is set correctly both in the profile prefs and
   // in storage.
@@ -604,42 +614,41 @@ TEST_F(ProfileManagerTest, CreateMultiProfilesAsync) {
   const std::string profile_name1 = "New Profile 1";
   const std::string profile_name2 = "New Profile 2";
 
+  base::RunLoop run_loop;
   MockObserver mock_observer;
-  EXPECT_CALL(mock_observer, OnProfileCreated(
-      testing::NotNull(), NotFail())).Times(testing::AtLeast(3));
+  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), Created()))
+      .Times(2);
+  EXPECT_CALL(mock_observer,
+              OnProfileCreated(testing::NotNull(), Initialized()))
+      .WillOnce(testing::Return())
+      .WillOnce([&run_loop] { run_loop.Quit(); });
 
   ProfileManager* profile_manager = g_browser_process->profile_manager();
 
   CreateMultiProfileAsync(profile_manager, profile_name1, &mock_observer);
   CreateMultiProfileAsync(profile_manager, profile_name2, &mock_observer);
-
-  content::RunAllTasksUntilIdle();
+  run_loop.Run();
 }
 
 TEST_F(ProfileManagerTest, CreateMultiProfileAsyncMultipleRequests) {
-  Profile* profile1 = nullptr;
-  MockObserver mock_observer1;
-  EXPECT_CALL(mock_observer1, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(1))
-      .WillRepeatedly(testing::SaveArg<0>(&profile1));
-  Profile* profile2 = nullptr;
-  MockObserver mock_observer2;
-  EXPECT_CALL(mock_observer2, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(1))
-      .WillRepeatedly(testing::SaveArg<0>(&profile2));
-  Profile* profile3 = nullptr;
-  MockObserver mock_observer3;
-  EXPECT_CALL(mock_observer3, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(1))
-      .WillRepeatedly(testing::SaveArg<0>(&profile3));
+  base::RunLoop run_loop;
+  MockObserver mock_observer;
+  Profile *profile1 = nullptr, *profile2 = nullptr, *profile3 = nullptr;
+  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), Created()))
+      .Times(3);
+  EXPECT_CALL(mock_observer,
+              OnProfileCreated(testing::NotNull(), Initialized()))
+      .WillOnce(testing::SaveArg<0>(&profile1))
+      .WillOnce(testing::SaveArg<0>(&profile2))
+      .WillOnce(testing::DoAll(testing::SaveArg<0>(&profile3),
+                               [&run_loop] { run_loop.Quit(); }));
 
   ProfileManager* profile_manager = g_browser_process->profile_manager();
   const std::string profile_name = "New Profile";
-  CreateMultiProfileAsync(profile_manager, profile_name, &mock_observer1);
-  CreateMultiProfileAsync(profile_manager, profile_name, &mock_observer2);
-  CreateMultiProfileAsync(profile_manager, profile_name, &mock_observer3);
-
-  content::RunAllTasksUntilIdle();
+  CreateMultiProfileAsync(profile_manager, profile_name, &mock_observer);
+  CreateMultiProfileAsync(profile_manager, profile_name, &mock_observer);
+  CreateMultiProfileAsync(profile_manager, profile_name, &mock_observer);
+  run_loop.Run();
 
   // A new profile should have been created for each call.
   EXPECT_NE(profile1, profile2);
@@ -663,13 +672,17 @@ TEST_F(ProfileManagerTest,
   profile_manager->GetProfileAttributesStorage().AddProfile(
       std::move(params_1));
 
-  MockObserver mock_observer2;
+  base::RunLoop run_loop;
+  MockObserver mock_observer;
   Profile* profile2 = nullptr;
-  EXPECT_CALL(mock_observer2, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(2))
-      .WillRepeatedly(testing::SaveArg<0>(&profile2));
-  CreateMultiProfileAsync(profile_manager, "Profile B", &mock_observer2);
-  content::RunAllTasksUntilIdle();
+  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), Created()))
+      .WillOnce(testing::SaveArg<0>(&profile2));
+  EXPECT_CALL(mock_observer,
+              OnProfileCreated(testing::NotNull(), Initialized()))
+      .WillOnce([&run_loop] { run_loop.Quit(); });
+
+  CreateMultiProfileAsync(profile_manager, "Profile B", &mock_observer);
+  run_loop.Run();
 
   ASSERT_NE(profile2, nullptr);
   EXPECT_EQ(profile2->GetPath().BaseName().value(),
@@ -685,13 +698,16 @@ TEST_F(ProfileManagerTest,
   ProfileManager* profile_manager = g_browser_process->profile_manager();
   ASSERT_TRUE(profile_manager);
 
-  MockObserver mock_observer1;
+  base::RunLoop run_loop;
+  MockObserver mock_observer;
   Profile* profile1 = nullptr;
-  EXPECT_CALL(mock_observer1, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(2))
-      .WillRepeatedly(testing::SaveArg<0>(&profile1));
-  CreateMultiProfileAsync(profile_manager, "Profile A", &mock_observer1);
-  content::RunAllTasksUntilIdle();
+  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), Created()))
+      .WillOnce(testing::SaveArg<0>(&profile1));
+  EXPECT_CALL(mock_observer,
+              OnProfileCreated(testing::NotNull(), Initialized()))
+      .WillOnce([&run_loop] { run_loop.Quit(); });
+  CreateMultiProfileAsync(profile_manager, "Profile A", &mock_observer);
+  run_loop.Run();
 
   ASSERT_NE(profile1, nullptr);
   EXPECT_EQ(profile1->GetPath().BaseName().value(),
@@ -719,13 +735,16 @@ TEST_F(ProfileManagerTest,
   ProfileManager* profile_manager = g_browser_process->profile_manager();
   ASSERT_TRUE(profile_manager);
 
-  MockObserver mock_observer2;
+  base::RunLoop run_loop;
+  MockObserver mock_observer;
   Profile* profile2 = nullptr;
-  EXPECT_CALL(mock_observer2, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(2))
-      .WillRepeatedly(testing::SaveArg<0>(&profile2));
-  CreateMultiProfileAsync(profile_manager, "Profile B", &mock_observer2);
-  content::RunAllTasksUntilIdle();
+  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), Created()))
+      .WillOnce(testing::SaveArg<0>(&profile2));
+  EXPECT_CALL(mock_observer,
+              OnProfileCreated(testing::NotNull(), Initialized()))
+      .WillOnce([&run_loop] { run_loop.Quit(); });
+  CreateMultiProfileAsync(profile_manager, "Profile B", &mock_observer);
+  run_loop.Run();
 
   ASSERT_NE(profile2, nullptr);
   // The profile uses the same base name as in the pre test.
@@ -739,11 +758,14 @@ TEST_F(ProfileManagerTest,
 }
 
 TEST_F(ProfileManagerTest, CreateHiddenProfileAsync) {
+  base::RunLoop run_loop;
   Profile* profile = nullptr;
   MockObserver mock_observer;
-  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), NotFail()))
-      .Times(testing::AtLeast(2))
-      .WillRepeatedly(testing::SaveArg<0>(&profile));
+  EXPECT_CALL(mock_observer, OnProfileCreated(testing::NotNull(), Created()))
+      .WillOnce(testing::SaveArg<0>(&profile));
+  EXPECT_CALL(mock_observer,
+              OnProfileCreated(testing::NotNull(), Initialized()))
+      .WillOnce([&run_loop] { run_loop.Quit(); });
 
   ProfileManager* profile_manager = g_browser_process->profile_manager();
 
@@ -752,7 +774,7 @@ TEST_F(ProfileManagerTest, CreateHiddenProfileAsync) {
       base::BindRepeating(&MockObserver::OnProfileCreated,
                           base::Unretained(&mock_observer)));
 
-  content::RunAllTasksUntilIdle();
+  run_loop.Run();
   ASSERT_NE(profile, nullptr);
 
   ProfileAttributesEntry* entry =