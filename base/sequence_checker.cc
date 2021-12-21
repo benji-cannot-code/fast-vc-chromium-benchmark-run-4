497c8b74d7c9b0f502332b07c1ae8226b2d06fda
Lei Zhang <thestig@chromium.org>
2021-12-21 17:56:05 +0000

    Move base::ScopedValidateSequenceChecker impl into a .cc file.
    Reduce the number of includes in base/sequence_checker.h.
    
    Change-Id: If778dd6152e82553ac674188fe8f7652f71db42b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347930
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953254}

---
+++ base/sequence_checker.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/sequence_checker.h"
+
+#if DCHECK_IS_ON()
+#include <memory>
+#include <ostream>
+
+#include "base/check.h"
+#include "base/debug/stack_trace.h"
+#endif
+
+namespace base {
+
+#if DCHECK_IS_ON()
+ScopedValidateSequenceChecker::ScopedValidateSequenceChecker(
+    const SequenceChecker& checker) {
+  std::unique_ptr<debug::StackTrace> bound_at;
+  DCHECK(checker.CalledOnValidSequence(&bound_at))
+      << (bound_at ? "\nWas attached to sequence at:\n" + bound_at->ToString()
+                   : "");
+}
+
+ScopedValidateSequenceChecker::~ScopedValidateSequenceChecker() = default;
+#endif  // DCHECK_IS_ON()
+
+}  // namespace base
