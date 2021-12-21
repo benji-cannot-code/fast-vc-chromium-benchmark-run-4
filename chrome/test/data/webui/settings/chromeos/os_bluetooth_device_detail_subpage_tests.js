ac341abad28365a44e7a92f0b6794e1ba4794081
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2021-12-21 23:15:14 +0000

    [CrOS Bluetooth] Fix flaky device detail subpage test
    Bug: b:207588508, 1280869
    Change-Id: Ic97182c9ffc3c46c42bc4b6579561a1910346a1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352606
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#953350}

--- chrome/test/data/webui/settings/chromeos/os_bluetooth_device_detail_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_bluetooth_device_detail_subpage_tests.js
@@ -75,6 +75,8 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
       async function() {
         init();
         bluetoothConfig.setBluetoothEnabledState(/*enabled=*/ true);
+        const windowPopstatePromise =
+            test_util.eventToPromise('popstate', window);
 
         const getBluetoothConnectDisconnectBtn = () =>
             bluetoothDeviceDetailPage.$$('#connectDisconnectBtn');
@@ -114,7 +116,7 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
         assertTrue(!!getConnectionFailedText());
 
         settings.Router.getInstance().navigateToPreviousRoute();
-        await test_util.waitAfterNextRender(bluetoothDeviceDetailPage);
+        await windowPopstatePromise;
         assertFalse(!!getConnectionFailedText());
       });
 
@@ -284,8 +286,8 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
 
     assertTrue(!!getBluetoothStatusIcon());
     assertTrue(!!getBluetoothStateText());
-    assertTrue(!!getBluetoothForgetBtn());
     assertTrue(!!getBluetoothDeviceNameLabel());
+    assertFalse(!!getBluetoothForgetBtn());
     assertFalse(!!getBluetoothStateBtn());
     assertFalse(!!getBluetoothDeviceBatteryInfo());
 
@@ -311,6 +313,8 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
     settings.Router.getInstance().navigateTo(
         settings.routes.BLUETOOTH_DEVICE_DETAIL, params);
     await flushAsync();
+
+    assertTrue(!!getBluetoothForgetBtn());
 
     // Simulate connected state and audio capable.
     assertTrue(!!getBluetoothStateBtn());
