7f079920cc7636f1b59ff098b0889124cb1db953
chinsenj <chinsenj@chromium.org>
2021-12-21 20:44:51 +0000

    desk_templates: Disable the save button if there aren't apps with app id
    Currently if a user has the KSV open, the save desk as template button
    is not disabled and when they save the template it is empty. This is
    because the KSV is a supported app and not a transient child, but it has
    no Full Restore app id. If the active desk has no supported apps or no
    apps with a Full Restore app id, the save template button should be
    disabled.
    
    This CL modifies the way `Desk::num_supported_windows_` is incremented
    and decremented such that windows without an app id aren't considered.
    
    Test: modified
    Bug: 1275621
    Change-Id: Ib13d18a53ca5a2a86e6d3adeb8f42169610f3e43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3335716
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953309}

--- ash/wm/desks/desk.cc
+++ ash/wm/desks/desk.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/stringprintf.h"
 #include "chromeos/ui/base/window_properties.h"
+#include "components/app_restore/full_restore_utils.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window_tracker.h"
 #include "ui/compositor/layer.h"
@@ -95,6 +96,17 @@ bool CanMoveWindowOutOfDeskContainer(aura::Window* window) {
   // Only allow app windows to move to other desks.
   return window->GetProperty(aura::client::kAppType) !=
          static_cast<int>(AppType::NON_APP);
+}
+
+// Returns true if `window` is supported by Desks Templates and has an app id.
+bool IsSupportedByDesksTemplatesAndHasAppId(aura::Window* window) {
+  auto* delegate = Shell::Get()->desks_templates_delegate();
+  return delegate && delegate->IsWindowSupportedForDeskTemplate(window) &&
+         !wm::GetTransientParent(window) &&
+         (Shell::Get()
+              ->desks_controller()
+              ->disable_app_id_check_for_desk_templates() ||
+          !full_restore::GetAppId(window).empty());
 }
 
 // Adjusts the z-order stacking of |window_to_fix| in its parent to match its
@@ -286,12 +298,10 @@ void Desk::OnRootWindowClosing(aura::Window* root) {
 void Desk::AddWindowToDesk(aura::Window* window) {
   DCHECK(!base::Contains(windows_, window));
 
-  // Increment `num_supported_windows_` if the window is supported.
-  auto* delegate = Shell::Get()->desks_templates_delegate();
-  if (delegate && delegate->IsWindowSupportedForDeskTemplate(window) &&
-      !wm::GetTransientParent(window)) {
+  // Increment `num_supported_windows_` if the window is supported and has a
+  // Full Restore app id.
+  if (IsSupportedByDesksTemplatesAndHasAppId(window))
     num_supported_windows_++;
-  }
 
   windows_.push_back(window);
   // No need to refresh the mini_views if the destroyed window doesn't show up
@@ -317,11 +327,8 @@ void Desk::RemoveWindowFromDesk(aura::Window* window) {
   DCHECK(base::Contains(windows_, window));
 
   // Decrement `num_supported_windows_` if the window was supported.
-  auto* delegate = Shell::Get()->desks_templates_delegate();
-  if (delegate && delegate->IsWindowSupportedForDeskTemplate(window) &&
-      !wm::GetTransientParent(window)) {
+  if (IsSupportedByDesksTemplatesAndHasAppId(window))
     num_supported_windows_--;
-  }
 
   base::Erase(windows_, window);
   // No need to refresh the mini_views if the destroyed window doesn't show up
