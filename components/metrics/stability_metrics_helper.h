ca32c989a55527a8ddcd7f99f87a968f72190639
Will Harris <wfh@chromium.org>
2021-12-22 18:48:03 +0000

    Add new metrics for browser child process launch failures.
    BUG=1280005
    
    Change-Id: I5abbdfc14a1532b4a0672f9f64e7596b6c34ede4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3351093
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953596}

--- components/metrics/stability_metrics_helper.h
+++ components/metrics/stability_metrics_helper.h
@@ -10,6 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/process/kill.h"
+#include "build/build_config.h"
+
+#if defined(OS_WIN)
+#include "base/win/windows_types.h"
+#endif
 
 class PrefRegistrySimple;
 class PrefService;
@@ -64,6 +69,17 @@ class StabilityMetricsHelper {
   // Records a utility process crash with name |metrics_name|.
   void BrowserUtilityProcessCrashed(const std::string& metrics_name,
                                     int exit_code);
+
+  // Records that a utility process with name |metrics_name| failed to launch.
+  // The |launch_error_code| is a platform-specific error code. On Windows, a
+  // |last_error| is also supplied to help diagnose the launch failure.
+  void BrowserUtilityProcessLaunchFailed(const std::string& metrics_name,
+                                         int launch_error_code
+#if defined(OS_WIN)
+                                         ,
+                                         DWORD last_error
+#endif
+  );
 
   // Records a browser child process crash.
   void BrowserChildProcessCrashed();
