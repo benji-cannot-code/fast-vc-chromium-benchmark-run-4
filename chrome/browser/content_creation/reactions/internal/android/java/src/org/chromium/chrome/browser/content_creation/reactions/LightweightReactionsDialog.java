fb6b6a4316eb52d7f3d7d9a11813203b1004787b
Guillaume Jenkins <gujen@google.com>
2021-12-22 22:09:03 +0000

    [LR] Better handling of orientation change
    Adds logic to better handle orientation changes during Lightweight
    Reactions scene editing. When the device is rotated, the reactions are
    now individually repositioned and resized so they end up in a similar
    state relative to the new screenshot image dimensions (that was scaled
    due the orientation change).
    
    As a drive-by, this CL also renames the mSceneBackground member to
    mSceneView, which better reflects the fact that the view in question
    is the root view of the scene, and not just a background element.
    
    Bug: 1272573
    Change-Id: I7e4bf1e1f18b2a6abd84769ecc83aad1cf2219cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3349467
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#953658}

--- chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsDialog.java
+++ chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsDialog.java
@@ -56,8 +56,7 @@ public class LightweightReactionsDialog extends DialogFragment {
 
         mContentView = getActivity().getLayoutInflater().inflate(R.layout.reactions_dialog, null);
         setBackgroundImage();
-        mSceneCoordinator.setSceneBackground(
-                mContentView.findViewById(R.id.lightweight_reactions_scene),
+        mSceneCoordinator.setSceneViews(mContentView.findViewById(R.id.lightweight_reactions_scene),
                 mContentView.findViewById(R.id.lightweight_reactions_background));
         builder.setView(mContentView);
 
@@ -75,6 +74,7 @@ public class LightweightReactionsDialog extends DialogFragment {
         if (mCurrentOrientation != newConfig.orientation) {
             mCurrentOrientation = newConfig.orientation;
             LightweightReactionsMetrics.recordOrientationChange(newConfig.orientation);
+            mSceneCoordinator.handleOrientationChange();
         }
     }
 
