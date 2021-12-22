b9b1cd4431707348a693f9e2273ac44b45d87589
Nicolás Peña Moreno <npm@chromium.org>
2021-12-22 14:54:27 +0000

    Remove Event.FractionOfTimeWithoutUserInput histogram
    The histogram is long expired. Remove the code computing it.
    
    Bug: 1205083
    Change-Id: Ie422179d4d7855690ae6191449e6b144ef019ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3345134
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953535}

--- ui/aura/window_event_dispatcher.h
+++ ui/aura/window_event_dispatcher.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/event_processor.h"
 #include "ui/events/event_targeter.h"
-#include "ui/events/fraction_of_time_without_user_input_recorder.h"
 #include "ui/events/gestures/gesture_recognizer.h"
 #include "ui/events/gestures/gesture_types.h"
 #include "ui/events/types/event_type.h"
@@ -299,9 +298,6 @@ class AURA_EXPORT WindowEventDispatcher : public ui::EventProcessor,
   raw_ptr<Window> touchpad_pinch_handler_ = nullptr;
   raw_ptr<Window> event_dispatch_target_ = nullptr;
   raw_ptr<Window> old_dispatch_target_ = nullptr;
-
-  ui::FractionOfTimeWithoutUserInputRecorder
-      fraction_of_time_without_user_input_recorder_;
 
   bool synthesize_mouse_move_ = false;
 
