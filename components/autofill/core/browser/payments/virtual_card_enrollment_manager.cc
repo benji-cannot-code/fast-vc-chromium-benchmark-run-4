910f2d1d2a0321896a8567f6dc5235ab0b21565a
Vinny Persky <vinnypersky@google.com>
2021-12-22 21:01:30 +0000

    Update Virtual Card Enrollment Manager Skeleton
    This CL is a minor update to Virtual Card Enrollment Manager Skeleton,
    and it adds more comments for readability as well as updates the param
    for OnDidGetUpdateVirtualCardEnrollmentResponse to only take in the RPC
    call result.
    
    Bug: 1281695
    Change-Id: I6875010fabbfe930e406b3c5b00440932f14a78d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352912
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#953629}

--- components/autofill/core/browser/payments/virtual_card_enrollment_manager.cc
+++ components/autofill/core/browser/payments/virtual_card_enrollment_manager.cc
@@ -47,7 +47,7 @@ void VirtualCardEnrollmentManager::OnVirtualCardEnrollmentBubbleAccepted(
     raw_ptr<CreditCard> credit_card) {}
 
 void VirtualCardEnrollmentManager::OnDidGetUpdateVirtualCardEnrollmentResponse(
-    CreditCard::VirtualCardEnrollmentState virtual_card_enrollment_state) {}
+    AutofillClient::PaymentsRpcResult result) {}
 
 void VirtualCardEnrollmentManager::OnVirtualCardEnrollmentBubbleCancelled() {}
 
