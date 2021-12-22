9e7a3ed4f376d33345e032b690bdb648a5aae5b1
Gauthier Ambard <gambard@chromium.org>
2021-12-22 16:50:48 +0000

    [iOS] Add a flag to try different SFSymbols
    This CL adds a flag allowing to change some images to SF Symbols.
    It is also using it on the toolbar images.
    
    Fixed: 1282183
    Change-Id: I2392f7a7890de2a13701a35887d3354ac66d3837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3353446
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953565}

--- ios/chrome/browser/ui/toolbar/buttons/toolbar_button_factory.mm
+++ ios/chrome/browser/ui/toolbar/buttons/toolbar_button_factory.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_tab_grid_button.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_tools_menu_button.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_constants.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/rtl_geometry.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -25,6 +26,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+
+// Returns the default configuration for Symbols.
+UIImageConfiguration* SymbolConfiguration() {
+  return [UIImageSymbolConfiguration
+      configurationWithPointSize:22
+                          weight:UIImageSymbolWeightMedium];
+}
+
+}  // namespace
 
 @implementation ToolbarButtonFactory
 
@@ -40,8 +52,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Buttons
 
 - (ToolbarButton*)backButton {
+  UIImage* backImage;
+  if (base::FeatureList::IsEnabled(kUseSFSymbolsSamples)) {
+    backImage = [UIImage systemImageNamed:@"arrow.left"
+                        withConfiguration:SymbolConfiguration()];
+  } else {
+    backImage = [UIImage imageNamed:@"toolbar_back"];
+  }
   ToolbarButton* backButton = [ToolbarButton
-      toolbarButtonWithImage:[[UIImage imageNamed:@"toolbar_back"]
+      toolbarButtonWithImage:[backImage
                                  imageFlippedForRightToLeftLayoutDirection]];
   [self configureButton:backButton width:kAdaptiveToolbarButtonWidth];
   backButton.accessibilityLabel = l10n_util::GetNSString(IDS_ACCNAME_BACK);
@@ -54,8 +73,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Returns a forward button without visibility mask configured.
 - (ToolbarButton*)forwardButton {
+  UIImage* forwardImage;
+  if (base::FeatureList::IsEnabled(kUseSFSymbolsSamples)) {
+    forwardImage = [UIImage systemImageNamed:@"arrow.right"
+                           withConfiguration:SymbolConfiguration()];
+  } else {
+    forwardImage = [UIImage imageNamed:@"toolbar_forward"];
+  }
   ToolbarButton* forwardButton = [ToolbarButton
-      toolbarButtonWithImage:[[UIImage imageNamed:@"toolbar_forward"]
+      toolbarButtonWithImage:[forwardImage
                                  imageFlippedForRightToLeftLayoutDirection]];
   [self configureButton:forwardButton width:kAdaptiveToolbarButtonWidth];
   forwardButton.visibilityMask =
@@ -69,8 +95,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (ToolbarTabGridButton*)tabGridButton {
-  ToolbarTabGridButton* tabGridButton = [ToolbarTabGridButton
-      toolbarButtonWithImage:[UIImage imageNamed:@"toolbar_switcher"]];
+  UIImage* tabGridImage;
+  if (base::FeatureList::IsEnabled(kUseSFSymbolsSamples)) {
+    tabGridImage = [UIImage systemImageNamed:@"square"
+                           withConfiguration:SymbolConfiguration()];
+  } else {
+    tabGridImage = [UIImage imageNamed:@"toolbar_switcher"];
+  }
+
+  ToolbarTabGridButton* tabGridButton =
+      [ToolbarTabGridButton toolbarButtonWithImage:tabGridImage];
   [self configureButton:tabGridButton width:kAdaptiveToolbarButtonWidth];
   SetA11yLabelAndUiAutomationName(tabGridButton, IDS_IOS_TOOLBAR_SHOW_TABS,
                                   kToolbarStackButtonIdentifier);
@@ -104,8 +138,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (ToolbarButton*)shareButton {
-  ToolbarButton* shareButton = [ToolbarButton
-      toolbarButtonWithImage:[UIImage imageNamed:@"toolbar_share"]];
+  UIImage* shareImage;
+  if (base::FeatureList::IsEnabled(kUseSFSymbolsSamples)) {
+    shareImage = [UIImage systemImageNamed:@"square.and.arrow.up"
+                         withConfiguration:SymbolConfiguration()];
+  } else {
+    shareImage = [UIImage imageNamed:@"toolbar_share"];
+  }
+
+  ToolbarButton* shareButton =
+      [ToolbarButton toolbarButtonWithImage:shareImage];
   [self configureButton:shareButton width:kAdaptiveToolbarButtonWidth];
   SetA11yLabelAndUiAutomationName(shareButton, IDS_IOS_TOOLS_MENU_SHARE,
                                   kToolbarShareButtonIdentifier);
@@ -119,8 +161,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (ToolbarButton*)reloadButton {
+  UIImage* reloadImage;
+  if (base::FeatureList::IsEnabled(kUseSFSymbolsSamples)) {
+    reloadImage = [UIImage systemImageNamed:@"arrow.clockwise"
+                          withConfiguration:SymbolConfiguration()];
+  } else {
+    reloadImage = [UIImage imageNamed:@"toolbar_reload"];
+  }
+
   ToolbarButton* reloadButton = [ToolbarButton
-      toolbarButtonWithImage:[[UIImage imageNamed:@"toolbar_reload"]
+      toolbarButtonWithImage:[reloadImage
                                  imageFlippedForRightToLeftLayoutDirection]];
   [self configureButton:reloadButton width:kAdaptiveToolbarButtonWidth];
   reloadButton.accessibilityLabel =
@@ -134,8 +184,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (ToolbarButton*)stopButton {
-  ToolbarButton* stopButton = [ToolbarButton
-      toolbarButtonWithImage:[UIImage imageNamed:@"toolbar_stop"]];
+  UIImage* stopImage;
+  if (base::FeatureList::IsEnabled(kUseSFSymbolsSamples)) {
+    stopImage = [UIImage systemImageNamed:@"multiply"
+                        withConfiguration:SymbolConfiguration()];
+  } else {
+    stopImage = [UIImage imageNamed:@"toolbar_stop"];
+  }
+
+  ToolbarButton* stopButton = [ToolbarButton toolbarButtonWithImage:stopImage];
   [self configureButton:stopButton width:kAdaptiveToolbarButtonWidth];
   stopButton.accessibilityLabel = l10n_util::GetNSString(IDS_IOS_ACCNAME_STOP);
   [stopButton addTarget:self.actionHandler
@@ -146,8 +203,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (ToolbarButton*)openNewTabButton {
-  ToolbarNewTabButton* newTabButton = [ToolbarNewTabButton
-      toolbarButtonWithImage:[UIImage imageNamed:@"toolbar_new_tab_page"]];
+  UIImage* newTabImage;
+  if (base::FeatureList::IsEnabled(kUseSFSymbolsSamples)) {
+    newTabImage = [UIImage systemImageNamed:@"plus"
+                          withConfiguration:SymbolConfiguration()];
+  } else {
+    newTabImage = [UIImage imageNamed:@"toolbar_new_tab_page"];
+  }
+  ToolbarNewTabButton* newTabButton =
+      [ToolbarNewTabButton toolbarButtonWithImage:newTabImage];
 
   [newTabButton addTarget:self.actionHandler
                    action:@selector(searchAction:)
--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -75,6 +75,9 @@ const base::Feature kEnableShortenedPasswordAutoFillInstruction{
 const base::Feature kAddSettingForDefaultPageMode{
     "DefaultRequestedMode", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kUseSFSymbolsSamples{"UseSFSymbolsSamples",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
+
 bool IsContextMenuActionsRefreshEnabled() {
   return base::FeatureList::IsEnabled(kContextMenuActionsRefresh);
 }
