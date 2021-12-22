313dd9c6542b033661a3d555b4ec2a4e2de9f1cc
Christian Flach <cmfcmf@chromium.org>
2021-12-22 11:27:11 +0000

    Test isolation of service workers when receiving push notifications
    Service workers in isolated web apps should always use a non-default
    StoragePartition. This test checks that the non-default partition is
    also used when a service worker is woken up due to a push notification.
    Additionally, it tests that opening additional windows/tabs from the
    service worker that are also part of the isolated web app opens them
    in the same isolated storage partition.
    
    Bug: 1277817
    Change-Id: Idc6ec29c914e993036bc509bb33731d8b14782cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3334582
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953489}

---
+++ chrome/test/data/banners/isolated/service_worker.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+self.addEventListener('fetch', e => {
+  e.respondWith((async () => {
+    try {
+      return await fetch(e.request);
+    } catch (error) {
+      return new Response('Hello offline page');
+    }
+  })());
+});
+
+self.addEventListener('push', event => {
+  self.registration.showNotification('Hello world!', {
+    body: 'Test Notification',
+  });
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(self.clients.openWindow(
+    self.location.origin + '/banners/isolated/service_worker.html'
+  ));
+});
