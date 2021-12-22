454d0c075c6b98f3a57c6daffdc0a1606ce8229d
Yue Zhang <yuezhanggg@chromium.org>
2021-12-22 21:24:21 +0000

    [Sheriff] Disable flaky LongScreenshotsTabServiceTest on emulators
    Bug: 1282258
    Change-Id: Ifc697314d8fd99610234f3d26ac9fc58f0b764c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3353283
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Owners-Override: Yue Zhang <yuezhanggg@chromium.org>
    Auto-Submit: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953638}

--- chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/long_screenshots/bitmap_generation/LongScreenshotsTabServiceTest.java
+++ chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/long_screenshots/bitmap_generation/LongScreenshotsTabServiceTest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Matchers;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -88,7 +89,10 @@ public class LongScreenshotsTabServiceTest {
     @Test
     @MediumTest
     @Feature({"LongScreenshots"})
-    public void testCapturedFilesystem() throws Exception {
+    @DisableIf.Build(message = "Flaky on emulators; see https://crbug.com/1282258",
+            supported_abis_includes = "x86")
+    public void
+    testCapturedFilesystem() throws Exception {
         EmbeddedTestServer testServer = mActivityTestRule.getTestServer();
         final String url = testServer.getURL("/chrome/test/data/android/about.html");
 
