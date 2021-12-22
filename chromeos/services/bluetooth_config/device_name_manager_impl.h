37f692075d44663d33c845f990b919e8e8eb0c57
Gordon Seto <gordonseto@google.com>
2021-12-22 18:37:03 +0000

    [CrOS Bluetooth] Add RemoveDeviceNickname to DeviceNameManager.
    Update DeviceNameManager to contain a RemoveDeviceNickname method which
    removes the nickname for a given device id. Observers methods are
    updated to send null if a removal occurs.
    This will be used when a device is forgotten in a follow-up CL.
    
    Bug: b/210018208
    Change-Id: If3c3ba610b5bba46877ff8c4f777f9d9543bdab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352909
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#953593}

--- chromeos/services/bluetooth_config/device_name_manager_impl.h
+++ chromeos/services/bluetooth_config/device_name_manager_impl.h
@@ -30,6 +30,7 @@ class DeviceNameManagerImpl : public DeviceNameManager {
       const std::string& device_id) override;
   void SetDeviceNickname(const std::string& device_id,
                          const std::string& nickname) override;
+  void RemoveDeviceNickname(const std::string& device_id) override;
   void SetPrefs(PrefService* local_state) override;
 
  private:
