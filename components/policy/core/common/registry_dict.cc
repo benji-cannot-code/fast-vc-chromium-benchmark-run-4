bdf323780b1e4f0dec176dc5a9fb62b6e71c5775
Austin Sullivan <asully@chromium.org>
2021-12-22 18:19:39 +0000

    [Code Health] Refactor RegistryDict off base::DictValue::CreateDeepCopy
    Also does some refactors of unique_ptr<Value> to Value.
    
    This change is a no-op except for the SetValue() method, which will
    no longer remove am entry if null is passed (it is no longer possible
    to pass a null value).
    
    Bug: 1187059
    Change-Id: Ib5e2a2053a72bfe801d595f466df8aef9c94b42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3330021
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953588}

--- components/policy/core/common/registry_dict.cc
+++ components/policy/core/common/registry_dict.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_byteorder.h"
 #include "base/values.h"
 #include "components/policy/core/common/schema.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 
 #if defined(OS_WIN)
 #include "base/win/registry.h"
@@ -189,27 +190,20 @@ void RegistryDict::ClearKeys() {
 
 base::Value* RegistryDict::GetValue(const std::string& name) {
   auto entry = values_.find(name);
-  return entry != values_.end() ? entry->second.get() : nullptr;
+  return entry != values_.end() ? &entry->second : nullptr;
 }
 
 const base::Value* RegistryDict::GetValue(const std::string& name) const {
   auto entry = values_.find(name);
-  return entry != values_.end() ? entry->second.get() : nullptr;
+  return entry != values_.end() ? &entry->second : nullptr;
 }
 
-void RegistryDict::SetValue(const std::string& name,
-                            std::unique_ptr<base::Value> dict) {
-  if (!dict) {
-    RemoveValue(name);
-    return;
-  }
-
+void RegistryDict::SetValue(const std::string& name, base::Value&& dict) {
   values_[name] = std::move(dict);
 }
 
-std::unique_ptr<base::Value> RegistryDict::RemoveValue(
-    const std::string& name) {
-  std::unique_ptr<base::Value> result;
+absl::optional<base::Value> RegistryDict::RemoveValue(const std::string& name) {
+  absl::optional<base::Value> result;
   auto entry = values_.find(name);
   if (entry != values_.end()) {
     result = std::move(entry->second);
@@ -232,8 +226,7 @@ void RegistryDict::Merge(const RegistryDict& other) {
 
   for (auto entry(other.values_.begin()); entry != other.values_.end();
        ++entry) {
-    SetValue(entry->first,
-             base::Value::ToUniquePtrValue(entry->second->Clone()));
+    SetValue(entry->first, entry->second.Clone());
   }
 }
 
@@ -253,8 +246,7 @@ void RegistryDict::ReadRegistry(HKEY hive, const std::wstring& root) {
     switch (it.Type()) {
       case REG_SZ:
       case REG_EXPAND_SZ:
-        SetValue(name,
-                 std::make_unique<base::Value>(base::WideToUTF8(it.Value())));
+        SetValue(name, base::Value(base::WideToUTF8(it.Value())));
         continue;
       case REG_DWORD_LITTLE_ENDIAN:
       case REG_DWORD_BIG_ENDIAN:
@@ -264,8 +256,7 @@ void RegistryDict::ReadRegistry(HKEY hive, const std::wstring& root) {
             dword_value = base::NetToHost32(dword_value);
           else
             dword_value = base::ByteSwapToLE32(dword_value);
-          SetValue(name, std::make_unique<base::Value>(
-                             static_cast<int>(dword_value)));
+          SetValue(name, base::Value(static_cast<int>(dword_value)));
           continue;
         }
         FALLTHROUGH;
@@ -311,7 +302,7 @@ std::unique_ptr<base::Value> RegistryDict::ConvertToJSON(
           matching_schemas.push_back(Schema());
         for (const Schema& subschema : matching_schemas) {
           absl::optional<base::Value> converted =
-              ConvertRegistryValue(*entry->second, subschema);
+              ConvertRegistryValue(entry->second, subschema);
           if (converted.has_value()) {
             result->SetKey(entry->first, std::move(converted.value()));
             break;
@@ -354,7 +345,7 @@ std::unique_ptr<base::Value> RegistryDict::ConvertToJSON(
         if (!IsKeyNumerical(entry->first))
           continue;
         absl::optional<base::Value> converted =
-            ConvertRegistryValue(*entry->second, item_schema);
+            ConvertRegistryValue(entry->second, item_schema);
         if (converted.has_value())
           result->Append(std::move(converted.value()));
       }
