7746a28e0fca87da23a283682f86bbb6f64ec44d
Daniel White <danieltwhite@google.com>
2021-12-22 20:32:16 +0000

    Implemented WebFrame::ExecuteJavaScript
    This CL implements the ExecuteJavaScript function in the WebFrame class.
    This allows users to execute arbitrary JavaScript strings via the
    WebFrame instead of the WebState which helps eliminate the codebase's
    dependency on |windowId|.
    
    In a future CL, an additional ExecuteJavaScript function will be
    implemented in the |WebFrame| class that takes a JavaScript string
    and a callback function.
    
    Bug: 1279946
    Change-Id: Ia12575530de213adf0b93fcb3efef531fae4b2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3348229
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#953617}

--- ios/web/js_messaging/web_frame_impl_unittest.mm
+++ ios/web/js_messaging/web_frame_impl_unittest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/base64.h"
 #include "base/bind.h"
+#include "base/ios/ios_util.h"
 #include "base/json/json_reader.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
@@ -445,6 +446,34 @@ TEST_F(WebFrameImplTest, CallJavaScriptFunctionIFrameFrameWithoutKey) {
   NSString* last_script =
       base::SysUTF16ToNSString(fake_web_state.GetLastExecutedJavascript());
   EXPECT_EQ(last_script.length, 0ul);
+}
+
+// Tests that the WebFrame can execute arbitrary JavaScript.
+// if and only if it is a main frame.
+TEST_F(WebFrameImplTest, ExecuteJavaScript) {
+  if (!base::ios::IsRunningOnIOS14OrLater()) {
+    return;
+  }
+
+  FakeWebState fake_web_state;
+  GURL security_origin;
+
+  NSString* script = @"__gCrWeb = {};"
+                     @"__gCrWeb['fakeFunction'] = function() {"
+                     @"  return '10';"
+                     @"}";
+
+  WebFrameImpl web_frame([[WKFrameInfo alloc] init], kFrameId,
+                         /*is_main_frame=*/true, security_origin,
+                         &fake_web_state);
+
+  EXPECT_TRUE(web_frame.ExecuteJavaScript(base::SysNSStringToUTF8(script)));
+
+  WebFrameImpl web_frame2([[WKFrameInfo alloc] init], kFrameId,
+                          /*is_main_frame=*/false, security_origin,
+                          &fake_web_state);
+  // Executing arbitrary code on an iframe should return false.
+  EXPECT_FALSE(web_frame2.ExecuteJavaScript(base::SysNSStringToUTF8(script)));
 }
 
 }  // namespace web
