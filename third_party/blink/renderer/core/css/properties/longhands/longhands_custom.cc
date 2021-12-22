16286c02ce0f467847795244820d1bb86e29cd27
Kent Tamura <tkent@chromium.org>
2021-12-22 04:16:04 +0000

    CSS: Fix computed style of text-emphasis-position
    If the specified value is only 'over' or 'under', getComputedStyle()
    should return it as is. It returned 'over right' or 'under right'.
    
    Bug: 666433
    Change-Id: If13fe2bed8904ff9653d6f47558482d71a7f7e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3351587
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953427}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -8230,11 +8230,10 @@ const CSSValue* TextEmphasisPosition::ParseSingleValue(
   }
   if (!over_under)
     return nullptr;
-  if (!left_right)
-    left_right = CSSIdentifierValue::Create(CSSValueID::kRight);
   CSSValueList* list = CSSValueList::CreateSpaceSeparated();
   list->Append(*over_under);
-  list->Append(*left_right);
+  if (left_right)
+    list->Append(*left_right);
   return list;
 }
 
@@ -8244,6 +8243,9 @@ const CSSValue* TextEmphasisPosition::CSSValueFromComputedStyleInternal(
     bool allow_visited_style) const {
   CSSValueList* list = CSSValueList::CreateSpaceSeparated();
   switch (style.GetTextEmphasisPosition()) {
+    case blink::TextEmphasisPosition::kOver:
+      list->Append(*CSSIdentifierValue::Create(CSSValueID::kOver));
+      break;
     case blink::TextEmphasisPosition::kOverRight:
       list->Append(*CSSIdentifierValue::Create(CSSValueID::kOver));
       list->Append(*CSSIdentifierValue::Create(CSSValueID::kRight));
@@ -8251,6 +8253,9 @@ const CSSValue* TextEmphasisPosition::CSSValueFromComputedStyleInternal(
     case blink::TextEmphasisPosition::kOverLeft:
       list->Append(*CSSIdentifierValue::Create(CSSValueID::kOver));
       list->Append(*CSSIdentifierValue::Create(CSSValueID::kLeft));
+      break;
+    case blink::TextEmphasisPosition::kUnder:
+      list->Append(*CSSIdentifierValue::Create(CSSValueID::kUnder));
       break;
     case blink::TextEmphasisPosition::kUnderRight:
       list->Append(*CSSIdentifierValue::Create(CSSValueID::kUnder));
