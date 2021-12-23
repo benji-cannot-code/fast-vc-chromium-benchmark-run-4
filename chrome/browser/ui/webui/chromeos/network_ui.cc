85a3421a33675d5812abf43cc14e8a6a31c7fb96
Azeem Arshad <azeemarshad@chromium.org>
2021-12-23 23:24:10 +0000

    [CrOS-Network] Fix crash in chrome://network page
    This fixes crash in chrome://network page. This was caused because
    a class named NetworkConfigMessageHandler was conflicting with another
    added in https://crrev.com/c/3229272 was conflicting. Fixed by moving
    this class to network_ui namespace.
    
    Bug: b/211897290
    Change-Id: Ic1f865df6b4cd520ef54c9829cf64ccb8d237eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3353867
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953964}

--- chrome/browser/ui/webui/chromeos/network_ui.cc
+++ chrome/browser/ui/webui/chromeos/network_ui.cc
@@ -128,6 +128,8 @@ class NetworkDiagnosticsMessageHandler : public content::WebUIMessageHandler {
 
 }  // namespace
 
+namespace network_ui {
+
 class NetworkConfigMessageHandler : public content::WebUIMessageHandler {
  public:
   NetworkConfigMessageHandler() {}
@@ -384,6 +386,8 @@ class NetworkConfigMessageHandler : public content::WebUIMessageHandler {
   base::WeakPtrFactory<NetworkConfigMessageHandler> weak_ptr_factory_{this};
 };
 
+}  // namespace network_ui
+
 // static
 void NetworkUI::GetLocalizedStrings(base::DictionaryValue* localized_strings) {
   localized_strings->SetString("titleText",
@@ -542,7 +546,8 @@ void NetworkUI::GetLocalizedStrings(base::DictionaryValue* localized_strings) {
 
 NetworkUI::NetworkUI(content::WebUI* web_ui)
     : ui::MojoWebUIController(web_ui, /*enable_chrome_send=*/true) {
-  web_ui->AddMessageHandler(std::make_unique<NetworkConfigMessageHandler>());
+  web_ui->AddMessageHandler(
+      std::make_unique<network_ui::NetworkConfigMessageHandler>());
   web_ui->AddMessageHandler(std::make_unique<OncImportMessageHandler>());
   web_ui->AddMessageHandler(std::make_unique<NetworkLogsMessageHandler>());
   web_ui->AddMessageHandler(
