d3fca6c83805500e0824c18c28b8e8c81812f504
Sharon Yang <yangsharon@chromium.org>
2021-12-23 21:53:18 +0000

    [bookmarks] Remove WebContents::GetURL calls in //chrome bookmarks
    WebContents::GetURL is deprecated. Replace it with
    WebContents::GetVisibleURL
    
    Bug: 237908
    Change-Id: I339f5f76f24122dbfae2fcc0324cfa92486c594e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3354073
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953942}

--- chrome/browser/ui/bookmarks/bookmark_utils.cc
+++ chrome/browser/ui/bookmarks/bookmark_utils.cc
@@ -88,11 +88,12 @@ GURL GetURLToBookmark(content::WebContents* web_contents) {
     return GURL(kChromeUINewTabURL);
   // Users cannot bookmark Reader Mode pages directly, so the bookmark
   // interaction is as if it were with the original page.
-  if (dom_distiller::url_utils::IsDistilledPage(web_contents->GetURL())) {
+  if (dom_distiller::url_utils::IsDistilledPage(
+          web_contents->GetVisibleURL())) {
     return dom_distiller::url_utils::GetOriginalUrlFromDistillerUrl(
-        web_contents->GetURL());
+        web_contents->GetVisibleURL());
   }
-  return web_contents->GetURL();
+  return web_contents->GetVisibleURL();
 }
 
 bool GetURLAndTitleToBookmark(content::WebContents* web_contents,
@@ -102,12 +103,13 @@ bool GetURLAndTitleToBookmark(content::WebContents* web_contents,
   if (!u.is_valid())
     return false;
   *url = u;
-  if (dom_distiller::url_utils::IsDistilledPage(web_contents->GetURL())) {
+  if (dom_distiller::url_utils::IsDistilledPage(
+          web_contents->GetVisibleURL())) {
     // Users cannot bookmark Reader Mode pages directly. Instead, a bookmark
     // is added for the original page and original title.
     *title =
         base::UTF8ToUTF16(dom_distiller::url_utils::GetTitleFromDistillerUrl(
-            web_contents->GetURL()));
+            web_contents->GetVisibleURL()));
   } else {
     *title = web_contents->GetTitle();
   }
