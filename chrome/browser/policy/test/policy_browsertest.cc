0c353d8e572a77cfbba23990ee5573e3d31581df
Sharon Yang <yangsharon@chromium.org>
2021-12-23 19:10:28 +0000

    [policy] Remove WebContents::GetURL calls in //chrome policy
    WebContents::GetURL is deprecated. Replace it with
    WebContents::GetLastCommittedURL.
    
    Bug: 237908
    Change-Id: I3627879dfb1db75417a72cac7661c3022e564851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352907
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953904}

--- chrome/browser/policy/test/policy_browsertest.cc
+++ chrome/browser/policy/test/policy_browsertest.cc
@@ -142,9 +142,9 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, MAYBE_HomepageLocation) {
             browser()->profile()->GetHomePage());
   content::WebContents* contents =
       browser()->tab_strip_model()->GetActiveWebContents();
-  EXPECT_EQ(GURL(url::kAboutBlankURL), contents->GetURL());
+  EXPECT_EQ(GURL(url::kAboutBlankURL), contents->GetLastCommittedURL());
   EXPECT_TRUE(chrome::ExecuteCommand(browser(), IDC_HOME));
-  EXPECT_EQ(GURL(chrome::kChromeUIPolicyURL), contents->GetURL());
+  EXPECT_EQ(GURL(chrome::kChromeUIPolicyURL), contents->GetVisibleURL());
 
   // Now override with policy.
   PolicyMap policies;
@@ -154,7 +154,7 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, MAYBE_HomepageLocation) {
   UpdateProviderPolicy(policies);
   EXPECT_TRUE(chrome::ExecuteCommand(browser(), IDC_HOME));
   EXPECT_TRUE(content::WaitForLoadStop(contents));
-  EXPECT_EQ(GURL(chrome::kChromeUICreditsURL), contents->GetURL());
+  EXPECT_EQ(GURL(chrome::kChromeUICreditsURL), contents->GetLastCommittedURL());
 
   policies.Set(key::kHomepageIsNewTabPage, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD, base::Value(true),
@@ -163,7 +163,7 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, MAYBE_HomepageLocation) {
   EXPECT_TRUE(chrome::ExecuteCommand(browser(), IDC_HOME));
   EXPECT_TRUE(content::WaitForLoadStop(contents));
   EXPECT_EQ(ntp_test_utils::GetFinalNtpUrl(browser()->profile()),
-            contents->GetURL());
+            contents->GetLastCommittedURL());
 }
 
 #if defined(OS_MAC) && defined(ADDRESS_SANITIZER)
