2811d84d66028556b1d4f142bb1fbaf1bc1a399b
Courtney Wong <courtneywong@chromium.org>
2021-12-23 01:34:29 +0000

    Disable new screencast button when there is no input device available
    b: 197164243
    Change-Id: I67b1edf57d86504e32066a3496404f0c0265abea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352399
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953719}

--- ash/projector/projector_controller_impl.h
+++ ash/projector/projector_controller_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ash_export.h"
+#include "ash/components/audio/cras_audio_handler.h"
 #include "ash/projector/model/projector_session_impl.h"
 #include "ash/public/cpp/projector/projector_controller.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -25,8 +26,10 @@ class ProjectorUiController;
 class ProjectorMetadataController;
 
 // A controller to handle projector functionalities.
-class ASH_EXPORT ProjectorControllerImpl : public ProjectorController,
-                                           public ProjectorSessionObserver {
+class ASH_EXPORT ProjectorControllerImpl
+    : public ProjectorController,
+      public ProjectorSessionObserver,
+      public CrasAudioHandler::AudioObserver {
  public:
   // Callback that should be executed when the screencast container directory is
   // created. `screencast_file_path_no_extension` is the path of screencast file
@@ -105,9 +108,14 @@ class ASH_EXPORT ProjectorControllerImpl : public ProjectorController,
   ProjectorUiController* ui_controller() { return ui_controller_.get(); }
   ProjectorSessionImpl* projector_session() { return projector_session_.get(); }
 
+  // CrasAudioHandler::AudioObserver:
+  void OnAudioNodesChanged() override;
+
  private:
   // ProjectorSessionObserver:
   void OnProjectorSessionActiveStateChanged(bool active) override;
+
+  bool IsInputDeviceAvailable() const;
 
   // Starts or stops the speech recognition session.
   void StartSpeechRecognition();
--- ash/projector/projector_controller_unittest.cc
+++ ash/projector/projector_controller_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/components/audio/cras_audio_handler.h"
 #include "ash/constants/ash_features.h"
 #include "ash/projector/model/projector_session_impl.h"
 #include "ash/projector/test/mock_projector_client.h"
@@ -25,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/values.h"
+#include "chromeos/dbus/audio/audio_node.h"
+#include "chromeos/dbus/audio/fake_cras_audio_client.h"
 #include "media/mojo/mojom/speech_recognition_service.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -33,6 +36,14 @@ namespace ash {
 namespace {
 using testing::_;
 using testing::ElementsAre;
+
+struct AudioNodeInfo {
+  bool is_input;
+  uint64_t id;
+  const char* const device_name;
+  const char* const type;
+  const char* const name;
+};
 
 void NotifyControllerForFinalSpeechResult(ProjectorControllerImpl* controller) {
   media::SpeechRecognitionResult result;
@@ -120,6 +131,34 @@ TEST_F(ProjectorControllerTest, OnTranscriptionPartialResult) {
   // called since it is not a final result.
   EXPECT_CALL(*mock_metadata_controller_, RecordTranscription(_)).Times(0);
   NotifyControllerForPartialSpeechResult(controller_);
+}
+
+TEST_F(ProjectorControllerTest, OnAudioNodesChanged) {
+  ON_CALL(mock_client_, IsDriveFsMounted())
+      .WillByDefault(testing::Return(true));
+
+  const AudioNodeInfo kInternalMic[] = {
+      {true, 55555, "Fake Mic", "INTERNAL_MIC", "Internal Mic"}};
+  const chromeos::AudioNode audio_node = chromeos::AudioNode(
+      kInternalMic->is_input, kInternalMic->id,
+      /*has_v2_stable_device_id=*/false, kInternalMic->id,
+      /*stable_device_id_v2=*/0, kInternalMic->device_name, kInternalMic->type,
+      kInternalMic->name, /*active=*/false,
+      /*plugged_time=*/0, /*max_supported_channels=*/1, /*audio_effect=*/1);
+  chromeos::FakeCrasAudioClient::Get()->SetAudioNodesForTesting({audio_node});
+
+  CrasAudioHandler::Get()->SetActiveInputNodes({kInternalMic->id});
+  EXPECT_CALL(mock_client_,
+              OnNewScreencastPreconditionChanged(NewScreencastPrecondition(
+                  NewScreencastPreconditionState::kEnabled, {})));
+  controller_->OnAudioNodesChanged();
+
+  CrasAudioHandler::Get()->SetActiveInputNodes({});
+  EXPECT_CALL(mock_client_,
+              OnNewScreencastPreconditionChanged(NewScreencastPrecondition(
+                  NewScreencastPreconditionState::kDisabled,
+                  {NewScreencastPreconditionReason::kNoMic})));
+  controller_->OnAudioNodesChanged();
 }
 
 TEST_F(ProjectorControllerTest, OnSpeechRecognitionAvailabilityChanged) {
