9063a85c86dbebac8dbf65085f402e6c9a3371f0
Amanda Deacon <amandadeacon@chromium.org>
2021-12-23 02:23:12 +0000

    [Launcher pop-in] Separate new result arrival from ranking/publishing.
    This refactor separates into two functions:
    
    - the arrival of new results from a search provider
    
    and
    
    - the subsequent re-ranking of results/categories, and publication to
      the model updater.
    
    This will be useful for implementing a burn-in period for result
    updates.
    
    Test: none
    Bug: 1279686
    Change-Id: I3a45303df4430c67646c1682087245a7add7df98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347017
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953728}

--- chrome/browser/ui/app_list/search/search_controller_impl_new.h
+++ chrome/browser/ui/app_list/search/search_controller_impl_new.h
@@ -89,6 +89,8 @@ class SearchControllerImplNew : public SearchController {
   }
 
  private:
+  void RankAndPublish(const ash::AppListSearchResultType provider_type);
+
   Profile* profile_;
 
   // The query associated with the most recent search.
