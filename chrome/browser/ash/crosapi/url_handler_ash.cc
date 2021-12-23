196683652c7f70293efbe4d8882ca5af90326ad9
Stefan Kuhne <skuhne@google.com>
2021-12-23 02:03:40 +0000

    Add direct file-manager and scanning url redirect to app
    The file manager and scanning system app required a direct
    system app invocation, rather than letting the generic
    system app take care of it.
    Also removed the os scheme for chrome-untrusted apps.
    
    BUG: 1280019
    Change-Id: Ied4d2df5fb87d4e99ff34e4f0accd1d618267316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352914
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953726}

--- chrome/browser/ash/crosapi/url_handler_ash.cc
+++ chrome/browser/ash/crosapi/url_handler_ash.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/web_applications/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
+#include "chrome/browser/web_applications/system_web_apps/system_web_app_types.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chromeos/crosapi/cpp/gurl_os_handler_utils.h"
@@ -19,6 +20,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_constants.h"
 
 namespace {
+
+// Various schemes which we use and which are not defined anywhere.
+const char kChromeScheme[] = "chrome";
+const char kChromeUrlPrefix[] = "chrome://";
+const char kChromeUntrustedScheme[] = "chrome-untrusted";
+
+const char kFileManagerHost[] = "file-manager";
+
+// Checks if a given URL is a valid file manager URL (trusted or untrusted).
+bool IsFileManagerUrl(const GURL& url) {
+  return url.has_host() && url.host() == kFileManagerHost && url.has_scheme() &&
+         (url.scheme() == kChromeScheme ||
+          url.scheme() == kChromeUntrustedScheme);
+}
 
 // Show a chrome:// (os://) app for a given URL.
 void ShowOsAppForProfile(Profile* profile,
@@ -90,21 +105,18 @@ bool UrlHandlerAsh::OpenUrlInternal(const GURL& url) {
       target_url == GURL(chrome::kOsUIFlagsURL)) {
     app_id = web_app::SystemAppType::OS_FLAGS;
     target_url = GURL(chrome::kChromeUIFlagsURL);
-  } else if (target_url == GURL(chrome::kChromeUIUntrustedCroshURL) ||
-             target_url == GURL(chrome::kOsUICroshURL) ||
-             target_url == GURL(chrome::kChromeUIOsCroshAppURL)) {
+  } else if (target_url == GURL(chrome::kChromeUIUntrustedCroshURL)) {
     app_id = web_app::SystemAppType::CROSH;
-    target_url = GURL(chrome::kChromeUIUntrustedCroshURL);
+  } else if (IsFileManagerUrl(target_url)) {
+    app_id = web_app::SystemAppType::FILE_MANAGER;
+  } else if (target_url == GURL(chrome::kChromeUIScanningAppURL)) {
+    app_id = web_app::SystemAppType::SCANNING;
   } else if (ChromeWebUIControllerFactory::GetInstance()->CanHandleUrl(
                  target_url)) {
     app_id = web_app::SystemAppType::OS_URL_HANDLER;
-    // Convert a os://<url> into a chrome://<url> or chrome-untrusted://<url>.
-    if (target_url == GURL(chrome::kOsUITerminalURL) ||
-        target_url == GURL(chrome::kOsUIFileManagerURL)) {
-      target_url = GURL("chrome-untrusted://" + target_url.host());
-    } else if (crosapi::gurl_os_handler_utils::IsAshOsUrl(target_url)) {
+    if (crosapi::gurl_os_handler_utils::IsAshOsUrl(target_url)) {
       target_url =
-          GURL("chrome://" +
+          GURL(kChromeUrlPrefix +
                crosapi::gurl_os_handler_utils::AshOsUrlHost(target_url));
     }
   } else {
