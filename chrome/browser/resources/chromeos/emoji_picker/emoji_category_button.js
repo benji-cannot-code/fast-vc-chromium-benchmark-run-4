b76cbd8c8f718948dff5db3298c8dbf918bcbed4
Chuong Ho <hdchuong@google.com>
2021-12-23 02:28:32 +0000

    [emoji picker] Add three buttons for categories under emoji extension feature flag
    add SVGs, shrink search, add buttons
    add initial files for category button
    include feature flag
    
    Change-Id: Ib410278664fd4e2f05d7675eed257804b4b35f00
    Bug: b:210912380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3341138
    Reviewed-by: My Nguyen <myy@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#953729}

---
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_category_button.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './icons.js';
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+export class EmojiCategoryButton extends PolymerElement {
+  static get is() {
+    return 'emoji-category-button';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
+  static get properties() {
+    return {
+      /** @type {!string} */
+      icon: {type: String, readonly: true}
+    };
+  }
+
+  constructor() {
+    super();
+  }
+}
+
+customElements.define(EmojiCategoryButton.is, EmojiCategoryButton);
--- chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
+++ chrome/browser/resources/chromeos/emoji_picker/emoji_picker.js
@@ -176,6 +176,8 @@ export class EmojiPicker extends PolymerElement {
     /** @private {boolean} */
     this.groupTabsMoving = false;
 
+    /** @private {boolean} */
+    this.v2Enabled = false;
 
     this.addEventListener(
         GROUP_BUTTON_CLICK, ev => this.selectGroup(ev.detail.group));
@@ -243,7 +245,7 @@ export class EmojiPicker extends PolymerElement {
    * @param {!Array<!Feature>} featureList
    */
   setActiveFeatures(featureList) {
-    // TODO(b/207451560): Handle active features.
+    this.v2Enabled = featureList.includes(Feature.EMOJI_PICKER_EXTENSION);
   }
 
   fetchEmojiData() {
