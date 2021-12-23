f13cb77cebcef6ea2b64558a7ceed8c093eaf794
Austin Sullivan <asully@chromium.org>
2021-12-23 16:38:44 +0000

    [Code Health] base::Value cleanup after PrefService refactor
    https://crrev.com/c/3343526 recently landed as a LSC, which
    refactored the PrefService GetList() and GetDictionary() methods.
    The blast radius of this interface was quite large, so bare minimum
    changes were made at call sites.
    
    This CL is part of an effort to clean up these call sites.
    
    The unsplit version of this CL can be found at
    https://crrev.com/c/3351044
    
    base::Value refactor guidelines:
    http://doc/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ
    
    This CL was uploaded by git cl split.
    
    R=poromov@chromium.org
    
    Bug: 1187001, 1187061, 1187062
    Change-Id: Icbff2b5bceb5fb53fdb0d815dbd73636fe7e474f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3348165
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953861}

--- chrome/browser/ash/app_mode/kiosk_app_data_base.h
+++ chrome/browser/ash/app_mode/kiosk_app_data_base.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 
 namespace base {
-class DictionaryValue;
+class Value;
 }
 
 namespace ash {
@@ -56,8 +56,7 @@ class KioskAppDataBase : public KioskAppIconLoader::Delegate {
   // Helper to load name and icon from provided dictionary.
   // if |lazy_icon_load| is set to true, the icon will not be updated, only
   // icon_path_.
-  bool LoadFromDictionary(const base::DictionaryValue& dict,
-                          bool lazy_icon_load = false);
+  bool LoadFromDictionary(const base::Value& dict, bool lazy_icon_load = false);
 
   // Starts loading the icon from |icon_path_|;
   void DecodeIcon();
