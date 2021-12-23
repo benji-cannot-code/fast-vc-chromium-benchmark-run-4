9e874da34e8d1a306b38de7e8d60c56b3b46c532
Jimmy Gong <jimmyxgong@google.com>
2021-12-23 18:56:50 +0000

    fwupd: Add verbose logs to fwupd dbus client
    Bug: 1252981
    Change-Id: I660be1dc8118a95069534dc07c2c084758b0d426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3346443
    Auto-Submit: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953901}

--- chromeos/dbus/fwupd/fwupd_client.cc
+++ chromeos/dbus/fwupd/fwupd_client.cc
@@ -55,6 +55,7 @@ class FwupdClientImpl : public FwupdClient {
 
   void RequestUpdates(const std::string& device_id) override {
     CHECK(features::IsFirmwareUpdaterAppEnabled());
+    VLOG(1) << "fwupd: RequestUpdates called for: " << device_id;
     dbus::MethodCall method_call(kFwupdServiceInterface,
                                  kFwupdGetUpgradesMethodName);
     dbus::MessageWriter writer(&method_call);
@@ -69,6 +70,7 @@ class FwupdClientImpl : public FwupdClient {
 
   void RequestDevices() override {
     CHECK(features::IsFirmwareUpdaterAppEnabled());
+    VLOG(1) << "fwupd: RequestDevices called";
     dbus::MethodCall method_call(kFwupdServiceInterface,
                                  kFwupdGetDevicesMethodName);
     proxy_->CallMethodWithErrorResponse(
@@ -80,6 +82,7 @@ class FwupdClientImpl : public FwupdClient {
   void InstallUpdate(const std::string& device_id,
                      base::ScopedFD file_descriptor,
                      FirmwareInstallOptions options) override {
+    VLOG(1) << "fwupd: InstallUpdate called for id: " << device_id;
     dbus::MethodCall method_call(kFwupdServiceInterface,
                                  kFwupdInstallMethodName);
     dbus::MessageWriter writer(&method_call);
@@ -184,6 +187,8 @@ class FwupdClientImpl : public FwupdClient {
       const bool success = version && description && priority;
       // TODO(michaelcheco): Confirm that this is the expected behavior.
       if (success) {
+        VLOG(1) << "fwupd: Found update version for device: " << device_id
+                << " with version: " << version->GetString();
         updates.emplace_back(version->GetString(), description->GetString(),
                              priority->GetInt());
       } else {
@@ -231,6 +236,8 @@ class FwupdClientImpl : public FwupdClient {
         return;
       }
 
+      VLOG(1) << "fwupd: Device found: " << id->GetString() << " "
+              << name->GetString();
       devices.emplace_back(id->GetString(), name->GetString());
     }
 
@@ -247,6 +254,7 @@ class FwupdClientImpl : public FwupdClient {
       success = false;
     }
 
+    VLOG(1) << "fwupd: InstallUpdate returned with: " << success;
     for (auto& observer : observers_)
       observer.OnInstallResponse(success);
   }
