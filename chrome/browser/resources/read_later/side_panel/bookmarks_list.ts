2edf281feb8e00e37eea1bee9ebafdd05d604d38
John Lee <johntlee@chromium.org>
2021-12-23 07:26:13 +0000

    Side panel Bookmarks: Only show hover CSS on pointermove
    The side panel's bookmarks uses dom-repeat to list out all bookmarks,
    and dom-repeat re-uses the existing DOM elements when the list gets
    reordered. Therefore, during drag and drop, the data gets shifted
    around while the HTML elements remain static. This causes issues on Mac
    as after a drag and drop, the browser still attempts to show hover CSS
    for the dropped bookmark's original HTML element.
    
    To fix this, this CL sets an attribute on BookmarksListElement that is
    toggled on pointermove/pointerleave to only show hover CSS specifically
    if the mouse is moving within the element. On drop, this attribute is
    removed to force the hover CSS to disappear until the user moves their
    pointer again.
    
    Bug: 1276485
    Change-Id: I6a356dba86b74b745da12ccc86838af1f85cab9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340233
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953771}

--- chrome/browser/resources/read_later/side_panel/bookmarks_list.ts
+++ chrome/browser/resources/read_later/side_panel/bookmarks_list.ts
@@ -36,6 +36,11 @@ export class BookmarksListElement extends PolymerElement {
         value: () => [],
       },
 
+      hoverVisible: {
+        reflectToAttribute: true,
+        value: false,
+      },
+
       openFolders_: {
         type: Array,
         value: () => [],
@@ -51,6 +56,7 @@ export class BookmarksListElement extends PolymerElement {
       ReadLaterApiProxyImpl.getInstance();
   private listeners_ = new Map<string, Function>();
   private folders_: chrome.bookmarks.BookmarkTreeNode[];
+  hoverVisible: boolean;
   private openFolders_: string[];
 
   ready() {
@@ -60,6 +66,8 @@ export class BookmarksListElement extends PolymerElement {
         e => this.onFolderOpenChanged_(
             e as CustomEvent<{id: string, open: boolean}>));
     this.addEventListener('keydown', e => this.onKeydown_(e));
+    this.addEventListener('pointermove', () => this.hoverVisible = true);
+    this.addEventListener('pointerleave', () => this.hoverVisible = false);
   }
 
   connectedCallback() {
@@ -138,6 +146,7 @@ export class BookmarksListElement extends PolymerElement {
     }
 
     this.focusBookmark_(bookmarks[0]!.id);
+    this.hoverVisible = false;
   }
 
   /** BookmarksDragDelegate */
