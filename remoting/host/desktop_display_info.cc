52fee2a269b37690e572e155db728d58bd6f4941
Lambros Lambrou <lambroslambrou@chromium.org>
2021-12-23 20:44:28 +0000

    [remoting] Add 'const' to some DesktopDisplayInfo methods.
    This fixes all non-mutating DesktopDisplayInfo methods to be "const".
    
    Change-Id: Idbe56d5e135686ff3d8ae032a2a08f30d7e37360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355154
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#953926}

--- remoting/host/desktop_display_info.cc
+++ remoting/host/desktop_display_info.cc
@@ -17,7 +17,7 @@ DesktopDisplayInfo& DesktopDisplayInfo::operator=(DesktopDisplayInfo&&) =
     default;
 DesktopDisplayInfo::~DesktopDisplayInfo() = default;
 
-bool DesktopDisplayInfo::operator==(const DesktopDisplayInfo& other) {
+bool DesktopDisplayInfo::operator==(const DesktopDisplayInfo& other) const {
   if (other.displays_.size() == displays_.size()) {
     for (size_t display = 0; display < displays_.size(); display++) {
       const DisplayGeometry& this_display = displays_[display];
@@ -38,7 +38,7 @@ bool DesktopDisplayInfo::operator==(const DesktopDisplayInfo& other) {
   return false;
 }
 
-bool DesktopDisplayInfo::operator!=(const DesktopDisplayInfo& other) {
+bool DesktopDisplayInfo::operator!=(const DesktopDisplayInfo& other) const {
   return !(*this == other);
 }
 
@@ -62,11 +62,12 @@ void DesktopDisplayInfo::Reset() {
   displays_.clear();
 }
 
-int DesktopDisplayInfo::NumDisplays() {
+int DesktopDisplayInfo::NumDisplays() const {
   return displays_.size();
 }
 
-const DisplayGeometry* DesktopDisplayInfo::GetDisplayInfo(unsigned int id) {
+const DisplayGeometry* DesktopDisplayInfo::GetDisplayInfo(
+    unsigned int id) const {
   if (id < 0 || id >= displays_.size())
     return nullptr;
   return &displays_[id];
@@ -94,7 +95,7 @@ const DisplayGeometry* DesktopDisplayInfo::GetDisplayInfo(unsigned int id) {
 // x = upper left of desktop
 // a,b,c = origin of display A,B,C
 webrtc::DesktopVector DesktopDisplayInfo::CalcDisplayOffset(
-    webrtc::ScreenId disp_id) {
+    webrtc::ScreenId disp_id) const {
   bool full_desktop = (disp_id == webrtc::kFullDesktopScreenId);
   unsigned int disp_index = disp_id;
 
