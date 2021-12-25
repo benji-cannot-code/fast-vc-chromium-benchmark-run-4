149645b04b253f5cb4445a3f494130f0702c7173
Zoru Lee <donzoru@gmail.com>
2021-12-25 03:53:32 +0000

    fix "base::i18n:IsRTL()" typo in menu_types.h
    Change-Id: I83e7c73d3ba8d03bda2b6969b65c78a34499f7e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3354440
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Colin Kincaid <ckincaid@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954081}

--- ui/views/controls/menu/menu_types.h
+++ ui/views/controls/menu/menu_types.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 
 // Where a popup menu should be anchored to for non-RTL languages. The opposite
-// position will be used if base::i18n:IsRTL() is true. The Bubble flags are
+// position will be used if base::i18n::IsRTL() is true. The Bubble flags are
 // used when the menu should get enclosed by a bubble.
 enum class MenuAnchorPosition {
   kTopLeft,
