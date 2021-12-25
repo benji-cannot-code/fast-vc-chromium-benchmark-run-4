93b81b24a237f27fa6948c62b4d2a4b31c584706
Gabriel Charette <gab@chromium.org>
2021-12-25 19:08:06 +0000

    [base] Fix a class of data races with ScopedFeatureList
    Many ScopedFeatureList data races are fixed by avoiding running
    tasks during Init/Reset. Do it automatically instead of relying on
    callers to flush all tasks.
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20TSan%20Tests?limit=200
    is red as far as logs go because of this issue (many tests to be
    re-enabled after this lands).
    
    The initial patch set here was attempting to merely invoke
    ThreadPoolInstance::FlushForTesting before modifying the FeatureList
    but that turned out to be insufficient as some tests using
    ExecutionMode::QUEUED would stall with that approach.
    Flushing all tasks was also overkill. The latest patch set introduces
    TaskEnvironment::ParallelExecutionFence which ensures no tasks are
    running during its lifetime (waits for already running tasks at
    construction).
    
    This enforces that ScopedFeatureList only be used from the test main
    thread but this was luckily already the case across the codebase.
    
    Made FeatureList const-correct as a drive-by to prove that other calls
    to it in ScopedFeatureList aren't modifications.
    
    Changes required for existing tests:
     - PageInfoBubbleViewBrowserTest.BlockedAndInvalidCert resulted
       in a never dismissed ::CryptUIDlgViewCertificate. With this change
       that would hang in ~InProcessBrowserTest when Reset()'ing its
       ScopedFeatureList. That's the only test affected by this so fixed
       the test instead of allowing all CONTINUE_ON_SHUTDOWN tasks to
       hang (the FeatureList singleton doesn't AssertSingletonAllowed
       and thus can also be used from CONTINUE_ON_SHUTDOWN tasks so
       covering them is required: https://crrev.com/c/3337219)
     - WaylandClientTestSuiteServer: since the tests run on the
       ClientThread, this is also what the
       TaskEnvironment/ScopedFeatureList use as their "main thread". Add
       support for that use case. Without this, the CHECK in
       TaskEnvironment::ParallelExecutionFence's constructor fires.
    
    Bug: 1275502, 846380
    Change-Id: I6b5980fa627fb0d807de480c703a46131221092d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320617
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954109}

--- base/test/scoped_feature_list.cc
+++ base/test/scoped_feature_list.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/test/mock_entropy_provider.h"
+#include "base/test/task_environment.h"
 
 namespace base {
 namespace test {
@@ -152,6 +153,18 @@ void ScopedFeatureList::Reset() {
 
   init_called_ = false;
 
+  // ThreadPool tasks racily probing FeatureList while it's initialized/reset
+  // are problematic and while callers should ideally set up ScopedFeatureList
+  // before TaskEnvironment, that's not always possible. Fencing execution here
+  // avoids an entire class of bugs by making sure no ThreadPool task queries
+  // FeatureList while it's being modified. This local action is preferred to
+  // requiring all such callers to manually flush all tasks before each
+  // ScopedFeatureList Init/Reset: crbug.com/1275502#c45
+  //
+  // All FeatureList modifications in this file should have this as well.
+  TaskEnvironment::ParallelExecutionFence fence(
+      "ScopedFeatureList must be Reset from the test main thread");
+
   FeatureList::ClearInstanceForTesting();
 
   if (field_trial_list_) {
@@ -175,6 +188,11 @@ void ScopedFeatureList::Init() {
 void ScopedFeatureList::InitWithFeatureList(
     std::unique_ptr<FeatureList> feature_list) {
   DCHECK(!original_feature_list_);
+
+  // Execution fence required while modifying FeatureList, as in Reset.
+  TaskEnvironment::ParallelExecutionFence fence(
+      "ScopedFeatureList must be Init from the test main thread");
+
   original_feature_list_ = FeatureList::ClearInstanceForTesting();
   FeatureList::SetInstance(std::move(feature_list));
   init_called_ = true;
@@ -229,7 +247,7 @@ void ScopedFeatureList::InitWithFeaturesImpl(
 
   std::string current_enabled_features;
   std::string current_disabled_features;
-  FeatureList* feature_list = FeatureList::GetInstance();
+  const FeatureList* feature_list = FeatureList::GetInstance();
   if (feature_list) {
     feature_list->GetFeatureOverrides(&current_enabled_features,
                                       &current_disabled_features);
