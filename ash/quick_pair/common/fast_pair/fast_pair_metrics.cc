3709c855627c0e6e3128a2a151b8b4663fdc848e
Juliet Levesque <julietlevesque@google.com>
2021-12-28 20:31:37 +0000

    [Fast Pair] Log the error code of Gatt connection failures.
    Adds a metric to log the error code behind failures to create a
    Gatt connection. Emits following a Gatt connection failure in the
    GattServiceClientImpl.
    
    Change-Id: I046f9f240868fdd650637854a181276ead0e9d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331118
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#954342}

--- ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
+++ ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
@@ -30,6 +30,8 @@ const char kTotalGattConnectionTimeMetric[] =
     "Bluetooth.ChromeOS.FastPair.TotalGattConnectionTime";
 const char kGattConnectionResult[] =
     "Bluetooth.ChromeOS.FastPair.GattConnection.Result";
+const char kGattConnectionErrorMetric[] =
+    "Bluetooth.ChromeOS.FastPair.GattConnection.ErrorReason";
 const char kFastPairPairFailureInitialMetric[] =
     "Bluetooth.ChromeOS.FastPair.PairFailure.InitialPairingProtocol";
 const char kFastPairPairFailureSubsequentMetric[] =
@@ -124,6 +126,13 @@ void RecordTotalGattConnectionTime(base::TimeDelta total_gatt_connection_time) {
 
 void RecordGattConnectionResult(bool success) {
   base::UmaHistogramBoolean(kGattConnectionResult, success);
+}
+
+void RecordGattConnectionErrorCode(
+    device::BluetoothDevice::ConnectErrorCode error_code) {
+  base::UmaHistogramEnumeration(
+      kGattConnectionErrorMetric, error_code,
+      device::BluetoothDevice::NUM_CONNECT_ERROR_CODES);
 }
 
 void RecordPairingResult(const Device& device, bool success) {
--- ash/quick_pair/common/fast_pair/fast_pair_metrics.h
+++ ash/quick_pair/common/fast_pair/fast_pair_metrics.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/common/pair_failure.h"
 #include "base/component_export.h"
 #include "base/time/time.h"
+#include "device/bluetooth/bluetooth_device.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace ash {
@@ -84,6 +85,10 @@ void RecordTotalGattConnectionTime(base::TimeDelta total_gatt_connection_time);
 
 COMPONENT_EXPORT(QUICK_PAIR_COMMON)
 void RecordGattConnectionResult(bool success);
+
+COMPONENT_EXPORT(QUICK_PAIR_COMMON)
+void RecordGattConnectionErrorCode(
+    device::BluetoothDevice::ConnectErrorCode error_code);
 
 COMPONENT_EXPORT(QUICK_PAIR_COMMON)
 void RecordPairingFailureReason(const Device& device, PairFailure failure);
