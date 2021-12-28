f06e8893e9cb1833725675b68580a8945971745d
cfredric <cfredric@chromium.org>
2021-12-28 16:34:54 +0000

    Remove IsInNontrivialFirstPartySet method.
    Now that FirstOwner is part of the FirstPartySets public API, clients
    can call that instead of IsInNontrivialFirstPartySet.
    
    Change-Id: Ia3eb2f243d4bd802cdf9cab9f3807a5d029caa5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355155
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954302}

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -586,7 +586,7 @@ void URLRequestHttpJob::AddCookieHeaderAndStart() {
         cookie_store->cookie_access_delegate();
 
     bool is_in_nontrivial_first_party_set =
-        delegate && delegate->IsInNontrivialFirstPartySet(request_site);
+        delegate && delegate->FindFirstPartySetOwner(request_site).has_value();
     CookieOptions options = CreateCookieOptions(
         same_site_context, request_->same_party_context(),
         request_->isolation_info(), is_in_nontrivial_first_party_set);
@@ -752,7 +752,7 @@ void URLRequestHttpJob::SaveCookiesAndNotifyHeadersComplete(int result) {
   net::SchemefulSite request_site(request_->url());
 
   bool is_in_nontrivial_first_party_set =
-      delegate && delegate->IsInNontrivialFirstPartySet(request_site);
+      delegate && delegate->FindFirstPartySetOwner(request_site).has_value();
   CookieOptions options = CreateCookieOptions(
       same_site_context, request_->same_party_context(),
       request_->isolation_info(), is_in_nontrivial_first_party_set);
