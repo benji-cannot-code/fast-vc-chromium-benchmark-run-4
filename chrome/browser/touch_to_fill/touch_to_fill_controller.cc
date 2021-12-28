92f22c68c111211111b8769c9419b597317d11a1
Maxim Kolosovskiy <kolos@chromium.org>
2021-12-28 15:36:35 +0000

    [Passwords] Implement the metric for the outcome of TouchToFill
    There is a chance that various additions in TTF (e.g., automatic form
    submission, proactive triggering) can decrease acceptance of TTF. This
    CL implements a metric that tracks how often TTF opening leads to
    credentials filling.
    
    Bug: 1283004
    Change-Id: I53d76e59d8766cc99bd9e687a219690d6db39e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3359415
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954292}

--- chrome/browser/touch_to_fill/touch_to_fill_controller.cc
+++ chrome/browser/touch_to_fill/touch_to_fill_controller.cc
@@ -136,6 +136,8 @@ void TouchToFillController::OnManagePasswordsSelected() {
   password_client_->NavigateToManagePasswordsPage(
       password_manager::ManagePasswordsReferrer::kTouchToFill);
 
+  base::UmaHistogramEnumeration("PasswordManager.TouchToFill.Outcome",
+                                TouchToFillOutcome::kManagePasswordsSelected);
   ukm::builders::TouchToFill_Shown(source_id_)
       .SetUserAction(static_cast<int64_t>(UserAction::kSelectedManagePasswords))
       .Record(ukm::UkmRecorder::Get());
@@ -148,6 +150,8 @@ void TouchToFillController::OnDismiss() {
 
   std::exchange(driver_, nullptr)->TouchToFillClosed(ShowVirtualKeyboard(true));
 
+  base::UmaHistogramEnumeration("PasswordManager.TouchToFill.Outcome",
+                                TouchToFillOutcome::kSheetDismissed);
   ukm::builders::TouchToFill_Shown(source_id_)
       .SetUserAction(static_cast<int64_t>(UserAction::kDismissed))
       .Record(ukm::UkmRecorder::Get());
@@ -165,6 +169,8 @@ void TouchToFillController::OnReauthCompleted(UiCredential credential,
   if (!auth_successful) {
     std::exchange(driver_, nullptr)
         ->TouchToFillClosed(ShowVirtualKeyboard(true));
+    base::UmaHistogramEnumeration("PasswordManager.TouchToFill.Outcome",
+                                  TouchToFillOutcome::kReauthenticationFailed);
     return;
   }
 
@@ -180,4 +186,7 @@ void TouchToFillController::FillCredential(const UiCredential& credential) {
 
   std::exchange(driver_, nullptr)
       ->FillSuggestion(credential.username(), credential.password());
+
+  base::UmaHistogramEnumeration("PasswordManager.TouchToFill.Outcome",
+                                TouchToFillOutcome::kCredentialFilled);
 }
