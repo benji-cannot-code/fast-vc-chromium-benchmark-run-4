c8fbab7d47427fb308b284a0fd3dce73c2bb6f3d
Regan Hsu <hsuregan@google.com>
2021-12-28 19:50:56 +0000

    [CrOS Network] Add user initiated network connection result metric.
    Adds Network.Ash.{NetworkType}.ConnectionResult.UserInitiated metric
    recorded whenever a user initiated network connection attempt fails
    or succeeds.
    
    Bug: b/207589664
    Change-Id: Iefb256a0910c1b67e856c0d40e6a9b5a18b8aa37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350350
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954336}

--- chromeos/network/metrics/network_metrics_helper.cc
+++ chromeos/network/metrics/network_metrics_helper.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
-#include "chromeos/network/metrics/shill_connect_result.h"
+#include "chromeos/network/metrics/connection_results.h"
 #include "chromeos/network/network_handler.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
@@ -19,6 +19,8 @@ namespace {
 
 const char kNetworkMetricsPrefix[] = "Network.Ash.";
 const char kAllConnectionResultSuffix[] = ".ConnectionResult.All";
+const char kUserInitiatedConnectionResultSuffix[] =
+    ".ConnectionResult.UserInitiated";
 
 const char kWifi[] = "WiFi";
 const char kWifiOpen[] = "WiFi.SecurityOpen";
@@ -141,15 +143,39 @@ void NetworkMetricsHelper::LogAllConnectionResult(
   DCHECK(GetNetworkStateHandler());
   const NetworkState* network_state =
       GetNetworkStateHandler()->GetNetworkStateFromGuid(guid);
+  if (!network_state)
+    return;
 
   ShillConnectResult connect_result =
-      shill_error.has_value() ? ShillErrorToConnectResult(*shill_error)
-                              : ShillConnectResult::kSuccess;
+      shill_error ? ShillErrorToConnectResult(*shill_error)
+                  : ShillConnectResult::kSuccess;
 
   for (const auto& network_type : GetNetworkTypeHistogramNames(network_state)) {
     base::UmaHistogramEnumeration(
         kNetworkMetricsPrefix + network_type + kAllConnectionResultSuffix,
         connect_result);
+  }
+}
+
+// static
+void NetworkMetricsHelper::LogUserInitiatedConnectionResult(
+    const std::string& guid,
+    const absl::optional<std::string>& network_connection_error) {
+  DCHECK(GetNetworkStateHandler());
+  const NetworkState* network_state =
+      GetNetworkStateHandler()->GetNetworkStateFromGuid(guid);
+  if (!network_state)
+    return;
+
+  UserInitiatedConnectResult connect_result =
+      network_connection_error
+          ? NetworkConnectionErrorToConnectResult(*network_connection_error)
+          : UserInitiatedConnectResult::kSuccess;
+
+  for (const auto& network_type : GetNetworkTypeHistogramNames(network_state)) {
+    base::UmaHistogramEnumeration(kNetworkMetricsPrefix + network_type +
+                                      kUserInitiatedConnectionResultSuffix,
+                                  connect_result);
   }
 }
 
