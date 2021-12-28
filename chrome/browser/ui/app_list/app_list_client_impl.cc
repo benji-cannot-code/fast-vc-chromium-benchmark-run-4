5763e4fee1d6c06ecaf352b769bedee7ddcb1c9d
Stefan Kuhne <skuhne@google.com>
2021-12-28 19:43:29 +0000

    Addressed nits from CP
    Bug: 1280070
    Change-Id: I95e063c486af200a39e32093cc5d3eaa400259ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3353859
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Stefan Kuhne <skuhne@chromium.org>
    Auto-Submit: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954334}

--- chrome/browser/ui/app_list/app_list_client_impl.cc
+++ chrome/browser/ui/app_list/app_list_client_impl.cc
@@ -77,6 +77,16 @@ bool IsSessionActive() {
          session_manager::SessionState::ACTIVE;
 }
 
+bool CanBeHandledAsSystemUrl(const GURL& sanitized_url,
+                             ui::PageTransition transition) {
+  if (!PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_TYPED) &&
+      !PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_GENERATED)) {
+    return false;
+  }
+  return ChromeWebUIControllerFactory::GetInstance()->CanHandleUrl(
+      sanitized_url);
+}
+
 }  // namespace
 
 AppListClientImpl::AppListClientImpl()
@@ -562,11 +572,7 @@ void AppListClientImpl::OpenURL(Profile* profile,
   if (crosapi::browser_util::IsLacrosPrimaryBrowser()) {
     const GURL sanitized_url =
         crosapi::gurl_os_handler_utils::SanitizeAshURL(url);
-    if ((PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_TYPED) ||
-         PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_GENERATED)) &&
-        ChromeWebUIControllerFactory::GetInstance()->CanHandleUrl(
-            sanitized_url)) {
-      // Let our os url handler take care of the call.
+    if (CanBeHandledAsSystemUrl(sanitized_url, transition)) {
       crosapi::UrlHandlerAsh().OpenUrl(sanitized_url);
     } else {
       // Send the url to the current primary browser.
