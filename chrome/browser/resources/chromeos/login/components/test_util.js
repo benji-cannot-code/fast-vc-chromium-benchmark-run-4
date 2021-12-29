25d5025b7fc6125f93c660c39c0379b4ade70040
Renato Silva <rrsilva@google.com>
2021-12-29 18:21:09 +0000

    OOBE - Polymer3 - test_util
    Add a modularised version of test_util and a closure
    compilation target.
    
    Bug: 1184731
    Change-Id: I54182546e6521f79429c3cc47d8ed0305f2ddd45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3348617
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#954475}

---
+++ chrome/browser/resources/chromeos/login/components/test_util.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// #import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
+
+class ErrorStore {
+  constructor() {
+    this.store_ = [];
+    window.addEventListener('error', (e) => {
+      this.store_.push(e);
+    });
+  }
+
+  get length() {
+    return this.store_.length;
+  }
+}
+
+cr.addSingletonGetter(ErrorStore);
+if (window.cr == undefined) {
+  window.cr = {};
+}
+window.cr.ErrorStore = ErrorStore;
+cr.ErrorStore.getInstance();
--- chrome/browser/resources/chromeos/login/oobe_polymer3.js
+++ chrome/browser/resources/chromeos/login/oobe_polymer3.js
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {Oobe} from './cr_ui.m.js';
+import {DebuggerUI} from './debug/debug.m.js';
+import {loadTimeData} from './i18n_setup.js';
+import 'chrome://oobe/components/test_util.m.js';
+
 import 'chrome://oobe/screens/common/app_downloading.m.js';
 import 'chrome://oobe/screens/common/app_launch_splash.m.js';
 import 'chrome://oobe/screens/common/adb_sideloading.m.js';
@@ -46,10 +51,6 @@ import 'chrome://oobe/screens/oobe/oobe_eula.m.js';
 import 'chrome://oobe/screens/oobe/welcome.m.js';
 import 'chrome://oobe/screens/oobe/packaged_license.m.js';
 import 'chrome://oobe/screens/oobe/update.m.js';
-
-import {Oobe} from './cr_ui.m.js';
-import {DebuggerUI} from './debug/debug.m.js';
-import {loadTimeData} from './i18n_setup.js';
 
 function initializeDebugger() {
   if (document.readyState === 'loading')
