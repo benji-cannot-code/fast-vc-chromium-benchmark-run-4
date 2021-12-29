db0d5048b46778440fa189935131f7b4cd81591c
K. Moon <kmoon@chromium.org>
2021-12-29 21:40:57 +0000

    Fix PDFExtensionKeyEventTest flakiness
    Adds logic to PDFExtensionKeyEventTest::ScrollEventWaiter to only
    trigger when there are no pending scroll-to-remote operations. This
    prevents flakiness caused by confusing scroll operations resulting from
    loading with those triggered intentionally by the test.
    
    Fixed: 1281749
    Change-Id: If148f95431b8b2294fb8b09e6b92cfcbd0e654fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3361255
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954502}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -1601,8 +1601,16 @@ class PDFExtensionKeyEventTest : public PDFExtensionTest {
       content::ExecuteScriptAsync(
           guest_contents,
           R"(viewer.shadowRoot.querySelector('#scroller').onscroll = () => {
+            if (viewer.viewport.scrollContent_.unackedScrollsToRemote_ === 0) {
+              window.domAutomationController.send('dispatchedScrollEvent');
+            }
+          };
+          if (viewer.viewport.scrollContent_.unackedScrollsToRemote_ === 0) {
             window.domAutomationController.send('dispatchedScrollEvent');
           })");
+
+      // Wait for pending scroll-to-remotes to complete.
+      Wait();
     }
 
     void Reset() { message_queue_.ClearQueue(); }
@@ -1652,13 +1660,7 @@ class PDFExtensionKeyEventTest : public PDFExtensionTest {
 // static
 constexpr int PDFExtensionKeyEventTest::kScrollIncrement;
 
-// crbug.com/1281749
-#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MAC)
-#define MAYBE_ScrollWithSpace DISABLED_ScrollWithSpace
-#else
-#define MAYBE_ScrollWithSpace ScrollWithSpace
-#endif
-IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest, MAYBE_ScrollWithSpace) {
+IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest, ScrollWithSpace) {
   WebContents* guest_contents = LoadPdfGetGuestContents(
       embedded_test_server()->GetURL("/pdf/test-bookmarks.pdf"));
   SetInputFocusOnPlugin(guest_contents);
@@ -1696,13 +1698,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest, MAYBE_ScrollWithSpace) {
   EXPECT_EQ(viewport_height, GetViewportScrollPositionY(guest_contents));
 }
 
-// crbug.com/1281749
-#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MAC)
-#define MAYBE_ScrollWithPageDownUp DISABLED_ScrollWithPageDownUp
-#else
-#define MAYBE_ScrollWithPageDownUp ScrollWithPageDownUp
-#endif
-IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest, MAYBE_ScrollWithPageDownUp) {
+IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest, ScrollWithPageDownUp) {
   WebContents* guest_contents = LoadPdfGetGuestContents(
       embedded_test_server()->GetURL("/pdf/test-bookmarks.pdf"));
   SetInputFocusOnPlugin(guest_contents);
@@ -1742,14 +1738,7 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest, MAYBE_ScrollWithPageDownUp) {
   EXPECT_EQ(viewport_height, GetViewportScrollPositionY(guest_contents));
 }
 
-// crbug.com/1281749
-#if defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MAC)
-#define MAYBE_ScrollWithArrowLeftRight DISABLED_ScrollWithArrowLeftRight
-#else
-#define MAYBE_ScrollWithArrowLeftRight ScrollWithArrowLeftRight
-#endif
-IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest,
-                       MAYBE_ScrollWithArrowLeftRight) {
+IN_PROC_BROWSER_TEST_P(PDFExtensionKeyEventTest, ScrollWithArrowLeftRight) {
   WebContents* guest_contents = LoadPdfGetGuestContents(
       embedded_test_server()->GetURL("/pdf/test-bookmarks.pdf#zoom=200"));
   SetInputFocusOnPlugin(guest_contents);
