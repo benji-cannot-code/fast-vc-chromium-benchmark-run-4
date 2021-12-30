b9d68bbfc3e3850f4d495dc9e1ef732dd1527ff9
James Cook <jamescook@chromium.org>
2021-12-30 19:24:02 +0000

    ash: Fix log spam in tests from ash::(anonymous)::IsEphemeralUser
    Unit tests may not have a UserManager. This is expected, so remove the
    NOTIMPLEMENTED that emits a log line.
    
    Bug: none
    Change-Id: Id436c0f6b4a4ef68570f3d138d80a5bcc74392ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3362436
    Reviewed-by: Angus McLean <angusmclean@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954630}

--- ash/wallpaper/wallpaper_controller_impl.cc
+++ ash/wallpaper/wallpaper_controller_impl.cc
@@ -510,7 +510,7 @@ bool IsEphemeralUser(const AccountId& id) {
     return user_manager::UserManager::Get()->IsUserNonCryptohomeDataEphemeral(
         id);
   }
-  NOTIMPLEMENTED();
+  // Unit tests may not have a UserManager.
   return false;
 }
 
@@ -520,7 +520,7 @@ user_manager::UserType GetUserType(const AccountId& id) {
     if (auto* user = user_manager::UserManager::Get()->FindUser(id))
       return user->GetType();
   }
-  NOTIMPLEMENTED();
+  // Unit tests may not have a UserManager.
   return user_manager::USER_TYPE_REGULAR;
 }
 
