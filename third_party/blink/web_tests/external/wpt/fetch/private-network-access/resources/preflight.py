0ad4eaad138e9cbb10ed83b1c81ff81e0229b43d
Titouan Rigoudy <titouan@chromium.org>
2022-01-03 12:53:33 +0000

    [Private Network Access] Test Worker and SharedWorker script fetches.
    It turns out that Worker fetches respect PNA when not run with
    PlzDedicatedWorker enabled. This makes some sense, since the scripts are
    fetched from within Blink using the renderer's URLLoaderFactory.
    
    When PlzDedicatedWorker is enabled however, the fetches are not subject
    to PNA checks, since they are performed via a URLLoaderFactory belonging
    to the browser process (AFAIU). This factory must be passed the correct
    ClientSecurityState explicitly in order for PNA checks to apply.
    
    Shared workers are in a similar state, except PlzSharedWorker is enabled
    by default.
    
    Bug: chromium:1282637,chromium:1282639
    Change-Id: I22cd1364b6cc6b558267867480e4b22c67d78759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3356196
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954919}

--- third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/preflight.py
+++ third_party/blink/web_tests/external/wpt/fetch/private-network-access/resources/preflight.py
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #   - unspecified: this endpoint responds with no CORS headers to non-preflight
 #     requests. This should fail CORS-enabled requests, but be sufficient for
 #     no-CORS requests.
+# - mime-type: Optional. If set, the final response's `Content-Type` header is
+#   set to this parameter's value.
+# - body: Optional. If set, the final response's body is set to this parameter's
+#   value.
 #
 
 _ACAO = ("Access-Control-Allow-Origin", "*")
@@ -59,12 +63,18 @@ def _handle_preflight_request(request, response):
 def _handle_final_request(request, response):
   uuid = _get_uuid(request)
   if uuid is not None and request.server.stash.take(uuid) is None:
-    raise Exception("no matching preflight request for {}".format(uuid))
+    return (405, [], "no preflight received for {}".format(uuid))
 
   mode = request.GET.get(b"final-headers")
   headers = _get_response_headers(request.method, mode)
 
-  return (headers, "success")
+  mime_type = request.GET.get(b"mime-type")
+  if mime_type is not None:
+    headers.append(("Content-Type", mime_type),)
+
+  body = request.GET.get(b"body") or "success"
+
+  return (headers, body)
 
 def main(request, response):
   try:
