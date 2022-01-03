4be19c7cfaef37c7ea0d124535d7c7b41b118572
Sergiy Belozorov <sergiyb@chromium.org>
2022-01-03 15:32:35 +0000

    Simplify algorithm computing integer and string hashes
    Bug: 1280752
    Change-Id: I99d99c82a6113dd1db9e3b264309189a268beb9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352459
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Sergiy Belozorov <sergiyb@chromium.org>
    Auto-Submit: Sergiy Belozorov <sergiyb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#954954}

--- components/policy/test_support/policy_storage.cc
+++ components/policy/test_support/policy_storage.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/policy/test_support/policy_storage.h"
+#include "base/big_endian.h"
 #include "crypto/sha2.h"
 
 namespace policy {
@@ -62,16 +63,14 @@ std::vector<std::string> PolicyStorage::GetMatchingSerialHashes(
     uint64_t remainder) const {
   std::vector<std::string> hashes;
   for (const auto& [serial, enrollment_state] : initial_enrollment_states_) {
-    uint64_t hash = 0;
-    char hstr[sizeof(hash)];
-    crypto::SHA256HashString(serial, &hstr, sizeof(hash));
-    // Convert hash string to uint64_t using big-endian byte-order.
-    for (size_t i = 0; i < sizeof(hash); i++) {
-      uint64_t byte = static_cast<unsigned char>(hstr[i]);
-      hash += byte << (sizeof(hash) - i - 1) * CHAR_BIT;
+    uint64_t hash = 0UL;
+    uint8_t hash_bytes[sizeof(hash)];
+    crypto::SHA256HashString(serial, hash_bytes, sizeof(hash));
+    base::ReadBigEndian(hash_bytes, &hash);
+    if (hash % modulus == remainder) {
+      hashes.emplace_back(reinterpret_cast<const char*>(hash_bytes),
+                          sizeof(hash));
     }
-    if (hash % modulus == remainder)
-      hashes.emplace_back(hstr, sizeof(uint64_t));
   }
   return hashes;
 }
