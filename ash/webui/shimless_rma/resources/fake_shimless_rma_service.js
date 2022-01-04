fcf3ec5654433694ecfd41e158691ade78daeceb
Gavin Williams <gavinwill@chromium.org>
2022-01-04 18:53:28 +0000

    Shimless: Add retry finalization to RMA service
    - Retries after finalization fails
    - Implemented in upstream CL
    
    Bug: 1198187
    Change-Id: If05e4b34b92d699ede810591adfde05c804967b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3360057
    Reviewed-by: Gavin Dodd <gavindodd@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955247}

--- ash/webui/shimless_rma/resources/fake_shimless_rma_service.js
+++ ash/webui/shimless_rma/resources/fake_shimless_rma_service.js
@@ -669,6 +669,13 @@ export class FakeShimlessRmaService {
   /**
    * @return {!Promise<!StateResult>}
    */
+  retryFinalization() {
+    return this.getNextStateForMethod_('retryFinalization', State.kFinalize);
+  }
+
+  /**
+   * @return {!Promise<!StateResult>}
+   */
   writeProtectManuallyEnabled() {
     return this.getNextStateForMethod_(
         'writeProtectManuallyEnabled', State.kWaitForManualWPEnable);
@@ -1229,6 +1236,7 @@ export class FakeShimlessRmaService {
     this.methods_.register('retryProvisioning');
     this.methods_.register('provisioningComplete');
 
+    this.methods_.register('retryFinalization');
     this.methods_.register('finalizationComplete');
 
     this.methods_.register('writeProtectManuallyEnabled');
