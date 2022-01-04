5cb315c0f6da985de82bc47e01c5b91d497c81e4
Roland Bock <rbock@google.com>
2022-01-04 15:39:05 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=engedy@chromium.org
    
    Bug: 1283907
    Change-Id: I65565c919a88466c62529e088a17b4712f9671dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366379
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955136}

--- chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
+++ chrome/browser/file_system_access/chrome_file_system_access_permission_context.cc
@@ -866,7 +866,7 @@ ChromeFileSystemAccessPermissionContext::GetReadPermissionGrant(
           // Open and Save dialog only grant read access for individual files.
           if (handle_type == HandleType::kDirectory)
             break;
-          FALLTHROUGH;
+          [[fallthrough]];
         case UserAction::kDragAndDrop:
           // Drag&drop grants read access for all handles.
           existing_grant->SetStatus(
