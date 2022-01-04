d513daffab75d4eb563b795a36857eae4a2a1c74
Roland Bock <rbock@google.com>
2022-01-04 19:35:37 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=qinmin@chromium.org
    
    Bug: 1283907
    Change-Id: I4b21a239ab40d5b9ff954fc452365c3eb0b02a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366207
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955287}

--- components/download/internal/common/download_utils.cc
+++ components/download/internal/common/download_utils.cc
@@ -648,7 +648,7 @@ bool IsDownloadDone(const GURL& url,
     case DownloadItem::IN_PROGRESS:
       return false;
     case DownloadItem::COMPLETE:
-      FALLTHROUGH;
+      [[fallthrough]];
     case DownloadItem::CANCELLED:
       return true;
     case DownloadItem::INTERRUPTED:
