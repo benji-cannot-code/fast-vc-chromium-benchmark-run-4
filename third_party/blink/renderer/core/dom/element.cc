679e005123409f54f5ad5989a319f4f07b714a7c
Rune Lillesveen <futhark@chromium.org>
2022-01-04 09:45:45 +0000

    [@container] Avoid layout root inside skipped container
    Marking an element for layout outside of style recalc may cause us to
    have an element marked as a layout root inside a query container when we
    reach the query container for style recalc.
    
    The query container was skipped for style recalc if the container itself
    or any descendants were marked for layout. In the case where a container
    query descendant was a root we would not reach the container during the
    subsequent layout.
    
    Change the condition to having the query container itself being marked
    for layout.
    
    Bug: 1279368
    Change-Id: Ibdf03c4582ca7e99261dc66d01ad8dbb08bc4e50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3360892
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955085}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2987,7 +2987,7 @@ bool Element::SkipStyleRecalcForContainer(
     }
   } else {
     LayoutObject* layout_object = GetLayoutObject();
-    if (!layout_object || !layout_object->NeedsLayout() ||
+    if (!layout_object || !layout_object->SelfNeedsLayout() ||
         !layout_object->IsEligibleForSizeContainment()) {
       return false;
     }
