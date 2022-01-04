5763144a84188e99c2d0f21db24c6c0808dd066f
Maks Orlovich <morlovich@chromium.org>
2022-01-04 16:50:20 +0000

    Code health: port away from deprecated DictionaryValue::HasKey in /chrome/browser/extensions/api/proxy
    This is a portion for /chrome/browser/extensions/api/proxy.
    (And in some cases also adjust away from other deprecated Value APIs,
     when convenient/nearby)
    
    This CL was uploaded by git cl split.
    
    R=battre@chromium.org
    
    Bug: 1187060
    Change-Id: Ia97d72a3f8caeb281c8010703e64e07b04bcc19d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366695
    Auto-Submit: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955188}

--- chrome/browser/extensions/api/proxy/proxy_api_helpers.h
+++ chrome/browser/extensions/api/proxy/proxy_api_helpers.h
@@ -11,16 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/values.h"
 #include "components/proxy_config/proxy_prefs.h"
 #include "net/proxy_resolution/proxy_config.h"
 
 class ProxyConfigDictionary;
-
-namespace base {
-class DictionaryValue;
-class ListValue;
-class Value;
-}
 
 namespace extensions {
 namespace proxy_api_helpers {
@@ -105,12 +100,11 @@ bool GetProxyServer(const base::DictionaryValue* proxy_server,
 
 // Joins a list of URLs (stored as StringValues) in |list| with |joiner|
 // to |out|. Returns true if successful and sets |error| otherwise.
-bool JoinUrlList(const base::ListValue* list,
+bool JoinUrlList(base::Value::ConstListView list,
                  const std::string& joiner,
                  std::string* out,
                  std::string* error,
                  bool* bad_message);
-
 
 // Helper functions for browser->extension pref transformation:
 
