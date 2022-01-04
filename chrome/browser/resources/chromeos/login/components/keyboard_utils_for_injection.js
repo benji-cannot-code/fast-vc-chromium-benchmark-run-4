a4b04de8cbee8bbbc8486749db2617d8d5af83fe
Renato Silva <rrsilva@google.com>
2022-01-04 17:00:21 +0000

    OOBE - Make keyboard_utils accessible via JS
    Prepares c/b/r/chromeos/keyboard/keyboard_utils to be accessible
    via JS. The script defined in this file is used within OOBE via
    normal imports and through its plain text representation that is
    exposed to the global object via keyboard_utils_for_injection.js.
    
    Also improve the closure compilation of the welcome dialog.
    
    Bug: 1184731
    Change-Id: I1270e8ca9f7f625c5980c3be578612d6a552f44c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3361038
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#955196}

---
+++ chrome/browser/resources/chromeos/login/components/keyboard_utils_for_injection.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * TODO(crbug.com/1229130) - Find a better way to do this.
+ *
+ * Code which is embedded inside of the enterprise enrollment webview.
+ * See /screens/oobe/enterprise_enrollment.js for details.
+ */
+const INJECTED_WEBVIEW_SCRIPT = String.raw`
+                    (function() {
+                       // <include src="../../keyboard/keyboard_utils.js">
+                       keyboard.initializeKeyboardFlow(true);
+                     })();`;
+
+window.INJECTED_WEBVIEW_SCRIPT = INJECTED_WEBVIEW_SCRIPT;
