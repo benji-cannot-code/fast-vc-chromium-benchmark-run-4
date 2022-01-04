7148abbca96766eca652a95486a22c57fa14086a
Ella Ge <eirage@google.com>
2022-01-04 18:03:35 +0000

    [Sheriff] Disable PageActionBrowserTest.UnloadPageAction on Linux
    Test is flaky.
    
    Bug: 1283900
    Change-Id: I165f526aa803d75105c3c0480999612308098474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3365988
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Ella Ge <eirage@google.com>
    Owners-Override: Ella Ge <eirage@google.com>
    Cr-Commit-Position: refs/heads/main@{#955215}

--- chrome/browser/extensions/page_action_browsertest.cc
+++ chrome/browser/extensions/page_action_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_action_test_util.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
@@ -117,7 +118,13 @@ IN_PROC_BROWSER_TEST_P(PageActionBrowserTest, SameDocumentNavigation) {
 }
 
 // Tests that the location bar forgets about unloaded page actions.
-IN_PROC_BROWSER_TEST_P(PageActionBrowserTest, UnloadPageAction) {
+// Flaky on Linux: https://crbug.com/1283900
+#if defined(OS_LINUX)
+#define MAYBE_UnloadPageAction DISABLED_UnloadPageAction
+#else
+#define MAYBE_UnloadPageAction UnloadPageAction
+#endif
+IN_PROC_BROWSER_TEST_P(PageActionBrowserTest, MAYBE_UnloadPageAction) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   base::FilePath extension_path(
