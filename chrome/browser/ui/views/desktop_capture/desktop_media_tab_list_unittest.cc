e4bc82a508f2c2299062af90741dc373e275b375
Ella Ge <eirage@chromium.org>
2022-01-04 17:53:14 +0000

    [Sheriff] Disable IgnorePreviewUpdatesForUnselectedSource on Lacros
    Test is flaky.
    
    Bug: 1284150
    Change-Id: I68686d3f7a04c8cd92ea6293f8d3fd442aa2adc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366524
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Owners-Override: Ella Ge <eirage@google.com>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955210}

--- chrome/browser/ui/views/desktop_capture/desktop_media_tab_list_unittest.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_tab_list_unittest.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/desktop_capture/desktop_media_tab_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "chrome/browser/media/webrtc/desktop_media_list.h"
 #include "chrome/browser/media/webrtc/fake_desktop_media_list.h"
 #include "chrome/browser/ui/views/desktop_capture/desktop_media_picker_views.h"
@@ -155,7 +157,15 @@ TEST_F(DesktopMediaTabListTest, UpdatedPreview) {
   EXPECT_TRUE(preview_->GetImage().BackedBySameObjectAs(new_preview));
 }
 
-TEST_F(DesktopMediaTabListTest, IgnorePreviewUpdatesForUnselectedSource) {
+// crbug.com/1284150: flaky on Lacros
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_IgnorePreviewUpdatesForUnselectedSource \
+  DISABLED_IgnorePreviewUpdatesForUnselectedSource
+#else
+#define MAYBE_IgnorePreviewUpdatesForUnselectedSource \
+  IgnorePreviewUpdatesForUnselectedSource
+#endif
+TEST_F(DesktopMediaTabListTest, MAYBE_IgnorePreviewUpdatesForUnselectedSource) {
   test_api_.PressMouseOnSourceAtIndex(0);
 
   // Let the tab list know that the non-selected source #1 has a new preview.
