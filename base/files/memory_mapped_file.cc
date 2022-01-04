49c6959f7e5fe10a7d7b4af952ef320144ca0fbc
Roland Bock <rbock@google.com>
2022-01-04 16:09:18 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=fdoray@chromium.org, wfh@chromium.org
    
    Bug: 1283907
    Change-Id: I02571bc74507a6f6938faf5154d11efb5b456499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366340
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955162}

--- base/files/memory_mapped_file.cc
+++ base/files/memory_mapped_file.cc
@@ -90,7 +90,7 @@ bool MemoryMappedFile::Initialize(File file,
           return false;
         }
       }
-      FALLTHROUGH;
+      [[fallthrough]];
     case READ_ONLY:
     case READ_WRITE:
       // Ensure that the region values are valid.
