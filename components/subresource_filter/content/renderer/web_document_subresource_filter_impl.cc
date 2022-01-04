5921915e526a8c5ff9b7eff8371c03bc4064e3c7
Roland Bock <rbock@google.com>
2022-01-04 21:42:21 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=johnidel@chromium.org
    
    Bug: 1283907
    Change-Id: I6907786be1b7ecf46ea783f9c2ee5dc4093c95ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3360670
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955368}

--- components/subresource_filter/content/renderer/web_document_subresource_filter_impl.cc
+++ components/subresource_filter/content/renderer/web_document_subresource_filter_impl.cc
@@ -82,7 +82,7 @@ proto::ElementType ToElementType(
 WebLoadPolicy ToWebLoadPolicy(LoadPolicy load_policy) {
   switch (load_policy) {
     case LoadPolicy::EXPLICITLY_ALLOW:
-      FALLTHROUGH;
+      [[fallthrough]];
     case LoadPolicy::ALLOW:
       return WebLoadPolicy::kAllow;
     case LoadPolicy::DISALLOW:
