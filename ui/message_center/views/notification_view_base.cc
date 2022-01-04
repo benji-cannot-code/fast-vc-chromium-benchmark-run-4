10a0b5a148bf8552f62e436d5bd28fdf35557602
Andre Le <leandre@chromium.org>
2022-01-04 21:26:08 +0000

    NotificationRevamp: Animation for toggle inline settings
    This CL added animations when toggling inline settings according to
    spec mentioned in the bug.
    
    Fixed: 1282009
    Change-Id: I45b656e646aacfc62f746afb153b71aed1f93442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352411
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955349}

--- ui/message_center/views/notification_view_base.cc
+++ ui/message_center/views/notification_view_base.cc
@@ -909,8 +909,6 @@ void NotificationViewBase::ToggleInlineSettings(const ui::Event& event) {
     if (!weak_ptr)
       return;
   }
-
-  PreferredSizeChanged();
 }
 
 NotificationControlButtonsView* NotificationViewBase::GetControlButtonsView()
