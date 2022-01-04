0551204d7a2011fb3428c5ef2dc15438b829a75f
Mike Dougherty <michaeldo@chromium.org>
2022-01-04 20:12:16 +0000

    [iOS] Remove the URLBlocklistIOS feature flag
    This feature was disabled by default and preventing enterprise policy
    from enabling URLBlocking.
    
    Bug: 1074322
    Change-Id: I496084da47355dcc993e2ca8dadac42b7f364228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3321511
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955312}

--- ios/chrome/browser/policy/policy_features.cc
+++ ios/chrome/browser/policy/policy_features.cc
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/version_info/version_info.h"
 #include "ios/chrome/browser/chrome_switches.h"
 #include "ios/chrome/common/channel_info.h"
-#include "ios/web/common/features.h"
-
-const base::Feature kURLBlocklistIOS{"URLBlocklistIOS",
-                                     base::FEATURE_ENABLED_BY_DEFAULT};
 
 namespace {
 
@@ -48,6 +44,5 @@ bool ShouldInstallURLBlocklistPolicyHandlers() {
 }
 
 bool IsURLBlocklistEnabled() {
-  return ShouldInstallURLBlocklistPolicyHandlers() &&
-         base::FeatureList::IsEnabled(kURLBlocklistIOS);
+  return ShouldInstallURLBlocklistPolicyHandlers();
 }
--- ios/chrome/browser/policy/policy_features.h
+++ ios/chrome/browser/policy/policy_features.h
@@ -6,11 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_POLICY_POLICY_FEATURES_H_
 #define IOS_CHROME_BROWSER_POLICY_POLICY_FEATURES_H_
 
-#include "base/feature_list.h"
-
-// Feature flag for supporting the URLBlocklist enterprise policy on iOS.
-extern const base::Feature kURLBlocklistIOS;
-
 // Returns true if the core enterprise policy infrastructure is enabled. Does
 // not control whether policy data is parsed and made user visible; that is
 // controlled by |ShouldInstallEnterprisePolicyHandlers()| below.
