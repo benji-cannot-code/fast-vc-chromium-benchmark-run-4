d5a0ffc06eaa88f703b175f225cc92f7331cabfc
Roland Bock <rbock@google.com>
2022-01-04 18:41:41 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=khorimoto@chromium.org
    
    Bug: 1283907
    Change-Id: I4e952a9cc9f713923b8612e44d4381843f577956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3360663
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955236}

--- ash/components/phonehub/tether_controller_impl.cc
+++ ash/components/phonehub/tether_controller_impl.cc
@@ -449,9 +449,9 @@ TetherController::Status TetherControllerImpl::ComputeStatus() const {
 
   switch (connection_state) {
     case ConnectionStateType::kOnline:
-      FALLTHROUGH;
+      [[fallthrough]];
     case ConnectionStateType::kConnected:
-      FALLTHROUGH;
+      [[fallthrough]];
     case ConnectionStateType::kPortal:
       return Status::kConnected;
 
