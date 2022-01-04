8252583b5527711cc030c1b006056c709fdf2d0a
Roland Bock <rbock@google.com>
2022-01-04 18:39:23 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=msw@chromium.org
    
    Bug: 1283907
    Change-Id: I04a461eab5ac76d1c347802a6d44ed4ebf9e1f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366343
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955234}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -400,7 +400,7 @@ bool BrowserCommandController::ExecuteCommandWithDisposition(
       break;
     case IDC_RELOAD_CLEARING_CACHE:
       ClearCache(browser_);
-      FALLTHROUGH;
+      [[fallthrough]];
     case IDC_RELOAD_BYPASSING_CACHE:
       ReloadBypassingCache(browser_, disposition);
       break;
--- chrome/browser/ui/browser_navigator.cc
+++ chrome/browser/ui/browser_navigator.cc
@@ -189,7 +189,7 @@ std::pair<Browser*, int> GetBrowserAndTabForDisposition(
         return index;
     }
 #endif
-      FALLTHROUGH;
+      [[fallthrough]];
     case WindowOpenDisposition::CURRENT_TAB:
       if (params.browser)
         return {params.browser, -1};
@@ -213,7 +213,7 @@ std::pair<Browser*, int> GetBrowserAndTabForDisposition(
           return index;
       }
     }
-      FALLTHROUGH;
+      [[fallthrough]];
     case WindowOpenDisposition::NEW_FOREGROUND_TAB:
     case WindowOpenDisposition::NEW_BACKGROUND_TAB:
       // See if we can open the tab in the window this navigator is bound to.
@@ -306,7 +306,7 @@ void NormalizeDisposition(NavigateParams* params) {
       // automatically.
       if (params->window_action == NavigateParams::NO_ACTION)
         params->window_action = NavigateParams::SHOW_WINDOW;
-      FALLTHROUGH;
+      [[fallthrough]];
     }
     case WindowOpenDisposition::NEW_FOREGROUND_TAB:
     case WindowOpenDisposition::SINGLETON_TAB:
