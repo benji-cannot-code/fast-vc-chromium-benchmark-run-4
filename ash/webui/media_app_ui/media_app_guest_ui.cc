99686939fb14895c39f9c2fd2c8119e3d2139666
Joseph Kim <josephkimsh@google.com>
2022-01-04 07:40:22 +0000

    Media App: Put PDF in readonly mode when `PdfAnnotationsEnabled` is off
    This prevents users from being able to save{,as,copy}, add freehand or
    text annotations while a PDF is loaded in the media app and PDF
    annotations are disallowed by enterprise policy.
    
    Requires passing down the WebUI to get Prefs similar to how Help App
    does it
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/web_applications/help_app/help_app_untrusted_ui_config.cc;l=94?q=help%20app%20populateLoadTime&ss=chromium%2Fchromium%2Fsrc
    
    Bug: b:207552979
    Change-Id: I52b32e319b5a2f3616e6af470f8069626a26e58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3365876
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Commit-Queue: Joseph Kim <josephkimsh@google.com>
    Cr-Commit-Position: refs/heads/main@{#955065}

--- ash/webui/media_app_ui/media_app_guest_ui.cc
+++ ash/webui/media_app_ui/media_app_guest_ui.cc
@@ -24,6 +24,7 @@ namespace ash {
 namespace {
 
 content::WebUIDataSource* CreateMediaAppUntrustedDataSource(
+    content::WebUI* web_ui,
     MediaAppGuestUIDelegate* delegate) {
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(kChromeUIMediaAppGuestURL);
@@ -52,7 +53,7 @@ content::WebUIDataSource* CreateMediaAppUntrustedDataSource(
       kChromeosMediaAppBundleResources, kChromeosMediaAppBundleResourcesSize));
 
   // Note: go/bbsrc/flags.ts processes this.
-  delegate->PopulateLoadTimeData(source);
+  delegate->PopulateLoadTimeData(web_ui, source);
   source->UseStringsJs();
 
   source->AddFrameAncestor(GURL(kChromeUIMediaAppURL));
@@ -105,7 +106,7 @@ MediaAppGuestUI::MediaAppGuestUI(content::WebUI* web_ui,
     : UntrustedWebUIController(web_ui),
       WebContentsObserver(web_ui->GetWebContents()) {
   content::WebUIDataSource* untrusted_source =
-      CreateMediaAppUntrustedDataSource(delegate);
+      CreateMediaAppUntrustedDataSource(web_ui, delegate);
 
   auto* browser_context = web_ui->GetWebContents()->GetBrowserContext();
   content::WebUIDataSource::Add(browser_context, untrusted_source);
--- chrome/browser/ash/web_applications/media_app/media_app_guest_ui_config.cc
+++ chrome/browser/ash/web_applications/media_app/media_app_guest_ui_config.cc
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/webui/media_app_ui/url_constants.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/common/channel_info.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/prefs/pref_service.h"
 #include "components/version_info/channel.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -21,13 +24,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 ChromeMediaAppGuestUIDelegate::ChromeMediaAppGuestUIDelegate() = default;
 
 void ChromeMediaAppGuestUIDelegate::PopulateLoadTimeData(
+    content::WebUI* web_ui,
     content::WebUIDataSource* source) {
+  Profile* profile = Profile::FromWebUI(web_ui);
+  PrefService* pref_service = profile->GetPrefs();
+
   source->AddString("appLocale", g_browser_process->GetApplicationLocale());
   source->AddBoolean(
       "audioHandler",
       base::FeatureList::IsEnabled(chromeos::features::kMediaAppHandlesAudio));
   source->AddBoolean("pdfInInk", base::FeatureList::IsEnabled(
                                      chromeos::features::kMediaAppHandlesPdf));
+  source->AddBoolean("pdfReadonly",
+                     !pref_service->GetBoolean(prefs::kPdfAnnotationsEnabled));
   source->AddBoolean(
       "pdfTextAnnotation",
       base::FeatureList::IsEnabled(chromeos::features::kMediaAppHandlesPdf));
