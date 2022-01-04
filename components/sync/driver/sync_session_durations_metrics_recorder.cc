3555453a6b9882158542f2d3c8cedcdc73017ae9
Roland Bock <rbock@google.com>
2022-01-04 16:08:48 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=jkrcal@chromium.org
    
    Bug: 1283907
    Change-Id: I7c9ab0e782810ecf2d6d171b4e0fa5a602644a58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366209
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Jan Krčál <jkrcal@chromium.org>
    Commit-Queue: Jan Krčál <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955161}

--- components/sync/driver/sync_session_durations_metrics_recorder.cc
+++ components/sync/driver/sync_session_durations_metrics_recorder.cc
@@ -225,7 +225,7 @@ void SyncSessionDurationsMetricsRecorder::LogSigninDuration(
     case FeatureState::UNKNOWN:
       // Since the feature wasn't working for the user if we didn't know its
       // state, log the status as off.
-      FALLTHROUGH;
+      [[fallthrough]];
     case FeatureState::OFF:
       LogDuration("Session.TotalDuration.WithoutAccount", session_length);
       break;
@@ -247,7 +247,7 @@ void SyncSessionDurationsMetricsRecorder::LogSyncAndAccountDuration(
       break;
     case GetFeatureStates(FeatureState::ON, FeatureState::UNKNOWN):
       // Sync engine not initialized yet, default to it being off.
-      FALLTHROUGH;
+      [[fallthrough]];
     case GetFeatureStates(FeatureState::ON, FeatureState::OFF):
       LogDuration("Session.TotalDuration.NotOptedInToSyncWithAccount",
                   session_length);
@@ -258,7 +258,7 @@ void SyncSessionDurationsMetricsRecorder::LogSyncAndAccountDuration(
       break;
     case GetFeatureStates(FeatureState::OFF, FeatureState::UNKNOWN):
       // Sync engine not initialized yet, default to it being off.
-      FALLTHROUGH;
+      [[fallthrough]];
     case GetFeatureStates(FeatureState::OFF, FeatureState::OFF):
       LogDuration("Session.TotalDuration.NotOptedInToSyncWithoutAccount",
                   session_length);
