e84ec176dd207b0ec01c33d40291f54c58c7f4a4
Tim Sergeant <tsergeant@chromium.org>
2022-01-04 22:29:44 +0000

    ArcIntentHelper: Remove deprecated OnPreferredAppsChanged mojo method
    This method has been deprecated for ~6 weeks and is no longer called by
    ARC code.
    
    Bug: 1265318
    Change-Id: I6589f0edfb8851050bb071b03a3f9e0f59ff2555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3352266
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955398}

--- components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge_unittest.cc
@@ -210,7 +210,6 @@ TEST_F(ArcIntentHelperTest, TestObserver) {
                 OnIntentFiltersUpdated,
                 (const absl::optional<std::string>& package_name),
                 (override));
-    MOCK_METHOD(void, OnPreferredAppsChanged, (), (override));
     MOCK_METHOD(
         void,
         OnArcSupportedLinksChanged,
@@ -252,13 +251,6 @@ TEST_F(ArcIntentHelperTest, TestObserver) {
   }
 
   {
-    // Observer should be called when preferred apps change.
-    EXPECT_CALL(observer, OnPreferredAppsChanged);
-    instance_->OnPreferredAppsChangedDeprecated(/*added=*/{}, /*deleted=*/{});
-    testing::Mock::VerifyAndClearExpectations(&observer);
-  }
-
-  {
     // Observer should be called when supported links change.
     EXPECT_CALL(observer, OnArcSupportedLinksChanged);
     instance_->OnSupportedLinksChanged(
@@ -273,7 +265,6 @@ TEST_F(ArcIntentHelperTest, TestObserver) {
   instance_->OnDownloadAdded(/*relative_path=*/"Download/foo/bar.pdf",
                              /*owner_package_name=*/"owner_package_name");
   instance_->OnIntentFiltersUpdated(/*filters=*/{});
-  instance_->OnPreferredAppsChangedDeprecated(/*added=*/{}, /*deleted=*/{});
   instance_->OnSupportedLinksChanged(
       /*added_packages=*/{},
       /*removed_packages=*/{},
