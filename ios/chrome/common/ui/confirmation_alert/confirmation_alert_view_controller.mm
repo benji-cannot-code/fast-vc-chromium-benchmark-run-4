af301bb84062c778abeb032ea0480ab7f059db86
Aliona DANGLA <alionadangla@google.com>
2022-01-04 19:33:27 +0000

    [iOS] Add accessibility label to password manager logo
    As VoiceOver did not work on derived child of view inside the scroll
    view, the specific content view is the scroll view.
    
    See VoiceOver + dynamic type test video:
    https://drive.google.com/file/d/16_8BC_DjRsTvWP1xZeIW2Kqs8YS917nE/view?usp=sharing&resourcekey=0-8FI-QkiH9T303ApmO4ydnA
    
    Fixed: 1282434
    Change-Id: Ibe19e8b921985ac31df9c1984cdb05bf7c25bbf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3358588
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Cr-Commit-Position: refs/heads/main@{#955283}

--- ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.mm
+++ ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.mm
@@ -70,8 +70,7 @@ constexpr CGFloat kContentMaxWidth = 500;
     _customSpacingAfterImage = kStackViewSpacingAfterIllustration;
     _showDismissBarButton = YES;
     _dismissBarButtonSystemItem = UIBarButtonSystemItemDone;
-    _specificContentView = [[UIView alloc] init];
-    _specificContentView.translatesAutoresizingMaskIntoConstraints = NO;
+    _specificContentLayoutGuide = [[UILayoutGuide alloc] init];
   }
   return self;
 }
@@ -105,8 +104,12 @@ constexpr CGFloat kContentMaxWidth = 500;
   UIScrollView* scrollView = [self createScrollView];
   [scrollView addSubview:scrollContentView];
   [scrollView addSubview:stackView];
-  [scrollView addSubview:self.specificContentView];
+  [scrollView addLayoutGuide:self.specificContentLayoutGuide];
   [self.view addSubview:scrollView];
+
+  // Needed to have VoiceOver working to elements added in derived view
+  // controller.
+  self.specificContentSuperview = scrollView;
 
   self.view.preservesSuperviewLayoutMargins = YES;
   UILayoutGuide* margins = self.view.layoutMarginsGuide;
@@ -133,14 +136,15 @@ constexpr CGFloat kContentMaxWidth = 500;
     [stackView.topAnchor
         constraintGreaterThanOrEqualToAnchor:scrollContentView.topAnchor],
     [stackView.bottomAnchor
-        constraintLessThanOrEqualToAnchor:self.specificContentView.topAnchor
+        constraintLessThanOrEqualToAnchor:self.specificContentLayoutGuide
+                                              .topAnchor
                                  constant:-kScrollViewBottomInsets],
 
-    [self.specificContentView.bottomAnchor
+    [self.specificContentLayoutGuide.bottomAnchor
         constraintEqualToAnchor:scrollContentView.bottomAnchor],
-    [self.specificContentView.centerXAnchor
+    [self.specificContentLayoutGuide.centerXAnchor
         constraintEqualToAnchor:self.view.centerXAnchor],
-    [self.specificContentView.widthAnchor
+    [self.specificContentLayoutGuide.widthAnchor
         constraintLessThanOrEqualToAnchor:scrollView.widthAnchor],
 
     // Constrain its height to at least the scroll view height, so that derived
