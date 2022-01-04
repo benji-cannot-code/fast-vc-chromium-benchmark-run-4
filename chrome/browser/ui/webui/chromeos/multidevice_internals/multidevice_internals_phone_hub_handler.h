0b38c9d30040b92d8fc464e52d4729d77acefb51
Matt Walliser <mattwalliser@google.com>
2022-01-04 22:42:51 +0000

    [CameraRoll] Update chrome://multidevice-internals for new features.
    https://screenshot.googleplex.com/9bxipXAxqVQpnuL
    
    Bug: 1221297
    Change-Id: Icc4c14dc9d9ca67d4a553b3be002fb353a85ef5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3346420
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Matt Walliser <mattwalliser@google.com>
    Cr-Commit-Position: refs/heads/main@{#955409}

--- chrome/browser/ui/webui/chromeos/multidevice_internals/multidevice_internals_phone_hub_handler.h
+++ chrome/browser/ui/webui/chromeos/multidevice_internals/multidevice_internals_phone_hub_handler.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_MULTIDEVICE_INTERNALS_MULTIDEVICE_INTERNALS_PHONE_HUB_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_MULTIDEVICE_INTERNALS_MULTIDEVICE_INTERNALS_PHONE_HUB_HANDLER_H_
 
+#include "ash/components/phonehub/camera_roll_manager.h"
 #include "ash/components/phonehub/do_not_disturb_controller.h"
 // TODO(https://crbug.com/1164001): move to forward declaration.
 #include "ash/components/phonehub/fake_phone_hub_manager.h"
@@ -26,7 +27,8 @@ class MultidevicePhoneHubHandler
       public phonehub::DoNotDisturbController::Observer,
       public phonehub::FindMyDeviceController::Observer,
       public phonehub::TetherController::Observer,
-      public phonehub::OnboardingUiTracker::Observer {
+      public phonehub::OnboardingUiTracker::Observer,
+      public ash::phonehub::CameraRollManager::Observer {
  public:
   MultidevicePhoneHubHandler();
   MultidevicePhoneHubHandler(const MultidevicePhoneHubHandler&) = delete;
@@ -56,6 +58,9 @@ class MultidevicePhoneHubHandler
   // OnboardingUiTracker::Observer
   void OnShouldShowOnboardingUiChanged() override;
 
+  // CameraRollManager::Observer:
+  void OnCameraRollViewUiStateUpdated() override;
+
   void EnableRealPhoneHubManager();
   void EnableFakePhoneHubManager();
   void HandleEnableFakePhoneHubManager(const base::ListValue* args);
@@ -73,7 +78,7 @@ class MultidevicePhoneHubHandler
   void HandleResetHasNotificationSetupUiBeenDismissed(
       const base::ListValue* args);
   void HandleResetCameraRollOnboardingUiDismissed(const base::ListValue* args);
-  void HandleSetCameraRoll(const base::ListValue* args);
+  void HandleSetFakeCameraRoll(const base::ListValue* args);
 
   void AddObservers();
   void RemoveObservers();
@@ -94,6 +99,9 @@ class MultidevicePhoneHubHandler
   base::ScopedObservation<phonehub::OnboardingUiTracker,
                           phonehub::OnboardingUiTracker::Observer>
       onboarding_ui_tracker_observation_{this};
+  base::ScopedObservation<ash::phonehub::CameraRollManager,
+                          ash::phonehub::CameraRollManager::Observer>
+      camera_roll_manager_observation_{this};
 };
 
 }  // namespace multidevice
