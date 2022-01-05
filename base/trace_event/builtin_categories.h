157a414ecf4b029db4ded5e636a7ed25c0e57dad
Alexander Timin <altimin@chromium.org>
2022-01-05 18:43:21 +0000

    [tracing] Add v8.inspector to the list of categories
    A recent v8 change added v8.inspector category [1] , which caused build failures when building with Perfetto [2] as Perfetto-based TRACE_EVENT macros have stricter compile-time checks.
    
    Add v8.inspector to the list of categories to fix this.
    
    [1] https://chromium-review.googlesource.com/c/v8/v8/+/3364085
    [2] https://ci.chromium.org/ui/p/chromium/builders/try/linux-perfetto-rel/5926/overview
    
    R=primiano@chromium.org
    
    Change-Id: I94b8802d262a54b538f775482e935aea4753e336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3360674
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955796}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -249,6 +249,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("v8.gc"))                                  \
   X(TRACE_DISABLED_BY_DEFAULT("v8.gc_stats"))                            \
   X(TRACE_DISABLED_BY_DEFAULT("v8.ic_stats"))                            \
+  X(TRACE_DISABLED_BY_DEFAULT("v8.inspector"))                           \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime"))                             \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats_sampling"))              \
