49a3c579c24d4533ab4c68e637e79ba1f92f1c18
Roland Bock <rbock@google.com>
2022-01-05 02:16:14 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=kcwu@chromium.org
    
    Bug: 1283907
    Change-Id: I91c3c53cb892f667bdc7e8c0f36f5fd7b51ec591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366394
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955521}

--- media/gpu/vaapi/vaapi_picture_factory.cc
+++ media/gpu/vaapi/vaapi_picture_factory.cc
@@ -146,7 +146,7 @@ void VaapiPictureFactory::DeterminePictureCreationAndDownloadingMechanism() {
       break;
 #else
       // ozone or egl must be used to use the DRM implementation.
-      FALLTHROUGH;
+      [[fallthrough]];
 #endif
     default:
       NOTREACHED();
