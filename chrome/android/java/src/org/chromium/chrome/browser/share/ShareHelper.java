bcd20461d1df6fabb7c2da45af253a9a87f6a738
Elly Fong-Jones <ellyjones@chromium.org>
2022-01-05 18:43:58 +0000

    ShareHub: move direct share logging
    Logging in shareDirectly(), rather than in shareWithLastUsedComponent(),
    causes us to log 3P app shares as both a 3P share *and* a direct share,
    since sharing to a 3P app goes through shareDirectly as well. Oops!
    
    Fixed: 1277240
    Change-Id: Iaf3f58e0bcf410e70c6d2686e0aaa308a01bd0c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340053
    Reviewed-by: Travis Skare <skare@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955797}

--- chrome/android/java/src/org/chromium/chrome/browser/share/ShareHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/share/ShareHelper.java
@@ -61,7 +61,6 @@ public class ShareHelper extends org.chromium.components.browser_ui.share.ShareH
      */
     public static void shareDirectly(
             @NonNull ShareParams params, @NonNull ComponentName component) {
-        recordShareSource(ShareSourceAndroid.DIRECT_SHARE);
         Intent intent = getShareLinkIntent(params);
 
         intent.addFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT | Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
@@ -80,6 +79,7 @@ public class ShareHelper extends org.chromium.components.browser_ui.share.ShareH
         ComponentName component = getLastShareComponentName();
         if (component == null) return;
         assert params.getCallback() == null;
+        recordShareSource(ShareSourceAndroid.DIRECT_SHARE);
         shareDirectly(params, component);
     }
 
