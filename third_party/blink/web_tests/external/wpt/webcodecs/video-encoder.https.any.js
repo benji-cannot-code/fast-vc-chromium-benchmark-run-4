eb3948ba51f9b3592abc1b5fd065e07b6d4b995f
Eugene Zemtsov <eugene@chromium.org>
2022-01-05 01:56:54 +0000

    webcodecs: Move encoding web test to the external section
    Also split by codec configuration to prevent timeouts and improve clarity.
    
    This CL doesn't remove old tests from the internal section, it
    will be done in a follow up CL.
    
    Bug: 1276335
    Change-Id: I73aaec2e0bc7048fda185b467988120cc89127b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3354313
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955512}

--- third_party/blink/web_tests/external/wpt/webcodecs/video-encoder.https.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/video-encoder.https.any.js
@@ -2,33 +2,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window,dedicatedworker
 // META: script=/common/media.js
 // META: script=/webcodecs/utils.js
+// META: script=/webcodecs/video-encoder-utils.js
 
 const defaultConfig = {
   codec: 'vp8',
   width: 640,
   height: 480
 };
-
-let bitmap_blob = null;
-
-async function generateBitmap(width, height) {
-  if (!bitmap_blob) {
-    let response = await fetch("pattern.png");
-    bitmap_blob = await response.blob();
-  }
-
-  var size = {
-    resizeWidth: width,
-    resizeHeight: height
-  };
-
-  return createImageBitmap(bitmap_blob, size);
-}
-
-async function createVideoFrame(width, height, timestamp) {
-  let bitmap = await generateBitmap(width, height);
-  return new VideoFrame(bitmap, { timestamp: timestamp });
-}
 
 promise_test(t => {
   // VideoEncoderInit lacks required fields.
@@ -82,8 +62,8 @@ promise_test(async t => {
   let encoder = new VideoEncoder(codecInit);
   encoder.configure(encoderConfig);
 
-  let frame1 = await createVideoFrame(640, 480, 0);
-  let frame2 = await createVideoFrame(640, 480, 33333);
+  let frame1 = createFrame(640, 480, 0);
+  let frame2 = createFrame(640, 480, 33333);
 
   encoder.encode(frame1);
   encoder.encode(frame2);
@@ -130,17 +110,15 @@ promise_test(async t => {
   const frames_count = 100;
   let frames = [];
   for (let i = 0; i < frames_count; i++) {
-    let frame = await createVideoFrame(320, 200, i * 16000);
+    let frame = createFrame(320, 200, i * 16000);
     frames.push(frame);
   }
 
   for (let frame of frames)
     encoder.encode(frame);
 
-  // Some encodes should have already started being processed, but not all
-  // 100 of them.
   assert_greater_than(encoder.encodeQueueSize, 0);
-  assert_less_than(encoder.encodeQueueSize, frames_count);
+  assert_less_than_equal(encoder.encodeQueueSize, frames_count);
 
   await encoder.flush();
   // We can guarantee that all encodes are processed after a flush.
@@ -164,7 +142,7 @@ promise_test(async t => {
   const timestamp_step = 40000;
   const expected_callbacks_before_reset = 3;
   let codecInit = getDefaultCodecInit(t);
-  let bitmap = await generateBitmap(320, 200);
+  let original = createFrame(320, 200, 0);
   let encoder = null;
   let reset_completed = false;
   codecInit.output = (chunk, metadata) => {
@@ -189,7 +167,7 @@ promise_test(async t => {
   // and make sure no more chunks are emitted afterwards.
   let encodes_before_reset = expected_callbacks_before_reset * 10;
   for (let i = 0; i < encodes_before_reset; i++) {
-    let frame = new VideoFrame(bitmap, { timestamp: timestamp });
+    let frame = new VideoFrame(original, { timestamp: timestamp });
     timestamp += timestamp_step;
     encoder.encode(frame);
     frame.close();
@@ -209,7 +187,7 @@ promise_test(async t => {
 
   const frames_after_reset = 5;
   for (let i = 0; i < frames_after_reset; i++) {
-    let frame = await createVideoFrame(800, 600, timestamp + 1);
+    let frame = createFrame(800, 600, timestamp + 1);
     timestamp += timestamp_step;
     encoder.encode(frame);
     frame.close();
@@ -237,8 +215,8 @@ promise_test(async t => {
 
   encoder.configure(config);
 
-  let frame1 = await createVideoFrame(640, 480, 0);
-  let frame2 = await createVideoFrame(640, 480, 33333);
+  let frame1 = createFrame(640, 480, 0);
+  let frame2 = createFrame(640, 480, 33333);
 
   encoder.encode(frame1);
   encoder.configure(config);
@@ -248,16 +226,16 @@ promise_test(async t => {
   await encoder.flush();
 
   // We can guarantee that all encodes are processed after a flush.
-  assert_equals(encoder.encodeQueueSize, 0);
+  assert_equals(encoder.encodeQueueSize, 0, "queue size after encode");
 
-  assert_true(output_chunks.length == 2);
+  assert_equals(output_chunks.length, 2, "number of chunks");
   assert_equals(output_chunks[0].timestamp, frame1.timestamp);
   assert_equals(output_chunks[1].timestamp, frame2.timestamp);
 
   output_chunks = [];
 
-  let frame3 = await createVideoFrame(640, 480, 66666);
-  let frame4 = await createVideoFrame(640, 480, 100000);
+  let frame3 = createFrame(640, 480, 66666);
+  let frame4 = createFrame(640, 480, 100000);
 
   encoder.encode(frame3);
 
@@ -277,7 +255,7 @@ promise_test(async t => {
 promise_test(async t => {
   let encoder = new VideoEncoder(getDefaultCodecInit(t));
 
-  let frame = await createVideoFrame(640, 480, 0);
+  let frame = createFrame(640, 480, 0);
 
   return testClosedCodec(t, encoder, defaultConfig, frame);
 }, 'Verify closed VideoEncoder operations');
@@ -285,7 +263,7 @@ promise_test(async t => {
 promise_test(async t => {
   let encoder = new VideoEncoder(getDefaultCodecInit(t));
 
-  let frame = await createVideoFrame(640, 480, 0);
+  let frame = createFrame(640, 480, 0);
 
   return testUnconfiguredCodec(t, encoder, frame);
 }, 'Verify unconfigured VideoEncoder operations');
@@ -293,7 +271,7 @@ promise_test(async t => {
 promise_test(async t => {
   let encoder = new VideoEncoder(getDefaultCodecInit(t));
 
-  let frame = await createVideoFrame(640, 480, 0);
+  let frame = createFrame(640, 480, 0);
   frame.close();
 
   encoder.configure(defaultConfig);
@@ -312,7 +290,7 @@ promise_test(async t => {
   let config = defaultConfig;
   encoder.configure(config);
 
-  let frame = await createVideoFrame(640, 480, -10000);
+  let frame = createFrame(640, 480, -10000);
   encoder.encode(frame);
   frame.close();
   await encoder.flush();
