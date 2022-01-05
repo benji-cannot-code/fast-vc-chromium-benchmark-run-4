5e845fd9f68db6ac52df5b635c8b351efd4ef14f
Eriko Kurimoto <elkurin@chromium.org>
2022-01-05 07:05:25 +0000

    [ArcMigration: Lacros to ARC] Call Callback when crosapi connection fails
    This CL lets RequestActivityIcons and RequestUrlHandlerList call
    callback when the connection fails to avoid dropping response.
    
    Bug: 1268687
    Change-Id: I145af2c8c97ccc30281e173849726eef21617874
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344466
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955577}

--- chrome/browser/lacros/arc/arc_icon_cache.h
+++ chrome/browser/lacros/arc/arc_icon_cache.h
@@ -51,7 +51,8 @@ class ArcIconCache : public arc::LinkHandlerModelDelegate,
   // ArcIconCacheDelegate::IntentHandlerInfo.
   void OnRequestUrlHandlerList(
       RequestUrlHandlerListCallback callback,
-      std::vector<crosapi::mojom::IntentHandlerInfoPtr> handlers);
+      std::vector<crosapi::mojom::IntentHandlerInfoPtr> handlers,
+      crosapi::mojom::RequestUrlHandlerListStatus status);
 
   // Cached activity icons.
   ActivityIconLoader icon_loader_;
