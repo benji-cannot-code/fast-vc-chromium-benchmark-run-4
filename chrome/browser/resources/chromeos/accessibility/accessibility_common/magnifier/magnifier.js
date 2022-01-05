8b059579fee857c98cbb5a5cca2c2f9e8e28f120
Josiah K <josiahk@google.com>
2022-01-05 09:35:09 +0000

    [Magnifier] Fix Magnifier viewport panning to left edge on page load
    Magnifier now ignores focus events on root nodes, web views, and
    embedded objects (e.g. PDFs), fixing Magnifier panning to new page edge.
    
    AX-Relnotes: Magnifier should behave more smoothly when loading new webpages and pdfs.
    Test: Manual testing with search results page and a pdf.
    Fixed: 1280823
    Change-Id: I2618ab3856913bb169556fe817f60a233770e964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344308
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#955626}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier.js
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+const EventType = chrome.automation.EventType;
+const RoleType = chrome.automation.RoleType;
+
 /**
  * Main class for the Chrome OS magnifier.
  */
@@ -37,22 +40,21 @@ export class Magnifier {
 
     /** @private {!EventHandler} */
     this.focusHandler_ = new EventHandler(
-        [], chrome.automation.EventType.FOCUS,
-        event => this.onFocusOrSelectionChanged_(event));
+        [], EventType.FOCUS, event => this.onFocusOrSelectionChanged_(event));
 
     /** @private {!EventHandler} */
     this.activeDescendantHandler_ = new EventHandler(
-        [], chrome.automation.EventType.ACTIVE_DESCENDANT_CHANGED,
+        [], EventType.ACTIVE_DESCENDANT_CHANGED,
         event => this.onActiveDescendantChanged_(event));
 
     /** @private {!EventHandler} */
     this.selectionHandler_ = new EventHandler(
-        [], chrome.automation.EventType.SELECTION,
+        [], EventType.SELECTION,
         event => this.onFocusOrSelectionChanged_(event));
 
     /** @private {!EventHandler} */
     this.onCaretBoundsChangedHandler = new EventHandler(
-        [], chrome.automation.EventType.CARET_BOUNDS_CHANGED,
+        [], EventType.CARET_BOUNDS_CHANGED,
         event => this.onCaretBoundsChanged(event));
 
     /** @private {!ChromeEventHandler} */
@@ -179,12 +181,19 @@ export class Magnifier {
    * @private
    */
   onFocusOrSelectionChanged_(event) {
-    const {location} = event.target;
-    if (!location || !this.shouldFollowFocus()) {
+    const node = event.target;
+    if (!node.location || !this.shouldFollowFocus()) {
       return;
     }
 
-    chrome.accessibilityPrivate.moveMagnifierToRect(location);
+    // Skip trying to move magnifier to encompass whole webpage or pdf. It's too
+    // big, and magnifier usually ends up in middle at left edge of page.
+    if (node.isRootNode || node.role === RoleType.WEB_VIEW ||
+        node.role === RoleType.EMBEDDED_OBJECT) {
+      return;
+    }
+
+    chrome.accessibilityPrivate.moveMagnifierToRect(node.location);
   }
 
   /**
