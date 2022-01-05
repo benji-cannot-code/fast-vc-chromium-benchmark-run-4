ea402f6e41271727465de868d22198e875317fa0
Roland Bock <rbock@google.com>
2022-01-05 08:16:56 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=sammc@chromium.org
    
    Bug: 1283907
    Change-Id: I657fb12015ffad70e83eeb6fba51851d51c56e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366403
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#955605}

--- chromeos/services/device_sync/public/mojom/device_sync_mojom_traits.cc
+++ chromeos/services/device_sync/public/mojom/device_sync_mojom_traits.cc
@@ -95,9 +95,9 @@ chromeos::device_sync::mojom::CryptAuthService EnumTraits<
     case cryptauthv2::TargetService::DEVICE_SYNC:
       return chromeos::device_sync::mojom::CryptAuthService::kDeviceSync;
     case cryptauthv2::TargetService::TARGET_SERVICE_UNSPECIFIED:
-      FALLTHROUGH;
+      [[fallthrough]];
     case cryptauthv2::TargetService::TargetService_INT_MIN_SENTINEL_DO_NOT_USE_:
-      FALLTHROUGH;
+      [[fallthrough]];
     case cryptauthv2::TargetService::TargetService_INT_MAX_SENTINEL_DO_NOT_USE_:
       NOTREACHED();
       return chromeos::device_sync::mojom::CryptAuthService::kDeviceSync;
