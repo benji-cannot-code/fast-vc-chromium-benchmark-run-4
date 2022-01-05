62bf04bda4f1d3668a2fa3e42d6d8b16309a031e
Darren W <djw@chromium.org>
2022-01-05 08:19:05 +0000

    More thoroughly test the UserInitiated metric for PageLoad metrics.
    The new tests verify that browser-initiated navigations are always
    considered user initiated, but renderer-initiated may or may not be
    (depending on the navigation).
    Also checks the UserPerceivedPageVisit version of the metric.
    
    Change-Id: I8a004aeff12ee29ecf62809fa2a50afbee07ee8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347188
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Darren Willis <djw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955609}

--- content/test/navigation_simulator_impl.cc
+++ content/test/navigation_simulator_impl.cc
@@ -950,6 +950,11 @@ void NavigationSimulatorImpl::SetHasUserGesture(bool has_user_gesture) {
   has_user_gesture_ = has_user_gesture;
 }
 
+void NavigationSimulatorImpl::SetNavigationInputStart(
+    base::TimeTicks navigation_input_start) {
+  navigation_input_start_ = navigation_input_start;
+}
+
 void NavigationSimulatorImpl::SetReloadType(ReloadType reload_type) {
   CHECK_EQ(INITIALIZATION, state_) << "The reload_type parameter cannot "
                                       "be set after the navigation has started";
@@ -1295,6 +1300,7 @@ bool NavigationSimulatorImpl::SimulateRendererInitiatedStart() {
           absl::nullopt /* web_bundle_token */);
   auto common_params = blink::CreateCommonNavigationParams();
   common_params->navigation_start = base::TimeTicks::Now();
+  common_params->input_start = navigation_input_start_;
   common_params->url = navigation_url_;
   common_params->initiator_origin = initiator_origin_.value();
   common_params->method = initial_method_;
