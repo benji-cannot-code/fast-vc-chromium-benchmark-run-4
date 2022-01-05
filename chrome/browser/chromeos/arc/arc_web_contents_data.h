3492c4574ec7217f63c23bdd05da4bb91a2649e8
Eriko Kurimoto <elkurin@chromium.org>
2022-01-05 03:55:06 +0000

    Move arc_web_contents_data to //chrome/browser/chromeos
    This CL movew arc_web_contents_data.{cc, h} to //chrome/browser/chromeos
    from //chrome/browser/ash.
    
    By this change, Lacros can use arc::ArcWebContentsData while it cannot
    previously because //chrome/browsser/ash is not visible from Lacros.
    This is a preparation for ARC external dialog protocol implementation
    in Lacros which requires arc_web_contents_data.
    
    Change-Id: I76a7c180e601c4ce7a3d445839ee2c18152b30ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3353575
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955537}

--- chrome/browser/chromeos/arc/arc_web_contents_data.h
+++ chrome/browser/chromeos/arc/arc_web_contents_data.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_ARC_ARC_WEB_CONTENTS_DATA_H_
-#define CHROME_BROWSER_ASH_ARC_ARC_WEB_CONTENTS_DATA_H_
+#ifndef CHROME_BROWSER_CHROMEOS_ARC_ARC_WEB_CONTENTS_DATA_H_
+#define CHROME_BROWSER_CHROMEOS_ARC_ARC_WEB_CONTENTS_DATA_H_
 
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -31,4 +31,4 @@ class ArcWebContentsData
 
 }  // namespace arc
 
-#endif  // CHROME_BROWSER_ASH_ARC_ARC_WEB_CONTENTS_DATA_H_
+#endif  // CHROME_BROWSER_CHROMEOS_ARC_ARC_WEB_CONTENTS_DATA_H_
