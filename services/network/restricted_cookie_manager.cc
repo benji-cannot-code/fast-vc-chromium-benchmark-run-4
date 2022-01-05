b915337e1a1b3e8dfd420295553ebdb7ab0c3942
Roland Bock <rbock@google.com>
2022-01-05 21:12:18 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=jam@chromium.org
    
    Bug: 1283907
    Change-Id: Idae5295ce82a93abca558b0efa595ab7846e19c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3365532
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955870}

--- services/network/restricted_cookie_manager.cc
+++ services/network/restricted_cookie_manager.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
-#include "base/compiler_specific.h"  // for FALLTHROUGH;
+#include "base/compiler_specific.h"  // for [[fallthrough]];
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
