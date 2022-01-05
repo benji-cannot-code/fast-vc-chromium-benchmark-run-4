5208463784396f20516ffcc4b42625ab213386a3
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-01-05 01:38:27 +0000

    [Extensions] Move i18n API to //extensions
    Move the i18n API to //extensions, rather than //chrome. This includes
    the following:
    * API schema file (i18n.json)
    * API features file entries (_api_features.json)
    * Renderer hooks unittest (i18n_hooks_delegate_unittest.cc; note that
      the hooks themselves - i18n_hooks_delegate.* - were already moved
      to //extensions).
    * API implementation files (i18n_api.*)
    The associated API tests (i18n_apitest.cc) are deliberately not moved,
    because API tests are best run in a "real" chrome browser environment
    to ensure end-to-end correctness.
    
    The only change needed was to remove a dependency on `Profile` in the
    API implementation files; the profile was used to get the PrefService,
    which can instead be retrieved via the ExtensionsBrowserClient and the
    BrowserContext.
    
    This has a number of advantages:
    - Moves an API that is conceptually independent of the chrome browser
      to the //extensions layer, which is where it more appropriately
      belongs.
    - Removes the need to copy-paste the i18n-related files in the
      //chromecast layer, since //chromecast already fully depends on the
      //extensions layer.
    - As a result, actually implements i18n.getAcceptLanguages() for
      chromecast; previously, it was stubbed out.
    
    Bug: None
    Change-Id: I6f3e901909688f517309217d40ed3884591676b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3362491
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955501}

--- extensions/browser/api/i18n/i18n_api.h
+++ extensions/browser/api/i18n/i18n_api.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMECAST_BROWSER_EXTENSIONS_API_I18N_I18N_API_H_
-#define CHROMECAST_BROWSER_EXTENSIONS_API_I18N_I18N_API_H_
+#ifndef EXTENSIONS_BROWSER_API_I18N_I18N_API_H_
+#define EXTENSIONS_BROWSER_API_I18N_I18N_API_H_
 
 #include "extensions/browser/extension_function.h"
 
@@ -18,4 +18,4 @@ class I18nGetAcceptLanguagesFunction : public ExtensionFunction {
 
 }  // namespace extensions
 
-#endif  // CHROMECAST_BROWSER_EXTENSIONS_API_I18N_I18N_API_H_
+#endif  // EXTENSIONS_BROWSER_API_I18N_I18N_API_H_
