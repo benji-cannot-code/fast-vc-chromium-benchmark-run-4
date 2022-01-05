89d4ba694b8b9896ddd125815669b24e76d72927
Roland Bock <rbock@google.com>
2022-01-05 04:04:13 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=hiroh@chromium.org
    
    Bug: 1283907
    Change-Id: I89a556a2ede9be80d23cde519cbd2f7cf46425d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366424
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955552}

--- media/gpu/chromeos/fourcc.cc
+++ media/gpu/chromeos/fourcc.cc
@@ -83,7 +83,7 @@ absl::optional<Fourcc> Fourcc::FromVideoPixelFormat(
         return Fourcc(P010);
       case PIXEL_FORMAT_UYVY:
         NOTREACHED();
-        FALLTHROUGH;
+        [[fallthrough]];
       case PIXEL_FORMAT_I420A:
       case PIXEL_FORMAT_I444:
       case PIXEL_FORMAT_RGB24:
@@ -118,7 +118,7 @@ absl::optional<Fourcc> Fourcc::FromVideoPixelFormat(
         return Fourcc(NM21);
       case PIXEL_FORMAT_UYVY:
         NOTREACHED();
-        FALLTHROUGH;
+        [[fallthrough]];
       case PIXEL_FORMAT_I420A:
       case PIXEL_FORMAT_I444:
       case PIXEL_FORMAT_YUY2:
