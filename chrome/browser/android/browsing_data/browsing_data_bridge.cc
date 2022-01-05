9542a6f327f784197c780371c04d7045a1880c99
Roland Bock <rbock@google.com>
2022-01-05 07:50:39 +0000

    LSC: Replace FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    This CL was uploaded by git cl split.
    
    R=msramek@chromium.org
    
    Bug: 1283907
    Change-Id: I3e68a1c01352d0c776716a5689b8ea1fff426943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3363391
    Auto-Submit: Roland Bock <rbock@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#955595}

--- chrome/browser/android/browsing_data/browsing_data_bridge.cc
+++ chrome/browser/android/browsing_data/browsing_data_bridge.cc
@@ -129,7 +129,7 @@ static void JNI_BrowsingDataBridge_ClearBrowsingData(
       case browsing_data::BrowsingDataType::HOSTED_APPS_DATA:
         // Only implemented on Desktop.
         NOTREACHED();
-        FALLTHROUGH;
+        [[fallthrough]];
       case browsing_data::BrowsingDataType::NUM_TYPES:
         NOTREACHED();
     }
