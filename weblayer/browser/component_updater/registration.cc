83aa422b90e9a9b11c4daa1700c5f88701c8863b
Hazem Ashmawy <hazems@chromium.org>
2022-01-05 16:09:47 +0000

    WebLayer: Add client side phishing component
    Register the phishing component in the shared downloading service
    between WebView and WebLayer. It always uses the installer attribute
    "tag"="default" because Finch isn't initialized in the process that
    hosts the download service.
    
    Add a component loader policy to load the phishing component and
    register it in WebLayer, this is guarded by the off by default
    kWebLayerClientSidePhishingDetection feature flag.
    
    Add the component suffix to the histogram suffixes list, the status of
    loading the histogram can be then viewed in UMA dashboard by looking at
    ComponentUpdater.AndroidComponentLoader.LoadStatus.ClientSidePhishing
    histogram.
    
    Bug: 1115700
    Test: add logs to the component loader policy and run_weblayer_shell_trichrome_google
    Test: Open WebView DevTools app and make sure the new component shows up
    Change-Id: I726f982a874e14aeefae18160e58a477df2dc202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3295381
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955731}

--- weblayer/browser/component_updater/registration.cc
+++ weblayer/browser/component_updater/registration.cc
@@ -5,11 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "weblayer/browser/component_updater/registration.h"
 
+#include "weblayer/browser/component_updater/client_side_phishing_component_loader_policy.h"
+
 namespace weblayer {
 
 component_updater::ComponentLoaderPolicyVector GetComponentLoaderPolicies() {
+  component_updater::ComponentLoaderPolicyVector policies;
+
+  LoadClientSidePhishingComponent(policies);
   // TODO(crbug.com/1233490) register AutoFillRegex component loader policy.
-  return component_updater::ComponentLoaderPolicyVector();
+
+  return policies;
 }
 
 }  // namespace weblayer
