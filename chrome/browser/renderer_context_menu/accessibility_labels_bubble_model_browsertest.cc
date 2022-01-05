818eb8698c8691be29eec8a28fe335ad4652fb73
Sharon Yang <yangsharon@chromium.org>
2022-01-05 21:36:58 +0000

    [renderer context menu] Remove WebContents::GetURL calls in //chrome
    WebContents::GetURL is deprecated. Replace it with
    WebContents::GetLastCommittedURL.
    
    Bug: 237908
    Change-Id: Ic7f426bbcc1e7ae0932f0983d90b9752546d1b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355471
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955883}

--- chrome/browser/renderer_context_menu/accessibility_labels_bubble_model_browsertest.cc
+++ chrome/browser/renderer_context_menu/accessibility_labels_bubble_model_browsertest.cc
@@ -68,7 +68,7 @@ IN_PROC_BROWSER_TEST_F(AccessibilityLabelsBubbleModelTest, OpenHelpPage) {
   model->OpenHelpPage();
   content::WebContents* web_contents = waiter.Wait();
   EXPECT_EQ(web_contents->GetBrowserContext(), browser()->profile());
-  EXPECT_EQ(web_contents->GetURL(), model->GetHelpPageURL());
+  EXPECT_EQ(web_contents->GetVisibleURL(), model->GetHelpPageURL());
 }
 
 // Tests that closing the tab with WebContents that was used to construct
@@ -90,5 +90,5 @@ IN_PROC_BROWSER_TEST_F(AccessibilityLabelsBubbleModelTest,
   model->OpenHelpPage();
   content::WebContents* web_contents = waiter.Wait();
   EXPECT_EQ(web_contents->GetBrowserContext(), browser()->profile());
-  EXPECT_EQ(web_contents->GetURL(), model->GetHelpPageURL());
+  EXPECT_EQ(web_contents->GetVisibleURL(), model->GetHelpPageURL());
 }
--- chrome/browser/renderer_context_menu/spelling_bubble_model_browsertest.cc
+++ chrome/browser/renderer_context_menu/spelling_bubble_model_browsertest.cc
@@ -69,7 +69,7 @@ IN_PROC_BROWSER_TEST_F(SpellingBubbleModelTest, OpenHelpPage) {
   model->OpenHelpPage();
   content::WebContents* web_contents = waiter.Wait();
   EXPECT_EQ(web_contents->GetBrowserContext(), browser()->profile());
-  EXPECT_EQ(web_contents->GetURL(), model->GetHelpPageURL());
+  EXPECT_EQ(web_contents->GetVisibleURL(), model->GetHelpPageURL());
 }
 
 // Tests that closing the tab with WebContents that was used to construct the
@@ -90,5 +90,5 @@ IN_PROC_BROWSER_TEST_F(SpellingBubbleModelTest,
   model->OpenHelpPage();
   content::WebContents* web_contents = waiter.Wait();
   EXPECT_EQ(web_contents->GetBrowserContext(), browser()->profile());
-  EXPECT_EQ(web_contents->GetURL(), model->GetHelpPageURL());
+  EXPECT_EQ(web_contents->GetVisibleURL(), model->GetHelpPageURL());
 }
