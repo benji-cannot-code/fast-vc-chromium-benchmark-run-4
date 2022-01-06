9640f85fa809b3cfbe49922d6f6cd6ea332762a4
Nancy Wang <nancylingwang@chromium.org>
2022-01-06 00:21:00 +0000

    Modify full restore to restore Lacros.
    This CL modifies full restore to add MaybeRestoreLacros to restore
    lacros browser if there is a lacros window before reboot or login.
    
    The FullRestoreForLacros flag is disabled as the default setting, so
    this CL doesn't change any current behavior.
    
    If The FullRestoreForLacros flag is enabled,
    FullRestoreAppLaunchHandler checks whether there is any saved Lacros
    windows, and restore Lacros windows.
    
    Similar with the Kiosk mode, the initial browser action is modified to
    kDoNotOpenWindow when full restore supports Lacros, because OS/the
    ash side will check the full restore data to decide whether launch
    browser window or not.
    
    Modify startup_browser_creator for Lacros, to suppress/not launch
    browser window when the InitialBrowserAction is kDoNotOpenWindow,
    because full restore will decide whether to new browser window later
    based on the full restore data.
    
    Tests will be added later in a separate follow up CL.
    
    BUG=1239984
    
    Change-Id: I4dbfc2513e311b8a3b39c97a485bdc8c349c4756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3358829
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955962}

--- chrome/browser/ash/app_restore/full_restore_app_launch_handler.h
+++ chrome/browser/ash/app_restore/full_restore_app_launch_handler.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/app_restore/app_launch_handler.h"
+#include "chrome/browser/ash/crosapi/browser_manager.h"
+#include "chrome/browser/ash/crosapi/browser_manager_observer.h"
 #include "chrome/browser/sessions/session_restore_observer.h"
 #include "components/app_restore/restore_data.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
@@ -71,7 +73,8 @@ enum class SessionRestoreWindowCount {
 // the notification dialog.
 // 3. The app is ready.
 class FullRestoreAppLaunchHandler : public AppLaunchHandler,
-                                    public SessionRestoreObserver {
+                                    public SessionRestoreObserver,
+                                    public crosapi::BrowserManagerObserver {
  public:
   explicit FullRestoreAppLaunchHandler(Profile* profile,
                                        bool should_init_service = false);
@@ -99,6 +102,9 @@ class FullRestoreAppLaunchHandler : public AppLaunchHandler,
 
   // SessionRestoreObserver:
   void OnGotSession(Profile* profile, bool for_apps, int window_count) override;
+
+  // crosapi::BrowserManagerObserver:
+  void OnStateChanged() override;
 
   // Force launch browser for testing.
   void ForceLaunchBrowserForTesting();
@@ -131,6 +137,8 @@ class FullRestoreAppLaunchHandler : public AppLaunchHandler,
   // browsers when upgrading to the full restore version.
   void LaunchBrowserForFirstRunFullRestore();
 
+  void MaybeRestoreLacros();
+
   // AppLaunchHandler:
   void RecordRestoredAppLaunch(apps::AppTypeName app_type_name) override;
 
@@ -157,6 +165,10 @@ class FullRestoreAppLaunchHandler : public AppLaunchHandler,
   // Restored browser window count. This is used for debug only.
   int browser_app_window_count_ = 0;
   int browser_window_count_ = 0;
+
+  base::ScopedObservation<crosapi::BrowserManager,
+                          crosapi::BrowserManagerObserver>
+      observation_{this};
 
   base::WeakPtrFactory<FullRestoreAppLaunchHandler> weak_ptr_factory_{this};
 };
--- chrome/browser/ash/app_restore/full_restore_service.cc
+++ chrome/browser/ash/app_restore/full_restore_service.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/account_id/account_id.h"
+#include "components/app_restore/features.h"
 #include "components/app_restore/full_restore_info.h"
 #include "components/app_restore/full_restore_save_handler.h"
 #include "components/app_restore/full_restore_utils.h"
@@ -49,6 +50,18 @@ const char kRestoreForCrashNotificationHistogramName[] =
     "Apps.RestoreForCrashNotification";
 const char kRestoreSettingHistogramName[] = "Apps.RestoreSetting";
 const char kRestoreInitSettingHistogramName[] = "Apps.RestoreInitSetting";
+
+bool IsFullRestoreAvailableForLacros() {
+  if (!::full_restore::features::IsFullRestoreForLacrosEnabled())
+    return false;
+
+  const user_manager::User* user =
+      user_manager::UserManager::Get()->GetPrimaryUser();
+  DCHECK(user);
+  Profile* profile = chromeos::ProfileHelper::Get()->GetProfileByUser(user);
+  DCHECK(profile);
+  return FullRestoreServiceFactory::IsFullRestoreAvailableForProfile(profile);
+}
 
 // static
 FullRestoreService* FullRestoreService::GetForProfile(Profile* profile) {
