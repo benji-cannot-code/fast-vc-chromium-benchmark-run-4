5eb9de143b1972631d4a5fd557317012b7670e95
Andrew Grieve <agrieve@chromium.org>
2022-01-06 22:19:17 +0000

    SuperSize: Minor simplification to not pass around container_list
    No behavior change.
    
    Change-Id: I8a1a6bdb267dd4fb83376a92aebad93ea2b02509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3370080
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956261}

--- tools/binary_size/libsupersize/archive.py
+++ tools/binary_size/libsupersize/archive.py
@@ -1193,16 +1193,16 @@ def _ParseNinjaFiles(output_directory, elf_path=None):
   return source_mapper, ninja_elf_object_paths
 
 
-def CreateContainerAndSymbols(*,
-                              container_name,
-                              metadata,
-                              apk_spec,
-                              pak_spec,
-                              native_spec,
-                              source_directory,
-                              output_directory=None,
-                              resources_pathmap_path=None,
-                              pak_id_map=None):
+def CreateContainerSymbols(*,
+                           container_name,
+                           metadata,
+                           apk_spec,
+                           pak_spec,
+                           native_spec,
+                           source_directory,
+                           output_directory=None,
+                           resources_pathmap_path=None,
+                           pak_id_map=None):
   """Creates a Container (with sections sizes) and symbols for a SizeInfo.
 
   Args:
@@ -1220,10 +1220,7 @@ def CreateContainerAndSymbols(*,
     pak_id_map: Instance of PakIdMap, or None.
 
   Returns:
-    A tuple of (container, raw_symbols).
-    containers is a Container instance that stores metadata and section_sizes
-    (section_sizes maps section names to respective sizes).
-    raw_symbols is a list of Symbol objects.
+    List of symbols.
   """
   apk_elf_result = None
   if apk_spec and native_spec and native_spec.apk_so_path:
@@ -1411,17 +1408,13 @@ def CreateContainerAndSymbols(*,
     symbol.container = container
 
   file_format.SortSymbols(raw_symbols, check_already_mostly_sorted=True)
-
-  return container, raw_symbols
+  return raw_symbols
 
 
 def CreateSizeInfo(build_config,
-                   container_list,
                    raw_symbols_list,
                    normalize_names=True):
   """Performs operations on all symbols and creates a SizeInfo object."""
-  assert len(container_list) == len(raw_symbols_list)
-
   all_raw_symbols = []
   for raw_symbols in raw_symbols_list:
     file_format.CalculatePadding(raw_symbols)
@@ -1434,6 +1427,8 @@ def CreateSizeInfo(build_config,
 
     all_raw_symbols += raw_symbols
 
+  # Containers should always have at least one symbol.
+  container_list = [syms[0].container for syms in raw_symbols_list]
   return models.SizeInfo(build_config, container_list, all_raw_symbols)
 
 
@@ -2025,7 +2020,6 @@ def Run(top_args, on_config_error):
 
   build_config = {}
   seen_container_names = set()
-  container_list = []
   raw_symbols_list = []
   pak_id_map = pakfile.PakIdMap()
 
@@ -2045,7 +2039,7 @@ def Run(top_args, on_config_error):
                                 native_spec=native_spec,
                                 source_directory=sub_args.source_directory,
                                 output_directory=sub_args.output_directory)
-      container, raw_symbols = CreateContainerAndSymbols(
+      raw_symbols = CreateContainerSymbols(
           container_name=container_name,
           metadata=metadata,
           apk_spec=apk_spec,
@@ -2055,12 +2049,11 @@ def Run(top_args, on_config_error):
           output_directory=sub_args.output_directory,
           resources_pathmap_path=resources_pathmap_path,
           pak_id_map=pak_id_map)
+      assert raw_symbols, f'Container {container_name} had no symbols.'
 
-      container_list.append(container)
       raw_symbols_list.append(raw_symbols)
 
   size_info = CreateSizeInfo(build_config,
-                             container_list,
                              raw_symbols_list,
                              normalize_names=False)
 
