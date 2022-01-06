1f8a066aad765c9d0035cf259dd9c1ee2b07ba1a
Gordon Seto <gordonseto@google.com>
2022-01-06 23:08:23 +0000

    [CrOS Bluetooth] Add message displayed in HID's detail pages.
    Add message to <os-settings-bluetooth-device-detail-subpage> that is
    displayed when the device is a human-interactive device, informing users
    the device will connect or disconnect automatically.
    
    Screenshots:
    HID connected:
    https://screenshot.googleplex.com/AiPATAzH77j7iA7.png
    HID disconnected:
    https://screenshot.googleplex.com/4CrQqRfKSr66bYU.png
    Non-HID:
    https://screenshot.googleplex.com/6NM4Qs5yvXhc3ZL.png
    
    Fixed: b/209033114
    Change-Id: I3dc8bd6c0c35da060ee572fc33691116f5e1fcec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3370488
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#956279}

--- chrome/test/data/webui/settings/chromeos/os_bluetooth_device_detail_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_bluetooth_device_detail_subpage_tests.js
@@ -281,6 +281,8 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
         bluetoothDeviceDetailPage.$$('#bluetoothDeviceNameLabel');
     const getBluetoothDeviceBatteryInfo = () =>
         bluetoothDeviceDetailPage.$$('#batteryInfo');
+    const getNonAudioOutputDeviceMessage = () =>
+        bluetoothDeviceDetailPage.$$('#nonAudioOutputDeviceMessage');
 
     bluetoothConfig.setBluetoothEnabledState(/*enabled=*/ true);
 
@@ -290,6 +292,7 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
     assertFalse(!!getBluetoothForgetBtn());
     assertFalse(!!getBluetoothStateBtn());
     assertFalse(!!getBluetoothDeviceBatteryInfo());
+    assertFalse(!!getNonAudioOutputDeviceMessage());
 
     const deviceNickname = 'device1';
     const device1 = createDefaultBluetoothDevice(
@@ -327,6 +330,7 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
     assertEquals(
         'os-settings:bluetooth-connected', getBluetoothStatusIcon().icon);
     assertTrue(!!getBluetoothDeviceBatteryInfo());
+    assertFalse(!!getNonAudioOutputDeviceMessage());
 
     // Simulate disconnected state and not audio capable.
     device1.deviceProperties.connectionState =
@@ -349,6 +353,23 @@ suite('OsBluetoothDeviceDetailPageTest', function() {
         getBluetoothForgetBtn().ariaLabel,
         bluetoothDeviceDetailPage.i18n(
             'bluetoothDeviceDetailForgetA11yLabel', deviceNickname));
+    assertTrue(!!getNonAudioOutputDeviceMessage());
+    assertEquals(
+        bluetoothDeviceDetailPage.i18n(
+            'bluetoothDeviceDetailHIDMessageDisconnected'),
+        getNonAudioOutputDeviceMessage().textContent.trim());
+
+    // Simulate connected state and not audio capable.
+    device1.deviceProperties.connectionState =
+        mojom.DeviceConnectionState.kConnected;
+    bluetoothConfig.updatePairedDevice(device1);
+    await flushAsync();
+
+    assertTrue(!!getNonAudioOutputDeviceMessage());
+    assertEquals(
+        bluetoothDeviceDetailPage.i18n(
+            'bluetoothDeviceDetailHIDMessageConnected'),
+        getNonAudioOutputDeviceMessage().textContent.trim());
   });
 
   test(
