e581dc9fdbd03cca6103edd367f7b3bb4083f832
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-01-06 05:32:53 +0000

    Import wpt@40a8ec17f2f2002e555af10311e53a0ee866668f
    Using wpt-import in Chromium 96307ebd5057ba3e5e3d5f2fe5c5f07598d07006.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org, mek@chromium.org:
      external/wpt/FileAPI
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: Ic4fdf847281b328374bea4adcdc29dcceb6d8a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3369061
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#956039}

--- third_party/blink/web_tests/external/wpt/FileAPI/blob/Blob-in-worker.worker.js
+++ third_party/blink/web_tests/external/wpt/FileAPI/blob/Blob-in-worker.worker.js
@@ -1,15 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts("/resources/testharness.js");
 
-async_test(function() {
-  var data = "TEST";
-  var blob = new Blob([data], {type: "text/plain"});
-  var reader = new FileReader();
-  reader.onload = this.step_func_done(function() {
-    assert_equals(reader.result, data);
-  });
-  reader.onerror = this.unreached_func("Unexpected error event");
-  reader.readAsText(blob);
-}, "Create Blob in Worker");
+promise_test(async () => {
+  const data = "TEST";
+  const blob = new Blob([data], {type: "text/plain"});
+  assert_equals(await blob.text(), data);
+}, 'Create Blob in Worker');
 
 done();
