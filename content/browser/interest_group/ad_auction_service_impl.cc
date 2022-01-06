cfc35f487f17db19e0e3a7b8a778b5c4a23bca7b
Caleb Raitto <caraitto@chromium.org>
2022-01-06 22:58:42 +0000

    Add UMA for page auction results.
    These UMA are only concerned with whether or not the auction
    successfully produced a result, and not the contents of the result.
    
    Bug: 1186444
    Change-Id: I36c79a00f5a7415b3782ddf4c5a1184e3bd22e2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3279620
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956276}

--- content/browser/interest_group/ad_auction_service_impl.cc
+++ content/browser/interest_group/ad_auction_service_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/browser/devtools/devtools_instrumentation.h"
 #include "content/browser/fenced_frame/fenced_frame_url_mapping.h"
+#include "content/browser/interest_group/ad_auction_result_metrics.h"
 #include "content/browser/interest_group/auction_runner.h"
 #include "content/browser/interest_group/interest_group_manager.h"
 #include "content/browser/renderer_host/page_impl.h"
@@ -430,10 +431,15 @@ void AdAuctionServiceImpl::OnAuctionComplete(
         base::StrCat({"Worklet error: ", error}));
   }
 
+  auto* auction_result_metrics = AdAuctionResultMetrics::GetOrCreateForPage(
+      render_frame_host()->GetPage());
+
   if (!render_url) {
     DCHECK(!bidder_report_url);
     DCHECK(!seller_report_url);
     std::move(callback).Run(absl::nullopt);
+    auction_result_metrics->ReportAuctionResult(
+        AdAuctionResultMetrics::AuctionResult::kFailed);
     return;
   }
 
@@ -453,6 +459,8 @@ void AdAuctionServiceImpl::OnAuctionComplete(
   DCHECK(render_url->is_valid());
 
   std::move(callback).Run(render_url);
+  auction_result_metrics->ReportAuctionResult(
+      AdAuctionResultMetrics::AuctionResult::kSucceeded);
 
   network::mojom::URLLoaderFactory* factory = GetTrustedURLLoaderFactory();
   if (bidder_report_url) {
