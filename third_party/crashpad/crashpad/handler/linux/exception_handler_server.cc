cac32c5b9dff3b6af30c9ef3a8827b6c2c1fc77a
Roland Bock <rbock@google.com>
2022-01-06 16:56:58 +0000

    FALLTHROUGH macro with C++17 [[fallthrough]] attribute
    See go/chromium-lsc-fallthrough
    
    Bug: 1283907
    Change-Id: Iae3c2a14c22f77a95d1e22c2a1ca76e5cd6ef6ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3369368
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#956140}

--- third_party/crashpad/crashpad/handler/linux/exception_handler_server.cc
+++ third_party/crashpad/crashpad/handler/linux/exception_handler_server.cc
@@ -199,7 +199,7 @@ class PtraceStrategyDeciderImpl : public PtraceStrategyDecider {
         if (HaveCapSysPtrace()) {
           return Strategy::kDirectPtrace;
         }
-        FALLTHROUGH;
+        [[fallthrough]];
       case PtraceScope::kNoAttach:
         LOG(WARNING) << "no ptrace";
         return Strategy::kNoPtrace;
