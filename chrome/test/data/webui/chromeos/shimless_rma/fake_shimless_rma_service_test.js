827e41f187b622d9f70211656d0353a20a4a6d46
Gavin Dodd <gavindodd@google.com>
2022-01-06 16:52:24 +0000

    shimless: Clean up fake_shimless_rma_service_test
    Replace all underscore_variable_names with camelCase
    
    Bug: 1198187
    Test: Run browser tests for Shimless*
    Change-Id: I81ec878bb097baeb4cececb4f9a304e401f291a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3362331
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Gavin Dodd <gavindodd@google.com>
    Cr-Commit-Position: refs/heads/main@{#956139}

--- chrome/test/data/webui/chromeos/shimless_rma/fake_shimless_rma_service_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/fake_shimless_rma_service_test.js
@@ -282,10 +282,10 @@ export function fakeShimlessRmaServiceTestSuite() {
   });
 
   test('SetGetRsuDisableWriteProtectChallengeResultUpdatesResult', () => {
-    let expected_challenge = '9876543210';
-    service.setGetRsuDisableWriteProtectChallengeResult(expected_challenge);
+    let expectedChallenge = '9876543210';
+    service.setGetRsuDisableWriteProtectChallengeResult(expectedChallenge);
     return service.getRsuDisableWriteProtectChallenge().then((challenge) => {
-      assertEquals(challenge.challenge, expected_challenge);
+      assertEquals(challenge.challenge, expectedChallenge);
     });
   });
 
@@ -322,7 +322,7 @@ export function fakeShimlessRmaServiceTestSuite() {
   });
 
   test('SetGetComponentListResultUpdatesResult', () => {
-    let expected_components = [
+    let expectedComponents = [
       {
         component: ComponentType.kKeyboard,
         state: ComponentRepairStatus.kOriginal
@@ -332,9 +332,9 @@ export function fakeShimlessRmaServiceTestSuite() {
         state: ComponentRepairStatus.kMissing
       },
     ];
-    service.setGetComponentListResult(expected_components);
+    service.setGetComponentListResult(expectedComponents);
     return service.getComponentList().then((components) => {
-      assertDeepEquals(components.components, expected_components);
+      assertDeepEquals(components.components, expectedComponents);
     });
   });
 
@@ -494,10 +494,10 @@ export function fakeShimlessRmaServiceTestSuite() {
   });
 
   test('SetGetOriginalSerialNumberResultUpdatesResult', () => {
-    let expected_serial_number = '123456789';
-    service.setGetOriginalSerialNumberResult(expected_serial_number);
-    return service.getOriginalSerialNumber().then((serial_number) => {
-      assertEquals(serial_number.serialNumber, expected_serial_number);
+    let expectedSerialNumber = '123456789';
+    service.setGetOriginalSerialNumberResult(expectedSerialNumber);
+    return service.getOriginalSerialNumber().then((serialNumber) => {
+      assertEquals(serialNumber.serialNumber, expectedSerialNumber);
     });
   });
 
@@ -508,10 +508,10 @@ export function fakeShimlessRmaServiceTestSuite() {
   });
 
   test('SetGetOriginalRegionResultUpdatesResult', () => {
-    let expected_region = 1;
-    service.setGetOriginalRegionResult(expected_region);
+    let expectedRegion = 1;
+    service.setGetOriginalRegionResult(expectedRegion);
     return service.getOriginalRegion().then((region) => {
-      assertEquals(region.regionIndex, expected_region);
+      assertEquals(region.regionIndex, expectedRegion);
     });
   });
 
@@ -522,10 +522,10 @@ export function fakeShimlessRmaServiceTestSuite() {
   });
 
   test('SetGetOriginalSkuResultUpdatesResult', () => {
-    let expected_sku = 1;
-    service.setGetOriginalSkuResult(expected_sku);
+    let expectedSku = 1;
+    service.setGetOriginalSkuResult(expectedSku);
     return service.getOriginalSku().then((sku) => {
-      assertEquals(sku.skuIndex, expected_sku);
+      assertEquals(sku.skuIndex, expectedSku);
     });
   });
 
@@ -935,12 +935,12 @@ export function fakeShimlessRmaServiceTestSuite() {
            * Implements
            * HardwareVerificationStatusObserverRemote.
            *      onHardwareVerificationResult()
-           * @param {boolean} is_compliant
-           * @param {string} error_message
+           * @param {boolean} isCompliant
+           * @param {string} errorMessage
            */
-          onHardwareVerificationResult(is_compliant, error_message) {
-            assertEquals(true, is_compliant);
-            assertEquals('ok', error_message);
+          onHardwareVerificationResult(isCompliant, errorMessage) {
+            assertEquals(true, isCompliant);
+            assertEquals('ok', errorMessage);
           }
         });
     service.observeHardwareVerificationStatus(observer);
