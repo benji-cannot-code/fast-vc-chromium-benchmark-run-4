59e8bad312eaa48bd1b78e40a772df06a12b6219
Ghazale Hosseinabadi <ghazale@google.com>
2022-01-06 23:17:47 +0000

    Pass origin to RegisterNonNetworkSubresourceURLLoaderFactories
    This CL enables RegisterNonNetworkSubresourceURLLoaderFactories
    to retrieve the origin that is going to use the factories.
    
    Bug: 1227761
    Change-Id: I3dfb9f14df8bf60c003efe2ea4281d92696d9501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350608
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956283}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -4904,6 +4904,7 @@ void ChromeContentBrowserClient::
     RegisterNonNetworkSubresourceURLLoaderFactories(
         int render_process_id,
         int render_frame_id,
+        const absl::optional<url::Origin>& request_initiator_origin,
         NonNetworkURLLoaderFactoryMap* factories) {
 #if BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(ENABLE_EXTENSIONS)
   content::RenderFrameHost* frame_host =
