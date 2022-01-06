249d1658f00cea344fcd519f2f55cef3bf253953
Daniel Murphy <dmurph@chromium.org>
2022-01-06 00:29:59 +0000

    Revert "[web apps] Remove WebContents::GetURL calls in //chrome web_apps"
    This reverts commit 3723412682636975b3c7ae9e9e68d587fd8b9218.
    
    Reason for revert: URLs are now missing sometimes in snapshots:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win7%20Tests%20(dbg)(1)/92681/test-results
    
    Bug: 1284803
    
    Original change's description:
    > [web apps] Remove WebContents::GetURL calls in //chrome web_apps
    >
    > WebContents::GetURL is deprecated. Replace it with
    > WebContents::GetLastCommittedURL
    >
    > Bug: 237908
    > Change-Id: I87ae6d2d08526bc06a705c08cf300019d53664f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355458
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#955814}
    
    Bug: 237908
    Change-Id: If81cd5438883efa0531b7b25ef1f735a84c15bcd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3368925
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sharon Yang <yangsharon@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#955966}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.cc
@@ -904,10 +904,7 @@ void WebAppIntegrationTestDriver::CheckAppNavigationIsStartUrl() {
   BeforeStateCheckAction();
   ASSERT_FALSE(active_app_id_.empty());
   ASSERT_TRUE(app_browser());
-  GURL url = app_browser()
-                 ->tab_strip_model()
-                 ->GetActiveWebContents()
-                 ->GetLastCommittedURL();
+  GURL url = app_browser()->tab_strip_model()->GetActiveWebContents()->GetURL();
   EXPECT_EQ(url, provider()->registrar().GetAppStartUrl(active_app_id_));
   AfterStateCheckAction();
 }
@@ -1234,7 +1231,7 @@ WebAppIntegrationTestDriver::ConstructStateSnapshot() {
       for (int i = 0; i < tabs->count(); ++i) {
         content::WebContents* tab = tabs->GetWebContentsAt(i);
         DCHECK(tab);
-        GURL url = tab->GetLastCommittedURL();
+        GURL url = tab->GetURL();
         auto* app_banner_manager =
             webapps::TestAppBannerManagerDesktop::FromWebContents(tab);
         bool installable = app_banner_manager->WaitForInstallableCheck();
@@ -1388,9 +1385,8 @@ bool WebAppIntegrationTestDriver::AreNoAppWindowsOpen(Profile* profile,
     if (browser->IsAttemptingToCloseBrowser()) {
       continue;
     }
-    const GURL& browser_url = browser->tab_strip_model()
-                                  ->GetActiveWebContents()
-                                  ->GetLastCommittedURL();
+    const GURL& browser_url =
+        browser->tab_strip_model()->GetActiveWebContents()->GetURL();
     if (AppBrowserController::IsWebApp(browser) &&
         IsInScope(browser_url, app_scope)) {
       return false;
@@ -1440,7 +1436,7 @@ void WebAppIntegrationTestDriver::MaybeWaitForManifestUpdates() {
 
 void WebAppIntegrationTestDriver::MaybeNavigateTabbedBrowserInScope(
     const std::string& site_mode) {
-  auto browser_url = GetCurrentTab(browser())->GetLastCommittedURL();
+  auto browser_url = GetCurrentTab(browser())->GetURL();
   auto dest_url = GetInScopeURL(site_mode);
   if (browser_url.is_empty() || browser_url != dest_url) {
     NavigateTabbedBrowserToSite(dest_url);
