5ec08915cd622594bdbaa8adfcaf838abb89c481
Rune Lillesveen <futhark@chromium.org>
2022-01-06 17:23:22 +0000

    [@container] Don't establish containers in legacy tree
    Container Queries and legacy layout don't work together. Don't skip
    style recalc or create a query evaluator for the cases we know we are
    in a legacy tree.
    
    Bug: 1282782
    Change-Id: Ibcebf427a5eff583da1f73fb990239da99305fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3370281
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956150}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -1998,7 +1998,7 @@ static void AssertLayoutTreeUpdated(Node& root,
           (allow_dirty_container_subtrees && element->GetLayoutObject() &&
            element->GetLayoutObject()
                ->StyleRef()
-               .IsContainerForContainerQueries())) {
+               .IsContainerForContainerQueries(*element))) {
         node = FlatTreeTraversal::NextSkippingChildren(*node);
         continue;
       }
