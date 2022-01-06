d91e95542c7e63c5e98ea2613365db5086be6a32
Patrick Noland <pnoland@chromium.org>
2022-01-06 18:28:42 +0000

    Revert "[Start] Don't load the first Tab if Start surface is showing on Startup."
    This reverts commit 7eaabbc72a22283a8a3c26069b1c992acd5d0a1d.
    
    Reason for revert: Suspect this is causing a variety of test failures:
    https://ci.chromium.org/p/chromium/builders/ci/android-bfcache-rel
    https://ci.chromium.org/p/chromium/builders/ci/android-arm64-proguard-rel
    https://ci.chromium.org/p/chromium/builders/ci/android-marshmallow-arm64-rel
    
    Original change's description:
    > [Start] Don't load the first Tab if Start surface is showing on Startup.
    >
    > This CL follows up the refactor in CL (https://crrev.com/c/3343014).
    >
    > During startup, if Start surface is showing, we set a flag to the
    > StartSurfaceHelper, and rests the value after the Start surface is
    > hiding. The TabPersistentStore reads this flag when setting the index of
    > the TabModel and allows not loading the last selected Tab during the Tab
    > restore.
    >
    > Bug: 1229187
    > Change-Id: Id048d6525b109a28071d8ad1a53d2637e62fabca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3343249
    > Reviewed-by: Hao Dong <spdonghao@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Commit-Queue: Xi Han <hanxi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#955778}
    
    Bug: 1229187
    Change-Id: I7125fe5200061557792c335920e699ae583a2f2b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3370484
    Auto-Submit: Patrick Noland <pnoland@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956176}

--- chrome/android/features/start_surface/public/java/src/org/chromium/chrome/features/start_surface/StartSurfaceUserData.java
+++ chrome/android/features/start_surface/public/java/src/org/chromium/chrome/features/start_surface/StartSurfaceUserData.java
@@ -22,11 +22,6 @@ public class StartSurfaceUserData implements UserData {
 
     // Saves the Feeds instance state.
     private String mFeedsInstanceState;
-    /**
-     * Tracks whether the last visited Tab is restored at startup but not showing due to the
-     * overview page is showing at the startup.
-     */
-    private boolean mUnusedTabRestoredAtStartup;
 
     /**
      * Static class that implements the initialization-on-demand holder idiom.
@@ -159,21 +154,5 @@ public class StartSurfaceUserData implements UserData {
     @Nullable
     protected String restoreFeedInstanceState() {
         return mFeedsInstanceState;
-    }
-
-    /**
-     * Sets whether an unused Tab is restored at startup due to an overview page is showing at the
-     * startup.
-     */
-    public void setUnusedTabRestoredAtStartup(boolean overviewShownAtStartup) {
-        mUnusedTabRestoredAtStartup = overviewShownAtStartup;
-    }
-
-    /**
-     * Gets whether an unused Tab is restored at startup due to an overview page is showing at the
-     * startup.
-     */
-    public boolean getUnusedTabRestoredAtStartup() {
-        return mUnusedTabRestoredAtStartup;
     }
 }
