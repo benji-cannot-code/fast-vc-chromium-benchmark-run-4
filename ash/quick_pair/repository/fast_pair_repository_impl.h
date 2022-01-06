8f2f55e778508dd14af67e0b9d2dcc4bc230f68e
Daniel Classon <dclasson@google.com>
2022-01-06 20:32:01 +0000

    [Fast Pair Device Images] Add DeviceIdMap class
    Adds the DeviceIdMap class which stores records of device ID -> model ID
    mappings in memory. Also provides methods to persist and evict records
    from local state prefs. Used by FastPairRepositoryImpl to fetch device
    images.
    
    Change-Id: Ic932eeaaf7e871158eae18c880afda6dc31edffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3343211
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#956221}

--- ash/quick_pair/repository/fast_pair_repository_impl.h
+++ ash/quick_pair/repository/fast_pair_repository_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_QUICK_PAIR_REPOSITORY_FAST_PAIR_REPOSITORY_IMPL_H_
 
 #include "ash/quick_pair/common/device.h"
+#include "ash/quick_pair/repository/fast_pair/device_metadata.h"
 #include "ash/quick_pair/repository/fast_pair_repository.h"
 #include "base/callback.h"
 #include "base/containers/flat_map.h"
@@ -26,6 +27,7 @@ class UserReadDevicesResponse;
 namespace ash {
 namespace quick_pair {
 
+class DeviceIdMap;
 class DeviceImageStore;
 class DeviceMetadataFetcher;
 class FastPairImageDecoder;
@@ -84,6 +86,7 @@ class FastPairRepositoryImpl : public FastPairRepository {
   std::unique_ptr<DeviceMetadataFetcher> device_metadata_fetcher_;
   std::unique_ptr<FootprintsFetcher> footprints_fetcher_;
   std::unique_ptr<FastPairImageDecoder> image_decoder_;
+  std::unique_ptr<DeviceIdMap> device_id_map_;
   std::unique_ptr<DeviceImageStore> device_image_store_;
   std::unique_ptr<SavedDeviceRegistry> saved_device_registry_;
 
