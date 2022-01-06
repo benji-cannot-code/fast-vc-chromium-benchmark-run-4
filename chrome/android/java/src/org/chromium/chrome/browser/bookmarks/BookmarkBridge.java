bd9b9f2087e0911a2fea28274bbc0211fbbfd0b8
Matt Jones <mdjones@chromium.org>
2022-01-06 20:15:23 +0000

    [Bookmarks] Ensure BookmarkAdded user action is recorded on Android
    Bug: 1280293
    Change-Id: I6701c300029db548be454dde67821b169d680031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342207
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956218}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkBridge.java
@@ -20,6 +20,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmarksShim;
 import org.chromium.chrome.browser.power_bookmarks.PowerBookmarkMeta;
 import org.chromium.chrome.browser.power_bookmarks.PowerBookmarkType;
@@ -883,6 +884,8 @@ public class BookmarkBridge {
         assert title != null;
         assert url != null;
 
+        recordBookmarkAdded();
+
         if (TextUtils.isEmpty(title)) title = url.getSpec();
         return BookmarkBridgeJni.get().addBookmark(
                 mNativeBookmarkBridge, BookmarkBridge.this, parent, index, title, url);
@@ -909,9 +912,16 @@ public class BookmarkBridge {
         assert title != null;
         assert url != null;
 
+        recordBookmarkAdded();
+
         if (TextUtils.isEmpty(title)) title = url.getSpec();
         return BookmarkBridgeJni.get().addPowerBookmark(
                 mNativeBookmarkBridge, this, webContents, parent, index, title, url);
+    }
+
+    /** Record the user action for adding a bookmark. */
+    private void recordBookmarkAdded() {
+        RecordUserAction.record("BookmarkAdded");
     }
 
     @Deprecated // Only included until internal repository is updated.
