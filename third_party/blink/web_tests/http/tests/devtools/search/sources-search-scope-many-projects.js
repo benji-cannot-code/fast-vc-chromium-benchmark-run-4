ad113baf70c1e1b9b277ffcc04c11e753d199659
Tim van der Lippe <tvanderlippe@chromium.org>
2022-01-06 17:05:57 +0000

    Replace loadResourcePromise with fetch
    R=aerotwist@chromium.org
    
    Bug: none
    Change-Id: I4aa6e8e602e2fc168fd39c79525fb782f5a6ba42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3370222
    Commit-Queue: Tim Van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Tim Van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956146}

--- third_party/blink/web_tests/http/tests/devtools/search/sources-search-scope-many-projects.js
+++ third_party/blink/web_tests/http/tests/devtools/search/sources-search-scope-many-projects.js
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   function loadResource(name) {
     var url = TestRunner.url('resources/' + name);
-    return Root.Runtime.loadResourcePromise(url).then(function(text) {
+    return fetch(url).then(result => result.text()).then(function(text) {
       resources[name] = text;
     });
   }
