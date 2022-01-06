f6ef0fc1a058a6b11c38f9a84a530fcbc873b268
Stephen Roettger <sroettger@google.com>
2022-01-06 17:01:39 +0000

    Reject more mojo messages on non-broker nodes
    OnIntroduce and OnBroadcast are only sent to the broker process. Reject
    these messages on any other node.
    
    Change-Id: Id5adc30cf38ceca3a31ec0165551e14d77ea8c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3338417
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Stephen Röttger <sroettger@google.com>
    Cr-Commit-Position: refs/heads/main@{#956145}

--- mojo/core/node_controller.cc
+++ mojo/core/node_controller.cc
@@ -1141,6 +1141,12 @@ void NodeController::OnRequestIntroduction(const ports::NodeName& from_node,
                                            const ports::NodeName& name) {
   DCHECK(io_task_runner_->RunsTasksInCurrentSequence());
 
+  if (broker_name_ != ports::kInvalidNodeName) {
+    DLOG(ERROR) << "Ignoring OnRequestIntroduction on non-broker node.";
+    DropPeer(from_node, nullptr);
+    return;
+  }
+
   scoped_refptr<NodeChannel> requestor = GetPeerChannel(from_node);
   if (from_node == name || name == ports::kInvalidNodeName || !requestor) {
     DLOG(ERROR) << "Rejecting invalid OnRequestIntroduction message from "
@@ -1212,6 +1218,12 @@ void NodeController::OnIntroduce(const ports::NodeName& from_node,
 void NodeController::OnBroadcast(const ports::NodeName& from_node,
                                  Channel::MessagePtr message) {
   DCHECK(!message->has_handles());
+
+  if (broker_name_ != ports::kInvalidNodeName) {
+    DLOG(ERROR) << "Ignoring OnBroadcast on non-broker node.";
+    DropPeer(from_node, nullptr);
+    return;
+  }
 
   auto event = DeserializeEventMessage(from_node, std::move(message));
   if (!event) {
