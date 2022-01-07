e36af4a590d63a0da3ee248c9b8dee0e62bac27f
Florian Gauger <fga@google.com>
2022-01-07 17:26:51 +0000

    [Autofill Assistant] Add interface for module install ui
    We are in the process of moving all Autofill Assistant code to //components. For this we need to remove all dependencies on Chrome code.
    
    In this cl we abstract away Chrome's ModuleInstallUi into an interface.
    
    Bug: b/173103628
    Change-Id: I478b53c3bee7bf0ffaf6d8faf8521efcb4e95f97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3362528
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956557}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabHelpers.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabHelpers.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab;
 
 import org.chromium.chrome.browser.SwipeRefreshHandler;
-import org.chromium.chrome.browser.autofill_assistant.AssistantDependencyUtilsChrome;
 import org.chromium.chrome.browser.autofill_assistant.AutofillAssistantTabHelper;
 import org.chromium.chrome.browser.complex_tasks.TaskTabHelper;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchTabHelper;
@@ -39,8 +38,7 @@ public final class TabHelpers {
         TabBrowserControlsConstraintsHelper.createForTab(tab);
         ContinuousSearchTabHelper.createForTab(tab);
         if (ReaderModeManager.isEnabled()) ReaderModeManager.createForTab(tab);
-        AutofillAssistantTabHelper.createForTab(tab, AssistantDependencyUtilsChrome::isGsa,
-                AssistantDependencyUtilsChrome::isMakeSearchesAndBrowsingBetterSettingEnabled);
+        AutofillAssistantTabHelper.createForTab(tab);
 
         // The following will start prefetching data for the price drops feature, so
         // we should only do it if the user is eligible for the feature (e.g. has sync enabled).
