bfa4e4c541037fb1199a3f9cfd14d9d16ad80534
Daniel White <danieltwhite@google.com>
2022-01-07 15:42:08 +0000

    Removal of Non-test GetJSInjectionReceiver() Usage
    In this CL, invocations of GetJSInjectionReceiver() in a non-test
    context where removed and replaced with WebFrame::ExecuteJavaScript.
    The purpose of GetJSInjectionReceiver()'s removal is to eliminate the
    codebase's dependency on |windowId|.
    
    This CL does not removal all instances of GetJSInjectionReceiver() in a
    non-test context. The remaining invocations require refactoring the
    codebase to be able to remove them. This will be done in a future CL.
    
    Bug: 1278454
    Change-Id: Ia6a56ca69bec168dac5a099f6cdfaaf006b81cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3370241
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#956519}

--- ios/chrome/browser/web_state_list/view_source_browser_agent.mm
+++ ios/chrome/browser/web_state_list/view_source_browser_agent.mm
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web_state_list/tab_insertion_browser_agent.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/web/public/deprecated/crw_js_injection_receiver.h"
+#import "ios/web/public/js_messaging/web_frame.h"
+#import "ios/web/public/js_messaging/web_frame_util.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "ios/web/public/web_state.h"
 
@@ -28,17 +30,28 @@ void ViewSourceBrowserAgent::ViewSourceForActiveWebState() {
   web::WebState* web_state = browser_->GetWebStateList()->GetActiveWebState();
   DCHECK(web_state);
 
-  NSString* script = @"document.documentElement.outerHTML;";
-  auto completionHandlerBlock = ^(id result, NSError*) {
-    if (!web_state)
-      return;
-    if (![result isKindOfClass:[NSString class]])
-      result = @"Not an HTML page";
-    InsertSourceViewTab(result, web_state);
-  };
-  [web_state->GetJSInjectionReceiver()
-      executeJavaScript:script
-      completionHandler:completionHandlerBlock];
+  web::WebFrame* web_frame = web::GetMainFrame(web_state);
+  static const char kScript[] = "document.documentElement.outerHTML;";
+
+  web_frame->ExecuteJavaScript(
+      kScript,
+      base::BindOnce(
+          &ViewSourceBrowserAgent::OnHandleViewSourceForActiveWebStateResult,
+          weak_ptr_factory_.GetWeakPtr()));
+}
+
+void ViewSourceBrowserAgent::OnHandleViewSourceForActiveWebStateResult(
+    const base::Value* value) {
+  web::WebState* web_state = browser_->GetWebStateList()->GetActiveWebState();
+  DCHECK(web_state);
+
+  NSString* result;
+  if (value->is_string()) {
+    result = base::SysUTF8ToNSString(value->GetString());
+  } else {
+    result = @"Not an HTML page";
+  }
+  InsertSourceViewTab(result, web_state);
 }
 
 void ViewSourceBrowserAgent::InsertSourceViewTab(NSString* source,
