4801c63058fe8e3eda69fd9cec32a257e677e45f
Ayu Ishii <ayui@chromium.org>
2022-01-07 23:40:22 +0000

    StorageBuckets: IndexedDB GetOrCreateBucket Error Handling
    This change updates IndexedDB to return an error if GetOrCreateBucket
    fails. This is necessary because we do not want to store any data
    for a StorageKey unless there is a StorageBucket associated with it.
    Eventually when storage APIs integrate with buckets, the data
    directory for default buckets will change to a path with bucket_id.
    APIs will not be able to store data without a bucket. We enforce this
    rule now, before we bootstrap QuotaDatabase to create buckets for all
    StorageKeys with data, and QuotaDatabase becomes the source of truth
    for all stored data for quota managed Storage APIs.
    
    Change-Id: I204f8f4a6e70918ccf1b0ae09653a3ed98bf0cb6
    Bug: 1267041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3271759
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956715}

--- content/browser/indexed_db/indexed_db_dispatcher_host.h
+++ content/browser/indexed_db/indexed_db_dispatcher_host.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/sequence_checker.h"
+#include "components/services/storage/public/cpp/buckets/bucket_locator.h"
 #include "components/services/storage/public/mojom/blob_storage_context.mojom-forward.h"
 #include "components/services/storage/public/mojom/file_system_access_context.mojom-forward.h"
 #include "content/browser/indexed_db/indexed_db_external_object.h"
@@ -52,6 +53,7 @@ class CONTENT_EXPORT IndexedDBDispatcherHost : public blink::mojom::IDBFactory {
 
   void AddReceiver(
       const blink::StorageKey& storage_key,
+      absl::optional<storage::BucketLocator> bucket,
       mojo::PendingReceiver<blink::mojom::IDBFactory> pending_receiver);
 
   void AddDatabaseBinding(
@@ -137,7 +139,9 @@ class CONTENT_EXPORT IndexedDBDispatcherHost : public blink::mojom::IDBFactory {
   // Shared task runner used to read blob files on.
   const scoped_refptr<base::TaskRunner> file_task_runner_;
 
-  mojo::ReceiverSet<blink::mojom::IDBFactory, blink::StorageKey> receivers_;
+  mojo::ReceiverSet<blink::mojom::IDBFactory,
+                    absl::optional<storage::BucketLocator>>
+      receivers_;
   mojo::UniqueAssociatedReceiverSet<blink::mojom::IDBDatabase>
       database_receivers_;
   mojo::UniqueAssociatedReceiverSet<blink::mojom::IDBCursor> cursor_receivers_;
