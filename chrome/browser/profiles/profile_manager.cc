1cb14ec7439b23871267d7f4b26d2bd28675d3f3
Mihai Sardarescu <msarda@chromium.org>
2022-01-07 10:26:28 +0000

    Remove primary account policy manager from component
    This CL moves the PrimaryAccountPolicyManager out of component/signin.
    It reuses the logic in //chrome when a profile needs to be deleted,
    showing a warning dialog that this will happen.
    
    Bug: 1284607
    
    Change-Id: I92558302cabfc8f1643421355c0b6042fd8081fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3361021
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956452}

--- chrome/browser/profiles/profile_manager.cc
+++ chrome/browser/profiles/profile_manager.cc
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/signin/account_reconcilor_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/signin/primary_account_policy_manager_factory.h"
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
@@ -1548,7 +1549,7 @@ void ProfileManager::DoFinalInit(ProfileInfo* profile_info,
   // had enough time to initialize and should have updated the user signout
   // flag attached to the profile.
   signin_util::EnsureUserSignoutAllowedIsInitializedForProfile(profile);
-  signin_util::EnsurePrimaryAccountAllowedForProfile(profile);
+  PrimaryAccountPolicyManagerFactory::GetForProfile(profile)->Initialize();
 
 #if !defined(OS_ANDROID)
   // The caret browsing command-line switch toggles caret browsing on
--- components/signin/public/identity_manager/access_token_fetcher_unittest.cc
+++ components/signin/public/identity_manager/access_token_fetcher_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/testing_pref_service.h"
 #include "components/signin/internal/identity_manager/account_tracker_service.h"
 #include "components/signin/internal/identity_manager/fake_profile_oauth2_token_service.h"
-#include "components/signin/internal/identity_manager/primary_account_policy_manager_impl.h"
 #include "components/signin/internal/identity_manager/profile_oauth2_token_service_delegate.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/signin/public/base/test_signin_client.h"
@@ -68,8 +67,7 @@ class AccessTokenFetcherTest
         account_tracker_(std::make_unique<AccountTrackerService>()),
         primary_account_manager_(&signin_client_,
                                  &token_service_,
-                                 account_tracker_.get(),
-                                 nullptr /* policy_manager */) {
+                                 account_tracker_.get()) {
     AccountTrackerService::RegisterPrefs(pref_service_.registry());
     PrimaryAccountManager::RegisterProfilePrefs(pref_service_.registry());
 
