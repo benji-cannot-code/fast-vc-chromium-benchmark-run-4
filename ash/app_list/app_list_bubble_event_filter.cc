5b0f3e06f943833665e4047a85cb4fa600b69df2
James Cook <jamescook@chromium.org>
2022-01-07 20:05:26 +0000

    ash: Fix ClickingShelfArrowDoesNotHideAppList for productivity launcher
    This AppListPresenterTest caught a real behavior issue -- the bubble
    launcher *was* closing when you click on a shelf arrow.
    
    Fix by porting some code from the fullscreen's launcher's event filter
    to the bubble launcher event filter. Parameterize the test by feature
    ProductivityLauncher so it won't regress.
    
    Bug: 1280300
    Test: ash_unittests
    Change-Id: Ieb543aaf9bc9afdd02e1deeca128e04ff049f0c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373303
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956629}

--- ash/app_list/app_list_bubble_event_filter.cc
+++ ash/app_list/app_list_bubble_event_filter.cc
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/app_list_bubble_event_filter.h"
 
 #include "ash/bubble/bubble_utils.h"
+#include "ash/shelf/hotseat_widget.h"
+#include "ash/shelf/shelf.h"
 #include "ash/shell.h"
 #include "base/callback.h"
 #include "base/check.h"
+#include "ui/aura/window.h"
 #include "ui/events/event.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
@@ -65,6 +68,16 @@ void AppListBubbleEventFilter::ProcessPressedEvent(
     views::View::ConvertPointFromScreen(button_, &point_in_button);
     if (button_->HitTestPoint(point_in_button))
       return;
+  }
+
+  // Ignore clicks in the shelf area containing app icons.
+  aura::Window* target = static_cast<aura::Window*>(event.target());
+  if (target) {
+    Shelf* shelf = Shelf::ForWindow(target);
+    if (target == shelf->hotseat_widget()->GetNativeWindow() &&
+        shelf->hotseat_widget()->EventTargetsShelfView(event)) {
+      return;
+    }
   }
 
   on_click_outside_.Run();
