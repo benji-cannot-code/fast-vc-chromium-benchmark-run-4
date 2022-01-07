e81edaaac9340d409c9a2c32d214e1a38791be16
Jayson Adams <shrike@chromium.org>
2022-01-07 04:36:24 +0000

    [macOS Window Occlusion] Work around platform occlusion detection quirks
    macOS supports platform-level occlusion detection but because of
    the platform theme's rounded window corners it won't deem one window
    occluded by another unless their sides are "far enough" apart
    (empirically about 10pts, which is the window corner curvature radius).
    
    This CL augments the macOS occlusion system with manual occlusion
    detection. This manual system does not try to build an aggregate
    occlusion region from multiple windows above but instead checks to
    see if any single window occludes another window. The result is not
    perfect occlusion detection but certainly better than what macOS gives
    us today, in particular on laptops where multiple browser windows will
    tend to have the same frame, one that fills the entire screen (sans menu
    and dock).
    
    This CL also ropes in display sleep and application hide as signals
    that feed into an occlusion determination.
    
    Bug: 883031, 1263532
    
    Test: out/Release/content_browsertests \
        --gtest_filter=WindowOcclusionBrowserTestMac*
    
    Change-Id: Ifcdd2e983841f733cdf4ced5b8853a3168fc6cf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3238127
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956382}

--- content/app_shim_remote_cocoa/web_contents_view_cocoa.h
+++ content/app_shim_remote_cocoa/web_contents_view_cocoa.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_nsobject.h"
 #include "content/common/content_export.h"
+#include "content/common/web_contents_ns_view_bridge.mojom.h"
 #import "ui/base/cocoa/base_view.h"
 #import "ui/base/cocoa/views_hostable.h"
 
@@ -70,8 +71,14 @@ CONTENT_EXPORT
                         image:(NSImage*)image
                        offset:(NSPoint)offset;
 - (void)clearViewsHostableView;
-- (void)updateWebContentsVisibility;
+- (void)updateWebContentsVisibility:
+    (remote_cocoa::mojom::Visibility)visibilityState;
 - (void)viewDidBecomeFirstResponder:(NSNotification*)notification;
+@end
+
+@interface NSWindow (WebContentsViewCocoa)
+// Returns all the WebContentsViewCocoas in the window.
+- (NSArray<WebContentsViewCocoa*>*)webContentsViewCocoa;
 @end
 
 #endif  // CONTENT_APP_SHIM_REMOTE_COCOA_WEB_CONTENTS_VIEW_COCOA_H_
