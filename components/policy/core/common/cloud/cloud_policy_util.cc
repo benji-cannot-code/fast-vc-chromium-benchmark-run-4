28674314d14cd0c55d8f899c7a2d6b9225cb98b1
Bryant Chandler <bryantchandler@chromium.org>
2022-01-07 21:45:22 +0000

    [fuchsia] Implement cloud policy GetMachineName
    This updates GetMachineName to use the posix API for Fuchsia.
    
    Bug: 961457, 1257674
    Change-Id: I5c7b9345bb2b517b289951b9ea47322131b42e55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322023
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956673}

--- components/policy/core/common/cloud/cloud_policy_util.cc
+++ components/policy/core/common/cloud/cloud_policy_util.cc
@@ -19,7 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wincred.h>
 #endif
 
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS) || defined(OS_APPLE)
+#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS) || defined(OS_APPLE) || \
+    defined(OS_FUCHSIA)
 #include <pwd.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -65,11 +66,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_cftyperef.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/system/sys_info.h"
-#endif
-
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "base/system/sys_info.h"
 #endif
 
 #if defined(OS_IOS)
@@ -81,7 +77,7 @@ namespace policy {
 namespace em = enterprise_management;
 
 std::string GetMachineName() {
-#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(OS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS) || defined(OS_FUCHSIA)
   char hostname[HOST_NAME_MAX];
   if (gethostname(hostname, HOST_NAME_MAX) == 0)  // Success.
     return hostname;
@@ -127,13 +123,13 @@ std::string GetMachineName() {
     return result;
   }
   return std::string();
-#elif defined(OS_ANDROID) || defined(OS_FUCHSIA)
-  // TODO(crbug.com/1257674): This should be fully implemented when there is
-  // support in fuchsia.
+#elif defined(OS_ANDROID)
   return std::string();
-#else
+#elif defined(OS_CHROMEOS)
   NOTREACHED();
   return std::string();
+#else
+#error Unsupported platform
 #endif
 }
 
