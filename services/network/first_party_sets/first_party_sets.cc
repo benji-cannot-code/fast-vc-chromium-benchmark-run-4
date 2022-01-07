15f603948971fda1e4920429baaa1c4ae2278a4c
cfredric <cfredric@chromium.org>
2022-01-07 21:16:00 +0000

    Reland "Initialize the FirstPartySets instance with empty set if the component isn't installed yet."
    This is a reland of 2f2a77742d986df879b1f4b1dbd43d879ff6c4b0
    
    Diffing patchset 1 to patchset 2 shows the fix for the crash that caused
    the revert.
    
    Original change's description:
    > Initialize the FirstPartySets instance with empty set if the component
    > isn't installed yet.
    >
    > This fixes some inconsistent behavior where if the client hadn't
    > installed the component yet, the FirstPartySets instance would take a
    > long time to become initialized (O(minutes), depending on when Component
    > Updater Service next checked for updates); and would get initialized in
    > the middle of the user's browsing session, resulting in inconsistent
    > cookie behavior during the session.
    >
    > Bug: 1283271
    > Change-Id: I158689f61a58a682a47f4c325dffcc5502ccd4a2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3359804
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#955723}
    
    Bug: 1283271, 1284786
    Change-Id: Iac00902c0b37505640f14e5ad3ce0288c5d2b253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373142
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956660}

--- services/network/first_party_sets/first_party_sets.cc
+++ services/network/first_party_sets/first_party_sets.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
@@ -101,8 +102,17 @@ void FirstPartySets::SetManuallySpecifiedSet(const std::string& flag_value) {
 
 void FirstPartySets::ParseAndSet(base::File sets_file) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (!enabled_)
+  if (!enabled_ || component_sets_parse_progress_ != Progress::kNotStarted) {
     return;
+  }
+
+  component_sets_parse_progress_ = Progress::kStarted;
+
+  if (!sets_file.IsValid()) {
+    OnReadSetsFile("");
+    return;
+  }
+
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
       base::BindOnce(&ReadSetsFile, std::move(sets_file)),
@@ -112,8 +122,8 @@ void FirstPartySets::ParseAndSet(base::File sets_file) {
 
 void FirstPartySets::OnReadSetsFile(const std::string& raw_sets) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (!enabled_)
-    return;
+  DCHECK_EQ(component_sets_parse_progress_, Progress::kStarted);
+  DCHECK(enabled_);
 
   bool is_v1_format = raw_sets.find('[') < raw_sets.find('{');
   if (is_v1_format) {
@@ -129,7 +139,7 @@ void FirstPartySets::OnReadSetsFile(const std::string& raw_sets) {
                             is_v1_format);
 
   ApplyManuallySpecifiedSet();
-  component_sets_ready_ = true;
+  component_sets_parse_progress_ = Progress::kFinished;
   ClearSiteDataOnChangedSetsIfReady();
 }
 
@@ -346,8 +356,9 @@ base::flat_set<net::SchemefulSite> FirstPartySets::ComputeSetsDiff(
 
 void FirstPartySets::ClearSiteDataOnChangedSetsIfReady() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (!persisted_sets_ready_ || !component_sets_ready_ || !manual_sets_ready_ ||
-      on_site_data_cleared_.is_null())
+  if (!persisted_sets_ready_ ||
+      component_sets_parse_progress_ != Progress::kFinished ||
+      !manual_sets_ready_ || on_site_data_cleared_.is_null())
     return;
 
   base::flat_set<net::SchemefulSite> diff = ComputeSetsDiff(
