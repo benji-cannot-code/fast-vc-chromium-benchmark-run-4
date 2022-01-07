08fdc94f331581b151dbc4676ecf2c8c7f5380c8
K. Moon <kmoon@chromium.org>
2022-01-07 18:04:41 +0000

    [unseasoned-pdf] Add PdfViewPluginBase::HandleInputEvent() test
    Adds a minimal unit test for PdfViewPluginBase::HandleInputEvent().
    
    Bug: 1191817
    Change-Id: I98c131242d1d0ebce1fb4889d890f7b307ee17a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373134
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Reviewed-by: Hui Yingst <nigi@chromium.org>
    Commit-Queue: Hui Yingst <nigi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956572}

--- pdf/pdf_view_plugin_base_unittest.cc
+++ pdf/pdf_view_plugin_base_unittest.cc
@@ -32,7 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/public/common/input/web_input_event.h"
+#include "third_party/blink/public/common/input/web_mouse_event.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace chrome_pdf {
@@ -161,6 +164,7 @@ class FakePdfViewPluginBase : public PdfViewPluginBase {
   using PdfViewPluginBase::accessibility_state;
   using PdfViewPluginBase::engine;
   using PdfViewPluginBase::full_frame;
+  using PdfViewPluginBase::HandleInputEvent;
   using PdfViewPluginBase::HandleMessage;
   using PdfViewPluginBase::LoadUrl;
   using PdfViewPluginBase::SetZoom;
@@ -779,6 +783,25 @@ TEST_F(PdfViewPluginBaseTest, DocumentHasUnsupportedFeatureWithoutFullFrame) {
   EXPECT_TRUE(fake_plugin_.UnsupportedFeatureIsReportedForTesting(kMetric));
   EXPECT_FALSE(
       fake_plugin_.GetNotifiedBrowserAboutUnsupportedFeatureForTesting());
+}
+
+TEST_F(PdfViewPluginBaseWithEngineTest, HandleInputEvent) {
+  auto* engine = static_cast<TestPDFiumEngine*>(fake_plugin_.engine());
+  EXPECT_CALL(*engine, HandleInputEvent)
+      .WillRepeatedly([](const blink::WebInputEvent& event) {
+        const auto& mouse_event =
+            static_cast<const blink::WebMouseEvent&>(event);
+        EXPECT_EQ(blink::WebInputEvent::Type::kMouseDown,
+                  mouse_event.GetType());
+        EXPECT_EQ(gfx::PointF(10.0f, 20.0f), mouse_event.PositionInWidget());
+        return true;
+      });
+
+  blink::WebMouseEvent mouse_event;
+  mouse_event.SetType(blink::WebInputEvent::Type::kMouseDown);
+  mouse_event.SetPositionInWidget(10.0f, 20.0f);
+
+  EXPECT_TRUE(fake_plugin_.HandleInputEvent(mouse_event));
 }
 
 TEST_F(PdfViewPluginBaseTest, EnteredEditMode) {
