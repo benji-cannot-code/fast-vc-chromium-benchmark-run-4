3042dcf024cf3233e2077f4d1c6a4d118bddbe07
Collin Baker <collinbaker@chromium.org>
2022-01-07 21:59:50 +0000

    Import Rust bindings from old Mojo SDK
    First step in project described in
    https://docs.google.com/document/d/1c_rAwe9E7xVaGrxc_XCuYoONIK-CHSVa9mX2XH7uSdk/edit?usp=sharing
    
    Begins implementing Rust Mojo bindings. Since it is easier than
    starting from scratch, imports outdated but nearly-functional bindings
    from the defunct Mojo SDK. These are roughly 5 years old, and Mojo has
    changed significantly since, but it is similar enough to start with.
    
    Copies //rust from
    https://chromium.googlesource.com/external/github.com/domokit/mojo_sdk/+/da7adf03fa17561fa257306664651fc89a451f41
    to //mojo/public/rust in Chromium and adds an OWNERs file.
    
    Makes no other changes. The code is not built or run in any
    configuration. Subsequent CLs will add it to the build system,
    modernize it, etc.
    
    Bug: 1274864
    Change-Id: I06a591a711794a63843533676f2a4512db18660d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3285285
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956681}

---
+++ mojo/public/rust/src/bindings/mod.rs
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#[macro_use]
+mod macros;
+mod util;
+
+pub mod encoding;
+pub mod decoding;
+pub mod message;
+pub mod mojom;
+pub mod run_loop;
