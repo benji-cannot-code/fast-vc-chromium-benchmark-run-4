1119eac68833a26fbe32863981748a07303f90ff
Aida Zolic <aidazolic@chromium.org>
2022-01-07 11:00:10 +0000

    Support DLP warning mode in TabCaptureAccessHandler
    Adds support for warning level Data Leak Prevention restriction for
    requests coming from Tab Capture API, which are handled by
    TabCaptureAccessHandler.
    
    
    Bug: 1259605
    Change-Id: Ia974ee462a7cfa92d8ec9b53f5753ec842c3ced9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3314586
    Reviewed-by: Will Cassella <cassew@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956458}

--- chrome/browser/media/capture_access_handler_base.cc
+++ chrome/browser/media/capture_access_handler_base.cc
@@ -181,8 +181,8 @@ void CaptureAccessHandlerBase::UpdateMediaRequestState(
 
 void CaptureAccessHandlerBase::UpdateExtensionTrusted(
     const content::MediaStreamRequest& request,
-    const extensions::Extension* extension) {
-  UpdateTrusted(request, IsExtensionAllowedForScreenCapture(extension) ||
+    bool is_allowlisted_extension) {
+  UpdateTrusted(request, is_allowlisted_extension ||
                              IsBuiltInFeedbackUI(request.security_origin));
 }
 
