66882e7eea79db128d9df2d7f03bd0fba4651906
Yutaka Hirano <yhirano@chromium.org>
2022-01-07 01:38:24 +0000

    Stream cancellation in page should be notified to service worker
    When the script transfers a body stream via FetchEvent.RespondWith and
    the stream consumption is cancelled in page, that cancellation should be
    observable in the service worker via UnderlyingSource.cancel.
    
    Bug: 638494
    Change-Id: Ib46c9ae73c17dd2c04432d23b06dad76466105ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347484
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956321}

--- third_party/blink/renderer/core/fetch/fetch_data_loader.cc
+++ third_party/blink/renderer/core/fetch/fetch_data_loader.cc
@@ -532,8 +532,11 @@ class FetchDataLoaderAsDataPipe final : public FetchDataLoader,
       scoped_refptr<base::SingleThreadTaskRunner> task_runner)
       : data_pipe_watcher_(FROM_HERE,
                            mojo::SimpleWatcher::ArmingPolicy::MANUAL,
-                           std::move(task_runner)) {}
-  ~FetchDataLoaderAsDataPipe() override {}
+                           task_runner),
+        data_pipe_close_watcher_(FROM_HERE,
+                                 mojo::SimpleWatcher::ArmingPolicy::AUTOMATIC,
+                                 std::move(task_runner)) {}
+  ~FetchDataLoaderAsDataPipe() override = default;
 
   void Start(BytesConsumer* consumer,
              FetchDataLoader::Client* client) override {
@@ -573,8 +576,14 @@ class FetchDataLoaderAsDataPipe final : public FetchDataLoader,
           out_data_pipe_.get(), MOJO_HANDLE_SIGNAL_WRITABLE,
           WTF::BindRepeating(&FetchDataLoaderAsDataPipe::OnWritable,
                              WrapWeakPersistent(this)));
+      data_pipe_close_watcher_.Watch(
+          out_data_pipe_.get(), MOJO_HANDLE_SIGNAL_PEER_CLOSED,
+          MOJO_TRIGGER_CONDITION_SIGNALS_SATISFIED,
+          WTF::BindRepeating(&FetchDataLoaderAsDataPipe::OnPeerClosed,
+                             WrapWeakPersistent(this)));
 
       data_pipe_watcher_.ArmOrNotify();
+      data_pipe_close_watcher_.ArmOrNotify();
     }
 
     // Give the resulting pipe consumer handle to the client.
@@ -587,6 +596,11 @@ class FetchDataLoaderAsDataPipe final : public FetchDataLoader,
     if (consumer->GetPublicState() !=
         BytesConsumer::PublicState::kReadableOrWaiting)
       OnStateChange();
+  }
+
+  void OnPeerClosed(MojoResult result, const mojo::HandleSignalsState& state) {
+    StopInternal();
+    client_->DidFetchDataLoadFailed();
   }
 
   void OnWritable(MojoResult) { OnStateChange(); }
@@ -653,17 +667,21 @@ class FetchDataLoaderAsDataPipe final : public FetchDataLoader,
  private:
   void StopInternal() {
     consumer_->Cancel();
-    data_pipe_watcher_.Cancel();
-    out_data_pipe_.reset();
+    Dispose();
   }
 
-  void Dispose() { data_pipe_watcher_.Cancel(); }
+  void Dispose() {
+    data_pipe_watcher_.Cancel();
+    data_pipe_close_watcher_.Cancel();
+    out_data_pipe_.reset();
+  }
 
   Member<BytesConsumer> consumer_;
   Member<FetchDataLoader::Client> client_;
 
   mojo::ScopedDataPipeProducerHandle out_data_pipe_;
   mojo::SimpleWatcher data_pipe_watcher_;
+  mojo::SimpleWatcher data_pipe_close_watcher_;
 };
 
 }  // namespace
--- third_party/blink/renderer/core/fetch/readable_stream_bytes_consumer.cc
+++ third_party/blink/renderer/core/fetch/readable_stream_bytes_consumer.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_iterator_result_value.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/scoped_persistent.h"
+#include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding_macros.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -175,6 +176,16 @@ void ReadableStreamBytesConsumer::ClearClient() {
 void ReadableStreamBytesConsumer::Cancel() {
   if (state_ == PublicState::kClosed || state_ == PublicState::kErrored)
     return;
+  // BytesConsumer::Cancel can be called with ScriptForbiddenScope (e.g.,
+  // in ExecutionContextLifecycleObserver::ContextDestroyed()). We don't run
+  // ReadableStreamDefaultReader::cancel in such a case.
+  if (!ScriptForbiddenScope::IsScriptForbidden()) {
+    ScriptState::Scope scope(script_state_);
+    ExceptionState exception_state(script_state_->GetIsolate(),
+                                   ExceptionState::kUnknownContext, "", "");
+    reader_->cancel(script_state_, exception_state);
+    // We ignore exceptions as we can do nothing here.
+  }
   state_ = PublicState::kClosed;
   ClearClient();
   reader_ = nullptr;
