5067bdb1381e219e6c0c2610f6af2722340e86df
Juliet Levesque <julietlevesque@google.com>
2022-01-07 00:51:24 +0000

    [Fast Pair] Log the result of fetching metadata from the repository.
    Adds a metric to log the success or failure of fetchng device
    metadata from the Fast Pair repository.
    
    Change-Id: I3f48048ac51d7ee852254e58ae84cb23c61188ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342666
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#956310}

--- ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
+++ ash/quick_pair/common/fast_pair/fast_pair_metrics.cc
@@ -139,6 +139,8 @@ const char kMessageStreamConnectToServiceResult[] =
 const char kMessageStreamConnectToServiceTime[] =
     "Bluetooth.ChromeOS.FastPair.MessageStream.ConnectToService."
     "TotalConnectTime";
+const char kDeviceMetadataFetchResult[] =
+    "Bluetooth.ChromeOS.FastPair.DeviceMetadataFetcher.Result";
 
 }  // namespace
 
@@ -378,6 +380,10 @@ void RecordMessageStreamConnectToServiceTime(
     base::TimeDelta total_connect_time) {
   base::UmaHistogramTimes(kMessageStreamConnectToServiceTime,
                           total_connect_time);
+}
+
+void RecordDeviceMetadataFetchResult(bool success) {
+  base::UmaHistogramBoolean(kDeviceMetadataFetchResult, success);
 }
 
 }  // namespace quick_pair
--- ash/quick_pair/repository/fast_pair_repository_impl.cc
+++ ash/quick_pair/repository/fast_pair_repository_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/quick_pair/repository/fast_pair_repository_impl.h"
 
+#include "ash/quick_pair/common/fast_pair/fast_pair_metrics.h"
 #include "ash/quick_pair/common/logging.h"
 #include "ash/quick_pair/proto/fastpair.pb.h"
 #include "ash/quick_pair/proto/fastpair_data.pb.h"
@@ -62,6 +63,8 @@ void FastPairRepositoryImpl::OnMetadataFetched(
     const std::string& normalized_model_id,
     DeviceMetadataCallback callback,
     absl::optional<nearby::fastpair::GetObservedDeviceResponse> response) {
+  RecordDeviceMetadataFetchResult(/*success=*/response.has_value());
+
   if (!response) {
     std::move(callback).Run(nullptr);
     return;
