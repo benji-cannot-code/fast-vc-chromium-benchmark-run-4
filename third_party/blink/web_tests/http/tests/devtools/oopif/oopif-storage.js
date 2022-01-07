1741fc2cea1cae337f6a284ae67f635dba473109
Yang Guo <yangguo@chromium.org>
2022-01-07 14:51:10 +0000

    Re-enable devtools/oopif/oopif-storage test
    Bug: 1203169
    Change-Id: Id70e143f2c1b3c14fdfad5b9911e4b48c55f9b8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3368082
    Auto-Submit: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956502}

--- third_party/blink/web_tests/http/tests/devtools/oopif/oopif-storage.js
+++ third_party/blink/web_tests/http/tests/devtools/oopif/oopif-storage.js
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.showPanel('resources');
 
   TestRunner.deprecatedRunAfterPendingDispatches(function() {
-    const localStorageTree = Resources.ResourcesPanel._instance()._sidebar.localStorageListTreeElement;
+    const localStorageTree = Resources.ResourcesPanel.instance().sidebar.localStorageListTreeElement;
     localStorageTree.expandRecursively(1000);
-    const sessionStorageTree = Resources.ResourcesPanel._instance()._sidebar.sessionStorageListTreeElement;
+    const sessionStorageTree = Resources.ResourcesPanel.instance().sidebar.sessionStorageListTreeElement;
     sessionStorageTree.expandRecursively(1000);
 
     TestRunner.addResult('Local Storage:');
