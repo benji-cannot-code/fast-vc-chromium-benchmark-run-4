310abd0d15b98afebd684265ef068f89ae4db6d5
Joshua Pawlicki <waffles@chromium.org>
2022-01-07 17:39:33 +0000

    Reland "Omaha: Cease transmission of device ID in update checks. (Android)"
    This is a reland of 0876c6345f7e4361087d45b03ec69fe1f7c41281
    Patch set 1 is the original change. Patch set 2 is the fixed version.
    
    Original change's description:
    > Omaha: Cease transmission of device ID in update checks. (Android)
    >
    > A Finch feature is introduced in case reverting to the old behavior is
    > necessary.
    >
    > Bug: 1139505
    > Change-Id: Ief902f0dd4a194fcf2274867e4caf12bd5a03b55
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350784
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#953306}
    
    Bug: 1139505
    Change-Id: Ia3e698e2c36cee77e7c26d1725cac2a67956e037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3367405
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956561}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omaha/OmahaBaseTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omaha/OmahaBaseTest.java
@@ -19,8 +19,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.FeatureList;
 import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.base.test.util.Feature;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.omaha.MockRequestGenerator;
 import org.chromium.chrome.test.omaha.MockRequestGenerator.DeviceType;
@@ -189,10 +191,14 @@ public class OmahaBaseTest {
         Context targetContext = InstrumentationRegistry.getTargetContext();
         OmahaBase.setIsDisabledForTesting(false);
         mContext = new AdvancedMockContext(targetContext);
+        FeatureList.TestValues overrides = new FeatureList.TestValues();
+        overrides.addFeatureFlagOverride(ChromeFeatureList.ANONYMOUS_UPDATE_CHECKS, true);
+        FeatureList.setTestValues(overrides);
     }
 
     @After
     public void tearDown() {
+        FeatureList.setTestValues(null);
         OmahaBase.setIsDisabledForTesting(true);
     }
 
