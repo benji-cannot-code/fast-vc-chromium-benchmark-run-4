6b304b8442c362da7c806513184ed31cf92d0953
Jinsuk Kim <jinsukkim@chromium.org>
2022-01-07 02:13:46 +0000

    Android: Handle fullscreen requests canceled at pending state
    Introduces a new flag |mCanceled| in pending fullscreen option. This
    is set to true when the fullscreen request gets canceled at pending
    state, therefore browser controls needs restoring.
    
    Bug: 1270593
    Change-Id: Idd63f53317fbdc3560cee5ae374befb98a759a0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3362010
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956338}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabBrowserControlsConstraintsHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabBrowserControlsConstraintsHelper.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
 import org.chromium.base.UserData;
@@ -183,6 +184,11 @@ public class TabBrowserControlsConstraintsHelper implements UserData {
     @BrowserControlsState
     private int getConstraints() {
         return mVisibilityDelegate == null ? BrowserControlsState.BOTH : mVisibilityDelegate.get();
+    }
+
+    @VisibleForTesting
+    public static void setForTesting(Tab tab, TabBrowserControlsConstraintsHelper helper) {
+        tab.getUserDataHost().setUserData(USER_DATA_KEY, helper);
     }
 
     @NativeMethods
--- chrome/browser/fullscreen/android/java/src/org/chromium/chrome/browser/fullscreen/FullscreenOptions.java
+++ chrome/browser/fullscreen/android/java/src/org/chromium/chrome/browser/fullscreen/FullscreenOptions.java
@@ -11,6 +11,10 @@ package org.chromium.chrome.browser.fullscreen;
 public class FullscreenOptions {
     private boolean mShowNavigationBar;
 
+    // Used by FullscreenHtmlApiHandler internally to indicate that the fullscreen request
+    // associated with this option got canceled at the pending state.
+    private boolean mCanceled;
+
     /**
      * Constructs FullscreenOptions.
      *
@@ -25,6 +29,14 @@ public class FullscreenOptions {
      */
     public boolean showNavigationBar() {
         return mShowNavigationBar;
+    }
+
+    void setCanceled() {
+        mCanceled = true;
+    }
+
+    boolean canceled() {
+        return mCanceled;
     }
 
     @Override
