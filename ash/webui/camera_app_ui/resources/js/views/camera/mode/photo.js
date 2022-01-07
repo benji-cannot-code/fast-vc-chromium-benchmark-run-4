a36286dbb50a9834d162b4d2255c215410ae4826
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-07 07:47:23 +0000

    CCA: Fix protected members not renamed in downstream js files
    In CL:3322473 some downstream .js files have left-over protected members
    not renamed properly. Fix them.
    
    Bug: b:172340451
    Test: tsc compile
    Change-Id: Ie0dd90e3e41b6990d9a40e61acc8675487a40890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372386
    Reviewed-by: Inker Kuo <inker@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956419}

--- ash/webui/camera_app_ui/resources/js/views/camera/mode/photo.js
+++ ash/webui/camera_app_ui/resources/js/views/camera/mode/photo.js
@@ -109,7 +109,7 @@ export class Photo extends ModeBase {
      * @protected
      */
     this.crosImageCapture_ =
-        new CrosImageCapture(this.stream_.getVideoTracks()[0]);
+        new CrosImageCapture(this.stream.getVideoTracks()[0]);
 
     /**
      * The observer endpoint for saving metadata.
@@ -131,9 +131,9 @@ export class Photo extends ModeBase {
    * @param {!MediaStream} stream
    */
   updatePreview(stream) {
-    this.stream_ = stream;
+    this.stream = stream;
     this.crosImageCapture_ =
-        new CrosImageCapture(this.stream_.getVideoTracks()[0]);
+        new CrosImageCapture(this.stream.getVideoTracks()[0]);
   }
 
   /**
@@ -213,7 +213,7 @@ export class Photo extends ModeBase {
    * @return {!Promise} Promise for the operation.
    */
   async addMetadataObserver() {
-    if (!this.stream_) {
+    if (!this.stream) {
       return;
     }
 
@@ -246,7 +246,7 @@ export class Photo extends ModeBase {
       this.pendingResultForMetadata_.shift()?.signal(parsedMetadata);
     };
 
-    const deviceId = this.stream_.getVideoTracks()[0].getSettings().deviceId;
+    const deviceId = this.stream.getVideoTracks()[0].getSettings().deviceId;
     this.metadataObserver_ = await deviceOperator.addMetadataObserver(
         deviceId, callback, StreamType.JPEG_OUTPUT);
   }
@@ -256,7 +256,7 @@ export class Photo extends ModeBase {
    * @return {!Promise} Promise for the operation.
    */
   async removeMetadataObserver() {
-    if (!this.stream_ || this.metadataObserver_ === null) {
+    if (!this.stream || this.metadataObserver_ === null) {
       return;
     }
 
