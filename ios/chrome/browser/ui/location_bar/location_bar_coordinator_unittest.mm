fc3b689d768f4897849e42e37b5c201a953e98c8
Mikel Astiz <mastiz@chromium.org>
2022-01-07 13:13:45 +0000

    [iOS] Migrate away from TestChromeBrowserState::CreateHistoryService()
    Tests should avoid the deprecated API in TestChromeBrowserState and
    instead should add testing factories via
    TestChromeBrowserState::Builder. This patch updates
    /ios/chrome/browser/ui/location_bar.
    
    This CL was uploaded by git cl split.
    
    R=stkhapugin@chromium.org
    
    Bug: 1106699
    Change-Id: Ib2bcfda687b4ec1cf2945d36c2223f394fa788fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372782
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956486}

--- ios/chrome/browser/ui/location_bar/location_bar_coordinator_unittest.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_coordinator_unittest.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/files/scoped_temp_dir.h"
 #include "components/omnibox/browser/test_location_bar_model.h"
 #include "components/variations/scoped_variations_ids_provider.h"
 #include "components/variations/variations_ids_provider.h"
@@ -18,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/favicon/favicon_service_factory.h"
 #include "ios/chrome/browser/favicon/ios_chrome_favicon_loader_factory.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_service_factory.h"
+#include "ios/chrome/browser/history/history_service_factory.h"
 #import "ios/chrome/browser/main/test_browser.h"
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
@@ -75,9 +75,6 @@ class LocationBarCoordinatorTest : public PlatformTest {
 
     TestChromeBrowserState::Builder test_cbs_builder;
 
-    ASSERT_TRUE(state_dir_.CreateUniqueTempDir());
-    test_cbs_builder.SetPath(state_dir_.GetPath());
-
     test_cbs_builder.AddTestingFactory(
         ios::TemplateURLServiceFactory::GetInstance(),
         ios::TemplateURLServiceFactory::GetDefaultFactory());
@@ -93,9 +90,11 @@ class LocationBarCoordinatorTest : public PlatformTest {
     test_cbs_builder.AddTestingFactory(
         ios::FaviconServiceFactory::GetInstance(),
         ios::FaviconServiceFactory::GetDefaultFactory());
+    test_cbs_builder.AddTestingFactory(
+        ios::HistoryServiceFactory::GetInstance(),
+        ios::HistoryServiceFactory::GetDefaultFactory());
 
     browser_state_ = test_cbs_builder.Build();
-    ASSERT_TRUE(browser_state_->CreateHistoryService());
 
     browser_ =
         std::make_unique<TestBrowser>(browser_state_.get(), &web_state_list_);
@@ -125,11 +124,6 @@ class LocationBarCoordinatorTest : public PlatformTest {
 
     PlatformTest::TearDown();
   }
-
-  // A state directory that outlives |task_environment_| is needed because
-  // CreateHistoryService/CreateBookmarkModel use the directory to host
-  // databases. See https://crbug.com/546640 for more details.
-  base::ScopedTempDir state_dir_;
 
   web::WebTaskEnvironment task_environment_;
   variations::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
