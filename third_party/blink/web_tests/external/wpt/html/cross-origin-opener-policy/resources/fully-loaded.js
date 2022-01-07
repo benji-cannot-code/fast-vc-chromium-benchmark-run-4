6281d694ac28912be35cb54baaaeda9ddca05f25
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-01-07 07:27:30 +0000

    Import wpt@819f257ec64f1c400d0340f288a72cf1c4796fbe
    Using wpt-import in Chromium 985b6ff0dcbe9a8a808ec65e5236edaa33b57205.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel
    Change-Id: I80c5c86102e17f31c9b28c047ec1cc4681a59cef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371682
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#956412}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/resources/fully-loaded.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-opener-policy/resources/fully-loaded.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-r// Return a promise, which resolves when new navigations aren't considered
+// Return a promise, which resolves when new navigations aren't considered
 // client-side redirects anymore.
 //
 // Note: A long `setTimeout` is used, because client-side redirect is an
