003c0143c29c95e6b2a660ac1a6620ab2bca971e
Yury Semikhatsky <yurys@chromium.org>
2022-01-08 01:46:45 +0000

    Check delegate_ for null in WebContentsView::GetTopLevelNativeWindow
    Bug: 1285467
    Change-Id: I2b136d4885c6b479810c41ddd6199c90b27a0bce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373703
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956769}

--- headless/test/headless_protocol_browsertest.cc
+++ headless/test/headless_protocol_browsertest.cc
@@ -289,6 +289,9 @@ HEADLESS_PROTOCOL_TEST(BrowserSetInitialProxyConfig,
 HEADLESS_PROTOCOL_TEST(BrowserUniversalNetworkAccess,
                        "sanity/universal-network-access.js")
 
+HEADLESS_PROTOCOL_TEST(ShowDirectoryPickerNoCrash,
+                       "sanity/show-directory-picker-no-crash.js")
+
 class HeadlessProtocolBrowserTestWithProxy
     : public HeadlessProtocolBrowserTest {
  public:
