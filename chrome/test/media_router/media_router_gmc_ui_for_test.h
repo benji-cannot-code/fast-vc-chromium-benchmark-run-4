970d4ab73f6620dd97dc8ad7f7a71cc7795cc1a8
Takumi Fujimoto <takumif@chromium.org>
2022-01-08 00:01:22 +0000

    Remove tests for local media casting
    Remove integration tests that tested the "cast file" option that is
    being removed in a subsequent CL.
    
    Bug: 1284717
    Change-Id: I116d93cca9a36ddb5d3b4fafc59959cadfd624f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355500
    Reviewed-by: John Williams <jrw@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956730}

--- chrome/test/media_router/media_router_gmc_ui_for_test.h
+++ chrome/test/media_router/media_router_gmc_ui_for_test.h
@@ -46,8 +46,6 @@ class MediaRouterGmcUiForTest
   void WaitForAnyRoute() override;
   void WaitForDialogShown() override;
   void WaitForDialogHidden() override;
-  void SetLocalFile(const GURL& file_url) override;
-  void SetLocalFileSelectionIssue(const IssueInfo& issue) override;
 
  private:
   friend class content::WebContentsUserData<MediaRouterGmcUiForTest>;
