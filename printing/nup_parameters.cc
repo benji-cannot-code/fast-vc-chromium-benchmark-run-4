0180c8c6cbdc6d56dfdd0f30914e9d0c5e3a40d9
Lei Zhang <thestig@chromium.org>
2022-01-08 00:49:19 +0000

    Simplify printing::NupParameters().
    - Get rid of Clear() as it is mostly only used in tests. Just
      instantiate more instances for testing. For production use, initialize
      member variables in the header.
    - Make SetParameters() private. Replace the default constructor with a
      2-param constructor, which passes the arguments to SetParameters().
    
    Change-Id: I007a4f5b02069a2778d124ae278d4620d8daeecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371163
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956748}

--- printing/nup_parameters.cc
+++ printing/nup_parameters.cc
@@ -13,14 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace printing {
 
-NupParameters::NupParameters() {
-  Clear();
-}
-
-void NupParameters::Clear() {
-  landscape_ = false;
-  num_pages_on_x_axis_ = 1;
-  num_pages_on_y_axis_ = 1;
+NupParameters::NupParameters(int pages_per_sheet, bool is_source_landscape) {
+  SetParameters(pages_per_sheet, is_source_landscape);
 }
 
 // static
