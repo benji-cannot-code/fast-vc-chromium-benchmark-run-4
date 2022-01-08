2196b7a2f4faa4e42f9834fe4a3c6285d88078a9
Erik Chen <erikchen@chromium.org>
2022-01-08 04:46:37 +0000

    lacros: Set window properties for chrome app windows.
    This CL ensures that all chrome app windows have their kAppID and
    kShelfID properties set.
    
          shelf icon is correctly highlighted when the app is focused.
    
    Bug: 1269152
    Test: Install a chrome app with the CL patched in and confirm that the
    Change-Id: Ie026892157f37f39df6aa1da05b57189e220fe64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373664
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956803}

--- chrome/browser/ui/ash/shelf/standalone_browser_extension_app_shelf_item_controller.cc
+++ chrome/browser/ui/ash/shelf/standalone_browser_extension_app_shelf_item_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <utility>
 
+#include "ash/public/cpp/window_properties.h"
 #include "ash/shell.h"
 #include "ash/wm/window_util.h"
 #include "base/bind.h"
@@ -340,6 +341,10 @@ void StandaloneBrowserExtensionAppShelfItemController::InitWindowStatus(
   }
   window_status_[window] = state;
   UpdateInstance(window, state);
+
+  // Also update the window properties in exo.
+  window->SetProperty(ash::kAppIDKey, shelf_id().app_id);
+  window->SetProperty(ash::kShelfIDKey, shelf_id().Serialize());
 }
 
 void StandaloneBrowserExtensionAppShelfItemController::UpdateInstance(
