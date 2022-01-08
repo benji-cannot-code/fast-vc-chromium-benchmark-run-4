53990b216dc70cf7d60b9ec659909dc717f9e655
Ayu Ishii <ayui@chromium.org>
2022-01-08 04:46:07 +0000

    StorageBuckets: Deletion callback to return after DB bucket deletion
    This change updates Bucket deletion logic to return QuotaStatus
    after deletion from QuotaDatabase. StorageKeyDeletion did not wait for
    deletion to complete because entries for storage_key in the
    QuotaDatabase were created and removed silently. But this should be
    different for buckets because bucket data will continue to appear for
    the Buckets API as long as it still exists in the QuotaDatabase.
    Therefore deletion success should be returned after we know deletion
    from the QuotaDatabase was successful.
    
    Change-Id: Ic229678fa38125945883a446bccc095f76820faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3272085
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956802}

--- storage/browser/quota/quota_manager_impl.h
+++ storage/browser/quota/quota_manager_impl.h
@@ -419,6 +419,8 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManagerImpl
     eviction_disabled_ = disable;
   }
 
+  void SetQuotaDatabaseForTesting(std::unique_ptr<QuotaDatabase> database);
+
  protected:
   ~QuotaManagerImpl() override;
   void SetQuotaChangeCallbackForTesting(
@@ -514,16 +516,19 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManagerImpl
   // bucket from the bucket table.
   void DeleteBucketDataInternal(const BucketLocator& bucket,
                                 QuotaClientTypes quota_client_types,
-                                bool is_eviction,
                                 StatusCallback callback);
+  // Cleans up BucketDataDeleter tasks that have completed.
+  void DidDeleteBucketData();
 
   // Methods for eviction logic.
   void StartEviction();
   void DeleteStorageKeyFromDatabase(const blink::StorageKey& storage_key,
                                     blink::mojom::StorageType type);
-  void DeleteBucketFromDatabase(BucketId bucket_id, bool is_eviction);
+  void DeleteBucketFromDatabase(BucketId bucket_id,
+                                base::OnceCallback<void(QuotaError)> callback);
 
-  void DidBucketDataEvicted(blink::mojom::QuotaStatusCode status);
+  void DidBucketDataEvicted(QuotaDatabase::BucketTableEntry entry,
+                            blink::mojom::QuotaStatusCode status);
 
   void ReportHistogram();
   void DidGetTemporaryGlobalUsageForHistogram(int64_t usage,
@@ -548,6 +553,10 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManagerImpl
   void EvictBucketData(const BucketLocator& bucket,
                        StatusCallback callback) override;
   void GetEvictionRoundInfo(EvictionRoundInfoCallback callback) override;
+
+  void DidGetBucketInfoForEviction(
+      const BucketLocator& bucket,
+      QuotaErrorOr<QuotaDatabase::BucketTableEntry> result);
 
   void DidGetEvictionRoundInfo();
 
@@ -721,6 +730,7 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManagerImpl
   GetVolumeInfoFn get_volume_info_fn_;
 
   std::unique_ptr<EvictionRoundInfoHelper> eviction_helper_;
+  std::vector<std::unique_ptr<BucketDataDeleter>> bucket_data_deleters_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
