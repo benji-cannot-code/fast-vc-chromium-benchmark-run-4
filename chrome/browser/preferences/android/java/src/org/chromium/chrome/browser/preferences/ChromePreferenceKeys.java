99d88a6d898fb227b542e3aed802fe16cba78b0f
Hailey Wang <haileywang@google.com>
2022-01-10 20:57:44 +0000

    [QueryTiles] Record segmentation model result
    Record a histogram of the result of segmentation model compared to the
    result from code logic.
    
    Add config param to be able to use segmentation model when ready.
    
    Bug: 1261158
    Change-Id: I5cb08547d0523c6bf684206d9d369913f269f18d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3324688
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#957218}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeys.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeys.java
@@ -754,6 +754,12 @@ public final class ChromePreferenceKeys {
     public static final String QUERY_TILES_SHOW_ON_NTP = "Chrome.Querytiles.ShowOnNTP";
 
     /**
+     * Keys used to store result from segmentation model of showing query tiles on NTP.
+     */
+    public static final String QUERY_TILES_SHOW_SEGMENTATION_RESULT =
+            "Chrome.QueryTiles.ShowSegmentationResult";
+
+    /**
      * Keys used to store user actions for behavioral targeting of showing Start surface on startup.
      */
     public static final String PRIMARY_ACCOUNT_SYNC = "Chrome.StartSurface.PrimaryAccountSync";
@@ -1106,6 +1112,7 @@ public final class ChromePreferenceKeys {
                 QUERY_TILES_NUM_RECENT_MV_TILE_CLICKS,
                 QUERY_TILES_NUM_RECENT_QUERY_TILE_CLICKS,
                 QUERY_TILES_SHOW_ON_NTP,
+                QUERY_TILES_SHOW_SEGMENTATION_RESULT,
                 REGULAR_TAB_COUNT,
                 SETTINGS_SAFETY_CHECK_LAST_RUN_TIMESTAMP,
                 SETTINGS_SAFETY_CHECK_RUN_COUNTER,
