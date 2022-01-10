42c1a9ed212416909dfdd52c71f6ba1f5dc4c3fc
Jose Dapena Paz <jdapena@igalia.com>
2022-01-10 19:25:53 +0000

    IWYU: missing include for using memset in ScopedResState.
    Fix build error with libstdc++:
        ../../net/dns/public/scoped_res_state.cc: In constructor ‘net::ScopedResState::ScopedResState()’:
        ../../net/dns/public/scoped_res_state.cc:20:3: error: ‘memset’ was not declared in this scope
           20 |   memset(&res_, 0, sizeof(res_));
              |   ^~~~~~
    
    Bug: 957519
    Change-Id: Id6202959ebb15b7be25d966843ce25214b3407d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3377522
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#957142}

--- net/dns/public/scoped_res_state.cc
+++ net/dns/public/scoped_res_state.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/dns/public/scoped_res_state.h"
 
+#include <cstring>
 #include <memory>
 
 #include "base/check.h"
