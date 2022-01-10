7722967d6b1fe2a21e0f2a6697f286f19a7e745d
James Cook <jamescook@chromium.org>
2022-01-10 20:14:44 +0000

    ash: Fix AppListTest with productivity launcher enabled
    - Query the bubble presenter when using productivity launcher
    - Fix display ID being invalid during OnVisibilityChanged() observer
      notification
    
    Bug: 1280300
    Test: ash_unittests
    Change-Id: I5f9c00763bd10909fa502e1817fd803b1d30d64d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378449
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957162}

--- ash/app_list/app_list_unittest.cc
+++ ash/app_list/app_list_unittest.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/app_list/app_list_bubble_presenter.h"
 #include "ash/app_list/app_list_controller_impl.h"
 #include "ash/app_list/app_list_presenter_impl.h"
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shelf/home_button.h"
 #include "ash/shelf/shelf.h"
@@ -16,14 +18,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
+#include "base/test/scoped_feature_list.h"
+#include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/window.h"
 
 namespace ash {
+namespace {
 
-using AppListTest = AshTestBase;
+// Returns visibility from the presenter's perspective.
+bool GetPresenterVisibility() {
+  auto* controller = Shell::Get()->app_list_controller();
+  if (features::IsProductivityLauncherEnabled())
+    return controller->bubble_presenter_for_test()->IsShowing();
+  return controller->presenter()->GetTargetVisibility();
+}
+
+}  // namespace
+
+class AppListTest : public AshTestBase,
+                    public testing::WithParamInterface<bool> {
+ public:
+  AppListTest() {
+    feature_list_.InitWithFeatureState(features::kProductivityLauncher,
+                                       GetParam());
+  }
+  base::test::ScopedFeatureList feature_list_;
+};
+
+INSTANTIATE_TEST_SUITE_P(ProductivityLauncher, AppListTest, testing::Bool());
 
 // An integration test to toggle the app list by pressing the shelf button.
-TEST_F(AppListTest, PressHomeButtonToShowAndDismiss) {
+TEST_P(AppListTest, PressHomeButtonToShowAndDismiss) {
   aura::Window* root_window = Shell::GetPrimaryRootWindow();
   Shelf* shelf = Shelf::ForWindow(root_window);
   ShelfWidget* shelf_widget = shelf->shelf_widget();
@@ -38,14 +63,13 @@ TEST_F(AppListTest, PressHomeButtonToShowAndDismiss) {
 
   // Click the home button to show the app list.
   auto* controller = Shell::Get()->app_list_controller();
-  auto* presenter = controller->presenter();
   EXPECT_FALSE(controller->GetTargetVisibility(GetPrimaryDisplay().id()));
-  EXPECT_FALSE(presenter->GetTargetVisibility());
+  EXPECT_FALSE(GetPresenterVisibility());
   EXPECT_EQ(0u, app_list_container->children().size());
   EXPECT_FALSE(home_button->IsShowingAppList());
 
   LeftClickOn(home_button);
-  EXPECT_TRUE(presenter->GetTargetVisibility());
+  EXPECT_TRUE(GetPresenterVisibility());
   EXPECT_EQ(1u, app_list_container->children().size());
   EXPECT_TRUE(home_button->IsShowingAppList());
 
@@ -58,7 +82,7 @@ TEST_F(AppListTest, PressHomeButtonToShowAndDismiss) {
 
 // Tests that the app list gets toggled by pressing the shelf button on
 // secondary display.
-TEST_F(AppListTest, PressHomeButtonToShowAndDismissOnSecondDisplay) {
+TEST_P(AppListTest, PressHomeButtonToShowAndDismissOnSecondDisplay) {
   UpdateDisplay("1024x768,1024x768");
   aura::Window* root_window =
       Shell::GetRootWindowForDisplayId(GetSecondaryDisplay().id());
@@ -75,17 +99,16 @@ TEST_F(AppListTest, PressHomeButtonToShowAndDismissOnSecondDisplay) {
 
   // Click the home button to show the app list.
   auto* controller = Shell::Get()->app_list_controller();
-  auto* presenter = controller->presenter();
   EXPECT_FALSE(controller->GetTargetVisibility(GetPrimaryDisplay().id()));
   EXPECT_FALSE(controller->GetTargetVisibility(GetSecondaryDisplay().id()));
-  EXPECT_FALSE(presenter->GetTargetVisibility());
+  EXPECT_FALSE(GetPresenterVisibility());
   EXPECT_EQ(0u, app_list_container->children().size());
   EXPECT_FALSE(home_button->IsShowingAppList());
 
   LeftClickOn(home_button);
   EXPECT_FALSE(controller->GetTargetVisibility(GetPrimaryDisplay().id()));
   EXPECT_TRUE(controller->GetTargetVisibility(GetSecondaryDisplay().id()));
-  EXPECT_TRUE(presenter->GetTargetVisibility());
+  EXPECT_TRUE(GetPresenterVisibility());
   EXPECT_EQ(1u, app_list_container->children().size());
   EXPECT_TRUE(home_button->IsShowingAppList());
 
