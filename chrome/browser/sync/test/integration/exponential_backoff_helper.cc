0572e9b2a6e7f4759d014468de6b88a544331eb9
Mikel Astiz <mastiz@chromium.org>
2022-01-10 10:57:17 +0000

    [sync] Move test-only backoff checker to (new) helper file
    No behavioral changes: This patch renames retry_verifier.h/.cc to
    exponential_backoff_helper.h/.cc and moves the related checker
    ExponentialBackoffChecker as is to the same file. Future patches intend
    to refactor the two and/or merge them, since ExponentialBackoffChecker
    is the only user of RetryVerifier, and having this artifical split has
    several downsides.
    
    Change-Id: I399bbb496b35dcc06adf646bee5d8eafefc639cb
    Bug: 1170609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3376343
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957002}

--- chrome/browser/sync/test/integration/exponential_backoff_helper.cc
+++ chrome/browser/sync/test/integration/exponential_backoff_helper.cc
@@ -3,15 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/sync/test/integration/retry_verifier.h"
+#include "chrome/browser/sync/test/integration/exponential_backoff_helper.h"
 
 #include <string.h>
 
 #include <algorithm>
+#include <ostream>
 
 #include "base/logging.h"
 #include "components/sync/engine/cycle/sync_cycle_snapshot.h"
 #include "components/sync/engine/polling_constants.h"
+
+namespace exponential_backoff_helper {
 
 namespace {
 
@@ -108,3 +111,23 @@ void RetryVerifier::VerifyRetryInterval(const syncer::SyncCycleSnapshot& snap) {
     return;
   }
 }
+
+ExponentialBackoffChecker::ExponentialBackoffChecker(
+    syncer::SyncServiceImpl* sync_service)
+    : SingleClientStatusChangeChecker(sync_service) {
+  const syncer::SyncCycleSnapshot& snap =
+      service()->GetLastCycleSnapshotForDebugging();
+  retry_verifier_.Initialize(snap);
+}
+
+bool ExponentialBackoffChecker::IsExitConditionSatisfied(std::ostream* os) {
+  *os << "Verifying backoff intervals (" << retry_verifier_.retry_count() << "/"
+      << RetryVerifier::kMaxRetry << ")";
+
+  const syncer::SyncCycleSnapshot& snap =
+      service()->GetLastCycleSnapshotForDebugging();
+  retry_verifier_.VerifyRetryInterval(snap);
+  return (retry_verifier_.done() && retry_verifier_.Succeeded());
+}
+
+}  // namespace exponential_backoff_helper
