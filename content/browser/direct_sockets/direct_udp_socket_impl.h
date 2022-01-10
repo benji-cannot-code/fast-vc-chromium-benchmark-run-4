e7bfb5f404a2cadc1be0f86f96d137f11971793e
Eric Willigers <ericwilligers@chromium.org>
2022-01-10 01:23:25 +0000

    Direct Sockets: Unit test for DirectUDPSocketImpl::Send
    We tests that DirectUDPSocketImpl forwards send requests to the
    underlying DirectUDPSocket.
    
    Bug: 1119620
    Change-Id: I48883f30398cf8254c81744d432c28c3a40617c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3367579
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#956935}

--- content/browser/direct_sockets/direct_udp_socket_impl.h
+++ content/browser/direct_sockets/direct_udp_socket_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_DIRECT_SOCKETS_DIRECT_UDP_SOCKET_IMPL_H_
 #define CONTENT_BROWSER_DIRECT_SOCKETS_DIRECT_UDP_SOCKET_IMPL_H_
 
+#include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "net/base/ip_endpoint.h"
@@ -18,7 +19,8 @@ namespace content {
 // Forwards requests from the Renderer to the connected UDPSocket.
 // We do not expose the UDPSocket directly to the Renderer, as that
 // would allow a compromised Renderer to contact other end points.
-class DirectUDPSocketImpl : public blink::mojom::DirectUDPSocket {
+class CONTENT_EXPORT DirectUDPSocketImpl
+    : public blink::mojom::DirectUDPSocket {
  public:
   typedef network::mojom::UDPSocket::ConnectCallback ConnectCallback;
 
@@ -32,15 +34,14 @@ class DirectUDPSocketImpl : public blink::mojom::DirectUDPSocket {
                network::mojom::UDPSocketOptionsPtr options,
                ConnectCallback callback);
 
- protected:
-  // blink:mojom::DirectUDPSocket:
+  // blink::mojom::DirectUDPSocket:
   void ReceiveMore(uint32_t num_additional_datagrams) override;
   void Send(base::span<const uint8_t> data, SendCallback callback) override;
   void Close() override;
 
+ private:
   void OnDisconnect();
 
- private:
   mojo::Remote<network::mojom::UDPSocket> remote_;
 };
 
