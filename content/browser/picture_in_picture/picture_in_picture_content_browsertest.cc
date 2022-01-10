8da1909a9c35d5a793d17638159e9045d50a7982
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2022-01-10 06:21:19 +0000

    Restore browser client last in test
    By moving the ContentBrowserClient restoration to the test fixture
    destructor we avoid a data race between SetBrowserClientForTesting()
    called by the test on the main thread and a FileURLLoader task on a
    helper thread.
    
    Bug: 1255230
    Change-Id: I6487c9160f55b678d257e47c7f25921353a819a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373102
    Auto-Submit: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#956964}

--- content/browser/picture_in_picture/picture_in_picture_content_browsertest.cc
+++ content/browser/picture_in_picture/picture_in_picture_content_browsertest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
-#include "build/build_config.h"
 #include "content/browser/picture_in_picture/picture_in_picture_service_impl.h"
 #include "content/browser/picture_in_picture/picture_in_picture_window_controller_impl.h"
 #include "content/public/browser/content_browser_client.h"
@@ -149,6 +148,11 @@ class TestWebContentsDelegate : public WebContentsDelegate {
 
 class PictureInPictureContentBrowserTest : public ContentBrowserTest {
  public:
+  ~PictureInPictureContentBrowserTest() override {
+    if (old_browser_client_.has_value())
+      SetBrowserClientForTesting(old_browser_client_.value());
+  }
+
   void SetUpCommandLine(base::CommandLine* command_line) override {
     ContentBrowserTest::SetUpCommandLine(command_line);
 
@@ -165,12 +169,6 @@ class PictureInPictureContentBrowserTest : public ContentBrowserTest {
 
     web_contents_delegate_ = std::make_unique<TestWebContentsDelegate>(shell());
     shell()->web_contents()->SetDelegate(web_contents_delegate_.get());
-  }
-
-  void TearDownOnMainThread() override {
-    SetBrowserClientForTesting(old_browser_client_);
-
-    ContentBrowserTest::TearDownOnMainThread();
   }
 
   void WaitForPlaybackState(OverlayWindow::PlaybackState playback_state,
@@ -211,7 +209,7 @@ class PictureInPictureContentBrowserTest : public ContentBrowserTest {
 
  private:
   std::unique_ptr<TestWebContentsDelegate> web_contents_delegate_;
-  raw_ptr<ContentBrowserClient> old_browser_client_ = nullptr;
+  absl::optional<raw_ptr<ContentBrowserClient>> old_browser_client_;
   TestContentBrowserClient content_browser_client_;
 };
 
@@ -540,15 +538,8 @@ IN_PROC_BROWSER_TEST_F(MediaSessionPictureInPictureContentBrowserTest,
 // Tests Media Session action availability upon reaching the end of stream by
 // verifying that the "nexttrack" action can be invoked after playing through
 // to the end of media.
-#if defined(OS_LINUX) && defined(THREAD_SANITIZER)
-#define MAYBE_ActionAvailableAfterEndOfStreamAndSrcUpdate \
-  DISABLED_ActionAvailableAfterEndOfStreamAndSrcUpdate
-#else
-#define MAYBE_ActionAvailableAfterEndOfStreamAndSrcUpdate \
-  ActionAvailableAfterEndOfStreamAndSrcUpdate
-#endif
 IN_PROC_BROWSER_TEST_F(MediaSessionPictureInPictureContentBrowserTest,
-                       MAYBE_ActionAvailableAfterEndOfStreamAndSrcUpdate) {
+                       ActionAvailableAfterEndOfStreamAndSrcUpdate) {
   ASSERT_TRUE(NavigateToURL(
       shell(), GetTestUrl("media/picture_in_picture", "one-video.html")));
 
