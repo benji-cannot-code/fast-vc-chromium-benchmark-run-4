2c52c361eac88dea084c2a24ef784ba0a0db3410
Joey Arhar <jarhar@chromium.org>
2022-01-10 17:30:46 +0000

    Limit custom validation message length for OOPIFs
    Fixed: 1261191
    Change-Id: I4dbcab19a213de6f730d8daf5b2d46ef0b77f684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3340071
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957097}

--- third_party/blink/renderer/core/page/validation_message_client_impl.cc
+++ third_party/blink/renderer/core/page/validation_message_client_impl.cc
@@ -42,6 +42,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+// The max length of 256 is also used by other browsers:
+// https://bugs.chromium.org/p/chromium/issues/detail?id=1261191#c17
+constexpr int kMaxValidationStringLength = 256;
+}  // namespace
+
 ValidationMessageClientImpl::ValidationMessageClientImpl(Page& page)
     : page_(&page), current_anchor_(nullptr) {}
 
@@ -53,16 +59,26 @@ LocalFrameView* ValidationMessageClientImpl::CurrentView() {
 
 void ValidationMessageClientImpl::ShowValidationMessage(
     const Element& anchor,
-    const String& message,
+    const String& original_message,
     TextDirection message_dir,
     const String& sub_message,
     TextDirection sub_message_dir) {
-  if (message.IsEmpty()) {
+  if (original_message.IsEmpty()) {
     HideValidationMessage(anchor);
     return;
   }
   if (!anchor.GetLayoutObject())
     return;
+
+  // If this frame is cross origin to the main frame, then shorten the
+  // validation message to prevent validation popups that cover too much of the
+  // main frame.
+  String message = original_message;
+  if (original_message.length() > kMaxValidationStringLength &&
+      anchor.GetDocument().GetFrame()->IsCrossOriginToMainFrame()) {
+    message = original_message.Substring(0, kMaxValidationStringLength) + "...";
+  }
+
   if (current_anchor_)
     HideValidationMessageImmediately(*current_anchor_);
   current_anchor_ = &anchor;
