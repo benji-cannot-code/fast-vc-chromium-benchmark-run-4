d22b8689e6606204e691b50c8807afd725a442f5
Pavol Marko <pmarko@google.com>
2022-01-10 22:52:54 +0000

    Improve logging in client_cert_resolver.cc and policy_applicator.cc
    This will help debug issues with client cert resolving / network policy
    application.
    
    Example output:
    2021-12-30T09:43:24.488212Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:24.488] Network: EVENT: client_cert_resolver.cc:750 Start task for resolving client cert patterns for wifi_802_1x_5[guid='testwifitls1-Wifi'], wifi_802_1x_0[guid='testwifi_eap_tls_global-Wifi'], wifi_802_1x_17[guid='testwifitls2-Wifi'], wifi_802_1x_18[guid='testwifitls3-Wifi'], wifi_802_1x_19[guid='testwifitls4-Wifi'], wifi_802_1x_20[guid='testwifitls5-Wifi'], wifi_802_1x_21[guid='testwifitls6-Wifi']
    2021-12-30T09:43:25.360183Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:25.360] Network: EVENT: client_cert_resolver.cc:810 Configuring certificate for network: wifi_802_1x_5[guid='testwifitls1-Wifi']
    2021-12-30T09:43:25.360463Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:25.360] Network: EVENT: client_cert_resolver.cc:810 Configuring certificate for network: wifi_802_1x_17[guid='testwifitls2-Wifi']
    2021-12-30T09:43:25.360626Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:25.360] Network: EVENT: client_cert_resolver.cc:810 Configuring certificate for network: wifi_802_1x_18[guid='testwifitls3-Wifi']
    2021-12-30T09:43:25.360784Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:25.360] Network: EVENT: client_cert_resolver.cc:810 Configuring certificate for network: wifi_802_1x_19[guid='testwifitls4-Wifi']
    2021-12-30T09:43:25.361196Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:25.361] Network: EVENT: client_cert_resolver.cc:810 Configuring certificate for network: wifi_802_1x_20[guid='testwifitls5-Wifi']
    2021-12-30T09:43:25.361775Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:25.361] Network: EVENT: client_cert_resolver.cc:810 Configuring certificate for network: wifi_802_1x_21[guid='testwifitls6-Wifi']
    2021-12-30T09:43:25.362149Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:43:25.361] Network: EVENT: client_cert_resolver.cc:838 Summary: wifi_802_1x_5[guid='testwifitls1-Wifi']:match,identity='0MV891AJ202032', wifi_802_1x_0[guid='testwifi_eap_tls_global-Wifi']:match,no_change, wifi_802_1x_17[guid='testwifitls2-Wifi']:match,identity='0MV891AJ202032', wifi_802_1x_18[guid='testwifitls3-Wifi']:match,identity='0MV891AJ202032', wifi_802_1x_19[guid='testwifitls4-Wifi']:match,identity='0MV891AJ202032', wifi_802_1x_20[guid='testwifitls5-Wifi']:match,identity='0MV891AJ202032', wifi_802_1x_21[guid='testwifitls6-Wifi']:match,identity='0MV891AJ202032'
    
    2021-12-30T09:42:47.340511Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:42:47.340] Network: EVENT: policy_applicator.cc:332 Updating previously managed configuration with the updated policy testwifi_peap_global-Wifi.
    2021-12-30T09:42:47.680865Z VERBOSE1 chrome[5844:5844]: [device_event_log_impl.cc(216)] [10:42:47.680] Network: EVENT: policy_applicator.cc:448 Creating new configuration managed by policy asd-Ethernet in profile NetworkProfile(SHARED, /profile/default).
    
    Bug: b:209084821
    Change-Id: I03beddcb2953e9189e4908a401bae2c8692ea9aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3358245
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957290}

--- chromeos/network/client_cert_resolver.cc
+++ chromeos/network/client_cert_resolver.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/strings/string_util.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/time/clock.h"
@@ -40,6 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
+
+std::string GetNetworkIdWithGuid(const NetworkState* network_state) {
+  return NetworkId(network_state) + "[guid='" + network_state->guid() + "']";
+}
 
 // Global override for the getter function. This is used for testing purposes.
 // See SetProvisioningIdForCertGetterForTesting for details.
@@ -628,7 +633,7 @@ void ClientCertResolver::NetworkConnectionStateChanged(
     return;
   if (!network->IsConnectingOrConnected()) {
     NET_LOG(EVENT) << "ClientCertResolver: ConnectionStateChanged: "
-                   << NetworkId(network);
+                   << GetNetworkIdWithGuid(network);
     ResolveNetworks(NetworkStateHandler::NetworkStateList(1, network));
   }
 }
@@ -665,7 +670,7 @@ void ClientCertResolver::PolicyAppliedToNetwork(
     return;
   }
   NET_LOG(EVENT) << "ClientCertResolver: PolicyAppliedToNetwork: "
-                 << NetworkId(network);
+                 << GetNetworkIdWithGuid(network);
   NetworkStateHandler::NetworkStateList networks;
   networks.push_back(network);
   ResolveNetworks(networks);
@@ -675,6 +680,7 @@ void ClientCertResolver::ResolveNetworks(
     const NetworkStateHandler::NetworkStateList& networks) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   std::vector<NetworkAndCertConfig> networks_to_resolve;
+  std::vector<std::string> networks_to_resolve_info;
 
   // Filter networks with ClientCertPattern, ClientCertRef, or
   // ProvisioningProfileId. As these can only be set by policy, we check there.
@@ -713,6 +719,7 @@ void ClientCertResolver::ResolveNetworks(
 
     networks_to_resolve.push_back(
         NetworkAndCertConfig(network->path(), cert_config));
+    networks_to_resolve_info.push_back(GetNetworkIdWithGuid(network));
   }
 
   if (networks_to_resolve.empty()) {
@@ -731,7 +738,8 @@ void ClientCertResolver::ResolveNetworks(
     return;
   }
 
-  VLOG(2) << "Start task for resolving client cert patterns.";
+  NET_LOG(EVENT) << "Start task for resolving client cert patterns for "
+                 << base::JoinString(networks_to_resolve_info, ", ");
   resolve_task_running_ = true;
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE,
@@ -766,11 +774,23 @@ void ClientCertResolver::ResolvePendingNetworks() {
 void ClientCertResolver::ConfigureCertificates(
     std::vector<NetworkAndMatchingCert> matches) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  std::vector<std::string> resolved_networks_info;
   for (const NetworkAndMatchingCert& match : matches) {
+    const NetworkState* network_state =
+        network_state_handler_->GetNetworkStateFromServicePath(
+            match.service_path, /*configured_only=*/true);
+    if (!network_state) {
+      resolved_networks_info.push_back(
+          "Skipping " + match.service_path +
+          " because it is not configured anymore.");
+      continue;
+    }
     MatchingCertAndResolveStatus& network_status =
         networks_status_[match.service_path];
     if (network_status.resolve_status == ResolveStatus::kResolved &&
         network_status.matching_cert == match.matching_cert) {
+      resolved_networks_info.push_back(GetNetworkIdWithGuid(network_state) +
+                                       ":match,no_change");
       // The same certificate was configured in the last ConfigureCertificates
       // call, so don't do anything for this network.
       continue;
@@ -780,7 +800,7 @@ void ClientCertResolver::ConfigureCertificates(
     network_properties_changed_ = true;
 
     NET_LOG(EVENT) << "Configuring certificate for network: "
-                   << NetworkPathId(match.service_path);
+                   << GetNetworkIdWithGuid(network_state);
 
     base::DictionaryValue shill_properties;
     if (match.matching_cert.has_value()) {
@@ -788,11 +808,16 @@ void ClientCertResolver::ConfigureCertificates(
       client_cert::SetShillProperties(
           match.cert_config_type, matching_cert.key_slot_id,
           matching_cert.pkcs11_id, &shill_properties);
+      resolved_networks_info.push_back(GetNetworkIdWithGuid(network_state) +
+                                       ":match,identity='" +
+                                       matching_cert.identity + "'");
       if (!matching_cert.identity.empty()) {
         shill_properties.SetKey(shill::kEapIdentityProperty,
                                 base::Value(matching_cert.identity));
       }
     } else {
+      resolved_networks_info.push_back(GetNetworkIdWithGuid(network_state) +
+                                       ":no_match");
       client_cert::SetEmptyShillProperties(match.cert_config_type,
                                            &shill_properties);
     }
@@ -801,6 +826,9 @@ void ClientCertResolver::ConfigureCertificates(
         base::DoNothing(), base::BindOnce(&LogError, match.service_path));
     network_state_handler_->RequestUpdateForNetwork(match.service_path);
   }
+
+  NET_LOG(EVENT) << "Summary: "
+                 << base::JoinString(resolved_networks_info, ", ");
   resolve_task_running_ = false;
   if (queued_networks_to_resolve_.empty())
     NotifyResolveRequestCompleted();
