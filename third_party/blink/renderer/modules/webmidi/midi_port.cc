aef3bc894c00677f78ef9d7616786636710e3661
Vadym Kargin <vadym.kargin@accenture.com>
2022-01-10 21:13:12 +0000

    Replace the old ToV8 with new ToV8Traits interface in the Web MIDI module.
    This update is part of work on replacing the old ToV8 interface with new ToV8Traits<IDLType>::ToV8 interface.
    
    Issue: Replace ToV8 with ToV8Traits<IDLType>::ToV8
    Bug: 1199627
    Change-Id: Id1da38cbb70de3b4247b607a89b0a07ebdcf1ec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3307536
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Vadym Kargin <vadym.kargin@accenture.com>
    Cr-Commit-Position: refs/heads/main@{#957241}

--- third_party/blink/renderer/modules/webmidi/midi_port.cc
+++ third_party/blink/renderer/modules/webmidi/midi_port.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/modules/webmidi/midi_access.h"
@@ -236,9 +237,9 @@ void MIDIPort::CloseAsynchronously(ScriptPromiseResolver* resolver) {
 }
 
 ScriptPromise MIDIPort::Accept(ScriptState* script_state) {
-  return ScriptPromise::Cast(script_state,
-                             ToV8(this, script_state->GetContext()->Global(),
-                                  script_state->GetIsolate()));
+  return ScriptPromise::Cast(
+      script_state,
+      ToV8Traits<MIDIPort>::ToV8(script_state, this).ToLocalChecked());
 }
 
 void MIDIPort::SetStates(PortState state, ConnectionState connection) {
