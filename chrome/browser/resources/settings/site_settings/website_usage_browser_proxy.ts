0972ae0d5b23bb93dc3026fe76f471de531c4d3a
sauski <sauski@google.com>
2022-01-10 20:17:03 +0000

    CHIPS: Correct remaining usage of clearUsage
    CL changes the remaining usage of 'clearUsage' to the appropriate
    'clearUnpartitionedUsage' for the site details page.
    
    Bug: 1279760
    Change-Id: Ie4bf7b33e82ec8b6fddc4768796c77cf08527dad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378069
    Auto-Submit: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#957167}

--- chrome/browser/resources/settings/site_settings/website_usage_browser_proxy.ts
+++ chrome/browser/resources/settings/site_settings/website_usage_browser_proxy.ts
@@ -14,7 +14,7 @@ export class WebsiteUsageBrowserProxyImpl implements WebsiteUsageBrowserProxy {
   }
 
   clearUsage(origin: string) {
-    chrome.send('clearUsage', [origin]);
+    chrome.send('clearUnpartitionedUsage', [origin]);
   }
 
   static getInstance(): WebsiteUsageBrowserProxy {
