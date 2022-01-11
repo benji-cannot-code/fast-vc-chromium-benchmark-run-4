7abc68d87d5c01ee3c74dd458473d54b10bec078
Tibor Goldschwendt <tiborg@chromium.org>
2022-01-11 02:14:34 +0000

    [ntp][modules] Use client-side string for recipes module title
    This localizes the title into all Chrome languages.
    
    Bug: 1286060
    Change-Id: I4a0c09cdd3b096c85435276e87d0f0c869653469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379606
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Mohamad Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957381}

--- chrome/browser/resources/new_tab_page/modules/task_module/module.js
+++ chrome/browser/resources/new_tab_page/modules/task_module/module.js
@@ -45,6 +45,12 @@ class TaskModuleElement extends mixinBehaviors
       task: Object,
 
       /** @private {string} */
+      title_: {
+        type: String,
+        computed: 'computeTitle_(taskModuleType, task)',
+      },
+
+      /** @private {string} */
       dismissName_: {
         type: String,
         computed: 'computeDismissName_(taskModuleType, task)',
@@ -62,6 +68,21 @@ class TaskModuleElement extends mixinBehaviors
     super();
     /** @type {IntersectionObserver} */
     this.intersectionObserver_ = null;
+  }
+
+  /**
+   * @return {string}
+   * @private
+   */
+  computeTitle_() {
+    switch (this.taskModuleType) {
+      case taskModule.mojom.TaskModuleType.kRecipe:
+        return loadTimeData.getString('modulesRecipeTasksSentence');
+      case taskModule.mojom.TaskModuleType.kShopping:
+        return this.task.title;
+      default:
+        return '';
+    }
   }
 
   /**
