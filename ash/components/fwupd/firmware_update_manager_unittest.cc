c4dae432478af6224cdd1eb25c540a0213f63b65
Michael Checo <michaelcheco@google.com>
2022-01-11 19:35:20 +0000

    Firmware UI: Request updates after registering an observer
    - This change also removes the "HasPendingUpdates()" DCHECK
      in RequestAllUpdates.
    
    Bug: 1252981
    Change-Id: I61bc7a2f0ff2c94344fbd9d133b1d4678286f6b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371636
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#957660}

--- ash/components/fwupd/firmware_update_manager_unittest.cc
+++ ash/components/fwupd/firmware_update_manager_unittest.cc
@@ -395,7 +395,6 @@ TEST_F(FirmwareUpdateManagerTest, RequestAllUpdatesNoDevices) {
       .WillRepeatedly(Invoke(this, &FirmwareUpdateManagerTest::OnMethodCalled));
 
   dbus_responses_.push_back(CreateEmptyDeviceResponse());
-  firmware_update_manager_->RequestAllUpdates();
   FakeUpdateObserver update_observer;
   SetupObserver(&update_observer);
   const std::vector<firmware_update::mojom::FirmwareUpdatePtr>& updates =
@@ -410,7 +409,6 @@ TEST_F(FirmwareUpdateManagerTest, RequestAllUpdatesOneDeviceNoUpdates) {
 
   dbus_responses_.push_back(CreateOneDeviceResponse());
   dbus_responses_.push_back(CreateNoUpdateResponse());
-  firmware_update_manager_->RequestAllUpdates();
   FakeUpdateObserver update_observer;
   SetupObserver(&update_observer);
   const std::vector<firmware_update::mojom::FirmwareUpdatePtr>& updates =
@@ -426,7 +424,6 @@ TEST_F(FirmwareUpdateManagerTest, RequestAllUpdatesOneDeviceOneUpdate) {
 
   dbus_responses_.push_back(CreateOneDeviceResponse());
   dbus_responses_.push_back(CreateOneUpdateResponse());
-  firmware_update_manager_->RequestAllUpdates();
   FakeUpdateObserver update_observer;
   SetupObserver(&update_observer);
   const std::vector<firmware_update::mojom::FirmwareUpdatePtr>& updates =
@@ -453,7 +450,6 @@ TEST_F(FirmwareUpdateManagerTest, RequestAllUpdatesTwoDeviceOneWithUpdate) {
   dbus_responses_.push_back(CreateTwoDeviceResponse());
   dbus_responses_.push_back(CreateNoUpdateResponse());
   dbus_responses_.push_back(CreateOneUpdateResponse());
-  firmware_update_manager_->RequestAllUpdates();
   FakeUpdateObserver update_observer;
   SetupObserver(&update_observer);
   base::RunLoop().RunUntilIdle();
