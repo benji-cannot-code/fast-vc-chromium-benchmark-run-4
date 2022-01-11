0a7f2afda956635b3d0f9dbc54a43f97477d664d
Gyuyoung Kim <gyuyoung@igalia.com>
2022-01-11 00:30:05 +0000

    MPArch: Ensure that fenced frame doesn't clear active extensions
    This CL adds a test to make sure that a fenced frame doesn't
    clear active extensions in
    ActiveTabPermissionGranter::DidFinishNavigation.
    
    This test refers to the existing test to simulate the situation
    that |granted_extensions_| is not empty.
    
    Bug: 698985, 1218946, 1284194
    Change-Id: Ia3aa803048026a5561a02ebbaee81d925ffc8e0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3365919
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#957341}

--- chrome/browser/extensions/active_tab_permission_granter.h
+++ chrome/browser/extensions/active_tab_permission_granter.h
@@ -63,6 +63,9 @@ class ActiveTabPermissionGranter
   void RevokeForTesting();
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(ExtensionActionRunnerFencedFrameBrowserTest,
+                           FencedFrameDoesNotClearActiveExtensions);
+
   // content::WebContentsObserver implementation.
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
