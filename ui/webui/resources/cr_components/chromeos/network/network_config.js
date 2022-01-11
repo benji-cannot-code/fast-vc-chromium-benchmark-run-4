ded4f23a96ed845c9c6d5010c7513ab4422f4d9a
Taoyu Li <taoyl@google.com>
2022-01-11 11:52:35 +0000

    [CrOS setting] Showing a placeholder for configured wireguard peer preshared key
    Since WireGuard peer preshared key is a credential field, shill
    does not return the value of it for a configured service. In order
    to tell between no change and explicitly deleting a configured
    preshared key, showing a placeholder for this field of configured
    services. Note that since preshared key is of a certain format,
    the placeholder string can never be a real preshared key value.
    
    Bug: b:206127526
    Change-Id: I90b6bf8cc9f7bcdb8d4400aec79d7462425a9014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3353647
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Taoyu Li <taoyl@chromium.org>
    Auto-Submit: Taoyu Li <taoyl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957512}

--- ui/webui/resources/cr_components/chromeos/network/network_config.js
+++ ui/webui/resources/cr_components/chromeos/network/network_config.js
@@ -42,6 +42,8 @@ const mojom = chromeos.networkConfig.mojom;
 /** @type {string}  */ const NO_CERTS_HASH = 'no-certs';
 /** @type {string}  */ const NO_USER_CERT_HASH = 'no-user-cert';
 
+/** @type {string}  */ const PLACEHOLDER_CREDENTIAL = '(credential)';
+
 Polymer({
   is: 'network-config',
 
@@ -888,7 +890,13 @@ Polymer({
     };
     if (wireguard.peers && wireguard.peers.activeValue) {
       for (const peer of wireguard.peers.activeValue) {
-        config.peers.push(Object.assign({}, peer));
+        const peerCopied = Object.assign({}, peer);
+        if (this.hasGuid_()) {
+          // Shill does not return exact value for crendential fields, showing
+          // a placeholder here.
+          peerCopied.presharedKey = PLACEHOLDER_CREDENTIAL;
+        }
+        config.peers.push(peerCopied);
       }
     }
     return config;
@@ -1646,7 +1654,8 @@ Polymer({
     if (!this.isValidWireGuardKey_(peer.publicKey)) {
       return false;
     }
-    if (!!peer.presharedKey && !this.isValidWireGuardKey_(peer.presharedKey)) {
+    if (!!peer.presharedKey && peer.presharedKey !== PLACEHOLDER_CREDENTIAL &&
+        !this.isValidWireGuardKey_(peer.presharedKey)) {
       return false;
     }
     // endpoint should be the form of IP:port or hostname:port
@@ -1824,6 +1833,14 @@ Polymer({
       delete wireguard.privateKey;
     } else if (this.wireguardKeyType_ === WireGuardKeyConfigType.GENERATE_NEW) {
       wireguard.privateKey = '';
+    }
+    assert(!!wireguard.peers);
+    for (const peer of wireguard.peers) {
+      if (peer.presharedKey === PLACEHOLDER_CREDENTIAL) {
+        delete peer.presharedKey;  // No modification
+      } else if (peer.presharedKey === undefined) {
+        peer.presharedKey = '';  // Explicitly removed
+      }
     }
   },
 
