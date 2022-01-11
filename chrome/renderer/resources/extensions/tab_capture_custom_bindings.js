368d1b509ffc64bf53e22b13a7988992d12fcb13
mark a. foltz <mfoltz@chromium.org>
2022-01-11 20:19:12 +0000

    [chrome.tabCapture] Update bindings to use getUserMedia.
    This also modernizes Javascript usage in these bindings.
    
    Bug: 1270535
    Change-Id: I30f0226fb0fde89295330024996f4f6cbdf137e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3308388
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957686}

--- chrome/renderer/resources/extensions/tab_capture_custom_bindings.js
+++ chrome/renderer/resources/extensions/tab_capture_custom_bindings.js
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Custom binding for the Tab Capture API.
 
 apiBridge.registerCustomHook(function(bindingsAPI, extensionId) {
-  var apiFunctions = bindingsAPI.apiFunctions;
-
   function proxyToGetUserMedia(callback, response) {
     if (!callback)
       return;
@@ -19,28 +17,25 @@ apiBridge.registerCustomHook(function(bindingsAPI, extensionId) {
       return;
     }
 
-    // Convenience function for processing webkitGetUserMedia() error objects to
+    // Convenience function for processing getUserMedia() error objects to
     // provide runtime.lastError messages for the tab capture API.
-    function getErrorMessage(error, fallbackMessage) {
+    const getErrorMessage = (error, fallbackMessage) => {
       if (!error || (typeof error.message !== 'string'))
         return fallbackMessage;
-      return error.message.replace(/(navigator\.)?(webkit)?GetUserMedia/gi,
+      return error.message.replace('navigator.mediaDevices.getUserMedia',
                                    'tabCapture.capture');
-    }
+    };
 
-    var options = {};
+    let constraints = {};
     if (response.audioConstraints)
-      options.audio = response.audioConstraints;
+      constraints.audio = response.audioConstraints;
     if (response.videoConstraints)
-      options.video = response.videoConstraints;
+      constraints.video = response.videoConstraints;
     try {
-      navigator.webkitGetUserMedia(
-          options,
-          function onSuccess(media_stream) {
-            callback(media_stream);
-          },
-          function onError(error) {
-            bindingUtil.runCallbackWithLastError(
+      navigator.mediaDevices.getUserMedia(constraints)
+          .then(callback)
+          .catch(error => {
+              bindingUtil.runCallbackWithLastError(
                 getErrorMessage(error, "Failed to start MediaStream."),
                 $Function.bind(callback, null, null));
           });
@@ -51,5 +46,5 @@ apiBridge.registerCustomHook(function(bindingsAPI, extensionId) {
     }
   }
 
-  apiFunctions.setCustomCallback('capture', proxyToGetUserMedia);
+  bindingsAPI.apiFunctions.setCustomCallback('capture', proxyToGetUserMedia);
 });
