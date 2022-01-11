11a414cc9656023732cf8eccd8923830cc240323
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2022-01-11 15:43:59 +0000

    Clean up origin and key handling in service worker tests.
    Avoid recreating Origin or StorageKey from scope GURL.
    
    Bug: 1285240, 1270878
    Change-Id: I08e457dd75ddd5d42c6641b0e4a33dd72bc6f0cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380490
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957570}

--- content/browser/service_worker/service_worker_version_unittest.cc
+++ content/browser/service_worker/service_worker_version_unittest.cc
@@ -206,8 +206,7 @@ class ServiceWorkerVersionTest : public testing::Test {
     container_host->UpdateUrls(
         registration_->scope(),
         net::SiteForCookies::FromUrl(registration_->scope()),
-        url::Origin::Create(registration_->scope()),
-        blink::StorageKey(url::Origin::Create(registration_->scope())));
+        registration_->key().origin(), registration_->key());
     container_host->SetControllerRegistration(
         registration_, false /* notify_controllerchange */);
     EXPECT_TRUE(version_->HasControllee());
@@ -373,8 +372,7 @@ TEST_F(ServiceWorkerVersionTest, StartUnregisteredButStillLiveWorker) {
   absl::optional<blink::ServiceWorkerStatusCode> status;
   base::RunLoop run_loop;
   helper_->context()->registry()->DeleteRegistration(
-      registration_,
-      blink::StorageKey(url::Origin::Create(registration_->scope())),
+      registration_, registration_->key(),
       ReceiveServiceWorkerStatus(&status, run_loop.QuitClosure()));
   run_loop.Run();
   ASSERT_EQ(blink::ServiceWorkerStatusCode::kOk, status.value());
@@ -464,8 +462,7 @@ TEST_F(ServiceWorkerVersionTest, Doom) {
   container_host->UpdateUrls(
       registration_->scope(),
       net::SiteForCookies::FromUrl(registration_->scope()),
-      url::Origin::Create(registration_->scope()),
-      blink::StorageKey(url::Origin::Create(registration_->scope())));
+      registration_->key().origin(), registration_->key());
   container_host->SetControllerRegistration(registration_, false);
   EXPECT_TRUE(version_->HasControllee());
   EXPECT_TRUE(container_host->controller());
@@ -1260,8 +1257,7 @@ TEST_F(ServiceWorkerVersionTest,
   container_host->UpdateUrls(
       registration_->scope(),
       net::SiteForCookies::FromUrl(registration_->scope()),
-      url::Origin::Create(registration_->scope()),
-      blink::StorageKey(url::Origin::Create(registration_->scope())));
+      registration_->key().origin(), registration_->key());
   container_host->SetControllerRegistration(
       registration_, false /* notify_controllerchange */);
   EXPECT_TRUE(version_->HasControllee());
