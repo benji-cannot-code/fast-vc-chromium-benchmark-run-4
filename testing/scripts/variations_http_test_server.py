98ca00054add4495bf978254e08b8ba26d09c946
Arthur Wang <wuwang@google.com>
2022-01-11 01:22:08 +0000

    Revert "Stop running variations_smoke_tests on bots for now"
    This reverts commit 9ef43df31342fb0fc854de5233b7170039028bc1.
    
    Reason for revert: Added a local http server to stable the test
    
    Original change's description:
    > Stop running variations_smoke_tests on bots for now
    >
    > The test is consistently failing on many bots.
    >
    > Apparently it loads data off chromium.org. It shouldn't run on
    > bots until its hermetic.
    >
    > Bug: 1278535
    > Ignore-Freeze: true
    > Change-Id: I841ce909e39d6df39672307a52689d622b23ce02
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3339494
    > Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    > Reviewed-by: Erik Staab <estaab@chromium.org>
    > Commit-Queue: Nico Weber <thakis@chromium.org>
    > Auto-Submit: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#952510}
    
    Bug: 1278535
    Change-Id: Icc5957fb2d269cf2d2f83e3dec1896e00fbf3bad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3358562
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957364}

--- testing/scripts/variations_http_test_server.py
+++ testing/scripts/variations_http_test_server.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 serves content from a base_path.
 """
 
+import os
 try:
   # Python 2
   from SimpleHTTPServer import SimpleHTTPRequestHandler
@@ -31,7 +32,8 @@ class HTTPServer(BaseHTTPServer):
     self.stop = False
     BaseHTTPServer.__init__(self, server_address, RequestHandlerClass)
 
-  def serve_forever(self, poll_interval=0.1):
+  #pylint: disable=unused-argument
+  def server_forever(self, poll_interval=0.1):
     self.stop = False
     while not self.stop:
       self.handle_request()
