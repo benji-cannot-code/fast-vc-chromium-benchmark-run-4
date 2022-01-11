21d4036d7bc478a3e8d7e71aa1b9a09c04647e2e
Peter Kasting <pkasting@chromium.org>
2022-01-11 02:08:44 +0000

    Use a weak pointer factory for metrics emitter tasks.
    This prevents UAF if the emitter is destroyed before the tasks run.
    
    Bug: 1281763
    Change-Id: I2c5255e72c2780b2b4fa60ba15b13c4c5a56fcf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378382
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957378}

--- chrome/browser/google/google_search_domain_mixing_metrics_emitter.h
+++ chrome/browser/google/google_search_domain_mixing_metrics_emitter.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/clock.h"
@@ -80,6 +81,8 @@ class GoogleSearchDomainMixingMetricsEmitter : public KeyedService {
       std::make_unique<base::RepeatingTimer>();
   base::CancelableTaskTracker task_tracker_;
   scoped_refptr<base::SingleThreadTaskRunner> ui_thread_task_runner_;
+  base::WeakPtrFactory<GoogleSearchDomainMixingMetricsEmitter>
+      weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_GOOGLE_GOOGLE_SEARCH_DOMAIN_MIXING_METRICS_EMITTER_H_
