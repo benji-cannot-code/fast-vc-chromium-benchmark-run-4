f33fc8d902657b5fa4e65d974108583b28222f63
Lei Zhang <thestig@chromium.org>
2022-01-11 06:55:06 +0000

    Rewrite PDF thumbnail code to avoid using Skia classes.
    The code does not directly interact with Skia for its production use
    case. So replace SkBitmap with a gfx::Size and base::Value::BlobStorage
    pair. Then the pixel data can be taken instead of copied at the end of
    the Thumbnail class's lifecycle.
    
    Change-Id: I534291e36fd5fcfe70e9152726df25872d034e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3368635
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957445}

--- pdf/pdfium/pdfium_page.cc
+++ pdf/pdfium/pdfium_page.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/pdfium/public/cpp/fpdf_scopers.h"
 #include "third_party/pdfium/public/fpdf_annot.h"
 #include "third_party/pdfium/public/fpdf_catalog.h"
-#include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/rect.h"
@@ -1556,22 +1555,22 @@ Thumbnail PDFiumPage::GenerateThumbnail(float device_pixel_ratio) {
   gfx::Size page_size(base::saturated_cast<int>(FPDF_GetPageWidthF(page)),
                       base::saturated_cast<int>(FPDF_GetPageHeightF(page)));
   Thumbnail thumbnail(page_size, device_pixel_ratio);
+  const gfx::Size& image_size = thumbnail.image_size();
 
-  SkBitmap& sk_bitmap = thumbnail.bitmap();
   ScopedFPDFBitmap fpdf_bitmap(FPDFBitmap_CreateEx(
-      sk_bitmap.width(), sk_bitmap.height(), FPDFBitmap_BGRA,
-      sk_bitmap.getPixels(), sk_bitmap.rowBytes()));
+      image_size.width(), image_size.height(), FPDFBitmap_BGRA,
+      thumbnail.GetImageData().data(), thumbnail.stride()));
 
   // Clear the bitmap.
   FPDFBitmap_FillRect(fpdf_bitmap.get(), /*left=*/0, /*top=*/0,
-                      sk_bitmap.width(), sk_bitmap.height(),
+                      image_size.width(), image_size.height(),
                       /*color=*/0xFFFFFFFF);
 
   // The combination of the `FPDF_REVERSE_BYTE_ORDER` rendering flag and the
   // `FPDFBitmap_BGRA` format when initializing `fpdf_bitmap` results in an RGBA
   // rendering, which is the format required by HTML <canvas>.
   FPDF_RenderPageBitmap(fpdf_bitmap.get(), GetPage(), /*start_x=*/0,
-                        /*start_y=*/0, sk_bitmap.width(), sk_bitmap.height(),
+                        /*start_y=*/0, image_size.width(), image_size.height(),
                         /*rotate=*/0, FPDF_ANNOT | FPDF_REVERSE_BYTE_ORDER);
 
   return thumbnail;
