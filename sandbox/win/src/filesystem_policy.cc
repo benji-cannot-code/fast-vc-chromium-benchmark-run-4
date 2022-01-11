6d8a592920c6284712cc9cae8bafd74bd3577267
James Forshaw <forshaw@chromium.org>
2022-01-11 00:52:57 +0000

    [Windows] Don't add NtSetInfoRename policy for pipes.
    This CL checks if a file sandbox policy is a pipe and removed the
    NtSetInfoRename rule which isn't needed.
    
    Bug: 1270309
    Change-Id: I1f9ec222707762a41ed23a0d2aeed7452030257f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379190
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957354}

--- sandbox/win/src/filesystem_policy.cc
+++ sandbox/win/src/filesystem_policy.cc
@@ -83,6 +83,7 @@ bool FileSystemPolicy::GenerateRules(const wchar_t* name,
     return false;
   }
 
+  bool is_pipe = IsPipe(mod_name);
   if (!PreProcessName(&mod_name)) {
     // The path to be added might contain a reparse point.
     NOTREACHED();
@@ -172,7 +173,7 @@ bool FileSystemPolicy::GenerateRules(const wchar_t* name,
     return false;
   }
 
-  if ((rule_to_add & kCallNtSetInfoRename) &&
+  if ((rule_to_add & kCallNtSetInfoRename) && !is_pipe &&
       (!rename.AddStringMatch(IF, OpenFile::NAME, name, CASE_INSENSITIVE) ||
        !policy->AddRule(IpcTag::NTSETINFO_RENAME, &rename))) {
     return false;
