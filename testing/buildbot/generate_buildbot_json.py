8cdc99902211d7cc3dbffd355bf7832cdb4757a9
Chong Gu <chonggu@google.com>
2022-01-11 19:52:17 +0000

    [Fuchsia] Align telemetry_gpu_integration_test with other OS
    Moving Fuchsia's gpu integration test targets from //content/test to
    //chrome/test to align with other OS. Note that the old target cannot
    be deleted yet since astro and sherlock builder still uses it.
    Implementation wise, we use an approach similar to Android, where
    the browser to use is set via a gn_arg named fuchsia_browser_type.
    One difference from Android is that the target name remains
    telemetry_gpu_integration_test_fuchsia regardless of the browser chosen.
    
    Bug: 1284984
    Change-Id: I51484ef2a92702323cb5d44f2761655b71dc5a97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371466
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Chong Gu <chonggu@google.com>
    Cr-Commit-Position: refs/heads/main@{#957675}

--- testing/buildbot/generate_buildbot_json.py
+++ testing/buildbot/generate_buildbot_json.py
@@ -400,6 +400,9 @@ class BBJSONGenerator(object):
   def is_chromeos(self, tester_config):
     return tester_config.get('os_type') == 'chromeos'
 
+  def is_fuchsia(self, tester_config):
+    return tester_config.get('os_type') == 'fuchsia'
+
   def is_lacros(self, tester_config):
     return tester_config.get('os_type') == 'lacros'
 
@@ -998,6 +1001,8 @@ class BBJSONGenerator(object):
       return (
           'telemetry_gpu_integration_test' +
           BROWSER_CONFIG_TO_TARGET_SUFFIX_MAP[tester_config['browser_config']])
+    elif self.is_fuchsia(tester_config):
+      return 'telemetry_gpu_integration_test_fuchsia'
     else:
       return 'telemetry_gpu_integration_test'
 
