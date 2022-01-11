e8f8a9d574bd6ab6e5fcc317b3ee3fe12ace3464
Phillis Tang <phillis@google.com>
2022-01-11 19:49:28 +0000

    DPWA: remove recording NewTabPage.NumberOfPreinstalledApps
    Bug: 1257275
    Change-Id: I5406e97af134e7900a3a8b0212bf0ae64e27b53e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379084
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957671}

--- chrome/browser/ui/search/ntp_user_data_logger.cc
+++ chrome/browser/ui/search/ntp_user_data_logger.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ntp_tiles/metrics.h"
 #include "components/prefs/pref_service.h"
 #include "components/search/ntp_features.h"
-#include "extensions/common/constants.h"
 
 namespace {
 
@@ -265,12 +264,6 @@ LogoClickType LoggingEventToLogoClick(NTPLoggingEventType event) {
   }
 }
 
-bool IsImpressionFromPreinstalledApp(
-    const ntp_tiles::NTPTileImpression& impression) {
-  return impression.url_for_rappor.is_valid() &&
-         impression.url_for_rappor.SchemeIs(extensions::kExtensionScheme) &&
-         extension_misc::IsPreinstalledAppId(impression.url_for_rappor.host());
-}
 }  // namespace
 
 // Helper macro to log a load time to UMA. There's no good reason why we don't
@@ -437,10 +430,6 @@ void NTPUserDataLogger::LogMostVisitedImpression(
 void NTPUserDataLogger::LogMostVisitedNavigation(
     const ntp_tiles::NTPTileImpression& impression) {
   ntp_tiles::metrics::RecordTileClick(impression);
-  if (IsImpressionFromPreinstalledApp(impression)) {
-    base::RecordAction(
-        base::UserMetricsAction("NewTabPage.PreinstalledApps.Clicked"));
-  }
 
   // Records the action. This will be available as a time-stamped stream
   // server-side and can be used to compute time-to-long-dwell.
@@ -465,7 +454,6 @@ void NTPUserDataLogger::EmitNtpStatistics(base::TimeDelta load_time,
   }
 
   int tiles_count = 0;
-  int num_of_default_apps = 0;
   for (const absl::optional<ntp_tiles::NTPTileImpression>& impression :
        logged_impressions_) {
     if (!impression.has_value()) {
@@ -473,14 +461,8 @@ void NTPUserDataLogger::EmitNtpStatistics(base::TimeDelta load_time,
     }
     ntp_tiles::metrics::RecordTileImpression(*impression);
     ++tiles_count;
-
-    if (IsImpressionFromPreinstalledApp(*impression)) {
-      ++num_of_default_apps;
-    }
   }
   ntp_tiles::metrics::RecordPageImpression(tiles_count);
-  UMA_HISTOGRAM_COUNTS_100("NewTabPage.NumberOfPreinstalledApps",
-                           num_of_default_apps);
 
   DVLOG(1) << "Emitting NTP load time: " << load_time << ", "
            << "number of tiles: " << tiles_count;
