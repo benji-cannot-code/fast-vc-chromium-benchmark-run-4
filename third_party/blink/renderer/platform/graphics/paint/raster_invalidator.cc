fe7760836b62bc3491f923aaa0a1b79bb442d37d
Xianzhu Wang <wangxianzhu@chromium.org>
2022-01-11 02:01:53 +0000

    Fix raster invalidaiton for moved chunk with changed layer state
    The optimization in RasterInvalidation for paint chunks moved from
    cached subsequences doesn't apply if the layer state changed.
    
    Bug: 1277923
    Change-Id: Ib00a789a5a84d50ef7d0d69706f8520fa7dbd40c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3376744
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957374}

--- third_party/blink/renderer/platform/graphics/paint/raster_invalidator.cc
+++ third_party/blink/renderer/platform/graphics/paint/raster_invalidator.cc
@@ -168,7 +168,7 @@ static bool ShouldSkipForRasterInvalidation(
 // reordering, property changes) of chunks. The logic is similar to
 // PaintController::GenerateRasterInvalidations(). The complexity is between
 // O(n) and O(m*n) where m and n are the numbers of old and new chunks,
-// respectively. Normally both m and n are small numbers. The best caseis that
+// respectively. Normally both m and n are small numbers. The best case is that
 // all old chunks have matching new chunks in the same order. The worst case is
 // that no matching chunks except the first one (which always matches otherwise
 // we won't reuse the RasterInvalidator), which is rare. In
@@ -177,10 +177,9 @@ static bool ShouldSkipForRasterInvalidation(
 void RasterInvalidator::GenerateRasterInvalidations(
     RasterInvalidationFunction function,
     const PaintChunkSubset& new_chunks,
-    const PropertyTreeState& layer_state,
-    bool layer_offset_changed,
+    bool layer_offset_or_state_changed,
     Vector<PaintChunkInfo>& new_chunks_info) {
-  ChunkToLayerMapper mapper(layer_state, layer_offset_);
+  ChunkToLayerMapper mapper(layer_state_, layer_offset_);
   Vector<bool> old_chunks_matched;
   old_chunks_matched.resize(old_paint_chunks_info_.size());
   wtf_size_t old_index = 0;
@@ -218,10 +217,11 @@ void RasterInvalidator::GenerateRasterInvalidations(
 
     // No need to invalidate if the chunk is moved from cached subsequence and
     // its paint properties didn't change relative to the layer.
-    if (!layer_offset_changed && reason == PaintInvalidationReason::kNone &&
+    if (!layer_offset_or_state_changed &&
+        reason == PaintInvalidationReason::kNone &&
         new_chunk.is_moved_from_cached_subsequence &&
         !new_chunk.properties.GetPropertyTreeState().Changed(
-            PaintPropertyChangeType::kChangedOnlySimpleValues, layer_state)) {
+            PaintPropertyChangeType::kChangedOnlySimpleValues, layer_state_)) {
       new_chunks_info.emplace_back(old_chunk_info, it);
     } else {
       mapper.SwitchToChunk(new_chunk);
@@ -229,7 +229,7 @@ void RasterInvalidator::GenerateRasterInvalidations(
 
       if (reason == PaintInvalidationReason::kNone) {
         reason = ChunkPropertiesChanged(new_chunk, old_chunk, new_chunk_info,
-                                        old_chunk_info, layer_state);
+                                        old_chunk_info, layer_state_);
       }
 
       if (IsFullPaintInvalidationReason(reason)) {
@@ -330,10 +330,12 @@ void RasterInvalidator::Generate(
   if (RasterInvalidationTracking::ShouldAlwaysTrack())
     EnsureTracking();
 
-  bool layer_offset_changed = layer_offset_ != layer_offset;
+  bool layer_offset_or_state_changed =
+      layer_offset_ != layer_offset || layer_state_ != layer_state;
   bool layer_bounds_was_empty = layer_bounds_.IsEmpty();
   layer_offset_ = layer_offset;
   layer_bounds_ = layer_bounds;
+  layer_state_ = layer_state;
   current_paint_artifact_ = &new_chunks.GetPaintArtifact();
 
   Vector<PaintChunkInfo> new_chunks_info;
@@ -358,8 +360,7 @@ void RasterInvalidator::Generate(
     }
   } else {
     GenerateRasterInvalidations(raster_invalidation_function, new_chunks,
-                                layer_state, layer_offset_changed,
-                                new_chunks_info);
+                                layer_offset_or_state_changed, new_chunks_info);
   }
 
   old_paint_chunks_info_ = std::move(new_chunks_info);
