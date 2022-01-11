6121d722cc880a89f27487553e3cdf2a0c31ebfe
Abhijeet Kandalkar <abhijeet@igalia.com>
2022-01-11 06:48:35 +0000

    [mparch]Refactor the derived classes of ContentSettingBubbleModel
    This CL plumbed the correct RenderFrameHost by replacing use of
    `web_contents()->GetMainFrame()` by `&GetPage().GetMainDocument()` for
    classes ContentSetting{SimpleBubbleModel, MixedScriptBubbleModel,
    SingleRadioGroup, GeolocationBubbleModel}.
    
    Bug: 1285043, 1103176
    Change-Id: I3fb0212365a4b92977ec7d745b21cdbcef5c91b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372526
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#957442}

--- chrome/browser/ui/content_settings/content_setting_bubble_model.cc
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.cc
@@ -223,9 +223,8 @@ ContentSettingSimpleBubbleModel::AsSimpleBubbleModel() {
 }
 
 void ContentSettingSimpleBubbleModel::SetTitle() {
-  // TODO(https://crbug.com/1103176): Plumb the actual frame reference here
   PageSpecificContentSettings* content_settings =
-      PageSpecificContentSettings::GetForFrame(web_contents()->GetMainFrame());
+      PageSpecificContentSettings::GetForFrame(&GetPage().GetMainDocument());
 
   static const ContentSettingsTypeIdEntry kBlockedTitleIDs[] = {
       {ContentSettingsType::COOKIES, IDS_BLOCKED_COOKIES_TITLE},
@@ -261,7 +260,7 @@ void ContentSettingSimpleBubbleModel::SetTitle() {
 
 void ContentSettingSimpleBubbleModel::SetMessage() {
   PageSpecificContentSettings* content_settings =
-      PageSpecificContentSettings::GetForFrame(web_contents()->GetMainFrame());
+      PageSpecificContentSettings::GetForFrame(&GetPage().GetMainDocument());
 
   // TODO(https://crbug.com/978882): Make the two arrays below static again once
   // we no longer need to check base::FeatureList.
@@ -378,7 +377,7 @@ void ContentSettingMixedScriptBubbleModel::OnCustomLinkClicked() {
   if (mixed_content_settings) {
     // Update browser side settings to allow active mixed content.
     mixed_content_settings->AllowRunningOfInsecureContent(
-        *web_contents()->GetMainFrame());
+        GetPage().GetMainDocument());
   }
 
   // Update renderer side settings to allow active mixed content.
@@ -552,7 +551,7 @@ void ContentSettingSingleRadioGroup::SetRadioGroup() {
   std::u16string display_host = url_formatter::FormatUrlForSecurityDisplay(url);
 
   PageSpecificContentSettings* content_settings =
-      PageSpecificContentSettings::GetForFrame(web_contents()->GetMainFrame());
+      PageSpecificContentSettings::GetForFrame(&GetPage().GetMainDocument());
   bool allowed = !content_settings->IsContentBlocked(content_type());
 
   // For the frame busting case the content is blocked but its content type is
@@ -1239,7 +1238,7 @@ ContentSettingGeolocationBubbleModel::ContentSettingGeolocationBubbleModel(
   SetCustomLink();
 #if defined(OS_MAC)
   PageSpecificContentSettings* content_settings =
-      PageSpecificContentSettings::GetForFrame(web_contents->GetMainFrame());
+      PageSpecificContentSettings::GetForFrame(&GetPage().GetMainDocument());
   if (!content_settings)
     return;
 
@@ -1314,7 +1313,7 @@ void ContentSettingGeolocationBubbleModel::SetCustomLink() {
       web_contents()->GetBrowserContext());
   SettingInfo info;
   const GURL url =
-      web_contents()->GetMainFrame()->GetLastCommittedOrigin().GetURL();
+      GetPage().GetMainDocument().GetLastCommittedOrigin().GetURL();
   map->GetWebsiteSetting(url, url, ContentSettingsType::GEOLOCATION, &info);
   if (info.session_model == SessionModel::OneTime)
     set_custom_link(l10n_util::GetStringUTF16(IDS_GEOLOCATION_WILL_ASK_AGAIN));
