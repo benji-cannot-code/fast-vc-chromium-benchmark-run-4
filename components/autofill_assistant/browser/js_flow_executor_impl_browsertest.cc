e88c20411bfe4135344e4a047d93ead99ff6d374
Stephane Zermatten <szermatt@chromium.org>
2022-01-11 10:40:31 +0000

    Use _browsertest.cc extension for test run by components_browsertests.
    This change proposes having js_flow_executor_impl_unittest.cc follow the
    convention of using a _unittest.cc extension for tests that are run as
    part of components_unittests and _browsertest.cc extension for tests
    that are run as part of components_browsertests.
    
    Change-Id: Id4e97bb3a3d9fc137638bbe1a6e47c5f56f7a7b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3377024
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957499}

--- components/autofill_assistant/browser/js_flow_executor_impl_browsertest.cc
+++ components/autofill_assistant/browser/js_flow_executor_impl_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/autofill_assistant/browser/js_flow_executor_impl.h"
+
 #include "base/callback.h"
 #include "base/json/json_reader.h"
 #include "base/strings/strcat.h"
