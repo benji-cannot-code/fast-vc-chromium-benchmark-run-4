5afee688eeb43a550c88f21661f76a49c18382b3
Alex Gough <ajgo@chromium.org>
2022-01-11 19:29:28 +0000

    Tell elevated processes directly to get field trail shm from parent
    On Windows processes can be launched using two mechanisms. The common
    way allows handles to be inherited by a child. Chrome sometimes also
    launches elevated processes (ultimately via ShellExecute) which does
    not allow for handle inheritance. In these cases the child must obtain
    its handle to the readonly field trial shared memory region directly
    from the parent - this is possible as the elevated child has permission
    to do this.
    
    Before this change the child would use IsCurrentProcessElevated() to
    determine how it was launched - however this function returns true in
    some cases if the parent was running as Admin (as happens in Chrome's
    tests). However, sometimes the child is still sandboxed so that it
    cannot open the parent.
    
    This change communicates to the child how it was started. LaunchOptions
    has an additional advisory field `elevated` which should be set if
    base::LaunchElevatedProcess is to be called. This can be inspected by
    other parts of the launching chain, such as the field trials helpers.
    
    The field trial shm handle format is extended to allow a field to
    indicate how the child should obtain the handle, either by (i)nheritance
    or from its (p)arent. On other platforms this field is ignored, but is
    set to (r)endevous on Mac and (i)nherited for Linux/Fuchsia.
    
    Bug: 1262370
    Change-Id: Iff32322c08e2c04fe3283de4a62552cb10f73e92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371395
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957659}

--- base/process/launch_win.cc
+++ base/process/launch_win.cc
@@ -215,6 +215,7 @@ Process LaunchProcess(const CommandLine& cmdline,
 
 Process LaunchProcess(const CommandLine::StringType& cmdline,
                       const LaunchOptions& options) {
+  CHECK(!options.elevated);
   // Mitigate the issues caused by loading DLLs on a background thread
   // (http://crbug/973868).
   SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY();
@@ -410,6 +411,7 @@ Process LaunchProcess(const CommandLine::StringType& cmdline,
 
 Process LaunchElevatedProcess(const CommandLine& cmdline,
                               const LaunchOptions& options) {
+  CHECK(options.elevated);
   const FilePath::StringType file = cmdline.GetProgram().value();
   const CommandLine::StringType arguments = cmdline.GetArgumentsString();
 
