ba10837412931c07962ae69e85a91b84083ae8b2
Tom Anderson <thomasanderson@chromium.org>
2022-01-11 07:55:09 +0000

    Modernize usage of base::Value in //dbus
    base::Value refactoring doc:
    https://docs.google.com/document/d/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ
    
    R=hashimoto
    
    Bug: 1187001
    Change-Id: Ia5907236f125ce530b451bc7f214715fea865791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3367747
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957457}

--- chromeos/dbus/shill/shill_client_helper.cc
+++ chromeos/dbus/shill/shill_client_helper.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-// Class to hold onto a reference to a ShillClientHelper. This calss
+// Class to hold onto a reference to a ShillClientHelper. This class
 // is owned by callbacks and released once the callback completes.
 // Note: Only success callbacks hold the reference. If an error callback is
 // invoked instead, the success callback will still be destroyed and the
@@ -146,12 +146,12 @@ void OnValueMethod(ShillClientHelper::RefHolder* ref_holder,
     return;
   }
   dbus::MessageReader reader(response);
-  std::unique_ptr<base::Value> value(dbus::PopDataAsValue(&reader));
-  if (!value.get()) {
+  base::Value value(dbus::PopDataAsValue(&reader));
+  if (value.is_none()) {
     std::move(callback).Run(absl::nullopt);
     return;
   }
-  std::move(callback).Run(std::move(*value));
+  std::move(callback).Run(std::move(value));
 }
 
 // Handles responses for methods without results.
@@ -169,13 +169,13 @@ void OnValueMethodWithErrorCallback(
     ShillClientHelper::ErrorCallback error_callback,
     dbus::Response* response) {
   dbus::MessageReader reader(response);
-  std::unique_ptr<base::Value> value(dbus::PopDataAsValue(&reader));
-  if (!value.get() || !value->is_dict()) {
+  base::Value value(dbus::PopDataAsValue(&reader));
+  if (!value.is_dict()) {
     std::move(error_callback)
         .Run(kInvalidResponseErrorName, kInvalidResponseErrorMessage);
     return;
   }
-  std::move(callback).Run(std::move(*value));
+  std::move(callback).Run(std::move(value));
 }
 
 // Handles responses for methods with ListValue results.
@@ -185,13 +185,13 @@ void OnListValueMethodWithErrorCallback(
     ShillClientHelper::ErrorCallback error_callback,
     dbus::Response* response) {
   dbus::MessageReader reader(response);
-  std::unique_ptr<base::Value> value(dbus::PopDataAsValue(&reader));
-  if (!value.get() || !value->is_list()) {
+  base::Value value(dbus::PopDataAsValue(&reader));
+  if (!value.is_list()) {
     std::move(error_callback)
         .Run(kInvalidResponseErrorName, kInvalidResponseErrorMessage);
     return;
   }
-  std::move(callback).Run(base::Value::AsListValue(*value));
+  std::move(callback).Run(base::Value::AsListValue(value));
 }
 
 // Handles running appropriate error callbacks.
@@ -435,8 +435,8 @@ void AppendValueDataAsVariantInternal(dbus::MessageWriter* writer,
         writer->OpenVariant("aa{ss}", &variant_writer);
         dbus::MessageWriter array_writer(nullptr);
         variant_writer.OpenArray("a{ss}", &array_writer);
-        for (const auto& value : list_view) {
-          AppendStringDictionary(value, &array_writer);
+        for (const auto& item : list_view) {
+          AppendStringDictionary(item, &array_writer);
         }
         variant_writer.CloseContainer(&array_writer);
         writer->CloseContainer(&variant_writer);
@@ -538,12 +538,12 @@ void ShillClientHelper::OnPropertyChanged(dbus::Signal* signal) {
   std::string name;
   if (!reader.PopString(&name))
     return;
-  std::unique_ptr<base::Value> value(dbus::PopDataAsValue(&reader));
-  if (!value.get())
+  base::Value value(dbus::PopDataAsValue(&reader));
+  if (value.is_none())
     return;
 
   for (auto& observer : observer_list_)
-    observer.OnPropertyChanged(name, *value);
+    observer.OnPropertyChanged(name, value);
 }
 
 }  // namespace chromeos
--- dbus/values_util.h
+++ dbus/values_util.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <memory>
-
 #include "dbus/dbus_export.h"
 
 namespace base {
@@ -22,11 +20,10 @@ class MessageReader;
 class MessageWriter;
 
 // Pops a value from |reader| as a base::Value.
-// Returns NULL if an error occurs.
+// Returns base::Value() if an error occurs.
 // Note: Integer values larger than int32_t (including uint32_t) are converted
 // to double.  Non-string dictionary keys are converted to strings.
-CHROME_DBUS_EXPORT std::unique_ptr<base::Value> PopDataAsValue(
-    MessageReader* reader);
+CHROME_DBUS_EXPORT base::Value PopDataAsValue(MessageReader* reader);
 
 // Appends a basic type value to |writer|. Basic types are BOOLEAN, INTEGER,
 // DOUBLE, and STRING. Use this function for values that are known to be basic
