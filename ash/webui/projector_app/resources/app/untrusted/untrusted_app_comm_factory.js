16b053ffbdb3b3cfaa986530108b72e581570b02
dpapad <dpapad@chromium.org>
2022-01-11 10:37:31 +0000

    Simplify shared WebUI resources access in chrome-untrusted://projector
    Remove registration of shared WebUI resources under
    chrome-untrusted://projector, and use chrome-untrusted://resources
    instead.
    
    Bug: 1082937
    Change-Id: If2e7d2c79f3a026376d1df1cbb1d302647051ab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372426
    Reviewed-by: Yilkal Abe <yilkal@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957497}

--- ash/webui/projector_app/resources/app/untrusted/untrusted_app_comm_factory.js
+++ ash/webui/projector_app/resources/app/untrusted/untrusted_app_comm_factory.js
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {PostMessageAPIClient} from 'chrome-untrusted://projector/js/post_message_api_client.m.js';
-import {RequestHandler} from 'chrome-untrusted://projector/js/post_message_api_request_handler.m.js';
+import {PostMessageAPIClient} from '//resources/js/post_message_api_client.m.js';
+import {RequestHandler} from '//resources/js/post_message_api_request_handler.m.js';
 
 import {ProjectorError} from '../../communication/message_types.js';
 
