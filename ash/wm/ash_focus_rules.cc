3c84e710ec9a5969911c69afd3b18635c57ffa0e
Mikel Astiz <mastiz@chromium.org>
2022-01-11 09:16:09 +0000

    Revert "Desk Templates: Update ash_focus_rules to handle template grid"
    This reverts commit ba9d4f9daecc335d133d68e2c0d553c2d4df9f29.
    
    Reason for revert: suspect of breaking ash_unittests, see
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/19647/overview
    
    Original change's description:
    > Desk Templates: Update ash_focus_rules to handle template grid
    >
    > There are focus issues when we exit out of the templates grid. This CL
    > fixes those activation issues by checking what window is losing
    > activation and determining if we want to focus on the overview focus
    > window.
    >
    > Bug: 1277769
    > Test: manual, added new test
    > Change-Id: I66905f2d684851a853c9e540c761d7b9c518cd8b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336756
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Richard Chui <richui@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#957403}
    
    Bug: 1277769
    Change-Id: I132529842d633122cde1d39254efb376c73e07f5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378664
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Owners-Override: Mikel Astiz <mastiz@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#957473}

--- ash/wm/ash_focus_rules.cc
+++ ash/wm/ash_focus_rules.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/container_finder.h"
 #include "ash/wm/desks/desks_util.h"
 #include "ash/wm/mru_window_tracker.h"
-#include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/window_restore/window_restore_controller.h"
 #include "ash/wm/window_state.h"
 #include "base/containers/contains.h"
@@ -155,13 +154,7 @@ aura::Window* AshFocusRules::GetNextActivatableWindow(
   // start from the container of |ignore|.
   aura::Window* starting_window = nullptr;
   aura::Window* transient_parent = ::wm::GetTransientParent(ignore);
-  OverviewController* overview_controller = Shell::Get()->overview_controller();
-  if (overview_controller->InOverviewSession() &&
-      overview_controller->overview_session()->IsTemplatesUiLosingActivation(
-          ignore)) {
-    starting_window =
-        overview_controller->overview_session()->GetOverviewFocusWindow();
-  } else if (transient_parent) {
+  if (transient_parent) {
     starting_window = transient_parent;
   } else {
     MruWindowTracker* mru = Shell::Get()->mru_window_tracker();
