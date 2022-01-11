5054fcdf51a2d48a996f4aaaeb7f26c4c47e8d40
Michael Checo <michaelcheco@google.com>
2022-01-11 01:02:46 +0000

    Firmware UI: Implement StartUpdate
    Bug: 1252981
    Test: ash_components_unittests
    Change-Id: I79e22606d0e67de5ba7ba1b579aabeeb1964acbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336516
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#957358}

--- ash/webui/firmware_update_ui/resources/fake_update_provider.js
+++ ash/webui/firmware_update_ui/resources/fake_update_provider.js
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {FakeObservables} from 'chrome://resources/ash/common/fake_observables.js';
 
-import {FirmwareUpdate, UpdateObserver, UpdateProviderInterface} from './firmware_update_types.js';
-import {setUseFakeProviders} from './mojo_interface_provider.js';
+import {FakeInstallControllerInterface, FirmwareUpdate, InstallControllerInterface, UpdateObserver, UpdateProviderInterface} from './firmware_update_types.js';
+import {getUpdateController, getUpdateProvider, setUseFakeProviders} from './mojo_interface_provider.js';
 
 // Method names.
 export const ON_UPDATE_LIST_CHANGED = 'UpdateObserver_onUpdateListChanged';
@@ -38,6 +38,17 @@ export class FakeUpdateProvider {
         this.observe_(ON_UPDATE_LIST_CHANGED, (firmwareUpdates) => {
           remote.onUpdateListChanged(firmwareUpdates);
         });
+  }
+
+  /**
+   * @param {string} deviceId
+   * @return {!Promise}
+   */
+  prepareForUpdate(deviceId) {
+    /** @type {InstallControllerInterface|FakeInstallControllerInterface} */
+    const controller = getUpdateController();
+    controller.setDeviceIdForUpdateInProgress(deviceId);
+    return new Promise((resolve) => resolve({controller}));
   }
 
   /**
