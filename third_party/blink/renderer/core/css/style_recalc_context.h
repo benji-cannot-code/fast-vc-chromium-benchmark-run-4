789c319bb4359653831382b00dba8bce96264e13
Rune Lillesveen <futhark@chromium.org>
2022-01-12 15:35:17 +0000

    [@container] Look for shadow-including ancestor containers
    The proposed spec for shadow dom is that containers in the
    shadow-including ancestry of the selector's root originating element can
    be queried.
    
    This CL modified FromAncestors and FromInclusiveAncestors to walk
    shadow-including ancestors instead of the flat tree.
    
    Using the correct container for the originating element for ::slotted()
    and ::part() will be done in follow-up CLs.
    
    Bug: 1269192
    Change-Id: I95b8ee2e802c22f9a42487e37f8cf8cba55ce552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3347991
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958089}

--- third_party/blink/renderer/core/css/style_recalc_context.h
+++ third_party/blink/renderer/core/css/style_recalc_context.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_STYLE_RECALC_CONTEXT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_STYLE_RECALC_CONTEXT_H_
 
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 
 namespace blink {
@@ -18,7 +19,7 @@ class Element;
 // Its purpose is to hold context related to the style recalc process as
 // a whole, i.e. information not directly associated to the specific element
 // style is being calculated for.
-class StyleRecalcContext {
+class CORE_EXPORT StyleRecalcContext {
   STACK_ALLOCATED();
 
  public:
