a99412c2141e7d74e70ffbac5164233dbb46f71a
Mirko Bonadei <mbonadei@chromium.org>
2022-01-12 12:12:40 +0000

    Roll abseil_revision c498947f8c..2a042b082c
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/c498947f8c..2a042b082c
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/c498947f8c..2a042b082c
    
    Bug: None
    Change-Id: I8686283211108e57099af8c83b333d0a1fd1a367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378349
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958026}

--- third_party/abseil-cpp/absl/strings/internal/str_format/output.h
+++ third_party/abseil-cpp/absl/strings/internal/str_format/output.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ABSL_STRINGS_INTERNAL_STR_FORMAT_OUTPUT_H_
 
 #include <cstdio>
+#include <ios>
 #include <ostream>
 #include <string>
 
@@ -72,7 +73,7 @@ inline void AbslFormatFlush(std::string* out, string_view s) {
   out->append(s.data(), s.size());
 }
 inline void AbslFormatFlush(std::ostream* out, string_view s) {
-  out->write(s.data(), s.size());
+  out->write(s.data(), static_cast<std::streamsize>(s.size()));
 }
 
 inline void AbslFormatFlush(FILERawSink* sink, string_view v) {
