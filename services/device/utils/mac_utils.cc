487ee248075991ba0215f1091cc81040e72fc81a
Jack Hsieh <chengweih@chromium.org>
2022-01-12 20:29:17 +0000

    Move shared code of HID and USB on macOS into a utils file.
    Create utils folder to hold common utility codes that will be shared
    across modules in the service/device directory. In this change list, it
    moves GetStringProperty, GetIntegerProperty, and HexErrorCode into the
    utils.
    
    Bug: 1104271
    Change-Id: I8592c571302b0f8f28628da6add413d6d4228ab9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3374301
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958254}

---
+++ services/device/utils/mac_utils.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/device/utils/mac_utils.h"
+#include "base/strings/stringprintf.h"
+
+namespace device {
+
+std::string HexErrorCode(IOReturn error_code) {
+  return base::StringPrintf("0x%04x", error_code);
+}
+
+}  // namespace device
