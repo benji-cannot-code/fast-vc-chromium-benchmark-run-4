c3af3f93ca7933657e6a073b88b10f64e6eddb4e
Matt Jones <mdjones@chromium.org>
2022-01-12 21:07:27 +0000

    Reland "[Shopping] Ensure bookmark model is loaded before unsubscribing"
    This is a reland of 0b462d2e0d7628e674217ca5c63c8be4375381a2
    
    Patch set 1 -> 2 has the addition of a BookmarkBridge for testing so
    we don't attempt to access the profile.
    
    Original change's description:
    > [Shopping] Ensure bookmark model is loaded before unsubscribing
    >
    > This patch attempts to load the bookmark model when turning off
    > notifications. This allows us to sync the state of bookmarks if the
    > browser hasn't been started.
    >
    > Bug: 1284730
    > Change-Id: I8b75d7d0bc6f281affba84f166158d5518451af2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381397
    > Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    > Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    > Commit-Queue: Matthew Jones <mdjones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#957780}
    
    Bug: 1284730
    Change-Id: I214a0637785a233623901a5553998ee71374ab25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383076
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958277}

--- chrome/browser/commerce/price_tracking/android/javatests/src/org/chromium/chrome/browser/price_tracking/PriceDropNotificationManagerTest.java
+++ chrome/browser/commerce/price_tracking/android/javatests/src/org/chromium/chrome/browser/price_tracking/PriceDropNotificationManagerTest.java
@@ -15,6 +15,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import android.app.NotificationManager;
 import android.content.Intent;
@@ -39,6 +40,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.chrome.browser.bookmarks.BookmarkBridge;
 import org.chromium.chrome.browser.browserservices.intents.WebappConstants;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -99,11 +101,16 @@ public class PriceDropNotificationManagerTest {
     @Mock
     private SubscriptionsManagerImpl mMockSubscriptionsManager;
 
+    @Mock
+    private BookmarkBridge mMockBookmarkBridge;
+
     @Before
     public void setUp() {
         mMockNotificationManager = new MockNotificationManagerProxy();
         PriceDropNotificationManager.setNotificationManagerForTesting(mMockNotificationManager);
         mPriceDropNotificationManager = new PriceDropNotificationManager();
+        when(mMockBookmarkBridge.isBookmarkModelLoaded()).thenReturn(true);
+        PriceDropNotificationManager.setBookmarkBridgeForTesting(mMockBookmarkBridge);
     }
 
     @After
