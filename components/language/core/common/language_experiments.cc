587ff0739efc89e3c10dc56bf53b991421c8d3a4
Anthony Cui <cuianthony@google.com>
2022-01-12 23:42:18 +0000

    Remove UseULPLanguagesInChrome flag code
    Bug: 1267379
    Change-Id: I7f1f3140eaf5ebfe4d2587a1efb8da8a6498233e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383961
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958347}

--- components/language/core/common/language_experiments.cc
+++ components/language/core/common/language_experiments.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
-#include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
