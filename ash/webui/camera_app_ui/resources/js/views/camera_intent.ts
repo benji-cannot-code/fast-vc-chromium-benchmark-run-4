60e848c5e0c4d1102c1d1928099bed7b4b3ee0af
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-12 05:20:11 +0000

    CCA: Migrate views/camera_intent.js to TypeScript
    Bug: b:172340451
    Test: tsc compiles
    Change-Id: Ide6e8bdf994d04f9ea04587ecad6408a81c62e07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3325369
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957936}

--- ash/webui/camera_app_ui/resources/js/views/camera_intent.ts
+++ ash/webui/camera_app_ui/resources/js/views/camera_intent.ts
@@ -3,117 +3,81 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// eslint-disable-next-line no-unused-vars
 import {assert, assertNotReached} from '../assert.js';
 import {
-  PhotoConstraintsPreferrer,  // eslint-disable-line no-unused-vars
-  VideoConstraintsPreferrer,  // eslint-disable-line no-unused-vars
+  PhotoConstraintsPreferrer,
+  VideoConstraintsPreferrer,
 } from '../device/constraints_preferrer.js';
-// eslint-disable-next-line no-unused-vars
 import {DeviceInfoUpdater} from '../device/device_info_updater.js';
 import {I18nString} from '../i18n_string.js';
-// eslint-disable-next-line no-unused-vars
 import {Intent} from '../intent.js';
 import * as metrics from '../metrics.js';
-// eslint-disable-next-line no-unused-vars
 import {FileAccessEntry} from '../models/file_system_access_entry.js';
-// eslint-disable-next-line no-unused-vars
-import {ResultSaver} from '../models/result_saver.js';
 import {VideoSaver} from '../models/video_saver.js';
-// eslint-disable-next-line no-unused-vars
 import {PerfLogger} from '../perf.js';
 import {scaleImage} from '../thumbnailer.js';
-// eslint-disable-next-line no-unused-vars
 import {Mode, Resolution} from '../type.js';
 import * as util from '../util.js';
 
 import {Camera} from './camera.js';
-// eslint-disable-next-line no-unused-vars
 import {PhotoResult, VideoResult} from './camera/mode/index.js';
 import * as review from './review.js';
 
 /**
  * The maximum number of pixels in the downscaled intent photo result. Reference
  * from GCA: https://goto.google.com/gca-inline-bitmap-max-pixel-num
- * @type {number}
- * @const
  */
 const DOWNSCALE_INTENT_MAX_PIXEL_NUM = 50 * 1024;
 
-/**
- * @typedef {{
- *   resolution: !Resolution,
- *   duration?: number,
- * }}
- */
-let MetricArgs;  // eslint-disable-line no-unused-vars
+interface MetricArgs {
+  resolution: Resolution;
+  duration?: number;
+}
 
 /**
  * Camera-intent-view controller.
  */
 export class CameraIntent extends Camera {
-  /**
-   * @param {!Intent} intent
-   * @param {!DeviceInfoUpdater} infoUpdater
-   * @param {!PhotoConstraintsPreferrer} photoPreferrer
-   * @param {!VideoConstraintsPreferrer} videoPreferrer
-   * @param {!Mode} mode
-   * @param {!PerfLogger} perfLogger
-   */
+  private videoResultFile: FileAccessEntry|null = null;
+
   constructor(
-      intent, infoUpdater, photoPreferrer, videoPreferrer, mode, perfLogger) {
-    const resultSaver = /** @type {!ResultSaver} */ ({
-      savePhoto: async (blob) => {
-        if (intent.shouldDownScale) {
-          const image = await util.blobToImage(blob);
-          const ratio = Math.sqrt(
-              DOWNSCALE_INTENT_MAX_PIXEL_NUM / (image.width * image.height));
-          blob = await scaleImage(
-              blob, Math.floor(image.width * ratio),
-              Math.floor(image.height * ratio));
-        }
-        const buf = await blob.arrayBuffer();
-        await this.intent_.appendData(new Uint8Array(buf));
-      },
-      startSaveVideo: async (outputVideoRotation) => {
-        return VideoSaver.createForIntent(intent, outputVideoRotation);
-      },
-      finishSaveVideo: async (video) => {
-        this.videoResultFile_ = await video.endWrite();
-      },
-      saveGif: () => {
-        assertNotReached();
-      },
-    });
+      private readonly intent: Intent,
+      infoUpdater: DeviceInfoUpdater,
+      photoPreferrer: PhotoConstraintsPreferrer,
+      videoPreferrer: VideoConstraintsPreferrer,
+      mode: Mode,
+      perfLogger: PerfLogger,
+  ) {
     super(
-        resultSaver, infoUpdater, photoPreferrer, videoPreferrer, mode,
-        perfLogger, /* facing= */ null);
-
-    /**
-     * @type {!Intent}
-     * @private
-     */
-    this.intent_ = intent;
-
-    /**
-     * @type {?PhotoResult}
-     * @private
-     */
-    this.photoResult_ = null;
-
-    /**
-     * @type {?FileAccessEntry}
-     * @private
-     */
-    this.videoResultFile_ = null;
+        {
+          savePhoto: async (blob) => {
+            if (intent.shouldDownScale) {
+              const image = await util.blobToImage(blob);
+              const ratio = Math.sqrt(
+                  DOWNSCALE_INTENT_MAX_PIXEL_NUM /
+                  (image.width * image.height));
+              blob = await scaleImage(
+                  blob, Math.floor(image.width * ratio),
+                  Math.floor(image.height * ratio));
+            }
+            const buf = await blob.arrayBuffer();
+            await this.intent.appendData(new Uint8Array(buf));
+          },
+          startSaveVideo: async (outputVideoRotation) => {
+            return VideoSaver.createForIntent(intent, outputVideoRotation);
+          },
+          finishSaveVideo: async (video) => {
+            this.videoResultFile = await video.endWrite();
+          },
+          saveGif: () => {
+            assertNotReached();
+          },
+        },
+        infoUpdater, photoPreferrer, videoPreferrer, mode, perfLogger,
+        /* facing= */ null);
   }
 
-  /**
-   * @param {!MetricArgs} metricArgs
-   * @return {!Promise<void>}
-   * @private
-   */
-  reviewIntentResult_(metricArgs) {
+  private reviewIntentResult(metricArgs: MetricArgs): Promise<void> {
     return this.prepareReview(async () => {
       const confirmed = await this.review.startReview(new review.OptionGroup({
         template: review.ButtonGroupTemplate.intent,
@@ -131,7 +95,6 @@ export class CameraIntent extends Camera {
               },
               {exitValue: false}),
         ],
-
       }));
       metrics.sendCaptureEvent({
         facing: this.facingMode,
@@ -141,8 +104,7 @@ export class CameraIntent extends Camera {
         shutterType: this.shutterType,
       });
       if (confirmed) {
-        await this.intent_.finish();
-
+        await this.intent.finish();
         const appWindow = window.appWindow;
         if (appWindow === null) {
           window.close();
@@ -152,36 +114,28 @@ export class CameraIntent extends Camera {
           await appWindow.notifyClosingItself();
         }
       } else {
-        await this.intent_.clearData();
+        await this.intent.clearData();
       }
     });
   }
 
-  /**
-   * @override
-   */
-  async onPhotoCaptureDone(pendingPhotoResult) {
+  async onPhotoCaptureDone(pendingPhotoResult: Promise<PhotoResult>):
+      Promise<void> {
     await super.onPhotoCaptureDone(pendingPhotoResult);
     const {blob, resolution} = await pendingPhotoResult;
     await this.review.setReviewPhoto(blob);
-    await this.reviewIntentResult_({resolution});
+    await this.reviewIntentResult({resolution});
   }
 
-  /**
-   * @override
-   */
-  async onVideoCaptureDone(videoResult) {
+  async onVideoCaptureDone(videoResult: VideoResult): Promise<void> {
     await super.onVideoCaptureDone(videoResult);
-    assert(this.videoResultFile_ !== null);
-    await this.review.setReviewVideo(this.videoResultFile_);
-    await this.reviewIntentResult_(
+    assert(this.videoResultFile !== null);
+    await this.review.setReviewVideo(this.videoResultFile);
+    await this.reviewIntentResult(
         {resolution: videoResult.resolution, duration: videoResult.duration});
   }
 
-  /**
-   * @override
-   */
-  async startWithDevice(deviceId) {
+  async startWithDevice(deviceId: string): Promise<boolean> {
     return this.startWithMode(deviceId, this.defaultMode);
   }
 }
