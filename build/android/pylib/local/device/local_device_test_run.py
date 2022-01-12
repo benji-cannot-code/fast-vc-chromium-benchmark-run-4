c543926ea222bf50f3bdbd9f2e7012ceb725076f
Takuto Ikuta <tikuta@chromium.org>
2022-01-12 03:41:47 +0000

    android: use deterministic hash to sort test
    hash is not deterministic in python3.
    
    ref: https://crbug.com/1279388#c19
    
    Bug: 1279388
    Change-Id: Idb8af272a5b1140fff3dde2423bc76ea4e61dc65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379883
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957911}

--- build/android/pylib/local/device/local_device_test_run.py
+++ build/android/pylib/local/device/local_device_test_run.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import fnmatch
+import hashlib
 import logging
 import posixpath
 import signal
@@ -275,10 +276,10 @@ class LocalDeviceTestRun(test_run.TestRun):
   # Sort by hash so we don't put all tests in a slow suite in the same
   # partition.
   def _SortTests(self, tests):
-    return sorted(
-        tests,
-        key=lambda t: hash(
-            self._GetUniqueTestName(t[0] if isinstance(t, list) else t)))
+    return sorted(tests,
+                  key=lambda t: hashlib.sha256(
+                      self._GetUniqueTestName(t[0] if isinstance(t, list) else t
+                                              ).encode()).hexdigest())
 
   # Partition tests evenly into |num_desired_partitions| partitions where
   # possible. However, many constraints make partitioning perfectly impossible.
