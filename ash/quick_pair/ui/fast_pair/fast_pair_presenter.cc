510ed6d2d5b0872a07cd4e78df90ea9284289fb4
Juliet Levesque <julietlevesque@google.com>
2022-01-12 20:32:56 +0000

    Reland "[Fast Pair] Add notification timeout and pin notifications."
    This is a reland of 63ce066a79bd0979f51beb5a79f9b0f3d6cb6c5c
    
    The reason why it crashed previously was that the FastPairPresenter
    doesn't expliclity initialize FastPairNotificationController, so the
    timer and weak pointer were not properly initialized before use. The
    reason I didn't catch this before is because I was using
    chrome://nearby-internals to verify the notification behavior, and the
    web handler for chrome://nearby-internals explicitly initializes the
    FastPairNotificationController.
    
    I have verified on ToT by completing the full Fast Pair pairing flow
    that this fixes the crash.
    
    Original change's description:
    > [Fast Pair] Add notification timeout and pin notifications.
    >
    > Adds a 30 second timeout to remove the Discovery and Associate Account
    > notifications, as follows:
    > https://drive.google.com/file/d/1li64LA_3YYS9Thd1ekY_0WVyLNYt9XHc/view?usp=sharing&resourcekey=0-jM7gfq50n7XisO8_aJo6Og
    >
    > Fixed: b/213503021, b/213503274
    > Change-Id: Iedadc2d4e769cf20fa9f96611e94ab2b58b50f80
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372945
    > Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    > Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    > Cr-Commit-Position: refs/heads/main@{#957122}
    
    Fixed: b/214111281
    Change-Id: I3b937928b3e7e783bd33eabe0856dff6002db5a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383675
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Juliet Levesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#958258}

--- ash/quick_pair/ui/fast_pair/fast_pair_presenter.cc
+++ ash/quick_pair/ui/fast_pair/fast_pair_presenter.cc
@@ -32,7 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace quick_pair {
 
-FastPairPresenter::FastPairPresenter() = default;
+FastPairPresenter::FastPairPresenter()
+    : notification_controller_(
+          std::make_unique<FastPairNotificationController>()) {}
+
 FastPairPresenter::~FastPairPresenter() = default;
 
 void FastPairPresenter::ShowDiscovery(scoped_refptr<Device> device,
