98a63ccf36e1d09fea531ef59cf8a2c2f5473554
Florian Gauger <fga@google.com>
2022-01-12 09:07:52 +0000

    [Autofill Assistant] Move optimization guide instantiation to dependencies
    Bug: b/173103628
    Change-Id: Ibc1cae8b0d93e94cbf970b87b3687854d8b2d8f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380916
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#957987}

--- chrome/browser/android/autofill_assistant/dependencies_chrome.h
+++ chrome/browser/android/autofill_assistant/dependencies_chrome.h
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/strings/string_piece.h"
 #include "chrome/browser/android/autofill_assistant/assistant_field_trial_util.h"
+#include "components/autofill_assistant/content/browser/annotate_dom_model_service.h"
 #include "components/metrics/metrics_service_accessor.h"
 #include "components/variations/service/variations_service.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 
 namespace autofill_assistant {
@@ -31,6 +33,9 @@ class DependenciesChrome : public Dependencies {
 
   std::string GetChromeSignedInEmailAddress(
       content::WebContents* web_contents) const override;
+
+  AnnotateDomModelService* GetAnnotateDomModelService(
+      content::BrowserContext* browser_context) const override;
 };
 
 }  // namespace autofill_assistant
