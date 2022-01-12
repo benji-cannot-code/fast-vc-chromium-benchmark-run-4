a6b3126e8d7e0299fe87c8522da193af1a2d4f4c
Yuki Yamada <yukiy@chromium.org>
2022-01-12 12:57:28 +0000

    Layout: Add missing NOT_DESTROYED()
    This adds CheckIsNotDestroyed() call at the beginning of every methods
    of LayoutObjects and its children. It's required to enable the
    "check-layout-object-methods" clang plugin.
    
    This is a follow-up for https://crrev.com/c/3351679.
    
    Bug: 1030176, 1183625
    Change-Id: Ia9c58f88567f8c9c93e269a3b908a4574b12419f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379796
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Auto-Submit: Yuki Yamada <yukiy@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958036}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -7791,11 +7791,13 @@ LayoutRect LayoutBox::VisualOverflowRect() const {
 
 #if DCHECK_IS_ON()
 LayoutRect LayoutBox::VisualOverflowRectAllowingUnset() const {
+  NOT_DESTROYED();
   NGInkOverflow::ReadUnsetAsNoneScope read_unset_as_none;
   return VisualOverflowRect();
 }
 
 PhysicalRect LayoutBox::PhysicalVisualOverflowRectAllowingUnset() const {
+  NOT_DESTROYED();
   NGInkOverflow::ReadUnsetAsNoneScope read_unset_as_none;
   return PhysicalVisualOverflowRect();
 }
