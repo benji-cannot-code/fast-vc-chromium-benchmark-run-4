2091d22d3af0ccce782d80247844af22382915b4
Sylvain Defresne <sdefresne@chromium.org>
2022-01-12 11:52:47 +0000

    Revert "[ios] Expose `app_utils` API as free functions"
    This reverts commit b885d8fd00405a46bc1ab5b8e8665c8d8b9caec0.
    
    Reason for revert: Breaks the internal repository.
    
    Original change's description:
    > [ios] Expose `app_utils` API as free functions
    >
    > Add three free functions exposing the `app_utils` API with the goal
    > to replace app related methods of ChromeBrowserProvider class, and
    > use them to implement the old methods.
    >
    > This is part of a multi-CL change:
    >  => https://crrev.com/c/3381137
    >   - https://crrev.com/i/4447060
    >   - https://crrev.com/c/3380848
    >   - https://crrev.com/i/4451096
    >
    > Bug: 1201182
    > Change-Id: Iade9b0daf3bd18413c5a354a0ccac14c1af9ddc1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381137
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Mark Cogan <marq@chromium.org>
    > Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#958018}
    
    Bug: 1201182
    Change-Id: Ia3b82056a8835811052574d7c8f25c5b740e8d4a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379998
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958020}

--- ios/public/provider/chrome/browser/chrome_browser_provider.mm
+++ ios/public/provider/chrome/browser/chrome_browser_provider.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "components/metrics/metrics_provider.h"
-#import "ios/public/provider/chrome/browser/app_utils/app_utils_api.h"
 #import "ios/public/provider/chrome/browser/mailto/mailto_handler_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #import "ios/public/provider/chrome/browser/ui_utils/ui_utils_api.h"
@@ -50,10 +49,7 @@ ChromeBrowserProvider::~ChromeBrowserProvider() {
 
 void ChromeBrowserProvider::AppendSwitchesFromExperimentalSettings(
     NSUserDefaults* experimental_settings,
-    base::CommandLine* command_line) const {
-  provider::AppendSwitchesFromExperimentalSettings(experimental_settings,
-                                                   command_line);
-}
+    base::CommandLine* command_line) const {}
 
 void ChromeBrowserProvider::Initialize() const {}
 
@@ -79,9 +75,7 @@ UITextField* ChromeBrowserProvider::CreateStyledTextField() const {
   return provider::CreateStyledTextField();
 }
 
-void ChromeBrowserProvider::AttachBrowserAgents(Browser* browser) const {
-  provider::AttachBrowserAgents(browser);
-}
+void ChromeBrowserProvider::AttachBrowserAgents(Browser* browser) const {}
 
 id<LogoVendor> ChromeBrowserProvider::CreateLogoVendor(
     Browser* browser,
