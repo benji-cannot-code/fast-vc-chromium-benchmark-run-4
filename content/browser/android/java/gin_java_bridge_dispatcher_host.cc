0111386ecaf8f89159b1f98c0f870b9ca2cc993f
Dave Tapuska <dtapuska@chromium.org>
2022-01-12 19:21:17 +0000

    Replace ForEachFrame with GetMainFrame()->ForEachRenderFrameHost.
    The JS dispatching bridge is used for WebView and WebLayer from Android.
    These environments do not support back/forward cache nor prerender so
    converting to GetMainFrame()->ForEachRenderFrameHost is correct and
    conveys the intent of the method.
    
    BUG=1208438
    
    Change-Id: I22be31a3985df8255db568f0ed12750972689f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381934
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958212}

--- content/browser/android/java/gin_java_bridge_dispatcher_host.cc
+++ content/browser/android/java/gin_java_bridge_dispatcher_host.cc
@@ -56,8 +56,8 @@ void GinJavaBridgeDispatcherHost::InstallFilterAndRegisterAllRoutingIds() {
     return;
   }
 
-  // Unretained() is safe because ForEachFrame() is synchronous.
-  web_contents()->ForEachFrame(base::BindRepeating(
+  // Unretained() is safe because ForEachRenderFrameHost() is synchronous.
+  web_contents()->GetMainFrame()->ForEachRenderFrameHost(base::BindRepeating(
       [](GinJavaBridgeDispatcherHost* host, RenderFrameHost* frame) {
         AgentSchedulingGroupHost& agent_scheduling_group =
             static_cast<RenderFrameHostImpl*>(frame)->GetAgentSchedulingGroup();
@@ -101,8 +101,8 @@ void GinJavaBridgeDispatcherHost::RenderFrameCreated(
 }
 
 void GinJavaBridgeDispatcherHost::WebContentsDestroyed() {
-  // Unretained() is safe because ForEachFrame() is synchronous.
-  web_contents()->ForEachFrame(base::BindRepeating(
+  // Unretained() is safe because ForEachRenderFrameHost() is synchronous.
+  web_contents()->GetMainFrame()->ForEachRenderFrameHost(base::BindRepeating(
       [](GinJavaBridgeDispatcherHost* host, RenderFrameHost* frame) {
         AgentSchedulingGroupHost& agent_scheduling_group =
             static_cast<RenderFrameHostImpl*>(frame)->GetAgentSchedulingGroup();
