d40eec0031acebebdeb179aba7d2803640ddf3df
Tim Sergeant <tsergeant@chromium.org>
2022-01-12 07:33:54 +0000

    Intents: Remove obsolete app-service-external-protocol flag
    This flag does not have any implementation, and we don't have any
    near-term plans to change that.
    
    Bug: 853604
    Change-Id: Ic7c3cf30ad3ae3fc1f613d9bcc300b4413c4a8c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378997
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957967}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -76,8 +76,6 @@ extern const base::Feature kAppManagementIntentSettings;
 
 #if !defined(OS_ANDROID)
 COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::Feature kAppServiceExternalProtocol;
-COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kAppServiceLoadIconWithoutMojom;
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kAppServiceExtension;
