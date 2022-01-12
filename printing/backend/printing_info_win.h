1d6f43395e503ddd2bac90b963ecba0ea31782b6
Lei Zhang <thestig@chromium.org>
2022-01-12 08:57:27 +0000

    Modernize PrinterInfo/DriverInfo code.
    - Replace uses of the new keyword. Use more std::unique_ptrs.
    - Use nullptr everywhere.
    - Replace typedef statements with using statements.
    
    Change-Id: Ie33f8aa82d2668b8770139ea6e41603a226a332d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379092
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957984}

--- printing/backend/printing_info_win.h
+++ printing/backend/printing_info_win.h
@@ -19,9 +19,9 @@ namespace printing {
 namespace internal {
 
 COMPONENT_EXPORT(PRINT_BACKEND)
-uint8_t* GetDriverInfo(HANDLE printer, int level);
+std::unique_ptr<uint8_t[]> GetDriverInfo(HANDLE printer, int level);
 COMPONENT_EXPORT(PRINT_BACKEND)
-uint8_t* GetPrinterInfo(HANDLE printer, int level);
+std::unique_ptr<uint8_t[]> GetPrinterInfo(HANDLE printer, int level);
 
 // This class is designed to work with PRINTER_INFO_X structures
 // and calls GetPrinter internally with correctly allocated buffer.
@@ -29,7 +29,7 @@ template <typename PrinterInfoType, int level>
 class PrinterInfo {
  public:
   bool Init(HANDLE printer) {
-    buffer_.reset(GetPrinterInfo(printer, level));
+    buffer_ = GetPrinterInfo(printer, level);
     return buffer_ != nullptr;
   }
 
@@ -47,7 +47,7 @@ template <typename DriverInfoType, int level>
 class DriverInfo {
  public:
   bool Init(HANDLE printer) {
-    buffer_.reset(GetDriverInfo(printer, level));
+    buffer_ = GetDriverInfo(printer, level);
     return buffer_ != nullptr;
   }
 
@@ -61,10 +61,10 @@ class DriverInfo {
 
 }  // namespace internal
 
-typedef internal::PrinterInfo<PRINTER_INFO_2, 2> PrinterInfo2;
-typedef internal::PrinterInfo<PRINTER_INFO_5, 5> PrinterInfo5;
+using PrinterInfo2 = internal::PrinterInfo<PRINTER_INFO_2, 2>;
+using PrinterInfo5 = internal::PrinterInfo<PRINTER_INFO_5, 5>;
 
-typedef internal::DriverInfo<DRIVER_INFO_6, 6> DriverInfo6;
+using DriverInfo6 = internal::DriverInfo<DRIVER_INFO_6, 6>;
 
 }  // namespace printing
 
