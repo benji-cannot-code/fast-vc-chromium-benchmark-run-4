9fbb1d2aef7f5ca72fd316905ca0cc5e062c8ebb
Simon Hangl <simonha@google.com>
2022-01-12 14:54:08 +0000

    Isolated apps can open cross-origin links externally.
    So far, a click on a cross-origin link / redirect in an isolated app
    resulted in the navigation being cancelled.
    With this change, a cross-origin link / redirect will still be
    cancelled in the original window, but instead will be opened
    externally in the systems' default application.
    
    Bug: 1273345
    Change-Id: I98c46e6d8022dea15a29d3010525527e6611e3c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350651
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#958074}

--- content/browser/renderer_host/isolated_app_throttle_unittest.cc
+++ content/browser/renderer_host/isolated_app_throttle_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
+#include "ui/base/page_transition_types.h"
 #include "url/origin.h"
 
 namespace content {
@@ -41,6 +42,41 @@ class IsolatedAppContentBrowserClient : public ContentBrowserClient {
                                              const GURL& url) override {
     return url::Origin::Create(url) == url::Origin::Create(GURL(kAppUrl));
   }
+
+  bool HandleExternalProtocol(
+      const GURL& url,
+      base::RepeatingCallback<WebContents*()> web_contents_getter,
+      int child_id,
+      int frame_tree_node_id,
+      NavigationUIData* navigation_data,
+      bool is_main_frame,
+      network::mojom::WebSandboxFlags sandbox_flags,
+      ui::PageTransition page_transition,
+      bool has_user_gesture,
+      const absl::optional<url::Origin>& initiating_origin,
+      RenderFrameHost* initiator_document,
+      mojo::PendingRemote<network::mojom::URLLoaderFactory>* out_factory)
+      override {
+    external_protocol_call_count_++;
+    last_page_transition_ = page_transition;
+    return true;
+  }
+
+  unsigned int GetExternalProtocolCallCount() const {
+    return external_protocol_call_count_;
+  }
+
+  ui::PageTransition GetLastPageTransition() { return last_page_transition_; }
+
+  void ResetExternalProtocolCallCount() {
+    external_protocol_call_count_ = 0;
+    last_page_transition_ = ui::PageTransition::PAGE_TRANSITION_QUALIFIER_MASK;
+  }
+
+ private:
+  unsigned int external_protocol_call_count_ = 0;
+  ui::PageTransition last_page_transition_ =
+      ui::PageTransition::PAGE_TRANSITION_QUALIFIER_MASK;
 };
 
 }  // namespace
@@ -132,6 +168,8 @@ class IsolatedAppThrottleTest : public RenderViewHostTestHarness {
     return coop_coep_headers_;
   }
 
+  IsolatedAppContentBrowserClient& GetBrowserClient() { return test_client_; }
+
   scoped_refptr<net::HttpResponseHeaders> corp_coep_headers() {
     return corp_coep_headers_;
   }
@@ -217,6 +255,18 @@ TEST_F(IsolatedAppThrottleTest, CancelCrossOriginNavigation) {
 
   auto start_result = simulator->GetLastThrottleCheckResult();
   EXPECT_EQ(NavigationThrottle::CANCEL, start_result.action());
+  EXPECT_EQ(1u, GetBrowserClient().GetExternalProtocolCallCount());
+  EXPECT_TRUE(ui::PageTransitionTypeIncludingQualifiersIs(
+      GetBrowserClient().GetLastPageTransition(),
+      ui::PageTransition::PAGE_TRANSITION_LINK));
+
+  simulator = StartRendererInitiatedNavigation(main_frame_id(), kNonAppUrl2);
+  start_result = simulator->GetLastThrottleCheckResult();
+  EXPECT_EQ(NavigationThrottle::CANCEL, start_result.action());
+  EXPECT_EQ(2u, GetBrowserClient().GetExternalProtocolCallCount());
+  EXPECT_TRUE(ui::PageTransitionTypeIncludingQualifiersIs(
+      GetBrowserClient().GetLastPageTransition(),
+      ui::PageTransition::PAGE_TRANSITION_LINK));
 }
 
 TEST_F(IsolatedAppThrottleTest, BlockNavigationWithinIsolatedAppWithBadCoop) {
@@ -252,6 +302,10 @@ TEST_F(IsolatedAppThrottleTest, BlockRedirectOutOfIsolatedApp) {
 
   auto redirect_result = simulator->GetLastThrottleCheckResult();
   EXPECT_EQ(NavigationThrottle::CANCEL, redirect_result.action());
+  EXPECT_EQ(1u, GetBrowserClient().GetExternalProtocolCallCount());
+  EXPECT_TRUE(ui::PageTransitionTypeIncludingQualifiersIs(
+      GetBrowserClient().GetLastPageTransition(),
+      ui::PageTransition::PAGE_TRANSITION_SERVER_REDIRECT));
 }
 
 TEST_F(IsolatedAppThrottleTest, AllowIframeNavigationOutOfApp) {
