095771b205654d960e906603dc31237f1369097f
Daniel Vogelheim <vogelheim@chromium.org>
2022-01-12 13:30:08 +0000

    [Sanitizer API] Rework name handling.
    This is a preparatory CL for support of namespaced content in the
    Sanitizer (SVG + MathML). This moves handling of element and attribute
    names (converting between API & internal representation, and name
    matching) to one set of functions. This should then ease extending
    these functions to cover SVG + MathML content.
    
    Bug: 1225606
    Change-Id: I246cfd3c088611594cce61ee8818098889d98600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366414
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958043}

--- third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
+++ third_party/blink/renderer/modules/sanitizer_api/sanitizer.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SANITIZER_API_SANITIZER_H_
 
 #include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
-#include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/sanitizer_api/sanitizer_config_impl.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
@@ -18,8 +17,11 @@ namespace blink {
 class ContainerNode;
 class Document;
 class DocumentFragment;
+class Element;
 class ExceptionState;
 class ExecutionContext;
+class Node;
+class LocalDOMWindow;
 class SanitizerConfig;
 class ScriptState;
 
@@ -57,16 +59,9 @@ class MODULES_EXPORT Sanitizer final : public ScriptWrappable {
   void Trace(Visitor*) const override;
 
  private:
-  bool AttrListMatches(const HashMap<String, Vector<String>>&,
-                       const String& attr,
-                       const String& element);
-  Node* DropElement(Node*, ContainerNode*);
-  Node* BlockElement(Node*, ContainerNode*, ExceptionState&);
-  Node* KeepElement(Node*, ContainerNode*, String&, LocalDOMWindow*);
-
-  void ElementFormatter(HashSet<String>&, const Vector<String>&);
-  void AttrFormatter(HashMap<String, Vector<String>>&,
-                     const Vector<std::pair<String, Vector<String>>>&);
+  Node* DropNode(Node*, ContainerNode*);
+  Node* BlockElement(Element*, ContainerNode*, ExceptionState&);
+  Node* KeepElement(Element*, ContainerNode*, LocalDOMWindow*);
 
   DocumentFragment* PrepareFragment(LocalDOMWindow* window,
                                     ScriptState* script_state,
