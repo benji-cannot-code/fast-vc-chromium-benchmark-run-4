8289dc916fd6a690e9258824f55f262ca702ef92
Alexander Dunaev <adunaev@igalia.com>
2022-01-12 09:04:00 +0000

    [linux/wayland] Fixed the pinch zoom.
    Blink handles the pinch zoom in Viewport::PinchUpdate() [1] that expects
    a "magnify delta" and applies it to the current scale of the page.  When
    the user moves their fingers, the scale is updated on every change to
    their position, and every time the page scale should be updated to the
    delta calculated between the current step and the previous one.
    
    However, libinput does it different way.  With every update, it sends
    the absolute scale compared to the beginning of the gesture [2].  The
    platform currently sends that scale to Blink, which causes the page
    zooming with fast acceleration.
    
    This patch fixes the situation by replacing the "accumulated" scale with
    its delta to the previous step.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/master:cc/layers/viewport.cc;l=215
    [2] https://wayland.freedesktop.org/libinput/doc/latest/gestures.html#pinch-gestures
    
    Bug: 1283652
    Change-Id: I4747e941de2f8b53fa4402d807bee128576a35dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3369422
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#957986}

--- ui/ozone/platform/wayland/test/test_wayland_server_thread.h
+++ ui/ozone/platform/wayland/test/test_wayland_server_thread.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/test/test_subcompositor.h"
 #include "ui/ozone/platform/wayland/test/test_surface_augmenter.h"
 #include "ui/ozone/platform/wayland/test/test_viewporter.h"
+#include "ui/ozone/platform/wayland/test/test_wp_pointer_gestures.h"
 #include "ui/ozone/platform/wayland/test/test_zcr_text_input_extension.h"
 #include "ui/ozone/platform/wayland/test/test_zwp_linux_explicit_synchronization.h"
 #include "ui/ozone/platform/wayland/test/test_zwp_text_input_manager.h"
@@ -122,6 +123,8 @@ class TestWaylandServerThread : public base::Thread,
     return primary_selection_device_manager_.get();
   }
 
+  TestWpPointerGestures& wp_pointer_gestures() { return wp_pointer_gestures_; }
+
   void set_output_delegate(OutputDelegate* delegate) {
     output_delegate_ = delegate;
   }
@@ -161,6 +164,7 @@ class TestWaylandServerThread : public base::Thread,
   TestZwpLinuxExplicitSynchronizationV1 zwp_linux_explicit_synchronization_v1_;
   MockZwpLinuxDmabufV1 zwp_linux_dmabuf_v1_;
   MockWpPresentation wp_presentation_;
+  TestWpPointerGestures wp_pointer_gestures_;
   std::unique_ptr<TestSelectionDeviceManager> primary_selection_device_manager_;
 
   std::vector<std::unique_ptr<GlobalObject>> globals_;
