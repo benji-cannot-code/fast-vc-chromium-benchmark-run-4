97340c33b6c70557cd92cf05dfedb9a9afa337e2
Hidehiko Abe <hidehiko@chromium.org>
2022-01-12 05:21:16 +0000

    Fix timing issue of OpenUrl.
    In the old implementation, if there's no browser window when OpenUrl is called,
    the tab exiting check fails always even if there's tabs to be restored,
    and so a new tab is always opened.
    
    This CL fixes the behavior by triggering session restoring beforehand.
    
    BUG=1273656
    TEST=Ran on DUT.
    
    Change-Id: I16cb458b40bb5996281b9bce98b524c0c74ba4d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372249
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957937}

--- chrome/browser/lacros/browser_service_lacros.h
+++ chrome/browser/lacros/browser_service_lacros.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/callback_list.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chromeos/crosapi/mojom/crosapi.mojom.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 
 class GURL;
+class Profile;
 class ScopedKeepAlive;
 
 // BrowserSerivce's Lacros implementation.
@@ -51,6 +53,8 @@ class BrowserServiceLacros : public crosapi::mojom::BrowserService,
   void UpdateKeepAlive(bool enabled) override;
 
  private:
+  struct PendingOpenUrl;
+
   void OnSystemInformationReady(
       GetFeedbackDataCallback callback,
       std::unique_ptr<system_logs::SystemLogsResponse> sys_info);
@@ -58,13 +62,20 @@ class BrowserServiceLacros : public crosapi::mojom::BrowserService,
   void OnGetCompressedHistograms(GetHistogramsCallback callback,
                                  const std::string& compressed_histogram);
 
+  void OpenUrlImpl(Profile* profile, const GURL& url, OpenUrlCallback callback);
+
+  // Called when a session is restored.
+  void OnSessionRestored(Profile* profile, int num_tabs_restored);
+
   // BrowserListObserver:
   void OnBrowserAdded(Browser* browser) override;
 
   // Keeps the Lacros browser alive in the background. This is destroyed once
   // any browser window is opened.
   std::unique_ptr<ScopedKeepAlive> keep_alive_;
+  std::vector<PendingOpenUrl> pending_open_urls_;
 
+  base::CallbackListSubscription session_restored_subscription_;
   mojo::Receiver<crosapi::mojom::BrowserService> receiver_{this};
   base::WeakPtrFactory<BrowserServiceLacros> weak_ptr_factory_{this};
 };
