f29b45d270ab60e904c2251927097ccbfb3db732
Benoit Lize <lizeb@chromium.org>
2022-01-12 10:56:52 +0000

    [base] Start removing tcmalloc from the codebase.
    tcmalloc is not used anywhere since M93. This commit starts removing its
    code from the codebase. This is split into several CLs, this one
    handles:
    - Forbidding "use_allocator = "tcmalloc""
    - Related code in //base (except for //base/allocator)
    
    Bug: 1257213
    Change-Id: I6a7e996272cad9316bcb58c76e3508fc6d0cd2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372825
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958006}

--- base/process/memory_linux.cc
+++ base/process/memory_linux.cc
@@ -19,11 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
 
-#if BUILDFLAG(USE_TCMALLOC)
-#include "third_party/tcmalloc/chromium/src/config.h"
-#include "third_party/tcmalloc/chromium/src/gperftools/tcmalloc.h"
-#endif
-
 namespace base {
 
 namespace {
@@ -48,9 +43,6 @@ void EnableTerminationOnOutOfMemory() {
 
 #if BUILDFLAG(USE_ALLOCATOR_SHIM)
   allocator::SetCallNewHandlerOnMallocFailure(true);
-#elif defined(USE_TCMALLOC)
-  // For tcmalloc, we need to tell it to behave like new.
-  tc_set_new_mode(1);
 #endif
 }
 
@@ -117,13 +109,10 @@ bool AdjustOOMScore(ProcessId process, int score) {
 bool UncheckedMalloc(size_t size, void** result) {
 #if BUILDFLAG(USE_ALLOCATOR_SHIM)
   *result = allocator::UncheckedAlloc(size);
-#elif defined(MEMORY_TOOL_REPLACES_ALLOCATOR) || \
-    (!defined(LIBC_GLIBC) && !BUILDFLAG(USE_TCMALLOC))
+#elif defined(MEMORY_TOOL_REPLACES_ALLOCATOR) || !defined(LIBC_GLIBC)
   *result = malloc(size);
-#elif defined(LIBC_GLIBC) && !BUILDFLAG(USE_TCMALLOC)
+#elif defined(LIBC_GLIBC)
   *result = __libc_malloc(size);
-#elif BUILDFLAG(USE_TCMALLOC)
-  *result = tc_malloc_skip_new_handler(size);
 #endif
   return *result != nullptr;
 }
@@ -131,13 +120,10 @@ bool UncheckedMalloc(size_t size, void** result) {
 void UncheckedFree(void* ptr) {
 #if BUILDFLAG(USE_ALLOCATOR_SHIM)
   allocator::UncheckedFree(ptr);
-#elif defined(MEMORY_TOOL_REPLACES_ALLOCATOR) || \
-    (!defined(LIBC_GLIBC) && !BUILDFLAG(USE_TCMALLOC))
+#elif defined(MEMORY_TOOL_REPLACES_ALLOCATOR) || !defined(LIBC_GLIBC)
   free(ptr);
-#elif defined(LIBC_GLIBC) && !BUILDFLAG(USE_TCMALLOC)
+#elif defined(LIBC_GLIBC)
   __libc_free(ptr);
-#elif BUILDFLAG(USE_TCMALLOC)
-  tc_free(ptr);
 #endif
 }
 
