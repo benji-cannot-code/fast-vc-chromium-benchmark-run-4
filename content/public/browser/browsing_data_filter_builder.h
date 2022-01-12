cb4aba480700a2d928d2591a77a9eabefb8f6d76
Lei Zhang <thestig@chromium.org>
2022-01-12 18:58:38 +0000

    Don't import cookie_manager.mojom in network_service.mojom.
    It is not used there. Then do IWYU in browsing_data_filter_builder.h to
    fix the build.
    
    Change-Id: Icc64d0cb450819caa7ee5aac5d1c5fbe3a11a67d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3369092
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958200}

--- content/public/browser/browsing_data_filter_builder.h
+++ content/public/browser/browsing_data_filter_builder.h
@@ -7,14 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_PUBLIC_BROWSER_BROWSING_DATA_FILTER_BUILDER_H_
 
 #include <memory>
-#include <ostream>
-#include <set>
 #include <string>
-#include <vector>
 
 #include "base/callback_forward.h"
 #include "content/common/content_export.h"
 #include "net/cookies/cookie_partition_key_collection.h"
+#include "services/network/public/mojom/cookie_manager.mojom.h"
 #include "services/network/public/mojom/network_service.mojom.h"
 
 class GURL;
