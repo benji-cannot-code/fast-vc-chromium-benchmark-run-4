10a2aee3e03b8be6f05b96e4d84a93bf5c5cef30
Alison Maher <almaher@microsoft.com>
2022-01-12 15:44:18 +0000

    Reland "[LayoutNG] Substructs for break token data"
    This is a reland of d12d4fd76dae21cc47bd7ac5a4e4d5db24726a28
    
    The CL had been reverted because we could end up deleting a pointer
    to NGGridBreakTokenData, with the assumption that it was a
    NGBlockBreakTokenData. This was fixed by adding a virtual
    destructor to NGBlockBreakTokenData.
    
    This solution isn't ideal. Other options may be investigated in the
    future, such as making the block break token a templated class.
    
    Original change's description:
    > [LayoutNG] Substructs for break token data
    >
    > We create a special break token data structure for grid and flexbox.
    > However, we will never need both of these data structures at the same
    > time.
    >
    > This CL fixes this by creating a new NGBlockBreakTokenData struct that
    > is stored on the NGBlockBreakToken. NGFlexBreakTokenData and
    > NGGridBreakTokenData are now substructs of NGBlockBreakTokenData,
    > allowing us to have one data struct per break token.
    >
    > The reason that I chose to subclass a member variable of the break
    > token rather than the break token itself is that NGBlockBreakToken
    > holds a flexible array, making it tricky to add any subclass. I'd be
    > open to testing out other ideas, as well.
    >
    > Bug: 660611,614667
    > Change-Id: I82355239b935646f3ac742e9295a7271339f3321
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3366808
    > Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    > Commit-Queue: Alison Maher <almaher@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#957373}
    
    Bug: 660611,614667
    Change-Id: I89a6f9111cfa3c4ba80a54a0870e84348230689f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381216
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#958091}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_button.h"
 #include "third_party/blink/renderer/core/layout/layout_flexible_box.h"
 #include "third_party/blink/renderer/core/layout/ng/flex/layout_ng_flexible_box.h"
+#include "third_party/blink/renderer/core/layout/ng/flex/ng_flex_break_token_data.h"
 #include "third_party/blink/renderer/core/layout/ng/flex/ng_flex_child_iterator.h"
 #include "third_party/blink/renderer/core/layout/ng/flex/ng_flex_item_iterator.h"
 #include "third_party/blink/renderer/core/layout/ng/flex/ng_flex_line.h"
@@ -819,9 +820,10 @@ scoped_refptr<const NGLayoutResult> NGFlexLayoutAlgorithm::LayoutInternal() {
   Vector<NGFlexLine> flex_line_outputs;
   bool use_empty_line_block_size;
   if (IsResumingLayout(BreakToken())) {
-    auto& flex_data = BreakToken()->FlexData();
-    total_intrinsic_block_size_ = flex_data.intrinsic_block_size;
-    flex_line_outputs = flex_data.flex_lines;
+    const NGFlexBreakTokenData* flex_data =
+        To<NGFlexBreakTokenData>(BreakToken()->TokenData());
+    total_intrinsic_block_size_ = flex_data->intrinsic_block_size;
+    flex_line_outputs = flex_data->flex_lines;
 
     use_empty_line_block_size =
         flex_line_outputs.IsEmpty() && Node().HasLineIfEmpty();
@@ -912,9 +914,9 @@ scoped_refptr<const NGLayoutResult> NGFlexLayoutAlgorithm::LayoutInternal() {
 #endif
 
   if (ConstraintSpace().HasBlockFragmentation()) {
-    container_builder_.SetFlexBreakTokenData(
-        std::make_unique<NGFlexBreakTokenData>(flex_line_outputs,
-                                               total_intrinsic_block_size_));
+    container_builder_.SetBreakTokenData(std::make_unique<NGFlexBreakTokenData>(
+        container_builder_.GetBreakTokenData(), flex_line_outputs,
+        total_intrinsic_block_size_));
   }
 
   // Un-freeze descendant scrollbars before we run the OOF layout part.
