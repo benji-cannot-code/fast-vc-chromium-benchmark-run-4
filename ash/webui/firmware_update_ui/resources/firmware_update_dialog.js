9616bd15e68414d24fb95ec7390e299310ca2f28
Michael Checo <michaelcheco@google.com>
2022-01-12 20:05:28 +0000

    Firmware UI: Check installation percentage when updating dialog state
    Bug: 1252981
    Change-Id: I07a45216a8ba7dc6e35ca95f36cfa2c6f611534c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381498
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#958233}

--- ash/webui/firmware_update_ui/resources/firmware_update_dialog.js
+++ ash/webui/firmware_update_ui/resources/firmware_update_dialog.js
@@ -141,7 +141,8 @@ export class FirmwareUpdateDialogElement extends
       UpdateState.kFailed,
       UpdateState.kSuccess,
     ];
-    return activeDialogStates.includes(this.installationProgress.state);
+    return activeDialogStates.includes(this.installationProgress.state) ||
+        this.installationProgress.percentage > 0;
   }
 
   /**
@@ -160,6 +161,12 @@ export class FirmwareUpdateDialogElement extends
    * @return {boolean}
    */
   isUpdateInProgress_() {
+    /** @type {!Array<!UpdateState>} */
+    const inactiveDialogStates = [UpdateState.kUnknown, UpdateState.kIdle];
+    if (inactiveDialogStates.includes(this.installationProgress.state)) {
+      return this.installationProgress.percentage > 0;
+    }
+
     return this.installationProgress.state === UpdateState.kUpdating;
   }
 
