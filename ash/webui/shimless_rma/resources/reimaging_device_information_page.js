50adeefeda7e0ae10b1c87ac281d3f36bb8598c1
Gavin Williams <gavinwill@chromium.org>
2022-01-12 19:50:58 +0000

    Shimless: Disable Dram and White-label inputs
    These inputs need to be disabled along with the rest of the already
    disabled inputs when processing
    
    Bug: 1198187
    Change-Id: Ic5fdfcb3284ede3087da24ab9a6bfbaf279e5144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373500
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958231}

--- ash/webui/shimless_rma/resources/reimaging_device_information_page.js
+++ ash/webui/shimless_rma/resources/reimaging_device_information_page.js
@@ -80,14 +80,14 @@ export class ReimagingDeviceInformationPage extends
       disableResetWhiteLabel_: {
         type: Boolean,
         computed: 'getDisableResetWhiteLabel_(' +
-            'originalWhiteLabelIndex_, whiteLabelIndex_)',
+            'originalWhiteLabelIndex_, whiteLabelIndex_, allButtonsDisabled)',
       },
 
       /** @protected */
       disableResetDramPartNumber_: {
         type: Boolean,
         computed: 'getDisableResetDramPartNumber_(' +
-            'originalDramPartNumber_, dramPartNumber_)',
+            'originalDramPartNumber_, dramPartNumber_, allButtonsDisabled)',
       },
 
       /** @protected */
@@ -304,12 +304,14 @@ export class ReimagingDeviceInformationPage extends
 
   /** @protected */
   getDisableResetWhiteLabel_() {
-    return this.originalWhiteLabelIndex_ === this.whiteLabelIndex_;
+    return this.originalWhiteLabelIndex_ === this.whiteLabelIndex_ ||
+        this.allButtonsDisabled;
   }
 
   /** @protected */
   getDisableResetDramPartNumber_() {
-    return this.originalDramPartNumber_ === this.dramPartNumber_;
+    return this.originalDramPartNumber_ === this.dramPartNumber_ ||
+        this.allButtonsDisabled;
   }
 
   /** @protected */
