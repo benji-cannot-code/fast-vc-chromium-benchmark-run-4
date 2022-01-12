d4c7e979bb9f49db58c127203bbd40a8ba919e02
Owen Min <zmin@chromium.org>
2022-01-12 22:47:04 +0000

    Create ChromeProfileRequestGenerator
    This Generator creates the ChromeProfileRequestReport proto and fill in
    browser, os info and profile information.
    
    This reports also contains browser version and channel information on
    Chrome OS.
    
    Bug: 1261945
    Change-Id: I61eda50d28794f1afca88c1a156f8d62f430833d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373609
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958326}

---
+++ components/enterprise/browser/reporting/os_report_generator.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/browser/reporting/os_report_generator.h"
+
+#include "components/policy/core/common/cloud/cloud_policy_util.h"
+
+namespace enterprise_reporting {
+
+namespace em = enterprise_management;
+
+std::unique_ptr<em::OSReport> GetOSReport() {
+  auto report = std::make_unique<em::OSReport>();
+  report->set_name(policy::GetOSPlatform());
+  report->set_arch(policy::GetOSArchitecture());
+  report->set_version(policy::GetOSVersion());
+  return report;
+}
+
+}  // namespace enterprise_reporting
