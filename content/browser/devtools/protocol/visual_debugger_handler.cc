616468642c862a91c6868ae0d9f586b680f5a268
Benedikt Meurer <bmeurer@chromium.org>
2022-01-12 20:13:41 +0000

    [Code Health] Roll inspector_protocol to 9a50beee69a31869d1f95c2c63bc3a3ffe725b97.
    This migrates the DevTools toProtocolValue() and toBaseValue()
    conversions to the new style for passing base::Value by const reference
    and returning base::Value (via move optimization) instead of wrapping
    them into std::unique_ptr.
    
    Bug: chromium:1187001
    Doc: https://docs.google.com/document/d/1uDLu5uTRlCWePxQUEHc8yNQdEoE1BDISYdpggWEABnw
    Change-Id: I42bb6d27710a1808baeeb2628a624f6c4a4e7349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3377009
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958245}

--- content/browser/devtools/protocol/visual_debugger_handler.cc
+++ content/browser/devtools/protocol/visual_debugger_handler.cc
@@ -37,8 +37,7 @@ void VisualDebuggerHandler::Wire(UberDispatcher* dispatcher) {
 
 DispatchResponse VisualDebuggerHandler::FilterStream(
     std::unique_ptr<protocol::DictionaryValue> in_filter) {
-  std::unique_ptr<base::Value> dict =
-      toBaseValue(in_filter.get(), sMaxJsonDepth);
+  base::Value dict = toBaseValue(in_filter.get(), sMaxJsonDepth);
 
   GpuProcessHost::CallOnIO(
       GPU_PROCESS_KIND_SANDBOXED,
@@ -47,7 +46,7 @@ DispatchResponse VisualDebuggerHandler::FilterStream(
           [](base::Value json, GpuProcessHost* host) {
             host->gpu_host()->FilterVisualDebugStream(std::move(json));
           },
-          std::move(*dict.get())));
+          std::move(dict)));
 
   return DispatchResponse::Success();
 }
@@ -75,7 +74,7 @@ void VisualDebuggerHandler::OnFrameResponse(base::Value json) {
   // above and thus should be in the correct thread.
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   std::unique_ptr<protocol::DictionaryValue> dict =
-      protocol::DictionaryValue::cast(toProtocolValue(&json, sMaxJsonDepth));
+      protocol::DictionaryValue::cast(toProtocolValue(json, sMaxJsonDepth));
   frontend_->FrameResponse(std::move(dict));
 }
 
