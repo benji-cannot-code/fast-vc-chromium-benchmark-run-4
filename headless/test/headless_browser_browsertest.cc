4d7093f105e57e97ebce69a5ad2d80f82f6b063e
Stefano Duo <stefanoduo@google.com>
2022-01-12 10:48:43 +0000

    [Code Health Rotation] Remove Value::CreateDeepCopy
    Bug: 1187013
    Change-Id: I71335141b27559c1baab8fb8f8b02509a28cd247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380515
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#958004}

--- headless/test/headless_browser_browsertest.cc
+++ headless/test/headless_browser_browsertest.cc
@@ -581,7 +581,7 @@ class TraceHelper : public tracing::ExperimentalObserver {
   // tracing::ExperimentalObserver implementation:
   void OnDataCollected(const tracing::DataCollectedParams& params) override {
     for (const auto& value : *params.GetValue()) {
-      tracing_data_->Append(value->CreateDeepCopy());
+      tracing_data_->Append(value->Clone());
     }
   }
 
