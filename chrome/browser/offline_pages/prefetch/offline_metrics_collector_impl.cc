9952d930cdd8cf05424af608f6c0ddcc45399c1a
Yaron Friedman <yfriedman@chromium.org>
2022-01-12 18:51:43 +0000

    [Jank] Switch OfflinePages metric to async write.
    Randomly noticed while looking for a startup regression, this does a
    synchronous disk write after every navigation taking ~0.2% of
    DidFinishNavigaiton time according to pprof and it would be much higher
    for non-first navigations (cause some observers are doing initial
    config). AFAICT these metrics aren't especially critical and can just be
    committed lazily like other metrics.
    
    Change-Id: I1a29022682458527b0f3e546e7295911186f4229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379698
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958193}

--- chrome/browser/offline_pages/prefetch/offline_metrics_collector_impl.cc
+++ chrome/browser/offline_pages/prefetch/offline_metrics_collector_impl.cc
@@ -191,7 +191,7 @@ void OfflineMetricsCollectorImpl::SaveToPrefs() {
   prefs_->SetInteger(prefs::kPrefetchUsageOpenedCount, prefetch_opened_count_);
   prefs_->SetInteger(prefs::kPrefetchUsageMixedCount, prefetch_mixed_count_);
 
-  prefs_->CommitPendingWrite();
+  prefs_->SchedulePendingLossyWrites();
 }
 
 void OfflineMetricsCollectorImpl::SetTrackingFlag(bool* flag) {
