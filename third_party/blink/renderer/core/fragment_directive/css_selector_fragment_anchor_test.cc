2b7bbd617b7dbec94d1feca4b8a941ea91e65a95
Cheick Cisse <cheickcisse@google.com>
2022-01-12 18:24:13 +0000

    [SH] Enable Shared Highlighting 2.0 by default
    Bug: 1277020
    Change-Id: I1a998a8c9888b6737581fd30fc338a0281c094bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342266
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#958170}

--- third_party/blink/renderer/core/fragment_directive/css_selector_fragment_anchor_test.cc
+++ third_party/blink/renderer/core/fragment_directive/css_selector_fragment_anchor_test.cc
@@ -309,11 +309,12 @@ TEST_F(CssSelectorFragmentAnchorTest, DISABLED_CheckCssSelectorRestrictions) {
   EXPECT_EQ(GetDocument().Url(), "https://example.com/test.html");
 }
 
-// Make sure fragment is dismissed after user clicks
-TEST_F(CssSelectorFragmentAnchorTest, DismissFragmentAfterUserClicks) {
+// Make sure fragment is not dismissed after user clicks
+TEST_F(CssSelectorFragmentAnchorTest, FragmentStaysAfterUserClicks) {
   base::test::ScopedFeatureList feature_list_;
-  feature_list_.InitAndEnableFeature(
-      blink::features::kCssSelectorFragmentAnchor);
+  feature_list_.InitWithFeatures(
+      /*enabled_features=*/{blink::features::kCssSelectorFragmentAnchor},
+      /*disabled_features=*/{});
 
   SimRequest main_request(
       "https://example.com/"
@@ -344,6 +345,13 @@ TEST_F(CssSelectorFragmentAnchorTest, DismissFragmentAfterUserClicks) {
   test::RunPendingTasks();
   Compositor().BeginFrame();
 
+  KURL expected_url = GetDocument()
+                          .GetFrame()
+                          ->Loader()
+                          .GetDocumentLoader()
+                          ->GetHistoryItem()
+                          ->Url();
+
   Element& img = *GetDocument().getElementById("image");
   EXPECT_TRUE(IsVisibleInViewport(img))
       << "<img> Element wasn't scrolled into view, viewport's scroll offset: "
@@ -352,7 +360,7 @@ TEST_F(CssSelectorFragmentAnchorTest, DismissFragmentAfterUserClicks) {
 
   SimulateClick(100, 100);
 
-  EXPECT_FALSE(GetDocument().View()->GetFragmentAnchor());
+  EXPECT_TRUE(GetDocument().View()->GetFragmentAnchor());
 
   KURL url = GetDocument()
                  .GetFrame()
@@ -360,7 +368,8 @@ TEST_F(CssSelectorFragmentAnchorTest, DismissFragmentAfterUserClicks) {
                  .GetDocumentLoader()
                  ->GetHistoryItem()
                  ->Url();
-  EXPECT_EQ("https://example.com/test.html", url);
+
+  EXPECT_EQ(expected_url, url);
 }
 
 // Although parsed correctly, the element is not found, hence no scroll happens
