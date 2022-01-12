d050e45af6ad3ea9559fc1af47a368b6b4f2229f
Veronique Nguyen <veronguyen@google.com>
2022-01-12 21:18:57 +0000

    [iOS] Add policy handler for NewTabPageLocation
    Following my last CL to implement the custom NewTabPageLocation URL,
    this CL is to verify if the policy is using a valid URL. Do not set
    the value if it is incorrect.
    
    Bug: 1284402
    Change-Id: I9878318dc4705e60aabb246da15fb08f8c8a8451
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380014
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#958283}

--- ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
+++ ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/pref_names.h"
 #include "components/variations/service/variations_service.h"
 #include "ios/chrome/browser/policy/browser_signin_policy_handler.h"
+#include "ios/chrome/browser/policy/new_tab_page_location_policy_handler.h"
 #include "ios/chrome/browser/policy/policy_features.h"
 #import "ios/chrome/browser/policy/restrict_accounts_policy_handler.h"
 #include "ios/chrome/browser/pref_names.h"
@@ -104,9 +105,6 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { policy::key::kUrlKeyedAnonymizedDataCollectionEnabled,
     unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled,
     base::Value::Type::BOOLEAN },
-  { policy::key::kNewTabPageLocation,
-    prefs::kNewTabPageLocationOverride,
-    base::Value::Type::STRING },
 };
 // clang-format on
 
@@ -153,6 +151,8 @@ std::unique_ptr<policy::ConfigurationPolicyHandlerList> BuildPolicyHandlerList(
   handlers->AddHandler(std::make_unique<syncer::SyncPolicyHandler>());
   handlers->AddHandler(
       std::make_unique<enterprise_reporting::CloudReportingPolicyHandler>());
+  handlers->AddHandler(
+      std::make_unique<policy::NewTabPageLocationPolicyHandler>());
 
   if (ShouldInstallURLBlocklistPolicyHandlers()) {
     handlers->AddHandler(std::make_unique<policy::URLBlocklistPolicyHandler>(
