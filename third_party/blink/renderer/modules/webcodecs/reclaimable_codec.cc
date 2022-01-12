758b578200670973419d968e5ccda236558a6770
Scott Haseley <shaseley@chromium.org>
2022-01-12 04:28:13 +0000

    [scheduler] Refactor lifecycle observers as callbacks
    Before this CL, FrameOrWorkerScheduler lifecycle callbacks were
    implemented as an observer interface and we held a raw pointer to the
    observer. These observers could be either on-heap or off-heap objects
    which violates the rule of not storing raw pointers to on-heap objects
    since this risks UAF.
    
    To work around this, this CL changes the lifecycle observers to
    callbacks, with the observers using WrapWeakPersistent with Bind where
    needed. This is relatively clean since the observer interface only has
    one method. We could alternatively store WeakPersistent references
    to the on-heap obejcts, but this is more complicated since we need to
    handle both on-heap and off-heap obejcts (possible, but probably overly
    complicated for this case).
    
    Bug: 1281239, 1212957
    Change-Id: I13a14c3228b1fd7043f94becfacf8ec5429329ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373869
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957925}

--- third_party/blink/renderer/modules/webcodecs/reclaimable_codec.cc
+++ third_party/blink/renderer/modules/webcodecs/reclaimable_codec.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/time/default_tick_clock.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
 
@@ -32,7 +34,9 @@ ReclaimableCodec::ReclaimableCodec(ExecutionContext* context)
   if (base::FeatureList::IsEnabled(kOnlyReclaimBackgroundWebCodecs)) {
     // Do this last, it will immediately re-enter via OnLifecycleStateChanged().
     observer_handle_ = context->GetScheduler()->AddLifecycleObserver(
-        FrameOrWorkerScheduler::ObserverType::kWorkerScheduler, this);
+        FrameOrWorkerScheduler::ObserverType::kWorkerScheduler,
+        WTF::BindRepeating(&ReclaimableCodec::OnLifecycleStateChanged,
+                           WrapWeakPersistent(this)));
   } else {
     // Pretend we're always in the background to _always_ reclaim.
     is_backgrounded_ = true;
--- third_party/blink/renderer/platform/loader/fetch/resource_load_scheduler.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_load_scheduler.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/loader/fetch/console_logger.h"
 #include "third_party/blink/renderer/platform/loader/fetch/loading_behavior_observer.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/network/network_state_notifier.h"
 #include "third_party/blink/renderer/platform/scheduler/public/aggregated_metric_reporter.h"
 #include "third_party/blink/renderer/platform/scheduler/public/frame_status.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
 
@@ -105,7 +107,9 @@ ResourceLoadScheduler::ResourceLoadScheduler(
                                kTightLimitForRendererSideResourceScheduler);
 
   scheduler_observer_handle_ = frame_or_worker_scheduler->AddLifecycleObserver(
-      FrameScheduler::ObserverType::kLoader, this);
+      FrameScheduler::ObserverType::kLoader,
+      WTF::BindRepeating(&ResourceLoadScheduler::OnLifecycleStateChanged,
+                         WrapWeakPersistent(this)));
 }
 
 ResourceLoadScheduler::~ResourceLoadScheduler() = default;
