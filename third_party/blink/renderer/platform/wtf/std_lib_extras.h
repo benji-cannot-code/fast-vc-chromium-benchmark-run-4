96a986ba5d9c2dddbc8f428cb3b10ba55872a26f
Lei Zhang <thestig@chromium.org>
2022-01-12 00:18:31 +0000

    Swap base/logging.h for base/check.h in Blink's std_lib_extras.h.
    It doesn't do any logging.
    
    Change-Id: I46beb146c6baf239ec92f5b8112950689a3e8eb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3374147
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#957823}

--- third_party/blink/renderer/platform/wtf/std_lib_extras.h
+++ third_party/blink/renderer/platform/wtf/std_lib_extras.h
@@ -29,8 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstddef>
 
+#include "base/check.h"
 #include "base/dcheck_is_on.h"
-#include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 #include "build/build_config.h"
 #include "third_party/blink/renderer/platform/wtf/leak_annotations.h"
