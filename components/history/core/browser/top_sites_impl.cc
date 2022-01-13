cc9caeae07037e22d0edd3306e472b9f5526faaa
Alex Turner <alexmt@chromium.org>
2022-01-13 20:37:49 +0000

    Modernize {Dictionary,List}PrefUpdate in /components/history
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /components/history
    
    This CL was uploaded by git cl split.
    
    R=tommycli@chromium.org
    
    Bug: 1285745
    Change-Id: Ib52b9bab3af06d4cfa9b687fbc8d797049cadc90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386087
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958804}

--- components/history/core/browser/top_sites_impl.cc
+++ components/history/core/browser/top_sites_impl.cc
@@ -152,7 +152,7 @@ void TopSitesImpl::AddBlockedUrl(const GURL& url) {
   DCHECK(thread_checker_.CalledOnValidThread());
 
   {
-    DictionaryPrefUpdateDeprecated update(pref_service_, kBlockedUrlsPrefsKey);
+    DictionaryPrefUpdate update(pref_service_, kBlockedUrlsPrefsKey);
     base::Value* blocked_urls = update.Get();
     blocked_urls->SetKey(GetURLHash(url), base::Value());
   }
@@ -164,7 +164,7 @@ void TopSitesImpl::AddBlockedUrl(const GURL& url) {
 void TopSitesImpl::RemoveBlockedUrl(const GURL& url) {
   DCHECK(thread_checker_.CalledOnValidThread());
   {
-    DictionaryPrefUpdateDeprecated update(pref_service_, kBlockedUrlsPrefsKey);
+    DictionaryPrefUpdate update(pref_service_, kBlockedUrlsPrefsKey);
     base::Value* blocked_urls = update.Get();
     blocked_urls->RemoveKey(GetURLHash(url));
   }
@@ -182,7 +182,7 @@ bool TopSitesImpl::IsBlocked(const GURL& url) {
 void TopSitesImpl::ClearBlockedUrls() {
   DCHECK(thread_checker_.CalledOnValidThread());
   {
-    DictionaryPrefUpdateDeprecated update(pref_service_, kBlockedUrlsPrefsKey);
+    DictionaryPrefUpdate update(pref_service_, kBlockedUrlsPrefsKey);
     base::Value* blocked_urls = update.Get();
     blocked_urls->DictClear();
   }
