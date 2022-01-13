67d824cf0d3380be6eb600fb7ef1f73fb71d5979
sandromaggi <sandromaggi@google.com>
2022-01-13 09:28:41 +0000

    [Autofill Assistant] Remove unused code
    With the removal of the DateTime handling from CUD
    (crrev/c/3377048) some of the fields in the user data
    coordinator have become unused.
    
    Removing them.
    
    Bug: b/213306025
    Change-Id: I82f400c951056810ceffb90d4e5f3b7360390b77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383343
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Auto-Submit: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/main@{#958486}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/user_data/AssistantCollectUserDataCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/user_data/AssistantCollectUserDataCoordinator.java
@@ -6,21 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill_assistant.user_data;
 
 import android.app.Activity;
-import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
-import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill_assistant.AssistantTagsForTesting;
 import org.chromium.chrome.browser.autofill_assistant.LayoutUtils;
 import org.chromium.chrome.browser.autofill_assistant.user_data.additional_sections.AssistantAdditionalSectionContainer;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
-
-import java.text.DateFormat;
-import java.util.Locale;
 
 // TODO(crbug.com/806868): Use mCarouselCoordinator to show chips.
 
@@ -36,20 +30,6 @@ public class AssistantCollectUserDataCoordinator {
 
     public AssistantCollectUserDataCoordinator(
             Activity activity, AssistantCollectUserDataModel model) {
-        this(activity, model,
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
-                        ? activity.getResources().getConfiguration().getLocales().get(0)
-                        : activity.getResources().getConfiguration().locale);
-    }
-
-    private AssistantCollectUserDataCoordinator(
-            Activity activity, AssistantCollectUserDataModel model, Locale locale) {
-        this(activity, model, locale, DateFormat.getDateInstance(DateFormat.MEDIUM, locale));
-    }
-
-    @VisibleForTesting
-    public AssistantCollectUserDataCoordinator(Activity activity,
-            AssistantCollectUserDataModel model, Locale locale, DateFormat dateFormat) {
         mActivity = activity;
         mModel = model;
         int sectionToSectionPadding = activity.getResources().getDimensionPixelSize(
