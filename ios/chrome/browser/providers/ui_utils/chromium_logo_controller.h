f23c20ee56942d37477053e7cf3443633e7fa3ce
Sylvain Defresne <sdefresne@chromium.org>
2022-01-13 08:41:21 +0000

    [ios] Expose `ui_utils` API as free functions
    Add five free functions exposing the `ui_utils` API with the goal
    to replace UI related methods of ChromeBrowserProvider class, but
    do not use the new functions yet (as they have no implementation
    in the internal repository yet).
    
    This is a reland of https://crrev.com/c/3380978 fixed to not call
    the provider API yet in ChromeBrowserProvider (as doing so broke
    the internal repository).
    
    This is part of a multi-CL change:
     => https://crrev.com/c/3381143
      - https://crrev.com/i/4447058
      - https://crrev.com/c/3383576
      - https://crrev.com/i/4451098
      - https://crrev.com/c/3380979
    
    Bug: 1201182
    Change-Id: I633d8d3b4c632f909425c24063345e4f26af8e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381143
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958478}

--- ios/chrome/browser/providers/ui_utils/chromium_logo_controller.h
+++ ios/chrome/browser/providers/ui_utils/chromium_logo_controller.h
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_PROVIDERS_CHROMIUM_LOGO_CONTROLLER_H_
-#define IOS_CHROME_BROWSER_PROVIDERS_CHROMIUM_LOGO_CONTROLLER_H_
+#ifndef IOS_CHROME_BROWSER_PROVIDERS_UI_UTILS_CHROMIUM_LOGO_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_PROVIDERS_UI_UTILS_CHROMIUM_LOGO_CONTROLLER_H_
 
 #import <Foundation/Foundation.h>
 
 #import "ios/chrome/browser/ui/ntp/logo_vendor.h"
 
-@interface ChromiumLogoController : NSObject<LogoVendor>
+@interface ChromiumLogoController : NSObject <LogoVendor>
 @end
 
-#endif  // IOS_CHROME_BROWSER_PROVIDERS_CHROMIUM_LOGO_CONTROLLER_H_
+#endif  // IOS_CHROME_BROWSER_PROVIDERS_UI_UTILS_CHROMIUM_LOGO_CONTROLLER_H_
