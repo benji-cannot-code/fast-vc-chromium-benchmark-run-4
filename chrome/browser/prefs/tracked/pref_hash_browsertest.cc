7cb2ab11c5ab738330cc3dc0cbc742d8c13d9d29
Alex Turner <alexmt@chromium.org>
2022-01-13 10:36:37 +0000

    Modernize {Dictionary,List}PrefUpdate in /chrome/browser/prefs
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /chrome/browser/prefs
    
    This CL was uploaded by git cl split.
    
    R=battre@chromium.org
    
    Bug: 1285745
    Change-Id: Idb12fffca11fafb75d40b0fe6e8358b197da2afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386018
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958507}

--- chrome/browser/prefs/tracked/pref_hash_browsertest.cc
+++ chrome/browser/prefs/tracked/pref_hash_browsertest.cc
@@ -741,8 +741,8 @@ class PrefHashBrowserTestChangedAtomic : public PrefHashBrowserTestBase {
     profile()->GetPrefs()->SetInteger(prefs::kRestoreOnStartup,
                                       SessionStartupPref::URLS);
 
-    ListPrefUpdateDeprecated update(profile()->GetPrefs(),
-                                    prefs::kURLsToRestoreOnStartup);
+    ListPrefUpdate update(profile()->GetPrefs(),
+                          prefs::kURLsToRestoreOnStartup);
     update->Append("http://example.com");
   }
 
