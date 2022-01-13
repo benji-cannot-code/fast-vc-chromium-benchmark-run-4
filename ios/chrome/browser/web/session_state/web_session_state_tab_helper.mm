b05b71c0c10f0ce278b4e15aaa59c0822c19ef96
Justin Cohen <justincohen@google.com>
2022-01-13 20:50:14 +0000

    ios: Don't save interactionState for downloads.
    Downloads update the WKWebView URL but not the backForwardList
    currentItem.  The URL is quickly reverted to currentItem, but there's
    no simple way to pipe that change down to WebSessionStateTabHelper.
    
    Since there's no value in saving interactionState for downloads, just
    drop them here.
    
    Fixed: 1285856
    Change-Id: I27c3e5918d60c69046ae6b971183f6bd1e58bd24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387428
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958813}

--- ios/chrome/browser/web/session_state/web_session_state_tab_helper.mm
+++ ios/chrome/browser/web/session_state/web_session_state_tab_helper.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/session_state/web_session_state_cache_factory.h"
 #include "ios/web/common/features.h"
 #import "ios/web/public/js_messaging/web_frame.h"
+#include "ios/web/public/navigation/navigation_context.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #import "ios/web/public/session/serializable_user_data_manager.h"
 #include "ios/web/public/web_client.h"
@@ -144,6 +145,11 @@ void WebSessionStateTabHelper::WebStateDestroyed(web::WebState* web_state) {
 void WebSessionStateTabHelper::DidFinishNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
+  // Don't record navigations that result in downloads, since these will be
+  // discarded and there's no simple callback when discarded.
+  if (navigation_context->IsDownload())
+    return;
+
   MarkStale();
 }
 
