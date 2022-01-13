c47cedcb30949367c2cfa1187eb8f43ea86e8237
Tarun Bansal <tbansal@google.com>
2022-01-13 15:08:32 +0000

    Remove previews ukm observer
    This can be removed as the feature is not launching.
    
    Change-Id: I51c2d9c9125125fee64a4274b832fab2ffdfec85
    Bug: 1285486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3385358
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958604}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/portal_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/prefetch_proxy_page_load_metrics_observer.h"
-#include "chrome/browser/page_load_metrics/observers/previews_ukm_observer.h"
 #include "chrome/browser/page_load_metrics/observers/protocol_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/scheme_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/security_state_page_load_metrics_observer.h"
@@ -127,7 +126,6 @@ void PageLoadMetricsEmbedder::RegisterEmbedderObservers(
         std::make_unique<MultiTabLoadingPageLoadMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<OptimizationGuidePageLoadMetricsObserver>());
-    tracker->AddObserver(std::make_unique<previews::PreviewsUKMObserver>());
     tracker->AddObserver(
         std::make_unique<ServiceWorkerPageLoadMetricsObserver>());
     tracker->AddObserver(
