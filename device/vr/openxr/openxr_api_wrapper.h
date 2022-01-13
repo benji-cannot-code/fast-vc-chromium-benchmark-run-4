4ac05e900d90e3b4f2bc8d77d29c41153c6f44ce
Patrick To <patrto@microsoft.com>
2022-01-13 23:36:34 +0000

    Reland "Add support for secondary views in WebXR"
    This change adds support for requesting secondary views in WebXR.
    Secondary views can be requested by passing in 'secondary-views'
    to requestSession. With this enabled, an arbitrary number of
    secondary views can be exposed from the XR runtime on each frame.
    
    This change also implements secondary views in the OpenXR backend,
    supporting the first-person observer view configuration. Secondary views
    in OpenXR are only exposed to WebXR only when they are active (such as
    when the camera feed is being captured) and requested by WebXR.
    
    Change-Id: Ic02d1140ef1c07218c61fc7e6c8a8e6bb85c5b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3384272
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: danakj chromium <danakj@chromium.org>
    Commit-Queue: Patrick To <patrto@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#958914}

--- device/vr/openxr/openxr_api_wrapper.h
+++ device/vr/openxr/openxr_api_wrapper.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/openxr/openxr_anchor_manager.h"
 #include "device/vr/openxr/openxr_scene_understanding_manager.h"
 #include "device/vr/openxr/openxr_util.h"
+#include "device/vr/openxr/openxr_view_configuration.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
 #include "device/vr/vr_export.h"
 #include "third_party/openxr/src/include/openxr/openxr.h"
@@ -60,6 +61,7 @@ class OpenXrApiWrapper {
   bool UpdateAndGetSessionEnded();
 
   XrResult InitSession(
+      const std::unordered_set<mojom::XRSessionFeature>& enabled_features,
       const Microsoft::WRL::ComPtr<ID3D11Device>& d3d_device,
       const OpenXrExtensionHelper& extension_helper,
       const SessionEndedCallback& on_session_ended_callback,
@@ -67,8 +69,8 @@ class OpenXrApiWrapper {
 
   XrSpace GetReferenceSpace(device::mojom::XRReferenceSpaceType type) const;
 
-  XrResult BeginFrame(Microsoft::WRL::ComPtr<ID3D11Texture2D>* texture,
-                      gpu::MailboxHolder* mailbox_holder);
+  XrResult BeginFrame(Microsoft::WRL::ComPtr<ID3D11Texture2D>& texture,
+                      gpu::MailboxHolder& mailbox_holder);
   XrResult EndFrame();
   bool HasPendingFrame() const;
   bool HasFrameState() const;
@@ -78,13 +80,13 @@ class OpenXrApiWrapper {
   std::vector<mojom::XRInputSourceStatePtr> GetInputState(
       bool hand_input_enabled);
 
-  const std::vector<XrViewConfigurationView>& GetViewConfigs() const;
+  std::vector<mojom::XRViewPtr> GetDefaultViews() const;
   gfx::Size GetSwapchainSize() const;
   XrTime GetPredictedDisplayTime() const;
-  XrResult GetLuid(LUID* luid,
-                   const OpenXrExtensionHelper& extension_helper) const;
-  bool GetStageParameters(XrExtent2Df* stage_bounds,
-                          gfx::Transform* local_from_stage);
+  XrResult GetLuid(const OpenXrExtensionHelper& extension_helper,
+                   LUID& luid) const;
+  bool GetStageParameters(XrExtent2Df& stage_bounds,
+                          gfx::Transform& local_from_stage);
 
   device::mojom::XREnvironmentBlendMode PickEnvironmentBlendModeForSession(
       device::mojom::XRSessionMode session_mode);
@@ -94,7 +96,9 @@ class OpenXrApiWrapper {
   OpenXRSceneUnderstandingManager* GetOrCreateSceneUnderstandingManager(
       const OpenXrExtensionHelper& extension_helper);
 
-  void CreateSharedMailboxes(viz::ContextProvider* context_provider);
+  void OnContextProviderCreated(
+      scoped_refptr<viz::ContextProvider> context_provider);
+  void OnContextProviderLost();
 
   bool CanEnableAntiAliasing() const;
   bool IsUsingSharedImages() const;
@@ -103,27 +107,17 @@ class OpenXrApiWrapper {
   void StoreFence(Microsoft::WRL::ComPtr<ID3D11Fence> d3d11_fence,
                   int16_t frame_index);
 
-  static mojom::XREye GetEyeFromIndex(int i);
-
-  // The number of views the OpenXR runtime is returning on each frame.
-  static constexpr uint32_t kNumViews = 2;
-
-  // Per the OpenXR 1.0 spec for the XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO
-  // view configuration: View index 0 must represent the left eye and view index
-  // 1 must represent the right eye.
-  static constexpr uint32_t kLeftView = 0;
-  static constexpr uint32_t kRightView = 1;
-  // Since kNumViews is used to size a vector that uses kLeftView/kRightView as
-  // indices, ensure that kNumViews is greater than the largest index.
-  static_assert(kRightView < kNumViews,
-                "kNumViews must be greater than kRightView");
-
  private:
   void Reset();
   bool Initialize(XrInstance instance);
   void Uninitialize();
 
   XrResult InitializeSystem();
+  XrResult InitializeViewConfig(XrViewConfigurationType type,
+                                OpenXrViewConfiguration& view_config);
+  XrResult GetPropertiesForViewConfig(
+      XrViewConfigurationType type,
+      std::vector<XrViewConfigurationView>& view_properties) const;
   XrResult InitializeEnvironmentBlendMode(XrSystemId system);
   XrResult ProcessEvents();
   void EnsureEventPolling();
@@ -131,12 +125,16 @@ class OpenXrApiWrapper {
   XrResult CreateSession(
       const Microsoft::WRL::ComPtr<ID3D11Device>& d3d_device);
   XrResult CreateSwapchain();
+  bool RecomputeSwapchainSizeAndViewports();
   XrResult CreateSpace(XrReferenceSpaceType type, XrSpace* space);
 
   XrResult BeginSession();
-  XrResult UpdateProjectionLayers();
-  XrResult LocateViews(XrReferenceSpaceType type,
-                       std::vector<XrView>* views) const;
+  XrResult UpdateSecondaryViewConfigStates(
+      const std::vector<XrSecondaryViewConfigurationStateMSFT>& states);
+  XrResult UpdateViewConfigurations();
+  XrResult PrepareViewConfigForRender(OpenXrViewConfiguration& view_config);
+  XrResult LocateViews(XrReferenceSpaceType space_type,
+                       OpenXrViewConfiguration& view_config) const;
 
   bool HasInstance() const;
   bool HasSystem() const;
@@ -151,7 +149,16 @@ class OpenXrApiWrapper {
   device::mojom::XREnvironmentBlendMode GetMojoBlendMode(
       XrEnvironmentBlendMode xr_blend_mode);
 
+  mojom::XRViewPtr CreateView(const OpenXrViewConfiguration& view_config,
+                              uint32_t view_index,
+                              mojom::XREye eye,
+                              uint32_t x_offset) const;
+
   bool ShouldCreateSharedImages() const;
+  void CreateSharedMailboxes();
+  void ReleaseColorSwapchainImages();
+
+  static mojom::XREye GetEyeFromIndex(int i);
 
   // The session is running only after xrBeginSession and before xrEndSession.
   // It is not considered running after creation but before xrBeginSession.
@@ -169,18 +176,20 @@ class OpenXrApiWrapper {
 
   // OpenXR objects
 
-  // These objects are valid on successful initialization.
+  // These objects are initialized on successful initialization.
   XrInstance instance_;
   XrSystemId system_;
-  std::vector<XrViewConfigurationView> view_configs_;
-  gfx::Size swapchain_size_;
   XrEnvironmentBlendMode blend_mode_;
   XrExtent2Df stage_bounds_;
 
-  // These objects are valid only while a session is active,
-  // and stay constant throughout the lifetime of a session.
+  // These objects are initialized when a session begins and stay constant
+  // throughout the lifetime of the session.
   XrSession session_;
-  XrSwapchain color_swapchain_;
+  XrSpace local_space_;
+  XrSpace stage_space_;
+  XrSpace view_space_;
+  XrSpace unbounded_space_;
+  std::unordered_set<mojom::XRSessionFeature> enabled_features_;
 
   // When shared images are being used, there is a corresponding MailboxHolder
   // and D3D11Fence for each D3D11 texture in the vector.
@@ -189,26 +198,36 @@ class OpenXrApiWrapper {
     ~SwapChainInfo();
     SwapChainInfo(SwapChainInfo&&);
 
+    void Clear();
+
     raw_ptr<ID3D11Texture2D> d3d11_texture = nullptr;
     gpu::MailboxHolder mailbox_holder;
     Microsoft::WRL::ComPtr<ID3D11Fence> d3d11_fence;
   };
+
+  // The swapchain is initializd when a session begins and is re-created when
+  // the state of a secondary view configuration changes.
+  XrSwapchain color_swapchain_;
+  gfx::Size swapchain_size_;
   std::vector<SwapChainInfo> color_swapchain_images_;
 
-  XrSpace local_space_;
-  XrSpace stage_space_;
-  XrSpace view_space_;
-  XrSpace unbounded_space_;
-
-  // These objects store information about the current frame. They're
-  // valid only while a session is active, and they are updated each frame.
+  // The rest of these objects store information about the current frame and are
+  // updated each frame.
   XrFrameState frame_state_;
-  std::vector<XrView> local_from_eye_views_;
+
+  OpenXrViewConfiguration primary_view_config_;
+  std::unordered_map<XrViewConfigurationType, OpenXrViewConfiguration>
+      secondary_view_configs_;
+
+  // Location that describes the viewer (aka. head) pose.
   XrSpaceLocation local_from_viewer_;
-  std::vector<XrCompositionLayerProjectionView> layer_projection_views_;
 
   std::unique_ptr<OpenXrAnchorManager> anchor_manager_;
   std::unique_ptr<OpenXRSceneUnderstandingManager> scene_understanding_manager_;
+
+  // The context provider is owned by the OpenXrRenderLoop, and may change when
+  // there is a context lost.
+  scoped_refptr<viz::ContextProvider> context_provider_;
 
   base::WeakPtrFactory<OpenXrApiWrapper> weak_ptr_factory_{this};
 };
