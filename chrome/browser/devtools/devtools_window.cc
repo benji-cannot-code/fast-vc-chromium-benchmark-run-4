6ebbc305da7f5ecc50a04f202dbdbbdef108d966
Alex Turner <alexmt@chromium.org>
2022-01-13 13:55:47 +0000

    Modernize {Dictionary,List}PrefUpdate in /chrome/browser/devtools
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /chrome/browser/devtools
    
    This CL was uploaded by git cl split.
    
    R=yangguo@chromium.org
    
    Bug: 1285745
    Change-Id: Id76e966dd9a241cc6214a660dd0875fcaa01e267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386000
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958582}

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -134,8 +134,7 @@ void SetPreferencesFromJson(Profile* profile, const std::string& json) {
   absl::optional<base::Value> parsed = base::JSONReader::Read(json);
   if (!parsed || !parsed->is_dict())
     return;
-  DictionaryPrefUpdateDeprecated update(profile->GetPrefs(),
-                                        prefs::kDevToolsPreferences);
+  DictionaryPrefUpdate update(profile->GetPrefs(), prefs::kDevToolsPreferences);
   for (auto dict_value : parsed->DictItems()) {
     if (!dict_value.second.is_string())
       continue;
@@ -1659,7 +1658,7 @@ void DevToolsWindow::CreateDevToolsBrowser() {
            ->FindKey(kDevToolsApp)) {
     // Ensure there is always a default size so that
     // BrowserFrame::InitBrowserFrame can retrieve it later.
-    DictionaryPrefUpdateDeprecated update(prefs, prefs::kAppWindowPlacement);
+    DictionaryPrefUpdate update(prefs, prefs::kAppWindowPlacement);
     base::Value* wp_prefs = update.Get();
     base::Value dev_tools_defaults(base::Value::Type::DICTIONARY);
     dev_tools_defaults.SetIntKey("left", 100);
