678635d25c80dc605c22f148b80631835f741220
Dave Tapuska <dtapuska@chromium.org>
2022-01-13 20:31:57 +0000

    Use ForEachRenderFrameHost instead of ForEachFrame.
    As a followup to crrev.com/b1aa83f2 iterate over the page's frames
    instead of going via WebContents.
    
    R=msramek@chromium.org
    BUG=1208438
    
    Change-Id: Id97a0f0849505fd7f1e31fd5dff29dd320297166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387691
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958799}

--- chrome/browser/ui/content_settings/content_setting_bubble_model.cc
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.cc
@@ -380,7 +380,7 @@ void ContentSettingMixedScriptBubbleModel::OnCustomLinkClicked() {
   }
 
   // Update renderer side settings to allow active mixed content.
-  web_contents()->ForEachFrame(
+  GetPage().GetMainDocument().ForEachRenderFrameHost(
       base::BindRepeating(&::SetAllowRunningInsecureContent));
 }
 
