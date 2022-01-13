75456ce1ef6393c1c14fe27afa838958078f65cc
David Black <dmblack@google.com>
2022-01-13 04:55:40 +0000

    Expose feature flag to enable holding space in-progress animations v2.
    When enabled, v2 in-progress animations will be used which adds a
    different outer ring treatment and adds an inner ring and inner icon
    to progress indicators.
    
    Bug: 1280377
    Change-Id: I2fe16cb2b2b2d9a42cc37d6a7de23b1a85254261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3384941
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#958436}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2711,6 +2711,9 @@ extern const char kHelpAppLauncherSearchDescription[];
 extern const char kHelpAppSearchServiceIntegrationName[];
 extern const char kHelpAppSearchServiceIntegrationDescription[];
 
+extern const char kHoldingSpaceInProgressAnimationV2Name[];
+extern const char kHoldingSpaceInProgressAnimationV2Description[];
+
 extern const char kHoldingSpaceInProgressDownloadsIntegrationName[];
 extern const char kHoldingSpaceInProgressDownloadsIntegrationDescription[];
 
