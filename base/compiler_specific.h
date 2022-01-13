b06cfb912765459531e54ebf9b9c39f251473e5a
Roland Bock <rbock@google.com>
2022-01-13 13:40:29 +0000

    Drop FALLTHROUGH macro
    It has been replaced by the C++17 attribute [[fallthrough]].
    
    See go/chromium-lsc-fallthrough
    
    Bug: 1283907
    Change-Id: I0e1bdfc9146540f4de70b1868d1ab8b0e74832e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379142
    Reviewed-by: danakj chromium <danakj@chromium.org>
    Commit-Queue: danakj chromium <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958576}

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -241,9 +241,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAS_FEATURE(FEATURE) 0
 #endif
 
-// Macro for telling -Wimplicit-fallthrough that a fallthrough is intentional.
-#define FALLTHROUGH [[fallthrough]]
-
 #if defined(COMPILER_GCC)
 #define PRETTY_FUNCTION __PRETTY_FUNCTION__
 #elif defined(COMPILER_MSVC)
