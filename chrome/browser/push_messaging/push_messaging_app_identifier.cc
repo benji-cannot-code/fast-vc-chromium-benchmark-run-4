372b138c9452307c44dae6da9267f68eefc4af25
Alex Turner <alexmt@chromium.org>
2022-01-13 11:21:37 +0000

    Modernize {Dictionary,List}PrefUpdate in /chrome/browser/push_messaging
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /chrome/browser/push_messaging
    
    This CL was uploaded by git cl split.
    
    R=mvanouwerkerk@chromium.org
    
    Bug: 1285745
    Change-Id: Ib65c95e1c849155ba604782f440e3d631f92d849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386021
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958526}

--- chrome/browser/push_messaging/push_messaging_app_identifier.cc
+++ chrome/browser/push_messaging/push_messaging_app_identifier.cc
@@ -222,8 +222,8 @@ std::vector<PushMessagingAppIdentifier> PushMessagingAppIdentifier::GetAll(
 
 // static
 void PushMessagingAppIdentifier::DeleteAllFromPrefs(Profile* profile) {
-  DictionaryPrefUpdateDeprecated update(profile->GetPrefs(),
-                                        prefs::kPushMessagingAppIdentifierMap);
+  DictionaryPrefUpdate update(profile->GetPrefs(),
+                              prefs::kPushMessagingAppIdentifierMap);
   base::Value* map = update.Get();
   map->DictClear();
 }
@@ -260,8 +260,8 @@ bool PushMessagingAppIdentifier::IsExpired() const {
 void PushMessagingAppIdentifier::PersistToPrefs(Profile* profile) const {
   DCheckValid();
 
-  DictionaryPrefUpdateDeprecated update(profile->GetPrefs(),
-                                        prefs::kPushMessagingAppIdentifierMap);
+  DictionaryPrefUpdate update(profile->GetPrefs(),
+                              prefs::kPushMessagingAppIdentifierMap);
   base::Value* map = update.Get();
 
   // Delete any stale entry with the same origin and Service Worker
@@ -279,8 +279,8 @@ void PushMessagingAppIdentifier::PersistToPrefs(Profile* profile) const {
 void PushMessagingAppIdentifier::DeleteFromPrefs(Profile* profile) const {
   DCheckValid();
 
-  DictionaryPrefUpdateDeprecated update(profile->GetPrefs(),
-                                        prefs::kPushMessagingAppIdentifierMap);
+  DictionaryPrefUpdate update(profile->GetPrefs(),
+                              prefs::kPushMessagingAppIdentifierMap);
   base::Value* map = update.Get();
   map->RemoveKey(app_id_);
 }
