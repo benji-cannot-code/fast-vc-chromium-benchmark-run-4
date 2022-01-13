b172b7d1309dd81bd60052469a8d4ec171e335ab
Jason Zhang <jiajunz@google.com>
2022-01-13 23:47:43 +0000

    [CrOS Cellular] Hide related search result for eSIM policy
    This CL removes the Rename/Remove cellular profile from search bar if
    the active profile is managed, also removes the Add Cellular result if
    restrict managed cellular network is enabled.
    
    Bug: b/212638661
    Change-Id: I47d0ae14c599a7d3641a00f1653801415480c79d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371629
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#958924}

--- chrome/browser/ui/webui/settings/chromeos/internet_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/internet_section.cc
@@ -356,7 +356,7 @@ const std::vector<SearchConcept>& GetCellularConnectedSearchConcepts() {
   return *tags;
 }
 
-const std::vector<SearchConcept>& GetCellularESimCapableSearchTerms() {
+const std::vector<SearchConcept>& GetCellularAddESimSearchTerms() {
   static const base::NoDestructor<std::vector<SearchConcept>> tags({
       {IDS_OS_SETTINGS_TAG_ADD_ESIM,
        mojom::kMobileDataNetworksSubpagePath,
@@ -370,7 +370,8 @@ const std::vector<SearchConcept>& GetCellularESimCapableSearchTerms() {
   return *tags;
 }
 
-const std::vector<SearchConcept>& GetCellularPrimaryIsESimSearchConcepts() {
+const std::vector<SearchConcept>&
+GetCellularPrimaryIsNonPolicyESimSearchConcepts() {
   static const base::NoDestructor<std::vector<SearchConcept>> tags({
       {IDS_OS_SETTINGS_TAG_CELLULAR_REMOVE_PROFILE,
        mojom::kCellularDetailsSubpagePath,
@@ -566,8 +567,14 @@ std::string GetDetailsSubpageUrl(const std::string& url_to_modify,
       url_to_modify.find('?') == std::string::npos ? "?" : "&", guid.c_str());
 }
 
-bool IsESimCapable() {
-  return HermesManagerClient::Get()->GetAvailableEuiccs().size() != 0;
+bool AllowAddESim(const network_config::mojom::GlobalPolicyPtr& global_policy) {
+  if (HermesManagerClient::Get()->GetAvailableEuiccs().size() == 0)
+    return false;
+
+  if (!base::FeatureList::IsEnabled(ash::features::kESimPolicy))
+    return true;
+
+  return !global_policy->allow_only_policy_cellular_networks;
 }
 
 absl::optional<std::string> GetCellularActiveSimIccid(
@@ -577,6 +584,11 @@ absl::optional<std::string> GetCellularActiveSimIccid(
       return sim_info->iccid;
   }
   return absl::nullopt;
+}
+
+bool IsPolicySource(network_config::mojom::OncSource onc_source) {
+  return onc_source == network_config::mojom::OncSource::kUserPolicy ||
+         onc_source == network_config::mojom::OncSource::kDevicePolicy;
 }
 
 }  // namespace
@@ -1034,11 +1046,19 @@ void InternetSection::OnActiveNetworksChanged(
 }
 
 void InternetSection::FetchDeviceList() {
+  cros_network_config_->GetGlobalPolicy(
+      base::BindOnce(&InternetSection::OnGlobalPolicy, base::Unretained(this)));
+}
+
+void InternetSection::OnGlobalPolicy(
+    network_config::mojom::GlobalPolicyPtr global_policy) {
   cros_network_config_->GetDeviceStateList(
-      base::BindOnce(&InternetSection::OnDeviceList, base::Unretained(this)));
+      base::BindOnce(&InternetSection::OnDeviceList, base::Unretained(this),
+                     std::move(global_policy)));
 }
 
 void InternetSection::OnDeviceList(
+    network_config::mojom::GlobalPolicyPtr global_policy,
     std::vector<network_config::mojom::DeviceStatePropertiesPtr> devices) {
   using network_config::mojom::DeviceStateType;
   using network_config::mojom::NetworkType;
@@ -1050,7 +1070,7 @@ void InternetSection::OnDeviceList(
   updater.RemoveSearchTags(GetWifiOffSearchConcepts());
   updater.RemoveSearchTags(GetCellularOnSearchConcepts());
   updater.RemoveSearchTags(GetCellularOffSearchConcepts());
-  updater.RemoveSearchTags(GetCellularESimCapableSearchTerms());
+  updater.RemoveSearchTags(GetCellularAddESimSearchTerms());
   updater.RemoveSearchTags(GetInstantTetheringSearchConcepts());
   updater.RemoveSearchTags(GetInstantTetheringOnSearchConcepts());
   updater.RemoveSearchTags(GetInstantTetheringOffSearchConcepts());
@@ -1079,8 +1099,9 @@ void InternetSection::OnDeviceList(
         // check is in OnNetworkList().
         if (device->device_state == DeviceStateType::kEnabled) {
           updater.AddSearchTags(GetCellularOnSearchConcepts());
-          if (IsESimCapable())
-            updater.AddSearchTags(GetCellularESimCapableSearchTerms());
+          if (AllowAddESim(global_policy)) {
+            updater.AddSearchTags(GetCellularAddESimSearchTerms());
+          }
         } else if (device->device_state == DeviceStateType::kDisabled) {
           updater.AddSearchTags(GetCellularOffSearchConcepts());
         }
@@ -1128,7 +1149,7 @@ void InternetSection::OnNetworkList(
   updater.RemoveSearchTags(GetWifiHiddenSearchConcepts());
   updater.RemoveSearchTags(GetCellularSearchConcepts());
   updater.RemoveSearchTags(GetCellularConnectedSearchConcepts());
-  updater.RemoveSearchTags(GetCellularPrimaryIsESimSearchConcepts());
+  updater.RemoveSearchTags(GetCellularPrimaryIsNonPolicyESimSearchConcepts());
   updater.RemoveSearchTags(GetCellularMeteredSearchConcepts());
   updater.RemoveSearchTags(GetInstantTetheringConnectedSearchConcepts());
   updater.RemoveSearchTags(GetVpnConnectedSearchConcepts());
@@ -1152,9 +1173,12 @@ void InternetSection::OnNetworkList(
         active_cellular_guid_ = network->guid;
         updater.AddSearchTags(GetCellularSearchConcepts());
 
-        // If the primary cellular network is ESim.
-        if (!network->type_state->get_cellular()->eid.empty())
-          updater.AddSearchTags(GetCellularPrimaryIsESimSearchConcepts());
+        // If the primary cellular network is ESim and not policy ESim.
+        if (!network->type_state->get_cellular()->eid.empty() &&
+            !IsPolicySource(network->source)) {
+          updater.AddSearchTags(
+              GetCellularPrimaryIsNonPolicyESimSearchConcepts());
+        }
       }
     }
 
--- chrome/browser/ui/webui/settings/chromeos/internet_section.h
+++ chrome/browser/ui/webui/settings/chromeos/internet_section.h
@@ -62,7 +62,9 @@ class InternetSection
   void OnNetworkCertificatesChanged() override {}
 
   void FetchDeviceList();
+  void OnGlobalPolicy(network_config::mojom::GlobalPolicyPtr global_policy);
   void OnDeviceList(
+      network_config::mojom::GlobalPolicyPtr global_policy,
       std::vector<network_config::mojom::DeviceStatePropertiesPtr> devices);
 
   void FetchNetworkList();
