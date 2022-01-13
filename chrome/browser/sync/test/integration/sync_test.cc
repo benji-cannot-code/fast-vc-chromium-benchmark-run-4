180ff759dba9562802b55c2dcf0ccc4c48063541
Alex Turner <alexmt@chromium.org>
2022-01-13 10:50:06 +0000

    Modernize {Dictionary,List}PrefUpdate in /chrome/browser/sync
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /chrome/browser/sync
    
    This CL was uploaded by git cl split.
    
    R=treib@chromium.org
    
    Bug: 1285745
    Change-Id: I03c1df9cb1d90d45a406d4ad038af7c128517c6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386031
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958512}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -776,7 +776,7 @@ void SyncTest::SetUpInvalidations(int index) {
       // real clients, those are stored upon subscription with the
       // per-user-topic server. The pref name is defined in
       // per_user_topic_subscription_manager.cc.
-      DictionaryPrefUpdateDeprecated update(
+      DictionaryPrefUpdate update(
           GetProfile(index)->GetPrefs(),
           "invalidation.per_sender_registered_for_invalidation");
       update->SetKey(kInvalidationGCMSenderId,
@@ -792,11 +792,11 @@ void SyncTest::SetUpInvalidations(int index) {
                      base::Value("/private/" + notification_type +
                                  "-topic_server_user_id"));
       }
-      DictionaryPrefUpdateDeprecated update_client_id(
+      DictionaryPrefUpdate update_client_id(
           GetProfile(index)->GetPrefs(),
           invalidation::prefs::kInvalidationClientIDCache);
 
-      update_client_id->SetString(kInvalidationGCMSenderId, client_id);
+      update_client_id->SetStringKey(kInvalidationGCMSenderId, client_id);
       break;
     }
     case SERVER_TYPE_UNDECIDED:
