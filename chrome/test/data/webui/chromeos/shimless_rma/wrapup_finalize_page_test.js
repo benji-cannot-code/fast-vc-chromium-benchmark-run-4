0da3c2e09c7febafc9fb6608219a90bb9a1c3f2f
Gavin Williams <gavinwill@chromium.org>
2022-01-13 01:50:10 +0000

    Shimless: Disable finalize retry button
    Disable finalize retry button while processing a retry
    
    https://screenshot.googleplex.com/BZmD9SjKjybNJo9
    
    Bug: 1198187
    Change-Id: I228745bb24935c15ba6a3d02fa87a36d3bfbea98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3371398
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958396}

--- chrome/test/data/webui/chromeos/shimless_rma/wrapup_finalize_page_test.js
+++ chrome/test/data/webui/chromeos/shimless_rma/wrapup_finalize_page_test.js
@@ -151,4 +151,14 @@ export function wrapupFinalizePageTest() {
     await flushTasks();
     assertEquals(1, callCount);
   });
+
+  test('FinalizationRetryButtonDisabled', async () => {
+    await initializeFinalizePage();
+
+    const retryButton =
+        component.shadowRoot.querySelector('#retryFinalizationButton');
+    assertFalse(retryButton.disabled);
+    component.allButtonsDisabled = true;
+    assertTrue(retryButton.disabled);
+  });
 }
