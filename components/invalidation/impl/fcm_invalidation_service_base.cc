822b029160cc8cd451c4d2e5a368bff6c776952a
Alex Turner <alexmt@chromium.org>
2022-01-13 11:24:38 +0000

    Modernize {Dictionary,List}PrefUpdate in /components/invalidation
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /components/invalidation
    
    This CL was uploaded by git cl split.
    
    R=treib@chromium.org
    
    Bug: 1285745
    Change-Id: Idef5d9bb3da028fb5a8beacf5e83ae9bc9eb2c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386088
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958528}

--- components/invalidation/impl/fcm_invalidation_service_base.cc
+++ components/invalidation/impl/fcm_invalidation_service_base.cc
@@ -30,9 +30,8 @@ void MigratePrefs(PrefService* prefs, const std::string& sender_id) {
   if (!prefs->HasPrefPath(prefs::kFCMInvalidationClientIDCacheDeprecated)) {
     return;
   }
-  DictionaryPrefUpdateDeprecated update(prefs,
-                                        prefs::kInvalidationClientIDCache);
-  update->SetString(
+  DictionaryPrefUpdate update(prefs, prefs::kInvalidationClientIDCache);
+  update->SetStringKey(
       sender_id,
       prefs->GetString(prefs::kFCMInvalidationClientIDCacheDeprecated));
   prefs->ClearPref(prefs::kFCMInvalidationClientIDCacheDeprecated);
@@ -277,8 +276,7 @@ void FCMInvalidationServiceBase::ResetClientID() {
   // source of truth, and are responsible for ensuring that the deletion
   // actually happens.
   client_id_.clear();
-  DictionaryPrefUpdateDeprecated update(pref_service_,
-                                        prefs::kInvalidationClientIDCache);
+  DictionaryPrefUpdate update(pref_service_, prefs::kInvalidationClientIDCache);
   update->RemoveKey(sender_id_);
 
   // Also let the registrar (and its observers) know that the instance ID is
@@ -297,8 +295,8 @@ void FCMInvalidationServiceBase::OnInstanceIDReceived(
   diagnostic_info_.instance_id_received = base::Time::Now();
   if (client_id_ != instance_id) {
     client_id_ = instance_id;
-    DictionaryPrefUpdateDeprecated update(pref_service_,
-                                          prefs::kInvalidationClientIDCache);
+    DictionaryPrefUpdate update(pref_service_,
+                                prefs::kInvalidationClientIDCache);
     update->SetStringKey(sender_id_, instance_id);
     invalidator_registrar_.UpdateInvalidatorInstanceId(instance_id);
   }
