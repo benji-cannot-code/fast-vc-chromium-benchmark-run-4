c27593a6d5312db0bba9e793bd6aedd34e39bae5
Sean Topping <seantopping@chromium.org>
2022-01-13 21:30:28 +0000

    [Chromecast] Use MemoryPressureMonitor from BrowserMainLoop
    We shouldn't create MultiSourceMemoryPressureMonitor ourselves since
    there is already an instance. Instead of creating a duplicate (which
    triggers a DCHECK), just reuse the existing instance.
    
    CastSystemMemoryPressureEvaluatorAdjuster is also unused at this point,
    so it has been removed.
    
    Bug: None
    Test: Build/run cast_browser, verify no crash.
    Change-Id: Ia04a39466f847eec7f3771605bc0e88f40893af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3385298
    Reviewed-by: Junbo Ke <juke@chromium.org>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958833}

--- chromecast/browser/cast_browser_main_parts.h
+++ chromecast/browser/cast_browser_main_parts.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "chromecast/browser/display_configurator_observer.h"
 #include "chromecast/chromecast_buildflags.h"
-#include "components/memory_pressure/multi_source_memory_pressure_monitor.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_main_parts.h"
 #include "content/public/common/main_function_params.h"
@@ -37,7 +36,6 @@ class ViewsDelegate;
 
 namespace chromecast {
 class CastFeatureUpdateObserver;
-class CastSystemMemoryPressureEvaluatorAdjuster;
 class CastWebService;
 class DisplaySettingsManager;
 class ServiceConnector;
@@ -160,12 +158,6 @@ class CastBrowserMainParts : public content::BrowserMainParts {
   // Tracks all media pipeline backends.
   std::unique_ptr<media::MediaPipelineBackendManager>
       media_pipeline_backend_manager_;
-#if !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
-  std::unique_ptr<memory_pressure::MultiSourceMemoryPressureMonitor>
-      memory_pressure_monitor_;
-#endif  // !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
-  CastSystemMemoryPressureEvaluatorAdjuster*
-      cast_system_memory_pressure_evaluator_adjuster_;
 
 #if BUILDFLAG(ENABLE_CHROMECAST_EXTENSIONS)
   std::unique_ptr<extensions::ExtensionsClient> extensions_client_;
