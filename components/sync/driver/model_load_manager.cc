416c2a86a8504b4df2482373f33abbcdbe717c63
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-01-13 18:52:22 +0000

    Reland "[C++17] Use structured bindings in c/sync/* and some unrelated clean-ups"
    This is a reland of d81c754d42a1e5717a93bce50c27b529a8bc5349
    I believe the original change caused the compiler to mistake a function
    call inside TRACE_EVENT for a declaration, and then complain that it
    shadowed an existing variable. Not sure if that's really it, but either
    way the build error is fixed.
    
    Original change's description:
    > [C++17] Use structured bindings in c/sync/* and some unrelated clean-ups
    >
    > No behavior is changed.
    > This avoids having confusing `first/second` all over, especially when
    > iterating over maps.
    > This feature is already allowed by the styleguide [1].
    > crrev.com/c/3385515 is the corresponding CL for components/sync_*/
    >
    > [1] https://source.chromium.org/chromium/chromium/src/+/main:styleguide/c++/c++11.md;l=484;drc=6f510fa949f6bf2425891387d492c932f513ac47
    >
    > Bug: 1286934
    > Change-Id: Iac4a9bec878958cea7e2a13af598c1d77c84bec4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3385175
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Victor Vianna <victorvianna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#958533}
    
    Bug: 1286934
    Change-Id: Id8a1cd632798ae0dc59d5596a808339578cb0b7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386553
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958725}

--- components/sync/driver/model_load_manager.cc
+++ components/sync/driver/model_load_manager.cc
@@ -56,8 +56,7 @@ void ModelLoadManager::Initialize(ModelTypeSet desired_types,
 
   DVLOG(1) << "ModelLoadManager: Stopping disabled types.";
   std::map<DataTypeController*, ShutdownReason> types_to_stop;
-  for (const auto& type_and_dtc : *controllers_) {
-    DataTypeController* dtc = type_and_dtc.second.get();
+  for (const auto& [type, dtc] : *controllers_) {
     // We generally stop all data types which are not desired. When the storage
     // option changes, we need to restart all data types so that they can
     // re-wire to the correct storage.
@@ -79,7 +78,7 @@ void ModelLoadManager::Initialize(ModelTypeSet desired_types,
           configure_context_.sync_mode == SyncMode::kTransportOnly) {
         reason = ShutdownReason::STOP_SYNC_AND_KEEP_DATA;
       }
-      types_to_stop[dtc] = reason;
+      types_to_stop[dtc.get()] = reason;
     }
   }
 
@@ -90,9 +89,7 @@ void ModelLoadManager::Initialize(ModelTypeSet desired_types,
       types_to_stop.size(), base::BindOnce(&ModelLoadManager::LoadDesiredTypes,
                                            weak_ptr_factory_.GetWeakPtr()));
 
-  for (const auto& dtc_and_reason : types_to_stop) {
-    DataTypeController* dtc = dtc_and_reason.first;
-    const ShutdownReason reason = dtc_and_reason.second;
+  for (const auto& [dtc, reason] : types_to_stop) {
     DVLOG(1) << "ModelLoadManager: stop " << dtc->name() << " due to "
              << ShutdownReasonToString(reason);
     StopDatatypeImpl(SyncError(), reason, dtc, barrier_closure);
@@ -156,13 +153,13 @@ void ModelLoadManager::Stop(ShutdownReason shutdown_reason) {
   weak_ptr_factory_.InvalidateWeakPtrs();
 
   // Stop started data types.
-  for (const auto& type_and_dtc : *controllers_) {
-    DataTypeController* dtc = type_and_dtc.second.get();
+  for (const auto& [type, dtc] : *controllers_) {
     if (dtc->state() != DataTypeController::NOT_RUNNING &&
         dtc->state() != DataTypeController::STOPPING) {
       // We don't really wait until all datatypes have been fully stopped, which
       // is only required (and in fact waited for) when Initialize() is called.
-      StopDatatypeImpl(SyncError(), shutdown_reason, dtc, base::DoNothing());
+      StopDatatypeImpl(SyncError(), shutdown_reason, dtc.get(),
+                       base::DoNothing());
       DVLOG(1) << "ModelLoadManager: Stopped " << dtc->name();
     }
   }
