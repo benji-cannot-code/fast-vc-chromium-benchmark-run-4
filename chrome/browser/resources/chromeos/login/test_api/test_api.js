5bb593fe69f1aabe6a8c908464a219ce2ce56aee
Roman Sorokin <rsorokin@chromium.org>
2022-01-13 17:09:10 +0000

    [ChromeOS][OOBE] Provide EULA next button name via test api
    The name to be used to find the button on the tast side (see
    http://crrev.com/c/3338658)
    
    Bug: 1286336
    Change-Id: If659acba9259002a87999fad2a7e4674b9c6d71a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379155
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958662}

--- chrome/browser/resources/chromeos/login/test_api/test_api.js
+++ chrome/browser/resources/chromeos/login/test_api/test_api.js
@@ -192,6 +192,10 @@ class EulaScreenTester extends ScreenElementApi {
     return this.isVisible() && this.eulaStep.isVisible() &&
         this.nextButton.isVisible();
   }
+
+  getNextButtonName() {
+    return loadTimeData.getString('oobeEulaAcceptAndContinueButtonText');
+  }
 }
 
 class UpdateScreenTester extends ScreenElementApi {
