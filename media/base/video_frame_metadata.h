1fd7413db22feed4db0fc14830017830aab54824
Dale Curtis <dalecurtis@chromium.org>
2022-01-13 21:45:00 +0000

    Fix issue with VideoFrameMetadata merging for bool=true fields.
    The code was written assuming that if (metadata.value) was false,
    it shouldn't be merged. This is fine for optional fields, but not
    bool fields.
    
    Specifically in this bug, `texture_origin_is_top_left` defaults to
    true, but is set to false when a canvas backed VideoFrame is made.
    The old path would fail to copy the false value over.
    
    This fixes the merge process and adds a test for this case. It also
    adds a missing 'dcomp_surface' to the merge process.
    
    R=tguilbert
    
    Fixed: 1277865
    Change-Id: I96f7a8032327adaa150ffe7412883bcfb051f71e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3385357
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958843}

--- media/base/video_frame_metadata.h
+++ media/base/video_frame_metadata.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+// NOTE: When adding new VideoFrameMetadata fields, please ensure you update the
+// MergeMetadataFrom() method.
 struct MEDIA_EXPORT VideoFrameMetadata {
   VideoFrameMetadata();
   ~VideoFrameMetadata() = default;
