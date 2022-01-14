45a466a5f4ac3825433342e8a2408fa5bb99ead8
Ayu Ishii <ayui@chromium.org>
2022-01-14 02:02:42 +0000

    StorageBuckets: Fix FileSystem GetOrCreateBucket
    This change updates the automatic default bucket creation
    in FileSystem to take in StorageType. We still support
    kSyncable and kPersistent for now, and should make a default
    bucket with the appropriate type. This also adds a new
    GetOrCreateBucketDeprecated method because we'd like to
    discourage new usage of this method that sets StorageType.
    All other use cases should use GetOrCreateBucket which
    creates a kTemporary storage bucket. Deprecation efforts for
    kPersistent & kSyncable can be tracked here:
    crbug.com/1233525, crbug.com/1286964
    
    Change-Id: I11dc198845507565603e28f9a1072aa1630d3d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3385254
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958978}

--- chrome/browser/sync_file_system/local/local_file_sync_context_unittest.cc
+++ chrome/browser/sync_file_system/local/local_file_sync_context_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/test/test_future.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/sync_file_system/local/canned_syncable_file_system.h"
 #include "chrome/browser/sync_file_system/local/local_file_change_tracker.h"
@@ -24,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/sync_file_metadata.h"
 #include "chrome/browser/sync_file_system/sync_status_code.h"
 #include "chrome/browser/sync_file_system/syncable_file_system_util.h"
+#include "components/services/storage/public/cpp/buckets/bucket_info.h"
+#include "components/services/storage/public/cpp/buckets/constants.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/browser_task_environment.h"
@@ -31,8 +34,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/file_system/file_system_context.h"
 #include "storage/browser/file_system/file_system_operation_runner.h"
 #include "storage/browser/file_system/isolated_context.h"
+#include "storage/browser/quota/quota_manager.h"
+#include "storage/browser/quota/quota_manager_proxy.h"
 #include "storage/browser/test/mock_blob_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/leveldatabase/leveldb_chrome.h"
 
 #define FPL FILE_PATH_LITERAL
@@ -402,6 +408,40 @@ TEST_F(LocalFileSyncContextTest, InitializeFileSystemContext) {
   file_system.GetChangedURLsInTracker(&urls);
   ASSERT_EQ(1U, urls.size());
   EXPECT_TRUE(base::Contains(urls, kURL));
+
+  // Finishing the test.
+  sync_context_->ShutdownOnUIThread();
+  file_system.TearDown();
+}
+
+TEST_F(LocalFileSyncContextTest, CreateDefaultSyncableBucket) {
+  CannedSyncableFileSystem file_system(GURL(kOrigin1), in_memory_env_.get(),
+                                       io_task_runner_.get(),
+                                       file_task_runner_.get());
+  file_system.SetUp(CannedSyncableFileSystem::QUOTA_ENABLED);
+
+  sync_context_ = base::MakeRefCounted<LocalFileSyncContext>(
+      dir_.GetPath(), in_memory_env_.get(), ui_task_runner_.get(),
+      io_task_runner_.get());
+
+  // Initializes file_system using `sync_context_`.
+  EXPECT_EQ(SYNC_STATUS_OK,
+            file_system.MaybeInitializeFileSystemContext(sync_context_.get()));
+
+  // Opens the file_system, to verify a kSyncable bucket is created.
+  EXPECT_EQ(base::File::FILE_OK, file_system.OpenFileSystem());
+
+  base::test::TestFuture<storage::QuotaErrorOr<storage::BucketInfo>> future;
+  file_system.quota_manager()->proxy()->GetBucket(
+      blink::StorageKey::CreateFromStringForTesting(kOrigin1),
+      storage::kDefaultBucketName, blink::mojom::StorageType::kSyncable,
+      base::SequencedTaskRunnerHandle::Get(), future.GetCallback());
+
+  const auto result = future.Take();
+  EXPECT_TRUE(result.ok());
+  EXPECT_EQ(result->name, storage::kDefaultBucketName);
+  EXPECT_EQ(result->type, blink::mojom::StorageType::kSyncable);
+  EXPECT_GT(result->id.value(), 0);
 
   // Finishing the test.
   sync_context_->ShutdownOnUIThread();
