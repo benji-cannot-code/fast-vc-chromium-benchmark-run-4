ecf9e2c19a7a4c5bafcd79c0a5f6bb2da5988ca5
Aliona DANGLA <alionadangla@google.com>
2022-01-14 15:07:53 +0000

    [iOS] Remove Password manager logo on alerts
    This CL:
    * Removes Google password manager logo image as it can't be localised
      easily
    * Reverts confirmation_alert_view_controller at its initial state
      (crrev.com/c/3289421) as specific content view is not needed any more
    
    See breach alert screens:
    https://drive.google.com/file/d/16rbZgHvSGhtHyofapMY_LSf7MQlrchW_/view?usp=sharing
    https://drive.google.com/file/d/1mhlC2PppdYwv02Ar4GleRmDQsRFJ1ZfG/view?usp=sharing
    
    See dynamic type + VoiceOver video:
    https://drive.google.com/file/d/1WwOaMZenYL7qS2i_yMxDY5D6ulIXX2rK/view?usp=sharing
    
    Fixed: 1283622, 1286774
    Change-Id: I2bb3908b3fcff7be41bb0fe3b3feebfe161ad814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383858
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@google.com>
    Cr-Commit-Position: refs/heads/main@{#959179}

--- ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
+++ ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
@@ -15,13 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-namespace {
-
-// Picture width of the branding.
-constexpr CGFloat kLogoWidth = 180;
-
-}  // namespace
-
 @implementation PasswordBreachViewController
 
 #pragma mark - Public
@@ -47,33 +40,6 @@ constexpr CGFloat kLogoWidth = 180;
   [super viewDidLoad];
   self.view.accessibilityIdentifier =
       kPasswordBreachViewAccessibilityIdentifier;
-
-  if (base::FeatureList::IsEnabled(
-          password_manager::features::
-              kIOSEnablePasswordManagerBrandingUpdate)) {
-    UIImageView* logoImageView = [[UIImageView alloc]
-        initWithImage:[UIImage imageNamed:@"passwords_logo_colored"]];
-    logoImageView.contentMode = UIViewContentModeScaleAspectFit;
-    logoImageView.translatesAutoresizingMaskIntoConstraints = NO;
-
-    [self.specificContentSuperview addSubview:logoImageView];
-
-    logoImageView.isAccessibilityElement = YES;
-    logoImageView.accessibilityLabel = l10n_util::GetNSString(
-        IDS_IOS_PASSWORD_MANAGER_LOGO_ACCESSIBILITY_LABEL);
-
-    [NSLayoutConstraint activateConstraints:@[
-      [logoImageView.topAnchor
-          constraintEqualToAnchor:self.specificContentLayoutGuide.topAnchor],
-      [logoImageView.centerXAnchor
-          constraintEqualToAnchor:self.specificContentLayoutGuide
-                                      .centerXAnchor],
-      [logoImageView.widthAnchor
-          constraintLessThanOrEqualToConstant:kLogoWidth],
-      [logoImageView.bottomAnchor
-          constraintEqualToAnchor:self.specificContentLayoutGuide.bottomAnchor],
-    ]];
-  }
 }
 
 #pragma mark - PasswordBreachConsumer
