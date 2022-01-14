f5fd4af01b0e14d0dfb811fbc541e5f8971eb6e1
Kuo Jen Wei <inker@google.com>
2022-01-14 08:28:39 +0000

    CCA: Enumerate camera configuration candidates with AsyncIter
    Rewrite the old camera configuration candidates enumeration of chaining
    calls from multiple methods and replace it with a AsyncIter
    implementation.
    
    Bug: b:209726472
    Test: tast run "camera.CCAUI*"
    Change-Id: I9ef4b5a04ea646181460a62a94183617843b8df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3385120
    Auto-Submit: Inker Kuo <inker@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959068}

--- ash/webui/camera_app_ui/resources/js/views/camera_intent.ts
+++ ash/webui/camera_app_ui/resources/js/views/camera_intent.ts
@@ -135,7 +135,7 @@ export class CameraIntent extends Camera {
         {resolution: videoResult.resolution, duration: videoResult.duration});
   }
 
-  async startWithDevice(deviceId: string): Promise<boolean> {
-    return this.startWithMode(deviceId, this.defaultMode);
+  protected async getModeCandidates(): Promise<Mode[]> {
+    return [this.defaultMode];
   }
 }
