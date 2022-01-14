5f8c4d2fb1c2894249ee214b08271b23cca7f68f
Xinghui Lu <xinghuilu@chromium.org>
2022-01-14 01:35:00 +0000

    Remove pending download requests before the profile is destroyed.
    Currently, CheckClientDownloadRequestBase holds a raw pointer of
    browser_context. If the function is triggered after the profile is
    destroyed, it will cause UAF.
    
    In this CL, let SafeBrowsingService notify DownloadProtectionService
    that the profile is going to be destroyed, and DownloadProtectionService
    will delete all pending requests that are associated with this profile.
    
    A long term fix would be making the DownloadProtectionService a keyed
    service and removing the pending requests directly during shutdown. This
    fix will be addressed in a future CL.
    
    Bug: 1284584
    Change-Id: I540735a0ca522c98db34d4bb8c766601d4e41a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381677
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958965}

--- chrome/browser/safe_browsing/services_delegate_desktop.cc
+++ chrome/browser/safe_browsing/services_delegate_desktop.cc
@@ -158,4 +158,8 @@ void ServicesDelegateDesktop::StopOnIOThread(bool shutdown) {
   database_manager_->StopOnIOThread(shutdown);
 }
 
+void ServicesDelegateDesktop::OnProfileWillBeDestroyed(Profile* profile) {
+  download_service_->RemovePendingDownloadRequests(profile);
+}
+
 }  // namespace safe_browsing
