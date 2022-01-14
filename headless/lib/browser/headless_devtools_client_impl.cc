b50a011e05a8e6eba9ac4a2c05218251e4a2ba42
Michael Ershov <miersh@google.com>
2022-01-14 04:16:34 +0000

    [Code Health] Remove DictionaryValue::GetString
    Replace deprecated DictionaryValue::GetString with a modern
    alternative.
    
    Bug: 1187036
    Test: Existing automated tests
    Change-Id: I77c93c345d24948658a31c7c3ddf7665c5750799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386659
    Auto-Submit: Michael Ershov <miersh@google.com>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959032}

--- headless/lib/browser/headless_devtools_client_impl.cc
+++ headless/lib/browser/headless_devtools_client_impl.cc
@@ -162,9 +162,9 @@ void HeadlessDevToolsClientImpl::ReceiveProtocolMessage(
   std::unique_ptr<base::DictionaryValue> message_dict =
       base::DictionaryValue::From(std::move(message));
 
-  std::string session_id;
-  if (message_dict->GetString("sessionId", &session_id)) {
-    auto it = sessions_.find(session_id);
+  const std::string* session_id = message_dict->FindStringKey("sessionId");
+  if (session_id) {
+    auto it = sessions_.find(*session_id);
     if (it != sessions_.end()) {
       it->second->ReceiveProtocolMessage(json_message, std::move(message_dict));
       return;
@@ -522,7 +522,7 @@ HeadlessDevToolsClientImpl::Callback::Callback(
 
 HeadlessDevToolsClientImpl::Callback::~Callback() = default;
 
-HeadlessDevToolsClientImpl::Callback& HeadlessDevToolsClientImpl::Callback::
-operator=(Callback&& other) = default;
+HeadlessDevToolsClientImpl::Callback&
+HeadlessDevToolsClientImpl::Callback::operator=(Callback&& other) = default;
 
 }  // namespace headless
