85cc341c1f318e2143245ca6e7bdde6cf0d36fe6
Daniel Cheng <dcheng@chromium.org>
2022-01-14 14:39:55 +0000

    Convert WARN_UNUSED_RESULT to [[nodiscard]] in //courgette.
    This is an automated conversion produced by the script and command in
    the attached bug.
    
    Bug: 1287045
    Change-Id: I9217c39f13991966e35ea1c4fc02e4229f7bf595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387499
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959153}

--- courgette/memory_allocator.h
+++ courgette/memory_allocator.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 #include <string.h>
 
-#include "base/compiler_specific.h"
 #include "base/files/file.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
@@ -361,7 +360,7 @@ class NoThrowBuffer {
     return size_ == 0;
   }
 
-  CheckBool reserve(size_t size) WARN_UNUSED_RESULT {
+  [[nodiscard]] CheckBool reserve(size_t size) {
     if (failed())
       return false;
 
@@ -387,7 +386,7 @@ class NoThrowBuffer {
     return !failed();
   }
 
-  CheckBool append(const T* data, size_t size) WARN_UNUSED_RESULT {
+  [[nodiscard]] CheckBool append(const T* data, size_t size) {
     if (failed())
       return false;
 
@@ -421,7 +420,7 @@ class NoThrowBuffer {
     return true;
   }
 
-  CheckBool resize(size_t size, const T& init_value) WARN_UNUSED_RESULT {
+  [[nodiscard]] CheckBool resize(size_t size, const T& init_value) {
     if (size > size_) {
       if (!reserve(size))
         return false;
@@ -437,9 +436,7 @@ class NoThrowBuffer {
     return true;
   }
 
-  CheckBool push_back(const T& item) WARN_UNUSED_RESULT {
-    return append(&item, 1);
-  }
+  [[nodiscard]] CheckBool push_back(const T& item) { return append(&item, 1); }
 
   const T& back() const {
     return buffer_[size_ - 1];
