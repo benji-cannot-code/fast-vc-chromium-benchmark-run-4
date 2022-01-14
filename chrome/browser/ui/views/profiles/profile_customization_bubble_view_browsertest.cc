6099f7fee1b6910f98b43162b195264ca8ee27cb
Christopher Lam <calamity@chromium.org>
2022-01-14 03:26:31 +0000

    Disable ProfileCustomizationBubbleBrowserTest.InvokeUi_default on Windows
    Bug: 1287341
    Change-Id: I5ae683029bfaf72cfd84ed28b686f1e7bdad30db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388688
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Christopher Lam <calamity@chromium.org>
    Owners-Override: Christopher Lam <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959014}

--- chrome/browser/ui/views/profiles/profile_customization_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_customization_bubble_view_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "chrome/browser/ui/views/profiles/profile_customization_bubble_view.h"
 
 #include <string>
@@ -72,8 +73,13 @@ class ProfileCustomizationBubbleBrowserTest : public DialogBrowserTest {
   base::CallbackListSubscription subscription_;
 };
 
+#if defined(OS_WIN)
+#define MAYBE_InvokeUi_default DISABLED_InvokeUi_default
+#else
+#define MAYBE_InvokeUi_default InvokeUi_default
+#endif
 IN_PROC_BROWSER_TEST_F(ProfileCustomizationBubbleBrowserTest,
-                       InvokeUi_default) {
+                       MAYBE_InvokeUi_default) {
   ShowAndVerifyUi();
 }
 
