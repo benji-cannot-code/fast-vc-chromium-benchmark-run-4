fc22989213b13cb65bb3e9ef2f312cb8ec380970
Avi Drissman <avi@chromium.org>
2022-01-14 01:42:19 +0000

    Use [[maybe_unused]] in Blink
    This cleans up uses of ALLOW_UNUSED_LOCAL and ALLOW_UNUSED_TYPE.
    
    Bug: 1286390
    Change-Id: I6c79d5dc4bf16b3ce46291f64fa345f0786941da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388179
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#958968}

--- third_party/blink/renderer/core/html/forms/html_select_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element.cc
@@ -1158,8 +1158,7 @@ void HTMLSelectElement::SelectOptionByAccessKey(HTMLOptionElement* option) {
 
 unsigned HTMLSelectElement::length() const {
   unsigned options = 0;
-  for (auto* const option : GetOptionList()) {
-    ALLOW_UNUSED_LOCAL(option);
+  for ([[maybe_unused]] auto* const option : GetOptionList()) {
     ++options;
   }
   return options;
