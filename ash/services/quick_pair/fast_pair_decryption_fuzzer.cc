bac8603338fe109c4e672a4842abfa9d0a52f70f
Ben Wagner <bungeman@chromium.org>
2022-01-14 19:21:02 +0000

    [FastPair] Fix fast_pair_encryption_fuzzer build
    In "[FastPair] Move encryption and gatt classes" [0][1] some header
    files were moved, but an include in fast_pair_decryption_fuzzer.cc was
    not updated to the new path. Update this include path.
    
    This was not detected initially since there is no current trybot which
    builds chromeos specific fuzzers. This change should unstick the
    "Libfuzzer Upload Chrome OS ASan" bot [2].
    
    [0] https://chromium-review.googlesource.com/c/chromium/src/+/3291927
    [1] https://chromium.googlesource.com/chromium/src/+/280fd87d777198ac4798a8358d8dcc4d5b952593
    [2] https://ci.chromium.org/p/chromium/builders/ci/Libfuzzer%20Upload%20Chrome%20OS%20ASan
    
    Change-Id: I268c9868bc1a089b7fed27d2c759ff9c383d7e9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388611
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959323}

--- ash/services/quick_pair/fast_pair_decryption_fuzzer.cc
+++ ash/services/quick_pair/fast_pair_decryption_fuzzer.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 
 #include "ash/quick_pair/common/logging.h"
-#include "ash/quick_pair/pairing/fast_pair/fast_pair_encryption.h"
+#include "ash/quick_pair/fast_pair_handshake/fast_pair_encryption.h"
 #include "base/no_destructor.h"
 
 namespace {
