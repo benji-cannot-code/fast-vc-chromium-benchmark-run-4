3efc34c1659929048c639d9f8f27ed2430527346
Xiaohan Wang <xhwang@chromium.org>
2022-01-14 14:47:20 +0000

    components/browsing_data: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Ifc02a3b79f2f27b8a28c0764bfaa8f21660840c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388566
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959158}

--- components/browsing_data/core/pref_names.cc
+++ components/browsing_data/core/pref_names.cc
@@ -90,7 +90,7 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterIntegerPref(
       kClearBrowsingDataHistoryNoticeShownTimes, 0);
 
-#if !defined(OS_IOS)
+#if !BUILDFLAG(IS_IOS)
   registry->RegisterBooleanPref(
       kDeleteDownloadHistory, true,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
@@ -102,7 +102,7 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 #else
   registry->RegisterInt64Pref(prefs::kLastClearBrowsingDataTime, 0);
-#endif  // !defined(OS_IOS)
+#endif  // !BUILDFLAG(IS_IOS)
 
   registry->RegisterIntegerPref(kLastClearBrowsingDataTab, 0);
   registry->RegisterBooleanPref(
