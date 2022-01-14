24f317fc6e12b1fd0d60ca6071611e07d52d0234
Xiaohan Wang <xhwang@chromium.org>
2022-01-14 18:39:38 +0000

    components/enterprise: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I1d25c9a0fcb5f1b83fd19913959673c2b9b4bfc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388736
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959291}

--- components/enterprise/browser/controller/chrome_browser_cloud_management_controller.cc
+++ components/enterprise/browser/controller/chrome_browser_cloud_management_controller.cc
@@ -31,10 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/configuration_policy_provider.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
-#if !defined(OS_ANDROID)
+#if !BUILDFLAG(IS_ANDROID)
 #include "components/enterprise/browser/reporting/report_generator.h"
 #include "components/enterprise/browser/reporting/report_scheduler.h"
-#endif  // !defined(OS_ANDROID)
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 namespace policy {
 
--- components/enterprise/browser/reporting/cloud_reporting_policy_handler.cc
+++ components/enterprise/browser/reporting/cloud_reporting_policy_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/enterprise/browser/reporting/cloud_reporting_policy_handler.h"
 
 #include "base/values.h"
+#include "build/build_config.h"
 #include "components/enterprise/browser/reporting/common_pref_names.h"
 #include "components/policy/core/browser/policy_error_map.h"
 #include "components/policy/core/common/cloud/dm_token.h"
@@ -14,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_value_map.h"
 #include "components/strings/grit/components_strings.h"
 
-#if !defined(OS_CHROMEOS)
+#if !BUILDFLAG(IS_CHROMEOS)
 #include "components/enterprise/browser/controller/browser_dm_token_storage.h"
-#endif  //! defined(OS_CHROMEOS)
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 namespace enterprise_reporting {
 
@@ -33,13 +34,13 @@ bool CloudReportingPolicyHandler::CheckPolicySettings(
     return true;
   if (!TypeCheckingPolicyHandler::CheckPolicySettings(policies, errors))
     return false;
-#if !defined(OS_CHROMEOS)
+#if !BUILDFLAG(IS_CHROMEOS)
   // We don't return false here because machine enrollment status change may
   // not trigger this PolicyHandler later.
   if (!policy::BrowserDMTokenStorage::Get()->RetrieveDMToken().is_valid())
     errors->AddError(policy_name(),
                      IDS_POLICY_CLOUD_MANAGEMENT_ENROLLMENT_ONLY_ERROR);
-#endif  //! defined(OS_CHROMEOS)
+#endif  // !BUILDFLAG(IS_CHROMEOS)
   return true;
 }
 
