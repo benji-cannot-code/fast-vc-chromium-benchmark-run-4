7acaa20c80be87f84a3ce0a29e0c7ff9efbc0703
Nicolas Dossou-Gbete <dgn@chromium.org>
2022-01-14 23:41:55 +0000

    Allow `PostProfileInit()` to be called for each profile load
    Makes `ChromeBrowserMainParts` use a `ProfileManagerObserver` to react
    to profiles being added to trigger `PostProfileInit()`. This allows it
    to be called with the right profile when it is selected during Profile
    Picker startups.
    
    This behaviour is gated by the new `ObserverBasedPostProfileInit`
    feature flag.
    
    Bug: 1150326
    Change-Id: I65ce0e48651b8079169255601c40fb9549c2e283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3323255
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959374}

--- chrome/browser/chrome_browser_main.h
+++ chrome/browser/chrome_browser_main.h
@@ -109,6 +109,7 @@ class ChromeBrowserMainParts : public content::BrowserMainParts {
   }
 
  private:
+  class ProfileInitManager;
   friend class ChromeBrowserMainPartsTestApi;
 
   // Constructs the metrics service and initializes metrics recording.
@@ -218,6 +219,10 @@ class ChromeBrowserMainParts : public content::BrowserMainParts {
   // Indicates that the initial profile has been created and we started
   // executing `PostProfileInit()` for it.
   bool initialized_initial_profile_ = false;
+
+  // Observer that triggers `PostProfileInit()` when new user profiles are
+  // created.
+  std::unique_ptr<ProfileInitManager> profile_init_manager_;
 };
 
 #endif  // CHROME_BROWSER_CHROME_BROWSER_MAIN_H_
