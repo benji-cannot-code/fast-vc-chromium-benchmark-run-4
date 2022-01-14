c7b36f8514b213d3812aea0966d09e1c1bd78eeb
Dominique Fauteux-Chapleau <domfc@chromium.org>
2022-01-14 18:24:58 +0000

    [Sheriff] Disable NotifiesDelegateOfAnimationCycleMarkers on CrOS
    Bug: 1287542
    Change-Id: I81e3e76f1688b336f40898f9a0488e08de7e4e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388873
    Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959287}

--- ash/ambient/ui/ambient_animation_view_unittest.cc
+++ ash/ambient/ui/ambient_animation_view_unittest.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
+#include "build/chromeos_buildflags.h"
 #include "cc/paint/display_item_list.h"
 #include "cc/paint/paint_op_buffer.h"
 #include "cc/paint/skottie_wrapper.h"
@@ -90,7 +92,16 @@ class AmbientAnimationViewTest : public views::ViewsTestBase {
   AmbientAnimationView* view_;
 };
 
-TEST_F(AmbientAnimationViewTest, NotifiesDelegateOfAnimationCycleMarkers) {
+// Flaky: crbug.com/1287542
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_NotifiesDelegateOfAnimationCycleMarkers \
+  DISABLED_NotifiesDelegateOfAnimationCycleMarkers
+#else
+#define MAYBE_NotifiesDelegateOfAnimationCycleMarkers \
+  NotifiesDelegateOfAnimationCycleMarkers
+#endif
+TEST_F(AmbientAnimationViewTest,
+       MAYBE_NotifiesDelegateOfAnimationCycleMarkers) {
   view_->SetBoundsRect(widget_->GetWindowBoundsInScreen());
   widget_->Show();
 
