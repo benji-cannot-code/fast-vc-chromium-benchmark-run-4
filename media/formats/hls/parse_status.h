9e24cf3c8dad602c0075ab06cc41bc47fff989c5
Will Cassella <cassew@chromium.org>
2022-01-14 18:43:59 +0000

    HLS: Add parsing support for attribute lists
    Several HLS tags have their content defined in the form of an
    attribute-list, which is a comma separated list of key-value pairs. The
    valid format of each value is fairly contextual, so this CL adds
    utilities responsible for extracting these key-value pairs, leaving
    their interpretation up the caller. The spec says that unrecognized keys
    should be ignored for forward-compatibility purposes, and that
    duplicated keys should result in an error, both of which this CL
    supports through the AttributeMap type.
    
    Bug: 1266991
    Change-Id: I24f77ff0bd9ff72839ef731ef1c48da44f481687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3372601
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Will Cassella <cassew@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959295}

--- media/formats/hls/parse_status.h
+++ media/formats/hls/parse_status.h
@@ -19,6 +19,8 @@ enum class ParseStatusCode : StatusCodeType {
   kFailedToParseDecimalFloatingPoint,
   kFailedToParseSignedDecimalFloatingPoint,
   kInvalidPlaylistVersion,
+  kMalformedAttributeList,
+  kAttributeListHasDuplicateNames,
   kPlaylistMissingM3uTag,
   kMediaSegmentMissingInfTag,
 };
