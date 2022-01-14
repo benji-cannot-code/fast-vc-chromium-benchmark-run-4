86c8f937e3423f2b5d977b789436b8bdabd84965
Keren Zhu <kerenzhu@chromium.org>
2022-01-14 19:11:51 +0000

    Reland "[WebUI] Add chrome.timeTicks.nowInMicroseconds() to WebUI"
    This is a reland of 36623368a2bfd96750b3b6e7e1de0d4972bad5c0
    
    A race condition is causing flaky test if chrome.send() is called very
    early in the test. See crbug.com/1287020#c3. The root cause is unknown.
    
    This reland removes the end-to-end test.
    
    Original change's description:
    > [WebUI] Add chrome.timeTicks.nowInMicroseconds() to WebUI
    >
    > Add chrome.timeTicks.nowInMicroseconds() to WebUI. It returns the
    > microseconds since the origin of TimeTicks as a BigInt. It is equivalent
    > of TimeTicks::Now().since_origin().InMicroseconds() in C++.
    >
    > This function will facilitate calculating delta between TimeTicks
    > captured in browser and WebUI. This is currently not possible because
    > time units in JS (performance.now, Date) use different origins from
    > TimeTicks.
    >
    > See more discussion in doc: go/timeticks-in-webui
    >
    > Note: We can't expose TimeTicks's raw value as chrome.timeTicks.now().
    > because a) it will involve the banned TimeTicks::ToInternalValue(),
    > b) TimeTicks difference in JS becomes meaningless because it has no unit.
    >
    > Bug: 1269417
    > Change-Id: I18a1868ce1c1f9498660c7298b038907d0fef25a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331615
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#958422}
    
    Bug: 1269417
    Change-Id: Ie9d560ed325b312b9fdaa30cde3d8cec501e8082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388113
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959316}

--- gin/converter.cc
+++ gin/converter.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/strings/string_util.h"
+#include "base/time/time.h"
 #include "v8/include/v8-array-buffer.h"
 #include "v8/include/v8-external.h"
 #include "v8/include/v8-function.h"
@@ -18,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8-value.h"
 
 using v8::ArrayBuffer;
-using v8::Boolean;
 using v8::External;
 using v8::Function;
 using v8::Int32;
@@ -49,7 +49,7 @@ bool FromMaybe(Maybe<T> maybe, U* out) {
 namespace gin {
 
 Local<Value> Converter<bool>::ToV8(Isolate* isolate, bool val) {
-  return Boolean::New(isolate, val).As<Value>();
+  return v8::Boolean::New(isolate, val).As<Value>();
 }
 
 bool Converter<bool>::FromV8(Isolate* isolate, Local<Value> val, bool* out) {
@@ -181,6 +181,12 @@ bool Converter<std::u16string>::FromV8(Isolate* isolate,
              reinterpret_cast<uint16_t*>(base::WriteInto(out, length + 1)), 0,
              length);
   return true;
+}
+
+v8::Local<v8::Value> Converter<base::TimeTicks>::ToV8(v8::Isolate* isolate,
+                                                      base::TimeTicks val) {
+  return v8::BigInt::New(isolate, val.since_origin().InMicroseconds())
+      .As<v8::Value>();
 }
 
 Local<Value> Converter<Local<Function>>::ToV8(Isolate* isolate,
