4de490eec675a4242ac3e7e12de34b96b110c7f5
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 22:37:14 +0000

    chrome/browser/favicon: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I83c6c4905df119e91a489589850ab10755be4719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392722
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959700}

--- chrome/browser/favicon/large_icon_service_factory.cc
+++ chrome/browser/favicon/large_icon_service_factory.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 // Seems like on Android `1 dip == 1 px`. The value of `kDipForServerRequests`
 // can be overridden by `features::kLargeFaviconFromGoogle`.
 const int kDipForServerRequests = 24;
