f03d8f32fe4d031fd168272ae61f93c55632a81f
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 22:48:01 +0000

    chrome/browser/ash: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I27d98fadcfafeb7e7243f8256223206ceb02e29b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392663
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959706}

--- chrome/browser/ash/login/wizard_controller_browsertest.cc
+++ chrome/browser/ash/login/wizard_controller_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/ash/base/locale_util.h"
 #include "chrome/browser/ash/login/demo_mode/demo_setup_controller.h"
@@ -2057,7 +2058,7 @@ class WizardControllerProxyAuthOnSigninTest : public WizardControllerTest {
 };
 
 // TODO(crbug.com/1286218): Flakes on CrOS.
-#if defined(OS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_ProxyAuthDialogOnSigninScreen \
   DISABLED_ProxyAuthDialogOnSigninScreen
 #else
