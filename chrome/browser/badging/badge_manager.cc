de036d9e6e0e2d435a7bf183b2f5e1353c9dc3f9
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 21:37:33 +0000

    chrome/browser/badging: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I99b6ce2b303d93ddf7f47a31d180fd7bbda5bdd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392669
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959671}

--- chrome/browser/badging/badge_manager.cc
+++ chrome/browser/badging/badge_manager.cc
@@ -31,9 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/strings/grit/ui_strings.h"
 
-#if defined(OS_MAC)
+#if BUILDFLAG(IS_MAC)
 #include "chrome/browser/badging/badge_manager_delegate_mac.h"
-#elif defined(OS_WIN)
+#elif BUILDFLAG(IS_WIN)
 #include "chrome/browser/badging/badge_manager_delegate_win.h"
 #endif
 
@@ -72,9 +72,9 @@ BadgeManager::BadgeManager(Profile* profile,
       sync_bridge_(sync_bridge) {
   // The delegate is also set for Chrome OS but is set from the constructor of
   // web_apps_chromeos.cc.
-#if defined(OS_MAC)
+#if BUILDFLAG(IS_MAC)
   SetDelegate(std::make_unique<BadgeManagerDelegateMac>(profile, this));
-#elif defined(OS_WIN)
+#elif BUILDFLAG(IS_WIN)
   SetDelegate(std::make_unique<BadgeManagerDelegateWin>(profile, this));
 #endif
 }
