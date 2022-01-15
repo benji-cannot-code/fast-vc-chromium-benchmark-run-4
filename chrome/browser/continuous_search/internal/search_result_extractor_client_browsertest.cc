43cc5189242ab41805e59d330ba6f94a5ee4cae9
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 22:00:04 +0000

    chrome/browser/continuous_search: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I0db90e736bc724f93bf54801e6a76842d5b448b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392704
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959675}

--- chrome/browser/continuous_search/internal/search_result_extractor_client_browsertest.cc
+++ chrome/browser/continuous_search/internal/search_result_extractor_client_browsertest.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 #include "chrome/test/base/android/android_browser_test.h"
 #else
 #include "chrome/test/base/in_process_browser_test.h"
@@ -32,7 +32,7 @@ namespace continuous_search {
 class SearchResultExtractorClientBrowserTest : public PlatformBrowserTest {
  public:
   SearchResultExtractorClientBrowserTest() {
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
     feature_list_.InitWithFeatures({features::kContinuousSearch}, {});
 #else
     feature_list_.InitWithFeatures(
