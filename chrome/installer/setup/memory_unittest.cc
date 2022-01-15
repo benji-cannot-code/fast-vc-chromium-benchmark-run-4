3873e7227fb351900a51ab3d908ac568e0beb562
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 19:47:50 +0000

    chrome/installer: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I8c7c7d3812ad8528193087832161e348af9e8797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392627
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959656}

--- chrome/installer/setup/memory_unittest.cc
+++ chrome/installer/setup/memory_unittest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if defined(OS_MAC)
+#if BUILDFLAG(IS_MAC)
 #include "base/allocator/allocator_interception_mac.h"
 #endif
 
@@ -39,7 +39,7 @@ TEST(OutOfMemoryHandledTest, UncheckedMalloc) {
   EXPECT_FALSE(base::UncheckedMalloc(kUnsafeMallocSize, &value));
   EXPECT_EQ(nullptr, value);
 
-#if defined(OS_MAC)
+#if BUILDFLAG(IS_MAC)
   base::allocator::UninterceptMallocZonesForTesting();
 #endif
 }
