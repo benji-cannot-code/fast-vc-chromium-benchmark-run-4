fad734bab9a9f3eeba1b06f01c341f4969122e65
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2022-01-15 00:26:53 +0000

    [BrowserSwitcher] Treat <open-in>None as greylist
    When the BrowserSwitcherParsingMode policy is set to IE-compatible,
    treat <open-in>None as greylist, rather than inverted sitelist entries.
    Same for doNotTransition="true" in v1 sitelists.
    
    Bug: 1282233
    Change-Id: I1194f8973777f1b455cd7befcfeb664dc37b8dc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3368841
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959417}

---
+++ chrome/browser/browser_switcher/browser_switcher_features.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+// Feature for session-only data deletion on startup.
+
+#include "chrome/browser/browser_switcher/browser_switcher_features.h"
+
+// A kill-switch for the <open-in>None behavior change made in M99, in
+// IeemSitelistParser. This makes it easy to revert the change if we break
+// a customer.
+//
+// TODO(crbug.com/1282233): Remove this flag once we're confident this
+// doesn't break customers.
+extern const base::Feature kBrowserSwitcherNoneIsGreylist{
+    "BrowserSwitcherNoneIsGreylist", base::FEATURE_DISABLED_BY_DEFAULT};
