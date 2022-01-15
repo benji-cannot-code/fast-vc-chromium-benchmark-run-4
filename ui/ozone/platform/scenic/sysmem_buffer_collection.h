aafd0a7444b096295c35504b43c83589b54166b5
Yilong Li <liyl@google.com>
2022-01-15 03:15:14 +0000

    [fuchsia] Use upstream FUCHSIA_buffer_collection extension.
    This migrates Chromium to use the upstream FUCHSIA buffer
    collection extension which is in the public Vulkan 1.2.194
    header. It removes the old vkSetBufferCollectionConstraints()
    and replaces it with vkSetBufferCollectionImageConstraints().
    This change also adds logic to generate the image constraints
    structs.
    
    Bug: chromium:1285429
    Change-Id: I11ad7cde1bc1f035c25ce08e2fa7e2fd7c3153d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3390511
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Yilong Li <liyl@google.com>
    Cr-Commit-Position: refs/heads/main@{#959514}

--- ui/ozone/platform/scenic/sysmem_buffer_collection.h
+++ ui/ozone/platform/scenic/sysmem_buffer_collection.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread_checker.h"
 #include "gpu/ipc/common/vulkan_ycbcr_info.h"
-#include "gpu/vulkan/fuchsia/vulkan_fuchsia_ext.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/geometry/size.h"
@@ -129,7 +128,7 @@ class SysmemBufferCollection
 
   // Handle for the Vulkan object that holds the same logical buffer collection
   // that is referenced by |collection_|.
-  VkBufferCollectionFUCHSIAX vk_buffer_collection_ = VK_NULL_HANDLE;
+  VkBufferCollectionFUCHSIA vk_buffer_collection_ = VK_NULL_HANDLE;
 
   // |scenic_overlay_view_| view should be used and deleted on the same thread
   // as creation.
