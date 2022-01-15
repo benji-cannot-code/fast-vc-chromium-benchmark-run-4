66d44954fdcd147056cc14a54c368a467e2d5baf
Yury Semikhatsky <yurys@chromium.org>
2022-01-15 03:28:34 +0000

    DevTools: accept per script settings in Page.setFontFamilies
    It is possible to specify script specific default font preferences
    such as kWebKitFixedFontFamilyJapanese. This change extends setFontFamilies
    command to allow overriding default fonts for certain script codes.
    
    Bug: None
    Change-Id: I02f53e744c7b42102ad2caf64887261c7588fed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388550
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959521}

--- third_party/blink/renderer/core/inspector/inspector_page_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_page_agent.h
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class GenericFontFamilySettings;
+
 namespace probe {
 class RecalculateStyle;
 class UpdateLayout;
@@ -171,7 +173,9 @@ class CORE_EXPORT InspectorPageAgent final
                                          Maybe<bool> grant_universal_access,
                                          int* execution_context_id) override;
   protocol::Response setFontFamilies(
-      std::unique_ptr<protocol::Page::FontFamilies>) override;
+      std::unique_ptr<protocol::Page::FontFamilies>,
+      Maybe<protocol::Array<protocol::Page::ScriptFontFamilies>> forScripts)
+      override;
   protocol::Response setFontSizes(
       std::unique_ptr<protocol::Page::FontSizes>) override;
   protocol::Response generateTestReport(const String& message,
@@ -260,6 +264,9 @@ class CORE_EXPORT InspectorPageAgent final
 
   void PageLayoutInvalidated(bool resized);
 
+  protocol::Response setFontFamilies(
+      GenericFontFamilySettings& family_settings,
+      const protocol::Array<protocol::Page::ScriptFontFamilies>& forScripts);
   std::unique_ptr<protocol::Page::Frame> BuildObjectForFrame(LocalFrame*);
   std::unique_ptr<protocol::Page::FrameTree> BuildObjectForFrameTree(
       LocalFrame*);
@@ -288,14 +295,9 @@ class CORE_EXPORT InspectorPageAgent final
   InspectorAgentState::StringMap worlds_to_evaluate_on_load_;
   InspectorAgentState::BooleanMap
       include_command_line_api_for_scripts_to_evaluate_on_load_;
-  InspectorAgentState::String standard_font_family_;
-  InspectorAgentState::String fixed_font_family_;
-  InspectorAgentState::String serif_font_family_;
-  InspectorAgentState::String sans_serif_font_family_;
-  InspectorAgentState::String cursive_font_family_;
-  InspectorAgentState::String fantasy_font_family_;
   InspectorAgentState::Integer standard_font_size_;
   InspectorAgentState::Integer fixed_font_size_;
+  InspectorAgentState::Bytes script_font_families_cbor_;
 };
 
 }  // namespace blink
