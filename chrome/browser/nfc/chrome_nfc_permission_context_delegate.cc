5eec0e94980f25c3baa5bd3135843e74bb2192da
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 23:43:54 +0000

    chrome/browser/nfc: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Ie4c7a9a62af3e6bb5bf524dab66cb28e2090a136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392775
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959741}

--- chrome/browser/nfc/chrome_nfc_permission_context_delegate.cc
+++ chrome/browser/nfc/chrome_nfc_permission_context_delegate.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/nfc/chrome_nfc_permission_context_delegate.h"
 
-#if defined(OS_ANDROID)
+#include "build/build_config.h"
+
+#if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/android/tab_android.h"
 #endif
 
@@ -15,7 +17,7 @@ ChromeNfcPermissionContextDelegate::ChromeNfcPermissionContextDelegate() =
 ChromeNfcPermissionContextDelegate::~ChromeNfcPermissionContextDelegate() =
     default;
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 bool ChromeNfcPermissionContextDelegate::IsInteractable(
     content::WebContents* web_contents) {
   TabAndroid* tab = TabAndroid::FromWebContents(web_contents);
