afa7865ede26b79b2fe6e97682f25af641d6b333
Yury Semikhatsky <yurys@chromium.org>
2022-01-15 01:57:54 +0000

    DevTools: preserve filechooser interception state on cross-process navigation
    Bug: None
    Change-Id: Idc2a8d0439746169a620a5d9ea615e74b67c835b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3390638
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959475}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/intercept-file-chooser-after-navigation.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests that Page.setInterceptFileChooserDialog state is preserved after cross-process navigation`);
+
+  await dp.Page.enable();
+  await dp.Runtime.enable();
+
+  await page.navigate('http://localhost:8000/inspector-protocol/resources/empty.html');
+  await dp.Page.setInterceptFileChooserDialog({enabled: true});
+  testRunner.log('Navigating cross-process');
+  await page.navigate('http://127.0.0.1:8000/inspector-protocol/resources/empty.html');
+
+  testRunner.log('Did navigate');
+
+  const [event] = await Promise.all([
+    dp.Page.onceFileChooserOpened(),
+    session.evaluateAsyncWithUserGesture(async () => {
+      const picker = document.createElement('input');
+      picker.type = 'file';
+      picker.click();
+    })
+  ]);
+  testRunner.log('Intercepted file chooser in mode: ' + event.params.mode);
+  
+  testRunner.completeTest();
+})
