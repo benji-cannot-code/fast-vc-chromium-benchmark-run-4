574ebaed58dee8231f4c00019e66918f6c0f26ae
chinsenj <chinsenj@chromium.org>
2022-01-15 00:09:52 +0000

    bento: Add metrics for improved desks keyboard shortcuts feature
    This CL adds metrics for the new improved desks keyboard shortcuts
    feature. It also adds some unit tests for them and the recording of
    their metrics.
    
    Test: added
    Bug: 1260485
    Change-Id: I25cc17f8e69a2dcae4eeea5713ab5aa2382e5b95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342667
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Jeremy Chinsen <chinsenj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959405}

---
+++ chromeos/ui/wm/desks/chromeos_desks_histogram_enums.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_UI_WM_DESKS_CHROMEOS_DESKS_HISTOGRAM_ENUMS_H_
+#define CHROMEOS_UI_WM_DESKS_CHROMEOS_DESKS_HISTOGRAM_ENUMS_H_
+
+namespace chromeos {
+
+// These values are logged to UMA. Entries should not be renumbered and
+// numeric values should never be reused. Please keep in sync with
+// DesksAssignToAllDesksSource in src/tools/metrics/histograms/enums.xml.
+enum class DesksAssignToAllDesksSource {
+  kMoveToDeskMenu = 0,
+  kKeyboardShortcut = 1,
+  kMaxValue = kKeyboardShortcut,
+};
+
+static constexpr char kDesksAssignToAllDesksSourceHistogramName[] =
+    "Ash.Desks.AssignToAllDesksSource";
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_UI_WM_DESKS_CHROMEOS_DESKS_HISTOGRAM_ENUMS_H_
