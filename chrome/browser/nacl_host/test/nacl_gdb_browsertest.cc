5bb9b44b7cc47e985619f603e847f1f27e4602d0
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 23:35:24 +0000

    chrome/browser/nacl_host: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I3dc5341eac4bd5691a95df615f8622449d63d334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392771
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959735}

--- chrome/browser/nacl_host/test/nacl_gdb_browsertest.cc
+++ chrome/browser/nacl_host/test/nacl_gdb_browsertest.cc
@@ -16,12 +16,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/nacl/common/nacl_switches.h"
 #include "content/public/test/browser_test.h"
 
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
 #include "base/win/windows_version.h"
 #endif
 
 static const base::FilePath::CharType kMockNaClGdb[] =
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
     FILE_PATH_LITERAL("mock_nacl_gdb.exe");
 #else
     FILE_PATH_LITERAL("mock_nacl_gdb");
@@ -53,7 +53,7 @@ class NaClGdbTest : public PPAPINaClNewlibTest {
     // base::LaunchProcess doesn't support creating suspended processes. We need
     // to either add suspended process support to base::LaunchProcess or use
     // Win API.
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
     if (base::win::OSInfo::GetInstance()->IsWowDisabled()) {
       return;
     }
@@ -80,7 +80,7 @@ class NaClGdbTest : public PPAPINaClNewlibTest {
 
 // Fails on the ASAN test bot. See http://crbug.com/122219
 // Flaky on Linux and CrOS test bots. See http://crbug.com/1126321
-#if defined(ADDRESS_SANITIZER) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+#if defined(ADDRESS_SANITIZER) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_Empty DISABLED_Empty
 #else
 #define MAYBE_Empty Empty
