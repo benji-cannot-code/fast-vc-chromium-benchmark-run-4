abe7721aafcfa917275ba9a61c88286c2d4a33c2
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 20:03:10 +0000

    chrome/child: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I4de8465b73e223695d595981424dbc18260b4317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392623
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959663}

--- chrome/child/pdf_child_init.cc
+++ chrome/child/pdf_child_init.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
 #include "base/command_line.h"
 #include "base/no_destructor.h"
 #include "base/win/current_module.h"
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
 typedef decltype(::GetFontData)* GetFontDataPtr;
 GetFontDataPtr g_original_get_font_data = nullptr;
 
@@ -47,12 +47,12 @@ DWORD WINAPI GetFontDataPatch(HDC hdc,
   }
   return rv;
 }
-#endif  // defined(OS_WIN)
+#endif  // BUILDFLAG(IS_WIN)
 
 }  // namespace
 
 void MaybePatchGdiGetFontData() {
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
   // Only patch utility processes which explicitly need GDI.
   auto& command_line = *base::CommandLine::ForCurrentProcess();
   auto service_sandbox_type =
@@ -81,5 +81,5 @@ void MaybePatchGdiGetFontData() {
       reinterpret_cast<void*>(GetFontDataPatch));
   g_original_get_font_data = reinterpret_cast<GetFontDataPtr>(
       patch_get_font_data->original_function());
-#endif  // defined(OS_WIN)
+#endif  // BUILDFLAG(IS_WIN)
 }
