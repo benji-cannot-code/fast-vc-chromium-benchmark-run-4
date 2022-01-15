29b39a46afa59ac87bc37b504d13929e809f9c96
Xiaohan Wang <xhwang@chromium.org>
2022-01-15 14:43:11 +0000

    content/browser/net: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Ie0aaa944f64846fa2cd1352a18b957f3e788977f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3391455
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959586}

--- content/browser/net/trust_token_browsertest.cc
+++ content/browser/net/trust_token_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_restrictions.h"
+#include "build/build_config.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/test/browser_test.h"
@@ -1698,7 +1699,7 @@ class TrustTokenBrowsertestWithPlatformIssuance : public TrustTokenBrowsertest {
   base::test::ScopedFeatureList features_;
 };
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 HandlerWrappingLocalTrustTokenFulfiller::
     HandlerWrappingLocalTrustTokenFulfiller(TrustTokenRequestHandler& handler)
     : handler_(handler) {
@@ -1831,8 +1832,8 @@ IN_PROC_BROWSER_TEST_F(TrustTokenBrowsertestWithPlatformIssuance,
                     "PlatformProvided"}),
       1);
 }
-#endif  // defined(OS_ANDROID)
-#if !defined(OS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID)
+#if !BUILDFLAG(IS_ANDROID)
 IN_PROC_BROWSER_TEST_F(
     TrustTokenBrowsertestWithPlatformIssuance,
     IssuanceOnOsNotSpecifiedInKeyCommitmentsReturnsErrorIfConfiguredToDoSo) {
@@ -1923,6 +1924,6 @@ IN_PROC_BROWSER_TEST_F(
                     "PlatformProvided"}),
       0);  // No platform-provided operation was attempted.
 }
-#endif  // !defined(OS_ANDROID)
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace content
--- content/browser/net/trust_token_browsertest.h
+++ content/browser/net/trust_token_browsertest.h
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test.h"
 #include "services/network/trust_tokens/test/trust_token_request_handler.h"
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 #include "content/public/browser/android/java_interfaces.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
-#endif  // defined(OS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID)
 
 namespace content {
 
@@ -67,7 +67,7 @@ class TrustTokenBrowsertest : virtual public ContentBrowserTest {
   net::EmbeddedTestServer server_{net::EmbeddedTestServer::TYPE_HTTPS};
 };
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 // HandlerWrappingLocalTrustTokenFulfiller serves two purposes:
 //
 // 1. Its lifetime scopes an override to content::GetGlobalJavaInterfaces()'s
