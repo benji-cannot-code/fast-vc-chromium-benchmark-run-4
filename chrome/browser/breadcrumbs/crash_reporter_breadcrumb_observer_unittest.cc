dbe964eeccee0bd9612104c80a6397aeebb69860
Xiaohan Wang <xhwang@chromium.org>
2022-01-16 04:30:12 +0000

    chrome/browser/breadcrumbs: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I3cecbd4580e9c46a1fbe7d329b218f57c4969d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392675
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959831}

--- chrome/browser/breadcrumbs/crash_reporter_breadcrumb_observer_unittest.cc
+++ chrome/browser/breadcrumbs/crash_reporter_breadcrumb_observer_unittest.cc
@@ -99,7 +99,7 @@ TEST_F(CrashReporterBreadcrumbObserverTest, EventsAttachedToCrashReport) {
 }
 
 // TODO(crbug.com/1255177): re-enable the test once this Breakpad bug is fixed.
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_ProductDataOverflow DISABLED_ProductDataOverflow
 #else
 #define MAYBE_ProductDataOverflow ProductDataOverflow
@@ -122,7 +122,7 @@ TEST_F(CrashReporterBreadcrumbObserverTest, MAYBE_ProductDataOverflow) {
 
   // Confirm that the total length of the breadcrumbs crash string is
   // |breadcrumbs::kMaxDataLength|.
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
   // Linux uses Breakpad, which breaks the crash key value up into chunks of 127
   // characters each, named <crash key>__1, <crash key>__2, etc. These must be
   // summed to determine the total length of the breadcrumbs crash string.
@@ -143,7 +143,7 @@ TEST_F(CrashReporterBreadcrumbObserverTest, MAYBE_ProductDataOverflow) {
 #else
   EXPECT_EQ(breadcrumbs::kMaxDataLength,
             GetBreadcrumbsCrashKeyValue().length());
-#endif  // defined(OS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 }
 
 // Tests that breadcrumb events logged to multiple BreadcrumbManagerKeyedService
