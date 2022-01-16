7efe769f3f64a6e8d396a0f03e0496bcfbcf89cf
Xiaohan Wang <xhwang@chromium.org>
2022-01-16 00:44:11 +0000

    chrome/browser/speech: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I95732c0eb8ca4e6227eb64d2c3ce6c8c57515c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392901
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959772}

--- chrome/browser/speech/speech_recognition_service_browsertest.cc
+++ chrome/browser/speech/speech_recognition_service_browsertest.cc
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/audio/public/cpp/fake_stream_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
 #include <windows.h>
 #else
 #include <unistd.h>
@@ -150,7 +150,7 @@ class SpeechRecognitionServiceTest
 
   // Disable the sandbox on Windows and MacOS as the sandboxes on those
   // platforms have not been configured yet.
-#if defined(OS_WIN) || defined(OS_MAC)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   void SetUpCommandLine(base::CommandLine* command_line) override {
     // Required for the utility process to access the directory containing the
     // test files.
@@ -218,7 +218,7 @@ void SpeechRecognitionServiceTest::OnLanguageIdentificationEvent(
 
 void SpeechRecognitionServiceTest::SetUpPrefs() {
   base::FilePath soda_binary_path;
-#if defined(OS_WIN) || defined(OS_MAC)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   soda_binary_path =
       test_data_dir_.Append(base::FilePath(soda::kSodaResourcePath))
           .Append(soda::kSodaTestBinaryRelativePath);
@@ -334,7 +334,7 @@ void SpeechRecognitionServiceTest::SendAudioChunk(
 
     // Sleep for 20ms to simulate real-time audio. SODA requires audio
     // streaming in order to return events.
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
     ::Sleep(20);
 #else
     usleep(20000);
@@ -381,7 +381,7 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest, RecognizePhrase) {
   base::RunLoop().RunUntilIdle();
 
   // Sleep for 50ms to ensure SODA has returned real-time results.
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
   ::Sleep(50);
 #else
   usleep(50000);
@@ -455,7 +455,7 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest,
   base::RunLoop().RunUntilIdle();
 
   // Sleep for 50ms to ensure SODA has returned real-time results.
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
   ::Sleep(50);
 #else
   usleep(50000);
@@ -479,7 +479,7 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest, CreateAudioSourceFetcher) {
 
   // TODO(crbug.com/1185978): Check implementation / sandbox policy on Mac and
   // Windows.
-#if defined(OS_CHROMEOS) || defined(OS_LINUX)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
   // Check that Start begins audio recording.
   // TODO(crbug.com/1173135): Try to mock audio input, maybe with
   // TestStreamFactory::stream_, to test end-to-end.
