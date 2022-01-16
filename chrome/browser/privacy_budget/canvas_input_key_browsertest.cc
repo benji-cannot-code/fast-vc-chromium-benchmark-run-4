23f0d96538bfa73184e636ec672dc8b131b71cc3
Xiaohan Wang <xhwang@chromium.org>
2022-01-16 00:26:11 +0000

    chrome/browser/privacy_budget: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Ifb9f8897139f6a833a50559a2c1b037c6eae4dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392845
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959761}

--- chrome/browser/privacy_budget/canvas_input_key_browsertest.cc
+++ chrome/browser/privacy_budget/canvas_input_key_browsertest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/privacy_budget/identifiable_surface.h"
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 #include "chrome/test/base/android/android_browser_test.h"
 #else
 #include "chrome/test/base/in_process_browser_test.h"
