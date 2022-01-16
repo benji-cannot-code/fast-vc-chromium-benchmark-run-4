54487e3978690a443292191a529a07272ae51d2f
Maxim Kolosovskiy <kolos@chromium.org>
2022-01-16 19:43:09 +0000

    [Passwords] Keyed service for password change success metric
    This CL adds a stub keyed service and its factory to track password
    changes.
    
    DD go/password-change-e2e-metric
    
    Related changes are in progress in crrev.com/c/3351791 and other CLs.
    
    Bug: 1281844
    Change-Id: Idc00f7f89eaed8bcffb29e4eadc53adc674883a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380489
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959878}

---
+++ components/password_manager/core/browser/password_change_success_tracker_impl.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/password_change_success_tracker_impl.h"
+
+#include "url/gurl.h"
+
+namespace password_manager {
+
+PasswordChangeSuccessTrackerImpl::PasswordChangeSuccessTrackerImpl() = default;
+
+PasswordChangeSuccessTrackerImpl::~PasswordChangeSuccessTrackerImpl() = default;
+
+void PasswordChangeSuccessTrackerImpl::OnChangePasswordFlowStarted(
+    const GURL& url,
+    const std::string& username,
+    StartEvent event_type) {
+  // TODO(crbug.com/1281844): Implement metrics recoding.
+}
+
+void PasswordChangeSuccessTrackerImpl::OnChangePasswordFlowCompleted(
+    const GURL& url,
+    const std::string& username,
+    EndEvent event_type) {
+  // TODO(crbug.com/1281844): Implement metrics recoding.
+}
+
+}  // namespace password_manager
---
+++ components/password_manager/core/browser/password_change_success_tracker_impl.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_CHANGE_SUCCESS_TRACKER_IMPL_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_CHANGE_SUCCESS_TRACKER_IMPL_H_
+
+#include "components/password_manager/core/browser/password_change_success_tracker.h"
+
+class GURL;
+
+namespace password_manager {
+
+class PasswordChangeSuccessTrackerImpl
+    : public password_manager::PasswordChangeSuccessTracker {
+ public:
+  PasswordChangeSuccessTrackerImpl();
+
+  ~PasswordChangeSuccessTrackerImpl() override;
+
+  void OnChangePasswordFlowStarted(const GURL& url,
+                                   const std::string& username,
+                                   StartEvent event_type) override;
+
+  void OnChangePasswordFlowCompleted(const GURL& url,
+                                     const std::string& username,
+                                     EndEvent event_type) override;
+};
+
+}  // namespace password_manager
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_CHANGE_SUCCESS_TRACKER_IMPL_H_
