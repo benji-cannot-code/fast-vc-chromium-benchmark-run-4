e771531d008d7f7491be2513152e899b92c667ef
Xiaohan Wang <xhwang@chromium.org>
2022-01-16 00:06:13 +0000

    chrome/browser/plugins: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Ie3b1f0e6c0876397b912f9dd821720af3daf64d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392815
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959755}

--- chrome/browser/plugins/plugin_infobar_delegates.cc
+++ chrome/browser/plugins/plugin_infobar_delegates.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
 
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
 #include <windows.h>
 
 #include <shellapi.h>
--- chrome/browser/plugins/plugin_observer.cc
+++ chrome/browser/plugins/plugin_observer.cc
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/buildflags/buildflags.h"
 #include "ui/base/l10n/l10n_util.h"
 
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
 #include "base/win/windows_types.h"
 #endif
 
@@ -100,7 +100,7 @@ void PluginObserver::PluginCrashed(const base::FilePath& plugin_path,
   std::u16string plugin_name =
       PluginService::GetInstance()->GetPluginDisplayNameByPath(plugin_path);
   std::u16string infobar_text;
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
   // Find out whether the plugin process is still alive.
   // Note: Although the chances are slim, it is possible that after the plugin
   // process died, |plugin_pid| has been reused by a new process. The
