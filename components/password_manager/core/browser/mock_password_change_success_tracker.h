3cbe10bf2b229e70f628c092f513f6e110078fb3
Maxim Kolosovskiy <kolos@chromium.org>
2022-01-16 20:16:07 +0000

    [Passwords] Implement MockPasswordChangeSuccessTracker
    A mock for PasswordChangeSuccessTracker is used by two CLs that are
    currently in progress: crrev.com/c/3379988 and crrev.com/c/3380669.
    The common part is extracted into this CL.
    
    Bug: 1281844
    Change-Id: Ia652c4dc3c668a86363707eec73ea6f744cd1cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387331
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959884}

---
+++ components/password_manager/core/browser/mock_password_change_success_tracker.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_MOCK_PASSWORD_CHANGE_SUCCESS_TRACKER_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_MOCK_PASSWORD_CHANGE_SUCCESS_TRACKER_H_
+
+#include "components/password_manager/core/browser/password_change_success_tracker.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace password_manager {
+
+// Mocked PasswordChangeSuccessTracker used by unit tests.
+class MockPasswordChangeSuccessTracker : public PasswordChangeSuccessTracker {
+ public:
+  MockPasswordChangeSuccessTracker();
+  ~MockPasswordChangeSuccessTracker() override;
+
+  MOCK_METHOD3(OnChangePasswordFlowStarted,
+               void(const GURL& url,
+                    const std::string& username,
+                    StartEvent event_type));
+  MOCK_METHOD3(OnChangePasswordFlowCompleted,
+               void(const GURL& url,
+                    const std::string& username,
+                    EndEvent event_type));
+};
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_MOCK_PASSWORD_CHANGE_SUCCESS_TRACKER_H_
