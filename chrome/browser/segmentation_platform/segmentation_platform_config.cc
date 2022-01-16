7caa8d9af582825fcee6b5a9fd5e48fba6c8f358
Xiaohan Wang <xhwang@chromium.org>
2022-01-16 01:29:43 +0000

    chrome/browser/segmentation_platform: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: Iffef43eb1a82004e872eba3f544e02db10ebfb5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392652
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959788}

--- chrome/browser/segmentation_platform/segmentation_platform_config.cc
+++ chrome/browser/segmentation_platform/segmentation_platform_config.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/public/config.h"
 #include "components/segmentation_platform/public/features.h"
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/feature_guide/notifications/feature_notification_guide_service.h"
 #include "chrome/browser/flags/android/cached_feature_flags.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
@@ -32,7 +32,7 @@ namespace {
 
 constexpr int kDummyFeatureSelectionTTLDays = 1;
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 
 constexpr int kAdaptiveToolbarDefaultSelectionTTLDays = 28;
 
@@ -52,9 +52,9 @@ const char kNumDaysMVCkicksBelowThreshold[] =
 constexpr int kQueryTilesDefaultSelectionTTLDays = 28;
 // DEFAULT_NUM_DAYS_MV_CLICKS_BELOW_THRESHOLD
 constexpr int kQueryTilesDefaultUnknownTTLDays = 7;
-#endif  // defined(OS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID)
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 std::unique_ptr<Config> GetConfigForAdaptiveToolbar() {
   auto config = std::make_unique<Config>();
   config->segmentation_key = kAdaptiveToolbarSegmentationKey;
@@ -87,7 +87,7 @@ std::unique_ptr<Config> GetConfigForDummyFeature() {
   return config;
 }
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
 std::unique_ptr<Config> GetConfigForChromeStartAndroid() {
   auto config = std::make_unique<Config>();
   config->segmentation_key = kChromeStartAndroidSegmentationKey;
@@ -141,7 +141,7 @@ std::unique_ptr<Config> GetConfigForChromeLowUserEngagement() {
   return config;
 }
 
-#endif  // defined(OS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID)
 
 }  // namespace
 
@@ -151,7 +151,7 @@ std::vector<std::unique_ptr<Config>> GetSegmentationPlatformConfig() {
           segmentation_platform::features::kSegmentationPlatformDummyFeature)) {
     configs.emplace_back(GetConfigForDummyFeature());
   }
-#if defined(OS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
   if (base::FeatureList::IsEnabled(
           chrome::android::kAdaptiveButtonInTopToolbarCustomizationV2)) {
     configs.emplace_back(GetConfigForAdaptiveToolbar());
