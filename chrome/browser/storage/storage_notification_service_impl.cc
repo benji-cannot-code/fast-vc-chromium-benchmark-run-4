e22be62037810b1ac7f1a99f76d91be5458b0c15
Xiaohan Wang <xhwang@chromium.org>
2022-01-16 01:35:57 +0000

    chrome/browser/storage: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983).
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Test: No functionality change
    Change-Id: I615159e1153f020f3de24f4e72c3c0c10a37084f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3392905
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#959795}

--- chrome/browser/storage/storage_notification_service_impl.cc
+++ chrome/browser/storage/storage_notification_service_impl.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 
-#if !defined(OS_ANDROID)
+#if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/storage_pressure_bubble.h"
 #endif
 
