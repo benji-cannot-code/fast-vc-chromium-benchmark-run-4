eb2d3294e9bd881ff34d16d3fbafd43492ac0ce3
Greg Thompson <grt@chromium.org>
2022-01-17 17:31:33 +0000

    [fuchsia] More robust use of ffx for CFv2-based tests.
    Among the improvements:
    
    - The --target switch is now used to specify the target of ffx
      invocations rather than trying to set the default target (which could
      interfere with developer workflows).
    - Use of ffx should now work when using a DeviceTarget (i.e.,
      "-d"). Previously, only emulator targets worked.
    - `ffx target wait` is now used to wait for ffx to establish a
      connection with the target.
    - When -v is used to get verbose logging from the various scripts,
      better ffx-related diagnostics are emitted to stdout.
    - When --logs-dir is specified to persist operational logs, detailed
      logging of ffx commands are deposited into the file `ffx_log`.
    - When --logs-dir is specified, all output from the device pulled by
      `ffx` is left in the `test_outputs` subdir. This includes useful
      things like system logs, stdout/stderr, etc.
    - Any changes made to ffx's user configuration are reverted on exit.
    
    This is a reland of https://crrev.com/959143.
    
    (cherry picked from commit 90d96ddd9bb33af0856ccd50cebeded3dd0bc2ce)
    
    Bug: 1256502
    Change-Id: I9c096fff5c49dfbb22f677b3b63b3947a2df2615
    Cq-Include-Trybots: luci.chrome.try:fuchsia-fyi-astro
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3394746
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Benjamin Lerman <qsr@chromium.org>
    Commit-Queue: Benjamin Lerman <qsr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960128}

--- build/fuchsia/generic_x64_target.py
+++ build/fuchsia/generic_x64_target.py
@@ -52,7 +52,7 @@ class GenericX64PavedDeviceTarget(device_target.DeviceTarget):
     should_provision = False
 
     if self._Discover():
-      self._WaitUntilReady()
+      self._ConnectToTarget()
 
       if self._os_check != 'ignore':
         if self._SDKHashMatches():
