f982e76a8c2a16e977f7976ba4172e457788c040
Stefano Duo <stefanoduo@google.com>
2022-01-17 13:39:28 +0000

    [Cronet] Add HangingRequestURL to Http2TestServer
    Add an URL to the test server which will cause the request to hang
    until the supplied latch is opened. This will be later used to test the
    detect broken connection experimental feature.
    
    The hanging URL handler uses a CountDownLatch that must be supplied by
    the test at startHttp2TestServer call time. It defaults to null to avoid
    having to always provide it (using getHangingRequestUrl without
    providing it will result in a NullPointerException, which should be
    acceptable since this is only used for testing).
    
    Bug: 1183119, b:189096305
    Change-Id: Ic696654583e20db42d723f766ac93dd9f0a2122a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3326701
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#960038}

--- components/cronet/android/test/src/org/chromium/net/Http2TestHandler.java
+++ components/cronet/android/test/src/org/chromium/net/Http2TestHandler.java
@@ -5,6 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net;
 
+import static io.netty.buffer.Unpooled.copiedBuffer;
+import static io.netty.buffer.Unpooled.unreleasableBuffer;
+import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.logging.LogLevel.INFO;
+
 import org.chromium.base.Log;
 
 import java.io.ByteArrayOutputStream;
@@ -13,12 +19,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-
-import static io.netty.buffer.Unpooled.copiedBuffer;
-import static io.netty.buffer.Unpooled.unreleasableBuffer;
-import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
-import static io.netty.handler.codec.http.HttpResponseStatus.OK;
-import static io.netty.handler.logging.LogLevel.INFO;
+import java.util.concurrent.CountDownLatch;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
@@ -50,6 +51,7 @@ public final class Http2TestHandler extends Http2ConnectionHandler implements Ht
     public static final String REPORTING_COLLECTOR_PATH = "/reporting-collector";
     public static final String SUCCESS_WITH_NEL_HEADERS_PATH = "/success-with-nel";
     public static final String COMBINED_HEADERS_PATH = "/combinedheaders";
+    public static final String HANGING_REQUEST_PATH = "/hanging-request";
 
     private static final String TAG = Http2TestHandler.class.getSimpleName();
     private static final Http2FrameLogger sLogger =
@@ -61,6 +63,7 @@ public final class Http2TestHandler extends Http2ConnectionHandler implements Ht
 
     private ReportingCollector mReportingCollector;
     private String mServerUrl;
+    private CountDownLatch mHangingUrlLatch;
 
     /**
      * Builder for HTTP/2 test handler.
@@ -81,6 +84,11 @@ public final class Http2TestHandler extends Http2ConnectionHandler implements Ht
             return this;
         }
 
+        public Builder setHangingUrlLatch(CountDownLatch hangingUrlLatch) {
+            mHangingUrlLatch = hangingUrlLatch;
+            return this;
+        }
+
         @Override
         public Http2TestHandler build() {
             return super.build();
@@ -89,14 +97,15 @@ public final class Http2TestHandler extends Http2ConnectionHandler implements Ht
         @Override
         protected Http2TestHandler build(Http2ConnectionDecoder decoder,
                 Http2ConnectionEncoder encoder, Http2Settings initialSettings) {
-            Http2TestHandler handler = new Http2TestHandler(
-                    decoder, encoder, initialSettings, mReportingCollector, mServerUrl);
+            Http2TestHandler handler = new Http2TestHandler(decoder, encoder, initialSettings,
+                    mReportingCollector, mServerUrl, mHangingUrlLatch);
             frameListener(handler);
             return handler;
         }
 
         private ReportingCollector mReportingCollector;
         private String mServerUrl;
+        private CountDownLatch mHangingUrlLatch;
     }
 
     private class RequestResponder {
@@ -158,6 +167,17 @@ public final class Http2TestHandler extends Http2ConnectionHandler implements Ht
             encoder().writeHeaders(ctx, streamId, responseHeaders, 0, false, ctx.newPromise());
             encoder().writeData(ctx, streamId, content, 0, true, ctx.newPromise());
             ctx.flush();
+        }
+    }
+
+    private class HangingRequestResponder extends RequestResponder {
+        @Override
+        void onHeadersRead(ChannelHandlerContext ctx, int streamId, boolean endOfStream,
+                Http2Headers headers) {
+            try {
+                mHangingUrlLatch.await();
+            } catch (InterruptedException e) {
+            }
         }
     }
 
@@ -323,11 +343,12 @@ public final class Http2TestHandler extends Http2ConnectionHandler implements Ht
     }
 
     private Http2TestHandler(Http2ConnectionDecoder decoder, Http2ConnectionEncoder encoder,
-            Http2Settings initialSettings, ReportingCollector reportingCollector,
-            String serverUrl) {
+            Http2Settings initialSettings, ReportingCollector reportingCollector, String serverUrl,
+            CountDownLatch hangingUrlLatch) {
         super(decoder, encoder, initialSettings);
         mReportingCollector = reportingCollector;
         mServerUrl = serverUrl;
+        mHangingUrlLatch = hangingUrlLatch;
     }
 
     @Override
@@ -371,6 +392,8 @@ public final class Http2TestHandler extends Http2ConnectionHandler implements Ht
             responder = new SuccessWithNELHeadersResponder();
         } else if (path.startsWith(COMBINED_HEADERS_PATH)) {
             responder = new CombinedHeadersResponder();
+        } else if (path.startsWith(HANGING_REQUEST_PATH)) {
+            responder = new HangingRequestResponder();
         } else {
             responder = new RequestResponder();
         }
