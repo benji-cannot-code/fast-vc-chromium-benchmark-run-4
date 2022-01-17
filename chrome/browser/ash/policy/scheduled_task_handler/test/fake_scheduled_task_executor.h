d825e02f90050bfc287569a85ba2724ccbe57406
Patrick Monette <pmonette@chromium.org>
2022-01-17 14:32:19 +0000

    Remove usage of PostCancelableDelayedTask in FakeScheduledTaskExecutor
    PostCancelableDelayedTask() is a private API of SequencedTaskRunner and
    should not be used outside of timer implementations.
    
    Change-Id: I1a77372d1f24788f1ecb3c4275f51fdcc5dc6953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388512
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960056}

--- chrome/browser/ash/policy/scheduled_task_handler/test/fake_scheduled_task_executor.h
+++ chrome/browser/ash/policy/scheduled_task_handler/test/fake_scheduled_task_executor.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_POLICY_SCHEDULED_TASK_HANDLER_TEST_FAKE_SCHEDULED_TASK_EXECUTOR_H_
 #define CHROME_BROWSER_ASH_POLICY_SCHEDULED_TASK_HANDLER_TEST_FAKE_SCHEDULED_TASK_EXECUTOR_H_
 
-#include "base/task/delayed_task_handle.h"
 #include "base/time/clock.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/ash/policy/scheduled_task_handler/scheduled_task_executor.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
 
@@ -49,7 +49,7 @@ class FakeScheduledTaskExecutor : public ScheduledTaskExecutor {
   bool simulate_calculate_next_update_check_failure_ = false;
 
   // Timer that is scheduled to execute the task.
-  base::DelayedTaskHandle delayed_task_handle_;
+  base::OneShotTimer timer_;
 };
 }  // namespace policy
 
