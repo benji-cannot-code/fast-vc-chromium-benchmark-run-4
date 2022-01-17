134b272158d68e085e13e9613613bd5f6a76a494
Alex Turner <alexmt@chromium.org>
2022-01-17 20:13:33 +0000

    Modernize {Dictionary,List}PrefUpdate in /components/metrics
    This cl replaces instances of DictionaryPrefUpdateDeprecated, which
    wraps the deprecated base::DictionaryValue, with DictionaryPrefUpdate,
    which wraps base::Value. This cl also does the equivalent for
    ListPrefUpdateDeprecated. A range of other base::Value modernization
    changes are made to allow these replacements and as drive-by fixes.
    This cl handles the directory /components/metrics
    
    This CL was uploaded by git cl split.
    
    R=bcwhite@chromium.org
    
    Bug: 1285745
    Change-Id: I0f9eab8b4467425a84173119dbc7e9e0618e1016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386090
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960172}

--- components/metrics/file_metrics_provider.cc
+++ components/metrics/file_metrics_provider.cc
@@ -684,8 +684,8 @@ bool FileMetricsProvider::ProvideIndependentMetricsOnTaskRunner(
 }
 
 void FileMetricsProvider::AppendToSamplesCountPref(size_t samples_count) {
-  ListPrefUpdateDeprecated update(pref_service_,
-                                  metrics::prefs::kMetricsFileMetricsMetadata);
+  ListPrefUpdate update(pref_service_,
+                        metrics::prefs::kMetricsFileMetricsMetadata);
   update->Append(static_cast<int>(samples_count));
 }
 
@@ -925,8 +925,8 @@ bool FileMetricsProvider::SimulateIndependentMetrics() {
     return false;
   }
 
-  ListPrefUpdateDeprecated list_value(
-      pref_service_, metrics::prefs::kMetricsFileMetricsMetadata);
+  ListPrefUpdate list_value(pref_service_,
+                            metrics::prefs::kMetricsFileMetricsMetadata);
   if (list_value->GetList().empty())
     return false;
 
