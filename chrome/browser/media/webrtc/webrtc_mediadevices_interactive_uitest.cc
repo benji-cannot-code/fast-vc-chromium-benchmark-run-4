2b4cd9f72eef166f837146020adc998d605ebb91
Fergal Daly <fergal@chromium.org>
2022-01-17 16:17:20 +0000

    Check the return value of AddTabAtIndex(ToBrowser) in tests.
    Final goal is to mark WaitForStopLoading as WARN_IF_UNUSED.
    
    This adds ASSERT_TRUE/FALSE to all calls such that tests continue to
    pass (except in non-void functions which must use EXPECT instead).
    
    This CL is a split of https://crrev.com/c/2852829.
    It covers /chrome/browser/media/webrtc
    
    I'm splitting into many to avoid one rollback undoing the whole thing
    as some of these may turn out to be flaky.
    
    There are 37 CLs covering 161 call-sites in total, I cannot
    investigate why individual cases are unexpectedly TRUE/FALSE. Unless
    you think it indicates a bug in the test infrastructure, please
    approve and investigate the unexpected cases of your directory in
    follow-on bugs/CLs.
    
    This CL was uploaded by git cl split.
    
    R=guidou@chromium.org
    
    Bug: 1014186,425335
    Change-Id: Ibd12710a786ed359c676026d1f4ef4ef61e5b2c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3394074
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960089}

--- chrome/browser/media/webrtc/webrtc_mediadevices_interactive_uitest.cc
+++ chrome/browser/media/webrtc/webrtc_mediadevices_interactive_uitest.cc
@@ -213,7 +213,8 @@ IN_PROC_BROWSER_TEST_F(WebRtcMediaDevicesInteractiveUITest,
   ASSERT_TRUE(embedded_test_server()->Start());
   GURL url(embedded_test_server()->GetURL(kMainWebrtcTestHtmlPage));
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
-  AddTabAtIndexToBrowser(browser(), 1, url, ui::PAGE_TRANSITION_LINK, true);
+  ASSERT_TRUE(AddTabAtIndexToBrowser(browser(), 1, url,
+                                     ui::PAGE_TRANSITION_LINK, true));
 
   content::WebContents* focused_tab =
       browser()->tab_strip_model()->GetWebContentsAt(1);
@@ -229,7 +230,8 @@ IN_PROC_BROWSER_TEST_F(WebRtcMediaDevicesInteractiveUITest,
   ASSERT_TRUE(embedded_test_server()->Start());
   GURL url(embedded_test_server()->GetURL(kMainWebrtcTestHtmlPage));
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
-  AddTabAtIndexToBrowser(browser(), 1, url, ui::PAGE_TRANSITION_LINK, true);
+  ASSERT_TRUE(AddTabAtIndexToBrowser(browser(), 1, url,
+                                     ui::PAGE_TRANSITION_LINK, true));
 
   content::WebContents* tab = browser()->tab_strip_model()->GetWebContentsAt(0);
   GetUserMediaReturnsFalseIfWaitIsTooLong(tab, kAudioVideoCallConstraints);
