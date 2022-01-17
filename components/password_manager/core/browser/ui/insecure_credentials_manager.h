fc20bb87421daa6d55ac2b4f53621d67fb052ebe
Elias Khsheibun <eliaskh@chromium.org>
2022-01-17 16:10:34 +0000

    Add MuteCredential functionality to insecure_credentials_manager
    Bug: 1178762
    Change-Id: I25498f73dfe8d913bb4c849d0942497204a519dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3382823
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Elias Khsheibun <eliaskh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960086}

--- components/password_manager/core/browser/ui/insecure_credentials_manager.h
+++ components/password_manager/core/browser/ui/insecure_credentials_manager.h
@@ -116,7 +116,6 @@ struct CredentialView {
 struct CredentialWithPassword : CredentialView {
   explicit CredentialWithPassword(const CredentialView& credential);
   explicit CredentialWithPassword(const InsecureCredential& credential);
-
   CredentialWithPassword(const CredentialWithPassword& other);
   CredentialWithPassword(CredentialWithPassword&& other);
   ~CredentialWithPassword();
@@ -126,6 +125,7 @@ struct CredentialWithPassword : CredentialView {
   base::Time create_time;
   InsecureCredentialTypeFlags insecure_type =
       InsecureCredentialTypeFlags::kSecure;
+  IsMuted is_muted{false};
 };
 
 // Comparator that can compare CredentialView or CredentialsWithPasswords.
@@ -140,10 +140,10 @@ struct PasswordCredentialLess {
 struct CredentialMetadata;
 
 // This class provides clients with saved insecure credentials and possibility
-// to save new LeakedCredentials, edit/delete insecure credentials and match
-// insecure credentials with corresponding autofill::PasswordForms. It supports
-// an observer interface, and clients can register themselves to get notified
-// about changes to the list.
+// to save new LeakedCredentials, edit/delete/mute insecure credentials and
+// match insecure credentials with corresponding autofill::PasswordForms. It
+// supports an observer interface, and clients can register themselves to get
+// notified about changes to the list.
 class InsecureCredentialsManager : public SavedPasswordsPresenter::Observer {
  public:
   using CredentialsView = base::span<const CredentialWithPassword>;
@@ -184,6 +184,10 @@ class InsecureCredentialsManager : public SavedPasswordsPresenter::Observer {
   // Attempts to remove |credential| from the password store. Returns whether
   // the remove succeeded.
   bool RemoveCredential(const CredentialView& credential);
+
+  // Attempts to mute |credential| from the password store.
+  // Returns whether the mute succeeded.
+  bool MuteCredential(const CredentialView& credential);
 
   // Returns a vector of currently insecure credentials.
   std::vector<CredentialWithPassword> GetInsecureCredentials() const;
