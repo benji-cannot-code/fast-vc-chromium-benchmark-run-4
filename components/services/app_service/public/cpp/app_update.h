2195214327b27c217614d1f87e58f2d959ad1f60
Nancy Wang <nancylingwang@chromium.org>
2022-01-18 07:56:35 +0000

    Add permission struct to the non-mojom app struct.
    BUG=1253250
    
    Change-Id: I6533283104a4a2fcbcbbac5246f41194b622ceb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3394768
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960278}

--- components/services/app_service/public/cpp/app_update.h
+++ components/services/app_service/public/cpp/app_update.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "components/account_id/account_id.h"
 #include "components/services/app_service/public/cpp/app_types.h"
+#include "components/services/app_service/public/cpp/permission.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -125,6 +126,7 @@ class COMPONENT_EXPORT(APP_UPDATE) AppUpdate {
   bool InstallTimeChanged() const;
 
   std::vector<apps::mojom::PermissionPtr> Permissions() const;
+  apps::Permissions GetPermissions() const;
   bool PermissionsChanged() const;
 
   apps::mojom::InstallReason InstallReason() const;
