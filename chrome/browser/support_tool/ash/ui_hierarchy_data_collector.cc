8e3d498344629ad4b6baa51062cfcaa6ef1b836b
Irem Uguz <iremuguz@google.com>
2022-01-18 11:04:50 +0000

    support-tool: Return errors in a struct that contains code and message
    Add SupportToolError struct which contains a general meaning error code
    and detailed error message and return when an error happens during data
    collection or export. These detailed error messages will be added to
    support packet's metadata file later. This CL is a follow-up to comment
    thread of previous crrev/c/3373024.
    
    Bug:b:214196981
    Test:Run unit tests with "autoninja -C out/Default/ unit_tests &&
    out/Default/unit_tests --gtest_filter=*DataCollector* &&
    out/Default/unit_tests --gtest_filter=*SupportTool*" command.
    
    Change-Id: I3c7449b36c7c2e28871645812b6dab1b2b81dd28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386795
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#960319}

--- chrome/browser/support_tool/ash/ui_hierarchy_data_collector.cc
+++ chrome/browser/support_tool/ash/ui_hierarchy_data_collector.cc
@@ -117,7 +117,7 @@ void UiHierarchyDataCollector::CollectDataAndDetectPII(
   data_ = std::move(ui_hierarchy_data.data);
   // `data_` can't be empty.
   DCHECK(!data_.empty());
-  std::move(on_data_collected_callback).Run(/*error_code=*/absl::nullopt);
+  std::move(on_data_collected_callback).Run(/*error=*/absl::nullopt);
 }
 
 void UiHierarchyDataCollector::ExportCollectedDataWithPII(
@@ -141,11 +141,12 @@ void UiHierarchyDataCollector::OnDataExportDone(
     bool success) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!success) {
-    std::move(on_exported_callback)
-        .Run(SupportToolError::kUIHierarchyDataCollectorError);
+    SupportToolError error = {SupportToolErrorCode::kDataCollectorError,
+                              "Failed on data export."};
+    std::move(on_exported_callback).Run(error);
     return;
   }
-  std::move(on_exported_callback).Run(/*error_code=*/absl::nullopt);
+  std::move(on_exported_callback).Run(/*error=*/absl::nullopt);
 }
 
 void UiHierarchyDataCollector::InsertIntoPIIMap(
