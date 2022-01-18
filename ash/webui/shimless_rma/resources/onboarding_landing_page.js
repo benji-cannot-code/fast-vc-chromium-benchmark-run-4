efe0b226a6fed1a53f65386881edd2bf8d983ffe
Gavin Williams <gavinwill@chromium.org>
2022-01-18 19:45:22 +0000

    Shimless: Show unqualified components dialog
    - When the hardware verifier returns an error, show link that opens a
      dialog showing the unqualified components
    - Remove the |verificationMessage_| property and choose the appropriate
      string in the HTML
    
    screenshot: http://screen/boLUoevqPLLUpzG
    
    Bug: 1198187
    Change-Id: I2418a170cdf8067e88e51ca2faa42f805c7363a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389675
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960539}

--- ash/webui/shimless_rma/resources/onboarding_landing_page.js
+++ ash/webui/shimless_rma/resources/onboarding_landing_page.js
@@ -41,17 +41,11 @@ export class OnboardingLandingPage extends OnboardingLandingPageBase {
 
   static get properties() {
     return {
-      /** @protected */
-      verificationMessage_: {
-        type: String,
-        value: '',
-      },
-
       /**
-       * Error code from rmad service, not i18n.
+       * List of unqualified components from rmad service, not i18n.
        * @protected
        */
-      errorMessage_: {
+      componentsList_: {
         type: String,
         value: '',
       },
@@ -69,6 +63,12 @@ export class OnboardingLandingPage extends OnboardingLandingPageBase {
       isCompliant_: {
         type: Boolean,
         value: false,
+      },
+
+      /** @protected */
+      verificationFailedMessage_: {
+        type: String,
+        value: '',
       },
     };
   }
@@ -92,7 +92,6 @@ export class OnboardingLandingPage extends OnboardingLandingPageBase {
   /** @override */
   ready() {
     super.ready();
-    this.verificationMessage_ = this.i18n('validatingComponentsText');
   }
 
   /** @return {!Promise<StateResult>} */
@@ -119,18 +118,36 @@ export class OnboardingLandingPage extends OnboardingLandingPageBase {
    * @param {string} errorMessage
    */
   onHardwareVerificationResult(isCompliant, errorMessage) {
-    if (isCompliant) {
-      this.verificationMessage_ = this.i18n('validatedComponentsSuccessText');
-    } else {
-      this.verificationMessage_ = this.i18n('validatedComponentsFailText');
-      this.errorMessage_ = errorMessage;
-    }
     this.isCompliant_ = isCompliant;
     this.verificationInProgress_ = false;
+
+    if (!this.isCompliant_) {
+      this.componentsList_ = errorMessage;
+      this.setVerificationFailedMessage_();
+    }
+
     this.dispatchEvent(new CustomEvent(
         'disable-next-button',
         {bubbles: true, composed: true, detail: false},
         ));
+  }
+
+  /** @private */
+  setVerificationFailedMessage_() {
+    this.verificationFailedMessage_ =
+        this.i18nAdvanced('validatedComponentsFailText', {attrs: ['id']});
+    const linkElement =
+        this.shadowRoot.querySelector('#unqualifiedComponentsLink');
+    linkElement.setAttribute('href', '#');
+    linkElement.addEventListener(
+        'click',
+        () => this.shadowRoot.querySelector('#unqualifiedComponentsDialog')
+                  .showModal());
+  }
+
+  /** @private */
+  closeDialog_() {
+    this.shadowRoot.querySelector('#unqualifiedComponentsDialog').close();
   }
 }
 
