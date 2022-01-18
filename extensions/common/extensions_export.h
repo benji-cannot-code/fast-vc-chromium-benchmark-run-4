ec04f12ea0211544ba0d7ee93743474fd042e0bb
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-01-18 23:01:59 +0000

    [Extensions] Make extensions/common:common_constants component-friendly
    The //extensions layer is currently not component-friendly - it is a
    bunch of targets with source_sets, which means that if components depend
    on these targets, they are pulled in multiple times. Among other things,
    this is very bad for singletons (which are no longer singletons).
    
    A number of //components depend on //extensions; however, all but
    //components/sessions only appear to depend on the
    //extensions/common:common_constants target.
    
    Make //extensions/common:common_constants a component-friendly target
    and introduce a top-level //extensions component target (which currently
    only includes //extensions/common:common_constants). This allows
    //components to depend on this target, and gives us a component that we
    can slowly add items to to make the //extensions layer itself a full
    component.
    
    Bug: 1286881
    Change-Id: I7d90aa119c5d7611a0311150b3c034159c37ec3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380398
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960670}

---
+++ extensions/common/extensions_export.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXTENSIONS_COMMON_EXTENSIONS_EXPORT_H_
+#define EXTENSIONS_COMMON_EXTENSIONS_EXPORT_H_
+
+#if defined(COMPONENT_BUILD)
+
+#if defined(WIN32)
+
+#if defined(EXTENSIONS_COMPONENT_IMPLEMENTATION)
+#define EXTENSIONS_EXPORT __declspec(dllexport)
+#else
+#define EXTENSIONS_EXPORT __declspec(dllimport)
+#endif  // defined(EXTENSIONS_COMPONENT_IMPLEMENTATION)
+
+#else  // defined(WIN32)
+#if defined(EXTENSIONS_COMPONENT_IMPLEMENTATION)
+#define EXTENSIONS_EXPORT __attribute__((visibility("default")))
+#else
+#define EXTENSIONS_EXPORT
+#endif
+#endif
+
+#else  // defined(COMPONENT_BUILD)
+#define EXTENSIONS_EXPORT
+#endif
+
+#endif  // EXTENSIONS_COMMON_EXTENSIONS_EXPORT_H_
