32c48dc0e07ccf5aebae36bce420f522eaa38f51
adamta <adamta@google.com>
2022-01-18 16:09:16 +0000

    [iOS] Remove unused feed callbacks
    Removes metric for infinite feed triggering, since we no longer call it
    manually. Also removes callback for FeedWasShown, which is unused.
    
    Bug: 1200303, 1126940
    Change-Id: Id5bcec7ce688dc29a19eee991f286c0c50e1bca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3328563
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Auto-Submit: Adam Trudeau-Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#960411}

--- ios/chrome/browser/ui/ntp/feed_metrics_recorder.mm
+++ ios/chrome/browser/ui/ntp/feed_metrics_recorder.mm
@@ -50,10 +50,6 @@ enum class FeedLoadStreamStatus {
 };
 
 namespace {
-// Histogram name for the infinite feed trigger.
-const char kDiscoverFeedInfiniteFeedTriggered[] =
-    "ContentSuggestions.Feed.LoadStreamStatus.LoadMore";
-
 // User action names for the device orientation having changed.
 const char kDiscoverFeedHistogramDeviceOrientationChangedToPortrait[] =
     "ContentSuggestions.Feed.DeviceOrientationChanged.Portrait";
@@ -115,10 +111,6 @@ const char kDiscoverFeedUserActionManageActivityTapped[] =
     "ContentSuggestions.Feed.HeaderAction.ManageActivity";
 const char kDiscoverFeedUserActionManageInterestsTapped[] =
     "ContentSuggestions.Feed.HeaderAction.ManageInterests";
-
-// User action name for infinite feed triggering.
-const char kDiscoverFeedUserActionInfiniteFeedTriggered[] =
-    "ContentSuggestions.Feed.InfiniteFeedTriggered";
 
 // User action name for engaging with feed.
 const char kDiscoverFeedUserActionEngaged[] = "ContentSuggestions.Feed.Engaged";
@@ -222,13 +214,6 @@ const int kMinutesBetweenSessions = 5;
     [self recordEngagementTypeHistogram:FeedEngagementType::kFeedScrolled];
     self.scrolledReported = YES;
   }
-}
-
-- (void)recordInfiniteFeedTriggered {
-  UMA_HISTOGRAM_ENUMERATION(kDiscoverFeedInfiniteFeedTriggered,
-                            FeedLoadStreamStatus::kLoadedFromNetwork);
-  base::RecordAction(
-      base::UserMetricsAction(kDiscoverFeedUserActionInfiniteFeedTriggered));
 }
 
 - (void)recordDeviceOrientationChanged:(UIDeviceOrientation)orientation {
--- ios/public/provider/chrome/browser/discover_feed/discover_feed_provider.mm
+++ ios/public/provider/chrome/browser/discover_feed/discover_feed_provider.mm
@@ -41,5 +41,3 @@ void DiscoverFeedProvider::UpdateFeedForAccountChange() {}
 
 void DiscoverFeedProvider::AddObserver(Observer* observer) {}
 void DiscoverFeedProvider::RemoveObserver(Observer* observer) {}
-void DiscoverFeedProvider::LoadMoreFeedArticles() {}
-void DiscoverFeedProvider::FeedWasShown() {}
