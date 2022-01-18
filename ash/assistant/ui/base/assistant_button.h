e04b7696fcbf71c32e091bac5438f363dbf428d5
Yuki Awano <yawano@google.com>
2022-01-18 22:55:42 +0000

    Make ripple of assistant button work with dark light mode
    Bug: b:204489074
    Change-Id: Iac5b6f38449d01cb07c021cf0902f6824bac5a0d
    Fixed: b:204489074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3373983
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#960661}

--- ash/assistant/ui/base/assistant_button.h
+++ ash/assistant/ui/base/assistant_button.h
@@ -84,6 +84,8 @@ class COMPONENT_EXPORT(ASSISTANT_UI) AssistantButton
 
  private:
   void OnButtonPressed();
+  void UpdateFocusPainter();
+  void UpdateInkDropColors();
 
   AssistantButtonListener* listener_;
   const AssistantButtonId id_;
