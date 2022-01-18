3d63fee6d5912635a00557bcdb645263d965b232
Rune Lillesveen <futhark@chromium.org>
2022-01-18 16:10:54 +0000

    [@container] Update StyleRecalcContext for ::slotted and ::part
    The closest query container for ::slotted and ::part rules are in the
    shadow-including inclusive ancestors for the slot and host originating
    elements respectively. Adjust the StyleRecalcContext when matching such
    rules.
    
    Bug: 1269192
    Change-Id: I709741dc5f5d12d3bce300c72241fcc8b745251f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3397052
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960413}

--- third_party/blink/renderer/core/css/element_rule_collector.h
+++ third_party/blink/renderer/core/css/element_rule_collector.h
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class CSSStyleSheet;
+class Element;
+class HTMLSlotElement;
 class PartNames;
 class RuleData;
 class SelectorFilter;
@@ -149,6 +151,30 @@ class CORE_EXPORT ElementRuleCollector {
   PseudoId GetPseudoId() const { return pseudo_style_request_.pseudo_id; }
 
   void AddMatchedRulesToTracker(StyleRuleUsageTracker*) const;
+
+  // Temporarily swap the StyleRecalcContext with one which points to the
+  // closest query container for matching ::slotted rules for a given slot.
+  class SlottedRulesScope {
+   public:
+    SlottedRulesScope(ElementRuleCollector& collector, HTMLSlotElement& slot)
+        : context_(&collector.style_recalc_context_,
+                   collector.style_recalc_context_.ForSlottedRules(slot)) {}
+
+   private:
+    base::AutoReset<StyleRecalcContext> context_;
+  };
+
+  // Temporarily swap the StyleRecalcContext with one which points to the
+  // closest query container for matching ::part rules for a given host.
+  class PartRulesScope {
+   public:
+    PartRulesScope(ElementRuleCollector& collector, Element& host)
+        : context_(&collector.style_recalc_context_,
+                   collector.style_recalc_context_.ForPartRules(host)) {}
+
+   private:
+    base::AutoReset<StyleRecalcContext> context_;
+  };
 
  private:
   struct PartRequest {
