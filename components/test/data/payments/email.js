497191d894392813713b1b2c63c909011e7fa6f9
Liquan (Max) Gu <maxlg@chromium.org>
2022-01-18 21:05:51 +0000

    Migrate 1 test: payment_request_journey_logger_browsertest
    This CL migrates 1 tests that depends on basic-card. The migrated
    tests are added as new tests so that the coverage for the old paths
    will be preserved.
    
    Bug: 1221559
    Change-Id: I390298ecc7a9264111442b06d09802c60310aa2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396597
    Reviewed-by: Nick Burris <nburris@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960602}

--- components/test/data/payments/email.js
+++ components/test/data/payments/email.js
@@ -11,15 +11,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Launches the PaymentRequest UI that requests email address.
  */
 function buy() { // eslint-disable-line no-unused-vars
+  buyWithMethods([
+    {supportedMethods: 'https://bobpay.com'},
+    {
+      supportedMethods: 'basic-card',
+      data: {supportedNetworks: ['visa']},
+    },
+  ]);
+}
+
+/**
+ * Launches the PaymentRequest UI that requests email address.
+ * @param {sequence<PaymentMethodData>} methodData An array of payment method
+ *        objects.
+ */
+function buyWithMethods(methodData) {
   try {
     new PaymentRequest(
-        [
-          {supportedMethods: 'https://bobpay.com'},
-          {
-            supportedMethods: 'basic-card',
-            data: {supportedNetworks: ['visa']},
-          },
-        ],
+        methodData,
         {total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}}},
         {requestPayerEmail: true})
         .show()
