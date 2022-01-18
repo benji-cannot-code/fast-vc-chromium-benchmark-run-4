b7a1097fcebeade54b3191fef37d9a85a02605a8
Sylvain Defresne <sdefresne@chromium.org>
2022-01-18 11:48:50 +0000

    [ios] Allow use of ObjC in ApplicationContext
    The class ApplicationContext currently is pure C++ and thus
    cannot return Objective-C instances. Convert all C++ files
    that includes //ios/chrome/browser/application_context.h to
    Objective-C++ files (i.e. rename them from .cc to .mm, and
    add pre-processor guards to ensure that ARC is enabled).
    
    This will allow use of ObjC in ApplicationContext in followup
    changes.
    
    Bug: 1201182
    Change-Id: I6bfad8e6ab60cae1e3a4df343f670b4ff1263a38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396475
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960326}

---
+++ ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.mm
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
+
+#include "ios/chrome/browser/application_context.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_manager.h"
+#include "ios/chrome/test/testing_application_context.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+IOSChromeScopedTestingChromeBrowserStateManager::
+    IOSChromeScopedTestingChromeBrowserStateManager(
+        std::unique_ptr<ios::ChromeBrowserStateManager> browser_state_manager)
+    : browser_state_manager_(std::move(browser_state_manager)),
+      saved_browser_state_manager_(
+          GetApplicationContext()->GetChromeBrowserStateManager()) {
+  TestingApplicationContext::GetGlobal()->SetChromeBrowserStateManager(
+      browser_state_manager_.get());
+}
+
+IOSChromeScopedTestingChromeBrowserStateManager::
+    ~IOSChromeScopedTestingChromeBrowserStateManager() {
+  TestingApplicationContext::GetGlobal()->SetChromeBrowserStateManager(
+      saved_browser_state_manager_);
+}
