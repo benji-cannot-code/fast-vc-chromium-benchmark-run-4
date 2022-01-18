02afeebaea91bbea3111d7b066b02c94d32ee7b0
David Black <dmblack@google.com>
2022-01-18 19:59:28 +0000

    personalization: Render Google Photos albums.
    This CL modifies the existing WallpaperGridItem element to handle
    rendering of images and text instead of just hosting a slotted element.
    This capability is then used to render Google Photos albums.
    
    Screenshot: http://shortn/_ajEd5l8H8H
    
    Bug: b:215163996
    Change-Id: I563bf053e6754fb40e93f2f4f9347a91e5dbeda5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3397181
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#960553}

--- ash/webui/personalization_app/resources/common/utils.ts
+++ ash/webui/personalization_app/resources/common/utils.ts
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * code.
  */
 
+import {loadTimeData} from '//resources/js/load_time_data.m.js';
+
 /**
  * Checks if argument is an array with non-zero length.
  */
@@ -66,18 +68,33 @@ export function promisifyOnload(
 
 type WallpaperSelectionEvent =
     MouseEvent&{type: 'click'}|KeyboardEvent&{key: 'Enter'};
-/**
- * Returns true if this event is a user action to select an item.
- */
+/** Returns true if this event is a user action to select an item. */
 export function isSelectionEvent(event: Event):
     event is WallpaperSelectionEvent {
   return (event instanceof MouseEvent && event.type === 'click') ||
       (event instanceof KeyboardEvent && event.key === 'Enter');
 }
 
-/**
- * Sets a css variable to control the animation delay.
- */
+/** Returns the text to display for a number of images. */
+export function getCountText(x: number|null|undefined): string {
+  switch (x) {
+    case null:
+    case undefined:
+      return '';
+    case 0:
+      return loadTimeData.getString('zeroImages');
+    case 1:
+      return loadTimeData.getString('oneImage');
+    default:
+      if ('number' !== typeof x || x < 0) {
+        console.error('Received an impossible value');
+        return '';
+      }
+      return loadTimeData.getStringF('multipleImages', x);
+  }
+}
+
+/** Returns a css variable to control the animation delay. */
 export function getLoadingPlaceholderAnimationDelay(index: number): string {
   return `--animation-delay: ${index * 83}ms;`;
 }
--- ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_photos_element.ts
+++ ash/webui/personalization_app/resources/trusted/wallpaper/google_photos_photos_element.ts
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 import 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
-import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import './styles.js';
 import '../../common/styles.js';
 
