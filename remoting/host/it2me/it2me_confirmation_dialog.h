d5cea7438ebc499e02e95f9d4d10c040b5fc78ae
Avi Drissman <avi@chromium.org>
2022-01-18 18:52:50 +0000

    Use [[maybe_unused]] in remoting/
    This cleans up a use of ALLOW_UNUSED_TYPE.
    
    Bug: 1286390
    Change-Id: Ic829a28f3f93708cb295f5c1829c2ca50d7ee49f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3393062
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960505}

--- remoting/host/it2me/it2me_confirmation_dialog.h
+++ remoting/host/it2me/it2me_confirmation_dialog.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback_forward.h"
-#include "base/compiler_specific.h"
 
 namespace remoting {
 
@@ -52,7 +51,7 @@ class It2MeConfirmationDialogFactory {
 
  private:
   // This field is only used on ChromeOS.
-  ALLOW_UNUSED_TYPE It2MeConfirmationDialog::DialogStyle dialog_style_ =
+  [[maybe_unused]] It2MeConfirmationDialog::DialogStyle dialog_style_ =
       It2MeConfirmationDialog::DialogStyle::kConsumer;
 };
 
