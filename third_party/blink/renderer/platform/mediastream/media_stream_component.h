db7577697078a16327c6868f0dc94cab577f1a9b
Tony Herre <toprice@chromium.org>
2022-01-18 09:43:49 +0000

    Reland "Reland "Take the playout AudioDevice from a MediaStreamTrack's creation frame""
    This is a reland of c10f7b216fe47ef7cb21c8b218fab676c752014b
    
    Delta to previous reland:
    - Fix leak by only storing a weak reference from RemoteMediaStreamTrackAdapter to the ExecutionContext
    - Also driveby to only forward declare web_local_frame in media_stream_component.h per comment on previous cl
    
    
    Original change's description:
    > Reland "Take the playout AudioDevice from a MediaStreamTrack's creation frame"
    >
    > This is a reland of 6a17272bc28c96460f3b055e10d6990e5bd3f836
    >
    > Delta: Fix crash at the end of WPTs by checking the LocalDOMWindow's LocalFrame is not null before accessing it. When the frame is destroyed this is set to null.
    >
    > Original change's description:
    > > Take the playout AudioDevice from a MediaStreamTrack's creation frame
    > >
    > > When playing a Remote Audio MediaStreamTrack (one received over a PeerConnection), use the WebRtcAudioDeviceImpl instance associated with the frame where the track was created.
    > >
    > > This fixes an issue where audio is not played if the track is attached to a media element inside a (same origin) iframe.
    > >
    > > Bug: 1239207
    > > Change-Id: Ib97e43913fce75982b5e3f3b2c54a9d9a9a815a8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386796
    > > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > > Commit-Queue: Tony Herre <toprice@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#959107}
    >
    > Bug: 1239207
    > Change-Id: Ie572d38c3d2a06498cb52280bcd8e5efc9e3c755
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3394627
    > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > Commit-Queue: Tony Herre <toprice@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#960125}
    
    Bug: 1239207
    Change-Id: Icf2e6b1ce1e40de9cc6cc84631dfc34e90525ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3395841
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960303}

--- third_party/blink/renderer/platform/mediastream/media_stream_component.h
+++ third_party/blink/renderer/platform/mediastream/media_stream_component.h
@@ -51,6 +51,7 @@ namespace blink {
 
 class MediaStreamSource;
 class WebAudioSourceProvider;
+class WebLocalFrame;
 
 // A MediaStreamComponent is a MediaStreamTrack.
 // TODO(hta): Consider merging the two classes.
@@ -107,6 +108,11 @@ class PLATFORM_EXPORT MediaStreamComponent final
   void GetSettings(MediaStreamTrackPlatform::Settings&);
   MediaStreamTrackPlatform::CaptureHandle GetCaptureHandle();
 
+  WebLocalFrame* CreationFrame() { return creation_frame_; }
+  void SetCreationFrame(WebLocalFrame* creation_frame) {
+    creation_frame_ = creation_frame;
+  }
+
   String ToString() const;
 
   void Trace(Visitor*) const;
@@ -139,6 +145,7 @@ class PLATFORM_EXPORT MediaStreamComponent final
 
   AudioSourceProviderImpl source_provider_;
   Member<MediaStreamSource> source_;
+
   String id_;
   int unique_id_;
   bool enabled_ = true;
@@ -147,6 +154,8 @@ class PLATFORM_EXPORT MediaStreamComponent final
       WebMediaStreamTrack::ContentHintType::kNone;
   MediaConstraints constraints_;
   std::unique_ptr<MediaStreamTrackPlatform> platform_track_;
+  // Frame where the referenced platform track was created, if applicable.
+  WebLocalFrame* creation_frame_;
 };
 
 typedef HeapVector<Member<MediaStreamComponent>> MediaStreamComponentVector;
