31b982c049848af24f802b816672fa7d7fe6a09e
xiangdong kong <xiangdongkong@google.com>
2022-01-18 19:15:18 +0000

    feedback: Add search page skeleton
    The search page is the first page of the feedback tool.
    
    Bug: 185624798
    Change-Id: Ib17d3dcf62d649721a2d327c61d2aaae9be176f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3390831
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#960520}

--- chrome/test/data/webui/chromeos/os_feedback_ui/os_feedback_unified_test.js
+++ chrome/test/data/webui/chromeos/os_feedback_ui/os_feedback_unified_test.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
 
 import {confirmationPageTest} from './confirmation_page_test.js';
+import {searchPageTestSuite} from './search_page_test.js';
 
 window.test_suites_list = [];
 
@@ -14,4 +15,5 @@ function runSuite(suiteName, testFn) {
   suite(suiteName, testFn);
 }
 
-runSuite('ConfirmationPageTest', confirmationPageTest);
+runSuite('confirmationPageTest', confirmationPageTest);
+runSuite('searchPageTest', searchPageTestSuite);
---
+++ chrome/test/data/webui/chromeos/os_feedback_ui/search_page_test.js
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {SearchPageElement} from 'chrome://os-feedback/search_page.js';
+
+import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
+import {flushTasks} from '../../test_util.js';
+
+export function searchPageTestSuite() {
+  /** @type {?SearchPageElement} */
+  let page = null;
+
+  setup(() => {
+    document.body.innerHTML = '';
+  });
+
+  teardown(() => {
+    page.remove();
+    page = null;
+  });
+
+  function initializePage() {
+    assertFalse(!!page);
+    page =
+        /** @type {!SearchPageElement} */ (
+            document.createElement('search-page'));
+    assertTrue(!!page);
+    document.body.appendChild(page);
+    return flushTasks();
+  }
+
+  test('SearchPageLoaded', () => {
+    return initializePage().then(() => {
+      // Verify the title is in the page
+      const title = page.shadowRoot.querySelector('#title');
+      assertTrue(!!title);
+      assertEquals('Send feedback', title.textContent);
+
+      // Verify the continue button is in the page
+      const btnContinue = page.shadowRoot.querySelector('#btnContinue');
+      assertTrue(!!btnContinue);
+    });
+  });
+}
