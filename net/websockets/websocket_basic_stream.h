ab61f24ef088197cae6fcfdbd22d305dabcd7157
Nanami Mikiya <nmikiya@google.com>
2022-01-18 11:32:10 +0000

    Adapt the size of read_buffer_ in WebSocket
    WebSocket had a fixed read_buffer_ size tuned for maximum throughput at the expense of memory consumption. Prepare another small size and an algorithm to switch the buffer size dynamically according to the type of traffic. This algorithm should satisfy the following conditions:
    1. For the high throughput test, switch the read buffer size to a large one quickly and keep the high throughput.
    2. For the low throughput test, keep using the small read buffer.
    The change is protected by a feature which is disabled by default.
    
    BUG=1001012
    
    Change-Id: I99b679a8245813828507410f4015e440e7225cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3327840
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Nanami Mikiya <nmikiya@google.com>
    Cr-Commit-Position: refs/heads/main@{#960322}

--- net/websockets/websocket_basic_stream.h
+++ net/websockets/websocket_basic_stream.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/queue.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/time/time.h"
 #include "net/base/completion_once_callback.h"
 #include "net/base/net_export.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
@@ -148,7 +150,10 @@ class NET_EXPORT_PRIVATE WebSocketBasicStream final : public WebSocketStream {
   void AddToIncompleteControlFrameBody(base::span<const char> data);
 
   // Storage for pending reads.
-  const scoped_refptr<IOBufferWithSize> read_buffer_;
+  scoped_refptr<IOBufferWithSize> read_buffer_;
+
+  // The best read buffer size for the current throughput.
+  size_t target_read_buffer_size_;
 
   // The connection, wrapped in a ClientSocketHandle so that we can prevent it
   // from being returned to the pool.
@@ -195,6 +200,15 @@ class NET_EXPORT_PRIVATE WebSocketBasicStream final : public WebSocketStream {
   // User callback saved for asynchronous writes and reads.
   CompletionOnceCallback write_callback_;
   CompletionOnceCallback read_callback_;
+
+  // This keeps the timestamps to calculate the throughput.
+  base::queue<base::TimeTicks> read_start_timestamps_;
+
+  // The sum of the last few read size.
+  int rolling_byte_total_ = 0;
+
+  // This keeps the read size.
+  base::queue<int> recent_read_sizes_;
 };
 
 }  // namespace net
