15e4d74dcf1d94bd14b754504faf4871ef83ddc4
Allen Bauer <kylixrd@chromium.org>
2022-01-18 20:04:35 +0000

    Remove use of GridLayout in UpdateAddressProfileView.
    Added missing browser test to invoke UI.
    
    Bug: 1261301
    Change-Id: Ic2664291f6c9afd9cccf1f682c64c6fd20cf37d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389723
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960560}

--- chrome/browser/ui/autofill/save_update_address_profile_bubble_controller_impl_browsertest.cc
+++ chrome/browser/ui/autofill/save_update_address_profile_bubble_controller_impl_browsertest.cc
@@ -36,9 +36,10 @@ class SaveUpdateAddressProfileBubbleControllerImplTest
         browser()->tab_strip_model()->GetActiveWebContents();
     autofill::ChromeAutofillClient* autofill_client =
         autofill::ChromeAutofillClient::FromWebContents(web_contents);
+    AutofillProfile profile = test::GetFullProfile();
+    AutofillProfile* original_profile = (name == "Update") ? &profile : nullptr;
     autofill_client->ConfirmSaveAddressProfile(
-        test::GetFullProfile(),
-        /*original_profile=*/nullptr,
+        profile, original_profile,
         AutofillClient::SaveAddressProfilePromptOptions{.show_prompt = true},
         base::DoNothing());
     controller_ = SaveUpdateAddressProfileBubbleControllerImpl::FromWebContents(
@@ -57,6 +58,11 @@ class SaveUpdateAddressProfileBubbleControllerImplTest
 
 IN_PROC_BROWSER_TEST_F(SaveUpdateAddressProfileBubbleControllerImplTest,
                        InvokeUi_Save) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_F(SaveUpdateAddressProfileBubbleControllerImplTest,
+                       InvokeUi_Update) {
   ShowAndVerifyUi();
 }
 
