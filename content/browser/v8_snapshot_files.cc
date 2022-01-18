54aa083c0354e1c8aae80107f3cb10af923c0ca7
Scott Violet <sky@chromium.org>
2022-01-18 19:07:18 +0000

    android: removes ability to include both v8 snapshots
    At this time the increase in disk size is not worth the benefit.
    
    This is mostly a revert of
    https://chromium-review.googlesource.com/c/chromium/src/+/3209682
    
    BUG=764576,1257321
    TEST=none
    
    Change-Id: I82506d3287192baab8cb14131ff129987f44fc29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381737
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960513}

--- content/browser/v8_snapshot_files.cc
+++ content/browser/v8_snapshot_files.cc
@@ -5,17 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/v8_snapshot_files.h"
 
-#include "base/command_line.h"
-#include "base/feature_list.h"
 #include "build/build_config.h"
 #include "content/public/common/content_descriptor_keys.h"
-#include "content/public/common/content_features.h"
-#include "content/public/common/content_switches.h"
 
 namespace content {
 
-std::map<std::string, base::FilePath> GetV8SnapshotFilesToPreload(
-    base::CommandLine& process_command_line) {
+std::map<std::string, base::FilePath> GetV8SnapshotFilesToPreload() {
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
 #if defined(USE_V8_CONTEXT_SNAPSHOT)
   return {{kV8ContextSnapshotDataDescriptor,
@@ -25,14 +20,7 @@ std::map<std::string, base::FilePath> GetV8SnapshotFilesToPreload(
            base::FilePath(FILE_PATH_LITERAL("snapshot_blob.bin"))}};
 #endif
 #elif defined(OS_ANDROID)
-#if defined(INCLUDE_BOTH_V8_SNAPSHOTS) || !defined(USE_V8_CONTEXT_SNAPSHOT)
-#if defined(INCLUDE_BOTH_V8_SNAPSHOTS)
-  if (base::FeatureList::IsEnabled(features::kUseContextSnapshot)) {
-    process_command_line.AppendSwitch(switches::kUseContextSnapshotSwitch);
-    // For USE_V8_CONTEXT_SNAPSHOT, the renderer reads the files directly.
-    return {};
-  }
-#endif
+#if !defined(USE_V8_CONTEXT_SNAPSHOT)
   return {{kV8Snapshot64DataDescriptor,
            base::FilePath(FILE_PATH_LITERAL("assets/snapshot_blob_64.bin"))},
           {kV8Snapshot32DataDescriptor,
--- content/browser/v8_snapshot_files.h
+++ content/browser/v8_snapshot_files.h
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 
-namespace base {
-class CommandLine;
-}
-
 namespace content {
 
 // Returns a mapping of V8 snapshot files to be preloaded for child processes
@@ -22,11 +18,8 @@ namespace content {
 // be empty or unused on some.
 //
 // This mapping can be passed to
-// `BrowserChildProcessHost::LaunchWithPreloadedFiles()`. `process_command_line`
-// is the command line that will be used in launching the process the files will
-// be supplied to.
-std::map<std::string, base::FilePath> GetV8SnapshotFilesToPreload(
-    base::CommandLine& process_command_line);
+// `BrowserChildProcessHost::LaunchWithPreloadedFiles()`.
+std::map<std::string, base::FilePath> GetV8SnapshotFilesToPreload();
 
 }  // namespace content
 
