80601f967502aa299a6b73ebcf51b088b849982e
Milica Selakovic <selakovic@google.com>
2022-01-18 17:47:46 +0000

    [Password manager] Record UKMs when user trigger password change
    UKMs can be recorded when web content exists. Once user trigger manual
    password change we pass user's intent via intent parameters. When tab
    and web content is created we record UKM.
    
    UKM collection review doc: https://docs.google.com/document/d/1DEd-Q4O4FRGdO8Gj2lKw259xes29hHSrGAgJEbJHJpg/edit?resourcekey=0-D0OnMfeMj3uOgheLfNS64Q
    
    Bug:1280581
    
    Change-Id: I06be4261c91d024008597e8b684fad112bd50c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3361047
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Milica Selakovic <selakovic@google.com>
    Cr-Commit-Position: refs/heads/main@{#960473}

---
+++ chrome/browser/password_check/android/java/src/org/chromium/chrome/browser/password_check/PasswordChangeType.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_check;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * These values are persisted to logs. Entries should not be renumbered and
+ * numeric values should never be reused. To be kept in sync with PasswordChangeType in
+ * enums.xml.
+ */
+@IntDef({PasswordChangeType.UNKNOWN, PasswordChangeType.MANUAL_CHANGE,
+        PasswordChangeType.AUTOMATED_CHANGE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface PasswordChangeType {
+    int UNKNOWN = 0;
+    /**
+     * A user opened a site to change a password manually.
+     */
+    int MANUAL_CHANGE = 1;
+    /**
+     * A user started an automated password change flow.
+     */
+    int AUTOMATED_CHANGE = 2;
+}
