bf485f636274d2b67658cc7afcbc19a27d42473b
Yusuke Sato <yusukes@chromium.org>
2022-01-18 21:28:34 +0000

    Add a unit test for arc::ThrottleService::GetObserverByName()
    This makes the file's unit test coverage ~100% again.
    
    BUG=b:213636597
    
    Change-Id: I08e9387e232716a128b746fc4b57195645f18cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389976
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960621}

--- chrome/browser/ash/throttle_service_unittest.cc
+++ chrome/browser/ash/throttle_service_unittest.cc
@@ -15,6 +15,9 @@ namespace ash {
 
 namespace {
 
+constexpr const char kFirstObserverName[] = "o1";
+constexpr const char kSecondObserverName[] = "o2";
+
 class TestObserver : public ThrottleService::ServiceObserver {
  public:
   TestObserver() = default;
@@ -80,8 +83,9 @@ class ThrottleServiceTest : public testing::Test {
  public:
   ThrottleServiceTest() : service_(&profile_) {
     std::vector<std::unique_ptr<ThrottleObserver>> observers;
-    observers.push_back(std::make_unique<ThrottleObserver>("o1"));
-    observers.push_back(std::make_unique<ThrottleObserver>("o2"));
+    observers.push_back(std::make_unique<ThrottleObserver>(kFirstObserverName));
+    observers.push_back(
+        std::make_unique<ThrottleObserver>(kSecondObserverName));
     service_.SetObserversForTesting(std::move(observers));
   }
 
@@ -216,6 +220,16 @@ TEST_F(ThrottleServiceTest, TestObservers) {
   service()->observers_for_testing()[1]->SetActive(true);
   EXPECT_FALSE(service()->should_throttle());
   EXPECT_EQ(0, test_observer.GetUpdateCountAndReset());
+}
+
+// Tests that getting an observer by its name works.
+TEST_F(ThrottleServiceTest, TestGetObserverByName) {
+  auto* first_observer = service()->GetObserverByName(kFirstObserverName);
+  auto* second_observer = service()->GetObserverByName(kSecondObserverName);
+  EXPECT_NE(nullptr, first_observer);
+  EXPECT_NE(nullptr, second_observer);
+  EXPECT_NE(first_observer, second_observer);
+  EXPECT_EQ(nullptr, service()->GetObserverByName("NonExistentObserverName"));
 }
 
 }  // namespace ash
