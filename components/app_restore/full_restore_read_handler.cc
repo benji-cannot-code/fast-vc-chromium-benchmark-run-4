66496a94a51c611212f45c13deacaa7f794efbdf
Nancy Wang <nancylingwang@chromium.org>
2022-01-19 03:27:36 +0000

    Add LacrosSaveHandler to restore Lacros windows.
    This CL adds the LacrosSaveHandler class to help restore Lacros windows
    for FullRestoreReadHandler.
    
    Modify IsFullRestoreRunning to set 1 minute waiting time for Lacros
    windows restoration.
    
    BUG=1239984
    
    Change-Id: If38c3344027633feb218f1e9bb9977cd247f0fae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3388311
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960774}

--- components/app_restore/full_restore_read_handler.cc
+++ components/app_restore/full_restore_read_handler.cc
@@ -16,9 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/app_restore/app_launch_info.h"
 #include "components/app_restore/desk_template_read_handler.h"
+#include "components/app_restore/features.h"
 #include "components/app_restore/full_restore_file_handler.h"
 #include "components/app_restore/full_restore_info.h"
 #include "components/app_restore/full_restore_save_handler.h"
+#include "components/app_restore/lacros_read_handler.h"
 #include "components/app_restore/restore_data.h"
 #include "components/app_restore/window_info.h"
 #include "components/app_restore/window_properties.h"
@@ -34,6 +36,7 @@ namespace {
 //  apps.
 constexpr base::TimeDelta kFullRestoreEstimateDuration = base::Seconds(5);
 constexpr base::TimeDelta kFullRestoreARCEstimateDuration = base::Minutes(5);
+constexpr base::TimeDelta kFullRestoreLacrosEstimateDuration = base::Minutes(1);
 
 }  // namespace
 
@@ -138,6 +141,15 @@ void FullRestoreReadHandler::OnTaskCreated(const std::string& app_id,
 void FullRestoreReadHandler::OnTaskDestroyed(int32_t task_id) {
   if (arc_read_handler_)
     arc_read_handler_->OnTaskDestroyed(task_id);
+}
+
+void FullRestoreReadHandler::OnLacrosBrowserWindowAdded(
+    aura::Window* const window,
+    uint32_t restored_browser_session_id) {
+  if (lacros_read_handler_) {
+    lacros_read_handler_->OnLacrosBrowserWindowAdded(
+        window, restored_browser_session_id);
+  }
 }
 
 void FullRestoreReadHandler::SetActiveProfilePath(
@@ -308,11 +320,21 @@ bool FullRestoreReadHandler::IsFullRestoreRunning() const {
     return false;
 
   base::TimeDelta elapsed_time = base::TimeTicks::Now() - it->second;
+
   // We estimate that full restore is still running if it has been less than
-  // five seconds since it started, or five minutes if there is at least one ARC
-  // app.
-  return arc_read_handler_ ? elapsed_time < kFullRestoreARCEstimateDuration
-                           : elapsed_time < kFullRestoreEstimateDuration;
+  // five minutes since it started, when there is at least one ARC app, since it
+  // might take long time to boot ARC.
+  if (arc_read_handler_)
+    return elapsed_time < kFullRestoreARCEstimateDuration;
+
+  // We estimate that full restore is still running if it has been less than
+  // one minute since it started, when Lacros is available.
+  if (lacros_read_handler_)
+    return elapsed_time < kFullRestoreLacrosEstimateDuration;
+
+  // We estimate that full restore is still running if it has been less than
+  // five seconds since it started.
+  return elapsed_time < kFullRestoreEstimateDuration;
 }
 
 void FullRestoreReadHandler::AddChromeBrowserLaunchInfoForTesting(
@@ -355,6 +377,11 @@ void FullRestoreReadHandler::OnGetRestoreData(
   if (restore_data) {
     profile_path_to_restore_data_[profile_path] = restore_data->Clone();
 
+    if (::full_restore::features::IsFullRestoreForLacrosEnabled()) {
+      lacros_read_handler_ =
+          std::make_unique<app_restore::LacrosReadHandler>(profile_path);
+    }
+
     for (auto it = restore_data->app_id_to_launch_list().begin();
          it != restore_data->app_id_to_launch_list().end(); it++) {
       const std::string& app_id = it->first;
@@ -371,6 +398,10 @@ void FullRestoreReadHandler::OnGetRestoreData(
         } else {
           window_id_to_app_restore_info_[window_id] =
               std::make_pair(profile_path, app_id);
+          // TODO(crbug.com/1239984): Remove restore data from
+          // `lacros_read_handler_` for ash browser apps.
+          if (lacros_read_handler_ && app_id != extension_misc::kChromeAppId)
+            lacros_read_handler_->AddRestoreData(app_id, window_id);
         }
       }
     }
