bea29ae3c19de315a0ff512ae12f1888b4e51182
David Sanders <dsanders11@ucsbalum.com>
2022-01-19 19:38:43 +0000

    Do IWYU for //base/containers/id_map.h
    Prerequisite for unblocking crbug.com/1288836
    
    Bug: 1288836
    Change-Id: I55768ad9de4fcdb33e79f961507a6d0d6d802aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3394006
    Reviewed-by: danakj chromium <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961073}

--- base/containers/id_map.h
+++ base/containers/id_map.h
@@ -9,12 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
-#include <memory>
-#include <set>
+#include <iterator>
+#include <ostream>
 #include <type_traits>
 #include <unordered_map>
 #include <utility>
 
+#include "base/check.h"
 #include "base/check_op.h"
 #include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
