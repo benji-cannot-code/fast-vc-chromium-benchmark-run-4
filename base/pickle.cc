6e9ea32a36e7fedd9b13f73f65c1f3ec742e194e
Nick Diego Yamane <nickdiego@igalia.com>
2022-01-19 19:02:51 +0000

    base: properly deal with pickles built with malformed buffers
    When created using ctor the accepts a raw byte array + its size [1],
    |header_| may end up being null. However there are several parts of
    Pickle code that assume it is always non-null, which may lead to
    segfaults as the one observed in the linked crbug.
    
    This follows up on https://crrev.com/c/3379541, which fixes a crash that
    happens when the copy constructor is used to to instantiate a Pickle
    built with an ill-formed buffer, in that case, caused by a bug in Lacros
    (see the linked CL for more details).
    
    R=dcheng
    
    Bug: 1271311
    Test: Covered by base_unittests.
    Change-Id: Iea8b8e66f06c40aa09c4cf47a9ac06a007b2e46b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3384744
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#961056}

--- base/pickle.cc
+++ base/pickle.cc
@@ -5,9 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/pickle.h"
 
-#include <stdlib.h>
-
 #include <algorithm>  // for max()
+#include <cstdlib>
 #include <limits>
 
 #include "base/bits.h"
@@ -25,8 +24,7 @@ static const size_t kCapacityReadOnly = static_cast<size_t>(-1);
 PickleIterator::PickleIterator(const Pickle& pickle)
     : payload_(pickle.payload()),
       read_index_(0),
-      end_index_(pickle.payload_size()) {
-}
+      end_index_(pickle.payload_size()) {}
 
 template <typename Type>
 inline bool PickleIterator::ReadBuiltinType(Type* result) {
@@ -49,7 +47,7 @@ inline void PickleIterator::Advance(size_t size) {
   }
 }
 
-template<typename Type>
+template <typename Type>
 inline const char* PickleIterator::GetReadPointerAndAdvance() {
   if (sizeof(Type) > end_index_ - read_index_) {
     read_index_ = end_index_;
@@ -270,8 +268,10 @@ Pickle::Pickle(const Pickle& other)
       header_size_(other.header_size_),
       capacity_after_header_(0),
       write_offset_(other.write_offset_) {
-  Resize(other.header_->payload_size);
-  memcpy(header_, other.header_, header_size_ + other.header_->payload_size);
+  if (other.header_) {
+    Resize(other.header_->payload_size);
+    memcpy(header_, other.header_, header_size_ + other.header_->payload_size);
+  }
 }
 
 Pickle::~Pickle() {
@@ -292,10 +292,12 @@ Pickle& Pickle::operator=(const Pickle& other) {
     header_ = nullptr;
     header_size_ = other.header_size_;
   }
-  Resize(other.header_->payload_size);
-  memcpy(header_, other.header_,
-         other.header_size_ + other.header_->payload_size);
-  write_offset_ = other.write_offset_;
+  if (other.header_) {
+    Resize(other.header_->payload_size);
+    memcpy(header_, other.header_,
+           other.header_size_ + other.header_->payload_size);
+    write_offset_ = other.write_offset_;
+  }
   return *this;
 }
 
@@ -402,7 +404,8 @@ bool Pickle::PeekNext(size_t header_size,
   return true;
 }
 
-template <size_t length> void Pickle::WriteBytesStatic(const void* data) {
+template <size_t length>
+void Pickle::WriteBytesStatic(const void* data) {
   WriteBytesCommon(data, length);
 }
 
