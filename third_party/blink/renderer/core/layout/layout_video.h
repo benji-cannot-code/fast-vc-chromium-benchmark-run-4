dab06ef589cb8843e71f3c13674a7010154a7972
Xianzhu Wang <wangxianzhu@chromium.org>
2022-01-19 15:54:19 +0000

    Revert "Don't composite LayoutVideo itself"
    This reverts commit d627fa66c962075658f316063982ed4e1bc8747c.
    
    Reason for revert: Caused memory regressions (crbug.com/1288701).
    
    Original change's description:
    > Don't composite LayoutVideo itself
    >
    > After launch of CompositeAfterPaint, we no longer need to composite
    > a replaced element itself with accelerated content. This avoids
    > unnecessary compositing of the border, background, etc. of the element.
    > The content can still be composited and recorded as a foreign layer.
    >
    > Bug: 1277748
    > Change-Id: I6cca043c18fb0bbbe849c9acd2fc935901c429b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386736
    > Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Stefan Zager <szager@chromium.org>
    > Commit-Queue: Stefan Zager <szager@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#960572}
    
    Bug: 1277748, 1288701
    Change-Id: I6429b6704e33fea9475ecabcc4ede8371edb3118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3401261
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#960970}

--- third_party/blink/renderer/core/layout/layout_video.h
+++ third_party/blink/renderer/core/layout/layout_video.h
@@ -82,6 +82,12 @@ class LayoutVideo final : public LayoutMedia {
 
   LayoutUnit MinimumReplacedHeight() const override;
 
+  bool CanHaveAdditionalCompositingReasons() const override {
+    NOT_DESTROYED();
+    return true;
+  }
+  CompositingReasons AdditionalCompositingReasons() const override;
+
   void UpdatePlayer(bool is_in_layout);
 
   LayoutSize cached_image_size_;
