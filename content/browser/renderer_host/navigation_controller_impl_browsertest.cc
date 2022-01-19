edd5883065d4ba5d4042f8a2f826dae2fa832248
Dominic Farolino <dom@chromium.org>
2022-01-19 18:18:14 +0000

    Remove the kHistoryPreventSandboxedNavigation feature
    Bug: 705583
    Change-Id: Iab3ae2135f2c31d3edc1d597b8ee240a791f5f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387371
    Reviewed-by: Charles Reis <creis@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961028}

--- content/browser/renderer_host/navigation_controller_impl_browsertest.cc
+++ content/browser/renderer_host/navigation_controller_impl_browsertest.cc
@@ -17492,11 +17492,6 @@ IN_PROC_BROWSER_TEST_P(NavigationControllerBrowserTestNoServer,
 class SandboxedNavigationControllerBrowserTest
     : public NavigationControllerBrowserTest {
  protected:
-  void SetUp() override {
-    feature_list_.InitAndEnableFeature(
-        features::kHistoryPreventSandboxedNavigation);
-    NavigationControllerBrowserTest::SetUp();
-  }
 
   void SetupNavigation() {
     NavigationControllerImpl& controller =
@@ -17614,8 +17609,7 @@ class SandboxedNavigationControllerWithBfcacheBrowserTest
   void SetUp() override {
     feature_list_.InitWithFeaturesAndParameters(
         {{features::kBackForwardCache,
-          {{"TimeToLiveInBackForwardCacheInSeconds", "3600"}}},
-         {features::kHistoryPreventSandboxedNavigation, {}}},
+          {{"TimeToLiveInBackForwardCacheInSeconds", "3600"}}}},
         // Allow BackForwardCache for all devices regardless of their memory.
         {features::kBackForwardCacheMemoryControls});
     NavigationControllerBrowserTest::SetUp();
@@ -17659,11 +17653,6 @@ IN_PROC_BROWSER_TEST_P(SandboxedNavigationControllerWithBfcacheBrowserTest,
 class SandboxedNavigationControllerPopupBrowserTest
     : public NavigationControllerBrowserTest {
  protected:
-  void SetUp() override {
-    feature_list_.InitAndEnableFeature(
-        features::kHistoryPreventSandboxedNavigation);
-    NavigationControllerBrowserTest::SetUp();
-  }
 
   void SetupNavigation() {
     EXPECT_EQ(1u, Shell::windows().size());
