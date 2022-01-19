e13be408393752f831631a078524ccbd8a29b0ea
Yuki Shiino <yukishiino@chromium.org>
2022-01-19 07:25:54 +0000

    partition_alloc: Move PartitionAlloc into the own namespaces (1 of N)
    Moves the PartitionAlloc APIs and implementations into the own
    namespaces (partition_alloc and partition_alloc::internal).
    
    This patch moves constants.
    
    Bug: 1288247
    Change-Id: Ibc50441ab9ca0f80c5305e61ca934d7a5522298d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3398311
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960816}

--- base/allocator/partition_allocator/partition_alloc_forward.h
+++ base/allocator/partition_allocator/partition_alloc_forward.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/dcheck_is_on.h"
 
-namespace base {
+namespace partition_alloc::internal {
 
 // Alignment has two constraints:
 // - Alignment requirement for scalar types: alignof(std::max_align_t)
@@ -37,6 +37,14 @@ constexpr size_t kAlignment = alignof(max_align_t);
 static_assert(kAlignment <= 16,
               "PartitionAlloc doesn't support a fundamental alignment larger "
               "than 16 bytes.");
+
+}  // namespace partition_alloc::internal
+
+namespace base {
+
+// TODO(https://crbug.com/1288247): Remove these 'using' declarations once
+// the migration to the new namespaces gets done.
+using ::partition_alloc::internal::kAlignment;
 
 namespace internal {
 
@@ -82,7 +90,8 @@ class PartitionStatsDumper;
 // kAlignment boundary. This is useful for e.g. using aligned vector
 // instructions in the constructor for zeroing.
 #if __has_attribute(assume_aligned)
-#define MALLOC_ALIGNED __attribute__((assume_aligned(base::kAlignment)))
+#define MALLOC_ALIGNED \
+  __attribute__((assume_aligned(::partition_alloc::internal::kAlignment)))
 #endif
 
 #endif  // defined(__has_attribute)
