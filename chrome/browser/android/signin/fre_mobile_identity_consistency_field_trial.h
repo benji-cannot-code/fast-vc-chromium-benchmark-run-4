12c28c9531d8c9a351870582d37f12bb11986f38
Tanmoy Mollik <triploblastic@chromium.org>
2022-01-19 20:00:44 +0000

    [FRE] Add variation strings experiment for the new signin fre screen
    This cl adds new variation strings that will be used as title and
    subtitle in the new signin fre page. These variation strings will
    constitute several arms for the signin fre experiment.
    
    The variation title and subtitles are gated by a new synthetic finch
    flag. This cl also creates the flag and the variation groups. The
    variation strings are not yet used in the UI. So there is no UI change
    introduced by this cl. The UI changes will be added in subsequent cls.
    
    String descriptions can be found in this doc:
    https://crbug.com/1276961#c48
    
    Bug: 1276961
    Change-Id: I4f42bc393fa735f4f57a1f4d7a52e668ca175875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3359617
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961085}

--- chrome/browser/android/signin/fre_mobile_identity_consistency_field_trial.h
+++ chrome/browser/android/signin/fre_mobile_identity_consistency_field_trial.h
@@ -11,8 +11,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace fre_mobile_identity_consistency_field_trial {
 
 // Returns the field trial group created in Java code.
-// The groups are created in FREFieldTrial.java.
+// The groups are created in FREMobileIdentityConsistencyFieldTrial.java.
 std::string GetFREFieldTrialGroup();
+
+// Returns whether MobileIdentityConsistencyFRESynthetic field trial is
+// enabled.
+bool IsFREFieldTrialEnabled();
+
+// Returns the variation field trial group created in Java code.
+// The groups are created in FREMobileIdentityConsistencyFieldTrial.java.
+std::string GetFREVariationsFieldTrialGroup();
 
 }  // namespace fre_mobile_identity_consistency_field_trial
 
--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeys.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeys.java
@@ -383,6 +383,9 @@ public final class ChromePreferenceKeys {
      */
     public static final String FIRST_RUN_FIELD_TRIAL_GROUP = "Chrome.FirstRun.FieldTrialEnabled";
 
+    public static final String FIRST_RUN_VARIATIONS_FIELD_TRIAL_GROUP =
+            "Chrome.FirstRun.VariationFieldTrialGroup";
+
     /**
      * The Feed articles visibility. This value is used as a pre-native cache and should be kept
      * consistent with {@link Pref.ARTICLES_LIST_VISIBLE}.
@@ -1074,6 +1077,7 @@ public final class ChromePreferenceKeys {
                 EXPLORE_OFFLINE_CONTENT_AVAILABILITY_STATUS,
                 FEED_ARTICLES_LIST_VISIBLE,
                 FIRST_RUN_FIELD_TRIAL_GROUP,
+                FIRST_RUN_VARIATIONS_FIELD_TRIAL_GROUP,
                 FIRST_RUN_SKIPPED_BY_POLICY,
                 FLAGS_CACHED.pattern(),
                 FLAGS_CRASH_STREAK_BEFORE_CACHE,
