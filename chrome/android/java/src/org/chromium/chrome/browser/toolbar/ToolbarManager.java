5f2e7a0fe0a4746d3b788e806f666c57d42aaaba
Patrick Noland <pnoland@chromium.org>
2022-01-19 17:50:20 +0000

    [toolbar] Guard against null mBookmarkBridgeSupplier
    Bug: 1287679
    
    This can be null post-destroy, which can cause a crash e.g. if a page
    load triggers a late color change.
    
    Change-Id: I83064ada81dca9d4b9aaf7c2567db1137caccfac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3400012
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961018}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -1829,6 +1829,7 @@ public class ToolbarManager implements UrlFocusChangeListener, ThemeColorObserve
     }
 
     private void updateBookmarkButtonStatus() {
+        if (mBookmarkBridgeSupplier == null) return;
         Tab currentTab = mLocationBarModel.getTab();
         BookmarkBridge bridge = mBookmarkBridgeSupplier.get();
         boolean isBookmarked =
