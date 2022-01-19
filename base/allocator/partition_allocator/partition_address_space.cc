6fa87ed25fdb8d90151c3eec2c594d3056022963
Bartek Nowierski <bartekn@chromium.org>
2022-01-19 16:07:12 +0000

    [PA] Make InitConfigurablePool use uintptr_t
    For consistency with the rest of the file...
    
    Bug: 1280844
    Change-Id: Ie950619d7a9a6e02c037f51a70d5deec9d910443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396398
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960973}

--- base/allocator/partition_allocator/partition_address_space.cc
+++ base/allocator/partition_allocator/partition_address_space.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_address_space.h"
 
 #include <array>
+#include <cstdint>
 #include <ostream>
 
 #include "base/allocator/partition_allocator/address_pool_manager.h"
@@ -124,21 +125,21 @@ void PartitionAddressSpace::Init() {
 #endif  // PA_STARSCAN_USE_CARD_TABLE
 }
 
-// TODO(bartekn): Consider void* -> uintptr_t
-void PartitionAddressSpace::InitConfigurablePool(void* address, size_t size) {
+void PartitionAddressSpace::InitConfigurablePool(uintptr_t pool_base,
+                                                 size_t size) {
   // The ConfigurablePool must only be initialized once.
   PA_CHECK(!IsConfigurablePoolInitialized());
 
   // The other Pools must be initialized first.
   Init();
 
-  PA_CHECK(address);
+  PA_CHECK(pool_base);
   PA_CHECK(size <= kConfigurablePoolMaxSize);
   PA_CHECK(size >= kConfigurablePoolMinSize);
   PA_CHECK(bits::IsPowerOfTwo(size));
-  PA_CHECK(reinterpret_cast<uintptr_t>(address) % size == 0);
+  PA_CHECK(pool_base % size == 0);
 
-  setup_.configurable_pool_base_address_ = reinterpret_cast<uintptr_t>(address);
+  setup_.configurable_pool_base_address_ = pool_base;
   setup_.configurable_pool_base_mask_ = ~(size - 1);
 
   setup_.configurable_pool_ = internal::AddressPoolManager::GetInstance()->Add(
--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <cstdint>
 #include <memory>
 
 #include "base/allocator/partition_allocator/page_allocator.h"
@@ -447,10 +448,10 @@ void V8Initializer::Initialize(IsolateHolder::ScriptMode mode,
 #endif
     // Try to reserve the maximum size of the pool at first, then keep halving
     // the size on failure until it succeeds.
-    void* pool_base = nullptr;
+    uintptr_t pool_base = 0;
     while (!pool_base && pool_size >= min_pool_size) {
-      pool_base = reinterpret_cast<void*>(sandbox_address_space->AllocatePages(
-          0, pool_size, pool_size, v8::PagePermissions::kNoAccess));
+      pool_base = sandbox_address_space->AllocatePages(
+          0, pool_size, pool_size, v8::PagePermissions::kNoAccess);
       if (!pool_base) {
         pool_size /= 2;
       }
