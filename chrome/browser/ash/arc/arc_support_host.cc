faea282190ba43046fb9e153ff42c57d0140f537
Maggie Cai <mxcai@google.com>
2022-01-19 00:05:52 +0000

    [Lacros] Use special play store extension launch interface.
    This CL changes the play store extension launch interface to a special
    launch interface in browser app launcher, so that we can remove the
    generic LaunchAppWithParams interface in browser app launcher.
    
    BUG=1244506
    
    Change-Id: Ie04a87303d85a3b394870d2e27d909d33c036ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380428
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960702}

--- chrome/browser/ash/arc/arc_support_host.cc
+++ chrome/browser/ash/arc/arc_support_host.cc
@@ -23,13 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/consent_auditor/consent_auditor_factory.h"
-#include "chrome/browser/extensions/extension_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/browser/ui/chrome_pages.h"
-#include "chrome/browser/ui/extensions/app_launch_params.h"
 #include "chrome/browser/ui/webui/chromeos/diagnostics_dialog.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
@@ -135,16 +133,9 @@ constexpr char kEventOnOpenPrivacySettingsPageClicked[] =
     "onOpenPrivacySettingsPageClicked";
 
 void RequestOpenApp(Profile* profile) {
-  const extensions::Extension* extension =
-      extensions::ExtensionRegistry::Get(profile)->GetInstalledExtension(
-          arc::kPlayStoreAppId);
-  DCHECK(extension);
-  DCHECK(extensions::util::IsAppLaunchable(arc::kPlayStoreAppId, profile));
   apps::AppServiceProxyFactory::GetForProfile(profile)
       ->BrowserAppLauncher()
-      ->LaunchAppWithParams(CreateAppLaunchParamsUserContainer(
-          profile, extension, WindowOpenDisposition::NEW_WINDOW,
-          apps::mojom::LaunchSource::kFromChromeInternal));
+      ->LaunchPlayStoreWithExtensions();
 }
 
 std::ostream& operator<<(std::ostream& os, ArcSupportHost::UIPage ui_page) {
