d78c4546826755e0674faede3982f3046a238333
Adithya Srinivasan <adithyas@chromium.org>
2022-01-19 13:05:14 +0000

    DevTools: Fix crash when obtaining permissions policy for fenced frame
    Currently in TracePermissionsPolicyBlockSource, we try to find the
    closest ancestor that has the feature enabled. However, when we try to
    do this inside a FencedFrame's frame tree, it doesn't find one as the
    root has all features disabled (and all of its children inherit this).
    This CL adds special handling for the fenced frame tree case, and adds
    a new block reason to represent a feature being blocked due to the frame
    being in a fenced frame tree.
    
    Bug: 1280320
    Change-Id: I937d4d5839934e3f7e73449577b648d56db22d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396248
    Reviewed-by: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960908}

--- third_party/blink/renderer/core/permissions_policy/permissions_policy_devtools_support.h
+++ third_party/blink/renderer/core/permissions_policy/permissions_policy_devtools_support.h
@@ -21,6 +21,9 @@ enum class PermissionsPolicyBlockReason {
   // or in iframe attribute.
   kHeader,
   kIframeAttribute,
+  // All permissions are disabled by default for fenced frames, irrespective of
+  // headers.
+  kInFencedFrameTree,
 };
 
 struct PermissionsPolicyBlockLocator {
