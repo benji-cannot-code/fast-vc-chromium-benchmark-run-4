1ca41d5194752e82486866a0e850e2400411cbd8
sauski <sauski@google.com>
2022-01-19 09:15:07 +0000

    Privacy Sandbox: Change cookie settings raw_ptr to a scoped_refptr
    The PrivacySandboxSettings service holds a raw pointer to the ref
    counted CookieSettings service. This is worked around by declaring
    a service level dependency to ensure the pointer remains valid.
    
    This approach requires that the CookieSettings factory, and all it's
    dependencies factories are created along with the
    PrivacySandboxSettings factory. Through the HostContentSettingsMap
    factory, this causes lazily initialised factories to be waited on.
    
    This is an issue for services created early in the profile lifetime,
    which may like to depend on the PrivacySandboxSettings service - as
    they now must wait for the lazily init factories.
    
    The CL changes the PrivacySandboxSettings service to hold a scoped
    refptr to the CookieSettings instead, and removes dependency
    declarations for both CookieSettings & HostContentSettingsMap.
    
    The scoped_refptr will ensure CookieSettings remains valid for the
    life of the PrivacySandboxSettings service, and as CookieSettings
    explicitly depends on HostContentSettingsMap (which is also ref
    counted, but only provides raw ptr references) it will also be
    valid.
    
    This approach is also used by the ProfileNetworkContextService.
    
    Bug: 1154686
    Change-Id: I49ce8f9e22e3d2baf0109ec6ebadf917d059ecd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3383388
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#960851}

--- chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.cc
@@ -28,10 +28,7 @@ PrivacySandboxSettings* PrivacySandboxSettingsFactory::GetForProfile(
 PrivacySandboxSettingsFactory::PrivacySandboxSettingsFactory()
     : BrowserContextKeyedServiceFactory(
           "PrivacySandboxSettings",
-          BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(HostContentSettingsMapFactory::GetInstance());
-  DependsOn(CookieSettingsFactory::GetInstance());
-}
+          BrowserContextDependencyManager::GetInstance()) {}
 
 KeyedService* PrivacySandboxSettingsFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
