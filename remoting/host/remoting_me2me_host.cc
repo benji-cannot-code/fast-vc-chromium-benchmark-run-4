7094d24c23c93f90191f34d5b676e4f6d2cfcb2f
Nicolas Avrutin <rasputin@google.com>
2022-01-19 21:03:52 +0000

    Add flag to log webrtc trace events to a file.
    BUG=b/202511979
    
    Change-Id: I5ee5150c12e49284f8af705a72bbb48e361c4de1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3399049
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Nicolas "an integer in chubby" Avrutin <rasputin@google.com>
    Cr-Commit-Position: refs/heads/main@{#961126}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -105,7 +105,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/signaling/remoting_log_to_server.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/webrtc/api/scoped_refptr.h"
+#include "third_party/webrtc/rtc_base/event_tracer.h"
 
 #if BUILDFLAG(IS_POSIX)
 #include <signal.h>
@@ -205,6 +205,10 @@ const char kHostOfflineReasonZombieStateDetected[] = "ZOMBIE_STATE_DETECTED";
 // The default email domain for Googlers. Used to determine whether the host's
 // email address is Google-internal or not.
 constexpr char kGooglerEmailDomain[] = "@google.com";
+
+// File to write webrtc trace events to. If not specified, webrtc trace events
+// will not be enabled.
+const char kWebRtcTraceEventFile[] = "webrtc-trace-event-file";
 
 }  // namespace
 
@@ -1882,14 +1886,14 @@ void HostProcess::CrashHostProcess(const std::string& function_name,
 
 int HostProcessMain() {
   HOST_LOG << "Starting host process: version " << STRINGIZE(VERSION);
+  const base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
   // Initialize Xlib for multi-threaded use, allowing non-Chromium code to
   // use X11 safely (such as the WebRTC capturer, GTK ...)
   x11::InitXlib();
 
-  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          kReportOfflineReasonSwitchName)) {
+  if (!cmd_line->HasSwitch(kReportOfflineReasonSwitchName)) {
     // Required for any calls into GTK functions, such as the Disconnect and
     // Continue windows, though these should not be used for the Me2Me case
     // (crbug.com/104377).
@@ -1904,6 +1908,12 @@ int HostProcessMain() {
   // network thread. base::GetLinuxDistro() caches the result.
   base::GetLinuxDistro();
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+
+  if (cmd_line->HasSwitch(kWebRtcTraceEventFile)) {
+    rtc::tracing::SetupInternalTracer();
+    rtc::tracing::StartInternalCapture(
+        cmd_line->GetSwitchValuePath(kWebRtcTraceEventFile).value().c_str());
+  }
 
   base::ThreadPoolInstance::CreateAndStartWithDefaultParams("Me2Me");
 
@@ -1942,6 +1952,8 @@ int HostProcessMain() {
 
   // Block until tasks blocking shutdown have completed their execution.
   base::ThreadPoolInstance::Get()->Shutdown();
+
+  rtc::tracing::ShutdownInternalTracer();
 
   return exit_code;
 }
