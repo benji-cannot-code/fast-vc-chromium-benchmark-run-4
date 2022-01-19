7d01deb5ee5823c57d769fa2530e5d05da631811
Benoit Lize <lizeb@chromium.org>
2022-01-19 19:11:44 +0000

    [PartitionAlloc] Record how long periodic memory reclaim takes
    Memory reclaim runs on the UI thread, and with the partition lock
    held. Tracking the time it takes is useful to assess whether it should
    be split, or release the lock.
    
    Bug: 998048
    Change-Id: I735381295b8ee069d4ab2a57441db6051da265b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3401639
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961061}

--- base/allocator/partition_alloc_support.cc
+++ base/allocator/partition_alloc_support.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/ignore_result.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -152,7 +153,13 @@ void RunPartitionAllocMemoryReclaimer(
     scoped_refptr<SequencedTaskRunner> task_runner) {
   TRACE_EVENT0("base", "PartitionAllocMemoryReclaimer::Reclaim()");
   auto* instance = PartitionAllocMemoryReclaimer::Instance();
-  instance->ReclaimNormal();
+
+  {
+    // Micros, since memory reclaiming should typically take at most a few ms.
+    SCOPED_UMA_HISTOGRAM_TIMER_MICROS("Memory.PartitionAlloc.MemoryReclaim");
+    instance->ReclaimNormal();
+  }
+
   TimeDelta delay =
       Microseconds(instance->GetRecommendedReclaimIntervalInMicroseconds());
   task_runner->PostDelayedTask(
