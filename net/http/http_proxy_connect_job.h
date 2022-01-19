0e51b06c6002f68612bd743585a65cebcbcd4d30
David Benjamin <davidben@chromium.org>
2022-01-19 00:10:32 +0000

    Simplify HttpProxyConnectJob state machine.
    The state machine currently branches for TCP vs TLS(+TCP) connect, with
    separate DoTransportConnectComplete and DoSSLConnectComplete states.
    However, but for having creating a different nested_connect_job_, and
    some histogram tweaks, the codepaths are about the same. Merge them
    together. Also move the SetSocket call to the state machine, which
    avoids HandleConnectResult being called in two places.
    
    This is in preparation for
    https://chromium-review.googlesource.com/c/chromium/src/+/3324884, which
    will introduce an alternate SetSocket call in between connecting to the
    proxy and setting up the tunnel. It also removes a redundant place where
    we start the tunnel timer.
    
    Bug: 1277656
    Change-Id: I903aa32312a96f0da938613f810706972a957303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3399511
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960704}

--- net/http/http_proxy_connect_job.h
+++ net/http/http_proxy_connect_job.h
@@ -156,10 +156,8 @@ class NET_EXPORT_PRIVATE HttpProxyConnectJob : public ConnectJob,
  private:
   enum State {
     STATE_BEGIN_CONNECT,
-    STATE_TCP_CONNECT,
-    STATE_TCP_CONNECT_COMPLETE,
-    STATE_SSL_CONNECT,
-    STATE_SSL_CONNECT_COMPLETE,
+    STATE_TRANSPORT_CONNECT,
+    STATE_TRANSPORT_CONNECT_COMPLETE,
     STATE_HTTP_PROXY_CONNECT,
     STATE_HTTP_PROXY_CONNECT_COMPLETE,
     STATE_SPDY_PROXY_CREATE_STREAM,
@@ -192,12 +190,9 @@ class NET_EXPORT_PRIVATE HttpProxyConnectJob : public ConnectJob,
 
   // Determine if need to go through TCP or SSL path.
   int DoBeginConnect();
-  // Connecting to HTTP Proxy
+  // Connecting to HTTP or HTTPS Proxy
   int DoTransportConnect();
   int DoTransportConnectComplete(int result);
-  // Connecting to HTTPS Proxy
-  int DoSSLConnect();
-  int DoSSLConnectComplete(int result);
 
   int DoHttpProxyConnect();
   int DoHttpProxyConnectComplete(int result);
@@ -215,8 +210,6 @@ class NET_EXPORT_PRIVATE HttpProxyConnectJob : public ConnectJob,
   // ConnectJob implementation.
   void ChangePriorityInternal(RequestPriority priority) override;
   void OnTimedOutInternal() override;
-
-  int HandleConnectResult(int result);
 
   void OnAuthChallenge();
 
