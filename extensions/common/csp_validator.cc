ca808c3ce8e2d8a415ca3b58ddd835c26ad2b31c
Francis McCabe <fgm@chromium.org>
2022-01-19 18:38:32 +0000

    Allow wasm-unsafe-eval in extensions manifest
    Bug: 1173354
    Change-Id: Icfce31706e74cf6cc9c56b814cf12304fe48504a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3367239
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Francis McCabe <fgm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961043}

--- extensions/common/csp_validator.cc
+++ extensions/common/csp_validator.cc
@@ -46,6 +46,7 @@ const char kWorkerSrc[] = "worker-src";
 const char kSelfSource[] = "'self'";
 const char kNoneSource[] = "'none'";
 const char kWasmEvalSource[] = "'wasm-eval'";
+const char kWasmUnsafeEvalSource[] = "'wasm-unsafe-eval'";
 
 const char kDirectiveSeparator = ';';
 
@@ -243,7 +244,8 @@ std::string GetSecureDirectiveValues(
 
     // We might need to relax this allowlist over time.
     if (source_lower == kSelfSource || source_lower == kNoneSource ||
-        source_lower == kWasmEvalSource || source_lower == "blob:" ||
+        source_lower == kWasmEvalSource ||
+        source_lower == kWasmUnsafeEvalSource || source_lower == "blob:" ||
         source_lower == "filesystem:" ||
         isNonWildcardTLD(source_lower, "https://", true) ||
         isNonWildcardTLD(source_lower, "chrome://", false) ||
@@ -711,7 +713,8 @@ bool DoesCSPDisallowRemoteCode(const std::string& content_security_policy,
             return true;
           }
 
-          if (source_lower == kWasmEvalSource &&
+          if ((source_lower == kWasmEvalSource ||
+               source_lower == kWasmUnsafeEvalSource) &&
               base::FeatureList::IsEnabled(
                   extensions_features::kAllowWasmInMV3)) {
             return true;
