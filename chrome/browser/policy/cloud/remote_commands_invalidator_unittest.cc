93c458b4abf0b3d0401904e201955c5599e52edf
Roland Bock <rbock@google.com>
2022-01-19 10:38:35 +0000

    Replace Invalidation::Equals with Invalidation::operator==
    This allows to rewrite SingleTopicInvalidationSet::operator==()
    trivially, which as a side effect also eliminates undefined behavior.
    
    Bug: 1287992
    Test: Existing unit tests
    Change-Id: I770e9dfcb828e00210f6ff725deeba3ea494c2ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3397925
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#960875}

--- chrome/browser/policy/cloud/remote_commands_invalidator_unittest.cc
+++ chrome/browser/policy/cloud/remote_commands_invalidator_unittest.cc
@@ -18,16 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace em = enterprise_management;
 
+using ::testing::Eq;
 using ::testing::Mock;
 using ::testing::StrictMock;
-
-namespace {
-
-MATCHER_P(InvalidationsEqual, expected_invalidation, "") {
-  return arg.Equals(expected_invalidation);
-}
-
-}  // namespace
 
 namespace policy {
 
@@ -141,9 +134,8 @@ class RemoteCommandsInvalidatorTest : public testing::Test {
   void VerifyInvalidationEnabled(const invalidation::Topic& topic) {
     EXPECT_TRUE(invalidator_.invalidations_enabled());
 
-    EXPECT_CALL(
-        invalidator_,
-        DoRemoteCommandsFetch(InvalidationsEqual(CreateInvalidation(topic))))
+    EXPECT_CALL(invalidator_,
+                DoRemoteCommandsFetch(Eq(CreateInvalidation(topic))))
         .Times(1);
     const invalidation::Invalidation invalidation = FireInvalidation(topic);
 
@@ -188,8 +180,8 @@ TEST_F(RemoteCommandsInvalidatorTest, FiredInvalidation) {
 
   // Fire the invalidation, it should be acknowledged and trigger a remote
   // commands fetch.
-  EXPECT_CALL(invalidator_, DoRemoteCommandsFetch(InvalidationsEqual(
-                                CreateInvalidation(kTestingTopic1))))
+  EXPECT_CALL(invalidator_,
+              DoRemoteCommandsFetch(Eq(CreateInvalidation(kTestingTopic1))))
       .Times(1);
   const invalidation::Invalidation invalidation2 =
       FireInvalidation(kTestingTopic1);
--- components/invalidation/impl/topic_invalidation_map_test_util.cc
+++ components/invalidation/impl/topic_invalidation_map_test_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/invalidation/impl/topic_invalidation_map_test_util.h"
 
+#include <base/containers/contains.h>
 #include <algorithm>
 
 namespace invalidation {
@@ -36,14 +37,6 @@ TopicInvalidationMapEqMatcher::TopicInvalidationMapEqMatcher(
     const TopicInvalidationMap& expected)
     : expected_(expected) {}
 
-struct InvalidationEqPredicate {
-  explicit InvalidationEqPredicate(const Invalidation& inv1) : inv1_(inv1) {}
-
-  bool operator()(const Invalidation& inv2) { return inv1_.Equals(inv2); }
-
-  const Invalidation& inv1_;
-};
-
 bool TopicInvalidationMapEqMatcher::MatchAndExplain(
     const TopicInvalidationMap& actual,
     MatchResultListener* listener) const {
@@ -57,18 +50,13 @@ bool TopicInvalidationMapEqMatcher::MatchAndExplain(
   std::vector<Invalidation> actual_only;
 
   for (const auto& expected_invalidation : expected_invalidations) {
-    if (std::find_if(actual_invalidations.begin(), actual_invalidations.end(),
-                     InvalidationEqPredicate(expected_invalidation)) ==
-        actual_invalidations.end()) {
+    if (!base::Contains(actual_invalidations, expected_invalidation)) {
       expected_only.push_back(expected_invalidation);
     }
   }
 
   for (const auto& actual_invalidation : actual_invalidations) {
-    if (std::find_if(expected_invalidations.begin(),
-                     expected_invalidations.end(),
-                     InvalidationEqPredicate(actual_invalidation)) ==
-        expected_invalidations.end()) {
+    if (!base::Contains(expected_invalidations, actual_invalidation)) {
       actual_only.push_back(actual_invalidation);
     }
   }
