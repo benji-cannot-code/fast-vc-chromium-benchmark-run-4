b204a926ee28b6c72221ad0b744bf3f6ae9e416a
Aya ElAttar <ayaelattar@chromium.org>
2022-01-19 00:00:08 +0000

    Avoid calling spec() for invalid urls
    Calling spec for GetFileSystemRootURI without checking
    whether the url is valid will trigger the DCHECK(false)
    in GURL::spec()
    
    BUG: NULL
    Change-Id: I2a6a0fae8aefe472edfc3f4fabd7d9b0aa635e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3390128
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960698}

--- storage/browser/file_system/file_system_url.cc
+++ storage/browser/file_system/file_system_url.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/common/file_system/file_system_types.h"
 #include "storage/common/file_system/file_system_util.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
+#include "url/gurl.h"
 #include "url/origin.h"
 
 namespace storage {
@@ -113,10 +114,11 @@ GURL FileSystemURL::ToGURL() const {
   if (!is_valid_)
     return GURL();
 
-  std::string url =
-      GetFileSystemRootURI(storage_key_.origin().GetURL(), mount_type_).spec();
-  if (url.empty())
+  GURL url = GetFileSystemRootURI(storage_key_.origin().GetURL(), mount_type_);
+  if (!url.is_valid())
     return GURL();
+
+  std::string url_string = url.spec();
 
   // Exactly match with DOMFileSystemBase::createFileSystemURL()'s encoding
   // behavior, where the path is escaped by KURL::encodeWithURLEscapeSequences
@@ -125,10 +127,10 @@ GURL FileSystemURL::ToGURL() const {
       virtual_path_.NormalizePathSeparatorsTo('/').AsUTF8Unsafe(),
       false /* use_plus */);
   base::ReplaceSubstringsAfterOffset(&escaped, 0, "%2F", "/");
-  url.append(escaped);
+  url_string.append(escaped);
 
   // Build nested GURL.
-  return GURL(url);
+  return GURL(url_string);
 }
 
 std::string FileSystemURL::DebugString() const {
