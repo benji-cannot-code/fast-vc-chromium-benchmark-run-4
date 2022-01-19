55bb2415aae22f7dac909ad152164f140f1d990f
anina koehler <aninak@google.com>
2022-01-19 22:01:03 +0000

    Observe the fullscreen window to handle window destruction
    Bug: 1280244
    Change-Id: I4c39a8d355bc00906b5503cfa55bdac97cae1665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386808
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Anina Koehler <aninak@google.com>
    Cr-Commit-Position: refs/heads/main@{#961152}

--- ash/session/fullscreen_notification_bubble.h
+++ ash/session/fullscreen_notification_bubble.h
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_state_observer.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
+#include "ui/aura/window.h"
+#include "ui/aura/window_observer.h"
 
 class SubtleNotificationView;
 
@@ -32,7 +35,8 @@ namespace ash {
 // A notification bubble shown when the device returns from sleep, low
 // brightness or the lock screen to remind the user that full screen mode is
 // active.
-class ASH_EXPORT FullscreenNotificationBubble : public WindowStateObserver {
+class ASH_EXPORT FullscreenNotificationBubble : public aura::WindowObserver,
+                                                public WindowStateObserver {
  public:
   FullscreenNotificationBubble();
 
@@ -48,6 +52,9 @@ class ASH_EXPORT FullscreenNotificationBubble : public WindowStateObserver {
   views::Widget* widget_for_test() { return widget_; }
 
  private:
+  // aura::WindowObserver:
+  void OnWindowDestroying(aura::Window* window) override;
+
   // WindowStateObserver:
   void OnPreWindowStateTypeChange(WindowState* window_state,
                                   chromeos::WindowStateType old_type) override;
@@ -63,13 +70,13 @@ class ASH_EXPORT FullscreenNotificationBubble : public WindowStateObserver {
   // The widget containing the bubble.
   views::Widget* widget_ = nullptr;
 
-  // The window state currently observed in order to hide the bubble if the user
-  // exits full screen mode before the timer is elapsed. It is set when the
-  // bubble is shown and reset when it is hidden.
-  WindowState* window_state_ = nullptr;
-
   // A timer to auto-dismiss the bubble after a short period of time.
   std::unique_ptr<base::OneShotTimer> timer_;
+
+  base::ScopedObservation<aura::Window, aura::WindowObserver>
+      window_observation_{this};
+  base::ScopedObservation<ash::WindowState, ash::WindowStateObserver>
+      window_state_observation_{this};
 
   base::WeakPtrFactory<FullscreenNotificationBubble> weak_ptr_factory_{this};
 };
