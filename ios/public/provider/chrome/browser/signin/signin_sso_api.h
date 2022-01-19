2ed3e49a3fa151d4c450c7562a91596baa52c3c4
Sylvain Defresne <sdefresne@chromium.org>
2022-01-19 08:45:15 +0000

    [ios] Expose `signin_sso` API as free function
    Add a protocol corresponding to a Single-Sign-On service
    and a corresponding factory free function.
    
    This is part of a multi-CL change:
     => https://crrev.com/c/3398629
      - https://crrev.com/i/4471114
      - https://crrev.com/c/3398502
      - https://crrev.com/i/4471115
      - https://crrev.com/c/3398906
      - https://crrev.com/i/4471117
    
    Bug: 1201182
    Change-Id: I4ab0be1e7ef77e6beae3272b7857a5d889edb729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3398629
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#960843}

---
+++ ios/public/provider/chrome/browser/signin/signin_sso_api.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_SIGNIN_SIGNIN_SSO_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_SIGNIN_SIGNIN_SSO_API_H_
+
+#import <Foundation/Foundation.h>
+
+// Abstract protocol of the Single-Sign-On Service used by Chrome.
+@protocol SingleSignOnService <NSObject>
+@end
+
+namespace ios {
+namespace provider {
+
+// Creates a new instance of SingleSignOnService.
+id<SingleSignOnService> CreateSSOService();
+
+}  // namespace provider
+}  // namespace ios
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_SIGNIN_SIGNIN_SSO_API_H_
