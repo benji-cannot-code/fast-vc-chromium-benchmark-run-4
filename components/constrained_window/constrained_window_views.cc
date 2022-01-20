0898a622378c37841bdb807e8a8bbc79c08d49f6
Xiyuan Xia <xiyuan@google.com>
2022-01-20 21:29:01 +0000

    Use debug alias to capture scheme/url for bad web modal UI creation
    Previously added LOG_IF(FATAL, !manager) somehow does not get
    into chrome.txt uploaded with crash reports. Use debug alias
    to capture scheme/url in the stack of minidumps.
    
    Bug: 1273287
    Change-Id: Ie3d80194352fc128331972806a203edea48d9c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402881
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961585}

--- components/constrained_window/constrained_window_views.cc
+++ components/constrained_window/constrained_window_views.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
 #include "ui/views/window/dialog_delegate.h"
+#include "url/gurl.h"
 
 #if defined(USE_OZONE)
 #include "ui/ozone/public/ozone_platform.h"
@@ -195,11 +196,16 @@ views::Widget* CreateWebModalDialogViews(views::WidgetDelegate* dialog,
   DCHECK_EQ(ui::MODAL_TYPE_CHILD, dialog->GetModalType());
   web_modal::WebContentsModalDialogManager* manager =
       web_modal::WebContentsModalDialogManager::FromWebContents(web_contents);
-  LOG_IF(FATAL, !manager) << "CreateWebModalDialogViews without a manager"
-                          << ", scheme="
-                          << web_contents->GetLastCommittedURL().scheme_piece()
-                          << ", host="
-                          << web_contents->GetLastCommittedURL().host_piece();
+
+  // TODO(http://crbug/1273287): Drop "if" and DEBUG_ALIAS_FOR_GURL after fix.
+  if (!manager) {
+    const GURL& url = web_contents->GetLastCommittedURL();
+    DEBUG_ALIAS_FOR_GURL(url_alias, url);
+    LOG_IF(FATAL, !manager)
+        << "CreateWebModalDialogViews without a manager"
+        << ", scheme=" << url.scheme_piece() << ", host=" << url.host_piece();
+  }
+
   return views::DialogDelegate::CreateDialogWidget(
       dialog, nullptr,
       manager->delegate()->GetWebContentsModalDialogHost()->GetHostView());
