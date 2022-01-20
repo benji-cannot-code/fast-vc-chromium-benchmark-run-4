8ba4700b1e67b9b4ae481c55cdd4d62bdccbc00d
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-20 12:04:30 +0000

    Profile Picker: Discard the result in ensureLazyLoaded
    The return result from customElements.whenDefined are not used, and this
    will become a type error in later TypeScript version when there's better
    typing for Promise. Fix by discarding the result and return
    Promise<void> instead.
    
    Bug: 1285794
    Test: chrome compiles
    Change-Id: I830b257288823ecb43b482cba45e66a6d710d48e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403822
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961393}

--- chrome/browser/resources/signin/profile_picker/ensure_lazy_loaded.ts
+++ chrome/browser/resources/signin/profile_picker/ensure_lazy_loaded.ts
@@ -3,24 +3,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-let lazyLoadPromise: Promise<void[]>|null = null;
+let lazyLoadPromise: Promise<void>|null = null;
 
 /**
  * @return Resolves when the lazy load module is imported.
  */
-export function ensureLazyLoaded(): Promise<void[]> {
+export function ensureLazyLoaded(): Promise<void> {
   if (!lazyLoadPromise) {
     const script = document.createElement('script');
     script.type = 'module';
     script.src = './lazy_load.js';
     document.body.appendChild(script);
 
-    lazyLoadPromise = Promise.all([
-      'profile-type-choice', 'local-profile-customization', 'profile-switch',
-      // <if expr="lacros">
-      'account-selection-lacros',
-      // </if>
-    ].map(name => customElements.whenDefined(name)));
+    lazyLoadPromise = Promise
+                          .all([
+                            'profile-type-choice',
+                            'local-profile-customization', 'profile-switch',
+                            // <if expr="lacros">
+                            'account-selection-lacros',
+                            // </if>
+                          ].map(name => customElements.whenDefined(name)))
+                          .then(() => {});
   }
   return lazyLoadPromise;
 }
