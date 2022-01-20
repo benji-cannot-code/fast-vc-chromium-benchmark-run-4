eb1d542b720bb114dfa01d16e76039bcf7a7e2ee
tby <tby@chromium.org>
2022-01-20 02:53:03 +0000

    Fix local files not appearing in Continue at startup in tablet mode
    We only return zero-state local files ('files') when a search call is
    made after the proto holding model state has been loaded from disk. In
    clamshell, we have some grace period because the user has to actually
    open the launcher. In tablet, that's not the case -- the UI almost
    always shows before the proto is read from disk. This means local files
    don't show up, and will only appear after the user searches something.
    
    This CL makes the zero-state provider set its results when the proto
    returns, so at least files will pop in when they are ready.
    
    To do this I've ported the search provider to using the MrfuCache
    class, because it has a convenient RegisterOnRead method. The algorithm
    is a bit better, too. The MrfuCache is missing a delete method though,
    which has been replaced with a todo.
    
    Bug: 1288709
    Change-Id: Iec251c85de31d77201ecac33c73fe550c1c8da7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3400781
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961291}

--- chrome/browser/ui/app_list/search/files/zero_state_file_provider.h
+++ chrome/browser/ui/app_list/search/files/zero_state_file_provider.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_manager/file_tasks_notifier.h"
 #include "chrome/browser/ash/file_manager/file_tasks_observer.h"
 #include "chrome/browser/ui/app_list/search/search_provider.h"
+#include "chrome/browser/ui/app_list/search/util/mrfu_cache.h"
+#include "chrome/browser/ui/app_list/search/util/persistent_proto.h"
 #include "chrome/browser/ui/ash/thumbnail_loader.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -33,8 +35,6 @@ using ScoredResults = std::vector<std::pair<base::FilePath, float>>;
 using ValidAndInvalidResults = std::pair<ScoredResults, Results>;
 
 }  // namespace internal
-
-class RecurrenceRanker;
 
 // ZeroStateFileProvider dispatches queries to extensions and fetches the
 // results from them via chrome.launcherSearchProvider API.
@@ -60,11 +60,13 @@ class ZeroStateFileProvider : public SearchProvider,
   // Takes a pair of vectors: <valid paths, invalid paths>, and converts the
   // valid paths to ZeroStatFilesResults and sets them as this provider's
   // results. The invalid paths are removed from the model.
-  void SetSearchResults(const internal::ValidAndInvalidResults& results);
+  void SetSearchResults(internal::ValidAndInvalidResults results);
 
   // TODO(crbug.com/1216084): Remove this after finishing developing Continue
   // Section. Appends mock results to the driver provider.
   void AppendFakeSearchResults(Results* results);
+
+  void OnProtoInitialized(ReadStatus status);
 
   // The reference to profile to get ZeroStateFileProvider service.
   Profile* const profile_;
@@ -73,7 +75,7 @@ class ZeroStateFileProvider : public SearchProvider,
 
   // The ranking model used to produce local file results for searches with an
   // empty query.
-  std::unique_ptr<RecurrenceRanker> files_ranker_;
+  std::unique_ptr<MrfuCache> files_ranker_;
 
   base::TimeTicks query_start_time_;
 
