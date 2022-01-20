73c409dc3460e1f560563086bff2cfeab40e592c
Yoav Weiss <yoavweiss@chromium.org>
2022-01-20 06:47:46 +0000

    Report post-mouseover LCP as an LCP type
    It seems like we have an issue with zoom widgets triggered by a
    mouseover event over a previous <img>-based LCP image.
    Such widgets are loading larger-than-current-LCP images, and since
    moving the mouse doesn't count as an interaction, those LCP are
    counted.
    That goes against the goal of LCP to be a stable metric that doesn't
    change based on user actions.
    
    At the same time, it's not currently clear what the impact of this is on
    CrUX results. This CL will enable us to get a better understanding of
    when this scenario happens, and what is its impact when it does.
    
    Change-Id: Ic761efa9e010cd27dc6fa17565cdd7b102013f16
    Bug: 1288027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3395315
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961341}

--- third_party/blink/renderer/core/paint/paint_timing.h
+++ third_party/blink/renderer/core/paint/paint_timing.h
@@ -159,6 +159,11 @@ class CORE_EXPORT PaintTiming final : public GarbageCollected<PaintTiming>,
 
   void OnRestoredFromBackForwardCache();
 
+  // Indicates whether a mouseover event was recently dispatched over an
+  // HTMLImageElement LCP element.
+  bool IsLCPMouseoverDispatchedRecently();
+  void SetLCPMouseoverDispatched();
+
   void Trace(Visitor*) const override;
 
  private:
@@ -221,6 +226,8 @@ class CORE_EXPORT PaintTiming final : public GarbageCollected<PaintTiming>,
   base::TimeTicks first_eligible_to_paint_;
 
   base::TimeTicks last_portal_activated_presentation_;
+
+  base::TimeTicks lcp_mouse_over_dispatch_time_;
 
   Member<FirstMeaningfulPaintDetector> fmp_detector_;
 
