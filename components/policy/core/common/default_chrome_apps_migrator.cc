771eedc2aaca69a1858cd4311bb8a5f27f56570f
Viktoriia Kovalova <vkovalova@google.com>
2022-01-20 15:17:42 +0000

    Add Maps to the list of Chrome apps that will be migrated to PWAs (for managed users).
    Change-Id: Iad29ea6c507b1e91d05b57595bd5c2cd8a2bd6c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396198
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Viktoriia Kovalova <vkovalova@google.com>
    Cr-Commit-Position: refs/heads/main@{#961451}

--- components/policy/core/common/default_chrome_apps_migrator.cc
+++ components/policy/core/common/default_chrome_apps_migrator.cc
@@ -13,7 +13,11 @@ namespace policy {
 namespace {
 
 std::map<std::string, std::string> GetChromeAppToWebAppMapping() {
-  return std::map<std::string, std::string>();
+  return std::map<std::string, std::string>({
+      // Maps
+      {"lneaknkopdijkpnocmklfnjbeapigfbh",
+       "https://www.google.com/maps?force=tt&source=ttpwa"},
+  });
 }
 
 }  // namespace
