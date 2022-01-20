d8ca67ec3b17de2a663b53239590c564478a12b3
Sophie Chang <sophiechang@chromium.org>
2022-01-20 02:49:33 +0000

    Integrate page entities model executor directly with opt guide shared library
    This also bundles the shared library into Chrome binary for release so
    it all works if build_with_internal_optimization_guide is enabled
    
    Cq-Include-Trybots: luci.chrome.try:linux-chrome
    Cq-Include-Trybots: luci.chrome.try:mac-chrome
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Cq-Include-Trybots: luci.chrome.try:win-chrome
    Bug: 1266935
    Change-Id: I4ccb4218d9cbbd8c58fc4e1e4114a1bf079011d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3331363
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961286}

--- components/optimization_guide/content/browser/page_content_annotations_model_manager.cc
+++ components/optimization_guide/content/browser/page_content_annotations_model_manager.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 
 #if BUILDFLAG(BUILD_WITH_INTERNAL_OPTIMIZATION_GUIDE)
-#include "components/optimization_guide/internal/page_entities_model_executor_impl.h"
+#include "components/optimization_guide/core/page_entities_model_executor_impl.h"
 #endif
 
 namespace optimization_guide {
