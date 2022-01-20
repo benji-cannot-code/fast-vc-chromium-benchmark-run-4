904c4762bbd9549e1f5ccbeeb68655c2cfbf4dd8
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-20 09:40:16 +0000

    CCA: Fix all errors in waitable_event.ts with strict mode
    Bug: b:172340451
    Test: tsc compiles for the file with strict: true compiler options set
    Change-Id: I0fbc5b316f86c1e776c4af8d455c90bb2aebc823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3354559
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961381}

--- ash/webui/camera_app_ui/resources/js/waitable_event.ts
+++ ash/webui/camera_app_ui/resources/js/waitable_event.ts
@@ -8,11 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 export class WaitableEvent<T = void> {
   private isSignaledInternal = false;
-  private resolve: (val: T) => void;
+  // The field is definitely assigned in the constructor since the argument to
+  // the Promise constructor is called immediately, but TypeScript can't
+  // recognize that. Disable the check by adding "!" to the property name.
+  private resolve!: (val: T) => void;
   private promise: Promise<T>;
-  /**
-   * @public
-   */
+
   constructor() {
     this.promise = new Promise((resolve) => {
       this.resolve = resolve;
@@ -29,7 +30,7 @@ export class WaitableEvent<T = void> {
   /**
    * Signals the event.
    */
-  signal(value: T|undefined): void {
+  signal(value: T): void {
     if (this.isSignaledInternal) {
       return;
     }
