b1d8739c9738ca625587a65840f85ca24cc53730
Lei Zhang <thestig@chromium.org>
2022-01-20 16:57:03 +0000

    Simplify BrowserContext::GarbageCollectStoragePartitions().
    Just pass a std::unordered_set around, and don't bother wrapping it
    inside a std::unique_ptr. Along the way, modernize / clean up its caller
    and callee.
    
    Change-Id: Ie4c451faea7e513df33c2ecfc1d48f7d32f3e271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3400550
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charles Reis <creis@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961489}

--- content/browser/storage_partition_impl_map.cc
+++ content/browser/storage_partition_impl_map.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
+#include "base/containers/contains.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -260,10 +261,10 @@ void NormalizeActivePaths(const base::FilePath& storage_root,
 void BlockingGarbageCollect(
     const base::FilePath& storage_root,
     const scoped_refptr<base::TaskRunner>& file_access_runner,
-    std::unique_ptr<std::unordered_set<base::FilePath>> active_paths) {
+    std::unordered_set<base::FilePath> active_paths) {
   CHECK(storage_root.IsAbsolute());
 
-  NormalizeActivePaths(storage_root, active_paths.get());
+  NormalizeActivePaths(storage_root, &active_paths);
 
   base::FileEnumerator enumerator(storage_root, false, kAllFileTypes);
   base::FilePath trash_directory;
@@ -274,8 +275,7 @@ void BlockingGarbageCollect(
   }
   for (base::FilePath path = enumerator.Next(); !path.empty();
        path = enumerator.Next()) {
-    if (active_paths->find(path) == active_paths->end() &&
-        path != trash_directory) {
+    if (!base::Contains(active_paths, path) && path != trash_directory) {
       // Since |trash_directory| is unique for each run of this function there
       // can be no colllisions on the move.
       base::Move(path, trash_directory.Append(path.BaseName()));
@@ -416,16 +416,14 @@ void StoragePartitionImplMap::AsyncObliterate(
 }
 
 void StoragePartitionImplMap::GarbageCollect(
-    std::unique_ptr<std::unordered_set<base::FilePath>> active_paths,
+    std::unordered_set<base::FilePath> active_paths,
     base::OnceClosure done) {
   // Include all paths for current StoragePartitions in the active_paths since
   // they cannot be deleted safely.
-  for (PartitionMap::const_iterator it = partitions_.begin();
-       it != partitions_.end();
-       ++it) {
-    const StoragePartitionConfig& config = it->first;
+  for (const auto& part : partitions_) {
+    const StoragePartitionConfig& config = part.first;
     if (!config.in_memory())
-      active_paths->insert(it->second->GetPath());
+      active_paths.insert(part.second->GetPath());
   }
 
   // Find the directory holding the StoragePartitions and delete everything in
