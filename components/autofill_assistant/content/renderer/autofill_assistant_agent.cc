bc48773bd4f6b09caa25bf4664557e8fe12fff6f
sandromaggi <sandromaggi@google.com>
2022-01-20 10:01:05 +0000

    [Autofill Assistant] Change agent interface
    This adds a "success" flag to the interface's result
    callback.
    
    Bug: b/204841212
    Change-Id: I32c40fd32c60c1aceda43894348d92453a8ed195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3399751
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#961387}

--- components/autofill_assistant/content/renderer/autofill_assistant_agent.cc
+++ components/autofill_assistant/content/renderer/autofill_assistant_agent.cc
@@ -48,7 +48,7 @@ void AutofillAssistantAgent::GetSemanticNodes(
 
   blink::WebLocalFrame* frame = render_frame()->GetWebFrame();
   if (!frame) {
-    std::move(callback).Run(nodes);
+    std::move(callback).Run(false, nodes);
     return;
   }
 
@@ -64,7 +64,7 @@ void AutofillAssistantAgent::GetSemanticNodes(
     nodes.push_back(node_data);
   }
 
-  std::move(callback).Run(nodes);
+  std::move(callback).Run(true, nodes);
 }
 
 void AutofillAssistantAgent::GetAnnotateDomModel(
