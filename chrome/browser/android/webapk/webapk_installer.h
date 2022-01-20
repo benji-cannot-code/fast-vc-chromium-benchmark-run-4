f615b4ed277e29b08be452c1654e2c6ec329adc3
Glenn Hartmann <hartmanng@chromium.org>
2022-01-20 14:54:44 +0000

    Add network traffic annotations for webapk_installer.cc
    See
    https://chromium.googlesource.com/chromium/src.git/+/refs/heads/main/docs/network_traffic_annotations.md
    for documentation about these tags.
    
    Bug: 995852
    Change-Id: I8e67cd0324a901a2ea9b640b81b7818e5d3fd20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387714
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961423}

--- chrome/browser/android/webapk/webapk_installer.h
+++ chrome/browser/android/webapk/webapk_installer.h
@@ -33,13 +33,17 @@ class BrowserContext;
 class WebContents;
 }  // namespace content
 
+namespace net {
+struct NetworkTrafficAnnotationTag;
+}  // namespace net
+
 namespace network {
 class SimpleURLLoader;
 }  // namespace network
 
 namespace webapps {
 struct ShortcutInfo;
-}
+}  // namespace webapps
 
 // The enum values are persisted to logs |WebApkInstallSpaceStatus| in
 // enums.xml, therefore they should never be reused nor renumbered.
@@ -205,7 +209,8 @@ class WebApkInstaller {
   // Sends a request to WebAPK server to create/update WebAPK. During a
   // successful request the WebAPK server responds with a token to send to
   // Google Play.
-  void SendRequest(std::unique_ptr<std::string> serialized_proto);
+  void SendRequest(const net::NetworkTrafficAnnotationTag& traffic_annotation,
+                   std::unique_ptr<std::string> serialized_proto);
 
   // Returns the WebAPK server URL based on the command line.
   GURL GetServerUrl();
