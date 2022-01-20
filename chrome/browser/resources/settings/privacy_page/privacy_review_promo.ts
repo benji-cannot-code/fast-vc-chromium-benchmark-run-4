9b53c277b7210ff7d390386d4c4265fe7b248636
Ehimare Okoyomon <eokoyomon@chromium.org>
2022-01-20 21:43:38 +0000

    Privacy Review: Add no thanks button functionality
    Show the privacy review promo depending on if it has been viewed or if
    the user clicks no thanks.
    
    Bug: 1215630
    Change-Id: I0db9351969ecc604801b9b8a94c6d1daefd407a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380471
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961610}

--- chrome/browser/resources/settings/privacy_page/privacy_review_promo.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_review_promo.ts
@@ -11,10 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {MetricsBrowserProxy, MetricsBrowserProxyImpl} from '../metrics_browser_proxy.js';
+import {PrefsMixin} from '../prefs/prefs_mixin.js';
 import {routes} from '../route.js';
 import {Router} from '../router.js';
 
-export class PrivacyReviewPromoElement extends PolymerElement {
+const PrivacyReviewPromoElementBase = PrefsMixin(PolymerElement);
+
+export class PrivacyReviewPromoElement extends PrivacyReviewPromoElementBase {
   static get is() {
     return 'settings-privacy-review-promo';
   }
@@ -23,10 +26,26 @@ export class PrivacyReviewPromoElement extends PolymerElement {
     return html`{__html_template__}`;
   }
 
-  private onPrivacyReviewClick_() {
+  static get properties() {
+    return {
+      /**
+       * Preferences state.
+       */
+      prefs: {
+        type: Object,
+        notify: true,
+      },
+    };
+  }
+
+  private onPrivacyReviewStartClick_() {
     MetricsBrowserProxyImpl.getInstance().recordAction(
         'Settings.PrivacyGuide.StartPromo');
     Router.getInstance().navigateTo(routes.PRIVACY_REVIEW);
+  }
+
+  private onNoThanksButtonClick_() {
+    this.setPrefValue('privacy_guide.viewed', true);
   }
 }
 
