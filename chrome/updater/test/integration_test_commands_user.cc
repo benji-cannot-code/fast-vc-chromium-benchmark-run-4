559bbdd8fbc25deb9b74b8662f1d834f78344003
Joshua Pawlicki <waffles@chromium.org>
2022-01-20 21:45:43 +0000

    Reland "Updater: Create self-update integration test."
    This is a reland of abb75945430a3d5f85ae3e44878032950776b03e
    
    Original change's description:
    > Updater: Create self-update integration test.
    >
    > The test is only implemented for mac Chromium-branded
    > builds at this time. This is because we haven't yet
    > set up imports of Windows or Google-branded code into
    > the codebase.
    >
    > Bug: 1268555
    > Change-Id: I4c8e1d9e6edc00cde727d9bedf7285b4588c2def
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3382966
    > Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Commit-Queue: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#961033}
    
    Bug: 1268555
    Change-Id: Ib4ec8afa7e5548715c9fdf729a2fbd7d3d7dc654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402642
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961613}

--- chrome/updater/test/integration_test_commands_user.cc
+++ chrome/updater/test/integration_test_commands_user.cc
@@ -62,6 +62,10 @@ class IntegrationTestCommandsUser : public IntegrationTestCommands {
     updater::test::EnterTestMode(url);
   }
 
+  void ExpectSelfUpdateSequence(ScopedServer* test_server) const override {
+    updater::test::ExpectSelfUpdateSequence(updater_scope_, test_server);
+  }
+
   void ExpectUpdateSequence(ScopedServer* test_server,
                             const std::string& app_id,
                             const base::Version& from_version,
@@ -88,6 +92,10 @@ class IntegrationTestCommandsUser : public IntegrationTestCommands {
 
   void SetupFakeUpdaterLowerVersion() const override {
     updater::test::SetupFakeUpdaterLowerVersion(updater_scope_);
+  }
+
+  void SetupRealUpdaterLowerVersion() const override {
+    updater::test::SetupRealUpdaterLowerVersion(updater_scope_);
   }
 
   void SetExistenceCheckerPath(const std::string& app_id,
@@ -124,6 +132,10 @@ class IntegrationTestCommandsUser : public IntegrationTestCommands {
 
   void RunWake(int exit_code) const override {
     updater::test::RunWake(updater_scope_, exit_code);
+  }
+
+  void RunWakeActive(int exit_code) const override {
+    updater::test::RunWakeActive(updater_scope_, exit_code);
   }
 
   void Update(const std::string& app_id) const override {
