ac5124fa5c355a21e14d73cb0a6f188ff586a373
wutao <wutao@chromium.org>
2022-01-20 21:30:26 +0000

    personalization: Add a toggle button to ambient sub page
    This cl only adds the html element. The real value will be read from
    ambient provider in a follow up cl.
    
          --gtest_filter= "*PersonalizationAppComponentBrowserTest*"
    
    Bug: b/213592885
    Test: browser_tests
    Change-Id: I7c9ce084e47295e557cf49b8046ca636a753017b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3399882
    Reviewed-by: Yuki Awano <yawano@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961588}

---
+++ ash/webui/personalization_app/resources/trusted/ambient/toggle_row.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview This component displays a description text and a toggle button.
+ */
+
+import 'chrome://resources/cr_elements/cr_toggle/cr_toggle.m.js';
+
+import {CrToggleElement} from 'chrome://resources/cr_elements/cr_toggle/cr_toggle.m.js';
+import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+export interface ToggleRowElement {
+  $: {toggle: CrToggleElement}
+}
+
+export class ToggleRowElement extends PolymerElement {
+  static get is() {
+    return 'toggle-row';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
+  static get properties() {
+    return {
+      checked: {type: Boolean, value: false, reflectToAttribute: true},
+      description: String,
+    };
+  }
+}
+
+customElements.define(ToggleRowElement.is, ToggleRowElement);
--- chrome/test/data/webui/chromeos/personalization_app/ambient_subpage_element_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/ambient_subpage_element_test.ts
@@ -3,9 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {AmbientSubpage} from 'chrome://personalization/trusted/ambient/ambient_subpage_element.js';
+import 'chrome://personalization/trusted/ambient/toggle_row.js';
 
-import {assertEquals} from 'chrome://webui-test/chai_assert.js';
+import {AmbientSubpage} from 'chrome://personalization/trusted/ambient/ambient_subpage_element.js';
+import {CrToggleElement} from 'chrome://resources/cr_elements/cr_toggle/cr_toggle.m.js';
+
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 import {initElement, teardownElement} from './personalization_app_test_utils.js';
 
@@ -24,5 +27,13 @@ export function AmbientSubpageTest() {
     assertEquals(
         'Ambient',
         ambientSubpageElement.shadowRoot!.querySelector('h2')!.innerText);
+
+    const toggleRow =
+        ambientSubpageElement.shadowRoot!.querySelector('toggle-row');
+    assertTrue(!!toggleRow);
+    const toggleButton =
+        toggleRow!.shadowRoot!.querySelector('cr-toggle') as CrToggleElement;
+    assertTrue(!!toggleButton);
+    assertFalse(toggleButton!.checked);
   });
 }
