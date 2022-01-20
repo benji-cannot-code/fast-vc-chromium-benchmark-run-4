e1c513bb23ffca35d2461031683f2507ffe08864
Eriko Kurimoto <elkurin@chromium.org>
2022-01-20 03:07:54 +0000

    Remove ArcAsh observer when ArcIntentHelperBridge is shutdown
    This CL changes when ArcAsh observer is removed.
    It should be removed when ArcIntentHelperBridge is shut down while it
    used to be removed when ArcIntentHelperBridge is destroyed.
    This is because KeyedService is shut down before dtor is called and
    should not be accessed after it is shut down.
    
    Change-Id: Ic850e1ae1af2788249fe1c3479b89a2eab458617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3400773
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961297}

--- components/arc/intent_helper/arc_intent_helper_bridge.h
+++ components/arc/intent_helper/arc_intent_helper_bridge.h
@@ -75,6 +75,9 @@ class ArcIntentHelperBridge : public KeyedService,
   ArcIntentHelperBridge& operator=(const ArcIntentHelperBridge&) = delete;
   ~ArcIntentHelperBridge() override;
 
+  // KeyedService:
+  void Shutdown() override;
+
   // mojom::IntentHelperHost
   void OnIconInvalidated(const std::string& package_name) override;
   void OnIntentFiltersUpdated(
