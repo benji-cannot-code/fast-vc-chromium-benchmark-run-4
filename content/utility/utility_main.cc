e88d9d5ff24b8f1197dac81201d1e418bbe1b194
Scott Violet <sky@chromium.org>
2022-01-20 21:23:34 +0000

    network-service: adds feature to change network service priority
    This adds the feature NetworkServiceIOThreadUsesDisplayThreadPriority.
    When enabled, the thread priority of the io thread (which is where
    the network service runs) is set to DISPLAY.
    
    This only impacts the desktop.
    
    It turns out on most desktop OSs renderers run with the io thread
    priority set to DISPLAY.
    
    BUG=1289300
    TEST=none
    
    Change-Id: I07ae3f821c5f957d63f9d012c847781d5b805e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402994
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961575}

--- content/utility/utility_main.cc
+++ content/utility/utility_main.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_switches_internal.h"
 #include "content/common/partition_alloc_support.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
 #include "content/public/utility/content_utility_client.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/policy/mojom/sandbox.mojom.h"
 #include "sandbox/policy/sandbox.h"
 #include "sandbox/policy/sandbox_type.h"
+#include "services/network/public/mojom/network_service.mojom.h"
 #include "services/tracing/public/cpp/trace_startup.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/icu/source/common/unicode/unistr.h"
@@ -70,6 +72,17 @@ sandbox::TargetServices* g_utility_target_services = nullptr;
 #endif
 
 namespace content {
+namespace {
+
+base::ThreadPriority GetIOThreadPriority(const std::string& utility_sub_type) {
+  return (base::FeatureList::IsEnabled(
+              features::kNetworkServiceUsesDisplayThreadPriority) &&
+          utility_sub_type == network::mojom::NetworkService::Name_)
+             ? base::ThreadPriority::DISPLAY
+             : base::ThreadPriority::NORMAL;
+}
+
+}  // namespace
 
 // Mainline routine for running as the utility process.
 int UtilityMain(MainFunctionParams parameters) {
@@ -112,13 +125,14 @@ int UtilityMain(MainFunctionParams parameters) {
   base::SingleThreadTaskExecutor main_thread_task_executor(message_pump_type);
   base::PlatformThread::SetName("CrUtilityMain");
 
+  const std::string utility_sub_type =
+      parameters.command_line->GetSwitchValueASCII(switches::kUtilitySubType);
+
   if (parameters.command_line->HasSwitch(switches::kUtilityStartupDialog)) {
     auto dialog_match = parameters.command_line->GetSwitchValueASCII(
         switches::kUtilityStartupDialog);
-    auto sub_type =
-        parameters.command_line->GetSwitchValueASCII(switches::kUtilitySubType);
-    if (dialog_match.empty() || dialog_match == sub_type) {
-      WaitForDebugger(sub_type.empty() ? "Utility" : sub_type);
+    if (dialog_match.empty() || dialog_match == utility_sub_type) {
+      WaitForDebugger(utility_sub_type.empty() ? "Utility" : utility_sub_type);
     }
   }
 
@@ -185,7 +199,7 @@ int UtilityMain(MainFunctionParams parameters) {
   g_utility_target_services = parameters.sandbox_info->target_services;
 #endif
 
-  ChildProcess utility_process;
+  ChildProcess utility_process(GetIOThreadPriority(utility_sub_type));
   GetContentClient()->utility()->PostIOThreadCreated(
       utility_process.io_task_runner());
   base::RunLoop run_loop;
