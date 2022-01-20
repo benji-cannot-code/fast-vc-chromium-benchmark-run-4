964639fb50ae240a72470ecc049c816c566f4f10
Rune Lillesveen <futhark@chromium.org>
2022-01-20 14:56:56 +0000

    [@container] Finish style recalc for legacy layout
    A sibling subtree may enforce legacy layout for a query container via a
    common ancestor. That may not be known at style recalc time. Make sure
    we finish any skipped container style recalcs during layout tree
    building since we will not be able to resume recalc during layout for
    legacy trees.
    
    Bug: 1288879
    Change-Id: If473851982a621625820054f1ed72fa97447a63a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402119
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961427}

--- third_party/blink/renderer/core/css/style_engine.h
+++ third_party/blink/renderer/core/css/style_engine.h
@@ -482,6 +482,7 @@ class CORE_EXPORT StyleEngine final : public GarbageCollected<StyleEngine>,
   void UpdateStyleAndLayoutTreeForContainer(Element& container,
                                             const LogicalSize&,
                                             LogicalAxes contained_axes);
+  void RecalcStyleForContainerDescendantsInLegacyLayoutTree(Element& container);
   void RecalcStyle();
 
   void ClearEnsuredDescendantStyles(Element& element);
@@ -659,6 +660,8 @@ class CORE_EXPORT StyleEngine final : public GarbageCollected<StyleEngine>,
   void PropagateWritingModeAndDirectionToHTMLRoot();
 
   void RecalcStyle(StyleRecalcChange, const StyleRecalcContext&);
+  void RecalcStyleForContainer(Element& container, StyleRecalcChange change);
+
   void RecalcTransitionPseudoStyle();
 
   // We may need to update whitespaces in the layout tree after a flat tree
