94aad4f03b68cb2d89855886b51435cf27ebb8f7
Mohsen Izadi <mohsen@chromium.org>
2022-01-20 21:30:07 +0000

    Remove TotalLatencyToSwapBegin breakdown from scroll EventLatency metrics
    TotalLatencyToSwapBegin breakdown measures the time from when a scroll
    event is performed by the user until the frame produced as a result of
    it is swapped on the GPU service. This is exactly what the old
    Event.Latency metrics measures and was used to make sure that the new
    metrics match the old ones. Now that the new metrics are confirmed to
    match the old one and replacing them, the breakdown is not needed
    anymore and can be cleaned up.
    
    Bug: 1287243
    Change-Id: Id62b8379a0aa6ae793502bd2e7c4b397f11c3525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3398245
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961587}

--- cc/trees/ukm_manager_unittest.cc
+++ cc/trees/ukm_manager_unittest.cc
@@ -105,7 +105,6 @@ const char kVizBreakdownLatchToSwapEnd[] =
 const char kVizBreakdownSwapEndToPresentationCompositorFrame[] =
     "SubmitCompositorFrameToPresentationCompositorFrame."
     "SwapEndToPresentationCompositorFrame";
-const char kTotalLatencyToSwapBegin[] = "TotalLatencyToSwapBegin";
 const char kTotalLatency[] = "TotalLatency";
 
 // Names of frame sequence types use in compositor latency UKM metrics (see
@@ -810,11 +809,6 @@ TEST_F(UkmManagerTest, EventLatency) {
         entry, kVizBreakdownSwapEndToPresentationCompositorFrame,
         (viz_breakdown.presentation_feedback.timestamp -
          viz_breakdown.swap_timings.swap_end)
-            .InMicroseconds());
-    test_ukm_recorder_->ExpectEntryMetric(
-        entry, kTotalLatencyToSwapBegin,
-        (viz_breakdown.swap_timings.swap_start -
-         event_dispatch_times[i].generated)
             .InMicroseconds());
     test_ukm_recorder_->ExpectEntryMetric(
         entry, kTotalLatency,
