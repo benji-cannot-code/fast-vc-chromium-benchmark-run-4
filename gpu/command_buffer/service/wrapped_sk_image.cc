fa748809600c6405b0b25a8a103e9f8407e6eecc
Peng Huang <penghuang@chromium.org>
2022-01-20 15:08:21 +0000

    RawDraw: implement the OnMemoryDump() for raw draw backing
    Bug: 1288524
    Change-Id: Ic0d6a17d1223da64040734d010672dfc18511579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3404511
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961446}

--- gpu/command_buffer/service/wrapped_sk_image.cc
+++ gpu/command_buffer/service/wrapped_sk_image.cc
@@ -68,44 +68,6 @@ SkImageInfo MakeSkImageInfo(const gfx::Size& size, viz::ResourceFormat format) {
                            kOpaque_SkAlphaType);
 }
 
-uint64_t BackendTextureTracingID(const GrBackendTexture& backend_texture) {
-  switch (backend_texture.backend()) {
-    case GrBackendApi::kOpenGL: {
-      GrGLTextureInfo tex_info;
-      if (backend_texture.getGLTextureInfo(&tex_info))
-        return tex_info.fID;
-      break;
-    }
-#if BUILDFLAG(IS_MAC)
-    case GrBackendApi::kMetal: {
-      GrMtlTextureInfo image_info;
-      if (backend_texture.getMtlTextureInfo(&image_info))
-        return reinterpret_cast<uint64_t>(image_info.fTexture.get());
-      break;
-    }
-#endif
-#if BUILDFLAG(ENABLE_VULKAN)
-    case GrBackendApi::kVulkan: {
-      GrVkImageInfo image_info;
-      if (backend_texture.getVkImageInfo(&image_info))
-        return reinterpret_cast<uint64_t>(image_info.fImage);
-      break;
-    }
-#endif
-#if BUILDFLAG(SKIA_USE_DAWN)
-    case GrBackendApi::kDawn: {
-      GrDawnTextureInfo tex_info;
-      if (backend_texture.getDawnTextureInfo(&tex_info))
-        return reinterpret_cast<uint64_t>(tex_info.fTexture.Get());
-      break;
-    }
-#endif
-    default:
-      break;
-  }
-  return 0;
-}
-
 class WrappedSkImage : public ClearTrackingSharedImageBacking {
  public:
   WrappedSkImage(base::PassKey<WrappedSkImageFactory>,
@@ -330,7 +292,7 @@ class WrappedSkImage : public ClearTrackingSharedImageBacking {
     }
 
     promise_texture_ = SkPromiseImageTexture::Make(backend_texture_);
-    tracing_id_ = BackendTextureTracingID(backend_texture_);
+    tracing_id_ = GrBackendTextureTracingID(backend_texture_);
 
     return true;
   }
@@ -370,7 +332,7 @@ class WrappedSkImage : public ClearTrackingSharedImageBacking {
       SetCleared();
 
     promise_texture_ = SkPromiseImageTexture::Make(backend_texture_);
-    tracing_id_ = BackendTextureTracingID(backend_texture_);
+    tracing_id_ = GrBackendTextureTracingID(backend_texture_);
 
     return true;
   }
