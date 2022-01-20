6b122a1fa6e41b8cec51a91e2ab6847554fe334e
Daniel Cheng <dcheng@chromium.org>
2022-01-20 00:16:01 +0000

    Convert remaining WARN_UNUSED_RESULT to [[nodiscard]] in //content.
    Bug: 1287045
    Change-Id: Ied3bf1e46b233f4d9026bb0cd7167d6ce7d6a88c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402402
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961201}

--- content/public/test/browser_test_utils.cc
+++ content/public/test/browser_test_utils.cc
@@ -158,21 +158,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
-// Specifying a prototype so that we can add the WARN_UNUSED_RESULT attribute.
+// Executes the passed |script| in the frame specified by |render_frame_host|.
+// If |result| is not NULL, stores the value that the evaluation of the script
+// in |result|.  Returns true on success.
 [[nodiscard]] bool ExecuteScriptHelper(RenderFrameHost* render_frame_host,
                                        const std::string& script,
                                        bool user_gesture,
                                        int32_t world_id,
-                                       std::unique_ptr<base::Value>* result);
-
-// Executes the passed |script| in the frame specified by |render_frame_host|.
-// If |result| is not NULL, stores the value that the evaluation of the script
-// in |result|.  Returns true on success.
-bool ExecuteScriptHelper(RenderFrameHost* render_frame_host,
-                         const std::string& script,
-                         bool user_gesture,
-                         int32_t world_id,
-                         std::unique_ptr<base::Value>* result) {
+                                       std::unique_ptr<base::Value>* result) {
   // TODO(lukasza): Only get messages from the specific |render_frame_host|.
   DOMMessageQueue dom_message_queue(render_frame_host);
 
