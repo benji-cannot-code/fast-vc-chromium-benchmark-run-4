0d5e4c1207e2b5f401ae51db490b35415d08488e
Tibor Goldschwendt <tiborg@chromium.org>
2022-01-20 03:37:39 +0000

    [ntp][modules] Fix viewed tags of recipe module in all languages
    Currently, the viewed tags of the recipes module is not correct in all
    languages. Examples of incorrect tags are:
    * (German) "vor 1 Jahr angesehen" should be "Vor 1 Jahr angesehen"
    * (Spanish) "Visto Hace 1 año" should be "Visto hace 1 año"
    * (French) "Vu Il y a 1 an" should be "Vu il y a 1 an"
    
    The root issue is that we piece the tag together from a template string
    and a dynamically localized, relative timestamp. The individual pieces
    do not always agree on the capitalization, creating the issue.
    
    This CL fixes the issue by limiting the number of possible tag strings
    to a handful, which are all individually translated as a unit. We then
    map any given timestamp to one of those tag strings. This makes the tag
    less precise but correct in all languages.
    
    Fixed: 1286060
    Change-Id: I090be23d49fb3a901f04faecb589f51e79fe7f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403488
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961303}

---
+++ chrome/browser/new_tab_page/modules/task_module/time_format_util.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NEW_TAB_PAGE_MODULES_TASK_MODULE_TIME_FORMAT_UTIL_H_
+#define CHROME_BROWSER_NEW_TAB_PAGE_MODULES_TASK_MODULE_TIME_FORMAT_UTIL_H_
+
+#include <string>
+
+// Turns the |viewed_timestamp| Unix Epoch timestamp of a task visit into a
+// human-readable, localized string indicating when this task was viewed (e.g.
+// "Viewed today", "Viewed in the past week").
+std::string GetViewedItemText(int viewed_timestamp);
+
+#endif  // CHROME_BROWSER_NEW_TAB_PAGE_MODULES_TASK_MODULE_TIME_FORMAT_UTIL_H_
