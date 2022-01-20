09fceedde2a959010c38d4e4ff780a5bd4a47295
Mitsuru Oshima <oshima@chromium.org>
2022-01-20 20:34:21 +0000

    Call OnDragExited upon window destruction.
    Call the OnDragExited in OnWindowDestroying instead of Destroyed,
    to give a drag source a chance to perform proper cleanup.
    
    Bug: 1287539
    Change-Id: I51bbccf99b8ec0b4ef44e351ff8f2441df4e0564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387570
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961556}

--- ash/drag_drop/drag_drop_controller_unittest.cc
+++ ash/drag_drop/drag_drop_controller_unittest.cc
@@ -786,6 +786,9 @@ TEST_F(DragDropControllerTest, DragCanceledThenWindowDestroyedDuringDragDrop) {
   AddViewToWidgetAndResize(widget.get(), drag_view);
   aura::Window* window = widget->GetNativeView();
 
+  EventTargetTestDelegate delegate(window);
+  aura::client::SetDragDropDelegate(window, &delegate);
+
   ui::test::EventGenerator generator(Shell::GetPrimaryRootWindow(),
                                      widget->GetNativeView());
   generator.PressLeftButton();
@@ -819,6 +822,8 @@ TEST_F(DragDropControllerTest, DragCanceledThenWindowDestroyedDuringDragDrop) {
   // Drag must have been canceled.
   EXPECT_TRUE(drag_drop_controller_->drag_canceled_);
   EXPECT_FALSE(drag_drop_controller_->drop_received_);
+
+  EXPECT_EQ(EventTargetTestDelegate::State::kDragExitInvoked, delegate.state());
 }
 
 TEST_F(DragDropControllerTest, WindowDestroyedDuringDragDrop) {
@@ -826,6 +831,9 @@ TEST_F(DragDropControllerTest, WindowDestroyedDuringDragDrop) {
   DragTestView* drag_view = new DragTestView;
   AddViewToWidgetAndResize(widget.get(), drag_view);
   aura::Window* window = widget->GetNativeView();
+
+  EventTargetTestDelegate delegate(window);
+  aura::client::SetDragDropDelegate(window, &delegate);
 
   ui::test::EventGenerator generator(Shell::GetPrimaryRootWindow(),
                                      widget->GetNativeView());
@@ -853,7 +861,6 @@ TEST_F(DragDropControllerTest, WindowDestroyedDuringDragDrop) {
   RunWithClosure(base::BindLambdaForTesting(loop_task));
   EXPECT_TRUE(dragged);
   EXPECT_FALSE(GetDragWindow());
-  LOG(ERROR) << "N=" << n;
   EXPECT_GT(n, 50);
   generator.ReleaseLeftButton();
 
@@ -861,6 +868,7 @@ TEST_F(DragDropControllerTest, WindowDestroyedDuringDragDrop) {
   // Drag must have been canceled.
   EXPECT_TRUE(drag_drop_controller_->drag_canceled_);
   EXPECT_FALSE(drag_drop_controller_->drop_received_);
+  EXPECT_EQ(EventTargetTestDelegate::State::kDragExitInvoked, delegate.state());
 }
 
 TEST_F(DragDropControllerTest, SyntheticEventsDuringDragDrop) {
