8440ac6b0180b6df6c038cd898917c304f549579
Morten Stenshorne <mstensho@chromium.org>
2022-01-20 14:55:44 +0000

    Legacy fallback - make sure that we walk far enough up the ancestry.
    We cannot stop looking for a legacy fallback root, just because we've
    found something that was marked earlier. It may be that an ancestor has
    become a fragmentation context root since last time.
    
    Add two crash tests. The first one being more true to the original
    fuzzer test, while the other one is simplified (note that the DCHECK
    failure happens somewhere else for this one).
    
    Bug: 1288769
    Change-Id: I979774cd57bb5811d02268f5a073ae44bae2d175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3404079
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961424}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4863,11 +4863,6 @@ bool Element::ForceLegacyLayoutInFragmentationContext(
         !parent->GetComputedStyle()
              ->InsideFragmentationContextWithNondeterministicEngine())
       break;
-
-    // Otherwise, if this element is always marked for legacy fallback, we can
-    // bail.
-    if (legacy_root->ShouldForceLegacyLayoutForChild())
-      return false;
   }
 
   // Only mark for reattachment if needed. Unnecessary reattachments may lead to
