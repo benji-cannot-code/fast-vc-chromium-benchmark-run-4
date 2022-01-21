2bdb2d096845bdfe9d83cb5663c2a7b33e97ff2b
Pi-Hsun Shih <pihsun@chromium.org>
2022-01-21 10:22:42 +0000

    CCA: Fix all errors in models/file_system_access_entry.ts with strict mode
    Bug: b:172340451
    Test: tsc compiles for the file with strict: true compiler options set
    Change-Id: Idcaa176b0708d7d95cb544d49da1488eeee2f331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3354561
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961851}

--- ash/webui/camera_app_ui/resources/js/models/file_system_access_entry.ts
+++ ash/webui/camera_app_ui/resources/js/models/file_system_access_entry.ts
@@ -177,6 +177,7 @@ export class DirectoryAccessEntryImpl implements DirectoryAccessEntry {
       await this.getFile(name);
       return true;
     } catch (e) {
+      assert(e instanceof Error);
       if (e.name === 'NotFoundError') {
         return false;
       }
@@ -189,7 +190,7 @@ export class DirectoryAccessEntryImpl implements DirectoryAccessEntry {
   }
 
   async createFile(name: string): Promise<FileAccessEntry> {
-    return createFileJobs.push(async () => {
+    const file = await createFileJobs.push(async () => {
       let uniqueName = name;
       for (let i = 0; await this.isExist(uniqueName);) {
         uniqueName = name.replace(/^(.*?)(?=\.)/, `$& (${++i})`);
@@ -198,6 +199,11 @@ export class DirectoryAccessEntryImpl implements DirectoryAccessEntry {
           await this.handle.getFileHandle(uniqueName, {create: true});
       return new FileAccessEntry(handle, this);
     });
+    // The createFileJobs is never cancelled, so the return file should never
+    // be null.
+    // TODO(b/215662731): Remove this after we have better AsyncJobQueue type.
+    assert(file !== null);
+    return file;
   }
 
   async getDirectory({name, createIfNotExist}:
@@ -209,7 +215,8 @@ export class DirectoryAccessEntryImpl implements DirectoryAccessEntry {
       assert(handle !== null);
       return new DirectoryAccessEntryImpl(handle);
     } catch (error) {
-      if (!createIfNotExist && error.name === 'NotFoundError') {
+      if (!createIfNotExist && error instanceof Error &&
+          error.name === 'NotFoundError') {
         return null;
       }
       throw error;
