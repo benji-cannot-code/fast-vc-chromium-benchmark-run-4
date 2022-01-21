e42d06c9ba4d535274fbf61bc54bfbbc63bc4d36
Brian Malcolm <bmalcolm@chromium.org>
2022-01-21 20:51:43 +0000

    Loosen the json->proto checks
    Not every proto field (especially network fields) will exist for
    every device. Loosen the checks here and depend on the checks which
    take the proto and turn it into a MediaSink to detect any failures for
    network / capabilities.
    
    BUG=b:204572061
    
    Change-Id: Ia4104f27b22b9756704cad89842f4edd0cafb009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405224
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Tyler Garrett <tylergarrett@chromium.org>
    Commit-Queue: Brian Malcolm <bmalcolm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962072}

--- chrome/browser/media/router/discovery/access_code/access_code_cast_discovery_interface_unittest.cc
+++ chrome/browser/media/router/discovery/access_code/access_code_cast_discovery_interface_unittest.cc
@@ -79,6 +79,26 @@ const char kEndpointResponseSuccess[] =
         }
       }
     })";
+// videoOut and ipV6Address are missing, but that should be ok
+const char kEndpointResponseSuccessPartialData[] =
+    R"({
+      "device": {
+        "displayName": "test_device",
+        "id": "1234",
+        "deviceCapabilities": {
+          "videoIn": true,
+          "audioOut": true,
+          "audioIn": true,
+          "devMode": true
+        },
+        "networkInfo": {
+          "hostName": "GoogleNet",
+          "port": "666",
+          "ipV4Address": "192.0.2.146"
+        }
+      }
+    })";
+// networkInfo is missing
 const char kEndpointResponseFieldsMissing[] =
     R"({
       "device": {
@@ -90,10 +110,6 @@ const char kEndpointResponseFieldsMissing[] =
           "audioOut": true,
           "audioIn": true,
           "devMode": true
-        },
-        "networkInfo": {
-          "hostName": "GoogleNet",
-          "port": "666",
         }
       }
     })";
@@ -294,6 +310,29 @@ TEST_F(AccessCodeCastDiscoveryInterfaceTest, ServerResponseSucess) {
 
   DiscoveryDevice discovery_device_proto =
       media_router::BuildDiscoveryDeviceProto();
+
+  EXPECT_CALL(mock_callback,
+              Run(DiscoveryDeviceProtoEquals(discovery_device_proto),
+                  AddSinkResultCode::OK));
+
+  stub_interface()->ValidateDiscoveryAccessCode(mock_callback.Get());
+  base::RunLoop().RunUntilIdle();
+}
+
+TEST_F(AccessCodeCastDiscoveryInterfaceTest,
+       ServerResponseSucessWithPartialData) {
+  // Test to validate that a successful server response is propagated from
+  // the discovery interface and all fields are set in the returned proto.
+  SetEndpointFetcherMockResponse(GURL(kMockEndpoint),
+                                 kEndpointResponseSuccessPartialData,
+                                 net::HTTP_OK, net::OK);
+
+  MockDiscoveryDeviceCallback mock_callback;
+
+  DiscoveryDevice discovery_device_proto =
+      media_router::BuildDiscoveryDeviceProto();
+  discovery_device_proto.mutable_device_capabilities()->clear_video_out();
+  discovery_device_proto.mutable_network_info()->clear_ip_v6_address();
 
   EXPECT_CALL(mock_callback,
               Run(DiscoveryDeviceProtoEquals(discovery_device_proto),
