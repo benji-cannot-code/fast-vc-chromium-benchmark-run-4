27928248ba87df83502ec20c89860e19468d8e15
ckitagawa <ckitagawa@chromium.org>
2022-01-21 16:28:59 +0000

    [Tab Management] Close All Tabs Modal Dialog
    Adds a confirmation Modal Dialog for the "Close all tabs" action in the
    app menu. This should reduce accidental close all tabs presses.
    
    Put the feature behind a flag for finch trials and record metrics for
    running an experiment.
    
    Follow-ups:
    * Get UX approved strings.
    
    Bug: 1287281
    Change-Id: I30e91f9697dae9e55e9a7dd6aab6a62d6acf39e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3387102
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Yashar Dabiran <yashard@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Yashar Dabiran <yashard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961927}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -169,6 +169,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kBackgroundThreadPool,
     &kBookmarkBottomSheet,
     &kCastDeviceFilter,
+    &kCloseAllTabsModalDialog,
     &kCloseTabSuggestions,
     &kCriticalPersistedTabData,
     &kCCTBackgroundTab,
@@ -436,6 +437,9 @@ const base::Feature kConditionalTabStripAndroid{
 // Used in downstream code.
 const base::Feature kCastDeviceFilter{"CastDeviceFilter",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kCloseAllTabsModalDialog{"CloseAllTabsModalDialog",
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kCloseTabSuggestions{"CloseTabSuggestions",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
