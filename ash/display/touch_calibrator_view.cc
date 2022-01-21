3d45b5ca4140f827f6d621b1d6bba981774b1472
Mitsuru Oshima <oshima@chromium.org>
2022-01-21 18:53:00 +0000

    Revert "Convert TouchCalibratorView to use AnimationBuilder for animations."
    This is partial revert of commit 3f0d175e3774497244956e988659ee17cdf6cbb7 (crrev.com/c/3158791)
    Reason for revert: it's causing crbug.com/1289414.
    
    Bug: b:214598600, crbug.com/1289414, crbug.com/1236069
    Change-Id: I235aa79ad3de69e9aa9048078a006be574a736aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407656
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962002}

--- ash/display/touch_calibrator_view.cc
+++ ash/display/touch_calibrator_view.cc
@@ -10,16 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
-#include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "ui/aura/window.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/compositor/layer.h"
+#include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/animation/throb_animation.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/strings/grit/ui_strings.h"
-#include "ui/views/animation/animation_builder.h"
 #include "ui/views/background.h"
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/label.h"
@@ -104,6 +103,18 @@ gfx::Size GetSizeForString(const std::u16string& text,
   int height = 0, width = 0;
   gfx::Canvas::SizeStringInt(text, font_list, &width, &height, 0, 0);
   return gfx::Size(width, height);
+}
+
+void AnimateLayerToPosition(views::View* view,
+                            base::TimeDelta duration,
+                            gfx::Point end_position,
+                            float opacity = 1.f) {
+  ui::ScopedLayerAnimationSettings slide_settings(view->layer()->GetAnimator());
+  slide_settings.SetPreemptionStrategy(
+      ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
+  slide_settings.SetTransitionDuration(duration);
+  view->SetBoundsRect(gfx::Rect(end_position, view->size()));
+  view->layer()->SetOpacity(opacity);
 }
 
 }  // namespace
@@ -514,6 +525,7 @@ void TouchCalibratorView::InitViewContents() {
   touch_point_view_->SetVisible(false);
   touch_point_view_->SetPaintToLayer();
   touch_point_view_->layer()->SetFillsBoundsOpaquely(false);
+  touch_point_view_->layer()->GetAnimator()->AddObserver(this);
   touch_point_view_->SetBackground(
       views::CreateSolidBackground(SK_ColorTRANSPARENT));
 
@@ -582,6 +594,7 @@ void TouchCalibratorView::InitViewContents() {
   completion_message_view_->SetVisible(false);
   completion_message_view_->SetPaintToLayer();
   completion_message_view_->layer()->SetFillsBoundsOpaquely(false);
+  completion_message_view_->layer()->GetAnimator()->AddObserver(this);
   completion_message_view_->SetBackground(
       views::CreateSolidBackground(SK_ColorTRANSPARENT));
 }
@@ -639,6 +652,38 @@ void TouchCalibratorView::AnimationEnded(const gfx::Animation* animation) {
       break;
   }
 }
+
+void TouchCalibratorView::OnLayerAnimationStarted(
+    ui::LayerAnimationSequence* sequence) {}
+
+void TouchCalibratorView::OnLayerAnimationEnded(
+    ui::LayerAnimationSequence* sequence) {
+  switch (state_) {
+    case ANIMATING_1_TO_2:
+      state_ = DISPLAY_POINT_2;
+      tap_label_->SetVisible(true);
+      break;
+    case ANIMATING_2_TO_3:
+      state_ = DISPLAY_POINT_3;
+      break;
+    case ANIMATING_3_TO_4:
+      state_ = DISPLAY_POINT_4;
+      break;
+    case ANIMATING_FINAL_MESSAGE:
+      state_ = CALIBRATION_COMPLETE;
+      break;
+    default:
+      break;
+  }
+}
+
+void TouchCalibratorView::OnLayerAnimationAborted(
+    ui::LayerAnimationSequence* sequence) {
+  OnLayerAnimationEnded(sequence);
+}
+
+void TouchCalibratorView::OnLayerAnimationScheduled(
+    ui::LayerAnimationSequence* sequence) {}
 
 void TouchCalibratorView::AdvanceToNextState() {
   // Stop any previous animations and skip them to the end.
@@ -764,45 +809,6 @@ void TouchCalibratorView::SkipCurrentAnimation() {
       touch_point_view_->layer()->GetAnimator()->is_animating()) {
     touch_point_view_->layer()->GetAnimator()->StopAnimating();
   }
-}
-
-void TouchCalibratorView::OnStateAnimationEnded() {
-  switch (state_) {
-    case ANIMATING_1_TO_2:
-      state_ = DISPLAY_POINT_2;
-      tap_label_->SetVisible(true);
-      break;
-    case ANIMATING_2_TO_3:
-      state_ = DISPLAY_POINT_3;
-      break;
-    case ANIMATING_3_TO_4:
-      state_ = DISPLAY_POINT_4;
-      break;
-    case ANIMATING_FINAL_MESSAGE:
-      state_ = CALIBRATION_COMPLETE;
-      break;
-    default:
-      break;
-  }
-}
-
-void TouchCalibratorView::AnimateLayerToPosition(views::View* view,
-                                                 base::TimeDelta duration,
-                                                 gfx::Point end_position,
-                                                 float opacity) {
-  views::AnimationBuilder()
-      .SetPreemptionStrategy(
-          ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET)
-      // base::Unretained is safe here since the lifetime of the animated views
-      // is tied to the TouchCalibratorView itself.
-      .OnEnded(base::BindOnce(&TouchCalibratorView::OnStateAnimationEnded,
-                              base::Unretained(this)))
-      .OnAborted(base::BindOnce(&TouchCalibratorView::OnStateAnimationEnded,
-                                base::Unretained(this)))
-      .Once()
-      .SetDuration(duration)
-      .SetBounds(view, gfx::Rect(end_position, view->size()))
-      .SetOpacity(view, opacity);
 }
 
 }  // namespace ash
