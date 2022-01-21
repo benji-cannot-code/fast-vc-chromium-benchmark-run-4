ed67d99fa8a54b44c17d9362d4e21c2fc7a895e2
Hiroki Sato <hirokisato@google.com>
2022-01-21 04:40:17 +0000

    Make sure |isScrolling| is set false on exiting AutoScrollHandler
    There is a case where a node is scrollable but performing an action
    returns false, e.g. when the node is already at the end of the scroll.
    When this happens, previous implementation forgot to set |isScrolling|
    back to false. This change fixes it.
    
    AX-Relnotes: n/a
    
    Bug: 827351
    Test: ChromeVoxAutoScrollHandlerTest
    Test: RecyclerView test app works well.
    Change-Id: I4eb0c17e85b5c17ef54a066443dc7f7aad83bc57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403873
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961792}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/auto_scroll_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/auto_scroll_handler.js
@@ -125,6 +125,7 @@ AutoScrollHandler = class {
         }
       });
       if (!scrollResult) {
+        this.isScrolling_ = false;
         ChromeVoxState.instance.navigateToRange(target, false, speechProps);
         return;
       }
@@ -215,7 +216,7 @@ AutoScrollHandler = class {
 
       // Usual case. Retry navigation with a refreshed tree.
       retryCommandFunc();
-    })().catch(e => {
+    })().finally(() => {
       this.isScrolling_ = false;
     });
 
