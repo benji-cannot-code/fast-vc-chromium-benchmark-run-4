8035240b4d57372e0b3bce043762f1d4e7011fbc
Ayu Ishii <ayui@chromium.org>
2022-01-21 19:52:30 +0000

    Remove unused ScopedFeatureList in FileSystemBrowserTest
    Change-Id: I7f53e88e7ccd73a39ce89a75eecc3669b569e6bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405421
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962039}

--- content/browser/file_system/file_system_browsertest.cc
+++ content/browser/file_system/file_system_browsertest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/task/single_thread_task_runner.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/thread_test_helper.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_context.h"
@@ -66,7 +65,6 @@ class FileSystemBrowserTest : public ContentBrowserTest,
  protected:
   bool is_incognito_;
   raw_ptr<Shell> browser_ = nullptr;
-  base::test::ScopedFeatureList feature_list_;
 };
 
 INSTANTIATE_TEST_SUITE_P(All, FileSystemBrowserTest, ::testing::Bool());
