be0546e3a91f2a69f7af6697c87cb1453563d9d8
Darren W <djw@chromium.org>
2022-01-21 02:05:14 +0000

    Add a feature flag for allowing AppBanner to use the BFCache.
    Pipes the flag through to the renderer, which uses it to decide if being
    eligible for AppBanner should block a page from BFCache.
    
    Also parameterises the browser test so we can test the flag.
    
    Bug: 1276864
    Change-Id: I7c32dda0bb375a664ccec1e1f763532090425d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380432
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Darren Willis <djw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961747}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1188,5 +1188,8 @@ const base::Feature kLazyInitializeTimeZoneController{
 const base::Feature kCompositedCaret{"CompositedCaret",
                                      base::FEATURE_ENABLED_BY_DEFAULT};
 
+const base::Feature kBackForwardCacheAppBanner{
+    "BackForwardCacheAppBanner", base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace features
 }  // namespace blink
