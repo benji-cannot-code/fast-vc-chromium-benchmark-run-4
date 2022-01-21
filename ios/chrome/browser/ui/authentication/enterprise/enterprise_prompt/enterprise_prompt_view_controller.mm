87884d77e5f530255d3c206da016a80695daf46f
Ewann <ewannpv@google.com>
2022-01-21 16:27:07 +0000

    [iOS][Enterprise][4/N] Unify prompts
    This CL adds the 'learn more' secondary action to the restrict account
    signed out prompt.
    
    Bug: 1261423
    Change-Id: Ic610fda571679763f2210b873667bf213c220a6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3401041
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961924}

--- ios/chrome/browser/ui/authentication/enterprise/enterprise_prompt/enterprise_prompt_view_controller.mm
+++ ios/chrome/browser/ui/authentication/enterprise/enterprise_prompt/enterprise_prompt_view_controller.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/authentication/enterprise/enterprise_prompt/enterprise_prompt_view_controller.h"
 
-#include "base/notreached.h"
 #include "ios/chrome/grit/ios_google_chrome_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util_mac.h"
@@ -43,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.primaryActionString =
       l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_CONTINUE);
+  self.secondaryActionString =
+      l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_LEARN_MORE);
 
   switch (self.promptType) {
     case EnterprisePromptTypeRestrictAccountSignedOut:
@@ -81,8 +82,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.titleString = l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT);
   self.subtitleString =
       l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_SUBTEXT);
-  self.secondaryActionString =
-      l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_LEARN_MORE);
 }
 
 // Updates the view with sync disabled informations.
@@ -90,8 +89,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.titleString = l10n_util::GetNSString(IDS_IOS_SYNC_SYNC_DISABLED);
   self.subtitleString =
       l10n_util::GetNSString(IDS_IOS_SYNC_SYNC_DISABLED_DESCRIPTION);
-  self.secondaryActionString =
-      l10n_util::GetNSString(IDS_IOS_ENTERPRISE_SIGNED_OUT_LEARN_MORE);
 }
 
 @end
