7cebfe183710348c58d3da434451d6698c3a6167
Kevin Marshall <kmarshall@chromium.org>
2022-01-21 02:33:24 +0000

    [fuchsia] Make FileSystemApiTests build on Fuchsia.
    Enables building of FileSystemApiTest tests on Fuchsia.
    Does not imply that the feature nor the tests actually run; that will
    be revisited in a followup bug/CL.
    
    Bug: 1271639
    Change-Id: I6c2e861cc6e15f6b1fa6db11dc68b01948583db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403351
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961758}

--- chrome/browser/extensions/api/file_system/file_system_apitest.cc
+++ chrome/browser/extensions/api/file_system/file_system_apitest.cc
@@ -358,7 +358,7 @@ IN_PROC_BROWSER_TEST_F(FileSystemApiTest,
   CheckStoredDirectoryMatches(base::FilePath());
 }
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_POSIX)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_POSIX) || BUILDFLAG(IS_FUCHSIA)
 IN_PROC_BROWSER_TEST_F(FileSystemApiTest,
                        FileSystemApiOpenDirectoryOnGraylistAndAllowTest) {
   base::FilePath test_file = TempFilePath("open_existing.txt", true);
@@ -440,7 +440,7 @@ IN_PROC_BROWSER_TEST_F(FileSystemApiTest,
       << message_;
   CheckStoredDirectoryMatches(test_file);
 }
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_POSIX)
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_POSIX) || BUILDFLAG(IS_FUCHSIA)
 
 IN_PROC_BROWSER_TEST_F(FileSystemApiTest,
     FileSystemApiInvalidChooseEntryTypeTest) {
