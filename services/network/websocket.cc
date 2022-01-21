d6169733333e86fd8398dbaf509780643d7e9899
Danil Somsikov <dsv@chromium.org>
2022-01-21 02:02:55 +0000

    Allow ResumeDataPipeReading to proceed if it not interrupted with any resume reason.
    This addresses the problem of websocket being stuck if message is being
    sent async over the channel, when OnSendDataFrameDone callback is effectively invoking OnReadable which uses kMojoPipe as a resume reason although the websocket is not interrupted.
    
    Bug: 1286909
    Change-Id: I5695dbf9f0f07aee45a330e2eda0070f01bceda5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3404081
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961746}

--- services/network/websocket.cc
+++ services/network/websocket.cc
@@ -744,7 +744,8 @@ void WebSocket::OnReadable(MojoResult result,
 }
 
 void WebSocket::ReadAndSendFromDataPipe(InterruptionReason resume_reason) {
-  if (outgoing_frames_interrupted_ != resume_reason)
+  if (outgoing_frames_interrupted_ != resume_reason &&
+      outgoing_frames_interrupted_ != InterruptionReason::kNone)
     return;
 
   if (outgoing_frames_interrupted_ != InterruptionReason::kNone)
