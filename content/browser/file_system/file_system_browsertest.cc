1441f153c0a0275899e9c49aa36e1ba620629fa9
Victor Costan <pwnall@chromium.org>
2022-01-21 22:08:56 +0000

    File System: Fix race condition in browser test.
    Before this CL, FileSystemBrowserTestWithLowQuota::SetLowQuota() posts a
    task on the IO thread to configure QuotaManager, but returns before the
    task completes. This means that the task is racing with other test
    steps. This adds unnecessary complexity to the test's timing.
    
    Change-Id: Iffe38bcbd66365a69891e37e350467173dfc2f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407934
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962107}

--- content/browser/file_system/file_system_browsertest.cc
+++ content/browser/file_system/file_system_browsertest.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
+#include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/test/bind.h"
 #include "base/test/thread_test_helper.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_context.h"
@@ -81,21 +83,23 @@ class FileSystemBrowserTestWithLowQuota : public FileSystemBrowserTest {
   }
 
   static void SetLowQuota(scoped_refptr<QuotaManager> qm) {
-    if (!BrowserThread::CurrentlyOn(BrowserThread::IO)) {
-      GetIOThreadTaskRunner({})->PostTask(
-          FROM_HERE,
-          base::BindOnce(&FileSystemBrowserTestWithLowQuota::SetLowQuota, qm));
-      return;
-    }
-    DCHECK_CURRENTLY_ON(BrowserThread::IO);
-    // These sizes must correspond with expectations in html and js.
-    const int kMeg = 1000 * 1024;
-    storage::QuotaSettings settings;
-    settings.pool_size = 25 * kMeg;
-    settings.per_host_quota = 5 * kMeg;
-    settings.must_remain_available = 10 * kMeg;
-    settings.refresh_interval = base::TimeDelta::Max();
-    qm->SetQuotaSettings(settings);
+    DCHECK(!BrowserThread::CurrentlyOn(BrowserThread::IO));
+
+    base::RunLoop run_loop;
+    GetIOThreadTaskRunner({})->PostTaskAndReply(
+        FROM_HERE, base::BindLambdaForTesting([&]() {
+          DCHECK_CURRENTLY_ON(BrowserThread::IO);
+          // These sizes must correspond with expectations in html and js.
+          const int kMeg = 1000 * 1024;
+          storage::QuotaSettings settings;
+          settings.pool_size = 25 * kMeg;
+          settings.per_host_quota = 5 * kMeg;
+          settings.must_remain_available = 10 * kMeg;
+          settings.refresh_interval = base::TimeDelta::Max();
+          qm->SetQuotaSettings(settings);
+        }),
+        run_loop.QuitClosure());
+    run_loop.Run();
   }
 };
 
