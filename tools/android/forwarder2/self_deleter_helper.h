fea47a0ba6a7b2292fccd40e63a5d73d1cf54556
Avi Drissman <avi@chromium.org>
2022-01-21 17:11:16 +0000

    Use std::ignore rather than ignore_result in tools/
    Bug: 1285085
    Change-Id: Ia281082836da088bf3cbc79928a838f692c528e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3374339
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961941}

--- tools/android/forwarder2/self_deleter_helper.h
+++ tools/android/forwarder2/self_deleter_helper.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/check_op.h"
-#include "base/ignore_result.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
@@ -88,7 +87,7 @@ namespace forwarder2 {
 //     DCHECK_EQ(object_, object);
 //     // Do some extra work with |object| before it gets deleted...
 //     object_.reset();
-//     ignore_result(object.release());
+//     std::ignore = object.release();
 //   }
 //
 //   base::ThreadChecker thread_checker_;
