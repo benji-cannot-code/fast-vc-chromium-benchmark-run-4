9b31c9c204ce7f1c7f8acdee118f2c19765b407d
Christoph Schwering <schwering@google.com>
2022-01-21 01:11:30 +0000

    [Autofill] Support multiple expectedValues in captured-site tests.
    This CL extends the JSON format for captured-site tests with
    "expectedValues" in addition to "expectedValue". This is intended
    to allow for nondeterministic behaviour (due to race conditions with
    JS).
    
    The CL also updates the flaky collage and customink tests accordingly.
    
    Bug: 1287988, 1172848, 1287986
    Change-Id: I6faabb71208b7285028b8bcbfd6575e3f574dc26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3395555
    Reviewed-by: Ian Struiksma <ianstruiksma@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#961724}

--- chrome/browser/autofill/captured_sites_test_utils.h
+++ chrome/browser/autofill/captured_sites_test_utils.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "base/strings/strcat.h"
+#include "base/types/strong_alias.h"
 #include "base/values.h"
 #include "chrome/browser/ui/browser.h"
 #include "components/autofill/core/browser/test_autofill_clock.h"
@@ -284,6 +285,8 @@ class TestRecipeReplayer {
                                    const gfx::Point& point);
 
  private:
+  using IgnoreCase = base::StrongAlias<struct IgnoreCaseTag, bool>;
+
   static bool GetIFrameOffsetFromIFramePath(
       const std::vector<std::string>& iframe_path,
       content::RenderFrameHost* frame,
@@ -370,13 +373,13 @@ class TestRecipeReplayer {
                           const std::string& element_xpath,
                           const std::string& get_property_function_body,
                           std::string* property);
-  bool ExpectElementPropertyEquals(
+  bool ExpectElementPropertyEqualsAnyOf(
       const content::ToRenderFrameHost& frame,
       const std::string& element_xpath,
       const std::string& get_property_function_body,
-      const std::string& expected_value,
+      const std::vector<std::string>& expected_value,
       const std::string& validation_field,
-      const bool ignoreCase = false);
+      IgnoreCase ignore_case = IgnoreCase(false));
   void SimulateKeyPressWrapper(content::WebContents* web_contents,
                                ui::DomKey key);
   void NavigateAwayAndDismissBeforeUnloadDialog();
