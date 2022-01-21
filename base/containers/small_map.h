de9c2938c59c5b5dfdbf8efdeb3f778a36cc3abb
Bruce Dawson <brucedawson@chromium.org>
2022-01-21 08:40:11 +0000

    Remove anomymous namespace from containers header
    There is no need to put constants in header files in anonymous
    namespaces. This change undoes that and adds inline (for constant
    folding) for clarity of intent.
    
    Bug: 1287650
    Change-Id: Ib344e6d6cdd938a81bb642e380c0a3182b7d03c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3406141
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961828}

--- base/containers/small_map.h
+++ base/containers/small_map.h
@@ -16,9 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/memory/raw_ptr.h"
 
-namespace {
-constexpr size_t kUsingFullMapSentinel = std::numeric_limits<size_t>::max();
-}  // namespace
+constexpr inline size_t kUsingFullMapSentinel =
+    std::numeric_limits<size_t>::max();
 
 namespace base {
 
