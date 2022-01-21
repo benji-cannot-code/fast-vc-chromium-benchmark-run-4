fea451f0f41cc26ac16d72eba6b0aacbd944c0d2
Daniel Andersson <dandersson@chromium.org>
2022-01-21 23:06:34 +0000

    desks templates: Removing "manual" text elision from DesksTextfield.
    The DesksTextfield class is a base class used for desk names, and desk
    template names. It has functionality to ellipsize text that is too big
    to fit the text field. This has traditionally been done by intercepting
    and actually modifying the text that the TextfieldModel has.
    
    With this change, we'll instead use RenderText's capability to elide
    text on the fly. The benefit of this is that the text model always holds
    the *actual* text, rather than something with inserted ellipsis (…).
    
    Bug: 1282314
    Test: Manually tested.
    Change-Id: Ib6d2db59c6614aabff0bb7bfbab1bdc2b38a3cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405456
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962128}

--- ash/wm/desks/templates/desks_templates_name_view.cc
+++ ash/wm/desks/templates/desks_templates_name_view.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_grid.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/gfx/text_elider.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/widget/widget.h"
@@ -66,13 +65,6 @@ void DesksTemplatesNameView::CommitChanges(views::Widget* widget) {
 
 void DesksTemplatesNameView::OnContentsChanged() {
   PreferredSizeChanged();
-}
-
-void DesksTemplatesNameView::SetTextAndElideIfNeeded(
-    const std::u16string& text) {
-  SetText(gfx::ElideText(text, GetFontList(), GetAvailableWidth(),
-                         gfx::ELIDE_TAIL));
-  full_text_ = text;
 }
 
 gfx::Size DesksTemplatesNameView::CalculatePreferredSize() const {
