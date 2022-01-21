7e7cbc4539cbe16bc236eb3b6a437b5440f6f27e
Daniel Murphy <dmurph@chromium.org>
2022-01-21 01:15:58 +0000

    Revert "Remove WARN_UNUSED_RESULT from //base."
    This reverts commit 8193a6cb85debec6bff939185e46029bcf0917c5.
    
    Reason for revert: Broke build: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20Full/205730/overview due to recent commit: https://chromium-review.googlesource.com/c/chromium/src/+/3397372
    
    Original change's description:
    > Remove WARN_UNUSED_RESULT from //base.
    >
    > Any remaining references/uses have been migrated to [[nodiscard]],
    > outside of ignore_result.h, which is being separately deleted.
    >
    > Fixed: 1287045
    > Change-Id: Ic61cc9cd9df30b6d1766d98192d182de2c3743cf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405291
    > Reviewed-by: danakj chromium <danakj@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#961710}
    
    Change-Id: I01ecc58c7601db8b4092df486eec453fe38f3ba6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405552
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961729}

--- base/task/sequence_manager/task_queue.h
+++ base/task/sequence_manager/task_queue.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/common/checked_lock.h"
 #include "base/task/sequence_manager/lazy_now.h"
@@ -423,8 +424,8 @@ class BASE_EXPORT TaskQueue : public RefCountedThreadSafe<TaskQueue> {
   // not be a null callback. Must be called on the thread this task queue is
   // associated with, and the handle returned must be destroyed on the same
   // thread.
-  [[nodiscard]] std::unique_ptr<OnTaskPostedCallbackHandle>
-  AddOnTaskPostedHandler(OnTaskPostedHandler handler);
+  std::unique_ptr<OnTaskPostedCallbackHandle> AddOnTaskPostedHandler(
+      OnTaskPostedHandler handler) WARN_UNUSED_RESULT;
 
   // Set a callback to fill trace event arguments associated with the task
   // execution.
