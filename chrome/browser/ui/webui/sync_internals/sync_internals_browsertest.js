e0e445fb92d2753b731cbed94f644f521b86b4be
Xiaohan Wang <xhwang@chromium.org>
2022-01-21 19:19:22 +0000

    chrome/browser/ui/*.js: Use BUILDFLAG for OS checking
    Use BUILDFLAG(IS_XXX) instead of defined(OS_XXX).
    
    Generated by `os_buildflag_migration.py` (https://crrev.com/c/3311983)
    with modifications to handle .js files.
    
    R=thakis@chromium.org
    
    Bug: 1234043
    Change-Id: Ifdc9f636f6500e76f6e980e9194edbb1317ff4f8
    Test: No functionality change
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407313
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962017}

--- chrome/browser/ui/webui/sync_internals/sync_internals_browsertest.js
+++ chrome/browser/ui/webui/sync_internals/sync_internals_browsertest.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+GEN('#include "build/build_config.h"');
 GEN('#include "components/sync/driver/sync_driver_switches.h"');
 GEN('#include "content/public/test/browser_test.h"');
 
@@ -229,7 +230,7 @@ TEST_F('SyncInternalsWebUITest', 'Uninitialized', function() {
   assertNotEquals(null, getAboutInfoForTest());
 });
 
-GEN('#if defined(OS_CHROMEOS)');
+GEN('#if BUILDFLAG(IS_CHROMEOS)');
 
 // Sync should be disabled if there was no primary account set.
 TEST_F('SyncInternalsWebUITest', 'SyncDisabledByDefaultChromeOS', function() {
@@ -254,7 +255,7 @@ TEST_F('SyncInternalsWebUITest', 'SyncDisabledByDefault', function() {
   expectTrue(this.hasInDetails(true, 'Username', ''));
 });
 
-GEN('#endif  // defined(OS_CHROMEOS)');
+GEN('#endif');
 
 TEST_F('SyncInternalsWebUITest', 'LoadPastedAboutInfo', function() {
   // Expose the text field.
