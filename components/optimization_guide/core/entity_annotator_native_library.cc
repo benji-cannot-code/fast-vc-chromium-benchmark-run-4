422e056918462bf97c705cc886df433f456ac8e4
Sophie Chang <sophiechang@chromium.org>
2022-01-21 22:54:24 +0000

    Add DISABLE_CFI_ICALL to all entity_annotator_native_library functions since they are calling dlsym-ed functions
    Bug: 1289734
    Change-Id: I74b2b23fc1bf85595ef699f979e0d896b653aa53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3408183
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962125}

--- components/optimization_guide/core/entity_annotator_native_library.cc
+++ components/optimization_guide/core/entity_annotator_native_library.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/entity_annotator_native_library.h"
 
 #include "base/base_paths.h"
+#include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
@@ -17,6 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/bundle_locations.h"
 #include "base/mac/foundation_util.h"
 #endif
+
+// IMPORTANT: All functions in this file that call dlsym()'ed
+// functions should be annotated with DISABLE_CFI_ICALL.
 
 namespace optimization_guide {
 
@@ -99,6 +103,7 @@ EntityAnnotatorNativeLibrary::Create() {
   return nullptr;
 }
 
+DISABLE_CFI_ICALL
 void EntityAnnotatorNativeLibrary::LoadFunctions() {
   get_max_supported_feature_flag_func_ =
       reinterpret_cast<GetMaxSupportedFeatureFlagFunc>(
@@ -189,6 +194,7 @@ void EntityAnnotatorNativeLibrary::LoadFunctions() {
               "dex"));
 }
 
+DISABLE_CFI_ICALL
 bool EntityAnnotatorNativeLibrary::IsValid() const {
   return get_max_supported_feature_flag_func_ && create_from_options_func_ &&
          get_creation_error_func_ && delete_func_ &&
@@ -209,6 +215,7 @@ bool EntityAnnotatorNativeLibrary::IsValid() const {
          entity_metadata_get_human_readable_category_score_at_index_func_;
 }
 
+DISABLE_CFI_ICALL
 int32_t EntityAnnotatorNativeLibrary::GetMaxSupportedFeatureFlag() {
   DCHECK(IsValid());
   if (!IsValid()) {
@@ -218,6 +225,7 @@ int32_t EntityAnnotatorNativeLibrary::GetMaxSupportedFeatureFlag() {
   return get_max_supported_feature_flag_func_();
 }
 
+DISABLE_CFI_ICALL
 void* EntityAnnotatorNativeLibrary::CreateEntityAnnotator(
     const ModelInfo& model_info) {
   DCHECK(IsValid());
@@ -242,6 +250,7 @@ void* EntityAnnotatorNativeLibrary::CreateEntityAnnotator(
   return entity_annotator;
 }
 
+DISABLE_CFI_ICALL
 bool EntityAnnotatorNativeLibrary::PopulateEntityAnnotatorOptionsFromModelInfo(
     void* options,
     const ModelInfo& model_info) {
@@ -322,6 +331,7 @@ bool EntityAnnotatorNativeLibrary::PopulateEntityAnnotatorOptionsFromModelInfo(
   return true;
 }
 
+DISABLE_CFI_ICALL
 void EntityAnnotatorNativeLibrary::DeleteEntityAnnotator(
     void* entity_annotator) {
   DCHECK(IsValid());
@@ -332,6 +342,7 @@ void EntityAnnotatorNativeLibrary::DeleteEntityAnnotator(
   delete_func_(reinterpret_cast<void*>(entity_annotator));
 }
 
+DISABLE_CFI_ICALL
 absl::optional<std::vector<ScoredEntityMetadata>>
 EntityAnnotatorNativeLibrary::AnnotateText(void* annotator,
                                            const std::string& text) {
@@ -362,6 +373,7 @@ EntityAnnotatorNativeLibrary::AnnotateText(void* annotator,
   return scored_md;
 }
 
+DISABLE_CFI_ICALL
 absl::optional<EntityMetadata>
 EntityAnnotatorNativeLibrary::GetEntityMetadataForEntityId(
     void* annotator,
@@ -387,6 +399,7 @@ EntityAnnotatorNativeLibrary::GetEntityMetadataForEntityId(
   return md;
 }
 
+DISABLE_CFI_ICALL
 EntityMetadata EntityAnnotatorNativeLibrary::
     GetEntityMetadataFromOptimizationGuideEntityMetadata(
         const void* og_entity_metadata) {
