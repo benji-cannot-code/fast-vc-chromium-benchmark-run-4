d93573f002d2e526646ce160f433232c3619fca0
Avi Drissman <avi@chromium.org>
2022-01-21 18:43:09 +0000

    Use std::ignore rather than ignore_result in skia/
    Bug: 1285085
    Change-Id: I6761c19ca289b66637bcf9bce83918280a47e8d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3374336
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961993}

--- skia/ext/SkMemory_new_handler.cpp
+++ skia/ext/SkMemory_new_handler.cpp
@@ -6,14 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdlib.h>
 
+#include <tuple>
+
 #include "base/debug/alias.h"
-#include "base/ignore_result.h"
 #include "base/process/memory.h"
 #include "build/build_config.h"
 #include "third_party/skia/include/core/SkTypes.h"
 #include "third_party/skia/include/private/SkMalloc.h"
 
-#if defined(OS_WIN)
+#if BUILDFLAG(IS_WIN)
 #include <windows.h>
 #endif
 
@@ -79,16 +80,16 @@ static void* malloc_nothrow(size_t size) {
   // TODO(b.kelemen): we should always use UncheckedMalloc but currently it
   // doesn't work as intended everywhere.
   void* result;
-#if  defined(OS_IOS)
-    result = malloc(size);
+#if BUILDFLAG(IS_IOS)
+  result = malloc(size);
 #else
-    // It's the responsibility of the caller to check the return value.
-    ignore_result(base::UncheckedMalloc(size, &result));
+  // It's the responsibility of the caller to check the return value.
+  std::ignore = base::UncheckedMalloc(size, &result);
 #endif
-    if (result) {
-        prevent_overcommit(0x47, size, result);
-    }
-    return result;
+  if (result) {
+    prevent_overcommit(0x47, size, result);
+  }
+  return result;
 }
 
 static void* calloc_throw(size_t size) {
@@ -99,16 +100,16 @@ static void* calloc_nothrow(size_t size) {
   // TODO(b.kelemen): we should always use UncheckedCalloc but currently it
   // doesn't work as intended everywhere.
   void* result;
-#if  defined(OS_IOS)
-    result = calloc(1, size);
+#if BUILDFLAG(IS_IOS)
+  result = calloc(1, size);
 #else
-    // It's the responsibility of the caller to check the return value.
-    ignore_result(base::UncheckedCalloc(size, 1, &result));
+  // It's the responsibility of the caller to check the return value.
+  std::ignore = base::UncheckedCalloc(size, 1, &result);
 #endif
-    if (result) {
-        prevent_overcommit(0, size, result);
-    }
-    return result;
+  if (result) {
+    prevent_overcommit(0, size, result);
+  }
+  return result;
 }
 
 void* sk_malloc_flags(size_t size, unsigned flags) {
