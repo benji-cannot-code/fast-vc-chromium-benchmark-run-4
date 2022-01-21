810143822085638a301d290eb1ea7c3efed53d65
Hazem Ashmawy <hazems@chromium.org>
2022-01-21 09:46:52 +0000

    Replace PureJavaExceptionReporter with ChromePureJavaExceptionReporter
    PureJavaExceptionReporter is moved to components/crash and is being
    replaced by ChromePureJavaExceptionReporter for the chrome browser.
    
    Downstream dependency has been resolved so deleting the class and
    migrating all upstream usage of it to the new class.
    
    Bug: 1081916
    Change-Id: Icff042c8cb6d1a3090c1a84ffddbf0f5cb4548eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402223
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#961842}

--- chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeSurveyController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeSurveyController.java
@@ -26,7 +26,7 @@ import org.chromium.base.task.AsyncTask;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.crash.PureJavaExceptionReporter;
+import org.chromium.chrome.browser.crash.ChromePureJavaExceptionReporter;
 import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -249,7 +249,7 @@ public class ChromeSurveyController implements InfoBarAnimationListener {
                 Log.w(TAG, logMessage);
                 PostTask.postTask(TaskTraits.BEST_EFFORT_MAY_BLOCK,
                         ()
-                                -> PureJavaExceptionReporter.reportJavaException(
+                                -> ChromePureJavaExceptionReporter.reportJavaException(
                                         new Throwable(logMessage)));
                 return;
             }
@@ -314,7 +314,7 @@ public class ChromeSurveyController implements InfoBarAnimationListener {
                             Log.w(TAG, logMessage);
                             PostTask.postTask(TaskTraits.BEST_EFFORT_MAY_BLOCK,
                                     ()
-                                            -> PureJavaExceptionReporter.reportJavaException(
+                                            -> ChromePureJavaExceptionReporter.reportJavaException(
                                                     new Throwable(logMessage)));
                             mMessageDispatcher.dismissMessage(
                                     message, DismissReason.DISMISSED_BY_FEATURE);
--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -46,7 +46,7 @@ import org.chromium.chrome.browser.compositor.layouts.LayoutManagerImpl;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchManager;
-import org.chromium.chrome.browser.crash.PureJavaExceptionReporter;
+import org.chromium.chrome.browser.crash.ChromePureJavaExceptionReporter;
 import org.chromium.chrome.browser.directactions.DirectActionInitializer;
 import org.chromium.chrome.browser.download.DownloadManagerService;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
@@ -1048,7 +1048,7 @@ public class RootUiCoordinator
                     mActivity.getWindow().getDecorView().findViewById(R.id.menu_anchor_stub));
             AppMenuCoordinatorFactory.setExceptionReporter(
                     (throwable)
-                            -> PureJavaExceptionReporter.reportJavaException(
+                            -> ChromePureJavaExceptionReporter.reportJavaException(
                                     (Throwable) throwable));
 
             mAppMenuCoordinator.registerAppMenuBlocker(this);
@@ -1144,7 +1144,8 @@ public class RootUiCoordinator
                 () -> mActivity.findViewById(R.id.sheet_container));
         BottomSheetControllerFactory.setExceptionReporter(
                 (throwable)
-                        -> PureJavaExceptionReporter.reportJavaException((Throwable) throwable));
+                        -> ChromePureJavaExceptionReporter.reportJavaException(
+                                (Throwable) throwable));
         BottomSheetControllerFactory.attach(mWindowAndroid, mBottomSheetController);
 
         mBottomSheetManager = new BottomSheetManager(mBottomSheetController, mActivityTabProvider,
