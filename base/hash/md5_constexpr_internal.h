6c255d4235d21bc0b7cfe88a669f077860aa68a2
Lei Zhang <thestig@chromium.org>
2022-01-21 20:16:14 +0000

    Remove unused base::MD5SumConstexpr().
    They have no callers except in unit tests.
    
    Change-Id: I8701b24931038bb50fb42cf4065f607e18646056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405457
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962053}

--- base/hash/md5_constexpr_internal.h
+++ base/hash/md5_constexpr_internal.h
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_HASH_MD5_CONSTEXPR_INTERNAL_H_
 #define BASE_HASH_MD5_CONSTEXPR_INTERNAL_H_
 
-#include <array>
-#include <cstddef>
-#include <cstdint>
+#include <stdint.h>
 
-#include "base/check.h"
-#include "base/hash/md5.h"
+#include <array>
+
+#include "base/check_op.h"
 
 namespace base {
 namespace internal {
@@ -238,27 +237,6 @@ struct MD5CE {
   //////////////////////////////////////////////////////////////////////////////
   // HELPER FUNCTIONS
 
-  // Converts an IntermediateData to a final digest.
-  static constexpr MD5Digest IntermediateDataToMD5Digest(
-      const IntermediateData& intermediate) {
-    return MD5Digest{{static_cast<uint8_t>((intermediate.a >> 0) & 0xff),
-                      static_cast<uint8_t>((intermediate.a >> 8) & 0xff),
-                      static_cast<uint8_t>((intermediate.a >> 16) & 0xff),
-                      static_cast<uint8_t>((intermediate.a >> 24) & 0xff),
-                      static_cast<uint8_t>((intermediate.b >> 0) & 0xff),
-                      static_cast<uint8_t>((intermediate.b >> 8) & 0xff),
-                      static_cast<uint8_t>((intermediate.b >> 16) & 0xff),
-                      static_cast<uint8_t>((intermediate.b >> 24) & 0xff),
-                      static_cast<uint8_t>((intermediate.c >> 0) & 0xff),
-                      static_cast<uint8_t>((intermediate.c >> 8) & 0xff),
-                      static_cast<uint8_t>((intermediate.c >> 16) & 0xff),
-                      static_cast<uint8_t>((intermediate.c >> 24) & 0xff),
-                      static_cast<uint8_t>((intermediate.d >> 0) & 0xff),
-                      static_cast<uint8_t>((intermediate.d >> 8) & 0xff),
-                      static_cast<uint8_t>((intermediate.d >> 16) & 0xff),
-                      static_cast<uint8_t>((intermediate.d >> 24) & 0xff)}};
-  }
-
   static constexpr uint32_t StringLength(const char* string) {
     const char* end = string;
     while (*end != 0)
@@ -278,10 +256,6 @@ struct MD5CE {
   //////////////////////////////////////////////////////////////////////////////
   // WRAPPER FUNCTIONS
 
-  static constexpr MD5Digest Sum(const char* data, uint32_t n) {
-    return IntermediateDataToMD5Digest(ProcessMessage(data, n));
-  }
-
   static constexpr uint64_t Hash64(const char* data, uint32_t n) {
     IntermediateData intermediate = ProcessMessage(data, n);
     return (static_cast<uint64_t>(SwapEndian(intermediate.a)) << 32) |
@@ -297,14 +271,6 @@ struct MD5CE {
 }  // namespace internal
 
 // Implementations of the functions exposed in the public header.
-
-constexpr MD5Digest MD5SumConstexpr(const char* string) {
-  return internal::MD5CE::Sum(string, internal::MD5CE::StringLength(string));
-}
-
-constexpr MD5Digest MD5SumConstexpr(const char* string, uint32_t length) {
-  return internal::MD5CE::Sum(string, length);
-}
 
 constexpr uint64_t MD5Hash64Constexpr(const char* string) {
   return internal::MD5CE::Hash64(string, internal::MD5CE::StringLength(string));
--- third_party/blink/renderer/modules/peerconnection/media_stream_track_metrics.cc
+++ third_party/blink/renderer/modules/peerconnection/media_stream_track_metrics.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <inttypes.h>
 #include <string>
 
-//#include "base/hash/md5.h"
+#include "base/hash/md5.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_checker.h"
 #include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
