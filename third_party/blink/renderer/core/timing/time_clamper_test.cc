41bfca92cccefc063ef2027759a6087778a8ccf8
Justin Schuh <jschuh@chromium.org>
2022-01-21 23:29:35 +0000

    Reland "Convert base/time to ClampedNumeric"
    This is a reland of 1ee775afba8b163ebc7b7ed2cd63db95ca846486
    
    Original change's description:
    > Convert base/time to ClampedNumeric
    >
    > This simplifies the code and should marginally reduce binary size and
    > improve performance (particularly on Arm architectures). The code should
    > otherwise perform identically and be covered by existing tests.
    >
    > Change-Id: I084d87d9109278eb0559521af988978900b4267e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389079
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#960596}
    
    Change-Id: I509f5eca4537b1346e530670beff91eaec94a142
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3408028
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Justin Schuh <jschuh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962136}

--- third_party/blink/renderer/core/timing/time_clamper_test.cc
+++ third_party/blink/renderer/core/timing/time_clamper_test.cc
@@ -93,7 +93,7 @@ TEST(TimeClamperTest, ClampingIsPerInstance) {
                      .InMicroseconds()) >= 1) {
       break;
     }
-    time_microseconds += kIntervalInMicroseconds;
+    time_microseconds += 1;
   }
 }
 
