0c8d7fc7b1eae64ec3f0343b7527d87f51cadf25
Elaine Chien <elainechien@chromium.org>
2022-01-21 20:41:57 +0000

    [Media Tabs] Add metrics for Media Tabs.
    This CL adds metrics to track
    - how often users close media tabs.
    - how often users switch to media tabs.
    - the number of media tabs present when the Tab Search bubble is opened.
    
    Bug: 1261515
    Change-Id: I225bc1d10ed738832e076901139cbc1513872fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3376303
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Olivier Li Shing Tat-Dupuis <olivierli@google.com>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962064}

--- chrome/browser/resources/tab_search/tab_search_api_proxy.ts
+++ chrome/browser/resources/tab_search/tab_search_api_proxy.ts
@@ -24,7 +24,9 @@ export enum TabSwitchAction {
 }
 
 export interface TabSearchApiProxy {
-  closeTab(tabId: number, withSearch: boolean, closedTabIndex: number): void;
+  closeTab(
+      tabId: number, withSearch: boolean, isMediaTab: boolean,
+      closedTabIndex: number): void;
 
   getProfileData(): Promise<{profileData: ProfileData}>;
 
@@ -32,7 +34,7 @@ export interface TabSearchApiProxy {
       id: number, withSearch: boolean, isTab: boolean, index: number): void;
 
   switchToTab(
-      info: SwitchToTabInfo, withSearch: boolean,
+      info: SwitchToTabInfo, withSearch: boolean, isMediaTab: boolean,
       switchedTabIndex: number): void;
 
   getCallbackRouter(): PageCallbackRouter;
@@ -53,11 +55,17 @@ export class TabSearchApiProxyImpl implements TabSearchApiProxy {
         this.handler.$.bindNewPipeAndPassReceiver());
   }
 
-  closeTab(tabId: number, withSearch: boolean, closedTabIndex: number) {
+  closeTab(
+      tabId: number, withSearch: boolean, isMediaTab: boolean,
+      closedTabIndex: number) {
     chrome.metricsPrivate.recordSmallCount(
         withSearch ? 'Tabs.TabSearch.WebUI.IndexOfCloseTabInFilteredList' :
                      'Tabs.TabSearch.WebUI.IndexOfCloseTabInUnfilteredList',
         closedTabIndex);
+    if (isMediaTab) {
+      chrome.metricsPrivate.recordBoolean(
+          'Tabs.TabSearch.WebUI.CloseMediaTabAction', withSearch);
+    }
     this.handler.closeTab(tabId);
   }
 
@@ -82,7 +90,8 @@ export class TabSearchApiProxyImpl implements TabSearchApiProxy {
   }
 
   switchToTab(
-      info: SwitchToTabInfo, withSearch: boolean, switchedTabIndex: number) {
+      info: SwitchToTabInfo, withSearch: boolean, isMediaTab: boolean,
+      switchedTabIndex: number) {
     chrome.metricsPrivate.recordEnumerationValue(
         'Tabs.TabSearch.WebUI.TabSwitchAction',
         withSearch ? TabSwitchAction.WITH_SEARCH :
@@ -92,6 +101,10 @@ export class TabSearchApiProxyImpl implements TabSearchApiProxy {
         withSearch ? 'Tabs.TabSearch.WebUI.IndexOfSwitchTabInFilteredList' :
                      'Tabs.TabSearch.WebUI.IndexOfSwitchTabInUnfilteredList',
         switchedTabIndex);
+    if (isMediaTab) {
+      chrome.metricsPrivate.recordBoolean(
+          'Tabs.TabSearch.WebUI.SwitchToMediaTabAction', withSearch);
+    }
 
     this.handler.switchToTab(info);
   }
--- chrome/test/data/webui/tab_search/tab_search_app_test.ts
+++ chrome/test/data/webui/tab_search/tab_search_app_test.ts
@@ -213,10 +213,11 @@ suite('TabSearchAppTest', () => {
     const tabSearchItemCloseButton =
         tabSearchItem.shadowRoot!.querySelector('cr-icon-button')!;
     tabSearchItemCloseButton.click();
-    const [tabId, withSearch, closedTabIndex] =
+    const [tabId, withSearch, isMediaTab, closedTabIndex] =
         await testProxy.whenCalled('closeTab');
     assertEquals(tabData.tabId, tabId);
     assertFalse(withSearch);
+    assertFalse(isMediaTab);
     assertEquals(0, closedTabIndex);
   });
 
@@ -561,9 +562,10 @@ suite('TabSearchAppTest', () => {
 
     // Assert switchToTab() was called appropriately for an unfiltered tab list.
     await testProxy.whenCalled('switchToTab')
-        .then(([tabInfo, withSearch, switchedTabIndex]) => {
+        .then(([tabInfo, withSearch, isMediaTab, switchedTabIndex]) => {
           assertEquals(1, tabInfo.tabId);
           assertFalse(withSearch);
+          assertFalse(isMediaTab);
           assertEquals(0, switchedTabIndex);
         });
 
@@ -575,9 +577,10 @@ suite('TabSearchAppTest', () => {
 
     // Assert switchToTab() was called appropriately for an unfiltered tab list.
     await testProxy.whenCalled('switchToTab')
-        .then(([tabInfo, withSearch, switchedTabIndex]) => {
+        .then(([tabInfo, withSearch, isMediaTab, switchedTabIndex]) => {
           assertEquals(6, tabInfo.tabId);
           assertFalse(withSearch);
+          assertFalse(isMediaTab);
           assertEquals(2, switchedTabIndex);
         });
 
@@ -597,9 +600,10 @@ suite('TabSearchAppTest', () => {
     // Assert switchToTab() was called appropriately for a tab list fitlered by
     // the search query.
     await testProxy.whenCalled('switchToTab')
-        .then(([tabInfo, withSearch, switchedTabIndex]) => {
+        .then(([tabInfo, withSearch, isMediaTab, switchedTabIndex]) => {
           assertEquals(2, tabInfo.tabId);
           assertTrue(withSearch);
+          assertFalse(isMediaTab);
           assertEquals(0, switchedTabIndex);
         });
   });
