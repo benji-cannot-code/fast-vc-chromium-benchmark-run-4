67c2b0295c3f55d7c792b542ba98247602aa96cb
Bence Béky <bnc@chromium.org>
2022-01-21 23:44:11 +0000

    Add HTTP/2 GREASE SETTINGS flags to WebView DevTools.
    See cl/422641302 for context.
    
    Change-Id: I12ac0d2f6242b58095a7a83db9ef86f38b5241ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403720
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962146}

--- components/network_session_configurator/common/network_switches.cc
+++ components/network_session_configurator/common/network_switches.cc
@@ -10,6 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
+// `kEnableHttp2GreaseSettings` does not include the word "enable" for
+// historical reasons.
+const char kEnableHttp2GreaseSettings[] = "http2-grease-settings";
+const char kDisableHttp2GreaseSettings[] = "disable-http2-grease-settings";
+
 #define NETWORK_SWITCH(name, value) const char name[] = value;
 #include "components/network_session_configurator/common/network_switch_list.h"
 #undef NETWORK_SWITCH
@@ -21,6 +26,8 @@ namespace network_session_configurator {
 void CopyNetworkSwitches(const base::CommandLine& src_command_line,
                          base::CommandLine* dest_command_line) {
   static const char* const kSwitchNames[] = {
+      switches::kEnableHttp2GreaseSettings,
+      switches::kDisableHttp2GreaseSettings,
 #define NETWORK_SWITCH(name, value) switches::name,
 #include "components/network_session_configurator/common/network_switch_list.h"
 #undef NETWORK_SWITCH
