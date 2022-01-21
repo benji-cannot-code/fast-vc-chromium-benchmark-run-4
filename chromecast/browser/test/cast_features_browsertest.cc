c0735a8a117deca9b66f6110b3556b90553d0391
Alex Turner <alexmt@chromium.org>
2022-01-21 22:40:42 +0000

    Migrate to modern DictionaryPrefUpdate in //chromecast
    This cl replaces instances of ScopedUserPrefUpdate that use the
    deprecated base::DictionaryValue with the modern DictionaryPrefUpdate
    (that uses base::Value under the hood).
    
    Bug: 1285745
    Change-Id: I86f70e40c7eb22a6529dbfbf13e545f9813c129a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3406387
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962119}

--- chromecast/browser/test/cast_features_browsertest.cc
+++ chromecast/browser/test/cast_features_browsertest.cc
@@ -125,8 +125,7 @@ class CastFeaturesBrowserTest : public CastBrowserTest {
   // setting features from the server.
   virtual void SetFeatures(const base::DictionaryValue& dcs_features) {
     auto pref_features = GetOverriddenFeaturesForStorage(dcs_features);
-    ScopedUserPrefUpdate<base::DictionaryValue, base::Value::Type::DICTIONARY>
-        dict(pref_service(), prefs::kLatestDCSFeatures);
+    DictionaryPrefUpdate dict(pref_service(), prefs::kLatestDCSFeatures);
     dict->MergeDictionary(&pref_features);
     pref_service()->CommitPendingWrite();
   }
@@ -134,8 +133,7 @@ class CastFeaturesBrowserTest : public CastBrowserTest {
   // Clears |features| from the PrefStore. Should be called in a PRE_PRE_*
   // method for any tested feature in a test to ensure consistent state.
   void ClearFeaturesFromPrefs(std::vector<base::Feature> features) {
-    ScopedUserPrefUpdate<base::DictionaryValue, base::Value::Type::DICTIONARY>
-        dict(pref_service(), prefs::kLatestDCSFeatures);
+    DictionaryPrefUpdate dict(pref_service(), prefs::kLatestDCSFeatures);
     for (auto f : features)
       dict->RemoveKey(f.name);
     pref_service()->CommitPendingWrite();
