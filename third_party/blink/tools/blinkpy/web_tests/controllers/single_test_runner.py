5060e8411e2594521d473b7ccc5510c282678270
Preethi Mohan <preethim@google.com>
2022-01-22 02:49:20 +0000

    [PY3 migration]: blink_web_tests - Fix some  err_string conversions
    Bug: 1285299
    Change-Id: I55991bfb46a572484d0b4cab4e2050fa7c696d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3408181
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/main@{#962217}

--- third_party/blink/tools/blinkpy/web_tests/controllers/single_test_runner.py
+++ third_party/blink/tools/blinkpy/web_tests/controllers/single_test_runner.py
@@ -619,7 +619,9 @@ class SingleTestRunner(object):
 
             if err_str:
                 _log.error('  %s : %s', self._test_name, err_str)
-                driver_output.error = (driver_output.error or '') + err_str
+                driver_output.error = (driver_output.error
+                                       or b'') + err_str.encode(
+                                           'utf8', 'replace')
 
             if diff or err_str:
                 return [
@@ -760,7 +762,8 @@ class SingleTestRunner(object):
             if err_str:
                 _log.error(err_str)
                 actual_driver_output.error = (actual_driver_output.error
-                                              or '') + err_str
+                                              or b'') + err_str.encode(
+                                                  'utf8', 'replace')
 
             if diff or err_str:
                 failures.append(
