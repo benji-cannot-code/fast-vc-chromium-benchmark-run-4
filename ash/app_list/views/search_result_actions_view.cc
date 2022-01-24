0d457f6b578b6c23d68b0af8d4c1d47d0510ff82
Yulun Wu <yulunwu@chromium.org>
2022-01-24 19:00:06 +0000

    Replace inkdrop highlights with focus ring.
    app_list_utils will be refactored in the future so ash/search_box
    will be able to take advantage of PaintFocusRing().
    
    see: crbug/1289467
    
    Bug: 1288658
    Change-Id: I51126d801f5dabcb0e8d8d0f345bb74bb8b02ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405465
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962603}

--- ash/app_list/views/search_result_actions_view.cc
+++ ash/app_list/views/search_result_actions_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <memory>
 
+#include "ash/app_list/app_list_util.h"
 #include "ash/app_list/views/search_result_actions_view_delegate.h"
 #include "ash/app_list/views/search_result_view.h"
 #include "ash/public/cpp/app_list/app_list_color_provider.h"
@@ -35,8 +36,6 @@ namespace {
 // Image buttons.
 constexpr int kImageButtonSizeDip = 40;
 constexpr int kActionButtonBetweenSpacing = 8;
-// The width of the focus ring.
-constexpr int kFocusRingWidth = 2;
 
 }  // namespace
 
@@ -156,13 +155,7 @@ void SearchResultImageButton::UpdateOnStateChanged() {
 
 void SearchResultImageButton::OnPaintBackground(gfx::Canvas* canvas) {
   if (HasFocus() || parent_->GetSelectedAction() == tag()) {
-    cc::PaintFlags circle_flags;
-    circle_flags.setAntiAlias(true);
-    circle_flags.setColor(AppListColorProvider::Get()->GetFocusRingColor());
-    circle_flags.setStyle(cc::PaintFlags::kStroke_Style);
-    circle_flags.setStrokeWidth(kFocusRingWidth);
-    canvas->DrawCircle(GetLocalBounds().CenterPoint(),
-                       GetButtonRadius() - kFocusRingWidth, circle_flags);
+    PaintFocusRing(canvas, GetLocalBounds().CenterPoint(), GetButtonRadius());
   }
 }
 
