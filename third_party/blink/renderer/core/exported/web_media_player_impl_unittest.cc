0d3f4626041dfebfc4e56568d8a4deaf2ec81dfc
Henrique Ferreiro <hferreiro@igalia.com>
2022-01-24 10:14:55 +0000

    Remove MediaFactory dependency on WebMediaPlayerImpl
    In this CL, WebMediaPlayerImpl is built indirectly via a builder
    function that just passes the constructor parameters removing a direct
    dependency from MediaFactory. This allows to move WebMediaPlayerImpl out
    of the Blink public API with the following benefits:
    * WebMediaPlayerImpl has some Mojo class members which can now be
      changed to use the Blink-variant Mojo types.
    * Some dependencies of WebMediaPlayerImpl can be moved out of
      public/platform/media/ as they aren't used elsewhere.
    
    web_media_player_impl.h is still including '*.mojom.h' files so we need
    to disable the presubmit checks. Porting it to Blink would require many
    more changes and will be done in future CLs.
    
    No-Presubmit: true
    Bug: 1198318
    Change-Id: I1f5fbec5e463cb902596d57c81c48eae72d76b8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3361040
    Reviewed-by: Will Cassella <cassew@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#962416}

--- third_party/blink/renderer/core/exported/web_media_player_impl_unittest.cc
+++ third_party/blink/renderer/core/exported/web_media_player_impl_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/public/platform/media/web_media_player_impl.h"
+#include "third_party/blink/renderer/platform/media/web_media_player_impl.h"
 
 #include <stdint.h>
 
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
-#include "third_party/blink/public/platform/media/web_media_player_params.h"
+#include "third_party/blink/public/platform/media/web_media_player_builder.h"
 #include "third_party/blink/public/platform/media/webmediaplayer_delegate.h"
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
 #include "third_party/blink/public/platform/web_fullscreen_video_status.h"
@@ -422,8 +422,14 @@ class WebMediaPlayerImplTest
     url_index_ = std::make_unique<UrlIndex>(&mock_resource_fetch_context_,
                                             media_thread_.task_runner());
 
-    auto params = std::make_unique<WebMediaPlayerParams>(
-        std::move(media_log), WebMediaPlayerParams::DeferLoadCB(), audio_sink_,
+    auto compositor = std::make_unique<NiceMock<MockVideoFrameCompositor>>(
+        media_thread_.task_runner());
+    compositor_ = compositor.get();
+
+    wmpi_ = std::make_unique<WebMediaPlayerImpl>(
+        GetWebLocalFrame(), &client_, &encrypted_client_, &delegate_,
+        std::move(factory_selector), url_index_.get(), std::move(compositor),
+        std::move(media_log), WebMediaPlayerBuilder::DeferLoadCB(), audio_sink_,
         media_thread_.task_runner(), media_thread_.task_runner(),
         media_thread_.task_runner(), media_thread_.task_runner(),
         base::BindRepeating(&WebMediaPlayerImplTest::OnAdjustAllocatedMemory,
@@ -435,16 +441,7 @@ class WebMediaPlayerImplTest
         viz::TestContextProvider::Create(),
         WebMediaPlayer::SurfaceLayerMode::kAlways,
         is_background_suspend_enabled_, is_background_video_playback_enabled_,
-        true, std::move(demuxer_override));
-
-    auto compositor = std::make_unique<NiceMock<MockVideoFrameCompositor>>(
-        params->video_frame_compositor_task_runner());
-    compositor_ = compositor.get();
-
-    wmpi_ = std::make_unique<WebMediaPlayerImpl>(
-        GetWebLocalFrame(), &client_, &encrypted_client_, &delegate_,
-        std::move(factory_selector), url_index_.get(), std::move(compositor),
-        nullptr, std::move(params));
+        true, std::move(demuxer_override), nullptr);
   }
 
   std::unique_ptr<WebSurfaceLayerBridge> CreateMockSurfaceLayerBridge(
