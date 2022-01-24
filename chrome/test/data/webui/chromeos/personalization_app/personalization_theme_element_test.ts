c30572724bb6c487cde39910e12ad779f1935cca
Jason Thai <jasontt@chromium.org>
2022-01-24 21:49:39 +0000

    personalization: separate theme observer to a its own file
    Bug: b/202860714
    Test: browser_tests
            --gtest_filter="PersonalizationAppComponentBrowserTest*"
    Test: unit_tests
            --gtest_filter="PersonalizationAppThemeProviderImplTest.*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I225375a509dc6d556b54768d5352bffd16451b53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413264
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962703}

--- chrome/test/data/webui/chromeos/personalization_app/personalization_theme_element_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/personalization_theme_element_test.ts
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {emptyState} from 'chrome://personalization/trusted/personalization_state.js';
 import {PersonalizationThemeElement} from 'chrome://personalization/trusted/personalization_theme_element.js';
 import {SetDarkModeEnabledAction, ThemeActionName} from 'chrome://personalization/trusted/theme/theme_actions.js';
+import {ThemeObserver} from 'chrome://personalization/trusted/theme/theme_observer.js';
 import {assertDeepEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks, waitAfterNextRender} from 'chrome://webui-test/test_util.js';
 
@@ -24,6 +25,7 @@ export function PersonalizationThemeTest() {
     const mocks = baseSetup();
     themeProvider = mocks.themeProvider;
     personalizationStore = mocks.personalizationStore;
+    ThemeObserver.initThemeObserverIfNeeded();
   });
 
   teardown(async () => {
@@ -31,6 +33,7 @@ export function PersonalizationThemeTest() {
       personalizationThemeElement.remove();
     }
     personalizationThemeElement = null;
+    ThemeObserver.shutdown();
     await flushTasks();
   });
 
