c5fd7fa0e91329c2f68c4949a07c717d18b807ad
David Sanders <dsanders11@ucsbalum.com>
2022-01-24 04:57:21 +0000

    Fixup includes in render_process_host_internal_observer.cc
    Change-Id: I02163c0a41d8202fc9c7481aba3fc961885b678b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3411531
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962373}

--- content/browser/renderer_host/render_process_host_internal_observer.cc
+++ content/browser/renderer_host/render_process_host_internal_observer.cc
@@ -3,7 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/renderer_host/render_process_host_impl.h"
+#include "content/browser/renderer_host/render_process_host_internal_observer.h"
+
+#include "base/check.h"
+#include "base/observer_list_types.h"
 
 namespace content {
 
