eb401d79d77c54d17f396c4e967de2bbfedbfc29
Ethan Jimenez <ethavar@microsoft.com>
2022-01-24 23:05:07 +0000

    [GridNG] Move InitializeGridItem out of NGGridLayoutAlgorithm
    This is a follow up of http://crrev.com/c/3319426; following the move of
    `GridItemData` out of `NGGridLayoutAlgorithm`, `InitializeGridItem` is
    also moved to `ng_grid_item.cc` as a constructor.
    
    This change shouldn't have any behavioral impact.
    
    Bug: 618969
    Change-Id: I24f58449bb91668d69e35a06ea7ee8de1971a777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413585
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#962731}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_item.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_item.h
@@ -32,8 +32,9 @@ struct OutOfFlowItemPlacement {
 struct CORE_EXPORT GridItemData {
   DISALLOW_NEW();
 
-  explicit GridItemData(const NGBlockNode node)
-      : node(node), is_sizing_dependent_on_block_size(false) {}
+  GridItemData(const NGBlockNode node,
+               const ComputedStyle& container_style,
+               const WritingMode container_writing_mode);
 
   void SetAlignmentFallback(const GridTrackSizingDirection track_direction,
                             const ComputedStyle& container_style,
