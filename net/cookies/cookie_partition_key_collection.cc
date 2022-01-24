bd16453458419987751a648b164dbff92250b14c
cfredric <cfredric@chromium.org>
2022-01-24 17:06:07 +0000

    Replace std::vector with base::flat_set in CookiePartitionKeyCollection.
    The CookiePartitionKeyCollection doesn't require the underlying keys to
    be in insertion order; and no consumer should access the underlying
    keys directly (apart from mojo, for serialization), so consumers do not
    rely on ordering assumptions either.
    
    This CL also makes it impossible to have the same key in a given
    CookiePartitionKeyCollection instance more than once. (Which would be
    harmless, except for using unnecessary memory; but would be
    meaningless.)
    
    Change-Id: I3abc45213194c852ba91ccc2b9a40a2c8b2dab7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407602
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962528}

--- net/cookies/cookie_partition_key_collection.cc
+++ net/cookies/cookie_partition_key_collection.cc
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/cookies/cookie_partition_key_collection.h"
 
+#include <vector>
+
 #include "base/barrier_callback.h"
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/containers/contains.h"
+#include "base/containers/flat_set.h"
 #include "net/cookies/cookie_access_delegate.h"
 #include "net/cookies/cookie_partition_key.h"
 
@@ -23,13 +26,12 @@ CookiePartitionKeyCollection::CookiePartitionKeyCollection(
     CookiePartitionKeyCollection&& other) = default;
 
 CookiePartitionKeyCollection::CookiePartitionKeyCollection(
-    const CookiePartitionKey& key) {
-  keys_.push_back(key);
-}
+    const CookiePartitionKey& key)
+    : CookiePartitionKeyCollection(base::flat_set<CookiePartitionKey>({key})) {}
 
 CookiePartitionKeyCollection::CookiePartitionKeyCollection(
-    const std::vector<CookiePartitionKey>& keys)
-    : keys_(keys) {}
+    base::flat_set<CookiePartitionKey> keys)
+    : keys_(std::move(keys)) {}
 
 CookiePartitionKeyCollection::CookiePartitionKeyCollection(
     bool contains_all_keys)
@@ -67,12 +69,11 @@ void CookiePartitionKeyCollection::FirstPartySetify(
               const net::CookiePartitionKey& key = key_and_owner.first;
               const absl::optional<SchemefulSite>& first_party_set_owner =
                   key_and_owner.second;
-              if (first_party_set_owner) {
-                keys.push_back(CookiePartitionKey::FromWire(
-                    first_party_set_owner.value(), key.nonce()));
-              } else {
-                keys.push_back(key);
-              }
+              keys.push_back(
+                  first_party_set_owner.has_value()
+                      ? CookiePartitionKey::FromWire(
+                            first_party_set_owner.value(), key.nonce())
+                      : key);
             }
             std::move(callback).Run(CookiePartitionKeyCollection(keys));
           },
--- net/cookies/cookie_partition_key_collection.h
+++ net/cookies/cookie_partition_key_collection.h
@@ -6,9 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_COOKIES_COOKIE_PARTITION_KEY_COLLECTION_H_
 #define NET_COOKIES_COOKIE_PARTITION_KEY_COLLECTION_H_
 
-#include <vector>
-
 #include "base/callback_forward.h"
+#include "base/containers/flat_set.h"
 #include "net/base/net_export.h"
 #include "net/cookies/cookie_partition_key.h"
 
@@ -31,9 +30,9 @@ class NET_EXPORT CookiePartitionKeyCollection {
   CookiePartitionKeyCollection(CookiePartitionKeyCollection&& other);
   // Creates a key collection with a single element.
   explicit CookiePartitionKeyCollection(const CookiePartitionKey& key);
-  // Creates a set that contains each partition key in the vector.
+  // Creates a set that contains each partition key in the set.
   explicit CookiePartitionKeyCollection(
-      const std::vector<CookiePartitionKey>& keys);
+      base::flat_set<CookiePartitionKey> keys);
 
   CookiePartitionKeyCollection& operator=(
       const CookiePartitionKeyCollection& other);
@@ -82,7 +81,7 @@ class NET_EXPORT CookiePartitionKeyCollection {
 
   // Iterate over all keys in the key collection, do not call this method if
   // `contains_all_keys` is true.
-  const std::vector<CookiePartitionKey>& PartitionKeys() const {
+  const base::flat_set<CookiePartitionKey>& PartitionKeys() const {
     DCHECK(!contains_all_keys_);
     return keys_;
   }
@@ -96,7 +95,7 @@ class NET_EXPORT CookiePartitionKeyCollection {
   bool contains_all_keys_ = false;
   // If `contains_all_keys_` is true, `keys_` must be empty.
   // If `keys_` is not empty, then `contains_all_keys_` must be false.
-  std::vector<CookiePartitionKey> keys_;
+  base::flat_set<CookiePartitionKey> keys_;
 };
 
 }  // namespace net
