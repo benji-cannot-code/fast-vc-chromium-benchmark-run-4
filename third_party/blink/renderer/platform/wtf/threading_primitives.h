4d0ae6ba98ae93a77e2c2b842b3ae6cc31b7b61b
Benoit Lize <lizeb@chromium.org>
2022-01-24 14:35:29 +0000

    [WTF] Make RecursiveMutex's constructor private
    This class only has one caller left, make sure that no new one appear.
    
    Bug: 856641
    Change-Id: I0bb02d344498f1811743cc6d03f5f3e978011fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3411874
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962473}

--- third_party/blink/renderer/platform/wtf/threading_primitives.h
+++ third_party/blink/renderer/platform/wtf/threading_primitives.h
@@ -65,6 +65,10 @@ struct BLINK_CONDITION_VARIABLE {
 #include <pthread.h>
 #endif
 
+namespace blink {
+class DeferredTaskHandler;
+}
+
 namespace WTF {
 
 #if BUILDFLAG(IS_WIN)
@@ -126,8 +130,15 @@ class LOCKABLE WTF_EXPORT Mutex : public MutexBase {
 // https://crbug.com/856641
 class WTF_EXPORT RecursiveMutex : public MutexBase {
  public:
-  RecursiveMutex() : MutexBase(true) {}
   bool TryLock();
+
+ private:
+  // Private constructor to ensure that no new users appear. This class will be
+  // removed.
+  RecursiveMutex() : MutexBase(true) {}
+
+  // DO NOT ADD any new caller.
+  friend class ::blink::DeferredTaskHandler;
 };
 
 class SCOPED_LOCKABLE MutexLocker final {
@@ -184,11 +195,11 @@ class WTF_EXPORT ThreadCondition final {
 
 }  // namespace WTF
 
-using WTF::MutexBase;
 using WTF::Mutex;
-using WTF::RecursiveMutex;
+using WTF::MutexBase;
 using WTF::MutexLocker;
 using WTF::MutexTryLocker;
+using WTF::RecursiveMutex;
 using WTF::ThreadCondition;
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_THREADING_PRIMITIVES_H_
