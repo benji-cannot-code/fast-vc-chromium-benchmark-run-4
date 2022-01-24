466eaa8536bc42d998bc0ea0af4ad2b768edc66b
Avi Drissman <avi@chromium.org>
2022-01-24 21:44:28 +0000

    Switch from (void) to std::ignore in document_provider
    Bug: 1289456
    Change-Id: I8739b36ef5aab053e2a321425206a45dc7e7939f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405492
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962700}

--- components/omnibox/browser/document_provider.cc
+++ components/omnibox/browser/document_provider.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <map>
 #include <numeric>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -221,7 +222,9 @@ int CalculateScore(const std::u16string& input,
       SplitByColon(String16VectorFromString16(input, false, nullptr));
 
   for (const auto& word : input_words) {
-    (void)std::find_if(
+    // FieldMatches::Includes() updates state, so it is OK to ignore the result
+    // of `find_if`.
+    std::ignore = std::find_if(
         field_matches_vec.begin(), field_matches_vec.end(),
         [word](auto& field_matches) { return field_matches.Includes(word); });
   }
