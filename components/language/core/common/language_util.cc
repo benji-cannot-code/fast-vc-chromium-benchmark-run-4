de5f5e5c59e9ddd591b7414aae7263d551cda7d8
Peter Kasting <pkasting@chromium.org>
2022-01-24 19:04:01 +0000

    Use structured bindings more: /components/language
    These changes were hand-authored by looking for instances of
    "std::tie(...) = ...".  The purpose is cleanup now that C++17
    structured bindings are allowed; no behavior change is intended.
    
    This CL was uploaded by git cl split.
    
    R=sclittle@chromium.org
    
    Bug: none
    Change-Id: I16f78865728ba221f5905ad03bdc07e43b1bc598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413054
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962606}

--- components/language/core/common/language_util.cc
+++ components/language/core/common/language_util.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 #include <algorithm>
-#include <tuple>
 
 #include "base/strings/strcat.h"
 #include "base/strings/string_piece.h"
@@ -111,8 +110,7 @@ void ToChromeLanguageSynonym(std::string* language) {
     }
   }
 
-  base::StringPiece main_part, tail_part;
-  std::tie(main_part, tail_part) = language::SplitIntoMainAndTail(*language);
+  auto [main_part, tail_part] = language::SplitIntoMainAndTail(*language);
   if (main_part.empty())
     return;
 
