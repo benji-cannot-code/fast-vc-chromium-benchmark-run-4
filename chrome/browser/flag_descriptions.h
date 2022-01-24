168a52873b2640a3b0f0e764bc3cc516e7a07462
Norge Vizcay <vizcay@google.com>
2022-01-24 13:30:02 +0000

    [Passwords] Introduce PasswordDomainCapabilitiesFetching flag.
    The flag will enable clients to use an updated process to fetch script capabilities of domain urls from saved credentials (go/replacing-gstatic-files-apc). As of now it is not in use, code that uses the flag is being introduced.
    
    Bug: b/209429727
    Change-Id: Id861811b4714e8d844e6fb0715ea3468593fa2ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402230
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#962458}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1102,6 +1102,9 @@ extern const char kPasswordImportDescription[];
 extern const char kPasswordsAccountStorageRevisedOptInFlowName[];
 extern const char kPasswordsAccountStorageRevisedOptInFlowDescription[];
 
+extern const char kPasswordDomainCapabilitiesFetchingName[];
+extern const char kPasswordDomainCapabilitiesFetchingDescription[];
+
 extern const char kPasswordScriptsFetchingName[];
 extern const char kPasswordScriptsFetchingDescription[];
 
