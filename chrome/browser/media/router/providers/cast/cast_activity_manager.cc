3ac6aef86258afe9a9c8fbf6e224231dc16c0c65
Muyao Xu <muyaoxu@google.com>
2022-01-24 18:08:22 +0000

    [Logger] Fix Uaf in CastActivityManager::TerminateSession
    Bug: 1289378
    Change-Id: I4e003f15f9f01e9846a7d7083d9c64b0dca39276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405565
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#962558}

--- chrome/browser/media/router/providers/cast/cast_activity_manager.cc
+++ chrome/browser/media/router/providers/cast/cast_activity_manager.cc
@@ -416,17 +416,17 @@ void CastActivityManager::TerminateSession(
     const MediaRoute::Id& route_id,
     mojom::MediaRouteProvider::TerminateRouteCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  const std::string source_id =
+      MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id);
+  const std::string presentation_id =
+      MediaRoute::GetPresentationIdFromMediaRouteId(route_id);
   logger_->LogInfo(mojom::LogCategory::kRoute, kLoggerComponent,
-                   "Terminating a session.", "",
-                   MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id),
-                   MediaRoute::GetPresentationIdFromMediaRouteId(route_id));
+                   "Terminating a session.", "", source_id, presentation_id);
   auto activity_it = activities_.find(route_id);
   if (activity_it == activities_.end()) {
-    logger_->LogWarning(
-        mojom::LogCategory::kRoute, kLoggerComponent,
-        "Cannot find the activity to terminate with route id.", "",
-        MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id),
-        MediaRoute::GetPresentationIdFromMediaRouteId(route_id));
+    logger_->LogWarning(mojom::LogCategory::kRoute, kLoggerComponent,
+                        "Cannot find the activity to terminate with route id.",
+                        "", source_id, presentation_id);
     std::move(callback).Run("Activity not found",
                             RouteRequestResult::ROUTE_NOT_FOUND);
     return;
@@ -439,12 +439,14 @@ void CastActivityManager::TerminateSession(
   // There is no session associated with the route, e.g. the launch request is
   // still pending.
   if (!session_id) {
+    // |route_id| might be a reference to the item in |routes_by_tab_|.
+    // RemoveActivity() deletes this item in |routes_by_tab_| and invalidates
+    // |route_id|.
     RemoveActivity(activity_it, PresentationConnectionState::TERMINATED,
                    PresentationConnectionCloseReason::CLOSED);
     logger_->LogInfo(mojom::LogCategory::kRoute, kLoggerComponent,
-                     "Terminated session has no session ID.", "",
-                     MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id),
-                     MediaRoute::GetPresentationIdFromMediaRouteId(route_id));
+                     "Terminated session has no session ID.", "", source_id,
+                     presentation_id);
     std::move(callback).Run(absl::nullopt, RouteRequestResult::OK);
     return;
   }
@@ -874,7 +876,15 @@ void CastActivityManager::HandleStopSessionResponse(
     return;
   }
 
+  const std::string source_id =
+      MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id);
+  const std::string presentation_id =
+      MediaRoute::GetPresentationIdFromMediaRouteId(route_id);
+
   if (result == cast_channel::Result::kOk) {
+    // |route_id| might be a reference to the item in |routes_by_tab_|.
+    // RemoveActivity() deletes this item in |routes_by_tab_| and invalidates
+    // |route_id|.
     RemoveActivity(activity_it, PresentationConnectionState::TERMINATED,
                    PresentationConnectionCloseReason::CLOSED);
     std::move(callback).Run(absl::nullopt, RouteRequestResult::OK);
@@ -882,16 +892,13 @@ void CastActivityManager::HandleStopSessionResponse(
     logger_->LogInfo(mojom::LogCategory::kRoute, kLoggerComponent,
                      "Terminated a route successfully after receiving "
                      "StopSession response OK.",
-                     "", MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id),
-                     MediaRoute::GetPresentationIdFromMediaRouteId(route_id));
+                     "", source_id, presentation_id);
   } else {
     std::string error_msg =
         "StopSession response is not OK. Failed to terminate route.";
     std::move(callback).Run(error_msg, RouteRequestResult::UNKNOWN_ERROR);
     logger_->LogError(mojom::LogCategory::kRoute, kLoggerComponent, error_msg,
-                      "",
-                      MediaRoute::GetMediaSourceIdFromMediaRouteId(route_id),
-                      MediaRoute::GetPresentationIdFromMediaRouteId(route_id));
+                      "", source_id, presentation_id);
   }
 }
 
