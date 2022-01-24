ddfb97cbf09f97ca4f68d9354fe438011bb0402c
Leonid Baraz <lbaraz@chromium.org>
2022-01-24 18:58:26 +0000

    Attach CRD reporting to remoting host
    Do it with actual implementation in ChromeOS, and with dummy in all
    other configurations, if necessary.
    
    Bug: b:190805227
    Change-Id: I442ad5a4f1c4187dd598bc33c9195fc81dcbe631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389771
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962602}

--- remoting/host/it2me/it2me_host.cc
+++ remoting/host/it2me/it2me_host.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/chromoting_host_context.h"
 #include "remoting/host/ftl_signaling_connector.h"
 #include "remoting/host/host_event_logger.h"
+#include "remoting/host/host_event_reporter.h"
 #include "remoting/host/host_secret.h"
 #include "remoting/host/host_status_logger.h"
 #include "remoting/host/it2me/it2me_confirmation_dialog.h"
@@ -275,6 +276,9 @@ void It2MeHost::ConnectOnNetworkThread(
   // Create event logger.
   host_event_logger_ =
       HostEventLogger::Create(host_->status_monitor(), kApplicationName);
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  host_event_reporter_ = HostEventReporter::Create(host_->status_monitor());
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   // Connect signaling and start the host.
   signal_strategy_->Connect();
@@ -612,6 +616,9 @@ void It2MeHost::DisconnectOnNetworkThread(protocol::ErrorCode error_code) {
         kDestroySignalingDelay);
   }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  host_event_reporter_.reset();
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   host_event_logger_ = nullptr;
 
   // Post tasks to delete UI objects on the UI thread.
