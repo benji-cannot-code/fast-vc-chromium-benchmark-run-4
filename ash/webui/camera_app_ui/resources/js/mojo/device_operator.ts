28337b959f201953aa3bcbea27d4d66b21c5327b
Wei Lee <wtlee@google.com>
2022-01-24 09:15:23 +0000

    CCA: Use CancelableEvent for reprocess event
    We used to use WaitableEvent for reprocess event but unfortunately it
    won't reject the promise when signalling with error, causing the error
    being handled unexpectedly.
    
    This CL changes to use CancelableEvent so the error can be thrown
    properly when the returned promise is awaited.
    
    Bug: b:216040255
    Test: Pass tsc
    Change-Id: I4485a6613c0681b93e5b99c26eb9c082e4671fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3406382
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962405}

--- ash/webui/camera_app_ui/resources/js/mojo/device_operator.ts
+++ ash/webui/camera_app_ui/resources/js/mojo/device_operator.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assert, assertNotReached} from '../assert.js';
+import {assert, assertExists, assertNotReached} from '../assert.js';
 import {AsyncJobQueue} from '../async_job_queue.js';
 import {reportError} from '../error.js';
 import {Point} from '../geometry.js';
@@ -16,7 +16,7 @@ import {
   ResolutionList,
   VideoConfig,
 } from '../type.js';
-import {WaitableEvent} from '../waitable_event.js';
+import {CancelableEvent, WaitableEvent} from '../waitable_event.js';
 
 import {
   AndroidInfoSupportedHardwareLevel,
@@ -562,7 +562,7 @@ export class DeviceOperator {
     const reprocessEvents = new Map;
     const callbacks = [];
     for (const effect of effects) {
-      const event = new WaitableEvent<Blob>();
+      const event = new CancelableEvent<Blob>();
       reprocessEvents.set(effect, event);
       callbacks.push(event.wait());
     }
@@ -571,12 +571,9 @@ export class DeviceOperator {
         wrapEndpoint(new ReprocessResultListenerCallbackRouter());
     listenerCallbacksRouter.onReprocessDone.addListener(
         (effect, status, blob) => {
-          const event = reprocessEvents.get(effect);
-          if (event === undefined) {
-            throw new Error(`Reprocess done with unexpected effect: ${effect}`);
-          }
+          const event = assertExists(reprocessEvents.get(effect));
           if (blob === null || status !== 0) {
-            event.signal(new Error(`Set reprocess failed: ${status}`));
+            event.signalError(new Error(`Set reprocess failed: ${status}`));
           } else {
             const {data, mimeType} = blob;
             event.signal(new Blob([new Uint8Array(data)], {type: mimeType}));
