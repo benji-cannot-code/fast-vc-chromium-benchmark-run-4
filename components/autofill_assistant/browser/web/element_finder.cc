b11c82f61e547f1c398420d2653997057f7d1e90
David Bengoa <dbengoa@google.com>
2022-01-24 16:21:18 +0000

    [Autofill Assistant] Add WebController#ObserveSelectors
    Regarding the size, I tried a couple of things but nothing made a
    significant difference. One thing I didn't do but we could in the future
    is to minimize and gzip the script. This would probably only save about
    2KiB and since it would make it harder to debug I decided against it at
    this time.
    
    In the medium term, we will probably only have one kind of WaitForDom,
    so either we will remove this one or always use this one (and we can
    remove some unused stuff in BatchElementChecker / ElementFinder).
    
    Bug: b/205676462
    Binary-Size: Size increase is unavoidable (see above).
    Change-Id: I8b137891791fe96927ad78e64b0aad7bded08bdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3312493
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: David Bengoa <dbengoa@google.com>
    Cr-Commit-Position: refs/heads/main@{#962504}

--- components/autofill_assistant/browser/web/element_finder.cc
+++ components/autofill_assistant/browser/web/element_finder.cc
@@ -18,8 +18,7 @@ namespace autofill_assistant {
 
 namespace {
 // Javascript code to get document root element.
-const char kGetDocumentElement[] =
-    "(function() { return document.documentElement; }())";
+const char kGetDocumentElement[] = "document.documentElement;";
 
 const char kGetArrayElement[] = "function(index) { return this[index]; }";
 
@@ -241,6 +240,8 @@ void ElementFinder::ExecuteNextTask() {
     }
 
     case SelectorProto::Filter::kNthMatch: {
+      // TODO(b/205676462): This could be done with javascript like in
+      // |SelectorObserver|.
       std::string object_id;
       if (!ConsumeMatchAtOrFail(filter.nth_match().index(), object_id))
         return;
