688ebc3143becf5644a6e066583284b907e08fae
Eric Willigers <ericwilligers@chromium.org>
2022-01-24 23:06:45 +0000

    Update chromium.googlesource.com links in web_tests
    Replace instances of "master" in web_tests
    
    Change-Id: Ia08435a1b33609ed1f7ea2c0f4baa2814afd2b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3411716
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Louise Brett <loubrett@google.com>
    Commit-Queue: Louise Brett <loubrett@google.com>
    Cr-Commit-Position: refs/heads/main@{#962733}

--- third_party/blink/web_tests/resources/compositor-controls.js
+++ third_party/blink/web_tests/resources/compositor-controls.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // To make sure animation events get processed, periodic lifecycle phase runs
 // will include rasterization. See
-// https://chromium.googlesource.com/chromium/src/+/master/docs/testing/writing_web_tests.md
+// https://chromium.googlesource.com/chromium/src/+/main/docs/testing/writing_web_tests.md
 // for more information.
 function setAnimationRequiresRaster() {
   if (window.testRunner) {
