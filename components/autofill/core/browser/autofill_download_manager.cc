7e4013ce57c80113b2d8463848cc83c6bd614432
Peter Kasting <pkasting@chromium.org>
2022-01-24 22:02:58 +0000

    Use structured bindings more: /components/autofill/core/browser
    These changes were hand-authored by looking for instances of
    "std::tie(...) = ...".  The purpose is cleanup now that C++17
    structured bindings are allowed; no behavior change is intended.
    
    This CL was uploaded by git cl split.
    
    R=sebsg@chromium.org
    
    Bug: none
    Change-Id: Ia7089bb87b926a45b80ffce6eb4df42a5f441f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413052
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962712}

--- components/autofill/core/browser/autofill_download_manager.cc
+++ components/autofill/core/browser/autofill_download_manager.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_download_manager.h"
 
 #include <algorithm>
-#include <tuple>
 #include <utility>
 
 #include "base/base64url.h"
@@ -830,9 +829,7 @@ bool AutofillDownloadManager::StartRequest(FormRequestData request_data) {
   DCHECK(url_loader_factory);
 
   // Get the URL and method to use for this request.
-  std::string method;
-  GURL request_url;
-  std::tie(request_url, method) = GetRequestURLAndMethod(request_data);
+  auto [request_url, method] = GetRequestURLAndMethod(request_data);
 
   // Track the URL length for GET queries because the URL length can be in the
   // thousands when rich metadata is enabled.
