3c99f81cea65c7cd1b9fdeec9299794e4d3a9240
Antonio Gomes <tonikitoo@igalia.com>
2022-01-24 18:46:11 +0000

    Remove useless exo::DragDropOperation::OnDragEnded() override
    BUG=NONE
    R=oshima@chromium.org
    
    Change-Id: I27dcd8e700ff02adb09443561dcc6bdcfb899c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413068
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#962588}

--- components/exo/drag_drop_operation.cc
+++ components/exo/drag_drop_operation.cc
@@ -389,8 +389,6 @@ void DragDropOperation::OnDragStarted() {
     delete this;
 }
 
-void DragDropOperation::OnDragEnded() {}
-
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 void DragDropOperation::OnDragActionsChanged(int actions) {
   if (!started_by_this_object_)
