26af8458b26ce483c5f66ab9bad43f47657d9c0a
Paul Adedeji <pauladedeji@google.com>
2022-01-25 18:37:29 +0000

    Wrap sibling "short" modules in a separate container
    If two short modules neighbor each other we wrap them in their own container so Flexbox moves both of them together instead of placing them side by side.
    This change also adds variation to the heights of the dummy modules so we can be sure the logic works correctly for short modules that have a sibling short module and short modules that don't. If they don't they should be in a container of their own.
    
    Test screenshot:screenshot/A22jxXsTBsJLXxu (has changes to layout not included in this CL to make the change easier to notice)
    Dev tools screenshot:screenshot/5dgWZoaCEh7wwUK
    
    Bug: 1289793
    Change-Id: Ifdaf8bae9cbdcd9d27088861a6fd2c9853ef1f4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3390731
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#963094}

--- chrome/browser/resources/new_tab_page/modules/dummy_v2/module.js
+++ chrome/browser/resources/new_tab_page/modules/dummy_v2/module.js
@@ -90,7 +90,7 @@ export const dummyV2Descriptor = new ModuleDescriptorV2(
 export const dummyV2Descriptor02 = new ModuleDescriptorV2(
     /*id=*/ 'dummy2',
     /*name=*/ loadTimeData.getString('modulesDummy2Title'),
-    /*height*/ ModuleHeight.SHORT,
+    /*height*/ ModuleHeight.TALL,
     () => Promise.resolve(createDummyElement('modulesDummy2Title')));
 
 /** @type {!ModuleDescriptorV2} */
@@ -104,7 +104,7 @@ export const dummyV2Descriptor03 = new ModuleDescriptorV2(
 export const dummyV2Descriptor04 = new ModuleDescriptorV2(
     /*id=*/ 'dummy4',
     /*name=*/ loadTimeData.getString('modulesDummy4Title'),
-    /*height*/ ModuleHeight.SHORT,
+    /*height*/ ModuleHeight.TALL,
     () => Promise.resolve(createDummyElement('modulesDummy4Title')));
 
 /** @type {!ModuleDescriptorV2} */
@@ -125,7 +125,7 @@ export const dummyV2Descriptor06 = new ModuleDescriptorV2(
 export const dummyV2Descriptor07 = new ModuleDescriptorV2(
     /*id=*/ 'dummy7',
     /*name=*/ loadTimeData.getString('modulesDummy7Title'),
-    /*height*/ ModuleHeight.TALL,
+    /*height*/ ModuleHeight.SHORT,
     () => Promise.resolve(createDummyElement('modulesDummy7Title')));
 
 /** @type {!ModuleDescriptorV2} */
@@ -146,19 +146,19 @@ export const dummyV2Descriptor09 = new ModuleDescriptorV2(
 export const dummyV2Descriptor10 = new ModuleDescriptorV2(
     /*id=*/ 'dummy10',
     /*name=*/ loadTimeData.getString('modulesDummy10Title'),
-    /*height*/ ModuleHeight.TALL,
+    /*height*/ ModuleHeight.SHORT,
     () => Promise.resolve(createDummyElement('modulesDummy10Title')));
 
 /** @type {!ModuleDescriptorV2} */
 export const dummyV2Descriptor11 = new ModuleDescriptorV2(
     /*id=*/ 'dummy11',
     /*name=*/ loadTimeData.getString('modulesDummy11Title'),
-    /*height*/ ModuleHeight.TALL,
+    /*height*/ ModuleHeight.SHORT,
     () => Promise.resolve(createDummyElement('modulesDummy11Title')));
 
 /** @type {!ModuleDescriptorV2} */
 export const dummyV2Descriptor12 = new ModuleDescriptorV2(
     /*id=*/ 'dummy12',
     /*name=*/ loadTimeData.getString('modulesDummy12Title'),
-    /*height*/ ModuleHeight.TALL,
+    /*height*/ ModuleHeight.SHORT,
     () => Promise.resolve(createDummyElement('modulesDummy12Title')));
