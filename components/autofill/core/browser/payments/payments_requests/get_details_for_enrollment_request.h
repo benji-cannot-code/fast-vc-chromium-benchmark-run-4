d2f828a893a8296acaf6aad6d97eb66cf52f8234
Siyu An <siyua@chromium.org>
2022-01-25 22:59:31 +0000

    [VCN] Implement VCEM to send GetDetailsForEnrollment request
    Connect VCEM and PaymentsClient. This is the last missing part for the
    GetDetailsForEnrollment request.
    
    Also refactor related files a little bit
    
    Bug: 1281695
    Change-Id: Ia5867ffab6289e03dc14d81910cf3f5a06a3d9d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3408556
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963257}

--- components/autofill/core/browser/payments/payments_requests/get_details_for_enrollment_request.h
+++ components/autofill/core/browser/payments/payments_requests/get_details_for_enrollment_request.h
@@ -18,9 +18,10 @@ class GetDetailsForEnrollmentRequest : public PaymentsRequest {
   GetDetailsForEnrollmentRequest(
       const PaymentsClient::GetDetailsForEnrollmentRequestDetails&
           request_details,
-      base::OnceCallback<void(
-          AutofillClient::PaymentsRpcResult,
-          PaymentsClient::GetDetailsForEnrollmentResponseDetails&)> callback);
+      base::OnceCallback<
+          void(AutofillClient::PaymentsRpcResult,
+               const PaymentsClient::GetDetailsForEnrollmentResponseDetails&)>
+          callback);
   GetDetailsForEnrollmentRequest(const GetDetailsForEnrollmentRequest&) =
       delete;
   GetDetailsForEnrollmentRequest& operator=(
@@ -48,7 +49,7 @@ class GetDetailsForEnrollmentRequest : public PaymentsRequest {
   // The callback function to be invoked when the response is received.
   base::OnceCallback<void(
       AutofillClient::PaymentsRpcResult,
-      PaymentsClient::GetDetailsForEnrollmentResponseDetails&)>
+      const PaymentsClient::GetDetailsForEnrollmentResponseDetails&)>
       callback_;
 };
 
--- components/autofill/core/browser/payments/test_payments_client.cc
+++ components/autofill/core/browser/payments/test_payments_client.cc
@@ -14,9 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
-namespace autofill {
-
-namespace payments {
+namespace autofill::payments {
 
 namespace {
 // Base64 encoding of "This is a test challenge".
@@ -35,7 +33,7 @@ TestPaymentsClient::TestPaymentsClient(
   unmask_details_.unmask_auth_method = AutofillClient::UnmaskAuthMethod::kCvc;
 }
 
-TestPaymentsClient::~TestPaymentsClient() {}
+TestPaymentsClient::~TestPaymentsClient() = default;
 
 void TestPaymentsClient::GetUnmaskDetails(
     base::OnceCallback<void(AutofillClient::PaymentsRpcResult,
@@ -110,6 +108,15 @@ void TestPaymentsClient::SelectChallengeOption(
   }
   std::move(callback).Run(AutofillClient::PaymentsRpcResult::kSuccess,
                           "context_token from SelectChallengeOption");
+}
+
+void TestPaymentsClient::GetVirtualCardEnrollmentDetails(
+    const GetDetailsForEnrollmentRequestDetails& request_details,
+    base::OnceCallback<void(AutofillClient::PaymentsRpcResult,
+                            const payments::PaymentsClient::
+                                GetDetailsForEnrollmentResponseDetails&)>
+        callback) {
+  get_details_for_enrollment_request_details_ = std::move(request_details);
 }
 
 void TestPaymentsClient::UpdateVirtualCardEnrollment(
@@ -221,5 +228,4 @@ std::unique_ptr<base::Value> TestPaymentsClient::LegalMessage() {
   }
 }
 
-}  // namespace payments
-}  // namespace autofill
+}  // namespace autofill::payments
