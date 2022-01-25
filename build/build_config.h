1217464e39b46fa2824fc6bb5ad4dbbeb88b7306
Xiaohan Wang <xhwang@chromium.org>
2022-01-25 17:42:26 +0000

    build: Fix OS buildflag comment
    BUILDFLAG(IS_CHROMEOS) is also supported. Fix the comment in
    build_config.h to reflect that.
    
    Bug: 1234043
    Change-Id: I2d5cfd236a2e3a7121e9f7e4223f08c84c5f1e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415176
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963063}

--- build/build_config.h
+++ build/build_config.h
@@ -16,9 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    Deprecated: #if defined(OS_WIN)
 //
 //  Operating System:
-//    IS_AIX / IS_ANDROID / IS_ASMJS / IS_FREEBSD / IS_FUCHSIA / IS_IOS /
-//    IS_LINUX / IS_MAC / IS_NACL / IS_NETBSD / IS_OPENBSD /
-//    IS_QNX / IS_SOLARIS / IS_WIN
+//    IS_AIX / IS_ANDROID / IS_ASMJS / IS_CHROMEOS / IS_FREEBSD / IS_FUCHSIA /
+//    IS_IOS / IS_LINUX / IS_MAC / IS_NACL / IS_NETBSD / IS_OPENBSD / IS_QNX /
+//    IS_SOLARIS / IS_WIN
 //  Operating System family:
 //    IS_APPLE: IOS or MAC
 //    IS_BSD: FREEBSD or NETBSD or OPENBSD
