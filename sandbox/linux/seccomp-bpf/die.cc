dcaf2f0c588751be17677358b422547f0185b1f2
Avi Drissman <avi@chromium.org>
2022-01-25 17:10:57 +0000

    Remove ignore_result.h
    This removes the header file and the ignore_result()
    function. Use `std::ignore =` instead.
    
    Bug: 1285085
    Change-Id: I4d5c8a1678a89ee7512dc37b910e4d8079d28ed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3374762
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: danakj chromium <danakj@chromium.org>
    Owners-Override: danakj chromium <danakj@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963041}

--- sandbox/linux/seccomp-bpf/die.cc
+++ sandbox/linux/seccomp-bpf/die.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 
 #include <string>
+#include <tuple>
 
-#include "base/ignore_result.h"
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
 #include "sandbox/linux/seccomp-bpf/syscall.h"
@@ -84,8 +84,8 @@ void Die::LogToStderr(const char* msg, const char* file, int line) {
 
     // No need to loop. Short write()s are unlikely and if they happen we
     // probably prefer them over a loop that blocks.
-    ignore_result(
-        HANDLE_EINTR(Syscall::Call(__NR_write, 2, s.c_str(), s.length())));
+    std::ignore =
+        HANDLE_EINTR(Syscall::Call(__NR_write, 2, s.c_str(), s.length()));
   }
 }
 
