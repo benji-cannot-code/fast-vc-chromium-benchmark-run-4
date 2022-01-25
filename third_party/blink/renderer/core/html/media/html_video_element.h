734e9649e3d6787f639dbbebadfbc330ff4edc70
Seth Brenith <seth.brenith@microsoft.com>
2022-01-25 00:26:27 +0000

    Fix building with cppgc_enable_object_names = true
    HTMLVideoElement has a member variable of type
    std::unique_ptr<CanvasResourceProvider>, so the implicit destructor for
    HTMLVideoElement needs the definition of CanvasResourceProvider to be
    available. This inclusion change makes the file html_video_element.h
    properly self-contained.
    
    Change-Id: I97e8f85d3d38e641db52cb70100ef7da112cfdba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413745
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Seth Brenith <seth.brenith@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#962769}

--- third_party/blink/renderer/core/html/media/html_video_element.h
+++ third_party/blink/renderer/core/html/media/html_video_element.h
@@ -33,10 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_image_loader.h"
 #include "third_party/blink/renderer/core/html/media/html_media_element.h"
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap_source.h"
+#include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 
 namespace blink {
 
-class CanvasResourceProvider;
 class ImageBitmapOptions;
 class IntersectionObserverEntry;
 class MediaCustomControlsFullscreenDetector;
