ab79a7818cbe188cc0b07adc22aab76bc721bd76
Gavin Williams <gavinwill@chromium.org>
2022-01-25 01:17:20 +0000

    Shimless: Create an event for disabling all buttons
    - Create an event listener for 'disable-all-buttons' to disable the
      Next, Back and Cancel buttons
    - Send the 'disable-all-buttons' while performing an OS update
    
    Bug: 1198187
    Change-Id: Id50f1aa4d1a864c658309f722206d02157c3fa39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403556
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962783}

--- ash/webui/shimless_rma/resources/onboarding_update_page.js
+++ ash/webui/shimless_rma/resources/onboarding_update_page.js
@@ -58,6 +58,12 @@ export class OnboardingUpdatePageElement extends
 
   static get properties() {
     return {
+      /**
+       * Set by shimless_rma.js.
+       * @type {boolean}
+       */
+      allButtonsDisabled: Boolean,
+
       /** @protected */
       currentVersionText_: {
         type: String,
@@ -93,6 +99,7 @@ export class OnboardingUpdatePageElement extends
       updateInProgress_: {
         type: Boolean,
         value: false,
+        observer: 'onUpdateInProgressChange_',
       },
 
       /** @protected */
@@ -198,18 +205,10 @@ export class OnboardingUpdatePageElement extends
   /** @protected */
   onUpdateButtonClicked_() {
     this.updateInProgress_ = true;
-    this.dispatchEvent(new CustomEvent(
-        'disable-next-button',
-        {bubbles: true, composed: true, detail: false},
-        ));
     this.shimlessRmaService_.updateOs().then((res) => {
       if (!res.updateStarted) {
         this.updateProgressMessage_ = this.i18n('osUpdateFailedToStartText');
         this.updateInProgress_ = false;
-        this.dispatchEvent(new CustomEvent(
-            'disable-next-button',
-            {bubbles: true, composed: true, detail: false},
-            ));
       }
     });
   }
@@ -247,10 +246,6 @@ export class OnboardingUpdatePageElement extends
         operation === OsUpdateOperation.kNeedPermissionToUpdate ||
         operation === OsUpdateOperation.kDisabled) {
       this.updateInProgress_ = false;
-      this.dispatchEvent(new CustomEvent(
-          'disable-next-button',
-          {bubbles: true, composed: true, detail: false},
-          ));
     }
     this.updateProgressMessage_ = this.i18n(
         'onboardingUpdateProgress', this.i18n(operationNameKeys[operation]),
@@ -313,6 +308,15 @@ export class OnboardingUpdatePageElement extends
   /** @private */
   closeDialog_() {
     this.shadowRoot.querySelector('#unqualifiedComponentsDialog').close();
+  }
+
+  /** @private */
+  onUpdateInProgressChange_() {
+    const shouldDisableAllButtons = this.updateInProgress_;
+    this.dispatchEvent(new CustomEvent(
+        'disable-all-buttons',
+        {bubbles: true, composed: true, detail: shouldDisableAllButtons},
+        ));
   }
 }
 
