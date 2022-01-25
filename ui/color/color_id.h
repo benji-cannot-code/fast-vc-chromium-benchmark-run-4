d76f77a16d9a82dd33e7516b6273ea79a2b2fb7d
minch <minch@chromium.org>
2022-01-25 22:54:33 +0000

    dark_mode: Add AshColorMixer
    Add AshColorMixer to let some color ids have specific colors under ash.
    Their overridden colors will be gotten from AshColorProvider.
    
    This cl adds `kColorTouchableMenuBackground` for the background color
    of context menu under ash. More context menu related color ids will be
    added in the follow up cls.
    
    Screenshots of the context menu inside browser and system UI after the
    change can be found at
    https://bugs.chromium.org/p/chromium/issues/detail?id=1156249#c22
    
    Bug: 1156249
    Change-Id: I75a72ab9c884114f8553e7b1840054c894570a77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405560
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963255}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorLinkForegroundDisabled) \
   E_CPONLY(kColorLinkForegroundPressed) \
   E(kColorMenuBackground, NativeTheme::kColorId_MenuBackgroundColor) \
+  E(kColorTouchableMenuBackground, NativeTheme::kColorId_MenuBackgroundColor) \
   E_CPONLY(kColorMenuBorder) \
   E_CPONLY(kColorMenuDropmarker) \
   E(kColorMenuIcon, NativeTheme::kColorId_MenuIconColor) \
