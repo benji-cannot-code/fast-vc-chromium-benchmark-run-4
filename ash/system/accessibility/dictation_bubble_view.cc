3a858d5d5394b9f866dc8217fa9ac441d4ed982a
Akihiro Ota <akihiroota@chromium.org>
2022-01-25 00:55:32 +0000

    Dictation: Add standby animation
    This change makes use of the AnimatedImageView to display a standby
    animation to users of accessibility Dictation. If the animation fails
    to load, we use the existing image as a fallback.
    
    The current (placeholder) json file encodes the following animation:
    https://skottie-internal.skia.org/6735b01ea908c0e827fdb962b3e021e3?h=256&w=256
    
    The final animation, which will be uploaded in a follow-up CL, will
    look like the following:
    https://carbon.googleplex.com/cr-os-motion-work/pages/audio
    
    AX-Relnotes: N/A
    Bug: 1252037
    Change-Id: I152b0765420bca86a2d4526ac4886ecdd14d4fe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386830
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962777}

--- ash/system/accessibility/dictation_bubble_view.cc
+++ ash/system/accessibility/dictation_bubble_view.cc
@@ -6,12 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/accessibility/dictation_bubble_view.h"
 
 #include "ash/public/cpp/accessibility_controller_enums.h"
+#include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "cc/paint/skottie_wrapper.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/lottie/animation.h"
+#include "ui/views/controls/animated_image_view.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
@@ -33,8 +39,14 @@ DictationBubbleView::~DictationBubbleView() = default;
 
 void DictationBubbleView::Update(DictationBubbleIconType icon,
                                  const absl::optional<std::u16string>& text) {
-  // Update icon visibility.
-  standby_image_->SetVisible(icon == DictationBubbleIconType::kStandby);
+  // Update visibility.
+  if (use_standby_animation_) {
+    standby_animation_->SetVisible(icon == DictationBubbleIconType::kStandby);
+    icon == DictationBubbleIconType::kStandby ? standby_animation_->Play()
+                                              : standby_animation_->Stop();
+  } else {
+    standby_image_->SetVisible(icon == DictationBubbleIconType::kStandby);
+  }
   macro_succeeded_image_->SetVisible(icon ==
                                      DictationBubbleIconType::kMacroSuccess);
   macro_failed_image_->SetVisible(icon == DictationBubbleIconType::kMacroFail);
@@ -53,19 +65,11 @@ void DictationBubbleView::Init() {
 
   UseCompactMargins();
 
-  auto create_image_view = [](views::ImageView** destination_view,
-                              const gfx::VectorIcon& icon) {
-    return views::Builder<views::ImageView>()
-        .CopyAddressTo(destination_view)
-        .SetImage(gfx::CreateVectorIcon(icon, kIconSizeDip, kIconAndLabelColor))
-        .Build();
-  };
-
-  AddChildView(create_image_view(&standby_image_, kDictationBubbleIcon));
-  AddChildView(create_image_view(&macro_succeeded_image_,
-                                 kDictationBubbleMacroSucceededIcon));
+  AddChildView(CreateStandbyView());
+  AddChildView(CreateImageView(&macro_succeeded_image_,
+                               kDictationBubbleMacroSucceededIcon));
   AddChildView(
-      create_image_view(&macro_failed_image_, kDictationBubbleMacroFailedIcon));
+      CreateImageView(&macro_failed_image_, kDictationBubbleMacroFailedIcon));
   AddChildView(CreateLabel(std::u16string()));
 }
 
@@ -87,7 +91,10 @@ std::u16string DictationBubbleView::GetTextForTesting() {
   return label_->GetText();
 }
 
-bool DictationBubbleView::IsStandbyImageVisibleForTesting() {
+bool DictationBubbleView::IsStandbyViewVisibleForTesting() {
+  if (use_standby_animation_)
+    return standby_animation_->GetVisible();
+
   return standby_image_->GetVisible();
 }
 
@@ -97,6 +104,34 @@ bool DictationBubbleView::IsMacroSucceededImageVisibleForTesting() {
 
 bool DictationBubbleView::IsMacroFailedImageVisibleForTesting() {
   return macro_failed_image_->GetVisible();
+}
+
+std::unique_ptr<views::View> DictationBubbleView::CreateStandbyView() {
+  absl::optional<std::string> json =
+      ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
+          IDR_DICTATION_BUBBLE_ANIMATION);
+  if (json.has_value()) {
+    use_standby_animation_ = true;
+    auto skottie = cc::SkottieWrapper::CreateSerializable(
+        std::vector<uint8_t>(json.value().begin(), json.value().end()));
+    return views::Builder<views::AnimatedImageView>()
+        .CopyAddressTo(&standby_animation_)
+        .SetAnimatedImage(std::make_unique<lottie::Animation>(skottie))
+        .SetImageSize(gfx::Size(32, 16))
+        .Build();
+  }
+
+  use_standby_animation_ = false;
+  return CreateImageView(&standby_image_, kDictationBubbleIcon);
+}
+
+std::unique_ptr<views::ImageView> DictationBubbleView::CreateImageView(
+    views::ImageView** destination_view,
+    const gfx::VectorIcon& icon) {
+  return views::Builder<views::ImageView>()
+      .CopyAddressTo(destination_view)
+      .SetImage(gfx::CreateVectorIcon(icon, kIconSizeDip, kIconAndLabelColor))
+      .Build();
 }
 
 std::unique_ptr<views::Label> DictationBubbleView::CreateLabel(
