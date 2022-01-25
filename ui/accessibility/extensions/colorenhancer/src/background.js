e4492eb2592073f9306fc679c5e4efd152e68339
Anastasia Helfinstein <anastasi@google.com>
2022-01-25 21:56:50 +0000

    Fix error in color enhancer extension console
    There was a runtime error caused by an async response to a message.
    This change keeps the message context open for the asynchronous
    callback.
    
    AX-Relnotes: n/a.
    Bug: 1289423
    Test: Manual - ran extension and confirmed the error was gone
    Change-Id: Ica8e329b4444abf714d9214e1cb2a71fffbef51c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414540
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963225}

--- ui/accessibility/extensions/colorenhancer/src/background.js
+++ ui/accessibility/extensions/colorenhancer/src/background.js
@@ -85,6 +85,7 @@ async function onInitReceived(sender) {
       function(message, sender, sendResponse) {
         if (message === 'init') {
           onInitReceived(sender).then(sendResponse);
+          return true;  // Keep message context open for async response.
         }
       });
 
