6a0bca3d2ca0649f1b4dc75598714d644bcbbf06
David Black <dmblack@google.com>
2022-01-25 21:31:54 +0000

    Refactor `ProgressIndicatorAnimationRegistry` (Part 1 of N).
    This CL moves maps previously owned by `HoldingSpaceAnimationRegistry`
    (indirectly) into `ProgressIndicatorAnimationRegistry`. The next N CLs
    will migrate base-level method implementations from the holding space
    specific implementation to the base class.
    
    Methods will be migrated in small batches to keep CL size down and to
    minimize the likelihood of regression.
    
    Bug: 1289842
    Change-Id: I169f183e66151cc1d0ab610694790a2ccd6815a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3408297
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#963211}

---
+++ ash/system/progress_indicator/progress_indicator_animation_registry.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/progress_indicator/progress_indicator_animation_registry.h"
+
+namespace ash {
+
+ProgressIndicatorAnimationRegistry::ProgressIndicatorAnimationRegistry() =
+    default;
+
+ProgressIndicatorAnimationRegistry::~ProgressIndicatorAnimationRegistry() =
+    default;
+
+}  // namespace ash
