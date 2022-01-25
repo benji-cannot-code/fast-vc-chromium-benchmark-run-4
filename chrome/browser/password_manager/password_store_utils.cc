ce54890cdb03ede478a3ac2b5c26f578d4e5661f
Mohamed Amir Yosef <mamir@chromium.org>
2022-01-25 16:45:51 +0000

    [Passwords] Split PasswordManager metrics for primary user profiles
    This CL splits the PasswordManager metrics to be reported separately
    for primary user profile (the user who created the current session)
    and non-primary user profiles.
    
    The output of
     python3 ./tools/metrics/histograms/print_histogram_names.py --diff origin/main
    is in
    http://go/paste/5593622218014720
    
    For more context, see http://docs/document/d/1eC2jIeACjcNx0oxWg9xLMRJ2Z53TAZoMwI24-Q_68Rw?resourcekey=0-YJEfh-JUUi1cNX_IR3WmhA
    
    Bug: 1259121
    Change-Id: Iad8cb4b1980240aa0f3a442d3e7591d2c7af9497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412057
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963027}

--- chrome/browser/password_manager/password_store_utils.cc
+++ chrome/browser/password_manager/password_store_utils.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/password_reuse_manager_factory.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
@@ -81,6 +82,7 @@ class StoreMetricReporterHelper : public base::SupportsUserData::Data {
             profile_store, account_store, sync_service, identity_manager,
             pref_service, password_reuse_manager,
             IsUnderAdvancedProtection(profile_),
+            ProfileManager::GetPrimaryUserProfile() == profile_,
             base::BindOnce(
                 &StoreMetricReporterHelper::RemoveInstanceFromProfileUserData,
                 weak_ptr_factory_.GetWeakPtr()));
