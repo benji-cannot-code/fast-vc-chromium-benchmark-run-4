8835ee29255bfd824907092d8d7140ec3fd0ca49
Ryan Keane <rwkeane@google.com>
2022-01-25 23:30:03 +0000

    [CWR] Fix All Apps Crashing
    This CL fixes a bug where all apps are crashing at the start of
    playback. This fix has been landed downstream and this CL upstreams it
    back to Chromium.
    
    Bug: b/215612546
    Change-Id: Ie19ec2982cc5759a2cc9b7a1d6cfe1bbb68d7b68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415650
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#963269}

--- chromecast/cast_core/runtime/browser/web_runtime_application.cc
+++ chromecast/cast_core/runtime/browser/web_runtime_application.cc
@@ -69,7 +69,7 @@ void WebRuntimeApplication::InitializeApplication(
 }
 
 bool WebRuntimeApplication::IsStreamingApplication() const {
-  return true;
+  return false;
 }
 
 void WebRuntimeApplication::InnerContentsCreated(
