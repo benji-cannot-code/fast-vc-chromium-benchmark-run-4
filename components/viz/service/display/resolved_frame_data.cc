1b47c04687dd5bcc4c4ef1d5986972cd3e3bb8c7
kylechar <kylechar@chromium.org>
2022-01-25 20:16:07 +0000

    Move will_draw into AggregationPassData
    During SurfaceAggregator prewalk `will_draw` is computed to tell if a
    surface / render pass is embedded from the root surface root render
    pass. Move this property into AggregationPassData so it can be used
    later during aggregation.
    
    Bug: 1288950
    Change-Id: I575a73df54912f291d4c694136a003014ea67b8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3401661
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963155}

--- components/viz/service/display/resolved_frame_data.cc
+++ components/viz/service/display/resolved_frame_data.cc
@@ -193,6 +193,10 @@ bool ResolvedFrameData::CheckIfUsedAndReset() {
   return std::exchange(used_, false);
 }
 
+bool ResolvedFrameData::WillDraw() const {
+  return GetRootRenderPassData().aggregation().will_draw;
+}
+
 ResolvedPassData& ResolvedFrameData::GetRenderPassDataById(
     CompositorRenderPassId render_pass_id) {
   return const_cast<ResolvedPassData&>(
