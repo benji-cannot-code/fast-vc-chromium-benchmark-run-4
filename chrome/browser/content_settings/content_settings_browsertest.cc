f8526dcfda8a8870993ab4da6638662dbb641f1e
Kevin Marshall <kmarshall@chromium.org>
2022-01-25 13:33:42 +0000

    [chrome] Make include of plugin_service.h depend on ENABLE_PLUGINS.
    Makes an include conditional on platform support for PPAPI.
    
    Bug: 1271639
    Change-Id: I8097cb6bc991a295c625344057284605f3fb6080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3408355
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962962}

--- chrome/browser/content_settings/content_settings_browsertest.cc
+++ chrome/browser/content_settings/content_settings_browsertest.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/cookie_access_details.h"
 #include "content/public/browser/navigation_handle.h"
-#include "content/public/browser/plugin_service.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
@@ -66,6 +65,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_MAC)
 #include "base/mac/scoped_nsautorelease_pool.h"
+#endif
+
+#if BUILDFLAG(ENABLE_PLUGINS)
+#include "content/public/browser/plugin_service.h"
 #endif
 
 using content::BrowserThread;
