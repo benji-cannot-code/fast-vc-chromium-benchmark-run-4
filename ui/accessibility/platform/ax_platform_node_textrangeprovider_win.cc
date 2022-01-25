d7a0fd7bb5dfe75dcbaa8b7538934bdc8c7e0eab
Nektarios Paisios <nektar@chromium.org>
2022-01-25 18:39:03 +0000

    Fixes handling of empty and all whitespace objects in AXPosition and AXRange
    Makes several improvements to how empty and all whitespace AXPositions are handled.
    (In AXPosition, a position on an empty object is defined as one anchored to
    a node that does not contribute anything to the tree's text representation but on which nevertheless
    assistive software should stop when navigating by character, word, or sentence.)
    
    The improvements are:
    1. Nodes that are ignored for text navigation should also count as empty objects because they affect paragraph breaks
    and because they are indeed empty objects according to our above definition.
    Some examples of such objects are empty generic containers in non-editable text.
    In UI Automation, for example, empty generic containers in non-editable text are
    ignored when the user navigates through them because we don't want the user to stop on an object that
    doesn't announce anything. However, such containers should nevertheless cause the
    next / previous node in the tree to be placed in a separate paragraph, so they should not be
    "invisible" to AXPosition handling.
    2. Nodes with no text should not be considered as being whitespace because they neither
    form line breaks, nor do they affect paragraph breaks in AXRange.
    	3. Empty leaf positions that are "at end of anchor" need to have the same child index as
    non-empty ones, because it affects how AXRange::GetText() determines whether to retrieve the text or not.
    As an example, previously, one position at start and another at end
    of anchor would mistakenly compare as equal.
    4. Empty objects should form a paragraph and a word boundary only if they are unignored.
    For example, if a text field is empty but aria-hidden and thus ignored, it should not form
    a word boundary.
    5. When computing the leaf equivalent position we should be careful not to
    switch from the initial tree position to a text position right away,
    because we might lose the information that the child index provides to us
    if the tree's text representation is made up of empty leaf nodes.
    ((See AsLeafTextPositionWithTreePositionAndEmptyLeafDescendants in AXPosition's unittests.
    for an example)
    
    In order to finish making tests work I had to fix
    AXPosition::operator!= so that two non-comparable positions are determined to be unequal.
    The above was certainly a logic bug.
    
    Also took the opportunity to start removing the reliance on a global state
    for the "embedded object behavior" so that tests would produce the same results on all platforms.
    A followup patch should remove the global state completely in favor of having the parameter
    passed into AXPosition's constructor.
    
    R=aleventhal@chromium.org, kschmi@microsoft.com
    
    AXRelnotes: Improved paragraph and word navigation in certain very rare scenarios.
    Bug: 1165853
    Change-Id: I86993a25e264d0c2826d979f001c105fa886ca97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407636
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963095}

--- ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
+++ ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
@@ -1071,7 +1071,8 @@ std::u16string AXPlatformNodeTextRangeProviderWin::GetString(
     size_t* appended_newlines_count) {
   AXNodeRange range(start()->Clone(), end()->Clone());
   return range.GetText(AXTextConcatenationBehavior::kWithParagraphBreaks,
-                       max_count, false, appended_newlines_count);
+                       AXEmbeddedObjectBehavior::kExposeCharacter, max_count,
+                       false, appended_newlines_count);
 }
 
 AXPlatformNodeWin* AXPlatformNodeTextRangeProviderWin::GetOwner() const {
