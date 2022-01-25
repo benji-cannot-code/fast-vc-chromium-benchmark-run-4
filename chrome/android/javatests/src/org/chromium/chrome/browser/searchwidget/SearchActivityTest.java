142accc0f91030746eae0c749426f2c3930992b0
Rohit Agarwal <roagarwal@chromium.org>
2022-01-25 13:54:24 +0000

    [Sheriff] Disable test SearchActivityTest#testNewIntentDiscardsQuery.
    Bug: 1166647
    Change-Id: I6f7986483c5f8734fd94e8d5abae5449131166fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412267
    Owners-Override: Rohit Agarwal <roagarwal@chromium.org>
    Auto-Submit: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#962969}

--- chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
@@ -480,6 +480,7 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/1166647")
     public void testNewIntentDiscardsQuery() {
         final SearchActivity searchActivity = startSearchActivity();
         // Note: we should not need to request focus here.
