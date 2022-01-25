b78dc16d7845c525c281ed045bd8eacf35638468
Emilia Paz <emiliapaz@chromium.org>
2022-01-25 23:28:58 +0000

    [Extensions] Add discover more button in extensions tab
    Bug: 1263311
    Change-Id: I20684dbb824576c0d4562fc31f180f54ba923251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402878
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963267}

--- chrome/browser/ui/chrome_pages.cc
+++ chrome/browser/ui/chrome_pages.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/common/constants.h"
+#include "extensions/common/extension_urls.h"
 #include "net/base/escape.h"
 #include "net/base/url_util.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -436,6 +437,11 @@ void ShowAboutChrome(Browser* browser) {
 void ShowSearchEngineSettings(Browser* browser) {
   base::RecordAction(UserMetricsAction("EditSearchEngines"));
   ShowSettingsSubPage(browser, kSearchEnginesSubPage);
+}
+
+void ShowWebStore(Browser* browser) {
+  ShowSingletonTabIgnorePathOverwriteNTP(
+      browser, extension_urls::GetWebstoreLaunchURL());
 }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
