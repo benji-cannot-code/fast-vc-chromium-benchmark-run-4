46078f29fa9540c02e0527da8f3ed4ba88b04a0f
Yury Khmel <khmel@chromium.org>
2022-01-25 23:17:33 +0000

    arc: Fix crash on performance tracing for Ghost window.
    Introducing ARC ghost window also introduced the new sequence for
    widget/surface destruction. In last case surface is discarded first and
    it is implemented this way that correlation native window and surface is
    broken so ARC performance tracing is unable to remove Surface observer
    safely. This CL changed concept of explicit observer adding/removing to
    scoped. There is another problem when ARC ghost window should not be
    handled by ARC tracing and it will be addressed in next CL
    
    BUG=b:206885759
    BUG=b:216177169
    TEST=add unit test that simulates crash conditions and
         unit_tests --gtest_filter=*ArcAppPerformanceTracingTest*
    
    Change-Id: If2c899c5aaee1794e055c6b515dca1b655a46247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414102
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963261}

--- chrome/browser/ash/arc/tracing/arc_app_performance_tracing_unittest.cc
+++ chrome/browser/ash/arc/tracing/arc_app_performance_tracing_unittest.cc
@@ -86,7 +86,7 @@ class ArcAppPerformanceTracingTest : public BrowserWithTestWindowTest {
   }
 
   void TearDown() override {
-    shell_root_surface_.reset();
+    ResetRootSurface();
     tracing_helper_.TearDown();
     arc_test_.TearDown();
     BrowserWithTestWindowTest::TearDown();
@@ -112,6 +112,8 @@ class ArcAppPerformanceTracingTest : public BrowserWithTestWindowTest {
     DCHECK(tracing_helper().GetTracingSession()->tracing_active());
     return arc_widget;
   }
+
+  void ResetRootSurface() { shell_root_surface_.reset(); }
 
   ArcAppPerformanceTracingTestHelper& tracing_helper() {
     return tracing_helper_;
@@ -296,6 +298,17 @@ TEST_F(ArcAppPerformanceTracingTest, TimeToFirstFrameRendered) {
   DCHECK(samples.get());
   EXPECT_EQ(0, samples->TotalCount());
 
+  arc_widget->Close();
+}
+
+// This test verifies the case when surface is destroyed before window close.
+TEST_F(ArcAppPerformanceTracingTest, DestroySurface) {
+  views::Widget* const arc_widget = StartArcFocusAppTracing();
+  ASSERT_TRUE(tracing_helper().GetTracingSession());
+  EXPECT_TRUE(tracing_helper().GetTracingSession()->tracing_active());
+  ResetRootSurface();
+  ASSERT_TRUE(tracing_helper().GetTracingSession());
+  EXPECT_FALSE(tracing_helper().GetTracingSession()->tracing_active());
   arc_widget->Close();
 }
 
