79db1271c226c1591e818b797ea4f9dc9f6f7c67
Rouslan Solomakhin <rouslan@chromium.org>
2022-01-25 14:43:24 +0000

    [Web Payment] Record "canmakepayment" and IS_READY_TO_PAY result.
    Bug: 1290475
    Change-Id: I4ca891a0f2534061dc4d35e83d8399820fee8bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413015
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962978}

--- components/payments/content/android/java/src/org/chromium/components/payments/intent/IsReadyToPayServiceHelper.java
+++ components/payments/content/android/java/src/org/chromium/components/payments/intent/IsReadyToPayServiceHelper.java
@@ -15,6 +15,7 @@ import android.os.RemoteException;
 
 import org.chromium.IsReadyToPayService;
 import org.chromium.IsReadyToPayServiceCallback;
+import org.chromium.base.metrics.RecordHistogram;
 
 /** A helper to query the payment app's IsReadyToPay service. */
 public class IsReadyToPayServiceHelper
@@ -132,6 +133,8 @@ public class IsReadyToPayServiceHelper
     @Override
     public void handleIsReadyToPay(boolean isReadyToPay) throws RemoteException {
         if (mResultHandler == null) return;
+        RecordHistogram.recordBooleanHistogram(
+                "PaymentRequest.EventResponse.IsReadyToPay", isReadyToPay);
         mResultHandler.onIsReadyToPayServiceResponse(isReadyToPay);
         mResultHandler = null;
         destroy();
--- components/payments/content/service_worker_payment_app.cc
+++ components/payments/content/service_worker_payment_app.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/payments/content/payment_event_response_util.h"
@@ -205,6 +206,8 @@ void ServiceWorkerPaymentApp::OnCanMakePaymentEventResponded(
   // |can_make_payment| is true as long as there is a matching payment handler.
   can_make_payment_result_ = true;
   has_enrolled_instrument_result_ = response->can_make_payment;
+  base::UmaHistogramBoolean("PaymentRequest.EventResponse.CanMakePayment",
+                            response->can_make_payment);
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE,
       base::BindOnce(std::move(callback), this, can_make_payment_result_));
