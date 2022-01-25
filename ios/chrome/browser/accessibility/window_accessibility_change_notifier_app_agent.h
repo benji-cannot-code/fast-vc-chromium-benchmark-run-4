13a9d61156bfe12c222aaba9554cdf4035ad9f6a
Darshan Sen <raisinten@gmail.com>
2022-01-25 13:02:32 +0000

    Fix a typo in WindowAccessibityChangeNotifierAppAgent by renaming it to WindowAccessibilityChangeNotifierAppAgent.
    Change-Id: I1385eee2f8be460d40dc44d19e86ed3c839c05d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414180
    Auto-Submit: Darshan Sen <raisinten@gmail.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#962957}

--- ios/chrome/browser/accessibility/window_accessibility_change_notifier_app_agent.h
+++ ios/chrome/browser/accessibility/window_accessibility_change_notifier_app_agent.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // An app agent that montitors the number of visible (that is: active) scenes
 // and provides accessibility notifications when this count changes.
-@interface WindowAccessibityChangeNotifierAppAgent : NSObject <AppStateAgent>
+@interface WindowAccessibilityChangeNotifierAppAgent : NSObject <AppStateAgent>
 @end
 
 #endif  // IOS_CHROME_BROWSER_ACCESSIBILITY_WINDOW_ACCESSIBILITY_CHANGE_NOTIFIER_APP_AGENT_H_
