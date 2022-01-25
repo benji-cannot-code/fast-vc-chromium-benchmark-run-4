2d667145d261ab34ded393e42efa4cdca7df2b31
Aya ElAttar <ayaelattar@chromium.org>
2022-01-25 15:44:47 +0000

    sheriff: disable VariationsSafeModeEndToEndBrowserTest.ExtendedSafeModeEndToEnd on mac
    Bug: 1290822
    Change-Id: I4be1320f9a8bf0fb65468c321fff9bb126398c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414777
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Owners-Override: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963000}

--- chrome/browser/metrics/variations/variations_safe_mode_end_to_end_browsertest.cc
+++ chrome/browser/metrics/variations/variations_safe_mode_end_to_end_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/launcher/test_launcher.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_switches.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -181,7 +182,13 @@ class VariationsSafeModeEndToEndBrowserTest : public ::testing::Test {
   base::FilePath local_state_file_;
 };
 
-TEST_F(VariationsSafeModeEndToEndBrowserTest, ExtendedSafeModeEndToEnd) {
+// TODO(crbug.com/1290822): Test failed on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_ExtendedSafeModeEndToEnd DISABLED_ExtendedSafeModeEndToEnd
+#else
+#define MAYBE_ExtendedSafeModeEndToEnd ExtendedSafeModeEndToEnd
+#endif
+TEST_F(VariationsSafeModeEndToEndBrowserTest, MAYBE_ExtendedSafeModeEndToEnd) {
   // Reuse the browser_tests binary (i.e., that this test code is in), to
   // manually run the sub-test.
   base::CommandLine sub_test =
