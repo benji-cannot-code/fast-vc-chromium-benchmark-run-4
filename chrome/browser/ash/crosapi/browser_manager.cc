78a99d7c3535b0e055dcab506caa281cfb3239a8
Yuta Hijikata <ythjkt@google.com>
2022-01-26 02:00:33 +0000

    Split out methods to _util.* file.
    This patch is part of a refactor to make `BrowserDataMigratorImpl` only
    responsible for high level coordination of the migration such as
    determining if migration should happen and initiating the migration. The
    actual migration will be handled by two separate classes, one for copy
    migration and one for move migration.
    
    This patch moves methods that can be shared between copy and move
    migration logic to _util.cc.
    
    Test: unit_tests
    Bug: 1261730
    Change-Id: I5a1b90bdfaa39479e5a75488ed4c3357299c85f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414181
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963344}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/ash/app_restore/full_restore_service.h"
 #include "chrome/browser/ash/crosapi/browser_data_migrator.h"
+#include "chrome/browser/ash/crosapi/browser_data_migrator_util.h"
 #include "chrome/browser/ash/crosapi/browser_loader.h"
 #include "chrome/browser/ash/crosapi/browser_service_host_ash.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
@@ -578,7 +579,7 @@ void BrowserManager::InitializeAndStart() {
   // inside the profile data directory.
   base::ThreadPool::PostTask(
       FROM_HERE, {base::MayBlock()},
-      base::BindOnce(&ash::BrowserDataMigratorImpl::DryRunToCollectUMA,
+      base::BindOnce(&ash::browser_data_migrator_util::DryRunToCollectUMA,
                      ProfileManager::GetPrimaryUserProfile()->GetPath()));
 }
 
