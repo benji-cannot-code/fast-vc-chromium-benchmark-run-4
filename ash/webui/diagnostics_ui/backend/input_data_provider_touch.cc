767a5d80069ec68faeed40f9bc78286e6ab495d0
Harry Cutts <hcutts@chromium.org>
2022-01-26 23:18:40 +0000

    input: Add keyboard event observation to input diagnostics
    Support observation interface for key events from input
    diagnostics; only available through binding to
    InputDataProvider, and constrained to only deliver events
    when the the diagnostics window is visible and focused.
    
    Bug: 1207678
    Test: ./out/Debug/ash_webui_unittests --gtest_filter=*InputDataProviderTest*
    Test: ./out/Debug/browser_tests --gtest_filter='DiagnosticsAppWithInput.*'
    Test: tested with subsequent patches on Jinlon, for internal and external keyboards
    Change-Id: I4394052889e9ff2d7b6fb9d476e0d8fc4471cf67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3280162
    Reviewed-by: Harry Cutts <hcutts@chromium.org>
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Auto-Submit: Kenneth Albanowski <kenalba@google.com>
    Commit-Queue: Kenneth Albanowski <kenalba@google.com>
    Cr-Commit-Position: refs/heads/main@{#963770}

--- ash/webui/diagnostics_ui/backend/input_data_provider_touch.cc
+++ ash/webui/diagnostics_ui/backend/input_data_provider_touch.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/diagnostics_ui/backend/input_data_provider_touch.h"
 #include "ash/webui/diagnostics_ui/backend/input_data_provider.h"
-#include "base/logging.h"
 #include "ui/events/ozone/evdev/event_device_info.h"
 
 namespace ash {
