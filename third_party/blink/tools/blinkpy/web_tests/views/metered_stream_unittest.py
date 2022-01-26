03c1eb8b2b4ba61adf2980dbd397b6e4e80e3225
Preethi Mohan <preethim@google.com>
2022-01-26 18:16:04 +0000

    PY3 migration(blinkpy unit tests): blinkpy.web_tests.views suite to PY3.
    All tests are now passing in blinkpy.web_tests.views.
    vpython3 ./run_blinkpy_tests.py blinkpy.web_tests.views
    
    Bug: 1218641
    
    Change-Id: Ia205b6bbefa5d03ca39f953322811086beaa7df4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416505
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Preethi Mohan <preethim@google.com>
    Cr-Commit-Position: refs/heads/main@{#963596}

--- third_party/blink/tools/blinkpy/web_tests/views/metered_stream_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/views/metered_stream_unittest.py
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import logging
 import re
+import six
 import unittest
 
 from blinkpy.web_tests.views.metered_stream import MeteredStream
@@ -42,7 +43,6 @@ class RegularTest(unittest.TestCase):
 
     def setUp(self):
         self.stream = StringIO()
-        self.buflist = self.stream.buflist
         self.stream.isatty = lambda: self.isatty
 
         # configure a logger to test that log calls do normally get included.
@@ -51,7 +51,7 @@ class RegularTest(unittest.TestCase):
         self.logger.propagate = False
 
         # add a dummy time counter for a default behavior.
-        self.times = range(10)
+        self.times = list(range(10))
 
         self.meter = MeteredStream(self.stream, self.verbose, self.logger,
                                    self.time_fn, 8675)
@@ -79,7 +79,7 @@ class RegularTest(unittest.TestCase):
             self.meter.write_throttled_update('foo')
             self.meter.write_update('bar')
             self.meter.write('baz')
-            self.assertEqual(logging_stream.buflist, [])
+            self.assertEqual(logging_stream.getvalue().splitlines(), [])
         finally:
             root_logger.removeHandler(handler)
             root_logger.setLevel(orig_level)
@@ -92,24 +92,24 @@ class RegularTest(unittest.TestCase):
         self.meter.write_throttled_update('baz 2')
         self.meter.writeln('done')
         self.assertEqual(self.times, [])
-        return self.buflist
+        return self.stream.getvalue().splitlines()
 
     def test_basic(self):
         buflist = self._basic([0, 1, 2, 13, 14])
-        self.assertEqual(buflist, ['foo\n', 'bar\n', 'baz 2\n', 'done\n'])
+        self.assertEqual(buflist, ['foo', 'bar', 'baz 2', 'done'])
 
     def _log_after_update(self):
         self.meter.write_update('foo')
         self.logger.info('bar')
-        return self.buflist
+        return self.stream.getvalue().splitlines()
 
     def test_log_after_update(self):
         buflist = self._log_after_update()
-        self.assertEqual(buflist, ['foo\n', 'bar\n'])
+        self.assertEqual(self.stream.getvalue().splitlines(), ['foo', 'bar'])
 
     def test_log_args(self):
         self.logger.info('foo %s %d', 'bar', 2)
-        self.assertEqual(self.buflist, ['foo bar 2\n'])
+        self.assertEqual(self.stream.getvalue().splitlines(), ['foo bar 2'])
 
 
 class TtyTest(RegularTest):
@@ -119,23 +119,30 @@ class TtyTest(RegularTest):
     def test_basic(self):
         buflist = self._basic([0, 1, 1.05, 1.1, 2])
         self.assertEqual(buflist, [
-            'foo',
-            MeteredStream._erasure('foo'), 'bar',
-            MeteredStream._erasure('bar'), 'baz 2',
-            MeteredStream._erasure('baz 2'), 'done\n'
+            'foo' + MeteredStream._erasure('foo') + 'bar' +
+            MeteredStream._erasure('bar') + 'baz 2' +
+            MeteredStream._erasure('baz 2') + 'done'
         ])
 
     def test_log_after_update(self):
         buflist = self._log_after_update()
         self.assertEqual(buflist,
-                         ['foo', MeteredStream._erasure('foo'), 'bar\n'])
+                         ['foo' + MeteredStream._erasure('foo') + 'bar'])
 
     def test_bytestream(self):
         self.meter.write('German umlauts: \xe4\xf6\xfc')
         self.meter.write(u'German umlauts: \xe4\xf6\xfc')
-        self.assertEqual(
-            self.buflist,
-            ['German umlauts: \xe4\xf6\xfc', u'German umlauts: \xe4\xf6\xfc'])
+        if six.PY2:
+            # TODO(preethim) : self.stream.getvalue() was giving unicode error.
+            # continued with buflist for now.
+            self.assertEqual(self.stream.buflist, [
+                'German umlauts: \xe4\xf6\xfc', u'German umlauts: \xe4\xf6\xfc'
+            ])
+
+        else:
+            self.assertEqual(self.stream.getvalue().splitlines(), [
+                'German umlauts: \xe4\xf6\xfc' + 'German umlauts: \xe4\xf6\xfc'
+            ])
 
 
 class VerboseTest(RegularTest):
@@ -146,23 +153,22 @@ class VerboseTest(RegularTest):
         buflist = self._basic([0, 1, 2.1, 13, 14.1234])
         # We don't bother to match the hours and minutes of the timestamp since
         # the local timezone can vary and we can't set that portably and easily.
-        self.assertTrue(re.match(r'\d\d:\d\d:00.000 8675 foo\n', buflist[0]))
-        self.assertTrue(re.match(r'\d\d:\d\d:01.000 8675 bar\n', buflist[1]))
-        self.assertTrue(re.match(r'\d\d:\d\d:13.000 8675 baz 2\n', buflist[2]))
-        self.assertTrue(re.match(r'\d\d:\d\d:14.123 8675 done\n', buflist[3]))
+        self.assertTrue(re.match(r'\d\d:\d\d:00.000 8675 foo', buflist[0]))
+        self.assertTrue(re.match(r'\d\d:\d\d:01.000 8675 bar', buflist[1]))
+        self.assertTrue(re.match(r'\d\d:\d\d:13.000 8675 baz 2', buflist[2]))
+        self.assertTrue(re.match(r'\d\d:\d\d:14.123 8675 done', buflist[3]))
         self.assertEqual(len(buflist), 4)
 
     def test_log_after_update(self):
         buflist = self._log_after_update()
-        self.assertTrue(re.match(r'\d\d:\d\d:00.000 8675 foo\n', buflist[0]))
+        self.assertTrue(re.match(r'\d\d:\d\d:00.000 8675 foo', buflist[0]))
 
         # The second argument should have a real timestamp and pid, so we just check the format.
-        self.assertTrue(
-            re.match(r'\d\d:\d\d:\d\d.\d\d\d \d+ bar\n', buflist[1]))
+        self.assertTrue(re.match(r'\d\d:\d\d:\d\d.\d\d\d \d+ bar', buflist[1]))
 
         self.assertEqual(len(buflist), 2)
 
     def test_log_args(self):
         self.logger.info('foo %s %d', 'bar', 2)
-        self.assertEqual(len(self.buflist), 1)
-        self.assertTrue(self.buflist[0].endswith('foo bar 2\n'))
+        self.assertEqual(len(self.stream.getvalue().splitlines()), 1)
+        self.assertTrue(self.stream.getvalue().endswith('foo bar 2\n'))
