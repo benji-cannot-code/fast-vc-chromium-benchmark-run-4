e2c58028080c6ae4800da5074568607633c4356f
Florian Gauger <fga@google.com>
2022-01-26 11:11:23 +0000

    [Autofill Assistant] Move remaining dependency implementations into public/
    We are in the process of moving all Autofill Assistant code to //components. For this we need to remove all dependencies on Chrome code.
    
    In this cl we move the Chrome dependencies out of the DFM to simplify
    their creation.
    
    Bug: b/173103628
    Change-Id: I12d5aba2dbc5aba656db9077ff1892c05b7fcefa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416873
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#963460}

--- chrome/browser/android/autofill_assistant/dependencies_chrome.cc
+++ chrome/browser/android/autofill_assistant/dependencies_chrome.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/scoped_java_ref.h"
 #include "base/strings/string_piece.h"
-#include "chrome/android/features/autofill_assistant/jni_headers/AssistantStaticDependenciesChrome_jni.h"
+#include "chrome/android/features/autofill_assistant/jni_headers_public/AssistantStaticDependenciesChrome_jni.h"
 #include "chrome/browser/android/autofill_assistant/annotate_dom_model_service_factory.h"
 #include "chrome/browser/android/autofill_assistant/assistant_field_trial_util.h"
 #include "chrome/browser/browser_process.h"
