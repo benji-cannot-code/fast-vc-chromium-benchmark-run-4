731fd76bde12e110d386c030392761177a55c163
Dan Clark <daniec@microsoft.com>
2022-01-26 19:34:07 +0000

    Reland: [selectmenu] Defer missing parts console warnings
    Originally reverted due to a build break noted in crbug.com/1289333.
    The cause was that node_signals.cc was previously getting the
    definition for Node::HasTagName from a chain of #includes starting
    with shadow_root.h --> html_select_menu_element.h.
    The reland has node_signals.cc include html_element.h directly.
    
    Original change description:
    Currently <selectmenu> synchronously logs console warnings when a
    button or listbox part is removed (or slotted out) without a
    replacement.
    
    Unfortunately there are some typical scenarios where we send this
    warning even though a replacement part is just about to be inserted.
    For example:
    
    <selectmenu>
      <div slot="button">
        <div behavior="button">Custom button</div>
      </div>
      <option>one</option>
      ...
    </selectmenu>
    
    The slotting for div[slot=button] is handled before the
    div[behavior=button] is parsed, so we log a missing part warning
    even though the slot is about to be filled with a new button
    part.
    
    This issue also occurs when a block of script synchronously removes
    and then replaces a button or listbox part.
    
    To prevent developer confusion from these premature error messages,
    defer the warnings about missing parts until they are found to
    be missing after a lifecycle update, by which point they should
    be indicative of a real issue.
    
    I un-inlined ShadowRoot::UpdateType() in order to eliminate
    the html_select_menu_element.h #include from shadow_root.h,
    because that was causing us to bump up against the
    max_tokens_here pragma in input_event.cc.
    
    Change-Id: I384728aabfd1144eb28cdcfafba3e426622011ef
    Bug: 1121840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405264
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#963640}

--- third_party/blink/renderer/modules/autofill_assistant/node_signals.cc
+++ third_party/blink/renderer/modules/autofill_assistant/node_signals.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/qualified_name.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/space_split_string.h"
+#include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style/computed_style_base_constants.h"
