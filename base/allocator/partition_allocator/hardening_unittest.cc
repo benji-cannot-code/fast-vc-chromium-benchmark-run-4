2f22995125089e7cecb154d939c5883e8892f0ad
Bartek Nowierski <bartekn@chromium.org>
2022-01-26 16:35:57 +0000

    [PA] Refactor freelist code
    This achieves a few things:
    - better encapsulates the encoded pointer and make the logic clearer
    - reduces number of reinterpret_casts
    - better defines boundary where MTE tag should be used
    
    Bug: 1280844
    Change-Id: I989924d43256dbba57774235e1553dba902156ba
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403878
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963545}

--- base/allocator/partition_allocator/hardening_unittest.cc
+++ base/allocator/partition_allocator/hardening_unittest.cc
@@ -3,11 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
 #include "base/allocator/partition_allocator/partition_alloc_config.h"
 #include "base/allocator/partition_allocator/partition_freelist_entry.h"
+#include "base/allocator/partition_allocator/partition_page.h"
 #include "base/allocator/partition_allocator/partition_root.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -42,14 +44,13 @@ TEST(HardeningTest, PartialCorruption) {
   root.Free(data2);
   root.Free(data);
 
-  // root->bucket->active_slot_span_head->freelist_head is data, next is data2.
-  // We can corrupt *data to get a new pointer next.
-
-  // However even if the freelist entry looks reasonable (valid encoded
-  // pointer), freelist corruption detection will make the code crash.
-  *reinterpret_cast<EncodedPartitionFreelistEntry**>(data) =
-      PartitionFreelistEntry::Encode(
-          reinterpret_cast<PartitionFreelistEntry*>(to_corrupt));
+  // root->bucket->active_slot_span_head->freelist_head points to data, next_
+  // points to data2. We can corrupt *data to get overwrite the next_ pointer.
+  // Even if it looks reasonable (valid encoded pointer), freelist corruption
+  // detection will make the code crash, because shadow_ doesn't match
+  // encoded_next_.
+  PartitionFreelistEntry::EmplaceAndInitForTest(
+      root.AdjustPointerForExtrasSubtract(data), to_corrupt, false);
   EXPECT_DEATH(root.Alloc(kAllocSize, ""), "");
 }
 
@@ -73,12 +74,10 @@ TEST(HardeningTest, OffHeapPointerCrashing) {
   root.Free(data2);
   root.Free(data);
 
-  // See "PartialCorruption" above for details.
-  uintptr_t* data_ptr = reinterpret_cast<uintptr_t*>(data);
-  *data_ptr = reinterpret_cast<uintptr_t>(PartitionFreelistEntry::Encode(
-      reinterpret_cast<PartitionFreelistEntry*>(to_corrupt)));
-  // This time, make the second pointer consistent.
-  *(data_ptr + 1) = ~(*data_ptr);
+  // See "PartialCorruption" above for details. This time, make shadow_
+  // consistent.
+  PartitionFreelistEntry::EmplaceAndInitForTest(
+      root.AdjustPointerForExtrasSubtract(data), to_corrupt, true);
 
   // Crashes, because |to_corrupt| is not on the same superpage as data.
   EXPECT_DEATH(root.Alloc(kAllocSize, ""), "");
@@ -101,17 +100,11 @@ TEST(HardeningTest, MetadataPointerCrashing) {
   root.Free(data2);
   root.Free(data);
 
-  uintptr_t data_address = reinterpret_cast<uintptr_t>(data);
-  uintptr_t metadata_address =
-      data_address & kSuperPageBaseMask + SystemPageSize();
+  auto* metadata = SlotSpanMetadata<ThreadSafe>::FromSlotInnerPtr(data);
+  PartitionFreelistEntry::EmplaceAndInitForTest(
+      root.AdjustPointerForExtrasSubtract(data), metadata, true);
 
-  uintptr_t* data_ptr = reinterpret_cast<uintptr_t*>(data);
-  *data_ptr = reinterpret_cast<uintptr_t>(PartitionFreelistEntry::Encode(
-      reinterpret_cast<PartitionFreelistEntry*>(metadata_address)));
-  // This time, make the second pointer consistent.
-  *(data_ptr + 1) = ~(*data_ptr);
-
-  // Crashes, because |metadata_address| points inside the metadata area.
+  // Crashes, because |metadata| points inside the metadata area.
   EXPECT_DEATH(root.Alloc(kAllocSize, ""), "");
 }
 #endif  // !BUILDFLAG(IS_ANDROID) && defined(GTEST_HAS_DEATH_TEST) &&
@@ -140,11 +133,8 @@ TEST(HardeningTest, SuccessfulCorruption) {
   root.Free(data2);
   root.Free(data);
 
-  uintptr_t* data_ptr = reinterpret_cast<uintptr_t*>(data);
-  *data_ptr = reinterpret_cast<uintptr_t>(PartitionFreelistEntry::Encode(
-      reinterpret_cast<PartitionFreelistEntry*>(to_corrupt)));
-  // If we don't have PA_HAS_FREELIST_HARDENING, this is not needed.
-  *(data_ptr + 1) = ~(*data_ptr);
+  PartitionFreelistEntry::EmplaceAndInitForTest(
+      root.AdjustPointerForExtrasSubtract(data), to_corrupt, true);
 
   // Next allocation is what was in
   // root->bucket->active_slot_span_head->freelist_head, so not the corrupted
