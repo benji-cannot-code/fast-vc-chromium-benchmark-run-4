1b9383715dd452a4538494e0a0a23cf86908778d
mcrouse <mcrouse@google.com>
2022-01-26 20:25:12 +0000

    Deflake Visit Batch Annotations tests.
    Bug: 1289353,1289353
    Change-Id: I58ad5b97e9d5b60d5b12214db39dcc04f490ad7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416694
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963675}

--- components/optimization_guide/content/browser/page_content_annotations_service.cc
+++ components/optimization_guide/content/browser/page_content_annotations_service.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_enums.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
+#include "components/optimization_guide/core/optimization_guide_switches.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "services/metrics/public/cpp/metrics_utils.h"
@@ -140,9 +141,13 @@ void PageContentAnnotationsService::Annotate(const HistoryVisit& visit) {
 #if BUILDFLAG(BUILD_WITH_TFLITE_LIB)
   if (!visit.text_to_annotate)
     return;
+  // Used for testing.
+  LOCAL_HISTOGRAM_BOOLEAN(
+      "PageContentAnnotations.AnnotateVisit.AnnotationRequested", true);
   visits_to_annotate_.emplace_back(visit);
   if (visits_to_annotate_.size() >= features::AnnotateVisitBatchSize()) {
     if (current_visit_annotation_batch_.empty()) {
+      // Used for testing.
       LOCAL_HISTOGRAM_BOOLEAN(
           "PageContentAnnotations.AnnotateVisit.BatchAnnotationStarted", true);
       current_visit_annotation_batch_ = std::move(visits_to_annotate_);
@@ -152,9 +157,11 @@ void PageContentAnnotationsService::Annotate(const HistoryVisit& visit) {
     // The queue is full and an batch annotation is actively being done so
     // we will remove the "oldest" visit.
     visits_to_annotate_.erase(visits_to_annotate_.begin());
+    // Used for testing.
     LOCAL_HISTOGRAM_BOOLEAN(
         "PageContentAnnotations.AnnotateVisit.QueueFullVisitDropped", true);
   }
+  // Used for testing.
   LOCAL_HISTOGRAM_BOOLEAN(
       "PageContentAnnotations.AnnotateVisit.AnnotationRequestQueued", true);
 #endif
@@ -164,8 +171,11 @@ void PageContentAnnotationsService::Annotate(const HistoryVisit& visit) {
 void PageContentAnnotationsService::AnnotateVisitBatch() {
   DCHECK(!current_visit_annotation_batch_.empty());
 
-  // if (pause_execution_for_testing_)
-  // return;
+  if (switches::StopHistoryVisitBatchAnnotateForTesting()) {
+    // Code beyond this is tested in multiple places. This just ensures the
+    // calls up to this point can be more easily configured.
+    return;
+  }
 
   if (current_visit_annotation_batch_.empty()) {
     return;
--- components/optimization_guide/core/optimization_guide_switches.h
+++ components/optimization_guide/core/optimization_guide_switches.h
@@ -35,6 +35,7 @@ extern const char kDisableModelDownloadVerificationForTesting[];
 extern const char kModelOverride[];
 extern const char kDebugLoggingEnabled[];
 extern const char kModelValidate[];
+extern const char kStopHistoryVisitBatchAnnotateForTesting[];
 
 // Returns whether the hint component should be processed.
 // Available hint components are only processed if a proto override isn't being
@@ -92,6 +93,10 @@ absl::optional<std::string> GetModelOverride();
 
 // Returns true if debug logs are enabled for the optimization guide.
 bool IsDebugLogsEnabled();
+
+// Whether to prevent annotations from happening when in a batch. For testing
+// purposes only.
+bool StopHistoryVisitBatchAnnotateForTesting();
 
 }  // namespace switches
 }  // namespace optimization_guide
