8dd7bc47ec38b5426775c42a59d4b9823fc3b685
dpapad <dpapad@chromium.org>
2022-01-26 09:50:17 +0000

    NewTabPage: Migrate voice_search_overlay.js, mini_page.js to TypeScript.
    Bug: 1273590
    Change-Id: I1eb5abc14dd278eeaccba876b34c6807bef709cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414611
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963447}

--- chrome/browser/resources/new_tab_page/app.js
+++ chrome/browser/resources/new_tab_page/app.js
@@ -442,7 +442,12 @@ class AppElement extends mixinBehaviors
     }
   }
 
-  /** @private */
+  /**
+   * TODO(crbug.com/1273590): Remove suppression when app.js is converted to
+   * TypeScript.
+   * @suppress {checkTypes}
+   * @private
+   */
   onOpenVoiceSearch_() {
     this.showVoiceSearchOverlay_ = true;
     recordVoiceAction(VoiceAction.kActivateSearchBox);
@@ -470,6 +475,9 @@ class AppElement extends mixinBehaviors
    * voice search.
    * @param {KeyboardEvent} e
    * @private
+   * TODO(crbug.com/1273590): Remove suppression when app.js is converted to
+   * TypeScript.
+   * @suppress {checkTypes}
    */
   onWindowKeydown_(e) {
     let ctrlKeyPressed = e.ctrlKey;
