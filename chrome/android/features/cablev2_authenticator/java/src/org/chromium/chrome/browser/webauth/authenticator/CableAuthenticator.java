11633e23acac0d87cc39367a0a37f75fcf206308
Adam Langley <agl@chromium.org>
2022-01-26 03:57:02 +0000

    webauthn: allow setting a screen-lock and rewrite caBLE UI.
    Once a phone can be used by scanning a QR code it becomes possible that
    someone could do so without a screen-lock set. Previously this was a
    corner-case error because the phone wouldn't advertise itself in Sync
    without a screen-lock.
    
    Therefore add a button on the error page to let users open Settings to
    set a screen-lock when one is missing. This complicates the UI state
    machine to the point where it's just getting silly. Thus this change
    also rewrites that state machine completely.
    
    BUG=1002262
    
    Change-Id: I4fa8a1e11ff02f17c492342349e90c2eb55fe9e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414854
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963376}

--- chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticator.java
+++ chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticator.java
@@ -70,6 +70,8 @@ class CableAuthenticator {
     private final String mQRURI;
     // mLinkQR stores whether a QR transaction should send linking information.
     private boolean mLinkQR;
+    // mAccessory contains the USB device, if operating in USB mode.
+    private UsbAccessory mAccessory;
 
     // mHandle is the opaque ID returned by the native code to ensure that
     // |stop| doesn't apply to a transaction that this instance didn't create.
@@ -96,6 +98,7 @@ class CableAuthenticator {
         mFCMEvent = fcmEvent;
         mServerLinkData = serverLink;
         mQRURI = qrURI;
+        mAccessory = accessory;
 
         // networkContext can only be used from the UI thread, therefore all
         // short-lived work is done on that thread.
@@ -104,13 +107,7 @@ class CableAuthenticator {
 
         CableAuthenticatorJni.get().setup(registration, networkContext, secret, metricsEnabled);
 
-        if (accessory != null) {
-            // USB mode can start immediately.
-            mHandle = CableAuthenticatorJni.get().startUSB(
-                    this, new USBHandler(context, mTaskRunner, accessory));
-        }
-
-        // Otherwise wait for |onBluetoothReady|.
+        // Wait for |onTransportReady|.
     }
 
     // Calls from native code.
@@ -325,16 +322,20 @@ class CableAuthenticator {
     }
 
     /**
-     * Called to indicate that Bluetooth is now enabled and a cloud message can be processed.
+     * Called to indicate that either USB or Bluetooth transports are ready for processing.
      */
-    void onBluetoothReady() {
+    void onTransportReady() {
         assert mTaskRunner.belongsToCurrentThread();
+
         if (mServerLinkData != null) {
             mHandle = CableAuthenticatorJni.get().startServerLink(this, mServerLinkData);
         } else if (mQRURI != null) {
             mHandle = CableAuthenticatorJni.get().startQR(this, getName(), mQRURI, mLinkQR);
-        } else {
+        } else if (mFCMEvent != null) {
             mHandle = CableAuthenticatorJni.get().startCloudMessage(this, mFCMEvent);
+        } else {
+            mHandle = CableAuthenticatorJni.get().startUSB(
+                    this, new USBHandler(mContext, mTaskRunner, mAccessory));
         }
     }
 
