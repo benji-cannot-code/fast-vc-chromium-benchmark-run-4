aa296e67eb52385dd6ddcfab0da2498c7662333b
Julie Jeongeun Kim <jkim@igalia.com>
2022-01-26 01:45:13 +0000

    [mparch] Ensure SafeBrowsingTriggeredPopupBlocker uses the right frame
    This CL uses NH::GetRenderFrameHost() instead of
    WebContents::GetMainFrame() on DidFinishNavigation() to use
    the correct frame for console messages.
    It also adds a browser test that ensures it has the correct source
    frame for a console message in prerendering.
    
    Bug: 1289212
    Change-Id: I938ce883ce92cd57887f734079e84af5d935df10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403513
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#963340}

--- components/blocked_content/safe_browsing_triggered_popup_blocker.cc
+++ components/blocked_content/safe_browsing_triggered_popup_blocker.cc
@@ -155,7 +155,7 @@ void SafeBrowsingTriggeredPopupBlocker::DidFinishNavigation(
             back_forward_cache::DisabledReasonId::
                 kSafeBrowsingTriggeredPopupBlocker));
   } else if (level == SubresourceFilterLevel::WARN) {
-    web_contents()->GetMainFrame()->AddMessageToConsole(
+    navigation_handle->GetRenderFrameHost()->AddMessageToConsole(
         blink::mojom::ConsoleMessageLevel::kWarning, kAbusiveWarnMessage);
     LogAction(Action::kWarningSite);
   }
