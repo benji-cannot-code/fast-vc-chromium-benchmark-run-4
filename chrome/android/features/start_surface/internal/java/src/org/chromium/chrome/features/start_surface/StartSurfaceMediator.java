55b0af98519999c001a62b01117d47a5b1a9b9b8
Xi Han <hanxi@google.com>
2022-01-26 15:49:22 +0000

    [Start] Polish Logo and toolbar.
    In this CL, the following UI is adjusted:
    1. Increase Logo size to 40dp, and move it down to leave extra top
       margin.
    2. Increase Identity disk's start margin.
    3. Increase the space between logo and fake search box to 24dp; while
       decrease the left and right margin of search box by 4dp.
    4. Decrease the space between MV tiles and search box.
    
    Before:
    https://drive.google.com/file/d/1L9ynmpxxvL7NilQV1wspcyhsDhagG38Q/view?usp=sharing
    After:
    https://drive.google.com/file/d/1VNXZUkFAkMyn9P-wVtzcJ9WWTicAgOIo/view?usp=sharing
    
    Bug: 1290360
    Change-Id: I4c0661c5c7ca24ec3514571458a5bde830fd2d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413012
    Reviewed-by: Hao Dong <spdonghao@google.com>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963518}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.features.start_surface;
 
+import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.FAKE_SEARCH_BOX_TOP_MARGIN;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_FAKE_SEARCH_BOX_VISIBLE;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_INCOGNITO;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_INCOGNITO_DESCRIPTION_INITIALIZED;
@@ -308,6 +309,8 @@ class StartSurfaceMediator implements StartSurface.Controller, TabSwitcher.Overv
                     resources.getDimensionPixelSize(R.dimen.mv_tiles_container_top_margin));
             mPropertyModel.set(TAB_SWITCHER_TITLE_TOP_MARGIN,
                     resources.getDimensionPixelSize(R.dimen.tab_switcher_title_top_margin));
+            mPropertyModel.set(FAKE_SEARCH_BOX_TOP_MARGIN,
+                    resources.getDimensionPixelSize(R.dimen.fake_search_box_top_margin));
         }
 
         mController.addOverviewModeObserver(this);
