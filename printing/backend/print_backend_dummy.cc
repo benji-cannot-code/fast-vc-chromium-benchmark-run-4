0bac43324b14897c2cb573980f3bf7b92e35029e
Lei Zhang <thestig@chromium.org>
2022-01-26 21:59:21 +0000

    Remove PrintBackend::CreateInstanceForCloudPrint().
    Its only caller is gone. Then remove the `for_cloud_print` boolean
    parameter to PrintBackend::CreateInstanceImpl(), as it is never set to
    true.
    
    Bug: 1162164
    Change-Id: If5f055d62f821b6cd34bb2bf102db0123a69cc1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416693
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963727}

--- printing/backend/print_backend_dummy.cc
+++ printing/backend/print_backend_dummy.cc
@@ -64,8 +64,7 @@ class DummyPrintBackend : public PrintBackend {
 // static
 scoped_refptr<PrintBackend> PrintBackend::CreateInstanceImpl(
     const base::DictionaryValue* print_backend_settings,
-    const std::string& locale,
-    bool /*for_cloud_print*/) {
+    const std::string& locale) {
   return base::MakeRefCounted<DummyPrintBackend>(locale);
 }
 
