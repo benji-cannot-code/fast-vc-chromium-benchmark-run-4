e3d5619854f724fa87319155db84bacdc444fae3
Mirko Bonadei <mbonadei@chromium.org>
2022-01-26 10:32:48 +0000

    Roll abseil_revision c59e7e59f5..e3fdd9b16a
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/c59e7e59f5..e3fdd9b16a
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/c59e7e59f5..e3fdd9b16a
    
    Bug: None
    Change-Id: I28beae45a02bedc62357f68c3c608a2806799bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416871
    Reviewed-by: Danil Chapovalov <danilchap@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963454}

--- third_party/abseil-cpp/absl/status/internal/status_internal.h
+++ third_party/abseil-cpp/absl/status/internal/status_internal.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "absl/base/attributes.h"
 #include "absl/container/inlined_vector.h"
 #include "absl/strings/cord.h"
 
@@ -26,7 +27,14 @@ namespace absl {
 ABSL_NAMESPACE_BEGIN
 // Returned Status objects may not be ignored. Codesearch doesn't handle ifdefs
 // as part of a class definitions (b/6995610), so we use a forward declaration.
+//
+// TODO(b/176172494): ABSL_MUST_USE_RESULT should expand to the more strict
+// [[nodiscard]]. For now, just use [[nodiscard]] directly when it is available.
+#if ABSL_HAVE_CPP_ATTRIBUTE(nodiscard)
+class [[nodiscard]] Status;
+#else
 class ABSL_MUST_USE_RESULT Status;
+#endif
 ABSL_NAMESPACE_END
 }  // namespace absl
 #endif  // !SWIG
