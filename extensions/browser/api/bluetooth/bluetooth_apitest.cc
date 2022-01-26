69dca74e94f82c3ae3c1f8f6962a5a605a42ffdf
Avi Drissman <avi@chromium.org>
2022-01-26 20:31:18 +0000

    Clean up uses of (void)
    This migrates uses of (void) to [[maybe_unused]] or std::ignore as
    appropriate.
    
    This CL was uploaded by git cl split.
    
    R=khorimoto@chromium.org
    
    Bug: 1289456
    Change-Id: I8dbeef10fd68726f9b82be78695a08fd5e87368e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3417236
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963678}

--- extensions/browser/api/bluetooth/bluetooth_apitest.cc
+++ extensions/browser/api/bluetooth/bluetooth_apitest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include <memory>
+#include <tuple>
 
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
@@ -214,8 +215,8 @@ IN_PROC_BROWSER_TEST_F(BluetoothApiTest, Discovery) {
   FailNextCall();
   scoped_refptr<api::BluetoothStopDiscoveryFunction> stop_function;
   stop_function = setupFunction(new api::BluetoothStopDiscoveryFunction);
-  (void)utils::RunFunctionAndReturnSingleResult(stop_function.get(), "[]",
-                                                browser());
+  std::ignore = utils::RunFunctionAndReturnSingleResult(stop_function.get(),
+                                                        "[]", browser());
   SetUpMockAdapter();
 }
 
