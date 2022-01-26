53f35033eef14ef95804e3d80cb15c270fe50bd2
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2022-01-26 19:18:18 +0000

    [CrOS Bluetooth] Update disconnection metric
    Adds new user initiated disconnect metrics to record all user initiated
    disconnect attempts, also adds new device disconnect metrics that
    records all device disconnection. This CL also makes obsolete
    Bluetooth.ChromeOS.Disconnect.Result metric as it is replaced by the new
    user initiated disconnect metrics.
    
    Fixed: b/215601529
    Change-Id: I5deef05091398353127670180554572cab3eef2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415664
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#963633}

--- device/bluetooth/bluez/bluetooth_adapter_bluez.cc
+++ device/bluetooth/bluez/bluetooth_adapter_bluez.cc
@@ -1399,6 +1399,8 @@ void BluetoothAdapterBlueZ::NotifyDeviceConnectedStateChanged(
   if (is_now_connected) {
     device::BluetoothConnectionLogger::RecordDeviceConnected(
         device->GetIdentifier(), device->GetDeviceType());
+  } else {
+    device::RecordDeviceDisconnect(device->GetDeviceType());
   }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
 
