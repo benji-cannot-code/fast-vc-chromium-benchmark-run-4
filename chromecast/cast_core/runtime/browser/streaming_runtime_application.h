f8fb0cd5f51ef92db3218f2c01da7149caffd99e
Ryan Keane <rwkeane@google.com>
2022-01-26 20:31:57 +0000

    [CWR] Refactor cast_streaming calls to new class
    This change refactors the existing streaming_receiver_session_client to
    move cast_streaming specific pieces to a new class. No functional
    changes have been made.
    
    This is required so that Cast Remoting may be updated to be included
    in the cast_streaming component, and in the CWR, without any risk of
    affecting the working mirroring implementation
    
    Bug: b/182429524
    Change-Id: I1690f285bb1720c05f4f7da133a2431e53f9b457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405453
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#963679}

--- chromecast/cast_core/runtime/browser/streaming_runtime_application.h
+++ chromecast/cast_core/runtime/browser/streaming_runtime_application.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/cast_core/runtime/browser/runtime_application_base.h"
 #include "chromecast/cast_core/runtime/browser/streaming_receiver_session_client.h"
 #include "components/cast_streaming/browser/public/network_context_getter.h"
-#include "components/cast_streaming/public/mojom/renderer_controller.mojom.h"
-#include "media/mojo/mojom/renderer.mojom.h"
-#include "mojo/public/cpp/bindings/associated_remote.h"
-#include "mojo/public/cpp/bindings/remote.h"
 
 namespace chromecast {
 
@@ -25,8 +21,7 @@ class MessagePortService;
 
 class StreamingRuntimeApplication final
     : public RuntimeApplicationBase,
-      public StreamingReceiverSessionClient::Handler,
-      public CastWebContents::Observer {
+      public StreamingReceiverSessionClient::Handler {
  public:
   // |web_service| is expected to exist for the lifetime of this instance.
   StreamingRuntimeApplication(
@@ -54,20 +49,6 @@ class StreamingRuntimeApplication final
   void OnResolutionChanged(
       const gfx::Rect& size,
       const ::media::VideoTransformation& transformation) override;
-
-  // CastWebContents::Observer overrides.
-  void MainFrameReadyToCommitNavigation(
-      content::NavigationHandle* navigation_handle) override;
-
-  // Helper method to start playback using |renderer_connection_| and
-  // |renderer_controls_|.
-  void StartRenderer();
-
-  bool has_started_streaming_ = false;
-
-  mojo::AssociatedRemote<cast_streaming::mojom::RendererController>
-      renderer_connection_;
-  mojo::Remote<::media::mojom::Renderer> renderer_controls_;
 
   media::VideoPlaneController* video_plane_controller_;
 
