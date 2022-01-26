7249dd3bf7e073690d38e75eb6d2c09c600971d9
David Maunder <davidjm@chromium.org>
2022-01-26 06:02:44 +0000

    Adapt Prefetch Active Tab for CriticalPersistedTabData
    If the CriticalPersistedTabData flag is on we should try and pre-fetch
    the serialized CriticalPersistedTabData and fallback to fetching
    TabState. Prior to this change, we just prefetch TabState which will
    lead to a startup regression because
    1) Once we've transitioned to CriticalPersistedTabData it is unnecessary
    to prefetch TabState.
    2) There is no pre-fetch of CriticalPersistedTabData - it is only
    acquired on the UI thread rather than UI thread acquisition being the
    fallback. This will slow app startup.
    
    Bug: 1289447
    Change-Id: I2ac186cc7bc905fbbab56eb55323ec0a3d35da9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405455
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963405}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabPersistentStoreTest.java
@@ -419,7 +419,7 @@ public class TabPersistentStoreTest {
             store.addObserver(mockObserver);
 
             // Should not prefetch with no prior active tab preference stored.
-            Assert.assertNull(store.mPrefetchActiveTabTask);
+            Assert.assertNull(store.getPrefetchTabStateActiveTabTaskForTesting());
 
             // Make sure the metadata file loads properly and in order.
             store.loadState(false /* ignoreIncognitoFiles */);
@@ -764,7 +764,12 @@ public class TabPersistentStoreTest {
         TestThreadUtils.runOnUiThreadBlocking(() -> store.addObserver(mockObserver));
         store.waitForMigrationToFinish();
 
-        Assert.assertNotNull(store.mPrefetchActiveTabTask);
+        if (isCriticalPersistedTabDataEnabled) {
+            Assert.assertNotNull(
+                    store.getPrefetchCriticalPersistedTabDataActiveTabTaskForTesting());
+        } else {
+            Assert.assertNotNull(store.getPrefetchTabStateActiveTabTaskForTesting());
+        }
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             store.loadState(false /* ignoreIncognitoFiles */);
@@ -774,8 +779,14 @@ public class TabPersistentStoreTest {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             // Confirm that the pre-fetched active tab state was used, must be done here on the
             // UI thread as the message to finish the task is posted here.
-            Assert.assertEquals(
-                    AsyncTask.Status.FINISHED, store.mPrefetchActiveTabTask.getStatus());
+            if (isCriticalPersistedTabDataEnabled) {
+                Assert.assertEquals(AsyncTask.Status.FINISHED,
+                        store.getPrefetchCriticalPersistedTabDataActiveTabTaskForTesting()
+                                .getStatus());
+            } else {
+                Assert.assertEquals(AsyncTask.Status.FINISHED,
+                        store.getPrefetchTabStateActiveTabTaskForTesting().getStatus());
+            }
 
             // Confirm that the correct active tab ID is updated when saving state.
             mPreferences.writeInt(ChromePreferenceKeys.TABMODEL_ACTIVE_TAB_ID, -1);
