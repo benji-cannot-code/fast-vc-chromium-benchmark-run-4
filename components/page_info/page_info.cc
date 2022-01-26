6888d660bc09fa513a36a0c74d80bfe62e018160
Olesia Marukhno <olesiamarukhno@google.com>
2022-01-26 17:27:55 +0000

    [privacy sandbox settings] Set up new data type for PageInfo
    Add methods and struct to represent new data type displayed in PageInfo.
    
    Bug: 1286276
    Change-Id: I3070a6b5aea856c2ed959df0e24484d01c1e5cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412076
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#963568}

--- components/page_info/page_info.cc
+++ components/page_info/page_info.cc
@@ -411,6 +411,7 @@ void PageInfo::InitializeUiState(PageInfoUI* ui, base::OnceClosure done) {
   PresentSiteIdentity();
   PresentPageFeatureInfo();
   PresentSiteData(std::move(done));
+  PresentAdPersonalizationData();
 }
 
 void PageInfo::UpdateSecurityState() {
@@ -1063,6 +1064,18 @@ void PageInfo::PresentPageFeatureInfo() {
       delegate_->IsContentDisplayedInVrHeadset();
 
   ui_->SetPageFeatureInfo(info);
+}
+
+void PageInfo::PresentAdPersonalizationData() {
+  PageInfoUI::AdPersonalizationInfo info;
+  auto* settings = GetPageSpecificContentSettings();
+  if (!settings)
+    return;
+
+  // TODO(olesiamarukhno): Use actual info.
+  info.has_joined_user_to_interest_group =
+      site_url_ == GURL("https://example.com");
+  ui_->SetAdPersonalizationInfo(info);
 }
 
 #if BUILDFLAG(FULL_SAFE_BROWSING)
