4305e00b3f9b8d298d1c9401b220f1559649eeec
dpapad <dpapad@chromium.org>
2022-01-26 09:38:54 +0000

    NewTabPage: Migrate iframe.js, logo.js to TypeScript.
    Bug: 1273590
    Change-Id: I08927984e5a8f4217b1037ad29cf0948f87e9b8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389063
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963445}

--- chrome/browser/resources/new_tab_page/iframe.ts
+++ chrome/browser/resources/new_tab_page/iframe.ts
@@ -14,24 +14,18 @@ import {WindowProxy} from './window_proxy.js';
  * and postMessaging in tests.
  */
 
-class IframeElement extends PolymerElement {
+export class IframeElement extends PolymerElement {
   static get is() {
     return 'ntp-iframe';
   }
 
-  static get template() {
-    return html`{__html_template__}`;
-  }
-
   static get properties() {
     return {
-      /** @type {string} */
       src: {
         reflectToAttribute: true,
         type: String,
       },
 
-      /** @private */
       src_: {
         type: String,
         computed: 'computeSrc_(src)',
@@ -39,23 +33,23 @@ class IframeElement extends PolymerElement {
     };
   }
 
-  /**
-   * Sends message to iframe.
-   * @param {*} message
-   */
-  postMessage(message) {
+  src: string;
+  private src_: string;
+
+  // Sends message to iframe.
+  postMessage(message: any) {
     assert(this.shadowRoot);
     WindowProxy.getInstance().postMessage(
-        strictQuery(this.shadowRoot, '#iframe', HTMLIFrameElement), message,
+        strictQuery(this.shadowRoot!, '#iframe', HTMLIFrameElement), message,
         new URL(this.src).origin);
   }
 
-  /**
-   * @return {string}
-   * @private
-   */
-  computeSrc_() {
+  private computeSrc_(): string {
     return WindowProxy.getInstance().createIframeSrc(this.src);
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
   }
 }
 
