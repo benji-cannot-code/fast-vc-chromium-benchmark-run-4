023e216a7d49e4074f070788a41c9d8a0056fab2
Ivan Murashov <ivan.murashov@lge.com>
2022-01-27 19:06:22 +0000

    Remove unused method from X509CertificatePrivate in PPAPI
    After CL https://crrev.com/c/2808690 the method
    X509CertificatePrivate::SendToBrowser() no more needed.
    Hence unused code is removed.
    
    Bug: 904556
    Change-Id: Ib8de30655a162fee6c9cafa1e959dc98d75b1edd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3356199
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#964175}

--- ppapi/proxy/ppb_x509_certificate_private_proxy.cc
+++ ppapi/proxy/ppb_x509_certificate_private_proxy.cc
@@ -27,9 +27,6 @@ class X509CertificatePrivate : public PPB_X509Certificate_Private_Shared {
 
   bool ParseDER(const std::vector<char>& der,
                 PPB_X509Certificate_Fields* result) override;
-
- private:
-  void SendToBrowser(IPC::Message* msg);
 };
 
 X509CertificatePrivate::X509CertificatePrivate(PP_Instance instance)
@@ -43,10 +40,6 @@ bool X509CertificatePrivate::ParseDER(const std::vector<char>& der,
                                       PPB_X509Certificate_Fields* result) {
   return PPB_X509Util_Shared::GetCertificateFields(der.data(), der.size(),
                                                    result);
-}
-
-void X509CertificatePrivate::SendToBrowser(IPC::Message* msg) {
-  PluginGlobals::Get()->GetBrowserSender()->Send(msg);
 }
 
 }  // namespace
