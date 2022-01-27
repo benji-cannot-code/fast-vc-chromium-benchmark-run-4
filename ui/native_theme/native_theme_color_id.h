fc4a0581f5e1252afe8a4707331bdc6cdd35ff34
Peter Kasting <pkasting@chromium.org>
2022-01-27 18:54:53 +0000

    Remove more now-unused native theme colors.
    Bug: 1249558
    Change-Id: I35b9b4275715c719cefbb7df41fa79eb50f5fc83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414972
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964163}

--- ui/native_theme/native_theme_color_id.h
+++ ui/native_theme/native_theme_color_id.h
@@ -9,14 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Clang format mangles lists like the below badly.
 // clang-format off
 #define NATIVE_THEME_CROSS_PLATFORM_COLOR_IDS                                  \
-  OP(kColorId_DefaultIconColor),                                               \
   OP(kColorId_FocusedBorderColor),                                             \
-  OP(kColorId_FocusedMenuItemBackgroundColor),                                 \
-  OP(kColorId_MenuBackgroundColor),                                            \
-  OP(kColorId_MenuIconColor),                                                  \
-  OP(kColorId_MenuSeparatorColor),                                             \
   OP(kColorId_ProminentButtonColor),                                           \
-  OP(kColorId_TextOnProminentButtonColor),                                     \
   OP(kColorId_ThrobberSpinningColor),                                          \
   OP(kColorId_ThrobberWaitingColor),                                           \
   OP(kColorId_WindowBackground)
