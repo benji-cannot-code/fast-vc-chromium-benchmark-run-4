96ed4a04701c7da2fbd7d7115f12cbeeefe080ca
Ender <ender@google.com>
2022-01-27 17:20:07 +0000

    Deflake remaining VoiceRecognitionHandlerTests.
    This change ensures that all tests start with clean policy setting.
    
    Change-Id: Ic619a2a39e0f54c6d5eeb378eb9adceec1db1fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419517
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Josh Simmons <jds@google.com>
    Commit-Queue: Josh Simmons <jds@google.com>
    Cr-Commit-Position: refs/heads/main@{#964103}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/voice/VoiceRecognitionHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/voice/VoiceRecognitionHandlerTest.java
@@ -669,6 +669,7 @@ public class VoiceRecognitionHandlerTest {
     @After
     public void tearDown() {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
+            setAudioCapturePref(true);
             mHandler.removeObserver(mObserver);
             VoiceRecognitionHandler.setIsRecognitionIntentPresentForTesting(null);
             mWindowAndroid.destroy();
