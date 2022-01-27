f00251f0353681ce370438fdc39cd4860ba82c63
Alexander Tekle <alexandertekle@google.com>
2022-01-27 22:13:57 +0000

    [M2] Learn more link for Virtual Card Manual Fallback Bubble
    This CL implements the learn more link for the Virtual Card Manual Fallback Bubble. The link redirects users to a GPay help page to learn more about virtual cards.
    
    Screenshot: https://screenshot.googleplex.com/AMbxcMvgTQSF2Kd.png
    
    Change-Id: I2946b7a46365f612fca48cb0dd8433213b4f0ede
    Bug: 1281695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3417577
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#964288}

--- chrome/browser/ui/autofill/payments/virtual_card_manual_fallback_bubble_controller_impl.h
+++ chrome/browser/ui/autofill/payments/virtual_card_manual_fallback_bubble_controller_impl.h
@@ -47,6 +47,7 @@ class VirtualCardManualFallbackBubbleControllerImpl
   AutofillBubbleBase* GetBubble() const override;
   const gfx::Image& GetBubbleTitleIcon() const override;
   std::u16string GetBubbleTitleText() const override;
+  std::u16string GetLearnMoreLinkText() const override;
   std::u16string GetEducationalBodyLabel() const override;
   std::u16string GetVirtualCardNumberFieldLabel() const override;
   std::u16string GetExpirationDateFieldLabel() const override;
@@ -58,6 +59,7 @@ class VirtualCardManualFallbackBubbleControllerImpl
       VirtualCardManualFallbackBubbleField field) const override;
   const CreditCard* GetVirtualCard() const override;
   bool ShouldIconBeVisible() const override;
+  void OnLinkClicked(const GURL& url) override;
   void OnBubbleClosed(PaymentsBubbleClosedReason closed_reason) override;
   void OnFieldClicked(VirtualCardManualFallbackBubbleField field) override;
 
