6d3c2580181a2ac151ad79bbfd60f4d27958f1a6
Ionel Popescu <iopopesc@microsoft.com>
2022-01-27 01:39:25 +0000

    [Popup] Handle shadow DOM event retargeting for light dismiss.
    This CL updates the <popup> light dismiss behavior so that
    shadow DOM event retargeting is considered when computing the
    event target node.
    
    Bug: 1250447
    Change-Id: Ic25ef33610b23bc2d1e75dbda8bd0350ae7f34c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414444
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#963832}

--- third_party/blink/renderer/core/html/html_popup_element.cc
+++ third_party/blink/renderer/core/html/html_popup_element.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/dom/events/event_path.h"
 #include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
 #include "third_party/blink/renderer/core/events/keyboard_event.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
@@ -285,7 +286,11 @@ const HTMLPopupElement* HTMLPopupElement::NearestOpenAncestralPopup(
 }
 
 void HTMLPopupElement::HandleLightDismiss(const Event& event) {
-  auto* target_node = event.target()->ToNode();
+  if (event.GetEventPath().IsEmpty())
+    return;
+  // Ensure that shadow DOM event retargeting is considered when computing
+  // the event target node.
+  auto* target_node = event.GetEventPath()[0].Target()->ToNode();
   if (!target_node)
     return;
   auto& document = target_node->GetDocument();
