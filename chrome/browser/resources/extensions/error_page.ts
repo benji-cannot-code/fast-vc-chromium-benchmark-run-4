155f0143b662973febf2a4d4481977ed563400e9
rbpotter <rbpotter@chromium.org>
2022-01-27 00:11:14 +0000

    Extensions: Migrate error console and error page tests to TS
    Bug: 1266535
    Change-Id: I668f2bb4f9ca1cd844e27c2a68dd53948b5c635b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416692
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963797}

--- chrome/browser/resources/extensions/error_page.ts
+++ chrome/browser/resources/extensions/error_page.ts
@@ -76,7 +76,7 @@ function getErrorSeverityText_(
   return warn;
 }
 
-interface ExtensionsErrorPageElement {
+export interface ExtensionsErrorPageElement {
   $: {
     closeButton: HTMLElement,
   };
@@ -84,7 +84,7 @@ interface ExtensionsErrorPageElement {
 
 const ExtensionsErrorPageElementBase = CrContainerShadowMixin(PolymerElement);
 
-class ExtensionsErrorPageElement extends ExtensionsErrorPageElementBase {
+export class ExtensionsErrorPageElement extends ExtensionsErrorPageElementBase {
   static get is() {
     return 'extensions-error-page';
   }
@@ -383,6 +383,12 @@ class ExtensionsErrorPageElement extends ExtensionsErrorPageElementBase {
 
   static get template() {
     return html`{__html_template__}`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'extensions-error-page': ExtensionsErrorPageElement;
   }
 }
 
