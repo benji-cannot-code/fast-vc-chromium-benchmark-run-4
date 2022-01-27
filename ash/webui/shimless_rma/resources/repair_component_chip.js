8e97a264056405b883fe197bc573e471a6209131
Anton Swifton <swifton@google.com>
2022-01-27 21:03:34 +0000

    Shimless: turn on Chrome OS text colors and fonts
    onboarding_landing_page:
    https://screenshot.googleplex.com/7ez8yjbiF8M5giV
    onboarding_network_page:
    https://screenshot.googleplex.com/ZpRy6MZFvxxQUig
    onboarding_update_page:
    https://screenshot.googleplex.com/5tXT4WaRRxorfyV
    onboarding_select_components_page:
    https://screenshot.googleplex.com/3w6wvc2wFdyuqZr
    onboarding_choose_destination_page:
    https://screenshot.googleplex.com/rJL8AsxTN3pjm54
    onboarding_choose_wp_disable_method_page:
    https://screenshot.googleplex.com/Aya4V2EXwnTjFqt
    onboarding_enter_rsu_wp_disable_code_page:
    https://screenshot.googleplex.com/7MxtgNBFhdRgYMT
    onboarding_wait_for_manual_wp_disable_page:
    https://screenshot.googleplex.com/4yJbtYVYctiUoKf
    onboarding_wp_disable_complete_page:
    https://screenshot.googleplex.com/52zPzQJjFR9unTD
    reimaging_firmware_update_page:
    https://screenshot.googleplex.com/5Ttqe2GYztR5H67
    reimaging_device_information_page:
    https://screenshot.googleplex.com/4rCEHfJRBkmbMy5
    wrapup_restock_page:
    https://screenshot.googleplex.com/9L8rrU3xdMmdrqD
    reimaging_calibration_failed_page:
    https://screenshot.googleplex.com/56CEFJfgsWKxDdU
    reimaging_calibration_setup_page:
    https://screenshot.googleplex.com/VeCPFvti3mvrAky
    reimaging_calibration_run_page:
    https://screenshot.googleplex.com/8m8DL5aSdYQvNgr
    reimaging_provisioning_page:
    https://screenshot.googleplex.com/jG9p9H9RFux2iYe
    wrapup_wait_for_manual_wp_enable_page:
    https://screenshot.googleplex.com/9ck27pWCbrUrEuU
    wrapup_finalize_page:
    https://screenshot.googleplex.com/69TyqxjuB8CDDTh
    wrapup_repair_complete_page:
    https://screenshot.googleplex.com/AwSioLpj3xjuxTJ
    critical_error_page:
    https://screenshot.googleplex.com/Mgy2XzikiyVxPRH
    
    Bug: 1198187
    Test: local
    Change-Id: I9db3285268274ddf490bea4a092f8f25f2a091e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415643
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Anton Swifton <swifton@google.com>
    Cr-Commit-Position: refs/heads/main@{#964250}

--- ash/webui/shimless_rma/resources/repair_component_chip.js
+++ ash/webui/shimless_rma/resources/repair_component_chip.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './shimless_rma_fonts_css.js';
 import './shimless_rma_shared_css.js';
 import './icons.js';
 
