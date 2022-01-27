02711a986ef9e98054467d476719dc988252cbc5
Michael Bai <michaelbai@chromium.org>
2022-01-27 19:38:55 +0000

    Aw: Rename to set/is AlgorithmicDarkeningAllowed
    Rename set/getAllowAlgorithmicDarkening to
    set/isAlgorithmicDarkeningAllowed according to API council's
    feedback in http://b/216369031
    
    Bug: 1287883
    Change-Id: Ic3c7893448539c9b6f62b9ab5a8e2cccd1876a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419520
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964196}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwDarkModeTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwDarkModeTest.java
@@ -144,7 +144,7 @@ public class AwDarkModeTest {
 
         // Check setForceDarkMode has noops, otherwise ForceDarkening will be turned off.
         mAwContents.getSettings().setForceDarkMode(AwSettings.FORCE_DARK_OFF);
-        mAwContents.getSettings().setAllowAlgorithmicDarkening(true);
+        mAwContents.getSettings().setAlgorithmicDarkeningAllowed(true);
         // Set force dark mode again to check no ordering issue.
         mAwContents.getSettings().setForceDarkMode(AwSettings.FORCE_DARK_OFF);
 
@@ -163,7 +163,7 @@ public class AwDarkModeTest {
 
         // Check setForceDarkMode has noops, otherwise ForceDarkening will be turned off.
         mAwContents.getSettings().setForceDarkMode(AwSettings.FORCE_DARK_OFF);
-        mAwContents.getSettings().setAllowAlgorithmicDarkening(true);
+        mAwContents.getSettings().setAlgorithmicDarkeningAllowed(true);
         // Set force dark mode again to check no ordering issue.
         mAwContents.getSettings().setForceDarkMode(AwSettings.FORCE_DARK_OFF);
 
