6eea0868d366e9df55383ab9ac5d345cd108a9ab
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-01-27 04:03:34 +0000

    [Extensions] Support i18n.getMessage() in service workers
    Add support for i18n.getMessage() in extension service workers. This
    changes the implementation of the GetMessage() implementation in
    I18nHooksDelegate to conditionally use either the render frame or
    the WorkerThreadDispatcher (depending on the context type) for the
    IPC sender to retrieve the message map, which fixes support for
    service worker contexts.
    
    Also remove the restriction on getMessage() from the _api_features.json
    file, exposing i18n.getMessage() to service worker contexts, and enable
    service worker-based tests for i18n.getMessage() by reverting the
    changes made to the i18n tests in revision a35eaacc117.
    
    After this CL, i18n.getMessage() should be fully supported for extension
    service worker contexts.
    
    Bug: 1268098
    Change-Id: I29eced9b7b3588fd7940071901178803738921b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3413070
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963881}

--- extensions/renderer/i18n_hooks_delegate.cc
+++ extensions/renderer/i18n_hooks_delegate.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/get_script_context.h"
 #include "extensions/renderer/script_context.h"
 #include "extensions/renderer/shared_l10n_map.h"
+#include "extensions/renderer/worker_thread_dispatcher.h"
 #include "gin/converter.h"
 #include "gin/data_object_builder.h"
 #include "third_party/cld_3/src/src/nnet_language_identifier.h"
@@ -150,12 +151,12 @@ v8::Local<v8::Value> GetI18nMessage(const std::string& message_name,
                                     const std::string& extension_id,
                                     v8::Local<v8::Value> v8_substitutions,
                                     v8::Local<v8::Value> v8_options,
-                                    content::RenderFrame* render_frame,
+                                    IPC::Sender* message_sender,
                                     v8::Local<v8::Context> context) {
   v8::Isolate* isolate = context->GetIsolate();
 
   std::string message = SharedL10nMap::GetInstance().GetMessage(
-      extension_id, message_name, render_frame);
+      extension_id, message_name, message_sender);
 
   std::vector<std::string> substitutions;
   // For now, we just suppress all errors, but that's really not the best.
@@ -285,10 +286,18 @@ RequestResult I18nHooksDelegate::HandleGetMessage(
   DCHECK_EQ(binding::AsyncResponseType::kNone, parse_result.async_type);
   DCHECK(script_context->extension());
   DCHECK(arguments[0]->IsString());
+
+  IPC::Sender* message_sender = nullptr;
+  if (script_context->IsForServiceWorker()) {
+    message_sender = WorkerThreadDispatcher::Get();
+  } else {
+    message_sender = script_context->GetRenderFrame();
+  }
+
   v8::Local<v8::Value> message = GetI18nMessage(
       gin::V8ToString(script_context->isolate(), arguments[0]),
       script_context->extension()->id(), arguments[1], arguments[2],
-      script_context->GetRenderFrame(), script_context->v8_context());
+      message_sender, script_context->v8_context());
 
   RequestResult result(RequestResult::HANDLED);
   result.return_value = message;
