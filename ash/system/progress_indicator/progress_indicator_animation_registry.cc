08c3b743cb4b52791939213e7c195f3930aa2f1b
David Black <dmblack@google.com>
2022-01-27 00:50:05 +0000

    Refactor `ProgressIndicatorAnimationRegistry` (Part 3 of N).
    Moves the following APIs to the base class:
    
    * EraseAllAnimations()
    * EraseAllAnimationsForKey()
    * EraseAllAnimationsForKeyIf()
    
    Bug: 1289842
    Change-Id: Ibd06ebdc43f2234a314d6a9edde428bb3c7b85d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415577
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#963819}

--- ash/system/progress_indicator/progress_indicator_animation_registry.cc
+++ ash/system/progress_indicator/progress_indicator_animation_registry.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/progress_indicator/progress_indicator_animation_registry.h"
 
+#include <set>
+
 namespace ash {
 namespace {
 
@@ -169,6 +171,34 @@ ProgressIndicatorAnimationRegistry::SetProgressRingAnimationForKey(
   return SetAnimationForKey(&ring_animations_by_key_,
                             &ring_animation_changed_callback_lists_by_key_, key,
                             std::move(animation));
+}
+
+void ProgressIndicatorAnimationRegistry::EraseAllAnimations() {
+  EraseAllAnimationsForKeyIf(
+      base::BindRepeating([](const void* key) { return true; }));
+}
+
+void ProgressIndicatorAnimationRegistry::EraseAllAnimationsForKey(
+    const void* key) {
+  SetProgressIconAnimationForKey(key, nullptr);
+  SetProgressRingAnimationForKey(key, nullptr);
+}
+
+void ProgressIndicatorAnimationRegistry::EraseAllAnimationsForKeyIf(
+    base::RepeatingCallback<bool(const void* key)> predicate) {
+  std::set<const void*> keys_to_erase;
+  for (const auto& icon_animation_by_key : icon_animations_by_key_) {
+    const void* key = icon_animation_by_key.first;
+    if (predicate.Run(key))
+      keys_to_erase.insert(key);
+  }
+  for (const auto& ring_animation_by_key : ring_animations_by_key_) {
+    const void* key = ring_animation_by_key.first;
+    if (predicate.Run(key))
+      keys_to_erase.insert(key);
+  }
+  for (const void* key : keys_to_erase)
+    EraseAllAnimationsForKey(key);
 }
 
 }  // namespace ash
