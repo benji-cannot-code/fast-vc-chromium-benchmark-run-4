cd723547083c1fc7680c6c8c393f460761c83ba4
Ehimare Okoyomon <eokoyomon@chromium.org>
2022-01-27 14:12:13 +0000

    Privacy Review: Hide promo if more than x impressions
    Use local storage to store the number of impressions and hide the
    promo if more than the max.
    
    Bug: 1215630, 1287253
    Change-Id: I50845bee1e0b3bd4d9dc0e66c4e927d03f4169dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379161
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964016}

--- chrome/browser/resources/settings/basic_page/basic_page.ts
+++ chrome/browser/resources/settings/basic_page/basic_page.ts
@@ -41,6 +41,7 @@ import {loadTimeData} from '../i18n_setup.js';
 import {PageVisibility} from '../page_visibility.js';
 import {SyncStatus} from '../people_page/sync_browser_proxy.js';
 import {PrefsMixin, PrefsMixinInterface} from '../prefs/prefs_mixin.js';
+import {MAX_PRIVACY_REVIEW_PROMO_IMPRESSION, PrivacyReviewBrowserProxy, PrivacyReviewBrowserProxyImpl} from '../privacy_page/privacy_review/privacy_review_browser_proxy.js';
 import {routes} from '../route.js';
 import {Route, RouteObserverMixin, RouteObserverMixinInterface, Router} from '../router.js';
 import {getSearchManager, SearchResult} from '../search_settings.js';
@@ -145,8 +146,7 @@ export class SettingsBasicPageElement extends SettingsBasicPageElementBase {
        */
       showPrivacyReviewPromo_: {
         type: Boolean,
-        computed:
-            'computeShowPrivacyReviewPromo_(isManaged_, isChildUser_, prefs.privacy_guide.viewed.value)',
+        value: false,
       },
 
       isManaged_: {
@@ -180,6 +180,12 @@ export class SettingsBasicPageElement extends SettingsBasicPageElementBase {
     };
   }
 
+  static get observers() {
+    return [
+      'updatePrivacyReviewPromoVisibility_(isManaged_, isChildUser_, prefs.privacy_guide.viewed.value)',
+    ];
+  }
+
   pageVisibility: PageVisibility;
   inSearchMode: boolean;
   advancedToggleExpanded: boolean;
@@ -194,8 +200,12 @@ export class SettingsBasicPageElement extends SettingsBasicPageElementBase {
   private currentRoute_: Route;
   private advancedTogglingInProgress_: boolean;
 
+  private showPrivacyReviewPromo_: boolean;
+  private privacyReviewPromoWasShown_: boolean;
   private isManaged_: boolean;
   private isChildUser_: boolean;
+  private privacyReviewBrowserProxy_: PrivacyReviewBrowserProxy =
+      PrivacyReviewBrowserProxyImpl.getInstance();
 
   ready() {
     super.ready();
@@ -233,6 +243,9 @@ export class SettingsBasicPageElement extends SettingsBasicPageElementBase {
     }
 
     super.currentRouteChanged(newRoute, oldRoute);
+    if (newRoute === routes.PRIVACY) {
+      this.updatePrivacyReviewPromoVisibility_();
+    }
   }
 
   /**
@@ -253,11 +266,22 @@ export class SettingsBasicPageElement extends SettingsBasicPageElementBase {
         .get();
   }
 
-  private computeShowPrivacyReviewPromo_(): boolean {
-    return this.pageVisibility.privacy !== false &&
-        loadTimeData.getBoolean('privacyReviewEnabled') && !this.isManaged_ &&
-        !this.isChildUser_ && this.prefs !== undefined &&
-        !this.getPref('privacy_guide.viewed').value;
+  private updatePrivacyReviewPromoVisibility_() {
+    if (this.pageVisibility.privacy === false ||
+        !loadTimeData.getBoolean('privacyReviewEnabled') || this.isManaged_ ||
+        this.isChildUser_ || this.prefs === undefined ||
+        this.getPref('privacy_guide.viewed').value ||
+        this.privacyReviewBrowserProxy_.getPromoImpressionCount() >=
+            MAX_PRIVACY_REVIEW_PROMO_IMPRESSION ||
+        this.currentRoute_ !== routes.PRIVACY) {
+      this.showPrivacyReviewPromo_ = false;
+      return;
+    }
+    this.showPrivacyReviewPromo_ = true;
+    if (!this.privacyReviewPromoWasShown_) {
+      this.privacyReviewBrowserProxy_.incrementPromoImpressionCount();
+      this.privacyReviewPromoWasShown_ = true;
+    }
   }
 
   private onIsManagedChanged_(isManaged: boolean) {
