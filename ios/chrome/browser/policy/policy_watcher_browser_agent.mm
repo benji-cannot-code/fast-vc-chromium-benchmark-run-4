16dff31b828a0437376ab82cb0a696f03052d707
Ewann <ewannpv@google.com>
2022-01-27 12:35:42 +0000

    [iOS][Enterprise][7/N] Unify prompts
    This CL waits for the app to correctly start before trying to display
    the sync disabled prompt.
    
    Bug: 1261423
    Change-Id: Id19729ba3c166491846a678c487fcf0cd11d8e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402128
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963996}

--- ios/chrome/browser/policy/policy_watcher_browser_agent.mm
+++ ios/chrome/browser/policy/policy_watcher_browser_agent.mm
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #include "base/metrics/histogram_functions.h"
+#include "base/run_loop.h"
+#include "base/task/post_task.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "components/sync/base/pref_names.h"
@@ -21,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/policy_change_commands.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
 #import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
+#include "ios/web/public/thread/web_task_traits.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -77,7 +80,10 @@ void PolicyWatcherBrowserAgent::Initialize(id<PolicyChangeCommands> handler) {
           base::Unretained(this)));
 
   // Try to show the alert in case the policy changed since last time.
-  ShowSyncDisabledPromptIfNeeded();
+  base::SequencedTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&PolicyWatcherBrowserAgent::ShowSyncDisabledPromptIfNeeded,
+                     weak_factory_.GetWeakPtr()));
 }
 
 void PolicyWatcherBrowserAgent::ForceSignOutIfSigninDisabled() {
@@ -115,9 +121,15 @@ void PolicyWatcherBrowserAgent::ShowSyncDisabledPromptIfNeeded() {
           syncer::prefs::kSyncManaged);
 
   if (!syncDisabledAlertShown && isSyncDisabledByAdministrator) {
-    [handler_ showSyncDisabledPrompt];
-    // Will never trigger again unless policy changes.
-    [standard_defaults setBool:YES forKey:kSyncDisabledAlertShownKey];
+    SceneState* scene_state =
+        SceneStateBrowserAgent::FromBrowser(browser_)->GetSceneState();
+    BOOL scene_is_active =
+        scene_state.activationLevel >= SceneActivationLevelForegroundActive;
+    if (scene_is_active) {
+      [handler_ showSyncDisabledPrompt];
+      // Will never trigger again unless policy changes.
+      [standard_defaults setBool:YES forKey:kSyncDisabledAlertShownKey];
+    }
   } else if (syncDisabledAlertShown && !isSyncDisabledByAdministrator) {
     // Will trigger again, if policy is turned back on.
     [standard_defaults setBool:NO forKey:kSyncDisabledAlertShownKey];
