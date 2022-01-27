e3137e3f8a83544d3b51e8a721071cf3a526c33b
Bartek Nowierski <bartekn@chromium.org>
2022-01-27 23:31:29 +0000

    [PA] More cast removal + defining the line for MTE (un)tagging
    This CL also takes a step towards using more descriptive names like
    |object| and |slot_start|, instead of generic |ptr| or |address| that
    don't carry any information about where ptr/address exactly point to.
    
    Bug: 1280844
    Change-Id: I85f935cb701cff30dd5d7a8993945c514cbe553a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412967
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964324}

--- base/allocator/partition_allocator/starscan/pcscan.h
+++ base/allocator/partition_allocator/starscan/pcscan.h
@@ -98,7 +98,7 @@ class BASE_EXPORT PCScan final {
   // Registers a newly allocated super page for |root|.
   static void RegisterNewSuperPage(Root* root, uintptr_t super_page_base);
 
-  ALWAYS_INLINE static void MoveToQuarantine(void* ptr,
+  ALWAYS_INLINE static void MoveToQuarantine(void* object,
                                              size_t usable_size,
                                              uintptr_t slot_start,
                                              size_t slot_size);
@@ -231,7 +231,7 @@ ALWAYS_INLINE void PCScan::JoinScanIfNeeded() {
     instance.JoinScan();
 }
 
-ALWAYS_INLINE void PCScan::MoveToQuarantine(void* ptr,
+ALWAYS_INLINE void PCScan::MoveToQuarantine(void* object,
                                             size_t usable_size,
                                             uintptr_t slot_start,
                                             size_t slot_size) {
@@ -243,9 +243,10 @@ ALWAYS_INLINE void PCScan::MoveToQuarantine(void* ptr,
     // TODO(bikineev): If we start protecting quarantine memory, we can lose
     // double-free coverage (the check below). Consider performing the
     // double-free check before protecting if eager clearing becomes default.
-    SecureMemset(ptr, 0, usable_size);
+    SecureMemset(object, 0, usable_size);
   }
 
+  // TODO(bartekn): Remove MTE untagging, once its done in the caller.
   uintptr_t unmasked_slot_start = memory::UnmaskPtr(slot_start);
   auto* state_bitmap = StateBitmapFromAddr(unmasked_slot_start);
 
