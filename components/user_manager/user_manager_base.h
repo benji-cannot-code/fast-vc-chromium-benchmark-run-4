5ccc7f2e57d3832c4cb982494211634e5b4408d4
Roman Sorokin <rsorokin@chromium.org>
2022-01-27 16:52:32 +0000

    UserManagerBase: Nuke use of base::ListValue
    Bug: 1187062, 1288884
    Change-Id: I0d7d93cf8bdb9abbf52cf4950ae684eb7d1b44a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420332
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#964090}

--- components/user_manager/user_manager_base.h
+++ components/user_manager/user_manager_base.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/synchronization/lock.h"
 #include "base/time/time.h"
+#include "base/values.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/remove_user_delegate.h"
 #include "components/user_manager/user.h"
@@ -29,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class PrefRegistrySimple;
 
 namespace base {
-class ListValue;
 class SingleThreadTaskRunner;
 }
 
@@ -156,11 +156,10 @@ class USER_MANAGER_EXPORT UserManagerBase : public UserManager {
   // not full initialized yet" flag.
   virtual void SetIsCurrentUserNew(bool is_new);
 
-  // TODO(crbug.com/1187062): Refactor this to remove use of ListValue.
   // Helper function that converts users from |users_list| to |users_vector| and
   // |users_set|. Duplicates and users already present in |existing_users| are
   // skipped.
-  void ParseUserList(const base::ListValue& users_list,
+  void ParseUserList(const base::Value::ConstListView& users_list,
                      const std::set<AccountId>& existing_users,
                      std::vector<AccountId>* users_vector,
                      std::set<AccountId>* users_set);
