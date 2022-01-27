cc5507974ae2344eda75af5ffdb24038a567048c
Toby Huang <tobyhuang@chromium.org>
2022-01-27 22:13:41 +0000

    Projector: Add feature flag for app debug mode
    This CL adds a ProjectorAppDebug feature flag to launch the Projector
    app in debug mode on ChromeOS. The error messages would be more
    detailed and show a stack trace, making the errors easier to debug.
    
    Bug: b/200293105
    Change-Id: I28d22f0fea23f02997eb2992c848a92641938dfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3418265
    Auto-Submit: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964287}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -396,6 +396,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kProjector;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kProjectorManagedUser;
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kProjectorAnnotator;
+COMPONENT_EXPORT(ASH_CONSTANTS) extern const base::Feature kProjectorAppDebug;
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const base::Feature kQuickDim;
 COMPONENT_EXPORT(ASH_CONSTANTS)
@@ -630,6 +631,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsProjectorAllUserEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsProjectorManagedUserEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsProjectorFeaturePodEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsProjectorAnnotatorEnabled();
+COMPONENT_EXPORT(ASH_CONSTANTS) bool IsProjectorAppDebugMode();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsQuickDimEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsQuickSettingsNetworkRevampEnabled();
--- chrome/browser/ash/web_applications/projector_app/untrusted_projector_ui_config.cc
+++ chrome/browser/ash/web_applications/projector_app/untrusted_projector_ui_config.cc
@@ -21,6 +21,7 @@ void ChromeUntrustedProjectorUIDelegate::PopulateLoadTimeData(
     content::WebUIDataSource* source) {
   version_info::Channel channel = chrome::GetChannel();
   source->AddBoolean("isDevChannel", channel == version_info::Channel::DEV);
+  source->AddBoolean("isDebugMode", ash::features::IsProjectorAppDebugMode());
 }
 
 UntrustedProjectorUIConfig::UntrustedProjectorUIConfig()
