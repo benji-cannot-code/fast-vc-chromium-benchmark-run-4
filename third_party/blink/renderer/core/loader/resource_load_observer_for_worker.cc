daa25c15691dc9acd652b1a6a2d5c54debf372ec
Yifan Luo <lyf@google.com>
2022-01-27 10:27:59 +0000

    [Private Network Access] Record UseCounter for subresource fetch in shared worker
    Bug: 1282639
    Change-Id: I2fda3290d4498b9899f7453a1b503a899f075814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412256
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Auto-Submit: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#963961}

--- third_party/blink/renderer/core/loader/resource_load_observer_for_worker.cc
+++ third_party/blink/renderer/core/loader/resource_load_observer_for_worker.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/loader/resource_load_observer_for_worker.h"
 
+#include "services/network/public/cpp/ip_address_space_util.h"
 #include "third_party/blink/renderer/core/core_probes_inl.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/loader/mixed_content_checker.h"
 #include "third_party/blink/renderer/core/loader/worker_fetch_context.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_client_settings_object.h"
@@ -48,12 +50,31 @@ void ResourceLoadObserverForWorker::DidChangePriority(
     ResourceLoadPriority priority,
     int intra_priority_value) {}
 
+// Record use counter for private network access.
+void RecordPrivateNetworkAccessFeature(ExecutionContext* execution_context,
+                                       const ResourceResponse& response) {
+  DCHECK(execution_context);
+  if (!network::IsLessPublicAddressSpace(response.AddressSpace(),
+                                         execution_context->AddressSpace()))
+    return;
+  // Only record the feature for worker contexts, not worklets. The address
+  // space of worklets is not yet specified.
+  // TODO(https://crbug.com/1291176): Revisit this if worklets should be subject
+  // to PNA checks.
+  if (!execution_context->IsWorkerGlobalScope())
+    return;
+  execution_context->CountUse(WebFeature::kPrivateNetworkAccessWithinWorker);
+}
+
 void ResourceLoadObserverForWorker::DidReceiveResponse(
     uint64_t identifier,
     const ResourceRequest& request,
     const ResourceResponse& response,
     const Resource* resource,
     ResponseSource) {
+  RecordPrivateNetworkAccessFeature(
+      worker_fetch_context_->GetExecutionContext(), response);
+
   if (response.HasMajorCertificateErrors()) {
     MixedContentChecker::HandleCertificateError(
         response, request.GetRequestContext(),
