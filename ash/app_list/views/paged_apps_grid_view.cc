c45242feb9377b18cbb7975e96fbb16dfadbc023
Toni Barzic <tbarzic@chromium.org>
2022-01-28 01:26:12 +0000

    Set ClipsChildren ax attribute on paged apps grid view
    Paged apps grid view bounds clip the children that are not on the
    current page, so mark it as such for accessibility so automation stack
    correctly marks child items not on the current app list page as
    offscreen.
    
    BUG=None
    
    Change-Id: I3e76aae973f6846a4f380ae94f30b184c8cd8d72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3421151
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964368}

--- ash/app_list/views/paged_apps_grid_view.cc
+++ ash/app_list/views/paged_apps_grid_view.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "cc/paint/paint_flags.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/compositor/animation_throughput_reporter.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/paint_recorder.h"
@@ -486,6 +487,11 @@ void PagedAppsGridView::Layout() {
     MaskContainerToBackgroundBounds();
   }
   views::ViewModelUtils::SetViewBoundsToIdealBounds(pulsing_blocks_model());
+}
+
+void PagedAppsGridView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  node_data->AddBoolAttribute(ax::mojom::BoolAttribute::kClipsChildren, true);
+  AppsGridView::GetAccessibleNodeData(node_data);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
