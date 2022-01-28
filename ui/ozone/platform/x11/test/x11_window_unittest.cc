500e857d2abcaf0819dedb6e094fd14e556e3f9d
Lingqi Chi <lingqi@chromium.org>
2022-01-28 06:53:23 +0000

    Sheriff: disable X11WindowTest.WindowManagerTogglesFullscreen on Linux ASAN and ChromeOS
    Bug: 1291868
    Change-Id: Ib5fee93ed6bffec61263e8ecdecec6750114ffb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423406
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964460}

--- ui/ozone/platform/x11/test/x11_window_unittest.cc
+++ ui/ozone/platform/x11/test/x11_window_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/base/x/test/x11_property_change_waiter.h"
@@ -379,9 +380,17 @@ TEST_F(X11WindowTest, DISABLED_Shape) {
   EXPECT_FALSE(ShapeRectContainsPoint(shape_rects, 500, 500));
 }
 
+// Flaky on Linux ASAN and ChromeOS. https://crbug.com/1291868
+#if (BUILDFLAG(IS_LINUX) && defined(ADDRESS_SANITIZER)) || \
+    BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_WindowManagerTogglesFullscreen \
+  DISABLED_WindowManagerTogglesFullscreen
+#else
+#define MAYBE_WindowManagerTogglesFullscreen WindowManagerTogglesFullscreen
+#endif
 // Test that the widget reacts on changes in fullscreen state initiated by the
 // window manager (e.g. via a window manager accelerator key).
-TEST_F(X11WindowTest, WindowManagerTogglesFullscreen) {
+TEST_F(X11WindowTest, MAYBE_WindowManagerTogglesFullscreen) {
   if (!WmSupportsHint(x11::GetAtom("_NET_WM_STATE_FULLSCREEN")))
     return;
 
