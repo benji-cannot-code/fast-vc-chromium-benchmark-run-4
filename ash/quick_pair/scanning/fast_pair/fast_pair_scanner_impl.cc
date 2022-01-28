8d7e79f3f9330d605327364c961d96172ea2b5c8
Shane Fitzpatrick <shanefitz@google.com>
2022-01-28 20:45:20 +0000

    [FastPair] Low power scanning mode
    This change introduces a new scanning mode which uses less power at
    the cost of discover latency.
    
    While in this mode, we cycle between scanning and not scanning in 2 and
    3 second intervals.
    
    It is controlled via the #fast-pair-low-power flag.
    
    Fixed: b/207589322
    Change-Id: I5e17e377681c43250954b143ce47a09086896a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3417505
    Reviewed-by: Juliet Levesque <julietlevesque@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Shane Fitzpatrick <shanefitz@google.com>
    Cr-Commit-Position: refs/heads/main@{#964761}

--- ash/quick_pair/scanning/fast_pair/fast_pair_scanner_impl.cc
+++ ash/quick_pair/scanning/fast_pair/fast_pair_scanner_impl.cc
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/quick_pair/scanning/fast_pair/fast_pair_scanner_impl.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/quick_pair/common/constants.h"
 #include "ash/quick_pair/common/fast_pair/fast_pair_metrics.h"
 #include "ash/quick_pair/common/logging.h"
 #include "ash/quick_pair/fast_pair_handshake/fast_pair_handshake.h"
 #include "ash/quick_pair/fast_pair_handshake/fast_pair_handshake_lookup.h"
+#include "base/bind.h"
 #include "base/containers/contains.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/bluetooth_low_energy_scan_filter.h"
@@ -22,6 +25,8 @@ constexpr base::TimeDelta kFilterDeviceLostTimeout = base::Seconds(6);
 constexpr uint8_t kFilterPatternStartPosition = 0;
 const std::vector<uint8_t> kFastPairFilterPatternValue = {0x2c, 0xfe};
 constexpr base::TimeDelta kRssiSamplingPeriod = base::Milliseconds(500);
+constexpr base::TimeDelta kLowPowerScanningActiveTime = base::Seconds(2);
+constexpr base::TimeDelta kLowPowerScanningInactiveTime = base::Seconds(3);
 
 }  // namespace
 
@@ -39,7 +44,8 @@ std::ostream& operator<<(
   return out;
 }
 
-FastPairScannerImpl::FastPairScannerImpl() {
+FastPairScannerImpl::FastPairScannerImpl()
+    : task_runner_(base::SequencedTaskRunnerHandle::Get()) {
   device::BluetoothAdapterFactory::Get()->GetAdapter(base::BindOnce(
       &FastPairScannerImpl::OnGetAdapter, weak_ptr_factory_.GetWeakPtr()));
 }
@@ -51,6 +57,12 @@ void FastPairScannerImpl::OnGetAdapter(
   adapter_ = adapter;
   adapter_observation_.Observe(adapter_.get());
 
+  task_runner_->PostTask(FROM_HERE,
+                         base::BindOnce(&FastPairScannerImpl::StartScanning,
+                                        weak_ptr_factory_.GetWeakPtr()));
+}
+
+void FastPairScannerImpl::StartScanning() {
   device::BluetoothLowEnergyScanFilter::Pattern pattern(
       kFilterPatternStartPosition,
       device::BluetoothLowEnergyScanFilter::AdvertisementDataType::kServiceData,
@@ -69,6 +81,26 @@ void FastPairScannerImpl::OnGetAdapter(
 
   background_scan_session_ = adapter_->StartLowEnergyScanSession(
       std::move(filter), weak_ptr_factory_.GetWeakPtr());
+
+  if (features::IsFastPairLowPowerEnabled()) {
+    task_runner_->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&FastPairScannerImpl::StopScanning,
+                       weak_ptr_factory_.GetWeakPtr()),
+        kLowPowerScanningActiveTime);
+  }
+}
+
+void FastPairScannerImpl::StopScanning() {
+  DCHECK(features::IsFastPairLowPowerEnabled());
+
+  background_scan_session_.reset();
+
+  task_runner_->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&FastPairScannerImpl::StartScanning,
+                     weak_ptr_factory_.GetWeakPtr()),
+      kLowPowerScanningInactiveTime);
 }
 
 void FastPairScannerImpl::AddObserver(FastPairScanner::Observer* observer) {
@@ -99,7 +131,6 @@ void FastPairScannerImpl::OnSessionInvalidated(
   // TODO(crbug.com/1227519) Handle Session Invalidation by adding exponential
   // retry to restart the scanner.
   background_scan_session_.reset();
-  device_address_advertisement_data_map_.clear();
 }
 
 void FastPairScannerImpl::OnDeviceFound(
@@ -112,13 +143,20 @@ void FastPairScannerImpl::OnDeviceFound(
     return;
   }
 
+  if (base::Contains(device_address_advertisement_data_map_,
+                     device->GetAddress())) {
+    QP_LOG(INFO) << __func__
+                 << ": Ignoring found device because it was already found.";
+    return;
+  }
+
   FastPairHandshake* handshake =
       FastPairHandshakeLookup::GetInstance()->Get(device->GetAddress());
 
   if (handshake) {
-    QP_LOG(WARNING) << __func__
-                    << ": We have an active handshake for this device, which "
-                       "means we never 'lost' it. We ignore this event.";
+    QP_LOG(INFO) << __func__
+                 << ": We have an active handshake for this device, which "
+                    "means we never 'lost' it. We ignore this event.";
     return;
   }
 
@@ -148,6 +186,17 @@ void FastPairScannerImpl::DeviceChanged(device::BluetoothAdapter* adapter,
 
   device_address_advertisement_data_map_[device_address].insert(*service_data);
   NotifyDeviceFound(device);
+}
+
+void FastPairScannerImpl::DeviceRemoved(device::BluetoothAdapter* adapter,
+                                        device::BluetoothDevice* device) {
+  device_address_advertisement_data_map_.erase(device->GetAddress());
+}
+
+void FastPairScannerImpl::DevicePairedChanged(device::BluetoothAdapter* adapter,
+                                              device::BluetoothDevice* device,
+                                              bool new_paired_status) {
+  device_address_advertisement_data_map_.erase(device->GetAddress());
 }
 
 void FastPairScannerImpl::NotifyDeviceFound(device::BluetoothDevice* device) {
