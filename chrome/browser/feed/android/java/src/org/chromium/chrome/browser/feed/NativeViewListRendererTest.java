e3513526855ff6fea015eaa599631ac476ad5a80
Jian Li <jianli@chromium.org>
2022-01-28 00:11:52 +0000

    Update NativeViewListRendererTest to make it work for robolectric 4.6
    Bug: 1288622
    Change-Id: I8a08b5c121dda07dcf227d7234dadb9b80f6bd67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3422087
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964339}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/NativeViewListRendererTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/NativeViewListRendererTest.java
@@ -114,7 +114,7 @@ public class NativeViewListRendererTest {
         mRenderer.unbind();
         assertNull(view.getAdapter());
         assertNull(view.getLayoutManager());
-        verify(mRenderer, times(1)).notifyItemRangeRemoved(0, 3);
+        verify(mRenderer, times(1)).onItemRangeRemoved(0, 3);
     }
 
     @Test
@@ -123,7 +123,7 @@ public class NativeViewListRendererTest {
                 Arrays.asList(new NtpListContentManager.FeedContent[] {
                         createContent("1"), createContent("2"), createContent("3")}));
         mRenderer.bind(mManager);
-        verify(mRenderer, times(1)).notifyItemRangeInserted(0, 3);
+        verify(mRenderer, times(1)).onItemRangeInserted(0, 3);
     }
 
     @Test
@@ -132,7 +132,7 @@ public class NativeViewListRendererTest {
         mManager.addContents(0,
                 Arrays.asList(new NtpListContentManager.FeedContent[] {
                         createContent("1"), createContent("2"), createContent("3")}));
-        verify(mRenderer, times(1)).notifyItemRangeInserted(0, 3);
+        verify(mRenderer, times(1)).onItemRangeInserted(0, 3);
     }
 
     @Test
@@ -143,7 +143,7 @@ public class NativeViewListRendererTest {
         mRenderer.bind(mManager);
 
         mManager.removeContents(1, 2);
-        verify(mRenderer, times(1)).notifyItemRangeRemoved(1, 2);
+        verify(mRenderer, times(1)).onItemRangeRemoved(1, 2);
     }
 
     @Test
@@ -154,7 +154,7 @@ public class NativeViewListRendererTest {
         mRenderer.bind(mManager);
 
         mRenderer.unbind();
-        verify(mRenderer, times(1)).notifyItemRangeRemoved(0, 3);
+        verify(mRenderer, times(1)).onItemRangeRemoved(0, 3);
     }
 
     @Test
@@ -167,7 +167,7 @@ public class NativeViewListRendererTest {
         mManager.updateContents(1,
                 Arrays.asList(new NtpListContentManager.FeedContent[] {
                         createContent("a"), createContent("b")}));
-        verify(mRenderer, times(1)).notifyItemRangeChanged(1, 2);
+        verify(mRenderer, times(1)).onItemRangeChanged(1, 2);
     }
 
     @Test
@@ -178,7 +178,7 @@ public class NativeViewListRendererTest {
         mRenderer.bind(mManager);
 
         mManager.moveContent(2, 1);
-        verify(mRenderer, times(1)).notifyItemMoved(2, 1);
+        verify(mRenderer, times(1)).onItemMoved(2, 1);
     }
 
     private NtpListContentManager.FeedContent createContent(String text) {
