71f78637de83d2935015ca93a2cae14182fd5aa1
Sharon Yang <yangsharon@chromium.org>
2022-01-28 01:07:33 +0000

    Clean up unused RenderProcessHostFactor in SiteInstanceImpl
    The g_render_process_host_factory_ field in SiteInstanceImpl is no
    longer used. Remove it and the corresponding forward declaration and
    include.
    
    Change-Id: I67b90721c9e52c208ae12fb4659ec78c09bedf2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3421756
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964358}

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/storage_partition_impl.h"
 #include "content/public/browser/browser_or_resource_context.h"
 #include "content/public/browser/content_browser_client.h"
-#include "content/public/browser/render_process_host_factory.h"
 #include "content/public/browser/site_isolation_policy.h"
 #include "content/public/browser/web_ui_controller_factory.h"
 #include "content/public/common/content_client.h"
