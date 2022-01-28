908fb7c45adf4c6631b24c77274559fb3844c56a
minch <minch@chromium.org>
2022-01-28 21:17:37 +0000

    dark_mode: Let the icon of CloseButton can be overridden
    Bug: 1290896
    Change-Id: If3195cbe8342e62a29a70d8501ca85ecf948f760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3418534
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964781}

--- ash/style/close_button.h
+++ ash/style/close_button.h
@@ -10,6 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/view_targeter_delegate.h"
 
+namespace gfx {
+struct VectorIcon;
+}  // namespace gfx
+
 namespace ash {
 
 // A circular ImageButton with kCloseButtonIcon inside. It has small, medium and
@@ -44,6 +48,12 @@ class CloseButton : public views::ImageButton,
   // Resets the listener so that the listener can go out of scope.
   void ResetListener();
 
+  // Sets the vector icon of the button. Note, doing this only when the button
+  // wants to have different icons as the default close icon. E.g, the delete
+  // button inside desks template wants to have trash icon instead of close
+  // icon.
+  void SetVectorIcon(const gfx::VectorIcon& icon);
+
  private:
   // views::ImageButton:
   void OnThemeChanged() override;
@@ -53,7 +63,10 @@ class CloseButton : public views::ImageButton,
   bool DoesIntersectRect(const views::View* target,
                          const gfx::Rect& rect) const override;
 
+  void UpdateVectorIcon();
+
   const Type type_;
+  const gfx::VectorIcon* icon_;
 
   // True if the button wants to use light colors when the D/L mode feature is
   // not enabled. Note, can be removed when D/L mode feature is fully launched.
