fa05ff18fcf44828f535e59f067ddcc11aaefee5
tby <tby@chromium.org>
2022-01-28 04:35:03 +0000

    [Categorical search] set result scores solely with the FTRL ranker
    Removes the obsolete members from the scoring struct, and rewrites the
    FinalScore method to only use the output of the FTRL ranker
    
    Bug: 1199206
    Change-Id: I54d6cc02ae9201696a17c94dd52ac0dbc90e8113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402573
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964429}

--- chrome/browser/ui/app_list/search/search_controller_impl_new_unittest.cc
+++ chrome/browser/ui/app_list/search/search_controller_impl_new_unittest.cc
@@ -46,7 +46,7 @@ class TestSearchResult : public ChromeSearchResult {
     SetCategory(category);
     scoring().best_match_rank = best_match_rank;
     set_relevance(relevance);
-    scoring().normalized_relevance = relevance;
+    scoring().ftrl_result_score = relevance;
   }
 
   TestSearchResult(const TestSearchResult&) = delete;
