c45f89ab3f042c64f4e89c0eeec427747aa8b513
ginnnnnnny <ginnyhuang@chromium.org>
2022-01-28 22:38:41 +0000

    [iOS][media-permissions] Add tab helper and infobar delegate
    This CL adds Permissions tab helper and its respective InfoBarDelegate
    so that the infobar could be added/removed/replaced when any web state
    permissions changes.
    
    Tests will be implemented in follow up CLs.
    
    Bug: 1291206
    Change-Id: Ib9b5993d6c886b02391995054570c7a2b0a62a8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416653
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964815}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/invalid_url_tab_helper.h"
 #import "ios/chrome/browser/web/load_timing_tab_helper.h"
 #import "ios/chrome/browser/web/page_placeholder_tab_helper.h"
+#import "ios/chrome/browser/web/permissions_tab_helper.h"
 #import "ios/chrome/browser/web/print/print_tab_helper.h"
 #import "ios/chrome/browser/web/sad_tab_tab_helper.h"
 #import "ios/chrome/browser/web/session_state/web_session_state_tab_helper.h"
@@ -214,6 +215,7 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
 
   OfflinePageTabHelper::CreateForWebState(
       web_state, ReadingListModelFactory::GetForBrowserState(browser_state));
+  PermissionsTabHelper::CreateForWebState(web_state);
 
   // The language detection helper accepts a callback from the translate
   // client, so must be created after it.
