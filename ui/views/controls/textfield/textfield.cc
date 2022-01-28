e4a5a25a7ae4e8a9051e604895082a8520429cd1
Martin Bidlingmaier <mbid@google.com>
2022-01-28 09:10:02 +0000

    Update password edit cursor on change event
    Previously the position of the cursor was updated whenever the cursor
    visibility changed because of blinking, and wasn't updated in
    `OnCaretBoundsChanged`. The issue was somewhat hidden because the position was
    updated on every cursor blink. This meant that the cursor didn't always move
    immediately but only on the next blink. Now the position is recalculated in
    `OnCaretBoundsChanged`. On blink we now only DCHECK that cursor is displayed
    where it should be.
    
    Bug: 1198742
    Change-Id: I7e645fc1086f99e3b6a4d929ce3466f97ad8187c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420567
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Martin Bidlingmaier <mbid@google.com>
    Cr-Commit-Position: refs/heads/main@{#964488}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -306,7 +306,6 @@ void Textfield::SetTextInputType(ui::TextInputType type) {
   text_input_type_ = type;
   if (GetInputMethod())
     GetInputMethod()->OnTextInputTypeChanged(this);
-  UpdateCursorViewPosition();
   OnCaretBoundsChanged();
   OnPropertyChanged(&text_input_type_, kPropertyEffectsPaint);
 }
@@ -1458,6 +1457,10 @@ bool Textfield::CanComposeInline() const {
   return true;
 }
 
+// TODO(mbid): GetCaretBounds is const but calls
+// RenderText::GetUpdatedCursorBounds, which is not const and in fact mutates
+// internal state. (Is it at least logically const?) Violation of const
+// correctness?
 gfx::Rect Textfield::GetCaretBounds() const {
   gfx::Rect rect = GetRenderText()->GetUpdatedCursorBounds();
   ConvertRectToScreen(this, &rect);
@@ -2402,13 +2405,17 @@ void Textfield::UpdateCursorVisibility() {
     StopBlinkingCursor();
 }
 
-void Textfield::UpdateCursorViewPosition() {
+gfx::Rect Textfield::CalculateCursorViewBounds() const {
   gfx::Rect location(GetRenderText()->GetUpdatedCursorBounds());
   location.set_x(GetMirroredXForRect(location));
   location.set_height(
       std::min(location.height(),
                GetLocalBounds().height() - location.y() - location.y()));
-  cursor_view_->SetBoundsRect(location);
+  return location;
+}
+
+void Textfield::UpdateCursorViewPosition() {
+  cursor_view_->SetBoundsRect(CalculateCursorViewBounds());
 }
 
 int Textfield::GetTextStyle() const {
@@ -2466,6 +2473,8 @@ void Textfield::OnCaretBoundsChanged() {
   // Screen reader users don't expect notifications about unfocused textfields.
   if (HasFocus())
     NotifyAccessibilityEvent(ax::mojom::Event::kTextSelectionChanged, true);
+
+  UpdateCursorViewPosition();
 }
 
 void Textfield::OnBeforeUserAction() {
@@ -2614,7 +2623,7 @@ void Textfield::StopBlinkingCursor() {
 
 void Textfield::OnCursorBlinkTimerFired() {
   DCHECK(ShouldBlinkCursor());
-  UpdateCursorViewPosition();
+  DCHECK_EQ(CalculateCursorViewBounds(), cursor_view_->bounds());
   cursor_view_->SetVisible(!cursor_view_->GetVisible());
 }
 
