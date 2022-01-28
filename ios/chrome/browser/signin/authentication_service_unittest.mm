680f376b8036b678a52145f9c73ffd17000ae07b
Jérôme Lebel <jlebel@chromium.org>
2022-01-28 20:25:55 +0000

    [iOS] Rename 'keychain_reload' parameter in ChromeIdentityService
    This patch is for AuthenticationService, ChromeIdentityService and
    AuthenticationServiceTest.
    This is to match the real meaning of this parameter.
    
    Bug: 1291155
    Change-Id: I2ccc8118fbb8b70e14f39387f04990f81e306b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423755
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964744}

--- ios/chrome/browser/signin/authentication_service_unittest.mm
+++ ios/chrome/browser/signin/authentication_service_unittest.mm
@@ -126,8 +126,8 @@ class AuthenticationServiceTest : public PlatformTest {
     authentication_service()->OnAccessTokenRefreshFailed(identity, user_info);
   }
 
-  void FireIdentityListChanged(bool keychain_reload) {
-    authentication_service()->OnIdentityListChanged(keychain_reload);
+  void FireIdentityListChanged(bool notify_user) {
+    authentication_service()->OnIdentityListChanged(notify_user);
   }
 
   void SetCachedMDMInfo(ChromeIdentity* identity, NSDictionary* user_info) {
@@ -337,7 +337,7 @@ TEST_F(AuthenticationServiceTest, AccountListApprovedByUser_AddedByUser) {
   authentication_service()->SignIn(identity(0));
 
   identity_service()->AddIdentities(@[ @"foo3" ]);
-  FireIdentityListChanged(/*keychain_reload=*/false);
+  FireIdentityListChanged(/*notify_user=*/false);
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(authentication_service()->IsAccountListApprovedByUser());
 }
@@ -349,7 +349,7 @@ TEST_F(AuthenticationServiceTest, AccountListApprovedByUser_ChangedByKeychain) {
   authentication_service()->SignIn(identity(0));
 
   identity_service()->AddIdentities(@[ @"foo3" ]);
-  FireIdentityListChanged(/*keychain_reload=*/true);
+  FireIdentityListChanged(/*notify_user=*/true);
   base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(authentication_service()->IsAccountListApprovedByUser());
 }
@@ -362,14 +362,14 @@ TEST_F(AuthenticationServiceTest,
   authentication_service()->SignIn(identity(0));
 
   identity_service()->AddIdentities(@[ @"foo3" ]);
-  FireIdentityListChanged(/*keychain_reload=*/true);
+  FireIdentityListChanged(/*notify_user=*/true);
   base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(authentication_service()->IsAccountListApprovedByUser());
 
   // Simulate a switching to background, changing the accounts while in
   // background.
   identity_service()->AddIdentities(@[ @"foo4" ]);
-  FireIdentityListChanged(/*keychain_reload=*/true);
+  FireIdentityListChanged(/*notify_user=*/true);
   base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(authentication_service()->IsAccountListApprovedByUser());
 }
@@ -381,7 +381,7 @@ TEST_F(AuthenticationServiceTest,
   authentication_service()->SignIn(identity(0));
 
   identity_service()->AddIdentities(@[ @"foo3" ]);
-  FireIdentityListChanged(/*keychain_reload=*/true);
+  FireIdentityListChanged(/*notify_user=*/true);
   base::RunLoop().RunUntilIdle();
   EXPECT_FALSE(authentication_service()->IsAccountListApprovedByUser());
 
