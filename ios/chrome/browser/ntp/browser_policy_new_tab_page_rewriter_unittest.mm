21468d10d435880db922d82f195370d983dd736f
Veronique Nguyen <veronguyen@google.com>
2022-01-28 17:29:23 +0000

    [NewTabPageLocation]: Write missing EG tests
    - Add New Tab UI EG tests.
    - Fix comments in unit test files.
    
    Bug: 1289778
    Change-Id: Iec5d2e9169040d6b16af16d7c93543170c604fe9
    Validate-Test-Flakiness: skip
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3401572
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#964650}

--- ios/chrome/browser/ntp/browser_policy_new_tab_page_rewriter_unittest.mm
+++ ios/chrome/browser/ntp/browser_policy_new_tab_page_rewriter_unittest.mm
@@ -31,7 +31,7 @@ class BrowserPolicyNewTabPageRewriterTest : public PlatformTest {
   std::unique_ptr<TestChromeBrowserState> browser_state_;
 };
 
-// Test that chrome://newtab is re-written to the custom NTP Location URL when
+// Tests that chrome://newtab is re-written to the custom NTP Location URL when
 // it is set by the policy.
 TEST_F(BrowserPolicyNewTabPageRewriterTest, CustomNtpUrl) {
   std::string custom_url = "https://store.google.com";
@@ -44,8 +44,8 @@ TEST_F(BrowserPolicyNewTabPageRewriterTest, CustomNtpUrl) {
   EXPECT_EQ(url, GURL(custom_url));
 }
 
-// Test that chrome://newtab is not re-written if the custom NTP Location URL is
-// the same.
+// Tests that chrome://newtab is not re-written if the custom NTP Location URL
+// set by the policy is the same.
 TEST_F(BrowserPolicyNewTabPageRewriterTest, SameNtpUrl) {
   std::string custom_url = kChromeUINewTabURL;
   browser_state_->GetPrefs()->SetString(prefs::kNewTabPageLocationOverride,
@@ -58,8 +58,8 @@ TEST_F(BrowserPolicyNewTabPageRewriterTest, SameNtpUrl) {
   EXPECT_EQ(url, GURL(kChromeUINewTabURL));
 }
 
-// Test that chrome://newtab is not re-written if the custom NTP Location URL is
-// not valid.
+// Tests that chrome://newtab is not re-written if the custom NTP Location URL
+// set by the policy is not valid.
 TEST_F(BrowserPolicyNewTabPageRewriterTest, InvalidCustomNtpUrl) {
   std::string custom_url = "blabla";
   browser_state_->GetPrefs()->SetString(prefs::kNewTabPageLocationOverride,
@@ -71,8 +71,8 @@ TEST_F(BrowserPolicyNewTabPageRewriterTest, InvalidCustomNtpUrl) {
   EXPECT_EQ(url, GURL(kChromeUINewTabURL));
 }
 
-// Test that chrome://newtab is not re-written when there is no custom NTP
-// Location URL.
+// Tests that chrome://newtab is not re-written when there is no custom NTP
+// Location URL set by the policy.
 TEST_F(BrowserPolicyNewTabPageRewriterTest, NoCustomNtpUrl) {
   GURL url = GURL(kChromeUINewTabURL);
   EXPECT_FALSE(
@@ -80,7 +80,7 @@ TEST_F(BrowserPolicyNewTabPageRewriterTest, NoCustomNtpUrl) {
   EXPECT_EQ(url, GURL(kChromeUINewTabURL));
 }
 
-// Test that chrome://newtab is not re-written when it is in incognito mode.
+// Tests that chrome://newtab is not re-written when it is in incognito mode.
 TEST_F(BrowserPolicyNewTabPageRewriterTest, IncognitoMode) {
   web::FakeBrowserState fake_browser_state;
   fake_browser_state.SetOffTheRecord(true);
