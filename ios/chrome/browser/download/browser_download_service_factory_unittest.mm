5ab24f457eabc1d6b2c8e6b216180e763333ef3d
Sylvain Defresne <sdefresne@chromium.org>
2022-01-28 15:15:04 +0000

    [ios] Simplify some unit tests
    Avoid keeping TestChromeBrowserState::Builder as local variables
    or member fields when temporary objects can be used.
    
    Simplify BrowserDownloadServiceTest that was creating a second
    BrowserDownloadService when it could just use the one created
    from the default factory (since it use a TestChromeBrowserState
    the service has no previous state so can used directly).
    
    Bug: none
    Change-Id: I4f4af11ee9cf8f890596cd372903df0af903f8bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420475
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964582}

--- ios/chrome/browser/download/browser_download_service_factory_unittest.mm
+++ ios/chrome/browser/download/browser_download_service_factory_unittest.mm
@@ -19,10 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class BrowserDownloadServiceFactoryTest : public PlatformTest {
  protected:
   BrowserDownloadServiceFactoryTest()
-      : browser_state_(browser_state_builder_.Build()) {}
+      : browser_state_(TestChromeBrowserState::Builder().Build()) {}
   // ChromeBrowserState needs thread.
   web::WebTaskEnvironment task_environment_;
-  TestChromeBrowserState::Builder browser_state_builder_;
   std::unique_ptr<TestChromeBrowserState> browser_state_;
 };
 
