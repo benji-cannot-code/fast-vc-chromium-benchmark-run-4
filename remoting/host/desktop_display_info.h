30993b44ed8a78b023ff70c6acb125abff8d7908
Lambros Lambrou <lambroslambrou@chromium.org>
2022-01-29 03:03:47 +0000

    [remoting] Change SelectSource() to take a monitor ID, not an index.
    This updates WebrtcVideoStream::SelectSource() and
    DesktopCapturerProxy::SelectSource() to accept a monitor ID instead of
    an index into the display-info list.
    
    This also fixes an issue in ClientSession::SelectDesktopDisplay() where
    |newGeo| and |oldGeo| were always the same, because they were being
    assigned after updating |show_display_id_|.
    
    The host will now include the monitor ID in VideoLayout control
    messages sent to the website. This can be 64-bit on some platforms, so
    this CL updates the protobuf "id" field to int64.
    
    Change-Id: Ic0bb60ad2de706f846ab04993eae881f0748f4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3414244
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964914}

--- remoting/host/desktop_display_info.h
+++ remoting/host/desktop_display_info.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 
 struct DisplayGeometry {
-  int32_t id;
+  webrtc::ScreenId id;
   int32_t x, y;
   uint32_t width, height;
   uint32_t dpi;     // Number of pixels per logical inch.
