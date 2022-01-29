31cad9323b8ed390dbc16e2caf8b4d5fb0ce4804
Austin Sullivan <asully@chromium.org>
2022-01-29 01:40:16 +0000

    FSA: Improve error message when calling move() on a locked handle
    The default kNoModificationAllowedError DomException is not very
    descriptive.
    
    Bug: 1290878
    Change-Id: I6b6bb5f7a9f47db7015209aff9edc8dca2689f1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3421876
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#964892}

--- content/browser/file_system_access/file_system_access_handle_base.cc
+++ content/browser/file_system_access/file_system_access_handle_base.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/files/file_path.h"
+#include "base/strings/strcat.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "content/browser/file_system_access/file_system_access_directory_handle_impl.h"
@@ -35,6 +37,13 @@ namespace content {
 
 using WriteLock = FileSystemAccessWriteLockManager::WriteLock;
 using WriteLockType = FileSystemAccessWriteLockManager::WriteLockType;
+
+namespace {
+std::string GetURLDisplayName(const storage::FileSystemURL& url) {
+  return base::UTF16ToUTF8(url.path().BaseName().LossyDisplayName());
+}
+
+}  // namespace
 
 FileSystemAccessHandleBase::FileSystemAccessHandleBase(
     FileSystemAccessManagerImpl* manager,
@@ -335,7 +344,10 @@ void FileSystemAccessHandleBase::DidCreateDestinationDirectoryHandle(
       manager()->TakeWriteLock(url(), WriteLockType::kExclusive);
   if (!source_write_lock.has_value()) {
     std::move(callback).Run(file_system_access_error::FromStatus(
-        blink::mojom::FileSystemAccessStatus::kNoModificationAllowedError));
+        blink::mojom::FileSystemAccessStatus::kNoModificationAllowedError,
+        base::StrCat(
+            {"Failed to move ", GetURLDisplayName(url()),
+             ". A FileSystemHandle cannot be moved while it is locked."})));
     return;
   }
   locks.emplace_back(std::move(source_write_lock.value()));
@@ -347,7 +359,11 @@ void FileSystemAccessHandleBase::DidCreateDestinationDirectoryHandle(
         manager()->TakeWriteLock(dest_url, WriteLockType::kExclusive);
     if (!dest_write_lock.has_value()) {
       std::move(callback).Run(file_system_access_error::FromStatus(
-          blink::mojom::FileSystemAccessStatus::kNoModificationAllowedError));
+          blink::mojom::FileSystemAccessStatus::kNoModificationAllowedError,
+          base::StrCat({"Failed to move ", GetURLDisplayName(url()), " to ",
+                        GetURLDisplayName(dest_url),
+                        ". A FileSystemHandle cannot be moved to a destination "
+                        "which is locked."})));
       return;
     }
     locks.emplace_back(std::move(dest_write_lock.value()));
