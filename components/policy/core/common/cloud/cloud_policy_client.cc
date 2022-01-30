ce59902f3b765ed9ee51ca99d96a7295c8c65a02
Pavol Marko <pmarko@google.com>
2022-01-30 23:55:16 +0000

    policy: Remove DMAuth arg from RegisterWithCertificate
    RegisterWithCertificate uses the certificate + signature as
    authentication and doesn't use an Authentication header. This is also
    documented in device_management_backend.proto.
    
    Remove the DMAuth argument so it doesn't confuse readers.
    
    Bug: 1292377
    Change-Id: I075f917250fc6e5fced5d6a085badbd161b0f6af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386842
    Reviewed-by: Roland Bock <rbock@google.com>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965022}

--- components/policy/core/common/cloud/cloud_policy_client.cc
+++ components/policy/core/common/cloud/cloud_policy_client.cc
@@ -272,7 +272,6 @@ void CloudPolicyClient::Register(const RegistrationParameters& parameters,
 void CloudPolicyClient::RegisterWithCertificate(
     const RegistrationParameters& parameters,
     const std::string& client_id,
-    DMAuth auth,
     const std::string& pem_certificate_chain,
     const std::string& sub_organization,
     SigningService* signing_service) {
@@ -299,7 +298,7 @@ void CloudPolicyClient::RegisterWithCertificate(
   signing_service->SignData(
       data.SerializeAsString(),
       base::BindOnce(&CloudPolicyClient::OnRegisterWithCertificateRequestSigned,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(auth)));
+                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void CloudPolicyClient::RegisterWithToken(
@@ -330,7 +329,6 @@ void CloudPolicyClient::RegisterWithToken(
 }
 
 void CloudPolicyClient::OnRegisterWithCertificateRequestSigned(
-    DMAuth auth,
     bool success,
     em::SignedData signed_data) {
   if (!success) {
@@ -342,7 +340,7 @@ void CloudPolicyClient::OnRegisterWithCertificateRequestSigned(
   std::unique_ptr<RegistrationJobConfiguration> config = std::make_unique<
       RegistrationJobConfiguration>(
       DeviceManagementService::JobConfiguration::TYPE_CERT_BASED_REGISTRATION,
-      this, std::move(auth),
+      this, DMAuth::NoAuth(),
       /*oauth_token=*/absl::nullopt,
       base::BindOnce(&CloudPolicyClient::OnRegisterCompleted,
                      weak_ptr_factory_.GetWeakPtr()));
