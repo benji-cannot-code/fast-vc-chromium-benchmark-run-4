e85bd89085c95fa96a12fe456fc6b90bf270d410
Stefan Zager <szager@chromium.org>
2022-01-30 23:51:49 +0000

    Apply ProtectedSequence semantics to cc::Layer subclasses
    Bug: chromium:1255972
    Change-Id: Ib6386473231d6cde9785f52c5a2e8ab778f5d9c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3410916
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965015}

--- cc/trees/property_tree.h
+++ cc/trees/property_tree.h
@@ -58,6 +58,12 @@ using SyncedScrollOffset =
 
 class PropertyTrees;
 
+enum {
+  kInvalidPropertyNodeId = -1,
+  kRootPropertyNodeId = 0,
+  kSecondaryRootPropertyNodeId = 1
+};
+
 template <typename T>
 class CC_EXPORT PropertyTree {
  public:
