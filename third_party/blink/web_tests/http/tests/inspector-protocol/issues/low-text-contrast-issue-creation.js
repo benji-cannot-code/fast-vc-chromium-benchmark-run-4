40d50362a6c462d76a7994696508c69493531d3d
Alex Rudenko <alexrudenko@chromium.org>
2022-01-31 10:59:25 +0000

    Fix flakiness in issues/low-text-contrast-issue-creation.js
    Fixed: 1289181
    Change-Id: I9cac8eb8fb8760faac00e4021e9cb89aa5b0e2ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423795
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965127}

--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/low-text-contrast-issue-creation.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/low-text-contrast-issue-creation.js
@@ -9,7 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   const issue = await dp.Audits.onceIssueAdded();
   // Round contrast ratio to avoid test failure on different platforms.
-  issue.params.issue.details.lowTextContrastIssueDetails.contrastRatio = Number(issue.params.issue.details.lowTextContrastIssueDetails.contrastRatio.toFixed(2));
+  const details = issue.params.issue.details.lowTextContrastIssueDetails;
+  details.contrastRatio = Number(details.contrastRatio.toFixed(2));
+  // details.violatingNodeId might not match exactly depending on the test environment.
+  if (!Number.isInteger(details.violatingNodeId)) {
+    testRunner.log("Error: violatingNodeId is not an integer.");
+  } else {
+    details.violatingNodeId = '<integer>';
+  }
   testRunner.log(issue.params, "Inspector issue: ");
   testRunner.completeTest();
 });
