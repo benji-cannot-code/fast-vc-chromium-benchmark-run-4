40d835724e47fb4d641f9518a114b46f772c3cbc
Darren Shen <shend@chromium.org>
2022-01-31 05:48:37 +0000

    sheriff: Disable flaky testIconLoadingWhenTileNotRegistered and testRenderTileViewReplacing
    Bug: 1288425,1286755
    Change-Id: I0196cae829c89078ced8b160eff63d6f5fff02ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426861
    Auto-Submit: Darren Shen <shend@chromium.org>
    Owners-Override: Darren Shen <shend@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#965082}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGroupUnitTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/tile/TileGroupUnitTest.java
@@ -44,6 +44,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.UiThreadTest;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.browser.native_page.ContextMenuManager;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.suggestions.ImageFetcher;
@@ -335,6 +336,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @FlakyTest(message = "Test is flaky, see crbug.com/1286755")
     public void testRenderTileViewReplacing() {
         SuggestionsUiDelegate uiDelegate = mSuggestionsUiDelegate;
         when(uiDelegate.getImageFetcher()).thenReturn(mMockImageFetcher);
@@ -407,6 +409,7 @@ public class TileGroupUnitTest {
     @Test
     @UiThreadTest
     @SmallTest
+    @FlakyTest(message = "Test is flaky, see crbug.com/1288425")
     public void testIconLoadingWhenTileNotRegistered() {
         TileGroup tileGroup = initialiseTileGroup();
         Tile tile = new Tile(createSiteSuggestion("title", URLS[0]), 0);
