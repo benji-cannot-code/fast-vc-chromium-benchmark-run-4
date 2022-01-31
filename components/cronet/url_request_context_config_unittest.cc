68493b87847a5611ddb5046a0730ee1e3caa8006
Stefano Duo <stefanoduo@google.com>
2022-01-31 12:20:41 +0000

    [Code Health Rotation] Remove DictionaryValue
    Refactor Cronet experimental option parsing to use
    base::Value::DictStorage instead of base::DictionaryValue. Store them as
    a base::Value inside of CronetURLRequestContextor for ease of use.
    
    Also, make the parsing code more consistent (i.e., remove a lot of if by
    making use of optional::value_or).
    
    Bug: 1187061
    Change-Id: Id40c569e24139efe275b9ceb495928a385963107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386968
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#965141}

--- components/cronet/url_request_context_config_unittest.cc
+++ components/cronet/url_request_context_config_unittest.cc
@@ -184,7 +184,8 @@ TEST(URLRequestContextConfigTest, TestExperimentalOptionParsing) {
 
   net::URLRequestContextBuilder builder;
   config->ConfigureURLRequestContextBuilder(&builder);
-  EXPECT_FALSE(config->effective_experimental_options->HasKey("UnknownOption"));
+  EXPECT_FALSE(
+      config->effective_experimental_options.contains("UnknownOption"));
   // Set a ProxyConfigService to avoid DCHECK failure when building.
   builder.set_proxy_config_service(
       std::make_unique<net::ProxyConfigServiceFixed>(
@@ -1456,7 +1457,7 @@ TEST(URLRequestContextConfigTest, WrongSpdyGoAwayOnIPChangeValue) {
 
   net::URLRequestContextBuilder builder;
   config->ConfigureURLRequestContextBuilder(&builder);
-  EXPECT_FALSE(config->effective_experimental_options->HasKey(
+  EXPECT_FALSE(config->effective_experimental_options.contains(
       "spdy_go_away_on_ip_change"));
 }
 
@@ -1488,7 +1489,7 @@ TEST(URLRequestContextConfigTest, BidiStreamDetectBrokenConnection) {
           // User-Agent request header field.
           "fake agent",
           // JSON encoded experimental options.
-          "{\"bidi_stream_detect_broken_connection\": 10}",
+          "{\"bidi_stream_detect_broken_connection\":10}",
           // MockCertVerifier to use for testing purposes.
           std::unique_ptr<net::CertVerifier>(),
           // Enable network quality estimator.
@@ -1500,7 +1501,7 @@ TEST(URLRequestContextConfigTest, BidiStreamDetectBrokenConnection) {
 
   net::URLRequestContextBuilder builder;
   config->ConfigureURLRequestContextBuilder(&builder);
-  EXPECT_TRUE(config->effective_experimental_options->HasKey(
+  EXPECT_TRUE(config->effective_experimental_options.contains(
       "bidi_stream_detect_broken_connection"));
   EXPECT_TRUE(config->bidi_stream_detect_broken_connection);
   EXPECT_EQ(config->heartbeat_interval, base::Seconds(10));
@@ -1546,7 +1547,7 @@ TEST(URLRequestContextConfigTest, WrongBidiStreamDetectBrokenConnectionValue) {
 
   net::URLRequestContextBuilder builder;
   config->ConfigureURLRequestContextBuilder(&builder);
-  EXPECT_FALSE(config->effective_experimental_options->HasKey(
+  EXPECT_FALSE(config->effective_experimental_options.contains(
       "bidi_stream_detect_broken_connection"));
 }
 
