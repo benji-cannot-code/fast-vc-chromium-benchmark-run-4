1c532e2c9d430ca70d70d9841a87046b584e0cc0
Ewann <ewannpv@google.com>
2022-01-31 15:48:25 +0000

    [iOS][MediaPermissions][4/N] Adds infobar UI
    This CL adds the modal permissions infobar skeleton.
    
    Bug: 1289645
    Change-Id: Ieffc6b7d817b9df54ae8f0d429b458a96e996ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3417034
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965198}

---
+++ ios/chrome/browser/ui/infobars/modals/permissions/infobar_permissions_modal_consumer.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_PERMISSIONS_INFOBAR_PERMISSIONS_MODAL_CONSUMER_H_
+#define IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_PERMISSIONS_INFOBAR_PERMISSIONS_MODAL_CONSUMER_H_
+
+#import <Foundation/Foundation.h>
+
+// Consumer for model to push configurations to the permissions UI.
+@protocol InfobarPermissionsModalConsumer <NSObject>
+
+// TODO(crbug.com/1289645): Implement this.
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_PERMISSIONS_INFOBAR_PERMISSIONS_MODAL_CONSUMER_H_
---
+++ ios/chrome/browser/ui/infobars/modals/permissions/infobar_permissions_modal_delegate.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_PERMISSIONS_INFOBAR_PERMISSIONS_MODAL_DELEGATE_H_
+#define IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_PERMISSIONS_INFOBAR_PERMISSIONS_MODAL_DELEGATE_H_
+
+#import "ios/chrome/browser/ui/infobars/modals/infobar_modal_delegate.h"
+
+// Delegate to handle permissions modal actions.
+@protocol InfobarPermissionsModalDelegate <InfobarModalDelegate>
+
+// TODO(crbug.com/1289645): Implement this.
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_INFOBARS_MODALS_PERMISSIONS_INFOBAR_PERMISSIONS_MODAL_DELEGATE_H_
