e01277e6a3258b291cd4d6810339afb60f41c168
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-01-31 14:50:22 +0000

    HTML spec: use the multi page.
    (This patch doesn't modify Chrome's behavior.)
    
    A nice tips I learnt from yhirano@: Any URL toward the html spec can be
    turned into one using the multi-page, by prepending /C in the URL.
    
    The single-page URL is complex and takes a dozen of seconds to load on
    high end laptop. It may also have difficulties to load at all on some
    low end computers.
    This patch applies this suggestion everywhere arthursonzogni@ and
    yhirano@ have OWNERship. It was already applied in most locations, but
    not all. It is better to stay consistent and this make it easy for other
    to "discover" what is /C.
    
    Note: third_party/web_tests has been ignored, because it contains to
    many changes.
    
    Bug: None
    Change-Id: Iba90c6a7054e3e8220ff1ad480cf4ce82397ce70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423922
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965170}

--- third_party/blink/renderer/core/html/parser/parser_scripting_flag_policy.h
+++ third_party/blink/renderer/core/html/parser/parser_scripting_flag_policy.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // Decides whether the scripting flag of the parser should be set to enabled.
-// https://html.spec.whatwg.org/#scripting-flag
+// https://html.spec.whatwg.org/C/#scripting-flag
 enum class ParserScriptingFlagPolicy { kOnlyIfScriptIsEnabled, kEnabled };
 
 }  // namespace blink
