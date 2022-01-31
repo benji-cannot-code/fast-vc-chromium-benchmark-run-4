8172a8e96ac1c06ff32220e814a138b725e64545
David Sanders <dsanders11@ucsbalum.com>
2022-01-31 23:09:58 +0000

    Use std::make_unique instead of base::WrapUnique
    Bug: 1291614
    Change-Id: I979b04b914ac4325b705d1ee8501eef0e8b3eab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3421330
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965406}

--- chrome/browser/ui/views/chrome_constrained_window_views_client.cc
+++ chrome/browser/ui/views/chrome_constrained_window_views_client.cc
@@ -47,5 +47,5 @@ class ChromeConstrainedWindowViewsClient
 
 std::unique_ptr<constrained_window::ConstrainedWindowViewsClient>
 CreateChromeConstrainedWindowViewsClient() {
-  return base::WrapUnique(new ChromeConstrainedWindowViewsClient);
+  return std::make_unique<ChromeConstrainedWindowViewsClient>();
 }
