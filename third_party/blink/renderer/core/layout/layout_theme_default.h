6744cfda33da243205f1ab52812b4a9cceb315f4
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-01-31 03:37:53 +0000

    Remove win_quirks.css stylesheet.
    The win_quirks.css stylesheet contains a single rule:
    textarea {
      padding: 2px 0 0 2px;
    }
    (it removes the right/bottom padding from textarea).
    
    AFAICT we are the only "modern" browser who was shipping this quirk.
    From my quick investigation:
     - Firefox (any OS) - no quirk.
     - Safari - no quirk.
     - EdgeHTML18 (Win10) - no quirk.
    
    Tried to dig up why this exists, wasn't super helpful. Additionally at
    some point we changed behaviour as this is shipping on all OSes for us.
    
    Additionally this is highly unlikely to result in a change of size
    behaviour *if the size is explicitly set* as the default box-sizing
    in quirks mode is border-box.
    
    Change-Id: I6b18db569758ba1f0b1ad49b88febf2186f72d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3422590
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965062}

--- third_party/blink/renderer/core/layout/layout_theme_default.h
+++ third_party/blink/renderer/core/layout/layout_theme_default.h
@@ -39,7 +39,6 @@ namespace blink {
 class CORE_EXPORT LayoutThemeDefault : public LayoutTheme {
  public:
   String ExtraDefaultStyleSheet() override;
-  String ExtraQuirksStyleSheet() override;
 
   Color PlatformActiveSelectionBackgroundColor(
       mojom::blink::ColorScheme color_scheme) const override;
