bcefe2d80c9b8d9eb91ff02a95b642da6a930341
Peter Varga <pvarga@inf.u-szeged.hu>
2022-01-31 21:18:15 +0000

    IWYU: add algorithm for std::max and std::min
    Bug: none
    Change-Id: I0305f5c1a52f8245e7c4bd38e7364905b5fd38e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3425369
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965356}

--- ui/gfx/range/range.cc
+++ ui/gfx/range/range.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <inttypes.h>
 
-#include <algorithm>
-
 #include "base/strings/stringprintf.h"
 
 namespace gfx {
