ef33684d861426777a5a3ea9ecf9ffe384a5aace
Andrey Kosyakov <caseq@chromium.org>
2022-01-31 23:31:16 +0000

    headless: fix fallback loading of resources.pak
    Bring back loading of resources.pak as a fall-back when headless-specific
    resources are not found, this happens to be important in configurations
    that don't ship headless resources explicitly.
    Originally regressed by https://crrev.com/c/3407727
    
    Change-Id: I2b0ceea9dba10c502cc68d334297ba8ce7eed258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428144
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965416}

--- headless/lib/headless_content_main_delegate.cc
+++ headless/lib/headless_content_main_delegate.cc
@@ -119,6 +119,8 @@ void InitializeResourceBundle(const base::CommandLine& command_line) {
   ui::ResourceBundle::InitSharedInstanceWithLocale(
       locale, nullptr, ui::ResourceBundle::DO_NOT_LOAD_COMMON_RESOURCES);
   // Otherwise, load resources.pak, chrome_100 and chrome_200.
+  base::FilePath resources_pak =
+      resource_dir.Append(FILE_PATH_LITERAL("resources.pak"));
   base::FilePath chrome_100_pak =
       resource_dir.Append(FILE_PATH_LITERAL("chrome_100_percent.pak"));
   base::FilePath chrome_200_pak =
@@ -127,7 +129,9 @@ void InitializeResourceBundle(const base::CommandLine& command_line) {
 #if BUILDFLAG(IS_MAC) && !defined(COMPONENT_BUILD)
   // In non component builds, check if fall back in Resources/ folder is
   // available.
-  if (!base::PathExists(chrome_100_pak)) {
+  if (!base::PathExists(resources_pak)) {
+    resources_pak =
+        resource_dir.Append(FILE_PATH_LITERAL("Resources/resources.pak"));
     chrome_100_pak = resource_dir.Append(
         FILE_PATH_LITERAL("Resources/chrome_100_percent.pak"));
     chrome_200_pak = resource_dir.Append(
@@ -135,6 +139,8 @@ void InitializeResourceBundle(const base::CommandLine& command_line) {
   }
 #endif
 
+  ui::ResourceBundle::GetSharedInstance().AddDataPackFromPath(
+      resources_pak, ui::kScaleFactorNone);
   ui::ResourceBundle::GetSharedInstance().AddDataPackFromPath(chrome_100_pak,
                                                               ui::k100Percent);
   ui::ResourceBundle::GetSharedInstance().AddDataPackFromPath(chrome_200_pak,
