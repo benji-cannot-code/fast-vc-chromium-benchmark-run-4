6eca305ac2678bba5245896cfc808af5b5e84a8e
Nanami Mikiya <nmikiya@google.com>
2022-01-31 03:22:25 +0000

    Logging WebSocket handshake failure
    The failure during WebSocket handshake is reported only by OnFailure()
    method. Logging it on NetLog will help developers check details.
    
    As an example, the original NetLog shows:
    +HTTP_TRANSACTION_READ_HEADERS
       HTTP_STREAM_PARSER_READ_HEADERS
    -HTTP_TRANSACTION_READ_HEADERS
    
    This CL makes it like:
    +HTTP_TRANSACTION_READ_HEADERS
       HTTP_STREAM_PARSER_READ_HEADERS
       WEBSOCKET_UPGRADE_FAILURE
       --> message = "XX"
       --> net_error = XX
    -HTTP_TRANSACTION_READ_HEADERS
    
    Change-Id: Icd3d75fe28cad9b45038cc4fde3d181f696cef90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419421
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nanami Mikiya <nmikiya@google.com>
    Cr-Commit-Position: refs/heads/main@{#965057}

--- net/websockets/websocket_basic_handshake_stream.h
+++ net/websockets/websocket_basic_handshake_stream.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/completion_once_callback.h"
 #include "net/base/net_export.h"
 #include "net/http/http_basic_state.h"
+#include "net/log/net_log_with_source.h"
 #include "net/websockets/websocket_handshake_stream_base.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
@@ -156,6 +157,8 @@ class NET_EXPORT_PRIVATE WebSocketBasicHandshakeStream final
   const raw_ptr<WebSocketStreamRequestAPI> stream_request_;
 
   const raw_ptr<WebSocketEndpointLockManager> websocket_endpoint_lock_manager_;
+
+  NetLogWithSource net_log_;
 
   base::WeakPtrFactory<WebSocketBasicHandshakeStream> weak_ptr_factory_{this};
 };
