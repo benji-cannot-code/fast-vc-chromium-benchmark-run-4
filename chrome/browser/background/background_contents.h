49b4d1a6950028d0a0c37618899071eea6f67614
Alex Moshchuk <alexmos@chromium.org>
2022-01-31 22:59:25 +0000

    Remove StoragePartitionId.
    (original CL by acolwell@, rebased and submitted by alexmos@)
    
    This is the final patch in the series to migrate the
    NavigationControllerImpl::session_storage_namespace_map_ to use a
    StoragePartitionConfig instead of a partition ID string. It removes the
    StoragePartitionId class and updates all the existing usage to
    StoragePartitionConfig. This should not introduce user visible behavior
    changes because the current StoragePartitionId implementation is
    essentially a wrapper around a StoragePartitionConfig.  That change was
    made in https://crrev.com/c/2808670, which has reached stable in M93
    and raised no issues.
    
    - Removes the StoragePartitionId object and replace usage with
      StoragePartitionConfig.
    - Removed NavigationControllerImpl::partition_config_to_id_map_ since
      it is no longer needed.
    - Removed SiteInfo usage from RenderViewHostImpl now that we only
      need a StoragePartitionConfig to get the SessionStorageNamespace.
    - Added GetStoragePartitionConfig() method to SiteInstance to make
      it easier to get the StoragePartitionConfig for a SiteInstance.
      This will also make it easier to remove GetSiteURL() usage related
      to storage partitions in followup CLs.
    - Removed SiteInstanceImpl::GetSiteInfoForRenderViewHost() and
      move code that enabled storage partition verification to the new
      GetStoragePartitionConfig() method.
    
    Bug: 1166021
    Change-Id: Iefc00108e2d1274dc69bd31a7e4d8f667ffea960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2811120
    Reviewed-by: Charles Reis <creis@chromium.org>
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965388}

--- chrome/browser/background/background_contents.h
+++ chrome/browser/background/background_contents.h
@@ -65,7 +65,7 @@ class BackgroundContents : public extensions::DeferredStartRenderHost,
       content::RenderFrameHost* opener,
       bool is_new_browsing_instance,
       Delegate* delegate,
-      const content::StoragePartitionId& partition_id,
+      const content::StoragePartitionConfig& partition_config,
       content::SessionStorageNamespace* session_storage_namespace);
 
   BackgroundContents(const BackgroundContents&) = delete;
