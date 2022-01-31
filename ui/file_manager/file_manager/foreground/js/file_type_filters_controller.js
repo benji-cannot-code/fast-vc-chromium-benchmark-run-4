39f72a039698a43d5d97574dbcd23d4e418103b8
Wenbo Jie <wenbojie@chromium.org>
2022-01-31 23:58:27 +0000

    [FilesRecentFilter] Add UMA for recent filter buttons
    Add an enum metric for Recent file type filter, when "All/Audio/Images/
    Videos" filter buttons are clicked, a enum metric will be triggered
    with the corresponding value.
    
    Demo: http://shortn/_55mGTeSJFK
    Note: When the "Recent" is clicked on the left side nav, the "All"
    filter button will be active by default, this won't be recorded by
    this metric, because the "Recent" click will be recorded by other
    metrics (e.g. Location.OnEntrySelected.TopLevel).
    
    Bug: 1128377
    Test: use chrome://histograms/ to check the metric triggered
    Test: browser_tests --gtest-filter=*FileTypeFiltersController*
    Change-Id: I718adfb2f988ce8f3444ed9d0fd6cc56fae78efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3406243
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965432}

--- ui/file_manager/file_manager/foreground/js/file_type_filters_controller.js
+++ ui/file_manager/file_manager/foreground/js/file_type_filters_controller.js
@@ -3,7 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {metrics} from '../../common/js/metrics.js';
 import {str, util} from '../../common/js/util.js';
+import {DirectoryChangeEvent} from '../../externs/directory_change_event.js';
 import {FakeEntry} from '../../externs/files_app_entry_interfaces.js';
 
 import {DirectoryModel} from './directory_model.js';
@@ -76,6 +78,29 @@ export class FileTypeFiltersController {
   }
 
   /**
+   * @param {chrome.fileManagerPrivate.RecentFileType} fileType File type filter
+   *     which needs to be recorded.
+   * @private
+   */
+  recordFileTypeFilterUMA_(fileType) {
+    /**
+     * Keep the order of this in sync with FileManagerRecentFilterType in
+     * tools/metrics/histograms/enums.xml.
+     * The array indices will be recorded in UMA as enum values. The index for
+     * each filter type should never be renumbered nor reused in this array.
+     */
+    const FileTypeFiltersForUMA =
+        /** @type {!Array<!chrome.fileManagerPrivate.RecentFileType>} */ ([
+          chrome.fileManagerPrivate.RecentFileType.ALL,    // 0
+          chrome.fileManagerPrivate.RecentFileType.AUDIO,  // 1
+          chrome.fileManagerPrivate.RecentFileType.IMAGE,  // 2
+          chrome.fileManagerPrivate.RecentFileType.VIDEO,  // 3
+        ]);
+    Object.freeze(FileTypeFiltersForUMA);
+    metrics.recordEnum('Recent.FilterByType', fileType, FileTypeFiltersForUMA);
+  }
+
+  /**
    * Creates filter button's UI element.
    * @param {string} label Label of the filter button.
    * @param {chrome.fileManagerPrivate.RecentFileType} fileType File type filter
@@ -101,8 +126,10 @@ export class FileTypeFiltersController {
    * @private
    */
   onCurrentDirectoryChanged_(event) {
+    const directoryChangeEvent = /** @type {!DirectoryChangeEvent} */ (event);
     // We show filter buttons only in Recents view at this moment.
-    this.container_.hidden = !(event.newDirEntry == this.recentEntry_);
+    this.container_.hidden =
+        !(directoryChangeEvent.newDirEntry == this.recentEntry_);
   }
 
   /**
@@ -111,7 +138,7 @@ export class FileTypeFiltersController {
    * @private
    */
   onFilterButtonClicked_(event) {
-    const {target} = event;
+    const target = /** @type {HTMLButtonElement} */ (event.target);
     // At least one filter button should be selected, so we don't allow user to
     // unselect a filter button by clicking it, e.g. do nothing if an active
     // filter button is clicked.
@@ -125,6 +152,7 @@ export class FileTypeFiltersController {
     // Refresh current directory with the updated Recent setting.
     // We don't need to invalidate the cached metadata for this rescan.
     this.directoryModel_.rescan(false);
+    this.recordFileTypeFilterUMA_(newFilter);
   }
 
   /**
