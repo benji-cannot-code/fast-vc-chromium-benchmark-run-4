cf6ea9e2acdb6786a2120c2ecbd7ca4fdef91a8a
Florian Gauger <fga@google.com>
2022-02-01 16:28:18 +0000

    [Autofill Assistant] Fix crash in ~ClientAndroid()
    The crash was caused by us moving the same casted pointer inside two
    separate unique_ptr.
    
    Bug: 3421044
    Change-Id: Ifd3e43c371cc9c2ba0dd51070f2ecfd9001a6ce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3429169
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#965721}

--- chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantStaticDependencies.java
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantStaticDependencies.java
@@ -23,7 +23,7 @@ import org.chromium.ui.util.AccessibilityUtil;
 @JNINamespace("autofill_assistant")
 public interface AssistantStaticDependencies {
     @CalledByNative
-    long getNativePointer();
+    long createNative();
 
     /**
      * Create the Activity specific dependencies.
--- chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantStaticDependenciesChrome.java
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantStaticDependenciesChrome.java
@@ -32,11 +32,9 @@ public class AssistantStaticDependenciesChrome implements AssistantStaticDepende
     private long mNativePointer;
 
     @Override
-    public long getNativePointer() {
-        if (mNativePointer == 0) {
-            mNativePointer = AssistantStaticDependenciesChromeJni.get().init(this);
-        }
-        return mNativePointer;
+    public long createNative() {
+        return AssistantStaticDependenciesChromeJni.get().init(
+                new AssistantStaticDependenciesChrome());
     }
 
     @Override
