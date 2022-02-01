ef9fb5ebdbea8d7911589e35d8999f1f0791d775
Hirthanan Subenderan <hirthanan@google.com>
2022-02-01 20:19:58 +0000

    Add full hardware class to device active ping.
    Retrieve the full hardware class from the machine statistics provider,
    and attach it to the check in ping sent to Fresnel.
    
    Privacy, security, and legal approvals can be found in crbug below.
    
    Googlers: go/cros-device-activity-basic-dims
    
    BUG: chromium:1251669
    Change-Id: I44692bf8ca7b870d194bba0ad006761d6f5bf9d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426784
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#965840}

--- ash/components/device_activity/device_activity_controller.h
+++ ash/components/device_activity/device_activity_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/components/device_activity/trigger.h"
 #include "base/component_export.h"
+#include "chromeos/system/statistics_provider.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 class PrefRegistrySimple;
@@ -49,7 +50,16 @@ class COMPONENT_EXPORT(ASH_DEVICE_ACTIVITY) DeviceActivityController {
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       const std::string& psm_device_active_secret);
 
+  void OnMachineStatisticsLoaded(
+      Trigger trigger,
+      PrefService* local_state,
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+      const std::string& psm_device_active_secret);
+
   std::unique_ptr<DeviceActivityClient> da_client_network_;
+
+  // Singleton lives throughout class lifetime.
+  chromeos::system::StatisticsProvider* const statistics_provider_;
 
   // Automatically cancels callbacks when the referent of weakptr gets
   // destroyed.
