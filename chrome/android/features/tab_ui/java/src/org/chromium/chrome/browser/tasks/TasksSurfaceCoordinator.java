e1febc4c1a459a9f5a7098740a771bc872b6182c
Min Qin <qinmin@chromium.org>
2022-02-01 20:10:51 +0000

    Implement the tile click for QueryTiles on start surface
    This CL hooks up tile click to OmniboxStub for start surface, so
    QT will now work the same way as NTP
    
    BUG=1269921
    
    Change-Id: I3747fc1a9f0fe0045ed2fdb004d2a9e0617bf088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423434
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965831}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksSurfaceCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksSurfaceCoordinator.java
@@ -152,7 +152,7 @@ public class TasksSurfaceCoordinator implements TasksSurface {
     }
 
     private void performSearchQuery(QueryInfo queryInfo) {
-        // TODO(qinmin): load the search result page.
+        mMediator.performSearchQuery(queryInfo.queryText, queryInfo.searchParams);
     }
 
     /**
