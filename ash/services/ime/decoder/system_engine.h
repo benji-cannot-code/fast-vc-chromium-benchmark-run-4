c52d04c6fc488678ec3fa4d9cda44768d328de7c
Curtis McMullan <curtismcmullan@chromium.org>
2022-02-01 01:17:18 +0000

    ime: add message pipe to ime service mojom
    This is the first in a number of CLs that aim to migrate the
    bidirectional communication between Chromium and the IME shared library
    from two mojo pipes, to a single shared pipe with associated interfaces.
    
    In this CL we are providing a new mojo method and interface that will be
    used to bootstrap this shared pipe in the shared library.
    
    Once this is landed we can import the mojom definitions to the shared
    library and implement the receiving side.
    
    BUG=b:209695253
    
    Change-Id: I361b1145f2aa699893e2a9993b48fe6ff82148ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3418802
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965459}

--- ash/services/ime/decoder/system_engine.h
+++ ash/services/ime/decoder/system_engine.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/services/ime/ime_decoder.h"
 #include "ash/services/ime/input_engine.h"
 #include "ash/services/ime/public/cpp/shared_lib/interfaces.h"
+#include "ash/services/ime/public/mojom/connection_factory.mojom.h"
 #include "ash/services/ime/public/mojom/input_engine.mojom.h"
 #include "ash/services/ime/public/mojom/input_method.mojom.h"
 #include "ash/services/ime/public/mojom/input_method_host.mojom.h"
@@ -36,6 +37,10 @@ class SystemEngine : public InputEngine {
   bool BindRequest(const std::string& ime_spec,
                    mojo::PendingReceiver<mojom::InputMethod> receiver,
                    mojo::PendingRemote<mojom::InputMethodHost> host);
+
+  // Binds the mojom::ConnectionFactory interface in the shared library.
+  bool BindConnectionFactory(
+      mojo::PendingReceiver<mojom::ConnectionFactory> receiver);
 
   // InputEngine:
   bool IsConnected() override;
