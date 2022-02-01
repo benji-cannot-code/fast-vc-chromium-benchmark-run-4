08f87bcbf9825438e8381031a359fc25d9c67572
Maksim Moskvitin <mmoskvitin@google.com>
2022-02-01 12:25:24 +0000

    [Sync][Lacros] Redirect to OS SyncSetup page when asking for passphrase
    Once Lacros is launched there should be no way to access Ash browser
    settings pages from the OS. Passphrase required notification belongs to
    Ash and should work regardless of the Lacros state.
    
    This CL redirects the user to OS SyncSetup page when they click on the
    passphrase required notification instead of browser SyncSetup page. The
    change takes the effect only when Lacros is the primary browser.
    
    Bug: 1233545
    Change-Id: Ic199f84c7568da08b015e99d30c2eb295182d518
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379160
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#965641}

--- chrome/browser/ash/sync/sync_error_notifier.cc
+++ chrome/browser/ash/sync/sync_error_notifier.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_common.h"
 #include "chrome/browser/notifications/notification_display_service.h"
@@ -17,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
+#include "chrome/browser/ui/settings_window_manager_chromeos.h"
+#include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
 #include "chrome/common/url_constants.h"
@@ -54,7 +57,14 @@ void ShowSyncSetup(Profile* profile) {
     return;
   }
 
-  chrome::ShowSettingsSubPageForProfile(profile, chrome::kSyncSetupSubPage);
+  if (crosapi::browser_util::IsLacrosPrimaryBrowser()) {
+    chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
+        profile, chromeos::settings::mojom::kSyncSetupSubpagePath);
+  } else {
+    // TODO(crbug.com/1286405): remove this once it's not possible to use ash
+    // as a primary browser.
+    chrome::ShowSettingsSubPageForProfile(profile, chrome::kSyncSetupSubPage);
+  }
 }
 
 void TriggerSyncKeyRetrieval(Profile* profile) {
