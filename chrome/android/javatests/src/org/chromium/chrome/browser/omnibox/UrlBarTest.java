260b0ef08343fab8fb5254fa0ad69d2ff0c1262d
Boris Sazonov <bsazonov@chromium.org>
2022-02-01 10:01:54 +0000

    Disable UrlBarTest#{testSendCursorPosition, testAutocompleteUpdatedOnSelection}
    Reason: failing on "test-o-phone".
    
    Bug: 1291968
    Change-Id: I0fd8da137b8f5d7e334ba098e006c0aa6c362a47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423768
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965606}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
@@ -29,6 +29,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.UrlUtils;
@@ -204,6 +205,7 @@ public class UrlBarTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1291968")
     public void testAutocompleteUpdatedOnSelection() throws TimeoutException {
         // Verify that setting a selection before the autocomplete clears it.
         verifySelectionState("test", "ing is fun", 1, 1, false, "test", "test", true, "test");
@@ -253,6 +255,7 @@ public class UrlBarTest {
      */
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1291968")
     public void testSendCursorPosition() throws TimeoutException {
         final CallbackHelper autocompleteHelper = new CallbackHelper();
         final AtomicInteger cursorPositionUsed = new AtomicInteger();
