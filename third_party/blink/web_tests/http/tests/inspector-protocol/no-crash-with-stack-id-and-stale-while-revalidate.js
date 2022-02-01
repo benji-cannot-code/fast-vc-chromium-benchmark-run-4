7c45e0160ce1a076f5d4d8f12e70f83016b33486
Danil Somsikov <dsv@chromium.org>
2022-02-01 17:04:53 +0000

    Remove non-essential logging that seems to be racey and cause flakiness.
    Bug: 1211297
    Change-Id: I4e9472c0fb2a6894e3072e2fb21a54f975fe3d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3427550
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965744}

--- third_party/blink/web_tests/http/tests/inspector-protocol/no-crash-with-stack-id-and-stale-while-revalidate.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/no-crash-with-stack-id-and-stale-while-revalidate.js
@@ -10,15 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   dp.Network.enable();
   dp.Network.setAttachDebugStack({enabled: true});
   await dp.Page.reload({ignoreCache: false});
-  dp.Network.onRequestWillBeSent(request => {
-    const url = request.params.request.url;
-    if (/stale-script\.php$/.test(url)) {
-      testRunner.log('Request Will be Sent for ' + url.substr(url.lastIndexOf('stale-script.php')));
-    }
-  });
 
   let numResponses = 0;
-
   dp.Network.onResponseReceived(request => {
     const url = request.params.response.url;
     if (/stale-script\.php$/.test(url)) {
