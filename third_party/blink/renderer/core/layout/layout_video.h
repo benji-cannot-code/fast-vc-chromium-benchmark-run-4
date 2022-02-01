a0f57bd39b58c99bb357a95c8b74417416a23b4d
Xianzhu Wang <wangxianzhu@chromium.org>
2022-02-01 05:03:57 +0000

    Reland "Don't composite LayoutVideo itself"
    This reverts commit c68a34c4bfa5aa31227726f719d84e90f156ff46.
    
    Reason for revert: Now put the change behind a feature
    CompositeVideoElement (change = disabled, enabled by default).
    
    Original change's description:
    > Revert "Don't composite LayoutVideo itself"
    >
    > This reverts commit d627fa66c962075658f316063982ed4e1bc8747c.
    >
    > Reason for revert: Caused memory regressions (crbug.com/1288701).
    >
    > Original change's description:
    > > Don't composite LayoutVideo itself
    > >
    > > After launch of CompositeAfterPaint, we no longer need to composite
    > > a replaced element itself with accelerated content. This avoids
    > > unnecessary compositing of the border, background, etc. of the element.
    > > The content can still be composited and recorded as a foreign layer.
    > >
    > > Bug: 1277748
    > > Change-Id: I6cca043c18fb0bbbe849c9acd2fc935901c429b0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3386736
    > > Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Reviewed-by: Stefan Zager <szager@chromium.org>
    > > Commit-Queue: Stefan Zager <szager@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#960572}
    >
    > Bug: 1277748, 1288701
    > Change-Id: I6429b6704e33fea9475ecabcc4ede8371edb3118
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3401261
    > Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#960970}
    
    Bug: 1277748, 1288701
    Change-Id: I0abaf4817fdb917a42393cd7a277507e55f90ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423348
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965533}

--- third_party/blink/renderer/core/layout/layout_video.h
+++ third_party/blink/renderer/core/layout/layout_video.h
@@ -84,7 +84,7 @@ class LayoutVideo final : public LayoutMedia {
 
   bool CanHaveAdditionalCompositingReasons() const override {
     NOT_DESTROYED();
-    return true;
+    return RuntimeEnabledFeatures::CompositeVideoElementEnabled();
   }
   CompositingReasons AdditionalCompositingReasons() const override;
 
