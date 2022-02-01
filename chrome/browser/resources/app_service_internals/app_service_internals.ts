86bd556b2c6ba3e89333f0e83efd4cf9a7962433
dpapad <dpapad@chromium.org>
2022-02-01 22:24:40 +0000

    AppServiceInternals: Remove obsolete reference to Mojo Lite bindings.
    This was causing the page to be blank, since [1] no longer exists on
    non-CrOS builds.
    
    [1] chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js
    
    Bug: 1292893
    Change-Id: I087790711d2f911d2c4d12c466815aaaa9f5cd20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3429082
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965903}

--- chrome/browser/resources/app_service_internals/app_service_internals.ts
+++ chrome/browser/resources/app_service_internals/app_service_internals.ts
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
-
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {AppInfo, AppServiceInternalsPageHandler, PreferredAppInfo} from './app_service_internals.mojom-webui.js';
