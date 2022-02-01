e0869f5bed2f582128bcf1a497244f4c97f93362
Ghazale Hosseinabadi <ghazale@google.com>
2022-02-01 06:00:10 +0000

    [Code health] Remove uses of Value::Equal
    This CL removes some uses of Value::Equal related to the //extensions
    code.
    
    Bug: 1187018
    Change-Id: Ia966b454338f2f9fe3168f8e0477a8424d416cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426297
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965542}

--- chrome/browser/ui/webui/print_preview/extension_printer_handler_unittest.cc
+++ chrome/browser/ui/webui/print_preview/extension_printer_handler_unittest.cc
@@ -565,7 +565,7 @@ TEST_F(ExtensionPrinterHandlerTest, GetPrinters) {
   EXPECT_EQ(1u, call_count);
   EXPECT_TRUE(is_done);
   ASSERT_TRUE(printers.get());
-  EXPECT_TRUE(printers->Equals(original_printers.get()))
+  EXPECT_EQ(*printers, *original_printers)
       << *printers << ", expected: " << *original_printers;
 }
 
@@ -681,7 +681,7 @@ TEST_F(ExtensionPrinterHandlerTest, GetCapability) {
 
   EXPECT_EQ(1u, call_count);
   ASSERT_TRUE(capability.get());
-  EXPECT_TRUE(capability->Equals(original_capability.get()))
+  EXPECT_EQ(*capability, *original_capability)
       << *capability << ", expected: " << *original_capability;
 }
 
@@ -1012,7 +1012,7 @@ TEST_F(ExtensionPrinterHandlerTest, GrantUsbPrinterAccess) {
 
   EXPECT_EQ(1u, call_count);
   ASSERT_TRUE(printer_info.get());
-  EXPECT_TRUE(printer_info->Equals(original_printer_info.get()))
+  EXPECT_EQ(*printer_info, *original_printer_info)
       << *printer_info << ", expected: " << *original_printer_info;
 }
 
