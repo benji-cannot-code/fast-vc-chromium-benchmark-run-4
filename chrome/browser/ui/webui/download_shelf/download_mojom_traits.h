1f94cc6dbc1ea2d2274389dff48caaf916a88789
Mark Brand <markbrand@google.com>
2022-02-01 15:09:44 +0000

    Fix includes in download_shelf mojom traits.
    Bug: 1291213
    Change-Id: I487bf816dbbe9a24eb99674df4606315c407bce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3427683
    Auto-Submit: Mark Brand <markbrand@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965689}

--- chrome/browser/ui/webui/download_shelf/download_mojom_traits.h
+++ chrome/browser/ui/webui/download_shelf/download_mojom_traits.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_DOWNLOAD_SHELF_DOWNLOAD_MOJOM_TRAITS_H_
 
 #include "chrome/browser/ui/download/download_item_mode.h"
-#include "chrome/browser/ui/webui/download_shelf/download_shelf.mojom.h"
+#include "chrome/browser/ui/webui/download_shelf/download_shelf.mojom-shared.h"
 #include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/download_item.h"
 
