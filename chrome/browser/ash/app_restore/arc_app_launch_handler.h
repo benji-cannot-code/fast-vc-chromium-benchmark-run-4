385e8855b70c6490a92a96364ca870d94f65fb85
Shengsong Tan <sstan@chromium.org>
2022-02-01 05:26:26 +0000

    app_restore: Add func to check if the ARC app existed in any handler
    This CL add a function to check if the ARC app id existed in any
    ArcAppLaunchHandler. It's for prevent launch app conflict with
    App Restore and Desk Template.
    
    BUG=b:211609015
    
    Change-Id: I2f32b1b6e99ff766a27914e3237e429442604b58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426901
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Shengsong Tan <sstan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965537}

--- chrome/browser/ash/app_restore/arc_app_launch_handler.h
+++ chrome/browser/ash/app_restore/arc_app_launch_handler.h
@@ -134,6 +134,8 @@ class ArcAppLaunchHandler
 
   void LaunchApp(const std::string& app_id);
 
+  bool IsAppPendingRestore(const std::string& app_id) const;
+
   // apps::AppRegistryCache::Observer:
   void OnAppUpdate(const apps::AppUpdate& update) override;
   void OnAppRegistryCacheWillBeDestroyed(
