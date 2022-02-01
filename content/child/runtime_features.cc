45334c1e582b299be2f4c10c3b558c9e02dd697d
Francois Beaufort <beaufort.francois@gmail.com>
2022-02-01 11:26:52 +0000

    Disable Web Bluetooth on Android WebView
    This CL makes sure the Web Bluetooth API is not available on Android
    WebView so that developers are not surprised when Web Bluetooth fails to
    show a browser picker for instance.
    
    Bug: 1100993
    Change-Id: I2a1b36ce6a52ebb6f7af83b5f1d2471d0d134d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3416872
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#965627}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -298,6 +298,7 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnableVideoPlaybackQuality, features::kVideoPlaybackQuality},
     {wf::EnableVideoWakeLockOptimisationHiddenMuted,
      media::kWakeLockOptimisationHiddenMuted},
+    {wf::EnableWebBluetooth, features::kWebBluetooth, kSetOnlyIfOverridden},
     {wf::EnableWebBluetoothGetDevices,
      features::kWebBluetoothNewPermissionsBackend, kSetOnlyIfOverridden},
     {wf::EnableWebBluetoothWatchAdvertisements,
