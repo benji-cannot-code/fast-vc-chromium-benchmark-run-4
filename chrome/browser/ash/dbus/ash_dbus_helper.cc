338fbed63ea1a88d3eba198603142ae00e1e72fa
Jason Jeremy Iman <jasongustaman@google.com>
2022-02-01 03:29:29 +0000

    arc: net: Add chromeos::PatchPanelClient
    Chrome needs to get patchpanel managed devices in order to pass
    the correct information to ARC. Implement the client to do so.
    
    BUG=b:185881882
    TEST=Call GetDevices from Chrome
    
    Change-Id: I48cbf43acb51f29ca4013383f11baa719b3c594f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412616
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Jason Jeremy Iman <jasongustaman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965510}

--- chrome/browser/ash/dbus/ash_dbus_helper.cc
+++ chrome/browser/ash/dbus/ash_dbus_helper.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/media_analytics/media_analytics_client.h"
 #include "chromeos/dbus/missive/missive_client.h"
 #include "chromeos/dbus/os_install/os_install_client.h"
+#include "chromeos/dbus/patchpanel/patchpanel_client.h"
 #include "chromeos/dbus/pciguard/pciguard_client.h"
 #include "chromeos/dbus/permission_broker/permission_broker_client.h"
 #include "chromeos/dbus/power/power_manager_client.h"
@@ -135,6 +136,7 @@ void InitializeDBus() {
   InitializeDBusClient<chromeos::MediaAnalyticsClient>(bus);
   InitializeDBusClient<chromeos::MissiveClient>(bus);
   InitializeDBusClient<chromeos::OsInstallClient>(bus);
+  InitializeDBusClient<chromeos::PatchPanelClient>(bus);
   InitializeDBusClient<chromeos::PciguardClient>(bus);
   InitializeDBusClient<chromeos::PermissionBrokerClient>(bus);
   InitializeDBusClient<chromeos::PowerManagerClient>(bus);
@@ -218,6 +220,7 @@ void ShutdownDBus() {
   chromeos::PowerManagerClient::Shutdown();
   chromeos::PermissionBrokerClient::Shutdown();
   chromeos::PciguardClient::Shutdown();
+  chromeos::PatchPanelClient::Shutdown();
   chromeos::OsInstallClient::Shutdown();
   chromeos::MissiveClient::Shutdown();
   chromeos::MediaAnalyticsClient::Shutdown();
