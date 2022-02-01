b9ef403934d90c4be69429db2d12dbc318c5c5e7
Peter McNeeley <petermcneeley@chromium.org>
2022-02-01 20:37:54 +0000

    Add delegated compositing flag in chrome://flags page
    This is simply so users (non-devs) can disable delegated compositing on
    the fly if they run into some specific issue.
    
    
    Bug: 1219029
    Change-Id: Idd0d7cfe772499d0f8e19c4d996fab878bfb6246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428907
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965850}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -449,6 +449,9 @@ extern const char kEnablePreconnectToSearchDescription[];
 extern const char kEnableRawDrawName[];
 extern const char kEnableRawDrawDescription[];
 
+extern const char kEnableDelegatedCompositingName[];
+extern const char kEnableDelegatedCompositingDescription[];
+
 extern const char kEnableRemovingAllThirdPartyCookiesName[];
 extern const char kEnableRemovingAllThirdPartyCookiesDescription[];
 
