97ce361bd4cde22049128f9c5f2f399dd833ddd9
Qingxin Wu <qingxinwu@google.com>
2022-02-01 23:33:02 +0000

    Add forDebuggingOnly reporting APIs.
    The APIs are forDebuggingOnly.reportAdAuctionLoss() and forDebuggingOnly.reportAdAuctionWin(). They can be called in scoreAd() and generateBid(), to provide URLs that are fetched after auction is completed.
    
    Fix: b/206628611
    Change-Id: I506dcd456ee737aeac1fdc9b1e8349c7429ed240
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355196
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#965941}

---
+++ content/test/data/interest_group/decision_logic_with_debugging_report.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function scoreAd(
+    adMetadata, bid, auctionConfig, trustedScoringSignals, browserSignals) {
+  // const adMetadataJSON = JSON.stringify(adMetadata);
+  forDebuggingOnly.reportAdAuctionLoss(
+      auctionConfig.seller + '/echo?seller_debug_report_loss/' + adMetadata);
+  forDebuggingOnly.reportAdAuctionWin(
+      auctionConfig.seller + '/echo?seller_debug_report_win/' + adMetadata);
+  return bid;
+}
+
+function reportResult(auctionConfig, browserSignals) {
+  sendReportTo(auctionConfig.seller + '/echo?report_seller');
+}
