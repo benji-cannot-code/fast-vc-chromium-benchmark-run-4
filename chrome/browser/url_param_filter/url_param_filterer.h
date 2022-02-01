76337592595ecd5e3223f7b605cd48549f39a58a
Matt Reichhoff <mreichhoff@chromium.org>
2022-02-01 16:44:00 +0000

    Add inert url filtering function
    This is an early version of a feature that will become more
    sophisticated over time, with navigations opened in incognito the first
    use case.
    
    Near-future CLs will add:
    -a feature flag that will allow experimentation
    -when the flag is set, applying this protection to the open in
    incognito action, plus associated browsertests
    -plumbing that will allow a set of parameter classifications to be
    communicated and used
    -additional metrics
    
    Bug: 1265734
    Change-Id: I908b8d0a67a85bce7c261d98f4c3565193e16799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3422901
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965727}

---
+++ chrome/browser/url_param_filter/url_param_filterer.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_URL_PARAM_FILTER_URL_PARAM_FILTERER_H_
+#define CHROME_BROWSER_URL_PARAM_FILTER_URL_PARAM_FILTERER_H_
+
+#include <unordered_map>
+#include "chrome/browser/url_param_filter/url_param_filter_classification.pb.h"
+#include "url/gurl.h"
+
+namespace url_param_filter {
+using ClassificationMap =
+    std::unordered_map<std::string, url_param_filter::FilterClassification>;
+
+// Filter the destination URL according to the parameter classifications for the
+// source and destination URLs.
+GURL FilterUrl(const GURL& source_url,
+               const GURL& destination_url,
+               const ClassificationMap& source_classification_map,
+               const ClassificationMap& destination_classification_map);
+
+}  // namespace url_param_filter
+#endif  // CHROME_BROWSER_URL_PARAM_FILTER_URL_PARAM_FILTERER_H_
