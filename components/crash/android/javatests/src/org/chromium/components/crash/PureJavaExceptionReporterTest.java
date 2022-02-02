0e169c3e07acf0f4999fcda709103e36602ff38c
Hazem Ashmawy <hazems@chromium.org>
2022-02-02 22:28:08 +0000

    [AW] Register PureJavaExceptionHandler in nonembedded webview
    PureJavaExceptionHandler was accidentally initialized for all Monochrome
    processes including WebView ones but not for all other WebView flavours
    which resulted in getting crash reports for these processes from
    Monochrome only. This CL explicitly installs the handler for all
    nonembedded WebView processes including Monochrome with the right
    attributes in the crash report.
    
    Now nonebmedded ":webview_apk" and ":webview_service" processes can
    capture pure java exceptions in the processes and create and upload
    crash minidumps for them. Unlike chrome the PureJavaExceptionHandler
    isn't uninstalled when native is loaded because crashpad isn't
    initialized in these nonembedded processes yet.
    
    Bug: 1081916
    Test: Manually throw RuntimeException in DevUI and see that a crash report is created
    Change-Id: I3b20bf26f6c5775ccc55e4161eb76ee0d2fec55c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3361053
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966445}

--- components/crash/android/javatests/src/org/chromium/components/crash/PureJavaExceptionReporterTest.java
+++ components/crash/android/javatests/src/org/chromium/components/crash/PureJavaExceptionReporterTest.java
@@ -29,9 +29,13 @@ public class PureJavaExceptionReporterTest {
     @Rule
     public CrashTestRule mTestRule = new CrashTestRule();
 
-    private static class TestPureJavaExceptionReporter extends PureJavaExceptionReporter {
+    private class TestPureJavaExceptionReporter extends PureJavaExceptionReporter {
         private boolean mReportUploaded;
         private File mMinidump;
+
+        public TestPureJavaExceptionReporter() {
+            super(mTestRule.getCacheDir());
+        }
 
         @Override
         protected String getProductName() {
@@ -42,6 +46,11 @@ public class PureJavaExceptionReporterTest {
         protected void uploadMinidump(File minidump) {
             mMinidump = minidump;
             mReportUploaded = (minidump != null);
+        }
+
+        @Override
+        protected String getMinidumpPrefix() {
+            return "test-minidump-";
         }
 
         public boolean reportUploaded() {
