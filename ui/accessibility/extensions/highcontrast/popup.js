3666311d280d8b2d71f5eda7350dd8459bb7f3e7
Anastasia Helfinstein <anastasi@google.com>
2022-02-02 20:27:26 +0000

    Convert high contrast background code to an ES6 style class
    This change is a pure refactor.
    
    AX-Relnotes: n/a.
    Bug: 1292237
    Test: Manually inspected extension behavior.
    Change-Id: Ic1c345e7bbd48c956293e497af1b31413b1ce3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423334
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#966360}

--- ui/accessibility/extensions/highcontrast/popup.js
+++ ui/accessibility/extensions/highcontrast/popup.js
@@ -58,7 +58,7 @@ class Popup {
     } else {
       document.documentElement.setAttribute('hc', 'a0');
     }
-    chrome.extension.getBackgroundPage().updateTabs();
+    chrome.extension.sendRequest({updateTabs: true});
   }
 
   /** @param {number} value */
