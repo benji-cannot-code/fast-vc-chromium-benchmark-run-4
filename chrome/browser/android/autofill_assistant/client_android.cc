fcb0546e54f6abb9afbd84d79328eabec8164622
Florian Gauger <fga@google.com>
2022-02-02 15:05:30 +0000

    [Autofill Assistant] Remove isCCT function Chrome dependency
    We are in the process of moving all Autofill Assistant code to //components. For this we need to remove all dependencies on Chrome code.
    
    In this cl we move the isCustomTab function to outside the dependency
    object and also pass in a function for when the module is not yet
    available.
    
    Bug: b/173103628
    Change-Id: I67d240e5188960516078059900d6866548c7d0b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3380981
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#966192}

--- chrome/browser/android/autofill_assistant/client_android.cc
+++ chrome/browser/android/autofill_assistant/client_android.cc
@@ -257,7 +257,8 @@ void ClientAndroid::FetchWebsiteActions(
           base::android::JavaParamRef<jobjectArray>(nullptr),
           /* onboarding_shown = */ false,
           /* is_direct_action = */ true,
-          /* jinitial_url = */ nullptr),
+          /* jinitial_url = */ nullptr,
+          /* is_custom_tab = */ dependencies_->IsCustomTab(*GetWebContents())),
       base::BindOnce(&ClientAndroid::OnFetchWebsiteActions,
                      weak_ptr_factory_.GetWeakPtr(), scoped_jcallback));
 }
@@ -366,7 +367,9 @@ bool ClientAndroid::PerformDirectAction(
       base::android::JavaParamRef<jobjectArray>(nullptr),
       /* onboarding_shown = */ false,
       /* is_direct_action = */ true,
-      /* jinitial_url = */ nullptr);
+      /* jinitial_url = */
+      nullptr,
+      /* is_custom_tab = */ dependencies_->IsCustomTab(*GetWebContents()));
 
   int action_index = FindDirectAction(action_name);
   if (action_index == -1)
