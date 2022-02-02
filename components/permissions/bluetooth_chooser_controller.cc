01db23600c3c40296c7888f95946629971324f38
Reilly Grant <reillyg@chromium.org>
2022-02-02 03:34:31 +0000

    Generate crash dumps for BluetoothChooserController::GetOption DCHECKs
    Convert the DCHECKs in this function to conditionals which call
    base::debug::DumpWithoutCrashing() in order to generate more useful
    crash dumps in order to diagnose issue 1292234.
    
    Bug: 1292234
    Change-Id: I37c352e70347ad87f8b555369323ad31024621ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426392
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966037}

--- components/permissions/bluetooth_chooser_controller.cc
+++ components/permissions/bluetooth_chooser_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/check_op.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/strings/utf_string_conversions.h"
@@ -80,12 +81,16 @@ bool BluetoothChooserController::IsPaired(size_t index) const {
 }
 
 std::u16string BluetoothChooserController::GetOption(size_t index) const {
-  DCHECK_LT(index, devices_.size());
+  // Change these back to DCHECKs once https://crbug.com/1292234 is resolved.
+  if (index >= devices_.size())
+    base::debug::DumpWithoutCrashing();
   const std::string& device_id = devices_[index].id;
   const auto& device_name_it = device_id_to_name_map_.find(device_id);
-  DCHECK(device_name_it != device_id_to_name_map_.end());
+  if (device_name_it == device_id_to_name_map_.end())
+    base::debug::DumpWithoutCrashing();
   const auto& it = device_name_counts_.find(device_name_it->second);
-  DCHECK(it != device_name_counts_.end());
+  if (it == device_name_counts_.end())
+    base::debug::DumpWithoutCrashing();
   return it->second == 1
              ? device_name_it->second
              : l10n_util::GetStringFUTF16(
