8fe97459e6267e5f58d80b5132fc55a29eb81595
Jeroen Dhollander <jeroendh@google.com>
2022-02-02 16:09:46 +0000

    Fix calculation of screen origin during CRD
    On ChromeOS the screen origin is relative to the origin of the primary
    display, but this was incorrectly calculated relative to the upper-left
    corner of the display.
    
    Previously this didn't cause issues because CRD also assumed ChromeOS
    was streaming an image of the full unified screen as opposed to the
    primary display ChromeOS was really streaming.
    And because the full unified screen always has an offset of (0,0) CRD
    used this offset which is correct for ChromeOS.
    
    Now with support for multiple monitors in CRD this bug was causing the
    mouse position to be calculated wrong.
    
    Bug: b/208370410
    Test: remoting_unittests
    Test: manually deployed and verified the mouse position is correct.
    Change-Id: Id10bd71765bbdebf0cdc029377ddceae3c7d4909
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3320335
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#966213}

--- remoting/host/desktop_display_info_unittest.cc
+++ remoting/host/desktop_display_info_unittest.cc
@@ -11,6 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
+// Depending on the platform the origin of the desktop is calculated relative to
+// the primary display, or relative to the upper-left of the entire desktop
+// region. See comment at DesktopDisplayInfo::CalcDisplayOffset() for more
+// information.
+#define OS_USES_PRIMARY_DISPLAY_AS_ORIGIN \
+  BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_CHROMEOS)
+
 class DesktopDisplayInfoTest : public testing::Test {
  public:
   void AddDisplay(int x, int y, uint32_t width, uint32_t height) {
@@ -83,13 +90,13 @@ TEST_F(DesktopDisplayInfoTest, DualDisplayLeft) {
   AddDisplay(0, 0, 500, 400);
   AddDisplay(-300, 0, 300, 200);
 
-#if BUILDFLAG(IS_APPLE)
+#if OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
   VerifyDisplayOffset(FROM_HERE, 0, 0, 0);
   VerifyDisplayOffset(FROM_HERE, 1, -300, 0);
 #else
   VerifyDisplayOffset(FROM_HERE, 0, 300, 0);
   VerifyDisplayOffset(FROM_HERE, 1, 0, 0);
-#endif  // BUILDFLAG(IS_APPLE)
+#endif  // OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
 }
 
 // +---------o------------+
@@ -101,13 +108,13 @@ TEST_F(DesktopDisplayInfoTest, DualDisplayLeft_ReverseOrder) {
   AddDisplay(-300, 0, 300, 200);
   AddDisplay(0, 0, 500, 400);
 
-#if BUILDFLAG(IS_APPLE)
+#if OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
   VerifyDisplayOffset(FROM_HERE, 0, -300, 0);
   VerifyDisplayOffset(FROM_HERE, 1, 0, 0);
 #else
   VerifyDisplayOffset(FROM_HERE, 0, 0, 0);
   VerifyDisplayOffset(FROM_HERE, 1, 300, 0);
-#endif  // BUILDFLAG(IS_APPLE)
+#endif  // OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
 }
 
 // +---------o------------+
@@ -120,7 +127,7 @@ TEST_F(DesktopDisplayInfoTest, TripleDisplayMiddle) {
   AddDisplay(0, 0, 500, 400);  // Default display.
   AddDisplay(500, 50, 400, 350);
 
-#if BUILDFLAG(IS_APPLE)
+#if OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
   VerifyDisplayOffset(FROM_HERE, 0, -300, 0);
   VerifyDisplayOffset(FROM_HERE, 1, 0, 0);
   VerifyDisplayOffset(FROM_HERE, 2, 500, 50);
@@ -128,7 +135,7 @@ TEST_F(DesktopDisplayInfoTest, TripleDisplayMiddle) {
   VerifyDisplayOffset(FROM_HERE, 0, 0, 0);
   VerifyDisplayOffset(FROM_HERE, 1, 300, 0);
   VerifyDisplayOffset(FROM_HERE, 2, 800, 50);
-#endif  // BUILDFLAG(IS_APPLE)
+#endif  // OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
 }
 
 //  x         o-----------+            - 0
@@ -145,7 +152,7 @@ TEST_F(DesktopDisplayInfoTest, Multimon3) {
   AddDisplay(300, 400, 600, 450);
   AddDisplay(-300, 350, 300, 200);
 
-#if BUILDFLAG(IS_APPLE)
+#if OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
   VerifyDisplayOffset(FROM_HERE, 0, 0, 0);
   VerifyDisplayOffset(FROM_HERE, 1, 300, 400);
   VerifyDisplayOffset(FROM_HERE, 2, -300, 350);
@@ -153,7 +160,7 @@ TEST_F(DesktopDisplayInfoTest, Multimon3) {
   VerifyDisplayOffset(FROM_HERE, 0, 300, 0);
   VerifyDisplayOffset(FROM_HERE, 1, 600, 400);
   VerifyDisplayOffset(FROM_HERE, 2, 0, 350);
-#endif  // BUILDFLAG(IS_APPLE)
+#endif  // OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
 }
 
 //  x                     +-------+               -- -50
@@ -182,7 +189,7 @@ TEST_F(DesktopDisplayInfoTest, Multimon7) {
   AddDisplay(70, 100, 65, 20);
   AddDisplay(0, 0, 80, 55);  // Default display.
 
-#if BUILDFLAG(IS_APPLE)
+#if OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
   // Relative to display 6.
   VerifyDisplayOffset(FROM_HERE, 0, 80, -10);
   VerifyDisplayOffset(FROM_HERE, 1, 60, -50);
@@ -199,7 +206,7 @@ TEST_F(DesktopDisplayInfoTest, Multimon7) {
   VerifyDisplayOffset(FROM_HERE, 4, 30, 30);
   VerifyDisplayOffset(FROM_HERE, 5, 140, 150);
   VerifyDisplayOffset(FROM_HERE, 6, 70, 50);
-#endif  // BUILDFLAG(IS_APPLE)
+#endif  // OS_USES_PRIMARY_DISPLAY_AS_ORIGIN
 }
 
 }  // namespace remoting
