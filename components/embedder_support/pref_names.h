379022e705684d7566a994545fd9f588083f625b
Nathan Eliason <eliason@chromium.org>
2022-02-02 20:52:43 +0000

    Respect --force-major-version-to-minor in User-Agent utils metadata.
    This change fixes an inconsistency in how --force-major-version-to-minor
    and its associated enterprise policy are applied within user_agent_utils.
    
    This change also introduces a new UserAgentOptions struct that will be
    more widely adopted as part of follow-up crbug.com/1291612. The scope
    of this change is minimized as it must be cherrypicked into M99.
    
    Bug: 1290902
    Change-Id: I3ce2b27cb8ca389611b0a0c5bcbaad0b46397248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3430684
    Reviewed-by: Ali Beyad <abeyad@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Nathan Eliason <eliason@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966373}

--- components/embedder_support/pref_names.h
+++ components/embedder_support/pref_names.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace embedder_support {
 
 extern const char kAlternateErrorPagesEnabled[];
+extern const char kForceMajorVersionToMinorPosition[];
 
 }  // namespace embedder_support
 
