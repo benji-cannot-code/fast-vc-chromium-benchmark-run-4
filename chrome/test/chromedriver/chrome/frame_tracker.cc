9026b049e30417dd7a533335456336c3c6befef5
Adithya Srinivasan <adithyas@chromium.org>
2022-02-02 21:06:12 +0000

    ChromeDriver: Create child WebView for fenced frame targets
    This CL allows ChromeDriver clients to switch to a fenced frame target.
    We have been exposing targets for fenced frames in CDP since
    https://crrev.com/c/3214370.
    
    Bug: chromedriver:4002
    Change-Id: I5b46f0229f534d313f83ecb1c09d40b49c73cfad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3421158
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966388}

--- chrome/test/chromedriver/chrome/frame_tracker.cc
+++ chrome/test/chromedriver/chrome/frame_tracker.cc
@@ -165,7 +165,7 @@ Status FrameTracker::OnEvent(DevToolsClient* client,
     if (!type)
       return Status(kUnknownError,
                     "missing target type in Target.attachedToTarget event");
-    if (*type == "iframe") {
+    if (*type == "iframe" || *type == "page") {
       const std::string* target_id =
           params.FindStringPath("targetInfo.targetId");
       if (!target_id)
