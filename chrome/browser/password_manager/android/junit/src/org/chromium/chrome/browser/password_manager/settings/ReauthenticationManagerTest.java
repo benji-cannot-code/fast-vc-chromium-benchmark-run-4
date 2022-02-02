d7b98eeddd660d0a5cf8e59f112fab1bb91829c8
Ben Joyce <bjoyce@google.com>
2022-02-02 17:36:21 +0000

    Add legacy looper to password_manager_junit_tests
    Needed for robolectric 4.6
    
    Bug: 1195173
    Change-Id: Ic321acc49819e01fdd980399159b39a80938bfca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3430410
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Benjamin Joyce <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966268}

--- chrome/browser/password_manager/android/junit/src/org/chromium/chrome/browser/password_manager/settings/ReauthenticationManagerTest.java
+++ chrome/browser/password_manager/android/junit/src/org/chromium/chrome/browser/password_manager/settings/ReauthenticationManagerTest.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
+import org.robolectric.annotation.LooperMode;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.R;
@@ -32,6 +33,7 @@ import org.chromium.chrome.R;
  */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
+@LooperMode(LooperMode.Mode.LEGACY)
 public class ReauthenticationManagerTest {
     private FragmentManager mFragmentManager;
 
