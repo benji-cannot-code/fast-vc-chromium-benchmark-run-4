d5ea2b5de1f0451e9cbb0fd6d290aea445b4f5b2
sisidovski <sisidovski@google.com>
2022-02-02 22:42:37 +0000

    Add WPT to check if downloads inside of a fenced frame is blocked
    This CL is a follow up for the previous CL crrev.com/c/3312446. This CL
    will add web platform tests to check the behaviour of a fenced frame,
    which blocks download triggered inside of it.
    
    Taking the same approach as iframe sandbox WPT verifying download.
    crrev.com/c/1422667
    
    Bug: 1271406 Change-Id: I0372f6d2cc1f8fba397a2d63ec0fa6a949400215
    Change-Id: I0372f6d2cc1f8fba397a2d63ec0fa6a949400215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403882
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@google.com>
    Cr-Commit-Position: refs/heads/main@{#966452}

---
+++ third_party/blink/web_tests/wpt_internal/fenced_frame/resources/download-helper.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function StreamDownloadFinishDelay() {
+  return 1000;
+}
+
+function DownloadVerifyDelay() {
+  return 1000;
+}
+
+async function VerifyDownload(test_obj, token) {
+  const verifyToken = async (token) => {
+    const url = `resources/download-stash.py?verify-token&token=${token}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('An error happened in the server');
+    }
+    const message = await response.text();
+    return message === 'TOKEN_SET';
+  };
+
+  return new Promise((resolve) => {
+    test_obj.step_wait(
+        async () => {
+          const result = await verifyToken(token);
+          resolve(result);
+        },
+        'Check if the download has finished or not',
+        StreamDownloadFinishDelay() + DownloadVerifyDelay());
+  });
+}
