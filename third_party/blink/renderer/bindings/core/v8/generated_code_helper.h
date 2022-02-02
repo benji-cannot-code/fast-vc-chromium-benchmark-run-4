f4e2db0d716bfe5138a49f11630700acb8d70717
Lei Zhang <thestig@chromium.org>
2022-02-02 20:50:33 +0000

    Reduce includes in Blink V8 binding headers, part 2.
    Avoid including v8_binding.h in generated_code_helper.h, which in turn
    removes v8_binding.h from 2000+ generated files.
    
    Bug: 242216
    Change-Id: I0070f9ca2d694b59d66e4ed19e48013cafaf1c65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428559
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966371}

--- third_party/blink/renderer/bindings/core/v8/generated_code_helper.h
+++ third_party/blink/renderer/bindings/core/v8/generated_code_helper.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/platform/bindings/exception_messages.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
-#include "third_party/blink/renderer/platform/bindings/v8_binding.h"
 #include "v8/include/v8.h"
 
 namespace blink {
@@ -46,15 +45,12 @@ class CORE_EXPORT ExceptionToRejectPromiseScope final {
     if (LIKELY(!exception_state_.HadException()))
       return;
 
-    // As exceptions must always be created in the current realm, reject
-    // promises must also be created in the current realm while regular promises
-    // are created in the relevant realm of the context object.
-    ScriptState* script_state = ScriptState::ForCurrentRealm(info_);
-    V8SetReturnValue(
-        info_, ScriptPromise::Reject(script_state, exception_state_).V8Value());
+    ConvertExceptionToRejectPromise();
   }
 
  private:
+  void ConvertExceptionToRejectPromise();
+
   const v8::FunctionCallbackInfo<v8::Value>& info_;
   ExceptionState& exception_state_;
 };
