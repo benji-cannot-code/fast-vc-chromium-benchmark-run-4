d1aaa27df7ea4b95464b62228f2064785b8baddc
Anders Hartvoll Ruud <andruud@chromium.org>
2022-02-02 11:52:04 +0000

    Abort Element::SetNeedsAnimationStyleRecalc during EnsureComputedStyle
    The animations machinery currently calls Element::
    SetNeedsAnimationStyleRecalc during style resolution, but we early-out
    from that function if Document::InStyleRecalc is true. This does
    however not cover Element::EnsureComputedStyle, which means that
    calling that function can actually mark elements for style recalc.
    
    To address this, let InStyleRecalc be 'true' during
    EnsureComputedStyle.
    
    Fixed: 1290016
    Change-Id: Ic25f2f7f22f3d3960d4ee0726c16d9a7a91f24b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423875
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966140}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -8233,7 +8233,8 @@ void Document::RunPostPrerenderingActivationSteps() {
 
 bool Document::InStyleRecalc() const {
   return lifecycle_.GetState() == DocumentLifecycle::kInStyleRecalc ||
-         style_engine_->InContainerQueryStyleRecalc();
+         style_engine_->InContainerQueryStyleRecalc() ||
+         style_engine_->InEnsureComputedStyle();
 }
 
 void Document::DelayLoadEventUntilLayoutTreeUpdate() {
