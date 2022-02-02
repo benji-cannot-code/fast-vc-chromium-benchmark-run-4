9ae8be81f40d461d491acc93134b3817e15e6143
David Jacobo <djacobo@google.com>
2022-02-02 22:29:07 +0000

    input-overlay:add InputOverlayMenuView resources/entry point.
    This CL adds basic skeleton so we can tryout a new display mode policy
    that will allow views contained within the input overlay feature to
    see and consume events. This CL doesn't touch said policy by itself,
    it just provides an entry point to access a menu for gaming input
    overlay's settings and the corresponding string/icon.
    
    For translation folks, the introduced string is not displayed to the
    user, but tied to an ImageButton (the one with the gamepad icon).
    
    Bug: b:188452744
    Test: install/open archero apk on arc, game.icon must be visible.
    Change-Id: Ia30b4517e3936dbe4b6244e6d5a52bb319f30b81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419340
    Auto-Submit: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966446}

--- chrome/browser/ash/arc/input_overlay/display_overlay_controller.h
+++ chrome/browser/ash/arc/input_overlay/display_overlay_controller.h
@@ -8,8 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/arc/input_overlay/actions/action.h"
 #include "chrome/browser/ash/arc/input_overlay/touch_injector.h"
-#include "ui/views/widget/widget.h"
+#include "ui/gfx/geometry/point.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/views/controls/button/image_button.h"
 #include "ui/views/widget/widget_observer.h"
+
+namespace views {
+class Widget;
+}  // namespace views
 
 namespace arc {
 namespace input_overlay {
@@ -35,12 +41,22 @@ class DisplayOverlayController {
 
   void AddOverlay();
   void RemoveOverlayIfAny();
-  void AddInputMappingView();
+  void AddOverlayChildrenViews();
+  void AddInputMappingView(views::Widget* overlay_widget);
+  void AddInputOverlayMenuView(views::Widget* overlay_widget);
+  void OnMenuAnchorPressed();
   void RemoveInputMappingView();
   views::Widget* GetOverlayWidget();
+  gfx::Point CalculateMenuAnchorPosition();
+  gfx::Rect get_overlay_menu_anchor_bounds() const {
+    return overlay_menu_anchor_->bounds();
+  }
 
   TouchInjector* touch_injector_;
+
+  // References to UI elements owned by the overlay widget.
   InputMappingView* input_mapping_view_ = nullptr;
+  views::ImageButton* overlay_menu_anchor_ = nullptr;
 };
 
 }  // namespace input_overlay
