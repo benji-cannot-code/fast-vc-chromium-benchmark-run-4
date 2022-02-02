0e661c9db9578ae756a4d3f8ca62ecf414859932
Johannes Kron <kron@chromium.org>
2022-02-02 22:16:52 +0000

    Implement WebrtcVideoStatsDB with LevelDB
    The LevelDB implementation supports the production use case of storing
    WebRTC video statistics that are later used to answer WebRTC
    MediaCapabilities queries.
    
    This CL implements the interface and tests. A later CL will instantiate
    the DB as part of the WebrtcVideoPerfHistory service for actual usage.
    
    The CL closely follows the architecture that is used in the implementation
    of VideoDecodeStatsDB.
    
    Bug: chromium:1187565
    Change-Id: I301ad13498855905221085895029623f3361489e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420905
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966436}

--- components/leveldb_proto/public/shared_proto_database_client_list.cc
+++ components/leveldb_proto/public/shared_proto_database_client_list.cc
@@ -109,6 +109,8 @@ std::string SharedProtoDatabaseClientList::ProtoDbTypeToString(
       return "CouponDatabase";
     case ProtoDbType::PAGE_ENTITY_METADATA_STORE:
       return "PageEntityMetadataDatabase";
+    case ProtoDbType::WEBRTC_VIDEO_STATS_DB:
+      return "WebrtcVideoStatsDB";
     case ProtoDbType::LAST:
       NOTREACHED();
       return std::string();
