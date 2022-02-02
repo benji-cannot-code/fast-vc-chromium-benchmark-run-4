6167db5657d2aa6b4af2616d567ba6136c6903a5
Amanda Deacon <amandadeacon@chromium.org>
2022-02-02 00:27:04 +0000

    [Shortcuts] Add skeleton class KeyboardShortcutResult.
    Bug: 1290682
    Change-Id: I230b4071ab48b7f42289915d5447727bb6dfda52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3426741
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965969}

--- ash/public/cpp/app_list/app_list_metrics.h
+++ ash/public/cpp/app_list/app_list_metrics.h
@@ -112,6 +112,8 @@ enum SearchResultType {
   HELP_APP_UPDATES,
   // A Help App result about the "Discover" page.
   HELP_APP_DISCOVER,
+  // A keyboard shortcut result from the Keyboard Shortcut provider.
+  KEYBOARD_SHORTCUT,
   // Boundary is always last.
   SEARCH_RESULT_TYPE_BOUNDARY
 };
