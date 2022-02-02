d5614d2032299518554ec7bab788e8024bb837f9
Vidhan Jain <vidhanj@google.com>
2022-02-02 18:00:28 +0000

    [Code Health] Modernise Value API in */web/favicon/
    Replaces base::ListValue with base::Value as base::ListValue has been
    deprecated.
    
    Bug: 1187001
    Change-Id: I1c3f94fc61b4237f6cac2d7582a3050ab549318b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431285
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#966282}

--- ios/web/favicon/favicon_util.h
+++ ios/web/favicon/favicon_util.h
@@ -6,11 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_WEB_FAVICON_FAVICON_UTIL_H_
 #define IOS_WEB_FAVICON_FAVICON_UTIL_H_
 
+#include "base/values.h"
 #include "ios/web/public/favicon/favicon_url.h"
-
-namespace base {
-class ListValue;
-}  // namespace base
 
 namespace web {
 
@@ -20,7 +17,7 @@ namespace web {
 // structured as containing a list of favicons containing the href, rel and
 // sizes attributes of the favicons. Returns whether the extraction was
 // completely successful or not.
-bool ExtractFaviconURL(const base::ListValue* favicons,
+bool ExtractFaviconURL(const base::Value::ConstListView& favicons,
                        const GURL& page_origin,
                        std::vector<web::FaviconURL>* out_parameter);
 
--- ios/web/favicon/favicon_util.mm
+++ ios/web/favicon/favicon_util.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
-#include "base/values.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -20,11 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
-bool ExtractFaviconURL(const base::ListValue* favicons,
+bool ExtractFaviconURL(const base::Value::ConstListView& favicons,
                        const GURL& page_origin,
                        std::vector<web::FaviconURL>* urls) {
   BOOL has_favicon = NO;
-  for (const base::Value& favicon : favicons->GetList()) {
+  for (const base::Value& favicon : favicons) {
     if (!favicon.is_dict())
       return false;
 
