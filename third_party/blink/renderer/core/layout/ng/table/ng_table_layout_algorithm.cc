075de3cb73bd47205d0aae9e47c53c41082d61f6
Morten Stenshorne <mstensho@chromium.org>
2022-02-02 06:14:31 +0000

    Allow NG tables in multicol if NG table frag is enabled.
    Also add a layout_ng_table_frag virtual testsuite.
    
    Don't let a table trigger legacy layout fallback when
    LayoutNGTableFragmentation is enabled. There's currently no
    fragmentation support in NG tables (they just ignore any fragmentation
    specified on the constraint space), so they are treated as monolithic -
    but that doesn't mean that we cannot allow NG tables inside NG multicol.
    Make the no-break-token DCHECK in the table layout algorithm less
    strict, by allowing break-before break tokens. This keeps tests that
    expect a table to be pushed to the next fragmentainer from crashing; in
    fact, they will even pass, as long as the table doesn't need to fragment
    *inside*.
    
    Also add a table cell hook in the column layout algorithm, so that
    multicol containers displayed as a table-cell will no longer crash.
    There's still work to do here, and some of the existing tests are
    failing because of this.
    
    Bug: 1078927
    Change-Id: I5c431980f4044a7d614b746b2c0094fb7d086121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3427684
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966068}

--- third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/table/ng_table_layout_algorithm.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_disable_side_effects_scope.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_length_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_out_of_flow_layout_part.h"
@@ -453,7 +454,7 @@ LayoutUnit NGTableLayoutAlgorithm::ComputeCaptionBlockSize(
 }
 
 scoped_refptr<const NGLayoutResult> NGTableLayoutAlgorithm::Layout() {
-  DCHECK(!BreakToken());
+  DCHECK(!IsResumingLayout(BreakToken()));
 
   const bool is_fixed_layout = Style().IsFixedTableLayout();
   const LogicalSize border_spacing = Style().TableBorderSpacing();
