69392e11b2bbf517ff3a8d05c40ef5c0c188ca91
David Tseng <dtseng@google.com>
2022-02-02 23:19:40 +0000

    ChromeVox: optimize performance of complex lines
    ChromeVox's EditableLine.speakLine() when instrumented takes ~800MS.
    
    This measurement was taken from one line down action in
    ChromeVoxEditingTest.MoveByWordSuggestions
    
    This is because the function loops over every line node and generates
    speech output -- a costly computation.
    
    Fix this by only processing the current line node for the current
    callstack. Sbusequent nodes are processed via speech end callbacks. This
    gives the user a chance to interrupt the entire chain by stopping speech
    via the control key.
    
    R=josiahk@google.com
    
    Bug: none
    Test: run ChromeVoxEditingTest.MoveByWordSuggestions; verify editing events <= 100MS
    Change-Id: I33280b094a469bd04619383a986bd265e1d7f32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3433600
    Reviewed-by: Josiah Krutz <josiahk@google.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966474}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editing_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editing_test.js
@@ -1690,7 +1690,7 @@ TEST_F('ChromeVoxEditingTest', 'MoveByCharSuggestions', function() {
     await this.focusFirstTextField(root);
 
     mockFeedback.call(this.press(KeyCode.DOWN))
-        .expectSpeech(' typing')
+        .expectSpeech('I ')
         // Move forward through line.
         .call(this.press(KeyCode.RIGHT))
         .expectSpeech(' ')
@@ -1739,7 +1739,7 @@ TEST_F('ChromeVoxEditingTest', 'MoveByWordSuggestions', function() {
     await this.focusFirstTextField(root);
 
     mockFeedback.call(this.press(KeyCode.DOWN))
-        .expectSpeech(' typing')
+        .expectSpeech('I ')
         // Move forward through line.
         .call(this.press(KeyCode.RIGHT, {ctrl: true}))
         .expectSpeech('I')
@@ -1805,7 +1805,7 @@ TEST_F('ChromeVoxEditingTest', 'MoveByWordSuggestionsNoIntents', function() {
     await this.focusFirstTextField(root);
 
     mockFeedback.call(this.press(KeyCode.DOWN))
-        .expectSpeech(' typing')
+        .expectSpeech('I ')
         // Move forward through line.
 
         // This first right arrow is allowed to be processed by the content
