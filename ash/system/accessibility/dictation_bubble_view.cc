b42799b0b5b389a59b485776abea9b571984a561
Akihiro Ota <akihiroota@chromium.org>
2022-02-02 18:41:57 +0000

    Dictation: Bubble UI polish
    This change adjusts spacing and colors to align the bubble UI with the
    UX mocks (attached below):
    
    https://docs.google.com/presentation/d/1Uz_tSJKMJFPjoUTsCBtSjYs0XU3_SFjgHABXAgEelSo/edit#slide=id.gd875d30f9c_0_0
    
    AX-Relnotes: N/A
    Bug: 1252037
    Change-Id: Ib4e41a354fac042bf899cbed04a5d04ab3a21a29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3430275
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966293}

--- ash/system/accessibility/dictation_bubble_view.cc
+++ ash/system/accessibility/dictation_bubble_view.cc
@@ -31,8 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 namespace {
-constexpr int kIconSizeDip = 15;
-constexpr int kSpaceBetweenIconAndTextDip = 10;
+constexpr int kIconSizeDip = 16;
+constexpr int kSpaceBetweenTopRowAndHintViewsDip = 4;
+constexpr int kSpaceBetweenHintLabelsDip = 4;
+constexpr int kSpaceBetweenIconAndTextDip = 4;
 constexpr int kMaxNumHints = 5;
 
 std::unique_ptr<views::ImageView> CreateImageView(
@@ -207,12 +209,16 @@ class ASH_EXPORT HintView : public views::View {
     std::unique_ptr<views::BoxLayout> layout =
         std::make_unique<views::BoxLayout>(
             views::BoxLayout::Orientation::kVertical);
+    layout->set_between_child_spacing(kSpaceBetweenHintLabelsDip);
     SetLayoutManager(std::move(layout));
+
+    SkColor primary = AshColorProvider::Get()->GetContentLayerColor(
+        AshColorProvider::ContentLayerType::kTextColorPrimary);
+    SkColor secondary = AshColorProvider::Get()->GetContentLayerColor(
+        AshColorProvider::ContentLayerType::kTextColorSecondary);
     for (size_t i = 0; i < labels_.size(); ++i) {
-      AddChildView(CreateLabelView(
-          &labels_[i], std::u16string(),
-          AshColorProvider::Get()->GetContentLayerColor(
-              AshColorProvider::ContentLayerType::kTextColorPrimary)));
+      SkColor color = i == 0 ? secondary : primary;
+      AddChildView(CreateLabelView(&labels_[i], std::u16string(), color));
     }
   }
 
@@ -223,8 +229,11 @@ class ASH_EXPORT HintView : public views::View {
   // Updates the text content and visibility of all labels in this view.
   void Update(
       const absl::optional<std::vector<DictationBubbleHintType>>& hints) {
-    if (hints.has_value())
+    int num_visible_hints = 0;
+    if (hints.has_value()) {
       DCHECK(hints.value().size() <= kMaxNumHints);
+      num_visible_hints = hints.value().size();
+    }
 
     for (size_t i = 0; i < labels_.size(); ++i) {
       bool has_hint_for_index = hints.has_value() && (i < hints.value().size());
@@ -236,6 +245,10 @@ class ASH_EXPORT HintView : public views::View {
         labels_[i]->SetText(std::u16string());
       }
     }
+
+    // Set visibility of this view based on the number of visible hints.
+    SetVisible(num_visible_hints > 0 ? true : false);
+
     SizeToPreferredSize();
   }
 
@@ -245,10 +258,12 @@ class ASH_EXPORT HintView : public views::View {
     if (!color_provider)
       return;
 
-    SkColor text_color = color_provider->GetContentLayerColor(
+    SkColor primary = color_provider->GetContentLayerColor(
         AshColorProvider::ContentLayerType::kTextColorPrimary);
+    SkColor secondary = color_provider->GetContentLayerColor(
+        AshColorProvider::ContentLayerType::kTextColorSecondary);
     for (size_t i = 0; i < labels_.size(); ++i) {
-      labels_[i]->SetEnabledColor(text_color);
+      labels_[i]->SetEnabledColor(i == 0 ? secondary : primary);
     }
   }
 
@@ -297,6 +312,7 @@ void DictationBubbleView::OnColorModeChanged(bool dark_mode_enabled) {
 void DictationBubbleView::Init() {
   std::unique_ptr<views::BoxLayout> layout = std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical);
+  layout->set_between_child_spacing(kSpaceBetweenTopRowAndHintViewsDip);
   SetLayoutManager(std::move(layout));
   UseCompactMargins();
 
