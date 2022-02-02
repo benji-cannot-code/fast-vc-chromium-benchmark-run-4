7a4b6eca7c85632b7903ac61a3852ec215f562da
Florian Gauger <fga@google.com>
2022-02-02 14:20:54 +0000

    [Autofill Assistant] Create native dependency object in the starter_android constructor
    We are in the process of moving all Autofill Assistant code to //components. For this we need to remove all dependencies on Chrome code.
    
    In this cl we create the native dependency object when StarterAndroid is
    constructed and before the DFM is loaded.
    
    Bug: b/173103628
    Change-Id: Icc2a56a9f4468b1d30872cff7bd847bd1d12a8ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420466
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Florian Gauger <fga@google.com>
    Cr-Commit-Position: refs/heads/main@{#966180}

--- chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/Starter.java
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/Starter.java
@@ -118,7 +118,8 @@ public class Starter implements AssistantTabObserver, UserData {
             if (mWebContents != null) {
                 // Some dependencies are tied to the web-contents and need to be fetched again.
                 mDependencies = null;
-                mNativeStarter = StarterJni.get().fromWebContents(mWebContents);
+                mNativeStarter =
+                        StarterJni.get().fromWebContents(mWebContents, mStaticDependencies);
                 // Note: This is intentionally split into two methods (fromWebContents, attach).
                 // It ensures that at the time of attach, the native pointer is already set and
                 // this instance is ready to serve requests from native.
@@ -321,7 +322,8 @@ public class Starter implements AssistantTabObserver, UserData {
 
     @NativeMethods
     interface Natives {
-        long fromWebContents(WebContents webContents);
+        long fromWebContents(
+                WebContents webContents, AssistantStaticDependencies staticDependencies);
         void attach(long nativeStarterAndroid, Starter caller);
         void detach(long nativeStarterAndroid, Starter caller);
         void onFeatureModuleInstalled(long nativeStarterAndroid, Starter caller, int result);
