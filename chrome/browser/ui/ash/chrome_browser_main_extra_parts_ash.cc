239446be88a250b68b9df797fc5c46eeb9899140
Hidehiko Abe <hidehiko@chromium.org>
2022-02-02 01:41:22 +0000

    Extract ArcOpenUrlDelegateImpl.
    This is preparation to connect ARC and Lacros.
    Currently, arc::OpenUrlDelegate is implemented by ChromeNewWindowDelegate.
    This CL extracts it into another class ArcOpenUrlDelegateImpl,
    and let it use ChromeNewWindowDelegate.
    
    BUG=1291192
    TEST=Ran on DUT.
    
    Change-Id: I1a448104fa3a73f1a75f6255f39dd9e38c63e7e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420965
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#965999}

--- chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
+++ chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/ash/accessibility/accessibility_controller_client.h"
 #include "chrome/browser/ui/ash/ambient/ambient_client_impl.h"
+#include "chrome/browser/ui/ash/arc_open_url_delegate_impl.h"
 #include "chrome/browser/ui/ash/ash_shell_init.h"
 #include "chrome/browser/ui/ash/ash_web_view_factory_impl.h"
 #include "chrome/browser/ui/ash/cast_config_controller_media_router.h"
@@ -170,6 +171,7 @@ void ChromeBrowserMainExtraPartsAsh::PreProfileInit() {
         std::make_unique<ChromeNewWindowDelegateProvider>(
             std::move(chrome_new_window_client),
             std::move(crosapi_new_window_delegate));
+    arc_open_url_delegate_impl_ = std::make_unique<ArcOpenUrlDelegateImpl>();
   }
 
   ime_controller_client_ = std::make_unique<ImeControllerClientImpl>(
@@ -343,6 +345,7 @@ void ChromeBrowserMainExtraPartsAsh::PostMainMessageLoopRun() {
   session_controller_client_.reset();
   ime_controller_client_.reset();
   in_session_auth_dialog_client_.reset();
+  arc_open_url_delegate_impl_.reset();
   new_window_delegate_provider_.reset();
   accessibility_controller_client_.reset();
   // AppListClientImpl indirectly holds WebContents for answer card and
