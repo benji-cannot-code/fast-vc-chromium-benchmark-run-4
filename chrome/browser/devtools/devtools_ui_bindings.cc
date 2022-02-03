61fa9e8491f2e2ea6875710eab8b7c493ac3db87
Danil Somsikov <dsv@chromium.org>
2022-02-03 07:04:10 +0000

    Disallow devtools extensions access to component extensions.
    Bug: 1115460
    Change-Id: I560bedd9042268a932fe4f28a5c344c218f0865c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3429170
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966617}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/devtools/devtools_file_watcher.h"
 #include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/devtools/url_constants.h"
+#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_avatar_icon_util.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
@@ -77,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_constants.h"
 #include "content/public/common/url_utils.h"
 #include "extensions/browser/extension_registry.h"
+#include "extensions/browser/extension_system.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "google_apis/google_api_keys.h"
@@ -1517,9 +1519,13 @@ void DevToolsUIBindings::AddDevToolsExtensionsToClient() {
     return;
 
   base::ListValue results;
+  base::ListValue component_extension_origins;
   bool have_user_installed_devtools_extensions = false;
   for (const scoped_refptr<const extensions::Extension>& extension :
        registry->enabled_extensions()) {
+    if (extensions::Manifest::IsComponentLocation(extension->location())) {
+      component_extension_origins.Append(extension->origin().Serialize());
+    }
     if (extensions::chrome_manifest_urls::GetDevToolsPage(extension.get())
             .is_empty()) {
       continue;
@@ -1560,6 +1566,8 @@ void DevToolsUIBindings::AddDevToolsExtensionsToClient() {
                               is_developer_mode);
   }
 
+  CallClientMethod("DevToolsAPI", "setOriginsForbiddenForExtensions",
+                   std::move(component_extension_origins));
   CallClientMethod("DevToolsAPI", "addExtensions", std::move(results));
 }
 
