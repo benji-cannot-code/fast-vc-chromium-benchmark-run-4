7b96adddb98cecf4becf41e3ef3215f2819cbbaf
Reilly Grant <reillyg@chromium.org>
2022-02-03 04:06:32 +0000

    Handle parallel requests to acquire a BluetoothAdapter
    Creating and initializing a BluetoothAdapter is an asynchronous
    operation and so multiple calls, like bluetooth.getAvailability(),
    could be made while it is in progress. This change tightens the
    relationship between BluetoothAdapterFactoryWrapper to take advantage of
    the guarantee that while AcquireAdapter() may be called more than once
    ReleaseAdapter() will not. This makes it okay for these duplicate calls
    to be ignored.
    
    Bug: 1233746
    Change-Id: I24772d58c0addb72826a375e5a8b44dcb5eb70b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3430518
    Reviewed-by: Ovidio Ruiz-Henríquez <odejesush@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966581}

--- content/browser/bluetooth/web_bluetooth_service_impl_unittest.cc
+++ content/browser/bluetooth/web_bluetooth_service_impl_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/gmock_callback_support.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
 #include "content/browser/bluetooth/bluetooth_adapter_factory_wrapper.h"
 #include "content/browser/bluetooth/bluetooth_allowed_devices.h"
 #include "content/browser/bluetooth/web_bluetooth_pairing_manager.h"
@@ -40,6 +41,7 @@ namespace content {
 
 namespace {
 
+using ::base::test::TestFuture;
 using ::blink::mojom::WebBluetoothCharacteristicClient;
 using ::blink::mojom::WebBluetoothGATTQueryQuantity;
 using ::blink::mojom::WebBluetoothRemoteGATTCharacteristicPtr;
@@ -497,11 +499,16 @@ class WebBluetoothServiceImplTest : public RenderViewHostImplTestHarness,
     service_ =
         contents()->GetMainFrame()->CreateWebBluetoothServiceForTesting();
 
-    // GetAvailability connects the Web Bluetooth service to the adapter.
-    base::RunLoop run_loop;
-    service_->GetAvailability(base::BindLambdaForTesting(
-        [&run_loop](bool success) { run_loop.Quit(); }));
-    run_loop.Run();
+    // GetAvailability connects the Web Bluetooth service to the adapter. Call
+    // it twice in parallel to exercise what happens when multiple requests to
+    // acquire the BluetoothAdapter are in flight.
+    TestFuture<bool> future_1;
+    TestFuture<bool> future_2;
+    service_->GetAvailability(future_1.GetCallback());
+    service_->GetAvailability(future_2.GetCallback());
+    // Use Wait() instead of Get() because we don't care about the result.
+    EXPECT_TRUE(future_1.Wait());
+    EXPECT_TRUE(future_2.Wait());
   }
 
   void TearDown() override {
