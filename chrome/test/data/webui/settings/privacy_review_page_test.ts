aadaaefcc69d699f2da72dab654fa1f5c00e0586
Sean Harrison <harrisonsean@chromium.org>
2022-02-03 11:49:31 +0000

    [Privacy Guide][WebUI] Add action metics for settings changes
    Add action metrics for users changing their settings inside of Privacy
    guide.
    
    Bug: 1215630
    Change-Id: I47bc89c12a503ae3c6273a3bfbc4c8af9d95d08c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431426
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966684}

--- chrome/test/data/webui/settings/privacy_review_page_test.ts
+++ chrome/test/data/webui/settings/privacy_review_page_test.ts
@@ -348,6 +348,12 @@ suite('PrivacyReviewPage', function() {
       page.shadowRoot!.querySelector('#' + PrivacyReviewStep.MSBB)!.shadowRoot!
           .querySelector<HTMLElement>('#urlCollectionToggle')!.click();
       flush();
+      const actionResult =
+          await testMetricsBrowserProxy.whenCalled('recordAction');
+      assertEquals(
+          actionResult,
+          msbbStartOn ? 'Settings.PrivacyGuide.ChangeMSBBOff' :
+                        'Settings.PrivacyGuide.ChangeMSBBOn');
     }
 
     page.shadowRoot!.querySelector<HTMLElement>('#backButton')!.click();
@@ -379,6 +385,12 @@ suite('PrivacyReviewPage', function() {
       page.shadowRoot!.querySelector('#' + PrivacyReviewStep.HISTORY_SYNC)!
           .shadowRoot!.querySelector<HTMLElement>('#historyToggle')!.click();
       flush();
+      const actionResult =
+          await testMetricsBrowserProxy.whenCalled('recordAction');
+      assertEquals(
+          actionResult,
+          historySyncStartOn ? 'Settings.PrivacyGuide.ChangeHistorySyncOff' :
+                               'Settings.PrivacyGuide.ChangeHistorySyncOn');
     }
 
     page.shadowRoot!.querySelector<HTMLElement>('#nextButton')!.click();
@@ -414,6 +426,13 @@ suite('PrivacyReviewPage', function() {
                   '#safeBrowsingRadioStandard' :
                   '#safeBrowsingRadioEnhanced')!.click();
       flush();
+      const actionResult =
+          await testMetricsBrowserProxy.whenCalled('recordAction');
+      assertEquals(
+          actionResult,
+          safeBrowsingStartsEnhanced ?
+              'Settings.PrivacyGuide.ChangeSafeBrowsingStandard' :
+              'Settings.PrivacyGuide.ChangeSafeBrowsingEnhanced');
     }
 
     page.shadowRoot!.querySelector<HTMLElement>('#nextButton')!.click();
@@ -448,6 +467,13 @@ suite('PrivacyReviewPage', function() {
               cookieStartsBlock3PIncognito ? '#block3P' :
                                              '#block3PIncognito')!.click();
       flush();
+      const actionResult =
+          await testMetricsBrowserProxy.whenCalled('recordAction');
+      assertEquals(
+          actionResult,
+          cookieStartsBlock3PIncognito ?
+              'Settings.PrivacyGuide.ChangeCookiesBlock3P' :
+              'Settings.PrivacyGuide.ChangeCookiesBlock3PIncognito');
     }
 
     page.shadowRoot!.querySelector<HTMLElement>('#nextButton')!.click();
