1d9f3d827b8c4d0fe62503b290d6c8834f6fc822
Dan Sanders <sandersd@chromium.org>
2022-02-03 20:10:10 +0000

    [webcodecs] Schedule blink_perf.webcodecs
    Bug: 1275360
    Change-Id: I0dbb67438dea97f31fd0ec81071105778dd0e1ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428245
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966875}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -724,7 +724,7 @@ class BlinkPerfWebCodecs(_BlinkPerfBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'UNSCHEDULED_blink_perf.webcodecs'
+    return 'blink_perf.webcodecs'
 
 
 @benchmark.Info(
