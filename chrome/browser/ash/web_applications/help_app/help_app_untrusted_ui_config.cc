66a7c4e1be06ac132e10260008ab254dd30861d0
Zufeng Wang <zufeng@google.com>
2022-02-03 23:55:19 +0000

    Help-app: Enable dark/light depending on the chromeos dark/light flag
    Similar to how media app does it in http://crrev/c/3359106
    
    Bug: b:216023140
    Test: Run in Ozone with the flag enabled and open the help app.
    Change-Id: Id2ef25a56b0acb1382f168bdaf6040476987b1dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3421448
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Reviewed-by: dstockwell <dstockwell@google.com>
    Commit-Queue: Zufeng Wang <zufeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#967003}

--- chrome/browser/ash/web_applications/help_app/help_app_untrusted_ui_config.cc
+++ chrome/browser/ash/web_applications/help_app/help_app_untrusted_ui_config.cc
@@ -75,6 +75,8 @@ void PopulateLoadTimeData(content::WebUI* web_ui,
                         "device-help-content-id"));
 
   // Add any features that have been enabled.
+  source->AddBoolean("colorThemes",
+                     chromeos::features::IsDarkLightModeEnabled());
   source->AddBoolean("HelpAppReleaseNotes", true);
   source->AddBoolean(
       "HelpAppLauncherSearch",
