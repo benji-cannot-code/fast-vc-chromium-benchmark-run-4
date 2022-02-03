2f4d888c521dfe8947afb842829d21506074f7c3
Yuki Shiino <yukishiino@chromium.org>
2022-02-03 11:22:30 +0000

    partition_alloc: Move PartitionAlloc into the own namespaces (8 of N)
    Moves the PartitionAlloc APIs and implementations into the own
    namespaces (partition_alloc and partition_alloc::internal).
    
    This patch moves PartitionAllocHooks.
    
    Bug: 1288247
    Change-Id: I27e5ccb996bbd83cb24801233f3c0067f1bb5259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3429359
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966679}

--- base/allocator/partition_allocator/partition_alloc_hooks.cc
+++ base/allocator/partition_allocator/partition_alloc_hooks.cc
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
 #include "base/allocator/partition_allocator/partition_lock.h"
 
-namespace base {
+namespace partition_alloc {
 
 namespace {
 
-internal::PartitionLock g_hook_lock;
+internal::Lock g_hook_lock;
 
-internal::PartitionLock& GetHooksLock() {
+internal::Lock& GetHooksLock() {
   return g_hook_lock;
 }
 
@@ -36,7 +36,7 @@ std::atomic<PartitionAllocHooks::ReallocOverrideHook*>
 
 void PartitionAllocHooks::SetObserverHooks(AllocationObserverHook* alloc_hook,
                                            FreeObserverHook* free_hook) {
-  internal::PartitionAutoLock guard(GetHooksLock());
+  internal::ScopedGuard guard(GetHooksLock());
 
   // Chained hooks are not supported. Registering a non-null hook when a
   // non-null hook is already registered indicates somebody is trying to
@@ -53,7 +53,7 @@ void PartitionAllocHooks::SetObserverHooks(AllocationObserverHook* alloc_hook,
 void PartitionAllocHooks::SetOverrideHooks(AllocationOverrideHook* alloc_hook,
                                            FreeOverrideHook* free_hook,
                                            ReallocOverrideHook realloc_hook) {
-  internal::PartitionAutoLock guard(GetHooksLock());
+  internal::ScopedGuard guard(GetHooksLock());
 
   PA_CHECK((!allocation_override_hook_ && !free_override_hook_ &&
             !realloc_override_hook_) ||
@@ -119,4 +119,4 @@ bool PartitionAllocHooks::ReallocOverrideHookIfEnabled(size_t* out,
   return false;
 }
 
-}  // namespace base
+}  // namespace partition_alloc
