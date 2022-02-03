0015b4fa4dadb89f86bdb8bee76b1141b7a726e0
Alexander Dunaev <adunaev@igalia.com>
2022-02-03 20:57:51 +0000

    [linux/wayland] Fix crash on middle click on the tab bar.
    On Linux, depending on the environment, the browser may request the
    window manager to lower the window in the stack of windows.  That logic
    had an error that resulted in nullptr dereference on Wayland.
    
    This patch fixes the issue.
    
    Bug: 1293080
    Change-Id: Ic1961042d90262d18762e89e062f3004980ba7ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436007
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#966900}

--- ui/views/widget/desktop_aura/window_event_filter_linux.cc
+++ ui/views/widget/desktop_aura/window_event_filter_linux.cc
@@ -172,7 +172,7 @@ void WindowEventFilterLinux::ToggleMaximizedState() {
 }
 
 void WindowEventFilterLinux::LowerWindow() {
-  desktop_window_tree_host_->LowerXWindow();
+  desktop_window_tree_host_->LowerWindow();
 }
 
 void WindowEventFilterLinux::MaybeDispatchHostWindowDragMovement(
