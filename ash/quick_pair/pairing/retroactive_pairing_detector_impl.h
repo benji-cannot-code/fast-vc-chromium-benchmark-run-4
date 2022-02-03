48d42aedea5088dcfb683bab83d53608bbec90b4
Juliet Levesque <julietlevesque@google.com>
2022-02-03 01:54:11 +0000

    [Fast Pair] Instantiate RetroactivePairing for delayed log in
    Instantiates the RetroactivePairing scenario when there has been a
    delayed log in event from the SessionController to properly enable
    the scenario for logged in users.
    
    Fixed: b/207589802
    Change-Id: I7ffa0ff1ba58f8cbd02984acca41aa0c3d77eb49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3432689
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#966495}

--- ash/quick_pair/pairing/retroactive_pairing_detector_impl.h
+++ ash/quick_pair/pairing/retroactive_pairing_detector_impl.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/quick_pair/pairing/retroactive_pairing_detector.h"
 
+#include "ash/public/cpp/session/session_controller.h"
+#include "ash/public/cpp/session/session_observer.h"
 #include "ash/quick_pair/common/account_key_failure.h"
 #include "ash/quick_pair/common/pair_failure.h"
 #include "ash/quick_pair/common/protocol.h"
@@ -40,7 +42,8 @@ class RetroactivePairingDetectorImpl final
       public device::BluetoothAdapter::Observer,
       public PairerBroker::Observer,
       public MessageStreamLookup::Observer,
-      public MessageStream::Observer {
+      public MessageStream::Observer,
+      public SessionObserver {
  public:
   RetroactivePairingDetectorImpl(PairerBroker* pairer_broker,
                                  MessageStreamLookup* message_stream_lookup);
@@ -68,6 +71,9 @@ class RetroactivePairingDetectorImpl final
     std::string model_id;
     std::string ble_address;
   };
+
+  // SessionObserver:
+  void OnLoginStatusChanged(LoginStatus login_status) override;
 
   // device::BluetoothAdapter::Observer
   void DevicePairedChanged(device::BluetoothAdapter* adapter,
@@ -132,10 +138,18 @@ class RetroactivePairingDetectorImpl final
   base::flat_map<std::string, RetroactivePairingInformation>
       device_pairing_information_;
 
+  // Helps us keep track of whether the RetroactivePairingDetector has already
+  // been instantiated when we get a logged-in event from the SessionObserver
+  // so we can determine if we need to instantiate the objects.
+  bool retroactive_pairing_detector_instatiated_ = false;
+
+  PairerBroker* pairer_broker_ = nullptr;
   MessageStreamLookup* message_stream_lookup_ = nullptr;
   scoped_refptr<device::BluetoothAdapter> adapter_;
   base::ObserverList<RetroactivePairingDetector::Observer> observers_;
 
+  base::ScopedObservation<SessionController, SessionObserver>
+      shell_observation_{this};
   base::ScopedObservation<device::BluetoothAdapter,
                           device::BluetoothAdapter::Observer>
       adapter_observation_{this};
