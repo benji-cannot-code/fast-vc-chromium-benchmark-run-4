135e5c1091f770cf9869531c1cf9a104e775478d
Devlin Cronin <rdevlin.cronin@chromium.org>
2022-02-03 02:23:57 +0000

    [Extensions UI] Small popup trigger cleanups
    Add a few quick cleanups to the extensions code for triggering popups:
    - We don't need `grant_tab_permissions` in ShowPopup()
    - The popup URL can be determined in TriggerPopup(), instead of
      ExecuteAction()
    - Creating an ExtensionHost for the popup should never fail, so
      TriggerPopup doesn't need to return a boolean
    
    This CL should have no behavior change.
    
    Bug: None
    Change-Id: I73697c8a7eec6c1d21d53f3b6764d4a8461ca744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3433601
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966543}

--- chrome/browser/ui/extensions/extension_action_view_controller.h
+++ chrome/browser/ui/extensions/extension_action_view_controller.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 class ExtensionActionPlatformDelegate;
-class GURL;
 class IconWithBadgeImageSource;
 class ExtensionsContainer;
 
@@ -148,16 +147,15 @@ class ExtensionActionViewController
   // e.g. an API). Returns true if a popup is shown.
   bool ExecuteAction(PopupShowAction show_action, bool grant_tab_permissions);
 
-  // Begins the process of showing the popup for the extension action, given the
-  // associated |popup_url|. |grant_tab_permissions| is true if active tab
+  // Begins the process of showing the popup for the extension action over the
+  // given |web_contents|. |grant_tab_permissions| is true if active tab
   // permissions should be given to the extension; this is only true if the
   // popup is opened through a user action.
   // The popup may not be shown synchronously if the extension is hidden and
   // first needs to slide itself out.
-  // Returns true if a popup will be shown.
-  bool TriggerPopupWithUrl(PopupShowAction show_action,
-                           const GURL& popup_url,
-                           bool grant_tab_permissions);
+  void TriggerPopup(PopupShowAction show_action,
+                    content::WebContents* web_contents,
+                    bool grant_tab_permissions);
 
   // Shows the popup with the given |host|.
   void ShowPopup(std::unique_ptr<extensions::ExtensionViewHost> host,
