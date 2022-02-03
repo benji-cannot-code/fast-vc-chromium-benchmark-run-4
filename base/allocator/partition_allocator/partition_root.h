6eb1deaf223d4ff452e15ca0018eaa7fc4b12814
Yuki Shiino <yukishiino@chromium.org>
2022-02-03 12:38:58 +0000

    partition_alloc: Move PartitionAlloc into the own namespaces (11 of N)
    Moves the PartitionAlloc APIs and implementations into the own
    namespaces (partition_alloc and partition_alloc::internal).
    
    This patch moves partition_alloc-inl.h.
    
    Bug: 1288247
    Change-Id: Ia1630ec9c9e0fb6af434a8395baf0d4e1cb34820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431926
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966698}

--- base/allocator/partition_allocator/partition_root.h
+++ base/allocator/partition_allocator/partition_root.h
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/partition_alloc_hooks.h"
 #include "base/allocator/partition_allocator/partition_alloc_notreached.h"
 #include "base/allocator/partition_allocator/partition_bucket_lookup.h"
+#include "base/allocator/partition_allocator/partition_cookie.h"
 #include "base/allocator/partition_allocator/partition_direct_map_extent.h"
 #include "base/allocator/partition_allocator/partition_freelist_entry.h"
 #include "base/allocator/partition_allocator/partition_lock.h"
