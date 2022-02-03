4816262348bd163e0ec4072f5dc980b9160553ff
amraboelkher <amraboelkher@chromium.org>
2022-02-03 15:21:02 +0000

    Fix ignored return type qualifier warning
    shell-encryption and private_membership leaked some configs
    which disable some default warnings.
    
    In order to re-enable all these warnings for chrome/browser/*
    in this CL crrev.com/c/3303510, all warnings need to be fixed.
    
    This CL is removing the const type qualifier that has no effect
    on the return type. The exact warning error:
    
    warning: 'const' type qualifier on return type has no effect.
    
    This CL was uploaded by git cl split.
    
    R=afakhry@chromium.org
    
    Bug: chromium:1212837, b:201763017
    Change-Id: I6a29a75ed4f9b4f153a731e77021f2888417599c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3427502
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966734}

--- chrome/browser/ui/ash/projector/projector_soda_installation_controller_unittest.cc
+++ chrome/browser/ui/ash/projector/projector_soda_installation_controller_unittest.cc
@@ -29,7 +29,7 @@ inline const std::string& GetLocale() {
   return g_browser_process->GetApplicationLocale();
 }
 
-inline const void SetLocale(const std::string& locale) {
+inline void SetLocale(const std::string& locale) {
   g_browser_process->SetApplicationLocale(locale);
 }
 
