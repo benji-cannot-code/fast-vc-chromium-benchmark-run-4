2f304d1c284fa31e398f4a9cd782708fde0b0a8c
Dan Harrington <harringtond@chromium.org>
2022-02-03 15:43:02 +0000

    TaskQueue: optionally track task add location
    This change adds an optional base::Location parameter to
    AddTask(), so that the source location that added
    the task can be tracked, and displayed for testing.
    
    This is helpful for debugging tests.
    
    Also, while making this change, I removed some dead code.
    And, previously, a task remained in the kSuspended state
    after it called Resume(). I've added a new kPendingResume
     state, which prevents some bad usage (Calling
    TaskComplete() or Resume() while in this state)
    through DCHECKs.
    
    Change-Id: Ibae7d25a30dd5c3065decff1a2c5db539154757c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3424811
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966741}

--- components/feed/core/v2/web_feed_subscription_coordinator.cc
+++ components/feed/core/v2/web_feed_subscription_coordinator.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_helpers.h"
 #include "base/feature_list.h"
+#include "base/location.h"
 #include "base/memory/raw_ptr.h"
 #include "base/task/post_task.h"
 #include "base/threading/sequenced_task_runner_handle.h"
@@ -274,11 +275,13 @@ void WebFeedSubscriptionCoordinator::FollowWebFeedFromUrlStart(
 
   SubscribeToWebFeedTask::Request request;
   request.page_info = page_info;
-  feed_stream_->GetTaskQueue().AddTask(std::make_unique<SubscribeToWebFeedTask>(
-      feed_stream_, std::move(request),
-      base::BindOnce(&WebFeedSubscriptionCoordinator::FollowWebFeedComplete,
-                     base::Unretained(this), std::move(callback),
-                     /*followed_with_id=*/false)));
+  feed_stream_->GetTaskQueue().AddTask(
+      FROM_HERE,
+      std::make_unique<SubscribeToWebFeedTask>(
+          feed_stream_, std::move(request),
+          base::BindOnce(&WebFeedSubscriptionCoordinator::FollowWebFeedComplete,
+                         base::Unretained(this), std::move(callback),
+                         /*followed_with_id=*/false)));
 }
 
 void WebFeedSubscriptionCoordinator::FollowWebFeed(
@@ -301,11 +304,13 @@ void WebFeedSubscriptionCoordinator::FollowWebFeedFromIdStart(
   SubscribeToWebFeedTask::Request request;
   request.web_feed_id = web_feed_id;
 
-  feed_stream_->GetTaskQueue().AddTask(std::make_unique<SubscribeToWebFeedTask>(
-      feed_stream_, std::move(request),
-      base::BindOnce(&WebFeedSubscriptionCoordinator::FollowWebFeedComplete,
-                     base::Unretained(this), std::move(callback),
-                     /*followed_with_id=*/true)));
+  feed_stream_->GetTaskQueue().AddTask(
+      FROM_HERE,
+      std::make_unique<SubscribeToWebFeedTask>(
+          feed_stream_, std::move(request),
+          base::BindOnce(&WebFeedSubscriptionCoordinator::FollowWebFeedComplete,
+                         base::Unretained(this), std::move(callback),
+                         /*followed_with_id=*/true)));
 }
 
 void WebFeedSubscriptionCoordinator::FollowWebFeedComplete(
@@ -352,6 +357,7 @@ void WebFeedSubscriptionCoordinator::UnfollowWebFeedStart(
                             : absl::nullopt);
 
   feed_stream_->GetTaskQueue().AddTask(
+      FROM_HERE,
       std::make_unique<UnsubscribeFromWebFeedTask>(
           feed_stream_, web_feed_id,
           base::BindOnce(
@@ -653,6 +659,7 @@ void WebFeedSubscriptionCoordinator::FetchRecommendedWebFeedsStart() {
     return;
   fetching_recommended_web_feeds_ = true;
   feed_stream_->GetTaskQueue().AddTask(
+      FROM_HERE,
       std::make_unique<FetchRecommendedWebFeedsTask>(
           feed_stream_,
           base::BindOnce(
@@ -727,6 +734,7 @@ void WebFeedSubscriptionCoordinator::FetchSubscribedWebFeedsStart() {
   }
   fetching_subscribed_web_feeds_ = true;
   feed_stream_->GetTaskQueue().AddTask(
+      FROM_HERE,
       std::make_unique<FetchSubscribedWebFeedsTask>(
           feed_stream_,
           base::BindOnce(
