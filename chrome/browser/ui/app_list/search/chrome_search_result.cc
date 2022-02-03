26f33fb11128ee33fb8a34f6015161366bb0fc77
Thanh Nguyen <thanhdng@chromium.org>
2022-02-03 05:03:53 +0000

    Fix private variables style
    Bug: 1256949
    Change-Id: Ia66fda8e1f79ac58fdd4f9a7374c8215d78a411f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3435525
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Auto-Submit: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966597}

--- chrome/browser/ui/app_list/search/chrome_search_result.cc
+++ chrome/browser/ui/app_list/search/chrome_search_result.cc
@@ -46,7 +46,7 @@ void ChromeSearchResult::SetTitleTags(const Tags& tags) {
 
 void ChromeSearchResult::MaybeUpdateTitleVector() {
   // Create and setup title tags if not set explicitly.
-  if (!explicit_title_vector) {
+  if (!explicit_title_vector_) {
     std::vector<TextItem> text_vector;
     TextItem text_item(ash::SearchResultTextItemType::kString);
     text_item.SetText(metadata_->title);
@@ -70,7 +70,7 @@ void ChromeSearchResult::SetDetailsTags(const Tags& tags) {
 
 void ChromeSearchResult::MaybeUpdateDetailsVector() {
   // Create and setup details tags if not set explicitly.
-  if (!explicit_details_vector) {
+  if (!explicit_details_vector_) {
     std::vector<TextItem> text_vector;
     TextItem text_item(ash::SearchResultTextItemType::kString);
     text_item.SetText(metadata_->details);
@@ -82,13 +82,13 @@ void ChromeSearchResult::MaybeUpdateDetailsVector() {
 
 void ChromeSearchResult::SetTitleTextVector(const TextVector& text_vector) {
   metadata_->title_vector = text_vector;
-  explicit_title_vector = true;
+  explicit_title_vector_ = true;
   SetSearchResultMetadata();
 }
 
 void ChromeSearchResult::SetDetailsTextVector(const TextVector& text_vector) {
   metadata_->details_vector = text_vector;
-  explicit_details_vector = true;
+  explicit_details_vector_ = true;
   SetSearchResultMetadata();
 }
 
