e58a45d40cbee1240556f255daafd1f6d3d10c12
Sharon Yang <yangsharon@chromium.org>
2022-02-03 23:47:19 +0000

    [page load metrics] Remove WebContents::GetURL calls in //chrome
    WebContents::GetURL is deprecated. Replace it with
    WebContents::GetLastCommittedURL
    
    Bug: 237908
    Change-Id: I82acf396654459a578bb67b5a768f2c74c8b99d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3355473
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966999}

--- chrome/browser/page_load_metrics/observers/https_engagement_metrics/https_engagement_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/https_engagement_metrics/https_engagement_page_load_metrics_observer_browsertest.cc
@@ -97,8 +97,9 @@ class HttpsEngagementPageLoadMetricsBrowserTest : public InProcessBrowserTest {
     // Make sure the correct tab is in the foreground.
     TabStripModel* tab_strip_model = browser()->tab_strip_model();
     EXPECT_EQ(2, tab_strip_model->count());
-    EXPECT_EQ(url, tab_strip_model->GetWebContentsAt(0)->GetURL());
-    EXPECT_NE(url, tab_strip_model->GetActiveWebContents()->GetURL());
+    EXPECT_EQ(url, tab_strip_model->GetWebContentsAt(0)->GetLastCommittedURL());
+    EXPECT_NE(url,
+              tab_strip_model->GetActiveWebContents()->GetLastCommittedURL());
 
     content::WebContentsDestroyedWatcher destroyed_watcher(
         tab_strip_model->GetWebContentsAt(0));
@@ -120,8 +121,9 @@ class HttpsEngagementPageLoadMetricsBrowserTest : public InProcessBrowserTest {
     // Make sure the correct tab is in the foreground.
     TabStripModel* tab_strip_model = browser()->tab_strip_model();
     EXPECT_EQ(2, tab_strip_model->count());
-    EXPECT_EQ(url, tab_strip_model->GetWebContentsAt(1)->GetURL());
-    EXPECT_NE(url, tab_strip_model->GetActiveWebContents()->GetURL());
+    EXPECT_EQ(url, tab_strip_model->GetWebContentsAt(1)->GetLastCommittedURL());
+    EXPECT_NE(url,
+              tab_strip_model->GetActiveWebContents()->GetLastCommittedURL());
 
     content::WebContentsDestroyedWatcher destroyed_watcher(
         tab_strip_model->GetWebContentsAt(1));
@@ -142,8 +144,9 @@ class HttpsEngagementPageLoadMetricsBrowserTest : public InProcessBrowserTest {
     // Make sure the correct tab is in the foreground.
     TabStripModel* tab_strip_model = browser()->tab_strip_model();
     EXPECT_EQ(2, tab_strip_model->count());
-    EXPECT_EQ(url, tab_strip_model->GetWebContentsAt(1)->GetURL());
-    EXPECT_NE(url, tab_strip_model->GetActiveWebContents()->GetURL());
+    EXPECT_EQ(url, tab_strip_model->GetWebContentsAt(1)->GetLastCommittedURL());
+    EXPECT_NE(url,
+              tab_strip_model->GetActiveWebContents()->GetLastCommittedURL());
 
     // Close the foreground tab.
     base::TimeTicks start = base::TimeTicks::Now();
@@ -154,7 +157,8 @@ class HttpsEngagementPageLoadMetricsBrowserTest : public InProcessBrowserTest {
 
     // Now the background tab should have moved to the foreground.
     EXPECT_EQ(1, tab_strip_model->count());
-    EXPECT_EQ(url, tab_strip_model->GetActiveWebContents()->GetURL());
+    EXPECT_EQ(url,
+              tab_strip_model->GetActiveWebContents()->GetLastCommittedURL());
 
     content::WebContentsDestroyedWatcher second_watcher(
         tab_strip_model->GetActiveWebContents());
--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -2305,7 +2305,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestorePageLoadMetricsBrowserTest,
         WindowOpenDisposition::CURRENT_TAB);
     ASSERT_EQ(1, browser()->tab_strip_model()->count());
     ASSERT_TRUE(tab_contents);
-    ASSERT_EQ(GetTestURL(), tab_contents->GetURL());
+    ASSERT_EQ(GetTestURL(), tab_contents->GetLastCommittedURL());
 
     session_restore_paint_waiter.WaitForForegroundTabs(1);
     ExpectFirstPaintMetricsTotalCount(1);
@@ -2320,7 +2320,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestorePageLoadMetricsBrowserTest,
     ASSERT_EQ(2, browser()->tab_strip_model()->count());
     ASSERT_EQ(1, browser()->tab_strip_model()->active_index());
     ASSERT_TRUE(tab_contents);
-    ASSERT_EQ(GetTestURL(), tab_contents->GetURL());
+    ASSERT_EQ(GetTestURL(), tab_contents->GetLastCommittedURL());
 
     session_restore_paint_waiter.WaitForForegroundTabs(1);
     ExpectFirstPaintMetricsTotalCount(2);
@@ -2334,7 +2334,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestorePageLoadMetricsBrowserTest,
     ASSERT_EQ(3, browser()->tab_strip_model()->count());
     ASSERT_EQ(1, browser()->tab_strip_model()->active_index());
     ASSERT_TRUE(tab_contents);
-    ASSERT_EQ(GetTestURL(), tab_contents->GetURL());
+    ASSERT_EQ(GetTestURL(), tab_contents->GetLastCommittedURL());
     ASSERT_NO_FATAL_FAILURE(WaitForTabsToLoad(browser()));
 
     // Do not record timings of initially background tabs.
