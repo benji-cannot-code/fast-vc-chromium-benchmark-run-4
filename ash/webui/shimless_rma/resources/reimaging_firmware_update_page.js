8c7e4f5f7114c56ed0f1f70a5c93296e85f22ed9
Gavin Williams <gavinwill@chromium.org>
2022-02-03 20:58:31 +0000

    Shimless: Show USB firmware update warning
    - Show warning icon in kFileNotFound state
    - Move warning fill color from SVG definition to CSS file
    - Update warning icon class in critical-error-page
    
    screenshot: http://screen/5pkEimv2Tb7KmG8
    
    Bug: 1198187
    Change-Id: I39743d7cfe7f47fa231aea725eecd9e5597752e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3430511
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966901}

--- ash/webui/shimless_rma/resources/reimaging_firmware_update_page.js
+++ ash/webui/shimless_rma/resources/reimaging_firmware_update_page.js
@@ -79,6 +79,12 @@ export class UpdateRoFirmwarePage extends UpdateRoFirmwarePageBase {
         type: Boolean,
         value: false,
       },
+
+      /** @protected {boolean} */
+      shouldShowWarning_: {
+        type: Boolean,
+        value: false,
+      },
     };
   }
 
@@ -106,6 +112,8 @@ export class UpdateRoFirmwarePage extends UpdateRoFirmwarePageBase {
   onUpdateRoFirmwareStatusChanged(status) {
     this.status_ = status;
     this.shouldShowSpinner_ = this.status_ === UpdateRoFirmwareStatus.kUpdating;
+    this.shouldShowWarning_ =
+        this.status_ === UpdateRoFirmwareStatus.kFileNotFound;
 
     const disabled = this.status_ != UpdateRoFirmwareStatus.kComplete;
     this.dispatchEvent(new CustomEvent(
