b4adf2ac2b45dd097c6fbc354ff415bbf596c34e
Victor Vianna <victorvianna@google.com>
2022-02-03 14:57:31 +0000

    Revert "Re-run default web app migration if old Chrome app is still installed"
    This reverts commit 8527009fc4034884a55d2c48b2fc4383a441cc95.
    
    Reason for revert: Causes breakages on linux-chromeos-chrome, see crbug.com/1293806.
    
    Original change's description:
    > Re-run default web app migration if old Chrome app is still installed
    >
    > During the default web app migration away from Chrome apps a number
    > of users experienced app duplication with the old Chrome app returning
    > after migration. https://crbug.com/1266234
    >
    > This CL adds a PreinstalledWebAppDuplicationFixer that listens for
    > the old Chrome apps to be replaced and clears any migration prefs
    > to enable the migration to run again.
    >
    > A separate effort will prevent these Chrome apps from getting
    > reinstalled after migration: https://crbug.com/1290718
    >
    > Bug: 1290716
    > Change-Id: I54aac86a34a16e8d303d778eed1f5fbc8191fbcf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419422
    > Reviewed-by: Glen Robertson <glenrob@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Alan Cutter <alancutter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#966480}
    
    Bug: 1290716
    Bug: 1293806
    Change-Id: I58c3cf82167adbfe29a10a192a86c5ec903a0ae0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436587
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#966726}

--- chrome/browser/web_applications/adjustments/web_app_adjustments.h
+++ chrome/browser/web_applications/adjustments/web_app_adjustments.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "chrome/browser/web_applications/adjustments/link_capturing_pref_migration.h"
-#include "chrome/browser/web_applications/adjustments/preinstalled_web_app_duplication_fixer.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 
@@ -26,22 +25,12 @@ namespace web_app {
 // Everything in here should have a removal date.
 class WebAppAdjustments : public KeyedService {
  public:
-  static WebAppAdjustments* Get(Profile* profile);
-
   explicit WebAppAdjustments(Profile* profile);
   ~WebAppAdjustments() override;
-
-  PreinstalledWebAppDuplicationFixer* preinstalled_web_app_duplication_fixer() {
-    return preinstalled_web_app_duplication_fixer_.get();
-  }
 
  private:
   // TODO(crbug.com/1262906): This was added in M97, remove in M107.
   std::unique_ptr<LinkCapturingPrefMigration> link_capturing_pref_migration_;
-
-  // TODO(crbug.com/1290716): This was added in M100, remove in M120.
-  std::unique_ptr<PreinstalledWebAppDuplicationFixer>
-      preinstalled_web_app_duplication_fixer_;
 };
 
 class WebAppAdjustmentsFactory : public BrowserContextKeyedServiceFactory {
@@ -52,8 +41,6 @@ class WebAppAdjustmentsFactory : public BrowserContextKeyedServiceFactory {
   static WebAppAdjustmentsFactory* GetInstance();
 
  private:
-  friend class WebAppAdjustments;
-
   // BrowserContextKeyedServiceFactory:
   KeyedService* BuildServiceInstanceFor(
       content::BrowserContext* context) const override;
--- chrome/browser/web_applications/externally_installed_web_app_prefs.h
+++ chrome/browser/web_applications/externally_installed_web_app_prefs.h
@@ -57,7 +57,6 @@ class ExternallyInstalledWebAppPrefs {
   void Insert(const GURL& url,
               const AppId& app_id,
               ExternalInstallSource install_source);
-  bool Remove(const GURL& url);
   absl::optional<AppId> LookupAppId(const GURL& url) const;
   bool HasNoApps() const;
 
