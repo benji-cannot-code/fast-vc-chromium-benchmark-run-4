ceca58b6ef72900c5bdd8f100b84f758693a90ab
François Beaufort <beaufort.francois@gmail.com>
2022-02-03 14:43:21 +0000

    Reenable WebViewLayoutTest#testWebViewExcludedInterfaces
    This CL reenables the WebViewLayoutTest#testWebViewExcludedInterfaces
    after it was disabled. It does so by taking into account blink platform
    specific features. It was not the case before causing Bluetooth related
    features to not be in expected blink features.
    
    Bug: 1293089
    Change-Id: I7c1db86a30c4c1ea399182770c017a1517f3e187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431446
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#966720}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -147,7 +147,6 @@ public class WebViewLayoutTest {
         }
     }
 
-    @DisabledTest(message = "https://crbug.com/1293089")
     @Test
     @MediumTest
     public void testWebViewExcludedInterfaces() throws Exception {
@@ -156,6 +155,8 @@ public class WebViewLayoutTest {
                 + "webexposed/global-interface-listing.html", mTestActivity);
         String blinkExpected = readFile(PATH_BLINK_PREFIX
                 + "webexposed/global-interface-listing-expected.txt");
+        String blinkPlatformSpecificExpected = readFile(PATH_BLINK_PREFIX
+                + "webexposed/global-interface-listing-platform-specific-expected.txt");
         String webviewExcluded = readFile(PATH_WEBVIEW_PREFIX
                 + "webexposed/not-webview-exposed.txt");
         mTestActivity.waitForFinish(TIMEOUT_SECONDS, TimeUnit.SECONDS);
@@ -165,6 +166,8 @@ public class WebViewLayoutTest {
                 buildHashMap(webviewExcluded);
         HashMap<String, HashSet<String>> webviewInterfacesMap = buildHashMap(result);
         HashMap<String, HashSet<String>> blinkInterfacesMap = buildHashMap(blinkExpected);
+        blinkInterfacesMap = buildHashMap(blinkPlatformSpecificExpected, blinkInterfacesMap);
+
         StringBuilder unexpected = new StringBuilder();
 
         // Check that each excluded interface and its properties are present in blinkInterfaceMap
@@ -438,10 +441,16 @@ public class WebViewLayoutTest {
     }
 
     private HashMap<String, HashSet<String>> buildHashMap(String contents) {
+        HashMap<String, HashSet<String>> interfaces = new HashMap<String, HashSet<String>>();
+
+        return buildHashMap(contents, interfaces);
+    }
+
+    private HashMap<String, HashSet<String>> buildHashMap(
+            String contents, HashMap<String, HashSet<String>> interfaces) {
         String[] lineByLine = contents.split("\\n");
 
         HashSet<String> subset = null;
-        HashMap<String, HashSet<String>> interfaces = new HashMap<String, HashSet<String>>();
         for (String line : lineByLine) {
             String s = trimAndRemoveComments(line);
             if (isInterfaceOrGlobalObject(s)) {
