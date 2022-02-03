cd8680edbd1badc7985303a9d591e09d00bbe8f0
Yongshun Liu <yongshun@chromium.org>
2022-02-03 23:28:40 +0000

    desk_templates: Update the icon of delete template button
    Updates the icon of delete template button from x to trash.
    
    Test: manual
    Bug: 1280799
    Change-Id: I2ebee39a1a5c53d9ff07853d104f95b547e714f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3437349
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966990}

--- ash/wm/desks/templates/desks_templates_item_view.cc
+++ ash/wm/desks/templates/desks_templates_item_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accessibility/accessibility_controller_impl.h"
 #include "ash/public/cpp/desk_template.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_provider.h"
@@ -78,7 +79,7 @@ std::u16string GetTimeStr(base::Time timestamp) {
   // Returns empty if `timestamp` is out of relative date range, which is
   // yesterday and today as of now. Please see `ui/base/l10n/time_format.h` for
   // more details.
-  date = ui::TimeFormat::RelativeDate(timestamp, NULL);
+  date = ui::TimeFormat::RelativeDate(timestamp, nullptr);
   if (date.empty()) {
     // Syntax `yMMMdjmm` is used by the File App if it's not a relative date.
     // Please note, this might be slightly different for different locales.
@@ -189,6 +190,7 @@ DesksTemplatesItemView::DesksTemplatesItemView(DeskTemplate* desk_template)
       base::BindRepeating(&DesksTemplatesItemView::OnDeleteButtonPressed,
                           weak_ptr_factory_.GetWeakPtr()),
       CloseButton::Type::kMedium));
+  delete_button_->SetVectorIcon(kDeleteIcon);
 
   name_view_->set_controller(this);
   name_view_observation_.Observe(name_view_);
@@ -600,7 +602,7 @@ views::View* DesksTemplatesItemView::GetView() {
 }
 
 void DesksTemplatesItemView::MaybeActivateHighlightedView() {
-  MaybeLaunchTemplate(/*delay=*/false);
+  MaybeLaunchTemplate(/*should_delay=*/false);
 }
 
 void DesksTemplatesItemView::MaybeCloseHighlightedView() {
