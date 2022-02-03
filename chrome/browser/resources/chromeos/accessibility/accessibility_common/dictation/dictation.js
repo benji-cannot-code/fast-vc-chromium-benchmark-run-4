c443cd725285da1aea0528a2b24b02124fb42b61
Akihiro Ota <akihiroota@chromium.org>
2022-02-03 22:26:25 +0000

    Dictation: Allow Dictation UI to be shown when ChromeVox is active
    This change allows the Dictation UI to be shown when ChromeVox is
    active. Previously, we disallowed this behavior because we were using
    the IME window as a temporary UI, which caused a lot of noisiness with
    ChromeVox. This change also ensures that ChromeVox announces Dictation
    UI hints when they appear by sending an alert event to ChromeVox.
    
    AX-Relnotes: N/A
    Bug: 1252037
    Change-Id: I7a4f327908da4b701a04769ca1d3f2875120e41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3434288
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966954}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/dictation.js
@@ -11,20 +11,16 @@ import {MetricsUtils} from './metrics_utils.js';
 import {SpeechParser} from './parse/speech_parser.js';
 
 const ErrorEvent = chrome.speechRecognitionPrivate.SpeechRecognitionErrorEvent;
+const HintType = chrome.accessibilityPrivate.DictationBubbleHintType;
+const IconType = chrome.accessibilityPrivate.DictationBubbleIconType;
 const ResultEvent =
     chrome.speechRecognitionPrivate.SpeechRecognitionResultEvent;
 const StartOptions = chrome.speechRecognitionPrivate.StartOptions;
 const StopEvent = chrome.speechRecognitionPrivate.SpeechRecognitionStopEvent;
 const SpeechRecognitionType =
     chrome.speechRecognitionPrivate.SpeechRecognitionType;
-const IconType = chrome.accessibilityPrivate.DictationBubbleIconType;
-const HintType = chrome.accessibilityPrivate.DictationBubbleHintType;
 
-/**
- * Main class for the Chrome OS dictation feature.
- * Please note: this is being developed behind the flag
- * --enable-experimental-accessibility-dictation-extension
- */
+/** Main class for the Chrome OS dictation feature. */
 export class Dictation {
   constructor() {
     /** @private {InputController} */
@@ -404,8 +400,7 @@ export class Dictation {
    * @private
    */
   setInterimText_(text) {
-    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
-      // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
+    if (!this.commandsFeatureEnabled_) {
       return;
     }
 
@@ -429,8 +424,7 @@ export class Dictation {
    * @private
    */
   clearInterimText_() {
-    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
-      // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
+    if (!this.commandsFeatureEnabled_) {
       return;
     }
 
@@ -450,8 +444,7 @@ export class Dictation {
    * @private
    */
   showMacroExecuted_(macro, transcript) {
-    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
-      // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
+    if (!this.commandsFeatureEnabled_) {
       return;
     }
 
@@ -481,8 +474,7 @@ export class Dictation {
    * @private
    */
   showMacroExecutionFailed_(macro, transcript) {
-    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
-      // Using chrome.input.ime for UI causes too much verbosity with ChromeVox.
+    if (!this.commandsFeatureEnabled_) {
       return;
     }
 
@@ -517,7 +509,7 @@ export class Dictation {
    * @private
    */
   hideCommandsUI_() {
-    if (this.chromeVoxEnabled_ || !this.commandsFeatureEnabled_) {
+    if (!this.commandsFeatureEnabled_) {
       return;
     }
 
