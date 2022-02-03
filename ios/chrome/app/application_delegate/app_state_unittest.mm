838a4e24f183fe443859d95cbc3331b52dfa7cf8
Sylvain Defresne <sdefresne@chromium.org>
2022-02-03 17:05:26 +0000

    [ios] Further document InitStage
    It is dangerous to have high-level code execute before the app
    reaches InitStageBrowserObjectsForBackgroundHandlers stage, so
    document this explicitly.
    
    Bug: none
    Change-Id: I59397751437060421a7ba8cd481f051bddc45ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3432128
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966777}

--- ios/chrome/app/application_delegate/app_state_unittest.mm
+++ ios/chrome/app/application_delegate/app_state_unittest.mm
@@ -112,10 +112,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       break;
     case InitStageSafeMode:
       break;
-    case InitStageEnterprise:
-      break;
     case InitStageBrowserObjectsForBackgroundHandlers:
       [appState queueTransitionToNextInitStage];
+      break;
+    case InitStageEnterprise:
       break;
     case InitStageBrowserObjectsForUI:
       [appState queueTransitionToNextInitStage];
--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -640,11 +640,11 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
     case InitStageSafeMode:
       [self addPostSafeModeAgents];
       break;
-    case InitStageEnterprise:
-      break;
     case InitStageBrowserObjectsForBackgroundHandlers:
       [self startUpBrowserBackgroundInitialization];
       [appState queueTransitionToNextInitStage];
+      break;
+    case InitStageEnterprise:
       break;
     case InitStageBrowserObjectsForUI:
       // When adding a new initialization flow, consider setting
