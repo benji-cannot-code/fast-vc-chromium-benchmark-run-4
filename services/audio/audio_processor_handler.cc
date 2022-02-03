7858c94eb48b4f8daeb9a3c35a4f37bb60808ec8
Sam Zackrisson <saza@chromium.org>
2022-02-03 09:28:58 +0000

    Move impl of ReferenceOutput::Listener::OnPlayoutData to separate class
    audio::OutputTapper currently manages both subscription to and reception of the audio output reference.
    This CL moves the ReferenceOutput::Listener implementation to a separate class, AudioProcessorHandler.
    
    This is part of a larger effort to move input audio processing effects (noise suppression, echo cancellation, etc) into the audio process.
    The AudioProcessorHandler will manage the audio processing effects for the InputController.
    
    The OutputTapper may be used even when no AudioProcessorHandler is present. This is handled by adding a no-op implementation of ReferenceOutput::Listener.
    
    A note on names: OutputTapper was up until recently named AudioProcessorHandler, but has been renamed to better reflect the setup in this CL.
    https://chromium-review.googlesource.com/c/chromium/src/+/3420493
    
    Bug: chromium:1215061
    Change-Id: Iff5cf14dd79ae4227cca92dfa5666374f9ecddb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420412
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966655}

---
+++ services/audio/audio_processor_handler.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/audio/audio_processor_handler.h"
+
+#include "base/trace_event/trace_event.h"
+#include "media/base/audio_bus.h"
+
+namespace audio {
+
+AudioProcessorHandler::AudioProcessorHandler(
+    const media::AudioProcessingSettings& settings) {
+  DCHECK(settings.NeedAudioModification());
+}
+
+AudioProcessorHandler::~AudioProcessorHandler() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
+}
+
+void AudioProcessorHandler::OnPlayoutData(const media::AudioBus& audio_bus,
+                                          int sample_rate,
+                                          base::TimeDelta delay) {
+  TRACE_EVENT2("audio", "AudioProcessorHandler::OnPlayoutData", " this ",
+               static_cast<void*>(this), "delay", delay.InMillisecondsF());
+  // TODO(https://crbug.com/1215061): Forward playout data to audio processor.
+}
+}  // namespace audio
