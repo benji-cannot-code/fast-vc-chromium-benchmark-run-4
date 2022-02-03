c632dfe45110da2ee84eb1c5126b55c7529d1b0f
David Sanders <dsanders11@ucsbalum.com>
2022-02-03 22:52:40 +0000

    Remove unused include of //components/viz/common/resources/resource_format_utils.h
    Bug: 242216
    Change-Id: I9b5918429ce27ed1a7ca763408fdce82c1f43d83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3430637
    Reviewed-by: Behdad Bakhshinategh <behdadb@chromium.org>
    Commit-Queue: Behdad Bakhshinategh <behdadb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966971}

--- cc/tiles/tile_draw_info.h
+++ cc/tiles/tile_draw_info.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/traced_value.h"
 #include "cc/resources/resource_pool.h"
 #include "components/viz/common/resources/platform_color.h"
-#include "components/viz/common/resources/resource_format_utils.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 namespace cc {
