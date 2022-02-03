0f26df9d359e312c2ca463dc5092f58845d4789c
Hans Wennborg <hans@chromium.org>
2022-02-03 15:16:40 +0000

    Don't include service_worker_container.mojom-shared.h in platform.h
    Saves about 200 MB of compiler input for the 'chrome' target [1].
    
    1. https://commondatastorage.googleapis.com/chromium-browser-clang/chrome_includes_2022-02-03_090608.html#view=edges&filter=%5Ethird_party%2Fblink%2Fpublic%2Fplatform%2Fplatform%5C.h%24&sort=asize&reverse=&includer=%5Ethird_party%2Fblink%2Fpublic%2Fplatform%2Fplatform%5C.h%24&included=&limit=1000
    
    Bug: 242216
    Change-Id: Idfd8b72c8aba0c1d28e6422736da6eee6eb300e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436406
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#966733}

--- third_party/blink/public/platform/platform.h
+++ third_party/blink/public/platform/platform.h
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/security/protocol_handler_security_level.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "third_party/blink/public/mojom/loader/code_cache.mojom-forward.h"
-#include "third_party/blink/public/mojom/service_worker/service_worker_container.mojom-shared.h"
 #include "third_party/blink/public/mojom/timing/worker_timing_container.mojom-forward.h"
 #include "third_party/blink/public/platform/audio/web_audio_device_source_type.h"
 #include "third_party/blink/public/platform/blame_context.h"
@@ -150,6 +149,10 @@ struct WebContentSecurityPolicyHeader;
 
 namespace scheduler {
 class WebThreadScheduler;
+}
+
+namespace mojom {
+class ServiceWorkerContainerHostInterfaceBase;
 }
 
 class BLINK_PLATFORM_EXPORT Platform {
