8ebfe6abd1f2d5b0fa85bb03baa617d462aab297
mcrouse <mcrouse@google.com>
2022-02-04 22:44:50 +0000

    [PCAS] Add model latency without threading delays for PageEntities.
    This adds metrics for the PageEntities model without including any
    threading delays (scheduling or hops).
    
    
    Bug: 1294311
    Change-Id: I34986a8296f92a16b47f8fa49041168ffe1b7a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441178
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967493}

--- components/optimization_guide/core/page_entities_model_executor_impl.cc
+++ components/optimization_guide/core/page_entities_model_executor_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/optimization_guide/core/page_entities_model_executor_impl.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "components/optimization_guide/core/entity_annotator_native_library.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
@@ -96,8 +97,15 @@ void EntityAnnotatorHolder::AnnotateEntitiesMetadataModelOnBackgroundThread(
   absl::optional<std::vector<ScoredEntityMetadata>> scored_md;
   if (entity_annotator_) {
     DCHECK(entity_annotator_native_library_);
+    base::TimeTicks start_time = base::TimeTicks::Now();
     scored_md =
         entity_annotator_native_library_->AnnotateText(entity_annotator_, text);
+    // The max of this histogram is 1 hour because we want to understand
+    // tail behavior and catch long running model executions.
+    base::UmaHistogramLongTimes(
+        "OptimizationGuide.PageContentAnnotationsService.ModelExecutionLatency."
+        "PageEntities",
+        base::TimeTicks::Now() - start_time);
   }
   reply_task_runner_->PostTask(FROM_HERE,
                                base::BindOnce(std::move(callback), scored_md));
