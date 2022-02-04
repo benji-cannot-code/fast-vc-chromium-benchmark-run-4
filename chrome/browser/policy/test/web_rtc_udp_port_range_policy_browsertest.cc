f10cde875be88e488926a41cec1f09c3738179f1
Yoichi Osato <yoichio@chromium.org>
2022-02-04 21:01:54 +0000

    [CodeHealth] Remove the use of deprecated GetAsString in //chrome/browser/policy/
    This change removes the use of Value::GetAsString and replaces with
    Value::is_string, Value::GetString(), Value::GetIfString().
    Design doc: https://docs.google.com/document/d/1CwYuMXnVQsRsghwVzEkWj9GZzfERputSLQaKx5xLhjQ/edit#
    
    Bug: 1187007
    
    Change-Id: Ib951a6c1a55669a3a268e4e90e581b29720345ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3434689
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967437}

--- chrome/browser/policy/test/web_rtc_udp_port_range_policy_browsertest.cc
+++ chrome/browser/policy/test/web_rtc_udp_port_range_policy_browsertest.cc
@@ -55,7 +55,8 @@ IN_PROC_BROWSER_TEST_F(WebRtcUdpPortRangeEnabledPolicyTest,
   const PrefService::Preference* pref =
       user_prefs::UserPrefs::Get(browser()->profile())
           ->FindPreference(prefs::kWebRTCUDPPortRange);
-  pref->GetValue()->GetAsString(&port_range);
+  if (pref->GetValue()->is_string())
+    port_range = pref->GetValue()->GetString();
   EXPECT_EQ(kTestWebRtcUdpPortRange, port_range);
 }
 
@@ -65,7 +66,8 @@ IN_PROC_BROWSER_TEST_F(WebRtcUdpPortRangeDisabledPolicyTest,
   const PrefService::Preference* pref =
       user_prefs::UserPrefs::Get(browser()->profile())
           ->FindPreference(prefs::kWebRTCUDPPortRange);
-  pref->GetValue()->GetAsString(&port_range);
+  if (pref->GetValue()->is_string())
+    port_range = pref->GetValue()->GetString();
   EXPECT_TRUE(port_range.empty());
 }
 
