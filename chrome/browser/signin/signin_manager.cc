22c4a579c3ae30edaf0be1dc05fc9536de69b79a
David Roger <droger@chromium.org>
2022-02-04 09:36:04 +0000

    [signin] Remove unnecessary optional in ComputeUnconsentedPrimaryAccountInfo()
    This is a code cleanup with no behavior change.
    Retuning an optional<CoreAccountInfo> is confusing as it's not clear what
    is the difference between nullopt and the empty account.
    
    This CL removes the optional and returns an empty account instead, which
    simplifies the caller code. In particular this allows removing a DCHECK.
    
    In the past, this function used to have a different semantic:
    - nullopt meant that the UPA is unknow and should not be changed
    - empty account meant that the UPA should be cleared.
    At this time, the optional was necessary.
    
    This semantic was changed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/2741124
    but the optional was not removed at that time.
    
    Change-Id: I43b3f0e46d0e5be433f93746f2c4ca1c5611b8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431425
    Reviewed-by: Jan Krčál <jkrcal@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967156}

--- chrome/browser/signin/signin_manager.cc
+++ chrome/browser/signin/signin_manager.cc
@@ -33,17 +33,15 @@ void SigninManager::UpdateUnconsentedPrimaryAccount() {
     return;
   }
 
-  absl::optional<CoreAccountInfo> account =
-      ComputeUnconsentedPrimaryAccountInfo();
+  CoreAccountInfo account = ComputeUnconsentedPrimaryAccountInfo();
 
-  DCHECK(!account || !account->IsEmpty());
-  if (account) {
+  if (!account.IsEmpty()) {
     if (identity_manager_->GetPrimaryAccountInfo(
             signin::ConsentLevel::kSignin) != account) {
       DCHECK(
           !identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSync));
       identity_manager_->GetPrimaryAccountMutator()->SetPrimaryAccount(
-          account->account_id, signin::ConsentLevel::kSignin);
+          account.account_id, signin::ConsentLevel::kSignin);
     }
   } else if (identity_manager_->HasPrimaryAccount(
                  signin::ConsentLevel::kSignin)) {
@@ -54,8 +52,7 @@ void SigninManager::UpdateUnconsentedPrimaryAccount() {
   }
 }
 
-absl::optional<CoreAccountInfo>
-SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
+CoreAccountInfo SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
   DCHECK(identity_manager_->AreRefreshTokensLoaded());
 
   // UPA is equal to the primary account with sync consent if it exists.
@@ -72,7 +69,7 @@ SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
   // It was considered simpler to keep the logic to update the unconsented
   // primary account in a single place.
   if (!signin_allowed_.GetValue())
-    return absl::nullopt;
+    return CoreAccountInfo();
 
   signin::AccountsInCookieJarInfo cookie_info =
       identity_manager_->GetAccountsInCookieJar();
@@ -86,7 +83,7 @@ SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
     // in cookies if it exists and has a refresh token.
     if (cookie_accounts.empty()) {
       // Cookies are empty, the UPA is empty.
-      return absl::nullopt;
+      return CoreAccountInfo();
     }
 
     AccountInfo account_info =
@@ -99,12 +96,11 @@ SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
         identity_manager_->HasAccountWithRefreshTokenInPersistentErrorState(
             account_info.account_id);
 
-    return error_state ? absl::nullopt
-                       : absl::make_optional<CoreAccountInfo>(account_info);
+    return error_state ? CoreAccountInfo() : account_info;
   }
 
   if (!identity_manager_->HasPrimaryAccount(signin::ConsentLevel::kSignin))
-    return absl::nullopt;
+    return CoreAccountInfo();
 
   // If cookies or tokens are not loaded, it is not possible to fully compute
   // the unconsented primary account. However, if the current unconsented
@@ -115,14 +111,14 @@ SigninManager::ComputeUnconsentedPrimaryAccountInfo() const {
   if (!identity_manager_->HasAccountWithRefreshToken(current_account)) {
     // Tokens are loaded, but the current UPA doesn't have a refresh token.
     // Clear the current UPA.
-    return absl::nullopt;
+    return CoreAccountInfo();
   }
 
   if (cookie_info.accounts_are_fresh) {
     if (cookie_accounts.empty() || cookie_accounts[0].id != current_account) {
       // The current UPA is not the first in fresh cookies. It needs to be
       // cleared.
-      return absl::nullopt;
+      return CoreAccountInfo();
     }
   }
 
