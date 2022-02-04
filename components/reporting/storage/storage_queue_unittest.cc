b3d7672092c4bc723340f6f87d2115d95852e0a1
Hong Xu <xuhong@chromium.org>
2022-02-04 04:36:46 +0000

    Create a function to delete enumerated files
    Currently in storage_queue_unittest, many files are deleted while
    traversing with `FileEnumerator`. This might cause flaky tests.
    
    This CL creates a new utility function `DeleteFilesWarnIfFailed` that
    delete files based on a `FileEnumerator` object, since we are doing this
    in multiple spots within the codebase. This utility function only delete
    a file after the `FileEnumerator` object finishes traversing. This CL
    replaces many code blocks that delete after traversing with a
    `FileEnumerator` object with a call of `DeleteFilesWarnIfFailed`.
    
    Bug: b:216390774
    Change-Id: Ib25a1dfddb303a02158c901a8a44262019c7955c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3434291
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#967097}

--- components/reporting/storage/storage_queue_unittest.cc
+++ components/reporting/storage/storage_queue_unittest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/reporting/proto/synced/record.pb.h"
 #include "components/reporting/resources/resource_interface.h"
 #include "components/reporting/storage/storage_configuration.h"
+#include "components/reporting/util/file.h"
 #include "components/reporting/util/status.h"
 #include "components/reporting/util/statusor.h"
 #include "components/reporting/util/test_support_callbacks.h"
@@ -73,6 +74,19 @@ const base::FilePath::StringPieceType kInvalidDirectoryPath =
 const base::FilePath::StringPieceType kInvalidDirectoryPath =
     FILE_PATH_LITERAL("////////////");
 #endif
+
+// Ensure files as specified by the parameters are deleted. Take the same
+// parameters as base::FileEnumerator().
+template <typename... FileEnumeratorParams>
+void EnsureDeletingFiles(FileEnumeratorParams... file_enum_params) {
+  base::FileEnumerator dir_enum(file_enum_params...);
+  ASSERT_TRUE(DeleteFilesWarnIfFailed(dir_enum));
+  // Ensure that the files have been deleted
+  ASSERT_TRUE(base::FileEnumerator(
+                  std::forward<FileEnumeratorParams>(file_enum_params)...)
+                  .Next()
+                  .empty());
+}
 
 class StorageQueueTest
     : public ::testing::TestWithParam<
@@ -782,14 +796,9 @@ TEST_P(StorageQueueTest,
   ResetTestStorageQueue();
 
   // Delete all metadata files.
-  base::FileEnumerator dir_enum(
-      options.directory(),
-      /*recursive=*/false, base::FileEnumerator::FILES,
-      base::StrCat({METADATA_NAME, FILE_PATH_LITERAL(".*")}));
-  base::FilePath full_name;
-  while (full_name = dir_enum.Next(), !full_name.empty()) {
-    base::DeleteFile(full_name);
-  }
+  EnsureDeletingFiles(options.directory(),
+                      /*recursive=*/false, base::FileEnumerator::FILES,
+                      base::StrCat({METADATA_NAME, FILE_PATH_LITERAL(".*")}));
 
   // Reopen, starting a new generation.
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
@@ -889,17 +898,11 @@ TEST_P(StorageQueueTest,
   // Reopen with the same generation and sequencing information.
   CreateTestStorageQueueOrDie(BuildStorageQueueOptionsPeriodic());
 
-  // Delete the data file *.generation.0
-  {
-    base::FileEnumerator dir_enum(
-        options.directory(),
-        /*recursive=*/false, base::FileEnumerator::FILES,
-        base::StrCat({options.file_prefix(), FILE_PATH_LITERAL(".*.0")}));
-    base::FilePath full_name;
-    while (full_name = dir_enum.Next(), !full_name.empty()) {
-      base::DeleteFile(full_name);
-    }
-  }
+  // Delete the data files *.generation.0
+  EnsureDeletingFiles(
+      options.directory(),
+      /*recursive=*/false, base::FileEnumerator::FILES,
+      base::StrCat({options.file_prefix(), FILE_PATH_LITERAL(".*.0")}));
 
   // Write more data.
   WriteStringOrDie(kMoreData[0]);
--- components/reporting/util/file.h
+++ components/reporting/util/file.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_REPORTING_UTIL_FILE_H_
 #define COMPONENTS_REPORTING_UTIL_FILE_H_
 
+#include "base/callback.h"
+#include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 
 namespace reporting {
@@ -17,6 +19,16 @@ namespace reporting {
 // warning if the deletion fails. Useful when we do not care about whether the
 // deletion succeeds or not.
 bool DeleteFileWarnIfFailed(const base::FilePath& path);
+
+// Enumerates over |dir_enum|, and deletes the file if pred.Run(file) returns
+// true. If |pred| is unspecified, all files enumerated are deleted. It deletes
+// each individual file with DeleteFileWarnIfFailed(). Refer to
+// DeleteFileWarnIfFailed() for the effect of the deletion.
+// Returns true if all files are deleted successfully, otherwise returns false.
+bool DeleteFilesWarnIfFailed(
+    base::FileEnumerator& dir_enum,
+    base::RepeatingCallback<bool(const base::FilePath&)> pred =
+        base::BindRepeating([](const base::FilePath&) { return true; }));
 
 }  // namespace reporting
 
