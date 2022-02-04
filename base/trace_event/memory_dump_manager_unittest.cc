403f50c25eb5d72fdbad33e41b584a31d2ccb01f
Nate Fischer <ntfschr@chromium.org>
2022-02-04 11:55:47 +0000

    Code inclusion: prefer allowlist (base/trace_event)
    No change to logic. This renames most references of "whitelist" to
    "allowlist" within //base/trace_event. This mostly changes
    variable/class names, but also changes some strings used in tests.
    
    To be cautious, this CL does not change the string
    "event_whitelist_predicate". I'm not totally sure how this string is
    used, so I'm not sure if it needs to stay backward compatible (e.g., if
    it's compared to user input, analyzed by a backend pipeline, or
    something like that).
    
    Test: out/Default/base_unittests (compiled for desktop platform)
    Change-Id: Icdd8f2b15303144f0782a523f0f9c5c31607b1d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3434385
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967192}

--- base/trace_event/memory_dump_manager_unittest.cc
+++ base/trace_event/memory_dump_manager_unittest.cc
@@ -66,8 +66,8 @@ MATCHER(IsNotDeterministicDump, "") {
 namespace {
 
 const char* kMDPName = "TestDumpProvider";
-const char* kWhitelistedMDPName = "WhitelistedTestDumpProvider";
-const char* const kTestMDPWhitelist[] = {kWhitelistedMDPName, nullptr};
+const char* kAllowlistedMDPName = "AllowlistedTestDumpProvider";
+const char* const kTestMDPAllowlist[] = {kAllowlistedMDPName, nullptr};
 
 void RegisterDumpProvider(
     MemoryDumpProvider* mdp,
@@ -755,13 +755,13 @@ TEST_F(MemoryDumpManagerTest, TriggerDumpWithoutTracing) {
                                         MemoryDumpDeterminism::NONE));
 }
 
-TEST_F(MemoryDumpManagerTest, BackgroundWhitelisting) {
-  SetDumpProviderAllowlistForTesting(kTestMDPWhitelist);
+TEST_F(MemoryDumpManagerTest, BackgroundAllowlisting) {
+  SetDumpProviderAllowlistForTesting(kTestMDPAllowlist);
 
   // Standard provider with default options (create dump for current process).
   MockMemoryDumpProvider backgroundMdp;
   RegisterDumpProvider(&backgroundMdp, nullptr, kDefaultOptions,
-                       kWhitelistedMDPName);
+                       kAllowlistedMDPName);
 
   EnableForTracing();
 
@@ -861,7 +861,7 @@ class SimpleMockMemoryDumpProvider : public MemoryDumpProvider {
 };
 
 TEST_F(MemoryDumpManagerTest, NoStackOverflowWithTooManyMDPs) {
-  SetDumpProviderAllowlistForTesting(kTestMDPWhitelist);
+  SetDumpProviderAllowlistForTesting(kTestMDPAllowlist);
 
   int kMDPCount = 1000;
   std::vector<std::unique_ptr<SimpleMockMemoryDumpProvider>> mdps;
@@ -872,14 +872,14 @@ TEST_F(MemoryDumpManagerTest, NoStackOverflowWithTooManyMDPs) {
   for (int i = 0; i < kMDPCount; ++i) {
     mdps.push_back(std::make_unique<SimpleMockMemoryDumpProvider>(3));
     RegisterDumpProvider(mdps.back().get(), nullptr, kDefaultOptions,
-                         kWhitelistedMDPName);
+                         kAllowlistedMDPName);
   }
   std::unique_ptr<Thread> stopped_thread(new Thread("test thread"));
   stopped_thread->Start();
   for (int i = 0; i < kMDPCount; ++i) {
     mdps.push_back(std::make_unique<SimpleMockMemoryDumpProvider>(0));
     RegisterDumpProvider(mdps.back().get(), stopped_thread->task_runner(),
-                         kDefaultOptions, kWhitelistedMDPName);
+                         kDefaultOptions, kAllowlistedMDPName);
   }
   stopped_thread->Stop();
 
