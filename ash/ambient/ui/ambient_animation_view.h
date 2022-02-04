b70aacecb7f20ce8bb921aaa967b1664cef212cf
Eric Sum <esum@google.com>
2022-02-04 00:46:47 +0000

    Add debug logging for ambient animation frame rate/performance.
    I've been carrying this local patch for a while and I'm tired of it
    so I'm checking it in. This is just debug logging, so I haven't
    bothered adding tests. In the future, we will very likely use this
    for metrics reporting in some form, so I'll add tests then.
    
    Bug: b:211909881
    Change-Id: I392f6eb18e0d83035436a4a1c451106b22ab1435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3421533
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Behdad Bakhshinategh <behdadb@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#967025}

--- ash/ambient/ui/ambient_animation_view.h
+++ ash/ambient/ui/ambient_animation_view.h
@@ -11,6 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ambient/model/ambient_animation_photo_provider.h"
 #include "ash/ash_export.h"
 #include "base/scoped_observation.h"
+#include "base/timer/timer.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "ui/compositor/throughput_tracker.h"
 #include "ui/lottie/animation_observer.h"
 #include "ui/views/view.h"
 #include "ui/views/view_observer.h"
@@ -47,6 +50,9 @@ class ASH_EXPORT AmbientAnimationView : public views::View,
 
   void OnViewBoundsChanged(View* observed_view) override;
 
+  void StartThroughputTracking();
+  void RestartThroughputTracking();
+
   AmbientViewEventHandler* const event_handler_;
 
   const std::unique_ptr<const AmbientAnimationStaticResources>
@@ -56,6 +62,9 @@ class ASH_EXPORT AmbientAnimationView : public views::View,
   views::AnimatedImageView* animated_image_view_ = nullptr;
   base::ScopedObservation<View, ViewObserver> animated_image_view_observer_{
       this};
+
+  absl::optional<ui::ThroughputTracker> throughput_tracker_;
+  base::RepeatingTimer throughput_tracker_restart_timer_;
 };
 
 }  // namespace ash
