a690c67ed88334447f5108b50f0442e0fb5fb4dd
Anders Hartvoll Ruud <andruud@chromium.org>
2022-02-04 22:14:17 +0000

    Support small/large/dynamic/logical viewport units
    This CL adds support for new units:
    
     * svw, svh (etc): Resolves against the viewport as it would be with
                       browser controls fully expanded.
     * lvw, lvh (etc): Resolves against the viewport as it would be with
                       browser controls fully minimized.
     * dvw, dvh (etc): Resolves against the viewport as it presently
                       appears given the current state of browser controls.
     * vi, vb:         Resolves against vw/vh depending on the writing-mode
                       of the target element.
    
    Note that dv* units are deliberately not responsive to browser control
    animations, and will always resolve to either sv* or lv* (not some
    value in between). This is to avoid excessive scroll-driven relayouts
    on mobile devices.
    
    This CL uses the existing system/bits for invalidation of viewport
    units. Targeted invalidation of dynamic units will come in a
    subsequent CL.
    
    Bug: 1093055
    Change-Id: I3ba55a86de53514f2020a2d5bdda579f06dd9d0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3344791
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967475}

--- third_party/blink/renderer/core/css/css_primitive_value.h
+++ third_party/blink/renderer/core/css/css_primitive_value.h
@@ -80,16 +80,45 @@ class CORE_EXPORT CSSPrimitiveValue : public CSSValue {
     kPoints,
     kPicas,
     kQuarterMillimeters,
+
+    // https://drafts.csswg.org/css-values-4/#viewport-relative-lengths
+    //
+    // See also IsViewportPercentageLength.
     kViewportWidth,
     kViewportHeight,
+    kViewportInlineSize,
+    kViewportBlockSize,
     kViewportMin,
     kViewportMax,
+    kSmallViewportWidth,
+    kSmallViewportHeight,
+    kSmallViewportInlineSize,
+    kSmallViewportBlockSize,
+    kSmallViewportMin,
+    kSmallViewportMax,
+    kLargeViewportWidth,
+    kLargeViewportHeight,
+    kLargeViewportInlineSize,
+    kLargeViewportBlockSize,
+    kLargeViewportMin,
+    kLargeViewportMax,
+    kDynamicViewportWidth,
+    kDynamicViewportHeight,
+    kDynamicViewportInlineSize,
+    kDynamicViewportBlockSize,
+    kDynamicViewportMin,
+    kDynamicViewportMax,
+
+    // https://drafts.csswg.org/css-contain-3/#container-lengths
+    //
+    // See also IsContainerPercentageLength.
     kContainerWidth,
     kContainerHeight,
     kContainerInlineSize,
     kContainerBlockSize,
     kContainerMin,
     kContainerMax,
+
     kRems,
     kChs,
     kUserUnits,  // The SVG term for unitless lengths
@@ -129,8 +158,28 @@ class CORE_EXPORT CSSPrimitiveValue : public CSSValue {
     kUnitTypeZeroCharacterWidth,
     kUnitTypeViewportWidth,
     kUnitTypeViewportHeight,
+    kUnitTypeViewportInlineSize,
+    kUnitTypeViewportBlockSize,
     kUnitTypeViewportMin,
     kUnitTypeViewportMax,
+    kUnitTypeSmallViewportWidth,
+    kUnitTypeSmallViewportHeight,
+    kUnitTypeSmallViewportInlineSize,
+    kUnitTypeSmallViewportBlockSize,
+    kUnitTypeSmallViewportMin,
+    kUnitTypeSmallViewportMax,
+    kUnitTypeLargeViewportWidth,
+    kUnitTypeLargeViewportHeight,
+    kUnitTypeLargeViewportInlineSize,
+    kUnitTypeLargeViewportBlockSize,
+    kUnitTypeLargeViewportMin,
+    kUnitTypeLargeViewportMax,
+    kUnitTypeDynamicViewportWidth,
+    kUnitTypeDynamicViewportHeight,
+    kUnitTypeDynamicViewportInlineSize,
+    kUnitTypeDynamicViewportBlockSize,
+    kUnitTypeDynamicViewportMin,
+    kUnitTypeDynamicViewportMax,
     kUnitTypeContainerWidth,
     kUnitTypeContainerHeight,
     kUnitTypeContainerInlineSize,
@@ -183,7 +232,8 @@ class CORE_EXPORT CSSPrimitiveValue : public CSSValue {
   }
   bool IsAngle() const;
   static bool IsViewportPercentageLength(UnitType type) {
-    return type >= UnitType::kViewportWidth && type <= UnitType::kViewportMax;
+    return type >= UnitType::kViewportWidth &&
+           type <= UnitType::kDynamicViewportMax;
   }
   static bool IsContainerPercentageLength(UnitType type) {
     return type >= UnitType::kContainerWidth && type <= UnitType::kContainerMax;
--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1487,6 +1487,8 @@ class CORE_EXPORT Document : public ContainerNode,
   void SetHasViewportUnits() { has_viewport_units_ = true; }
   bool HasViewportUnits() const { return has_viewport_units_; }
   void LayoutViewportWasResized();
+  // dv*
+  void DynamicViewportUnitsChanged();
 
   void InvalidateStyleAndLayoutForFontUpdates();
 
