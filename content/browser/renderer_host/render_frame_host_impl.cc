fed22c868c4b53492c24546cfc56a4547bb841c1
Sharon Yang <yangsharon@chromium.org>
2022-02-04 19:44:54 +0000

    Update GetOpenerFrameToken to take a SiteInstanceGroup
    Update RenderFrameHostManager::GetOpenerFrameToken and
    GetFrameTokenForSiteInstance to take a SiteInstanceGroup, as the
    latter already uses a SiteInstanceGroup to get the frame token.
    
    Test: No behaviour change
    Bug: 1261963
    Change-Id: I54d9dfab5488daeaab1c4ec40417bd6b1cac34f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3437659
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Sharon Yang <yangsharon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967394}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -7771,7 +7771,7 @@ void RenderFrameHostImpl::UpdateOpener() {
 
   auto opener_frame_token =
       frame_tree_node_->render_manager()->GetOpenerFrameToken(
-          GetSiteInstance());
+          GetSiteInstance()->group());
   GetAssociatedLocalFrame()->UpdateOpener(opener_frame_token);
 }
 
