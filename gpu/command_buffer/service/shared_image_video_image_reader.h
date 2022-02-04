23c0c09f5ed04616e5d9493fcbc8cf9e9ed8ae88
Vasiliy Telezhnikov <vasilyt@chromium.org>
2022-02-04 16:07:00 +0000

    Create new SharedImageRepresentation for SurfaceView overlays
    Before this CL legacy overlays on android work via
    SharedImageRepresentationOverlay, while they don't really follow the
    usual flow of overlays with BeginRead/EndRead and getting buffer.
    Instead it uses NotifyOverlayPromotion to let media code know that
    overlay promotion happened and uses BeginRead to trigger
    RenderToOverlay().
    
    This makes things tricky for implementing overlay representation for
    video on android, so this CL moves this functionaliy to new
    representation to reduce complexity. This also needed to support
    webview overlays (will be in follow up CLs).
    
    Bug: 1248067
    Change-Id: I53306408dcd920a216f02eae2124004db15d66dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3433008
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967270}

--- gpu/command_buffer/service/shared_image_video_image_reader.h
+++ gpu/command_buffer/service/shared_image_video_image_reader.h
@@ -68,8 +68,9 @@ class GPU_GLES2_EXPORT SharedImageVideoImageReader
       gpu::SharedImageManager* manager,
       gpu::MemoryTypeTracker* tracker) override;
 
-  // TODO(vikassoni): Add overlay and AHardwareBuffer representations in future
-  // patch. Overlays are anyways using legacy mailbox for now.
+  std::unique_ptr<gpu::SharedImageRepresentationLegacyOverlay>
+  ProduceLegacyOverlay(gpu::SharedImageManager* manager,
+                       gpu::MemoryTypeTracker* tracker) override;
 
  private:
   // Helper class for observing SharedContext loss on gpu main thread and
@@ -98,6 +99,7 @@ class GPU_GLES2_EXPORT SharedImageVideoImageReader
   class SharedImageRepresentationGLTexturePassthroughVideo;
   class SharedImageRepresentationVideoSkiaVk;
   class SharedImageRepresentationOverlayVideo;
+  class SharedImageRepresentationLegacyOverlayVideo;
 
   void BeginGLReadAccess(const GLuint service_id);
 
