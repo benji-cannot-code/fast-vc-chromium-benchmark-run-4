509f3b95e51f10c0fa0bdae8f53a728a6445a91f
Andrew Grieve <agrieve@chromium.org>
2022-02-04 22:01:45 +0000

    SuperSize: Fix log messages complaining about sort order
    By fixing symbols being out of order.
    
    Bug: None
    Change-Id: Ic8e0d09058aa5256f133db2ad776285b0b0b4288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438758
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967463}

--- tools/binary_size/libsupersize/apk.py
+++ tools/binary_size/libsupersize/apk.py
@@ -11,6 +11,7 @@ import re
 import zipfile
 
 import archive_util
+import file_format
 import models
 import zip_util
 
@@ -90,7 +91,7 @@ class _ResourceSourceMapper:
 
 
 def CreateApkOtherSymbols(*, metadata, apk_spec, native_spec):
-  """Creates a Container (with sections sizes) and symbols for a SizeInfo.
+  """Creates symbols for resources / assets within the apk.
 
   Args:
     metadata: Metadata dict from CreateMetadata().
@@ -161,4 +162,5 @@ def CreateApkOtherSymbols(*, metadata, apk_spec, native_spec):
   section_ranges = {}
   archive_util.ExtendSectionRange(section_ranges, models.SECTION_OTHER,
                                   sum(s.size for s in raw_symbols))
+  file_format.SortSymbols(raw_symbols)
   return section_ranges, raw_symbols
