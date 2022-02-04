26bb56a5827d854b4d4c0a5ec5bdb74d83fdfd7f
Andrii Natiahlyi <natiahlyi@google.com>
2022-02-04 15:32:48 +0000

    [Passwords] Added unmuteInsecureCredential to the passwords extension private api.
    Bug: 1178762
    Change-Id: I2529ab38c4125e068960bb78dd71121be9fd9c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436705
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#967256}

--- chrome/browser/extensions/api/passwords_private/passwords_private_api.h
+++ chrome/browser/extensions/api/passwords_private/passwords_private_api.h
@@ -328,6 +328,19 @@ class PasswordsPrivateMuteInsecureCredentialFunction
   ResponseAction Run() override;
 };
 
+class PasswordsPrivateUnmuteInsecureCredentialFunction
+    : public ExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("passwordsPrivate.unmuteInsecureCredential",
+                             PASSWORDSPRIVATE_UNMUTEINSECURECREDENTIAL)
+
+ protected:
+  ~PasswordsPrivateUnmuteInsecureCredentialFunction() override;
+
+  // ExtensionFunction overrides.
+  ResponseAction Run() override;
+};
+
 class PasswordsPrivateStartPasswordCheckFunction : public ExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("passwordsPrivate.startPasswordCheck",
--- chrome/browser/extensions/api/passwords_private/test_passwords_private_delegate.cc
+++ chrome/browser/extensions/api/passwords_private/test_passwords_private_delegate.cc
@@ -263,10 +263,7 @@ void TestPasswordsPrivateDelegate::GetPlaintextInsecurePassword(
 bool TestPasswordsPrivateDelegate::ChangeInsecureCredential(
     const api::passwords_private::InsecureCredential& credential,
     base::StringPiece new_password) {
-  return std::any_of(insecure_credentials_.begin(), insecure_credentials_.end(),
-                     [&credential](const auto& insecure_credential) {
-                       return insecure_credential.id == credential.id;
-                     });
+  return IsCredentialPresentInInsecureCredentialsList(credential);
 }
 
 // Fake implementation of RemoveInsecureCredential. This succeeds if the
@@ -283,10 +280,14 @@ bool TestPasswordsPrivateDelegate::RemoveInsecureCredential(
 // delegate knows of a insecure credential with the same id.
 bool TestPasswordsPrivateDelegate::MuteInsecureCredential(
     const api::passwords_private::InsecureCredential& credential) {
-  return std::any_of(insecure_credentials_.begin(), insecure_credentials_.end(),
-                     [&credential](const auto& insecure_credential) {
-                       return insecure_credential.id == credential.id;
-                     });
+  return IsCredentialPresentInInsecureCredentialsList(credential);
+}
+
+// Fake implementation of UnmuteInsecureCredential. This succeeds if the
+// delegate knows of a insecure credential with the same id.
+bool TestPasswordsPrivateDelegate::UnmuteInsecureCredential(
+    const api::passwords_private::InsecureCredential& credential) {
+  return IsCredentialPresentInInsecureCredentialsList(credential);
 }
 
 void TestPasswordsPrivateDelegate::StartPasswordCheck(
@@ -347,6 +348,14 @@ void TestPasswordsPrivateDelegate::SendPasswordExceptionsList() {
       PasswordsPrivateEventRouterFactory::GetForProfile(profile_);
   if (router)
     router->OnPasswordExceptionsListChanged(current_exceptions_);
+}
+
+bool TestPasswordsPrivateDelegate::IsCredentialPresentInInsecureCredentialsList(
+    const api::passwords_private::InsecureCredential& credential) {
+  return std::any_of(insecure_credentials_.begin(), insecure_credentials_.end(),
+                     [&credential](const auto& insecure_credential) {
+                       return insecure_credential.id == credential.id;
+                     });
 }
 
 }  // namespace extensions
