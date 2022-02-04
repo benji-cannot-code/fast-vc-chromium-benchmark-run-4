d97003bd70d9eab785d1d80b13b8652438fa486d
Joanmarie Diggs <jdiggs@igalia.com>
2022-02-04 17:52:35 +0000

    Ensure visible labels are not pruned from the accessibility tree
    Visible label elements which were associated with a hidden input element
    were getting incorrectly pruned from the accessibility tree. This commit
    fixes that bug.
    
    AX-Relnotes: Visible labels which are associated with a hidden input
    element are no longer getting removed from the accessibility tree and
    can be accessed via screen reader.
    
    Bug: 1283575
    Change-Id: I00d7bcdfaa99a461ff1964ca92dce0e54f2f6d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3439970
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967323}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -3496,6 +3496,10 @@ bool AXNodeObject::IsRedundantLabel(HTMLLabelElement* label) {
   if (!input)
     return false;
 
+  if (!input->GetLayoutObject() ||
+      input->GetLayoutObject()->Style()->Visibility() != EVisibility::kVisible)
+    return false;
+
   if (!input->IsCheckable())
     return false;
 
