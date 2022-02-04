f5e83de66e2ee5cea839eb634542a06571d55436
Austin Eng <enga@chromium.org>
2022-02-04 16:00:40 +0000

    Ensure mappable GPUBuffer free all shmem on ContextDestroyed
    Fixes flaky DCHECK on teardown of WebGPUImplementation.
    
    Adds an additional refcounted set of WGPUBuffer handles that need
    to be destroyed. This is necessary because some GPUBuffers may be
    marked for GC but not yet finalized. We still need to eagerly free
    the memory. It is not sufficient because if we only called
    wgpuBufferDestroy, DOMArrayBuffers still pointing to the shmem
    backing could use freed memory for the short period of time JS may
    still execute after the ContextDestroyed notification. Thus, we
    still need the current HeapHashSet<WeakMember<GPUBuffer>> to detach
    any live DOMArrayBuffers still using the shmem backing.
    
    Bug: 1005284
    Change-Id: If1915483ba1b8fdb3a8e6bf9f7a8bb3b7afa6dd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428485
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967266}

--- third_party/blink/renderer/modules/webgpu/gpu_buffer.h
+++ third_party/blink/renderer/modules/webgpu/gpu_buffer.h
@@ -19,6 +19,7 @@ class DOMArrayBuffer;
 class GPUBufferDescriptor;
 class GPUMappedDOMArrayBuffer;
 class ExecutionContext;
+struct BoxedMappableWGPUBufferHandles;
 class ScriptPromiseResolver;
 
 class GPUBuffer : public DawnObject<WGPUBuffer> {
@@ -27,9 +28,8 @@ class GPUBuffer : public DawnObject<WGPUBuffer> {
  public:
   static GPUBuffer* Create(GPUDevice* device,
                            const GPUBufferDescriptor* webgpu_desc);
-  explicit GPUBuffer(GPUDevice* device,
-                     uint64_t size,
-                     WGPUBuffer buffer);
+  GPUBuffer(GPUDevice* device, uint64_t size, WGPUBuffer buffer);
+  ~GPUBuffer() override;
 
   GPUBuffer(const GPUBuffer&) = delete;
   GPUBuffer& operator=(const GPUBuffer&) = delete;
@@ -56,7 +56,7 @@ class GPUBuffer : public DawnObject<WGPUBuffer> {
   void unmap(ScriptState* script_state);
   void destroy(ScriptState* script_state);
 
-  void Destroy(v8::Isolate* isolate);
+  void DetachMappedArrayBuffers(v8::Isolate* isolate);
 
  private:
   ScriptPromise MapAsyncImpl(ScriptState* script_state,
@@ -83,6 +83,11 @@ class GPUBuffer : public DawnObject<WGPUBuffer> {
   // Holds onto any ArrayBuffers returned by getMappedRange, mapReadAsync, or
   // mapWriteAsync.
   HeapVector<Member<GPUMappedDOMArrayBuffer>> mapped_array_buffers_;
+
+  // Mappable buffers remove themselves from this set on destruction.
+  // It tracks the set of buffers that need to be destroyed in the
+  // GPU::ContextDestroyed notification.
+  scoped_refptr<BoxedMappableWGPUBufferHandles> mappable_buffer_handles_;
 
   // List of ranges currently returned by getMappedRange, to avoid overlaps.
   Vector<std::pair<size_t, size_t>> mapped_ranges_;
