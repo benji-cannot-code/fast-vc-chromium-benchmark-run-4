7f9b8bb9fc5a7b633137614d0420cb2dc7a42db4
Lei Zhang <thestig@chromium.org>
2022-02-04 20:21:40 +0000

    Disable flaky ash network tests for ASAN.
    They are already flaky on the Chrome OS debug bots. Update the condition
    to include ASAN as well. Since the bug migrated to Buganizer, update the
    bug number in the comments as well.
    
    Bug: b/217762791
    Change-Id: I3627bd112a1c3427796162066f80ffa5104ab50a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441123
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967416}

--- ash/system/network/network_feature_pod_controller_unittest.cc
+++ ash/system/network/network_feature_pod_controller_unittest.cc
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/image_button.h"
 
-// Flaky on Chrome OS debug. http://crbug.com/1293903
-#if BUILDFLAG(IS_CHROMEOS) && !defined(NDEBUG)
+// Flaky on Chrome OS debug and Linux Chromium OS ASan LSan Tests. b/217762791
+#if !defined(NDEBUG) || defined(ADDRESS_SANITIZER)
 #define MAYBE_HasCorrectButtonStateWhenNetworkStateChanges \
   DISABLED_HasCorrectButtonStateWhenNetworkStateChanges
 #define MAYBE_PressingIconOrLabelIsHandledCorrectly_Cellular \
