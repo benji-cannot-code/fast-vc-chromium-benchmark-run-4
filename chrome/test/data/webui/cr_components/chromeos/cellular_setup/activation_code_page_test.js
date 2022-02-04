9a2bd90e6698213121382d9449b6fcbe45a4e4e3
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2022-02-04 20:00:34 +0000

    [CrOS Cellular] Disable flaky activation code page test
    Bug: b/217936048
    Change-Id: Ia951bafe4f510e0c56104d098305aaffbd87d755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440567
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#967401}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/activation_code_page_test.js
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/activation_code_page_test.js
@@ -155,7 +155,8 @@ suite('CrComponentsActivationCodePageTest', function() {
     assertFalse(!!qrCodeDetectorContainer);
   });
 
-  test('Switch camera button states', async function() {
+  // TODO(b/217936048) Disable flaky test. Renable when test is fixed.
+  test.skip('Switch camera button states', async function() {
     await flushAsync();
     const video = activationCodePage.$$('#video');
     const startScanningButton = activationCodePage.$$('#startScanningButton');
