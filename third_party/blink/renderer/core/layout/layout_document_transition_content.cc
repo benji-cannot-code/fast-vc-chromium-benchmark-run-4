5d33bd33eab91131fa13d279160b68e69db5f553
Vladimir Levin <vmpstr@chromium.org>
2022-02-04 17:29:22 +0000

    Revert "SET: Respect source element opacity in pseudo element display."
    This reverts commit 2af75335250c34955cca20a4077b76f495e33d07.
    
    Reason for revert: going with a different approach instead
    
    Original change's description:
    > SET: Respect source element opacity in pseudo element display.
    >
    > This patch propagates the opacity from the source shared element to
    > the pseudo that is used to represent it. For old content (mode 1) we
    > only update it when we're preparing to "bake" opacity in. For new
    > content we always update it to reflect the possibility of changing
    > opacity.
    >
    > R=​chrishtr@chromium.org
    >
    > Change-Id: I734315fe4fd780f8ee19cb95073a75d757c2bb82
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3437145
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#967268}
    
    Change-Id: I2011c5f402f38efb852f49593dd9c286ac84b672
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440562
    Auto-Submit: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#967302}

--- third_party/blink/renderer/core/layout/layout_document_transition_content.cc
+++ third_party/blink/renderer/core/layout/layout_document_transition_content.cc
@@ -14,7 +14,9 @@ LayoutDocumentTransitionContent::LayoutDocumentTransitionContent(
     DocumentTransitionContentElement* element)
     : LayoutReplaced(element),
       layer_(
-          cc::DocumentTransitionContentLayer::Create(element->resource_id())) {}
+          cc::DocumentTransitionContentLayer::Create(element->resource_id())) {
+  SetIntrinsicSize(element->intrinsic_size());
+}
 
 LayoutDocumentTransitionContent::~LayoutDocumentTransitionContent() = default;
 
@@ -35,10 +37,6 @@ CompositingReasons
 LayoutDocumentTransitionContent::AdditionalCompositingReasons() const {
   NOT_DESTROYED();
   return CompositingReason::kDocumentTransitionContentElement;
-}
-
-void LayoutDocumentTransitionContent::SetSourceOpacity(float opacity) {
-  layer_->SetSourceOpacity(opacity);
 }
 
 void LayoutDocumentTransitionContent::PaintReplaced(
