606d43a2fea95b8a408eaa998b883040fbf6787b
Amanda Deacon <amandadeacon@chromium.org>
2022-02-04 01:34:26 +0000

    [Shortcuts] Add relevance score calculation to KeyboardShortcutResult.
    Test: unit tests added
    Bug: 1279686
    Change-Id: I115099ec387916a9cd86ec118f201cef2e95e85b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3435290
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967052}

---
+++ chrome/browser/ui/app_list/search/keyboard_shortcut_result_unittest.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/app_list/search/keyboard_shortcut_result.h"
+
+#include "base/test/task_environment.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/test/base/testing_profile.h"
+#include "chromeos/components/string_matching/tokenized_string.h"
+#include "content/public/test/browser_task_environment.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace app_list {
+namespace {
+
+using chromeos::string_matching::TokenizedString;
+
+}  // namespace
+
+class KeyboardShortcutResultTest : public testing::Test {};
+
+TEST_F(KeyboardShortcutResultTest, CalculateRelevance) {
+  const std::u16string query(u"minimize");
+  const std::u16string target(u"Minimize window");
+
+  const TokenizedString query_tokenized(query, TokenizedString::Mode::kWords);
+  double relevance =
+      KeyboardShortcutResult::CalculateRelevance(query_tokenized, target);
+
+  EXPECT_GT(relevance, 0.5);
+}
+
+}  // namespace app_list
