3eeaf73e30fe684b74a2699378ffedd370783d94
Yutaka Hirano <yhirano@chromium.org>
2022-02-05 00:19:49 +0000

    Stop using TestURLRequestContext in //chrome
    Stop using TestURLRequestContext and its subclasses, and use
    URLRequestContextBuilder instead in //chrome.
    
    Currently many tests set context's members after it is built, which is
    a violation of a contract. This change is a step to remove setters from
    URLRequestContext to make the violation impossible.
    
    Bug: 1282359, 1282361
    Change-Id: Ide4ca888ed712fc6491aa3864264ffeef1b77956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436665
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967530}

--- chrome/browser/extensions/api/socket/tls_socket_unittest.cc
+++ chrome/browser/extensions/api/socket/tls_socket_unittest.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/base/test_completion_callback.h"
 #include "net/socket/socket_test_util.h"
+#include "net/url_request/url_request_context.h"
+#include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_test_util.h"
 #include "services/network/network_context.h"
 #include "services/network/public/mojom/network_context.mojom.h"
@@ -34,8 +36,10 @@ const char FAKE_ID[] = "abcdefghijklmnopqrst";
 
 class TLSSocketTestBase : public extensions::ExtensionServiceTestBase {
  public:
-  TLSSocketTestBase() : url_request_context_(true) {}
-  ~TLSSocketTestBase() override {}
+  TLSSocketTestBase()
+      : url_request_context_builder_(
+            net::CreateTestURLRequestContextBuilder()) {}
+  ~TLSSocketTestBase() override = default;
 
   // Creates a TCP socket.
   std::unique_ptr<TCPSocket> CreateTCPSocket() {
@@ -91,19 +95,20 @@ class TLSSocketTestBase : public extensions::ExtensionServiceTestBase {
   void SetUp() override { InitializeEmptyExtensionService(); }
 
   void Initialize() {
-    url_request_context_.Init();
+    url_request_context_ = url_request_context_builder_->Build();
     network_context_ = std::make_unique<network::NetworkContext>(
         nullptr, network_context_remote_.BindNewPipeAndPassReceiver(),
-        &url_request_context_,
+        url_request_context_.get(),
         /*cors_exempt_header_list=*/std::vector<std::string>());
     partition_.set_network_context(network_context_remote_.get());
   }
 
-  net::TestURLRequestContext url_request_context_;
+  std::unique_ptr<net::URLRequestContextBuilder> url_request_context_builder_;
 
  private:
   static const int kPort = 1234;
   TestingProfile profile_;
+  std::unique_ptr<net::URLRequestContext> url_request_context_;
   content::TestStoragePartition partition_;
   std::unique_ptr<network::NetworkContext> network_context_;
   mojo::Remote<network::mojom::NetworkContext> network_context_remote_;
@@ -116,7 +121,7 @@ class TLSSocketTest : public TLSSocketTestBase,
  public:
   TLSSocketTest() : TLSSocketTestBase() {
     mock_client_socket_factory_.set_enable_read_if_ready(true);
-    url_request_context_.set_client_socket_factory(
+    url_request_context_builder_->set_client_socket_factory_for_testing(
         &mock_client_socket_factory_);
     Initialize();
   }
