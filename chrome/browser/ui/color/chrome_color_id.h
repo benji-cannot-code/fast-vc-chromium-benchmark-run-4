e15f53f7d3a63b92c9a37d58b23e664930f4e0b8
Allen Bauer <kylixrd@chromium.org>
2022-02-07 19:15:09 +0000

    Remove use of HasCustomColor in TabStrip and use ColorProvider.
    Systematized all the ChromeColorIds used for the tab strip.
    
    Bug: 1292176
    Change-Id: I9dbed732cd977a184c59e45d53b918774a44799d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3425646
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967904}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -60,10 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kColorOmniboxText, ThemeProperties::COLOR_OMNIBOX_TEXT) \
   E(kColorOmniboxTextDimmed, ThemeProperties::COLOR_OMNIBOX_TEXT_DIMMED) \
   /* Tab output colors. */ \
-  E(kColorTabForegroundActiveFrameActive, \
-    ThemeProperties::COLOR_TAB_FOREGROUND_ACTIVE_FRAME_ACTIVE) \
-  E(kColorTabForegroundActiveFrameInactive, \
-    ThemeProperties::COLOR_TAB_FOREGROUND_ACTIVE_FRAME_INACTIVE) \
   E(kColorTabBackgroundActiveFrameActive, \
     ThemeProperties::COLOR_TAB_BACKGROUND_ACTIVE_FRAME_ACTIVE) \
   E(kColorTabBackgroundActiveFrameInactive, \
@@ -97,6 +93,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kColorTabGroupContextMenuOrange, \
     ThemeProperties::COLOR_TAB_GROUP_CONTEXT_MENU_ORANGE)
 
+#define UNTESTABLE_COLOR_IDS \
+  /* Tab output colors. */ \
+  E(kColorTabForegroundActiveFrameActive, \
+    ThemeProperties::COLOR_TAB_FOREGROUND_ACTIVE_FRAME_ACTIVE) \
+  E(kColorTabForegroundActiveFrameInactive, \
+    ThemeProperties::COLOR_TAB_FOREGROUND_ACTIVE_FRAME_INACTIVE) \
+  E(kColorTabForegroundInactiveFrameActive, \
+    ThemeProperties::COLOR_TAB_FOREGROUND_INACTIVE_FRAME_ACTIVE) \
+  E(kColorTabForegroundInactiveFrameInactive, \
+    ThemeProperties::COLOR_TAB_FOREGROUND_INACTIVE_FRAME_INACTIVE)
+
 #if BUILDFLAG(IS_WIN)
 #define CHROME_NATIVE_COLOR_IDS \
     /* The colors of the 1px border around the window on Windows 10. */ \
@@ -114,6 +121,7 @@ enum ChromeColorIds : ui::ColorId {
   kChromeColorsStart = ui::kUiColorsEnd,
 
   CHROME_COLOR_IDS
+  UNTESTABLE_COLOR_IDS
 
   kChromeColorsEnd,
 };
