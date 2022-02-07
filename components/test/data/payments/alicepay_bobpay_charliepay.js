619d527a5d13526e785c0acb40b89c9ef46b5b12
Liquan (Max) Gu <maxlg@chromium.org>
2022-02-07 17:12:03 +0000

    Fix testPaymentAppsSortingByFrecency
    The test fails because the basic-card feature has been disabled. This
    CL is to remove its dependencies on basic-card.
    
    Bug: 1294798
    Test: bin/run_chrome_public_test_apk -f "*testPaymentAppsSortingByFrecency*"
    Change-Id: If8ca15c54e8c9d2639aae1fddefd60e909f62c3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441258
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967842}

--- components/test/data/payments/alicepay_bobpay_charliepay.js
+++ components/test/data/payments/alicepay_bobpay_charliepay.js
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* global PaymentRequest:false */
 
 /**
- * Launches the PaymentRequest UI with Bob Pay, Alice Pay and credit cards as
- * payment methods.
+ * Launches the PaymentRequest UI with Bob Pay, Alice Pay as payment methods.
  */
 function buy() { // eslint-disable-line no-unused-vars
   try {
@@ -17,10 +16,7 @@ function buy() { // eslint-disable-line no-unused-vars
         [
           {supportedMethods: 'https://alicepay.com'},
           {supportedMethods: 'https://bobpay.com'},
-          {supportedMethods: 'https://charliepay.com'}, {
-            supportedMethods: 'basic-card',
-            data: {supportedNetworks: ['visa', 'mastercard']},
-          },
+          {supportedMethods: 'https://charliepay.com'},
         ],
         {total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}}})
         .show()
