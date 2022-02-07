e8934329d66c9bed267dfbfc5e555fb80f44bd61
Morten Stenshorne <mstensho@chromium.org>
2022-02-07 16:52:56 +0000

    CodeHealth: Avoid DictionaryValue::HasKey() in extensions/browser
    Use FindKey() instead.
    
    Bug: 1187060
    Change-Id: I1f7fb1b428cb8e9127440480d479e18a8f90b4e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440136
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967831}

--- extensions/browser/api/lock_screen_data/data_item.cc
+++ extensions/browser/api/lock_screen_data/data_item.cc
@@ -152,7 +152,7 @@ void RegisterItem(OperationResult* result,
     return;
   }
 
-  if (dict->HasKey(item_id)) {
+  if (dict->FindKey(item_id)) {
     *result = OperationResult::kAlreadyRegistered;
     return;
   }
