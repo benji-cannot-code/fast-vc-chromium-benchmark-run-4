712814cfa7e69e1c5e39c97f0330bbf641c3b0ed
Victor Costan <pwnall@chromium.org>
2022-02-07 23:08:53 +0000

    sql: Document some SQLite VFS assumptions as DCHECKs.
    Change-Id: I370c051f27f73363967dd98bf7e876492aab63ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3442495
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968058}

--- sql/sandboxed_vfs_file.h
+++ sql/sandboxed_vfs_file.h
@@ -13,6 +13,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sql {
 
+// The file types associated with a SQLite database.
+enum class SandboxedVfsFileType {
+  // The main file, which stores the database pages.
+  kDatabase,
+  // The transaction rollback journal file. Used when WAL is off.
+  // This file has the same path as the database, plus the "-journal" suffix.
+  kJournal,
+  // The Write-Ahead Log (WAL) file.
+  // This file has the same path as the database, plus the "-wal" suffix.
+  kWal,
+};
+
 class SandboxedVfs;
 
 // SQLite VFS file implementation that works in a sandboxed process.
@@ -37,6 +49,9 @@ class SandboxedVfsFile {
   // returned sqlite3_file object.
   static void Create(base::File file,
                      base::FilePath file_path,
+#if DCHECK_IS_ON()
+                     SandboxedVfsFileType file_type,
+#endif  // DCHECK_IS_ON()
                      SandboxedVfs* vfs,
                      sqlite3_file& buffer);
 
@@ -69,6 +84,9 @@ class SandboxedVfsFile {
  private:
   SandboxedVfsFile(base::File file,
                    base::FilePath file_path,
+#if DCHECK_IS_ON()
+                   SandboxedVfsFileType file_type,
+#endif  // DCHECK_IS_ON()
                    SandboxedVfs* vfs);
   ~SandboxedVfsFile();
 
@@ -78,6 +96,10 @@ class SandboxedVfsFile {
   int sqlite_lock_mode_;
   // The SandboxedVfs that created this instance.
   const raw_ptr<SandboxedVfs> vfs_;
+#if DCHECK_IS_ON()
+  // Tracked to check assumptions about SQLite's locking protocol.
+  const SandboxedVfsFileType file_type_;
+#endif  // DCHECK_IS_ON()
   // Used to identify the file in IPCs to the browser process.
   const base::FilePath file_path_;
 };
