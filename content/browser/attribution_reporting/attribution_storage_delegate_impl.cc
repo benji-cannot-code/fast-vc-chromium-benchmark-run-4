00d687c628e9c5d81aebb22b6057e6c44514d1a5
Andrew Paseltiner <apaseltiner@chromium.org>
2022-02-07 14:21:02 +0000

    Align event-level report rate limits with updated explainer
    - Rate limits are combined across navigation and event sources
    - Rate limits are scoped to reporting origin in addition to source site
      and destination site
    - The newly stored reporting origin in this table participates in
      origin-based data deletion
    
    https://github.com/WICG/conversion-measurement-api/blob/main/EVENT.md#reporting-cooldown--rate-limits
    
    The new limits on unique reporting origins per source and per report
    will be addressed in a followup CL.
    
    Change-Id: I2abb8172f107b1d399f94d7fe03c8cadd57a8ad5
    Bug: 1294223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3425176
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967779}

--- content/browser/attribution_reporting/attribution_storage_delegate_impl.cc
+++ content/browser/attribution_reporting/attribution_storage_delegate_impl.cc
@@ -16,12 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-namespace {
-
-using AttributionType = ::content::AttributionStorage::AttributionType;
-
-}  // namespace
-
 AttributionStorageDelegateImpl::AttributionStorageDelegateImpl(bool debug_mode)
     : debug_mode_(debug_mode) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
@@ -55,18 +49,12 @@ int AttributionStorageDelegateImpl::
 }
 
 AttributionStorage::Delegate::RateLimitConfig
-AttributionStorageDelegateImpl::GetRateLimits(
-    AttributionType attribution_type) const {
+AttributionStorageDelegateImpl::GetRateLimits() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  // TODO(csharrison): Finalize `max_contributions_per_window` value.
-  switch (attribution_type) {
-    case AttributionType::kNavigation:
-    case AttributionType::kEvent:
-      return {
-          .time_window = base::Days(30),
-          .max_contributions_per_window = 100,
-      };
-  }
+  return RateLimitConfig{
+      .time_window = base::Days(30),
+      .max_attributions_per_window = 100,
+  };
 }
 
 base::TimeDelta
--- content/browser/attribution_reporting/attribution_storage_unittest.cc
+++ content/browser/attribution_reporting/attribution_storage_unittest.cc
@@ -774,7 +774,7 @@ TEST_F(AttributionStorageTest, DeleteAllNullDeleteBegin) {
 TEST_F(AttributionStorageTest, MaxAttributionReportsBetweenSites) {
   delegate()->set_rate_limits({
       .time_window = base::TimeDelta::Max(),
-      .max_contributions_per_window = 2,
+      .max_attributions_per_window = 2,
   });
 
   auto conversion = DefaultTrigger();
@@ -796,10 +796,10 @@ TEST_F(AttributionStorageTest, MaxAttributionReportsBetweenSites) {
 }
 
 TEST_F(AttributionStorageTest,
-       MaxAttributionReportsBetweenSites_RespectsSourceType) {
+       MaxAttributionReportsBetweenSites_IgnoresSourceType) {
   delegate()->set_rate_limits({
       .time_window = base::TimeDelta::Max(),
-      .max_contributions_per_window = 1,
+      .max_attributions_per_window = 1,
   });
 
   storage()->StoreSource(
@@ -813,22 +813,7 @@ TEST_F(AttributionStorageTest,
       SourceBuilder()
           .SetSourceType(CommonSourceInfo::SourceType::kEvent)
           .Build());
-  // This would fail if the source types had a combined limit or the incorrect
-  // source type were stored.
-  EXPECT_EQ(CreateReportStatus::kSuccess,
-            MaybeCreateAndStoreReport(DefaultTrigger()));
-
-  storage()->StoreSource(
-      SourceBuilder()
-          .SetSourceType(CommonSourceInfo::SourceType::kEvent)
-          .Build());
-  EXPECT_EQ(CreateReportStatus::kRateLimited,
-            MaybeCreateAndStoreReport(DefaultTrigger()));
-
-  storage()->StoreSource(
-      SourceBuilder()
-          .SetSourceType(CommonSourceInfo::SourceType::kNavigation)
-          .Build());
+  // This would fail if the source types had separate limits.
   EXPECT_EQ(CreateReportStatus::kRateLimited,
             MaybeCreateAndStoreReport(DefaultTrigger()));
 }
@@ -876,7 +861,7 @@ TEST_F(AttributionStorageTest, NeverAttributeImpression_Deactivates) {
 TEST_F(AttributionStorageTest, NeverAttributeImpression_RateLimitsNotChanged) {
   delegate()->set_rate_limits({
       .time_window = base::TimeDelta::Max(),
-      .max_contributions_per_window = 1,
+      .max_attributions_per_window = 1,
   });
 
   delegate()->set_randomized_response(
