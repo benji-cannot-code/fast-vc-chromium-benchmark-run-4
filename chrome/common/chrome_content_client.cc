4cff1c092cadd83fc3aef00c9457d5944f3e9f73
Javier Fernández García-Boente <jfernandez@igalia.com>
2022-02-07 13:41:02 +0000

    Move the Protocol Handler related message IDs to //component
    The RegisterProtocolHandlerPermissionRequest could be moved easily to
    the new Custom Handlers //component, except for the access to the
    chrome specific message Ids used for internationalization.
    
    This CL moves the message IDs currently defined in the chrome's
    generated_resources.grd have now their own file and will be integrated
    as grd part in the components_strings.grd.
    
    Since I'm just moving already existent IDs and no new ID added is added
    I think it's legit to bypass the screenshot requirement (despite most
    of this ID's lacking such screenshot).
    
    Bug: 1293295, 1139176
    Change-Id: I127ae1583635b78eaf9c6c952bff7d295a8bdb4f
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440149
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#967757}

--- chrome/common/chrome_content_client.cc
+++ chrome/common/chrome_content_client.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/dom_distiller/core/url_constants.h"
 #include "components/embedder_support/origin_trials/origin_trial_policy_impl.h"
 #include "components/services/heap_profiling/public/cpp/profiling_client.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/common/cdm_info.h"
 #include "content/public/common/content_constants.h"
 #include "content/public/common/content_switches.h"
