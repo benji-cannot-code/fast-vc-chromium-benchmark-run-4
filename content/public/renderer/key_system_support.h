37b78f4fb5b5255a84c5d4760dd4ed887b1fd10c
Xiaohan Wang <xhwang@chromium.org>
2022-02-07 22:11:23 +0000

    media: Make mojom::IsKeySystemSupported() async
    Currently media::mojom::IsKeySystemSupported() is sync, which could
    cause renderer hang due to delayed responses from the browser process,
    i.e. when the OS query is slow. This CL converts it to an async call to
    solve the issue.
    
    Bug: 853264,b/213941141
    Test: Existing tests pass. Manually tested with test page as well.
    Change-Id: I4501e9d8b4b732a3d1378445f1f35a2cd8820777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3437123
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968017}

--- content/public/renderer/key_system_support.h
+++ content/public/renderer/key_system_support.h
@@ -14,12 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// Determines if |key_system| is supported by calling into the browser.
-// If it is supported, return true and |key_system_capability| is updated
-// to match what |key_system| supports. If not supported, false is returned.
-CONTENT_EXPORT bool IsKeySystemSupported(
-    const std::string& key_system,
-    media::mojom::KeySystemCapabilityPtr* key_system_capability);
+using IsKeySystemSupportedCB =
+    media::mojom::KeySystemSupport::IsKeySystemSupportedCallback;
+
+// Determines if |key_system| is supported by calling into the browser and
+// calls the `cb` with the result.
+CONTENT_EXPORT void IsKeySystemSupported(const std::string& key_system,
+                                         IsKeySystemSupportedCB cb);
 
 }  // namespace content
 
