92a1ddc484ea1bf12e27fd793328f6f6accf38b9
Sirisha Kavuluru <skavuluru@google.com>
2022-02-07 23:07:07 +0000

    Adding animation for strip scrim
    Bug: 1294937
    Change-Id: I99d075afd2945bd425d6d48c3017bd2795eb9f2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3444242
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#968055}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.compositor.overlays.strip;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -43,6 +44,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabObserver;
 import org.chromium.chrome.features.start_surface.StartSurface;
+import org.chromium.components.browser_ui.widget.animation.Interpolators;
 import org.chromium.ui.base.LocalizationUtils;
 import org.chromium.ui.resources.ResourceManager;
 import org.chromium.url.GURL;
@@ -54,6 +56,7 @@ import java.util.List;
  * all input and model events to the proper destination.
  */
 public class StripLayoutHelperManager implements SceneOverlay {
+    private static final long FADE_SCRIM_DURATION_MS = 500;
     // Caching Variables
     private final RectF mStripFilterArea = new RectF();
 
@@ -83,6 +86,7 @@ public class StripLayoutHelperManager implements SceneOverlay {
     private final CompositorButton mModelSelectorButton;
 
     private final StripScrim mStripScrim;
+    private ValueAnimator mScrimFadeAnimation;
 
     private TabStripSceneLayer mTabStripTreeProvider;
 
@@ -186,6 +190,22 @@ public class StripLayoutHelperManager implements SceneOverlay {
 
         private void updateScrimVisibility(boolean visibility) {
             if (!isGridTabSwitcherEnabled()) return;
+
+            if (mScrimFadeAnimation != null) {
+                mScrimFadeAnimation.cancel();
+            }
+
+            float startAlpha = visibility ? 0.f : 1.f;
+            float endAlpha = visibility ? 1.f : 0.f;
+            mScrimFadeAnimation = ValueAnimator.ofFloat(startAlpha, endAlpha);
+            mScrimFadeAnimation.setInterpolator(Interpolators.LINEAR_INTERPOLATOR);
+            mScrimFadeAnimation.setDuration(FADE_SCRIM_DURATION_MS);
+            mScrimFadeAnimation.addUpdateListener((anim -> {
+                final float currentAlpha = (float) anim.getAnimatedValue();
+                mStripScrim.setAlpha(currentAlpha);
+                mTabStripTreeProvider.updateStripScrim(mStripScrim);
+            }));
+            mScrimFadeAnimation.start();
             mStripScrim.setVisible(visibility);
         }
     }
@@ -241,7 +261,7 @@ public class StripLayoutHelperManager implements SceneOverlay {
                 res.getString(R.string.accessibility_tabstrip_btn_incognito_toggle_standard),
                 res.getString(R.string.accessibility_tabstrip_btn_incognito_toggle_incognito));
 
-        mStripScrim = new StripScrim(mWidth);
+        mStripScrim = new StripScrim(mWidth, mHeight);
         mStripScrim.setVisible(false);
 
         onContextChanged(context);
