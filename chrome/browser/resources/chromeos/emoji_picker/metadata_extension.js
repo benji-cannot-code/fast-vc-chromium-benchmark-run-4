6b01f97ec2bbf3f8a8bf72d57fa60afbf6eff902
Chuong Ho <hdchuong@google.com>
2022-02-07 02:49:42 +0000

    emoji-picker: pass category metadata control to emoji-picker
    ** To enable update category metadata by scrolling, this CL proposes to
    pass this metadata control to <emoji-picker/>, because emoji-picker
    currently manages scrolling logic.
    
    Such change also solves the current problem with emoji-search.
    * Information about "current category" is saved into two different
    classes, <emoji-search> and <emoji-picker>. This CL moves current
    category data into <emoji-picker> (Single Source of Truth).
    * Intuitively, <emoji-search> should only manage searching capability,
    which is achieved by this CL (Single Responsibility).
    * As a result, we also reduce the number of event handlers around
    emoji-picker. Now whenever category button is clicked,
    only one CATEGORY_BUTTON_CLICK event is handled at emoji-picker. It
    used to be 2 events handled at <emoji-search> and <emoji-picker>.
    
    Bug: b/216103506
    Demo: None. This CL should not result in any feature change/add/delete.
    
    Change-Id: Ia2a2022f52335ec34197d3135ee063c98171a006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3422426
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#967659}

--- chrome/browser/resources/chromeos/emoji_picker/metadata_extension.js
+++ chrome/browser/resources/chromeos/emoji_picker/metadata_extension.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export const CATEGORY_DATA = [
+export const CATEGORY_METADATA = [
   {
     name: 'emoji',
     icon: 'emoji_picker:insert_emoticon',
