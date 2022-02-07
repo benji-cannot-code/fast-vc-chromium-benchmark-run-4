1fdd6a8b6041dbd42d7c34fa56d57ef1c2db0b60
Yuki Shiino <yukishiino@chromium.org>
2022-02-07 07:51:42 +0000

    partition_alloc: Move tagging constants into an namespace
    In order not to pollute the global namespace, moves constants
    in tagging.h into partition_alloc::internal.
    
    Bug: 1288247
    Change-Id: I3e2ae7c672ca1f508ac9407a866e300d1151c4d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436945
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967698}

--- base/allocator/partition_allocator/tagging.h
+++ base/allocator/partition_allocator/tagging.h
@@ -17,13 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "build/build_config.h"
 
-constexpr int kMemTagGranuleSize = 16u;
-#if defined(PA_HAS_MEMORY_TAGGING)
-constexpr uint64_t kMemTagUnmask = 0x00ffffffffffffffuLL;
-#else
-constexpr uint64_t kMemTagUnmask = 0xffffffffffffffffuLL;
-#endif
-
 namespace partition_alloc {
 
 // Enum configures Arm's MTE extension to operate in different modes
@@ -44,6 +37,13 @@ BASE_EXPORT void ChangeMemoryTaggingModeForCurrentThread(
     TagViolationReportingMode);
 
 namespace internal {
+
+constexpr int kMemTagGranuleSize = 16u;
+#if defined(PA_HAS_MEMORY_TAGGING)
+constexpr uint64_t kMemTagUnmask = 0x00ffffffffffffffuLL;
+#else
+constexpr uint64_t kMemTagUnmask = 0xffffffffffffffffuLL;
+#endif  // defined(PA_HAS_MEMORY_TAGGING)
 
 #if BUILDFLAG(IS_ANDROID)
 // Changes the memory tagging mode for all threads in the current process.
