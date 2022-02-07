70f7c9a3732cfd0b03b28d1f33af51e848a8c36c
Colin Kincaid <ckincaid@chromium.org>
2022-02-07 19:00:47 +0000

    personalization: Add Google Photos support to fetch a single photo
    This CL adds an item ID parameter to the Wallpaper app's existing mojo
    API for fetching photos from Google Photos. If provided, the ID
    specifies one photo to fetch. This option takes precedence over the
    option to fetch an album if an album ID is provided. If neither is
    provided, the whole library is fetched.
    
    We will need to fetch a single photo from Google Photos in order to set
    it as a device's wallpaper.
    
    Fixed: b/217612619
    Change-Id: I1cccafbc8e0a2559b3985aae9310744b36a6ba53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3433993
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967901}

--- chrome/browser/ash/web_applications/personalization_app/personalization_app_wallpaper_provider_impl.cc
+++ chrome/browser/ash/web_applications/personalization_app/personalization_app_wallpaper_provider_impl.cc
@@ -209,6 +209,7 @@ void PersonalizationAppWallpaperProviderImpl::FetchGooglePhotosCount(
 }
 
 void PersonalizationAppWallpaperProviderImpl::FetchGooglePhotosPhotos(
+    const absl::optional<std::string>& item_id,
     const absl::optional<std::string>& album_id,
     const absl::optional<std::string>& resume_token,
     FetchGooglePhotosPhotosCallback callback) {
@@ -228,7 +229,7 @@ void PersonalizationAppWallpaperProviderImpl::FetchGooglePhotosPhotos(
             profile_);
   }
   google_photos_photos_fetcher_->AddRequestAndStartIfNecessary(
-      album_id, resume_token, std::move(callback));
+      item_id, album_id, resume_token, std::move(callback));
 }
 
 void PersonalizationAppWallpaperProviderImpl::GetLocalImages(
