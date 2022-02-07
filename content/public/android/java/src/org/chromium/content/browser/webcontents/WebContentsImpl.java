c70a7670ea64eafbfc3a8b5a9b4fb2bc39020907
Peilin Wang <peilinwang@google.com>
2022-02-07 19:29:37 +0000

    [WebContentsImpl] Detect handling observer method in destroy
    Adds an assert to crash if we are trying to destroy a WebContents while
    handling its observer methods.
    
    Stability sherrifs: We don't expect this to cause many crashes, but we
    aren't sure, so we're sending this out of abundance in caution. This is
    expected to catch bugs in the field. If you see a crash with this stack,
    it's likely indicative of a failure caused by other code, and not this
    instrumentation. Reverting this CL will just result in a separate crash
    in native code that's unactionable, but the calling stack is what should
    be investigated and fixed.
    
    Bug: 1290842
    Change-Id: Ida938fa691d5dce43d6106481ba084b679a83efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428633
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#967917}

--- content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/webcontents/WebContentsImpl.java
@@ -367,6 +367,8 @@ public class WebContentsImpl implements WebContents, RenderFrameHostDelegate, Wi
             throw new IllegalStateException("Attempting to destroy WebContents on non-UI thread");
         }
 
+        assert mObserverProxy == null || !mObserverProxy.isHandlingObserverCall();
+
         if (mNativeWebContentsAndroid != 0) {
             WebContentsImplJni.get().destroyWebContents(mNativeWebContentsAndroid);
         }
