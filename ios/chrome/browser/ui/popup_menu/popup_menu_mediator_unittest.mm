ef2d5e6edcc6bac5a21af76059b533c16aef7689
Quentin Pubert <qpubert@google.com>
2022-02-07 09:08:00 +0000

    Fixed PopupMenuMediatorTest.TestToolsMenuItemsCount
    This test does not account for the "New Window" item being visible if
    base::ios::IsMultipleScenesSupported() returns true. This CL tests this
    value and accounts for this item.
    
    Fixed: 1294243
    Change-Id: I20cd94ded74280b5e509f6c2b864e720441f52bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3439416
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#967713}

--- ios/chrome/browser/ui/popup_menu/popup_menu_mediator_unittest.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_mediator_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_mediator.h"
 
 #include "base/files/scoped_temp_dir.h"
+#include "base/ios/ios_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/default_clock.h"
@@ -309,10 +310,16 @@ TEST_F(PopupMenuMediatorTest, TestToolsMenuItemsCount) {
     number_of_action_items++;
   }
 
+  // Stop/Reload, New Tab, New Incognito Tab.
+  NSUInteger number_of_tab_actions = 3;
+  if (base::ios::IsMultipleScenesSupported()) {
+    // New Window option is added in this case.
+    number_of_tab_actions++;
+  }
+
   // Checks that Tools Menu has the right number of items in each section.
   CheckMediatorSetItems(@[
-    // Stop/Reload, New Tab, New Incognito Tab.
-    @(3),
+    @(number_of_tab_actions),
     // 4 collections, Downloads, Settings.
     @(6),
     // Other actions, depending on configuration.
