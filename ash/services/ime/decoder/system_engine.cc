877000ef9c14e25f15d51e0004892e32c4b01dd8
Bao-Duy Tran <tranbaoduy@chromium.org>
2022-02-07 08:16:49 +0000

    Add emptiness-guards for usage of SystemEngine::decoder_entry_points_.
    decoder_entry_points_ can be "empty", as per the notion defined by
    absl::optional, akin to "null". All usage had better be guarded by
    appropriate emptiness checks.
    
    The guards are missing in SystemEngine (used for Mojo-mode access
    to CrOS IME shared lib), while they already exist for DecoderEngine
    (used for Proto-mode access to CrOS IME shared lib).
    
    __SIMPLE_REVIEW__
    
    Bug: 1287417
    Change-Id: If9c009ce0f2651da1223757970a87db964e80dc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428261
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#967705}

--- ash/services/ime/decoder/system_engine.cc
+++ ash/services/ime/decoder/system_engine.cc
@@ -34,6 +34,10 @@ bool SystemEngine::BindRequest(
     const std::string& ime_spec,
     mojo::PendingReceiver<mojom::InputMethod> receiver,
     mojo::PendingRemote<mojom::InputMethodHost> host) {
+  if (!decoder_entry_points_) {
+    return false;
+  }
+
   auto receiver_pipe_handle = receiver.PassPipe().release().value();
   auto host_pipe_version = host.version();
   auto host_pipe_handle = host.PassPipe().release().value();
@@ -52,7 +56,8 @@ bool SystemEngine::BindConnectionFactory(
 }
 
 bool SystemEngine::IsConnected() {
-  return decoder_entry_points_->is_input_method_connected();
+  return decoder_entry_points_ &&
+         decoder_entry_points_->is_input_method_connected();
 }
 
 }  // namespace ime
