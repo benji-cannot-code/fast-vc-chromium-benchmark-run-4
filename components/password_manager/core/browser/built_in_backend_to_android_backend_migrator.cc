b0329869bb9218e8846f9e0720f7b0e9bc9bbd25
Mohamed Amir Yosef <mamir@chromium.org>
2022-02-08 15:18:35 +0000

    [Passwords] BuiltInBackendToAndroidBackendMigrator::MigrationFinished()
    This CL introduces a new method that is responsible for reporting
    metrics and deleting the metrics reporter instance.
    
    This CL is purely mechanical and shouldn't introduce any behavioral
    changes.
    
    In addition, it reorders the methods in the header file to match the
    same order as in the .cc file.
    
    Bug: 1278761
    Change-Id: I5468326ca311f7892d0e02277a1b9e006ca40a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447500
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968327}

--- components/password_manager/core/browser/built_in_backend_to_android_backend_migrator.cc
+++ components/password_manager/core/browser/built_in_backend_to_android_backend_migrator.cc
@@ -76,14 +76,14 @@ class BuiltInBackendToAndroidBackendMigrator::MigrationMetricsReporter {
       : metric_infix_(metric_infix) {}
   ~MigrationMetricsReporter() = default;
 
-  void MigrationFinished(bool is_success) {
+  void ReportMetrics(bool migration_succeeded) {
     auto BuildMetricName = [this](base::StringPiece suffix) {
       return base::StrCat({"PasswordManager.UnifiedPasswordManager.",
                            metric_infix_, ".", suffix});
     };
     base::TimeDelta duration = base::Time::Now() - start_;
     base::UmaHistogramMediumTimes(BuildMetricName("Latency"), duration);
-    base::UmaHistogramBoolean(BuildMetricName("Success"), is_success);
+    base::UmaHistogramBoolean(BuildMetricName("Success"), migration_succeeded);
   }
 
  private:
@@ -176,8 +176,7 @@ void BuiltInBackendToAndroidBackendMigrator::
   DCHECK_EQ(2u, results.size());
 
   if (results[0].HasError() || results[1].HasError()) {
-    metrics_reporter_->MigrationFinished(/*is_success=*/false);
-    metrics_reporter_.reset();
+    MigrationFinished(/*is_success=*/false);
     return;
   }
 
@@ -218,9 +217,8 @@ void BuiltInBackendToAndroidBackendMigrator::
   // completion for the next operation. At the end, update pref to mark
   // successful completion.
   base::OnceClosure callbacks_chain =
-      base::BindOnce(&BuiltInBackendToAndroidBackendMigrator::
-                         MigrationMetricsReporter::MigrationFinished,
-                     std::move(metrics_reporter_), /*is_success=*/true);
+      base::BindOnce(&BuiltInBackendToAndroidBackendMigrator::MigrationFinished,
+                     weak_ptr_factory_.GetWeakPtr(), /*is_success=*/true);
 
   callbacks_chain =
       base::BindOnce(
@@ -302,9 +300,8 @@ void BuiltInBackendToAndroidBackendMigrator::
   // Callbacks are chained like in a stack way by passing 'callback_chain' as a
   // completion for the next operation.
   base::OnceClosure callbacks_chain =
-      base::BindOnce(&BuiltInBackendToAndroidBackendMigrator::
-                         MigrationMetricsReporter::MigrationFinished,
-                     std::move(metrics_reporter_), /*is_success=*/true);
+      base::BindOnce(&BuiltInBackendToAndroidBackendMigrator::MigrationFinished,
+                     weak_ptr_factory_.GetWeakPtr(), /*is_success=*/true);
 
   for (auto* const login : built_in_backend_logins) {
     auto android_login_iter = android_logins.find(login);
@@ -378,6 +375,13 @@ void BuiltInBackendToAndroidBackendMigrator::RemoveLoginFromBackend(
     base::OnceClosure callback) {
   backend->RemoveLoginAsync(
       form, IgnoreChangeListAndRunCallback(std::move(callback)));
+}
+
+void BuiltInBackendToAndroidBackendMigrator::MigrationFinished(
+    bool is_success) {
+  DCHECK(metrics_reporter_);
+  metrics_reporter_->ReportMetrics(is_success);
+  metrics_reporter_.reset();
 }
 
 }  // namespace password_manager
