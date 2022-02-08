76f8d15b50833ae9f80dd4ede7a22ac6f8f50f83
Sorin Jianu <sorin@chromium.org>
2022-02-08 00:09:00 +0000

    Add COM initialization for setup and uninstall code.
    Bug: 1295075
    Change-Id: I5d2d6b2309ccc33a4ab643545e06bd446435532e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445483
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968095}

--- chrome/updater/win/setup/uninstall.cc
+++ chrome/updater/win/setup/uninstall.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/launch.h"
 #include "base/process/process.h"
 #include "base/strings/stringprintf.h"
+#include "base/win/scoped_com_initializer.h"
 #include "chrome/installer/util/install_service_work_item.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/work_item_list.h"
@@ -147,6 +148,10 @@ int UninstallImpl(UpdaterScope scope, bool uninstall_all) {
       scope == UpdaterScope::kSystem ? HKEY_LOCAL_MACHINE : HKEY_CURRENT_USER;
 
   updater::UnregisterWakeTask(scope);
+
+  auto scoped_com_initializer =
+      std::make_unique<base::win::ScopedCOMInitializer>(
+          base::win::ScopedCOMInitializer::kMTA);
 
   if (uninstall_all) {
     std::unique_ptr<WorkItemList> uninstall_list(
