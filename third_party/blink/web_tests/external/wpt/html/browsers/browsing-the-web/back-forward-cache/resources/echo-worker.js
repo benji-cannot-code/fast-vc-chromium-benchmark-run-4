367d12e829af8e4125818a7e548c41a7a685a493
Hiroshige Hayashizaki <hiroshige@chromium.org>
2022-02-08 13:46:07 +0000

    [WPT] Eligibility: dedicated and shared workers
    When there are dedicated workers/shared workers,
    
    Chrome: The page is not BFCached.
    Firefox: The page is BFCached.
    Safari: The page is BFCached for dedicated workers.
            Shared workers are not supported.
    
    Bug: 1146955
    Change-Id: Ia901715436e1a0c64c68ad5b537e99f685825489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3220944
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968294}

---
+++ third_party/blink/web_tests/external/wpt/html/browsers/browsing-the-web/back-forward-cache/resources/echo-worker.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// On receiving a message from the parent Document, send back a message to the
+// parent Document. This is used to wait for worker initialization and test
+// that this worker is alive and working.
+
+// For dedicated workers.
+self.addEventListener('message', event => {
+  postMessage(event.data);
+});
+
+// For shared workers.
+onconnect = e => {
+  const port = e.ports[0];
+  port.onmessage = event => {
+    port.postMessage(event.data);
+  }
+};
