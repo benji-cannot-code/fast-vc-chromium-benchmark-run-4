88b7e165dd4520601f26e216e8f87706165a895b
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-02-08 10:33:11 +0000

    Trivially s/"constexpr inline"/"inline constexpr" in base/
    Per thread in cxx@chromium.org, the latter is a more common writing in
    Chromium (27 occurrences to 1). While we don't want to invest in `git
    cl format` to enforce this, this CL does make it less likely that
    developers replicate the first writing.
    
    https://groups.google.com/u/4/a/chromium.org/g/cxx/c/hmyGFD80ocE
    
    Change-Id: Ie5ff0a6a16dabde5233bd593353f46a7c6f2d3bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3443428
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968261}

--- base/containers/small_map.h
+++ base/containers/small_map.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/memory/raw_ptr.h"
 
-constexpr inline size_t kUsingFullMapSentinel =
+inline constexpr size_t kUsingFullMapSentinel =
     std::numeric_limits<size_t>::max();
 
 namespace base {
