d486ffbdc23d5a553c73a96067f6c78596091787
Wei Lee <wtlee@google.com>
2022-02-08 07:54:31 +0000

    CCA: Let chrome-untrusted://camera-app has its own WebUI controller
    Bug: b:214777310
    Test: tast run [DUT] camera.CCAUI*
    Change-Id: I1d25eb8de7f4730bc9de5b5ce6bca92e52c13688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419402
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968221}

--- chrome/browser/ui/webui/chrome_untrusted_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_untrusted_web_ui_controller_factory.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/help_app_ui/help_app_kids_magazine_untrusted_ui.h"
 #include "ash/webui/os_feedback_ui/os_feedback_untrusted_ui.h"
 #include "ash/webui/personalization_app/untrusted_personalization_app_ui_config.h"
+#include "chrome/browser/ash/web_applications/camera_app/camera_app_untrusted_ui_config.h"
 #include "chrome/browser/ash/web_applications/crosh_ui.h"
 #include "chrome/browser/ash/web_applications/help_app/help_app_untrusted_ui_config.h"
 #include "chrome/browser/ash/web_applications/media_app/media_app_guest_ui_config.h"
@@ -83,6 +84,7 @@ WebUIConfigList CreateConfigs() {
   register_config(std::make_unique<MediaAppGuestUIConfig>());
   register_config(std::make_unique<ash::UntrustedPersonalizationAppUIConfig>());
   register_config(std::make_unique<ash::HelpAppUntrustedUIConfig>());
+  register_config(std::make_unique<ash::CameraAppUntrustedUIConfig>());
   register_config(
       std::make_unique<ash::HelpAppKidsMagazineUntrustedUIConfig>());
   if (ash::features::IsProjectorEnabled())
