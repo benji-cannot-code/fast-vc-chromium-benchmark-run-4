b15ef9766f1d12a3d21d4e0ed54ac040c0760f11
Matt Jones <mdjones@chromium.org>
2022-02-08 17:25:33 +0000

    [Shopping] Ensure shopping subscriptions are synced with the service
    This patch checks that the shopping subscriptions that the backend
    service has and the ones that chrome has in bookmarks are in
    alignment. There are two main cases this patch handles:
    
    1. The backend has a subscription that is not in chrome's bookmarks.
       In this case, a request is made to the backend to unsubscribe the
       user from the product.
    
    2. The client has a bookmark flagged as tracked but the backend
       doesn't have the subscription. In this case, the local bookmark
       is set to be not tracked and requires the user to re-request
       price tracking.
    
    This validation runs on startup and whenever the user enters the
    bookmark manager.
    
    Bug: 1293885
    Change-Id: I94aeee71d173031bf69a63d0da72ccb385650197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440934
    Reviewed-by: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968394}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -39,6 +39,8 @@ import org.chromium.chrome.browser.ChromeActionModeHandler;
 import org.chromium.chrome.browser.ChromePowerModeVoter;
 import org.chromium.chrome.browser.app.tab_activity_glue.TabReparentingController;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge;
+import org.chromium.chrome.browser.bookmarks.PowerBookmarkUtils;
+import org.chromium.chrome.browser.commerce.shopping_list.ShoppingFeatures;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
 import org.chromium.chrome.browser.compositor.bottombar.OverlayPanel;
 import org.chromium.chrome.browser.compositor.bottombar.OverlayPanelManager;
@@ -85,6 +87,7 @@ import org.chromium.chrome.browser.share.ShareDelegate.ShareOrigin;
 import org.chromium.chrome.browser.share.ShareUtils;
 import org.chromium.chrome.browser.share.qrcode.QrCodeDialog;
 import org.chromium.chrome.browser.share.scroll_capture.ScrollCaptureManager;
+import org.chromium.chrome.browser.subscriptions.CommerceSubscriptionsServiceFactory;
 import org.chromium.chrome.browser.tab.AccessibilityVisibilityHandler;
 import org.chromium.chrome.browser.tab.AutofillSessionLifetimeController;
 import org.chromium.chrome.browser.tab.Tab;
@@ -659,6 +662,16 @@ public class RootUiCoordinator
 
         initMerchantTrustSignals();
         initScrollCapture();
+
+        // TODO(1293885): Remove this validator once we have an API on the backend that sends
+        //                success/failure information back.
+        if (ShoppingFeatures.isShoppingListEnabled() && mBookmarkBridgeSupplier.get() != null) {
+            PowerBookmarkUtils.validateBookmarkedCommerceSubscriptions(
+                    mBookmarkBridgeSupplier.get(),
+                    new CommerceSubscriptionsServiceFactory()
+                            .getForLastUsedProfile()
+                            .getSubscriptionsManager());
+        }
     }
 
     private void initMerchantTrustSignals() {
