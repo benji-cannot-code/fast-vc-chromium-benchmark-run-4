dd3ba9f3a35083b4669c41d7675c38f8335770ab
Mahmoud Rashad <mmrashad@google.com>
2022-02-08 14:31:28 +0000

    [sync] Remove obsolete kSyncConsiderEmptyWindowsSyncable flag
    No behavior is changed.
    
    Bug: 1294935
    Change-Id: I18ef16ec73208e0e2599db121ba65387dc0efdfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447345
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#968309}

--- chrome/browser/ui/sync/browser_synced_window_delegate.cc
+++ chrome/browser/ui/sync/browser_synced_window_delegate.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/sync/browser_synced_tab_delegate.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "components/sync_sessions/switches.h"
 
 BrowserSyncedWindowDelegate::BrowserSyncedWindowDelegate(Browser* browser)
     : browser_(browser) {}
@@ -74,7 +73,5 @@ bool BrowserSyncedWindowDelegate::ShouldSync() const {
   }
 
   // Do not sync windows which are about to be closed.
-  return !browser_->IsAttemptingToCloseBrowser() ||
-         !base::FeatureList::IsEnabled(
-             switches::kSyncConsiderEmptyWindowsSyncable);
+  return !browser_->IsAttemptingToCloseBrowser();
 }
