336bdc9c1e67f1d4326f3469140e3ff458a86258
Fergus Dall <sidereal@google.com>
2022-02-08 04:24:18 +0000

    crostini_upgrader: Split up the ERROR state
    This state was vague, and the information it did have was actually
    incorrect as it could only occur when there was an error backing up or
    restoring the container, not during the actual upgrade. Replace this
    with BACKUP_ERROR and RESTORE_ERROR states that have informative
    messages.
    
    Screenshots:
    https://screenshot.googleplex.com/7xBH4Nx7eBNuuXx
    https://screenshot.googleplex.com/AWgufCktY7fcd9a
    
    Bug: 1217902
    Change-Id: I1041ed46c137407e9824c0ef2e2fe1d6b4366214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3439387
    Reviewed-by: David Munro <davidmunro@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/main@{#968175}

--- chrome/browser/resources/chromeos/crostini_upgrader/app.js
+++ chrome/browser/resources/chromeos/crostini_upgrader/app.js
@@ -22,14 +22,15 @@ import {BrowserProxy} from './browser_proxy.js';
 const State = {
   PROMPT: 'prompt',
   BACKUP: 'backup',
+  BACKUP_ERROR: 'backupError',
   BACKUP_SUCCEEDED: 'backupSucceeded',
   PRECHECKS_FAILED: 'prechecksFailed',
   UPGRADING: 'upgrading',
   UPGRADE_ERROR: 'upgrade_error',
   OFFER_RESTORE: 'offerRestore',
   RESTORE: 'restore',
+  RESTORE_ERROR: 'restoreError',
   RESTORE_SUCCEEDED: 'restoreSucceeded',
-  ERROR: 'error',
   CANCELING: 'canceling',
   SUCCEEDED: 'succeeded',
 };
@@ -150,7 +151,7 @@ Polymer({
       }),
       callbackRouter.onBackupFailed.addListener(() => {
         assert(this.state_ === State.BACKUP);
-        this.state_ = State.ERROR;
+        this.state_ = State.BACKUP_ERROR;
       }),
       callbackRouter.precheckStatus.addListener((status) => {
         this.precheckStatus_ = status;
@@ -197,11 +198,11 @@ Polymer({
       }),
       callbackRouter.onRestoreFailed.addListener(() => {
         assert(this.state_ === State.RESTORE);
-        this.state_ = State.ERROR;
+        this.state_ = State.RESTORE_ERROR;
       }),
       callbackRouter.onCanceled.addListener(() => {
         if (this.state_ === State.RESTORE) {
-          this.state_ = State.ERROR;
+          this.state_ = State.RESTORE_ERROR;
           return;
         }
         this.closePage_();
@@ -279,8 +280,9 @@ Polymer({
         this.closePage_();
         break;
       case State.PRECHECKS_FAILED:
+      case State.BACKUP_ERROR:
       case State.UPGRADE_ERROR:
-      case State.ERROR:
+      case State.RESTORE_ERROR:
       case State.OFFER_RESTORE:
       case State.SUCCEEDED:
         this.closePage_();
@@ -422,6 +424,9 @@ Polymer({
       case State.BACKUP:
         titleId = 'backingUpTitle';
         break;
+      case State.BACKUP_ERROR:
+        titleId = 'backupErrorTitle';
+        break;
       case State.BACKUP_SUCCEEDED:
         titleId = 'backupSucceededTitle';
         break;
@@ -433,11 +438,13 @@ Polymer({
         break;
       case State.OFFER_RESTORE:
       case State.UPGRADE_ERROR:
-      case State.ERROR:
         titleId = 'errorTitle';
         break;
       case State.RESTORE:
         titleId = 'restoreTitle';
+        break;
+      case State.RESTORE_ERROR:
+        titleId = 'restoreErrorTitle';
         break;
       case State.RESTORE_SUCCEEDED:
         titleId = 'restoreSucceededTitle';
@@ -465,11 +472,7 @@ Polymer({
         return loadTimeData.getString('upgrade');
       case State.PRECHECKS_FAILED:
         return loadTimeData.getString('retry');
-      case State.UPGRADE_ERROR:
-      case State.ERROR:
-        return loadTimeData.getString('cancel');
       case State.SUCCEEDED:
-      case State.RESTORE_SUCCEEDED:
         return loadTimeData.getString('done');
       case State.OFFER_RESTORE:
         return loadTimeData.getString('restore');
@@ -484,10 +487,10 @@ Polymer({
    */
   getCancelButtonLabel_(state) {
     switch (state) {
-      case State.SUCCEEDED:
       case State.RESTORE_SUCCEEDED:
+      case State.BACKUP_ERROR:
       case State.UPGRADE_ERROR:
-      case State.ERROR:
+      case State.RESTORE_ERROR:
         return loadTimeData.getString('close');
       case State.PROMPT:
         return loadTimeData.getString('notNow');
@@ -509,6 +512,9 @@ Polymer({
         break;
       case State.BACKUP:
         messageId = 'backingUpMessage';
+        break;
+      case State.BACKUP_ERROR:
+        messageId = 'backupErrorMessage';
         break;
       case State.BACKUP_SUCCEEDED:
         messageId = 'backupSucceededMessage';
@@ -536,6 +542,9 @@ Polymer({
       case State.RESTORE:
         messageId = 'restoreMessage';
         break;
+      case State.RESTORE_ERROR:
+        messageId = 'restoreErrorMessage';
+        break;
       case State.RESTORE_SUCCEEDED:
         messageId = 'restoreSucceededMessage';
         break;
@@ -562,10 +571,11 @@ Polymer({
    */
   getIllustrationStyle_(state) {
     switch (state) {
+      case State.BACKUP_ERROR:
       case State.BACKUP_SUCCEEDED:
+      case State.RESTORE_ERROR:
       case State.RESTORE_SUCCEEDED:
       case State.PRECHECKS_FAILED:
-      case State.ERROR:
         return 'img-square-illustration';
     }
     return 'img-rect-illustration';
@@ -582,7 +592,8 @@ Polymer({
       case State.RESTORE_SUCCEEDED:
         return 'images/success_illustration.svg';
       case State.PRECHECKS_FAILED:
-      case State.ERROR:
+      case State.BACKUP_ERROR:
+      case State.RESTORE_ERROR:
         return 'images/error_illustration.png';
     }
     return 'images/linux_illustration.png';
