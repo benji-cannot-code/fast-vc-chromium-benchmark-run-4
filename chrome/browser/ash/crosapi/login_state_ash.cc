e6a708eeb0a1e2ff7ba877879eeceb4b397ad685
Gavin Williams <gavinwill@chromium.org>
2022-02-08 21:50:41 +0000

    shimless: Add RMA session state
    New session state used when running RMA app at device boot.
    Used by follow on cl
    https://chromium-review.googlesource.com/c/chromium/src/+/3339547
    
    Bug: 1198187, 1284389
    Test: Build and run Chrome
    
    Change-Id: Idc6fcc959f83e71b57f08b02e9ee21fcdc24d29d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3342329
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968569}

--- chrome/browser/ash/crosapi/login_state_ash.cc
+++ chrome/browser/ash/crosapi/login_state_ash.cc
@@ -38,6 +38,8 @@ mojom::SessionState ToMojo(session_manager::SessionState state) {
       return mojom::SessionState::kInSession;
     case session_manager::SessionState::LOCKED:
       return mojom::SessionState::kInLockScreen;
+    case session_manager::SessionState::RMA:
+      return mojom::SessionState::kInRmaScreen;
   }
   NOTREACHED();
   return mojom::SessionState::kUnknown;
