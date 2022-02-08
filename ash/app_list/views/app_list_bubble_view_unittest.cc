e7a613b6ca2712a65b2a92cd1334ad2c45c7db4a
Wen-Chien Wang <wcwang@chromium.org>
2022-02-08 22:28:28 +0000

    Animates app list reorder nudge when showing launcher
    Previously the toast container that contains the app list reorder nudge
    doesn't animates with other views when launcher is opening. Fixed the
    issue in this cl.
    
    Bug: 1277668
    Change-Id: I3a43088d6600c50adc425b51b8e4eb70946972d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445270
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968596}

--- ash/app_list/views/app_list_bubble_view_unittest.cc
+++ ash/app_list/views/app_list_bubble_view_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/views/app_list_bubble_apps_page.h"
 #include "ash/app_list/views/app_list_bubble_search_page.h"
 #include "ash/app_list/views/app_list_folder_view.h"
+#include "ash/app_list/views/app_list_toast_container_view.h"
 #include "ash/app_list/views/assistant/app_list_bubble_assistant_page.h"
 #include "ash/app_list/views/continue_section_view.h"
 #include "ash/app_list/views/continue_task_view.h"
@@ -155,6 +156,10 @@ class AppListBubbleViewTest : public AshTestBase {
     return GetAppListTestHelper()->GetBubbleRecentAppsView();
   }
 
+  AppListToastContainerView* GetToastContainerView() {
+    return GetAppsPage()->toast_container_for_test();
+  }
+
   ScrollableAppsGridView* GetAppsGridView() {
     return GetAppListTestHelper()->GetScrollableAppsGridView();
   }
@@ -253,6 +258,9 @@ TEST_F(AppListBubbleViewTest, OpeningBubbleTriggersAnimations) {
   ui::ScopedAnimationDurationScaleMode duration(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
 
+  // Enable app list nudge to test the animation.
+  GetAppListTestHelper()->DisableAppListNudge(false);
+
   // Show an app list with all sections.
   AddContinueSuggestionResult(4);
   AddRecentApps(5);
@@ -281,16 +289,24 @@ TEST_F(AppListBubbleViewTest, OpeningBubbleTriggersAnimations) {
       view->layer(), ui::LayerAnimationElement::AnimatableProperty::TRANSFORM));
   EXPECT_FLOAT_EQ(view->layer()->transform().To2dTranslation().y(), 40.f);
 
-  view = GetAppsGridView();
+  view = GetToastContainerView();
   EXPECT_TRUE(IsAnimatingProperty(
       view->layer(), ui::LayerAnimationElement::AnimatableProperty::TRANSFORM));
   EXPECT_FLOAT_EQ(view->layer()->transform().To2dTranslation().y(), 60.f);
+
+  view = GetAppsGridView();
+  EXPECT_TRUE(IsAnimatingProperty(
+      view->layer(), ui::LayerAnimationElement::AnimatableProperty::TRANSFORM));
+  EXPECT_FLOAT_EQ(view->layer()->transform().To2dTranslation().y(), 80.f);
 }
 
 TEST_F(AppListBubbleViewTest, OpeningBubbleWithSideShelfTriggersAnimations) {
   // Enable animations.
   ui::ScopedAnimationDurationScaleMode duration(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
+
+  // Enable app list nudge to test the animation.
+  GetAppListTestHelper()->DisableAppListNudge(false);
 
   // Enable side shelf.
   GetPrimaryShelf()->SetAlignment(ShelfAlignment::kRight);
@@ -323,16 +339,24 @@ TEST_F(AppListBubbleViewTest, OpeningBubbleWithSideShelfTriggersAnimations) {
       view->layer(), ui::LayerAnimationElement::AnimatableProperty::TRANSFORM));
   EXPECT_FLOAT_EQ(view->layer()->transform().To2dTranslation().y(), -40.f);
 
-  view = GetAppsGridView();
+  view = GetToastContainerView();
   EXPECT_TRUE(IsAnimatingProperty(
       view->layer(), ui::LayerAnimationElement::AnimatableProperty::TRANSFORM));
   EXPECT_FLOAT_EQ(view->layer()->transform().To2dTranslation().y(), -60.f);
+
+  view = GetAppsGridView();
+  EXPECT_TRUE(IsAnimatingProperty(
+      view->layer(), ui::LayerAnimationElement::AnimatableProperty::TRANSFORM));
+  EXPECT_FLOAT_EQ(view->layer()->transform().To2dTranslation().y(), -80.f);
 }
 
 TEST_F(AppListBubbleViewTest, ShowAnimationCreatesAndDestroysLayers) {
   // Enable animations.
   ui::ScopedAnimationDurationScaleMode duration(
       ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
+
+  // Enable app list nudge to test the animation.
+  GetAppListTestHelper()->DisableAppListNudge(false);
 
   // Show an app list with all sections.
   AddContinueSuggestionResult(4);
@@ -348,6 +372,8 @@ TEST_F(AppListBubbleViewTest, ShowAnimationCreatesAndDestroysLayers) {
   EXPECT_TRUE(recent_apps->layer());
   auto* separator = GetAppsPage()->separator_for_test();
   EXPECT_TRUE(separator->layer());
+  auto* toast_container = GetToastContainerView();
+  EXPECT_TRUE(toast_container->layer());
   auto* apps_grid_view = GetAppsGridView();
   EXPECT_TRUE(apps_grid_view->layer());
 
@@ -358,6 +384,7 @@ TEST_F(AppListBubbleViewTest, ShowAnimationCreatesAndDestroysLayers) {
   EXPECT_FALSE(continue_section->layer());
   EXPECT_FALSE(recent_apps->layer());
   EXPECT_FALSE(separator->layer());
+  EXPECT_FALSE(toast_container->layer());
 
   // The apps grid view always has a layer, it still exists.
   EXPECT_TRUE(apps_grid_view->layer());
