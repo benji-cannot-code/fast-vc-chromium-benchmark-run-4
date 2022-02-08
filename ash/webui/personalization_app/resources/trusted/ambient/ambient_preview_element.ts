9d00c91e57c778dd676e766af7e91861ece12f09
Jason Thai <jasontt@chromium.org>
2022-02-08 00:04:18 +0000

    personalization: add basic style to hub main page
    Add basic grid structure to hub main page
    
    Bug: b/217785868
    Test: Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I6ae821d2c0afced4f24fc6ba241fa1df0b323845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441297
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968090}

--- ash/webui/personalization_app/resources/trusted/ambient/ambient_preview_element.ts
+++ ash/webui/personalization_app/resources/trusted/ambient/ambient_preview_element.ts
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * screensaver.
  */
 
+import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import '../../common/styles.js';
+
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 export class AmbientPreview extends PolymerElement {
--- ash/webui/personalization_app/resources/trusted/wallpaper/wallpaper_preview_element.ts
+++ ash/webui/personalization_app/resources/trusted/wallpaper/wallpaper_preview_element.ts
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 import '/common/icons.js';
+import '/common/styles.js';
 import './styles.js';
 
 import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
