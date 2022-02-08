a5b159f5eb87112f8a35fff4225b0ed728c56865
Ben Kelly <wanderview@chromium.org>
2022-02-08 22:53:47 +0000

    URLPattern: Set unmatched optional groups to undefined instead of ''.
    This addresses the issues raised in:
    
    https://github.com/WICG/urlpattern/issues/162
    
    The main changes in this CL are:
    
    1. The webidl is modified to allow passed back undefined.  I'm told
       real webidl should support `(USVString or undefined)` here, but our
       webidl compiler does not support that.  So we use `any` instead.
    2. We must examine if the WTF::String is null or not before populating
       the returned array.  If its null, then we convert to undefined
       instead.
    
    Bug: 1292699
    Change-Id: I27a0be2567bb9ce4592ca15a5216fd5a58bdbf17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428631
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968611}

--- third_party/blink/renderer/modules/url_pattern/url_pattern.cc
+++ third_party/blink/renderer/modules/url_pattern/url_pattern.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_util.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_regexp.h"
+#include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_urlpatterninit_usvstring.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_url_pattern_component_result.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_url_pattern_init.h"
@@ -182,11 +183,33 @@ void ApplyInit(const URLPatternInit* init,
 }
 
 URLPatternComponentResult* MakeURLPatternComponentResult(
+    ScriptState* script_state,
     const String& input,
     const Vector<std::pair<String, String>>& group_values) {
   auto* result = URLPatternComponentResult::Create();
   result->setInput(input);
-  result->setGroups(group_values);
+
+  // Convert null WTF::String values to v8::Undefined.  We have to do this
+  // manually because the webidl compiler compiler does not currently
+  // support `(USVString or undefined)` in a record value.
+  // TODO(crbug.com/1293259): Use webidl `(USVString or undefined)` when
+  //                          available in the webidl compiler.
+  HeapVector<std::pair<String, ScriptValue>> v8_group_values;
+  v8_group_values.ReserveCapacity(group_values.size());
+  for (const auto& pair : group_values) {
+    v8::Local<v8::Value> v8_value;
+    if (pair.second.IsNull()) {
+      v8_value = v8::Undefined(script_state->GetIsolate());
+    } else {
+      v8_value = ToV8Traits<IDLUSVString>::ToV8(script_state, pair.second)
+                     .ToLocalChecked();
+    }
+    v8_group_values.emplace_back(
+        pair.first,
+        ScriptValue(script_state->GetIsolate(), std::move(v8_value)));
+  }
+
+  result->setGroups(std::move(v8_group_values));
   return result;
 }
 
@@ -339,33 +362,34 @@ URLPattern::URLPattern(Component* protocol,
       search_(search),
       hash_(hash) {}
 
-bool URLPattern::test(
-    const V8URLPatternInput* input,
-    const String& base_url,
-    ExceptionState& exception_state) const {
-  return Match(input, base_url, /*result=*/nullptr, exception_state);
+bool URLPattern::test(ScriptState* script_state,
+                      const V8URLPatternInput* input,
+                      const String& base_url,
+                      ExceptionState& exception_state) const {
+  return Match(script_state, input, base_url, /*result=*/nullptr,
+               exception_state);
 }
 
-bool URLPattern::test(
-    const V8URLPatternInput* input,
-    ExceptionState& exception_state) const {
-  return test(input, /*base_url=*/String(), exception_state);
+bool URLPattern::test(ScriptState* script_state,
+                      const V8URLPatternInput* input,
+                      ExceptionState& exception_state) const {
+  return test(script_state, input, /*base_url=*/String(), exception_state);
 }
 
-URLPatternResult* URLPattern::exec(
-    const V8URLPatternInput* input,
-    const String& base_url,
-    ExceptionState& exception_state) const {
+URLPatternResult* URLPattern::exec(ScriptState* script_state,
+                                   const V8URLPatternInput* input,
+                                   const String& base_url,
+                                   ExceptionState& exception_state) const {
   URLPatternResult* result = URLPatternResult::Create();
-  if (!Match(input, base_url, result, exception_state))
+  if (!Match(script_state, input, base_url, result, exception_state))
     return nullptr;
   return result;
 }
 
-URLPatternResult* URLPattern::exec(
-    const V8URLPatternInput* input,
-    ExceptionState& exception_state) const {
-  return exec(input, /*base_url=*/String(), exception_state);
+URLPatternResult* URLPattern::exec(ScriptState* script_state,
+                                   const V8URLPatternInput* input,
+                                   ExceptionState& exception_state) const {
+  return exec(script_state, input, /*base_url=*/String(), exception_state);
 }
 
 String URLPattern::protocol() const {
@@ -442,7 +466,8 @@ void URLPattern::Trace(Visitor* visitor) const {
   ScriptWrappable::Trace(visitor);
 }
 
-bool URLPattern::Match(const V8URLPatternInput* input,
+bool URLPattern::Match(ScriptState* script_state,
+                       const V8URLPatternInput* input,
                        const String& base_url,
                        URLPatternResult* result,
                        ExceptionState& exception_state) const {
@@ -571,19 +596,22 @@ bool URLPattern::Match(const V8URLPatternInput* input,
 
   result->setInputs(std::move(inputs));
 
-  result->setProtocol(
-      MakeURLPatternComponentResult(protocol, protocol_group_list));
-  result->setUsername(
-      MakeURLPatternComponentResult(username, username_group_list));
-  result->setPassword(
-      MakeURLPatternComponentResult(password, password_group_list));
-  result->setHostname(
-      MakeURLPatternComponentResult(hostname, hostname_group_list));
-  result->setPort(MakeURLPatternComponentResult(port, port_group_list));
-  result->setPathname(
-      MakeURLPatternComponentResult(pathname, pathname_group_list));
-  result->setSearch(MakeURLPatternComponentResult(search, search_group_list));
-  result->setHash(MakeURLPatternComponentResult(hash, hash_group_list));
+  result->setProtocol(MakeURLPatternComponentResult(script_state, protocol,
+                                                    protocol_group_list));
+  result->setUsername(MakeURLPatternComponentResult(script_state, username,
+                                                    username_group_list));
+  result->setPassword(MakeURLPatternComponentResult(script_state, password,
+                                                    password_group_list));
+  result->setHostname(MakeURLPatternComponentResult(script_state, hostname,
+                                                    hostname_group_list));
+  result->setPort(
+      MakeURLPatternComponentResult(script_state, port, port_group_list));
+  result->setPathname(MakeURLPatternComponentResult(script_state, pathname,
+                                                    pathname_group_list));
+  result->setSearch(
+      MakeURLPatternComponentResult(script_state, search, search_group_list));
+  result->setHash(
+      MakeURLPatternComponentResult(script_state, hash, hash_group_list));
 
   return true;
 }
