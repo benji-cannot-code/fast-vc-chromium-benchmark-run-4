ac56eeeeca3beae6930c20c987c9408fb0dc9ca5
Wei Lee <wtlee@google.com>
2022-02-08 08:50:39 +0000

    CCA: Remove unnecessary WaitableEvent in DeviceOperator
    Since now the CameraAppDevice is decoupled with the VCD lifetime, it is
    not necessary to wait for the camera ready before connecting to the
    CameraAppDevice. This CL removes the uncessary waiting.
    
    Bug: b:215074520
    Test: tast run [DUT] camera.CCAUI*
    Change-Id: I9bcf82e3c59a38445ee68cd42c9d4b97eb681966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3396343
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Inker Kuo <inker@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968236}

--- ash/webui/camera_app_ui/resources/js/mojo/device_operator.ts
+++ ash/webui/camera_app_ui/resources/js/mojo/device_operator.ts
@@ -16,7 +16,7 @@ import {
   ResolutionList,
   VideoConfig,
 } from '../type.js';
-import {CancelableEvent, WaitableEvent} from '../waitable_event.js';
+import {CancelableEvent} from '../waitable_event.js';
 
 import {
   AndroidInfoSupportedHardwareLevel,
@@ -115,21 +115,9 @@ function getMetadataData(
 let instance: DeviceOperator|null = null;
 
 /**
- * A ready event which should be signaled once the camera resource is ready.
- */
-const readyEvent = new WaitableEvent();
-
-/**
  * Job queue to sequentialize devices operations.
  */
 const operationQueue = new AsyncJobQueue();
-
-/**
- * Notified when the camera resource is ready.
- */
-export function notifyCameraResourceReady(): void {
-  readyEvent.signal();
-}
 
 /**
  * Operates video capture device through CrOS Camera App Mojo interface.
@@ -688,7 +676,6 @@ export class DeviceOperator {
    * @return The singleton instance.
    */
   static async getInstance(): Promise<DeviceOperator|null> {
-    await readyEvent.wait();
     if (instance === null) {
       instance = new DeviceOperator();
     }
