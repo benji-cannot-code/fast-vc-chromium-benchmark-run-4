8401f8fe2415dacc3d6120790e10062e458fa825
David Munro <davidmunro@google.com>
2022-02-08 16:19:35 +0000

    metrics/histograms: 43%/20s faster presubmits by using python 3
    Specifically, CheckChangeOnCommit went from 47 seconds to 27 seconds on
    a P920.
    
    Using Python3 gives the presubmit checks a nice speed boost, and python2
    is deprecated, so switch to 3. Note that USE_PYTHON3 in the
    PRESUBMIT.py only affects the PRESUBMIT.py file itself, so we still need
    to run the children with python3 explicitly. We could also run them
    in-process instead of spawning child processes, I'm not sure why it was
    done this way so I'm keeping it the same.
    
    Bug: 1294667
    Test: Run with/without change
    Change-Id: I3c1423ccd802c37f2170084e431e0d0ed2759c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445802
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968355}

--- tools/metrics/histograms/PRESUBMIT.py
+++ tools/metrics/histograms/PRESUBMIT.py
@@ -13,9 +13,11 @@ USE_PYTHON3 = True
 
 def GetPrettyPrintErrors(input_api, output_api, cwd, rel_path, results):
   """Runs pretty-print command for specified file."""
-  exit_code = input_api.subprocess.call(
-      [input_api.python_executable, 'pretty_print.py', rel_path, '--presubmit',
-       '--non-interactive'], cwd=cwd)
+  args = [
+      input_api.python3_executable, 'pretty_print.py', rel_path, '--presubmit',
+      '--non-interactive'
+  ]
+  exit_code = input_api.subprocess.call(args, cwd=cwd)
 
   if exit_code != 0:
     error_msg = (
@@ -26,7 +28,7 @@ def GetPrettyPrintErrors(input_api, output_api, cwd, rel_path, results):
 def GetPrefixErrors(input_api, output_api, cwd, rel_path, results):
   """Validates histogram prefixes in specified file."""
   exit_code = input_api.subprocess.call(
-      [input_api.python_executable, 'validate_prefix.py', rel_path], cwd=cwd)
+      [input_api.python3_executable, 'validate_prefix.py', rel_path], cwd=cwd)
 
   if exit_code != 0:
     error_msg = ('%s contains histogram(s) with disallowed prefix, please run '
@@ -37,7 +39,8 @@ def GetPrefixErrors(input_api, output_api, cwd, rel_path, results):
 def GetObsoleteXmlErrors(input_api, output_api, cwd, results):
   """Validates all histograms in the file are obsolete."""
   exit_code = input_api.subprocess.call(
-      [input_api.python_executable, 'validate_obsolete_histograms.py'], cwd=cwd)
+      [input_api.python3_executable, 'validate_obsolete_histograms.py'],
+      cwd=cwd)
 
   if exit_code != 0:
     error_msg = (
@@ -49,7 +52,7 @@ def GetObsoleteXmlErrors(input_api, output_api, cwd, results):
 def GetValidateHistogramsError(input_api, output_api, cwd, results):
   """Validates histograms format and index file."""
   exit_code = input_api.subprocess.call(
-      [input_api.python_executable, 'validate_format.py'], cwd=cwd)
+      [input_api.python3_executable, 'validate_format.py'], cwd=cwd)
 
   if exit_code != 0:
     error_msg = (
@@ -58,7 +61,7 @@ def GetValidateHistogramsError(input_api, output_api, cwd, results):
     results.append(output_api.PresubmitError(error_msg))
 
   exit_code = input_api.subprocess.call(
-      [input_api.python_executable, 'validate_histograms_index.py'], cwd=cwd)
+      [input_api.python3_executable, 'validate_histograms_index.py'], cwd=cwd)
 
   if exit_code != 0:
     error_msg = (
