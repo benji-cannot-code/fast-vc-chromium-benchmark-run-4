7d78c160f23210613e7c74eb7f89756030c845ad
François Degros <fdegros@chromium.org>
2022-02-08 10:36:28 +0000

    [Unzipper] Redact ZIP path from LOG message
    BUG=chromium:1287893
    TEST=autoninja -C out/Default components_unittests && out/Default/components_unittests --gtest_filter='UnzipTest.*'
    
    Change-Id: I0c5393d352371beda3e3e61bbf23d1ac0bdae39d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3446429
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968263}

--- components/services/unzip/public/cpp/unzip.cc
+++ components/services/unzip/public/cpp/unzip.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/post_task.h"
@@ -26,8 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 
 namespace unzip {
-
 namespace {
+
+std::string Redact(const base::FilePath& path) {
+  return LOG_IS_ON(INFO) ? "'" + path.AsUTF8Unsafe() + "'" : "(redacted)";
+}
 
 class UnzipFilter : public unzip::mojom::UnzipFilter {
  public:
@@ -195,8 +199,8 @@ void DoDetectEncoding(
     scoped_refptr<base::SequencedTaskRunner> background_task_runner) {
   base::File zip_file(zip_path, base::File::FLAG_OPEN | base::File::FLAG_READ);
   if (!zip_file.IsValid()) {
-    LOG(ERROR) << "Cannot open ZIP archive '" << zip_path
-               << "': " << base::File::ErrorToString(zip_file.error_details());
+    LOG(ERROR) << "Cannot open ZIP archive " << Redact(zip_path) << ": "
+               << base::File::ErrorToString(zip_file.error_details());
     callback_task_runner->PostTask(
         FROM_HERE,
         base::BindOnce(std::move(result_callback), UNKNOWN_ENCODING));
