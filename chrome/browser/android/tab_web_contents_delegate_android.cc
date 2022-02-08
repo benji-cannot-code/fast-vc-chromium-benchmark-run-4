db0c4016ee6318c71ff9b708267fc6262b57433e
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-02-08 08:04:28 +0000

    Remove recording of some obsolete renderer histograms
    Bug: None
    Change-Id: I5b3f09a30fde5b5be8e31940854141eb4bddd3ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3443347
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968225}

--- chrome/browser/android/tab_web_contents_delegate_android.cc
+++ chrome/browser/android/tab_web_contents_delegate_android.cc
@@ -657,9 +657,6 @@ void JNI_TabWebContentsDelegateAndroidImpl_OnRendererResponsive(
   if (!hung_renderer_infobar)
     return;
 
-  hung_renderer_infobar->delegate()
-      ->AsHungRendererInfoBarDelegate()
-      ->OnRendererResponsive();
   infobar_manager->RemoveInfoBar(hung_renderer_infobar);
 }
 
