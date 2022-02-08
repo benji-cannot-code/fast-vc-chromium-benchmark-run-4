16c5afbefdcbf0748ea1cf66cffde0a7e7c2afd3
Peng Huang <penghuang@chromium.org>
2022-02-08 16:07:09 +0000

    RawDraw: fix tile size calculation problems
    Sometime tile size calculator get an area which is long in one
    direction, but very short in the order direction, and the
    base_tile_size is (0x0). In this case, the calculator will split
    the area to many very small tiles. Having too many tiles affects
    raw draw performance. This CL will detect this case, not split
    those kind of areas.
    
    Bug: 1295275
    Change-Id: I8ba7d9f1bb9aca6a7b60c650666b6c3baf7f6f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441055
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968347}

--- cc/layers/picture_layer_impl_unittest.cc
+++ cc/layers/picture_layer_impl_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cxx17_backports.h"
 #include "base/location.h"
 #include "base/memory/raw_ptr.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "cc/animation/animation_host.h"
 #include "cc/base/math_util.h"
@@ -42,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/tile_draw_quad.h"
 #include "components/viz/test/begin_frame_args_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/test/geometry_util.h"
@@ -5503,6 +5505,39 @@ TEST_F(TileSizeTest, GPUTileSizes) {
   result = layer->CalculateTileSize(gfx::Size(500, 499));
   EXPECT_EQ(result.width(), 512);
   EXPECT_EQ(result.height(), 512);  // 500 + 2, 32-byte aligned.
+}
+
+TEST_F(TileSizeTest, RawDrawTileSizes) {
+  base::test::ScopedFeatureList scoped_feature_list(features::kRawDraw);
+  SetupPendingTree();
+  auto* layer = pending_layer();
+
+  host_impl()->active_tree()->SetDeviceViewportRect(gfx::Rect(1000, 1000));
+  gfx::Size result;
+
+  host_impl()->CommitComplete();
+
+  // Gpu-RawDraw-rasterization uses 100%
+  // std::max({viewport-width, viewport-height, 2280}) tiles.
+  EXPECT_EQ(host_impl()->gpu_rasterization_status(),
+            GpuRasterizationStatus::ON);
+  gfx::Rect viewport_rect(2000, 2000);
+  host_impl()->active_tree()->SetDeviceViewportRect(viewport_rect);
+  layer->set_gpu_raster_max_texture_size(viewport_rect.size());
+  host_impl()->NotifyReadyToActivate();
+
+  gfx::Size content_bounds(10000, 10000);
+  result = layer->CalculateTileSize(content_bounds);
+  EXPECT_EQ(result, gfx::Size(2048, 2048));
+
+  viewport_rect = gfx::Rect(1000, 1000);
+  host_impl()->active_tree()->SetDeviceViewportRect(viewport_rect);
+  layer->set_gpu_raster_max_texture_size(gfx::Size());
+  host_impl()->NotifyReadyToActivate();
+  content_bounds = gfx::Size(1600, 100);
+
+  result = layer->CalculateTileSize(content_bounds);
+  EXPECT_EQ(result, gfx::Size(1600, 128));
 }
 
 class HalfWidthTileTest : public PictureLayerImplTest {};
