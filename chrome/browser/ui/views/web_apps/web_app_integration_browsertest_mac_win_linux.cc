54e03bd4a6c5ffbfcbc99bb5e9d53015b389a56a
Dibyajyoti Pal <dibyapal@chromium.org>
2022-02-08 18:03:42 +0000

    dPWA: Disable InstallPolicyApp tests
    This CL disables the newly added policy app install tests while the
    root cause for the slowness is being investigated to reduce
    occurrence of flaky test failures.
    
    Bug: 1294464
    Change-Id: I68c011006f4231d8931165a727548e394b3d75b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447534
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968413}

--- chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac_win_linux.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac_win_linux.cc
@@ -58,7 +58,7 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
-                       CheckRunOnOsLoginWorksOnPolicyAppAllowed) {
+                       DISABLED_CheckRunOnOsLoginWorksOnPolicyAppAllowed) {
   helper_.InstallPolicyAppOsLoginModeAllowed("SiteA");
   helper_.EnableRunOnOSLoginMode("SiteA");
   helper_.CheckRunOnOSLoginModeEnabled("SiteA");
@@ -69,7 +69,7 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
-                       CheckRunOnOsLoginWorksOnPolicyAppBlocked) {
+                       DISABLED_CheckRunOnOsLoginWorksOnPolicyAppBlocked) {
   helper_.InstallPolicyAppOsLoginModeBlocked("SiteA");
   helper_.EnableRunOnOSLoginMode("SiteA");
   helper_.CheckRunOnOSLoginModeDisabled("SiteA");
