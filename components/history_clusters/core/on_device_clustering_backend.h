304dd24a2ff8a5b7de66a8a835d796b25ff8c235
Tarun Bansal <tbansal@google.com>
2022-02-08 19:13:58 +0000

    Add engagement score cache
    The use of cache is behind a feature and can be remotely disabled.
    
    BugL1295326
    
    Change-Id: I5ce058132f0a2017f4fea770452a63a0f01f54db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440553
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968472}

--- components/history_clusters/core/on_device_clustering_backend.h
+++ components/history_clusters/core/on_device_clustering_backend.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
+#include "base/containers/lru_cache.h"
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -77,6 +78,11 @@ class OnDeviceClusteringBackend : public ClusteringBackend {
 
   // The task runner to run all clustering passes on.
   scoped_refptr<base::SequencedTaskRunner> background_task_runner_;
+
+  // Last time |engagement_score_cache_| was refreshed.
+  base::TimeTicks engagement_score_cache_last_refresh_timestamp_;
+  // URL host to score mapping.
+  base::HashingLRUCache<std::string, float> engagement_score_cache_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
