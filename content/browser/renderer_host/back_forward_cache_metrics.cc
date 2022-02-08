c465394ff011dc5fcca0124fdba7a0f85f466296
Rakina Zata Amni <rakina@chromium.org>
2022-02-08 06:50:47 +0000

    Don't do proactive BI swap on non-primary main frame navigations
    Non-primary pages are not BFCacheable, so there's no point in trying
    to do a proactive BI swap on them. Adds a new no-swap reason,
    kNo_NotPrimaryMainFrame (since this only applies to proactive BI swaps,
    while kNo_NotMainFrame applies to BI swaps in general).
    
    Also updates a test for crrev.com/c/3398220.
    
    Change-Id: I480eba43ad62cd467041071e895e583ba4ca859c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419478
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968204}

--- content/browser/renderer_host/back_forward_cache_metrics.cc
+++ content/browser/renderer_host/back_forward_cache_metrics.cc
@@ -533,6 +533,7 @@ bool BackForwardCacheMetrics::DidSwapBrowsingInstance() const {
     case ShouldSwapBrowsingInstance::kNo_HasNotComittedAnyNavigation:
     case ShouldSwapBrowsingInstance::
         kNo_UnloadHandlerExistsOnSameSiteNavigation:
+    case ShouldSwapBrowsingInstance::kNo_NotPrimaryMainFrame:
       return false;
     case ShouldSwapBrowsingInstance::kYes_ForceSwap:
     case ShouldSwapBrowsingInstance::kYes_CrossSiteProactiveSwap:
