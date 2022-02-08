180f91f1a826e59575add59a1cff0e0f5d58b42a
David Sanders <dsanders11@ucsbalum.com>
2022-02-08 16:11:13 +0000

    Move Windows-only include into #if
    Bug: 242216
    Change-Id: I0095ba420cbf63c2b5d8e135b292b7814d31839c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445099
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968350}

--- content/browser/webauth/is_uvpaa.cc
+++ content/browser/webauth/is_uvpaa.cc
@@ -8,16 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "content/browser/webauth/authenticator_environment_impl.h"
 #include "content/public/browser/authenticator_request_client_delegate.h"
 #include "content/public/common/content_client.h"
 #include "device/fido/features.h"
 
 #if BUILDFLAG(IS_MAC)
+#include "content/public/browser/content_browser_client.h"
 #include "device/fido/mac/authenticator.h"
 #endif
 
 #if BUILDFLAG(IS_WIN)
+#include "content/browser/webauth/authenticator_environment_impl.h"
 #include "device/fido/win/authenticator.h"
 #endif
 
