bfe21192347a890e2d7dd7ccdde54fb7adf1e421
Vidhan <vidhanj@google.com>
2022-02-08 15:05:53 +0000

    [Code Health] Use RegisterMessageCallback in InspectMessageHandler
    This CL replaces the usages WebUI::DeprecatedMessageCallback to
    WebUI::MessageCallback since base::ListValue is deprecated.
    
    Bug: 1187062, 1243386
    Change-Id: I7821072013682cb01f19480a7eed511d8985aebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440995
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#968322}

--- chrome/browser/ui/webui/inspect_ui.h
+++ chrome/browser/ui/webui/inspect_ui.h
@@ -17,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_data_source.h"
 
-namespace base {
-class Value;
-}
-
 namespace content {
 class DevToolsAgentHost;
 }
