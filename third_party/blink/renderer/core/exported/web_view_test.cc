188bd5f738c685391b230542219733cab13fb26d
Tsuyoshi Horo <horo@chromium.org>
2022-02-08 04:03:33 +0000

    Remove ChromeClient::OverrideVisibleRectForMainFrame()
    This method is not called after crrev.com/c/2861137
    
    And also this CL removes DevToolsEmulator::OverrideVisibleRect().
    
    Bug: 1203584
    Change-Id: Ibcb09fce3494d6762a1acdc37971e46e76c0edf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3439225
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968169}

--- third_party/blink/renderer/core/exported/web_view_test.cc
+++ third_party/blink/renderer/core/exported/web_view_test.cc
@@ -5176,11 +5176,6 @@ TEST_F(WebViewTest, ForceAndResetViewport) {
   TransformationMatrix expected_matrix;
   expected_matrix.MakeIdentity();
   EXPECT_EQ(expected_matrix, web_view_impl->GetDeviceEmulationTransform());
-  {
-    gfx::Rect visible_rect(1, 2, 3, 4);
-    dev_tools_emulator->OverrideVisibleRect(gfx::Size(), &visible_rect);
-    EXPECT_EQ(gfx::Rect(1, 2, 3, 4), visible_rect);  // Was modified.
-  }
 
   // Override applies transform, sets visible rect, and disables
   // visual viewport clipping.
@@ -5188,33 +5183,18 @@ TEST_F(WebViewTest, ForceAndResetViewport) {
       dev_tools_emulator->ForceViewportForTesting(gfx::PointF(50, 55), 2.f);
   expected_matrix.MakeIdentity().Scale(2.f).Translate(-50, -55);
   EXPECT_EQ(expected_matrix, matrix);
-  {
-    gfx::Rect visible_rect(1, 2, 3, 4);
-    dev_tools_emulator->OverrideVisibleRect(gfx::Size(100, 150), &visible_rect);
-    EXPECT_EQ(gfx::Rect(50, 55, 100, 150), visible_rect);
-  }
 
   // Setting new override discards previous one.
   matrix = dev_tools_emulator->ForceViewportForTesting(gfx::PointF(5.4f, 10.5f),
                                                        1.5f);
   expected_matrix.MakeIdentity().Scale(1.5f).Translate(-5.4f, -10.5f);
   EXPECT_EQ(expected_matrix, matrix);
-  {
-    gfx::Rect visible_rect(1, 2, 3, 4);
-    dev_tools_emulator->OverrideVisibleRect(gfx::Size(100, 150), &visible_rect);
-    EXPECT_EQ(gfx::Rect(5, 10, 101, 151), visible_rect);  // Was modified.
-  }
 
   // Clearing override restores original transform, visible rect and
   // visual viewport clipping.
   matrix = dev_tools_emulator->ResetViewportForTesting();
   expected_matrix.MakeIdentity();
   EXPECT_EQ(expected_matrix, matrix);
-  {
-    gfx::Rect visible_rect(1, 2, 3, 4);
-    dev_tools_emulator->OverrideVisibleRect(gfx::Size(), &visible_rect);
-    EXPECT_EQ(gfx::Rect(1, 2, 3, 4), visible_rect);  // Not modified.
-  }
 }
 
 TEST_F(WebViewTest, ViewportOverrideIntegratesDeviceMetricsOffsetAndScale) {
@@ -5249,7 +5229,6 @@ TEST_F(WebViewTest, ViewportOverrideAdaptsToScaleAndScroll) {
   SetViewportSize(gfx::Size(100, 150));
   LocalFrameView* frame_view =
       web_view_impl->MainFrameImpl()->GetFrame()->View();
-  DevToolsEmulator* dev_tools_emulator = web_view_impl->GetDevToolsEmulator();
 
   TransformationMatrix expected_matrix;
   expected_matrix.MakeIdentity();
@@ -5272,12 +5251,6 @@ TEST_F(WebViewTest, ViewportOverrideAdaptsToScaleAndScroll) {
       .Translate(100, 150)
       .Scale(1. / 1.5f);
   EXPECT_EQ(expected_matrix, web_view_impl->GetDeviceEmulationTransform());
-  // Scale is irrelevant for visible rect.
-  {
-    gfx::Rect visible_rect(1, 2, 3, 4);
-    dev_tools_emulator->OverrideVisibleRect(gfx::Size(100, 150), &visible_rect);
-    EXPECT_EQ(gfx::Rect(50 - 100, 55 - 150, 100, 150), visible_rect);
-  }
 
   // Transform adapts to scroll changes.
   frame_view->LayoutViewport()->SetScrollOffset(
@@ -5289,12 +5262,6 @@ TEST_F(WebViewTest, ViewportOverrideAdaptsToScaleAndScroll) {
       .Translate(50, 55)
       .Scale(1. / 1.5f);
   EXPECT_EQ(expected_matrix, web_view_impl->GetDeviceEmulationTransform());
-  // Visible rect adapts to scroll change.
-  {
-    gfx::Rect visible_rect(1, 2, 3, 4);
-    dev_tools_emulator->OverrideVisibleRect(gfx::Size(100, 150), &visible_rect);
-    EXPECT_EQ(gfx::Rect(50 - 50, 55 - 55, 100, 150), visible_rect);
-  }
 
   // Transform adapts to page scale changes.
   web_view_impl->SetPageScaleFactor(2.f);
@@ -5304,12 +5271,6 @@ TEST_F(WebViewTest, ViewportOverrideAdaptsToScaleAndScroll) {
       .Translate(50, 55)
       .Scale(1. / 2.f);
   EXPECT_EQ(expected_matrix, web_view_impl->GetDeviceEmulationTransform());
-  // Visible rect doesn't change.
-  {
-    gfx::Rect visible_rect(1, 2, 3, 4);
-    dev_tools_emulator->OverrideVisibleRect(gfx::Size(100, 150), &visible_rect);
-    EXPECT_EQ(gfx::Rect(50 - 50, 55 - 55, 100, 150), visible_rect);
-  }
 }
 
 TEST_F(WebViewTest, ResizeForPrintingViewportUnits) {
--- third_party/blink/renderer/core/inspector/dev_tools_emulator.h
+++ third_party/blink/renderer/core/inspector/dev_tools_emulator.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 class PointF;
-class Rect;
 }  // namespace gfx
 
 namespace blink {
@@ -70,14 +69,6 @@ class CORE_EXPORT DevToolsEmulator final
   // frame. Returns an updated emulation transform for a viewport override, and
   // should only be called when HasViewportOverride() is true.
   TransformationMatrix MainFrameScrollOrScaleChanged();
-
-  // Rewrites the |visible_rect| to the area of the devtools custom viewport if
-  // it is enabled. Otherwise, leaves |visible_rect| unchanged. Takes as input
-  // the size of the viewport, which gives an upper bound on the size of the
-  // area that is visible. The |viewport_size| is physical pixels if
-  // UseZoomForDSF() is enabled, or DIP otherwise.
-  void OverrideVisibleRect(const gfx::Size& viewport_size,
-                           gfx::Rect* visible_rect) const;
 
   // Returns the scale used to convert incoming input events while emulating
   // device metics.
