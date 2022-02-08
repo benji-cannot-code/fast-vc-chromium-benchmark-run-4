cb7a84de5f9b080f409f464aa1d2afcdaaaea689
Tim van der Lippe <tvanderlippe@chromium.org>
2022-02-08 17:08:29 +0000

    Remove browser discovery page
    Follow-up to https://crrev.com/c/3436827
    
    R=caseq@chromium.org
    
    Change-Id: If016a6ba2fa714029fb0fe324b750a2848042dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440208
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Tim Van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968384}

--- chrome/browser/android/devtools_manager_delegate_android.h
+++ chrome/browser/android/devtools_manager_delegate_android.h
@@ -29,7 +29,6 @@ class DevToolsManagerDelegateAndroid : public content::DevToolsManagerDelegate {
   content::DevToolsAgentHost::List RemoteDebuggingTargets() override;
   scoped_refptr<content::DevToolsAgentHost> CreateNewTarget(
       const GURL& url) override;
-  std::string GetDiscoveryPageHTML() override;
   bool IsBrowserTargetDiscoverable() override;
 };
 
