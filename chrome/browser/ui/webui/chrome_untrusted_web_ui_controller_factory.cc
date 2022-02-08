264edaba8d15f35cb93437b3b88154ad1d9d36bb
Justin DeWitt <dewittj@chromium.org>
2022-02-08 06:34:48 +0000

    Add chrome-untrusted webui for desktop feed.
    To begin prototyping we won't put this in any Chrome UI; adding
    it will come as a later step.
    
    Bug: 1292559
    Change-Id: Ibb6665009e94cdfa790f1f7bf13d69cedc4433aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3336513
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968200}

--- chrome/browser/ui/webui/chrome_untrusted_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_untrusted_web_ui_controller_factory.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/webui/video_tutorials/video_player_ui.h"
+#else
+#include "chrome/browser/ui/webui/feed/feed_ui_config.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -70,6 +72,8 @@ WebUIConfigList CreateConfigs() {
 
 #if BUILDFLAG(IS_ANDROID)
   register_config(std::make_unique<video_tutorials::VideoPlayerUIConfig>());
+#else
+  register_config(std::make_unique<feed::FeedUIConfig>());
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -97,7 +101,6 @@ WebUIConfigList CreateConfigs() {
   register_config(std::make_unique<ash::UntrustedSampleSystemWebAppUIConfig>());
 #endif  // !defined(OFFICIAL_BUILD)
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
   return config_list;
 }
 
