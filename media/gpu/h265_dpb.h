d344e212f152457c231bb9e55cc060314f317e79
Qiu Jianlin <jianlin.qiu@intel.com>
2022-02-08 03:29:05 +0000

    Video: implement D3D11VA HEVC decode accelerator on Windows.
    This implements the D3D11VA HEVC decode accelerator for both clear
    and encrypted content. Needs to set enable_platform_hevc,
    enable_platform_hevc_decoding and enable_platform_encrypted_hevc
    to true for building this feature in.
    
    Instantiating of the accelerator will be added by follow up CLs
    after HEVC playback feature flags are ready.
    
    Bug: 1286132, webrtc:13485
    Change-Id: I638a4bcb541c6fd9f8cd08c7064113a235a61bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3400213
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#968162}

--- media/gpu/h265_dpb.h
+++ media/gpu/h265_dpb.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 class VaapiH265Picture;
+class D3D11H265Picture;
 
 // A picture (a frame or a field) in the H.265 spec sense.
 // See spec at http://www.itu.int/rec/T-REC-H.265
@@ -29,6 +30,7 @@ class MEDIA_GPU_EXPORT H265Picture : public CodecPicture {
   H265Picture& operator=(const H265Picture&) = delete;
 
   virtual VaapiH265Picture* AsVaapiH265Picture();
+  virtual D3D11H265Picture* AsD3D11H265Picture();
 
   enum ReferenceType {
     kUnused = 0,
