8bf3c69f6d84411eb9d6932c2bfb2c3332286b37
Vigaash Sivasothy <vigaash@google.com>
2022-02-08 21:01:48 +0000

    SuperSize: Add url of review via command line.
    Bug: 978305
    Change-Id: I7b5780d5213b450d7b20639588523c619b691abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445340
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Vigaash Sivasothy <vigaash@google.com>
    Cr-Commit-Position: refs/heads/main@{#968536}

--- tools/binary_size/libsupersize/archive.py
+++ tools/binary_size/libsupersize/archive.py
@@ -222,7 +222,7 @@ def LoadAndPostProcessDeltaSizeInfo(path, file_obj=None):
   return before_size_info, after_size_info
 
 
-def CreateBuildConfig(output_directory, source_directory):
+def CreateBuildConfig(output_directory, source_directory, url=None, title=None):
   """Creates the dict to use for SizeInfo.build_info."""
   logging.debug('Constructing build_config')
   build_config = {}
@@ -233,6 +233,10 @@ def CreateBuildConfig(output_directory, source_directory):
   git_rev = _DetectGitRevision(source_directory)
   if git_rev:
     build_config[models.BUILD_CONFIG_GIT_REVISION] = git_rev
+  if url is not None:
+    build_config[models.BUILD_CONFIG_URL] = url
+  if title is not None:
+    build_config[models.BUILD_CONFIG_TITLE] = title
 
   return build_config
 
@@ -525,6 +529,9 @@ def _AddContainerArguments(parser, is_top_args=False):
 
 def AddArguments(parser):
   parser.add_argument('size_file', help='Path to output .size file.')
+  parser.add_argument('--title',
+                      help='Value for the "title" build_config entry.')
+  parser.add_argument('--url', help='Value for the "url" build_config entry.')
   _AddContainerArguments(parser, is_top_args=True)
 
 
@@ -1042,7 +1049,9 @@ def Run(top_args, on_config_error):
     container_specs = _FilterContainerSpecs(container_specs, container_re)
 
     build_config = CreateBuildConfig(top_args.output_directory,
-                                     top_args.source_directory)
+                                     top_args.source_directory,
+                                     url=top_args.url,
+                                     title=top_args.title)
     size_info = CreateSizeInfo(container_specs,
                                build_config,
                                apk_file_manager,
