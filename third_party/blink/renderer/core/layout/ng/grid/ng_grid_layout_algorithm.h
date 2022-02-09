db45382277df1a0aefe4db5512b5476f114ee3a1
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-02-09 18:54:51 +0000

    [GridFragmentation] Add OOF candidates.
    This patch adds the OOF candidates with the correct static position
    to the grid fragment.
    
    This places all the OOF candidates into the last fragment - as this
    has the most up to date grid geometry information.
    
    Bug: 614667
    Change-Id: I0b6a19789de2adc8a286a817650d60e6cfa6eb6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448032
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969026}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.h
@@ -51,7 +51,6 @@ class CORE_EXPORT NGGridLayoutAlgorithm
       const NGGridLayoutData& layout_data,
       const NGBoxStrut& borders,
       const LogicalSize& border_box_size,
-      const LayoutUnit block_size,
       GridItemData* out_of_flow_item);
 
   // Helper that computes tracks sizes in a given range.
@@ -225,7 +224,6 @@ class CORE_EXPORT NGGridLayoutAlgorithm
       LayoutUnit* start_offset,
       LayoutUnit* size) const;
 
-  LogicalSize border_box_size_;
   LogicalSize grid_available_size_;
   LogicalSize grid_min_available_size_;
   LogicalSize grid_max_available_size_;
