6ff856d2533beacea06779728b0267d40822877a
Oriol Brufau <obrufau@igalia.com>
2022-02-09 01:03:43 +0000

    [a11y] A11y shouldn't have a copy of Element::IsFocusableStyle()
    To check focusability, AXObject::IsFocusableStyleUsingBestAvailableState
    uses element->IsFocusableStyle() if element doesn't need a layout tree
    update, since that will use element->GetLayoutObject().
    
    But if that's not possible, then it basically had a copy of the base
    Element::IsFocusableStyle, except that it would use the LayoutObject
    from AXObject::GetLayoutObject() instead of element->GetLayoutObject().
    
    But in r963655 I changed Element::IsFocusableStyle(), and the AXObject
    copy got out of sync.
    
    So this patch moves the inner logic of Element::IsFocusableStyle() into
    a new Element::IsBaseElementFocusableStyle(LayoutObject*) method, which
    can also be called from a11y with the AXObject::GetLayoutObject().
    
    Bug: none
    Change-Id: I09008c0f0efac06e4f9833f671900c5134cbf0d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3419436
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#968680}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -685,7 +685,11 @@ bool Element::IsFocusableStyle() const {
   DCHECK(
       !GetDocument().IsActive() || GetDocument().InStyleRecalc() ||
       !GetDocument().NeedsLayoutTreeUpdateForNodeIncludingDisplayLocked(*this));
+  return IsBaseElementFocusableStyle(GetLayoutObject());
+}
 
+bool Element::IsBaseElementFocusableStyle(
+    const LayoutObject* layout_object) const {
   if (LayoutObject* layout_object = GetLayoutObject())
     return layout_object->StyleRef().IsFocusable();
 
