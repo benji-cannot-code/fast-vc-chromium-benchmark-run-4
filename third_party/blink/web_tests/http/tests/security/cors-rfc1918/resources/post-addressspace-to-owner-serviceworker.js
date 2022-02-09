fa132985649a0d2f0d482ac03497f614c1b30621
Titouan Rigoudy <titouan@chromium.org>
2022-02-09 14:42:26 +0000

    [Private Network Access] Remove `addressSpace` from IDL.
    This attribute has been removed from the specification for a long time:
    https://github.com/WICG/private-network-access/issues/21
    
    Also remove the old CORS-RFC1918 web tests exercising this attribute, as
    well as the virtual test suites running only these tests. One of these
    tests was flaky, so this CL also fixed crbug.com/1267552.
    
    Bug: chromium:1204028
    Change-Id: I47eb8c0d0eeac838ec9a584c8505459347baac6f
    Fixed: chromium:1267552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3442962
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968916}

--- third_party/blink/web_tests/http/tests/security/cors-rfc1918/resources/post-addressspace-to-owner-serviceworker.js
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.addEventListener('message', e => {
-    e.ports[0].postMessage({
-        "messageType": "AddressSpaceMessage",
-        "origin": self.location.origin,
-        "addressSpace": self.addressSpace
-    });
-});
