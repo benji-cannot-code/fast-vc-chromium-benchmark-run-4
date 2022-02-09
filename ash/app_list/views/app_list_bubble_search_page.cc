8f817c67576d8fb900a061c00de0b3fbd46bef58
James Cook <jamescook@chromium.org>
2022-02-09 19:52:09 +0000

    ash: Add fade-in for transition from launcher apps page to search page
    Fade in the search page after a short (50ms) delay. This "softens" the
    motion of the search results as they slide up.
    
    The animation spec also calls for a delay before the search results
    start the slide up. However, there is already a natural delay due to
    the search backend computing the results, so I did not add another
    artificial delay. The final result looks fine to me. See video in bug.
    
    Bug: 1294346
    Test: added to ash_unittests
    Change-Id: Ib504b3a0b8a3d26fe979ce5d45d9dd16bfb9b20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447256
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969061}

--- ash/app_list/views/app_list_bubble_search_page.cc
+++ ash/app_list/views/app_list_bubble_search_page.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/app_list/views/productivity_launcher_search_view.h"
 #include "base/check_op.h"
+#include "base/time/time.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_type.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
@@ -37,6 +38,26 @@ AppListBubbleSearchPage::AppListBubbleSearchPage(
 }
 
 AppListBubbleSearchPage::~AppListBubbleSearchPage() = default;
+
+void AppListBubbleSearchPage::AnimateShowPage() {
+  SetVisible(true);
+
+  // If skipping animations, just update visibility.
+  if (ui::ScopedAnimationDurationScaleMode::is_zero())
+    return;
+
+  ui::Layer* layer = search_view_->GetPageAnimationLayer();
+  DCHECK_EQ(layer->type(), ui::LAYER_TEXTURED);
+
+  views::AnimationBuilder()
+      .SetPreemptionStrategy(
+          ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET)
+      .Once()
+      .SetOpacity(layer, 0.f)
+      .At(base::Milliseconds(50))
+      .SetDuration(base::Milliseconds(100))
+      .SetOpacity(layer, 1.f);
+}
 
 void AppListBubbleSearchPage::AnimateHidePage() {
   // If skipping animations, just update visibility.
@@ -72,6 +93,10 @@ void AppListBubbleSearchPage::AnimateHidePage() {
       .SetDuration(kHideAnimationDuration)
       .SetOpacity(layer, 0.f)
       .SetTransform(layer, translate_up);
+}
+
+void AppListBubbleSearchPage::AbortAllAnimations() {
+  search_view_->GetPageAnimationLayer()->GetAnimator()->AbortAllAnimations();
 }
 
 ui::Layer* AppListBubbleSearchPage::GetPageAnimationLayerForTest() {
