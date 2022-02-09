e69b759013490127bb0213bb2e1477442a464b76
Michael Thiessen <mthiesse@chromium.org>
2022-02-09 22:44:25 +0000

    Secure ChromotingDownloadManager BroadcastHandler
    This broadcast was exported, and so callable by any app on the device.
    The DownloadManager holds a permission callers can check to ensure the
    Broadcast is coming from the DownloadManager.
    
    Bug: 1278830
    Change-Id: Ib442900e356e9ca2132ea48f63e024b8736ee76e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449198
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969156}

--- remoting/android/java/src/org/chromium/chromoting/ChromotingDownloadManager.java
+++ remoting/android/java/src/org/chromium/chromoting/ChromotingDownloadManager.java
@@ -26,6 +26,10 @@ import java.util.Set;
  * Class that manages download operation for Chromoting activity.
  */
 public class ChromotingDownloadManager {
+    // Undocumented outside of Android source, but held by the Android Download Manager since at
+    // least Android M in order to send download completed broadcasts.
+    private static final String PERMISSION_SEND_DOWNLOAD_COMPLETED_INTENTS =
+            "android.permission.SEND_DOWNLOAD_COMPLETED_INTENTS";
     /**
      * Callback for download manager. This will be executed on application's main thread.
      */
@@ -109,8 +113,9 @@ public class ChromotingDownloadManager {
                 }
             };
 
-            mActivity.registerReceiver(mDownloadReceiver,
-                    new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+            ContextUtils.registerExportedBroadcastReceiver(mActivity, mDownloadReceiver,
+                    new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE),
+                    PERMISSION_SEND_DOWNLOAD_COMPLETED_INTENTS);
         }
     }
 
