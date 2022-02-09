918672206d953a637184347f8a569f32c3eb7773
David Munro <davidmunro@google.com>
2022-02-09 05:51:05 +0000

    guest_os: Add a stub Guest OS Service
    Bug: 1293229
    Test: Build
    Change-Id: I8ae45e35b6d25e6217d75f5b0eb23598ca42bef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447712
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/main@{#968766}

---
+++ chrome/browser/ash/guest_os/public/guest_os_service.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_GUEST_OS_PUBLIC_GUEST_OS_SERVICE_H_
+#define CHROME_BROWSER_ASH_GUEST_OS_PUBLIC_GUEST_OS_SERVICE_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+
+class Profile;
+
+namespace guest_os {
+
+// A service to hold the subservices that make up the Guest OS API surface.
+// NOTE: We don't start at browser startup, instead being created on-demand. At
+// some point we may change that, but for now creating is cheap, we won't always
+// be used (e.g. if the guest os flag isn't set), and we don't need to listen
+// for events - everything we care about knows how to access us via KeyedService
+// machinery.
+class GuestOsService : public KeyedService {
+ public:
+  GuestOsService();
+  ~GuestOsService() override;
+
+  // Helper method to get the service instance for the given profile.
+  static GuestOsService* GetForProfile(Profile* profile);
+};
+
+}  // namespace guest_os
+
+#endif  // CHROME_BROWSER_ASH_GUEST_OS_PUBLIC_GUEST_OS_SERVICE_H_
