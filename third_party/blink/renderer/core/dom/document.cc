f06b4a672833408624504d4a5cda0f6cfa8d7110
Miyoung Shin <myid.shin@igalia.com>
2022-02-09 08:55:15 +0000

    [Fenced Frame] Do not support beforeunload&unload events
    This CL disables the beforeunload event and the unload event in
    the fenced frame and adds WPT tests to ensure the beforeunload
    handler is not called.
    
    This CL doesn't add a TC for the unload event since await keyword
    in async function wouldn't be worked in unload handler and it
    doesn't guarantee to write the result in writeValueToServer()
    to the primary main test page.
    
    Skip virtual/fenced-frame-*/wpt_internal/fenced_frame/navigate-ancestor.html
    
    Bug: 1271408
    Change-Id: Ieea317c78eb3b2f2ff0454a8a7bc4011b913fdad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3412501
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#968804}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -3622,7 +3622,8 @@ enum class BeforeUnloadUse {
   kNoDialogMultipleConfirmationForNavigation,
   kShowDialog,
   kNoDialogAutoCancelTrue,
-  kMaxValue = kNoDialogAutoCancelTrue,
+  kNotSupportedInFencedFrame,
+  kMaxValue = kNotSupportedInFencedFrame,
 };
 
 void RecordBeforeUnloadUse(BeforeUnloadUse metric) {
@@ -3642,6 +3643,11 @@ bool Document::DispatchBeforeUnloadEvent(ChromeClient* chrome_client,
 
   if (ProcessingBeforeUnload())
     return false;
+
+  if (GetFrame()->IsInFencedFrameTree()) {
+    RecordBeforeUnloadUse(BeforeUnloadUse::kNotSupportedInFencedFrame);
+    return true;
+  }
 
   PageDismissalScope in_page_dismissal;
   auto& before_unload_event = *MakeGarbageCollected<BeforeUnloadEvent>();
@@ -3732,6 +3738,13 @@ void Document::DispatchUnloadEvents(UnloadEventTimingInfo* unload_timing_info) {
   Element* current_focused_element = FocusedElement();
   if (auto* input = DynamicTo<HTMLInputElement>(current_focused_element))
     input->EndEditing();
+
+  // A frame that is a fenced frame or in fenced frame tree does not support the
+  // unload event.
+  if (GetFrame()->IsInFencedFrameTree()) {
+    load_event_progress_ = kUnloadEventHandled;
+    return;
+  }
 
   // If we've dispatched the pagehide event with 'persisted' set to true, it
   // means we've dispatched the visibilitychange event before too. Also, we
