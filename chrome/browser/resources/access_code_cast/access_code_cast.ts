14dc0e1f202c3d5d5e0fa01bc0cc1f2e1d1482ce
Benjamin Zielinski <bzielinski@google.com>
2022-02-09 00:16:14 +0000

    Revert "[Cast2Class] Refactor code input"
    This reverts commit a70d8facd8581a9140c61631a3d4ca317b6c31fb.
    
    Reason for revert: Causes AccessCodeCastCodeInputElementTest.All to fail on mac bots
    
    Original change's description:
    > [Cast2Class] Refactor code input
    >
    > Refactor the Cast2Class code input element to use a single text field
    > under the hood instead of six. This makes the element more accessible
    > to screen readers.
    >
    > - Support enabled and disabled state
    > - Support focused and unfocused state
    > - Support multi-character selection
    > - Support copy / paste
    >
    > Screencast: https://screencast.googleplex.com/cast/NjIwOTgwMTU5Mjc2NjQ2NHxlMWE5NzEzMS04MQ
    >
    > Bug: b/215166445
    > Change-Id: Ia8aedcc024429d9581f4087d2becd427d8493274
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3444862
    > Reviewed-by: Brian Malcolm <bmalcolm@chromium.org>
    > Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    > Cr-Commit-Position: refs/heads/main@{#968430}
    
    Bug: b/215166445
    Change-Id: Iddeb4f60eb15de2ab467d5be0f0f52e8205ff17f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449275
    Auto-Submit: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#968656}

--- chrome/browser/resources/access_code_cast/access_code_cast.ts
+++ chrome/browser/resources/access_code_cast/access_code_cast.ts
@@ -191,7 +191,7 @@ class AccessCodeCastElement extends AccessCodeCastElementBase {
     if (this.castButtonDisabled(this.accessCode, this.canCast)) {
       return;
     }
-    if (!this.$.codeInput.focused) {
+    if (this.$.codeInput.getFocusedIndex() === -1) {
       return;
     }
     if (this.state !== PageState.CODE_INPUT) {
