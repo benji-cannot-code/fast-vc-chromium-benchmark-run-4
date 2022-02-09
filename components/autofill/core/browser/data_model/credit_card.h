cd4afd2983ee7df5bf74e05cb98740daec62393c
David Sanders <dsanders11@ucsbalum.com>
2022-02-09 14:31:14 +0000

    Remove unused include of autofill_field.h
    Also adds some includes to other files which were getting
    them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: Ia70811ac27e59f327db046087f1702fdccc2ce9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448215
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968909}

--- components/autofill/core/browser/data_model/credit_card.h
+++ components/autofill/core/browser/data_model/credit_card.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/strings/string_piece_forward.h"
 #include "build/build_config.h"
-#include "components/autofill/core/browser/autofill_field.h"
 #include "components/autofill/core/browser/data_model/autofill_data_model.h"
 #include "url/gurl.h"
 
--- components/autofill/core/browser/payments/payments_requests/get_details_for_enrollment_request.cc
+++ components/autofill/core/browser/payments/payments_requests/get_details_for_enrollment_request.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_requests/get_details_for_enrollment_request.h"
 
 #include "base/json/json_writer.h"
+#include "base/strings/string_number_conversions.h"
 
 namespace autofill::payments {
 
