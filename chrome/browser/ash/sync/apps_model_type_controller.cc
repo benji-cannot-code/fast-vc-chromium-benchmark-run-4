7e17f844793e403e7aa335518f1f07fc7d0af040
Boris Sazonov <bsazonov@chromium.org>
2022-02-09 18:00:38 +0000

    Remove OsSyncModelTypeController
    After https://crrev.com/c/3350767, this is no longer needed.
    
    Bug: 1274802
    Change-Id: I8f736800f2f37c7b99fb295dc760b64b134980e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440151
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969000}

--- chrome/browser/ash/sync/apps_model_type_controller.cc
+++ chrome/browser/ash/sync/apps_model_type_controller.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/check.h"
 #include "base/memory/ptr_util.h"
-#include "chrome/browser/ash/sync/os_sync_model_type_controller.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync/model/client_tag_based_model_type_processor.h"
@@ -29,10 +28,9 @@ std::unique_ptr<AppsModelTypeController> AppsModelTypeController::Create(
     syncer::OnceModelTypeStoreFactory store_factory,
     base::WeakPtr<syncer::SyncableService> syncable_service,
     const base::RepeatingClosure& dump_stack,
-    syncer::SyncService* sync_service,
     Profile* profile) {
   DCHECK(chromeos::features::IsSyncSettingsCategorizationEnabled());
-  // Create the bridge here so it can be used to construct forwarding delegates
+  // Create the bridge here so it can be used to construct a forwarding delegate
   // passed to the superclass constructor.
   auto bridge = std::make_unique<SyncableServiceBasedBridge>(
       syncer::APPS, std::move(store_factory),
@@ -41,13 +39,10 @@ std::unique_ptr<AppsModelTypeController> AppsModelTypeController::Create(
       syncable_service.get());
   ModelTypeControllerDelegate* delegate =
       bridge->change_processor()->GetControllerDelegate().get();
-  // Runs in transport-mode and full-sync mode, sharing the bridge's delegate.
   return base::WrapUnique(new AppsModelTypeController(
       std::move(bridge), /*delegate_for_full_sync_mode=*/
       std::make_unique<ForwardingModelTypeControllerDelegate>(delegate),
-      /*delegate_for_transport_mode=*/
-      std::make_unique<ForwardingModelTypeControllerDelegate>(delegate),
-      sync_service, profile));
+      profile));
 }
 
 AppsModelTypeController::~AppsModelTypeController() = default;
@@ -56,15 +51,9 @@ AppsModelTypeController::AppsModelTypeController(
     std::unique_ptr<syncer::ModelTypeSyncBridge> bridge,
     std::unique_ptr<syncer::ModelTypeControllerDelegate>
         delegate_for_full_sync_mode,
-    std::unique_ptr<syncer::ModelTypeControllerDelegate>
-        delegate_for_transport_mode,
-    syncer::SyncService* sync_service,
     Profile* profile)
-    : OsSyncModelTypeController(syncer::APPS,
-                                std::move(delegate_for_full_sync_mode),
-                                std::move(delegate_for_transport_mode),
-                                profile->GetPrefs(),
-                                sync_service),
+    : syncer::ModelTypeController(syncer::APPS,
+                                  std::move(delegate_for_full_sync_mode)),
       bridge_(std::move(bridge)),
       profile_(profile) {
   DCHECK(profile_);
@@ -76,5 +65,5 @@ void AppsModelTypeController::LoadModels(
   DCHECK(CalledOnValidThread());
   extensions::ExtensionSystem::Get(profile_)->InitForRegularProfile(
       /*extensions_enabled=*/true);
-  OsSyncModelTypeController::LoadModels(configure_context, model_load_callback);
+  ModelTypeController::LoadModels(configure_context, model_load_callback);
 }
