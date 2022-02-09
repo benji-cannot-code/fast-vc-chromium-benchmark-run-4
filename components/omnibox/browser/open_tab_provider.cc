c1ac56872766086720686880fb2f325cfb22549c
Rachel Wong <wrong@chromium.org>
2022-02-09 04:09:39 +0000

    [open tabs] Add a skeleton OpenTabProvider.
    This CL adds a skeleton Autocomplete provider for open tab search. This
    provider is not added to the default providers, since it will be used
    by the ChromeOS launcher and not the browser (at least for now).
    
    Bug: 1293702
    Change-Id: I5e948e51af57cf83b5fc2f1079ec9b85c59b62b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3434691
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968743}

---
+++ components/omnibox/browser/open_tab_provider.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/open_tab_provider.h"
+
+OpenTabProvider::OpenTabProvider()
+    : AutocompleteProvider(AutocompleteProvider::TYPE_OPEN_TAB) {}
+
+OpenTabProvider::~OpenTabProvider() = default;
+
+void OpenTabProvider::Start(const AutocompleteInput& input,
+                            bool minimal_changes) {
+  // TODO(crbug.com/1293702): WIP.
+}
