948afc3e31ca58197bbbba7c771b8b32fa2c0daf
Greg Thompson <grt@chromium.org>
2022-02-09 14:12:09 +0000

    [fuchsia] Stop crashing when IconLoader::ReadGroup is called.
    Obey the IconLoader contract to avoid crashes and hangs:
    
    - GroupForFilepath must return something (file extension is enough).
    - GetReadIconTaskRunner must return a valid TaskRunner.
    - ReadIcon must post the completion callback.
    
    Bug: 1295538
    Change-Id: I522e4ef2c96b8a44cb1a1ad66efe9c24fb1397c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449078
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Benjamin Lerman <qsr@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968897}

--- chrome/browser/icon_loader_fuchsia.cc
+++ chrome/browser/icon_loader_fuchsia.cc
@@ -7,22 +7,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/icon_loader.h"
 
+#include <utility>
+
+#include "base/bind.h"
+#include "base/callback.h"
+#include "base/location.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/notreached.h"
+#include "base/task/single_thread_task_runner.h"
+#include "base/task/thread_pool.h"
+#include "ui/gfx/image/image.h"
 
 // static
 IconLoader::IconGroup IconLoader::GroupForFilepath(
     const base::FilePath& file_path) {
   NOTIMPLEMENTED_LOG_ONCE();
-  return {};
+  return file_path.Extension();
 }
 
 // static
 scoped_refptr<base::TaskRunner> IconLoader::GetReadIconTaskRunner() {
   NOTIMPLEMENTED_LOG_ONCE();
-  return {};
+  return base::ThreadPool::CreateTaskRunner(traits());
 }
 
 void IconLoader::ReadIcon() {
   NOTIMPLEMENTED_LOG_ONCE();
+
+  // Report back that no icon was found.
+  target_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback_), gfx::Image(), group_));
   delete this;
 }
