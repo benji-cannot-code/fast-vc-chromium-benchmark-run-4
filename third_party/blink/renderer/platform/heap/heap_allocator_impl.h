7d348bbfc27bea9861475bb9f63a0397bed73219
Michael Lippautz <mlippautz@chromium.org>
2022-02-09 10:07:32 +0000

    heap: Raise max backing store capacity for HeapVector
    Align constant with PartitionAlloc, which uses something along the
    lines of (1<<31)-X where X is some internal constant.
    
    Oilpan doesn't have an implementation-imposed limit on the maximum
    size of objects but rather can allocate until it runs OOM due to
    either virtual cage exhaustion or actually running out of physical
    memory.
    
    Aligning the constant with PartitionAlloc allows for changing from
    Vector to HeapVector and vice versa without side effects around
    capacity.
    
    Raising the corresponding limit in PA above something larger (than
    fits into 32-bit signed integers) is a separate discussion to be held.
    
    Change-Id: I842963c702640c3ba1680b82b339d4824bdfc2ad
    Bug: chromium:1288561, chromium:1209303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447300
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Yamada <yukiy@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968823}

--- third_party/blink/renderer/platform/heap/heap_allocator_impl.h
+++ third_party/blink/renderer/platform/heap/heap_allocator_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/write_barrier.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/partition_allocator.h"
 #include "v8/include/cppgc/explicit-management.h"
 #include "v8/include/cppgc/heap-consistency.h"
 #include "v8/include/cppgc/trace-trait.h"
@@ -32,13 +33,13 @@ class PLATFORM_EXPORT HeapAllocator {
 
   static constexpr bool kIsGarbageCollected = true;
 
-  // See wtf/size_t.h for details.
-  static constexpr size_t kMaxHeapObjectSizeLog2 = 27;
-  static constexpr size_t kMaxHeapObjectSize = 1 << kMaxHeapObjectSizeLog2;
-
   template <typename T>
   static size_t MaxElementCountInBackingStore() {
-    return kMaxHeapObjectSize / sizeof(T);
+    // Oilpan doesn't have a limit for supported capacity and instead supports
+    // arbitrary sized allocations. Delegate to PA to keep limits in sync which
+    // may be enforced for security reasons. E.g. PA may cap the limit below
+    // 32-bit sizes to avoid integer overflows in old code.
+    return WTF::PartitionAllocator::MaxElementCountInBackingStore<T>();
   }
 
   template <typename T>
