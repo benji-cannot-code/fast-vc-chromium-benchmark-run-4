3636c5771e94a6df4b2114b3ad19194efae935ab
Jan Krcal <jkrcal@chromium.org>
2022-02-09 15:51:06 +0000

    [BrowserServiceLacros] Open primary profile window if last used is Guest
    This fixes startup for Chrome if the user does not get Profile picker
    (either having 1 profile or having "Show at startup" off) and the last
    used profile is Guest.
    
    Instead of launching Guest (which is a trap, one cannot open another
    profile from Guest), the primary profile is launched.
    
    Bug: 1293643
    Change-Id: Id6d6c6c5a29194855233c2c0afaa32ae260668ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448783
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Jan Krčál <jkrcal@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968944}

--- chrome/browser/lacros/browser_service_lacros.cc
+++ chrome/browser/lacros/browser_service_lacros.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/feedback/feedback_dialog_utils.h"
 #include "chrome/browser/lacros/app_mode/kiosk_session_service_lacros.h"
 #include "chrome/browser/lacros/feedback_util.h"
@@ -143,11 +144,24 @@ void BrowserServiceLacros::NewWindow(bool incognito,
     return;
   }
 
+  ProfileManager* profile_manager = g_browser_process->profile_manager();
   // TODO(crbug.com/1102815): Find what profile should be used.
-  ProfileManager::LoadLastUsedProfileAllowedByPolicy(
-      base::BindOnce(&BrowserServiceLacros::NewWindowWithProfile,
-                     weak_ptr_factory_.GetWeakPtr(), incognito,
-                     should_trigger_session_restore, std::move(callback)));
+  // Open the last used profile, unless it's a special profile like Guest. This
+  // deviates from other entry points here as this is the primary entry point to
+  // _launch_ Chrome. As there's no way to open other profiles from within
+  // Guest, user could get stuck with Guest forever without this treatment.
+  base::FilePath profile_path = profile_manager->GetLastUsedProfileDir();
+  if (profile_path == ProfileManager::GetGuestProfilePath() ||
+      profile_path == ProfileManager::GetSystemProfilePath()) {
+    profile_path = ProfileManager::GetPrimaryUserProfilePath();
+  }
+  profile_manager->LoadProfileByPath(
+      profile_path, /*incognito=*/false,
+      base::BindOnce(&ProfileManager::MaybeForceOffTheRecordMode)
+          .Then(base::BindOnce(&BrowserServiceLacros::NewWindowWithProfile,
+                               weak_ptr_factory_.GetWeakPtr(), incognito,
+                               should_trigger_session_restore,
+                               std::move(callback))));
 }
 
 void BrowserServiceLacros::NewFullscreenWindow(
