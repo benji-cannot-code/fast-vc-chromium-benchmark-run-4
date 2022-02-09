bffd8685ecd192e3f5e80c77e178fe1ec525a1d2
Daniel Murphy <dmurph@chromium.org>
2022-02-09 19:11:45 +0000

    [dPWA] Move icon operations to sequenced task runner
    Icon operations on disk were using a non-sequenced task runner, so they
    could race each other. This meant that if the code here was reading icons
    for the in-memory cache and the web app was uninstalled (or any other
    operation happened to update the icons) then the disk operations would
    race each other and cause failures.
    
    This change fixes this by making all of the operations in the
    WebAppIconManager scheduled on a single shared sequenced task runner.
    
    Bug: 1294088,1275752,1221237,1222972
    Change-Id: I267b1213a7cb95f51f5f6ca18ae9250ff30e83b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450056
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969040}

--- chrome/browser/web_applications/web_app_icon_manager.h
+++ chrome/browser/web_applications/web_app_icon_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
+#include "base/task/sequenced_task_runner.h"
 #include "chrome/browser/web_applications/app_registrar_observer.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
@@ -205,6 +206,7 @@ class WebAppIconManager : public AppRegistrarObserver {
   WebAppRegistrar& registrar_;
   base::FilePath web_apps_directory_;
   scoped_refptr<FileUtilsWrapper> utils_;
+  scoped_refptr<base::SequencedTaskRunner> icon_task_runner_;
 
   base::ScopedObservation<WebAppRegistrar, AppRegistrarObserver>
       registrar_observation_{this};
