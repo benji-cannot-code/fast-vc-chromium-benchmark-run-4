6b253faab4bfcf122468cd71ef82c39bb3962c6c
Benjamin Lerman <qsr@chromium.org>
2022-02-09 17:11:15 +0000

    [fuchsia] Disconnect element.Controllers when last window closes
    User of element::Manager expects the Controller to be closed when the
    element they started finishes. As chrome can starts new browser window
    independantly of the element API, there is no element/view mapping.
    Instead, as a workaround, this CL close all connected controller when
    the last window is closed.
    
    Bug: 1285106
    
    Change-Id: If83ce60f6dac5a9cf83edf71bedc59e7b2c6fa08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436167
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Benjamin Lerman <qsr@chromium.org>
    Auto-Submit: Benjamin Lerman <qsr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968974}

--- chrome/browser/fuchsia/element_manager_impl.cc
+++ chrome/browser/fuchsia/element_manager_impl.cc
@@ -5,10 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/fuchsia/element_manager_impl.h"
 
+#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "chrome/browser/chrome_browser_main.h"
+#include "chrome/browser/ui/browser_list.h"
+
+namespace {
+bool HaveBrowser() {
+  return !BrowserList::GetInstance()->empty();
+}
+}  // namespace
 
 bool ElementManagerImpl::AnnotationKeyCompare::operator()(
     const fuchsia::element::AnnotationKey& lhs,
@@ -22,9 +30,12 @@ ElementManagerImpl::ElementManagerImpl(
     NewProposalCallback callback)
     : binding_(outgoing_directory, this), callback_(std::move(callback)) {
   DCHECK(callback_);
+  BrowserList::AddObserver(this);
 }
 
-ElementManagerImpl::~ElementManagerImpl() = default;
+ElementManagerImpl::~ElementManagerImpl() {
+  BrowserList::RemoveObserver(this);
+}
 
 std::vector<fuchsia::element::Annotation> ElementManagerImpl::GetAnnotations() {
   std::vector<fuchsia::element::Annotation> annotations;
@@ -82,4 +93,13 @@ void ElementManagerImpl::GetAnnotations(GetAnnotationsCallback callback) {
       GetAnnotations());
   callback(fuchsia::element::AnnotationController_GetAnnotations_Result::
                WithResponse(std::move(response)));
+}
+
+void ElementManagerImpl::OnBrowserRemoved(Browser* browser) {
+  // If the browser was the last, clear all active bindings to notify the shell.
+  bool have_browser =
+      have_browser_for_test_ ? have_browser_for_test_.Run() : HaveBrowser();
+  if (!have_browser) {
+    controller_bindings_.CloseAll();
+  }
 }
