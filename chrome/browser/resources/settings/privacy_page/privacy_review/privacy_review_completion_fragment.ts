9022c0034e9b2d4ca842c175e151338012294082
Rainhard Findling <rainhard@chromium.org>
2022-02-09 19:41:42 +0000

    Privacy Guide: browsertest refactoring (completion card)
    Move tests that purely test inner completion fragment functionality into
    a separate completion fragment test suite. This reduces the runtime of
    the main test suite significantly (from ~16s to ~9s on a Chromium build
    machine) and should hence significantly contribute to fixing 1293840.
    
    Before:
      [1/2] [...].HistorySyncFragmentTests (3416 ms)
      [2/2] [...].PrivacyReviewPageTests (15743 ms)
    
    After:
      [1/3] [...].HistorySyncFragmentTests (3419 ms)
      [2/3] [...].CompletionFragmentTests (6043 ms)
      [3/3] [...].PrivacyReviewPageTests (9452 ms)
    
    Bug: 1215630,1293840
    Change-Id: I376ef562dd2693d53bf4dec04e46bd3a80226432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450117
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969055}

--- chrome/browser/resources/settings/privacy_page/privacy_review/privacy_review_completion_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_review/privacy_review_completion_fragment.ts
@@ -101,6 +101,13 @@ export class PrivacyReviewCompletionFragmentElement extends
   }
 }
 
+declare global {
+  interface HTMLElementTagNameMap {
+    'privacy-review-completion-fragment':
+        PrivacyReviewCompletionFragmentElement;
+  }
+}
+
 customElements.define(
     PrivacyReviewCompletionFragmentElement.is,
     PrivacyReviewCompletionFragmentElement);
--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -507,10 +507,14 @@ TEST_F(
       runMochaSuite('PrivacyReviewPage');
     });
 
-
 TEST_F(
     'CrSettingsPrivacyReviewPageTest', 'HistorySyncFragmentTests', function() {
       runMochaSuite('HistorySyncFragment');
+    });
+
+TEST_F(
+    'CrSettingsPrivacyReviewPageTest', 'CompletionFragmentTests', function() {
+      runMochaSuite('CompletionFragment');
     });
 
 var CrSettingsRouteTest = class extends CrSettingsBrowserTest {
