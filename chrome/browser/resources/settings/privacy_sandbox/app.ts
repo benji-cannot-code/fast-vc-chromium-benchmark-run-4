d1324ab181c939b7628f09b676d6f3ccf5edaa34
Ehimare Okoyomon <eokoyomon@chromium.org>
2022-02-09 12:17:29 +0000

    Privacy Sandbox Settings: Replace desktop icons
    Screenshot: crbug.com/1286276#c23
    Bug: 1286276
    Change-Id: I4d5a0a4e8ae5a060c492a4f55d20dbaddd2b3b6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3432485
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968856}

--- chrome/browser/resources/settings/privacy_sandbox/app.ts
+++ chrome/browser/resources/settings/privacy_sandbox/app.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_page_host_style_css.js';
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
+import './icons.js';
 import '../settings.js';
 
 import {addWebUIListener} from 'chrome://resources/js/cr.m.js';
