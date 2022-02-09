6b9f748fbc2a12446124ab50d2b27e5702785027
Pi-Hsun Shih <pihsun@chromium.org>
2022-02-09 03:18:06 +0000

    Print Preview: Fix argument type in JSDoc for setLoadCompleteCallback
    Bug: 1285794
    Test: TypeScript compiles with newer tsc version in CL:3380108
    Change-Id: I6d75337caf5a41784d74799f57bb8a0b1923210a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3446700
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968730}

--- chrome/browser/resources/pdf/pdf_scripting_api.js
+++ chrome/browser/resources/pdf/pdf_scripting_api.js
@@ -40,7 +40,7 @@ export class PDFPlugin extends HTMLIFrameElement {
   setKeyEventCallback(callback) {}
 
   /**
-   * @param {function(success): void} callback
+   * @param {function(boolean): void} callback
    */
   setLoadCompleteCallback(callback) {}
 
