0f7a00057c4920ce306d310058aa727875c13711
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-02-09 20:09:51 +0000

    Remove unused c/b/sync/test/integration/feature_toggler.h
    No behavior is changed.
    
    Bug: None
    Change-Id: I4e25e0f6a36fd71b64019657a67e6e5bb21cd98d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450976
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#969073}

--- chrome/browser/sync/test/integration/single_client_autofill_profile_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_autofill_profile_sync_test.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "chrome/browser/sync/test/integration/autofill_helper.h"
-#include "chrome/browser/sync/test/integration/feature_toggler.h"
 #include "chrome/browser/sync/test/integration/single_client_status_change_checker.h"
 #include "chrome/browser/sync/test/integration/sync_service_impl_harness.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
