716756dd4877b589dc759a1f9666c5df201d2921
Juan Mojica <juanmojica@google.com>
2022-02-09 22:07:46 +0000

    Update header UI of Lens side panel.
    - Fixes hover state to be maximum roundness.
    - Reduces spacing between the close button and header right border.
    - Reduces spacing between the header buttons.
    
    Screenshot: https://screenshot.googleplex.com/A5JKU8sjV69U3ca
    
    Bug: b:213333596, 1295485
    Change-Id: I1121e51a07b86b32e025583067fe63eb4e0764d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448882
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#969136}

--- chrome/browser/ui/views/lens/lens_side_panel_view.cc
+++ chrome/browser/ui/views/lens/lens_side_panel_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/border.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/image_button_factory.h"
+#include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/separator.h"
@@ -67,6 +68,9 @@ std::unique_ptr<views::ImageButton> CreateControlButton(
   button->SetTooltipText(tooltip_text);
   button->SetImageHorizontalAlignment(views::ImageButton::ALIGN_CENTER);
   button->SetProperty(views::kMarginsKey, margin_insets);
+  // Make sure the hover background behind the button is a circle, rather than a
+  // rounded square.
+  views::InstallCircleHighlightPathGenerator(button.get());
   return button;
 }
 
@@ -134,8 +138,12 @@ void LensSidePanelView::CreateAndInstallHeader(
 
   // Set the interior margins of the header on the left and right sides.
   header->SetInteriorMargin(gfx::Insets(
-      0, chrome_layout_provider->GetDistanceMetric(
-             views::DistanceMetric::DISTANCE_RELATED_CONTROL_HORIZONTAL)));
+      0,
+      chrome_layout_provider->GetDistanceMetric(
+          views::DistanceMetric::DISTANCE_RELATED_CONTROL_HORIZONTAL),
+      0,
+      chrome_layout_provider->GetDistanceMetric(
+          ChromeDistanceMetric::DISTANCE_SIDE_PANEL_HEADER_RIGHT_MARGIN)));
   // Set alignments for horizontal (main) and vertical (cross) axes.
   header->SetMainAxisAlignment(views::LayoutAlignment::kStart);
   header->SetCrossAxisAlignment(views::LayoutAlignment::kCenter);
@@ -160,10 +168,9 @@ void LensSidePanelView::CreateAndInstallHeader(
 
   launch_button_ = header->AddChildView(CreateControlButton(
       this, launch_callback, views::kLaunchIcon,
-      gfx::Insets(
-          0, 0, 0,
-          chrome_layout_provider->GetDistanceMetric(
-              views::DistanceMetric::DISTANCE_RELATED_CONTROL_HORIZONTAL)),
+      gfx::Insets(0, 0, 0,
+                  chrome_layout_provider->GetDistanceMetric(
+                      views::DistanceMetric::DISTANCE_CLOSE_BUTTON_MARGIN)),
       l10n_util::GetStringUTF16(IDS_ACCNAME_OPEN),
       ChromeLayoutProvider::Get()->GetDistanceMetric(
           ChromeDistanceMetric::DISTANCE_SIDE_PANEL_HEADER_VECTOR_ICON_SIZE)));
