d2d1d006bec1bf5ed7316cd1b68e095e4f894035
Amanda Deacon <amandadeacon@chromium.org>
2022-02-09 05:38:20 +0000

    [Shortcuts] Determine accessible name for KeyboardShortcutResult.
    This implementation is derived from logic in:
    
      ash/shortcut_viewer/views/keyboard_shortcut_item_view.cc
    
    It involves obtaining the accessible string for each key code in a
    shortcut chord, and substituting these into the appropriate shortcut
    message template.
    
    Test: unit tests
    Bug: 1290682
    Change-Id: I7d09b7db37a743f89b016c292a21e4967a1d6f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3446382
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968763}

--- chrome/browser/ui/app_list/search/keyboard_shortcut_data.cc
+++ chrome/browser/ui/app_list/search/keyboard_shortcut_data.cc
@@ -5,12 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/app_list/search/keyboard_shortcut_data.h"
 
+#include "base/strings/string_util.h"
+
 namespace app_list {
 
 // TODO(crbug.com/1290682): Continue implementation.
 KeyboardShortcutData::KeyboardShortcutData(
     const ash::KeyboardShortcutItem& item)
-    : description(l10n_util::GetStringUTF16(item.description_message_id)) {}
+    : description_message_id(item.description_message_id),
+      description(base::CollapseWhitespace(
+          l10n_util::GetStringUTF16(item.description_message_id),
+          true)),
+      shortcut_message_id(item.shortcut_message_id),
+      shortcut_key_codes(item.shortcut_key_codes) {}
+
+KeyboardShortcutData::KeyboardShortcutData(const KeyboardShortcutData&) =
+    default;
 
 KeyboardShortcutData::~KeyboardShortcutData() = default;
 
--- chrome/browser/ui/app_list/search/keyboard_shortcut_data.h
+++ chrome/browser/ui/app_list/search/keyboard_shortcut_data.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/keyboard_shortcut_item.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/events/keycodes/keyboard_codes.h"
 
 namespace app_list {
 
@@ -30,13 +31,24 @@ struct KeyboardShortcutData {
   explicit KeyboardShortcutData(const ash::KeyboardShortcutItem& item);
   // For testing purposes.
   explicit KeyboardShortcutData(const std::u16string description);
-  KeyboardShortcutData(const KeyboardShortcutData&) = default;
+  KeyboardShortcutData(const KeyboardShortcutData&);
   KeyboardShortcutData& operator=(const KeyboardShortcutData&) = default;
 
   ~KeyboardShortcutData();
 
-  // The description of the shortcut action e.g. "Dock a window on the right".
+  // ID of the message resource describing the action the shortcut performs.
+  int description_message_id;
+  // The description (corresponding to |description_message_id|) of the shortcut
+  // action e.g. "Dock a window on the right".
   std::u16string description;
+
+  // ID of the message template resource used to list the keys making up the
+  // shortcut.
+  absl::optional<int> shortcut_message_id;
+
+  // The VKEY codes of the key and each modifier comprising the shortcut. See
+  // ash/public/cpp/keyboard_shortcut_item.h for more detail.
+  std::vector<ui::KeyboardCode> shortcut_key_codes;
 };
 
 }  // namespace app_list
