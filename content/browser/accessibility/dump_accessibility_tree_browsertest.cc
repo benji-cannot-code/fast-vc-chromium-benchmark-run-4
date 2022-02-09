fbb4d2c18ca92453224ab8759c842208f2dcb80c
Oriol Brufau <obrufau@igalia.com>
2022-02-09 21:07:23 +0000

    [inert] Add a11y test for contents of inert frames
    I plan to stop propagating inertness into frame contents from a styling
    point of view. But the contents should still be consisdered to be inert
    from an accessibility point of view.
    
    This patch adds a test to prevent regressions. AXObject::Serialize needs
    to be modified to avoid skipping unfocusable ignored nodes when enabling
    the AccessibilityExposeIgnoredNodes feature.
    
    Bug: 618746
    
    TEST=All/DumpAccessibilityTreeTestWithIgnoredNodes.AccessibilityModalDialogAndIframe/blink
    
    AX-Relnotes: n/a.
    Change-Id: Ic487c9bef4b91f12af929a74a93e1556bdb3e3ca
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449143
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#969100}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2562,6 +2562,11 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 }
 */
 
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTestWithIgnoredNodes,
+                       AccessibilityModalDialogAndIframe) {
+  RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-and-iframes.html"));
+}
+
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
                        AccessibilityModalDialogStack) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-stack.html"));
