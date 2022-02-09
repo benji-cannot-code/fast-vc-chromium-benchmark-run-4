65613ed45c1d7fb548199421886439cac00df932
avynn <avynn@google.com>
2022-02-09 01:05:03 +0000

    Desk Templates: Fix duplicate launch metric
    Fixes a bug where the launch metric for desk templates is counted
    twice.  This was caused by a duplicate call to the launch metric that
    was recorded in both the desks template presenter and the desks_client.
    The secondary call in the desks_client was originally meant for the
    templates API but was called in the same stack as the native UI.  The
    second call was moved to a location where the conflict would no longer
    occur.
    
    TESTED = Ran desk template client browser tests and modified them
    to test for the proper behavior.
    
    Bug: 1287649
    Change-Id: Ie884165fbec70c27b48939100f53ce83f97dd9f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445480
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#968681}

--- chrome/browser/ui/ash/desks_templates/desks_templates_client.cc
+++ chrome/browser/ui/ash/desks_templates/desks_templates_client.cc
@@ -212,8 +212,6 @@ void DesksTemplatesClient::LaunchAppsFromTemplate(
   DCHECK(app_launch_handler_);
   app_launch_handler_->set_delay(delay);
   app_launch_handler_->SetRestoreDataAndLaunch(restore_data->Clone());
-
-  RecordLaunchFromTemplateHistogram();
 }
 
 desks_storage::DeskModel* DesksTemplatesClient::GetDeskModel() {
@@ -326,6 +324,8 @@ void DesksTemplatesClient::OnGetTemplateForDeskLaunch(
     std::move(callback).Run(std::string(kStorageError));
     return;
   }
+
+  RecordLaunchFromTemplateHistogram();
 
   // Launch the windows as specified in the template to a new desk.
   const auto template_name = entry->template_name();
