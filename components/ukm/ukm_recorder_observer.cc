e2b70a54bb9ff2d2a76352d595a8d9d49464191d
Min Qin <qinmin@chromium.org>
2022-02-09 04:48:06 +0000

    Add observer class to UkmRecorderImpl
    This CL adds 2 observer classes to UkmRecorderImpl so that others
    can be notified when a UKM entry is recorded, or when source
    URL changes.
    This allows Segmentation Platform to listen to UKM changes and
    use the data to build its own DB for structured metrics.
    
    For details about segmentation platform, see:
    go/chrome-context-engine
    
    For details about structured metrics, see:
    https://docs.google.com/document/d/19Y_FaDSW5CgPRQPiAonVA5uAtL6_SnIfqk1ubaqW3g4/edit?resourcekey=0-mFENjbCxHXzDR37JM1LJ8Q
    
    Bug: 1286178
    Change-Id: I1abcea509f3a65e6b973d2f5666ce232dc3edf96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3379169
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968754}

---
+++ components/ukm/ukm_recorder_observer.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/ukm/ukm_recorder_observer.h"
+
+namespace ukm {
+
+void UkmRecorderObserver::OnEntryAdded(mojom::UkmEntryPtr entry) {}
+
+void UkmRecorderObserver::OnUpdateSourceURL(SourceId source_id,
+                                            const std::vector<GURL>& urls) {}
+
+void UkmRecorderObserver::OnPurgeRecordingsWithUrlScheme(
+    const std::string& url_scheme) {}
+
+void UkmRecorderObserver::OnPurge() {}
+
+}  // namespace ukm
