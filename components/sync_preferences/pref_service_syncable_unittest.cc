12feaebe848c04ea1a758067f6d2227d94e04f07
Ian Helmke <helmke@google.com>
2022-02-09 11:51:16 +0000

    Use JsonPrefStore for standalone_browser_prefs
    Persist standalone browser prefs in ash using json_pref_store.
    
    Delete standalone_browser_pref_store, as it is now unneeded.
    
    Initially it seemed like a standalone_browser_pref_store would be
    necessary because, in the case of extension prefs, we write the
    prefs to the user pref store. However, reusing json_pref_store seems
    simpler in this case; it persists a pref_store to a file which is
    exactly what is needed for standalone browser prefs.
    
    Standalone browser prefs are initialised in roughly the same way
    as the user_pref_store, as they are both loaded from files.
    
    Bug: 1218145
    Change-Id: Ic79c3bedc8164864cbcabe09256767cd7f4c3727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3381696
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Ian Helmke <helmke@google.com>
    Cr-Commit-Position: refs/heads/main@{#968848}

--- components/sync_preferences/pref_service_syncable_unittest.cc
+++ components/sync_preferences/pref_service_syncable_unittest.cc
@@ -406,10 +406,11 @@ class PrefServiceSyncableMergeTest : public testing::Test {
                                              new TestingPrefStore,
                                              new TestingPrefStore,
                                              user_prefs_.get(),
-                                             new TestingPrefStore,
+                                             standalone_browser_prefs_.get(),
                                              pref_registry_->defaults().get(),
                                              pref_notifier_),
             user_prefs_,
+            standalone_browser_prefs_,
             pref_registry_,
             &client_,
             /*read_error_callback=*/base::DoNothing(),
@@ -501,6 +502,8 @@ class PrefServiceSyncableMergeTest : public testing::Test {
   scoped_refptr<TestingPrefStore> managed_prefs_ =
       base::MakeRefCounted<TestingPrefStore>();
   scoped_refptr<TestingPrefStore> user_prefs_ =
+      base::MakeRefCounted<TestingPrefStore>();
+  scoped_refptr<TestingPrefStore> standalone_browser_prefs_ =
       base::MakeRefCounted<TestingPrefStore>();
   TestPrefModelAssociatorClient client_;
   PrefServiceSyncable prefs_;
@@ -912,7 +915,8 @@ class PrefServiceSyncableChromeOsTest : public testing::Test {
   PrefServiceSyncableChromeOsTest()
       : pref_registry_(base::MakeRefCounted<PrefRegistrySyncable>()),
         pref_notifier_(new PrefNotifierImpl),
-        user_prefs_(base::MakeRefCounted<TestingPrefStore>()) {}
+        user_prefs_(base::MakeRefCounted<TestingPrefStore>()),
+        standalone_browser_prefs_(base::MakeRefCounted<TestingPrefStore>()) {}
 
   void CreatePrefService() {
     // Register prefs of various types.
@@ -936,9 +940,9 @@ class PrefServiceSyncableChromeOsTest : public testing::Test {
         std::make_unique<PrefValueStore>(
             new TestingPrefStore, new TestingPrefStore, new TestingPrefStore,
             new TestingPrefStore, new TestingPrefStore, user_prefs_.get(),
-            new TestingPrefStore, pref_registry_->defaults().get(),
+            standalone_browser_prefs_.get(), pref_registry_->defaults().get(),
             pref_notifier_),
-        user_prefs_, pref_registry_, &client_,
+        user_prefs_, standalone_browser_prefs_, pref_registry_, &client_,
         /*read_error_callback=*/base::DoNothing(),
         /*async=*/false);
   }
@@ -990,6 +994,7 @@ class PrefServiceSyncableChromeOsTest : public testing::Test {
   scoped_refptr<PrefRegistrySyncable> pref_registry_;
   PrefNotifierImpl* pref_notifier_;  // Owned by |prefs_|.
   scoped_refptr<TestingPrefStore> user_prefs_;
+  scoped_refptr<TestingPrefStore> standalone_browser_prefs_;
   TestPrefModelAssociatorClient client_;
   std::unique_ptr<PrefServiceSyncable> prefs_;
 };
