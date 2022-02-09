992c77ea2e9eb392a190e95dfcb5c92dc4201a90
Adem Derinel <derinel@google.com>
2022-02-09 15:38:21 +0000

    [Passwords] Call unmute when restore warning button is clicked
    This CL follows https://crrev.com/c/3420477/14 and implements the unmute
    button.
    
    Bug: 1178762
    Change-Id: Ibf996e4619bcc065d3532c0df0be70b48de7d689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447898
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#968928}

--- chrome/browser/resources/settings/autofill_page/password_manager_proxy.ts
+++ chrome/browser/resources/settings/autofill_page/password_manager_proxy.ts
@@ -251,6 +251,12 @@ export interface PasswordManagerProxy {
                              chrome.passwordsPrivate.InsecureCredential): void;
 
   /**
+   * Restores / unmutes the |insecureCredential| in the passwords store.
+   */
+  unmuteInsecureCredential(
+      insecureCredential: chrome.passwordsPrivate.InsecureCredential): void;
+
+  /**
    * Add an observer to the compromised passwords change.
    */
   addCompromisedCredentialsListener(listener: CredentialsChangedListener): void;
@@ -552,6 +558,11 @@ export class PasswordManagerImpl implements PasswordManagerProxy {
   muteInsecureCredential(insecureCredential:
                              chrome.passwordsPrivate.InsecureCredential) {
     chrome.passwordsPrivate.muteInsecureCredential(insecureCredential);
+  }
+
+  unmuteInsecureCredential(insecureCredential:
+                               chrome.passwordsPrivate.InsecureCredential) {
+    chrome.passwordsPrivate.unmuteInsecureCredential(insecureCredential);
   }
 
   addCompromisedCredentialsListener(listener: CredentialsChangedListener) {
--- chrome/test/data/webui/settings/test_password_manager_proxy.ts
+++ chrome/test/data/webui/settings/test_password_manager_proxy.ts
@@ -111,6 +111,7 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
       'getUrlCollection',
       'addPassword',
       'muteInsecureCredential',
+      'unmuteInsecureCredential',
     ]);
 
     /** @private {!PasswordManagerExpectations} */
@@ -386,6 +387,11 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
   muteInsecureCredential(insecureCredential:
                              chrome.passwordsPrivate.InsecureCredential) {
     this.methodCalled('muteInsecureCredential', insecureCredential);
+  }
+
+  unmuteInsecureCredential(insecureCredential:
+                               chrome.passwordsPrivate.InsecureCredential) {
+    this.methodCalled('unmuteInsecureCredential', insecureCredential);
   }
 
   cancelExportPasswords() {}
