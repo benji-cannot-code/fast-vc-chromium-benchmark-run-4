aacc85d445cdbc1de212b61f62b3dfa6fae4ca5b
Ehimare Okoyomon <eokoyomon@chromium.org>
2022-02-09 15:40:26 +0000

    Privacy Sandbox Settings: Add learn more dialog
    Add link that opens up a dialog to learn more about ad personalization.
    
    Screenshot: crbug.com/1286276#c46
    Bug: 1286276
    Change-Id: Iebbca48d53d1e4abc3f57f418bd78c5ce6ec7755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447406
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968931}

--- chrome/test/data/webui/settings/privacy_sandbox_test.ts
+++ chrome/test/data/webui/settings/privacy_sandbox_test.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://settings/privacy_sandbox/app.js';
 
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
+import {CrDialogElement} from 'chrome://settings/lazy_load.js';
 import {PrivacySandboxAppElement} from 'chrome://settings/privacy_sandbox/app.js';
 import {PrivacySandboxBrowserProxy, PrivacySandboxBrowserProxyImpl} from 'chrome://settings/privacy_sandbox/privacy_sandbox_browser_proxy.js';
 import {CrButtonElement, CrSettingsPrefs, HatsBrowserProxyImpl, loadTimeData, MetricsBrowserProxyImpl, TrustSafetyInteraction} from 'chrome://settings/settings.js';
@@ -255,5 +256,26 @@ suite('PrivacySandboxSettings3', function() {
                              'Settings.PrivacySandbox.ApisEnabled',
           await metricsBrowserProxy.whenCalled('recordAction'));
     });
+  });
+
+  test('testLearnMoreDialog', function() {
+    // The learn more link should be visible but the dialog itself not.
+    assertTrue(isChildVisible(page, '#learnMoreLink'));
+    assertFalse(
+        page.shadowRoot!.querySelector<CrDialogElement>(
+                            '#learnMoreDialog')!.open);
+
+    // Clicking on the learn more link should open the dialog.
+    page.shadowRoot!.querySelector<HTMLElement>('#learnMoreLink')!.click();
+    assertTrue(
+        page.shadowRoot!.querySelector<CrDialogElement>(
+                            '#learnMoreDialog')!.open);
+
+    // Clicking on the close button of the dialog should close it.
+    page.shadowRoot!.querySelector<HTMLElement>(
+                        '#learnMoreCloseButton')!.click();
+    assertFalse(
+        page.shadowRoot!.querySelector<CrDialogElement>(
+                            '#learnMoreDialog')!.open);
   });
 });
