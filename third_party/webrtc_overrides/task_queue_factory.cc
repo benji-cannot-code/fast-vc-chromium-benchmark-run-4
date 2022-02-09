ec18b38c7a4850102b0ca751727929612c576a80
Francois Pierre Doray <fdoray@chromium.org>
2022-02-09 07:54:09 +0000

    Revert "Introduce WebRtcReasonableTimerSlack."
    This reverts commit 39289e7ae65a49d34b22a80dbbb8a7208b32a483.
    
    Reason for revert: The LudicrousTimerSlack and 
    WebRtcReasonableTimerSlack experiments are no longer
    running. They are replaced by the AlignWakeUps feature, which allows
    individual tasks to be opted-out via DeadlineTimer or 
    SequencedTaskRunner::PostCancelableDelayedTaskAt. Reverting this
    CL to avoid accumulating technical debt.
    
    Original change's description:
    > Introduce WebRtcReasonableTimerSlack.
    >
    > This change introduces a new feature, WebRtcReasonableTimerSlack. When
    > enabled, it's only in effect when LudicrousTimerSlack is enabled, and
    > enables WebRTC on Mac to run timers with leeway equal to the value of
    > LudicrousTimerSlack:slack_ms feature parameter.
    >
    > The intent is to use the feature for testing on test farms.
    >
    > Locally, WebRtcReasonableTimerSlack+LudicrousTimerSlack:slack_ms/8ms
    > brings Meet 50p wakeups north of 1 kHz down to about 450 Hz.
    >
    > Bug: chromium:1243235
    > Change-Id: Iade3815f62b1ca16a27f9fb4cba6048e15dceb39
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3113935
    > Commit-Queue: Markus Handell <handellm@google.com>
    > Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#915129}
    
    Bug: chromium:1243235
    Change-Id: Ifb7bdcdf1ccd1a695cf31a8120d96d01dcad3957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448689
    Reviewed-by: Markus Handell <handellm@google.com>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968787}

--- third_party/webrtc_overrides/task_queue_factory.cc
+++ third_party/webrtc_overrides/task_queue_factory.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc_overrides/task_queue_factory.h"
 
 #include "base/bind.h"
-#include "base/feature_list.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/message_loop/timer_slack.h"
@@ -17,13 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/task_queue/task_queue_base.h"
 #include "third_party/webrtc/api/task_queue/task_queue_factory.h"
 
-namespace webrtc {
-
-constexpr base::Feature kWebRtcReasonableTimerSlack{
-    "WebRtcReasonableTimerSlack", base::FEATURE_DISABLED_BY_DEFAULT};
-
-}  // namespace webrtc
-
 namespace {
 
 class WebrtcTaskQueue final : public webrtc::TaskQueueBase {
@@ -32,9 +24,7 @@ class WebrtcTaskQueue final : public webrtc::TaskQueueBase {
       : task_runner_(base::ThreadPool::CreateSequencedTaskRunner(traits)),
         is_active_(new base::RefCountedData<bool>(true)),
         suspend_timer_slack_(traits.priority() !=
-                                 base::TaskPriority::BEST_EFFORT &&
-                             !base::FeatureList::IsEnabled(
-                                 webrtc::kWebRtcReasonableTimerSlack)) {
+                             base::TaskPriority::BEST_EFFORT) {
     DCHECK(task_runner_);
   }
 
