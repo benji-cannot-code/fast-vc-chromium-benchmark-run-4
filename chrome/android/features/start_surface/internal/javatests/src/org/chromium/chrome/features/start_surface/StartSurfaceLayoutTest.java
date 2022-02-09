933bf928726e878273327c56e847ed4307e3661d
Matt Jones <mdjones@chromium.org>
2022-02-09 15:50:02 +0000

    Generalize layout showing logic in new public method
    This patch adds a public method to the LayoutManager that allows
    features to request a specific layout be shown. This functionality
    replaces that of the OverviewModeController and requires features
    be explicit about which layout should be visible (as opposed to
    hideOverview which was ambiguous).
    
    New test utils have also been added to deflake tests that were
    depending on very specific behavior.
    
    Bug: 1108496
    Change-Id: I8a6be5b1f47f5d48dcacf3174abdb7cbfa64c904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3151137
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968943}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
@@ -100,6 +100,7 @@ import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.flags.CachedFeatureFlags;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.layouts.LayoutType;
 import org.chromium.chrome.browser.night_mode.ChromeNightModeTestUtils;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
 import org.chromium.chrome.browser.tab.Tab;
@@ -2243,7 +2244,9 @@ public class StartSurfaceLayoutTest {
         // TODO(wychen): use TabUiTestHelper.enterTabSwitcher() instead.
         //  Might increase flakiness though. See crbug.com/1024742.
         TestThreadUtils.runOnUiThreadBlocking(
-                () -> mActivityTestRule.getActivity().getLayoutManager().showOverview(true));
+                ()
+                        -> mActivityTestRule.getActivity().getLayoutManager().showLayout(
+                                LayoutType.TAB_SWITCHER, true));
         assertTrue(mActivityTestRule.getActivity().getLayoutManager().overviewVisible());
 
         // Make sure the fading animation is done.
@@ -2279,7 +2282,9 @@ public class StartSurfaceLayoutTest {
             // If the last thumbnail is missing, try without animation.
             Espresso.pressBack();
             TestThreadUtils.runOnUiThreadBlocking(
-                    () -> mActivityTestRule.getActivity().getLayoutManager().showOverview(false));
+                    ()
+                            -> mActivityTestRule.getActivity().getLayoutManager().showLayout(
+                                    LayoutType.TAB_SWITCHER, false));
             TabUiTestHelper.verifyAllTabsHaveThumbnail(
                     mActivityTestRule.getActivity().getCurrentTabModel());
         }
