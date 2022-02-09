76ac28336579cf50996ad9d38f9d103406242260
Curtis McMullan <curtismcmullan@chromium.org>
2022-02-09 01:56:21 +0000

    ime: connect input method via connection factory
    This CL completes the migration from two mojo pipes initialized between
    the shared library and Chromium, to a single mojo pipe.
    
    The pipe is bootstrapped via the InitializeConnectionFactory method, and
    the mojo::InputMethod and mojo::InputMethodHost interfaces are
    associated with the connected pipe.
    
    BUG=b:209695253
    
    Change-Id: I1843335d702604ccab8441e1a985f3f019e44b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3439146
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968698}

--- ash/services/ime/ime_service.h
+++ ash/services/ime/ime_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "ash/services/ime/connection_factory.h"
 #include "ash/services/ime/decoder/decoder_engine.h"
 #include "ash/services/ime/input_engine.h"
 #include "ash/services/ime/public/cpp/shared_lib/interfaces.h"
@@ -56,6 +57,7 @@ class ImeService : public mojom::ImeService,
       ConnectToInputMethodCallback callback) override;
   void InitializeConnectionFactory(
       mojo::PendingReceiver<mojom::ConnectionFactory> connection_factory,
+      mojom::ConnectionTarget connection_target,
       InitializeConnectionFactoryCallback callback) override;
 
   // ImeCrosPlatform overrides:
@@ -87,6 +89,10 @@ class ImeService : public mojom::ImeService,
   // decoder engine or input engine instance.
   std::unique_ptr<DecoderEngine> decoder_engine_;
   std::unique_ptr<InputEngine> input_engine_;
+
+  // Used to support connections to the RuleBasedEngine implemented in the
+  // ime service itself.
+  std::unique_ptr<ConnectionFactory> connection_factory_;
 
   // Platform delegate for access to privilege resources.
   mojo::Remote<mojom::PlatformAccessProvider> platform_access_;
