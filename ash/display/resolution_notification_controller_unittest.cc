94dc9597986b3ef86924dbe6f44e2a68fdf3cfde
Jimmy Gong <jimmyxgong@google.com>
2022-02-09 02:27:43 +0000

    Re-enable time based tests
    Previously there was some flakes with the notification appearing vs.
    expecting the notification to appear instantly (15s revert timeout).
    Adjust the tests to use the actual timeout into the expected
    message.
    
    Bug: 1293686
    Test: ash_unittests
    Change-Id: I26d2a8d58c8269d97dc5f74075e328c9411f4830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448581
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#968714}

--- ash/display/resolution_notification_controller_unittest.cc
+++ ash/display/resolution_notification_controller_unittest.cc
@@ -42,12 +42,13 @@ class ResolutionNotificationControllerTest
 
   std::u16string ExpectedNotificationMessage(int64_t display_id,
                                              const gfx::Size& new_resolution,
-                                             float new_refresh_rate) {
+                                             float new_refresh_rate,
+                                             uint16_t timeout_count) {
     const std::u16string display_name =
         base::UTF8ToUTF16(display_manager()->GetDisplayNameForId(display_id));
-    const std::u16string countdown =
-        ui::TimeFormat::Simple(ui::TimeFormat::FORMAT_DURATION,
-                               ui::TimeFormat::LENGTH_LONG, base::Seconds(15));
+    const std::u16string countdown = ui::TimeFormat::Simple(
+        ui::TimeFormat::FORMAT_DURATION, ui::TimeFormat::LENGTH_LONG,
+        base::Seconds(timeout_count));
     if (::display::features::IsListAllDisplayModesEnabled()) {
       return l10n_util::GetStringFUTF16(
           IDS_ASH_RESOLUTION_REFRESH_CHANGE_DIALOG_CHANGED, display_name,
@@ -64,12 +65,13 @@ class ResolutionNotificationControllerTest
       const gfx::Size& specified_resolution,
       float specified_refresh_rate,
       const gfx::Size& fallback_resolution,
-      float fallback_refresh_rate) {
+      float fallback_refresh_rate,
+      uint16_t timeout_count) {
     const std::u16string display_name =
         base::UTF8ToUTF16(display_manager()->GetDisplayNameForId(display_id));
-    const std::u16string countdown =
-        ui::TimeFormat::Simple(ui::TimeFormat::FORMAT_DURATION,
-                               ui::TimeFormat::LENGTH_LONG, base::Seconds(15));
+    const std::u16string countdown = ui::TimeFormat::Simple(
+        ui::TimeFormat::FORMAT_DURATION, ui::TimeFormat::LENGTH_LONG,
+        base::Seconds(timeout_count));
     if (::display::features::IsListAllDisplayModesEnabled()) {
       return l10n_util::GetStringFUTF16(
           IDS_ASH_RESOLUTION_REFRESH_CHANGE_DIALOG_FALLBACK,
@@ -154,6 +156,10 @@ class ResolutionNotificationControllerTest
     return controller()->dialog_for_testing()->label_->GetText();
   }
 
+  static uint16_t GetTimeoutCount() {
+    return controller()->dialog_for_testing()->timeout_count_;
+  }
+
   static void ClickOnNotification() {
     controller()->dialog_for_testing()->AcceptDialog();
   }
@@ -190,7 +196,7 @@ class ResolutionNotificationControllerTest
 };
 
 // Basic behaviors and verifies it doesn't cause crashes.
-TEST_P(ResolutionNotificationControllerTest, DISABLED_Basic) {
+TEST_P(ResolutionNotificationControllerTest, Basic) {
   UpdateDisplay("400x300#400x300%57|300x200%58,300x250#300x250%60|300x200%59");
   display::test::DisplayManagerTestApi display_manager_test(display_manager());
   int64_t id2 = display_manager_test.GetSecondaryDisplay().id();
@@ -202,7 +208,9 @@ TEST_P(ResolutionNotificationControllerTest, DISABLED_Basic) {
                                 gfx::Size(300, 200), 59, /*old_is_native=*/true,
                                 /*new_is_native=*/false);
   EXPECT_TRUE(IsNotificationVisible());
-  EXPECT_EQ(ExpectedNotificationMessage(id2, gfx::Size(300, 200), 59),
+
+  EXPECT_EQ(ExpectedNotificationMessage(id2, gfx::Size(300, 200), 59,
+                                        GetTimeoutCount()),
             GetNotificationMessage());
   display::ManagedDisplayMode mode;
   EXPECT_TRUE(display_manager()->GetSelectedModeForDisplayId(id2, &mode));
@@ -414,7 +422,7 @@ TEST_P(ResolutionNotificationControllerTest, MultipleResolutionChange) {
   EXPECT_EQ(58.0f, mode.refresh_rate());
 }
 
-TEST_P(ResolutionNotificationControllerTest, DISABLED_Fallback) {
+TEST_P(ResolutionNotificationControllerTest, Fallback) {
   UpdateDisplay(
       "400x300#400x300%56|300x200%57,"
       "350x250#350x250%60|220x210%60|300x200%60");
@@ -429,7 +437,8 @@ TEST_P(ResolutionNotificationControllerTest, DISABLED_Fallback) {
       gfx::Size(300, 200), 60, /*old_is_native=*/true, /*new_is_native=*/false);
   EXPECT_TRUE(IsNotificationVisible());
   EXPECT_EQ(ExpectedFallbackNotificationMessage(id2, gfx::Size(220, 210), 60,
-                                                gfx::Size(300, 200), 60),
+                                                gfx::Size(300, 200), 60,
+                                                GetTimeoutCount()),
             GetNotificationMessage());
   display::ManagedDisplayMode mode;
   EXPECT_TRUE(display_manager()->GetSelectedModeForDisplayId(id2, &mode));
