a46f6bec839c21f437a2123803d34def7ae7852e
Andrew Grieve <agrieve@chromium.org>
2022-02-10 04:07:00 +0000

    avd.py: Ergonomic improvements to the script
    * -v now shows emulator logs for "start" command
    * "start" now runs "install" if obviously necessary (config.ini not present)
    * "list" command added, which makes it clear what --avd-config options
      exist.
    
    Bug: 1276006
    Change-Id: Idc37641d9a665f7460e2eabe5348aea1e262d247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438186
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969283}

--- build/android/pylib/local/emulator/avd.py
+++ build/android/pylib/local/emulator/avd.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 
+import collections
 import contextlib
 import json
 import logging
@@ -187,6 +188,7 @@ class AvdConfig:
     Args:
       avd_proto_path: path to a textpb file containing an Avd message.
     """
+    self.avd_proto_path = avd_proto_path
     self._config = _Load(avd_proto_path)
 
     self._emulator_home = os.path.join(constants.DIR_SOURCE_ROOT,
@@ -202,6 +204,22 @@ class AvdConfig:
   @property
   def avd_settings(self):
     return self._config.avd_settings
+
+  @property
+  def avd_name(self):
+    return self._config.avd_name
+
+  @property
+  def _avd_home(self):
+    return os.path.join(self._emulator_home, 'avd')
+
+  @property
+  def _avd_dir(self):
+    return os.path.join(self._avd_home, '%s.avd' % self._config.avd_name)
+
+  @property
+  def _config_ini_path(self):
+    return os.path.join(self._avd_dir, 'config.ini')
 
   def Create(self,
              force=False,
@@ -246,7 +264,7 @@ class AvdConfig:
         self._config.system_image_package,
     ])
 
-    android_avd_home = os.path.join(self._emulator_home, 'avd')
+    android_avd_home = self._avd_home
 
     if not os.path.exists(android_avd_home):
       os.makedirs(android_avd_home)
@@ -267,8 +285,7 @@ class AvdConfig:
       root_ini = os.path.join(android_avd_home,
                               '%s.ini' % self._config.avd_name)
       features_ini = os.path.join(self._emulator_home, 'advancedFeatures.ini')
-      avd_dir = os.path.join(android_avd_home, '%s.avd' % self._config.avd_name)
-      config_ini = os.path.join(avd_dir, 'config.ini')
+      avd_dir = self._avd_dir
 
       with ini.update_ini_file(root_ini) as root_ini_contents:
         root_ini_contents['path.rel'] = 'avd/%s.avd' % self._config.avd_name
@@ -280,7 +297,7 @@ class AvdConfig:
         features_ini_contents.clear()
         features_ini_contents.update(self.avd_settings.advanced_features)
 
-      with ini.update_ini_file(config_ini) as config_ini_contents:
+      with ini.update_ini_file(self._config_ini_path) as config_ini_contents:
         height = self.avd_settings.screen.height or _DEFAULT_SCREEN_HEIGHT
         width = self.avd_settings.screen.width or _DEFAULT_SCREEN_WIDTH
         density = self.avd_settings.screen.density or _DEFAULT_SCREEN_DENSITY
@@ -412,6 +429,26 @@ class AvdConfig:
         logging.info('Deleting AVD.')
         avd_manager.Delete(avd_name=self._config.avd_name)
 
+  def IsAvailable(self, packages=_ALL_PACKAGES):
+    """Returns whether emulator is up-to-date."""
+    if not os.path.exists(self._config_ini_path):
+      return False
+
+    for cipd_root, pkgs in self._IterVersionedCipdPackages(packages):
+      stdout = subprocess.run(['cipd', 'installed', '--root', cipd_root],
+                              capture_output=True,
+                              check=False,
+                              encoding='utf8').stdout
+      # Output looks like:
+      # Packages:
+      #   name1:version1
+      #   name2:version2
+      installed = [l.strip().split(':', 1) for l in stdout.splitlines()[1:]]
+
+      if any([p.package_name, p.version] not in installed for p in pkgs):
+        return False
+    return True
+
   def Install(self, packages=_ALL_PACKAGES):
     """Installs the requested CIPD packages and prepares them for use.
 
@@ -425,8 +462,8 @@ class AvdConfig:
     self._MakeWriteable()
     self._EditConfigs()
 
-  def _InstallCipdPackages(self, packages):
-    pkgs_by_dir = {}
+  def _IterVersionedCipdPackages(self, packages):
+    pkgs_by_dir = collections.defaultdict(list)
     if packages is _ALL_PACKAGES:
       packages = [
           self._config.avd_package,
@@ -434,13 +471,18 @@ class AvdConfig:
           self._config.system_image_package,
       ]
     for pkg in packages:
-      if not pkg.dest_path in pkgs_by_dir:
-        pkgs_by_dir[pkg.dest_path] = []
-      pkgs_by_dir[pkg.dest_path].append(pkg)
+      # Skip when no version exists to prevent "IsAvailable()" returning False
+      # for emualtors set up using Create() (rather than Install()).
+      if pkg.version:
+        pkgs_by_dir[pkg.dest_path].append(pkg)
 
-    for pkg_dir, pkgs in list(pkgs_by_dir.items()):
-      logging.info('Installing packages in %s', pkg_dir)
+    for pkg_dir, pkgs in pkgs_by_dir.items():
       cipd_root = os.path.join(constants.DIR_SOURCE_ROOT, pkg_dir)
+      yield cipd_root, pkgs
+
+  def _InstallCipdPackages(self, packages):
+    for cipd_root, pkgs in self._IterVersionedCipdPackages(packages):
+      logging.info('Installing packages in %s', cipd_root)
       if not os.path.exists(cipd_root):
         os.makedirs(cipd_root)
       ensure_path = os.path.join(cipd_root, '.ensure')
@@ -465,8 +507,7 @@ class AvdConfig:
       except subprocess.CalledProcessError as e:
         # avd.py is executed with python2.
         # pylint: disable=W0707
-        raise AvdException('Failed to install CIPD package %s: %s' %
-                           (pkg.package_name, str(e)),
+        raise AvdException('Failed to install CIPD packages: %s' % str(e),
                            command=ensure_cmd)
 
   def _MakeWriteable(self):
@@ -480,10 +521,9 @@ class AvdConfig:
         os.chmod(path, mode)
 
   def _EditConfigs(self):
-    android_avd_home = os.path.join(self._emulator_home, 'avd')
-    avd_dir = os.path.join(android_avd_home, '%s.avd' % self._config.avd_name)
+    avd_dir = self._avd_dir
 
-    config_path = os.path.join(avd_dir, 'config.ini')
+    config_path = self._config_ini_path
     if os.path.exists(config_path):
       with open(config_path) as config_file:
         config_contents = ini.load(config_file)
