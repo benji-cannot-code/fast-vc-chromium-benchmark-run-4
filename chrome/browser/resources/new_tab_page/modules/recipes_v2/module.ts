e395a93ab5515119da6dd23d0eb5c335c830f337
dpapad <dpapad@chromium.org>
2022-02-10 18:58:37 +0000

    NewTabPage: Remove unused imports of |html| tag literal.
    Forgot to remove these as part of r968691.
    
    Bug: 1292025
    Change-Id: Ib22196fe0f64146a4d414cd59175eae524116d10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452502
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969560}

--- chrome/browser/resources/new_tab_page/modules/recipes_v2/module.ts
+++ chrome/browser/resources/new_tab_page/modules/recipes_v2/module.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../module_header.js';
 
-import {DomRepeat, html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {DomRepeat, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {I18nMixin, loadTimeData} from '../../i18n_setup.js';
 import {TaskItem, TaskModuleType} from '../../task_module.mojom-webui.js';
--- chrome/browser/resources/new_tab_page/realbox/realbox_action.ts
+++ chrome/browser/resources/new_tab_page/realbox/realbox_action.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/shared_style_css.m.js';
 
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {Action} from '../realbox.mojom-webui.js';
 import {decodeString16} from '../utils.js';
