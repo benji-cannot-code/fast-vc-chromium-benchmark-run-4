761a899322b1d2ab5880799d8228f714c43933d7
Eugene Zemtsov <eugene@chromium.org>
2022-02-10 21:33:42 +0000

    webcodecs: Set default bitrate for VPX and AV1 encoders
    Bug: 1267388
    Change-Id: Ib4c97ff175054d1f8c33128d7ccaa8354237211e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3451139
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969643}

--- media/base/video_encoder.h
+++ media/base/video_encoder.h
@@ -21,6 +21,9 @@ namespace media {
 
 class VideoFrame;
 
+MEDIA_EXPORT uint32_t GetDefaultVideoEncodeBitrate(gfx::Size frame_size,
+                                                   uint32_t framerate);
+
 // Encoded video frame, its data and metadata.
 struct MEDIA_EXPORT VideoEncoderOutput {
   VideoEncoderOutput();
