054084dcabe278d3697062193fde917c871af247
Maksim Ivanov <emaxx@chromium.org>
2022-02-10 12:20:13 +0000

    Fix use-after-move in arc_app_utils
    Fix a potential scenario in LaunchAppWithIntent() that could lead to
    reading from a moved-from Mojo pointer. This can happen if the ghost
    window launch failed and a fallback the shelf spinner happens.
    
    This issue was found by the clang-tidy bugprone-use-after-move
    diagnostics:
    
      arc_app_utils.cc:390:
      'window_info' used after it was moved
      arc_app_utils.cc:376:
      move occurred here
    
    Bug: 1122844
    Change-Id: I5f1c02ccb501e9c9cb1cc1a17a2cffa833899d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449097
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969391}

--- chrome/browser/ash/arc/window_predictor/window_predictor_utils.cc
+++ chrome/browser/ash/arc/window_predictor/window_predictor_utils.cc
@@ -34,7 +34,7 @@ bool LaunchArcAppWithGhostWindow(Profile* profile,
                                  const ArcAppListPrefs::AppInfo& app_info,
                                  int event_flags,
                                  arc::UserInteractionType user_interaction_type,
-                                 arc::mojom::WindowInfoPtr window_info) {
+                                 const arc::mojom::WindowInfoPtr& window_info) {
   WindowPredictor::GetInstance()->MaybeCreateAppLaunchHandler(profile);
 
   auto* arc_task_handler =
@@ -65,8 +65,8 @@ bool LaunchArcAppWithGhostWindow(Profile* profile,
   }
 
   arc::mojom::WindowInfoPtr predict_window_info =
-      WindowPredictor::GetInstance()->PredictAppWindowInfo(
-          app_info, std::move(window_info));
+      WindowPredictor::GetInstance()->PredictAppWindowInfo(app_info,
+                                                           window_info.Clone());
   auto* app_launch_handler =
       WindowPredictor::GetInstance()->app_launch_handler();
   DCHECK(app_launch_handler);
