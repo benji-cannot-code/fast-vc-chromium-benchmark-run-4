9556d295c09a552c4249aca50d0bbad1d8b1ff9e
Vikas Soni <vikassoni@chromium.org>
2022-02-10 01:54:20 +0000

    [Dr-Dc] Add logging to debug context MakeCurrent failure.
    Add LOG(ERROR) to all the failing return paths encountered while trying
    to make a SharedContextState current.
    
    Bug: 1273425
    Change-Id: I018ef25c29839e33bfd0c0206cbc415d02c782d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449673
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969236}

--- ui/gl/gl_context_egl.cc
+++ ui/gl/gl_context_egl.cc
@@ -429,8 +429,8 @@ void GLContextEGL::ReleaseYUVToRGBConvertersAndBackpressureFences() {
       // allocated in GLImageIOSurfaceEGL::CopyTexImage, which is only on
       // MacOS, where surfaceless EGL contexts are always supported.
       if (!eglMakeCurrent(display_, EGL_NO_SURFACE, EGL_NO_SURFACE, context_)) {
-        DVLOG(1) << "eglMakeCurrent failed with error "
-                 << GetLastEGLErrorString();
+        LOG(ERROR) << "eglMakeCurrent failed with error "
+                   << GetLastEGLErrorString();
       }
     }
 
@@ -447,8 +447,8 @@ void GLContextEGL::ReleaseYUVToRGBConvertersAndBackpressureFences() {
     if (context_ != current_egl_context) {
       if (!eglMakeCurrent(display_, current_draw_surface, current_read_surface,
                           current_egl_context)) {
-        DVLOG(1) << "eglMakeCurrent failed with error "
-                 << GetLastEGLErrorString();
+        LOG(ERROR) << "eglMakeCurrent failed with error "
+                   << GetLastEGLErrorString();
       }
     }
   }
@@ -456,8 +456,10 @@ void GLContextEGL::ReleaseYUVToRGBConvertersAndBackpressureFences() {
 
 bool GLContextEGL::MakeCurrentImpl(GLSurface* surface) {
   DCHECK(context_);
-  if (lost_)
+  if (lost_) {
+    LOG(ERROR) << "Failed to make context current since it is marked as lost";
     return false;
+  }
   if (IsCurrent(surface))
     return true;
 
@@ -474,8 +476,8 @@ bool GLContextEGL::MakeCurrentImpl(GLSurface* surface) {
                       surface->GetHandle(),
                       surface->GetHandle(),
                       context_)) {
-    DVLOG(1) << "eglMakeCurrent failed with error "
-             << GetLastEGLErrorString();
+    LOG(ERROR) << "eglMakeCurrent failed with error "
+               << GetLastEGLErrorString();
     return false;
   }
 
@@ -508,8 +510,8 @@ void GLContextEGL::ReleaseCurrent(GLSurface* surface) {
   SetCurrent(nullptr);
   if (!eglMakeCurrent(display_, EGL_NO_SURFACE, EGL_NO_SURFACE,
                       EGL_NO_CONTEXT)) {
-    DVLOG(1) << "eglMakeCurrent failed to release current with error "
-             << GetLastEGLErrorString();
+    LOG(ERROR) << "eglMakeCurrent failed to release current with error "
+               << GetLastEGLErrorString();
     lost_ = true;
   }
 
