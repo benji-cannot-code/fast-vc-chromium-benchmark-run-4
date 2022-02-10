803a8babdabe34bbdf434279a9b800a1faa53bb4
Peter Kasting <pkasting@chromium.org>
2022-02-10 18:24:37 +0000

    Chrome UI color definition cleanup.
    Merge autofill mixer into main chrome color mixer. Unless there's
    significant complexity associated with a mixer, it's probably better to
    keep colors in one place.
    
    Alphabetize IDs.
    
    Convert to straight-line code for brevity and readability.
    
    Bug: none
    Change-Id: Id842eb07cc8b99ac0ca73b31a14496802abb4262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453102
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969547}

--- chrome/browser/ui/color/chrome_color_mixers.cc
+++ chrome/browser/ui/color/chrome_color_mixers.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/fixed_flat_map.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_piece.h"
-#include "chrome/browser/ui/color/autofill_color_mixer.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/color/chrome_color_mixer.h"
 #include "chrome/browser/ui/color/native_chrome_color_mixer.h"
@@ -56,7 +55,6 @@ void AddChromeColorMixers(ui::ColorProvider* provider,
   AddChromeColorMixer(provider, key);
   AddNativeChromeColorMixer(provider, key);
 
-  AddAutofillColorMixer(provider, key);
   AddOmniboxColorMixer(provider, key);
   AddTabStripColorMixer(provider, key);
 
