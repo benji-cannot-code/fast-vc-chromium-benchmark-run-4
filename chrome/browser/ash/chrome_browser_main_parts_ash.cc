e006ee1b82db457c4ffde7d4e2467039b8fe56d4
Nigel Tao <nigeltao@chromium.org>
2022-02-10 05:19:31 +0000

    fusebox: implement the FuseBoxService D-Bus interface
    This commit only implements the Stat method. Follow-up commits will
    implement other methods.
    
    Bug: 1249754
    Change-Id: I2824ab03c4de9f03e41f45e043fb6182e6d1ea7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3423648
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969299}

--- chrome/browser/ash/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/chrome_browser_main_parts_ash.cc
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/dbus/dlp_files_policy_service_provider.h"
 #include "chrome/browser/ash/dbus/drive_file_stream_service_provider.h"
 #include "chrome/browser/ash/dbus/encrypted_reporting_service_provider.h"
+#include "chrome/browser/ash/dbus/fusebox_service_provider.h"
 #include "chrome/browser/ash/dbus/kiosk_info_service_provider.h"
 #include "chrome/browser/ash/dbus/libvda_service_provider.h"
 #include "chrome/browser/ash/dbus/lock_to_single_user_service_provider.h"
@@ -420,11 +421,18 @@ class DBusServices {
                                 dbus::ObjectPath(smbfs::kSmbFsServicePath),
                                 CrosDBusService::CreateServiceProviderList(
                                     std::make_unique<SmbFsServiceProvider>()));
+
     lock_to_single_user_service_ = CrosDBusService::Create(
         system_bus, lock_to_single_user::kLockToSingleUserServiceName,
         dbus::ObjectPath(lock_to_single_user::kLockToSingleUserServicePath),
         CrosDBusService::CreateServiceProviderList(
             std::make_unique<LockToSingleUserServiceProvider>()));
+
+    fusebox_service_ = CrosDBusService::Create(
+        system_bus, fusebox::kFuseBoxServiceName,
+        dbus::ObjectPath(fusebox::kFuseBoxServicePath),
+        CrosDBusService::CreateServiceProviderList(
+            std::make_unique<FuseBoxServiceProvider>()));
 
     mojo_connection_service_ = CrosDBusService::Create(
         system_bus,
@@ -508,6 +516,7 @@ class DBusServices {
     cryptohome_key_delegate_service_.reset();
     encrypted_reporting_service_.reset();
     lock_to_single_user_service_.reset();
+    fusebox_service_.reset();
     mojo_connection_service_.reset();
     ProcessDataCollector::Shutdown();
     PowerDataCollector::Shutdown();
@@ -542,6 +551,7 @@ class DBusServices {
   std::unique_ptr<CrosDBusService> machine_learning_decision_service_;
   std::unique_ptr<CrosDBusService> smb_fs_service_;
   std::unique_ptr<CrosDBusService> lock_to_single_user_service_;
+  std::unique_ptr<CrosDBusService> fusebox_service_;
   std::unique_ptr<CrosDBusService> mojo_connection_service_;
   std::unique_ptr<CrosDBusService> dlp_files_policy_service_;
 };
