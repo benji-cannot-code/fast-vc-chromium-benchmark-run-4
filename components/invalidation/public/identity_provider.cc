ac052046b45e42c8fa34d951379a6fa4a7d32175
Morten Stenshorne <mstensho@chromium.org>
2022-02-10 10:45:32 +0000

    CodeHealth: Avoid deprecated DictionaryValue::Set*() in components/invalidation
    Bug: 1187025, 1187026, 1187028
    Change-Id: I927057e9ce72b660c4abfa69faf38bd49c8a6655
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449081
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969378}

--- components/invalidation/public/identity_provider.cc
+++ components/invalidation/public/identity_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/invalidation/public/identity_provider.h"
 
 #include "base/i18n/time_formatting.h"
+#include "base/strings/utf_string_conversions.h"
 
 namespace invalidation {
 
@@ -61,12 +62,12 @@ IdentityProvider::Diagnostics::Diagnostics() = default;
 base::DictionaryValue IdentityProvider::Diagnostics::CollectDebugData() const {
   base::DictionaryValue status;
 
-  status.SetInteger("IdentityProvider.token-removal-for-not-active-account",
+  status.SetIntPath("IdentityProvider.token-removal-for-not-active-account",
                     token_removal_for_not_active_account_count);
-  status.SetInteger("IdentityProvider.token-update-for-not-active-account",
+  status.SetIntPath("IdentityProvider.token-update-for-not-active-account",
                     token_update_for_not_active_account_count);
-  status.SetString("IdentityProvider.account-token-updated",
-                   base::TimeFormatShortDateAndTime(account_token_updated));
+  status.SetStringPath("IdentityProvider.account-token-updated",
+                       base::TimeFormatShortDateAndTime(account_token_updated));
   return status;
 }
 
