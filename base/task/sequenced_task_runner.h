38210cf476dbcfd07913de8873e93817001edfdf
Etienne Pierre-doray <etiennep@chromium.org>
2022-02-10 12:38:44 +0000

    [task] Use PostDelayedTaskAt with kPrecise in webrtc
    To avoid being affected by AlignWakeUps experiment, use kPrecise
    when posting tasks from webrtc as an escape hatch.
    This CL is a short term work around, a proper solution to expose a
    knob in webrtc is being worked on here:
    https://webrtc-review.googlesource.com/c/src/+/251360
    
    Note that I've ignored third_party/webrtc_overrides/task_queue_factory.cc
    since these task run in chrome ThreadPool which isn't currently affected
    by the experiment.
    
    Bug: 1153139
    Change-Id: Iabc3bb8b6638ea5360d7ce3e437738638fcd8104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450596
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969395}

--- base/task/sequenced_task_runner.h
+++ base/task/sequenced_task_runner.h
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class TimerBase;
 }
+namespace webrtc {
+class ThreadWrapper;
+class WebRtcMetronomeTaskQueue;
+}  // namespace webrtc
 
 namespace base {
 
@@ -44,6 +48,8 @@ class PostDelayedTaskPassKey {
   friend class base::DeadlineTimer;
   friend class blink::TimerBase;
   friend class PostDelayedTaskPassKeyForTesting;
+  friend class webrtc::ThreadWrapper;
+  friend class webrtc::WebRtcMetronomeTaskQueue;
 };
 
 class PostDelayedTaskPassKeyForTesting : public PostDelayedTaskPassKey {};
