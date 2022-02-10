f819245745601ec3f7c626bcf509a7ca4c57027f
Ghazale Hosseinabadi <ghazale@google.com>
2022-02-10 01:44:38 +0000

    [Extensions] Initialize developer_mode_only feature and enable in tests.
    This CL initializes developer_mode_only feature and enables it in the
    relevant test.
    
    Bug: 1267489
    Change-Id: Ic681557d4cc2015c9ef8a71150772f38c32bcb85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438388
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969233}

--- extensions/common/features/simple_feature.cc
+++ extensions/common/features/simple_feature.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "components/crx_file/id_util.h"
 #include "extensions/common/extension_api.h"
+#include "extensions/common/extension_features.h"
 #include "extensions/common/features/feature_channel.h"
 #include "extensions/common/features/feature_developer_mode_only.h"
 #include "extensions/common/features/feature_flags.h"
@@ -632,8 +633,11 @@ Feature::Availability SimpleFeature::GetEnvironmentAvailability(
   if (!MatchesSessionTypes(session_type))
     return CreateAvailability(INVALID_SESSION_TYPE, session_type);
 
-  if (developer_mode_only_ && !GetCurrentDeveloperMode(context_id))
+  if (base::FeatureList::IsEnabled(
+          extensions_features::kRestrictDeveloperModeAPIs) &&
+      developer_mode_only_ && !GetCurrentDeveloperMode(context_id)) {
     return CreateAvailability(REQUIRES_DEVELOPER_MODE);
+  }
 
   return CreateAvailability(IS_AVAILABLE);
 }
