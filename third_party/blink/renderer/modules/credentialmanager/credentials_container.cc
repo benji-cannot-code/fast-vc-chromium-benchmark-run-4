bf3ae7a7973446c67d328823ce2670ffcbee42f7
Christian Biesinger <cbiesinger@chromium.org>
2022-02-10 17:36:10 +0000

    [FedCM] Perform CSP checks on the provider URL
    Bug: 1233548
    Change-Id: I043caf897ef5c344181b86a2773a5c669029f871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440497
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969514}

--- third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanager/credentials_container.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/frame.h"
 #include "third_party/blink/renderer/core/frame/frame_console.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -1125,6 +1126,9 @@ ScriptPromise CredentialsContainer::get(
 
   Vector<KURL> providers;
   if (options->hasFederated() && options->federated()->hasProviders()) {
+    ContentSecurityPolicy* policy =
+        resolver->GetExecutionContext()
+            ->GetContentSecurityPolicyForCurrentWorld();
     for (const auto& provider : options->federated()->providers()) {
       if (provider->IsString()) {
         KURL url = KURL(NullURL(), provider->GetAsString());
@@ -1154,6 +1158,17 @@ ScriptPromise CredentialsContainer::get(
           resolver->Reject(MakeGarbageCollected<DOMException>(
               DOMExceptionCode::kInvalidStateError,
               "Provided provider information is incomplete."));
+          return promise;
+        }
+        // We disallow redirects (in idp_network_request_manager.cc), so it is
+        // enough to check the initial URL here.
+        if (!policy->AllowConnectToSource(provider_url, provider_url,
+                                          RedirectStatus::kNoRedirect)) {
+          WTF::String error =
+              "Refused to connect to '" + provider_url.ElidedString() +
+              "' because it violates the document's Content Security Policy.";
+          resolver->Reject(MakeGarbageCollected<DOMException>(
+              DOMExceptionCode::kNetworkError, error));
           return promise;
         }
         DCHECK(options->federated()->hasPreferAutoSignIn());
