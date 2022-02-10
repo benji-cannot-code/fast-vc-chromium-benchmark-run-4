b820c0ef3ebfc99496008f88e9510cbd9c93406c
Benjamin Beaudry <bebeaudr@microsoft.com>
2022-02-10 20:14:46 +0000

    [Focusgroup] Parse 'focusgroup' attribute to FocusgroupFlags
    In this CL, we handle the parsing of the new 'focusgroup' attribute.
    
    Explainer: https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/main/Focusgroup/explainer.md
    Design doc: https://docs.google.com/document/d/1eh2Y93w_NkZ-6GKmAufAGWZeteKaAFgbZUsjWD6p7WE/edit?usp=sharing
    
    Bug: 1286127
    Change-Id: Ib6a0882a9106f569d40a7606729aa2e596d33ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3445623
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#969606}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2590,6 +2590,13 @@ Node::InsertionNotificationRequest Element::InsertedInto(
   if (!name_value.IsNull())
     UpdateName(g_null_atom, name_value);
 
+  if (RuntimeEnabledFeatures::FocusgroupEnabled()) {
+    const AtomicString& focusgroup_value =
+        FastGetAttribute(html_names::kFocusgroupAttr);
+    if (!focusgroup_value.IsNull())
+      UpdateFocusgroup(focusgroup_value);
+  }
+
   if (parentElement() && parentElement()->IsInCanvasSubtree())
     SetIsInCanvasSubtree(true);
 
@@ -2655,6 +2662,7 @@ void Element::RemovedFrom(ContainerNode& insertion_point) {
   if (HasRareData()) {
     ElementRareData* data = GetElementRareData();
 
+    data->ClearFocusgroupFlags();
     data->ClearRestyleFlags();
 
     if (ElementAnimations* element_animations = data->GetElementAnimations())
@@ -4247,6 +4255,14 @@ void Element::ParseAttribute(const AttributeModificationParams& params) {
     } else {
       // We only set when value is in integer range.
       SetTabIndexExplicitly();
+    }
+  } else if (params.name == html_names::kFocusgroupAttr) {
+    // Only update the focusgroup flags when the node has been added to the
+    // tree. This is because the computed focusgroup value will depend on the
+    // focusgroup value of its closest ancestor node that is a focusgroup, if
+    // any.
+    if (parentNode()) {
+      UpdateFocusgroup(params.new_value);
     }
   } else if (params.name == xml_names::kLangAttr) {
     PseudoStateChanged(CSSSelector::kPseudoLang);
@@ -6339,6 +6355,14 @@ inline void Element::UpdateId(TreeScope& scope,
     UpdateIdNamedItemRegistration(type, old_id, new_id);
 }
 
+inline void Element::UpdateFocusgroup(const AtomicString& input) {
+  if (!RuntimeEnabledFeatures::FocusgroupEnabled())
+    return;
+
+  EnsureElementRareData().SetFocusgroupFlags(
+      focusgroup::ParseFocusgroup(this, input));
+}
+
 void Element::WillModifyAttribute(const QualifiedName& name,
                                   const AtomicString& old_value,
                                   const AtomicString& new_value) {
@@ -7160,6 +7184,12 @@ void Element::RebuildTransitionPseudoLayoutTree(
 bool Element::IsInertRoot() {
   return RuntimeEnabledFeatures::InertAttributeEnabled() &&
          FastHasAttribute(html_names::kInertAttr) && IsHTMLElement();
+}
+
+FocusgroupFlags Element::GetFocusgroupFlags() const {
+  if (!RuntimeEnabledFeatures::FocusgroupEnabled() || !HasRareData())
+    return FocusgroupFlags::kNone;
+  return GetElementRareData()->GetFocusgroupFlags();
 }
 
 }  // namespace blink
