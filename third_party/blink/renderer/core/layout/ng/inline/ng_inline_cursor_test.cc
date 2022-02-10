36e57b641b61e6989ff007889a27ca7ca3622f5c
Yoshifumi Inoue <yosin@chromium.org>
2022-02-10 08:50:30 +0000

    [block-in-inline] Make NGFragmentItem::BlockInInline() to return LayoutObject
    This patch changes |NGFragmentItem::BlockInInline()| to return
    |LayoutObject| instead of |LayoutBlock| because the first child of
    block-in-inline can be |LayoutImage|, to avoid invalid type cast
    |To<LayoutBlock>()|.
    
    Example:
    # HTML Document
      <div><span><img style="display:block"></span></div>
    
    # Layout tree
      LayoutNGBlockFlow P id="root"
        +--LayoutInline SPAN
         |  +--LayoutNGBlockFlow (anonymous)  # block-in-inline
         |  |  +--LayoutImage IMG id="target" # first child of block-in-inline
         +--LayoutText #text ""
    
    Bug: 1295087
    Change-Id: I6c9c49727f925a72f677a04a87d2100fe9c35f65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449737
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969347}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor_test.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/editing/text_affinity.h"
+#include "third_party/blink/renderer/core/layout/layout_image.h"
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_node_data.h"
@@ -418,6 +419,27 @@ TEST_P(NGInlineCursorTest, FirstLastLogicalLeafWithImages) {
   NGInlineCursor last_logical_leaf(cursor);
   last_logical_leaf.MoveToLastLogicalLeaf();
   EXPECT_EQ("#last", ToDebugString(last_logical_leaf));
+}
+
+// http://crbug.com/1295087
+TEST_P(NGInlineCursorTest, FirstNonPseudoLeafWithBlockImage) {
+  InsertStyleElement("img { display: block; }");
+  NGInlineCursor cursor = SetupCursor("<p id=root><b><img id=target></b></p>");
+
+  // Note: The first child of block-in-inline can be |LayoutImage|.
+  // LayoutNGBlockFlow P id="root"
+  //   +--LayoutInline SPAN
+  //   |  +--LayoutNGBlockFlow (anonymous)  # block-in-inline
+  //   |  |  +--LayoutImage IMG id="target" # first child of block-in-inline
+  //   +--LayoutText #text ""
+  const auto& target =
+      *To<LayoutImage>(GetElementById("target")->GetLayoutObject());
+
+  cursor.MoveToFirstNonPseudoLeaf();
+  ASSERT_TRUE(cursor.Current());
+  EXPECT_EQ(target.Parent(), cursor.Current().GetLayoutObject());
+  ASSERT_TRUE(cursor.Current()->IsBlockInInline());
+  EXPECT_EQ(&target, cursor.Current()->BlockInInline());
 }
 
 TEST_P(NGInlineCursorTest, IsEmptyLineBox) {
