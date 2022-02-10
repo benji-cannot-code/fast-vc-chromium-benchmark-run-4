3aab4a02b7621d02d8ec54da705b393e053792cc
Lijin Shen <lazzzis@google.com>
2022-02-10 19:30:41 +0000

    Add flags for improving reader mode prompt
    Bug: 1240741
    Change-Id: I3d3a1418b9c78b0a91d4bed1b243b77d22bc057e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447312
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#969583}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1952,6 +1952,9 @@ extern const char kReaderModeHeuristicsAllArticles[];
 extern const char kReaderModeHeuristicsAlwaysOff[];
 extern const char kReaderModeHeuristicsAlwaysOn[];
 
+extern const char kImproveReaderModePromptName[];
+extern const char kImproveReaderModePromptDescription[];
+
 extern const char kReaderModeInCCTName[];
 extern const char kReaderModeInCCTDescription[];
 
