c8da05d13906b50a6c21dfb5a96fe4b408599e85
Olesia Marukhno <olesiamarukhno@google.com>
2022-02-10 12:40:10 +0000

    [privacy sandbox settings] Delay showing the dialog
    Delay showing the dialog until the HTML is loaded. The dialog is be
    shown when DOMContentLoaded is called and the content height is provided
    to the dialog view. The dialog load time is logged via a histogram.
    
    Bug: 1286276
    Change-Id: I4930bdc4220ce8f3bd9e156ea59bdb652e32ccfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450118
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#969397}

--- chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.cc
+++ chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/privacy_sandbox/privacy_sandbox_dialog.h"
@@ -34,8 +35,7 @@ void ShowPrivacySandboxDialog(Browser* browser,
   auto dialog =
       std::make_unique<PrivacySandboxDialogView>(browser, dialog_type);
   constrained_window::CreateBrowserModalDialogViews(
-      std::move(dialog), browser->window()->GetNativeWindow())
-      ->Show();
+      std::move(dialog), browser->window()->GetNativeWindow());
 }
 
 PrivacySandboxDialogView::PrivacySandboxDialogView(
@@ -43,6 +43,7 @@ PrivacySandboxDialogView::PrivacySandboxDialogView(
     PrivacySandboxService::DialogType dialog_type)
     : browser_(browser) {
   // Create the web view in the native bubble.
+  dialog_created_time_ = base::TimeTicks::Now();
   web_view_ =
       AddChildView(std::make_unique<views::WebView>(browser->profile()));
   web_view_->LoadInitialURL(GURL(chrome::kChromeUIPrivacySandboxDialogURL));
@@ -88,6 +89,11 @@ void PrivacySandboxDialogView::ResizeNativeView(int height) {
   web_view_->SetPreferredSize(gfx::Size(web_view_->GetPreferredSize().width(),
                                         std::min(height, max_height)));
   SizeToContents();
+  GetWidget()->Show();
+
+  DCHECK(!dialog_created_time_.is_null());
+  base::UmaHistogramTimes("Settings.PrivacySandbox.DialogLoadTime",
+                          base::TimeTicks::Now() - dialog_created_time_);
 }
 
 void PrivacySandboxDialogView::OpenPrivacySandboxSettings() {
