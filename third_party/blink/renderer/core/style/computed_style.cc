9c25f0492b7a4d3138af7619203911a95d19d671
Rune Lillesveen <futhark@chromium.org>
2022-02-10 10:00:50 +0000

    [@container] SVG - Only outer roots be size query containers
    SVG outer roots can be size query containers like other replaced
    elements. Other SVG elements are not laid out per se.
    
    Bug: 1295709
    Change-Id: Ica3cc57dca9e2e1ffa6606dc1d59b477ef52edf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447237
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969363}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/style_inherited_variables.h"
 #include "third_party/blink/renderer/core/style/style_non_inherited_variables.h"
 #include "third_party/blink/renderer/core/style/style_ray.h"
+#include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/platform/fonts/font.h"
 #include "third_party/blink/renderer/platform/fonts/font_selector.h"
 #include "third_party/blink/renderer/platform/geometry/length_functions.h"
@@ -2650,7 +2651,10 @@ bool ComputedStyle::ShouldApplyAnyContainment(const Element& element) const {
 
 bool ComputedStyle::IsContainerForContainerQueries(
     const Element& element) const {
-  return IsContainerForContainerQueries() && !element.ShouldForceLegacyLayout();
+  return IsContainerForContainerQueries() &&
+         !element.ShouldForceLegacyLayout() &&
+         (!element.IsSVGElement() ||
+          To<SVGElement>(element).IsOutermostSVGSVGElement());
 }
 
 STATIC_ASSERT_ENUM(cc::OverscrollBehavior::Type::kAuto,
