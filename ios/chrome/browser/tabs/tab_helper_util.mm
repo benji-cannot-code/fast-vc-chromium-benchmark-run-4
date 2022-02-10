551f6db966802c8d2ac266927a4d4d6c85dbff8d
ginnnnnnny <ginnyhuang@chromium.org>
2022-02-10 21:27:36 +0000

    [iOS] Manually show new permissions banner when old banner is dismissed
    Currently, if the user enables camera permission and dismisses the
    resulting banner, when they enable a new permission, the infobar banner
    would not show again.
    
    This is because of the way the overlay request cancel handler handles
    replacement. A new request for the replacement's banner is only inserted
    if an infobar is replaced while its banner request is in the queue.
    
    This CL makes an exception for permissions - a new request for the
    updated infobar is manually added when its banner request is NOT in the
    queue.
    
    Fixed: 1295833
    Change-Id: If6c02489ac31d8dc6f440855ce74d65f2f4ca002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449587
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969642}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/infobars/infobar_manager_impl.h"
 #import "ios/chrome/browser/infobars/overlays/infobar_overlay_request_inserter.h"
 #import "ios/chrome/browser/infobars/overlays/infobar_overlay_tab_helper.h"
+#import "ios/chrome/browser/infobars/overlays/permissions_overlay_tab_helper.h"
 #import "ios/chrome/browser/infobars/overlays/translate_overlay_tab_helper.h"
 #import "ios/chrome/browser/itunes_urls/itunes_urls_handler_tab_helper.h"
 #import "ios/chrome/browser/language/url_language_histogram_factory.h"
@@ -78,7 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/invalid_url_tab_helper.h"
 #import "ios/chrome/browser/web/load_timing_tab_helper.h"
 #import "ios/chrome/browser/web/page_placeholder_tab_helper.h"
-#import "ios/chrome/browser/web/permissions_tab_helper.h"
 #import "ios/chrome/browser/web/print/print_tab_helper.h"
 #import "ios/chrome/browser/web/sad_tab_tab_helper.h"
 #import "ios/chrome/browser/web/session_state/web_session_state_tab_helper.h"
@@ -215,7 +215,7 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
 
   OfflinePageTabHelper::CreateForWebState(
       web_state, ReadingListModelFactory::GetForBrowserState(browser_state));
-  PermissionsTabHelper::CreateForWebState(web_state);
+  PermissionsOverlayTabHelper::CreateForWebState(web_state);
 
   language::IOSLanguageDetectionTabHelper::CreateForWebState(
       web_state,
