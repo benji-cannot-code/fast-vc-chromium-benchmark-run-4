43832c5153cc87535bae7c5892ac14baadae67b7
Irem Uguz <iremuguz@google.com>
2022-02-11 11:18:48 +0000

    support-tool: Use html_to_wrapper() for getting template
    As suggested in crbug/1292025, use html_to_wrapper() for getting the
    template at runtime.
    
    BUG=b:218617022
    TEST=Enable "SupportTool" feature and compile and run Chrome. Check
    
    chrome: //support-tool page if it's shown.
    Change-Id: I0672e7d5dd176ceb044fd5411b4bec826f0075f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452062
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#969890}

--- chrome/browser/resources/support_tool/support_tool.ts
+++ chrome/browser/resources/support_tool/support_tool.ts
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
 import {isChromeOS} from 'chrome://resources/js/cr.m.js';
-import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {getTemplate} from './support_tool.html.js';
 
 export class SupportToolElement extends PolymerElement {
   static get is() {
@@ -16,7 +17,7 @@ export class SupportToolElement extends PolymerElement {
 
   // TODO(crbug.com/1292025): Use html_to_wrapper() for getting the template.
   static get template() {
-    return html`{__html_template__}`;
+    return getTemplate();
   }
 
   static get properties() {
