f5e057eadd831f645efb1663649f5db0f532bff8
xiangdong kong <xiangdongkong@google.com>
2022-02-11 02:12:49 +0000

    feedback: Add skeleton and build plumbing for help content provider
    - Adds backend class skeleton
    - Adds simple unit test
    - Add build plumbing for tests and code
    
    Bug: b:185624798
    Test: ash_webui_unittests --gtest_filter="HelpContentProviderTest.*"
    Change-Id: Iea87eb1588eea5d3734bc1fdf7d332723d3cf72f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3451178
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#969789}

---
+++ ash/webui/os_feedback_ui/backend/help_content_provider_unittest.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/webui/os_feedback_ui/backend/help_content_provider.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ash {
+namespace feedback {
+
+class HelpContentProviderTest : public testing::Test {
+ public:
+  HelpContentProviderTest() = default;
+  ~HelpContentProviderTest() override = default;
+};
+
+TEST_F(HelpContentProviderTest, DummyTest) {
+  HelpContentProvider provider;
+  EXPECT_TRUE(true);
+}
+
+}  // namespace feedback
+}  // namespace ash
