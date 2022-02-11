49153fd8a9472954aa74dcb5e71517505f59409b
Veronique Nguyen <veronguyen@google.com>
2022-02-11 14:37:22 +0000

    [iOS][Passwords] Crash when focus accessibility on Check Now status.
    - Added a boolean to determine when the focus accessibility method
    should be called (only when the user tap on the "Check Now" button).
    - Added -hasItemForItemType:sectionIdentifier: verification.
    
    Bug: 1295743
    Change-Id: I2c18f1c178c6fabf481de88ed2a0e4886a0746d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453056
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Veronique Nguyen <veronguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#969937}

--- ios/chrome/browser/ui/settings/password/passwords_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/passwords_table_view_controller.mm
@@ -314,6 +314,9 @@ void RemoveFormsToBeDeleted(
 // site equivalent to that of |mostRecentlyUpdatedPassword|.
 @property(nonatomic, weak) PasswordFormContentItem* mostRecentlyUpdatedItem;
 
+// YES, if the user has tapped on the "Check Now" button.
+@property(nonatomic, assign) BOOL shouldFocusAccessibilityOnPasswordCheckStatus;
+
 @end
 
 @implementation PasswordsTableViewController
@@ -1508,10 +1511,13 @@ void RemoveFormsToBeDeleted(
   }
 
   // Notify the accessibility to focus on the password check status cell when
-  // the status changed to unsafe, safe or error.
-  if (state == PasswordCheckStateUnSafe || state == PasswordCheckStateSafe ||
-      state == PasswordCheckStateError) {
+  // the status changed to unsafe, safe or error. (Only do it after the user tap
+  // on the "Check Now" button.)
+  if (self.shouldFocusAccessibilityOnPasswordCheckStatus &&
+      (state == PasswordCheckStateUnSafe || state == PasswordCheckStateSafe ||
+       state == PasswordCheckStateError)) {
     [self focusAccessibilityOnPasswordCheckStatus];
+    self.shouldFocusAccessibilityOnPasswordCheckStatus = NO;
   }
 }
 
@@ -1699,12 +1705,15 @@ void RemoveFormsToBeDeleted(
 // Notifies accessibility to focus on the Password Check Status cell when its
 // layout changed.
 - (void)focusAccessibilityOnPasswordCheckStatus {
-  NSIndexPath* indexPath =
-      [self.tableViewModel indexPathForItemType:ItemTypePasswordCheckStatus
-                              sectionIdentifier:SectionIdentifierPasswordCheck];
-  UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:indexPath];
-  UIAccessibilityPostNotification(UIAccessibilityLayoutChangedNotification,
-                                  cell);
+  if ([self.tableViewModel hasItemForItemType:ItemTypePasswordCheckStatus
+                            sectionIdentifier:SectionIdentifierPasswordCheck]) {
+    NSIndexPath* indexPath = [self.tableViewModel
+        indexPathForItemType:ItemTypePasswordCheckStatus
+           sectionIdentifier:SectionIdentifierPasswordCheck];
+    UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    UIAccessibilityPostNotification(UIAccessibilityLayoutChangedNotification,
+                                    cell);
+  }
 }
 
 #pragma mark - UITableViewDelegate
@@ -1757,6 +1766,7 @@ void RemoveFormsToBeDeleted(
     case ItemTypeCheckForProblemsButton:
       if (self.passwordCheckState != PasswordCheckStateRunning) {
         [self.delegate startPasswordCheck];
+        self.shouldFocusAccessibilityOnPasswordCheckStatus = YES;
         UmaHistogramEnumeration("PasswordManager.BulkCheck.UserAction",
                                 PasswordCheckInteraction::kManualPasswordCheck);
       }
