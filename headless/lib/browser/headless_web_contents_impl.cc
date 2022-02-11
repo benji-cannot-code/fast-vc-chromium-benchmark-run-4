ce7175fa151c1628a053c6fbe0d9cc1b309f77ef
Andrey Kosyakov <caseq@chromium.org>
2022-02-11 19:47:27 +0000

    inspector_protocol: switch headless/ to use_embedders_types
    mostly a no-op here.
    
    Bug: 1294416, 1296032
    Change-Id: Iacebb77bc41d05c0b9aa96d907f91f4d858789a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453505
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970084}

--- headless/lib/browser/headless_web_contents_impl.cc
+++ headless/lib/browser/headless_web_contents_impl.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_browser_main_parts.h"
 #include "headless/lib/browser/headless_devtools_agent_host_client.h"
 #include "headless/lib/browser/protocol/headless_handler.h"
-#include "headless/public/internal/headless_devtools_client_impl.h"
 #include "printing/buildflags/buildflags.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
 #include "third_party/skia/include/core/SkBitmap.h"
