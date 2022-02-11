ad8bfa32bafa950ba46af26e8fe453c714627be2
Wen-Chien Wang <wcwang@chromium.org>
2022-02-11 19:41:56 +0000

    Stop showing launcher nudge in guest session
    As the prefs aren't saved in ephemeral guest session, the nudge might
    show repeatedly. This cl add the condition to stop showing launcher
    nudge in these session types.
    
    Bug: 1294320
    Change-Id: Ic9755d72326c9bf8256efa4442c6b5228f7b00ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447960
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970076}

--- ash/shelf/launcher_nudge_controller.cc
+++ ash/shelf/launcher_nudge_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/app_list_controller_impl.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
+#include "ash/public/cpp/session/session_types.h"
 #include "ash/root_window_controller.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shelf/home_button.h"
@@ -247,6 +248,15 @@ void LauncherNudgeController::ScheduleShowNudgeAttempt(
 
 void LauncherNudgeController::OnActiveUserPrefServiceChanged(
     PrefService* prefs) {
+  // If the current session is a guest session which is ephemeral and doesn't
+  // save prefs, return early and don't show nudges for these session types.
+  if (Shell::Get()
+          ->session_controller()
+          ->GetUserSession(0)
+          ->user_info.is_ephemeral) {
+    return;
+  }
+
   if (Shell::Get()->session_controller()->IsUserFirstLogin()) {
     // If the current logged in user is a new one, record the first login time
     // to know when to show the nudge.
