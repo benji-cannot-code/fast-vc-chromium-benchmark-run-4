1e360c0d3c9460a74e8063e980837a173051bbb7
Olga Sharonova <olka@chromium.org>
2022-02-11 16:21:16 +0000

    Audio service: depend on media/webrtc only for chrome-wide echo cancellation
    Bug: 1215049
    Change-Id: Ib49cd7c9f2eaa573bca402e0b6a160e58c2544b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455497
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Sam Zackrisson <saza@chromium.org>
    Commit-Queue: Sam Zackrisson <saza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969965}

--- services/audio/input_controller_unittest.cc
+++ services/audio/input_controller_unittest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/test_audio_thread.h"
 #include "media/base/audio_processing.h"
 #include "media/base/user_input_monitor.h"
-#include "media/webrtc/webrtc_features.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/audio/concurrent_stream_metric_reporter.h"
 #include "services/audio/device_output_listener.h"
