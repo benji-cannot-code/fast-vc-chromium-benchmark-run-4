26d506d0c437f5143fa2b5ac060e26432ad04155
Siyu An <siyua@chromium.org>
2022-02-11 23:05:18 +0000

    Reland "[Offer notification] Add cross-tab tracking for notification UI"
    This is a reland of b800eeec28ebe34339dca61f9ea5938e027be8e4
    We have an uninitialized pointer |offer_|. Changed it in
    controller_impl.h
    
    Original change's description:
    > [Offer notification] Add cross-tab tracking for notification UI
    >
    > So that the notification will be shown on one merchant website once per
    > profile launch.
    >
    > AutofillOfferManager is created per profile start. So we can track
    > notification status in it. Meanwhile we would like to keep the behavior
    > consistent before and after the change:
    > 1. Offer notification is not origin sticky - dismiss upon page nav.
    > 2. Notification bubble is shown only once per domain, but omnibox icon
    >    is always visible when offers are available.
    > 3. Notification is shown only after the web content is visible and
    >    active.
    >
    > Created a OfferNotificationHandler to specifically handle the logic
    > and make decision on whether to update the notification UI
    >
    > Bug: 1187190, 1093057
    > Change-Id: I5d3eefbaf081ba706364181c9509f7316999fb1b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3322593
    > Reviewed-by: Siddharth Shah <siashah@chromium.org>
    > Reviewed-by: Jared Saul <jsaul@google.com>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Commit-Queue: Siyu An <siyua@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#968592}
    
    Bug: 1187190, 1093057
    Change-Id: I016cd4085ee18a74d654d3e68e2b2c0bb71882af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452935
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970190}

--- chrome/browser/ui/autofill/payments/offer_notification_infobar_controller_impl.h
+++ chrome/browser/ui/autofill/payments/offer_notification_infobar_controller_impl.h
@@ -33,6 +33,9 @@ class OfferNotificationInfoBarControllerImpl {
   // origin.
   void ShowIfNecessary(const AutofillOfferData* offer, const CreditCard* card);
 
+  // Dismiss the infobar if it is visible.
+  void Dismiss();
+
  private:
   raw_ptr<content::WebContents> web_contents_;
 };
