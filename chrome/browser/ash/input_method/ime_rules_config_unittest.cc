9fa8842c857ec3791e2b54d9982305b1a6531951
David Vallet <dvallet@google.com>
2022-02-11 07:23:12 +0000

    Add additional default deny list for autocorrect
    BUG=b:196740952,b:200861149,b:202979739
    TEST=Unit tests
    
    	modified:   chrome/browser/ash/input_method/ime_rules_config.h
    	modified:   chrome/browser/ash/input_method/ime_rules_config_unittest.cc
    
    Change-Id: If321da6a646c7c5515edbc67677a915bf1865db0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453984
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: David Vallet <dvallet@google.com>
    Cr-Commit-Position: refs/heads/main@{#969836}

--- chrome/browser/ash/input_method/ime_rules_config_unittest.cc
+++ chrome/browser/ash/input_method/ime_rules_config_unittest.cc
@@ -46,7 +46,7 @@ class ImeRulesConfigTest : public testing::Test {
   ~ImeRulesConfigTest() override = default;
 
   std::vector<std::string> GetAutocorrectDomainDenylistForTest() {
-    return ImeRulesConfig::GetInstance()->auto_correct_domain_denylist_;
+    return ImeRulesConfig::GetInstance()->rule_auto_correct_domain_denylist_;
   }
 };
 
@@ -74,6 +74,10 @@ TEST_F(ImeRulesConfigTest, IsAutoCorrectDisabled) {
       FakeTextFieldContextualInfo(GURL("https://www.example.com"))));
   EXPECT_TRUE(rules->IsAutoCorrectDisabled(
       FakeTextFieldContextualInfo(GURL("https://test.com.au"))));
+  EXPECT_TRUE(rules->IsAutoCorrectDisabled(
+      FakeTextFieldContextualInfo(GURL("https://www.youtube.com"))));
+  EXPECT_TRUE(rules->IsAutoCorrectDisabled(
+      FakeTextFieldContextualInfo(GURL("https://b.corp.google.com/134"))));
   EXPECT_TRUE(rules->IsAutoCorrectDisabled(FakeTextFieldContextualInfo(
       GURL("https://docs.google.com/document/d/documentId/edit"))));
 }
