7b532ecb8e3fd886c2adf9d290a0e7d8654a9841
Xiaoling Bao <xiaolingbao@chromium.org>
2022-02-11 19:09:17 +0000

    Make system shim talk to user updater for user-level app.
    Service scope is chosen in the following order:
      1. If user explicitly specified the scope.
      2. Choose user scope if shim is user scope.
      3. Choose system updater if user is root.
      4. Prefer system updater if app ID is given and is a system app.
      5. Otherwise choose user updater.
    
    This change assumes that user can talk to system updater to GetAppState(), which is currently not the case:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/updater/app/server/mac/service_delegate.mm;l=325
    
    This CL also simplified command line parsing a bit.
    
    Bug: 1280037
    Change-Id: I8f5721e07c206926bf9d78650cdb14944ac133a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449793
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970056}

--- chrome/updater/mac/keystone/ksadmin_unittest.cc
+++ chrome/updater/mac/keystone/ksadmin_unittest.cc
@@ -64,6 +64,7 @@ TEST(KSAdminTest, ParseCommandLine) {
   std::map<std::string, std::string> arg_map =
       ParseCommandLine(std::size(argv), argv);
   EXPECT_EQ(arg_map.size(), size_t{5});
+  EXPECT_EQ(arg_map.count("register"), size_t{1});
   EXPECT_EQ(arg_map["register"], "");
   EXPECT_EQ(arg_map["P"], "com.google.kipple");
   EXPECT_EQ(arg_map["v"], "1.2.3.4");
@@ -77,6 +78,7 @@ TEST(KSAdminTest, ParseCommandLine_DiffByCase) {
   std::map<std::string, std::string> arg_map =
       ParseCommandLine(std::size(argv), argv);
   EXPECT_EQ(arg_map.size(), size_t{2});
+  EXPECT_EQ(arg_map.count("k"), size_t{1});
   EXPECT_EQ(arg_map["k"], "");
   EXPECT_EQ(arg_map["K"], "Tag");
 }
@@ -87,6 +89,7 @@ TEST(KSAdminTest, ParseCommandLine_CombinedShortOptions) {
   std::map<std::string, std::string> arg_map =
       ParseCommandLine(std::size(argv), argv);
   EXPECT_EQ(arg_map.size(), size_t{2});
+  EXPECT_EQ(arg_map.count("p"), size_t{1});
   EXPECT_EQ(arg_map["p"], "");
   EXPECT_EQ(arg_map["P"], "com.google.Chrome");
 }
