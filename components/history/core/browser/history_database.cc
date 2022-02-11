4b3d21e90efc440c53d0e1552397693f6d87632e
Sophie Chang <sophiechang@chromium.org>
2022-02-11 17:55:29 +0000

    Persist search normalized URL and search terms to visit content annotations db
    This allows us to pass the search terms to the clustering backend for
    history clusters for potentially better labeling and clustering, as well
    as avoid the templateurlservice call on the ui thread for the clustering
    backend
    
    Bug: 1295759,1294348
    Change-Id: Id1a1c15e696de6c669c84c5e0fb37b1b8fdd9ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453931
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970011}

--- components/history/core/browser/history_database.cc
+++ components/history/core/browser/history_database.cc
@@ -39,7 +39,7 @@ namespace {
 // Current version number. We write databases at the "current" version number,
 // but any previous version that can read the "compatible" one can make do with
 // our database without *too* many bad effects.
-const int kCurrentVersionNumber = 52;
+const int kCurrentVersionNumber = 53;
 const int kCompatibleVersionNumber = 16;
 const char kEarlyExpirationThresholdKey[] = "early_expiration_threshold";
 
@@ -671,6 +671,13 @@ sql::InitStatus HistoryDatabase::EnsureCurrentVersion() {
   if (cur_version == 51) {
     if (!MigrateEmbedderDownloadData())
       return LogMigrationFailure(51);
+    cur_version++;
+    meta_table_.SetVersionNumber(cur_version);
+  }
+
+  if (cur_version == 52) {
+    if (!MigrateContentAnnotationsAddSearchMetadata())
+      return LogMigrationFailure(52);
     cur_version++;
     meta_table_.SetVersionNumber(cur_version);
   }
