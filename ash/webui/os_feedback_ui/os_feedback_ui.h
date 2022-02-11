2a285d9df9a3393fc43277433e7d23bd0993760a
xiangdong kong <xiangdongkong@google.com>
2022-02-11 23:53:39 +0000

    feedback: Add initial mojom definition for help content provider
    This mojom interface provides a message pipe between the Feedback
    Tool SWA and the c++ handler. Followup CL will use the real mojom
    implementation instead of the fake mojom provide in the UI.
    
    - Will add more tests to increase code coverage in a follow Cl.
    
          browser_tests --gtest_filter="OSFeedbackBrowserTest.*"
    
    Bug: b:185624798
    Test: ash_webui_unittests --gtest_filter="HelpContentProviderTest.*"
    Change-Id: I6cf84b619599a1adece88cd9288ce00dea264aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449705
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#970223}

--- ash/webui/os_feedback_ui/os_feedback_ui.h
+++ ash/webui/os_feedback_ui/os_feedback_ui.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WEBUI_OS_FEEDBACK_UI_OS_FEEDBACK_UI_H_
 #define ASH_WEBUI_OS_FEEDBACK_UI_OS_FEEDBACK_UI_H_
 
+#include <memory>
+
+#include "ash/webui/os_feedback_ui/backend/help_content_provider.h"
+#include "ash/webui/os_feedback_ui/mojom/os_feedback_ui.mojom.h"
 #include "ui/webui/mojo_web_ui_controller.h"
 
 namespace content {
@@ -20,6 +24,12 @@ class OSFeedbackUI : public ui::MojoWebUIController {
   OSFeedbackUI(const OSFeedbackUI&) = delete;
   OSFeedbackUI& operator=(const OSFeedbackUI&) = delete;
   ~OSFeedbackUI() override;
+
+ private:
+  void BindInterface(
+      mojo::PendingReceiver<os_feedback_ui::mojom::HelpContentProvider>
+          receiver);
+  std::unique_ptr<feedback::HelpContentProvider> helpContentProvider_;
 };
 
 }  // namespace ash
