786c81fa359f9a985876d301fffeb81a6a5c6132
Andre Le <leandre@chromium.org>
2022-02-11 18:27:31 +0000

    NotificationRevamp: Add metrics and tests for some animations.
    Added performance metrics and unit tests for the bounds animation of
    expand button as well as the scale and translate animation of images.
    This CL also fixed the bounds animation that was broken.
    
    Bug: 1289915
    Change-Id: I66077acbcb8960c07994b74b880df1b00641392d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3439170
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970028}

--- ash/system/message_center/ash_notification_expand_button.h
+++ ash/system/message_center/ash_notification_expand_button.h
@@ -49,6 +49,11 @@ class AshNotificationExpandButton : public views::Button {
 
   // views::Button:
   void OnThemeChanged() override;
+  gfx::Size CalculatePreferredSize() const override;
+
+  void set_label_fading_out(bool label_fading_out) {
+    label_fading_out_ = label_fading_out;
+  }
 
   views::Label* label_for_test() { return label_; }
 
@@ -69,6 +74,9 @@ class AshNotificationExpandButton : public views::Button {
 
   // The expand state of the button.
   bool expanded_ = false;
+
+  // True if `label_` is in its fade out animation.
+  bool label_fading_out_ = false;
 
   base::WeakPtrFactory<AshNotificationExpandButton> weak_factory_{this};
 };
