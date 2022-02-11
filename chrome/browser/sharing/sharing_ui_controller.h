cd790b1b16cc8256bc14d5b537d24dda754e6b55
David Sanders <dsanders11@ucsbalum.com>
2022-02-11 13:51:32 +0000

    Remove unused include of //ui/views/controls/styled_label.h
    Bug: 242216
    Change-Id: Ie8d16a0007fb4e8ef2e6562947f226c001bd414b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3451173
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969917}

--- chrome/browser/sharing/sharing_ui_controller.h
+++ chrome/browser/sharing/sharing_ui_controller.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/protocol/device_info_specifics.pb.h"
 #include "components/sync_device_info/device_info.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "ui/views/controls/styled_label.h"
 #include "url/origin.h"
 
 class SharingDialog;
