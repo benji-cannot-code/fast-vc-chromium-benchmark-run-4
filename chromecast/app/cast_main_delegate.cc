7ff55ec4e092505687f63af9341db1ef1cfccf7e
Junbo Ke <juke@google.com>
2022-02-11 19:58:59 +0000

    [Chromecast] Remove pref config copy logic from cast_browser.
    Now that the process lifetimes of cast_browser and cast_service have
    been inverted, we should move the copy logic to cast_service.
    
    Bug: b:218707037
    Test: Run cast_service & cast_browser on desktop
    Change-Id: I7909068d518a4f24a603bdf6457f26b0e8c4dcf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453508
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970092}

--- chromecast/app/cast_main_delegate.cc
+++ chromecast/app/cast_main_delegate.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/base/process_types.h"
 #include "chromecast/browser/cast_content_browser_client.h"
 #include "chromecast/browser/cast_feature_list_creator.h"
-#include "chromecast/browser/migration/migration_utils.h"
 #include "chromecast/chromecast_buildflags.h"
 #include "chromecast/common/cast_resource_delegate.h"
 #include "chromecast/common/global_descriptors.h"
@@ -249,14 +248,6 @@ void CastMainDelegate::PostEarlyInitialization(bool is_running_tests) {
   CHECK(base::CreateDirectory(home_dir));
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-  const base::CommandLine* command_line =
-      base::CommandLine::ForCurrentProcess();
-  const bool use_browser_config =
-      command_line->HasSwitch(switches::kUseCastBrowserPrefConfig);
-  if (use_browser_config) {
-    cast_browser_migration::CopyPrefConfigsIfMissing();
-  }
-
   // TODO(crbug/1249485): If we're able to create the MetricsStateManager
   // earlier, clean up the below if and else blocks and call
   // MetricsStateManager::InstantiateFieldTrialList().
@@ -277,6 +268,10 @@ void CastMainDelegate::PostEarlyInitialization(bool is_running_tests) {
   // Initialize the base::FeatureList and the PrefService (which it depends on),
   // so objects initialized after this point can use features from
   // base::FeatureList.
+  const base::CommandLine* command_line =
+      base::CommandLine::ForCurrentProcess();
+  const bool use_browser_config =
+      command_line->HasSwitch(switches::kUseCastBrowserPrefConfig);
   ProcessType process_type = use_browser_config ? ProcessType::kCastBrowser
                                                 : ProcessType::kCastService;
   cast_feature_list_creator_->CreatePrefServiceAndFeatureList(process_type);
