3d4885e0c5e5053bdc0b1a72b8c2077a90bcc279
Rayan Kanso <rayankans@google.com>
2022-02-11 11:53:07 +0000

    [WebAPK] Make the shell apk version a feature param
    The default value will still be the value that is checked-in with the
    shell apk, but can be overridden as part of the feature.
    
    Change-Id: Ic6978f7acb2d5d0820375c11d208bd635fe65665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449264
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969898}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebApkUpdateManager.java
@@ -43,7 +43,6 @@ import org.chromium.components.webapps.WebApkUpdateReason;
 import org.chromium.components.webapps.WebappsIconUtils;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
-import org.chromium.webapk.lib.client.WebApkVersion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,6 +73,9 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer, De
 
     /** Whether updates are enabled. Some tests disable updates. */
     private static boolean sUpdatesEnabled = true;
+
+    /** The minimum shell version the WebAPK needs to be using. */
+    private static int sWebApkTargetShellVersion;
 
     /** Data extracted from the WebAPK's launch intent and from the WebAPK's Android Manifest. */
     private WebappInfo mInfo;
@@ -372,11 +374,18 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer, De
         }
     }
 
+    private static int webApkTargetShellVersion() {
+        if (sWebApkTargetShellVersion == 0) {
+            sWebApkTargetShellVersion = WebApkUpdateManagerJni.get().getWebApkTargetShellVersion();
+        }
+        return sWebApkTargetShellVersion;
+    }
+
     /**
      * Whether there is a new version of the //chrome/android/webapk/shell_apk code.
      */
     private static boolean isShellApkVersionOutOfDate(WebappInfo info) {
-        return info.shellApkVersion() < WebApkVersion.REQUEST_UPDATE_FOR_SHELL_APK_VERSION;
+        return info.shellApkVersion() < webApkTargetShellVersion();
     }
 
     /**
@@ -396,8 +405,7 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer, De
         if (!info.webApkPackageName().startsWith(WEBAPK_PACKAGE_PREFIX)) return false;
 
         if (isShellApkVersionOutOfDate(info)
-                && WebApkVersion.REQUEST_UPDATE_FOR_SHELL_APK_VERSION
-                        > mStorage.getLastRequestedShellApkVersion()) {
+                && webApkTargetShellVersion() > mStorage.getLastRequestedShellApkVersion()) {
             return true;
         }
 
@@ -415,8 +423,7 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer, De
         storage.updateTimeOfLastWebApkUpdateRequestCompletion();
         storage.updateDidLastWebApkUpdateRequestSucceed(result == WebApkInstallResult.SUCCESS);
         storage.setRelaxedUpdates(relaxUpdates);
-        storage.updateLastRequestedShellApkVersion(
-                WebApkVersion.REQUEST_UPDATE_FOR_SHELL_APK_VERSION);
+        storage.updateLastRequestedShellApkVersion(webApkTargetShellVersion());
     }
 
     /**
@@ -644,5 +651,6 @@ public class WebApkUpdateManager implements WebApkUpdateDataFetcher.Observer, De
                 boolean isAppIdentityUpdateSupported, int[] updateReasons,
                 Callback<Boolean> callback);
         public void updateWebApkFromFile(String updateRequestPath, WebApkUpdateCallback callback);
+        public int getWebApkTargetShellVersion();
     }
 }
---
+++ chrome/browser/android/webapk/webapk_features.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/webapk/webapk_features.h"
+
+const base::Feature kWebApkShellUpdate{"WebApkShellUpdate",
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::FeatureParam<int> kWebApkTargetShellVersion{
+    &kWebApkShellUpdate, "version", REQUEST_UPDATE_FOR_SHELL_APK_VERSION_VALUE};
