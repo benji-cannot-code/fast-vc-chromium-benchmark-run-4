a7ff8d80aceb78ad6a9ebe5d7db009ad4563c109
Akihiro Ota <akihiroota@chromium.org>
2022-02-11 21:00:29 +0000

    Dictation: Temporary workaround for spacing issue
    There is currently a bug in SODA (b/213934503) where final speech
    results do not start with a space. This results in a Dictation bug
    (crbug.com/1294050) where finalized speech results are not separated
    by a space when committed to a text field.
    
    This change ensures that Dictation prepends a space to an utterance if
    there is text directly left of the text caret. Once the blocking SODA
    bug is resolved, this workaround will be removed.
    
    AX-Relnotes: N/A
    Bug: 1294050
    Change-Id: Ia872c62295f643511ee6948c8fef44347b1a265f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441221
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970121}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/input_controller.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/dictation/input_controller.js
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {BubbleProperties} from './constants.js';
 
+const AutomationNode = chrome.automation.AutomationNode;
+const AutomationEvent = chrome.automation.AutomationEvent;
+const EventType = chrome.automation.EventType;
 const IconType = chrome.accessibilityPrivate.DictationBubbleIconType;
 
 /**
@@ -34,6 +37,15 @@ export class InputController {
     /** @private {?function():void} */
     this.onConnectCallback_ = null;
 
+    /**
+     * The currently focused editable node.
+     * @private {?AutomationNode}
+     */
+    this.editableNode_ = null;
+
+    /** @private {?EventHandler} */
+    this.focusHandler_ = null;
+
     this.initialize_();
   }
 
@@ -47,6 +59,16 @@ export class InputController {
         (context) => this.onImeFocus_(context));
     chrome.input.ime.onBlur.addListener(
         (contextId) => this.onImeBlur_(contextId));
+
+    // IME focus and blur listeners do not tell us which AutomationNode is
+    // currently focused. Register a focus event handler that will give us this
+    // information.
+    this.focusHandler_ = new EventHandler(
+        [], EventType.FOCUS, event => this.onFocusChanged_(event));
+    chrome.automation.getDesktop((desktop) => {
+      this.focusHandler_.setNodes(desktop);
+      this.focusHandler_.start();
+    });
   }
 
   /**
@@ -78,7 +100,7 @@ export class InputController {
    */
   saveCurrentInputMethodAndStart_(method) {
     this.previousImeEngineId_ = method;
-    // Add AccessibilityCommon as an input method and active it.
+    // Add AccessibilityCommon as an input method and activate it.
     chrome.languageSettingsPrivate.addInputMethod(
         InputController.IME_ENGINE_ID);
     chrome.inputMethodPrivate.setCurrentInputMethod(
@@ -133,6 +155,8 @@ export class InputController {
     if (!this.isActive()) {
       return;
     }
+
+    text = this.adjustCommitText_(text);
     chrome.input.ime.commitText({contextID: this.activeImeContextId_, text});
     this.setCurrentComposition('');
   }
@@ -186,9 +210,52 @@ export class InputController {
     }
   }
 
+  /**
+   * @param {!AutomationEvent} event
+   * @private
+   */
+  onFocusChanged_(event) {
+    const node = event.target;
+    if (!node || !AutomationPredicate.editText(node)) {
+      this.editableNode_ = null;
+      return;
+    }
+
+    this.editableNode_ = node;
+  }
+
   /** @param {string} text */
   setCurrentComposition(text) {
     this.currentComposition_ = text;
+  }
+
+  /**
+   * @param {string} text
+   * @return {string}
+   */
+  adjustCommitText_(text) {
+    // There is currently a bug in SODA (b/213934503) where final speech results
+    // do not start with a space. This results in a Dictation bug
+    // (crbug.com/1294050), where final speech results are not separated by a
+    // space when committed to a text field. This is a temporary workaround
+    // until the blocking SODA bug can be fixed. Note, a similar strategy
+    // already exists in Dictation::OnSpeechResult().
+    if (!this.editableNode_ ||
+        InputController.BEGINS_WITH_WHITESPACE_REGEX_.test(text)) {
+      return text;
+    }
+
+    const value = this.editableNode_.value;
+    const selStart = this.editableNode_.textSelStart;
+    const selEnd = this.editableNode_.textSelEnd;
+    // Prepend a space to `text` if there is text directly left of the cursor.
+    if (!selStart || selStart !== selEnd || !value ||
+        InputController.BEGINS_WITH_WHITESPACE_REGEX_.test(
+            value[selStart - 1])) {
+      return text;
+    }
+
+    return ' ' + text;
   }
 }
 
@@ -204,3 +271,9 @@ InputController.IME_ENGINE_ID =
  * @const
  */
 InputController.NO_ACTIVE_IME_CONTEXT_ID_ = -1;
+
+/**
+ * @private {!RegExp}
+ * @const
+ */
+InputController.BEGINS_WITH_WHITESPACE_REGEX_ = /^\s/;
