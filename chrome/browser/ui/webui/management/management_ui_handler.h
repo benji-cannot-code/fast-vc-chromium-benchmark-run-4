b00d3543bfbd875dc3de96f8a61ea63d6b2a9d16
Alberto Juarez <albertojuarez@google.com>
2022-02-11 00:58:54 +0000

    Audio new privacy string
    Adds "Audio peripherals attached" privacy string to
    ReportAudioStatus.
    
    Bug: b:192638329
    Change-Id: Id4656fbad199ac82bc019e98deb406eafd1b976c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438100
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Greg Levin <glevin@chromium.org>
    Commit-Queue: Alberto Juarez <albertojuarez@google.com>
    Cr-Commit-Position: refs/heads/main@{#969758}

--- chrome/browser/ui/webui/management/management_ui_handler.h
+++ chrome/browser/ui/webui/management/management_ui_handler.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // load time data.
 extern const char kManagementLogUploadEnabled[];
 extern const char kManagementReportActivityTimes[];
+extern const char kManagementReportDeviceAudioStatus[];
 extern const char kManagementReportNetworkData[];
 extern const char kManagementReportHardwareData[];
 extern const char kManagementReportUsers[];
