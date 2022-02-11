efaa6974b627b89ffce2f6e82e3ca989f1cbf0aa
David Benjamin <davidben@chromium.org>
2022-02-11 18:22:21 +0000

    Plumb the ECHConfigList from TransportConnectJob to SSLClientSocket
    With this CL, once the corresponding DNS bits have landed, TLS
    Encrypted ClientHello, without the recovery flow, should work for
    TCP-based HTTPS requests.
    
    A subsequent CL will fill in the recovery flow half. (This is all gated
    on the relevant feature flags so nothing is actually enabled yet.)
    
    Bug: 1091403
    Change-Id: Id2a6303d4aa8494a73c6aeb956b3d31e657dda19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3418661
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970025}

--- net/socket/connect_job.cc
+++ net/socket/connect_job.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <utility>
 
+#include "base/no_destructor.h"
 #include "base/trace_event/trace_event.h"
+#include "net/base/connection_endpoint_metadata.h"
 #include "net/base/net_errors.h"
 #include "net/base/trace_constants.h"
 #include "net/dns/public/secure_dns_policy.h"
@@ -133,6 +135,11 @@ bool ConnectJob::IsSSLError() const {
 
 scoped_refptr<SSLCertRequestInfo> ConnectJob::GetCertRequestInfo() {
   return nullptr;
+}
+
+const ConnectionEndpointMetadata& ConnectJob::GetEndpointMetadata() const {
+  static const base::NoDestructor<ConnectionEndpointMetadata> empty_metadata;
+  return *empty_metadata;
 }
 
 void ConnectJob::SetSocket(std::unique_ptr<StreamSocket> socket,
--- net/socket/ssl_connect_job.cc
+++ net/socket/ssl_connect_job.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
+#include "net/base/connection_endpoint_metadata.h"
 #include "net/base/features.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/net_errors.h"
@@ -378,6 +379,10 @@ int SSLConnectJob::DoSSLConnect() {
   ssl_config.network_isolation_key = params_->network_isolation_key();
   ssl_config.privacy_mode = params_->privacy_mode();
   ssl_config.disable_legacy_crypto = disable_legacy_crypto_with_fallback_;
+  if (base::FeatureList::IsEnabled(features::kEncryptedClientHello)) {
+    ssl_config.ech_config_list =
+        nested_connect_job_->GetEndpointMetadata().ech_config_list;
+  }
   ssl_socket_ = client_socket_factory()->CreateSSLClientSocket(
       ssl_client_context(), std::move(nested_socket_), params_->host_and_port(),
       ssl_config);
