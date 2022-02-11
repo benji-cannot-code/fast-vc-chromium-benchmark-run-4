fe227d57501d79fdc05b567a4dbd115d3dbcdb63
tby <tby@chromium.org>
2022-02-11 07:28:50 +0000

    Limit local files in Continue to edits in the last week
    This also adds the ability to delete items out of the Mrfu cache, which
    we do when a file is removed or falls out of the 7 day window.
    
    Fixed: 1295024, 1288739
    Change-Id: I1bbc8bf6a496c3be46c2592aad04a91493f6ae02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3454722
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969845}

--- chrome/browser/ui/app_list/search/files/zero_state_file_provider.h
+++ chrome/browser/ui/app_list/search/files/zero_state_file_provider.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/files/file_path.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
