30789e300f0c562acab3e38a94f427d47c06aa1a
Chris Harrelson <chrishtr@chromium.org>
2022-02-11 20:08:27 +0000

    Load all lazy iframes when printing
    Bug: 875403
    Change-Id: I045eda1f261f5c0da753777ca3a55a00c7bfc16f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3425848
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970094}

--- third_party/blink/renderer/core/html/lazy_load_frame_observer.h
+++ third_party/blink/renderer/core/html/lazy_load_frame_observer.h
@@ -65,6 +65,7 @@ class LazyLoadFrameObserver final
   void RecordMetricsOnLoadFinished();
 
   void LoadImmediately();
+  void LoadImmediatelyAndBlockLoadingEvent();
 
   void Trace(Visitor*) const;
 
