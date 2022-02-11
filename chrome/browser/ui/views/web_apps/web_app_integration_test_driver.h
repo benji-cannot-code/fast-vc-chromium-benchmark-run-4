ca21f359ecd6013903bddcf0e4dd1724c9b105a7
Finnur Thorarinsson <finnur@chromium.org>
2022-02-11 18:33:24 +0000

    [dPWA/AppId]: Add simple integration test for title update.
    Bug: 1292563
    Change-Id: I571a861dd1599bf58ee5675a10e9038ff333b7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448785
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970035}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
@@ -146,6 +146,7 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   // https://docs.google.com/spreadsheets/d/1d3iAOAnojp4_WrPky9exz1-mjkeulOJVUav5QYG99MQ/edit#gid=2008870403
 
   // State change actions:
+  void AcceptAppIdUpdateDialog();
   void CloseCustomToolbar();
   void ClosePwa();
   void DisableRunOnOSLoginMode(const std::string& site_mode);
@@ -168,6 +169,7 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   void NavigatePwaSiteATo(const std::string& site_mode);
   void NavigateNotfoundUrl();
   void NavigateTabbedBrowserToSite(const GURL& url);
+  void ManifestUpdateTitle(const std::string& site_mode);
   void ManifestUpdateDisplayMinimal(const std::string& site_mode);
   void ManifestUpdateScopeSiteAFooTo(const std::string& scope_mode);
   void SetOpenInTab(const std::string& site_mode);
@@ -187,6 +189,7 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   void CheckAppInListTabbed(const std::string& site_mode);
   void CheckAppNavigationIsStartUrl();
   void CheckAppNotInList(const std::string& site_mode);
+  void CheckAppTitleSiteA(const std::string& title);
   void CheckAppWindowMode(const std::string& site_mode,
                           apps::mojom::WindowMode window_mode);
   void CheckInstallable();
