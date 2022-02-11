561925f61eb6ad419479ce69aece06ae7c079fb5
Gauthier Ambard <gambard@chromium.org>
2022-02-11 16:32:57 +0000

    [iOS] Change String to Default Page Mode
    The subtitle should adapt to the selected option.
    
    Bug: 1276922
    Change-Id: I5857e3e4b6cb74825688848619bfa92184ec5724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455903
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969972}

--- ios/chrome/browser/ui/settings/content_settings/default_page_mode_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/content_settings/default_page_mode_table_view_controller.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/content_settings/default_page_mode_table_view_controller.h"
 
+#import "base/mac/foundation_util.h"
 #include "base/metrics/user_metrics.h"
 #import "ios/chrome/browser/ui/settings/content_settings/default_page_mode_table_view_controller_delegate.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.h"
@@ -72,7 +73,13 @@ typedef NS_ENUM(NSInteger, ItemType) {
 
   TableViewLinkHeaderFooterItem* footer =
       [[TableViewLinkHeaderFooterItem alloc] initWithType:ItemTypeFooter];
-  footer.text = l10n_util::GetNSString(IDS_IOS_DEFAULT_PAGE_MODE_SUBTITLE);
+  if (self.chosenItemType == ItemTypeDesktop) {
+    footer.text =
+        l10n_util::GetNSString(IDS_IOS_DEFAULT_PAGE_MODE_DESKTOP_SUBTITLE);
+  } else {
+    footer.text =
+        l10n_util::GetNSString(IDS_IOS_DEFAULT_PAGE_MODE_MOBILE_SUBTITLE);
+  }
   [model setFooter:footer forSectionWithIdentifier:SectionIdentifierMode];
 }
 
@@ -110,11 +117,25 @@ typedef NS_ENUM(NSInteger, ItemType) {
     }
   }
 
+  TableViewLinkHeaderFooterItem* footer =
+      base::mac::ObjCCastStrict<TableViewLinkHeaderFooterItem>(
+          [self.tableViewModel
+              footerForSectionWithIdentifier:SectionIdentifierMode]);
+  if (mode == DefaultPageModeDesktop) {
+    footer.text =
+        l10n_util::GetNSString(IDS_IOS_DEFAULT_PAGE_MODE_DESKTOP_SUBTITLE);
+  } else {
+    footer.text =
+        l10n_util::GetNSString(IDS_IOS_DEFAULT_PAGE_MODE_MOBILE_SUBTITLE);
+  }
+
   base::RecordAction(
       base::UserMetricsAction("MobileDefaultPageModeSettingsToggled"));
-  [self reloadCellsForItems:[self.tableViewModel itemsInSectionWithIdentifier:
-                                                     SectionIdentifierMode]
-           withRowAnimation:UITableViewRowAnimationAutomatic];
+  NSIndexSet* section = [NSIndexSet
+      indexSetWithIndex:[self.tableViewModel
+                            sectionForSectionIdentifier:SectionIdentifierMode]];
+  [self.tableView reloadSections:section
+                withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 #pragma mark - SettingsControllerProtocol
