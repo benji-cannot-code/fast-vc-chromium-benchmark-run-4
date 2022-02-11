e44d56d63d9bf69f098151cce59a36e60a3e1121
Yusuke Sato <yusukes@chromium.org>
2022-02-11 23:37:39 +0000

    reland: arc: Remove cast_receiver.mojom and related classes
    Those are unused.
    
    Diff since the last commit: None, but I've landed ag/16770763
    and ag/16770775 on the Android side. Those CLs have already been
    included in Chrome OS M100-14497.0.0 and 14498.0.0 respectively.
    
    BUG=b:184988296
    TEST=try, arc.Boot.vm
    
    Change-Id: Ie63bbe62b97fef07a6c2b752aa0dccdbe8f6af53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3430712
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970213}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/auth/arc_auth_service.h"
 #include "chrome/browser/ash/arc/bluetooth/arc_bluetooth_bridge.h"
 #include "chrome/browser/ash/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.h"
-#include "chrome/browser/ash/arc/cast_receiver/arc_cast_receiver_service.h"
 #include "chrome/browser/ash/arc/enterprise/arc_enterprise_reporting_service.h"
 #include "chrome/browser/ash/arc/enterprise/cert_store/cert_store_service.h"
 #include "chrome/browser/ash/arc/file_system_watcher/arc_file_system_watcher_service.h"
@@ -200,7 +199,6 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcBootErrorNotification::GetForBrowserContext(profile);
   ArcBootPhaseMonitorBridge::GetForBrowserContext(profile);
   ArcCameraBridge::GetForBrowserContext(profile);
-  ArcCastReceiverService::GetForBrowserContext(profile);
   ArcClipboardBridge::GetForBrowserContext(profile);
   ArcCrashCollectorBridge::GetForBrowserContext(profile);
   ArcDarkThemeBridge::GetForBrowserContext(profile);
