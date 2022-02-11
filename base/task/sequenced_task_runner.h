89042d0c8a6a6f592c9ad3608ce3adeb7edf6070
Henrik Boström <hbos@chromium.org>
2022-02-11 12:31:54 +0000

    [WebRTC] Migrate WebRtcMetronomeTaskQueue to PostDelayedTaskAt().
    MetronomeSource allows listeners to align their wakeups on metronome
    ticks.
    
    This CL refactors WebRtcMetronomeTaskQueue to align its tasks on
    metronome ticks using PostDelayedTaskAt() instead, which it can do using
    MetronomeSource::GetTimeSnappedToNextMetronomeTick().
    
    This takes us one step closer from deleting the complexity of
    MetronomeSource's ticking logic.
    
    Bug: chromium:1296138
    Change-Id: I2dd62fd883a067fb700e844494ce7873acd334b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452347
    Reviewed-by: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#969906}

--- base/task/sequenced_task_runner.h
+++ base/task/sequenced_task_runner.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class MetronomeSource;
 class TimerBase;
+class WebRtcMetronomeTaskQueue;
 }
 namespace webrtc {
 class ThreadWrapper;
@@ -49,6 +50,7 @@ class PostDelayedTaskPassKey {
   friend class base::DeadlineTimer;
   friend class blink::MetronomeSource;
   friend class blink::TimerBase;
+  friend class blink::WebRtcMetronomeTaskQueue;
   friend class PostDelayedTaskPassKeyForTesting;
   friend class webrtc::ThreadWrapper;
   friend class webrtc::WebRtcMetronomeTaskQueue;
