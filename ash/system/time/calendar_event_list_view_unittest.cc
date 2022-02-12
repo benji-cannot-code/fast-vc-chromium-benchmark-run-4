2c84642b8826796cdc8bb8aecc1a934499b503f9
Jiaming Cheng <jiamingc@chromium.org>
2022-02-12 00:05:47 +0000

    Implement the open google calendar button ui
    Previous the text font and round border was not implemented. As
    described in the bug:
    "The text "Open in Google calendar" appears. However, it does not look
    like a button, nor like a link. It doesn't look clickable."
    
    Also added tool tip and a11y labels to this button.
    
    Screenshot: https://screenshot.googleplex.com/Anum9KjtabSuWcF
    
    Bug: 1295332
    Change-Id: Ife7b815fabb8f827da5a68cdb32ecf18caa7fb27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3450652
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970229}

--- ash/system/time/calendar_event_list_view_unittest.cc
+++ ash/system/time/calendar_event_list_view_unittest.cc
@@ -5,11 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/time/calendar_event_list_view.h"
 
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/system/time/calendar_event_list_item_view.h"
 #include "ash/system/time/calendar_unittest_utils.h"
 #include "ash/system/time/calendar_view_controller.h"
 #include "ash/test/ash_test_base.h"
 #include "base/time/time.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/views/controls/button/label_button.h"
 #include "ui/views/controls/label.h"
 
 namespace ash {
@@ -70,6 +73,7 @@ class CalendarViewEventListViewTest : public AshTestBase {
     controller_->unified_system_tray_controller()
         ->calendar_model()
         ->InsertEvents(CreateMockEventList());
+    controller_->selected_date_ = date;
     event_list_view_ =
         std::make_unique<CalendarEventListView>(controller_.get());
   }
@@ -90,6 +94,13 @@ class CalendarViewEventListViewTest : public AshTestBase {
             ->summary_);
   }
 
+  std::u16string GetEmptyLabel() {
+    return static_cast<views::LabelButton*>(
+               static_cast<views::View*>(content_view()->children()[0])
+                   ->children()[0])
+        ->GetText();
+  }
+
  private:
   std::unique_ptr<CalendarEventListView> event_list_view_;
   std::unique_ptr<CalendarViewController> controller_;
@@ -101,11 +112,12 @@ TEST_F(CalendarViewEventListViewTest, ShowEvents) {
   base::Time date;
   ASSERT_TRUE(base::Time::FromString("18 Nov 2021 10:00 GMT", &date));
 
-  CreateEventListView(date);
+  CreateEventListView(date - base::Days(1));
 
-  // No events, so we see the empty list default.
+  // No events on 17 Nov 2021, so we see the empty list default.
   EXPECT_EQ(1u, content_view()->children().size());
-  EXPECT_EQ(u"Open in Google calendar", GetSummary(0)->GetText());
+  EXPECT_EQ(l10n_util::GetStringUTF16(IDS_ASH_CALENDAR_NO_EVENTS),
+            GetEmptyLabel());
 
   SetSelectedDate(date);
 
@@ -125,7 +137,8 @@ TEST_F(CalendarViewEventListViewTest, ShowEvents) {
 
   // 0 event on 20 Nov 2021.
   EXPECT_EQ(1u, content_view()->children().size());
-  EXPECT_EQ(u"Open in Google calendar", GetSummary(0)->GetText());
+  EXPECT_EQ(l10n_util::GetStringUTF16(IDS_ASH_CALENDAR_NO_EVENTS),
+            GetEmptyLabel());
 
   SetSelectedDate(date + base::Days(3));
 
