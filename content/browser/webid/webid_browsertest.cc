62a657bd6dab0d28860f85d5db29b10d8dac6af0
Yi Gu <yigu@chromium.org>
2022-02-12 05:36:45 +0000

    [FedCM] Fix incorrect manifest file name
    Per spec the file name should be "fedcm.json" instead of "fedcm".
    
    Bug: 1291405
    Change-Id: Iebb2ce4a92291141414fa296e22fee16b817bb68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3457383
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970307}

--- content/browser/webid/webid_browsertest.cc
+++ content/browser/webid/webid_browsertest.cc
@@ -47,7 +47,7 @@ namespace {
 
 constexpr char kRpHostName[] = "rp.example";
 constexpr char kIdpOrigin[] = "https://idp.example.org";
-constexpr char kExpectedManifestPath[] = "/fedcm";
+constexpr char kExpectedManifestPath[] = "/fedcm.json";
 constexpr char kIdpEndpointRelativeValue[] = "/fedcm/sign-in";
 constexpr char kTestManifestResponseBody[] =
     "{\"idp_endpoint\": \"/fedcm/sign-in\"}";
