3486bd1b37178e30e43fb31ffa79327780b74e33
Shakti Sahu <shaktisahu@chromium.org>
2022-02-12 01:02:47 +0000

    Added support for showing permission rationale UI for notifications
    This CL
    - Adds a NotificationPermissionController that will request notification
    permissions on startup at the interval of 7 days and optionally show
    a rationale.
    - Added two shared prefs to remember the timestamps of when last time
    rationale was shown, and when android prompt was shown.
    - The lower layer permission logic contained in
    AndroidPermissionDelegateWithRequester remains intact.
    - Added an AndroidManifest.xml file to chrome/android/junit. This is
    needed for using ActivityScenario and shadow activities in junit tests.
    
    Bug: 1293971
    Change-Id: I964119491d2ef50a8f04d633b96d0a87c9a387db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3438826
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Auto-Submit: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970256}

---
+++ chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/permissions/TestRationaleDelegate.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.notifications.permissions;
+
+import org.chromium.base.Callback;
+import org.chromium.chrome.browser.notifications.permissions.NotificationPermissionController.RationaleDelegate;
+import org.chromium.ui.modaldialog.DialogDismissalCause;
+
+/**
+ * Test implementation of {@link RationaleDelegate}.
+ */
+class TestRationaleDelegate implements RationaleDelegate {
+    private Integer mDialogAction;
+
+    @Override
+    public void showRationaleUi(Callback<Boolean> callback) {
+        assert mDialogAction != null;
+        if (mDialogAction == DialogDismissalCause.POSITIVE_BUTTON_CLICKED) {
+            callback.onResult(true);
+        } else {
+            callback.onResult(false);
+        }
+    }
+
+    /** Called by tests to set the user action to be taken when the dialog shows up.*/
+    void setDialogAction(Integer accept) {
+        mDialogAction = accept;
+    }
+}
