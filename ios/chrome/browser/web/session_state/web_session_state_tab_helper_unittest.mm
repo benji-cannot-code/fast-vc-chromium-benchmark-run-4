8b2295968faf0bdd6974ab7aeaff0da974cb424f
Justin Cohen <justincohen@google.com>
2022-02-12 23:53:52 +0000

    Revert "ios: Disable SessionRestorationBrowserAgentTest.SaveAndRestoreSession"
    This reverts commit f9ab2e8d3eb8a7dfb83194eb6cbcf7716f359988.
    
    Reason for revert: DCHECK should be ASSERT/EXPECT
    Original change's description:
    > ios: Disable SessionRestorationBrowserAgentTest.SaveAndRestoreSession
    >
    > Currently failing on devices.
    >
    > Bug: 1296739
    > Change-Id: Ia2e2037ad4e8cea98f0c3002d5b6252bf924ac66
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456590
    > Owners-Override: Chris Lu <thegreenfrog@google.com>
    > Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    > Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#970193}
    
    Bug: 1296739
    Change-Id: I335de4ef69ea6c79737984bee0fa05771e39b6d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3458842
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970361}

--- ios/chrome/browser/web/session_state/web_session_state_tab_helper_unittest.mm
+++ ios/chrome/browser/web/session_state/web_session_state_tab_helper_unittest.mm
@@ -78,7 +78,7 @@ TEST_F(WebSessionStateTabHelperTest, DisableFeature) {
   GURL url(kChromeUIAboutNewTabURL);
   web::NavigationManager::WebLoadParams params(url);
   web_state()->GetNavigationManager()->LoadURLWithParams(params);
-  DCHECK(WaitUntilConditionOrTimeout(kWaitForPageLoadTimeout, ^bool {
+  ASSERT_TRUE(WaitUntilConditionOrTimeout(kWaitForPageLoadTimeout, ^bool {
     return !web_state()->IsLoading();
   }));
   helper->SaveSessionState();
@@ -102,7 +102,7 @@ TEST_F(WebSessionStateTabHelperTest, SessionStateRestore) {
   GURL url(kChromeUIAboutNewTabURL);
   web::NavigationManager::WebLoadParams params(url);
   web_state()->GetNavigationManager()->LoadURLWithParams(params);
-  DCHECK(WaitUntilConditionOrTimeout(kWaitForPageLoadTimeout, ^bool {
+  ASSERT_TRUE(WaitUntilConditionOrTimeout(kWaitForPageLoadTimeout, ^bool {
     return !web_state()->IsLoading();
   }));
   // As well as waiting for the page to finish loading, it seems an extra wait
