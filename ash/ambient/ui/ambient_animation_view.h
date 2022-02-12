2afff35e9fbb9348cfe2e9f0cd0004f71b8f6a08
Eric Sum <esum@google.com>
2022-02-12 01:55:09 +0000

    Hook up all the screen burn elements in AmbientAnimationView.
    The screen burn config parameters all match guidance from
    ChromeOS screen burn experts. The config may be adjusted later
    when we get the weather/time/media string specs from UX, but
    this is good for now.
    
    It is very difficult to add tests for this at the view level
    of the stack, but most of the actual application logic is
    already unit tested in previous CLs. The view just hooks
    everything up.
    
    Bug: b:217271333
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Icc36a7e94b76807e1fc2dfc19ca2d87993bb33ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453876
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#970272}

--- ash/ambient/ui/ambient_animation_view.h
+++ ash/ambient/ui/ambient_animation_view.h
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ambient/model/ambient_animation_photo_provider.h"
+#include "ash/ambient/ui/jitter_calculator.h"
 #include "ash/ash_export.h"
 #include "base/scoped_observation.h"
+#include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/compositor/throughput_tracker.h"
@@ -65,6 +67,9 @@ class ASH_EXPORT AmbientAnimationView : public views::View,
 
   absl::optional<ui::ThroughputTracker> throughput_tracker_;
   base::RepeatingTimer throughput_tracker_restart_timer_;
+
+  JitterCalculator animation_jitter_calculator_;
+  base::TimeTicks last_jitter_timestamp_;
 };
 
 }  // namespace ash
