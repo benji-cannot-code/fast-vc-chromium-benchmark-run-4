ebb3fc4e7c3279e936f138cde287adce0b64aa7d
Oriol Brufau <obrufau@igalia.com>
2022-02-14 20:28:52 +0000

    [inert] Add a11y dump tree test for inert attribute
    Bug: 692360
    
    TEST=All/DumpAccessibilityTreeTestWithIgnoredNodes.InertAttribute/blink
    
    AX-Relnotes: n/a
    
    Change-Id: I98581e36c1412d0e3baf003f0a991ec7f3324329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461360
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#970792}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -2120,6 +2120,13 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityInPageLinks) {
   RunHtmlTest(FILE_PATH_LITERAL("in-page-links.html"));
 }
 
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTestWithIgnoredNodes,
+                       InertAttribute) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kEnableBlinkFeatures, "InertAttribute");
+  RunHtmlTest(FILE_PATH_LITERAL("inert-attribute.html"));
+}
+
 // TODO(crbug.com/1193963): fails on Windows.
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityInputButton) {
   RunHtmlTest(FILE_PATH_LITERAL("input-button.html"));
@@ -2551,7 +2558,7 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 */
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTestWithIgnoredNodes,
-                       AccessibilityModalDialogAndIframe) {
+                       AccessibilityModalDialogAndIframes) {
   RunHtmlTest(FILE_PATH_LITERAL("modal-dialog-and-iframes.html"));
 }
 
