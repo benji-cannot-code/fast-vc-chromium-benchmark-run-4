697fed47166045f9437e33da47c5eb52841c3e1a
Peter Kasting <pkasting@chromium.org>
2022-02-14 22:35:00 +0000

    Reorder GTK color mixer definitions.
    Split core concepts from UI concepts and alphabetize.  Inline the alert
    colors.
    
    Bug: none
    Change-Id: I73aea10460a23fd7c7fbe72253c1b5e2bdddf736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462093
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970871}

--- ui/gtk/gtk_color_mixers.h
+++ ui/gtk/gtk_color_mixers.h
@@ -14,8 +14,8 @@ class ColorProvider;
 
 namespace gtk {
 
-void AddGtkNativeCoreColorMixer(ui::ColorProvider* provider,
-                                const ui::ColorProviderManager::Key& key);
+void AddGtkNativeColorMixers(ui::ColorProvider* provider,
+                             const ui::ColorProviderManager::Key& key);
 
 }  // namespace gtk
 
