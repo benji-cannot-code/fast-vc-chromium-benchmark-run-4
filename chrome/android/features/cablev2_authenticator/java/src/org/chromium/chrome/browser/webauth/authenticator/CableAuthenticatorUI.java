56f920a48a1c016b65e57184d7858f059fb6bde0
Adam Langley <agl@chromium.org>
2022-02-14 21:05:23 +0000

    webauthn: maybe record events for the BLE permission.
    On Android 12 there's a new permission for sending BLE adverts. When
    doing server-link (i.e. a sign-in on accounts.google.com) and when the
    user opted in UMA, record events for this permission prompt so that we
    can see what the drop-off rate is.
    
    BUG=1002262
    
    Change-Id: Ie1cea8d69c1911543915a17bcf13eeea01e8d1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460368
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970819}

--- chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticatorUI.java
+++ chrome/android/features/cablev2_authenticator/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticatorUI.java
@@ -88,6 +88,13 @@ public class CableAuthenticatorUI extends Fragment implements OnClickListener {
     private static final int ERROR_AUTHENTICATOR_SELECTION_RECEIVED = 114;
     private static final int ERROR_DISCOVERABLE_CREDENTIALS_REQUEST = 115;
 
+    // These entries duplicate some of the enum values from
+    // `CableV2MobileEvent`. The C++ enum is the source of truth for these
+    // values.
+    private static final int EVENT_BLUETOOTH_ADVERTISE_PERMISSION_REQUESTED = 23;
+    private static final int EVENT_BLUETOOTH_ADVERTISE_PERMISSION_GRANTED = 24;
+    private static final int EVENT_BLUETOOTH_ADVERTISE_PERMISSION_REJECTED = 25;
+
     private enum Mode {
         QR, // QR code scanned by external app.
         FCM, // Triggered by user selecting notification; handshake already running.
@@ -350,6 +357,8 @@ public class CableAuthenticatorUI extends Fragment implements OnClickListener {
                     // permission.
                     if (BuildInfo.isAtLeastS() && requestBluetoothPermissions()) {
                         mState = State.BLUETOOTH_ADVERTISE_PERMISSION_REQUESTED;
+                        mAuthenticator.maybeRecordEvent(
+                                EVENT_BLUETOOTH_ADVERTISE_PERMISSION_REQUESTED);
                         return;
                     }
 
@@ -360,6 +369,7 @@ public class CableAuthenticatorUI extends Fragment implements OnClickListener {
                     if (event != Event.PERMISSIONS_GRANTED) {
                         return;
                     }
+                    mAuthenticator.maybeRecordEvent(EVENT_BLUETOOTH_ADVERTISE_PERMISSION_GRANTED);
                     mState = State.BLUETOOTH_READY;
                     break;
 
@@ -619,6 +629,9 @@ public class CableAuthenticatorUI extends Fragment implements OnClickListener {
             return;
         }
 
+        if (mState == State.BLUETOOTH_ADVERTISE_PERMISSION_REQUESTED) {
+            mAuthenticator.maybeRecordEvent(EVENT_BLUETOOTH_ADVERTISE_PERMISSION_REJECTED);
+        }
         mState = State.ERROR;
         mErrorCode = ERROR_NO_BLUETOOTH_PERMISSION;
         updateUiForState();
