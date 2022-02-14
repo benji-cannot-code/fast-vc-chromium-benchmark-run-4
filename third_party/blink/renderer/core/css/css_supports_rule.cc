734342cd5cf24024f3d2f2d8f16e287bb47e3939
Changhao Han <changhaohan@chromium.org>
2022-02-14 11:46:02 +0000

    Add CSS.setSupportsText in CDP for editing @supports rule
    This CL adds a CDP command CSS.setSupportsText to edit the prelude of a
    @supports at-rule. To do this, this CL also adds a SetConditionText()
    to StyleRuleSupports.
    
    Bug: 1222574, 1222573
    Change-Id: I6553b689ce194f42dfbebb4b405cf6c19e6202af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460864
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970575}

--- third_party/blink/renderer/core/css/css_supports_rule.cc
+++ third_party/blink/renderer/core/css/css_supports_rule.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_supports_rule.h"
 
 #include "third_party/blink/renderer/core/css/css_rule.h"
+#include "third_party/blink/renderer/core/css/css_style_sheet.h"
 #include "third_party/blink/renderer/core/css/style_rule.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
@@ -49,6 +50,14 @@ String CSSSupportsRule::cssText() const {
   result.Append('}');
 
   return result.ReleaseString();
+}
+
+void CSSSupportsRule::SetConditionText(
+    const ExecutionContext* execution_context,
+    String value) {
+  CSSStyleSheet::RuleMutationScope mutation_scope(this);
+  To<StyleRuleSupports>(group_rule_.Get())
+      ->SetConditionText(execution_context, value);
 }
 
 }  // namespace blink
