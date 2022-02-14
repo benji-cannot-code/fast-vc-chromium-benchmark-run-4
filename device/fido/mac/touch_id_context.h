135f4871affeff9d5d393c2b184f583cbed320ef
Mark Mentovai <mark@chromium.org>
2022-02-14 20:05:23 +0000

    fido/mac: Use SecTask instead of blocking and broken SecCode
    device::fido::mac::TouchIdContext wants to determine whether the main
    executable is signed with the entitlement required for Touch ID to work
    before advertising this capability to the web. Prior to this change, it
    did so by examining the main executable’s entitlements via the SecCode
    interface. SecCode (specifically SecStaticCode, but you can’t avoid
    SecCode’s use of SecStaticCode) pokes around on the disk. This means
    that it’s potentially blocking, and there’s a disassociation between the
    effective entitlements (set at launch time) and the entitlements found
    on disk (because the main executable’s path may no longer correspond to
    the vnode associated with the running executable). Where an application
    has been updated on disk but not yet relaunched, the file may be
    completely different, and other bugs in SecCode may result in it
    erroneously determining that no entitlements are present at all, even
    when both the old and new files on disk were properly entitled.
    
    Instead of SecCode, SecTask will now be used. Internally, SecTask
    consults the live copy of code signing attributes in the kernel, without
    touching the disk at all. SecTask’s view of the process’ entitlements is
    thus consistent with the process’ effective entitlements, and can be
    obtained without blocking.
    
    This preserves the application’s ability to use Touch ID even when an
    update has been applied but the application has not yet been relaunched,
    while also preserving the application’s ability to check whether it has
    been signed with the appropriate entitlement to access this feature.
    
    Bug: 1295403, 1296607
    Change-Id: I69b6f5a23627c4c057ebab46534aff6a68a8ec50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462051
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970773}

--- device/fido/mac/touch_id_context.h
+++ device/fido/mac/touch_id_context.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DEVICE_FIDO_MAC_TOUCH_ID_CONTEXT_H_
 
 #import <LocalAuthentication/LocalAuthentication.h>
-#import <Security/Security.h>
+#include <Security/Security.h>
 
 #include <string>
 
@@ -45,7 +45,7 @@ class COMPONENT_EXPORT(DEVICE_FIDO)
   static std::unique_ptr<TouchIdContext> Create();
 
   // Returns whether the device has a secure enclave and can authenticate the
-  // local user, and whether the current binary carries a
+  // local user, and whether the main executable carries a
   // keychain-access-groups entitlement that matches the one set in |config|.
   static void TouchIdAvailable(AuthenticatorConfig config,
                                base::OnceCallback<void(bool is_available)>);
@@ -76,8 +76,8 @@ class COMPONENT_EXPORT(DEVICE_FIDO)
   using CreateFuncPtr = decltype(&Create);
   static CreateFuncPtr g_create_;
 
-  static bool TouchIdAvailableImplBlocking(AuthenticatorConfig config);
-  using TouchIdAvailableFuncPtr = decltype(&TouchIdAvailableImplBlocking);
+  static bool TouchIdAvailableImpl(AuthenticatorConfig config);
+  using TouchIdAvailableFuncPtr = decltype(&TouchIdAvailableImpl);
   static TouchIdAvailableFuncPtr g_touch_id_available_;
 
   static std::unique_ptr<TouchIdContext> CreateImpl();
--- device/fido/mac/touch_id_context.mm
+++ device/fido/mac/touch_id_context.mm
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/fido/mac/touch_id_context.h"
 
-#import <CoreFoundation/CoreFoundation.h>
-#import <Security/Security.h>
+#include <CoreFoundation/CoreFoundation.h>
+#import <Foundation/Foundation.h>
+#include <Security/Security.h>
 
 #include "base/bind.h"
 #include "base/logging.h"
@@ -40,46 +41,32 @@ base::ScopedCFTypeRef<SecAccessControlRef> DefaultAccessControl() {
           nullptr));
 }
 
-// Returns whether the current binary is signed with a keychain-access-groups
+// Returns whether the main executable is signed with a keychain-access-groups
 // entitlement that contains |keychain_access_group|. This is required for the
 // TouchIdAuthenticator to access key material stored in the Touch ID secure
 // enclave.
-bool BinaryHasKeychainAccessGroupEntitlementBlocking(
+bool ExecutableHasKeychainAccessGroupEntitlement(
     const std::string& keychain_access_group) {
-  // This method makes call into the macOS Security Framework, which may block.
-  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
-                                                base::BlockingType::MAY_BLOCK);
+  base::ScopedCFTypeRef<SecTaskRef> task(SecTaskCreateFromSelf(nullptr));
+  if (!task) {
+    return false;
+  }
 
-  base::ScopedCFTypeRef<SecCodeRef> code;
-  if (SecCodeCopySelf(kSecCSDefaultFlags, code.InitializeInto()) !=
-      errSecSuccess) {
+  base::ScopedCFTypeRef<CFTypeRef> entitlement_value_cftype(
+      SecTaskCopyValueForEntitlement(task, CFSTR("keychain-access-groups"),
+                                     nullptr));
+  if (!entitlement_value_cftype) {
     return false;
   }
-  base::ScopedCFTypeRef<CFDictionaryRef> signing_info;
-  if (SecCodeCopySigningInformation(code, kSecCSDefaultFlags,
-                                    signing_info.InitializeInto()) !=
-      errSecSuccess) {
+
+  NSArray* entitlement_value_nsarray = base::mac::CFToNSCast(
+      base::mac::CFCast<CFArrayRef>(entitlement_value_cftype));
+  if (!entitlement_value_nsarray) {
     return false;
   }
-  CFDictionaryRef entitlements =
-      base::mac::GetValueFromDictionary<CFDictionaryRef>(
-          signing_info, kSecCodeInfoEntitlementsDict);
-  if (!entitlements) {
-    return false;
-  }
-  CFArrayRef keychain_access_groups =
-      base::mac::GetValueFromDictionary<CFArrayRef>(
-          entitlements,
-          base::ScopedCFTypeRef<CFStringRef>(
-              base::SysUTF8ToCFStringRef("keychain-access-groups")));
-  if (!keychain_access_groups) {
-    return false;
-  }
-  return CFArrayContainsValue(
-      keychain_access_groups,
-      CFRangeMake(0, CFArrayGetCount(keychain_access_groups)),
-      base::ScopedCFTypeRef<CFStringRef>(
-          base::SysUTF8ToCFStringRef(keychain_access_group)));
+
+  return [entitlement_value_nsarray
+      containsObject:base::SysUTF8ToNSString(keychain_access_group)];
 }
 
 // Returns whether creating a key pair in the secure enclave succeeds. Keys are
@@ -124,12 +111,12 @@ std::unique_ptr<TouchIdContext> TouchIdContext::Create() {
 }
 
 // static
-bool TouchIdContext::TouchIdAvailableImplBlocking(AuthenticatorConfig config) {
-  // Ensure that the binary is signed with the keychain-access-group
+bool TouchIdContext::TouchIdAvailableImpl(AuthenticatorConfig config) {
+  // Ensure that the main executable is signed with the keychain-access-group
   // entitlement that is configured by the embedder; that user authentication
   // with biometry, watch, or device passcode possible; and that the device has
   // a secure enclave.
-  if (!BinaryHasKeychainAccessGroupEntitlementBlocking(
+  if (!ExecutableHasKeychainAccessGroupEntitlement(
           config.keychain_access_group)) {
     FIDO_LOG(ERROR)
         << "Touch ID authenticator unavailable because keychain-access-group "
@@ -155,7 +142,7 @@ bool TouchIdContext::TouchIdAvailableImplBlocking(AuthenticatorConfig config) {
 
 // Testing seam to allow faking Touch ID in tests.
 TouchIdContext::TouchIdAvailableFuncPtr TouchIdContext::g_touch_id_available_ =
-    &TouchIdContext::TouchIdAvailableImplBlocking;
+    &TouchIdContext::TouchIdAvailableImpl;
 
 // static
 void TouchIdContext::TouchIdAvailable(
