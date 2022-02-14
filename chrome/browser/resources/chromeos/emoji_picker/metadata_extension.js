0c3b0b480700d2c37218370b52d3bc93bbf56085
Chuong Ho <hdchuong@google.com>
2022-02-14 14:16:14 +0000

    emoji picker: remove symbol category
    Because there is no symbol data at the moment to be rendered, we want to
    remove symbol category from the UI.
    
    Refer to b/213141035 to add the symbol category back.
    
    Screenshot: https://screenshot.googleplex.com/AaFRj8bHZjZddga
    Change-Id: I4398f2b0f165b9b1ce47cd26eb74dc537d380fc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3362007
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#970612}

--- chrome/browser/resources/chromeos/emoji_picker/metadata_extension.js
+++ chrome/browser/resources/chromeos/emoji_picker/metadata_extension.js
@@ -9,11 +9,7 @@ export const CATEGORY_METADATA = [
     icon: 'emoji_picker:insert_emoticon',
     active: true,
   },
-  {
-    name: 'symbol',
-    icon: 'emoji_picker_v2:symbol_omega',
-    active: false,
-  },
+  // TODO(b/213141035): insert symbols metadata back.
   {
     name: 'emoticon',
     icon: 'emoji_picker_v2:emoticon_gssmiley',
