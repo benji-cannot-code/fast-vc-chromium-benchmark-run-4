faf6aa4d7968708ca4e51bd848c8fdee7a452099
Alberto Juarez <albertojuarez@google.com>
2022-02-14 20:42:52 +0000

    Add ERP enable/disable policy
    New device policy proto to start having configurations
    for the Encrypted Reporting Pipeline in the future,
    starting with the red button policy to disable the
    ERP if we need to.
    
    Bug: b:214043929
    Change-Id: Ia33b1b74857c001cb1d02fda306f3e9c6bebf432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3407782
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Alberto Juarez <albertojuarez@google.com>
    Cr-Commit-Position: refs/heads/main@{#970801}

--- chrome/browser/ash/settings/device_settings_provider.cc
+++ chrome/browser/ash/settings/device_settings_provider.cc
@@ -82,6 +82,7 @@ const char* const kKnownSettings[] = {
     kDeviceDisabledMessage,
     kDeviceDisplayResolution,
     kDeviceDockMacAddressSource,
+    kDeviceEncryptedReportingPipelineEnabled,
     kDeviceHostnameTemplate,
     kDeviceHostnameUserConfigurable,
     kDeviceLoginScreenInputMethods,
@@ -1269,6 +1270,15 @@ void DecodeGenericPolicies(const em::ChromeDeviceSettingsProto& policy,
       policy.hardware_data_usage_enabled().hardware_data_usage_enabled();
   new_values_cache->SetBoolean(kRevenEnableDeviceHWDataUsage,
                                reven_enable_device_hw_data_usage);
+
+  if (policy.has_device_encrypted_reporting_pipeline_enabled()) {
+    const em::EncryptedReportingPipelineConfigurationProto& container(
+        policy.device_encrypted_reporting_pipeline_enabled());
+    if (container.has_enabled()) {
+      new_values_cache->SetValue(kDeviceEncryptedReportingPipelineEnabled,
+                                 base::Value(container.enabled()));
+    }
+  }
 }
 
 void DecodeLogUploadPolicies(const em::ChromeDeviceSettingsProto& policy,
