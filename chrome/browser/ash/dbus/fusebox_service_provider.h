1e8351e22aae526c00645c6e09d8d53ecada1085
Nigel Tao <nigeltao@chromium.org>
2022-02-14 23:14:50 +0000

    Do FuseBoxServiceProvider enabled check once
    Do this once (in FuseBoxServiceProvider::Start) instead of once per
    D-Bus call. This lets us remove the enabled_ field as well as move the
    ParseResult type and ParseCommonDBusMethodArguments function to the .cc
    file, simplifying the .h file.
    
    This is a slight behavior change in that, when FuseBox is not enabled,
    D-Bus methods will get a "no such method" D-Bus error instead of a
    base::File::Error::FILE_ERROR_FAILED error code, but the only D-Bus
    client we're expecting to make those calls (the /usr/bin/fusebox) binary
    (1) treats both errors similarly and (2) shouldn't even be run when
    FuseBox is not enabled.
    
    Bug: 1249754
    Change-Id: I9da9fb2017648f83dce250bffc9a9edf40e27991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453083
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970891}

--- chrome/browser/ash/dbus/fusebox_service_provider.h
+++ chrome/browser/ash/dbus/fusebox_service_provider.h
@@ -6,14 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_DBUS_FUSEBOX_SERVICE_PROVIDER_H_
 #define CHROME_BROWSER_ASH_DBUS_FUSEBOX_SERVICE_PROVIDER_H_
 
-#include "base/files/file.h"
-#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/dbus/services/cros_dbus_service.h"
 #include "dbus/exported_object.h"
-#include "dbus/message.h"
-#include "storage/browser/file_system/file_system_context.h"
-#include "storage/browser/file_system/file_system_url.h"
 
 namespace ash {
 
@@ -31,27 +26,6 @@ class FuseBoxServiceProvider
   void Start(scoped_refptr<dbus::ExportedObject> object) override;
 
  private:
-  // ParseResult is the type returned by ParseCommonDBusMethodArguments. It is
-  // a result type (see https://en.wikipedia.org/wiki/Result_type), being
-  // either an error or a value. In this case, the error type is a
-  // base::File::Error (a numeric code) and the value type is a pair of
-  // storage::FileSystemContext and storage::FileSystemURL.
-  struct ParseResult {
-    explicit ParseResult(base::File::Error error_code_arg);
-    ParseResult(scoped_refptr<storage::FileSystemContext> fs_context_arg,
-                storage::FileSystemURL fs_url_arg);
-    ~ParseResult();
-
-    base::File::Error error_code;
-    scoped_refptr<storage::FileSystemContext> fs_context;
-    storage::FileSystemURL fs_url;
-  };
-
-  // All of the D-Bus methods' arguments start with a FileSystemURL (as a
-  // string). This method consumes and parses that first argument, as well as
-  // finding the FileSystemContext we will need to serve those methods.
-  ParseResult ParseCommonDBusMethodArguments(dbus::MessageReader* reader);
-
   // D-Bus methods.
   //
   // In terms of semantics, they're roughly equivalent to the C standard
@@ -61,9 +35,6 @@ class FuseBoxServiceProvider
   // TODO(nigeltao): add Open, Read, ReadDir, etc.
   void Stat(dbus::MethodCall* method,
             dbus::ExportedObject::ResponseSender sender);
-
-  // True if the FuseBoxService is enabled.
-  bool const enabled_;
 
   // base::WeakPtr{this} factory.
   base::WeakPtrFactory<FuseBoxServiceProvider> weak_ptr_factory_{this};
