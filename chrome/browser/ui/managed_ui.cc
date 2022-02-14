32e4bb9ce3427b97acf2438c3f0832adfa21c43c
Jan Krcal <jkrcal@chromium.org>
2022-02-14 12:14:46 +0000

    [Profiles] Adapt profile creation enterprise disclaimer on lacros
    This CL changes the device management string in profile creation flow
    to a session management string, that fits better to lacros management
    model.
    
    Bug: 1296308
    Change-Id: Iefe07b5bd68e8a6ecaeab7420af5d676eec534e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3454002
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Commit-Queue: Jan Krčál <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970581}

--- chrome/browser/ui/managed_ui.cc
+++ chrome/browser/ui/managed_ui.cc
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/user_cloud_policy_manager.h"
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "components/policy/core/common/policy_loader_lacros.h"
+#endif
+
 namespace chrome {
 
 namespace {
@@ -143,6 +147,14 @@ absl::optional<std::string> GetDeviceManagerIdentity() {
                           ->machine_level_user_cloud_policy_manager());
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+absl::optional<std::string> GetSessionManagerIdentity() {
+  if (!policy::PolicyLoaderLacros::IsMainUserManaged())
+    return absl::nullopt;
+  return policy::PolicyLoaderLacros::main_user_policy_data()->managed_by();
+}
+#endif
 
 absl::optional<std::string> GetAccountManagerIdentity(Profile* profile) {
   if (!policy::ManagementServiceFactory::GetForProfile(profile)->IsManaged())
