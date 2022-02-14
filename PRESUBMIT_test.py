1742da908630458cef2ba7caf91ad5b76dad8195
Kevin McNee <mcnee@chromium.org>
2022-02-14 20:02:04 +0000

    Include explanatory text when adding the MPArch watchlist during PRESUBMIT
    We now have a good idea of the average volume of CLs affected by this
    PRESUBMIT (0 to 9 per day, typically around 4). It seems like we could
    add some guidance here without being too spammy.
    
    Bug: 1190019
    Change-Id: Iaea9f969c6af7b651fef88290ca1012dabbcd2d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3457088
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970772}

--- PRESUBMIT_test.py
+++ PRESUBMIT_test.py
@@ -4140,21 +4140,26 @@ class CheckDeprecationOfPreferencesTest(unittest.TestCase):
         errors[0].message)
 
 class MPArchApiUsage(unittest.TestCase):
-  def _assert_notify(self, expect_cc, msg, local_path, new_contents):
+  def _assert_notify(
+      self, expected_uses, msg, local_path, new_contents):
     mock_input_api = MockInputApi()
     mock_output_api = MockOutputApi()
     mock_input_api.files = [
         MockFile(local_path, new_contents),
     ]
-    PRESUBMIT.CheckMPArchApiUsage(mock_input_api, mock_output_api)
+    result = PRESUBMIT.CheckMPArchApiUsage(mock_input_api, mock_output_api)
     self.assertEqual(
-        expect_cc,
+        bool(expected_uses),
         'mparch-reviews+watch@chromium.org' in mock_output_api.more_cc,
         msg)
+    if expected_uses:
+        self.assertEqual(1, len(result), msg)
+        self.assertEqual(result[0].type, 'notify', msg)
+        self.assertEqual(sorted(result[0].items), sorted(expected_uses), msg)
 
   def testNotify(self):
     self._assert_notify(
-        True,
+        ['WebContentsObserver', 'WebContentsUserData'],
         'Introduce WCO and WCUD',
         'chrome/my_feature.h',
         ['class MyFeature',
@@ -4162,14 +4167,14 @@ class MPArchApiUsage(unittest.TestCase):
          '      public content::WebContentsUserData<MyFeature> {};',
         ])
     self._assert_notify(
-        True,
+        ['DidFinishNavigation'],
         'Introduce WCO override',
         'chrome/my_feature.h',
         ['void DidFinishNavigation(',
          '    content::NavigationHandle* navigation_handle) override;',
         ])
     self._assert_notify(
-        True,
+        ['IsInMainFrame'],
         'Introduce IsInMainFrame',
         'chrome/my_feature.cc',
         ['void DoSomething(content::NavigationHandle* navigation_handle) {',
@@ -4178,7 +4183,7 @@ class MPArchApiUsage(unittest.TestCase):
          '}',
         ])
     self._assert_notify(
-        True,
+        ['FromRenderFrameHost'],
         'Introduce WC::FromRenderFrameHost',
         'chrome/my_feature.cc',
         ['void DoSomething(content::RenderFrameHost* rfh) {',
@@ -4189,7 +4194,7 @@ class MPArchApiUsage(unittest.TestCase):
 
   def testNoNotify(self):
     self._assert_notify(
-        False,
+        [],
         'No API usage',
         'chrome/my_feature.cc',
         ['void DoSomething() {',
@@ -4199,7 +4204,7 @@ class MPArchApiUsage(unittest.TestCase):
     # Something under a top level directory we're not concerned about happens
     # to share a name with a content API.
     self._assert_notify(
-        False,
+        [],
         'Uninteresting top level directory',
         'third_party/my_dep/my_code.cc',
         ['bool HasParent(Node* node) {',
@@ -4208,7 +4213,7 @@ class MPArchApiUsage(unittest.TestCase):
         ])
     # We're not concerned with usage in test code.
     self._assert_notify(
-        False,
+        [],
         'Usage in test code',
         'chrome/my_feature_unittest.cc',
         ['TEST_F(MyFeatureTest, DoesSomething) {',
