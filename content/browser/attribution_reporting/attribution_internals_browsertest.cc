3402ac3f3ad14115e11c6bba45b7d23f41924400
Andrew Paseltiner <apaseltiner@chromium.org>
2022-02-14 23:47:14 +0000

    Show rejected attribution sources in internals UI
    We propagate rejected sources from the manager's observer to a combined
    Mojo method for deactivated and rejected sources.
    
    This will improve debuggability of the API.
    
    Bug: 1296631
    Change-Id: Iaa368cc850aca3bc62adb1766c868ab7677c3687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456908
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970907}

--- content/browser/attribution_reporting/attribution_internals_browsertest.cc
+++ content/browser/attribution_reporting/attribution_internals_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/attribution_reporting/attribution_trigger.h"
 #include "content/browser/attribution_reporting/common_source_info.h"
 #include "content/browser/attribution_reporting/send_result.h"
+#include "content/browser/attribution_reporting/storable_source.h"
 #include "content/browser/attribution_reporting/stored_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -243,10 +244,29 @@ IN_PROC_BROWSER_TEST_F(AttributionInternalsWebUiBrowserTest,
       DeactivatedSource(SourceBuilder(now + base::Hours(3)).BuildStored(),
                         DeactivatedSource::Reason::kReachedAttributionLimit));
 
+  // This shouldn't result in a row, as registration succeeded.
+  manager_.NotifySourceHandled(SourceBuilder(now).Build(),
+                               StorableSource::Result::kSuccess);
+
+  manager_.NotifySourceHandled(SourceBuilder(now + base::Hours(4)).Build(),
+                               StorableSource::Result::kInternalError);
+
+  manager_.NotifySourceHandled(
+      SourceBuilder(now + base::Hours(5)).Build(),
+      StorableSource::Result::kInsufficientSourceCapacity);
+
+  manager_.NotifySourceHandled(
+      SourceBuilder(now + base::Hours(6)).Build(),
+      StorableSource::Result::kInsufficientUniqueDestinationCapacity);
+
+  manager_.NotifySourceHandled(
+      SourceBuilder(now + base::Hours(7)).Build(),
+      StorableSource::Result::kExcessiveReportingOrigins);
+
   static constexpr char wait_script[] = R"(
     let table = document.querySelector("#source-table-wrapper tbody");
     let obs = new MutationObserver(() => {
-      if (table.children.length === 4 &&
+      if (table.children.length === 8 &&
           table.children[0].children[0].innerText === $1 &&
           table.children[0].children[6].innerText === "Navigation" &&
           table.children[1].children[6].innerText === "Event" &&
@@ -259,7 +279,11 @@ IN_PROC_BROWSER_TEST_F(AttributionInternalsWebUiBrowserTest,
           table.children[0].children[10].innerText === "Unattributable: noised" &&
           table.children[1].children[10].innerText === "Attributable" &&
           table.children[2].children[10].innerText === "Unattributable: replaced by newer source" &&
-          table.children[3].children[10].innerText === "Unattributable: reached attribution limit") {
+          table.children[3].children[10].innerText === "Unattributable: reached attribution limit" &&
+          table.children[4].children[10].innerText === "Rejected: internal error" &&
+          table.children[5].children[10].innerText === "Rejected: insufficient source capacity" &&
+          table.children[6].children[10].innerText === "Rejected: insufficient unique destination capacity" &&
+          table.children[7].children[10].innerText === "Rejected: excessive reporting origins") {
         document.title = $3;
       }
     });
