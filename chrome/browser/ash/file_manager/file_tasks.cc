5994405b0241c16b1de61e80972f4e35514616c8
Jérémie Boulic <jboulic@chromium.org>
2022-02-14 08:18:51 +0000

    [Files app] Initial Web Drive support implementation for Office files
    Register Office file handler.
    
    Adapt file tasks post processing to disable Web Drive support based on
    the flag and the file's location: only Drive paths are allowed.
    Improvement with check of alternate URL in later CL.
    
    Implement task execution for Office files.
    
    Test: CL:3450261
    Bug: 1293251
    Change-Id: I117b32fb3d8e2aa2de43f72c373e043d6cf4cfda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3429383
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970521}

--- chrome/browser/ash/file_manager/file_tasks.cc
+++ chrome/browser/ash/file_manager/file_tasks.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_manager/file_browser_handlers.h"
 #include "chrome/browser/ash/file_manager/file_tasks_notifier.h"
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
+#include "chrome/browser/ash/file_manager/filesystem_api_util.h"
 #include "chrome/browser/ash/file_manager/guest_os_file_tasks.h"
 #include "chrome/browser/ash/file_manager/open_util.h"
 #include "chrome/browser/ash/file_manager/open_with_browser.h"
@@ -338,6 +339,18 @@ void PostProcessFoundTasks(
     }
   }
 
+  if (!base::FeatureList::IsEnabled(ash::features::kFilesWebDriveOffice)) {
+    disabled_actions.emplace("open-web-drive-office");
+  } else {
+    for (const auto& entry : entries) {
+      // Allow the Web Drive Office task only if the entries are on Drive.
+      if (!::file_manager::util::IsDriveLocalPath(profile, entry.path)) {
+        disabled_actions.emplace("open-web-drive-office");
+        break;
+      }
+    }
+  }
+
 #if !BUILDFLAG(ENABLE_PDF)
   disabled_actions.emplace("view-pdf");
 #endif  // !BUILDFLAG(ENABLE_PDF)
@@ -361,7 +374,8 @@ bool ShouldBeOpenedWithBrowser(const std::string& extension_id,
           action_id == "open-hosted-generic" ||
           action_id == "open-hosted-gdoc" ||
           action_id == "open-hosted-gsheet" ||
-          action_id == "open-hosted-gslides");
+          action_id == "open-hosted-gslides" ||
+          action_id == "open-web-drive-office");
 }
 
 // Opens the files specified by |file_urls| with the browser for |profile|.
