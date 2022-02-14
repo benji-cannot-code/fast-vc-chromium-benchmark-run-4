9d420e270d8287348c5bcb3390f06c2eeb05107c
David Sanders <dsanders11@ucsbalum.com>
2022-02-14 10:02:47 +0000

    Remove unused include of translate_bubble_model.h
    Also adds an include to fix the build, where it was
    being transitively included.
    
    Bug: 242216
    Change-Id: I7f296649a457fce41eb327e3e796a908d4e0182a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3451574
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970553}

--- chrome/browser/autofill/autofill_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_interactive_uitest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/translate/translate_bubble_model.h"
 #include "chrome/browser/ui/translate/translate_bubble_test_utils.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
--- chrome/browser/translate/chrome_translate_client.h
+++ chrome/browser/translate/chrome_translate_client.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/gtest_prod_util.h"
 #include "build/build_config.h"
-#include "chrome/browser/ui/translate/translate_bubble_model.h"
 #include "components/autofill_assistant/browser/public/runtime_observer.h"
 #include "components/language/core/browser/url_language_histogram.h"
 #include "components/translate/content/browser/content_translate_driver.h"
