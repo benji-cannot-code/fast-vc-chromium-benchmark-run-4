791ab5f5cdb6ea630623b0342bf8f88d2704f7ad
Morten Stenshorne <mstensho@chromium.org>
2022-02-14 08:52:53 +0000

    CodeHealth: Modernize DictionaryValue::Set in chrome/browser/ui/webui
    Bug: 1187025, 1187026, 1187028
    Change-Id: I5c31da993264a2517dc3294fa5a9864b5932a042
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455702
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970533}

--- chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.cc
+++ chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.cc
@@ -197,7 +197,7 @@ void SyncInternalsMessageHandler::HandleRequestIncludeSpecificsInitialState(
   AllowJavascript();
 
   DictionaryValue value;
-  value.SetBoolean(syncer::sync_ui_util::kIncludeSpecifics,
+  value.SetBoolKey(syncer::sync_ui_util::kIncludeSpecifics,
                    GetIncludeSpecificsInitialState());
 
   FireWebUIListener(
