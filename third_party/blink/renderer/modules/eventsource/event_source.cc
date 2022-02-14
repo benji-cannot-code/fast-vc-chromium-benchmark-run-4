ffa89e642c2520335e4b31f76979d092edaf9794
Yoichi Osato <yoichio@chromium.org>
2022-02-14 06:10:10 +0000

    Add UMA metrics for Last-Event-ID
    This histogram records if Last-Event-ID header value is CORS safe or not
    when the EventSource requires reconnection.
    
    Bug: 1295510
    Change-Id: I30f021b10cdb20b2a09e8df6d63bfd6b0d3ecf8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3447000
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970493}

--- third_party/blink/renderer/modules/eventsource/event_source.cc
+++ third_party/blink/renderer/modules/eventsource/event_source.cc
@@ -63,6 +63,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
+namespace {
+// https://fetch.spec.whatwg.org/#cors-unsafe-request-header-byte
+bool IsCorsUnsafeRequestHeaderByte(char c) {
+  const auto u = static_cast<uint8_t>(c);
+  return (u < 0x20 && u != 0x09) || u == 0x22 || u == 0x28 || u == 0x29 ||
+         u == 0x3a || u == 0x3c || u == 0x3e || u == 0x3f || u == 0x40 ||
+         u == 0x5b || u == 0x5c || u == 0x5d || u == 0x7b || u == 0x7d ||
+         u == 0x7f;
+}
+
+void ReportUMA(ExecutionContext& context,
+               const std::string& value,
+               network::mojom::FetchResponseType response_type) {
+  if (response_type == network::mojom::FetchResponseType::kCors &&
+      (value.size() > 128 || std::any_of(value.begin(), value.end(),
+                                         IsCorsUnsafeRequestHeaderByte))) {
+    UseCounter::Count(context,
+                      WebFeature::kFetchEventSourceLastEventIdCorsUnSafe);
+  }
+}
+
+}  // anonymous namespace
 
 const uint64_t EventSource::kDefaultReconnectDelay = 3000;
 
@@ -266,6 +288,9 @@ void EventSource::DidReceiveResponse(uint64_t identifier,
     if (parser_) {
       // The new parser takes over the event ID.
       last_event_id = parser_->LastEventId();
+      DCHECK(GetExecutionContext());
+      ReportUMA(*GetExecutionContext(), last_event_id.Utf8(),
+                response.GetType());
     }
     parser_ = MakeGarbageCollected<EventSourceParser>(last_event_id, this);
     DispatchEvent(*Event::Create(event_type_names::kOpen));
