15ae245097559c73974137ff3221647ebf41d069
Tim Sergeant <tsergeant@chromium.org>
2022-02-14 07:19:26 +0000

    Link Capturing: Don't auto display the intent picker in chip-style UI
    The link capturing UX flow is being updated to eliminate the need
    for an intent picker in most cases. When this updated UI is enabled, we
    do not need to auto-display the intent picker.
    
    Bug: 1280507
    Change-Id: I44cc6269875affe1cc1c656650644c648d999415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3454583
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970505}

--- chrome/browser/apps/intent_helper/chromeos_intent_picker_helpers.cc
+++ chrome/browser/apps/intent_helper/chromeos_intent_picker_helpers.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/debug/dump_without_crashing.h"
+#include "base/feature_list.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -49,6 +50,10 @@ bool ShouldAutoDisplayUi(
   }
 
   const GURL& url = navigation_handle->GetURL();
+
+  // Disable Auto-display when the Intent Chip is enabled.
+  if (base::FeatureList::IsEnabled(features::kLinkCapturingUiUpdate))
+    return false;
 
   if (apps_for_picker.empty())
     return false;
