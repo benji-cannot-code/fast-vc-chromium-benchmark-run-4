925595dc835216631243071587bd82c6f2c28d80
Glen Robertson <glenrob@chromium.org>
2022-02-14 01:34:13 +0000

    codehealth: Detangle write/commit callback calls in WA install finalizer
    Calls in SetWebAppManifestFieldsAndWriteData to write icons,
    translations, then commit to the sync bridge were unfortunately
    dependent on subsequent calls in the sequence. This makes them mostly
    independent (though they still pass a success flag through).
    
    Change-Id: If080ff90137a3f8d66efbe55974766ddec4d3f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449736
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#970448}

--- chrome/browser/web_applications/web_app_install_finalizer.h
+++ chrome/browser/web_applications/web_app_install_finalizer.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/os_integration_manager.h"
 #include "chrome/browser/web_applications/web_app_chromeos_data.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
+#include "chrome/browser/web_applications/web_app_id.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/browser/web_applications/web_app_system_web_app_data.h"
 #include "chrome/browser/web_applications/web_app_uninstall_job.h"
@@ -179,14 +180,13 @@ class WebAppInstallFinalizer {
       std::unique_ptr<WebApp> web_app,
       CommitCallback commit_callback);
 
-  void WriteTranslationsThenCommitToSyncBridge(
-      CommitCallback commit_callback,
-      std::unique_ptr<WebApp> web_app,
-      const WebAppInstallInfo& web_app_info,
-      bool success);
+  void WriteTranslations(const AppId& app_id,
+                         const WebAppInstallInfo& web_app_info,
+                         CommitCallback commit_callback,
+                         bool success);
 
-  void CommitToSyncBridge(CommitCallback commit_callback,
-                          std::unique_ptr<WebApp> web_app,
+  void CommitToSyncBridge(std::unique_ptr<WebApp> web_app,
+                          CommitCallback commit_callback,
                           bool success);
 
   void OnDatabaseCommitCompletedForInstall(InstallFinalizedCallback callback,
