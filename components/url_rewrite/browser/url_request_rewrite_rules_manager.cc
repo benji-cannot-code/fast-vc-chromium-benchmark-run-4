5cc6161b4b6755c9afa2419ff6bfe18a4d190076
David Dorwin <ddorwin@chromium.org>
2022-02-14 21:08:44 +0000

    [url_rewrite] Keep track of previously created RenderFrameHosts
    Previously, UrlRequestRewriteRulesManager was not keeping track of
    RenderFrameHosts created before the WebContents was created. This
    would eventually trigger a DCHECK when RenderFrameDeleted() was called
    since the corresponding RenderFrameHost never had an Updater attached
    to it.
    
    In practice, this was not a problem with the current users of the
    url_rewrite component since we set up the observer very early after
    the WebContents is created.
    
    This fixes the issue by iterating over the RenderFrameHosts already
    created in the WebContents when
    UrlRequestRewriteRulesManager::AddWebContents() is called.
    
    Bug: 1152930
    Change-Id: Ic9bede0ea76f3c9211bfb23d727aba0a0a7d10f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3458302
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Fabrice de Gans <fdegans@chromium.org>
    Reviewed-by: Vigen Issahhanjan <vigeni@google.com>
    Cr-Commit-Position: refs/heads/main@{#970820}

--- components/url_rewrite/browser/url_request_rewrite_rules_manager.cc
+++ components/url_rewrite/browser/url_request_rewrite_rules_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_rewrite/browser/url_request_rewrite_rules_manager.h"
 
 #include "components/url_rewrite/browser/url_request_rewrite_rules_validation.h"
+#include "content/public/browser/web_contents.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 
 namespace url_rewrite {
@@ -76,7 +77,11 @@ size_t UrlRequestRewriteRulesManager::GetUpdatersSizeForTesting() const {
 UrlRequestRewriteRulesManager::Updater::Updater(
     content::WebContents* web_contents,
     const scoped_refptr<UrlRequestRewriteRules>& cached_rules)
-    : content::WebContentsObserver(web_contents), cached_rules_(cached_rules) {}
+    : content::WebContentsObserver(web_contents), cached_rules_(cached_rules) {
+  web_contents->ForEachRenderFrameHost(base::BindRepeating(
+      &UrlRequestRewriteRulesManager::Updater::MaybeRegisterExistingRenderFrame,
+      base::Unretained(this)));
+}
 
 UrlRequestRewriteRulesManager::Updater::~Updater() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -92,6 +97,15 @@ void UrlRequestRewriteRulesManager::Updater::OnRulesUpdated(
   // Send the updated rules to the updaters.
   for (const auto& receiver_pair : active_remotes_) {
     receiver_pair.second->OnRulesUpdated(mojo::Clone(cached_rules_->data));
+  }
+}
+
+void UrlRequestRewriteRulesManager::Updater::MaybeRegisterExistingRenderFrame(
+    content::RenderFrameHost* render_frame_host) {
+  if (render_frame_host->IsRenderFrameCreated()) {
+    // Call RenderFrameCreated() for frames that were created before this
+    // observer started observing this WebContents.
+    RenderFrameCreated(render_frame_host);
   }
 }
 
