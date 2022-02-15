f7a619221e3b90c6f4acd954e4201e1b9546f6ab
Gordon Seto <gordonseto@google.com>
2022-02-15 18:34:50 +0000

    [CrOS Bluetooth] Update BluetoothPairingUi to use FetchDevice().
    Update BluetoothPairingUi to search for Bluetooth devices using
    FetchDevice() when pairing with a device by address.
    
    Fixed: b/218357106
    Change-Id: I9a2b5220b080bec3c9a4bd1b0ba301ac6c61404b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462062
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#971331}

--- chrome/test/data/webui/cr_components/chromeos/bluetooth/fake_device_pairing_handler.js
+++ chrome/test/data/webui/cr_components/chromeos/bluetooth/fake_device_pairing_handler.js
@@ -37,7 +37,7 @@ export class FakeDevicePairingHandler {
     this.pairDeviceRejectCallback_ = null;
 
     /**
-     * @private {?function({result:
+     * @private {?function({device:
      *     ?chromeos.bluetoothConfig.mojom.BluetoothDeviceProperties})}
      */
     this.fetchDeviceCallback_ = null;
@@ -56,6 +56,9 @@ export class FakeDevicePairingHandler {
 
     /** @private {?function()} */
     this.waitForPairDeviceCallback_ = null;
+
+    /** @private {?function()} */
+    this.waitForFetchDeviceCallback_ = null;
 
     /** @private {?function()} */
     this.finishRequestConfirmPasskeyCallback_ = null;
@@ -79,6 +82,10 @@ export class FakeDevicePairingHandler {
 
   /** @override */
   fetchDevice(deviceAddress) {
+    if (this.waitForFetchDeviceCallback_) {
+      this.waitForFetchDeviceCallback_();
+    }
+
     return new Promise((resolve, reject) => {
       this.fetchDeviceCallback_ = resolve;
     });
@@ -240,10 +247,21 @@ export class FakeDevicePairingHandler {
   }
 
   /**
+   * Returns a promise that will be resolved the next time
+   * fetchDevice() is called.
+   * @return {Promise}
+   */
+  waitForFetchDevice() {
+    return new Promise((resolve) => {
+      this.waitForFetchDeviceCallback_ = resolve;
+    });
+  }
+
+  /**
    * @param {?chromeos.bluetoothConfig.mojom.BluetoothDeviceProperties} device
    */
   completeFetchDevice(device) {
     assert(this.fetchDeviceCallback_, 'fetchDevice() was never called');
-    this.fetchDeviceCallback_({result: device});
+    this.fetchDeviceCallback_({device: device});
   }
 }
