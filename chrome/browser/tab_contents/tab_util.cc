c4fd29bcf12a2fdf09e7c469158542159eb39d25
Hyowon Kim <hyowon@igalia.com>
2022-02-15 04:18:39 +0000

    Refactor ArcExternalProtocolDialog to use WeakPtr<WebContents>.
    This CL uses WebContents's WeakPtrs instead of render process IDs and
    routing IDs when running ArcExternalProtocolDialog.
    
    Bug: 1296482, 1136237
    Change-Id: I25cb15fa5f34e774dd5be5d2e21cdd0f39b71f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455362
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Hyowon Kim <hyowon@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#971035}

--- chrome/browser/tab_contents/tab_util.cc
+++ chrome/browser/tab_contents/tab_util.cc
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
 #include "content/public/browser/browser_url_handler.h"
-#include "content/public/browser/render_view_host.h"
 #include "content/public/browser/site_instance.h"
-#include "content/public/browser/web_contents.h"
 #include "extensions/buildflags/buildflags.h"
 #include "url/gurl.h"
 
@@ -18,20 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #endif
 
-using content::RenderViewHost;
 using content::SiteInstance;
-using content::WebContents;
 
 namespace tab_util {
-
-content::WebContents* GetWebContentsByID(int render_process_id,
-                                         int render_view_id) {
-  RenderViewHost* render_view_host =
-      RenderViewHost::FromID(render_process_id, render_view_id);
-  if (!render_view_host)
-    return NULL;
-  return WebContents::FromRenderViewHost(render_view_host);
-}
 
 scoped_refptr<SiteInstance> GetSiteInstanceForNewTab(Profile* profile,
                                                      GURL url) {
