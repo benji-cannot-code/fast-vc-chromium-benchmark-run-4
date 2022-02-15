e8d8482a1895f23ace9e6aef565d7e055518d9ae
Yuichiro Hanada <yhanada@google.com>
2022-02-15 16:49:51 +0000

    exo: Add a flag to disable haptic feedback support.
    The flag provides us a safe way to disable the haptic feedback
    support.
    
    Test: manual - diable the feature and confirm the state is not sent to clients.
    Change-Id: I6698799f812dbb68f77a68da3aad4d45052890ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461102
    Auto-Submit: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Commit-Queue: Sean O'Brien <seobrien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971265}

--- components/exo/wayland/zcr_touchpad_haptics.cc
+++ components/exo/wayland/zcr_touchpad_haptics.cc
@@ -9,11 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wayland-server-core.h>
 #include <wayland-server-protocol-core.h>
 
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "components/exo/wayland/server_util.h"
 #include "ui/events/devices/haptic_touchpad_effects.h"
 #include "ui/ozone/public/input_controller.h"
 #include "ui/ozone/public/ozone_platform.h"
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/constants/ash_features.h"
+#endif
 
 namespace exo {
 namespace wayland {
@@ -26,6 +31,12 @@ class WaylandTouchpadHapticsDelegate {
   ~WaylandTouchpadHapticsDelegate() = default;
 
   void UpdateTouchpadHapticsState() {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    if (!base::FeatureList::IsEnabled(
+            chromeos::features::kExoHapticFeedbackSupport))
+      return;
+#endif
+
     ui::InputController* controller =
         ui::OzonePlatform::GetInstance()->GetInputController();
     if (!controller) {
@@ -69,6 +80,11 @@ void touchpad_haptics_play(wl_client* client,
                            wl_resource* resource,
                            uint32_t effect,
                            int32_t strength) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (!base::FeatureList::IsEnabled(
+          chromeos::features::kExoHapticFeedbackSupport))
+    return;
+#endif
   GetUserDataAs<WaylandTouchpadHapticsDelegate>(resource)->Play(effect,
                                                                 strength);
 }
