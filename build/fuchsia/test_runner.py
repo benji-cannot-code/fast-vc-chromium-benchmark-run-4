1726bb0d4af0252d30ae12ab2b68ce9d3ef92959
Greg Thompson <grt@chromium.org>
2022-02-15 14:08:31 +0000

    Revert "[fuchsia] Adjust core count depending on nested virtualization."
    This reverts commit 32b1a2db08e040f8c2b2c873317d207eef1ea6fb.
    
    Reason for revert: breaks non-virtualized environments.
    
    Original change's description:
    > [fuchsia] Adjust core count depending on nested virtualization.
    >
    > Allows the use of more emulated CPU cores, with the maximum
    > amount capped in certain situations such as running under nested
    > virtualization or on ARM testbots.
    >
    > Bug: 1293574
    > Change-Id: I3e0ab8543451dba83afaf723782cc26dac05901d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448512
    > Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Zijie He <zijiehe@google.com>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#970983}
    
    Bug: 1293574
    Change-Id: Iae1a1782cccf5a9b245b6ea23d3298a758285585
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461898
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971195}

--- build/fuchsia/test_runner.py
+++ build/fuchsia/test_runner.py
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Deploys and runs a test package on a Fuchsia target."""
 
 import argparse
-import common
 import os
 import shutil
 import sys
@@ -246,20 +245,6 @@ def MapFilterFileToPackageFile(filter_file):
   return '/pkg/' + filter_file[filter_file.index(FILTER_DIR):]
 
 
-def MaybeApplyTestBotOverrides(parsed_args):
-  """Overrides certain arguments when running on test bots."""
-
-  if not parsed_args.test_launcher_bot_mode:
-    return
-
-  if common.GetHostArchFromPlatform() == 'arm64':
-    # Cap the number of cores for ARM bots.
-    # ARM-based test bots use container-level isolation, so the reported core
-    # count from the system reflects the actual number of physical system cores,
-    # not just the budget for this test task.
-    parsed_args.cpu_cores = min(parsed_args.cpu_cores, 4)
-
-
 def main():
   parser = argparse.ArgumentParser()
   AddTestExecutionArgs(parser)
@@ -285,8 +270,6 @@ def main():
     else:
       raise ValueError('Collecting code coverage info requires using '
                        'run-test-component.')
-
-  MaybeApplyTestBotOverrides(args)
 
   ConfigureLogging(args)
 
