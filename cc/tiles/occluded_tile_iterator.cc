6d3c5a68db5e7bd40ec140fd5588fe551939dac6
Scott Violet <sky@chromium.org>
2022-02-15 20:16:27 +0000

    cc: makes OccludedTileIterator iterate through pending and active layers
    BUG=1289362
    
    Change-Id: I207cec78429d7f6c2701756d1e8f906fc8615260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3457354
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971400}

--- cc/tiles/occluded_tile_iterator.cc
+++ cc/tiles/occluded_tile_iterator.cc
@@ -11,8 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 OccludedTileIterator::OccludedTileIterator(
-    const std::vector<PictureLayerImpl*>& picture_layers)
-    : picture_layers_(picture_layers) {
+    const std::vector<PictureLayerImpl*>* picture_layers,
+    const std::vector<PictureLayerImpl*>* secondary_picture_layers)
+    : picture_layers_(picture_layers),
+      secondary_picture_layers_(secondary_picture_layers),
+      active_layers_(picture_layers) {
   FindNextInPictureLayers();
 }
 
@@ -44,7 +47,23 @@ void OccludedTileIterator::Next() {
 }
 
 bool OccludedTileIterator::FindNextInPictureLayers() {
-  for (; current_picture_layer_index_ < picture_layers_.size();
+  if (FindNextInActiveLayers())
+    return true;
+  DCHECK(AtEnd());
+  if (is_active_layers_secondary_layers_)
+    return false;
+  // Finished iterating through primary picture layers. Start iterating
+  // through secondary layers.
+  is_active_layers_secondary_layers_ = true;
+  active_layers_ = secondary_picture_layers_;
+  if (!active_layers_)
+    return false;
+  current_picture_layer_index_ = 0;
+  return FindNextInActiveLayers();
+}
+
+bool OccludedTileIterator::FindNextInActiveLayers() {
+  for (; current_picture_layer_index_ < active_layers_->size();
        ++current_picture_layer_index_) {
     current_picture_layer_tiling_index_ = 0u;
     if (FindNextInPictureLayerTilingSet())
@@ -53,7 +72,6 @@ bool OccludedTileIterator::FindNextInPictureLayers() {
   // No more tiles to look at. Reset `tile_iterator_` so that AtEnd() returns
   // true.
   tile_iterator_.reset();
-  DCHECK(AtEnd());
   return false;
 }
 
@@ -72,14 +90,17 @@ bool OccludedTileIterator::FindNextInPictureLayerTilingSet() {
 bool OccludedTileIterator::FindNextInTileIterator() {
   PictureLayerTiling* tiling = CurrentPictureLayerTiling();
   for (; !tile_iterator_->AtEnd(); tile_iterator_->Next()) {
-    if (tiling->IsTileOccluded(tile_iterator_->GetCurrent()))
+    Tile* tile = tile_iterator_->GetCurrent();
+    if (visited_.insert(tile).second && tile->draw_info().has_resource() &&
+        tiling->IsTileOccluded(tile_iterator_->GetCurrent())) {
       return true;
+    }
   }
   return false;
 }
 
 PictureLayerTilingSet* OccludedTileIterator::CurrentPictureLayerTilingSet() {
-  return picture_layers_[current_picture_layer_index_]
+  return (*active_layers_)[current_picture_layer_index_]
       ->picture_layer_tiling_set();
 }
 
