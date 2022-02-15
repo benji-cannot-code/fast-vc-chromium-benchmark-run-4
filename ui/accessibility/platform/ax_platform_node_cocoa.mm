fdd3f8ca2c9f1242f947b5c9b90a668e5c5dcd0a
Valerie Young <spectranaut@igalia.com>
2022-02-15 19:57:34 +0000

    mac_merge: update AXVisited to match Safari values
    See discussion here:
    https://chromium-review.googlesource.com/c/chromium/src/+/3444661/comments/b798bc1f_b638f046
    
    Change-Id: Iee0330e96be7450f1e35f6b277abe8aaabc198dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453202
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#971383}

--- ui/accessibility/platform/ax_platform_node_cocoa.mm
+++ ui/accessibility/platform/ax_platform_node_cocoa.mm
@@ -1277,7 +1277,7 @@ bool IsAXSetter(SEL selector) {
   if (![self instanceActive])
     return nil;
 
-  return _node->HasState(ax::mojom::State::kVisited) ? @1 : @0;
+  return @(_node->HasState(ax::mojom::State::kVisited));
 }
 
 - (NSString*)AXHelp {
