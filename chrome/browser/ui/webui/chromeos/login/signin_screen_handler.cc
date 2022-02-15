e395d574bde4cf13718a7ced744477909e6ac03b
Roman Sorokin <rsorokin@chromium.org>
2022-02-15 03:27:38 +0000

    Migrate kUsersLastInputMethod values into the known_user system
    Bug: 1297407
    Change-Id: I9b5b12c9b29980825b2104f09578a98cfa69af92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461656
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971017}

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -505,6 +505,7 @@ void SigninScreenHandler::Initialize() {
 }
 
 void SigninScreenHandler::RegisterPrefs(PrefRegistrySimple* registry) {
+  // The pref is deprecated. Remove around 09/2022 (https://crbug.com/1297407)
   registry->RegisterDictionaryPref(prefs::kUsersLastInputMethod);
 }
 
