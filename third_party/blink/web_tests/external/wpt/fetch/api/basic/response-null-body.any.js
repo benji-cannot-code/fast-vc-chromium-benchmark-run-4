5ca2ae48b4af5da27fbfd34cf06e1853ded48bf5
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-02-15 07:30:58 +0000

    Import wpt@2c6485bc2c46775f044b5c704d6ff61683b529d7
    Using wpt-import in Chromium 9d90b9eabe99cd147f84de54528765916971cbb6.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: If014df77a29d56c8fcfff9e1855f5565cec8cdff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462494
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#971091}

---
+++ third_party/blink/web_tests/external/wpt/fetch/api/basic/response-null-body.any.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=../resources/utils.js
+
+const nullBodyStatus = [204, 205, 304];
+const methods = ["GET", "POST", "OPTIONS"];
+
+for (const status of nullBodyStatus) {
+  for (const method of methods) {
+    promise_test(
+      async () => {
+        const url =
+          `${RESOURCES_DIR}status.py?code=${status}&content=hello-world`;
+        const resp = await fetch(url, { method });
+        assert_equals(resp.status, status);
+        assert_equals(resp.body, null, "the body should be null");
+        const text = await resp.text();
+        assert_equals(text, "", "null bodies result in empty text");
+      },
+      `Response.body is null for responses with status=${status} (method=${method})`,
+    );
+  }
+}
+
+promise_test(async () => {
+  const url = `${RESOURCES_DIR}status.py?code=200&content=hello-world`;
+  const resp = await fetch(url, { method: "HEAD" });
+  assert_equals(resp.status, 200);
+  assert_equals(resp.body, null, "the body should be null");
+  const text = await resp.text();
+  assert_equals(text, "", "null bodies result in empty text");
+}, `Response.body is null for responses with method=HEAD`);
