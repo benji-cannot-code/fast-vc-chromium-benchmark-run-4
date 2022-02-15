40cfeb84937bd235de6d12c357f17703bc45f776
Norge Vizcay <vizcay@google.com>
2022-02-15 22:12:41 +0000

    [PasswordChange] SavedPasswordsCapabilitiesFetcher metrics.
    The CL introduces metrics related to SavedPasswordsCapabilitiesFetcher and CapabilitiesService. In particular:
    - CapabilitiesService.HttpResponseCode
    - SavedPasswordsCapabilitiesFetcher.CacheState
    - SavedPasswordsCapabilitiesFetcher.AllOriginsResponseTime
    - SavedPasswordsCapabilitiesFetcher.SingleOriginResponseTime
    
    Bug: b/209429727
    Change-Id: Id1280f9aab8da2b77a79a2e97e83a41afff10a40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461654
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#971439}

--- components/password_manager/core/browser/capabilities_service_impl.cc
+++ components/password_manager/core/browser/capabilities_service_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/hash/legacy_hash.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/ranges/algorithm.h"
 #include "base/stl_util.h"
 #include "base/strings/string_split.h"
@@ -86,8 +87,9 @@ void CapabilitiesServiceImpl::OnGetCapabilitiesResult(
     ResponseCallback callback,
     int http_status,
     const std::vector<CapabilitiesInfo>& infos) {
+  base::UmaHistogramSparse(
+      "PasswordManager.CapabilitiesService.HttpResponseCode", http_status);
   if (http_status != net::HTTP_OK) {
-    // TODO(b/209429727) Record network failure metrics.
     std::move(callback).Run(std::set<url::Origin>());
     return;
   }
