5a1244c0909ba3f7270998e46ef1f5159f099e3f
Xianzhu Wang <wangxianzhu@chromium.org>
2022-02-15 04:32:39 +0000

    Reland "Do a full PaintArtifactCompositor update on Layer::DrawsContent changes"
    This reverts commit ee4772925b7a2f76a30cab3c8939b379c21d39c1.
    
    Change from original: It seems that crbug.com/1283840 was caused by that
    some composited mask layer didn't have needed draws_content() flag.
    Because PaintChunk::DrawsContent() is still incomplete, we just use it
    for change detection (which is good because we only need the change
    detection for repaint-only changes, as change of other factors
    affecting draws_content() already cause full PAC update), while still
    use the original condition for cc::Layer::SetIsDrawable().
    
    Original change's description:
    > Revert "Do a full PaintArtifactCompositor update on Layer::DrawsContent changes"
    >
    > This reverts commit 67a18f9f29b23c5deddae19f1945f3a992b1079f.
    >
    > Reason for revert: May have introduced flakes on Pixel_CSSFilterEffects_NoOverlays
    >
    > Original change's description:
    > > Do a full PaintArtifactCompositor update on Layer::DrawsContent changes
    > >
    > > We have an optimization to avoid creating mask layers for non-drawable
    > > layers (see: SwitchToEffectNodeWithSynthesizedClip). When a layer goes
    > > from non-drawable to drawable (or the reverse), we need to run a full
    > > PaintArtifactCompositor update to generate the correct mask layers. This
    > > patch ensures a full PaintArtifactCompositor update, rather than a
    > > repaint-only update, is run when Layer::DrawsContent changes.
    > >
    > > |ContentLayerClientImpl::UpdateCcPictureLayer| updates
    > > |Layer::DrawsContent|. Before this patch, we set the value based on
    > > the cc_display_item_list_ being empty/non-empty. This patch makes
    > > |PendingLayer::DrawsContent| the source-of-truth for whether content
    > > is drawn because it can be calculated without running the expensive
    > > |PaintChunksToCcLayer::Convert|. |PendingLayer::DrawsContent| is set
    > > based on |PaintChunk::DrawsContent| which is set based on the drawable
    > > bounds of the display items. With this approach,
    > > |PaintArtifactCompositor::NeedsFullUpdateAfterPaintingChunk| now returns
    > > true when |PendingLayer::DrawsContent| changes, causing a full update.
    > >
    > > Bug: 1274530
    > > Change-Id: Ie3dcd2b36afc511a151a761b855eb123780c810e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3360227
    > > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Commit-Queue: Philip Rogers <pdr@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#954858}
    >
    > Bug: 1274530, 1283840
    > Change-Id: Ie0fd06f7085dc9d769b66572a2a65de8a0c505a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3368922
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#955983}
    
    Bug: 1274530, 1283840
    Change-Id: Ib8be97e8e1c2ba56bee7317f9b639e494a0abb8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3448040
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971041}

--- third_party/blink/renderer/platform/graphics/compositing/content_layer_client_impl.cc
+++ third_party/blink/renderer/platform/graphics/compositing/content_layer_client_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/platform/geometry/geometry_as_json.h"
 #include "third_party/blink/renderer/platform/graphics/compositing/paint_chunks_to_cc_layer.h"
+#include "third_party/blink/renderer/platform/graphics/compositing/pending_layer.h"
 #include "third_party/blink/renderer/platform/graphics/logging_canvas.h"
 #include "third_party/blink/renderer/platform/graphics/paint/display_item_list.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_display_item.h"
@@ -62,10 +63,8 @@ void ContentLayerClientImpl::AppendAdditionalInfoAsJSON(
 }
 
 scoped_refptr<cc::PictureLayer> ContentLayerClientImpl::UpdateCcPictureLayer(
-    const PaintChunkSubset& paint_chunks,
-    const gfx::Vector2dF& layer_offset,
-    const gfx::Size& layer_bounds,
-    const PropertyTreeState& layer_state) {
+    const PendingLayer& pending_layer) {
+  const auto& paint_chunks = pending_layer.Chunks();
   if (paint_chunks.begin()->is_cacheable)
     id_.emplace(paint_chunks.begin()->id);
   else
@@ -86,9 +85,12 @@ scoped_refptr<cc::PictureLayer> ContentLayerClientImpl::UpdateCcPictureLayer(
 
   // The raster invalidator will only handle invalidations within a cc::Layer so
   // we need this invalidation if the layer's properties have changed.
+  auto layer_state = pending_layer.GetPropertyTreeState();
   if (layer_state != layer_state_)
     cc_picture_layer_->SetSubtreePropertyChanged();
 
+  gfx::Size layer_bounds = pending_layer.LayerBounds();
+  gfx::Vector2dF layer_offset = pending_layer.LayerOffset();
   gfx::Size old_layer_bounds = raster_invalidator_.LayerBounds();
   DCHECK_EQ(old_layer_bounds, cc_picture_layer_->bounds());
   raster_invalidator_.Generate(raster_invalidation_function_, paint_chunks,
@@ -127,6 +129,11 @@ scoped_refptr<cc::PictureLayer> ContentLayerClientImpl::UpdateCcPictureLayer(
 
   cc_picture_layer_->SetBounds(layer_bounds);
   cc_picture_layer_->SetHitTestable(true);
+  // TODO(wangxianzhu): Use SetIsDrawable(pending_layer.DrawsContent()) when
+  // it's accurate. For now it only covers a subset of drawing content
+  // conditions (i.e. the following DCHECK).
+  DCHECK(!pending_layer.DrawsContent() ||
+         cc_display_item_list_->TotalOpCount());
   cc_picture_layer_->SetIsDrawable(
       (!layer_bounds.IsEmpty() && cc_display_item_list_->TotalOpCount()) ||
       // Backdrop effects and filters require the layer to be drawable even if
--- third_party/blink/renderer/platform/graphics/paint/raster_invalidator_test.cc
+++ third_party/blink/renderer/platform/graphics/paint/raster_invalidator_test.cc
@@ -217,6 +217,7 @@ TEST_P(RasterInvalidatorTest, ReorderChunks) {
                                   .Chunk(2)
                                   .Chunk(1)
                                   .Bounds(gfx::Rect(11, 22, 33, 44))
+                                  .DrawableBounds(gfx::Rect(11, 22, 33, 44))
                                   .Build());
   invalidator_.Generate(base::DoNothing(), new_chunks, kDefaultLayerOffset,
                         kDefaultLayerBounds, DefaultPropertyTreeState());
@@ -248,6 +249,7 @@ TEST_P(RasterInvalidatorTest, ReorderChunkSubsequences) {
                                   .IsMovedFromCachedSubsequence()
                                   .Chunk(1)
                                   .Bounds(gfx::Rect(11, 22, 33, 44))
+                                  .DrawableBounds(gfx::Rect(11, 22, 33, 44))
                                   .Chunk(2)
                                   .Build());
   invalidator_.Generate(base::DoNothing(), new_chunks, kDefaultLayerOffset,
@@ -434,14 +436,17 @@ TEST_P(RasterInvalidatorTest, ClipPropertyChangeSimple) {
   auto clip1 = CreateClip(*clip0, t0(), clip_rect);
 
   PropertyTreeState layer_state = PropertyTreeState::Root();
-  PaintChunkSubset chunks(TestPaintArtifact()
-                              .Chunk(0)
-                              .Properties(t0(), *clip0, e0())
-                              .Bounds(gfx::ToEnclosingRect(clip_rect.Rect()))
-                              .Chunk(1)
-                              .Properties(t0(), *clip1, e0())
-                              .Bounds(gfx::ToEnclosingRect(clip_rect.Rect()))
-                              .Build());
+  PaintChunkSubset chunks(
+      TestPaintArtifact()
+          .Chunk(0)
+          .Properties(t0(), *clip0, e0())
+          .Bounds(gfx::ToEnclosingRect(clip_rect.Rect()))
+          .DrawableBounds(gfx::ToEnclosingRect(clip_rect.Rect()))
+          .Chunk(1)
+          .Properties(t0(), *clip1, e0())
+          .Bounds(gfx::ToEnclosingRect(clip_rect.Rect()))
+          .DrawableBounds(gfx::ToEnclosingRect(clip_rect.Rect()))
+          .Build());
 
   invalidator_.Generate(base::DoNothing(), chunks, kDefaultLayerOffset,
                         kDefaultLayerBounds, layer_state);
@@ -497,12 +502,14 @@ TEST_P(RasterInvalidatorTest, ClipChangeOnCachedSubsequence) {
   auto c1 = CreateClip(c0(), t0(), clip_rect);
 
   PropertyTreeState layer_state = PropertyTreeState::Root();
-  PaintChunkSubset chunks(TestPaintArtifact()
-                              .Chunk(0)
-                              .Properties(t0(), *c1, e0())
-                              .Bounds(gfx::ToEnclosingRect(clip_rect.Rect()))
-                              .IsMovedFromCachedSubsequence()
-                              .Build());
+  PaintChunkSubset chunks(
+      TestPaintArtifact()
+          .Chunk(0)
+          .Properties(t0(), *c1, e0())
+          .Bounds(gfx::ToEnclosingRect(clip_rect.Rect()))
+          .DrawableBounds(gfx::ToEnclosingRect(clip_rect.Rect()))
+          .IsMovedFromCachedSubsequence()
+          .Build());
 
   invalidator_.Generate(base::DoNothing(), chunks, kDefaultLayerOffset,
                         kDefaultLayerBounds, layer_state);
@@ -539,6 +546,7 @@ TEST_P(RasterInvalidatorTest, ClipPropertyChangeWithOutsetForRasterEffects) {
           .Chunk(0)
           .Properties(t0(), *clip, e0())
           .Bounds(gfx::ToEnclosingRect(clip_rect.Rect()))
+          .DrawableBounds(gfx::ToEnclosingRect(clip_rect.Rect()))
           .SetRasterEffectOutset(RasterEffectOutset::kWholePixel)
           .Build());
 
@@ -760,6 +768,7 @@ TEST_P(RasterInvalidatorTest, TransformPropertyTinyChangeScale) {
                               .Chunk(0)
                               .Properties(*chunk_transform, c0(), e0())
                               .Bounds(chunk_bounds)
+                              .DrawableBounds(chunk_bounds)
                               .Build());
 
   invalidator_.Generate(base::DoNothing(), chunks, kDefaultLayerOffset,
