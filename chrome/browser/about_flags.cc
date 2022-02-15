947cababa4ee11c5283e88b45735c19bb8ae9bf0
Yu Su <yusuyoutube@google.com>
2022-02-15 19:26:25 +0000

    Add a tablet variant to the context menu search in about_flag
    Change-Id: I59663a0999d316bdb061cf2d22306be475c5a791
    Bug: b/218511872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3464495
    Reviewed-by: Ben Goldberger <benwgold@google.com>
    Commit-Queue: Yu Su <yusuyoutube@google.com>
    Cr-Commit-Position: refs/heads/main@{#971367}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2073,6 +2073,14 @@ const FeatureEntry::FeatureVariation kLensContextMenuTranslateVariations[] = {
      base::size(kLensContextMenuTranslateHideRemoveIcon), nullptr},
 };
 
+const FeatureEntry::FeatureParam kLensContextMenuSearchOnTablet[] = {
+    {"enableContextMenuSearchOnTablet", "true"}};
+
+const FeatureEntry::FeatureVariation kLensContextMenuSearchVariations[] = {
+    {"(on Tablet)", kLensContextMenuSearchOnTablet,
+     base::size(kLensContextMenuSearchOnTablet), nullptr},
+};
+
 const FeatureEntry::FeatureParam kDynamicColorFull[] = {
     {"dynamic_color_full", "true"}};
 
@@ -6197,7 +6205,10 @@ const FeatureEntry kFeatureEntries[] = {
     {"context-menu-search-with-google-lens",
      flag_descriptions::kContextMenuSearchWithGoogleLensName,
      flag_descriptions::kContextMenuSearchWithGoogleLensDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(chrome::android::kContextMenuSearchWithGoogleLens)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(
+         chrome::android::kContextMenuSearchWithGoogleLens,
+         kLensContextMenuSearchVariations,
+         "ContextMenuSearchWithGoogleLens")},
 
     {"context-menu-shop-with-google-lens",
      flag_descriptions::kContextMenuShopWithGoogleLensName,
