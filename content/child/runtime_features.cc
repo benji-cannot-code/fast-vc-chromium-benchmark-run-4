a701728e0d9745531ad65ae08303e717b4ddc2e3
Mustaq Ahmed <mustaq@google.com>
2022-02-15 19:19:19 +0000

    PaymentRequest: add a flag for enforcing user activation requirement
    Intent to Deprecate and Remove:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/xCW746n6XJI
    
    This CL separates out the above feature from "Capability Delegation with
    Payment Request" so that we can control user activation enforcement
    independently from the latter feature which will be shipped soon.
    
    Bug: 825270
    Change-Id: I966913b5842e4085739d26d6bf4cbb9c0dc8126e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460387
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971361}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -263,6 +263,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
     {wf::EnablePaymentApp, features::kServiceWorkerPaymentApps},
     {wf::EnablePaymentRequest, features::kWebPayments},
     {wf::EnablePaymentRequestBasicCard, features::kPaymentRequestBasicCard},
+    {wf::EnablePaymentRequestRequiresUserActivation,
+     features::kPaymentRequestRequiresUserActivation},
     {wf::EnablePercentBasedScrolling, features::kPercentBasedScrolling},
     {wf::EnablePeriodicBackgroundSync, features::kPeriodicBackgroundSync},
     {wf::EnablePictureInPicture, media::kPictureInPicture},
--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -274,6 +274,12 @@ void WebRuntimeFeatures::EnablePaymentRequestBasicCard(bool enable) {
   RuntimeEnabledFeatures::SetPaymentRequestBasicCardEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnablePaymentRequestRequiresUserActivation(
+    bool enable) {
+  RuntimeEnabledFeatures::SetPaymentRequestRequiresUserActivationEnabled(
+      enable);
+}
+
 void WebRuntimeFeatures::EnablePercentBasedScrolling(bool enable) {
   RuntimeEnabledFeatures::SetPercentBasedScrollingEnabled(enable);
 }
