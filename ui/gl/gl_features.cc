ea93bfa7afd46c0b3709b86123078e3ba809304e
Geoff Lang <geofflang@google.com>
2022-02-15 15:01:13 +0000

    Launch the passthrough command decoder on Mac.
    Block MacOS 10.15 until ANGLE's Metal backend ships.
    
    Bug: chromium:1257538, chromium:982292
    Change-Id: I7681eb7d8576041b266525fadbaf3cd0a97b8704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3459830
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971213}

--- ui/gl/gl_features.cc
+++ ui/gl/gl_features.cc
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "ui/gl/gl_switches.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/build_info.h"
 #include "base/metrics/field_trial_params.h"
@@ -74,7 +78,8 @@ const base::Feature kDefaultPassthroughCommandDecoder {
   "DefaultPassthroughCommandDecoder",
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_FUCHSIA) ||              \
     ((BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)) && \
-     !defined(CHROMECAST_BUILD))
+     !defined(CHROMECAST_BUILD)) ||                            \
+    BUILDFLAG(IS_MAC)
       base::FEATURE_ENABLED_BY_DEFAULT
 #else
       base::FEATURE_DISABLED_BY_DEFAULT
@@ -90,6 +95,15 @@ bool UseGpuVsync() {
 bool UsePassthroughCommandDecoder() {
   if (!base::FeatureList::IsEnabled(kDefaultPassthroughCommandDecoder))
     return false;
+
+#if BUILDFLAG(IS_MAC)
+  // Excessive crashes are seen in GL drivers on MacOS 10.15.7 in the glFlush
+  // function when using ANGLE and the passthrough command decoder.
+  // crbug.com/1257538
+  if (base::mac::IsOS10_15()) {
+    return false;
+  }
+#endif
 
 #if BUILDFLAG(IS_ANDROID)
   // Check block list against build info.
