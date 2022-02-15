0731098445a9462ced49be3899f03d7988b2fc4f
sandromaggi <sandromaggi@google.com>
2022-02-15 08:54:36 +0000

    [Autofill Assistant] Create public_java build target
    This creates a public_java build target that's included
    in chrome/android to be able to link dependencies to
    the build which wasn't possible with the java_sources.
    
    Currently only parts of the public java is moved - the
    parts related to editing addresses and credit cards.
    
    Bug: b/211748133
    Change-Id: I861b118cb116714d6fa65cd8992b196df6073957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3442960
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#971118}

---
+++ chrome/android/features/autofill_assistant/public/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantContactEditorAccount.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill_assistant;
+
+import org.chromium.base.Callback;
+import org.chromium.chrome.browser.autofill_assistant.AssistantEditor.AssistantContactEditor;
+import org.chromium.chrome.browser.autofill_assistant.AssistantOptionModel.ContactModel;
+import org.chromium.chrome.browser.autofill_assistant.user_data.GmsIntegrator;
+
+/**
+ * Editor for contact information in Chrome/WebLayer using a GMS intent.
+ */
+public class AssistantContactEditorAccount implements AssistantContactEditor {
+    private GmsIntegrator mGmsIntegrator;
+
+    @Override
+    public void createOrEditItem(ContactModel oldItem, Callback<ContactModel> doneCallback,
+            Callback<ContactModel> cancelCallback) {
+        // TODO(b/211748133)
+    }
+
+    @Override
+    public void addContactInformationForAutocomplete(AssistantAutofillProfile contact) {
+        // TODO(b/211748133)
+    }
+}
