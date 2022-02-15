34fdf8ad3c8082ec7ac4e3ed4b71dabbe1fb0866
Sarah Murphy <murphys@microsoft.com>
2022-02-15 20:22:45 +0000

    Reland "Verify WebContents isn't being destroyed before calling GetPrerenderHostRegistry."
    This releads commit f3ca490104fd92634339a354e34160e1d325f3da.
    
    Reason for revert: Causing failures, see crbug.com/1296027
    
    Several crashes have been reported in GetOutermostFrameTrees due to
    invalid pointer access when GetPrerenderHostRegistry is called.  This
    appears to be happening due to some observer clean-up code while
    the WebContents is being destroyed.  When the WebContentsImpl object
    is destroyed, it triggers a .reset() call on prerender_host_registry_.
    This change verifies that GetPrerenderHostRegistry does not get called
    when the WebContents is being destroyed.
    
    Bug: 1284452, 1296027
    Change-Id: I855fafc5d772d6158ca0346304344b0f1bf6a495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452712
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Sarah Murphy <murphys@microsoft.com>
    Auto-Submit: Sarah Murphy <murphys@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#971403}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -1327,6 +1327,7 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
 
   friend class RenderFrameHostImplBeforeUnloadBrowserTest;
   friend class WebContentsImplBrowserTest;
+  friend class TestWebContentsDestructionObserver;
   friend class BeforeUnloadBlockingDelegate;
   friend class TestWCDelegateForDialogsAndFullscreen;
 
