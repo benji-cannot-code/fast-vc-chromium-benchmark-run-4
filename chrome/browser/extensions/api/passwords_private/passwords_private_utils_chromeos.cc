883f6ce77b10ad3f0bb916e2a2d996862bd46d30
Roman Sorokin <rsorokin@chromium.org>
2022-02-15 11:55:20 +0000

    known_user: Migrate off deprecated *IsUsingSAMLPrincipalsAPI functions
    to their KnownUser versions
    
    Bug: 1150434
    Change-Id: I5c70ebdce6d67cd3417e8fd68b7bc41f50dead89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456962
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971163}

--- chrome/browser/extensions/api/passwords_private/passwords_private_utils_chromeos.cc
+++ chrome/browser/extensions/api/passwords_private/passwords_private_utils_chromeos.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_factory.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_storage.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/user_manager/user.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
@@ -43,6 +44,7 @@ bool IsOsReauthAllowedAsh(Profile* profile,
                           base::TimeDelta auth_token_lifetime) {
   const bool user_cannot_manually_enter_password =
       !ash::password_visibility::AccountHasUserFacingPassword(
+          g_browser_process->local_state(),
           ash::ProfileHelper::Get()->GetUserByProfile(profile)->GetAccountId());
   if (user_cannot_manually_enter_password)
     return true;
