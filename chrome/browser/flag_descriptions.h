6dbb5b2115318273078bdbdfa41925ffedaac880
Josiah K <josiahk@google.com>
2022-02-15 20:39:13 +0000

    [A11y Settings Revamp] Add feature flag for improved settings visibility
    Adding a feature flag to gate the first set of improvements for the
    Accessibility OS Settings Revamp.
    
    AX-Relnotes: N/A.
    Bug: 1292142
    Change-Id: I84073442a373a83c5676c5bc6455e56d7a056c36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3457491
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#971409}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -548,6 +548,9 @@ extern const char kEnableTLS13EarlyDataDescription[];
 extern const char kEnhancedNetworkVoicesName[];
 extern const char kEnhancedNetworkVoicesDescription[];
 
+extern const char kAccessibilityOSSettingsVisibilityName[];
+extern const char kAccessibilityOSSettingsVisibilityDescription[];
+
 extern const char kPostQuantumCECPQ2Name[];
 extern const char kPostQuantumCECPQ2Description[];
 
