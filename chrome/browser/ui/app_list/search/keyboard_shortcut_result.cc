7bccc12b64cb210579b009b51278cdaebe4743b4
Amanda Deacon <amandadeacon@chromium.org>
2022-02-15 04:45:47 +0000

    [Shortcuts] Introduce a keyboard shortcuts icon.
    This CL:
    
    1) Introduces a keyboard shortcuts app .icon file.
    2) Sets this icon as the icon for keyboard shortcut results.
    
    Example of icon appearance in launcher results list:
    https://screenshot.googleplex.com/fqiuLeD85fYovEd.png
    
    The introduced .icon file was created from this .svg:
    http://shortn/_CFKJo8FtjU
    
    Test: manual on-device
    Bug: 1290682
    Change-Id: I27e628dcf4a42c3b02d77287186fd2e6d24aecef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460199
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Amanda Deacon <amandadeacon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971042}

--- chrome/browser/ui/app_list/search/keyboard_shortcut_result.cc
+++ chrome/browser/ui/app_list/search/keyboard_shortcut_result.cc
@@ -14,10 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/ui/app_list/search/common/icon_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/components/string_matching/tokenized_string_match.h"
+#include "chromeos/ui/vector_icons/vector_icons.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
+#include "ui/gfx/paint_vector_icon.h"
 
 namespace app_list {
 
@@ -169,6 +172,7 @@ KeyboardShortcutResult::KeyboardShortcutResult(Profile* profile,
   SetMetricsType(ash::KEYBOARD_SHORTCUT);
   SetDisplayType(DisplayType::kList);
   SetCategory(Category::kHelp);
+  UpdateIcon();
 
   // Set the details to the display name of the Keyboard Shortcut Viewer app.
   std::u16string sanitized_name = base::CollapseWhitespace(
@@ -298,6 +302,15 @@ double KeyboardShortcutResult::CalculateRelevance(
   TokenizedStringMatch match;
   match.Calculate(query_tokenized, target_tokenized);
   return match.relevance();
+}
+
+void KeyboardShortcutResult::UpdateIcon() {
+  // TODO(crbug.com/1290682): Set the icon to a larger size when a
+  // KeyboardShortcutResult is promoted to an answer card.
+  gfx::ImageSkia icon =
+      gfx::CreateVectorIcon(chromeos::kKeyboardShortcutsIcon,
+                            kSystemIconDimension, SK_ColorTRANSPARENT);
+  SetIcon(IconInfo(icon, kSystemIconDimension));
 }
 
 }  // namespace app_list
