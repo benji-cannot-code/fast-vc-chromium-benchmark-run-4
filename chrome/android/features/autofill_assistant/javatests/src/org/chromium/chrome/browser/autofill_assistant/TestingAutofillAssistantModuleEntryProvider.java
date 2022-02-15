ad49147c790fafcf10225862a49db62e98f4b6ee
Florian Gauger <fga@google.com>
2022-02-15 15:53:28 +0000

    [Autofill Assistant] Remove BrowserControlsStateProvider from module interface
    We are in the process of moving all Autofill Assistant code to //components. For this we need to remove all dependencies on Chrome code.
    
    Bug: b/173103628
    Change-Id: I11d3c47b09cfbdd3abb6a7a7679f3e91f2e5691b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3420568
    Auto-Submit: Florian Gauger <fga@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/main@{#971239}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/TestingAutofillAssistantModuleEntryProvider.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/TestingAutofillAssistantModuleEntryProvider.java
@@ -11,7 +11,6 @@ import android.view.View;
 import org.chromium.base.Callback;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.autofill_assistant.onboarding.OnboardingCoordinatorFactory;
-import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.content_public.browser.WebContents;
 
@@ -36,13 +35,12 @@ class TestingAutofillAssistantModuleEntryProvider extends AutofillAssistantModul
     static class MockAutofillAssistantActionHandler extends AutofillAssistantActionHandlerImpl {
         public MockAutofillAssistantActionHandler(Context context,
                 BottomSheetController bottomSheetController,
-                BrowserControlsStateProvider browserControls, View rootView,
+                AssistantBrowserControlsFactory browserControlsFactory, View rootView,
                 Supplier<WebContents> webContentsSupplier,
                 AssistantStaticDependencies staticDependencies) {
             super(new OnboardingCoordinatorFactory(context, bottomSheetController,
-                          ()
-                                  -> new AssistantBrowserControlsChrome(browserControls),
-                          rootView, staticDependencies.getAccessibilityUtil(),
+                          browserControlsFactory, rootView,
+                          staticDependencies.getAccessibilityUtil(),
                           staticDependencies.createInfoPageUtil()),
                     webContentsSupplier, staticDependencies);
         }
@@ -71,11 +69,11 @@ class TestingAutofillAssistantModuleEntryProvider extends AutofillAssistantModul
         @Override
         public AutofillAssistantActionHandler createActionHandler(Context context,
                 BottomSheetController bottomSheetController,
-                BrowserControlsStateProvider browserControls, View rootView,
+                AssistantBrowserControlsFactory browserControlsFactory, View rootView,
                 Supplier<WebContents> webContentsSupplier,
                 AssistantStaticDependencies staticDependencies) {
             return new MockAutofillAssistantActionHandler(context, bottomSheetController,
-                    browserControls, rootView, webContentsSupplier, staticDependencies);
+                    browserControlsFactory, rootView, webContentsSupplier, staticDependencies);
         }
     }
 
