314abd85fc817f0286599e30b71a1f6ec15440ed
Patryk Duda <pdk@semihalf.com>
2022-02-15 15:39:28 +0000

    chromeos/dbus/biod: Remove support for ScanResult in AuthScanDone signal
    Fingerprint.AuthScanDoneSignal.UpgradeToFingerprintMessage shows that
    all AuthScanDone signals contain FingerprintMessage protobuf, so let's
    remove support for older version of the signal.
    
    Support for FingerprintMessage protobuf was added in CL:3304058
    
    BUG=b:212581721
    TEST=autoninja -C out/Debug_chromeos/ chromeos_unittests
         ./out/Debug_chromeos/chromeos_unittests
    TEST=autoninja -C out/Debug_chromeos/ browser_tests
         ./out/Debug_chromeos/browser_tests
    
    Change-Id: I743288585d650b578f362f94085ff6c2e6b8a336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452428
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Patryk Duda <patrykd@google.com>
    Cr-Commit-Position: refs/heads/main@{#971232}

--- chromeos/dbus/biod/biod_client.cc
+++ chromeos/dbus/biod/biod_client.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/strings/stringprintf.h"
 #include "chromeos/dbus/biod/fake_biod_client.h"
 #include "chromeos/dbus/biod/messages.pb.h"
@@ -27,11 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
-
-// Name of UMA boolean histogram used to count signals with FingerprintMessage
-// and ScanResult.
-const char* kUmaUpgradeToFingerprintMessage =
-    "Fingerprint.AuthScanDoneSignal.UpgradeToFingerprintMessage";
 
 BiodClient* g_instance = nullptr;
 
@@ -378,21 +372,9 @@ class BiodClientImpl : public BiodClient {
     biod::FingerprintMessage msg;
 
     if (!signal_reader.PopArrayOfBytesAsProto(&msg)) {
-      LOG(WARNING) << "Signal doesn't contain protobuf with authentication "
-                   << "result. Trying to extract ScanResult enum";
-      uint32_t scan_result;
-      if (!signal_reader.PopUint32(&scan_result)) {
-        LOG(ERROR) << "Can't read ScanResult from AuthScanDone signal";
-        return;
-      }
-      // Copy obtained ScanResult to FingerprintMessage protobuf.
-      msg.set_scan_result(static_cast<biod::ScanResult>(scan_result));
-
-      // AuthScanDone signal contains ScanResult enum.
-      base::UmaHistogramBoolean(kUmaUpgradeToFingerprintMessage, false);
-    } else {
-      // AuthScanDone signal contains FingerprintMessage protobuf.
-      base::UmaHistogramBoolean(kUmaUpgradeToFingerprintMessage, true);
+      LOG(ERROR) << "Signal doesn't contain protobuf with authentication "
+                 << "result.";
+      return;
     }
 
     if (!signal_reader.PopArray(&array_reader)) {
