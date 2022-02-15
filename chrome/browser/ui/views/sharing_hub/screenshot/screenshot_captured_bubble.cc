ddfe9b354913625dbc2db5b33eaeeeeabcede58f
Takashi Sakamoto <tasak@google.com>
2022-02-15 03:29:05 +0000

    Revert "[Desktop Screenshots] Add untrusted JavaScript and Mojo."
    This reverts commit 38e715e60699f9c26f47dbdb971ac746994c0488.
    
    Reason for revert: suspect causing compile failure on chromeos-octopus-chrome, chromeos-betty-pi-arc-cfi-thin-lto-chrome, and "chromeos-eve-arc-r-chrome.
    ---
    KeyError: 'Duplicate resource IDs: [23000, 23001, 23002, 23003]. This is likely because the reserved ID ranges defined in tools/gritsettings/resource_ids.spec have been exhausted.'
    ---
    
    First build failure:
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-octopus-chrome/4128/overview
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-pi-arc-cfi-thin-lto-chrome/32385/overview
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-eve-arc-r-chrome/15890/overview
    
    Original change's description:
    > [Desktop Screenshots] Add untrusted JavaScript and Mojo.
    >
    > This CL builds the mojo and JS needed for executing IPC commands in the
    > image editor code path.
    >
    > Bug: 1249227
    > Change-Id: I36779aca6db6885676d50e79fcb13c63ea9d4016
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3350989
    > Reviewed-by: Christopher Lam <calamity@chromium.org>
    > Reviewed-by: Jiewei Qian <qjw@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Travis Skare <skare@chromium.org>
    > Auto-Submit: Jeffrey Cohen <jeffreycohen@chromium.org>
    > Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#970991}
    
    Bug: 1249227
    Change-Id: Ic7b90b427193f65c495ed2fd0421e93492b65852
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462786
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Takashi Sakamoto <tasak@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#971018}

--- chrome/browser/ui/views/sharing_hub/screenshot/screenshot_captured_bubble.cc
+++ chrome/browser/ui/views/sharing_hub/screenshot/screenshot_captured_bubble.cc
@@ -71,7 +71,9 @@ bool ScreenshotCapturedBubble::ShouldShowCloseButton() const {
   return true;
 }
 
-void ScreenshotCapturedBubble::WindowClosing() {}
+void ScreenshotCapturedBubble::WindowClosing() {
+  NOTIMPLEMENTED();
+}
 
 void ScreenshotCapturedBubble::Init() {
   auto* layout_provider = ChromeLayoutProvider::Get();
--- chrome/browser/ui/webui/chrome_untrusted_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_untrusted_web_ui_controller_factory.cc
@@ -24,9 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/webui/video_tutorials/video_player_ui.h"
-#else  // !BUILDFLAG(IS_ANDROID)
+#else
 #include "chrome/browser/ui/webui/feed/feed_ui_config.h"
-#include "chrome/browser/ui/webui/image_editor/image_editor_untrusted_ui.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -74,10 +73,8 @@ WebUIConfigList CreateConfigs() {
 
 #if BUILDFLAG(IS_ANDROID)
   register_config(std::make_unique<video_tutorials::VideoPlayerUIConfig>());
-#else  // !BUILDFLAG(IS_ANDROID)
+#else
   register_config(std::make_unique<feed::FeedUIConfig>());
-  register_config(
-      std::make_unique<image_editor::ImageEditorUntrustedUIConfig>());
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
