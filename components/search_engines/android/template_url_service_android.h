116a5066ad711e4aa8470bb50c0c4880dfd21b19
Donn Denman <donnd@chromium.org>
2022-02-16 01:34:04 +0000

    Fix TemplateUrl GetUrlFor* methods with base set
    Fixes an issue that hampers development using the google-base-url
    command line flag - URL substitutions are silently dropped.
    
    Also applies the same fix to Voice Search preemptively.
    
    BUG=1294276
    
    Change-Id: I74e16bf78b6729c0ed59b36f784b4280aaf062ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440559
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971531}

--- components/search_engines/android/template_url_service_android.h
+++ components/search_engines/android/template_url_service_android.h
@@ -119,6 +119,8 @@ class TemplateUrlServiceAndroid : public TemplateURLServiceObserver {
       const base::android::JavaParamRef<jobject>& obj);
 
  private:
+  bool IsDefaultSearchEngineGoogle();
+
   void OnTemplateURLServiceLoaded();
 
   // TemplateUrlServiceObserver:
