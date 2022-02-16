b050d738450ef475d652d3cebef69de4a4b729cc
Kenichi Ishibashi <bashi@chromium.org>
2022-02-16 23:22:16 +0000

    Experiment batching Component Updater
    This CL allows SimpleURLLoaders for Component Updater to be batched
    when (1) a field experiment configuration is enabled and (2) the default
    network state is not active (i.e., in low power state).
    
    The feature is disabled by default.
    
    Bug: 1293657
    Change-Id: If8daee91da3bb274021ee3a312732a255e4eaff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465961
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972132}

--- components/update_client/net/network_impl.cc
+++ components/update_client/net/network_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/cpp/simple_url_loader_throttle.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "url/gurl.h"
 
@@ -119,6 +120,8 @@ void NetworkFetcherImpl::PostRequest(
     resource_request->headers.SetHeader(header.first, header.second);
   simple_url_loader_ = network::SimpleURLLoader::Create(
       std::move(resource_request), traffic_annotation);
+  if (network::SimpleURLLoaderThrottle::IsBatchingEnabled(traffic_annotation))
+    simple_url_loader_->SetAllowBatching();
   simple_url_loader_->SetRetryOptions(
       kMaxRetriesOnNetworkChange,
       network::SimpleURLLoader::RETRY_ON_NETWORK_CHANGE);
