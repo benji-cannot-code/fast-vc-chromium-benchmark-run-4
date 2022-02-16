58f8924a3a4037e4540190d4f35b69c55505f31b
Dibyajyoti Pal <dibyapal@chromium.org>
2022-02-16 20:08:31 +0000

    dPWA: Callbacks for OS Hooks operation completion
    This CL introduces callbacks for OS Hook operations to be completed
    so as to fix race conditions that might occur when install/uninstall
    operations interact with setting of os hooks for various settings (like
    run on OS Login). This also fixes flakiness
    in WebAppIntegrationTests as a result.
    
    Bug: 1294464, 1295126
    Change-Id: Idc9204e2e85b6b44afdced8cc66364257526d49c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456442
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972010}

--- chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac_win_linux.cc
+++ chrome/browser/ui/views/web_apps/web_app_integration_browsertest_mac_win_linux.cc
@@ -33,7 +33,7 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
 
 // Disabled due to flakiness. crbug.com/1294464
 IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
-                       DISABLED_CheckRunOnOsLoginModeOnPolicyAppWorks) {
+                       CheckRunOnOsLoginModeOnPolicyAppWorks) {
   helper_.InstallPolicyAppTabbedNoShortcut("SiteA");
   helper_.CheckPlatformShortcutNotExists("SiteA");
   helper_.EnableRunOnOSLoginMode("SiteA");
@@ -58,7 +58,7 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
-                       DISABLED_CheckRunOnOsLoginWorksOnPolicyAppAllowed) {
+                       CheckRunOnOsLoginWorksOnPolicyAppAllowed) {
   helper_.InstallPolicyAppOsLoginModeAllowed("SiteA");
   helper_.EnableRunOnOSLoginMode("SiteA");
   helper_.CheckRunOnOSLoginModeEnabled("SiteA");
@@ -69,7 +69,7 @@ IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
 }
 
 IN_PROC_BROWSER_TEST_F(WebAppIntegrationBrowserTestMacWinLinux,
-                       DISABLED_CheckRunOnOsLoginWorksOnPolicyAppBlocked) {
+                       CheckRunOnOsLoginWorksOnPolicyAppBlocked) {
   helper_.InstallPolicyAppOsLoginModeBlocked("SiteA");
   helper_.EnableRunOnOSLoginMode("SiteA");
   helper_.CheckRunOnOSLoginModeDisabled("SiteA");
