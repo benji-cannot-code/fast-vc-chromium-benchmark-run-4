525de090f5ef8bfc355fe752e64765b0da761d79
Titouan Rigoudy <titouan@chromium.org>
2022-02-16 15:10:00 +0000

    [Private Network Access] Use network IP address spaces for UseCounters.
    This aligns the metrics recorded by Blink with the checks performed by
    the network service. Previously, differences in implementation (such as
    the carveouts for cached and proxied resources) resulted in incorrect
    metrics.
    
    Also fix a typo in the .mock-http-headers file name.
    
    Bug: chromium:1293764
    Change-Id: I2c5a9c01b426355babfa09dceacf45f842dd9dfe
    Fixed: chromium:1204028, chromium:1278273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3435986
    Commit-Queue: Mike West <mkwst@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971824}

--- third_party/blink/public/platform/web_url_response.h
+++ third_party/blink/public/platform/web_url_response.h
@@ -50,7 +50,7 @@ enum class FetchResponseSource;
 enum class FetchResponseType : int32_t;
 enum class IPAddressSpace : int32_t;
 class LoadTimingInfo;
-}
+}  // namespace mojom
 }  // namespace network
 
 namespace net {
@@ -216,6 +216,11 @@ class WebURLResponse {
   // Address space from which this resource was fetched.
   BLINK_PLATFORM_EXPORT network::mojom::IPAddressSpace AddressSpace() const;
   BLINK_PLATFORM_EXPORT void SetAddressSpace(network::mojom::IPAddressSpace);
+
+  BLINK_PLATFORM_EXPORT network::mojom::IPAddressSpace ClientAddressSpace()
+      const;
+  BLINK_PLATFORM_EXPORT void SetClientAddressSpace(
+      network::mojom::IPAddressSpace);
 
   // ALPN negotiated protocol of the socket which fetched this resource.
   BLINK_PLATFORM_EXPORT bool WasAlpnNegotiated() const;
