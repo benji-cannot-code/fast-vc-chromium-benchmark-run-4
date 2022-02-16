db35c52b964545a837683ce5f42c34bec7e46803
Alexander Tekle <alexandertekle@google.com>
2022-02-16 01:25:46 +0000

    [M2] Sync avatar animation with VCN enrollment bubble for upstream flow.
    This CL handles animation syncing for the M2 VCN Enrollment upstream flow. If a user is saving a new card, we will wait until after the save card highlight animation completes and the OnDidGetDetailsResponse event to complete to show the enrollment bubble.
    
    Change-Id: I78b9e6ec0fc0ee3d80dce859eb054906fd741b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428244
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#971516}

--- components/autofill/core/browser/payments/test_virtual_card_enrollment_manager.cc
+++ components/autofill/core/browser/payments/test_virtual_card_enrollment_manager.cc
@@ -25,4 +25,8 @@ void TestVirtualCardEnrollmentManager::Reset() {
   reset_called_ = true;
 }
 
+void TestVirtualCardEnrollmentManager::ShowVirtualCardEnrollmentBubble() {
+  bubble_shown_ = true;
+}
+
 }  // namespace autofill
