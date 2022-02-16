239eba1258d1149e85eb75a3b6de3d76cb02e7a8
Takuto Ikuta <tikuta@chromium.org>
2022-02-16 13:45:15 +0000

    testing: migrate browser tests to python3
    Fixed: 1293351
    Cq-Include-Trybots: luci.chromium.try:mac11-arm64-rel
    Change-Id: Ife9bce74168cd9f051f186283681a95995603f57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465962
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971783}

--- chrome/test/data/native_messaging/native_hosts/empty_app.bat
+++ chrome/test/data/native_messaging/native_hosts/empty_app.bat
@@ -4,4 +4,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
-python "%~dp0/empty_app.py" %*
+python3 "%~dp0/empty_app.py" %*
