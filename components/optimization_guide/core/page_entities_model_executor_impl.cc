bcec7088029ad5b9fd94b9bae10fafa0193ede31
Sophie Chang <sophiechang@chromium.org>
2022-02-16 00:34:26 +0000

    Add new executionlatency histogram for PageEntities that uses base::ElapsedThreadTimer
    Bug: 1297608
    Change-Id: Id735059f4a54f4b236e5320c67dbe035df8d82ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465235
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971489}

--- components/optimization_guide/core/page_entities_model_executor_impl.cc
+++ components/optimization_guide/core/page_entities_model_executor_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_functions.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "base/timer/elapsed_timer.h"
 #include "components/optimization_guide/core/entity_annotator_native_library.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
 #include "components/optimization_guide/proto/page_entities_model_metadata.pb.h"
@@ -93,6 +94,7 @@ void EntityAnnotatorHolder::AnnotateEntitiesMetadataModelOnBackgroundThread(
     const std::string& text,
     PageEntitiesMetadataModelExecutedCallback callback) {
   DCHECK(background_task_runner_->RunsTasksInCurrentSequence());
+  base::ElapsedThreadTimer annotate_timer;
 
   absl::optional<std::vector<ScoredEntityMetadata>> scored_md;
   if (entity_annotator_) {
@@ -100,12 +102,17 @@ void EntityAnnotatorHolder::AnnotateEntitiesMetadataModelOnBackgroundThread(
     base::TimeTicks start_time = base::TimeTicks::Now();
     scored_md =
         entity_annotator_native_library_->AnnotateText(entity_annotator_, text);
-    // The max of this histogram is 1 hour because we want to understand
+    // The max of the below histograms is 1 hour because we want to understand
     // tail behavior and catch long running model executions.
     base::UmaHistogramLongTimes(
         "OptimizationGuide.PageContentAnnotationsService.ModelExecutionLatency."
         "PageEntities",
         base::TimeTicks::Now() - start_time);
+
+    base::UmaHistogramLongTimes(
+        "OptimizationGuide.PageContentAnnotationsService."
+        "ModelThreadExecutionLatency.PageEntities",
+        annotate_timer.Elapsed());
   }
   reply_task_runner_->PostTask(FROM_HERE,
                                base::BindOnce(std::move(callback), scored_md));
