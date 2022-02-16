4f3f2fba451a0ec6c740d577f55b2e99d8de1f49
Eugene Zemtsov <eugene@chromium.org>
2022-02-16 20:53:19 +0000

    Handle mojo disconnection in mojo video encoder
    Bug: 1292782
    Change-Id: Ibcedfd86206f26abf03a44ca622b867d66388e88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466780
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972035}

--- media/mojo/clients/mojo_video_encode_accelerator.h
+++ media/mojo/clients/mojo_video_encode_accelerator.h
@@ -54,6 +54,7 @@ class MojoVideoEncodeAccelerator : public VideoEncodeAccelerator {
  private:
   // Only Destroy() should be deleting |this|.
   ~MojoVideoEncodeAccelerator() override;
+  void MojoDisconnectionHandler();
 
   mojo::Remote<mojom::VideoEncodeAccelerator> vea_;
 
