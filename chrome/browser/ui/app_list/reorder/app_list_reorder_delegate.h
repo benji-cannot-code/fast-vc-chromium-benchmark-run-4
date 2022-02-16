46735e235617c6d1c56d58215c873690a8de03fe
Andrew Xu <andrewxu@chromium.org>
2022-02-16 05:40:09 +0000

    [Launcher Sort] Fix position calculation for the item already in model
    When calculating the position under sorting order for an item that is
    already in the model, that item should be excluded from the group on
    which entropy is measured. Otherwise, the calculation result could be
    wrong in this scenario:
    
    Step 1: Modify an item's attribute (such as name or icon color)
    Step 2: Calculate this item's position after attribute change.
    
    Because of the step 1, the item could be out of order, which yields
    a non-zero entropy when measuring the entropy on the all local items,
    even when the remaining items are perfectly ordered.
    
    This CL fixes this issue by: during position calculation, when
    generating the list of local items, skip the item whose position is
    under calculation.
    
    In addition, this CL polishes the logic that the item's current
    position should be kept if the current position maintains the sort
    order. Add more tests to protect this feature.
    
    Bug: 1293162
    Change-Id: I9d4c841d3681b085a2be9f7988443d1283ae6ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465778
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971653}

--- chrome/browser/ui/app_list/reorder/app_list_reorder_delegate.h
+++ chrome/browser/ui/app_list/reorder/app_list_reorder_delegate.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/model/string_ordinal.h"
 
-class ChromeAppListItem;
-
 namespace ash {
 enum class AppListSortOrder;
 struct AppListItemMetadata;
@@ -40,7 +38,6 @@ class AppListReorderDelegate {
   // a target position could exist, in which case `target_position` is set.
   virtual bool CalculateItemPositionInPermanentSortOrder(
       const ash::AppListItemMetadata& metadata,
-      const std::vector<const ChromeAppListItem*>& local_items,
       syncer::StringOrdinal* target_position) const = 0;
 
   // Returns the sorting order that is saved in perf service and gets shared
