fdea49af4f2e4c4cebca5c63c013b2cef67f089e
Alex Turner <alexmt@chromium.org>
2022-02-16 19:55:11 +0000

    Add optional debug fields to aggregatable reports
    Adds the debug_mode field to AggregatableReportSharedInfo and the
    debug_cleartext_payload field to AggregationServicePayload. The latter
    field is populated if the first is kEnabled. These allow for easier
    debugging by including the unencrypted payload when requested and
    enabling other server-side debugging information. See
    https://github.com/WICG/conversion-measurement-api/blob/main/AGGREGATE.md#aggregatable-reports
    for more detail.
    
    Bug: 1207974
    Change-Id: I8b897b58de481086d9cd8fa4d48fa4f4c1126b58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453284
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971998}

--- content/public/test/test_aggregation_service.h
+++ content/public/test/test_aggregation_service.h
@@ -57,7 +57,8 @@ class TestAggregationService {
                     ProcessingType processing_type,
                     url::Origin reporting_origin,
                     std::string privacy_budget_key,
-                    std::vector<url::Origin> processing_origins);
+                    std::vector<url::Origin> processing_origins,
+                    bool is_debug_mode_enabled);
     AssembleRequest(AssembleRequest&& other);
     AssembleRequest& operator=(AssembleRequest&& other);
     ~AssembleRequest();
@@ -76,6 +77,8 @@ class TestAggregationService {
     std::string privacy_budget_key;
     // Specifies the aggregation server origins.
     std::vector<url::Origin> processing_origins;
+    // Whether debug_mode should be enabled for the report.
+    bool is_debug_mode_enabled;
   };
 
   virtual ~TestAggregationService() = default;
