ef6d806ae4d28ac7a612d39b4ce5ea2f4485b9bc
tby <tby@chromium.org>
2022-02-16 06:28:35 +0000

    Fix DCHECK in KeyboardShortcutResult
    There's a DCHECK_LT(num_substitutions, 9) but there's a single shortcut
    with 9 substitutions: Highlight the previous item on your shelf.
    
    This CL fixes that and adds a test to make sure all possible results
    construct ok.
    
    Fixed: 1297002
    Change-Id: Iac4e1bff792963f297ac4ece07986310b5c970ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461400
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971662}

--- chrome/browser/ui/app_list/search/keyboard_shortcut_result_unittest.cc
+++ chrome/browser/ui/app_list/search/keyboard_shortcut_result_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/keyboard_shortcut_result.h"
 
 #include "ash/public/cpp/app_list/app_list_types.h"
+#include "ash/shortcut_viewer/keyboard_shortcut_viewer_metadata.h"
 #include "base/test/task_environment.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/events/devices/device_data_manager_test_api.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
 
 namespace app_list {
@@ -46,6 +48,21 @@ TEST_F(KeyboardShortcutResultTest, CalculateRelevance) {
       KeyboardShortcutResult::CalculateRelevance(query_tokenized, target);
 
   EXPECT_GT(relevance, 0.5);
+}
+
+// Smoke test to ensure that our assumptions about the format of each keyboard
+// shortcut result hold true.
+TEST_F(KeyboardShortcutResultTest, MakeEveryResult) {
+  // A DCHECK inside a KSV metadata utility function relies on device lists
+  // being complete.
+  ui::DeviceDataManagerTestApi().OnDeviceListsComplete();
+
+  for (const auto& item :
+       keyboard_shortcut_viewer::GetKeyboardShortcutItemList()) {
+    KeyboardShortcutResult result(
+        /*profile=*/nullptr, KeyboardShortcutData(item), /*relevance=*/0.1);
+  }
+  SUCCEED();
 }
 
 TEST_F(KeyboardShortcutResultTest,
