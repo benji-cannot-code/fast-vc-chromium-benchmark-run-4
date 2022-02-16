38f9066456940866762763a9ebfe0bc2b44dd7b3
Sammie Quon <sammiequon@chromium.org>
2022-02-16 18:35:13 +0000

    desk_templates: Update desk templates textfield & colors to match spec.
    Makes textfield hover highlight larger than text.
    Uses a flex layout which gives more control.
    Update background of some buttons for more contrast.
    Moves time view label so its not so close to the title.
    
    Specs and screenshots in bug.
    
    Test: manual
    Change-Id: I7d63f1a9e3ed13adba2a5674a8df88b11c1bedc9
    Fixed: 1292130, 1290647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466136
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971959}

--- ash/wm/desks/templates/desks_templates_grid_view.cc
+++ ash/wm/desks/templates/desks_templates_grid_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/style/ash_color_provider.h"
 #include "ash/style/pill_button.h"
 #include "ash/wm/desks/templates/desks_templates_animations.h"
 #include "ash/wm/desks/templates/desks_templates_item_view.h"
@@ -146,6 +147,9 @@ void DesksTemplatesGridView::PopulateGridUI(
       l10n_util::GetStringUTF16(
           IDS_ASH_PERSISTENT_DESKS_BAR_CONTEXT_MENU_FEEDBACK),
       PillButton::Type::kIcon, &kPersistentDesksBarFeedbackIcon));
+  feedback_button_->SetBackgroundColor(
+      AshColorProvider::Get()->GetBaseLayerColor(
+          AshColorProvider::BaseLayerType::kTransparent80));
 
   const gfx::Size previous_size = size();
 
--- ash/wm/desks/templates/save_desk_template_button.cc
+++ ash/wm/desks/templates/save_desk_template_button.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/style/ash_color_provider.h"
 #include "ash/wm/wm_highlight_item_border.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -23,6 +24,8 @@ SaveDeskTemplateButton::SaveDeskTemplateButton(base::RepeatingClosure callback)
                  &kSaveDeskAsTemplateIcon),
       callback_(callback) {
   SetBorder(std::make_unique<WmHighlightItemBorder>(kCornerRadius));
+  SetBackgroundColor(AshColorProvider::Get()->GetBaseLayerColor(
+      AshColorProvider::BaseLayerType::kTransparent80));
 }
 
 SaveDeskTemplateButton::~SaveDeskTemplateButton() = default;
