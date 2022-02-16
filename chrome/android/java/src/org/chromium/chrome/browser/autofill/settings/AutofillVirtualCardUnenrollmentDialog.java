b9771ed2993e2395a5df4890bc7f223fecd2fb44
siashah <siashah@chromium.org>
2022-02-16 00:01:34 +0000

    [VCN][M2] Add virtual card enrollment dialog
    This dialog will be called from the AutofillServerCardEditor in a
    follow up change which will also include tests.
    The implementation has some fake strings as these would be populated
    from the server response.
    
    Mock: https://screenshot.googleplex.com/9AiAnWC9dZ43pXu.png
    Implementation: https://screenshot.googleplex.com/6dSKcSU65ydApy4.png
    
    Bug: 1281695
    Change-Id: I9d6824fbe0ab2167c5e461852ac21eb601b43cd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455322
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971471}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillVirtualCardUnenrollmentDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillVirtualCardUnenrollmentDialog.java
@@ -6,36 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill.settings;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.Browser;
-import android.text.SpannableString;
-import android.view.View;
-
-import androidx.browser.customtabs.CustomTabsIntent;
 
 import org.chromium.base.Callback;
-import org.chromium.base.IntentUtils;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.LaunchIntentDispatcher;
+import org.chromium.chrome.browser.ChromeStringConstants;
+import org.chromium.chrome.browser.autofill.AutofillUiUtils;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
 import org.chromium.ui.modaldialog.SimpleModalDialogController;
 import org.chromium.ui.modelutil.PropertyModel;
-import org.chromium.ui.text.NoUnderlineClickableSpan;
-import org.chromium.ui.text.SpanApplier;
-import org.chromium.ui.text.SpanApplier.SpanInfo;
 
 /**
  * Dialog that confirms whether the user wishes to unenroll their card from Virtual Cards.
  */
 public class AutofillVirtualCardUnenrollmentDialog {
-    // LINT.IfChange
-    private static final String VIRTUAL_CARD_ENROLLMENT_SUPPORT_URL =
-            "https://support.google.com/googlepay/answer/11234179?hl=en";
-    // LINT.ThenChange(//components/autofill/core/browser/payments/payments_service_url.cc)
-
     private final Context mContext;
     private final ModalDialogManager mModalDialogManager;
     private final Callback<Boolean> mResultHandler;
@@ -61,7 +46,12 @@ public class AutofillVirtualCardUnenrollmentDialog {
                         .with(ModalDialogProperties.TITLE,
                                 mContext.getString(
                                         R.string.autofill_credit_card_editor_virtual_card_unenroll_dialog_title))
-                        .with(ModalDialogProperties.MESSAGE, buildUnenrollMessageWithLink())
+                        .with(ModalDialogProperties.MESSAGE,
+                                AutofillUiUtils.getSpannableStringWithClickableSpansToOpenLinksInCustomTabs(
+                                        mContext,
+                                        R.string.autofill_credit_card_editor_virtual_card_unenroll_dialog_message,
+                                        ChromeStringConstants
+                                                .AUTOFILL_VIRTUAL_CARD_ENROLLMENT_SUPPORT_URL))
                         .with(ModalDialogProperties.POSITIVE_BUTTON_TEXT,
                                 mContext.getString(
                                         R.string.autofill_credit_card_editor_virtual_card_unenroll_dialog_positive_button_label))
@@ -69,29 +59,5 @@ public class AutofillVirtualCardUnenrollmentDialog {
                                 mContext.getString(android.R.string.cancel));
 
         mModalDialogManager.showDialog(builder.build(), ModalDialogManager.ModalDialogType.APP);
-    }
-
-    private SpannableString buildUnenrollMessageWithLink() {
-        return SpanApplier.applySpans(
-                mContext.getString(
-                        R.string.autofill_credit_card_editor_virtual_card_unenroll_dialog_message,
-                        VIRTUAL_CARD_ENROLLMENT_SUPPORT_URL),
-                new SpanInfo("<link1>", "</link1>",
-                        new NoUnderlineClickableSpan(
-                                mContext.getResources(), createOpenMyActivityCallback())));
-    }
-
-    private Callback<View> createOpenMyActivityCallback() {
-        return (widget) -> {
-            CustomTabsIntent customTabIntent =
-                    new CustomTabsIntent.Builder().setShowTitle(true).build();
-            customTabIntent.intent.setData(Uri.parse(VIRTUAL_CARD_ENROLLMENT_SUPPORT_URL));
-            Intent intent = LaunchIntentDispatcher.createCustomTabActivityIntent(
-                    mContext, customTabIntent.intent);
-            intent.setPackage(mContext.getPackageName());
-            intent.putExtra(Browser.EXTRA_APPLICATION_ID, mContext.getPackageName());
-            IntentUtils.addTrustedIntentExtras(intent);
-            IntentUtils.safeStartActivity(mContext, intent);
-        };
     }
 }
