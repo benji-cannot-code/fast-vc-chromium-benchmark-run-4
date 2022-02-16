ab1eeeca33204aa79c0c7fa7bd6dc3c6ebd7ccb3
Sinan Sahin <sinansahin@google.com>
2022-02-16 17:31:37 +0000

    [GMNext] Make default_text_color_secondary dynamic
    Bug: 1241971
    Change-Id: Ie93c42d21c98ea828778a565788fb61a3c9dfea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3457848
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#971916}

--- chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/findinpage/FindToolbarPhone.java
@@ -64,8 +64,7 @@ public class FindToolbarPhone extends FindToolbar {
         }
         mFindQuery.setTextColor(
                 AppCompatResources.getColorStateList(getContext(), queryTextColorId));
-        mFindQuery.setHintTextColor(
-                ApiCompatibilityUtils.getColor(getContext().getResources(), queryHintTextColorId));
+        mFindQuery.setHintTextColor(getContext().getColor(queryHintTextColorId));
     }
 
     @Override
