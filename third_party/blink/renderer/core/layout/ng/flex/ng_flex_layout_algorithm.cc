be5c483e890177c1084cc8e3880c5780fc398089
Kent Tamura <tkent@chromium.org>
2022-02-16 04:35:38 +0000

    Add a UseCounter for multiple-lines BUTTONs with non-whitespace text
    Bug: 690036
    Change-Id: I6cb1e14e4c6abd338ec4d52f7dc208601e4d801d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466975
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971631}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -39,6 +39,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+bool ContainsNonWhitespace(const LayoutBox* box) {
+  const LayoutObject* next = box;
+  while ((next = next->NextInPreOrder(box))) {
+    if (const auto* text = DynamicTo<LayoutText>(next)) {
+      if (!text->GetText().ContainsOnlyWhitespaceOrEmpty())
+        return true;
+    }
+  }
+  return false;
+}
+
+}  // anonymous namespace
+
 NGFlexLayoutAlgorithm::NGFlexLayoutAlgorithm(
     const NGLayoutAlgorithmParams& params,
     DevtoolsFlexInfo* layout_info_for_devtools)
@@ -1473,6 +1488,12 @@ void NGFlexLayoutAlgorithm::AdjustButtonBaseline(
   if (container_builder_.Baseline() != child_baseline) {
     UseCounter::Count(Node().GetDocument(),
                       WebFeature::kWrongBaselineOfMultiLineButton);
+    String text = Node().GetDOMNode()->textContent();
+    if (ContainsNonWhitespace(Node().GetLayoutBox())) {
+      UseCounter::Count(
+          Node().GetDocument(),
+          WebFeature::kWrongBaselineOfMultiLineButtonWithNonSpace);
+    }
   }
 }
 
