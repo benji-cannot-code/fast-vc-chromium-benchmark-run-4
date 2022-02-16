d2ddb316c8a56b6e4d1ca78af630f4211ccb90ed
Hiroki Nakagawa <nhiroki@chromium.org>
2022-02-16 00:55:29 +0000

    Reland "Month dwell time metric"
    This reverts commit 87b7f43c5a009d44fb83e1c0d8f7ef64a7ba1d6d.
    
    Reason for revert:
    This change causes compile failures on CrOS bots.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20Full/207958/overview
    
    Original change's description:
    > Revert "Month dwell time metric"
    >
    > This reverts commit ac8dd45401ceaedcf9efbabb4b1dbe90cfcbdf5c.
    >
    > Reason for revert: need to add metrics to //ash/histograms.xml
    >
    > Original change's description:
    > > Month dwell time metric
    > >
    > > Records time spent on a month before scrolling to another one.
    > >
    > > Bug: 1290966
    > > Change-Id: Ie5e25450a56e5ba94000b41f39ded099be1a80b1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3433996
    > > Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    > > Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#970889}
    >
    > Bug: 1290966
    > Change-Id: I7b147dc23ce9fcba60587b6fcadf76f6cc239d41
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465007
    > Auto-Submit: Kevin Radtke <kradtke@chromium.org>
    > Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    > Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#971465}
    
    Bug: 1290966
    Change-Id: I19cbd9568f0bd1be54fbb7c0fbf60eeec83fb0f5
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466874
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Owners-Override: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#971491}

--- ash/system/time/calendar_metrics.h
+++ ash/system/time/calendar_metrics.h
@@ -10,6 +10,10 @@ namespace ui {
 class Event;
 }  // namespace ui
 
+namespace base {
+class TimeDelta;
+}  // namespace base
+
 namespace ash {
 
 namespace calendar_metrics {
@@ -52,6 +56,8 @@ void RecordCalendarDateCellActivated(const ui::Event& event);
 void RecordMonthArrowButtonActivated(bool up, const ui::Event& event);
 
 void RecordEventListItemActivated(const ui::Event& event);
+
+void RecordMonthDwellTime(const base::TimeDelta& dwell_time);
 
 }  // namespace calendar_metrics
 
