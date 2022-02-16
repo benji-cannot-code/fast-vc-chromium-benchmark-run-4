abf015641aced01b32b9481ddb52af2dba538ddf
Melissa Zhang <melzhang@google.com>
2022-02-16 04:05:56 +0000

    [Sharesheet] Update empty state icon to use lottie assets for dark mode.
    Screenshots in bug.
    
    Bug: 1263326
    Change-Id: I1cb21099a9d3f35ceffecffbd024fb1e34ae90ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3459370
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971621}

--- chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view_unittest.cc
+++ chrome/browser/ui/ash/sharesheet/sharesheet_bubble_view_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/test/test_clipboard.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/events/base_event_utils.h"
+#include "ui/lottie/resource.h"
 #include "ui/views/controls/native/native_view_host.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
@@ -95,6 +96,10 @@ class SharesheetBubbleViewTest : public ChromeAshTestBase {
     widget->GetContentsView()->AddChildView(content_view);
 
     parent_window_ = widget->GetNativeWindow();
+
+    ui::ResourceBundle::SetLottieParsingFunctions(
+        &lottie::ParseLottieAsStillImage,
+        &lottie::ParseLottieAsThemedStillImage);
   }
 
   void ShowAndVerifyBubble(apps::mojom::IntentPtr intent,
