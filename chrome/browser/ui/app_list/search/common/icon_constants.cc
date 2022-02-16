2bcc4593a7e42fa0c47cface76f8e2946429c8e7
Toni Barzic <tbarzic@chromium.org>
2022-02-16 03:17:55 +0000

    Improve default omnibox result icons contrast in new launcher
    The generic omnibox icon color does not work well in productivity
    launcher, this changes the icon color to button icon color (as indicated
    by spec for remove action icon). Longer term, we should pass vector
    icons instead of image skias to search model, and let the UI set correct
    icon sizes and colors.
    
    Change-Id: Icdab4a7c5916b1d40b408a54e8cc2f46893d4cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465743
    Reviewed-by: Rachel Wong <wrong@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971593}

--- chrome/browser/ui/app_list/search/common/icon_constants.cc
+++ chrome/browser/ui/app_list/search/common/icon_constants.cc
@@ -6,8 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/common/icon_constants.h"
 
 #include "ash/constants/ash_features.h"
+#include "ash/public/cpp/style/color_provider.h"
 
 namespace app_list {
+
+namespace {
+
+constexpr SkColor kOmniboxGenericIconColor = gfx::kGoogleGrey700;
+
+}  // namespace
 
 int GetAnswerCardIconDimension() {
   return ash::features::IsProductivityLauncherEnabled() ? 28 : 24;
@@ -19,6 +26,14 @@ int GetAppIconDimension() {
 
 int GetImageIconDimension() {
   return ash::features::IsProductivityLauncherEnabled() ? 28 : 32;
+}
+
+SkColor GetGenericIconColor() {
+  if (ash::features::IsProductivityLauncherEnabled()) {
+    return ash::ColorProvider::Get()->GetContentLayerColor(
+        ash::ColorProvider::ContentLayerType::kButtonIconColor);
+  }
+  return kOmniboxGenericIconColor;
 }
 
 }  // namespace app_list
