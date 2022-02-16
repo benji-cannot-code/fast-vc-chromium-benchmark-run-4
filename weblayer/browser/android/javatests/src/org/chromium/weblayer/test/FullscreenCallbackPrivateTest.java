fe19d03efd03c6431ec0c5378993a1c1697c1817
Dominic Battre <battre@chromium.org>
2022-02-16 14:09:27 +0000

    Disable o.c.weblayer.test.FullscreenCallbackPrivateTest#testToastShown
    Disabling test due to flakiness.
    
    TBR=sky@chromium.org
    
    Bug: 1293419
    Change-Id: Iecdd1df3c33b2fd4fbdcb9ee120aae886f0537d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468537
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Owners-Override: Dominic Battré <battre@chromium.org>
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Reviewed-by: Elizabeth Popova <lizapopova@google.com>
    Commit-Queue: Elizabeth Popova <lizapopova@google.com>
    Cr-Commit-Position: refs/heads/main@{#971795}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/FullscreenCallbackPrivateTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/FullscreenCallbackPrivateTest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.FullscreenCallback;
 import org.chromium.weblayer.TestWebLayer;
@@ -66,6 +67,7 @@ public class FullscreenCallbackPrivateTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1293419")
     public void testToastShown() throws Throwable {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             mActivity.getTab().setFullscreenCallback(new FullscreenCallback() {
