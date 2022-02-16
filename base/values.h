0da32b97a3107e2be96d5eb92782247d0527ec49
Daniel Cheng <dcheng@chromium.org>
2022-02-16 19:40:29 +0000

    Make base::Value::DoubleStorage a proper class.
    This allows simplification of a number of comparison operators.
    
    Change-Id: I56dc57bd40e9ac7c2d287116ffc4c68eca7d727d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3464508
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971988}

--- base/values.h
+++ base/values.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
 #include <initializer_list>
 #include <iosfwd>
 #include <memory>
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_export.h"
+#include "base/bit_cast.h"
 #include "base/containers/checked_iterators.h"
 #include "base/containers/checked_range.h"
 #include "base/containers/cxx20_erase_vector.h"
@@ -1005,14 +1007,47 @@ class BASE_EXPORT Value {
   //
   // To override this, store the value as an array of 32-bit integers, and
   // perform the appropriate bit casts when reading / writing to it.
-  using DoubleStorage = struct { alignas(4) char v[sizeof(double)]; };
+  class DoubleStorage {
+   public:
+    explicit DoubleStorage(double v);
+    DoubleStorage(const DoubleStorage&) = default;
+    DoubleStorage& operator=(const DoubleStorage&) = default;
+
+    double value() const { return bit_cast<double>(v_); }
+
+   private:
+    friend bool operator==(const DoubleStorage& lhs, const DoubleStorage& rhs) {
+      return lhs.value() == rhs.value();
+    }
+
+    friend bool operator!=(const DoubleStorage& lhs, const DoubleStorage& rhs) {
+      return !(lhs == rhs);
+    }
+
+    friend bool operator<(const DoubleStorage& lhs, const DoubleStorage& rhs) {
+      return lhs.value() < rhs.value();
+    }
+
+    friend bool operator>(const DoubleStorage& lhs, const DoubleStorage& rhs) {
+      return rhs < lhs;
+    }
+
+    friend bool operator<=(const DoubleStorage& lhs, const DoubleStorage& rhs) {
+      return !(rhs < lhs);
+    }
+
+    friend bool operator>=(const DoubleStorage& lhs, const DoubleStorage& rhs) {
+      return !(lhs < rhs);
+    }
+
+    alignas(4) std::array<char, sizeof(double)> v_;
+  };
 
   // Internal constructors, allowing the simplify the implementation of Clone().
   explicit Value(absl::monostate);
   explicit Value(DoubleStorage storage);
 
   friend class ValuesTest_SizeOfValue_Test;
-  double AsDoubleInternal() const;
 
   absl::variant<absl::monostate,
                 bool,
