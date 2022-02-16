aae65ff5475472423cb84c7b42d3a7b8acfc3d6e
Yoshifumi Inoue <yosin@chromium.org>
2022-02-16 13:44:31 +0000

    Make FontGlobalContext::Get() to return FontGlobalContext&
    This patch changes |FontGlobalContext::Get()| to return reference of
    |FontGlobalContext| instead of pointer because it is always non-null
    for improving readability.
    
    Bug: 1264280
    Change-Id: I506aa0bfb9322de2bb0cde60bd20bc36bc938c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466858
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971782}

--- third_party/blink/renderer/core/css/local_font_face_source.cc
+++ third_party/blink/renderer/core/css/local_font_face_source.cc
@@ -32,7 +32,7 @@ LocalFontFaceSource::~LocalFontFaceSource() {}
 
 bool LocalFontFaceSource::IsLocalNonBlocking() const {
   FontUniqueNameLookup* unique_name_lookup =
-      FontGlobalContext::Get()->GetFontUniqueNameLookup();
+      FontGlobalContext::Get().GetFontUniqueNameLookup();
   if (!unique_name_lookup)
     return true;
   return unique_name_lookup->IsFontUniqueNameLookupReadyForSyncLookup();
@@ -122,7 +122,7 @@ void LocalFontFaceSource::BeginLoadIfNeeded() {
     return;
 
   FontUniqueNameLookup* unique_name_lookup =
-      FontGlobalContext::Get()->GetFontUniqueNameLookup();
+      FontGlobalContext::Get().GetFontUniqueNameLookup();
   DCHECK(unique_name_lookup);
   unique_name_lookup->PrepareFontUniqueNameLookup(
       WTF::Bind(&LocalFontFaceSource::NotifyFontUniqueNameLookupReady,
