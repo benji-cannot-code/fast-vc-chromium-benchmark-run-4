73130e939b8bf8c1b87accf32e3689ef8c9892eb
Adam Langley <agl@chromium.org>
2022-02-16 04:48:31 +0000

    Revert "webauthn: enable discoverable credentials over caBLEv2"
    This reverts commit 1e13a5498291ee2032423c69ed0c032b629f5cec.
    
    Reason for revert: don't want this in M100. Will reland after the branch.
    
    Original change's description:
    > webauthn: enable discoverable credentials over caBLEv2
    >
    > The Android implementation of caBLEv2 does not yet support discoverable
    > credentials. Because of this, Chromium on the desktop assumes that no
    > caBLEv2 implementation supports them. But Android isn't the only
    > implementation now.
    >
    > Thus this change enables discoverable credentials over caBLEv2 on the
    > desktop. Because it's too late to get a new error string on Android for
    > M100, I plan on reverting this change prior to the M100 branch. If
    > someone really wants to try this in M100, the flag is in chrome://flags.
    >
    > On Android, we need to handle this case better. Thus this change also:
    >
    >   1) Adds a specific error for discoverable credential requests.
    >   2) Advertises CTAP 2.1 support so that clients will send an
    >      authenticatorSelection command, rather than a dummy makeCredential,
    >      if they want to make a caBLE device “flash”. That is caught and
    >      also results in a specific error.
    >
    > BUG=1002262
    >
    > Change-Id: I2da176ac57f985ec6dd2853cef38862efffe1597
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3433592
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Auto-Submit: Adam Langley <agl@chromium.org>
    > Commit-Queue: Adam Langley <agl@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#969697}
    
    Bug: 1002262
    Change-Id: I8b848852a7465431cc9b0eae506a2a2a722ffc96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466425
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971634}

--- device/fido/cable/v2_test_util.cc
+++ device/fido/cable/v2_test_util.cc
@@ -32,7 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/webauthn/authenticator.mojom.h"
 #include "url/gurl.h"
 
-namespace device::cablev2 {
+namespace device {
+namespace cablev2 {
 namespace {
 
 // TestNetworkContext intercepts WebSocket creation calls and simulates a
@@ -357,11 +358,9 @@ class DummyBLEAdvert
 class TestPlatform : public authenticator::Platform {
  public:
   TestPlatform(Discovery::AdvertEventStream::Callback ble_advert_callback,
-               device::VirtualCtap2Device* ctap2_device,
-               authenticator::Observer* observer)
+               device::VirtualCtap2Device* ctap2_device)
       : ble_advert_callback_(ble_advert_callback),
-        ctap2_device_(ctap2_device),
-        observer_(observer) {}
+        ctap2_device_(ctap2_device) {}
 
   void MakeCredential(
       blink::mojom::PublicKeyCredentialCreationOptionsPtr params,
@@ -402,17 +401,8 @@ class TestPlatform : public authenticator::Platform {
                        weak_factory_.GetWeakPtr(), std::move(callback)));
   }
 
-  void OnStatus(Status status) override {
-    if (observer_) {
-      observer_->OnStatus(status);
-    }
-  }
-
-  void OnCompleted(absl::optional<Error> maybe_error) override {
-    if (observer_) {
-      observer_->OnCompleted(maybe_error);
-    }
-  }
+  void OnStatus(Status status) override {}
+  void OnCompleted(absl::optional<Error> maybe_error) override {}
 
   std::unique_ptr<authenticator::Platform::BLEAdvert> SendBLEAdvert(
       base::span<const uint8_t, kAdvertSize> payload) override {
@@ -523,7 +513,6 @@ class TestPlatform : public authenticator::Platform {
 
   Discovery::AdvertEventStream::Callback ble_advert_callback_;
   const raw_ptr<device::VirtualCtap2Device> ctap2_device_;
-  authenticator::Observer* const observer_;
   base::WeakPtrFactory<TestPlatform> weak_factory_{this};
 };
 
@@ -538,11 +527,11 @@ namespace authenticator {
 
 std::unique_ptr<authenticator::Platform> NewMockPlatform(
     Discovery::AdvertEventStream::Callback ble_advert_callback,
-    device::VirtualCtap2Device* ctap2_device,
-    authenticator::Observer* observer) {
-  return std::make_unique<TestPlatform>(ble_advert_callback, ctap2_device,
-                                        observer);
+    device::VirtualCtap2Device* ctap2_device) {
+  return std::make_unique<TestPlatform>(ble_advert_callback, ctap2_device);
 }
 
 }  // namespace authenticator
-}  // namespace device::cablev2
+
+}  // namespace cablev2
+}  // namespace device
