3c8965e358560a167b25d881a4300276e3f0108d
Dale Curtis <dalecurtis@chromium.org>
2022-02-16 02:27:52 +0000

    Add createImageBitmap and drawImage tests for ImageDecoder frames.
    This adds createImageBitmap tests for ImageDecoder, VideoDecoder,
    and canvas backed frames as well as a drawImage test for an
    ImageDecoder backed frame.
    
    This ensures we have coverage of SkImage unwrapping paths as well
    as general coverage for createImageBitmap.
    
    Even though createImageBitmap(videoFrameFromCanvas) is unlikely to
    be common, it ensure coverage of texture backed VideoFrame
    to ImageBitmap.
    
    R=sandersd
    
    Bug: 1275360
    Change-Id: I415d4acf9a9927307aa0e5b776a6a8541caa652d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466581
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971565}

---
+++ third_party/blink/perf_tests/webcodecs/createImageBitmap-test.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function runCreateImageBitmapTest(frame, desc) {
+  let isDone = false;
+
+  function runTest() {
+    let startTime = PerfTestRunner.now();
+    PerfTestRunner.addRunTestStartMarker();
+    window.createImageBitmap(frame)
+        .then(bitmap => {
+          PerfTestRunner.measureValueAsync(PerfTestRunner.now() - startTime);
+          PerfTestRunner.addRunTestEndMarker();
+          if (!isDone)
+            runTest();
+        })
+        .catch(e => {
+          PerfTestRunner.logFatalError('Test error: ' + e);
+        })
+  }
+
+  PerfTestRunner.startMeasureValuesAsync({
+    description: desc,
+    unit: 'ms',
+    done: _ => {
+      isDone = true;
+      frame.close();
+    },
+    run: _ => {
+      runTest();
+    },
+  });
+}
