a1d0b492c226a45dc4edd4e1b1b0d83d5c876efc
Curtis McMullan <curtismcmullan@chromium.org>
2022-02-16 01:49:59 +0000

    ime: reinstate ime_service ConnectToInputMethod
    Previously in crrev/c/3439146 we cut over the ime service connection
    logic from the old ConnectToInputMethod, to the new ConnectionFactory
    method.
    
    As a precaution, we would like to gate the use of the new
    ConnectionFactory method behind the multi word flag which requires it.
    This is a safety measure to ensure we can switch off the logic if
    required.
    
    This CL in light of this reintroduces the body of the deprecated method
    ImeService::ConnectToInputMethod.
    
    In a future CL we will update the connection from the NativeIME to use
    ImeService::ConnectToInputMethod by default, and
    ImeService::InitializeConnectionFactory when the multi word flag is
    enabled.
    
    BUG=b:209695253
    
    Change-Id: Icb76b431acd62e9579edac0addc1ae3225b97f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462617
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971539}

--- ash/services/ime/connection_factory.h
+++ ash/services/ime/connection_factory.h
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "ash/services/ime/associated_rule_based_engine.h"
 #include "ash/services/ime/public/mojom/connection_factory.mojom.h"
 #include "ash/services/ime/public/mojom/input_method.mojom.h"
 #include "ash/services/ime/public/mojom/input_method_host.mojom.h"
-#include "ash/services/ime/rule_based_engine.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_remote.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -46,7 +46,7 @@ class ConnectionFactory : public mojom::ConnectionFactory {
 
   // This connection factory is only ever used to connect to a rule based
   // engine.
-  std::unique_ptr<RuleBasedEngine> rule_based_engine_;
+  std::unique_ptr<AssociatedRuleBasedEngine> rule_based_engine_;
 };
 
 }  // namespace ime
