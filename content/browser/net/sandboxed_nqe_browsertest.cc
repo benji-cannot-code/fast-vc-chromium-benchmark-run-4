a65e970245b26d1554074bcdf5f4dbe3870c6481
Yoichi Osato <yoichio@chromium.org>
2022-02-16 01:03:25 +0000

    Introduce SandboxedNQEBrowserTest
    This CL adds a sandboxed test harness and a test to create a network
    service inside it.
    That's because in the original design doc [1], creating the service
    failed in a sandboxed process due to the network quality estimator
    components, in which some system calls are removed.
    
    I'm planning to add more tests for it.
    
    Bug: 1297437
    Change-Id: Ia7f39ac33bf8800089bd175ce76feead36ca514f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460888
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#971502}

---
+++ content/browser/net/sandboxed_nqe_browsertest.cc
@@ -0,0 +1,61 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
+#include "content/public/browser/network_service_instance.h"
+#include "content/public/common/content_features.h"
+#include "content/public/common/network_service_util.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/content_browser_test.h"
+#include "sandbox/policy/features.h"
+
+namespace content {
+namespace {
+
+class SandboxedNQEBrowserTest : public ContentBrowserTest {
+ public:
+  SandboxedNQEBrowserTest() {
+    std::vector<base::Feature> enabled_features = {
+#if !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_FUCHSIA)
+      // Network Service Sandboxing is unconditionally enabled on these
+      // platforms.
+      sandbox::policy::features::kNetworkServiceSandbox,
+#endif
+    };
+    scoped_feature_list_.InitWithFeatures(
+        enabled_features,
+        /*disabled_features=*/{features::kNetworkServiceInProcess});
+  }
+
+  void SetUp() override {
+#if BUILDFLAG(IS_WIN)
+    if (!sandbox::policy::features::IsWinNetworkServiceSandboxSupported()) {
+      // On *some* Windows, sandboxing cannot be enabled. We skip all the tests
+      // on such platforms.
+      GTEST_SKIP();
+    }
+#endif
+
+    // These assertions need to precede ContentBrowserTest::SetUp to prevent the
+    // test body from running when one of the assertions fails.
+    ASSERT_TRUE(IsOutOfProcessNetworkService());
+    ASSERT_TRUE(sandbox::policy::features::IsNetworkSandboxEnabled());
+
+    ContentBrowserTest::SetUp();
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(SandboxedNQEBrowserTest, GetNetworkService) {
+  EXPECT_TRUE(GetNetworkService());
+}
+
+// TODO(yoichio): Add more tests.
+
+}  // namespace
+}  // namespace content
