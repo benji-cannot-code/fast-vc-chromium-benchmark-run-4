cf3548a369a4f044ac50c597dead1a93dfe02959
Roger Tinkoff <rtinkoff@google.com>
2022-02-17 13:59:50 +0000

    ash: Remove the concept of "non-prunable" months
    Treat the set of "base" months surrounding today's date the same as
    any other months.  Meaning: if we scroll far enough away from today's
    date these months' events will be purged from the cache, and will be
    fetched again next time we scroll to them.
    
    Bug: 1297826
    Change-Id: If6a8769abe80a8d29194a2dbaaf2ab7fa4779847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3470179
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Roger Tinkoff <rtinkoff@google.com>
    Cr-Commit-Position: refs/heads/main@{#972421}

--- ash/system/time/calendar_model.cc
+++ ash/system/time/calendar_model.cc
@@ -107,9 +107,8 @@ class CalendarEventFetch {
 };
 
 // The calendar model itself.
-CalendarModel::CalendarModel(const std::set<base::Time>& non_prunable_months)
-    : non_prunable_months_(non_prunable_months) {
-  FetchEventsForBaseMonths();
+CalendarModel::CalendarModel(const std::set<base::Time>& base_months) {
+  FetchEvents(base_months);
 }
 
 CalendarModel::~CalendarModel() = default;
@@ -125,11 +124,6 @@ void CalendarModel::RemoveObserver(Observer* observer) {
 }
 
 bool CalendarModel::IsMonthAlreadyFetched(base::Time start_of_month) const {
-  if (non_prunable_months_fetched_.find(start_of_month) !=
-      non_prunable_months_fetched_.end()) {
-    return true;
-  }
-
   for (auto& month : prunable_months_mru_) {
     if (month == start_of_month)
       return true;
@@ -168,17 +162,10 @@ void CalendarModel::MaybeFetchMonth(base::Time start_of_month) {
 }
 
 void CalendarModel::MarkMonthAsFetched(base::Time start_of_month) {
-  if (non_prunable_months_.find(start_of_month) != non_prunable_months_.end())
-    non_prunable_months_fetched_.emplace(start_of_month);
-  else
-    QueuePrunableMonth(start_of_month);
+  QueuePrunableMonth(start_of_month);
 }
 
 void CalendarModel::QueuePrunableMonth(base::Time start_of_month) {
-  // For safety, make sure we aren't queuing a non-prunable month.
-  if (non_prunable_months_.find(start_of_month) != non_prunable_months_.end())
-    return;
-
   // If start_of_month is already most-recently-used, nothing to do.
   if (!prunable_months_mru_.empty() &&
       prunable_months_mru_.front() == start_of_month)
@@ -198,11 +185,6 @@ void CalendarModel::QueuePrunableMonth(base::Time start_of_month) {
 
 void CalendarModel::FetchEvents(const std::set<base::Time>& months) {
   for (auto& month : months)
-    MaybeFetchMonth(month.UTCMidnight());
-}
-
-void CalendarModel::FetchEventsForBaseMonths() {
-  for (auto& month : non_prunable_months_)
     MaybeFetchMonth(month.UTCMidnight());
 }
 
