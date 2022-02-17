c0567fa22c077058d7c471845341ec54bf756e64
Adithya Srinivasan <adithyas@chromium.org>
2022-02-17 14:29:20 +0000

    ChromeDriver: Fix switchToFrame for shadow-dom fenced frames
    The shadow-dom implementation of fenced frames uses an iframe - this CL
    associates the fenced frame element with the frame id of the iframe
    in DomTracker to make switchToFrame(fencedFrame) switch to the iframe
    inside the shadow dom tree.
    
    Bug: chromedriver:4002
    Change-Id: I1d916d5c69a415d7a168a0974d747e3f26029b20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441481
    Reviewed-by: Vladimir Nechaev <nechaev@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972433}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -4027,10 +4027,15 @@ class ChromeDriverTestLegacy(ChromeDriverBaseTestWithWebServer):
     self.assertEqual('events: touchstart touchend', events.GetText())
 
 class ChromeDriverFencedFrame(ChromeDriverBaseTestWithWebServer):
-  def setUp(self):
-    self._driver = self.CreateDriver(chrome_switches=['--site-per-process', '--enable-features=FencedFrames:implementation_type/mparch'])
+  def testCanSwitchToFencedFrame_ShadowDom(self):
+    self.runTest('shadow_dom')
 
-  def testCanSwitchToFencedFrame(self):
+  def testCanSwitchToFencedFrame_MPArch(self):
+    self.runTest('mparch')
+
+  def runTest(self, fenced_frame_implementation):
+    self._driver = self.CreateDriver(chrome_switches=['--site-per-process',
+        '--enable-features=FencedFrames:implementation_type/%s' % fenced_frame_implementation])
     self._http_server.SetDataForPath('/main.html', bytes("""
       <!DOCTYPE html>
         <html>
