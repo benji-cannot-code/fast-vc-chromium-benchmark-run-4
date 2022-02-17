a4d0125db89b3c85bef15c48dc8722ec3f491368
David Sanders <dsanders11@ucsbalum.com>
2022-02-17 13:38:33 +0000

    Replace <string> with <iosfwd> in //base/strings/string_piece_forward.h
    std::char_traits is forward declared in <iosfwd>
    
    Change-Id: Icf7bdda55260d5cc5c4553fff25827a23c4db9d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3471359
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972413}

--- base/strings/string_piece_forward.h
+++ base/strings/string_piece_forward.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_STRINGS_STRING_PIECE_FORWARD_H_
 #define BASE_STRINGS_STRING_PIECE_FORWARD_H_
 
-#include <string>
+#include <iosfwd>
 
 namespace base {
 
