d78eb24149d4453d2453bc2c49db972e22129a20
Johannes Kron <kron@chromium.org>
2022-02-17 02:26:41 +0000

    Add StatsCollectingEncoder for the MediaCapabilities API
    This encoder is used to collect relevant WebRTC encode stats
    that is needed for the MediaCapabilities API to predict if
    future streams can be handled smoothly or not.
    
    Bug: chromium:1187565
    Change-Id: Id8191a1493286065843713beff182272326a0fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3463439
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972235}

--- third_party/blink/renderer/platform/peerconnection/webrtc_encoding_info_handler.cc
+++ third_party/blink/renderer/platform/peerconnection/webrtc_encoding_info_handler.cc
@@ -30,7 +30,8 @@ WebrtcEncodingInfoHandler* WebrtcEncodingInfoHandler::Instance() {
 
 WebrtcEncodingInfoHandler::WebrtcEncodingInfoHandler()
     : WebrtcEncodingInfoHandler(blink::CreateWebrtcVideoEncoderFactory(
-                                    Platform::Current()->GetGpuFactories()),
+                                    Platform::Current()->GetGpuFactories(),
+                                    base::DoNothing()),
                                 blink::CreateWebrtcAudioEncoderFactory()) {}
 
 WebrtcEncodingInfoHandler::WebrtcEncodingInfoHandler(
