bd928e3c212a5a16c65b5161046e0c97754ae8b9
Taylor Bergquist <tbergquist@chromium.org>
2022-02-17 23:52:48 +0000

    Move layout_helper_ from TabStrip down into TabContainer.
    For now, TabContainer provides an accessor for TabStrip to get the
    TabStripLayoutHelper and use it at before. Followup CLs will move call
    sites down into TabContainer as it becomes possible to do so.
    
    Bug: 1295774
    Change-Id: Ie157f1bf1595f087b7d755f2cfbe079f8397ac30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3459796
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972720}

--- chrome/browser/ui/views/tabs/tab_container.h
+++ chrome/browser/ui/views/tabs/tab_container.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include "chrome/browser/ui/views/tabs/tab.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_layout_helper.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
@@ -21,10 +22,10 @@ class TabContainer : public views::View {
  public:
   METADATA_HEADER(TabContainer);
 
-  TabContainer();
+  explicit TabContainer(TabStripController* controller_);
   ~TabContainer() override;
 
-  Tab* AddTab(std::unique_ptr<Tab> tab, int model_index);
+  Tab* AddTab(std::unique_ptr<Tab> tab, int model_index, TabPinned pinned);
   void MoveTab(Tab* tab, int from_model_index, int to_model_index);
 
   // Remove the tab from |tabs_view_model_|, but *not* from the View hierarchy,
@@ -40,6 +41,13 @@ class TabContainer : public views::View {
   Tab* GetTabAtModelIndex(int index) const;
 
   int GetTabCount() const;
+
+  // TODO (1295774): Move callers down into TabContainer so this
+  // encapsulation-breaking getter can be removed.
+  TabStripLayoutHelper* layout_helper() const { return layout_helper_.get(); }
+
+  // views::View
+  gfx::Size GetMinimumSize() const override;
 
  private:
   // Returns the corresponding view index of a |tab| to be inserted at
@@ -62,6 +70,8 @@ class TabContainer : public views::View {
   // |layout_helper_| (and remains a View child) until
   // the remove animation completes.
   views::ViewModelT<Tab> tabs_view_model_;
+
+  std::unique_ptr<TabStripLayoutHelper> layout_helper_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TABS_TAB_CONTAINER_H_
