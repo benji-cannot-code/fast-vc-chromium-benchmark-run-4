cd555a026b379426c02eaa18dfbd6151a07f0e39
Christian Biesinger <cbiesinger@chromium.org>
2022-02-17 17:50:55 +0000

    [FedCM] Support IdentityRequestDialogController in content_shell
    Similar in spirit to whitelisted permissions in ShellPermissionManager,
    this auto-selects the first account.
    
    Bug: 1155312
    Change-Id: I38e281685ad078d212c19e2500c3ea8b927c5de4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3470975
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972521}

--- content/shell/browser/shell_content_browser_client.cc
+++ content/shell/browser/shell_content_browser_client.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell_browser_context.h"
 #include "content/shell/browser/shell_browser_main_parts.h"
 #include "content/shell/browser/shell_devtools_manager_delegate.h"
+#include "content/shell/browser/shell_identity_dialog_controller.h"
 #include "content/shell/browser/shell_paths.h"
 #include "content/shell/browser/shell_quota_permission_context.h"
 #include "content/shell/browser/shell_web_contents_view_delegate_creator.h"
@@ -616,6 +617,11 @@ void ShellContentBrowserClient::GetHyphenationDictionary(
 
 bool ShellContentBrowserClient::HasErrorPage(int http_status_code) {
   return http_status_code >= 400 && http_status_code < 600;
+}
+
+std::unique_ptr<IdentityRequestDialogController>
+ShellContentBrowserClient::CreateIdentityRequestDialogController() {
+  return std::make_unique<ShellIdentityDialogController>();
 }
 
 void ShellContentBrowserClient::CreateFeatureListAndFieldTrials() {
