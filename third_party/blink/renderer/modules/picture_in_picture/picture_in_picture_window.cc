21afca1f5b9bc46c3af4e78066096a765a1e7d12
Tommy Steimel <steimel@chromium.org>
2022-02-17 16:23:25 +0000

    PiP2: Return a document for window.requestPictureInPictureWindow()
    This CL modifies the |window.requestPictureInPictureWindow()| API to
    actually return a PictureInPictureWindow object in its promise. This
    PictureInPictureWindow object has a document on it that the website can
    use to populate the PiP window.
    
    Bug: 1253970
    Change-Id: Id53d2e46a376b8e2713e9cb9db3d0ed08a30c530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456162
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972470}

--- third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_window.cc
+++ third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_window.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/picture_in_picture/picture_in_picture_window.h"
 
+#include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
@@ -17,8 +18,17 @@ PictureInPictureWindow::PictureInPictureWindow(
     const gfx::Size& size)
     : ExecutionContextClient(execution_context), size_(size) {}
 
+PictureInPictureWindow::PictureInPictureWindow(
+    ExecutionContext* execution_context,
+    const gfx::Size& size,
+    Document* document)
+    : ExecutionContextClient(execution_context),
+      size_(size),
+      document_(document) {}
+
 void PictureInPictureWindow::OnClose() {
   size_ = gfx::Size();
+  document_.Clear();
 }
 
 void PictureInPictureWindow::OnResize(const gfx::Size& size) {
@@ -50,6 +60,7 @@ bool PictureInPictureWindow::HasPendingActivity() const {
 }
 
 void PictureInPictureWindow::Trace(Visitor* visitor) const {
+  visitor->Trace(document_);
   EventTargetWithInlineData::Trace(visitor);
   ExecutionContextClient::Trace(visitor);
 }
