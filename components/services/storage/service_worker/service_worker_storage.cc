48d540817efe6af259e7bc8c3b7eab7f9f3744c2
Minoru Chikamune <chikamune@chromium.org>
2022-02-17 23:37:57 +0000

    service worker: UMA: Add metrics to track ServiceWorker initialization on browser startup (follow up)
    This CL is a follow up CL for https://crrev.com/c/3448624.
    
    In this CL:
     - Replace UMA_HISTOGRAM_MEDIUM_TIMES with
       base::UmaHistogramMediumTimes() because it's preferred.
    
    Bug: 1146713
    Change-Id: I522c7c742b129ddf1ba2903bf64a65651260bf93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468878
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972709}

--- components/services/storage/service_worker/service_worker_storage.cc
+++ components/services/storage/service_worker/service_worker_storage.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/run_loop.h"
 #include "base/task/post_task.h"
 #include "base/task/sequenced_task_runner.h"
@@ -1610,8 +1609,9 @@ void ServiceWorkerStorage::ReadInitialDataFromDB(
   original_task_runner->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), std::move(data), status));
 
-  UMA_HISTOGRAM_MEDIUM_TIMES("ServiceWorker.Storage.ReadInitialDataFromDB.Time",
-                             base::TimeTicks::Now() - now);
+  base::UmaHistogramMediumTimes(
+      "ServiceWorker.Storage.ReadInitialDataFromDB.Time",
+      base::TimeTicks::Now() - now);
 }
 
 void ServiceWorkerStorage::DeleteRegistrationFromDB(
--- content/browser/service_worker/service_worker_context_wrapper.cc
+++ content/browser/service_worker/service_worker_context_wrapper.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool.h"
@@ -1630,7 +1630,7 @@ void ServiceWorkerContextWrapper::DidGetRegisteredStorageKeys(
   if (on_registrations_initialized_)
     std::move(on_registrations_initialized_).Run();
 
-  UMA_HISTOGRAM_MEDIUM_TIMES(
+  base::UmaHistogramMediumTimes(
       "ServiceWorker.Storage.RegisteredStorageKeyCacheInitialization.Time",
       base::TimeTicks::Now() - start_time);
 }
