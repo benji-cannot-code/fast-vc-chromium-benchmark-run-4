4278b075f2070318eb5afcd910d219cc6a0780c3
Matt Walliser <mattwalliser@google.com>
2022-02-17 21:12:05 +0000

    [CameraRoll] Update multidevice settings to use combined setup item.
    Bug: 1221297
    Change-Id: I746beb1a3f9cb3af9aa294825dadcc952c617561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3389998
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Matt Walliser <mattwalliser@google.com>
    Cr-Commit-Position: refs/heads/main@{#972624}

--- chromeos/services/multidevice_setup/global_state_feature_manager_impl.h
+++ chromeos/services/multidevice_setup/global_state_feature_manager_impl.h
@@ -45,8 +45,6 @@ class GlobalStateFeatureManagerImpl
   class Factory {
    public:
     enum class Option {
-      // Corresponds to |mojom::Feature::kPhoneHubCameraRoll|.
-      kPhoneHubCameraRoll,
       // Corresponds to |mojom::Feature::kWifiSync|.
       kWifiSync
     };
