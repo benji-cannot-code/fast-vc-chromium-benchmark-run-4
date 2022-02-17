5adf123fe9095616c7244a573c80935d31d0237e
Wen-Chien Wang <wcwang@chromium.org>
2022-02-17 18:29:46 +0000

    Syncing states between AppListNudgeController and ContinueSectionView
    In this cl, states between AppListNudgeController and the privacy
    notice in ContinueSectionView are synced to make sure only one nudge
    can be shown at a time.
    Note that prefs updating is still handled in the original files
    respectively. This will be reviewed and combined in the next cl.
    
    Bug: 1277666
    Change-Id: I659ad0b6660a3f4e6af862b887441d173c3f9b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456918
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972542}

--- ash/app_list/views/apps_container_view.cc
+++ ash/app_list/views/apps_container_view.cc
@@ -163,7 +163,6 @@ class AppsContainerView::ContinueContainer : public views::View {
         view_delegate, kContinueColumnCount, /*tablet_mode=*/true));
     continue_section_->SetPaintToLayer();
     continue_section_->layer()->SetFillsBoundsOpaquely(false);
-    continue_section_->UpdateSuggestionTasks();
 
     recent_apps_ = AddChildView(
         std::make_unique<RecentAppsView>(apps_container, view_delegate));
@@ -268,6 +267,12 @@ AppsContainerView::AppsContainerView(ContentsView* contents_view)
   if (features::IsProductivityLauncherEnabled()) {
     continue_container_ = scrollable_container_->AddChildView(
         std::make_unique<ContinueContainer>(this, view_delegate));
+    continue_container_->continue_section()->SetNudgeController(
+        app_list_nudge_controller_.get());
+    // Update the suggestion tasks after the app list nudge controller is set in
+    // continue section.
+    continue_container_->continue_section()->UpdateSuggestionTasks();
+
     // Add a empty container view. A toast view should be added to
     // `toast_container_` when the app list starts temporary sorting.
     if (features::IsLauncherAppSortEnabled()) {
@@ -1009,6 +1014,11 @@ void AppsContainerView::OnShown() {
 
   GetViewAccessibility().OverrideIsLeaf(false);
   is_active_page_ = true;
+
+  // Update the continue section.
+  if (continue_container_)
+    continue_container_->continue_section()->SetShownInBackground(false);
+
   // Updates the visibility state in toast container.
   if (toast_container_) {
     toast_container_->UpdateVisibilityState(
@@ -1031,6 +1041,10 @@ void AppsContainerView::OnHidden() {
   GetViewAccessibility().OverrideIsLeaf(true);
 
   is_active_page_ = false;
+
+  // Update the continue section.
+  if (continue_container_)
+    continue_container_->continue_section()->SetShownInBackground(true);
 
   // Updates the visibility state in toast container.
   if (toast_container_) {
