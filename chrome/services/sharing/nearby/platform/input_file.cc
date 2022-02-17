d07d74ab2ae1855241e4f0382d3da8472ac852c0
Josh Nohle <nohle@google.com>
2022-02-17 18:13:26 +0000

    [Nearby] Replace variable-length array with std::vector
    Verified with "git cl lint".
    
    Change-Id: Ie97df9315ca31e7f322bfe68cd0215d3bb5180b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469358
    Auto-Submit: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#972529}

--- chrome/services/sharing/nearby/platform/input_file.cc
+++ chrome/services/sharing/nearby/platform/input_file.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/services/sharing/nearby/platform/input_file.h"
 
+#include <vector>
+
 #include "base/logging.h"
 
 namespace location {
@@ -35,13 +37,13 @@ ExceptionOr<ByteArray> InputFile::Read(std::int64_t size) {
     return ExceptionOr<ByteArray>(ByteArray());
   }
 
-  char buf[size];
-  int num_bytes_read = file_.ReadAtCurrentPos(buf, size);
+  std::vector<char> buf(size);
+  int num_bytes_read = file_.ReadAtCurrentPos(buf.data(), size);
 
   if (num_bytes_read < 0 || num_bytes_read > GetTotalSize())
     return Exception::kIo;
 
-  return ExceptionOr<ByteArray>(ByteArray(buf, num_bytes_read));
+  return ExceptionOr<ByteArray>(ByteArray(buf.data(), num_bytes_read));
 }
 
 Exception InputFile::Close() {
