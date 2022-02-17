ec297247607a745f1a92058f502e4db9451ef123
Mike Dougherty <michaeldo@chromium.org>
2022-02-17 00:26:51 +0000

    Implement Search Open Tabs Suggested Action
    Additionally, prevent RecentTabsTableViewController from removing and
    re-inserting sections while search is in progress.
    
    Bug: 1290663
    Change-Id: I8d0be113a2bcfbc3715056800dc3514073d08ab3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466418
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972175}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -964,6 +964,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                focusOmnibox:NO];
 }
 
+- (void)showRegularTabGridFromRecentTabs {
+  [self.baseViewController setCurrentPageAndPageControl:TabGridPageRegularTabs
+                                               animated:YES];
+}
+
 #pragma mark - HistoryPresentationDelegate
 
 - (void)showActiveRegularTabFromHistory {
