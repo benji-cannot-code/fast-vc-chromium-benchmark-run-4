c3aa8f055a5842f7217a6f3ae63ec8f54ba731e1
Nan Lin <linnan@chromium.org>
2022-02-17 16:42:16 +0000

    Fenced Frames: CSP check should only match https: and https://*:*
    For opaque fenced frames the URL is opaque to the embedding context,
    therefore the embedding context should not tell whether the actual URL
    it maps to is allowed or not and should only match the scheme.
    
    Note that opaque fenced frames only allow https urls, therefore we will
    only match 'https:' and 'https://*:*'rules. If we do want to support
    non-https potentially trustworthy urls, then we might need to extend the
    https scheme to match all potentially trustworthy urls to avoid privacy
    leak.
    
    This CL introduced a fenced frame mode in FrameTreeNode to indicate
    whether the fenced frame url is opaque(urn:uuid) or transparent. The
    mode is implicitly inferred from the url of the first navigation and
    will stay the same for the lifetime of the fenced frame. A follow-up CL
    will enforce that the embedder will not be able to navigate a fenced
    frame in an opaque mode to a transparent URL, or vice versa. See the
    following docs for the 'mode' attribute to be introduced.
    
    https://docs.google.com/document/d/1HAU9IiHZU4KBPC_rEk3BQYrWTK50PdNGg8CcHhVLXig/edit#heading=h.d4ku3xszbtiv
    
    https://docs.google.com/document/d/1XKBrAINpA6raSR0mMf7_T1RMUyq4JOmNufhUPrs69vA/edit#heading=h.sd3c9yhx0k8
    
    Bug: 1278482
    Change-Id: I968f49322d82b56c3f67a79caf3eccf5e4e82fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3399204
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972480}

--- services/network/public/cpp/content_security_policy/csp_source_list.cc
+++ services/network/public/cpp/content_security_policy/csp_source_list.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/cpp/content_security_policy/csp_source_list.h"
 
+#include "base/check_op.h"
 #include "base/containers/cxx20_erase.h"
 #include "base/containers/flat_set.h"
 #include "base/ranges/algorithm.h"
@@ -21,9 +22,11 @@ namespace {
 bool AllowFromSources(const GURL& url,
                       const std::vector<mojom::CSPSourcePtr>& sources,
                       const mojom::CSPSource& self_source,
-                      bool has_followed_redirect) {
+                      bool has_followed_redirect,
+                      bool is_opaque_fenced_frame) {
   for (const auto& source : sources) {
-    if (CheckCSPSource(*source, url, self_source, has_followed_redirect))
+    if (CheckCSPSource(*source, url, self_source, has_followed_redirect,
+                       is_opaque_fenced_frame))
       return true;
   }
   return false;
@@ -193,7 +196,11 @@ bool CheckCSPSourceList(mojom::CSPDirectiveName directive_name,
                         const GURL& url,
                         const mojom::CSPSource& self_source,
                         bool has_followed_redirect,
-                        bool is_response_check) {
+                        bool is_response_check,
+                        bool is_opaque_fenced_frame) {
+  if (is_opaque_fenced_frame)
+    DCHECK_EQ(directive_name, mojom::CSPDirectiveName::FencedFrameSrc);
+
   // If the source list allows all redirects, the decision can't be made until
   // the response is received.
   if (directive_name == mojom::CSPDirectiveName::NavigateTo &&
@@ -206,6 +213,11 @@ bool CheckCSPSourceList(mojom::CSPDirectiveName directive_name,
   // https://w3c.github.io/webappsec-csp/#match-url-to-source-expression. Other
   // schemes, including custom schemes, must be explicitly listed in a source
   // list.
+  // Note: Opaque fenced frames only allow https urls, therefore it's fine to
+  // allow '*'.
+  // TODO(crbug.com/1243568): Update the return condition below if opaque
+  // fenced frames can map to non-https potentially trustworthy urls to avoid
+  // privacy leak.
   if (source_list.allow_star) {
     if (url.SchemeIsHTTPOrHTTPS() || url.SchemeIsWSOrWSS() ||
         url.SchemeIs("ftp")) {
@@ -216,12 +228,13 @@ bool CheckCSPSourceList(mojom::CSPDirectiveName directive_name,
   }
 
   if (source_list.allow_self &&
-      CheckCSPSource(self_source, url, self_source, has_followed_redirect)) {
+      CheckCSPSource(self_source, url, self_source, has_followed_redirect,
+                     is_opaque_fenced_frame)) {
     return true;
   }
 
   return AllowFromSources(url, source_list.sources, self_source,
-                          has_followed_redirect);
+                          has_followed_redirect, is_opaque_fenced_frame);
 }
 
 bool CSPSourceListSubsumes(
