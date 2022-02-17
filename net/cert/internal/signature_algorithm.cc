89c07f415f8fac8088a285774c7d0043f99a5b97
David Benjamin <davidben@chromium.org>
2022-02-17 01:22:32 +0000

    Histogram non-standard RSA-PSS parameters.
    Matching Go and TLS 1.3, it would good to get to only a small set of
    allowed RSA-PSS parameters. This would allow SignatureAlgorithm to be a
    simple enum. Start by measuring what we see in the wild. Although
    neither Go nor TLS 1.3 (nor our own verifier) allows it, I've split out
    SHA-1 into its own enum just in case it's still reachable somehow.
    
    Bug: 1279975
    Change-Id: Ic04ac500bf08b570d0b1d5ad5544f6ab1b5d444a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469618
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972205}

--- net/cert/internal/signature_algorithm.cc
+++ net/cert/internal/signature_algorithm.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/notreached.h"
 #include "base/numerics/safe_math.h"
 #include "net/cert/internal/cert_error_params.h"
 #include "net/cert/internal/cert_errors.h"
@@ -415,6 +416,36 @@ std::unique_ptr<SignatureAlgorithm> ParseEcdsa(DigestAlgorithm digest,
   return true;
 }
 
+RsaPssClassification ClassifyRsaPssParams(DigestAlgorithm digest,
+                                          DigestAlgorithm mgf1_hash,
+                                          uint32_t salt_length) {
+  if (digest != mgf1_hash) {
+    return RsaPssClassification::kDigestMismatch;
+  }
+  switch (digest) {
+    case DigestAlgorithm::Sha1:
+      return salt_length == 20 ? RsaPssClassification::kSha1
+                               : RsaPssClassification::kSha1NonstandardSalt;
+    case DigestAlgorithm::Sha256:
+      return salt_length == 32 ? RsaPssClassification::kSha256
+                               : RsaPssClassification::kSha256NonstandardSalt;
+    case DigestAlgorithm::Sha384:
+      return salt_length == 48 ? RsaPssClassification::kSha384
+                               : RsaPssClassification::kSha384NonstandardSalt;
+    case DigestAlgorithm::Sha512:
+      return salt_length == 64 ? RsaPssClassification::kSha512
+                               : RsaPssClassification::kSha512NonstandardSalt;
+    case DigestAlgorithm::Md2:
+    case DigestAlgorithm::Md4:
+    case DigestAlgorithm::Md5:
+      // Assuming anything using RSA-PSS long postdates these digests. Note this
+      // is also unreachable because `ParseHashAlgorithm` does not output these.
+      return RsaPssClassification::kLegacyDigest;
+  }
+  NOTREACHED();
+  return RsaPssClassification::kLegacyDigest;
+}
+
 // Parses the parameters for an RSASSA-PSS signature algorithm, as defined by
 // RFC 5912:
 //
@@ -537,6 +568,10 @@ std::unique_ptr<SignatureAlgorithm> ParseRsaPss(const der::Input& params) {
     return nullptr;
 
   UMA_HISTOGRAM_BOOLEAN("Net.CertVerifier.InvalidRsaPssParams", der_error);
+
+  // See https://crbug.com/1279975.
+  UMA_HISTOGRAM_ENUMERATION("Net.CertVerifier.RsaPssClassification",
+                            ClassifyRsaPssParams(hash, mgf1_hash, salt_length));
 
   return SignatureAlgorithm::CreateRsaPss(hash, mgf1_hash, salt_length);
 }
