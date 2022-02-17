c29d54b880d0e5a54b72d6712d676f15bfc40550
Francois Doray <fdoray@chromium.org>
2022-02-17 23:47:48 +0000

    [battery] Remove the display brightness histogram and UKM field.
    Display brightness does affect battery discharge, but we're not doing
    anything with the data for now. Therefore, I'm removing the histogram
    and UKM field to reduce the amount of unnecessary work done by Chrome.
    
    Change-Id: I4bdeb1508e7bb36f5763275eba5135b031257516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3431905
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972716}

--- chrome/browser/metrics/power/power_metrics_reporter.h
+++ chrome/browser/metrics/power/power_metrics_reporter.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/metrics/power/battery_level_provider.h"
-#include "chrome/browser/metrics/power/power_details_provider.h"
 #include "chrome/browser/metrics/usage_scenario/usage_scenario_data_store.h"
 #include "chrome/browser/metrics/usage_scenario/usage_scenario_tracker.h"
 #include "chrome/browser/performance_monitor/process_monitor.h"
@@ -40,6 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // This class should be created shortly after
 // performance_monitor::ProcessMonitor.
+//
+// Previous histograms reported by this class:
+// - Main screen brightness, removed by https://crrev.com/c/3431905
+// Historical data: go/chrome-historical-power-histograms
 class PowerMetricsReporter
     : public performance_monitor::ProcessMonitor::Observer {
  public:
@@ -87,11 +90,6 @@ class PowerMetricsReporter
   static int64_t GetBucketForSampleForTesting(base::TimeDelta value);
   static std::vector<const char*> GetLongIntervalSuffixesForTesting(
       const UsageScenarioDataStore::IntervalData& interval_data);
-
-  void set_power_details_provider_for_testing(
-      std::unique_ptr<PowerDetailsProvider> provider) {
-    power_details_provider_ = std::move(provider);
-  }
 
  protected:
   // Any change to this enum should be reflected in the corresponding enums.xml
@@ -166,8 +164,7 @@ class PowerMetricsReporter
   void ReportUKMs(const UsageScenarioDataStore::IntervalData& interval_data,
                   const ProcessMonitor::Metrics& aggregated_process_metrics,
                   base::TimeDelta interval_duration,
-                  BatteryDischarge battery_discharge,
-                  absl::optional<int64_t> main_screen_brightness) const;
+                  BatteryDischarge battery_discharge) const;
 
   // Computes and returns the battery discharge mode and rate during the
   // interval, and reset |battery_state_| to the current state. If the discharge
@@ -194,8 +191,6 @@ class PowerMetricsReporter
   raw_ptr<UsageScenarioDataStore> long_usage_scenario_data_store_;
 
   std::unique_ptr<BatteryLevelProvider> battery_level_provider_;
-
-  std::unique_ptr<PowerDetailsProvider> power_details_provider_;
 
   BatteryLevelProvider::BatteryState battery_state_{0, 0, absl::nullopt, false,
                                                     base::TimeTicks::Now()};
