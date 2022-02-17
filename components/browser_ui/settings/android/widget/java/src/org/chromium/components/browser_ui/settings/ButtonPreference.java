9b834f8685c83db31ebf5430aa497a53caf5067f
Christian Dullweber <dullweber@chromium.org>
2022-02-17 15:41:24 +0000

    Privacy Sandbox Settings: Add topics and click handler to pageinfo
    Display topic in pageinfo. Add click handler and update button style.
    
    Screenshot: https://crbug.com/1286276#c78
    Bug: 1286276
    Change-Id: I298e057a5f1340b3a70a7e88742ac322472367b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468499
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Ehimare Okoyomon <eokoyomon@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972451}

--- components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ButtonPreference.java
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ButtonPreference.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.settings;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.Button;
 
@@ -25,7 +26,11 @@ public class ButtonPreference extends Preference {
     public ButtonPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         setLayoutResource(R.layout.button_preference_layout);
-        setWidgetLayoutResource(R.layout.button_preference_button);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ButtonPreference);
+        setWidgetLayoutResource(a.getResourceId(
+                R.styleable.ButtonPreference_buttonLayout, R.layout.button_preference_button));
+        a.recycle();
 
         // Only the inner button element should be focusable.
         setSelectable(false);
