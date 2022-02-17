162d833ea6b48ffa525cd9408dad65226f1acbb1
Austin Eng <enga@chromium.org>
2022-02-17 16:41:10 +0000

    WebGPU CTS: Add script to expand CTS query-based expectations
    Expanding queries into lists of full expectations so that they can be
    compatible with the Telemetry harness.
    
    Example usage:
    ./third_party/webgpu-cts/scripts/generate_telemetry_expectations.py
      content/test/gpu/gpu_tests/test_expectations/webgpu_cts_expectation_queries.js
    
    --js-out-dir may be passed to cache the compiled Typescript results
    --expectations-out may be passed to write expectations to a file instead of stdout
    
    Bug: 1297379
    Change-Id: I597eb8e30a137c9a80e4770f1bb905e98665465a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453929
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972479}

---
+++ content/test/gpu/gpu_tests/test_expectations/webgpu_cts_expectation_queries.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// So that expecations below have lighter syntax without quotes.
+const Mac = 'Mac';
+const Win = 'Win';
+const Failure = 'Failure';
+
+const expectations = [
+  // Example:
+  // {
+  //   b: 'crbug.com/dawn/1304812342',
+  //   t: [Mac],
+  //   q: 'webgpu:api,operation,command_buffer,copyTextureToTexture:*',
+  //   e: [Failure]
+  // },
+  // {
+  //   b: 'crbug.com/dawn/9839583',
+  //   t: [Win],
+  //   q: 'webgpu:api,validation,resource_usages,texture,in_pass_encoder:unused_bindings_in_pipeline:*',
+  //   e: [Failure]
+  // }
+];
+
+module.exports = { expectations };
