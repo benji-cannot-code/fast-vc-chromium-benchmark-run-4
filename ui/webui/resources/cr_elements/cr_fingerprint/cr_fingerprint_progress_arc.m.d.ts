b8f3e788ff432bcaf43420654a23e592afc3e2a5
Colin Kincaid <ckincaid@chromium.org>
2022-02-17 16:43:37 +0000

    Fix fingerprint enrollment animations for dark mode (Part 1 of N)
    This CL scaffolds future dark mode fixes for the fingerprint enrollment
    workflow by replacing the current MediaQueryList approach with an
    iron-media-query. This change is a pure refactor that makes future fixes
    easier by introducing the onDarkModeChanged_() observer function.
    
    Forthcoming CLs will fix fingerprint enrollment onboarding animations,
    progress circle painting, progress fingerprint animation, and enrollment
    done animation.
    
    Bug: 1295944
    Change-Id: I4118c93649c22cf95d917237e3f6eb7136c9273f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466634
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Colin Kincaid <ckincaid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972481}

--- ui/webui/resources/cr_elements/cr_fingerprint/cr_fingerprint_progress_arc.m.d.ts
+++ ui/webui/resources/cr_elements/cr_fingerprint/cr_fingerprint_progress_arc.m.d.ts
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {CrLottieElement} from 'chrome://resources/cr_elements/cr_lottie/cr_lottie.m.js';
 import {LegacyElementMixin} from 'chrome://resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
 
-export const FINGEPRINT_TICK_DARK_URL: string;
-export const FINGEPRINT_TICK_LIGHT_URL: string;
+export const FINGERPRINT_TICK_DARK_URL: string;
+export const FINGERPRINT_TICK_LIGHT_URL: string;
 
 interface CrFingerprintProgressArcElement extends LegacyElementMixin,
                                                   HTMLElement {
