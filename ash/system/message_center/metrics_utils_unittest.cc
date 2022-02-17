598fad30c28a48e02df2afea7d21c10672bbe3a7
amehfooz <amehfooz@chromium.org>
2022-02-17 22:19:01 +0000

    Fix system notifications with notification grouping
    This fix makes sure we only group web notifications.
    Also we want to make sure notification updates are checked for
    grouping.
    
    Bug: 1295344
    Change-Id: I0fae3ee6258e3ffc5f9f945d9820e221f48cafd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465783
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972670}

--- ash/system/message_center/metrics_utils_unittest.cc
+++ ash/system/message_center/metrics_utils_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
+#include "ui/message_center/public/cpp/notifier_id.h"
 #include "ui/views/widget/widget_utils.h"
 
 using message_center::Notification;
@@ -76,6 +77,7 @@ class MessageCenterMetricsUtilsTest : public AshTestBase {
         message_center::SettingsButtonHandler::INLINE;
     message_center::NotifierId notifier_id;
     notifier_id.profile_id = "a@b.com";
+    notifier_id.type = message_center::NotifierType::WEB_PAGE;
     return std::make_unique<Notification>(
         message_center::NOTIFICATION_TYPE_BASE_FORMAT,
         base::NumberToString(current_id_++), u"title", u"message", gfx::Image(),
