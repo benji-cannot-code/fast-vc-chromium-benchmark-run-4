342362c64d8f932b517ae4d1a17de6dc9a07fef8
Alex Turner <alexmt@chromium.org>
2022-02-17 17:13:03 +0000

    Support 128-bit buckets in aggregatable reports
    Updates implementation to match explainer. A bytestring (in big-endian/
    network byte order) is used to encode the 128-bit integer in CBOR. Due
    to the use of an unsigned integer, the code enforcing against negative
    buckets is removed.
    
    Bug: 1293930
    Change-Id: Ibe3abc5f485846f64df773921e8bb8204023ed25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469340
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972497}

--- content/browser/aggregation_service/aggregatable_report.h
+++ content/browser/aggregation_service/aggregatable_report.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "content/browser/aggregation_service/public_key.h"
 #include "content/common/content_export.h"
+#include "third_party/abseil-cpp/absl/numeric/int128.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/origin.h"
 
@@ -43,12 +44,12 @@ struct CONTENT_EXPORT AggregationServicePayloadContents {
   };
 
   AggregationServicePayloadContents(Operation operation,
-                                    int bucket,
+                                    absl::uint128 bucket,
                                     int value,
                                     ProcessingType processing_type);
 
   Operation operation;
-  int bucket;
+  absl::uint128 bucket;
   int value;
   ProcessingType processing_type;
 };
@@ -105,15 +106,18 @@ class CONTENT_EXPORT AggregatableReport {
 
     // This payload is constructed using the data in the
     // AggregationServicePayloadContents and then encrypted with one of
-    // `origin`'s public keys. For the kTwoParty processing type, the plaintext
-    // of the encrypted payload is a serialized CBOR map structured as follows:
+    // `origin`'s public keys. For the `kSingleServer` processing type, the
+    // plaintext of the encrypted payload is a serialized CBOR map structured as
+    // follows:
     // {
     //   "operation": "<chosen operation as string>",
-    //   "dpf_key": <binary serialization of the DPF key>,
+    //   "data": [{
+    //     "bucket": <128-bit integer encoded as a big-endian bytestring>,
+    //     "value": <integer>,
+    //   }],
     // }
-    // For the kSingleServer processing type, the "dpf_key" field is replaced
-    // with:
-    //   "data": [{ "bucket": <bucket>, "value": <value> }]
+    // For the `kTwoParty` processing type, the "data" field is replaced with:
+    //   "dpf_key": <binary serialization of the DPF key>
     std::vector<uint8_t> payload;
 
     // Indicates the chosen encryption key.
@@ -215,18 +219,17 @@ class CONTENT_EXPORT AggregatableReport {
 // processing origin.
 class CONTENT_EXPORT AggregatableReportRequest {
  public:
-  // Returns `absl::nullopt` if `payload_contents` has a negative bucket or
-  // value. Also returns `absl::nullopt` if `shared_info.report_id` is not
-  // valid.
+  // Returns `absl::nullopt` if `payload_contents` has a negative value. Also
+  // returns `absl::nullopt` if `shared_info.report_id` is not valid.
   static absl::optional<AggregatableReportRequest> Create(
       AggregationServicePayloadContents payload_contents,
       AggregatableReportSharedInfo shared_info);
 
-  // Returns `absl::nullopt` if `payload_contents` has a negative bucket or
-  // value. Also returns `absl::nullopt` if `processing_origins.size()` is not
-  // valid for the `payload_contents.processing_type` (see
-  // `IsNumberOfProcessingOriginsValid` above). Also returns `absl::nullopt` if
-  // `shared_info.report_id` is not valid.
+  // Returns `absl::nullopt` if `payload_contents` has a negative value. Also
+  // returns `absl::nullopt` if `processing_origins.size()` is not valid for the
+  // `payload_contents.processing_type` (see `IsNumberOfProcessingOriginsValid`
+  // above). Also returns `absl::nullopt` if `shared_info.report_id` is not
+  // valid.
   static absl::optional<AggregatableReportRequest> CreateForTesting(
       std::vector<url::Origin> processing_origins,
       AggregationServicePayloadContents payload_contents,
