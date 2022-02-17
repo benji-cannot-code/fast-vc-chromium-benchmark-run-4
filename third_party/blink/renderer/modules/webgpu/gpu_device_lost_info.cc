e5094bb7bd2e504c578d52e9f3d9bd536155a837
Robert Kroeger <rjkroege@chromium.org>
2022-02-17 17:38:20 +0000

    Revert "Change GPUDeviceLostInfo.reason to any"
    This reverts commit 64eef749cabe22b649f27cbebc02a3c594e54c10.
    
    Reason for revert: speculative fix for https://crbug.com/1298543
    
    Original change's description:
    > Change GPUDeviceLostInfo.reason to any
    >
    > This CL changes the type of GPUDeviceLostInfo.label in IDL to follow the
    > latest WebGPU spec. Ideally the type should be
    > (GPUDeviceLostReason or undefined) for the consistency with the spec but
    > currently the bindings somehow can't handle it although it should be
    > vaild. Until the bindings supports it we set the type as any and do
    > custom handling in Blink code as a workaround.
    >
    > Bug: 1290729
    > Change-Id: I298738fd502c2aab6bcaf6cbb2cc66a023181e60
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3455182
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Reviewed-by: Austin Eng <enga@chromium.org>
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#972240}
    
    Bug: 1290729
    Change-Id: Ic99ec7cf1971b432aee90965a618922408342835
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3471954
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Robert Kroeger <rjkroege@google.com>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972517}

--- third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.h"
-#include "third_party/blink/renderer/bindings/core/v8/to_v8_for_core.h"
 
 namespace blink {
 
@@ -26,12 +25,8 @@ const String& GPUDeviceLostInfo::message() const {
   return message_;
 }
 
-const ScriptValue GPUDeviceLostInfo::reason(ScriptState* script_state) const {
-  if (reason_.IsNull()) {
-    v8::Isolate* isolate = script_state->GetIsolate();
-    return ScriptValue(isolate, v8::Undefined(isolate));
-  }
-  return ScriptValue::From(script_state, reason_);
+const String& GPUDeviceLostInfo::reason() const {
+  return reason_;
 }
 
 }  // namespace blink
--- third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.h
+++ third_party/blink/renderer/modules/webgpu/gpu_device_lost_info.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <dawn/webgpu.h>
 
-#include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -26,7 +25,7 @@ class GPUDeviceLostInfo : public ScriptWrappable {
 
   // gpu_device_lost_info.idl
   const String& message() const;
-  const ScriptValue reason(ScriptState* script_state) const;
+  const String& reason() const;
 
  private:
   String message_;
