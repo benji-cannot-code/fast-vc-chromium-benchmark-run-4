98cd23dda39317523da529a73204ba2c4c2d35c8
Kevin Radtke <kradtke@chromium.org>
2022-02-17 01:20:31 +0000

    Change histogram macros call to histogram functions
    Use histogram_functions instead of macros based on
    https://chromium.googlesource.com/chromium/src/+/HEAD/tools/metrics/histograms/README.md#coding-emitting-to-histograms.
    
    Change-Id: I7e8e14c28f75e316abc4803700c13ace4f5fbd3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3470375
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972202}

--- ash/system/time/calendar_view_controller.cc
+++ ash/system/time/calendar_view_controller.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -61,8 +60,8 @@ CalendarViewController::~CalendarViewController() {
   if (user_journey_time_recorded_)
     return;
 
-  UMA_HISTOGRAM_MEDIUM_TIMES("Ash.Calendar.UserJourneyTime.EventNotLaunched",
-                             base::TimeTicks::Now() - calendar_open_time_);
+  UmaHistogramMediumTimes("Ash.Calendar.UserJourneyTime.EventNotLaunched",
+                          base::TimeTicks::Now() - calendar_open_time_);
 }
 
 void CalendarViewController::AddObserver(Observer* observer) {
@@ -237,8 +236,8 @@ void CalendarViewController::OnEventListClosed() {
 }
 
 void CalendarViewController::OnCalendarEventWillLaunch() {
-  UMA_HISTOGRAM_MEDIUM_TIMES("Ash.Calendar.UserJourneyTime.EventLaunched",
-                             base::TimeTicks::Now() - calendar_open_time_);
+  UmaHistogramMediumTimes("Ash.Calendar.UserJourneyTime.EventLaunched",
+                          base::TimeTicks::Now() - calendar_open_time_);
   user_journey_time_recorded_ = true;
 }
 
