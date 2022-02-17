d40cd1adb15038a1aa3a790e020783238ee8ee8a
Fritz Koenig <frkoenig@chromium.org>
2022-02-17 02:14:29 +0000

    media/gpu/v4l2: Remove unnecessary and outdated comments
    Commit 9a0342b added support for memory mapped output
    frames, but never updated the comment.
    
    Bug: None
    Change-Id: I45a185d19c25e960ceba749b947b3066f56f2e97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468974
    Auto-Submit: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972223}

--- media/gpu/v4l2/v4l2_image_processor_backend.cc
+++ media/gpu/v4l2/v4l2_image_processor_backend.cc
@@ -275,8 +275,6 @@ V4L2ImageProcessorBackend::CreateWithOutputMode(
     return nullptr;
   }
 
-  // V4L2ImageProcessorBackend supports either DmaBuf-backed or memory-based
-  // video frame for input.
   VideoFrame::StorageType input_storage_type = VideoFrame::STORAGE_UNKNOWN;
   for (auto input_type : input_config.preferred_storage_types) {
     v4l2_memory v4l2_memory_type = InputStorageTypeToV4L2Memory(input_type);
@@ -291,8 +289,6 @@ V4L2ImageProcessorBackend::CreateWithOutputMode(
     return nullptr;
   }
 
-  // V4L2ImageProcessorBackend only supports DmaBuf-backed video frame for
-  // output.
   VideoFrame::StorageType output_storage_type = VideoFrame::STORAGE_UNKNOWN;
   for (auto output_type : output_config.preferred_storage_types) {
     v4l2_memory v4l2_memory_type = InputStorageTypeToV4L2Memory(output_type);
