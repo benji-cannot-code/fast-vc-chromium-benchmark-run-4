f5bcd133bd8a97f0ccc4265d0f7abab7a5017f77
François Beaufort <beaufort.francois@gmail.com>
2022-02-17 08:30:39 +0000

    Record the most likely cost for the default network connection.
    This CL adds some UMA to know how common metered connections are. This
    UMA is only recorded on Windows for now. If Windows version is older
    than Windows 10, CONNECTION_COST_UNKNOWN is returned.
    
    Change-Id: Id5e00494b7ffcf5494ba12d54a2d6b70cbf3b2d8
    Bug: 1297055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3460867
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#972338}

--- net/base/network_change_notifier.cc
+++ net/base/network_change_notifier.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/cxx17_backports.h"
 #include "base/memory/ref_counted.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/sequence_checker.h"
@@ -981,8 +982,11 @@ void NetworkChangeNotifier::NotifyObserversOfSpecificNetworkChange(
 void NetworkChangeNotifier::NotifyObserversOfConnectionCostChange() {
   if (g_network_change_notifier &&
       !NetworkChangeNotifier::test_notifications_only_) {
+    NetworkChangeNotifier::ConnectionCost connection_cost = GetConnectionCost();
+    base::UmaHistogramEnumeration("Net.NetworkChangeNotifier.GetConnectionCost",
+                                  connection_cost, CONNECTION_COST_LAST);
     g_network_change_notifier->NotifyObserversOfConnectionCostChangeImpl(
-        GetConnectionCost());
+        connection_cost);
   }
 }
 
