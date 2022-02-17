c2aa556f602bfac22939feb86d48d6d138e46787
Richard Chui <richui@chromium.org>
2022-02-17 22:27:22 +0000

    Desk Templates: Add bounds animations for template items
    Currently whenever we add/remove/update template items, their bounds are
    set immediately. This CL uses a `BoundsAnimator` to animate each of the
    template items so that they translate between their original to their
    final positions.
    
    Bug: 1257322
    Test: manual, updated to wait on animations
    Change-Id: I098dee71954a78f2d857e33963da45faf1ea0352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469143
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972677}

--- ash/wm/desks/templates/desks_templates_test_util.cc
+++ ash/wm/desks/templates/desks_templates_test_util.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/zero_state_button.h"
 #include "ash/wm/overview/overview_grid.h"
 #include "ash/wm/overview/overview_test_util.h"
+#include "ui/views/animation/bounds_animator.h"
+#include "ui/views/animation/bounds_animator_observer.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/window/dialog_delegate.h"
 
@@ -31,6 +33,40 @@ OverviewGrid* GetPrimaryOverviewGrid() {
                           : nullptr;
 }
 
+// `BoundsAnimatorWaiter` observes the `BoundsAnimator` and waits for the
+// template grid items animations to finish.
+class BoundsAnimatorWaiter : public views::BoundsAnimatorObserver {
+ public:
+  explicit BoundsAnimatorWaiter(views::BoundsAnimator& animator)
+      : animator_(animator) {
+    animator_.AddObserver(this);
+  }
+
+  BoundsAnimatorWaiter(const BoundsAnimatorWaiter&) = delete;
+  BoundsAnimatorWaiter& operator=(const BoundsAnimatorWaiter&) = delete;
+
+  ~BoundsAnimatorWaiter() override { animator_.RemoveObserver(this); }
+
+  void Wait() {
+    if (!animator_.IsAnimating())
+      return;
+
+    run_loop_ = std::make_unique<base::RunLoop>();
+    run_loop_->Run();
+  }
+
+ private:
+  // views::BoundsAnimatorObserver:
+  void OnBoundsAnimatorProgressed(views::BoundsAnimator* animator) override {}
+  void OnBoundsAnimatorDone(views::BoundsAnimator* animator) override {
+    if (run_loop_)
+      run_loop_->Quit();
+  }
+
+  views::BoundsAnimator& animator_;
+  std::unique_ptr<base::RunLoop> run_loop_;
+};
+
 }  // namespace
 
 DesksTemplatesPresenterTestApi::DesksTemplatesPresenterTestApi(
@@ -45,6 +81,18 @@ void DesksTemplatesPresenterTestApi::SetOnUpdateUiClosure(
     base::OnceClosure closure) {
   DCHECK(!presenter_->on_update_ui_closure_for_testing_);
   presenter_->on_update_ui_closure_for_testing_ = std::move(closure);
+}
+
+DesksTemplatesGridViewTestApi::DesksTemplatesGridViewTestApi(
+    DesksTemplatesGridView* grid_view)
+    : grid_view_(grid_view) {
+  DCHECK(grid_view_);
+}
+
+DesksTemplatesGridViewTestApi::~DesksTemplatesGridViewTestApi() = default;
+
+void DesksTemplatesGridViewTestApi::WaitForItemMoveAnimationDone() {
+  BoundsAnimatorWaiter(grid_view_->bounds_animator_).Wait();
 }
 
 DesksTemplatesItemViewTestApi::DesksTemplatesItemViewTestApi(
