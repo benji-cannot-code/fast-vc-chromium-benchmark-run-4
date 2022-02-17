a47b7e7577b15bd1f032b4cc2242024f31f56cb8
Ian Kilpatrick <ikilpatrick@chromium.org>
2022-02-17 01:27:14 +0000

    [GridFragmentation] Propagate break-before/break-after from container.
    ...when unfragmented). This patch captures the break-before/break-after
    values for grid row(s) when unfragmented. We pass in a break-between
    vector into |PlaceItems| which will then use this to set the
    break-before/break-after on the fragment.
    
    We also use these values to add the correct break-before/break-after
    on the grid container.
    
    Bug: 614667
    Change-Id: Ie529efe0e5bfe53262d10d874149ffc5c6e67100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3453286
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972206}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_layout_algorithm.cc
@@ -379,12 +379,12 @@ const NGLayoutResult* NGGridLayoutAlgorithm::LayoutInternal() {
         &intrinsic_block_size, &column_track_collection, &row_track_collection);
   }
 
+  Vector<EBreakBetween> row_break_between;
   if (UNLIKELY(InvolvedInBlockFragmentation(container_builder_))) {
     // Either retrieve all items offsets, or generate them using the
     // non-fragmented |PlaceGridItems| pass.
     Vector<GridItemOffsets> offsets;
     Vector<LayoutUnit> row_offset_adjustments;
-    Vector<EBreakBetween> row_break_between;
     if (IsResumingLayout(BreakToken())) {
       const auto* grid_data =
           To<NGGridBreakTokenData>(BreakToken()->TokenData());
@@ -394,9 +394,7 @@ const NGLayoutResult* NGGridLayoutAlgorithm::LayoutInternal() {
     } else {
       row_offset_adjustments =
           Vector<LayoutUnit>(grid_geometry.row_geometry.sets.size());
-      row_break_between = Vector<EBreakBetween>(
-          grid_geometry.row_geometry.sets.size(), EBreakBetween::kAuto);
-      PlaceGridItems(grid_items, grid_geometry, &offsets, &row_break_between);
+      PlaceGridItems(grid_items, grid_geometry, &row_break_between, &offsets);
     }
 
     PlaceGridItemsForFragmentation(
@@ -407,7 +405,7 @@ const NGLayoutResult* NGGridLayoutAlgorithm::LayoutInternal() {
         container_builder_.GetBreakTokenData(), grid_geometry, offsets,
         row_offset_adjustments, row_break_between, intrinsic_block_size));
   } else {
-    PlaceGridItems(grid_items, grid_geometry);
+    PlaceGridItems(grid_items, grid_geometry, &row_break_between);
   }
 
   auto layout_data = std::make_unique<NGGridLayoutData>(BundleLayoutData(
@@ -468,6 +466,11 @@ const NGLayoutResult* NGGridLayoutAlgorithm::LayoutInternal() {
     // fragmentation related fields should have been set.
     container_builder_.CheckNoBlockFragmentation();
 #endif
+  }
+  // Set our break-before/break-after.
+  if (constraint_space.ShouldPropagateChildBreakValues()) {
+    container_builder_.SetInitialBreakBefore(row_break_between.front());
+    container_builder_.SetPreviousBreakAfter(row_break_between.back());
   }
 
   PlaceOutOfFlowItems(*layout_data, block_size);
@@ -3037,11 +3040,19 @@ class BaselineAccumulator {
 void NGGridLayoutAlgorithm::PlaceGridItems(
     const GridItems& grid_items,
     const NGGridGeometry& grid_geometry,
-    Vector<GridItemOffsets>* out_offsets,
-    Vector<EBreakBetween>* out_row_break_between) {
+    Vector<EBreakBetween>* out_row_break_between,
+    Vector<GridItemOffsets>* out_offsets) {
+  DCHECK(out_row_break_between);
   const auto& container_space = ConstraintSpace();
   const auto container_writing_direction =
       container_space.GetWritingDirection();
+  bool should_propagate_child_break_values =
+      ConstraintSpace().ShouldPropagateChildBreakValues();
+
+  if (should_propagate_child_break_values) {
+    *out_row_break_between = Vector<EBreakBetween>(
+        grid_geometry.row_geometry.sets.size(), EBreakBetween::kAuto);
+  }
 
   BaselineAccumulator baseline_accumulator;
 
@@ -3119,7 +3130,7 @@ void NGGridLayoutAlgorithm::PlaceGridItems(
                                       containing_grid_area.offset.block_offset);
     }
 
-    if (out_row_break_between) {
+    if (should_propagate_child_break_values) {
       auto item_break_before = JoinFragmentainerBreakValues(
           item_style.BreakBefore(), result->InitialBreakBefore());
       auto item_break_after = JoinFragmentainerBreakValues(
