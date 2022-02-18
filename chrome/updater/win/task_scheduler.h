2ea9f20d2e2a1682b128c5a3615a739ae7a05096
S. Ganesh <ganesh@chromium.org>
2022-02-18 01:38:56 +0000

    More logging for the task scheduler
    Bug: 1295399
    Change-Id: Ia9f403a1e8a7152fe4d1e84d11ea6c99969d488f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472545
    Auto-Submit: S. Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972764}

--- chrome/updater/win/task_scheduler.h
+++ chrome/updater/win/task_scheduler.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_path.h"
+#include "base/strings/strcat.h"
+#include "base/strings/stringprintf.h"
 
 namespace base {
 class CommandLine;
@@ -61,6 +63,12 @@ class TaskScheduler {
     base::FilePath application_path;
     base::FilePath working_dir;
     std::wstring arguments;
+
+    std::wstring value() const {
+      return base::StrCat({L"[TaskExecAction][application_path]",
+                           application_path.value(), L"[working_dir]",
+                           working_dir.value(), L"[arguments]", arguments});
+    }
   };
 
   // Detailed description of a scheduled task. This type is returned by the
@@ -86,6 +94,22 @@ class TaskScheduler {
     // The log-on requirements for the task's actions to be run. A bit mask with
     // the mapping defined by LogonType.
     uint32_t logon_type = 0;
+
+    // User ID under which the task runs.
+    std::wstring user_id;
+
+    std::wstring value() const {
+      std::wstring value =
+          base::StrCat({L"[TaskInfo][name]", name, L"[description]",
+                        description, L"[exec_actions]"});
+      for (auto exec_action : exec_actions)
+        value += base::StrCat({L"[exec_action]", exec_action.value()});
+
+      value += base::StrCat({L"[logon_type]",
+                             base::StringPrintf(L"0x%x", logon_type),
+                             L"[user_id]", user_id});
+      return value;
+    }
   };
 
   static std::unique_ptr<TaskScheduler> CreateInstance();
