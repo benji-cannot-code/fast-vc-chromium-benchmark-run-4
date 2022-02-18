cd0c76377127a75f85448a63e6448dbe180b9212
dpapad <dpapad@chromium.org>
2022-02-18 05:56:11 +0000

    CrOS: Migrate chrome://slow to JS Modules.
    Bug: 1173575
    Change-Id: I380769cf8bb084a79764709e750c3c49dd14eb59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3471256
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972828}

--- chrome/browser/ui/webui/chromeos/slow_ui.cc
+++ chrome/browser/ui/webui/chromeos/slow_ui.cc
@@ -34,9 +34,6 @@ const char kJsApiDisableTracing[] = "disableTracing";
 const char kJsApiEnableTracing[] = "enableTracing";
 const char kJsApiLoadComplete[] = "loadComplete";
 
-// Page JS API function names.
-const char kJsApiTracingPrefChanged[] = "options.Slow.tracingPrefChanged";
-
 }  // namespace
 
 namespace chromeos {
@@ -71,6 +68,8 @@ class SlowHandler : public WebUIMessageHandler {
 
   // WebUIMessageHandler implementation.
   void RegisterMessages() override;
+  void OnJavascriptAllowed() override;
+  void OnJavascriptDisallowed() override;
 
  private:
   void UpdatePage();
@@ -87,6 +86,8 @@ class SlowHandler : public WebUIMessageHandler {
 // SlowHandler ------------------------------------------------------------
 
 SlowHandler::SlowHandler(Profile* profile) : profile_(profile) {
+  user_pref_registrar_ = std::make_unique<PrefChangeRegistrar>();
+  user_pref_registrar_->Init(profile_->GetPrefs());
 }
 
 SlowHandler::~SlowHandler() {
@@ -102,12 +103,16 @@ void SlowHandler::RegisterMessages() {
   web_ui()->RegisterDeprecatedMessageCallback(
       kJsApiLoadComplete,
       base::BindRepeating(&SlowHandler::LoadComplete, base::Unretained(this)));
+}
 
-  user_pref_registrar_ = std::make_unique<PrefChangeRegistrar>();
-  user_pref_registrar_->Init(profile_->GetPrefs());
+void SlowHandler::OnJavascriptAllowed() {
   user_pref_registrar_->Add(
       prefs::kPerformanceTracingEnabled,
       base::BindRepeating(&SlowHandler::UpdatePage, base::Unretained(this)));
+}
+
+void SlowHandler::OnJavascriptDisallowed() {
+  user_pref_registrar_->RemoveAll();
 }
 
 void SlowHandler::HandleDisable(const base::ListValue* args) {
@@ -121,6 +126,7 @@ void SlowHandler::HandleEnable(const base::ListValue* args) {
 }
 
 void SlowHandler::LoadComplete(const base::ListValue* args) {
+  AllowJavascript();
   UpdatePage();
 }
 
@@ -128,7 +134,7 @@ void SlowHandler::UpdatePage() {
   PrefService* pref_service = profile_->GetPrefs();
   bool enabled = pref_service->GetBoolean(prefs::kPerformanceTracingEnabled);
   base::Value pref_value(enabled);
-  web_ui()->CallJavascriptFunctionUnsafe(kJsApiTracingPrefChanged, pref_value);
+  FireWebUIListener("tracing-pref-changed", pref_value);
 }
 
 // SlowUI -----------------------------------------------------------------
