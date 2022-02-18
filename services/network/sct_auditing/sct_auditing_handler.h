8231594b077b7259670b460bda93f1146b052c54
Eugene Zemtsov <eugene@chromium.org>
2022-02-18 02:25:16 +0000

    Revert "[CT] Hashdance: implement lookup query"
    This reverts commit 293642f97776b185e55a5ff5a910239952fb1cf6.
    
    Reason for revert: annotation bot failing crbug.com/1298742
    
    Original change's description:
    > [CT] Hashdance: implement lookup query
    >
    > Implement the hashdance lookup query, parsing the results and making a
    > report decision based on them.
    >
    > Design doc:
    > https://docs.google.com/document/d/16G-Q7iN3kB46GSW5b-sfH5MO3nKSYyEb77YsM7TMZGE
    >
    > Bug: 1249925
    > Change-Id: Iea1d2ebfcc90ec227207a4f52c59b0fe2cfb4443
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456912
    > Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Adam Langley <agl@chromium.org>
    > Reviewed-by: Chris Thompson <cthomp@chromium.org>
    > Commit-Queue: Chris Thompson <cthomp@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#972718}
    
    Bug: 1249925, 1298742
    Change-Id: I75922eba6e08759801e6194e9fdc355a38c382c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473724
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Eugene Zemtsov <ezemtsov@google.com>
    Owners-Override: Eugene Zemtsov <ezemtsov@google.com>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972784}

--- services/network/sct_auditing/sct_auditing_handler.h
+++ services/network/sct_auditing/sct_auditing_handler.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_context.mojom-shared.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "services/network/sct_auditing/sct_auditing_cache.h"
-#include "services/network/sct_auditing/sct_auditing_reporter.h"
 #include "url/gurl.h"
 
 namespace sct_auditing {
@@ -30,6 +29,7 @@ class SCTClientReport;
 namespace network {
 
 class NetworkContext;
+class SCTAuditingReporter;
 
 // SCTAuditingHandler owns SCT auditing reports for a specific NetworkContext.
 // Each SCTAuditingHandler is owned by its matching NetworkContext. The
@@ -90,11 +90,10 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) SCTAuditingHandler
   // Merkle tree leaf hash of a randomly selected SCT.
   // Optionally takes in a BackoffEntry for recreating reporter state from
   // persisted storage.
-  void AddReporter(
-      net::HashValue reporter_key,
-      std::unique_ptr<sct_auditing::SCTClientReport> report,
-      absl::optional<SCTAuditingReporter::SCTHashdanceMetadata> sct_metadata,
-      std::unique_ptr<net::BackoffEntry> backoff_entry = nullptr);
+  void AddReporter(net::HashValue reporter_key,
+                   std::unique_ptr<sct_auditing::SCTClientReport> report,
+                   absl::optional<std::string> leaf_hash,
+                   std::unique_ptr<net::BackoffEntry> backoff_entry = nullptr);
 
   // Loads serialized reports from `serialized` and creates a new
   // SCTAuditingReporter for each (if a reporter for that report does not yet
