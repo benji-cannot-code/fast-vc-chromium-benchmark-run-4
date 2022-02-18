1a6d5d6a0b679a0917cff4131dba336347228355
Johannes Kron <kron@chromium.org>
2022-02-18 21:15:24 +0000

    Reland "Query WebrtcVideoPerfHistory for MediaCapabilities smoothness score"
    This reverts commit bb956058580e619cf9b31160d2d24cb6ce197920.
    
    Reason for revert: Fixed unit tests by removing static_cast.
    
    Original change's description:
    > Revert "Query WebrtcVideoPerfHistory for MediaCapabilities smoothness score"
    >
    > This reverts commit 9651b5121a3d173b95ae23790217f16420fa1cf5.
    >
    > Reason for revert:  Step "extensions_unittests on Ubuntu-18.04" failing on builder "linux-ubsan-vptr" crbug.com/1298732
    >
    > Original change's description:
    > > Query WebrtcVideoPerfHistory for MediaCapabilities smoothness score
    > >
    > > Bug: chromium:1187565
    > > Change-Id: Icbdf47552a6637d098a3808fbe50d9439e587c5c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3465276
    > > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > > Reviewed-by: Will Cassella <cassew@chromium.org>
    > > Commit-Queue: Johannes Kron <kron@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#972548}
    >
    > Bug: 1187565, 1298732
    > Change-Id: Ieb51ea021af630600524c25c09e88e1992267935
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473515
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Eugene Zemtsov <ezemtsov@google.com>
    > Owners-Override: Eugene Zemtsov <ezemtsov@google.com>
    > Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    > Reviewed-by: Glen Robertson <glenrob@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#972760}
    
    Bug: 1187565, 1298732
    Change-Id: I206d028fc8a5abacd4fefb42965df0a976f36ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472861
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973121}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -973,11 +973,21 @@ void PopulateFrameBinders(RenderFrameHostImpl* host, mojo::BinderMap* map) {
       [](RenderFrameHostImpl* host,
          mojo::PendingReceiver<media::mojom::WebrtcVideoPerfRecorder>
              receiver) {
+        DCHECK_CURRENTLY_ON(BrowserThread::UI);
         media::WebrtcVideoPerfRecorder::Create(
-            BrowserContextImpl::From(
-                host->GetSiteInstance()->GetBrowserContext())
+            BrowserContextImpl::From(host->GetBrowserContext())
                 ->GetWebrtcVideoPerfHistory(),
             std::move(receiver));
+      },
+      base::Unretained(host)));
+
+  map->Add<media::mojom::WebrtcVideoPerfHistory>(base::BindRepeating(
+      [](RenderFrameHostImpl* host,
+         mojo::PendingReceiver<media::mojom::WebrtcVideoPerfHistory> receiver) {
+        DCHECK_CURRENTLY_ON(BrowserThread::UI);
+        BrowserContextImpl::From(host->GetBrowserContext())
+            ->GetWebrtcVideoPerfHistory()
+            ->BindReceiver(std::move(receiver));
       },
       base::Unretained(host)));
 
@@ -1231,6 +1241,8 @@ void PopulateDedicatedWorkerBinders(DedicatedWorkerHost* host,
   // RenderProcessHost binders
   map->Add<media::mojom::VideoDecodePerfHistory>(BindWorkerReceiver(
       &RenderProcessHostImpl::BindVideoDecodePerfHistory, host));
+  map->Add<media::mojom::WebrtcVideoPerfHistory>(BindWorkerReceiver(
+      &RenderProcessHostImpl::BindWebrtcVideoPerfHistory, host));
 
   // RenderProcessHost binders taking a StorageKey
   map->Add<blink::mojom::FileSystemAccessManager>(
@@ -1320,6 +1332,8 @@ void PopulateSharedWorkerBinders(SharedWorkerHost* host, mojo::BinderMap* map) {
   // RenderProcessHost binders
   map->Add<media::mojom::VideoDecodePerfHistory>(BindWorkerReceiver(
       &RenderProcessHostImpl::BindVideoDecodePerfHistory, host));
+  map->Add<media::mojom::WebrtcVideoPerfHistory>(BindWorkerReceiver(
+      &RenderProcessHostImpl::BindWebrtcVideoPerfHistory, host));
 
   // RenderProcessHost binders taking a StorageKey
   map->Add<blink::mojom::FileSystemAccessManager>(
@@ -1409,6 +1423,8 @@ void PopulateServiceWorkerBinders(ServiceWorkerHost* host,
   // RenderProcessHost binders
   map->Add<media::mojom::VideoDecodePerfHistory>(BindServiceWorkerReceiver(
       &RenderProcessHostImpl::BindVideoDecodePerfHistory, host));
+  map->Add<media::mojom::WebrtcVideoPerfHistory>(BindServiceWorkerReceiver(
+      &RenderProcessHostImpl::BindWebrtcVideoPerfHistory, host));
   map->Add<blink::mojom::PushMessaging>(BindServiceWorkerReceiver(
       &RenderProcessHostImpl::BindPushMessaging, host));
 }
