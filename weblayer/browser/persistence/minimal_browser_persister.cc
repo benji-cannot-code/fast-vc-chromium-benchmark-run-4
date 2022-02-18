31c47c5fe3396c49cce0f1c5a8fe573b9290fd01
Rakina Zata Amni <rakina@chromium.org>
2022-02-18 12:13:14 +0000

    Don't persist/sync initial NavigationEntry
    The initial NavigationEntry should not be persisted for session restore
    as otherwise it can trigger an empty-url navigation on restore, leading
    to crashes (see linked bug). This CL ensures that we'll always skip
    the initial NavigationEntry (and empty URL entries in general) when
    persisting session history. This is OK because initial NavigationEntries
    used to not exist before (they were added in M98 under the
    InitialNavigationEntry flag, without it we would just have no
    NavigationEntry in the tab), and even if we try to restore them it would
    just result in about:blank.
    
    Bug: 1240138
    Change-Id: I7c5f24673c7007457e5ac8d041fcf6930df967cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461082
    Reviewed-by: Charles Reis <creis@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972911}

--- weblayer/browser/persistence/minimal_browser_persister.cc
+++ weblayer/browser/persistence/minimal_browser_persister.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/core/session_id.h"
 #include "components/sessions/core/session_service_commands.h"
 #include "components/sessions/core/session_types.h"
+#include "content/public/browser/navigation_entry.h"
 #include "weblayer/browser/browser_impl.h"
 #include "weblayer/browser/persistence/browser_persistence_common.h"
 #include "weblayer/browser/tab_impl.h"
@@ -184,7 +185,8 @@ class MinimalRestorer {
 };
 
 // Iterates over the NavigationEntries of a tab in the order they should be
-// written.
+// written. Starts at the pending NavigationEntry if it exists, and skips over
+// a NavigationEntry if it is the initial NavigationEntry.
 class NavigationEntryIterator {
  public:
   explicit NavigationEntryIterator(Tab* tab)
@@ -196,6 +198,13 @@ class NavigationEntryIterator {
                                  : controller_.GetCurrentEntryIndex()) {
     // GetPendingEntryIndex() returns -1 for new entries, which this implicitly
     // skips (Chrome's persistence code does the same).
+
+    if (content::NavigationEntry* current_entry = entry()) {
+      // If `entry_index_` points to the initial NavigationEntry, skip it, as
+      // initial NavigationEntries should not be persisted.
+      if (!at_pending_ && current_entry->IsInitialEntry())
+        Next();
+    }
   }
   NavigationEntryIterator(const NavigationEntryIterator&) = delete;
   NavigationEntryIterator& operator=(const NavigationEntryIterator&) = delete;
@@ -205,7 +214,7 @@ class NavigationEntryIterator {
   int entry_index() const { return entry_index_; }
 
   // Returns the current entry.
-  content::NavigationEntry* entry() {
+  content::NavigationEntry* entry() const {
     if (at_pending_)
       return controller_.GetPendingEntry();
     return entry_index_ == -1 ? nullptr
@@ -226,6 +235,13 @@ class NavigationEntryIterator {
         --entry_index_;
     } else if (entry_index_ != -1) {
       --entry_index_;
+    }
+
+    // Skip over the initial NavigationEntry as it shouldn't be persisted.
+    content::NavigationEntry* entry = controller_.GetEntryAtIndex(entry_index_);
+    if (entry && entry->IsInitialEntry()) {
+      DCHECK_EQ(0, entry_index_);
+      entry_index_ = -1;
     }
     return !at_end();
   }
