b5a59e1dd4d00b88e7e03eb992afd3a5d3ae75e3
Peter Kasting <pkasting@chromium.org>
2022-02-18 21:20:36 +0000

    Add missing pieces for already-ported colors.
    Bug: 1296206
    Change-Id: I8d454c7b68fdd40e24cf974cb939da22312bdcf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3470165
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973124}

--- chrome/browser/ui/color/linux/native_chrome_color_mixer_linux.cc
+++ chrome/browser/ui/color/linux/native_chrome_color_mixer_linux.cc
@@ -45,4 +45,8 @@ void AddNativeChromeColorMixer(ui::ColorProvider* provider,
   mixer[kColorNewTabPageBackground] = {ui::kColorTextfieldBackground};
   mixer[kColorNewTabPageHeader] = {ui::kColorNativeButtonBorder};
   mixer[kColorNewTabPageText] = {ui::kColorTextfieldForeground};
+  mixer[kColorToolbarButtonIcon] = {kColorToolbarText};
+  mixer[kColorToolbarContentAreaSeparator] = {kColorToolbarSeparator};
+  mixer[kColorToolbarSeparator] = {ui::kColorNativeButtonBorder};
+  mixer[kColorToolbarText] = {ui::kColorNativeLabelForeground};
 }
