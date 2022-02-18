0d4a94245903b53878f9b23319cfbd44ecf6a744
Kevin Radtke <kradtke@chromium.org>
2022-02-18 00:01:27 +0000

    Metrics: Calendar Keyboard Navigation
    Implement metric that records whether a user is navigating the calendar
    view using the tab or arrow keys.
    
    Bug: 1290962
    Change-Id: I531f43251c92982b81c180db38bb3939bbc8e38e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469435
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972726}

--- ash/system/time/calendar_metrics.cc
+++ ash/system/time/calendar_metrics.cc
@@ -32,6 +32,8 @@ constexpr char kCalendarMonthUpArrowButtonActivated[] =
     "Ash.Calendar.MonthUpArrowButton.Activated";
 constexpr char kCalendarMonthDwellTime[] = "Ash.Calendar.MonthDwellTime";
 constexpr char kCalendarScrollSource[] = "Ash.Calendar.ScrollSource";
+constexpr char kCalendarKeyboardNavigation[] =
+    "Ash.Calendar.KeyboardNavigation";
 
 }  // namespace
 
@@ -106,6 +108,11 @@ ui::AnimationThroughputReporter CreateAnimationReporter(
             base::UmaHistogramPercentage(animation_histogram_name, smoothness);
           },
           animation_histogram_name)));
+}
+
+void RecordCalendarKeyboardNavigation(
+    const CalendarKeyboardNavigationSource key_source) {
+  base::UmaHistogramEnumeration(kCalendarKeyboardNavigation, key_source);
 }
 
 }  // namespace calendar_metrics
