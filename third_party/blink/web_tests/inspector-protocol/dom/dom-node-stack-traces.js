b78db3e9964e5d3e65f669357b3b14dec207217a
Connor Clark <cjamcl@google.com>
2022-02-18 21:28:43 +0000

    Use HashMap::find instead of ::at in getNodeStackTraces.
    Fixes crash bug when this experimental feature is enabled in DevTools.
    
    Bug: 1058527
    Change-Id: I852d1a303f0d6d66155865b3d9bf747874579305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473873
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Connor Clark <cjamcl@google.com>
    Cr-Commit-Position: refs/heads/main@{#973129}

---
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-node-stack-traces.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startHTML(`
+    <div id="none-1">No stack</div>
+    <script>
+      var el = document.createElement('div');
+      el.id = 'none-2';
+      document.body.append(el);
+    </script>
+  `, 'Tests DOM.getNodeStackTraces method.');
+
+  await dp.DOM.enable();
+  await dp.DOM.setNodeStackTracesEnabled({enable: true});
+
+  var document = (await dp.DOM.getDocument()).result.root;
+
+  async function logStackTraces(selector) {
+    var node = (await dp.DOM.querySelector({nodeId: document.nodeId, selector})).result;
+    var stackTraces = (await dp.DOM.getNodeStackTraces({nodeId: node.nodeId})).result;
+    testRunner.log(selector);
+    testRunner.log(stackTraces);
+  }
+
+  await logStackTraces('#none-1');
+  // No stack traces because `setNodeStackTracesEnabled` was called after node creation.
+  await logStackTraces('#none-2');
+  await session.evaluate(function createElementForStackTraces() {
+    var el = document.createElement('div');
+    el.id = 'stacks';
+    document.body.append(el);
+  });
+  await logStackTraces('#stacks');
+
+  testRunner.completeTest();
+})
