f3a07ed4fc78349fa81b43520abe38e84aa58e1b
Angela Xiao <angelaxiao@chromium.org>
2022-02-18 00:07:53 +0000

    Add inkdrop on click for assistant buttons
    Screenshots:
    https://screenshot.googleplex.com/3G3r6ZUYj2rHHvy.png
    https://screenshot.googleplex.com/7UJFyBubn7g3YCW.png
    https://screenshot.googleplex.com/A8zyQsorYeFyvpS.png
    
    Bug: 1298205
    Change-Id: Icf48add0de065a1c86cb790bf8fa45b1ca82b6ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472887
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Commit-Queue: Angela Xiao <angelaxiao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972729}

--- ash/assistant/ui/base/assistant_button.h
+++ ash/assistant/ui/base/assistant_button.h
@@ -88,6 +88,7 @@ class COMPONENT_EXPORT(ASSISTANT_UI) AssistantButton
 
  private:
   void OnButtonPressed();
+  void UpdateInkDropColors();
 
   AssistantButtonListener* listener_;
   const AssistantButtonId id_;
