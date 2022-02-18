3556c262cad08ef24c591a8ddc0631f866945d94
Roman Sorokin <rsorokin@chromium.org>
2022-02-18 18:52:56 +0000

    Fix OobeAPI.loginAsGuest
    It crashes if ExistingUserController is missing. So skipping to the
    login screen first
    
    Bug: 1297409
    Change-Id: I02c69165b5328a71b9aa56bb1c73e12fda5f374d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473224
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Auto-Submit: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#973035}

--- chrome/browser/ui/webui/chromeos/login/testapi/oobe_test_api_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/testapi/oobe_test_api_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
 #include "base/bind.h"
+#include "base/check.h"
 #include "base/logging.h"
 #include "build/branding_buildflags.h"
 #include "chrome/browser/ash/login/existing_user_controller.h"
@@ -82,6 +83,9 @@ void OobeTestAPIHandler::SkipPostLoginScreens() {
 }
 
 void OobeTestAPIHandler::LoginAsGuest() {
+  ash::WizardController::default_controller()
+      ->SkipToLoginForTesting();  // IN-TEST
+  CHECK(ash::ExistingUserController::current_controller());
   UserContext context(user_manager::USER_TYPE_GUEST, EmptyAccountId());
   ash::ExistingUserController::current_controller()->Login(context,
                                                            SigninSpecifics());
