155c8eabf5676c98f34e4ea94a2c7ca3cfe29216
Tommy Steimel <steimel@chromium.org>
2022-02-18 22:32:26 +0000

    [Code Health] Use base::Reversed() in remoting/
    This CL replaces reverse iterator loops with |base::Reversed()| in the
    remoting/ directory.
    
    Bug: 1287358
    Change-Id: I92c36f1ad7acdf81d690e05fa4217829928f4508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3475371
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973148}

--- remoting/base/telemetry_log_writer.cc
+++ remoting/base/telemetry_log_writer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/containers/adapters.h"
 #include "base/json/json_string_value_serializer.h"
 #include "base/logging.h"
 #include "net/http/http_status_code.h"
@@ -154,11 +155,11 @@ void TelemetryLogWriter::OnSendLogResult(
                  << "status: " << status.error_message();
 
     // Reverse iterating + push_front in order to restore the order of logs.
-    for (auto i = sending_entries_.rbegin(); i < sending_entries_.rend(); i++) {
-      if (i->send_attempts() >= kMaxSendAttempts) {
+    for (auto& entry : base::Reversed(sending_entries_)) {
+      if (entry.send_attempts() >= kMaxSendAttempts) {
         break;
       }
-      pending_entries_.push_front(std::move(*i));
+      pending_entries_.push_front(std::move(entry));
     }
   } else {
     backoff_.InformOfRequest(true);
