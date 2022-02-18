589003a95debe2a8de9375f59c3e38dbddaf55e9
Yeunjoo Choi <ychoi@igalia.com>
2022-02-18 01:19:26 +0000

    Move URL constants used by ash components to //ash/constants
    This CL moves URL constants used by ash components only from
    chrome/common/url_constants.h to ash/constants/url_constants.h
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Change-Id: If8c8c1a7a2b88420f0c830d063f9b1884eb20141
    Bug: 1164001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466477
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#972756}

--- chrome/browser/ash/crostini/crostini_export_import_notification_controller.cc
+++ chrome/browser/ash/crostini/crostini_export_import_notification_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/crostini/crostini_export_import_notification_controller.h"
 
+#include "ash/constants/url_constants.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -16,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
-#include "chrome/common/url_constants.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
