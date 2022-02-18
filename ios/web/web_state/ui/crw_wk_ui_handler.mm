293548aa7b0a70bd7ee54909c22313ee2664d94c
Gauthier Ambard <gambard@chromium.org>
2022-02-18 14:29:10 +0000

    [iOS] Don't check if a tab has an opener before closing it through JS
    WebKit is already doing this check, so there is no need to do it again
    in our code.
    
    Fixed: 902393
    Change-Id: I5eb0753d9703f3b17696e15df32117572e0434ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472486
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972944}

--- ios/web/web_state/ui/crw_wk_ui_handler.mm
+++ ios/web/web_state/ui/crw_wk_ui_handler.mm
@@ -110,7 +110,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)webViewDidClose:(WKWebView*)webView {
-  if (self.webStateImpl && self.webStateImpl->HasOpener()) {
+  // This is triggered by a JavaScript |close()| method call, only if the tab
+  // was opened using |window.open|. WebKit is checking that this is the case,
+  // so we can close the tab unconditionally here.
+  if (self.webStateImpl) {
     __weak __typeof(self) weakSelf = self;
     // -webViewDidClose will typically trigger another webState to activate,
     // which may in turn also close. To prevent reentrant modificationre in
