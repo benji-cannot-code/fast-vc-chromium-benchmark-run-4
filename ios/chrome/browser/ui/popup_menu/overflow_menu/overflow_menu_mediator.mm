410a279862e356bae3c336e956ac27e3f8311e6c
Robbie Gibson <rkgibson@google.com>
2022-02-18 14:37:01 +0000

    [iOS] Add OverflowMenuMediator unittests
    This adds new OverflowMenuMediator unittests, mostly taken from
    the popup menu mediator unittests.
    
    It also fixes a bug. The user feedback option only shows when user
    feedback is enabled via the corresponding provider.
    
    Bug: 1259052
    Change-Id: Ic5087424086bedaa8cded4f55fe488b3eb2422b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472296
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#972947}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/follow/follow_provider.h"
+#import "ios/public/provider/chrome/browser/user_feedback/user_feedback_provider.h"
 #include "ios/web/common/user_agent.h"
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/navigation/navigation_manager.h"
@@ -251,6 +252,12 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
   if (_webState) {
     [self updateModel];
     _webState->AddObserver(_webStateObserver.get());
+
+    // Observe the language::IOSLanguageDetectionTabHelper for |_webState|.
+    _iOSLanguageDetectionTabHelperObserverBridge =
+        std::make_unique<language::IOSLanguageDetectionTabHelperObserverBridge>(
+            language::IOSLanguageDetectionTabHelper::FromWebState(_webState),
+            self);
   }
 }
 
@@ -267,12 +274,6 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
 
   if (_webStateList) {
     _webStateList->AddObserver(_webStateListObserver.get());
-
-    // Observe the language::IOSLanguageDetectionTabHelper for |_webState|.
-    _iOSLanguageDetectionTabHelperObserverBridge =
-        std::make_unique<language::IOSLanguageDetectionTabHelperObserverBridge>(
-            language::IOSLanguageDetectionTabHelper::FromWebState(_webState),
-            self);
   }
 }
 
@@ -497,13 +498,10 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
                                                [weakSelf openHelp];
                                              });
 
-  // Footer vary based on state, so it's set in -updateModel.
+  // Footer and actions vary based on state, so they're set in -updateModel.
   self.helpActionsGroup =
       [[OverflowMenuActionGroup alloc] initWithGroupName:@"help_actions"
-                                                 actions:@[
-                                                   self.reportIssueAction,
-                                                   self.helpAction,
-                                                 ]
+                                                 actions:@[]
                                                   footer:nil];
 
   // Destinations and footer vary based on state, so they're set in
@@ -613,6 +611,19 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(int nameID,
           }
         }));
   }
+
+  NSMutableArray<OverflowMenuAction*>* helpActions =
+      [[NSMutableArray alloc] init];
+
+  if (ios::GetChromeBrowserProvider()
+          .GetUserFeedbackProvider()
+          ->IsUserFeedbackEnabled()) {
+    [helpActions addObject:self.reportIssueAction];
+  }
+
+  [helpActions addObject:self.helpAction];
+
+  self.helpActionsGroup.actions = helpActions;
 
   // Set footer (on last section), if any.
   if (_browserPolicyConnector &&
