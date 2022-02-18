640cf2bf7f401029c3e9821074fea4ea9a489ec3
S. Ganesh <ganesh@chromium.org>
2022-02-18 05:52:45 +0000

    Fix Updater tasks expire in 7 days
    Updater tasks should not expire.
    Also, defining << for TaskScheduler::TaskInfo.
    
    Bug: 1295399,1298777
    Change-Id: I0a3573646bea87198f3034399c32a0eda221729f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472560
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Auto-Submit: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972821}

--- chrome/updater/win/task_scheduler.cc
+++ chrome/updater/win/task_scheduler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <taskschd.h>
 #include <wrl/client.h>
 
+#include <ostream>
 #include <string>
 #include <utility>
 #include <vector>
@@ -45,7 +46,6 @@ const wchar_t kV2Library[] = L"taskschd.dll";
 // Text for times used in the V2 API of the Task Scheduler.
 const wchar_t kOneHourText[] = L"PT1H";
 const wchar_t kFiveHoursText[] = L"PT5H";
-const wchar_t kZeroMinuteText[] = L"PT0M";
 const wchar_t kFifteenMinutesText[] = L"PT15M";
 const wchar_t kOneDayText[] = L"P1D";
 
@@ -54,9 +54,6 @@ const wchar_t kTaskCompanyFolder[] = L"\\" COMPANY_SHORTNAME_STRING;
 const wchar_t kTaskSubfolderName[] =
     L"\\" COMPANY_SHORTNAME_STRING L"\\" PRODUCT_FULLNAME_STRING;
 
-// Most of the users with pending logs succeeds within 7 days, so no need to
-// try for longer than that, especially for those who keep crashing.
-const int kNumDaysBeforeExpiry = 7;
 const size_t kNumDeleteTaskRetry = 3;
 const size_t kDeleteRetryDelayInMs = 100;
 
@@ -456,14 +453,6 @@ class TaskSchedulerV2 final : public TaskScheduler {
       return false;
     }
 
-    // TODO(csharp): Find a way to only set this for log upload retry.
-    hr = task_settings->put_DeleteExpiredTaskAfter(
-        base::win::ScopedBstr(kZeroMinuteText).Get());
-    if (FAILED(hr)) {
-      PLOG(ERROR) << "Can't put 'DeleteExpiredTaskAfter'. " << std::hex << hr;
-      return false;
-    }
-
     hr = task_settings->put_DisallowStartIfOnBatteries(VARIANT_FALSE);
     if (FAILED(hr)) {
       PLOG(ERROR) << "Can't put 'DisallowStartIfOnBatteries' to false. "
@@ -593,17 +582,6 @@ class TaskSchedulerV2 final : public TaskScheduler {
       }
     }
 
-    // None of the triggers should go beyond kNumDaysBeforeExpiry.
-    base::Time expiry_date(base::Time::NowFromSystemTime() +
-                           base::Days(kNumDaysBeforeExpiry));
-    base::win::ScopedBstr end_boundary(GetTimestampString(expiry_date));
-    hr = trigger->put_EndBoundary(end_boundary.Get());
-    if (FAILED(hr)) {
-      PLOG(ERROR) << "Can't put 'EndBoundary' to " << end_boundary.Get() << ". "
-                  << std::hex << hr;
-      return false;
-    }
-
     Microsoft::WRL::ComPtr<IActionCollection> actions;
     hr = task->get_Actions(&actions);
     if (FAILED(hr)) {
@@ -638,6 +616,10 @@ class TaskSchedulerV2 final : public TaskScheduler {
       PLOG(ERROR) << "Can't set arguments of exec action. " << std::hex << hr;
       return false;
     }
+
+    base::win::ScopedBstr task_xml;
+    task->get_XmlText(task_xml.Receive());
+    VLOG(2) << "Registering Task with XML: " << task_xml.Get();
 
     Microsoft::WRL::ComPtr<IRegisteredTask> registered_task;
     base::win::ScopedVariant user(user_name.Get());
@@ -1105,5 +1087,19 @@ std::unique_ptr<TaskScheduler> TaskScheduler::CreateInstance() {
 }
 
 TaskScheduler::TaskScheduler() = default;
+
+std::ostream& operator<<(std::ostream& stream,
+                         const TaskScheduler::TaskInfo& t) {
+  std::wstring value =
+      base::StrCat({L"[TaskInfo][name]", t.name, L"[description]",
+                    t.description, L"[exec_actions]"});
+  for (auto exec_action : t.exec_actions)
+    value += base::StrCat({L"[exec_action]", exec_action.value()});
+
+  value +=
+      base::StrCat({L"[logon_type]", base::StringPrintf(L"0x%x", t.logon_type),
+                    L"[user_id]", t.user_id});
+  return stream << value;
+}
 
 }  // namespace updater
--- chrome/updater/win/task_scheduler.h
+++ chrome/updater/win/task_scheduler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -97,19 +98,6 @@ class TaskScheduler {
 
     // User ID under which the task runs.
     std::wstring user_id;
-
-    std::wstring value() const {
-      std::wstring value =
-          base::StrCat({L"[TaskInfo][name]", name, L"[description]",
-                        description, L"[exec_actions]"});
-      for (auto exec_action : exec_actions)
-        value += base::StrCat({L"[exec_action]", exec_action.value()});
-
-      value += base::StrCat({L"[logon_type]",
-                             base::StringPrintf(L"0x%x", logon_type),
-                             L"[user_id]", user_id});
-      return value;
-    }
   };
 
   static std::unique_ptr<TaskScheduler> CreateInstance();
@@ -164,6 +152,9 @@ class TaskScheduler {
  protected:
   TaskScheduler();
 };
+
+std::ostream& operator<<(std::ostream& stream,
+                         const TaskScheduler::TaskInfo& t);
 
 }  // namespace updater
 
