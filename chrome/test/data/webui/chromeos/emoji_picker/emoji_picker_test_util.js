f4848e8616fa902674874b2189e9bd41bf59a3ca
Chuong Ho <hdchuong@google.com>
2022-02-18 01:42:16 +0000

    emoji-search: implement algorithm to allow multiword prefix search.
    This CL implements the ranking algorithms to enable multiword prefix
    search. The formula is provided in https://docs.google.com/document/d/1Ub89xsElqVyRaq8tldhligd29-iXsSMWlAdL6Q-Xpr8/edit#heading=h.c0uts5ftkk58
    
    * `EmojiPrefixSearch` class now supports query like 'sm ca',
    'smili face'.
    * New algorithm beats FuSeJS by 1.5-10 times on query time. Please
    refer to this sheets for some performance recording: https://docs.google.com/spreadsheets/d/1wzPh2PvYCWqLcfKMlj40kyO_n8IjHSvdv6qHFi9h9Ac/edit?usp=sharing
    Bug: b/219364826
    Demo: None. Please follow along unit tests to see what is the expected
    output.
    
    Change-Id: Ia61065ef91c447b3b9aeec8f6f667176f522d6ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3458342
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#972766}

--- chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_test_util.js
+++ chrome/test/data/webui/chromeos/emoji_picker/emoji_picker_test_util.js
@@ -4,6 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {assert} from 'chrome://resources/js/assert.m.js';
+import {assertTrue} from '../../chai_assert.js';
+
+export function assertCloseTo(actual, expected) {
+  assertTrue(
+      Math.abs(1 - actual / expected) <= 0.001,
+      `expected ${expected} to be close to ${actual}`);
+}
 
 /**
  * Queries for an element through a path of custom elements.
