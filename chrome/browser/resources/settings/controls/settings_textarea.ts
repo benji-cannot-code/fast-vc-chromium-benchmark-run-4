951fbeae6dd82a20f348d7f139f42e2426de31fa
Adem Derinel <derinel@google.com>
2022-02-18 13:52:13 +0000

    [Password] Add note section to edit dialog
    Adding the note section behind a flag. This CL does not change the
    current behavior.
    Implementation: https://screenshot.googleplex.com/7bTYFwqsYpyKgxu
    Mocks: https://screenshot.googleplex.com/9EnCthCk8Hpah5i
    
    Bug: 1298027
    Change-Id: Ifb3908a54dfa7f49e054f15f87be94b6f541357e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3467814
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#972932}

--- chrome/browser/resources/settings/controls/settings_textarea.ts
+++ chrome/browser/resources/settings/controls/settings_textarea.ts
@@ -53,6 +53,11 @@ export class SettingsTextareaElement extends PolymerElement {
         observer: 'onDisabledChanged_'
       },
 
+      /** Maximum length (in characters) of the text area. */
+      maxlength: {
+        type: Number,
+      },
+
       /** Number of rows (lines) of the text area. */
       rows: {
         type: Number,
