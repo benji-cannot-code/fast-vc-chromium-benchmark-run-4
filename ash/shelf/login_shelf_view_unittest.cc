c927621608e9d33391eb4430ff5b4c45cca1aec2
Sherri Lin <sherrilin@google.com>
2022-02-18 09:22:24 +0000

    [CrOS] Add metrics to count shelf shutdown confirmation bubble actions
    Add histograms that records the following actions: clicking shelf
    shutdown button, clicking cancel button on the shutdown confirmation
    bubble, clicking confirm button on the shutdown confirmation bubble.
    
    Bug: 1275370
    Change-Id: I2a7b2dd93a43525ba250d49e96c807a5e86da247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3429028
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Sherri Lin <sherrilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#972869}

--- ash/shelf/login_shelf_view_unittest.cc
+++ ash/shelf/login_shelf_view_unittest.cc
@@ -1007,7 +1007,8 @@ class LoginShelfViewWithShutdownConfirmationTest : public LoginShelfViewTest {
     ui::test::EventGenerator* event_generator = GetEventGenerator();
     event_generator->MoveMouseTo(
         login_shelf_view_->GetShutdownConfirmationBubbleForTesting()
-            ->GetViewByID(ShelfShutdownConfirmationBubble::kCancel)
+            ->GetViewByID(static_cast<int>(
+                ShelfShutdownConfirmationBubble::ButtonId::kCancel))
             ->GetBoundsInScreen()
             .CenterPoint());
     event_generator->ClickLeftButton();
@@ -1020,7 +1021,8 @@ class LoginShelfViewWithShutdownConfirmationTest : public LoginShelfViewTest {
     ui::test::EventGenerator* event_generator = GetEventGenerator();
     event_generator->MoveMouseTo(
         login_shelf_view_->GetShutdownConfirmationBubbleForTesting()
-            ->GetViewByID(ShelfShutdownConfirmationBubble::kShutdown)
+            ->GetViewByID(static_cast<int>(
+                ShelfShutdownConfirmationBubble::ButtonId::kShutdown))
             ->GetBoundsInScreen()
             .CenterPoint());
     event_generator->ClickLeftButton();
--- ash/shelf/shelf_shutdown_confirmation_bubble.cc
+++ ash/shelf/shelf_shutdown_confirmation_bubble.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_forward.h"
 #include "base/callback_helpers.h"
+#include "base/metrics/histogram_functions.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/aura/window.h"
@@ -45,6 +46,11 @@ const gfx::Insets GetShutdownConfirmationBubbleInsets() {
   insets.set_bottom(kShutdownConfirmationBubbleInsetsBottom);
   return insets;
 }
+
+// Histogram for tracking the number of actions on the shelf shutdown
+// confirmation bubble.
+constexpr char kActionHistogramName[] =
+    "Ash.Shelf.ShutdownConfirmationBubble.Action";
 
 }  // namespace
 
@@ -111,7 +117,7 @@ ShelfShutdownConfirmationBubble::ShelfShutdownConfirmationBubble(
                           base::Unretained(this)),
       l10n_util::GetStringUTF16(IDS_CANCEL), PillButton::Type::kIconless,
       /*icon=*/nullptr);
-  cancel_button->SetID(kCancel);
+  cancel_button->SetID(static_cast<int>(ButtonId::kCancel));
   cancel_ = button_container->AddChildView(std::move(cancel_button));
 
   auto confirm_button = std::make_unique<PillButton>(
@@ -119,7 +125,7 @@ ShelfShutdownConfirmationBubble::ShelfShutdownConfirmationBubble(
                           base::Unretained(this)),
       l10n_util::GetStringUTF16(IDS_ASH_SHUTDOWN_CONFIRM_BUTTON),
       PillButton::Type::kIconless, /*icon=*/nullptr);
-  confirm_button->SetID(kShutdown);
+  confirm_button->SetID(static_cast<int>(ButtonId::kShutdown));
   confirm_ = button_container->AddChildView(std::move(confirm_button));
 
   CreateBubble();
@@ -133,9 +139,16 @@ ShelfShutdownConfirmationBubble::ShelfShutdownConfirmationBubble(
           views::Emphasis::kHigh));
   GetBubbleFrameView()->SetBubbleBorder(std::move(bubble_border));
   GetWidget()->Show();
+
+  base::UmaHistogramEnumeration(kActionHistogramName,
+                                ShelfShutdownConfirmationBubble::kOpened);
 }
 
 ShelfShutdownConfirmationBubble::~ShelfShutdownConfirmationBubble() {
+  if (cancel_callback_ && confirm_callback_) {
+    base::UmaHistogramEnumeration(kActionHistogramName,
+                                  ShelfShutdownConfirmationBubble::kDismissed);
+  }
   // In case shutdown confirmation bubble was dismissed, the pointer of the
   // ShelfShutdownConfirmationBubble in LoginShelfView shall be cleaned up.
   if (cancel_callback_) {
@@ -164,11 +177,19 @@ void ShelfShutdownConfirmationBubble::OnThemeChanged() {
 
 void ShelfShutdownConfirmationBubble::OnCancelled() {
   GetWidget()->Close();
+
+  base::UmaHistogramEnumeration(kActionHistogramName,
+                                ShelfShutdownConfirmationBubble::kCancelled);
+
   std::move(cancel_callback_).Run();
 }
 
 void ShelfShutdownConfirmationBubble::OnConfirmed() {
   GetWidget()->Close();
+
+  base::UmaHistogramEnumeration(kActionHistogramName,
+                                ShelfShutdownConfirmationBubble::kConfirmed);
+
   std::move(confirm_callback_).Run();
 }
 
