5c33ad5940c4e908674a3c1c4e3e93b4e2e8475a
Elaine Chien <elainechien@chromium.org>
2022-02-18 18:49:37 +0000

    [Media Tabs] Add additional metrics
    This CL adds two additional histograms and moves metric recording out of
    TabSearchApiProxy calls.
    
    For
    Tabs.TabSearch.WebUI.IndexRelativeToOpenTabsSectionOf{Action}InUnFilteredList
    we do not record the relative index of tabs in the Audio and Video
    section in this metric because the relative index for the Audio and
    Video section is the same as the already tracked absolute index from
    the top. We do not record the relative index when there is a search
    query (filtered list) because there will be no Audio and Video section
    during search and the relative index will always be the same as the
    absolute index.
    
    Tabs.TabSearch.DistanceOf{Action}FromInitiallySelectedTabIn{State}List
    will help provide insight into whether having the initially selected tab
    be the most recently used tab (that is not the active tab) is desired
    behavior.
    
    Tabs.TabSearch.WebUI.CloseMediaTabAction and
    Tabs.TabSearch.WebUI.SwitchToMediaTabAction are now
    Tabs.TabSearch.WebUI.MediaTab{Action}Action. We have not enabled media
    tabs yet so these histograms have not been recorded to.
    
    Bug: 1261515
    Change-Id: Id79e16f0197e378aa30589ac4143342909e784bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3464501
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973029}

--- chrome/test/data/webui/tab_search/tab_search_app_test.ts
+++ chrome/test/data/webui/tab_search/tab_search_app_test.ts
@@ -213,12 +213,8 @@ suite('TabSearchAppTest', () => {
     const tabSearchItemCloseButton =
         tabSearchItem.shadowRoot!.querySelector('cr-icon-button')!;
     tabSearchItemCloseButton.click();
-    const [tabId, withSearch, isMediaTab, closedTabIndex] =
-        await testProxy.whenCalled('closeTab');
+    const [tabId] = await testProxy.whenCalled('closeTab');
     assertEquals(tabData.tabId, tabId);
-    assertFalse(withSearch);
-    assertFalse(isMediaTab);
-    assertEquals(0, closedTabIndex);
   });
 
   test('Click on recently closed tab item triggers action', async () => {
@@ -642,7 +638,7 @@ suite('TabSearchAppTest', () => {
     assertEquals(0, tabSearchApp.getSelectedIndex());
   });
 
-  test('Verify tab switch is logged correctly', async () => {
+  test('Verify tab switch is called correctly', async () => {
     await setupTest(createProfileData());
     // Make sure that tab data has been recieved.
     verifyTabIds(queryRows(), [1, 5, 6, 2, 3, 4]);
@@ -653,13 +649,9 @@ suite('TabSearchAppTest', () => {
     tabSearchItem.click();
 
     // Assert switchToTab() was called appropriately for an unfiltered tab list.
-    await testProxy.whenCalled('switchToTab')
-        .then(([tabInfo, withSearch, isMediaTab, switchedTabIndex]) => {
-          assertEquals(1, tabInfo.tabId);
-          assertFalse(withSearch);
-          assertFalse(isMediaTab);
-          assertEquals(0, switchedTabIndex);
-        });
+    await testProxy.whenCalled('switchToTab').then(([tabInfo]) => {
+      assertEquals(1, tabInfo.tabId);
+    });
 
     testProxy.reset();
     // Click the first element with tabId 6.
@@ -668,13 +660,9 @@ suite('TabSearchAppTest', () => {
     tabSearchItem.click();
 
     // Assert switchToTab() was called appropriately for an unfiltered tab list.
-    await testProxy.whenCalled('switchToTab')
-        .then(([tabInfo, withSearch, isMediaTab, switchedTabIndex]) => {
-          assertEquals(6, tabInfo.tabId);
-          assertFalse(withSearch);
-          assertFalse(isMediaTab);
-          assertEquals(2, switchedTabIndex);
-        });
+    await testProxy.whenCalled('switchToTab').then(([tabInfo]) => {
+      assertEquals(6, tabInfo.tabId);
+    });
 
     // Force a change to filtered tab data that would result in a
     // re-render.
@@ -691,13 +679,9 @@ suite('TabSearchAppTest', () => {
 
     // Assert switchToTab() was called appropriately for a tab list fitlered by
     // the search query.
-    await testProxy.whenCalled('switchToTab')
-        .then(([tabInfo, withSearch, isMediaTab, switchedTabIndex]) => {
-          assertEquals(2, tabInfo.tabId);
-          assertTrue(withSearch);
-          assertFalse(isMediaTab);
-          assertEquals(0, switchedTabIndex);
-        });
+    await testProxy.whenCalled('switchToTab').then(([tabInfo]) => {
+      assertEquals(2, tabInfo.tabId);
+    });
   });
 
   test('Verify showUI() is called correctly', async () => {
