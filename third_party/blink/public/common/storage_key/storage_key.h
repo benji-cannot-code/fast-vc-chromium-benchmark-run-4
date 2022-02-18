00fac14eb974e062b4464e92f421400f7d439607
kyraseevers <kyraseevers@chromium.org>
2022-02-18 16:05:16 +0000

    Update EncodedAttribute enum to follow style guidelines
    Changes EncodedAttribute to use the kMaxValue feature rather than
    implementing its own kMax element. In addition, changes the return type
    of DeserializeAttributeSeparator to be absl::optional for better error
    handling.
    
    Bug: 1294808
    Fixed: 1294808
    Change-Id: I2ef46265ef0b71b9bc9435b013db87530cd2d078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469619
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kyra Seevers <kyraseevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972967}

--- third_party/blink/public/common/storage_key/storage_key.h
+++ third_party/blink/public/common/storage_key/storage_key.h
@@ -167,7 +167,7 @@ class BLINK_COMMON_EXPORT StorageKey {
     kNonceHigh = 1,
     kNonceLow = 2,
     kAncestorChainBit = 3,
-    kMax
+    kMaxValue = kAncestorChainBit,
   };
 
   StorageKey(const url::Origin& origin,
@@ -190,7 +190,7 @@ class BLINK_COMMON_EXPORT StorageKey {
   // Converts the serialized separator into an EncodedAttribute enum.
   // E.x.: "^0" becomes kTopLevelSite.
   // Expects `in` to have a length of 2.
-  static EncodedAttribute DeserializeAttributeSeparator(
+  static absl::optional<EncodedAttribute> DeserializeAttributeSeparator(
       const base::StringPiece& in);
 
   BLINK_COMMON_EXPORT
