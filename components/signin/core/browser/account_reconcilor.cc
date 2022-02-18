3fa9b9e1114af1b338f6ac44e98dcf698f883e4c
David Roger <droger@chromium.org>
2022-02-18 09:39:14 +0000

    [Lacros] Instantiate ConsistencyCookieManager
    Instantiate the ConsistencyCookieManager owned by the
    AccountReconcilor. The ConsistencyCookieManager is created lazily for
    now, but in the future it should be created eagerly at
    AccountReconcilor construction.
    
    Bug: 1260291
    Change-Id: I4e14afc5fb9ec018351c5327c7a05640f1c41be6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3436968
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#972875}

--- components/signin/core/browser/account_reconcilor.cc
+++ components/signin/core/browser/account_reconcilor.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/account_consistency_method.h"
 #include "components/signin/public/base/signin_client.h"
 #include "components/signin/public/base/signin_metrics.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/accounts_cookie_mutator.h"
 #include "components/signin/public/identity_manager/accounts_in_cookie_jar_info.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
@@ -35,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "components/signin/core/browser/consistency_cookie_manager.h"
+#endif
 
 using signin::AccountReconcilorDelegate;
 using signin::ConsentLevel;
@@ -796,6 +802,21 @@ bool AccountReconcilor::IsReconcileBlocked() const {
   DCHECK_GE(account_reconcilor_lock_count_, 0);
   return account_reconcilor_lock_count_ > 0;
 }
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+signin::ConsistencyCookieManager*
+AccountReconcilor::GetConsistencyCookieManager() {
+  if (base::FeatureList::IsEnabled(switches::kLacrosNonSyncingProfiles) &&
+      !consistency_cookie_manager_) {
+    // TODO(https://crbug.com/1260291): Instantiate the ConsistencyCookieManager
+    // at creation of the AccountReconcilor, once the cookie can be cleared
+    // correctly.
+    consistency_cookie_manager_ =
+        std::make_unique<signin::ConsistencyCookieManager>(client_, this);
+  }
+  return consistency_cookie_manager_.get();
+}
+#endif
 
 void AccountReconcilor::BlockReconcile() {
   DCHECK(IsReconcileBlocked());
