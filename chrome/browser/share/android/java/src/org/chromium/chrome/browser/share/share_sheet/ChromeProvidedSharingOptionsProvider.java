a34fb66fa6183fdf2b5283539f646f5e390a3832
Sophey <sophey@chromium.org>
2022-02-18 17:45:54 +0000

    [SharingHub] Disable printing when Tab is null.
    Fixed: 1263043
    Change-Id: Idb0017a810a8457d8a2299629f5ddbe787a8db9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473511
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973009}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProvider.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProvider.java
@@ -302,7 +302,9 @@ public class ChromeProvidedSharingOptionsProvider {
         if (!mIsIncognito) {
             mOrderedFirstPartyOptions.add(createQrCodeFirstPartyOption());
         }
-        if (UserPrefs.get(Profile.getLastUsedRegularProfile()).getBoolean(Pref.PRINTING_ENABLED)) {
+        if (mTabProvider.hasValue()
+                && UserPrefs.get(Profile.getLastUsedRegularProfile())
+                           .getBoolean(Pref.PRINTING_ENABLED)) {
             mOrderedFirstPartyOptions.add(createPrintingFirstPartyOption());
         }
         mOrderedFirstPartyOptions.add(createSaveImageFirstPartyOption());
