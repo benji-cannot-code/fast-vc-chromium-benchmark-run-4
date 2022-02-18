028602bd8578b6a0d3442d240a7100995fdb5e8b
Sophey <sophey@chromium.org>
2022-02-18 19:13:02 +0000

    [SharingHub] Remove tab from external screenshots.
    This is the second of 3 CLs:
    1. The first one added a new method that takes in a WindowAndroid
    instead of a Tab, and adds a override suppression to the original method
    so there will not be an issue when removing the method from upstream.
    https://crrev.com/i/4555194
    2. This one removes Tab from upstream.
    3. This one will remove Tab completely from internal screenshots and
    adds @Override to the new method.
    
    Bug: 1263037
    Change-Id: I50ba00b8a05282382c2c163b36bd17d78b44e193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472543
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973053}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/screenshot/ScreenshotCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/screenshot/ScreenshotCoordinator.java
@@ -15,7 +15,6 @@ import org.chromium.chrome.browser.image_editor.ImageEditorDialogCoordinator;
 import org.chromium.chrome.browser.modules.ModuleInstallUi;
 import org.chromium.chrome.browser.share.BaseScreenshotCoordinator;
 import org.chromium.chrome.browser.share.share_sheet.ChromeOptionShareCallback;
-import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.modules.image_editor.ImageEditorModuleProvider;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.ui.base.WindowAndroid;
@@ -29,7 +28,7 @@ public class ScreenshotCoordinator extends BaseScreenshotCoordinator {
     static final int MAX_INSTALL_ATTEMPTS = 5;
     private static int sInstallAttempts;
 
-    protected final Tab mTab;
+    private final WindowAndroid mWindowAndroid;
     private final ScreenshotShareSheetDialog mDialog;
     private final ImageEditorModuleProvider mImageEditorModuleProvider;
 
@@ -37,18 +36,18 @@ public class ScreenshotCoordinator extends BaseScreenshotCoordinator {
      * Constructs a new ScreenshotCoordinator which may launch the editor, or a fallback.
      *
      * @param activity The parent activity.
-     * @param tab The Tab which contains the content to share.
+     * @param windowAndroid The {@link WindowAndroid} which contains the content to share.
      * @param shareUrl The URL associated with the screenshot.
      * @param chromeOptionShareCallback An interface to share sheet APIs.
      * @param sheetController The {@link BottomSheetController} for the current activity.
      * @param imageEditorModuleProvider An interface to install and/or instantiate the image editor.
      */
-    public ScreenshotCoordinator(Activity activity, Tab tab, String shareUrl,
+    public ScreenshotCoordinator(Activity activity, WindowAndroid windowAndroid, String shareUrl,
             ChromeOptionShareCallback chromeOptionShareCallback,
             BottomSheetController sheetController,
             ImageEditorModuleProvider imageEditorModuleProvider) {
         super(activity, shareUrl, chromeOptionShareCallback, sheetController);
-        mTab = tab;
+        mWindowAndroid = windowAndroid;
         mDialog = new ScreenshotShareSheetDialog();
         mImageEditorModuleProvider = imageEditorModuleProvider;
     }
@@ -57,7 +56,7 @@ public class ScreenshotCoordinator extends BaseScreenshotCoordinator {
      * Constructor for testing.
      *
      * @param activity The parent activity.
-     * @param tab The Tab which contains the content to share.
+     * @param windowAndroid The WindowAndroid which contains the content to share.
      * @param shareUrl The URL associated with the screenshot.
      * @param screenshotSource The screenshot source.
      * @param dialog The Share Sheet dialog to use as fallback.
@@ -66,13 +65,13 @@ public class ScreenshotCoordinator extends BaseScreenshotCoordinator {
      * @param imageEditorModuleProvider An interface to install and/or instantiate the image editor.
      */
     @VisibleForTesting
-    ScreenshotCoordinator(Activity activity, Tab tab, String shareUrl,
+    ScreenshotCoordinator(Activity activity, WindowAndroid windowAndroid, String shareUrl,
             EditorScreenshotSource screenshotSource, ScreenshotShareSheetDialog dialog,
             ChromeOptionShareCallback chromeOptionShareCallback,
             BottomSheetController sheetController,
             ImageEditorModuleProvider imageEditorModuleProvider) {
         super(activity, shareUrl, chromeOptionShareCallback, sheetController, screenshotSource);
-        mTab = tab;
+        mWindowAndroid = windowAndroid;
         mDialog = dialog;
         mImageEditorModuleProvider = imageEditorModuleProvider;
     }
@@ -109,7 +108,8 @@ public class ScreenshotCoordinator extends BaseScreenshotCoordinator {
         assert mImageEditorModuleProvider != null;
         ImageEditorDialogCoordinator editor =
                 mImageEditorModuleProvider.getImageEditorDialogCoordinator();
-        editor.launchEditor(mActivity, mScreenshot, mTab, mShareUrl, mChromeOptionShareCallback);
+        editor.launchEditor(
+                mActivity, mScreenshot, mWindowAndroid, mShareUrl, mChromeOptionShareCallback);
         mScreenshot = null;
     }
 
@@ -118,8 +118,9 @@ public class ScreenshotCoordinator extends BaseScreenshotCoordinator {
      */
     protected void launchSharesheet() {
         ScreenshotShareSheetDialogCoordinator shareSheet =
-                new ScreenshotShareSheetDialogCoordinator(mActivity, mDialog, mScreenshot, mTab,
-                        mShareUrl, mChromeOptionShareCallback, this::retryInstallEditor);
+                new ScreenshotShareSheetDialogCoordinator(mActivity, mDialog, mScreenshot,
+                        mWindowAndroid, mShareUrl, mChromeOptionShareCallback,
+                        this::retryInstallEditor);
         shareSheet.showShareSheet();
         mScreenshot = null;
     }
@@ -151,13 +152,12 @@ public class ScreenshotCoordinator extends BaseScreenshotCoordinator {
         ModuleInstallUi.Delegate moduleInstallUiDelegate = new ModuleInstallUi.Delegate() {
             @Override
             public WindowAndroid getWindowAndroid() {
-                return mTab.getWindowAndroid();
+                return mWindowAndroid;
             }
 
             @Override
             public Context getContext() {
-                return mTab.getWindowAndroid() != null ? mTab.getWindowAndroid().getActivity().get()
-                                                       : null;
+                return mWindowAndroid != null ? mWindowAndroid.getActivity().get() : null;
             }
         };
         final ModuleInstallUi ui = new ModuleInstallUi(moduleInstallUiDelegate,
