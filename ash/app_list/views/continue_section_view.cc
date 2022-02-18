bfe94e6c46afeb2d402d791202410bc94a50e126
Ana Salazar <anasalazar@chromium.org>
2022-02-18 20:17:00 +0000

    Add Privacy nudge icons.
    Set the privacy nudge icons for dark and light mode.
    Adjust margins and size to match spec.
    
    Screenshots:
      -Dark:
        https://screenshot.googleplex.com/4Ab4Lz7L9nL9vPB.png
        https://screenshot.googleplex.com/ACgDCowR5atFoNB.png
      -Light:
        https://screenshot.googleplex.com/5pAV3HYiGPixRQe.png
        https://screenshot.googleplex.com/Ke2qEJvtcyUh9MH.png
    
    Bug: 1277666
    Change-Id: I7951857f3bb6c0957ab2b1cb8a5ecacb4d131cc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473852
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973089}

--- ash/app_list/views/continue_section_view.cc
+++ ash/app_list/views/continue_section_view.cc
@@ -54,8 +54,11 @@ constexpr size_t kMinFilesForContinueSectionClamshellMode = 3;
 constexpr size_t kMinFilesForContinueSectionTabletMode = 2;
 
 // Privacy toast icon size.
-constexpr size_t kPrivacyIconSizeClamshell = 64;
+constexpr size_t kPrivacyIconSizeClamshell = 60;
 constexpr size_t kPrivacyIconSizeTablet = 48;
+
+// Privacy toast interior margin
+constexpr gfx::Insets kPrivacyToastInteriorMargin(12, 12, 12, 16);
 
 // Delay before marking the privacy notice as swhon.
 const base::TimeDelta kPrivacyNoticeShownDelay = base::Seconds(6);
@@ -264,6 +267,7 @@ void ContinueSectionView::MaybeCreatePrivacyNotice() {
                          &ContinueSectionView::MarkPrivacyNoticeAccepted,
                          base::Unretained(this)))
           .SetStyleForTabletMode(tablet_mode_)
+          .SetThemingIcons(&kContinueFilesDarkIcon, &kContinueFilesLightIcon)
           .SetIconSize(tablet_mode_ ? kPrivacyIconSizeTablet
                                     : kPrivacyIconSizeClamshell)
           .Build());
@@ -271,6 +275,7 @@ void ContinueSectionView::MaybeCreatePrivacyNotice() {
       views::kFlexBehaviorKey,
       views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToMinimum,
                                views::MaximumFlexSizeRule::kScaleToMaximum));
+  privacy_toast_->UpdateInteriorMargins(kPrivacyToastInteriorMargin);
 }
 
 bool ContinueSectionView::FirePrivacyNoticeShownTimerForTest() {
