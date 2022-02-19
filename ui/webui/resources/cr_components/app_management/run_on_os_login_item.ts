420af3326975bd66cdbf2a717a6d8b531067b378
Evan Stade <estade@chromium.org>
2022-02-19 02:07:23 +0000

    dPWA: Web App Settings - hide page until data is available
    Initially hide the contents of the Web App Settings page, and only
    reveal it after the data for the app is available. This avoids
    flashes during layout:
    
    * permissions items, which unhide their shadow root and cause the
      layout to change (in the future, file handling controls will
      behave this way as well)
    * other rows, which don't affect layout but show text and toggles
      only after data is available
    
    This also allows us to remove code from Window Mode and Run On Os
    Login which hides the contents of the row until data is ready so
    as to avoid showing an animating toggle control. Since the whole
    page is hidden when data is unavailable, we don't need this code
    to be implemented on an individual item basis. In the future,
    should optional display be required, these elements should follow
    the precedent set by pin_to_shelf_item.js.
    
    Also fix a missing </div> tag in app.html.
    
    Bug: none
    Change-Id: Ie3f36f35dba0c8be6ccea70d35da51631f86ec8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462190
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973228}

--- ui/webui/resources/cr_components/app_management/run_on_os_login_item.ts
+++ ui/webui/resources/cr_components/app_management/run_on_os_login_item.ts
@@ -29,12 +29,6 @@ export class AppManagementRunOnOsLoginItemElement extends PolymerElement {
       loginModeLabel: String,
 
       app: Object,
-
-      available_: {
-        type: Boolean,
-        computed: 'isAvailable_(app)',
-        reflectToAttribute: true,
-      },
     };
   }
 
@@ -47,16 +41,8 @@ export class AppManagementRunOnOsLoginItemElement extends PolymerElement {
     this.addEventListener('change', this.toggleOsLoginMode_);
   }
 
-  private isAvailable_(app: App): boolean {
-    if (app === undefined) {
-      return false;
-    }
-    assert(app);
-    return app.runOnOsLogin !== undefined;
-  }
-
   private isManaged_(app: App): boolean {
-    if (app === undefined || !this.isAvailable_(app)) {
+    if (app === undefined) {
       return false;
     }
     assert(app);
@@ -67,7 +53,6 @@ export class AppManagementRunOnOsLoginItemElement extends PolymerElement {
     }
     return false;
   }
-
 
   private getValue_(app: App): boolean {
     if (app === undefined) {
@@ -89,7 +74,7 @@ export class AppManagementRunOnOsLoginItemElement extends PolymerElement {
         .querySelector<AppManagementToggleRowElement>('#toggle-row')!.click();
   }
 
-  toggleOsLoginMode_() {
+  private toggleOsLoginMode_() {
     assert(this.app);
     const currentRunOnOsLoginData = this.app.runOnOsLogin;
     if (currentRunOnOsLoginData) {
@@ -105,12 +90,12 @@ export class AppManagementRunOnOsLoginItemElement extends PolymerElement {
           this.app.id,
           newRunOnOsLoginMode,
       );
-      const booleanWindowMode =
+      const booleanRunOnOsLoginMode =
           this.getRunOnOsLoginModeBoolean(newRunOnOsLoginMode);
-      const windowModeChangeAction = booleanWindowMode ?
+      const runOnOsLoginModeChangeAction = booleanRunOnOsLoginMode ?
           AppManagementUserAction.RunOnOsLoginModeTurnedOn :
           AppManagementUserAction.RunOnOsLoginModeTurnedOff;
-      recordAppManagementUserAction(this.app.type, windowModeChangeAction);
+      recordAppManagementUserAction(this.app.type, runOnOsLoginModeChangeAction);
     }
   }
 
--- ui/webui/resources/cr_components/app_management/window_mode_item.ts
+++ ui/webui/resources/cr_components/app_management/window_mode_item.ts
@@ -17,7 +17,7 @@ import {recordAppManagementUserAction} from './util.js';
 
 export class AppManagementWindowModeElement extends PolymerElement {
   static get is() {
-    return 'app-management-window-mode';
+    return 'app-management-window-mode-item';
   }
 
   static get template() {
@@ -29,15 +29,6 @@ export class AppManagementWindowModeElement extends PolymerElement {
       windowModeLabel: String,
 
       app: Object,
-
-      /**
-       * True if the window mode type is available for the app.
-       */
-      available_: {
-        type: Boolean,
-        computed: 'isAvailable_(app)',
-        reflectToAttribute: true,
-      },
     };
   }
 
@@ -50,16 +41,8 @@ export class AppManagementWindowModeElement extends PolymerElement {
     this.addEventListener('change', this.toggleWindowMode_);
   }
 
-  private isAvailable_(app: App): boolean {
-    if (app === undefined) {
-      return false;
-    }
-    assert(app);
-    return app.windowMode !== undefined;
-  }
-
   private getValue_(app: App): boolean {
-    if (!this.isAvailable_(app)) {
+    if (app === undefined) {
       return false;
     }
     assert(app);
@@ -111,7 +94,7 @@ export class AppManagementWindowModeElement extends PolymerElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'app-management-window-mode': AppManagementWindowModeElement;
+    'app-management-window-mode-item': AppManagementWindowModeElement;
   }
 }
 
