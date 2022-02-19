afbfe47b8a04bf8e1a5bd3f757e2a4b404677717
Jason Thai <jasontt@chromium.org>
2022-02-19 03:09:59 +0000

    personalization: style user subpage
    * Add grid layout, separator
    * Style the images and folder icon
    
    BUG=b/220371971
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Ie5bdd6f311a24dcefd5eac81372cbf62336fadf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3475593
    Reviewed-by: Jeffrey Young <cowmoo@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973233}

--- chrome/test/data/webui/chromeos/personalization_app/user_subpage_element_test.ts
+++ chrome/test/data/webui/chromeos/personalization_app/user_subpage_element_test.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {UserSubpage} from 'chrome://personalization/trusted/user/user_subpage_element.js';
 
-import {assertEquals} from 'chrome://webui-test/chai_assert.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 import {initElement, teardownElement} from './personalization_app_test_utils.js';
 
@@ -21,7 +21,11 @@ export function UserSubpageTest() {
 
   test('displays content', async () => {
     userSubpageElement = initElement(UserSubpage);
-    assertEquals(
-        'User', userSubpageElement.shadowRoot!.querySelector('h2')!.innerText);
+    const userPreview =
+        userSubpageElement.shadowRoot!.querySelector('user-preview');
+    assertTrue(!!userPreview);
+    const avatarList =
+        userSubpageElement.shadowRoot!.querySelector('avatar-list');
+    assertTrue(!!avatarList);
   });
 }
