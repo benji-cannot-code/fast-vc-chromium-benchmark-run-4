e3e7a0e7d517b3c394bd4d0b190ea2025b2cc3c5
Nancy Wang <nancylingwang@chromium.org>
2022-02-19 00:24:52 +0000

    Enable the AppServiceOnAppTypeInitializedWithoutMojom flag.
    BUG=1253250
    
    Change-Id: Id81e654e9b704934dba07e3f30af7a83ffd8ca5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3461159
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973193}

--- components/services/app_service/public/cpp/features.cc
+++ components/services/app_service/public/cpp/features.cc
@@ -9,7 +9,7 @@ namespace apps {
 
 const base::Feature kAppServiceOnAppTypeInitializedWithoutMojom{
     "AppServiceOnAppTypeInitializedWithoutMojom",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kAppServiceOnAppUpdateWithoutMojom{
     "AppServiceOnAppUpdateWithoutMojom", base::FEATURE_DISABLED_BY_DEFAULT};
