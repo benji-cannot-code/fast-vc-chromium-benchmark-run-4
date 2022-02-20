e589ecfe76901d86afc674e69070e2ed8273982f
Wenbo Jie <wenbojie@chromium.org>
2022-02-20 23:11:47 +0000

    [FilesA11y] Keep focus on "switch view" button after it's activated
    When "switch view" button is activated by keyboard, the focus should
    stay on the button to keep consistent behaviour as other action buttons.
    
    Note: there are also other changes in the CL
    * Add a public method for FilesTooltip called showTooltip(), it will
    explicitly show tooltip for the current visible tooltip target.
    * Handle keyup/click event together to prevent infinite triggering for
    callback. "click" event will be triggered multiple times if users hold
    Enter/Space key down without releasing (demo: http://shortn/_Z1x7Cq1SfF),
    we need to have additional logic there to prevent it (demo:
    http://shortn/_QzY2THz20b)
    
    Demo: http://shortn/_NzAKaQfuHo
    
    Bug: 1221202
    Test: deployed to DUT and visually inspect
    Test: browser_tests --gtest_filter="*FilesTooltip*"
    Change-Id: Ib1ab52ee7c54f253731611934d77163dc2df235b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3459366
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973389}

--- ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_manager_ui.js
@@ -183,6 +183,13 @@ export class FileManagerUI {
     this.toolbar = queryRequiredElement('.dialog-header', this.element);
 
     /**
+     * The tooltip element.
+     * @type {!FilesTooltip}
+     */
+    this.filesTooltip =
+        assertInstanceof(document.querySelector('files-tooltip'), FilesTooltip);
+
+    /**
      * The actionbar which contains buttons to perform actions on selected
      * file(s).
      * @type {!HTMLElement}
@@ -546,11 +553,9 @@ export class FileManagerUI {
    * Attaches files tooltip.
    */
   attachFilesTooltip() {
-    const filesTooltip =
-        assertInstanceof(document.querySelector('files-tooltip'), FilesTooltip);
-    filesTooltip.addTargets(document.querySelectorAll('[has-tooltip]'));
+    this.filesTooltip.addTargets(document.querySelectorAll('[has-tooltip]'));
 
-    this.locationLine.filesTooltip = filesTooltip;
+    this.locationLine.filesTooltip = this.filesTooltip;
   }
 
   /**
