d8f245ed592a9bb39a84b6f05ac17fb18bc3c4ce
David Sanders <dsanders11@ucsbalum.com>
2022-02-20 22:56:20 +0000

    Remove unused include of //base/threading/thread_local.h
    Bug: 242216
    Change-Id: I3b7987205622412911740a66fcb3954cf1c48d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3476477
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973385}

--- base/trace_event/trace_event_impl.h
+++ base/trace_event/trace_event_impl.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
-#include "base/threading/thread_local.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "base/trace_event/thread_instruction_count.h"
 #include "base/trace_event/trace_arguments.h"
