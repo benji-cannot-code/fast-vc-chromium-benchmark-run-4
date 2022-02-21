a92f107aec6087fa4354a1ee751be69cdc1508ad
Andrea Orru <andreaorru@chromium.org>
2022-02-21 01:09:36 +0000

    Reland "Merge common areas of icudtl.dat in Lacros and Ash"
    This is a reland of 155b701b5c9b4050a3698ee0d537d4f6490640eb
    
    Lack of BASE_I18N_EXPORT was causing the component build to fail
    after submitting to CQ. This change addresses the issue.
    
    Original change's description:
    > Merge common areas of icudtl.dat in Lacros and Ash
    >
    > There are two copies of icudtl.dat in the ChromeOS system.
    > One is included in ash-chrome, and the other one in lacros-chrome.
    > These two revisions of the file are largely the same, even across
    > releases. However, both are loaded in RAM at the same time.
    >
    > This change causes Lacros to scan Ash's version of icudtl.dat
    > in search of overlapping pages. If they are found, it unmaps
    > them and maps them from Ash's file instead. This can save
    > up to ~11MB of memory in the best case.
    >
    > Test: Output the amount of merged bytes in the log to
    > verify that pages with the same content were merged.
    > Takes ~2.5ms on Eve to perform the merging with pre-computed hashes.
    > ~4.5ms without pre-computed hashes.
    >
    > Bug: 1253279
    > Change-Id: If5374787b6a132ca85b2f8aefab57d7272154a3e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3397889
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#972204}
    
    Bug: 1253279
    Change-Id: I5c767e9925dcb82711f53af32fa86c13717888a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468881
    Auto-Submit: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973396}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3142,6 +3142,9 @@ extern const char kLauncherNudgeShortIntervalDescription[];
 extern const char kDesktopCaptureLacrosV2Name[];
 extern const char kDesktopCaptureLacrosV2Description[];
 
+extern const char kLacrosMergeIcuDataFileName[];
+extern const char kLacrosMergeIcuDataFileDescription[];
+
 extern const char kLacrosNonSyncingProfilesName[];
 extern const char kLacrosNonSyncingProfilesDescription[];
 
