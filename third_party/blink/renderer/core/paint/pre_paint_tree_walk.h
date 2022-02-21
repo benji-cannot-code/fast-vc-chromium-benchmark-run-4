35b1a4281ab513731db6e9eee6aa9c959741ad1f
Morten Stenshorne <mstensho@chromium.org>
2022-02-21 08:24:04 +0000

    Pre-paint: Update first-for-node state at fragmentainers.
    A nested multicol container may have a padding so tall that there won't
    be room for any columns until we get to the next outer column. An
    out-of-flow positioned descendant that is *not* contained by the
    innermost multicol container needs to be visited exactly once. We missed
    the OOF, because is_parent_first_for_node in PrePaintTreeWalk::
    WalkLayoutObjectChildren() was wrong. It was set by the inner multicol
    container, but the first fragment of the inner multicol container is one
    without columns (just padding there), and OOFs are only discovered when
    walking inside columns.
    
      Box (block-flow-root block-flow)      #outer
        Box (column block-flow)
          Box (block-flow-root block-flow)  #inner
        Box (column block-flow)
          Box (block-flow-root block-flow)  #inner
            Box (column block-flow)
    
    Therefore, update is_parent_first_for_node for each column as well.
    
    WalkFragmentationContextRootChildren() already did quite a few things to
    the PrePaintTreeWalkContext object passed, which we'd have to reset when
    entering non-fragmentainer children (such as column spanners). This
    would be one more thing to reset. Instead of this mess, establish a new
    PrePaintTreeWalkContext for each column. (It also looks like we had
    forgotten to reset fragmentation_nesting_level when entering spanners.)
    This also allows us to add some constness to the PrePaintTreeWalkContext
    argument.
    
    Bug: 1295998
    Change-Id: I8c471936be9c05a6475a4c135bfa7490ac7b09a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468548
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973450}

--- third_party/blink/renderer/core/paint/pre_paint_tree_walk.h
+++ third_party/blink/renderer/core/paint/pre_paint_tree_walk.h
@@ -186,10 +186,10 @@ class CORE_EXPORT PrePaintTreeWalk final {
 
   void WalkFragmentationContextRootChildren(const LayoutObject&,
                                             const NGPhysicalBoxFragment&,
-                                            PrePaintTreeWalkContext&);
+                                            const PrePaintTreeWalkContext&);
   void WalkLayoutObjectChildren(const LayoutObject&,
                                 const NGPhysicalBoxFragment*,
-                                PrePaintTreeWalkContext&);
+                                const PrePaintTreeWalkContext&);
   void WalkChildren(const LayoutObject&,
                     const NGPhysicalBoxFragment*,
                     PrePaintTreeWalkContext&,
