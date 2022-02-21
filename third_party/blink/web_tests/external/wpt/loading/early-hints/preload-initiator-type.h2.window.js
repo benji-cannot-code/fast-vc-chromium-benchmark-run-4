cda8ab46b9adc13d2f4c8523d7aad09007f38d46
Kenichi Ishibashi <bashi@chromium.org>
2022-02-21 11:59:02 +0000

    Add an Early Hints WPT
    The test checks that the `initiatorType` of a resource timing entry is
    set to "early-hints" when the resource is preloaded via Early Hints.
    
    Bug: 1212542
    Change-Id: I0ea2c9c36d24664441bbec53ecee55d109a7d7b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3476162
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973483}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/preload-initiator-type.h2.window.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=resources/early-hints-helpers.js
+
+test(() => {
+    const preloads = [{
+        "url": "empty.js?" + Date.now(),
+        "as_attr": "script",
+    }];
+    navigateToTestWithEarlyHints("resources/preload-initiator-type.html", preloads);
+});
