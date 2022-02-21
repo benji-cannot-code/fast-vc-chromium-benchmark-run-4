1270b71d6172959d35982f1e672877c3b6ef91ea
Olesia Marukhno <olesiamarukhno@google.com>
2022-02-21 16:30:38 +0000

    [privacy sandbox settings] Fix the bubble position on Win
    Update PrivacySandboxDialogView to be a view instead of DialogDelegate.
    Create a DialogDelegate and set PrivacySandboxDialogView as the content
    view.
    
    PrivacySandboxDialogView was BubbleDelegateView and it caused bugs on
    Windows and Linux with the bubble position.
    
    Bug: 1286276
    Change-Id: I2b8fbb31ea2827efdda6a9a975be1c861141a7a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3476998
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#973541}

--- chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.h
+++ chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
 #include "ui/base/metadata/metadata_header_macros.h"
-#include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/view.h"
 
 class Browser;
 
@@ -18,7 +18,7 @@ class WebView;
 
 // Implements the PrivacySandboxDialog as a View. The view contains a WebView
 // into which is loaded a WebUI page which renders the actual dialog content.
-class PrivacySandboxDialogView : public views::BubbleDialogDelegateView {
+class PrivacySandboxDialogView : public views::View {
  public:
   METADATA_HEADER(PrivacySandboxDialogView);
   PrivacySandboxDialogView(Browser* browser,
