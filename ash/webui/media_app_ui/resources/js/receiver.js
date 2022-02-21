7523458ed1a43e14cf8536c27376b1449a517ca5
Trent Apted <tapted@chromium.org>
2022-02-21 00:31:26 +0000

    Use a popup for the readonly "pdf viewer" in chrome://media-app
    Currently the pdf viewer opens in a Tab, but that tab will be an Ash
    browser. And the viewer is more than just a URL -- it needs to get a
    Blob resource from the opener -- so it can't open in a LacrOS browser.
    
    The fix is to use a popup. But if the untrusted context that hosts the
    app attempts to open a popup with a different top-frame origin, it gets
    a confusing banner in the popup saying ~chrome-untrusted is insecure.
    So, to fix that, the secure context opens the popup, which has another
    sandbox to actually read the blob; communicating between two
    chrome-untrusted://media-app origins.
    
    Bug: b/216020585
    Change-Id: I08350fa9b052feedefa63c11f0c3ea37ea77a5e5
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462613
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973394}

--- ash/webui/media_app_ui/resources/js/receiver.js
+++ ash/webui/media_app_ui/resources/js/receiver.js
@@ -331,7 +331,15 @@ const DELEGATE = {
       }
       throw e;
     }
-  }
+  },
+  /**
+   * @param {string} title
+   * @param {string} blobUuid
+   */
+  openInSandboxedViewer(title, blobUuid) {
+    parentMessagePipe.sendMessage(
+        Message.OPEN_IN_SANDBOXED_VIEWER, {title, blobUuid});
+  },
 };
 
 /**
---
+++ ash/webui/media_app_ui/resources/js/viewpdfhost.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const params = new URLSearchParams(document.location.search);
+const title = params.get('title');
+const blob = params.get('blobUuid');
+document.title = title;
+document.querySelector('iframe').src =
+    'chrome-untrusted://media-app/assets/viewpdf.html?' +
+    `${new URLSearchParams({title, blob})}`;
