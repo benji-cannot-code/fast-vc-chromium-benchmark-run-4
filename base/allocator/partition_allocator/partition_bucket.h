93c433ceae1f699e2c4c2f50e7d4c8b8387cb3f8
Bruce Dawson <brucedawson@chromium.org>
2022-02-21 18:57:47 +0000

    Remove anomymous namespace from PA header
    Partition Alloc has numerous anonymous namespaces in header files. This
    one I was confident in removing, since it just declares an integer
    constant.
    
    Bug: 1287650
    Change-Id: I090426e574b36a1317b48624037a4a7d31a7b517
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3405217
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973559}

--- base/allocator/partition_allocator/partition_bucket.h
+++ base/allocator/partition_allocator/partition_bucket.h
@@ -18,11 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace partition_alloc::internal {
 
-namespace {
-
-constexpr int kPartitionNumSystemPagesPerSlotSpanBits = 8;
-
-}  // namespace
+constexpr inline int kPartitionNumSystemPagesPerSlotSpanBits = 8;
 
 template <bool thread_safe>
 struct PartitionBucket {
