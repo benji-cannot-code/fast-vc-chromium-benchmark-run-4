8410156c146f988f5d82cc95c5d08f2e424151b1
David Munro <davidmunro@google.com>
2022-02-21 23:58:13 +0000

    guest_os: Add a MountProvider and registry
    As part of go/guest-os-file-sharing-sketch, create a registry and base
    provider class for Guest Os's that support sharing files from guest to
    host.  Guest OSs implement the base class and register it, the files app
    queries the registry for everything in it.
    
    Bug: 1293229
    Test: Unit tests, CQ
    Change-Id: I2a5e037997f1914050c29fa1c789eb6dfffd74d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462611
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#973590}

--- chrome/browser/ash/guest_os/public/guest_os_service.h
+++ chrome/browser/ash/guest_os/public/guest_os_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_GUEST_OS_PUBLIC_GUEST_OS_SERVICE_H_
 #define CHROME_BROWSER_ASH_GUEST_OS_PUBLIC_GUEST_OS_SERVICE_H_
 
+#include "chrome/browser/ash/guest_os/public/guest_os_mount_provider_registry.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 class Profile;
@@ -25,6 +26,11 @@ class GuestOsService : public KeyedService {
 
   // Helper method to get the service instance for the given profile.
   static GuestOsService* GetForProfile(Profile* profile);
+
+  GuestOsMountProviderRegistry* MountProviderRegistry();
+
+ private:
+  GuestOsMountProviderRegistry mount_provider_registry_;
 };
 
 }  // namespace guest_os
