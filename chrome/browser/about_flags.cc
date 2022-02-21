5bc2c4b49df1581a0e361387e9b6ca6d20fa6167
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-02-21 01:21:57 +0000

    Revert "Reland "Merge common areas of icudtl.dat in Lacros and Ash""
    This reverts commit ab48630ade1ad3395fe37b0405412d35071f577e.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision ab48630ade1ad3395fe37b0405412d35071f577e as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8821660673144766769
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2Fab48630ade1ad3395fe37b0405412d35071f577e&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+ab48630ade1ad3395fe37b0405412d35071f577e
    
    Original change's description:
    > Reland "Merge common areas of icudtl.dat in Lacros and Ash"
    > 
    > This is a reland of 155b701b5c9b4050a3698ee0d537d4f6490640eb
    > 
    > Lack of BASE_I18N_EXPORT was causing the component build to fail
    > after submitting to CQ. This change addresses the issue.
    > 
    > Original change's description:
    > > Merge common areas of icudtl.dat in Lacros and Ash
    > >
    > > There are two copies of icudtl.dat in the ChromeOS system.
    > > One is included in ash-chrome, and the other one in lacros-chrome.
    > > These two revisions of the file are largely the same, even across
    > > releases. However, both are loaded in RAM at the same time.
    > >
    > > This change causes Lacros to scan Ash's version of icudtl.dat
    > > in search of overlapping pages. If they are found, it unmaps
    > > them and maps them from Ash's file instead. This can save
    > > up to ~11MB of memory in the best case.
    > >
    > > Test: Output the amount of merged bytes in the log to
    > > verify that pages with the same content were merged.
    > > Takes ~2.5ms on Eve to perform the merging with pre-computed hashes.
    > > ~4.5ms without pre-computed hashes.
    > >
    > > Bug: 1253279
    > > Change-Id: If5374787b6a132ca85b2f8aefab57d7272154a3e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3397889
    > > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > > Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#972204}
    > 
    > Bug: 1253279
    > Change-Id: I5c767e9925dcb82711f53af32fa86c13717888a2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3468881
    > Auto-Submit: Andrea Orru <andreaorru@chromium.org>
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#973396}
    
    
    Change-Id: If8cbadd8a1051bb475b5951d4e26a733690a1190
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1253279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3476469
    Reviewed-by: Yuki Yamada <yukiy@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Owners-Override: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973398}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -251,7 +251,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "base/i18n/icu_mergeable_data_file.h"
 #include "chrome/browser/lacros/lacros_url_handling.h"
 #include "chrome/common/webui_url_constants.h"
 #endif
@@ -8047,11 +8046,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kDesktopCaptureLacrosV2Name,
      flag_descriptions::kDesktopCaptureLacrosV2Description, kOsCrOS | kOsLinux,
      FEATURE_VALUE_TYPE(features::kDesktopCaptureLacrosV2)},
-
-    {"lacros-merge-icu-data-file",
-     flag_descriptions::kLacrosMergeIcuDataFileName,
-     flag_descriptions::kLacrosMergeIcuDataFileDescription, kOsCrOS,
-     FEATURE_VALUE_TYPE(base::i18n::kLacrosMergeIcuDataFile)},
 
     {"lacros-non-syncing-profiles",
      flag_descriptions::kLacrosNonSyncingProfilesName,
