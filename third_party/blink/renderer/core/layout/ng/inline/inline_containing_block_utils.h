c964ac27e7ea17c9f2decb782af4ea6a08a3325d
Yuki Yamada <yukiy@chromium.org>
2022-02-21 05:17:16 +0000

    Layout: Replace UntracedMember in InlineContainingBlockMap by Persistent
    Bug: 1030176, 1239524
    Change-Id: Ic663e85cbb9661f26cc3954c0214a7f682805071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3474097
    Auto-Submit: Yuki Yamada <yukiy@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973426}

--- third_party/blink/renderer/core/layout/ng/inline/inline_containing_block_utils.h
+++ third_party/blink/renderer/core/layout/ng/inline/inline_containing_block_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_rect.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
 namespace blink {
@@ -34,10 +35,9 @@ class InlineContainingBlockUtils {
     LogicalOffset relative_offset;
   };
 
-  // It is safe to use UntracedMember here because this is used only on stack
-  // and stores references to LayoutObject owned by other objects.
+  // It is safe to use Persistent here because this is used only on stack.
   using InlineContainingBlockMap =
-      HashMap<UntracedMember<const LayoutObject>,
+      HashMap<Persistent<const LayoutObject>,
               absl::optional<InlineContainingBlockGeometry>>;
 
   // Computes the geometry required for any inline containing blocks.
