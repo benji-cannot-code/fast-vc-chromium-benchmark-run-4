e774f90b7512f719cb4ff82d44d8c0768b7bbd4f
Marshall Greenblatt <marshall@chromium.org>
2022-02-22 19:38:04 +0000

    Add missing lock.h include
    Fixes the following error building on Windows with `use_custom_libcxx=false`:
    
    ../../base/debug/dump_without_crashing.cc(20,35): error: no member named 'Lock' in namespace 'base'
    
    Change-Id: Ifbdae56ae152cdf70580f117da4fcdaa4c662a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3479396
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Marshall Greenblatt <marshall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973856}

--- base/debug/dump_without_crashing.cc
+++ base/debug/dump_without_crashing.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
+#include "base/synchronization/lock.h"
 #include "base/trace_event/base_tracing.h"
 
 namespace {
