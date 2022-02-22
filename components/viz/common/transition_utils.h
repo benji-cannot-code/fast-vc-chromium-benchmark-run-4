4710eb05540a27e0e162284193014c3f8b4970cc
Vladimir Levin <vmpstr@chromium.org>
2022-02-22 16:02:06 +0000

    SET: Add some debugging functionality to render pass printing.
    This patch adds a way to print render passes with dependencies
    in a succinct way. This is under TransitionUtils, because it prioritizes
    printing things with SharedElement quads and dependent render passes.
    
    R=pdr@chromium.org
    
    Change-Id: I1a4f235732a0144676a6f34769a981b0ac019cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473590
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973742}

--- components/viz/common/transition_utils.h
+++ components/viz/common/transition_utils.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_COMMON_TRANSITION_UTILS_H_
 #define COMPONENTS_VIZ_COMMON_TRANSITION_UTILS_H_
 
+#include <memory>
+#include <string>
+
 #include "base/callback_forward.h"
 #include "components/viz/common/quads/compositor_render_pass.h"
 #include "components/viz/common/viz_common_export.h"
@@ -38,6 +41,9 @@ class VIZ_COMMON_EXPORT TransitionUtils {
       const CompositorRenderPassId& id) {
     return CompositorRenderPassId(id.GetUnsafeValue() + 1);
   }
+
+  static std::string RenderPassListToString(
+      const CompositorRenderPassList& render_passes);
 };
 
 }  // namespace viz
