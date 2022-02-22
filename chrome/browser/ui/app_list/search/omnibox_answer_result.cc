a9fddd8a9a3d8a2f68ef21ddb0535b20dac997c0
Rachel Wong <wrong@chromium.org>
2022-02-22 04:14:17 +0000

    [answers] Add dark mode icons for Omnibox answers.
    Dark mode colours should be used if the productivity launcher is
    enabled, or if dark mode is enabled in the classic launcher.
    
    Bug: 1299098
    Change-Id: If2e5a913553e2af0dd33af9dd32bbf0676255e09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3479082
    Reviewed-by: Amanda Deacon <amandadeacon@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973622}

--- chrome/browser/ui/app_list/search/omnibox_answer_result.cc
+++ chrome/browser/ui/app_list/search/omnibox_answer_result.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/app_list/vector_icons/vector_icons.h"
+#include "ash/public/cpp/style/color_provider.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher.h"
@@ -42,9 +43,16 @@ constexpr char kOmniboxAnswerSchema[] = "omnibox_answer://";
 ChromeSearchResult::IconInfo CreateAnswerIconInfo(
     const gfx::VectorIcon& vector_icon) {
   const int dimension = GetAnswerCardIconDimension();
-  const auto icon = gfx::ImageSkiaOperations::CreateImageWithCircleBackground(
-      dimension / 2, gfx::kGoogleBlue600,
-      gfx::CreateVectorIcon(vector_icon, SK_ColorWHITE));
+  const bool dark_mode = ash::features::IsProductivityLauncherEnabled() ||
+                         (ash::features::IsDarkLightModeEnabled() &&
+                          ash::ColorProvider::Get()->IsDarkModeEnabled());
+  const auto icon =
+      dark_mode ? gfx::ImageSkiaOperations::CreateImageWithCircleBackground(
+                      dimension / 2, gfx::kGoogleBlue300,
+                      gfx::CreateVectorIcon(vector_icon, gfx::kGoogleGrey900))
+                : gfx::ImageSkiaOperations::CreateImageWithCircleBackground(
+                      dimension / 2, gfx::kGoogleBlue600,
+                      gfx::CreateVectorIcon(vector_icon, SK_ColorWHITE));
   return ChromeSearchResult::IconInfo(icon, dimension);
 }
 
