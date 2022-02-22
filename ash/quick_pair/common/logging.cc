1feb01f9db06114e1356390911454965ebd1499a
Juliet Levesque <julietlevesque@google.com>
2022-02-22 21:25:49 +0000

    [Fast Pair] Increase test coverage for Fast Pair logging classes
    Fixed: b/220144347, b/220145770
    Change-Id: I9d5dc75190aa7a575c6538ae86dc16ecd6c59f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3481301
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#973909}

--- ash/quick_pair/common/logging.cc
+++ ash/quick_pair/common/logging.cc
@@ -11,29 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace quick_pair {
 
-namespace {
-
-bool g_logging_enabled = true;
-
-}  // namespace
-
-ScopedDisableLoggingForTesting::ScopedDisableLoggingForTesting() {
-  g_logging_enabled = false;
-}
-
-ScopedDisableLoggingForTesting::~ScopedDisableLoggingForTesting() {
-  g_logging_enabled = true;
-}
-
 ScopedLogMessage::ScopedLogMessage(const char* file,
                                    int line,
                                    logging::LogSeverity severity)
     : file_(file), line_(line), severity_(severity) {}
 
 ScopedLogMessage::~ScopedLogMessage() {
-  if (!g_logging_enabled)
-    return;
-
   const std::string string_from_stream = stream_.str();
   LogBuffer::GetInstance()->AddLogMessage(LogBuffer::LogMessage(
       string_from_stream, base::Time::Now(), file_, line_, severity_));
