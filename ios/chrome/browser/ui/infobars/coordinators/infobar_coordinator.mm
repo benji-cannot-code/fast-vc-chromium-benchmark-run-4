497f31b657fe327acc076b4345abe8752aa46012
Sylvain Defresne <sdefresne@chromium.org>
2022-02-22 10:10:42 +0000

    [ios] Fix AnimatedScopedFullscreenDisabler
    FullscreenController is a base::SupportsUserData::Data attached to a
    Browser, like the owner of AnimatedScopedFullscreenDisabler. There is
    no guarantee in which order the base::SupportsUserData::Data are to
    be destroyed.
    
    Observe FullscreenController in AnimatedScopedFullscreenDisabler (as
    it can notify that it will be destroyed) and set `controller_` to
    null when this happen (this likely mean that the application is to
    be destroyed).
    
    Also use base::WeakPtr<AnimatedScopedFullscreenDisabler> to prevent the
    blocks passed to -[UIView animateWithDuration:animation:completion:]
    from capturing pointers to C++ objects (can lead to use-after-free).
    
    Bug: 1241310
    Change-Id: I6cf37599af0b9861d10c6414cf5a55e9f4ad8adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3478034
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973661}

--- ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator.mm
@@ -98,9 +98,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   // Make sure to display the Toolbar/s before presenting the Banner.
-    _animatedFullscreenDisabler =
-        std::make_unique<AnimatedScopedFullscreenDisabler>(
-            FullscreenController::FromBrowser(self.browser));
+  _animatedFullscreenDisabler =
+      std::make_unique<AnimatedScopedFullscreenDisabler>(
+          FullscreenController::FromBrowser(self.browser));
   _animatedFullscreenDisabler->StartAnimation();
 
   [self.bannerViewController
