1f53ec209263e3dfe7d5d41a05e54eb95401dc38
Mihai Sardarescu <msarda@google.com>
2022-02-22 20:34:54 +0000

    Change subtitle for the "Import/Merge data dialog"
    This CL changes the subtitle of the import / merge data dialog to:
    "<user_email> was using Chrome and left bookmarks, passwords, and other
    settings on this device."
    
    Screenshot:
    https://drive.google.com/file/d/1pEx8DnTwUAc9hQPJdeiTWPPcHFht-5qQ/view?usp=sharing
    
    Bug: 1287055
    Change-Id: Ia9a6ffbfb08cdad3cec9576c4d8173b7ff3d9904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3477078
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Auto-Submit: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973891}

--- ios/chrome/browser/ui/settings/import_data_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/import_data_table_view_controller_unittest.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/cells/settings_image_detail_text_item.h"
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_controller_test.h"
 #import "ios/chrome/browser/ui/table_view/table_view_model.h"
+#include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
