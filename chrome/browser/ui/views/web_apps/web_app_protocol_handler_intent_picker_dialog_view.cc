ddbf234ee164c113d2de901b2fb90e288dcaa5ed
Ben Mason <benmason@chromium.org>
2022-02-22 19:53:11 +0000

    Revert "dPWA: File Handling - tweak cosmetics of launch dialog"
    This reverts commit 9d90b9eabe99cd147f84de54528765916971cbb6.
    
    Reason for revert: crbug.com/1298613
    
    Original change's description:
    > dPWA: File Handling - tweak cosmetics of launch dialog
    >
    > This also moves the string for the protocol handling PWA
    > dialog back to //chrome/ (it was mistakenly relocated along
    > with registerProtocolHandler related strings).
    >
    > Bug: 1277588
    > Change-Id: I914a07adb7a074143e8b2bcec04d0cab99ceeef6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449096
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#971039}
    
    Bug: 1277588, 1298613
    Change-Id: Ia89481aa4bbe48e93d4269c60bddc00737fbd464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3480918
    Owners-Override: Ben Mason <benmason@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973867}

--- chrome/browser/ui/views/web_apps/web_app_protocol_handler_intent_picker_dialog_view.cc
+++ chrome/browser/ui/views/web_apps/web_app_protocol_handler_intent_picker_dialog_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/common/custom_handlers/protocol_handler.h"
@@ -31,12 +32,6 @@ WebAppProtocolHandlerIntentPickerView::WebAppProtocolHandlerIntentPickerView(
       views::DialogContentType::kText, views::DialogContentType::kControl));
   set_fixed_width(layout_provider->GetDistanceMetric(
       views::DISTANCE_BUBBLE_PREFERRED_WIDTH));
-  SetButtonLabel(ui::DIALOG_BUTTON_OK,
-                 l10n_util::GetStringUTF16(IDS_PERMISSION_ALLOW));
-  SetButtonLabel(
-      ui::DIALOG_BUTTON_CANCEL,
-      l10n_util::GetStringUTF16(IDS_WEB_APP_PERMISSION_NEGATIVE_BUTTON));
-  SetDefaultButton(ui::DIALOG_BUTTON_CANCEL);
 }
 
 WebAppProtocolHandlerIntentPickerView::
