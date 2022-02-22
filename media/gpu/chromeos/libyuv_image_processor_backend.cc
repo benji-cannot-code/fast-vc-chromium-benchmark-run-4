87cda51b8194af9b496c838d2349e81b13438548
Justin Green <greenjustin@google.com>
2022-02-22 19:44:13 +0000

    media/gpu/chromeos: Adding tracing to libyuv image processor
    Add a trace even to the LibYUVImageProcessorBackend's Process() method
    to gather performance metrics.
    
    Bug: b:217466767
    Change-Id: I48dcb7846cdfae1b21efa34f338236903a5e15a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472505
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Justin Green <greenjustin@google.com>
    Cr-Commit-Position: refs/heads/main@{#973860}

--- media/gpu/chromeos/libyuv_image_processor_backend.cc
+++ media/gpu/chromeos/libyuv_image_processor_backend.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/contains.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/numerics/checked_math.h"
+#include "base/trace_event/trace_event.h"
 #include "media/gpu/chromeos/fourcc.h"
 #include "media/gpu/macros.h"
 #include "media/gpu/video_frame_mapper.h"
@@ -347,7 +349,14 @@ void LibYUVImageProcessorBackend::Process(
       return;
     }
   }
-  int res = DoConversion(input_frame.get(), mapped_frame.get());
+
+  int res;
+  {
+    TRACE_EVENT0("media", "LibYUVImageProcessorBackend::Process");
+    SCOPED_UMA_HISTOGRAM_TIMER("LibYUVImageProcessorBackend::Process");
+    res = DoConversion(input_frame.get(), mapped_frame.get());
+  }
+
   if (res != 0) {
     VLOGF(1) << "libyuv returns non-zero code: " << res;
     error_cb_.Run();
