01e287cb245298268d3d2c956f7b00a18d11b16f
Joemer Ramos <joemerramos@chromium.org>
2022-02-22 15:31:44 +0000

    [iOS] Changes to CRWWebViewContentView for fullscreen API bug fix
    In testing the fullscreen API introduced in iOS 15.4, I found an issue
    where web content glitches after orientating a device. In order
    to implement the fix without having to cast a UIView into a WKWebView
    in another implementation file, I have modified the interface so that
    we have proper access to instance properties in a WKWebView.
    
    Bug: 1296706
    Change-Id: Ica97297d3782c8de87287e35cb4e9cd0dd6ab5f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3462690
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#973728}

--- ios/web/web_state/ui/crw_web_controller_container_view.mm
+++ ios/web/web_state/ui/crw_web_controller_container_view.mm
@@ -150,6 +150,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self setNeedsLayout];
 }
 
+#if defined(__IPHONE_15_4) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_15_4
+- (void)updateWebViewContentViewFullscreenState:
+    (WKFullscreenState)fullscreenState {
+  DCHECK(_webViewContentView);
+  if (@available(iOS 15.4, *)) {
+    [self.webViewContentView updateFullscreenState:fullscreenState];
+  }
+}
+#endif  // defined(__IPHONE_15_4)
+
 #pragma mark UIView (printing)
 
 // Only print the web view by returning the web view printformatter.
