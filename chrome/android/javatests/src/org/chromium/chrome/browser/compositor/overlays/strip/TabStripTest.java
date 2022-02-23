600992956fd367d04ee5ead4334a4eaf729e3733
Matthew Jones <mdjones@chromium.org>
2022-02-23 16:55:09 +0000

    Revert "[Sheriff] Disable failing TabStripTest tests"
    This reverts commit 83f07760cadb0b19b1c30fa08c41ba1e79e8da6f.
    
    Reason for revert: "Reenabling" these tests since the flakes
    should be fixed by https://crrev.com/c/3471723
    
    Original change's description:
    > [Sheriff] Disable failing TabStripTest tests
    >
    > Disables flaky testStripScrimOnHidingOverviewMode and
    > testStripScrimOnShowingOverviewMode.
    >
    > Bug: 1299415
    > Change-Id: Idb9ebe118649fa63de6a6ed8e1bc10020864b958
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3477157
    > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > Owners-Override: Boris Sazonov <bsazonov@chromium.org>
    > Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > Commit-Queue: Alex Ilin <alexilin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#973515}
    
    Bug: 1299415
    Change-Id: I9a62d3685df4532bb1ddeb1999cffc4a8d095099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3484138
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974219}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripTest.java
@@ -23,7 +23,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.compositor.layouts.components.CompositorButton;
@@ -864,7 +863,6 @@ public class TabStripTest {
      */
     @Test
     @LargeTest
-    @DisabledTest(message = "https://crbug.com/1299415")
     @Restriction(UiRestriction.RESTRICTION_TYPE_TABLET)
     @Feature({"TabStrip"})
     @Features.EnableFeatures(ChromeFeatureList.GRID_TAB_SWITCHER_FOR_TABLETS)
@@ -897,7 +895,6 @@ public class TabStripTest {
      */
     @Test
     @LargeTest
-    @DisabledTest(message = "https://crbug.com/1299415")
     @Restriction(UiRestriction.RESTRICTION_TYPE_TABLET)
     @Feature({"TabStrip"})
     @Features.EnableFeatures(ChromeFeatureList.GRID_TAB_SWITCHER_FOR_TABLETS)
