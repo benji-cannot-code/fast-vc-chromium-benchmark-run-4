25ad5e906b1b8476006cfff48c4af9d94711119d
Ghazale Hosseinabadi <ghazale@google.com>
2022-02-23 02:10:41 +0000

    [Extensions] developer_mode_only api tests
    This CL implements api tests to verify restricting API access
    controlled by developer_mode_only.
    
    Bug: 1267489
    Change-Id: Ie9a4c6c84efee42e442b4b07ea507e1d8aceae49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456206
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974022}

---
+++ chrome/test/data/extensions/api_test/native_bindings/developer_mode_only_with_api_permission/service_worker_background.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  var inDeveloperMode = config.customArg == 'in_developer_mode';
+
+  chrome.test.runTests([
+    function testDebuggerApiAccess() {
+      if (inDeveloperMode) {
+        chrome.test.assertTrue(!!chrome.debugger);
+        chrome.test.assertTrue(!!chrome.debugger.getTargets);
+      } else {
+        var expectedError = 'The \'debugger\' API is only '
+                          + 'available for users in developer mode.';
+        var functionThatThrows = function() {
+          chrome.debugger;
+        };
+        chrome.test.assertThrows(functionThatThrows, [], expectedError);
+      }
+      chrome.test.succeed();
+    }
+  ]);
+});
---
+++ chrome/test/data/extensions/api_test/native_bindings/developer_mode_only_without_api_permission/service_worker_background.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function testGetTargetsThrows() {
+    chrome.test.assertEq(undefined, chrome.debugger);
+    chrome.test.succeed();
+  }
+]);
