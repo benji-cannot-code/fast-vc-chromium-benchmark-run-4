512ed38ca81e0ec4fb9c33b791aa4b935e510cfa
Sophey <sophey@chromium.org>
2022-02-23 17:41:46 +0000

    [LR] Stop focusing on the scene background.
    Currently Talkback tries to read the text in the screenshot. This
    prevents that.
    
    Fixed: 1299900
    Change-Id: Id05549244f9e72d786689d31664f9aee8a2fec06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482295
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974231}

--- chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsDialog.java
+++ chrome/browser/content_creation/reactions/internal/android/java/src/org/chromium/chrome/browser/content_creation/reactions/LightweightReactionsDialog.java
@@ -83,5 +83,6 @@ public class LightweightReactionsDialog extends DialogFragment {
                 mContentView.findViewById(R.id.lightweight_reactions_background);
         Drawable background = new BitmapDrawable(getResources(), mScreenshot);
         sceneBackground.setImageDrawable(background);
+        sceneBackground.setFocusable(false);
     }
 }
