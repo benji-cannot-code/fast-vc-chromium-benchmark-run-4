00f25f52488272c3182622040d563ae9c7fb9520
Robert Ogden <robertogden@chromium.org>
2022-02-23 23:50:22 +0000

    Implement the Override List for Page Topics Annotations
    Looks for a gzipped, binary protobuf file that contains a lookup table
    for PageTopics model executions, short-cutting the model execution
    entirely if the given domain is in the lookup table.
    
    The override list is loaded when the model file would also expect to be
    loaded, and unloaded at the same time.
    
    See bug for design doc.
    
    Bug: 1295766
    Change-Id: I1ffc19c0f86be8993f3e692ad4383cb71bbdeb6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469662
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974400}

--- components/optimization_guide/core/page_topics_model_executor.h
+++ components/optimization_guide/core/page_topics_model_executor.h
@@ -6,8 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_PAGE_TOPICS_MODEL_EXECUTOR_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CORE_PAGE_TOPICS_MODEL_EXECUTOR_H_
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include "base/callback.h"
+#include "base/files/file_path.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "components/optimization_guide/core/bert_model_handler.h"
 #include "components/optimization_guide/core/page_content_annotation_job.h"
 #include "components/optimization_guide/core/page_content_annotation_job_executor.h"
@@ -16,7 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace optimization_guide {
 
-// A BERT-based mode executor for page topics annotations.
+// A BERT-based mode executor for page topics annotations. All the derived
+// functionality of this class is exclusive to the UI thread, but may post
+// things to the background task runner.
 class PageTopicsModelExecutor : public PageContentAnnotationJobExecutor,
                                 public BertModelHandler {
  public:
@@ -27,10 +35,17 @@ class PageTopicsModelExecutor : public PageContentAnnotationJobExecutor,
   ~PageTopicsModelExecutor() override;
 
   // PageContentAnnotationJobExecutor:
+  void ExecuteJob(base::OnceClosure on_job_complete_callback,
+                  std::unique_ptr<PageContentAnnotationJob> job) override;
   void ExecuteOnSingleInput(
       AnnotationType annotation_type,
       const std::string& input,
       base::OnceCallback<void(const BatchAnnotationResult&)> callback) override;
+
+  // BertModelHandler:
+  void UnloadModel() override;
+  void OnModelUpdated(proto::OptimizationTarget optimization_target,
+                      const ModelInfo& model_info) override;
 
   // Creates a BatchAnnotationResult from the output of the model, calling
   // |ExtractCategoriesFromModelOutput| in the process.
@@ -48,6 +63,30 @@ class PageTopicsModelExecutor : public PageContentAnnotationJobExecutor,
       const std::vector<tflite::task::core::Category>& model_output) const;
 
  private:
+  void OnOverrideListLoadAttemptDone(
+      base::OnceClosure on_job_complete_callback,
+      std::unique_ptr<PageContentAnnotationJob> job,
+      absl::optional<
+          std::unordered_map<std::string, std::vector<WeightedIdentifier>>>
+          override_list);
+
+  scoped_refptr<base::SequencedTaskRunner> background_task_runner_;
+
+  // Set whenever a valid override list file is passed along with the model file
+  // update. This will be reset if the provided file is deemed invalid on the
+  // first attempted load.
+  // Used on the UI thread.
+  absl::optional<base::FilePath> override_list_file_path_;
+
+  // Set whenever an override list file is available and the model file is
+  // loaded into memory. Reset whenever the model file is unloaded.
+  // Used on the UI thread.
+  absl::optional<
+      std::unordered_map<std::string, std::vector<WeightedIdentifier>>>
+      override_list_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
   base::WeakPtrFactory<PageTopicsModelExecutor> weak_ptr_factory_{this};
 };
 
