66c2e3213c50e39115b40ea694fd52f855a00f20
Eugene Zemtsov <eugene@chromium.org>
2022-02-23 09:41:24 +0000

    webcodecs: Properly handle failure to create decoder source in GPU tests
    Also:
    - proper disposal of frame sources
    - WebCodecs_EncodingModes_offscreen uses 'offscreen' source now as
      suggested by the name.
    
    Bug: 1291374
    Change-Id: Iecc6e227ebeee8049893311793fa5ee6919a0af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482720
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974103}

--- content/test/data/gpu/webcodecs/webcodecs_common.js
+++ content/test/data/gpu/webcodecs/webcodecs_common.js
@@ -176,6 +176,8 @@ class FrameSource {
   async getNextFrame() {
     return null;
   }
+
+  close() {}
 }
 
 // Source of video frames coming from taking snapshots of a canvas.
@@ -214,6 +216,11 @@ class StreamSource extends FrameSource {
     const result = await this.reader.read();
     const frame = result.value;
     return frame;
+  }
+
+  close() {
+    if (this.reader)
+      this.reader.cancel();
   }
 }
 
@@ -294,6 +301,11 @@ class DecoderSource extends FrameSource {
 
     return next.promise;
   }
+
+  close() {
+    if (this.decoder)
+      this.decoder.close();
+  }
 }
 
 function createCanvasCaptureSource(width, height) {
@@ -368,7 +380,15 @@ async function prepareDecoderSource(
     encoder.encode(frame, {keyFrame: false});
     frame.close();
   }
-  await encoder.flush();
+  try {
+    await encoder.flush();
+    encoder.close();
+    canvasSource.close();
+  } catch (e) {
+    errors++;
+    TEST.log(e);
+  }
+
   if (errors > 0)
     return null;
 
