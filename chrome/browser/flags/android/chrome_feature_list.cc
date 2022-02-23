85fabf5fd6eb60179fa47a04e7baac16b71ae3ac
Ender <ender@google.com>
2022-02-23 19:26:25 +0000

    [Widget] Clean up Widget feature flags.
    Do not submit before 2/17 / while holdback experiment is running.
    Bug: 1241926
    
    Change-Id: Ie8153bc487531bf8d8425d93b28d3a6a5f8440a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3415640
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#974272}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -190,7 +190,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTResizableForFirstParties,
     &kCCTResizableForThirdParties,
     &kCCTResourcePrefetch,
-    &kQuickActionSearchWidgetAndroidDinoVariant,
     &kDontAutoHideBrowserControls,
     &kChromeNewDownloadTab,
     &kChromeShareLongScreenshot,
@@ -251,7 +250,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kBookmarksImprovedSaveFlow,
     &kBookmarksRefresh,
     &kProbabilisticCryptidRenderer,
-    &kQuickActionSearchWidgetAndroid,
     &kReachedCodeProfiler,
     &kImproveReaderModePrompt,
     &kReaderModeInCCT,
@@ -596,10 +594,6 @@ const base::Feature kContextualTriggersSelectionMenu{
 const base::Feature kContextualTriggersSelectionSize{
     "ContextualTriggersSelectionSize", base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kQuickActionSearchWidgetAndroidDinoVariant{
-    "QuickActionSearchWidgetAndroidDinoVariant",
-    base::FEATURE_ENABLED_BY_DEFAULT};
-
 const base::Feature kDirectActions{"DirectActions",
                                    base::FEATURE_ENABLED_BY_DEFAULT};
 
@@ -710,9 +704,6 @@ const base::Feature kBookmarksRefresh{"BookmarksRefresh",
 
 const base::Feature kProbabilisticCryptidRenderer{
     "ProbabilisticCryptidRenderer", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kQuickActionSearchWidgetAndroid{
-    "QuickActionSearchWidgetAndroid", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kReachedCodeProfiler{"ReachedCodeProfiler",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
