78e85ae0376277aced7e84e71788de50ba25919c
Caitlin Fischer <caitlinfischer@google.com>
2022-02-23 20:59:24 +0000

    Schedule a Local State write after updating the GPU crash count.
    Bug: 1300240, b/211786119
    Change-Id: Ib105c8f9252211dd990c113cf1acc3798107cc7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3484610
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Auto-Submit: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974322}

--- components/metrics/stability_metrics_helper.cc
+++ components/metrics/stability_metrics_helper.cc
@@ -168,6 +168,7 @@ void StabilityMetricsHelper::IncreaseRendererCrashCount() {
 
 void StabilityMetricsHelper::IncreaseGpuCrashCount() {
   IncrementPrefValue(prefs::kStabilityGpuCrashCount);
+  local_state_->CommitPendingWrite();  // Schedule a Local State write.
   RecordStabilityEvent(StabilityEventType::kGpuCrash);
 }
 
