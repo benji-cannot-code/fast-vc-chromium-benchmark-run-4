61dcffec04526f9659f508d51d81463ecbf627eb
Kevin Lubick <kjlubick@chromium.org>
2022-02-23 21:37:55 +0000

    Add include of SkRRect to appropriate places
    We aim to remove the include from SkPathRef in
    https://skia-review.googlesource.com/c/skia/+/512158
    
    Change-Id: I1abcfedd27f24f50573111278b467fd5115d5ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485346
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974335}

--- cc/paint/paint_op_buffer.h
+++ cc/paint/paint_op_buffer.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRect.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
+#include "third_party/skia/include/core/SkRRect.h"
 #include "third_party/skia/include/core/SkScalar.h"
 #include "ui/gfx/geometry/rect.h"
 
