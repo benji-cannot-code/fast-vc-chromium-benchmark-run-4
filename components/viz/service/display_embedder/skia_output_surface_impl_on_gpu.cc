56dbb4e4edb038904245166e399bfbd544e9810a
kylechar <kylechar@chromium.org>
2022-02-23 19:31:18 +0000

    Fix primary plane DCHECK on skipped swap
    Reset primary plane information set on SkiaOutputSurfaceImplOnGpu on
    skipped swap too. The next display frame will set the primary plane
    information again anyways, since it might have changed, but this
    triggered a DCHECK in ScheduleOutputSurfaceAsOverlay() that the primary
    plane is only set once per display frame.
    
    Bug: 1297666
    Change-Id: I4e7f96f8e00610a95ffee449aad4d146d5679b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485239
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974273}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -1815,7 +1815,6 @@ void SkiaOutputSurfaceImplOnGpu::PostSubmit(
     }
 
     output_device_->SchedulePrimaryPlane(output_surface_plane_);
-    output_surface_plane_.reset();
 
     if (frame->sub_buffer_rect) {
       if (capabilities().supports_post_sub_buffer) {
@@ -1836,6 +1835,9 @@ void SkiaOutputSurfaceImplOnGpu::PostSubmit(
                                   std::move(*frame));
     }
   }
+
+  // Reset the primary plane information even on skipped swap.
+  output_surface_plane_.reset();
 
   destroy_after_swap_.clear();
   context_state_->UpdateSkiaOwnedMemorySize();
