c5a4fdc40648ff749d86501bc64b6980496dcc17
Cici Ruan <cuicuiruan@google.com>
2022-02-23 00:43:21 +0000

    arc: implement the game control toggle and save status
    - Implement the logic when toggling game control. When gaming control
    (touch injector) is disabled, the input mapping toggle is disabled too,
    which means it doesn't show the input mapping. When gaming control is
    enabled, the input mapping becomes visible again.
    
    - Currently the menu toggle status is not saved if the window is
    unfocused or minimized because display overlay is destroyed. Save the
    status in the TouchInjector.
    
    Bug: b:188452744
    Test: manual test when switching the toggles and changing window focus.
    Change-Id: I1b45130e0e6642087bc6c210a29eca3f5e82a1a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473423
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Auto-Submit: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#973994}

--- chrome/browser/ash/arc/input_overlay/touch_injector.cc
+++ chrome/browser/ash/arc/input_overlay/touch_injector.cc
@@ -279,9 +279,6 @@ ui::EventDispatchDetails TouchInjector::RewriteEvent(
   if (text_input_active_)
     return SendEvent(continuation, &event);
 
-  if (switch_mode_ && switch_mode_->Process(event))
-    return DiscardEvent(continuation);
-
   if (display_mode_ != DisplayMode::kView)
     return SendEvent(continuation, &event);
 
@@ -298,6 +295,9 @@ ui::EventDispatchDetails TouchInjector::RewriteEvent(
     display_overlay_controller_->SetDisplayMode(DisplayMode::kMenu);
     return SendEvent(continuation, &event);
   }
+
+  if (!touch_injector_enable_)
+    return SendEvent(continuation, &event);
 
   if (mouse_lock_ && mouse_lock_->Process(event))
     return DiscardEvent(continuation);
