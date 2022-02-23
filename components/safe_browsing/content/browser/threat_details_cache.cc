a718934e039eb1db1fce329c489d02affc7e7577
Daniel Rubery <drubery@chromium.org>
2022-02-23 05:06:54 +0000

    Use WeakPtr and unique_ptr for ownership in ThreatDetails
    Currently we use scoped_refptr for the ThreatDetailsCacheCollector,
    which holds pointers into resources owned by ThreatDetails. When the
    ThreatDetails is destroyed, it's intended that the
    ThreatDetailsCacheCollector is destroyed with it, but that does not
    occur if there is a pending task with a reference to the
    ThreatDetailsCacheCollector.
    
    By having the ThreatDeails hold a unique_ptr, we can ensure that
    destruction happens as planned.
    
    Bug: 1297498
    Change-Id: I5f14a33d56a86c271b249534ee7410f4045f4f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482677
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974062}

--- components/safe_browsing/content/browser/threat_details_cache.cc
+++ components/safe_browsing/content/browser/threat_details_cache.cc
@@ -55,7 +55,8 @@ void ThreatDetailsCacheCollector::StartCacheCollection(
   // Post a task in the message loop, so the callers don't need to
   // check if we call their callback immediately.
   content::GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE, base::BindOnce(&ThreatDetailsCacheCollector::OpenEntry, this));
+      FROM_HERE, base::BindOnce(&ThreatDetailsCacheCollector::OpenEntry,
+                                weak_factory_.GetWeakPtr()));
 }
 
 bool ThreatDetailsCacheCollector::HasStarted() {
@@ -121,6 +122,7 @@ void ThreatDetailsCacheCollector::OpenEntry() {
   current_load_->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
       url_loader_factory_.get(),
       base::BindOnce(&ThreatDetailsCacheCollector::OnURLLoaderComplete,
+                     // This is safe because `current_load_` is owned by `this`.
                      base::Unretained(this)));
 }
 
@@ -230,7 +232,8 @@ void ThreatDetailsCacheCollector::AdvanceEntry() {
 
   // Create a task so we don't take over the UI thread for too long.
   content::GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE, base::BindOnce(&ThreatDetailsCacheCollector::OpenEntry, this));
+      FROM_HERE, base::BindOnce(&ThreatDetailsCacheCollector::OpenEntry,
+                                weak_factory_.GetWeakPtr()));
 }
 
 void ThreatDetailsCacheCollector::AllDone(bool success) {
