066ffd0a4b9ccaee4a4ca1d5229dc5d6fe774817
dpapad <dpapad@chromium.org>
2022-02-23 04:29:34 +0000

    Migrate chrome://connection-help to JS Modules.
     - Fixes runtime error thrown (cr.js no longer exists on Desktop
       builds), see [1].
     - Fixes deprecation warnings, see [1].
     - Adds JS type checking for connection_help.js.
    
    [1] https://i.imgur.com/ZffY1S1.png
    
    Bug: 1173575
    Change-Id: Id4bb1a4d6eafc3707a6edb3a88d5f38723ca4a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3470193
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974053}

--- components/security_interstitials/content/resources/connection_help.js
+++ components/security_interstitials/content/resources/connection_help.js
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './strings.m.js';
+
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+import {$} from 'chrome://resources/js/util.m.js';
+
 const HIDDEN_CLASS = 'hidden';
 
 function setupEvents() {
