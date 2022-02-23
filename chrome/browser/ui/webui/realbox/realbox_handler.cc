a14f5625de71065e34c28d2ee0a6b08933ec0db1
Marlon Facey <mfacey@chromium.org>
2022-02-23 22:28:22 +0000

    [realbox] Add icons for batch 3 pedals in realbox
    The icon for the batch 3 pedals were added to the realbox. This also
    enables previously disabled tests due to the pedals batch 3 icons
    missing from the realbox.
    
    Before: screenshot/5vtbsQqXytkbcTA
    After:
    Dino Game: screenshot/7WGwhaWR6vykanW
    MacOS Share: screenshot/8V8rzmWvwkUzmsH
    
    
    Bug: 1287340
    Change-Id: Ib64e1bfb412e58cbe67bb6c6c8914789a83c3afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3444506
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974366}

--- chrome/browser/ui/webui/realbox/realbox_handler.cc
+++ chrome/browser/ui/webui/realbox/realbox_handler.cc
@@ -81,6 +81,7 @@ constexpr char kBookmarkIconResourceName[] =
 constexpr char kCalculatorIconResourceName[] = "realbox/icons/calculator.svg";
 constexpr char kClockIconResourceName[] =
     "chrome://resources/images/icon_clock.svg";
+constexpr char kDinoIconResourceName[] = "realbox/icons/dino.svg";
 constexpr char kDriveDocsIconResourceName[] = "realbox/icons/drive_docs.svg";
 constexpr char kDriveFolderIconResourceName[] =
     "realbox/icons/drive_folder.svg";
@@ -101,10 +102,21 @@ constexpr char kGoogleCalendarIconResourceName[] = "realbox/icons/calendar.svg";
 constexpr char kGoogleKeepNoteIconResourceName[] = "realbox/icons/note.svg";
 constexpr char kGoogleSitesIconResourceName[] = "realbox/icons/sites.svg";
 #endif
+constexpr char kIncognitoIconResourceName[] = "realbox/icons/incognito.svg";
 constexpr char kPageIconResourceName[] = "realbox/icons/page.svg";
 constexpr char kPedalsIconResourceName[] =
     "chrome://theme/current-channel-logo";
 constexpr char kTrendingUpIconResourceName[] = "realbox/icons/trending_up.svg";
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+constexpr char kCrosShareIconResourceName[] = "realbox/icons/cros_share.svg";
+#elif BUILDFLAG(IS_MAC)
+constexpr char kMacShareIconResourceName[] = "realbox/icons/mac_share.svg";
+#elif BUILDFLAG(IS_WIN)
+constexpr char kWinShareIconResourceName[] = "realbox/icons/win_share.svg";
+#else
+constexpr char kLinuxShareIconResourceName[] = "realbox/icons/linux_share.svg";
+#endif
 
 base::flat_map<int32_t, realbox::mojom::SuggestionGroupPtr>
 CreateSuggestionGroupsMap(
@@ -386,6 +398,9 @@ std::string RealboxHandler::AutocompleteMatchVectorIconToResourceName(
 // static
 std::string RealboxHandler::PedalVectorIconToResourceName(
     const gfx::VectorIcon& icon) {
+  if (icon.name == omnibox::kDinoIcon.name) {
+    return kDinoIconResourceName;
+  }
   if (icon.name == omnibox::kDriveFormsIcon.name) {
     return kDriveFormIconResourceName;
   }
@@ -412,9 +427,29 @@ std::string RealboxHandler::PedalVectorIconToResourceName(
     return kGoogleGIconResourceName;
   }
 #endif
+  if (icon.name == omnibox::kIncognitoIcon.name) {
+    return kIncognitoIconResourceName;
+  }
   if (icon.name == omnibox::kPedalIcon.name) {
     return kPedalsIconResourceName;
   }
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (icon.name == omnibox::kShareIcon.name) {
+    return kCrosShareIconResourceName;
+  }
+#elif BUILDFLAG(IS_MAC)
+  if (icon.name == omnibox::kShareMacIcon.name) {
+    return kMacShareIconResourceName;
+  }
+#elif BUILDFLAG(IS_WIN)
+  if (icon.name == omnibox::kShareWinIcon.name) {
+    return kWinShareIconResourceName;
+  }
+#else
+  if (icon.name == omnibox::kSendIcon.name) {
+    return kLinuxShareIconResourceName;
+  }
+#endif
   NOTREACHED() << "Every vector icon returned by OmniboxAction::GetVectorIcon "
                   "must have an equivalent SVG resource for the NTP Realbox.";
   return "";
