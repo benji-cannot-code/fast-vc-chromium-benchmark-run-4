c52a3eb9d3e4bdac3ff72d4c3fbbbc56cd433c9b
Dave Tapuska <dtapuska@chromium.org>
2022-02-23 22:01:52 +0000

    Export new fields for Declarative WebRequest API.
    Add parentDocumentId, frameType, documentLifecycle to the Declarative
    WebRequest message event.
    
    See https://bit.ly/3G4RBEn for discussion regarding this change.
    
    BUG=1264911
    
    Change-Id: I766d7db262fe540659e07182f4578738a43f12e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3440461
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974356}

--- chrome/test/data/extensions/api_test/webrequest_sendmessage/background.js
+++ chrome/test/data/extensions/api_test/webrequest_sendmessage/background.js
@@ -44,6 +44,10 @@ runTests([
         chrome.test.fail("Invalid message: " + details.message);
       }
       chrome.test.assertEq(getURLHttpSimpleLoad(), details.url);
+      chrome.test.assertEq('outermost_frame', details.frameType);
+      chrome.test.assertEq('active', details.documentLifecycle);
+      chrome.test.assertFalse('parentDocumentId' in details);
+      chrome.test.assertFalse('documentId' in details);
       var messageKey = details.stage + "-" + details.message;
       if (messageKey in expectedEvents) {
         delete expectedEvents[messageKey];
@@ -72,4 +76,3 @@ runTests([
     });
   }
 ]);
-
