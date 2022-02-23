cd94088833e7a5b0cb0d2f11274608fdfd5a1222
Pi-Hsun Shih <pihsun@chromium.org>
2022-02-23 11:05:34 +0000

    CCA: ESLint: Add @typescript-eslint/consistent-type-definitions
    This covers go/tsstyle#interfaces-vs-type-aliases.
    
    Bug: b:211723843
    Test: cca.py lint
    Change-Id: I1ef7b5be362a7ab80d8bca4e38ee0512e1ea88e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3361665
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974114}

--- ash/webui/camera_app_ui/resources/.eslintrc.js
+++ ash/webui/camera_app_ui/resources/.eslintrc.js
@@ -497,6 +497,9 @@ module.exports = {
         objectLiteralTypeAssertions: 'never',
       },
     ],
+
+    // go/tsstyle#interfaces-vs-type-aliases
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
   }),
   'overrides': [{
     files: ['**/*.ts'],
