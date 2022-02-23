2c960c1ea0fc111b1fc0ec9a76ca5160ddf3a046
Gaurav Jayasawal <gauravjj@google.com>
2022-02-23 00:15:24 +0000

    Apply dynamic colors to New Tab button
    This CL changes the New Tab button from a CompositorButton to a
    TintedCompositorButton to allow applying dynamic colors.
    
    Bug: 1297175
    Change-Id: I3417f06c5f29d3ee023cc992a766cb8b3709e261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3474173
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Gaurav Jayasawal <gauravjj@google.com>
    Cr-Commit-Position: refs/heads/main@{#973979}

--- chrome/browser/android/compositor/scene_layer/tab_strip_scene_layer.h
+++ chrome/browser/android/compositor/scene_layer/tab_strip_scene_layer.h
@@ -76,6 +76,8 @@ class TabStripSceneLayer : public SceneLayer {
       jfloat width,
       jfloat height,
       jboolean visible,
+      jint tint,
+      jfloat button_alpha,
       const base::android::JavaParamRef<jobject>& jresource_manager);
 
   void UpdateModelSelectorButton(
