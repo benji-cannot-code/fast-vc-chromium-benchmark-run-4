d31fdb02e56fcf6a362ccc5e7343163265fec6f6
Gabriel Charette <gab@chromium.org>
2022-02-23 22:07:59 +0000

    Re-enable CodecReclamation test
    The underlying issue crbug.com/846380 was fixed.
    
    Bug: 846380
    Fixed: 1247967
    
    Change-Id: Ica5b05619c51c01e0c91f6d7cf3b48cf6627ee25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3481308
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974359}

--- third_party/blink/renderer/modules/webcodecs/decoder_template_test.cc
+++ third_party/blink/renderer/modules/webcodecs/decoder_template_test.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/build_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_function.h"
@@ -150,14 +149,8 @@ TYPED_TEST(DecoderTemplateTest, ResetDuringFlush) {
   }
 }
 
-#if BUILDFLAG(IS_LINUX) && defined(THREAD_SANITIZER)
-// https://crbug.com/1247967
-#define MAYBE_NoPressureByDefault DISABLED_NoPressureByDefault
-#else
-#define MAYBE_NoPressureByDefault NoPressureByDefault
-#endif
 // Ensures codecs do not apply reclamation pressure by default.
-TYPED_TEST(DecoderTemplateTest, MAYBE_NoPressureByDefault) {
+TYPED_TEST(DecoderTemplateTest, NoPressureByDefault) {
   V8TestingScope v8_scope;
 
   // Create a decoder.
