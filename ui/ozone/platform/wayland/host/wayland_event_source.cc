79b48740e9c475ba79afa1107c493f881e442549
Nick Diego Yamane <nickdiego@igalia.com>
2022-02-24 08:54:19 +0000

    wayland: include keyboard flags in touch events
    Set keyboard flags for modifiers keys in touch events. Without them Ctrl
    + TouchPressing links are not handled correctly by Chrome, for example,
    as described in the linked crbug.
    
    R=msisov@igalia.com
    
    Bug: 1298604
    Test: Covered by ozone_unittests
    Change-Id: I3036040426309efaf09caf241e52c3f513321f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486101
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#974549}

--- ui/ozone/platform/wayland/host/wayland_event_source.cc
+++ ui/ozone/platform/wayland/host/wayland_event_source.cc
@@ -352,7 +352,8 @@ void WaylandEventSource::OnTouchPressEvent(WaylandWindow* window,
   }
 
   PointerDetails details(EventPointerType::kTouch, id);
-  TouchEvent event(ET_TOUCH_PRESSED, loc, loc, timestamp, details);
+  TouchEvent event(ET_TOUCH_PRESSED, loc, loc, timestamp, details,
+                   keyboard_modifiers_);
   DispatchEvent(&event);
 }
 
@@ -369,7 +370,8 @@ void WaylandEventSource::OnTouchReleaseEvent(base::TimeTicks timestamp,
   gfx::PointF location = touch_point->last_known_location;
   PointerDetails details(EventPointerType::kTouch, id);
 
-  TouchEvent event(ET_TOUCH_RELEASED, location, location, timestamp, details);
+  TouchEvent event(ET_TOUCH_RELEASED, location, location, timestamp, details,
+                   keyboard_modifiers_);
   DispatchEvent(&event);
 
   HandleTouchFocusChange(touch_point->window, false, id);
@@ -392,7 +394,8 @@ void WaylandEventSource::OnTouchMotionEvent(const gfx::PointF& location,
           : location;
   it->second->last_known_location = loc;
   PointerDetails details(EventPointerType::kTouch, id);
-  TouchEvent event(ET_TOUCH_MOVED, loc, loc, timestamp, details);
+  TouchEvent event(ET_TOUCH_MOVED, loc, loc, timestamp, details,
+                   keyboard_modifiers_);
   DispatchEvent(&event);
 }
 
