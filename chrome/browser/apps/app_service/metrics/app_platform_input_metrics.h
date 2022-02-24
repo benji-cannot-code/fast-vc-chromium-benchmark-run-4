c1e17bdd1d4824d32d61455a2dcfec5e006e7ecc
Nancy Wang <nancylingwang@chromium.org>
2022-02-24 01:14:36 +0000

    Add 2 hours timer in AppPlatformMetricsService for AppKM report.
    This CL adds 2 hours timer in AppPlatformMetricsService to report the
    app usage time and input event AppKM.
    
    Empty function OnTwoHours is added in AppPlatformMetrics and
    AppPlatformInputMetrics, and the implementation will be added in the
    follow up CLs.
    
    BUG=1299978
    
    Change-Id: I199825f30b7fab480ea9c18097d90476a5b22577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3484656
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974445}

--- chrome/browser/apps/app_service/metrics/app_platform_input_metrics.h
+++ chrome/browser/apps/app_service/metrics/app_platform_input_metrics.h
@@ -55,6 +55,9 @@ class AppPlatformInputMetrics : public ui::EventHandler,
 
   void OnFiveMinutes();
 
+  // Records the input events AppKM each 2 hours.
+  void OnTwoHours();
+
  private:
   struct AppInfo {
     std::string app_id;
