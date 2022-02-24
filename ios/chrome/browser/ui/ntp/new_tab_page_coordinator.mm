0dee0a261b28a40953e7b68177eb168267023a37
Justin Cohen <justincohen@google.com>
2022-02-24 14:18:19 +0000

    ios: Remove UpdateNTPForFeedFix kill switch.
    This was a temporary kill switch added to crrev.com/c/3312118 to make
    cherry picking safer. This can now be removed.
    
    Bug: 1275749
    Change-Id: I33fbd41d44b14be99795c68bb00fa9d8328c8493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487206
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#974624}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -86,11 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Kill switch guarding a fix an NTP/discover memory leak fix. Behind a feature
-// flag so we can validate the impact, as well as safety for a stable respin.
-const base::Feature kUpdateNTPForFeedFix{"UpdateNTPForFeedFix",
-                                         base::FEATURE_ENABLED_BY_DEFAULT};
-
 }  // namespace
 
 @interface NewTabPageCoordinator () <AppStateObserver,
@@ -648,9 +643,7 @@ const base::Feature kUpdateNTPForFeedFix{"UpdateNTPForFeedFix",
 - (void)updateNTPForFeed {
   DCHECK(self.ntpViewController);
 
-  static bool update_ntp_for_feed_fix =
-      base::FeatureList::IsEnabled(kUpdateNTPForFeedFix);
-  if (update_ntp_for_feed_fix && !self.started) {
+  if (!self.started) {
     return;
   }
 
