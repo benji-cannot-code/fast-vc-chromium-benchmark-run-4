6d5ec83958afddeefb119e1804222eb59d9dfa12
David Sanders <dsanders11@ucsbalum.com>
2022-02-24 09:18:49 +0000

    Remove unused include of execution_context.h
    Also adds some includes to other files which were getting
    them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: Icac972c68f70067c1015d53e50a98f6255412a57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486352
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974560}

--- third_party/blink/renderer/core/layout/ng/custom/custom_layout_fragment.cc
+++ third_party/blink/renderer/core/layout/ng/custom/custom_layout_fragment.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/custom/custom_layout_fragment.h"
 
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/layout/ng/custom/custom_layout_child.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 
