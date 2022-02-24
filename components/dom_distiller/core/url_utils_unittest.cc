9f8e9b5e31ac8cba4aaa79276efba9a168dcf8ce
Daniel Cheng <dcheng@chromium.org>
2022-02-24 19:56:53 +0000

    Replace url::Replacements<T> with GURL::Replacements in /components/dom_distiller.
    When clearing URL components, the two interfaces are exactly identical.
    When replacing a URL component, GURL::Replacements has a simpler
    interface.
    
    This CL was uploaded by git cl split.
    
    R=wychen@chromium.org
    
    Bug: 1271865
    Change-Id: I3225fe0bcb4a703b3f295139838f303c49544db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487329
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Commit-Queue: Wei-Yin Chen <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974770}

--- components/dom_distiller/core/url_utils_unittest.cc
+++ components/dom_distiller/core/url_utils_unittest.cc
@@ -48,7 +48,7 @@ TEST(DomDistillerUrlUtilsTest, TestGetValueForKeyInUrlPathQuery) {
 }
 
 void AssertEqualExceptHost(const GURL& a, const GURL& b) {
-  url::Replacements<char> no_host;
+  GURL::Replacements no_host;
   no_host.ClearHost();
   EXPECT_EQ(a.ReplaceComponents(no_host), b.ReplaceComponents(no_host));
 }
