62e54f2ef9906f0bb0df624eebe172976b46c9e9
Alison Maher <almaher@microsoft.com>
2022-02-24 22:10:15 +0000

    [FlexNG] AttemptSoftBreak() for rows
    Add an optional block-size parameter to AttemptSoftBreak() and
    BreakBeforeChild() so that the flex row block-size is used when
    breaking before a row. As a result, AttemptRowSoftBreak() can
    be removed.
    
    As a result of this change, BreakBeforeChild() will now use the
    correct block-size for rows when calling PropagateSpaceShortage(),
    which results in the following test passing as expected:
    external/wpt/css/css-break/flexbox/multi-line-row-flex-fragmentation-038.html
    
    Bug: 660611
    Change-Id: Icaa3ddb4b92298da3ae3e641fe5cc45565048543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486261
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#974855}

--- third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.cc
@@ -1178,7 +1178,7 @@ void NGBlockLayoutAlgorithm::HandleFloat(
     LayoutUnit fragmentainer_block_offset =
         ConstraintSpace().FragmentainerOffsetAtBfc() +
         positioned_float.bfc_offset.block_offset;
-    BreakBeforeChild(ConstraintSpace(), child, *positioned_float.layout_result,
+    BreakBeforeChild(ConstraintSpace(), child, positioned_float.layout_result,
                      fragmentainer_block_offset,
                      /* appeal */ absl::nullopt,
                      /* is_forced_break */ false, &container_builder_);
@@ -2395,7 +2395,7 @@ NGBreakStatus NGBlockLayoutAlgorithm::BreakBeforeChildIfNeeded(
     EBreakBetween break_between =
         CalculateBreakBetweenValue(child, layout_result, container_builder_);
     if (IsForcedBreakValue(ConstraintSpace(), break_between)) {
-      BreakBeforeChild(ConstraintSpace(), child, layout_result,
+      BreakBeforeChild(ConstraintSpace(), child, &layout_result,
                        fragmentainer_block_offset, kBreakAppealPerfect,
                        /* is_forced_break */ true, &container_builder_);
       ConsumeRemainingFragmentainerSpace(previous_inflow_position);
@@ -2502,7 +2502,7 @@ NGBreakStatus NGBlockLayoutAlgorithm::BreakBeforeChildIfNeeded(
     }
   }
 
-  if (!AttemptSoftBreak(ConstraintSpace(), child, layout_result,
+  if (!AttemptSoftBreak(ConstraintSpace(), child, &layout_result,
                         fragmentainer_block_offset, appeal_before,
                         &container_builder_))
     return NGBreakStatus::kNeedsEarlierBreak;
