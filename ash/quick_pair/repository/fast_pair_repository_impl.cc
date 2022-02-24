2eea08cb2d7da226061c091895a96ef79f8c4555
Jon Mann <jonmann@chromium.org>
2022-02-24 06:03:07 +0000

    Fast Pair: Implement FakeFootprintsFetcher.
    Change-Id: I6ef9f11b57e42640657df2ea0058cb673464784f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485523
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974506}

--- ash/quick_pair/repository/fast_pair_repository_impl.cc
+++ ash/quick_pair/repository/fast_pair_repository_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/repository/fast_pair/device_metadata_fetcher.h"
 #include "ash/quick_pair/repository/fast_pair/fast_pair_image_decoder_impl.h"
 #include "ash/quick_pair/repository/fast_pair/footprints_fetcher.h"
+#include "ash/quick_pair/repository/fast_pair/footprints_fetcher_impl.h"
 #include "ash/quick_pair/repository/fast_pair/proto_conversions.h"
 #include "ash/quick_pair/repository/fast_pair/saved_device_registry.h"
 #include "ash/services/quick_pair/public/cpp/account_key_filter.h"
@@ -30,7 +31,7 @@ namespace quick_pair {
 FastPairRepositoryImpl::FastPairRepositoryImpl()
     : FastPairRepository(),
       device_metadata_fetcher_(std::make_unique<DeviceMetadataFetcher>()),
-      footprints_fetcher_(std::make_unique<FootprintsFetcher>()),
+      footprints_fetcher_(std::make_unique<FootprintsFetcherImpl>()),
       image_decoder_(std::make_unique<FastPairImageDecoderImpl>()),
       device_id_map_(std::make_unique<DeviceIdMap>()),
       device_image_store_(
