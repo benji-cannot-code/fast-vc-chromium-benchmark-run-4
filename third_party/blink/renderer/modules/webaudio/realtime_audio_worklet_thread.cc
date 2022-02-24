6ada61953be15b9a6f1d96bc6536cb3b63ba60b2
David Sanders <dsanders11@ucsbalum.com>
2022-02-24 09:00:42 +0000

    Remove unused include of global_scope_creation_params.h
    Also adds some includes to other files which were getting
    them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: I4d2a753639ea2164849ed1758ec94bafe4350e17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487259
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974551}

--- third_party/blink/renderer/modules/webaudio/realtime_audio_worklet_thread.cc
+++ third_party/blink/renderer/modules/webaudio/realtime_audio_worklet_thread.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/renderer/core/workers/global_scope_creation_params.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_worklet_global_scope.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 
