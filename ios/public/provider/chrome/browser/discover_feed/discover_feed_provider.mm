9f83ea092fee00e85a94146b060a0b4ad4722d6e
adamta <adamta@google.com>
2022-02-24 02:15:36 +0000

    [iOS] Remove unused feed service functions upstream
    Removes Start/StopFeed in favour of Start/StopFeedService now that they
    are unused. To be landed after https://crrev.com/i/4570831 has rolled.
    
    Bug: 1277504
    Change-Id: I966db637b6f30fae79999963c2921d5924a6bd67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485036
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#974454}

--- ios/public/provider/chrome/browser/discover_feed/discover_feed_provider.mm
+++ ios/public/provider/chrome/browser/discover_feed/discover_feed_provider.mm
@@ -9,11 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-void DiscoverFeedProvider::StartFeed(
-    DiscoverFeedConfiguration* discover_config) {}
-
-void DiscoverFeedProvider::StopFeed() {}
-
 void DiscoverFeedProvider::StartFeedService(
     DiscoverFeedConfiguration* discover_config) {}
 
