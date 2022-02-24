f4d531c16057df544e71d0652fbf34830b55fe16
Bo Liu <boliu@chromium.org>
2022-02-24 16:43:23 +0000

    Fix RendererBlinkPlatformImpl::GetIOThreadId
    Currently it returns invalid id if called from a non-main thread.
    
    Note a previous version that blocks and waits on the IO thread in
    RendererBlinkPlatformImpl constructor caused a performance regression:
    crbug.com/1278470
    
    So this is a more advanced version to asynchronously request the ID from
    IO thread in the constructor, but wait for the result in GetIOThreadId.
    This assumes that all other threads are allowed to block on the IO
    thread, which I believe is the case.
    
    Bug: 1157620
    Change-Id: I4936d6cc3f419e71572b60054b23f98c67edf0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3474997
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974682}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1672,10 +1672,6 @@ gpu::GpuChannelHost* RenderThreadImpl::GetGpuChannel() {
   return gpu_->GetGpuChannel().get();
 }
 
-base::PlatformThreadId RenderThreadImpl::GetIOPlatformThreadId() const {
-  return ChildProcess::current()->io_thread_id();
-}
-
 void RenderThreadImpl::CreateAgentSchedulingGroup(
     mojo::PendingReceiver<IPC::mojom::ChannelBootstrap> bootstrap,
     mojo::PendingRemote<blink::mojom::BrowserInterfaceBroker> broker_remote) {
