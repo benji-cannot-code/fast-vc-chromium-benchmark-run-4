2b3705271f7d73fdf92dcaa6a612b60de43fb3a0
Elly Fong-Jones <ellyjones@chromium.org>
2022-02-24 19:52:30 +0000

    clank-sh: remove ShareUsageRanking disabled path
    Note that this change does *not* remove the pre-ShareUsageRanking
    logic, which is still used when no Profile is present; a followup
    bug (https://crbug.com/1299846) covers dealing with that case,
    which will allow actually removing the old logic as well.
    
    Bug: 1299846
    Change-Id: I9cfdc62dea38d36b6cbdc07c40588449af2497ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3481598
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974763}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -126,8 +126,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kPwaUpdateDialogForNameAndIcon,
     &features::kQuietNotificationPrompts,
     &features::kRequestDesktopSiteForTablets,
-    &features::kShareUsageRanking,
-    &features::kShareUsageRankingFixedMore,
     &features::kToolbarUseHardwareBitmapDraw,
     &features::kWebNfc,
     &features::kIncognitoNtpRevamp,
