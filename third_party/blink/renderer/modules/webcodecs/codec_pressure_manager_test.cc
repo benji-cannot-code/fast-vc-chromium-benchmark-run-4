bd471ac4e60a6ddd12ae7dd0b2030d7717b6d683
Michael Lippautz <mlippautz@chromium.org>
2022-02-24 21:12:27 +0000

    CodecPressureManagerTest: Fix GC executed through run loops
    Manually setting up run loops breaks the assumption around how tasks
    are nested. Specifically, any GC executed through such a run loop
    would think that it doesn't need to conservatively scan the stack. Use
    a specific scope to instruct the GC to consider the stack in the test.
    
    Bug: chromium:1300492
    Change-Id: I10e1de6f9d961c8bdc638dda1b62109f97305983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488470
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974828}

--- third_party/blink/renderer/modules/webcodecs/codec_pressure_manager_test.cc
+++ third_party/blink/renderer/modules/webcodecs/codec_pressure_manager_test.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webcodecs/codec_pressure_manager_provider.h"
 #include "third_party/blink/renderer/modules/webcodecs/reclaimable_codec.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/heap/heap_test_utilities.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
 
 namespace blink {
@@ -97,6 +98,7 @@ class CodecPressureManagerTest
     base::RunLoop run_loop;
     base::SequencedTaskRunnerHandle::Get()->PostTask(FROM_HERE,
                                                      run_loop.QuitClosure());
+    HeapPointersOnStackScope pointers_on_stack(ThreadState::Current());
     run_loop.Run();
   }
 
