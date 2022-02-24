50a4a1502d2220d1f6710662985bfa7da4454a0c
Courtney Wong <courtneywong@chromium.org>
2022-02-24 18:12:25 +0000

    Remove laser pointer and header from annotation tray
    Change-Id: Ide90a527949fb80aeb8eb1915542e571955c9fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3475565
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974727}

--- ash/projector/projector_controller_impl.cc
+++ ash/projector/projector_controller_impl.cc
@@ -313,11 +313,6 @@ void ProjectorControllerImpl::OnRecordingStartAborted() {
   RecordCreationFlowMetrics(ProjectorCreationFlow::kRecordingAborted);
 }
 
-void ProjectorControllerImpl::OnLaserPointerPressed() {
-  DCHECK(ui_controller_);
-  ui_controller_->OnLaserPointerPressed();
-}
-
 void ProjectorControllerImpl::OnMarkerPressed() {
   DCHECK(ui_controller_);
   ui_controller_->OnMarkerPressed();
@@ -326,10 +321,6 @@ void ProjectorControllerImpl::OnMarkerPressed() {
 void ProjectorControllerImpl::ResetTools() {
   if (ui_controller_)
     ui_controller_->ResetTools();
-}
-
-bool ProjectorControllerImpl::IsLaserPointerEnabled() {
-  return ui_controller_ && ui_controller_->IsLaserPointerEnabled();
 }
 
 bool ProjectorControllerImpl::IsAnnotatorEnabled() {
--- ash/projector/projector_ui_controller.cc
+++ ash/projector/projector_ui_controller.cc
@@ -50,12 +50,6 @@ constexpr char kProjectorErrorNotificationId[] = "projector_error_notification";
 constexpr char kProjectorSaveErrorNotificationId[] =
     "projector_save_error_notification";
 
-void EnableLaserPointer(bool enabled) {
-  auto* laser_pointer_controller = Shell::Get()->laser_pointer_controller();
-  DCHECK(laser_pointer_controller);
-  Shell::Get()->laser_pointer_controller()->SetEnabled(enabled);
-}
-
 void ToggleAnnotator() {
   auto* capture_mode_controller = CaptureModeController::Get();
   // TODO(b/200292852): This check should not be necessary, but because
@@ -121,10 +115,6 @@ void ProjectorUiController::ShowSaveFailureNotification() {
 
 ProjectorUiController::ProjectorUiController(
     ProjectorControllerImpl* projector_controller) {
-  auto* laser_pointer_controller = Shell::Get()->laser_pointer_controller();
-  DCHECK(laser_pointer_controller);
-  laser_pointer_controller_observation_.Observe(laser_pointer_controller);
-
   projector_session_observation_.Observe(
       projector_controller->projector_session());
 }
@@ -149,13 +139,7 @@ void ProjectorUiController::CloseToolbar() {
   projector_annotation_tray->SetVisiblePreferred(false);
 }
 
-void ProjectorUiController::OnLaserPointerPressed() {
-  EnableLaserPointer(!IsLaserPointerEnabled());
-  RecordToolbarMetrics(ProjectorToolbar::kLaserPointer);
-}
-
 void ProjectorUiController::OnMarkerPressed() {
-  EnableLaserPointer(false);
   ToggleAnnotator();
   annotator_enabled_ = !annotator_enabled_;
   RecordToolbarMetrics(ProjectorToolbar::kMarkerTool);
@@ -171,27 +155,10 @@ void ProjectorUiController::ResetTools() {
     ToggleAnnotator();
     annotator_enabled_ = false;
   }
-
-  if (IsLaserPointerEnabled())
-    EnableLaserPointer(false);
-}
-
-bool ProjectorUiController::IsLaserPointerEnabled() {
-  return Shell::Get()->laser_pointer_controller()->is_enabled();
 }
 
 void ProjectorUiController::OnProjectorSessionActiveStateChanged(bool active) {
   if (!active)
     ResetTools();
 }
-
-void ProjectorUiController::OnLaserPointerStateChanged(bool enabled) {
-  // If laser pointer is enabled, disable marker and magnifier.
-  if (!enabled || !annotator_enabled_)
-    return;
-
-  ToggleAnnotator();
-  annotator_enabled_ = false;
-}
-
 }  // namespace ash
