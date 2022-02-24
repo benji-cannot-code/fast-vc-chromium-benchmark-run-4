878564dea44228e00ba30f26f88cc2ddca14cfb5
Joshua Hood <jdh@chromium.org>
2022-02-24 20:33:32 +0000

    Reland "[CodeHealth] Migrate presubmit checks in components/cronet/ to Python3"
    This is a reland of I0bbfa3262091f8e83c9fc28363ede156ab7c2100
    
    Original change's description:
    >[CodeHealth] Migrate presubmit checks in components/cronet/ to Python3
    >
    >and fixed resulting errors.
    >
    >Bug: 1262285
    >Change-Id: I5960d8eb23a4d879c94388a11adcf86d9b86ccba
    >Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3282041
    >Reviewed-by: Ryan Hamilton <rch@chromium.org>
    >Commit-Queue: Joshua Hood‎ <jdh@chromium.org>
    >Cr-Commit-Position: refs/heads/main@{#944270}
    
    Bug: 1262285, 1272291
    Change-Id: I45afd161db7f293f8f0771eed7be7bf4e5ed063d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488687
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974806}

--- components/cronet/tools/generators/cronet_bindings_generator.py
+++ components/cronet/tools/generators/cronet_bindings_generator.py
@@ -80,7 +80,7 @@ def MakeImportStackMessage(imported_filename_stack):
                     zip(imported_filename_stack[1:], imported_filename_stack)]))
 
 
-class RelativePath(object):
+class RelativePath():
   """Represents a path relative to the source tree."""
   def __init__(self, path, source_root):
     self.path = path
@@ -140,7 +140,7 @@ def ReadFileContents(filename):
     return f.read()
 
 
-class MojomProcessor(object):
+class MojomProcessor():
   """Parses mojom files and creates ASTs for them.
 
   Attributes:
