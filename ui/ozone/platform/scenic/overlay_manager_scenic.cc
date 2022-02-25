f93417792357bb28bee783622d964ee25c5fe153
Sergey Ulanov <sergeyu@chromium.org>
2022-02-25 02:03:26 +0000

    [Fuchsia] Make ScenicOverlayView re-usable with a new ScenicSurface
    Previously ScenicOverlayView was attaching itself to ScenicSurface and
    would never re-attach itself if ScenicSurface is replaced with a new
    one. This CL makes ScenicSurface responsible for attaching
    ScenicOverlayView when necessary. This ensures that ScenicOverlayView
    can be re-used for new ScenicSurface instances.
    
    Bug: 1300385
    Change-Id: Ie94e502246c6b6287b7ec915710489d441d9bf3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487147
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974953}

--- ui/ozone/platform/scenic/overlay_manager_scenic.cc
+++ ui/ozone/platform/scenic/overlay_manager_scenic.cc
@@ -14,7 +14,7 @@ namespace {
 
 class OverlayCandidatesScenic : public OverlayCandidatesOzone {
  public:
-  OverlayCandidatesScenic(gfx::AcceleratedWidget widget) : widget_(widget) {}
+  OverlayCandidatesScenic() = default;
 
   void CheckOverlaySupport(OverlaySurfaceCandidateList* candidates) override {
     for (auto& candidate : *candidates) {
@@ -22,13 +22,9 @@ class OverlayCandidatesScenic : public OverlayCandidatesOzone {
         continue;
       SysmemNativePixmap* sysmem_native_pixmap =
           reinterpret_cast<SysmemNativePixmap*>(candidate.native_pixmap.get());
-      candidate.overlay_handled =
-          sysmem_native_pixmap->SupportsOverlayPlane(widget_);
+      candidate.overlay_handled = sysmem_native_pixmap->SupportsOverlayPlane();
     }
   }
-
- private:
-  const gfx::AcceleratedWidget widget_;
 };
 
 }  // namespace
@@ -42,7 +38,7 @@ OverlayManagerScenic::~OverlayManagerScenic() = default;
 
 std::unique_ptr<OverlayCandidatesOzone>
 OverlayManagerScenic::CreateOverlayCandidates(gfx::AcceleratedWidget widget) {
-  return std::make_unique<OverlayCandidatesScenic>(widget);
+  return std::make_unique<OverlayCandidatesScenic>();
 }
 
 }  // namespace ui
--- ui/ozone/platform/scenic/sysmem_native_pixmap.h
+++ ui/ozone/platform/scenic/sysmem_native_pixmap.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+class ScenicOverlayView;
+
 class SysmemNativePixmap : public gfx::NativePixmap {
  public:
   SysmemNativePixmap(scoped_refptr<SysmemBufferCollection> collection,
@@ -40,7 +42,10 @@ class SysmemNativePixmap : public gfx::NativePixmap {
 
   // Returns true if overlay planes are supported and ScheduleOverlayPlane() can
   // be called.
-  bool SupportsOverlayPlane(gfx::AcceleratedWidget widget) const;
+  bool SupportsOverlayPlane() const;
+
+  // Returns true ScenicOverlayView for the pixmap if any.
+  ScenicOverlayView* GetScenicOverlayView();
 
  private:
   ~SysmemNativePixmap() override;
