904c20b8b2b356e5c624dc162e00cfe5abc93af5
David Sanders <dsanders11@ucsbalum.com>
2022-02-25 17:33:22 +0000

    Remove unused include of event_target_names.h
    Bug: 242216
    Change-Id: Ic70f7e64114a880d14143412a7d6fd7099422cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488634
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975159}

--- third_party/blink/renderer/core/dom/events/event_target.h
+++ third_party/blink/renderer/core/dom/events/event_target.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatch_result.h"
 #include "third_party/blink/renderer/core/dom/events/event_listener_map.h"
-#include "third_party/blink/renderer/core/event_target_names.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
--- third_party/blink/renderer/core/workers/worker_thread_test_helper.h
+++ third_party/blink/renderer/core/workers/worker_thread_test_helper.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/source_location.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_gc_controller.h"
 #include "third_party/blink/renderer/bindings/core/v8/worker_or_worklet_script_controller.h"
+#include "third_party/blink/renderer/core/event_target_names.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
