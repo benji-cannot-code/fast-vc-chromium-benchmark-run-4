8d7c8e32611ba85a6dcd06cdbcb4bbf8066c0020
echo linker <ajlinker@chromium.org>
2022-02-25 02:06:12 +0000

    [App Details] Adding installation source to App Management mojom/backend
    This adds an `install_source` field to App Management's mojom type and
    pipes through the value from `AppUpdate` to the App Management backend.
    
    Bug: 1295875
    Change-Id: I722d51b14beb6a75ed9150608e204a4c8a94c9f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473745
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: echo linker <ajlinker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974957}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/fake_page_handler.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/fake_page_handler.js
@@ -110,6 +110,7 @@ export class FakePageHandler {
       hideResizeLocked: true,
       supportedLinks: [],
       runOnOsLogin: null,
+      installSource: apps.mojom.InstallSource.kUnknown,
     };
 
     if (optConfig) {
