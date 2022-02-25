e02c97f81e6f19ea223b32ecce88ecb07e78f838
Damien Dejean <damiendejean@google.com>
2022-02-25 08:26:24 +0000

    [CrOS settings] remove extensions from always-on VPN.
    Chrome extension VPN is deprecated and incompatible with the "lockdown"
    mode from always-on VPN. This patch excludes Chrome extension VPNs from
    the VPN services available for always-on.
    
      Configure Cisco AnyConnect VPN client and check the VPN service does
      not appear in the always-on VPN dropdown menu.
    
    Bug: b:206910855
    Change-Id: I995a2a13c4995424166946c081ac55e8edd9a707
    Test: browser_tests --gtest_filter="OSSettingsInternetSubpageV3Test.*"
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3483960
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Damien Dejean <damiendejean@google.com>
    Cr-Commit-Position: refs/heads/main@{#975043}

--- chrome/test/data/webui/settings/chromeos/internet_subpage_tests.js
+++ chrome/test/data/webui/settings/chromeos/internet_subpage_tests.js
@@ -595,7 +595,7 @@ suite('InternetSubpage', function() {
                   internetSubpage.$$('#alwaysOnVpnSelector');
               assert(networkAlwaysOnVpn);
               // The list should contain 2 compatible networks.
-              assertEquals(5, networkAlwaysOnVpn.networks.length);
+              assertEquals(2, networkAlwaysOnVpn.networks.length);
             });
       });
     });
