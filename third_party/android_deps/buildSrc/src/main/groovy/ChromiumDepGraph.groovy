04ccf2621ad0f01c1aba708ade6f8009f869d3d5
Ben Joyce <bjoyce@google.com>
2022-02-25 00:01:21 +0000

    Add 3pp libraries for robolectric 4.7.3
    Bug: 1300351
    Change-Id: Iefa4d49a1b4a7bd6a0161c93489211a531576fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485005
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Benjamin Joyce <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974915}

--- third_party/android_deps/buildSrc/src/main/groovy/ChromiumDepGraph.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/ChromiumDepGraph.groovy
@@ -182,6 +182,9 @@ class ChromiumDepGraph {
         org_robolectric_junit: new PropertyOverride(
             licensePath: 'licenses/Codehaus_License-2009.txt',
             licenseName: 'MIT'),
+        org_robolectric_nativeruntime: new PropertyOverride(
+            licensePath: 'licenses/Codehaus_License-2009.txt',
+            licenseName: 'MIT'),
         org_robolectric_pluginapi: new PropertyOverride(
             licensePath: 'licenses/Codehaus_License-2009.txt',
             licenseName: 'MIT'),
