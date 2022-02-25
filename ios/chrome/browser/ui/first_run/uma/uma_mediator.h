97875e4bb1558d901c287506812baad7a7972e16
jlebel <jlebel@chromium.org>
2022-02-25 10:47:41 +0000

    [iOS] Add UMA dialog
    This patch adds a new UMA metric dialog for the new FRE:
    https://www.figma.com/file/Ffjr5RVVjIYRyEG0Dh6Zj5/Bling-FRE-follow-up?node-id=410%3A4352
    
    The design for iPad has not be implemented yet.
    
    Screenshot:
    https://drive.google.com/file/d/1GRtJ2ICMJQzAJBNuEk9jbY-gborPycdL/view?usp=sharing&resourcekey=0-Fzgwym0ARIxRzCmBL19_hQ
    
    This patch also adds TableViewAttributedStringHeaderFooterItem to display
    attributed string in a table view section footer.
    
    Bug: 1300377
    Change-Id: I9cc2b2cc6284cfcc4b5cfab4c3ca0c4b985a8bdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472843
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975066}

---
+++ ios/chrome/browser/ui/first_run/uma/uma_mediator.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FIRST_RUN_UMA_UMA_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_UI_FIRST_RUN_UMA_UMA_MEDIATOR_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/first_run/uma/uma_table_view_controller_model_delegate.h"
+
+// Mediator for the UMA dialog.
+@interface UMAMediator : NSObject <UMATableViewControllerModelDelegate>
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_UMA_UMA_MEDIATOR_H_
---
+++ ios/chrome/browser/ui/first_run/uma/uma_table_view_controller.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FIRST_RUN_UMA_UMA_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_FIRST_RUN_UMA_UMA_TABLE_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_controller.h"
+
+@class UMATableViewController;
+@protocol UMATableViewControllerModelDelegate;
+
+// Delegate for UMATableViewController, to receive events when the view
+// controller is dismissed.
+@protocol UMATableViewControllerPresentationDelegate <NSObject>
+
+// Called when the view controller is dismissed, using the "Done" button.
+- (void)UMATableViewControllerDidDismiss:
+    (UMATableViewController*)viewController;
+
+@end
+
+// View controller for the UMA dialog.
+@interface UMATableViewController : ChromeTableViewController
+
+@property(nonatomic, weak) id<UMATableViewControllerModelDelegate>
+    modelDelegate;
+@property(nonatomic, weak) id<UMATableViewControllerPresentationDelegate>
+    presentationDelegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_UMA_UMA_TABLE_VIEW_CONTROLLER_H_
