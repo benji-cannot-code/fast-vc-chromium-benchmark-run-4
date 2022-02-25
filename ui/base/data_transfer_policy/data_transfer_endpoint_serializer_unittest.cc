e350d3b1303bf9bd5da0ba4933079f0cdb0793dc
Alvin Lee <alvinlee@google.com>
2022-02-25 00:15:17 +0000

    Enable non-URL DTE serializer unit tests for Lacros.
    VM DataTransferEndpoint types were built for Lacros in
    https://crrev.com/c/3361031. Hence, the non-URL DTE serializer tests
    can be built for and run on Lacros. This CL changes the Ash specific
    build guards, for the non-URL tests, to a ChromeOS build guard.
    
    Bug: 1300478, 1280545
    Change-Id: Ie4cb422655765e4b5ad3ea8aeefbe1f26bc93cc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487746
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Alvin Lee <alvinlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#974921}

--- ui/base/data_transfer_policy/data_transfer_endpoint_serializer_unittest.cc
+++ ui/base/data_transfer_policy/data_transfer_endpoint_serializer_unittest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/data_transfer_policy/data_transfer_endpoint_serializer.h"
 
-#include "build/chromeos_buildflags.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 #include "url/gurl.h"
@@ -19,11 +19,9 @@ constexpr char kExampleJsonUrlType[] =
     R"({"endpoint_type":"url","url":"https://www.google.com/","url_origin":"https://www.google.com"})";
 constexpr char kExampleJsonUrlTypeNoUrl[] = R"({"endpoint_type":"url"})";
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-// TODO(crbug.com/1280545): Enable test when VM DataTransferEndpoint endpoint
-// types are built in Lacros.
+#if BUILDFLAG(IS_CHROMEOS)
 constexpr char kExampleJsonNonUrlType[] = R"({"endpoint_type":"crostini"})";
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace
 
@@ -53,9 +51,7 @@ TEST(DataTransferEndpointSerializerTest,
   EXPECT_EQ(nullptr, actual);
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-// TODO(crbug.com/1280545): Enable test when VM DataTransferEndpoint endpoint
-// types are built in Lacros.
+#if BUILDFLAG(IS_CHROMEOS)
 TEST(DataTransferEndpointSerializerTest, DataTransferEndpointToJsonNonUrl) {
   const DataTransferEndpoint example(EndpointType::kCrostini,
                                      /*notify_if_restricted=*/true);
@@ -72,6 +68,6 @@ TEST(DataTransferEndpointSerializerTest, JsonToDataTransferEndpointNonUrl) {
   EXPECT_EQ(EndpointType::kCrostini, actual->type());
   EXPECT_EQ(nullptr, actual->GetURL());
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace ui
