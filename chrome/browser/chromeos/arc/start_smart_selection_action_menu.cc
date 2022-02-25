e1059751ac23070b56ce99f7a3ec335fd2961de8
jorgegil@google.com <jorgegil@google.com>
2022-02-25 18:50:40 +0000

    Use HandleIntent() when the intent app can't launch
    This fixes a regression introduced in https://crrev.com/c/2738591.
    
    Even though smart selection action intents point to a
    specific package/activity to launch, it doesn't mean that
    the app is a launcher app that can be launched and then handle the
    intent. In the case of the intent_helper APK, the intent_helper app
    is not a launcher app and won't be opened with LaunchAppWithIntent,
    resulting in some context menu actions being dropped when
    clicked by the user (e.g. URLs, street addresses).
    
    This CL checks whether the app in the intent is the intent_helper app
    and if it is then it sends the intent directly to the intent helper to
    be handled without a specific app set to launch first.
    
    BUG=b/219834442
    TEST=with no Maps ARC++ installed, select a street address, right
    click on it, select the "Map" option in the context menu, verify
    it opens google maps in the chrome browser.
    
    Change-Id: Ifb4210545bbddd32486fa9861124ae4a409e3895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485322
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Jorge Gil <jorgegil@google.com>
    Cr-Commit-Position: refs/heads/main@{#975188}

--- chrome/browser/chromeos/arc/start_smart_selection_action_menu.cc
+++ chrome/browser/chromeos/arc/start_smart_selection_action_menu.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/launch_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/arc/common/intent_helper/arc_intent_helper_package.h"
 #include "components/renderer_context_menu/render_view_context_menu_proxy.h"
 #include "content/public/browser/context_menu_params.h"
 #include "ui/base/layout.h"
@@ -145,6 +146,21 @@ void StartSmartSelectionActionMenu::ExecuteCommand(int command_id) {
       display::Screen::GetScreen()->GetDisplayNearestPoint(point);
 
   Profile* profile = Profile::FromBrowserContext(context_);
+  if (actions_[index].activity.package_name ==
+      arc::kArcIntentHelperPackageName) {
+    // The intent_helper app can't be launched as a regular app that then
+    // handles this smart action intent because it is not a launcher app.
+    // Instead, directly request that the intent be handled by it without
+    // really launching the app.
+    // This is necessary for "generic" smart selection actions that aren't
+    // created for specific apps such as opening a URL or a street address.
+    delegate_->HandleIntent(std::move(actions_[index].action_intent),
+                            internal::ActivityIconLoader::ActivityName(
+                                actions_[index].activity.package_name,
+                                actions_[index].activity.activity_name));
+    return;
+  }
+  // The app that this intent points to is able to handle it, launch it.
   apps::AppServiceProxyFactory::GetForProfile(profile)->LaunchAppWithIntent(
       actions_[index].app_id, ui::EF_NONE,
       CreateIntent(std::move(actions_[index].action_intent),
