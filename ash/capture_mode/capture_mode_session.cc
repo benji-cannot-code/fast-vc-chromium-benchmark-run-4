4218a0be5a1a14144f3e09e8e35ba4745596ceac
minch <minch@chromium.org>
2022-02-25 23:00:04 +0000

    capture_selfie_cam: Put camera preview inside work area in `kFullscreen`
    Test: Modified a test
    Bug: 1296419
    Change-Id: I2bcca0fdcac81c27e81b7570703d6d861e6947b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490575
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975329}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -848,7 +848,9 @@ gfx::Rect CaptureModeSession::GetCameraPreviewConfineBounds() const {
     case CaptureModeSource::kFullscreen: {
       auto* parent = GetCameraPreviewParentWindow();
       DCHECK(parent);
-      return parent->GetBoundsInScreen();
+      return display::Screen::GetScreen()
+          ->GetDisplayNearestWindow(parent)
+          .work_area();
     }
     case CaptureModeSource::kWindow: {
       aura::Window* selected_window = GetSelectedWindow();
@@ -1002,8 +1004,9 @@ void CaptureModeSession::OnWindowDestroying(aura::Window* window) {
 void CaptureModeSession::OnDisplayMetricsChanged(
     const display::Display& display,
     uint32_t metrics) {
-  if (!(metrics & (DISPLAY_METRIC_BOUNDS | DISPLAY_METRIC_ROTATION |
-                   DISPLAY_METRIC_DEVICE_SCALE_FACTOR))) {
+  if (!(metrics &
+        (DISPLAY_METRIC_BOUNDS | DISPLAY_METRIC_ROTATION |
+         DISPLAY_METRIC_DEVICE_SCALE_FACTOR | DISPLAY_METRIC_WORK_AREA))) {
     return;
   }
 
