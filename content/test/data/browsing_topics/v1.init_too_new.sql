df29611079544bfe88520209e307dce4df11bbc4
Yao Xiao <yaoxia@chromium.org>
2022-02-25 01:08:20 +0000

    [Topics] Implement BrowsingTopicsSiteDataStorage database
    Explainer: https://github.com/jkarlin/topics
    
    Design doc: https://docs.google.com/document/d/12UEo6PgeySUgEpkaAoawPjhjR0GSz-XhSOXSmrMxV6U
    
    PoC CL: https://chromium-review.googlesource.com/c/chromium/src/+/3416117
    
    Bug: 1294456
    Change-Id: I6ab632d10147737a6560539a6a4698e87013dddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456782
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974934}

---
+++ content/test/data/browsing_topics/v1.init_too_new.sql
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+PRAGMA foreign_keys=OFF;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE browsing_topics_api_usages (
+hashed_context_domain INTEGER NOT NULL,
+hashed_top_host INTEGER NOT NULL,
+last_usage_time INTEGER NOT NULL,
+PRIMARY KEY (hashed_context_domain, hashed_top_host));
+
+CREATE INDEX last_usage_time_idx ON browsing_topics_api_usages(last_usage_time);
+
+CREATE TABLE meta(key LONGVARCHAR NOT NULL UNIQUE PRIMARY KEY, value LONGVARCHAR);
+
+INSERT INTO meta VALUES('version','2');
+INSERT INTO meta VALUES('last_compatible_version','1');
+
+INSERT INTO browsing_topics_api_usages VALUES (111, 222, 333);
+
+COMMIT;
