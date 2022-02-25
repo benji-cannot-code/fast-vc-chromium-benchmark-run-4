588d811b626e0509dfa784822c6bee2413c8b0a8
Ben Reich <benreich@chromium.org>
2022-02-25 22:51:50 +0000

    Ensure SelectFileDialogLacros::IsRunning returns a valid response
    The SelectFileDialogLacros::IsRunning method was previously hard coded
    to return true. When opening a save dialog a second time after
    cancelling the first in a save to PDF dialog, the dialog doesn't load
    because it assumes it is already running.
    
    Update the SelectFileDialogLacros instance to keep track of the unique
    wayland shell surface ID on open and on close, clear the value. This can
    be verified against the gfx::NativeWindow passed through to the
    IsRunning method.
    
    Bug: 1300286
    Change-Id: I35bad9e1a16eb88dba6e549433d3d65a2a27e3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489034
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975324}

--- ui/shell_dialogs/select_file_dialog_lacros.cc
+++ ui/shell_dialogs/select_file_dialog_lacros.cc
@@ -98,7 +98,8 @@ bool SelectFileDialogLacros::HasMultipleFileTypeChoicesImpl() {
 }
 
 bool SelectFileDialogLacros::IsRunning(gfx::NativeWindow owning_window) const {
-  return true;
+  return !owning_shell_window_id_.empty() &&
+         GetShellWindowUniqueId(owning_window) == owning_shell_window_id_;
 }
 
 void SelectFileDialogLacros::SelectFileImpl(
@@ -129,8 +130,10 @@ void SelectFileDialogLacros::SelectFileImpl(
         GetMojoAllowedPaths(file_types->allowed_paths);
   }
   // Modeless file dialogs have no owning window.
-  if (owning_window)
-    options->owning_shell_window_id = GetShellWindowUniqueId(owning_window);
+  if (owning_window) {
+    owning_shell_window_id_ = GetShellWindowUniqueId(owning_window);
+    options->owning_shell_window_id = owning_shell_window_id_;
+  }
 
   // Send request to ash-chrome.
   chromeos::LacrosService::Get()
@@ -143,6 +146,7 @@ void SelectFileDialogLacros::OnSelected(
     crosapi::mojom::SelectFileResult result,
     std::vector<crosapi::mojom::SelectedFileInfoPtr> mojo_files,
     int file_type_index) {
+  owning_shell_window_id_.clear();
   if (!listener_)
     return;
   if (mojo_files.empty()) {
