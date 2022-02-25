5afafe407e3cc6ea4628e2e8bfdeb11eb813f381
Ryan Sturm <ryansturm@chromium.org>
2022-02-25 21:28:27 +0000

    [search prefetch] cleaning up cruft
    Headers/unused variable cleanup on field trial settings.
    
    Bug: 1299062
    Change-Id: Ifeced85e0e4573c60c355786353d6220de9bf181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491367
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Auto-Submit: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975272}

--- chrome/browser/prefetch/search_prefetch/field_trial_settings.h
+++ chrome/browser/prefetch/search_prefetch/field_trial_settings.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 extern const base::Feature kSearchPrefetchServicePrefetching;
 
-extern const char kSearchPrefetchServiceCommandLineFlag[];
-
 // Whether the search prefetch service actually initiates prefetches.
 bool SearchPrefetchServicePrefetchingIsEnabled();
 
