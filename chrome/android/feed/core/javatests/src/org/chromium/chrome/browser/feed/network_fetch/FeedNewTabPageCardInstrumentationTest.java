065f8952cac450b918de814af32e28f91eb3ce05
Andrew Heard <andrewheard@chromium.org>
2022-02-25 22:42:22 +0000

    Android: Set Java package in FeedNewTabPageCardInstrumentationTest
    Added a Java package declaration in the file
    FeedNewTabPageCardInstrumentationTest.java so that it is not ignored by
    the existing script that collects Java test health data for the
    go/clank- dashboard.
    
    The existing script (locally-run, invoked manually) is being replaced
    by an automated process, which handles Java files without a package
    declaration. This change minimizes diffs between the output of the
    existing and replacement test health data collection scripts to simplify
    verification.
    
    Bug: 1254072
    Change-Id: I67a25f195be895afc0b42b75f792e7910c446703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489074
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Andrew Heard <andrewheard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975314}

--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/network_fetch/FeedNewTabPageCardInstrumentationTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/network_fetch/FeedNewTabPageCardInstrumentationTest.java
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+package org.chromium.chrome.browser.feed.network_fetch;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
