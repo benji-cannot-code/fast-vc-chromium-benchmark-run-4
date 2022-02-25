83367513ee4d816c545d8099405ffebc018ae49e
Wang Weiwei <wangww@dingdao.com>
2022-02-25 19:35:20 +0000

    Optimazation for TabStripModel destructor
    Remove one line of unused code in destructor function ~TabStripModel in
    chrome/browser/ui/tabs/tab_strip_model.cc
    
    Change-Id: I699c885e6e539b2d3bf96c1cca829efefe69f267
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489122
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975215}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -342,7 +342,6 @@ TabStripModel::TabStripModel(TabStripModelDelegate* delegate,
 }
 
 TabStripModel::~TabStripModel() {
-  std::vector<TabStripModelObserver*> observers;
   for (auto& observer : observers_)
     observer.ModelDestroyed(TabStripModelObserver::ModelPasskey(), this);
 
