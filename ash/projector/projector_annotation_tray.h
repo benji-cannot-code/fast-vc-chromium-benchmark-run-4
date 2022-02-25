b16b93cb7011d1111e682b23eca76ff1045b6eb0
Courtney Wong <courtneywong@chromium.org>
2022-02-25 18:32:38 +0000

    Update annotation tools bubble to match UX spec
    Also don't have ProjectorAnnotationTray implement ViewClickListener
    because we only care about clicks on the marker color buttons,
    not the view.
    
    Tested: https://screenshot.googleplex.com/86jn3uP2zekUm2r
    
    b: 221247450
    Change-Id: I97e2a748bd04039311b707b989d6b2d8c6f2411a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485504
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975180}

--- ash/projector/projector_annotation_tray.h
+++ ash/projector/projector_annotation_tray.h
@@ -16,8 +16,7 @@ class TrayBubbleWrapper;
 
 // Status area tray which allows you to access the annotation tools for
 // Projector.
-class ProjectorAnnotationTray : public TrayBackgroundView,
-                                public ViewClickListener {
+class ProjectorAnnotationTray : public TrayBackgroundView {
  public:
   explicit ProjectorAnnotationTray(Shelf* shelf);
   ProjectorAnnotationTray(const ProjectorAnnotationTray&) = delete;
@@ -35,9 +34,6 @@ class ProjectorAnnotationTray : public TrayBackgroundView,
   TrayBubbleView* GetBubbleView() override;
   views::Widget* GetBubbleWidget() const override;
   void OnThemeChanged() override;
-
-  // ViewClickListener:
-  void OnViewClicked(views::View* sender) override;
 
  private:
   // Deactives any annotation tool that is currently enabled and update the UI.
