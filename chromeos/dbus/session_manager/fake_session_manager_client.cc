a68f8e5e7b9f04d3987f2f70c04704ab50c3ac6b
Yuta Hijikata <ythjkt@google.com>
2022-02-25 03:30:03 +0000

    Force resume move migration before user profile creation.
    If move migration is finished undone in the previous attempt past a no
    point of return where both Ash and Lacros profile data directories are
    in a fragmented state, force restart Ash to resume move migration from
    where the migration has been left off. This has to happen before the
    user profile is created since letting Ash create a profile from a
    fragmented profile data directory can lead to an unexpected state.
    
    Test: browser_tests
    Bug: 1261730
    Change-Id: Ie877054e1739316bf395684eb1438011ff63f26d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466876
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#974984}

--- chromeos/dbus/session_manager/fake_session_manager_client.cc
+++ chromeos/dbus/session_manager/fake_session_manager_client.cc
@@ -409,6 +409,8 @@ void FakeSessionManagerClient::NotifyLockScreenDismissed() {
 
 bool FakeSessionManagerClient::RequestBrowserDataMigration(
     const cryptohome::AccountIdentifier& cryptohome_id) {
+  request_browser_data_migration_called_ = true;
+
   return true;
 }
 
