077fcaca05d748597749bfae8c81174d1da398d2
Tom Anderson <thomasanderson@chromium.org>
2022-02-25 21:57:18 +0000

    [X11] Add window name and _GTK_FRAME_EXTENTS properties to window cache
    This should be the last thing necessary to implement
    Get{Accelerated,LocalProcess}WidgetAtScreenPoint.
    
    R=sky
    
    Bug: 739898
    Change-Id: I483aca22be173237c71e6076962ec2165ac9928a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490207
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975288}

--- ui/gfx/x/window_cache.h
+++ ui/gfx/x/window_cache.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/memory/weak_ptr.h"
+#include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/x/connection.h"
 #include "ui/gfx/x/shape.h"
 #include "ui/gfx/x/xproto.h"
@@ -40,6 +41,10 @@ class COMPONENT_EXPORT(X11) WindowCache : public EventObserver {
 
     Window parent = Window::None;
     bool mapped = false;
+
+    // Properties
+    bool has_wm_name = false;
+    gfx::Insets gtk_frame_extents_px;
 
     int16_t x_px = 0;
     int16_t y_px = 0;
@@ -86,6 +91,9 @@ class COMPONENT_EXPORT(X11) WindowCache : public EventObserver {
   // Returns a vector of child windows or nullptr if `window` is not cached.
   std::vector<Window>* GetChildren(Window window);
 
+  // Makes a GetProperty request with a callback to OnGetPropertyResponse().
+  void GetProperty(Window window, Atom property, uint32_t length);
+
   // Common response handler that's called at the beginning of each On*Response.
   // Returns the WindowInfo for `window` or nullptr if `window` is not cached
   // or `has_reply` is false.
@@ -98,6 +106,10 @@ class COMPONENT_EXPORT(X11) WindowCache : public EventObserver {
 
   void OnQueryTreeResponse(Window window, QueryTreeResponse response);
 
+  void OnGetPropertyResponse(Window window,
+                             Atom atom,
+                             GetPropertyResponse response);
+
   void OnGetRectanglesResponse(Window window,
                                Shape::Sk kind,
                                Shape::GetRectanglesResponse response);
@@ -106,6 +118,7 @@ class COMPONENT_EXPORT(X11) WindowCache : public EventObserver {
 
   Connection* const connection_;
   const Window root_;
+  const Atom gtk_frame_extents_;
   std::unique_ptr<XScopedEventSelector> root_events_;
 
   std::unordered_map<Window, WindowInfo> windows_;
