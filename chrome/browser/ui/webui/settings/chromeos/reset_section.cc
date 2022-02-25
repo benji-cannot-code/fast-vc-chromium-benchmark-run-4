b6074ea09fca3b2e8ea3f9ed3f2ff5cc3919f23e
Yeunjoo Choi <ychoi@igalia.com>
2022-02-25 01:48:32 +0000

    Move //chrome/browser/ui/webui/settings/{chromeos=>ash} [2/N]
    This CL moves search/ to ash from chromeos.
    
    This CL is part of the Chrome OS source code directory migration:
    https://docs.google.com/document/d/1g-98HpzA8XcoGBWUv1gQNr4rbnD5yfvbtYZyPDDbkaE.
    
    Change-Id: I35636415c5ad5710c75a53d9607c9238d69a8083
    Bug: 1164001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3466857
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Owners-Override: James Cook <jamescook@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#974943}

--- chrome/browser/ui/webui/settings/chromeos/reset_section.cc
+++ chrome/browser/ui/webui/settings/chromeos/reset_section.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/ui/webui/settings/chromeos/search/search_tag_registry.h"
+#include "chrome/browser/ui/webui/settings/ash/search/search_tag_registry.h"
 #include "chrome/browser/ui/webui/settings/reset_settings_handler.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/grit/chromium_strings.h"
