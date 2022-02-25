26ca2011ffdbcccbb8e6b792a026b05cc3e43d21
Daniel Cheng <dcheng@chromium.org>
2022-02-25 06:54:13 +0000

    Replace url::Replacements<T> with GURL::Replacements in /chrome/browser/apps/guest_view.
    When clearing URL components, the two interfaces are exactly identical.
    When replacing a URL component, GURL::Replacements has a simpler
    interface.
    
    This CL was uploaded by git cl split.
    
    R=lazyboy@chromium.org
    
    Bug: 1271865
    Change-Id: I42b4c279ec078e68d8c58f0708a29cf6ef8b3e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487326
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975028}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -3525,9 +3525,8 @@ IN_PROC_BROWSER_TEST_F(WebViewTest,
   for (auto* download : saved_downloads) {
     const std::string port_string =
         base::NumberToString(embedded_test_server()->port());
-    url::Replacements<char> replacements;
-    replacements.SetPort(port_string.c_str(),
-                         url::Component(0, port_string.size()));
+    GURL::Replacements replacements;
+    replacements.SetPortStr(port_string);
     std::vector<GURL> url_chain;
     url_chain.push_back(download->GetURL().ReplaceComponents(replacements));
 
