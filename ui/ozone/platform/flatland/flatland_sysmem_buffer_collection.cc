9618e5b53a8e3a38cd73bb4487dc2af58cc3bc4d
Sergey Ulanov <sergeyu@chromium.org>
2022-02-26 00:22:11 +0000

    [Fuchsia] Fix SCANOUT_CPU_READ_WRITE handling.
    Previously VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT was set for all images
    that are allocated with usage=SCANOUT_CPU_READ_WRITE, but it's required
    only for SCANOUT buffers. Update both flatland and scenic to handle this
    case properly.
    Also updated comments for BufferUsage to clarify expectations for all
    other SCANOUT_* usage values.
    
    Bug: fuchsia:81736
    Change-Id: Idb791c478ec697552721d21ea86a665f1284e08a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482458
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975360}

--- ui/ozone/platform/flatland/flatland_sysmem_buffer_collection.cc
+++ ui/ozone/platform/flatland/flatland_sysmem_buffer_collection.cc
@@ -649,8 +649,7 @@ void FlatlandSysmemBufferCollection::InitializeImageCreateInfo(
   vk_image_info->usage = VK_IMAGE_USAGE_SAMPLED_BIT |
                          VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
                          VK_IMAGE_USAGE_TRANSFER_DST_BIT;
-  if (usage_ == gfx::BufferUsage::SCANOUT ||
-      usage_ == gfx::BufferUsage::SCANOUT_CPU_READ_WRITE) {
+  if (usage_ == gfx::BufferUsage::SCANOUT) {
     vk_image_info->usage |= VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
   }
 
