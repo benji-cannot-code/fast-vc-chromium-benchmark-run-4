23c9e6049ff1c0aa55b065a903a41e0f7b9d8276
David Sanders <dsanders11@ucsbalum.com>
2022-02-26 16:25:54 +0000

    Remove unused include of //chrome/browser/image_editor/screenshot_flow.h
    Bug: 242216
    Change-Id: I90e3f06e50622badc8e7e4d2aa364262ad3de0eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491942
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975480}

--- chrome/browser/ui/browser_commands.h
+++ chrome/browser/ui/browser_commands.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/devtools/devtools_toggle_action.h"
 #include "chrome/browser/devtools/devtools_window.h"
-#include "chrome/browser/image_editor/screenshot_flow.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_delegate.h"
