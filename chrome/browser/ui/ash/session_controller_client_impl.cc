31cc578df9521ff370ae599006cb5b531b898313
James Cook <jamescook@chromium.org>
2022-02-26 00:12:50 +0000

    ash: Clean up unnecessary EqualsTraits<gfx::ImageSkia> in session code
    For the mustash project, we used to send UserSession data from chrome
    to ash over mojo. Since mustash was canceled, we don't do that anymore.
    We don't need this helper function in the session controller code,
    so delete it.
    
    Follow-up to CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/3490031
    
    Bug: none
    Change-Id: I088f71f5f2e80b099ede88cedd354638b5a732eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490063
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975355}

--- chrome/browser/ui/ash/session_controller_client_impl.cc
+++ chrome/browser/ui/ash/session_controller_client_impl.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_type.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/notification_service.h"
-#include "mojo/public/cpp/bindings/equals_traits.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/chromeos/resources/grit/ui_chromeos_resources.h"
 #include "ui/gfx/image/image_skia.h"
@@ -129,20 +128,6 @@ void OnAcceptMultiprofilesIntroDialog(bool accept, bool never_show_again) {
 }
 
 }  // namespace
-
-namespace mojo {
-
-// When comparing two mojom::UserSession objects we need to decide if the avatar
-// images are changed. Consider them equal if they have the same storage rather
-// than comparing the backing pixels.
-template <>
-struct EqualsTraits<gfx::ImageSkia> {
-  static bool Equals(const gfx::ImageSkia& a, const gfx::ImageSkia& b) {
-    return a.BackedBySameObjectAs(b);
-  }
-};
-
-}  // namespace mojo
 
 SessionControllerClientImpl::SessionControllerClientImpl() {
   SessionManager::Get()->AddObserver(this);
