dcdd738533b566ecd0187c0062ef26078c975c81
Daniel Cheng <dcheng@chromium.org>
2022-02-26 17:50:15 +0000

    Migrate base::{size,empty,data} to STL equivalents in //fuchsia.
    Bug: 1299695
    Change-Id: Iae68b2cbeec3b95c478fb679852fe82a66d9d331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491924
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975483}

--- fuchsia/engine/web_engine_integration_test.cc
+++ fuchsia/engine/web_engine_integration_test.cc
@@ -5,14 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fuchsia/mediacodec/cpp/fidl.h>
 #include <fuchsia/mem/cpp/fidl.h>
+#include <lib/zx/vmo.h>
 #include <zircon/rights.h>
 #include <zircon/types.h>
 
 #include <string>
 
-#include <lib/zx/vmo.h>
-
-#include "base/cxx17_backports.h"
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/mem_buffer_util.h"
 #include "base/strings/stringprintf.h"
@@ -111,7 +109,7 @@ class WebEngineIntegrationUserAgentTest : public WebEngineIntegrationTest {
 
     // Ensure the field was actually populated.
     EXPECT_GT(expected_ua.length(),
-              base::size(kDefaultUserAgentStringWithVersionPlaceholder));
+              std::size(kDefaultUserAgentStringWithVersionPlaceholder));
     EXPECT_NE(expected_ua.find(version_info::GetVersionNumber()),
               std::string::npos);
 
