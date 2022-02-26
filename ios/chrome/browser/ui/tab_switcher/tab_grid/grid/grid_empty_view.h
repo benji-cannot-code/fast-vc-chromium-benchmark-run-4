e274ad53edaec046f4cfd4a523000a22cd9ad8f6
Mike Dougherty <michaeldo@chromium.org>
2022-02-26 08:35:04 +0000

    Update tab grid empty state message
    When the user is searching, the empty (no tabs) message should be
    specific to no matching tab results rather than the user having no tabs.
    This CL updates the text of the empty state view based on the current
    TabGridMode.
    
    Fixed: 1299899
    Change-Id: I16217eb021f440fcb4e577ce6e5d876cf7b875c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491078
    Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975437}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_empty_view.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_empty_view.h
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_paging.h"
+
 // Protocol defining the interface of the view displayed when the grid is empty.
-@protocol GridEmptyView
+@protocol GridEmptyView <TabGridPaging>
 
 // Insets of the inner ScrollView.
 @property(nonatomic, assign) UIEdgeInsets scrollViewContentInsets;
