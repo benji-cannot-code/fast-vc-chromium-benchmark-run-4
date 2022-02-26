6fb65ed7943b6b44ec4a5e546091501753271dcd
Donn Denman <donnd@chromium.org>
2022-02-26 03:42:33 +0000

    [TTS] Disable a Tab test for multiwindow
    Disables our test of Tab reparenting for Android S and above.
    
    BUG=1301017
    
    Change-Id: Ifaefa427978258dd69888785c194406ea1427ed0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491564
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975397}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -20,6 +20,7 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
 import android.text.TextUtils;
@@ -2824,6 +2825,7 @@ public class ContextualSearchManagerTest {
     @Feature({"ContextualSearch"})
     @CommandLineFlags.Add(ChromeSwitches.DISABLE_TAB_MERGING_FOR_TESTING)
     @MinAndroidSdkLevel(Build.VERSION_CODES.N)
+    @DisableIf.Build(sdk_is_greater_than = VERSION_CODES.R, message = "crbug.com/1301017")
     @ParameterAnnotations.UseMethodParameter(FeatureParamProvider.class)
     public void testTabReparenting(@EnabledFeature int enabledFeature) throws Exception {
         // Move our "tap_test" tab to another activity.
