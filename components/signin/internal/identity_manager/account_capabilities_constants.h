44b9479dd26aacee178f32d5641ce2fa1e076f3e
Nohemi Fernandez <fernandex@chromium.org>
2022-02-28 17:00:20 +0000

    Deprecate custom capability pref for CanOfferExtendedChromeSyncPromos.
    Derive capability preferences from the service name used to query the
    Capabilities server. This allows the code to streamline the
    identification between a Capability and its corresponding state both
    in local prefs and on the server.
    
    Bug: 1298865
    Change-Id: I8ccd0fd33123906921aad31b588609c962867bee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488189
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975802}

--- components/signin/internal/identity_manager/account_capabilities_constants.h
+++ components/signin/internal/identity_manager/account_capabilities_constants.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SIGNIN_INTERNAL_IDENTITY_MANAGER_ACCOUNT_CAPABILITIES_CONSTANTS_H_
 
 extern const char kCanOfferExtendedChromeSyncPromosCapabilityName[];
-extern const char kCanOfferExtendedChromeSyncPromosCapabilityPrefsPath[];
 extern const char kIsSubjectToParentalControlsCapabilityName[];
 
 #endif  // COMPONENTS_SIGNIN_INTERNAL_IDENTITY_MANAGER_ACCOUNT_CAPABILITIES_CONSTANTS_H_
