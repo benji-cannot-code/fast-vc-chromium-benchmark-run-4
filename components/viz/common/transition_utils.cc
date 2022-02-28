a65a928cd05411e5be01c73438329385951eec48
Vladimir Levin <vmpstr@chromium.org>
2022-02-28 22:31:53 +0000

    SET: Add SolidColorDrawQuad debugging info into render pass logging.
    This patch augments the transition render pass logging to add solid
    color draw quad, as well as material type logging.
    
    R=khushalsagar@chromium.org
    
    Change-Id: I02134f229d16103d4bdda8d6e3d09e8eb6da75d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3494367
    Auto-Submit: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975974}

--- components/viz/common/transition_utils.cc
+++ components/viz/common/transition_utils.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_render_pass_draw_quad.h"
 #include "components/viz/common/quads/render_pass_io.h"
 #include "components/viz/common/quads/shared_element_draw_quad.h"
+#include "components/viz/common/quads/solid_color_draw_quad.h"
+#include "third_party/skia/include/core/SkColor.h"
 
 namespace viz {
 
@@ -36,6 +38,13 @@ struct StackFrame {
   bool first_pass_visit = true;
 };
 
+std::string SkColorToRGBAString(SkColor color) {
+  std::ostringstream str;
+  str << "rgba(" << SkColorGetR(color) << ", " << SkColorGetG(color) << ", "
+      << SkColorGetB(color) << ", " << SkColorGetA(color) << ")";
+  return str.str();
+}
+
 std::unordered_set<uint64_t> ProcessStack(
     std::ostringstream& str,
     std::vector<StackFrame>& stack,
@@ -45,18 +54,25 @@ std::unordered_set<uint64_t> ProcessStack(
       str << " ";
   };
   auto write_render_pass = [&str](const CompositorRenderPass* pass) {
-    str << "(" << pass << ") render pass id=" << pass->id.GetUnsafeValue();
+    str << "(" << pass << ") render pass id=" << pass->id.GetUnsafeValue()
+        << " output_rect=" << pass->output_rect.ToString();
     if (pass->shared_element_resource_id.IsValid())
       str << " " << pass->shared_element_resource_id.ToString();
     str << "\n";
   };
   auto write_sqs = [&str](const SharedQuadState* sqs) {
-    str << "(" << sqs << ") switched to sqs with opacity " << sqs->opacity
-        << " and blend mode " << BlendModeToString(sqs->blend_mode) << "\n";
+    str << "(" << sqs << ") switched to sqs with opacity=" << sqs->opacity
+        << ", blend_mode=" << BlendModeToString(sqs->blend_mode)
+        << " quad_layer_rect=" << sqs->quad_layer_rect.ToString() << "\n";
   };
   auto write_shared_element_quad = [&str](const SharedElementDrawQuad* quad) {
     str << "(" << quad << ") SharedElementDrawQuad "
         << quad->resource_id.ToString() << "\n";
+  };
+  auto write_solid_color_quad = [&str](const SolidColorDrawQuad* quad) {
+    str << "(" << quad
+        << ") SolidColorDrawQuad color=" << SkColorToRGBAString(quad->color)
+        << "\n";
   };
   auto write_compositor_render_pass_quad =
       [&str](const CompositorRenderPassDrawQuad* quad) {
@@ -117,8 +133,14 @@ std::unordered_set<uint64_t> ProcessStack(
         write_shared_element_quad(quad);
         break;
       }
+      case DrawQuad::Material::kSolidColor: {
+        auto* quad = SolidColorDrawQuad::MaterialCast((*frame.quad_iter));
+        write_solid_color_quad(quad);
+        break;
+      }
       default:
-        str << "(other quad type)\n";
+        str << "DrawQuad, material: "
+            << DrawQuadMaterialToString((*frame.quad_iter)->material) << "\n";
         break;
     }
   }
