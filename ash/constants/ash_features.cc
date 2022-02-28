7f8e8d3f69743c222969d477b21603a893d78ea1
mlcui <mlcui@google.com>
2022-02-28 00:13:43 +0000

    Move "is language packs enabled" logic to function
    This makes updating code which needs to know "is language packs
    enabled?" easier, as it is all consolidated into a single place.
    
    Whenever new language pack features are added, they should be added to
    the new function. If a feature becomes enabled by default, the function
    can be rewritten to return a constant true.
    
    Bug: None
    Change-Id: I160469b7907c556f83ec0a6289ebc85a99f396be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3474092
    Auto-Submit: Michael Cui <mlcui@google.com>
    Reviewed-by: Claudio M <claudiomagni@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#975603}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1734,6 +1734,11 @@ bool IsKeyboardBasedDisplayArrangementInSettingsEnabled() {
       kKeyboardBasedDisplayArrangementInSettings);
 }
 
+bool IsLanguagePacksEnabled() {
+  return base::FeatureList::IsEnabled(kHandwritingLegacyRecognition) ||
+         base::FeatureList::IsEnabled(kHandwritingLegacyRecognitionAllLang);
+}
+
 bool IsLauncherAppSortEnabled() {
   return IsProductivityLauncherEnabled() &&
          base::FeatureList::IsEnabled(kLauncherAppSort);
