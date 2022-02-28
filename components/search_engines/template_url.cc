07e1ea7ef890c42fec2bcc7dafda194fdbaf7fe9
Tom Lukaszewicz <tluk@chromium.org>
2022-02-28 21:31:35 +0000

    [side search] Add a prepopulated search "side_search_param" field
    This CL adds a way for search engines to opt into the side search
    Chrome browser feature. Follows the same approach taken in
    CL:3043087
    
    Search engines do this by specifying a URL param that is appended
    to their search URLs for use in the side panel.
    
    Helpers have been added to the TemplateURLService that allows feature
    code to check participation and generate side panel URLs for the
    default search engine.
    
    Bug: 1300897
    Change-Id: Icb35ee6a5ba74be0911988a4e8ce45ce3016ea9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490824
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975947}

--- components/search_engines/template_url.cc
+++ components/search_engines/template_url.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/google_api_keys.h"
 #include "net/base/escape.h"
 #include "net/base/mime_util.h"
+#include "net/base/url_util.h"
 #include "third_party/metrics_proto/omnibox_input_type.pb.h"
 #include "ui/base/device_form_factor.h"
 #include "url/gurl.h"
@@ -1627,6 +1628,19 @@ GURL TemplateURL::GenerateSearchURL(
   return GURL(url_ref().ReplaceSearchTerms(
       TemplateURLRef::SearchTermsArgs(u"blah.blah.blah.blah.blah"),
       search_terms_data, nullptr));
+}
+
+bool TemplateURL::IsSideSearchSupported() const {
+  return !side_search_param().empty();
+}
+
+GURL TemplateURL::GenerateSideSearchURL(
+    const GURL& search_url,
+    const std::string& version,
+    const SearchTermsData& search_terms_data) const {
+  DCHECK(IsSideSearchSupported());
+  DCHECK(IsSearchURL(search_url, search_terms_data));
+  return net::AppendQueryParameter(search_url, side_search_param(), version);
 }
 
 void TemplateURL::CopyFrom(const TemplateURL& other) {
