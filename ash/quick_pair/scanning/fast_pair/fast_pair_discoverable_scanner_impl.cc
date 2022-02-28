d9af62204e70e6acb9e67cc8ac87254a5d124ebd
Shane Fitzpatrick <shanefitz@google.com>
2022-02-28 18:30:54 +0000

    [FastPair] Improve E2E logging
    This changes adds logs and increases the severity of some logs in order
    to improve the E2E flow logging overall.
    
    TEST=Ran on my DUT and observed the logs
    
    Change-Id: I108913ba594291fd46db3e5a4366b8626719646d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490026
    Auto-Submit: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#975850}

--- ash/quick_pair/scanning/fast_pair/fast_pair_discoverable_scanner_impl.cc
+++ ash/quick_pair/scanning/fast_pair/fast_pair_discoverable_scanner_impl.cc
@@ -106,6 +106,7 @@ void FastPairDiscoverableScannerImpl::OnDeviceFound(
 
   model_id_parse_attempts_[device->GetAddress()] = 1;
 
+  QP_LOG(INFO) << __func__ << ": Attempting to get model ID";
   quick_pair_process::GetHexModelIdFromServiceData(
       *fast_pair_service_data,
       base::BindOnce(&FastPairDiscoverableScannerImpl::OnModelIdRetrieved,
@@ -172,7 +173,7 @@ void FastPairDiscoverableScannerImpl::OnDeviceMetadataRetrieved(
   auto device = base::MakeRefCounted<Device>(model_id, address,
                                              Protocol::kFastPairInitial);
 
-  QP_LOG(VERBOSE) << __func__ << ": Id: " << model_id;
+  QP_LOG(INFO) << __func__ << ": Id: " << model_id;
 
   // Anti-spoofing keys were introduced in Fast Pair v2, so if this isn't
   // available then the device is v1.
@@ -220,6 +221,7 @@ void FastPairDiscoverableScannerImpl::NotifyDeviceFound(
     return;
   }
 
+  QP_LOG(INFO) << __func__ << ": Running found callback";
   notified_devices_[device->ble_address] = device;
   found_callback_.Run(device);
 }
@@ -241,6 +243,7 @@ void FastPairDiscoverableScannerImpl::OnDeviceLost(
   if (it == notified_devices_.end())
     return;
 
+  QP_LOG(INFO) << __func__ << ": Running lost callback";
   scoped_refptr<Device> notified_device = it->second;
   notified_devices_.erase(it);
   lost_callback_.Run(std::move(notified_device));
@@ -272,6 +275,7 @@ void FastPairDiscoverableScannerImpl::OnUtilityProcessStopped(
   const std::vector<uint8_t>* fast_pair_service_data =
       device->GetServiceDataForUUID(kFastPairBluetoothUuid);
 
+  QP_LOG(INFO) << __func__ << ": Retrying call to get model ID";
   quick_pair_process::GetHexModelIdFromServiceData(
       *fast_pair_service_data,
       base::BindOnce(&FastPairDiscoverableScannerImpl::OnModelIdRetrieved,
--- ash/quick_pair/scanning/fast_pair/fast_pair_not_discoverable_scanner_impl.cc
+++ ash/quick_pair/scanning/fast_pair/fast_pair_not_discoverable_scanner_impl.cc
@@ -137,6 +137,7 @@ void FastPairNotDiscoverableScannerImpl::OnDeviceFound(
 
   advertisement_parse_attempts_[device->GetAddress()] = 1;
 
+  QP_LOG(INFO) << __func__ << ": Attempting to parse advertisement.";
   quick_pair_process::ParseNotDiscoverableAdvertisement(
       *fast_pair_service_data,
       base::BindOnce(&FastPairNotDiscoverableScannerImpl::OnAdvertisementParsed,
@@ -160,6 +161,7 @@ void FastPairNotDiscoverableScannerImpl::OnDeviceLost(
   if (it == notified_devices_.end())
     return;
 
+  QP_LOG(INFO) << __func__ << ": Running lost callback";
   scoped_refptr<Device> notified_device = it->second;
   notified_devices_.erase(it);
   lost_callback_.Run(std::move(notified_device));
@@ -168,11 +170,17 @@ void FastPairNotDiscoverableScannerImpl::OnDeviceLost(
 void FastPairNotDiscoverableScannerImpl::OnAdvertisementParsed(
     const std::string& address,
     const absl::optional<NotDiscoverableAdvertisement>& advertisement) {
+  QP_LOG(INFO) << __func__
+               << ": Has value: " << (advertisement.has_value() ? "yes" : "no");
+
   auto it = advertisement_parse_attempts_.find(address);
 
   // If this check fails, the device was lost during parsing
-  if (it == advertisement_parse_attempts_.end())
+  if (it == advertisement_parse_attempts_.end()) {
+    QP_LOG(WARNING) << __func__
+                    << ": Ignoring because parse attempt was cancelled";
     return;
+  }
 
   advertisement_parse_attempts_.erase(it);
 
@@ -191,8 +199,10 @@ void FastPairNotDiscoverableScannerImpl::OnAdvertisementParsed(
   if (advertisement->battery_notification)
     SetBatteryInfo(device, advertisement->battery_notification.value());
 
-  if (!advertisement->show_ui)
+  if (!advertisement->show_ui) {
+    QP_LOG(INFO) << __func__ << ": Ignoring because show UI flag is false";
     return;
+  }
 
   auto filter_iterator =
       account_key_filters_
@@ -211,7 +221,7 @@ void FastPairNotDiscoverableScannerImpl::OnAccountKeyFilterCheckResult(
     absl::optional<PairingMetadata> metadata) {
   account_key_filters_.erase(address);
 
-  QP_LOG(VERBOSE) << __func__ << " Metadata: " << (metadata ? "yes" : "no");
+  QP_LOG(INFO) << __func__ << " Metadata: " << (metadata ? "yes" : "no");
 
   if (!metadata || !metadata->device_metadata)
     return;
@@ -223,7 +233,7 @@ void FastPairNotDiscoverableScannerImpl::OnAccountKeyFilterCheckResult(
   model_id_stream << std::uppercase << std::hex << details.id();
   std::string model_id = model_id_stream.str();
 
-  QP_LOG(VERBOSE) << __func__ << ": Id: " << model_id;
+  QP_LOG(INFO) << __func__ << ": Id: " << model_id;
   auto device = base::MakeRefCounted<Device>(model_id, address,
                                              Protocol::kFastPairSubsequent);
   device->SetAdditionalData(Device::AdditionalDataType::kAccountKey,
@@ -260,6 +270,7 @@ void FastPairNotDiscoverableScannerImpl::OnHandshakeComplete(
     return;
   }
 
+  QP_LOG(INFO) << __func__ << ": Running found callback";
   notified_devices_[device->ble_address] = device;
   found_callback_.Run(device);
 }
@@ -297,6 +308,7 @@ void FastPairNotDiscoverableScannerImpl::OnUtilityProcessStopped(
 
   advertisement_parse_attempts_[address] = current_retry_count + 1;
 
+  QP_LOG(INFO) << __func__ << ": Retrying call to parse advertisement";
   quick_pair_process::ParseNotDiscoverableAdvertisement(
       *fast_pair_service_data,
       base::BindOnce(&FastPairNotDiscoverableScannerImpl::OnAdvertisementParsed,
