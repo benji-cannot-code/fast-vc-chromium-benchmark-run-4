3142c32735c5c57446b7d13b51eb947dd320d95a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-02-28 20:29:36 +0000

    Import wpt@9261ca87db106c1f31f1ab4f54b8f4a2617cbc24
    Using wpt-import in Chromium cc708a408e94b2d38683e928719d0b7d00d36261.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I4e6ce6382ad42f46ad0ecaffa83f99d25620a38d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3493556
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Kyle Ju <kyleju@chromium.org>
    Commit-Queue: Kyle Ju <kyleju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975915}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/support/keys.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/perform_actions/support/keys.py
@@ -443,7 +443,7 @@ ALL_EVENTS = OrderedDict(
         )),
         ("META", OrderedDict(
             [
-                ("code", "MetaLeft"),
+                ("code", "OSLeft"),
                 ("ctrl", False),
                 ("key", "Meta"),
                 ("location", 1),
@@ -751,7 +751,7 @@ ALL_EVENTS = OrderedDict(
         )),
         ("R_META", OrderedDict(
             [
-                ("code", "MetaRight"),
+                ("code", "OSRight"),
                 ("ctrl", False),
                 ("key", "Meta"),
                 ("location", 2),
