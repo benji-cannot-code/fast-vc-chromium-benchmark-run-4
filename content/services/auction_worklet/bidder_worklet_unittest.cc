d5113c526a8c02e1a8a7e0ff17b51df73dec97d5
Matt Menke <mmenke@chromium.org>
2022-02-28 14:44:15 +0000

    [FLEDGE] Component auctions CL3: Add topLevelSeller to reportWin().
    Bug: 1288865
    Change-Id: I85f4c5d0374ad8f0494f61c0ee7b5b728947aa6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3459524
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975752}

--- content/services/auction_worklet/bidder_worklet_unittest.cc
+++ content/services/auction_worklet/bidder_worklet_unittest.cc
@@ -268,8 +268,8 @@ class BidderWorkletTest : public testing::Test {
     bidder_worklet->ReportWin(
         interest_group_name_, auction_signals_, per_buyer_signals_,
         seller_signals_, browser_signal_render_url_, browser_signal_bid_,
-        browser_signal_seller_origin_, data_version_.value_or(0),
-        data_version_.has_value(),
+        browser_signal_seller_origin_, browser_signal_top_level_seller_origin_,
+        data_version_.value_or(0), data_version_.has_value(),
         base::BindOnce(
             [](const absl::optional<GURL>& expected_report_url,
                const std::vector<std::string>& expected_errors,
@@ -1807,8 +1807,8 @@ TEST_F(BidderWorkletTest, WasmReportWin) {
   bidder_worklet->ReportWin(
       interest_group_name_, /*auction_signals_json=*/"0", per_buyer_signals_,
       seller_signals_, browser_signal_render_url_, browser_signal_bid_,
-      browser_signal_seller_origin_, data_version_.value_or(0),
-      data_version_.has_value(),
+      browser_signal_seller_origin_, browser_signal_top_level_seller_origin_,
+      data_version_.value_or(0), data_version_.has_value(),
       base::BindLambdaForTesting(
           [&run_loop](const absl::optional<GURL>& report_url,
                       const std::vector<std::string>& errors) {
@@ -2167,8 +2167,8 @@ TEST_F(BidderWorkletTest, DeleteBeforeReportWinCallback) {
   bidder_worklet->ReportWin(
       interest_group_name_, auction_signals_, per_buyer_signals_,
       seller_signals_, browser_signal_render_url_, browser_signal_bid_,
-      browser_signal_seller_origin_, data_version_.value_or(0),
-      data_version_.has_value(),
+      browser_signal_seller_origin_, browser_signal_top_level_seller_origin_,
+      data_version_.value_or(0), data_version_.has_value(),
       base::BindOnce([](const absl::optional<GURL>& report_url,
                         const std::vector<std::string>& errors) {
         ADD_FAILURE() << "Callback should not be invoked since worklet deleted";
@@ -2205,7 +2205,8 @@ TEST_F(BidderWorkletTest, ReportWinParallel) {
           interest_group_name_,
           /*auction_signals_json=*/base::NumberToString(i), per_buyer_signals_,
           seller_signals_, browser_signal_render_url_, browser_signal_bid_,
-          browser_signal_seller_origin_, data_version_.value_or(0),
+          browser_signal_seller_origin_,
+          browser_signal_top_level_seller_origin_, data_version_.value_or(0),
           data_version_.has_value(),
           base::BindLambdaForTesting(
               [&run_loop, &num_report_win_calls, i](
@@ -2244,8 +2245,8 @@ TEST_F(BidderWorkletTest, ReportWinParallelLoadFails) {
         interest_group_name_,
         /*auction_signals_json=*/base::NumberToString(i), per_buyer_signals_,
         seller_signals_, browser_signal_render_url_, browser_signal_bid_,
-        browser_signal_seller_origin_, data_version_.value_or(0),
-        data_version_.has_value(),
+        browser_signal_seller_origin_, browser_signal_top_level_seller_origin_,
+        data_version_.value_or(0), data_version_.has_value(),
         base::BindOnce([](const absl::optional<GURL>& report_url,
                           const std::vector<std::string>& errors) {
           ADD_FAILURE() << "Callback should not be invoked.";
@@ -2381,6 +2382,21 @@ TEST_F(BidderWorkletTest, ReportWinBrowserSignalSeller) {
       /*expected_report_url=*/seller_raw_url);
 }
 
+TEST_F(BidderWorkletTest, ReportWinBrowserSignalTopLevelSeller) {
+  browser_signal_top_level_seller_origin_ = absl::nullopt;
+  RunReportWinWithFunctionBodyExpectingResult(
+      R"(if (!("topLevelSeller" in browserSignals))
+            sendReportTo('https://pass.test');)",
+      /*expected_report_url=*/GURL("https://pass.test"));
+
+  GURL top_level_seller_raw_url = GURL("https://top.level.seller.origin.test");
+  browser_signal_top_level_seller_origin_ =
+      url::Origin::Create(top_level_seller_raw_url);
+  RunReportWinWithFunctionBodyExpectingResult(
+      "sendReportTo(browserSignals.topLevelSeller)",
+      /*expected_report_url=*/top_level_seller_raw_url);
+}
+
 // Subsequent runs of the same script should not affect each other. Same is true
 // for different scripts, but it follows from the single script case.
 //
@@ -2425,8 +2441,8 @@ TEST_F(BidderWorkletTest, ScriptIsolation) {
     bidder_worklet->ReportWin(
         interest_group_name_, auction_signals_, per_buyer_signals_,
         seller_signals_, browser_signal_render_url_, browser_signal_bid_,
-        browser_signal_seller_origin_, data_version_.value_or(0),
-        data_version_.has_value(),
+        browser_signal_seller_origin_, browser_signal_top_level_seller_origin_,
+        data_version_.value_or(0), data_version_.has_value(),
         base::BindLambdaForTesting(
             [&run_loop](const absl::optional<GURL>& report_url,
                         const std::vector<std::string>& errors) {
