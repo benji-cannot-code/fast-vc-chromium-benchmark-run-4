4aa7d059da2ca31fb1a4d095238f5973322d86d4
Eric Sum <esum@google.com>
2022-02-28 21:26:22 +0000

    Add weather/time to ambient animations.
    There are 3 major UX differences from the current screensaver:
    * Weather/time should appear in the top-left instead of bottom-left.
    * There should be 28 pixels of padding in both x and y directions
      from the top-left of the screen.
    * The time's font is 32px instead of 64px.
    
    Bug: b:217271405
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I5ae807af57bad004a3c2a543ab20201b5f019943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491319
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#975942}

--- ash/ambient/ui/ambient_animation_view_unittest.cc
+++ ash/ambient/ui/ambient_animation_view_unittest.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ambient/model/ambient_animation_photo_config.h"
 #include "ash/ambient/model/ambient_backend_model.h"
+#include "ash/ambient/test/ambient_ash_test_base.h"
 #include "ash/ambient/test/ambient_test_util.h"
 #include "ash/ambient/test/fake_ambient_animation_static_resources.h"
 #include "ash/ambient/test/mock_ambient_view_event_handler.h"
+#include "ash/ambient/ui/ambient_view_delegate.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
@@ -27,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image_unittest_util.h"
-#include "ui/views/test/views_test_base.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
@@ -56,14 +57,13 @@ const cc::DrawSkottieOp* FindSkottieOp(
 
 }  // namespace
 
-class AmbientAnimationViewTest : public views::ViewsTestBase {
+class AmbientAnimationViewTest : public AmbientAshTestBase,
+                                 public AmbientViewDelegate {
  protected:
-  AmbientAnimationViewTest()
-      : views::ViewsTestBase(
-            base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
+  AmbientAnimationViewTest() {}
 
   void SetUp() override {
-    ViewsTestBase::SetUp();
+    AmbientAshTestBase::SetUp();
 
     auto static_resources =
         std::make_unique<FakeAmbientAnimationStaticResources>();
@@ -82,9 +82,20 @@ class AmbientAnimationViewTest : public views::ViewsTestBase {
 
     widget_ = CreateTestWidget();
     view_ = widget_->GetRootView()->AddChildView(
-        std::make_unique<AmbientAnimationView>(model_.get(), &event_handler_,
+        std::make_unique<AmbientAnimationView>(this,
                                                std::move(static_resources)));
   }
+
+  // AmbientViewDelegate implementation:
+  void AddObserver(AmbientViewDelegateObserver* observer) override {}
+  void RemoveObserver(AmbientViewDelegateObserver* observer) override {}
+  AmbientBackendModel* GetAmbientBackendModel() override {
+    return model_.get();
+  }
+  AmbientViewEventHandler* GetAmbientViewEventHandler() override {
+    return &event_handler_;
+  }
+  void OnPhotoTransitionAnimationCompleted() override {}
 
   MockAmbientViewEventHandler event_handler_;
   std::unique_ptr<AmbientBackendModel> model_;
@@ -118,23 +129,6 @@ TEST_F(AmbientAnimationViewTest,
   EXPECT_CALL(event_handler_,
               OnMarkerHit(AmbientPhotoConfig::Marker::kUiCycleEnded));
   task_environment()->FastForwardBy(cc::kLottieDataWith2AssetsDuration);
-  Mock::VerifyAndClearExpectations(&event_handler_);
-}
-
-TEST_F(AmbientAnimationViewTest, WaitsForViewBoundariesBeforeRendering) {
-  gfx::Rect widget_bounds = widget_->GetWindowBoundsInScreen();
-
-  widget_->Show();
-
-  EXPECT_CALL(event_handler_, OnMarkerHit(_)).Times(0);
-  task_environment()->FastForwardBy(cc::kLottieDataWith2AssetsDuration * 0.1);
-  Mock::VerifyAndClearExpectations(&event_handler_);
-
-  view_->SetBoundsRect(widget_bounds);
-
-  EXPECT_CALL(event_handler_,
-              OnMarkerHit(AmbientPhotoConfig::Marker::kUiStartRendering));
-  task_environment()->FastForwardBy(cc::kLottieDataWith2AssetsDuration * 0.1);
   Mock::VerifyAndClearExpectations(&event_handler_);
 }
 
