be494b3e10f12e7322ee7cf04e453d26b35ec34e
David Sanders <dsanders11@ucsbalum.com>
2022-02-28 21:58:04 +0000

    Remove unused include of image_resource.h
    Bug: 242216
    Change-Id: I8951df0b46c582c806fa190a380b6100dfb10ffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491812
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975958}

--- third_party/blink/renderer/core/html/canvas/image_element_base.cc
+++ third_party/blink/renderer/core/html/canvas/image_element_base.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/image_loader.h"
 #include "third_party/blink/renderer/core/svg/graphics/svg_image_for_container.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/blink/renderer/platform/loader/fetch/resource_response.h"
 
 namespace blink {
 
--- third_party/blink/renderer/core/loader/image_loader.h
+++ third_party/blink/renderer/core/loader/image_loader.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/loader/resource/image_resource.h"
 #include "third_party/blink/renderer/core/loader/resource/image_resource_content.h"
 #include "third_party/blink/renderer/core/loader/resource/image_resource_observer.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
