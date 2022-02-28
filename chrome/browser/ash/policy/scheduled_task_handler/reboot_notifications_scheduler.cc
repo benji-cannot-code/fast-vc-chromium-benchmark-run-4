0226d0f71decf2cb61ca1054f85339bd6835c78c
Sanja Perisic <sanjaperisic@chromium.org>
2022-02-28 10:08:52 +0000

    Add fake reboot notifications scheduler
    Extract RebootNotificationScheduler class for testing so that it can be reused in other tests.
    
    Bug: 1293741
    Change-Id: I92423fca6fb87a3d5c212c6e6e4d8d3fe67bb005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490850
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Sanja Perisic <sanjaperisic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975705}

--- chrome/browser/ash/policy/scheduled_task_handler/reboot_notifications_scheduler.cc
+++ chrome/browser/ash/policy/scheduled_task_handler/reboot_notifications_scheduler.cc
@@ -25,12 +25,6 @@ RebootNotificationsScheduler::RebootNotificationsScheduler()
     : RebootNotificationsScheduler(base::DefaultClock::GetInstance(),
                                    base::DefaultTickClock::GetInstance()) {}
 
-RebootNotificationsScheduler::RebootNotificationsScheduler(
-    const base::Clock* clock,
-    const base::TickClock* tick_clock)
-    : notification_timer_(clock, tick_clock),
-      dialog_timer_(clock, tick_clock) {}
-
 RebootNotificationsScheduler::~RebootNotificationsScheduler() = default;
 
 void RebootNotificationsScheduler::ScheduleNotifications(
@@ -83,6 +77,17 @@ bool RebootNotificationsScheduler::ShouldApplyGraceTime(
   return ((delay + GetSystemUptime()) <= kGraceTime);
 }
 
+RebootNotificationsScheduler::RebootNotificationsScheduler(
+    const base::Clock* clock,
+    const base::TickClock* tick_clock)
+    : notification_timer_(clock, tick_clock),
+      dialog_timer_(clock, tick_clock) {}
+
+void RebootNotificationsScheduler::OnRebootButtonClicked() {
+  DCHECK(reboot_callback_);
+  std::move(reboot_callback_).Run();
+}
+
 void RebootNotificationsScheduler::MaybeShowNotification() {
   notification_controller_.MaybeShowPendingRebootNotification(
       reboot_time_,
@@ -95,11 +100,6 @@ void RebootNotificationsScheduler::MaybeShowDialog() {
       reboot_time_,
       base::BindOnce(&RebootNotificationsScheduler::OnRebootButtonClicked,
                      base::Unretained(this)));
-}
-
-void RebootNotificationsScheduler::OnRebootButtonClicked() {
-  DCHECK(reboot_callback_);
-  std::move(reboot_callback_).Run();
 }
 
 const base::Time RebootNotificationsScheduler::GetCurrentTime() const {
