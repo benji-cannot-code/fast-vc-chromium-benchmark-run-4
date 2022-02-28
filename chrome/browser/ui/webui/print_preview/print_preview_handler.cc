4a5fe966eb6f13d22f73a88d87027dc38548186e
Lei Zhang <thestig@chromium.org>
2022-02-28 17:56:57 +0000

    Remove chrome/common/cloud_print.
    This directory now only contains dead code.
    
    Bug: 1162164
    Change-Id: I5bd9b00f27d423a6303ddeac15bce39a81149c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491816
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975835}

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/print_preview/printer_handler.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/cloud_print/cloud_print_constants.h"
 #include "chrome/common/crash_keys.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/printing/printer_capabilities.h"
