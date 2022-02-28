d2248e89d001c407c8a8d34d7b56ff63e6e3eafa
Joe Mason <joenotcharles@google.com>
2022-02-28 18:45:04 +0000

    Log a trace event periodically while the SoftwareReporter is running
    R=etienneb
    
    Bug: 1300191
    Change-Id: Iae17411c64404ab12add2dcdfe6b489dc67e636f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3484162
    Quick-Run: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975859}

--- chrome/browser/safe_browsing/chrome_cleaner/reporter_runner_browsertest_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/reporter_runner_browsertest_win.cc
@@ -260,9 +260,18 @@ class ReporterRunnerTest
     return base::Process::Current();
   }
 
-  int WaitForReporterExit([
-      [maybe_unused]] const base::Process& reporter_process) const {
-    return exit_code_to_report_;
+  bool WaitForReporterExit(
+      [[maybe_unused]] const base::Process& reporter_process,
+      base::TimeDelta timeout,
+      int* exit_code) {
+    if (reporter_wait_count_++ < 2 * reporter_launch_count_) {
+      // Simulate a timeout to test the path where ReporterRunner waits more
+      // than once.
+      return false;
+    }
+    if (exit_code)
+      *exit_code = exit_code_to_report_;
+    return true;
   }
 
   // Returns the test's idea of the current time.
@@ -291,6 +300,7 @@ class ReporterRunnerTest
   void ResetReporterRuns(int exit_code_to_report) {
     exit_code_to_report_ = exit_code_to_report;
     reporter_launch_count_ = 0;
+    reporter_wait_count_ = 0;
     reporter_launch_parameters_.clear();
     dialog_controller_created_ = false;
   }
@@ -585,6 +595,7 @@ class ReporterRunnerTest
 
   bool dialog_controller_created_ = false;
   int reporter_launch_count_ = 0;
+  int reporter_wait_count_ = 0;
   std::vector<SwReporterInvocation> reporter_launch_parameters_;
   int exit_code_to_report_ = kReporterNotLaunchedExitCode;
 
