a8be6d5e05007b4fca3950601653c22fd36469ac
dpapad <dpapad@chromium.org>
2022-02-28 11:35:43 +0000

    WebUI: Migrate cr_components/customize_themes/ tests to TypeScript.
    Bug: 1189595
    Change-Id: I9d678aaa1a99947e293b92221c596b057f61f0f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488628
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975718}

--- ui/webui/resources/cr_components/customize_themes/browser_proxy.ts
+++ ui/webui/resources/cr_components/customize_themes/browser_proxy.ts
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * interact with the browser.
  */
 
-import {CustomizeThemesClientCallbackRouter, CustomizeThemesHandlerFactory, CustomizeThemesHandlerRemote} from './customize_themes.mojom-webui.js';
+import {CustomizeThemesClientCallbackRouter, CustomizeThemesHandlerFactory, CustomizeThemesHandlerInterface, CustomizeThemesHandlerRemote} from './customize_themes.mojom-webui.js';
 
 export interface CustomizeThemesBrowserProxy {
-  handler(): CustomizeThemesHandlerRemote;
+  handler(): CustomizeThemesHandlerInterface;
   callbackRouter(): CustomizeThemesClientCallbackRouter;
   open(url: string): void;
 }
