377bd27a75d33b09348c80b15b63a8244fabdc06
K. Moon <kmoon@chromium.org>
2022-02-28 19:32:42 +0000

    [unseasoned-pdf] Remove #pdf-unseasoned flag
    Removes support for chrome://flags/#pdf-unseasoned.
    
    Bug: 1300608
    Change-Id: I9d8e2a3235f1f73b462bf01fa6c7a805096b40c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488579
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Auto-Submit: K. Moon <kmoon@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975885}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1149,9 +1149,6 @@ extern const char kPasswordDomainCapabilitiesFetchingDescription[];
 extern const char kPasswordScriptsFetchingName[];
 extern const char kPasswordScriptsFetchingDescription[];
 
-extern const char kPdfUnseasonedName[];
-extern const char kPdfUnseasonedDescription[];
-
 extern const char kPdfXfaFormsName[];
 extern const char kPdfXfaFormsDescription[];
 
