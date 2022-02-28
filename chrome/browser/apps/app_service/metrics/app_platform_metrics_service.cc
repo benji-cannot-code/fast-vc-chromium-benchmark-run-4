6a3a6e8f02b113239cd82162fa2718d1a3c9ca6c
Nancy Wang <nancylingwang@chromium.org>
2022-02-28 01:13:05 +0000

    Save input events to the user pref.
    This CL modifies AppPlatformInputMetrics to save unreported input events
    to the user pref each 5 minutes, and clear the user pref each
    2 hours after record the input events UKM.
    
    Tests will be added in the follow up CL.
    
    BUG=1299978
    
    Change-Id: I1b3866f8a5cd732488420f5bb4ff34c01f71b094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3493196
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975616}

--- chrome/browser/apps/app_service/metrics/app_platform_metrics_service.cc
+++ chrome/browser/apps/app_service/metrics/app_platform_metrics_service.cc
@@ -49,6 +49,7 @@ void AppPlatformMetricsService::RegisterProfilePrefs(
   registry->RegisterDictionaryPref(kAppRunningDuration);
   registry->RegisterDictionaryPref(kAppActivatedCount);
   registry->RegisterDictionaryPref(kAppUsageTime);
+  registry->RegisterDictionaryPref(kAppInputEventsKey);
 }
 
 // static
