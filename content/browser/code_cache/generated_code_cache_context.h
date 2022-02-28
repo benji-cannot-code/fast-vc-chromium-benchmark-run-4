e27865d89500a94a9b7eaf10a006cd2b4c0e5590
Christian Dullweber <dullweber@chromium.org>
2022-02-28 10:13:14 +0000

    CodeCache: Reset members on the right thread
    The members of GeneratedCodeCacheContext are always accessed on a
    specific thread, so they also have to be reset on that thread to avoid
    TSAN failures (and potentially real crashes).
    
    Bug: 1298670, 1285414
    Change-Id: I273ed08f343844969823fb347992b39518a5437f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472846
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975706}

--- content/browser/code_cache/generated_code_cache_context.h
+++ content/browser/code_cache/generated_code_cache_context.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
+#include "base/thread_annotations.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -57,15 +58,18 @@ class CONTENT_EXPORT GeneratedCodeCacheContext
   ~GeneratedCodeCacheContext();
 
   void InitializeOnThread(const base::FilePath& path, int max_bytes);
+  void ShutdownOnThread();
 
   // Created, used and deleted on the code cache thread.
   std::unique_ptr<GeneratedCodeCache, base::OnTaskRunnerDeleter>
-      generated_js_code_cache_{nullptr, base::OnTaskRunnerDeleter(nullptr)};
+      generated_js_code_cache_ GUARDED_BY_CONTEXT(sequence_checker_) = {
+          nullptr, base::OnTaskRunnerDeleter(nullptr)};
   std::unique_ptr<GeneratedCodeCache, base::OnTaskRunnerDeleter>
-      generated_wasm_code_cache_{nullptr, base::OnTaskRunnerDeleter(nullptr)};
+      generated_wasm_code_cache_ GUARDED_BY_CONTEXT(sequence_checker_) = {
+          nullptr, base::OnTaskRunnerDeleter(nullptr)};
   std::unique_ptr<GeneratedCodeCache, base::OnTaskRunnerDeleter>
-      generated_webui_js_code_cache_{nullptr,
-                                     base::OnTaskRunnerDeleter(nullptr)};
+      generated_webui_js_code_cache_ GUARDED_BY_CONTEXT(sequence_checker_) = {
+          nullptr, base::OnTaskRunnerDeleter(nullptr)};
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   SEQUENCE_CHECKER(sequence_checker_);
