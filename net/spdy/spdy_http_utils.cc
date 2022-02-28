2d4f0f0de888522a2cd448fbafeda2d789d8eeb1
Bence Béky <bnc@chromium.org>
2022-02-28 17:04:40 +0000

    Make string manipulation more performant in spdy_http_utils.cc.
    Eliminate unnecessary copies in `status`, `value`, and `tval`.  Use
    StrCat() and StrAppend() with all available fragments at once instead of
    std::string::append() and push_back() one by one, because it can reserve
    the final size in advance, eliminating potential multiple resizes.
    
    Change-Id: I47ec22d0580521721bf50d21eaefae0b14283730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491743
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#975805}

--- net/spdy/spdy_http_utils.cc
+++ net/spdy/spdy_http_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/abseil_string_conversions.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
@@ -33,10 +34,8 @@ void AddSpdyHeader(const std::string& name,
   if (headers->find(name) == headers->end()) {
     (*headers)[name] = value;
   } else {
-    std::string joint_value = (*headers)[name].as_string();
-    joint_value.append(1, '\0');
-    joint_value.append(value);
-    (*headers)[name] = joint_value;
+    (*headers)[name] = base::StrCat(
+        {(*headers)[name].as_string(), base::StringPiece("\0", 1), value});
   }
 }
 
@@ -50,12 +49,11 @@ int SpdyHeadersToHttpResponse(const spdy::Http2HeaderBlock& headers,
   if (it == headers.end())
     return ERR_INCOMPLETE_HTTP2_HEADERS;
 
-  std::string status(it->second);
-  std::string raw_headers("HTTP/1.1 ");
-  raw_headers.append(status);
-  raw_headers.push_back('\0');
+  const auto status = base::StringViewToStringPiece(it->second);
+  std::string raw_headers =
+      base::StrCat({"HTTP/1.1 ", status, base::StringPiece("\0", 1)});
   for (it = headers.begin(); it != headers.end(); ++it) {
-    auto name = std::string(it->first);
+    const auto name = base::StringViewToStringPiece(it->first);
     DCHECK_GT(name.size(), 0u);
     if (name[0] == ':') {
       // https://tools.ietf.org/html/rfc7540#section-8.1.2.4
@@ -70,21 +68,18 @@ int SpdyHeadersToHttpResponse(const spdy::Http2HeaderBlock& headers,
     // becomes
     //    Set-Cookie: foo\0
     //    Set-Cookie: bar\0
-    auto value = std::string(it->second);
+    const auto value = base::StringViewToStringPiece(it->second);
     size_t start = 0;
     size_t end = 0;
     do {
-      raw_headers.append(name);
-      raw_headers.push_back(':');
-
       end = value.find('\0', start);
-      std::string tval;
+      base::StringPiece tval;
       if (end != value.npos)
         tval = value.substr(start, (end - start));
       else
         tval = value.substr(start);
-      raw_headers.append(tval);
-      raw_headers.push_back('\0');
+      base::StrAppend(&raw_headers,
+                      {name, ":", tval, base::StringPiece("\0", 1)});
       start = end + 1;
     } while (end != value.npos);
   }
