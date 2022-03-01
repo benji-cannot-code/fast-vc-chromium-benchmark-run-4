eb80c67beb1488874bb5c5298e508efe2eb97976
Chad Duffin <chadduffin@chromium.org>
2022-03-01 22:42:28 +0000

    [CrOS Network] Re-enable system UI network tests.
    This change re-enables multiple tests that were previously failing.
    While we had attempted to debug exactly what the issue was, the root
    cause of these failures was not determined. However, when circling back
    to debug them further, all failures except one appeared to be resolved.
    The remaining failure was due to the tether state remaining as
    connecting, which then caused the network icon to be frequently updated
    which stalled the task runner.
    
    Bug: b/217762791
    Test: Locally executed many times on Linux with a debug build.
    
    Change-Id: I631feaa775dbd243866142331bfb1b7d07ee1dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495699
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976442}

--- ash/system/network/active_network_icon_unittest.cc
+++ ash/system/network/active_network_icon_unittest.cc
@@ -22,17 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/image/image_unittest_util.h"
 
-// Flaky on Chrome OS debug and Linux Chromium OS ASan LSan Tests. b/217762791
-#if !defined(NDEBUG) || defined(ADDRESS_SANITIZER)
-#define MAYBE_GetSingleImage DISABLED_GetSingleImage
-#define MAYBE_CellularUninitialized DISABLED_CellularUninitialized
-#define MAYBE_CellularScanning DISABLED_CellularScanning
-#else
-#define MAYBE_GetSingleImage GetSingleImage
-#define MAYBE_CellularUninitialized CellularUninitialized
-#define MAYBE_CellularScanning CellularScanning
-#endif
-
 using chromeos::network_config::mojom::ConnectionStateType;
 using chromeos::network_config::mojom::NetworkType;
 
@@ -209,7 +198,7 @@ TEST_F(ActiveNetworkIconTest, GetConnectionStatusStrings) {
       tooltip);
 }
 
-TEST_F(ActiveNetworkIconTest, MAYBE_GetSingleImage) {
+TEST_F(ActiveNetworkIconTest, GetSingleImage) {
   // Cellular only = Cellular icon
   SetupCellular(shill::kStateOnline);
   bool animating;
@@ -260,7 +249,7 @@ TEST_F(ActiveNetworkIconTest, MAYBE_GetSingleImage) {
   EXPECT_FALSE(animating);
 }
 
-TEST_F(ActiveNetworkIconTest, MAYBE_CellularUninitialized) {
+TEST_F(ActiveNetworkIconTest, CellularUninitialized) {
   SetCellularUninitialized(false /* scanning */);
 
   bool animating;
@@ -272,7 +261,7 @@ TEST_F(ActiveNetworkIconTest, MAYBE_CellularUninitialized) {
   EXPECT_TRUE(animating);
 }
 
-TEST_F(ActiveNetworkIconTest, MAYBE_CellularScanning) {
+TEST_F(ActiveNetworkIconTest, CellularScanning) {
   SetCellularUninitialized(true /* scanning */);
 
   ASSERT_TRUE(network_state_handler()->GetScanningByType(
