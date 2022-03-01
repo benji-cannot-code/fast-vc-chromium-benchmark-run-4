add80fdba75bc1bd0e386adbbf3b5eff3b2371f0
Eric Willigers <ericwilligers@chromium.org>
2022-03-01 05:44:29 +0000

    Test Crosapi service for Sharesheet
    We verify that SharesheetAsh can (via the SharesheetService) launch a
    share target for a given Intent.
    
    Design doc: https://bit.ly/3rtDy6s
    
    Bug: 1225825
    Change-Id: Ie9c9edca58e96debfc4ea5e4ae42c901883fcee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488753
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976108}

--- chrome/browser/ash/crosapi/sharesheet_ash.h
+++ chrome/browser/ash/crosapi/sharesheet_ash.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_CROSAPI_SHARESHEET_ASH_H_
 #define CHROME_BROWSER_ASH_CROSAPI_SHARESHEET_ASH_H_
 
+#include <string>
+
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/sharesheet/sharesheet_types.h"
 #include "chromeos/crosapi/mojom/sharesheet.mojom.h"
--- chrome/browser/web_applications/system_web_apps/test/system_web_app_browsertest_base.h
+++ chrome/browser/web_applications/system_web_apps/test/system_web_app_browsertest_base.h
@@ -110,6 +110,10 @@ class SystemWebAppManagerBrowserTest
     : public TestProfileTypeMixin<SystemWebAppBrowserTestBase> {
  public:
   explicit SystemWebAppManagerBrowserTest(bool install_mock = true);
+  SystemWebAppManagerBrowserTest(const SystemWebAppManagerBrowserTest&) =
+      delete;
+  SystemWebAppManagerBrowserTest& operator=(
+      const SystemWebAppManagerBrowserTest&) = delete;
   ~SystemWebAppManagerBrowserTest() override = default;
 };
 
