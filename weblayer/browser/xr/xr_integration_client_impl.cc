ec7b3417fe08619f5746a3db924de8eb4d540155
Piotr Bialecki <bialpio@chromium.org>
2022-03-01 18:08:57 +0000

    WebXR AR: clean up stale TODOs
    Bug: 966647
    Change-Id: I89128635a53db6a0337037be21141019e01c12c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495660
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976300}

--- weblayer/browser/xr/xr_integration_client_impl.cc
+++ weblayer/browser/xr/xr_integration_client_impl.cc
@@ -60,7 +60,6 @@ XrIntegrationClientImpl::GetInstallHelper(device::mojom::XRDeviceId device_id) {
 content::XRProviderList XrIntegrationClientImpl::GetAdditionalProviders() {
   content::XRProviderList providers;
 
-  // TODO(https://crbug.com/966647) remove this check.
   if (base::FeatureList::IsEnabled(features::kWebXrArModule)) {
     base::android::ScopedJavaLocalRef<jobject>
         j_ar_compositor_delegate_provider =
