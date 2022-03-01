357ed9efb9e118baf95747178c597db329c20c84
Ana Salazar <anasalazar@chromium.org>
2022-03-01 16:59:31 +0000

    Create animation for dismissing the privacy toast
    When the user clicks on the button for acknowledging the privacy notice
    in the continue section, animate the privacy notice's opacity to 0 and
    then slide in the contents of the continue section.
    
    Video in slow motion:
    https://photos.app.goo.gl/JDumo65xE8MnyXTu9
    
    Bug: 1277666
    Change-Id: I5375dcce480fd194abec96b67ba338dfa5fd4b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3487913
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976277}

--- ash/app_list/views/continue_task_container_view.h
+++ ash/app_list/views/continue_task_container_view.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/timer/timer.h"
 #include "ui/base/models/list_model_observer.h"
+#include "ui/compositor/layer_animator.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -68,6 +69,14 @@ class ASH_EXPORT ContinueTaskContainerView : public ui::ListModelObserver,
 
   // See AppsGridView::DisableFocusForShowingActiveFolder().
   void DisableFocusForShowingActiveFolder(bool disabled);
+
+  // Start the animation for showing the suggestions in the continue section.
+  // Suggestion views will slide in from an evenly distributed amount of
+  // `available_space` into their final positions.
+  // This animation must only be used in clamshell mode.
+  void AnimateSlideInSuggestions(int available_space,
+                                 base::TimeDelta duration,
+                                 gfx::Tween::Type tween);
 
   base::OneShotTimer* animations_timer_for_test() { return &animations_timer_; }
 
