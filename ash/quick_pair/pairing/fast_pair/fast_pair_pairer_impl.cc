ee11c4c37e886f86bc35114ede3a1d2e7df990fc
Shane Fitzpatrick <shanefitz@google.com>
2022-03-01 17:52:27 +0000

    [FastPair] Revert change to ConnectDevice address type
    After further consultation with the BT platform team, we should actually
    be setting this as null  as we were originally.
    
    TEST=Verified locally on DUT.
    
    Change-Id: Iac489239d346df068ec1f1326cfadabe2c7d06b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498380
    Auto-Submit: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#976295}

--- ash/quick_pair/pairing/fast_pair/fast_pair_pairer_impl.cc
+++ ash/quick_pair/pairing/fast_pair/fast_pair_pairer_impl.cc
@@ -231,7 +231,7 @@ void FastPairPairerImpl::OnGattClientInitializedCallback(
 
         adapter_->ConnectDevice(
             device_address,
-            device::BluetoothDevice::AddressType::ADDR_TYPE_PUBLIC,
+            /*address_type=*/absl::nullopt,
             base::BindOnce(&FastPairPairerImpl::OnConnectDevice,
                            weak_ptr_factory_.GetWeakPtr()),
             base::BindOnce(&FastPairPairerImpl::OnConnectError,
