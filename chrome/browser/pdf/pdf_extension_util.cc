9571cbc7e1b14647ebb9357fc0e2656cdaa3f310
K. Moon <kmoon@chromium.org>
2022-03-01 16:38:50 +0000

    Clean up pdf_features.h dependencies
    Removes or narrows dependencies on //pdf/pdf_features.h, and the
    corresponding //pdf:features target.
    
    Bug: 1300608
    Change-Id: I857514d30c1b182e456ef3dc92213a0ac85140cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495661
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976269}

--- chrome/browser/pdf/pdf_extension_util.cc
+++ chrome/browser/pdf/pdf_extension_util.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/zoom/page_zoom_constants.h"
-#include "pdf/pdf_features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/webui/web_ui_util.h"
--- chrome/browser/printing/print_preview_dialog_controller_browsertest.cc
+++ chrome/browser/printing/print_preview_dialog_controller_browsertest.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
 #include "ipc/ipc_message_macros.h"
-#include "pdf/pdf_features.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_switches.h"
 #include "url/gurl.h"
