d2da03cd1c2e3ea1852a70c3e52d997714f8e9f0
Wenbo Jie <wenbojie@chromium.org>
2022-03-01 05:36:08 +0000

    [FilesRecentFilter] use generated json as file type data source
    With the json data generator tool introduced in CL:3473619, we extract
    the supported file type list from the original `file_type.js` and put
    it in a standalone `file_types.json5`, which serves as a single source
    of truth for all files we support in Files app.
    
    The json data generator will generate files both in Javascript and C++,
    so they can be imported directly in both frontend and backend as the
    file type data source. This CL covers the change in the frontend, we
    just import the generated js file in file_type.js to maintain the
    existing behaviour.
    
    Bug: 1259359
    Change-Id: I6e855b3625846538c19beaebeab78d13a7a445b3
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3469537
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976103}

--- ui/file_manager/file_manager/foreground/js/file_list_model.js
+++ ui/file_manager/file_manager/foreground/js/file_list_model.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {ArrayDataModel} from 'chrome://resources/js/cr/ui/array_data_model.m.js';
 
-import {FileType} from '../../common/js/file_type.js';
+import {FileExtensionType, FileType} from '../../common/js/file_type.js';
 import {str, strf, util} from '../../common/js/util.js';
 import {EntryLocation} from '../../externs/entry_location.js';
 import {VolumeManager} from '../../externs/volume_manager.js';
@@ -82,7 +82,8 @@ export class FileListModel extends ArrayDataModel {
   }
 
   /**
-   * @param {!Object} fileType Type object returned by FileType.getType().
+   * @param {!FileExtensionType} fileType Type object returned by
+   *     FileType.getType().
    * @return {string} Localized string representation of file type.
    */
   static getFileTypeString(fileType) {
