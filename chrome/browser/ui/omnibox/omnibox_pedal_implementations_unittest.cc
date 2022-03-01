f009e683786097d082b43ee8f1426d0223297e12
Orin Jaworski <orinj@google.com>
2022-03-01 21:06:42 +0000

    [omnibox] Add some triggers in English for dino and accessibility pedals
    Fixed: 1301025
    Change-Id: I55afdec9d003c3b6ba57c8ba8bb15540e56ccd4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3496394
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976389}

--- chrome/browser/ui/omnibox/omnibox_pedal_implementations_unittest.cc
+++ chrome/browser/ui/omnibox/omnibox_pedal_implementations_unittest.cc
@@ -15980,6 +15980,7 @@ class OmniboxPedalImplementationsTest : public testing::Test {
             "play chrome dino",
             "play chrome dino game",
             "play chrome dinosaur",
+            "play dino",
             "play dino game",
             "play dino run",
         },
