c893f3e5be87af80ed08c471d12ef9410fb1b686
dan sinclair <dsinclair@chromium.org>
2022-03-01 14:49:12 +0000

    [code health] Remove use of render host creation notification
    This CL removes the use of the NotificationService to receive
    notification of a render process host creation in the
    VisitedLinkEventListener. The NotificationService is replace with the
    RenderProcessHostCreationObserver.
    
    The unittests have been updated to call the observer instead of emitting
    the notification.
    
    More details can be seen in the bug.
    
    Bug: 357627
    Change-Id: I9b920abedb5e8ab3f2580b73db260686e2eed77f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3345129
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Dan Sinclair <dsinclair@chromium.org>
    Commit-Queue: Dan Sinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976229}

--- components/visitedlink/browser/visitedlink_event_listener.h
+++ components/visitedlink/browser/visitedlink_event_listener.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/visitedlink/browser/visitedlink_writer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/render_process_host_creation_observer.h"
 
 namespace content {
 class BrowserContext;
@@ -27,8 +28,10 @@ class VisitedLinkUpdater;
 // VisitedLinkEventListener broadcasts link coloring database updates to all
 // processes. It also coalesces the updates to avoid excessive broadcasting of
 // messages to the renderers.
-class VisitedLinkEventListener : public VisitedLinkWriter::Listener,
-                                 public content::NotificationObserver {
+class VisitedLinkEventListener
+    : public VisitedLinkWriter::Listener,
+      public content::NotificationObserver,
+      public content::RenderProcessHostCreationObserver {
  public:
   explicit VisitedLinkEventListener(content::BrowserContext* browser_context);
 
@@ -44,6 +47,9 @@ class VisitedLinkEventListener : public VisitedLinkWriter::Listener,
   // Sets a custom timer to use for coalescing events for testing.
   // |coalesce_timer_override| must outlive this.
   void SetCoalesceTimerForTest(base::OneShotTimer* coalesce_timer_override);
+
+  // content::RenderProcessHostCreationObserver:
+  void OnRenderProcessHostCreated(content::RenderProcessHost* rph) override;
 
  private:
   void CommitVisitedLinks();
