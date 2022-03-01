1de258675a0e62c8c7bb4e09d3cccf0f62737202
Kevin Ellis <kevers@google.com>
2022-03-01 21:43:25 +0000

    Update no-hover-during-___-scroll tests
    The main differences between tests in this set are the scrolling
    mechanism (keyboard, mouse, or programmatic) and scroll speed
    (smooth vs instant).  This patch consolidates the tests and should
    make them easier to maintain.
    
    Note that hover-during-scroll.html has similar issues, but the fix is
    deferred to a followup patch.
    
    Bug: 1229076, 1229064, 914792
    Change-Id: I40daeb33e124ecc829c0b711bc5dae94a9af954b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3496022
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976415}

--- third_party/blink/web_tests/fast/scrolling/resources/hover-on-scroll-checker.js
+++ third_party/blink/web_tests/fast/scrolling/resources/hover-on-scroll-checker.js
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-function validateHoverState(elementList, hoverIndex, reject, message) {
+function validateHoverState(elementList, hoverIndex, hoverMismatch) {
   for (let i = 0; i < elementList.length; i++) {
     if (elementList[i].matches(':hover') != (i == hoverIndex))
-      reject(message);
+      hoverMismatch(document.scrollingElement.scrollTop);
   }
 }
 
@@ -10,6 +10,7 @@ function runHoverStateOnScrollTest(scrollCallback, targetIndex) {
   const runTest = async (resolve, reject) => {
     await waitForCompositorCommit();
 
+    const array = document.getElementsByClassName('hoverme');
     let x = array[0].offsetLeft + array[0].clientWidth / 2;
     let y = array[0].offsetTop + array[0].clientHeight / 2;
     // Move cursor to 1st element.
@@ -20,30 +21,46 @@ function runHoverStateOnScrollTest(scrollCallback, targetIndex) {
 
     assert_equals(document.scrollingElement.scrollTop, 0);
 
-    validateHoverState(array, 0, reject,
-                       'Not hovering over the first element');
+    validateHoverState(array, 0, () => {
+      reject('Not hovering over the first element');
+    });
 
     // While scrolling the hover state shouldn't change. Note, this
     // behavior is not addressed in the specs. The rationale is to prevent the
     // scroll from potentially affecting layout.
-    const hoverStateCheck =  () => {
-      validateHoverState(array, 0, reject,
-                         'Unexpected change to hover state during scroll');
+    let firstHoverUpdate = undefined;
+    const hoverStateCheck = () => {
+      validateHoverState(array, 0, () => {
+        if (!firstHoverUpdate) {
+          firstHoverUpdate = document.scrollingElement.scrollTop;
+        };
+      });
     };
+
     const scrollListener =
         document.addEventListener('scroll', hoverStateCheck);
 
     const scrollEndPromise = waitForScrollendEvent(document);
-    scrollCallback();
+    await scrollCallback(x, y);
     await scrollEndPromise;
+
+    // If the hover change occurs after the last scroll event then
+    // firstHoverUpdate remains undefined.  If the last scroll event is delayed
+    // such that it occurs after the hover state update, that is OK provided
+    // that we have reached the end of the scroll.
+    if (firstHoverUpdate &&
+        firstHoverUpdate != document.scrollingElement.scrollTop) {
+      reject('Unexpected change to hover state during scroll');
+    }
 
     document.removeEventListener('scroll', scrollListener);
 
     await waitForCompositorCommit();
 
     // Once scrolling is complete, the hover state must update.
-    validateHoverState(array, targetIndex, reject,
-                       'Hover state must be updated after scroll ends');
+    validateHoverState(array, targetIndex, () => {
+      reject('Hover state must be updated after scroll ends');
+    });
     resolve();
   };
   return new Promise((resolve, reject) => {
