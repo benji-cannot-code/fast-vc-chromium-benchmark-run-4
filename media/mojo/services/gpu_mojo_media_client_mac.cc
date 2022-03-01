660763945b8ee795e2ff0d6a9cb6df07aa3dca55
Dale Curtis <dalecurtis@chromium.org>
2022-03-01 00:02:58 +0000

    Implement xHE-AAC decoding via AudioToolbox on macOS 10.15+
    This uses the macOS AudioConverter interface to decode xHE-AAC
    streams into planar floating point. Overall the interface is
    very similar to our own media::AudioConverter, but works with
    encoded data!
    
    This adds a couple of exceptions to the GPU sandbox in order
    to facilitate audio decoding.
    
    Bug: 1289825
    Change-Id: If9335f881992b8e651184a88635a30acf2625901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3258346
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976010}

--- media/mojo/services/gpu_mojo_media_client_mac.cc
+++ media/mojo/services/gpu_mojo_media_client_mac.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "media/base/audio_decoder.h"
+#include "media/filters/mac/audio_toolbox_audio_decoder.h"
 #include "media/gpu/ipc/service/vda_video_decoder.h"
 #include "media/mojo/services/gpu_mojo_media_client.h"
 
@@ -28,7 +29,7 @@ GetPlatformSupportedVideoDecoderConfigs(
 
 std::unique_ptr<AudioDecoder> CreatePlatformAudioDecoder(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
-  return nullptr;
+  return std::make_unique<AudioToolboxAudioDecoder>();
 }
 
 // This class doesn't exist on mac, so we need a stub for unique_ptr.
