df3cb59f32b4b7388c1b697f1439e0bd631fc3f8
Ryan Sturm <ryansturm@chromium.org>
2022-03-01 21:03:39 +0000

    [search prefetch] remove prefetch response container
    Class is unused after parent change.
    
    Bug: 1299062
    Change-Id: Ie8e89f70a59dd5bc7dd6a20a216e19d7c598181b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489381
    Reviewed-by: Simon Pelchat <spelchat@google.com>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976386}

--- chrome/browser/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
+++ chrome/browser/prefetch/search_prefetch/search_prefetch_url_loader_interceptor.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
-#include "chrome/browser/prefetch/search_prefetch/prefetched_response_container.h"
 #include "chrome/browser/prefetch/search_prefetch/search_prefetch_service.h"
 #include "chrome/browser/prefetch/search_prefetch/search_prefetch_service_factory.h"
 #include "chrome/browser/prefetch/search_prefetch/search_prefetch_url_loader.h"
