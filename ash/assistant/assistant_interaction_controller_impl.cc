5178a134c4d209299bea37b78d187291def69717
Toni Barzic <tbarzic@chromium.org>
2022-03-01 04:45:47 +0000

    Stop requesting screen context when launcher opens
    Stops screen context requests for assistant when launcher opens, of
    assistant UI gets shown. This was used for an assistant feature that got
    removed ("What's on my screen"), It's not needed any longer.
    
    Note that requests sent to ARC were causing a transparent app window to
    pop up for a moment, taking focus from launcher and causing launcher to
    close spontaneously.
    
    BUG=1298267
    
    Change-Id: I18027c7e88398ea2930123c44d96bbfdcb44f8de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3496222
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976095}

--- ash/assistant/assistant_interaction_controller_impl.cc
+++ ash/assistant/assistant_interaction_controller_impl.cc
@@ -265,9 +265,7 @@ void AssistantInteractionControllerImpl::OnHighlighterSelectionRecognized(
   DCHECK(AssistantState::Get()->IsScreenContextAllowed());
 
   AssistantUiController::Get()->ShowUi(AssistantEntryPoint::kStylus);
-  StartScreenContextInteraction(
-      /*include_assistant_structure=*/false, rect,
-      AssistantQuerySource::kStylus);
+  StartScreenContextInteraction(rect, AssistantQuerySource::kStylus);
 }
 
 void AssistantInteractionControllerImpl::OnInteractionStateChanged(
@@ -794,7 +792,6 @@ void AssistantInteractionControllerImpl::OnUiVisible(
 }
 
 void AssistantInteractionControllerImpl::StartScreenContextInteraction(
-    bool include_assistant_structure,
     const gfx::Rect& region,
     AssistantQuerySource query_source) {
   StopActiveInteraction(false);
@@ -804,7 +801,7 @@ void AssistantInteractionControllerImpl::StartScreenContextInteraction(
       query_source));
 
   assistant_controller_->screen_context_controller()->RequestScreenContext(
-      include_assistant_structure, region,
+      region,
       base::BindOnce(
           [](const base::WeakPtr<AssistantInteractionControllerImpl>& self,
              ax::mojom::AssistantStructurePtr assistant_structure,
