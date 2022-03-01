e86fb45575d51c357bb74d8f36c8ebe42796f442
Jarryd <jarrydg@chromium.org>
2022-03-01 16:32:27 +0000

    PermissionsPolicy: Rename FeaturePolicyNode.
    This change moves FeaturePolicyNode out of ParsingContext, and into the
    public section of PermissionsPolicyParser so that it can be used by
    the ManifestParser. The ManifestParser reads the permissions_policy
    field and constructs a temporary object, PermissionsPolicyNode, to
    hand over to the PermissionsPolicyParser.
    
    This change also renames FeaturePolicy(Declaration)?Node to match
    the change from Feature Policy to Permissions Policy.
    
    Bug: 1238341
    Change-Id: I5e3a96aaf7fd177ce9fcce70f5fcdce28f290230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482237
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976266}

--- third_party/blink/renderer/core/permissions_policy/permissions_policy_parser.cc
+++ third_party/blink/renderer/core/permissions_policy/permissions_policy_parser.cc
@@ -98,25 +98,17 @@ class ParsingContext {
   ParsedPermissionsPolicy ParsePermissionsPolicy(const String& policy);
 
  private:
-  // Following is the intermediate represetnation(IR) of permissions policy.
-  // Parsing of syntax structures is done in this IR, but semantic checks, e.g.
-  // whether feature_name is valid, are not yet performed.
-  struct FeaturePolicyDeclarationNode {
-    String feature_name;
-    Vector<String> allowlist;
-  };
-  using FeaturePolicyNode = Vector<FeaturePolicyDeclarationNode>;
-
-  ParsedPermissionsPolicy ParseIR(const FeaturePolicyNode& root);
-  FeaturePolicyNode ParseFeaturePolicyToIR(const String& policy);
-  FeaturePolicyNode ParsePermissionsPolicyToIR(const String& policy);
+  ParsedPermissionsPolicy ParseIR(const PermissionsPolicyParser::Node& root);
+  PermissionsPolicyParser::Node ParseFeaturePolicyToIR(const String& policy);
+  PermissionsPolicyParser::Node ParsePermissionsPolicyToIR(
+      const String& policy);
 
   // normally 1 char = 1 byte
   // max length to parse = 2^16 = 64 kB
   static constexpr wtf_size_t MAX_LENGTH_PARSE = 1 << 16;
 
   absl::optional<ParsedPermissionsPolicyDeclaration> ParseFeature(
-      const FeaturePolicyDeclarationNode&);
+      const PermissionsPolicyParser::Declaration&);
 
   struct ParsedAllowlist {
     std::vector<url::Origin> allowed_origins;
@@ -374,7 +366,7 @@ ParsingContext::ParsedAllowlist ParsingContext::ParseAllowlist(
 }
 
 absl::optional<ParsedPermissionsPolicyDeclaration> ParsingContext::ParseFeature(
-    const FeaturePolicyDeclarationNode& declaration_node) {
+    const PermissionsPolicyParser::Declaration& declaration_node) {
   absl::optional<mojom::blink::PermissionsPolicyFeature> feature =
       ParseFeatureName(declaration_node.feature_name);
   if (!feature)
@@ -405,10 +397,9 @@ ParsedPermissionsPolicy ParsingContext::ParsePermissionsPolicy(
 }
 
 ParsedPermissionsPolicy ParsingContext::ParseIR(
-    const ParsingContext::FeaturePolicyNode& root) {
+    const PermissionsPolicyParser::Node& root) {
   ParsedPermissionsPolicy parsed_policy;
-  for (const ParsingContext::FeaturePolicyDeclarationNode& declaration_node :
-       root) {
+  for (const PermissionsPolicyParser::Declaration& declaration_node : root) {
     absl::optional<ParsedPermissionsPolicyDeclaration> parsed_feature =
         ParseFeature(declaration_node);
     if (parsed_feature) {
@@ -421,9 +412,9 @@ ParsedPermissionsPolicy ParsingContext::ParseIR(
   return parsed_policy;
 }
 
-ParsingContext::FeaturePolicyNode ParsingContext::ParseFeaturePolicyToIR(
+PermissionsPolicyParser::Node ParsingContext::ParseFeaturePolicyToIR(
     const String& policy) {
-  ParsingContext::FeaturePolicyNode root;
+  PermissionsPolicyParser::Node root;
 
   if (policy.length() > MAX_LENGTH_PARSE) {
     logger_.Error("Feature policy declaration exceeds size limit(" +
@@ -470,7 +461,7 @@ ParsingContext::FeaturePolicyNode ParsingContext::ParseFeaturePolicyToIR(
       if (tokens.IsEmpty())
         continue;
 
-      ParsingContext::FeaturePolicyDeclarationNode declaration_node;
+      PermissionsPolicyParser::Declaration declaration_node;
       // Break tokens into head & tail, where
       // head = feature_name
       // tail = allowlist
@@ -486,7 +477,7 @@ ParsingContext::FeaturePolicyNode ParsingContext::ParseFeaturePolicyToIR(
   return root;
 }
 
-ParsingContext::FeaturePolicyNode ParsingContext::ParsePermissionsPolicyToIR(
+PermissionsPolicyParser::Node ParsingContext::ParsePermissionsPolicyToIR(
     const String& policy) {
   if (policy.length() > MAX_LENGTH_PARSE) {
     logger_.Error("Permissions policy declaration exceeds size limit(" +
@@ -503,7 +494,7 @@ ParsingContext::FeaturePolicyNode ParsingContext::ParsePermissionsPolicyToIR(
     return {};
   }
 
-  ParsingContext::FeaturePolicyNode ir_root;
+  PermissionsPolicyParser::Node ir_root;
   for (const auto& feature_entry : root.value()) {
     const auto& key = feature_entry.first;
     const char* feature_name = key.c_str();
@@ -555,7 +546,7 @@ ParsingContext::FeaturePolicyNode ParsingContext::ParsePermissionsPolicyToIR(
       allowlist.push_back("'none'");
 
     ir_root.push_back(
-        ParsingContext::FeaturePolicyDeclarationNode{feature_name, allowlist});
+        PermissionsPolicyParser::Declaration{feature_name, allowlist});
   }
 
   return ir_root;
