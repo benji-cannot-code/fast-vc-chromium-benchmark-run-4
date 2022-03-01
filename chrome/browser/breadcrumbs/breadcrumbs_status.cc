33e57eefdc3f9f6c4aa41df9eac11216a0b85bf1
Jesse McKenna <jessemckenna@google.com>
2022-03-01 20:58:44 +0000

    breadcrumbs: don't reenable if ever disabled
    The breadcrumbs feature is only enabled if UMA consent is provided.
    The logic to gate the feature behind UMA consent was written with the
    assumption that changing UMA consent required restarting the browser.
    However, that is not the case, particularly on Chrome OS.
    
    In light of that, this change prevents the feature from being
    reenabled if it was ever disabled. Breadcrumbs depends on components
    created at startup, e.g., in the global browser process, which will
    not have been created if breadcrumbs were disabled at the time of
    startup. This is suspected to cause some null-dereference crashes on
    Chrome OS, e.g., crbug.com/1287692.
    
    Bug: 1287692, 1163762
    Change-Id: I1d54669a8472912d1fde834bc18452ee42c96054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3493025
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#976381}

--- chrome/browser/breadcrumbs/breadcrumbs_status.cc
+++ chrome/browser/breadcrumbs/breadcrumbs_status.cc
@@ -10,8 +10,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/breadcrumbs/core/features.h"
 
 bool BreadcrumbsStatus::IsEnabled() {
-  return base::FeatureList::IsEnabled(breadcrumbs::kLogBreadcrumbs) &&
-         IsMetricsAndCrashReportingEnabled();
+  static bool is_enabled = true;
+  // If `is_enabled` is ever false, it will stay false to prevent parts of
+  // breadcrumbs trying to run (e.g., helpers on new tabs, browser agents on new
+  // windows) when others are uninitialized (e.g., the browser process logger).
+  // In other words, breadcrumbs is only enabled if it has been continuously
+  // enabled since Chrome started.
+  if (!is_enabled)
+    return is_enabled;
+  is_enabled = base::FeatureList::IsEnabled(breadcrumbs::kLogBreadcrumbs) &&
+               IsMetricsAndCrashReportingEnabled();
+  return is_enabled;
 }
 
 bool BreadcrumbsStatus::IsMetricsAndCrashReportingEnabled() {
