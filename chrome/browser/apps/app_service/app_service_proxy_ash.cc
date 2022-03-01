28e08d77d24e449fda07aa7d853b849e5a3d47c7
Nancy Wang <nancylingwang@chromium.org>
2022-03-01 03:00:59 +0000

    Replace AppUpdate::GetPaused with AppUpdate::Paused.
    This CL modifies AppUpdate::Paused to get value from the non mojom App
    struct when it is available, and remove AppUpdate::GetPaused.
    
    Modify tests to use and verify AppUpdate::Paused.
    
    BUG=1253250
    
    Change-Id: I30037ea04c23ee294ef5f3155ba1a74a84482b00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495722
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976063}

--- chrome/browser/apps/app_service/app_service_proxy_ash.cc
+++ chrome/browser/apps/app_service/app_service_proxy_ash.cc
@@ -166,7 +166,7 @@ void AppServiceProxyAsh::PauseApps(
 
     app_registry_cache_.ForOneApp(
         data.first, [this](const apps::AppUpdate& update) {
-          if (update.Paused() != apps::mojom::OptionalBool::kTrue) {
+          if (!update.Paused().value_or(false)) {
             pending_pause_requests_.MaybeAddApp(update.AppId());
           }
         });
@@ -351,7 +351,7 @@ bool AppServiceProxyAsh::MaybeShowLaunchPreventionDialog(
 
   // Return true, and load the icon for the app pause dialog when the app
   // is paused.
-  if (update.Paused() == apps::mojom::OptionalBool::kTrue ||
+  if (update.Paused().value_or(false) ||
       pending_pause_requests_.IsPaused(update.AppId())) {
     ash::app_time::AppTimeLimitInterface* app_limit =
         ash::app_time::AppTimeLimitInterface::Get(profile_);
@@ -463,7 +463,7 @@ void AppServiceProxyAsh::OnPauseDialogClosed(apps::mojom::AppType app_type,
   if (!should_pause_app) {
     app_registry_cache_.ForOneApp(
         app_id, [&should_pause_app](const apps::AppUpdate& update) {
-          if (update.Paused() == apps::mojom::OptionalBool::kTrue) {
+          if (update.Paused().value_or(false)) {
             should_pause_app = true;
           }
         });
@@ -474,8 +474,7 @@ void AppServiceProxyAsh::OnPauseDialogClosed(apps::mojom::AppType app_type,
 }
 
 void AppServiceProxyAsh::OnAppUpdate(const apps::AppUpdate& update) {
-  if ((update.PausedChanged() &&
-       update.Paused() == apps::mojom::OptionalBool::kTrue) ||
+  if ((update.PausedChanged() && update.Paused().value_or(false)) ||
       (update.ReadinessChanged() &&
        !apps_util::IsInstalled(update.Readiness()))) {
     pending_pause_requests_.MaybeRemoveApp(update.AppId());
