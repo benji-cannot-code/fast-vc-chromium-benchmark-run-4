9a0c33bbbf949f9e6fd09a6ef2e39536f3c5195e
Mike Pinkerton <pinkerton@google.com>
2022-03-01 17:01:01 +0000

    [Swift][Interop] Add namespace tests.
    Add tests for C++ namespaces.
    
    Change-Id: Ic7941c070568c8ba612e5ac374078c9974fd5d2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495344
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976279}

---
+++ ios/chrome/test/swift_interop/namespace/namespace.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_TEST_SWIFT_INTEROP_NAMESPACE_NAMESPACE_H_
+#define IOS_CHROME_TEST_SWIFT_INTEROP_NAMESPACE_NAMESPACE_H_
+
+class Goat {
+ public:
+  int GetValue() const { return 7; }
+};
+
+enum class Foo { cat, dog, goat };
+
+namespace space {
+
+class Goat {
+ public:
+  void DoNothing() const {}
+  int GetValue() const { return 42; }
+};
+
+enum class Animal { cat, dog, goat };
+enum Vehicle { car, boat, bike };
+enum { kPen, kPencil };
+
+}  // namespace space
+
+namespace outer {
+
+namespace inner {
+
+class NestedGoat {
+ public:
+  int GetValue() const { return 50; }
+  space::Animal GetAnimal() { return space::Animal::goat; }
+};
+
+}  // namespace inner
+
+}  // namespace outer
+
+#endif  // IOS_CHROME_TEST_SWIFT_INTEROP_NAMESPACE_NAMESPACE_H_
