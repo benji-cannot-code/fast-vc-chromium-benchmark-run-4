75d1eb39dbb2f0ce525d8c77dd474da5ec5fe702
Wenbo Jie <wenbojie@chromium.org>
2022-03-01 02:42:39 +0000

    [FilesRecentFilter] add json data generator tool
    This is part of the Files app Recent filter change. We need a new tool
    to utilize one or more JSON5 files as data sources and generate files
    based on the templates provided.
    
    The usage of this tool is in CL:3469537 where we use a file_types.json5
    file as the single source of truth to generate files both in Javascript
    and C++ to maintain consistency.
    
    Bug: 1259359
    Change-Id: I49af4a770868b2f454b74255fb6291a74f58eb98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473619
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976052}

---
+++ tools/json_data_generator/test/jinja_helper.py
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+def _custom_upper(name: str):
+    return name.upper()
+
+
+def get_custom_globals(model):
+    return {
+        'name_with_title': 'Mr ' + model['test_data1']['name'],
+    }
+
+
+def get_custom_filters(model):
+    return {
+        'custom_upper': _custom_upper,
+    }
