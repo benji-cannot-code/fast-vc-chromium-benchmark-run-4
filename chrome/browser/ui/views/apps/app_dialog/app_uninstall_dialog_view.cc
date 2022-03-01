b4b328a340112a8d58743aaef341c1cd7cca9dd2
Lucy Qu <lqu@google.com>
2022-03-01 07:08:01 +0000

    Change borealis settings strings
    BUG=b:194640944
    
    Change-Id: If83cf5cd791ffcb08c54f8635a7a40dd0f28e829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486239
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Lucy Qu <lqu@google.com>
    Cr-Commit-Position: refs/heads/main@{#976121}

--- chrome/browser/ui/views/apps/app_dialog/app_uninstall_dialog_view.cc
+++ chrome/browser/ui/views/apps/app_dialog/app_uninstall_dialog_view.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view_class_properties.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/ash/borealis/borealis_util.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #endif
 
@@ -177,8 +178,18 @@ void AppUninstallDialogView::InitializeView(Profile* profile,
 #endif
       break;
     case apps::mojom::AppType::kBorealis:
-      // TODO(b/178741230): Borealis' uninstaller needs custom text.  For now
-      // just use Crostini's.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+      if (app_id == borealis::kClientAppId) {
+        InitializeViewWithMessage(l10n_util::GetStringUTF16(
+            IDS_BOREALIS_CLIENT_UNINSTALL_CONFIRM_BODY));
+      } else {
+        InitializeViewWithMessage(l10n_util::GetStringUTF16(
+            IDS_BOREALIS_APPLICATION_UNINSTALL_CONFIRM_BODY));
+      }
+#else
+      NOTREACHED();
+#endif
+      break;
     case apps::mojom::AppType::kCrostini:
 #if BUILDFLAG(IS_CHROMEOS_ASH)
       InitializeViewWithMessage(l10n_util::GetStringUTF16(
