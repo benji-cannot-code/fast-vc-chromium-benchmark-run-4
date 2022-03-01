f0160cae5ee475080fa4ef7c59d3eeab9a5e061e
K. Moon <kmoon@chromium.org>
2022-03-01 22:04:45 +0000

    [unseasoned-pdf] Delete remaining PPAPI dependencies
    Deletes all remaining dependencies on PPAPI, other than //pdf:pdf_ppapi.
    
    Fixed: 702993
    Change-Id: Ibc3aec650d23c3674e49594a5aff6483d84ecc42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489826
    Reviewed-by: Nigi <nigi@chromium.org>
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976425}

--- pdf/ppapi_migration/image.h
+++ pdf/ppapi_migration/image.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PDF_PPAPI_MIGRATION_IMAGE_H_
 #define PDF_PPAPI_MIGRATION_IMAGE_H_
 
-#include "ppapi/cpp/image_data.h"
-#include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
 namespace chrome_pdf {
@@ -18,20 +16,15 @@ namespace chrome_pdf {
 // underlying pixel memory, so this class may be copied freely.
 class Image final {
  public:
-  explicit Image(const pp::ImageData& pepper_image);
   explicit Image(const SkBitmap& skia_image);
   Image(const Image& other);
   Image& operator=(const Image& other);
   ~Image();
 
-  const pp::ImageData& pepper_image() const {
-    return absl::get<pp::ImageData>(image_);
-  }
-
-  const SkBitmap& skia_image() const { return absl::get<SkBitmap>(image_); }
+  const SkBitmap& skia_image() const { return image_; }
 
  private:
-  absl::variant<pp::ImageData, SkBitmap> image_;
+  SkBitmap image_;
 };
 
 }  // namespace chrome_pdf
