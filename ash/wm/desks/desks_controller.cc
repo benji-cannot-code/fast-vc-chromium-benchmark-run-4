54642720e6410454d878926031f19f799edd2f1e
amehfooz <amehfooz@chromium.org>
2022-03-01 00:03:57 +0000

    Add fullscreen state change notification to shell
    Currently when there is a desk change with one of the
    desks containing a window in fullscreen, there is no
    full screen state change notification  being sent out
    to ShellObservers. This is resulting in notification
    popups being suppressed in cases where they should be
    shown.
    
    This CL adds a way to notify observers for this
    specific case by checking if the fullscreen state
    changes when a different desk is activated and notifying
    observers if needed. Also added unit test to cover this
    case.
    
    Bug: b/204970107
    Change-Id: If50f284af2d10e20658de3fa2651f2a654c9f4fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485642
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976011}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_restore/window_restore_controller.h"
 #include "ash/wm/window_restore/window_restore_util.h"
 #include "ash/wm/window_util.h"
+#include "ash/wm/workspace/workspace_layout_manager.h"
+#include "ash/wm/workspace_controller.h"
 #include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/check.h"
@@ -1241,6 +1243,8 @@ void DesksController::ActivateDeskInternal(const Desk* desk,
   for (auto& observer : observers_)
     observer.OnDeskActivationChanged(active_desk_, old_active);
 
+  NotifyFullScreenStateChangedAcrossDesksIfNeeded(old_active);
+
   // Only update active desk prefs when a primary user switches a desk.
   if (shell->session_controller()->IsUserPrimary()) {
     desks_restore_util::UpdatePrimaryUserActiveDeskPrefs(
@@ -1423,6 +1427,26 @@ void DesksController::MoveVisibleOnAllDesksWindowsFromActiveDeskTo(
   }
 
   mru_tracker->SetIgnoreActivations(false);
+}
+
+void DesksController::NotifyFullScreenStateChangedAcrossDesksIfNeeded(
+    const Desk* previous_active_desk) {
+  Shell* shell = Shell::Get();
+  for (aura::Window* root : Shell::GetAllRootWindows()) {
+    aura::Window* active_desk_container =
+        active_desk_->GetDeskContainerForRoot(root);
+    const bool is_active_desk_fullscreen =
+        GetWorkspaceController(active_desk_container)
+            ->layout_manager()
+            ->is_fullscreen();
+    if (GetWorkspaceController(
+            previous_active_desk->GetDeskContainerForRoot(root))
+            ->layout_manager()
+            ->is_fullscreen() != is_active_desk_fullscreen) {
+      shell->NotifyFullscreenStateChanged(is_active_desk_fullscreen,
+                                          active_desk_container);
+    }
+  }
 }
 
 void DesksController::RestackVisibleOnAllDesksWindowsOnActiveDesk() {
