1c8e6c111cdf23800c48590bc5a2ebcea1b1ad40
Eric Sum <esum@google.com>
2022-03-02 22:45:05 +0000

    Update animation dynamic image asset id naming convention.
    Before, the asset ids were _CrOS_1, _CrOS_2, etc. To support cross-fade
    between photos though, we need a way of assigning multiple image assets
    to a given photo "position" in the animation and a way of ordering the
    assets at that position (transition from image 1 to image 2 in that
    order).
    
    Thus, the new naming convention agreed upon with UX is
    _CrOS_Photo_PositionA_1,
    _CrOS_Photo_PositionB_1,
    _CrOS_Photo_PositionA_2,
    _CrOS_Photo_PositionB_2,
    
    With simple reg-ex parsing, we can extract a position identifier
    and order the assets at each position. Practically speaking, there
    will be 6 positions and 2 assets per position in the new cross-fade
    animation.
    
    The existing animation has also been updated to reflect this naming
    convention to avoid breakage in the mean-time (there is just 1 asset
    per position currently).
    
    Bug: b:217271452
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I588c0c00376de7d658c947c787274213a1664235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499340
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#976846}

--- ash/ambient/model/ambient_animation_attribution_provider_unittest.cc
+++ ash/ambient/model/ambient_animation_attribution_provider_unittest.cc
@@ -111,8 +111,9 @@ class AmbientAnimationAttributionProviderTest2DynamicAssets
       : AmbientAnimationAttributionProviderTest(
             GenerateLottieCustomizableIdForTesting(0) + "Attribution",
             GenerateLottieCustomizableIdForTesting(1) + "Attribution",
-            GenerateLottieCustomizableIdForTesting(0) + "Asset",
-            GenerateLottieCustomizableIdForTesting(1) + "Asset") {}
+            GenerateLottieDynamicAssetIdForTesting(/*position=*/"A", /*idx=*/1),
+            GenerateLottieDynamicAssetIdForTesting(/*position=*/"A",
+                                                   /*idx=*/2)) {}
 };
 
 class AmbientAnimationAttributionProviderTest1DynamicAsset
@@ -123,7 +124,8 @@ class AmbientAnimationAttributionProviderTest1DynamicAsset
             "static-text-node",
             GenerateLottieCustomizableIdForTesting(1) + "Attribution",
             "static-asset-id",
-            GenerateLottieCustomizableIdForTesting(1) + "Asset") {}
+            GenerateLottieDynamicAssetIdForTesting(/*position=*/"A",
+                                                   /*idx=*/1)) {}
 };
 
 TEST_F(AmbientAnimationAttributionProviderTest2DynamicAssets,
--- ash/ambient/test/ambient_test_util.cc
+++ ash/ambient/test/ambient_test_util.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "cc/paint/skottie_resource_metadata.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 
@@ -20,12 +21,21 @@ std::string GenerateLottieCustomizableIdForTesting(int unique_id) {
       {kLottieCustomizableIdPrefix, base::NumberToString(unique_id)});
 }
 
+std::string GenerateLottieDynamicAssetIdForTesting(base::StringPiece position,
+                                                   int idx) {
+  CHECK(!position.empty());
+  return base::StringPrintf("%s_Photo_Position%s_%d",
+                            kLottieCustomizableIdPrefix.data(), position.data(),
+                            idx);
+}
+
 AmbientPhotoConfig GenerateAnimationConfigWithNAssets(int num_assets) {
   cc::SkottieResourceMetadataMap resource_metadata;
   for (int i = 0; i < num_assets; ++i) {
     CHECK(resource_metadata.RegisterAsset(
         "test-resource-path", "test-resource-name",
-        GenerateLottieCustomizableIdForTesting(/*unique_id=*/i),
+        GenerateLottieDynamicAssetIdForTesting(
+            /*position=*/base::NumberToString(i), /*idx=*/1),
         /*size=*/absl::nullopt));
   }
   return CreateAmbientAnimationPhotoConfig(resource_metadata);
