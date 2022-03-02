e7dc312edc34689d580982d5092c2b568116373c
Bruce Dawson <brucedawson@chromium.org>
2022-03-02 00:02:45 +0000

    Remove inappropriate NOTIMPLEMENTED
    There are six different implementations of
    PowerMonitorDeviceSource::IsOnBatteryPower, depending on platform. The
    one in power_monitor_device_source_stub.cc does nothing, but it is the
    implementation for (as far as I can tell) both Linux and Fuchsia.
    There doesn't seem to be a bug tracking plans to implement it and the
    output it generates shows up as an error on many tests, like this:
    
       ...:ERROR:power_monitor_device_source_stub.cc(11)] Not implemented
       reached in virtual bool
       base::PowerMonitorDeviceSource::IsOnBatteryPower()
    
    It's been doing this since around 2013 so I think it's time to call this
    the implementation and reduce the misleading :ERROR: output, to avoid
    wasting developer time when tracking down other issues such as
    https://crbug.com/1296147
    
    Change-Id: If18a1a1219289ed0ba9905dccbc27a8bd2aec6fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489921
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976476}

--- base/power_monitor/power_monitor_device_source_stub.cc
+++ base/power_monitor/power_monitor_device_source_stub.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 bool PowerMonitorDeviceSource::IsOnBatteryPower() {
-  NOTIMPLEMENTED();
   return false;
 }
 
