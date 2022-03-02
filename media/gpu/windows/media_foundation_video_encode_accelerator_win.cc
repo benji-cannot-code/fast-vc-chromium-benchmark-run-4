46f3018c3fe72c2be5e52bd9ece4ebd6d79def8b
Chunbo Hua <chunbo.hua@intel.com>
2022-03-02 01:43:00 +0000

    Guard the source and destiny buffer for memcpy.
    Bug: 1293463
    Change-Id: I66e3a4e064f12d18ad8f2ce07635c70baaeb5af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498020
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Chunbo Hua <chunbo.hua@intel.com>
    Cr-Commit-Position: refs/heads/main@{#976501}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -1289,6 +1289,11 @@ void MediaFoundationVideoEncodeAccelerator::ProcessOutput() {
 
   {
     MediaBufferScopedPointer scoped_buffer(output_buffer.Get());
+    if (!buffer_ref->mapping.IsValid() || !scoped_buffer.get()) {
+      DLOG(ERROR) << "Failed to copy bitstream media buffer.";
+      return;
+    }
+
     memcpy(buffer_ref->mapping.memory(), scoped_buffer.get(), size);
   }
 
