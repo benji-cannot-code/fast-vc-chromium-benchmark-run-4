002be473ed36c8d87613d48cd851de0aa05bc443
Michael Bai <michaelbai@chromium.org>
2022-03-02 22:24:54 +0000

    Aw: Add AlgorithmicDarkeningAllowed to boundary interface
    Bug: 1287883
    Change-Id: I59f9316007cd4b43c85e9ad5f454819bc67e84d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498326
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Michael Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976839}

--- android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebSettingsBoundaryInterface.java
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebSettingsBoundaryInterface.java
@@ -32,6 +32,9 @@ public interface WebSettingsBoundaryInterface {
     void setForceDark(int forceDarkMode);
     int getForceDark();
 
+    void setAlgorithmicDarkeningAllowed(boolean allow);
+    boolean isAlgorithmicDarkeningAllowed();
+
     @Retention(RetentionPolicy.SOURCE)
     @interface ForceDarkBehavior {
         int FORCE_DARK_ONLY = 0;
