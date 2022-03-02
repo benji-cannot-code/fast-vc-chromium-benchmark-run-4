c63fdd027c35e2935de91817568face93de2f8bd
Peter Kasting <pkasting@chromium.org>
2022-03-02 17:37:01 +0000

    Flesh out NTP colors for color pipeline.
    Bug: 1296206
    Change-Id: Ia87dd79c2673ec1146bf884cc209ad6bafc83955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498767
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976717}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -65,7 +65,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   /* New Tab Page colors. */ \
   E(kColorNewTabPageBackground, ThemeProperties::COLOR_NTP_BACKGROUND) \
   E(kColorNewTabPageHeader, ThemeProperties::COLOR_NTP_HEADER) \
+  E(kColorNewTabPageLink, ThemeProperties::COLOR_NTP_LINK) \
+  E(kColorNewTabPageLogo, ThemeProperties::COLOR_NTP_LOGO) \
+  E(kColorNewTabPageMostVisitedTileBackground, \
+    ThemeProperties::COLOR_NTP_SHORTCUT) \
+  E(kColorNewTabPageSectionBorder, ThemeProperties::COLOR_NTP_SECTION_BORDER) \
   E(kColorNewTabPageText, ThemeProperties::COLOR_NTP_TEXT) \
+  E(kColorNewTabPageTextLight, ThemeProperties::COLOR_NTP_TEXT_LIGHT) \
   /* Omnibox colors. */ \
   E(kColorOmniboxBackground, ThemeProperties::COLOR_OMNIBOX_BACKGROUND) \
   E(kColorOmniboxBackgroundHovered, \
