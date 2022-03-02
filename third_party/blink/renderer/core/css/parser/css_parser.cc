758011749b73b95505c90ceb85ee86827d8fe88a
Steinar H. Gunderson <sesse@chromium.org>
2022-03-02 11:38:32 +0000

    Make MutableCSSPropertyValueSet into an enum.
    Currently, this is a struct with two flags (did_parse, did_change),
    but only three of the combinations are actually possible (a value that
    didn't parse, but changed something, is meaningless). As the next patch
    will introduce a fourth state, change it into an enum instead of
    introducing a third non-orthogonal flag.
    
    Change-Id: I93db46c8530dc9957938a6c6ea077ec0c7187c13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3449268
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976614}

--- third_party/blink/renderer/core/css/parser/css_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_parser.cc
@@ -111,9 +111,7 @@ MutableCSSPropertyValueSet::SetResult CSSParser::ParseValue(
     const ExecutionContext* execution_context) {
   DCHECK(ThreadState::Current()->IsAllocationAllowed());
   if (string.IsEmpty()) {
-    bool did_parse = false;
-    bool did_change = false;
-    return MutableCSSPropertyValueSet::SetResult{did_parse, did_change};
+    return MutableCSSPropertyValueSet::kParseError;
   }
 
   CSSPropertyID resolved_property = ResolveCSSPropertyID(unresolved_property);
@@ -121,10 +119,10 @@ MutableCSSPropertyValueSet::SetResult CSSParser::ParseValue(
   CSSValue* value = CSSParserFastPaths::MaybeParseValue(resolved_property,
                                                         string, parser_mode);
   if (value) {
-    bool did_parse = true;
-    bool did_change = declaration->SetProperty(CSSPropertyValue(
-        CSSPropertyName(resolved_property), *value, important));
-    return MutableCSSPropertyValueSet::SetResult{did_parse, did_change};
+    return declaration->SetProperty(CSSPropertyValue(
+               CSSPropertyName(resolved_property), *value, important))
+               ? MutableCSSPropertyValueSet::kDidChange
+               : MutableCSSPropertyValueSet::kUnchanged;
   }
   CSSParserContext* context;
   if (style_sheet) {
@@ -156,9 +154,7 @@ MutableCSSPropertyValueSet::SetResult CSSParser::ParseValueForCustomProperty(
   DCHECK(ThreadState::Current()->IsAllocationAllowed());
   DCHECK(CSSVariableParser::IsValidVariableName(property_name));
   if (value.IsEmpty()) {
-    bool did_parse = false;
-    bool did_change = false;
-    return MutableCSSPropertyValueSet::SetResult{did_parse, did_change};
+    return MutableCSSPropertyValueSet::kParseError;
   }
   CSSParserMode parser_mode = declaration->CssParserMode();
   CSSParserContext* context;
