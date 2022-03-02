b9497536166d5fce0ace4aa236ac1c96dcf4a39f
Christian Xu <christianxu@google.com>
2022-03-02 19:24:03 +0000

    [iOS] Add close button in bubble view
    Adds an optional close button to the bubble view. This option will be
    used later on for Autofill IPH to present different variants of
    BubbleViews.
    
    Day:
    https://drive.google.com/file/d/13PMTnaFinSLjAlGxW1Tq2aoWnKdLg_gk/view?usp=sharing
    
    Dark:
    https://drive.google.com/file/d/137YQQg8xkMW8_G-RF4wlvuM2Erp6LeC6/view?usp=sharing
    
    Bug: 1286669
    Change-Id: I7eb91201aa5d1d90372cf18358257215f9d6e92f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3477019
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976772}

--- ios/chrome/browser/ui/bubble/bubble_view.h
+++ ios/chrome/browser/ui/bubble/bubble_view.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+// Accessibility identifier for the close button.
+extern NSString* const kBubbleViewCloseButtonIdentifier;
+
 // Direction for the bubble to point.
 typedef NS_ENUM(NSInteger, BubbleArrowDirection) {
   // Bubble is below the target UI element and the arrow is pointing up.
@@ -26,6 +29,16 @@ typedef NS_ENUM(NSInteger, BubbleAlignment) {
   BubbleAlignmentTrailing,
 };
 
+// Delegate for actions happening in BubbleView.
+@protocol BubbleViewDelegate <NSObject>
+
+@optional
+
+// User tapped on the close button.
+- (void)didTapCloseButton;
+
+@end
+
 // Speech bubble shaped view that displays a message.
 @interface BubbleView : UIView
 
@@ -41,6 +54,13 @@ typedef NS_ENUM(NSInteger, BubbleAlignment) {
 - (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
 
 - (instancetype)init NS_UNAVAILABLE;
+
+// Controls if there is a close button in the view. Must be set before the view
+// is added to a superview.
+@property(nonatomic) BOOL showsCloseButton;
+
+// The delegate for interactions in this View.
+@property(nonatomic, weak) id<BubbleViewDelegate> delegate;
 
 @end
 
