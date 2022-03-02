da126266a51b8e57afdeddf18592ec2534c08c30
Clement Yan <clamclamyan@google.com>
2022-03-02 03:47:45 +0000

    Refactor getting windows in window management
    Bug: 221124401
    Change-Id: I120cbe99a1dcc9620fe7176b9e5e27ff99d8d55b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499503
    Reviewed-by: Christopher Lam <calamity@chromium.org>
    Commit-Queue: Clement Yan <clamclamyan@google.com>
    Cr-Commit-Position: refs/heads/main@{#976532}

--- chrome/browser/ash/system_extensions/api/window_management/window_management_impl.h
+++ chrome/browser/ash/system_extensions/api/window_management/window_management_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/unguessable_token.h"
 #include "third_party/blink/public/mojom/chromeos/system_extensions/window_management/cros_window_management.mojom.h"
+#include "ui/aura/window.h"
 
 namespace content {
 class BrowserContext;
@@ -29,6 +30,8 @@ class WindowManagementImpl : public blink::mojom::CrosWindowManagement {
                        int32_t height) override;
 
  private:
+  aura::Window* GetWindow(const base::UnguessableToken& id);
+
   content::BrowserContext* browser_context_;
 };
 
