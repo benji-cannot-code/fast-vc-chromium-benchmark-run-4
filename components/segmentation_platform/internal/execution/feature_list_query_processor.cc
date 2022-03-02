7b68a639795ca69a28434f0c32f8b2aeec586bf8
Hailey Wang <haileywang@google.com>
2022-03-02 02:29:30 +0000

    [Feature Processor] Adding SqlProcessor template to feature processor
    Bug: 1300003
    Change-Id: Ic1aeb1d5b3fce0c891b8f186b50c1613bd76b746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482718
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#976512}

--- components/segmentation_platform/internal/execution/feature_list_query_processor.cc
+++ components/segmentation_platform/internal/execution/feature_list_query_processor.cc
@@ -13,11 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/internal/database/metadata_utils.h"
 #include "components/segmentation_platform/internal/execution/custom_input_processor.h"
 #include "components/segmentation_platform/internal/execution/feature_processor_state.h"
+#include "components/segmentation_platform/internal/execution/sql_feature_processor.h"
 #include "components/segmentation_platform/internal/execution/uma_feature_processor.h"
 #include "components/segmentation_platform/internal/proto/model_metadata.pb.h"
 #include "components/segmentation_platform/internal/stats.h"
 
 namespace segmentation_platform {
+
+namespace {
+// Index not actually used for legacy code in FeatureQueryProcessor.
+const int kIndexNotUsed = 0;
+}  // namespace
 
 FeatureListQueryProcessor::FeatureListQueryProcessor(
     SignalDatabase* signal_database,
@@ -79,7 +85,25 @@ void FeatureListQueryProcessor::ProcessNextInputFeature(
         input_feature.custom_input(), std::move(feature_processor_state),
         base::BindOnce(&FeatureListQueryProcessor::ProcessNextInputFeature,
                        weak_ptr_factory_.GetWeakPtr()));
+  } else if (input_feature.has_sql_feature()) {
+    std::map<SqlFeatureProcessor::FeatureIndex, proto::SqlFeature> queries = {
+        {kIndexNotUsed, input_feature.sql_feature()}};
+    auto sql_feature_processor = std::make_unique<SqlFeatureProcessor>(queries);
+    auto* sql_feature_processor_ptr = sql_feature_processor.get();
+    sql_feature_processor_ptr->Process(
+        std::move(feature_processor_state),
+        base::BindOnce(&FeatureListQueryProcessor::OnSqlQueryProcessed,
+                       weak_ptr_factory_.GetWeakPtr(),
+                       std::move(sql_feature_processor)));
   }
+}
+
+void FeatureListQueryProcessor::OnSqlQueryProcessed(
+    std::unique_ptr<SqlFeatureProcessor> sql_feature_processor,
+    std::unique_ptr<FeatureProcessorState> feature_processor_state,
+    QueryProcessor::IndexedTensors result) {
+  feature_processor_state->AppendInputTensor(result[kIndexNotUsed]);
+  ProcessNextInputFeature(std::move(feature_processor_state));
 }
 
 }  // namespace segmentation_platform
