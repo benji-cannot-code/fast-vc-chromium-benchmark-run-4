20e5d9fdac6a6f35ea877f54bffa4538f65fc37f
Nektarios Paisios <nektar@chromium.org>
2022-03-02 09:06:56 +0000

    Adds a feature flag for PDF OCR
    Inaccessible PDFs will be made accessible via optical character recognition.
    More info in:
    https://docs.google.com/document/d/1Xe1BBncEddGz8DYYnGdTHI0cwhJ_3BvfELCQY6T1rBw/edit?usp=sharing
    
    AX-Relnotes: n/a.
    Change-Id: I3cec0af8a9a51bf3f26ccfec48ab833c5a8852f7
    Bug: 1248380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497057
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976594}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1155,6 +1155,9 @@ extern const char kPasswordDomainCapabilitiesFetchingDescription[];
 extern const char kPasswordScriptsFetchingName[];
 extern const char kPasswordScriptsFetchingDescription[];
 
+extern const char kPdfOcrName[];
+extern const char kPdfOcrDescription[];
+
 extern const char kPdfXfaFormsName[];
 extern const char kPdfXfaFormsDescription[];
 
