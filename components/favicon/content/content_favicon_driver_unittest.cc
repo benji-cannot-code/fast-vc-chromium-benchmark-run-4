f9f0405bd516dd18c3edb19be529474a7de0e4c0
Victor Hugo Vianna Silva <victorvianna@google.com>
2022-03-02 13:45:18 +0000

    Remove unnecessary usage of testing::_ in components/favicon/
    No behavior changed. This is a simple run of [1] followed by `git cl
    format`.
    
    [1] find -type f -wholename "./components/favicon/*" -exec perl -0777 -pi -e 's/\((_(,\n?\ +)?)+\)//g' '{}' \;
    
    Bug: None
    Change-Id: Ie200c7062441baf6f2b3470d6adefcf80fa9b82d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498343
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976635}

--- components/favicon/content/content_favicon_driver_unittest.cc
+++ components/favicon/content/content_favicon_driver_unittest.cc
@@ -53,7 +53,7 @@ class ContentFaviconDriverTest : public content::RenderViewHostTestHarness {
   const GURL kFakeManifestURL = GURL("http://www.google.com/manifest.json");
 
   ContentFaviconDriverTest() {
-    ON_CALL(favicon_service_, UpdateFaviconMappingsAndFetch(_, _, _, _, _, _))
+    ON_CALL(favicon_service_, UpdateFaviconMappingsAndFetch)
         .WillByDefault([](auto, auto, auto, auto,
                           favicon_base::FaviconResultsCallback callback,
                           base::CancelableTaskTracker* tracker) {
@@ -63,7 +63,7 @@ class ContentFaviconDriverTest : public content::RenderViewHostTestHarness {
                   std::move(callback),
                   std::vector<favicon_base::FaviconRawBitmapResult>()));
         });
-    ON_CALL(favicon_service_, GetFaviconForPageURL(_, _, _, _, _))
+    ON_CALL(favicon_service_, GetFaviconForPageURL)
         .WillByDefault([](auto, auto, auto,
                           favicon_base::FaviconResultsCallback callback,
                           base::CancelableTaskTracker* tracker) {
--- components/favicon/core/large_icon_service_impl_unittest.cc
+++ components/favicon/core/large_icon_service_impl_unittest.cc
@@ -136,7 +136,7 @@ TEST_F(LargeIconServiceTest, ShouldGetFromGoogleServer) {
       "&check_seen=true&size=48&min_size=16&max_size=256"
       "&fallback_opts=TYPE,SIZE,URL&url=http://www.example.com/");
 
-  EXPECT_CALL(mock_favicon_service_, UnableToDownloadFavicon(_)).Times(0);
+  EXPECT_CALL(mock_favicon_service_, UnableToDownloadFavicon).Times(0);
   EXPECT_CALL(mock_favicon_service_,
               CanSetOnDemandFavicons(GURL(kDummyUrl),
                                      favicon_base::IconType::kTouchIcon, _))
@@ -282,7 +282,7 @@ TEST_F(LargeIconServiceTest, ShouldNotCheckOnPublicUrls) {
 TEST_F(LargeIconServiceTest, ShouldNotQueryGoogleServerIfInvalidScheme) {
   const GURL kDummyFtpUrl("ftp://www.example.com");
 
-  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_(_, _, _, _)).Times(0);
+  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_).Times(0);
 
   base::MockCallback<favicon_base::GoogleFaviconServerCallback> callback;
 
@@ -303,7 +303,7 @@ TEST_F(LargeIconServiceTest, ShouldNotQueryGoogleServerIfInvalidScheme) {
 TEST_F(LargeIconServiceTest, ShouldNotQueryGoogleServerIfInvalidURL) {
   const GURL kDummyInvalidUrl("htt");
 
-  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_(_, _, _, _)).Times(0);
+  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_).Times(0);
 
   base::MockCallback<favicon_base::GoogleFaviconServerCallback> callback;
 
@@ -334,8 +334,7 @@ TEST_F(LargeIconServiceTest, ShouldReportUnavailableIfFetchFromServerFails) {
         return base::ThreadTaskRunnerHandle::Get()->PostTask(
             FROM_HERE, base::BindOnce(std::move(callback), true));
       });
-  EXPECT_CALL(mock_favicon_service_, SetOnDemandFavicons(_, _, _, _, _))
-      .Times(0);
+  EXPECT_CALL(mock_favicon_service_, SetOnDemandFavicons).Times(0);
 
   base::MockCallback<favicon_base::GoogleFaviconServerCallback> callback;
   EXPECT_CALL(*mock_image_fetcher_,
@@ -366,10 +365,9 @@ TEST_F(LargeIconServiceTest, ShouldNotGetFromGoogleServerIfUnavailable) {
                    "&fallback_opts=TYPE,SIZE,URL&url=http://www.example.com/")))
       .WillByDefault(Return(true));
 
-  EXPECT_CALL(mock_favicon_service_, UnableToDownloadFavicon(_)).Times(0);
-  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_(_, _, _, _)).Times(0);
-  EXPECT_CALL(mock_favicon_service_, SetOnDemandFavicons(_, _, _, _, _))
-      .Times(0);
+  EXPECT_CALL(mock_favicon_service_, UnableToDownloadFavicon).Times(0);
+  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_).Times(0);
+  EXPECT_CALL(mock_favicon_service_, SetOnDemandFavicons).Times(0);
 
   base::MockCallback<favicon_base::GoogleFaviconServerCallback> callback;
   large_icon_service_
@@ -387,7 +385,7 @@ TEST_F(LargeIconServiceTest, ShouldNotGetFromGoogleServerIfUnavailable) {
 }
 
 TEST_F(LargeIconServiceTest, ShouldNotGetFromGoogleServerIfCannotSet) {
-  EXPECT_CALL(mock_favicon_service_, UnableToDownloadFavicon(_)).Times(0);
+  EXPECT_CALL(mock_favicon_service_, UnableToDownloadFavicon).Times(0);
   EXPECT_CALL(mock_favicon_service_,
               CanSetOnDemandFavicons(GURL(kDummyUrl),
                                      favicon_base::IconType::kTouchIcon, _))
@@ -396,9 +394,8 @@ TEST_F(LargeIconServiceTest, ShouldNotGetFromGoogleServerIfCannotSet) {
             FROM_HERE, base::BindOnce(std::move(callback), false));
       });
 
-  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_(_, _, _, _)).Times(0);
-  EXPECT_CALL(mock_favicon_service_, SetOnDemandFavicons(_, _, _, _, _))
-      .Times(0);
+  EXPECT_CALL(*mock_image_fetcher_, FetchImageAndData_).Times(0);
+  EXPECT_CALL(mock_favicon_service_, SetOnDemandFavicons).Times(0);
 
   base::MockCallback<favicon_base::GoogleFaviconServerCallback> callback;
   large_icon_service_
