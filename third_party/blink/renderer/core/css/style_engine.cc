aa300cbb3751d0f55ee1039e0af5303c9f71cb4f
Byungwoo Lee <blee@igalia.com>
2022-03-02 16:19:22 +0000

    Check :has() invalidation is needed for a certain mutation type
    To skip :has() invalidation early, this CL added RuleFeatureSet methods
    that provides whether :has() invalidation is needed for a certain
    mutation type or not.
    
    Bug: 669058
    Change-Id: I4285ff2371c46bbe9ced3e6bcfb1e6a894f996f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499752
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Byungwoo Lee <blee@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#976691}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -1072,6 +1072,7 @@ void StyleEngine::ClassChangedForElement(
   const RuleFeatureSet& features = GetRuleFeatureSet();
 
   if (RuntimeEnabledFeatures::CSSPseudoHasEnabled() &&
+      features.NeedsHasInvalidationForClassChange() &&
       PossiblyAffectingHasState(element)) {
     unsigned changed_size = changed_classes.size();
     for (unsigned i = 0; i < changed_size; ++i) {
@@ -1106,9 +1107,12 @@ void StyleEngine::ClassChangedForElement(const SpaceSplitString& old_classes,
     return;
   }
 
+  const RuleFeatureSet& features = GetRuleFeatureSet();
+
   bool needs_schedule_invalidation = !IsSubtreeAndSiblingsStyleDirty(element);
   bool possibly_affecting_has_state =
       RuntimeEnabledFeatures::CSSPseudoHasEnabled() &&
+      features.NeedsHasInvalidationForClassChange() &&
       PossiblyAffectingHasState(element);
   if (!needs_schedule_invalidation && !possibly_affecting_has_state)
     return;
@@ -1119,7 +1123,6 @@ void StyleEngine::ClassChangedForElement(const SpaceSplitString& old_classes,
 
   InvalidationLists invalidation_lists;
   bool affecting_has_state = false;
-  const RuleFeatureSet& features = GetRuleFeatureSet();
 
   for (unsigned i = 0; i < new_classes.size(); ++i) {
     bool found = false;
@@ -1198,6 +1201,7 @@ void StyleEngine::AttributeChangedForElement(
   const RuleFeatureSet& features = GetRuleFeatureSet();
 
   if (RuntimeEnabledFeatures::CSSPseudoHasEnabled() &&
+      features.NeedsHasInvalidationForAttributeChange() &&
       PossiblyAffectingHasState(element)) {
     if (features.NeedsHasInvalidationForAttribute(attribute_name))
       InvalidateAncestorsOrSiblingsAffectedByHas(element);
@@ -1229,6 +1233,7 @@ void StyleEngine::IdChangedForElement(const AtomicString& old_id,
   const RuleFeatureSet& features = GetRuleFeatureSet();
 
   if (RuntimeEnabledFeatures::CSSPseudoHasEnabled() &&
+      features.NeedsHasInvalidationForIdChange() &&
       PossiblyAffectingHasState(element)) {
     if ((!old_id.IsEmpty() && features.NeedsHasInvalidationForId(old_id)) ||
         (!new_id.IsEmpty() && features.NeedsHasInvalidationForId(new_id))) {
@@ -1263,6 +1268,7 @@ void StyleEngine::PseudoStateChangedForElement(
 
   if (invalidate_ancestors_or_siblings &&
       RuntimeEnabledFeatures::CSSPseudoHasEnabled() &&
+      features.NeedsHasInvalidationForPseudoStateChange() &&
       PossiblyAffectingHasState(element)) {
     if (features.NeedsHasInvalidationForPseudoClass(pseudo_type))
       InvalidateAncestorsOrSiblingsAffectedByHasForPseudoChange(element);
@@ -1486,6 +1492,8 @@ void StyleEngine::ElementInsertedOrRemoved(Element* parent,
     return;
 
   const RuleFeatureSet& features = GetRuleFeatureSet();
+  if (!features.NeedsHasInvalidation())
+    return;
 
   if (features.NeedsHasInvalidationForElement(element)) {
     InvalidateAncestorsOrSiblingsAffectedByHas(
@@ -1506,6 +1514,9 @@ void StyleEngine::SubtreeInsertedOrRemoved(Element* parent,
     return;
 
   const RuleFeatureSet& features = GetRuleFeatureSet();
+  if (!features.NeedsHasInvalidation())
+    return;
+
   for (Element& element :
        ElementTraversal::InclusiveDescendantsOf(subtree_root)) {
     if (features.NeedsHasInvalidationForElement(element)) {
