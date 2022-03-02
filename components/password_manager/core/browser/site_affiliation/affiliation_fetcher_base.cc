ff353a38ffab89f94c9ff02bd6a2dee5aebf23c9
Viktor Semeniuk <vsemeniuk@google.com>
2022-03-02 12:29:27 +0000

    [Affiliations] Parse response body only if request succeeded
    Response body was parsed before checking the request's status. If
    request had failed but contained non-empty body it would result into
    malformed response. This CL fixes this.
    
    Also, response code and errors are recorded for
    all requests. Since semantics changed new histogram names are used.
    
    Bug: 1302225
    Change-Id: I05511ed90399d1be4fa9de453add798ef04c7809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3483666
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#976625}

--- components/password_manager/core/browser/site_affiliation/affiliation_fetcher_base.cc
+++ components/password_manager/core/browser/site_affiliation/affiliation_fetcher_base.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/net/variations_http_headers.h"
 #include "net/base/load_flags.h"
 #include "net/base/url_util.h"
+#include "net/http/http_status_code.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
@@ -142,25 +143,16 @@ void AffiliationFetcherBase::OnSimpleLoaderComplete(
   base::TimeDelta fetch_time = fetch_timer_.Elapsed();
   // Note that invoking the |delegate_| may destroy |this| synchronously, so the
   // invocation must happen last.
-  auto result_data = std::make_unique<AffiliationFetcherDelegate::Result>();
-  if (response_body) {
-    if (ParseResponse(*response_body, result_data.get())) {
-      LogFetchResult(AffiliationFetchResult::kSuccess, fetch_time,
-                     response_body->size());
-      delegate_->OnFetchSucceeded(this, std::move(result_data));
-    } else {
-      LogFetchResult(AffiliationFetchResult::kMalformed, fetch_time,
-                     response_body->size());
-      delegate_->OnMalformedResponse(this);
-    }
-  } else {
+  bool success = simple_url_loader_->NetError() == net::OK;
+  int response_code = 0;
+  if (simple_url_loader_->ResponseInfo() &&
+      simple_url_loader_->ResponseInfo()->headers) {
+    response_code =
+        simple_url_loader_->ResponseInfo()->headers->response_code();
+  }
+
+  if (!success || net::HTTP_OK != response_code) {
     LogFetchResult(AffiliationFetchResult::kFailure, fetch_time);
-    int response_code = -1;
-    if (simple_url_loader_->ResponseInfo() &&
-        simple_url_loader_->ResponseInfo()->headers) {
-      response_code =
-          simple_url_loader_->ResponseInfo()->headers->response_code();
-    }
     base::UmaHistogramSparse(
         "PasswordManager.AffiliationFetcher.FetchHttpResponseCode",
         response_code);
@@ -169,6 +161,18 @@ void AffiliationFetcherBase::OnSimpleLoaderComplete(
         "PasswordManager.AffiliationFetcher.FetchErrorCode",
         -simple_url_loader_->NetError());
     delegate_->OnFetchFailed(this);
+    return;
+  }
+
+  auto result_data = std::make_unique<AffiliationFetcherDelegate::Result>();
+  if (ParseResponse(*response_body, result_data.get())) {
+    LogFetchResult(AffiliationFetchResult::kSuccess, fetch_time,
+                   response_body->size());
+    delegate_->OnFetchSucceeded(this, std::move(result_data));
+  } else {
+    LogFetchResult(AffiliationFetchResult::kMalformed, fetch_time,
+                   response_body->size());
+    delegate_->OnMalformedResponse(this);
   }
 }
 
