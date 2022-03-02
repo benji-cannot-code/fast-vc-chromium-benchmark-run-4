75e57dd7a65754b4139527958a79e07e19882c1a
Eric Willigers <ericwilligers@chromium.org>
2022-03-02 03:26:54 +0000

    Web Applications: Call reserve before populating vectors
    When populating a vector using a for loop, we first reserve space for
    the elements being added.
    
    We avoid some copying by using const references and std::move.
    
    Bug: 1301149
    Change-Id: Ieae00c840e9c4d5f3568ce5a34b6a06edf195614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488663
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976528}

--- chrome/browser/web_applications/externally_managed_app_manager.cc
+++ chrome/browser/web_applications/externally_managed_app_manager.cc
@@ -93,7 +93,8 @@ void ExternallyManagedAppManager::SynchronizeInstalledApps(
   DCHECK(!base::Contains(synchronize_requests_, install_source));
 
   std::vector<GURL> installed_urls;
-  for (auto apps_it : registrar_->GetExternallyInstalledApps(install_source)) {
+  for (const auto& apps_it :
+       registrar_->GetExternallyInstalledApps(install_source)) {
     // TODO: Remove this check once we cleanup ExternallyInstalledWebAppPrefs on
     // external app uninstall.
     // https://crbug.com/1300382
@@ -109,6 +110,7 @@ void ExternallyManagedAppManager::SynchronizeInstalledApps(
   std::sort(installed_urls.begin(), installed_urls.end());
 
   std::vector<GURL> desired_urls;
+  desired_urls.reserve(desired_apps_install_options.size());
   for (const auto& info : desired_apps_install_options)
     desired_urls.push_back(info.install_url);
 
@@ -147,7 +149,7 @@ void ExternallyManagedAppManager::SynchronizeInstalledApps(
 
 void ExternallyManagedAppManager::SetRegistrationCallbackForTesting(
     RegistrationCallback callback) {
-  registration_callback_ = callback;
+  registration_callback_ = std::move(callback);
 }
 
 void ExternallyManagedAppManager::ClearRegistrationCallbackForTesting() {
