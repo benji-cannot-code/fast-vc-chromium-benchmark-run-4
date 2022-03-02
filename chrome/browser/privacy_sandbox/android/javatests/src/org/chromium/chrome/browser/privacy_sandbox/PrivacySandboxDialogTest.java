4b27437adaa37fed2571cdae26c9b86eafd4fea6
Andrey Zaytsev <andzaytsev@google.com>
2022-03-02 16:16:06 +0000

    Privacy Sandbox Settings: Fix skia tests race condition
    Bug: 1286276, 1300632
    Change-Id: Ib6767b8d915e2f410044605bbee0502c799f00e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497706
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#976688}

--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogTest.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogTest.java
@@ -18,6 +18,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 
+import static org.chromium.ui.test.util.ViewUtils.onViewWaiting;
+
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
@@ -110,6 +112,7 @@ public final class PrivacySandboxDialogTest {
     }
 
     private void renderViewWithId(int id, String renderId) {
+        onViewWaiting(withId(id));
         onView(withId(id)).check((v, noMatchException) -> {
             if (noMatchException != null) throw noMatchException;
             // Allow disk writes and slow calls to render from UI thread.
@@ -148,6 +151,7 @@ public final class PrivacySandboxDialogTest {
             mDialog = new PrivacySandboxDialogConsent(sActivityTestRule.getActivity());
             mDialog.show();
         });
+        onViewWaiting(withId(R.id.privacy_sandbox_dialog));
         onView(withId(R.id.dropdown_element)).perform(scrollTo(), click());
         onView(withId(R.id.privacy_sandbox_consent_dropdown)).perform(scrollTo());
         renderViewWithId(R.id.privacy_sandbox_dialog, "privacy_sandbox_consent_dialog_expanded");
