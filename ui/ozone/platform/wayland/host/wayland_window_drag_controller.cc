3850846493311c26ac8d537b77d5e5fdd05797fc
Nick Diego Yamane <nickdiego@igalia.com>
2022-03-02 02:18:16 +0000

    wayland: tabdrag: no longer dcheck for chromium/x-window mime type
    Due to crbug.com/1102946, the custom 'chromium/x-window' mime type does
    not get offered (through wl_data_offer.offer request), which was not an
    actual problem until https://crrev.com/c/3472073. Since then,
    chromium/x-data-transfer-endpoint mime type is offered unconditionally,
    thus causing the dcheck hit described in the linked crbug.
    
    R=oshima
    
    Bug: 1300393
    Change-Id: I24feebac1ea2cdb7b76aaf8ee6618aaac0c96f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498532
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#976509}

--- ui/ozone/platform/wayland/host/wayland_window_drag_controller.cc
+++ ui/ozone/platform/wayland/host/wayland_window_drag_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/check.h"
+#include "base/containers/contains.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "base/notreached.h"
@@ -233,12 +234,10 @@ void WaylandWindowDragController::OnDragEnter(WaylandWindow* window,
   // TODO(crbug.com/1102946): Exo does not support custom mime types. In this
   // case, |data_offer_| will hold an empty mime_types list and, at this point,
   // it's safe just to skip the offer checks and requests here.
-  if (data_offer_->mime_types().empty())
+  if (!base::Contains(data_offer_->mime_types(), kMimeTypeChromiumWindow)) {
+    DVLOG(1) << "OnEnter. No valid mime type found.";
     return;
-
-  // Ensure this is a valid "window drag" offer.
-  DCHECK_EQ(data_offer_->mime_types().size(), 1u);
-  DCHECK_EQ(data_offer_->mime_types().front(), kMimeTypeChromiumWindow);
+  }
 
   // Accept the offer and set the dnd action.
   data_offer_->SetDndActions(kDndActionWindowDrag);
