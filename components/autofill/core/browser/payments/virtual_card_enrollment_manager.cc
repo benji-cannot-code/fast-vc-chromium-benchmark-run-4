2da155b9dd00113f257bc539701acfea83c4c525
Siyu An <siyua@chromium.org>
2022-03-02 01:53:51 +0000

    [VCN] Fix enroll bubble in Upstream
    Virtual card enroll bubble not shown in Upstream because we wait on
    the card saved animation to be finished but when status chip is not
    enabled there is no card save animation.
    
    Bug: 1302117, 1281695
    Change-Id: I9141ad3e36a925ad7a75626e8e51a1510015f90f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499342
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976504}

--- components/autofill/core/browser/payments/virtual_card_enrollment_manager.cc
+++ components/autofill/core/browser/payments/virtual_card_enrollment_manager.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/strike_database.h"
 #include "components/autofill/core/browser/strike_database_base.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "ui/gfx/image/image.h"
 
 namespace autofill {
@@ -289,7 +290,9 @@ void VirtualCardEnrollmentManager::OnDidGetDetailsForEnrollResponse(
   }
 
 #if !BUILDFLAG(IS_ANDROID)
-  if (state_.virtual_card_enrollment_fields.virtual_card_enrollment_source ==
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillEnableToolbarStatusChip) &&
+      state_.virtual_card_enrollment_fields.virtual_card_enrollment_source ==
           VirtualCardEnrollmentSource::kUpstream &&
       !avatar_animation_complete_) {
     return;
--- components/autofill/core/browser/payments/virtual_card_enrollment_manager_unittest.cc
+++ components/autofill/core/browser/payments/virtual_card_enrollment_manager_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/test_autofill_client.h"
 #include "components/autofill/core/browser/test_autofill_driver.h"
 #include "components/autofill/core/browser/test_personal_data_manager.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/image/image_unittest_util.h"
@@ -466,6 +468,8 @@ TEST_F(VirtualCardEnrollmentManagerTest, UpstreamAnimationSync_AnimationFirst) {
 }
 
 TEST_F(VirtualCardEnrollmentManagerTest, UpstreamAnimationSync_ResponseFirst) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(features::kAutofillEnableToolbarStatusChip);
   personal_data_manager_->ClearCreditCardArtImages();
   SetUpCard();
   SetValidCardArtImageForCard(*card_);
