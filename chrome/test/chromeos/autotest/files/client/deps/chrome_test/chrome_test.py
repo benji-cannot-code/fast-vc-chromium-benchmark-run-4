e5d2f5b6ef7fd5d1566353cc300cbb5eaa9372c5
Ben Pastene <bpastene@chromium.org>
2022-03-02 18:06:03 +0000

    autotest: Remove 'commands' from import list in chrome_test.py
    This should hopefully help switch some ebuilds over to python3.
    
    Bug: None
    Change-Id: I1d25da8b51a941c58655b0ffca16104f027e74b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499061
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976730}

--- chrome/test/chromeos/autotest/files/client/deps/chrome_test/chrome_test.py
+++ chrome/test/chromeos/autotest/files/client/deps/chrome_test/chrome_test.py
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import common, commands, logging, os
+import common, os
 from autotest_lib.client.bin import utils
 
 version = 1
