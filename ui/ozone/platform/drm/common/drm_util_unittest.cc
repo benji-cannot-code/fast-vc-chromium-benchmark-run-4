3b5167e1cdabb8bf33c2cc7bacbc537b3fe6e98b
Xianzhu Wang <wangxianzhu@chromium.org>
2022-03-03 02:01:46 +0000

    Remove unnecessary include of skia_matrix_44.h from ui/ozone
    R=mcasas@chromium.org
    
    Bug: 1167153
    Change-Id: I97570dd380fc49d02ff165ac636c2161430c3f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499751
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Miguel Casas-Sanchez <mcasas@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976916}

--- ui/ozone/platform/drm/common/drm_util_unittest.cc
+++ ui/ozone/platform/drm/common/drm_util_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
-#include "skia/ext/skia_matrix_44.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "ui/display/types/display_snapshot.h"
