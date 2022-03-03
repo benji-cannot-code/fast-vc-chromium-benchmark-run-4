cbec7f91a5481f3899a841e53a47d3c1995e6ee0
Evan Stade <estade@chromium.org>
2022-03-03 07:20:05 +0000

    Fix UAF in apps page.
    Bug: 1302157
    Change-Id: I078d20add15bccec84ba13c384c191fd3b60c85b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498946
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977002}

--- chrome/browser/ui/webui/ntp/app_launcher_handler.h
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/task/cancelable_task_tracker.h"
@@ -184,6 +185,9 @@ class AppLauncherHandler
   void HandleRunOnOsLogin(const base::ListValue* args);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(AppLauncherHandlerTest,
+                           HandleClosedWhileUninstallingExtension);
+
   struct AppInstallInfo {
     AppInstallInfo();
     ~AppInstallInfo();
--- chrome/browser/ui/webui/ntp/app_launcher_handler_unittest.cc
+++ chrome/browser/ui/webui/ntp/app_launcher_handler_unittest.cc
@@ -17,16 +17,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_sync_bridge.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_renderer_host.h"
 #include "content/public/test/test_web_ui.h"
 #include "content/public/test/web_contents_tester.h"
+#include "extensions/common/extension_builder.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace web_app {
+using web_app::AppId;
+using web_app::WebAppProvider;
 
 namespace {
 
@@ -77,7 +79,7 @@ std::unique_ptr<WebAppInstallInfo> BuildWebAppInfo() {
 
 }  // namespace
 
-class AppLauncherHandlerTest : public testing::Test {
+class AppLauncherHandlerTest : public BrowserWithTestWindowTest {
  public:
   AppLauncherHandlerTest() = default;
 
@@ -87,13 +89,7 @@ class AppLauncherHandlerTest : public testing::Test {
   ~AppLauncherHandlerTest() override = default;
 
   void SetUp() override {
-    testing::Test::SetUp();
-
-    TestingProfile::Builder builder;
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-    builder.SetIsMainProfile(true);
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
-    testing_profile_ = builder.Build();
+    BrowserWithTestWindowTest::SetUp();
 
     extension_service_ = CreateTestExtensionService();
 
@@ -103,8 +99,6 @@ class AppLauncherHandlerTest : public testing::Test {
   }
 
  protected:
-  Profile* profile() { return testing_profile_->GetOriginalProfile(); }
-
   std::unique_ptr<TestAppLauncherHandler> GetAppLauncherHandler(
       content::TestWebUI* test_web_ui) {
     return std::make_unique<TestAppLauncherHandler>(
@@ -114,7 +108,8 @@ class AppLauncherHandlerTest : public testing::Test {
   // Install a web app and sets the locally installed property based on
   // |is_locally_installed|.
   AppId InstallWebApp(bool is_locally_installed = true) {
-    AppId installed_app_id = test::InstallWebApp(profile(), BuildWebAppInfo());
+    AppId installed_app_id =
+        web_app::test::InstallWebApp(profile(), BuildWebAppInfo());
     if (is_locally_installed)
       return installed_app_id;
 
@@ -157,12 +152,11 @@ class AppLauncherHandlerTest : public testing::Test {
   }
 
   std::unique_ptr<content::WebContents> CreateTestWebContents() {
-    auto site_instance = content::SiteInstance::Create(browser_context());
+    auto site_instance = content::SiteInstance::Create(profile());
     return content::WebContentsTester::CreateTestWebContents(
-        browser_context(), std::move(site_instance));
+        profile(), std::move(site_instance));
   }
 
- private:
   extensions::ExtensionService* CreateTestExtensionService() {
     auto* extension_system = static_cast<extensions::TestExtensionSystem*>(
         extensions::ExtensionSystem::Get(profile()));
@@ -173,11 +167,6 @@ class AppLauncherHandlerTest : public testing::Test {
     return ext_service;
   }
 
-  content::BrowserContext* browser_context() { return testing_profile_.get(); }
-
-  content::BrowserTaskEnvironment task_environment_;
-  content::RenderViewHostTestEnabler render_view_host_test_enabler_;
-  std::unique_ptr<TestingProfile> testing_profile_;
   web_app::OsIntegrationManager::ScopedSuppressForTesting os_hooks_suppress_;
   raw_ptr<extensions::ExtensionService> extension_service_;
 };
@@ -247,4 +236,24 @@ TEST_F(AppLauncherHandlerTest, HandleInstallAppLocally_MultipleWebUI) {
                                    installed_app_id);
 }
 
-}  // namespace web_app
+// Regression test for crbug.com/1302157.
+TEST_F(AppLauncherHandlerTest, HandleClosedWhileUninstallingExtension) {
+  scoped_refptr<const extensions::Extension> extension =
+      extensions::ExtensionBuilder("foo").Build();
+  extension_service_->AddExtension(extension.get());
+
+  AddTab(browser(), GURL("http://foo/1"));
+  content::WebContents* contents =
+      browser()->tab_strip_model()->GetWebContentsAt(0);
+  std::unique_ptr<content::WebContents> test_web_contents =
+      CreateTestWebContents();
+  std::unique_ptr<content::TestWebUI> test_web_ui = CreateTestWebUI(contents);
+  std::unique_ptr<TestAppLauncherHandler> app_launcher_handler =
+      GetAppLauncherHandler(test_web_ui.get());
+
+  app_launcher_handler->CreateExtensionUninstallDialog()->ConfirmUninstall(
+      extension, extensions::UNINSTALL_REASON_USER_INITIATED,
+      extensions::UNINSTALL_SOURCE_CHROME_APPS_PAGE);
+  app_launcher_handler.reset();
+  // No crash (in asan tester) indicates a passing score.
+}
