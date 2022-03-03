6cdbb30a130fc395077e3be3aebf39573d78cc93
Alexander Alekseev <alemate@chromium.org>
2022-03-03 08:21:49 +0000

    [ash test] Implement keep-login-events-for-testing flag.
    This test flag will activate
    AutotestPrivate.startLoginEventRecorderDataCollection when process
    starts for testing.
    
    Bug: b/215228561
    Change-Id: Id5ed3c31c3d472074d9e7d3dc5944bbcbc67d153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499421
    Auto-Submit: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977016}

--- chromeos/metrics/login_event_recorder.cc
+++ chromeos/metrics/login_event_recorder.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/json/json_reader.h"
@@ -298,6 +299,13 @@ LoginEventRecorder::LoginEventRecorder()
   DCHECK(base::CurrentUIThread::IsSet());
   login_time_markers_.reserve(30);
   logout_time_markers_.reserve(30);
+
+  // Remember login events for later retrieval by tests.
+  constexpr char kKeepLoginEventsForTesting[] = "keep-login-events-for-testing";
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          kKeepLoginEventsForTesting)) {
+    PrepareEventCollectionForTesting();  // IN-TEST
+  }
 }
 
 LoginEventRecorder::~LoginEventRecorder() = default;
