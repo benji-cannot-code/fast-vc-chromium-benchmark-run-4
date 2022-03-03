b24982c5b64aaec6107b6dc3b7bb3b3384330b04
Christian Xu <christianxu@google.com>
2022-03-03 12:59:21 +0000

    [iOS] Add optional title in Bubble View
    Adds optional title in BubbleView.
    When setting a title, the body's font will become smaller.
    
    Adds the option to change the TextAlignment.
    
    These options will be used later on for Autofill IPH to present
    different variants of BubbleViews.
    
    Before:
    https://drive.google.com/file/d/13PMTnaFinSLjAlGxW1Tq2aoWnKdLg_gk/view?usp=sharing
    
    Title day:
    https://drive.google.com/file/d/13QEqIh2iZRng5fBJsz5UEPWMCCYkKC25/view?usp=sharing
    
    Title dark:
    https://drive.google.com/file/d/13YXxDFnaGoZ15SXn4MyvlrqpZyFzozdW/view?usp=sharing
    
    Natural text alignment:
    https://drive.google.com/file/d/13QwhAtj09J4zN3NO1x94PzF0KeH1X86N/view?usp=sharing
    
    Bug: 1286669
    Change-Id: I60282932e15d946a9c3779541872f31ebc06c1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3479918
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977056}

--- ios/chrome/browser/ui/bubble/bubble_view.h
+++ ios/chrome/browser/ui/bubble/bubble_view.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Accessibility identifier for the close button.
 extern NSString* const kBubbleViewCloseButtonIdentifier;
+// Accessibility identifier for the title label.
+extern NSString* const kBubbleViewTitleLabelIdentifier;
 
 // Direction for the bubble to point.
 typedef NS_ENUM(NSInteger, BubbleArrowDirection) {
@@ -58,6 +60,13 @@ typedef NS_ENUM(NSInteger, BubbleAlignment) {
 // Controls if there is a close button in the view. Must be set before the view
 // is added to a superview.
 @property(nonatomic) BOOL showsCloseButton;
+
+// The headline of the bubble. If set, makes the body's font smaller. Must be
+// set before the view is added to a superview.
+@property(nonatomic, copy) NSString* titleString;
+
+// Text alignment used in this View. Default is NSTextAlignmentCenter.
+@property(nonatomic) NSTextAlignment textAlignment;
 
 // The delegate for interactions in this View.
 @property(nonatomic, weak) id<BubbleViewDelegate> delegate;
--- ios/chrome/browser/ui/bubble/bubble_view_unittest.mm
+++ ios/chrome/browser/ui/bubble/bubble_view_unittest.mm
@@ -64,14 +64,26 @@ class BubbleViewTest : public PlatformTest {
   // multiple lines.
   NSString* longText_;
 
-  UIButton* GetCloseButton(BubbleView* bubbleView) {
+  UIView* GetViewOfClassWithIdentifier(Class uiClass,
+                                       NSString* accessibilityIdentifier,
+                                       BubbleView* bubbleView) {
     for (UIView* subview in bubbleView.subviews) {
-      if ([subview isKindOfClass:[UIButton class]] &&
-          subview.accessibilityIdentifier == kBubbleViewCloseButtonIdentifier) {
-        return base::mac::ObjCCastStrict<UIButton>(subview);
+      if ([subview isKindOfClass:uiClass] &&
+          subview.accessibilityIdentifier == accessibilityIdentifier) {
+        return subview;
       }
     }
     return nil;
+  }
+
+  UIButton* GetCloseButton(BubbleView* bubbleView) {
+    return base::mac::ObjCCastStrict<UIButton>(GetViewOfClassWithIdentifier(
+        [UIButton class], kBubbleViewCloseButtonIdentifier, bubbleView));
+  }
+
+  UILabel* GetTitleLabel(BubbleView* bubbleView) {
+    return base::mac::ObjCCastStrict<UILabel>(GetViewOfClassWithIdentifier(
+        [UILabel class], kBubbleViewTitleLabelIdentifier, bubbleView));
   }
 };
 
@@ -138,4 +150,29 @@ TEST_F(BubbleViewTest, CloseButtonActionAndPresent) {
   // Tests close button action.
   [closeButton sendActionsForControlEvents:UIControlEventTouchUpInside];
   EXPECT_EQ(delegate.tapCounter, 1);
+}
+
+// Tests that the title is not showed when the option is set to hidden.
+TEST_F(BubbleViewTest, TitleIsNotPresent) {
+  BubbleView* bubble = [[BubbleView alloc] initWithText:longText_
+                                         arrowDirection:arrowDirection_
+                                              alignment:alignment_];
+  [bubble setTitleString:nil];
+  UIView* superview = [[UIView alloc] initWithFrame:CGRectZero];
+  [superview addSubview:bubble];
+  UILabel* titleLabel = GetTitleLabel(bubble);
+  ASSERT_FALSE(titleLabel);
+}
+
+// Tests that the title is present and correct.
+TEST_F(BubbleViewTest, TitleIsPresentAndCorrect) {
+  BubbleView* bubble = [[BubbleView alloc] initWithText:longText_
+                                         arrowDirection:arrowDirection_
+                                              alignment:alignment_];
+  [bubble setTitleString:shortText_];
+  UIView* superview = [[UIView alloc] initWithFrame:CGRectZero];
+  [superview addSubview:bubble];
+  UILabel* titleLabel = GetTitleLabel(bubble);
+  ASSERT_TRUE(titleLabel);
+  ASSERT_EQ(titleLabel.text, shortText_);
 }
