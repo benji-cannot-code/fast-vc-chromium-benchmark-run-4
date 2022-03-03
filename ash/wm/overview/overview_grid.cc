b35db00ec4f64c8134a0f3d41b31aa9e07d59787
Sophie Wen <sophiewen@google.com>
2022-03-03 22:18:46 +0000

    desk_templates: Update accessibility focus when showing templates grid.
    Currently showing templates grid doesn't allow a11y to navigate between
    the grid and desks bar. We call `UpdateAccessibilityFocus` to add the
    grid to a11y widgets.
    
    Test: manual + existing
    Fixed: 1279557
    Change-Id: I421977505e8e6c7f661ea84609d9ddf494b79778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502270
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@google.com>
    Cr-Commit-Position: refs/heads/main@{#977359}

--- ash/wm/overview/overview_grid.cc
+++ ash/wm/overview/overview_grid.cc
@@ -1740,8 +1740,6 @@ void OverviewGrid::ShowDesksTemplatesGrid(bool was_zero_state) {
   desks_templates_grid_widget_->Show();
 
   // Fade in the widget from its current opacity.
-  // TODO(crbug.com/1277160): Consider adding animate flag to determine whether
-  // to disable animations.
   PerformFadeInLayer(desks_templates_grid_widget_->GetLayer(),
                      /*animate=*/true);
 
@@ -1752,6 +1750,7 @@ void OverviewGrid::ShowDesksTemplatesGrid(bool was_zero_state) {
                                         /*expanded_desks_bar_button=*/true);
   }
   desks_bar_view_->UpdateButtonsForDesksTemplatesGrid();
+  overview_session_->UpdateAccessibilityFocus();
 }
 
 void OverviewGrid::HideDesksTemplatesGrid(bool exit_overview) {
@@ -1797,6 +1796,7 @@ void OverviewGrid::HideDesksTemplatesGrid(bool exit_overview) {
       /*animate=*/true,
       base::BindOnce(&OverviewGrid::OnDesksTemplatesGridFadedOut,
                      weak_ptr_factory_.GetWeakPtr()));
+  overview_session_->UpdateAccessibilityFocus();
 }
 
 bool OverviewGrid::IsShowingDesksTemplatesGrid() const {
