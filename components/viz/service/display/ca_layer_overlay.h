294337286d4898464917a10347ebe001d9a444bd
Xianzhu Wang <wangxianzhu@chromium.org>
2022-03-03 02:31:18 +0000

    Avoid direct reference to skia::Matrix44 in components/viz
    This allows us to change the underlying matrix type of gfx::Transform
    without changing other code.
    
    This CL was uploaded by git cl split.
    
    R=penghuang@chromium.org
    
    Bug: 1167153
    Change-Id: I47f68efb32e6ad5d876268cdabd4c97d8af8abb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499747
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976937}

--- components/viz/service/display/ca_layer_overlay.h
+++ components/viz/service/display/ca_layer_overlay.h
@@ -14,12 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_render_pass.h"
 #include "components/viz/service/viz_service_export.h"
 #include "gpu/command_buffer/common/mailbox.h"
-#include "skia/ext/skia_matrix_44.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "ui/gfx/ca_layer_result.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/rrect_f.h"
+#include "ui/gfx/geometry/transform.h"
 #include "ui/gfx/video_types.h"
 #include "ui/gl/ca_renderer_layer_params.h"
 
@@ -47,8 +47,7 @@ class VIZ_SERVICE_EXPORT CALayerOverlaySharedState
   // The opacity property for the CAayer.
   float opacity = 1;
   // The transform to apply to the CALayer.
-  skia::Matrix44 transform =
-      skia::Matrix44(skia::Matrix44::kIdentity_Constructor);
+  gfx::Transform transform;
 
  private:
   friend class base::RefCountedThreadSafe<CALayerOverlaySharedState>;
