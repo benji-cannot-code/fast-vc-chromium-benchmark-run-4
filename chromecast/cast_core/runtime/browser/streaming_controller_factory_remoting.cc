144b1601c978e7e68f7e3ecc876005073a04b749
Ryan Keane <rwkeane@google.com>
2022-03-03 18:57:36 +0000

    [Cast Remoting] Plumb support for Cast Streaming
    This CL plumbs the implementation of Remoting through the cast_streaming
    code, as well as updates the CWR to use new Remoting implementation in
    //components/cast_streaming
    
    Bug: b/182429524
    Change-Id: I0b20040cbee6e50f2990edc27023a7dc719e6d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3318764
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Fabrice de Gans <fdegans@chromium.org>
    Commit-Queue: Ryan Keane <rwkeane@google.com>
    Cr-Commit-Position: refs/heads/main@{#977225}

---
+++ chromecast/cast_core/runtime/browser/streaming_controller_factory_remoting.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+
+#include "chromecast/cast_core/runtime/browser/streaming_controller_base.h"
+#include "chromecast/cast_core/runtime/browser/streaming_controller_remoting.h"
+#include "components/cast/message_port/message_port.h"
+
+namespace chromecast {
+
+// static
+std::unique_ptr<StreamingController> StreamingControllerBase::Create(
+    std::unique_ptr<cast_api_bindings::MessagePort> message_port,
+    CastWebContents* cast_web_contents) {
+  return std::make_unique<StreamingControllerRemoting>(std::move(message_port),
+                                                       cast_web_contents);
+}
+
+}  // namespace chromecast
