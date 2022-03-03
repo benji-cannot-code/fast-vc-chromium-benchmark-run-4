1f520e9c419542ab986eb2c0ee2e64cd9af23de8
Keren Zhu <kerenzhu@chromium.org>
2022-03-03 21:51:42 +0000

    Remove GetThemeProviderForProfile() from 3rd party NTPUI
    * Use empty string for theme generation when theme provider is not
      available.
    * Query webui::GetThemeProvider() for theme provider. If that is not
      available, get it from the last created browser window.
    
    Bug: 1056758
    Change-Id: I93022a51412b8d7aaeda38e50732e810bc519a7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3456586
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977343}

--- chrome/browser/ui/webui/webui_util.cc
+++ chrome/browser/ui/webui/webui_util.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(TOOLKIT_VIEWS)
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/widget/widget.h"
@@ -120,7 +121,20 @@ const ui::ThemeProvider* GetThemeProvider(content::WebContents* web_contents) {
 
   auto* browser_window =
       BrowserWindow::FindBrowserWindowWithWebContents(web_contents);
-  return browser_window ? browser_window->GetThemeProvider() : nullptr;
+
+  if (browser_window)
+    return browser_window->GetThemeProvider();
+
+  // Fallback: get the theme provider from the last created browser.
+  // This is used in newly created tabs, e.g. NewTabPageUI. They need access to
+  // the theme browser before the WebContents is attached to a browser window.
+  // TODO(crbug.com/1298767): Remove this fallback by associating the
+  // WebContents during navigation.
+  BrowserList* browser_list = BrowserList::GetInstance();
+  const Browser* browser = browser_list->empty()
+                               ? nullptr
+                               : *std::prev(BrowserList::GetInstance()->end());
+  return browser ? browser->window()->GetThemeProvider() : nullptr;
 }
 
 void SetThemeProviderForTesting(const ui::ThemeProvider* theme_provider) {
