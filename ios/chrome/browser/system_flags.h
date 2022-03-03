27f9c69e9cf7db3f36ed8c4961dca7476597d903
Sylvain Defresne <sdefresne@chromium.org>
2022-03-03 16:18:24 +0000

    [ios] Remove GaiaEnvironment override from Experimental Settings
    The function GetGaiaEnvironment() is never used, so remove it
    and the entry in Experimental.plist.
    
    Bug: none
    Change-Id: Ia8c06b11c510a1ef6eb00d6c02bdaa36c9983a93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497693
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977118}

--- ios/chrome/browser/system_flags.h
+++ ios/chrome/browser/system_flags.h
@@ -16,12 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace experimental_flags {
 
-enum GaiaEnvironment {
-  GAIA_ENVIRONMENT_PROD,
-  GAIA_ENVIRONMENT_STAGING,
-  GAIA_ENVIRONMENT_TEST,
-};
-
 enum WhatsNewPromoStatus {
   WHATS_NEW_DEFAULT = 0,          // Not forced to enable a promo.
   WHATS_NEW_TEST_COMMAND_TIP,     // Test Tip that runs a command.
@@ -33,8 +27,6 @@ enum WhatsNewPromoStatus {
 
 // Whether the First Run UI will be always be displayed.
 bool AlwaysDisplayFirstRun();
-
-GaiaEnvironment GetGaiaEnvironment();
 
 // Returns the host name for an alternative Origin Server host for use by
 // |BrandCode| startup ping. Returns empty string if there is no alternative
