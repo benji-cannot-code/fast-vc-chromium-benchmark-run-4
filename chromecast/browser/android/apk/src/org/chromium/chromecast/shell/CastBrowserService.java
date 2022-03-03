d3cddd1723ba1c81381bb75f140d953449336093
Simeon Anfinrud <sanfin@chromium.org>
2022-03-03 23:14:44 +0000

    [Chromecast] Add ChromecastBrowserService to cast_shell_apk.
    This service can start the browser in response to an adb
    command to `startservice` or an Intent from an external apk.
    
    Further work to write up bindings for IPC between the base
    module and the browser module will arrive in follow-up CLs.
    
    org.chromium.chromecast.shell/.CastBrowserService`
    
    Bug: b/213235052
    Bug: b/183077904
    Test: run `adb shell am startservice
    Change-Id: I3f60f3dfdb7c47e9e8202cfcb09fa2bdf0d8f13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500979
    Auto-Submit: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Sean Topping <seantopping@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977381}

---
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastBrowserService.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromecast.shell;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * This Service allows launching the Cast browser module through an Intent. When the service is
+ * created, the browser main loop will start. This allows launching the browser module separately
+ * from the base module, so that the memory overhead of the browser is only incurred when needed.
+ */
+public class CastBrowserService extends Service {
+    @Override
+    public void onCreate() {
+        CastBrowserHelper.initializeBrowser(getApplicationContext());
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
