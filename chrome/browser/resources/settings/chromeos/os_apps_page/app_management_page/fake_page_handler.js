2275e6352cbde65f52a2203343103f5c35f8ec2e
echo linker <ajlinker@chromium.org>
2022-03-03 04:21:38 +0000

    [Reland][App Details] Add installation source to App Management backend.
    Reland of crrev.com/c/3473745. Pipes the installation source from the
    App Service into App Management.
    
    Bug: 1295875
    Change-Id: I994711e31bb98f624de8423ca76fdf10e8016cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3493240
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: echo linker <ajlinker@chromium.org>
    Reviewed-by: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976955}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/fake_page_handler.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/fake_page_handler.js
@@ -111,6 +111,7 @@ export class FakePageHandler {
       supportedLinks: [],
       runOnOsLogin: null,
       fileHandlingState: null,
+      installSource: apps.mojom.InstallSource.kUnknown,
     };
 
     if (optConfig) {
