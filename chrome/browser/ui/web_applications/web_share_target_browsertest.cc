cb3fcb4206246b12e9a81db6423d663e62ec2818
Eric Willigers <ericwilligers@chromium.org>
2022-03-03 05:25:05 +0000

    Desktop PWAs: remove redundant includes
    Detected using clang-tidy readability-duplicate-include
    
    Bug: 1301149
    Change-Id: Ie24576ae66c3e6a431e522c0369193b2c13fa11a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499494
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976979}

--- chrome/browser/ui/web_applications/web_share_target_browsertest.cc
+++ chrome/browser/ui/web_applications/web_share_target_browsertest.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chrome/browser/apps/app_service/intent_util.h"
 #include "chromeos/crosapi/mojom/app_service_types.mojom.h"
 #include "chromeos/crosapi/mojom/sharesheet.mojom.h"
 #include "chromeos/crosapi/mojom/sharesheet_mojom_traits.h"
--- chrome/browser/web_applications/web_app_install_finalizer_unittest.cc
+++ chrome/browser/web_applications/web_app_install_finalizer_unittest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/web_app_test.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_icon_manager.h"
-#include "chrome/browser/web_applications/web_app_install_finalizer.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/browser/web_applications/web_app_install_manager.h"
 #include "chrome/browser/web_applications/web_app_install_manager_observer.h"
