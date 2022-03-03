3dd0910dd4eab287cc0119ea830e0c63b341f521
Susanne Westphal <swestphal@chromium.org>
2022-03-03 19:10:30 +0000

    Move AddInstallShortcut call to java function in separate cpp class.
    This should not change any logic but makes the shortcut creation also available in other cpp classes which will be needed for the webapk installation in weblayer.
    
    Change-Id: I247abed48ecb2626a24962021403edc34b401e1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3488399
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977242}

---
+++ weblayer/browser/webapps/webapps_utils.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBLAYER_BROWSER_WEBAPPS_WEBAPPS_UTILS_H_
+#define WEBLAYER_BROWSER_WEBAPPS_WEBAPPS_UTILS_H_
+
+#include <string>
+
+class GURL;
+class SkBitmap;
+
+namespace webapps {
+
+// Adds a shortcut to the home screen. Calls the native method
+// addShortcutToHomescreen in WebappsHelper.java
+void addShortcutToHomescreen(const std::string& id,
+                             const GURL& url,
+                             const std::u16string& user_title,
+                             const SkBitmap& primary_icon,
+                             bool is_primary_icon_maskable);
+
+}  // namespace webapps
+
+#endif  // WEBLAYER_BROWSER_WEBAPPS_WEBAPPS_UTILS_H_
