ba557e959136dd8e3f34a450ecd881a2dfed93f1
Shaobo <shaobo.yan@intel.com>
2022-03-03 08:16:54 +0000

    Use CopyTextureForBrowser() for CopyExternalImageToTexture() CPU uploading path
    In previous, CopyExternalImageToTexture handles CPU source resource with Skia.
    It may triggers CPU transforms and we find some issues on specific platform.
    
    Since CopyTextureForBrowser() is mature and much more stable, we use
    CopyTextureForBrowser() to handle CPU uploading path too.
    
    This change alsore moves some files and duplicate logics.
    
    Bug: 1270986, 1269118, dawn:1279
    Change-Id: Ida2e3ea6244791638a7645d3419c79dd285628ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3470837
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    Cr-Commit-Position: refs/heads/main@{#977014}

--- third_party/blink/renderer/modules/webgpu/gpu_device.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_device.cc
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webgpu/gpu_uncaptured_error_event.h"
 #include "third_party/blink/renderer/modules/webgpu/gpu_validation_error.h"
 #include "third_party/blink/renderer/platform/bindings/microtask.h"
-#include "third_party/blink/renderer/platform/graphics/gpu/webgpu_image_bitmap_handler.h"
 
 namespace blink {
 
