e34e33ac2e998164e0487ad0e3cd1cffad06d2b6
Alan Ding <alanding@google.com>
2022-03-03 04:44:08 +0000

    arc: Add OpenFileSessionToWrite and CloseFileSession to operation runner
    Currently openFileToWrite in ARC detach the Android ParcelFileDescriptor
    and causes DocumentsProvider app's onClose() to be called immediately
    before the actual copy / write file operation(s) even started. This in
    turn results in a FileDescriptorDetachedException.
    
    This issue was reported by 3P dev and triaged by Chrome OS DevRel.
    The fix is to map the open url to the original ParcelFileDescriptor so
    that the object can be closed with close() or closeWithError(String)
    after the ChromeOS side file write operation(s) are completed.
    
    There is a follow-up CL to update FileStreamWriter runner calls but
    it must be done after this CL and the Android CLs are landed.
    
    The Android CLs counterpart is ag/16963727.
    
    BUG=b:215364617
    TEST=./out/DEBUG/unit_tests --gtest_filter=ArcFileSystemOperationRunnerTest*
    
    Change-Id: I784a240f7634d2f4e0316ebb2268fc02a536bb47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3496390
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Alan Ding <alanding@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976972}

--- chrome/browser/ash/arc/fileapi/arc_file_system_operation_runner_unittest.cc
+++ chrome/browser/ash/arc/fileapi/arc_file_system_operation_runner_unittest.cc
@@ -31,6 +31,7 @@ constexpr char kAuthority[] = "authority";
 constexpr char kDocumentId[] = "document_id";
 constexpr char kRootId[] = "root_id";
 constexpr char kUrl[] = "content://test";
+constexpr char kUrlId[] = "url_id";
 
 }  // namespace
 
@@ -133,6 +134,12 @@ class ArcFileSystemOperationRunnerTest : public testing::Test {
         base::BindOnce(
             [](int* counter, mojo::ScopedHandle handle) { ++*counter; },
             counter));
+    runner_->OpenFileSessionToWrite(
+        GURL(kUrl),
+        base::BindOnce([](int* counter,
+                          mojom::FileSessionPtr file_session) { ++*counter; },
+                       counter));
+    runner_->CloseFileSession(kUrlId, /*error_message=*/std::string());
 
     // RemoveWatcher() is never deferred.
     runner_->RemoveWatcher(
@@ -155,7 +162,7 @@ TEST_F(ArcFileSystemOperationRunnerTest, RunImmediately) {
   CallSetShouldDefer(false);
   CallAllFunctions(&counter);
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ(11, counter);
+  EXPECT_EQ(12, counter);
 }
 
 TEST_F(ArcFileSystemOperationRunnerTest, DeferAndRun) {
@@ -167,7 +174,7 @@ TEST_F(ArcFileSystemOperationRunnerTest, DeferAndRun) {
 
   CallSetShouldDefer(false);
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ(11, counter);
+  EXPECT_EQ(12, counter);
 }
 
 // TODO(nya,hidehiko): Check if we should keep this test.
@@ -192,7 +199,7 @@ TEST_F(ArcFileSystemOperationRunnerTest, FileInstanceUnavailable) {
   CallSetShouldDefer(false);
   CallAllFunctions(&counter);
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ(11, counter);
+  EXPECT_EQ(12, counter);
 }
 
 }  // namespace arc
