28c218c4d84029f471a94dbf8fba45c1097cf48e
Wan-Teh Chang <wtc@google.com>
2022-03-03 22:05:52 +0000

    Revert "Roll src/third_party/dav1d/libdav1d/ cce2b0564..b010080fe (29 commits)"
    This reverts commit b0c508ec58b179adba7cb0ca3d0b2a132e528e8f.
    
    Reason for revert: Broke the test media.desktop/video.html?src=tulip0.av1.mp4&seek
    
    Original change's description:
    > Roll src/third_party/dav1d/libdav1d/ cce2b0564..b010080fe (29 commits)
    >
    > https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/cce2b0564d3b..b010080fea75
    >
    > $ git log cce2b0564..b010080fe --date=short --no-merges --format='%ad %ae %s'
    > 2022-02-28 gramner x86: Fix data being stored below the stack pointer in SSSE3 itx asm
    > 2022-02-28 gramner x86: Remove redundant labels and undefs in SSSE3 itx asm
    > 2022-02-19 jb NEWS for 1.0.0
    > 2022-02-20 jamrial reset Dav1dDataProps to defaults when unreferencing data and pictures
    > 2022-02-20 jamrial obu: make all parsing errors use the same error path
    > 2022-02-25 jamrial lib: remove some unnecessary brackets
    > 2022-02-25 wtc Remove redundant unref of c->in in close_internal
    > 2022-02-25 wtc Unreference c->cache in dav1d_flush()
    > 2022-02-23 wtc Assign 8bpc avx2 functions if BITDEPTH == 8
    > 2022-02-22 wtc Always initialize b->uv_angle in decode_b()
    > 2022-02-21 gramner x86: Update x86inc.asm
    > 2022-02-19 code Update THANKS.md
    > 2022-02-19 jb Update README for 1.0
    > 2022-02-16 rsbultje task_thread: use EINVAL/ENOMEM instead of -1 for f->task_thread.retval
    > 2022-02-16 rsbultje Forward frame-thread decoding errors back to user thread
    > 2022-02-16 rsbultje decode.c: remove dead assignment
    > 2022-02-16 rsbultje decode.c: fix return value on bitstream decoding errors
    > 2022-02-16 rsbultje tools/dav1d: fix infinite loop on corrupt bitstreams
    > 2022-02-09 code meson: Use native check of return value
    > 2022-02-09 rsbultje Fix typo in EINVAL comparison
    > 2022-01-28 andre.kempe arm64: Add Armv8.3-A PAC support to assembly files
    > 2022-02-02 gramner x86: Add high bit-depth ipred filter AVX-512 (Ice Lake) asm
    > 2022-02-02 gramner x86: Add high bit-depth pal_pred AVX-512 (Ice Lake) asm
    > 2022-02-02 gramner x86: Add high bit-depth ipred smooth AVX-512 (Ice Lake) asm
    > 2022-02-02 gramner x86: Add high bit-depth ipred paeth AVX-512 (Ice Lake) asm
    > 2022-02-07 rsbultje tools/dav1d: continue on recoverable bitstream decoding errors
    > 2022-02-04 jamrial lib: fix signaling frames ready to be returned when no frame delay is expected
    > 2022-02-03 rsbultje Output only latest spatial layer if --alllayers 0
    > 2022-02-03 jamrial add a picture flag to signal that a new temporal delimiter unit was seen
    >
    > Created with:
    >   roll-dep src/third_party/dav1d/libdav1d
    > R=​dalecurtis@chromium.org,sandersd@chromium.org
    >
    > Bug: 1301452
    > Change-Id: I668096376f7ac0bb1ae5b8c1b1dd8e075b8804de
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3495543
    > Commit-Queue: Wan-Teh Chang <wtc@google.com>
    > Auto-Submit: Wan-Teh Chang <wtc@google.com>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#975898}
    
    Bug: 1301452
    Bug: 1302703
    Change-Id: Ifccd8d6e4ddff7bcf189d68906c0f98869246ac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3501299
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977352}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "0.9.2-151-gb010080"
+#define DAV1D_VERSION "0.9.2-122-gcce2b05"
