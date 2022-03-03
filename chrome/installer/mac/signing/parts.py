71d03e32d7652f8835a5d609d3e5f1bd40ad50ba
Avi Drissman <avi@chromium.org>
2022-03-03 19:21:12 +0000

    Convert signing script to use real enums
    Now that the signing script can assume Python 3, change VerifyOptions
    and CodeSignOptions to be real enums.
    
    Bug: 1291703
    Change-Id: I4f36dfa616bdbd23f2ad122a28aaa210b04c99da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498183
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977255}

--- chrome/installer/mac/signing/parts.py
+++ chrome/installer/mac/signing/parts.py
@@ -34,7 +34,7 @@ def get_parts(config):
     else:
         uncustomized_bundle_id = config.base_bundle_id
 
-    verify_options = VerifyOptions.DEEP + VerifyOptions.STRICT
+    verify_options = VerifyOptions.DEEP | VerifyOptions.STRICT
 
     parts = {
         'app':
@@ -74,8 +74,8 @@ def get_parts(config):
                 # Do not use |CodeSignOptions.FULL_HARDENED_RUNTIME_OPTIONS|
                 # because library validation is incompatible with the JIT
                 # entitlement.
-                options=CodeSignOptions.RESTRICT + CodeSignOptions.KILL +
-                CodeSignOptions.HARDENED_RUNTIME,
+                options=CodeSignOptions.RESTRICT | CodeSignOptions.KILL
+                | CodeSignOptions.HARDENED_RUNTIME,
                 entitlements='helper-renderer-entitlements.plist',
                 verify_options=verify_options),
         'helper-gpu-app':
@@ -86,8 +86,8 @@ def get_parts(config):
                 # Do not use |CodeSignOptions.FULL_HARDENED_RUNTIME_OPTIONS|
                 # because library validation is incompatible with more
                 # permissive code signing entitlements.
-                options=CodeSignOptions.RESTRICT + CodeSignOptions.KILL +
-                CodeSignOptions.HARDENED_RUNTIME,
+                options=CodeSignOptions.RESTRICT | CodeSignOptions.KILL
+                | CodeSignOptions.HARDENED_RUNTIME,
                 entitlements='helper-gpu-entitlements.plist',
                 verify_options=verify_options),
         'helper-plugin-app':
@@ -98,8 +98,8 @@ def get_parts(config):
                 # Do not use |CodeSignOptions.FULL_HARDENED_RUNTIME_OPTIONS|
                 # because library validation is incompatible with the
                 # disable-library-validation entitlement.
-                options=CodeSignOptions.RESTRICT + CodeSignOptions.KILL +
-                CodeSignOptions.HARDENED_RUNTIME,
+                options=CodeSignOptions.RESTRICT | CodeSignOptions.KILL
+                | CodeSignOptions.HARDENED_RUNTIME,
                 entitlements='helper-plugin-entitlements.plist',
                 verify_options=verify_options),
         'helper-alerts':
@@ -159,13 +159,13 @@ def get_installer_tools(config):
     )
     for binary in binaries:
         options = (
-            CodeSignOptions.HARDENED_RUNTIME + CodeSignOptions.RESTRICT +
-            CodeSignOptions.LIBRARY_VALIDATION + CodeSignOptions.KILL)
+            CodeSignOptions.HARDENED_RUNTIME | CodeSignOptions.RESTRICT
+            | CodeSignOptions.LIBRARY_VALIDATION | CodeSignOptions.KILL)
         tools[binary] = CodeSignedProduct(
             '{.packaging_dir}/{binary}'.format(config, binary=binary),
             binary.replace('.dylib', ''),
             options=options if not binary.endswith('dylib') else None,
-            verify_options=VerifyOptions.DEEP + VerifyOptions.STRICT)
+            verify_options=VerifyOptions.DEEP | VerifyOptions.STRICT)
 
     return tools
 
