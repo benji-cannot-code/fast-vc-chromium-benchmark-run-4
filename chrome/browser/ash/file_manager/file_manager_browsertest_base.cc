1fa070432b1a04d5cd56017264155e42c683208c
David Munro <davidmunro@google.com>
2022-03-03 09:11:41 +0000

    guest_os: file_manager: Tests control mock guests from JS instead of C++
    The tests are written in Javascript, so it's nice to have all the logic
    in one place (and necessary when we start testing dynamic
    addition/removal) instead of browsertest_base adding things which are
    then verified by the JS.
    So we add register/unregister messages and update the tests to set up
    their own mocks. As a bonus, this might fix the rare flakes in these
    tests where the expected mocks aren't found.
    
    Bug: 1293229
    Test: browser tests, CQ
    Change-Id: I050dea52c330404397f9324905b5c7919b200322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490514
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/main@{#977026}

--- chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_reader.h"
 #include "base/json/json_value_converter.h"
 #include "base/json/json_writer.h"
+#include "base/json/values_util.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
 #include "base/ranges/algorithm.h"
@@ -50,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
+#include "base/value_iterators.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -1931,14 +1933,6 @@ void FileManagerBrowserTestBase::SetUpOnMainThread() {
             base::BindRepeating(&FileManagerBrowserTestBase::MaybeMountCrostini,
                                 base::Unretained(this)));
 
-    if (options.enable_guest_os_files) {
-      // Create some mocks, that show up by default,
-      auto* registry = guest_os::GuestOsService::GetForProfile(profile())
-                           ->MountProviderRegistry();
-      registry->Register(std::make_unique<MockGuestOsMountProvider>("Jemima"));
-      registry->Register(std::make_unique<MockGuestOsMountProvider>("Electra"));
-    }
-
     if (arc::IsArcAvailable()) {
       // When ARC is available, create and register a fake FileSystemInstance
       // so ARC-related services work without a real ARC container.
@@ -3005,7 +2999,38 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
     return;
   }
 
+  if (HandleGuestOsCommands(name, value, output)) {
+    return;
+  }
+
   FAIL() << "Unknown test message: " << name;
+}
+
+bool FileManagerBrowserTestBase::HandleGuestOsCommands(
+    const std::string& name,
+    const base::DictionaryValue& value,
+    std::string* output) {
+  if (name == "registerMountableGuest") {
+    const std::string* displayName = value.GetDict().FindString("displayName");
+    CHECK(displayName != nullptr);
+    auto* registry = guest_os::GuestOsService::GetForProfile(profile())
+                         ->MountProviderRegistry();
+    auto id = registry->Register(
+        std::make_unique<MockGuestOsMountProvider>(*displayName));
+    base::JSONWriter::Write(base::Value(id), output);
+    return true;
+  }
+  if (name == "unregisterMountableGuest") {
+    int id;
+    auto* str = value.GetDict().FindString("guestId");
+    CHECK(str != nullptr);
+    CHECK(base::StringToInt(*str, &id));
+    auto* registry = guest_os::GuestOsService::GetForProfile(profile())
+                         ->MountProviderRegistry();
+    registry->Unregister(id);
+    return true;
+  }
+  return false;
 }
 
 drive::DriveIntegrationService*
