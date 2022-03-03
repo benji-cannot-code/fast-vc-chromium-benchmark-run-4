ef24c6f7c82a055dc9a78bfd3144de1609951763
Mike Dougherty <michaeldo@chromium.org>
2022-03-03 00:48:33 +0000

    [iOS] [Tab Grid] Remove context menu Select Tabs item
    Bug: 1302404
    Change-Id: I8a2492a59653dc4cc0497fd964dbbac757e01adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499142
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976900}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_context_menu_helper.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_context_menu_helper.mm
@@ -151,7 +151,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   }
 
-  if ([self.contextMenuDelegate respondsToSelector:@selector(selectTabs)]) {
+  if (scenario != MenuScenario::kTabGridSearchResult &&
+      [self.contextMenuDelegate respondsToSelector:@selector(selectTabs)]) {
     [menuElements addObject:[actionFactory actionToSelectTabsWithBlock:^{
                     [self.contextMenuDelegate selectTabs];
                   }]];
