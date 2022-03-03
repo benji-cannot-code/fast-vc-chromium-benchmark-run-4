dbc69a01d477c54a2983b809f0af200e5c01195c
Ken MacKay <kmackay@chromium.org>
2022-03-03 20:33:45 +0000

    [Chromecast] Add metrics for stream underruns
    Bug: internal b/220895998
    Change-Id: Ia7412ce3b6f66dd39dfe1eef14b4cfabffdf72f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498745
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977306}

--- chromecast/media/audio/mixer_service/output_stream_connection.cc
+++ chromecast/media/audio/mixer_service/output_stream_connection.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/audio/mixer_service/mixer_service_transport.pb.h"
 #include "chromecast/media/audio/net/common.pb.h"
 #include "chromecast/media/audio/net/conversions.h"
+#include "chromecast/metrics/metrics_recorder.h"
 #include "chromecast/net/io_buffer_pool.h"
 
 namespace chromecast {
@@ -237,6 +238,12 @@ bool OutputStreamConnection::HandleMetadata(const Generic& message) {
   }
 
   if (message.has_mixer_underrun()) {
+    std::string metric_name =
+        (message.mixer_underrun().type() == MixerUnderrun::INPUT_UNDERRUN
+             ? "Platform.Audio.Mixer.StreamUnderrun"
+             : "Platform.Audio.Mixer.OutputUnderrun");
+    RecordCastEvent(metric_name, CreateCastEvent(metric_name),
+                    /* verbose_log_level = */ 0);
     delegate_->OnMixerUnderrun(static_cast<Delegate::MixerUnderrunType>(
         message.mixer_underrun().type()));
   }
