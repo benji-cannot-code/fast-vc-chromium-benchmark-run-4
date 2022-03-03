391a186c404f26979649ae648fae218ce49bb861
Lucy Qu <lqu@google.com>
2022-03-03 00:44:17 +0000

    Change borealis splash screen to look more like UI specs.
    BUG=b:180563821
    
    Change-Id: I94387555ae411ae431b5c8ca40b24b2f00994cea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3473327
    Reviewed-by: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Lucy Qu <lqu@google.com>
    Cr-Commit-Position: refs/heads/main@{#976897}

--- chrome/browser/ash/borealis/borealis_util.h
+++ chrome/browser/ash/borealis/borealis_util.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/strings/string_piece.h"
+#include "chromeos/dbus/dlcservice/dlcservice_client.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/views/widget/widget.h"
 
@@ -80,6 +81,10 @@ bool GetProtonVersionInfo(const std::string& owner_id, std::string* output);
 // Parses the output returned by GetProtonVersionInfo.
 ProtonVersionInfo ParseProtonVersionInfo(absl::optional<int> game_id,
                                          const std::string& output);
+
+// Used in the splash screen to get the dlc path as dlcservice could not be
+// imported directly.
+void GetDlcPath(base::OnceCallback<void(const std::string& path)> callback);
 
 }  // namespace borealis
 
--- chrome/browser/ui/views/borealis/borealis_splash_screen_view.h
+++ chrome/browser/ui/views/borealis/borealis_splash_screen_view.h
@@ -6,14 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_BOREALIS_BOREALIS_SPLASH_SCREEN_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_BOREALIS_BOREALIS_SPLASH_SCREEN_VIEW_H_
 
+#include "chrome/browser/ash/borealis/borealis_util.h"
 #include "chrome/browser/ash/borealis/borealis_window_manager.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
-
 // A splash screen for borealis, displays when borealis is clicked and closed
 // when the first borealis window shows.
 namespace borealis {
 class BorealisSplashScreenView
-    : public views::BubbleDialogDelegateView,
+    : public views::DialogDelegateView,
       public borealis::BorealisWindowManager::AppWindowLifetimeObserver {
  public:
   explicit BorealisSplashScreenView(Profile* profile);
@@ -27,9 +27,13 @@ class BorealisSplashScreenView
       borealis::BorealisWindowManager* window_manager) override;
   // Close this view when borealis window launches
   void OnSessionStarted() override;
+  void OnGetRootPath(const std::string& path);
 
  private:
+  void CreateImageView(gfx::Image image);
+
   Profile* profile_ = nullptr;
+  base::WeakPtrFactory<BorealisSplashScreenView> weak_factory_;
 };
 
 }  // namespace borealis
