2309ca2b0bf4a7a0e890429291266d6b0e6982e6
Wenbo Jie <wenbojie@chromium.org>
2022-03-03 05:32:39 +0000

    [FileTypesData] use "translationKey" instead of the "name"
    A follow-up for CL:3469537, the "name" filed in file_types.json5 is
    confusing, the filed value is used as a translation key to get the
    translation string, hence renaming the field name to "translationKey"
    and update the related type definition and PRESUBMIT validation.
    
    Bug: 1259359
    Change-Id: Iaa6855b079cb0fc8b7fa1ab4f32f7a5ee821a1a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499525
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#976981}

--- ui/file_manager/file_manager/common/js/file_type.js
+++ ui/file_manager/file_manager/common/js/file_type.js
@@ -24,7 +24,7 @@ export {FileExtensionType};
  * @const
  */
 FileType.DIRECTORY = {
-  name: 'FOLDER',
+  translationKey: 'FOLDER',
   type: '.folder',
   icon: 'folder',
   subtype: ''
@@ -36,7 +36,7 @@ FileType.DIRECTORY = {
  * @const
  */
 FileType.PLACEHOLDER = {
-  name: 'NO_EXTENSION_FILE_TYPE',
+  translationKey: 'NO_EXTENSION_FILE_TYPE',
   type: 'UNKNOWN',
   icon: '',
   subtype: ''
@@ -110,7 +110,7 @@ FileType.getTypeForName = name => {
 
   // subtype is the extension excluding the first dot.
   return {
-    name: 'GENERIC_FILE_TYPE',
+    translationKey: 'GENERIC_FILE_TYPE',
     type: 'UNKNOWN',
     subtype: extension.substr(1).toUpperCase(),
     icon: ''
@@ -131,10 +131,10 @@ FileType.getType = (entry, opt_mimeType) => {
     if (/** @type {VolumeEntry}*/ (entry).volumeInfo &&
         /** @type {VolumeEntry}*/ (entry).volumeInfo.diskFileSystemType) {
       return {
-        name: '',
+        translationKey: '',
         type: 'partition',
-        subtype:
-            assert(/** @type {VolumeEntry}*/ (entry).volumeInfo.diskFileSystemType),
+        subtype: assert(
+            /** @type {VolumeEntry}*/ (entry).volumeInfo.diskFileSystemType),
         icon: '',
       };
     }
--- ui/file_manager/file_manager/foreground/js/file_list_model.js
+++ ui/file_manager/file_manager/foreground/js/file_list_model.js
@@ -93,9 +93,9 @@ export class FileListModel extends ArrayDataModel {
       return fileType.subtype;
     }
     if (fileType.subtype) {
-      return strf(fileType.name, fileType.subtype);
+      return strf(fileType.translationKey, fileType.subtype);
     } else {
-      return str(fileType.name);
+      return str(fileType.translationKey);
     }
   }
 
