74edacfd2ea9c58534370593195770b47e0f0236
Christian Xu <christianxu@google.com>
2022-03-03 14:17:02 +0000

    [iOS] Add optional image in Bubble View
    Adds optional image in BubbleView. This option will be used later on for
    Autofill IPH to present different variants of BubbleViews.
    
    Demo:
    https://drive.google.com/file/d/13iNDaleQ6dirFSGl_IvIiwb7Sa3w2FAA/view?usp=sharing
    
    Bug: 1286669
    Change-Id: I56268d2bca202d58db4c84e6388383216b1253c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3481436
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977069}

--- ios/chrome/browser/ui/bubble/bubble_view.h
+++ ios/chrome/browser/ui/bubble/bubble_view.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern NSString* const kBubbleViewCloseButtonIdentifier;
 // Accessibility identifier for the title label.
 extern NSString* const kBubbleViewTitleLabelIdentifier;
+// Accessibility identifier for the image view.
+extern NSString* const kBubbleViewImageViewIdentifier;
 
 // Direction for the bubble to point.
 typedef NS_ENUM(NSInteger, BubbleArrowDirection) {
@@ -67,6 +69,10 @@ typedef NS_ENUM(NSInteger, BubbleAlignment) {
 
 // Text alignment used in this View. Default is NSTextAlignmentCenter.
 @property(nonatomic) NSTextAlignment textAlignment;
+
+// The image displayed alongside the text. Must be set before the view is added
+// to a superview.
+@property(nonatomic, strong) UIImage* image;
 
 // The delegate for interactions in this View.
 @property(nonatomic, weak) id<BubbleViewDelegate> delegate;
