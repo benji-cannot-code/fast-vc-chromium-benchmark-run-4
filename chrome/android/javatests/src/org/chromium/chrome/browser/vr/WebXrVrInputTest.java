3e9dc8841cf9b7e62dd3f31bbd18c7aaa014d50a
Andrey Zaytsev <andzaytsev@google.com>
2022-03-03 15:05:12 +0000

    [Sheriff] Disable flaky WebXrVr tests
    Bug: 1302610
    Change-Id: I897f89a738134692ca1e9874637638bd12f77542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500411
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Owners-Override: Andrey Zaytsev <andzaytsev@google.com>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977087}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrInputTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrVrInputTest.java
@@ -164,6 +164,7 @@ public class WebXrVrInputTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1302610")
     @Restriction(RESTRICTION_TYPE_VIEWER_DAYDREAM_OR_STANDALONE)
     @CommandLineFlags.Add({"enable-features=WebXR"})
     @XrActivityRestriction({XrActivityRestriction.SupportedActivity.ALL})
@@ -280,6 +281,7 @@ public class WebXrVrInputTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1302610")
     @Restriction(RESTRICTION_TYPE_VIEWER_NON_DAYDREAM)
     @CommandLineFlags.Add({"enable-features=WebXR"})
     @XrActivityRestriction({XrActivityRestriction.SupportedActivity.ALL})
@@ -340,6 +342,7 @@ public class WebXrVrInputTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1302610")
     @CommandLineFlags.Add({"enable-features=WebXR"})
     @XrActivityRestriction({XrActivityRestriction.SupportedActivity.ALL})
     public void testPresentationLocksFocus_WebXr() {
@@ -451,6 +454,7 @@ public class WebXrVrInputTest {
      */
     @Test
     @LargeTest
+    @DisabledTest(message = "crbug.com/1302610")
     @Restriction(RESTRICTION_TYPE_VIEWER_DAYDREAM_OR_STANDALONE)
     @CommandLineFlags.Add({"enable-features=WebXR"})
     @XrActivityRestriction({XrActivityRestriction.SupportedActivity.ALL})
