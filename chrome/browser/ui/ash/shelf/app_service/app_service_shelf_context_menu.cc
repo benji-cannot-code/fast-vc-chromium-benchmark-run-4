cf69a5e932da60d4826e0bff1870cb535c99c20c
Nicholas Hollingum <hollingum@google.com>
2022-03-04 07:48:37 +0000

    borealis: Don't show borealis in the launcher and fix shelf integration
    Borealis' placeholder app should not be shown in the launcher, though it
    will still be shown in the shelf for use as the placeholder.
    
    Relatedly: while testing this a DCHECK crash was found, so we also fix
    that in this CL by excluding anonymous borealis apps from being
    pinnable.
    
    Bug: b/220961607, b/220206042
    Change-Id: I670b03f0e551185d80720f56efb514462412774a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3483315
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Daniel Ng <danielng@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#977542}

--- chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/menu_util.h"
 #include "chrome/browser/ash/app_restore/full_restore_service.h"
 #include "chrome/browser/ash/arc/app_shortcuts/arc_app_shortcuts_menu_builder.h"
+#include "chrome/browser/ash/borealis/borealis_window_manager.h"
 #include "chrome/browser/ash/crosapi/browser_manager.h"
 #include "chrome/browser/ash/crostini/crostini_manager.h"
 #include "chrome/browser/ash/crostini/crostini_shelf_utils.h"
@@ -90,10 +91,11 @@ AppServiceShelfContextMenu::AppServiceShelfContextMenu(
     const ash::ShelfItem* item,
     int64_t display_id)
     : ShelfContextMenu(controller, item, display_id) {
-  if (crostini::IsUnmatchedCrostiniShelfAppId(item->id.app_id)) {
-    // For Crostini app_id with the prefix "crostini:", set app_type as Unknown
-    // to skip the ArcAppShelfId valid. App type can't be set as Crostini,
-    // because the pin item should not be added for it.
+  if (crostini::IsUnmatchedCrostiniShelfAppId(item->id.app_id) ||
+      borealis::BorealisWindowManager::IsAnonymousAppId(item->id.app_id)) {
+    // Sometimes GuestOS runs applications that are not registered with the apps
+    // service. These "anonymous" apps should not be pinnable, so we set type
+    // "unknown" to avoid the ARC check below.
     app_type_ = apps::AppType::kUnknown;
     return;
   }
