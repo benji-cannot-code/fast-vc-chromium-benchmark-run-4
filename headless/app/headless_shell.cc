a566de3213fc78e1ea96ce1db6be397105ca306f
Ayu Ishii <ayui@chromium.org>
2022-03-04 19:06:35 +0000

    CodeHealth: Use base::Reversed() in headless
    Bug: 1287358
    Change-Id: Ie77b1c21f20dc8c9c66b54c8bed7e7f82b38b548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498829
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977750}

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
+#include "base/containers/adapters.h"
 #include "base/environment.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -127,8 +128,8 @@ std::vector<GURL> ConvertArgumentsToURLs(
     const base::CommandLine::StringVector& args) {
   std::vector<GURL> urls;
   urls.reserve(args.size());
-  for (auto it = args.rbegin(); it != args.rend(); ++it)
-    urls.push_back(ConvertArgumentToURL(*it));
+  for (const auto& arg : base::Reversed(args))
+    urls.push_back(ConvertArgumentToURL(arg));
   return urls;
 }
 
