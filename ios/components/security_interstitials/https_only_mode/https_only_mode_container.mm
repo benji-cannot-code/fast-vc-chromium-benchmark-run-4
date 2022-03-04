173fe7c6e0fff159b820c7c0008b369cec5ca146
Mustafa Emre Acer <meacer@chromium.org>
2022-03-04 18:37:27 +0000

    HTTPS-Only Mode: Add the iOS interstitial UI
    This CL adds the blocking page for the HTTPS-Only mode on iOS. It
    follows the same layout as the lookalike interstitial code on iOS.
    Future CLs will add tests when this code is connected to the rest
    of the feature.
    
    Bug: 1302509
    Change-Id: I0ba00829509e101de42da0673119b77d6f2b1203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3501574
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977739}

---
+++ ios/components/security_interstitials/https_only_mode/https_only_mode_container.mm
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/components/security_interstitials/https_only_mode/https_only_mode_container.h"
+
+#include "base/memory/ptr_util.h"
+#import "ios/web/public/web_state.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+WEB_STATE_USER_DATA_KEY_IMPL(HttpsOnlyModeContainer)
+
+HttpsOnlyModeContainer::HttpsOnlyModeContainer(web::WebState* web_state) {}
+
+HttpsOnlyModeContainer::HttpsOnlyModeContainer(HttpsOnlyModeContainer&& other) =
+    default;
+
+HttpsOnlyModeContainer& HttpsOnlyModeContainer::operator=(
+    HttpsOnlyModeContainer&& other) = default;
+
+HttpsOnlyModeContainer::~HttpsOnlyModeContainer() = default;
+
+HttpsOnlyModeContainer::InterstitialParams::InterstitialParams() = default;
+
+HttpsOnlyModeContainer::InterstitialParams::~InterstitialParams() = default;
+
+HttpsOnlyModeContainer::InterstitialParams::InterstitialParams(
+    const InterstitialParams& other) = default;
+
+void HttpsOnlyModeContainer::RecordBlockingPageParams(
+    const GURL& url,
+    const web::Referrer& referrer,
+    const std::vector<GURL>& redirect_chain) {
+  interstitial_params_->url = url;
+  interstitial_params_->referrer = referrer;
+  interstitial_params_->redirect_chain = redirect_chain;
+}
+
+void HttpsOnlyModeContainer::SetHttpUrl(const GURL& http_url) {
+  http_url_ = url;
+}
+
+GURL HttpsOnlyModeContainer::http_url() const {
+  return http_url_;
+}
+
+std::unique_ptr<HttpsOnlyModeContainer::InterstitialParams>
+HttpsOnlyModeContainer::ReleaseInterstitialParams() {
+  return std::move(interstitial_params_);
+}
---
+++ ios/components/security_interstitials/https_only_mode/https_only_mode_error.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_COMPONENTS_SECURITY_INTERSTITIALS_HTTPS_ONLY_MODE_HTTPS_ONLY_MODE_ERROR_H_
+#define IOS_COMPONENTS_SECURITY_INTERSTITIALS_HTTPS_ONLY_MODE_HTTPS_ONLY_MODE_ERROR_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/web/public/navigation/web_state_policy_decider.h"
+
+// The error domain for HTTPS-Only mode interstitial.
+extern const NSErrorDomain kHttpsOnlyModeErrorDomain;
+
+// Error code for HTTPS-Only mode interstitial.
+extern const NSInteger kHttpsOnlyModeErrorCode;
+
+// Creates a PolicyDecision that cancels a navigation to show an HTTPS-Only mode
+// error.
+web::WebStatePolicyDecider::PolicyDecision CreateHttpsOnlyModeErrorDecision();
+
+#endif  // IOS_COMPONENTS_SECURITY_INTERSTITIALS_HTTPS_ONLY_MODE_HTTPS_ONLY_MODE_ERROR_H_
