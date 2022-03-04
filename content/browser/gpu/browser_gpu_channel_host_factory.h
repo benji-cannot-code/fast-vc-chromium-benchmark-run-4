c229ec69c0af6d133f2f757d953277d54819be95
Keishi Hattori <keishi@chromium.org>
2022-03-04 06:22:27 +0000

    Revert "Assert by default in browser tests that NetworkService doesn't crash."
    This reverts commit 6543364eb5e359210b88cb73afadaf63fa0af4b8.
    
    Reason for revert: Caused ScrollBubblingFromNestedOOPIFTest to fail.
    crbug.com/1302897
    
    Original change's description:
    > Assert by default in browser tests that NetworkService doesn't crash.
    >
    > This CL prevents browser tests from succeeding if NetworkService
    > crashes during a test.
    >
    > This CL also fixes callers that intentionally crash network service
    > to set correct expectations. It also fixes an issue in the BFCache
    > test where a run_loop might have already quit as the result of
    > a network service callback that is executed when the network service
    > flushes during test shutdown.
    >
    > This CL was rebased from 2644067.
    >
    > Bug: 1169431
    > Change-Id: Ia7ab6345cbac2ab83bfcca9230dfe059f680b90b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3441285
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#977426}
    
    Bug: 1169431
    Change-Id: I8281052331dee699771c27ddd511997a96d0664b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499909
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977520}

--- content/browser/gpu/browser_gpu_channel_host_factory.h
+++ content/browser/gpu/browser_gpu_channel_host_factory.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
-#include "content/common/content_export.h"
 #include "gpu/ipc/client/gpu_channel_host.h"
 #include "ipc/message_filter.h"
 
@@ -31,9 +30,7 @@ class BrowserGpuChannelHostFactory : public gpu::GpuChannelEstablishFactory {
  public:
   static void Initialize(bool establish_gpu_channel);
   static void Terminate();
-  CONTENT_EXPORT static BrowserGpuChannelHostFactory* instance() {
-    return instance_;
-  }
+  static BrowserGpuChannelHostFactory* instance() { return instance_; }
 
   BrowserGpuChannelHostFactory(const BrowserGpuChannelHostFactory&) = delete;
   BrowserGpuChannelHostFactory& operator=(const BrowserGpuChannelHostFactory&) =
@@ -49,7 +46,7 @@ class BrowserGpuChannelHostFactory : public gpu::GpuChannelEstablishFactory {
 
   // Closes the channel to the GPU process. This should be called before the IO
   // thread stops.
-  CONTENT_EXPORT void CloseChannel();
+  void CloseChannel();
 
   // Notify the BrowserGpuChannelHostFactory of visibility, used to prevent
   // timeouts while backgrounded.
@@ -88,9 +85,7 @@ class BrowserGpuChannelHostFactory : public gpu::GpuChannelEstablishFactory {
 
   base::OneShotTimer timeout_;
 
-  // instance() might be inlined at a call site so instance_ must also be
-  // exported.
-  CONTENT_EXPORT static BrowserGpuChannelHostFactory* instance_;
+  static BrowserGpuChannelHostFactory* instance_;
 };
 
 }  // namespace content
