068d02f26234a808c0fcc6cfa5c4b6e4c37b291f
Christian Xu <christianxu@google.com>
2022-03-04 16:41:47 +0000

    [iOS] Add snooze button in BubbleView
    Adds optional snooze button in BubbleView.
    This button dismisses the IPH, it will be shown again later on.
    
    Demo day:
    https://drive.google.com/file/d/13s90Ih6jwnbFohIXZUQtS7vRIaKji-Yw/view?usp=sharing
    
    Demo dark:
    https://drive.google.com/file/d/13q2pJIz0i1O7g1VSIKgxGxPggBHeAb9s/view?usp=sharing
    
    Bug: 1286669
    Change-Id: I23f649efefd210247d9d52d427dd84c3034ace6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497421
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977685}

--- ios/chrome/browser/ui/bubble/bubble_view_unittest.mm
+++ ios/chrome/browser/ui/bubble/bubble_view_unittest.mm
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _tapCounter += 1;
 }
 
+- (void)didTapSnoozeButton {
+  _tapCounter += 1;
+}
+
 @end
 
 // Fixture to test BubbleView.
@@ -89,6 +93,11 @@ class BubbleViewTest : public PlatformTest {
   UIImageView* GetImageView(BubbleView* bubbleView) {
     return base::mac::ObjCCastStrict<UIImageView>(GetViewOfClassWithIdentifier(
         [UIImageView class], kBubbleViewImageViewIdentifier, bubbleView));
+  }
+
+  UIButton* GetSnoozeButton(BubbleView* bubbleView) {
+    return base::mac::ObjCCastStrict<UIButton>(GetViewOfClassWithIdentifier(
+        [UIButton class], kBubbleViewSnoozeButtonIdentifier, bubbleView));
   }
 };
 
@@ -206,4 +215,33 @@ TEST_F(BubbleViewTest, ImageIsPresentAndCorrect) {
   UIImageView* imageView = GetImageView(bubble);
   ASSERT_TRUE(imageView);
   EXPECT_EQ(imageView.image, testImage);
+}
+
+// Tests that the snooze button is not showed when the option is set to hidden.
+TEST_F(BubbleViewTest, SnoozeButtonIsNotPresent) {
+  BubbleView* bubble = [[BubbleView alloc] initWithText:longText_
+                                         arrowDirection:arrowDirection_
+                                              alignment:alignment_];
+  [bubble setShowsSnoozeButton:NO];
+  UIView* superview = [[UIView alloc] initWithFrame:CGRectZero];
+  [superview addSubview:bubble];
+  UIButton* snoozeButton = GetSnoozeButton(bubble);
+  ASSERT_FALSE(snoozeButton);
+}
+
+// Tests the snooze button action and its presence.
+TEST_F(BubbleViewTest, SnoozeButtonActionAndPresent) {
+  BubbleView* bubble = [[BubbleView alloc] initWithText:longText_
+                                         arrowDirection:arrowDirection_
+                                              alignment:alignment_];
+  BubbleViewDelegateTest* delegate = [[BubbleViewDelegateTest alloc] init];
+  [bubble setShowsSnoozeButton:YES];
+  [bubble setDelegate:delegate];
+  UIView* superview = [[UIView alloc] initWithFrame:CGRectZero];
+  [superview addSubview:bubble];
+  UIButton* snoozeButton = GetSnoozeButton(bubble);
+  ASSERT_TRUE(snoozeButton);
+  // Tests snooze button action.
+  [snoozeButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+  EXPECT_EQ(delegate.tapCounter, 1);
 }
