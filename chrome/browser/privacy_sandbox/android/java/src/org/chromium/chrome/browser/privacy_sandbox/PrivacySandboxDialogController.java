e5b584993eaed0156de979d614e8a606bd0e8eb9
Andrey Zaytsev <andzaytsev@google.com>
2022-03-04 12:52:20 +0000

    Privacy Sandbox Settings: Deflake and reenable dialog tests
    Bug: 1286276, 1300632
    Change-Id: I3fd32c86743129493f80c67535b78aced9d0a542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497781
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977622}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogController.java
@@ -5,16 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.privacy_sandbox;
 
+import android.app.Dialog;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.components.browser_ui.settings.SettingsLauncher;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Controller for the dialog shown for the Privacy Sandbox.
  */
 public class PrivacySandboxDialogController {
+    private static WeakReference<Dialog> sDialog;
+
     /**
      * Launches an appropriate dialog if necessary and returns whether that happened.
      */
@@ -25,19 +31,29 @@ public class PrivacySandboxDialogController {
         }
         @DialogType
         int dialogType = PrivacySandboxBridge.getRequiredDialogType();
+        Dialog dialog = null;
         switch (dialogType) {
             case DialogType.NONE:
                 return false;
             case DialogType.NOTICE:
-                new PrivacySandboxDialogNotice(context, settingsLauncher).show();
+                dialog = new PrivacySandboxDialogNotice(context, settingsLauncher);
+                dialog.show();
+                sDialog = new WeakReference<>(dialog);
                 return true;
             case DialogType.CONSENT:
-                new PrivacySandboxDialogConsent(context).show();
+                dialog = new PrivacySandboxDialogConsent(context);
+                dialog.show();
+                sDialog = new WeakReference<>(dialog);
                 return true;
             default:
                 assert false : "Unknown DialogType value.";
                 // Should not be reached.
                 return false;
         }
+    }
+
+    @VisibleForTesting
+    static Dialog getDialogForTesting() {
+        return sDialog != null ? sDialog.get() : null;
     }
 }
