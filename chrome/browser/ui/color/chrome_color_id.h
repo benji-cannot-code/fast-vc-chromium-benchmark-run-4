66880f16afe2c56b6947ba9e370d9ab95a7646d5
Tom Lukaszewicz <tluk@chromium.org>
2022-03-04 19:30:32 +0000

    [color pipeline] Add toolbar and tab separator colors to chrome mixers
    Bug: 1296206
    Change-Id: I9b18682d41995660505a3ab456a297355e5df117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498565
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977761}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -231,6 +231,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ThemeProperties::COLOR_TAB_GROUP_BOOKMARK_BAR_CYAN) \
   E(kColorTabGroupBookmarkBarOrange, \
     ThemeProperties::COLOR_TAB_GROUP_BOOKMARK_BAR_ORANGE) \
+  E(kColorTabStrokeFrameActive, \
+    ThemeProperties::COLOR_TAB_STROKE_FRAME_ACTIVE) \
+  E(kColorTabStrokeFrameInactive, \
+    ThemeProperties::COLOR_TAB_STROKE_FRAME_INACTIVE) \
   /* Thumbnail tab colors. */ \
   E_CPONLY(kColorThumbnailTabBackground) \
   E_CPONLY(kColorThumbnailTabForeground) \
@@ -255,6 +259,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kColorToolbarSeparator, \
     ThemeProperties::COLOR_TOOLBAR_VERTICAL_SEPARATOR) \
   E(kColorToolbarText, ThemeProperties::COLOR_TOOLBAR_TEXT) \
+  E(kColorToolbarTopSeparatorFrameActive, \
+    ThemeProperties::COLOR_TOOLBAR_TOP_SEPARATOR_FRAME_ACTIVE) \
+  E(kColorToolbarTopSeparatorFrameInactive, \
+    ThemeProperties::COLOR_TOOLBAR_TOP_SEPARATOR_FRAME_INACTIVE) \
   /* Window control button background colors */ \
   E(kColorWindowControlButtonBackgroundActive, \
     ThemeProperties::COLOR_WINDOW_CONTROL_BUTTON_BACKGROUND_ACTIVE) \
