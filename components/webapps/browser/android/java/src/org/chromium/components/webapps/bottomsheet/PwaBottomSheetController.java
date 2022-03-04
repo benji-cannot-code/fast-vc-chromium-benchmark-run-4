f573f18a8a363ef3a2580724b25e70dfd2d699b3
Susanne Westphal <swestphal@chromium.org>
2022-03-04 10:02:01 +0000

    Add PwaBottomSheetController to WebLayer
    Because of different classloaders in the weblayerShellActivity
    and weblayer support library the Context instead of the Activity
    needs to be passed to the PwaBottomSheetController.
    
    Bug: 1101999
    Change-Id: Iadf7831c2fb5a7154edb739f9554577cfed7f8d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497806
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Susanne Westphal <swestphal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977583}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/bottomsheet/PwaBottomSheetController.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/bottomsheet/PwaBottomSheetController.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.webapps.bottomsheet;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Pair;
@@ -47,7 +46,7 @@ import java.util.ArrayList;
 @JNINamespace("webapps")
 public class PwaBottomSheetController
         implements UnownedUserData, AddToHomescreenViewDelegate, View.OnClickListener {
-    private final Activity mActivity;
+    private final Context mContext;
 
     /** A pointer to the native version of this class. It's lifetime is controlled by this class. */
     private long mNativePwaBottomSheetController;
@@ -159,10 +158,10 @@ public class PwaBottomSheetController
 
     /**
      * Constructs a PwaBottomSheetController.
-     * @param activity The current activity.
+     * @param context The current context.
      */
-    public PwaBottomSheetController(Activity activity) {
-        mActivity = activity;
+    public PwaBottomSheetController(Context context) {
+        mContext = context;
     }
 
     // AddToHomescreenViewDelegate:
@@ -220,9 +219,9 @@ public class PwaBottomSheetController
             return;
         }
 
-        mScreenshotAdapter = new ScreenshotsAdapter(mActivity);
+        mScreenshotAdapter = new ScreenshotsAdapter(mContext);
         PwaInstallBottomSheetView view =
-                new PwaInstallBottomSheetView(mActivity, mScreenshotAdapter);
+                new PwaInstallBottomSheetView(mContext, mScreenshotAdapter);
         mPwaBottomSheetContent = new PwaInstallBottomSheetContent(view, this);
         mModel = new PropertyModel.Builder(AddToHomescreenProperties.ALL_KEYS)
                          .with(AddToHomescreenProperties.ICON, new Pair<>(icon, isAdaptiveIcon))
--- components/webapps/browser/android/java/src/org/chromium/components/webapps/bottomsheet/PwaBottomSheetControllerFactory.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/bottomsheet/PwaBottomSheetControllerFactory.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.webapps.bottomsheet;
 
-import android.app.Activity;
+import android.content.Context;
 
 import org.chromium.ui.base.WindowAndroid;
 
@@ -13,8 +13,8 @@ import org.chromium.ui.base.WindowAndroid;
  * A factory for producing a {@link PwaBottomSheetController}.
  */
 public class PwaBottomSheetControllerFactory {
-    public static PwaBottomSheetController createPwaBottomSheetController(Activity activity) {
-        return new PwaBottomSheetController(activity);
+    public static PwaBottomSheetController createPwaBottomSheetController(Context context) {
+        return new PwaBottomSheetController(context);
     }
 
     public static void attach(WindowAndroid windowAndroid, PwaBottomSheetController controller) {
