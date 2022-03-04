4ebbab5ed708ca764090b34813665d98539c6623
Elaine Chien <elainechien@chromium.org>
2022-03-04 23:13:59 +0000

    [Color Pipeline]Add ToolbarTopSeparatorFrameActive to mac high contrast colors
    Bug: 1296206
    Change-Id: I773d9622e17c089ea49f005b124f177814a5c716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502754
    Commit-Queue: Elaine Chien <elainechien@chromium.org>
    Auto-Submit: Elaine Chien <elainechien@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977854}

--- chrome/browser/ui/color/mac/native_chrome_color_mixer_mac.cc
+++ chrome/browser/ui/color/mac/native_chrome_color_mixer_mac.cc
@@ -32,4 +32,6 @@ void AddNativeChromeColorMixer(ui::ColorProvider* provider,
   mixer[kColorToolbar] = {dark_mode ? SK_ColorBLACK : SK_ColorWHITE};
   mixer[kColorToolbarContentAreaSeparator] = {foreground};
   mixer[kColorToolbarText] = {foreground};
+  mixer[kColorToolbarTopSeparatorFrameActive] = {dark_mode ? SK_ColorDKGRAY
+                                                           : SK_ColorLTGRAY};
 }
