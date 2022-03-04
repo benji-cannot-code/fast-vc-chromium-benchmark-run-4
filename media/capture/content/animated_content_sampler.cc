88dca9772cbcfdd3b5d2f06ab9aaf25baf0ddc97
Ayu Ishii <ayui@chromium.org>
2022-03-04 17:59:49 +0000

    CodeHealth: Use base::Reversed() in media
    Bug: 1287358
    Change-Id: Ia8df2c0fcd0ad130787837960774b2c4d013159d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502603
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977720}

--- media/capture/content/animated_content_sampler.cc
+++ media/capture/content/animated_content_sampler.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/containers/adapters.h"
+
 namespace media {
 
 namespace {
@@ -203,27 +205,27 @@ bool AnimatedContentSampler::AnalyzeObservations(
   size_t count_frame_durations = 0;
   base::TimeTicks first_event_time;
   base::TimeTicks last_event_time;
-  for (ObservationFifo::const_reverse_iterator i = observations_.rbegin();
-       i != observations_.rend(); ++i) {
-    const int area = i->damage_rect.size().GetArea();
+  for (const auto& observation : base::Reversed(observations_)) {
+    const int area = observation.damage_rect.size().GetArea();
     num_pixels_damaged_in_all += area;
-    if (i->damage_rect != elected_rect)
+    if (observation.damage_rect != elected_rect)
       continue;
     num_pixels_damaged_in_chosen += area;
     if (last_event_time.is_null()) {
-      last_event_time = i->event_time;
+      last_event_time = observation.event_time;
       if ((event_time - last_event_time) >= kNonAnimatingThreshold) {
         return false;  // Content animation has recently ended.
       }
     } else {
-      const base::TimeDelta frame_duration = first_event_time - i->event_time;
+      const base::TimeDelta frame_duration =
+          first_event_time - observation.event_time;
       if (frame_duration >= kNonAnimatingThreshold) {
         break;  // Content not animating before this point.
       }
       sum_frame_durations += frame_duration;
       ++count_frame_durations;
     }
-    first_event_time = i->event_time;
+    first_event_time = observation.event_time;
   }
 
   if ((last_event_time - first_event_time) < kMinObservationWindow) {
