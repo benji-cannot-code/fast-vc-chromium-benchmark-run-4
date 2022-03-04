e50f3839bd1d5d7bb60f43a9afee4743db936dd9
Eriko Kurimoto <elkurin@chromium.org>
2022-03-04 07:23:11 +0000

    [Resource file sharing] Add resourse path literal required for sharing
    This CL adds FILE_FALLBACK_RESOURCES_PACK, FILE_MAPPING_TABLE_PACK and
    FILE_ASH_RESOURCES_PACK to chrome paths.
    These paths are for Lacros to share the resources with Ash.
    
    Right now, these files don't exist nor are used yet, but will be
    required for Resource file sharing work.
    
    Bug: 1253280
    Change-Id: Ida6ac9443f0f49d398a8bd177dc7c027ab00e0e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499245
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977533}

--- chrome/common/chrome_paths.cc
+++ chrome/common/chrome_paths.cc
@@ -43,6 +43,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/widevine/cdm/widevine_cdm_common.h"  // nogncheck
 #endif
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/common/chrome_paths_lacros.h"  // nogncheck
+#endif
+
 namespace {
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
@@ -362,6 +366,24 @@ bool PathProvider(int key, base::FilePath* result) {
       cur = cur.Append(FILE_PATH_LITERAL("resources.pak"));
 #endif
       break;
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+    case chrome::FILE_FALLBACK_RESOURCES_PACK:
+      if (!base::PathService::Get(base::DIR_ASSETS, &cur))
+        return false;
+      cur = cur.Append(FILE_PATH_LITERAL("resources_fallback.pak"));
+      break;
+    case chrome::FILE_ASH_RESOURCES_PACK:
+      if (!chrome::GetAshResourcesPath(&cur))
+        return false;
+      cur = cur.Append("resources.pak");
+      break;
+    case chrome::FILE_RESOURCES_MAP:
+      if (!base::PathService::Get(base::DIR_ASSETS, &cur))
+        return false;
+      cur = cur.Append(FILE_PATH_LITERAL("resources.map"));
+      break;
+#endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     case chrome::DIR_CHROMEOS_WALLPAPERS:
