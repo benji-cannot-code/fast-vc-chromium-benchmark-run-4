28fe97fbdc43729b33926a44cd5530e56c51a56c
rajendrant <rajendrant@chromium.org>
2022-03-04 20:16:42 +0000

    Enable high priority download by default
    This is experimented with language detection. So enabling it by default.
    
    Bug: 1303017
    Change-Id: I48333dccfd228099973c7eb5bbd053197ee911ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499696
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977797}

--- components/optimization_guide/core/optimization_guide_features.cc
+++ components/optimization_guide/core/optimization_guide_features.cc
@@ -420,7 +420,7 @@ bool IsModelDownloadingEnabled() {
 bool IsUnrestrictedModelDownloadingEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kOptimizationGuideModelDownloading, "unrestricted_model_downloading",
-      false);
+      true);
 }
 
 bool IsPageContentAnnotationEnabled() {
