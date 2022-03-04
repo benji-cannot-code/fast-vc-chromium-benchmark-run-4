0e6fa7588333d995cd8787de36502f6c65599dc5
Christian Biesinger <cbiesinger@chromium.org>
2022-03-04 19:26:13 +0000

    Reland "[FedCM] Split get() into get()+login()"
    This reverts commit 2b540bc952136e552b98559fe0ddb4e149b39941.
    
    Reason for revert: Fixed compile error
    
    Original change's description:
    > Revert "[FedCM] Split get() into get()+login()"
    >
    > This reverts commit a6fba024e6fdead47455a413362261326e0ab7a6.
    >
    > Reason for revert: Might have caused compile failure
    > https://ci.chromium.org/p/chromium/builders/ci/linux-archive-rel/62048?
    >
    > Original change's description:
    > > [FedCM] Split get() into get()+login()
    > >
    > > This is a consequence of
    > > https://github.com/fedidcg/FedCM/commit/e49c2bb2fd949ef048f4f5e25cff26c3c7f72d91
    > >
    > > Bug: 1300009
    > > Change-Id: I36839e0e5748d1014e624d64f40d393fec4d2b91
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499423
    > > Reviewed-by: Yi Gu <yigu@chromium.org>
    > > Reviewed-by: Sam Goto <goto@chromium.org>
    > > Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#977460}
    >
    > Bug: 1300009
    > Change-Id: I6a1e3250f82f4111eac6c841127d044d197af97f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499935
    > Auto-Submit: Keishi Hattori <keishi@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Takashi Sakamoto <tasak@google.com>
    > Commit-Queue: Keishi Hattori <keishi@chromium.org>
    > Owners-Override: Keishi Hattori <keishi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#977469}
    
    Bug: 1300009
    Change-Id: Ibac6509a4a0657b8d2b8c6ff0c32dd1e25fcaf9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498694
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977759}

--- third_party/blink/renderer/modules/credentialmanager/credential_manager_proxy.cc
+++ third_party/blink/renderer/modules/credentialmanager/credential_manager_proxy.cc
@@ -18,7 +18,7 @@ CredentialManagerProxy::CredentialManagerProxy(LocalDOMWindow& window)
       credential_manager_(window.GetExecutionContext()),
       webotp_service_(window.GetExecutionContext()),
       payment_credential_(window.GetExecutionContext()),
-      fedcm_get_request_(window.GetExecutionContext()),
+      federated_auth_request_(window.GetExecutionContext()),
       fedcm_logout_request_(window.GetExecutionContext()) {}
 
 CredentialManagerProxy::~CredentialManagerProxy() = default;
@@ -84,12 +84,13 @@ void CredentialManagerProxy::BindRemoteForFedCm(
   remote.set_disconnect_handler(std::move(disconnect_closure));
 }
 
-mojom::blink::FederatedAuthRequest* CredentialManagerProxy::FedCmGetRequest() {
+mojom::blink::FederatedAuthRequest*
+CredentialManagerProxy::FederatedAuthRequest() {
   BindRemoteForFedCm(
-      fedcm_get_request_,
-      WTF::Bind(&CredentialManagerProxy::OnFedCmGetConnectionError,
+      federated_auth_request_,
+      WTF::Bind(&CredentialManagerProxy::OnFederatedAuthRequestConnectionError,
                 WrapWeakPersistent(this)));
-  return fedcm_get_request_.get();
+  return federated_auth_request_.get();
 }
 
 mojom::blink::FederatedAuthRequest*
@@ -101,8 +102,8 @@ CredentialManagerProxy::FedCmLogoutRpsRequest() {
   return fedcm_logout_request_.get();
 }
 
-void CredentialManagerProxy::OnFedCmGetConnectionError() {
-  fedcm_get_request_.reset();
+void CredentialManagerProxy::OnFederatedAuthRequestConnectionError() {
+  federated_auth_request_.reset();
   // TODO(crbug.com/1275769): Cache the resolver and resolve the promise with an
   // appropriate error message.
 }
@@ -132,7 +133,7 @@ void CredentialManagerProxy::Trace(Visitor* visitor) const {
   visitor->Trace(credential_manager_);
   visitor->Trace(webotp_service_);
   visitor->Trace(payment_credential_);
-  visitor->Trace(fedcm_get_request_);
+  visitor->Trace(federated_auth_request_);
   visitor->Trace(fedcm_logout_request_);
   Supplement<LocalDOMWindow>::Trace(visitor);
 }
--- third_party/blink/renderer/modules/credentialmanager/federated_credential.h
+++ third_party/blink/renderer/modules/credentialmanager/federated_credential.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class CredentialRequestOptions;
+class FederatedAccountLoginRequest;
 class FederatedCredentialInit;
 class FederatedIdentityProvider;
 
@@ -31,13 +33,23 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
       const String& name,
       const KURL& icon_url);
 
+  static FederatedCredential* Create(const KURL& provider_url,
+                                     const String& client_id,
+                                     const CredentialRequestOptions* options);
+
   FederatedCredential(const String& id,
                       scoped_refptr<const SecurityOrigin> provider,
                       const String& name,
                       const KURL& icon_url);
 
+  FederatedCredential(const KURL& provider_url,
+                      const String& client_id,
+                      const CredentialRequestOptions* options);
+
+  void Trace(Visitor*) const override;
+
   scoped_refptr<const SecurityOrigin> GetProviderAsOrigin() const {
-    return provider_;
+    return provider_origin_;
   }
 
   // Credential:
@@ -45,8 +57,8 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
 
   // FederatedCredential.idl
   String provider() const {
-    CHECK(provider_);
-    return provider_->ToString();
+    CHECK(provider_origin_);
+    return provider_origin_->ToString();
   }
   const String& name() const { return name_; }
   const KURL& iconURL() const { return icon_url_; }
@@ -56,17 +68,8 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
     return g_empty_string;
   }
 
-  const String& idToken() const {
-    // TODO(goto): This is a stub, so that we can port the WebID API
-    // gradually.
-    return g_empty_string;
-  }
-
-  const String& approvedBy() const {
-    // TODO(goto): This is a stub, so that we can port the WebID API
-    // gradually.
-    return g_empty_string;
-  }
+  ScriptPromise login(ScriptState* script_state,
+                      FederatedAccountLoginRequest* request);
 
   ScriptPromise logout();
 
@@ -79,9 +82,12 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
                               ExceptionState&);
 
  private:
-  const scoped_refptr<const SecurityOrigin> provider_;
+  const scoped_refptr<const SecurityOrigin> provider_origin_;
   const String name_;
   const KURL icon_url_;
+  const KURL provider_url_;
+  const String client_id_;
+  Member<const CredentialRequestOptions> options_;
 };
 
 }  // namespace blink
