9ccf4d65f5477ca64682be5f79650a3ae186fa1f
Theodore Olsauskas-Warren <sauski@google.com>
2022-03-04 15:11:49 +0000

    Privacy Sandbox Settings: Integrate restriction check into dialog logic
    CL integrates the IsPrivacySandboxRestricted signal into the logic for
    deciding whether a dialog should be show. If the sandbox is restricted,
    no dialog will be shown, and this is recorded so no future dialog is
    never shown, even if the sandbox becomes unrestricted.
    
    Additionally, logic to disable the V2 privacy sandbox pref if the
    sandbox is restricted is included. This is performed on service
    startup, which occurs on every profile startup.
    
    Bug: 1286276
    Change-Id: I565c7373b1b7a0ec297110cd6cb49609a8b35fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499997
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#977655}

--- components/privacy_sandbox/privacy_sandbox_prefs.cc
+++ components/privacy_sandbox/privacy_sandbox_prefs.cc
@@ -45,6 +45,9 @@ extern const char kPrivacySandboxConsentDecisionMade[] =
 extern const char kPrivacySandboxNoConfirmationSandboxDisabled[] =
     "privacy_sandbox.no_confirmation_sandbox_disabled";
 
+extern const char kPrivacySandboxNoConfirmationSandboxRestricted[] =
+    "privacy_sandbox.no_confirmation_sandbox_restricted";
+
 extern const char kPrivacySandboxDisabledInsufficientConfirmation[] =
     "privacy_sandbox.disabled_insufficient_confirmation";
 
@@ -77,6 +80,8 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
                                 false);
   registry->RegisterBooleanPref(
       prefs::kPrivacySandboxNoConfirmationSandboxDisabled, false);
+  registry->RegisterBooleanPref(
+      prefs::kPrivacySandboxNoConfirmationSandboxRestricted, false);
   registry->RegisterBooleanPref(
       prefs::kPrivacySandboxDisabledInsufficientConfirmation, false);
 }
