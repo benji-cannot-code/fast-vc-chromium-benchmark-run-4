00535e41320114ba46eb07eedd339100a5645346
Xinghui Lu <xinghuilu@chromium.org>
2022-03-04 20:12:29 +0000

    Support inactive state in download toolbar icon.
    Change the color to inactive if the download was completed for more
    than 1 minute or the button is pressed.
    
    Bug: 1282240
    Change-Id: Ide934dc6ccbeba962329af9e2d8b67f659a1e96f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486164
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977795}

--- chrome/browser/download/bubble/download_display.h
+++ chrome/browser/download/bubble/download_display.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DOWNLOAD_BUBBLE_DOWNLOAD_DISPLAY_H_
 #define CHROME_BROWSER_DOWNLOAD_BUBBLE_DOWNLOAD_DISPLAY_H_
 
-#include "chrome/browser/download/bubble/download_icon_state.h"
-
 class DownloadDisplay {
  public:
   // Shows the download display.
@@ -20,8 +18,8 @@ class DownloadDisplay {
   virtual void Enable() = 0;
   // Disables potential actions resulting from clicking the download display.
   virtual void Disable() = 0;
-  // Updates the download icon according to |state|.
-  virtual void UpdateDownloadIcon(download::DownloadIconState state) = 0;
+  // Updates the download icon.
+  virtual void UpdateDownloadIcon() = 0;
   // Shows detailed information on the download display. It can be a popup or
   // dialog other than the main view.
   virtual void ShowDetails() = 0;
--- chrome/browser/ui/views/download/bubble/download_toolbar_button_view.h
+++ chrome/browser/ui/views/download/bubble/download_toolbar_button_view.h
@@ -36,7 +36,7 @@ class DownloadToolbarButtonView : public ToolbarButton, public DownloadDisplay {
   bool IsShowing() override;
   void Enable() override;
   void Disable() override;
-  void UpdateDownloadIcon(download::DownloadIconState state) override;
+  void UpdateDownloadIcon() override;
   void ShowDetails() override;
 
   // ToolbarButton:
@@ -55,7 +55,6 @@ class DownloadToolbarButtonView : public ToolbarButton, public DownloadDisplay {
   std::unique_ptr<DownloadDisplayController> controller_;
   // Controller for the DownloadBubbleUI, both main view and partial view.
   std::unique_ptr<DownloadBubbleUIController> bubble_controller_;
-  download::DownloadIconState icon_state_;
   raw_ptr<views::BubbleDialogDelegate> bubble_delegate_ = nullptr;
 };
 
