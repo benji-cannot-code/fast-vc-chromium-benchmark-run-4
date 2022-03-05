bfed9fd4d14b5cf00a29a18a887670015295dcbf
Benjamin Beaudry <bebeaudr@microsoft.com>
2022-03-05 00:36:10 +0000

    [Focusgroup] Implement forward navigation
    In this CL, we implement the algorithm that takes care of moving the
    focus within a focusgroup when an arrow down or an arrow-right is
    pressed.
    
    The implementation for backward navigation (arrow up and arrow left)
    will follow soon.
    
    Bug: 1286127
    Change-Id: I98bd8e1e351f2ac7bb286b430fc12fdeab126367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3482239
    Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#977893}

--- third_party/blink/renderer/core/page/focusgroup_controller_utils.h
+++ third_party/blink/renderer/core/page/focusgroup_controller_utils.h
@@ -11,7 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class Element;
 class KeyboardEvent;
+
+enum FocusgroupFlags : int8_t;
 
 enum class FocusgroupDirection {
   kNone,
@@ -26,6 +29,18 @@ class CORE_EXPORT FocusgroupControllerUtils {
 
  public:
   static FocusgroupDirection FocusgroupDirectionForEvent(KeyboardEvent* event);
+  static bool IsDirectionBackward(FocusgroupDirection direction);
+  static bool IsDirectionForward(FocusgroupDirection direction);
+  static bool IsDirectionHorizontal(FocusgroupDirection direction);
+  static bool IsDirectionVertical(FocusgroupDirection direction);
+  static bool IsAxisSupported(FocusgroupFlags flags,
+                              FocusgroupDirection direction);
+  static bool WrapsInDirection(FocusgroupFlags flags,
+                               FocusgroupDirection direction);
+
+  static Element* FindNearestFocusgroupAncestor(const Element* element);
+  static Element* NextElement(const Element* current, bool skip_subtree);
+  static bool IsFocusgroupItem(const Element* element);
 };
 
 }  // namespace blink
