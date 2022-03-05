da1ea793626a04b3a9bfddaa6a2932fb3cdd0f66
Ben Joyce <bjoyce@google.com>
2022-03-05 00:18:01 +0000

    Don't rerun tests that have a matching result.
    Test names may contain a wildcard and so won't match any test names in
    r.GetName. This will result in tests getting indefintely added to be
    reran.
    
    This could have the problem where a wildcard matches two tests and
    one needs to be reran, but the other doesn't, but this would not have
    the test be reran.
    
    Bug: 1297230
    Change-Id: I0c4b68590c659e00ffc3c354f8c07b3ea2ec2c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3491155
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Benjamin Joyce <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977885}

--- build/android/pylib/local/device/local_device_gtest_run.py
+++ build/android/pylib/local/device/local_device_gtest_run.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import contextlib
 import collections
+import fnmatch
 import itertools
 import logging
 import math
@@ -912,6 +913,19 @@ class LocalDeviceGtestRun(local_device_test_run.LocalDeviceTestRun):
           gtest_test_instance.TestNameWithoutDisabledPrefix(t))
     not_run_tests = tests_stripped_disabled_prefix.difference(
         set(r.GetName() for r in results))
+
+    if self._test_instance.extract_test_list_from_filter:
+      # A test string might end with a * in this mode, and so may not match any
+      # r.GetName() for the set difference. It's possible a filter like foo.*
+      # can match two tests, ie foo.baz and foo.foo.
+      # When running it's possible Foo.baz is ran, foo.foo is not, but the test
+      # list foo.* will not be reran as at least one result matched it.
+      not_run_tests = {
+          t
+          for t in not_run_tests
+          if not any(fnmatch.fnmatch(r.GetName(), t) for r in results)
+      }
+
     return results, list(not_run_tests) if results else None
 
   #override
