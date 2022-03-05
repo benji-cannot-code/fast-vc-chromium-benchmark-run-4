010c4c5e53c47eba0383c9f37e9935c533968dca
Josiah K <josiahk@google.com>
2022-03-05 06:18:17 +0000

    [Sticky Keys] Fix Sticky Keys overlay hidden behind Docked Magnifier
    Sticky Keys overlay was positioned absolutely at top left of screen,
    causing it to be obscured behind Docked Magnifier or overlaid on top of
    ChromeVox panel. Fix to use work area to position Sticky Keys overlay
    safely in main area of screen, below Docked Magnifier or ChromeVox.
    
    Fixed: 1063020
    AX-Relnotes: Fix Sticky Keys overlay hidden behind Docked Magnifier.
    Change-Id: I2686e5bbedc38d0e6285b3457c1ea7eee2f2efef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3504225
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Josiah Krutz <josiahk@google.com>
    Cr-Commit-Position: refs/heads/main@{#977956}

--- ash/accessibility/sticky_keys/sticky_keys_overlay.cc
+++ ash/accessibility/sticky_keys/sticky_keys_overlay.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
+#include "ash/wm/work_area_insets.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -282,8 +283,15 @@ views::Widget* StickyKeysOverlay::GetWidgetForTesting() {
 }
 
 gfx::Rect StickyKeysOverlay::CalculateOverlayBounds() {
+  // Get work area to inset overlay (below Docked Magnifier or ChromeVox).
+  gfx::Rect work_area = WorkAreaInsets::ForWindow(Shell::GetPrimaryRootWindow())
+                            ->user_work_area_bounds();
+
   int x = is_visible_ ? kHorizontalOverlayOffset : -widget_size_.width();
-  return gfx::Rect(gfx::Point(x, kVerticalOverlayOffset), widget_size_);
+
+  return gfx::Rect(
+      gfx::Point(work_area.x() + x, work_area.y() + kVerticalOverlayOffset),
+      widget_size_);
 }
 
 void StickyKeysOverlay::OnImplicitAnimationsCompleted() {
