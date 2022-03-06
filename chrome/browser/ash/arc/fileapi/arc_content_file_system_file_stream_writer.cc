a2623110bd10da013ab518c06c14ee1dbee5fe9e
Alan Ding <alanding@google.com>
2022-03-06 08:27:25 +0000

    arc: Close DocumentsProvider FDs after FileStreamWriter operations
    Currently openFileToWrite in ARC detaches the Android file descriptor
    and causes DocumentsProvider app's onClose() to be called immediately
    before the actual copy / write file operation(s) even started. This in
    turn results in a FileDescriptorDetachedException.
    
    This issue was reported by JottaCloud and triaged by Chrome OS DevRel.
    The fix is to sessionize the open operation on Android side so that
    the originally opened ParcelFileDescriptor can be closed after the
    Chromium side file write operation(s). For FileStreamWriter, all file
    operations are completed when the writer object is destroyed.
    
    The Android CLs counterpart are ag/16963727 and ag/16963726 (P and R).
    
    BUG=b:215364617
    TEST=./out/Debug/unit_tests --gtest_filter=ArcFileSystemOperationRunnerTest*/ArcContentFileSystemFileStreamWriterTest*
    TEST=./out/Debug/browser_tests --gtest_filter=QuickView/FilesAppBrowserTest.Test/openQuickViewDocumentsProvider*
    TEST=Manually verified using bug's StorageProviderWriteException APK
    
    Change-Id: I91a5b5e064ef4fc8278ef4d9b69bf0e856c9712f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3476721
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Alan Ding <alanding@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978052}

--- chrome/browser/ash/arc/fileapi/arc_content_file_system_file_stream_writer.cc
+++ chrome/browser/ash/arc/fileapi/arc_content_file_system_file_stream_writer.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/scoped_blocking_call.h"
-#include "chrome/browser/ash/arc/fileapi/arc_file_system_operation_runner_util.h"
 #include "content/public/browser/browser_thread.h"
 #include "mojo/public/cpp/platform/platform_handle.h"
 #include "mojo/public/cpp/system/platform_handle.h"
@@ -62,6 +61,9 @@ ArcContentFileSystemFileStreamWriter::ArcContentFileSystemFileStreamWriter(
       has_pending_operation_(false) {}
 
 ArcContentFileSystemFileStreamWriter::~ArcContentFileSystemFileStreamWriter() {
+  CloseInternal(CloseStatus::kStatusOk);
+  // Use the task runner to destruct |file_| after the completion of all
+  // in-flight operations.
   task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&base::DeletePointer<base::File>, file_.release()));
@@ -80,9 +82,9 @@ int ArcContentFileSystemFileStreamWriter::Write(
     WriteInternal(buffer, buffer_length, std::move(callback));
     return net::ERR_IO_PENDING;
   }
-  file_system_operation_runner_util::OpenFileToWriteOnIOThread(
+  file_system_operation_runner_util::OpenFileSessionToWriteOnIOThread(
       arc_url_,
-      base::BindOnce(&ArcContentFileSystemFileStreamWriter::OnOpenFile,
+      base::BindOnce(&ArcContentFileSystemFileStreamWriter::OnOpenFileSession,
                      weak_ptr_factory_.GetWeakPtr(),
                      base::WrapRefCounted(buffer), buffer_length,
                      std::move(callback)));
@@ -110,13 +112,23 @@ int ArcContentFileSystemFileStreamWriter::Flush(
 
   has_pending_operation_ = true;
 
-  // |file_| is alive on FlushFile(), since the descructor will destruct
+  // |file_| is alive on FlushFile(), since the destructor will destruct
   // |task_runner_| along with |file_| and FlushFile() won't be called.
   base::PostTaskAndReplyWithResult(
       task_runner_.get(), FROM_HERE, base::BindOnce(&FlushFile, file_.get()),
       base::BindOnce(&ArcContentFileSystemFileStreamWriter::OnFlushFile,
                      weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
   return net::ERR_IO_PENDING;
+}
+
+void ArcContentFileSystemFileStreamWriter::CloseInternal(
+    const CloseStatus status) {
+  // Don't propagate the success status if there was a prior error or cancel.
+  if (!session_id_.empty()) {
+    file_system_operation_runner_util::CloseFileSession(session_id_, status);
+    // Clear the session to guarantee only one close per session.
+    session_id_.clear();
+  }
 }
 
 void ArcContentFileSystemFileStreamWriter::WriteInternal(
@@ -128,7 +140,7 @@ void ArcContentFileSystemFileStreamWriter::WriteInternal(
   DCHECK(file_->IsValid());
   DCHECK(has_pending_operation_);
 
-  // |file_| is alive on WriteFile(), since the descructor will destruct
+  // |file_| is alive on WriteFile(), since the destructor will destruct
   // |task_runner_| along with |file_| and WriteFile() won't be called.
   base::PostTaskAndReplyWithResult(
       task_runner_.get(), FROM_HERE,
@@ -148,27 +160,40 @@ void ArcContentFileSystemFileStreamWriter::OnWrite(
     return;
 
   has_pending_operation_ = false;
+  if (result < 0) {
+    CloseInternal(CloseStatus::kStatusError);
+  }
   std::move(callback).Run(result < 0 ? net::ERR_FAILED : result);
 }
 
-void ArcContentFileSystemFileStreamWriter::OnOpenFile(
+void ArcContentFileSystemFileStreamWriter::OnOpenFileSession(
     scoped_refptr<net::IOBuffer> buf,
     int buffer_length,
     net::CompletionOnceCallback callback,
-    mojo::ScopedHandle handle) {
+    mojom::FileSessionPtr file_session) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   DCHECK(!file_);
   DCHECK(has_pending_operation_);
+  DCHECK(session_id_.empty());
 
   if (CancelIfRequested())
     return;
 
+  if (file_session.is_null() || file_session->url_id.empty()) {
+    // The file_session and its url_id are required from the file system.
+    std::move(callback).Run(net::ERR_INVALID_ARGUMENT);
+    return;
+  }
+
+  session_id_ = std::move(file_session->url_id);
+  DCHECK(session_id_.length() > 0);
   mojo::PlatformHandle platform_handle =
-      mojo::UnwrapPlatformHandle(std::move(handle));
+      mojo::UnwrapPlatformHandle(std::move(file_session->fd));
   if (!platform_handle.is_valid()) {
     has_pending_operation_ = false;
     LOG(ERROR) << "PassWrappedInternalPlatformHandle failed";
-    std::move(callback).Run(net::ERR_FAILED);
+    CloseInternal(CloseStatus::kStatusError);
+    std::move(callback).Run(net::ERR_INVALID_HANDLE);
     return;
   }
   file_ = std::make_unique<base::File>(platform_handle.ReleaseFD());
@@ -178,7 +203,7 @@ void ArcContentFileSystemFileStreamWriter::OnOpenFile(
     OnSeekFile(buf, buffer_length, std::move(callback), 0);
     return;
   }
-  // |file_| is alive on SeekFile(), since the descructor will destruct
+  // |file_| is alive on SeekFile(), since the destructor will destruct
   // |task_runner_| along with |file_| and SeekFile() won't be called.
   base::PostTaskAndReplyWithResult(
       task_runner_.get(), FROM_HERE,
@@ -208,6 +233,7 @@ void ArcContentFileSystemFileStreamWriter::OnSeekFile(
     has_pending_operation_ = false;
     LOG(ERROR) << "Failed to seek: "
                << logging::SystemErrorCodeToString(seek_result);
+    CloseInternal(CloseStatus::kStatusError);
     std::move(callback).Run(
         net::FileErrorToNetError(base::File::OSErrorToFileError(seek_result)));
   }
@@ -232,6 +258,7 @@ bool ArcContentFileSystemFileStreamWriter::CancelIfRequested() {
     return false;
 
   has_pending_operation_ = false;
+  CloseInternal(CloseStatus::kStatusCancel);
   std::move(cancel_callback_).Run(net::OK);
   return true;
 }
