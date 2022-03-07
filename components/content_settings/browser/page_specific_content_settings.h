96ba765af01caaf9e96163d0cb113b781d165fc2
Christian Dullweber <dullweber@chromium.org>
2022-03-07 15:12:41 +0000

    Privacy Sandbox Settings: Add Topics to PageSpecificContentSettings
    Add Topics related methods to PageSpecificContentSettings to register
    when a Topic is accesseed in order to display this in PageInfo.
    
    Bug: 1286276
    Change-Id: Ie79f4eb663e1b8e531ee995de78f5c7f9f7f01b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3506424
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978262}

--- components/content_settings/browser/page_specific_content_settings.h
+++ components/content_settings/browser/page_specific_content_settings.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
+#include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/privacy_sandbox/canonical_topic.h"
 #include "content/public/browser/allow_service_worker_result.h"
 #include "content/public/browser/navigation_handle_user_data.h"
 #include "content/public/browser/page_user_data.h"
@@ -373,6 +375,10 @@ class PageSpecificContentSettings
                               bool blocked_by_policy);
   void OnInterestGroupJoined(const url::Origin api_origin,
                              bool blocked_by_policy);
+  void OnTopicAccessed(const url::Origin api_origin,
+                       bool blocked_by_policy,
+                       privacy_sandbox::CanonicalTopic topic);
+
   void OnWebDatabaseAccessed(const GURL& url, bool blocked_by_policy);
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
   void OnProtectedMediaIdentifierPermissionSet(const GURL& requesting_frame,
@@ -409,6 +415,9 @@ class PageSpecificContentSettings
 
   // Returns true if the page has accessed the Topics API.
   bool HasAccessedTopics() const;
+
+  // Returns the topics that were accessed by this page.
+  base::flat_set<privacy_sandbox::CanonicalTopic> GetAccessedTopics() const;
 
  private:
   friend class content::PageUserData<PageSpecificContentSettings>;
@@ -586,6 +595,9 @@ class PageSpecificContentSettings
   // only currently show the top frame as having attempted to join.
   std::vector<url::Origin> allowed_interest_group_api_;
   std::vector<url::Origin> blocked_interest_group_api_;
+
+  // Contains topics that were accessed by this page.
+  base::flat_set<privacy_sandbox::CanonicalTopic> accessed_topics_;
 
   // The Geolocation, camera, and/or microphone permission was granted to this
   // origin from a permission prompt that was triggered by the currently active
