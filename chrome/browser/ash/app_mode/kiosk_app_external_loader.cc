7b6e836461defba0331e361d4547cd3982b9d618
Ben Franz <bfranz@chromium.org>
2022-03-07 09:24:15 +0000

    Part 2/2: Split KioskAppManager into Ash and Lacros part
    This CL completes the split by taking out the necessary methods into its own class. In the process we rename the method calls to make it clearer what they are doing.
    
    Bug: b:214077940
    Test: kiosk_browsertest.cc
    Change-Id: Ic3fa8efc5ebb601e2477e8f8e123d866aba84f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3378633
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Ben Franz <bfranz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978194}

--- chrome/browser/ash/app_mode/kiosk_app_external_loader.cc
+++ chrome/browser/ash/app_mode/kiosk_app_external_loader.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/values.h"
-#include "chrome/browser/ash/app_mode/kiosk_app_manager.h"
+#include "chrome/browser/ash/app_mode/chrome_kiosk_external_loader_broker.h"
 
 namespace ash {
 
@@ -20,7 +20,8 @@ KioskAppExternalLoader::KioskAppExternalLoader(AppClass app_class)
 
 KioskAppExternalLoader::~KioskAppExternalLoader() {
   if (state_ != State::kInitial)
-    SetPrefsChangedHandler(InstallDataChangeCallback());
+    SetPrefsChangedHandler(
+        ChromeKioskExternalLoaderBroker::InstallDataChangeCallback());
 }
 
 void KioskAppExternalLoader::StartLoading() {
@@ -31,31 +32,27 @@ void KioskAppExternalLoader::StartLoading() {
 }
 
 void KioskAppExternalLoader::SetPrefsChangedHandler(
-    InstallDataChangeCallback handler) {
+    ChromeKioskExternalLoaderBroker::InstallDataChangeCallback handler) {
   switch (app_class_) {
     case AppClass::kPrimary:
-      KioskAppManager::Get()->SetPrimaryAppLoaderPrefsChangedHandler(
-          std::move(handler));
+      ChromeKioskExternalLoaderBroker::Get()
+          ->RegisterPrimaryAppInstallDataObserver(std::move(handler));
       break;
     case AppClass::kSecondary:
-      KioskAppManager::Get()->SetSecondaryAppsLoaderPrefsChangedHandler(
-          std::move(handler));
+      ChromeKioskExternalLoaderBroker::Get()
+          ->RegisterSecondaryAppInstallDataObserver(std::move(handler));
       break;
   }
 }
 
-void KioskAppExternalLoader::SendPrefs(
-    std::unique_ptr<base::DictionaryValue> prefs) {
-  if (!prefs)
-    return;
-
+void KioskAppExternalLoader::SendPrefs(base::DictionaryValue prefs) {
   const bool initial_load = state_ == State::kLoading;
   state_ = State::kLoaded;
 
   if (initial_load) {
-    LoadFinished(std::move(prefs));
+    LoadFinished(std::make_unique<base::DictionaryValue>(std::move(prefs)));
   } else {
-    OnUpdated(std::move(prefs));
+    OnUpdated(std::make_unique<base::DictionaryValue>(std::move(prefs)));
   }
 }
 
