a110ea79edd59109aba866ac93f80b36c7ab25af
Andrew Paseltiner <apaseltiner@chromium.org>
2022-03-07 23:09:40 +0000

    Merge ConversionMeasurementEventSources feature into ConversionMeasurement one
    Event sources should now be enabled together with the rest of the
    Attribution Reporting API.
    
    Bug: 1301981, 1275201
    Change-Id: Iff780ee92cf54196b916c0953a0d6ae6db3c5672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3504015
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978495}

--- content/browser/attribution_reporting/attribution_src_browsertest.cc
+++ content/browser/attribution_reporting/attribution_src_browsertest.cc
@@ -102,9 +102,6 @@ class AttributionSrcBrowserTest : public ContentBrowserTest {
     // Sets up the blink runtime feature for ConversionMeasurement.
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
-
-    // Sets up support for event sources.
-    command_line->AppendSwitch(switches::kEnableBlinkTestFeatures);
   }
 
   WebContents* web_contents() { return shell()->web_contents(); }
