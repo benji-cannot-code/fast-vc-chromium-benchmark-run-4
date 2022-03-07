7c2f8e6f65ed97479151b4daf64156d6f35dabdb
Austin Sullivan <asully@chromium.org>
2022-03-07 18:15:31 +0000

    Add debugging info to CdmStorageTest.Parallel* tests
    This should help diagnose why ParallelRead is flakily failing. Also
    adds debugging info to ParallelWrite since the tests are structured
    similarly.
    
    Bug: 1302616
    Change-Id: Ib1ca6644f2dbc7bc1959a29b07055df71fb624c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500859
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978340}

--- content/browser/media/cdm_storage_impl_unittest.cc
+++ content/browser/media/cdm_storage_impl_unittest.cc
@@ -358,7 +358,8 @@ TEST_P(CdmStorageTest, ParallelRead) {
   EXPECT_TRUE((status1 == CdmFile::Status::kSuccess &&
                status2 == CdmFile::Status::kFailure) ||
               (status1 == CdmFile::Status::kFailure &&
-               status2 == CdmFile::Status::kSuccess));
+               status2 == CdmFile::Status::kSuccess))
+      << "status 1: " << status1 << ", status2: " << status2;
 }
 
 TEST_P(CdmStorageTest, ParallelWrite) {
@@ -375,7 +376,8 @@ TEST_P(CdmStorageTest, ParallelWrite) {
   EXPECT_TRUE((status1 == CdmFile::Status::kSuccess &&
                status2 == CdmFile::Status::kFailure) ||
               (status1 == CdmFile::Status::kFailure &&
-               status2 == CdmFile::Status::kSuccess));
+               status2 == CdmFile::Status::kSuccess))
+      << "status 1: " << status1 << ", status2: " << status2;
 }
 
 }  // namespace content
