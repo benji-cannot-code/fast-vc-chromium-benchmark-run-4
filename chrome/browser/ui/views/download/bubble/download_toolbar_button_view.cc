01f00b29d572a0f958dfc5d5bd86d76f0f398792
Xinghui Lu <xinghuilu@chromium.org>
2022-03-07 20:05:24 +0000

    Update download toolbar icon.
    Per UX guidance, amplify the size of the download toolbar icon to match
    the other toolbar icons.
    
    Completed icon:
    * before: http://screen/6CHqerQPxYLZSad
    * after: http://screen/8XaU6M7GiVqah7B
    In progress icon:
    * before: http://screen/4eSnrCSrdq5Rhwt
    * after: http://screen/74Cibhe34L3u3rh
    
    Asset folder: go/downloads-redesign-assets
    
    Bug: 1282240
    Change-Id: Icd0af1c3d3f114038143f0883d289dcd4ea551e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502237
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Rohit Bhatia <bhatiarohit@google.com>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978397}

--- chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
+++ chrome/browser/ui/views/download/bubble/download_toolbar_button_view.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr int kProgressRingRadius = 7;
+constexpr int kProgressRingRadius = 9;
 constexpr float kProgressRingStrokeWidth = 1.7f;
 
 }  // namespace
