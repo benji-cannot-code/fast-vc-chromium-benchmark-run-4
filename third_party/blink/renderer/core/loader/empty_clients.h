97856c2c028d7b489ed8fa6cfe5d7fad6633d1b2
Stephan Hartmann <stha09@googlemail.com>
2022-03-07 03:10:50 +0000

    libstdc++: fix incomplete type of blink::WebURLLoaderFactory
    Destructor of std::unique_ptr in libstdc++ uses sizeof() which
    requires full definition of blink::WebURLLoaderFactory in
    blink::EmptyChromeClient.
    
    Bug: 957519
    Change-Id: I6a7b6aee0f712983cab77c038df1037cd6ad8e25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500523
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#978123}

--- third_party/blink/renderer/core/loader/empty_clients.h
+++ third_party/blink/renderer/core/loader/empty_clients.h
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/input/focus_type.mojom-blink-forward.h"
 #include "third_party/blink/public/platform/web_spell_check_panel_host_client.h"
 #include "third_party/blink/public/platform/web_url_loader.h"
+#include "third_party/blink/public/platform/web_url_loader_factory.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/remote_frame_client.h"
