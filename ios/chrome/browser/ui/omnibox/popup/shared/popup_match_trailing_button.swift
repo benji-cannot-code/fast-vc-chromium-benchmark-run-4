8b0a20fcdeccf170b09622ec8a8c4b34c2e92516
Quentin Pubert <qpubert@google.com>
2022-03-07 11:31:43 +0000

    [iOS] Move SwiftUI omnibox popup row trailing button in its own file
    This CL moves the SwiftUI omnibox popup trailing button in its own file.
    
    Bug: 1294267
    Change-Id: Ia1d27af30ecae7ca29b3c2cadd0266f5a8596574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498111
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#978218}

---
+++ ios/chrome/browser/ui/omnibox/popup/shared/popup_match_trailing_button.swift
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import SwiftUI
+
+/// A button view to act as the trailing button of a popup match and provide an additional action for a match.
+struct PopupMatchTrailingButton: View {
+  enum Colors {
+    static let trailingButtonColor = Color(red: 0.769, green: 0.769, blue: 0.769)
+  }
+
+  enum Dimensions {
+    static let extendedTouchTargetDiameter: CGFloat = 44
+    static let trailingButtonSize: CGFloat = 24
+  }
+
+  let match: PopupMatch
+  let action: () -> Void
+
+  var body: some View {
+    Button(action: action) {
+      Image(systemName: match.isAppendable ? "arrow.up.backward" : "arrow.right.square")
+        .foregroundColor(Colors.trailingButtonColor)
+        .aspectRatio(contentMode: .fit)
+        .frame(
+          width: Dimensions.trailingButtonSize, height: Dimensions.trailingButtonSize,
+          alignment: .center
+        )
+        .contentShape(
+          Circle().size(
+            width: Dimensions.extendedTouchTargetDiameter,
+            height: Dimensions.extendedTouchTargetDiameter)
+        )
+    }
+    .buttonStyle(.plain)
+  }
+}
