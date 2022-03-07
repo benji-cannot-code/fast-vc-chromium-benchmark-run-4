8c2195bec4118f36b033d40bafa225f8bd06d427
wangpei <wangpei@uniontech.com>
2022-03-07 18:14:44 +0000

    Code format optimization.
    Simple optimizations for code comments in chrome/browser/ui/browser_command_controller.cc.
    
    Bug: nobug
    Change-Id: I0ba4cf2b5d32f2879d9333649b3c5866313a0614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3506229
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978339}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -425,7 +425,7 @@ bool BrowserCommandController::ExecuteCommandWithDisposition(
       CloseTabSearch(browser_);
       break;
 
-      // Window management commands
+    // Window management commands
     case IDC_NEW_WINDOW:
       NewWindow(browser_);
       break;
