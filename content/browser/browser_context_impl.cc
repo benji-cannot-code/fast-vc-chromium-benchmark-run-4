4a293fd841b8c5422932c2bd5250bcfb7d415413
Max Curran <curranmax@chromium.org>
2022-03-07 19:52:38 +0000

    Establish high level structure of PrefetchProxy client refactor.
    This CL creates the main classes of this refactor, and outlines their
    responsibilities and major dependencies. These classes will be completed
    in future CLs.
    
    Bug: 1299059
    Change-Id: Iff1c2b0de4e6d6e517d89e350ee94fb31c6165aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3475599
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978388}

--- content/browser/browser_context_impl.cc
+++ content/browser/browser_context_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/browsing_data/browsing_data_remover_impl.h"
 #include "content/browser/download/download_manager_impl.h"
 #include "content/browser/permissions/permission_controller_impl.h"
+#include "content/browser/speculation_rules/prefetch/prefetch_service.h"
 #include "content/browser/speech/tts_controller_impl.h"
 #include "content/browser/storage_partition_impl_map.h"
 #include "content/public/browser/browser_context.h"
@@ -291,6 +292,13 @@ storage::ExternalMountPoints* BrowserContextImpl::GetMountPoints() {
 #else
   return nullptr;
 #endif
+}
+
+PrefetchService* BrowserContextImpl::GetPrefetchService() {
+  if (!prefetch_service_)
+    prefetch_service_ = PrefetchService::CreateIfPossible();
+
+  return prefetch_service_.get();
 }
 
 }  // namespace content
--- content/browser/speculation_rules/speculation_host_impl.cc
+++ content/browser/speculation_rules/speculation_host_impl.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/prerender/prerender_host_registry.h"
 #include "content/browser/renderer_host/render_frame_host_delegate.h"
 #include "content/browser/renderer_host/render_frame_host_impl.h"
+#include "content/browser/speculation_rules/prefetch/prefetch_document_manager.h"
+#include "content/browser/speculation_rules/prefetch/prefetch_features.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
@@ -102,6 +104,14 @@ void SpeculationHostImpl::UpdateSpeculationCandidates(
     return;
   if (render_frame_host()->GetParent())
     return;
+
+  if (base::FeatureList::IsEnabled(features::kPrefetchUseContentRefactor)) {
+    PrefetchDocumentManager* prefetch_document_manager =
+        PrefetchDocumentManager::GetOrCreateForCurrentDocument(
+            render_frame_host());
+
+    prefetch_document_manager->ProcessCandidates(candidates);
+  }
 
   // Let `delegate_` process the candidates that it is interested in.
   if (delegate_)
