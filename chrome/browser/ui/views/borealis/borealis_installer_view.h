f2b6828efd4582ee219cef40ce1c43c2c0e7829f
Nicholas Hollingum <hollingum@google.com>
2022-03-07 22:08:54 +0000

    borealis: Prevent dcheck crash in the installer
    The borealis installer would only AddObserver() when the user clicked
    start, but it would RemoveObserver() when destructed. This measn that if
    you cancel/exit the installer before clicking start, you try to remove
    an observer that was never added.
    
    We fix this by using ScopedObserver, which only tries to remove itself
    if it was added.
    
    Bug: b/220196378
    Change-Id: Ic746ad4189892beb8b55e4557a5ee7b41efc6aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3505744
    Reviewed-by: Daniel Ng <danielng@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/main@{#978467}

--- chrome/browser/ui/views/borealis/borealis_installer_view.h
+++ chrome/browser/ui/views/borealis/borealis_installer_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_BOREALIS_BOREALIS_INSTALLER_VIEW_H_
 
 #include "base/callback.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ash/borealis/borealis_installer.h"
 #include "chrome/browser/ash/borealis/borealis_metrics.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -100,6 +101,10 @@ class BorealisInstallerView : public views::DialogDelegateView,
   State state_ = State::kConfirmInstall;
   InstallingState installing_state_ = InstallingState::kInactive;
   absl::optional<borealis::BorealisInstallResult> result_;
+
+  base::ScopedObservation<borealis::BorealisInstaller,
+                          borealis::BorealisInstaller::Observer>
+      observation_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_BOREALIS_BOREALIS_INSTALLER_VIEW_H_
