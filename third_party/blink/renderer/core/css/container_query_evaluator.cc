ac7466711feb4e3dbe5cbdb9b96e63ccd8075088
Anders Hartvoll Ruud <andruud@chromium.org>
2022-03-07 23:18:12 +0000

    [@container] Store the 'Change' in ContainerQueryEvaluation's result
    Currently we rely on ContainerQuerySelector::IsNearest to determine
    the severity of a given evaluation change (see ComputeChange).
    However, as of recently spec changes, container selection does not
    have an unconditional "nearest" mode anymore. Now (for unnamed
    queries), we'll select the nearest container that can answer the
    features used in the query.
    
    Therefore, IsNearest can no longer be used to determine
    kNearestContainer/kDescendantContainers on changes.
    
    Instead, we store whether or not the selected container as a matter
    of fact *was* the nearest container in the results. This should have
    the added benefit of using kNearestContainer more often, since e.g.
    named queries can also happen to select the nearest container.
    
    Bug: 1300547
    Change-Id: If88ad90092aaed1968ca783cc68019b14c5d9a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500600
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978499}

--- third_party/blink/renderer/core/css/container_query_evaluator.cc
+++ third_party/blink/renderer/core/css/container_query_evaluator.cc
@@ -76,6 +76,21 @@ Element* ContainerQueryEvaluator::FindContainer(
   return nullptr;
 }
 
+bool ContainerQueryEvaluator::EvalAndAdd(const StyleRecalcContext& context,
+                                         const ContainerQuery& query,
+                                         MatchResult& match_result) {
+  Element* container = FindContainer(context, query.Selector());
+  if (!container)
+    return false;
+  ContainerQueryEvaluator* evaluator = container->GetContainerQueryEvaluator();
+  if (!evaluator)
+    return false;
+  Change change = (context.container == container)
+                      ? Change::kNearestContainer
+                      : Change::kDescendantContainers;
+  return evaluator->EvalAndAdd(query, change, match_result);
+}
+
 double ContainerQueryEvaluator::Width() const {
   return size_.width.ToDouble();
 }
@@ -97,11 +112,8 @@ bool ContainerQueryEvaluator::Eval(const ContainerQuery& container_query,
          KleeneValue::kTrue;
 }
 
-void ContainerQueryEvaluator::Add(const ContainerQuery& query, bool result) {
-  results_.Set(&query, result);
-}
-
 bool ContainerQueryEvaluator::EvalAndAdd(const ContainerQuery& query,
+                                         Change change,
                                          MatchResult& match_result) {
   MediaQueryResultList viewport_dependent;
   unsigned unit_flags = MediaQueryExpValue::UnitFlags::kNone;
@@ -113,7 +125,7 @@ bool ContainerQueryEvaluator::EvalAndAdd(const ContainerQuery& query,
     match_result.SetDependsOnRemContainerQueries();
   if (unit_flags & MediaQueryExpValue::UnitFlags::kFontRelative)
     depends_on_font_ = true;
-  Add(query, result);
+  results_.Set(&query, Result{result, change});
   return result;
 }
 
@@ -190,11 +202,8 @@ ContainerQueryEvaluator::Change ContainerQueryEvaluator::ComputeChange() const {
     return Change::kDescendantContainers;
 
   for (const auto& result : results_) {
-    if (Eval(*result.key) != result.value) {
-      change = std::max(change, result.key->Selector().IsNearest()
-                                    ? Change::kNearestContainer
-                                    : Change::kDescendantContainers);
-    }
+    if (Eval(*result.key) != result.value.value)
+      change = std::max(change, result.value.change);
   }
 
   return change;
