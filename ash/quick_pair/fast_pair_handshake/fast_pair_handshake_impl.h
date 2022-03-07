03ca418c7efc5a195387dedb8e1873a58ef571ab
Shane Fitzpatrick <shanefitz@google.com>
2022-03-07 22:53:28 +0000

    [FastPair] Remove unused IsConnected function
    TEST=Ran unit tests
    
    Change-Id: Iac58d81c1ad1f952635820b8492c867feb0a4815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508613
    Auto-Submit: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#978488}

--- ash/quick_pair/fast_pair_handshake/fast_pair_handshake_impl.h
+++ ash/quick_pair/fast_pair_handshake/fast_pair_handshake_impl.h
@@ -25,8 +25,6 @@ class FastPairHandshakeImpl : public FastPairHandshake {
   FastPairHandshakeImpl& operator=(const FastPairHandshakeImpl&) = delete;
   ~FastPairHandshakeImpl() override;
 
-  bool IsConnected() override;
-
  private:
   void OnGattClientInitializedCallback(absl::optional<PairFailure> failure);
   void OnDataEncryptorCreateAsync(
