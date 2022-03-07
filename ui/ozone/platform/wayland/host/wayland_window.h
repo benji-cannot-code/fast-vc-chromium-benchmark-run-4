279de29ea7f7840d07a7aec25f8345c88534f120
Kramer Ge <fangzhoug@chromium.org>
2022-03-07 23:53:29 +0000

    [Ozone/Wayland]Only remove pending_configure if it's sent to ui::Compositor and has been acked
    We're seeing cases where toplevel sends us {max_size, snapped_size} in
    that order, when the window is already rendering at |snapped_size|. When
    WaylandWindow receives a frame with |snapped_size| it clears out the
    |pending_configures_| up to (including) the latter |snapped_size|, just
    after SetBounds(|max_size|) is called. In this scenario the expected
    SetBounds(|snapped_size|) will never be called and the window is stuck
    in an incorrect bounds.
    
    Apply this patch s.t. we never clear a configure if no
    ApplyPendingConfigure() is called on it yet.
    
    Change-Id: I407f1ac25e5417820260d7858199f5a5765b249c
    Bug: 1291807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3503696
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978515}

--- ui/ozone/platform/wayland/host/wayland_window.h
+++ ui/ozone/platform/wayland/host/wayland_window.h
@@ -329,7 +329,7 @@ class WaylandWindow : public PlatformWindow,
   bool ProcessVisualSizeUpdate(const gfx::Size& size_px, float scale_factor);
 
   // Applies pending bounds.
-  virtual void ApplyPendingBounds() = 0;
+  virtual void ApplyPendingBounds();
 
   // These bounds attributes below have suffixes that indicate units used.
   // Wayland operates in DIP but the platform operates in physical pixels so
@@ -348,6 +348,8 @@ class WaylandWindow : public PlatformWindow,
   struct PendingConfigure {
     gfx::Rect bounds_dip;
     uint32_t serial;
+    // True if this configure has been passed to the compositor for rendering.
+    bool set = false;
   };
   base::circular_deque<PendingConfigure> pending_configures_;
 
