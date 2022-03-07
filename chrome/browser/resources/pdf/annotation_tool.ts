b00fd6e3ee2b92dc0340cdb3b2fdbd98dd9a304b
dpapad <dpapad@chromium.org>
2022-03-07 18:16:42 +0000

    [Reland] PDF Viewer: Migrate to TypeScript, part 3.
    Reason for original CL's revert has been addressed.
    
    Based on dhoss@'s https://chromium-review.googlesource.com/c/chromium/src/+/2986362
    
    Bug: 1260303
    Change-Id: I9b7672bf2a5c73dd5f83017cc06daa6996eb6e3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3506245
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978341}

--- chrome/browser/resources/pdf/annotation_tool.ts
+++ chrome/browser/resources/pdf/annotation_tool.ts
@@ -3,11 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * @typedef {{
- *   tool: string,
- *   size: number,
- *   color: (string|undefined),
- * }}
- */
-let AnnotationTool;
+export type AnnotationTool = {
+  tool: string,
+  size: number,
+  color?: string,
+};
--- chrome/browser/resources/pdf/ink/ink_api.js
+++ chrome/browser/resources/pdf/ink/ink_api.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Wraps the Ink component with an API that can be called
  * across an IFrame boundary.
  */
-class InkAPI {
+export class InkAPI {
   /** @param {!drawings.Canvas} canvas */
   constructor(canvas) {
     this.canvas_ = canvas;
