39be3a831814f118a7435c3c982b940711468c5f
Lei Zhang <thestig@chromium.org>
2022-03-07 19:16:16 +0000

    Change shabang to run vpython3 for coverage.py.
    As seen in https://crrev.com/922986, tools/code_coverage/coverage.py can
    be used with vpython3, so use that in the shabang and let it run with
    Python 3 by default.
    
    Fix a presubmit error for lines over 80 columns along the way.
    
    Change-Id: I83e4bfbdd14a97953d0dddfea111a9f7e1e800ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3504504
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978376}

--- tools/code_coverage/coverage.py
+++ tools/code_coverage/coverage.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env vpython
+#!/usr/bin/env vpython3
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -253,8 +253,8 @@ def _GeneratePerFileLineByLineCoverageInFormat(binary_paths, profdata_file_path,
   logging.debug('Finished running "llvm-cov show" command.')
 
 
-def _GeneratePerFileLineByLineCoverageInLcov(binary_paths, profdata_file_path, filters,
-                                             ignore_filename_regex):
+def _GeneratePerFileLineByLineCoverageInLcov(binary_paths, profdata_file_path,
+                                             filters, ignore_filename_regex):
   """Generates per file line-by-line coverage using "llvm-cov export".
 
   Args:
