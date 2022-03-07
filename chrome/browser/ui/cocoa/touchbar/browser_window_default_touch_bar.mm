60021053c12a7a8b14431967e058aba86a07f523
Erik Chen <erikchen@chromium.org>
2022-03-07 22:12:00 +0000

    Fix memory leak in calls to CreateTouchBarButton.
    The function returns a retained object, but there is no corresponding
    call to release or autorelease. This CL fixes the function to not return
    a retained object.
    
    Bug: 1301079
    Change-Id: Ie5a541df153af9dd057e180c853cbdc1efba92c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508254
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978471}

--- chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
+++ chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
@@ -101,7 +101,7 @@ NSButton* CreateTouchBarButtonWithImage(NSImage* image,
                                         action:@selector(executeCommand:)];
   button.tag = command;
   button.accessibilityTitle = l10n_util::GetNSString(tooltip_id);
-  return [button retain];
+  return button;
 }
 
 // Creates an NSButton for the touch bar using a vector icon.
@@ -497,8 +497,8 @@ class API_AVAILABLE(macos(10.12.2)) TouchBarNotificationBridge
   int tooltipId = _isStarred ? IDS_TOOLTIP_STARRED : IDS_TOOLTIP_STAR;
 
   if (!_starredButton) {
-    _starredButton.reset(CreateTouchBarButtonWithImage(
-        image, self, IDC_BOOKMARK_THIS_TAB, tooltipId));
+    _starredButton.reset([CreateTouchBarButtonWithImage(
+        image, self, IDC_BOOKMARK_THIS_TAB, tooltipId) retain]);
     return;
   }
 
@@ -518,8 +518,8 @@ class API_AVAILABLE(macos(10.12.2)) TouchBarNotificationBridge
   int tooltipId = _isPageLoading ? IDS_TOOLTIP_STOP : IDS_TOOLTIP_RELOAD;
 
   if (!_reloadStopButton) {
-    _reloadStopButton.reset(
-        CreateTouchBarButtonWithImage(image, self, commandId, tooltipId));
+    _reloadStopButton.reset([CreateTouchBarButtonWithImage(
+        image, self, commandId, tooltipId) retain]);
     return;
   }
 
