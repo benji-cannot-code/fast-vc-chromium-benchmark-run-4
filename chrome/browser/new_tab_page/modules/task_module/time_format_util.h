88aad09d7e352b3b43c1439eaa109b898f2e42ed
Gabriel Charette <gab@chromium.org>
2022-03-07 22:15:12 +0000

    Reland "Fix flaky TimeFormatUtilTest.GetViewedItemText"
    This is a reland of commit f685ea7129fa0ca804e8730c3a5daf8d83618385
    
    Reason for reland: Fixed null-deref caused by reading the optional
    unconditionally in the original change.
    
    Original change's description:
    > Fix flaky TimeFormatUtilTest.GetViewedItemText
    >
    > Time should be passed using base::Time, passing it as an int lost
    > precision and resulted in flaky results. Storing it as an int is fine
    > but that should be an implementation detail at the storage site,
    > understanding that we loose precision there as well but being okay
    > with that.
    >
    > Tweaked the "Viewed in the past month" test case as it was
    > effectively testing (now - base::Days(30)) < (now - base::Days(30)).
    > The only reason this passed most of the time was the loss of
    > precision making the recomputed value land a second short...
    > Flakily being == when the microseconds representation of Now() luckily
    > had enough 0's in its tail to not lose precision...
    >
    > Fixed: 1302663
    > Change-Id: I0ab90fe66bc4c1927429c3fbaa9cad91da1e2db6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498477
    > Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    > Commit-Queue: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#977664}
    
    Fixed: 1302663
    Change-Id: I250be959f6d09c18703f7e9fc741773558cca7c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507693
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Mohamad Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978473}

--- chrome/browser/new_tab_page/modules/task_module/time_format_util.h
+++ chrome/browser/new_tab_page/modules/task_module/time_format_util.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/time/time.h"
+
 // Turns the |viewed_timestamp| Unix Epoch timestamp of a task visit into a
 // human-readable, localized string indicating when this task was viewed (e.g.
 // "Viewed today", "Viewed in the past week").
-std::string GetViewedItemText(int viewed_timestamp);
+std::string GetViewedItemText(base::Time viewed_timestamp);
 
 #endif  // CHROME_BROWSER_NEW_TAB_PAGE_MODULES_TASK_MODULE_TIME_FORMAT_UTIL_H_
