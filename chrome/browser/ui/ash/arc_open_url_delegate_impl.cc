31c0704a570ab2aa0d5d8dd7904420319178831c
Nancy Wang <nancylingwang@chromium.org>
2022-03-07 23:27:17 +0000

    Replace AppUpdate::GetWindowMode with AppUpdate::WindowMode.
    This CL modifies AppUpdate::WindowMode to get value from the non mojom
    App struct when it is available, and remove AppUpdate::GetWindowMode.
    
    Modify tests to use and verify AppUpdate::WindowMode.
    
    Move AppServiceProxyAsh::OnLaunched in the cc file to align with the
    function definitaion sequence on the .h file.
    
    BUG=1253250
    
    Change-Id: I762bd28799a16cb30612355ac8c97ae50e00d838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499106
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978500}

--- chrome/browser/ui/ash/arc_open_url_delegate_impl.cc
+++ chrome/browser/ui/ash/arc_open_url_delegate_impl.cc
@@ -228,7 +228,7 @@ void ArcOpenUrlDelegateImpl::OpenWebAppFromArc(const GURL& url) {
 
   proxy->AppRegistryCache().ForOneApp(
       *app_id, [&event_flags](const apps::AppUpdate& update) {
-        if (update.WindowMode() == apps::mojom::WindowMode::kBrowser) {
+        if (update.WindowMode() == apps::WindowMode::kBrowser) {
           event_flags = apps::GetEventFlags(
               apps::mojom::LaunchContainer::kLaunchContainerTab,
               WindowOpenDisposition::NEW_FOREGROUND_TAB,
@@ -367,7 +367,7 @@ void ArcOpenUrlDelegateImpl::OpenAppWithIntent(
   auto disposition = WindowOpenDisposition::NEW_WINDOW;
   proxy->AppRegistryCache().ForOneApp(
       app_id, [&launch_container, &disposition](const apps::AppUpdate& update) {
-        if (update.WindowMode() == apps::mojom::WindowMode::kBrowser) {
+        if (update.WindowMode() == apps::WindowMode::kBrowser) {
           launch_container = apps::mojom::LaunchContainer::kLaunchContainerTab;
           disposition = WindowOpenDisposition::NEW_FOREGROUND_TAB;
         }
