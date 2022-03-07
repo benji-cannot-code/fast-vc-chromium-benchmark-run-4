e3b13091440163c196716395d3e766f999ec0c72
Andrew Grieve <agrieve@chromium.org>
2022-03-07 14:42:42 +0000

    SuperSize: Convert mock test wrappers python->python3
    Change-Id: Iccfadf20b577269d68d303a8c643c8c861d1b75d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502705
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978245}

--- tools/binary_size/libsupersize/testdata/mock_sdk/tools/bin/mock_aapt2.py
+++ tools/binary_size/libsupersize/testdata/mock_sdk/tools/bin/mock_aapt2.py
@@ -15,7 +15,7 @@ def main():
     try:
       # The AndroidManifest.xml file will have the aapt2 output, and not XML.
       with z.open('AndroidManifest.xml') as f:
-        sys.stdout.write(f.read())
+        sys.stdout.write(f.read().decode('utf8'))
     except KeyError:
       pass
 
