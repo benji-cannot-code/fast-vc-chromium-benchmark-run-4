d47fede2175f1730ca79f11ec8d16550403e4701
Findit <findit-for-me@appspot.gserviceaccount.com>
2022-03-08 21:13:19 +0000

    Revert "Disable UrlOverridingTest#testClearRedirectHandlerOnPageActivation"
    This reverts commit ae264e2c9ce1787d0f755d15720da0d52936bd97.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified this CL at revision ae264e2c9ce1787d0f755d15720da0d52936bd97 as
    the culprit for failures in the continuous build including:
    
    Sample Failed Build: https://ci.chromium.org/b/8820228574922743873
    Sample Failed Step: compile
    
    If it is a false positive, please report it at https://bugs.chromium.org/p/chromium/issues/entry?status=Available&comment=Datastore+key+for+the+culprit+entity%3A+chromium.googlesource.com%2Fchromium%2Fsrc%2Frefs%2Fheads%2Fmain%2Fae264e2c9ce1787d0f755d15720da0d52936bd97&labels=Test-Findit-Wrong&components=Tools%3ETest%3EFindIt&summary=Wrongly+blame+ae264e2c9ce1787d0f755d15720da0d52936bd97
    
    Original change's description:
    > Disable UrlOverridingTest#testClearRedirectHandlerOnPageActivation
    > 
    > Bug: 1304292
    > Change-Id: Ia6282f37ba4619f7ae70fe94288ff30f5d09c3ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3509145
    > Commit-Queue: Michael Bai <michaelbai@chromium.org>
    > Auto-Submit: Michael Bai <michaelbai@chromium.org>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#978830}
    
    
    Change-Id: I1ad891b25461fb735c9063915e985045cc940ff8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1304292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510930
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@google.com>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@google.com>
    Cr-Commit-Position: refs/heads/main@{#978861}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -808,7 +808,6 @@ public class UrlOverridingTest {
     @Test
     @LargeTest
     @Features.EnableFeatures({ChromeFeatureList.PRERENDER2})
-    @DisabledTest(message = "https://crbug.com/1304292")
     public void testClearRedirectHandlerOnPageActivation() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
 
