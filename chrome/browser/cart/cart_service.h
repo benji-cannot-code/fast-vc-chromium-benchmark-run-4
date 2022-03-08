a4afd2845bcf566f674c210bce778fd8a6e06adb
Mei Liang <meiliang@chromium.org>
2022-03-08 22:14:44 +0000

    [ConsentV2] Add user metrics
    See more detail about these metrics in https://docs.google.com/document/d/1yGCJ5ans_hmYN4sden7DSmLXQJbCwJfHg_op9yXPvog/edit?usp=sharing&resourcekey=0-gg6XdZFEmydQnNYXnjy-DA
    
    Change-Id: I3c75206307603bd2a8605a08cc30163f11e85a15
    Bug: 1298116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502273
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978908}

--- chrome/browser/cart/cart_service.h
+++ chrome/browser/cart/cart_service.h
@@ -97,6 +97,9 @@ class CartService : public history::HistoryServiceObserver,
   void AcknowledgeDiscountConsent(bool should_enable);
   // Gets called when user has dismissed the discount consent in cart module.
   void DismissedDiscountConsent();
+  // Gets called when user has click the 'Continue' button in the discount
+  // consent.
+  void InterestedInDiscountConsent();
   // Decides whether to show the consent card in module for rule-based discount,
   // and returns it in the callback.
   void ShouldShowDiscountConsent(base::OnceCallback<void(bool)> callback);
@@ -132,6 +135,7 @@ class CartService : public history::HistoryServiceObserver,
  private:
   friend class CartServiceFactory;
   friend class CartServiceTest;
+  friend class CartServiceDiscountConsentV2Test;
   friend class CartServiceDiscountTest;
   friend class CartServiceBrowserDiscountTest;
   friend class CartServiceDiscountFetchTest;
@@ -213,6 +217,8 @@ class CartService : public history::HistoryServiceObserver,
   void OnCartFeaturesChanged(const std::string& pref_name);
   // Get if cart and discount feature are both enabled.
   bool IsCartAndDiscountEnabled();
+  // Get calls when Cart module loads.
+  void RecordDiscountConsentStatusAtLoad(bool should_show_consent);
 
   raw_ptr<Profile> profile_;
   std::unique_ptr<CartDB> cart_db_;
--- chrome/browser/resources/new_tab_page/modules/cart/discount_consent_card.ts
+++ chrome/browser/resources/new_tab_page/modules/cart/discount_consent_card.ts
@@ -49,6 +49,8 @@ interface Step {
  *   * Inline has two steps.
  */
 export enum DiscountConsentVariation {
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
   Default = 0,
   StringChange = 1,
   Inline = 2,
@@ -139,7 +141,10 @@ export class DiscountConsentCard extends I18nMixin
           } else {
             this.showDiscountConsentDialog_ = true;
           }
-          // TODO(crbug.com/1298116): Record user click on this button.
+          chrome.metricsPrivate.recordUserAction(
+              'NewTabPage.Carts.ShowInterestInDiscountConsent');
+          this.dispatchEvent(
+              new CustomEvent('discount-consent-continued', {composed: true}));
         },
       }
     });
