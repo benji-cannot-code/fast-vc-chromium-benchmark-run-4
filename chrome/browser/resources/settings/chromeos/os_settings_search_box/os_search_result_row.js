6b4bbfc201901e22fe644dd82878fb3335ffaba8
Xiaohui Chen <xiaohuic@google.com>
2022-03-08 21:44:26 +0000

    os settings: migration icons file to polymer3
    BUG=1180979
    
    Change-Id: Ice316b72691686e0657f1dee96dcf6afb8e8c12b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3501783
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978877}

--- chrome/browser/resources/settings/chromeos/os_settings_search_box/os_search_result_row.js
+++ chrome/browser/resources/settings/chromeos/os_settings_search_box/os_search_result_row.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview 'os-search-result-row' is the container for one search result.
  */
 import '//resources/cr_elements/icons.m.js';
-import '../os_icons.m.js';
+import '../os_icons.js';
 import '../../settings_shared_css.js';
 
 import {assert, assertNotReached} from '//resources/js/assert.m.js';
