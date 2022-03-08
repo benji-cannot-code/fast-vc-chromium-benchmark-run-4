f5153c400037cfb8b99e23340126090891ecde65
Peter Kasting <pkasting@chromium.org>
2022-03-08 09:27:49 +0000

    Convert omnibox colors to color pipeline.
    Bug: 1296206
    Change-Id: Ib61c6414f457179faff3351c12746a86e170c187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508281
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978656}

--- chrome/browser/ui/views/omnibox/omnibox_suggestion_button_row_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_suggestion_button_row_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_SUGGESTION_BUTTON_ROW_VIEW_H_
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/omnibox/omnibox_theme.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/omnibox_popup_selection.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -33,8 +34,8 @@ class OmniboxSuggestionButtonRowView : public views::View {
       const OmniboxSuggestionButtonRowView&) = delete;
   ~OmniboxSuggestionButtonRowView() override;
 
-  // Called when results background color is refreshed.
-  void OnOmniboxBackgroundChange(SkColor omnibox_bg_color);
+  // Called when the theme state may have changed.
+  void SetThemeState(OmniboxPartState theme_state);
 
   // Updates the suggestion row buttons based on the model.
   void UpdateFromModel();
