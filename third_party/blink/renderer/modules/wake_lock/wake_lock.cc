e20cf1033125ecd7d3c138003379725785fab1c3
Vadym Kargin <vadym.kargin@accenture.com>
2022-03-08 15:37:31 +0000

    Replace DOMException with V8ThrowDOMException in the wake lock module.
    This CL is a part of chain of updates for replacing DOMException with V8ThrowDOMException.
    
    Issue: Replace DOMException with ExceptionState/V8ThrowDOMException
    Bug: 991544
    Change-Id: Ifc5e66972bd10734af6304e3ef410f3b710dc976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507831
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Vadym Kargin <vadym.kargin@accenture.com>
    Cr-Commit-Position: refs/heads/main@{#978723}

--- third_party/blink/renderer/modules/wake_lock/wake_lock.cc
+++ third_party/blink/renderer/modules/wake_lock/wake_lock.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -183,14 +185,24 @@ void WakeLock::DidReceivePermissionResponse(WakeLockType type,
   DCHECK(status == PermissionStatus::GRANTED ||
          status == PermissionStatus::DENIED);
   DCHECK(resolver);
+  // Support creating DOMException with JS stack.
+  ScriptState* resolver_script_state = resolver->GetScriptState();
+  if (!IsInParallelAlgorithmRunnable(resolver->GetExecutionContext(),
+                                     resolver_script_state)) {
+    return;
+  }
+  // switch to the resolver's context to let DOMException pick up the resolver's
+  // JS stack
+  ScriptState::Scope script_state_scope(resolver_script_state);
+
   // 8.2. If state is "denied", then:
   // 8.2.1. Queue a global task on the screen wake lock task source given
   //        document's relevant global object to reject promise with a
   //        "NotAllowedError" DOMException.
   // 8.2.2. Abort these steps.
   if (status != PermissionStatus::GRANTED) {
-    resolver->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kNotAllowedError,
+    resolver->Reject(V8ThrowDOMException::CreateOrDie(
+        resolver_script_state->GetIsolate(), DOMExceptionCode::kNotAllowedError,
         "Wake Lock permission request denied"));
     return;
   }
@@ -202,8 +214,8 @@ void WakeLock::DidReceivePermissionResponse(WakeLockType type,
     //        then:
     // 8.3.1.1. Reject promise with a "NotAllowedError" DOMException.
     // 8.3.1.2. Abort these steps.
-    resolver->Reject(MakeGarbageCollected<DOMException>(
-        DOMExceptionCode::kNotAllowedError,
+    resolver->Reject(V8ThrowDOMException::CreateOrDie(
+        resolver_script_state->GetIsolate(), DOMExceptionCode::kNotAllowedError,
         "The requesting page is not visible"));
     return;
   }
