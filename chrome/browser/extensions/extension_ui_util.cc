b8e5e04e522d59d555f44c02e77c0558fe248f29
Peter Kasting <pkasting@chromium.org>
2022-03-08 18:20:13 +0000

    ThemeProperties::GetDefaultColor() -> ThemeProvider::GetColor()
    This allows the test in question to be converted to ColorProvider.
    
    Bug: 805600
    Change-Id: Icf072e1dd81d542526724d41f1b9858790abc5f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508961
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978771}

--- chrome/browser/extensions/extension_ui_util.cc
+++ chrome/browser/extensions/extension_ui_util.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/themes/theme_properties.h"
-#include "chrome/browser/themes/theme_service.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -18,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/image_util.h"
-#include "ui/base/theme_provider.h"
 
 namespace extensions {
 
