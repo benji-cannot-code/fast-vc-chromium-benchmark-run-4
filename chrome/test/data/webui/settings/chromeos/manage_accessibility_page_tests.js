a7e47b06a58f58c77a434dc72f05c1b34824a725
Wes Okuhara <wesokuhara@google.com>
2022-03-08 00:08:00 +0000

    [CrOS Settings] Focus button when returning from a11y subpages
    This CL fixes an a11y issue where, on the ChromeOS Settings app >
    "Accessibility" > "Manage accessibility features" page,
    focus on buttons would be lost when navigating back from a subpage
    (e.g. "Captions") using the keyboard.
    
    Ultimately, the issue was that the underlying component
    for this page, `settings-manage-a11y-page`, overrides
    `currentRouteChanged()` which replaces the default logic implemented in
    `RouteOriginBehavior` to focus a selector when the page is changed.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/3362720
    was a good reference CL.
    
    Bug: 1227067
    Change-Id: I832263cc2300134acefa10db72cd0814463a59a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3486163
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#978525}

--- chrome/test/data/webui/settings/chromeos/manage_accessibility_page_tests.js
+++ chrome/test/data/webui/settings/chromeos/manage_accessibility_page_tests.js
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {assertEquals, assertFalse, assertTrue} from '../../chai_assert.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
-// #import {waitAfterNextRender} from 'chrome://test/test_util.js';
+// #import {eventToPromise, waitBeforeNextRender, waitAfterNextRender} from 'chrome://test/test_util.js';
 // clang-format on
 
 /**
@@ -392,5 +392,44 @@ suite('ManageAccessibilityPageTests', function() {
     assertEquals(
         'French (France) speech is sent to Google for processing',
         page.computeDictationLocaleSubtitle_());
+  });
+
+  [{selector: '#ttsSubpageButton', route: settings.routes.MANAGE_TTS_SETTINGS},
+   {
+     selector: '#captionsSubpageButton',
+     route: settings.routes.MANAGE_CAPTION_SETTINGS
+   },
+   {selector: '#displaySubpageButton', route: settings.routes.DISPLAY},
+   {selector: '#keyboardSubpageButton', route: settings.routes.KEYBOARD},
+   {selector: '#pointerSubpageButton', route: settings.routes.POINTERS},
+  ].forEach(({selector, route}) => {
+    test(
+        `should focus ${selector} button when returning from ${
+            route.path} subpage`,
+        async () => {
+          initPage();
+          Polymer.dom.flush();
+          const router = settings.Router.getInstance();
+
+          const subpageButton = page.$$(selector);
+          assertTrue(!!subpageButton);
+
+          subpageButton.click();
+          assertEquals(route, router.getCurrentRoute());
+          assertNotEquals(
+              subpageButton, page.shadowRoot.activeElement,
+              `${selector} should not be focused`);
+
+          const popStateEventPromise = eventToPromise('popstate', window);
+          router.navigateToPreviousRoute();
+          await popStateEventPromise;
+          await waitBeforeNextRender(page);
+
+          assertEquals(
+              settings.routes.MANAGE_ACCESSIBILITY, router.getCurrentRoute());
+          assertEquals(
+              subpageButton, page.shadowRoot.activeElement,
+              `${selector} should be focused`);
+        });
   });
 });
--- chrome/test/data/webui/settings/chromeos/os_bluetooth_summary_tests.js
+++ chrome/test/data/webui/settings/chromeos/os_bluetooth_summary_tests.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {createDefaultBluetoothDevice, FakeBluetoothConfig,} from 'chrome://test/cr_components/chromeos/bluetooth/fake_bluetooth_config.js';
 // #import {setBluetoothConfigForTesting} from 'chrome://resources/cr_components/chromeos/bluetooth/cros_bluetooth_config.js';
 // #import {mojoString16ToString} from 'chrome://resources/cr_components/chromeos/bluetooth/bluetooth_utils.js';
-// #import {eventToPromise, waitAfterNextRender} from 'chrome://test/test_util.js';
+// #import {eventToPromise, waitBeforeNextRender, waitAfterNextRender} from 'chrome://test/test_util.js';
 // clang-format on
 
 suite('OsBluetoothSummaryTest', function() {
@@ -68,21 +68,25 @@ suite('OsBluetoothSummaryTest', function() {
     await flushAsync();
     const iconButton = bluetoothSummary.$$('#arrowIconButton');
     assertTrue(!!iconButton);
-    iconButton.click();
 
-    await flushAsync();
+    iconButton.click();
     assertEquals(
         settings.Router.getInstance().getCurrentRoute(),
         settings.routes.BLUETOOTH_DEVICES);
+    assertNotEquals(
+        iconButton, bluetoothSummary.shadowRoot.activeElement,
+        'subpage icon should not be focused');
 
     // Navigate back to the top-level page.
-    assertNotEquals(iconButton, bluetoothSummary.shadowRoot.activeElement);
-    const windowPopstatePromise = test_util.eventToPromise('popstate', window);
+    const windowPopstatePromise = eventToPromise('popstate', window);
     settings.Router.getInstance().navigateToPreviousRoute();
     await windowPopstatePromise;
+    await waitBeforeNextRender(bluetoothSummary);
 
     // Check that |iconButton| has been focused.
-    assertEquals(iconButton, bluetoothSummary.shadowRoot.activeElement);
+    assertEquals(
+        iconButton, bluetoothSummary.shadowRoot.activeElement,
+        'subpage icon should be focused');
   });
 
   test('Toggle button creation and a11y', async function() {
