35a8e9f5443f2c599dc3a083da65edd28ec8d06c
Ed Chin <edchin@google.com>
2022-03-08 17:14:50 +0000

    [iOS] Add formatting for FollowedWebChannelItem cell
    - Formats "Unavailable" line for WebChannels that are not available.
    
    Bug: 1296745
    Change-Id: I4f4693d1dbbcb0a688745663288c02573e756841
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497865
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: edchin <edchin@google.com>
    Cr-Commit-Position: refs/heads/main@{#978739}

--- ios/chrome/browser/ui/ntp/feed_management/followed_web_channel_item.h
+++ ios/chrome/browser/ui/ntp/feed_management/followed_web_channel_item.h
@@ -6,14 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_NTP_FEED_MANAGEMENT_FOLLOWED_WEB_CHANNEL_ITEM_H_
 #define IOS_CHROME_BROWSER_UI_NTP_FEED_MANAGEMENT_FOLLOWED_WEB_CHANNEL_ITEM_H_
 
-#import "ios/chrome/browser/ui/ntp/feed_management/web_channel.h"
-#import "ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_image_item.h"
+
+@class WebChannel;
 
 // A table view item representing a web channel.
-@interface FollowedWebChannelItem : TableViewDetailIconItem
+@interface FollowedWebChannelItem : TableViewImageItem
 
 // Web channel associated with this table view item.
-@property(nonatomic, strong) WebChannel* channel;
+@property(nonatomic, strong) WebChannel* webChannel;
 
 @end
 
