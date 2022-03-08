ddb2ebc0189a43336be24eab5e255c58910aff0e
Eric Willigers <ericwilligers@chromium.org>
2022-03-08 22:09:01 +0000

    Apps Shortcut not enabled for Lacros
    IsAppsShortcutEnabled() now returns false for Lacros,
    just like Ash.
    
    This stops the Show Apps button from appearing in the Lacros
    bookmarks bar: users can use the Launcher and Shelf instead.
    
    Bug: 1300288
    Change-Id: I6d388edbf814b3a3b5b910f2ca6c62b88ee2b0f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3504584
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978902}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view_unittest.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/app_list/app_list_util.h"
+#include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view_test_helper.h"
 #include "chrome/browser/ui/views/native_widget_factory.h"
@@ -221,13 +222,13 @@ TEST_F(BookmarkBarViewTest, AppsShortcutVisibility) {
   EXPECT_FALSE(test_helper_->apps_page_shortcut()->GetVisible());
 
   // Try to make the Apps shortcut visible. Its visibility depends on whether
-  // the app launcher is enabled.
+  // the Apps shortcut is enabled.
   browser()->profile()->GetPrefs()->SetBoolean(
       bookmarks::prefs::kShowAppsShortcutInBookmarkBar, true);
-  if (IsAppLauncherEnabled()) {
-    EXPECT_FALSE(test_helper_->apps_page_shortcut()->GetVisible());
-  } else {
+  if (chrome::IsAppsShortcutEnabled(browser()->profile())) {
     EXPECT_TRUE(test_helper_->apps_page_shortcut()->GetVisible());
+  } else {
+    EXPECT_FALSE(test_helper_->apps_page_shortcut()->GetVisible());
   }
 
   // Make sure we can also properly transition from true to false.
@@ -494,7 +495,7 @@ TEST_F(BookmarkBarViewTest, DropCallback_InvalidatePtrTest) {
   EXPECT_EQ(output_drag_op, ui::mojom::DragOperation::kNone);
 }
 
-#if !BUILDFLAG(IS_CHROMEOS_ASH)
+#if !BUILDFLAG(IS_CHROMEOS)
 // Verifies that the apps shortcut is shown or hidden following the policy
 // value. This policy (and the apps shortcut) isn't present on ChromeOS.
 TEST_F(BookmarkBarViewTest, ManagedShowAppsShortcutInBookmarksBar) {
