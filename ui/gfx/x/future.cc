6f6112274e73931eec7126d57c94acb39347c91a
Tom Anderson <thomasanderson@chromium.org>
2022-03-08 01:28:29 +0000

    [X11] Move Future::Wait to FutureBase
    Wait() is the only method that doesn't depend on the template type
    parameter, so it can be extracted out.  This will be necessary
    for WindowCache to store heterogeneous Futures in a data structure.
    
    R=sky
    
    Bug: 739898
    Change-Id: I663eb4d3054e5fb48b46e7087af646930ec0de96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508645
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978564}

---
+++ ui/gfx/x/future.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/x/future.h"
+
+namespace x11 {
+
+FutureBase::FutureBase() = default;
+
+FutureBase::FutureBase(std::unique_ptr<Connection::FutureImpl> impl)
+    : impl_(std::move(impl)) {}
+
+FutureBase::FutureBase(FutureBase&&) = default;
+
+FutureBase& FutureBase::operator=(FutureBase&&) = default;
+
+FutureBase::~FutureBase() = default;
+
+void FutureBase::Wait() {
+  if (impl_)
+    impl_->Wait();
+}
+
+}  // namespace x11
