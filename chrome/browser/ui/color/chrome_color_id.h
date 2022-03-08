c5ecf4220e49714dc786c75ad28686d3a80594f8
Peter Kasting <pkasting@chromium.org>
2022-03-08 01:57:59 +0000

    Make Answers in Suggest colors have sufficient contrast in dark mode.
    These are currently hardcoded in the parser.  This CL plumbs new theme
    colors and computes them where the other omnibox theme colors are
    computed.  Due to bug 1303190, these are not currently visible, but I
    verified with a local hack that the plumbing is working correctly for
    when that bug gets fixed.
    
    In preparation for defining these in the color pipeline world, this
    uses color pipeline-compatible definitions of the colors.
    
    Bug: none
    Change-Id: Ibfb531a0d47ae47967e887961dda1fad36a5f050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508296
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978574}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -98,6 +98,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     ThemeProperties::COLOR_OMNIBOX_RESULTS_BG_HOVERED) \
   E(kColorOmniboxResultsBackgroundSelected, \
     ThemeProperties::COLOR_OMNIBOX_RESULTS_BG_SELECTED) \
+  E(kColorOmniboxResultsButtonBorder, \
+    ThemeProperties::COLOR_OMNIBOX_RESULTS_BUTTON_BORDER) \
   E(kColorOmniboxResultsIcon, ThemeProperties::COLOR_OMNIBOX_RESULTS_ICON) \
   E(kColorOmniboxResultsIconSelected, \
     ThemeProperties::COLOR_OMNIBOX_RESULTS_ICON_SELECTED) \
--- chrome/browser/ui/views/omnibox/omnibox_row_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_row_view.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/gfx/color_palette.h"
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/accessibility/view_accessibility.h"
