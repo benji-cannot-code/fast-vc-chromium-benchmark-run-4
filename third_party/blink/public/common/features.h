85fde21f9455e5ca67f9ad0b180be429a2443e3a
Titouan Rigoudy <titouan@chromium.org>
2022-03-08 20:38:57 +0000

    [Fixit] Control secure context for workers fix using Finch.
    This will allow enabling it by default in a follow-up CL (which can be
    independently reverted for maximum ease) and rolling things back using
    Finch if the launch encounters unexpected trouble.
    
    Bug: chromium:780031
    Change-Id: Idf2a2254592ad6107988b36d85538621f32f9dd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497437
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978834}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -664,6 +664,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kEventPath;
 // If enabled, the minor version of the User-Agent string will be reduced.
 BLINK_COMMON_EXPORT extern const base::Feature kReduceUserAgentMinorVersion;
 
+BLINK_COMMON_EXPORT extern const base::Feature kSecureContextFixForWorkers;
+
 }  // namespace features
 }  // namespace blink
 
