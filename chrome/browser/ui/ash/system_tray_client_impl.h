a1d17f23986b74fa8429a8419e6fc36e6e40318f
Thanh Nguyen <thanhdng@chromium.org>
2022-03-08 23:24:23 +0000

    [Snooping protection] Add buttons to HPS notification
    This CL add two buttons to the HPS notification.
    
    Change-Id: I9cd9d325241d101af8e0a2a9ce32ebd365bd146e
    Bug: 1298327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3493161
    Reviewed-by: Michael Martis <martis@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978948}

--- chrome/browser/ui/ash/system_tray_client_impl.h
+++ chrome/browser/ui/ash/system_tray_client_impl.h
@@ -72,6 +72,7 @@ class SystemTrayClientImpl : public ash::SystemTrayClient,
   void ShowStorageSettings() override;
   void ShowPowerSettings() override;
   void ShowPrivacyAndSecuritySettings() override;
+  void ShowSmartPrivacySettings() override;
   void ShowChromeSlow() override;
   void ShowIMESettings() override;
   void ShowConnectedDevicesSettings() override;
