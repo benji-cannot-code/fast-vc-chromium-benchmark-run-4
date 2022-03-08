802eec81ec29d96e6f52a9986db45186a48c70c0
Yi Gu <yigu@chromium.org>
2022-03-08 07:59:26 +0000

    [FedCM] Fix Null-dereference READ due to race condition
    |Complete[Revoke]Request| may get called in two scenarios:
    1. through nature flow (success, failure etc.)
    2. through DelayedTimer exact 1 min after the API all
    
    This may introduce a race condition: #1 gets triggered after 1 minute
    plus some small time delta, i.e. #2 is already triggered and the
    |network_manager_| has been reset.
    
    Bug: 1302727
    Change-Id: I9f4d37862d16bcee6bcb1facdf115737cde608f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502533
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978639}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -550,15 +550,15 @@ void FederatedAuthRequestImpl::OnRevokeResponse(
 void FederatedAuthRequestImpl::CompleteRevokeRequest(
     RevokeStatus status,
     bool should_call_callback) {
+  if (!revoke_callback_)
+    return;
+
   bool should_run_callback = should_call_callback ||
                              network_manager_->IsMockIdpNetworkRequestManager();
   network_manager_.reset();
   provider_ = GURL();
   account_id_ = std::string();
   client_id_ = std::string();
-
-  if (!revoke_callback_)
-    return;
 
   if (should_run_callback)
     std::move(revoke_callback_).Run(status);
@@ -930,6 +930,9 @@ void FederatedAuthRequestImpl::CompleteRequest(
     bool should_call_callback) {
   DCHECK(result == FederatedAuthRequestResult::kSuccess || id_token.empty());
 
+  if (!auth_request_callback_)
+    return;
+
   if (result != FederatedAuthRequestResult::kSuccess) {
     // It would be possible to add this inspector issue on the renderer, which
     // will receive the callback. However, it is preferable to do so on the
@@ -945,9 +948,6 @@ void FederatedAuthRequestImpl::CompleteRequest(
   bool should_run_callback = should_call_callback ||
                              network_manager_->IsMockIdpNetworkRequestManager();
   CleanUp();
-
-  if (!auth_request_callback_)
-    return;
 
   if (should_run_callback) {
     RequestIdTokenStatus status =
