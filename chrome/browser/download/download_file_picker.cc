0cb201de9387be78b32e7f731789ea1aba6d2b20
Jun Wang <wangjuna@uniontech.com>
2022-03-08 05:08:14 +0000

    Fix select file dialog is not modal dialog when it issued by extension download API
    In Linux OS, when extension use API chrome.downloads.download to popop a select file dialog,
    in class DownloadFilePicker::DownloadFilePicker, the select file dialog owning_window host is NULL.
    Then in fucntion SelectFileDialogImplGTK::SelectFileImpl, it can't set the select file dialog
    as a modal dialog. So we make the owning_window host to browser current active window if it is NULL.
    
    Bug: 1301898
    Change-Id: Iae451bf4aa18204f10ee067f9bab3d512bdd13e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500637
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978608}

--- chrome/browser/download/download_file_picker.cc
+++ chrome/browser/download/download_file_picker.cc
@@ -15,6 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/web_contents.h"
 
+#if BUILDFLAG(IS_LINUX)
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_window.h"
+#include "ui/aura/window.h"
+#endif
+
 using download::DownloadItem;
 using content::DownloadManager;
 using content::WebContents;
@@ -62,6 +68,18 @@ DownloadFilePicker::DownloadFilePicker(DownloadItem* item,
   gfx::NativeWindow owning_window =
       web_contents ? platform_util::GetTopLevel(web_contents->GetNativeView())
                    : gfx::kNullNativeWindow;
+
+  // If select_file_dialog_ issued by extension API,
+  // (e.g. chrome.downloads.download), the |owning_window| host
+  // could be null, then it will cause the select file dialog is not modal
+  // dialog in Linux. See SelectFileImpl() in select_file_dialog_linux_gtk.cc.
+  // Here we make owning_window host to browser current active window
+  // if it is null. https://crbug.com/1301898
+#if BUILDFLAG(IS_LINUX)
+  if (!owning_window || !owning_window->GetHost())
+    owning_window =
+        chrome::FindBrowserWithActiveWindow()->window()->GetNativeWindow();
+#endif
 
   select_file_dialog_->SelectFile(
       ui::SelectFileDialog::SELECT_SAVEAS_FILE, std::u16string(),
