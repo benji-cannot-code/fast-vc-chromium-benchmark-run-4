3382ae658331a50818f142e5b950ce57abcf6b57
Ahmed Nasr <anasr@google.com>
2022-03-08 23:15:24 +0000

    Observe kiosk profile load in ManagedSessionService.
    Bug: b:218751747
    Change-Id: Ic610630be31f9599d0d313703a86cf09532a0d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499169
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#978941}

--- chrome/browser/ash/policy/status_collector/managed_session_service.cc
+++ chrome/browser/ash/policy/status_collector/managed_session_service.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/status_collector/managed_session_service.h"
 
 #include "base/logging.h"
+#include "chrome/browser/ash/app_mode/kiosk_app_launch_error.h"
 #include "chrome/browser/ash/login/existing_user_controller.h"
+#include "chrome/browser/ash/login/ui/login_display_host.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "components/user_manager/user_manager.h"
 
@@ -15,6 +17,11 @@ namespace policy {
 namespace {
 
 constexpr base::TimeDelta kMinimumSuspendDuration = base::Minutes(1);
+
+ash::KioskLaunchController* GetKioskLaunchController() {
+  auto* host = ash::LoginDisplayHost::default_host();
+  return host ? host->GetKioskLaunchController() : nullptr;
+}
 
 }  // namespace
 
@@ -39,6 +46,10 @@ ManagedSessionService::~ManagedSessionService() {
   if (ash::ExistingUserController::current_controller()) {
     ash::ExistingUserController::current_controller()
         ->RemoveLoginStatusConsumer(this);
+  }
+
+  if (GetKioskLaunchController()) {
+    GetKioskLaunchController()->RemoveKioskProfileLoadFailedObserver(this);
   }
 
   if (ash::SessionTerminationManager::Get()) {
@@ -146,11 +157,25 @@ void ManagedSessionService::OnAuthAttemptStarted() {
     ash::ExistingUserController::current_controller()->AddLoginStatusConsumer(
         this);
   }
+
+  if (GetKioskLaunchController()) {
+    // Remove observer first in case the auth attempt is because of a retry, and
+    // the observation was added, if it was not added removing the observer will
+    // be a no-op.
+    GetKioskLaunchController()->RemoveKioskProfileLoadFailedObserver(this);
+    GetKioskLaunchController()->AddKioskProfileLoadFailedObserver(this);
+  }
 }
 
 void ManagedSessionService::OnAuthFailure(const ash::AuthFailure& error) {
   for (auto& observer : observers_) {
     observer.OnLoginFailure(error);
+  }
+}
+
+void ManagedSessionService::OnKioskProfileLoadFailed() {
+  for (auto& observer : observers_) {
+    observer.OnKioskLoginFailure();
   }
 }
 
