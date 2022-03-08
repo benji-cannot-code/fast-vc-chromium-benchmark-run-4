fec32256b0d0109b61a6449422254fbe0bc171e2
Zhiyuan Cai <zhiyuancai@chromium.org>
2022-03-08 01:49:59 +0000

    [Sheriff] Disable two tests in UrlOverridingTest
    Disable UrlOverridingTest#testNavigationFromXHRCallbackInSubFrame and
    UrlOverridingTest#testNavigationFromUserGestureInSubFrame.
    
    Bug: 1303626
    Change-Id: If890d3f5f73073f774606e613366d293b085dff1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507973
    Owners-Override: Zhiyuan Cai <zhiyuancai@chromium.org>
    Auto-Submit: Zhiyuan Cai <zhiyuancai@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978572}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -49,6 +49,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -419,6 +420,7 @@ public class UrlOverridingTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1303626")
     public void testNavigationFromUserGestureInSubFrame() {
         mActivityTestRule.startMainActivityOnBlankPage();
         loadUrlAndWaitForIntentUrl(
@@ -435,6 +437,7 @@ public class UrlOverridingTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/1303626")
     public void testNavigationFromXHRCallbackInSubFrame() {
         mActivityTestRule.startMainActivityOnBlankPage();
         loadUrlAndWaitForIntentUrl(
