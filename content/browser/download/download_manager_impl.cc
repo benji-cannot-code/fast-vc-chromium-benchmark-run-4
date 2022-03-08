8b7c13ebce36005c48ff4cbd91ad12c8082c1797
Lei Zhang <thestig@chromium.org>
2022-03-08 03:58:44 +0000

    Remove some redundant lookups in download_manager_impl.cc.
    Instead of calling base::Contains(container, key) and container[key] to
    do 2 lookups back-to-back, reuse the result of container.find(key).
    
    Change-Id: I720af69076e21a63716260cbe83506953d999b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3504801
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978597}

--- content/browser/download/download_manager_impl.cc
+++ content/browser/download/download_manager_impl.cc
@@ -834,8 +834,9 @@ void DownloadManagerImpl::OnFileExistenceChecked(uint32_t download_id,
   pending_disk_access_query_.erase(download_id);
 
   if (!result) {  // File does not exist.
-    if (base::Contains(downloads_, download_id))
-      downloads_[download_id]->OnDownloadedFileRemoved();
+    auto it = downloads_.find(download_id);
+    if (it != downloads_.end())
+      it->second->OnDownloadedFileRemoved();
   }
 }
 
@@ -1259,8 +1260,8 @@ int DownloadManagerImpl::NonMaliciousInProgressCount() {
 }
 
 download::DownloadItem* DownloadManagerImpl::GetDownload(uint32_t download_id) {
-  return base::Contains(downloads_, download_id) ? downloads_[download_id].get()
-                                                 : nullptr;
+  auto it = downloads_.find(download_id);
+  return it != downloads_.end() ? it->second.get() : nullptr;
 }
 
 download::DownloadItem* DownloadManagerImpl::GetDownloadByGuid(
@@ -1271,8 +1272,8 @@ download::DownloadItem* DownloadManagerImpl::GetDownloadByGuid(
         return it.get();
     }
   }
-  return base::Contains(downloads_by_guid_, guid) ? downloads_by_guid_[guid]
-                                                  : nullptr;
+  auto it = downloads_by_guid_.find(guid);
+  return it != downloads_by_guid_.end() ? it->second : nullptr;
 }
 
 void DownloadManagerImpl::GetAllDownloads(
