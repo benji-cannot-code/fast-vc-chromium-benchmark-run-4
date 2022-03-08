4d151bc82283fff481e6a6091774934c4d968101
Hailey Wang <haileywang@google.com>
2022-03-08 00:37:40 +0000

    [Feature Processor] Move error check to feature list processor
    Bug: 1302140
    Change-Id: Ib5acb4027405cec3e19389bcf44fbc1184dc79a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498225
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#978540}

--- components/segmentation_platform/internal/execution/feature_processor_state.cc
+++ components/segmentation_platform/internal/execution/feature_processor_state.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/segmentation_platform/internal/execution/feature_processor_state.h"
 
+#include "components/segmentation_platform/internal/database/ukm_types.h"
+
 namespace segmentation_platform {
 
 FeatureProcessorState::FeatureProcessorState(
@@ -43,6 +45,22 @@ void FeatureProcessorState::RunCallback() {
 
 void FeatureProcessorState::AppendInputTensor(const std::vector<float>& data) {
   input_tensor_.insert(input_tensor_.end(), data.begin(), data.end());
+}
+
+void FeatureProcessorState::AppendInputTensor(
+    const std::vector<ProcessedValue>& data) {
+  std::vector<float> tensor_result;
+  for (auto& value : data) {
+    if (value.type == ProcessedValue::Type::FLOAT) {
+      tensor_result.push_back(value.float_val);
+    } else {
+      SetError();
+      return;
+    }
+  }
+
+  input_tensor_.insert(input_tensor_.end(), tensor_result.begin(),
+                       tensor_result.end());
 }
 
 }  // namespace segmentation_platform
