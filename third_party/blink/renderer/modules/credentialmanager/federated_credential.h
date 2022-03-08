18e3749804d18569d9b3abefb86f9f9c27bf9255
Christian Biesinger <cbiesinger@chromium.org>
2022-03-08 22:07:37 +0000

    [FedCM] Make revoke a non-static method
    Bug: 1300009
    Change-Id: I4d521b36a1d8cc37cdea33e4e36ffc999e6e9c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502696
    Reviewed-by: Dan Sinclair <dsinclair@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978899}

--- third_party/blink/renderer/modules/credentialmanager/federated_credential.h
+++ third_party/blink/renderer/modules/credentialmanager/federated_credential.h
@@ -19,7 +19,6 @@ namespace blink {
 class CredentialRequestOptions;
 class FederatedAccountLoginRequest;
 class FederatedCredentialInit;
-class FederatedIdentityProvider;
 
 class MODULES_EXPORT FederatedCredential final : public Credential {
   DEFINE_WRAPPERTYPEINFO();
@@ -73,13 +72,11 @@ class MODULES_EXPORT FederatedCredential final : public Credential {
 
   ScriptPromise logout();
 
+  ScriptPromise revoke(ScriptState*, ExceptionState&);
+
   static ScriptPromise logoutRps(
       ScriptState*,
       const HeapVector<Member<FederatedCredentialLogoutRpsRequest>>&);
-  static ScriptPromise revoke(ScriptState*,
-                              const String&,
-                              FederatedIdentityProvider*,
-                              ExceptionState&);
 
  private:
   const scoped_refptr<const SecurityOrigin> provider_origin_;
