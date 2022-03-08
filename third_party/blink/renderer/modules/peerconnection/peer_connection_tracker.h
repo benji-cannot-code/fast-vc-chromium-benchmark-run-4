213d36c8a4527b8b94af06d401149b6419a80430
Scott Haseley <shaseley@chromium.org>
2022-03-08 22:03:17 +0000

    CodeHealth: Use v2 Dict and List APIs in PeerConnectionTracker
    This changes usage of base::Value with type DICTIONARY to
    base::Value::Dict, as well as converts related methods and data to use
    the v2 APIs (Dict and List) for better type safety. The stat reports,
    which this CL converts to a Value::List, are ultimately plumbed through
    mojo using mojo_base.mojom.ListValue. We convert to a base::Value at
    that boundary, which will be converted as part of crbug.com/1301890.
    
    Change-Id: I382f1bb146d043677729649b4e54cec11056d87b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3504326
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#978891}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.h
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/threading/thread_checker.h"
 #include "base/types/pass_key.h"
+#include "base/values.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/peerconnection/peer_connection_tracker.mojom-blink.h"
@@ -312,8 +313,8 @@ class MODULES_EXPORT PeerConnectionTracker
                                 const String& callback_type,
                                 const String& value);
 
-  void AddStandardStats(int lid, base::Value value);
-  void AddLegacyStats(int lid, base::Value value);
+  void AddStandardStats(int lid, base::Value::List value);
+  void AddLegacyStats(int lid, base::Value::List value);
 
   // This map stores the local ID assigned to each RTCPeerConnectionHandler.
   typedef WTF::HashMap<RTCPeerConnectionHandler*, int> PeerConnectionLocalIdMap;
