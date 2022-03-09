a92c13d65022451ecc43ea95e1e071592a230f34
Andy Paicu <andypaicu@chromium.org>
2022-03-09 23:29:52 +0000

    Put IdleDetection permission behind the IdleDetection feature policy
    Fixed: 1301341
    
    Change-Id: I2d3489fd100b8b9fc04c9e3387b79279d83eae23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3501700
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979486}

--- chrome/browser/idle/idle_detection_permission_context.cc
+++ chrome/browser/idle/idle_detection_permission_context.cc
@@ -17,10 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 IdleDetectionPermissionContext::IdleDetectionPermissionContext(
     content::BrowserContext* browser_context)
-    : PermissionContextBase(browser_context,
-                            ContentSettingsType::IDLE_DETECTION,
-                            blink::mojom::PermissionsPolicyFeature::kNotFound) {
-}
+    : PermissionContextBase(
+          browser_context,
+          ContentSettingsType::IDLE_DETECTION,
+          blink::mojom::PermissionsPolicyFeature::kIdleDetection) {}
 
 IdleDetectionPermissionContext::~IdleDetectionPermissionContext() = default;
 
