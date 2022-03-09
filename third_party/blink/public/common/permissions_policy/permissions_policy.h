2b99338ce2896844afca6bf2f9bb4be2f89ecd6e
Jarryd <jarrydg@chromium.org>
2022-03-09 15:41:20 +0000

    PermissionsPolicy: Create policy from manifest policy.
    * Add a method to create a PermissionsPolicy object from a
    ParsedPermissionsPolicy.
    * Add a new member onto PermissionsPolicy, allowlists_set_by_manifest_,
    which will be set to true if the policy was create from a manifest
    declared policy. This will be used to avoid applying policies declared
    in headers.
    
    Bug: 1238341
    Change-Id: I32dd69f05a7a4ee2faeaefd385172d5f126f0bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3490091
    Auto-Submit: Jarryd Goodman <jarrydg@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979259}

--- third_party/blink/public/common/permissions_policy/permissions_policy.h
+++ third_party/blink/public/common/permissions_policy/permissions_policy.h
@@ -187,6 +187,10 @@ class BLINK_COMMON_EXPORT PermissionsPolicy {
   static std::unique_ptr<PermissionsPolicy> CreateForFencedFrame(
       const url::Origin& origin);
 
+  static std::unique_ptr<PermissionsPolicy> CreateFromParsedPolicy(
+      const ParsedPermissionsPolicy& parsed_policy,
+      const url::Origin& origin);
+
   bool IsFeatureEnabled(mojom::PermissionsPolicyFeature feature) const;
 
   // Returns whether or not the given feature is enabled by this policy for a
@@ -243,6 +247,11 @@ class BLINK_COMMON_EXPORT PermissionsPolicy {
       const url::Origin& origin,
       const PermissionsPolicyFeatureList& features);
 
+  static std::unique_ptr<PermissionsPolicy> CreateFromParsedPolicy(
+      const ParsedPermissionsPolicy& parsed_policy,
+      const url::Origin& origin,
+      const PermissionsPolicyFeatureList& features);
+
   static std::unique_ptr<PermissionsPolicy> CreateForFencedFrame(
       const url::Origin& origin,
       const PermissionsPolicyFeatureList& features);
@@ -266,6 +275,10 @@ class BLINK_COMMON_EXPORT PermissionsPolicy {
 
   // Set this to true if `allowlists_` have already been checked.
   mutable bool allowlists_checked_;
+
+  // Set this to true if `allowlists_` was populated by web app manifest
+  // declared permissions policy.
+  bool allowlists_set_by_manifest_{false};
 
   // Records whether or not each feature was enabled for this frame by its
   // parent frame.
