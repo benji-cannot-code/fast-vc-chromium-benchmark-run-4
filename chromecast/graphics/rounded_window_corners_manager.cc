a68f8f2f6998876afb7dbea7ecdd57250eba699b
Ayu Ishii <ayui@chromium.org>
2022-03-09 22:09:03 +0000

    CodeHealth: Use base::Reversed() in chromecast
    Bug: 1287358
    Change-Id: Ib8aea3c51ff55abf0592a378b89ec22fb85123de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499569
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979446}

--- chromecast/graphics/rounded_window_corners_manager.cc
+++ chromecast/graphics/rounded_window_corners_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <unordered_set>
 
+#include "base/containers/adapters.h"
 #include "base/containers/contains.h"
 #include "base/strings/string_number_conversions.h"
 #include "chromecast/graphics/cast_window_manager.h"
@@ -29,10 +30,9 @@ aura::Window* FindTopmostVisibleNonCornersWindow(
   if (base::Contains(observed_container_windows, window))
     return window;
 
-  for (auto it = window->children().rbegin(); it != window->children().rend();
-       ++it) {
+  for (auto* child : base::Reversed(window->children())) {
     found_window =
-        FindTopmostVisibleNonCornersWindow(*it, observed_container_windows);
+        FindTopmostVisibleNonCornersWindow(child, observed_container_windows);
     if (found_window)
       return found_window;
   }
