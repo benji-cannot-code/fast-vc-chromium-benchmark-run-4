e5b9a6edbfecf68ea213e81c5a94823a43e3eaa8
Hong Xu <xuhong@chromium.org>
2022-03-09 01:35:43 +0000

    Hide `ReportingClient::TestEnvironment` from header
    This class is for test purpose only and should not be included as a
    public member in a public header.
    
    I moved `ReportingClient::TestEnvironment` to `reporting_client_test_util.h`.
    
    Bug: b:222592438
    Change-Id: I40bd65df79567b1fefe46058be7cf976eadc8e4b
    Fixed: b:222592438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508959
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#979012}

--- chrome/browser/policy/messaging_layer/public/report_client_unittest.cc
+++ chrome/browser/policy/messaging_layer/public/report_client_unittest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/policy/messaging_layer/public/report_client.h"
+#include "chrome/browser/policy/messaging_layer/public/report_client_test_util.h"
 
 #include <memory>
 #include <string>
