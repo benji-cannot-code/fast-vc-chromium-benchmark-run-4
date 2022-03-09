74c76a63713480d8daec5053c18e22ed4312cc35
Nancy Wang <nancylingwang@chromium.org>
2022-03-09 02:31:32 +0000

    Modify AppServiceAppResult::app_type_ to use the non mojom AppType.
    This CL is used to prepare for CL:3509514 to return the non mojom
    AppType for AppUpdate::AppType.
    
    BUG=1253250
    
    Change-Id: Ib32c3d3a3e2dca6932498b99d8639fb876e8de3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3509612
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979038}

--- chrome/browser/ui/app_list/search/app_service_app_result.h
+++ chrome/browser/ui/app_list/search/app_service_app_result.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/ui/app_list/search/app_result.h"
 #include "components/favicon_base/favicon_types.h"
+#include "components/services/app_service/public/cpp/app_types.h"
 #include "components/services/app_service/public/cpp/icon_cache.h"
 #include "components/services/app_service/public/cpp/icon_types.h"
 #include "components/services/app_service/public/mojom/types.mojom-forward.h"
@@ -85,7 +86,7 @@ class AppServiceAppResult : public AppResult {
   // the last icon is no longer used.
   std::unique_ptr<apps::IconLoader::Releaser> icon_loader_releaser_;
 
-  apps::mojom::AppType app_type_;
+  apps::AppType app_type_;
   bool is_platform_app_;
   bool show_in_launcher_;
 
