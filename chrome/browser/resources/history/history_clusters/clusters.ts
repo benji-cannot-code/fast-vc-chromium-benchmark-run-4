042d35a63e9da9c28460c6548e23f7dc0c96eb0b
Marlon Facey <mfacey@chromium.org>
2022-03-09 03:32:48 +0000

    [journeys] Show circular loading spinner when page is loading content
    Added a spinner that only displays after the button has been clicked
    and disappears when the results are finished loading.
    
    Bug: 1294324
    Change-Id: I90a8c2c4265684e3da2f4758003eaa2644999817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3511492
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979065}

--- chrome/browser/resources/history/history_clusters/clusters.ts
+++ chrome/browser/resources/history/history_clusters/clusters.ts
@@ -96,6 +96,14 @@ class HistoryClustersElement extends PolymerElement {
       result_: Object,
 
       /**
+       * Boolean determining if spinner shows instead of load more button.
+       */
+      showSpinner_: {
+        type: Boolean,
+        value: false,
+      },
+
+      /**
        * The list of visits to be removed. A non-empty array indicates a pending
        * remove request to the browser.
        */
@@ -116,6 +124,7 @@ class HistoryClustersElement extends PolymerElement {
   private onVisitsRemovedListenerId_: number|null = null;
   private pageHandler_: PageHandlerRemote;
   private result_: QueryResult;
+  private showSpinner_: boolean;
   private visitsToBeRemoved_: Array<URLVisit>;
 
   //============================================================================
@@ -172,6 +181,7 @@ class HistoryClustersElement extends PolymerElement {
 
   private onLoadMoreButtonClick_() {
     if (this.result_ && this.result_.canLoadMore) {
+      this.showSpinner_ = true;
       // Prevent sending further load-more requests until this one finishes.
       this.set('result_.canLoadMore', false);
       this.pageHandler_.loadMoreClusters(this.result_.query);
@@ -322,6 +332,7 @@ class HistoryClustersElement extends PolymerElement {
         this.onLoadMoreButtonClick_();
       }
     });
+    this.showSpinner_ = false;
   }
 
   /**
