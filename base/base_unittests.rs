4028ebbc295f4b44c3b74068bb9d71788a53862f
danakj <danakj@chromium.org>
2022-03-09 16:36:29 +0000

    Move tests for the base crate into base_unittests
    This makes use of the #[gtest] macro to define Rust tests inside the
    base_unittests binary. The tests use the base crate's public API (which
    includes a ForTesting function).
    
    This will enable the tests on the Rust FYI bots, as well as
    demonstrate a usage of cross-component Rust code in a component build.
    
    As there are now 2 crate roots in //base, one for prod and one for unit
    tests, we rename base/lib.rs to base/base.rs in order to make it more
    clear.
    
    Any assert_eq!() macros are converted to the gtest-friendly expect_eq!()
    in order to not take down the whole test suite on a failure.
    
    We make the base and base_unittests crate roots always exist (when
    enable_rust=true), and then conditionally enable the JSON parser and
    Values support when the build_rust_json_parser buildflag is enabled,
    with some TODOs around proper buildflag support. The Value support
    should move out of the JSON parser's buildflag later.
    
    And we avoid building the Rust part of base_unittests on IOS since
    the IOS test() templates do not support mixing in Rust with C++ yet.
    
    After this change, base_unittests includes (when rust is enabled):
    Note: Google Test filter = Rust*
    [==========] Running 7 tests from 2 test suites.
    [----------] Global test environment set-up.
    [----------] 6 tests from RustValuesTest
    [ RUN      ] RustValuesTest.SetDict
    [       OK ] RustValuesTest.SetDict (0 ms)
    [ RUN      ] RustValuesTest.SetList
    [       OK ] RustValuesTest.SetList (0 ms)
    [ RUN      ] RustValuesTest.ReuseSlot
    [       OK ] RustValuesTest.ReuseSlot (0 ms)
    [ RUN      ] RustValuesTest.StartsNone
    [       OK ] RustValuesTest.StartsNone (0 ms)
    [ RUN      ] RustValuesTest.SetSimpleOptionalValues
    [       OK ] RustValuesTest.SetSimpleOptionalValues (0 ms)
    [ RUN      ] RustValuesTest.AllocDealloc
    [       OK ] RustValuesTest.AllocDealloc (0 ms)
    [----------] 6 tests from RustValuesTest (0 ms total)
    
    [----------] 1 test from RustJsonParserTest
    [ RUN      ] RustJsonParserTest.DecodeJson
    [       OK ] RustJsonParserTest.DecodeJson (0 ms)
    [----------] 1 test from RustJsonParserTest (0 ms total)
    
    [----------] Global test environment tear-down
    [==========] 7 tests from 2 test suites ran. (0 ms total)
    [  PASSED  ] 7 tests.
    
    R=adetaylor@chromium.org
    
    Bug: 1293979, 1296156, 1304253
    Change-Id: I3c0b9c7724fac72e33aba635c4a7e96d9a76b0d4
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm-rel,linux-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3509990
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979275}

---
+++ base/base_unittests.rs
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(crbug.com/1298039): A buildflag macro should be used instead.
+#[cfg(buildflag__build_rust_json_parser)]
+#[path = "values_unittest.rs"]
+mod values_unittest;
+
+// TODO(crbug.com/1298039): A buildflag macro should be used instead.
+#[cfg(buildflag__build_rust_json_parser)]
+#[path = "json/json_parser_unittest.rs"]
+mod json_parser_unittest;
