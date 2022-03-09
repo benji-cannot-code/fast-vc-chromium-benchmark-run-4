04142d39fe6083f9e6461c5f2ef0979b6a347bc3
Daseul Lee <dslee@chromium.org>
2022-03-09 02:16:14 +0000

    Simplify the testing logic for Font Access API blob.
    Change-Id: Iafc132a0605df9d26553a2a0cead8a4f30eb2cbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499261
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979030}

---
+++ third_party/blink/web_tests/external/wpt/font-access/font_access_permission.tentative.https.window.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//META: script=/resources/testdriver.js
+//META: script=/resources/testdriver-vendor.js
+
+'use strict';
+
+promise_test(async t => {
+  await promise_rejects_dom(t, 'NotAllowedError', navigator.fonts.query());
+}, 'query(): permission not given');
+
+promise_test(async t => {
+  await test_driver.set_permission({name: 'font-access'}, 'denied');
+  await promise_rejects_dom(t, 'NotAllowedError', navigator.fonts.query());
+}, 'query(): permission denied');
+
+promise_test(async t => {
+  await test_driver.set_permission({name: 'font-access'}, 'granted');
+  const fonts = await navigator.fonts.query();
+  assert_greater_than_equal(
+      fonts.length, 1, 'Fonts are returned with permission granted.');
+}, 'query(): permission granted');
