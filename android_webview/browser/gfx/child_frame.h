129484c154c0f2182d5488a00761417d282dfbbf
Vasiliy Telezhnikov <vasilyt@chromium.org>
2022-03-09 19:42:25 +0000

    aw: Plumb BeginFrameArgs to ChildFrame
    This CL adds plubming of BeginFrameArgs for current frame to ChildFrame.
    There are no functional changes.
    
    Bug: 1233542
    Change-Id: I2806ac4a497db5ab7c96a72efffa71bd36a38fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3484782
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979370}

--- android_webview/browser/gfx/child_frame.h
+++ android_webview/browser/gfx/child_frame.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/circular_deque.h"
+#include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/surfaces/frame_sink_id.h"
 #include "components/viz/common/surfaces/surface_id.h"
 #include "content/public/browser/android/synchronous_compositor.h"
@@ -37,7 +38,8 @@ class ChildFrame {
       bool offscreen_pre_raster,
       float device_scale_factor,
       CopyOutputRequestQueue copy_requests,
-      bool did_invalidate);
+      bool did_invalidate,
+      const viz::BeginFrameArgs& begin_frame_args);
 
   ChildFrame(const ChildFrame&) = delete;
   ChildFrame& operator=(const ChildFrame&) = delete;
@@ -67,6 +69,13 @@ class ChildFrame {
 
   // Used for metrics, indicates that we invalidated for this frame.
   const bool did_invalidate;
+
+  // Latest BeginFrameArgs this frame is being presented for so far. Normally
+  // this corresponds to the begin frame of the current draw cycle (BeginFrame
+  // => DrawOnUI => Sync => DrawOnRT), but in cases when DrawOnRT doesn't happen
+  // (e.g webview is offscreen) this will be updated to more recent draws.
+  // See: `HardwareRenderer::WaitAndPruneFrameQueue()` for details.
+  viz::BeginFrameArgs begin_frame_args;
 };
 
 using ChildFrameQueue = base::circular_deque<std::unique_ptr<ChildFrame>>;
