2220f95b0b3a0099d36863e985e66bb429f013a0
Abhijeet Kandalkar <abhijeet@igalia.com>
2022-03-09 14:42:55 +0000

    [mparch] Use PrimaryPageChanged in HandoffActiveURLObserver
    This CL,
    - Replaces DidFinishNavigation with PrimaryPageChanged in
    HandoffActiveURLObserver
    - Adds the test to verify that as a user switches from the main page to
    prerendered page, the correct URL is being passed to the Handoff.
    
    Bug: 1286316
    Change-Id: I3650ee79cf1870e2135e20a16036b1b811952a90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3395413
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#979233}

--- chrome/browser/ui/cocoa/handoff_active_url_observer.h
+++ chrome/browser/ui/cocoa/handoff_active_url_observer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 
 namespace content {
+class Page;
 class WebContents;
 }
 
@@ -43,8 +44,7 @@ class HandoffActiveURLObserver : public BrowserListObserver,
       const TabStripSelectionChange& selection) override;
 
   // content::WebContentsObserver
-  void DidFinishNavigation(
-      content::NavigationHandle* navigation_handle) override;
+  void PrimaryPageChanged(content::Page& page) override;
 
   // Updates the active browser.
   void SetActiveBrowser(Browser* active_browser);
