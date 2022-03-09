f643bc525c68e531b5b04548e252d947b9dc9fe7
Irem Uguz <iremuguz@google.com>
2022-03-09 11:48:39 +0000

    support-tool: Add email address option and description to UI
    Add optional email address and issue description to Support Tool UI as
    specified in the mocks. Add tests for the new UI elements.
    Rest of the UI will be added in follow-up CLs.
    
    UI looks like this after this change: https://drive.google.com/file/d/1V_jNHR5_I2PPu2nA_CW88UJJkYVntU1W/view?usp=sharing&resourcekey=0-vESmyOHALNWHWw9qZOAvrA
    UI mocks: https://docs.google.com/presentation/d/1XT3CvqqDHy6E0F1_PJwQZMYG_dukKsne1eMQ9E1y-xE/edit?hl=en&forcehl=1#slide=id.8dTFyXz
    
    Bug:b:219730556
    Test:Run browser test with "autoninja -C out/Default/ browser_tests &&
    out/Default/browser_tests --gtest_filter=*SupportToolTest*" command
    
    Change-Id: Id8986ac86fb11eebf3b25f4872da60e3816281c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500414
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#979177}

---
+++ chrome/browser/resources/support_tool/browser_proxy.ts
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
+
+export interface BrowserProxy {
+  /**
+   * Gets the list of email addresses that are logged in from C++ side.
+   */
+  getEmailAddresses(): Promise<string[]>;
+}
+
+export class BrowserProxyImpl implements BrowserProxy {
+  getEmailAddresses() {
+    return sendWithPromise('getEmailAddresses');
+  }
+
+  static getInstance(): BrowserProxy {
+    return instance || (instance = new BrowserProxyImpl());
+  }
+
+  static setInstance(obj: BrowserProxy) {
+    instance = obj;
+  }
+}
+
+// The singleton instance_ is replaced with a test version of this wrapper
+// during testing.
+let instance: BrowserProxy|null = null;
