4f3130aec978c889b14d2058ce8002291b09a3f8
Aaron Tagliaboschi <aarontag@chromium.org>
2022-03-09 22:48:40 +0000

    Update documentation and test for chrome.webviewTag.setUserAgentOverride
    When the User-Agent string is overridden without an accompanying
    UserAgentMetadata, User-Agent Client Hint request headers will now with
    blank values (as opposed to not sending the headers at all). The
    documentation and tests for chrome.webviewTag.setUserAgentOverride have
    been updated to reflect this change.
    
    Bug: 1240171
    Change-Id: Ic31f7ed0abc57d98ccd4a4a1c1dcd3d12338064a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500976
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979469}

--- content/public/test/mock_client_hints_controller_delegate.h
+++ content/public/test/mock_client_hints_controller_delegate.h
@@ -3,17 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_TEST_MOCK_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
-#define CONTENT_TEST_MOCK_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
+#ifndef CONTENT_PUBLIC_TEST_MOCK_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
+#define CONTENT_PUBLIC_TEST_MOCK_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
 
 #include "content/public/browser/client_hints_controller_delegate.h"
-#include "content/test/mock_client_hints_utils.h"
 #include "services/network/public/cpp/network_quality_tracker.h"
 #include "third_party/blink/public/common/client_hints/enabled_client_hints.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
 #include "url/origin.h"
 
 namespace content {
+
+using ClientHintsContainer =
+    std::map<const url::Origin, blink::EnabledClientHints>;
 
 class MockClientHintsControllerDelegate : public ClientHintsControllerDelegate {
  public:
@@ -57,4 +59,4 @@ class MockClientHintsControllerDelegate : public ClientHintsControllerDelegate {
 };
 }  // end namespace content
 
-#endif  // CONTENT_TEST_MOCK_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
+#endif  // CONTENT_PUBLIC_TEST_MOCK_CLIENT_HINTS_CONTROLLER_DELEGATE_H_
