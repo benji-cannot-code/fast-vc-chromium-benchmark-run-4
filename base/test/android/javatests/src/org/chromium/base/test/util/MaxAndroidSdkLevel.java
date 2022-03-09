04691274dd52d8d99f8549951d1e73ee1798f3e4
Henrique Nakashima <hnakashima@chromium.org>
2022-03-09 22:54:14 +0000

    [Android] Introduce @MaxAndroidSdkLevel
    Like @MinAndroidSdkLevel, this limits annotated tests to certain API
    levels.
    
    Bug: 1302473
    Change-Id: I50e74bca0adb5c21716a3613dc7254de416e25e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508871
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979471}

--- base/test/android/javatests/src/org/chromium/base/test/util/MaxAndroidSdkLevel.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/MaxAndroidSdkLevel.java
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2014 The Chromium Authors. All rights reserved.
+// Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,16 +12,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Skips the annotated test when the device's API level is lower than {@code value()}.
+ * Skips the annotated test when the device's API level is higher than {@code value()}.
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
-public @interface MinAndroidSdkLevel {
-    /** Minimum API level in which the test is run. */
+public @interface MaxAndroidSdkLevel {
+    /** Maximum API level in which the test is run. */
     int value();
 
-    /** Why a test is skipped below this API level. */
+    /** Why a test is skipped above this API level. */
     String reason() default "";
 }
-
