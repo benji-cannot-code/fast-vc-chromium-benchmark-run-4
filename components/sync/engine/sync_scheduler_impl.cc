8f1fd55d93aa493d33e87170f45d0293c123272b
Maksim Moskvitin <mmoskvitin@google.com>
2022-03-09 14:22:37 +0000

    [Cleanup] Delete sync/base/logging.*
    Simple dead code deletion.
    
    Bug: none
    Change-Id: Ie4dce24a12f436f23fe146d69dcd8d37f96a5f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507553
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#979221}

--- components/sync/engine/sync_scheduler_impl.cc
+++ components/sync/engine/sync_scheduler_impl.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "components/sync/base/features.h"
-#include "components/sync/base/logging.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync/engine/backoff_delay_provider.h"
 #include "components/sync/protocol/sync_enums.pb.h"
