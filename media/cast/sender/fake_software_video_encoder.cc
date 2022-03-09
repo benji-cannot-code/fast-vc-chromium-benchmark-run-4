382b981e254b4226f25c80c87111996168c0cf8e
mark a. foltz <mfoltz@chromium.org>
2022-03-09 02:21:23 +0000

    [Capture Test Fixit] Fix video_encoder_unittest.cc.
    The fake software video encoder, used for unit tests, was disabled on
    official builds causing unittests to crash.
    
    Re-enable the fake codec gated behind a flag in the FrameSenderConfig
    which is only set in tests.
    
    Also, moves the fake codecs factory to the media/cast/test folder.
    
    Bug: 1292086
    Change-Id: I5d62ed561a73336f4ca84f989d658e56b67bb1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508803
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979033}

--- media/cast/sender/fake_software_video_encoder.cc
+++ media/cast/sender/fake_software_video_encoder.cc
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cast/common/rtp_time.h"
 #include "media/cast/constants.h"
 
-#ifndef OFFICIAL_BUILD
-
 namespace media {
 namespace cast {
 
@@ -83,5 +81,3 @@ void FakeSoftwareVideoEncoder::GenerateKeyFrame() {
 
 }  // namespace cast
 }  // namespace media
-
-#endif
