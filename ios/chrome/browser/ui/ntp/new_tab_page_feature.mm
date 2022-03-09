6a490fe0502b358835bc3a6617865b02bc05c864
Tina Wang <tinazwang@google.com>
2022-03-09 01:52:14 +0000

    [FeedPreview] Pre-work for launching
    Remove the field trail and enable the feature by default.
    
    Bug: 1222368
    Change-Id: I6f0462bf148ec1d6380c5cbe06b4a9a649f7571a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500079
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979018}

--- ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_feature.mm
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 const base::Feature kEnableDiscoverFeedPreview{
-    "EnableDiscoverFeedPreview", base::FEATURE_DISABLED_BY_DEFAULT};
+    "EnableDiscoverFeedPreview", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kEnableDiscoverFeedAppFlows{
     "EnableDiscoverFeedAppFlows", base::FEATURE_DISABLED_BY_DEFAULT};
