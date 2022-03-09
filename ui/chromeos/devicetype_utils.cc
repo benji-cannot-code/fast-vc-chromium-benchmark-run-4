16f35a7d617784858e2e3109efc105845b6dec98
Fabian Sommer <fabiansommer@chromium.org>
2022-03-09 12:27:27 +0000

    [Lacros] Enable GetChromeOSDeviceName
    Migrate devicetype_utils.cc to use the chromeos::GetDeviceType()
    provided by chromeos/constants instead of ash/constants.
    
    Both devicetype_utils.cc and the strings used by it used to be only
    available on Ash. Change some build files to enable them on Lacros.
    
    This is done so that Lacros can use GetChromeOSDeviceName, which will
    be used in some strings.
    
    Bug: 1284585
    Change-Id: Ia9a24389db9b03aa85d5bfe6272f2f48485ead72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3477021
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979190}

--- ui/chromeos/devicetype_utils.cc
+++ ui/chromeos/devicetype_utils.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/chromeos/devicetype_utils.h"
 
-#include "ash/constants/devicetype.h"
 #include "base/notreached.h"
 #include "build/chromeos_buildflags.h"
+#include "chromeos/constants/devicetype.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/strings/grit/ui_chromeos_strings.h"
 
