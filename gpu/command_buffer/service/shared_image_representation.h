b01d24ee7540633bb6f991392bc2a435fe6b9f18
Stephen White <senorblanco@chromium.org>
2022-03-09 23:24:22 +0000

    WebGPU/Compat cleanup.
    Now that SharedImageRepresentationDawnEGLImage holds onto a
    SharedImageRepresentationGLTexturePassthrough, we don't need to fiddle
    with the innards of the D3D11 backing, and can call BeginAccess() on
    the GL representation to achieve the same effect in a cross-platform
    manner.
    
    Bug: 1303602
    Change-Id: I0bea1e10d3b23016853a9be7b4acc91feadbb2d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3512838
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979482}

--- gpu/command_buffer/service/shared_image_representation.h
+++ gpu/command_buffer/service/shared_image_representation.h
@@ -248,6 +248,9 @@ class GPU_GLES2_EXPORT SharedImageRepresentationGLTexturePassthrough
   GetTexturePassthrough() = 0;
 
   gpu::TextureBase* GetTextureBase() override;
+
+ protected:
+  friend class SharedImageRepresentationDawnEGLImage;
 };
 
 class GPU_GLES2_EXPORT SharedImageRepresentationSkia
