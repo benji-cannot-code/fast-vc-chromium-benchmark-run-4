49fd677e55fa908f78d4eadedf4613b1b8c211e1
Muyao Xu <muyaoxu@google.com>
2022-03-09 21:29:06 +0000

    Revert "[dPWA/AppIdentity]: Add integration test for icon updates."
    This reverts commit 49b1515bf35644b06bdee315fca32261668fcbd9.
    
    Reason for revert:
    Possible culprit https://ci.chromium.org/p/chromium/builders/ci/Mac10.14%20Tests?limit=200
    
    Original change's description:
    > [dPWA/AppIdentity]: Add integration test for icon updates.
    >
    > Bug: 1292563
    > Change-Id: I153645c732b2131956f02f252e7f10416cb9f1df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507181
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#979318}
    
    Bug: 1292563
    Change-Id: Iabacf52ebfb5a9b203dd25d1fa9c94251649101f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514636
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Owners-Override: Muyao Xu <muyaoxu@google.com>
    Auto-Submit: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#979430}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
@@ -78,7 +78,6 @@ struct AppState {
            const apps::RunOnOsLoginMode& run_on_os_login_mode,
            const blink::mojom::DisplayMode& effective_display_mode,
            const blink::mojom::DisplayMode& user_display_mode,
-           const std::string& manifest_install_icon,
            bool is_installed_locally,
            bool is_shortcut_created);
   ~AppState();
@@ -92,7 +91,6 @@ struct AppState {
   apps::RunOnOsLoginMode run_on_os_login_mode;
   blink::mojom::DisplayMode effective_display_mode;
   blink::mojom::DisplayMode user_display_mode;
-  std::string manifest_install_icon;
   bool is_installed_locally;
   bool is_shortcut_created;
 };
@@ -179,7 +177,6 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   void NavigatePwaSiteATo(const std::string& site_mode);
   void NavigateNotfoundUrl();
   void NavigateTabbedBrowserToSite(const GURL& url);
-  void ManifestUpdateIcon(const std::string& site_mode);
   void ManifestUpdateTitle(const std::string& site_mode);
   void ManifestUpdateDisplayBrowser(const std::string& site_mode);
   void ManifestUpdateDisplayMinimal(const std::string& site_mode);
@@ -205,7 +202,6 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   void CheckBrowserNavigationIsAppSettings(const std::string& site_mode);
   void CheckAppSettingsAppState(const std::string& site_mode);
   void CheckAppNotInList(const std::string& site_mode);
-  void CheckAppIconSiteA(const std::string& color);
   void CheckAppTitleSiteA(const std::string& title);
   void CheckAppWindowMode(const std::string& site_mode,
                           apps::WindowMode window_mode);
