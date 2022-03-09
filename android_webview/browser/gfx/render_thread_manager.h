187465602df7ae507f04067151e229d655677f28
Vasiliy Telezhnikov <vasilyt@chromium.org>
2022-03-09 14:32:08 +0000

    Reland "aw: Make InvalidateTests closer to real path"
    This is a reland of commit 062e50913c3acb1c2a9fb16f74ed83517b329170
    
    Original change's description:
    > aw: Make InvalidateTests closer to real path
    >
    > This CL makes few changes:
    > * Involve RenderThreadManager into the process, to make sure queue
    >   inside it does not interferee with invalidation
    > * Allow presentation feedback to be delivered via BeginFrames from
    >   client.
    > * Allow RootFrameSink to call invalidate outside of begin frame (it's
    >   possible in real life)
    > * Make draw follow android cycle DrawOnUI/Sync/DrawOnRT
    > * Count how many times we invalidate
    >
    > Bug: 1233542
    > Change-Id: Iea98eea5b6bdc29504c27605fda1c3e96afa118c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3485016
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#974685}
    
    Bug: 1233542
    Change-Id: Ie174bb439cfbd28ee40a0ae251e87a875ba379bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510291
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979226}

--- android_webview/browser/gfx/render_thread_manager.h
+++ android_webview/browser/gfx/render_thread_manager.h
@@ -37,6 +37,8 @@ class RenderThreadManager : public CompositorFrameConsumer {
 
   ~RenderThreadManager() override;
 
+  void SetRootFrameSinkGetterForTesting(
+      RootFrameSinkGetter root_frame_sink_getter);
   // CompositorFrameConsumer methods.
   void SetCompositorFrameProducer(
       CompositorFrameProducer* compositor_frame_producer,
