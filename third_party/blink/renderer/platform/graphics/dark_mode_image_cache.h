dad447bfc5af4ddf708acb751eb1226c5f8f511a
Kevin Lubick <kjlubick@google.com>
2022-03-09 14:14:46 +0000

    Fix Skia transitive includes
    This is required to land https://skia-review.googlesource.com/c/skia/+/518276
    
    Bug: 242216
    Change-Id: I79ae57f94b67834d238ef8bfaa570d040aefa49c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513532
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Kevin Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979218}

--- third_party/blink/renderer/platform/graphics/dark_mode_image_cache.h
+++ third_party/blink/renderer/platform/graphics/dark_mode_image_cache.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/hash.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/skia/include/core/SkColorFilter.h"
+#include "third_party/skia/include/core/SkRect.h"
+#include "third_party/skia/include/core/SkRefCnt.h"
 
 namespace blink {
 
