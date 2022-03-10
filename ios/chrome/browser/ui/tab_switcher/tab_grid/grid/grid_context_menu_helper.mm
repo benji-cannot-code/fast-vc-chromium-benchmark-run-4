500273081770f4c294ecb3c0fc9978f572dec27b
Mike Dougherty <michaeldo@chromium.org>
2022-03-10 16:25:31 +0000

    Fix crash when long pressing an already closed tab in tab grid search
    When an item presented in search results is closed from another window,
    the TabSwitcherItem no longer references an exiting WebState. Ensure
    long pressing on these items does not cause an application crash. This
    CL simply returns an empty list of context menu items which prevents the
    context menu from being shown for these tab grid items.
    
    Fixed: 1305243
    
    Change-Id: Ibea0ffebb891646366a234498cd64f3f21d86097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514581
    Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979798}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_context_menu_helper.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_context_menu_helper.mm
@@ -95,6 +95,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   GridItem* item = [self.actionsDataSource
       gridItemForCellIdentifier:gridCell.itemIdentifier];
+  if (!item) {
+    return @[];
+  }
 
   NSMutableArray<UIMenuElement*>* menuElements = [[NSMutableArray alloc] init];
 
