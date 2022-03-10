24fd363871da86c6e959cbe96ff5c1c795c90c3c
Alex Mitra <alexmitra@chromium.org>
2022-03-10 21:52:06 +0000

    Creates a coordinator for Download Interstitials.
    The factory helper can be used to create an instance of the internal
    coordinator from outside of the internal directory.
    
    Part of the refactoring of new download tab into a download interstitial
    component and a decoupled tabview provider.
    
    Bug: 1302647
    Change-Id: Id514727f657b823359d825f21d5f788b228e0a1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513752
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979960}

---
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/interstitial/DownloadInterstitialCoordinator.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download.interstitial;
+
+import android.view.View;
+
+import org.chromium.base.lifetime.Destroyable;
+
+/**
+ * Coordinator to set up a download interstitial which displays the progress of the most recent
+ * download and provides utilities for the download once completed.
+ */
+public interface DownloadInterstitialCoordinator extends Destroyable {
+    /** @return The view containing the download interstitial. */
+    View getView();
+}
