e3a73aa34ec5074a8e0584e74dfd2a918b98eb7e
Xianzhu Wang <wangxianzhu@chromium.org>
2022-03-10 03:03:40 +0000

    Don't throttle frames when painting for printing
    The regression was caused by https://crrev.com/c/3358229 which merged
    LocalFrameView::PaintOutsideOfLifecycle() (with AllowThrottlingScope)
    and LocalFrameView::PaintContentsOutsideOfLifecycle (without
    AllowThrottlingScope), causing empty result when a throttled frame was
    printed.
    
    Now remove AllowThrottlingScope from PaintOutsideOfLifecycle() and
    add PaintOutsideOfLifecycleWithThrottlingAllowed().
    PaintOutsetOfLifecycle() is called where a non-throttling document
    lifecycle update has just been called, and the latter is called where
    the paint uses previous normal document lifecycle update result.
    
    Bug: 1303840
    Change-Id: Ia14e5aed65261891a06063f444fbec25f5883582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513296
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979615}

--- third_party/blink/renderer/core/scheduler_integration_tests/frame_throttling_test.cc
+++ third_party/blink/renderer/core/scheduler_integration_tests/frame_throttling_test.cc
@@ -8,10 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "cc/base/features.h"
 #include "cc/layers/picture_layer.h"
+#include "cc/paint/paint_record.h"
+#include "cc/paint/paint_recorder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/test/test_web_frame_content_dumper.h"
 #include "third_party/blink/public/web/web_hit_test_result.h"
+#include "third_party/blink/public/web/web_print_params.h"
 #include "third_party/blink/public/web/web_settings.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_intersection_observer_init.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -43,6 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/testing/paint_test_configurations.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/url_test_helpers.h"
+
+#include "third_party/blink/renderer/platform/graphics/logging_canvas.h"
 
 using testing::_;
 
@@ -1936,6 +1941,34 @@ TEST_P(FrameThrottlingTest, ClearPaintArtifactOnThrottlingLocalRoot) {
   // results.
   EXPECT_TRUE(
       view->GetPaintControllerForTesting().GetPaintArtifact().IsEmpty());
+}
+
+TEST_P(FrameThrottlingTest, PrintThrottledFrame) {
+  SimRequest main_resource("https://example.com/", "text/html");
+  SimRequest frame_resource("https://example.com/iframe.html", "text/html");
+
+  LoadURL("https://example.com/");
+  // The frame is initially throttled.
+  main_resource.Complete(R"HTML(
+    <div style="height: 2000px"></div>
+    <iframe id="frame" sandbox src="iframe.html"></iframe>
+  )HTML");
+  frame_resource.Complete("ABC");
+  CompositeFrame();
+
+  auto* frame_element =
+      To<HTMLIFrameElement>(GetDocument().getElementById("frame"));
+  auto* sub_frame = To<LocalFrame>(frame_element->ContentFrame());
+  EXPECT_TRUE(sub_frame->View()->ShouldThrottleRenderingForTest());
+  auto* web_frame = WebLocalFrameImpl::FromFrame(sub_frame);
+  WebPrintParams print_params(gfx::Size(500, 500));
+  web_frame->PrintBegin(print_params, blink::WebNode());
+  cc::PaintRecorder recorder;
+  web_frame->PrintPage(0, recorder.beginRecording(500, 500));
+  auto record = recorder.finishRecordingAsPicture();
+  String record_string = RecordAsDebugString(*record);
+  EXPECT_TRUE(record_string.Contains("drawTextBlob")) << record_string.Utf8();
+  web_frame->PrintEnd();
 }
 
 }  // namespace blink
