2961acbae0f4416048c1de222b43f1533fe2008a
Gabriel Charette <gab@chromium.org>
2022-03-10 00:33:06 +0000

    [ios] Introduce web::GetUIThreadTaskRunner and its IO counterpart
    //ios equivalent to
    https://chromium-review.googlesource.com/c/chromium/src/+/2014055
    
    Doesn't honor traits.priority() as iOS never did (crbug.com/1304248).
    Hence this CL is a logical no-op aside from enabling Task APIs v3 in
    iOS. And thus enabling full deprecation of base/task/post_task.h (final
    migration to occur in //ios after this CL; all of //content and
    dependents were already migrated to content::GetUIThreadTaskRunner()).
    
    Migration step #1 @
    https://chromium-review.googlesource.com/c/chromium/src/+/3509967
    shows that this is WAI (no-op).
    
    Bug: 1026641
    Change-Id: I42b089d61eadf08fa25fde7fd823e190e5a42e19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507458
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979533}

--- ios/web/web_thread_unittest.cc
+++ ios/web/web_thread_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "ios/web/public/test/web_task_environment.h"
 #include "ios/web/public/thread/web_task_traits.h"
+#include "ios/web/public/thread/web_thread.h"
 #include "ios/web/web_thread_impl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
@@ -19,19 +20,38 @@ class WebThreadTest : public PlatformTest {
  protected:
   static void BasicFunction(base::OnceClosure continuation,
                             WebThread::ID target) {
+    auto other_thread = target == WebThread::UI ? WebThread::IO : WebThread::UI;
+
     EXPECT_TRUE(WebThread::CurrentlyOn(target));
+    EXPECT_FALSE(WebThread::CurrentlyOn(other_thread));
     std::move(continuation).Run();
   }
 
-  web::WebTaskEnvironment task_environment_;
+  web::WebTaskEnvironment task_environment_{WebTaskEnvironment::REAL_IO_THREAD};
 };
 
-TEST_F(WebThreadTest, PostTask) {
+TEST_F(WebThreadTest, BasePostTask) {
   base::RunLoop run_loop;
   EXPECT_TRUE(base::PostTask(
       FROM_HERE, {WebThread::IO},
       base::BindOnce(&BasicFunction, run_loop.QuitWhenIdleClosure(),
                      WebThread::IO)));
+  run_loop.Run();
+}
+
+TEST_F(WebThreadTest, GetUITaskRunnerPostTask) {
+  base::RunLoop run_loop;
+  EXPECT_TRUE(GetUIThreadTaskRunner({})->PostTask(
+      FROM_HERE, base::BindOnce(&BasicFunction, run_loop.QuitWhenIdleClosure(),
+                                WebThread::UI)));
+  run_loop.Run();
+}
+
+TEST_F(WebThreadTest, GetIOTaskRunnerPostTask) {
+  base::RunLoop run_loop;
+  EXPECT_TRUE(GetIOThreadTaskRunner({})->PostTask(
+      FROM_HERE, base::BindOnce(&BasicFunction, run_loop.QuitWhenIdleClosure(),
+                                WebThread::IO)));
   run_loop.Run();
 }
 
