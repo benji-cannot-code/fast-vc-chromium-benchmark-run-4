fb8b9ee5aea0b5ecea0015885b03d2d348f99f76
Guillaume Jenkins <gujen@google.com>
2022-03-10 19:46:30 +0000

    [Notes] Add model map in UserNoteService
    Creates the model map in the UserNoteService. The model map is used to
    store the UserNote models in memory, and to keep track of which tabs are
    displaying a UserNoteInstance of this model.
    
    Also adds the methods that the UserNotesManager objects will use to
    update this model map.
    
    Bug: 1302739
    Change-Id: Ib27b51583e8b10c79921558b975621d7a5fa0960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498419
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#979903}

--- components/user_notes/browser/user_note_service.cc
+++ components/user_notes/browser/user_note_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_notes/browser/user_note_service.h"
 
 #include "base/notreached.h"
+#include "components/user_notes/browser/user_notes_manager.h"
 #include "components/user_notes/user_notes_features.h"
 
 namespace user_notes {
@@ -13,6 +14,33 @@ namespace user_notes {
 UserNoteService::UserNoteService() = default;
 
 UserNoteService::~UserNoteService() = default;
+
+void UserNoteService::OnNoteInstanceAddedToPage(const std::string& guid,
+                                                UserNotesManager* manager) {
+  DCHECK(IsUserNotesEnabled());
+  DCHECK(model_map_.find(guid) != model_map_.end())
+      << "A note instance without backing model was added to a page";
+
+  model_map_.at(guid).second.insert(manager);
+}
+
+void UserNoteService::OnNoteInstanceRemovedFromPage(const std::string& guid,
+                                                    UserNotesManager* manager) {
+  DCHECK(IsUserNotesEnabled());
+
+  auto model = model_map_.find(guid);
+  DCHECK(model != model_map_.end())
+      << "A note model was destroyed before all its instances";
+
+  auto deleteCount = (*model).second.second.erase(manager);
+  DCHECK(deleteCount > 0) << "Attempted to remove a ref to a note manager that "
+                             "wasn't in the model map";
+
+  // If there are no longer any pages displaying this model, destroy it.
+  if ((*model).second.second.empty()) {
+    model_map_.erase(guid);
+  }
+}
 
 void UserNoteService::OnNoteFocused(const std::string& guid) {
   DCHECK(IsUserNotesEnabled());
