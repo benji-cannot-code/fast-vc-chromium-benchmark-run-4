386de17b51898736cec2a51c0ea0c122a089d2f0
Nina Satragno <nsatragno@chromium.org>
2022-03-10 18:06:19 +0000

    [CT] Deflake SCTAuditingHandlerTest test
    Attempt to deflake SCTAuditingHandlerTest.ReportHighWaterMarkMetrics.
    The test takes too long on some bots and it causes a timeout
    (particularly in fuchsia).
    
    I noticed the runtime is proportional to the task fast forward period,
    so this CL allows tests to override the default 1 hour period to
    something much shorter. Locally, runtime went from ~50-150ms to ~5ms. I
    suspect NetworkContext is setting some recurring task that causes this
    slowdown.
    
    Fixed: 1303024
    Change-Id: Ib59fa765f05d974f0482f6d40f7b767ac4ecfb55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510373
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979845}

--- services/network/sct_auditing/sct_auditing_handler.h
+++ services/network/sct_auditing/sct_auditing_handler.h
@@ -127,6 +127,10 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) SCTAuditingHandler
 
   base::ImportantFileWriter* GetFileWriterForTesting() { return writer_.get(); }
 
+  void set_hwm_metrics_period_for_testing(base::TimeDelta hwm_metrics_period) {
+    hwm_metrics_period_ = hwm_metrics_period;
+  }
+
   base::WeakPtr<SCTAuditingHandler> GetWeakPtr();
 
  private:
@@ -162,6 +166,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) SCTAuditingHandler
   scoped_refptr<base::SequencedTaskRunner> background_runner_;
   bool is_after_startup_ = false;
   bool persisted_reports_read_ = false;
+
+  // How often the high-water-mark metrics get logged.
+  base::TimeDelta hwm_metrics_period_ = base::Hours(1);
 
   base::WeakPtrFactory<SCTAuditingHandler> weak_factory_{this};
 };
