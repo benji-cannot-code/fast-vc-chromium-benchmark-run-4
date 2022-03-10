540a3ae94fd1bf04f4adf91b0dfe6156f8ad5b75
Rainhard Findling <rainhard@chromium.org>
2022-03-10 16:46:30 +0000

    Privacy Guide: arrow-key navigation
    Adds arrow key navigation to the privacy guide card:
    - The card observes left and right arrow key events and triggers a
      navigation between cards on them.
    - If the focus is in a radio group (which uses arrow keys to change the
      radio button selection), no navigation in between cards is triggered.
    - Adds tests that ensure correct functionality in different cases via
      simulated key press events.
    
    Bug: 1215630
    Change-Id: I872e8686574bf3a4e8dc0edfffda9ac2ec6c6050
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3506526
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979808}

--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_page.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_page.ts
@@ -350,7 +350,6 @@ export class SettingsPrivacyGuidePageElement extends PrivacyGuideBase {
   private navigateForward_() {
     const components =
         this.privacyGuideStepToComponentsMap_.get(this.privacyGuideStep_)!;
-    assert(components.onForwardNavigation || components.nextStep);
     if (components.onForwardNavigation) {
       components.onForwardNavigation();
     }
@@ -369,10 +368,9 @@ export class SettingsPrivacyGuidePageElement extends PrivacyGuideBase {
     if (components.onBackwardNavigation) {
       components.onBackwardNavigation();
     }
-    this.navigateToCard_(
-        this.privacyGuideStepToComponentsMap_.get(this.privacyGuideStep_)!
-            .previousStep!,
-        true, true);
+    if (components.previousStep) {
+      this.navigateToCard_(components.previousStep, true, true);
+    }
   }
 
   private navigateToCard_(
@@ -477,6 +475,18 @@ export class SettingsPrivacyGuidePageElement extends PrivacyGuideBase {
         assert((event.target! as HTMLElement)
                    .shadowRoot!.querySelector<HTMLElement>('[focus-element]'));
     afterNextRender(this, () => elementToFocus!.focus());
+  }
+
+  private onKeyDown_(event: KeyboardEvent) {
+    const isLtr = loadTimeData.getString('textdirection') === 'ltr';
+    switch (event.key) {
+      case 'ArrowLeft':
+        isLtr ? this.navigateBackward_() : this.navigateForward_();
+        break;
+      case 'ArrowRight':
+        isLtr ? this.navigateForward_() : this.navigateBackward_();
+        break;
+    }
   }
 }
 
