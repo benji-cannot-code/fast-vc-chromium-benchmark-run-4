9eb22d508661a2930e5a11ed19d9fd5460d90664
Alex Ilin <alexilin@chromium.org>
2022-03-10 21:41:19 +0000

    [signin] Implement AccountCapabilitiesFetcherAndroid
    This CL adds an AccountCapabilitiesFetcher implementation for Android.
    AccountCapabilitiesFetcherAndroid has C++ and Java counterparts. The
    native class makes a JNI calls to query capabilities from
    AccountManagerFacade.
    
    AccountCapabilitiesFetcherGaia is removed from Android build, since
    it shouldn't be used for fetching account capabilities on Android.
    
    account_capabilities_fetcher_unittest.cc is generalized to share the
    same tests between *Gaia and *Android implementations as much as
    possible.
    
    And lastly, this CL makes kEnableFetchingAccountCapabilities
    base::Feature available on Android.
    
    Manual tests confirmed that the pipeline works and AccountCapabilities
    get correctly populated in AccountTrackerService. Automated integration
    tests will be added in future CLs.
    
    Bug: 1298465
    Change-Id: I71aac6f49108920654dc1c785700dc814edd8380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513175
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979958}

--- components/signin/public/identity_manager/identity_manager_builder.cc
+++ components/signin/public/identity_manager/identity_manager_builder.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/image_fetcher/core/image_decoder.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/internal/identity_manager/account_capabilities_fetcher_factory.h"
-#include "components/signin/internal/identity_manager/account_capabilities_fetcher_factory_gaia.h"
 #include "components/signin/internal/identity_manager/account_fetcher_service.h"
 #include "components/signin/internal/identity_manager/account_tracker_service.h"
 #include "components/signin/internal/identity_manager/accounts_cookie_mutator_impl.h"
@@ -29,9 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/device_accounts_synchronizer.h"
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
+#include "components/signin/internal/identity_manager/account_capabilities_fetcher_factory_android.h"
+#else
+#include "components/signin/internal/identity_manager/account_capabilities_fetcher_factory_gaia.h"
 #include "components/signin/public/webdata/token_web_data.h"
-#endif
+#endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_IOS)
 #include "components/signin/public/identity_manager/ios/device_accounts_provider.h"
@@ -163,9 +165,16 @@ IdentityManager::InitParameters BuildIdentityManagerInitParameters(
   init_params.diagnostics_provider = std::make_unique<DiagnosticsProviderImpl>(
       token_service.get(), gaia_cookie_manager_service.get());
 
-  auto account_capabilities_fetcher_factory =
+  std::unique_ptr<AccountCapabilitiesFetcherFactory>
+      account_capabilities_fetcher_factory;
+#if BUILDFLAG(IS_ANDROID)
+  account_capabilities_fetcher_factory =
+      std::make_unique<AccountCapabilitiesFetcherFactoryAndroid>();
+#else
+  account_capabilities_fetcher_factory =
       std::make_unique<AccountCapabilitiesFetcherFactoryGaia>(
           token_service.get(), params->signin_client);
+#endif  // BULIDFLAG(IS_ANDROID)
 
   init_params.account_fetcher_service = BuildAccountFetcherService(
       params->signin_client, token_service.get(), account_tracker_service.get(),
