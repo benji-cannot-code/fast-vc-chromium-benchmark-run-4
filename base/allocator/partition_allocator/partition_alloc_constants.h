3ec48cad50a8e04e04c6cad03c866e9aecf74581
Stephan Hartmann <stha09@googlemail.com>
2022-03-10 10:51:58 +0000

    IWYU: add climits for CHAR_BIT in partition_alloc_constants
    Bug: 957519
    Change-Id: Ic7081e26293d927b80c49769408da938f2bf398c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514074
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Stephan Hartmann <stha09@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#979710}

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_PARTITION_ALLOC_CONSTANTS_H_
 
 #include <algorithm>
+#include <climits>
 #include <cstddef>
 #include <limits>
 
