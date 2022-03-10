2536c9cfd537746525eb70480342c8b8f1ae6d45
Rakina Zata Amni <rakina@chromium.org>
2022-03-10 02:06:27 +0000

    Enable same-site bfcache by default
    We've experimented with same-site bfcache on Stable for a while, and
    it's safe to enable same-site navigation support on bfcache by default
    now. Note that bfcache is already enabled by default since M93, but
    only for cross-site navigations.
    
    Bug: 1136658
    Change-Id: I6c306005abc0d1df28dcf6d16ff84011855d1967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3435587
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979585}

--- components/page_load_metrics/browser/metrics_web_contents_observer_unittest.cc
+++ components/page_load_metrics/browser/metrics_web_contents_observer_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
+#include "content/public/test/back_forward_cache_util.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/render_frame_host_test_support.h"
 #include "content/public/test/test_renderer_host.h"
@@ -439,6 +440,12 @@ TEST_F(MetricsWebContentsObserverTest, DontLogIrrelevantNavigation) {
 }
 
 TEST_F(MetricsWebContentsObserverTest, EmptyTimingError) {
+  // Page load timing errors are not being reported when the error occurs for a
+  // page that gets preserved in the back/forward cache.
+  // TODO(https://crbug.com/1294103): Fix this.
+  content::DisableBackForwardCacheForTesting(
+      web_contents(), content::BackForwardCache::TEST_REQUIRES_NO_CACHING);
+
   mojom::PageLoadTiming timing;
   page_load_metrics::InitPageLoadTimingForTest(&timing);
 
@@ -462,6 +469,11 @@ TEST_F(MetricsWebContentsObserverTest, EmptyTimingError) {
 }
 
 TEST_F(MetricsWebContentsObserverTest, NullNavigationStartError) {
+  // Page load timing errors are not being reported when the error occurs for a
+  // page that gets preserved in the back/forward cache.
+  // TODO(https://crbug.com/1294103): Fix this.
+  content::DisableBackForwardCacheForTesting(
+      web_contents(), content::BackForwardCache::TEST_REQUIRES_NO_CACHING);
   mojom::PageLoadTiming timing;
   page_load_metrics::InitPageLoadTimingForTest(&timing);
   timing.parse_timing->parse_start = base::Milliseconds(1);
@@ -486,6 +498,11 @@ TEST_F(MetricsWebContentsObserverTest, NullNavigationStartError) {
 }
 
 TEST_F(MetricsWebContentsObserverTest, TimingOrderError) {
+  // Page load timing errors are not being reported when the error occurs for a
+  // page that gets preserved in the back/forward cache.
+  // TODO(https://crbug.com/1294103): Fix this.
+  content::DisableBackForwardCacheForTesting(
+      web_contents(), content::BackForwardCache::TEST_REQUIRES_NO_CACHING);
   mojom::PageLoadTiming timing;
   page_load_metrics::InitPageLoadTimingForTest(&timing);
   timing.navigation_start = base::Time::FromDoubleT(1);
--- third_party/blink/web_tests/virtual/bfcache-same-site/README.md
+++
@@ -1,2 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-Tests run with back-forward same-site cache enabled (`--enable-features=BackForwardCache:enable_same_site/true`).
