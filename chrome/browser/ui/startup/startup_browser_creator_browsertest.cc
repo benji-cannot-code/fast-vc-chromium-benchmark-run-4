f54453e2de77929cf60536a411333291ac5f21ae
Dibyajyoti Pal <dibyapal@chromium.org>
2022-03-10 21:29:46 +0000

    Reland "dPWA: Create container for deprecated app dialog view"
    This CL is a reland of commit 15a91953bb5e0567eb86965a8dd1d16fe467e891
    
    This CL relands the dialog view CL by fixing the unexpected behavior
    in tests on slower platforms by using callbacks.
    
    Original change's description:
    > dPWA: Create container for deprecated app dialog view
    >
    > This CL creates the container for the deprecated app dialog view and
    > sets up the information flow from app_launcher_handler where deprecated
    > apps are set to show on the dialog. The container is completed with all
    > functionalities, including the accept callbacks for uninstalling
    > extensions.
    >
    > Doc: go/chrome-apps-removal-ui
    >
    > Follow up CLs will include the following:
    > 1. Invoking this dialog from platform apps (crbug.com/1300775).
    > 2. Adding a link to chrome://apps to open the dialog (crbug.com/1225782).
    >
    > Screenshot: https://screenshot.googleplex.com/AfehKzBHBiFM8NR
    >
    > Bug: 1225778, 1225779
    > Change-Id: Iad37b09ffe5c0124fc0ad6ff0819df8049174dfa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3472598
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Phillis Tang <phillis@chromium.org>
    > Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#978874}
    
    Bug: 1225778, 1225779
    Change-Id: I164a6deec0ebd6187586f8a2036a2a61ebe59b64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3512341
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979950}

--- chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
+++ chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
@@ -665,7 +665,8 @@ class StartupBrowserCreatorChromeAppShortcutTest
     switch (std::get<0>(GetParam())) {
       case ChromeAppDeprecationFeatureValue::kDefault:
         break;
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_FUCHSIA)
       case ChromeAppDeprecationFeatureValue::kEnabled:
         scoped_feature_list_.InitAndEnableFeature(
             features::kChromeAppsDeprecation);
@@ -683,7 +684,8 @@ class StartupBrowserCreatorChromeAppShortcutTest
     switch (std::get<1>(GetParam())) {
       case ChromeAppsEnabledPrefValue::kDefault:
         break;
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_FUCHSIA)
       case ChromeAppsEnabledPrefValue::kEnabled:
         browser()->profile()->GetPrefs()->SetBoolean(
             extensions::pref_names::kChromeAppsEnabled, true);
