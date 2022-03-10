a7122dca410da5c3bd9059197dfc247dd955406a
David Dorwin <ddorwin@chromium.org>
2022-03-10 18:50:09 +0000

    [pdf] Remove obsolete include and PPAPI deps
    Bug: 1302684, 702993
    Change-Id: Ieed33beeb5f8e9f17b4147b054e5e7d62daaab79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514416
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Auto-Submit: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979873}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/pdfium/pdfium_mem_buffer_file_write.h"
 #include "pdf/pdfium/pdfium_permissions.h"
 #include "pdf/pdfium/pdfium_unsupported_features.h"
-#include "pdf/ppapi_migration/bitmap.h"
 #include "pdf/ppapi_migration/url_loader.h"
 #include "pdf/url_loader_wrapper_impl.h"
 #include "printing/mojom/print.mojom-shared.h"
