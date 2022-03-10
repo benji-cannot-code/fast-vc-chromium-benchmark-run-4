80e6780d4d73d0bec988af2da9a3c698a6d9d0c7
Michael Hansen <hansenmichael@google.com>
2022-03-10 22:42:45 +0000

    [Nearby] Remove the need to cast to BluetoothAdapterBlueZ
    It is no longer safe to assume that a BluetoothAdapter on Chrome OS is a
    BluetoothAdapterBlueZ. When Floss is enabled, this will be a
    BluetoothAdapterFloss [1].
    
    Downcasting under this assumption in BluetoothAdapterManager lead to a
    crash on certain boards. This is a small refactor to move one method
    declaration up to the base BluetoothAdapter to remove the need to
    downcast.
    
    This updates the BluetoothAdapterManager tests to use a mock adapter to
    directly test the Shutdown() logic, and moves the previous functionality
    coverage to //device/bluetooth/bluez/.
    
    1) https://source.chromium.org/chromium/chromium/src/+/main:device/bluetooth/bluetooth_adapter_chromeos.cc
    
    Fixed: b/220885891
    Change-Id: If357c724b5b5bc49b4d5829306a9780f436e9ba9
    Tested: Unit tests updated, and manually verified on Atlas.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3509811
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#979988}

--- device/bluetooth/floss/bluetooth_adapter_floss.cc
+++ device/bluetooth/floss/bluetooth_adapter_floss.cc
@@ -705,6 +705,12 @@ BluetoothAdapterFloss::GetLowEnergyScanSessionHardwareOffloadingStatus() {
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH) || BUILDFLAG(IS_CHROMEOS_LACROS)
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+void BluetoothAdapterFloss::SetStandardChromeOSAdapterName() {
+  NOTIMPLEMENTED();
+}
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+
 void BluetoothAdapterFloss::RemovePairingDelegateInternal(
     device::BluetoothDevice::PairingDelegate* pairing_delegate) {
   NOTIMPLEMENTED();
