200739c37158d98b1a75c3d2f8d96bdf138e00b1
Ari Chivukula <arichiv@chromium.org>
2022-03-10 18:42:23 +0000

    [Track postMessage usage] (2) Track cross-origin calls
    We need to track whether calls are to/from a first/third party context,
    and whether the calls are between un/like storage buckets. These
    measurements will help indicate where we could prune postMessage access.
    
    Part of this CL adds new functionality to StorageKey, specifically the
    concept of 'CopyWithForceEnabledThirdPartyStoragePartitioning'. The idea
    is that rather than writing non-future-proof code and pass both origin
    and top-site into the counter, we can just pass the storage key and
    'force enable' partitioning just for where it's convenient. This might
    need to be re-worked or split into a different CL.
    
    This CL is a part of a series:
    (1) Consolidate existing metrics
    (2) Track cross-origin calls
    
    Design Doc: https://docs.google.com/document/d/1GnD0JkfDswaqnu62XKz38SfaiM5q5RZZw05yTOvGJ_c/edit
    Privacy Doc: https://docs.google.com/document/d/1Vk5MdZjLVwPPaezqdZ6xO_3m8Lp76dvVuuzgFZbf9qo/edit
    
    Bug: 1159586
    Change-Id: I88cd9fc492ba9c73d8750266ec7cf32f5ee5ea31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3489385
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Quick-Run: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979867}

--- third_party/blink/public/common/metrics/post_message_counter.h
+++ third_party/blink/public/common/metrics/post_message_counter.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "third_party/blink/public/common/common_export.h"
+#include "third_party/blink/public/common/storage_key/storage_key.h"
 
 namespace blink {
 
@@ -26,8 +27,10 @@ class BLINK_COMMON_EXPORT PostMessageCounter {
       : partition_(partition) {}
   ~PostMessageCounter() = default;
 
-  void RecordMessage(ukm::SourceId source,
-                     ukm::SourceId target,
+  void RecordMessage(ukm::SourceId source_id,
+                     const StorageKey& source_storage_key,
+                     ukm::SourceId target_id,
+                     const StorageKey& target_storage_key,
                      ukm::UkmRecorder* recorder);
 
  private:
