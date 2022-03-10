7145936839d657a5e6e37cd6a5018791099b8636
Khushal <khushalsagar@chromium.org>
2022-03-10 16:06:41 +0000

    blink/transitions: Make WA-API work for shared element transitions.
    Fix the pseudo argument parsing for ::transition* pseudo elements in the
    WA-API.
    
    R=flackr@chromium.org, vmpstr@chromium.org
    
    Change-Id: I746ce247b95f895ce41d11e9e45d5ce92e6ea864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513607
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979789}

--- third_party/blink/renderer/core/animation/keyframe_effect.cc
+++ third_party/blink/renderer/core/animation/keyframe_effect.cc
@@ -51,11 +51,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/platform/animation/compositor_animation.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
 
@@ -127,8 +129,14 @@ KeyframeEffect* KeyframeEffect::Create(
     effect->target_pseudo_ = pseudo;
     if (element) {
       element->GetDocument().UpdateStyleAndLayoutTreeForNode(element);
-      effect->effect_target_ = element->GetPseudoElement(
-          CSSSelectorParser::ParsePseudoElement(pseudo, element));
+      PseudoId pseudo_id =
+          CSSSelectorParser::ParsePseudoElement(pseudo, element);
+      AtomicString pseudo_argument =
+          PseudoElementHasArguments(pseudo_id)
+              ? CSSSelectorParser::ParsePseudoElementArgument(pseudo)
+              : WTF::g_null_atom;
+      effect->effect_target_ =
+          element->GetNestedPseudoElement(pseudo_id, pseudo_argument);
     }
   }
   return effect;
--- third_party/blink/renderer/core/style/computed_style_constants.h
+++ third_party/blink/renderer/core/style/computed_style_constants.h
@@ -125,6 +125,10 @@ inline bool IsTransitionPseudoElement(PseudoId pseudo_id) {
 inline bool PseudoElementHasArguments(PseudoId pseudo_id) {
   switch (pseudo_id) {
     case kPseudoIdHighlight:
+    case kPseudoIdPageTransitionContainer:
+    case kPseudoIdPageTransitionImageWrapper:
+    case kPseudoIdPageTransitionIncomingImage:
+    case kPseudoIdPageTransitionOutgoingImage:
       return true;
     default:
       return false;
