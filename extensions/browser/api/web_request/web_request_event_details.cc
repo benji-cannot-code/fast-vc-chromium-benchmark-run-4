2941a2b29f33fccbd79390e7c7094fccbfe10748
David Bertoni <dbertoni@chromium.org>
2022-03-10 23:49:51 +0000

    [CodeHealth] Remove uses of ListValue::Append() in /extensions/browser/api/web_request.
    This CL removes calls to this deprecated member function. When
    it's not too intrusive, this CL attempts to modernize the use
    of DictionaryValue and ListValue to base::Value::Dict and
    base::Value::List to avoid using Value::FromUniquePtrValue.
    
    This CL was uploaded by git cl split.
    
    R=battre@chromium.org
    
    Bug: 1187097
    Change-Id: Ib5ba972d8bd01382ac188cb3a18b8bc703f7a5d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518136
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980027}

--- extensions/browser/api/web_request/web_request_event_details.cc
+++ extensions/browser/api/web_request/web_request_event_details.cc
@@ -94,7 +94,8 @@ void WebRequestEventDetails::SetRequestHeaders(
 
   base::ListValue* headers = new base::ListValue();
   for (net::HttpRequestHeaders::Iterator it(request_headers); it.GetNext();)
-    headers->Append(helpers::CreateHeaderDictionary(it.name(), it.value()));
+    headers->Append(
+        base::Value(helpers::CreateHeaderDictionary(it.name(), it.value())));
   request_headers_.reset(headers);
 }
 
@@ -135,7 +136,8 @@ void WebRequestEventDetails::SetResponseHeaders(
                                                                  name)) {
           continue;
         }
-        headers->Append(helpers::CreateHeaderDictionary(name, value));
+        headers->Append(
+            base::Value(helpers::CreateHeaderDictionary(name, value)));
       }
     }
     response_headers_.reset(headers);
