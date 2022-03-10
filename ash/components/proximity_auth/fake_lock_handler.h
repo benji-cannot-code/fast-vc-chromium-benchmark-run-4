7c407653e59b91cb0bd1cf5dd35ef0e4e35f785c
Jason Rhee <jasonrhee@google.com>
2022-03-10 21:12:56 +0000

    [SmartLock] Add unit tests for code added to easy_unlock_service.cc
    Tested incremental code added/modified as part of Smart Lock UI Revamp.
    Did not add tests for code that would not use the kSmartLockUiRevamp
    flag.
    
    [Incremental Coverage Spreadsheet](https://docs.google.com/spreadsheets/d/17cwZ09gzMS1g3wMhr0k0asFsxeY76e5CxJksADCL83k/edit?resourcekey=0-fR6wu2z5-n2Hri0vstHYYg#gid=0)
    
    Change-Id: I274f523bc3de85bb4ec14866d4590d3e01473234
    Bug: b/222161376, b/215469118
    Fixes: b/222161376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508625
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Jason Rhee <jasonrhee@google.com>
    Cr-Commit-Position: refs/heads/main@{#979938}

--- ash/components/proximity_auth/fake_lock_handler.h
+++ ash/components/proximity_auth/fake_lock_handler.h
@@ -47,6 +47,7 @@ class FakeLockHandler : public ScreenlockBridge::LockHandler {
   absl::optional<bool> smart_lock_auth_result() const {
     return smart_lock_auth_result_;
   }
+  int unlock_called() const { return unlock_called_; }
 
   void ClearSmartLockState();
   void ClearSmartLockAuthResult();
@@ -54,6 +55,8 @@ class FakeLockHandler : public ScreenlockBridge::LockHandler {
  private:
   absl::optional<ash::SmartLockState> smart_lock_state_;
   absl::optional<bool> smart_lock_auth_result_;
+  mojom::AuthType latest_set_auth_type_ = mojom::AuthType::USER_CLICK;
+  int unlock_called_ = 0;
 };
 
 }  // namespace proximity_auth
