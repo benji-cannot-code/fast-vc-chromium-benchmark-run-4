be6095f022877f283788523cc1a8ca48fe61eae1
Robert Ogden <robertogden@chromium.org>
2022-03-10 00:42:55 +0000

    Add public API and placeholder for PageTopics URL preprocessing
    Adds the public API, but without the "real" logic to process urls into
    the strings expected by the model (while I chase down exactly what those
    are).
    
    Bug: 1240822
    Change-Id: Ia861ff0850c3cd021ae670cf3dea61e81caa16b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514547
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979539}

---
+++ components/optimization_guide/content/browser/page_content_annotations_service_unittest.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/optimization_guide/content/browser/page_content_annotations_service.h"
+
+#include <string>
+#include <vector>
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace optimization_guide {
+
+class PageContentAnnotationsServiceTest : public testing::Test {
+ public:
+  PageContentAnnotationsServiceTest() = default;
+  ~PageContentAnnotationsServiceTest() override = default;
+
+  std::string CallStringInputForPageTopicsDomain(const GURL& url) {
+    return PageContentAnnotationsService::StringInputForPageTopicsDomain(url);
+  }
+};
+
+TEST_F(PageContentAnnotationsServiceTest, PageTopicsDomain) {
+  std::vector<std::pair<GURL, std::string>> tests = {
+      {GURL("https://www.chromium.org"), "www.chromium.org"},
+  };
+
+  for (const auto& test : tests) {
+    GURL url = test.first;
+    std::string expected = test.second;
+    std::string got = CallStringInputForPageTopicsDomain(url);
+
+    EXPECT_EQ(expected, got) << url;
+  }
+}
+
+}  // namespace optimization_guide
