d8da918a8f0ec646daa2b969e1036a8194a60de0
Rakina Zata Amni <rakina@chromium.org>
2022-03-10 23:04:11 +0000

    Reland test updates & fixes for crrev.com/c/3435587
    crrev.com/c/3435587 "Enable same-site bfcache by default" was reverted
    because of test failures on the waterfall. This CL relands the test
    updates and bugfixes that are part of that CL, without actually enabling
    same-site bfcache by default (to avoid getting reverted again).
    
    Bug: 1136658
    Change-Id: Ifa6f197f1d632522443a69c10f5cf4ac32567d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3515714
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Wei-Yin Chen <wychen@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980001}

--- chrome/renderer/cart/commerce_hint_agent_browsertest.cc
+++ chrome/renderer/cart/commerce_hint_agent_browsertest.cc
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/ukm/test_ukm_recorder.h"
+#include "content/public/common/content_features.h"
+#include "content/public/test/back_forward_cache_util.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/fenced_frame_test_util.h"
 #include "content/public/test/test_navigation_observer.h"
@@ -467,6 +469,13 @@ IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, AddToCartByURL_XHR) {
 }
 
 IN_PROC_BROWSER_TEST_F(CommerceHintAgentTest, VisitCart) {
+  // The test is flaky with same-site back/forward cache, presumably because it
+  // doesn't expect a RenderView change on same-site navigations.
+  // TODO(https://crbug.com/1302902): Investigate and fix this.
+  content::DisableBackForwardCacheForTesting(
+      web_contents(),
+      content::BackForwardCache::TEST_ASSUMES_NO_RENDER_FRAME_CHANGE);
+
   // Cannot use dummy page with zero products, or the cart would be deleted.
   NavigateToURL("https://www.guitarcenter.com/cart.html");
 
