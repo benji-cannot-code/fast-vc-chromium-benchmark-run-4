7096adfd7f8fdfeb0f2d5a884c5e333d4052bbc4
Pawel Baran <pbaran@opera.com>
2022-03-10 10:20:49 +0000

    [OSCrypt] Aesthetic changes
    Changed const to constexpr. Removed table with pointers to
    functions. Added const to some local variables. In next step I
    will introduce member methods, backed by static functions.
    
    Change-Id: I40597623cd89bbe69a0a7f0a4c3a833d76f13a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3509532
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Paweł Barań <pbaran@opera.com>
    Cr-Commit-Position: refs/heads/main@{#979704}

--- components/os_crypt/os_crypt_linux.cc
+++ components/os_crypt/os_crypt_linux.cc
@@ -26,32 +26,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Salt for Symmetric key derivation.
-const char kSalt[] = "saltysalt";
+constexpr char kSalt[] = "saltysalt";
 
 // Key size required for 128 bit AES.
-const size_t kDerivedKeySizeInBits = 128;
+constexpr size_t kDerivedKeySizeInBits = 128;
 
-// Constant for Symmetic key derivation.
-const size_t kEncryptionIterations = 1;
+// Constant for Symmetric key derivation.
+constexpr size_t kEncryptionIterations = 1;
 
 // Size of initialization vector for AES 128-bit.
-const size_t kIVBlockSizeAES128 = 16;
+constexpr size_t kIVBlockSizeAES128 = 16;
 
-// Password version. V10 means that the hardcoded password will be used.
-// V11 means that a password is/will be stored using an OS-level library (e.g
-// Libsecret). V11 will not be used if such a library is not available.
-// Used for array indexing.
-enum Version {
-  V10 = 0,
-  V11 = 1,
-};
-
-// Prefix for cipher text returned by obfuscation version.  We prefix the
+// Prefixes for cypher text returned by obfuscation version.  We prefix the
 // ciphertext with this string so that future data migration can detect
-// this and migrate to full encryption without data loss.
-const char kObfuscationPrefix[][4] = {
-    "v10", "v11",
-};
+// this and migrate to full encryption without data loss. kObfuscationPrefixV10
+// means that the hardcoded password will be used. kObfuscationPrefixV11 means
+// that a password is/will be stored using an OS-level library (e.g Libsecret).
+// V11 will not be used if such a library is not available.
+constexpr char kObfuscationPrefixV10[] = "v10";
+constexpr char kObfuscationPrefixV11[] = "v11";
 
 // Everything in Cache may be leaked on shutdown.
 struct Cache {
@@ -89,7 +82,7 @@ std::unique_ptr<KeyStorageLinux> (*g_key_storage_provider)() =
 // generation error occurs.
 std::unique_ptr<crypto::SymmetricKey> GenerateEncryptionKey(
     const std::string& password) {
-  std::string salt(kSalt);
+  const std::string salt(kSalt);
 
   // Create an encryption key from our password and salt.
   std::unique_ptr<crypto::SymmetricKey> encryption_key(
@@ -127,13 +120,6 @@ crypto::SymmetricKey* GetPasswordV11() {
   return g_cache.Get().password_v11_cache.get();
 }
 
-// Pointers to functions that return a password for deriving the encryption key.
-// One function for each supported password version (see Version enum).
-crypto::SymmetricKey* (*g_get_password[])() = {
-    &GetPasswordV10,
-    &GetPasswordV11,
-};
-
 }  // namespace
 
 // static
@@ -163,17 +149,17 @@ bool OSCrypt::EncryptString(const std::string& plaintext,
 
   // If we are able to create a V11 key (i.e. a KeyStorage was available), then
   // we'll use it. If not, we'll use V10.
-  Version version = Version::V11;
-  crypto::SymmetricKey* encryption_key = g_get_password[version]();
+  crypto::SymmetricKey* encryption_key = GetPasswordV11();
+  std::string obfuscation_prefix = kObfuscationPrefixV11;
   if (!encryption_key) {
-    version = Version::V10;
-    encryption_key = g_get_password[version]();
+    encryption_key = GetPasswordV10();
+    obfuscation_prefix = kObfuscationPrefixV10;
   }
 
   if (!encryption_key)
     return false;
 
-  std::string iv(kIVBlockSizeAES128, ' ');
+  const std::string iv(kIVBlockSizeAES128, ' ');
   crypto::Encryptor encryptor;
   if (!encryptor.Init(encryption_key, crypto::Encryptor::CBC, iv))
     return false;
@@ -182,7 +168,7 @@ bool OSCrypt::EncryptString(const std::string& plaintext,
     return false;
 
   // Prefix the cipher text with version information.
-  ciphertext->insert(0, kObfuscationPrefix[version]);
+  ciphertext->insert(0, obfuscation_prefix);
   return true;
 }
 
@@ -197,13 +183,16 @@ bool OSCrypt::DecryptString(const std::string& ciphertext,
   // Check that the incoming ciphertext was encrypted and with what version.
   // Credit card numbers are current legacy unencrypted data, so false match
   // with prefix won't happen.
-  Version version;
-  if (base::StartsWith(ciphertext, kObfuscationPrefix[Version::V10],
+  crypto::SymmetricKey* encryption_key = nullptr;
+  std::string obfuscation_prefix;
+  if (base::StartsWith(ciphertext, kObfuscationPrefixV10,
                        base::CompareCase::SENSITIVE)) {
-    version = Version::V10;
-  } else if (base::StartsWith(ciphertext, kObfuscationPrefix[Version::V11],
+    encryption_key = GetPasswordV10();
+    obfuscation_prefix = kObfuscationPrefixV10;
+  } else if (base::StartsWith(ciphertext, kObfuscationPrefixV11,
                               base::CompareCase::SENSITIVE)) {
-    version = Version::V11;
+    encryption_key = GetPasswordV11();
+    obfuscation_prefix = kObfuscationPrefixV11;
   } else {
     // If the prefix is not found then we'll assume we're dealing with
     // old data saved as clear text and we'll return it directly.
@@ -211,20 +200,19 @@ bool OSCrypt::DecryptString(const std::string& ciphertext,
     return true;
   }
 
-  crypto::SymmetricKey* encryption_key(g_get_password[version]());
   if (!encryption_key) {
     VLOG(1) << "Decryption failed: could not get the key";
     return false;
   }
 
-  std::string iv(kIVBlockSizeAES128, ' ');
+  const std::string iv(kIVBlockSizeAES128, ' ');
   crypto::Encryptor encryptor;
   if (!encryptor.Init(encryption_key, crypto::Encryptor::CBC, iv))
     return false;
 
   // Strip off the versioning prefix before decrypting.
-  std::string raw_ciphertext =
-      ciphertext.substr(strlen(kObfuscationPrefix[version]));
+  const std::string raw_ciphertext =
+      ciphertext.substr(obfuscation_prefix.length());
 
   if (!encryptor.Decrypt(raw_ciphertext, plaintext)) {
     VLOG(1) << "Decryption failed";
@@ -243,7 +231,7 @@ void OSCrypt::SetConfig(std::unique_ptr<os_crypt::Config> config) {
 
 // static
 bool OSCrypt::IsEncryptionAvailable() {
-  return g_get_password[Version::V11]();
+  return GetPasswordV11();
 }
 
 // static
@@ -268,10 +256,9 @@ void OSCrypt::SetRawEncryptionKey(const std::string& raw_key) {
 
 // static
 std::string OSCrypt::GetRawEncryptionKey() {
-  crypto::SymmetricKey* key = g_get_password[Version::V11]();
-  if (!key)
-    return std::string();
-  return key->key();
+  if (crypto::SymmetricKey* key = GetPasswordV11())
+    return key->key();
+  return std::string();
 }
 
 // static
