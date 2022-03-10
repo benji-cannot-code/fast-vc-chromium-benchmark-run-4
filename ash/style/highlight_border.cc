6583e4148bff138edf608f573d2ecab6e0b9b6ef
Cattalyya Nuengsigkapian <cattalyya@chromium.org>
2022-03-10 19:59:54 +0000

    dark_mode: Fix highlight border paint to include ScopedCanvas
    To make sure `UndoDeviceScaleFactor()` is reverted after usage,
    add ScopedCanvas in Paint().
    
    Bug: 1304335
    Test: Manual
    Change-Id: I06d76bb19988f6b527c130897f53d375ee4183f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513426
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Cattalyya Nuengsigkapian <cattalyya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979913}

--- ash/style/highlight_border.cc
+++ ash/style/highlight_border.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/dip_util.h"
+#include "ui/gfx/scoped_canvas.h"
 #include "ui/views/view.h"
 
 namespace ash {
@@ -52,6 +53,7 @@ void HighlightBorder::PaintBorderToCanvas(gfx::Canvas* canvas,
 
   // Scale bounds and corner radius with device scale factor to make sure
   // border bounds match content bounds but keep border stroke width the same.
+  gfx::ScopedCanvas scoped_canvas(canvas);
   const float dsf = canvas->UndoDeviceScaleFactor();
   const gfx::RectF pixel_bounds = gfx::ConvertRectToPixels(bounds, dsf);
   const float scaled_corner_radius = dsf * corner_radius;
