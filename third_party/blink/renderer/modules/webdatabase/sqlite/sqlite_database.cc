e3a16c41d98027f19bbc05a634772e2cc7aa6841
Victor Costan <pwnall@chromium.org>
2022-03-10 22:10:45 +0000

    sql: Use sqlite3_extended_errcode() instead of sqlite3_errcode().
    The two functions return the same results in Chrome, because we enable
    SQLite's extended error codes when we open databases.
    sqlite3_extended_errcode() is a bit clearer to read and, as a bonus, its
    implementation is one opcode shorter.
    
    Change-Id: I179f5482ef4ab3df7e08fa565020d964f449c8fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3516853
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979971}

--- third_party/blink/renderer/modules/webdatabase/sqlite/sqlite_database.cc
+++ third_party/blink/renderer/modules/webdatabase/sqlite/sqlite_database.cc
@@ -285,7 +285,7 @@ int SQLiteDatabase::LastChanges() {
 }
 
 int SQLiteDatabase::LastError() {
-  return db_ ? sqlite3_errcode(db_) : open_error_;
+  return db_ ? sqlite3_extended_errcode(db_) : open_error_;
 }
 
 const char* SQLiteDatabase::LastErrorMsg() {
