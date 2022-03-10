d3c2e646ea637b2164d23c25da662139e8c58c64
Marc Treib <treib@chromium.org>
2022-03-10 07:53:48 +0000

    SyncUserSettingsImpl: rename ResolvePreferredTypes helper
    As suggested on crrev.com/c/3507177: This helper really has nothing to
    do with "preferred" types, it just converts from UserSelectableTypeSet
    to ModelTypeSet. This CL renames the helper function accordingly.
    No behavior changes.
    
    Bug: 950874
    Change-Id: I295a0703772c51a5803822f297c31b5becccd82e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3512854
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979658}

--- chrome/browser/sync/test/integration/enable_disable_test.cc
+++ chrome/browser/sync/test/integration/enable_disable_test.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/driver/glue/sync_transport_data_prefs.h"
 #include "components/sync/driver/sync_service_impl.h"
-#include "components/sync/driver/sync_user_settings_impl.h"
 #include "components/sync/test/fake_server/bookmark_entity_builder.h"
 #include "components/sync/test/fake_server/entity_builder_factory.h"
 #include "content/public/test/browser_test.h"
@@ -48,7 +47,7 @@ ModelTypeSet MultiGroupTypes(const ModelTypeSet& registered_types) {
   ModelTypeSet multi;
   for (UserSelectableType type : UserSelectableTypeSet::All()) {
     const ModelTypeSet grouped_types =
-        syncer::SyncUserSettingsImpl::ResolvePreferredTypesForTesting({type});
+        syncer::UserSelectableTypeToAllModelTypes(type);
     for (ModelType grouped_type : grouped_types) {
       if (seen.Has(grouped_type)) {
         multi.Put(grouped_type);
@@ -112,7 +111,7 @@ class EnableDisableSingleClientTest : public SyncTest {
 
   ModelTypeSet ResolveGroup(UserSelectableType type) {
     ModelTypeSet grouped_types =
-        syncer::SyncUserSettingsImpl::ResolvePreferredTypesForTesting({type});
+        syncer::UserSelectableTypeToAllModelTypes(type);
     grouped_types.RetainAll(registered_data_types_);
     return grouped_types;
   }
