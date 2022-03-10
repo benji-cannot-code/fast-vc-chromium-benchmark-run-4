f3d728730ed4de52b33dfcc1fd689334862112b7
Xing Liu <xingliu@chromium.org>
2022-03-10 18:26:40 +0000

    Reland: "Enable high priority download by default"
    Reland Ifee7b3ddb6653ddda35480385d43b74518d1ceb5, which was reverted
    in e8b5ba87dcd3147c32fce3c582ca5e27f2655531.
    
    Bug: 1303017
    Change-Id: I703e04ee57b464eee7ce9bb6685be0e7d3a3f46a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514820
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979860}

--- components/optimization_guide/core/optimization_guide_features.cc
+++ components/optimization_guide/core/optimization_guide_features.cc
@@ -425,7 +425,7 @@ bool IsModelDownloadingEnabled() {
 bool IsUnrestrictedModelDownloadingEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kOptimizationGuideModelDownloading, "unrestricted_model_downloading",
-      false);
+      true);
 }
 
 bool IsPageContentAnnotationEnabled() {
