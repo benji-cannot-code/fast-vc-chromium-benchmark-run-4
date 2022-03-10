470bc4cdad62437e196e41815baf5263aef7efdb
Mitsuru Oshima <oshima@chromium.org>
2022-03-10 18:22:30 +0000

    [lacros/exo] add set_window_bounds/origin_change to aura toplevel
    This is reland of crrev.com/c/3496388
    
    Server side change to support screen coordinates.
    
    * Update aura-shell/aura-toplevel to include requesting a change in the
    window origin, as well as receiving notifications as to when the
    origin changes. This is required to support several JS APIs,
    as well as window position restoration.
    * Do not accept the bounds change during drag&drop because the server
    side bounds should take a priority during drag.
    
    Bug: 1261321, 1245499, 1245490
    Test: covered by unittest
    Change-Id: If270b0bff330ac4cf2078976ea20e288bb31cbc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3516505
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979857}

--- components/exo/test/exo_test_data_exchange_delegate.cc
+++ components/exo/test/exo_test_data_exchange_delegate.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/files/file_util.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/pickle.h"
 #include "base/strings/string_split.h"
@@ -96,6 +97,37 @@ std::vector<ui::FileInfo> TestDataExchangeDelegate::ParseFileSystemSources(
       file_info.push_back(ui::FileInfo(std::move(path), base::FilePath()));
   }
   return file_info;
+}
+
+TestDataSourceDelegate::TestDataSourceDelegate() = default;
+TestDataSourceDelegate::~TestDataSourceDelegate() = default;
+
+void TestDataSourceDelegate::OnSend(const std::string& mime_type,
+                                    base::ScopedFD fd) {
+  constexpr char kText[] = "test";
+  if (data_map_.empty()) {
+    base::WriteFileDescriptor(fd.get(), kText);
+  } else {
+    base::WriteFileDescriptor(fd.get(), data_map_[mime_type]);
+  }
+}
+
+void TestDataSourceDelegate::OnCancelled() {
+  cancelled_ = true;
+}
+
+void TestDataSourceDelegate::OnDndFinished() {
+  finished_ = true;
+}
+
+bool TestDataSourceDelegate::CanAcceptDataEventsForSurface(
+    Surface* surface) const {
+  return true;
+}
+
+void TestDataSourceDelegate::SetData(const std::string& mime_type,
+                                     std::vector<uint8_t> data) {
+  data_map_[mime_type] = std::move(data);
 }
 
 }  // namespace exo
