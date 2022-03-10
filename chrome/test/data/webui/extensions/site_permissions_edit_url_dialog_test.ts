bfa6fea0ab068053fdf7a1b06df4e70f31202b48
Kelvin Jiang <kelvinjiang@chromium.org>
2022-03-10 22:31:15 +0000

    [Extensions] Disallow wildcard schemes for user specified sites
    This CL revises the valid regex for user specified sites entered through
    WebUI by disallowing wildcard schemes
    
    Bug: 1305069
    Change-Id: I6751d141e6c0aee8b1abe5f65869f922a84e7914
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3515496
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979981}

--- chrome/test/data/webui/extensions/site_permissions_edit_url_dialog_test.ts
+++ chrome/test/data/webui/extensions/site_permissions_edit_url_dialog_test.ts
@@ -64,6 +64,12 @@ suite('SitePermissionsEditUrlDialog', function() {
     input.fire('input');
     assertFalse(input.invalid);
     assertFalse(submit.disabled);
+
+    // Wildcard scheme is considered invalid input.
+    input.value = '*://www.example.com';
+    input.fire('input');
+    assertTrue(input.invalid);
+    assertTrue(submit.disabled);
   });
 
   test('editing current site', async function() {
