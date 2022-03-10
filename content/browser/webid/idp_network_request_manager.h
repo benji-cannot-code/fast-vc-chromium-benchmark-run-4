c9003240deac04918cad3b697adb81da6cfdf275
Nicolás Peña Moreno <npm@chromium.org>
2022-03-10 00:07:44 +0000

    [FedCM] Rename revoke_endpoint to revocation_endpoint
    Bug: 1304893
    Change-Id: Ie0c083c7727ef1f46db36dadfe44e6a0c88c0443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514873
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#979516}

--- content/browser/webid/idp_network_request_manager.h
+++ content/browser/webid/idp_network_request_manager.h
@@ -96,7 +96,7 @@ class CONTENT_EXPORT IdpNetworkRequestManager {
     std::string token;
     std::string accounts;
     std::string client_metadata;
-    std::string revoke;
+    std::string revocation;
   };
 
   struct ClientMetadata {
