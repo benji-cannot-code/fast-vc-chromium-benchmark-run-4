dd4d41bed7ae2efa6b1cad015308e0dd3e3f0d8e
Guillaume Jenkins <gujen@google.com>
2022-03-10 18:09:55 +0000

    [Notes] Add interface for the UI layer
    - Creates the interface that the UI layer for the Notes feature needs to
      implement to fulfill commands from the UserNoteService and the rest of
      the User Notes component.
    - Moves interfaces to their own user_notes/interfaces directory so that
      consumers of the component can have more granularity with their deps
      if they only need to implement an interface.
    
    Bug: 1302738
    Change-Id: I3243968e32339092b0e1bdd7740a23871ed3d70d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3501909
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#979849}

--- components/user_notes/browser/user_note_service.h
+++ components/user_notes/browser/user_note_service.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "components/user_notes/browser/user_notes_ui_delegate.h"
+#include "components/user_notes/interfaces/user_notes_ui_delegate.h"
 
 namespace user_notes {
 
--- components/user_notes/interfaces/user_notes_ui_delegate.h
+++ components/user_notes/interfaces/user_notes_ui_delegate.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_USER_NOTES_BROWSER_USER_NOTES_UI_DELEGATE_H_
-#define COMPONENTS_USER_NOTES_BROWSER_USER_NOTES_UI_DELEGATE_H_
+#ifndef COMPONENTS_USER_NOTES_INTERFACES_USER_NOTES_UI_DELEGATE_H_
+#define COMPONENTS_USER_NOTES_INTERFACES_USER_NOTES_UI_DELEGATE_H_
 
 #include <string>
 
@@ -32,4 +32,4 @@ class UserNotesUIDelegate {
 
 }  // namespace user_notes
 
-#endif  // COMPONENTS_USER_NOTES_BROWSER_USER_NOTES_UI_DELEGATE_H_
+#endif  // COMPONENTS_USER_NOTES_INTERFACES_USER_NOTES_UI_DELEGATE_H_
