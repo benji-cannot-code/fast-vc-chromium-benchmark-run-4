da78bca605f2fae923a97087326023f37501f212
Vadym Kargin <vadym.kargin@accenture.com>
2022-03-11 19:43:28 +0000

    Replace DOMException with V8ThrowDOMException in the scheduler module.
    This CL is a part of chain of updates for replacing DOMException with V8ThrowDOMException.
    
    Issue: Replace DOMException with ExceptionState/V8ThrowDOMException
    Bug: 991544
    Change-Id: Id28c1999076c37db656c4b611ea04d52bc382ef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498295
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Vadym Kargin <vadym.kargin@accenture.com>
    Cr-Commit-Position: refs/heads/main@{#980224}

--- third_party/blink/renderer/modules/scheduler/dom_task.cc
+++ third_party/blink/renderer/modules/scheduler/dom_task.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/metrics/histogram_macros.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_throw_dom_exception.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_scheduler_post_task_callback.h"
 #include "third_party/blink/renderer/core/dom/abort_signal.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
@@ -148,9 +150,28 @@ void DOMTask::OnAbort() {
   if (!callback_)
     return;
 
-  task_handle_.Cancel();
-  resolver_->Reject(
-      MakeGarbageCollected<DOMException>(DOMExceptionCode::kAbortError));
+  DCHECK(resolver_);
+
+  ScriptState* resolver_script_state = resolver_->GetScriptState();
+
+  if (!IsInParallelAlgorithmRunnable(resolver_->GetExecutionContext(),
+                                     resolver_script_state)) {
+    task_handle_.Cancel();
+    async_task_context_.Cancel();
+    return;
+  }
+
+  // switch to the resolver's context to let DOMException pick up the resolver's
+  // JS stack
+  {
+    ScriptState::Scope script_state_scope(resolver_script_state);
+
+    task_handle_.Cancel();
+    // TODO(crbug.com/1293949): Add an error message.
+    resolver_->Reject(
+        V8ThrowDOMException::CreateOrDie(resolver_script_state->GetIsolate(),
+                                         DOMExceptionCode::kAbortError, ""));
+  }
 
   ScriptState* script_state =
       callback_->CallbackRelevantScriptStateOrReportError("DOMTask", "Abort");
