00d7fa8d3b03457652b0ab5e93692a310d1661ae
Dana Fried <dfried@chromium.org>
2022-03-11 01:54:03 +0000

    Delay or avoid hover card preview image capture under memory pressure.
    Hover card controller now watches memory pressure:
     - None: behavior does not change.
     - Moderate: longer delay before asking for image capture.
     - Critical: no image capture.
    
    In the case of moderate memory pressure, this makes the choice by a
    user to initiate conversion or capture of thumbnails significantly more
    deliberate, as they will have to hover a tab for significantly longer
    before capture happens (and of course, in a critical situation, no
    capture at all is allowed).
    
    The added delay affects both new capture and conversion of stored
    thumbnails into a form that can be displayed, as both have non-zero
    memory cost. However, because conversion of existing images is by
    default immediate (and has a lower cost), while capture of new images is
    already on a delay, conversion of new images still happens faster than
    capture of new thumbnails.
    
    Change-Id: Ie5fa678023be64f5c4acb58da04c429a16859950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514029
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980078}

--- chrome/browser/ui/views/tabs/tab_hover_card_controller.h
+++ chrome/browser/ui/views/tabs/tab_hover_card_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/gtest_prod_util.h"
+#include "base/memory/memory_pressure_listener.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
@@ -109,6 +110,9 @@ class TabHoverCardController : public views::ViewObserver,
   void OnPreviewImageAvaialble(TabHoverCardThumbnailObserver* observer,
                                gfx::ImageSkia thumbnail_image);
 
+  void OnMemoryPressureChanged(
+      base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level);
+
   TabHoverCardMetrics* metrics_for_testing() const { return metrics_.get(); }
 
   bool waiting_for_preview() const {
@@ -146,6 +150,12 @@ class TabHoverCardController : public views::ViewObserver,
   base::CallbackListSubscription fade_complete_subscription_;
   base::CallbackListSubscription slide_progressed_subscription_;
   base::CallbackListSubscription slide_complete_subscription_;
+
+  // Track memory pressure on the system. We'll delay or stop requesting
+  // previews if memory pressure gets too high.
+  base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level_ =
+      base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_NONE;
+  std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
 
   // Ensure that this timer is destroyed before anything else is cleaned up.
   base::OneShotTimer delayed_show_timer_;
