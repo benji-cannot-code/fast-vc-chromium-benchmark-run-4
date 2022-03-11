c8aac6bf181e112b16d26b93ae664c49dc5bcf85
Peter Kasting <pkasting@chromium.org>
2022-03-11 16:05:19 +0000

    Eliminate hardcoded colors from eye_dropper_view.cc.
    Bug: 1059923
    Change-Id: I9d34c09e1d07b437ec808aa62d068d467741df59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518451
    Reviewed-by: Ionel Popescu <iopopesc@microsoft.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980204}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -52,6 +52,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorDownloadToolbarButtonActive) \
   E_CPONLY(kColorDownloadToolbarButtonInactive) \
   E_CPONLY(kColorDownloadToolbarButtonRingBackground) \
+  /* Eyedropper colors. */ \
+  E_CPONLY(kColorEyedropperBoundary) \
+  E_CPONLY(kColorEyedropperCentralPixelInnerRing) \
+  E_CPONLY(kColorEyedropperCentralPixelOuterRing) \
+  E_CPONLY(kColorEyedropperGrid) \
   /* Feature Promo bubble colors. */ \
   E(kColorFeaturePromoBubbleBackground, \
     ThemeProperties::COLOR_FEATURE_PROMO_BUBBLE_BACKGROUND) \
