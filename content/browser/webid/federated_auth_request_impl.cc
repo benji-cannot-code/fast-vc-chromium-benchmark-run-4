2632f7e4567dc6b2cbc221716e27afa7ff1ca334
Christian Biesinger <cbiesinger@chromium.org>
2022-03-11 18:00:29 +0000

    Implement RP sign out
    R=yigu@chromium.org
    
    Fixed: 1266054
    Change-Id: I97994ce147d754efbbb4acb3141ba819e28bb9f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513425
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980217}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using blink::mojom::FederatedAuthRequestResult;
 using blink::mojom::LogoutRpsStatus;
+using blink::mojom::LogoutStatus;
 using blink::mojom::RequestIdTokenStatus;
 using blink::mojom::RevokeStatus;
 using LoginState = content::IdentityRequestAccount::LoginState;
@@ -357,6 +358,20 @@ void FederatedAuthRequestImpl::Revoke(
   FetchManifest(
       base::BindOnce(&FederatedAuthRequestImpl::OnManifestFetchedForRevoke,
                      weak_ptr_factory_.GetWeakPtr()));
+}
+
+void FederatedAuthRequestImpl::Logout(
+    const GURL& provider,
+    const std::string& account_id,
+    blink::mojom::FederatedAuthRequest::LogoutCallback callback) {
+  url::Origin idp_origin(url::Origin::Create(provider));
+  auto* context = GetActiveSessionPermissionContext();
+  if (!context || !context->HasActiveSession(origin_, idp_origin, account_id)) {
+    std::move(callback).Run(LogoutStatus::kNotLoggedIn);
+    return;
+  }
+  context->RevokeActiveSession(origin_, idp_origin, account_id);
+  std::move(callback).Run(LogoutStatus::kSuccess);
 }
 
 // TODO(kenrb): Depending on how this code evolves, it might make sense to
--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-mock.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-mock.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-import { RequestIdTokenStatus, LogoutRpsStatus, RevokeStatus, FederatedAuthRequest, FederatedAuthRequestReceiver } from '/gen/third_party/blink/public/mojom/webid/federated_auth_request.mojom.m.js';
+import { RequestIdTokenStatus, LogoutStatus, LogoutRpsStatus, RevokeStatus, FederatedAuthRequest, FederatedAuthRequestReceiver } from '/gen/third_party/blink/public/mojom/webid/federated_auth_request.mojom.m.js';
 
 function toMojoIdTokenStatus(status) {
   return RequestIdTokenStatus["k" + status];
@@ -16,6 +16,7 @@ export class MockFederatedAuthRequest {
     this.interceptor_.start();
     this.idToken_ = null;
     this.status_ = RequestIdTokenStatus.kError;
+    this.logoutStatus_ = LogoutStatus.kNotLoggedIn;
     this.logoutRpsStatus_ = LogoutRpsStatus.kError;
     this.revokeStatus_ = RevokeStatus.kError;
     this.returnPending_ = false;
@@ -45,6 +46,13 @@ export class MockFederatedAuthRequest {
   }
 
   logoutReturn(status) {
+    let validated = LogoutStatus[status];
+    if (validated === undefined)
+      throw new Error("Invalid status: " + status);
+    this.logoutStatus_ = validated;
+  }
+
+  logoutRpsReturn(status) {
     let validated = LogoutRpsStatus[status];
     if (validated === undefined)
       throw new Error("Invalid status: " + status);
@@ -81,6 +89,10 @@ export class MockFederatedAuthRequest {
       idToken: null
     });
     this.pendingPromiseResolve_ = null;
+  }
+
+  async logout() {
+    return Promise.resolve({status: this.logoutStatus_});
   }
 
   async logoutRps(logout_endpoints) {
