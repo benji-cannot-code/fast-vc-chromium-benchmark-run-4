64738bd1b5cc4887f1ca31cafd9db465b1196122
Nick Yamane <nickdiego@igalia.com>
2022-03-14 23:11:31 +0000

    lacros: use WaylandWindow::AsWeakPtr in XDGSurfaceWrapperImpl::Configure
    ...instead of adding an unneeded WeakPtrFactory variable, etc. This
    basically simplifies https://crrev.com/c/3519638.
    
    Bug: 1303425
    Change-Id: I377f32931a7939230d22d01ff7b4daadec75d955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3522904
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#980831}

--- ui/ozone/platform/wayland/host/xdg_surface_wrapper_impl.h
+++ ui/ozone/platform/wayland/host/xdg_surface_wrapper_impl.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 
-#include "base/memory/weak_ptr.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 
 namespace gfx {
@@ -45,8 +44,6 @@ class XDGSurfaceWrapperImpl : public ShellSurfaceWrapper {
                         struct xdg_surface* xdg_surface,
                         uint32_t serial);
 
-  void OnConfigure(uint32_t serial);
-
   // Non-owing WaylandWindow that uses this surface wrapper.
   WaylandWindow* const wayland_window_;
   WaylandConnection* const connection_;
@@ -54,8 +51,6 @@ class XDGSurfaceWrapperImpl : public ShellSurfaceWrapper {
   bool is_configured_ = false;
 
   wl::Object<struct xdg_surface> xdg_surface_;
-
-  base::WeakPtrFactory<XDGSurfaceWrapperImpl> weak_ptr_factory_{this};
 };
 
 }  // namespace ui
