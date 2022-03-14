4db9cda02202da915e9e0d913199924121c8ffcb
Tommy C. Li <tommycli@chromium.org>
2022-03-14 19:21:46 +0000

    [journeys] For labelled clusters, move menu to the label row itself
    After this CL, for labelled clusters, the three dot menu and timestamp
    are attached to the header label itself, rather than the top visit.
    
    This required making the menu container its own reusable
    <menu-container> Web Component, with parameters to allow it to be used
    in all three contexts:
     1. For the header row of labelled clusters.
     2. For the "top visit" of unlabelled clusters.
     3. For ordinary visits, as a shown-on-hover action menu only.
    
    Screenshot of the before:
    https://screenshot.googleplex.com/5ZWsFPAqLVo8CAc
    
    Screenshot of the after:
    https://screenshot.googleplex.com/69EVD5SHxmjDvGL
    
    Bug: 1294348
    Change-Id: Idbf8bac0a97ce54d851e1364aeb23b36235fdbb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518006
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980712}

--- chrome/browser/resources/history/history_clusters/cluster.ts
+++ chrome/browser/resources/history/history_clusters/cluster.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './menu_container.js';
 import './search_query.js';
 import './shared_style.js';
 import './shared_vars.js';
