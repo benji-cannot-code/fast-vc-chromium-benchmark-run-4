2806298bf5525f801a37e7521ca762e20e4c02d6
Kevin Lubick <kjlubick@google.com>
2022-03-14 18:50:45 +0000

    Fix Skia transitive dependencies
    Bug: 242216
    Change-Id: I405cb4c47a12cfefaf88c463e543e3b3b0e85cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517906
    Auto-Submit: Kevin Lubick <kjlubick@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980698}

--- ash/hud_display/graph.cc
+++ ash/hud_display/graph.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBlendMode.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
+#include "third_party/skia/include/core/SkPoint.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/rect_f.h"
 
