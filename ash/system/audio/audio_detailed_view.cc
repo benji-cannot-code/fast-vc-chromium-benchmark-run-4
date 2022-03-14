d62af2d0c77d54624c183005bd02b60f14395421
Jimmy Gong <jimmyxgong@google.com>
2022-03-14 21:47:42 +0000

    cras: Request NC support after Cras Service is available
    Request Noise Cancellation support as soon as the Cras service is
    available. This CL is tied to crrev/c/3508996 in which the NC support
    state is available during Cras initialization.
    
    Removed irrelevant tests.
    
    Bug: b:218398659, b:221115011
    Change-Id: Ic68fda5bb67998d9fff4ad1d79bf49326e1d356a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514291
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980791}

--- ash/system/audio/audio_detailed_view.cc
+++ ash/system/audio/audio_detailed_view.cc
@@ -84,9 +84,6 @@ AudioDetailedView::AudioDetailedView(DetailedViewDelegate* delegate)
     : TrayDetailedView(delegate) {
   CreateItems();
 
-  CrasAudioHandler::Get()->RequestNoiseCancellationSupported(
-      base::BindOnce(&AudioDetailedView::Update, base::Unretained(this)));
-
   Shell::Get()->accessibility_controller()->AddObserver(this);
 }
 
