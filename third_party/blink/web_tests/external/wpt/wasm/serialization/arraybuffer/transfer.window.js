ff2b6883a771124ab3b4400de77ac2ffcb01ea9a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2022-03-14 19:25:55 +0000

    Import wpt@f0d3621b1a7981b26229568c093b2a2f508eef90
    Using wpt-import in Chromium 27217651767ed8d671ddf68d9e26e3738eb2750d.
    With Chromium commits locally applied on WPT:
    9903cb3f09 "[Trusted Types] Ensure Trusted Types check runs on all Attr methods."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    jsbell@chromium.org:
      external/wpt/resources
    rego@igalia.com:
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,linux-blink-rel
    Change-Id: I3874ca7e46f39dd0359697a63f1aa691a0e478b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3522181
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#980715}

--- third_party/blink/web_tests/external/wpt/wasm/serialization/arraybuffer/transfer.window.js
+++ third_party/blink/web_tests/external/wpt/wasm/serialization/arraybuffer/transfer.window.js
@@ -1,7 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 test(() => {
   const buffer = new WebAssembly.Memory({initial: 4}).buffer;
+  postMessage(buffer, '*');
+}, "Serializing a WebAssembly.Memory-backed ArrayBuffer works");
+
+test(() => {
+  const buffer = new WebAssembly.Memory({initial: 4}).buffer;
   assert_throws_js(TypeError, () => {
     postMessage('foo', '*', [buffer]);
   });
-});
+}, "Transfering a WebAssembly.Memory-backed ArrayBuffer throws");
