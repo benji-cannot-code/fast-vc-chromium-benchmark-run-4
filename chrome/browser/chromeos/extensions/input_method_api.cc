700652553db73291489d7d4aaa52cc7104950f53
David Bertoni <dbertoni@chromium.org>
2022-03-14 22:21:54 +0000

    [CodeHealth] Remove uses of ListValue::Append() in /chrome/browser/chromeos/extensions.
    This CL removes calls to this deprecated member function. When
    it's not too intrusive, this CL attempts to modernize the use
    of DictionaryValue and ListValue to base::Value::Dict and
    base::Value::List to avoid using Value::FromUniquePtrValue.
    
    This CL was uploaded by git cl split.
    
    R=rdevlin.cronin@chromium.org
    
    Bug: 1187097
    Change-Id: Ia190b567fb87768ac9a4f75dd6a21aa44486a1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3516995
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980810}

--- chrome/browser/chromeos/extensions/input_method_api.cc
+++ chrome/browser/chromeos/extensions/input_method_api.cc
@@ -181,7 +181,7 @@ InputMethodPrivateSwitchToLastUsedInputMethodFunction::Run() {
 
 ExtensionFunction::ResponseAction
 InputMethodPrivateGetInputMethodsFunction::Run() {
-  std::unique_ptr<base::ListValue> output(new base::ListValue());
+  base::Value::List output;
   auto* manager = ash::input_method::InputMethodManager::Get();
   ash::input_method::InputMethodUtil* util = manager->GetInputMethodUtil();
   scoped_refptr<ash::input_method::InputMethodManager::State> ime_state =
@@ -191,14 +191,13 @@ InputMethodPrivateGetInputMethodsFunction::Run() {
   for (size_t i = 0; i < input_methods->size(); ++i) {
     const ash::input_method::InputMethodDescriptor& input_method =
         (*input_methods)[i];
-    auto val = std::make_unique<base::DictionaryValue>();
-    val->SetStringKey("id", input_method.id());
-    val->SetStringKey("name", util->GetInputMethodLongName(input_method));
-    val->SetStringKey("indicator", input_method.GetIndicator());
-    output->Append(std::move(val));
+    base::Value::Dict val;
+    val.Set("id", input_method.id());
+    val.Set("name", util->GetInputMethodLongName(input_method));
+    val.Set("indicator", input_method.GetIndicator());
+    output.Append(std::move(val));
   }
-  return RespondNow(
-      OneArgument(base::Value::FromUniquePtrValue(std::move(output))));
+  return RespondNow(OneArgument(base::Value(std::move(output))));
 }
 
 ExtensionFunction::ResponseAction
@@ -591,8 +590,6 @@ InputMethodPrivateSetAutocorrectRangeFunction::Run() {
   if (!engine->InputMethodEngine::SetAutocorrectRange(
           params.context_id,
           gfx::Range(params.selection_start, params.selection_end), &error)) {
-    auto results = std::make_unique<base::ListValue>();
-    results->Append(std::make_unique<base::Value>(false));
     return RespondNow(Error(InformativeError(error, static_function_name())));
   }
   return RespondNow(NoArguments());
@@ -615,7 +612,7 @@ InputMethodPrivateSetSelectionRangeFunction::Run() {
           params.context_id, *params.selection_start, *params.selection_end,
           &error)) {
     auto results = std::make_unique<base::ListValue>();
-    results->Append(std::make_unique<base::Value>(false));
+    results->Append(false);
     return RespondNow(ErrorWithArguments(
         std::move(results), InformativeError(error, static_function_name())));
   }
