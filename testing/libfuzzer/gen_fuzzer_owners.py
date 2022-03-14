74ade8c1f1891cc96e93d4794feabc51e0d338db
Joshua Hood <jdh@chromium.org>
2022-03-14 16:18:50 +0000

    [CodeHealth] Migrate testing/libfuzzer/ to Python 3 compatible syntax
    and fix resulting breakages, to permit future migration of everything
    under testing/ to Python3
    
    Bug: 1262363
    Change-Id: I802be2e0135be9ec32777eac3c885b1beacfaa41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3510055
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Auto-Submit: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Commit-Queue: Mike Pinkerton <pinkerton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980591}

--- testing/libfuzzer/gen_fuzzer_owners.py
+++ testing/libfuzzer/gen_fuzzer_owners.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 #
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -65,6 +65,7 @@ def GetOwnersIfThirdParty(source):
 
   return None
 
+# pylint: disable=inconsistent-return-statements
 def GetOwnersForFuzzer(sources):
   """Return owners given a list of sources as input."""
   if not sources:
@@ -102,7 +103,7 @@ def GetOwnersForFuzzer(sources):
       return GetAuthorFromGitBlame(blame_output)
 
   return None
-
+# pylint: enable=inconsistent-return-statements
 
 def FindGroupsAndDepsInDeps(deps_list, build_dir):
   """Return list of groups, as well as their deps, from a list of deps."""
@@ -193,7 +194,7 @@ def GNPath():
 
 def SubStringExistsIn(substring_list, string):
   """Return true if one of the substring in the list is found in |string|."""
-  return any([substring in string for substring in substring_list])
+  return any(substring in string for substring in substring_list)
 
 
 def main():
--- testing/libfuzzer/tests/check_fuzzer_config.py
+++ testing/libfuzzer/tests/check_fuzzer_config.py
@@ -1,19 +1,23 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 # Script that prints out "option=value" from the config file. Used for testing.
 
-import ConfigParser
 import os
 import sys
+
+if sys.version_info.major == 2:
+    from ConfigParser import ConfigParser
+else:
+    from configparser import ConfigParser
 
 OPTIONS_SECTION_LIBFUZZER = 'libfuzzer'
 
 config_path = os.path.join(os.path.dirname(sys.argv[0]), sys.argv[1])
-fuzzer_config = ConfigParser.ConfigParser()
+fuzzer_config = ConfigParser()
 fuzzer_config.read(config_path)
 
 if not fuzzer_config.has_section(OPTIONS_SECTION_LIBFUZZER):
