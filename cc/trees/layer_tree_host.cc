beed893fe3d8f79dccf7ea64831440457fd23a85
Robert Flack <flackr@chromium.org>
2022-03-14 18:09:19 +0000

    Revert "Reland "Remove tracking of which animating elements exist.""
    This reverts commit 85c7ec9d9caf533428c6b40f85d3b03de4c4f81c.
    
    Reason for revert: Causing crashes due to animating non-existent property nodes https://crbug.com/1305503 https://crbug.com/1305487
    
    Original change's description:
    > Reland "Remove tracking of which animating elements exist."
    >
    > The crashes seen last time should have been resolved by https://chromium-review.googlesource.com/c/chromium/src/+/3504027, https://chromium-review.googlesource.com/c/chromium/src/+/3498651, and https://chromium-review.googlesource.com/c/chromium/src/+/3517088
    >
    > Original change's description:
    > > Remove tracking of which animating elements exist.
    > >
    > > Prior to property trees, composited animations were created before their
    > > corresponding layers existed. However, the blink lifecycle now ensures
    > > that composited animations are created in sync with their property tree
    > > nodes such that they always exist in the tree. Removing this code saves
    > > time and will unlock further simplifications of the code.
    > >
    > > The ui animation infrastructure eagerly creates layers before their
    > > animations so it doesn't have this issue and the LayerAnimator class
    > > ensures that animations are cleaned up on Layer removal.
    > >
    > > Bug: 900241
    > > Change-Id: If854cf67ba8bd21e727e42475d26ce1a846a7674
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3200347
    > > Reviewed-by: David Baron <dbaron@chromium.org>
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Commit-Queue: Robert Flack <flackr@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#975833}
    >
    > Bug: 900241
    > Change-Id: I59b620322c77bb98d935f8785fbb87afad8e7be1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498292
    > Reviewed-by: David Baron <dbaron@chromium.org>
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#980063}
    
    Bug: 900241
    Change-Id: I73c960f092773d193639c64322881b655ee00505
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517538
    Auto-Submit: Robert Flack <flackr@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980670}

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <memory>
 #include <string>
+#include <unordered_map>
 
 #include "base/atomic_sequence_num.h"
 #include "base/auto_reset.h"
@@ -1538,12 +1539,20 @@ void LayerTreeHost::RegisterLayer(Layer* layer) {
   DCHECK(!LayerById(layer->id()));
   DCHECK(!in_paint_layer_contents_);
   layer_id_map_[layer->id()] = layer;
+  if (!IsUsingLayerLists() && layer->element_id()) {
+    mutator_host()->RegisterElementId(layer->element_id(),
+                                      ElementListType::ACTIVE);
+  }
 }
 
 void LayerTreeHost::UnregisterLayer(Layer* layer) {
   DCHECK(IsMainThread());
   DCHECK(LayerById(layer->id()));
   DCHECK(!in_paint_layer_contents_);
+  if (!IsUsingLayerLists() && layer->element_id()) {
+    mutator_host()->UnregisterElementId(layer->element_id(),
+                                        ElementListType::ACTIVE);
+  }
   pending_commit_state()->layers_that_should_push_properties.erase(layer);
   layer_id_map_.erase(layer->id());
 }
@@ -1679,14 +1688,20 @@ void LayerTreeHost::RegisterElement(ElementId element_id,
                                     Layer* layer) {
   DCHECK(IsMainThread());
   element_layers_map_[element_id] = layer;
+  if (!IsUsingLayerLists())
+    mutator_host()->RegisterElementId(element_id, ElementListType::ACTIVE);
 }
 
 void LayerTreeHost::UnregisterElement(ElementId element_id) {
   DCHECK(IsMainThread());
-  if (!IsUsingLayerLists()) {
-    mutator_host()->RemoveElementId(element_id);
-  }
+  if (!IsUsingLayerLists())
+    mutator_host()->UnregisterElementId(element_id, ElementListType::ACTIVE);
   element_layers_map_.erase(element_id);
+}
+
+void LayerTreeHost::UpdateActiveElements() {
+  DCHECK(IsUsingLayerLists());
+  mutator_host()->UpdateRegisteredElementIds(ElementListType::ACTIVE);
 }
 
 void LayerTreeHost::SetElementIdsForTesting() {
@@ -1822,8 +1837,7 @@ void LayerTreeHost::ElementIsAnimatingChanged(
     ElementListType list_type,
     const PropertyAnimationState& mask,
     const PropertyAnimationState& state) {
-  if (list_type == ElementListType::PENDING)
-    return;
+  DCHECK_EQ(ElementListType::ACTIVE, list_type);
   property_trees()->ElementIsAnimatingChanged(element_id_map, mask, state,
                                               true);
 }
@@ -1831,8 +1845,7 @@ void LayerTreeHost::ElementIsAnimatingChanged(
 void LayerTreeHost::MaximumScaleChanged(ElementId element_id,
                                         ElementListType list_type,
                                         float maximum_scale) {
-  if (list_type == ElementListType::PENDING)
-    return;
+  DCHECK_EQ(ElementListType::ACTIVE, list_type);
   property_trees()->MaximumAnimationScaleChanged(element_id, maximum_scale);
 }
 
--- cc/trees/mutator_host_client.h
+++ cc/trees/mutator_host_client.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_worklet_input.h"
 #include "cc/trees/property_animation_state.h"
 #include "cc/trees/target_property.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
 
 namespace gfx {
 class Transform;
