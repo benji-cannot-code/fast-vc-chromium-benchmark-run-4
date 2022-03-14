bad95a1c0fde8edef198bfc6e62db3b43c04f28f
Ethan Jimenez <ethavar@microsoft.com>
2022-03-14 19:54:18 +0000

    [GridNG] Disallow copy of NGGridSizingTrackCollection
    1. Refactoring `NGGridLayoutData` to store pointers to the track
       collections instead of the actual objects to allow downcasting.
    
    2. Removing the copy constructor for `NGGridSizingTrackCollection`; when
       caching the grid geometry in the layout result or break token data
       (and for future changes in `NGConstraintSpace`) we should drop the
       `NGGridSet` data since tracks must be immutable at that point.
    
    This change shouldn't have any behavioral impact.
    
    Bug: 618969
    Change-Id: I6dbf36d07b6d8a8d45eef05392c06b7b202b2ebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518294
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#980736}

--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_break_token_data.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_break_token_data.h
@@ -30,7 +30,6 @@ struct GridItemPlacementData {
 };
 
 struct NGGridBreakTokenData final : NGBlockBreakTokenData {
- public:
   NGGridBreakTokenData(
       const NGBlockBreakTokenData* break_token_data,
       const NGGridLayoutData& layout_data,
--- third_party/blink/renderer/core/layout/ng/grid/ng_grid_data.h
+++ third_party/blink/renderer/core/layout/ng/grid/ng_grid_data.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_GRID_NG_GRID_DATA_H_
 
 #include "third_party/blink/renderer/core/layout/ng/grid/ng_grid_track_collection.h"
-#include "third_party/blink/renderer/platform/geometry/layout_unit.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
@@ -44,20 +43,40 @@ struct CORE_EXPORT NGGridPlacementData {
   wtf_size_t row_start_offset;
 };
 
-// This struct contains a bundle of the ranges from the track collection and the
-// resolved used sizes of the sets in a given track direction.
+// This struct contains the column and row data necessary to layout grid items.
+// For grid sizing, it will store |NGGridSizingTrackCollection| pointers, which
+// are able to modify the geometry of its sets. However, after sizing is done,
+// it should only copy |NGGridLayoutTrackCollection| immutable data.
 struct CORE_EXPORT NGGridLayoutData {
   USING_FAST_MALLOC(NGGridLayoutData);
 
  public:
-  NGGridLayoutData() : columns(kForColumns), rows(kForRows) {}
+  NGGridLayoutData() = default;
 
-  NGGridLayoutData(NGGridSizingTrackCollection&& columns,
-                   NGGridSizingTrackCollection&& rows)
-      : columns(std::move(columns)), rows(std::move(rows)) {}
+  NGGridLayoutData(const NGGridLayoutData& other) { CopyFrom(other); }
 
-  NGGridSizingTrackCollection columns;
-  NGGridSizingTrackCollection rows;
+  NGGridLayoutData& operator=(const NGGridLayoutData& other) {
+    CopyFrom(other);
+    return *this;
+  }
+
+  void CopyFrom(const NGGridLayoutData& other) {
+    columns = std::make_unique<NGGridLayoutTrackCollection>(*other.Columns());
+    rows = std::make_unique<NGGridLayoutTrackCollection>(*other.Rows());
+  }
+
+  NGGridLayoutTrackCollection* Columns() const {
+    DCHECK(columns);
+    return columns.get();
+  }
+
+  NGGridLayoutTrackCollection* Rows() const {
+    DCHECK(rows);
+    return rows.get();
+  }
+
+  std::unique_ptr<NGGridLayoutTrackCollection> columns;
+  std::unique_ptr<NGGridLayoutTrackCollection> rows;
 };
 
 }  // namespace blink
