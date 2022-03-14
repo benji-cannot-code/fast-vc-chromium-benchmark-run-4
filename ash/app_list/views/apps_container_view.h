3835952f78fb96cbb745d9001631e088d7930032
Andrew Xu <andrewxu@chromium.org>
2022-03-14 17:19:58 +0000

    [Launcher] Synchronize toast fade in with apps grid fade in animation
    Currently, the toast fade in animation is configured with a different
    animation builder from the one used for the apps grid fade in animation.
    As a result, two animations have to be synchronized manually: when the
    apps grid fade in animation is aborted, the toast fade in should be
    aborted as well. The code is duplicate and fragile.
    
    A recent CL enables the animation sequence block to be carried by an
    animation builder. Therefore, in this CL, the toast fade in animation
    can share the animation builder with the apps grid fade in animation
    easily. Synchronization is guaranteed in this way.
    
    This CL should not bring any noticeable differences.
    
    Bug: 1289462
    Change-Id: I2d9d2848b4467397a452e06a4e133d19c2fbd491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3516698
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980635}

--- ash/app_list/views/apps_container_view.h
+++ ash/app_list/views/apps_container_view.h
@@ -306,10 +306,6 @@ class ASH_EXPORT AppsContainerView
   // `aborted` indicates whether the fade in animation is aborted.
   void OnAppsGridViewFadeInAnimationEnded(bool aborted);
 
-  // Called when the animation to fade in children views such as the undo toast
-  // is completed. `aborted` is true if the animation is aborted.
-  void OnFadeInChildrenAnimationEnded(bool aborted);
-
   // While true, the gradient mask will not be removed as a mask layer until
   // cardified state ends.
   bool keep_gradient_mask_for_cardified_state_ = false;
@@ -364,10 +360,6 @@ class ASH_EXPORT AppsContainerView
   // A closure to update item positions. It should run at the end of the fade
   // out animation when items are reordered.
   base::OnceClosure update_position_closure_;
-
-  // The handle to abort the toast/continue section fade in animation triggered
-  // by app list reorder.
-  std::unique_ptr<views::AnimationAbortHandle> fade_in_abort_handle_;
 
   base::WeakPtrFactory<AppsContainerView> weak_ptr_factory_{this};
 };
