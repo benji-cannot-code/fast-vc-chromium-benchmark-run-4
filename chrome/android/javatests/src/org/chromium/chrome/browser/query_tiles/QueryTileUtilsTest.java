f6d1574432ef9cdaeee2a3d5c70f222c8debba09
Hailey Wang <haileywang@google.com>
2022-03-14 19:41:15 +0000

    [Query Tiles] Record comparison histogram for every request
    Bug: 1261158
    Change-Id: I06683c58207c95de825bbee2d692b06cc12e934c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3508306
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#980723}

--- chrome/android/javatests/src/org/chromium/chrome/browser/query_tiles/QueryTileUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/query_tiles/QueryTileUtilsTest.java
@@ -146,13 +146,13 @@ public class QueryTileUtilsTest {
     public void
     testShowQueryTilesSegmentationResultComparison() {
         QueryTileUtils.setSegmentationResultsForTesting(1 /*DONT_SHOW*/);
-        Assert.assertEquals(0,
+        Assert.assertEquals(1,
                 RecordHistogram.getHistogramTotalCountForTesting(
                         "Search.QueryTiles.ShowQueryTilesSegmentationResultComparison"));
 
         nextDecisionTimeReached();
         Assert.assertTrue(QueryTileUtils.shouldShowQueryTiles());
-        Assert.assertEquals(1,
+        Assert.assertEquals(2,
                 RecordHistogram.getHistogramTotalCountForTesting(
                         "Search.QueryTiles.ShowQueryTilesSegmentationResultComparison"));
         Assert.assertEquals(1,
@@ -164,7 +164,7 @@ public class QueryTileUtilsTest {
         nextDecisionTimeReached();
         QueryTileUtils.onMostVisitedTileClicked();
         Assert.assertFalse(QueryTileUtils.shouldShowQueryTiles());
-        Assert.assertEquals(2,
+        Assert.assertEquals(3,
                 RecordHistogram.getHistogramTotalCountForTesting(
                         "Search.QueryTiles.ShowQueryTilesSegmentationResultComparison"));
         Assert.assertEquals(1,
