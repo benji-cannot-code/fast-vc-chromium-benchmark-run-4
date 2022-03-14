1640a8a82449384884affe9f515c10b23fec5fe4
Nancy Wang <nancylingwang@chromium.org>
2022-03-14 23:44:36 +0000

    Add the Intent convert functions.
    This CL is used to prepare for CL:3509337 to return the non mojom
    IntentFilters for AppUpdate::IntentFilters.
    
    BUG=1253250
    
    Change-Id: I15abb1014e824b87cc51aa1ddf1763ee17becbed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3520779
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980857}

--- components/services/app_service/public/cpp/intent.h
+++ components/services/app_service/public/cpp/intent.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/files/safe_base_name.h"
+#include "components/services/app_service/public/mojom/types.mojom.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
 
@@ -84,6 +85,21 @@ struct Intent {
   // Optional string extras.
   base::flat_map<std::string, std::string> extras;
 };
+
+using IntentPtr = std::unique_ptr<Intent>;
+
+// TODO(crbug.com/1253250): Remove these functions after migrating to non-mojo
+// AppService.
+IntentFilePtr ConvertMojomIntentFileToIntentFile(
+    const apps::mojom::IntentFilePtr& mojom_intent_file);
+
+apps::mojom::IntentFilePtr ConvertIntentFileToMojomIntentFile(
+    const IntentFilePtr& intent_file);
+
+IntentPtr ConvertMojomIntentToIntent(
+    const apps::mojom::IntentPtr& mojom_intent);
+
+apps::mojom::IntentPtr ConvertIntentToMojomIntent(const IntentPtr& intent);
 
 }  // namespace apps
 
