e9496f544f871804a5270768b586f536ed8b2f7e
Tom Lukaszewicz <tluk@chromium.org>
2022-03-14 16:05:18 +0000

    [color pipeline] Enable redirection tests for ChromeOS
    Bug: 1296206
    Change-Id: I6428f3355cca9fa6c12dac7df1358c282cab03ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514821
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980581}

--- chrome/browser/ui/color/mac/native_chrome_color_mixer_mac.cc
+++ chrome/browser/ui/color/mac/native_chrome_color_mixer_mac.cc
@@ -5,33 +5,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/color/native_chrome_color_mixer.h"
 
-#include "chrome/browser/ui/color/chrome_color_id.h"
-#include "third_party/skia/include/core/SkColor.h"
-#include "ui/color/color_mixer.h"
-#include "ui/color/color_provider.h"
-#include "ui/color/color_provider_manager.h"
-#include "ui/color/color_recipe.h"
-#include "ui/color/color_transform.h"
-
 void AddNativeChromeColorMixer(ui::ColorProvider* provider,
-                               const ui::ColorProviderManager::Key& key) {
-  if (key.contrast_mode != ui::ColorProviderManager::ContrastMode::kHigh)
-    return;
-
-  ui::ColorMixer& mixer = provider->AddMixer();
-  const bool dark_mode =
-      key.color_mode == ui::ColorProviderManager::ColorMode::kDark;
-  const SkColor foreground = dark_mode ? SK_ColorWHITE : SK_ColorBLACK;
-  mixer[kColorDownloadShelfContentAreaSeparator] = {
-      kColorToolbarContentAreaSeparator};
-  mixer[kColorInfoBarContentAreaSeparator] = {
-      kColorToolbarContentAreaSeparator};
-  mixer[kColorLocationBarBorder] = {foreground};
-  mixer[kColorTabForegroundInactiveFrameActive] = {SK_ColorWHITE};
-  mixer[kColorTabForegroundInactiveFrameInactive] = {SK_ColorBLACK};
-  mixer[kColorToolbar] = {dark_mode ? SK_ColorBLACK : SK_ColorWHITE};
-  mixer[kColorToolbarContentAreaSeparator] = {foreground};
-  mixer[kColorToolbarText] = {foreground};
-  mixer[kColorToolbarTopSeparatorFrameActive] = {dark_mode ? SK_ColorDKGRAY
-                                                           : SK_ColorLTGRAY};
-}
+                               const ui::ColorProviderManager::Key& key) {}
