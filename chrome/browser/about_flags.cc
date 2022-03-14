1c8ce40b0838b7646d1ea79776eb4be63003099a
sauski <sauski@google.com>
2022-03-14 14:49:48 +0000

    Privacy Sandbox Settings: Add Account Capability fetcher to flags
    Adds the required account capability fetcher feature to the set of
    features that are enabled by the chrome://flags Privacy Sandbox 3 entry.
    
    Bug: 1286276
    Change-Id: Ia0dfa7a022f07d25dddbd64b16e95bb991ffad56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3521446
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980557}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -7244,10 +7244,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kPrivacySandboxV3Description, kOsDesktop,
      // Use a command-line parameter instead of a FEATURE_VALUE_TYPE to enable
      // multiple related features.
-     SINGLE_VALUE_TYPE_AND_VALUE(switches::kEnableFeatures,
-                                 "PrivacySandboxSettings3:disable-dialog-for-"
-                                 "testing/true,InterestGroupStorage,"
-                                 "AdInterestGroupAPI,Fledge,FencedFrames")},
+     SINGLE_VALUE_TYPE_AND_VALUE(
+         switches::kEnableFeatures,
+         "PrivacySandboxSettings3:disable-dialog-for-"
+         "testing/true,EnableFetchingAccountCapabilities,InterestGroupStorage,"
+         "AdInterestGroupAPI,Fledge,FencedFrames")},
 
     {"privacy-sandbox-v3-android", flag_descriptions::kPrivacySandboxV3Name,
      flag_descriptions::kPrivacySandboxV3Description, kOsAndroid,
@@ -7255,7 +7256,8 @@ const FeatureEntry kFeatureEntries[] = {
      // multiple related features when they are available.
      SINGLE_VALUE_TYPE_AND_VALUE(
          switches::kEnableFeatures,
-         "PrivacySandboxSettings3:disable-dialog-for-testing/true")},
+         "PrivacySandboxSettings3:disable-dialog-for-testing/"
+         "true,EnableFetchingAccountCapabilities")},
 
     {"animated-image-resume", flag_descriptions::kAnimatedImageResumeName,
      flag_descriptions::kAnimatedImageResumeDescription, kOsAll,
