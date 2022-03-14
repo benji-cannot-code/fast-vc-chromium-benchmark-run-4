502e2f39c7c8f560fbd615f74d9ef350ef04a66c
Scott Haseley <shaseley@chromium.org>
2022-03-14 16:26:13 +0000

    Remove awaiting cls_run_tests in BFCache CLS tests
    Two BFCache CLS tests are awaiting the results of the cls_run_tests
    promise upon navigating back to CLS test pages. But the result itself is
    going unused and the waiting is happening when navigating back to the
    test page rather than on the initial load. The tests already wait on
    kTimingField::LayoutShift (which occurs during the test page's tests),
    so explicitly waiting on the page tests to finish is redundant.
    
    This is being split off of crrev.com/c/3511407, which is converting
    instances of EvalJsResult::ExtractList(), which these tests are calling
    but not using the results of.
    
    Bug: 1303966
    Change-Id: I0fca2ebc293dc3fe96b4bd0457b9baaebbfc0039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3513866
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980592}

--- chrome/browser/page_load_metrics/observers/back_forward_cache_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/back_forward_cache_page_load_metrics_observer_browsertest.cc
@@ -379,8 +379,6 @@ IN_PROC_BROWSER_TEST_F(BackForwardCachePageLoadMetricsObserverBrowserTest,
     EXPECT_NE(rfh_a->GetLifecycleState(),
               content::RenderFrameHost::LifecycleState::kInBackForwardCache);
 
-    base::Value expectations =
-        EvalJs(web_contents(), "cls_run_tests").ExtractList();
     next_score = EvalJs(web_contents(),
                         R"((async() => {
 const shifter = document.querySelector('#shifter');
@@ -557,8 +555,6 @@ IN_PROC_BROWSER_TEST_F(BackForwardCachePageLoadMetricsObserverBrowserTest,
     EXPECT_NE(rfh_a->GetLifecycleState(),
               content::RenderFrameHost::LifecycleState::kInBackForwardCache);
 
-    base::Value expectations =
-        EvalJs(web_contents(), "cls_run_tests").ExtractList();
     next_score = EvalJs(web_contents(),
                         R"((async() => {
 const shifter = document.querySelector('#shifter');
