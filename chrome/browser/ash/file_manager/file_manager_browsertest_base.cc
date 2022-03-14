99f17ba150e555cd16dde3ffd326615715ef814d
David Munro <davidmunro@google.com>
2022-03-14 07:23:25 +0000

    file_manager: Tidy up a few things
    * Removed a couple redundant casts to the same type
    * Added a missing override
    * Avoid an unneeded string creation
    * Range-based for loops instead of 'classic'
    
    Bug: None
    Test: CQ
    Change-Id: I555255d1bcd12ebf30597aab203a584d55434384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3515713
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980449}

--- chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest_base.cc
@@ -520,7 +520,7 @@ struct AddEntriesMessage {
 
     // Maps |value| to base::Time. Returns true on success.
     static bool MapStringToTime(base::StringPiece value, base::Time* time) {
-      return base::Time::FromString(std::string(value).c_str(), time);
+      return base::Time::FromString(value.data(), time);
     }
   };
 };
@@ -833,7 +833,7 @@ class FileManagerBrowserTestBase::MockFileTasksObserver
   MOCK_METHOD2(OnFilesOpenedImpl,
                void(const std::string& path, OpenType open_type));
 
-  void OnFilesOpened(const std::vector<FileOpenEvent>& opens) {
+  void OnFilesOpened(const std::vector<FileOpenEvent>& opens) override {
     ASSERT_TRUE(!opens.empty());
     for (auto& open : opens) {
       OnFilesOpenedImpl(open.path.value(), open.open_type);
@@ -2374,8 +2374,7 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
           drive::DriveIntegrationServiceFactory::GetForProfile(profile());
       if (drive_integration_service->IsMounted()) {
         const auto drive_mount_name =
-            base::FilePath(drive_integration_service->GetMountPointPath())
-                .BaseName();
+            drive_integration_service->GetMountPointPath().BaseName();
         dictionary.SetStringKey(
             "drive", base::StrCat({"/", drive_mount_name.value(), "/root"}));
       }
