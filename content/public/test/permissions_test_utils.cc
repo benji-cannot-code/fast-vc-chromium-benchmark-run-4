b3626b277f948ddcfd84d3f1e3f9bea196c2f6c5
Illia Klimov <elklm@google.com>
2022-03-14 14:28:38 +0000

    Add end-to-end permissions tests for an embedded cross-origin iframe.
    This CL adds interaction tests for an embedded HTTPS cross-origin
    iframe. Additionally, it removes unit tests for a deprecated API that is
    blocking https://crrev.com/c/3495142
    
    Bug: 1271543
    Change-Id: I7b80e504ae08e220a47b922ead828e050ab3ef52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3497669
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Illia Klimov <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980547}

---
+++ content/public/test/permissions_test_utils.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/test/permissions_test_utils.h"
+#include "content/browser/permissions/permission_controller_impl.h"
+
+namespace content {
+
+void SetPermissionControllerOverrideForDevTools(
+    PermissionController* permission_controller,
+    const absl::optional<url::Origin>& origin,
+    PermissionType permission,
+    const blink::mojom::PermissionStatus& status) {
+  PermissionControllerImpl* permission_controller_impl =
+      static_cast<PermissionControllerImpl*>(permission_controller);
+  permission_controller_impl->SetOverrideForDevTools(origin, permission,
+                                                     status);
+}
+
+}  // namespace content
