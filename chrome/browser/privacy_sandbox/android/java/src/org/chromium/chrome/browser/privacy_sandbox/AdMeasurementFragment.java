8a009cee9bcb092d9f76db9feb1bfabe0385b9dc
Christian Dullweber <dullweber@chromium.org>
2022-03-14 17:49:06 +0000

    Privacy Sandbox Settings: Add string descriptions
    Add descriptions for Android settings, consent and notice strings.
    Remove some duplicate strings and add screenshots for translation.
    
    Bug: 1286276
    Change-Id: I08be410e1eef7ef638c4cc77a2909f68ebe5f535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3521849
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#980661}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/AdMeasurementFragment.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/AdMeasurementFragment.java
@@ -6,15 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.privacy_sandbox;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.preference.PreferenceFragmentCompat;
 
+import org.chromium.components.browser_ui.settings.ChromeBasePreference;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.ui.text.NoUnderlineClickableSpan;
+import org.chromium.ui.text.SpanApplier;
 
 /**
  * Settings fragment for privacy sandbox settings.
  */
 public class AdMeasurementFragment extends PreferenceFragmentCompat {
+    public static final String AD_MEASUREMENT_DESCRIPTION = "ad_measurement_description";
+
     /**
      * Initializes all the objects related to the preferences page.
      */
@@ -22,5 +28,15 @@ public class AdMeasurementFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle bundle, String s) {
         getActivity().setTitle(R.string.privacy_sandbox_ad_measurement_title);
         SettingsUtils.addPreferencesFromResource(this, R.xml.ad_measurement_preference);
+
+        ChromeBasePreference descriptionPreference = findPreference(AD_MEASUREMENT_DESCRIPTION);
+        descriptionPreference.setSummary(SpanApplier.applySpans(
+                getResources().getString(R.string.privacy_sandbox_ad_measurement_description),
+                new SpanApplier.SpanInfo("<link>", "</link>",
+                        new NoUnderlineClickableSpan(getContext(), this::showHistory))));
+    }
+
+    private void showHistory(View view) {
+        // TODO(crbug.com/1286276): Show history page
     }
 }
