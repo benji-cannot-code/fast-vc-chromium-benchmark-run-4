cd628d2745bdfc748ffadc9aad4f087225a3b8b0
Angela Yoeurng <yoangela@chromium.org>
2022-03-15 21:53:39 +0000

    [omnibox][settings] Enable OmniboxActiveSearchEngines by default
    Bug: 1213659
    
    Change-Id: Ie1fbe6def8b2dfd7f34825bb0f2559b48c4155ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3523385
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981340}

--- components/omnibox/browser/autocomplete_provider_unittest.cc
+++ components/omnibox/browser/autocomplete_provider_unittest.cc
@@ -544,6 +544,7 @@ void AutocompleteProviderTest::ResetControllerWithKeywordProvider() {
   data.SetShortName(u"foo.com");
   data.SetKeyword(u"foo.com");
   data.SetURL("http://foo.com/{searchTerms}");
+  data.is_active = TemplateURLData::ActiveStatus::kTrue;
   TemplateURL* keyword_turl =
       turl_model->Add(std::make_unique<TemplateURL>(data));
   ASSERT_NE(0, keyword_turl->id());
@@ -553,6 +554,7 @@ void AutocompleteProviderTest::ResetControllerWithKeywordProvider() {
   data.SetShortName(u"f");
   data.SetKeyword(u"f");
   data.SetURL("http://f.com/{searchTerms}");
+  data.is_active = TemplateURLData::ActiveStatus::kTrue;
   keyword_turl = turl_model->Add(std::make_unique<TemplateURL>(data));
   ASSERT_NE(0, keyword_turl->id());
 
@@ -560,6 +562,7 @@ void AutocompleteProviderTest::ResetControllerWithKeywordProvider() {
   data.SetShortName(u"bar.com");
   data.SetKeyword(u"bar.com");
   data.SetURL("http://bar.com/{searchTerms}");
+  data.is_active = TemplateURLData::ActiveStatus::kTrue;
   keyword_turl = turl_model->Add(std::make_unique<TemplateURL>(data));
   ASSERT_NE(0, keyword_turl->id());
 
