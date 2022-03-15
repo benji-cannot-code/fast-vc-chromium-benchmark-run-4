2f65741014b1187456538a506dc6e6ef1f385a33
ckitagawa <ckitagawa@chromium.org>
2022-03-15 19:21:36 +0000

    [Paint Preview] Fix failing a11y test
    CHECK(IsRootTree()) was causing our a11y test to fail on debug due to
    our "secondary" tree for Paint Preview not satisfying IsRootTree(). I'm
    removing the check for now as Paint Preview is the only caller and
    replaced with a TODO to improve validation once used elsewhere.
    
    Bug: 1287526
    Change-Id: Ief0d0d42e724a0f3bdc85660350ba5b2196ffd0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3452245
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981256}

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -1807,7 +1807,8 @@ void BrowserAccessibilityManager::BuildAXTreeHitTestCacheInternal(
 
 BrowserAccessibility* BrowserAccessibilityManager::AXTreeHitTest(
     const gfx::Point& blink_screen_point) const {
-  DCHECK(IsRootTree());
+  // TODO(crbug.com/1287526): assert that this gets called on a valid node. This
+  // should usually be the root node except for Paint Preview.
   DCHECK(cached_node_rtree_);
 
   std::vector<ui::AXNodeID> results;
