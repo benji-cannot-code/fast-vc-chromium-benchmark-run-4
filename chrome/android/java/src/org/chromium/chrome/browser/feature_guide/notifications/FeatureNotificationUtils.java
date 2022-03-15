d3731d193a95df3d75f8993632da8f06e6e0b645
Shakti Sahu <shaktisahu@chromium.org>
2022-03-15 08:17:36 +0000

    [feature_notification_guide] Fixed discover IPH
    This CL adds some fixes:
    - Earlier discover IPH was sticky. Made it dismissable on touch.
    - Increased the IPH trigger delay to 20ms.
    
    Bug: 1305418
    Change-Id: I9b3f142cf5823b0710f4fbdfad0f0637ccc9b04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517681
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981013}

--- chrome/android/java/src/org/chromium/chrome/browser/feature_guide/notifications/FeatureNotificationUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/feature_guide/notifications/FeatureNotificationUtils.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.feature_guide.notifications;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.text.TextUtils;
 
 import androidx.annotation.StringRes;
@@ -21,6 +22,7 @@ import org.chromium.chrome.browser.ui.default_browser_promo.DefaultBrowserPromoD
 import org.chromium.chrome.browser.ui.default_browser_promo.DefaultBrowserPromoUtils;
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxy;
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxyImpl;
+import org.chromium.components.browser_ui.widget.textbubble.TextBubble;
 import org.chromium.components.feature_engagement.FeatureConstants;
 import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.components.prefs.PrefService;
@@ -52,9 +54,13 @@ public final class FeatureNotificationUtils {
                 IntentUtils.safeGetIntExtra(intent, EXTRA_FEATURE_TYPE, FeatureType.INVALID);
         if (featureType == FeatureType.INVALID) return;
 
-        Tracker tracker = TrackerFactory.getTrackerForProfile(Profile.getLastUsedRegularProfile());
-        tracker.setPriorityNotification(
-                FeatureNotificationUtils.getIPHFeatureForNotificationFeatureType(featureType));
+        TextBubble.dismissBubbles();
+        new Handler().post(() -> {
+            Tracker tracker =
+                    TrackerFactory.getTrackerForProfile(Profile.getLastUsedRegularProfile());
+            tracker.setPriorityNotification(
+                    FeatureNotificationUtils.getIPHFeatureForNotificationFeatureType(featureType));
+        });
     }
 
     /**
