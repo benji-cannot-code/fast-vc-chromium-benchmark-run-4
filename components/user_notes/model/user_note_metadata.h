06ed09abae481c029b19b2d0b8a0879d1d24bed9
Guillaume Jenkins <gujen@google.com>
2022-03-15 14:15:35 +0000

    [Notes] Add UserNoteStorage interface
    Creates the UserNoteStorage interface that the UserNoteService will use
    to interact with notes on disk.
    
    Some aspects have changed compared to the design doc:
    
    - Async API responses are now returned via base::OnceCallback callbacks
      instead of a Consumer class. This is to avoid interdependency between
      the service and the storage (storage will need a ref to its consumer,
      consumer/service will need a ref to the storage for API calls), which
      would complicate destruction order.
    
    - The API to get all notes for a set of URLs has been replaced with
      an API to only get the metadata of the notes. This API was meant
      to allow the service to calculate the diff between the current model
      and the new one. To calculate this diff, only the IDs and the
      timestamp are needed. After the diff has been calculated, the
      service will query the full data only for the necessary notes,
      which will avoid a large memory usage spike on each storage change
      event.
    
    - The storage API to get the full list of annotated URLs has been
      removed. This was done because keeping that list up to date comes
      with some timing issues (e.g. if a navigation comes in before the
      list is updated) and would require constant adjusting on each
      storage change event. Instead, on each navigation, the service
      will make 2 calls to the DB: first, to get the metadata of all notes
      for this URL, and second, to get the UserNote objects it didn't
      already have in its model map. This will reduce memory usage spikes.
    
    Bug: 1303899
    Change-Id: Iac3409ace50e4a188bcedcfc7aec230eb9564adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514258
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#981098}

---
+++ components/user_notes/model/user_note_metadata.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_USER_NOTES_MODEL_USER_NOTE_METADATA_H_
+#define COMPONENTS_USER_NOTES_MODEL_USER_NOTE_METADATA_H_
+
+namespace user_notes {
+
+// Model class for a note.
+class UserNoteMetadata {
+ public:
+  explicit UserNoteMetadata();
+  ~UserNoteMetadata();
+  UserNoteMetadata(const UserNoteMetadata&) = delete;
+  UserNoteMetadata& operator=(const UserNoteMetadata&) = delete;
+};
+
+}  // namespace user_notes
+
+#endif  // COMPONENTS_USER_NOTES_MODEL_USER_NOTE_METADATA_H_
