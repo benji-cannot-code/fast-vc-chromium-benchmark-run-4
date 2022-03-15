ba6a95cf28ac543a9c00109bf93be8560e2fc133
Michael Thiessen <mthiesse@chromium.org>
2022-03-15 22:23:20 +0000

    Don't avoid disambiguation dialog in CCT/WebLayer if Chrome can't handle
    For external protocols, if we block the outgoing intent because it'll
    show the disambiguation dialog, we end up landing on a page that just
    says '"<protocol>" links are blocked'.
    
    Given the goal of blocking the intent chooser is to avoid showing the
    Chrome icon showing for navigations the CCT/WebLayer could handle, it
    should be fine to show the dialog for things that are explicitly
    supposed to leave the CCT/WebLayer (through the use of external
    protocols).
    
    Bug: 1305256
    Change-Id: Icaf6b9e5058f35e89fb191e0e7b9b3b92ce67393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517311
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981353}

--- components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
+++ components/external_intents/android/javatests/src/org/chromium/components/external_intents/ExternalNavigationHandlerTest.java
@@ -2355,6 +2355,9 @@ public class ExternalNavigationHandlerTest {
 
         mDelegate.setShouldAvoidDisambiguationDialog(true);
         checkUrl(YOUTUBE_URL).expecting(OverrideUrlLoadingResultType.NO_OVERRIDE, IGNORE);
+        checkUrl(INTENT_URL_WITH_FALLBACK_URL)
+                .expecting(OverrideUrlLoadingResultType.OVERRIDE_WITH_EXTERNAL_INTENT,
+                        START_OTHER_ACTIVITY);
     }
 
     @Test
