9b8e0291c4f6f74d6d154f0c1253ffad3376f369
Marlon Facey <mfacey@chromium.org>
2022-03-15 23:22:28 +0000

    [realbox] Change shadow and border for dark mode in realbox
    With the match searchbox flag toggled, the realbox still had a
    different border and drop shadow then the searchbox in dark mode. This
    CL updates the border and drop shadow on dark mode to match the
    searchbox.
    
    Before: screenshot/kUT7jtoQxVfCuHF
    After: screenshot/98AwQc7h2hzvzK6
    
    Bug: 1306333
    Change-Id: I659fec0e0ef1f065cec34693d18afe5d00134412
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3522666
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981382}

--- chrome/browser/resources/new_tab_page/realbox/realbox.ts
+++ chrome/browser/resources/new_tab_page/realbox/realbox.ts
@@ -58,6 +58,13 @@ export class RealboxElement extends PolymerElement {
       // Public properties
       //========================================================================
 
+      /** Whether the theme is dark. */
+      isDark: {
+        type: Boolean,
+        computed: `computeIsDark_(theme)`,
+        reflectToAttribute: true,
+      },
+
       /** Whether matches are currently visible. */
       matchesAreVisible: {
         type: Boolean,
@@ -174,6 +181,7 @@ export class RealboxElement extends PolymerElement {
     };
   }
 
+  isDark: boolean;
   matchesAreVisible: boolean;
   matchSearchbox: boolean;
   theme: SearchBoxTheme;
@@ -645,6 +653,13 @@ export class RealboxElement extends PolymerElement {
   //============================================================================
   // Helpers
   //============================================================================
+
+  private computeIsDark_(): boolean {
+    if (!this.theme) {
+      return false;
+    }
+    return this.theme.isDark;
+  }
 
   private computeSelectedMatch_(): AutocompleteMatch|null {
     if (!this.result_ || !this.result_.matches) {
