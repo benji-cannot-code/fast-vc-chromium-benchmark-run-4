bc8901a3a85b22d61c70c17c389c51b4fe7b58c9
hmare <hmare@google.com>
2022-03-15 15:36:59 +0000

    Implemented Rotation Specific Logic For The Management Service.
    I added the implementation class that's responsible for kicking off the
    key rotation. This is to split process specific and key rotation
    specific code. Also added more constants to the shared constants file
    and changed the linux network delegate, persistence delegate, and key
    rotation command to use them.
    
    Bug: b/210343344
    
    Change-Id: I61b944fb1ef1b9808af3dd6a1df694ce5868ad17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3498876
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#981127}

--- chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/linux_key_rotation_command_unittest.cc
+++ chrome/browser/enterprise/connectors/device_trust/key_management/browser/commands/linux_key_rotation_command_unittest.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/multiprocess_test.h"
 #include "base/test/task_environment.h"
 #include "base/threading/platform_thread.h"
-#include "chrome/browser/enterprise/connectors/device_trust/key_management/common/chrome_management_service_constants.h"
+#include "chrome/browser/enterprise/connectors/device_trust/key_management/core/shared_command_constants.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
 #include "mojo/public/cpp/system/invitation.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
@@ -43,11 +43,8 @@ constexpr char kFakeDmServerUrl[] =
 
 static constexpr const char* kSwitches[] = {
 
-    chrome_management_service::switches::kRotateDTKey,
-    chrome_management_service::switches::kDmServerUrl,
-    chrome_management_service::switches::kPipeName,
-    chrome_management_service::switches::kNonce,
-    mojo::PlatformChannel::kHandleSwitch};
+    switches::kRotateDTKey, switches::kDmServerUrl, switches::kPipeName,
+    switches::kNonce, mojo::PlatformChannel::kHandleSwitch};
 
 }  // namespace
 
@@ -124,9 +121,8 @@ MULTIPROCESS_TEST_MAIN(MojoInvitation) {
       mojo::PlatformChannel::RecoverPassedEndpointFromCommandLine(command_line);
   auto incoming_invitation =
       mojo::IncomingInvitation::Accept(std::move(channel_endpoint));
-  auto pipe =
-      incoming_invitation.ExtractMessagePipe(command_line.GetSwitchValueNative(
-          chrome_management_service::switches::kPipeName));
+  auto pipe = incoming_invitation.ExtractMessagePipe(
+      command_line.GetSwitchValueNative(switches::kPipeName));
   mojo::Remote<network::mojom::URLLoaderFactory> url_loader_factory(
       mojo::PendingRemote<network::mojom::URLLoaderFactory>(std::move(pipe),
                                                             0));
@@ -143,13 +139,10 @@ MULTIPROCESS_TEST_MAIN(MojoInvitation) {
   base::Base64Encode(kNonce, &nonce_base64);
 
   EXPECT_EQ(token_base64,
-            command_line.GetSwitchValueNative(
-                chrome_management_service::switches::kRotateDTKey));
+            command_line.GetSwitchValueNative(switches::kRotateDTKey));
   EXPECT_EQ(kFakeDmServerUrl,
-            command_line.GetSwitchValueNative(
-                chrome_management_service::switches::kDmServerUrl));
-  EXPECT_EQ(nonce_base64, command_line.GetSwitchValueNative(
-                              chrome_management_service::switches::kNonce));
+            command_line.GetSwitchValueNative(switches::kDmServerUrl));
+  EXPECT_EQ(nonce_base64, command_line.GetSwitchValueNative(switches::kNonce));
 
   return testing::Test::HasFailure();
 }
@@ -160,7 +153,7 @@ TEST_F(LinuxKeyRotationCommandTest, MojoAcceptInvitation) {
 
 // Tests for a key rotation when the chrome management service succeeded.
 MULTIPROCESS_TEST_MAIN(Success) {
-  return chrome_management_service::kSuccess;
+  return kSuccess;
 }
 
 TEST_F(LinuxKeyRotationCommandTest, RotateSuccess) {
@@ -169,7 +162,7 @@ TEST_F(LinuxKeyRotationCommandTest, RotateSuccess) {
 
 // Tests for a key rotation failure when the chrome management service failed.
 MULTIPROCESS_TEST_MAIN(Failure) {
-  return chrome_management_service::kFailure;
+  return kFailure;
 }
 
 TEST_F(LinuxKeyRotationCommandTest, RotateFailure) {
--- chrome/browser/enterprise/connectors/device_trust/key_management/core/shared_command_constants.h
+++ chrome/browser/enterprise/connectors/device_trust/key_management/core/shared_command_constants.h
@@ -2,19 +2,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2022 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// Defines all constants related to the chrome management service process.
 
-#ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_COMMON_CHROME_MANAGEMENT_SERVICE_CONSTANTS_H_
-#define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_COMMON_CHROME_MANAGEMENT_SERVICE_CONSTANTS_H_
+#ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_SHARED_COMMAND_CONSTANTS_H_
+#define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_SHARED_COMMAND_CONSTANTS_H_
 
-namespace chrome_management_service {
+// Defines constants shared by both the browser and installers chrome
+// management service executable.
+namespace enterprise_connectors {
 
 // Process exit codes of the chrome management service executable.
 enum Status {
   kSuccess = 0,
   kFailure = 1,
 };
+
+namespace constants {
+
+// Specifies the filename of the chrome management service executable.
+extern const char kBinaryFileName[];
+
+// Path to where the signing key is stored.
+extern const char kSigningKeyFilePath[];
+
+}  // namespace constants
 
 namespace switches {
 
@@ -33,6 +43,6 @@ extern const char kRotateDTKey[];
 
 }  // namespace switches
 
-}  // namespace chrome_management_service
+}  // namespace enterprise_connectors
 
-#endif  // CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_COMMON_CHROME_MANAGEMENT_SERVICE_CONSTANTS_H_
+#endif  // CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_CORE_SHARED_COMMAND_CONSTANTS_H_
