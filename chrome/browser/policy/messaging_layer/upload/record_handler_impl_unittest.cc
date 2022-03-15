85ae7177994858152d0a80115b352f8efda79742
Hong Xu <xuhong@chromium.org>
2022-03-15 00:16:07 +0000

    Add a matcher to verify the validity of a request
    Add a matcher `IsDataUploadRequestValid` to verify the general validity
    of a data upload request. This matcher is intended to be called for
    most tested data upload requests to verify whether the request is
    valid on some basic fronts, such as containing an "encryptedRecord"
    key, etc.
    
    Bug: b:214040998
    Change-Id: Idcb988f083639156eecea000bf31444e1c2e2a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3519885
    Auto-Submit: Hong Xu <xuhong@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#980874}

--- chrome/browser/policy/messaging_layer/upload/record_handler_impl_unittest.cc
+++ chrome/browser/policy/messaging_layer/upload/record_handler_impl_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/policy/messaging_layer/upload/dm_server_upload_service.h"
 #include "chrome/browser/policy/messaging_layer/upload/record_upload_request_builder.h"
+#include "chrome/browser/policy/messaging_layer/upload/test_util.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
 #include "components/policy/core/common/cloud/dm_token.h"
 #include "components/policy/core/common/cloud/mock_cloud_policy_client.h"
@@ -259,7 +260,7 @@ TEST_P(RecordHandlerImplTest, ForwardsRecordsToCloudPolicyClient) {
       .sequence_information = test_records->back().sequence_information(),
       .force_confirm = force_confirm()};
 
-  EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+  EXPECT_CALL(*client_, UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
       .WillOnce(WithArgs<0, 2>(
           Invoke([&force_confirm_by_server](
                      base::Value::Dict request,
@@ -295,7 +296,7 @@ TEST_P(RecordHandlerImplTest, MissingPriorityField) {
   auto test_records = BuildTestRecordsVector(kNumTestRecords, kGenerationId);
   const auto force_confirm_by_server = force_confirm();
 
-  EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+  EXPECT_CALL(*client_, UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
       .WillOnce(WithArgs<0, 2>(
           Invoke([&force_confirm_by_server](
                      base::Value::Dict request,
@@ -324,7 +325,7 @@ TEST_P(RecordHandlerImplTest, InvalidPriorityField) {
   auto test_records = BuildTestRecordsVector(kNumTestRecords, kGenerationId);
   const auto force_confirm_by_server = force_confirm();
 
-  EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+  EXPECT_CALL(*client_, UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
       .WillOnce(WithArgs<0, 2>(
           Invoke([&force_confirm_by_server](
                      base::Value::Dict request,
@@ -352,7 +353,7 @@ TEST_P(RecordHandlerImplTest, ReportsUploadFailure) {
   static constexpr int64_t kGenerationId = 1234;
   auto test_records = BuildTestRecordsVector(kNumTestRecords, kGenerationId);
 
-  EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+  EXPECT_CALL(*client_, UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
       .WillOnce(WithArgs<2>(Invoke(
           [](base::OnceCallback<void(absl::optional<base::Value::Dict>)>
                  callback) { std::move(callback).Run(absl::nullopt); })));
@@ -390,7 +391,8 @@ TEST_P(RecordHandlerImplTest, UploadsGapRecordOnServerFailure) {
   // Once for failure, and once for gap.
   {
     ::testing::InSequence seq;
-    EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+    EXPECT_CALL(*client_,
+                UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
         .WillOnce(WithArgs<0, 2>(
             Invoke([](base::Value::Dict request,
                       policy::CloudPolicyClient::ResponseCallback callback) {
@@ -398,7 +400,8 @@ TEST_P(RecordHandlerImplTest, UploadsGapRecordOnServerFailure) {
               FailedResponseFromRequest(request, response);
               std::move(callback).Run(std::move(response));
             })));
-    EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+    EXPECT_CALL(*client_,
+                UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
         .WillOnce(WithArgs<0, 2>(
             Invoke([&force_confirm_by_server](
                        base::Value::Dict request,
@@ -440,7 +443,7 @@ TEST_P(RecordHandlerImplTest, HandleUnknownResponseFromServer) {
   static constexpr int64_t kGenerationId = 1234;
   auto test_records = BuildTestRecordsVector(kNumTestRecords, kGenerationId);
 
-  EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+  EXPECT_CALL(*client_, UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
       .WillOnce(WithArgs<2>(
           Invoke([](policy::CloudPolicyClient::ResponseCallback callback) {
             std::move(callback).Run(base::Value::Dict());
@@ -475,7 +478,7 @@ TEST_P(RecordHandlerImplTest, AssignsRequestIdForRecordUploads) {
       .sequence_information = test_records->back().sequence_information(),
       .force_confirm = force_confirm()};
 
-  EXPECT_CALL(*client_, UploadEncryptedReport(_, _, _))
+  EXPECT_CALL(*client_, UploadEncryptedReport(IsDataUploadRequestValid(), _, _))
       .WillOnce(WithArgs<0, 2>(
           Invoke([&force_confirm_by_server](
                      base::Value::Dict request,
