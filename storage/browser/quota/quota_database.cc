35e5389fa640c18982e610ac738b606bbfa9032d
Ayu Ishii <ayui@chromium.org>
2022-03-15 22:15:31 +0000

    StorageBuckets: Commit Immediately for Bucket Creation/Deletion
    This change updates Bucket creation & deletion queries to
    immediately commit transactions so that changes are reflected
    immediately instead of being batched.
    
    Change-Id: Iff6c2e9f7151fb0a868dfea8e7b3a94289f2c999
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526174
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981349}

--- storage/browser/quota/quota_database.cc
+++ storage/browser/quota/quota_database.cc
@@ -589,7 +589,9 @@ QuotaError QuotaDatabase::DeleteBucketInfo(BucketId bucket_id) {
   if (!statement.Run())
     return QuotaError::kDatabaseError;
 
-  ScheduleCommit();
+  // Commit so deletion is reflected immediately.
+  Commit();
+
   return QuotaError::kNone;
 }
 
@@ -1063,10 +1065,12 @@ QuotaErrorOr<BucketInfo> QuotaDatabase::CreateBucketInternal(
   if (!statement.Run())
     return QuotaError::kDatabaseError;
 
-  ScheduleCommit();
-
   int64_t bucket_id = db_->GetLastInsertRowId();
   DCHECK_GT(bucket_id, 0);
+
+  // Commit so bucket data is persisted immediately.
+  Commit();
+
   return BucketInfo(BucketId(bucket_id), storage_key, type, bucket_name,
                     base::Time::Max(), 0);
 }
