960fb752a99587d229ae308ad14026c1eeecce58
Alexander Bolodurin <alexbn@chromium.org>
2022-03-15 10:25:37 +0000

    [lacros pwa install] add WebAppProviderBridge to Lacros
    Add WebAppProviderBridge crosapi implementation to Lacros,
    instantiated and registered with Ash at startup.
    
    Bug: 1225830
    Change-Id: Iebafddce6f2105de7c3c7b5f5a817452ee07f1a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499617
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981039}

--- chromeos/lacros/lacros_service.cc
+++ chromeos/lacros/lacros_service.cc
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/timezone.mojom.h"
 #include "chromeos/crosapi/mojom/tts.mojom.h"
 #include "chromeos/crosapi/mojom/url_handler.mojom.h"
+#include "chromeos/crosapi/mojom/web_app_service.mojom.h"
 #include "chromeos/crosapi/mojom/web_page_info.mojom.h"
 #include "chromeos/lacros/lacros_service_never_blocking_state.h"
 #include "chromeos/lacros/native_theme_cache.h"
@@ -373,6 +374,9 @@ LacrosService::LacrosService()
                   Crosapi::MethodMinVersions::kBindUrlHandlerMinVersion>();
   ConstructRemote<crosapi::mojom::AppPublisher, &Crosapi::BindWebAppPublisher,
                   Crosapi::MethodMinVersions::kBindWebAppPublisherMinVersion>();
+  ConstructRemote<crosapi::mojom::WebAppService,
+                  &crosapi::mojom::Crosapi::BindWebAppService,
+                  Crosapi::MethodMinVersions::kBindWebAppServiceMinVersion>();
   ConstructRemote<
       crosapi::mojom::WebPageInfoFactory,
       &crosapi::mojom::Crosapi::BindWebPageInfoFactory,
