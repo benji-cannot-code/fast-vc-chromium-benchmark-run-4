6cfc000234d02588da3ffdcdc1c15da9da35ccf5
Thomas Guilbert <tguilbert@chromium.org>
2022-03-16 01:19:08 +0000

    Handle variable input channel count
    This CL updates AudioOpusEncoder to handle inputs with varying channel
    counts.
    
    See attached bug for details.
    
    Bug: 1300411
    Change-Id: I50fdf3bd4cc1d1ccef52eae4a33c01d79b554a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500545
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981451}

--- media/audio/audio_opus_encoder.cc
+++ media/audio/audio_opus_encoder.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/checked_math.h"
 #include "base/strings/stringprintf.h"
 #include "media/base/bind_to_current_loop.h"
+#include "media/base/channel_mixer.h"
 #include "media/base/encoder_status.h"
 #include "media/base/timestamp_constants.h"
 
@@ -273,6 +274,31 @@ AudioOpusEncoder::CodecDescription AudioOpusEncoder::PrepareExtraData() {
   return extra_data;
 }
 
+std::unique_ptr<AudioBus> AudioOpusEncoder::EnsureExpectedChannelCount(
+    std::unique_ptr<AudioBus> audio_bus) {
+  // No mixing required.
+  if (audio_bus->channels() == input_params_.channels())
+    return audio_bus;
+
+  const int& incoming_channels = audio_bus->channels();
+  if (!mixer_ || mixer_input_params_.channels() != incoming_channels) {
+    // Both the format and the sample rate are unused for mixing, but we still
+    // need to pass a value below.
+    mixer_input_params_.Reset(input_params_.format(),
+                              GuessChannelLayout(incoming_channels),
+                              incoming_channels, input_params_.sample_rate());
+
+    mixer_ = std::make_unique<ChannelMixer>(mixer_input_params_, input_params_);
+  }
+
+  auto mixed_bus =
+      AudioBus::Create(input_params_.channels(), audio_bus->frames());
+
+  mixer_->Transform(audio_bus.get(), mixed_bus.get());
+
+  return mixed_bus;
+}
+
 void AudioOpusEncoder::Encode(std::unique_ptr<AudioBus> audio_bus,
                               base::TimeTicks capture_time,
                               EncoderStatusCB done_cb) {
@@ -287,12 +313,11 @@ void AudioOpusEncoder::Encode(std::unique_ptr<AudioBus> audio_bus,
   }
 
   DCHECK(timestamp_tracker_);
-  DCHECK_EQ(audio_bus->channels(), input_params_.channels());
 
   if (timestamp_tracker_->base_timestamp() == kNoTimestamp)
     timestamp_tracker_->SetBaseTimestamp(capture_time - base::TimeTicks());
 
-  fifo_->Push(std::move(audio_bus));
+  fifo_->Push(EnsureExpectedChannelCount(std::move(audio_bus)));
 
   while (fifo_->frames() >= min_input_frames_needed_ && current_done_cb_)
     OnEnoughInputFrames();
--- media/audio/audio_opus_encoder.h
+++ media/audio/audio_opus_encoder.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+class ChannelMixer;
+
 using OpusEncoderDeleterType = void (*)(OpusEncoder* encoder_ptr);
 using OwnedOpusEncoder = std::unique_ptr<OpusEncoder, OpusEncoderDeleterType>;
 
@@ -55,6 +57,9 @@ class MEDIA_EXPORT AudioOpusEncoder : public AudioEncoder {
 
   EncoderStatus::Or<OwnedOpusEncoder> CreateOpusEncoder();
 
+  std::unique_ptr<AudioBus> EnsureExpectedChannelCount(
+      std::unique_ptr<AudioBus> audio_bus);
+
   AudioParameters input_params_;
 
   // Output parameters after audio conversion. This may differ from the input
@@ -73,6 +78,11 @@ class MEDIA_EXPORT AudioOpusEncoder : public AudioEncoder {
   // converter.
   // Note: Must be destroyed before |converter_|.
   std::unique_ptr<InputFramesFifo> fifo_;
+
+  // Used to mix incoming Encode() buffers to match the expect input channel
+  // count.
+  std::unique_ptr<ChannelMixer> mixer_;
+  AudioParameters mixer_input_params_;
 
   // This is the destination AudioBus where the |converter_| teh audio into.
   std::unique_ptr<AudioBus> converted_audio_bus_;
