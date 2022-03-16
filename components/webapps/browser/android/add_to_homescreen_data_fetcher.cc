dbdff9526aeac226c9803d3d05973585cd97da78
Finnur Thorarinsson <finnur@chromium.org>
2022-03-16 17:49:26 +0000

    [A2HS]: Prefer title in A2HS suggestion over blanks in app name.
    If an app specifies...
    <meta name="application-name" content="&nbsp;">
    ... that value (space) is not a great thing to use as a value
    to suggest to the user as a name for the shortcut, and is ignored
    by Firefox, for example.
    
    This CL trims the whitespace before checking if string is empty.
    
    Bug: 1253299
    Change-Id: I9b232c12a3055d0980267cd0a415486551c13e94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3525397
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981730}

--- components/webapps/browser/android/add_to_homescreen_data_fetcher.cc
+++ components/webapps/browser/android/add_to_homescreen_data_fetcher.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/task/sequenced_task_runner.h"
@@ -160,9 +161,11 @@ void AddToHomescreenDataFetcher::OnDidGetWebPageMetadata(
   }
 
   // Set the user-editable title to be the page's title.
-  shortcut_info_.user_title = web_page_metadata->application_name.empty()
-                                  ? web_contents_->GetTitle()
-                                  : web_page_metadata->application_name;
+  std::u16string app_name;
+  base::TrimWhitespace(web_page_metadata->application_name,
+                       base::TrimPositions::TRIM_ALL, &app_name);
+  shortcut_info_.user_title =
+      app_name.empty() ? web_contents_->GetTitle() : app_name;
   shortcut_info_.short_name = shortcut_info_.user_title;
   shortcut_info_.name = shortcut_info_.user_title;
 
