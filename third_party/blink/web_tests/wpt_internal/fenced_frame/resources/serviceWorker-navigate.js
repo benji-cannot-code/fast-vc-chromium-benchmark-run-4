bc987a8c4726073dfc4b8642b34c32f3fa708fda
Dave Tapuska <dtapuska@chromium.org>
2022-03-16 20:40:57 +0000

    Ensure a OpenURL is not a top level navigation for fenced frames.
    ServiceWorkers and some flags for WebView will can cause OpenURL code
    paths with a specific FrameTreeNodeId. Make sure that we navigate the
    frame tree directly (like prerender) and don't pass the URL to the
    WebContentsDelegate.
    
    BUG=1306247
    
    Change-Id: If6ba615cfebce5181efb04b345c581769e24ac91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3525799
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981846}

---
+++ third_party/blink/web_tests/wpt_internal/fenced_frame/resources/serviceWorker-navigate.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.importScripts('utils.js');
+
+self.onmessage = function(e) {
+  var key = e.data.key;
+  var url = e.data.url;
+
+  e.waitUntil(self.clients.claim().then(() => {
+      return self.clients.matchAll({type: 'window'});
+    }).then(clients => {
+      return clients.map(client => {
+        // Check to make sure WindowClient.navigate() is supported.
+        if (client.url === url) {
+          return client.navigate(generateURL('serviceWorker-navigate-inner-success.html',
+                                        [key]));
+        }
+      });
+    }));
+};
