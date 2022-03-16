ae4a6e1b9aa4279adf3e7188e57975c9b2ff0cea
Aida Zolic <aidazolic@chromium.org>
2022-03-16 16:25:56 +0000

    Add UMA stats for warning on screen share
    Adds histograms for different warn level restriction cases for screen
    sharing, both before a screen share starts and during an already
    running share.
    
    R=poromov@chromium.org
    
    Fixed: 1304725
    Change-Id: I45b24387bafbfa4144fc067f9dc0622eb3bb4d14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3512623
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981677}

--- chrome/browser/chromeos/policy/dlp/dlp_content_manager.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_content_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/containers/cxx20_erase.h"
 #include "base/memory/weak_ptr.h"
+#include "base/notreached.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_confidential_contents.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_content_manager_observer.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_content_restriction_set.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/views/widget/widget.h"
 #include "url/gurl.h"
 
@@ -71,6 +73,27 @@ bool IsWarn(RestrictionLevelAndUrl restriction_info) {
 bool IsReported(RestrictionLevelAndUrl restriction_info) {
   return restriction_info.level == DlpRulesManager::Level::kReport ||
          IsBlocked(restriction_info);
+}
+
+// Maps restriction to the correct suffix used for logging WarnProceeded
+// metrics. Returns the suffix for supported restrictions and null otherwise.
+const absl::optional<std::string> RestrictionToWarnProceededUMASuffix(
+    DlpRulesManager::Restriction restriction) {
+  switch (restriction) {
+    case DlpRulesManager::Restriction::kScreenShare:
+      return absl::make_optional(dlp::kScreenShareWarnProceededUMA);
+    case DlpRulesManager::Restriction::kScreenshot:
+    case DlpRulesManager::Restriction::kPrinting:
+      // TODO(crbug.com/1304750, crbug.com/1304752): Add UMA stats for warning
+      // for printing and screenshots.
+      return absl::nullopt;
+    case DlpRulesManager::Restriction::kUnknownRestriction:
+    case DlpRulesManager::Restriction::kClipboard:
+    case DlpRulesManager::Restriction::kPrivacyScreen:
+    case DlpRulesManager::Restriction::kFiles:
+      NOTREACHED();
+      return absl::nullopt;
+  }
 }
 
 }  // namespace
@@ -487,6 +510,8 @@ void DlpContentManager::ProcessScreenShareRestriction(
                    DlpRulesManager::Restriction::kScreenShare);
   DlpBooleanHistogram(dlp::kScreenShareBlockedUMA,
                       IsBlocked(info.restriction_info));
+  DlpBooleanHistogram(dlp::kScreenShareWarnedUMA,
+                      IsWarn(info.restriction_info));
   if (IsBlocked(info.restriction_info)) {
     ShowDlpScreenShareDisabledNotification(application_title);
     std::move(callback).Run(false);
@@ -499,6 +524,7 @@ void DlpContentManager::ProcessScreenShareRestriction(
                           DlpRulesManager::Restriction::kScreenShare);
     if (info.confidential_contents.IsEmpty()) {
       // The user already allowed all the visible content.
+      DlpBooleanHistogram(dlp::kScreenShareWarnSilentProceededUMA, true);
       std::move(callback).Run(true);
       return;
     }
@@ -571,6 +597,10 @@ void DlpContentManager::CheckRunningScreenShares() {
     }
     screen_share->SetConfidentialContentsInfo(info);
 
+    DlpBooleanHistogram(dlp::kScreenShareBlockedUMA,
+                        IsBlocked(info.restriction_info));
+    DlpBooleanHistogram(dlp::kScreenShareWarnedUMA,
+                        IsWarn(info.restriction_info));
     if (IsBlocked(info.restriction_info)) {
       if (screen_share->IsRunning()) {
         screen_share->Pause();
@@ -593,6 +623,7 @@ void DlpContentManager::CheckRunningScreenShares() {
           screen_share->Resume();
           screen_share->MaybeUpdateNotifications();
         }
+        DlpBooleanHistogram(dlp::kScreenShareWarnSilentProceededUMA, true);
         continue;
       }
       if (screen_share->IsRunning()) {
@@ -634,6 +665,7 @@ void DlpContentManager::OnDlpScreenShareWarnDialogReply(
     // to do anything.
     return;
 
+  DlpBooleanHistogram(dlp::kScreenShareWarnProceededUMA, should_proceed);
   if (should_proceed) {
     ReportWarningProceededEvent(info.restriction_info.url,
                                 DlpRulesManager::Restriction::kScreenShare,
@@ -658,6 +690,9 @@ void DlpContentManager::OnDlpWarnDialogReply(
     DlpRulesManager::Restriction restriction,
     OnDlpRestrictionCheckedCallback callback,
     bool should_proceed) {
+  auto suffix = RestrictionToWarnProceededUMASuffix(restriction);
+  if (suffix.has_value())
+    DlpBooleanHistogram(suffix.value(), should_proceed);
   if (should_proceed) {
     for (const auto& content : confidential_contents.GetContents()) {
       user_allowed_contents_cache_.Cache(content, restriction);
