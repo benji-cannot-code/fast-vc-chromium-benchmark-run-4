b37aabfb0e6c0ccfe3ec6c449dc86ea4835d3d43
François Beaufort <beaufort.francois@gmail.com>
2022-03-16 16:41:47 +0000

    bluetooth: Enable UKM for navigator.bluetooth.getAvailability()
    This change adds UKM reporting for site usage of the Web Bluetooth
    method navigator.bluetooth.getAvailability() whichi checks if Web
    Bluetooth is allowed and if a Bluetooth radio is available.
    
    The overall usage (0.4%) is measured already via UseCounter:
    https://www.chromestatus.com/metrics/feature/timeline/popularity/3708
    
    Bug: 1306400
    Change-Id: Iee97cb8c44ec2e93db11fc67b79e0873ca8532e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3524801
    Reviewed-by: Vincent Scheib <scheib@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#981684}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -224,6 +224,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kWebCodecsVideoFrameFromBuffer,
           WebFeature::kOpenWebDatabaseInsecureContext,
           WebFeature::kPrivateNetworkAccessIgnoredPreflightError,
+          WebFeature::kWebBluetoothGetAvailability,
       }));
   return *opt_in_features;
 }
