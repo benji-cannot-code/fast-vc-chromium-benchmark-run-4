24829755e6a9a993a098469746e0fb4fb884119f
Dan McArdle <dmcardle@chromium.org>
2022-03-16 00:39:18 +0000

    Restore list of disabled DoH providers in NetLog
    I'm making the corresponding change to the NetLog viewer in this CL:
    https://chromium-review.googlesource.com/c/catapult/+/3526853
    
    Bug: 1300296
    Change-Id: I584577f02120eb7b2600fb985214730716e549f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526375
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981432}

--- net/base/net_info_source_list.h
+++ net/base/net_info_source_list.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const char kNetInfoProxySettings[] = "proxySettings";
 const char kNetInfoBadProxies[] = "badProxies";
 const char kNetInfoHostResolver[] = "hostResolverInfo";
+const char kNetInfoDohProvidersDisabledDueToFeature[] =
+    "dohProvidersDisabledDueToFeature";
 const char kNetInfoSocketPool[] = "socketPoolInfo";
 const char kNetInfoQuic[] = "quicInfo";
 const char kNetInfoSpdySessions[] = "spdySessionInfo";
