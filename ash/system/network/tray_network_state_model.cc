d33e0f07b0c2eec48eccc4282a16df6ec7bc73d7
Azeem Arshad <azeemarshad@chromium.org>
2022-03-16 19:37:18 +0000

    [CrOS WiFi] Hide join wifi button in quick settings by policy.
    This CL modifies WiFiSectionHeaderView so that the Join WiFi button
    is not displayed when allow_only_policy_wifi_network_to_connect or
    allow_only_policy_wifi_networks_to_connect_if_available policies are
    enabled.
    
    TEST=Manually tested quick settings with admin console. Unit tests are
         not included since this will be tackled as part of b/207089013
    
    Fixed: b/217968206
    Change-Id: I0dfb43bb9172c8eb1a477636922277679a571ec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502280
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981789}

--- ash/system/network/tray_network_state_model.cc
+++ ash/system/network/tray_network_state_model.cc
@@ -93,6 +93,12 @@ class TrayNetworkStateModel::Impl
                        base::Unretained(model_)));
   }
 
+  void GetGlobalPolicy() {
+    DCHECK(remote_cros_network_config_);
+    remote_cros_network_config_->GetGlobalPolicy(base::BindOnce(
+        &TrayNetworkStateModel::OnGetGlobalPolicy, base::Unretained(model_)));
+  }
+
   void SetNetworkTypeEnabledState(NetworkType type, bool enabled) {
     DCHECK(remote_cros_network_config_);
     remote_cros_network_config_->SetNetworkTypeEnabledState(type, enabled,
@@ -126,7 +132,9 @@ class TrayNetworkStateModel::Impl
 
   void OnNetworkCertificatesChanged() override {}
 
-  void OnPoliciesApplied(const std::string& userhash) override {}
+  void OnPoliciesApplied(const std::string& userhash) override {
+    GetGlobalPolicy();
+  }
 
   TrayNetworkStateModel* model_;
   mojo::Remote<chromeos::network_config::mojom::CrosNetworkConfig>
@@ -148,6 +156,7 @@ TrayNetworkStateModel::TrayNetworkStateModel()
   impl_->GetActiveNetworks();
   impl_->GetVirtualNetworks();
   impl_->GetDeviceStateList();
+  impl_->GetGlobalPolicy();
 }
 
 TrayNetworkStateModel::~TrayNetworkStateModel() {
@@ -264,12 +273,23 @@ void TrayNetworkStateModel::OnGetVirtualNetworks(
   has_vpn_ = !networks.empty();
 }
 
+void TrayNetworkStateModel::OnGetGlobalPolicy(
+    chromeos::network_config::mojom::GlobalPolicyPtr global_policy) {
+  global_policy_ = std::move(global_policy);
+  NotifyGlobalPolicyChanged();
+}
+
 void TrayNetworkStateModel::NotifyNetworkListChanged() {
   if (timer_.IsRunning())
     return;
   timer_.Start(FROM_HERE, base::Milliseconds(update_frequency_),
                base::BindOnce(&TrayNetworkStateModel::SendNetworkListChanged,
                               base::Unretained(this)));
+}
+
+void TrayNetworkStateModel::NotifyGlobalPolicyChanged() {
+  for (auto& observer : observer_list_)
+    observer.GlobalPolicyChanged();
 }
 
 void TrayNetworkStateModel::NotifyVpnProvidersChanged() {
