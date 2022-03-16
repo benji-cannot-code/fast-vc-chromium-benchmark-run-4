25fe4ab262df891da24def339f90fb9503bccbb2
David Sanders <dsanders11@ucsbalum.com>
2022-03-16 05:08:49 +0000

    Fix include in //content/public/browser/commit_deferring_condition.cc
    Bug: 242216
    Change-Id: I8deb72d84c04779903428d6367d284c1463c9a73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527639
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#981494}

--- content/public/browser/commit_deferring_condition.cc
+++ content/public/browser/commit_deferring_condition.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/commit_deferring_condition.h"
 
-#include "content/browser/renderer_host/navigation_request.h"
+#include "content/public/browser/navigation_handle.h"
 
 namespace content {
 
