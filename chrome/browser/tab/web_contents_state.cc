55afcbe520d4bc45f9b3b6666f94935e7ecf2913
ckitagawa <ckitagawa@chromium.org>
2022-03-16 15:53:56 +0000

    [Android Tab Restore] Make HistoricalTabSaver use Tab
    In order to facilitate bulk closures it is infeasible to create web
    contents for each of possibly 10 or 100s of tabs before passing to
    native and saving to TabRestoreService. Work towards passing Tabs down
    to native in HistoricalTabSaver.
    
    Create a mechanism for unfreezing a tab temporarily without a renderer
    as it is stored to TabRestoreService to improve performance.
    
    Check URL validity in Java to avoid passing tabs that won't be saved
    down to native where the tab must be unfrozen to check the URL.
    
    Bug: 1306278
    Change-Id: I045932375147a61be9e3c13e6cacd331aa457107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3502978
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981664}

--- chrome/browser/tab/web_contents_state.cc
+++ chrome/browser/tab/web_contents_state.cc
@@ -352,7 +352,8 @@ ScopedJavaLocalRef<jobject> WriteNavigationsAsByteBuffer(
 WebContents* RestoreContentsFromByteBuffer(void* data,
                                            int size,
                                            int saved_state_version,
-                                           bool initially_hidden) {
+                                           bool initially_hidden,
+                                           bool no_renderer) {
   bool is_off_the_record;
   int current_entry_index;
   std::vector<sessions::SerializedNavigationEntry> navigations;
@@ -377,6 +378,10 @@ WebContents* RestoreContentsFromByteBuffer(void* data,
   WebContents::CreateParams params(profile);
 
   params.initially_hidden = initially_hidden;
+  if (no_renderer) {
+    params.desired_renderer_state =
+        WebContents::CreateParams::kNoRendererProcess;
+  }
   std::unique_ptr<WebContents> web_contents(WebContents::Create(params));
   web_contents->GetController().Restore(
       current_entry_index, content::RestoreType::kRestored, &entries);
@@ -488,7 +493,8 @@ ScopedJavaLocalRef<jobject> WebContentsState::RestoreContentsFromByteBuffer(
     JNIEnv* env,
     jobject state,
     jint saved_state_version,
-    jboolean initially_hidden) {
+    jboolean initially_hidden,
+    jboolean no_renderer) {
   void* data = env->GetDirectBufferAddress(state);
   int size = env->GetDirectBufferCapacity(state);
 
@@ -497,7 +503,7 @@ ScopedJavaLocalRef<jobject> WebContentsState::RestoreContentsFromByteBuffer(
     return ScopedJavaLocalRef<jobject>();
 
   WebContents* web_contents = ::RestoreContentsFromByteBuffer(
-      data, size, saved_state_version, initially_hidden);
+      data, size, saved_state_version, initially_hidden, no_renderer);
 
   if (web_contents)
     return web_contents->GetJavaWebContents();
@@ -545,9 +551,10 @@ JNI_WebContentsStateBridge_RestoreContentsFromByteBuffer(
     JNIEnv* env,
     const JavaParamRef<jobject>& state,
     jint saved_state_version,
-    jboolean initially_hidden) {
+    jboolean initially_hidden,
+    jboolean no_renderer) {
   return WebContentsState::RestoreContentsFromByteBuffer(
-      env, state, saved_state_version, initially_hidden);
+      env, state, saved_state_version, initially_hidden, no_renderer);
 }
 
 static ScopedJavaLocalRef<jobject>
