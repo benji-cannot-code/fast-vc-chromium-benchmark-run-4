cc5e6031a4e3467e13613ece3bf11f762782dc35
Jonathan Lee <jonathanjlee@google.com>
2022-03-16 21:35:14 +0000

    Strip WPT prefix ("external/wpt/") from --gtest_filter paths
    Bug: 1293893
    Test: run_{system_webview_wpt,weblayer_shell_wpt,chrome_public_wpt,wpt_tests_isolate} --gtest_filter=external/wpt/badging:external/wpt/webusb/usb.serviceworker.https.html --gtest_repeat=2
    Change-Id: I66fa8d3d7f4f33a54a821b5dea64814678895125
    Cq-Include-Trybots: luci.chromium.try:linux-wpt-identity-fyi-rel,linux-wpt-input-fyi-rel,android-weblayer-pie-x86-wpt-smoketest,linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3523398
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#981868}

--- testing/scripts/wpt_common.py
+++ testing/scripts/wpt_common.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import base64
 import json
 import os
+import posixpath
 import sys
 import time
 
@@ -20,6 +21,7 @@ DEFAULT_ISOLATED_SCRIPT_TEST_OUTPUT = os.path.join(OUT_DIR, "results.json")
 TYP_DIR = os.path.join(CATAPULT_DIR, 'third_party', 'typ')
 WEB_TESTS_DIR = os.path.normpath(
     os.path.join(BLINK_TOOLS_DIR, os.pardir, 'web_tests'))
+_WPT_ROOT_FRAGMENT = posixpath.join('external', 'wpt', '')
 TESTS_ROOT_DIR = os.path.join(WEB_TESTS_DIR, 'external', 'wpt')
 
 if BLINK_TOOLS_DIR not in sys.path:
@@ -36,6 +38,23 @@ from blinkpy.web_tests.models import test_failures
 from typ.artifacts import Artifacts
 from typ.json_results import Result
 from typ.result_sink import ResultSinkReporter
+
+
+def strip_wpt_root_prefix(path):
+    """Remove a redundant prefix from a WPT path.
+
+    ResultDB identifies WPTs as web tests with the prefix "external/wpt", but
+    wptrunner expects paths relative to the WPT root, which already ends in
+    "external/wpt". This function transforms a general web test path into a
+    WPT path.
+
+    WPT paths are always POSIX-style.
+    """
+    if path.startswith(_WPT_ROOT_FRAGMENT):
+        return posixpath.relpath(path, _WPT_ROOT_FRAGMENT)
+    # Path is absolute or does not start with the prefix.
+    # Assume the path already points to a valid WPT and pass through.
+    return path
 
 
 class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
@@ -369,8 +388,7 @@ class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
         """
         # When looking into the WPT manifest, we omit "external/wpt" from the
         # test name, since that part of the path is only in Chromium.
-        wpt_test_name = test_name.replace(
-            os.path.join("external", "wpt", ""), "")
+        wpt_test_name = strip_wpt_root_prefix(test_name)
         test_file_subpath = self.wpt_manifest.file_path_for_test_url(
             wpt_test_name)
         if not test_file_subpath:
@@ -495,8 +513,7 @@ class BaseWptScriptAdapter(common.BaseIsolatedScriptArgsAdapter):
             # When comparing the test name to the image URL, we omit
             # "external/wpt" from the test name, since that part of the path is
             # only in Chromium.
-            wpt_test_name = test_name.replace(
-                os.path.join("external", "wpt", ""), "")
+            wpt_test_name = strip_wpt_root_prefix(test_name)
             if wpt_test_name == url:
                 screenshot_key = "actual_image"
                 file_suffix = test_failures.FILENAME_SUFFIX_ACTUAL
