7720020726aa787375a1ca51f02ba7e3bcaf6285
ckitagawa <ckitagawa@chromium.org>
2022-03-16 14:09:16 +0000

    [Android Tab Restore] Don't start a renderer on background restore
    When restoring background tabs using the TabRestoreService we shouldn't
    need to load and start renderers for each tab. Instead:
    1. Start a WebContents in a no renderer state.
    2. Attach the navigation history to the web contents.
    3. Make the web contents into an Android Tab.
    4. Force update the Tab's title to ensure it corresponds to the title
       in the navigation controller (so it appears correctly in various UI).
    
    On opening the tab the WebContents will automatically reload to the
    correct navigation entry.
    
    Note there will not be a thumbnail for the tab; however, this is already
    the current behavior. That will potentially be fixed as a follow up.
    
    This is foundational work for bulk tab restore where starting renderers
    for each of possibly 10s or 100s of background restored tabs would be
    infeasible.
    Prototype CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/3489596
    
    Bug: 1304886
    Change-Id: Ic27c9ea1a90d7b6d6c7f8f9d1d7b90d238c2731f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3499300
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981606}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentlyClosedBridgeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/RecentlyClosedBridgeTest.java
@@ -121,7 +121,11 @@ public class RecentlyClosedBridgeTest {
             final TabModel model = mActivity.getTabModelSelectorSupplier().get().getModel(false);
             final TabList list = model.getComprehensiveModel();
             Assert.assertEquals(2, list.getCount());
-            Assert.assertTrue(hasTabWithUrlAndTitle(list, getUrl(TEST_PAGE_A), titles[0]));
+            final Tab tab = findTabWithUrlAndTitle(list, getUrl(TEST_PAGE_A), titles[0]);
+            Assert.assertNotNull(tab);
+
+            // No Renderer for background tab.
+            Assert.assertNull(tab.getWebContents().getRenderWidgetHostView());
         });
     }
 
@@ -179,6 +183,8 @@ public class RecentlyClosedBridgeTest {
                             .getNavigationController()
                             .getNavigationHistory()
                             .getEntryCount());
+            // Has renderer for foreground tab.
+            Assert.assertNotNull(tabC.getWebContents().getRenderWidgetHostView());
         });
     }
 
@@ -224,20 +230,24 @@ public class RecentlyClosedBridgeTest {
             final TabModel model = mActivity.getTabModelSelectorSupplier().get().getModel(false);
             final TabList list = model.getComprehensiveModel();
             Assert.assertEquals(3, list.getCount());
-            Assert.assertTrue(hasTabWithUrlAndTitle(list, getUrl(TEST_PAGE_A), titles[0]));
+            final Tab tab = findTabWithUrlAndTitle(list, getUrl(TEST_PAGE_A), titles[0]);
+            Assert.assertNotNull(tab);
+
+            // No renderer for background tab.
+            Assert.assertNull(tab.getWebContents().getRenderWidgetHostView());
         });
     }
 
-    private boolean hasTabWithUrlAndTitle(TabList list, String url, String title) {
-        boolean foundRestoredTab = false;
+    private Tab findTabWithUrlAndTitle(TabList list, String url, String title) {
+        Tab targetTab = null;
         for (int i = 0; i < list.getCount(); ++i) {
             Tab tab = list.getTabAt(i);
             if (tab.getUrl().getSpec().equals(url) && tab.getTitle().equals(title)) {
-                foundRestoredTab = true;
+                targetTab = tab;
                 break;
             }
         }
-        return foundRestoredTab;
+        return targetTab;
     }
 
     private String getUrl(String relativeUrl) {
