3388e49e9a688085457feca7ad1836709a4cd260
Azeem Arshad <azeemarshad@chromium.org>
2022-03-16 18:23:04 +0000

    [CrOS Cellular] Fix memory leak in HermesManagerClient
    This CL changes a plain pointer reference to
    HermesManagerClient::Properties to std::unique_ptr so that it is freed
    correctly on shutdown.
    
    Fixed: b/224990576
    Change-Id: Ie96c418574794615d794c6c0397f445dce8e0c0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530788
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981747}

--- chromeos/dbus/hermes/hermes_manager_client.cc
+++ chromeos/dbus/hermes/hermes_manager_client.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/dbus/hermes/hermes_manager_client.h"
 
+#include <memory>
+
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -32,7 +34,7 @@ class HermesManagerClientImpl : public HermesManagerClient {
     dbus::ObjectPath hermes_manager_path(hermes::kHermesManagerPath);
     object_proxy_ =
         bus->GetObjectProxy(hermes::kHermesServiceName, hermes_manager_path);
-    properties_ = new Properties(
+    properties_ = std::make_unique<Properties>(
         object_proxy_,
         base::BindRepeating(&HermesManagerClientImpl::OnPropertyChanged,
                             weak_ptr_factory_.GetWeakPtr(),
@@ -82,7 +84,7 @@ class HermesManagerClientImpl : public HermesManagerClient {
   }
 
   dbus::ObjectProxy* object_proxy_;
-  Properties* properties_;
+  std::unique_ptr<Properties> properties_;
   base::WeakPtrFactory<HermesManagerClientImpl> weak_ptr_factory_{this};
 };
 
