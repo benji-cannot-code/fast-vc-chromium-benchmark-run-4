d63d2b1e7ca63729d68b039ca958fb7965353ccf
Gavin Williams <gavinwill@chromium.org>
2022-03-16 18:12:00 +0000

    Shimless: Use Chrome device placeholder strings
    Sets the strings to use Chromebook/Chromebox/Chromebit based on the
    device under repair.
    
    Bug: 1198187
    Change-Id: Ia7b8175966ba1212da38058218cd2eb3580729b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3523467
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981742}

--- ash/webui/shimless_rma/shimless_rma.cc
+++ ash/webui/shimless_rma/shimless_rma.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "services/network/public/mojom/content_security_policy.mojom.h"
+#include "ui/chromeos/devicetype_utils.h"
 #include "ui/chromeos/strings/network_element_localized_strings_provider.h"
 #include "ui/resources/grit/webui_generated_resources.h"
 #include "ui/resources/grit/webui_resources.h"
@@ -71,7 +72,6 @@ void AddShimlessRmaStrings(content::WebUIDataSource* html_source) {
       {"componentKeyboard", IDS_SHIMLESS_RMA_COMPONENT_KEYBOARD},
       {"componentPowerButton", IDS_SHIMLESS_RMA_COMPONENT_POWER_BUTTON},
       // Splash screen
-      {"shimlessSplashTitle", IDS_SHIMLESS_RMA_SPLASH_TITLE},
       {"shimlessSplashRemembering", IDS_SHIMLESS_RMA_SPLASH_REMEMBERING},
       {"shimlessSplashLoading", IDS_SHIMLESS_RMA_SPLASH_LOADING},
       // Common buttons
@@ -82,7 +82,6 @@ void AddShimlessRmaStrings(content::WebUIDataSource* html_source) {
       {"okButtonLabel", IDS_SHIMLESS_RMA_OK_BUTTON},
       {"retryButtonLabel", IDS_SHIMLESS_RMA_RETRY_BUTTON},
       // Landing page
-      {"welcomeTitleText", IDS_SHIMLESS_RMA_LANDING_PAGE_TITLE},
       {"beginRmaWarningText", IDS_SHIMLESS_RMA_AUTHORIZED_TECH_ONLY_WARNING},
       {"validatingComponentsText", IDS_SHIMLESS_RMA_VALIDATING_COMPONENTS},
       {"validatedComponentsSuccessText",
@@ -137,8 +136,6 @@ void AddShimlessRmaStrings(content::WebUIDataSource* html_source) {
        IDS_SHIMLESS_RMA_CHOOSE_WP_DISABLE_METHOD_PAGE_TITLE},
       {"manualWpDisableMethodOptionText",
        IDS_SHIMLESS_RMA_MANUAL_WP_DISABLE_METHOD_OPTION},
-      {"manualWpDisableMethodDescriptionText",
-       IDS_SHIMLESS_RMA_MANUAL_WP_DISABLE_METHOD_DESCRIPTION},
       {"rsuWpDisableMethodOptionText",
        IDS_SHIMLESS_RMA_RSU_WP_DISABLE_METHOD_OPTION},
       {"rsuWpDisableMethodDescriptionText",
@@ -299,7 +296,6 @@ void AddShimlessRmaStrings(content::WebUIDataSource* html_source) {
       {"onboardingUpdatePermission",
        IDS_SHIMLESS_RMA_ONBOARDING_UPDATE_PERMISSION},
       // Critical error
-      {"criticalErrorTitleText", IDS_SHIMLESS_RMA_CRITICAL_ERROR_TITLE},
       {"criticalErrorMessageText", IDS_SHIMLESS_RMA_CRITICAL_ERROR_MESSAGE},
       {"criticalErrorExitText", IDS_SHIMLESS_RMA_CRITICAL_EXIT_BUTTON},
       {"criticalErrorRebootText", IDS_SHIMLESS_RMA_CRITICAL_REBOOT_BUTTON},
@@ -317,6 +313,22 @@ void AddShimlessRmaStrings(content::WebUIDataSource* html_source) {
 
   html_source->AddLocalizedStrings(kLocalizedStrings);
   html_source->UseStringsJs();
+}
+
+void AddDevicePlaceholderStrings(content::WebUIDataSource* html_source) {
+  html_source->AddString(
+      "shimlessSplashTitle",
+      ui::SubstituteChromeOSDeviceType(IDS_SHIMLESS_RMA_SPLASH_TITLE));
+  html_source->AddString(
+      "welcomeTitleText",
+      ui::SubstituteChromeOSDeviceType(IDS_SHIMLESS_RMA_LANDING_PAGE_TITLE));
+  html_source->AddString(
+      "manualWpDisableMethodDescriptionText",
+      ui::SubstituteChromeOSDeviceType(
+          IDS_SHIMLESS_RMA_MANUAL_WP_DISABLE_METHOD_DESCRIPTION));
+  html_source->AddString(
+      "criticalErrorTitleText",
+      ui::SubstituteChromeOSDeviceType(IDS_SHIMLESS_RMA_CRITICAL_ERROR_TITLE));
 }
 
 }  // namespace
@@ -371,6 +383,7 @@ ShimlessRMADialogUI::ShimlessRMADialogUI(
   SetUpWebUIDataSource(html_source, resources, IDR_ASH_SHIMLESS_RMA_INDEX_HTML);
 
   AddShimlessRmaStrings(html_source);
+  AddDevicePlaceholderStrings(html_source);
 
   ui::network_element::AddLocalizedStrings(html_source);
   ui::network_element::AddOncLocalizedStrings(html_source);
