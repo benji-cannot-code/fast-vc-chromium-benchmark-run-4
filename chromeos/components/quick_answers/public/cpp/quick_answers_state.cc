5a4bb4e387bceef3c5262085e67ec378c5cc2bac
Yue Li <updowndota@google.com>
2022-03-16 17:57:15 +0000

    Quick Answers: Rewrite consent logic to respect enforced state
    Directly set consent status instead of expose enforced state of the
    prefs when the feature is enforced off.
    
    Bug: b/222156844
    Test: Run existing tests
    Change-Id: Ia14fc2a2ae01173c27f4ed9c3d603d3d30595e59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526565
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981733}

--- chromeos/components/quick_answers/public/cpp/quick_answers_state.cc
+++ chromeos/components/quick_answers/public/cpp/quick_answers_state.cc
@@ -198,11 +198,6 @@ bool QuickAnswersState::ShouldUseQuickAnswersTextAnnotator() {
          use_text_annotator_for_testing_;
 }
 
-bool QuickAnswersState::IsSettingsEnforced() {
-  return pref_change_registrar_->prefs()->IsManagedPreference(
-      quick_answers::prefs::kQuickAnswersEnabled);
-}
-
 void QuickAnswersState::InitializeObserver(
     QuickAnswersStateObserver* observer) {
   if (prefs_initialized_)
@@ -210,8 +205,9 @@ void QuickAnswersState::InitializeObserver(
 }
 
 void QuickAnswersState::UpdateSettingsEnabled() {
-  auto settings_enabled =
-      pref_change_registrar_->prefs()->GetBoolean(kQuickAnswersEnabled);
+  auto* prefs = pref_change_registrar_->prefs();
+
+  auto settings_enabled = prefs->GetBoolean(kQuickAnswersEnabled);
   if (settings_enabled_ == settings_enabled) {
     return;
   }
@@ -220,8 +216,14 @@ void QuickAnswersState::UpdateSettingsEnabled() {
   // If the user turn on the Quick Answers in settings, set the consented status
   // to true.
   if (settings_enabled_) {
-    pref_change_registrar_->prefs()->SetInteger(kQuickAnswersConsentStatus,
-                                                ConsentStatus::kAccepted);
+    prefs->SetInteger(kQuickAnswersConsentStatus, ConsentStatus::kAccepted);
+  }
+
+  // If the feature is enforced off by the administrator policy, set the
+  // consented status to rejected.
+  if (!settings_enabled_ &&
+      prefs->IsManagedPreference(quick_answers::prefs::kQuickAnswersEnabled)) {
+    prefs->SetInteger(kQuickAnswersConsentStatus, ConsentStatus::kRejected);
   }
 
   for (auto& observer : observers_)
