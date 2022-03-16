9c09418c0ace1620cb559eda9caf9fcefcde4f74
Alexander Bolodurin <alexbn@chromium.org>
2022-03-16 07:45:09 +0000

    [lacros dgapi] stub implementation
    Add a stub Digital Goods implementation in Lacros to avoid a renderer
    crash until a proper Digital Goods service is implemented for Lacros.
    
    Bug: 1203666
    Change-Id: I7355882f3c99d970593aecca7e70c3013483fc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3528034
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981522}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -269,6 +269,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/webui/resources/cr_components/app_management/app_management.mojom.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "chrome/browser/apps/digital_goods/digital_goods_factory_stub.h"
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #include "components/services/screen_ai/public/cpp/screen_ai_service_router.h"
 #include "components/services/screen_ai/public/cpp/screen_ai_service_router_factory.h"
@@ -679,6 +683,12 @@ void PopulateChromeFrameBinders(
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   map->Add<payments::mojom::DigitalGoodsFactory>(base::BindRepeating(
       &apps::DigitalGoodsFactoryImpl::BindDigitalGoodsFactory));
+#endif
+
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  // TODO(crbug.com/1203666): replace with a real implementation for Lacros.
+  map->Add<payments::mojom::DigitalGoodsFactory>(
+      base::BindRepeating(&apps::DigitalGoodsFactoryStub::Bind));
 #endif
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
