81a515ff065fee68367210c63e21a2c60d7d7539
David Sanders <dsanders11@ucsbalum.com>
2022-03-16 19:25:57 +0000

    Remove unused includes in render_widget_helper.cc
    Bug: 242216
    Change-Id: Ifa313dc6c8787d18c1f85dcc13e345f1021055af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526711
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#981782}

--- content/browser/renderer_host/render_widget_helper.cc
+++ content/browser/renderer_host/render_widget_helper.cc
@@ -8,11 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/lazy_instance.h"
-#include "base/posix/eintr_wrapper.h"
-#include "base/threading/thread.h"
-#include "base/threading/thread_restrictions.h"
-#include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/public/browser/browser_task_traits.h"
+#include "content/public/browser/browser_thread.h"
 
 namespace content {
 namespace {
