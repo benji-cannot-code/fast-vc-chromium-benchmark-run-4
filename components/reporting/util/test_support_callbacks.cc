ab520fd1291dd274e505fda34d8c82d7227c8fbe
Leonid Baraz <lbaraz@chromium.org>
2022-03-16 19:23:08 +0000

    Ensure TestEvents run on the main thread
    Flakes that were detected recently seem to be related to Write operation
    completion guarantee. RunLoop::Quit is allowed to run on threads, but
    in that case its completion is not immediate, and  waiting for the
    result on the main thread could be delayed, potentially making result
    retrieval flaky on weakly ordered CPUs.
    Fixing it here by updating result on the main thread (the same one where
    the TestEvent was placed).
    TestEvents and other support classes semantics remains the same, but
    should be now consistent on weakly ordered architectures too.
    
    Bug: b:222539905
    Bug: 1306208
    Change-Id: I49afce7a19859f4fe3a24ce047525254ab5c7572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3522700
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981781}

--- components/reporting/util/test_support_callbacks.cc
+++ components/reporting/util/test_support_callbacks.cc
@@ -5,13 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/reporting/util/test_support_callbacks.h"
 
-#include "base/run_loop.h"
-
 namespace reporting {
 namespace test {
 
-TestCallbackWaiter::TestCallbackWaiter()
-    : run_loop_(base::RunLoop::Type::kNestableTasksAllowed) {}
+TestCallbackWaiter::TestCallbackWaiter() = default;
 TestCallbackWaiter::~TestCallbackWaiter() = default;
 
 TestCallbackAutoWaiter::TestCallbackAutoWaiter() {
