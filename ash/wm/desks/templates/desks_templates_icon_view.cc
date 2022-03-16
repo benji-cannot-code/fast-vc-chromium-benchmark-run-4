44ddae76cab12c83705e66b4c9e6495222420b1a
Andrew Pantera <andp@chromium.org>
2022-03-16 00:19:14 +0000

    Desk Templates: Modify icon container max count
    Changed the maximum displayed app count from 9 to 99. When the count enters the double digits the pills resize to respect the defined padding
    
    Bug: 1288994
    Test: updated existing test
    Change-Id: I86749eb9d74f9e54c269ba48efa02d20840c469e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3523522
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Andrew Pantera <andp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981415}

--- ash/wm/desks/templates/desks_templates_icon_view.cc
+++ ash/wm/desks/templates/desks_templates_icon_view.cc
@@ -39,13 +39,13 @@ constexpr int kCountLabelInsetSize = 4;
 // resizing.
 constexpr int kAppIdImageSize = 64;
 
-// Return the formatted string for `count`. If `count` is <=9, the string will
-// be "+<count>". If `count` is >9, the string will be "9+". If `show_plus` is
-// true, the string will be just the count.
+// Return the formatted string for `count`. If `count` is <=99, the string will
+// be "+<count>". If `count` is >99, the string will be "+99". If `show_plus` is
+// false, the string will be just the count.
 std::u16string GetCountString(int count, bool show_plus) {
   if (show_plus) {
-    return base::UTF8ToUTF16(count > 9 ? "9+"
-                                       : base::StringPrintf("+%i", count));
+    return base::UTF8ToUTF16(count > 99 ? "+99"
+                                        : base::StringPrintf("+%i", count));
   }
   return base::NumberToString16(count);
 }
@@ -154,7 +154,9 @@ void DesksTemplatesIconView::UpdateCount(int count) {
 }
 
 gfx::Size DesksTemplatesIconView::CalculatePreferredSize() const {
-  return gfx::Size(count_ > 1 && icon_view_ ? 2 * kIconSize : kIconSize,
+  return gfx::Size(count_ > 1 && icon_view_ && count_label_
+                       ? count_label_->bounds().width() + kIconSize
+                       : kIconSize,
                    kIconSize);
 }
 
@@ -164,7 +166,8 @@ void DesksTemplatesIconView::Layout() {
 
   if (count_label_) {
     count_label_->SetBoundsRect(
-        gfx::Rect(icon_view_ ? kIconSize : 0, 0, kIconSize, kIconSize));
+        gfx::Rect(icon_view_ ? kIconSize : 0, 0,
+                  count_ > 9 ? 1.25 * kIconSize : kIconSize, kIconSize));
   }
 }
 
