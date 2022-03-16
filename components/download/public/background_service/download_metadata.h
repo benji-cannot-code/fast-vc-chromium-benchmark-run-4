e55ef87b827d9d6b49e7f5c3880ad609087ddd4a
Xing Liu <xingliu@chromium.org>
2022-03-16 02:30:18 +0000

    Download: Add CustomData in background download API.
    Adds a CustomData key value pair in background download service public
    API. This could be used from client teams to track metrics, etc.
    
    Internal changes will be done in following CLs.
    
    Bug: 1304424
    Change-Id: I76c875514bdf4f3c594b87e2acdd43bf136db411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3523901
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981468}

--- components/download/public/background_service/download_metadata.h
+++ components/download/public/background_service/download_metadata.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/scoped_refptr.h"
 #include "build/build_config.h"
+#include "components/download/public/background_service/download_params.h"
 #include "net/http/http_response_headers.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
 #include "url/gurl.h"
@@ -50,6 +51,9 @@ struct CompletionInfo {
   // An optional base::HexEncoded SHA-256 hash (if available) of the file
   // contents.  If empty there is no available hash value.
   std::string hash256;
+
+  // The custom data sent back to clients when download is completed or failed.
+  DownloadParams::CustomData custom_data;
 
   CompletionInfo();
   CompletionInfo(
--- components/download/public/background_service/download_params.h
+++ components/download/public/background_service/download_params.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DOWNLOAD_PUBLIC_BACKGROUND_SERVICE_DOWNLOAD_PARAMS_H_
 #define COMPONENTS_DOWNLOAD_PUBLIC_BACKGROUND_SERVICE_DOWNLOAD_PARAMS_H_
 
+#include <map>
+#include <string>
+
 #include "base/callback.h"
 #include "base/time/time.h"
 #include "components/download/public/background_service/clients.h"
@@ -127,6 +130,8 @@ struct RequestParams {
 // The |client| needs to be properly created and registered for this service for
 // the download to be accepted.
 struct DownloadParams {
+  using CustomData = std::map<std::string, std::string>;
+
   enum StartResult {
     // The download is accepted and persisted.
     ACCEPTED,
@@ -171,6 +176,11 @@ struct DownloadParams {
   // A callback that will be notified if this download has been accepted and
   // persisted by the DownloadService.
   StartCallback callback;
+
+  // Custom key value pair to store custom data for various purposes. Has a 1024
+  // bytes size limit for each key or value. Will be sent back to clients when
+  // download is completed or failed.
+  CustomData custom_data;
 
   // The parameters that determine under what device conditions this download
   // will occur.
