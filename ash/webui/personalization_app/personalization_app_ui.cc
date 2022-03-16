1dd483ed515907b56e9f02a4a47e269dab9eaa8b
Jason Thai <jasontt@chromium.org>
2022-03-16 19:52:40 +0000

    personalization: show animation themes by default
    After CL https://crrev.com/c/3524563 is merged, we no longer need to
    hide the animation themes in the hub.
    
    BUG=b/213597539
    TEST=Manually
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I8df5758c02e4d8f0f98ac7812ea824a7deb8499f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527754
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981802}

--- ash/webui/personalization_app/personalization_app_ui.cc
+++ ash/webui/personalization_app/personalization_app_ui.cc
@@ -187,9 +187,6 @@ void AddBooleans(content::WebUIDataSource* source) {
 
   source->AddBoolean("isDarkLightModeEnabled",
                      features::IsDarkLightModeEnabled());
-
-  source->AddBoolean("isAmbientModeAnimationEnabled",
-                     features::IsAmbientModeAnimationEnabled());
 }
 
 }  // namespace
