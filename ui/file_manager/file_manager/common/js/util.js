082acbc56b5e4b720fb80ed784ed4227d871c35e
Bo Majewski <majewski@chromium.org>
2022-03-16 04:57:33 +0000

    [Files]: Convert console.error to console.warn - common
    File manager issues a number of errors to the console. They are then
    reported as crashes by finch and the like. However, in many cases these
    are not true crashes. Instead they are handled errors for which we
    provide a graceful solution. If the user is not impacted, these should
    be reported as warnings, helping us with the debugging effort. However,
    they should not be reported as errors. This CL attempts to adjust the
    logging level for the common files.
    
    Bug: b/223286333
    Change-Id: I8416a59cfbd0aecb9c52cd1395e3505b704bfe97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3515413
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981489}

--- ui/file_manager/file_manager/common/js/util.js
+++ ui/file_manager/file_manager/common/js/util.js
@@ -313,7 +313,7 @@ export function str(id) {
   try {
     return loadTimeData.getString(id);
   } catch (e) {
-    console.warn('Failed to get string for ', id);
+    console.warn('Failed to get string for', id);
     return id;
   }
 }
@@ -365,42 +365,6 @@ util.addIsFocusedMethod = () => {
   };
 };
 
-/**
- * Checks, if the Files app's window is in a full screen mode.
- *
- * @param {chrome.app.window.AppWindow} appWindow App window to be maximized.
- * @return {boolean} True if the full screen mode is enabled.
- */
-util.isFullScreen = appWindow => {
-  if (appWindow) {
-    return appWindow.isFullscreen();
-  } else {
-    console.error(
-        'App window not passed. Unable to check status of the full screen ' +
-        'mode.');
-    return false;
-  }
-};
-
-/**
- * Toggles the full screen mode.
- *
- * @param {chrome.app.window.AppWindow} appWindow App window to be maximized.
- * @param {boolean} enabled True for enabling, false for disabling.
- */
-util.toggleFullScreen = (appWindow, enabled) => {
-  if (appWindow) {
-    if (enabled) {
-      appWindow.fullscreen();
-    } else {
-      appWindow.restore();
-    }
-    return;
-  }
-
-  console.error(
-      'App window not passed. Unable to toggle the full screen mode.');
-};
 
 /**
  * The type of a file operation.
@@ -983,7 +947,7 @@ util.URLsToEntries = (urls, opt_callback) => {
           opt_callback(result.entries, result.failureUrls);
         })
         .catch(error => {
-          console.error(
+          console.warn(
               'util.URLsToEntries is failed.',
               error.stack ? error.stack : error);
         });
@@ -1283,7 +1247,7 @@ util.addEventListenerToBackgroundComponent = (target, type, handler) => {
  */
 util.checkAPIError = () => {
   if (chrome.runtime.lastError) {
-    console.error(chrome.runtime.lastError.message);
+    console.warn(chrome.runtime.lastError.message);
   }
 };
 
