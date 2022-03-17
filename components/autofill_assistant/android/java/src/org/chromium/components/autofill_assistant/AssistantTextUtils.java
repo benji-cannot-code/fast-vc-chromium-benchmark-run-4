dcb02bb11285f533ca7eae1716354d3af0375a11
Sergio Vila Prado <sergiovila@google.com>
2022-03-17 14:59:37 +0000

    Add dialog to DataOriginNotice
    Bug: b/204420238
    Change-Id: Ibb79e25c077d7df1b2abf61b5c4ab2ae6c94db4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3522071
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Commit-Queue: Sergio Vila Prado <sergiovila@google.com>
    Cr-Commit-Position: refs/heads/main@{#982211}

--- components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/AssistantTextUtils.java
+++ components/autofill_assistant/android/java/src/org/chromium/components/autofill_assistant/AssistantTextUtils.java
@@ -4,8 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 package org.chromium.components.autofill_assistant;
-
+import android.content.Context;
 import android.graphics.Typeface;
+import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.style.StyleSpan;
 import android.widget.TextView;
@@ -48,6 +49,21 @@ public class AssistantTextUtils {
      */
     public static void applyVisualAppearanceTags(
             TextView view, String text, @Nullable Callback<Integer> linkCallback) {
+        SpannableString textWithSpans =
+                applyVisualAppearanceTags(text, view.getContext(), linkCallback);
+        view.setText(textWithSpans);
+        if (textWithSpans.getSpans(0, textWithSpans.length(), NoUnderlineClickableSpan.class).length
+                != 0) {
+            view.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+    }
+
+    /**
+     * Returns a {@link SpannableString} version of {@code text}, with the appropriate appearance
+     * tags.
+     */
+    public static SpannableString applyVisualAppearanceTags(
+            String text, Context context, @Nullable Callback<Integer> linkCallback) {
         List<SpanApplier.SpanInfo> spans = new ArrayList<>();
         if (text.contains("<b>")) {
             spans.add(BOLD_SPAN_INFO);
@@ -65,7 +81,7 @@ public class AssistantTextUtils {
 
         for (Integer linkId : linkIds) {
             spans.add(new SpanApplier.SpanInfo("<link" + linkId + ">", "</link" + linkId + ">",
-                    new NoUnderlineClickableSpan(view.getContext(), (unusedView) -> {
+                    new NoUnderlineClickableSpan(context, (unusedView) -> {
                         if (linkCallback != null) {
                             linkCallback.onResult(linkId);
                         }
@@ -83,10 +99,7 @@ public class AssistantTextUtils {
                 Log.d(TAG, "Mismatching span", e);
             }
         }
-        view.setText(SpanApplier.applySpans(
-                text, successfulSpans.toArray(new SpanApplier.SpanInfo[successfulSpans.size()])));
-        if (!linkIds.isEmpty()) {
-            view.setMovementMethod(LinkMovementMethod.getInstance());
-        }
+        return SpanApplier.applySpans(
+                text, successfulSpans.toArray(new SpanApplier.SpanInfo[successfulSpans.size()]));
     }
 }
