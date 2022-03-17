a93739732b50c1baafd5687475cd02a7390aa193
Yanzhu Du <yzd@google.com>
2022-03-17 10:18:23 +0000

    Add missing fields to Desk Sync
    Several fields are missing in Desk Syncs. This CL fixes that by
    adding them as part of the App definition in the
    workspace_desk_specifics proto file and fills them in the sync
    bridge.
    
    TESTED = Tested round trip equivalency between protos before and
    after sync storage. Manually tested that broken apps launch
    correctly.
    
    Bug: 1298711
    Change-Id: I238c9b6836cccf7b2bff3554d7b89c93e8c749aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529754
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Yanzhu Du <yzd@google.com>
    Cr-Commit-Position: refs/heads/main@{#982114}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -1245,6 +1245,9 @@ VISIT_PROTO_FIELDS(const sync_pb::WorkspaceDeskSpecifics::App& proto) {
   VISIT(window_id);
   VISIT(display_id);
   VISIT_ENUM(pre_minimized_window_state);
+  VISIT_ENUM(container);
+  VISIT_ENUM(disposition);
+  VISIT(app_name);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::WorkspaceDeskSpecifics::AppOneOf& proto) {
@@ -1258,6 +1261,7 @@ VISIT_PROTO_FIELDS(
     const sync_pb::WorkspaceDeskSpecifics::BrowserAppWindow& proto) {
   VISIT_REP(tabs);
   VISIT(active_tab_index);
+  VISIT(show_as_app);
 }
 
 VISIT_PROTO_FIELDS(
