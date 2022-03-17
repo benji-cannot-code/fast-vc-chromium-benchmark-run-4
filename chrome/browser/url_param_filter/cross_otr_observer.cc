6daec026976c4f203174c1dc34074ab2f4386078
Matt Reichhoff <mreichhoff@chromium.org>
2022-03-17 01:38:46 +0000

    [UrlParamFilter] Add cross-OTR refresh metric
    This metric will be used to track breakage when filtering URL params.
    Upcoming CLs will further segment the metric.
    
    Bug: 1265734
    Change-Id: I1137504ba1de6ddb005f49a37931f443e3dc0d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529547
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981981}

--- chrome/browser/url_param_filter/cross_otr_observer.cc
+++ chrome/browser/url_param_filter/cross_otr_observer.cc
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace url_param_filter {
 
-const char kCrossOtrResponseCodeMetricName[] =
+constexpr char kCrossOtrResponseCodeMetricName[] =
     "Navigation.CrossOtr.ContextMenu.ResponseCodeExperimental";
+constexpr char kCrossOtrRefreshCountMetricName[] =
+    "Navigation.CrossOtr.ContextMenu.RefreshCountExperimental";
 
 void CrossOtrObserver::MaybeCreateForWebContents(
     content::WebContents* web_contents,
@@ -39,15 +41,26 @@ CrossOtrObserver::CrossOtrObserver(content::WebContents* web_contents)
 void CrossOtrObserver::DidFinishNavigation(
     content::NavigationHandle* navigation_handle) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  const net::HttpResponseHeaders* headers =
-      navigation_handle->GetResponseHeaders();
-  if (headers) {
-    base::UmaHistogramSparse(
-        kCrossOtrResponseCodeMetricName,
-        net::HttpUtil::MapStatusCodeForHistogram(headers->response_code()));
+  // We only want the first navigation to be counted; after that point, no
+  // response codes should be tracked. The observer is left in place to track
+  // refreshes on the first page.
+  if (!wrote_response_metric_) {
+    wrote_response_metric_ = true;
+    const net::HttpResponseHeaders* headers =
+        navigation_handle->GetResponseHeaders();
+    if (headers) {
+      base::UmaHistogramSparse(
+          kCrossOtrResponseCodeMetricName,
+          net::HttpUtil::MapStatusCodeForHistogram(headers->response_code()));
+    }
+  } else if (navigation_handle->GetReloadType() != content::ReloadType::NONE) {
+    refresh_count_++;
+  } else if (navigation_handle->IsInPrimaryMainFrame() &&
+             !navigation_handle->IsSameDocument() &&
+             navigation_handle->HasCommitted()) {
+    Detach();
+    // DO NOT add code past this point. `this` is destroyed.
   }
-  web_contents()->RemoveUserData(CrossOtrObserver::kUserDataKey);
-  // DO NOT add code past this point. `this` is destroyed.
 }
 
 void CrossOtrObserver::DidRedirectNavigation(
@@ -55,11 +68,26 @@ void CrossOtrObserver::DidRedirectNavigation(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   const net::HttpResponseHeaders* headers =
       navigation_handle->GetResponseHeaders();
-  if (headers) {
+  // After the first navigation has committed, we no longer want to track
+  // redirects.
+  if (!wrote_response_metric_ && headers) {
     base::UmaHistogramSparse(
         kCrossOtrResponseCodeMetricName,
         net::HttpUtil::MapStatusCodeForHistogram(headers->response_code()));
   }
+}
+void CrossOtrObserver::WebContentsDestroyed() {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  // The user has closed the tab or otherwise destroyed the web contents. Flush
+  // metrics and cease observation.
+  Detach();
+  // DO NOT add code past this point. `this` is destroyed.
+}
+
+void CrossOtrObserver::Detach() {
+  base::UmaHistogramCounts100(kCrossOtrRefreshCountMetricName, refresh_count_);
+  web_contents()->RemoveUserData(CrossOtrObserver::kUserDataKey);
+  // DO NOT add code past this point. `this` is destroyed.
 }
 
 }  // namespace url_param_filter
