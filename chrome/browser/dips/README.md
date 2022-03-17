1ad1c4218685a7e2a5f02513867978305d35d646
Ryan Tarpine <rtarpine@chromium.org>
2022-03-17 02:02:54 +0000

    Initial DIPSService skeleton.
    DIPSService will implement the DIPS (Delete Incidental Party State)
    intervention detailed at go/dips. This initial CL is just a skeleton,
    recording some internal state and two metrics. Internal state is
    currently kept only in memory; persistence will be implemented in a
    future CL.
    
    Bug: 1304755
    Change-Id: I6d1e6cd024160517042f7aa21821d370aa08b9aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3428199
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#981991}

---
+++ chrome/browser/dips/README.md
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# DIPS (go/dips)
+## Detect Incidental Party State
+
+Incidental parties are sites without meaningful user interactions, such as
+bounce trackers.
---
+++ chrome/browser/dips/dips_service.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/dips/dips_service.h"
+
+#include "chrome/browser/dips/dips_service_factory.h"
+
+namespace dips {
+
+DIPSService::DIPSService(content::BrowserContext* context)
+    : browser_context_(context) {}
+
+DIPSService::~DIPSService() = default;
+
+/* static */
+DIPSService* DIPSService::Get(content::BrowserContext* context) {
+  return DIPSServiceFactory::GetForBrowserContext(context);
+}
+
+}  // namespace dips
