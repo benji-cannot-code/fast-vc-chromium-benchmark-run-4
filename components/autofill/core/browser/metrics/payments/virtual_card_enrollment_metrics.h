05da867a4b8ad2ba6fb948ce9ac2c6315aee175c
Vinny Persky <vinnypersky@google.com>
2022-03-17 20:43:31 +0000

    Add metrics for when the user cancels the virtual card enroll bubble and refactor Virtual Card Enroll Bubble metrics interactive UI tests
    We noticed that there is no metrics logging for when a user cancels the Virtual Card Enroll Bubble, this CL adds a branch in VirtualCardEnrollBubbleControllerImpl::OnBubbleClosed() to log cancel metrics as well.
    
    This CL also refactors the metrics interactive UI tests to use a shared helper function as they are very similar.
    
    Lastly, this CL adds a separate Bubble Closed test case.
    
    Diffs:
    - Bubble Lost Focus Diff: https://diff.googleplex.com/#key=K54soPa5ycWO
    - Bubble Accepted Diff: https://diff.googleplex.com/#key=H4QRQTEuA3db
    - Bubble Not Interacted Diff: https://diff.googleplex.com/#key=RWcVzWffchq7
    
    Bug: 1306290
    Change-Id: I91fc9f60fc1c85b9210fe91d0134fbf4b9c60325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529381
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#982397}

--- components/autofill/core/browser/metrics/payments/virtual_card_enrollment_metrics.h
+++ components/autofill/core/browser/metrics/payments/virtual_card_enrollment_metrics.h
@@ -32,7 +32,9 @@ enum class VirtualCardEnrollmentBubbleResult {
   VIRTUAL_CARD_ENROLLMENT_BUBBLE_NOT_INTERACTED = 3,
   // The bubble lost focus and was deactivated.
   VIRTUAL_CARD_ENROLLMENT_BUBBLE_LOST_FOCUS = 4,
-  kMaxValue = VIRTUAL_CARD_ENROLLMENT_BUBBLE_LOST_FOCUS,
+  // The user cancelled the bubble.
+  VIRTUAL_CARD_ENROLLMENT_BUBBLE_CANCELLED = 5,
+  kMaxValue = VIRTUAL_CARD_ENROLLMENT_BUBBLE_CANCELLED,
 };
 
 // Metrics to record the source that prompted the virtual card enrollment
