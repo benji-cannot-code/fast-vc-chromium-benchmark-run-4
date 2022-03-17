ee7603a0bb184bb0144528261c6ae74ab8f35277
Joel Hockey <joelhockey@chromium.org>
2022-03-17 04:20:47 +0000

    Store URL directly for Terminal Menu rather than using shortcut format
    Bug: b/223076712
    Change-Id: I3135c0c1d66051470d6944bae19754104c1b3f24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529989
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Jason Lin <lxj@google.com>
    Commit-Queue: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/main@{#982036}

--- chrome/browser/ui/web_applications/terminal_system_app_menu_model_chromeos.cc
+++ chrome/browser/ui/web_applications/terminal_system_app_menu_model_chromeos.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/public/cpp/app_menu_constants.h"
-#include "base/json/json_reader.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
@@ -47,7 +46,9 @@ void TerminalSystemAppMenuModel::Build() {
   std::vector<std::pair<std::string, std::string>> connections =
       crostini::GetSSHConnections(browser()->profile());
   for (const auto& connection : connections) {
-    shortcuts_.push_back(crostini::ShortcutIdForSSH(connection.first));
+    urls_.push_back(GURL(base::StrCat(
+        {chrome::kChromeUIUntrustedTerminalURL,
+         "html/terminal_ssh.html#profile-id:", connection.first})));
     AddItemWithIcon(next_command++, base::UTF8ToUTF16(connection.second),
                     icon(kTerminalSshIcon));
   }
@@ -56,7 +57,8 @@ void TerminalSystemAppMenuModel::Build() {
   std::vector<crostini::ContainerId> containers =
       crostini::GetLinuxContainers(browser()->profile());
   for (const auto& container : containers) {
-    shortcuts_.push_back(crostini::ShortcutIdFromContainerId(container));
+    urls_.push_back(
+        crostini::GenerateTerminalURL(browser()->profile(), container));
     // Use label 'Linux' if we have only the default container, else use
     // <vm_name>:<container_name>.
     std::u16string label = l10n_util::GetStringUTF16(IDS_APP_TERMINAL_LINUX);
@@ -81,34 +83,11 @@ GURL TerminalSystemAppMenuModel::GetURLForCommand(int command_id) const {
   if (command_id == IDC_TERMINAL_HOME) {
     return GURL(base::StrCat(
         {chrome::kChromeUIUntrustedTerminalURL, "html/terminal_home.html"}));
-
   } else if (command_id >= ash::LAUNCH_APP_SHORTCUT_FIRST &&
              command_id <= ash::LAUNCH_APP_SHORTCUT_LAST) {
     int i = command_id - ash::LAUNCH_APP_SHORTCUT_FIRST;
-    std::string shortcut_id;
-    if (i < shortcuts_.size()) {
-      shortcut_id = shortcuts_[i];
-    }
-    auto shortcut = base::JSONReader::Read(shortcut_id);
-    if (!shortcut || !shortcut->is_dict()) {
-      return GURL();
-    }
-    const std::string* shortcut_value =
-        shortcut->FindStringKey(crostini::kShortcutKey);
-    if (shortcut_value && *shortcut_value == crostini::kShortcutValueSSH) {
-      const std::string* profileId =
-          shortcut->FindStringKey(crostini::kProfileIdKey);
-      if (!profileId) {
-        return GURL();
-      }
-      return GURL(
-          base::StrCat({chrome::kChromeUIUntrustedTerminalURL,
-                        "html/terminal_ssh.html#profile-id:", *profileId}));
-    }
-
-    if (shortcut_value && *shortcut_value == crostini::kShortcutValueTerminal) {
-      return crostini::GenerateTerminalURL(
-          browser()->profile(), crostini::ContainerId(std::move(*shortcut)));
+    if (i < urls_.size()) {
+      return urls_[i];
     }
   }
   return GURL();
