fe620d48f96af53db146420d1070afe76597292f
Igor Ruvinov <igorruvinov@chromium.org>
2022-03-17 21:10:11 +0000

    Migrate policy value accessors to typesafe versions.
    This covers various files under chrome/browser/*.
    
    Reasons for using "unsafe" over type-checking versions:
    - Handling multiple policies and types
    - Need to differentiate between nullptr (unset) and type mismatch
    
    Bug: 1294817, b/221918700
    Change-Id: I39e15650310f7fe7d706b6e0a6760181a3f90d39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530366
    Reviewed-by: Josh Simmons <jds@google.com>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982421}

--- chrome/browser/ssl/secure_origin_policy_handler.cc
+++ chrome/browser/ssl/secure_origin_policy_handler.cc
@@ -29,7 +29,8 @@ SecureOriginPolicyHandler::~SecureOriginPolicyHandler() = default;
 
 void SecureOriginPolicyHandler::ApplyPolicySettings(const PolicyMap& policies,
                                                     PrefValueMap* prefs) {
-  const base::Value* value = policies.GetValue(policy_name());
+  const base::Value* value =
+      policies.GetValue(policy_name(), base::Value::Type::LIST);
   if (!value)
     return;
 
