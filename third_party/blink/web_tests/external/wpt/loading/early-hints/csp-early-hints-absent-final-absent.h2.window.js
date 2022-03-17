68cbb5ee4a2993cf0c4060312eccb04217efe702
Kenichi Ishibashi <bashi@chromium.org>
2022-03-17 10:37:35 +0000

    Add basic CSP Early Hints WPTs
    The test scenario is that:
    - Navigate to the test page. The test page sends an Early Hints response
      to perform a cross origin preload with a CSP.
    - The page sends the final response with a CSP.
    - CSPs are configured to allow or disallow cross origin fetches, or
      may be absent.
    - The test page checks whether the preload is allowed/disallowed.
    
    Currently disallowed -> allowed cases are failing. Subsequent CLs would
    resolve the failures.
    
    Bug: 1305896
    Change-Id: I3a9ca4a06fe0b1c48f5a93c8881750295510886c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527873
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982123}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/csp-early-hints-absent-final-absent.h2.window.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/early-hints-helpers.sub.js
+
+test(() => {
+    const early_hints_policy = "absent";
+    const final_policy = "absent";
+    navigateToContentSecurityPolicyBasicTest(early_hints_policy, final_policy);
+});
