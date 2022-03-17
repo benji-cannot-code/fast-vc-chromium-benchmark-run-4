dd0d3b42442f2dd7fdfc29a024f48412f2e42674
Mike Taylor <miketaylr@chromium.org>
2022-03-17 21:01:06 +0000

    Fix spelling of kCriticalCH
    Change-Id: I79d940728721f9fe9d1404d9ea6fdb42a2b72319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3532959
    Reviewed-by: Aaron Tagliaboschi <aarontag@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982415}

--- chrome/browser/client_hints/client_hints_browsertest.cc
+++ chrome/browser/client_hints/client_hints_browsertest.cc
@@ -264,13 +264,13 @@ class AlternatingCriticalCHRequestHandler {
     status_code_ = status_code;
   }
 
-  static constexpr char kCriticaCH[] = "/critical-ch";
+  static constexpr char kCriticalCH[] = "/critical-ch";
 
  private:
   // A response that flips between two critical-ch headers
   std::unique_ptr<net::test_server::HttpResponse> DifferentCriticalCH(
       const net::test_server::HttpRequest& request) {
-    if (!base::StartsWith(request.relative_url, kCriticaCH))
+    if (!base::StartsWith(request.relative_url, kCriticalCH))
       return nullptr;
 
     request_count_++;
@@ -3217,7 +3217,7 @@ IN_PROC_BROWSER_TEST_F(CriticalClientHintsBrowserTest, OneRestartSingleOrigin) {
   base::HistogramTester histogram;
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(),
-      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticaCH)));
+      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticalCH)));
   histogram.ExpectBucketCount("ClientHints.CriticalCHRestart",
                               2 /*=kNavigationRestarted*/, 1);
   EXPECT_EQ(2, handler.request_count());
@@ -3238,10 +3238,10 @@ IN_PROC_BROWSER_TEST_F(CriticalClientHintsBrowserTest,
   base::HistogramTester histogram;
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(),
-      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticaCH)));
+      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticalCH)));
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(),
-      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticaCH)));
+      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticalCH)));
   histogram.ExpectBucketCount("ClientHints.CriticalCHRestart",
                               2 /*=kNavigationRestarted*/, 2);
   EXPECT_EQ(4, handler.request_count());
@@ -3383,7 +3383,7 @@ IN_PROC_BROWSER_TEST_P(CriticalClientHintsRedirectBrowserTest,
   base::HistogramTester histogram;
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(),
-      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticaCH)));
+      https_server.GetURL(AlternatingCriticalCHRequestHandler::kCriticalCH)));
   histogram.ExpectBucketCount("ClientHints.CriticalCHRestart",
                               2 /*=kNavigationRestarted*/, 1);
   EXPECT_EQ(2, handler.request_count());
@@ -3416,9 +3416,9 @@ IN_PROC_BROWSER_TEST_P(CriticalClientHintsRedirectBrowserTest,
   // This will send the two servers redirecting to each other in a loop until
   // the navigation redirect break is tripped.
   handler_1.SetRedirectLocation(
-      https_server_2.GetURL(AlternatingCriticalCHRequestHandler::kCriticaCH));
+      https_server_2.GetURL(AlternatingCriticalCHRequestHandler::kCriticalCH));
   handler_2.SetRedirectLocation(
-      https_server_1.GetURL(AlternatingCriticalCHRequestHandler::kCriticaCH));
+      https_server_1.GetURL(AlternatingCriticalCHRequestHandler::kCriticalCH));
 
   handler_1.SetStatusCode(GetParam());
   handler_2.SetStatusCode(GetParam());
@@ -3426,7 +3426,7 @@ IN_PROC_BROWSER_TEST_P(CriticalClientHintsRedirectBrowserTest,
   base::HistogramTester histogram;
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(),
-      https_server_1.GetURL(AlternatingCriticalCHRequestHandler::kCriticaCH)));
+      https_server_1.GetURL(AlternatingCriticalCHRequestHandler::kCriticalCH)));
   histogram.ExpectBucketCount("ClientHints.CriticalCHRestart",
                               2 /*=kNavigationRestarted*/, 2);
   EXPECT_EQ(net::URLRequest::kMaxRedirects,
