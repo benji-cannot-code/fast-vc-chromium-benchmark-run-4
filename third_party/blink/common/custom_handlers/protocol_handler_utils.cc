6e9f0acabf81e63655cda36ab5508180014aa394
Javier Fernández García-Boente <jfernandez@igalia.com>
2022-03-17 23:25:27 +0000

    Move the protocol handler url's security checks to blink
    The NavigatorContentUtils class implements the security checks in the
    VerifyCustomHandlerURLSecurity static function, which is a complete
    implementation of the steps 6 and 7 defined in the spec [1]. This code
    is expected to be run by the renderer process only.
    
    The ProtocolHandler class, now part of the Custom Handlers component,
    defines the IsValid method, which implements steps 6 and 7 (except the
    same origin restriction) and also the steps 1 and 2  of the mentioned
    spec [1]. For the record, the same origin restriction is implemented in
    the WebContentsImpl class. All this logic is expected to be run by the
    browser process only.
    
    In order to avoid unneeded code duplication and prevent
    inconsistencies, in this CL we are refactoring the code and move it to
    blink/common, so that we have the same logic for both the renderer and
    browser processes.
    
    It's worth mentioned that we've introduced GURL in the blink/renderer
    code, which is discouraged [2], but I think this is one of the cases
    that could fit in the exceptions described in the documentation.
    
    [1] https://html.spec.whatwg.org/multipage/system-state.html#normalize-protocol-handler-parameters
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/README.md
    
    Bug: 1303595
    Change-Id: I823d7832d1abf1ea1e59526b459df035104e80cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3507332
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#982488}

--- third_party/blink/common/custom_handlers/protocol_handler_utils.cc
+++ third_party/blink/common/custom_handlers/protocol_handler_utils.cc
@@ -8,13 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
+#include "services/network/public/cpp/is_potentially_trustworthy.h"
+#include "third_party/blink/public/common/scheme_registry.h"
+#include "url/gurl.h"
 
 namespace blink {
 
 bool IsValidCustomHandlerScheme(const base::StringPiece scheme,
-                                bool allow_ext_prefix,
-                                bool& has_custom_scheme_prefix) {
-  has_custom_scheme_prefix = false;
+                                ProtocolHandlerSecurityLevel security_level,
+                                bool* has_custom_scheme_prefix) {
+  bool allow_scheme_prefix =
+      (security_level >= ProtocolHandlerSecurityLevel::kExtensionFeatures);
+  if (has_custom_scheme_prefix)
+    *has_custom_scheme_prefix = false;
 
   static constexpr const char kWebPrefix[] = "web+";
   static constexpr const char kExtPrefix[] = "ext+";
@@ -22,10 +28,11 @@ bool IsValidCustomHandlerScheme(const base::StringPiece scheme,
   static constexpr const size_t kPrefixLength = std::size(kWebPrefix) - 1;
   if (base::StartsWith(scheme, kWebPrefix,
                        base::CompareCase::INSENSITIVE_ASCII) ||
-      (allow_ext_prefix &&
+      (allow_scheme_prefix &&
        base::StartsWith(scheme, kExtPrefix,
                         base::CompareCase::INSENSITIVE_ASCII))) {
-    has_custom_scheme_prefix = true;
+    if (has_custom_scheme_prefix)
+      *has_custom_scheme_prefix = true;
     // HTML5 requires that schemes with the |web+| prefix contain one or more
     // ASCII alphas after that prefix.
     auto scheme_name = scheme.substr(kPrefixLength);
@@ -45,6 +52,15 @@ bool IsValidCustomHandlerScheme(const base::StringPiece scheme,
       "sip",     "sms",    "smsto",  "ssb",  "ssh",  "tel",  "urn",
       "webcal",  "wtai",   "xmpp"};
   return base::Contains(kProtocolSafelist, base::ToLowerASCII(scheme));
+}
+
+bool IsAllowedCustomHandlerURL(const GURL& url,
+                               ProtocolHandlerSecurityLevel security_level) {
+  bool has_valid_scheme =
+      url.SchemeIsHTTPOrHTTPS() ||
+      (security_level == ProtocolHandlerSecurityLevel::kExtensionFeatures &&
+       CommonSchemeRegistry::IsExtensionScheme(url.scheme()));
+  return has_valid_scheme && network::IsUrlPotentiallyTrustworthy(url);
 }
 
 }  // namespace blink
