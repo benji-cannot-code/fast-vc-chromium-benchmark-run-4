6a554e6a89042530e418d64cf89d501871752b5c
Chris Lu <thegreenfrog@chromium.org>
2022-03-17 00:30:35 +0000

    [ios] Add tap gesture recognizer when refreshing Most Visited tiles
    Change-Id: I81d74a4a20e81d812ff8a752beb503b0faf08e15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3531079
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#981940}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller.mm
@@ -280,6 +280,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [view removeFromSuperview];
     }
     [self.mostVisitedViews removeAllObjects];
+    [self.mostVisitedTapRecognizers removeAllObjects];
   }
   NSInteger index = 0;
   for (ContentSuggestionsMostVisitedItem* item in configs) {
@@ -289,6 +290,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     view.menuProvider = self.menuProvider;
     [self.mostVisitedViews addObject:view];
     if (refreshingTiles) {
+      UITapGestureRecognizer* tapRecognizer = [[UITapGestureRecognizer alloc]
+          initWithTarget:self
+                  action:@selector(contentSuggestionsElementTapped:)];
+      [view addGestureRecognizer:tapRecognizer];
+      tapRecognizer.enabled = YES;
+      [self.mostVisitedTapRecognizers addObject:tapRecognizer];
       view.accessibilityIdentifier = [NSString
           stringWithFormat:
               @"%@%li",
