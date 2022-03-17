187b726e026050bcd38759ece4b15422ac58794f
Chris Lu <thegreenfrog@chromium.org>
2022-03-17 18:53:11 +0000

    [ios] Use multiple items for each Save Card legal message
    The current implementation does not apply links correctly when
    there are multiple legal messages, by concatenating the lines into one
    without updating the ranges to reflect the new link positions.
    
    As a solution, each legal message will be assigned to a separate
    TableViewTextLinkItem as this also complies with a need to paragraph-
    separate the text.
    
    Change-Id: If354f5b4c71563880fbdc1780c53d7ab67b49e0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3526978
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982299}

--- ios/chrome/browser/ui/table_view/cells/table_view_text_link_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_link_item.h
@@ -8,9 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#include <vector>
+
 #import "ios/chrome/browser/ui/table_view/cells/table_view_cell.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_item.h"
 
+class GURL;
 @class CrURL;
 @class TableViewTextLinkCell;
 
@@ -25,8 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface TableViewTextLinkItem : TableViewItem
 // Text being stored by this item.
 @property(nonatomic, readwrite, strong) NSString* text;
-// URL link being stored by this item. If empty or not valid no URL will be set.
-@property(nonatomic, readwrite, strong) CrURL* linkURL;
+// URL links being stored by this item.
+@property(nonatomic, assign) std::vector<GURL> linkURLs;
+// Character range for the links in |linkURLs|. Order should match order in
+// |linkURLs|.
+@property(nonatomic, strong) NSArray* linkRanges;
+
 @end
 
 // TableViewCell that displays a text label that might contain a link.
