e16716db63e3163fb01fc908727a119947e5c3f5
Domenic Denicola <domenic@chromium.org>
2022-03-17 20:11:23 +0000

    App history API to navigation API rename (6/n)
    See https://github.com/WICG/navigation-api/issues/83 and
    https://github.com/WICG/navigation-api/pull/203 for context.
    
    This CL tidies up remaining references to app history across the
    codebase, mostly in comments and in enum names. Some DOMException
    messages also were forgotten in previous passes, and needed to be
    updated.
    
    Bug: 1300246
    Change-Id: I10eeffb0cb6cc753679d45e450c8f5268bd500c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3523471
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982361}

--- third_party/blink/renderer/core/loader/frame_loader_types.h
+++ third_party/blink/renderer/core/loader/frame_loader_types.h
@@ -54,7 +54,7 @@ enum SinglePageAppNavigationType {
   kSPANavTypeHistoryPushStateOrReplaceState = 0,
   kSPANavTypeSameDocumentBackwardOrForward = 1,
   kSPANavTypeOtherFragmentNavigation = 2,
-  kSPANavTypeAppHistoryTransitionWhile = 3,
+  kSPANavTypeNavigationApiTransitionWhile = 3,
   kSPANavTypeCount
 };
 
--- third_party/blink/renderer/core/navigation_api/navigation_api.cc
+++ third_party/blink/renderer/core/navigation_api/navigation_api.cc
@@ -471,7 +471,7 @@ NavigationResult* NavigationApi::navigate(ScriptState* script_state,
     if (options->hasState()) {
       ExceptionState exception_state(
           script_state->GetIsolate(),
-          ExceptionContext::Context::kOperationInvoke, "AppHistory",
+          ExceptionContext::Context::kOperationInvoke, "Navigation",
           "navigate");
       serialized_state = SerializeState(options->state(), exception_state);
       if (exception_state.HadException()) {
@@ -502,7 +502,7 @@ NavigationResult* NavigationApi::reload(ScriptState* script_state,
     if (options->hasState()) {
       ExceptionState exception_state(
           script_state->GetIsolate(),
-          ExceptionContext::Context::kOperationInvoke, "AppHistory", "reload");
+          ExceptionContext::Context::kOperationInvoke, "Navigation", "reload");
       serialized_state = SerializeState(options->state(), exception_state);
       if (exception_state.HadException()) {
         NavigationResult* result =
@@ -560,7 +560,7 @@ NavigationResult* NavigationApi::traverseTo(ScriptState* script_state,
                                             const String& key,
                                             NavigationOptions* options) {
   if (DOMException* maybe_ex =
-          PerformSharedNavigationChecks("goTo()/back()/forward()")) {
+          PerformSharedNavigationChecks("traverseTo()/back()/forward()")) {
     return EarlyErrorResult(script_state, maybe_ex);
   }
 
@@ -779,7 +779,7 @@ NavigationApi::DispatchResult NavigationApi::DispatchNavigateEvent(
     // steps. Instead it does stuff like the loading spinner and use counters.
     GetSupplementable()->document()->Loader()->RunURLAndHistoryUpdateSteps(
         url, destination_item,
-        mojom::blink::SameDocumentNavigationType::kAppHistoryTransitionWhile,
+        mojom::blink::SameDocumentNavigationType::kNavigationApiTransitionWhile,
         state_object, type, is_browser_initiated, is_synchronously_committed);
   }
 
