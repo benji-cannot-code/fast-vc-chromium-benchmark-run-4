e1c880512a993568607dc03868d437b7a95c2d35
Henrique Nakashima <hnakashima@chromium.org>
2022-03-17 23:04:17 +0000

    [Android] Create @DoNotRevive annotation
    Tests annotated with this won't be run in Test Reviver.
    
    Bug: 1306975
    Change-Id: I4f853a8705f56fd6303ec6559174e1b97a3e1fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529253
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982479}

---
+++ base/test/android/javatests/src/org/chromium/base/test/util/DoNotRevive.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.base.test.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is for disabled tests that should not be run in Test Reviver. Tests that cause
+ * other tests to fail or caue problems to the testing the infrastructure should have this
+ * annotation. <p> This should be used in conjunction with @DisabledTest or @DisableIf to prevent a
+ * test from running on normal bots.
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DoNotRevive {
+    String reason();
+}
