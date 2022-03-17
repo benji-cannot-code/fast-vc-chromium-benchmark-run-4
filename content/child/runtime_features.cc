0bac29cf47c6b871c78458c25b755b316b0b80a9
Yao Xiao <yaoxia@chromium.org>
2022-03-17 21:07:29 +0000

    [Topics] Add document.browsingTopics() API
    - For now, the API returns an empty array.
    - Rely on browsertests in the short/medium term: it'd be hard to drive
    a web_test to test any real functions as it will involve querying the
    browsing history and executing a model, or we'd have to override some
    states.
    
    Explainer: https://github.com/jkarlin/topics
    
    Design doc:
    https://docs.google.com/document/d/12UEo6PgeySUgEpkaAoawPjhjR0GSz-XhSOXSmrMxV6U
    
    PoC CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/3416117
    
    Bug: 1294456
    Change-Id: I824d0bf609ca80147727d34b111bf9fa1b53d57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3514296
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982420}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -377,6 +377,7 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
           {"ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes",
            blink::features::
                kThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes},
+          {"TopicsAPI", blink::features::kBrowsingTopics},
           {"TrustedDOMTypes", features::kTrustedDOMTypes},
           {"UserAgentClientHint", blink::features::kUserAgentClientHint},
           {"ViewportHeightClientHintHeader",
