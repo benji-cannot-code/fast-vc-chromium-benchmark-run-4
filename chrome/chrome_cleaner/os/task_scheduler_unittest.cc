b84755ef8102777c731666cf21b43aff18876335
Tomasz Wiszkowski <ender@google.com>
2022-03-17 19:32:20 +0000

    [Sheriff] Disable TaskSchedulerTest.RunAProgramNow on Windows7
    Bug: 1307401
    Change-Id: I635ac1daf406fd05138fcaee3eda7f0c059fdcb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3533053
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Owners-Override: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#982329}

--- chrome/chrome_cleaner/os/task_scheduler_unittest.cc
+++ chrome/chrome_cleaner/os/task_scheduler_unittest.cc
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/chrome_cleaner/test/test_strings.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "base/win/windows_version.h"
+#endif
+
 namespace chrome_cleaner {
 
 namespace {
@@ -95,6 +99,13 @@ TEST_F(TaskSchedulerTests, DeleteAndIsRegistered) {
 }
 
 TEST_F(TaskSchedulerTests, RunAProgramNow) {
+#if BUILDFLAG(IS_WIN)
+    // TODO(crbug.com/1307401): Failing on Windows7.
+    if (base::win::GetVersion() <= base::win::Version::WIN7) {
+      return;
+    }
+#endif
+
   base::FilePath executable_path;
   ASSERT_TRUE(base::PathService::Get(base::DIR_EXE, &executable_path));
   base::CommandLine command_line(
