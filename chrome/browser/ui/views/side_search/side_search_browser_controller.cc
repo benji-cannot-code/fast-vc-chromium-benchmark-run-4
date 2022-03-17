e7a23d419094fe03f154641427f83b83eba3ddaa
Tom Lukaszewicz <tluk@chromium.org>
2022-03-17 21:02:11 +0000

    [side search] Update the header with light/dark mode support
    This CL updates the side panel header to support light/dark mode.
    This aligns with the current read later side panel behavior.
    
    dark:
    https://screenshot.googleplex.com/6R9A7yjKknRZGrU.png
    
    light:
    https://screenshot.googleplex.com/B8X7iDYmpvCmMcL.png
    
    Bug: 1307393
    Change-Id: Ib9b4408e2822f81ff55d59cf19ccdd651e9b3f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3533610
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982417}

--- chrome/browser/ui/views/side_search/side_search_browser_controller.cc
+++ chrome/browser/ui/views/side_search/side_search_browser_controller.cc
@@ -35,7 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/pointer/touch_ui_controller.h"
+#include "ui/color/color_id.h"
 #include "ui/gfx/image/canvas_image_source.h"
+#include "ui/views/animation/ink_drop.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/image_button_factory.h"
@@ -54,20 +56,6 @@ namespace {
 
 constexpr int kDefaultTouchableIconSize = 24;
 
-// Below are hardcoded color constants for the side panel. This is a UX decision
-// to ensure that the colors align with the tier 2 Google SRP which only
-// supports light mode. These are not intended to change to match the light/dark
-// system setting or custom theme colors.
-
-// White background to match the Google SRP.
-constexpr SkColor kHeaderBackgroundColor = SK_ColorWHITE;
-
-// Default light mode icon color for controls.
-constexpr SkColor kIconColor = gfx::kGoogleGrey700;
-
-// Default light mode separator color.
-constexpr SkColor kSeparatorColor = gfx::kGoogleGrey300;
-
 // Base header button class. Responds appropriately to touch ui changes.
 class HeaderButton : public views::ImageButton {
  public:
@@ -84,6 +72,13 @@ class HeaderButton : public views::ImageButton {
   }
   ~HeaderButton() override = default;
 
+  // views::ImageButton:
+  void OnThemeChanged() override {
+    ImageButton::OnThemeChanged();
+    views::InkDrop::Get(this)->SetBaseColor(
+        GetColorProvider()->GetColor(ui::kColorIcon));
+  }
+
   void UpdateIcon() {
     const int icon_size =
         ui::TouchUiController::Get()->touch_ui()
@@ -91,7 +86,11 @@ class HeaderButton : public views::ImageButton {
             : ChromeLayoutProvider::Get()->GetDistanceMetric(
                   ChromeDistanceMetric::
                       DISTANCE_SIDE_PANEL_HEADER_VECTOR_ICON_SIZE);
-    views::SetImageFromVectorIconWithColor(this, icon_, icon_size, kIconColor);
+    SetImageModel(Button::STATE_NORMAL, ui::ImageModel::FromVectorIcon(
+                                            icon_, ui::kColorIcon, icon_size));
+    SetImageModel(Button::STATE_DISABLED,
+                  ui::ImageModel::FromVectorIcon(icon_, ui::kColorIconDisabled,
+                                                 icon_size));
   }
 
  private:
@@ -212,7 +211,8 @@ class HeaderView : public views::View {
         views::kFlexBehaviorKey,
         views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
                                  views::MaximumFlexSizeRule::kPreferred));
-    SetBackground(views::CreateSolidBackground(kHeaderBackgroundColor));
+    SetBackground(
+        views::CreateThemedSolidBackground(this, ui::kColorDialogBackground));
     UpdateSpacing();
   }
   ~HeaderView() override = default;
@@ -247,12 +247,6 @@ class HeaderView : public views::View {
 BEGIN_METADATA(HeaderView, views::View)
 END_METADATA
 
-std::unique_ptr<views::Separator> CreateSeparator() {
-  auto separator = std::make_unique<views::Separator>();
-  separator->SetColor(kSeparatorColor);
-  return separator;
-}
-
 views::WebView* ConfigureSidePanel(views::View* side_panel,
                                    Profile* profile,
                                    Browser* browser,
@@ -262,7 +256,7 @@ views::WebView* ConfigureSidePanel(views::View* side_panel,
   container->SetCrossAxisAlignment(views::LayoutAlignment::kStretch);
   container->AddChildView(
       std::make_unique<HeaderView>(std::move(callback), browser));
-  container->AddChildView(CreateSeparator());
+  container->AddChildView(std::make_unique<views::Separator>());
 
   // The WebView will fill the remaining space after the header view has been
   // laid out.
