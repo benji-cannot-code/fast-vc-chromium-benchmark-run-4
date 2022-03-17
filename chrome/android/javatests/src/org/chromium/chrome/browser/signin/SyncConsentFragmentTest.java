726c4594ef87607644e9c64e1080a2e21911aafd
James Lee <ljjlee@google.com>
2022-03-17 18:10:06 +0000

    Get child account status for all sync consent screens
    Previously this was only populated when the screen is launched from FRE,
    as that's the only way a child user could access it.  With the changes
    to allow child users to have sync disabled, they can now access the
    consent screen from other entry points.
    
    Bug: 1304737
    Change-Id: Idd7b045a91612fa90412785d1e6334a70ba282df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3516012
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#982281}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SyncConsentFragmentTest.java
@@ -227,6 +227,26 @@ public class SyncConsentFragmentTest {
     @Test
     @LargeTest
     @Feature("RenderTest")
+    // This test is only relevant if child users do not have sync force-enabled (if they do, then
+    // they can only ever access this fragment from the FRE).
+    @EnableFeatures({ChromeFeatureList.ALLOW_SYNC_OFF_FOR_CHILD_ACCOUNTS})
+    public void testSyncConsentScreenWithChildAccount() throws IOException {
+        CoreAccountInfo accountInfo =
+                mAccountManagerTestRule.addChildTestAccountThenWaitForSignin();
+        mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_EMAIL);
+        mSyncConsentActivity = ActivityTestUtils.waitForActivity(
+                InstrumentationRegistry.getInstrumentation(), SyncConsentActivity.class, () -> {
+                    SyncConsentActivityLauncherImpl.get().launchActivityForPromoDefaultFlow(
+                            mChromeActivityTestRule.getActivity(),
+                            SigninAccessPoint.BOOKMARK_MANAGER, accountInfo.getEmail());
+                });
+        mRenderTestRule.render(mSyncConsentActivity.findViewById(R.id.fragment_container),
+                "sync_consent_fragment_child_account");
+    }
+
+    @Test
+    @LargeTest
+    @Feature("RenderTest")
     public void testSigninFREFragmentWithNoAccountsOnDevice() throws IOException {
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
@@ -264,7 +284,7 @@ public class SyncConsentFragmentTest {
     @Test
     @LargeTest
     @Feature("RenderTest")
-    public void testFragmentWithChildAccount() throws IOException {
+    public void testFRESyncConsentScreenWithChildAccount() throws IOException {
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
         mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_EMAIL);
@@ -287,7 +307,7 @@ public class SyncConsentFragmentTest {
     @LargeTest
     @Feature("RenderTest")
     @EnableFeatures({ChromeFeatureList.ALLOW_SYNC_OFF_FOR_CHILD_ACCOUNTS})
-    public void testFragmentWithChildAccountAllowSyncOff() throws IOException {
+    public void testFRESyncConsentScreenWithChildAccountAllowSyncOff() throws IOException {
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
         mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_EMAIL);
@@ -310,7 +330,7 @@ public class SyncConsentFragmentTest {
     @LargeTest
     @Feature("RenderTest")
     @CommandLineFlags.Remove({ChromeSwitches.FORCE_ENABLE_SIGNIN_FRE})
-    public void testFragmentWithChildAccountLegacy() throws IOException {
+    public void testFRESyncConsentScreenWithChildAccountLegacy() throws IOException {
         HistogramDelta startPageHistogram =
                 new HistogramDelta("Signin.SigninStartedAccessPoint", SigninAccessPoint.START_PAGE);
         mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_EMAIL);
