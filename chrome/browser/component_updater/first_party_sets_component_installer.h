06adef87aec83250a11dcc789ef018ea64c012b4
cfredric <cfredric@chromium.org>
2022-03-17 20:13:15 +0000

    Fix race between NetworkService creation and FPS component registration.
    Change-Id: Ib59a7bdd549621bdafdd862dbd802cb771ed97a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3533001
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982363}

--- chrome/browser/component_updater/first_party_sets_component_installer.h
+++ chrome/browser/component_updater/first_party_sets_component_installer.h
@@ -47,6 +47,7 @@ class FirstPartySetsComponentInstallerPolicy : public ComponentInstallerPolicy {
   static void ReconfigureAfterNetworkRestart(
       SetsReadyOnceCallback on_sets_ready);
 
+  // To be called once registration is complete.
   void OnRegistrationComplete();
 
   // Resets static state. Should only be used to clear state during testing.
@@ -69,6 +70,10 @@ class FirstPartySetsComponentInstallerPolicy : public ComponentInstallerPolicy {
                            LoadsSets_OnComponentReady);
   FRIEND_TEST_ALL_PREFIXES(FirstPartySetsComponentInstallerFeatureEnabledTest,
                            LoadsSets_OnNetworkRestart);
+  FRIEND_TEST_ALL_PREFIXES(FirstPartySetsComponentInstallerFeatureEnabledTest,
+                           ReconfigureNetworkBeforeComponentReady);
+  FRIEND_TEST_ALL_PREFIXES(FirstPartySetsComponentInstallerFeatureEnabledTest,
+                           ReconfigureNetworkBeforeRegistrationComplete);
   FRIEND_TEST_ALL_PREFIXES(FirstPartySetsComponentInstallerFeatureEnabledTest,
                            IgnoreNewSets_NoInitialComponent);
   FRIEND_TEST_ALL_PREFIXES(FirstPartySetsComponentInstallerFeatureEnabledTest,
