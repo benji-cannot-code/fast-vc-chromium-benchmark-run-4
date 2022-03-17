67b38b2aefee8ae617250b7ba0d95568cbab44d7
Wan-Teh Chang <wtc@google.com>
2022-03-17 23:42:58 +0000

    Roll src/third_party/dav1d/libdav1d/ b1a5189c9..56e7ffc0d (2 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/b1a5189c9d37..56e7ffc0dbe4
    
    $ git log b1a5189c9..56e7ffc0d --date=short --no-merges --format='%ad %ae %s'
    2022-03-09 wtc Set f->n_tile_data to 0 in dav1d_decode_frame()
    2022-03-12 jb NEWS
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=sandersd@chromium.org,cassew@chromium.org
    
    Change-Id: Ia70d9a1545e5a2826efdacd14430047a7672a51c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3534025
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#982501}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "0.9.2-172-gb1a5189"
+#define DAV1D_VERSION "0.9.2-174-g56e7ffc"
