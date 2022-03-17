74ee0fb0103fa4acf6975757e9929e2418413cbe
Sophie Chang <sophiechang@chromium.org>
2022-03-17 22:54:16 +0000

    Place the capability to delete the entity annotator on shutdown behind feature flag
    Bug: 1295187
    Change-Id: If93985c2427a96ceb8ce71a0f9b8b7d091b879ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3533027
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982470}

--- components/optimization_guide/core/page_entities_model_executor_impl.cc
+++ components/optimization_guide/core/page_entities_model_executor_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/timer/elapsed_timer.h"
 #include "components/optimization_guide/core/entity_annotator_native_library.h"
+#include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
 #include "components/optimization_guide/proto/page_entities_model_metadata.pb.h"
 
@@ -31,7 +32,9 @@ EntityAnnotatorHolder::EntityAnnotatorHolder(
 EntityAnnotatorHolder::~EntityAnnotatorHolder() {
   DCHECK(background_task_runner_->RunsTasksInCurrentSequence());
 
-  ResetEntityAnnotator();
+  if (features::ShouldResetPageEntitiesModelOnShutdown()) {
+    ResetEntityAnnotator();
+  }
 }
 
 void EntityAnnotatorHolder::
