0df46a768c7cc3770a893a1a9af6dcfabf19a4e6
Henrique Nakashima <hnakashima@chromium.org>
2022-03-17 22:05:53 +0000

    [Android] Fix Safe Mode checkpoints for multiple ChromeActivities
    Checkpoints are used to detect crashes during initialization.
    
    The creation of every ChromeActivity triggers the start checkpoint,
    but only the first ChromeActivity is triggering the end checkpoint due
    to the early return in ChromeCachedFlags#cacheNativeFlags().
    
    This CL triggers the end checkpoint in the early return path to fix
    this issue.
    
    Bug: 1199069
    Change-Id: Ia1dbf18fb7f8e1d62c844cf41fceee08100c857c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3528600
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982444}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFlagsSafeMode.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/CachedFlagsSafeMode.java
@@ -25,6 +25,7 @@ import org.chromium.components.version_info.VersionInfo;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -64,6 +65,9 @@ class CachedFlagsSafeMode {
 
     private AtomicInteger mBehavior = new AtomicInteger(Behavior.UNKNOWN);
 
+    private AtomicBoolean mStartCheckpointWritten = new AtomicBoolean(false);
+    private AtomicBoolean mEndCheckpointWritten = new AtomicBoolean(false);
+
     CachedFlagsSafeMode() {}
 
     /**
@@ -99,11 +103,20 @@ class CachedFlagsSafeMode {
     }
 
     /**
-     * Call at an early point in the path that leads to caching flags. If onFinishedCachingFlags()
+     * Call at an early point in the path that leads to caching flags. If onEndCheckpoint()
      * does not get called before the next run, this run will be considered a crash for purposes of
      * counting the crash streak and entering Safe Mode.
      */
     public void onStartOrResumeCheckpoint() {
+        if (mEndCheckpointWritten.get()) {
+            // Do not increment the streak if it was already reset.
+            return;
+        }
+        if (mStartCheckpointWritten.getAndSet(true)) {
+            // Limit to one increment per run.
+            return;
+        }
+
         SharedPreferencesManager.getInstance().incrementInt(
                 ChromePreferenceKeys.FLAGS_CRASH_STREAK_BEFORE_CACHE);
         RecordHistogram.recordEnumeratedHistogram(
@@ -115,6 +128,15 @@ class CachedFlagsSafeMode {
      * incremented in {@link #onStartOrResumeCheckpoint} but does not reset it.
      */
     public void onPauseCheckpoint() {
+        if (mEndCheckpointWritten.get()) {
+            // Do not change the streak if it was already reset.
+            return;
+        }
+        if (!mStartCheckpointWritten.getAndSet(false)) {
+            // Do not change the streak if it hasn't been incremented yet.
+            return;
+        }
+
         int currentStreak = SharedPreferencesManager.getInstance().readInt(
                 ChromePreferenceKeys.FLAGS_CRASH_STREAK_BEFORE_CACHE);
         assert currentStreak >= 0;
@@ -129,6 +151,11 @@ class CachedFlagsSafeMode {
      * be saved to be used in Safe Mode.
      */
     void onEndCheckpoint(ValuesReturned safeValuesReturned) {
+        if (mEndCheckpointWritten.getAndSet(true)) {
+            // Limit to one reset per run.
+            return;
+        }
+
         SharedPreferencesManager.getInstance().writeInt(
                 ChromePreferenceKeys.FLAGS_CRASH_STREAK_BEFORE_CACHE, 0);
 
@@ -219,6 +246,8 @@ class CachedFlagsSafeMode {
 
     void clearMemoryForTesting() {
         mBehavior.set(Behavior.UNKNOWN);
+        mStartCheckpointWritten.set(false);
+        mEndCheckpointWritten.set(false);
     }
 
     @SuppressLint({"ApplySharedPref"})
