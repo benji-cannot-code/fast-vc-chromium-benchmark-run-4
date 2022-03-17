acb9868ac6ec2c87165be2d2d54c58480703bd0f
Juliet Levesque <julietlevesque@google.com>
2022-03-17 23:53:49 +0000

    [Fast Pair] Expose APIs in the repository to check/write opt-in status
    Adds a CheckShouldSaveDevicesToAccount function and a
    UpdateSaveDevicesToAccountStatus function to the FastPairRepository to
    communicate with the FootprintsFetcher (and ultimately the Footprints
    server) the user's "opt-in status". The "opt-in status" reflects the
    user's preference of whether they want their devices saved to the
    Footprints server with Fast Pair, and it is synced across all devices
    that use their account.
    
    TEST=unit tests, will manually test in subsequent CL's when I hook
    this logic up to other classes with more UI
    
    Change-Id: I84c74d1aa7f2e7a280774ba94a96a1b65707d70a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3524083
    Reviewed-by: Shane Fitzpatrick <shanefitz@google.com>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#982510}

--- ash/quick_pair/repository/fast_pair/footprints_fetcher.h
+++ ash/quick_pair/repository/fast_pair/footprints_fetcher.h
@@ -35,8 +35,8 @@ class FootprintsFetcher {
   virtual ~FootprintsFetcher() = default;
 
   virtual void GetUserDevices(UserReadDevicesCallback callback) = 0;
-  virtual void AddUserDevice(nearby::fastpair::FastPairInfo info,
-                             AddDeviceCallback callback) = 0;
+  virtual void AddUserFastPairInfo(nearby::fastpair::FastPairInfo info,
+                                   AddDeviceCallback callback) = 0;
   virtual void DeleteUserDevice(const std::string& hex_account_key,
                                 DeleteDeviceCallback callback) = 0;
 };
