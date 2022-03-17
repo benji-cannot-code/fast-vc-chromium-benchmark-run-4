14c42a6aec81e1b8cbb4fa766af74ddd0d3eb110
Joey Arhar <jarhar@chromium.org>
2022-03-17 22:31:08 +0000

    Update hidden attribute IDL for hidden=until-found
    Bug: 1055002
    Fixed: 1280032
    Change-Id: I1cb9d80488a933546072daed704af87100d113cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3402520
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982460}

--- third_party/blink/renderer/core/html/html_element.h
+++ third_party/blink/renderer/core/html/html_element.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_ELEMENT_H_
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_union_boolean_string_unrestricteddouble_null.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/simulated_click_options.h"
@@ -173,6 +174,9 @@ class CORE_EXPORT HTMLElement : public Element {
   void UpdateDescendantDirectionality(TextDirection direction);
   void AdjustDirectionalityIfNeededAfterShadowRootChanged();
   void BeginParsingChildren() override;
+
+  V8UnionBooleanOrStringOrUnrestrictedDoubleOrNull* hidden() const;
+  void setHidden(const V8UnionBooleanOrStringOrUnrestrictedDoubleOrNull*);
 
  protected:
   enum AllowPercentage { kDontAllowPercentageValues, kAllowPercentageValues };
