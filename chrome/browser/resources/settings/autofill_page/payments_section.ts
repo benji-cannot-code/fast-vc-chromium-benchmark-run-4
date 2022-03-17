69f4a199d777721ddf55ed3d6e4728b6fdfa75a4
Siyu An <siyua@chromium.org>
2022-03-17 19:25:57 +0000

    [VCN] Fix settings page add card button
    Hitting an error so the button is not responding:
    
    "Uncaught TypeError: Cannot read properties of undefined (reading
    'isVirtualCardEnrollmentEligible')", source:
    chrome://settings/autofill_page/payments_section.js (288)
    
    It refers to line 405 before the change
    
    Bug: 1306633, 1281695
    Change-Id: I457978ef23ad1477b4b46e8fae8fcc8b6254127d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527555
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982324}

--- chrome/browser/resources/settings/autofill_page/payments_section.ts
+++ chrome/browser/resources/settings/autofill_page/payments_section.ts
@@ -398,8 +398,7 @@ export class SettingsPaymentsSectionElement extends
 
   private shouldShowAddVirtualCardButton_(): boolean {
     if (!this.virtualCardEnrollmentEnabled_ ||
-        this.activeCreditCard_ === null ||
-        this.activeCreditCard_.metadata === null) {
+        this.activeCreditCard_ === null || !this.activeCreditCard_!.metadata) {
       return false;
     }
     return !!this.activeCreditCard_!.metadata!
@@ -409,8 +408,7 @@ export class SettingsPaymentsSectionElement extends
 
   private shouldShowRemoveVirtualCardButton_(): boolean {
     if (!this.virtualCardEnrollmentEnabled_ ||
-        this.activeCreditCard_ === null ||
-        this.activeCreditCard_.metadata === null) {
+        this.activeCreditCard_ === null || !this.activeCreditCard_!.metadata) {
       return false;
     }
     return !!this.activeCreditCard_!.metadata!
