89839b47f5fefd752afc7a67ec6fd0624621d4f9
Sophie Chang <sophiechang@chromium.org>
2022-03-18 18:27:45 +0000

    Convert remaining on-device clustering backend tests to limit use of ScopedFeatureList
    This is flaky otherwise
    
    Change-Id: I0c821e45ab8aacad2b0211c9470fefbb27210b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537232
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982806}

--- components/history_clusters/core/content_annotations_cluster_processor_unittest.cc
+++ components/history_clusters/core/content_annotations_cluster_processor_unittest.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history_clusters/core/content_annotations_cluster_processor.h"
 
 #include "base/run_loop.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "components/history_clusters/core/clustering_test_utils.h"
+#include "components/history_clusters/core/config.h"
 #include "components/history_clusters/core/on_device_clustering_features.h"
 #include "components/search_engines/template_url_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -23,11 +23,10 @@ using ::testing::UnorderedElementsAre;
 class ContentAnnotationsClusterProcessorTest : public ::testing::Test {
  public:
   ContentAnnotationsClusterProcessorTest() {
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
-        features::kOnDeviceClustering,
-        {{"content_clustering_enabled", "true"},
-         {"use_content_clustering_intersection_similarity", "false"},
-         {"content_clustering_similarity_threshold", "0.5"}});
+    config_.content_clustering_enabled = true;
+    config_.content_cluster_on_intersection_similarity = false;
+    config_.content_clustering_similarity_threshold = 0.5;
+    SetConfigForTesting(config_);
   }
 
   void SetUp() override {
@@ -42,7 +41,7 @@ class ContentAnnotationsClusterProcessorTest : public ::testing::Test {
   }
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_;
+  Config config_;
   std::unique_ptr<ContentAnnotationsClusterProcessor> cluster_processor_;
 };
 
@@ -153,14 +152,13 @@ class ContentAnnotationsIntersectionMetricTest
     : public ContentAnnotationsClusterProcessorTest {
  public:
   ContentAnnotationsIntersectionMetricTest() {
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
-        features::kOnDeviceClustering,
-        {{"content_clustering_enabled", "true"},
-         {"content_clustering_interaction_similarity", "true"}});
+    config_.content_clustering_enabled = true;
+    config_.content_cluster_on_intersection_similarity = true;
+    SetConfigForTesting(config_);
   }
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_;
+  Config config_;
 };
 
 TEST_F(ContentAnnotationsIntersectionMetricTest, AboveThreshold) {
--- components/history_clusters/core/keyword_cluster_finalizer_unittest.cc
+++ components/history_clusters/core/keyword_cluster_finalizer_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "components/history_clusters/core/clustering_test_utils.h"
+#include "components/history_clusters/core/config.h"
 #include "components/history_clusters/core/on_device_clustering_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -21,10 +22,10 @@ class KeywordClusterFinalizerTest : public ::testing::Test {
  public:
   void SetUp() override {
     cluster_finalizer_ = std::make_unique<KeywordClusterFinalizer>();
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
-        features::kOnDeviceClustering,
-        {{"exclude_keywords_from_noisy_visits", "true"},
-         {"include_categories_in_keywords", "false"}});
+
+    config_.should_exclude_keywords_from_noisy_visits = true;
+    config_.should_include_categories_in_keywords = false;
+    SetConfigForTesting(config_);
   }
 
   void TearDown() override { cluster_finalizer_.reset(); }
@@ -34,7 +35,7 @@ class KeywordClusterFinalizerTest : public ::testing::Test {
   }
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_;
+  Config config_;
   std::unique_ptr<KeywordClusterFinalizer> cluster_finalizer_;
   base::test::TaskEnvironment task_environment_;
 };
@@ -77,10 +78,10 @@ class KeywordClusterFinalizerIncludeAllTest : public ::testing::Test {
  public:
   void SetUp() override {
     cluster_finalizer_ = std::make_unique<KeywordClusterFinalizer>();
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
-        features::kOnDeviceClustering,
-        {{"exclude_keywords_from_noisy_visits", "false"},
-         {"include_categories_in_keywords", "true"}});
+
+    config_.should_exclude_keywords_from_noisy_visits = false;
+    config_.should_include_categories_in_keywords = true;
+    SetConfigForTesting(config_);
   }
 
   void TearDown() override { cluster_finalizer_.reset(); }
@@ -90,7 +91,7 @@ class KeywordClusterFinalizerIncludeAllTest : public ::testing::Test {
   }
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_;
+  Config config_;
   std::unique_ptr<KeywordClusterFinalizer> cluster_finalizer_;
   base::test::TaskEnvironment task_environment_;
 };
