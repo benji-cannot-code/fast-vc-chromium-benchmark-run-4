1b54e984f927035f685b8181350977766c12f665
Olivier Li <olivierli@chromium.org>
2022-03-18 21:32:32 +0000

    Stop supporting custom executable paths in tools/mac/power.
    Using local executables instead of /Applications can create problems
    with the automation and should be avoided. When using binaries directly
    the OS will sometimes refuse to send AppleScript commands and the only
    fix is to switch to using the application from /Applications/
    
    Bug: 1224994
    Change-Id: Ifc13260fedcac9e958a05cc2b20970c24277e5eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536589
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982887}

--- tools/mac/power/benchmark.py
+++ tools/mac/power/benchmark.py
@@ -68,10 +68,6 @@ def main():
       dest='chrome_user_dir',
       action='store',
       help='The user data dir to pass to Chrome via --user-data-dir')
-  parser.add_argument('--chromium-path',
-                      dest='chromium_path',
-                      action='store',
-                      help='The path to Chromium.app')
 
   parser.add_argument('--verbose',
                       action='store_true',
@@ -103,8 +99,7 @@ def main():
     def BrowserFactory(browser_name, variation):
       return browsers.MakeBrowserDriver(browser_name,
                                         variation,
-                                        chrome_user_dir=args.chrome_user_dir,
-                                        chromium_path=args.chromium_path)
+                                        chrome_user_dir=args.chrome_user_dir)
 
     for scenario in IterScenarios(args.scenarios,
                                   BrowserFactory,
