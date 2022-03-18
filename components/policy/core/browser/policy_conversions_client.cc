b2d21932d5061fd9d4982f6870ec87568def8a94
Samuel Huang <huangs@chromium.org>
2022-03-18 13:51:28 +0000

    Revert "Hide default Google Update policies on chrome://policy"
    This reverts commit ae98cde11abe29f01f1a96feb389fcc6e1b1e10a.
    
    Reason for revert: CL causes compile error on {win-chrome, win64-chrome} because EnableDropDefaultValues() is undefined (likely should be SetDropDefaultValues()).
    
    Original change's description:
    > Hide default Google Update policies on chrome://policy
    >
    > Do not show the Google Update default policies on the policy page.
    >
    > Bug: 1222444
    > Change-Id: Ifccc34971da5de09a06f544418880929402e9c76
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3518887
    > Reviewed-by: Yann Dago <ydago@chromium.org>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Commit-Queue: Jinyoung Hur <hur.ims@navercorp.com>
    > Cr-Commit-Position: refs/heads/main@{#982539}
    
    Bug: 1222444
    Change-Id: Icd94f485b089b5bdeaa1417b4acd29af21db679a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3535661
    Auto-Submit: Samuel Huang <huangs@chromium.org>
    Owners-Override: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#982688}

--- components/policy/core/browser/policy_conversions_client.cc
+++ components/policy/core/browser/policy_conversions_client.cc
@@ -57,10 +57,6 @@ void PolicyConversionsClient::EnableUserPolicies(bool enabled) {
   user_policies_enabled_ = enabled;
 }
 
-void PolicyConversionsClient::SetDropDefaultValues(bool enabled) {
-  drop_default_values_enabled_ = enabled;
-}
-
 #if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
 void PolicyConversionsClient::SetUpdaterPolicies(
     std::unique_ptr<PolicyMap> policies) {
@@ -371,8 +367,6 @@ Value PolicyConversionsClient::GetPolicyValues(
     const std::string& policy_name = entry.first;
     const PolicyMap::Entry& policy = entry.second;
     if (policy.scope == POLICY_SCOPE_USER && !user_policies_enabled_)
-      continue;
-    if (policy.IsDefaultValue() && drop_default_values_enabled_)
       continue;
     base::Value value =
         GetPolicyValue(policy_name, policy, deprecated_policies,
