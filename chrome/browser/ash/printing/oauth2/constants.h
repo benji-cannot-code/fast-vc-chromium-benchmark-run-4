05fce25d60d75abf253ce4ebf6a8510c6e460bc6
Piotr Pawliczek <pawliczek@chromium.org>
2022-03-18 19:12:44 +0000

    OAuth2 for IPP: AuthorizationServerData
    This class is responsible for downloading metadata from the
    authorization server and registration of a new client if necessary.
    
    BUG=b:201442267
    TEST=unit_tests --gtest_filter=PrintingOAuth2AuthorizationServerData*
    
    Change-Id: I9c790b683424b07d48b3bea60b394b7a9567a686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3517497
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982828}

---
+++ chrome/browser/ash/printing/oauth2/constants.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_PRINTING_OAUTH2_CONSTANTS_H_
+#define CHROME_BROWSER_ASH_PRINTING_OAUTH2_CONSTANTS_H_
+
+namespace ash {
+namespace printing {
+namespace oauth2 {
+
+// TODO(pawliczek) - this value is not known yet.
+// This is the (internal) URL which the internet browser must be redirected to
+// to complete the authorization procedure.
+constexpr char kRedirectURI[] = "https://TODO.set.redirect.uri/for/ipp/oauth2";
+
+// When required, ChromeOS tries to register to Authorization Server with this
+// name.
+constexpr char kClientName[] = "ChromeOS";
+
+}  // namespace oauth2
+}  // namespace printing
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_PRINTING_OAUTH2_CONSTANTS_H_
