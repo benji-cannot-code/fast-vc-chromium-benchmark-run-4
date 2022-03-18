1391c6cb4b623e95e65eca757d198774ea473d7f
Daniel Cheng <dcheng@chromium.org>
2022-03-18 00:49:28 +0000

    Use //base shmem APIs in media::MojoSharedBufferVideoFrame.
    This also fixes the API for creating a MojoSharedBufferVideoFrame to
    not require heap allocating the offsets and strides, by changing the
    factory function to accept spans instead of vectors. This allows the
    Mojo deserialization code to avoid some copies + heap traffic.
    
    Bug: 1306358
    Change-Id: I3401403e883dd4f3cdd98908aa7c8f3b9c145d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3528200
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982530}

--- media/gpu/test/video_test_helpers.cc
+++ media/gpu/test/video_test_helpers.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_helpers.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/shared_memory_mapping.h"
+#include "base/memory/unsafe_shared_memory_region.h"
 #include "base/stl_util.h"
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "gpu/ipc/service/gpu_memory_buffer_factory.h"
@@ -365,9 +367,9 @@ bool EncodedDataHelper::HasConfigInfo(const uint8_t* data,
 
 struct AlignedDataHelper::VideoFrameData {
   VideoFrameData() = default;
-  VideoFrameData(mojo::ScopedSharedBufferHandle mojo_handle)
-      : mojo_handle(std::move(mojo_handle)) {}
-  VideoFrameData(gfx::GpuMemoryBufferHandle gmb_handle)
+  explicit VideoFrameData(base::UnsafeSharedMemoryRegion shmem_region)
+      : shmem_region(std::move(shmem_region)) {}
+  explicit VideoFrameData(gfx::GpuMemoryBufferHandle gmb_handle)
       : gmb_handle(std::move(gmb_handle)) {}
 
   VideoFrameData(VideoFrameData&&) = default;
@@ -375,7 +377,7 @@ struct AlignedDataHelper::VideoFrameData {
   VideoFrameData(const VideoFrameData&) = delete;
   VideoFrameData& operator=(const VideoFrameData&) = delete;
 
-  mojo::ScopedSharedBufferHandle mojo_handle;
+  base::UnsafeSharedMemoryRegion shmem_region;
   gfx::GpuMemoryBufferHandle gmb_handle;
 };
 
@@ -487,11 +489,10 @@ scoped_refptr<VideoFrame> AlignedDataHelper::GetNextFrame() {
         dummy_mailbox, base::DoNothing() /* mailbox_holder_release_cb_ */,
         frame_timestamp);
   } else {
-    const auto& mojo_handle = video_frame_data_[read_frame_index].mojo_handle;
-    auto dup_handle =
-        mojo_handle->Clone(mojo::SharedBufferHandle::AccessMode::READ_WRITE);
-    if (!dup_handle.is_valid()) {
-      LOG(ERROR) << "Failed duplicating mojo handle";
+    const auto& shmem_region = video_frame_data_[read_frame_index].shmem_region;
+    auto dup_region = shmem_region.Duplicate();
+    if (!dup_region.IsValid()) {
+      LOG(ERROR) << "Failed duplicating shmem region";
       return nullptr;
     }
 
@@ -503,10 +504,10 @@ scoped_refptr<VideoFrame> AlignedDataHelper::GetNextFrame() {
     }
     const size_t video_frame_size =
         layout_->planes().back().offset + layout_->planes().back().size;
+    DCHECK_EQ(video_frame_size, dup_region.GetSize());
     return MojoSharedBufferVideoFrame::Create(
         layout_->format(), layout_->coded_size(), visible_rect_, natural_size_,
-        std::move(dup_handle), video_frame_size, offsets, strides,
-        frame_timestamp);
+        std::move(dup_region), offsets, strides, frame_timestamp);
   }
 }
 
@@ -543,11 +544,11 @@ void AlignedDataHelper::InitializeAlignedMemoryFrames(
   const size_t num_planes = VideoFrame::NumPlanes(pixel_format);
   const uint8_t* src_frame_ptr = &stream[0];
   for (size_t i = 0; i < num_frames_; i++) {
-    auto handle = mojo::SharedBufferHandle::Create(video_frame_size);
-    ASSERT_TRUE(handle.is_valid()) << "Failed allocating a handle";
-    auto mapping = handle->Map(video_frame_size);
-    ASSERT_TRUE(!!mapping);
-    uint8_t* buffer = reinterpret_cast<uint8_t*>(mapping.get());
+    auto region = base::UnsafeSharedMemoryRegion::Create(video_frame_size);
+    ASSERT_TRUE(region.IsValid()) << "Failed allocating a region";
+    base::WritableSharedMemoryMapping mapping = region.Map();
+    ASSERT_TRUE(mapping.IsValid());
+    uint8_t* buffer = mapping.GetMemoryAs<uint8_t>();
     for (size_t j = 0; j < num_planes; j++) {
       auto src_plane_layout = src_layout.planes()[j];
       auto dst_plane_layout = layout_->planes()[j];
@@ -558,7 +559,7 @@ void AlignedDataHelper::InitializeAlignedMemoryFrames(
                         src_plane_rows[j]);
     }
     src_frame_ptr += src_video_frame_size;
-    video_frame_data_[i] = VideoFrameData(std::move(handle));
+    video_frame_data_[i] = VideoFrameData(std::move(region));
   }
 }
 
