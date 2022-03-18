806983b0256dc6e998d0d7f9d95ec01c9ff84ac2
David Bertoni <dbertoni@chromium.org>
2022-03-18 01:40:10 +0000

    [CodeHealth] Remove uses of ListValue::Append() in /chrome/browser/extensions/api/input_ime.
    This CL removes calls to this deprecated member function. When
    it's not too intrusive, this CL attempts to modernize the use
    of DictionaryValue and ListValue to base::Value::Dict and
    base::Value::List to avoid using Value::FromUniquePtrValue.
    
    This CL was uploaded by git cl split.
    
    R=keithlee@chromium.org
    
    Bug: 1187097
    Change-Id: I9570c55b00f5566c99a05c6288d021a33cc21545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3516996
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982549}

--- chrome/browser/extensions/api/input_ime/input_ime_api.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api.cc
@@ -167,7 +167,7 @@ ExtensionFunction::ResponseAction InputImeSetCompositionFunction::Run() {
                               segments, &error)) {
     std::unique_ptr<base::ListValue> results =
         std::make_unique<base::ListValue>();
-    results->Append(std::make_unique<base::Value>(false));
+    results->Append(false);
     return RespondNow(ErrorWithArguments(
         std::move(results), InformativeError(error, static_function_name())));
   }
@@ -188,7 +188,7 @@ ExtensionFunction::ResponseAction InputImeCommitTextFunction::Run() {
                           &error)) {
     std::unique_ptr<base::ListValue> results =
         std::make_unique<base::ListValue>();
-    results->Append(std::make_unique<base::Value>(false));
+    results->Append(false);
     return RespondNow(ErrorWithArguments(
         std::move(results), InformativeError(error, static_function_name())));
   }
