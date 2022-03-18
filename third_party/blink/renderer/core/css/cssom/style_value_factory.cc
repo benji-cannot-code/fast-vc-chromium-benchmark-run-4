051641ae312935108950fec0f64970972f8e2450
Rune Lillesveen <futhark@chromium.org>
2022-03-18 16:48:46 +0000

    [@container] Typed OM support for container-type
    Currently failures due to not supporting 'style' queries.
    
    Removed 'block-size' from container-type keywords.
    
    Bug: 1213890
    Change-Id: I2f5a340a576b619098122f16a7b0041ec793fd3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3535441
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982741}

--- third_party/blink/renderer/core/css/cssom/style_value_factory.cc
+++ third_party/blink/renderer/core/css/cssom/style_value_factory.cc
@@ -116,7 +116,8 @@ CSSStyleValue* CreateStyleValueWithPropertyInternal(CSSPropertyID property_id,
       return MakeGarbageCollected<CSSUnsupportedStyleValue>(
           CSSPropertyName(property_id), value);
     }
-    case CSSPropertyID::kContain: {
+    case CSSPropertyID::kContain:
+    case CSSPropertyID::kContainerType: {
       if (value.IsIdentifierValue())
         return CreateStyleValue(value);
 
@@ -229,6 +230,9 @@ CSSStyleValue* CreateStyleValueWithPropertyInternal(CSSPropertyID property_id,
 CSSStyleValue* CreateStyleValueWithProperty(CSSPropertyID property_id,
                                             const CSSValue& value) {
   DCHECK_NE(property_id, CSSPropertyID::kInvalid);
+
+  if (UNLIKELY(value.IsPendingSubstitutionValue()))
+    return nullptr;
 
   if (CSSStyleValue* style_value = CreateStyleValueWithoutProperty(value))
     return style_value;
