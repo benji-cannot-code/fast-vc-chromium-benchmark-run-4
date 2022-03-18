dbc012da353017b307e8fe7cb85eacc1dddea515
Yulun Wu <yulunwu@chromium.org>
2022-03-18 17:49:04 +0000

    Check for in progress animations when query is updated.
    This makes entering the fast animation state more reliable.
    
    Video comparison in bug.
    
    go/cros-search-result-animation-time
    
    Bug: 1306999
    Change-Id: I41d86926e3684d90dcb9f764e94a5aaa1807e718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3531134
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982782}

--- ash/app_list/views/productivity_launcher_search_view.h
+++ ash/app_list/views/productivity_launcher_search_view.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/app_list/app_list_model_provider.h"
+#include "ash/app_list/model/search/search_box_model.h"
+#include "ash/app_list/model/search/search_box_model_observer.h"
 #include "ash/app_list/views/search_result_container_view.h"
 #include "ash/ash_export.h"
 #include "base/timer/timer.h"
@@ -29,7 +31,8 @@ class SearchResultPageDialogController;
 class ASH_EXPORT ProductivityLauncherSearchView
     : public views::View,
       public SearchResultContainerView::Delegate,
-      public AppListModelProvider::Observer {
+      public AppListModelProvider::Observer,
+      public SearchBoxModelObserver {
  public:
   METADATA_HEADER(ProductivityLauncherSearchView);
 
@@ -53,6 +56,11 @@ class ASH_EXPORT ProductivityLauncherSearchView
   // AppListModelProvider::Observer:
   void OnActiveAppListModelsChanged(AppListModel* model,
                                     SearchModel* search_model) override;
+
+  // Overridden from SearchBoxModelObserver:
+  void Update() override;
+  void SearchEngineChanged() override;
+  void ShowAssistantChanged() override;
 
   // Returns true if there are search results that can be keyboard selected.
   bool CanSelectSearchResults();
@@ -131,6 +139,9 @@ class ASH_EXPORT ProductivityLauncherSearchView
 
   // The last reported number of search results shown by all containers.
   int last_search_result_count_ = 0;
+
+  base::ScopedObservation<SearchBoxModel, SearchBoxModelObserver>
+      search_box_model_observer_{this};
 };
 
 }  // namespace ash
