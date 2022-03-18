0f90164df0c969c7ed06d4dc5f5a80b1952cfdc2
Daniel Cheng <dcheng@chromium.org>
2022-03-18 01:26:24 +0000

    Trim down functionality in mojo::AssociatedInterfacePtrInfo.
    At this point, this is just a helper class for conveying info when
    binding and unbinding an associated remote endpoint, so remove all other
    functionality.
    
    Bug: 875030, 955171
    Change-Id: I7711b97deb306536fe4951f5a575ef1231c2ebec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3527063
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982544}

--- mojo/public/cpp/bindings/deprecated_interface_types_forward.h
+++ mojo/public/cpp/bindings/deprecated_interface_types_forward.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace mojo {
 
 template <typename Interface>
-class InterfacePtrInfo;
-template <typename Interface>
 class AssociatedInterfacePtrInfo;
 
 }  // namespace mojo
