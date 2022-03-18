e2286b567e34965f84b8c5fc790c51b71a96a954
Rachel Wong <wrong@chromium.org>
2022-03-18 00:43:30 +0000

    [answers] Display query in the title.
    This CL swaps answer info so that the query is displayed in the title,
    and the answer is in the secondary text.
    
    There is no change for:
    - weather answers, which have different UI
    - dictionary answers, which were already swapped.
    
    Additionally, query highlighting is performed on the title only.
    
    Bug: 1303428
    Change-Id: Icd87c0260f78f302c67641423fb0f016dd2a3ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530471
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982529}

--- chrome/browser/ui/app_list/search/omnibox_answer_result.h
+++ chrome/browser/ui/app_list/search/omnibox_answer_result.h
@@ -50,7 +50,6 @@ class OmniboxAnswerResult : public ChromeSearchResult,
   void FetchImage(const GURL& url);
 
   bool IsCalculatorResult() const;
-  bool IsDictionaryResult() const;
   bool IsWeatherResult() const;
 
   Profile* profile_;
