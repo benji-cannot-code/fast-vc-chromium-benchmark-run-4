99624712f51273cde2e4e8a8342396b4f30e8dcf
Alex Gough <ajgo@chromium.org>
2022-03-18 00:05:38 +0000

    Move ownership of job to policy
    1. Job no longer needs to dispense its internal handle via Take() so
    that method is removed.
    2. Job can now set active process limit on itself (rather than as a
    static method).
    3. Job gets IsValid() and GetHandle() methods.
    3. PolicyBase now owns the job it is associated with (reflecting the 1:1
    job:process:policy approach in Chromium).
    4. This inverts some policy/job methods called in broker services.
    5. Job now not needed in JobTracker as the policy owns it directly.
    
    Bug: 1108031
    Change-Id: Id5a237b6d1509cb474975775ce47fe791c2e7ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530356
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982515}

--- sandbox/win/src/sandbox_policy_base.h
+++ sandbox/win/src/sandbox_policy_base.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/win/src/app_container_base.h"
 #include "sandbox/win/src/handle_closer.h"
 #include "sandbox/win/src/ipc_tags.h"
+#include "sandbox/win/src/job.h"
 #include "sandbox/win/src/policy_engine_opcodes.h"
 #include "sandbox/win/src/policy_engine_params.h"
 #include "sandbox/win/src/sandbox_policy.h"
@@ -88,11 +89,18 @@ class PolicyBase final : public TargetPolicy {
 
   // Creates a Job object with the level specified in a previous call to
   // SetJobLevel().
-  ResultCode MakeJobObject(base::win::ScopedHandle* job);
+  ResultCode InitJob();
 
-  // Updates the active process limit on the job to zero. Has no effect
-  // if the job is allowed to spawn processes.
-  ResultCode DropActiveProcessLimit(base::win::ScopedHandle* job);
+  // Returns the handle for this policy's job, or INVALID_HANDLE_VALUE if the
+  // job is not initialized.
+  HANDLE GetJobHandle();
+
+  // Returns true if a job is associated with this policy.
+  bool HasJob();
+
+  // Updates the active process limit on the policy's job to zero.
+  // Has no effect if the job is allowed to spawn processes.
+  ResultCode DropActiveProcessLimit();
 
   // Creates the two tokens with the levels specified in a previous call to
   // SetTokenLevel(). Also creates a lowbox token if specified based on the
@@ -105,10 +113,9 @@ class PolicyBase final : public TargetPolicy {
   // call to TargetProcess::Init() is issued.
   ResultCode ApplyToTarget(std::unique_ptr<TargetProcess> target);
 
-  // Called when there are no more active processes in a Job.
-  // Removes a Job object associated with this policy and the target associated
-  // with the job. If a process is not in a job, call OnProcessFinished().
-  bool OnJobEmpty(HANDLE job);
+  // Called when there are no more active processes in the policy's Job.
+  // If a process is not in a job, call OnProcessFinished().
+  bool OnJobEmpty();
 
   // Called when a process no longer needs to be tracked. Processes in jobs
   // should be notified via OnJobEmpty instead.
@@ -187,6 +194,7 @@ class PolicyBase final : public TargetPolicy {
 
   HANDLE effective_token_;
   bool allow_no_sandbox_job_;
+  Job job_;
 };
 
 }  // namespace sandbox
