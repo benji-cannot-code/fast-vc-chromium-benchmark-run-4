4d2caf185604318da14e5d66a3f1c22f672e5619
Joey Arhar <jarhar@chromium.org>
2022-03-18 20:48:43 +0000

    Move question mark outside parenthesis in IDL
    https://github.com/whatwg/html/pull/7475#discussion_r828496291
    
    Bug: 1307051
    Change-Id: I458f0c74ac24a663a6504746e7bac15218a9a8ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3529820
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982876}

--- third_party/blink/renderer/core/html/html_element.h
+++ third_party/blink/renderer/core/html/html_element.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_ELEMENT_H_
 
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/blink/renderer/bindings/core/v8/v8_union_boolean_string_unrestricteddouble_null.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_union_boolean_string_unrestricteddouble.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/simulated_click_options.h"
@@ -175,8 +175,8 @@ class CORE_EXPORT HTMLElement : public Element {
   void AdjustDirectionalityIfNeededAfterShadowRootChanged();
   void BeginParsingChildren() override;
 
-  V8UnionBooleanOrStringOrUnrestrictedDoubleOrNull* hidden() const;
-  void setHidden(const V8UnionBooleanOrStringOrUnrestrictedDoubleOrNull*);
+  V8UnionBooleanOrStringOrUnrestrictedDouble* hidden() const;
+  void setHidden(const V8UnionBooleanOrStringOrUnrestrictedDouble*);
 
  protected:
   enum AllowPercentage { kDontAllowPercentageValues, kAllowPercentageValues };
