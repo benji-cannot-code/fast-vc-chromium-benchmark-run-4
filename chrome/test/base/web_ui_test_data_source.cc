eaabd33a2d172a074ab3d65b1d7fc1622af5defa
dpapad <dpapad@chromium.org>
2022-03-18 13:08:00 +0000

    Serve PDF Viewer tests from chrome://webui-test/.
     - Pass all PDF Viewer JS tests to ts_library(), still using JS.
     - Register all resulting JS files (output of the TS compiler) with
       chrome://webui-test/
     - Add chrome://webui-test/ to the allowlist of URLs that can be loaded
       by the PDF Viewer extension.
     - Modify PDFExtensionJSTest to
         1) Instantiate and register the chrome://webui-test/ data source.
         2) Load the corresponding .pak file into the resource bundle.
         3) Serve test files from chrome://webui-test/ instead of
            chrome-extension://<extension_id>/_test_resources/
    
    This is in preparation of migrating the tests to TypeScript.
    
    The chrome-extension://<extension_id>/_test_resources/ mechanism has
    very limiting (and fragile) logic when it comes to loading generated
    files (added back in [1]), and using the chrome://webui-test/
    WebUIDataSource is a much better alternative and already used by all
    other TS WebUIS (see more context in crbug.com/1260297).
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2337258
    
    Bug: 1305967,1260297
    Change-Id: I1638b725b5ddc7c46bd254c8977b2f536a671be2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3530692
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982679}

--- chrome/test/base/web_ui_test_data_source.cc
+++ chrome/test/base/web_ui_test_data_source.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/base/web_ui_test_data_source.h"
 
+#include "chrome/common/webui_url_constants.h"
 #include "chrome/test/data/grit/webui_generated_test_resources.h"
 #include "chrome/test/data/grit/webui_generated_test_resources_map.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -13,7 +14,7 @@ namespace webui {
 
 content::WebUIDataSource* CreateWebUITestDataSource() {
   content::WebUIDataSource* source =
-      content::WebUIDataSource::Create("webui-test");
+      content::WebUIDataSource::Create(chrome::kChromeUIWebUITestHost);
   source->AddResourcePaths(base::make_span(kWebuiGeneratedTestResources,
                                            kWebuiGeneratedTestResourcesSize));
 
