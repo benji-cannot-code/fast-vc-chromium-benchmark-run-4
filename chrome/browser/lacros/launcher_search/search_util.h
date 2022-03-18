9ea81529b68e9fc792d2a5c3cc9354e2d957efb3
Rachel Wong <wrong@chromium.org>
2022-03-18 01:28:59 +0000

    [launcher-lacros] Implement the core launcher search logic in lacros.
    This CL implements the launcher search interface by forwarding queries
    to an AutocompleteController. Results are then converted into
    SearchResult format to be sent over the crosapi.
    
    Follow-up work will implement favicons and open tab results.
    
    Testing: Unit tests for the util file. Also tested the ash-lacros
    connection using the skeleton in crrev.com/3403505, on an eve.
    
    Bug: 1228587
    Change-Id: Ibb3f4368b7589185f69e61582cd9cf0105ab4fc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3403880
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982545}

---
+++ chrome/browser/lacros/launcher_search/search_util.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_LACROS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
+#define CHROME_BROWSER_LACROS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
+
+#include "chromeos/crosapi/mojom/launcher_search.mojom.h"
+#include "components/omnibox/browser/autocomplete_match.h"
+
+namespace crosapi {
+
+// Creates an Omnibox answer card result from the AutocompleteMatch.
+mojom::SearchResultPtr CreateAnswerResult(const AutocompleteMatch& match);
+
+// Creates an Omnibox search result from the AutocompleteMatch.
+mojom::SearchResultPtr CreateResult(const AutocompleteMatch& match);
+
+}  // namespace crosapi
+
+#endif  // CHROME_BROWSER_LACROS_LAUNCHER_SEARCH_SEARCH_UTIL_H_
