ef09bedbfcce5b885c7f39d18f199ac364f6bc83
Olga Sharonova <olka@chromium.org>
2022-03-18 13:35:47 +0000

    Remove MinimizeAudioProcessingForUnusedOutput feature
    There is no intention to launch it at the moment, and it conflicts with
    chrome-wide echo cancellation work. See b/154437967 for details.
    
    Bug: 1284652, b/154437967
    Change-Id: I414405796d8ef4bfbc98330d512ecf92fd513b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3532151
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#982687}

--- third_party/blink/renderer/modules/mediastream/media_stream_audio_processor.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_audio_processor.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "media/base/audio_parameters.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
-#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/modules/webrtc/webrtc_logging.h"
 #include "third_party/blink/renderer/modules/webrtc/webrtc_audio_device_impl.h"
 #include "third_party/blink/renderer/platform/mediastream/aec_dump_agent_impl.h"
@@ -109,13 +108,6 @@ MediaStreamAudioProcessor::GetInputFormatForTesting() const {
 
 const media::AudioParameters& MediaStreamAudioProcessor::OutputFormat() const {
   return audio_processor_->OutputFormat();
-}
-
-void MediaStreamAudioProcessor::SetOutputWillBeMuted(bool muted) {
-  DCHECK(main_thread_runner_->BelongsToCurrentThread());
-  DCHECK(base::FeatureList::IsEnabled(
-      features::kMinimizeAudioProcessingForUnusedOutput));
-  audio_processor_->SetOutputWillBeMuted(muted);
 }
 
 void MediaStreamAudioProcessor::OnStartDump(base::File dump_file) {
