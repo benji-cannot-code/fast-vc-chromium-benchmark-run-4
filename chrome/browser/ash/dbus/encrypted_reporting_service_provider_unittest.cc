48bd4dd18ce35bcf44671130f9fa13e91e1dc049
Hong Xu <xuhong@chromium.org>
2022-03-19 06:03:40 +0000

    Add a placeholder matcher for 2 failed test cases
    They should be removed once the underlying bug is fixed.
    
    Bug: b:214040998
    Change-Id: Ifdc4edd49b9231ebae68283ee1f8f2a14ec09316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537065
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Hong Xu <xuhong@google.com>
    Auto-Submit: Hong Xu <xuhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#983026}

--- chrome/browser/ash/dbus/encrypted_reporting_service_provider_unittest.cc
+++ chrome/browser/ash/dbus/encrypted_reporting_service_provider_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "chrome/browser/policy/messaging_layer/upload/fake_upload_client.h"
 #include "chrome/browser/policy/messaging_layer/upload/upload_client.h"
+#include "chrome/browser/policy/messaging_layer/util/test.h"
 #include "chromeos/dbus/missive/missive_client.h"
 #include "chromeos/dbus/services/service_provider_test_helper.h"
 #include "components/policy/core/common/cloud/dm_token.h"
@@ -219,7 +220,16 @@ TEST_F(EncryptedReportingServiceProviderTest, SuccessfullyUploadsRecord) {
   EXPECT_CALL(*this, ReportSuccessfulUpload(
                          EqualsProto(record_.sequence_information()), _))
       .Times(1);
-  EXPECT_CALL(cloud_policy_client_, UploadEncryptedReport(_, _, _))
+  EXPECT_CALL(
+      cloud_policy_client_,
+      // TODO(b/225412986): IsDataUploadRequestValid() reports "requestId" is
+      // missing. Change the first matcher to IsDataUploadRequestValid() once
+      // fixed.
+      UploadEncryptedReport(
+          ::reporting::RequestValidityMatcherBuilder<>::CreateDataUpload()
+              .RemoveMatcher("request-id-matcher")
+              .Build(),
+          _, _))
       .WillOnce(WithArgs<0, 2>(
           Invoke([](base::Value::Dict request,
                     policy::CloudPolicyClient::ResponseCallback response_cb) {
