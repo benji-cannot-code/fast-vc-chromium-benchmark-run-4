56b2d47595cb09f1d2cfb1b597cc7136e295b02b
David Sanders <dsanders11@ucsbalum.com>
2022-03-20 17:00:59 +0000

    Remove unused include of thread_checker.h
    Also adds some includes to other files which were getting
    them transitively, to fix the build.
    
    Bug: 242216
    Change-Id: Ie802807b1a9e718620ebe0461e46fb4b0e34dd01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3538475
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983147}

--- third_party/blink/public/web/modules/mediastream/media_stream_video_source.h
+++ third_party/blink/public/web/modules/mediastream/media_stream_video_source.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/threading/thread_checker.h"
 #include "base/token.h"
 #include "build/build_config.h"
 #include "media/base/video_frame.h"
--- third_party/blink/renderer/platform/wtf/functional.h
+++ third_party/blink/renderer/platform/wtf/functional.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/dcheck_is_on.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/threading/thread_checker.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 #include "third_party/blink/renderer/platform/wtf/type_traits.h"
