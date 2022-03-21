3ea94619a484cf3ba3ef4af41e5180f0e9cfd03d
Alan Ding <alanding@google.com>
2022-03-21 09:18:26 +0000

    arc: Remove deprecated and unused file_system.mojom methods
    Also fix any previous mojom methods usage syntax issues.
    
    The counterpart Android CL is: http://ag/17263915
    
    BUG=b:220547241
    TEST=./out/Debug/unit_tests --gtest_filter=ArcFileSystemOperationRunnerTest*
    TEST=./out/Debug/unit_tests --gtest_filter=ArcContentFileSystemFileStreamReaderTest*
    TEST=./out/Debug/unit_tests --gtest_filter=ArcContentFileSystemFileStreamWriterTest*
    TEST=./out/Debug/unit_tests --gtest_filter=ArcAppsPublisherTest*
    
    Change-Id: I9acfe8ba9dc1dddec5e88cf17e1b4f53d0bf020e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3537569
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Alan Ding <alanding@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983233}

--- chrome/browser/ash/arc/fileapi/arc_file_system_operation_runner_unittest.cc
+++ chrome/browser/ash/arc/fileapi/arc_file_system_operation_runner_unittest.cc
@@ -124,16 +124,6 @@ class ArcFileSystemOperationRunnerTest : public testing::Test {
         base::BindOnce(
             [](int* counter, mojom::RootSizePtr root_size) { ++*counter; },
             counter));
-    runner_->OpenFileToRead(
-        GURL(kUrl),
-        base::BindOnce(
-            [](int* counter, mojo::ScopedHandle handle) { ++*counter; },
-            counter));
-    runner_->OpenFileToWrite(
-        GURL(kUrl),
-        base::BindOnce(
-            [](int* counter, mojo::ScopedHandle handle) { ++*counter; },
-            counter));
     runner_->OpenFileSessionToWrite(
         GURL(kUrl),
         base::BindOnce([](int* counter,
@@ -167,7 +157,7 @@ TEST_F(ArcFileSystemOperationRunnerTest, RunImmediately) {
   CallSetShouldDefer(false);
   CallAllFunctions(&counter);
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ(13, counter);
+  EXPECT_EQ(11, counter);
 }
 
 TEST_F(ArcFileSystemOperationRunnerTest, DeferAndRun) {
@@ -179,7 +169,7 @@ TEST_F(ArcFileSystemOperationRunnerTest, DeferAndRun) {
 
   CallSetShouldDefer(false);
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ(13, counter);
+  EXPECT_EQ(11, counter);
 }
 
 // TODO(nya,hidehiko): Check if we should keep this test.
@@ -204,7 +194,7 @@ TEST_F(ArcFileSystemOperationRunnerTest, FileInstanceUnavailable) {
   CallSetShouldDefer(false);
   CallAllFunctions(&counter);
   base::RunLoop().RunUntilIdle();
-  EXPECT_EQ(13, counter);
+  EXPECT_EQ(11, counter);
 }
 
 }  // namespace arc
