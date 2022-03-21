4c4a956e6f19383473bb19e3a012d876e8defda9
Nohemi Fernandez <fernandex@chromium.org>
2022-03-21 20:13:37 +0000

    Use base::flat_map for account capabilities C++ internal container.
    Follow-up to crrev.com/c/3497813.
    
    Fixed: 1304117
    Change-Id: I122413bf08d035f906e1f48b8a9cf1c2f7494c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3540741
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983466}

--- components/signin/public/identity_manager/account_capabilities.h
+++ components/signin/public/identity_manager/account_capabilities.h
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SIGNIN_PUBLIC_IDENTITY_MANAGER_ACCOUNT_CAPABILITIES_H_
 #define COMPONENTS_SIGNIN_PUBLIC_IDENTITY_MANAGER_ACCOUNT_CAPABILITIES_H_
 
-#include <map>
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_map.h"
 #include "build/build_config.h"
 #include "components/signin/public/identity_manager/tribool.h"
 #include "third_party/abseil-cpp/absl/types/optional.h"
@@ -76,7 +76,7 @@ class AccountCapabilities {
   // Returns the list of account capability service names supported in Chrome.
   static const std::vector<std::string>& GetSupportedAccountCapabilityNames();
 
-  std::map<std::string, bool> capabilities_map_;
+  base::flat_map<std::string, bool> capabilities_map_;
 };
 
 #endif  // COMPONENTS_SIGNIN_PUBLIC_IDENTITY_MANAGER_ACCOUNT_CAPABILITIES_H_
