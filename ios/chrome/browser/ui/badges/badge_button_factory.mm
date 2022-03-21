6b34b9b773bc627e07db714eabcfef6afcaea696
ginnnnnnny <ginnyhuang@chromium.org>
2022-03-21 17:23:38 +0000

    [iOS] Add test case for new overflow popup menu
    This CL adds support for the new popup menu in the badge view controller
    in Showcase. Since the showcase app is not supposed to support flags,
    a switch is added in the view to control whether the legacy or the new
    overflow menu shows. To do so, the class "BadgeContainerViewController"
    is moved to a separate file.
    
    New test cases are also added to check the display of the overflow
    badge with the new popup menu.
    
    This CL also fixes the bug that tapping the new overflow badge would
    not invoke the delegate selector |overflowBadgeButtonTapped:|
    
    Fixed: 1293060
    Change-Id: I3c049056a03953fd6bae3f01ad5c60b063ba1839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3536594
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#983366}

--- ios/chrome/browser/ui/badges/badge_button_factory.mm
+++ ios/chrome/browser/ui/badges/badge_button_factory.mm
@@ -157,9 +157,6 @@ const CGFloat kSymbolImagePointSize = 18;
                           image:[[UIImage imageNamed:@"wrench_badge"]
                                     imageWithRenderingMode:
                                         UIImageRenderingModeAlwaysTemplate]];
-  [button addTarget:self.delegate
-                action:@selector(overflowBadgeButtonTapped:)
-      forControlEvents:UIControlEventTouchUpInside];
   button.accessibilityIdentifier = kBadgeButtonOverflowAccessibilityIdentifier;
   button.accessibilityLabel =
       l10n_util::GetNSString(IDS_IOS_OVERFLOW_BADGE_HINT);
@@ -181,6 +178,7 @@ const CGFloat kSymbolImagePointSize = 18;
       [weakSelf.delegate showModalForBadgeType:badgeType];
     };
     void (^buttonTapHandler)(UIAction*) = ^(UIAction* action) {
+      [weakSelf.delegate overflowBadgeButtonTapped:weakButton];
       weakButton.menu = GetOverflowMenuFromBadgeTypes(
           weakSelf.delegate.badgeTypesForOverflowMenu, showModalFunction);
     };
@@ -193,6 +191,10 @@ const CGFloat kSymbolImagePointSize = 18;
     // Attach the action to the button.
     [button addAction:action
         forControlEvents:UIControlEventMenuActionTriggered];
+  } else {
+    [button addTarget:self.delegate
+                  action:@selector(overflowBadgeButtonTapped:)
+        forControlEvents:UIControlEventTouchUpInside];
   }
   return button;
 }
--- ios/chrome/browser/ui/badges/badge_consumer.h
+++ ios/chrome/browser/ui/badges/badge_consumer.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_BADGES_BADGE_CONSUMER_H_
 #define IOS_CHROME_BROWSER_UI_BADGES_BADGE_CONSUMER_H_
 
+#import <Foundation/Foundation.h>
+
 @protocol BadgeItem;
 
 // Consumer protocol for the view controller that displays badges.
